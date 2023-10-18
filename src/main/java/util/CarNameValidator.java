0bbe89647575e37e00ca58d4dfce75a383a5d00d
seokhongkim <seokhong.kim@woowahan.com>
2023-10-18 15:05:24 +0900

    java-racingcar_test commit

---
+++ src/main/java/util/CarNameValidator.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.util.List;
+
+public class CarNameValidator {
+    public static final int MIN_CAR_NUMBER = 2;
+    public static final int MAX_NAME_LENGTH = 5;
+
+    private CarNameValidator() {
+    }
+
+    public static void validate(List<String> names) {
+        validateCarNumber(names);
+        names.forEach(CarNameValidator::validateCarNameLength);
+    }
+
+    private static void validateCarNumber(List<String> names) {
+        if (names.size() < MIN_CAR_NUMBER) {
+            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NUMBER.message);
+        }
+    }
+
+    private static void validateCarNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NAME_LENGTH.message);
+        }
+    }
+
+    public enum Message {
+        EXCEPTION_CAR_NUMBER("2개 이상의 자동차를 입력해 주세요."),
+        EXCEPTION_CAR_NAME_LENGTH("5자 이하의 이름을 입력해주세요.");
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+}
---
+++ src/test/java/util/CarNameValidatorTest.java
@@ -0,0 +1,53 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import util.CarNameValidator.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarNameValidatorTest {
+
+    @Nested
+    @DisplayName("비정상 입력값이 들어온 케이스")
+    class invalidInputTest {
+
+        @Test
+        @DisplayName("경주에 참여하는 자동차가 1대 이하면 예외처리 한다.")
+        void carNumberTest() {
+            List<String> names = new ArrayList<>(List.of("깃짱"));
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> CarNameValidator.validate(names))
+                    .withMessage(Message.EXCEPTION_CAR_NUMBER.getMessage());
+        }
+
+        @Test
+        @DisplayName("6글자 이상의 이름은 예외 처리한다.")
+        void carNameLengthTest() {
+            List<String> names = new ArrayList<>(List.of("깃짱", "이리내이리내"));
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> CarNameValidator.validate(names))
+                    .withMessage(Message.EXCEPTION_CAR_NAME_LENGTH.getMessage());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("정상 입력값이 들어온 케이스")
+    class validInputTest {
+        @Test
+        @DisplayName("올바른 입력값이 들어왔을 때 잘 작동하는지 검사한다.")
+        void 정상_입력() {
+            List<String> names = new ArrayList<>(List.of("깃짱", "이리내"));
+            assertThatCode(() -> CarNameValidator.validate(names))
+                    .doesNotThrowAnyException();
+        }
+
+    }
+
+}
