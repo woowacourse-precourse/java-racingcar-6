a356364e09c226c7c767c330a16100303980c422
song-jae-wan <sjo9810@gmail.com>
2023-10-26 23:11:00 +0900

    Feat: 게임 컨트롤러 구현
    race() : 회수에 따른 결과를 모델로 만들어 뷰로 전달
    receResult() : 우승자를 모델로 만들어
    뷰로 전달

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,18 +1,32 @@
 package controller;
 
+import service.GameService;
+
 import view.GameView;
 
 public class GameController {
 
     private final GameView gameView;
+    private final GameService gameService;
 
     public GameController() {
         gameView = new GameView();
+        gameService = new GameService();
     }
 
-    public void raceStart(String[] name, int count) {
+    public void raceSet(String[] names) {
         gameView.printResult();
+        gameService.fillNames(names);
+    }
 
+    public void race(int count) {
+        String result = gameService.raceResult(count);
+        gameView.printResult(result);
+    }
+
+    public void raceResult() {
+        String winners = gameService.getWinners();
+        gameView.printWinners(winners);
     }
 
 }
