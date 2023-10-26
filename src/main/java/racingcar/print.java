9b42a1114168f7613eeae11ad1412bde38f57a33
idingg <idingg@gmail.com>
2023-10-26 21:21:44 +0900

    refactor: Class Print access modifier 수정
    Class Print의 access modifier
    public에서 default 로 변경

--- src/main/java/racingcar/print.java
+++ src/main/java/racingcar/print.java
@@ -2,12 +2,12 @@ package racingcar;
 
 import java.util.List;
 
-public class Print {
-    public static void start() {
+class Print {
+    static void startString() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void repetitions() {
+    static void repetitionsString() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
@@ -16,14 +16,14 @@ public class Print {
         System.out.println("-".repeat(car.getPosition()));
     }
 
-    public static void raceSituation(Race race) {
+    static void raceSituation(Race race) {
         List<Car> cars = race.getCars();
         for (Car car : cars) {
             carStatus(car);
         }
     }
 
-    public static void winner(List<Car> winner) {
+    static void winner(List<Car> winner) {
         List<String> winnerName = winner.stream()
                 .map(Car::getName)
                 .toList();
