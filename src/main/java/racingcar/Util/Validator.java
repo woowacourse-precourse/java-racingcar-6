9bb1cc26ee1ceee51ae25bd0bc4276c5dd190fe0
Firedrago95 <yonghwa95@naver.com>
2023-10-26 16:12:02 +0900

    feat : 입력값의 유효성검사 2. 쉼표로 구분된 문자열인지 확인하는 기능 구현

--- src/main/java/racingcar/Util/Validator.java
+++ src/main/java/racingcar/Util/Validator.java
@@ -1,11 +1,17 @@
 package racingcar.Util;
 
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
 
     public static final String NULL_OR_EMPTY_MESSAGE = "값을 입력해주세요";
+    public static final String INVALID_PATTERN_MESSAGE = "자동차의 이름은 쉼표(,)로 구분해서 입력해주세요";
 
     public static void validateCarNames(String inputedCarNames) {
         validateNullOrEmpty(inputedCarNames);
+        validateCorrectPattern(inputedCarNames);
     }
 
     public static void validateNullOrEmpty(String inputedCarNames) {
@@ -13,4 +19,13 @@ public class Validator {
             throw new IllegalArgumentException(NULL_OR_EMPTY_MESSAGE);
         }
     }
+
+    public static void validateCorrectPattern(String inputedCarNames) {
+        Pattern pattern = Pattern.compile("^[\\w]{1,}(,[\\w]{1,})*$");
+        Matcher matcher = pattern.matcher(inputedCarNames);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INVALID_PATTERN_MESSAGE);
+        }
+    }
+
 }
