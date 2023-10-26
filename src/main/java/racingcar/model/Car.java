e44f9e6443e4f24d7cce3b775df2ac5c5be5c1d6
kim <hwiyul_g@daum.net>
2023-10-26 23:14:18 +0900

    feat(Car) : 자동차 이동 로직 함수 작성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,6 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 
+    private final String carName;
+    private final int position;
+
+    public Car(String carName, int position) {
+        this.carName = carName;
+        this.position = position;
+    }
+
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
@@ -11,5 +19,12 @@ public class Car {
     private boolean isCarMoving() {
         int randomNumber = generateRandomNumber();
         return randomNumber > 3;
+    }
+
+    public Car attemptMove() {
+        if (isCarMoving()) {
+            return new Car(carName, position + 1);
+        }
+        return new Car(carName, position);
     }
 }
