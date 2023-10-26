7c6bab0c571e9a27deaeb500571c5d32cb382bbd
euics <pjh575157@gmail.com>
2023-10-26 20:57:53 +0900

    test: testConstructor 메서드 테스트 코드 작성
    testConstructor 메서드 테스트 코드 추가.
    - Race 객체 생성 후, Car 객체 리스트와 동일한지 검증

---
+++ src/test/java/racingcar/model/RaceTest.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceTest {
+    private List<Car> cars;
+    private Race race;
+
+    @BeforeEach
+    public void setUp() {
+        cars = Arrays.asList(new Car("pobi"), new Car("woni"), new Car("jun"));
+        race = new Race(cars);
+    }
+
+    @Test
+    @DisplayName("Race 객체 생성 후, Car 객체 리스트와 동일한지 검증")
+    public void testConstructor() {
+        assertNotNull(race.getCars());
+        assertEquals(cars, race.getCars());
+    }
+}
