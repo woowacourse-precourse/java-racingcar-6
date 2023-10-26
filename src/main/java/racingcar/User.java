d881c0095b40603ac1acc1f5a61673542eb798e0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-27 02:37:20 +0900

    feat: 사용할 횟수가 숫자가 아닐 경우 예외 처리 기능 구현

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -21,9 +21,11 @@ public class User {
 
     public int getNumberOfMovesFromUser() {
 
-        int numberOfTimes = Integer.parseInt(Console.readLine());
+        String numberOfTimes = Console.readLine();
 
-        return numberOfTimes;
+        validateNumberOfTimesIsNumeric(numberOfTimes);
+
+        return Integer.parseInt(numberOfTimes);
     }
 
 
@@ -36,6 +38,15 @@ public class User {
                 throw new IllegalArgumentException("자동차 이름은 5글자 이하로만 가능합니다.");
 
             }
+        }
+    }
+
+    private void validateNumberOfTimesIsNumeric(String numberOfTimes) {
+
+        if (!numberOfTimes.chars().allMatch(Character::isDigit)) {
+
+            throw new IllegalArgumentException("사용할 횟수는 숫자로만 가능합니다.");
+
         }
     }
 }
