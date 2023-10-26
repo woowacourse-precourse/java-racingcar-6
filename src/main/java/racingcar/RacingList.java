50f4c1d031e7f83ea7ebf0381be148b28af0d5fe
leedonghyun <skydh507@ajou.ac.kr>
2023-10-27 04:08:11 +0900

    feat : 입력 길이 제한 예외 처리 기능 구현

--- src/main/java/racingcar/RacingList.java
+++ src/main/java/racingcar/RacingList.java
@@ -9,11 +9,19 @@ public class RacingList {
     public void inputCarList(String input) {
         String[] list = input.split(",");
         for (String s : list) {
+            sizeValidate(s);
             carList.put(s, 0);
         }
     }
 
     public LinkedHashMap<String, Integer> getCarList() {
         return carList;
+    }
+
+    public void sizeValidate(String input) {
+        int size = input.length();
+        if (size > 5) {
+            throw new IllegalArgumentException("이름의 길이가 5 이상입니다.");
+        }
     }
 }
