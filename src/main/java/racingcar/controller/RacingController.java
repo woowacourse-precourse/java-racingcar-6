8ab412499564eb54967570fc564eeb9b2dd64d07
anneu <b.imnuj@gmail.com>
2023-10-27 00:11:37 +0900

    feat: 경주에 참여할 자동차 목록을 만드는 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,9 +1,15 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.view.Input;
 import racingcar.view.Output;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RacingController {
+
+    private static final String CAR_NAME_DELIMITER = ",";
 
     private final Output output = new Output();
     private final Input input = new Input();
@@ -11,7 +17,16 @@ public class RacingController {
     public void run() {
         output.showCarNamesInputMessage();
         String carNames = input.readCarNames();
+
+        List<Car> cars = makeCars(carNames);
+
         output.showCountInputMessage();
         String count = input.readCount();
+    }
+
+    private List<Car> makeCars(String carNames) {
+        return Arrays.stream(carNames.split(CAR_NAME_DELIMITER))
+                .map(Car::new)
+                .toList();
     }
 }
