f9935ac2a82e16c04b257f39c8f822b054a237c0
heiler <threepebbles@naver.com>
2023-10-27 00:19:46 +0900

    feat: CarList 모델(일급컬렉션) 추가

---
+++ src/main/java/racingcar/model/CarList.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class CarList {
+    private List<Car> carList;
+
+    public CarList(List<Car> carList) {
+        this.carList = carList;
+    }
+}
