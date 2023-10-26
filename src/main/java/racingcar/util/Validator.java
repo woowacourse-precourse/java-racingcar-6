e96069173d749bbdd08c30b565a3e6289dbd7be8
junOTL <naqp99@gmail.com>
2023-10-27 01:19:39 +0900

    feat: 이름의 길이가 6자 이상인 경우 예외 발생 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -6,4 +6,10 @@ public class Validator {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_VALUE);
         }
     }
+
+    public static void isLengthLessThanFive(String input) {
+        if (input.replaceAll("\\s", "").length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
 }
