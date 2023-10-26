6f5becef3ada9fcd60cdee0ab9d4c609e395cdf7
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 21:08:32 +0900

    feat(MoveCountValidator) : null 값 입력 예외 검증 기능 추가

--- src/main/java/racingcar/validator/MoveCountValidator.java
+++ src/main/java/racingcar/validator/MoveCountValidator.java
@@ -5,6 +5,12 @@ public class MoveCountValidator {
     private MoveCountValidator() {
     }
 
+    public static void validateNull(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("아무런 값을 입력하지 않았습니다.");
+        }
+    }
+
     public static void validateInputFormat(String input) {
         if (!input.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("횟수는 숫자만 입력할 수 있습니다.");
