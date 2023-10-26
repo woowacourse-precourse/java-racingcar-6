f7bb4503993f5f6b008a2e9720d11c223000e6c4
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 21:17:16 +0900

    feat(InputView) : 자동차 이동 횟수 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.validator.CarNameValidator;
+import racingcar.validator.MoveCountValidator;
 
 public class InputView {
 
@@ -11,6 +12,12 @@ public class InputView {
     public static String inputCarNames() {
         String input = Console.readLine();
         CarNameValidator.validate(input);
+        return input;
+    }
+
+    public static String inputMoveCount() {
+        String input = Console.readLine();
+        MoveCountValidator.validate(input);
         return input;
     }
 }
