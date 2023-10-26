ed54e2aa12eb64942a6a98eb53bdeda67050f96c
minseo  jo <liging12@naver.com>
2023-10-27 00:28:57 +0900

    feat: RacingValidator 기능 추가
    Integer.parseInt(input) 는 input(String) 을 int 형으로 바꾸고, int 형 정수로 바꾸지 못하면, NumberFormatException 을 발생 시킨다.
    NumberFormatException 의 조상은 IllegalArgumentException 다.
    
    - 숫자가 아닌 경우 > validateNumericException()
    - 공백인 경우 > validateBlankException()
    - 공백을 포함한 문자열인 경우 > validateNumericException()
    - 0인 경우 (0인 경우 아무도 움직이지 않고, 공동 우승 이므로, 0인 경우는 나는 예외 처리르 했다) > validatePositiveNumber()
    - 음수인 경우 > validatePositiveNumber()
    - int 형 오버플로우 예외 > validateIntegerOverflowException
    - 소수점 예외 > validateNumericException()

---
+++ src/main/java/racingcar/exception/RacingValidator.java
@@ -0,0 +1,49 @@
+package racingcar.exception;
+
+public class RacingValidator {
+
+    private static final int MAX_INT_VALUE = 2_147_483_647;
+    private static final int MIN_INT_VALUE = 1;
+
+    private static final String ERROR = "[ERROR] ";
+    private static final String ERROR_BLANK = ERROR + "공백을 허용하지 않습니다.";
+    private static final String ERROR_NON_NUMERIC = ERROR + "숫자를 입력해 주세요.";
+    private static final String ERROR_NON_ZERO = ERROR + "1 이상인 양수를 입력해 주세요.";
+    private static final String ERROR_INT_OVERFLOW = ERROR + "입력 값은 최대 2,147,483,648 입니다.";
+
+
+    public static void validate(String input) {
+        validateBlankException(input);
+        validateNumericException(input);
+        validatePositiveNumber(input);
+        validateIntegerOverflowException(input);
+    }
+
+    public static void validateBlankException(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(ERROR_BLANK);
+        }
+    }
+
+    public static void validateNumericException(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NON_NUMERIC);
+        }
+    }
+
+    public static void validatePositiveNumber(String input) {
+        int number = Integer.parseInt(input);
+        if (number < MIN_INT_VALUE) {
+            throw new IllegalArgumentException(ERROR_NON_ZERO);
+        }
+    }
+
+    public static void validateIntegerOverflowException(String input) {
+        long number = Long.parseLong(input);
+        if (number > MAX_INT_VALUE || number < MIN_INT_VALUE) {
+            throw new IllegalArgumentException(ERROR_INT_OVERFLOW);
+        }
+    }
+}
