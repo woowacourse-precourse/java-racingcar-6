b48af6b87994ee23c7c8e7e2873119284d935d5a
heukwu <yoo4068@gmail.com>
2023-10-26 19:10:43 +0900

    Feat: 이동 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,12 @@
 package racingcar;
 
+import static racingcar.GameManager.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String[] names = GameManager.getNames();
+        String[] names = getNames();
+        int moveCount = getMoveCount();
 
     }
 }
--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameManager {
 
-    static String[] getNames() {
+    public static String[] getNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String input = Console.readLine().replaceAll(" ", "");
 
@@ -27,5 +27,19 @@ public class GameManager {
                 throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
             }
         }
+    }
+
+    public static int getMoveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        int count = 0;
+
+        try {
+            count = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+
+        return count;
     }
 }
