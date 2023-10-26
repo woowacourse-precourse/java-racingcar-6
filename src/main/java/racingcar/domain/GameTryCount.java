10195df34fd0fe362eb461f434d9ea7c2f8b4a54
SeoJiWon <sjiwon4491@gmail.com>
2023-10-26 18:38:17 +0900

    feat: 전체 이동 횟수에 대한 `GameTryCount` 생성

---
+++ src/main/java/racingcar/domain/GameTryCount.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+public class GameTryCount {
+    private final int remainingAttempts;
+
+    public GameTryCount(final int remainingAttempts) {
+        this.remainingAttempts = remainingAttempts;
+    }
+}
