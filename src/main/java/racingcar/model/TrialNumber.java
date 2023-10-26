38f4e395c5a422d07da77279698e14a451583abb
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-26 22:11:15 +0900

    feat: 회수 입력값 검증 기능 개발

---
+++ src/main/java/racingcar/model/TrialNumber.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+public class TrialNumber {
+    private final int trialNumber;
+
+    public TrialNumber(String trialNumber) {
+        checkIsNumber(trialNumber);
+        this.trialNumber = Integer.parseInt(trialNumber);
+    }
+
+    private void checkIsNumber(String trialNumber) {
+        for (char c : trialNumber.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+            }
+        }
+    }
+}
