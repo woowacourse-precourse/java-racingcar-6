376e4f2744ed175b31f0f50b4dbcfd4c92630db4
peppone-choi <peppone.choi@gmail.com>
2023-10-26 22:17:53 +0900

    test: Game와 Car 생성 관련 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.controller.CarController;
+import racingcar.controller.GameController;
+import racingcar.model.Car;
+import racingcar.model.Game;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -24,11 +29,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 차_생성() {
+        CarController carController = new CarController();
+        List<Car> cars = carController.makeCar("talal,sun");
+        assertSimpleTest(() -> {
+                    assertThat(cars.get(0).getName()).isEqualTo("talal");
+                    assertThat(cars.get(1).getName()).isEqualTo("sun");
+                }
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 라운드_생성() {
+        GameController gameController = new GameController();
+        Game game = gameController.makeRound("2");
+        assertThat(game.getMaxRoundNumber()).isEqualTo(2);
+    }
+
+    @Test
+    void 라운드_숫자에_대한_예외_처리() {
+        GameController gameController = new GameController();
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> gameController.makeRound("가나다"))
+                .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
