0297a2677c8045bba9be76571ba4fd62bc4ad483
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-26 20:04:39 +0900

    feat: GameConfig를 통한 게임 기초 설정

---
+++ src/main/java/Config/GameConfig.java
@@ -0,0 +1,10 @@
+package Config;
+
+public class GameConfig {
+    public int minValue = 0;
+    public int maxValue = 9;
+    public int movingCondition = 4;
+    public int nameLimit = 5;
+    public String nameSeparator = ",";
+    public boolean multipleWinning = true;
+}
