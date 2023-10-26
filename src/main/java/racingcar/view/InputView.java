14e6bea041aec54b7736c37854460d4fbb1aa027
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-26 23:38:27 +0900

    feat(OutputView): 각 단계에서의 위치 출력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public static String inputNames() {
+    public static String takeNames() {
         System.out.println(NAME_INPUT_MESSAGE);
         return Console.readLine();
     }
 
-    public static String inputCount() {
+    public static String takeCount() {
         System.out.println(COUNT_INPUT_MESSAGE);
         return Console.readLine();
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,20 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.dto.CarCondition;
+
 public class OutputView {
+    public static final String RACE_RESULT_MESSAGE = "실행 결과";
+
+    public static void sendResult() {
+        System.out.println(RACE_RESULT_MESSAGE);
+    }
+
+    public static void sendRaceCondition(CarCondition carCondition) {
+        List<String> names = carCondition.names();
+        List<Integer> positions = carCondition.positions();
+        for (int carNumber = 0; carNumber < names.size(); carNumber++) {
+            System.out.println(names.get(carNumber) + " : " + positions.get(carNumber));
+        }
+    }
 }
