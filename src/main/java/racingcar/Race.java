13c667047d743ae80f0c8ad3dda59055411de153
park_yeongyu <sjjs7771@naver.com>
2023-10-26 23:41:19 +0900

    feat : Race 클래스에 경주 진행중 실행 결과 출력 함수 작성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -2,29 +2,33 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Race{
+public class Race {
     int trial;
 
     RunningMessage message = new RunningMessage();
     CarSetting carSetting = new CarSetting();
-    public void start(){
+
+    public void start() {
         message.printStartMessage();
         carSetting.setCarName();
     }
-    public void running(){
-        message.printResultMessage();
-        for(int try_count =0; try_count < trial; try_count++){
 
+    public void running() {
+        message.printResultMessage();
+        for (int try_count = 0; try_count < trial; try_count++) {
+            inCars();
+            System.out.println();
         }
     }
-    public void setTrial(){
+
+    public void setTrial() {
         message.printHowManyGame();
         trial = Integer.parseInt(Console.readLine());
     }
 
-    public void inCars(){
+    public void inCars() {
         Car[] cars = carSetting.getCar();
-        for(Car car: cars){
+        for (Car car : cars) {
             message.printCarName(car.getCarName());
             message.printRunCount(car.getRunCount());
         }
