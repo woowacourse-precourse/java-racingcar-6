59426fac1ed604f347131a1e34a00cc775057798
SeongUk52 <rkfka7849@gmail.com>
2023-10-26 22:12:40 +0900

    feat(CarMover): Car 리스트를 한번에 이동시키는 기능 구현

--- src/main/java/racingcar/service/CarMover.java
+++ src/main/java/racingcar/service/CarMover.java
@@ -1,10 +1,18 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.stream.IntStream;
+import java.util.List;
+import racingcar.domain.Car;
 
 public class CarMover {
-    static boolean isMove() {
+
+    static void move(List<Car> carList) {
+        carList.stream()
+                .filter(i -> isMoving())
+                .forEach(Car::move);
+    }
+
+    static boolean isMoving() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
