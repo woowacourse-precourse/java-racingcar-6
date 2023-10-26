70299574b692edcb81e88f9751cd8cbbeb5b17af
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-10-27 04:41:15 +0900

    Feat: 자동차의 시작 위치 값은 0이다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,15 @@ package racingcar.domain;
 
 public class Car {
     private static final int CAR_NAME_UPPER_LIMIT = 5;
+    private static final int CAR_DEFAULT_LOCATION = 0;
     private String name;
+    private int location;
 
     public Car(String name) {
         validateName(name);
 
         this.name = name;
+        this.location = CAR_DEFAULT_LOCATION;
     }
 
     private void validateName(String name) {
@@ -18,5 +21,13 @@ public class Car {
         }
 
         throw new IllegalArgumentException("자동차의 이름은 5글자 이하여야 합니다");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+    private static final int CAR_DEFAULT_LOCATION = 0;
 
     @Test
     void 자동차_이름은_5자를_초과할_수_없음() {
@@ -15,6 +18,12 @@ class CarTest {
     @Test
     void 다섯자_이하_이름의_자동차는_생성() {
         new Car("abcde");
+    }
+
+    @Test
+    void 자동차의_시작위치는_0() {
+        Car car = new Car("aa");
+        assertThat(car.getLocation()).isEqualTo(CAR_DEFAULT_LOCATION);
     }
 
 }
