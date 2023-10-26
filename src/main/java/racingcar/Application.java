4db3edffbf5b22f6f5c5bf21a230ea305e99c850
RedJunKi <bing8821@naver.com>
2023-10-26 19:21:18 +0900

    fix(racingCar): 자동차 생성시 중복된 차 이름이 있을 경우 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameConfig gameConfig = new GameConfig();
-        List<Car> cars = gameConfig.createCars("민지,윤호,민겸,지희,지볌우시,바나바나,보노보노보,하이젠버그,한");
+        List<Car> cars = gameConfig.createCars("민지,윤호,민겸,지희,지볌우시,바나바나,보노보노보,하이젠버그,한,민지");
 
         for (Car car : cars) {
             System.out.println(car.getName());
--- src/main/java/racingcar/domain/GameConfig.java
+++ src/main/java/racingcar/domain/GameConfig.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameConfig {
+    public int moveCount;
+
+    public void setMoveCount(int moveCount) {
+        this.moveCount = moveCount;
+    }
 
     public List<Car> createCars(String carNames) {
         List<Car> cars = new ArrayList<>();
+        List<String> checkCarName = new ArrayList<>();
 
         String[] split = carNames.split(",");
         for (String carName : split) {
             if (1 > carName.length() || carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 미만까지 가능합니다.");
+            } else if (checkCarName.contains(carName)) {
+                throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
             } else {
+                checkCarName.add(carName);
                 Car car = new Car(carName);
                 cars.add(car);
             }
         }
         return cars;
-    }
-
-    public void moveCount(int number) {
-
     }
 }
