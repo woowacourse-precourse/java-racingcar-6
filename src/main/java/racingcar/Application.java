e2bee048f77891c1da62c082ee8dfdf82b0f3dea
aka-nick <bechalrang@gmail.com>
2023-10-26 18:55:02 +0900

    수정(RacingGame): 메서드명 start -> play로 변경
    게임을 '시작'보다는 '실행'이 의미가 더 적합한 것 같아 수정함.
    
    RacingGame
    Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         RacingGame racingGame = new RacingGame();
-        racingGame.start();
+        racingGame.play();
     }
 
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,12 +6,9 @@ import racingcar.io.Output;
 
 public class RacingGame {
 
-    public void start() {
+    public void play() {
         Output.consoleLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> racers = Input.consoleStrings(",");
-        for (String s : racers) {
-            System.out.println(s);
-        }
     }
 
 }
