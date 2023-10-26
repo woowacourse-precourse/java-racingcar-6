dde4cc7b236428760d5c6e11bc8a1a20b15e42b8
Cycrypto <jh01love@naver.com>
2023-10-26 20:42:49 +0900

    feat/game-flow:게임 흐름과 관련한 로직 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,7 +1,13 @@
 package racingcar.controller;
-
+import racingcar.view.*;
+import racingcar.controller.GameUtils;
 public class GameController {
+    OutputView getMessage = new OutputView();
+    InputView putMessage = new InputView();
+    GameUtils gameUtility = new GameUtils();
     public void run(){
-
+        getMessage.GameStartMessage();
+        String s = putMessage.getPlayerInput();
+        // 게임 흐름과 관련된 로직
     }
 }
---
+++ src/main/java/racingcar/controller/GameUtils.java
@@ -0,0 +1,5 @@
+package racingcar.controller;
+
+public class GameUtils {
+    // 게임 흐름에 필요한 도구들
+}
