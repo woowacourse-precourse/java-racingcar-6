7fb7c9f8c5bdd5d73b156df9ce268c2696af5eeb
kimwanyoung <dhks2869@gmail.com>
2023-10-26 21:59:16 +0900

    feat : Cars 일급컬렉션 구현
    가장 많이 움직인 이름을 반환하는 winner() 구현

---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(String[] names) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+
+        this.cars = carList;
+    }
+
+    public String[] winner() {
+        List<String> winners = new ArrayList<>();
+        cars.sort(Car::compareTo);
+
+        Car currentCar = cars.get(0);
+        winners.add(currentCar.toString());
+
+        for (int i = 1; i < cars.size(); i++) {
+            Car nextCar = cars.get(i);
+            if (!currentCar.isSameMovingCount(nextCar)) {
+                break;
+            }
+            winners.add(nextCar.toString());
+        }
+
+        return winners.toArray(new String[winners.size()]);
+    }
+}
