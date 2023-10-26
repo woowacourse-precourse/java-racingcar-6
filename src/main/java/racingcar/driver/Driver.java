98ea8acfd18c317b054fe06e1f04424cd5ed8d91
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-26 18:41:38 +0900

    fix: fixed term of 'for' sentence.

--- src/main/java/racingcar/driver/Driver.java
+++ src/main/java/racingcar/driver/Driver.java
@@ -8,7 +8,7 @@ public class Driver {
     public List<String> validateDriverLineUp(String drivers) {
         List<String> driverList = Arrays.asList(drivers.split(","));
         for (String driver : driverList) {
-            if (driver.length() >= 5) {
+            if (driver.length() > 5) {
                 throw new IllegalArgumentException("이름은 5자 이하로 입력해주세요.");
             }
         }
