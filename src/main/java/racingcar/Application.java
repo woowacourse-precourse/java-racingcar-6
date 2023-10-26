736215c2656edc31584d5fa9e7bb69d6bc4478db
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-10-26 22:37:40 +0900

    feat: add get racing car name input
    Add new method createRacingCars() for get racing car name input
    - output a message requesting input of car names
    - create instances of cars
    - initialize instances with car names
    
    Add new class Car for store and update cars's status
    - have 2 status (name, position)
    - add constructor with IllegalArgumentException for name
    - add method moveCar() as empty
    - the moveCar() will update car's position whenever it is called

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,44 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+class Car{
+    String name;
+    int position = 0;
+
+    Car (String name, int position) {
+        if (name.length() > 5)
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
+        this.name = name;
+        this.position = position;
+    }
+    public void moveCar() {
+
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        createRacingCars();
+
     }
+
+    /**
+     * 경주할 자동차들의 이름을 받아 이름만큼 자동차 인스턴스를 만드는 메소드
+     */
+    public static void createRacingCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = readLine();
+        String[] carList = cars.split(",");
+
+        // 자동차의 인스턴스 생성 및 초기화
+        Car[] car = new Car[carList.length];
+        for (int i = 0; i < carList.length; i++) {
+            car[i] = new Car(carList[i], 0);
+        }
+    }
+
 }
