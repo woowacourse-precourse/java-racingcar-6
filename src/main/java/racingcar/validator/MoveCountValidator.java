07da72c782101b4af3f02792fe2f5ad97f80e576
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 21:14:44 +0900

    feat(MoveCountValidator) : 이동 횟수를 0 또는 음수를 입력했는지 예외 검증하는 기능 추가

--- src/main/java/racingcar/validator/MoveCountValidator.java
+++ src/main/java/racingcar/validator/MoveCountValidator.java
@@ -1,5 +1,7 @@
 package racingcar.validator;
 
+import racingcar.util.Convertor;
+
 public class MoveCountValidator {
 
     private MoveCountValidator() {
@@ -14,6 +16,13 @@ public class MoveCountValidator {
     public static void validateInputFormat(String input) {
         if (!input.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("횟수는 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static void validateMoveCount(String input) {
+        int moveCount = Convertor.convertStringToInt(input);
+        if (moveCount < 1) {
+            throw new IllegalArgumentException("이동 횟수는 0보다 커야 합니다.");
         }
     }
 }
