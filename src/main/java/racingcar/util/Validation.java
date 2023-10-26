a5518714ec3c03d3530ebc075b5fca0d9143f661
k9want <zndwlrnjr26@daum.net>
2023-10-27 01:52:27 +0900

    feat: vali - 숫자가 맞는지 확인 (문자는 아닌지, 음수는 아닌지 확인)

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -3,6 +3,13 @@ package racingcar.util;
 public class Validation {
     private static final int CAR_NAME_LENGTH = 5;
 
+    public static void valiRacingCount(String racingCount) {
+        if (isNumber(racingCount) && isNaturalNumber(Integer.parseInt(racingCount))) {
+            return;
+        }
+        throw new IllegalArgumentException("Invalid racingCount");
+    }
+
     public static void valiEmptySplitInputCarNames(String[] carNames) {
         if (carNames.length == 0) {
             throw new IllegalArgumentException("Empty split to carNames Array");
@@ -25,5 +32,18 @@ public class Validation {
         if (carName.isEmpty()) {
             throw new IllegalArgumentException("Invalid car name isEmpty");
         }
+    }
+
+    private static boolean isNumber(String racingCount) {
+        try {
+            Integer.parseInt(racingCount);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isNaturalNumber(int racingCount) {
+        return racingCount > 0;
     }
 }
