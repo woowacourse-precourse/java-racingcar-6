5b47f106dfded6a6c77d4f3b930bcc5c4fa97b37
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-10-26 23:22:40 +0900

    2023.10.26 레이싱카 풀

---
+++ src/test/java/racingcar/Game.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+    Game(){
+        play();
+    }
+    private void play(){
+
+
+    }
+}
---
+++ src/test/java/racingcar/User.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class User {
+    private List<String> cars;
+
+    public void inputNames(){
+        System.out.println("참여시킬 자동차이름을 기재해주십쇼 (구분자 comma)");
+        String names= Console.readLine();
+    }
+    public void divisionNames(String names){
+
+
+    }
+
+}
