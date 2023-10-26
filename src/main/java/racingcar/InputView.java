f1bbcdd1c6405165646cf6db6bc0fca56f481b80
JongMinCh0i <wonderland314592@gmail.com>
2023-10-26 17:52:28 +0900

    feat(InputView): 게임 시작시 유저 입력 안내 메시지 기능 추가

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class InputView {
+
+    public void printInputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printHowManyTry() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+}
