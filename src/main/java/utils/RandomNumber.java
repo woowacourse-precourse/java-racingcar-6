681486a4bcffe06dd52ffacd82a4d75ee7ddccc9
happyryan <tjdgks120813@gmail.com>
2023-10-26 22:05:16 +0900

    feat(utils Random): implement RandomNumber utility class
    Added the RandomNumber utility class to generate random numbers within a specified range.

--- src/main/java/utils/RandomNumber.java
+++ src/main/java/utils/RandomNumber.java
@@ -1,2 +1,9 @@
-package utils;public class RandomNumber {
+package utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+    public static int create() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
