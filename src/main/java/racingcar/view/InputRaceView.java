3c18e45b113c75072cf8939f6ed756103a2e900d
euics <pjh575157@gmail.com>
2023-10-26 16:31:03 +0900

    feat(View): readRaceCount 메서드 구현
    사용자로부터 시도할 경주 횟수를 입력.
    - "시도할 회수는 몇회인가요?" 출력.

--- src/main/java/racingcar/view/InputRaceView.java
+++ src/main/java/racingcar/view/InputRaceView.java
@@ -7,4 +7,9 @@ public class InputRaceView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public static int readRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
