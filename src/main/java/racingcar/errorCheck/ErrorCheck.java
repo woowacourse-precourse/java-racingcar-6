54be4c7fd1ec532c2fc49d05a85137483e0e63b0
EHCTH <lees90896@gmail.com>
2023-10-26 18:56:27 +0900

    에러

--- src/main/java/racingcar/errorCheck/ErrorCheck.java
+++ src/main/java/racingcar/errorCheck/ErrorCheck.java
@@ -1,2 +1,25 @@
-package racingcar.errorCheck;public class ErrorCheck {
+package racingcar.errorCheck;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ErrorCheck {
+    public static void isCarNameFive(String[] cars) {
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i].length() > 5) {
+                throw new IllegalArgumentException("차 이름 길이가 5이하가 아닙니다");
+            }
+        }
+    }
+    public static void isDuplicationNameCar(String[] cars) {
+        Set<String> set = new HashSet<String>();
+        for (int i =0; i < cars.length; i++) {
+            set.add(cars[i]);
+        }
+        if (set.size() != cars.length) {
+            throw new IllegalArgumentException("중복된 이름이 있습니다");
+        }
+
+    }
+
 }
