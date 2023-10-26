0e8ca05ce4468d9742237f0af48c146c75771c4b
haruday97 <starharu97@naver.com>
2023-10-27 01:25:08 +0900

    feat: 유저 입력 기능 추가
    유저 입력을 반환하는 메서드 추가
    띄어쓰기가 포함되었을 경우를 고려한 trim메서드 체이닝

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     private static final String MSG_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -16,5 +18,9 @@ public class InputView {
 
     public static void printResult() {
         System.out.println(MSG_RESULT);
+    }
+
+    public static String getUserInput() {
+        return Console.readLine().trim();
     }
 }
