748d84cc01f41987f02c38822db8583ef4fe72c7
shin5774 <babo5774@naver.com>
2023-10-26 20:11:40 +0900

    test: 전진 기능 테스트 코드 작성
    - Position의 상태값에 1을 추가해서 전진을 하는 기능을 가진 메서드를 테스트하는 코드를 작성.

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,5 +16,14 @@ public class PositionTest {
         Position position = new Position();
 
         assertThat(position.checkValueIsOverFour(value)).isTrue();
+    }
+
+    @DisplayName("전진 기능")
+    @Test
+    void moveForward_전진_기능(){
+        Position position=new Position();
+        int beforePosition=position.position;
+        position.moveForward();
+        assertThat(position.position-1).isEqualTo(beforePosition);
     }
 }
