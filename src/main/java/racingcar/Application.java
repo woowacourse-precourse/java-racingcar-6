dbfbc5db9ee1dfb50c01efa987aa8778652a43d6
gyahn <pop2413@gmail.com>
2023-10-26 21:15:52 +0900

    add: 자동차 생성 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CreateCar createCar = new CreateCar();
+        System.out.println(Arrays.toString(createCar.getCarList()));
     }
 }
--- src/main/java/racingcar/CreateCar.java
+++ src/main/java/racingcar/CreateCar.java
@@ -1,5 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CreateCar {
     //자동차 생성 클래스
+
+    String[] getCarList(){
+        String carNm = Console.readLine();
+        String [] carList = carNm.split(",");
+        return carList;
+    }
+
+
 }
