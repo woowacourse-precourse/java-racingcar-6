fecd4fbc7086f6418c099298784f12a1965bb9c7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-27 02:34:16 +0900

    refactor: 원시타입 매핑하기

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,10 +1,10 @@
 package racingcar.model;
 
 public class Car {
-    private final String carName;
+    private final Name carName;
     private int carAdvances;
 
-    public Car(final String carName) {
+    public Car(final Name carName) {
         this.carName = carName;
         this.carAdvances = 0;
     }
@@ -14,7 +14,7 @@ public class Car {
     }
 
     public String getCarName(){
-        return this.carName;
+        return this.carName.getName();
     }
 
     public int getCarAdvances(){
