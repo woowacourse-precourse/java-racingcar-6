5f0a862c73f0b3120d62033e0080f9846da7a8ea
kimwanyoung <dhks2869@gmail.com>
2023-10-26 21:24:55 +0900

    feat : Car객체 구현
    매개변수로 받은 randomNumber보다 크면 movingCount증가 시키는 increaseMovingCountIfGreator() 구현
    현재 경주 상태 반환하는 currentRacingStatus() 구현
    오름차순 정렬되도록 compareTo() 오버라이드
    다른 Car객체와 movingCount가 같은지 비교하는 isSameMovingCount() 구현

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Car implements Comparable<Car> {
+    private final Name name;
+    private final MovingCount movingCount;
+    private static final String DASH = "-";
+    private static final String COLON = ":";
+    private static final String SPACE = " ";
+
+    public Car(String name) {
+        this.name = new Name(name);
+        this.movingCount = new MovingCount();
+    }
+
+    public boolean isSameMovingCount(Car otherCar) {
+        return this.movingCount == otherCar.movingCount;
+    }
+
+    public void increaseMovingCountIfGreater(int number, int randomNumber) {
+        if (randomNumber >= number) {
+            this.movingCount.increase();
+        }
+    }
+
+    public String currentRacingStatus() {
+        String moveDash = DASH.repeat(movingCount.getMovingCount());
+        return this + SPACE + COLON + SPACE + moveDash;
+    }
+
+    @Override
+    public int compareTo(Car otherCar) {
+        int otherMovingCount = otherCar.movingCount.getMovingCount();
+        int currentMovingCount = this.movingCount.getMovingCount();
+        return otherMovingCount - currentMovingCount;
+    }
+
+    @Override
+    public String toString() {
+        return this.name.getName();
+    }
+}
