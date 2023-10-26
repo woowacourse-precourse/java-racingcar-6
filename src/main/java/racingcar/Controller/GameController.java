81d134e2b20e8e7fa72dc7a9a8201268dc469545
kyoo0115 <internetkyoomin@gmail.com>
2023-10-26 22:14:22 +0900

    add: 게임 컨트롤러 생성

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -1,5 +1,25 @@
 package racingcar.Controller;
 
-public class GameController {
+import java.util.List;
+import racingcar.Model.Car;
+import racingcar.View.OutputView;
 
+public class GameController {
+    private InputController inputController;
+    private GameController raceController;
+    private OutputView raceView;
+
+    public GameController() {
+        inputController = new InputController();
+        raceView = new OutputView();
+        initiateGame();
+    }
+
+    public void initiateGame() {
+        List<Car> cars = inputController.getCarList();
+        int roundCount = inputController.getRoundCount();
+        raceController = new RaceController(cars, roundCount, raceView);
+        raceController.runRace();
+        raceController.displayWinners();
+    }
 }
