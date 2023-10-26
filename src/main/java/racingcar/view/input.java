7f383ec46533de61327d72f55d05373a796c6a23
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-10-26 17:44:18 +0900

    view 기능 1차 완료

---
+++ src/main/java/racingcar/view/input.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class input {
+    public static String printNameToBeInput () {
+        System.out.println("경주할 자동차 이름을 입력하세요.(임르은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String printNumToBeInput () {
+        System.out.println("시도할 회수는 몇번인가요?");
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/view/output.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public class output {
+    public static void printResultMsg () {
+        System.out.println("실행 결과");
+    }
+
+    public static void printProgressing (String name, String progress) {
+        System.out.println(name + " : " + progress);
+    }
+
+    public static void printWinner (String winners) {
+        System.out.println("최종 우승자" + winners);
+    }
+
+}
