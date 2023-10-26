7650e2da550c10f5353a599eeea4dbf8f1826232
takoyakimchi <c4nyou@gmail.com>
2023-10-27 04:14:46 +0900

    feat(car): name, position
    This class will manage the racecars in the game

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+public class Car implements Comparable<Car> {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.position - o.position;
+    }
+}
