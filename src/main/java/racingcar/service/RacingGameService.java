894b9380342fea4174ca6a102aa621919cf68e56
Isaac Han <isaachan233@gmail.com>
2023-10-27 07:48:19 +0900

    feat: 자동차들을 추가하는 메서드 추가

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,7 +1,21 @@
 package racingcar.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import racingcar.model.Cars;
+import racingcar.model.Winners;
 
 public class RacingGameService {
-    private final Cars cars = new Cars();
+    private final Cars cars;
+    private final Winners winners;
+    
+    public RacingGameService() {
+        this.cars = new Cars();
+        this.winners = new Winners();
+    }
+    
+    public void addCars (String carNames) {
+        cars.addCars(new ArrayList<String>(Arrays.asList(carNames.split(","))));
+    }
 }
