8db9b3d5abba34a96d5b3b3c25d9411810cf29f9
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-26 18:01:17 +0900

    feat: car 클래스 작성
    자동차 이름과 경주에서 사용될 위치값을 가진 자동차 클래스 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private finnal String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
