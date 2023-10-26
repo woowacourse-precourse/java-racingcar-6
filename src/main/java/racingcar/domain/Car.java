9030908ffcfa1ab64d89500bdfdda5a3e6555d3d
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 19:56:18 +0900

    [feat] car 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int moveDistance;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveDistance = 0;
+    }
+}
