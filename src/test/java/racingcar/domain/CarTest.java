e90d4fc0a067d91e7aa493c25a96eeb857f6f5b8
SeongUk52 <rkfka7849@gmail.com>
2023-10-26 22:07:11 +0900

    test(Car): move() 차를 한 칸 이동시키는 기능

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @DisplayName("move(): 차를 한 칸 이동시키는 기능")
+    @Test
+    void moveTest() {
+        Car car = new Car("붕붕이");
+        car.move();
+        car.move();
+        assertEquals(car.progress.get(), 2);
+    }
+}
