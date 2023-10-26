9c36673c34bfea651a6d1cef7adb132a2495416d
park_yeongyu <sjjs7771@naver.com>
2023-10-26 21:58:02 +0900

    feat : Input 클래스에 자동차 입력 받는 함수 작성

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+public class Input {
+    String Cars;
+    public void inputCars(){
+        Cars = Console.readLine();
+    }
+}
