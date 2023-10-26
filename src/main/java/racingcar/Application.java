842ae8960bf1450d1bd6d3c0ff065642297e65af
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-10-26 22:31:35 +0900

    Refactor: 고침 inputCarName, inputCountNum 출력문
    출력문 내용 예시와 동일하게 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Application {
 
     public static List<String> inputCarName() {  // checkInput 구현시 수정 필요
-        System.out.println("참가자 이름을 입력해 주세요!");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputStr = Console.readLine();
         inputStr = inputStr.replace(" ", "");
         String[] inputArr = inputStr.split(",");
@@ -17,7 +17,7 @@ public class Application {
     }
 
     public static int inputCountNum() { // checkInput 구현시 수정 필요
-        System.out.println("시도 횟수를 입력해 주세요!");
+        System.out.println("시도할 회수는 몇회인가요?");
         String inputStr = Console.readLine();
         int count = Integer.parseInt(inputStr);
         return count;
@@ -50,6 +50,8 @@ public class Application {
         forwardDistance.put(name, sumForward);
         return forwardDistance;
     }
+
+
 
     public static void main(String[] args) {
     }
