76e6da8935302bce10c06c21b66ab9fcf4c8462d
suihub <qkdcrt@gmail.com>
2023-10-26 17:54:30 +0900

    <test>: "자동차 이름, 전진 여부 확인 테스트 구현"
    다음 두 가지 테스트를 구현
    
    1. 자동차 객체 생성 시, 이름이 정상적으로 만들어졌는가.
    2. 자동차를 전진 시킬 시, 의도한 대로 한 칸씩 전진하는가.

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+    @Test
+    void 자동차_전진_확인(){
+        Car car = new Car("test");
+        int position = car.getPosition();
+
+        for (int i=0; i < 3; i++) {
+            car.moveForward();
+        }
+
+        int result = car.getPosition();
+
+        assertThat(position).isEqualTo(0);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void 자동차_이름_확인(){
+        Car car = new Car("test");
+        String name = car.getName();
+
+        assertThat(name).isEqualTo("test");
+    }
+}
