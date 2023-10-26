6f7178b15c5d2cfcfbaef7a1ab9a984f6383adff
Aeraekun <lsch3374@naver.com>
2023-10-27 02:33:09 +0900

    feat: util/countRecord 구현 ################
    ################
    
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/main/java/racingcar/util/Recorder.java
+++ src/main/java/racingcar/util/Recorder.java
@@ -1,18 +1,29 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Recorder {
     int[] now;
+
     public void record(List<String> car, int lap) {
         now = makeRecord(car.size());
         while (lap < 0) {
-
+            countRecord(car);
             lap--;
         }
     }
 
     public int[] makeRecord(int size) {
         return new int[size];
+    }
+
+    public void countRecord(List<String> car) {
+        for (int i = 0; i < car.size(); i++) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                now[i]++;
+            }
+        }
     }
 }
