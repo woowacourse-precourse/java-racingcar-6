c43e7388e1f1592421ef205e6a7a8fbe0825b2f1
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-27 00:03:50 +0900

    refactor: 매개변수명 오타 수정
    inputVaule > inputValue

--- src/main/java/racingcar/service/Exception.java
+++ src/main/java/racingcar/service/Exception.java
@@ -10,10 +10,10 @@ public class Exception {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final String BLANK = " ";
     public static List<String> carNames;
-    public static void checkCarName(String inputVaule) throws IllegalArgumentException {
-        checkEmpty(inputVaule);
+    public static void checkCarName(String inputValue) throws IllegalArgumentException {
+        checkEmpty(inputValue);
 
-        carNames = Arrays.asList(inputVaule.split(","));
+        carNames = Arrays.asList(inputValue.split(","));
         for (String name : carNames) {
             checkNoName(name);
             checkblank(name);
@@ -23,26 +23,30 @@ public class Exception {
         checkSameName();
     }
 
-    private static void checkEmpty(String inputVaule) {
-        if (inputVaule.isEmpty()) {
+    public static void checkRaceCount(String inputValue) {
+
+    }
+
+    private static void checkEmpty(String inputValue) {
+        if (inputValue.isEmpty()) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름을 입력해주세요.");
         }
     }
 
-    private static void checkNoName(String inputVaule) {
-        if (inputVaule.isEmpty() || inputVaule.equals(BLANK)) {
+    private static void checkNoName(String inputValue) {
+        if (inputValue.isEmpty() || inputValue.equals(BLANK)) {
             throw new IllegalArgumentException("잘못된 값 입력 : 입력되지 않은 자동차 이름이 있습니다.");
         }
     }
 
-    private static void checkblank(String inputVaule) {
-        if (inputVaule.contains(BLANK)) {
+    private static void checkblank(String inputValue) {
+        if (inputValue.contains(BLANK)) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름에는 공백이 들어갈 수 없습니다.");
         }
     }
 
-    private static void checkLength(String inputVaule) {
-        if (inputVaule.length() > CAR_NAME_MAX_LENGTH) {
+    private static void checkLength(String inputValue) {
+        if (inputValue.length() > CAR_NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름은 5자 이내로 입력해주세요");
         }
     }
@@ -53,4 +57,6 @@ public class Exception {
             throw new IllegalArgumentException("잘못된 값 입력 : 중복된 자동차 이름이 있습니다.");
         }
     }
+
+    //private static void check
 }
