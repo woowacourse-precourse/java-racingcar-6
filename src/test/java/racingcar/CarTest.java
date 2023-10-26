5f00370c7ba54268eba7d4b1761b1d0ce7bab9fe
jun <ggprgrkjh@naver.com>
2023-10-26 22:38:14 +0900

    test(game): 자동차 이름 분리 TDD 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,15 +2,11 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 
 public class CarTest {
-
-
-
-
-
-
 
 
 }
--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,4 +1,37 @@
 package racingcar;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.model.Game;
+
 public class GameTest {
+
+    Car car;
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+        game = new Game();
+    }
+
+
+    @Test
+    void 자동차_이름_분리_테스트() {
+
+        String input = "pobi,jun,king";
+        String[] inputs = input.split(",");
+        String[] carNames = game.splitCarName(input);
+
+        for (int i = 0; i < carNames.length; i++) {
+            Assertions.assertTrue(carNames[i].length() <= 5);
+            Assertions.assertTrue(carNames.length == input.length());
+        }
+
+
+    }
+
+
 }
