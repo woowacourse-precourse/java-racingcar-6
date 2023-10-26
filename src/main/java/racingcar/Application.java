b2f6d37bb38103927a3a2a79d1e27544fa878590
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-10-27 00:07:01 +0900

    feat(racingcar): 자동차 이름을 입력받아 쉼표를 기준으로 구분하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNamesList = inputCarNames();
+        System.out.println(carNamesList);
+    }
+
+    public static List<String> inputCarNames() {
+        // 문자열 받아서 콤마(,)롤 기준으로 나누어 List에 담음
+        String carNames = Console.readLine();
+        String[] carNamesSplitted = carNames.split(",");
+        List<String> carNamesList = new ArrayList<>();
+        Collections.addAll(carNamesList, carNamesSplitted);
+        return carNamesList;
     }
 }
