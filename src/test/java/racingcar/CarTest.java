b49e6ef0c3131de6ad3a3c3bdb71f371e7a1a4ba
jun <ggprgrkjh@naver.com>
2023-10-27 02:59:45 +0900

    test(car): 자동차 전진 TDD 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,11 +2,36 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.LinkedHashMap;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
+import racingcar.model.Game;
 
 public class CarTest {
+
+    Car car;
+    Game game;
+
+    LinkedHashMap<String,Integer> cars;
+
+    @BeforeEach
+    void setUp(){
+        game = new Game(cars);
+        car = new Car();
+
+    }
+
+    @Test
+    void 자동차_전진_테스트(){
+        int carNumber = game.createCarNumber();
+        int location = car.moveForward(carNumber);
+
+        Assertions.assertTrue(location>=1);
+
+    }
 
 
 }
