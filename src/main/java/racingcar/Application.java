4efac586e7b74ae0bf513ee22568969f2873798d
Namyejun <yejun1148@gmail.com>
2023-10-26 21:03:04 +0900

    메인 메소드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,10 @@
 package racingcar;
 
+import racingcar.implementation.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
+        RacingGame racingGame = new RacingGame();
+        racingGame.entireProcess();
     }
 }
