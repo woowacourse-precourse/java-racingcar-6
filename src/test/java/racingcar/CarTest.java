d67d7445ef8b41e6d2d0a28f19b6aee94e5d2463
wooteco-daram <wooteco.daram@gmail.com>
2023-10-27 06:56:45 +0900

    test: `Car_객체는_이름_속성을_가진다` 테스트 케이스 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void Car_객체는_이름_속성을_가진다() {
+        final String expectedName = "이름";
+        final Car car = new Car(expectedName);
+        assertThat(car.getName()).isEqualTo(expectedName);
+    }
+}
