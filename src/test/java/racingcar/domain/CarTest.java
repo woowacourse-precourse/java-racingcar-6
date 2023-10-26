b41821bcb49aca33638fbbcd58350f2866aab2b3
gyungchan Jo <namejgc@naver.com>
2023-10-27 00:47:55 +0900

    test(CarTest): BeforeEach로 중복 코드 제거

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,12 +8,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("pobi");
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 9})
     @DisplayName("자동차는 입력된 값이 4이상 9이하이면 전진할 수 있다.")
     void move(int number) {
-        Car car = new Car("pobi");
         car.moveOrStop(number);
 
         assertThat(car).isEqualTo(new Car("pobi", 1));
@@ -22,7 +28,6 @@ class CarTest {
     @ValueSource(ints = {0, 3})
     @DisplayName("자동차는 입력된 값이 0이상 3이하라면 전진할 수 없다.")
     void stop(int number) {
-        Car car = new Car("pobi");
         car.moveOrStop(number);
 
         assertThat(car).isEqualTo(new Car("pobi", 0));
@@ -32,8 +37,6 @@ class CarTest {
     @ValueSource(ints = {-1, 10})
     @DisplayName("입력된 값이 0보다 작거나 9보다 크다면 예외가 발생한다.")
     void invalidNumber(int number) {
-        Car car = new Car("pobi");
-
         assertThatThrownBy(() -> car.moveOrStop(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("잘못된 입력 값입니다.");
