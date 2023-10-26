7dcfe07730def9400b5367addcf685e1ca3ed5bf
euics <pjh575157@gmail.com>
2023-10-26 20:13:08 +0900

    feat(View): OutputRaceView Magic Number Constant 변수로 수정
    수정사항
    - printRaceResult 출력값 Constants.RACE_RESULT_HEADER로 변경
    - printWinners 출력값 Constants.WINNER_ANNOUNCEMENT로 변경

--- src/main/java/racingcar/view/OutputRaceView.java
+++ src/main/java/racingcar/view/OutputRaceView.java
@@ -1,13 +1,14 @@
 package racingcar.view;
 
 import racingcar.model.Car;
+import racingcar.util.Constants;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputRaceView {
     public static void printRaceResult(List<Car> cars) {
-        System.out.println("\n실행 결과");
+        System.out.println(Constants.RACE_RESULT_HEADER);
         cars.forEach(car -> {
             System.out.print(car.getName() + " : ");
             for (int i = 0; i < car.getPosition(); i++) {
@@ -18,6 +19,6 @@ public class OutputRaceView {
     }
 
     public static void printWinners(List<Car> winners) {
-        System.out.println("최종 우승자 : " + winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
+        System.out.println(Constants.WINNER_ANNOUNCEMENT + winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
     }
 }
