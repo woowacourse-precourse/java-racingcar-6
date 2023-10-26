d8f54a848d1e7903e9a537f53021424656679707
Firedrago95 <yonghwa95@naver.com>
2023-10-27 03:05:08 +0900

    test : 랜덤값에 따라 전진, 멈춤 기능 테스트 코드 작성

--- src/test/java/racingcar/ModelTest/CarTest.java
+++ src/test/java/racingcar/ModelTest/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.ModelTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -17,5 +18,25 @@ public class CarTest {
 
         assertThrows(IllegalArgumentException.class ,() -> new Car(invalidName));
         assertDoesNotThrow(() -> new Car(validName));
+    }
+
+    @Test
+    @DisplayName("4이상일때 전진")
+    public void moveTest() {
+        int randomNumber = 4;
+        Car car = new Car("test");
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3이하일때 멈춤")
+    public void stopTest() {
+        int randomNumber = 3;
+        Car car = new Car("test");
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
