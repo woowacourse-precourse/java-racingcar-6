0fd620900f3cb0cd047970f0cdc679d4e3efadec
wwan13 <wwan13@naver.com>
2023-10-27 00:59:42 +0900

    feat : `Car` 기능 구현 - 자동차의 이름 `CarName` 과 이동 횟수 `CarMovement`를 저장한다. - 1~9 사이의 임의의 수가 4 이상일 경우 자동차의 이동 횟수를 1만큼 증가시킨다. - 이름과 이동 횟수 만큼의 '-'를 담은 문자열을 반환한다.

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,35 @@
+package racingcar.domain.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private final int RANDOM_START_INCLUSIVE = 0;
+    private final int RANDOM_END_INCLUSIVE = 9;
+    private final int CRITICAL_VALUE = 4;
+    private final String CURRENT_STATE_FORMAT = "%s : %s";
+
+    private final CarName name;
+    private final CarMovement movement;
+
+    public Car(String name) {
+        this.name = new CarName(name);
+        this.movement = new CarMovement();
+    }
+
+    public void move() {
+        int randomValue = generateRandomValue();
+        if (randomValue >= CRITICAL_VALUE) {
+            movement.goForward();
+        }
+    }
+
+    private int generateRandomValue() {
+        return Randoms.pickNumberInRange(RANDOM_START_INCLUSIVE, RANDOM_END_INCLUSIVE);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(CURRENT_STATE_FORMAT, name.name(), movement.toString());
+    }
+}
