bd64d4fb44bc7a5ebc5f0cc253dc6bcdfa8761af
hyun-hyang <jimin3060@naver.com>
2023-10-26 19:57:47 +0900

    Feature : InputView finish

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,4 +1,41 @@
 package racingcar;
 
+import static java.util.Arrays.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    public static List<String> inputCarName() {
+        String[] carNamesSplit;
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNames = Console.readLine();
+            carNamesSplit = carNames.split(",");
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String> collect = stream(carNamesSplit).collect(Collectors.toList());
+        return collect;
+
+    }
+
+    public static int inputNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        int num;
+        try {
+            String number = Console.readLine();
+            num = Integer.parseInt(number);
+        } catch (Exception e1) {
+            throw new IllegalArgumentException();
+        }
+
+        return num;
+
+    }
 }
