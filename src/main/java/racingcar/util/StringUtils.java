5b74fb68926bb1e723e9e01b6b5fd16092f07132
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 21:09:23 +0900

    [feat] 리스트 string 변환 기능 구현

--- src/main/java/racingcar/util/StringUtils.java
+++ src/main/java/racingcar/util/StringUtils.java
@@ -1,9 +1,11 @@
 package racingcar.util;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class StringParser {
+public class StringUtils {
 
     public static List<String> toCarNameList(String input) {
         String[] nameArray = input.split(",");
@@ -12,5 +14,11 @@ public class StringParser {
 
     public static int toInteger(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static String listToString(List<String> input) {
+        String result = input.stream()
+                .collect(joining(", "));
+        return result;
     }
 }
