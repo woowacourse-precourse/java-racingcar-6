16bf56b18ec3d13069a7e964717691cd93639e48
heiler <threepebbles@naver.com>
2023-10-27 00:19:14 +0900

    feat: Car 객체 생성자에서 인스턴스 유효성 검사 기능 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateNull(name);
+        validateLength(name);
+    }
+
+    private void validateNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("null이 입력되었습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
+}
