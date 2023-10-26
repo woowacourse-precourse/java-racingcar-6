d8de4f75341a0e987af303c7fa4cb317b1610818
HaLin Kim <halin130@tukorea.ac.kr>
2023-10-26 23:55:17 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position++;
+    }
+}
