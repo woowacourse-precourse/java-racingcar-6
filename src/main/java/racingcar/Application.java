6795cb5bbb15e4a13c44d322ce28df22eecf1469
isyoo1213 <moethns@gmail.com>
2023-10-26 22:03:22 +0900

    feat: InputView 자동차 이름 입력 기능 추가 InputView - askCarsName() 추가 Application - InputView 인스턴스 생성 - InputView의 askCarsName() 정상 출력 확인 로직 README.md - 기능 명세 구현 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        String userInputCarsName = inputView.askCarsName();
+        System.out.println(userInputCarsName);
     }
 }
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String CAR_NAME_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)";
+    private static final String TRIAL_TIMES_INPUT_STRING = "시도할 횟수는 몇회인가요?";
+
+    public String askCarsName() {
+        System.out.println(CAR_NAME_INPUT_STRING);
+        return Console.readLine();
+    }
+}
