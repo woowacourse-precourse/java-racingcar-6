25c9ae0bf6c6a3936b130d115b6320bc931f35a4
RedJunKi <bing8821@naver.com>
2023-10-27 01:22:39 +0900

    correct(racingCar): 동일한 이름에 앞,뒤 공백이 있을경우 통과되던 오류 수정

--- src/main/java/racingcar/domain/GameConfig.java
+++ src/main/java/racingcar/domain/GameConfig.java
@@ -28,7 +28,7 @@ public class GameConfig {
         for (String carName : split) {
             if (1 > carName.length() || carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 미만까지 가능합니다.");
-            } else if (checkCarName.contains(carName)) {
+            } else if (checkCarName.contains(carName.trim())) {
                 throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
             } else {
                 checkCarName.add(carName);
