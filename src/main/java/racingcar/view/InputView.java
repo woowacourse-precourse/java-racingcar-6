3829a1507adfe5b46017a167e26c514becf89de2
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-26 16:48:18 +0900

    feat: 자동차 이름 입력 메서드, 시도할 횟수 입력 메서드 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static String INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String INPUT_ATTEMPTS = "시도할 회수는 몇회인가요?";
+
+    public static String carsName(String cars){
+        System.out.println(INPUT_CARS_NAME);
+        return Console.readLine();
+    }
+
+    public static String attempts(String attempts){
+        System.out.println(INPUT_ATTEMPTS);
+        return Console.readLine();
+    }
+}
