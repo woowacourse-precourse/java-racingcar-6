367adb030aae17212b469cb0204dc1f47a95d7d2
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 20:22:31 +0900

    [feat] car 리스트 생성 기능 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.util.StringParser;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(String carName) {
+        this.cars = toCarList(carName);
+    }
+
+    private List<Car> toCarList(String carName) {
+        List<String> carNameList = StringParser.toCarNameList(carName);
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNameList) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+        return cars;
+    }
+}
