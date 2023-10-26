7f35e64b31b254841cd4a75e01aebbf5e81c6774
Potato-Y <projectpotatoy@gmail.com>
2023-10-27 02:17:56 +0900

    feat: 자동차 전진 횟수 카운트 기능 추가
    - 자동차의 전진 횟수를 증가할 수 있다.
    - 자동차의 전진 횟수를 불러올 수 있다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -6,12 +6,25 @@ package racingcar.model;
 public class Car {
 
     private final Name name;
+    
+    /**
+     * 자동차가 전진한 횟수
+     */
+    private int advance = 0;
 
     public Car(String name) {
         this.name = new Name(name);
     }
 
+    public void increaseAdvance() {
+        this.advance++;
+    }
+
     public String getName() {
         return name.getName();
+    }
+
+    public int getAdvance() {
+        return advance;
     }
 }
