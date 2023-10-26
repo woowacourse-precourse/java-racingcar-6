d5ddc8e5b9856537d726ef982592d21b13678a81
aka-nick <bechalrang@gmail.com>
2023-10-26 18:41:31 +0900

    추가(RacingGame): '경주자' 이름을 형식에 맞춘 입력값으로 받기
    입력 안내 메시지를 출력하고, 형식에 맞는 입력값을 받도록 한다. Input, Output은 1주차의 클래스를 재사용했다.
    
    RacingGame
    Output
    Input
    InputMessage

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,9 +1,16 @@
 package racingcar;
 
+import racingcar.io.Input;
+import racingcar.io.Output;
+
 public class RacingGame {
 
     public void start() {
-        System.out.println("레이싱 게임 실행");
+        Output.consoleLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racers = Input.consoleLine();
+        for (String s : racers.split(",")) {
+            System.out.println(s);
+        }
     }
 
 }
