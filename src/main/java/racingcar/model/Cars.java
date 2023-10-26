aacdede693b2f9033c781b12b2c71010993290db
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-26 21:28:29 +0900

    refactor: 입력값 변경 및 Enum 클래스 상수 사용

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -3,24 +3,26 @@ package racingcar.model;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import racingcar.utils.Constraints;
 
 public class Cars {
-    private static final int MAX_SIZE = 5;
     private final List<String> carNames;
 
     public Cars(List<String> carNames) {
-        checkNameLength();
-        checkDuplicate();
+        checkNameLength(carNames);
+        checkDuplicate(carNames);
         this.carNames = carNames;
     }
 
-    private void checkNameLength() {
-        carNames.stream().filter(carName -> carName.length() > MAX_SIZE).forEach(carName -> {
-            throw new IllegalArgumentException("자동차의 이름은 " + MAX_SIZE + "보다 작아야 합니다.");
-        });
+    private void checkNameLength(List<String> carNames) {
+        carNames.stream()
+                .filter(carName -> carName.length() > Constraints.MAX_SIZE.getValue())
+                .forEach(carName -> {
+                    throw new IllegalArgumentException("자동차의 이름은 " + Constraints.MAX_SIZE.getValue() + "보다 작아야 합니다.");
+                });
     }
 
-    private void checkDuplicate() {
+    private void checkDuplicate(List<String> carNames) {
         Set<String> carNamesSet = new HashSet<>(carNames);
         if (carNames.size() != carNamesSet.size()) {
             throw new IllegalArgumentException("중복된 자동차 이름이 존재 합니다.");
