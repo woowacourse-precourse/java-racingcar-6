15abc609bb6fade1cc042f4649b266bda2942ddd
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-10-26 22:53:47 +0900

    Feat: Car 객체가 이동하는 기능, 지금의 상태를 출력하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,6 +4,10 @@ public class Car {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
 
+    // TODO: View와 의존적인 코드가 존재하는 것이 옳은지 생각해보기
+    private static final String CAR_INFO_FORMAT = "%s : %s";
+    private static final String POSITION_EXPRESS_CHARACTER = "-";
+
     private final String name;
     private int position;
 
@@ -16,5 +20,19 @@ public class Car {
         if (name.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("차 이름은 5자 이하여야 합니다.");
         }
+    }
+
+    public void drive() {
+        this.position += 1;
+    }
+
+    private String expressPosition() {
+        return POSITION_EXPRESS_CHARACTER.repeat(this.position);
+    }
+
+    @Override
+    public String toString() {
+        String position = expressPosition();
+        return String.format(CAR_INFO_FORMAT, this.name, position);
     }
 }
