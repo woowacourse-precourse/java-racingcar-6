3bd8957e7d2648e2a329d8e0f708c6301147263d
park_yeongyu <sjjs7771@naver.com>
2023-10-26 22:01:08 +0900

    feat : Input 클래스에 시도 횟수 받는 함수 작성

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -3,7 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 public class Input {
     String Cars;
+    int NumberOfAttempt;
     public void inputCars(){
         Cars = Console.readLine();
+    }
+
+    public void inputNumberOfAttempt(){
+        NumberOfAttempt = Integer.parseInt(Console.readLine());
     }
 }
