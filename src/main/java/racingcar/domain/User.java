07b981746bac2a17257b7f17f1366446f3b1b6d4
H-server <hani103201@naver.com>
2023-10-26 21:16:50 +0900

    feat: 자동차 이름 split

--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -1,2 +1,17 @@
-package racingcar.domain;public class User {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public String[] splitCarsName() {
+        String carsNameString = Console.readLine();
+        String[] carsNameArray = carsNameString.split(",");
+        return carsNameArray;
+    }
+
+    // 테스트를 위한 코드
+    public String[] splitCarsName(String input) {
+        String[] carsNameArray = input.split(",");
+        return carsNameArray;
+    }
 }
--- src/test/java/racingcar/domain/UserTest.java
+++ src/test/java/racingcar/domain/UserTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 class UserTest {
-  
+    @Test
+    void split_메서드로_주어진_값을_구분() {
+        final User user = new User();
+        final String input = "pobi,woni,jun";
+
+        final String[] carsNameArray = user.splitCarsName(input);
+
+        assertThat(carsNameArray).contains("pobi", "woni", "jun");
+    }
 }
