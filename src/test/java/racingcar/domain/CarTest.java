a0611dbdcebe7f09b33306d4b390894fd735c4e8
leegwichan <cnddkscndgus@naver.com>
2023-10-27 00:08:26 +0900

    feat (Car) : 자동차 이름 검증 과정 구현
    - 자동차 이름 형식이 항상 유효하기 위해, 객체 생성 과정에 자동차 이름 검증 과정을 추가
    - 차 이름 앞,뒤에 공백은 출력시에 오해를 불러 일으킬 수 있어 지워서 사용함

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CarTest {
+
+    @Nested
+    @DisplayName("자동차 이름 검증 테스트")
+    class ValidationNameTest {
+
+        @ParameterizedTest(name = "input : [{0}]")
+        @CsvSource({"''", "' '", "steven", "CA Lee"})
+        @DisplayName("자동차 이름의 형식이 올바르지 않으면, 예외를 던진다")
+        void creationTest_whenFormatIsNotMatch(String name) {
+
+            assertThatThrownBy(() -> Car.from(name))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("자동차 이름은 빈칸이 아니어야 하고 5글자 이하이어야 합니다");
+        }
+
+        @Test
+        @DisplayName("자동자 이름에 null이 들어갈 경우 예외를 던진다")
+        void creationTest_whenFormatIsNotMatch() {
+
+            assertThatThrownBy(() -> Car.from(null)).isInstanceOf(NullPointerException.class);
+        }
+
+        @ParameterizedTest(name = "input : [{0}], expect name : [{1}]")
+        @CsvSource({"' steve','steve'", "'po bi ','po bi'", "' jun ','jun'"})
+        @DisplayName("자동차 이름 앞뒤의 빈칸이 있다면, 해당 빈칸을 잘라낸다")
+        void creationTest_whenNameHasFrontBehindBlank(String inputName, String expected) {
+            Car car = Car.from(inputName);
+
+            String actual = car.getName();
+
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+}
