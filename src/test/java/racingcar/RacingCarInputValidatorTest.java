b0783fba6417ac41ada8051f95fb981f7fed580f
kunsanglee <gosmdochee2@gmail.com>
2023-10-26 21:03:29 +0900

    test(RacingCarInputValidatorTest): 사용자에게 입력받은 시도 횟수 검증 테스트코드 작성

--- src/test/java/racingcar/RacingCarInputValidatorTest.java
+++ src/test/java/racingcar/RacingCarInputValidatorTest.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RacingCarInputValidatorTest {
 
@@ -43,6 +44,32 @@ public class RacingCarInputValidatorTest {
                 arguments(List.of("jㄹㅇ ", "!@#"), "자동차 이름은 한글, 영어, 숫자만 가능합니다."),
                 arguments(List.of("ja va", ""), "자동차 이름은 한글, 영어, 숫자만 가능합니다."),
                 arguments(List.of(" java", "    "), "자동차 이름은 한글, 영어, 숫자만 가능합니다.")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "5", "99", "100000000"})
+    void validateAttemptCountsSuccessTest(String attempt) {
+        RacingCarInputValidator racingCarInputValidator = new RacingCarInputValidator();
+        assertThatCode(() -> racingCarInputValidator.validateAttemptCounts(attempt))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidateAttemptCountsFailTestArguments")
+    void validateAttemptCountsFailTest(String attempt, String message) {
+        RacingCarInputValidator racingCarInputValidator = new RacingCarInputValidator();
+        assertThatCode(() -> racingCarInputValidator.validateAttemptCounts(attempt))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(message);
+    }
+
+    static Stream<Arguments> provideValidateAttemptCountsFailTestArguments() {
+        return Stream.of(
+                arguments("-1", "시도 횟수는 0 이상 2억 이하의 양수만 입력 가능합니다."),
+                arguments("일부러", "입력값이 숫자가 아닙니다."),
+                arguments("~!@#$%^&*()_+", "입력값이 숫자가 아닙니다."),
+                arguments("fail", "입력값이 숫자가 아닙니다.")
         );
     }
 }
