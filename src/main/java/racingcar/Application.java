930fe49d50c1bf2c16e801fbaf973e35f6f7f734
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-26 18:32:32 +0900

    feat: added outline

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.drive.Drive;
+import racingcar.race.Racing;
+import racingcar.driver.Driver;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Driver driver = new Driver();
+        Drive drive = new Drive();
+        Racing racing = new Racing();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String drivers = Console.readLine();
+        System.out.println("시도활 회수는 몇 회 인가요?");
+        String opportunity = Console.readLine();
+        System.out.println("최종 우승자 : ");
     }
 }
