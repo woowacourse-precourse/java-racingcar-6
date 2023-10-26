8bf0ecdb061148a122357096bc2cc4482d3586da
junOTL <naqp99@gmail.com>
2023-10-27 01:47:48 +0900

    feat: 중복된 자동차 이름을 입력하는 경우 예외 발생 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,5 +1,8 @@
 package racingcar.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Validator {
     public static void isEmpty(String input) {
         if (input.trim().isEmpty()) {
@@ -10,6 +13,16 @@ public class Validator {
     public static void isLengthLessThanFive(String input) {
         if (input.replaceAll("\\s", "").length() > 5) {
             throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static void isDuplicateName(String[] inputs) {
+        List<String> names = new ArrayList<>();
+        for (String input : inputs) {
+            if (names.contains(input)) {
+                throw new IllegalArgumentException("중복되지 않는 자동차 이름을 입력해주세요.");
+            }
+            names.add(input);
         }
     }
 }
