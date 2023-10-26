b67447d66b5928fd4af02c8ba8d8cd1acc3a05f9
hwan2-99 <thh1233123@gmail.com>
2023-10-26 18:24:05 +0900

    refactor: 경주할 자동차 입력을 위한 문구 출력메소드 클래스 변경(OutputMessage -> InputMessage)

---
+++ src/main/java/racingcar/view/InputMessage.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+import racingcar.model.MessageEnum;
+
+public class InputMessage {
+    public void inputCarNameMessage(){
+        System.out.println(MessageEnum.INPUT_CAR_NAME_MESSAGE.getMessage());
+
+    }
+}
--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -4,7 +4,4 @@ import racingcar.model.MessageEnum;
 
 public class OutputMessage {
 
-    public void inputCarNameMessage(){
-        System.out.println(MessageEnum.INPUT_CAR_NAME_MESSAGE.getMessage());
-    }
 }
