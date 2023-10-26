1782e932ea17fcf983764009b3de82b8dc628d91
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 21:16:02 +0900

    update(MoveCountValidator) : 이동 횟수 입력 값 검증 로직 validate()로 합치기

--- src/main/java/racingcar/validator/MoveCountValidator.java
+++ src/main/java/racingcar/validator/MoveCountValidator.java
@@ -7,19 +7,25 @@ public class MoveCountValidator {
     private MoveCountValidator() {
     }
 
-    public static void validateNull(String input) {
+    public static void validate(String input) {
+        validateNull(input);
+        validateInputFormat(input);
+        validateMoveCount(input);
+    }
+
+    private static void validateNull(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("아무런 값을 입력하지 않았습니다.");
         }
     }
 
-    public static void validateInputFormat(String input) {
+    private static void validateInputFormat(String input) {
         if (!input.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("횟수는 숫자만 입력할 수 있습니다.");
         }
     }
 
-    public static void validateMoveCount(String input) {
+    private static void validateMoveCount(String input) {
         int moveCount = Convertor.convertStringToInt(input);
         if (moveCount < 1) {
             throw new IllegalArgumentException("이동 횟수는 0보다 커야 합니다.");
