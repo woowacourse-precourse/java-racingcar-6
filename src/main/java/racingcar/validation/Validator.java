8dc7b1d857a5c6d270d3375d355576b9b4d1f591
donghyunkim <ghffu405@gmail.com>
2023-10-26 17:20:57 +0900

    feat: 이름 검증메소드 추가

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,4 +1,18 @@
 package racingcar.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
+
+    public static void validateNames(String names){
+        String regex = "([a-zA-Z0-9]{1,5})(,[a-zA-Z0-9]{1,5})*";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(names);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("잘못된 형식으로 입력했습니다.");
+        }
+    }
 }
