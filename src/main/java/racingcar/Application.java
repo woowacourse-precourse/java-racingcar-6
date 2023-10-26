e692c4b75ea6b20cb6ed4b53f57aa0e925a88f8d
noxknow <chris2769@naver.com>
2023-10-27 00:17:47 +0900

    feat(RacingCarGameController) : Application class를 통한 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+import racingcar.handler.InputHandler;
+import racingcar.view.ConsoleInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final InputHandler inputHandler = new ConsoleInput();
+
+        new RacingCarGameController(inputHandler).run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -0,0 +1,16 @@
+package racingcar.controller;
+
+import racingcar.handler.InputHandler;
+
+public class RacingCarGameController {
+
+    private final InputHandler inputHandler;
+
+    public RacingCarGameController(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public void run() {
+
+    }
+}
