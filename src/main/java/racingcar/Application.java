aa07ffa67506951ac7f73dc7c85c5e13ef8c3db3
Lee Hyunju <guswnee00@gmail.com>
2023-10-26 21:01:44 +0900

    자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,38 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        test();
     }
+
+    public static void test() {
+        try {
+            input_car_name();
+        } catch (IllegalArgumentException e){
+            throw e;
+        }
+    }
+    public static String[] input_car_name(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input_name = Console.readLine();
+        String[] names = input_name.split(",");
+
+        List<String> car_name_list = new ArrayList<>();
+        for (String name : names) {
+            String trimmed_name = name.trim();
+            if (trimmed_name.isEmpty() || trimmed_name.length() >=6) {
+                throw new IllegalArgumentException();
+            }
+            car_name_list.add(trimmed_name);
+        }
+        return car_name_list.toArray(new String[0]);
+    }
+
+
+
 }
