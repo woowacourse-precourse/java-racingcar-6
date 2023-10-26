a133f391f2a63b07291e2b2fa94399d98cec9b80
uhanuu <hwryu990927@gmail.com>
2023-10-26 20:43:01 +0900

    feat: 사용자 입력 로직

--- src/main/java/racingcar/entity/car/Name.java
+++ src/main/java/racingcar/entity/car/Name.java
@@ -1,5 +1,7 @@
 package racingcar.entity.car;
 
+import java.util.Objects;
+
 import static racingcar.constants.ErrorConstants.*;
 
 public class Name {
@@ -14,5 +16,18 @@ public class Name {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
---
+++ src/main/java/racingcar/request/RequestChecker.java
@@ -0,0 +1,47 @@
+package racingcar.request;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.entity.car.Name;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static racingcar.constants.ErrorConstants.DUPLICATE_NAME_ERROR_MESSAGE;
+
+public class RequestChecker {
+
+    public static Set<Name> requestNames(){
+        String[] names = Console.readLine().split(",");
+        return convertToName(names);
+    }
+
+    private static Set<Name> convertToName(String[] oldNames){
+        Set<Name> names = Arrays.stream(oldNames)
+                .map(Name::new)
+                .collect(Collectors.toSet());
+
+        if (!hasDuplicateCarName(oldNames.length,names.size())){
+            throw new IllegalArgumentException(DUPLICATE_NAME_ERROR_MESSAGE);
+        }
+
+        return names;
+    }
+
+    private static boolean hasDuplicateCarName(int oldNameSize, int nameSize){
+        return oldNameSize == nameSize;
+    }
+
+    public static int getNumberOfMovementAttempts(){
+        String movementAttempts = Console.readLine();
+        return parseInt(movementAttempts);
+    }
+
+    private static int parseInt(String movementAttempts) {
+        try {
+            return Integer.parseInt(movementAttempts);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("시도할 횟수는 숫자만 입력 가능합니다.");
+        }
+    }
+}
