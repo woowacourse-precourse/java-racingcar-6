4ff72765e9aea21340fb990cdb718bca860283c4
liarreez <liarreez@gmail.com>
2023-10-26 21:32:46 +0900

    feat : 자동차 입력 문구 출력 기능 구현

---
+++ src/main/java/racingcar/input/Input.java
@@ -0,0 +1,9 @@
+package racingcar.input;
+
+public class Input {
+    private final String INPUT_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public void printInputCarsMessage() {
+        System.out.println(INPUT_CARS_MESSAGE);
+    }
+}
