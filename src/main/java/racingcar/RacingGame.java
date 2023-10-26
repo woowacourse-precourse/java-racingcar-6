a305469b456169de6bd766d639b2606a589de95a
shin5774 <babo5774@naver.com>
2023-10-26 17:49:44 +0900

    feat: 이름길이 적합성 판단 기능 구현
    이름의 길이의 적합성 판단 및 부적합시 Exception을 발생시키는 기능 구현.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,11 +2,18 @@ package racingcar;
 
 public class RacingGame {
     public static final String COMMA=",";
+    public static final int NAME_MAX_LENGTH=5;
     public RacingGame(){
 
     }
 
     public String[] splitNames(String names){
         return names.split(COMMA);
+    }
+
+    public void checkNameCorrectRange(String name){
+        if(name.length()>NAME_MAX_LENGTH){
+            throw new IllegalArgumentException("이름이 5자를 초과했습니다. 초과이름: "+name);
+        }
     }
 }
