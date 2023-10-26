ba7b8c763077ef913daea3fee734898d1ed300a5
Park Jae Won <ploi9@naver.com>
2023-10-26 19:29:30 +0900

    레이싱카 랜덤값에 따른 이동 여부, Map 자료 구조에서 value 추출시 null이 레이싱카 이동여부에 입력 방지되는 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,9 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 
@@ -11,32 +11,35 @@ public class Application {
     public static void main(String[] args) {
 
         Application app = new Application();
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
 
-        app.checkInput(input);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNameList = Arrays.asList(Console.readLine().split(","));
+
+        app.checkInput(carNameList);
 
         System.out.println("시도할 횟수는 몇회인가요?");
         int racingGameCount = Integer.parseInt(Console.readLine());
 
+        Map<String, String> carsAndMoves = app.addCountWhenMove(racingGameCount, carNameList);
+
+        System.out.println(app.determineMoveTheFurthest(carsAndMoves, carNameList));
 
     }
 
-    private void checkInput(String input) { // 각 자동차의 이름은 5글자 이하, 쉼표로만 구분
-        List<String> carNameList = Arrays.asList(input.split(","));
+    private void checkInput(List<String> carNameList) { // 각 자동차의 이름은 5글자 이하, 쉼표로만 구
 
-        for (int i=0; i<carNameList.size(); i++) {
+        for (int i = 0; i < carNameList.size(); i++) {
             String carName = carNameList.get(i);
 
             if (carName.length() > 5) { // 5자를 초과할 경우 예외처리
                 throw new IllegalArgumentException("레이싱 카 이름이 5글자를 초과하였습니다.");
             }
 
-            if (Pattern.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*",carName)) { // 한글이 들어간 경우 예외처리
+            if (Pattern.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*", carName)) { // 한글이 들어간 경우 예외처리
                 throw new IllegalArgumentException("레이싱 카 이름에 한글이 포함되어 있습니다.");
             }
 
-            if(validateDuplication(carNameList, carName, i)){ // 이름이 중복될 경우 예외 처리
+            if (validateDuplication(carNameList, carName, i)) { // 이름이 중복될 경우 예외 처리
                 throw new IllegalArgumentException(carName + " 이름이 중복됩니다.");
             }
 
@@ -45,13 +48,57 @@ public class Application {
     }
 
     private boolean validateDuplication(List<String> carNameList, String carName, int index) {
-        for (int i = index+1; i < carNameList.size(); i++) {
+        for (int i = index + 1; i < carNameList.size(); i++) {
             if (carNameList.get(i).equals(carName)) {
                 return true;
             }
         }
         return false;
 
+    }
+
+    private Map<String, String> addCountWhenMove(int racingGameCount, List<String> carNameList){
+        Map<String, String> carsAndMoves = new HashMap<>();
+
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < racingGameCount; i++) {
+            for (int j = 0; j < carNameList.size(); j++) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+                preventNullValue(carsAndMoves, carNameList, randomNumber, j);
+
+                System.out.println(carNameList.get(j)+" : "+carsAndMoves.get(carNameList.get(j)));
+            }
+            System.out.println();
+        }
+        return carsAndMoves;
+    }
+
+    private void preventNullValue(Map<String, String> carsAndMoves ,List<String> carNameList, int randomNumber, int index) {
+        String moveCount = carsAndMoves.get(carNameList.get(index));
+
+        if (randomNumber > 4) {
+
+            if (moveCount != null)
+                carsAndMoves.put(carNameList.get(index), moveCount + "-");
+
+            if (moveCount == null)
+                carsAndMoves.put(carNameList.get(index), "-");
+        }
+
+        if (randomNumber <= 4) {
+
+            if (moveCount != null)
+                carsAndMoves.put(carNameList.get(index), moveCount);
+
+            if (moveCount == null)
+                carsAndMoves.put(carNameList.get(index), "");
+        }
+    }
+
+    private String determineMoveTheFurthest(Map<String, String> carsAndMoves, List<String> carNameList) {
+        return carsAndMoves.get(carNameList.get(0)).length() + "";
     }
 
 
