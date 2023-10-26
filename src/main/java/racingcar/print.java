237f903afce6b2da41724a18b51d52df0c92e98b
idingg <idingg@gmail.com>
2023-10-26 19:18:54 +0900

    feat: 경기 상황 출력 기능 추가
    경주의 모든 자동차 이름 및 상태 출력 기능 추가

--- src/main/java/racingcar/print.java
+++ src/main/java/racingcar/print.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class Print {
     public static void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -9,8 +11,16 @@ public class Print {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void carStatus(Car car) {
+    private static void carStatus(Car car) {
         System.out.print(car.getName() + " : ");
         System.out.println("-".repeat(car.getPosition()));
     }
+
+    public static void raceSituation(Race race) {
+        List<Car> cars = race.getCars();
+        for (Car car : cars) {
+            carStatus(car);
+        }
+    }
+
 }
