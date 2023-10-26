0e7d149649f4ec0b35a6fae2a8aa6028d894190c
wooteco-daram <wooteco.daram@gmail.com>
2023-10-27 06:56:45 +0900

    feat: 경주할 자동차 이름을 입력하는 문구를 출력하는 `printInputCarNameMessage` 메소드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,4 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static void printInputCarNameMessage() {
+        final String STRING_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        System.out.println(STRING_INPUT_CAR_NAME);
+    }
 }
