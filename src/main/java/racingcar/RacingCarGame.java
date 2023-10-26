58f1ebe2c459ef5d36752491c5eba678632c9657
Austin-Choi <austinsupra@gmail.com>
2023-10-26 21:52:49 +0900

    Feat: RacingCarGame 구현

---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,96 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import data.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarGame {
+    private final static int GO = 4;
+    List<Car> cars;
+    int tryLimit;
+    int max;
+    List<Car> winnerCars;
+
+    public RacingCarGame(){
+        cars = new ArrayList<>();
+        tryLimit = 0;
+        int max = -1;
+        winnerCars = new ArrayList<Car>();
+    }
+
+    public void run(){
+        inputNames();
+        inputTryLimit();
+        System.out.println();
+        printRaceResults();
+        setMax();
+        setWinnerCars();
+        printWinnerNames();
+    }
+    //inputNames
+    private void inputNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+        String[] names = inputNames.split(",");
+        for(String name : names){
+            cars.add(new Car(name));
+        }
+    }
+
+    //inputTryLimit
+    private void inputTryLimit(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputTryLimit = Console.readLine();
+        tryLimit = Integer.parseInt(inputTryLimit);
+    }
+
+    //raceCars
+    private void printRaceResults(){
+        int command;
+        System.out.println("실행 결과");
+        for(int tryNum = 0; tryNum < tryLimit; tryNum++){
+            for(Car car : cars){
+                command = Randoms.pickNumberInRange(1,9);
+                if(command >= GO)
+                    car.add();
+                System.out.println(car);
+            }
+            System.out.println();
+        }
+    }
+
+    //setMax
+    private void setMax(){
+        for(Car car : cars){
+            int currentLength = car.getTrackLength();
+            if(currentLength>max) {
+                max = currentLength;
+            }
+        }
+    }
+
+    //setWinnerCars
+    private void setWinnerCars(){
+        for(Car car : cars){
+            if(car.getTrackLength() == max)
+                winnerCars.add(car);
+        }
+    }
+
+    //printWinnerNames
+    public void printWinnerNames(){
+        StringBuilder winners = new StringBuilder();
+        winners.append("최종 우승자 : ");
+        for(int index = 0; index < winnerCars.size(); index++){
+            Car car = winnerCars.get(index);
+            winners.append(car.getName());
+            if(index != winnerCars.size()-1)
+                winners.append(", ");
+        }
+        System.out.println(winners);
+        Console.close();
+    }
+}
