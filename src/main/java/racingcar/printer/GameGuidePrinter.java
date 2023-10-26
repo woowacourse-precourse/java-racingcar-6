d3f913cef27bc68729c8766bf7ff7b03210a1b0d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-26 20:38:29 +0900

    [feat] : add game guide printer class with constant string list

---
+++ src/main/java/racingcar/printer/GameGuidePrinter.java
@@ -0,0 +1,8 @@
+package racingcar.printer;
+
+public class GameGuidePrinter {
+    private static final String carNameInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String tryCountMessage = "시도할 회수는 몇회인가요?";
+    private static final String resultMessage = "실행 결과";
+    private static final String finalWinnerMessage = "최종 우승자 : ";
+}
