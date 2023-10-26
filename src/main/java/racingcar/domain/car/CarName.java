b2a5e2aaeb0f5efb63aad483a2b209a7fb0cc30d
mainfn <_@mainfn.dev>
2023-10-26 21:35:20 +0900

    feat: CarName 구현

--- src/main/java/racingcar/domain/car/CarName.java
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -1,11 +1,32 @@
 package racingcar.domain.car;
 
+import java.util.List;
+
 public record CarName(String carName) {
+
+    public static final List<String> BANNED_CHARACTERS = List.of(" ", "\n", "\r", "\t");
+    public static final int MIN_CAR_NAME_LENGTH = 1;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+
     public CarName {
         validateCarNameLength(carName);
+        validateNotContainsBannedChars(carName);
     }
 
     private static void validateCarNameLength(final String carName) {
-        // TODO:
+        if (carName == null || carName.isEmpty() || carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException("차 이름은 1~5자여야 합니다.");
+        }
+    }
+
+    private static void validateNotContainsBannedChars(final String carName) {
+        if (containsBannedChars(carName)) {
+            throw new IllegalArgumentException("차 이름은 공백이 포함될 수 없습니다.");
+        }
+    }
+
+    private static boolean containsBannedChars(final String carName) {
+        return BANNED_CHARACTERS.stream()
+                .anyMatch(carName::contains);
     }
 }
