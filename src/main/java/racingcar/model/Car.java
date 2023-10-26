b3d4874686e76d6277a43f0de435f8e33a1167cf
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-27 01:30:31 +0900

    feat : Car Model 구성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,13 @@
 package racingcar.model;
 
 public class Car {
+    private String name;
+    private int currentPosition;
+    private boolean isMoving;
+
+    public Car(String name) {
+        this.name = name;
+        this.currentPosition = 0;
+        this.isMoving = false;
+    }
 }
