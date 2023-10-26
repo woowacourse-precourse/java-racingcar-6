bc596e2b74c763eda7b69cd5c913f4128e0d590c
wwan13 <wwan13@naver.com>
2023-10-27 00:56:38 +0900

    feat : `CarMovement` 기능 구현 - 생성시 이동 횟수를 0으로 초기화 한다. - 이동 횟수를 1만큼 더한다. - 이동 횟수 만큼 '-'를 담은 문자열을 반환한다. - 이동 횟수가 같으면 true를 반환한다.

---
+++ src/main/java/racingcar/domain/car/CarMovement.java
@@ -0,0 +1,46 @@
+package racingcar.domain.car;
+
+import java.util.Objects;
+
+public class CarMovement {
+
+    private static final int INITIAL_VALUE = 0;
+    private static final String DASH = "-";
+
+    private int movement;
+
+    public CarMovement() {
+        this.movement = INITIAL_VALUE;
+    }
+
+    public void goForward() {
+        movement += 1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < movement; i++) {
+            stringBuilder.append(DASH);
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof CarMovement)) {
+            return false;
+        }
+
+        CarMovement carMovement = (CarMovement) object;
+        return movement == carMovement.movement;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movement);
+    }
+}
