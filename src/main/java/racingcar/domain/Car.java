40cf47dc41c03756b5b1d16c2ef23ef7797dbe2a
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-26 22:28:13 +0900

    feat(Car): 5자 이하의 이름

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,19 @@
 package racingcar.domain;
 
 public class Car {
+    private static final String INVALID_LENGTH = "자동차 이름의 길이는 5자 이하여야합니다.";
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (5 < name.length()) {
+            throw new IllegalArgumentException(INVALID_LENGTH);
+        }
+    }
 }
