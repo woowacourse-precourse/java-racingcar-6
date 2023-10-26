e64e66978f25bce906ed46a832c6544203f31126
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 21:19:52 +0900

    [fix] 입력값 판별시 음수를 숫자로 파악 못하는 오류 수정

--- src/main/java/racingcar/util/io/InputValidator.java
+++ src/main/java/racingcar/util/io/InputValidator.java
@@ -24,7 +24,7 @@ public class InputValidator {
     public static void validateMoveNumberInput(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("입력 값이 없습니다.");
-        } else if (!input.matches("\\d+")) {
+        } else if (!input.matches("-?\\d+")) {
             throw new IllegalArgumentException("입력 값이 숫자가 아닙니다.");
         } else if (Integer.parseInt(input) < 0) {
             throw new IllegalArgumentException("0 혹은 양수를 입력하세요");
