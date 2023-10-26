281d8b5b06c32c5505f880463b2e8836d61d75ef
dltjdn <hana40004@naver.com>
2023-10-27 05:04:45 +0900

    refactor(InputView): asList 사용해서 리스트 생성
    * 스트림과 컬렉터 사용하지 않아도 됨

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,12 +10,11 @@ public class InputView {
     private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_PROMPT = "시도할 회수는 몇회인가요?";
 
-    public List<String> readCarName(){
+    public List<String> readCarNames(){
         System.out.println(CAR_NAME_INPUT_PROMPT);
-        String names = Console.readLine();
+        String input = Console.readLine();
 
-        return Arrays.stream(names.split(","))
-                .collect(Collectors.toList());
+        return Arrays.asList(input.split(","));
     }
 
     public int readTryCount(){
