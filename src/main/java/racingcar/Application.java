328a4bf1ddbf18345fb707fd89f66445ea61ea6a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-27 01:55:45 +0900

    feat: 시도할 횟수를 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,13 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
 
+        User user = new User();
+
         // TODO: 프로그램 구현
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        user.getCarNamesFromUser();
+        System.out.println("시도할 회수는 몇회인가요?");
+        user.getNumberOfMovesFromUser();
     }
 }
--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -8,9 +8,6 @@ import java.util.List;
 
 public class User {
 
-    /** Get Car Names From User
-     *  사용자로부터 자동차 이름을 입력받는 메서드
-     */
     public List<String> getCarNamesFromUser() {
 
         String carName = Console.readLine().replace(" ", "").toLowerCase();
@@ -18,5 +15,12 @@ public class User {
         List<String> carNames = new ArrayList<>(Arrays.asList(carName.split(",")));
 
         return carNames;
+    }
+
+    public int getNumberOfMovesFromUser() {
+
+        int numberOfTimes = Integer.parseInt(Console.readLine());
+
+        return numberOfTimes;
     }
 }
