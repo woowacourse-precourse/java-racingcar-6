39f2e88d3e45d2bb15fde44492f195f5c265a209
park_yeongyu <sjjs7771@naver.com>
2023-10-26 23:56:13 +0900

    feat : Application 클래스에 main 에 자동차 경기중 모습을 출력하는 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,5 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Race race = new Race();
         race.start();
+        race.setTrial();
+        race.running();
     }
 }
