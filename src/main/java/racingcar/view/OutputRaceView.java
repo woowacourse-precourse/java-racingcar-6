a1095f321468f1821248d48408e10922b2bb6b51
euics <pjh575157@gmail.com>
2023-10-26 21:52:43 +0900

    refactor: OutputRaceView 클래스 Magic Number 변수 수정
    수정사항
    - 해당 역할에 맞게 Magic Number 클래스 별로 분리
      - OutputRaceViewConstant: RACE_RESULT_HEADER, WINNER_ANNOUNCEMENT

--- src/main/java/racingcar/view/OutputRaceView.java
+++ src/main/java/racingcar/view/OutputRaceView.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 import racingcar.model.Car;
-import racingcar.util.Constants;
+import racingcar.util.OutputRaceViewConstant;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +19,7 @@ public class OutputRaceView {
     }
 
     public static void printWinners(List<Car> winners) {
-        System.out.println(Constants.WINNER_ANNOUNCEMENT + winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
+        System.out.println(OutputRaceViewConstant.WINNER_ANNOUNCEMENT
+                + winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
     }
 }
