b171add6d99c5e0bd63b363a309e06722cc6f5be
uhanuu <hwryu990927@gmail.com>
2023-10-26 21:31:54 +0900

    feat: 시작 메시지 출력 클래스

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    @DisplayName("시작 메시지를 출력할 수 있다.")
+    public void printStartMessage() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
