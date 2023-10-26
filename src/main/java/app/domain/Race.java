f1f798247f0e21904cebdd4967fc9bb333c5f682
김승우 <tmd8633@naver.com>
2023-10-26 20:20:53 +0900

    Add : domain-Race 객체 추가

--- src/main/java/app/domain/Race.java
+++ src/main/java/app/domain/Race.java
@@ -3,16 +3,16 @@ package app.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class play {
+public class Race {
 
     private Map<String, Integer> race;
 
-    public play(String raceList) {
-        this.race = Arrays.stream(raceList.split(","))
-                .collect(Collectors.toMap(key -> key, value -> 0));
+    public Race(String raceList) {
+        this.race = Arrays.stream(raceList.split(",")).collect(Collectors.toMap(key -> key, value -> 0));
     }
 
     public Map<String, Integer> getRace() {
@@ -29,10 +29,26 @@ public class play {
     }
 
 
-    public void printResult() {
+    public void printPlay() {
         for (String car : race.keySet()) {
             System.out.println(car + " : " + "-".repeat(race.get(car)));
         }
         System.out.println();
+    }
+
+    public int getMaxNum() {
+        return race.entrySet()
+                .stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue))
+                .get()
+                .getValue();
+    }
+
+    public String[] getWinner(int max) {
+        return race.entrySet()
+                .stream()
+                .filter(x -> x.getValue() == max)
+                .map(x -> x.getKey())
+                .toArray(String[]::new);
     }
 }
