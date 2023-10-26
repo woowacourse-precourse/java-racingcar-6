8a4f496da234aaf58f8e1d1d33930a96c373ffff
leeedohyun <ldh19738@gmail.com>
2023-10-26 21:01:23 +0900

    feat: 자동차 움직이는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,15 +6,26 @@ public class Car {
     private static final int NAME_MAX_LENGTH = 5;
     private static final int FORWARD_BASE_NUMBER = 4;
     private static final String CAR_NAME_LENGTH_EXCEPTION_MESSAGE = "5자 이하의 이름만 가능합니다.";
+    private static final String EMPTY_SPACE = "";
+    private static final String FORWARD_MOVEMENT_INDICATOR = "-";
+    private static final String SEPARATOR = " : ";
 
     private final String name;
+    private String location = EMPTY_SPACE;
 
     public Car(String name) {
         validateCarNameLength(name);
         this.name = name;
     }
 
-    public boolean isForward(int randomNumber) {
+    public String move(int randomNumber) {
+        if (isForward(randomNumber)) {
+            location += FORWARD_MOVEMENT_INDICATOR;
+        }
+        return name + SEPARATOR + location;
+    }
+
+    private boolean isForward(int randomNumber) {
         return randomNumber >= FORWARD_BASE_NUMBER;
     }
 
