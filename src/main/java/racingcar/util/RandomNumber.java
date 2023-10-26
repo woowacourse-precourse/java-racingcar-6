880c879b63bb49dae244b610fcf78f7fb6fb6bcb
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 20:38:10 +0900

    [feat] 랜덤 숫자 이동 가능 여부 판별 기능 구현

--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -7,4 +7,8 @@ public class RandomNumber {
     public static int createRandomNumber() {
         return Randoms.pickNumberInRange(0,9);
     }
+
+    public static boolean canMove(int number) {
+        return number >= 4;
+    }
 }
