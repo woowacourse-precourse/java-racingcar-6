26927a59877e5d7c81f4b6512d1243363406ed0e
park_yeongyu <sjjs7771@naver.com>
2023-10-26 22:16:09 +0900

    feat : CarSetting 클래스에 랜덤 번호 만드는 함수 작성

---
+++ src/main/java/racingcar/CarSetting.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class CarSetting {
+    Input input = new Input();
+    String[] carName = input.getInputCars().split(",");
+    Car[] car = new Car[carName.length];
+
+    public void setCars(){
+        for(int index =0; index<carName.length; index++){
+            car[index] = new Car();
+            car[index].setCarName(carName[index]);
+
+        }
+    }
+    public int makeRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
