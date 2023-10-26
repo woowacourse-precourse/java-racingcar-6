200154c8b4beca43a44fd9b13b0bfdb58eb0e2aa
koojawon <jawonkoo@naver.com>
2023-10-26 21:19:59 +0900

    feat: InputValidator생성 -참가자의 이름이 유효한지 검증 -시뮬레이션 횟수가 유효한지 검증

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,36 @@
+package racingcar.validator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class InputValidator {
+
+    static int MAX_LENGTH = 5;
+
+    public ArrayList<String> validateParticipant(String s) {
+        Arrays
+                .stream(s.split(","))
+                .forEach(this::checkEmptyOrThrow);
+
+        return Arrays
+                .stream(s.split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public int validateCount(String s) {
+        int value;
+        try {
+            value = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return value;
+    }
+
+    void checkEmptyOrThrow(String s) {
+        if (s.isEmpty() || s.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
