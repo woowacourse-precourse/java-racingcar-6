ff14089bdf7e0e96ff7b02f0a5ecf9855d9ff53f
진환 정 <joung.jinhawn@gmail.com>
2023-10-26 18:19:29 +0900

    구현 기능 목록 4, 6 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
+        RacingCarGame game = new RacingCarGame();
 
+        game.startGame();
     }
 }
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -2,17 +2,35 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RacingCarGame {
     private String[] carNames;
     private CarInfo[] carArray;
     private int movementAmonut;
-    public RacingCarGame(){
+    private Queue<String> finalWinners;
 
+    public RacingCarGame(){
+        this.carNames = null;
+        this.carArray = null;
+        movementAmonut = 0;
+        finalWinners = new LinkedList<>();
     }
 
     public void startGame(){
+        inputCarNameMessage();
+        inputCarInfo();
+        inputMovementAmountMessage();
+        inputMovementAmount();
+        printCurrentResultMessage();
 
+        for(int i = 0; i < this.movementAmonut; i++) {
+            moveCars();
+            printCurrentResult();
+        }
+
+        printFinalResult();
     }
 
     private void inputCarInfo(){
@@ -94,10 +112,62 @@ public class RacingCarGame {
     }
 
     private void printCurrentResult(){
+        for(CarInfo car: this.carArray){
+            printCurrentCarInfo(car);
+        }
 
+        System.out.println();
+    }
+
+    private void printCurrentCarInfo(CarInfo car){
+        System.out.print(car.getCarName() + " : ");
+
+        for(int i = 0; i < car.getTotalMovement(); i++) {
+            System.out.print("-");
+        }
+
+        System.out.println();
     }
 
     private void printFinalResult(){
+        insertFinalWinners();
 
+        System.out.print("최종 우승자 : ");
+
+        String winner;
+        while(this.finalWinners.size() > 1) {
+            winner = this.finalWinners.poll();
+
+            System.out.print(winner + ", ");
+        }
+        winner = this.finalWinners.poll();
+        System.out.print(winner);
+    }
+
+    private void insertFinalWinners() {
+        int maximum = getMaximumMovement();
+
+        for(CarInfo car: this.carArray) {
+            if(car.getTotalMovement() >= maximum) {
+                finalWinners.add(car.getCarName());
+            } else {
+                continue;   // do nothing
+            }
+        }
+    }
+
+    private int getMaximumMovement() {
+        int maximum = 0;
+
+        for(CarInfo car: this.carArray){
+            if(maximum < car.getTotalMovement()){
+                maximum = car.getTotalMovement();
+            }
+            else{
+                continue;   // do nothing
+            }
+        }
+
+        return maximum;
     }
 }
