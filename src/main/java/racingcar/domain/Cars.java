6f95b5d70071fc6061c879a991ef1998039aeae2
yechop <channetattoo@gmail.com>
2023-10-26 21:18:33 +0900

    feat: List<Car>를 가진 Cars 객체로 변경 후 메서드 생성
    각 Car를 랜덤 번호 생성후 3보다 큰 숫자일시 움직이도록 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,14 +1,37 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    List<String> carNames;
-    int gameCount;
 
-    public Cars(List<String> carNames, int gameCount) {
-        this.carNames = carNames;
-        this.gameCount = gameCount;
+    List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        for (int i = 0; i < carNames.size(); i++) {
+            Car car = new Car(carNames.get(i));
+            cars.add(car);
+        }
     }
 
+    public void move(int gameCount) {
+        for (int j = 0; j < gameCount; j++) {
+            moveEachCar();
+        }
+    }
+
+    private void moveEachCar() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        for (int i = 0; i < cars.size(); i++) {
+            int randomNumber = randomNumberGenerator.generateNumber();
+            Car car = cars.get(i);
+            moveIfGreaterThanThree(car, randomNumber);
+        }
+    }
+
+    private void moveIfGreaterThanThree(Car car, int randomNumber) {
+        if (randomNumber > 3) {
+            car.move();
+        }
+    }
 }
