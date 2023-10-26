4d76342b74cd3b670e1b3ddde657b5469875f562
eom-tae-in <eti0728@daum.net>
2023-10-27 08:07:21 +0900

    feat: RacingGame으로 추상화시키기 위해 필요한 메서드 추가

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,6 +1,13 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.Map;
+
 public interface RacingGame {
 
     void move();
+
+    Map<Vehicle, Position> getRacingTrack();
+
+    List<String> getWinner();
 }
