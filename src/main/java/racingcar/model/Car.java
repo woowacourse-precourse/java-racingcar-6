166b67c8238efbccdf0c3ec12c6fbde4c45758ba
eom-tae-in <eti0728@daum.net>
2023-10-27 03:49:17 +0900

    feat: Car 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,30 @@
 package racingcar.model;
 
+import static racingcar.util.Constant.MAX_CAR_NAME_LENGTH;
+
+import racingcar.exception.car.NotValidNameLengthException;
+
 public class Car implements Vehicle {
+
+    private final String name;
+
+    public Car(final String name) {
+        validateCarName(name);
+        this.name = name;
+    }
+
+    private void validateCarName(final String name) {
+        if (isNameLengthValid(name)) {
+            throw new NotValidNameLengthException(name);
+        }
+    }
+
+    private boolean isNameLengthValid(final String name) {
+        return name.length() < MAX_CAR_NAME_LENGTH.getValue();
+    }
+
+    @Override
+    public boolean canMove() {
+        return false;
+    }
 }
