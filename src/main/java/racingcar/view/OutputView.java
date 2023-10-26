62e30d6741d0b7b6481b72b9a7eb7b8cc96641a3
donghyunkim <ghffu405@gmail.com>
2023-10-26 17:13:10 +0900

    feat: 우승자출력 메소드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,12 +2,32 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     public void printResult(List<Car> carList){
         carList.stream()
                 .forEach(car -> System.out.println(car.toString()));
+    }
+
+    public void printWinner(List<Car> carList){
+        int maxMoveNumber = carList.stream()
+                .mapToInt(Car::getMoveNumber)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException());
+
+        List<String> winnerNames = carList.stream()
+                .filter(car -> car.getMoveNumber() == maxMoveNumber)
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+
+        String join = String.join(", ", winnerNames);
+
+        System.out.println("최종 우승자 : " + join);
+
+
     }
 }
