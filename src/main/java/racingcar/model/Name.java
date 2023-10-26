9a27496bb5b5523f9d6d9ee248a0182e6e666aa3
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-27 02:55:52 +0900

    refactor: Name 검증 메서드로 전달해주는 인자 final 선언

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -14,7 +14,7 @@ public class Name {
         return value;
     }
 
-    private void validate(String name){
+    private void validate(final String name){
         if(isEmpty(name)){
             throw new IllegalArgumentException();
         }
@@ -28,11 +28,11 @@ public class Name {
         }
     }
 
-    private boolean isEmpty(String name){
+    private boolean isEmpty(final String name){
         return name.isBlank() || name == null;
     }
 
-    private boolean isDigit(String name){
+    private boolean isDigit(final String name){
         for (int i = 0; i < name.length(); i++) {
             if(Character.isDigit(name.charAt(i))){
                 return true;
