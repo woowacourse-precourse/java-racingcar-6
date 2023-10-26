047dd2e5f14c4b3d83d3ff19a44b71e8df8437dc
liarreez <liarreez@gmail.com>
2023-10-26 22:30:42 +0900

    feat : 경주할 자동차 이름 입력 받는 기능 구현

--- src/main/java/racingcar/input/Input.java
+++ src/main/java/racingcar/input/Input.java
@@ -1,9 +1,15 @@
 package racingcar.input;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
     private final String INPUT_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public void printInputCarsMessage() {
         System.out.println(INPUT_CARS_MESSAGE);
+    }
+
+    private String inputCarsName() {
+        return Console.readLine();
     }
 }
