1b2389d24b850dfb705c60ac4dad673f772bac54
park_yeongyu <sjjs7771@naver.com>
2023-10-26 22:02:29 +0900

    feat : Input 클래스에 자동차 반환 함수 작성

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,14 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
-    String Cars;
-    int NumberOfAttempt;
-    public void inputCars(){
-        Cars = Console.readLine();
+    String cars;
+    int numberOfAttempt;
+
+    public void inputCars() {
+        cars = Console.readLine();
     }
 
-    public void inputNumberOfAttempt(){
-        NumberOfAttempt = Integer.parseInt(Console.readLine());
+    public void inputNumberOfAttempt() {
+        numberOfAttempt = Integer.parseInt(Console.readLine());
+    }
+
+    public String getInputCars() {
+        return cars;
     }
 }
