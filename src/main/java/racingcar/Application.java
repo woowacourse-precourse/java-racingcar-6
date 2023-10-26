ed92cb1dfc1293d8ceaf3b45d08eacd8fb40953b
IMWoo94 <leemeawoo94@gmail.com>
2023-10-27 02:12:46 +0900

    feat : 게임 실행 기능 구현 Controller 를 통해서 게임 실행
    게임 실행 기능 구현 Controller 를 통해서 게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,52 +1,10 @@
 package racingcar;
 
-import java.util.List;
-import racingcar.domain.Car;
-import racingcar.views.InputView;
-import racingcar.views.OutputView;
+import racingcar.controller.RacingCarGameController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        // 오프닝 멘트
-        OutputView.racingCarOpeningComment();
-        // 자동차 이름 입력 받기
-        List<Car> cars = InputView.getCarNames();
-//        for (Car car : cars) {
-//            System.out.println("car.getName() = " + car.getName());
-//        }
-        // 도전 횟수 멘트
-        OutputView.tryCountInputComment();
-        // 도전 횟수 입력 받기
-        Integer tryCount = InputView.getTryCount();
-//        System.out.println("tryCount = " + tryCount);
-
-        // 레이싱 게임 결과 멘트
-        OutputView.racingCarResultComment();
-
-        // 레이싱 게임 시작
-        for (int i = 0; i < tryCount; i++) {
-            cars.stream()
-                    .forEach(car -> {
-                        car.tryAddPosition();
-                        car.carPositionPrint();
-                    });
-            OutputView.println();
-        }
-
-        // 레이싱 결과 출력
-        // 최대 거리 값 구하기
-        int maxPos = cars.stream().mapToInt(car -> car.getDistance()).max().getAsInt();
-        String[] resultArr = cars.stream()
-                .filter(car -> car.carEqualsMaxDistance(maxPos))
-                .map(car -> car.getName())
-                .toArray(String[]::new);
-
-//        for (String s : resultArr) {
-//            System.out.println("s = " + s);
-//        }
-
-        // 우승자 출력
-        OutputView.racingCarWinnerPrint(resultArr);
+        RacingCarGameController racingCarGame = new RacingCarGameController();
+        racingCarGame.racingCarGameStart();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -0,0 +1,57 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.views.InputView;
+import racingcar.views.OutputView;
+
+public class RacingCarGameController {
+
+    private List<Car> cars;
+    private Integer movingCount;
+
+    public RacingCarGameController() {
+    }
+
+    public void racingCarGameStart() {
+        racingCarGameInit();
+
+        racingCarGameRun();
+
+        racingCarGameResult();
+    }
+
+    private void racingCarGameInit() {
+        OutputView.racingCarOpeningComment();
+        cars = InputView.getCarNames();
+
+        OutputView.tryCountInputComment();
+        movingCount = InputView.getMovingCount();
+    }
+
+    private void racingCarGameRun() {
+        OutputView.racingCarResultComment();
+
+        for (int i = 0; i < movingCount; i++) {
+            cars.forEach(car -> {
+                car.tryAddPosition();
+                car.carPositionPrint();
+            });
+            OutputView.println();
+        }
+    }
+
+    private void racingCarGameResult() {
+        int maxPos = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElseGet(() -> 0);
+
+        String[] winnerArr = cars.stream()
+                .filter(car -> car.carEqualsMaxDistance(maxPos))
+                .map(Car::getName)
+                .toArray(String[]::new);
+
+        OutputView.racingCarWinnerPrint(winnerArr);
+    }
+}
