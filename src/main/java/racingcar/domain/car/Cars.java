c1134dd7b76130b10d276fb0250e1c386fb2386a
mainfn <_@mainfn.dev>
2023-10-26 23:19:56 +0900

    feat: Cars 생성 시 중복 Car 검증 로직 추가

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,12 +1,15 @@
 package racingcar.domain.car;
 
 import java.util.List;
-import racingcar.domain.move.MoveDecider;
+import racingcar.domain.game.MoveResult;
+import racingcar.domain.game.RoundResult;
+import racingcar.domain.move.MoveCommander;
 
 public final class Cars {
     private final List<Car> cars;
 
     private Cars(final List<Car> cars) {
+        validateDuplicateCars(cars);
         this.cars = cars;
     }
 
@@ -14,7 +17,22 @@ public final class Cars {
         return new Cars(cars);
     }
 
-    public void moveAllBy(final MoveDecider moveDecider) {
-        // TODO:
+    private static void validateDuplicateCars(final List<Car> cars) {
+        final int uniqueCarsSize = (int) cars.stream().distinct().count();
+        System.out.println(uniqueCarsSize);
+        final boolean hasDuplicateCarName = uniqueCarsSize != cars.size();
+
+        if (hasDuplicateCarName) {
+            throw new IllegalArgumentException();
+        }
     }
+
+    public RoundResult moveAllBy(final MoveCommander moveCommander) {
+        final List<MoveResult> moveResults = cars.stream()
+                .map(car -> car.movedBy(moveCommander.nextCommand()))
+                .toList();
+
+        return new RoundResult(moveResults);
+    }
+
 }
