92223b539f6e8bb00b766edd9afd83e3b2fb39a4
euics <pjh575157@gmail.com>
2023-10-26 21:52:25 +0900

    refactor: InputRaceViewConstant 클래스 Magic Number 변수 수정
    수정사항
    - 해당 역할에 맞게 Magic Number 클래스 별로 분리
      - InputRaceViewConstant: CAR_NAME_PROMPT, RACE_COUNT_PROMPT

--- src/main/java/racingcar/view/InputRaceView.java
+++ src/main/java/racingcar/view/InputRaceView.java
@@ -1,16 +1,16 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.util.Constants;
+import racingcar.util.InputRaceViewConstant;
 
 public class InputRaceView {
     public static String readCarNames() {
-        System.out.println(Constants.CAR_NAME_PROMPT);
+        System.out.println(InputRaceViewConstant.CAR_NAME_PROMPT);
         return Console.readLine();
     }
 
     public static int readRaceCount() {
-        System.out.println(Constants.RACE_COUNT_PROMPT);
+        System.out.println(InputRaceViewConstant.RACE_COUNT_PROMPT);
         return Integer.parseInt(Console.readLine());
     }
 }
