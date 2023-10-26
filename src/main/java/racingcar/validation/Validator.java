4b44ffdec4c990b3e43064b1a769a7a314b776e6
donghyunkim <ghffu405@gmail.com>
2023-10-26 17:42:42 +0900

    feat: 1이상의숫자 검증메소드 추가

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -15,4 +15,10 @@ public class Validator {
             throw new IllegalArgumentException("잘못된 형식으로 입력했습니다.");
         }
     }
+
+    public static void validateOverOneNumber(String str){
+        if(!str.matches("^[1-9][0-9]*$")){
+            throw new IllegalArgumentException("1이상의 숫자만 입력 해야합니다.");
+        }
+    }
 }
