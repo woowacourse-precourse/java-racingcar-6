eb23621a221c43de79874b61c2248d4141ce987b
minjun7410 <minjun7410@gmail.com>
2023-10-27 00:18:23 +0900

    feat(Car): 자동차 이름 0자 예외처리 추가
    * 자동차 이름이 공백일 때 예외처리하는 메서드 validateCarNameLengthZero 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,12 +6,20 @@ public class Car {
 
     public Car(String carName) {
         validateCarNameLengthExceed(carName);
+        validateCarNameLengthZero(carName);
         this.carName = carName;
         this.movingDistance = 0;
     }
+
     private void validateCarNameLengthExceed(String carName) {
         if (carName.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름은 5자를 넘어갈 수 없습니다.");
+        }
+    }
+
+    private void validateCarNameLengthZero(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
         }
     }
 }
