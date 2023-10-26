74d43af17b1a30e33f5ba03a9d3b398344fe4c8a
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-27 01:28:24 +0900

    feat: 차의 이름이 5를 넘으면 exception을 던지게 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,6 +7,9 @@ public class Car {
     private int distance = 0;
 
     public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
     }
 
