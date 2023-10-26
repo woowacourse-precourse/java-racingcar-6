68bf95c7220530c6f1aed65a080e2d49c5318ab3
beongjun1 <beongjun1@naver.com>
2023-10-26 23:28:42 +0900

    기능 1

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] cars_name;
+        Integer[] cars_race;
+        int n;
+        String input;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        input=Console.readLine();
+        cars_name=input.split(",");
+        cars_race=new Integer[cars_name.length];
+        for (int i=0;i<cars_race.length;i++){
+            cars_race[i]=0;
+        }
+
     }
 }
