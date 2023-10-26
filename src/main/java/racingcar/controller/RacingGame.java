c622cbd432d1dfb37c16a44b4f5892293765235f
Isaac Han <isaachan233@gmail.com>
2023-10-27 07:46:09 +0900

    feat: 차 이름에 대한 입력과 출력을 처리하는 함수 생성

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,18 +1,30 @@
 package racingcar.controller;
 
+import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGame {
     private final OutputView outputView;
-    private final InputView iutputView;
+    private final InputView inputView;
+    private final RacingGameService racingGameService;
     
 
     public RacingGame() {
         this.outputView = new OutputView();
-        this.iutputView = new InputView();
+        this.inputView = new InputView();
+        this.racingGameService = new RacingGameService();
     }
 
     public void run() {
+        handleCarNameInput();
     }
+    
+    public void handleCarNameInput() {
+        outputView.printCarNameInputMessage();
+        String carNames = inputView.carNameInput();
+        racingGameService.addCars(carNames);
+    }
+    
+    
 }
