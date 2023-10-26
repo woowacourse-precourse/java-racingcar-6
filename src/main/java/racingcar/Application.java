6569919937a12379e0fa0f126ff6fe1edd9bbc83
IMWoo94 <leemeawoo94@gmail.com>
2023-10-26 17:09:12 +0900

    feat : 자동차 입력 받기 기능 구현 자동차 입력 값에 대한 null, 빈값 확인 로직 구현
    자동차 값 입력 받기 구현
    자동차 값에 대해서 빈값, null 인 경우
    CarNameIncorrectException 예외 발생 <- IllegalArgumentException 상속받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,12 @@
 package racingcar;
 
+import racingcar.views.InputView;
 import racingcar.views.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         OutputView.racingCarOpeningComment();
+        InputView.getCarNames();
     }
 }
---
+++ src/main/java/racingcar/views/InputView.java
@@ -0,0 +1,38 @@
+package racingcar.views;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.CarNameIncorrectException;
+
+/**
+ * - 경주할 자동자 이름 입력 기능 - 경주할 자동차 이름들은 입력 받는다. ( camp.nextstep.edu.missionutils.Console의 readLine() ) - 입력 받은 이름은 쉼표로 구분
+ * 하여 관리 - 입력 받은 이름 중 5자 이하 인지 확인 - 5자 이하인 경우 정상 진행 - 5자 초과인 경우 IllegalArgumentException 예외 발생
+ */
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String getCarNames() {
+        String carnames = Console.readLine();
+        checkNullAndEmpty(carnames);
+        return carnames;
+    }
+
+    private static void checkNullAndEmpty(String str) {
+        isNull(str);
+        isEmpty(str);
+    }
+
+
+    private static void isNull(String str) {
+        if (str == null) {
+            throw new CarNameIncorrectException("null 이 입력 되었습니다.");
+        }
+    }
+
+    private static void isEmpty(String str) {
+        if (str.isEmpty()) {
+            throw new CarNameIncorrectException("빈값 이 입력 되었습니다.");
+        }
+    }
+}
