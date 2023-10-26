c49072ae6a9643d6ec7774f2003228d6c3b8745f
yechop <channetattoo@gmail.com>
2023-10-26 20:56:43 +0900

    refactor, docs: 메소드 기능 수정
    랜덤 숫자 생성을 숫자 1개만 리턴하도록 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -7,12 +7,17 @@ import java.util.List;
 
 public class RandomNumberGenerator {
 
-    public List<Integer> collectNumbers(int carsNumber) {
+/*    public List<Integer> collectNumbers(int carsNumber) {
         List<Integer> numbers = new ArrayList<>();
         for(int i=0; i<carsNumber; i++) {
             int num = Randoms.pickNumberInRange(0, 9);
             numbers.add(num);
         }
         return numbers;
+    }*/
+
+    public int generateNumber() {
+        int num = Randoms.pickNumberInRange(0, 9);
+        return num;
     }
 }
