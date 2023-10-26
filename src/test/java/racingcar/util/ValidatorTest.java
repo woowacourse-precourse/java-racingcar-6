f62226a76ec04f74804323245177768416a9886f
junOTL <naqp99@gmail.com>
2023-10-27 01:14:37 +0900

    test: 빈값 입력 예외 테스트

---
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -0,0 +1,17 @@
+package racingcar.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ValidatorTest {
+    @ParameterizedTest
+    @DisplayName("빈값 입력 예외 테스트")
+    @ValueSource(strings = {"", " ", "  ", "   "})
+    void invalidInputsTest1(String input) {
+        Assertions.assertThatThrownBy(() -> Validator.isEmpty(input)).
+                isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INPUT_VALUE);
+    }
+}
