2787bebd3a03b085dad6639c6875e76d1a5eb8c3
kyu-wooon <catdog0831@gmail.com>
2023-10-26 15:57:43 +0900

    최초 커밋 - README.md 업데이트 / 패키지 import

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,4 +1,6 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
