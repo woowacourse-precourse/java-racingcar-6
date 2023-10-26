7179866d8d902baf022199f941c1ea157488a00f
Firedrago95 <yonghwa95@naver.com>
2023-10-26 20:18:49 +0900

    feat : 게임횟수를 입력받는 기능 구현

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String REQUEST_ROUNDS_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static String requestCarNames() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String requestRounds() {
+        System.out.println(REQUEST_ROUNDS_MESSAGE);
         return Console.readLine();
     }
 }
