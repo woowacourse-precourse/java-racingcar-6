9f3234b11e95dad6c956fe5669fb100ef410446e
Nara Park <skfk6352@naver.com>
2023-10-27 04:37:48 +0900

    <feat>: create method for printing game winner message
    최종 우승자 출력을 알리는 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,13 @@ package racingcar.view;
 public class OutputView {
 
     private static final String GAME_RESULT_MESSAGE = "실행 결과";
+    private static final String GAME_WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void printResultMessage() {
         System.out.println(GAME_RESULT_MESSAGE);
+    }
+
+    public static void printWinnerMessage() {
+        System.out.print(GAME_WINNER_MESSAGE);
     }
 }
