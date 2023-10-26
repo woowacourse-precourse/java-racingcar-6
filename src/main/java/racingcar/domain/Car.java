311d22f6f1fd5acd42ca1f68666d46d67de1fede
yuseonjun <seonjun0906@gmail.com>
2023-10-26 18:14:44 +0900

    feat: 0에서 9 사이에 무작위 값을 구한 후 무작위 값이 4 이상이면 전진하고, 4 이하이면 전진하지 않는다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -10,9 +10,27 @@ public class Car {
         this.name = name;
     }
 
+    public void move(int number) {
+        if (judgeNumber(number)) {
+            position++;
+        }
+    }
+
+    private boolean judgeNumber(int number) {
+        return number >= 4;
+    }
+
     private void validateNameLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름 길이의 범위를 벗어났습니다.");
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
