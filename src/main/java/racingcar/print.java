e4f466736da9e571e1296cf83beb6915e2dffea6
idingg <idingg@gmail.com>
2023-10-26 18:59:42 +0900

    feat: 현재 자동차 상태 출력
    현재 자동차 상태 출력 기능 추가

--- src/main/java/racingcar/print.java
+++ src/main/java/racingcar/print.java
@@ -1,11 +1,16 @@
 package racingcar;
 
-public class print {
-	public void start() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-	}
+public class Print {
+    public static void start() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
 
-	public void repetitions() {
-		System.out.println("시도할 회수는 몇회인가요?");
-	}
+    public static void repetitions() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void carStatus(Car car) {
+        System.out.print(car.getName() + " : ");
+        System.out.println("-".repeat(car.getPosition()));
+    }
 }
