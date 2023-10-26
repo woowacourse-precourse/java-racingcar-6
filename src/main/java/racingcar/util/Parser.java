c66c1f8778a3e45be1004b290c1af5f29ca7423d
haruday97 <starharu97@naver.com>
2023-10-27 05:06:44 +0900

    refactor: parseStringToCars 리팩토링
    메서드명 및 로직 변경

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -1,10 +1,20 @@
 package racingcar.util;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
+import racingcar.domain.CarName;
+import racingcar.domain.CarPosition;
+
 
 public class Parser {
-    public static Car parseStringToCar(String input) {
-        return Car.nameOf(input);
+    public static List<Car> parseStringToCars(String input) {
+        List<String> carNames = Separator.separateByDelimeter(input);
+        return carNames.stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
+                .map(name -> new Car(new CarName(name), new CarPosition(0)))
+                .collect(Collectors.toList());
     }
 
 }
