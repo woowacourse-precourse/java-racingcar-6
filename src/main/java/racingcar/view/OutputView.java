a1502d3f67631b4b2fcf79dfad26b74a38174a45
leeedohyun <ldh19738@gmail.com>
2023-10-26 16:07:51 +0900

    feat: 자동차 입력 메시지 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static void printInputCarsNameMessage() {
+        System.out.println(INPUT_CARS_NAME_MESSAGE);
+    }
+}
