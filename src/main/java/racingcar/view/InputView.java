a046b54cef8c4f5659a4e15d818b4edd12e5bd3f
kylim99 <zxzx05050@naver.com>
2023-10-26 16:23:35 +0900

    feat: 이름들을 입력받아 list로 리턴하는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,16 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String CARS_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String SEPARATOR = ",";
+    public List<String> readCarNames(){
+        System.out.println(CARS_NAME_INPUT_MESSAGE);
+        String input  = Console.readLine();
+        return Arrays.asList(input.split(SEPARATOR));
+    }
 }
