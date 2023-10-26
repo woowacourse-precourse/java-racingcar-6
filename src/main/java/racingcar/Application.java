11e3a03933d3b0242466980abb75993d24474f8d
kylim99 <zxzx05050@naver.com>
2023-10-26 16:16:30 +0900

    feat: main 함수에서 view 객체 생성하여 controller에 생성자로 전달

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,15 @@
 package racingcar;
 
 import racingcar.controller.RacingCarGameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarGameController racingCarGameController = new RacingCarGameController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingCarGameController racingCarGameController = new RacingCarGameController(inputView,outputView);
         racingCarGameController.run();
     }
 }
--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,6 +1,17 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingCarGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingCarGameController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
     public void run(){
 
