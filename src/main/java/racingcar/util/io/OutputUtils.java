55ff2c9eb4b84aa16d1302c13d3d81dec2e87a0e
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 20:45:47 +0900

    [feat] 차 이동 출력 기능 구현

--- src/main/java/racingcar/util/io/OutputUtils.java
+++ src/main/java/racingcar/util/io/OutputUtils.java
@@ -4,6 +4,8 @@ public class OutputUtils {
     private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MOVE_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String CAR_MOVE = "%s : %s";
+    private static final String MOVE = "-";
 
     public static void printCarInputMessage() {
         System.out.println(CAR_INPUT_MESSAGE);
@@ -16,5 +18,9 @@ public class OutputUtils {
     public static void printExecutionResultMessage() {
         System.out.println();
         System.out.println(EXECUTION_RESULT_MESSAGE);
+    }
+
+    public static void printCarMove(String name, int move) {
+        System.out.println(String.format(CAR_MOVE, name, move));
     }
 }
