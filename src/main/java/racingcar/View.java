1dac2175288822e7f59106df107cdb4abf96cee0
HwangSeokJin <ghkd3703@gmail.com>
2023-10-26 20:21:13 +0900

    feat: 입력된 자동차 이름이 5자 이하인지 확인한다

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -10,6 +10,15 @@ public class View {
     public String[] inputCarNames() {
         String read = Console.readLine();
         String[] names = read.split(DELIMITER);
+        validateNameLength(names);
         return names;
+    }
+
+    private void validateNameLength(String[] names) {
+        for(String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("5자 이하만 입력할 수 있습니다.");
+            }
+        }
     }
 }
