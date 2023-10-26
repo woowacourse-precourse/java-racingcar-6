5f819d791c55c54709f9a00d9b6915f6b42dedad
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 00:22:41 +0900

    feat(RacingCarInputView): 시도할 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/RacingCarInputView.java
+++ src/main/java/racingcar/RacingCarInputView.java
@@ -5,14 +5,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RacingCarInputView {
-
-    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String DELIMITER = ",";
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_ATTEMPT_COUNTS_MESSAGE = "시도할 횟수는 몇회인가요?";
 
     public List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        String names = Console.readLine();
+        String names = readLineAndClose();
         return Arrays.stream(names.split(DELIMITER))
                 .toList();
+    }
+
+    public String inputAttemptCounts() {
+        System.out.println(INPUT_ATTEMPT_COUNTS_MESSAGE);
+        return readLineAndClose();
+    }
+
+    private String readLineAndClose() {
+        String names = Console.readLine();
+        Console.close();
+        return names;
     }
 }
