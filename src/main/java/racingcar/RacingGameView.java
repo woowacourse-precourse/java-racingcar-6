dd1868f5d06c8fe631541b6c6e5ac8693a3e16e4
정용우 <jyw1341@gmail.com>
2023-10-26 17:15:25 +0900

    feat(RacingGameView): 자동차 이름 입력 및 검증 기능 구현

--- src/main/java/racingcar/RacingGameView.java
+++ src/main/java/racingcar/RacingGameView.java
@@ -5,13 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class RacingGameView {
 
     private static final String NAME_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_INPUT_GUIDE = "시도할 회수는 몇회인가요?";
     private static final String ERROR_NAME_LENGTH = "이름은 5글자 이하만 가능합니다.";
     private static final int MAX_NAME_LENGTH = 5;
 
-    public void inputCarNames() {
+    public String inputCarNames() {
         System.out.println(NAME_INPUT_GUIDE);
         String carNames = Console.readLine();
         validateCarNames(carNames);
+        return carNames;
     }
 
     public void validateCarNames(String input) {
@@ -23,4 +25,5 @@ public class RacingGameView {
             }
         }
     }
+
 }
--- src/test/java/racingcar/RacingGameViewTest.java
+++ src/test/java/racingcar/RacingGameViewTest.java
@@ -12,7 +12,7 @@ class RacingGameViewTest {
     @ParameterizedTest
     @ValueSource(strings = {"a,ab,abc,abcd,abcde"})
     @DisplayName("입력받은 자동차의 이름이 정상적인 값만 들어오는 경우를 테스트.")
-    void testCarNamesLength(String value) {
+    void testValidateCarNames(String value) {
         //given
         RacingGameView racingGameView = new RacingGameView();
         //then
@@ -22,7 +22,7 @@ class RacingGameViewTest {
     @ParameterizedTest
     @ValueSource(strings = {"a,ab,abc,abcd,abcde,abcdef"})
     @DisplayName("입력받은 자동차의 이름 중 5글자를 초과하는 이름이 있으면 에러를 발생시킨다.")
-    void testCarNamesLengthOver(String value) {
+    void testValidateCarNamesOver(String value) {
         //given
         RacingGameView racingGameView = new RacingGameView();
         //then
