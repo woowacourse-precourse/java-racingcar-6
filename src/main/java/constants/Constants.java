8c5cf4f3276769d7634c070bc2b57dcd6d6fc3fb
euics <pjh575157@gmail.com>
2023-10-26 20:07:16 +0900

    feat(Util): constant 변수 관리 클래스 구현

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Constants {
+    // InputRaceView Constants
+    public static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String RACE_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
+}
