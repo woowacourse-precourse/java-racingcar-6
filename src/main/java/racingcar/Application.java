12b4f9aa62e3940b9576ceebdfee9a1d35ba5bb6
이소정 <grace@isojeong-ui-iMac.local>
2023-10-26 20:58:03 +0900

    전진or정지 여부를 판단하여 상태 업데이트 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -21,19 +23,42 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         int times = Integer.parseInt(Console.readLine());
 
-        printProcess(carNames, steps);
-        addSteps(steps);
-        printProcess(carNames, steps);
+        for(int i = 0; i < times; i++) {
+            List<Integer> randomNumbers = createRandomNumbers(carNames.size());
+            List<Integer> goingSteps = judge(randomNumbers);
+            addSteps(goingSteps, steps);
+            printProcess(carNames, steps);
+        }
 
+        String winners = String.join(",", pickWinner(carNames, steps));
+        System.out.println(winners);
     }
 
     //랜덤 숫자 생성
-    public static void addSteps(List<Integer> steps){
-        for(int i = 0; i < steps.size(); i++) {
-            int randomNumbers = Randoms.pickNumberInRange(0, 9);
-            System.out.println(randomNumbers);
-            steps.set(i, steps.get(i) + randomNumbers);
+    public static List<Integer> createRandomNumbers(int size){
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        for(int i = 0; i < size; i++) {
+            randomNumbers.add(Randoms.pickNumberInRange(0, 9));
         }
+        System.out.println(randomNumbers);
+        return randomNumbers;
+    }
+
+    //상태 update
+    public static void addSteps(List<Integer> goingSteps, List<Integer> steps){
+        for(int i = 0; i < steps.size(); i++) {
+            steps.set(i, steps.get(i) + goingSteps.get(i));
+        }
+    }
+
+    //전진 or 정지 판단
+    public static List<Integer> judge(List<Integer> randomNumbers){
+        for(int i = 0; i < randomNumbers.size(); i++){
+            if(randomNumbers.get(i) < 4)
+                randomNumbers.set(i, 0);
+        }
+        return randomNumbers;
     }
 
     //실행 결과 출력
@@ -48,4 +73,16 @@ public class Application {
         }
         System.out.println(str);
     }
+
+    public static List<String> pickWinner(List<String> carNames, List<Integer> steps){
+        List<String> winners = new ArrayList<>();
+
+        for(int i = 0; i < carNames.size(); i++){
+            if(steps.get(i).equals(Collections.max(steps)))
+                winners.add(carNames.get(i));
+        }
+
+        return winners;
+    }
+
 }
