1da3a02090c14b24526d6e54c9cc06f3f538f8f8
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 21:13:40 +0900

    feat(Convertor) : String -> int 변환 기능 추가

---
+++ src/main/java/racingcar/util/Convertor.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+public class Convertor {
+
+    private Convertor() {
+    }
+
+    public static int convertStringToInt(String str) {
+        return Integer.parseInt(str);
+    }
+}
