6d67be6c41fea19a4899c86fc3c6b1b9e0616e76
kimwanyoung <dhks2869@gmail.com>
2023-10-26 22:15:37 +0900

    feat : InputView 클래스 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,26 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String COMMA = ",";
+    private static final String NAMES_FROM_USER_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPTS_FROM_USER_INPUT = "시도할 회수는 몇회인가요?";
+
+    private InputView() {
+    }
+
+    public static String[] getCarsNameFromUserInput() {
+        System.out.println(NAMES_FROM_USER_INPUT);
+        String input = Console.readLine();
+        return input.split(COMMA);
+    }
+
+    public static int getAttemptsFromUserInput() {
+        System.out.println(ATTEMPTS_FROM_USER_INPUT);
+        String input = Console.readLine();
+        //TODO : validateNumericInput()
+        return Integer.parseInt(input);
+    }
+}
