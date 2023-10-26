58c0d31b62737831bbdcf8842c8aa11e05ed5efe
DooDuZ <sin9158@naver.com>
2023-10-27 04:20:24 +0900

    feat: Message 클래스 champion 메서드 추가
    우승자 문구 생성 기능

--- src/main/java/racingcar/game/Message.java
+++ src/main/java/racingcar/game/Message.java
@@ -1,11 +1,14 @@
 package racingcar.game;
 
+import java.util.List;
+
 public class Message {
-    private final String requestPlayerName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String requestPlayCount = "시도할 회수는 몇회인가요?";
+    public final String requestPlayerName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final String requestPlayCount = "시도할 회수는 몇회인가요?";
+    public final String result = "실행 결과";
     private final String champion = "최종 우승자 : ";
-    private final String result = "실행 결과";
     private final String spaceColon = " : ";
+    private final String comma = ", ";
     private final char distanceUnit = '-';
 
     public String distanceOfPlayer( Player player ){
@@ -15,6 +18,20 @@ public class Message {
 
         for(int i = 0 ; i < player.getDistance(); i++){
             message.append(distanceUnit);
+        }
+
+        return message.toString();
+    }
+
+    public String champions(List<Player> champions){
+        StringBuilder message = new StringBuilder();
+        message.append(champion);
+
+        for(int i = 0 ; i < champions.size(); i++){
+            message.append(champions.get(i).getName());
+            if(i<champions.size()-1){
+                message.append(comma);
+            }
         }
 
         return message.toString();
--- src/test/java/racingcar/game/MessageTest.java
+++ src/test/java/racingcar/game/MessageTest.java
@@ -1,5 +1,7 @@
 package racingcar.game;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,13 @@ public class MessageTest {
         }
 
         Assertions.assertThat(new Message().distanceOfPlayer(player)).isEqualTo("worm : ----");
+    }
+
+    @Test
+    void championMessageTest(){
+        List<Player> champions = new ArrayList<>();
+        champions.add(new Player("worm"));
+        champions.add(new Player("bird"));
+        Assertions.assertThat(new Message().champions(champions)).isEqualTo("최종 우승자 : worm, bird");
     }
 }
