c2654aed5187226cb9093bce93cb96c1117e774d
minjun7410 <minjun7410@gmail.com>
2023-10-27 00:23:38 +0900

    refactor(CarName, Car): 자동차 이름 관련 예외처리 기능 리팩토링
    * 원시변수였던 Car의 carName을 포장을 클래스로 포장한 CarName 생성
    * validateCarNameLengthExceed 와 validateCarNameLengthZero 메서드를 Car -> CarName 으로 옮겼다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,25 +1,11 @@
 package racingcar.domain;
 
 public class Car {
-    private String carName;
+    private CarName carName;
     private int movingDistance;
 
     public Car(String carName) {
-        validateCarNameLengthExceed(carName);
-        validateCarNameLengthZero(carName);
-        this.carName = carName;
+        this.carName = new CarName(carName);
         this.movingDistance = 0;
-    }
-
-    private void validateCarNameLengthExceed(String carName) {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차의 이름은 5자를 넘어갈 수 없습니다.");
-        }
-    }
-
-    private void validateCarNameLengthZero(String carName) {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
-        }
     }
 }
---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+public class CarName {
+    String carName;
+    public CarName(String carName) {
+        validateCarNameLengthExceed(carName);
+        validateCarNameLengthZero(carName);
+        this.carName = carName;
+    }
+
+    private void validateCarNameLengthExceed(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 넘어갈 수 없습니다.");
+        }
+    }
+
+    private void validateCarNameLengthZero(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+        }
+    }
+}
