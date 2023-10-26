0674f3225fb9b1185eb11729c4be1497d8f4f065
이진모 <jinmlee210@naver.com>
2023-10-26 23:28:56 +0900

    feat: n대의 자동차를 입력받을 inputCar() 메서드 기능구현

---
+++ src/main/java/racingcar/util/User.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class User {
+    public List<String> inputCar() {
+        String input = Console.readLine();
+        List<String> carName = Arrays.asList(input.split(","));
+        return carName;
+    }
+}
