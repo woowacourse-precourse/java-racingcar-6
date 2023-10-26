aaedf37f7d230f8b7ee73e724425ed243f993e8d
JuSeong1130 <jsmoon1130@naver.com>
2023-10-27 07:26:21 +0900

    feat(RacingGame) : 우승자 구하는 기능 구현
    1. getter를 쓰지않고 Car에게 물어봐 최대 포지션의 차 찾기
    2. 최대포지션의 차와 같은 차들 찾기

--- src/main/java/racingcar/domain/game/RacingGame.java
+++ src/main/java/racingcar/domain/game/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar.domain.game;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.car.Car;
 import racingcar.domain.generator.RandomNumberGenerator;
 
@@ -12,7 +13,7 @@ public class RacingGame {
         this.randomNumberGenerator = randomNumberGenerator;
     }
 
-    public void progress(List<Car> cars,int tryMaxCount) {
+    public void progress(List<Car> cars, int tryMaxCount) {
         int tryCount = 0;
         while (tryCount < tryMaxCount) {
             move(cars);
@@ -21,9 +22,9 @@ public class RacingGame {
     }
 
     private void move(List<Car> cars) {
-        for(Car car : cars) {
+        for (Car car : cars) {
             int randomNumber = randomNumberGenerator.createInRange(0, 9);
-            if(isPossibleMove(randomNumber)) {
+            if (isPossibleMove(randomNumber)) {
                 car.move();
             }
         }
@@ -31,6 +32,20 @@ public class RacingGame {
 
     private boolean isPossibleMove(int randomNumber) {
         return randomNumber >= 4;
+    }
+
+    public List<Car> getWinner(List<Car> cars) {
+        Car maxPosition = findMaxPositionCar(cars);
+        return  cars.stream()
+                .filter(car -> car.isSamePosition(maxPosition))
+                .collect(Collectors.toList());
+    }
+
+    private Car findMaxPositionCar(List<Car> cars) {
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("차가 없습니다."));
+
     }
 
 }
