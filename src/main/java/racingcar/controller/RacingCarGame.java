be8c22afc7230e82ce889da4fafe9b63e729af98
PARKHWANYEONG <welcome0828@naver.com>
2023-10-26 21:34:33 +0900

    feat: 경주 할 자동차 이름을 , 기준으로 입력받기

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,8 +1,18 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
 public class RacingCarGame {
 
     public void run() {
+        gameStart();
+    }
 
+    private void gameStart() {
+        String[] carNameArr = splitCarNames(InputView.carNames());
+    }
+
+    private String[] splitCarNames(String carNames) {
+        return carNames.split(",");
     }
 }
