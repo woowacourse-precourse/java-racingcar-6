226cef15dfca8bbb67f08492cd246302f6aea0f0
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-26 22:20:19 +0900

    feat(exception) : 시도 횟수 숫자가 아닐 경우 예외 발생 구현 - 정규표현식 \d+로 숫자인지 검사

--- src/main/java/racingcar/exception/TryCountInputErrorException.java
+++ src/main/java/racingcar/exception/TryCountInputErrorException.java
@@ -4,14 +4,22 @@ import java.util.List;
 
 public class TryCountInputErrorException implements InputErrorException{
     private final static String TRY_INPUT_IS_EMPTY_MESSAGE = "값을 입력해주세요. 게임을 종료합니다.";
+    private final static String TRY_INPUT_IS_NOT_NUMBER = "숫자만 입력이 가능합니다. 게임을 종료합니다.";
+    private final static String NUMBER_REGULAR_EXPRESSION = "\\d+";
     @Override
     public void checkUserInputValidate(String checkString) {
         if (isEmptyString(checkString)){
             throw new IllegalArgumentException(TRY_INPUT_IS_EMPTY_MESSAGE);
+        }else if (isNumeric(checkString)){
+            throw new IllegalArgumentException(TRY_INPUT_IS_NOT_NUMBER);
         }
     }
 
     private static boolean isEmptyString(String checkString){
         return checkString.isEmpty();
+    }
+
+    private static boolean isNumeric(String checkString){
+        return !checkString.matches(NUMBER_REGULAR_EXPRESSION);
     }
 }
