72f6fee7e13e5fa073ff04b17b9e01f35e598d71
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-26 22:11:31 +0900

    test(exception) : 시도 횟수 입력 빈 값 예외 발생 테스트

---
+++ src/test/java/racingcar/exception/TryCountInputErrorExceptionTest.java
@@ -0,0 +1,18 @@
+package racingcar.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TryCountInputErrorExceptionTest {
+    InputErrorException inputErrorException = new TryCountInputErrorException();
+    private final static String TRY_INPUT_IS_EMPTY_MESSAGE = "값을 입력해주세요. 게임을 종료합니다.";
+    @Test
+    void 자동차_경주_시도_횟수_빈_값_입력_시_예외_발생(){
+        //given
+        String checkInput = "";
+
+        //when,then
+        Assertions.assertThatThrownBy(() -> inputErrorException.checkUserInputValidate(checkInput))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(TRY_INPUT_IS_EMPTY_MESSAGE);
+    }
+}
