3a5a1b91c326338dde51ababe39920a97ec703f5
Hojoomoney <kimhojoo51@gmail.com>
2023-10-26 17:19:43 +0900

    feat : Application.java ( 자동차 이름을 쉼표(,) 기준으로 구분하고 5자 이하만 가능하게 하는 메소드 추가) docs : README.md (예외 처리 기능 작성)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,36 @@
 package racingcar;
 
 
+import java.util.*;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	
-    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-    	String cars = Console.readLine();
-    	
-    	
-    	
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String cars = Console.readLine();
+		List<String> carsList = splitComma(cars);
+		checkLength(carsList);
+		System.out.println(carsList);
+	}
+
+	private static List<String> splitComma(String cars) {
+
+		String[] carsArr = cars.split(",");
+		List<String> carsList = Arrays.asList(carsArr);
+
+		return carsList;
+	}
+
+	private static void checkLength(List<String> carsList) {
+
+		for (String cars : carsList) {
+			if (cars.length() > 5) {
+				throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+			}
+		}
+
+	}
 }
