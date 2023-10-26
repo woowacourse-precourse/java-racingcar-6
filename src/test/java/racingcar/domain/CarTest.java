70be3129a41e69e48e71f5d34babdba669e8e838
Taaate <an981022@naver.com>
2023-10-26 16:24:23 +0900

    test: 자동차 관련 테스트 작성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,54 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarTest {
+
+    @Test
+    void 자동차_정상_생성() {
+        //given
+        String validName = "pobi";
+
+        //when
+        Car car = new Car(validName);
+
+        //then
+        assertThat(car.getName()).isEqualTo(validName);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 공백_이름_예외() {
+        //given
+        String blankName = " ";
+
+        //when & then
+        assertThatThrownBy(() -> new Car(blankName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백은 입력 불가능 합니다.");
+    }
+
+    @Test
+    void 이름_길이_예외() {
+        //given
+        String longName = "abcdef";
+
+        //when & then
+        assertThatThrownBy(() -> new Car(longName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자 이하의 이름만 가능합니다.");
+    }
+
+    @Test
+    void 숫자_이름_예외() {
+        //given
+        String numberName = "123";
+
+        //when & then
+        assertThatThrownBy(() -> new Car(numberName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자는 입력 불가능 합니다.");
+    }
+}
