ff76229d9bc236b4a70d8147bf20252876a55139
hoyun06 <hoyun06@gmail.com>
2023-10-26 17:46:11 +0900

    feat: Position 클래스 생성

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+public class Position {
+
+    private int position;
+
+    public Position(int position) {
+        validatePosition(position);
+        this.position = position;
+    }
+
+    private void validatePosition(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("자동차 위치는 음수일 수 없습니다.");
+        }
+    }
+
+    public void increment() {
+        position++;
+    }
+}
