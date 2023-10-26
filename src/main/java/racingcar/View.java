9f3b3daa76317f582150f6ffb7df0c5a49ce3142
shin5774 <babo5774@naver.com>
2023-10-26 23:01:09 +0900

    feat: 콘솔로 사용자에게 이름을 입력받는 기능 구현
    - 미션에서 제공된 라이브러리를 이용해 콘솔에서 정상적으로 입력값이 들어오는것을 확인.

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class View {
+    public View(){
+
+    }
+
+    public String inputNames(){
+        return readLine();
+    }
+}
