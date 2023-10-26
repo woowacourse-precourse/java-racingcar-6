f2bd6155f5d1352a46f5db1acec46bb0b10f6c51
HwangSeokJin <ghkd3703@gmail.com>
2023-10-26 20:54:47 +0900

    feat: 사용자가 입력한 회수만큼 레이싱을 진행한다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private String name;
+
+    Car(String name) {
+        this.name = name;
+    }
+
     public boolean move() {
         int random = Randoms.pickNumberInRange(0, 9);
         if (random >= 4) {
---
+++ src/main/java/racingcar/RacingController.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+    View view;
+    List<Car> cars;
+
+    RacingController() {
+        view = new View();
+        cars = new ArrayList<>();
+    }
+
+    public void play() {
+        String[] carNames = view.inputCarNames();
+        int tryCount = view.inputTryCount();
+        race(tryCount);
+    }
+
+    private void race(int count) {
+        for (int i = 0; i < count; i++) {
+            moveCars();
+        }
+    }
+
+    private void setupCars(String[] carNames) {
+        for (String name : carNames) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+    }
+
+    private void moveCars() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+}
