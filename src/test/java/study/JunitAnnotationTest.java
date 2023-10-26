1fa7103c5d4abb607e1926d6df81df32c6ff567f
1000zoo <cjswldn99@gmail.com>
2023-10-27 01:18:34 +0900

    ValueSource 어노테이션
    - ints, strings와 같은 파라미터에 배열을 집어넣어 사용한다

--- src/test/java/study/JunitAnnotationTest.java
+++ src/test/java/study/JunitAnnotationTest.java
@@ -2,6 +2,7 @@ package study;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class JunitAnnotationTest {
 
@@ -10,5 +11,11 @@ public class JunitAnnotationTest {
     void csvSourceTest(String lower, String upper) {
         System.out.println("lower = " + lower);
         System.out.println("upper = " + upper);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,woni,jun", "jiwoo"})
+    void valueSourceTest(String names) {
+        System.out.println("names = " + names);
     }
 }
