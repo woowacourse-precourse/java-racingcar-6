90fed1f3dae35b2e12cf96da8df45f15a4b2f540
Isaac Han <isaachan233@gmail.com>
2023-10-27 08:04:06 +0900

    feat: 시도할 회수를 설정하는 메서드 추가

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -3,19 +3,28 @@ package racingcar.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import racingcar.model.Attemps;
 import racingcar.model.Cars;
 import racingcar.model.Winners;
 
 public class RacingGameService {
     private final Cars cars;
+    private final Attemps attemps;
     private final Winners winners;
     
     public RacingGameService() {
         this.cars = new Cars();
+        this.attemps = new Attemps();
         this.winners = new Winners();
     }
     
     public void addCars (String carNames) {
+        // TODO: 유효성 검사
         cars.addCars(new ArrayList<String>(Arrays.asList(carNames.split(","))));
+    }
+    
+    public void setAttemps (String numberOfAttemps) {
+        // TODO: 유효성 검사
+        attemps.setNumber(Integer.valueOf(numberOfAttemps));
     }
 }
