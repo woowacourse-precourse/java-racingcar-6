eb2b79f3e1fc1350ee028c20675ddef9fd8675ef
Arachneee <jhg2819@naver.com>
2023-10-26 19:57:50 +0900

    test: Car 이름 5글자 초과 테스트
    IllegalArgumentException 던지는지 확인

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     @Test
-    void 초기_위치_테스트() {
+    void 초기_위치_0() {
         // given
         String name = "1234";
 
@@ -19,6 +20,15 @@ class CarTest {
         // then
         assertThat(car.getName()).isEqualTo(name);
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 이름길이_5초과_예외() {
+
+        String name = "123456";
+
+        assertThatThrownBy(() -> Car.from(name))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
