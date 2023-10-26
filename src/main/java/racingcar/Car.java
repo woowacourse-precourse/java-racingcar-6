ac275467117cc56a3d77beec9e2d92209ee1eba2
parkchangeon <jg9870@naver.com>
2023-10-26 21:35:35 +0900

    skeleton(racingcar): 기능 목록 구성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public void moveForward() {
+        position++;
+    }
+}
---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Racing {
+
+    public String determineWinners() {
+        return "parkchangeon";
+    }
+}
