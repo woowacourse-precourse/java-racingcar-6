f72826b13b403a7f1f920fbac442897e680abb6b
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-26 22:53:51 +0900

    refactor: 파라미터명 및 로컬변수명 변경

--- src/main/java/racingcar/service/Exception.java
+++ src/main/java/racingcar/service/Exception.java
@@ -8,46 +8,46 @@ import java.util.Set;
 public class Exception {
 
     private static List<String> carNames;
-    public static void checkCarName(String input) throws IllegalArgumentException {
-        checkEmpty(input);
+    public static void checkCarName(String inputVaule) throws IllegalArgumentException {
+        checkEmpty(inputVaule);
 
-        carNames = Arrays.asList(input.split(","));
-        for (String e : carNames) {
-            checkNoName(e);
-            checkblank(e);
-            checkLength(e);
+        carNames = Arrays.asList(inputVaule.split(","));
+        for (String name : carNames) {
+            checkNoName(name);
+            checkblank(name);
+            checkLength(name);
         }
 
         checkSameName();
     }
 
-    private static void checkEmpty(String input) {
-        if (input.isEmpty()) {
+    private static void checkEmpty(String inputVaule) {
+        if (inputVaule.isEmpty()) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름을 입력해주세요.");
         }
     }
 
-    private static void checkNoName(String input) {
-        if (input.isEmpty() || input.equals(" ")) {
+    private static void checkNoName(String inputVaule) {
+        if (inputVaule.isEmpty() || inputVaule.equals(" ")) {
             throw new IllegalArgumentException("잘못된 값 입력 : 입력되지 않은 자동차 이름이 있습니다.");
         }
     }
 
-    private static void checkblank(String input) {
-        if (input.contains(" ")) {
+    private static void checkblank(String inputVaule) {
+        if (inputVaule.contains(" ")) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름에는 공백이 들어갈 수 없습니다.");
         }
     }
 
-    private static void checkLength(String input) {
-        if (input.length() > 5) {
+    private static void checkLength(String inputVaule) {
+        if (inputVaule.length() > 5) {
             throw new IllegalArgumentException("잘못된 값 입력 : 자동차 이름은 5자 이내로 입력해주세요");
         }
     }
 
     private static void checkSameName() {
-        Set<String> set = new HashSet<>(carNames);
-        if (carNames.size() != set.size()) {
+        Set<String> setTypeCarNames = new HashSet<>(carNames);
+        if (carNames.size() != setTypeCarNames.size()) {
             throw new IllegalArgumentException("잘못된 값 입력 : 중복된 자동차 이름이 있습니다.");
         }
     }
