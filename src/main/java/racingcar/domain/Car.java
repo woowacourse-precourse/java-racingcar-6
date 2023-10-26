c552e2c3a85eb213713132f3af4d616f41341f7e
SeoJiWon <sjiwon4491@gmail.com>
2023-10-26 18:03:29 +0900

    feat: 자동차 이름이 null이거나 비어있는지 검증 로직 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static racingcar.exception.ExceptionMessage.CarException.INVALID_NAME_LENGTH;
+import static racingcar.exception.ExceptionMessage.CarException.NAME_MUST_BE_EXISTS;
 
 public class Car {
     private static final int NAME_LENGTH = 5;
@@ -12,11 +13,15 @@ public class Car {
     }
 
     public static Car from(final String name) {
-        validateNameLength(name);
+        validateNameIsUsable(name);
         return new Car(name);
     }
 
-    private static void validateNameLength(final String name) {
+    private static void validateNameIsUsable(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(NAME_MUST_BE_EXISTS.message);
+        }
+
         if (name.length() > NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH.message);
         }
--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -2,6 +2,7 @@ package racingcar.exception;
 
 public interface ExceptionMessage {
     enum CarException {
+        NAME_MUST_BE_EXISTS("이름이 존재하지 않습니다."),
         INVALID_NAME_LENGTH("자동차는 5자 이하의 이름을 가질 수 있습니다."),
         ;
 
