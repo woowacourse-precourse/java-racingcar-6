e8fe4e1ea35c388a98cd7551d3c810a7c0ff8c93
Firedrago95 <yonghwa95@naver.com>
2023-10-27 02:48:30 +0900

    refactor : generateRandomNumber 정적메서드로 변경

--- src/main/java/racingcar/Util/RandomGenerator.java
+++ src/main/java/racingcar/Util/RandomGenerator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator {
 
-    public int generateRandomNumber() {
+    public static int generateRandomNumber() {
         return Randoms.pickNumberInRange(0,9);
     }
 
