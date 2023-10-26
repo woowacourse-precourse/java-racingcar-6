6f50ea65a8294c7742e01922da8c8427e4297e0c
hyeonjilee <skylar12200@gmail.com>
2023-10-26 21:15:09 +0900

    feat : 플레이어 이름 형식 검증 추가

--- src/main/java/racingcar/PlayerName.java
+++ src/main/java/racingcar/PlayerName.java
@@ -1,5 +1,11 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PlayerName {
 
     public void validateLength(String rawName) {
@@ -8,7 +14,25 @@ public class PlayerName {
         }
     }
 
+    //@ 여기 네이밍 고민
     private boolean isLengthOverFive(String rawName) {
         return rawName.length() > 5;
+    }
+
+    public void validateForm(String rawName) {
+        if (!isNamesForm(rawName)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNamesForm(String rawNames) {
+        String pattern = "^[^,]+(,[^,]+)+$";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(rawNames);
+        return matcher.matches();
+    }
+
+    public List<String> convertNames(String rawNames) {
+        return Arrays.asList(rawNames.split(","));
     }
 }
--- src/test/java/racingcar/PlayerNameTest.java
+++ src/test/java/racingcar/PlayerNameTest.java
@@ -5,8 +5,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerNameTest {
+
+    @ParameterizedTest
+    @EmptySource
+    void 플레이어_이름이_비어있으면_예외를_발생한다(String empty) {
+        PlayerName playerName = new PlayerName();
+        assertThatThrownBy(() -> playerName.validateForm(empty)).
+            isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     void 플레이어_이름이_다섯자를_초과하면_예외를_발생한다() {
@@ -18,5 +29,25 @@ public class PlayerNameTest {
     void 플레이어_이름이_다섯자를_이하면_예외를_발생시키지_않는다() {
         PlayerName playerName = new PlayerName();
         assertDoesNotThrow(() -> playerName.validateLength("fives"));
+    }
+
+    @Test
+    void 콤마로_구분된_플레이어_이름이_들어오면_리스트로_반환한다() {
+        PlayerName playerName = new PlayerName();
+        assertThat(playerName.convertNames("rose,lisa")).containsExactly("rose", "lisa");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"roseLisa", "rose,,lisa", ",rose,lisa", "rose,lisa,", "rose,"})
+    void 플레이어_이름이_콤마로_구분되지_않으면_예외를_발생한다(String names) {
+        PlayerName playerName = new PlayerName();
+        assertThatThrownBy(() -> playerName.validateForm(names)).
+            isInstanceOf(IllegalArgumentException.class);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"rose,lisa", "rose,lisa,jisu,jenny"})
+    void 플레이어_이름이_콤마로_구분되어_들어오면_예외를_발생시키지_않는다(String names) {
+        PlayerName playerName = new PlayerName();
+        assertDoesNotThrow(() -> playerName.validateForm(names));
     }
 }
