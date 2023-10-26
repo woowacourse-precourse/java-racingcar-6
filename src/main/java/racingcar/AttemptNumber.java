5409f0d2cf15cc96690858882c9ead7cad4b7917
1000zoo <cjswldn99@gmail.com>
2023-10-27 01:41:00 +0900

    fix(AttemptNumber): 불필요한 생성자 및 메소드 제거
    - 원래 예외처리를 여기서 하려했는데,
    - 입력받는 단계에서 예외처리

--- src/main/java/racingcar/AttemptNumber.java
+++ src/main/java/racingcar/AttemptNumber.java
@@ -1,12 +1,4 @@
 package racingcar;
 
 public record AttemptNumber(int number) {
-
-    public AttemptNumber {
-        throwIfInvalidAttemptNumber(number);
-    }
-
-    private void throwIfInvalidAttemptNumber(int number) {
-
-    }
 }
