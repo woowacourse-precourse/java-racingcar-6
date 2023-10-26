5379d6ad9ac3fe4c5d106d465da9005916287919
park_yeongyu <sjjs7771@naver.com>
2023-10-26 23:28:37 +0900

    feat : Race 클래스에 몇번 시도하는지 설정하는 함수 작성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,11 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Race{
+    int trial;
 
     RunningMessage message = new RunningMessage();
     CarSetting carSetting = new CarSetting();
     public void start(){
         message.printStartMessage();
         carSetting.setCarName();
+    }
+    public void setTrial(){
+        trial = Integer.parseInt(Console.readLine());
     }
 }
