878a9ea72a731e1b9da56b31287ecc8a89b04e31
mainfn <_@mainfn.dev>
2023-10-26 23:00:50 +0900

    test: MoveCommand가 적용된 Car에 맞춰 test case 변경

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -5,10 +5,44 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.game.MoveResult;
+import racingcar.domain.move.MoveCommand;
 
 final class CarTest {
+    @DisplayName("Car가 움직이지 않으면 coordinate는 0이다")
+    @Test
+    void carNotMovedTest() {
+        // given
+        final String carName1 = "a";
+        final Car car = new Car(carName1);
 
-    @DisplayName("moved를 N번 호출하면 coordinate는 N이다")
+        // when
+        final MoveResult moveResult = car.movedBy(MoveCommand.STAY);
+
+        // then
+        assertThat(moveResult.carName()).isEqualTo(carName1);
+        assertThat(moveResult.coordinate()).isEqualTo(0);
+
+    }
+
+    @DisplayName("MoveCommand.STAY 명령으로 N번 moved 해도 coordinate는 0이다")
+    @Test
+    void carStayTest() {
+        // given
+        final String carName1 = "a";
+        final Car car = new Car(carName1);
+        final int n = 100;
+
+        // when
+        // then
+        for (int i = 1; i <= n; i++) {
+            final MoveResult moveResult = car.movedBy(MoveCommand.STAY);
+            assertThat(moveResult.carName()).isEqualTo(carName1);
+            assertThat(moveResult.coordinate()).isEqualTo(0);
+        }
+
+    }
+
+    @DisplayName("MoveCommand.GO 명령과 함께 N번 moved하면 coordinate는 N이다")
     @Test
     void carMovedTest() {
         // given
@@ -19,7 +53,7 @@ final class CarTest {
         // when
         // then
         for (int i = 1; i <= n; i++) {
-            final MoveResult moveResult = car.moved();
+            final MoveResult moveResult = car.movedBy(MoveCommand.GO);
             assertThat(moveResult.carName()).isEqualTo(carName1);
             assertThat(moveResult.coordinate()).isEqualTo(i);
         }
