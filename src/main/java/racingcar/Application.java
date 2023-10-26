e0cf4fd34ddb907e5f7807ca8e1f40ac5f959a2f
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-10-26 21:34:34 +0900

    Feat: 추가 inputCarName 함수
    사용자로부터 참가 자동차 이름을 받는 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
+    public static List<String> inputCarName() {  // checkInput 구현시 수정 필요
+        System.out.println("참가자 이름을 입력해 주세요!");
+        String inputStr = Console.readLine();
+        inputStr = inputStr.replace(" ", "");
+        String[] inputArr = inputStr.split(",");
+        List<String> nameList = Arrays.asList(inputArr);
+        return nameList;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
