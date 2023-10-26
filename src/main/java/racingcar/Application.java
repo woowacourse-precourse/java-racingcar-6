cbc87de5c7700f7a6019b1b9bc4fd1cda2d54494
Taaate <an981022@naver.com>
2023-10-26 15:12:51 +0900

    feat: Controller 와 View 의 의존성 조립

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController(inputView(), outputView());
+        racingController.run();
+    }
+
+    private static InputView inputView() {
+        return new InputView();
+    }
+
+    private static OutputView outputView() {
+        return new OutputView();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
