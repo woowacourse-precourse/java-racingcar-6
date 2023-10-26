95770debd2408d95c72b05b4d0f41314aa8e68bf
mdgome <mdgome3@gmail.com>
2023-10-27 00:00:19 +0900

    Feat: 예외처리 기능 추가 (구분자 뒤 입력 값이 없을 경우)
    자동차 이름을 입력 받았을 때
    구분자(,) 뒤 입력받은 문자가 없는 경우 `IllegalArgumentException`을 발생

--- src/main/java/racingcar/util/Validate.java
+++ src/main/java/racingcar/util/Validate.java
@@ -2,13 +2,17 @@ package racingcar.util;
 
 import java.util.List;
 
-
 public class Validate {
     public static void CarNameNumeric(List<String> list) {
         for(String name: list) {
             if (name.chars().anyMatch(Character::isDigit)) {
                 throw new IllegalArgumentException("자동차 이름에 숫자는 넣을 수 없습니다.");
             }
+        }
+    }
+    public void LastCharIsComma(String name) {
+        if(name.substring(name.length() - 1) == ",") {
+            throw new IllegalArgumentException("잘못된 입력 : \",\" 뒤는 자동차 이름이 들어가야합니다.");
         }
     }
 }
