e41b5b2423c31ce1eace79580f11ffb4ff62cb48
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 20:47:58 +0900

    feat(InputValidator) : 쉼표로 구분된 자동차이름을 반복문을 통해 검증

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,5 +1,8 @@
 package racingcar.validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidator {
 
     private InputValidator() {
@@ -17,31 +20,44 @@ public class InputValidator {
         }
     }
 
-    public static void validateCarNameLength(String carName) {
+    public static void validateCarName(String input) {
+        String[] carNames = input.split(",");
+        Set<String> nameSet = new HashSet<>();
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+            validateCarNameIsBlank(carName);
+            validateIsStartBlank(carName);
+            validateIsEndBlank(carName);
+            nameSet.add(carName);
+        }
+        validateDuplication(nameSet.size(), carNames.length);
+    }
+
+    private static void validateCarNameLength(String carName) {
         if (carName.length() > 5 || carName.length() == 0) {
             throw new IllegalArgumentException("자동차 이름은 1 ~ 5글자로 입력해야 합니다.");
         }
     }
 
-    public static void validateCarNameIsBlank(String carName) {
+    private static void validateCarNameIsBlank(String carName) {
         if (carName.equals(" ") || carName.equals("")) {
             throw new IllegalArgumentException("자동차 이름을 공백으로 입력해선 안됩니다.");
         }
     }
 
-    public static void validateIsStartBlank(String carName) {
+    private static void validateIsStartBlank(String carName) {
         if (carName.matches("^\\s.*")) {
             throw new IllegalArgumentException("첫 글자를 공백으로 사용할 수 없습니다.");
         }
     }
 
-    public static void validateIsEndBlank(String carName) {
+    private static void validateIsEndBlank(String carName) {
         if (carName.matches(".*\\s$")) {
             throw new IllegalArgumentException("마지막 글자를 공백으로 사용할 수 없습니다.");
         }
     }
 
-    public static void validateDuplication(int nameSetSize, int carCount) {
+    private static void validateDuplication(int nameSetSize, int carCount) {
         if (nameSetSize != carCount) {
             throw new IllegalArgumentException("중복된 이름을 입력할 수 없습니다.");
         }
