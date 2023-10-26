6886fe8083d4ec89475233ab7b3b44442bf6877e
hackertaco <imsa1224@korea.ac.kr>
2023-10-26 16:44:41 +0900

    feat(1): 자동차 이름 리스트를 부여받고 5자가 초과하는지 Validate한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+    final static int CHARACTERS_LIMIT = 5;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        // 기능 1.
+        List<String> cars = Arrays.stream(Console.readLine().split(",")).toList();
+        boolean anyNameExceedsCharactersLimit = cars.stream().anyMatch(car->car.length() > CHARACTERS_LIMIT);
+        if(anyNameExceedsCharactersLimit) {
+            throw new IllegalArgumentException(String.format("이름은 %d자를 넘지 못합니다.", CHARACTERS_LIMIT));
+        }
+
+
+
     }
 }
