051bd1e2ccb5ef5b9bf1dd0314164bb3d6b6a973
IMWoo94 <leemeawoo94@gmail.com>
2023-10-26 16:44:41 +0900

    feat : 자동차 경주 게임 오프닝 출력 멘트 기능 구현
    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" 멘트 출력
    멘트는 상수로 관리

---
+++ src/main/java/racingcar/constants/RacingCarGameComments.java
@@ -0,0 +1,7 @@
+package racingcar.constants;
+
+public abstract class RacingCarGameComments {
+
+    public static final String OPENING_COMMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+}
---
+++ src/main/java/racingcar/views/OutputView.java
@@ -0,0 +1,14 @@
+package racingcar.views;
+
+import racingcar.constants.RacingCarGameComments;
+
+public class OutputView {
+
+    private OutputView() {
+
+    }
+
+    public static void racingCarOpeningComment() {
+        System.out.println(RacingCarGameComments.OPENING_COMMENT);
+    }
+}
