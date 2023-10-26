81d19ba36324d2172e8ba8082236f54b4ecccfd1
Isaac Han <isaachan233@gmail.com>
2023-10-27 07:58:53 +0900

    feat: 시도할 회수를 담당하는 클래스 생성

---
+++ src/main/java/racingcar/model/Attemps.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+public class Attemps {
+    private int number;
+    
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+}
