d6e3330504b242d1e89359ddda1a935e92fd679b
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-27 01:26:23 +0900

    feat: 랜덤 숫자를 뽑아서 차를 전진하는 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,18 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    private void go() {
+        if(Randoms.pickNumberInRange(0, 9) >= 4) {
+            distance++;
+        }
+    }
 }
