1febdc23b17dbb8eb2fac0bb1acdbe3cc53bdd35
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-26 20:18:33 +0900

    feat: 중복된 자동차명 검증 로직 개발

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Cars {
     private static final int MAX_SIZE = 5;
@@ -8,13 +10,20 @@ public class Cars {
 
     public Cars(List<String> carNames) {
         checkNameLength();
+        checkDuplicate();
         this.carNames = carNames;
     }
 
     private void checkNameLength() {
         carNames.stream().filter(carName -> carName.length() > MAX_SIZE).forEach(carName -> {
-            throw new IllegalArgumentException("자동차의 이름은 " + MAX_SIZE + "보다 작아야합니다.");
+            throw new IllegalArgumentException("자동차의 이름은 " + MAX_SIZE + "보다 작아야 합니다.");
         });
     }
 
+    private void checkDuplicate() {
+        Set<String> carNamesSet = new HashSet<>(carNames);
+        if (carNames.size() != carNamesSet.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 존재 합니다.");
+        }
+    }
 }
