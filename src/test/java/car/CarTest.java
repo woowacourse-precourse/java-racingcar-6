f84fc43d98dc800ef675779956751c4e5acc5b3f
song-jae-wan <sjo9810@gmail.com>
2023-10-26 21:29:54 +0900

    Test: 숫자 예외처리 테스트, 이름길이 예외처리 테스트
    숫자 예외처리, 이름길이 예외처리 단위 테스트

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -1,8 +1,13 @@
 package car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Cars;
+
+import exception.GameInputException;
+
+import message.GameMessage;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +23,23 @@ public class CarTest {
         String winner = cars.getWinners();
 
         assertThat(winner).isEqualTo("b");
+    }
+
+    @Test
+    void 숫자_정규식_확인() {
+        GameInputException gameException = new GameInputException();
+        String input = "123456789!";
+        assertThatThrownBy(() -> gameException.validateNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(GameMessage.countError.getMessage());
+    }
+
+    @Test
+    void 이름_길이_확인() {
+        GameInputException gameException = new GameInputException();
+        String[] names = {"a","a23","abc123"};
+        assertThatThrownBy(() -> gameException.validateNameLength(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(GameMessage.nameError.getMessage());
     }
 }
