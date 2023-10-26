d9e0e0124c05761a6c670664cdee3361ed28dc8f
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-26 20:21:12 +0900

    refactor: printResult, printWinner 형식에 맞게 변경

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,6 +1,7 @@
 package View;
 
 import java.util.List;
+import Model.RacingCar;
 
 public class OutputView {
     private static final String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -16,12 +17,14 @@ public class OutputView {
         System.out.println(TRIAL);
     }
 
-    public void printResult(/*List<RacingCar> RacingCars*/){
+    public void printResult(List<RacingCar> RacingCars){
         System.out.println(RESULT);
-        /* TODO */
+        for (RacingCar racingCar : RacingCars){
+            System.out.println(racingCar.getName() + " : " + racingCar.visibleDistance());
+        }
     }
 
     public void printWinner(List<String> Winners){
-        System.out.println(WINNER + Winners);
+        System.out.println(WINNER + String.join(", ", Winners));
     }
 }
