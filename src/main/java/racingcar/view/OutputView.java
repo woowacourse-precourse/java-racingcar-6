dbe4ad94da317c24bda4bb30ec896226cd5c4e4f
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-26 23:43:59 +0900

    feat(OutputView): 최종 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,13 @@ package racingcar.view;
 
 import java.util.List;
 import racingcar.dto.CarCondition;
+import racingcar.dto.RaceResult;
 
 public class OutputView {
     public static final String RACE_RESULT_MESSAGE = "실행 결과";
+    public static final String WINNERS = "최종 우승자 : ";
 
-    public static void sendResult() {
+    public static void sendRaceEnd() {
         System.out.println(RACE_RESULT_MESSAGE);
     }
 
@@ -16,5 +18,17 @@ public class OutputView {
         for (int carNumber = 0; carNumber < names.size(); carNumber++) {
             System.out.println(names.get(carNumber) + " : " + positions.get(carNumber));
         }
+    }
+
+    public static void sendWinners(RaceResult raceResult) {
+        System.out.println(WINNERS + createWinnersMessage(raceResult.winners()));
+    }
+
+    private static String createWinnersMessage(List<String> winners) {
+        StringBuilder builder = new StringBuilder();
+        for (String winner : winners) {
+            builder.append(winner);
+        }
+        return String.join(builder.toString(), ", ");
     }
 }
