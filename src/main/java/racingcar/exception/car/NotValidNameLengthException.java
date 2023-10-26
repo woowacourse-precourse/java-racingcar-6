34a2ac6a8f10affbf89b73421e71cd5b237d33fd
eom-tae-in <eti0728@daum.net>
2023-10-27 03:52:58 +0900

    feat: 자동차 이름이 5자가 넘으면 발생시킬 예외 클래스 생성

---
+++ src/main/java/racingcar/exception/car/NotValidNameLengthException.java
@@ -0,0 +1,10 @@
+package racingcar.exception.car;
+
+import static racingcar.util.Constant.MAX_CAR_NAME_LENGTH;
+
+public class NotValidNameLengthException extends IllegalArgumentException {
+    public NotValidNameLengthException(final String carName) {
+        super("자동차 이름의 길이는 " + MAX_CAR_NAME_LENGTH.getValue() + "자리 이하여야 합니다. 다시 입력해주세요. 작성하신 자동차 이름: "
+                + carName);
+    }
+}
