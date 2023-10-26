9b260aa3c358b0d3254c716f95c6c2b5800599b5
euics <pjh575157@gmail.com>
2023-10-26 16:33:17 +0900

    feat(View): printWinners 메서드 구현
    경주 게임의 최종 우승자를 출력.

--- src/main/java/racingcar/view/OutputRaceView.java
+++ src/main/java/racingcar/view/OutputRaceView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputRaceView {
     public static void printRaceResult(List<Car> cars) {
@@ -12,5 +13,9 @@ public class OutputRaceView {
             }
             System.out.println();
         });
+    }
+
+    public static void printWinners(List<Car> winners) {
+        System.out.println("최종 우승자 : " + winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
     }
 }
