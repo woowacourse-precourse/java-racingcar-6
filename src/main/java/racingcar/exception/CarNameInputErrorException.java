6a326df1420eb9200ff83b275f969248b9690568
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-26 20:49:08 +0900

    feat(exception) : 아무것도 입력 안했을 시 exception 발생

--- src/main/java/racingcar/exception/CarNameInputErrorException.java
+++ src/main/java/racingcar/exception/CarNameInputErrorException.java
@@ -7,11 +7,14 @@ public class CarNameInputErrorException implements InputErrorException{
     private final static int CAR_NAME_LEGTH_MAX_RANGE = 5;
     private final static String CAR_NAME_LENGTH_SHORT_MESSAGE = "각 자동차 이름은 1글자 이상 입력해주세요.";
     private final static String CAR_NAME_LENGTH_LONG_MESSAGE = "각 자동차 이름은 5글자 이하로 입력해주세요.";
+    private final static String CAR_NAME_LIST_EMPTY_MESSAGE = "각 자동차 이름을 ,로 구분해서 입력해주세요.";
 
     @Override
     public void checkUserInputValidate(List<String> checkStringList) {
         checkStringList.forEach(checkString -> {
-            if (isCarNameShorterThanMinLength(checkString)) {
+            if (isStringListEmpty(checkStringList)){
+                throw new IllegalArgumentException(CAR_NAME_LIST_EMPTY_MESSAGE);
+            } else if (isCarNameShorterThanMinLength(checkString)) {
                 throw new IllegalArgumentException(CAR_NAME_LENGTH_SHORT_MESSAGE);
             } else if (isCarNameLongerThanMaxLength(checkString)) {
                 throw new IllegalArgumentException(CAR_NAME_LENGTH_LONG_MESSAGE);
@@ -19,6 +22,9 @@ public class CarNameInputErrorException implements InputErrorException{
         });
     }
 
+    private static boolean isStringListEmpty(List<String> checkStringList){
+        return checkStringList.isEmpty();
+    }
     private static boolean isCarNameShorterThanMinLength(String checkString){
         return checkString.length() < CAR_NAME_LEGTH_MIN_RANGE;
     }
