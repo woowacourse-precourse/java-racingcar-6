ba4d293052da384056ec3955f38035ebec1996d8
IMWoo94 <leemeawoo94@gmail.com>
2023-10-26 18:19:29 +0900

    style : 요구사항 주석 제거
    요구사항 주석 제거

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -7,10 +7,6 @@ import java.util.stream.Collectors;
 import racingcar.exception.CarNameIncorrectException;
 import racingcar.exception.CarNameSizeLimitExceededException;
 
-/**
- * - 경주할 자동자 이름 입력 기능 - 경주할 자동차 이름들은 입력 받는다. ( camp.nextstep.edu.missionutils.Console의 readLine() ) - 입력 받은 이름은 쉼표로 구분
- * 하여 관리 - 입력 받은 이름 중 5자 이하 인지 확인 - 5자 이하인 경우 정상 진행 - 5자 초과인 경우 IllegalArgumentException 예외 발생
- */
 public class InputView {
 
     private InputView() {
