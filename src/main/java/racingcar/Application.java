a8e24f70c5f3cbbd115a14b238ed1c678839a949
woo <wsk221e@gmail.com>
2023-10-26 18:01:29 +0900

    feat: InputView - 여러 개의 이름을 받아 분할하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,14 @@
 package racingcar;
 
+import racingcar.View.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        // 자동차 이름 설정
+        InputView inputView = new InputView();
+
+        inputView.setCarNames();
     }
 }
--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,5 +1,17 @@
 package racingcar.View;
 
-class InputView {
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void setCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] names = input.split(",");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
 
 }
