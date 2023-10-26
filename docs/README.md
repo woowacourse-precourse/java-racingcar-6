bdf03b124feb17ec518294fd2e4bcc79939ce082
Reddyong <d0209hong@naver.com>
2023-10-26 17:51:15 +0900

    feat : create init()

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+# 구현 기능 목록
+
+## 1. 초기화 : init()
+### - 자동차들의 이름 입력
+* Console.readline()을 이용하여 String 형태로 입력 받은 자동차들의 이름들을 미리 선언해둔 전역변수 carString에 저장
+* checkCarLength() : carString에 저장된 차들의 이름 길이 판별
+  * 이름의 길이가 5보다 큰 경우는 IllegalArgumentException 발생시킴.
+### - 시도할 횟수 입력
+* Consol.readLine()을 이용하여 String 형태로 받은 값을 Integer.parseInt()를 이용하여 정수로 변환해 준다.
+* 이렇게 변환한 정수 값을 미리 선언해둔 전역변수 tryNum에 넣어준다.
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static String carString;
+    private static int tryNum;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        init();     // 자동차 이름 입력, 시도할 횟수 입력
+
+    }
+
+    public static void init() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carString = Console.readLine();
+        if (!checkCarLength(carString)) {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String tryString = Console.readLine();
+        tryNum = Integer.parseInt(tryString);
+    }
+
+    public static boolean checkCarLength(String carString) {
+        String[] carSplit = carString.split(",");
+        for (String c : carSplit) {
+            if (c.length() > 5) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
