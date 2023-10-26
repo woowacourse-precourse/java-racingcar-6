6d6ffca187f0a52e4be8f9cd574d6c670d2f1547
jun <ggprgrkjh@naver.com>
2023-10-27 00:42:44 +0900

    feat(outputview): 결과 시작 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,4 +9,7 @@ public class OutputView {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public static void printResultStartMessage() {
+        System.out.println("\n실행 결과");
+    }
 }
