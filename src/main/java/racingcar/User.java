2ddc276e4c9880c2330329ab699fc881998d0321
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-27 02:13:50 +0900

    feat: 자동차 이름이 6글자 이상일 경우 예외 처리 기능 추가

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -14,6 +14,8 @@ public class User {
 
         List<String> carNames = new ArrayList<>(Arrays.asList(carName.split(",")));
 
+        validateCarNameLength(carNames);
+
         return carNames;
     }
 
@@ -22,5 +24,18 @@ public class User {
         int numberOfTimes = Integer.parseInt(Console.readLine());
 
         return numberOfTimes;
+    }
+
+
+    private void validateCarNameLength(List<String> carNames) {
+
+        for (int i = 0; i < carNames.size(); i++) {
+
+            if (carNames.get(i).length() > 5) {
+
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하로만 가능합니다.");
+
+            }
+        }
     }
 }
