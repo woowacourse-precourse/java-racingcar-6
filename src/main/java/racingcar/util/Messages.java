da261e92f5f27b3a15532e2364e3b8b81de412eb
qudwns017 <qudwns017@naver.com>
2023-10-27 01:11:06 +0900

    Feat: Messages 클래스 추가

---
+++ src/main/java/racingcar/util/Messages.java
@@ -0,0 +1,8 @@
+package racingcar.util;
+
+public class Messages {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_RACING_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String RACING_RESULT = "실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+}
