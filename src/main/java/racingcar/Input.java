e2adf34c5ecf6f9858bbd033987ca2f26e22f021
idingg <idingg@gmail.com>
2023-10-26 23:12:40 +0900

    refactor: 수정 Class Input static method

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    List<String> inputCarName() {
+    static List<String> inputCarName() {
         String raw = Console.readLine();
 
         checkSplitFormatError(raw);
@@ -16,22 +16,22 @@ public class Input {
         return carNames;
     }
 
-    private void checkSplitFormatError(String raw) throws IllegalArgumentException {
+    private static void checkSplitFormatError(String raw) throws IllegalArgumentException {
         if (isEndWithComma(raw)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private boolean isEndWithComma(String raw) {
+    private static boolean isEndWithComma(String raw) {
         return raw.endsWith(",");
     }
 
-    private List<String> rawToList(String raw) {
+    private static List<String> rawToList(String raw) {
         String[] rawArray = raw.split(",");
         return new ArrayList<>(Arrays.asList(rawArray));
     }
 
-    private void checkCarNameError(List<String> carNames) throws IllegalArgumentException {
+    private static void checkCarNameError(List<String> carNames) throws IllegalArgumentException {
         for (String carName : carNames) {
             if (isNull(carName) || isBlank(carName) || isLength1To(carName, 5)) {
                 throw new IllegalArgumentException();
@@ -39,15 +39,15 @@ public class Input {
         }
     }
 
-    private boolean isNull(String raw) {
+    private static boolean isNull(String raw) {
         return raw == null;
     }
 
-    private boolean isBlank(String raw) {
+    private static boolean isBlank(String raw) {
         return raw.compareTo("") == 0;
     }
 
-    private boolean isLength1To(String raw, int length) {
+    private static boolean isLength1To(String raw, int length) {
         return raw.length() <= length;
     }
 }
