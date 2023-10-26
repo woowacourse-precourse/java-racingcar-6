4410b3f5df86682efc10adaea7fae1beb499cd27
jun <ggprgrkjh@naver.com>
2023-10-27 03:02:52 +0900

    feat(car): 자동차 전진 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,16 @@
 package racingcar.model;
 
 public class Car {
+
+    private String carName;
+    private int carLocation;
+
+    public int moveForward(int carNumber) {
+        if (carNumber >= 4) {
+            carLocation++;
+        }
+        return carLocation;
+    }
+
+
 }
