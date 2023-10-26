ceb60b48d8b12c1cc0731c9961b3cab069909c68
donghyunkim <ghffu405@gmail.com>
2023-10-26 17:31:26 +0900

    refactor: 최소최대숫자 상수로 변경

--- src/main/java/racingcar/domain/Dice.java
+++ src/main/java/racingcar/domain/Dice.java
@@ -7,13 +7,13 @@ import java.util.Random;
 public class Dice {
 
     private static final int MIN_DICE_NUMBER = 0;
-    private static final int MAX_DICE_NUMBER = 0;
+    private static final int MAX_DICE_NUMBER = 9;
 
     private Dice() {
     }
 
     public static int getRandomNumber(){
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(MIN_DICE_NUMBER, MAX_DICE_NUMBER);
     }
 
 
