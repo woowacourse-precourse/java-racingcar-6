281cc86698cc0b3361ff62c4e71b35e7f0d16105
saaut <saaut60@naver.com>
2023-10-26 16:16:45 +0900

    ãfeat: Car.java

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,4 +1,20 @@
 package racingcar.Model;
 
 public class Car {
+    String name;
+    String currentPosition;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCurrentPosition() {
+        return currentPosition;
+    }
+    public void setCurrentPosition(String currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
 }
