c0ca9f1f9d09062a40077758cf7a8a50ab450e9c
Octoping <mooner925@gmail.com>
2023-10-26 18:20:07 +0900

    feat: 자동차 클래스와 생성자 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private final CarEngine engine;
+
+    private int position;
+
+    public Car(String name, CarEngine engine) {
+        validateName(name);
+        this.name = name;
+        this.engine = engine;
+        this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5글자를 초과할 수 없습니다.");
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarTest {
+    @Test
+    @DisplayName("자동차는 처음 생성될 때 현 위치는 0이다")
+    void car_construct_test() {
+        // given
+        String name = "pobi";
+        CarEngine engine = () -> true;
+
+        // when
+        Car car = new Car(name, engine);
+
+        // then
+        assertThat(car.getPosition()).isZero();
+    }
+
+    @Test
+    @DisplayName("자동차의 이름은 5글자를 초과할 경우 IllegalArgumentException을 반환한다")
+    void car_construct_with_5length_over_name() {
+        // given
+        String name = "name-with-longer-than-5-length";
+        CarEngine engine = () -> true;
+
+        // when & then
+        assertThatThrownBy(() -> new Car(name, engine))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 5글자를 초과할 수 없습니다.");
+    }
+}
