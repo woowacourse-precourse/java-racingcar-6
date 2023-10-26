f626331c47f01f25bd0c09d3ce726374a1420451
park_yeongyu <sjjs7771@naver.com>
2023-10-26 23:40:19 +0900

    feat : Race 클래스에 세팅된 자동차 정보를 가지고 출력하는 함수 작성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -11,7 +11,22 @@ public class Race{
         message.printStartMessage();
         carSetting.setCarName();
     }
+    public void running(){
+        message.printResultMessage();
+        for(int try_count =0; try_count < trial; try_count++){
+
+        }
+    }
     public void setTrial(){
+        message.printHowManyGame();
         trial = Integer.parseInt(Console.readLine());
+    }
+
+    public void inCars(){
+        Car[] cars = carSetting.getCar();
+        for(Car car: cars){
+            message.printCarName(car.getCarName());
+            message.printRunCount(car.getRunCount());
+        }
     }
 }
