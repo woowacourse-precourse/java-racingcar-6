f107d8d02ddca121c10cd70fe6bf308f571790de
jun <ggprgrkjh@naver.com>
2023-10-26 22:23:06 +0900

    test(inputview): 경기 참여 자동차 입력 TDD 테스트

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,4 +1,20 @@
 package racingcar;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
+
 public class InputViewTest {
+    @Test
+    void 경기_참여_자동차_입력_테스트() {
+        String input = InputView.readCarName();
+        String[] inputs = input.split(",");
+
+        for(int i = 0 ; i < inputs.length; i++){
+            Assertions.assertTrue(inputs[i].length()<=5);
+            Assertions.assertTrue(inputs[i]!=null);
+        }
+
+    }
+
 }
