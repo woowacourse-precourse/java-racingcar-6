dabeb621916f19a84e82837b0553462dd5688c94
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-26 22:37:46 +0900

    feat(Application) : 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분");
+        String nameInput = Console.readLine();
+        // 자동차 이름 입력 관련 1차 유효성 검사
+        if (nameInput.contains(",")) {
+            if (nameInput.charAt(0) == ',' || nameInput.charAt(nameInput.length()-1) == ',') {
+                throw new IllegalArgumentException("Invalid name input format: "+nameInput);
+            }
+            if (nameInput.contains(",,")) {
+                throw new IllegalArgumentException("Invalid name input format: "+nameInput);
+            }
+        }
+        // 자동차 이름 입력 관련 2차 유효성 검사
+        Map<String, Integer> carList = new LinkedHashMap<>();
+        for (String carName : nameInput.split(",")) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("Not valid length: "+carName.length());
+            }
+            if (carList.containsKey(carName)) {
+                throw new IllegalArgumentException("Duplicated car name : " + carName);
+            }
+            carList.put(carName, 0);
+        }
+        // 입력된 자동차 이름순으로 저장되었는지 확인하기 위한 테스트 작성 필요
     }
 }
