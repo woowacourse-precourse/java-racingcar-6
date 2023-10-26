fd8daec34632d06ade2030d372fbf6272445c828
AmenPark <puzzlistpam@gmail.com>
2023-10-26 19:56:27 +0900

    create Car DTO class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+class Car {
+    private String name;
+    private int pos;
+
+    public Car(String name, int pos) {
+        this.name = name;
+        this.pos = pos;
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+}
