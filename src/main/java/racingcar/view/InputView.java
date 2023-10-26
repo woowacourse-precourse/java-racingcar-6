d15b210f22f0478f5e2dae283bde56864caa363b
haruday97 <starharu97@naver.com>
2023-10-27 01:21:20 +0900

    feat : 메시지 출력 메서드 추가
    유저 입력 및 결과 안내를 위한 메시지 출력 메서드 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+public class InputView {
+
+    private static final String MSG_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MSG_ASKING = "시도할 회수는 몇회인가요?";
+    private static final String MSG_RESULT = "실행 결과";
+
+    public static void printStart() {
+        System.out.println(MSG_START);
+    }
+
+    public static void printAsking() {
+        System.out.println(MSG_ASKING);
+    }
+
+    public static void printResult() {
+        System.out.println(MSG_RESULT);
+    }
+}
