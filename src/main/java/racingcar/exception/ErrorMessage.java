849dd199461b710625fb0aab260713a697ff1b10
Arachneee <jhg2819@naver.com>
2023-10-26 21:00:30 +0900

    fix: 에러메시지 잘못나오는 버그 수정
    refactoring하면서 수정하지 못한 버그를 수정했습니다.

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.exception;
 
 public enum ErrorMessage {
     NOT_INTEGER("[ERROR] 숫자만 입력가능합니다."),
-    INVALID_NAME_LENGTH("[ERROR] 이름 길이가 너무 깁니다."),
+    INVALID_NAME_LENGTH("[ERROR] 잘못된 이름 길이입니다."),
     CANT_FIND_MAX_VALUE("[ERROR] 최대 위치를 찾을 수 없습니다.");
 
     private final String message;
