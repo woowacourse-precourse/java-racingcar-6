61d3efb762dd6b98b1c0ea68f78a8e6027f5c6df
koomchang <leahpar0401@gmail.com>
2023-10-27 02:32:17 +0900

    feat: 자동차 경주에서 출력을 담당하는 OutputView 클래스 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void askForCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void askForNumberOfRounds() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    private void printEachResult() {
+
+    }
+
+    public void printWinners() {
+        System.out.println("최종 우승자 : ");
+    }
+
+}
