c6b949594b4a0b57506c6798062a35ed800bcc7d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-27 00:43:45 +0900

    feat: Add game start message

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
