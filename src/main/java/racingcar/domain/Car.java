cf148c1334066f79ccd771b4a70e252232706c77
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-26 22:39:17 +0900

    feat(Car): 전진

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,10 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MIN = 0;
+    private static final int MAX = 9;
     private static final String INVALID_LENGTH = "자동차 이름의 길이는 5자 이하여야합니다.";
 
     private final String name;
@@ -15,5 +19,16 @@ public class Car {
         if (5 < name.length()) {
             throw new IllegalArgumentException(INVALID_LENGTH);
         }
+    }
+
+    public void move() {
+        if (canGo()) {
+            position++;
+        }
+    }
+
+    private boolean canGo() {
+        int number = Randoms.pickNumberInRange(MIN, MAX);
+        return 4 <= number;
     }
 }
