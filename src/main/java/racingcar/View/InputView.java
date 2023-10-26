908a24e2a170bb8a3a709211df66152a3d0343bc
kyoo0115 <internetkyoomin@gmail.com>
2023-10-26 22:07:16 +0900

    feat: 프롬트 메세지 출력 구현

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,2 +1,13 @@
-package racingcar.View;public class InputView {
+package racingcar.View;
+
+import camp.nextstep.edu.missionutils.Console;
+public class InputView {
+    public static String getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+    public static String roundInput() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
 }
---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,2 @@
+package racingcar.View;public class OutputView {
+}
