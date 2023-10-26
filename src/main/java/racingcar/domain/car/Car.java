dea089a7721039d1bf33919455e31112e4ef347d
mainfn <_@mainfn.dev>
2023-10-26 22:51:08 +0900

    feat: Car 구현

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,12 +1,27 @@
 package racingcar.domain.car;
 
+import racingcar.domain.game.MoveResult;
 import racingcar.domain.move.Coordinate;
 
-public record Car(
-        CarName carName,
-        Coordinate coordinate
-) {
-    public void move() {
-        // TODO:
+public class Car {
+    private final CarName carName;
+    private final Coordinate coordinate;
+
+    private Car(
+            final CarName carName,
+            final Coordinate coordinate
+    ) {
+        this.carName = carName;
+        this.coordinate = coordinate;
     }
+
+    public Car(final String carName) {
+        this(new CarName(carName), Coordinate.zero());
+    }
+
+    public MoveResult moved() {
+        coordinate.increase();
+        return new MoveResult(carName.carName(), coordinate.getCoordinate());
+    }
+
 }
