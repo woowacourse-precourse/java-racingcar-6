f9033fed37dd0062e44d17f561520b950531b088
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-26 20:36:35 +0900

    fix(exception) : 5글자 초과 예외 문구 수정

--- src/main/java/racingcar/exception/CarNameInputErrorException.java
+++ src/main/java/racingcar/exception/CarNameInputErrorException.java
@@ -4,7 +4,7 @@ public class CarNameInputErrorException implements InputErrorException{
     private final static int CAR_NAME_LEGTH_MIN_RANGE = 1;
     private final static int CAR_NAME_LEGTH_MAX_RANGE = 5;
     private final static String CAR_NAME_LENGTH_SHORT_MESSAGE = "각 자동차 이름은 1글자 이상 입력해주세요.";
-    private final static String CAR_NAME_LENGTH_LONG_MESSAGE = "각 자동차 이름은 1글자 이상 입력해주세요.";
+    private final static String CAR_NAME_LENGTH_LONG_MESSAGE = "각 자동차 이름은 5글자 이하로 입력해주세요.";
 
     @Override
     public void checkUserInputValidate(String checkString) {
