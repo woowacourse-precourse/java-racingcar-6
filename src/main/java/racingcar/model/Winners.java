ffbae222743029acdaba006f3ee8bdb1a6ada767
Isaac Han <isaachan233@gmail.com>
2023-10-27 06:37:09 +0900

    feat: create winners

---
+++ src/main/java/racingcar/model/Winners.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+    private final List<Car> winnerList;
+    
+    public Winners() {
+        winnerList = new ArrayList<>();
+    }
+}
