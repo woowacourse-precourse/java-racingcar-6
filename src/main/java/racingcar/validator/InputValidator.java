008144145054ac22c154b9f948d7da8011f926d8
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 20:43:09 +0900

    feat(InputValidator) : 자동차 이름을 1 ~ 5글자로 입력하지 않았을 경우 예외 발생 기능 추가

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -16,4 +16,10 @@ public class InputValidator {
             throw new IllegalArgumentException("자동차 이름은 쉼표(,)를 기준으로 구분해야 합니다.");
         }
     }
+
+    public static void validateCarNameLength(String carName) {
+        if (carName.length() > 5 || carName.length() == 0) {
+            throw new IllegalArgumentException("자동차 이름은 1 ~ 5글자로 입력해야 합니다.");
+        }
+    }
 }
