afbcbbc949445eb14a8bc7fe5bed6dd0bbacc9c4
wooteco-daram <wooteco.daram@gmail.com>
2023-10-27 06:56:45 +0900

    feat: `Car` 객체가 `name` 이름 속성을 갖도록 멤버변수와 생성자, Getter 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,13 @@
 package racingcar;
 
 public class Car {
+    private final String name;
+
+    public Car(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
