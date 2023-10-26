e74c0cdc6c2ef33e22a1ade144a23ca78479e66a
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-26 23:33:16 +0900

    feat(InputView): 입력 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static String inputNames() {
+        System.out.println(NAME_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputCount() {
+        System.out.println(COUNT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }
