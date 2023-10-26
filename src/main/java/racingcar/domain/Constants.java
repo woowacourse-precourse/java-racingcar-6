e257e2a9171f054d271219e782aa4f24d5e8df1b
김영준 <rladudwns164@gmail.com>
2023-10-26 23:21:28 +0900

    feat(Constants): Constants 클래스 구현
    - Constants클래스를 이 레이싱 게임에 필요한 상수를 제공합니다.

---
+++ src/main/java/racingcar/domain/Constants.java
@@ -0,0 +1,8 @@
+package racingcar.domain;
+
+public class Constants {
+    public static final int NAME_SIZE = 5;
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+    public static final int GO_FOWARD = 4;
+}
