264a767a159df032f7a6995ce6d95a5397501cdd
koojawon <jawonkoo@naver.com>
2023-10-26 16:59:00 +0900

    feat: InputView 기본 기능 구현 -참가자 이름 입력 받기 -시도할 횟수 입력 받기

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+
+    public String getParticipant() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public String getTryCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return Console.readLine();
+    }
+}
