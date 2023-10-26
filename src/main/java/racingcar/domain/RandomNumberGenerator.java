bab52f0c24cebd538b488b88a2687cde067dc7a4
gyungchan Jo <namejgc@naver.com>
2023-10-27 01:12:29 +0900

    refactor(RandomNumberGenerator): 인터페이스를 통해 구현하도록 변경

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -3,8 +3,9 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.utils.Constants;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator {
 
+    @Override
     public int generate() {
         return Randoms.pickNumberInRange(
                 Constants.MIN_NUMBER_RANGE, Constants.MAX_NUMBER_RANGE);
