01f28a46aa75ed4f141fca6f720377a7bea7cbf3
yuseonjun <seonjun0906@gmail.com>
2023-10-26 18:00:21 +0900

    feat: 경주 할 자동차 이름을 입력할 때, 자동차 이름은 쉼표(,)를 기준으로 구분한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,22 +1,30 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private InputView() {
     }
 
-    public static String readCarNames() {
+    public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
         validateBlank(names);
-        return names;
+        return toList(names);
     }
 
     private static void validateBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("빈 값이 입력되면 안됩니다.");
         }
+    }
+
+    private static List<String> toList(String input) {
+        return Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
     }
 }
