36b6536125105ad3b8961615e368a3205e136709
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-26 19:59:14 +0900

    feat: OutputView 생성

---
+++ src/main/java/View/OutputView.java
@@ -0,0 +1,27 @@
+package View;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL = "시도할 회수는 몇회인가요?";
+    private static final String RESULT = "실행 결과";
+    private static final String WINNER = "최종 우승자 : ";
+
+    public void printStart(){
+        System.out.println(START);
+    }
+
+    public void printTrial(){
+        System.out.println(TRIAL);
+    }
+
+    public void printResult(/*List<RacingCar> RacingCars*/){
+        System.out.println(RESULT);
+        /* TODO */
+    }
+
+    public void printWinner(List<String> Winners){
+        System.out.println(WINNER + Winners);
+    }
+}
