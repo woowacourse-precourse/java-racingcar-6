82932219aa1af34be29c8269b27442238a62bb75
leeedohyun <ldh19738@gmail.com>
2023-10-26 21:06:20 +0900

    feat: 이동 횟수 입력 메시지 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,9 +2,14 @@ package racingcar.view;
 
 public class OutputView {
 
-    public static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUMBERS_OF_MOVES_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static void printInputCarsNameMessage() {
         System.out.println(INPUT_CARS_NAME_MESSAGE);
+    }
+
+    public static void printInputNumberOfMovesMessage() {
+        System.out.println(INPUT_NUMBERS_OF_MOVES_MESSAGE);
     }
 }
