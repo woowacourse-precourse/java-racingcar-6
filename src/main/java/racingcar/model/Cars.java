5a8e453c1b40267c9c6ae9f8fc86cec25cd979a8
Isaac Han <isaachan233@gmail.com>
2023-10-27 07:48:48 +0900

    feat: 자동차 이름들을 받아서 자동차를 추가하는 메서드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -6,12 +6,16 @@ import java.util.List;
 public class Cars {
     private final List<Car> carList = new ArrayList<>();
 
+    public void addCar(String name) {
+        carList.add(new Car(name));
+    }
+    
     public void addCars(List<String> names) {
         for (String name : names) {
-            carList.add(new Car(name));
+            addCar(name);
         }
     }
-
+    
     public List<Car> getCarList() {
         return carList;
     }
