fba9b545521d6c01f2b9011767c6dd68577bec6b
JuSeong1130 <jsmoon1130@naver.com>
2023-10-27 06:31:02 +0900

    feat(Name) : Name 생성 기능 구현
    Name은 생성시 빈칸이거나 5글자초과라면 예외가 발생합니다.

---
+++ src/main/java/racingcar/domain/car/Name.java
@@ -0,0 +1,28 @@
+package racingcar.domain.car;
+
+public class Name {
+
+    private String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        validateBlank(name);
+        validateName(name);
+    }
+
+    private void validateBlank(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 빈값이면 안됩니다");
+        }
+    }
+
+    private void validateName(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5글자 이하여야합니다.");
+        }
+    }
+}
