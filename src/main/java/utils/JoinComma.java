441081b13e039772e783cec3ac338fd458c6efc3
happyryan <tjdgks120813@gmail.com>
2023-10-27 00:19:46 +0900

    refactor(utils JoinComma): refactor playerInput and winnerOutput methods using String.join
    Updated the playerInput and winnerOutput methods to utilize String.join for improved code simplicity and readability.

--- src/main/java/utils/JoinComma.java
+++ src/main/java/utils/JoinComma.java
@@ -4,25 +4,11 @@ import java.util.*;
 
 public class JoinComma {
     public static String playerInput(List<String> players) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < players.size(); i++) {
-            sb.append(players.get(i));
-            if (i != players.size() - 1) {
-                sb.append(",");
-            }
-        }
-        return sb.toString();
+        return String.join(",", players);
     }
 
     public static String winnerOutput(List<String> winners) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < winners.size(); i++) {
-            sb.append(winners.get(i));
-            if (i != winners.size() - 1) {
-                sb.append(", ");
-            }
-        }
-        return sb.toString();
+        return String.join(", ", winners);
     }
 
 }
