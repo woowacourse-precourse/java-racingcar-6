2f25aac6600e3a3819241bd5744d5c51e6b6789e
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-26 23:50:19 +0900

    refactor: 잘못 기입된 메소드명 수정
    첫 문자 대문자 오입력으로 인한 메소드명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String InputCarName() {
+    public static String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
