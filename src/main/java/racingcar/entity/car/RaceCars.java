52f482c7e12874c1a814322b47c31f7baf4895d9
uhanuu <hwryu990927@gmail.com>
2023-10-27 01:19:28 +0900

    feat: raceService 구현

--- src/main/java/racingcar/entity/car/RaceCars.java
+++ src/main/java/racingcar/entity/car/RaceCars.java
@@ -1,12 +1,21 @@
 package racingcar.entity.car;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class RaceCars {
     private final List<Car> cars;
 
     public RaceCars(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public int size(){
+        return cars.size();
+    }
+
+    public Stream<Car> stream(){
+        return cars.stream();
     }
 
 }
---
+++ src/main/java/racingcar/service/RaceGameService.java
@@ -0,0 +1,27 @@
+package racingcar.service;
+
+import racingcar.entity.car.RaceCars;
+import racingcar.util.Randoms;
+import racingcar.view.ExecutionView;
+
+public class RaceGameService {
+    private RaceCars raceCars;
+    private final int attemptCount;
+
+    public RaceGameService(RaceCars raceCars, int attemptCount) {
+        this.raceCars = raceCars;
+        this.attemptCount = attemptCount;
+    }
+
+    public void run(){
+        ExecutionView.printExecutionStartMessage();
+
+        for (int i=0; i<attemptCount; i++){
+            raceCars.stream()
+                    .forEach(car -> car.controlMovement(Randoms.getNumber()));
+
+            raceCars.stream()
+                    .forEach(ExecutionView::printExecutionMessage);
+        }
+    }
+}
