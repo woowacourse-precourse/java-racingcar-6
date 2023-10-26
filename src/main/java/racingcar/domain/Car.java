e28dce779399d45b8d60b81b3d363f400d9398e2
minseo  jo <liging12@naver.com>
2023-10-26 21:00:07 +0900

    feat: Car 기능 추가
    정적 변수 Count 를 통해 각 차의 Id를 초기화 해줬다.
    차 중복 이름에 대한 처리를 카의 메타 데이터를 이용하기 위해 Id를 추가 했다.
    
    getFowardCount() 를 없앨 수 있다. 왜냐하면 Racing 에서 레이싱을 하면서 최대 거리를 매번 초기화 해주면서 Judgement 한테 메시지로 보내주면 되기 떄문이다. 우선 넣어 놓고, 추후 필요하지 않다면 방금 말한 플로우 대로 처리할 예정이다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+public class Car {
+
+    private static int count = 0; // ID 를 위한 정적 변수 (생성자를 호출할 때 마다, 카운트 증가 → 각각의 차 ID)
+
+    private final int id; // 중복 이름을 고려한, 차 ID
+    private final String name;
+    private int forwardCount;
+
+    public Car(String name) {
+        this.id = count++;
+        this.name = name;
+        this.forwardCount = 0;
+    }
+
+    public int getForwardCount() {
+        return forwardCount;
+    }
+
+    public void forward() {
+        forwardCount++;
+    }
+}
