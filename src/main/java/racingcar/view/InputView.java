adfa9c4ddf6343990e08dd790c416601bc18b70f
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-26 20:41:25 +0900

    feat(InputValidator) : 아무런 값을 입력하지 않았을 경우 예외 발생 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String inputCarNames() {
+        return Console.readLine();
+    }
+
+
+}
