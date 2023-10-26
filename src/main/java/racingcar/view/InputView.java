3959b7e3ac4320b59799d941518b0d44d6115b38
김영준 <rladudwns164@gmail.com>
2023-10-26 19:49:33 +0900

    feat(InputView) : InputView 클래스 구현
    - InputView클래스는 사용자 로부터 입력받는 기능, 입력받는 화면을 출력 하는 기능을 제공합니다.
    - displayCarNamePrompt()은 자동차 이름 입력을 요청하는 화면을 출력합니다.
    - displayRaceCountPrompt()은 레이스 횟수 입력을 요청하는 화면을  출력합니다.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,36 @@
+package racingcar.view;
+
+public class InputView {
+
+    private static final InputView INSTANCE = new InputView();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return INSTANCE;
+    }
+
+    public void displayCarNamePrompt() {
+        System.out.println(Message.CAR_NAME_INPUT.getMessage());
+    }
+
+    public void displayRaceCountPrompt() {
+        System.out.println(Message.RACE_COUNT.getMessage());
+    }
+
+    private enum Message {
+        CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        RACE_COUNT("시도할 회수는 몇회인가요?");
+
+        private String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        private String getMessage() {
+            return message;
+        }
+    }
+}
