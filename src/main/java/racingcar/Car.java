7ba1b0dadf51e3ede931d3dc07f403f00009e5ca
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2023-10-26 21:37:06 +0900

    feat(Car) : 자동차 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
+
+public class Car {
+    private String name;
+    private int position = 0;
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public void go(){
+        int num = Randoms.pickNumberInRange(0,9);
+        if (num >= 4){
+            position++;
+        }
+    }
+
+}
