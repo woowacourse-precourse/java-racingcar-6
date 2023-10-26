e903a9b421564dd74609f512f7ebaf4acf2e82b9
eom-tae-in <eti0728@daum.net>
2023-10-27 05:55:42 +0900

    feat: ConsoleInputView 클래스 구현

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -1,4 +1,34 @@
 package racingcar.view;
 
+import racingcar.controller.dto.GameResultResponse;
+import racingcar.controller.dto.GameWinnerResponse;
+
 public class ConsoleOutputView implements OutputView {
+
+    private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GAME_ATTEMPTS_NUMBER_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+
+    @Override
+    public void printCarNameRequestMessage() {
+        System.out.println(CAR_NAME_REQUEST_MESSAGE);
+    }
+
+    @Override
+    public void printGameAttemptsNumberRequestMessage() {
+        System.out.println(GAME_ATTEMPTS_NUMBER_REQUEST_MESSAGE);
+    }
+
+    @Override
+    public void printGameResultMessage() {
+        System.out.println(GAME_RESULT_MESSAGE);
+    }
+
+    @Override
+    public void printGameResult(final GameResultResponse gameResultResponse) {
+    }
+
+    @Override
+    public void printWinner(final GameWinnerResponse gameWinnerResponse) {
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,13 @@
 package racingcar.view;
 
+import racingcar.controller.dto.GameResultResponse;
+import racingcar.controller.dto.GameWinnerResponse;
+
 public interface OutputView {
+
+    void printCarNameRequestMessage();
+    void printGameAttemptsNumberRequestMessage();
+    void printGameResultMessage();
+    void printGameResult(GameResultResponse gameResultResponse);
+    void printWinner(GameWinnerResponse gameWinnerResponse);
 }
