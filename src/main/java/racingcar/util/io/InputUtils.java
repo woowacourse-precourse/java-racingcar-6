1f04286ed3d3e3c6eae2774196fce44e70cbc7d3
chaeshee0908 <chaeshee0908@naver.com>
2023-10-26 19:22:32 +0900

    [feat] 차 이름 입력 기능 구현

---
+++ src/main/java/racingcar/util/io/InputUtils.java
@@ -0,0 +1,10 @@
+package racingcar.util.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtils {
+
+    public static String getCarsName() {
+        return Console.readLine();
+    }
+}
