f0d923808a2517c3db1c9fa3c9d9f2e33d8ddd2e
Arachneee <jhg2819@naver.com>
2023-10-26 16:34:20 +0900

    feat: 수행 횟수 입력값 0 또는 양의 정수인지 검증하기 기능 구현
    정규 표현식을 이용해 0 또는 양의 정수인지 검증해서 정수로 변환하는 기능을 추가했습니다

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -13,10 +13,29 @@ public class RacingGame {
     public static void run() {
         String carNames = inputView.enterCarNames();
         Cars cars = Cars.from(carNames);
-        String inputNumberString = inputView.enterNumberOfTimes();
-        //Integer numberOfTimes = convertInteger(inputNumberString);
+
+        Integer numberOfTimes = convertToInteger(inputView.enterNumberOfTimes());
 
 
 
+    }
+
+    private static Integer convertToInteger(final String inputNumberString) {
+        validateNumber(inputNumberString);
+
+        return Integer.valueOf(inputNumberString);
+    }
+
+    private static void validateNumber(final String inputNumberString) {
+        if (isNotNumber(inputNumberString)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.");
+        }
+
+    }
+
+    private static boolean isNotNumber(final String inputNumberString) {
+        final String REGEX = "[0-9]+";
+
+        return !inputNumberString.matches(REGEX);
     }
 }
