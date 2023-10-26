e211fd32f877289913c6bae13be025531f207f76
k9want <zndwlrnjr26@daum.net>
2023-10-27 01:06:43 +0900

    feat: vali - 자동차 이름이 5자 이하가 맞는지 체크 (입력 안했는지, 6자리 이상인지 체크)

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -1,10 +1,18 @@
 package racingcar.util;
 
 public class Validation {
+    private static final int CAR_NAME_LENGTH = 5;
 
     public static void valiEmptySplitInputCarNames(String[] carNames) {
         if (carNames.length == 0) {
             throw new IllegalArgumentException("Empty split to carNames Array");
         }
+    }
+
+    private static void valiCarNameLength(String carName) {
+        if (carName.length() <= CAR_NAME_LENGTH && carName.length() > 0) {
+            return;
+        }
+        throw new IllegalArgumentException("Invalid car name length");
     }
 }
