59aa184d688e05e8a0b09044f480a9e582896bd9
jeongkyojun <kyojun2002@naver.com>
2023-10-26 16:14:36 +0900

    Player class 생성 및 구성함수 추가

---
+++ src/main/java/racingcar/Player.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class Player implements Comparable<Player>{
+    private String name;
+    private String result;
+    private int count;
+    Player(String name){
+        setCount(0);
+        setResult("");
+        setName(name);
+    }
+
+    public int getCount(){
+        return count;
+    }
+    public void setCount(int count){
+        this.count = count;
+    }
+    public String getResult(){
+        return result;
+    }
+    public void setResult(String result){
+        this.result = result;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void move(){
+        int score = Randoms.pickNumberInRange(0,9);
+        if(score>4){
+            setResult(getResult()+"-");
+            setCount(getCount()+1);
+        }
+    }
+
+    public void print(){
+        System.out.println(getName()+" : "+getResult());
+    }
+
+    @Override
+    public int compareTo(Player o) {
+        return o.count-count;
+    }
+}
