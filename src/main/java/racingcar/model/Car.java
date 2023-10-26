f34345e917288219f8f734a91633226505d57555
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-26 23:07:20 +0900

    feat(model) : car model에서 랜덤한 수를 받아 전진 or stop 메소드 구현 - docs : model -> car -> 랜덤한 수를 받아 전진,가만 구분 체크

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,9 +2,21 @@ package racingcar.model;
 
 public class Car {
     private final String name;
-    private int position = 0;
+    private int position;
+    private final static int CAR_MOVEABLE_VALUE = 4;
 
-    public Car(String name){
+    public Car(String name, int position){
         this.name = name;
+        this.position = position;
+    }
+
+    public void moveOrStop(int moveCommand){
+        if (isCarMoveable(moveCommand)) {
+            this.position += 1;
+        }
+    }
+
+    private static boolean isCarMoveable(int moveCommand){
+        return moveCommand >= CAR_MOVEABLE_VALUE;
     }
 }
