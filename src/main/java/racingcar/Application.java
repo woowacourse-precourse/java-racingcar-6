18f8c1341a1c5e1881cb8014412638550f255cd7
Nbowow <nbo5548@naver.com>
2023-10-27 00:00:57 +0900

    Feat: 기능 구 1. 경주 할 자동차 이름 입력 2. 몇 번 이동할지 입력 3. '자동차 마다' 0에서 9사이의 무작위 값 도출 4. '자동차 마다' 전진 or 멈춤 판단 후 출력 5. 우승자 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,55 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Scanner inputCar = new Scanner(System.in);
+        String totalCar = inputCar.nextLine();
+        String[] carName = totalCar.split(",");
+        int[] carPresentPoint = new int[carName.length];
+
+        for (String s : carName) {
+
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        Scanner inputCount = new Scanner(System.in);
+        int count = inputCount.nextInt();
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < carPresentPoint.length; j++) {
+                if (Randoms.pickNumberInRange(0, 9) >= MOVING_FORWARD) carPresentPoint[j] += 1;
+            }
+
+            for (int j = 0; j < carName.length; j++) {
+                System.out.println(carName[j] + " : " + "-".repeat(carPresentPoint[j]));
+            }
+            System.out.println("");
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < carPresentPoint.length; i++) {
+            max = Math.max(max, carPresentPoint[i]);
+        }
+
+        String answer = "";
+        for (int i = 0; i < carName.length; i++) {
+            if (carPresentPoint[i] == max) {
+                answer += carName[i];
+                answer += ", ";
+            }
+        }
+        answer = answer.substring(0, answer.length() - 2);
+
+        System.out.println("최종 우승자 : " + answer);
     }
 }
