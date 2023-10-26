5c89f4fba38623e7ccc38e56224f72773de865b5
IMWoo94 <leemeawoo94@gmail.com>
2023-10-26 16:45:56 +0900

    test : 자동차 경주 게임 오프닝 멘트 기능 테스트 작성
    자동차 경주 게임 오프닝 멘트 정상적인 오프닝 멘트인지 테스트

---
+++ src/test/java/racingcar/views/OutputViewTest.java
@@ -0,0 +1,31 @@
+package racingcar.views;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.constants.RacingCarGameComments;
+
+class OutputViewTest {
+
+    @Test
+    @DisplayName("레이싱 자동차 경주 오프닝 멘트 출력 값 테스트")
+    void racingCarOpeningComment() {
+
+        String output = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        String outputConstant = RacingCarGameComments.OPENING_COMMENT;
+        PrintStream standardOut = System.out;
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteOutput));
+
+        OutputView.racingCarOpeningComment();
+
+        System.setOut(standardOut);
+        String result = byteOutput.toString().trim();
+        assertThat(output).isEqualTo(result);
+        assertThat(outputConstant).isEqualTo(result);
+    }
+
+}
