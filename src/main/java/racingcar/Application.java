fe9d0027405c598bea71734b4e068c85f5910500
hackertaco <imsa1224@korea.ac.kr>
2023-10-26 16:55:41 +0900

    feat(2): 이동 횟수 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,6 +14,16 @@ public class Application {
         if(anyNameExceedsCharactersLimit) {
             throw new IllegalArgumentException(String.format("이름은 %d자를 넘지 못합니다.", CHARACTERS_LIMIT));
         }
+        // 기능 2.
+        System.out.println("시도할 회수는 몇회인가요?");
+        try {
+            int trial = Integer.parseInt(Console.readLine());
+            if(trial < 0 || trial > Integer.MAX_VALUE){
+                throw new IllegalArgumentException(String.format("게임 시도 횟수는 음수가 되면 안됩니다. 현재 작성한 횟수: %d", trial));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+        }
 
 
 
