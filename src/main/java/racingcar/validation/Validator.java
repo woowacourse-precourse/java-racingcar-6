b083d673f3d4358d4896ced51151f26f3a4d70bb
donghyunkim <ghffu405@gmail.com>
2023-10-26 17:53:43 +0900

    feat: comma포함 검증메소드 추가

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -5,6 +5,12 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
+    public static void validateContainsComma(String str){
+        if(!str.contains(",")){
+            throw new IllegalArgumentException("문자열에 , 가 포함되어 있지 않습니다.");
+        }
+    }
+
     public static void validateNames(String names){
         String regex = "([a-zA-Z0-9]{1,5})(,[a-zA-Z0-9]{1,5})*";
 
