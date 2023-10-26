7352c4edddb2062cae825a72a14f9c4406bfc893
woo <wsk221e@gmail.com>
2023-10-26 20:23:47 +0900

    feat: Car 클래스 move 메소드 조건 추가(요구사항 반영)

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,5 +1,7 @@
 package racingcar.Model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int moves = 0;
@@ -8,12 +10,18 @@ public class Car {
         this.name = name;
     }
 
-    public void move() {
-        this.moves++;
+    public void tryMove() {
+        if (canMove()) {
+            moves++;
+        }
     }
 
     public void getName() {
         System.out.println(this.name);
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) > 3;
     }
 
 }
