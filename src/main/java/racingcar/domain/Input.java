905d504195ba9642cde326c1a643b23df33b475c
yechop <channetattoo@gmail.com>
2023-10-26 23:50:05 +0900

    feat: 5글자 이상 예외발생 기능 구현

--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -14,7 +14,16 @@ public class Input {
         userInput = Console.readLine();
         List<String> carNames = new ArrayList<>();
         carNames = Arrays.asList(userInput.split(","));
+        validateNameLength(carNames);
         return carNames;
+    }
+
+    private void validateNameLength(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public int inputGameCount() {
