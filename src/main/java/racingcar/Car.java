4b21f38c95e9818ddbb972f63a3e0c7fc18fc137
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2023-10-26 21:54:52 +0900

    fix(Car) : 자동차 생성 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,15 +8,22 @@ public class Car {
     private String name;
     private int position = 0;
 
-    public Car(String name){
+    public Car(String name) {
         this.name = name;
     }
 
-    public void go(){
-        int num = Randoms.pickNumberInRange(0,9);
-        if (num >= 4){
+    public void go() {
+        int num = Randoms.pickNumberInRange(0, 9);
+        if (num >= 4) {
             position++;
         }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
