dd74374e0aa748948a9eba5650a3196112f04caf
mainfn <_@mainfn.dev>
2023-10-26 22:50:07 +0900

    test: CarTest 작성

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,16 +1,28 @@
-//package racingcar.domain.car;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//final class CarTest {
-//    @DisplayName()
-//    @Test
-//    void test() {
-//        // given
-//
-//        // when
-//
-//        // then
-//    }
-//}
+package racingcar.domain.car;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.game.MoveResult;
+
+final class CarTest {
+
+    @DisplayName("moved를 N번 호출하면 coordinate는 N이다")
+    @Test
+    void carMovedTest() {
+        // given
+        final String carName1 = "a";
+        final Car car = new Car(carName1);
+        final int n = 100;
+
+        // when
+        // then
+        for (int i = 1; i <= n; i++) {
+            final MoveResult moveResult = car.moved();
+            assertThat(moveResult.carName()).isEqualTo(carName1);
+            assertThat(moveResult.coordinate()).isEqualTo(i);
+        }
+
+    }
+}
