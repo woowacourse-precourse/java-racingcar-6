8ff5be73c885f039e8e8c16628b6787a7fd2c9ab
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-26 16:43:37 +0900

    feat: start메소드 큰틀 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,14 @@
+package racingcar.controller;
+
+public class GameController {
+
+    public void start(int attempts){
+
+        while(attempts!=0){
+            // 게임 시도
+
+            // 시도 횟수 감소
+            attempts--;
+        }
+    }
+}
