94459c8ed17d0ef64e02738b875828aa5de3dc93
park_yeongyu <sjjs7771@naver.com>
2023-10-26 21:36:35 +0900

    feat : RunningMessage 클래스에 몇번 시도하는지 묻는 함수 작성

--- src/main/java/racingcar/RunningMessage.java
+++ src/main/java/racingcar/RunningMessage.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 public class RunningMessage {
-    public void printStartMessage(){
+    public void printStartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void printHowManyGame() {
+        System.out.println("시도할 횟수는 몇회인가요?");
     }
 }
