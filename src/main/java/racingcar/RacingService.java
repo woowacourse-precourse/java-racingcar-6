95ad15793355e50357aaffa0d648dd1f408c5c51
jhon3242 <rldotnr2@gmail.com>
2023-10-26 20:52:59 +0900

    feat : Car 를 record 에서 class 로 수정
    Car 의 위치 정보인 position 필드값을 final 로 선언할 수 없어서 class 로 수정하였다.

--- src/main/java/racingcar/RacingService.java
+++ src/main/java/racingcar/RacingService.java
@@ -1,10 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.Car;
 
 public class RacingService {
     public void move(List<Car> carList) {
-//        carList.forEach();
+        carList.forEach(car -> {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                car.moveForward();
+            }
+        });
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,16 +2,25 @@ package racingcar.domain;
 
 import racingcar.message.ErrorMessage;
 
-public record Car(String name) {
+public class Car {
     private static final int MAX_NAME_LENGTH = 4;
+    private final String name;
+    private int position;
 
-    public Car {
+
+    public Car(String name) {
         validateName(name);
+        this.name = name;
+        this.position = 0;
     }
 
     private void validateName(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME);
         }
+    }
+
+    public void moveForward() {
+        position++;
     }
 }
