c32da6af34c9ca09e871d22216c5d69b2e2126e6
eom-tae-in <eti0728@daum.net>
2023-10-27 05:10:27 +0900

    feat: CarRacingTrackGenerator 클래스 구현

--- src/main/java/racingcar/model/CarRacingTrackGenerator.java
+++ src/main/java/racingcar/model/CarRacingTrackGenerator.java
@@ -1,4 +1,18 @@
 package racingcar.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CarRacingTrackGenerator implements RacingTrackGenerator<Car> {
+
+    private final Map<Car, Position> map = new HashMap<>();
+
+    @Override
+    public RacingGame create(final List<Car> vehicles, final int playCount) {
+        for (int index = 0; index < playCount; index++) {
+            map.put(vehicles.get(index), Position.createWith(index));
+        }
+        return new CarRacingGame(map);
+    }
 }
--- src/main/java/racingcar/model/RacingTrackGenerator.java
+++ src/main/java/racingcar/model/RacingTrackGenerator.java
@@ -1,4 +1,8 @@
 package racingcar.model;
 
+import java.util.List;
+
 public interface RacingTrackGenerator<T extends Vehicle> {
+
+    RacingGame create(List<T> vehicles, int playCount);
 }
