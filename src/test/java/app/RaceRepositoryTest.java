58ffba89b08965134a61bc7eb310983dafc9bc68
김승우 <tmd8633@naver.com>
2023-10-26 20:22:47 +0900

    Add : TEST 코드 작성 (Valid, Repository, Service)

--- src/test/java/app/RaceRepositoryTest.java
+++ src/test/java/app/RaceRepositoryTest.java
@@ -1,4 +1,79 @@
-import static org.junit.jupiter.api.Assertions.*;
+package app;
+
+import app.repository.RaceRepository;
+import app.repository.Valid;
+import app.template.InputTemplate;
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.*;
+
 class RaceRepositoryTest {
-  
+
+    private void command(String[] args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    @AfterEach
+    private void close() {
+        Console.close();
+    }
+
+    private final RaceRepository raceRepository = new RaceRepository();
+    private final Valid valid = new Valid();
+    private final InputTemplate template = new InputTemplate();
+
+    @Test
+    @DisplayName("getCarName 정상흐름")
+    void getCarName() {
+        String[] input = {"a,b,c,d,e", "a", "aaaa,qqqq,cccc,dddd,eeee,ffff,zxcv,asdf,qwer"};
+        command(input);
+
+        for (String inputName : input) {
+            String name = raceRepository.getCarName();
+            assertThat(name).isEqualTo(inputName);
+        }
+    }
+    @Test
+    @DisplayName("getCarName 예외흐름")
+    void getCarNameException() {
+        String[] input = {null, "", " ", "   ", "aaaaa", "a,,d", ",d,", ",,,"};
+        command(input);
+
+        for (String inputName : input) {
+            assertThatThrownBy(() -> raceRepository.getCarName())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("getRaceCount() 정상흐름")
+    void getRaceCount() {
+        String[] input = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        command(input);
+
+        for (String inputName : input) {
+            String getName = raceRepository.getRaceCount();
+            assertThat(getName).isEqualTo(inputName);
+        }
+
+    }
+
+    @Test
+    @DisplayName("getRaceCount() 예외흐름")
+    void getRaceCountException() {
+        String[] input = {null, "", " ", "   ", "aaaaa", "a,,d", ",d,", ",,,", "0", "a", "123123", " 123", "1 23", "1 1"};
+        command(input);
+
+        for (String inputName : input) {
+            assertThatThrownBy(() -> raceRepository.getRaceCount())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
 }
--- src/test/java/app/ValidTest.java
+++ src/test/java/app/ValidTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package app;
+
+import app.repository.Valid;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class ValidTest {
-  
+
+    private final Valid valid = new Valid();
+
+    @Test
+    @DisplayName("자동차 이름에 대한 예외")
+    void getCarNameException() {
+        String[] validTestList = {"", " ", "aaaaaaaaaa", null, "            "};
+        for (String test : validTestList) {
+            assertThatThrownBy(() -> valid.validCarName(test)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("경기 횟수에 대한 예외")
+    void isNumber() {
+        String[] validTestList = {"", " ", "aaaaaaaaaa", null, "            ", "0", "12", "a123", "a1", "1a"};
+        for (String test : validTestList) {
+            assertThatThrownBy(() -> valid.validCount(test)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
 }
