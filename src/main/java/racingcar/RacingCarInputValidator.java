4d6d449b955dbc44cc2cd9f49e89cb0de703c8d4
kunsanglee <gosmdochee2@gmail.com>
2023-10-26 20:43:56 +0900

    feat(RacingCarInputValidator): 입력받은 자동차의 이름이 한글, 영어, 숫자인지 검증하는 기능 구현

--- src/main/java/racingcar/RacingCarInputValidator.java
+++ src/main/java/racingcar/RacingCarInputValidator.java
@@ -5,16 +5,26 @@ import java.util.List;
 public class RacingCarInputValidator {
 
     public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final String MAX_CAR_NAME_EXCEPTION = "자동차 이름은 최대 5자 이하만 가능합니다.";
+    public static final String AVAILABLE_CHARACTER_REGEX = "[a-zA-Z0-9가-힣]*";
+    public static final String AVAILABLE_CHARACTER_EXCEPTION = "자동차 이름은 한글, 영어, 숫자만 가능합니다.";
 
     public void validateCarNames(List<String> carNames) {
         for (String carName : carNames) {
             validateLength(carName);
+            validateCharacter(carName);
         }
     }
 
     private void validateLength(String carName) {
         if (carName.isEmpty() || MAX_CAR_NAME_LENGTH < carName.length()) {
-            throw new IllegalArgumentException("자동차 이름은 최대 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException(MAX_CAR_NAME_EXCEPTION);
+        }
+    }
+
+    private void validateCharacter(String carName) {
+        if (!carName.matches(AVAILABLE_CHARACTER_REGEX)) {
+            throw new IllegalArgumentException(AVAILABLE_CHARACTER_EXCEPTION);
         }
     }
 }
