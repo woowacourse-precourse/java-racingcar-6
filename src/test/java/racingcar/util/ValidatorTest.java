80791563ed089d2988b6e69a04c0266a017a87f4
junOTL <naqp99@gmail.com>
2023-10-27 01:49:36 +0900

    test: 중복된 자동차 이름을 입력 예외 테스트

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,8 +1,11 @@
 package racingcar.util;
 
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
@@ -22,5 +25,21 @@ class ValidatorTest {
         Assertions.assertThatThrownBy(() -> Validator.isLengthLessThanFive(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 5자 이하만 가능합니다.");
+    }
+
+    private static Stream<Arguments> provideDuplicateTestCases() {
+        return Stream.of(Arguments.of((Object) new String[]{"pobi", "pobi", "pobi"}),
+                Arguments.of((Object) new String[]{"woni", "pobi", "woni"}),
+                Arguments.of((Object) new String[]{"jun", "woni", "jun"}),
+                Arguments.of((Object) new String[]{"woni", "jun", "jun"}));
+    }
+
+    @ParameterizedTest
+    @DisplayName("중복된 자동차 이름을 입력 예외 테스트")
+    @MethodSource("provideDuplicateTestCases")
+    public void invalidInputsTest3(String[] inputs) {
+        Assertions.assertThatThrownBy(() -> Validator.isDuplicateName(inputs))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복되지 않는 자동차 이름을 입력해주세요.");
     }
 }
