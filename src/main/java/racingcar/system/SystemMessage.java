97814e550700972baacd2d2825d03aa5f7b0747a
SeongUk52 <rkfka7849@gmail.com>
2023-10-27 00:25:43 +0900

    feat(SystemMessage): 출력 문자열을 상수형태로 저장하는 열거형 구현

---
+++ src/main/java/racingcar/system/SystemMessage.java
@@ -0,0 +1,19 @@
+package racingcar.system;
+
+public enum SystemMessage {
+    INPUT_CAR_NAME ("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
+    INPUT_NUMBER_OF_TRY ("시도할 회수는 몇회인가요?\n"),
+    RACE_RESULT ("실행 결과\n"),
+    FINAL_WINNER ("최종 우승자 : ");
+
+
+    private String message;
+
+    SystemMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
