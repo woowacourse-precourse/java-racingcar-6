1514541eaa579450df6995ea817ef4577f0359c3
이진모 <jinmlee210@naver.com>
2023-10-27 00:01:09 +0900

    feat: 자동차가 앞으로 갈지 결정해주는 checkForward() 메서드 기능구현

--- src/main/java/racingcar/controller/Race.java
+++ src/main/java/racingcar/controller/Race.java
@@ -4,4 +4,11 @@ public class Race {
     public void startRacing(){
 
     }
+
+    public boolean checkForward(int randomNumber){
+        if(randomNumber >= 4){
+            return true;
+        }
+        return false;
+    }
 }
