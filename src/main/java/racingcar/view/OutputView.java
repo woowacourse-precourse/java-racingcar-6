b4466ae36e99592e8ce5bf7ec12d220156eefa31
Nara Park <skfk6352@naver.com>
2023-10-27 04:00:50 +0900

    <feat>: create method for printing result message
    플레이어에게 실행 결과 문구를 출력하는 메서드 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+
+    public static void printResultMessage() {
+        System.out.println(GAME_RESULT_MESSAGE);
+    }
+}
