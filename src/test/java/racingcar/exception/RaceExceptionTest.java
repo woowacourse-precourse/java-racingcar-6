5b0d66ff3038a10b456775dcf6a035a09e4ae2fc
euics <pjh575157@gmail.com>
2023-10-26 21:02:21 +0900

    refactor: RaceExceptionTest 테스트 이름 수정
    수정사항
    - 각각의 메서드에 알맞는 DisplayName 설정

--- src/test/java/racingcar/exception/RaceExceptionTest.java
+++ src/test/java/racingcar/exception/RaceExceptionTest.java
@@ -1,5 +1,6 @@
 package racingcar.exception;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,18 +8,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RaceExceptionTest {
     @Test
+    @DisplayName("자동차 이름이 null일때 예외처리 발생")
     void shouldThrowExceptionWhenCarNameIsNull() {
         assertThatThrownBy(() -> CarNameValidator.validate(null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
+    @DisplayName("자동차 이름의 길이가 5보다 클 때 예외처리 발생")
     void shouldThrowExceptionWhenCarNameIsLongerThanFive() {
         assertThatThrownBy(() -> CarNameValidator.validate("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
+    @DisplayName("자동차 이름의 길이가 1보다 작을 때 예외처리 발생")
     void shouldThrowExceptionWhenCarNameIsEmpty() {
         assertThatThrownBy(() -> CarNameValidator.validate(""))
                 .isInstanceOf(IllegalArgumentException.class);
