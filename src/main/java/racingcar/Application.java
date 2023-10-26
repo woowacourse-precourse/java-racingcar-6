da7cea869f69c31d2905db163e69b0464459eec7
song-jae-wan <sjo9810@gmail.com>
2023-10-26 23:13:59 +0900

    Feat: Application에서 게임 실행부 추가 main 에서 객체 생성해 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import game.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame game = new RacingGame();
+        game.run();
     }
 }
