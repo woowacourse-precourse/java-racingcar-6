d8d9d3bb5bd83b680b830e946b625530505009bb
euics <pjh575157@gmail.com>
2023-10-26 20:17:48 +0900

    refactor(Exception): RaceException Magic Number Constant 변수로 수정

--- src/main/java/racingcar/exception/RaceException.java
+++ src/main/java/racingcar/exception/RaceException.java
@@ -1,9 +1,11 @@
 package racingcar.exception;
 
+import racingcar.util.Constants;
+
 public class RaceException {
 
     public static void validateCarName(String name) {
-        if (name == null || name.length() > 5 || name.length() < 1) {
+        if (name == null || name.length() > Constants.MAX_NAME_LENGTH || name.length() < Constants.MIN_NAME_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
