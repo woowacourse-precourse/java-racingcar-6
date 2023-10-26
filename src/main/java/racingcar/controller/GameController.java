297b7488098da2044caf2da0b45cdf7913c43690
suihub <qkdcrt@gmail.com>
2023-10-26 19:37:21 +0900

    <feat>: "Controller 패키지와 GameController 구현"
    도메인인 Car를 List로 담아 다루는 GameController 객체 생성.
    게임 로직상 재게임은 없으므로 List는 final을 통해 불변으로 만들었다.

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+    private final List<Car> carList = new ArrayList<>();
+    private int tryCount;
+
+    public void start() {
+
+    }
+}
