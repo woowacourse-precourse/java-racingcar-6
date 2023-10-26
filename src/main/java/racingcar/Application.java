5c116cce395949a291fd34bff674b25cc4f0a4cc
Octoping <mooner925@gmail.com>
2023-10-26 20:28:21 +0900

    feat: 어플리케이션 실행 시 게임이 실행되도록  main에 기능 연동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.domain.race.RaceChecker;
+import racingcar.domain.util.NumberGenerator;
+import racingcar.domain.util.RandomNumberGenerator;
+import racingcar.service.CarService;
+import racingcar.service.RacingCarGameService;
+import racingcar.usecase.RacingCarUseCase;
+import racingcar.view.RacingCarView;
+
 public class Application {
+    private static final int RANDOM_MIN_INCLUSIVE = 0;
+    private static final int RANDOM_MAX_INCLUSIVE = 9;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberGenerator generator = new RandomNumberGenerator(RANDOM_MIN_INCLUSIVE, RANDOM_MAX_INCLUSIVE);
+        RaceChecker raceChecker = new RaceChecker();
+
+        RacingCarUseCase racingCarUseCase = new RacingCarUseCase(
+                new CarService(generator),
+                new RacingCarGameService(raceChecker),
+                new RacingCarView(),
+                new RacingCarController()
+        );
+
+        racingCarUseCase.play();
     }
 }
