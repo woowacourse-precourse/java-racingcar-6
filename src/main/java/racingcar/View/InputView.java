d76f15747edfd9c5b7ebb5d8081fdb422f3727b0
Firedrago95 <yonghwa95@naver.com>
2023-10-26 17:55:23 +0900

    refactor : requesCarNames 메서드 유효성검사 하지않고 입력값 바로 반환하도록 변경 (테스트 편의성 목적)

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -8,11 +8,8 @@ public class InputView {
 
     public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public String requestCarNames() {
+    public static String requestCarNames() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
-        String inputedCarNames = Console.readLine();
-        Validator.validateCarNames(inputedCarNames);
-        String[] carNames = Parser.parseString(inputedCarNames);
-        return inputedCarNames;
+        return Console.readLine();
     }
 }
