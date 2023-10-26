2c95ab086484cd86d5837a8510af4687d9dbe687
kongji <kongji4092@gmail.com>
2023-10-26 21:27:32 +0900

    feat: 숫자 랜덤 생성 기능

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -1,9 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
 
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 0;
+
     public static int generateNumber() {
-        return 1;
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 
 }
