bca6418d1889b49a0fe05d3841eae485eb2084b5
liarreez <liarreez@gmail.com>
2023-10-26 21:31:00 +0900

    test : 자동차 입력 문구 출력 테스트 작성

---
+++ src/test/java/racingcar/input/InputTest.java
@@ -0,0 +1,35 @@
+package racingcar.input;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InputTest {
+    private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
+    @Test
+    @DisplayName("자동차 입력 문구 출력 테스트")
+    public void testInputCarsMessage() {
+
+        Input input = new Input();
+        input.printInputCarsMessage();
+        String output = byteArrayOutputStream.toString();
+
+        Assertions.assertThat(output.trim()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+}
