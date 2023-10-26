b069340684197431a9bfe3aa0d5c13ed13b15e19
heiler <threepebbles@naver.com>
2023-10-27 00:28:06 +0900

    feat: '실행 결과' 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import racingcar.model.CarList;
+
+public class OutputView {
+    public static void printResultMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printCurrentState(CarList carList) {
+        
+    }
+}
