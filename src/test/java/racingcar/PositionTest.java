35a6f750224b11ba7651c7b5bc7c0a4e49972269
shin5774 <babo5774@naver.com>
2023-10-26 20:21:54 +0900

    test: 위치 상태값을 작대기(-)로 변환해주는 기능 테스트코드 작성
    - 생성자를 통해 위치 상태값을 넣고 해당 위치 상태값을 작대기로 변환해주는 기능의 테스트코드 작성.

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PositionTest {
@@ -25,5 +26,13 @@ public class PositionTest {
         int beforePosition=position.position;
         position.moveForward();
         assertThat(position.position-1).isEqualTo(beforePosition);
+    }
+
+    @DisplayName("위치 값을 작대기(-)로 변환해주는 기능")
+    @ParameterizedTest
+    @CsvSource(value = {"0:''","1:-","2:--"},delimiter = ':')
+    void switchPositionToSlash_위치값_작대기_변환(int status,String expected){
+        Position position=new Position(status);
+        assertThat(position.switchPositionToSlash()).isEqualTo(expected);
     }
 }
