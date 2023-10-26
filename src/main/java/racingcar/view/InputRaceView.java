5231c3572af4634044b3416a436053add2e87147
euics <pjh575157@gmail.com>
2023-10-26 20:10:52 +0900

    refactor(View): Magic number 수정
    수정사항
    - readCarNames 출력값 CAR_NAME_PROMPT로 변경
    - readRaceCount 출력값 Constants.RACE_COUNT_PROMPT로 변경

--- src/main/java/racingcar/view/InputRaceView.java
+++ src/main/java/racingcar/view/InputRaceView.java
@@ -1,15 +1,16 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Constants;
 
 public class InputRaceView {
     public static String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(Constants.CAR_NAME_PROMPT);
         return Console.readLine();
     }
 
     public static int readRaceCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(Constants.RACE_COUNT_PROMPT);
         return Integer.parseInt(Console.readLine());
     }
 }
