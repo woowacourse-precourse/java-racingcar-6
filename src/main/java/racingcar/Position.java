acab9ea65a7b6afc34c9943ebc29964d52ed4708
shin5774 <babo5774@naver.com>
2023-10-26 20:34:21 +0900

    feat: 위치 상태값을 작대기(-)로 변환해주는 기능 구현
    - java 11에서 추가된 repeat 메서드를 통해 간단하게 구현함.

--- src/main/java/racingcar/Position.java
+++ src/main/java/racingcar/Position.java
@@ -3,9 +3,14 @@ package racingcar;
 public class Position {
     public int position;
     private static final int FORWARD_THRESHOLD=4;
+    private static final String SLASH="-";
 
     public Position(){
         position=0;
+    }
+
+    public Position(int status){
+        position=status;
     }
 
     public boolean checkValueIsOverFour(int value){
@@ -14,5 +19,9 @@ public class Position {
 
     public void moveForward(){
         position+=1;
+    }
+
+    public String switchPositionToSlash(){
+        return SLASH.repeat(position);
     }
 }
