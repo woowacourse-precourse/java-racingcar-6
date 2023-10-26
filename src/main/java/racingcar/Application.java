6174e2eed800cb3bb31a2f89990382d08bf9ec89
pyomin4973 <hyomin8918@naver.com>
2023-10-26 18:20:15 +0900

    feat(user-input): add user input feature for car's names
    Added new event:
    - user input feature for car's names
    - throw IllegalArgumentException if each name's length is over 5 characters

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> cars=inputCars();
+    }
+    public static List<String> inputCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> cars=new ArrayList<>();
+
+        String car=camp.nextstep.edu.missionutils.Console.readLine();
+        String checkCar[]=car.split(",");
+
+        for(int i=0;i<checkCar.length;i++){
+            if(checkCar[i].length()<=5)
+                cars.add(checkCar[i]);
+            else if (checkCar[i].length()>5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능하다");
+            }
+        }
+        return cars;
     }
 }
