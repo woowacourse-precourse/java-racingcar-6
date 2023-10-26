c8ec7ab8fbfca4ed6644d662a5a87e99e6d5f9a8
liarreez <liarreez@gmail.com>
2023-10-26 23:14:42 +0900

    feat : 최대 길이(5자)를 초과하는 자동차 이름 입력 시 에러 발생시키는 기능 구현
    최대 길이(5자)를 초과하는 자동차 이름이 입력값에 포함되어 있으면 IllegalArgumentException을 발생시키고 애플리케이션을 종료한다.

--- src/main/java/racingcar/input/Input.java
+++ src/main/java/racingcar/input/Input.java
@@ -1,6 +1,9 @@
 package racingcar.input;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Input {
     private final String INPUT_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -11,5 +14,19 @@ public class Input {
 
     private String inputCarsName() {
         return Console.readLine();
+    }
+
+    public List<String> parsedCarsName(String inputValue) {
+        List<String> cars = new ArrayList<>();
+        return cars = Arrays.asList(inputValue.split(","));
+    }
+
+    public boolean isValidCarNameLength(List<String> cars) {
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
     }
 }
