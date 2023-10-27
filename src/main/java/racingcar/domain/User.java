d002c1011157f34efd6b1c82023bd478cf2542a8
H-server <hani103201@naver.com>
2023-10-27 21:40:55 +0900

    fix: 시도할 횟수 할당

--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -6,7 +6,7 @@ public class User {
     public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ATTEMPTS_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String CAR_NAME_ERROR_MESSAGE = "이름이 5자를 초과하는 자동차가 있습니다.";
-    public static final String ATTEMPTS_INT_ERROR_MESSAGE = "입력한 회수가 정수가 아닙니다.";
+    public static final String ATTEMPTS_INT_ERROR_MESSAGE = "입력한 횟수가 정수가 아닙니다.";
     public static final String ATTEMPTS_NEGATIVE_ERROR_MESSAGE = "음의 정수를 입력하실 수 없습니다.";
     private String[] carsNameArray;
     private int attempts;
@@ -43,7 +43,7 @@ public class User {
 
     public void checkAttempts(String input) {
         try {
-            Integer.parseInt(input);
+            attempts = Integer.parseInt(input);
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException(ATTEMPTS_INT_ERROR_MESSAGE);
         }
