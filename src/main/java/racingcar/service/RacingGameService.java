61bcc0b69fbf5b299f53ddd9ac73e87bbe48ce30
Isaac Han <isaachan233@gmail.com>
2023-10-27 09:51:15 +0900

    feat: 실행 결과 도출 메서드 추가

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -2,8 +2,12 @@ package racingcar.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import racingcar.model.Attemps;
+import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.model.Winners;
 
@@ -11,20 +15,40 @@ public class RacingGameService {
     private final Cars cars;
     private final Attemps attemps;
     private final Winners winners;
-    
+
     public RacingGameService() {
         this.cars = new Cars();
         this.attemps = new Attemps();
         this.winners = new Winners();
     }
-    
-    public void addCars (String carNames) {
+
+    public void addCars(String carNames) {
         // TODO: 유효성 검사
         cars.addCars(new ArrayList<String>(Arrays.asList(carNames.split(","))));
     }
-    
-    public void setAttemps (String numberOfAttemps) {
+
+    public void setAttemps(String numberOfAttemps) {
         // TODO: 유효성 검사
         attemps.setNumber(Integer.valueOf(numberOfAttemps));
+    }
+
+    public List<Map<String, String>> getResultList() {
+        List<Map<String, String>> results = new ArrayList<>();
+        for (Car car : cars.getCarList()) {
+            Map<String, String> result = new HashMap<>();
+            car.moveForward();
+            result.put("carName", car.getName());
+            result.put("distance", car.getDistance());
+            results.add(result);
+        }
+        return results;
+    }
+
+    public boolean reaches() {
+        for (Car car : cars.getCarList()) {
+            if (car.getDistance().length() == attemps.getNumber())
+                return true;
+        }
+        return false;
     }
 }
