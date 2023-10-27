4064c343f68ce9d4a4d3f6de62084eaae1bed84b
1000zoo <cjswldn99@gmail.com>
2023-10-28 05:36:43 +0900

    test(study): Console도 되는 지 확인

--- src/test/java/study/MockingTest.java
+++ src/test/java/study/MockingTest.java
@@ -2,23 +2,27 @@ package study;
 
 import static org.mockito.Mockito.mockStatic;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-public class MockitoTest {
+public class MockingTest {
     private static MockedStatic<Randoms> random;
+    private static MockedStatic<Console> console;
 
     @BeforeAll
     public static void beforeAll() {
         random = mockStatic(Randoms.class);
+        console = mockStatic(Console.class);
     }
 
     @AfterAll
     public static void afterAll() {
         random.close();
+        console.close();
     }
 
     @Test
@@ -51,10 +55,25 @@ public class MockitoTest {
         test.randomPrint(); // 0
     }
 
+    @Test
+    void 입력도된다() {
+        console.when(Console::readLine).thenReturn("pobi");
+
+        InputTest test = new InputTest();
+        test.inputPrint();
+    }
+
     public static class RandomTest {
         public void randomPrint() {
             int num = Randoms.pickNumberInRange(1, 9);
             System.out.println("num = " + num);
+        }
+    }
+
+    public static class InputTest {
+        public void inputPrint() {
+            String input = Console.readLine();
+            System.out.println("input = " + input);
         }
     }
 }
