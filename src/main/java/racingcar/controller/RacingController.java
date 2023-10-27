0a1e4487e7d5a2ab57ac787246292e813a272122
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 01:32:40 +0900

    feat: 게임의 규칙에 맞게 컨트롤러 기능 추가
    입력을 통해 cars객체를만듦
    입력을 통해 시도 횟수를 가져옴
    입력 횟수동안 이동을 반복

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,9 @@
 package racingcar.controller;
 
+import racingcar.model.Cars;
+import racingcar.util.Converter;
+import racingcar.util.NumberGenerator;
+import racingcar.util.RandomNumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -8,12 +12,32 @@ public class RacingController {
     private final InputView inputView;
     private final OutputView outputView;
 
-    public RacingController(InputView inputView, OutputView outputView) {
+    public RacingController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void start() {
-        // TODO: 프로그램 구현
+        Cars cars = getCars();
+        int tryCount = getTryCount();
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        while (tryCount-- > 0) {
+            play(cars, numberGenerator);
+        }
+    }
+
+    private Cars getCars() {
+        outputView.printInputMessage();
+        String carNames = inputView.readInput();
+        return Cars.createFromCarNames(Converter.convertCommaSeparatedStringToList(carNames));
+    }
+
+    private int getTryCount() {
+        String count = inputView.readInput();
+        return Converter.convertStringToPositiveInt(count);
+    }
+
+    private void play(final Cars cars, final NumberGenerator numberGenerator) {
+        cars.moveAll(numberGenerator);
     }
 }
