8730bb389e73f7bef58cb7cd948885aba67470df
devhoya97 <diajbh123@gmail.com>
2023-10-27 13:45:23 +0900

    feat(racingcar) : 자동차 이름들 입력받는 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,2 +1,30 @@
-package racingcar;public class InputView {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
+
+public class InputView {
+    private static final String DELIMITER = ",";
+    public List<String> getCars() {
+        List<String> carStrings = new ArrayList<>();
+        String input = Console.readLine();
+        validateNullOrEmpty(input);
+        for (String carName : input.split(DELIMITER)) {
+            carStrings.add(carName.trim());
+        }
+        return carStrings;
+    }
+
+    public String getRounds() {
+        return Console.readLine();
+    }
+
+    private void validateNullOrEmpty(String string) {
+        if (string == null || string.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력이 널이거나 빈 문자열입니다.");
+        }
+    }
 }
