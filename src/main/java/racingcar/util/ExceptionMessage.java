3e92ad230b4a52c6ead5d10ce350fdf3f8ef46d6
Jiyoung Choi <12201817@inha.edu>
2023-10-27 20:35:34 +0900

    feat(ExceptionMessage) :  예외를 처리하는 enum 클래스 구현

--- src/main/java/racingcar/util/ExceptionMessage.java
+++ src/main/java/racingcar/util/ExceptionMessage.java
@@ -1,2 +1,19 @@
-package racingcar.util;public class Exception {
+package racingcar.util;
+
+public enum ExceptionMessage {
+    INPUT_NO_CAR_NAME_MESSAGE("경주할 자동차의 이름을 ,로 구분하여 입력해야 합니다."),
+    INPUT_WRONG_CAR_LENGTH_MESSAGE("최소한 1개 이상의 자동차 이름을 입력해야 합니다."),
+    INPUT_NO_COUNT_MESSAGE("시도 횟수를 입력해야 합니다."),
+    INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
+    INPUT_WRONG_RANGE_OF_NUMBER_MESSAGE("시도 횟수는 1이상이어야 합니다.");
+
+    private final String exceptionMessage;
+
+    ExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public void throwException(){
+        throw new IllegalArgumentException(exceptionMessage);
+    }
 }
