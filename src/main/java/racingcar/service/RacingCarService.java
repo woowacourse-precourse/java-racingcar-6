3eb79b0e314c38cdcfca06b08b01f678be4966fe
5jeong <wjdgns4385@naver.com>
2023-10-27 20:36:46 +0900

    [Feat] 우승자 판단기능 구현

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,8 +1,10 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import javax.print.DocFlavor.READER;
 import racingcar.model.Cars;
 import racingcar.util.ConverterUtil;
 import racingcar.util.RandomNumUtil;
@@ -10,18 +12,23 @@ import racingcar.util.RandomNumUtil;
 public class RacingCarService {
     private static final String FORWARD_MOVING = "-";
     private static Cars cars;
+
     private static HashMap<String, String> gameResult = new HashMap<>();
 
+    public HashMap<String, String> getGameResult() {
+        return gameResult;
+    }
+
     public void createCars(String carNames) {
-        List<String> carsList = ConverterUtil.covertStringToList(carNames);
-        cars = new Cars(carsList);
+        List<String> carsInfos = ConverterUtil.covertStringToList(carNames);
+        cars = new Cars(carsInfos);
         gameResult = cars.initCarsMap();
     }
 
-    public boolean judgeMove() {
-        int num = RandomNumUtil.generateRandomNum();
+    public boolean judgeMove(int randomNum) {
+//        int num = RandomNumUtil.generateRandomNum();
         boolean moving = false;
-        if (num >= 4) {
+        if (randomNum >= 4) {
             moving = true;
             return moving;
         }
@@ -29,17 +36,35 @@ public class RacingCarService {
     }
 
     public HashMap<String, String> judgeResult() {
+        int randomNum = RandomNumUtil.generateRandomNum();
 
         for (String car : gameResult.keySet()) {
-            if (judgeMove()) {
+            if (judgeMove(randomNum)) {
                 gameResult.put(car, gameResult.get(car) + FORWARD_MOVING);
             }
         }
         return gameResult;
     }
-    public List<String> judgeWinners(){
 
+    public int mostMovementCount() {
+        int maxLen = Integer.MIN_VALUE;
+        for (String car : gameResult.keySet()) {
+            if (maxLen < gameResult.get(car).length()) {
+                maxLen = gameResult.get(car).length();
+            }
+        }
+        return maxLen;
     }
 
+    public List<String> judgeWinners() {
+        List<String> winners = new ArrayList<>();
+        int mostMovementCount = mostMovementCount();
+        for (String car : gameResult.keySet()) {
+            if (mostMovementCount == gameResult.get(car).length()) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 
 }
