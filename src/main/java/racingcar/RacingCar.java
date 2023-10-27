f055d3238b597e1291c027c4c4d2b6199ef8abe5
이지윤 <02leejiyun@naver.com>
2023-10-28 02:12:50 +0900

    feat: 게임 진행 횟수 입력 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -8,12 +8,21 @@ import java.util.List;
 public class RacingCar {
     public void playGame() {
         List<String> carNames = getCarNames();
-//        System.out.println(carNames);
+        int num = getNumber();
+        System.out.println(carNames);
+        System.out.println(num);
     }
 
     private List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
         String[] nameArr = names.split(",");
         return Arrays.asList((nameArr));
+    }
+
+    private int getNumber() throws IllegalArgumentException {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String numStr = Console.readLine();
+        return Integer.parseInt(numStr);
     }
 }
