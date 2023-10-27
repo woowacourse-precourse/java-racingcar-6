f573462860c7f970c8a8d8ff0d966ea69c4a20f9
TaeHye0n <thk001229@gmail.com>
2023-10-27 20:52:24 +0900

    refactor(validator): 자동차 이름의 길이 검증  - 자동차 이름의 길이 검증은 자동차 객체와 연관된 곳에서 처리하기 위해 일급컬렉션 클래스 생성 시점에서 검증

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,7 +1,5 @@
 package racingcar.validator;
 
-import static racingcar.enums.Constant.MAX_NAME_LENGTH;
-import static racingcar.enums.ExceptionMessage.EXCEED_MAX_LENGTH_MESSAGE;
 import static racingcar.enums.ExceptionMessage.NON_NUMERIC_INPUT_ERROR_MESSAGE;
 import static racingcar.enums.ExceptionMessage.NULL_OR_EMPTY_INPUT_MESSAGE;
 
@@ -13,12 +11,6 @@ public class InputValidator {
         }
     }
 
-    public static void validateNameLength(String input) {
-        if (isNameExceedsMaxLength(input)) {
-            throw new IllegalArgumentException(EXCEED_MAX_LENGTH_MESSAGE.getMessage());
-        }
-    }
-
     public static void validateTryCount(String input) {
         for (char c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
@@ -27,11 +19,4 @@ public class InputValidator {
         }
     }
 
-    private static boolean isNameExceedsMaxLength(String name) {
-        if (name.length() > MAX_NAME_LENGTH.getConstant()) {
-            return true;
-        }
-
-        return false;
-    }
 }
