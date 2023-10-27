868b94db135882551eac2c51971c54bc5edc709c
Isaac Han <isaachan233@gmail.com>
2023-10-27 21:49:28 +0900

    refactor: 유효성 검사하는 부분을 따로 관리하여 가독성을 높이고 데이터를 받는 것에만 집중

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,44 +1,19 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.Validator;
 
 public class InputView {
+    private Validator validator = new Validator();
     public String carNameInput() {
         String carNames = Console.readLine();
-        if (carNames == null)
-            throw new IllegalArgumentException();
-        if (carNames.trim().isEmpty())
-            throw new IllegalArgumentException();
-        if (carNames.startsWith(","))
-            throw new IllegalArgumentException();
-        if (carNames.endsWith(","))
-            throw new IllegalArgumentException();
-
-        String[] carNamesArray = carNames.split(",");
-
-        for (String carName : carNamesArray) {
-            if (carName.length() > 5)
-                throw new IllegalArgumentException();
-        }
+        validator.validateCarNameInput(carNames);
         return carNames;
     }
 
     public String numberOfAttempsInput() {
-        try {
-            String numberOfAttemps = Console.readLine();
-            if (numberOfAttemps == null)
-                throw new IllegalArgumentException();
-            if (numberOfAttemps.trim().isEmpty())
-                throw new IllegalArgumentException();
-            if (numberOfAttemps.matches("^[^0-9]*$"))
-                throw new IllegalArgumentException();
-            int number = Integer.parseInt(numberOfAttemps);
-            
-            if (number < 1)
-                throw new IllegalArgumentException();
-            return Integer.toString(number);
-        } catch (NumberFormatException nfe) {
-            throw new IllegalArgumentException();
-        }
+        String numberOfAttemps = Console.readLine();
+        validator.validatenumberOfAttempsInput(numberOfAttemps);
+        return numberOfAttemps;
     }
 }
