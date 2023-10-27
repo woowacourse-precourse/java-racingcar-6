5f463f78545e62af8a9857256d95cc0a6c4a790c
Reddyong <d0209hong@naver.com>
2023-10-27 14:43:51 +0900

    feat : create carRace(), saveResult(), printResult()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     private static String carString;
@@ -10,9 +11,9 @@ public class Application {
         init();     // 자동차 이름 입력, 시도할 횟수 입력
 
         String[] carArray = changeToCarArr();       // 자동차 이름 분리 저장할 배열
+        String[] raceResultArr = new String[carArray.length];        // 자동차 경주 실행 결과 저장할 배열
 
-        carRace(carArray);
-
+        carRace(carArray, raceResultArr);
     }
 
     public static void init() {
@@ -25,6 +26,7 @@ public class Application {
         System.out.println("시도할 횟수는 몇회인가요?");
         String tryString = Console.readLine();
         tryNum = Integer.parseInt(tryString);
+        System.out.println();
     }
 
     public static boolean checkCarLength(String carString) {
@@ -42,9 +44,44 @@ public class Application {
         return carString.split(",");
     }
 
-    public static void carRace(String[] carArray) {
+    public static void carRace(String[] carArray, String[] raceResultArr) {
+        System.out.println("실행 결과");
         for (int i = 0; i < tryNum; i++) {
-
+            saveResult(raceResultArr);
+            printResult(carArray, raceResultArr);
+            System.out.println();
         }
+    }
+
+    public static void printResult(String[] carArray, String[] raceResultArr) {
+        for (int j = 0; j < raceResultArr.length; j++) {
+            if (raceResultArr[j] == null) {
+                raceResultArr[j] = "";
+            }
+            System.out.println(carArray[j] + " : " + raceResultArr[j]);
+        }
+    }
+
+    public static void saveResult(String[] raceResultArr) {
+        for (int i = 0; i < raceResultArr.length; i++) {
+            int car = Randoms.pickNumberInRange(0, 9);
+
+            if (!checkForward(car)) {
+                continue;
+            }
+
+            if (raceResultArr[i] == null) {
+                raceResultArr[i] = "-";
+            } else {
+                raceResultArr[i] += "-";
+            }
+        }
+    }
+
+    public static boolean checkForward(int car) {
+        if (car < 4) {
+            return false;
+        }
+        return true;
     }
 }
