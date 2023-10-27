8f3c600312cbe133646464f391f84e02f22348aa
Infikei <hckhong@naver.com>
2023-10-27 19:33:52 +0900

    feat: 경주할 자동차 이름 입력 구현
    - 경주할 자동차 이름을 입력하라는 안내 문구를 출력
    - 경주할 자동차 이름을 입력받고 문자열로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        String carNames = inputView.inputCarNames();
     }
 }
