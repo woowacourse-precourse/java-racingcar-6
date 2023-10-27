a14245d6b3f937e232874683a5e5e09ebb066252
pjm2571 <pjm2571@naver.com>
2023-10-27 20:22:05 +0900

    첫번째 기능 구현 [입력 라인을 공백 기준으로 분류, 5글자 예외 처리]

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
 public class Application {
+
+    private static void string_length_illegal_check(String input_string) {
+        if (input_string.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    private static List<String> input_to_namelist(String input_line) {
+        StringTokenizer st = new StringTokenizer(input_line, ",");
+
+        List<String> name_list = new ArrayList<String>();
+
+        while (st.hasMoreTokens()) {
+            String input_string = st.nextToken();
+
+            string_length_illegal_check(input_string);
+
+            name_list.add(input_string);
+        }
+
+        return name_list;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input_line = readLine();
+
+        List<String> name_list = input_to_namelist(input_line);
+
     }
 }
