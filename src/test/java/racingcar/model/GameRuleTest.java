fc0767a805cfcd225ddf82025bfc2f61e6f1fbc9
Maljaaa <Maljaaa@github.com>
2023-10-27 16:46:38 +0900

    test(GameRule): 자동차 이름 입력, 시도 횟수 입력 테스트

--- src/test/java/racingcar/model/GameRuleTest.java
+++ src/test/java/racingcar/model/GameRuleTest.java
@@ -1,44 +1,15 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Test;
-import racingcar.utils.NumberGenerator;
-import racingcar.utils.StubNumberGenerator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameRuleTest {
-
-    @Test
-    public void 전진_가능_테스트() throws Exception {
-        // given
-        NumberGenerator generator = new StubNumberGenerator(5);
-        GameRule gameRule = new GameRule(generator);
-
-        // when
-        boolean result = gameRule.isOkToMove();
-
-        // then
-        assertTrue(result, "자동차가 전진할 수 없습니다!");
-    }
-
-    @Test
-    public void 전진_불가능_테스트() throws Exception {
-        //given
-        NumberGenerator generator = new StubNumberGenerator(3);
-        GameRule gameRule = new GameRule(generator);
-
-        // when
-        boolean result = gameRule.isOkToMove();
-
-        // then
-        assertFalse(result, "자동차가 멈추지 않습니다!");
-    }
     
     @Test
     public void 자동차_이름_유효성_테스트() throws Exception {
         // given
-        NumberGenerator generator = new StubNumberGenerator(5);
-        GameRule gameRule = new GameRule(generator);
+        GameRule gameRule = new GameRule();
 
         // when & then
         assertDoesNotThrow(() -> gameRule.isOkToUseCarName("pobi"));
@@ -48,8 +19,7 @@ class GameRuleTest {
     @Test
     public void 시도_횟수_유효성_테스트() throws Exception {
         // given
-        NumberGenerator generator = new StubNumberGenerator(5);
-        GameRule gameRule = new GameRule(generator);
+        GameRule gameRule = new GameRule();
 
         // when & then
         assertTrue(gameRule.isOkToUseAttemptCount(3));
