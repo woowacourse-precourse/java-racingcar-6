a7524d02636074c9ed2159b7f10182edc4a2742d
waveofmymind <sjun0913@gmail.com>
2023-10-28 02:28:33 +0900

    refactor: CarFactory에서 Cars를 생성하도록 변경한다.

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -1,13 +1,10 @@
 package racingcar.domain;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CarFactory {
 
-    public List<Car> createCars(List<String> carNames) {
-        return carNames.stream()
-                .map(Car::new)
-                .collect(Collectors.toList());
+    public static Cars createCars(List<String> carNames) {
+        return new Cars(carNames);
     }
 }
