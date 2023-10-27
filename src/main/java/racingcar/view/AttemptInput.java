2c5c159cbeda095f5f03457b38bfa03d885a9923
1000zoo <cjswldn99@gmail.com>
2023-10-27 23:26:12 +0900

    fix(view): Input 관련 메소드 수정
    - 생성자에서 안내문구 출력
    - Input에 readLine 메소드 추가
    - AttemptInput의 readLine의 반환타입 String으로 변경

--- src/main/java/racingcar/view/AttemptInput.java
+++ src/main/java/racingcar/view/AttemptInput.java
@@ -1,16 +1,18 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.AttemptNumber;
 import racingcar.validator.AttemptsInputValidator;
 import racingcar.validator.Validator;
 
-public class AttemptInput implements Input{
+public class AttemptInput implements Input {
+
+    private static final String ASK_ATTEMPT_TIMES = "시도할 회수는 몇회인가요?";
 
     private final Validator validator = new AttemptsInputValidator();
     private final String attempts;
 
     public AttemptInput() {
+        Output.printMessage(ASK_ATTEMPT_TIMES);
         attempts = Console.readLine();
         validate();
     }
@@ -19,7 +21,7 @@ public class AttemptInput implements Input{
         validator.validate(attempts);
     }
 
-    public AttemptNumber readLine() {
-        return new AttemptNumber(Integer.parseInt(attempts));
+    public String readLine() {
+        return attempts;
     }
 }
