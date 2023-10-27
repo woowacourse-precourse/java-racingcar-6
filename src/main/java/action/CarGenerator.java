4e445b455565955ab8a00527ab40233431d706df
PJH <parkjh0858@gmail.com>
2023-10-27 12:12:52 +0900

    feat: 입력 받은 문자열로 부터 자동차 리스트 생성 기능 추가

---
+++ src/main/java/action/CarGenerator.java
@@ -0,0 +1,34 @@
+package action;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CarGenerator {
+    private static final int LENGTH = 5;
+    private String[] carNameArray;
+    public CarGenerator(String carString) {
+        carNameArray = carSplit(carString);
+    }
+
+    public String[] carSplit(String carString) {
+        return carString.split(",");
+    }
+
+    public List<Car> makeCars() {
+        List<Car> carList = new LinkedList<>();
+
+        for (String car : carNameArray) {
+            checkNameLength(car);
+            carList.add(new Car(car));
+        }
+
+        return carList;
+    }
+
+    public void checkNameLength(String carName) throws IllegalArgumentException {
+        if (carName.length() > LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
