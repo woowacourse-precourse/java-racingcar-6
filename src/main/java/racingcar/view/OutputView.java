48a571dd14888cfb2671d129c9a87f5a5b6f530a
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 01:33:20 +0900

    feat: 첫 출력 문구 추가
    경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)라는 문구 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
 public class OutputView {
+
+    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
+
+    public void printInputMessage() {
+        System.out.println(INPUT_MESSAGE);
+    }
 }
