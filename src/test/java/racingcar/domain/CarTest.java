8364f7c4d21112545f287a459c222fc7c21d561a
Dowon <3120kang@gmail.com>
2023-10-27 20:36:35 +0900

    Test:(Car): 자동차의 전진 기능 테스트 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,7 +1,49 @@
 package racingcar.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    @Test
+    @DisplayName("자동차는 한칸씩 전진해야 한다.")
+    void 전진_테스트(){
+        //given
+        Car car1=new Car("car1");
+        String locate="";
+
+        //when
+        for(int i=0; i<5;i++){
+            locate=car1.moveForward();
+        }
+
+        //then
+        Assertions.assertThat(locate).isEqualTo("-----");
+    }
+
+    @Test
+    @DisplayName("차가 두대 이상일 때, 전진 테스트")
+    void 전진_테스트_2(){
+        //given
+        Car car1=new Car("car1");
+        String locate1="";
+        Car car2=new Car("car2");
+        String locate2="";
+
+        //when
+        for(int i=0;i<3;i++){
+            locate1=car1.moveForward();
+        }
+        for(int j=0;j<5;j++){
+            locate2=car2.moveForward();
+        }
+
+        //then
+        Assertions.assertThat(locate1.equals("---")).isEqualTo(true);
+        Assertions.assertThat(locate2.equals("-----")).isEqualTo(true);
+
+    }
 
 }
