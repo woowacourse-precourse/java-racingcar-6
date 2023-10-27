2c92f0ab5f3ca534b6b718a301f28a10bf08ae21
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 17:27:32 +0900

    [feat] 사용자로부터 이름들 입력 받는 메서드 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,20 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    public List<String> inputNames() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String names = Console.readLine();
+        List<String> nameList = Arrays.stream(names
+                        .split(","))
+                        .toList();
+
+
+        return nameList;
+    }
 }
