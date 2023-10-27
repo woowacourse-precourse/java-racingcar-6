4152df6755a3e6f1d90193047a873c287d8c0ccd
2020006026 <jms0538@hanyang.ac.kr>
2023-10-27 17:32:04 +0900

    feat: add a start view

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,9 +1,16 @@
 package racingcar.controller;
 
+import racingcar.model.RacingCarGameService;
+import racingcar.view.RacingCarGameView;
+
 public class RacingCarGameController {
 
-    public RacingCarGameController() {
+    private RacingCarGameView racingCarGameView;
+    private RacingCarGameService racingCarGameService;
 
+    public RacingCarGameController() {
+        racingCarGameView = new RacingCarGameView();
+        racingCarGameService = new RacingCarGameService();
     }
 
     public void gameStart() {
--- src/main/java/racingcar/view/RacingCarGameView.java
+++ src/main/java/racingcar/view/RacingCarGameView.java
@@ -1,10 +1,14 @@
 package racingcar.view;
 
-import racingcar.controller.RacingCarGameController;
-
 public class RacingCarGameView {
+
+    private static final String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public RacingCarGameView() {
 
+    }
+
+    public void startView() {
+        System.out.println(START);
     }
 }
