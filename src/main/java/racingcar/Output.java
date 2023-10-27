bb46fb922a0058c52980e8641700a0f531ae866d
yubin <dbqls2821@gmail.com>
2023-10-27 23:53:45 +0900

    feat: 출력을 책임지는 Output 클래스 추가

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+final class Output {
+
+    private Output() {
+    }
+
+    static void printInputNames() {
+        System.out.println(OutputMessage.INPUT_NAMES.message);
+    }
+
+    static void printResult() {
+        System.out.print(OutputMessage.RESULT.message);
+    }
+
+    static void printGameResult(final String result) {
+        System.out.println(result);
+        System.out.println();
+    }
+
+    //TODO String말고 Winner 객체로?
+    static void printWinner(String winner) {
+        System.out.println(OutputMessage.WINNER.message + winner);
+    }
+}
