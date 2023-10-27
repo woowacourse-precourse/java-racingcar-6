788b0fb4979834ffc8f3bd09fd72273d9aee8be3
kkw2758 <dtc3229@gmail.com>
2023-10-27 21:26:09 +0900

    fix : Cars의 generateCarList 메서드 버그 수정
    carList를 새로 만드는 기능을 생각하고 구현하였는데 인스턴스 변수에 값을 추가하는 문제 확인(수정 완료)

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -33,7 +33,7 @@ public class Cars {
         return Collections.max(carPositionList);
     }
 
-    public int size(){
+    public int size() {
         return carList.size();
     }
 
@@ -44,7 +44,7 @@ public class Cars {
     private List<Car> generateCarList(String[] cars) {
         List<Car> carList = new ArrayList<>();
         for (String car : cars) {
-            this.carList.add(new Car(car));
+            carList.add(new Car(car));
         }
         return carList;
     }
@@ -54,6 +54,7 @@ public class Cars {
             throw new IllegalArgumentException("중복된 자동차 이름을 입력하였습니다.");
         }
     }
+
     private boolean hasDuplicatedCarName(String[] cars) {
         HashSet<String> carSet = new HashSet<String>();
         Collections.addAll(carSet, cars);
