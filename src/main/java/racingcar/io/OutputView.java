587ef442d4161a457bba5fec3d8130035be27174
김승진 <ohksj77@gmail.com>
2023-10-27 21:28:25 +0900

    feat(OutputView): 자동차 이름 입력 요청 메시지 출력 기능 구현

---
+++ src/main/java/racingcar/io/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.io;
+
+import racingcar.constant.ProgressMessage;
+
+public class OutputView {
+
+    public void printRacingCarNamesRequest() {
+        System.out.println(ProgressMessage.RACING_CAR_NAMES_REQUEST.toValue());
+    }
+}
