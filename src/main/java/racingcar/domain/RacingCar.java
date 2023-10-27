49ecf4382158f506256f560c8c35c1370fc5755f
김승진 <ohksj77@gmail.com>
2023-10-27 23:51:23 +0900

    feat(RacingCar): 상태에 따라 움직일지 결정하여 결과를 저장 기능 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,20 +1,31 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.constant.ErrorMessage;
+import racingcar.constant.MoveStatus;
 
 public final class RacingCar {
 
     private static final int MAX_NAME_LENGTH = 5;
     private final String name;
+    private final List<MoveStatus> moveStatuses;
 
     public RacingCar(final String name) {
         validate(name);
         this.name = name;
+        this.moveStatuses = new ArrayList<>();
     }
 
     private void validate(final String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_LENGTH.toValue());
+        }
+    }
+
+    public void addMoveStatusIfMove(final MoveStatus moveStatus) {
+        if (moveStatus.isMove()) {
+            moveStatuses.add(moveStatus);
         }
     }
 }
