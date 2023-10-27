4d85ad4805e56202d68a982d8085bd5769bc7b52
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 02:16:46 +0900

    feat: 우승자를 가져오는 메서드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -7,7 +7,7 @@ import racingcar.util.NumberGenerator;
 
 public class Cars {
 
-    private static final int DEFAULT_POSITION = 0;
+    private static final int INITIAL_POSITION = 0;
 
     private final List<Car> cars;
 
@@ -18,13 +18,28 @@ public class Cars {
     public static Cars createFromCarNames(final List<String> carsName) {
         List<Car> cars = new ArrayList<>();
         for (final String carName : carsName) {
-            cars.add(new Car(new Name(carName), new Position(DEFAULT_POSITION)));
+            cars.add(new Car(new Name(carName), new Position(INITIAL_POSITION)));
         }
         return new Cars(cars);
     }
 
     public void moveAll(final NumberGenerator generator) {
         cars.forEach(car -> car.move(generator));
+    }
+
+    public List<String> getWinnerNames() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .max(Integer::compareTo)
+                .orElse(INITIAL_POSITION);
     }
 
     public List<Car> getCars() {
