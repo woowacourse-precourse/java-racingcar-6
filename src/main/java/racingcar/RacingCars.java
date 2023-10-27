e396a0c885f5fa3521a7faf60f43afc6502aeeb5
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2023-10-27 23:32:09 +0900

    feat(RacingCars) : 우승자 목록 생성 함수 구현

--- src/main/java/racingcar/RacingCars.java
+++ src/main/java/racingcar/RacingCars.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.*;
 
+import static java.util.Collections.list;
+import static java.util.Collections.max;
+
 public class RacingCars {
     public ArrayList<Car> cars = new ArrayList<>();
 
@@ -11,5 +14,32 @@ public class RacingCars {
         for (String car : cars) {
             this.cars.add(new Car(car));
         }
+    }
+
+    public ArrayList<Car> RoundResult() {
+        for (Car car : cars) {
+            car.go();
+        }
+        return cars;
+    }
+
+    public ArrayList<Integer> getPosition() {
+        ArrayList<Integer> position = new ArrayList<>();
+        for (Car car : cars) {
+            position.add(car.getPosition());
+        }
+        return position;
+    }
+
+    public String[] getWinners() {
+        int winnerPosition = maxDistance(getPosition());
+        return cars
+                .stream().filter(car -> car.getPosition() == winnerPosition)
+                .map(Car::getName)
+                .toArray(String[]::new);
+    }
+
+    public static int maxDistance(ArrayList<Integer> position){
+        return Collections.max(position);
     }
 }
