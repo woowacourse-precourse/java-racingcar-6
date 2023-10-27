670557b1690969f729b39dd4356f40fab9fb1286
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-28 01:11:37 +0900

    feat(viewer): 시도 횟수를 입력받고 검증하는 메서드 추가
    - 라운드 시도 횟수 입력
    - 횟수가 숫자인지 검증

--- src/main/java/racingcar/Viewer.java
+++ src/main/java/racingcar/Viewer.java
@@ -10,17 +10,23 @@ public class Viewer {
     private final String CAR_NAME_DELIMITER = ",";
     private final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이하만 가능합니다.";
     private final String INVALID_CAR_NAME_DELIMITER = "쉼표(,)로 구분해주세요.";
+    private final Integer MIN_TRY_COUNT = 1;
+    private final String TRY_COUNT_ONLY_NUMBER = "시도 횟수는 숫자여야 합니다.";
+    private final String MINIMUM_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
 
     public List<String> inputCarNames() {
-        List<String> carNameList = Arrays
-                .stream(readLine().split(CAR_NAME_DELIMITER))
-                .toList();
-        validateCarNameList(carNameList);
+        List<String> carNameList = Arrays.asList(readLine().split(CAR_NAME_DELIMITER));
+        carNameList.forEach(this::validateCarName);
         return carNameList;
     }
-
-    private void validateCarNameList(List<String> carNames) {
-        carNames.forEach(this::validateCarName);
+    public int inputTryCount() {
+        try {
+            int tryCount = Integer.parseInt(readLine());
+            validateTryCount(tryCount);
+            return tryCount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(TRY_COUNT_ONLY_NUMBER);
+        }
     }
 
     private void validateCarName(String carName) {
@@ -38,4 +44,10 @@ public class Viewer {
             throw new IllegalArgumentException(INVALID_CAR_NAME_DELIMITER);
         }
     }
+    private void validateTryCount(int tryCount) {
+        if (tryCount < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException(MINIMUM_TRY_COUNT);
+        }
+    }
+
 }
