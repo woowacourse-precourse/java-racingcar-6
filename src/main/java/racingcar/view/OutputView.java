cf2539873a94eb65c2e06e4ff87a5089e1fc86f0
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-27 11:29:34 +0900

    feat : View, Input Output 구성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
+    public void printFirstResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printWinnerResult(String winner) {
+        System.out.println("최종 우승자 : " + winner);
+    }
+
+    public void printJointWinnerResult(List<String> jointWinner) {
+        String result = String.join(", ", jointWinner);
+        System.out.println("최종 우승자 : " + result);
+    }
 }
