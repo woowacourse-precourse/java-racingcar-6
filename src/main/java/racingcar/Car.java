b59e61036150b58f23a57374ab4bed11275eb79e
ldw <leehan02@hanyang.ac.kr>
2023-10-27 21:43:51 +0900

    fix(racingcar): 자동차 전진 여부에 따른 출력 기능 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,24 +1,23 @@
 package racingcar;
 
 public class Car {
-
     private final String name;
     private int countMove;
-
     private static final String MOVE = "-";
+
+    private String totalMove = "";
 
     public Car(String name) {
         this.name = name;
     }
 
     public void Move(boolean isMove) {
+        System.out.print(name + " : ");
         if (isMove) {
-            System.out.print(name + " : ");
-            for (int i = 0; i < countMove; i++) {
-                System.out.print(MOVE);
-            }
-            System.out.println();
             countMove++;
+            totalMove += MOVE;
         }
+
+        System.out.println(totalMove);
     }
 }
