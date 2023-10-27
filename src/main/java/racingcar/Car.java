e11942c8ccacbbb6719a50dac6c1d9619d7ae204
Bisi3asi <kueeng8008@naver.com>
2023-10-27 10:47:01 +0900

    feat : Car(자동차 객체 정의)
    1. 필드 : 이름, 이동 누적 거리(점수)
    2. 생성자 : 이름을 통한 생성
    3. Getter : 이름, 이동 누적 거리

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,18 @@
 package racingcar;
 
 public class Car {
-    private String name;
-    private int distance;
+    private final String name;
+    private int score; // 이동 누적 거리 == 점수
 
     Car(String name){
         this.name = name;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    int getScore(){
+        return this.score;
     }
 }
