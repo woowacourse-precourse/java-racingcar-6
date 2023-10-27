db8726d6589564f9e5809e03e4a76a2610c51f99
TaeHye0n <thk001229@gmail.com>
2023-10-27 16:47:18 +0900

    style(utils): 메서드 수정  - 가독성을 위해 스트림 및 컬렉터 제거

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -2,13 +2,10 @@ package racingcar.utils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Utils {
 
     public static List<String> parseNames(String input) {
-        String[] parseArray = input.split(",");
-        return Arrays.stream(parseArray).
-                collect(Collectors.toList());
+        return Arrays.asList(input.split(","));
     }
 }
