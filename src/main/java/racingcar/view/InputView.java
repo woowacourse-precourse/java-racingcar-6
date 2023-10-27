a09fd84b9bb1e545ca10de89e3a019a6154648e7
Infikei <hckhong@naver.com>
2023-10-27 20:37:03 +0900

    feat: 전진 시도할 횟수 입력 구현
    - 전진 시도할 횟수를 입력하라는 안내 문구를 출력
    - 전진 시도할 횟수를 입력받고 문자열로 리턴

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String RACING_CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MOVING_NUMBER_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String inputRacingCarNames() {
         System.out.println(RACING_CAR_NAMES_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputMovingNumber() {
+        System.out.println(MOVING_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
     }
 }
