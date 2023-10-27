c34efcea3546973197c6b581e2e2d694e6bf7470
yeseong31 <yeseong31@naver.com>
2023-10-27 22:04:11 +0900

    refactor(MessageService): 메시지 출력 포맷 수정

--- src/main/java/racingcar/service/MessageService.java
+++ src/main/java/racingcar/service/MessageService.java
@@ -6,8 +6,8 @@ public class MessageService {
 
     private static final String START_GAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_ATTEMPT = "시도할 회수는 몇회인가요?";
-    private static final String GAME_RESULT = "\n실행 결과";
-    private static final String FINAL_WINNER = "최종 우승자 : ";
+    private static final String GAME_RESULT = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자";
 
     public void printStartGame() {
         println(START_GAME);
@@ -18,18 +18,20 @@ public class MessageService {
     }
 
     public void printGameResult() {
-        println(GAME_RESULT);
+        String message = String.format("\n%s", GAME_RESULT);
+        println(message);
     }
 
-    public void printFinalWinner() {
-        print(FINAL_WINNER);
+    public void printFinalWinner(String winner) {
+        String message = String.format("%s : %s", FINAL_WINNER, winner);
+        print(message);
     }
 
     public void printRank(Cars cars) {
         for (String carInfo : cars.getCurrentState()) {
             println(carInfo);
         }
-        print("\n");
+        println();
     }
 
     private void print(String message) {
@@ -38,5 +40,9 @@ public class MessageService {
 
     private void println(String message) {
         System.out.println(message);
+    }
+
+    private void println() {
+        println("");
     }
 }
