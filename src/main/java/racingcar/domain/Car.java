36c747b1ebc29ad01ca03447816f2beda11f2665
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 13:48:32 +0900

    feat : 자동차 전진 후진을 결정할 랜덤한 수 생성 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,17 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private int getCondition() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     @Override
