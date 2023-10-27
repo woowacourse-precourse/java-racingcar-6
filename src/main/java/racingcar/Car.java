597c332e23b8ff32324118a82be76036b09268c8
Juho Lee <ailiartsuaL2@gmail.com>
2023-10-27 17:29:30 +0900

    feat: car domain 추가
    car domain 추가 및 cars 컬렉션 타입 변경

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+public final class Car {
+    private String name;
+    private int distance;
+
+    public static Car create(String name){
+        Car car = new Car();
+        car.name = name;
+        car.distance = 0;
+        return car;
+    }
+    public void run(){
+        this.distance ++;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getDistance() {
+        return distance;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+}
