1b6d5c088314205687431fbe9b2d58356aab0ac4
Roista57 <Roista57@gmail.com>
2023-10-27 13:14:08 +0900

    feat: 입력받는 자동차들의 이름을 ,로 나누어 구분하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,16 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 public class Application {
+    static ArrayList<String> carNameSplit(String racingCarNames){
+        ArrayList<String> nameList = new ArrayList<>();
+        for(String carName : racingCarNames.split(",")){
+            nameList.add(carName);
+        }
+        return nameList;
+    }
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String racingCarNames = Console.readLine();
