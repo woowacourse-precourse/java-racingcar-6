612ebb3f7d58dbc3d01287fdab57b40cff937558
Juhyorim <joohr1234@gmail.com>
2023-10-27 22:46:57 +0900

    feat(racingcar): 각 자동차의 현재까지 이동상태를 알아내는 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,15 +4,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private String name;
+    private int move;
 
     public Car(String name) {
         this.name = name;
+        this.move = 0;
     }
 
     public void moveOrStop() {
         if (isMove()) {
-
+            move++;
         }
+    }
+
+    public int getPath() {
+        return move;
     }
 
     private boolean isMove() {
