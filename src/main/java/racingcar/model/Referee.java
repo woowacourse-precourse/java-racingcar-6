eeafa22d992745c84476a416aa15b706394d9304
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-27 18:54:02 +0900

    feat : Model Referee 구현 우승자 판별 기능

--- src/main/java/racingcar/model/Referee.java
+++ src/main/java/racingcar/model/Referee.java
@@ -1,4 +1,28 @@
 package racingcar.model;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Referee {
+    private List<Car> winner;
+    private int winnerStep;
+
+    public void findWinnerStep(RacingCars cars) {
+        for (Car car : cars.getAllCars()) {
+            if (winnerStep < car.getCurrentPosition()) {
+                winnerStep = car.getCurrentPosition();
+            }
+        }
+    }
+
+    public List<Car> winnerResult(RacingCars cars) {
+        for (Car car : cars.getAllCars()) {
+            if (winnerStep == car.getCurrentPosition()) {
+                winner.add(car);
+            }
+        }
+        return Collections.unmodifiableList(winner);
+    }
+
+
 }
