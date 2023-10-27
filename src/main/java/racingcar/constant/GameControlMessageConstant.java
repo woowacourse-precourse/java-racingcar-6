ea703ce6b2bd620046ea3209216c48299fd15717
FL <refrain2k@gmail.com>
2023-10-27 18:53:30 +0900

    feat: 게임 제어 메시지 상수 클래스 작성

---
+++ src/main/java/racingcar/constant/GameControlMessageConstant.java
@@ -0,0 +1,7 @@
+package racingcar.constant;
+
+public class GameControlMessageConstant {
+
+    public static final String NEED_CAR_NAMES_INPUT_MESSAGE = "\"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\"";
+    public static final String NEED_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+}
