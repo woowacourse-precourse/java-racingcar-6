6dc97eb0bc5eef0e49cf3668af4664a62b1278db
scarlet0o0 <ghd7673@naver.com>
2023-10-27 16:55:27 +0900

    Feat : Name 객체 생성 후 구현
    name 객체는 사용자로부터 입력받은 이름이 5를 초과하는지 체크한다.

--- src/main/java/racingcar/car/Name.java
+++ src/main/java/racingcar/car/Name.java
@@ -1,5 +1,18 @@
 package racingcar.car;
 
 public class Name {
+    private final static int NAME_SIZE = 5;
+    private final String  value;
+
+    public Name(String name) {
+        if (name.length() > NAME_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        this.value = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
 
 }
