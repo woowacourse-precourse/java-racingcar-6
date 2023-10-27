6cca9eb45231ea1d47b93d21ce685570e13a0c49
kim <hwiyul_g@daum.net>
2023-10-27 23:26:32 +0900

    Test : 시도횟수 입력에 대한 validate 테스트 케이스 추가

--- src/test/java/racingcar/utils/InputValidatorTest.java
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -13,46 +13,66 @@ class InputValidatorTest {
     void 다섯_글자_넘는_경우() {
         List<String> carNames = Arrays.asList("다섯자이상의이름");
         assertThrows(IllegalArgumentException.class, () -> {
-           InputValidator.validateCarNames(carNames);
+            InputValidator.validateCarNames(carNames);
         });
     }
 
     void 중간에_다섯글자_넘는_경우() {
-        List<String> carNames = Arrays.asList("pobi","다섯글자넘는이름","jun");
-        assertThrows(IllegalArgumentException.class, ()->{
+        List<String> carNames = Arrays.asList("pobi", "다섯글자넘는이름", "jun");
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateCarNames(carNames);
         });
     }
 
     void 컴마_연속_두개() {
-        List<String> carNames = Arrays.asList("pobi","","jun","aa");
-        assertThrows(IllegalArgumentException.class, ()->{
+        List<String> carNames = Arrays.asList("pobi", "", "jun", "aa");
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateCarNames(carNames);
         });
     }
 
-    void 빈_문자열(){
+    void 빈_문자열() {
         List<String> carNames = Arrays.asList("");
-        assertThrows(IllegalArgumentException.class, ()->{
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateCarNames(carNames);
         });
     }
 
-    void 컴마_빈칸_컴마(){
+    void 컴마_빈칸_컴마() {
         List<String> carNames = Arrays.asList("pobi", " ", "jun");
-        assertThrows(IllegalArgumentException.class, ()->{
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateCarNames(carNames);
         });
     }
 
     void 올바른_입력() {
         List<String> carNames = Arrays.asList("pobi", "jun", "aa");
-        assertThrows(IllegalArgumentException.class, ()->{
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateCarNames(carNames);
         });
     }
 
 
+    @Test
+    void 시도횟수_정수가_아닌경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateAttemptCount("1.1");
+        });
+    }
+
+    @Test
+    void 시도횟수_0이하_인_경우() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            InputValidator.validateAttemptCount("-1");
+        });
+    }
+
+    @Test
+    void 시도횟수_숫자가_아닌_경우() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            InputValidator.validateAttemptCount("다섯");
+        });
+    }
 
 
 }
