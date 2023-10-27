2743557257ae823dcad5e3535afa7d4be79a1218
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-27 17:57:23 +0900

    feat(service):입력받은 값으로 자동차 생성 및 플레이어 생성

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,10 +1,37 @@
 package racingcar.service;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.GamePlayer;
+
 /*
  *   racingCar게임의 기능들을 담당
  * */
 
 public class GameService {
+
+
+    public GamePlayer settingPlayer(List<String> carNames) {
+        GamePlayer gamePlayer = new GamePlayer();
+        addCarsToGamePlayer(gamePlayer, initializeCars(carNames));
+        return gamePlayer;
+    }
+
+    private void addCarsToGamePlayer(GamePlayer gamePlayer, List<Car> cars) {
+        for (Car car : cars) {
+            gamePlayer.addCar(car);
+        }
+    }
+
+    private List<Car> initializeCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
 
 
 }
