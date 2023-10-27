3c4a46010946ca7f70afcb1e6cb0d9f31e1abf25
SongJwans <yhh5916@naver.com>
2023-10-28 00:35:30 +0900

    feat: 입출력 관련 기능을 담당하는 InputView, OutputView 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class InputView {
+    private static final String INPUT_START = "경주할 자동차 이름을 입력하세요.(이름은  쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT = "시도할 회수는 몇회인가요?";
+    public void inputCarNames(){
+        System.out.println(INPUT_START);
+    }
+    public void attemptNumber(){
+        System.out.println(INPUT_ATTEMPT);
+    }
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,43 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.CarList;
+import racingcar.domain.Winners;
+
+public class OutputView {
+    private static final String OUTPUT_RESULT = "실행 결과";
+    private static final String OUTPUT_FINAL_WINNERS = "최종 우승자 : ";
+    private static final String OUTPUT_ONE_DISTANCE = "-";
+    private static final String OUTPUT_FINAL_WINNERS_DELIMITER = ", ";
+
+    public void printResult(CarList carList) {
+        System.out.println(OUTPUT_RESULT);
+
+        for (Car car : carList.getCarList()) {
+            car.StopOrMove();
+            printNameAndDistance(car);
+        }
+        System.out.println();
+    }
+
+    public void printNameAndDistance(Car car) {
+        System.out.println(car.getName() + " : " + printDistance(car.getDistance()));
+    }
+
+    public String printDistance(int distance) {
+        String temp = "";
+        for (int i = 0; i < distance; i++) {
+            temp += OUTPUT_ONE_DISTANCE;
+        }
+        return temp;
+    }
+
+    public void printWinners(Winners winners) {
+        System.out.println(OUTPUT_FINAL_WINNERS + join(winners.getNames()));
+    }
+
+    public String join(List<String> input) {
+        return String.join(OUTPUT_FINAL_WINNERS_DELIMITER, input);
+    }
+}
