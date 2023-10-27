ae63159b8adb9a17504171ec8339b689e8dbd741
eom-tae-in <eti0728@daum.net>
2023-10-27 19:43:38 +0900

    refactor: 개행 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import racingcar.view.ConsoleInputView;
 import racingcar.view.ConsoleOutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         Controller controller = new Controller(new ConsoleOutputView(), new ConsoleInputView());
         controller.start();
