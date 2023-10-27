25b2c53112514100a5c61023fa9084e57ca0bc25
gijin <rkrhdlf1546@naver.com>
2023-10-27 23:38:43 +0900

    View.java코드 간단한 출력문 작성 시작메세지~최종우승자 메세지 및 자동차 객체 이름 출력값

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,4 +1,26 @@
 package racingcar;
 
 public class View {
+
+    public void printStartMeg(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printGoCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printResultMeg(){
+        System.out.println("실행 결과");
+    }
+
+    public void printRaceResult(Car[] cars){
+        for(Car car  : cars){
+            System.out.println(car);
+        }
+    }
+
+    public void printWinner(String winner){
+        System.out.println("최종 우승자 : " + winner);
+    }
 }
