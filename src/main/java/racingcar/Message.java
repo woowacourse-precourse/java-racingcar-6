784673eba3330aa6fad471648f397f205a4b8a74
bnfkim <kgr3819@gmail.com>
2023-10-27 19:09:44 +0900

    Feat: 입력이 유효한지 체크하는 메서드

--- src/main/java/racingcar/Message.java
+++ src/main/java/racingcar/Message.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 
 public class Message {
 
@@ -12,6 +16,27 @@ public class Message {
     public String inputGameRound(){
         System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
+    }
+
+    public void validInputCarName(String input){
+        if(input.isBlank()) throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
+
+        ArrayList<String> cars = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(input, ",");
+        while(st.hasMoreTokens()){
+            if(st.nextToken().length() > 5) throw new IllegalArgumentException("자동차 이름을 5자 이하로 입력해주세요.");
+        }
+    }
+
+    public void validInputGameRound(String input) {
+        if(input.isBlank()) throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
+
+        for(int i=0; i<input.length(); i++) {
+            if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException("정수 값을 입력해주세요.");
+        }
+
+        int num = Integer.parseInt(input);
+        if(num<=0) throw new IllegalArgumentException("1회 이상 게임이 진행 되어야 합니다.");
     }
 
     public void printResult() {
