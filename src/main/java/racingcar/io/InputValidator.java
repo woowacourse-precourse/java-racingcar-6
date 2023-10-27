83a2d6c9a3611d1d6751331426db83e64c753c89
김승진 <ohksj77@gmail.com>
2023-10-27 21:53:21 +0900

    feat(InputValidator): 자동차 이름 입력값 쉼표 구분 여부 검증 기능 구현

---
+++ src/main/java/racingcar/io/InputValidator.java
@@ -0,0 +1,18 @@
+package racingcar.io;
+
+import racingcar.constant.ErrorMessage;
+
+public class InputValidator {
+
+    private static final String CAR_NAMES_REGEX = "^[a-zA-Z]+(,[a-zA-Z]+)*$";
+
+    public void validateCarNames(final String input) {
+        if (input.isBlank() || hasInvalidDelimiterUsage(input)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_INPUT.toValue());
+        }
+    }
+
+    private boolean hasInvalidDelimiterUsage(final String input) {
+        return input.matches(CAR_NAMES_REGEX);
+    }
+}
