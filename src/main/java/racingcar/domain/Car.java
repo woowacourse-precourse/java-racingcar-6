c719f604629f4e423598d7a46ee0eb7781f6d152
nahyeon99 <nahyeonee99@gmail.com>
2023-10-28 00:12:03 +0900

    feat: 자동차 전진 시도 기능 추가 1. tryMove() : 전진 시도 2. isMove() : 전진 여부 체크 3. 메서드 순서 변경 4. 사이즈는 상수로 관리하도록 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,8 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.Console.Constants;
 
 public class Car {
+
+    private static final int MAX_NAME_SIZE = 5;
+    private static final int MIN_MOVE_SIZE = 0;
+    private static final int MAX_MOVE_SIZE = 9;
+    private static final int POSSIBLE_MOVE_STANDARD_SIZE = 4;
 
     private String name;
     private int moveCounts;
@@ -14,22 +20,26 @@ public class Car {
     }
 
     private void isValidNameSize(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_SIZE) {
             throw new IllegalArgumentException(Constants.EXCEPTION_CAR_NAME_SIZE_MESSAGE);
         }
     }
 
-    void addMoveCounts(boolean isMove) {
-        if (isMove) {
+    void tryMove() {
+        if (isMove()) {
             this.moveCounts++;
         }
     }
 
-    int getMoveCounts() {
-        return this.moveCounts;
+    private boolean isMove() {
+        return Randoms.pickNumberInRange(MIN_MOVE_SIZE, MAX_MOVE_SIZE) >= POSSIBLE_MOVE_STANDARD_SIZE;
     }
 
     String getName() {
         return this.name;
+    }
+
+    int getMoveCounts() {
+        return this.moveCounts;
     }
 }
