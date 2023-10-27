9742ea91c2aa99dcc8852d74d5a4346b79647d83
Haeun Jeong <gkdms4248@gmail.com>
2023-10-27 17:39:47 +0900

    test(view): 시도 회수 입력 기능 테스트 추가

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,14 +1,26 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputView;
 
 public class InputViewTest {
+    InputView inputView;
+    @BeforeEach
+    public void setUp() {
+        inputView = new InputView();
+    }
+
+    @AfterEach
+    public void reset() {
+        Console.close();
+    }
 
     @DisplayName("자동차 입력 기능 테스트")
     @Test
@@ -18,8 +30,20 @@ public class InputViewTest {
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        InputView inputView = new InputView();
         String result = inputView.inputCarNamesToRace();
+
+        assertEquals(expected, result);
+    }
+
+    @DisplayName("시도 회수 입력 기능 테스트")
+    @Test
+    void testInputNumberOfTry() {
+        String expected = "5";
+        String input = "5";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String result = inputView.inputNumberOfTry();
 
         assertEquals(expected, result);
     }
