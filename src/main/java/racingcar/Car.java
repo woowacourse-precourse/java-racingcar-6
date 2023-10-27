b8d1fe58bcf69552e2edff271857dc61323fd899
zjacom <eephus625@icloud.com>
2023-10-27 17:53:10 +0900

    feat(racingCar): 자동차 주행 기능 추가
    자동차 주행 기능에 필요한 자동차 전진 기능도 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -12,4 +12,23 @@ public class Car {
     public int createRandomNumber() {
         return Randoms.pickNumberInRange(0,9);
     }
+
+    public void moveForward(int randomNum) {
+        if (randomNum >= 4) {
+            this.distanceDriven += "-";
+        }
+    }
+
+    public void driving() {
+        int randomNum = createRandomNumber();
+        moveForward(randomNum);
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public String getDistanceDriven() {
+        return distanceDriven;
+    }
 }
