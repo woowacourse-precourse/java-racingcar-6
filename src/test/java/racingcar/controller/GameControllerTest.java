d82603a53e42de32d90130c5279b29e328e12063
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-10-27 23:53:52 +0900

    test: RaceTest 파일 작성

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -1,27 +1,60 @@
 package racingcar.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import racingcar.model.Race;
 import racingcar.view.ConsoleOutputView;
 
-class GameControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GameControllerTest {
     private GameController gameController;
     private Race race;
     private ConsoleOutputView consoleOutputView;
+    private final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
 
     @BeforeEach
-    void setUp() {
-        race = mock(Race.class);
-        consoleOutputView = mock(ConsoleOutputView.class);
+    public void setUp() {
+        race = new Race();
+        consoleOutputView = new ConsoleOutputView();
         gameController = new GameController(race, consoleOutputView);
     }
 
     @Test
-    void runGame_() {
+    void runGame_ValidInput() {
+        String input = "Car1,Car2,Car3\n3\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
 
+        gameController.runGame();
+
+        // 예상된 결과를 출력으로 가져와서 확인
+        String expectedOutput = "자동차 대수는 몇 대 인가요?\n" +
+                "시도할 회수는 몇 회 인가요?\n" +
+                "\n" +
+                "실행 결과\n" +
+                "Car1 : -\n" +
+                "Car2 : -\n" +
+                "Car3 : -\n" +
+                "\n" +
+                "Car1, Car2, Car3가 경주를 완료했습니다.\n";
+        assertEquals(expectedOutput, outputContent.toString());
+    }
+
+    @Test
+    void runGame_InvalidTryNumber() {
+        String input = "Car1,Car2,Car3\n-1\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            gameController.runGame();
+        });
+
+        assertEquals("잘못된 시도 횟수입니다.", exception.getMessage());
     }
 }
--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -1,26 +1,58 @@
 package racingcar.model;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
-class RaceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RaceTest {
+    private Race race = new Race();
 
     @Test
     void initialize() {
+        ArrayList<String> carNames = new ArrayList<>(List.of("Car1","Car2","Car3"));
+
+        race.initialize(carNames);
+
+        ArrayList<String> resultCarNames = race.getCarNames();
+        ArrayList<Integer> resultCarGoingCount = race.getCarGoingCount();
+
+        // initialize 메서드가 예상대로 동작하는지 확인
+        assertEquals(carNames, resultCarNames);
+        assertEquals(carNames.size(), resultCarGoingCount.size());
     }
 
     @Test
     void runRace() {
+        ArrayList<String> carNames = new ArrayList<>(List.of("Car1","Car2","Car3"));
+
+        race.initialize(carNames);
+
+        race.runRace();
+
+        ArrayList<Integer> resultCarGoingCount = race.getCarGoingCount();
+
+        // runRace 메서드가 예상대로 동작하는지 확인
+        for (int count : resultCarGoingCount) {
+            assertEquals(true, count >= 0 && count <= 1);
+        }
     }
 
     @Test
     void getWinners() {
-    }
+        ArrayList<String> carNames = new ArrayList<>(List.of("Car1","Car2","Car3"));
 
-    @Test
-    void getCarGoingCount() {
-    }
+        race.initialize(carNames);
 
-    @Test
-    void getCarNames() {
+        ArrayList<Integer> carGoingCount = new ArrayList<>(List.of(1, 2, 2));
+
+        for (int i=0; i<carGoingCount.size(); i++) {
+            race.getCarGoingCount().set(i, carGoingCount.get(i));
+        }
+        String winners = race.getWinners();
+
+        // getWinners 메서드가 예상대로 동작하는지 확인
+        assertEquals("Car2, Car3", winners);
     }
 }
