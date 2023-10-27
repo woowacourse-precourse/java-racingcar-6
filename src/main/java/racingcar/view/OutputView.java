89f6d0ece89c9ef4ad842b3054a7eb07ee7bccef
mainfn <_@mainfn.dev>
2023-10-27 23:48:12 +0900

    feat: OutputView 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import racingcar.component.ComponentWriter;
+import racingcar.component.out.GameResultComponent;
+import racingcar.component.out.RoundResultComponent;
+import racingcar.domain.car.dto.response.CarsRacingDto;
+import racingcar.domain.game.CarsFinishedDto;
+
 public final class OutputView {
+    private final ComponentWriter componentWriter;
+
+    public OutputView(final ComponentWriter componentWriter) {
+        this.componentWriter = componentWriter;
+    }
+
+    public void printRoundResult(final CarsRacingDto dto) {
+        componentWriter.write(new RoundResultComponent(dto));
+    }
+
+    public void printGameResult(final CarsFinishedDto dto) {
+        componentWriter.write(new GameResultComponent(dto));
+    }
 }
