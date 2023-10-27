031f5547a3f5ea8585e6884133bf51313c38392f
Sully <2dcoder@naver.com>
2023-10-27 19:11:37 +0900

    feat(CarNameValidator): 자동차 이름을 입력받을 때 생기는 예외를 처리하기 위한 클래스 생성
    RacingCarNameValidator는 지나치게 길어진다고 판단하여 CarNameValidator로 줄여 지었다.
    필드로 carName을 갖고 있는다.

---
+++ src/main/java/racingcar/domain/CarNameValidator.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class CarNameValidator {
+
+    private final String carName;
+
+    public CarNameValidator(String carName) {
+        this.carName = carName;
+    }
+
+}
