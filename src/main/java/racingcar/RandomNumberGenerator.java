c71d5033779fa6fbf5da97d7aeda9cb04f5ba0a2
kongji <kongji4092@gmail.com>
2023-10-27 15:34:26 +0900

    bug-fix: 숫자 랜덤 생성 기능 END_INCLUSIVE 값 수정

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberGenerator {
 
     private static final int START_INCLUSIVE = 0;
-    private static final int END_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
 
     public static int generateNumber() {
         return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
