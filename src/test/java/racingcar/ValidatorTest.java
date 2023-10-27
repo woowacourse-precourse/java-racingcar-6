9cfeb53bf19e1d1de1f4d54a1b62cf479db3817e
jmj005 <jmj00005@naver.com>
2023-10-28 01:45:18 +0900

    refactor: ValidateTest 테스트 코드 중복 제거

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,32 +1,28 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.controller.Validator;
 
 public class ValidatorTest extends ApplicationTest {
-    @Test
-    void 이름_중복_유효성_실패_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,pobi,woni", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-            );
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void 이름_유효성_실패_테스트(List<String> names) {
+        assertThrows(IllegalArgumentException.class,
+                () -> Validator.validateNames(names));
     }
 
-    @Test
-    void 이름_길이_유효성_실패_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,jjjjun", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 이름_공백_유효성_실패_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("pobi", "pobi", "woni")),
+                Arguments.of(Arrays.asList("pobi", "jjjjun")),
+                Arguments.of(Arrays.asList("", ""))
         );
     }
 }
