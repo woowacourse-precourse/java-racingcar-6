9b772651bfd24cd0df567a71ab1a4a8dfaa4795a
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-27 20:27:43 +0900

    feat: 0에서 9 사이에서 무작위 값을 반환하는 기능 구현 - RacingCarGame에서 사용

--- src/main/java/util/RacingCarRandoms.java
+++ src/main/java/util/RacingCarRandoms.java
@@ -3,5 +3,7 @@ package util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCarRandoms{
-    public int getRandomNumber(){}
+    public int getRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
 }
