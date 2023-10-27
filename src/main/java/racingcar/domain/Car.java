8e521df4dbe0e624b5bbdf7180f3e5cb37679a46
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 13:49:12 +0900

    feat : 랜덤한 수가 4이상일 경우 전진하는 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,11 +3,18 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private static final int MIN_MOVE_COND = 4;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move() {
+        if (getCondition() >= MIN_MOVE_COND) {
+            position++;
+        }
     }
 
     private int getCondition() {
