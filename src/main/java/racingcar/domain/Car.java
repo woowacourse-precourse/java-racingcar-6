3749fe6f8e42898a7ce66c5459b4431922e797fc
anneu <b.imnuj@gmail.com>
2023-10-27 11:18:27 +0900

    feat: 자동차 이름의 길이를 검증하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,22 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int MAX_LENGTH = 5;
+    private static final String NAME_LENGTH_EXCEPTION = "자동차 이름은 %d자 이하만 가능합니다.";
+
     private final String name;
     private int moveCount;
 
     public Car(String name) {
+        validateLength(name);
         this.name = name;
         this.moveCount = 0;
+    }
+
+    private void validateLength(String name) {
+        if (MAX_LENGTH < name.length()) {
+            throw new IllegalArgumentException(String.format(NAME_LENGTH_EXCEPTION, MAX_LENGTH));
+        }
     }
 
     public void move() {
