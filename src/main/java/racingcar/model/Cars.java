693b67ddbf644ec58d76e381a7355403deef4c3f
song windows <dkclasltmf@naver.com>
2023-10-27 19:38:46 +0900

    feat: Cars 클래스 생성
    List<Car> carList를 가지는 일급 컬렉션

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> carList;
+
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
+}
