4921dea3d1d67eb67b5a65b1f46611d75c284b3f
bumstead-bumstead <dldyghks951@gmail.com>
2023-10-27 22:39:57 +0900

    test: InputProcessorTest: manipulateRepetitionInput() 단위 테스트 구현

--- src/test/java/racingcar/InputProcessorTest.java
+++ src/test/java/racingcar/InputProcessorTest.java
@@ -53,4 +53,43 @@ public class InputProcessorTest {
             assertThrows(IllegalArgumentException.class, () -> inputProcessor.manipulateCarNameInput(input));
         }
     }
+
+    @Nested
+    @DisplayName("이동 횟수 입력 기능 테스트")
+    class manipulateRepetitionInputTest {
+        @Test
+        @DisplayName("입력받은 정보를 Integer로 반환한다.")
+        void parseTest() {
+            //given
+            String input = "12";
+
+            //when
+            Integer result = inputProcessor.manipulateRepetitionInput(input);
+
+            //then
+            assertThat(result).isEqualTo(12);
+        }
+
+        @Test
+        @DisplayName("정수 형태의 입력이 아닌 경우 IllegalArgumentException을 발생시킨다.")
+        void componentLengthTest() {
+            //given
+            String input = "a123";
+
+            //when
+            //then
+            assertThrows(IllegalArgumentException.class, () -> inputProcessor.manipulateRepetitionInput(input));
+        }
+
+        @Test
+        @DisplayName("음수를 입력한 경우 IllegalArgumentException을 발생시킨다.")
+        void inputFormatTest() {
+            //given
+            String input = "-123";
+
+            //when
+            //then
+            assertThrows(IllegalArgumentException.class, () -> inputProcessor.manipulateRepetitionInput(input));
+        }
+    }
 }
