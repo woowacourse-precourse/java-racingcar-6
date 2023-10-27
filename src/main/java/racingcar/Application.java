1d4bbaa1533d94e66af74b4c082e16eeaabf60b9
mango <esudatcom@gmail.com>
2023-10-28 00:47:40 +0900

    feat(InputView): 사용자의 입력을 받는 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.*;
 
 public class Application {
+
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MOVE_CRITERIA = 4;
+    private static final String READLINE = Console.readLine();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
