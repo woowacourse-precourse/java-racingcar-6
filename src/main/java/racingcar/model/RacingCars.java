269189c4a1431f0e1779bc420d3456fd5286e705
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-27 11:58:06 +0900

    feat : , 구성

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,4 +1,21 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RacingCars {
+    private List<Car> cars;
+
+    public RacingCars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> getAllCars() {
+        return Collections.unmodifiableList(cars);
+    }
 }
