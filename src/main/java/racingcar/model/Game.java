f0fdc355ca8b4a738908bae80368ac84890df014
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 19:11:33 +0900

    Feat: 게임 객체 추가

---
+++ src/main/java/racingcar/model/Game.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private final List<Car> carList ;
+
+    public Game(){
+        carList = new ArrayList<>();
+    }
+}
