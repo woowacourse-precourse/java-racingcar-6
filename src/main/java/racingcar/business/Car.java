5299f795a905f5ec57140acc5407256ecb9b82f4
gumgu <bienlee@naver.com>
2023-10-27 19:58:19 +0900

    Feat : 생성자를 통해 자동차(Car)의 이름 부여

--- src/main/java/racingcar/business/Car.java
+++ src/main/java/racingcar/business/Car.java
@@ -5,6 +5,10 @@ public class Car {
     private String name;
     private Integer distance;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     private void forwardCar() {
         distance ++;
     }
