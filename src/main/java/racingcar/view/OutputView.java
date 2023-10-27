6fae5e8a952330e119b7997b2c6e41934de7bcd8
newh08 <newh08@gmail.com>
2023-10-28 06:15:56 +0900

    ♻️REFACTOR: OutputView 리펙토링
    하드코딩 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,13 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String EXECUTION_RESULT_START_MESSAGE = "\n실행 결과";
 
     public void printResult() {
-        System.out.println("\n실행 결과");
+        System.out.println(EXECUTION_RESULT_START_MESSAGE);
     }
     public void printMoveResult(String result) {
-        System.out.println(result + '\n');
+        System.out.println(result);
     }
 
     public void printWinCars(String winCars) {
