0df1b423e6c8167e9308b89ff20db920b081f704
youngjunkim <kyj0703@konkuk.ac.kr>
2023-10-27 10:45:12 +0900

    feat(InputManager): 자동차 이름 사용자 입력 기능 추가
    메서드 명 : inputCarName

--- src/test/java/racingcar/manager/InputManager.java
+++ src/test/java/racingcar/manager/InputManager.java
@@ -1,11 +1,13 @@
 package racingcar.manager;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputManager {
 
     public String inputCarNames() {
 
-        System.out.println();
-        return null;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 
     public String inputTryCount() {
