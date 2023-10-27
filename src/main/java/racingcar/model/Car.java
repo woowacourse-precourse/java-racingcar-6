b718e28393cbd22745b7ec4495441d85796c2452
song windows <dkclasltmf@naver.com>
2023-10-27 19:17:45 +0900

    feat: Car 클래스 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,2 +1,11 @@
-package racingcar.model;public class Car {
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
 }
