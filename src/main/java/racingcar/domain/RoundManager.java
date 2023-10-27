875ad514ec64c2df9aab7d97c31ff7bbd8f66f66
Hoo-sung <gntjd135@gmail.com>
2023-10-27 10:08:25 +0900

    feat(RoundManager): 라운드마다 무작위 값에 의해 차량 전진 시키는 moveCarsInRound 함수 생성

--- src/main/java/racingcar/domain/RoundManager.java
+++ src/main/java/racingcar/domain/RoundManager.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class RoundManager {
 
+    public void moveCarsInRound(List<Car> cars){
+        for(Car car : cars){
+            if(shouldCarMove())
+                car.move();
+        }
+    }
+
     public boolean shouldCarMove(){
         int randomValue = RandomNumberGenerator.randomNumGenerate();
         return randomValue >=4;
