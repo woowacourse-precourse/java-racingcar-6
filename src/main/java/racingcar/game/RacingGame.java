a1a6218cd210898897d79abeb9cac7862ec267b7
aka-nick <bechalrang@gmail.com>
2023-10-27 11:44:13 +0900

    추가(RacingGame) : 최종 우승자를 추리는 기능
    최종 우승자를 추리는 기능과 출력하는 기능을 구현. 우승자를 추리는 기능은 궁극적으로는 Race 안으로 옮겨질 로직이다.
    
    RacingGame

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -1,8 +1,11 @@
 package racingcar.game;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import racingcar.collaborator.Race;
 import racingcar.collaborator.generic.RaceProgress;
+import racingcar.collaborator.generic.RacerProgress;
 import racingcar.io.Output;
 
 public class RacingGame {
@@ -16,6 +19,19 @@ public class RacingGame {
         List<RaceProgress> result = race.result();
         Output.consoleLine("실행 결과");
         Output.consoleLine(result);
+
+        RaceProgress raceProgress = result.get(result.size() - 1);
+        Integer biggestMileage = raceProgress.racerProgress().stream()
+                .sorted(Comparator.comparingInt(RacerProgress::mileage).reversed())
+                .map(RacerProgress::mileage)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("무조건 나오게 되어있는데 말이에요."));
+        List<String> winners = raceProgress.racerProgress().stream()
+                .filter(racerProgress -> Objects.equals(racerProgress.mileage(), biggestMileage))
+                .map(RacerProgress::name)
+                .toList();
+
+        Output.consoleLine("최종 우승자 : " + winners);
     }
 
 }
