82c739de4d4096bbdb0ce1b00f6a008dedb59d41
YongJOoOo <we072200@naver.com>
2023-10-27 11:19:04 +0900

    feat: 입력에 대한 예외처리 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,8 +12,26 @@ public class Application {
     static void inputUser() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
+        //입력값 검증 호출
+        validateInput(carName);
         System.out.println("시도할 회수는 몇회인가요?");
         int gameCount = Integer.parseInt(Console.readLine());
+    }
+
+    //2) 사용자 입력에 대한 예외처리
+    static void validateInput(String carName) throws IllegalArgumentException {
+        //1) 각 이름 길이 5초과 시 예외
+        for (String x : carName.split(",")) {
+            if (x.length() > 5) {
+                throw new IllegalArgumentException("경주할 자동차 이름의 길이는 5이하여야 합니다.");
+            }
+        }
+        //2) 각 이름에 " " 공백 포함 시 예외
+        for (String x : carName.split(",")) {
+            if (x.contains(" ")) {
+                throw new IllegalArgumentException("경주할 자동차 이름에 공백이 포함되면 안됩니다.");
+            }
+        }
     }
 
 }
