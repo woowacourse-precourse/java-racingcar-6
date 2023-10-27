a54747eb2e46d7aa2085e0f3f97783469a9f6460
waveofmymind <sjun0913@gmail.com>
2023-10-28 02:08:48 +0900

    fix: 빈 문자열인 경우 이름이 생성되는 문제를 해결한다.

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -9,9 +9,8 @@ record Name(
     }
 
     private void validate(String value) {
-        if (value.length() > NameConstraints.MAX_NAME_LENGTH.getValue()) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        if (value.length() > NameConstraints.MAX_NAME_LENGTH.getValue() || value.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
         }
     }
-
 }
