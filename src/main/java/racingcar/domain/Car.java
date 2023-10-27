68f0a3dcb0a1c96845d6ddebde44e8141d9e91ce
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-27 14:51:55 +0900

    Refactor: 랜덤으로 생성된 수가 4 이상이면 앞으로 전진하는 메소드로 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,12 +3,17 @@ package racingcar.domain;
 public class Car {
     private final String name;
     private int raceDistance;
+    private static final int START_MOVING_VALUE = 4;
+
     public Car(String name) {
         this.name = name;
     }
 
-    public void moveForward(){
-        this.raceDistance++;
+    public void moveForward(int randomNumber) {
+        if (randomNumber >= START_MOVING_VALUE) {
+            this.raceDistance++;
+            return;
+        }
     }
 
 
