c55773e6209109620f97610b1eb80668a3536238
박승찬 <seungchan141414@gmail.com>
2023-10-27 17:16:13 +0900

    [Feat(racing)] 스켈레톤 코드 작성 완료

--- src/main/java/racingcar/domain/RaceCar.java
+++ src/main/java/racingcar/domain/RaceCar.java
@@ -4,13 +4,17 @@ package racingcar.domain;
 public class RaceCar {
 
     // TODO: 10/27/23 생성자 생성
+    public RaceCar() {
+
+    }
+
     public RaceCar(String nameOfRaceCar) {
         // 이름은 5자 이하만 가능하다.
         // 생성자를 통해서 입력을 받는다.
     }
 
     // TODO: 10/27/23 자동차의 이동을 제어해준다.
-    public Boolean carMovementControl(){
+    public Boolean carMovementControl() {
 //        이동시 전진 카운터 증가.
 //        - [ ] 자동차가 전진을 한다. // RaceCar#moveForward
 //                - [ ] 이동은 출력은 "-" 으로 한다.
@@ -22,13 +26,13 @@ public class RaceCar {
     }
 
     // TODO: 10/27/23 자동차가 전진을 한다.  
-    private void moveForward(){
+    private void moveForward() {
 //        - [ ] 이동은 출력은 "-" 으로 한다.
 //        - [ ] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
     }
 
     // TODO: 10/27/23 자동차 멈춤(정지)
-    private void pauseTemp(){
+    private void pauseTemp() {
 //        - [ ] 멈춤은 아무런 출력을 하지 않는다.
 //        - [ ] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 미만일 경우이다.
     }
