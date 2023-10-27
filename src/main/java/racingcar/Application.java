ed0fb79dec819fa2981f0ff36ac76a891d10d391
alreadysons <alreadysons@gmail.com>
2023-10-27 23:36:55 +0900

    자동차의 입력을 받는RacerInput함수 생성,입력에 대한 예외처리를 하는 RacerCheck함수 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class Application {
+    private static void RacerInput(){
+        String RacerNameInput = Console.readLine();
+        String[] RacerNames = RacerCheck(RacerNameInput);
+    }
+    private static String[] RacerCheck(String RacerNameChk){
+        String[] RacerNames = RacerNameChk.split(",");;
+        if (!RacerNameChk.contains(",")) return RacerNames;
+        for (String racerName : RacerNames) {
+            if (racerName.length() > 5) throw new IllegalArgumentException("유효하지않은 수 입니다.");
+        }
+        return RacerNames;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacerInput();
     }
 }
