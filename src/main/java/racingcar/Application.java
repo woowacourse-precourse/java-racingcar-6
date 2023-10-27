bfb2cdf01c521de208d9e9280fdc254e99ac1114
unknown <y2kf9a0q@gmail.com>
2023-10-28 04:30:56 +0900

    feat: Game.java - run()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,16 @@
 package racingcar;
 
-import racingcar.domain.Car;
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Game;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Car car = new Car("myCar");
-//        car.move(5);
-//        car.move(6);
-        System.out.println(car);
+        try {
+            Game game = new Game();
+            game.run();
+        } finally {
+            Console.close();
+        }
     }
 }
--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -12,7 +12,22 @@ public class Game {
     private List<String> winners = new ArrayList<>();
 
     public void run() {
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = inputCarNames();
+        initCarList(carNames.split(","));
+        System.out.println("시도할 회수는 몇회인가요?");
+        inputGameTurns();
+        System.out.println("\n실행결과");
+        for (int i = 0; i < gameTurns; i++) {
+            for (Car car : cars) {
+                int randomNumber = RandomMaker.makeRandomNumber();
+                car.move(randomNumber);
+                System.out.println(car);
+            }
+            System.out.println();
+        }
+        findWinners();
+        System.out.println(winnersToString());
     }
 
     public String inputCarNames() {
@@ -24,11 +39,12 @@ public class Game {
     private void checkValidCarNames(String input) {
         StringTokenizer tokenizer = new StringTokenizer(input, ",");
         while (tokenizer.hasMoreTokens()) {
-            String carName = tokenizer.nextToken();
-            if (carName.isEmpty() || carName.length() > 5) {
+            String carName = tokenizer.nextToken().trim();
+            if (carName.isBlank() || carName.length() > 5) {
                 throw new IllegalArgumentException("Not valid input: car names");
             }
         }
+        // TODO : StringTokenizer vs String.split()
     }
 
     private void initCarList(String[] carNames) {
@@ -59,6 +75,6 @@ public class Game {
     }
 
     public String winnersToString() {
-        return "최종 우승자 : " + String.join(",", winners);
+        return "최종 우승자 : " + String.join(", ", winners);
     }
 }
