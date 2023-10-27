1b56df27fa3c1e21e05d80e995b246d7bc14d983
sinyebin <dpqls0625@naver.com>
2023-10-27 23:27:30 +0900

    feat: 입력횟수 예외처리 시도횟수 양수가 아닐 때 에러처리

--- src/main/java/racingcar/service/InputTryCount.java
+++ src/main/java/racingcar/service/InputTryCount.java
@@ -7,6 +7,7 @@ public class InputTryCount {
     public static int setInputTryCount() {
         int tryCount = 0;
         String input = Console.readLine();
+        isNumeric(input);
         tryCount = Integer.parseInt(input);
         checkNumber(tryCount);
         return tryCount;
@@ -14,6 +15,12 @@ public class InputTryCount {
 
     public static void checkNumber(int tryCount){
         if(tryCount<1){
+            throw new IllegalArgumentException(ErrorMessage.NOT_NATURAL_NUM_ERROR);
+        }
+    }
+
+    public static void isNumeric(String input) {
+        if(!input.matches("\\d+")){
             throw new IllegalArgumentException(ErrorMessage.NOT_NUM_ERROR);
         }
     }
