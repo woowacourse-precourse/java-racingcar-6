54d32791437881b9cc176b21f7a294cbd31259ee
shAn-kor <kjn0406@naver.com>
2023-10-27 12:39:19 +0900

    InputView 에서 validateInputSize 확인

--- src/main/java/racingcar/common/Config.java
+++ src/main/java/racingcar/common/Config.java
@@ -4,5 +4,7 @@ public class Config {
     public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String START_RACING_MESSAGE = "실행 결과";
     public static final String WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String SPLIT_REGEXP = ",";
     public static final Integer MAX_NAME_SIZE = 5;
+    public static final Integer FORWARD_NUMBER = 4;
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,18 +2,32 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.util.InputDataValidator.validateHasSpace;
+import static racingcar.util.InputDataValidator.validateInputSize;
 import static racingcar.util.InputDataValidator.validateIsInteger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.common.Config;
 
 public class InputView {
     String input = "";
 
-    public String inputCars() {
+    public List<String> inputCars() {
+        List<String> carList = new ArrayList<>();
+
         input = readLine();
+
         validateHasSpace(input);
 
-        return input;
+        carList = Arrays.stream(input.split(Config.SPLIT_REGEXP)).collect(Collectors.toList());
+
+        for (String carName : carList) {
+            validateInputSize(carName);
+        }
+
+        return carList;
     }
 
     public Integer inputCountNumber() {
