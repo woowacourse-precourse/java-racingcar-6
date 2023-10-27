bd4a003f440c1d03c205071e87c57a7da29cb187
devhoya97 <diajbh123@gmail.com>
2023-10-27 11:45:34 +0900

    feat(racingcar) : 자동차 이름 조회 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -13,6 +13,10 @@ public class Car {
         location += LOCATION;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return name + " : " + location;
