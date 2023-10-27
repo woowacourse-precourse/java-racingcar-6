28279b5133a8089043f5a636f5260bb76ee27e5b
YongHyeon <pyh4616@naver.com>
2023-10-27 16:32:59 +0900

    Feat: (Convert 클래스), String을 입력 받아 Integer로 변환 그리고 그 과정에서 NumberFormatException을 잡아 IllegalArgumentException을 발생시키는 기능을 stringToInteger 메서드로 구현

--- src/main/java/racingcar/util/Convert.java
+++ src/main/java/racingcar/util/Convert.java
@@ -7,4 +7,12 @@ public class Convert {
     public static List<String> stringToListUsingComma(String inputValue) {
         return List.of(inputValue.split(","));
     }
+
+    public static Integer stringToInteger(String inputValue) {
+        try {
+            return Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
