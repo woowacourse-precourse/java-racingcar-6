1e357779db92e9182faf03b342b2cb86a2846d5c
nylonmusk <2912rla@gmail.com>
2023-10-27 18:04:19 +0900

    feat : 동차의 전진, 상태, 위치를 반환하는 Cars클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveAll(RandomNumberGenerator randomNumberGenerator, RandomMoveStrategy randomMoveStrategy) {
+        for (Car car : cars) {
+            int randomNumber = randomNumberGenerator.getNumber();
+            if (randomMoveStrategy.isMovable(randomNumber)) {
+                car.move();
+            }
+        }
+    }
+
+    public List<CarDto> getStatus() {
+        return CarDto.from(cars);
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = getMaxPosition();
+        return cars.stream().filter(car -> car.getPosition() == maxPosition).collect(Collectors.toList());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+    }
+}
