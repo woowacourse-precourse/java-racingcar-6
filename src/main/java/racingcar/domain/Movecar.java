c520ffbc7bba9fa8022c1d3a21c16b60296d6d92
Yoon-HaeJun <0681hj@naver.com>
2023-10-28 02:25:56 +0900

    우승자 출력

--- src/main/java/racingcar/domain/Movecar.java
+++ src/main/java/racingcar/domain/Movecar.java
@@ -2,29 +2,38 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Movecar {
     public void print(int usercount, List<String> carname) {
+        int[] carmove = new int[carname.size()];
         for (int i = 0; i < usercount; i++) {
-            go_or_stop(carname);
+            go_or_stop(carname, carmove);
         }
+        winner(carname, carmove);
     }
 
-    public void go_or_stop(List<String> carname) {
-        int index = 0;
+    public void go_or_stop(List<String> carname, int[] carmove) {
         for (int i = 0; i < carname.size(); i++) {
-            if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                carname.set(i, carname.get(i) + '-');
-            }
-            if (carname.get(i).contains("-") == true) {
-                index = carname.get(i).indexOf("-");
-                System.out.printf("%s : %s\n", carname.get(i).substring(0, index), carname.get(i).substring(index));
-            }
-            if (carname.get(i).contains("-") == false) System.out.printf("%s : \n", carname.get(i));
+            if (Randoms.pickNumberInRange(0, 9) >= 4) carmove[i]++;
+            System.out.printf("%s : ", carname.get(i));
+            for (int j = 0; j < carmove[i]; j++) System.out.printf("-");
+            System.out.println();
         }
         System.out.println();
+    }
+
+    public void winner(List<String> carname, int[] carmove) {
+        int max = 0, maxuser = 0;
+        for (int i = 0; i < carname.size(); i++) {
+            if (carmove[i] > max) {
+                max = carmove[i];
+                maxuser = i;
+            }
+        }
+        System.out.printf("최종 우승자 : %s",carname.get(maxuser));
+        for (int i = maxuser + 1; i < carname.size(); i++) {
+            if (carmove[i] == max) System.out.printf(", %s", carname.get(i));
+        }
     }
 }
