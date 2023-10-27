5ee8197f72fa5b51ed52452a90e09d7902bd1895
gosu <q63530@naver.com>
2023-10-27 15:39:45 +0900

    test(domain): stopOrMoveDecision 테스트 추가

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+    @Test
+    void stopOrMoveDecisionTest() {
+        //given
+        Car car = new Car("hoho");
+        String testResult = "-";
+
+        //when
+        car.stopOrMoveDecision(5);
+
+        //then
+        Assertions.assertThat(car).extracting("stopOrMoveDecisionResult").isEqualTo("-");
+
+    }
+}
