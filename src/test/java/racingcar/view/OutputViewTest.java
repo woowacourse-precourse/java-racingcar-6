9da0357c593148b55cc33af61e31b8d9fb7f42d2
YongHyeon <pyh4616@naver.com>
2023-10-28 04:50:36 +0900

    Test: (OutputViewTest 클래스), 한 회차 시도의 결과를 출력하는 기능을 테스트

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,44 @@
+package racingcar.view;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+
+    private PrintStream standardOut;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        standardOut = System.out;
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void restore() {
+        System.setOut(standardOut);
+        System.out.println(outputStream.toString());
+    }
+
+    @Test
+    @DisplayName("temporary")
+    void temporary() {
+        OutputView outputView = new OutputView();
+        LinkedHashMap<String, Integer> carNamesAndMovingCount = new LinkedHashMap<>();
+        carNamesAndMovingCount.put("park", 3);
+        carNamesAndMovingCount.put("yong", 1);
+        carNamesAndMovingCount.put("hyeon", 2);
+        outputView.printExecutionResult(carNamesAndMovingCount);
+
+        String[] output = outputStream.toString().split(System.lineSeparator());
+
+        Assertions.assertThat(output).containsExactly("park : ---\nyong : -\nhyeon : --\n");
+    }
+}
