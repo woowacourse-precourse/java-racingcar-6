3c71578037f4a06bce37eaff775ebef6d288ffd1
jhyii <83736896+jhyii@users.noreply.github.com>
2023-10-27 16:46:05 +0900

    Car 클래스 작성, 기능 구현 목록 Car 클래스 변수 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+class Car{
+	
+	public static String name;
+	public static int step;
+	public static String show;
+	
+	public Car(String n) {
+		name = n;
+		step = 0;
+		show = "";
+	}
+
+	public static void go() {
+		int randomNumber = Randoms.pickNumberInRange(1, 9);
+		
+		if(randomNumber >= 4) {
+			step++;
+			show = show + "-";
+		}
+    }
+	
+	public static void print() {
+		System.out.printf("%s : %s", name, show);
+	}
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
