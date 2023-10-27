ca479be585fd4bea9e7fd2878912894bdb80c5d8
mina <qkralsdk991@naver.com>
2023-10-27 16:59:22 +0900

    Test : 입력 메시지 기능 테스트

--- src/test/java/racingcar/ViewTest/ViewTest.java
+++ src/test/java/racingcar/ViewTest/ViewTest.java
@@ -1,2 +1,17 @@
-package racingcar.ViewTest;public class ViewTest {
+package racingcar.ViewTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.View.RequestMessage;
+
+public class ViewTest {
+    @Test
+    public void setCarName(){
+        RequestMessage.setCarName();
+    }
+
+    @Test
+    public void setAttemptNum(){
+        RequestMessage.setAttemptNum();
+    }
 }
