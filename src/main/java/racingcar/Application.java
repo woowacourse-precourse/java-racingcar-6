21fc6094cb63af104ce5b0e8d5ddabac0eaa5686
KimDongEon <gold3987@naver.com>
2023-10-27 15:24:27 +0900

    docs(location) : 기능 목록 수정
    docs.README.md 에 들여쓰기 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<String> carList;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        carList = new ArrayList<>();
+        
+
     }
 }
