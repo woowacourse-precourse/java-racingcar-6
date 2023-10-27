9fbe2a949a5f221ed93ae96ebb23b0596d482c80
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-27 15:22:52 +0900

    refactor(RaceIOMessage) : RaceIOMessage를 enum 클래스에서 일반 클래스로 변경

--- src/main/java/racingcar/constant/RaceIOMessage.java
+++ src/main/java/racingcar/constant/RaceIOMessage.java
@@ -1,18 +1,8 @@
 package racingcar.constant;
 
-public enum RaceIOMessage {
-    CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)"),
-    NUMBER_INPUT("시도할 횟수는 몇회인가요?"),
-    RACE_RESULT("실행 결과"),
-    RACE_WINNER("최종 우승자 : ");
-
-    private final String message;
-
-    RaceIOMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public class RaceIOMessage {
+    public static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)";
+    public static final String NUMBER_INPUT = "시도할 횟수는 몇회인가요?";
+    public static final String RACE_RESULT = "실행 결과";
+    public static final String RACE_WINNER = "최종 우승자 : ";
 }
