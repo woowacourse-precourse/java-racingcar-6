8f03909592b51bc2f7a210bda91bd1d84f6b79b5
mainfn <_@mainfn.dev>
2023-10-27 21:47:06 +0900

    feat: Cars 상태 클래스 생성

---
+++ src/main/java/racingcar/domain/car/CarsFinished.java
@@ -0,0 +1,35 @@
+package racingcar.domain.car;
+
+import java.util.List;
+import racingcar.domain.game.WinnersResult;
+import racingcar.domain.move.Coordinate;
+
+public final class CarsFinished extends Cars {
+    private CarsFinished(final List<? extends Car> cars) {
+        super(cars);
+    }
+
+    public static CarsFinished from(final CarsRacing carsRacing) {
+        return new CarsFinished(carsRacing.cars);
+    }
+
+    public WinnersResult toWinnersResult() {
+        final List<String> winnerNames = getWinnerNames();
+        return new WinnersResult(winnerNames);
+    }
+
+    private List<String> getWinnerNames() {
+        final Coordinate max = getMaxCoordinate();
+
+        return cars.stream()
+                .filter(car -> car.coordinate.isSameAs(max))
+                .map(car -> car.carName.carName())
+                .toList();
+    }
+
+    private Coordinate getMaxCoordinate() {
+        return cars.stream()
+                .max((c1, c2) -> c2.coordinate.subtractAsInt(c1.coordinate))
+                .get().coordinate;
+    }
+}
---
+++ src/main/java/racingcar/domain/car/CarsRacing.java
@@ -0,0 +1,33 @@
+package racingcar.domain.car;
+
+import java.util.List;
+import racingcar.domain.car.dto.request.CreateCarsRacing;
+import racingcar.domain.move.MoveCommander;
+
+public final class CarsRacing extends Cars {
+    private CarsRacing(final List<CarRacing> cars) {
+        super(cars);
+    }
+
+    public static CarsRacing from(final List<CarRacing> cars) {
+        return new CarsRacing(cars);
+    }
+
+    public static CarsRacing from(final CreateCarsRacing createCarsRacing) {
+        final List<CarRacing> cars = createCarsRacing
+                .carNames()
+                .stream()
+                .map(CarRacing::new)
+                .toList();
+
+        return new CarsRacing(cars);
+    }
+
+
+    public void moveAllBy(final MoveCommander moveCommander) {
+        for (final Car car : cars) {
+            final CarRacing carRacing = (CarRacing) car;
+            carRacing.moveBy(moveCommander.nextCommand());
+        }
+    }
+}
