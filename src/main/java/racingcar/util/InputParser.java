585eddbfcbfc92a9deddd5ffc634bf1d77b4ff9f
pwrwpw <dlgustjr9912@gmail.com>
2023-10-28 03:36:10 +0900

    fix(InputParser): 정적 변수로 수정

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputParser {
-    public int tryCountParser(String input) {
+    public static int tryCountParser(String input) {
         return Integer.parseInt(input);
     }
 
-    public List<String> carsParser(String input) {
+    public static List<String> carsParser(String input) {
         String[] cars = input.split(",");
         return Arrays.stream(cars).toList();
     }
