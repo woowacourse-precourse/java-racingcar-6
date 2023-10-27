81262d5f51b836901bb34bfbc6ec787176d66b0e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-28 00:54:59 +0900

    refactor: 변하지 않는 문자열 final 키워드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static String INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static String INPUT_ATTEMPTS = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPTS = "시도할 회수는 몇회인가요?";
 
     public static String carsName(){
         System.out.println(INPUT_CARS_NAME);
