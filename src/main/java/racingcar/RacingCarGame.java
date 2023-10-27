decf3b3586953169637756fb6215ec023b8aaf07
bumstead-bumstead <dldyghks951@gmail.com>
2023-10-27 20:08:59 +0900

    refactor: 자동차 이름 입력 로직 리팩토링
    - outputProcessor : 경주 게임의 콘솔 출력을 처리하는 클래스 분리

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -5,16 +5,19 @@ import java.util.List;
 
 public class RacingCarGame {
     InputProcessor inputProcessor;
+    OutputProcessor outputProcessor;
 
     List<Car> cars;
 
     public RacingCarGame() {
         cars = new ArrayList<>();
         inputProcessor = new InputProcessor();
+        outputProcessor = new OutputProcessor();
     }
 
     public void run() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        cars = inputProcessor.getCarNameListInput();
+        outputProcessor.printCarNameInputMessage();
+        this.cars = inputProcessor.getCarNameListInput();
+
     }
 }
