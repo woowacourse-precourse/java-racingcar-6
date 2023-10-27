937f091b496d832679db67970a21b3dcfabd44b4
shAn-kor <kjn0406@naver.com>
2023-10-27 11:57:52 +0900

    공통 사용 상수 값 클래스로 묶어 저장

---
+++ src/main/java/racingcar/common/Config.java
@@ -0,0 +1,8 @@
+package racingcar.common;
+
+public class Config {
+    public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String START_RACING_MESSAGE = "실행 결과";
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
+    public static final Integer MAX_NAME_SIZE = 5;
+}
