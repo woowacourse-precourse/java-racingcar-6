21ee623c054d1964418746f7c4f7b40ac96edaad
bellring <starringbell@gmail.com>
2023-10-27 21:37:36 +0900

    refactor(UserInput): 안내 멘트 출력 별도 분리

--- src/main/java/racingcar/view/UserInput.java
+++ src/main/java/racingcar/view/UserInput.java
@@ -9,7 +9,6 @@ public class UserInput {
 
     public static int userInputToInt(String input) {
         try {
-            System.out.println("시도할 회수는 몇회인가요?");
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해 주세요");
@@ -18,7 +17,6 @@ public class UserInput {
 
 
     public static List<String> userInputToStringList(String input) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
