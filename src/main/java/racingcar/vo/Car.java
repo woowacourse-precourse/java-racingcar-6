4dae4d39936867b172ee97d1b7654034617e337d
seminss <2007ksm@naver.com>
2023-10-27 15:13:37 +0900

    feat: 자동차 정보 저장 VO 생성

---
+++ src/main/java/racingcar/vo/Car.java
@@ -0,0 +1,31 @@
+package racingcar.vo;
+
+import java.util.List;
+
+public class Car {
+    private final String name;
+    private final List<Boolean> movementFlags;
+    private int completedMoves;
+
+    public Car(String name, List<Boolean> movementFlags){
+        this.name=name;
+        this.movementFlags = movementFlags;
+        this.completedMoves = 0;
+    }
+
+    public void move(){
+        this.completedMoves++;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(name).append(" : ");
+        for(int step = 0; step< completedMoves; step++) {
+            if(movementFlags.get(step)) {
+                sb.append("-");
+            }
+        }
+        return sb.toString();
+    }
+}
