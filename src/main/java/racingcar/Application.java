6b0c0b434503bb4f2e1d4451555abd43a53eade0
JayG-5 <jg55552758@gmail.com>
2023-10-28 01:13:22 +0900

    chore: 입력 문자열 -> 자동차 객체 함수 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    private List<Car> transformStringToCarList(String cars) {
+    private static List<Car> transformStringToCarList(String cars) {
         List<Car> carList = new ArrayList<>();
         for (String carName : Arrays.stream(cars.split(",")).toList()) {
             if (carName.length() > 5) {
@@ -23,7 +23,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Console.readLine();
+        final List<Car> cars = transformStringToCarList(Console.readLine());
         System.out.println("시도할 회수는 몇회인가요?");
         Console.readLine();
     }
