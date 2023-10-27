906380a823ca79624e3364d11cb25802ddcf13f7
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-27 21:54:16 +0900

    feat: 자동차들의 이름을 입력받는 기능, 예외처리 구현 - 자동차들의 이름을 입력받는 기능 구현 - 입력받은 자동차들의 이름을 쉼표를 기준으로 List로 바꿔주는 기능 구현 - 입력받은 자동차들의 이름이 올바른 입력인지 검사하는 기능 구현

--- src/main/java/util/RacingCarInputReader.java
+++ src/main/java/util/RacingCarInputReader.java
@@ -1,6 +1,21 @@
 package util;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class RacingCarInputReader implements InputReader{
+    public List<String> inputPlayers() throws IllegalArgumentException{
+        List<String> players =  List.of(Console.readLine().split(","));
+        if(!isInputPlayerValid(players)){
+            throw new IllegalArgumentException();
+        }
+        return players;
+    }
+
+    public boolean isInputPlayerValid(List<String> players){
+        if(players.isEmpty()){
+            return false;
+        }
+        return players.stream().allMatch(player -> player.length() <= 5);
+    }
 }
