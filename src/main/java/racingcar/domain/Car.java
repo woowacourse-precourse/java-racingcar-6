8244355352a42edd6fa0f8427c275ba3b64142b5
hoyun06 <hoyun06@gmail.com>
2023-10-27 09:37:39 +0900

    feat: Car 내부에 attemptMovingForward 메소드 구현
    - attemptMovingForward 메소드: 0~9 사이의 숫자 랜덤으로 pick하고 4이상이면 position을 증가시킴

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private final Name name;
@@ -8,5 +10,14 @@ public class Car {
     public Car(String name) {
         this.name = new Name(name);
         this.position = new Position(0);
+    }
+
+    public void attemptMovingForward() {
+        int random = Randoms.pickNumberInRange(0, 9);
+
+        if (random < 4) {
+            return;
+        }
+        position.increment();
     }
 }
