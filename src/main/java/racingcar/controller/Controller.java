b341ede9f3acbde5bbfe9f390a2b0779d8b8bfb8
junOTL <naqp99@gmail.com>
2023-10-27 14:26:31 +0900

    feat: 게임 시작 로직 구현

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,16 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+
+public class Controller {
+    private final List<Car> cars;
+
+    public Controller() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void play() {
+    }
+}
