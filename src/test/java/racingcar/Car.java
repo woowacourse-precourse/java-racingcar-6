588b2040d9df5d0b7a3609341e37d73b59dbcbb6
sunghun <nokdonwithkj211@gmail.com>
2023-10-27 15:01:43 +0900

    feat(ModelTest, Car): Car 클래스 관련 테스트 코드 작성 - 생성

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import java.util.Objects;
+
+public class Car {
+    private final String carName;
+    private int distance;
+
+    public Car(String carName, int distance) {
+        this.carName = carName;
+        this.distance = distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(carName, car.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName, distance);
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
--- src/test/java/racingcar/ModelTest.java
+++ src/test/java/racingcar/ModelTest.java
@@ -1,5 +1,16 @@
 package racingcar;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ModelTest {
+    @Test
+    void Car_생성() {
+        Car car = new Car("pobi", 4);
+
+        Assertions.assertThat(car.getCarName()).isEqualTo("pobi");
+        Assertions.assertThat(car.getDistance()).isEqualTo(4);
+        Assertions.assertThat(car).isEqualTo(new Car("pobi", 4));
+    }
 
 }
