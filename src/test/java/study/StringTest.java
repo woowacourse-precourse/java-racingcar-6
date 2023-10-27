5a7ae131d70f1c34d76df1e4da882fd881b340cc
JUNG GYUWON <milestogo0905@naver.com>
2023-10-28 01:25:21 +0900

    test: AssertJ에서 제공하는 메소드 연습 코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,8 +12,23 @@ public class StringTest {
         String input = "1,2";
         String[] result = input.split(",");
 
+        //모두 입력하지 않아도, 중복 값 있어도, 순서 바뀌어도 성공, String/collection 사용가능
+        assertThat(result).contains("1", "2");
         assertThat(result).contains("2", "1");
+        assertThat(result).contains("1", "2", "2");
+        assertThat(result).contains("2");
+
+        //contain에서 원소 개수까지 맞아야함 (순서랑 중복은 상관 없음)
+        assertThat(result).containsOnly("1", "2");
+        assertThat(result).containsOnly("2", "1");
+        assertThat(result).containsOnly("1", "2", "2");
+//        assertThat(result).containsOnly("2");
+
+        //중복 값, 순서 다른 것 도 허용 안함
         assertThat(result).containsExactly("1", "2");
+//        assertThat(result).containsExactly("2", "1");
+//        assertThat(result).containsExactly("1", "2", "2");
+//        assertThat(result).containsExactly("2");
     }
 
     @Test
@@ -28,8 +43,15 @@ public class StringTest {
     void substring_메서드로_특정_구간_값을_반환() {
         String input = "(1,2)";
         String result = input.substring(1, 4);
+        String resultCopy = result;
 
+        // 값을 비교
         assertThat(result).isEqualTo("1,2");
+        assertThat(result).isEqualTo(resultCopy);
+        
+        //주소를 비교
+//        assertThat(result).isSameAs("1,2");
+        assertThat(result).isSameAs(resultCopy);
     }
 
     @Test
