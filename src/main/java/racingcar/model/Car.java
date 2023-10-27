b06660fc215fb2f7ee618bf944f3595e92f854dd
Isaac Han <isaachan233@gmail.com>
2023-10-28 08:32:18 +0900

    refactor: enum 적용 및 코드 가독성 높임

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,35 +1,35 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constants.Constants;
 
 public class Car {
     private String name;
-    // TODO: 진행한 거리 어떤 타입으로 만들지 고민하기 - 1차 stringbuilder로 만들어보기
-    private final StringBuilder distance; 
+    private final StringBuilder distance;
 
     public Car(String name) {
         this.name = name;
         this.distance = new StringBuilder();
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void moveForward() {
-        if(generateRandomNumber() < 4) return;
-        distance.append("-");
-    }
-    
-    public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
-    }
-
     public String getDistance() {
         return distance.toString();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void moveForward() {
+        if (generateRandomNumber() >= Constants.STANDARD_NUMBER.getIntValue())
+            distance.append("-");
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(Constants.MIN_NUMBER.getIntValue(), Constants.MAX_NUMBER.getIntValue());
     }
 }
