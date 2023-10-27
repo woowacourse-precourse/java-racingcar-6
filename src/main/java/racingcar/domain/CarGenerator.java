a784cf0e61466fbe088ed038b7c59b5ec7c5aee4
Junobee25 <wnsdhqo@naver.com>
2023-10-27 21:34:43 +0900

    style: String 배열 선언 부 공백 제거

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -1,17 +1,18 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.LinkedHashMap;
 
 public class CarGenerator {
 
     Car car = new Car();
 
-    public LinkedHashMap<String,String> createCarList() {
-        LinkedHashMap<String,String> carList = new LinkedHashMap<>();
-        String [] cars = splitStringByCommas();
-        for(String car : cars) {
-            carList.put(car,"");
+    public LinkedHashMap<String, String> createCarList() {
+        LinkedHashMap<String, String> carList = new LinkedHashMap<>();
+        String[] cars = splitStringByCommas();
+        for (String car : cars) {
+            carList.put(car, "");
         }
         return carList;
     }
