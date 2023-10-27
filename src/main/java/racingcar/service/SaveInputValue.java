cba2285dadbdb76131565a574e4d5fd2db31e82f
iceprins <junghankyul@gmail.com>
2023-10-28 01:08:11 +0900

    feat: 입력값 처리 후 저장하는 기능 추가

---
+++ src/main/java/racingcar/service/SaveInputValue.java
@@ -0,0 +1,52 @@
+package racingcar.service;
+
+import racingcar.model.InputProcess;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SaveInputValue {
+
+    InputProcess inputProcess = new InputProcess();
+    CarNameCheck carNameCheck = new CarNameCheck();
+    RepeatNumberCheck repeatNumberCheck = new RepeatNumberCheck();
+
+    List<String> cars;
+    String repeatNumber;
+
+    public SaveInputValue(List<String> cars, String repeatNumber) {
+        this.cars = cars;
+        this.repeatNumber = repeatNumber;
+    }
+
+    public void saveCarName() {
+        String carInput = inputProcess.getCarName();
+        String[] temp = carInput.split(",");
+        cars = new ArrayList<>(Arrays.asList(temp));
+        checkCarNames();
+        isCarNameDuplicated();
+    }
+
+    public void saveRepeatNumber() {
+        repeatNumber = inputProcess.getRepeatNumber();
+        checkRepeatNumber();
+    }
+
+    public void checkCarNames() {
+        for (String car : cars) {
+            carNameCheck.checkAll(car);
+        }
+    }
+
+    public void checkRepeatNumber() {
+        repeatNumberCheck.checkAll(repeatNumber);
+    }
+
+    public void isCarNameDuplicated() {
+        List<String> cmp = cars.stream().distinct().toList();
+        if (cars.size() != cmp.size()) {
+            throw new IllegalArgumentException("자동차 이름이 중복되었습니다.");
+        }
+    }
+}
