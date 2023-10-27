1c2a3550c148b0fb3e5f7271ec1943af8e37dde2
yubin <dbqls2821@gmail.com>
2023-10-27 16:52:49 +0900

    test: 동일한 이름을 가진 차는 Hash 자료구조에서도 동일한 객체로 인식하도록 하는 테스트코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class CarTest {
@@ -13,6 +16,19 @@ final class CarTest {
         Car car2 = new Car(sameName);
 
         assertEquals(car1, car2);
+    }
+
+    @Test
+    void 동일한_이름을_가진_차는_Hash_자료구조에서도_동일한_객체로_인식된다() {
+        Name sameName = new Name("동일한이름");
+        Car car1 = new Car(sameName);
+        Car car2 = new Car(sameName);
+        Set<Car> set = new HashSet<>();
+
+        set.add(car1);
+        set.add(car2);
+
+        assertEquals(set.size(), 1);
     }
 
     record Car(Name name) {
