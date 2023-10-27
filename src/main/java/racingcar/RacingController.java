49f7d95b74b23cfcd5ec810ef4bed59108174e2e
Bisi3asi <kueeng8008@naver.com>
2023-10-27 11:44:02 +0900

    feat : 2-1. 자동차 이동 회수 값 입력
    1. 사용자 입력 지시 텍스트 출력
    2. Console.readLine()

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -12,18 +12,31 @@ public class RacingController {
         List<String> validNameList = racingService.namesTest(names);
         racingService.generateCar(validNameList);
 
-        // 2. 자동차 이동 횟수(차수) 값 입력, 검증, 저장
+        // 2. 자동차 이동 회수(경주 차수) 값 입력, 검증, 저장
+        String count = inputCount();
+
         // 3. 차수별 경주 실행 및 결과 출력
         // 4. 우승자 출력
+
+        /**
+         * 1-1. 자동차 이름 입력
+         * @return 사용자 자동차 이름 입력값
+         */
+
     }
 
-    /**
-     * 1-1. 자동차 이름 입력
-     * @return 사용자 자동차 이름 입력값
-     */
-    String inputName(){
+    String inputName() {
         System.out.println("경주할 자동차 이름을 입력하세요.");
         return Console.readLine();
     }
 
+    /**
+     * 2-1. 시도 회수 입력
+     *
+     * @return 사용자 시도 회수 입력값
+     */
+    String inputCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
 }
