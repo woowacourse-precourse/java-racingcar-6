72ccfe570c6b253bf70e4fdb8f627a3201df63d8
anneu <b.imnuj@gmail.com>
2023-10-27 12:22:18 +0900

    feat: 자동차 이름의 앞뒤 공백을 제거하는 기능 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,9 +1,11 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
@@ -22,5 +24,12 @@ class CarTest {
     void NotLetterNameEx(String name) {
         assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름의 앞뒤 공백을 제거한다.")
+    @Test
+    void trimName() {
+        Car car = new Car("  pobi ");
+        assertThat(car.getName()).isEqualTo("pobi");
     }
 }
