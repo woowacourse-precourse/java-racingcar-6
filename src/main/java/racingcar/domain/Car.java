2e7e240c4f3efc62db919389ab1c07c106626029
elyudwo <dudqk9696@naver.com>
2023-10-27 22:49:14 +0900

    [Refactor] : Car 클래스 코드 정렬

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -15,13 +15,13 @@ public class Car {
     }
 
     public void validateCarName(String name) {
-        if(name.isBlank() || name.length() > 5) {
+        if (name.isBlank() || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름의 길이는 1~5 사이입니다.");
         }
     }
 
     public void moveCar(int randomNumber) {
-        if(randomNumber >= 4) {
+        if (randomNumber >= 4) {
             distance++;
         }
     }
