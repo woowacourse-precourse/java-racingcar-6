7cfa8b28f1efe967570604f23da6fba176c84cd0
Juhyorim <joohr1234@gmail.com>
2023-10-28 00:25:15 +0900

    refactor(racingcar): 출력문 상수화

--- src/main/java/racingcar/domain/ConsoleOutput.java
+++ src/main/java/racingcar/domain/ConsoleOutput.java
@@ -3,6 +3,11 @@ package racingcar.domain;
 import java.util.List;
 
 public class ConsoleOutput implements Output {
+    private static final String CAR_NAMES_INPUT_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MOVE_COUNT_INPUT_REQUEST = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_STARTED_MESSAGE = "\n실행 결과";
+    private static final String WINNER_IS = "최종 우승자 : ";
+
     private StringBuilder stringBuilder;
 
     public ConsoleOutput() {
@@ -11,17 +16,19 @@ public class ConsoleOutput implements Output {
 
     @Override
     public void printCarNamesInputRequest() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAMES_INPUT_REQUEST);
     }
 
     @Override
     public void printMoveCountInputRequest() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(MOVE_COUNT_INPUT_REQUEST);
     }
 
     @Override
     public void addCarState(String name, int path) {
-        stringBuilder.append(name + " : " + "-".repeat(path) + "\n");
+        stringBuilder.append("%s : ".formatted(name));
+        stringBuilder.append("-".repeat(path));
+        stringBuilder.append("\n");
     }
 
     @Override
@@ -32,11 +39,11 @@ public class ConsoleOutput implements Output {
 
     @Override
     public void printResultStartedMessage() {
-        System.out.println("\n실행 결과");
+        System.out.println(RESULT_STARTED_MESSAGE);
     }
 
     @Override
     public void printWinner(List<String> winnerNameList) {
-        System.out.println("최종 우승자 : " + String.join(", ", winnerNameList));
+        System.out.println(WINNER_IS + String.join(", ", winnerNameList));
     }
 }
