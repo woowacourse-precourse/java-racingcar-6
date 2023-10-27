6f38542939f5499588f308adc63a71c75c7eb7a0
xogus3492 <wkdxogus3492@gmail.com>
2023-10-27 20:27:57 +0900

    Feat(OutputView): 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,15 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.domain.Cars;
+import racingcar.domain.Winners;
 
 public class OutputView {
     private static final int INITIAL_COUNT = 0;
+    private static final int LAST_INDEX_CHECK_NUMBER = 1;
     private static final String EXECUTION_RESULT_OUTPUT_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNER_OUT_MESSAGE = "최종 우승자 : ";
+    private static final String FINAL_WINNER_SEPARATOR = ", ";
     private static final String DASH = "-";
 
     public void printResultMessage() {
@@ -21,10 +26,25 @@ public class OutputView {
         System.out.println();
     }
 
+    public void printFinalWinner(Winners winners) {
+        List<String> winnerList = winners.getWinners();
+        System.out.print(FINAL_WINNER_OUT_MESSAGE);
+        for (String carName : winnerList) {
+            System.out.print(carName);
+            printFinalWinnerToDistinguish(winnerList, carName);
+        }
+    }
+
     private void printDash(int count) {
         int currentCount = INITIAL_COUNT;
         while (currentCount++ < count) {
             System.out.print(DASH);
+        }
+    }
+
+    private void printFinalWinnerToDistinguish(List<String> winnerList, String carName) {
+        if ((winnerList.indexOf(carName) + LAST_INDEX_CHECK_NUMBER) < winnerList.size()) {
+            System.out.print(FINAL_WINNER_SEPARATOR);
         }
     }
 }
