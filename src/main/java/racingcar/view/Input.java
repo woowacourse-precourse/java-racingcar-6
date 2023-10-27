23ca1eb53680ade1043347fa517d9df8b9608e2d
KimSehoon <shggm2000@gmail.com>
2023-10-28 02:14:30 +0900

    feat: getCarNameList method 구현
    - 사용자에게 자동차 이름을 받아서 List<String> 형식으로 반환하는 getCarNameList method 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Input {
+    public List<String> getCarNameList() {
+        List<String> carNameList = new ArrayList<>();
+        StringTokenizer stringTokenizer = new StringTokenizer(Console.readLine(), ",");
+        while (stringTokenizer.hasMoreTokens()) {
+            carNameList.add(stringTokenizer.nextToken());
+        }
+
+        return carNameList;
+    }
 }
