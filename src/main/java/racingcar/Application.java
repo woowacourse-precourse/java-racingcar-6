2ec7058396ca0e38afd3b747f5ffd8e229dc7015
brightfanta <brightfanta@gmail.com>
2023-10-27 21:33:34 +0900

    feat: 자동차이름 입력 받고 쉼표(,)로 구분하고 구분된 자동차이름개수에 맞춰 자동차 인스턴스 생성하고 인스턴스 변수에 구분한 이름 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.GameController;
+import racingcar.domain.GameData;
+import racingcar.domain.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        View view = new View();
+        GameController controller = new GameController();
+        GameData gameData = new GameData();
+
+
+        view.printAskCarName();
+        String carNames = controller.getCarNames();
+        String[] carNamesArray = carNames.split(",");
+        gameData.setCarList(carNamesArray.length);
+
+        controller.splitCarNameInput(carNamesArray, gameData.getCarList());
+
     }
 }
