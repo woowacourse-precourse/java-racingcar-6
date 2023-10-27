ef61d2b469ebf432f82fea518b53153deffd877b
Roista57 <Roista57@gmail.com>
2023-10-27 13:37:59 +0900

    pickNumberInRange()을 이용하여 0~9 사이의 무작위 값을 구한 뒤 무작위 값이 4 이상이면 전지, 나머지는 정지로 판단하는 기능 수정 및 Application에서 선언하는 부분 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,5 +16,7 @@ public class Application {
         String racingCarNames = Console.readLine();
         System.out.println("시도할 회수는 몇회인가요?");
         String iterateNumber = Console.readLine();
+        ArrayList<String> nameList = carNameSplit(racingCarNames);
+        Control control = new Control(iterateNumber, nameList);
     }
 }
