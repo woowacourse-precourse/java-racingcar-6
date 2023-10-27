8d797066ffd392987d1fd54e64a9282dba458009
김승진 <ohksj77@gmail.com>
2023-10-28 01:18:59 +0900

    feat(RacingCars): 우승자 확인 기능 구현

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -5,7 +5,10 @@ public enum ErrorMessage {
     INVALID_CAR_NAME_INPUT("자동차 이름 입력값이 잘못 사용되었습니다."),
     INVALID_CAR_NAME_LENGTH("자동차 이름의 길이가 잘못되었습니다."),
     NOT_NUMERIC("입력값이 숫자가 아닙니다."),
-    INVALID_TRY_COUNT("시도 횟수가 잘못되었습니다.");
+    INVALID_TRY_COUNT("시도 횟수가 잘못되었습니다."),
+    INVALID_MAX_LOGIC("최대 이동 거리를 구하다 오류가 발생했습니다."),
+    INVALID_ELEMENT_SIZE("자동차가 존재하지 않습니다."),
+    INVALID_WINNER_NUM("우승자가 존재하지 않습니다.");
 
 
     private final String message;
--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import racingcar.constant.ErrorMessage;
 import racingcar.constant.MoveStatus;
 
 public final class RacingCars {
@@ -11,25 +12,34 @@ public final class RacingCars {
     private static final int START_INDEX = 0;
     private static final int MOVE_STANDARD = 4;
     private static final String NEW_LINE = "\n";
-    private final List<RacingCar> racingCars;
+    private final List<RacingCar> elements;
 
-    public RacingCars(final List<RacingCar> racingCars) {
-        this.racingCars = Collections.unmodifiableList(racingCars);
+    public RacingCars(final List<RacingCar> elements) {
+        validate(elements);
+        this.elements = Collections.unmodifiableList(elements);
+    }
+
+    private void validate(final List<RacingCar> racingCars) {
+        if (racingCars.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ELEMENT_SIZE.toValue());
+        }
     }
 
     public int numOfElement() {
-        return racingCars.size();
+        return elements.size();
     }
 
     public void moveByNumbers(final List<Integer> numbers) {
-        IntStream.range(START_INDEX, racingCars.size())
+        IntStream.range(START_INDEX, elements.size())
                 .forEach(index -> move(numbers, index));
     }
 
     private void move(final List<Integer> numbers, final int index) {
-        final RacingCar racingCar = racingCars.get(index);
+        final RacingCar racingCar = elements.get(index);
         final MoveStatus moveStatus = checkMoveStatus(numbers.get(index));
-        racingCar.addMoveStatusIfMove(moveStatus);
+        if (moveStatus.isMove()) {
+            racingCar.addMoveStatus(moveStatus);
+        }
     }
 
     private MoveStatus checkMoveStatus(final int number) {
@@ -44,8 +54,27 @@ public final class RacingCars {
     }
 
     public String toResultMessage() {
-        return racingCars.stream()
+        return elements.stream()
                 .map(RacingCar::toResultMessage)
                 .collect(Collectors.joining(NEW_LINE)) + NEW_LINE;
+    }
+
+    public Winners findWinners() {
+        final Integer maxDistance = calculateMaxDistance();
+        return findSameDistanceCars(maxDistance);
+    }
+
+    private Winners findSameDistanceCars(final Integer distance) {
+        return elements.stream()
+                .filter(racingCar -> racingCar.hasSameDistance(distance))
+                .map(RacingCar::toName)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Winners::new));
+    }
+
+    private Integer calculateMaxDistance() {
+        return elements.stream()
+                .map(RacingCar::moveDistance)
+                .max(Integer::compare)
+                .orElseThrow(() -> new IllegalStateException(ErrorMessage.INVALID_MAX_LOGIC.toValue()));
     }
 }
