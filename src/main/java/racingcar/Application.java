1b6ad410240d19ed0d2729006e4bd68c50194215
KimDabomi <cdabomi@nate.com>
2023-10-27 20:44:42 +0900

    feat: Add main method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        List<String> carNamesList = Collections.singletonList(inputView.getCarNames());
+        String carNamesInput = String.join(",", carNamesList);
+        String[] carNames = carNamesInput.split(",");
+        int tryCount = inputView.getTryCount();
+
+        List<Cars> cars = Arrays.stream(carNames)
+                .map(Cars::new)
+                .collect(Collectors.toList());
+
+        Race race = new Race(cars, tryCount);
+        ResultView resultView = new ResultView(cars);
+        race.startRace(resultView);
+
+        ResultView.printWinners(race.getWinners());
     }
 }
