b1bfa8ea00e89b656ccbec5cc3b22b8a2b3b24c5
Mingeun Park <mindal99546@gmail.com>
2023-10-27 18:00:08 +0900

    refactor: RefereeTest 중복 제거

--- src/test/java/racingcar/RefereeTest.java
+++ src/test/java/racingcar/RefereeTest.java
@@ -5,18 +5,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 public class RefereeTest {
 
-    @Test
-    void 우승한_차량이_한_대() {
-        // given
-        int laps = 10;
-        GameRule rule = new GameRule(5, 0, 9);
-        List<Car> cars = new ArrayList<>();
+    private static GameRule rule;
+    private Referee referee;
+    private List<Car> cars;
+    private static int laps;
+    private static MockedStatic<RandomNumberGenerator> mocked;
+
+    @BeforeEach
+    private void setCarsAndReferee() {
+        cars = new ArrayList<>();
         cars.add(new Car(rule, "Max"));
         cars.add(new Car(rule, "Lando"));
         cars.add(new Car(rule, "Lewis"));
@@ -24,8 +30,28 @@ public class RefereeTest {
         cars.add(new Car(rule, "Alex"));
         cars.add(new Car(rule, "Sainz"));
         cars.add(new Car(rule, "Kevin"));
-        MockedStatic<RandomNumberGenerator> mocked = Mockito.mockStatic(RandomNumberGenerator.class);
-        Referee referee = new Referee(cars);
+        referee = new Referee(cars);
+    }
+
+    @BeforeAll
+    private static void init() {
+        rule = new GameRule(5, 0, 9);
+        laps = 10;
+        mocked = Mockito.mockStatic(RandomNumberGenerator.class);
+    }
+
+    @AfterAll
+    private static void clean() {
+        mocked.close();
+    }
+
+
+    /**
+     * 매 lap마다 각 자동차는 자신의 index만큼 앞으로 전진한다.
+     * 인덱스가 4 이상인 자동차만 앞으로 전진하게 되고 인덱스 값이 가장 큰 Kevin이 우승한다.
+     */
+    @Test
+    void 우승한_차량이_한_대() {
         // when
         for (int i = 0; i < laps; i++) {
             for (int j = 0; j < cars.size(); j++) {
@@ -36,24 +62,14 @@ public class RefereeTest {
         }
         // then
         assertThat(referee.announceAllWinners()).isEqualTo("Kevin");
-        mocked.close();
     }
 
+    /**
+     * 매 lap마다 각 자동차는 자신의 이름 길이 만큼 앞으로 전진한다.
+     * 이름이 4글자 이상인 자동차만 앞으로 전진하게 되고 이름이 5글자인 자동차들이 우승하게 된다.
+     */
     @Test
     void 우승한_차량이_두_대_이상() {
-        // given
-        int laps = 10;
-        GameRule rule = new GameRule(5, 0, 9);
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(rule, "Max"));
-        cars.add(new Car(rule, "Lando"));
-        cars.add(new Car(rule, "Lewis"));
-        cars.add(new Car(rule, "Oscar"));
-        cars.add(new Car(rule, "Alex"));
-        cars.add(new Car(rule, "Sainz"));
-        cars.add(new Car(rule, "Kevin"));
-        MockedStatic<RandomNumberGenerator> mocked = Mockito.mockStatic(RandomNumberGenerator.class);
-        Referee referee = new Referee(cars);
         // when
         for (int i = 0; i < laps; i++) {
             for (int j = 0; j < cars.size(); j++) {
@@ -68,37 +84,30 @@ public class RefereeTest {
                 .map(Car::getName)
                 .collect(Collectors.joining(", "));
         assertThat(referee.announceAllWinners()).isEqualTo(winners);
-        mocked.close();
     }
 
+    /**
+     * 모든 자동차들이 동일한 거리를 laps회 전진한다.
+     */
     @Test
     void 모든_차량이_동일한_거리_이동() {
-        // given
-        int laps = 10;
-        GameRule rule = new GameRule(5, 0, 9);
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(rule, "Max"));
-        cars.add(new Car(rule, "Lando"));
-        cars.add(new Car(rule, "Lewis"));
-        cars.add(new Car(rule, "Oscar"));
-        cars.add(new Car(rule, "Alex"));
-        cars.add(new Car(rule, "Sainz"));
-        cars.add(new Car(rule, "Kevin"));
-        MockedStatic<RandomNumberGenerator> mocked = Mockito.mockStatic(RandomNumberGenerator.class);
         mocked.when(() -> RandomNumberGenerator.pickRandomNumber(rule))
-                .thenReturn(cars.get(5).getName().length());
+                .thenReturn(7);
         Referee referee = new Referee(cars);
         // when
-        for (int i = 0; i < laps; i++) {
-            for (int j = 0; j < cars.size(); j++) {
-                cars.get(j).moveForward();
-            }
-        }
+        moveForwardForGivenLaps();
         // then
         String winners = cars.stream()
                 .map(Car::getName)
                 .collect(Collectors.joining(", "));
         assertThat(referee.announceAllWinners()).isEqualTo(winners);
-        mocked.close();
+    }
+
+    void moveForwardForGivenLaps() {
+        for (int i = 0; i < this.laps; i++) {
+            for (int j = 0; j < this.cars.size(); j++) {
+                this.cars.get(j).moveForward();
+            }
+        }
     }
 }
