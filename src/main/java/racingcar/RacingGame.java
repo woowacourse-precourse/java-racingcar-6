d57612f49cb1360b2cd7fd6854aa84aa8b55fd35
SeYoE <onetuks@naver.com>
2023-10-28 03:31:23 +0900

    feat: 레이싱 로직 구현
    - 시도 횟수만큼 경주 반복
    - 무작위 전진량 구하기
    - 전진량 4 이상인 경우만 (전진량-4) 만큼 전진
    - 모든 경주의 자동차 전진량 누적

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,6 +9,10 @@ import racingcar.model.Car;
 import racingcar.model.LoopCount;
 
 public class RacingGame {
+
+    private static final int BOUND = 4;
+
+    private Map<Car, Integer> scoreBoard;
 
     private final ConsoleProcessor consoleProcessor;
 
@@ -19,17 +24,35 @@ public class RacingGame {
     public void hostGame() {
         final String[] carNames = consoleProcessor.getCarNames();
 
-        Map<Car, Integer> scoreBoard = initScoreBoard(carNames);
-        LoopCount loopCount = LoopCount.of(consoleProcessor.getLoopCount());
+        scoreBoard = initScoreBoard(carNames);
+        final LoopCount loopCount = LoopCount.of(consoleProcessor.getLoopCount());
+
+        for (int round = 0; round < loopCount.getValue(); round++) {
+            playRound();
+        }
 
         consoleProcessor.closeConsole();
     }
 
+    private void playRound() {
+        scoreBoard.forEach((key, value) -> {
+            int count = regulateCount(Randoms.pickNumberInRange(0, 9));
+            scoreBoard.put(key, value + count);
+        });
+        System.out.println(scoreBoard);
+    }
+
+    private int regulateCount(final int count) {
+        final int regulatedCount = count - BOUND;
+        if (regulatedCount >= 0) {
+            return regulatedCount;
+        }
+        return 0;
+    }
+
     private Map<Car, Integer> initScoreBoard(final String[] carNames) {
         return Arrays.stream(carNames)
-                .collect(Collectors.toMap(
-                        Car::new, carName -> 0,
-                        (a, b) -> b
-                ));
+                .collect(Collectors.toMap(Car::new, carName -> 0,
+                        (a, b) -> b));
     }
 }
