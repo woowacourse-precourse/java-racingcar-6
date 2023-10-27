45c25711a53c3c6c9438692466befdab29c6743f
eotkd99 <eotkd99@gmail.com>
2023-10-27 19:19:27 +0900

    [Util] 3.1 자동차 이동 난수 생성 #1

--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,5 +10,8 @@ public class Util {
 
 	public static ArrayList<String> strToList(String carsString) {
 		return new ArrayList<>(Arrays.asList(carsString.split(Constants.DELIMITER)));
+	}
+	public static int getRandomNum() {
+		return Randoms.pickNumberInRange(0, 9);
 	}
 }
