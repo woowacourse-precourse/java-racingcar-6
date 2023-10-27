f5036ed6017207bd9625758a3fb4cfc4c9d2f410
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-27 18:01:51 +0900

    Feat: StartMessage()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,14 @@
 package racingcar;
-import camp.nextstep.edu.missionutils.*;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StartMessage();
     }
+
+    public static void StartMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
 }
