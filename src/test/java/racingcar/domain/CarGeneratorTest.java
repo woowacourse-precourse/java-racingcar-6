04ed3bacdcab67d9d13dd23ddbcfebe62c4fadb3
Junobee25 <wnsdhqo@naver.com>
2023-10-27 17:31:00 +0900

    test: LinkedHashMap_자동차리스트_값을_확인() 테스트 추가

--- src/test/java/racingcar/domain/CarGeneratorTest.java
+++ src/test/java/racingcar/domain/CarGeneratorTest.java
@@ -1,14 +1,22 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.LinkedHashMap;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarGeneratorTest {
 
     @Test
-    void createCarList() {
+    void LinkedHashMap_자동차리스트_값을_확인() {
+        LinkedHashMap<String,String> carList = new LinkedHashMap<>();
+        String[] cars = {"pobi","jason","juno"};
+        for(String car : cars) {
+            carList.put(car,"");
+        }
+        for (String car : cars) {
+            assertThat(carList).containsKey(car);
+        }
+
     }
 
     @Test
