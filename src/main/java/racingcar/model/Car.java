73996b46c324051dc04c544ebdcc2ffe743a2d75
Maljaaa <Maljaaa@github.com>
2023-10-27 16:44:54 +0900

    feat(Car): 자동차 이름 부여, 전진 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,41 @@
+package racingcar.model;
+
+import racingcar.utils.NumberGenerator;
+import racingcar.utils.RandomNumberGenerator;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    private final int STANDARD_NUMBER = 4;
+    private NumberGenerator numberGenerator;
+
+    public Car(String name, NumberGenerator numberGenerator) {
+        this.name = name;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void move() {
+        if(isOkToMove()) {
+            position++;
+        }
+    }
+
+    private boolean isOkToMove() {
+        int RANDOM_NUMBER = numberGenerator.generateNumberInRange();
+
+        if(RANDOM_NUMBER < STANDARD_NUMBER) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
