c098c8a4198025f95b65d503044b2d7e3220cb4b
gijin <rkrhdlf1546@naver.com>
2023-10-27 23:17:50 +0900

    요구사항 작성, 객체코드 생성 (Car, Race) MVC 패턴의 View, Controller 코드 생성, Car.java = 자동차 입력값 예외처리 및 움직임, 현재상황 오버라이딩 함수 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+public class Car {
+
+    private String name;
+    private int go = 0;
+
+    public Car(String name){
+        if(name == null || name.length() > 5){ // 자동차 입력값이 null, 5이상일 때 예외 호출
+            throw new IllegalArgumentException("사용자 입력 오류");
+        }
+        this.name = name;
+    }
+
+    public void move(int number){
+        if(number >= 4){
+            go++;
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(go);
+    }
+
+}
---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class Race {
+
+
+}
