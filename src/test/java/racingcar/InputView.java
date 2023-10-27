c4be84626dbfd3bd8a75d13706f4cb577ddb724c
chansoo <chansoo38@gmail.com>
2023-10-27 15:23:29 +0900

    <Feat> InputView - 경주할 자동차 이름을 입력받는 메세지를 출력하는 기능을 구현한다.

---
+++ src/test/java/racingcar/InputView.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class InputView {
+    private final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String TRY_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
+
+    public void printCarNameMessage() {
+        System.out.println(CAR_NAME_MESSAGE);
+    }
+}
