2aa50b36ed7c0f01662d67df40f2794046c8d355
Jiyoung Choi <12201817@inha.edu>
2023-10-27 16:17:40 +0900

    feat(OutputView) : 실행 결과 메시지 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,9 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+public class OutputView {
+    private final static String resultMessage = "실행 결과";
+
+    public static void printResult(){
+        System.out.println(resultMessage);
+    }
 }
