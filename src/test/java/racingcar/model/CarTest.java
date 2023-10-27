ccf56b8b66d15b03dd1cc6dd31870c7f755dbf92
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-28 01:57:10 +0900

    test(Car): 자동차 이름 예외 테스트 구현

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import racingcar.exception.BlankCarNameException;
+import racingcar.exception.CarNameRangeException;
+
+class CarTest {
+
+    @Test
+    void 자동차_이름이_5글자_초과시_예외_발생() {
+
+        assertThatThrownBy(() -> Car.createCar("JWbase"))
+                .isInstanceOf(CarNameRangeException.class)
+                .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    @Test
+    void 자동차_이름이_공백이면_예외_발생() {
+
+        assertThatThrownBy(() -> Car.createCar(" "))
+                .isInstanceOf(BlankCarNameException.class)
+                .hasMessage("자동차 이름을 입력하세요.");
+    }
+}
