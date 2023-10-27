31efb2d2356047295b62c26a2f60f3ce55e41917
scarlet0o0 <ghd7673@naver.com>
2023-10-27 17:10:23 +0900

    Feat : Engine 객체 생성 후 구현
    전진조건에 따라 전진여부를 결정하는 isForward 메소드를 만들었습니다.

--- src/main/java/racingcar/car/Engine.java
+++ src/main/java/racingcar/car/Engine.java
@@ -1,5 +1,14 @@
 package racingcar.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Engine {
+    private final static int MAX_RANDOM_VALUE = 9;
+    private final static int MIN_RANDOM_VALUE = 0;
+    private final static int FORWARD_DISTANCE_VALUE = 3;
+
+    public boolean isForward() {
+        return Randoms.pickNumberInRange(MAX_RANDOM_VALUE, MIN_RANDOM_VALUE) > FORWARD_DISTANCE_VALUE ;
+    }
 
 }
