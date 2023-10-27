0bfa2bce729aad5cfcb80c2b5ddeacdd64500648
slowcloud <sju9417@gmail.com>
2023-10-27 19:55:03 +0900

    feat: OutputView에서 시도 횟수를 입력받는 메시지를 출력하는 기능 구현

--- src/main/java/racingcar/controller/OutputView.java
+++ src/main/java/racingcar/controller/OutputView.java
@@ -3,9 +3,14 @@ package racingcar.controller;
 public class OutputView {
 
     public static final String GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String GET_ATTEMPT_MESSAGE = "시도할 횟수는 몇회인가요?";
 
     public void printGetCarNames() {
         System.out.println(GET_CAR_NAMES_MESSAGE);
+    }
+
+    public void printGetAttempt() {
+        System.out.println(GET_ATTEMPT_MESSAGE);
     }
 
 }
