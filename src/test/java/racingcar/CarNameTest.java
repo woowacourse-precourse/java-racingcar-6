5f143cd5e4ca7662a485bf5587a62ad312ce1438
space <enaenen@naver.com>
2023-10-28 02:36:31 +0900

    TEST: 자동차 이름 Wrapper class TEST CASE

---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarNameTest {
+    @Test
+    void 자동차_이름_입력_최대글자초과_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> new CarName("overSix"));
+    }
+
+    @Test
+    void 자동차_이름_입력_공백_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> new CarName(""));
+    }
+
+    @Test
+    void 자동차_이름_입력_null_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> new CarName(null));
+    }
+
+
+    @Test
+    void 자동차_이름_정상입력() {
+        assertDoesNotThrow(() -> new CarName("pobi"));
+    }
+
+    @Test
+    void 자동차_이름_입력_확인() {
+        String pobi = "pobi";
+        CarName pobiCar = new CarName(pobi);
+        assertEquals(pobi, pobiCar.getName());
+    }
+
+}
