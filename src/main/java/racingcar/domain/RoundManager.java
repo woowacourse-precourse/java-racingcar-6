15902a1744f5337d4f6749f62416fa4614a53c80
Hoo-sung <gntjd135@gmail.com>
2023-10-27 10:07:29 +0900

    feat(RoundManager): Car의 직진 여부를 결정하는 shouldCarMove 함수 생성

--- src/main/java/racingcar/domain/RoundManager.java
+++ src/main/java/racingcar/domain/RoundManager.java
@@ -1,4 +1,11 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class RoundManager {
+
+    public boolean shouldCarMove(){
+        int randomValue = RandomNumberGenerator.randomNumGenerate();
+        return randomValue >=4;
+    }
 }
