523bd1ed12caff1303efc2c21683b5cb0b395762
devhoya97 <diajbh123@gmail.com>
2023-10-27 13:59:41 +0900

    test(racingcar) : 자동차이름 입력 테스트 추가

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class InputViewTest {
-  
+    private InputStream originalSystemIn;
+
+    @BeforeEach
+    void setUp() {
+        originalSystemIn = System.in;
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(originalSystemIn);
+    }
+
+    @Test
+    void getCars_테스트() {
+        //given
+        InputStream inputStream = new ByteArrayInputStream("pobi, woni, jun".getBytes());
+        System.setIn(inputStream);
+        InputView inputView = new InputView();
+        //when
+        List<String> carStrings = inputView.getCars();
+        //then
+        assertThat(carStrings).containsExactly("pobi", "woni", "jun");
+    }
 }
