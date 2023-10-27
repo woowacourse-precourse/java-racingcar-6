a5c59de4bffd3748a86218cc2d41a444caaecb82
02ggang9 <02ggang9@gmail.com>
2023-10-27 19:27:50 +0900

    feat(InputView): 자동차의 이름을 받기 위한 안내 메시지 출력 기능 구현

--- src/main/java/racingcar/domain/view/InputView.java
+++ src/main/java/racingcar/domain/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.domain.view;
 
+import racingcar.domain.util.InputMessage;
+
+import static racingcar.domain.util.InputMessage.*;
+
 public class InputView {
+
+    public static void printGameStart() {
+        System.out.println(RECEIVE_CAR_NAME.getMessage());
+    }
 }
