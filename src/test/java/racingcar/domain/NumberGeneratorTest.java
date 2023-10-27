a0d55fe0e5e1adc366979469617682df92c5445b
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-10-27 17:32:20 +0900

    test(racingcar): NumberGenerator 테스트 코드 작성

--- src/test/java/racingcar/domain/NumberGeneratorTest.java
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -1,4 +1,16 @@
+package racingcar.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class NumberGeneratorTest {
-  
+
+    @Test
+    void createRandomNumber() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        int num = numberGenerator.createRandomNumber();
+        Assertions.assertThat(num).isBetween(0, 9);
+    }
 }
