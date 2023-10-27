c2c32fd4b2c2c8feafd7f21bd75bd58875e95793
SeoJiWon <sjiwon4491@gmail.com>
2023-10-27 16:55:07 +0900

    feat: OutputProcessor 최종 우승자 출력 로직 구현

--- src/main/java/racingcar/io/OutputProcessor.java
+++ src/main/java/racingcar/io/OutputProcessor.java
@@ -4,8 +4,13 @@ import racingcar.domain.Car;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 public class OutputProcessor {
+    private static final String ROUND_RESULT_FORMAT = "%s : %s\n";
+    private static final String WINNER_RESULT_FORMAT = "최종 우승자 : %s\n";
     private static final String ROAD = "-";
+    private static final String DELIMITER = ", ";
 
     private OutputProcessor() {
     }
@@ -13,7 +18,7 @@ public class OutputProcessor {
     public static void printRacingRound(final List<Car> cars) {
         for (final Car car : cars) {
             System.out.printf(
-                    "%s : %s\n",
+                    ROUND_RESULT_FORMAT,
                     car.getName().getValue(),
                     formattingPosition(car.getPosition().getValue())
             );
@@ -22,5 +27,15 @@ public class OutputProcessor {
 
     private static String formattingPosition(final int position) {
         return ROAD.repeat(position);
+    }
+
+    public static void printWinners(final List<Car> winners) {
+        System.out.printf(WINNER_RESULT_FORMAT, formattingWinners(winners));
+    }
+
+    private static String formattingWinners(final List<Car> winners) {
+        return winners.stream()
+                .map(winner -> winner.getName().getValue())
+                .collect(joining(DELIMITER));
     }
 }
