a2d797c63859dc7de4a493081f38174d885f0ce7
Namyejun <yejun1148@gmail.com>
2023-10-27 15:21:40 +0900

    메인 메소드 변경사항 없음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,10 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.implementation.RacingGame;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
