67df56d248d3ea2d60b10798a1c6ec947043b114
eom-tae-in <eti0728@daum.net>
2023-10-28 08:04:00 +0900

    test: 테스트를 편하게 하기 위해 TestNumberGenerator 클래스 구현

---
+++ src/test/java/helper/TestNumberGenerator.java
@@ -0,0 +1,17 @@
+package helper;
+
+import racingcar.util.NumberGenerator;
+
+public class TestNumberGenerator implements NumberGenerator {
+
+    private final int number;
+
+    public TestNumberGenerator(final int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int generate() {
+        return number;
+    }
+}
