0fbe920d1c12ba0b398307c21cbf93736b6015a4
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-27 18:27:19 +0900

    feat: 경주 컨트롤러 구현

---
+++ src/main/java/racingcar/controller/RaceControllerImpl.java
@@ -0,0 +1,45 @@
+package racingcar.controller;
+
+import racingcar.console.Console;
+import racingcar.domain.participant.Participant;
+import racingcar.service.RaceService;
+
+import java.util.List;
+
+public class RaceControllerImpl implements RaceController {
+    public RaceControllerImpl(Console console, RaceService raceService) {
+        this.console = console;
+        this.raceService = raceService;
+    }
+    private final Console console;
+    private final RaceService raceService;
+
+    @Override
+    public void run() {
+        String carNames;
+        String raceCount;
+        List<Participant> participants;
+
+        carNames = console.getPlayerCarNames();
+        raceService.validateCarName(carNames);
+
+        raceCount = console.getRaceCount();
+        raceService.validateRaceCount(raceCount);
+
+        participants = raceService.parseToParticipantList(carNames);
+
+        System.out.println();
+        console.displayResultText();
+
+        for ( int i = 0; i < Integer.parseInt( raceCount ); i++ ) {
+            raceService.runRace(participants);
+            participants.forEach(
+                    p -> console.displayState(p.getCarName(), p.getDistance())
+            );
+            System.out.println();
+        }
+
+        raceService.filterWinner(participants);
+        console.displayWinner(participants);
+    }
+}
