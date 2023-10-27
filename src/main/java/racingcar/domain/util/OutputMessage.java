6fb01652282f19512a158679c856ab5e236aafe8
02ggang9 <02ggang9@gmail.com>
2023-10-27 19:24:18 +0900

    feat(OutputMessage): 게임 진행 중 필요한 메시지 저장

---
+++ src/main/java/racingcar/domain/util/OutputMessage.java
@@ -0,0 +1,14 @@
+package racingcar.domain.util;
+
+public enum OutputMessage {
+
+    PRINT_ROUND_RESULT("실행 결과"),
+    PRINT_FINAL_WINNER("최종 우승자 :"),
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+}
