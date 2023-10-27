6625a313763ea30f12a944bee9b779764ed92814
kyoo0115 <internetkyoomin@gmail.com>
2023-10-28 01:28:08 +0900

    feat: RacingCar 클래스 생성자 구현, go 함수 구현

--- src/main/java/racingcar/Model/RacingCars.java
+++ src/main/java/racingcar/Model/RacingCars.java
@@ -1,9 +1,24 @@
 package racingcar.Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import racingcar.View.OutputView;
 
 public class RacingCars {
+    private final ArrayList<Car> CARS = new ArrayList<>();
     public RacingCars(ArrayList<String> carNames) {
+        carNames.forEach(car -> CARS.add(new Car(car)));
+    }
 
+//    public void showWinners(){
+//        OutputView.printWinner(new ArrayList<>(CARS.stream())
+//                .filter(car -> car.isWinner(Collections.max(getPosition())))
+//                .map(Car::getName))
+//                .collect(Collectors.toList());
+//    }
+
+    private void go(){
+        CARS.forEach(Car::go);
     }
 }
