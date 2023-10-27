b8430ff1f85e13831a22d73ce14c30207bd64a2f
shin5774 <babo5774@naver.com>
2023-10-27 17:05:03 +0900

    test: 시도 횟수 입력 기능 테스트코드
    - 시도 횟수를 입력하면 해당문자열에 대해 정수인지 검증하고 통과되면 이를 정수로 변환해서 반환해주는 기능.

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -49,4 +49,15 @@ public class RacingGameTest {
             assertThat(cars.get(index).getNameValue()).isEqualTo(splitName[index]);
         }
     }
+
+    @DisplayName("콘솔로 입력된 시도횟수의 validate 판단 및 정수로 변환해서 반환해주는 기능")
+    @Test
+    void inputAttemptCount_횟수_입력(){
+        RacingGame racingGame=new RacingGame();
+        String input="12345";
+
+        assertThat(racingGame.inputAttemptCount(input)).isEqualTo(12345);
+
+    }
+
 }
