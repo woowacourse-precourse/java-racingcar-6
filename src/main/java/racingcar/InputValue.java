df539b0a83d4e86fec039dce7a21d203dd9ab37a
Chenowoo <cjsgusdn14@naver.com>
2023-10-27 18:59:04 +0900

    feat(InputValue): 몇 대의 자동차가 경주할 것인지 입력받는 기능 구현 사용자로부터 몇 대의 자동차가 경주할 것인지 입력받고 그 수에 대한 검증한 후 리턴하는 기능 구현

--- src/main/java/racingcar/InputValue.java
+++ src/main/java/racingcar/InputValue.java
@@ -1,5 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputValue {
+
+    public static int InputNumberOfCars() {
+        int numberOfCars = Integer.parseInt(Console.readLine());
+        validateNumberOfCarsRange(numberOfCars);
+        return numberOfCars;
+    }
 
 }
