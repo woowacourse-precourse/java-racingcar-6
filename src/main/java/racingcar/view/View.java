65a37b06da889e593d038d6afdc01285d3e5461e
JUNG GYUWON <milestogo0905@naver.com>
2023-10-28 02:07:26 +0900

    feat: 문자열 입력 기능 추가
    자동차의 이름이나 이동 횟수를 입력받기 위한 기능 추가

---
+++ src/main/java/racingcar/view/View.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+public class View {
+
+    private View() {}
+
+    public static String getInput() {
+        return readLine();
+    }
+}
