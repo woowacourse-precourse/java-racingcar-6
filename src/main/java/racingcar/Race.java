ebed19cc7d67a8304bf70a91afcdee1040c890e1
Han Kyuhyun <khh5345@naver.com>
2023-10-27 16:20:49 +0900

    feat: Implement game mechanics for racing cars.
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,2 +1,26 @@
-package racingcar;public class Race {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Race {
+    private final List<Car> cars;
+
+    public Race(List<String> carNames) {
+        this.cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public void raceRound() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
