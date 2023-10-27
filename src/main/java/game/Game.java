0863b9126d7bb4ae4c8f97da4ab110a10f8fc380
dyjeong365 <dyjeong365@gmail.com>
2023-10-27 15:23:46 +0900

    refactor: return input value

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -6,22 +6,27 @@ import static exception.Validator.validateNaturalNumber;
 
 public class Game {
     public void start() {
-        getCarNameInput();
-        getRoundInput();
+        String[] carNameList = getCarNameInput();
+        final int ROUND = getRoundInput();
     }
 
-    private void getCarNameInput() {
+    private String[] getCarNameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNameList = readLine().split(",");
         validateCarNameLength(carNameList);
+
+        return carNameList;
     }
 
-    private void getRoundInput() {
+    private int getRoundInput() {
         System.out.println("시도할 회수는 몇회인가요?");
-        final int COUNT = Integer.parseInt(readLine());
-        validateNaturalNumber(COUNT);
+        final int ROUND = Integer.parseInt(readLine());
+        validateNaturalNumber(ROUND);
 
         System.out.println();
+        return ROUND;
+    }
+
     private boolean canCarMoveForward(int number) {
         final int MIN_FORWARD_COUNT = 4;
 
