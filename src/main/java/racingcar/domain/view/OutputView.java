18f73377ac2e8f2cb1ebe810e258de193b3528ef
02ggang9 <02ggang9@gmail.com>
2023-10-27 21:41:46 +0900

    feat(OutputView): 게임 종료 문구 출력 기능 구현

--- src/main/java/racingcar/domain/view/OutputView.java
+++ src/main/java/racingcar/domain/view/OutputView.java
@@ -1,4 +1,9 @@
 package racingcar.domain.view;
 
+import racingcar.domain.util.OutputMessage;
+
 public class OutputView {
+    public static void printEndGame() {
+        System.out.println(OutputMessage.PRINT_FINAL_WINNER);
+    }
 }
