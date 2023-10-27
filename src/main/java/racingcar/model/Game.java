a0e2ab04ec865aaad4fc7041f6d1d8874135833b
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 20:17:57 +0900

    Fix: 승자 계산용 변수 및 getter/setter추가

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Game {
     private final List<Car> carList ;
+    private String winnerPosition;
 
     public Game(){
         carList = new ArrayList<>();
+        winnerPosition="";
     }
 
     public void addCar(Car car){
@@ -16,5 +18,13 @@ public class Game {
 
     public List<Car> getCar(){
         return carList;
+    }
+
+    public void setWinnerPosition(String winnerPosition) {
+        this.winnerPosition = winnerPosition;
+    }
+
+    public String getWinnerPosition() {
+        return winnerPosition;
     }
 }
