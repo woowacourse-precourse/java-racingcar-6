d6589aad67072c672ffb799321f66a4ddf8165f1
Isaac Han <isaachan233@gmail.com>
2023-10-27 16:25:05 +0900

    feat: 유효성 검사 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,12 +4,41 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String carNameInput() {
-        String input = Console.readLine();
-        return input;
+        String carNames = Console.readLine();
+        if (carNames == null)
+            throw new IllegalArgumentException();
+        if (carNames.trim().isEmpty())
+            throw new IllegalArgumentException();
+        if (carNames.startsWith(","))
+            throw new IllegalArgumentException();
+        if (carNames.endsWith(","))
+            throw new IllegalArgumentException();
+
+        String[] carNamesArray = carNames.split(",");
+
+        for (String carName : carNamesArray) {
+            if (carName.length() > 5)
+                throw new IllegalArgumentException();
+        }
+        return carNames;
     }
 
-    public String numberOfAttempsInput() {
-        String input = Console.readLine();
-        return input;
+    public int numberOfAttempsInput() {
+        try {
+            String numberOfAttemps = Console.readLine();
+            if (numberOfAttemps == null)
+                throw new IllegalArgumentException();
+            if (numberOfAttemps.trim().isEmpty())
+                throw new IllegalArgumentException();
+            if (numberOfAttemps.matches("^[^0-9]*$"))
+                throw new IllegalArgumentException();
+            int number = Integer.parseInt(numberOfAttemps);
+            
+            if (number < 1)
+                throw new IllegalArgumentException();
+            return number;
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException();
+        }
     }
 }
