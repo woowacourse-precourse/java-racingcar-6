677b774f7f75f7ed1f98da5de7f6170e28ab27a5
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 19:16:20 +0900

    Feat: 예외처리 파일 추가

--- src/main/java/racingcar/util/validator.java
+++ src/main/java/racingcar/util/validator.java
@@ -1,2 +1,17 @@
-package racingcar.util;public class validator {
+package racingcar.util;
+
+public class validator {
+
+    private void validateCarLength(String carName){
+        if(carName.length()>5)
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+    }
+
+    private void validateIsNumber(String input){
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c))
+                throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
 }
