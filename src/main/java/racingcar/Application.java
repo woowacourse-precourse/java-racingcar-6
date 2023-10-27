b840491934c0f10637f4f37b82c253128131a0e8
gyungchan Jo <namejgc@naver.com>
2023-10-27 15:57:03 +0900

    feat(Application): 자동차 경주 게임을 실행하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.MainController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MainController mainController = new MainController(new InputView(), new OutputView());
+        mainController.run();
     }
 }
