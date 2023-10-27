82b9d4cb3787068239f3a7a3176071614d3fae6f
Hoo-sung <gntjd135@gmail.com>
2023-10-27 17:22:58 +0900

    feat(Referee): 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.domain.GameManager;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager gm = new GameManager();
+        gm.initGame();
+        gm.StartGame();
     }
 }
