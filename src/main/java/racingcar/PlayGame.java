151d21f8159c57395cf4ee0bdf35fd3f3a88db37
4444ki <qkrwogh423@gmail.com>
2023-10-27 16:56:48 +0900

    우승자를 찾기 위한 배열 확인 가장 많이 전진한 자동차의 인덱스 값을 얻은 것을 바탕으로 우승자를 출력하고, 각 자동차의 전진한 횟수 비교하여 공동 우승자가 있는지 찾는다. feat : 우승자 또는 공동 우승자를 찾는 함수를 추가하였습니다.

--- src/main/java/racingcar/PlayGame.java
+++ src/main/java/racingcar/PlayGame.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 public class PlayGame {
@@ -13,6 +15,7 @@ public class PlayGame {
             count = CheckMoveOrStop(members, count);
             System.out.println("");
         }
+        CheckWinner(members, count);
     }
     public List<Integer> CheckMoveOrStop(List<String> members, List<Integer> count){
         int MembersSize = members.size();
@@ -26,5 +29,26 @@ public class PlayGame {
             System.out.println(members.get(i) + " : " + result);
         }
         return count;
+    }
+    public void CheckWinner(List<String> members, List<Integer> count){
+        int MaxIndex = FindMaxIndex(count);
+        int CountSize = count.size();
+        int max = count.get(MaxIndex);
+        System.out.print("최종 우승자 : " + members.get(MaxIndex));
+        for(int i=0;i<CountSize;i++){
+            if(i == MaxIndex)
+                continue;
+            if(max == count.get(i))
+                System.out.print(", " + members.get(i));
+        }
+    }
+    public int FindMaxIndex(List<Integer> count){
+        int index = 0;
+        int size = count.size();
+        for(int i = size; i <= 0; i--){
+            if(count.get(i) == Collections.max(count))
+                index = i;
+        }
+        return index;
     }
 }
