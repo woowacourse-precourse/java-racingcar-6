ce8763cc302073be9343afdaacb05174183fb9b8
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-10-27 23:21:34 +0900

    Feat: CarList 클래스 findWinner 메서드 구현
    최종 우승자를 찾는 findWinner 메서드 구현
    뒤처진 차량을 Set형태의 목록에 저장하는 addRemoveList 구현
    뒤쳐진 차량 목록인 RemoveList를 만드는 makeRemoveList 구현

--- src/main/java/racingcar/Model/CarList.java
+++ src/main/java/racingcar/Model/CarList.java
@@ -1,20 +1,20 @@
 package racingcar.Model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarList {
 
 	private final String ENTERED_SEPARATOR = ",";
-	private List<Car> carList;
+	private final List<Car> carList = new ArrayList<>();
 
 	public CarList(String enteredList) {
 
 		String[] splitEnteredList = splitEnteredList(enteredList);
 
-		carList = new ArrayList<>();
-
-		for(String eachEnteredList : splitEnteredList) {
+		for (String eachEnteredList : splitEnteredList) {
 			this.carList.add(new Car(eachEnteredList.trim()));
 		}
 
@@ -24,6 +24,43 @@ public class CarList {
 		return enteredList.split(ENTERED_SEPARATOR);
 	}
 
+	public List<Car> findWinner() {
+
+		Set<Car> removeList = makeRemoveList();
+		List<Car> winningList = this.carList;
+
+		for (Car removedCar : removeList) {
+			winningList.remove(removedCar);
+		}
+
+		return winningList;
+
+	}
+
+	private Set<Car> makeRemoveList() {
+
+		Set<Car> removeList = new HashSet<>();
+
+		for (int i = 0; i < carList.size(); i++) {
+			for (int j = 0; j < carList.size(); j++) {
+
+				addRemoveList(removeList, i, j);
+
+			}
+		}
+
+		return removeList;
+	}
+
+	private void addRemoveList(Set<Car> removeList, int referenceIndex, int comparableIndex) {
+		final int COMPARE_EQUAL_SIGN = 0;
+
+		if (carList.get(referenceIndex).compareDistance(carList.get(comparableIndex)) < COMPARE_EQUAL_SIGN) {
+			removeList.add(carList.get(referenceIndex));
+		}
+
+	}
+
 	public Car getEachCar(int index) {
 		return carList.get(index);
 	}
@@ -31,8 +68,5 @@ public class CarList {
 	public int size() {
 		return carList.size();
 	}
-
-
-
 
 }
