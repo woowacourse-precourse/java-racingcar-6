7c180c2a6cf7ad26c9f6531889f79dd4dd3f9932
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-27 16:25:55 +0900

    refactor - 매개변수를 도메인에서 Dto로 변경

--- src/main/java/racingcar/service/Referee.java
+++ src/main/java/racingcar/service/Referee.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.domain.Car;
-import racingcar.domain.Cars;
+import racingcar.dto.CarsGenerateDto;
 import racingcar.dto.WinnerCarResponse;
 
 public class Referee {
@@ -12,18 +12,18 @@ public class Referee {
     private static final int MAX_VALUE = 0;
     private static final String MULTI_WINNER_DELIMITER = ", ";
 
-    public WinnerCarResponse judgeWinner(Cars cars) {
-        int maxCount = findMaxPosition(cars);
+    public WinnerCarResponse judgeWinner(CarsGenerateDto carsGenerateDto) {
+        int maxCount = findMaxPosition(carsGenerateDto);
 
-        List<Car> carsList = cars.getCars().stream()
+        List<Car> carsList = carsGenerateDto.cars().stream()
                 .filter(x -> x.getCarPosition() == maxCount)
                 .toList();
 
         return new WinnerCarResponse(carsList);
     }
 
-    private int findMaxPosition(Cars cars) {
-        List<Integer> list = cars.getCars().stream()
+    private int findMaxPosition(CarsGenerateDto generatedCars) {
+        List<Integer> list = generatedCars.cars().stream()
                 .map(Car::getCarPosition)
                 .sorted(Comparator.reverseOrder())
                 .toList();
@@ -31,8 +31,8 @@ public class Referee {
         return list.get(MAX_VALUE);
     }
 
-    public String getWinner(Cars cars) {
-        WinnerCarResponse winnerCarResponse = judgeWinner(cars);
+    public String getWinnersCarName(CarsGenerateDto generatedCars) {
+        WinnerCarResponse winnerCarResponse = judgeWinner(generatedCars);
         return winnerCarResponse.cars().stream()
                 .map(Car::getCarName)
                 .collect(Collectors.joining(MULTI_WINNER_DELIMITER));
