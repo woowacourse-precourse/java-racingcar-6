d074fbd2287ccc8e79c2e3768cf7b1732dedd043
Roista57 <Roista57@gmail.com>
2023-10-27 12:54:55 +0900

    경주할 자동차 이름을 입력받는 기능과 시도할 횟수를 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racingCarNames = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String iterateNumber = Console.readLine();
     }
 }
