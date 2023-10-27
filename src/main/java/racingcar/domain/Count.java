272c187826cc3d0e7994f10b048f18bece1a587a
Yoon-HaeJun <0681hj@naver.com>
2023-10-27 21:18:19 +0900

    시도할 횟수 입력

---
+++ src/main/java/racingcar/domain/Count.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Count {
+    public int usercount(){
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+}
