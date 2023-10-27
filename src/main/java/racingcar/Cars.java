d33d4fe80cdb0f7633777ab600f6b0bf96d10e71
devhoya97 <diajbh123@gmail.com>
2023-10-27 14:01:08 +0900

    refactor(racingcar) : 일급컬렉션 생성자 변경

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -4,14 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
-    private static final String DELIMITER = ",";
     private List<Car> cars;
 
-    public Cars(String carsString) {
-        List<Car> cars = Arrays.stream(carsString.split(DELIMITER))
-                .map(String::trim)
-                .map(Car::new)
-                .toList();
+    public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
