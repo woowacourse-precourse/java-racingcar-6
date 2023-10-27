cb5a9af8dbf4f9534beb6e94cf75333a237a40a6
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-27 15:41:05 +0900

    [feat] : add group cars by name method

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,7 +1,11 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
 import racingcar.printer.GameGuidePrinter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameService {
     private final GameGuidePrinter gameGuidePrinter;
@@ -13,11 +17,17 @@ public class GameService {
     public void startGame() {
         gameGuidePrinter.getCarNameInputMessage();
         String carList = carNameInput();
+        String[] cars = groupCarsByName(carList);
         gameGuidePrinter.getTryCountMessage();
     }
 
     private String carNameInput() {
         String carList = Console.readLine();
         return carList;
+    }
+
+    private String[] groupCarsByName(String carList) {
+        String[] cars = carList.split(",");
+        return cars;
     }
 }
