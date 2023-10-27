c0a419863630b937ce3efa353feeee27ed3188fc
1000zoo <cjswldn99@gmail.com>
2023-10-28 05:23:13 +0900

    test(RaceCar): 기존 테스트에 MockedStatic 적용

--- src/test/java/racingcar/race/RaceCarTest.java
+++ src/test/java/racingcar/race/RaceCarTest.java
@@ -1,10 +1,29 @@
 package racingcar.race;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import racingcar.CarName;
 
 public class RaceCarTest {
+
+    private static MockedStatic<Randoms> random;
+
+    @BeforeAll
+    public static void beforeAll() {
+        random = mockStatic(Randoms.class);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        random.close();
+    }
 
     @Test
     @DisplayName("RaceCar 대소비교")
@@ -17,6 +36,7 @@ public class RaceCarTest {
         RaceCar car2 = new RaceCar(name2);
 
         //when
+        random.when(() -> Randoms.pickNumberInRange(0, 9)).thenReturn(6);
         car1.moveForward();
         int compare1 = car1.compareTo(car2);
         car2.moveForward();
@@ -24,7 +44,7 @@ public class RaceCarTest {
         int compare2 = car1.compareTo(car2);
 
         // then
-//        assertThat(compare1).isEqualTo(1);
-//        assertThat(compare2).isEqualTo(-1);
+        assertThat(compare1).isEqualTo(1);
+        assertThat(compare2).isEqualTo(-1);
     }
 }
