fbb0ba6e9f08dccbdf2c7c4493ddbd1000de5c85
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-27 23:07:46 +0900

    refactor - "-" 출력 부분 책임 고려하여 분리

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -3,25 +3,18 @@ package racingcar.domain;
 import java.util.Objects;
 
 public class Position {
-
-    private static final String CAR_POSITION = "-";
-    private static final int MIN_VALUE = 0;
     private int carPosition;
 
     public Position(int carPosition) {
         this.carPosition = carPosition;
     }
 
-    public void increasePosition() {
+    public void increase() {
         carPosition++;
     }
 
     public int getCarPosition() {
         return carPosition;
-    }
-
-    public StringBuilder countPositionToString(StringBuilder stringBuilder) {
-        return stringBuilder.append(CAR_POSITION.repeat(Math.max(MIN_VALUE, carPosition)));
     }
 
     @Override
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,12 +3,15 @@ package racingcar.view;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.Position;
 import racingcar.service.Referee;
 
 public class OutputView {
 
-    private static final String DELIMITER = " : ";
 
+    private static final String CAR_POSITION = "-";
+    private static final int MIN_VALUE = 0;
+    private static final String DELIMITER = " : ";
     private static final String FINAL_WINNER = "최종 우승자";
 
     public void showResultMessage() {
@@ -19,9 +22,13 @@ public class OutputView {
     public void showResult(List<Car> cars) {
         for (Car car : cars) {
             StringBuilder stringBuilder = new StringBuilder();
-            System.out.println(car.getCarName() + DELIMITER + car.getPosition().countPositionToString(stringBuilder));
+            System.out.println(car.getCarName() + DELIMITER + countPositionToString(stringBuilder, car.getPosition()));
         }
         System.out.println();
+    }
+
+    public StringBuilder countPositionToString(StringBuilder stringBuilder, Position position) {
+        return stringBuilder.append(CAR_POSITION.repeat(Math.max(MIN_VALUE, position.getCarPosition())));
     }
 
     public void showWinner(Referee referee, Cars generatedCars) {
