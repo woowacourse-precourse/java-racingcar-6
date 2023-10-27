9f43b4bef17c87d24dc98fa9e2d52459fc11975f
shAn-kor <kjn0406@naver.com>
2023-10-27 17:25:51 +0900

    가독성 향상

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,7 +2,6 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
