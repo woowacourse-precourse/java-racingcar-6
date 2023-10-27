ebd23764be3ea8f2bb71d67eb8f907a11bb96d5d
hoyun06 <hoyun06@gmail.com>
2023-10-27 14:13:58 +0900

    feat: GameResultGenerator에 generateResult, getWinnerNames 메소드 추가
    - generateResult: 내부적으로 getWinnerNames 호출한 뒤에 결과가 담긴 문자열을 GameResult에 담아서 반환
    - getWinnerNames: 우승을 한 자동차의 이름을 문자열로 반환

--- src/main/java/racingcar/domain/GameResultGenerator.java
+++ src/main/java/racingcar/domain/GameResultGenerator.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class GameResultGenerator {
 
-    public void generateResult(List<Car> cars) {
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+
+    public static GameResult generateResult(Cars cars) {
+        List<Car> winnerCars = cars.getWinnerCars();
+        String winnerNames = getWinnerNames(winnerCars);
+        return new GameResult(FINAL_WINNER_MESSAGE + winnerNames);
+    }
+
+    private static String getWinnerNames(List<Car> winnerCars) {
+        StringBuilder sb = new StringBuilder();
+        winnerCars.forEach(car -> sb.append(car.getName() + ", "));
+
+        int len = sb.length();
+        sb.delete(len - 2, len);
+        return sb.toString();
     }
 }
