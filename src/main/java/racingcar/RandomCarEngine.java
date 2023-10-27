65ebb92799979281f5fbd36db884ba994afbbbc6
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 17:35:09 +0900

    style(RandomCarEngine): 상수 선언부 빈 라인 삭제

--- src/main/java/racingcar/RandomCarEngine.java
+++ src/main/java/racingcar/RandomCarEngine.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomCarEngine implements CarEngine {
-
     public static final int MIN_RANDOM_NUMBER = 0;
     public static final int MAX_RANDOM_NUMBER = 9;
     public static final int MIN_START_ENGINE_NUMBER = 4;
