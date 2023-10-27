3e6c1616b339cac576cc13b110bfc4c6bc3563e0
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2023-10-27 21:15:42 +0900

    리드미 수정 및 모든 기능 함수 구현

--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 # 기능 요구 사항
 ## 입력 데이터 <br>
 > 경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)<br>
->> 자동차 n개 (1개 이상?), 자동차 5자 이하<br>
+>> 자동차 n개 (1개 이상?), 자동차 5자 이하, 중복 가능?, 입력하지 않을 경우<br>
 > 
 >시도할 회수<br>
->> 제한된 요구사항 X
+>> 제한된 요구사항 X, 입력하지 않을 경우 throw
  <br>
 > 
 
@@ -27,33 +27,25 @@
 > 입력받은 자동차명 처리하는 함수 makeMapCars
 > ```java
 > //입력받은 이름을 해시맵의 key로 정리
-> public static Map<Integer> makeMapCars(String names)
+> public static LinkedHashMap<String, Integer> makeCarMap(String names)
 >```
 
-> #### 출력함수
->입력받은 횟수만큼 차수별 실행 결과 출력 loopResult
+> #### 출력 함수
+>입력받은 횟수만큼 차수별 실행 결과 출력 nLoop
 > ```java
-> public static void loopResult(int count)
+> public static LinkedHashMap nLoop(LinkedHashMap cars_map, long number)
 >```
->  단독 우승자 안내 문구 출력 printWinner
+> 횟수 한번의 carsDistance를 구하는 함수 oneLoop
 > ```java
-> public static void printWinner(list winner)
+> public static LinkedHashMap oneLoop(LinkedHashMap cars_map)
 >```
->  공동 우승자 안내 문구 출력 printWinners
+> 우승자를 출력하는 함수 winners
 > ```java
-> public static void printWinners(List winners)
->```
-
->#### 입력한 데이터를 출력하기 위한 함수
-> 자동차별로 전진하는 함수 carsDistance
-> ```java
-> //각 자동차별 랜던값을 생성 후 출력 -> 4 이상일 경우 count
-> //Randoms.pickNumberInRange(0,9)를 사용
-> public static int randomDistance(Map cars)
->```
-> 우승자 List를 만드는 함수 makeWinnerList
-> ```java
-> //각 자동차별 랜던값을 생성 후 출력 -> 4 이상일 경우 count
-> public static List makeWinnerList(Map cars)
+> public static void winners(LinkedHashMap cars_map)
 >```
 
+<br>
+<br>
+
+https://docs.oracle.com/javase/8/docs/api/
+https://github.com/AdoptOpenJDK/openjdk-jdk8u/blob/master/jdk/src/share/classes/java/util/LinkedHashMap.java
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,9 @@
 package racingcar;
 
-import java.util.HashMap;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -10,24 +13,84 @@ public class Application {
         // TODO: 프로그램 구현
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Map <String, Integer> cars_map= makeMapCars(readLine());
+        LinkedHashMap <String, Integer> cars_map= makeCarMap(readLine());
 
         System.out.println("시도할 회수는 몇회인가요?");
-        long N=Integer.parseInt(readLine());
+        long number=Integer.parseInt(readLine());
+        if(number==0)
+            throw new IllegalArgumentException("0이상이어야 합니다.");
 
         System.out.println("\n실행 결과");
+
+        winners(nLoop(cars_map,number));
 
     }
 
     //입력받은 자동차명을 해시맵 키값으로 설정
-    public static HashMap<String,Integer> makeMapCars(String names){
-        HashMap <String, Integer> cars_map= new HashMap<String, Integer>();
+    public static LinkedHashMap<String,Integer> makeCarMap(String names){
+        LinkedHashMap <String, Integer> cars_map= new LinkedHashMap<>();
         String[] name_array=names.split(",");
 
         for(int i=0;i< name_array.length;i++){
+            if(name_array[i].length()>5){
+                throw new IllegalArgumentException("자동차명을 5자 이하로 작성하세요.");
+            }
+            if(cars_map.containsKey(name_array[i].trim())){
+                throw new IllegalArgumentException("자동차명 중복을 허용하지 않습니다.");
+            }
             cars_map.put(name_array[i].trim(),0);
         }
 
         return cars_map;
     }
+
+
+    //한번의 루프를 입력받은 수만큼 돌리기 위한 함수
+    public static LinkedHashMap nLoop(LinkedHashMap cars_map, long number){
+        for(long i=0;i<number;i++){
+            oneLoop(cars_map);
+        }
+
+        return cars_map;
+    }
+
+
+    //자동차 전진 수 랜덤값 구하기
+    public static LinkedHashMap oneLoop(LinkedHashMap cars_map) {
+        Iterator<Map.Entry<String, Integer>> iter = cars_map.entrySet().iterator();
+        while (iter.hasNext()) {
+            var entry = iter.next();
+
+            int int_distance= Randoms.pickNumberInRange(0,9);
+            if(int_distance>3){//4이상일 경우 전진
+                cars_map.put(entry.getKey(),(int)cars_map.get(entry.getKey())+1);
+            }
+            String str_distance="-".repeat(int_distance);
+            System.out.println(entry.getKey() + " : " + str_distance);
+        }
+
+        System.out.println();
+        return cars_map;
+    }
+
+
+    //우승자 출력하기
+    public static void winners(LinkedHashMap cars_map){
+        String winner_name="";
+        long max=0;
+
+        Iterator<Map.Entry<String, Integer>> iter = cars_map.entrySet().iterator();
+        while (iter.hasNext()) {
+            var entry = iter.next();
+
+            if((int)cars_map.get(entry.getKey())>max){
+                winner_name=entry.getKey();
+            }else if((int)cars_map.get(entry.getKey())>=max){
+                winner_name+=", "+entry.getKey();
+            }
+        }
+
+        System.out.println("최종 우승자 : "+winner_name);
+    }
+
 }
