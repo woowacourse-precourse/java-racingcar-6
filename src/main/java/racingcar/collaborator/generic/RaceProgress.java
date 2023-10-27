2e682722c71ab18883ca9a86cb98b1248eb515bc
aka-nick <bechalrang@gmail.com>
2023-10-27 11:27:06 +0900

    수정(RaceProgress) : 필드명 오타 수정
    raceProgress -> racerProgress로 수정
    
    RaceProgress

--- src/main/java/racingcar/collaborator/generic/RaceProgress.java
+++ src/main/java/racingcar/collaborator/generic/RaceProgress.java
@@ -2,5 +2,5 @@ package racingcar.collaborator.generic;
 
 import java.util.List;
 
-public record RaceProgress(List<RacerProgress> raceProgress) {
+public record RaceProgress(List<RacerProgress> racerProgress) {
 }
