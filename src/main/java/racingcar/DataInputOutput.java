121a9992da6b95a4dfb638cbd14f3d875b3265a7
devpanic <yeon.dev.8@gmail.com>
2023-10-27 16:46:02 +0900

    feat(printSystemMessage) : Add Printing System Message

--- src/main/java/racingcar/DataInputOutput.java
+++ src/main/java/racingcar/DataInputOutput.java
@@ -6,8 +6,14 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class DataInputOutput {
+    static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String INPUT_MOVING_MSG = "시도할 회수는 몇회인가요?";
+    static final String OUTPUT_RESULT_START = "실행 결과";
+    static final String OUTPUT_RESULT_END = "최종 우승자";
+
     private static List<Car> carList = new ArrayList<Car>();
     private static int numTotalMoving;
+
     public void userInputCarsInfo(){
         // Validate Here
         StringTokenizer tokenizer = new StringTokenizer(Console.readLine(), ",");
@@ -23,7 +29,14 @@ public class DataInputOutput {
         //Validate Here
         numTotalMoving = Integer.parseInt(userInput);
     }
-    public void printSystemMessage(){}
+    public void printSystemMessage(String whichMsg){
+        switch (whichMsg) {
+            case "START" -> System.out.println(INPUT_CAR_NAME_MSG);
+            case "INPUT_MOVING" -> System.out.println(INPUT_MOVING_MSG);
+            case "OUTPUT_START" -> System.out.println(OUTPUT_RESULT_START);
+            case "OUTPUT_END" -> System.out.println(OUTPUT_RESULT_END);
+        }
+    }
     public void printMovingResult(){}
     public void makeRandomNumber(){}
 }
