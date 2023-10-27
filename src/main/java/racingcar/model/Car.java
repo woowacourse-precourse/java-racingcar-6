1be2a6495f400795afd65abe6b085401c2e88218
peppone-choi <peppone.choi@gmail.com>
2023-10-28 02:57:56 +0900

    feat: Car 내 distance 변수를 객체로 포장

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,31 +1,31 @@
 package racingcar.model;
 
 public class Car {
-    private CarName name;
-    private int distance; // TODO : 원시 값 포장 대상
+    private final CarName name;
+    private Distance distance;
 
     public Car(String name) {
         this.name = new CarName(name);
-        this.distance = 0;
+        this.distance = new Distance(0);
     }
 
     public CarName getName() {
         return name;
     }
 
-    public void addDistance(int dice, int forward) {
-        if (dice >= forward) {
-            distance++;
+    public void forward(int dice, int numberForForward) {
+        if (dice >= numberForForward) {
+            this.distance = distance.addDistance();
         }
     }
 
     public String getDistanceLine() {
         StringBuilder stringBuilder = new StringBuilder();
         return String.valueOf(
-                stringBuilder.append(getName().getRawName()).append(" : ").append("-".repeat(Math.max(0, this.distance))));
+                stringBuilder.append(getName().getRawName()).append(" : ").append("-".repeat(Math.max(0, this.distance.getDistance()))));
     }
 
-    public Integer getDistance() {
+    public Distance getDistance() {
         return distance;
     }
 }
