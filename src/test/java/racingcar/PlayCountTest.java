aa0f1abd8180f53c903adb5557491999cc6e418b
devholic22 <hyunjoon.tech@gmail.com>
2023-10-27 18:02:18 +0900

    test: 횟수 음수 입력 시 예외 테스트
    횟수에 음수 입력 시 예외 테스트 진행

--- src/test/java/racingcar/PlayCountTest.java
+++ src/test/java/racingcar/PlayCountTest.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.ExceptionMessage.PLAY_COUNT_VALUE_EXCEPTION;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.model.PlayCount;
 
@@ -9,5 +13,15 @@ public class PlayCountTest {
     void 횟수는_양수일_때_가능하다() {
         // given & when
         PlayCount count = PlayCount.from("5");
+    }
+
+    @Test
+    void 횟수는_음수일_수_없다() {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           PlayCount count = PlayCount.from("-3");
+        });
+        // then
+        assertThat(exception.getMessage()).isEqualTo(PLAY_COUNT_VALUE_EXCEPTION.toString());
     }
 }
