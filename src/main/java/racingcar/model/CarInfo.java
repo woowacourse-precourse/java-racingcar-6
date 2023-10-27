3c71f7eef09aeae57ffcd1b09ea0348cca9c26bd
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-27 17:49:29 +0900

    feat(CarInfo): add setCarName function
    Add setCarName function to save car names in ArrayList.

---
+++ src/main/java/racingcar/model/CarInfo.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import racingcar.util.validation;
+
+public class CarInfo {
+    private final List<String> carName = new ArrayList<>();
+    public void setCarName(String carName) {
+        String[] splitStr = carName.split(",");
+        Collections.addAll(this.carName, splitStr);
+        for (String s : this.carName) {
+            validation.checkCarName(s);
+        }
+    }
+}
