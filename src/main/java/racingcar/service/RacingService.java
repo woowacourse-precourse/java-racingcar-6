507d4f9f004948bcd844541951587feee3e7d48f
hwan2-99 <thh1233123@gmail.com>
2023-10-27 16:03:51 +0900

    fix: 자동차 전진 조건 4초과에서 4 이상으로 수정

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -5,6 +5,6 @@ import racingcar.model.NumberEnum;
 public class RacingService {
     public boolean moveOrStop(){
         int random = Randoms.pickNumberInRange(NumberEnum.MIN_RANGE.getNumber(), NumberEnum.MAX_RANGE.getNumber());
-        return random > NumberEnum.REQUIREMENT_MOVE.getNumber();
+        return random >= NumberEnum.REQUIREMENT_MOVE.getNumber();
     }
 }
