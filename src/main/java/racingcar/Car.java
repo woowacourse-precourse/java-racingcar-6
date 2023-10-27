a33b447572c199dc94056d9aa78eb0ba09d64cde
parking0 <p980710@gmail.com>
2023-10-27 17:13:01 +0900

    feat : 자동차 객체 함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,12 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 
+    public static final int LIMIT_NUMBER = 4;
     private final String name;
     private String move;
 
     public Car(String name){
         this.name = name;
         this.move = "";
+    }
+
+    public void run(){
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        if(randomNum >= LIMIT_NUMBER){
+            addMove();
+        }
     }
 
     public void addMove(){
