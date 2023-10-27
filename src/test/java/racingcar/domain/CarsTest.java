ee6a76fb3620ba8cb8956ca46590a5300bdf2f2e
yeseong31 <yeseong31@naver.com>
2023-10-27 13:05:19 +0900

    test(Cars): Cars 생성 및 예외 입력 값 추가 및 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,42 +3,56 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CarsTest {
-    
+
     @Nested
     @DisplayName("자동차 이름 목록 생성")
     class CreateCars {
-        
+
+        private static Stream<Arguments> successCarNames() {
+            return Stream.of(
+                    Arguments.of(List.of("poby", "woni", "jun")),
+                    Arguments.of(List.of("qm6", "cona", "k9", "motor"))
+            );
+        }
+
         @DisplayName("자동차 이름 목록을 올바르게 입력")
         @ParameterizedTest
-        @ValueSource(strings = "poby,woni,jun")
-        void string_자동차_이름_목록을_올바르게_입력(String names) {
-            assertThatNoException().isThrownBy(()
-                    -> new Cars(names));
+        @MethodSource("successCarNames")
+        void string_자동차_이름_목록을_올바르게_입력(List<String> names) {
+            assertThatNoException().isThrownBy(() -> new Cars(names));
         }
     }
-    
+
     @Nested
     @DisplayName("자동차 이름 목록 예외")
     class ValidateCars {
-        
-        @DisplayName("자동차 이름 목록이 빈 칸이면 예외 발생")
-        @ParameterizedTest
-        @ValueSource(strings = "poby,woni,jun")
-        void blank_자동차_이름_목록이_빈_칸이면_예외_발생() {
-            assertThatThrownBy(() -> new Cars(""))
-                    .isInstanceOf(IllegalArgumentException.class);
+
+        private static Stream<Arguments> exceptionCarNames() {
+            return Stream.of(
+                    Arguments.of(List.of(",")),
+                    Arguments.of(List.of(" , , ")),
+                    Arguments.of(List.of(", poby")),
+                    Arguments.of(List.of(",,,,,")),
+                    Arguments.of(List.of("poby,,jun")),
+                    Arguments.of(List.of("poby,poby")),
+                    Arguments.of(List.of("poby, jun")),
+                    Arguments.of(List.of("pobby111,woni222"))
+            );
         }
-    
+
         @DisplayName("자동차 이름 목록에 유효하지 않은 자동차 이름이 있으면 예외 발생")
         @ParameterizedTest
-        @ValueSource(strings = {", poby", ",", ",,,,,", "poby,,jun", "poby, jun", "poby111,woni"})
-        void blank_자동차_이름_목록에_유효하지_않은_자동차_이름이_있으면_예외_발생(String names) {
+        @MethodSource("exceptionCarNames")
+        void blank_자동차_이름_목록에_유효하지_않은_자동차_이름이_있으면_예외_발생(List<String> names) {
             assertThatThrownBy(() -> new Cars(names))
                     .isInstanceOf(IllegalArgumentException.class);
         }
