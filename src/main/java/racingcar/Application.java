86d357435ce00870f854d956dba1398ffbbfea74
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-27 18:10:57 +0900

    Feat: InputCarName()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,22 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    static String InputCarName = "";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         StartMessage();
+        InputCarName();
     }
 
     public static void StartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void InputCarName() {
+        InputCarName = readLine();
     }
 
 }
