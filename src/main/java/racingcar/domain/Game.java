20f850f9800e2f8e27f462bd601be8bfcd68415c
unknown <y2kf9a0q@gmail.com>
2023-10-28 03:01:01 +0900

    feat: skeleton of Game.java

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private int gameTimes;
+    private List<Car> cars = new ArrayList<>();
+
+    public void run() {
+
+    }
+
+    public void inputCarNames() {
+
+    }
+
+    public void inputGameTimes() {
+
+    }
+
+    public void getWinners() {
+
+    }
+}
