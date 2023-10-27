d8cd9acf07dd6308c0f6f4da222e3b58dbf356e6
gyungchan Jo <namejgc@naver.com>
2023-10-27 15:48:56 +0900

    feat(OutputView): 자동차 경주 게임의 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNERS_MESSAGE = "최종 우승자";
+
+    private static final String MESSAGE_FORMAT = "%s : %s";
     private static final String POSITION_EXPRESSION_SYMBOL = "-";
+    private static final String NAME_DELIMITER = ", ";
 
     public void printResultMessage() {
         newLine();
@@ -22,10 +26,14 @@ public class OutputView {
     }
 
     private String generateMessageOf(CarDto car) {
-        return String.format("%s : %s", car.name(), generatePositionMessage(car.position()));
+        return String.format(MESSAGE_FORMAT, car.name(), generatePositionMessage(car.position()));
     }
 
     private String generatePositionMessage(int position) {
         return POSITION_EXPRESSION_SYMBOL.repeat(position);
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.printf(MESSAGE_FORMAT, WINNERS_MESSAGE, String.join(NAME_DELIMITER, winners));
     }
 }
