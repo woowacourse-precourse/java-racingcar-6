3f5798c8f7ea1e5ddea96669e80e35eee08865d7
junOTL <naqp99@gmail.com>
2023-10-27 14:12:53 +0900

    feat: 무작위 값이 4 이상일 경우 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,8 +3,21 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public int getRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void move() {
+        int randomNumber = getRandomNumber();
+        if (randomNumber >= 4) {
+            this.position++;
+        }
     }
 }
