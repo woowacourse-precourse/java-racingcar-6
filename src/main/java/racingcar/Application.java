4596a02fa06bc654ce04df24619c421ae9eebf6b
DohunHyun <dohun.hyun@gmail.com>
2023-10-27 16:02:22 +0900

    Feat : 자동차 입력 부분 개발
    자동차 이름을 입력받고, 쉼표를 기준으로 분리해서 객체 Car 생성 및 ArrayList로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] input = Console.readLine().split(",");
+        ArrayList<Car> carList = new ArrayList<>();
+        for(String name : input) {
+            carList.add(new Car(name, 0));
+        }
     }
 }
---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+	String name;
+	int count;
+
+	public Car(String name, int count) {
+		this.name = name;
+		this.count = count;
+	}
+}
