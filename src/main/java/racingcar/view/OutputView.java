caf6551f1635e67d54076dde3b91dc83aa4ea7f9
Jang DaeYoung <789456jang@naver.com>
2023-10-27 15:18:46 +0900

    feat: 각 차수별 실행 결과

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,9 @@
 package racingcar.view;
 
+import racingcar.dto.RacingResult;
+
+import java.util.List;
+
 public class OutputView {
 
     private static final OutputView instance = new OutputView();
@@ -9,6 +13,15 @@ public class OutputView {
     }
 
     private OutputView() {
+    }
+
+    public void printResult(List<RacingResult> results) {
+        System.out.println("실행 결과");
+        results.forEach(result -> {
+            String name = result.getCarName();
+            String position = "-".repeat(result.getPosition());
+            System.out.println(name + " : " + position);
+        });
     }
 
 }
