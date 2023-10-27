e9f9e51aa80d77bcb60949f5c36c3b1b0d23036c
김영준 <rladudwns164@gmail.com>
2023-10-27 22:16:28 +0900

    test(ValidatorTest) : Validator 테스트 클래스 구현
    -ValidatorTest 클래스를 Validator의 기능을 테스트 합니다.

---
+++ src/test/java/test/ValidatorTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.util.Validator;
+
+public class ValidatorTest {
+    @Test
+    void InpuRaceCount_Valid_Test() {
+        assertDoesNotThrow(() -> Validator.InputRaceCount("4"));
+    }
+
+    @Test
+    void InpuRaceCount_InValid_Test() {
+        assertThrows(IllegalArgumentException.class, () -> Validator.InputRaceCount("가"));
+    }
+
+    @Test
+    void InputName_Invalid_Name_Length() {
+        List<String> cars = Arrays.asList("car123456", "car2", "car3");
+        assertThrows(IllegalArgumentException.class, () -> Validator.InputName(cars));
+    }
+
+    @Test
+    void InputName_Invalid_Name_Duplication() {
+        List<String> cars = Arrays.asList("car", "car");
+        assertThrows(IllegalArgumentException.class, () -> Validator.InputName(cars));
+    }
+
+}
+
