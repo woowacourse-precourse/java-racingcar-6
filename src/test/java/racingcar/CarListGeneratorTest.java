32c906235a200c934d155e5a8cb67a979ac867e3
devhoya97 <diajbh123@gmail.com>
2023-10-27 14:49:58 +0900

    test(racingcar) : 자동차 객체 리스트 생성기능 테스트

---
+++ src/test/java/racingcar/CarListGeneratorTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CarListGeneratorTest {
+
+    @Test
+    void getCarList() {
+        //given
+        List<String> carsString = Arrays.asList("pobi", "woni", "jun");
+        //when
+        //then
+        assertThat(CarListGenerator.getCarList(carsString).get(0)).isInstanceOf(Car.class);
+    }
+}
