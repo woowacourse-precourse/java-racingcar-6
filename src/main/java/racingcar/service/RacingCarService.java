f6bd46fac59c3a1129be23bbd48f4a8b21f97bbe
ParkDH0809 <znxn1237@gmail.com>
2023-10-27 21:58:27 +0900

    feat(service): 0~9 난수 반환 구현

---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,8 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class RacingCarService {
+    public static int getRandomNumber() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
