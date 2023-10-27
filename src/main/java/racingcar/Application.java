6f373ff28815cffe66ef0d1c9bd2908e4ba8486b
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-27 19:21:49 +0900

    feat: 애플리케이션 구동을 위해 Application.main() 메서드 작성 및 테스트 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.container.ApplicationContainer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new ApplicationContainer().getRaceController().run();
     }
 }
