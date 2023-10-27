46297b213b1d03c4109c442521c1a752967602e5
june-777 <wlwhswnsrl96@gmail.com>
2023-10-27 21:41:32 +0900

    feat(View): 경주 1회 결과 출력 기능

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class View {
     public void printGameStartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,5 +13,10 @@ public class View {
 
     public void printPlayResultText() {
         System.out.println("\n실행 결과");
+    }
+
+    public void printResult(List<String> names, List<Long> moveCounts) {
+        String result = convertToResultForm(names, moveCounts);
+        System.out.println(result);
     }
 }
