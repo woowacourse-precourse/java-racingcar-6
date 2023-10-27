492b20cbb641290fd8dae61582c236b8c2cddc06
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-27 23:03:36 +0900

    feat(InputView): 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String getRacingCarNames() {
         System.out.println(GAME_START_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String getTryCount() {
+        System.out.println(TRY_COUNT_MESSAGE);
         return Console.readLine();
     }
 
