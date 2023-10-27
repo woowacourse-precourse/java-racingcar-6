88fac2814ce45699b852b6c955ac9dbdc7ee4c3e
HiiWee <wpdlzhf159@gmail.com>
2023-10-27 16:18:25 +0900

    refactor: MoveResultMessageGenerator 스트림 최적화

--- src/main/java/racingcar/generator/MoveResultMessageGenerator.java
+++ src/main/java/racingcar/generator/MoveResultMessageGenerator.java
@@ -1,28 +1,21 @@
 package racingcar.generator;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 import racingcar.dto.MoveResult;
 
 public class MoveResultMessageGenerator {
 
     private static final String RESULT_FORMAT = "%s : %s\n";
     private static final String MOVEMENT_BAR = "-";
-    public static final String EMPTY_MESSAGE = "";
 
-
-    // TODO: 의존성 끊기?
     public static String generateMoveResultsMessage(final List<MoveResult> moveResults) {
         return moveResults.stream()
                 .map(MoveResultMessageGenerator::createSingleMessage)
-                .reduce(EMPTY_MESSAGE, (firstName, secondName) -> firstName + secondName);
+                .collect(Collectors.joining());
     }
 
-    // TODO 리팩토링: 책임분리
     private static String createSingleMessage(final MoveResult moveResult) {
-        StringBuilder movementBuilder = new StringBuilder();
-        IntStream.range(0, moveResult.getPosition())
-                .forEach(value -> movementBuilder.append(MOVEMENT_BAR));
-        return String.format(RESULT_FORMAT, moveResult.getName(), movementBuilder);
+        return String.format(RESULT_FORMAT, moveResult.getName(), MOVEMENT_BAR.repeat(moveResult.getPosition()));
     }
 }
