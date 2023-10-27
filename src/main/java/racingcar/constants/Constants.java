a1ac643b40f8755c89869a65998e641c1e391454
Isaac Han <isaachan233@gmail.com>
2023-10-27 21:45:30 +0900

    feat: 여러 곳에서 사용될 상수들을 저장해놓은 enum 생성

---
+++ src/main/java/racingcar/constants/Constants.java
@@ -0,0 +1,24 @@
+package racingcar.constants;
+
+public enum Constants {
+    STANDARD_NUMBER(4), MIN_NUMBER(0), MAX_NUMBER(9), MAX_LENGTH(5), SEPARATOR(",");
+
+    private int intValue;
+    private String stringValue;
+
+    private Constants(int value) {
+        this.intValue = value;
+    }
+
+    private Constants(String value) {
+        this.stringValue = value;
+    }
+    
+    public int getIntValue() {
+        return intValue;
+    }
+    
+    public String getStringValue() {
+        return stringValue;
+    }
+}
