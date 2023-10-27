40875a082c6a076bcf5d63bf611c9b746f1c59fa
Cycrypto <jh01love@naver.com>
2023-10-27 21:58:51 +0900

    Test: RacingCar 인스턴스를 제대로 생성하는지 테스트

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -1,2 +1,22 @@
-package racingcar.controller;public class GameControllerTest {
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.RacingCar;
+
+public class GameControllerTest {
+    private GameController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new GameController();
+    }
+
+    @Test
+    void 입력된_레이상카_리스트에_추가(){
+        controller.addRacingCar(Arrays.asList("abc","def","ghi"));
+        assertThat(controller.racingCarList).hasOnlyElementsOfType(RacingCar.class);
+    }
 }
