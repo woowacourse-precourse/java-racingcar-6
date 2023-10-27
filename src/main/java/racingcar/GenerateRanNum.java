8bd642f0df85522ed0bd8698843bb9cfc3bd2964
gyahn <pop2413@gmail.com>
2023-10-28 08:38:19 +0900

    add : 난수 생성 및 자량에 부여 기능

---
+++ src/main/java/racingcar/GenerateRanNum.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+
+public class GenerateRanNum {
+    //난수 생성
+    public int ranNum(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+}
