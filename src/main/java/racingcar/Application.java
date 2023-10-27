e9a3ce6d563e492427044c9b1c71addcbaae9e83
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-28 03:57:54 +0900

    feat: 예외 처리 테스트 코드 생성
    1자 미만, 5자 초과, 미입력, 문자 외 입력 예외 처리
    테스트 수행하는 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.model.Cars;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Cars a = new Cars();
     }
 }
---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.validator.CarNameValidator;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest  {
+
+    private CarNameValidator carNameValidator;
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test()
+    void 차_이름_안_적음_예외_처리() {
+        assertThatThrownBy(() -> carNameValidator = new CarNameValidator(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test()
+    void 문자_외_입력_예외_처리() {
+        assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hy un,pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+    @Test()
+    void 차_이름_1자_미만_예외_처리() {
+        assertThatThrownBy(() -> carNameValidator = new CarNameValidator(",pobi,woote"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test()
+    void 차_이름_5자_초과_예외_처리() {
+        assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hyun,pobi,wooteco"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+}
