6b4676abacaeed5398628d0a2a9b8e98eb3be9d8
정민규 <fastdodge7@naver.com>
2023-10-27 20:25:23 +0900

    feat(RacingGame): buildCarList 메서드 추가
    사용자가 입력한 자동차 이름들의 문자열을 Car객체 리스트로 변환해 준다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
 
@@ -13,16 +14,27 @@ public class RacingGame {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameInput = Console.readLine();
 
-        carNameInput.split(",");
+        buildCarList(carNameInput);
+
+        
 
     }
 
-    public List<String> extractCarNames(String userInput) {
+    private void buildCarList(String userInput) {
+        List<String> carNames = extractCarNames(userInput);
+
+        this.carList = carNames.stream()
+                .map(element -> new Car(element))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> extractCarNames(String userInput) {
         List<String> nameList = Arrays.asList(userInput.split(","));
 
         nameList.stream().forEach(element -> {
-            if (element.length() > 5 || element.length() <= 0)
+            if (element.length() > 5 || element.length() <= 0) {
                 throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+            }
         });
 
         return nameList;
