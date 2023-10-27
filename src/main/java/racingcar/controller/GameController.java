56e96c043478899e6322d55ef4c9fc9589c8e96f
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 21:24:25 +0900

    feat: names로 List<Car> 생성 후 반환하는 기능 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,15 +1,25 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.view.InputView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameController {
 
     public GameController() {}
 
     public void startGame() {
+        List<String> names = getInputNames();
+    }
+
+    private List<Car> createCars(List<String> names) {
+        return names.stream()
+                    .map(Car::new)
+                    .collect(Collectors.toList());
     }
 
     private List<String> getInputNames() {
