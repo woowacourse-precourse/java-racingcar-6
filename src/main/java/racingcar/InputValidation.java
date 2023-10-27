c32795233539ce8b8f64c26e696245607ecfb43b
lzlkolo <lzlko1209@gmail.com>
2023-10-27 16:59:50 +0900

    feat: Implement validation for car name length

--- src/main/java/racingcar/InputValidation.java
+++ src/main/java/racingcar/InputValidation.java
@@ -1,6 +1,10 @@
 package racingcar;
 
+import java.util.List;
+
 public class InputValidation {
+    private static final int MAX_LENGTH = 5;
+
     public static void validateInput(String input) throws IllegalArgumentException {
         if (!input.matches("^[a-zA-Z,]*$")) {
             throw new IllegalArgumentException();
@@ -12,5 +16,13 @@ public class InputValidation {
 
     private static boolean isEmpty(String input) {
         return input == null || input.isEmpty();
+    }
+
+    public static void validateCarNames(List<String> carNames) throws IllegalArgumentException {
+        for (String st : carNames) {
+            if (st.length() > MAX_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,12 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class InputView {
     public List<String> getCarNames() {
         List<String> carNames = new ArrayList<>();
         String inputCarNames = Console.readLine();
         InputValidation.validateInput(inputCarNames);
+
+        StringTokenizer st = new StringTokenizer(inputCarNames);
+        while (st.hasMoreTokens()) {
+            carNames.add(String.valueOf(st));
+        }
+        InputValidation.validateCarNames(carNames);
+
         return carNames;
     }
 }
