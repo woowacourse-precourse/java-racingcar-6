ce41cb9430d8039c45857eb90631588a68c7c4a1
Hoo-sung <gntjd135@gmail.com>
2023-10-27 10:55:28 +0900

    feat(GameManager): 클래스 생성

---
+++ src/main/java/racingcar/domain/GameManager.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class GameManager {
+
+    private final InputView inputView = new InputView();
+
+    private final RoundManager roundManager = new RoundManager();
+
+
+}
