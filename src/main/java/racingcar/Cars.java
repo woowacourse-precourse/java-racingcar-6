55766bd08465b01e6ec87f4c6b3b3bdc40d11e51
Henry-Cha <sungwon4728@naver.com>
2023-10-27 22:45:27 +0900

    feat: Cars 클래스 기능 추가
    현재 레이싱 상태 구하기, 우승자 구하기

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     List<Car> cars;
@@ -12,5 +13,17 @@ public class Cars {
 
     public void run(){
         cars.forEach(Car::move);
+    }
+    public String getCarStates(){
+        StringBuilder sb = new StringBuilder();
+        cars.forEach(car->sb.append(car.toString()).append("\n"));
+        return sb.toString();
+    }
+    public List<String> getWinner() {
+        int max = getMaxCar();
+        return cars.stream().filter(car -> car.moveCount==max).map(car -> car.name).collect(Collectors.toList());
+    }
+    public int getMaxCar(){
+        return cars.stream().max(Car::compareTo).orElseThrow(()->new RuntimeException("차량 목록이 비었습니다.")).moveCount;
     }
 }
