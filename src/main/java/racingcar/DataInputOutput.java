8b0a92ff08664883ec3102585431c056d2bd1d38
devpanic <yeon.dev.8@gmail.com>
2023-10-27 15:38:45 +0900

    feat(userInputCarsInfo) : Add Input Car Info

--- src/main/java/racingcar/DataInputOutput.java
+++ src/main/java/racingcar/DataInputOutput.java
@@ -1,7 +1,22 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class DataInputOutput {
-    public void userInputCarsInfo(){}
+    private static List<Car> carList = new ArrayList<Car>();
+    public void userInputCarsInfo(){
+        // Validate Here
+        StringTokenizer tokenizer = new StringTokenizer(Console.readLine(), ",");
+        // Validate Here
+        while(tokenizer.hasMoreTokens()){
+            Car newCar = new Car();
+            newCar.makeCar(tokenizer.nextToken());
+            carList.add(newCar);
+        }
+    }
     public void userInputMovingInfo(){}
     public void printSystemMessage(){}
     public void printMovingResult(){}
