7421997c1de926d1d32b7e21249d89f14e2165d7
space <enaenen@naver.com>
2023-10-28 03:07:03 +0900

    REFACTOR: ERROR Message static 클래스 추가

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 
-import camp.nextstep.edu.missionutils.Console;
 import org.junit.platform.commons.util.StringUtils;
 
 public class CarName {
@@ -11,10 +10,10 @@ public class CarName {
     private void validateName(String name) throws IllegalArgumentException {
         // 사용해도 되나?? static import
         if (StringUtils.isBlank(name)) {
-            throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다.");
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_BLANK);
         }
         if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_TOO_LONG);
         }
     }
 
---
+++ src/main/java/racingcar/ErrorMessage.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class ErrorMessage {
+    public static final String CAR_NAME_IS_BLANK = "자동차 이름은 공백이 될 수 없습니다.";
+    public static final String CAR_NAME_IS_TOO_LONG = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String MINIMUM_CAR_NEEDED = "레이싱을 위해서는 자동차가 2대 이상 필요합니다.";
+}
