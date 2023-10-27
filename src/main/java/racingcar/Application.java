f42b40392c3ba2812c36af1d8102b59333085951
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-10-27 18:05:05 +0900

    feat: 자동차 이름 입력값 쉼표로 구분하여 공백 및 빈값 제외한 리스트 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,18 @@
 package racingcar;
 
+import java.util.Arrays;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         
     	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    	
-    	
+    	String carNameInput = Console.readLine();
+    	String[] cars = carNameInput.split(",");
+    	for(int i = 0; i < cars.length; i++) {
+    		cars[i] = cars[i].trim();
+    	}
+    	System.out.println(Arrays.toString(cars));
     }
 }
