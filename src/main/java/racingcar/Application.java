aa98138937947b9566a559601de765c71eda6b1d
metishonora <metishonora@icloud.com>
2023-10-27 21:08:26 +0900

    feat: 사용자 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        List<String> cars = Arrays.stream(Console.readLine().split(","))
+                        .toList();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int round = Integer.parseInt(Console.readLine());
+        // System.out.println(cars);
+        // System.out.println(round);
+        Console.close();
     }
 }
