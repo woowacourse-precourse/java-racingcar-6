b18c59d9b5002167becb9697bbcb85ae7cd7d5a3
SonMinGyu <littleking13@naver.com>
2023-10-27 19:48:39 +0900

    feat: 필요한 출력 문구 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -35,25 +35,33 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playGame();
+    }
 
-
+    private static void playGame() {
+        printInputCarNameMessage();
         String[] carNames = inputCarNames();
+        printInputAttemptsNumMessage();
         int attemptsNum = inputAttemptsNum();
 
         List<Car> cars = createCars(carNames);
 
-//        for (Car car : cars) {
-//            System.out.print(car.getCarName() + " ");
-//            System.out.println(car.getLocation());
-//        }
-
+        printExecuteResultMessage();
         repeatAttempts(attemptsNum, cars);
         printWinners(cars);
-//        for (Car car : cars) {
-//            System.out.print(car.getCarName() + " ");
-//            System.out.println(car.getLocation());
-//        }
+    }
+
+    private static void printInputCarNameMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    private static void printInputAttemptsNumMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    private static void printExecuteResultMessage() {
+        System.out.println();
+        System.out.println("실행 결과");
     }
 
     private static void printWinners(final List<Car> cars) {
