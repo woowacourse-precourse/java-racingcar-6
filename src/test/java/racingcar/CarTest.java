2737099cd5f20c57051b6a4766935cd29dc333d8
jhon3242 <rldotnr2@gmail.com>
2023-10-27 13:02:13 +0900

    test : Car 이름에 대한 성공 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,15 +2,19 @@ package racingcar;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 public class CarTest {
 
-    @Test
-    void CarNameSuccessTest() {
-        Car car = new Car("ABC");
 
-        Assertions.assertThat(car.getName()).isEqualTo("ABC");
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "pobi", "a2", "abcd", "1234", "  abc   ",})
+    void CarNameSuccessTest(String name) {
+        Car car = new Car(name);
+
+        Assertions.assertThat(car.getName()).isEqualTo(name);
     }
 
     @Test
