b974498370e5519fbd7ac0cd99d2a7823a14e3d7
gosu <q63530@naver.com>
2023-10-27 11:54:55 +0900

    feat(view): attemptNumberInputView 메서드 구현
    시도 횟수 사용자 입력 뷰를 출력

--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -1,10 +1,15 @@
 package racingcar.view;
 
-import static racingcar.view.ViewConstant.RACING_START;
+import static racingcar.view.ViewConstant.ATTEMPT_NUMBER_INPUT;
+import static racingcar.view.ViewConstant.CAR_NAME_INPUT;
 
 public class RacingView {
 
-    public static void racingStartView() {
-        System.out.println(RACING_START);
+    public static void carNameInputView() {
+        System.out.println(CAR_NAME_INPUT);
+    }
+
+    public static void attemptNumberInputView() {
+        System.out.println(ATTEMPT_NUMBER_INPUT);
     }
 }
--- src/main/java/racingcar/view/ViewConstant.java
+++ src/main/java/racingcar/view/ViewConstant.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
 public class ViewConstant {
-    public static final String RACING_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ATTEMPT_NUMBER_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 }
