5fd5abdafafa895d07e6c095b992a06a550e6ffb
jaehee <y2hjjh@naver.com>
2023-10-28 00:00:51 +0900

    feat: 전진 여부 검사 및 하이픈 추가
    구한 무작위 값이 4 이상인지 확인하여 boolean 값 반환
    true일 경우 Car 객체에 하이픈 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -6,9 +6,22 @@ import racingcar.model.Car;
 public class RaceController {
     private static final int RANGE_START_NUMBER = 0;
     private static final int RANGE_END_NUMBER = 9;
+    private static final int MOVE_CRITERIA = 4;
 
     public int getRandomNumber() {
         return Randoms.pickNumberInRange(RANGE_START_NUMBER, RANGE_END_NUMBER);
     }
 
+    public boolean isMoved() {
+        if (getRandomNumber() >= MOVE_CRITERIA) {
+            return true;
+        }
+        return false;
+    }
+
+    public void addRecord(Car car) {
+        if (isMoved()) {
+            car.addRecord();
+        }
+    }
 }
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,10 +1,16 @@
 package racingcar.model;
 
 public class Car {
+    private static final String MOVED_LETTER = "-";
     private Name name;
     private String record;
 
     public Car(String name) {
         this.name = new Name(name);
+        this.record = "";
+    }
+
+    public void addRecord() {
+        record += MOVED_LETTER;
     }
 }
