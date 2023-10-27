f242aafab92b06bc25cb96a7d5f21b158fc0d39e
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-27 23:12:41 +0900

    feat(InputValidator): 시도 횟수 입력값 검증 기능 구현

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -3,6 +3,7 @@ package racingcar.util;
 public class InputValidator {
 
     private static final String CAR_NAME_LONG = "자동차 이름의 길이가 5를 초과하였습니다. : ";
+    private static final String NOT_NUMBER = "숫자가 아닙니다.";
     private static final int MAX_LENGTH = 5;
 
     public static void validateRacingCarNames(String[] racingCarNames) {
@@ -13,4 +14,18 @@ public class InputValidator {
         }
     }
 
+    public static void validateTryCount(String tryCount) {
+        if (!isNumeric(tryCount)) {
+            throw new IllegalArgumentException(NOT_NUMBER);
+        }
+    }
+
+    private static boolean isNumeric(String tryCount) {
+        for (int i = 0; i < tryCount.length(); i++) {
+            if (!Character.isDigit(tryCount.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
