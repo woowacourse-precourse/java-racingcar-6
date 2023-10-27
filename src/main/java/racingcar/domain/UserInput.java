54e7062a3047a1f79e490aecd6c6c40b3d03c8c0
2gyeong <knk222@naver.com>
2023-10-27 22:37:21 +0900

    refactor : InputException 코드 수정

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -1,5 +1,11 @@
 package racingcar.domain;
 
+import static racingcar.exception.InputException.checkCarCount;
+import static racingcar.exception.InputException.checkEmpty;
+import static racingcar.exception.InputException.checkFormat;
+import static racingcar.exception.InputException.checkLimit;
+import static racingcar.exception.InputException.checkSameName;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
@@ -11,24 +17,26 @@ public class UserInput {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputStr = Console.readLine();
 
-        validateInput(inputStr);
-
-        return Arrays.asList(inputStr.split(","));
-    }
-
-    private static void validateInput(String inputStr) {
-        InputException.isValidInputFormat(inputStr);
         String[] carNamesArray = inputStr.split(",");
 
-        InputException.limit(carNamesArray);
+        validateInput(carNamesArray);
 
-        InputException.sameName(carNamesArray);
+        return Arrays.asList(carNamesArray);
     }
 
+    private static void validateInput(String[] carNamesArray) {
+        checkEmpty(carNamesArray);
+        checkFormat(carNamesArray);
+        checkCarCount(carNamesArray);
+        checkLimit(carNamesArray);
+        checkSameName(carNamesArray);
+
+    }
+
+    // 자동차 경주 게임 플레이 횟수
     public static int inputGameCount(){
 
         System.out.println("시도할 횟수는 몇회인가요?");
-
         int count = Integer.parseInt(Console.readLine());
 
         InputException.isNumber(count);
--- src/main/java/racingcar/exception/InputException.java
+++ src/main/java/racingcar/exception/InputException.java
@@ -5,27 +5,47 @@ import java.util.Set;
 
 public class InputException extends IllegalArgumentException {
 
-    private static final int limitLength = 5;
+    private static final int limitlength = 5;
+    private static final int MINCARCOUNT = 2;
 
     // 자동차 이름이 5자가 넘어갈 경우
-    public static void limit(String[] carName){
+    public static void checkLimit(String[] carName){
         for(String name : carName){
-            if(name.length() > limitLength){
+            if(name.length() > limitlength){
                 throw new IllegalArgumentException(" -> 이름은 5자 이하여야 합니다." );
             }
         }
     }
 
-    // 자동차 이름을 구분하는 ','가 아닌 이외의 글자가 입력됐을 경우
-    // 자동차 이름은 영어 대소문자, 한글만 입력 가능하다.
-    public static void isValidInputFormat(String inputStr){
-        if (!inputStr.matches("^[a-zA-Z가-힣,]+$")) {
-            throw new IllegalArgumentException("올바른 형식의 입력이 아닙니다. ");
+    // 입력되지 않은 경우
+    public static void checkEmpty(String[] carNamesArray) {
+        for (String carName : carNamesArray) {
+            if (carName.trim().isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
+            }
+        }
+    }
+
+    // 형식이 틀린 경우
+        // 1. 자동차 이름을 구분하는 ','가 아닌 이외의 글자가 입력됐을 경우
+        // 2. 자동차 이름은 영어 대소문자, 한글만 입력 가능하다.
+    public static void checkFormat(String[] carNamesArray) {
+        for (String carName : carNamesArray) {
+            if (!carName.matches("^[a-zA-Z가-힣]+$")) {
+                throw new IllegalArgumentException("올바른 형식의 입력이 아닙니다. 자동차 이름은 영어 대소문자, 한글만 입력 가능합니다.");
+            }
+        }
+    }
+
+    // 자동차가 한 대만 입력된 경우
+    public static void checkCarCount(String[] carNamesArray) {
+        if (carNamesArray.length < MINCARCOUNT) {
+            throw new IllegalArgumentException("적어도 두 대의 자동차 이름이 필요합니다.");
         }
     }
 
     // 이름이 중복되는 경우
-    public static void sameName(String[] carName){
+    public static void checkSameName(String[] carName){
         Set<String> uniqueName = new HashSet<>();
         for (String name : carName) {
             if (!uniqueName.add(name)) {
@@ -33,9 +53,6 @@ public class InputException extends IllegalArgumentException {
             }
         }
     }
-
-    // 자동차가 한 대만 입력된 경우
-
 
 
     // 횟수
