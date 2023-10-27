341e8a8b10c5bc04eb165a58f4dc3026fd74ceec
zangsu <zangsu_@naver.com>
2023-10-27 15:39:52 +0900

    feat : create GameBuilder class, write TODO

---
+++ src/main/java/racingcar/GameBuilder.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class GameBuilder {
+    //TODO : GameBuilder getCarsName() throws IllegalArgumentException
+
+    //TODO : GameBuilder getRoundNumber()throws IllegalArgumentException
+
+    //TODO : Game build()
+}
