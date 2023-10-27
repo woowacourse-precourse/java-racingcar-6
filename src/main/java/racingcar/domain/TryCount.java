1d9123143623abc3582b3c12210ce95b6c821ccb
김승진 <ohksj77@gmail.com>
2023-10-28 00:25:43 +0900

    feat(TryCount): 잔여 시도 횟수 존재 여부 확인 및 다음 TryCount 생성 기능 구현

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -5,6 +5,8 @@ import racingcar.constant.ErrorMessage;
 public final class TryCount {
 
     private static final int MIN_VALUE = 1;
+    private static final int NONE_LEFT = 0;
+    private static final int MINUS_UNIT = 1;
     private final Integer value;
 
     public TryCount(final int value) {
@@ -12,9 +14,21 @@ public final class TryCount {
         this.value = value;
     }
 
+    public TryCount(final TryCount tryCount) {
+        this.value = tryCount.value - MINUS_UNIT;
+    }
+
     private void validate(final int value) {
         if (value < MIN_VALUE) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_TRY_COUNT.toValue());
         }
+    }
+
+    public boolean hasLeftCount() {
+        return value > NONE_LEFT;
+    }
+
+    public TryCount nextTryCount() {
+        return new TryCount(this);
     }
 }
