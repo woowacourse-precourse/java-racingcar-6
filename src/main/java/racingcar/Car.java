5b96a2cc5146a837fd658728d5c039cf08378b1f
plastichong <zakkslash18@naver.com>
2023-10-27 22:37:03 +0900

    feat: 전진 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String name;
@@ -16,4 +18,11 @@ public class Car {
     public int getForwardDistanceBetweenStartLine() {
         return forwardDistanceBetweenStartLine;
     }
+
+    public void tryForwardByCondition(int randomNumber) {
+        if (randomNumber >= 4){
+            forwardDistanceBetweenStartLine += 1;
+        }
+    }
+
 }
--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,18 +1,35 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
 public class GameManager {
 
+    List<Car> carList;
+
     public void gamePlay() {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNameList = UserInputReceiver.receiveCarNameAndGetCarNameList(Console.readLine());
-        CarFactory.createCars(carNameList);
+        carList = CarFactory.createCars(carNameList);
+
+        System.out.println("시도할 회수는 몇회인가요?");
 
         int tryTimes = UserInputReceiver.receiveTryTimes(Console.readLine());
+
+        startForward(carList, tryTimes);
+    }
+
+    public void startForward(List<Car> cars,int tryTimes) {
+
+        for (int i = 0; i < tryTimes; i++) {
+
+            cars.forEach(car->car.tryForwardByCondition(Randoms.pickNumberInRange(0,9)));
+
+        }
+
     }
 
 }
