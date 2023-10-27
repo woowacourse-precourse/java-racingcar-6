e13e7c53c976ddaaa679f4626e68ac8cdeb3a51f
Hoo-sung <gntjd135@gmail.com>
2023-10-27 09:47:42 +0900

    feat(RandomNumberGenerator): randomNumGenerate 함수 생성

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,5 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public final class RandomNumberGenerator {
 
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+    public static int randomNumGenerate(){
+        return Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE);
+    }
 }
