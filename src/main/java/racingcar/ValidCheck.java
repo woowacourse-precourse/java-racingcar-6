de2e8f31357a41bfbe04e3bd48fea4aa4efc9117
alswp006 <alswp006@gmail.com>
2023-10-27 20:04:31 +0900

    유효성 검사 기능 구현

---
+++ src/main/java/racingcar/ValidCheck.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import java.util.List;
+
+public class ValidCheck {
+    public static void namesize_check(String[] car_list){
+        for (String car_name : car_list){
+            if (car_name.length() > 4){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    public static int moveTypeCheck(String move){
+        try{
+            return Integer.parseInt(move);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
