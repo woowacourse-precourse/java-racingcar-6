b067a9ed909f8ce81edc1a79744543bcdf918da2
jaehee <y2hjjh@naver.com>
2023-10-27 23:14:27 +0900

    feat: 사용자에게 시도할 횟수를 입력 받는 기능 구현
    숫자가 아닐 경우 예외 처리

---
+++ src/main/java/racingcar/validator/CountInputValidator.java
@@ -0,0 +1,13 @@
+package racingcar.validator;
+
+public class CountInputValidator implements Validator {
+    private static final String COUNT_INPUT_EXCEPTION_MESSAGE = "숫자를 입력해주십시오.";
+    private static final String ONLY_NUMBER_REGEX = "\\d+";
+
+    @Override
+    public void validate(String playerInput) {
+        if (!playerInput.matches(ONLY_NUMBER_REGEX)) {
+            throwException(COUNT_INPUT_EXCEPTION_MESSAGE);
+        }
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,27 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.CountInputValidator;
 import racingcar.validator.NamesInputValidator;
 
 public class InputView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private final NamesInputValidator nameInputValidator = new NamesInputValidator();
+    private final CountInputValidator countInputValidator = new CountInputValidator();
 
     public String getCarNameInput() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         String playerInput = Console.readLine();
         nameInputValidator.validate(playerInput);
+        return playerInput;
+    }
+
+    public String getCountInput() {
+        System.out.println(COUNT_INPUT_MESSAGE);
+        String playerInput = Console.readLine();
+        countInputValidator.validate(playerInput);
+        System.out.println(playerInput);
         return playerInput;
     }
 }
