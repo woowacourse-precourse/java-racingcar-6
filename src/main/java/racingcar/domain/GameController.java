91ea40407486ff6789e3ad17a5ea948a806b1e2f
brightfanta <brightfanta@gmail.com>
2023-10-27 21:21:13 +0900

    feat: 1.자동차 이름 입력 받고 2.이름을 쉼표(,) 기준으로 나누어 3.자동차 인스턴스변수 중 이름에 저장

--- src/main/java/racingcar/domain/GameController.java
+++ src/main/java/racingcar/domain/GameController.java
@@ -1,12 +1,27 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class GameController {
 
     private Integer TryMoveForwardRandomNumber;
+    private List<Car> carList;
 
-    public void splitCarNameInput() {
+    public String getCarNames() {
+
+        return Console.readLine();
+    }
+
+    public void splitCarNameInput(String carNamesInput, List<Car> carList) {
+
+        String[] carNamesArray = carNamesInput.split(",");
+        for (int carIndex = 0; carIndex < carNamesArray.length; carIndex++) {
+            Car car = carList.get(carIndex);
+            car.setName(carNamesArray[carIndex]);
+            System.out.println("car.getName() = " + car.getName());
+        }
     }
 
     public void verifyMoveForward() {
