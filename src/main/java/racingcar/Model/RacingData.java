6202ddb39229c44003e16ee44df4f2bbaa21e59a
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-27 10:23:56 +0900

    :memo: Feat : moveNumber type change

--- src/main/java/racingcar/Model/RacingData.java
+++ src/main/java/racingcar/Model/RacingData.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class RacingData {
     private String carName; //자동차 이름
     private List<String> racingCar; //사용자가 입력할 자동차 이름을 저장할 리스트
-    private int moveNumber; //이동 횟수
+    private String moveNumber; //이동 횟수 -> 추후에 정수형으로 변환
     private List<Integer> moveValidationNumber; //전진과 멈춤을 판단하는 난수를 저장 할 리스트
 
     public String getCarName() {
@@ -24,11 +24,11 @@ public class RacingData {
         this.racingCar = racingCar;
     }
 
-    public int getMoveNumber() {
+    public String getMoveNumber() {
         return moveNumber;
     }
 
-    public void setMoveNumber(int moveNumber) {
+    public void setMoveNumber(String moveNumber) {
         this.moveNumber = moveNumber;
     }
 
