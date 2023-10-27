1b6b21d710792e4c8a64f0114a73fa2716e796cc
parkchangeon <jg9870@naver.com>
2023-10-27 21:57:09 +0900

    feat(racingcar): 입력 받는 기능 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+    public static List<Car> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");  // todo 같은 이름 입력하면?
+        return CarFactory.from(Console.readLine());
+    }
+
+    public static int getRacingRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+    }
+}
