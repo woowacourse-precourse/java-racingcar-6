60266ece8d9c25ab57c057c0944f0377124760f3
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-27 21:03:18 +0900

    chore:공백 및 클래스 제거

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -12,13 +12,11 @@ public class GameServiceTest {
     @Test
     public void 자동차_이름으로_자동차와_플레이어_생성() {
         //given
-
         List<String> names = new ArrayList(Arrays.asList("name1", "name2", "name3"));
         GameService gameService = new GameService();
         //when
         GamePlayer gamePlayer = gameService.settingPlayer(names);
         List<String> allCarNames = gamePlayer.getAllCarNames();
-
         //then
         Assertions.assertThat(allCarNames.size()).isEqualTo(3);
         Assertions.assertThat(allCarNames.get(0)).isEqualTo(names.get(0));
