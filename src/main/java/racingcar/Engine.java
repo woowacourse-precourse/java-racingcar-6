9cf4f047333a9bc83cd7525401c0b8fcf9488d19
june-777 <wlwhswnsrl96@gmail.com>
2023-10-27 14:16:15 +0900

    feat(Engine): 0 ~ 9 사이의 무작위 값을 구하는 기능

---
+++ src/main/java/racingcar/Engine.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Engine {
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
