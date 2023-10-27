c807e62dcf3e99510463cb79652b81ba9abca8af
koomchang <leahpar0401@gmail.com>
2023-10-27 19:24:56 +0900

    feat: Location VO 구현

--- src/main/java/racingcar/model/Location.java
+++ src/main/java/racingcar/model/Location.java
@@ -1,4 +1,29 @@
 package racingcar.model;
 
+import java.util.Objects;
+
 public class Location {
+
+    private final int location;
+
+    public Location(int location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Location location1 = (Location) o;
+        return location == location1.location;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
+    }
 }
