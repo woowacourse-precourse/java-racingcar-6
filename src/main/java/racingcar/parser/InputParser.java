ff1ed0535c6b8d5651738408baf9eb9a65d7982c
mainfn <_@mainfn.dev>
2023-10-27 23:51:11 +0900

    feat: InputParser 작성 및 Validation 로직 이동

--- src/main/java/racingcar/parser/InputParser.java
+++ src/main/java/racingcar/parser/InputParser.java
@@ -1,12 +1,34 @@
 package racingcar.parser;
 
+import static strings.Strings.COMMAS;
+import static strings.Strings.WHITE_SPACE;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.car.CarRacing;
 import racingcar.domain.car.CarsRacing;
+import racingcar.validator.InputValidator;
 
 public final class InputParser {
+
+    private final InputValidator inputValidator;
+
+    public InputParser(final InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
     public CarsRacing parseToCarsRacing(final String input) {
-        return null;
+        inputValidator.validateCarNamesInput(input);
+
+        final List<CarRacing> cars = Arrays.stream(input.split(COMMAS + WHITE_SPACE))
+                .map(CarRacing::new)
+                .toList();
+
+        return CarsRacing.from(cars);
     }
 
     public int parseToMaxRound(final String input) {
+        inputValidator.validateMaxRoundInput(input);
+        return Integer.parseInt(input);
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,22 +6,17 @@ import racingcar.component.in.MaxRoundComponent;
 import racingcar.component.in.NameComponent;
 import racingcar.domain.car.CarsRacing;
 import racingcar.parser.InputParser;
-import racingcar.validator.InputValidator;
 
 public final class InputView {
-    private final InputValidator inputValidator;
     private final InputParser inputParser;
-
     private final ComponentWriter componentWriter;
     private final Reader reader;
 
     public InputView(
-            final InputValidator inputValidator,
             final InputParser inputParser,
             final ComponentWriter componentWriter,
             final Reader reader
     ) {
-        this.inputValidator = inputValidator;
         this.inputParser = inputParser;
         this.componentWriter = componentWriter;
         this.reader = reader;
@@ -29,17 +24,11 @@ public final class InputView {
 
     public CarsRacing inputCarsRacing() {
         componentWriter.write(new NameComponent());
-        final String input = reader.readLine();
-        inputValidator.validateCarNamesInput(input);
-
-        return inputParser.parseToCarsRacing(input);
+        return inputParser.parseToCarsRacing(reader.readLine());
     }
 
     public int inputMaxRound() {
         componentWriter.write(new MaxRoundComponent());
-        final String input = reader.readLine();
-        inputValidator.validateMaxRoundInput(input);
-
-        return inputParser.parseToMaxRound(input);
+        return inputParser.parseToMaxRound(reader.readLine());
     }
 }
