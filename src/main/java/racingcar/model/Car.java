edd93551fc01e0985e4c74a5b00b0f723f3d9618
soozzang <soozzang@inha.edu>
2023-10-27 14:16:49 +0900

    Feat(Car): 생성자 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    final String name;
+    final Integer location;
+
+    Car(String name, Integer location){
+        this.name = name;
+        this.location = location;
+    }
+}
