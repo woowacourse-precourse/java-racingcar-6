39ad80951b6924d13264febe461120790dc35af6
hongseokhodev <hongseokhodev@gmail.com>
2023-10-27 18:07:44 +0900

    refactor: 이름 변경 NameRule -> NameConstants

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,9 +1,9 @@
 package racingcar.domain.car;
 
-import static racingcar.domain.car.NameRule.ALPHABET_PATTERN;
-import static racingcar.domain.car.NameRule.LENGTH_IS_OVER_MAX;
-import static racingcar.domain.car.NameRule.MAX_LENGTH;
-import static racingcar.domain.car.NameRule.NOT_ALPHABETIC;
+import static racingcar.domain.car.NameConstants.ALPHABET_PATTERN;
+import static racingcar.domain.car.NameConstants.LENGTH_IS_OVER_MAX;
+import static racingcar.domain.car.NameConstants.MAX_LENGTH;
+import static racingcar.domain.car.NameConstants.NOT_ALPHABETIC;
 
 import java.util.Objects;
 
--- src/main/java/racingcar/domain/car/NameConstants.java
+++ src/main/java/racingcar/domain/car/NameConstants.java
@@ -1,6 +1,6 @@
 package racingcar.domain.car;
 
-public final class NameRule {
+public final class NameConstants {
     public static final int MAX_LENGTH = 5;
     public static final String ALPHABET_PATTERN = "^[a-zA-Z]+$";
     public static final String LENGTH_IS_OVER_MAX = String.format("자동차 이름을 %d글자 이하로 입력하세요.", MAX_LENGTH);
