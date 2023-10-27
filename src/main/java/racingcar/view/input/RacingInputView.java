e87e39fbaf9ae7df29f3a96eca29d875ffb016f5
Sully <2dcoder@naver.com>
2023-10-27 17:29:32 +0900

    feat(RacingInputView): 경주할 자동차 이름을 입력 요청하는 메서드 추가
    입력을 요청할 때 요청 문장을 RacingOutputView에서 출력한다.

--- src/main/java/racingcar/view/input/RacingInputView.java
+++ src/main/java/racingcar/view/input/RacingInputView.java
@@ -1,5 +1,13 @@
 package racingcar.view.input;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.output.RacingOutputView;
+
 public class RacingInputView {
+
+    public String inputCarName() {
+        RacingOutputView.outputCarName();
+        return Console.readLine();
+    }
 
 }
