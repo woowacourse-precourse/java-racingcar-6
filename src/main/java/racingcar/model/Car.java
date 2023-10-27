94ede00a18768e7fbb9a4d7e7e2bb0dfacdf3716
isyoo1213 <moethns@gmail.com>
2023-10-27 19:30:40 +0900

    feat: Car 이름과 위치정보 저장기능 추가 Car - 모델 클래스인 Car 클래스 생성   - 생성자를 통해 이름 저장기능 추가   - calculatePosition() 을 통해 위치 정보 저장기능 추가 README.md - 기능 명세 구현 체크

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void calculatePosition(boolean moveState) {
+        if (moveState) {
+            this.position++;
+        }
+    }
+}
