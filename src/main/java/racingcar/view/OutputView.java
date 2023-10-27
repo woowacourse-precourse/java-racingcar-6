b065baf7d3fbc3e2813adaf27069c8e288f1d121
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-10-27 15:58:28 +0900

    feat: OutputView 출력문 static final 선언

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String INPUT_CARNAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROTATE_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String GAME_RESULT = "실행 결과";
+    private static final String WINNER = "최종 우승자 : ";
+}
