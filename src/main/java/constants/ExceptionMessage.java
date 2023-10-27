ef93837fa26efea9379a5e6cd597fd6cf2cf7c81
pwrwpw <dlgustjr9912@gmail.com>
2023-10-28 03:15:37 +0900

    feat(ExceptionMessage): 예외 메시지 출력 기능 추가
    docs : docs 수정 및 예외처리 기능 체크 완료

---
+++ src/main/java/constants/ExceptionMessage.java
@@ -0,0 +1,23 @@
+package constants;
+
+public enum ExceptionMessage {
+    NONE_INPUT_CARS("자동차 이름은 공백이 될 수 없습니다."),
+    CAR_NAME_LIMIT_EXCEEDED("자동차 이름은 5자 이하만 가능합니다."),
+    NOT_INPUT_CARS_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
+    CARS_LESS_THAN_TWO("자동차 이름은 2개 이상이어야 합니다."),
+    EXCESSIVE_COMMAS_ERROR("쉼표가 연속으로 입력되었습니다."),
+    START_COMMA_ERROR("쉼표로 시작하면 안됩니다."),
+    END_COMMA_ERROR("쉼표로 끝나면 안됩니다."),
+    INVALID_CHARACTER_INPUT_ERROR("문자,쉼표만 입력 가능 합니다."),
+    INVALID_INPUT_ERROR_MESSAGE("유효하지 않은 입력입니다. 0보다 큰 숫자만 입력 가능합니다.");
+    private static final String ERROR_TAG = "[ERROR] ";
+    private final String exceptionMessage;
+
+    ExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = ERROR_TAG + exceptionMessage;
+    }
+
+    public void throwException() {
+        throw new IllegalArgumentException(exceptionMessage);
+    }
+}
