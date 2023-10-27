1a7530e7f97751fd132da48743f268ffde2ba639
DESKTOP-FH4O6OD\syt11 <syt1128@gmail.com>
2023-10-27 21:54:16 +0900

    :add: RacingGameOutputs 추가
    게임을 진행하는 동안 사용될 출력 요소들을
    racingcar.veiw.racingGameOuts Class에 추가함

---
+++ src/main/java/racingcar/veiw/racingGameOutputs.java
@@ -0,0 +1,10 @@
+package racingcar.veiw;
+
+public class racingGameOutputs {
+    final public static String pleaseInuptCarNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    final public static String inputNumberOfAttempt = "시도할 회수는 몇회인가요?";
+    final public static String attemptResult = "실행 결과";
+    final public static String finalWinner = "최종 우승자 : ";
+    final public static String carMovingMotion = "-";
+
+}
