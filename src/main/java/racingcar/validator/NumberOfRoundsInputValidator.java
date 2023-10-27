67a13b9e1a133c9937cafb372684d7665e3c6e17
koomchang <leahpar0401@gmail.com>
2023-10-27 18:30:43 +0900

    feat: 시도 횟수 입력에 대한 검증 클래스 추가

--- src/main/java/racingcar/validator/NumberOfRoundsInputValidator.java
+++ src/main/java/racingcar/validator/NumberOfRoundsInputValidator.java
@@ -1,2 +1,38 @@
-package racingcar.validator;public class NumberOfRoundsInputValidator {
+package racingcar.validator;
+
+public class NumberOfRoundsInputValidator implements InputValidator {
+
+    @Override
+    public void validate(String userInput) {
+        isInputEmpty(userInput);
+        isInputInteger(userInput);
+        isInputPositive(userInput);
+        isInputNumeric(userInput);
+    }
+
+    private void isInputEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    private void isInputNumeric(String userInput) {
+        if (!userInput.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("입력값이 숫자가 아닙니다.");
+        }
+    }
+
+    private void isInputPositive(String userInput) {
+        if (Integer.parseInt(userInput) <= 0) {
+            throw new IllegalArgumentException("입력값이 0 이하의 숫자입니다.");
+        }
+    }
+
+    private void isInputInteger(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 정수가 아닙니다.");
+        }
+    }
 }
