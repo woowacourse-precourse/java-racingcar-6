08b8229b0f17b5239979a06f81cbf470b07fe840
Hanjaemo <hjm2530@gmail.com>
2023-10-27 20:14:13 +0900

    feat(GameController): 게임 초기값 설정 기능 구현

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.view.InputView;
+
 public class GameController {
 
     private final RandomNumberGenerator numberGenerator;
@@ -10,6 +15,16 @@ public class GameController {
 
     public void run() {
         // 게임 초기 설정
+        List<Car> cars = createCars();
+        int numberOfAttempts = InputView.readNumberOfAttempts();
+
         // 게임 진행
+    }
+
+    private List<Car> createCars() {
+        List<String> carNames = InputView.readCarNames();
+        return carNames.stream()
+                .map(Car::new)
+                .toList();
     }
 }
