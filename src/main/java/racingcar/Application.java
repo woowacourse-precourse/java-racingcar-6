a30073cdd1388dffe47fbadac192729a356caad9
02ggang9 <02ggang9@gmail.com>
2023-10-27 21:18:44 +0900

    feat(GameController): 시도횟수를 입력하는 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import racingcar.domain.car.dao.CarRepository;
 import racingcar.domain.gamemanager.controller.GameController;
+import racingcar.domain.gamemanager.service.GameService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController gameController = new GameController();
+        // FIX HERE!
+        GameController gameController = new GameController(new GameService(new CarRepository()));
         gameController.startGame();
     }
 }
--- src/main/java/racingcar/domain/gamemanager/controller/GameController.java
+++ src/main/java/racingcar/domain/gamemanager/controller/GameController.java
@@ -1,11 +1,24 @@
 package racingcar.domain.gamemanager.controller;
 
+import racingcar.domain.gamemanager.service.GameService;
 import racingcar.domain.view.InputView;
 
 public class GameController {
 
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     public void startGame() {
         InputView.printGameStart();
         String carNames = InputView.receiveUserInput();
+        gameService.saveCars(carNames);
+    }
+
+    public void receiveTryCount() {
+        InputView.printTryCount();
+        String userInput = InputView.receiveUserInput();
     }
 }
