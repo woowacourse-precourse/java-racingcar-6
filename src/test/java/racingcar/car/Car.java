59cc6df3fa7a202f83a0f5c82783cac99259b170
Jeonwoojae <dnwo0719@icloud.com>
2023-10-28 02:17:29 +0900

    test: 자동차 객체 생성 6자 이상 입력 예외 처리

--- src/test/java/racingcar/car/Car.java
+++ src/test/java/racingcar/car/Car.java
@@ -2,8 +2,14 @@ package racingcar.car;
 
 public class Car {
     private String name;
+
     public Car(String name) {
-        if (name == null) throw new IllegalArgumentException("이름이 null이 될 수 없습니다!");
+        if (name == null) {
+            throw new IllegalArgumentException("이름이 null이 될 수 없습니다!");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이상이 될 수 없습니다!");
+        }
         this.name = name;
     }
 }
--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
     @Test
-    void 자동차_생성_실패_null입력() {
+    void 자동차_생성_실패_null_입력() {
         assertThatThrownBy(() -> new Car(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 자동차_생성_실패_길이가_5글자보다_큰_입력() {
+        assertThatThrownBy(() -> new Car("aaaaaa"))
+               .isInstanceOf(IllegalArgumentException.class);
     }
 }
