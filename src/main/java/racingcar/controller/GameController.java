e2dd9961ef0c26d553360633df4a413539b4b7c6
suihub <qkdcrt@gmail.com>
2023-10-27 10:17:33 +0900

    <feat>: "입력 View 구현"
    자동차 이름, 시도 횟수를 입력받는 View 구현.
    이를 호출하도록 Controller 로직 추가.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,15 +1,18 @@
 package racingcar.controller;
 
 import racingcar.domain.Car;
+import racingcar.view.PlayerInput;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
     private final List<Car> carList = new ArrayList<>();
+    private final PlayerInput input = new PlayerInput();
     private int tryCount;
 
     public void start() {
-
+        input.inputCarNames();
+        input.inputTryCount();
     }
 }
---
+++ src/main/java/racingcar/view/PlayerInput.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class PlayerInput {
+
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public String inputTryCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return Console.readLine();
+    }
+}
