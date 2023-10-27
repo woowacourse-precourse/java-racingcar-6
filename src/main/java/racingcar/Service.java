a2375e6e2c62cc73954b82ae8ebc367eeeef2d9e
chansoo <chansoo38@gmail.com>
2023-10-27 18:15:05 +0900

    <Feat> Service - 최종 우승자를 보여주는 기능을 구현한다.

--- src/main/java/racingcar/Service.java
+++ src/main/java/racingcar/Service.java
@@ -1,13 +1,15 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
     private final ProgressBoard progressBoard;
-    private final InputView inputView ;
+    private final InputView inputView;
     private final OutputView outputView;
     private final int GO_NUMBER = 4;
+    private List<String> participants;
 
     public Service() {
         this.progressBoard = ProgressBoard.getInstance();
@@ -20,14 +22,18 @@ public class Service {
         String input = Console.readLine();
         return input;
     }
+
     public String receiveTrialCount() {
         inputView.printTryCountMessage();
         String input = Console.readLine();
         return input;
     }
+
     public void registerParticipants(List<String> participants) {
+        this.participants = participants;
         progressBoard.registerCars(participants);
     }
+
     public void moveForward(String carName) {
         progressBoard.moveForward(carName);
     }
@@ -36,10 +42,35 @@ public class Service {
         return numberGenerator.getRandomNumber() >= GO_NUMBER;
     }
 
-    public void showProgressOf(List<String> participants) {
+    public void showProgressOf() {
         outputView.printProgressMessage(participants, progressBoard);
     }
+
     public void printResultMessage() {
         outputView.printResultMessage();
+    }
+
+    public void showFinalWinner() {
+        List<String> winners = getWinners();
+        outputView.printFinalWinner(winners);
+    }
+
+    private List<String> getWinners() {
+        int maxMove = getMaxMove();
+        List<String> winners = new ArrayList<>();
+        for(String participant : participants) {
+            if(progressBoard.progressOf(participant).length()==maxMove) {
+                winners.add(participant);
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxMove() {
+        int result = 0;
+        for (String key : progressBoard.getParticipants()) {
+            result = Math.max(result, progressBoard.progressOf(key).length());
+        }
+        return result;
     }
 }
