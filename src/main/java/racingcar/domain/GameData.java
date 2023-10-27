de6f48f0c695645e780253cd02c5b834c023ce67
brightfanta <brightfanta@gmail.com>
2023-10-27 20:28:53 +0900

    feat: 최종 우승자 리스트 콜렉션 생성

--- src/main/java/racingcar/domain/GameData.java
+++ src/main/java/racingcar/domain/GameData.java
@@ -1,8 +1,23 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class GameData {
 
     private Integer tryRepetitionNumber;
+    private List<Car> winnerList;
+
+    public Integer getTryRepetitionNumber() {
+        return tryRepetitionNumber;
+    }
+
+    public List<Car> getWinnerList() {
+        return winnerList;
+    }
+
+    public void setWinnerList(List<Car> winnerList) {
+        this.winnerList = winnerList;
+    }
 
     public void setTryRepetitionNumber(Integer tryRepetitionNumber) {
         this.tryRepetitionNumber = tryRepetitionNumber;
