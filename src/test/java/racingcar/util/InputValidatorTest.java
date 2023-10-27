451a47a6ea408575c2a0530239a8f79f8be725c9
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-27 23:48:53 +0900

    test(InputValidatorTest): 시도 횟수 입력값 검증 기능 테스트 구현

--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class InputValidatorTest {
 
     private static final String CAR_NAME_LONG = "자동차 이름의 길이가 5를 초과하였습니다. : ";
+    private static final String NOT_NUMBER = "숫자가 아닙니다.";
     private static final String SEPARATOR = ",";
 
     @Test
@@ -27,6 +28,33 @@ class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.validateRacingCarNames(carNames))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(CAR_NAME_LONG);
+    }
+
+    @Test
+    void validateTryCount_시도횟수가_영어인_경우() {
+        String tryCount = "asd";
+
+        assertThatThrownBy(() -> InputValidator.validateTryCount(tryCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(NOT_NUMBER);
+    }
+
+    @Test
+    void validateTryCount_시도횟수가_숫자와_영어가_섞인_경우01() {
+        String tryCount = "5a";
+
+        assertThatThrownBy(() -> InputValidator.validateTryCount(tryCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(NOT_NUMBER);
+    }
+
+    @Test
+    void validateTryCount_시도횟수가_숫자와_영어가_섞인_경우02() {
+        String tryCount = "a5";
+
+        assertThatThrownBy(() -> InputValidator.validateTryCount(tryCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(NOT_NUMBER);
     }
 
 }
