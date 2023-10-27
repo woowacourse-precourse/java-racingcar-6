afa0a43afef162e12347217b42aa461f64463c70
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-27 19:13:04 +0900

    feat(controller) : gameController 생성 및 생성자 설정

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,13 @@
+package racingcar.controller;
+
+import racingcar.util.NumberGenrator;
+import racingcar.util.RandomNumberGenrator;
+import racingcar.view.InputView;
+
+public class GameController {
+    private final static InputView inputView = new InputView();
+    private final static NumberGenrator randomNUmberGenerator = new RandomNumberGenrator();
+
+    public void start() {
+    }
+}
