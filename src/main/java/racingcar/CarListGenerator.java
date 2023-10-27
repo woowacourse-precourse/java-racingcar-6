3455e70873b40778fcfe1f07936159098fc2084d
devhoya97 <diajbh123@gmail.com>
2023-10-27 14:35:59 +0900

    feat(racingcar) : 자동차 객체 리스트 생성기능 구현
    Cars 생성자의 매개변수로 전달할 List<Car> 객체를 생성하기 위해 필요한 클래스 구현함

--- src/main/java/racingcar/CarListGenerator.java
+++ src/main/java/racingcar/CarListGenerator.java
@@ -1,2 +1,11 @@
-package racingcar;public class CarListGenerator {
+package racingcar;
+
+import java.util.List;
+
+public class CarListGenerator {
+    public static List<Car> getCarList(List<String> carStrings) {
+        return carStrings.stream()
+                .map(Car::new)
+                .toList();
+    }
 }
