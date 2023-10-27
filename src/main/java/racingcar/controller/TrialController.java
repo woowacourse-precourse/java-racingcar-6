8eea23cf9e7572cba1ab3b2b540ec7a1fa661a46
isyoo1213 <moethns@gmail.com>
2023-10-27 21:53:48 +0900

    feat: TrialController 경주 시도 수행 기능 추가 TrialController - generateRandomNumber() 추가 - calculateMovement() 추가 README.md - 기능 명세 구현 체크

---
+++ src/main/java/racingcar/controller/TrialController.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class TrialController {
+    private static final int RANDOM_GENERATE_START_NUMBER = 0;
+    private static final int RANDOM_GENERATE_END_NUMBER = 9;
+    private static final int GO_OR_STOP_NUMBER = 4;
+
+    public boolean calculateMovement() {
+        return generateRandomNumber() >= GO_OR_STOP_NUMBER;
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(RANDOM_GENERATE_START_NUMBER, RANDOM_GENERATE_END_NUMBER);
+    }
+}
