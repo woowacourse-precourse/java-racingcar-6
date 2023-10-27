67d8c3d95b790a8ecc1dd37712058fbdb8fa0527
Namyejun <yejun1148@gmail.com>
2023-10-27 15:18:30 +0900

    테스트를 위해 static이었던 Set을 수정함, 초기화 생성자로 함

--- src/main/java/racingcar/implementation/UserInput.java
+++ src/main/java/racingcar/implementation/UserInput.java
@@ -5,10 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class UserInput {
+    public Scanner sc = new Scanner(System.in);
+    private ValidChecker checker;
+
+    public UserInput() {
+        checker = new ValidChecker();
+    }
 
     public List<String> getCarList() {
-        String input = Console.readLine();
-        List<String> carList = ValidChecker.carListCheck(input); // 5글자 이하에 대한 유효성 검사
+        String input = sc.next();
+        List<String> carList = checker.carListCheck(input);
 
         for (int i = 0; i < carList.size(); i++) {
             carList.set(i, carList.get(i).concat(" : "));
@@ -20,8 +26,8 @@ public class UserInput {
     public int getNumberOfTry() {
         int numberOfTry = 0;
 
-        String input = Console.readLine();
-        numberOfTry = ValidChecker.numberOfTryCheck(input);
+        String input = sc.next();
+        numberOfTry = checker.numberOfTryCheck(input);
 
         return numberOfTry;
     }
--- src/main/java/racingcar/implementation/ValidChecker.java
+++ src/main/java/racingcar/implementation/ValidChecker.java
@@ -3,9 +3,13 @@ package racingcar.implementation;
 import java.util.*;
 
 public class ValidChecker {
-    private static Set<String> carSet = new HashSet<>();
+    private Set<String> carSet;
 
-    public static void isAlphabetic(String car) {
+    public ValidChecker() {
+        carSet = new HashSet<>();
+    }
+
+    public void isAlphabetic(String car) {
         for (int i = 0; i < car.length(); i++) {
             if (!(97 <= car.charAt(i) && car.charAt(i) <= 122)) {
                 throw new IllegalArgumentException("알파벳 소문자가 아닌 값이 입력됐습니다.");
@@ -13,7 +17,7 @@ public class ValidChecker {
         }
     }
 
-    public static void isDuplicated(String car) {
+    public void isDuplicated(String car) {
         if (carSet.contains(car)) {
             throw new IllegalArgumentException("중복된 자동차 이름이 입력됐습니다.");
         } else {
@@ -21,7 +25,7 @@ public class ValidChecker {
         }
     }
 
-    public static ArrayList<String> carListCheck(String input) {
+    public ArrayList<String> carListCheck(String input) {
         ArrayList<String> carList = new ArrayList<>(Arrays.stream(input.split(",")).toList());
         Iterator<String> iterator = carList.iterator();
 
@@ -34,13 +38,13 @@ public class ValidChecker {
         return carList;
     }
 
-    public static void atMostFiveWords(String car) {
+    public void atMostFiveWords(String car) {
         if (car.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름은 다섯 글자 이하만 가능합니다.");
         }
     }
 
-    public static int numberOfTryCheck(String input) {
+    public int numberOfTryCheck(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
