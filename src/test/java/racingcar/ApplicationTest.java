6234db1beb8f21a84e5d18e3e9a6604162ba55a0
PJH <parkjh0858@gmail.com>
2023-10-27 11:13:32 +0900

    test: 주행 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import action.Car;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,38 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void 자동차_이름_길이_예외_처리() {
+        assertThatThrownBy(() -> Car.checkNameLength("applePie"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주행_테스트() {
+        Car car = new Car("test");
+        int action = car.action();
+
+        if (action == MOVING_FORWARD) {
+            assertThat(car.getStatus()).isEqualTo("-");
+        }
+        else {
+            assertThat(car.getStatus()).isEqualTo("");
+        }
+    }
+
+    @Test
+    void 주행_결과_출력_테스트() {
+        Car car = new Car("car1");
+        int action = car.action();
+
+        if(action == MOVING_FORWARD) {
+            assertThat(car.printResult()).isEqualTo("car1 : -");
+        }
+        else {
+            assertThat(car.printResult()).isEqualTo("car1 : ");
+        }
+    }
 
     @Test
     void 전진_정지() {
