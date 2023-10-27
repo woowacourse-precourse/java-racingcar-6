fc9cbb9bc9ed8cb2af300c16d1409ab30693ad0a
leeedohyun <ldh19738@gmail.com>
2023-10-27 19:20:55 +0900

    feat: 우승자 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_NUMBERS_OF_MOVES_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void printInputCarsNameMessage() {
         System.out.println(INPUT_CARS_NAME_MESSAGE);
@@ -15,5 +16,9 @@ public class OutputView {
 
     public static void printCurrentLocation(String currentLocation) {
         System.out.println(currentLocation);
+    }
+
+    public static void printWinnerNames(String winnerNames) {
+        System.out.println(WINNER_MESSAGE + winnerNames);
     }
 }
