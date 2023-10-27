0af63f172eee8a74cc936228a504e9748dd2b6ae
koomchang <leahpar0401@gmail.com>
2023-10-28 02:02:06 +0900

    feat: IsWinner VO에서 초기화 하는 함수 구현

--- src/main/java/racingcar/model/IsWinner.java
+++ src/main/java/racingcar/model/IsWinner.java
@@ -1,4 +1,14 @@
 package racingcar.model;
 
 public class IsWinner {
+
+    private final boolean isWinner;
+
+    public IsWinner(boolean isWinner) {
+        this.isWinner = isWinner;
+    }
+
+    public static IsWinner init() {
+        return new IsWinner(false);
+    }
 }
