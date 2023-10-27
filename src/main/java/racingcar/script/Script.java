b7cb97e05b66ea62c5d0556840f63cbf670a0427
DH Jeong <wjdehgns97@gmail.com>
2023-10-27 19:00:15 +0900

    feat: 미리 저장한 Enum Script 출력하는 기능 구현

---
+++ src/main/java/racingcar/script/Script.java
@@ -0,0 +1,18 @@
+package racingcar.script;
+
+public enum Script {
+    LINEUP("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
+    LAPS("시도할 회수는 몇회인가요?\n"),
+    RESULT("실행 결과\n"),
+    PRIZE_CEREMONY("최종 우승자 : ");
+
+    private final String script;
+
+    Script(String script) {
+        this.script = script;
+    }
+
+    public void print(){
+        System.out.print(script);
+    }
+}
