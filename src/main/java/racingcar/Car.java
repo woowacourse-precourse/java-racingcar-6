0ef7d32387a93c459c77a99d97164c1d03e66803
yubin <dbqls2821@gmail.com>
2023-10-27 18:36:24 +0900

    fix: Car객체가 인스턴스화될 때 String값을 받도록 수정 (Cars에서 Name에 대한 의존성 제거를 위함)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,25 @@
 package racingcar;
 
-record Car(Name name) {
-    Car {
-        if (name == null) throw new IllegalArgumentException();
+import java.util.Objects;
+
+class Car {
+
+    private final Name name;
+
+    Car(String name) {
+        this.name = new Name(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -17,7 +17,7 @@ final class CarTest {
 
     @Test
     void 동일한_이름을_가진_차는_동일한_객체로_인식한다() {
-        Name sameName = new Name("동일한이름");
+        String sameName = "동일한이름";
         Car car1 = new Car(sameName);
         Car car2 = new Car(sameName);
 
@@ -26,7 +26,7 @@ final class CarTest {
 
     @Test
     void 동일한_이름을_가진_차는_Hash_자료구조에서도_동일한_객체로_인식된다() {
-        Name sameName = new Name("동일한이름");
+        String sameName = "동일한이름";
         Car car1 = new Car(sameName);
         Car car2 = new Car(sameName);
         Set<Car> set = new HashSet<>();
