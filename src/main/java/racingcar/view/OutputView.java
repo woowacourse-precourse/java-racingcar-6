fcd3d1232f7d590ee7486f3eccc47fcfd8922f14
xogus3492 <wkdxogus3492@gmail.com>
2023-10-27 20:03:01 +0900

    Feat(OutputView): 자동차의 현재 전진 상태를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,30 @@
 package racingcar.view;
 
+import racingcar.domain.Cars;
+
 public class OutputView {
+    private static final int INITIAL_COUNT = 0;
     private static final String EXECUTION_RESULT_OUTPUT_MESSAGE = "실행 결과";
+    private static final String DASH = "-";
 
     public void printResultMessage() {
         System.out.println("\n" + EXECUTION_RESULT_OUTPUT_MESSAGE);
     }
 
+    public void printExecutionResult(Cars cars) {
+        for (String carName : cars.getCarNameList()) {
+            int advanceCount = cars.getCarAdvanceCount(carName);
+            System.out.print(carName + " : ");
+            printDash(advanceCount);
+            System.out.println();
+        }
+        System.out.println();
+    }
 
+    private void printDash(int count) {
+        int currentCount = INITIAL_COUNT;
+        while (currentCount++ < count) {
+            System.out.print(DASH);
+        }
+    }
 }
