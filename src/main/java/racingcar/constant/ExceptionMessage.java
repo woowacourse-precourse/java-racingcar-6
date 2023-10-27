9f4af980b2ea13dd0d6c87e991ecb425fad93ffc
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-27 18:50:39 +0900

    feat(ExceptionMessage) : 빈 문자열 입력 시 예외 처리 메시지 추가

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -1,8 +1,9 @@
 package racingcar.constant;
 
 public class ExceptionMessage {
+    public static final String BLANK_INPUT = "빈 문자열은 입력할 수 없습니다.";
     public static final String NAME_LENGTH_ZERO = "입력된 문자열에 길이가 0이 되는 자동차 이름이 있습니다: ";
-    public static final String OVER_NAME_LENGTH = "자동차 이름의 길이는 최대 5글자입니다: ";
+    public static final String OVER_NAME_LIMIT = "자동차 이름의 길이는 최대 5글자입니다: ";
     public static final String NAME_DUPLICATED = "자동차 이름은 서로 중복되지 않아야 합니다: ";
     public static final String NUMERIC = "문자가 아닌 숫자를 입력해야 합니다: ";
 }
