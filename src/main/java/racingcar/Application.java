be529eab933f7170f1cced285bff7e1fd5a05cb0
Arachneee <jhg2819@naver.com>
2023-10-28 00:09:39 +0900

    refactor: 객체 생성 방식 변경
    의존관계를 생성자 주입을 통해 할 수 있도록 변경했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import racingcar.controller.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
+        RacingGame racingGame = new RacingGame(new InputView(), new OutputView());
         racingGame.run();
     }
 
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,104 +1,41 @@
 package racingcar.controller;
 
-import static racingcar.constant.RacingGameConstant.CARS_SPLIT_STRING;
-import static racingcar.constant.RacingGameConstant.REGEX;
-import static racingcar.constant.RacingGameConstant.ZERO;
-import static racingcar.exception.ErrorMessage.NOT_INTEGER;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
 import racingcar.domain.Cars;
 import racingcar.dto.CarsDto;
-import racingcar.exception.RacingGameException;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGame {
 
-    private static final InputView inputView = new InputView();
-    private static final OutputView outputView = new OutputView();
-    private Integer rotateCount;
-    private Cars cars;
+    public static final Integer ZERO = 0;
+    private final InputView inputView;
+    private final OutputView outputView;
 
-    public RacingGame() {
+    public RacingGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run() {
-        init();
+        final Cars cars = Cars.from(inputView.enterCarNames());
+        Integer rotateCount = inputView.enterRotateNumber();
 
-        rotate();
+        rotate(cars, rotateCount);
 
-        finish();
+        outputView.printWinners(cars.findWinnerNameList());
+        Console.close();
     }
 
-    private void init() {
-        generateCars();
-        askRotateNumber();
-    }
-
-    private void generateCars() {
-        final String carNames = inputView.enterCarNames();
-
-        cars = Cars.from(splitString(carNames));
-    }
-
-    private void askRotateNumber() {
-        final String rotateNumber = inputView.enterRotateNumber();
-
-        rotateCount = convertToInteger(rotateNumber);
-    }
-
-    private List<String> splitString(String carNames) {
-        return Arrays.stream(carNames.split(CARS_SPLIT_STRING))
-                .toList();
-    }
-
-    private Integer convertToInteger(String inputNumberString) {
-        validateNumber(inputNumberString);
-
-        return Integer.valueOf(inputNumberString);
-    }
-
-    private void validateNumber(String inputNumberString) {
-        if (isNotNumber(inputNumberString)) {
-            throw RacingGameException.of(NOT_INTEGER);
-        }
-
-    }
-
-    private boolean isNotNumber(String inputNumberString) {
-        return !inputNumberString.matches(REGEX);
-    }
-
-    private void rotate() {
+    private void rotate(final Cars cars, Integer rotateCount) {
         outputView.printRunResult();
 
-        while (isRemainRotateCount()) {
-            raceStart();
-            subRotateCount();
+        while (rotateCount-- > ZERO) {
+            cars.go();
+            outputView.printCarsResults(CarsDto.from(cars));
         }
 
-    }
-
-    private void subRotateCount() {
-        rotateCount--;
-    }
-
-    private boolean isRemainRotateCount() {
-        return rotateCount > ZERO;
-    }
-
-    private void raceStart() {
-        cars.go();
-
-        outputView.printCarsResults(CarsDto.from(cars));
-    }
-
-    private void finish() {
-        outputView.printWinners(cars.findWinnerNameList());
-
-        Console.close();
     }
 
 }
