20ad64f53d22814a67fd1da94350637cecf535f5
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-27 11:11:55 +0900

    feat : 게임 진행 순서 정리

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+
+    private void gameStart(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = Console.readLine();
+
+        //userinput validation 체크
+
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryInput = Console.readLine();
+
+        //tryInput validation 체크
+
+
+        //게임 전광판에 띄워 출력
+
+        //최종 우승자 출력
+    }
+}
