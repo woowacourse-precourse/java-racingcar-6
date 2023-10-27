c47893d014114b1a0d01cfa223764914d3037511
Henry-Cha <sungwon4728@naver.com>
2023-10-27 22:44:26 +0900

    feat: car 클래스 기능 추가
    상태출력, 비교

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,21 +2,35 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Car {
+public class Car implements Comparable<Car>{
     String name;
     int moveCount;
+    StringBuilder sb;
 
     public Car(){}
     public Car(String name){
         validName(name);
         this.name = name;
         this.moveCount = 0;
+        this.sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" : ");
     }
     public void validName(String name){
         if(name.length()>5) throw new IllegalArgumentException("5글자 이하만 가능");
     }
     public void move(){
-        if(Randoms.pickNumberInRange(0,9)>=4)
+        if(Randoms.pickNumberInRange(0,9)>=4) {
             this.moveCount++;
+            this.sb.append("-");
+        }
+    }
+    public String toString(){
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.moveCount - o.moveCount;
     }
 }
