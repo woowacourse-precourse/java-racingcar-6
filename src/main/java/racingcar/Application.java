1c02f4ebac53cb97841ea58522dfb83ab201649a
SeongHun <tjdgns5506@gmail.com>
2023-10-27 13:38:55 +0900

    feat: 이름들이 입력 요구 사항에 맞는지 확인하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,37 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String stringCarNames = Console.readLine();
 
+        if(!checkCarNames(stringCarNames)) throw new IllegalArgumentException();
 
+
+    }
+
+    public static boolean checkCarNames(String stringCarNames) {
+
+        if(!stringCarNames.contains(",")|| stringCarNames.contains(" ")) return false;
+
+        String[] arrayCarNames = stringCarNames.split(",");
+
+        for(int i=0;i<arrayCarNames.length;i++){
+            if(arrayCarNames[i].length()>5) return false;
+        }
+
+        Set<String> set = new HashSet<>();
+        for (String name : arrayCarNames) {
+            set.add(name);
+        }
+
+        if (set.size() != arrayCarNames.length) return false;
+
+        return true;
     }
 }
