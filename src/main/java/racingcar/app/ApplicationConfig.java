2b5798e671c6e525e40a7c961688b12c9fd246e6
Jaehoon-So <jhspacelover@naver.com>
2023-10-28 01:11:30 +0900

    feat: 애플리케이션 의존관계 설정클래스 작성

--- src/main/java/racingcar/app/ApplicationConfig.java
+++ src/main/java/racingcar/app/ApplicationConfig.java
@@ -2,12 +2,17 @@ package racingcar.app;
 
 import racingcar.domain.RacingCarFactory;
 import racingcar.domain.RacingGame;
+import racingcar.domain.interfaces.CarFactory;
 import racingcar.domain.interfaces.Game;
 
 public class ApplicationConfig {
 
     public static Game getApplicationGame() {
-        return new RacingGame(new RacingCarFactory());
+        return new RacingGame(getGameCarFactory());
+    }
+
+    public static CarFactory getGameCarFactory() {
+        return new RacingCarFactory();
     }
 
 }
