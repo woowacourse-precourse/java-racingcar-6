1e42287628b1e6ec87ef41bde2185cff18cfc754
Han Kyuhyun <khh5345@naver.com>
2023-10-27 16:24:35 +0900

    feat: Implement result display showing round outcomes and overall winners.
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,13 +10,26 @@ public class Application {
     public static void main(String[] args) {
         List<String> carNames = requestCarNames();
         int rounds = requestRounds();
+
+        Race race = new Race(carNames);
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < rounds; i++) {
+            race.raceRound();
+            for (Car car : race.getCars()) {
+                System.out.println(car);
+            }
+            System.out.println();
+        }
+
+        List<String> winners = race.getWinners();
+        System.out.print("최종 우승자: ");
+        System.out.println(String.join(", ", winners));
     }
 
     private static List<String> requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Arrays.stream(Console.readLine().split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
+        return Arrays.stream(Console.readLine().split(",")).map(String::trim).collect(Collectors.toList());
     }
 
     private static int requestRounds() {
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -23,4 +23,19 @@ public class Race {
     public List<Car> getCars() {
         return cars;
     }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
 }
