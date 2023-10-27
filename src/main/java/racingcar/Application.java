824cc508d3da0c08837077aa7990dec7cd9db664
nylonmusk <2912rla@gmail.com>
2023-10-27 16:19:21 +0900

    feat : Application클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.domain.Player;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Player player = new Player();
+
+        Controller controller = new Controller(randomNumberGenerator, inputView, outputView, player);
+        controller.run();
     }
 }
