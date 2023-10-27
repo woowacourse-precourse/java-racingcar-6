fe13a2bb8fe43b5f748ada6e4f3d926f2f604cf6
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-27 22:54:12 +0900

    feat: 게임 생성자, 플레이어 설정, 시도 횟수 설정 기능 구현 - 생성자는 player리스트를 초기화한다 - 플레이어 설정은 플레이어를 List<String>에 저장 후 플레이어로 변환

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,15 +1,40 @@
 package racingcar;
 
 import game.Game;
+import java.util.ArrayList;
+import java.util.List;
+import util.stringconverter.RacingCarConverter;
+import util.RacingCarInputReader;
 
 public class RacingCarGame implements Game {
+    private List<RacingCarPlayer> players;
+
+    private int tryCount;
+    private final RacingCarInputReader racingCarInputReader;
+
+    private final RacingCarConverter racingCarConverter;
+
+    public RacingCarGame() {
+        this.players = new ArrayList<>();
+        this.racingCarInputReader = new RacingCarInputReader();
+        this.racingCarConverter = new RacingCarConverter();
+    }
+
     @Override
     public void start() {
-
     }
 
     @Override
     public void end() {
 
+    }
+
+    public void setPlayers(){
+        List<String> players = racingCarInputReader.inputPlayers();
+        players.stream().map(racingCarConverter::fromString).forEach(this.players::add);
+    }
+
+    public void setTryCount(){
+        this.tryCount = racingCarInputReader.inputTryCount();
     }
 }
