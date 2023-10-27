1e506dfbfa89ddcfbdb570988c8f0a51367537cf
jaegyeongkim <dongji_11@naver.com>
2023-10-27 17:16:04 +0900

    feat(Car): 자동차 생성

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Car {
+    String name;
+    String nowPlace = "";
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
