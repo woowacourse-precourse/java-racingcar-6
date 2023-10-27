04fe2b3c45d4ab14051e475d4d1ae330980ad543
kkw2758 <dtc3229@gmail.com>
2023-10-27 22:40:45 +0900

    feat : 우승자 출력, 각 차수별 실행 결과 출력 기능 구현
    - 화면 출력을 담당하는 Output 클래스 생성
      - printCarPosition() - 각 차수별 실행 결과 출력
      - printVictoryCarNames() - 우승자 출력
      - printCarInputMessage() - 게임에 참가할 차 이름 입력 문구 출력
      - printTryCountInputMessage() - 시도할 회수 입력 문구 출력

---
+++ src/main/java/racingcar/domain/Output.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Output {
+    public static void printCarPosition(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + ":" + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printCarInputMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printTryCountInputMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printVictoryCarNames(List<String> victoryCarName) {
+        System.out.print("최종 우승자 : " + String.join(", ", victoryCarName));
+        System.out.println();
+    }
+}
