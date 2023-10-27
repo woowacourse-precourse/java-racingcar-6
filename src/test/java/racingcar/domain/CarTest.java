a8900f25d58dea5c05da2037636c344aeba919ea
waveofmymind <sjun0913@gmail.com>
2023-10-28 02:08:04 +0900

    test: Car 생성시 이름 경곗값 테스트를 구현한다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,11 +8,13 @@ public class CarTest {
 
     @DisplayName("자동차의 이름을 인자로 받아 자동차를 생성한다.")
     @Test
-    void createCarTest() {
+    void createCarSuccessTest() {
         // given
         String name = "pobi";
+
         // when
         Car car = new Car(new Name(name));
+
         // then
         Assertions.assertThat(car.getName()).isEqualTo(new Name(name));
         Assertions.assertThat(car.getDistance()).isEqualTo(Distance.zero());
@@ -23,11 +25,35 @@ public class CarTest {
     void createCarExceptionTest() {
         // given
         String name = "waveofmymind";
+
         // when & then
         Assertions.assertThatThrownBy(() -> new Car(new Name(name)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
+                .hasMessageContaining("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+    }
+
+    @DisplayName("자동차의 이름이 5자 초과일 경우 IllegalArgumentException이 발생한다.")
+    @Test
+    void createCarExceptionTestByOverName() {
+        // given
+        String name = "waveofmymind";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Car(new Name(name)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+
+    }
+
+    @DisplayName("자동차의 이름이 1자 미만일 경우 IllegalArgumentException이 발생한다.")
+    @Test
+    void createCarExceptionTestByUnderName() {
+        // given
+        String name = "";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Car(new Name(name)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
     }
 }
-
-
