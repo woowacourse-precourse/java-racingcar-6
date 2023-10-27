07301344f7f5e287b7e866e78bcc1944ff922cc2
DESKTOP-FH4O6OD\syt11 <syt1128@gmail.com>
2023-10-27 21:59:11 +0900

    :add: UserInputs 추가
    사용자가 입력해야하는 요소들의 데이터를 저장하는
    carNames, loopCount를 포함하는 model인 UserInputs
    를 추가

---
+++ src/main/java/racingcar/model/UserInputs.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class UserInputs {
+
+    public static String carNames;
+    public static int loopCount;
+
+    public String carNamesAsString () {
+        return carNames;
+    }
+
+    public int attemptCount () {
+        return loopCount;
+    }
+}
