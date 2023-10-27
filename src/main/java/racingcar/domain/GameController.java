eae99533e03c38e3dab5124b436392a127e3abc7
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-10-27 19:39:13 +0900

    feat(GameController) : 자동차 이동 횟수 입력란 추가

--- src/main/java/racingcar/domain/GameController.java
+++ src/main/java/racingcar/domain/GameController.java
@@ -6,10 +6,16 @@ public class GameController {
 
     public GameController() {
         setCars();
+        setNumbers();
     }
 
     public void setCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String Car = Console.readLine();
+    }
+
+    public void setNumbers() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numbers = Integer.parseInt(Console.readLine());
     }
 }
