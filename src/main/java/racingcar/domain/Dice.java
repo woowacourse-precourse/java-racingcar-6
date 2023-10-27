8ceccb4f55f584be446be64781ddb4db3dd2b153
donghyunkim <ghffu405@gmail.com>
2023-10-27 17:07:00 +0900

    refactor: 싱글톤으로 변경

--- src/main/java/racingcar/domain/Dice.java
+++ src/main/java/racingcar/domain/Dice.java
@@ -8,13 +8,20 @@ public class Dice {
 
     private static final int MIN_DICE_NUMBER = 0;
     private static final int MAX_DICE_NUMBER = 9;
+    private static Dice dice = null;
 
     private Dice() {
     }
 
-    public static int getRandomNumber(){
-        return Randoms.pickNumberInRange(MIN_DICE_NUMBER, MAX_DICE_NUMBER);
+    public static Dice getInstance(){
+        if(dice == null){
+            dice = new Dice();
+        }
+        return dice;
     }
 
+    public int getRandomNumber(){
+        return Randoms.pickNumberInRange(MIN_DICE_NUMBER, MAX_DICE_NUMBER);
+    }
 
 }
