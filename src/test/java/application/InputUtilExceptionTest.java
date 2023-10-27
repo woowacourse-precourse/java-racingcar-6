d126a5e78ee617f24d137dd0ab59d5a3c261a089
jinny-l <jinny.l.dev@gmail.com>
2023-10-27 20:13:06 +0900

    ✅test: 기본 입력 기능 테스트 코드 작성

---
+++ src/test/java/application/InputUtilExceptionTest.java
@@ -0,0 +1,49 @@
+package application;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Application;
+
+@DisplayName("입력 기능 예외 테스트")
+class InputUtilExceptionTest extends NsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "      ", "\n","\r\n", "\r"})
+    void 공백_입력_값_예외_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"   ,  ", "pobi,   , jun"})
+    void 쉼표_기준_공백_입력_값_예외_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"eng;123;456", "a;b", "쉼표로'구분'안함", "쉼.표.없"})
+    void 구분자_예외_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
