f7f92db4f76c81be23552f347bea9539828ee907
mina <qkralsdk991@naver.com>
2023-10-27 17:21:05 +0900

    Refactor : 디렉토리 이름 소문자 변경

--- src/main/java/racingcar/view/RequestMessage.java
+++ src/main/java/racingcar/view/RequestMessage.java
@@ -1,4 +1,4 @@
-package racingcar.View;
+package racingcar.view;
 
 public class RequestMessage {
     private static final String CAR_NAME="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
--- src/test/java/racingcar/view/ViewTest.java
+++ src/test/java/racingcar/view/ViewTest.java
@@ -1,8 +1,6 @@
-package racingcar.ViewTest;
+package racingcar.view;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import racingcar.View.RequestMessage;
 
 public class ViewTest {
     @Test
