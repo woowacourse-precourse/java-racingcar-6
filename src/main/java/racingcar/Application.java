a67d6aa323cfacf9984c3b53f3f73015d25c7ac2
SeongJun Lee <ekdma3259@gmail.com>
2023-10-27 20:50:37 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class Application {
 	//게임 시작전 안내문 출력
     public static void startGame() {
     	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    
+    //차 이름(쉼표로 구분된 여러 이름) 입력받기
+    public static String inputCarName(){
+    	String carNames = Console.readLine();
+    	return carNames;
     }
     
 }
