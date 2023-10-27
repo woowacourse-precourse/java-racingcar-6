a2001af5c6dba77d51155a8a957bbdf1b7290d4c
heiler <threepebbles@naver.com>
2023-10-28 06:58:31 +0900

    test: 사용자 입력에 대한 예외 테스트들 추가

--- src/test/java/racingcar/myTest.java
+++ src/test/java/racingcar/myTest.java
@@ -9,16 +9,27 @@ import org.junit.jupiter.api.Test;
 
 public class myTest extends NsTest {
     @Test
-    void 자동차_이름_입력_예외_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    assertThatThrownBy(() -> {
-                        run("1,2,more_than_five");
-                    }).isInstanceOf(IllegalArgumentException.class)
-                            .hasMessageContaining("이름은 5자 이하만 가능합니다.");
-                },
-                1
-        );
+    void 자동차_이름_중복_예외_테스트() {
+        assertThatThrownBy(() -> {
+            run("succ,ess,ful,ess");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 이름이 있습니다.");
+    }
+
+    @Test
+    void 자동차_이름_공백_예외_테스트() {
+        assertThatThrownBy(() -> {
+            run("1,,more_than_five");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 1자 이상");
+    }
+
+    @Test
+    void 자동차_이름_길이_예외_테스트() {
+        assertThatThrownBy(() -> {
+            run("1,2,more_than_five");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 1자 이상");
     }
 
     @Test
@@ -28,7 +39,7 @@ public class myTest extends NsTest {
                     assertThatThrownBy(() -> {
                         run("john,pobi,tree,hash", "안녕");
                     }).isInstanceOf(IllegalArgumentException.class)
-                            .hasMessageContaining("음이 아닌 수를 입력하지 않았습니다.");
+                            .hasMessageContaining("양수를 입력하지 않았습니다.");
                 },
                 1
         );
