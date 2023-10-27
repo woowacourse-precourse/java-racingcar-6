f565c2e533d5ce49710bcab8cc850d1eb6cf4b7f
freekyr7529 <freekyr7529@gmail.com>
2023-10-28 00:58:49 +0900

    feat: 출력 구현
    기능 목록에 있는 출력 부분을 구현하였다. 목록은 아래와 같다.
    - 자동차 이름 입력 안내 메시지: inputNamesMsg
    - 시도할 횟수 입력 안내 메시지: inputRoundMsg
    - 실행 결과 제목: resultTitleMsg
    - 각 차수별 실행 결과: carResultMsg
    - 최종 우승자: finalWinnerMsg

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class View {
+
+    public void printMsg(String output) {
+        System.out.println(output);
+    }
+
+    public void inputNamesMsg() {
+        printMsg("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void inputRoundMsg() {
+        printMsg("시도할 횟수는 몇회인가요?");
+    }
+
+    public void resultTitleMsg() {
+        printMsg("실행 결과");
+    }
+
+    public void carResultMsg(String name, String process) {
+        printMsg(name + " : " + process);
+    }
+
+    public void finalWinnerMsg() {
+        System.out.print("최종 우승자 : ");
+    }
+}
