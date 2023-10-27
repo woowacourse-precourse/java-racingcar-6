e4e20b00d9ea8dc22432325404ef95652415ad8c
hyunjin <guswlsdl0121@gmail.com>
2023-10-27 21:58:57 +0900

    feat: 게임 첫 시작 문구 출력 및 자동차 이름 입력받기 구현
    개별 자동차를 나타내는 Car와 그들을 List로 가지는 Cars 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String[] nameInput = readNames();
+        List<Car> carList = Arrays.stream(nameInput)
+                .map(Car::new)
+                .toList();
+
+        Cars cars = new Cars(carList);
+    }
+
+    private static String[] readNames() {
+        return Console.readLine().split(",");
     }
 }
+
+
---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.List;
+
+public class Cars {
+    List<Car> carList;
+
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
+}
