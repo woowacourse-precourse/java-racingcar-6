65a8f8a22c755b72299d65787e21aa640b8b7efd
june-777 <wlwhswnsrl96@gmail.com>
2023-10-27 15:29:59 +0900

    test(GameUtilTest): 입력 받은 자동차의 이름을 쉼표로 구분하는 기능 테스트

---
+++ src/test/java/racingcar/gameutil/GameUtilTest.java
@@ -0,0 +1,31 @@
+package racingcar.gameutil;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.GameUtil;
+
+class GameUtilTest {
+
+    GameUtil gameUtil = new GameUtil();
+
+    @Test
+    void splitByCommasTest() {
+        // given
+        String input = "pobi,woni,jun";
+        // when
+        List<String> result = gameUtil.splitByCommas(input);
+        // then
+        Assertions.assertThat(result).containsExactly("pobi", "woni", "jun");
+    }
+
+    @Test
+    void splitByCommasTest2() {
+        // given
+        String input = "p,o,b,i";
+        // when
+        List<String> result = gameUtil.splitByCommas(input);
+        // then
+        Assertions.assertThat(result).containsExactly("p", "o", "b", "i");
+    }
+}
