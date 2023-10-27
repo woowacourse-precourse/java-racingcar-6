2da823f6dba4f7d087c68743d47cfb1836586832
Juho Lee <ailiartsuaL2@gmail.com>
2023-10-27 17:23:48 +0900

    feat: input 메서드 수정
    자동차만 입력 받음 -> 시도 회수 입력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,23 +7,37 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Application {
-    public static final Pattern INPUT_PATTERN = Pattern.compile("^[a-zA-Z]{1,5}$");
+    public static final Pattern CARS_PATTERN = Pattern.compile("^[a-zA-Z]{1,5}$");
+    public static final Pattern ATTEMPT_PATTERN = Pattern.compile("^[1-9]\\d*$");
+    static List<String> cars;
+    static int attempt;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        input();
     }
 
     // 자동차는 최소 1대, 자동차 이름은 최소 1글자 ~ 최대 5글자의 알파벳
     public static void input(){
-        String s = Console.readLine();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCars = Console.readLine();
 
-        List<String> cars = Arrays.asList(s.split(","));
+        cars = Arrays.asList(inputCars.split(","));
         if(cars.isEmpty()){
             throw new IllegalArgumentException("경주를 위해서는 자동차는 최소 1대 이상 필요해요.");
         }
         for (String car : cars) {
-            if(!INPUT_PATTERN.matcher(car).matches()){
-                throw new IllegalArgumentException("자동차 이름은 1글자에서 5글자 사이의 알파벳만 가능해요/");
+            if(!CARS_PATTERN.matcher(car).matches()){
+                throw new IllegalArgumentException("자동차 이름은 1글자에서 5글자 사이의 알파벳만 가능해요.");
             }
         }
+        
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputAttempt = Console.readLine();
+        if(!ATTEMPT_PATTERN.matcher(inputAttempt).matches()){
+            throw new IllegalArgumentException("회수는 1 이상의 숫자만 입력이 가능합니다.");
+        }
+        attempt = Integer.parseInt(inputAttempt);
+
+        System.out.println("");
     }
 }
