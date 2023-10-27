c5726b3b21aede89919b80bdc7a58981ca30084d
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 01:25:05 +0900

    test(InputTest) : InputView 관련 테스트 코드 추가

---
+++ src/test/java/racingcar/view/InputTest.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InputTest {
+    @Test
+    @DisplayName("입력값이 빈 문자열이면 예외 발생, 그렇지 않으면 정상 종료")
+    void validateInputBlankExceptionTest() {
+        assertThatCode(() -> new InputView().validateInputBlank(""))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatCode((() -> new InputView().validateInputBlank("name1,name2,name3")))
+                .doesNotThrowAnyException();
+    }
+}
