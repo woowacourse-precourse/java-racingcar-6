5e5b538a32455df69f097aebaada0fd1faa9bf93
HyungUk Ryu <hw0603@naver.com>
2023-10-27 16:57:25 +0900

    feat(Car): 자동차 이름에 알파벳과 숫자 이외의 문자가 있는 경우 예외처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,11 +3,13 @@ package racingcar.model;
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MIN_NAME_LENGTH = 1;
+    private static final String NAME_FORMAT_REGEX = "^[a-zA-Z0-9]*$";
     private final String name;
     private int position;
 
     public Car(String name, int position) {
         validateNameLength(name);
+        validateEachCharacter(name);
         this.name = name;
         this.position = position;
     }
@@ -17,6 +19,12 @@ public class Car {
             throw new IllegalArgumentException(
                     String.format("자동차 이름은 %d~%d 사이여야 합니다.", MIN_NAME_LENGTH, MAX_NAME_LENGTH)
             );
+        }
+    }
+
+    private void validateEachCharacter(String name) {
+        if (!name.matches(NAME_FORMAT_REGEX)) {
+            throw new IllegalArgumentException("자동차 이름에는 알파벳과 숫자만 사용할 수 있습니다.");
         }
     }
 }
