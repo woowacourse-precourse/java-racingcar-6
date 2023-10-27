98a581b6e5e1d94ab753dd6ac3709d369e6d719c
Junobee25 <wnsdhqo@naver.com>
2023-10-27 22:15:23 +0900

    feat: moveTheCar() 메서드 구현

--- src/main/java/racingcar/service/Move.java
+++ src/main/java/racingcar/service/Move.java
@@ -1,4 +1,18 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarGenerator;
+import racingcar.domain.RandomNumber;
+import racingcar.domain.RandomNumberGenerator;
+
 public class Move {
+    Car car = new Car();
+    CarGenerator carGenerator = new CarGenerator();
+    RandomNumber randomNumber = new RandomNumber();
+    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    public void moveTheCar(String carname) {
+        String moveValue = car.getCarList().get(carname);
+        car.getCarList().put(carname,moveValue + "-");
+    }
 }
