4dfd325a57ea5976deea71c685231b3121df8062
leechiyun <wychis28@gmail.com>
2023-10-27 11:32:58 +0900

    feat: 주어진 횟수 동안 멈추거나 전진 가능한 Player class 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.Player;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Player> players = Arrays.asList(new Player("honi"), new Player("jun"), new Player("woni"));
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < 5; i++) {
+            for (Player player: players) {
+                player.run();
+                player.print();
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/main/java/racingcar/domain/Player.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+public class Player {
+    private String name;
+    private int runCount;
+
+    public Player(String name){
+        this.name = name;
+        this.runCount = 0;
+    }
+
+    public void run(){
+        // 0 ~ 9 사이의 랜덤 숫자 생성
+        int randomNum = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
+
+        // 랜덤 값이 4 이상이면 전진
+        if(randomNum >= 4){
+            this.runCount++;
+        }
+    }
+
+    // 조건에 맞게 전진 횟수 출력
+    public void print(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name).append(" : ");
+
+        for (int i = 0; i < this.runCount; i++) {
+            sb.append("-");
+        }
+
+        System.out.println(sb);
+    }
+}
