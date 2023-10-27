845cebfab2e52362f5c9a0526206937df0b40e64
devhoya97 <diajbh123@gmail.com>
2023-10-27 16:13:54 +0900

    feat(racingcar) : 시나리오에 맞게 게임 구현 초판 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printCarNamesMessage();
+        List<String> carStrings = InputView.getCarStrings();
+        OutputView.printMoveCountMessage();
+        int moveCount = InputView.getMoveCount();
+        List<Car> carList = CarListGenerator.getCarList(carStrings);
+        Cars cars = new Cars(carList);
+        OutputView.printResult(cars, moveCount);
+        OutputView.printWinner(cars.getWinnerList());
     }
 }
