0741d980a52fa76c8ac0fac02c23762a64d9e1ba
isyoo1213 <moethns@gmail.com>
2023-10-27 14:08:28 +0900

    feat: 자동차 이름 입력값 유효성 검사 기능 추가 InputValidator - validateCarsNameInput() 추가   - convertStringToStringList()   - isOneToFive()   - isUsingCommaSeperator()   - isNotDuplicated() Application - 자동차 이름 유효성 검사 정상 출력 확인 로직 README.md - 기능 명세 구현 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,8 @@ package racingcar;
 import racingcar.utils.InputValidator;
 import racingcar.view.InputView;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
@@ -12,5 +14,10 @@ public class Application {
         System.out.println(userInputCarsName);
         String preprocessedInputString = inputValidator.preprocessInputString(userInputCarsName);
         System.out.println(preprocessedInputString);
+
+        List<String> inputStringList = inputValidator.validateCarsNameInput(preprocessedInputString);
+        for (String inputString : inputStringList) {
+            System.out.println("inputString = " + inputString);
+        }
     }
 }
--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -2,7 +2,13 @@ package racingcar.utils;
 
 import racingcar.constants.ExceptionMessages;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InputValidator {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 5;
     private static final String SPACING_STRING = " ";
     private static final String EMPTY_STRING = "";
 
@@ -16,6 +22,26 @@ public class InputValidator {
         return removeSpacing(inputString);
     }
 
+    public List<String> validateCarsNameInput(String inputString) {
+        if (!isUsingCommaSeperator(inputString)) {
+            ExceptionMessages.WRONG_SEPERATOR.throwException();
+        }
+        List<String> convertedStringList = convertStringToStringList(inputString);
+        if (!isOneToFive(convertedStringList)) {
+            ExceptionMessages.WRONG_RANGE_CAR_NAME.throwException();
+        }
+        if (!isNotDuplicated(convertedStringList)) {
+            ExceptionMessages.DUPLICATED_CAR_NAMES.throwException();
+        }
+        return convertedStringList;
+    }
+
+    private List<String> convertStringToStringList(String inputString) {
+        List<String> carsNameList;
+        carsNameList = Stream.of(inputString.split(",")).collect(Collectors.toList());
+        return carsNameList;
+    }
+
     private String removeSpacing(String inputString) {
         return inputString.replaceAll(SPACING_STRING, EMPTY_STRING);
     }
@@ -26,5 +52,21 @@ public class InputValidator {
 
     private boolean isEmpty(String inputString) {
         return inputString == EMPTY_STRING;
+    }
+
+    private boolean isOneToFive(List<String> inputStringList) {
+        return inputStringList.stream()
+                .filter((carName) -> ((carName.length() >= MINIMUM_NUMBER) && (carName.length() <= MAXIMUM_NUMBER)))
+                .count() == inputStringList.size();
+    }
+
+    private boolean isUsingCommaSeperator(String inputString) {
+        return ((inputString.length() > MAXIMUM_NUMBER) && (inputString.contains(",")));
+    }
+
+    private boolean isNotDuplicated(List<String> stringList) {
+        return stringList.stream()
+                .distinct()
+                .count() == stringList.size();
     }
 }
