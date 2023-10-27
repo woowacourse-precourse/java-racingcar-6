22de6d06b24e8a6b97435ca0095b986314678d23
정민규 <fastdodge7@naver.com>
2023-10-27 20:10:30 +0900

    feat(RacingGame): 사용자 입력, 자동차 이름 검증 기능 추가
    RacingGame 클래스의 run 메소드에서 사용자 입력을 받습니다.
    
    RacingGame 클래스의 extractCarNames 메소드에서 사용자 입력 문자열을 split하여 String 리스트로 만들어 줍니다.

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingGame {
+
+    private List<Car> carList;
+
+    public void runGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();
+
+        carNameInput.split(",");
+
+    }
+
+    public List<String> extractCarNames(String userInput) {
+        List<String> nameList = Arrays.asList(userInput.split(","));
+
+        nameList.stream().forEach(element -> {
+            if (element.length() > 5 || element.length() <= 0)
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        });
+
+        return nameList;
+    }
+}
