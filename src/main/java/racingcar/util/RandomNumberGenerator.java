b949440d5c868c18f2e8f2e0edd5d383ca46b12c
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:31:07 +0900

    [feat] 0~9 까지 난수 생성하는 메서드 기능 추가

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,4 +1,10 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
+
+    public int generate() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
