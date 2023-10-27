05f52ea4d7c0feac560381e30e3c7cf78ff1098a
KangGeonyoung <kky6335@gmail.com>
2023-10-27 22:52:53 +0900

    feat(InputValidator): 시도 횟수 valid 검사 - isValidAttemptNumber(String input): 사용자 input이 유효한 시도 횟수 입력인지 검사 - isIntegerNumber(String input): 사용자 input이 정수형 숫자인지 검사

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 
 public class InputValidator {
 
+    // 자동차 이름의 유효성 검사
     public static List<String> isValidCarName(String input) {
         List<String> carList = Stream.of(input.split(",")).collect(Collectors.toList());
 
@@ -13,6 +14,21 @@ public class InputValidator {
         isValidLength(carList);
 
         return carList;
+    }
+
+    // 시도 횟수의 유효성 검사
+    public static int isValidAttemptNumber(String input) {
+        int attemptNumber = isIntegerNumber(input);
+        return attemptNumber;
+    }
+
+    // 시도 횟수가 정수형 숫자인지 검사
+    private static int isIntegerNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // 자동차 이름들이 문자인지 검사
@@ -32,10 +48,5 @@ public class InputValidator {
                 throw new IllegalStateException();
             }
         }
-    }
-
-    public static void main(String[] args) {
-        String input = "pobiasdf,woni,jun!";
-        isValidCarName(input);
     }
 }
