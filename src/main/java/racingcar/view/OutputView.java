9549be8215ac1e4b96ea735b0d91dff3592b5c63
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 02:17:04 +0900

    feat: 출력문 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,13 +1,19 @@
 package racingcar.view;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import racingcar.controller.dto.RacingResultResponse;
 
 public class OutputView {
 
+    private static final int ZERO = 0;
+    private static final String COLON = " : ";
+    private static final String COMMA = ", ";
+    private static final String HYPHEN = "-";
     private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자";
 
     public void printCarNamesInputMessage() {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
@@ -22,17 +28,26 @@ public class OutputView {
     }
 
     public void printResult(final RacingResultResponse result) {
-        System.out.print(result.getCarName() + " : ");
+        System.out.print(result.getCarName() + COLON);
         printPositionResult(result.getCarPosition());
     }
 
     private void printPositionResult(final int count) {
-        IntStream.range(0, count)
-                .forEach(i -> System.out.print("-"));
+        IntStream.range(ZERO, count)
+                .forEach(each -> System.out.print(HYPHEN));
         printNewLine();
     }
 
     public void printNewLine() {
         System.out.println();
+    }
+
+    public void printWinners(final List<String> winnerNames) {
+        System.out.print(WINNER_MESSAGE + COLON);
+        printWinnerNames(winnerNames);
+    }
+
+    private void printWinnerNames(final List<String> winnerNames) {
+        System.out.println(String.join(COMMA, winnerNames));
     }
 }
