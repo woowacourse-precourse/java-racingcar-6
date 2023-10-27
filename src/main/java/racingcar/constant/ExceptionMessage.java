a1c54c012643a3d143b937500c63594195d32216
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 01:23:56 +0900

    fix(Validation) : roundsNumberZero() 메소드 추가 및 관련 코드 리팩토링 시도횟수가 0일 때의 유효성 검사 기능 추가, 예외 발생 시 메시지 추가

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -2,8 +2,9 @@ package racingcar.constant;
 
 public class ExceptionMessage {
     public static final String BLANK_INPUT = "빈 문자열은 입력할 수 없습니다.";
-    public static final String NAME_LENGTH_ZERO = "입력된 문자열에 길이가 0이 되는 자동차 이름이 있습니다: ";
-    public static final String OVER_NAME_LIMIT = "자동차 이름의 길이는 최대 5글자입니다: ";
-    public static final String NAME_DUPLICATED = "자동차 이름은 서로 중복되지 않아야 합니다: ";
-    public static final String NUMERIC = "문자가 아닌 숫자를 입력해야 합니다: ";
+    public static final String NULL_CAR_NAME = "입력된 문자열 안에 누락된 자동차 이름이 있습니다: ";
+    public static final String OVER_CAR_NAME_LIMIT = "자동차 이름의 길이는 최대 5글자입니다: ";
+    public static final String CAR_NAME_DUPLICATED = "자동차 이름은 서로 중복되지 않아야 합니다: ";
+    public static final String NUMERIC_ROUNDS = "문자가 아닌 숫자를 입력해야 합니다: ";
+    public static final String ZERO_ROUNDS = "시도 횟수는 0보다 커야 합니다.";
 }
--- src/main/java/racingcar/controller/Validation.java
+++ src/main/java/racingcar/controller/Validation.java
@@ -6,31 +6,32 @@ import static racingcar.constant.ExceptionMessage.*;
 import java.util.Map;
 
 public class Validation {
-    public void nameLengthZero(String carName) {
-        if (carName.contains(DELIMETER)) {
-            findCommaFirstOrLast(carName);
-            continuousComma(carName);
+    public void nameLengthZero(String carNameInput) {
+        if (carNameInput.contains(DELIMETER)) {
+            findCommaFirstOrLast(carNameInput);
+            continuousComma(carNameInput);
         }
     }
-    public void findCommaFirstOrLast(String carName) {
-        int lastIndex = carName.length()-1;
-        if (carName.charAt(0) == DELIMETER.charAt(0) || carName.charAt(lastIndex) == DELIMETER.charAt(0)) {
-            throw new IllegalArgumentException(NAME_LENGTH_ZERO + carName);
+
+    private void findCommaFirstOrLast(String carNameInput) {
+        int lastIndex = carNameInput.length()-1;
+        if (carNameInput.charAt(0) == DELIMETER.charAt(0) || carNameInput.charAt(lastIndex) == DELIMETER.charAt(0)) {
+            throw new IllegalArgumentException(NULL_CAR_NAME + carNameInput);
         }
     }
-    public void continuousComma(String carName) {
-        if (carName.contains(DELIMETER+DELIMETER)) {
-            throw new IllegalArgumentException(NAME_LENGTH_ZERO + carName);
+    private void continuousComma(String carNameInput) {
+        if (carNameInput.contains(DELIMETER+DELIMETER)) {
+            throw new IllegalArgumentException(NULL_CAR_NAME + carNameInput);
         }
     }
-    public void overNameLengthLimit(String carName) {
-        if (carName.length() > NAME_LENGTH_MAX) {
-            throw new IllegalArgumentException(OVER_NAME_LIMIT + carName);
+    public void overNameLengthLimit(String carNameToken) {
+        if (carNameToken.length() > NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException(OVER_CAR_NAME_LIMIT + carNameToken);
         }
     }
-    public void nameDuplicated(String carName, Map<String,Integer> carList) {
-        if (carList.containsKey(carName)) {
-            throw new IllegalArgumentException(NAME_DUPLICATED + carName);
+    public void nameDuplicated(String carNameToken, Map<String,Integer> carList) {
+        if (carList.containsKey(carNameToken)) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATED + carNameToken);
         }
     }
     public int roundsNumeric(String round) {
@@ -38,8 +39,14 @@ public class Validation {
         try {
             temp = Integer.parseInt(round);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NUMERIC + round);
+            throw new IllegalArgumentException(NUMERIC_ROUNDS + round);
         }
         return temp;
+    }
+
+    public void roundsNumberZero(int rounds) {
+        if (rounds == 0) {
+            throw new IllegalArgumentException(ZERO_ROUNDS);
+        }
     }
 }
