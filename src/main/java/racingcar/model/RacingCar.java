31d3a6f81f2ea064dbcae3982c2309f5c6328572
Infikei <hckhong@naver.com>
2023-10-27 21:19:05 +0900

    feat: RacingCar 클래스 생성
    - 차의 이름과 현재 위치를 저장하는 RacingCar 클래스 생성

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class RacingCar {
+    private String name;
+    private int position;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
