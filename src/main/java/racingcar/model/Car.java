f61985a89908c64135e1f8fbab5c637f7840120a
M1NS1K <rlaalstlr2001@gmail.com>
2023-10-27 14:00:25 +0900

    제약조건 설정

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+
+public class Car {
+    public static final int MOVE_CONDITION = 4;
+    public static final int CAR_NAME_LIMIT_SIZE = 5;
+
+    public static ArrayList<String> cars = new ArrayList<>();
+}
---
+++ src/main/java/racingcar/model/ExceptionHandling.java
@@ -0,0 +1,6 @@
+package racingcar.model;
+
+public class ExceptionHandling {
+    public static String INPUT_WRONG_EXCEPTION = "잘못된 입력";
+    public static String CAR_NAME_EXCEPTION = "너무 긴 이름";
+}
