fec5b055f3c0d194606f951dbd228e420eb7e114
anneu <b.imnuj@gmail.com>
2023-10-27 12:05:06 +0900

    feat: 자동차 이름에 특수문자나 숫자가 없는지 검증하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,12 +4,14 @@ public class Car {
 
     private static final int MAX_LENGTH = 5;
     private static final String NAME_LENGTH_EXCEPTION = "자동차 이름은 %d자 이하만 가능합니다.";
+    private static final String NAME_LETTER_EXCEPTION = "자동차 이름에 특수문자나 숫자는 포함될 수 없습니다.";
 
     private final String name;
     private int moveCount;
 
     public Car(String name) {
         validateLength(name);
+        validateLetter(name);
         this.name = name;
         this.moveCount = 0;
     }
@@ -17,6 +19,14 @@ public class Car {
     private void validateLength(String name) {
         if (MAX_LENGTH < name.length()) {
             throw new IllegalArgumentException(String.format(NAME_LENGTH_EXCEPTION, MAX_LENGTH));
+        }
+    }
+
+    private void validateLetter(String name) {
+        for (char c : name.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                throw new IllegalArgumentException(NAME_LETTER_EXCEPTION);
+            }
         }
     }
 
