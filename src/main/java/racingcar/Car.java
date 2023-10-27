fa00a05a6b4cbc23071377d968ac75d2d685040a
InGyu Moon <ansdlsrb1121@naver.com>
2023-10-27 22:21:03 +0900

    feat: 기능3 Car 객체 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    String name;
+    int step;
+    public Car(String name){
+        this.name = name;
+        step = 0;
+    }
+    public void tryOneStep(){
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if(randomNumber>=4){
+            step++;
+        }
+    }
+
+    public int getStep() {
+        return step;
+    }
+}
