4136760349be72fed9ba8987b4445e471e9dd7dc
shin5774 <babo5774@naver.com>
2023-10-27 17:56:18 +0900

    test: 자동차의 현재 진행상태 출력 기능 테스트코드
    - 자동차 현재 진행사태를 콘솔에 출력하는 기능의 테스트코드

--- src/test/java/racingcar/ViewTest.java
+++ src/test/java/racingcar/ViewTest.java
@@ -3,7 +3,9 @@ package racingcar;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +20,24 @@ public class ViewTest {
         View view=new View();
 
         assertThat(view.inputConsole()).isEqualTo(names);
+    }
+
+    @DisplayName("자동차의 현재 상태에 대해 출력하는 기능")
+    @Test
+    void printCurrentCarResult_자동차_상태출력(){
+        Car car=new Car(new Name("ski"),new Position(3));
+        View view=new View();
+
+        String expected="ski : ---";
+        PrintStream standardOut= System.out;
+        ByteArrayOutputStream captor=new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        view.printCurrentCarResult(car);
+
+        System.setOut(standardOut);
+        String result=captor.toString().trim();
+
+        assertThat(result).isEqualTo(expected);
     }
 }
