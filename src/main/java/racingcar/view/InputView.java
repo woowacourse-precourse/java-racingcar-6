b65602ac323813bba1266af3ab899bfe901ffbd4
kkkkimtaehyeon <kth1927@naver.com>
2023-10-27 20:53:05 +0900

    [view] [InputView] InputTryNumber() 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,8 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+    public String InputTryNumber(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
 }
