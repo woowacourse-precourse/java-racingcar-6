0094e39685f1154282f22ec110021c7b27230581
howudong <tjdvy963@naver.com>
2023-10-27 13:03:47 +0900

    test : 랜덤 난수를 생성해서 4이상때만 1을 반환하도록 하는 테스트코드 작성

--- src/test/java/racingcar/domain/MoveRuleByMinimumScoreTest.java
+++ src/test/java/racingcar/domain/MoveRuleByMinimumScoreTest.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
@@ -8,42 +7,57 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MoveRuleByMinimumScoreTest {
+    private NumberGenerator getTargetNumber(int number) {
+        return new RandomNumberGenerator() {
+            @Override
+            public int generate() {
+                return number;
+            }
+        };
+    }
+
     @Test
     @DisplayName("숫자가 4면 1을 반환한다.")
     void 숫자4_1_반환() {
         //given
-        MoveRule moveRule = new MoveRuleByMinimumScore();
+        NumberGenerator numberGenerator = getTargetNumber(4);
+        MoveRule moveRule = new MoveRuleByMinimumScore(numberGenerator);
         //when
+        int distance = moveRule.tryMove();
         //then
-        assertThat(moveRule.tryMove(4)).isEqualTo(1);
+        assertThat(distance).isEqualTo(1);
     }
 
     @Test
     @DisplayName("숫자가 5면 1을 반환한다.")
     void 숫자5_1_반환() {
         //given
-        MoveRule moveRule = new MoveRuleByMinimumScore();
+        NumberGenerator numberGenerator = getTargetNumber(5);
+        MoveRule moveRule = new MoveRuleByMinimumScore(numberGenerator);
         //when
+        int distance = moveRule.tryMove();
         //then
-        assertThat(moveRule.tryMove(5)).isEqualTo(1);
+        assertThat(distance).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("숫자가 4면 1을 반환한다.")
+    @DisplayName("숫자가 3면 0을 반환한다.")
     void 숫자3_0_반환() {
         //given
-        MoveRule moveRule = new MoveRuleByMinimumScore();
+        NumberGenerator numberGenerator = getTargetNumber(3);
+        MoveRule moveRule = new MoveRuleByMinimumScore(numberGenerator);
         //when
+        int distance = moveRule.tryMove();
         //then
-        assertThat(moveRule.tryMove(3)).isZero();
+        assertThat(distance).isZero();
     }
 
     @RepeatedTest(value = 20)
     @DisplayName("0 ~ 9 사이의 숫자가 나오면 성공한다.")
     void RandomNumberGeneratorTest() {
         //given
-        MoveRule numberGenerator = new MoveRuleByMinimumScore();
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
         //when,then
-        Assertions.assertThat(numberGenerator.generate()).isBetween(0, 9);
+        assertThat(numberGenerator.generate()).isBetween(0, 9);
     }
 }
