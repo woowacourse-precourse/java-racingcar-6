0827bb02a9767452bc5d22af767740431bd143f6
cheolhyeon <tghy4@naver.com>
2023-10-27 21:48:23 +0900

    refactor : RacingCarList의 메서드명 수정

--- src/main/java/racingcar/RacingCarList.java
+++ src/main/java/racingcar/RacingCarList.java
@@ -7,15 +7,29 @@ public class RacingCarList {
     private List<RacingCarCreator> racingCarsList = new ArrayList<>();
     private final User user;
     private final RacingCarCreator racingCarCreator;
+
     public RacingCarList(User user, RacingCarCreator racingCarCreator) {
         this.user = user;
         this.racingCarCreator = racingCarCreator;
     }
+
     public List<RacingCarCreator> getRacingCarList() {
-        String[] carNames = user.inputCarNames();
+        enrollNewRacingCar();
+        return racingCarsList;
+    }
+
+    private void enrollNewRacingCar() {
+        createNewRacingCar();
+    }
+
+    private void createNewRacingCar() {
+        String[] carNames = getCarNames();
         for (String carName : carNames) {
             racingCarsList.add(racingCarCreator.createNewCar(carName));
         }
-        return racingCarsList;
+    }
+
+    private String[] getCarNames() {
+        return user.inputCarNames();
     }
 }
