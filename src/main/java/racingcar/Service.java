0b20411a036ba667150762213b059159c1fc1cf7
chansoo <chansoo38@gmail.com>
2023-10-27 17:41:46 +0900

    <Feat> Service - 경주에 참가할 자동차를 입력받는 기능을 구현한다.

---
+++ src/main/java/racingcar/Service.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Service {
+    private final ProgressBoard progressBoard;
+    private final InputView inputView ;
+    private final OutputView outputView;
+
+    public Service() {
+        this.progressBoard = ProgressBoard.getInstance();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public String receiveParticipants() {
+        inputView.printCarNameMessage();
+        String input = Console.readLine();
+        return input;
+    }
+
+}
