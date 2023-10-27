455da60746f48e0e279ee703167af9489b6cd99c
junOTL <naqp99@gmail.com>
2023-10-27 18:49:33 +0900

    refactor: 입력시 검증 하도록 코드 위치 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,15 +6,18 @@ import racingcar.util.Validator;
 public class InputView {
     public static String[] inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return splitByComma(Console.readLine());
+        String[] inputs = splitByComma(Console.readLine());
+        return Validator.isValidCarName(inputs);
     }
 
     public static int inputAttemptCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
+        String input = Validator.isValidAttemptCount(Console.readLine());
+        return Integer.parseInt(input);
     }
 
     public static String[] splitByComma(String input) {
-        return Validator.isValidName(input.split(","));
+        Validator.isValidInput(input);
+        return input.split(",");
     }
 }
