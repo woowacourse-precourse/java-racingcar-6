9b0eba8617cdb01838e1addc4b859b637bbfd67d
mksystem98 <mksystem98@gmail.com>
2023-10-27 16:33:38 +0900

    refactor: InputView 메서드 기능 분리
    1.사용자로부터 입력을 받는 기능
    2.입력된 문자열을 쉼표(,)를 기준으로 나누고 공백을 제거하는 기능으로 분리.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,10 +11,12 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public List<String> inputCarNames() {
-        String userInput = Console.readLine();
+    public String getUserCarNames() {
+        return Console.readLine();
+    }
+
+    public List<String> parseCarNames(String userInput) {
         return Arrays.stream(userInput.replace(" ", "").split(","))
                 .collect(Collectors.toList());
-
     }
 }
