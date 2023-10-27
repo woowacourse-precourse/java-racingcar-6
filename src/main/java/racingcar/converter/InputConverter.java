414299cb95acf7a6cbfb2ec5cb4264578e73fbe0
minisyu <iswater.kim@gmail.com>
2023-10-27 22:06:48 +0900

    refactor(InputConverter): InputValidator 제거

--- src/main/java/racingcar/converter/InputConverter.java
+++ src/main/java/racingcar/converter/InputConverter.java
@@ -1,23 +1,13 @@
 package racingcar.converter;
 
 import racingcar.domain.game.Cars;
-import racingcar.util.InputValidator;
 
 public class InputConverter {
-    private final InputValidator inputValidator;
-
-    public InputConverter(InputValidator inputValidator) {
-        this.inputValidator = inputValidator;
-    }
-
     public Cars convertStringToCars(String input) {
-        inputValidator.validatePlayerNamesFormat(input);
-        inputValidator.validateNonEmpty(input);
         return Cars.of(input);
     }
 
     public int covertStringToInteger(String input) {
-        inputValidator.validateNumericString(input);
         return Integer.parseInt(input);
     }
 }
