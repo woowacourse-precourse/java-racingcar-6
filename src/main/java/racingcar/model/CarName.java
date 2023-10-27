31a7e54ca600ce7d4f12e47edf61f93f8b95ac6a
kkkkimtaehyeon <kth1927@naver.com>
2023-10-27 20:02:56 +0900

    [model] ㄴ[CarName]
    [InputCarNames]
    자동차 이름 입력 받음

---
+++ src/main/java/racingcar/model/CarName.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CarName {
+    static String carNames;
+
+    public void InputCarNames(){
+        carNames = Console.readLine();
+    }
+
+}
