e1db16a5b641c9cc207a3192a2fe60cab6d3bbc8
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 18:12:03 +0900

    feat: 시도할 횟수 입력 상수 메시지 추가

--- src/main/java/racingcar/constant/Message.java
+++ src/main/java/racingcar/constant/Message.java
@@ -5,5 +5,6 @@ public final class Message {
     private Message() {}
 
     public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
     public static final String WINNER_MESSAGE= "최종 우승자 : ";
 }
