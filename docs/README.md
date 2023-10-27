a45134536c8ad540d3b681efb5cc970b2df84f07
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-27 23:48:50 +0900

    feat/racingCarGame: init()함수 작성

--- docs/README.md
+++ docs/README.md
@@ -13,12 +13,13 @@ RacingCarGame을 실행시킴
 - plusDistance() : 이동량 증가
 
 ### RacingCarGame
-초기값을 입력받고 자동차를 만들고 게임 시작(필드로는 자동차 list,랜덤수생성기)
+초기값을 입력받고 자동차를 만들고 게임 시작(필드로는 자동차 list,랜덤수생성기,실행횟수)
 
 - playGame() : 게임시작
   - init() : 초기 자동차이름과 시도횟수 입력
-    - inputCarNamesAndNumOfTimes() : 자동차이름과횟수를 입력받기
-      - checkInputValid() : 입력한 것에 오류가 없는지 검증(5자리 이상의 이름이 있는지)
+    - 초기 자동차이름과 시도횟수 입력
+    - checkInputValid() : 입력한 것에 오류가 없는지 검증(5자리 이상의 이름이 있는지)
+    - setNumOfPlaying() : 실행횟수 setter함수 실행
     - generateRacingCars() : 입력받은 값을 가지고 RacingCar 생성후 자동차 list에 넣기
       - StringToListOfString() : 입력받은 문자열을 가지고 ,로 구분하여 String을 list<String>로 변환
       - generateRacingCar() : List에서 하나의 이름을 꺼내 RacingCar 객체를 생성(이동량의 default:0)
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,14 +1,46 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarGame {
+
     private List<RacingCar> racingCars;
     private RandomNumGenerator randomNumGenerator;
+    private int numOfPlaying;
 
     public RacingCarGame(){
-        racingCars = new ArrayList<>();
-        randomNumGenerator = new RandomNumGenerator();
+        this.racingCars = new ArrayList<>();
+        this.randomNumGenerator = new RandomNumGenerator();
+        this.numOfPlaying = 0;
+    }
+
+    private void init(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNum = Console.readLine();
+
+        checkInputValid(carNames,inputNum);
+
+        setNumOfPlaying(Integer.parseInt(inputNum));
+        generateRacingCar(carNames);
+    }
+
+    private void checkInputValid(String carNames,String inputNum){
+        //TODO: carNames 5자리 이하인지 확인
+
+        //TODO: inputNum이 숫자로 변환할수 있는지 확인
+    }
+
+    private void generateRacingCar(String carNames){
+        //TODO: StringToListOfString() - carNames 문자열에서 List<String>으로 변환
+
+        //TODO: List<String>에서 1개씩 꺼내며 RacingCar 객체 생성 후 List<RacingCar>에 넣기
+    }
+    private void setNumOfPlaying(int numOfPlaying) {
+        this.numOfPlaying = numOfPlaying;
     }
 }
