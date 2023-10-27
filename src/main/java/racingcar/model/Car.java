0cd103001b24e4569aa7dd74b469621b7d7f0766
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 19:13:16 +0900

    Feat: Car객체 생성자 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,4 +3,9 @@ package racingcar.model;
 public class Car {
     String name;
     String score;
+
+    public Car(String name, String score){
+        this.name = name;
+        this.score = score;
+    }
 }
