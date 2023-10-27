cffe90c814f403d732b716cc9a77f731c0dd2217
cheolhyeon <tghy4@naver.com>
2023-10-27 22:13:40 +0900

    feat : Validator 클래스의 자동차 이름을 5글자 이상인지 이하인지 검증하는 기능 구현

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -11,4 +11,16 @@ public class Validator {
             throw new IllegalArgumentException("??? ??? ???? ?? 2? ?????.");
         }
     }
+    public void checkCarNameLength(User user) {
+        String[] carNames = user.inputCarNames();
+        for (String carName : carNames) {
+            carNameMaxLength(carName);
+        }
+    }
+
+    private static void carNameMaxLength(String carName) {
+        if (carName.trim().length() > 5) {
+            throw new IllegalArgumentException("??? ??? 5?? ???? ???.");
+        }
+    }
 }
