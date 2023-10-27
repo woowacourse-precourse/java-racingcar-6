c0ff17625828340a11505eb1d5e68506c56aca1d
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-10-27 22:29:42 +0900

    refactor: 테스트 코드를 위한 return 추가

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -3,30 +3,32 @@ package racingcar.view;
 import java.util.ArrayList;
 
 public class ConsoleOutputView {
-    public void showInputCarNamesPrompt() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    public String showInputCarNamesPrompt() {
+        return "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     }
 
-    public void showInputTryNumberPrompt() {
-        System.out.println("시도할 회수는 몇회인가요?");
+    public String showInputTryNumberPrompt() {
+        return "시도할 회수는 몇회인가요?";
     }
 
-    public void showExecutionResultsHeader() {
-        System.out.println("실행 결과");
+    public String showExecutionResultsHeader() {
+        return "실행 결과";
     }
 
-    public void showExecutionResults(ArrayList<String> carNames, ArrayList<Integer> carGoingCount) {
+    public String showExecutionResults(ArrayList<String> carNames, ArrayList<Integer> carGoingCount) {
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < carNames.size(); i++) {
-            System.out.print(carNames.get(i) + " : ");
+            result.append(carNames.get(i)).append(" : ");
             for (int j = 0; j < carGoingCount.get(i); j++) {
-                System.out.print("-");
+                result.append("-");
             }
-            System.out.println();
+            result.append("\n");
         }
-        System.out.println();
+        result.append("\n");
+        return result.toString();
     }
 
-    public void showWinner(String winners) {
-        System.out.println("최종 우승자 : " + winners);
+    public String showWinner(String winners) {
+        return "최종 우승자 : " + winners;
     }
 }
--- src/test/java/racingcar/view/ConsoleOutputViewTest.java
+++ src/test/java/racingcar/view/ConsoleOutputViewTest.java
@@ -1,11 +1,15 @@
 package racingcar.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class ConsoleOutputViewTest {
-
     @Test
     void showInputCarNamesPrompt() {
+        ConsoleOutputView consoleOutputView = new ConsoleOutputView();
+        String output = consoleOutputView.showInputCarNamesPrompt();
+        assertEquals(output, "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     @Test
