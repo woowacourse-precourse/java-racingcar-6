149f910b1ce54d38afac945326136e793c9d6756
SeongUk52 <rkfka7849@gmail.com>
2023-10-27 19:36:47 +0900

    feat(InputVerifier): 길이 6자 이상의 문자열 입력에 대한 예외처리 구현

--- src/main/java/racingcar/verifier/InpurVerifier.java
+++ src/main/java/racingcar/verifier/InpurVerifier.java
@@ -1,11 +1,18 @@
 package racingcar.verifier;
 
-public class InpurVerifier {
-    public static void verifyName(String nameList) throws IllegalArgumentException {
+import java.util.List;
+import racingcar.system.ExceptionMessage;
 
+public class InpurVerifier {
+    public static void verifyName(String names) throws IllegalArgumentException {
+        List<String> nameList = List.of(names.split(","));
+        nameList
+                .forEach(InpurVerifier::checkLength);
     }
 
     private static void checkLength(String name) {
-        
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE.getMessage());
+        }
     }
 }
