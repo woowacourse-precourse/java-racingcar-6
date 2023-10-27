1d89c503a5fe83dedb4bdbc11e0dfffee88c0c8c
김승진 <ohksj77@gmail.com>
2023-10-27 22:27:33 +0900

    feat(RacingCarController): 자동차 이름 입력 기능 연결

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,16 +1,21 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingCars;
+import racingcar.io.InputManager;
 import racingcar.io.OutputView;
 
 public class RacingCarController {
 
     private final OutputView outputView;
+    private final InputManager inputManager;
 
-    public RacingCarController(final OutputView outputView) {
+    public RacingCarController(final OutputView outputView, final InputManager inputManager) {
         this.outputView = outputView;
+        this.inputManager = inputManager;
     }
 
     public void run() {
         outputView.printRacingCarNamesRequest();
+        final RacingCars racingCars = inputManager.readRacingCarNames();
     }
 }
--- src/main/java/racingcar/factory/ComponentFactory.java
+++ src/main/java/racingcar/factory/ComponentFactory.java
@@ -1,12 +1,32 @@
 package racingcar.factory;
 
 import racingcar.controller.RacingCarController;
+import racingcar.io.InputManager;
+import racingcar.io.InputMapper;
+import racingcar.io.InputValidator;
+import racingcar.io.InputView;
 import racingcar.io.OutputView;
 
 public class ComponentFactory {
 
     public RacingCarController racingCarController() {
-        return new RacingCarController(outputView());
+        return new RacingCarController(outputView(), inputManager());
+    }
+
+    private InputManager inputManager() {
+        return new InputManager(inputView(), inputMapper());
+    }
+
+    private InputMapper inputMapper() {
+        return new InputMapper();
+    }
+
+    private InputView inputView() {
+        return new InputView(inputValidator());
+    }
+
+    private InputValidator inputValidator() {
+        return new InputValidator();
     }
 
     private OutputView outputView() {
