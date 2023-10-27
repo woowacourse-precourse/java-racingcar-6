d47e894eff134181c454d46743fa34f0a322ee2b
zzawang <hobin0116@naver.com>
2023-10-27 18:04:25 +0900

    feat: 자동차 이름 중복 검사 추가
    - 자동차들의 이름 중 중복된 이름이 있는 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료된다.

---
+++ src/main/java/racingcar/UserInputException.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserInputException {
+    public void validateUniqueCarName(String[] carNames) {
+        // 자동차 이름 중복 검사
+        if (duplicateCarName(carNames) > 0) {
+            throw new IllegalArgumentException("자동차들의 이름 중 중복된 이름이 존재합니다. 게임이 종료됩니다.");
+        }
+    }
+
+    private long duplicateCarName(String[] carNames) {
+        Set<String> uniqueCarNames = new HashSet<>();
+
+        // 중복되는 자동차 이름의 개수를 센다.
+        return Arrays.stream(carNames)
+                .filter(carName -> !uniqueCarNames.add(carName))
+                .count();
+    }
+}
