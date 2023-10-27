322f1bb1b44b2f447111f44d81efc01f477177d1
gumgu <bienlee@naver.com>
2023-10-27 21:40:47 +0900

    Feat : 입력값(이름들) 구분자(",") 기준으로 구분하여 변환하는 기능 구현 (String -> List<String>)

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -7,6 +7,12 @@ import java.util.List;
 
 public class InputView {
 
+    public static final String SPLITTER = ",";
+
+    private List<String> splitNames(String input) {
+        //TODO String[], List<String>중 어느것이 더 적절할까?
+        return Arrays.asList(input.split(SPLITTER));
+    }
     private String getInput() {
         return Console.readLine();
     }
