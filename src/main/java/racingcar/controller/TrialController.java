7eeaadb591f5f7143c7800165ef5e9ecb2c96893
isyoo1213 <moethns@gmail.com>
2023-10-27 21:56:02 +0900

    feat: TrialController 경주시도 후 위치변경 기능추가 TrialController - applyTiralResult() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/racingcar/controller/TrialController.java
+++ src/main/java/racingcar/controller/TrialController.java
@@ -1,11 +1,16 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Cars;
 
 public class TrialController {
     private static final int RANDOM_GENERATE_START_NUMBER = 0;
     private static final int RANDOM_GENERATE_END_NUMBER = 9;
     private static final int GO_OR_STOP_NUMBER = 4;
+
+    public void applyTrialResult(Cars cars) {
+        cars.updateCarsPosition(calculateMovement());
+    }
 
     public boolean calculateMovement() {
         return generateRandomNumber() >= GO_OR_STOP_NUMBER;
