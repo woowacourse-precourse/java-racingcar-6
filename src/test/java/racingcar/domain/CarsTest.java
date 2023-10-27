955bc93d65dc0c4d00756a295167ebfbb33a7b0f
Arachneee <jhg2819@naver.com>
2023-10-27 13:40:44 +0900

    test: Cars생성 방식 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -15,10 +15,10 @@ class CarsTest {
     @DisplayName("자동차들 정상 생성")
     void Cars_정상_생성() {
         // given
-        String names = "a,bb,ccc,dddd";
+        List<String> nameList = List.of("a", "bb", "ccc", "dddd");
 
         // when
-        Cars cars = Cars.from(names);
+        Cars cars = Cars.from(nameList);
 
         // then
         assertThat(cars.getCars().size()).isEqualTo(4);
@@ -28,10 +28,10 @@ class CarsTest {
     @DisplayName("자동차들 이름에 5글자 초과가 들어올 수 없다.")
     void Cars_비정상_생성() {
         // given
-        String names = "a,bb,ccc,dddddd";
+        List<String> nameList = List.of("a", "bb", "ccc", "dddddd");
 
         // when then
-        assertThatThrownBy(() -> Cars.from(names))
+        assertThatThrownBy(() -> Cars.from(nameList))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -39,7 +39,7 @@ class CarsTest {
     @DisplayName("자동차들의 이름이 공백일 수 없다.")
     void Cars_비정상_생성_이름없음() {
         // given
-        String nameZero = ", ";
+        List<String> nameZero = List.of("", " ");
 
         // when then
         assertThatThrownBy(() -> Cars.from(nameZero))
@@ -51,7 +51,8 @@ class CarsTest {
     @DisplayName("최대 위치 추출")
     void 최대_위치_추출() {
         // given
-        Cars cars = Cars.from("a,b,c,d,e");
+        List<String> nameList = List.of("a", "b", "c", "d", "e");
+        Cars cars = Cars.from(nameList);
         int n = 10;
 
         // when
@@ -81,7 +82,8 @@ class CarsTest {
     @DisplayName("우승자 추출")
     void 우승자_이름_추출() {
         // given
-        Cars cars = Cars.from("a,b,c,d,e");
+        List<String> nameList = List.of("a", "b", "c", "d", "e");
+        Cars cars = Cars.from(nameList);
         int n = 10;
 
         // when
@@ -102,7 +104,7 @@ class CarsTest {
     @DisplayName("공동 우승자도 추출된다.")
     void 공동_우승자_추출() {
         // given
-        Cars cars = Cars.from("a,b,c,d,e");
+        Cars cars = Cars.from(List.of("a", "b", "c", "d", "e"));
         int n = 10;
 
         // when
@@ -129,7 +131,7 @@ class CarsTest {
     @DisplayName("자동차들 객체를 외부 변경으로부터 보호해야한다.")
     void 외부_변경_빈리스트_보호() {
         // given
-        Cars beforeCars = Cars.from("a,b,c");
+        Cars beforeCars = Cars.from(List.of("a", "b", "c"));
 
         // when
         List<Car> carList = beforeCars.getCars();
@@ -144,7 +146,7 @@ class CarsTest {
     @DisplayName("자동차들 객체 내부의 자동차 객체도 보호해야한다.")
     void 외부_변경_리스트_내부_변경_보호() {
         // given
-        Cars beforeCars = Cars.from("a,b,c");
+        Cars beforeCars = Cars.from(List.of("a", "b", "c"));
 
         // when
         List<Car> carList = beforeCars.getCars();
