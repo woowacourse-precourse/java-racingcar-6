e310cb7cbbb1f3e596d422099f4c322b9c9211a4
SongJwans <yhh5916@naver.com>
2023-10-28 00:27:33 +0900

    feat: 여러 개의 Car 정보를 담을 CarList를 구현, ',' 을 기준으로 저장할 수 있도록 splitName() 구현

---
+++ src/main/java/racingcar/domain/CarList.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CarList {
+    private final List<Car> carList = new ArrayList<>();
+
+    public void splitCarNames(String carNames) {
+        String[] carNameArray = splitName(carNames);
+        addCars(carNameArray);
+    }
+
+    private void addCars(String[] carNameArray) {
+        Arrays.stream(carNameArray)
+                .map(Car::new)
+                .forEach(carList::add);
+    }
+
+    public String[] splitName(String carNames) {
+        return carNames.split(",");
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public Winners getWinners() {
+        return new Winners(carList);
+    }
+
+}
