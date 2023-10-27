4dc1eb9949a397a2145f67832d449c4de62b8f19
Juhyorim <joohr1234@gmail.com>
2023-10-27 20:44:07 +0900

    feat(racingcar): 입력받은 자동차 개수가 1개 이하일 때 예외처리 추가

--- src/main/java/racingcar/domain/InputValidator.java
+++ src/main/java/racingcar/domain/InputValidator.java
@@ -8,6 +8,11 @@ import java.util.Set;
 public class InputValidator {
     public boolean validateCarNames(String cars) throws IllegalArgumentException {
         List<String> carNameList = splitByDelimiter(cars, ",");
+
+        if (carNameList.size() <= 1) {
+            throw new IllegalArgumentException("자동차 이름은 2개 이상 입력되어야 합니다");
+        }
+
         for (String carName : carNameList) {
             validateCarName(carName);
         }
@@ -31,7 +36,7 @@ public class InputValidator {
     private static List<String> splitByDelimiter(String cars, String delimiter) {
         List<String> nameList = new ArrayList<>(List.of(cars.split(delimiter)));
         //split 함수의 경우 "효림,현진,"일 때 마지막 공백을 무시하므로 직접 공백 하나를 추가함
-        if (cars.charAt(cars.length() - 1) == ',') {
+        if (cars.length() > 0 && cars.charAt(cars.length() - 1) == ',') {
             nameList.add("");
         }
 
