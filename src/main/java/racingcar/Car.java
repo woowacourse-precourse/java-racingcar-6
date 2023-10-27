8711cf31e60fe27b1ee1c4a0fb04ab260d314807
zangsu <zangsu_@naver.com>
2023-10-27 16:49:12 +0900

    feat : implement Car.tryMove()

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    public static final int MIN_MOVABLE_NUMBER = 4;
+    public static final int START_RANGE = 0;
+    public static final int END_RANGE = 9;
+
     String name;
     int location;
 
@@ -9,7 +15,18 @@ public class Car {
         this.location = 0;
     }
 
-    //TODO : boolean tryMove()
+    public boolean tryMove(){
+        int randomNum = Randoms.pickNumberInRange(START_RANGE, END_RANGE);
+        if(canGo(randomNum)){
+            this.location++;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canGo(int randomNum) {
+        return randomNum >= MIN_MOVABLE_NUMBER;
+    }
 
     //TODO : @Override String toString()
 }
