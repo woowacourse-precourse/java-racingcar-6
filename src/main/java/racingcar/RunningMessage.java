6131403662d79b18de4e8579abbbc30a7072b45c
park_yeongyu <sjjs7771@naver.com>
2023-10-27 17:36:57 +0900

    feat : RunningMessage 클래스에 최종 우승자 출력 함수 작성

--- src/main/java/racingcar/RunningMessage.java
+++ src/main/java/racingcar/RunningMessage.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class RunningMessage {
     public void printStartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -24,4 +26,16 @@ public class RunningMessage {
         }
         System.out.println();
     }
+
+    public void printWinner(List<String> winner) {
+        System.out.print("최종 우승자 : ");
+        for (int index = 0; index < winner.size(); index++) {
+            if (index + 1 != winner.size()) {
+                System.out.print(winner.get(index) + ", ");
+            } else {
+                System.out.println(winner.get(index));
+            }
+        }
+    }
+
 }
