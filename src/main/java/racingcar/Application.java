4c61286ff8e2d58da740072736c7da7710b26450
sejung2 <twindrop@naver.com>
2023-10-27 18:02:48 +0900

    0부터 9사이의 무작위 숫자를 생성하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.domain.GenerateRandomNum;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GenerateRandomNum randomNum = new GenerateRandomNum();
+        System.out.println(randomNum.createRandomNum());
+
     }
 }
