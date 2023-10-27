ea68b144731199ce1ade4cad50f80806ccd4040c
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:18:32 +0900

    [feat] Cars 클래스의 필드 정의

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,22 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> names, MovingStrategy movingStrategy) {
+        this.cars = createCars(names, movingStrategy);
+    }
+
+    private List<Car> createCars(List<String> names, MovingStrategy movingStrategy) {
+        return names.stream()
+                .map(name -> new Car(movingStrategy, name, 0))
+                .collect(Collectors.toList());
+    }
+
+
 }
