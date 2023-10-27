20f905c192399e9e886d66d1e796794a712e2bc5
HYUNJUN\sajun <sajun28@naver.com>
2023-10-27 12:57:29 +0900

    Feat: "Application GameController 객체 생성"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.start();
+//        InputView inputView = new InputView();
+//        String[] a = inputView.name().split(",");
+//        System.out.println(inputView.attemptsNum());
+//        System.out.println(Arrays.toString(a));
     }
 }
