d945a48e29797a11f13992cecd906c34e2410504
HeungY <rkrkrkrk02@naver.com>
2023-10-27 15:35:28 +0900

    feat: InputRacingCar에 멘트 출력 추가

--- src/main/java/racingcar/InputRacingCar.java
+++ src/main/java/racingcar/InputRacingCar.java
@@ -10,6 +10,9 @@ public class InputRacingCar {
 
     List<String> inputRacingCar(){
         List<String> inputCar = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String input = Console.readLine();
         StringTokenizer inputToken = new StringTokenizer(input,",");
 
