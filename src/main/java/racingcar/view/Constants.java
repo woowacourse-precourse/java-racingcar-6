5552ba5bdce1ada1e4627898f9ce981625c5b955
U-keun Song <sukeun319@gmail.com>
2023-10-28 00:04:35 +0900

    Feat : OutputView 클래스 메서드 작성
    StringBuilder에 결과를 기록하는 메서드와 출력하는 메서드를 작성했다. 출력 메시지에서 상수로 사용할 수 있는 것들을 지정해두었다.

--- src/main/java/racingcar/view/Constants.java
+++ src/main/java/racingcar/view/Constants.java
@@ -5,9 +5,20 @@ public enum Constants {
     입력 안내 메시지
      */
     INPUT_CARS_NAME_MESSAGE("경주할 자동차 이름을 입력하세.(이름은 쉼표(,) 기준으로 구분)"),
-    TRY_COUNT_MESSAGE("시도할 횟수는 몇회인가요?");
-    private static String message;
+    TRY_COUNT_MESSAGE("시도할 횟수는 몇회인가요?"),
+    /*
+    출력 안내 메시지
+     */
+    RESULT_MESSAGE_START("\n실행결과\n"),
+    WINNER_IS("최종 우승자 : "),
+    NEXT_LINE("\n");
+    private final String message;
 
     Constants(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,31 @@
 package racingcar.view;
 
+import static racingcar.view.Constants.NEXT_LINE;
+import static racingcar.view.Constants.RESULT_MESSAGE_START;
+import static racingcar.view.Constants.WINNER_IS;
+
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView implements View {
+    private final StringBuilder result;
+
+    OutputView() {
+        result = new StringBuilder();
+        result.append(RESULT_MESSAGE_START.getMessage());
+    }
+
+    public void recordStatus(final List<Car> cars) {
+        cars.forEach(car -> result.append(car.toString()).append(NEXT_LINE.getMessage()));
+        result.append(NEXT_LINE.getMessage());
+    }
+
+    public void recordWinner(final List<String> winners) {
+        result.append(WINNER_IS.getMessage());
+        result.append(winners.toString(), 1, winners.toString().length() - 1);
+    }
+
+    public void printResult() {
+        System.out.println(result);
+    }
 }
