b77141d934a453e7346334e239b9035ebb5597d5
irerin07 <counterchord89@gmail.com>
2023-10-27 13:50:42 +0900

    #1 자동차 일급 컬렉션 생성 - 자동차용 일급 컬렉션 생성 - 자동차 이름 검증 메서드 생성

---
+++ src/main/java/racingcar/racingcar/RacingCar.java
@@ -0,0 +1,30 @@
+package racingcar.racingcar;
+
+import java.util.List;
+
+/**
+ * @author 민경수
+ * @description racing car
+ * @since 2023.10.27
+ **********************************************************************************************************************/
+public class RacingCar {
+
+    private final List<String> cars;
+
+    public RacingCar(List<String> cars) {
+        validateNameLength(cars);
+        this.cars = cars;
+    }
+
+    /**
+     * 이름은 5글자 이하로만 설정할 수 있다.
+     */
+    private void validateNameLength(List<String> cars) {
+        for (String car : cars) {
+            if (car.length() <= 5) {
+                throw new IllegalArgumentException("자동차 이름은 공백 포함 5글자를 초과할 수 없습니다.");
+            }
+        }
+    }
+    
+}
