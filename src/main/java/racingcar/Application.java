7ace19e11127841bbc7e9fba6ec476e0f0a6bea9
YongJOoOo <we072200@naver.com>
2023-10-27 11:13:02 +0900

    feat: 사용자 입력 처리 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("TEST");
-
+        inputUser();
     }
+
+    //1) 사용자 입력처리 기능
+    static void inputUser() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int gameCount = Integer.parseInt(Console.readLine());
+    }
+
 }
