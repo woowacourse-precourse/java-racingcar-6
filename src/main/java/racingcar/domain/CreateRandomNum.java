5e488d5499eeb0cc7bcf290cc193e286b09b8910
박승찬 <seungchan141414@gmail.com>
2023-10-28 06:17:55 +0900

    [Style(racing)] 주석 수정

--- src/main/java/racingcar/domain/CreateRandomNum.java
+++ src/main/java/racingcar/domain/CreateRandomNum.java
@@ -4,6 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class CreateRandomNum {
 
+    /**
+     * 0에서 9까지의 정수 중 한 개의 정수 반환.
+     *
+     * @return
+     */
     public Integer createRanOneNum() {
         // 0에서 9까지의 정수 중 한 개의 정수 반환.
         return Randoms.pickNumberInRange(0, 9);
--- src/main/java/racingcar/domain/RaceCar.java
+++ src/main/java/racingcar/domain/RaceCar.java
@@ -1,27 +1,29 @@
 package racingcar.domain;
 
-// TODO: 10/27/23 경주를 하는 자동차 생성하는 클래스.
 public class RaceCar {
     private String nameOfCar;
     private Integer cntMovementOfCar;
 
-    // TODO: 10/27/23 생성자 생성
     public RaceCar() {
         nameOfCar = "";
         cntMovementOfCar = 0;
     }
 
+
     public RaceCar(String nameOfRaceCar) {
         // 이름은 5자 이하만 가능하다.
-        // 생성자를 통해서 입력을 받는다.
-
         if (nameOfRaceCar.length() >= 1 && nameOfRaceCar.length() <= 5) {
             nameOfCar = nameOfRaceCar;
             cntMovementOfCar = 0;
         }
     }
 
-    // TODO: 10/27/23 자동차의 이동을 제어해준다.
+    /**
+     * ranNumOfSingleDigit 이 4이상 9이하면 자동차는 이동을 한다.
+     *
+     * @param ranNumOfSingleDigit 0 ~ 9의 랜덤 한자릿 숫자
+     * @return
+     */
     public Boolean movementControlCar(Integer ranNumOfSingleDigit) {
         // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
         if (ranNumOfSingleDigit >= 4 && ranNumOfSingleDigit <= 9) {
@@ -32,17 +34,24 @@ public class RaceCar {
         return false;
     }
 
+    /**
+     * 차동차의 이동 cntMovementOfCar의 증가.
+     */
     public void moveForward() {
-//        이동시 전진 카운터 증가.
+        // 이동시 전진 카운터 증가.
         cntMovementOfCar = cntMovementOfCar + 1;
     }
 
     // TODO: 10/27/23 자동차 멈춤(정지), 나중에 추가할지 말지 생각
     public void pauseTemp() {
 //        - [ ] 멈춤은 아무런 출력을 하지 않는다.
-
     }
 
+    /**
+     * 문자열 리턴 "one"
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return nameOfCar;
