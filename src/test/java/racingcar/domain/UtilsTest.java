634f6bf90def6bd68483ea836ebb14587acfa048
kkw2758 <dtc3229@gmail.com>
2023-10-27 20:35:55 +0900

    test : Utils의 getRandomNumber()메서드가 반환하는 숫자 범위 테스트
    - getRandomNumber()의 역할은 0부터 9사이의 정수를 반환한다.
    - 경계 값인 0과 9가 포함이 되는지 확인하는 테스트 메서드를 추가하였다.

--- src/test/java/racingcar/domain/UtilsTest.java
+++ src/test/java/racingcar/domain/UtilsTest.java
@@ -1,12 +1,25 @@
 package racingcar.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class UtilsTest {
         @Test
         void 시도할_회수는_자연수만_가능합니다() {
-            assertEquals(Utils.isNaturalNumber("-1"), false);
+            assertFalse(Utils.isNaturalNumber("-1"));
+            assertFalse(Utils.isNaturalNumber("0"));
+            assertFalse(Utils.isNaturalNumber("1.5"));
         }
+
+    @Test
+    void 랜덤_숫자_범위_테스트() {
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            int randomNumber = Utils.getRandomNumber();
+            randomNumbers.add(randomNumber);
+        }
+        assertThat(randomNumbers).contains(0,9);
+    }
 }
