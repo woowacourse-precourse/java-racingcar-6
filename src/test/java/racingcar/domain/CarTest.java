63a2519cbac38fe4b97f5ad8df2d747f8971d399
elyudwo <dudqk9696@naver.com>
2023-10-27 22:28:17 +0900

    [Test] : Car 객체 정적 팩토리 메서드 테스트 작성
    정적 팩토리 메서드가 반환한 객체의 name 이 파라미터로 전달한 문자열과 같은지 확인
    private 필드 접근을 위해 리플렉션 사용

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,11 +1,11 @@
 package racingcar.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
@@ -39,7 +39,29 @@ class CarTest {
             field.setAccessible(true);
             int distance = (int) field.get(car);
 
-            Assertions.assertThat(distance).isEqualTo(1);
+            assertThat(distance).isEqualTo(1);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @DisplayName("정적 팩토리 메서드 파라미터로 들어온 값을 받아 객체로 반환한다.")
+    @Test
+    void makeCar() {
+        // given
+        String carName = "pobi";
+
+        // when
+        Car car = Car.makeCar(carName);
+
+        // then
+        try {
+            Field field = Car.class.getDeclaredField("name");
+            field.setAccessible(true);
+            String name = (String) field.get(car);
+
+            assertThat(name).isEqualTo(carName);
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
