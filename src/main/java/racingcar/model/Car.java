22f6a542a2c2f21dc97325a63c59ee3d78aa3223
Isaac Han <isaachan233@gmail.com>
2023-10-27 09:48:22 +0900

    feat: 전진, 랜덤 생성, get 메서드 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,15 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
-    // TODO: 진행한 거리 어떤 타입으로 만들지 고민하기
-    // distance 
+    // TODO: 진행한 거리 어떤 타입으로 만들지 고민하기 - 1차 stringbuilder로 만들어보기
+    private final StringBuilder distance; 
 
     public Car(String name) {
         this.name = name;
+        this.distance = new StringBuilder();
     }
 
     public void setName(String name) {
@@ -17,10 +20,16 @@ public class Car {
         return name;
     }
 
-    public void forward() {
+    public void moveForward() {
+        if(generateRandomNumber() < 4) return;
+        distance.append("-");
+    }
+    
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     public String getDistance() {
-        return "";
+        return distance.toString();
     }
 }
