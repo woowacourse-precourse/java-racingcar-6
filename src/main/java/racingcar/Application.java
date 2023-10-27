19ecc6aa91e682413c1b11ac034e50fa64ad76e7
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-10-27 16:54:08 +0900

    feat(racingcar): 사용자 입력 구현 및 유효성 검사

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,34 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.util.CarFactory;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carList = askCarName();
+        List<Car> racingList = CarFactory.createCars(carList);
+        int playRound = askPlayTurn();
     }
 
-    public static void askCarName() {
-
+    public static List<String> askCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String getCar = Console.readLine();
+        return List.of(getCar.split(","));
     }
 
-    public static void askPlayTurn() {
+    public static int askPlayTurn() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String getTurn = Console.readLine();
+        if (!isValidData(getTurn)){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        return Integer.parseInt(getTurn);
+    }
 
+    private static boolean isValidData(final String getTurn) {
+        return getTurn.matches("[1-9]+");
     }
 
     public static void announcementWinners() {
