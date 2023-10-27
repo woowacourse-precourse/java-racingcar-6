4784322ad6b876e61b91d51a3c0d37d0b931379c
jmj005 <jmj00005@naver.com>
2023-10-27 22:42:43 +0900

    test: 이름 중복 유효성 실패 테스트 추가

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest extends ApplicationTest {
+    @Test
+    void 이름_중복_유효성_실패_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+            );
+    }
+}
