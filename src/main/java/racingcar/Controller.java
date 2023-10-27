45cdf3429161d4b59fc8f41bb48b5ce00d1ab9eb
gijin <rkrhdlf1546@naver.com>
2023-10-28 00:38:04 +0900

    Controller.java 객체 ,로 분리하여 자동차 이름 가져오는 함수 구현

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,4 +1,19 @@
 package racingcar;
 
 public class Controller {
+
+    private Race race;
+    private View raceView = new View();
+
+    private String[] splitCarNames(String carNameInput){
+        String[] carNames = carNameInput.split(",");
+        for(int i=0; i<carNames.length;i++){
+            carNames[i] = carNames[i].trim();
+        }
+        return carNames;
+    }
+
+    public void startRace(){
+
+    }
 }
