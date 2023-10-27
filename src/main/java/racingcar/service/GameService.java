9bc69f9503d073f239b34ed9ce24be9b23fa115f
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-27 17:32:28 +0900

    feat: 우승 자동차 배열 반환 기능 개발

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,9 +1,22 @@
 package racingcar.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
 
 public class GameService {
+    /**
+     * 우승 자동차 배열 반환
+     */
+    public List<String> getWinnerCars(List<Car> cars) {
+        List<String> carNames;
+        int maxDistance = findMaxDistance(cars);
+        carNames = cars.stream()
+                .filter(car -> car.getDistance() == maxDistance).map(Car::getName)
+                .collect(Collectors.toList());
+        return carNames;
+    }
+
     /**
      * 최대로 움직인 횟수 탐색
      */
