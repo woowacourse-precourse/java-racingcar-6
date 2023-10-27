40deb8ec2ec17ac32cde1fc42632e0ad682dcb86
woong2e <tlswodnd12345@naver.com>
2023-10-27 18:51:18 +0900

    기능 목록 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Arrays;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
