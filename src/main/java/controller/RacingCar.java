f6340f5a3ff652841498d44bd5b576ed236d75c3
sinyebin <dpqls0625@naver.com>
2023-10-27 21:28:48 +0900

    feat: 게임시작 및 메시지 추가

---
+++ src/main/java/controller/RacingCar.java
@@ -0,0 +1,10 @@
+package controller;
+
+import view.PrintMessage;
+
+public class RacingCar {
+
+    public void startRacing(){
+        PrintMessage.printStart();
+    }
+}
---
+++ src/main/java/view/PrintMessage.java
@@ -0,0 +1,8 @@
+package view;
+
+public class PrintMessage {
+    private static String START_MESSAGE="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static void printStart(){
+        System.out.println(START_MESSAGE);
+    }
+}
