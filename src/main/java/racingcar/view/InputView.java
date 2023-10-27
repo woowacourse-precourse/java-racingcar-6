2e603c033362d1410029da6178d8ace1029016c2
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-27 16:25:37 +0900

    feat(InputView): add receiveTryNumber function
    Add receiveTryNumber function to receive a try number user input

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String receiveCarName() {
-        String inputCarName;
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        inputCarName = Console.readLine();
-        return inputCarName;
+        return Console.readLine();
     }
 
+    public static String receiveTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
 }
