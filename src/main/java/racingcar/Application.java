e5f27741484159b0d3a23d3d80898b3f74142744
RESD\dkanf <dkanfjgwls@naver.com>
2023-10-27 12:42:52 +0900

    feat: Application의 실제 게임 시작문구와 함수 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RacingCarGame racingCarGame = new RacingCarGame();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        racingCarGame.inputUserCarName();
+        System.out.println("시도할 회수는 몇회인가요?");
+        long userCount = racingCarGame.inputStartCount();
+        System.out.println();
+        racingCarGame.play(userCount);
+        racingCarGame.resultPrint();
     }
 }
