21dd7667dae824d89fa95796555a5dce1ebfe3b9
Namyejun <yejun1148@gmail.com>
2023-10-27 12:57:52 +0900

    유효성 검증에 대한 코드가 UserInput.java에 넘어와 있는 것 같아 기능 분리

--- src/main/java/racingcar/implementation/ValidChecker.java
+++ src/main/java/racingcar/implementation/ValidChecker.java
@@ -3,6 +3,8 @@ package racingcar.implementation;
 import java.util.*;
 
 public class ValidChecker {
+    private static Set<String> carSet = new HashSet<>();
+
     public static void isAlphabetic(String car) {
         for (int i = 0; i < car.length(); i++) {
             if (!(97 <= car.charAt(i) && car.charAt(i) <= 122)) {
@@ -11,15 +13,11 @@ public class ValidChecker {
         }
     }
 
-    public static void isDuplicated(List<String> carList) {
-        Set<String> carSet = new HashSet<>();
-
-        for (String car : carList) {
-            if (carSet.contains(car)) {
-                throw new IllegalArgumentException("중복된 자동차 이름이 입력됐습니다..");
-            } else {
-                carSet.add(car);
-            }
+    public static void isDuplicated(String car) {
+        if (carSet.contains(car)) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 입력됐습니다.");
+        } else {
+            carSet.add(car);
         }
     }
 
@@ -29,11 +27,17 @@ public class ValidChecker {
 
         while (iterator.hasNext()) {
             String car = iterator.next();
-            if (car.length() > 5) {
-                throw new IllegalArgumentException("자동차의 이름은 다섯 글자 이하만 가능합니다.");
-            }
+            atMostFiveWords(car);
+            isAlphabetic(car);
+            isDuplicated(car);
         }
         return carList;
+    }
+
+    public static void atMostFiveWords(String car) {
+        if (car.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 다섯 글자 이하만 가능합니다.");
+        }
     }
 
     public static int numberOfTryCheck(String input) {
