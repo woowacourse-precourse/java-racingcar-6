af5131a22be27222372e65e901b1d455788e5c2d
jinny-l <jinny.l.dev@gmail.com>
2023-10-27 20:28:17 +0900

    ✨feat: 게임 관련 정보를 상수로 모아둔 GameConfig 클래스 구현

---
+++ src/main/java/racingcar/config/GameConfig.java
@@ -0,0 +1,13 @@
+package racingcar.config;
+
+public abstract class GameConfig {
+
+    public static final String CAR_NAME_DELIMITER = ",";
+    public static final String CAR_NAME_DELIMITER_KR = "쉼표";
+    public static final int CAR_NAME_MAX_LENGTH = 5;
+    public static final int ATTEMPTS_MIN_VALUE = 1;
+    public static final int MOVING_FORWARD = 4;
+    public static final int MOVING_MIN_VALUE = 0;
+    public static final int MOVING_MAX_VALUE = 9;
+
+}
