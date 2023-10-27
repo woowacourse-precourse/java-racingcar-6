c3d6e480357b553ce01ac214d417b0f1a2c56e93
devholic22 <hyunjoon.tech@gmail.com>
2023-10-27 17:11:41 +0900

    test: 이름 길이 초과 예외 테스트
    이름 5자 초과 시 예외 발생 테스트 진행

---
+++ src/test/java/racingcar/NameTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.ExceptionMessage.CAR_NAME_LENGTH_EXCEPTION;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Name;
+
+public class NameTest {
+
+    @Test
+    void 이름은_5자_이하만_가능하다_예외() {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Name name = Name.from("nickname");
+        });
+        // then
+        assertThat(exception.getMessage()).isEqualTo(CAR_NAME_LENGTH_EXCEPTION.toString());
+    }
+}
