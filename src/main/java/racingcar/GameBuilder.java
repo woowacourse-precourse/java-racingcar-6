1ad159516f977dc502fcf81327247b90f47f271f
zangsu <zangsu_@naver.com>
2023-10-27 16:12:54 +0900

    fix : use validate logic in GameBuilder.getRoundNumber()

--- src/main/java/racingcar/GameBuilder.java
+++ src/main/java/racingcar/GameBuilder.java
@@ -19,9 +19,7 @@ public class GameBuilder {
     public GameBuilder getRoundNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         String roundNumberString = Console.readLine();
-
-        //InputValidator.validateRoundNumber();
-
+        InputValidator.validateRoundNumber(roundNumberString);
         this.roundNumber = Integer.parseInt(roundNumberString);
         return this;
     }
--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -5,5 +5,6 @@ public class ExceptionMessage {
     public static final String CAR_NAME_IS_TOO_LONG = "자동차 이름은 다섯 글자를 넘을 수 없습니다.";
     public static final String CAR_NAME_DUPLICATED = "중복된 자동차 이름이 존재합니다.";
 
-    public static final String ROUND_NUMBER_MUST_BE_NUMBER = "숫자를 입력해 주세요.";
+    public static final String ROUND_NUMBER_MUST_BE_NUMBER = "라운드 횟수는 숫자가 입력되어야 합니다.";
+    public static final String ROUND_NUMBER_MUST_POSITIVE = "라운드 횟수는 0보다 큰 수가 입력되어야 합니다.";
 }
