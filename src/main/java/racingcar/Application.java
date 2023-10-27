c5d3b07bdc2430218954756e63dfbb49ad5d5069
SeongHun <tjdgns5506@gmail.com>
2023-10-27 13:14:07 +0900

    feat: 경주할 자동차의 이름들을 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String stringCarNames = Console.readLine();
+
+
     }
 }
