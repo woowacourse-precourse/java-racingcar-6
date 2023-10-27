205a6d96111fd7a6af3a594c8d3dc28043ad5e47
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 19:26:36 +0900

    feat: 사용자 입력을 "," 로 나눈 후 List 형태로 변환하여 출력하는 기능 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,21 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GameController {
 
     public GameController() {}
+
+    public void startGame() {
+    }
+
+    private List<String> getInputNames() {
+        String inputNames = InputView.inputCarNames();
+
+        return Arrays.stream(inputNames.split(","))
+                .toList();
+    }
 }
