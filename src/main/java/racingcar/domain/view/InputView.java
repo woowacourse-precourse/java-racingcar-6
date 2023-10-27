0b016f59d197923d0b8a89cc0359522f8496f87a
02ggang9 <02ggang9@gmail.com>
2023-10-27 19:31:10 +0900

    feat(InputView): 사용자의 입력 값을 받는 기능 구현

--- src/main/java/racingcar/domain/view/InputView.java
+++ src/main/java/racingcar/domain/view/InputView.java
@@ -1,5 +1,6 @@
 package racingcar.domain.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.util.InputMessage;
 
 import static racingcar.domain.util.InputMessage.*;
@@ -8,5 +9,9 @@ public class InputView {
 
     public static void printGameStart() {
         System.out.println(RECEIVE_CAR_NAME.getMessage());
+    }
+
+    public static String receiveUserInput() {
+        return Console.readLine();
     }
 }
