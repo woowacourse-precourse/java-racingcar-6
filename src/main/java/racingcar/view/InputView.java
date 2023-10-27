39e0b000d3c3626e1928aac9f9b77431d97af440
dltjdn <hana40004@naver.com>
2023-10-28 04:07:37 +0900

    feat(InputView): 입력 관련 validate 기능 추가
    * 자동차 이름이 1개 이상인지 검증
    * 시도 횟수가 양수인지 검증

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,32 +3,48 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputView {
-
     private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_PROMPT = "시도할 회수는 몇회인가요?";
+    private static final String TRY_COUNT_INPUT_NON_NEGATIVE_ERROR = "시도 횟수는 0 이상이어야 합니다.";
+    private static final String INVALID_INPUT_ERROR = "잘못된 입력입니다. 유효한 숫자를 입력해주세요.";
+    private static final String CAR_NAME_INPUT_SIZE_ERROR = "적어도 한 개 이상의 자동차 이름을 입력해주세요.";
 
     public List<String> readCarNames(){
         System.out.println(CAR_NAME_INPUT_PROMPT);
         String input = Console.readLine();
+        List<String> names = Arrays.asList(input.split(","));
+        validateNotEmpty(names);
+        return names;
+    }
 
-        return Arrays.asList(input.split(","));
+    private void validateNotEmpty(List<String> names) {
+        if(names.isEmpty()){
+            throw new IllegalArgumentException(CAR_NAME_INPUT_SIZE_ERROR);
+        }
     }
 
     public int readTryCount(){
         System.out.println(TRY_COUNT_INPUT_PROMPT);
-
-        return readLineAndParseInt();
+        int tryCount = readLineAndParseInt();
+        validateNonNegative(tryCount);
+        return tryCount;
     }
-
 
     private int readLineAndParseInt(){
         try{
-            return Integer.parseInt(Console.readLine());
+            String input = Console.readLine().trim();
+            return Integer.parseInt(input);
         } catch(NumberFormatException e){
-            throw new IllegalArgumentException("숫자로 변환할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR);
         }
     }
+
+    private void validateNonNegative(int tryCount) {
+        if(tryCount < 0){
+            throw new IllegalArgumentException(TRY_COUNT_INPUT_NON_NEGATIVE_ERROR);
+        }
+    }
+
 }
