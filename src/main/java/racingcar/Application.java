ee07376f312b2c4c13c4a33236b234cfb6d782c3
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-27 14:27:10 +0900

    feat: 게임 시작하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    public static int times;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startGame();
+    }
+
+    public static void startGame(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> names = Arrays.asList(readLine().split(","));
+
+        List<List<String>> namesOfCars = new ArrayList<>();
+        for(int i=0;i<names.size();i++){
+            namesOfCars.add(Arrays.asList(names.get(i),""));
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        times = Integer.valueOf(readLine());
     }
 }
