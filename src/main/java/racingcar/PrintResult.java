ff32b49950be67120aa5c01bb2657d4ff5ceda2f
alswp006 <alswp006@gmail.com>
2023-10-27 21:30:13 +0900

    PrintResult 클래스 구현 완료

---
+++ src/main/java/racingcar/PrintResult.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintResult {
+    public static void printMove(List<String> car_list, StringBuilder[] move_result){
+        for (int i = 0; i<car_list.size(); i++){
+            System.out.println(car_list.get(i) + " : " + move_result[i]);
+        }
+    }
+
+    public static void printWinner(List<String> car_list, StringBuilder[] move_result) {
+        List<String> winner = selectWinner(car_list, move_result);
+
+        System.out.print("최종 우승자 : " + winner.get(0));
+        for (int i = 1; i<winner.size(); i++){
+            System.out.print(", " + winner.get(i));
+        }
+    }
+    public static List<String> selectWinner(List<String> car_list, StringBuilder[] move_result){
+        List<String> winner = new ArrayList<>();
+        int[] carMove = new int[move_result.length];
+        int max_move = 0;
+
+        for (int i = 0; i < move_result.length; i++) {
+            int numberOfMove = move_result[i].length();
+            carMove[i] = numberOfMove;
+            if (numberOfMove >= max_move) {
+                max_move = numberOfMove;
+            }
+        }
+        for (int i = 0; i < move_result.length; i++) {
+            if(move_result[i].length() == max_move){
+                winner.add(car_list.get(i));
+            }
+        }
+        return winner;
+    }
+}
