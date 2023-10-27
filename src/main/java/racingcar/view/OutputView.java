ae15ceb3e6d29addac6e38f61dc377dd60aaba8e
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-10-27 22:42:27 +0900

    Feat : OutputView 코드 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class OutputView {
+
+    private OutputView() {}
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+}
