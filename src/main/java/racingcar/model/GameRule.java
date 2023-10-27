f9558b3c4c76a85b3f11395802f157cda88f1229
Maljaaa <Maljaaa@github.com>
2023-10-27 16:45:32 +0900

    refactor(GameRule): 자동차 전진 규칙 분리

--- src/main/java/racingcar/model/GameRule.java
+++ src/main/java/racingcar/model/GameRule.java
@@ -3,23 +3,7 @@ package racingcar.model;
 import racingcar.utils.NumberGenerator;
 
 public class GameRule {
-
-    private final int STANDARD_NUMBER = 4;
     private final int STANDARD_NAME_SIZE = 5;
-    private NumberGenerator numberGenerator;
-
-    public GameRule(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
-    protected boolean isOkToMove() {
-        int RANDOM_NUMBER = numberGenerator.generateNumberInRange();
-
-        if(RANDOM_NUMBER < STANDARD_NUMBER) {
-            return false;
-        }
-        return true;
-    }
 
     protected boolean isOkToUseCarName(String carName) {
         if(carName.length() > STANDARD_NAME_SIZE) {
