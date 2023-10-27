85cfbe74853148be6b303fde754a541b59321e09
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:36:28 +0900

    [feat] MovingStrategy 클래스의 필드 정의

--- src/main/java/racingcar/domain/MovingStrategy.java
+++ src/main/java/racingcar/domain/MovingStrategy.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
+import racingcar.util.RandomNumberGenerator;
+
 public class MovingStrategy {
+
+    private final RandomNumberGenerator randomNumberGenerator;
+
+    public MovingStrategy(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
 }
