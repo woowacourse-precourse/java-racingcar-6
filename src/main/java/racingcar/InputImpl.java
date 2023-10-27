a113458fbe1cda94d5746a8e002a24dd169bdb03
unknown <kimji1026@gmail.com>
2023-10-27 19:33:17 +0900

    fix : InputImpl.java의 carName에 Console.close버그 수정

--- src/main/java/racingcar/InputImpl.java
+++ src/main/java/racingcar/InputImpl.java
@@ -12,6 +12,7 @@ public class InputImpl implements Input {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         List<String> carNames = validateCarNames(input);
+        //Console.close();
         return carNames;
     }
 
