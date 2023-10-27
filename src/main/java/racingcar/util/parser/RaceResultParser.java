ac6e90dbf7dbdbe7a9ccd253b7b8c0c6188c4424
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-27 19:20:31 +0900

    feat: 경주 결과 분석기 삭제(미사용)

--- src/main/java/racingcar/util/parser/RaceResultParser.java
+++
@@ -1,9 +0,0 @@
-package racingcar.util.parser;
-
-import racingcar.domain.participant.Participant;
-
-import java.util.List;
-
-public interface RaceResultParser {
-    String parse(List<Participant> winners);
-}
