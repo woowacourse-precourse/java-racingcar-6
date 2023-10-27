b6bde6e197be188c4c7fa7f9ad98cca95dcf1bdd
HyungUk Ryu <hw0603@naver.com>
2023-10-27 17:05:31 +0900

    test(Car): 자동차 이름에 금지된 문자가 있는 경우 테스트 코드 추가
    알파벳, 숫자 이외 문자가 있는 경우 예외

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -10,6 +10,15 @@ public class CarTest {
     @DisplayName("자동차 이름이 5글자 초과인 경우 예외")
     void name_greater_than_5() {
         assertThatThrownBy(() -> new Car("123456", 0))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("자동차 이름은 [0-9]~[0-9] 사이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("자동차 이름에 알파벳과 숫자 외 문자가 있는 경우 예외")
+    void name_not_allowed_character() {
+        assertThatThrownBy(() -> new Car("ab12^", 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름에는 알파벳과 숫자만 사용할 수 있습니다.");
     }
 }
