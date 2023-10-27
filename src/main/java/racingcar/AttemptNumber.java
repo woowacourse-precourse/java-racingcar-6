5c495654fcfaf47e7a02c0807d2f6bab4a793ac3
1000zoo <cjswldn99@gmail.com>
2023-10-27 23:29:29 +0900

    fix(AttemptNumber): record에서 일반 class로 수정
    - Input에서 받으면서 바로 생성하기 위해
    - 테스트 할 시에는, Input을 상속받는 별도의 클래스를 생성하여 진행

--- src/main/java/racingcar/AttemptNumber.java
+++ src/main/java/racingcar/AttemptNumber.java
@@ -1,4 +1,16 @@
 package racingcar;
 
-public record AttemptNumber(int number) {
+import racingcar.view.Input;
+
+public class AttemptNumber {
+
+    private final int number;
+
+    public AttemptNumber(Input numberInput) {
+        this.number = Integer.parseInt(numberInput.readLine());
+    }
+
+    public int getAttemptNumber() {
+        return number;
+    }
 }
