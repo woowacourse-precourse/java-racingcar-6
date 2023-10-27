d32e9cae1f3b4fabd8238faa8db0eaf3061552ee
Junobee25 <wnsdhqo@naver.com>
2023-10-27 23:19:21 +0900

    fix: moveTheCar 매개 변수 및 로직 수정

--- src/main/java/racingcar/service/Move.java
+++ src/main/java/racingcar/service/Move.java
@@ -5,12 +5,20 @@ import racingcar.domain.CarGenerator;
 import racingcar.domain.RandomNumber;
 import racingcar.domain.RandomNumberGenerator;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class Move {
     Car car = new Car();
 
-    public void moveTheCar(String carname) {
-        String moveValue = car.getCarList().get(carname);
-        car.getCarList().put(carname, moveValue + "-");
+    public void moveTheCar(LinkedHashMap<String, String> carList, List<Integer> randomNumberList) {
+        int randomNumberListIndex = 0;
+        carList.forEach((key, value) -> {
+            if (randomNumberList.get(randomNumberListIndex) >= 4) {
+                String moveValue = car.getCarList().get(key);
+                car.getCarList().put(key, moveValue + "-");
+            }
+        });
     }
 
     public void stopTheCar(String carname) {
