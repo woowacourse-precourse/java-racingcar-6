bf64aa2dee3dcfbec376ea8dcfce3a42053a2a9a
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 17:48:53 +0900

    refactor(CarGenerator) : 구분자 상수화

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarGenerator {
+    private static final String COMMA = ",";
 
     private CarGenerator() {
     }
 
     public static List<Car> generateParticipationCarList(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(COMMA))
                 .map(carName -> new Car(carName))
                 .collect(Collectors.toList());
     }
