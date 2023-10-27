90802bd07fd94511065acf6068d92701032c8e8f
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 17:19:55 +0900

    feat: Car 클래스에 전진 기능 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,7 +1,14 @@
 package racingcar.model;
 
 public class Car {
+    private String name;
+    private int position;
 
     public Car(String name) {
+        this.name = name;
+    }
+
+    public void forward() {
+        this.position++;
     }
 }
