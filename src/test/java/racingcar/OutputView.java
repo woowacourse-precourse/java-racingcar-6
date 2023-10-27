70ebaa061fde3e6604301523f7bde433a558e850
chansoo <chansoo38@gmail.com>
2023-10-27 15:33:04 +0900

    <Feat> OutputView - 최종 우승자 안내 문구를 출력하는 기능을 구현한다.

--- src/test/java/racingcar/OutputView.java
+++ src/test/java/racingcar/OutputView.java
@@ -1,12 +1,22 @@
 package racingcar;
 
+import java.util.StringJoiner;
+
 public class OutputView {
     private final String RESULT_MESSAGE = "실행 결과";
     private final String FINAL_WINNER = "최종 우승자 : ";
-    private final String DELIMITER = ",";
+    private final String DELIMITER = ", ";
     private final String COLON = " : ";
 
     public void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printFinalWinner(List<Car> winners) {
+        StringJoiner stringJoiner = new StringJoiner(DELIMITER);
+        for(Car winner : winners) {
+            stringJoiner.add(winner.getName());
+        }
+        System.out.println(FINAL_WINNER + stringJoiner);
     }
 }
