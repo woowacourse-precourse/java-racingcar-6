3f3ce761698fe3a536a45938ec9f38f7e2b7fe76
koomchang <leahpar0401@gmail.com>
2023-10-27 18:10:05 +0900

    feat: 자동차 이름에 대한 입력값 검증

---
+++ src/main/java/racingcar/validator/CarNamesInputValidator.java
@@ -0,0 +1,55 @@
+package racingcar.validator;
+
+import java.util.Arrays;
+
+public class CarNamesInputValidator {
+
+    public void validate(String input) {
+        isInputEmpty(input);
+        hasLessThanTwoCarNames(input);
+        hasDuplicateCarNames(input);
+        hasSpecialCharacters(input);
+        isEachCarNameLengthValid(input);
+    }
+
+    private void isInputEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    private void hasLessThanTwoCarNames(String input) {
+        if (input.split(",").length < 2) {
+            throw new IllegalArgumentException("자동차 이름은 2개 이상이어야 합니다.");
+        }
+    }
+
+    private void hasDuplicateCarNames(String input) {
+        if (isDuplicate(input)) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+        }
+    }
+
+    private void hasSpecialCharacters(String input) {
+        if (!input.matches("^[a-zA-Z0-9\\s가-힣]*$")) {
+            throw new IllegalArgumentException("자동차 이름에는 특수문자가 포함될 수 없습니다.");
+        }
+    }
+
+    private void isEachCarNameLengthValid(String input) {
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            if (carName.length() > 5 || carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    private boolean isDuplicate(String input) {
+        String[] carNames = input.split(",");
+        long deduplicationCount = Arrays.stream(carNames)
+                .distinct()
+                .count();
+        return carNames.length != deduplicationCount;
+    }
+}
