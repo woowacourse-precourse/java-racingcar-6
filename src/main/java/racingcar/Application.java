213635d5746ccda87b089bbcf315f4f26e2b475e
YBM <99byoungmin@gmail.com>
2023-10-27 16:14:48 +0900

    feat: Implementation of features

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import java.util.HashMap;
+import racingcar.view.inputView;
+import racingcar.view.outputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputView inputView = new inputView();
+        outputView outputView = new outputView();
+        HashMap<String, Integer> carNameHashMap = new HashMap<>();
+
+        String carName = inputView.gameStart();
+        String[] carNameArray = carName.split(",");
+
+        for (int i = 0; i < carNameArray.length; i++) {
+            carNameHashMap.put(carNameArray[i],0);
+        }
+
+        String askCount = inputView.gameStart();
+        Integer count = Integer.parseInt(askCount);
+
+        outputView.resultView(count,carNameHashMap);
     }
 }
