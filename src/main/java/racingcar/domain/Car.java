2d38614e0496968323a37f67b328f5613b55ee3f
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-27 22:06:22 +0900

    [feat] : add get name method

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,8 +2,13 @@ package racingcar.domain;
 
 public class Car {
     private String name;
+    private int position = 0;
 
+    public String getName() {
+        return name;
+    }
     public void setName(String carName) {
         this.name = carName;
     }
+
 }
