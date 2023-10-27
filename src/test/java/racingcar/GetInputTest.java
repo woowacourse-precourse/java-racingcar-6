6416bb79365a59100bfd08bfb54f046226ac65c5
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-27 14:57:54 +0900

    test: add a test to check if the input feature works correctly

---
+++ src/test/java/racingcar/GetInputTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GetInputTest {
+    String mockInput = "This is a mock message.";
+    private final InputStream inOriginal = System.in;
+
+    @BeforeEach
+    public void setUpStream() {
+        String simulatedInput  = mockInput;
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setIn(inOriginal);
+    }
+
+    @Test
+    public void shouldReadPredefinedInput() {
+        Machines machines = new Machines();
+
+        String receivedInput = machines.getInput();
+
+        assertThat(receivedInput).isEqualTo(mockInput);
+    }
+}
