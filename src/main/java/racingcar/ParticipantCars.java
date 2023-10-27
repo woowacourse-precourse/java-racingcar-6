c08cc97b7c6cf5bf2276196b0c130e6facdebc3e
JAY-Winter <rubat0@naver.com>
2023-10-27 18:30:24 +0900

    feat: 모든 참가 차량 관리

---
+++ src/main/java/racingcar/ParticipantCars.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.List;
+
+public class ParticipantCars {
+
+    private final List<Car> participantCars;
+
+    public ParticipantCars(List<Car> participantCars) {
+        this.participantCars = participantCars;
+    }
+}
