b7b807587a178632d67df88a7246cf65e54f5772
kimwanyoung <dhks2869@gmail.com>
2023-10-27 13:20:05 +0900

    refactor : 이름 검증 Car내부에서 하도록 수정

--- src/main/java/validate/Validator.java
+++ src/main/java/validate/Validator.java
@@ -1,19 +1,12 @@
 package validate;
 
 public class Validator {
-
-    private static final int MAX_LENGTH = 5;
-    private static final String TOO_LONG_NAME = "이름은 %d글자 미만이어야 합니다.";
+    
     private static final String ONLY_NUMBER = "숫자만 입력 가능합니다.";
 
     private Validator() {
     }
 
-    public static void validateNameLength(String name) {
-        if (isNameTooLong(name)) {
-            throw new IllegalArgumentException(String.format(TOO_LONG_NAME, MAX_LENGTH));
-        }
-    }
 
     public static void validateNumericInput(String attempts) {
         try {
@@ -21,9 +14,5 @@ public class Validator {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ONLY_NUMBER);
         }
-    }
-
-    private static boolean isNameTooLong(String name) {
-        return name.length() >= MAX_LENGTH;
     }
 }
