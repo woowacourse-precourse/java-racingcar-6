eb16b8737f5043de01361e8027e3d6dce389ced1
nayooniverse <kangna99@naver.com>
2023-10-28 05:20:44 +0900

    feat: 경주할 자동차 입력 받아 Map으로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String carNames = Console.readLine();
+
+        Map<String, Integer> carMap = Stream.of(carNames.split(","))
+                .collect(Collectors.toMap(carName -> carName, score -> 0));
     }
 }
