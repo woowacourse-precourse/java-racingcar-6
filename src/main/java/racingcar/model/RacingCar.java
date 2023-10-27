5f5036329e159ec512284fc1710daa1edb2ec34c
watermelon3737 <pshrina1@naver.com>
2023-10-27 16:36:15 +0900

    [feat] : racing car 생성자, 전진하는 메서드, 출력을 위해 알맞은 형식으로 변환해 주는 메서드, toString 오버 라이딩 메서드

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,40 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCar {
+    private String racingCarName;
+    private List<Boolean> racingCarMove;
+
+    public RacingCar(final String racingCarName) {
+        this.racingCarName = racingCarName;
+        this.racingCarMove= new ArrayList<>();
+    }
+
+    public void isRacingCarMove(final int randomNumber) {
+        if (randomNumber > 3) {
+            this.racingCarMove.add(true);
+        }
+    }
+
+    public String makeGameResult() {
+        if (this.racingCarMove == null) {
+            return "";
+        }
+        String gameResult = "";
+        for (int i = 0; i < this.racingCarMove.size(); i++) {
+            if (this.racingCarMove.get(i).equals(true)) {
+                gameResult += "-";
+            }
+        }
+        return gameResult;
+    }
+
+    @Override
+    public String toString() {
+        String gameResult = makeGameResult();
+
+        return this.racingCarName + " : " + gameResult;
+    }
+}
