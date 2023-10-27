0e561ce66434ee4931268c2047fe4ff142f1cf57
Roista57 <Roista57@gmail.com>
2023-10-27 14:19:00 +0900

    예외 처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,11 +7,14 @@ public class Application {
     static ArrayList<String> carNameSplit(String racingCarNames){
         ArrayList<String> nameList = new ArrayList<>();
         for(String carName : racingCarNames.split(",")){
+            if (carName.length() > 5){
+                throw new IllegalArgumentException("자동차 이름의 길이가 5보다 깁니다.");
+            }
             nameList.add(carName);
         }
         return nameList;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String racingCarNames = Console.readLine();
         System.out.println("시도할 회수는 몇회인가요?");
