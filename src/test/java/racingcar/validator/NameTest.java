d67b234101b83ba47c3dd1439dda5869c7bda9d3
shin5774 <babo5774@naver.com>
2023-10-27 14:48:29 +0900

    test: Name 객체의 검증후 생성 기능 테스트코드
    - 자동차의 이름을 담는 Name클래스에서 객체 생성시 validate를 거친후 정상적으로 담기는지 확인하는 테스트코드.

---
+++ src/test/java/racingcar/validator/NameTest.java
@@ -0,0 +1,17 @@
+package racingcar.validator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+    @DisplayName("이름이 객체에 잘 담기는지 확인.")
+    @Test
+    void createTest_생성_테스트(){
+        String input="t N ame ";
+        Name name=new Name(input);
+
+        assertThat(name.getName()).isEqualTo("tName");
+    }
+}
