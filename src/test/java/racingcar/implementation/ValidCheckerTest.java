c87404cbc9d93525aa3453438f9cd0c8b5cd03f8
Namyejun <yejun1148@gmail.com>
2023-10-27 15:19:13 +0900

    테스트를 위해 static이었던 Set을 수정함에 따라 바꿔줌

--- src/test/java/racingcar/implementation/ValidCheckerTest.java
+++ src/test/java/racingcar/implementation/ValidCheckerTest.java
@@ -1,6 +1,7 @@
 package racingcar.implementation;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import racingcar.implementation.ValidChecker;
 
@@ -8,25 +9,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ValidCheckerTest {
+    private static ValidChecker checker;
+
+    @BeforeAll
+    static void before() {
+        checker = new ValidChecker();
+    }
 
     @Test
     void 자동차_이름_중복_입력() {
-        ValidChecker.isDuplicated("test");
-        Assertions.assertThrows(IllegalArgumentException.class, () -> ValidChecker.isDuplicated("test"));
+        checker.isDuplicated("test");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> checker.isDuplicated("test"));
     }
 
     @Test
     void 자동차_이름_넌알파베틱_입력() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> ValidChecker.isAlphabetic("a;rw2"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> checker.isAlphabetic("a;rw2"));
     }
 
     @Test
     void 자동차_이름_다섯_글자_이상_입력() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> ValidChecker.atMostFiveWords("abcdefg"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> checker.atMostFiveWords("abcdefg"));
     }
 
     @Test
     void 시도_횟수_숫자_외_입력() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> ValidChecker.numberOfTryCheck("d"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> checker.numberOfTryCheck("d"));
     }
 }
