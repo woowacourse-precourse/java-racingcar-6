18dcfc5fe9baf908ad3e166fb92f55b5323d78bd
unknown <kimji1026@gmail.com>
2023-10-27 20:42:17 +0900

    feat : 공백 및 2개이상의 이름을 가지도록 수정

--- src/main/java/racingcar/InputImpl.java
+++ src/main/java/racingcar/InputImpl.java
@@ -6,7 +6,8 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputImpl implements Input {
-
+    private static final int MAX_NAME_LENGTH = 5;
+    
     @Override
     public List<String> carName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -22,20 +23,6 @@ public class InputImpl implements Input {
         return validateAttemptsNumber(input);
     }
     
-    private List<String> validateCarNames(String input){
-        String[] carNamesArray = input.split(",");
-        List<String> carNames = new ArrayList<>();
-        for (String name : carNamesArray) {
-            String trimmedName = name.trim();
-            if (trimmedName.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
-            carNames.add(trimmedName);
-        }
-        
-        return carNames;
-    }
-    
     private int validateAttemptsNumber(String input) {
         try {
             int attempts = Integer.parseInt(input);
@@ -47,5 +34,41 @@ public class InputImpl implements Input {
             throw new IllegalArgumentException("올바른 숫자를 입력하세요.");
         }
     }
+    
+    private List<String> validateCarNames(String input) {
+        String[] carNamesArray = input.split(",");
+        List<String> carNames = new ArrayList<>();
 
+        for (String name : carNamesArray) {
+            String trimmedName = name.trim();
+            validateNameLength(trimmedName);
+            carNames.add(trimmedName);
+        }
+
+        validateCarNamesCount(carNames);
+        validateNoSpaces(input);
+
+        return carNames;
+    }
+    
+    private boolean validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+        return true;
+    }
+
+    private void validateCarNamesCount(List<String> carNames) {
+        if (carNames.size() < 2) {
+            throw new IllegalArgumentException("자동차 이름은 2개 이상이어야 합니다.");
+        }
+    }
+
+    private void validateNoSpaces(String input) {
+        String trimmedInput = input.trim();
+        if (trimmedInput.endsWith(",")) {
+            throw new IllegalArgumentException("이름 뒤에 공백이 올 수 없습니다.");
+        }
+    }
 }
+
