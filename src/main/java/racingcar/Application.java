56c79a363678778b63361ce5aebbcc39aa43c72c
Identityless <rlandwnd555@naver.com>
2023-10-28 00:03:12 +0900

    feat : Application 스타트 포인트 코드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.component.GameController;
+import racingcar.component.Inputter;
+import racingcar.component.Printer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(
+                Inputter.getInputter(),
+                Printer.getPrinter()
+        );
+
+        gameController.startGame();
     }
 }
