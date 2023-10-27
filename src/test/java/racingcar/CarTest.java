16872ca0dae929fbc86aad5ad69a2fbecfa463b1
minjun7410 <minjun7410@gmail.com>
2023-10-27 23:41:03 +0900

    test(CarTest): 자동차 한 대의 랜덤 넘버 생성 기능 테스트 코드 구현
    * 자동차 한 대가 판정을 위해 랜덤 넘버를 생성하는 기능의 테스트 코드를 추가했다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -11,8 +11,18 @@ public class CarTest {
     void 자동차_이름_5자초과_예외처리_테스트() {
         assertThatThrownBy(() -> new Car("송민준입니다")).isInstanceOf(IllegalArgumentException.class);
     }
+
     @Test
     void 자동차_이름_0자_예외처리_테스트() {
         assertThatThrownBy(() -> new Car("")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_랜덤넘버_생성_테스트() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        for (int TC = 0; TC < 100; TC++) {
+            int randomNumber = numberGenerator.getRandomNumber();
+            assertThat((randomNumber >= 0) && (randomNumber <= 9)).isTrue();
+        }
     }
 }
