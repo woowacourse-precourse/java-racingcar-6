dd55568b460327d78ae1b4ea5525b6528fccc790
mainfn <_@mainfn.dev>
2023-10-27 23:41:19 +0900

    feat: InputValidator 추가

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,9 +1,23 @@
 package racingcar.validator;
 
+import java.util.regex.Pattern;
+import racingcar.exception.InvalidCarNamesInputException;
+import racingcar.exception.InvalidMaxRoundInputException;
+
 public final class InputValidator {
+
+    private static final String CAR_NAMES_REGEX = "^(\\w,)+\\w$";
+    private static final String MAX_ROUND_REGEX = "^\\d+$";
+
     public void validateCarNamesInput(final String input) {
+        if (!Pattern.matches(CAR_NAMES_REGEX, input)) {
+            throw new InvalidCarNamesInputException();
+        }
     }
 
     public void validateMaxRoundInput(final String input) {
+        if (!Pattern.matches(MAX_ROUND_REGEX, input)) {
+            throw new InvalidMaxRoundInputException();
+        }
     }
 }
