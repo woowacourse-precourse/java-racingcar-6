571ae06023f3dfb3c32d42b65fe1a7c55f867bb0
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 01:18:01 +0900

    refactor: 정적 팩토리 메서드 추가
    정적팩토리메서드 추가 및 기존의 생성자를 private로 수정

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,17 +1,28 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.util.NumberGenerator;
 
 public class Cars {
 
+    private static final int DEFAULT_POSITION = 0;
+
     private final List<Car> cars;
 
-    public Cars(final List<Car> cars) {
+    private Cars(final List<Car> cars) {
         this.cars = cars;
     }
 
-    public void move(final NumberGenerator generator) {
+    public static Cars createFromCarNames(final List<String> carsName) {
+        List<Car> cars = new ArrayList<>();
+        for (final String carName : carsName) {
+            cars.add(new Car(new Name(carName), new Position(DEFAULT_POSITION)));
+        }
+        return new Cars(cars);
+    }
+
+    public void moveAll(final NumberGenerator generator) {
         cars.forEach(car -> car.move(generator));
     }
 }
