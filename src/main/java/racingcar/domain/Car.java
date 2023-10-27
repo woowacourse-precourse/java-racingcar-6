0461136d890471f6cbe188aba5e7dadfc85a4284
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:03:43 +0900

    [feat] Car 클래스의 필드 정의

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,14 @@
 package racingcar.domain;
 
 public class Car {
+    private final MovingStrategy movingStrategy;
+
+    private final String name;
+    private int position;
+
+    public Car(MovingStrategy movingStrategy, String name, int position) {
+        this.movingStrategy = movingStrategy;
+        this.name = name;
+        this.position = position;
+    }
 }
