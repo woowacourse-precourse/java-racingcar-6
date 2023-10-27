e22aab45147f19409a7567327edd187f0a9babca
slowcloud <sju9417@gmail.com>
2023-10-27 23:53:03 +0900

    feat: OutputView에서 CarDistanceMessage를 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,14 @@
 package racingcar.view;
 
+import racingcar.domain.CarDistanceMessage;
+
+import java.util.List;
+
 public class OutputView {
 
     public static final String GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String GET_ATTEMPT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    public static final String RESULT_MESSAGE = "실행 결과";
 
     public void printGetCarNames() {
         System.out.println(GET_CAR_NAMES_MESSAGE);
@@ -11,6 +16,15 @@ public class OutputView {
 
     public void printGetAttempt() {
         System.out.println(GET_ATTEMPT_MESSAGE);
+    }
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printCarDistanceMessages(List<CarDistanceMessage> messages) {
+        messages.forEach(System.out::println);
+        System.out.println();
     }
 
 }
