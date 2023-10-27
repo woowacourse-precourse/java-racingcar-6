b33c2a9b825bf6a72a1a7c37f035d1f243dd6ea8
leedonghyun <skydh507@ajou.ac.kr>
2023-10-28 00:57:59 +0900

    feat : view와 RacingList와의 연동 및 게임 실행 기능 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGame {
+    private final RacingList racingList = new RacingList();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private Integer count;
+
+    public void run() {
+        createCarList();
+        count = inputView.inputCount();
+        gameStart(racingList, count);
+        outputView.printResult(racingList.returnWinners());
+    }
+
+    private void createCarList() {
+        String cars = inputView.inputCarList();
+        racingList.createData(cars);
+    }
+
+    public void gameStart(RacingList racingList, Integer count) {
+        System.out.println("실행결과");
+        for (int i = 0; i < count; i++) {
+            racingList.updateState();
+            outputView.printState(racingList);
+        }
+    }
+
+}
