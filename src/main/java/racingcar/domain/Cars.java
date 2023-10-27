f87ee5ae0c1eb35899f82e08599cc84ea596a446
hoyun06 <hoyun06@gmail.com>
2023-10-27 11:36:53 +0900

    refactor: Cars 생성 방식을 생성자 -> 정적 생성 메소드로 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Cars {
     private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
+    private Cars(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public static Cars createCars(List<Car> cars) {
+        return new Cars(cars);
     }
 }
