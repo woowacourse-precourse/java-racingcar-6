b9b525cff0637c21c7d676f94dca6fde8882d3df
Hanjaemo <hjm2530@gmail.com>
2023-10-27 14:47:17 +0900

    feat(Car): 자동차 생성 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int forwardCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.forwardCount = 0;
+    }
+}
