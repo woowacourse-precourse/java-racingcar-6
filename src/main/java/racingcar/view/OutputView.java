a64bad7a5cdd03fa149e66d77273bb0223e864b4
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-28 03:50:58 +0900

    feat: 사용자 입력 기능/메세지 출력 기능
    Consol.readline()으로 Input 받는 InputView 클래스와
    게임 메세지를 출력해주는 OutputView 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+public class OutputView {
+    private final static String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)";
+    private final static String TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final static String PLAY_MESSAGE = "실행 결과";
+    private final static String RESULT_MESSAGE = "최종 우승자 : ";
+
+    public static void inputMessage() {
+        System.out.println(INPUT_MESSAGE);
+    }
+
+    public static void truCountMessage() {
+        System.out.println(TRY_COUNT_MESSAGE);
+    }
+
+    public static void playMessage() {
+        System.out.println(PLAY_MESSAGE);
+    }
+
+    public static void resultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+}
