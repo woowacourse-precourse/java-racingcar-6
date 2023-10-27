f6c3b55fd096fc22a9a76f959fcbc6261fa4a95f
xogus3492 <wkdxogus3492@gmail.com>
2023-10-27 19:36:09 +0900

    Feat(OutputView): “실행 결과” 문구를 출력하는 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String EXECUTION_RESULT_OUTPUT_MESSAGE = "실행 결과";
+
+    public void printResultMessage() {
+        System.out.println("\n" + EXECUTION_RESULT_OUTPUT_MESSAGE);
+    }
+
+
+}
