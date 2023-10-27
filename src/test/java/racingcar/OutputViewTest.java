ebb6e8631bb3c5464397d0c738d607e9edc1e1dd
devhoya97 <diajbh123@gmail.com>
2023-10-27 15:44:25 +0900

    test(racingCar) : 각종 출력 기능 테스트

--- src/test/java/racingcar/OutputViewTest.java
+++ src/test/java/racingcar/OutputViewTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
-class OutputViewTest {
-  
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    public static void main(String[] args) {
+        //given
+        Cars cars = new Cars(Arrays.asList(new Car("pobi"), new Car("woni")));
+        //when
+        OutputView.printResult(cars, 2);
+        //then
+    }
+
+    @Test
+    void printResult_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    main(new String[]{});
+                    assertThat(output()).contains("실행 결과", "pobi : -", "woni : ", "pobi : --", "woni : -"); // output() 메서드의 trim() 적용됨을 유의
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void printWinner() {
+        //given
+        //when
+        OutputView.printWinner(new Car("pobi"), new Car("jun"));
+        //then
+        assertThat(output()).contains("최종 우승자 : pobi, jun");
+    }
+
+    @Override
+    public void runMain() {
+        main(new String[]{});
+    }
 }
