e098bda653f38232e4e2e7f13258d329917b7eef
Han Kyuhyun <khh5345@naver.com>
2023-10-27 16:16:25 +0900

    feat: Implement user input for car names and number of rounds.
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = requestCarNames();
+        int rounds = requestRounds();
+    }
+
+    private static List<String> requestCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Arrays.stream(Console.readLine().split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private static int requestRounds() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
