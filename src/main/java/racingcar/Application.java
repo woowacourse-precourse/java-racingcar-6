7c67327cc4a1bc24aa786c844ebf33024d7f3e95
JayG-5 <jg55552758@gmail.com>
2023-10-28 01:11:09 +0900

    feat: 자동차 이름 -> 객체리스트 변환 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,19 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+    private List<Car> transformStringToCarList(String cars){
+        List<Car> carList = new ArrayList<>();
+        for (String carName : Arrays.stream(cars.split(",")).toList()) {
+            final Car car = new Car(carName);
+            carList.add(car);
+        }
+        return carList;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
