649bb2e6def087978ba1e32620cde3935cd7ce60
leesangjun <hubls@naver.com>
2023-10-27 22:02:39 +0900

    feat: 자동차 이름에 5글자 초과 검증 기능

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -2,13 +2,21 @@ package util;
 
 public class CarNameValidator {
     private static final int CAR_NAME_LENGTH = 5;
+
     public static boolean isValidCarName(String name) {
         checkSpaceInName(name);
+        checkNameLength(name);
         return true;
     }
 
     private static void checkSpaceInName(String name) {
         if (name.contains(" ")) {
+            new IllegalArgumentException();
+        }
+    }
+
+    private static void checkNameLength(String name) {
+        if (name.length() > CAR_NAME_LENGTH) {
             new IllegalArgumentException();
         }
     }
