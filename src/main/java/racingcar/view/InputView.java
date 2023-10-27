b5bebb6583ca34bf267d88e2861059cf4e3f4754
ParkDH0809 <znxn1237@gmail.com>
2023-10-27 13:08:01 +0900

    feat(input): 입력값 검증 후 반환 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.InputValidation;
+
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
-    public String inputCarName() {
+    public List<String> inputCarName() {
         System.out.println(INPUT_CAR_NAME);
-        return Console.readLine();
+        return InputValidation.validateInputName(Console.readLine());
     }
 
-    public String inputTryNumber() {
+    public int inputTryNumber() {
         System.out.println(INPUT_TRY_NUMBER);
-        return Console.readLine();
+        return InputValidation.validateInputNumber(Console.readLine());
     }
 }
