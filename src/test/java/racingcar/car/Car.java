23404c32bb55a61b95955f9e14e818ea37f78992
Jeonwoojae <dnwo0719@icloud.com>
2023-10-28 02:43:41 +0900

    test: 자동차 객체 생성 특문, 숫자 예외 처리
    - @Nested로 Car 생성 테스트 묶어줌

--- src/test/java/racingcar/car/Car.java
+++ src/test/java/racingcar/car/Car.java
@@ -10,6 +10,9 @@ public class Car {
         if (name.length() > 5 || name.length() < 1) {
             throw new IllegalArgumentException("이름은 5자 이상이 될 수 없습니다!");
         }
+        if (!name.matches("^[a-zA-Z가-힣]*$")) {
+            throw new IllegalArgumentException("입력값은 영어나 한글만 가능합니다.");
+        }
         this.name = name;
     }
 }
--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -2,24 +2,37 @@ package racingcar.car;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
-    @Test
-    void 자동차_생성_실패_null_입력() {
-        assertThatThrownBy(() -> new Car(null))
-                .isInstanceOf(IllegalArgumentException.class);
+    @Nested
+    class 자동차_생성 {
+        @Test
+        void 자동차_생성_실패_null_입력() {
+            assertThatThrownBy(() -> new Car(null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 자동차_생성_실패_길이가_5글자보다_큰_입력() {
+            assertThatThrownBy(() -> new Car("aaaaaa"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 자동차_생성_실패_빈_문자열_입력() {
+            assertThatThrownBy(() -> new Car(""))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 자동차_생성_실패_특문_숫자_입력() {
+            assertThatThrownBy(()-> new Car("$#%@"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Car("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @Test
-    void 자동차_생성_실패_길이가_5글자보다_큰_입력() {
-        assertThatThrownBy(() -> new Car("aaaaaa"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 자동차_생성_실패_빈_문자열_입력() {
-        assertThatThrownBy(() -> new Car(""))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 }
