3fc331355876001963a290a29718d9c951eb1ec9
gosu <q63530@naver.com>
2023-10-27 12:18:20 +0900

    test(utill): attemptNumberValidation 메서드 테스트 추가

---
+++ src/test/java/racingcar/utill/ValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.utill;
+
+import static racingcar.utill.Validator.attemptNumberValidation;
+import static racingcar.utill.Validator.carNameValidation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+    @Test
+    void carNameValidationTest() {
+        //given
+        String blank = "";
+        String sixLength = "aaaaaa";
+
+        //when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> carNameValidation(blank));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> carNameValidation(sixLength));
+    }
+
+    @Test
+    void attemptNumberValidationTest() {
+        //given
+        String alphabet = "a";
+        String noNatural1 = "01";
+        String minus = "-1";
+        String floatNumber = "0.1";
+
+        //when , then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> attemptNumberValidation(alphabet));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> attemptNumberValidation(noNatural1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> attemptNumberValidation(minus));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> attemptNumberValidation(floatNumber));
+    }
+}
