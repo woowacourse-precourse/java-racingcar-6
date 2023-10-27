c7bebe80e0d6fb551ef92c58562ecf6ad2b83f1f
gijin <rkrhdlf1546@naver.com>
2023-10-28 00:08:58 +0900

    Race.java 구현 반복문으로 자동차 객체 생성 Race객체 생성, 랜덤 이동함수 구현 및 view.java에 입력 코드 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -3,5 +3,23 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 public class Race {
 
+    private Car[] cars;
+
+    public Race(String[] carNames){ // 자동차 객체마다 생성 이를 위해 Car.java 코드 구현
+        this.cars = new Car[carNames.length];
+        for(int i=0; i<carNames.length; i++){
+            cars[i] = new Car(carNames[i]);
+        }
+    }
+
+    public void moveCars(){ // 자동차 랜덤 이동 함수
+        for( Car car : cars){
+            car.move(Randoms.pickNumberInRange(0,9));
+        }
+    }
+
+    public Car[] getCars(){
+        return cars;
+    }
 
 }
