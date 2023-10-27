6793a7e10b7f4046fce0ec10c8444fefc0f28ac6
chansoo <chansoo38@gmail.com>
2023-10-27 16:32:16 +0900

    <Feat> ProgressBoard - 경주에 참여한 자동차들의 진행상황을 나타내는 객체를 싱글톤으로 생성한다.

---
+++ src/main/java/racingcar/ProgressBoard.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.HashMap;
+
+public class ProgressBoard {
+    private final HashMap<Car, String> SCORE_BOARD;
+    private final String EMPTY_STRING = "";
+    private static ProgressBoard INSTANCE = new ProgressBoard();
+
+    private ProgressBoard() {
+        SCORE_BOARD = new HashMap<>();
+    }
+
+    public static ProgressBoard getInstance() {
+        return INSTANCE;
+    }
+
+
+
+}
