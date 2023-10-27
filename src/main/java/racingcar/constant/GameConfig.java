4efa811457b8785eab7e4faf48d132c8163a7b91
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 16:11:33 +0900

    feat: 자동차 최대 이름 길이 상수 값 추가

--- src/main/java/racingcar/constant/GameConfig.java
+++ src/main/java/racingcar/constant/GameConfig.java
@@ -4,4 +4,5 @@ public final class GameConfig {
     private GameConfig() {}
 
     public static final int MINIMUM_NUMBER_FOR_FORWARD = 4;
+    public static final int MAXIMUM_CAR_NAME_LENGTH = 5;
 }
