7329d204123a99a7df9bb10def4bd07ee5047bf8
newh08 <newh08@gmail.com>
2023-10-28 03:08:03 +0900

    ♻️REFACTOR: WinCars 리펙토링
    메서드 분리, public 을 protected 로 변경

--- src/main/java/racingcar/domain/car/WinCars.java
+++ src/main/java/racingcar/domain/car/WinCars.java
@@ -3,36 +3,41 @@ package racingcar.domain.car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import racingcar.domain.car.carcomponent.CarPositionCompareResult;
+import racingcar.domain.car.carcomponent.carposition.CarPositionCompareResult;
 
 public class WinCars {
     private static final String DELIMITER_WIN_CAR = ", ";
     private static final String WIN_CAR_MESSAGE = "최종 우승자 : ";
+    private static final int FIRST_INDEX = 0;
     private final List<Car> winCars;
 
     public WinCars() {
         this.winCars = new ArrayList<>();
     }
 
-    public void checkCarWinGame(Car car) {
+    protected void checkCarWinGame(Car car) {
         if (winCars.isEmpty()) {
             winCars.add(car);
             return;
         }
-        Car higestCar = winCars.get(0);
+        Car higestCar = winCars.get(FIRST_INDEX);
         CarPositionCompareResult compareResult = car.comparePosition(higestCar);
+        updateWinCarsWithCompareResult(car, compareResult);
+    }
+
+    protected String getWinCars() {
+        String winCar = winCars.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(DELIMITER_WIN_CAR));
+        return WIN_CAR_MESSAGE + winCar;
+    }
+
+    private void updateWinCarsWithCompareResult(Car car, CarPositionCompareResult compareResult) {
         if (compareResult.isBig()) {
             winCars.clear();
         }
         if (!compareResult.isSmall()) {
             winCars.add(car);
         }
-    }
-
-    public String getWinCars() {
-        String winCar = winCars.stream()
-                .map(Car::getCarName)
-                .collect(Collectors.joining(DELIMITER_WIN_CAR));
-        return WIN_CAR_MESSAGE + winCar;
     }
 }
