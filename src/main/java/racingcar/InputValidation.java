3a8a865a43443a991a2fd1ac01bfe84105a1904e
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-27 11:58:10 +0900

    refactor : validation을 입력값 예외 처리만 하도록 변경

--- src/main/java/racingcar/InputValidation.java
+++ src/main/java/racingcar/InputValidation.java
@@ -1,12 +1,6 @@
 package racingcar;
 
-public class Validation {
-
-    private void carNamelengthValidate(String carName){
-        if(carName.length() > 5){
-            throw new IllegalArgumentException();
-        }
-    }
+public class InputValidation {
 
     private void tryInputNumberValidate(String tryInput){
         if(tryInput.matches("(\\d)+")){
