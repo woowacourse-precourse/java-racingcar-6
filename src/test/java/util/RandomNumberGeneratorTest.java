fe17542483ef754b37feebe6143585b83959cac7
eom-tae-in <eti0728@daum.net>
2023-10-28 08:05:17 +0900

    test: RandomNumberGenerator 테스트 작성

---
+++ src/test/java/util/RandomNumberGeneratorTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.util.Constant.END_INCLUSIVE;
+import static racingcar.util.Constant.START_INCLUSIVE;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import racingcar.util.NumberGenerator;
+import racingcar.util.RandomNumberGenerator;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class RandomNumberGeneratorTest {
+
+    @Test
+    void 게임의_규칙대로_랜덤_숫자를_만든다() {
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        // when
+        int generate = numberGenerator.generate();
+
+        // then
+        assertThat(generate).isBetween(START_INCLUSIVE.getValue(), END_INCLUSIVE.getValue());
+    }
+}
