becef5bf7a4d7bbc259868ce6d59ccba709ec16c
bnfkim <kgr3819@gmail.com>
2023-10-27 14:36:23 +0900

    Feat: Car class field & method

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Car {
+
+    private final String name;
+    private final Integer distance;
+
+    public Car(String name, Integer distance){
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public void move(){
+
+    }
+
+    public boolean isMove(){
+
+    }
+
+    public int makeRandomNum(){
+
+    }
+
+    public ArrayList<String> findWinner(){
+
+    }
+}
