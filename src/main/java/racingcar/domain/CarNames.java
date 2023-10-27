15e3cea4e365c6b5fa2127d797b65926f2f18f38
msung99 <msung6924@naver.com>
2023-10-28 08:20:54 +0900

    feat: CarNames 클래스 추가

--- src/main/java/racingcar/domain/CarNames.java
+++ src/main/java/racingcar/domain/CarNames.java
@@ -4,11 +4,25 @@ import java.util.List;
 
 public class CarNames {
     private List<String> nameList;
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
 
-    private CarNames(){
+    private CarNames(String input){
     }
 
     public static CarNames from(String input){
+        return new CarNames(input);
+    }
+
+    private void validateLength(){
+
+    }
+
+    private void validateDuplicate(){
+
+    }
+
+    private void validateEmpty(){
 
     }
 }
