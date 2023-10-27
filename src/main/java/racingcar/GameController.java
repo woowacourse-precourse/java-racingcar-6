43467515a4b10a7bd363056d21d8f4b3e11eaa1b
HanJunSeo <hjs123@jbnu.ac.kr>
2023-10-27 15:54:42 +0900

    README에 기능 구현에 대한 세부적인 내용 추가. insertCarToCarArray()함수 추가. askHowMany()함수 추가. printStartMessage()함수 추가.

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -3,59 +3,66 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class GameController {
 
     GameModel gameModel = new GameModel();
     GameView gameView = new GameView();
-    ArrayList<String> carNames = new ArrayList<>();
-    ArrayList<Integer> carPositions = new ArrayList<>();
-    public void run () {
+    Map<String, Integer> carData = new LinkedHashMap<>();
 
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-        String[] carNamesArray = Console.readLine().split(",");
+    public void insertCarToCarArray(String[] carNamesArray) {
         for (String carName : carNamesArray) {
-            carNames.add(carName);
-            carPositions.add(0);
+            carData.put(carName, 0);
         }
+    }
+
+    public void askHowManyTimes() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+    public void run() {
+        printStartMessage();
+        String[] carNamesArray = Console.readLine().split(",");
+        insertCarToCarArray(carNamesArray);
+
+        askHowManyTimes();
         int count = Integer.parseInt(Console.readLine());
 
-        for(int i = 0; i < carNames.size(); i++) {
-            for(int j = 0; j < count; j++) {
-                int randInt = Randoms.pickNumberInRange(0,9);
-                if(randInt >= 4) {
-                    carPositions.set(i, carPositions.get(i) + 1);
+        for (String carName : carData.keySet()) {
+            for (int j = 0; j < count; j++) {
+                int randInt = Randoms.pickNumberInRange(0, 9);
+                if (randInt >= 4) {
+                    carData.put(carName, carData.get(carName) + 1);
                 }
             }
         }
 
         System.out.println("실행 결과");
-        for(int i = 0; i < carNames.size(); i++) {
-            System.out.print(carNames.get(i) + " : ");
-            for(int j = 0; j < carPositions.get(i); j++) {
+        for (Map.Entry<String, Integer> entry : carData.entrySet()) {
+            System.out.print(entry.getKey() + " : ");
+            for (int j = 0; j < entry.getValue(); j++) {
                 System.out.print("-");
             }
             System.out.println();
         }
 
         // 최대값 찾기
-        int maxPosition = 0;
-        for(int position : carPositions) {
-            if(position > maxPosition) {
-                maxPosition = position;
-            }
-        }
+        int maxPosition = carData.values().stream().max(Integer::compare).orElse(0);
 
         // 최대값과 일치하는 자동차 이름 찾기
         ArrayList<String> winners = new ArrayList<>();
-        for(int i = 0; i < carPositions.size(); i++) {
-            if(carPositions.get(i) == maxPosition) {
-                winners.add(carNames.get(i));
+        for (Map.Entry<String, Integer> entry : carData.entrySet()) {
+            if (entry.getValue().equals(maxPosition)) {
+                winners.add(entry.getKey());
             }
         }
 
         // 우승자 출력
         System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public void printStartMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
     }
 }
