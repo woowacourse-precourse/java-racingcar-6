00a25b27ffe7d1ae1dfd0d91f41b52ad0cd90fcf
soozzang <soozzang@inha.edu>
2023-10-27 14:19:07 +0900

    Feat(Round): 생성자 추가 및 increaseRound() 추가
    increaseRound(), 캡슐화를 지키기 위해 따로 메서드를 만듬.

---
+++ src/main/java/racingcar/model/Round.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Round {
+    private int round;
+    Round(){
+        this.round = 0;
+    }
+
+    public void increaseRound(){
+        round++;
+    }
+}
