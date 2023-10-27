b93e26d942352c49359244011679f0121e8a760e
bnfkim <kgr3819@gmail.com>
2023-10-27 12:32:44 +0900

    Feat: Game method

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+public class Game {
+
+    public void play() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+    public void inputCarName(){
+
+    }
+
+    public void inputGameRound(){
+
+    }
+
+    public void printCarMove(){
+
+    }
+
+    public void printWinner(){
+
+    }
+}
