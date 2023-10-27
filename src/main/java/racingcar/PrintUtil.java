f7c35b24cb4cdfbd254e29e23fded0cca2b33b40
Chenowoo <cjsgusdn14@naver.com>
2023-10-28 00:13:41 +0900

    feat(PrintUtil): 상황에따라 출력을 담당하는 클래스 구현

---
+++ src/main/java/racingcar/PrintUtil.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import static racingcar.MessageConst.*;
+
+public class PrintUtil {
+
+    public static void printNumberOfCarsInputMsg() {
+        System.out.println(NUMBER_OF_CARS_INPUT_MESSAGE);
+    }
+
+    public static void printNameOfCarInputMsg() {
+        System.out.println(NAME_OF_CAR_INPUT_MESSAGE);
+    }
+
+    public static void printMoveCountInputMsg() {
+        System.out.println(MOVE_COUNT_INPUT_MESSAGE);
+    }
+
+    public void printResultMsg() {
+        System.out.println(RESULT_MESSAGE);
+
+        StringBuilder sb = new StringBuilder();
+        //결과값 구하는 로직 추가
+    }
+
+    public static void printWinnerMsg() {
+        System.out.println(WINNER_MESSAGE);
+
+        StringBuilder sb = new StringBuilder();
+    }
+
+}
