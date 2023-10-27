40822c03355033236ed6def1f403e68a99fd45a3
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-27 20:24:00 +0900

    feat: 게임 라운드 수 입력 기능

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -5,8 +5,12 @@ import racingcar.model.RacingCarGame;
 
 public class GameController {
 
+    private static int round;
+
     public static void run() {
         String[] carNames = InputView.inputCarNames();
         RacingCarGame racingCarGame = RacingCarGame.from(carNames);
+
+        round = InputView.inputRoundNumber();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,10 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
     }
+
+    public static int inputRoundNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
