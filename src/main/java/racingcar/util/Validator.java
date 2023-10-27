083dc855cbe2d4a32c869e29536b96e93fb7d4c7
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 19:20:37 +0900

    Fix: 클래스명 수정 및 함수 final로 변경

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,13 +1,13 @@
 package racingcar.util;
 
-public class validator {
+public class Validator {
 
-    private void validateCarLength(String carName){
+    public static void validateCarLength(String carName){
         if(carName.length()>5)
             throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
     }
 
-    private void validateIsNumber(String input){
+    public static void validateIsNumber(String input){
         for (char c : input.toCharArray()) {
             if (!Character.isDigit(c))
                 throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
