3556935146d98be81d504930578affa4b27c1145
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-27 17:12:32 +0900

    feat(Car): RandomNumber생성 후 기준 만족 시 앞으로 이동하는 기능 개발

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,9 @@
 package racingcar.model;
 
+import racingcar.utils.Constraints;
+import racingcar.utils.NumberGenerator;
+import racingcar.utils.RandomNumberGenerator;
+
 public class Car {
     private final String name;
     private int distance = 0;
@@ -9,6 +13,23 @@ public class Car {
      */
     public Car(String name) {
         this.name = name;
+        this.numberGenerator = numberGenerator;
+    }
+
+    /**
+     * 기준 만족 시 앞으로 이동
+     */
+    public void move() {
+        if (canMove()) {
+            distance++;
+        }
+    }
+
+    /**
+     * 앞으로 이동하는 기준
+     */
+    private boolean canMove() {
+        return Constraints.FORWARD_STANDARD.getValue() <= numberGenerator.generateNumber();
     }
 
     public String getName() {
