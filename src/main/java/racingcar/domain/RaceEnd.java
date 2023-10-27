30e20c00d5020b6f3c428f469631e57102400af8
eunzinn <dhr2176@gmail.com>
2023-10-27 13:51:46 +0900

    create program skeleton

---
+++ src/main/java/racingcar/domain/RaceEnd.java
@@ -0,0 +1,7 @@
+package racingcar.domain;
+
+public class RaceEnd {
+    public static void winner(){
+        //우승자 판별
+    }
+}
---
+++ src/main/java/racingcar/domain/RaceStart.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class RaceStart {
+    public static void allowNumber(){
+        //자동차에 무작위 값 할당하기
+    }
+
+    public static void forwardOrStop(){
+        //4 이상일 경우 전진
+    }
+}
