0e171a4cf7db1aae564f9bfe24f2ddb4365e6355
0O000 <zkzk_001@naver.com>
2023-10-27 22:29:19 +0900

    feat: 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,17 +6,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Car {
+    Move m = new Move();
     public void carRacing(){
-        CarName();
+        List<String> carName = CarName();
+        int move = m.getMove();
+        System.out.println("실행 결과");
+        m.printProgress(carName, move);
     }
-    public List<String> CarName() {
+    private List<String> CarName() {
         String[] carNameArray = getCarName().split(",");
         List <String> carNameList = new ArrayList<>();
         carNameList.addAll(Arrays.asList(carNameArray));
         return carNameList;
     }
-    public String getCarName() {
+    private String getCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         if(!isContainComma(input)){
@@ -25,7 +30,7 @@ public class Car {
         return input;
     }
 
-    public boolean isContainComma(String input){
+    private boolean isContainComma(String input){
         if(input.contains(","));{
             return true;
         }
---
+++ src/main/java/racingcar/Move.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Move {
+    public int getMove(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        int move = Integer.parseInt(Console.readLine());
+        return move;
+    }
+
+    public void printProgress(List<String> carNameList, int move){
+        int[] position = new int[carNameList.size()];
+        System.out.println(position[0] + " " + position[1] + " " + position[2]);
+        for(int i = 0; i < move; i++){
+            for (int j = 0; j  < carNameList.size(); j++) {
+                System.out.print(carNameList.get(j) + " : ");
+                if(stopAndGo()) {
+                    position[j]++;
+                }
+                for(int k = 0; k < position[j]; k++){
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i < position.length; i++){
+            if(position[i] == Arrays.stream(position).max().getAsInt()){
+                System.out.print(carNameList.get(i));
+            }
+            System.out.print(", ");
+        }
+    }
+
+    private boolean stopAndGo() {
+        int random = Randoms.pickNumberInRange(1, 9);
+        if (random > 3) {
+            return true;
+        }
+        return false;
+    }
+}
