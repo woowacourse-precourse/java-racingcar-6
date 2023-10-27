ff651f7069d1da5dc12640c79d27fa646da9cdb9
Jung-YouHwan <yhwjd99@gmail.com>
2023-10-27 16:47:01 +0900

    test : 시도할 횟수 입력 요청 메세지 출력 테스트 작성

--- src/test/java/racingcar/view/MessagePrinterTest.java
+++ src/test/java/racingcar/view/MessagePrinterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.constant.TextConstant.ATTEMPT_COUNT_INPUT_MESSAGE;
 import static racingcar.constant.TextConstant.RACING_CAR_NAME_INPUT_MESSAGE;
 
 class MessagePrinterTest {
@@ -25,6 +26,25 @@ class MessagePrinterTest {
         protected void runMain() {
             MessagePrinter messagePrinter = new MessagePrinter();
             messagePrinter.printCarNameInputMessage();
+        }
+    }
+
+    @Nested
+    class AttemptCountInputMessagePrinterTest extends NsTest {
+
+        @Test
+        void 시도할_횟수_입력_요청_메세지_출력() {
+            // when
+            runMain();
+
+            // then
+            assertThat(output()).isEqualTo(ATTEMPT_COUNT_INPUT_MESSAGE);
+        }
+
+        @Override
+        protected void runMain() {
+            MessagePrinter messagePrinter = new MessagePrinter();
+            messagePrinter.printAttemptCountInputMessage();
         }
     }
 }
