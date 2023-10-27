6ce44a9971b99f7bdebaf39293521320fde0ac46
ldw <leehan02@hanyang.ac.kr>
2023-10-28 00:31:52 +0900

    feat(racingcar): 우승자 판단 기능 구현

--- src/main/java/racingcar/Referee.java
+++ src/main/java/racingcar/Referee.java
@@ -1,7 +1,38 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Referee {
+    private int carsNum;
+    private int tryNum;
+
     public boolean judgeMoveOrStop(int num) {
         return num >= 4;
+    }
+
+    public List<String> judgeWinner(List<Car> cars) {
+
+        List<Integer> totalMoveList = new ArrayList<>();
+        cars.forEach(car -> {
+            totalMoveList.add(car.getTotalMove());
+        });
+
+        // max 값 계산
+        int max = totalMoveList.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElse(-1);
+
+        // 우승자 판단
+        List<String> winners = new ArrayList<>();
+        cars.forEach(car -> {
+            if (car.getTotalMove() == max)
+                winners.add(car.getName());
+        });
+
+        return winners;
     }
 }
