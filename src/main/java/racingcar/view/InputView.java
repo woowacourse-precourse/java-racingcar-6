cd2cd90a11023fb7e2c7c2ef4a3bc0b267d06d69
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-27 18:24:12 +0900

    refactor(InputView) : 유효성 검사 기능 관련 메소드 분리 validateIfInputBlank(), validateIfNameLengthZero(), validateOverNameLengthLimit(), validateNameDuplicated(), validateRoundsNumeric() 메소드 생성 및 기존 코드 리팩토링

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,39 +12,57 @@ public class InputView {
     public Map<String, Integer> inputCarName() {
         System.out.println(CAR_NAME_INPUT);
         String nameInput = Console.readLine();
-        // 자동차 이름 입력 관련 1차 유효성 검사
-        if (nameInput.contains(DELIMETER)) {
-            if (nameInput.charAt(0) == DELIMETER.charAt(0)
-                    || nameInput.charAt(nameInput.length()-1) == DELIMETER.charAt(0)) {
-                throw new IllegalArgumentException(NAME_LENGTH_ZERO + nameInput);
-            }
-            if (nameInput.contains(DELIMETER+DELIMETER)) {
-                throw new IllegalArgumentException(NAME_LENGTH_ZERO + nameInput);
-            }
-        }
+        validateInputBlank(nameInput);
+        validateNameLengthZero(nameInput);
         // 자동차 이름 입력 관련 2차 유효성 검사
         Map<String, Integer> carList = new LinkedHashMap<>();
         for (String carName : nameInput.split(DELIMETER)) {
-            if (carName.length() > NAME_LENGTH_MAX) {
-                throw new IllegalArgumentException(OVER_NAME_LENGTH + carName);
-            }
-            if (carList.containsKey(carName)) {
-                throw new IllegalArgumentException(NAME_DUPLICATED + carName);
-            }
+            validateOverNameLengthLimit(carName);
+            validateNameDuplicated(carName, carList);
             carList.put(carName, 0);
         }
         return carList;
     }
     public int inputRounds() {
         System.out.println(NUMBER_INPUT);
-        String countInput = Console.readLine();
-        // 시도 횟수 유효성 검사
-        int count;
-        try {
-            count = Integer.parseInt(countInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NUMERIC + countInput);
+        String roundsInput = Console.readLine();
+        validateInputBlank(roundsInput);
+        return validateRoundsNumeric(roundsInput);
+    }
+
+    public void validateInputBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_INPUT);
         }
-        return count;
+    }
+    public void validateNameLengthZero(String carName) {
+        int lastIndex = carName.length()-1;
+        if (carName.contains(DELIMETER)) {
+            if (carName.charAt(0) == DELIMETER.charAt(0) || carName.charAt(lastIndex) == DELIMETER.charAt(0)) {
+                throw new IllegalArgumentException(NAME_LENGTH_ZERO + carName);
+            }
+            if (carName.contains(DELIMETER+DELIMETER)) {
+                throw new IllegalArgumentException(NAME_LENGTH_ZERO + carName);
+            }
+        }
+    }
+    public void validateOverNameLengthLimit(String carName) {
+        if (carName.length() > NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException(OVER_NAME_LIMIT + carName);
+        }
+    }
+    public void validateNameDuplicated(String carName, Map<String,Integer> carList) {
+        if (carList.containsKey(carName)) {
+            throw new IllegalArgumentException(NAME_DUPLICATED + carName);
+        }
+    }
+    public int validateRoundsNumeric(String round) {
+        int temp;
+        try {
+            temp = Integer.parseInt(round);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC + round);
+        }
+        return temp;
     }
 }
