b43d8c1b687bd83d0055efcf26b852e1c0e13e1e
Hoo-sung <gntjd135@gmail.com>
2023-10-28 08:48:01 +0900

    refactor(Application) : 코드 두 줄 한줄로 리펙토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,6 @@ import racingcar.domain.GameManager;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameManager gameManager = new GameManager();
-        gameManager.run();
+        new GameManager().run();
     }
 }
