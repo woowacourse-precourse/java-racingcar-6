a1f64af55f324726df79a16890d9d7638e61b5fa
taehyeon3 <taehyeon@cx2r2s5.42seoul.kr>
2023-10-27 16:35:45 +0900

    feat(racingcar): 자동차를 전진시키는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Car car = new Car("aaa");
+        System.out.println(car.name);
+        System.out.println(car.mileage);
+        car.moveForward();
+        System.out.println(car.mileage);
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private String name;
-    private int mileage;
+    public String name;
+    public int mileage;
 
     public Car(String name) {
         this.name = name;
         this.mileage = 0;
+    }
+
+    public void moveForward() {
+        int number = Randoms.pickNumberInRange(0, 9);
+        if (number >= 4) {
+            ++this.mileage;
+        }
     }
 }
