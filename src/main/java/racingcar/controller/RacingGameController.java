91fb102e6773d4ad6823e0e0aa2fa1fca2194463
U-keun Song <sukeun319@gmail.com>
2023-10-28 00:11:24 +0900

    Feat : 게임 실행 기능 추가
    게임을 실행하는 gameStart 메서드를 작성했다.

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,15 +1,42 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Model;
+import racingcar.model.Rank;
 import racingcar.util.InputValidator;
 import racingcar.util.Validator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 import racingcar.view.View;
 
 public class RacingGameController {
     private final InputView inputView;
+    private final OutputView outputView;
 
     RacingGameController() {
         InputValidator inputValidator = Validator.generateInputValidator();
         this.inputView = View.generateInputView(inputValidator);
+        this.outputView = View.generateOutputView();
+    }
+
+    public void gameStart() {
+        List<String> participants = inputView.getUserInputName();
+        int tryCount = inputView.getUserInputCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (String name:participants) {
+            cars.add(Model.generateCar(name));
+        }
+
+        while (tryCount-- > 0) {
+            cars.forEach(Car::moveOrStop);
+            outputView.recordStatus(cars);
+        }
+
+        Rank rank = Model.generateRank(cars);
+        outputView.recordWinner(rank.getWinnerList());
+        outputView.printResult();
     }
 }
