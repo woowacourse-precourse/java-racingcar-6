3bbc3ac177ad2a2d9af528eaf21262fbd6a95e47
kimD0ngjun <chickentasty0112@gmail.com>
2023-10-28 01:49:32 +0900

    [feat] 경주 안내 메시지 출력 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,13 @@
 package racingcar.controller;
 
+import racingcar.view.MessagePrinter;
+
 public class RaceController {
+    public RaceController() {
+    }
+
+    public void startGame() {
+        MessagePrinter.printStartMessage();
+        MessagePrinter.printCountMessage();
+    }
 }
--- src/main/java/racingcar/view/MessagePrinter.java
+++ src/main/java/racingcar/view/MessagePrinter.java
@@ -1,11 +1,11 @@
 package racingcar.view;
 
 public class MessagePrinter {
-    public void printStartMessage() {
-
+    public static void printStartMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printCountMessage() {
-
+    public static void printCountMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
