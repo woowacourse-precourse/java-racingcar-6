77323fe2131887a52dacea9acbaef5f3e90a63aa
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:32:40 +0900

    [feat] 상수 정의

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
 
+    private static final Integer START_NUMBER = 0;
+    private static final Integer END_NUMBER = 9;
+
     public int generate() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 }
