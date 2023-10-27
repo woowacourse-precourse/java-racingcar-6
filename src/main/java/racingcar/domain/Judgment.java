a6086bab4cf4e26f4b57da6960ff9768995884ca
Dowon <3120kang@gmail.com>
2023-10-28 01:07:09 +0900

    Feat(Judgment): 랜0에서 9사이의 랜덤한 숫자를 만들어내는 기능 구현

--- src/main/java/racingcar/domain/Judgment.java
+++ src/main/java/racingcar/domain/Judgment.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Judgment {
 
+    private static final int FIRST_VALUE=0;
+    private static final int LAST_VALUE=0;
     public int createRandomNumber() {
-        return 0;
+        return Randoms.pickNumberInRange(FIRST_VALUE,LAST_VALUE);
     }
 
     public boolean isNumberFourOrMore() {
