3330ccd8c63846e6add7ed56bcd628c11e3e4148
pwrwpw <dlgustjr9912@gmail.com>
2023-10-27 12:55:56 +0900

    feat(OutputView): 게임시작 메시지 출력 main에서 게임 시작 메시지 출력 테스트 docs: 게임시작 메시지 체크 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        outputView.requestCarNames();
     }
 }
