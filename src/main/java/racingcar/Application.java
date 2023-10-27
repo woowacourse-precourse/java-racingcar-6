be8872089a05aee95b473026657ddf2373486fc4
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-27 14:38:25 +0900

    feat: 실행 결과 출력하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,19 +11,27 @@ public class Application {
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
     public static final int BOUNDARY_NUMBER = 4;
+
     public static int times;
+    public static List<List<String>> listOfCars;
+
     public static void main(String[] args) {
         startGame();
-
+        for(int i=0;i<times;i++){
+            if(i==0){
+                System.out.println("실행 결과");
+            }
+            printResult();
+        }
     }
 
     public static void startGame(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> names = Arrays.asList(readLine().split(","));
 
-        List<List<String>> namesOfCars = new ArrayList<>();
+        listOfCars = new ArrayList<>();
         for(int i=0;i<names.size();i++){
-            namesOfCars.add(Arrays.asList(names.get(i),""));
+            listOfCars.add(Arrays.asList(names.get(i),""));
         }
 
         System.out.println("시도할 회수는 몇회인가요?");
@@ -36,5 +44,16 @@ public class Application {
             return true;
         }
         return false;
+    }
+
+    public static void printResult(){
+        for(List<String> car : listOfCars){
+            if(pickRandomNumber()){
+                String location = car.get(1)+"-";
+                car.set(1,location);
+            }
+            System.out.println(car.get(0)+" : "+car.get(1));
+        }
+        System.out.println("");
     }
 }
