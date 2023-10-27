22b2cea6175b9bb2ec40b9e103452c875dafe393
GaBaljaintheroom <junsu1222@naver.com>
2023-10-27 20:30:34 +0900

    refactor(CarTest): validate_name_length 테스트 값 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,17 +1,19 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("자동차 객체에 대해")
 class CarTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"박준수12222", "123456", "1234567"})
     @DisplayName("이름 길이가 5 이하가 아니라면 에러가 발생한다.")
-    void validate_name_length() {
-        assertThatThrownBy(() -> new Car("박준수1222"))
+    void validate_name_length(String name) {
+        assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
