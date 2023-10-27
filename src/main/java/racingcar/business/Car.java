1e220c0523948be974fb5784f86b06479b1c95cb
gumgu <bienlee@naver.com>
2023-10-27 20:51:41 +0900

    Feat : 넘겨받은 숫자에 대해 전진(forward) 여부 판단 기능 추가

--- src/main/java/racingcar/business/Car.java
+++ src/main/java/racingcar/business/Car.java
@@ -2,11 +2,19 @@ package racingcar.business;
 
 public class Car {
 
+    private static final int FORWARD_DICISION_NUMBER = 4;
+
     private String name;
     private Integer distance;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void moveCar(Integer randomNumber) {
+        if (randomNumber.intValue() >= FORWARD_DICISION_NUMBER) {
+            forward();
+        }
     }
 
     private void forward() {
