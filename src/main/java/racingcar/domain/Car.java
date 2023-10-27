1cbf528cb79592fc83f0771bab3fd7fb6ed59579
unknown <y2kf9a0q@gmail.com>
2023-10-28 02:07:27 +0900

    feat: Car.java - define instance variables and constructor

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,12 @@
 package racingcar.domain;
 
 public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void move() {
 
