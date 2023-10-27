61429144638b6067ce1a8a8102f2dcd57b5f036a
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-27 19:17:53 +0900

    feat(view) : 시도 횟수 입력 문구 출력 구현 - 출력 문구 함수명 좀 더 직관적으로 변경 - docs : view -> outputview -> 시도 횟수 입력 문구 출력 체크

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,7 +3,11 @@ package racingcar.view;
 import racingcar.util.MessageUtil;
 
 public class OutputView {
-    public void printCarNameMessage() {
+    public void printCarNameInputMessage() {
         MessageUtil.CAR_NAME_INPUT_MESSAGE.messagePrintln();
+    }
+
+    public void printTryCountInputMessage() {
+        MessageUtil.TRY_COUNT_INPUT_MESSAGE.messagePrintln();
     }
 }
