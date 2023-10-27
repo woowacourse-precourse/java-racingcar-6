e4885e313c74bcaaa7a886525135ae060a45b970
elyudwo <dudqk9696@naver.com>
2023-10-27 20:57:46 +0900

    [Test] : 자동차 이동 테스트 작성
    랜덤 숫자로 4 이상의 숫자가 들어올 경우 자동차의 위치 1 증가
    private 필드 접근을 위해 리플렉션 사용

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,4 +21,28 @@ class CarTest {
         assertThatThrownBy(() -> car.validateCarName(carName))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("랜덤 숫자로 4 이상이 들어올 경우 위치를 1 증가시킨다.")
+    @Test
+    void moveCar() {
+        // given
+        int randomNumber = 4;
+        Car car = new Car();
+
+        // when
+        car.moveCar(randomNumber);
+
+        // then
+        try {
+            Field field = Car.class.getDeclaredField("distance");
+            field.setAccessible(true);
+            int distance = (int) field.get(car);
+
+            Assertions.assertThat(distance).isEqualTo(1);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
