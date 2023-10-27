bb3cadff5100bef9ca47891639265c68b4a500e0
happyryan <tjdgks120813@gmail.com>
2023-10-27 17:08:13 +0900

    refactor(utils Create): refactor refactor the playerObjectArray method for improved variable naming
    Updated the playerObjectArray method in the Create class to enhance variable naming for better code readability and clarity.

--- src/main/java/utils/Create.java
+++ src/main/java/utils/Create.java
@@ -12,9 +12,9 @@ public class Create {
 
     public static List<Player> playerObjectArray(List<String> players) {
         List<Player> playerObjectArray = new ArrayList<>();
-        for (String player : players) {
-            Player car = new Player(player);
-            playerObjectArray.add(car);
+        for (String playerName : players) {
+            Player player = new Player(playerName);
+            playerObjectArray.add(player);
         }
         return playerObjectArray;
     }
@@ -22,8 +22,8 @@ public class Create {
     public static Map<String, Integer> playerDistanceMapping(List<Player> playerObjectArray){
         // HashMap의 입력 순서를 보장하기 위해서 LinkedHashMap 사용
         Map<String, Integer> playerDistanceMap = new LinkedHashMap<>();
-        for (Player car : playerObjectArray) {
-            playerDistanceMap.put(car.getPlayerName(), car.getDist());
+        for (Player player : playerObjectArray) {
+            playerDistanceMap.put(player.getPlayerName(), player.getDist());
         }
         return playerDistanceMap;
     }
