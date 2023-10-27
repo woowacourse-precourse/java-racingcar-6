aa3c527762ea63b0ad30129b56c71a78492f9925
SonMinGyu <littleking13@naver.com>
2023-10-27 16:50:05 +0900

    feat: 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
+        String[] carNames = Console.readLine().split(",");
+        int attemptsNum = Integer.parseInt(Console.readLine());
+
+//        for (String carName : carNames) {
+//            System.out.println(carName);
+//        }
+//
+//        System.out.println(attemptsNum);
     }
+
 }
