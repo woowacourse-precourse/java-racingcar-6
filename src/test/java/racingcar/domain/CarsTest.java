5edebba11b50f81f3f8d49fc4a06d9d1794c7997
leeedohyun <ldh19738@gmail.com>
2023-10-27 16:51:52 +0900

    test: 우승자 판별 기능 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -33,5 +34,39 @@ class CarsTest {
         // then
         Assertions.assertThat(locationInformation).hasSize(3)
                 .contains("pobi : -", "woni : -", "jun : -");
+    }
+
+    @Test
+    public void 우승자_한_명인_경우_테스트() {
+        // given
+        final Cars cars = new Cars(List.of(new Car("pobi", "---"),
+                new Car("jun", "--"),
+                new Car("woni", "--")));
+
+        // when
+        final List<Car> winnerCars = cars.getWinnerCars();
+
+        final List<String> winnersName = cars.getNamesOfCars(winnerCars);
+
+        // then
+        Assertions.assertThat(winnersName).hasSize(1)
+                .contains("pobi");
+    }
+
+    @Test
+    public void 우승자_여러_명인_경우_테스트() {
+        // given
+        final Cars cars = new Cars(List.of(new Car("pobi", "---"),
+                new Car("jun", "---"),
+                new Car("woni", "--")));
+
+        // when
+        final List<Car> winnerCars = cars.getWinnerCars();
+
+        final List<String> winnersName = cars.getNamesOfCars(winnerCars);
+
+        // then
+        Assertions.assertThat(winnersName).hasSize(2)
+                .contains("pobi", "jun");
     }
 }
