a65f30ede6156ff1a253b2034e2061c13545aca2
Minjoo <m0522inju@gmail.com>
2023-10-28 02:27:24 +0900

    refactor: stub을 사용하지 않고 Mockito 프레임워크 사용

--- src/test/java/racingcar/CarsModelTest.java
+++ src/test/java/racingcar/CarsModelTest.java
@@ -1,30 +1,42 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
 public class CarsModelTest {
+    Cars cars;
+    Car car1;
+    Car car2;
+    Car car3;
+
+    @BeforeEach
+    void init() {
+        List<Car> carList = new ArrayList<>();
+        car1 = mock(Car.class);
+        car2 = mock(Car.class);
+        car3 = mock(Car.class);
+
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+
+        cars = new Cars(carList);
+    }
+
     @Test
     void 자동차_이름과_결과를_String_리스트로_리턴() {
-        List<Car> carList = new ArrayList<>();
-        List<String> carNameList = new ArrayList<>(Arrays.asList("abc", "def"));
+        when(car1.getNameWithPosition()).thenReturn("car1 : -----");
+        when(car2.getNameWithPosition()).thenReturn("car2 : ---");
+        when(car3.getNameWithPosition()).thenReturn("car3 : ----");
 
-        for(String name: carNameList) {
-            Car car = new StubCar(name);
-            carList.add(car);
-        }
-        Cars cars = new Cars(carList);
-
-        for (int i=1; i<=5; i++) {
-            cars.moveCars();
-        }
-
-        assertThat(cars.getRoundResult()).containsExactly("abc : -----", "def : -----");
+        assertThat(cars.getRoundResult()).containsExactly("car1 : -----", "car2 : ---", "car3 : ----");
     }
 }
