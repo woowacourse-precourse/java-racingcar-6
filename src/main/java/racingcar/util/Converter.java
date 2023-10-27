a40de3e9aaf930dd0394c910070257e7cfeae136
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 01:29:28 +0900

    feat: 들어온 문자열을 양의 정수로 변환하는 기능 추가
    들어온 문자열이 숫자인지 체크
    들어온 문자열이 양수인지 체크
    들어온 문자열을 int로 변환

--- src/main/java/racingcar/util/Converter.java
+++ src/main/java/racingcar/util/Converter.java
@@ -5,8 +5,39 @@ import java.util.List;
 
 public class Converter {
 
-    public static List<String> convertStringToList(final String input) {
-        return Arrays.stream(input.split(","))
-                .toList();
+    private static final int ZERO = 0;
+    private static final String NON_NUMERIC_INPUT_MESSAGE = "숫자가 아닙니다.";
+    private static final String POSITIVE_INT_INPUT_MESSAGE = "양의 정수가 아닙니다.";
+    private static final String NUMERIC_PATTERN = "\\d+";
+
+    public static List<String> convertCommaSeparatedStringToList(final String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    public static int convertStringToPositiveInt(final String input) {
+        validateInputIsNumeric(input);
+        int value = parseInt(input);
+        validatePositiveInt(value);
+        return value;
+    }
+
+    private static int parseInt(final String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static void validatePositiveInt(int value) {
+        if (value <= ZERO) {
+            throw new IllegalArgumentException(POSITIVE_INT_INPUT_MESSAGE);
+        }
+    }
+
+    private static void validateInputIsNumeric(final String input) {
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException(NON_NUMERIC_INPUT_MESSAGE);
+        }
+    }
+
+    private static boolean isNumeric(final String input) {
+        return input.matches(NUMERIC_PATTERN);
     }
 }
