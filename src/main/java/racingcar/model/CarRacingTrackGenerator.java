43846f785adc318885671195552c996aff0a1a4d
eom-tae-in <eti0728@daum.net>
2023-10-27 17:17:39 +0900

    refactor: CarRacing을 구현하는 방식 변경

--- src/main/java/racingcar/model/CarRacingTrackGenerator.java
+++ src/main/java/racingcar/model/CarRacingTrackGenerator.java
@@ -1,18 +1,30 @@
 package racingcar.model;
 
+import static racingcar.util.Constant.ZERO;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class CarRacingTrackGenerator implements RacingTrackGenerator<Car> {
+
+    private static final int ONE = 1;
 
     private final Map<Car, Position> map = new HashMap<>();
 
     @Override
     public RacingGame create(final List<Car> vehicles, final int playCount) {
-        for (int index = 0; index < playCount; index++) {
-            map.put(vehicles.get(index), Position.createWith(index));
+        Position initPosition = Position.createWith(ZERO.getValue());
+        for (Car car : vehicles) {
+            map.put(car, initPosition);
         }
+        createPosition(playCount);
         return new CarRacingGame(map);
+    }
+
+    private void createPosition(final int playCount) {
+        IntStream.rangeClosed(ONE, playCount)
+              .forEach(Position::createWith);
     }
 }
