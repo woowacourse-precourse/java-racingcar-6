518ef29b7f25ac3a8d4a9250c8660cdebf6413ed
HiiWee <wpdlzhf159@gmail.com>
2023-10-27 17:06:02 +0900

    refactor: 이동 결과 dto record 적용

--- src/main/java/racingcar/dto/MoveResult.java
+++ src/main/java/racingcar/dto/MoveResult.java
@@ -2,15 +2,7 @@ package racingcar.dto;
 
 import racingcar.domain.Car;
 
-public class MoveResult {
-
-    private final String name;
-    private final int position;
-
-    private MoveResult(final String name, final int position) {
-        this.name = name;
-        this.position = position;
-    }
+public record MoveResult(String name, int position) {
 
     public static MoveResult createResultOf(final Car car) {
         return new MoveResult(car.getName(), car.getPosition());
