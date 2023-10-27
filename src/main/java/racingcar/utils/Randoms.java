40c2ffc69a5b103b79da92ad37a3d00b1166204c
yeseong31 <yeseong31@naver.com>
2023-10-27 15:58:14 +0900

    feat(Randoms): 랜덤 난수 생성 기능 구현

---
+++ src/main/java/racingcar/utils/Randoms.java
@@ -0,0 +1,11 @@
+package racingcar.utils;
+
+public class Randoms {
+
+    private static final int MINIMUM_NUMBER_RANGE = 0;
+    private static final int MAXIMUM_NUMBER_RANGE = 9;
+
+    public static int pickNumberInRange() {
+        return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(MINIMUM_NUMBER_RANGE, MAXIMUM_NUMBER_RANGE);
+    }
+}
