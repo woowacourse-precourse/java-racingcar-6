c0393df34584b10b88c260b11d9fb8362069c162
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-27 22:00:54 +0900

    feat: 정수를 입력받아 반환하는 기능 구현, 예외처리 구현 - 정수를 입력받아 반환하는 기능 구현 - 올바른 정수를 입력했는지 검사하는 기능 구현

--- src/main/java/util/RacingCarInputReader.java
+++ src/main/java/util/RacingCarInputReader.java
@@ -7,7 +7,7 @@ public class RacingCarInputReader implements InputReader{
     public List<String> inputPlayers() throws IllegalArgumentException{
         List<String> players =  List.of(Console.readLine().split(","));
         if(!isInputPlayerValid(players)){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Invalid player names");
         }
         return players;
     }
@@ -17,5 +17,17 @@ public class RacingCarInputReader implements InputReader{
             return false;
         }
         return players.stream().allMatch(player -> player.length() <= 5);
+    }
+
+    public int inputTryCount() throws IllegalArgumentException{
+        int tryCount = Integer.parseInt(Console.readLine());
+        if(!isInputTryCountValid(tryCount)){
+            throw new IllegalArgumentException("Invalid try count");
+        }
+        return tryCount;
+    }
+
+    public boolean isInputTryCountValid(int tryCount){
+        return tryCount > 0;
     }
 }
