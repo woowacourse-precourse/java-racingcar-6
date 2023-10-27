749c089f8a1879db4829523fccba80d240e12be0
Junobee25 <wnsdhqo@naver.com>
2023-10-27 22:56:14 +0900

    fix: RandomNumber 는 리스트로 받도록 수정

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class RandomNumber {
     private List<Integer> randomNumberList;
 
-    public List<Integer> getRandomNumber() {
+    public List<Integer> getRandomNumberList() {
         return randomNumberList;
     }
 
