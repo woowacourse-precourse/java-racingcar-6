b8851481e37502dc4cda07b3ed2063a06b679e2f
ldw <leehan02@hanyang.ac.kr>
2023-10-27 15:58:17 +0900

    feat(racingcar): 사용자로부터 입력받은 자동차 이름이 5자 이하인지 검증하는 기능 구현

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
-
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_NUMBER_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     public String[] askCarsName() {
@@ -17,5 +16,13 @@ public class UserInput {
         System.out.println(TRY_NUMBER_INPUT_MESSAGE);
 
         return Integer.parseInt(Console.readLine());
+    }
+
+    // todo : choose validateUserInput or isValidName
+    public void validateUserInput(String[] carsName) {
+        for (String name : carsName) {
+            if (name.length() > 5)
+                throw new IllegalArgumentException();
+        }
     }
 }
