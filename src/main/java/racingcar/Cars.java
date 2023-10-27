d0568f54f522672d935405856ed8593b787b1700
devhoya97 <diajbh123@gmail.com>
2023-10-27 12:00:59 +0900

    feat(racingcar) : 자동차 리스트 일급컬렉션 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,2 +1,21 @@
-package racingcar;public class Cars {
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+    private static final String DELIMITER = ",";
+    private List<Car> cars;
+
+    public Cars(String carsString) {
+        List<Car> cars = Arrays.stream(carsString.split(DELIMITER))
+                .map(String::trim)
+                .map(Car::new)
+                .toList();
+        this.cars = cars;
+    }
+
+    public Car get(int index) {
+        return cars.get(index);
+    }
 }
