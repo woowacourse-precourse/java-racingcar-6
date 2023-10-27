3234c8bc37fe16df8890f4e8075b6c138649a9cb
dltjdn <hana40004@naver.com>
2023-10-28 04:50:21 +0900

    feat(Car): 자동차 이름 길이 검증 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,10 +3,19 @@ package racingcar.model;
 public class Car {
     private final String name;
     private final int forwardCount;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String NAME_LENGTH_ERROR = "이름은 5자 이하여야 합니다.";
 
     public Car(String name){
+        validateLength(name);
         this.name = name;
         this.forwardCount = 0;
+    }
+
+    private void validateLength(String name) {
+        if(name.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
     }
 
 }
