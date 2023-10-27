096227994c66d6072d3ef74a91230d4e3ca5f756
hoyun06 <hoyun06@gmail.com>
2023-10-27 12:12:07 +0900

    feat: Cars 내부에 getWinnerCars, getMaxPosition, getCarsAtMaxPosition 메소드 추가
    - getWinnerCars: 내부적으로 getMaxPosition, getCarsAtMaxPosition 호출
    - getMaxPosition: Cars 내부에 있는 각 Car들의 position 중에 가장 큰 값을 반환
    - getCarsAtMaxPosition: 위에서 반환한 max position에 있는 Car들을 List로 반환

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -19,4 +19,23 @@ public class Cars {
             car.printCar();
         });
     }
+
+    public List<Car> getWinnerCars() {
+        int maxPosition = getMaxPosition();
+        return getCarsAtMaxPosition(maxPosition);
+    }
+
+    private int getMaxPosition() {
+        int max = -1;
+        for (Car car : cars) {
+            max = Math.max(car.getPosition(), max);
+        }
+        return max;
+    }
+
+    private List<Car> getCarsAtMaxPosition(int maxPosition) {
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+    }
 }
