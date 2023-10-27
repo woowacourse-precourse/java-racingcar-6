eb189232a681c81986e0e1faf41fa22afe903b21
zangsu <zangsu_@naver.com>
2023-10-27 16:06:46 +0900

    feat : GameBuilder.getRoundNumber()

--- src/main/java/racingcar/GameBuilder.java
+++ src/main/java/racingcar/GameBuilder.java
@@ -7,6 +7,7 @@ import racingcar.constant.ExceptionMessage;
 
 public class GameBuilder {
     String[] names;
+    int roundNumber;
 
     public GameBuilder getCarsName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -15,10 +16,15 @@ public class GameBuilder {
         return this;
     }
 
+    public GameBuilder getRoundNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String roundNumberString = Console.readLine();
 
-    //TODO : GameBuilder getCarsName() throws IllegalArgumentException
+        //InputValidator.validateRoundNumber();
 
-    //TODO : GameBuilder getRoundNumber()throws IllegalArgumentException
+        this.roundNumber = Integer.parseInt(roundNumberString);
+        return this;
+    }
 
     //TODO : Game build()
 }
--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -4,4 +4,6 @@ public class ExceptionMessage {
     public static final String CAR_NAME_CANT_NOT_BLANK = "자동차 이름은 빈 값일 수 없습니다.";
     public static final String CAR_NAME_IS_TOO_LONG = "자동차 이름은 다섯 글자를 넘을 수 없습니다.";
     public static final String CAR_NAME_DUPLICATED = "중복된 자동차 이름이 존재합니다.";
+
+    public static final String ROUND_NUMBER_MUST_BE_NUMBER = "숫자를 입력해 주세요.";
 }
