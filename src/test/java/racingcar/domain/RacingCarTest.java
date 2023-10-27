4c6a50a368069da825cc6a9b55830d3c9390af5c
noxknow <chris2769@naver.com>
2023-10-27 20:12:44 +0900

    docs(README) : 1주차 미션 피드백과 적용 사항, 유효성 검사에 대한 내용 추가

--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -24,7 +24,7 @@ public class RacingCarTest {
 
     @DisplayName("자동차 이름이 영어와 한글이 아닌 경우 예외가 발생한다")
     @ParameterizedTest(name = "[{index}] input : {0} ")
-    @ValueSource(strings = {"poB1,현대,woni", "pobi,현대1,woni"})
+    @ValueSource(strings = {"poB1,현대,woni", "pobi,현대 ,woni"})
     void createCarNamesWithInvalidWord(String inputString) {
         assertThatThrownBy(() -> new RacingCar(List.of(inputString.split(","))))
                 .isInstanceOf(IllegalArgumentException.class)
