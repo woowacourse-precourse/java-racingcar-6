f5bc1023e02c90a34cbddc3af46afced73412f33
KimSehoon <shggm2000@gmail.com>
2023-10-28 01:54:22 +0900

    feat: printResultMessage method 구현
    - "실행 결과" 문구를 출력하는 printResultMessage method 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -9,4 +9,8 @@ public class Output {
     public static void printInputTryCountMessage(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
+
+    public static void printResultMessage(){
+        System.out.println("실행 결과");
+    }
 }
