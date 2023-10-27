1482a03c159572ea33f6164e82d850271d41dac4
gumgu <bienlee@naver.com>
2023-10-27 21:31:27 +0900

    Feat : 입력값을 가져오는 기능 구현

---
+++ src/main/java/racingcar/ui/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+
+    private String getInput() {
+        return Console.readLine();
+    }
+
+}
