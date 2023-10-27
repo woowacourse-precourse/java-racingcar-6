eae1b61e66a6c7ffa5438fd8c556154b63b5f19b
Hoo-sung <gntjd135@gmail.com>
2023-10-27 10:27:52 +0900

    feat(InputView): 경주 게임에 참여하는 Cars의 이름들 입력받는 getCarNames 함수 생성

--- src/main/java/racingcar/domain/InputView.java
+++ src/main/java/racingcar/domain/InputView.java
@@ -1,5 +1,18 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public List<String> getCarNames(){
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        String[] cars = Console.readLine().split(",");
+        return Arrays.stream(cars).toList();
+    }
+
 
 }
