da05ccb6087c133a82b2d104b7d4cab43e509e11
song windows <dkclasltmf@naver.com>
2023-10-27 19:27:03 +0900

    feat: GameView 클래스 생성

---
+++ src/main/java/racingcar/view/GameView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameView {
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+
+        return carNames;
+    }
+
+}
