b8fe217d0a8bb772d0b8bbbd436d387c0cfb371d
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 16:15:24 +0900

    feat: 자동차 이름 입력 상수 메시지 추가

---
+++ src/main/java/racingcar/constant/Message.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public final class Message {
+
+    private Message() {}
+
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+}
