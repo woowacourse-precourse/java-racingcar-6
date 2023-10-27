9ebb84967c80f301e1b8890b7c9ecb2cf2a52a46
koomchang <leahpar0401@gmail.com>
2023-10-28 02:04:42 +0900

    refactor: Car 클래스명을 RacingCar로 변경

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,18 +1,18 @@
 package racingcar.model;
 
-public class Car {
+public class RacingCar {
 
     private final Name name;
     private final Location location;
     private final IsWinner isWinner;
 
-    public Car(Name name, Location location, IsWinner isWinner) {
+    public RacingCar(Name name, Location location, IsWinner isWinner) {
         this.name = name;
         this.location = location;
         this.isWinner = isWinner;
     }
 
-    public Car init(String carName) {
-        return new Car(Name.init(carName), Location.init(), IsWinner.init());
+    public RacingCar init(String carName) {
+        return new RacingCar(Name.init(carName), Location.init(), IsWinner.init());
     }
 }
