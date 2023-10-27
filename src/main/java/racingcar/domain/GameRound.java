9f66f907bde93f2667cc39ed3eecf600edab1dc4
jongheonleee <qwefghnm1212@gmail.com>
2023-10-27 16:53:51 +0900

    [feat] GameRound 클래스의 필드 정의

--- src/main/java/racingcar/domain/GameRound.java
+++ src/main/java/racingcar/domain/GameRound.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
 public class GameRound {
+
+    private final int totalRound;
+    private int currentRound;
+
+    public GameRound(int totalRound, int currentRound) {
+        this.totalRound = totalRound;
+        this.currentRound = currentRound;
+    }
 }
