6e4a40aa9c113fd897db5bc155f71b571455015d
HwangSeokJin <ghkd3703@gmail.com>
2023-10-27 20:11:25 +0900

    test: 다른 자동차와 비교하는 함수 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -15,15 +15,31 @@ public class CarTest extends NsTest{
     @Test
     void 자동자의_이동거리를_문자열로_반환() {
         Car car = new Car("test");
-        assertRandomNumberInRangeTest(
-                () -> {
-                    for (int i = 0; i < 4; i++) {
+        int[] moves = {MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD};
+        move_car(car, moves);
+        assertThat(car.getCurrentResult()).isEqualTo("test : ---");
+    }
+    @Test
+    void 다른_자동차와_거리를_비교() {
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        move_car(car1, new int[]{MOVING_FORWARD, MOVING_FORWARD});
+        move_car(car2, new int[]{MOVING_FORWARD});
+        assertThat(car1.compareTo(car2)).isEqualTo(1);
+
+        move_car(car2, new int[]{MOVING_FORWARD, MOVING_FORWARD});
+        assertThat(car1.compareTo(car2)).isEqualTo(-1);
+    }
+
+    void move_car(Car car, int[] numbers) {
+        for (int number : numbers) {
+            assertRandomNumberInRangeTest(
+                    () -> {
                         car.move();
-                    }
-                    assertThat(car.getCurrentResult()).isEqualTo("test : ---");
-                },
-                MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD
-        );
+                    },
+                    number
+            );
+        }
     }
 
     @Override
