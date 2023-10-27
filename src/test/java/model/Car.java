2bd2a6885bc536694f391c09f6199d839841b780
sehee <tpgml701@naver.com>
2023-10-27 21:28:23 +0900

    test: CarTest 추가

---
+++ src/test/java/model/Car.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Car {
+    String carName ;
+    public Car(String carName) {
+        this.carName = carName;
+    }
+    public String getCarName() {
+        return carName;
+    }
+}
