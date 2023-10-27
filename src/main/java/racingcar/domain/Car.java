a53925d1c47c6e817c107f8c1856da24dab8e580
02ggang9 <02ggang9@gmail.com>
2023-10-27 18:40:45 +0900

    feat(README): 구현할 기능 목록과 역할, 책임 정리

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Car {
+
+    // 책임
+    // 1. 자동차의 이름을 알고 있어야 한다.
+    // 2. 전진과 후진을 할 수 있어야 한다.
+    // 3. 자동차의 이름고 함께 출력 해야 한다.
+
+    // 협력
+    // GameManager ?
+
+
+}
---
+++ src/main/java/racingcar/domain/RacingGameManager.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class RacingGameManager {
+
+    // 책임
+    // 1. 자동차 이름을 입력 받아야 한다.
+    // 2. 몇 번 이동할 지 입력 받아야 한다.
+    // 3. 무작위 값을 알아야 한다.
+
+    // 협력
+    // 1. Car
+
+}
