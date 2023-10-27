919e1eb51617ecfc3790b30eb124267ba203bb11
gumgu <bienlee@naver.com>
2023-10-27 20:52:42 +0900

    Style : 변수명 오타 수정 (dicision -> decision)

--- src/main/java/racingcar/business/Car.java
+++ src/main/java/racingcar/business/Car.java
@@ -2,7 +2,7 @@ package racingcar.business;
 
 public class Car {
 
-    private static final int FORWARD_DICISION_NUMBER = 4;
+    private static final int FORWARD_DECISION_NUMBER = 4;
 
     private String name;
     private Integer distance;
@@ -12,7 +12,7 @@ public class Car {
     }
 
     public void moveCar(Integer randomNumber) {
-        if (randomNumber.intValue() >= FORWARD_DICISION_NUMBER) {
+        if (randomNumber.intValue() >= FORWARD_DECISION_NUMBER) {
             forward();
         }
     }
