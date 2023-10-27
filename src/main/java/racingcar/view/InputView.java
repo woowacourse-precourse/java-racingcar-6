a5014d8c09407ed6f6cbae6c898d44187ef14dae
junOTL <naqp99@gmail.com>
2023-10-27 14:20:34 +0900

    feat: 경주 할 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,15 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Validator;
+
 public class InputView {
+    public static String[] inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return splitByComma(Console.readLine());
+    }
+
     public static String[] splitByComma(String input) {
-        return input.split(",");
+        return Validator.isValidName(input.split(","));
     }
 }
