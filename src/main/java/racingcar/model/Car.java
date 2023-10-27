739f09b21abbda124035553685413f2193abf882
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-27 17:26:00 +0900

    feat(Car): 생성한 무작위 값이 4 이상이라면 전진할 수 있다

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,6 +5,7 @@ import racingcar.util.RandomUtil;
 public class Car {
     private static final int MIN_RANDOM_NUMBER = 0;
     private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int REFERENCE_VALUE_MOVING_FORWARD = 4;
 
     private final String name;
     private Integer forwards;
@@ -14,7 +15,17 @@ public class Car {
         this.forwards = 0;
     }
 
+    public void moveForward() {
+        moveForward(createRandomNumber());
+    }
+
     private int createRandomNumber() {
         return RandomUtil.createRandomNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+
+    private void moveForward(int randomNumber) {
+        if(randomNumber >= REFERENCE_VALUE_MOVING_FORWARD) {
+            forwards += 1;
+        }
     }
 }
