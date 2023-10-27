c7bcc82c85c4b245fc9b10d3f1bc3a18d9c76938
JAY-Winter <rubat0@naver.com>
2023-10-27 23:15:36 +0900

    feat: Car Getter

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,23 @@
 package racingcar;
 
 public class Car {
+
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void move() {
+        position++;
     }
 }
