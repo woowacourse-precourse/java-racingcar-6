11120d080056e2e433a07b01ac648d21f77b4111
Cycrypto <jh01love@naver.com>
2023-10-27 18:18:23 +0900

    test:테스트코드 추가 및 작동확인

--- src/test/java/racingcar/controller/GameUtilsTest.java
+++ src/test/java/racingcar/controller/GameUtilsTest.java
@@ -1,2 +1,25 @@
-package racingcar.controller;public class GameUtilsTest {
+package racingcar.controller;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameUtilsTest {
+    private GameUtils utility;
+    @BeforeEach
+    void setUp(){
+        this.utility = new GameUtils();
+    }
+    @Test
+    void 사용자_입력값을_정상적으로_처리(){
+        String input = "123,456,789";
+        List<String> result = utility.splitByComma(input);
+        assertThat(result)
+                .isNotEmpty()
+                .contains("123","456","789");
+
+    }
 }
