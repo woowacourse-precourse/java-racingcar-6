3735f6ce0462a80bce7089edf8cf33438807456b
slowcloud <sju9417@gmail.com>
2023-10-27 11:47:05 +0900

    feat: Attempt에서 시도 횟수가 남았는지 확인하는 기능 구현

---
+++ src/main/java/racingcar/domain/Attempt.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class Attempt {
+
+    private Integer count;
+
+    public Attempt(Integer count) {
+        this.count = count;
+    }
+
+    public boolean isExist() {
+        return count > 0;
+    }
+
+}
