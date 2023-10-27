013af09d0bd23be0cf2aa6b22b05dbab2376190d
shin5774 <babo5774@naver.com>
2023-10-27 22:54:14 +0900

    feat: 콘솔 출력문 출력 기능 추가
    - 프로그램 과정중 출력 메시지 출력 기능 추가.
    - 실핼 결과 출력 과정중 횟수마다 띄어쓰기 추가.

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -15,9 +15,28 @@ public class View {
         for(Car car:cars.getCars()){
             printCurrentCarResult(car);
         }
+        System.out.println();
     }
 
     public void printCurrentCarResult(Car car){
         System.out.println(car.getNameValue()+" : "+car.getCurrentPositionStatus());
+    }
+
+    public void printNameInputMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printAttemptCountInputMessage(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printAttemptResultMessage(){
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void printFinalWinnerMessage(){
+        System.out.println();
+        System.out.print("최종 우승자 : ");
     }
 }
