c7c0c91c112552a51fa870fe09780507f41ab1da
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 17:27:44 +0900

    style(RacingController) : 시도 횟수 입력 메서드 racing() -> startGame()로 이동

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -11,14 +11,14 @@ public class RacingController {
     public void startGame() {
         OutputView.printCarNameInputMessage();
         String carNames = InputView.inputCarNames();
-        OutputView.printTryCountInputMessage();
         RacingCars racingCars = new RacingCars(CarGenerator.generateParticipationCarList(carNames));
-        racing(racingCars);
+        OutputView.printTryCountInputMessage();
+        int tryCount = Convertor.convertStringToInt(InputView.inputTryCount());
+        racing(racingCars, tryCount);
         OutputView.printWinner(racingCars.getWinner());
     }
 
-    private void racing(RacingCars racingCars) {
-        int tryCount = Convertor.convertStringToInt(InputView.inputTryCount());
+    private void racing(RacingCars racingCars, int tryCount) {
         OutputView.printExecutionResultMessage();
         while (tryCount > 0) {
             racingCars.move();
