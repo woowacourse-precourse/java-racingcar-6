6148a796d31981f8e8c7af9a5d1b3081daa84a2f
Identityless <rlandwnd555@naver.com>
2023-10-27 23:31:40 +0900

    feat : 최종 승자 목록 반환 기능 추가

--- src/main/java/racingcar/component/Printer.java
+++ src/main/java/racingcar/component/Printer.java
@@ -1,6 +1,9 @@
 package racingcar.component;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Printer {
     private Printer() {}
@@ -19,5 +22,21 @@ public class Printer {
             System.out.println(car.getName() + " : " + car.getProgress());
         }
         System.out.println();
+    }
+
+    private Queue<Car> getWinners(List<Car> cars) {
+        int top;
+        int i = 0;
+        Queue<Car> winners = new LinkedList<>();
+
+        Collections.sort(cars);
+        top = cars.get(0).getProgress().length();
+
+        while (cars.get(i).getProgress().length() == top) {
+            winners.add(cars.get(i));
+            i++;
+        }
+
+        return winners;
     }
 }
