9e96e7fdb9e22eef8011b7bd9c8aa81dc58cf2f2
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 00:40:08 +0900

    feat(model): 자동차 추가/업데이트 함수 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,25 @@
 package racingcar.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 public class Car {
+    HashMap<String, Integer> carList = new HashMap<>();
+
+    public void addCar(String carNames) {
+        carList = Arrays.stream(carNames.split(","))
+                .collect(Collectors.toMap(
+                        key -> key,
+                        value -> 0,
+                        (version1, version2) -> version1,
+                        HashMap::new
+                ));
+    }
+
+    public void updateCar(String carName) {
+        if (carList.containsKey(carName)) {
+            carList.put(carName, carList.get(carName) + 1);
+        }
+    }
 }
