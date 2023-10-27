37be10a01fc0872b3e60b62c02daddc63b0293ce
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 14:15:25 +0900

    feat(RandomCarEngine): 랜덤한 숫자를 뽑아 자동차가 움직일 수 있는지 반환하는 기능 구현

--- src/main/java/racingcar/RacingCarOutputView.java
+++ src/main/java/racingcar/RacingCarOutputView.java
@@ -1,10 +1,15 @@
 package racingcar;
 
+
 public class RacingCarOutputView {
 
     public static final String RACING_EXECUTE_MESSAGE = "실행 결과";
 
     public void printStartMessage() {
         System.out.println(RACING_EXECUTE_MESSAGE);
+    }
+
+    // 횟수는 밖에서 조절, Cars(Car들이 들어있는 일급컬렉션)를 넘겨받아 매 회차 레이싱 결과를 출력.
+    public void printEachRacingResult() {
     }
 }
