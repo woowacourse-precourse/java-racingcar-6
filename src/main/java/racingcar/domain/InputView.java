65725ac1d8906ad0dc2c90e150eb1a87579ca6b4
Hoo-sung <gntjd135@gmail.com>
2023-10-27 10:33:04 +0900

    feat(InputView): 시도할 횟수를 입력받는 getAttempts 함수 생성

--- src/main/java/racingcar/domain/InputView.java
+++ src/main/java/racingcar/domain/InputView.java
@@ -8,10 +8,17 @@ import java.util.List;
 public class InputView {
 
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private static final String ATTEMPTS_INPUT_MESSAGE = "시도할 횟수는 몇회인가요?";
     public List<String> getCarNames(){
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         String[] cars = Console.readLine().split(",");
         return Arrays.stream(cars).toList();
+    }
+
+    public Integer getAttempts(){
+        System.out.println(ATTEMPTS_INPUT_MESSAGE);
+        return Integer.parseInt(Console.readLine());
     }
 
 
