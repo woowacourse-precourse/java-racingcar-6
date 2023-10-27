83ef00243f83409a262bd058fdff92411bae248e
slowcloud <sju9417@gmail.com>
2023-10-27 23:37:55 +0900

    feat: Game에서 우승자를 추려서 WinnerMessage를 만드는 기능 구현

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
@@ -26,6 +27,30 @@ public class Game {
 
         return messages;
 
+    }
+
+    public WinnersMessage getWinners() {
+        Integer farthest = getFarthestDistance();
+        List<Car> winners = getWinners(farthest);
+        return new WinnersMessage(winners);
+    }
+
+    private List<Car> getWinners(Integer farthest) {
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getDistance().equals(farthest)) {
+                winners.add(car);
+            }
+        }
+        return Collections.unmodifiableList(winners);
+    }
+
+    private Integer getFarthestDistance() {
+        int farthest = 0;
+        for (Car car : cars) {
+            farthest = Math.max(farthest, car.getDistance());
+        }
+        return farthest;
     }
 
 }
