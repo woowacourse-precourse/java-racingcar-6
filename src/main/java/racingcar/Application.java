b3c51a9470ec5d99530a9f372f400ffd2259d785
taehyeon3 <taehyeon@cx2r2s5.42seoul.kr>
2023-10-27 16:21:00 +0900

    feat(racingcar): 각 자동차에 이름을 부여하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.domain.Car;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Car car = new Car("aaa");
     }
 }
---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int mileage;
+
+    public Car(String name) {
+        this.name = name;
+        this.mileage = 0;
+    }
+}
