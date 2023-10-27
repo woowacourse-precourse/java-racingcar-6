21725119c4c2355667e9a002c2252df70c703f9f
hlmg <devhlmg@gmail.com>
2023-10-28 07:17:30 +0900

    feat(Car): 자동차 생성 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,12 +3,24 @@ package racingcar.domain;
 import java.util.function.BooleanSupplier;
 
 public class Car {
+    private final String name;
     private final BooleanSupplier engine;
     private int position;
 
-    public Car(BooleanSupplier engine) {
+    public Car(String name, BooleanSupplier engine) {
+        validateName(name);
+        this.name = name;
         this.engine = engine;
         this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다");
+        }
     }
 
     public void moveForward() {
@@ -17,7 +29,11 @@ public class Car {
         }
     }
 
-    public int currentPosition() {
+    public int getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
