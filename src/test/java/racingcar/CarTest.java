8af99193575e81a50ef4e91d772530375a3e4d54
ailiartsua <ailiartsual2@gmail.com>
2023-10-28 00:32:15 +0900

    test : car domain _ run 테스트 추가
    랜덤 값이 3 이하인 경우, 4 이상인 경우의 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
     @Test
     void create_차량_생성_시_이름_설정(){
         //given
@@ -25,5 +27,31 @@ public class CarTest {
 
         //then
         Assertions.assertThat(car.getDistance()).isEqualTo(0);
+    }
+    @Test
+    void run_랜덤_숫자가_4_이상_시_거리_증가(){
+        //given
+        String carName = "test";
+        Car car = Car.create(carName);
+        int preDistance = car.getDistance();
+
+        //when
+        car.run(MOVING_FORWARD);
+
+        //then
+        Assertions.assertThat(car.getDistance()).isEqualTo(preDistance+1);
+    }
+    @Test
+    void run_랜덤_숫자가_3_이하_시_거리_유지(){
+        //given
+        String carName = "test";
+        Car car = Car.create(carName);
+        int preDistance = car.getDistance();
+
+        //when
+        car.run(STOP);
+
+        //then
+        Assertions.assertThat(car.getDistance()).isEqualTo(preDistance);
     }
 }
