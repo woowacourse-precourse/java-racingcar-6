431e045854c95f7bb72c0639c471234b95f5703d
parkchangeon <jg9870@naver.com>
2023-10-27 21:55:47 +0900

    feat(racingcar): 레이싱하는 기능 구현

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Race {
+    private final List<Car> cars;
+    private final int rounds;
+
+    public Race(List<Car> cars, int rounds) {
+        this.cars = cars;
+        this.rounds = rounds;
+    }
+
+    public void start() {
+        ResultView.printResultNotification();
+        for (int i = 0; i < rounds; i++) {
+            moveCars();
+        }
+    }
+
+    private void moveCars() {
+        for (Car car : cars) {
+            moveCar(car);
+            ResultView.printCarPosition(car);
+        }
+        ResultView.printLineBreak();
+    }
+
+    private void moveCar(Car car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            car.moveForward();
+        }
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+}
