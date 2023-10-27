09820416d90955bc588f85c37c95ebee1081a0e8
whxogus215 <whxogus215@naver.com>
2023-10-27 11:16:45 +0900

    feat: Car 객체에 getter 및 생성자 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,26 @@
 package racingcar.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Car {
     private String name;
-
     private Integer distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public Car(String name, Integer distance) {
+        this.name = name;
+        this.distance = distance;
+    }
 
     public void updateDistance(int randomNumber) {
         if (randomNumber >= 4) {
