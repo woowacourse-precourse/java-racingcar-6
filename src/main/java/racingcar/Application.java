6192615da5dbe0b19408df5be31e64d196118164
song windows <dkclasltmf@naver.com>
2023-10-28 00:06:40 +0900

    feat: GameController 객체 생성 후 startGame 메서드 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.handler.InputHandler;
+import racingcar.service.CarService;
+import racingcar.view.GameView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new GameView(), new InputHandler(), new CarService());
+        gameController.startGame();
     }
 }
