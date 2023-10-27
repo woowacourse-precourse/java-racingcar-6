7d372f7082111e7534c8ca8175ef05b243a58d52
Hoo-sung <gntjd135@gmail.com>
2023-10-27 11:38:43 +0900

    feat(OutputView): 라운드마다 실행 결과 출력해주는 printRoundResult 함수 생성

--- src/main/java/racingcar/domain/OutputView.java
+++ src/main/java/racingcar/domain/OutputView.java
@@ -1,7 +1,20 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
+    public void printRoundResult(List<Car> cars){
+        for(Car car : cars){
+            String messge = "";
+            messge+= car.getName();
+            messge+=" : ";
+            for(int i=0;i<car.getTotalMovedDistance();i++){
+                messge += "-";
+            }
+            System.out.println(messge);
+        }
+    }
 }
