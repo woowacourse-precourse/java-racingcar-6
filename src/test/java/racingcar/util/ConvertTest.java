37e0c373a0424171f4730b8ef3999345863c790e
YongHyeon <pyh4616@naver.com>
2023-10-27 16:21:26 +0900

    Test: (ConvertTest 클래스), 문자열을 받아 쉼표를 기준으로 리스트로 변환하는 기능을 테스트

---
+++ src/test/java/racingcar/util/ConvertTest.java
@@ -0,0 +1,23 @@
+package racingcar.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ConvertTest {
+
+    @Test
+    @DisplayName("문자열을 받아 쉼표를 기준으로 리스트로 변환한다.")
+    void should_ConvertToListUsingComma_When_ReceiveString() {
+        String inputValue = "park,yong,hyeon";
+
+        List<String> carNames = Convert.stringToListUsingComma(inputValue);
+
+        assertThat(carNames.size()).isEqualTo(3);
+        assertThat(carNames.get(0)).isEqualTo("park");
+        assertThat(carNames.get(1)).isEqualTo("yong");
+        assertThat(carNames.get(2)).isEqualTo("hyeon");
+    }
+}
