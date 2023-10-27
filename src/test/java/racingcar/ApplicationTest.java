3e102b3c4046dd48f20bfc67766a831c660549ff
박승찬 <seungchan141414@gmail.com>
2023-10-27 18:40:31 +0900

    [Test(racing)] TDD 작성 중 커밋

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,14 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.control.GameProcess;
+import racingcar.domain.CreateRandomNum;
+import racingcar.domain.GameHost;
+import racingcar.domain.JudgeStandard;
+import racingcar.domain.RaceCar;
+import racingcar.view.GameStart;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,15 +18,32 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private GameProcess gameProcess;
+    private CreateRandomNum createRandomNum;
+    private GameHost gameHost;
+    private JudgeStandard judgeStandard;
+    private RaceCar raceCar;
+    private GameStart gameStart;
+
+    @BeforeEach
+    void setup() {
+        gameProcess = new GameProcess();
+        createRandomNum = new CreateRandomNum();
+        gameHost = new GameHost();
+        judgeStandard = new JudgeStandard();
+        raceCar = new RaceCar();
+        gameStart = new GameStart();
+    }
+
 
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -30,6 +54,11 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    // 추가 - 박승찬
+
+
+    // 추가 완료 - 박승찬
 
     @Override
     public void runMain() {
