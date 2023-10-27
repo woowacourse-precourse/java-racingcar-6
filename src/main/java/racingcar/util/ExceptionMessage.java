cd8268fb9555d3c1de233277a8b69eff3fdbbc68
Jiyoung Choi <12201817@inha.edu>
2023-10-27 20:57:54 +0900

    style : ExceptionMessage 상수명 수정

--- src/main/java/racingcar/util/ExceptionMessage.java
+++ src/main/java/racingcar/util/ExceptionMessage.java
@@ -5,7 +5,7 @@ public enum ExceptionMessage {
     INPUT_WRONG_CAR_LENGTH_MESSAGE("최소한 1개 이상의 자동차 이름을 입력해야 합니다."),
     INPUT_NO_COUNT_MESSAGE("시도 횟수를 입력해야 합니다."),
     INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
-    INPUT_WRONG_RANGE_OF_NUMBER_MESSAGE("시도 횟수는 1이상이어야 합니다.");
+    INPUT_WRONG_RANGE_OF_COUNT_MESSAGE("시도 횟수는 1이상이어야 합니다.");
 
     private final String exceptionMessage;
 
