60c73e97d64ec95dae6526ed3bf7e7fe4e9e6a6c
JungYeon <jyp75580@gmail.com>
2023-10-27 16:15:47 +0900

    feat: RaceController 기능 구현

---
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -0,0 +1,48 @@
+package racingcar.Controller;
+
+import racingcar.Model.Car;
+import racingcar.Service.RaceService;
+import racingcar.View.InputView;
+import racingcar.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    RaceService raceService = new RaceService();
+    List<Car> racingCars;
+
+    public void RaceGamePlay(){
+        startGameSet();
+        int count =  playCount();
+        for(int i=0;i < count;i++){
+           // 각 객체마다 random값 비교
+            playRace();
+            outputView.outputResult(racingCars);
+        }
+        outputView.outputWinner(racingCars);
+
+    }
+
+    private void playRace() {
+        for(Car car : racingCars){
+            if(raceService.RandomNum() >= 4){
+                car.setLocation(car.getLocation()+1);
+            }
+        }
+    }
+
+    private int playCount() {
+        return inputView.inputPlayCount();
+    }
+
+    private void startGameSet() {
+        List<String> carNames = inputView.inputCarName();
+        racingCars = new ArrayList<>();
+        for (String carName : carNames){
+            racingCars.add(new Car(carName,0));
+        }
+    }
+}
