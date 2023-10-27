48dc37f93a14c8fe8be387f6e2460246cba84f89
newh08 <newh08@gmail.com>
2023-10-28 06:09:39 +0900

    ♻️REFACTOR: Car 리펙토링
    하드코딩 제거
    carName validation 추가 : 길이 5자 이하만 가능

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,15 +1,22 @@
 package racingcar.domain.car;
 
+import static racingcar.Constant.ZERO;
+
 import racingcar.domain.car.carcomponent.carposition.CarPosition;
 import racingcar.domain.car.carcomponent.carposition.CarPositionCompareResult;
 
 public class Car {
+    private static final String CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 5자 이하만 가능합니다.";
+    private static final String MOVE_RESULT_DELIMITER_FOR_CAR = " : ";
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     private final String carName;
     private CarPosition carPosition;
 
     public Car(String carName) {
+        validateCarName(carName);
         this.carName = carName;
-        this.carPosition = new CarPosition(0);
+        this.carPosition = new CarPosition(ZERO);
     }
 
     protected void move(boolean moveOrNot) {
@@ -28,7 +35,13 @@ public class Car {
 
     protected String makeMoveResultMessage() {
         return carName
-                + " : "
+                + MOVE_RESULT_DELIMITER_FOR_CAR
                 + carPosition.makePositionResultMessage();
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(CAR_NAME_ERROR_MESSAGE);
+        }
     }
 }
