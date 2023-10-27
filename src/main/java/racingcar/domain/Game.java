b805e6958761241b4eb9ec8285d9662531ae90c7
ddongjunn <ddongjunn@gmail.com>
2023-10-27 17:37:31 +0900

    feat : 자동차 객체를 생성하는 함수 개발

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -11,13 +11,15 @@ public class Game {
     public void play(){
         List<String> cars = parseCarNames(readLine());
     }
+
     public List<String> parseCarNames(String carNames){
         String regex = ",";
         return Arrays.asList(carNames.split(regex));
     }
 
     public void createCar(List<String> cars){
-
+        for(String car : cars){
+            participant.add(new Car(car, 0));
+        }
     }
-
 }
