98ec8b1e90699d99e454c34a3e714809a33cad36
park_yeongyu <sjjs7771@naver.com>
2023-10-27 17:10:45 +0900

    feat : Race 클래스에 가장 많이 move 한 횟수를 winnerRun 이라는 변수에 저장하는 함수 작성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,10 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Race {
     int trial;
-
+    int winnerRun =0;
+    List<String> winner;
     RunningMessage message = new RunningMessage();
     CarSetting carSetting = new CarSetting();
 
@@ -31,6 +33,11 @@ public class Race {
         for (Car car : cars) {
             message.printCarName(car.getCarName());
             message.printRunCount(car.getRunCount());
+            getWinnerRun(car.getRunCount());
         }
+    }
+
+    public void getWinnerRun(int runCount){
+        if(runCount > winnerRun) winnerRun = runCount;
     }
 }
