7c0ee9966cc95a0316ec62125826b02cbf0b2ed2
김승진 <ohksj77@gmail.com>
2023-10-27 23:48:42 +0900

    feat(RandomNumberGenerator): 랜덤 숫자 생성 기능 구현

---
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -0,0 +1,23 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class RandomNumberGenerator {
+
+    private static final int START_INDEX = 0;
+    private static final int MIN_NUMBER_RANGE = 0;
+    private static final int MAX_NUMBER_RANGE = 9;
+
+    public List<Integer> generate(final int size) {
+        return IntStream.range(START_INDEX, size)
+                .map(i -> generateOne())
+                .boxed()
+                .toList();
+    }
+
+    private int generateOne() {
+        return Randoms.pickNumberInRange(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE);
+    }
+}
