6e908308d8802c985779d0b50b21ba5d13f3a9ac
gyungchan Jo <namejgc@naver.com>
2023-10-27 14:12:59 +0900

    feat(InputView): 경주를 진행할 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,25 @@ import racingcar.view.validator.InputValidator;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_RACE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        String input = Console.readLine();
+        String input = readLine();
 
         InputValidator.validateInput(input);
         return input;
+    }
+
+    public int inputRaceCount() {
+        System.out.println(INPUT_RACE_COUNT_MESSAGE);
+        String input = readLine();
+
+        InputValidator.validateRaceCount(input);
+        return Integer.parseInt(input);
+    }
+
+    private String readLine() {
+        return Console.readLine();
     }
 }
