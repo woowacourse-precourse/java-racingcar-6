33a64d5e6dc8ddf99bda7a74e976db2764fa3b65
Henry-Cha <sungwon4728@naver.com>
2023-10-27 17:30:10 +0900

    feat: car 이동 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     String name;
+    int moveCount;
 
     public Car(){}
     public Car(String name){
         validName(name);
         this.name = name;
+        this.moveCount = 0;
     }
     public void validName(String name){
         if(name.length()>5) throw new IllegalArgumentException("5글자 이하만 가능");
+    }
+    public void move(){
+        if(Randoms.pickNumberInRange(0,9)>=4)
+            this.moveCount++;
     }
 }
