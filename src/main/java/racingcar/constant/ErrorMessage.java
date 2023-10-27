ae04174878653312425d4e93e7ade5f83a1e05ce
김승진 <ohksj77@gmail.com>
2023-10-27 22:53:33 +0900

    feat(InputValidator): 움직일 횟수 입력값 숫자 여부 검증 기능 구현

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.constant;
 public enum ErrorMessage {
 
     INVALID_CAR_NAME_INPUT("자동차 이름 입력값이 잘못 사용되었습니다."),
-    INVALID_CAR_NAME_LENGTH("자동차 이름의 길이가 잘못되었습니다.");
+    INVALID_CAR_NAME_LENGTH("자동차 이름의 길이가 잘못되었습니다."),
+    NOT_NUMERIC("입력값이 숫자가 아닙니다.");
 
     private final String message;
 
--- src/main/java/racingcar/io/InputValidator.java
+++ src/main/java/racingcar/io/InputValidator.java
@@ -5,6 +5,7 @@ import racingcar.constant.ErrorMessage;
 public class InputValidator {
 
     private static final String CAR_NAMES_REGEX = "^[a-zA-Z]+(,[a-zA-Z]+)*$";
+    private static final String NUMERIC_REGEX = "^[0-9]+$";
 
     public void validateRacingCarNames(final String input) {
         if (input.isBlank() || hasInvalidDelimiterUsage(input)) {
@@ -14,5 +15,11 @@ public class InputValidator {
 
     private boolean hasInvalidDelimiterUsage(final String input) {
         return !input.matches(CAR_NAMES_REGEX);
+    }
+
+    public void validateTryCount(final String input) {
+        if (!input.matches(NUMERIC_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMERIC.toValue());
+        }
     }
 }
