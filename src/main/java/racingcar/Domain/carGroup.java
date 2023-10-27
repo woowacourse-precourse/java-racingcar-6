c8396e6715a49ec30ac5da2a101aa7c27e005ad0
sami355-24 <ddung355@gmail.com>
2023-10-27 23:28:18 +0900

    feat(domain): carGroup 클래스 추가
    - 일급컬렉션으로 만들어 안정성을 높임
    - 정적 팩토리 메서드 패턴 적용
    - 생성자의 접근 제어자를 private으로 선언하여 외부에서 직접 생성하지 못하게 막음
    - carGroup의 크기 구하는 메서드 추가
    - 추가한 순서에 따라 자동차를 검색하는 기능 추가
    - 정적 팩토리 메서드 안에서 List를 Map으로 변환하는 작성 수행하여 캡슐화 하였음

--- src/main/java/racingcar/Domain/carGroup.java
+++ src/main/java/racingcar/Domain/carGroup.java
@@ -1,2 +1,30 @@
-package racingcar.Domain;public class carGroup {
+package racingcar.Domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class carGroup {
+    private final Map<Integer, car> car_group;
+
+    private carGroup(Map<Integer, car> carGroup) {
+        this.car_group = carGroup;
+    }
+
+    public static carGroup of(List<String> carGroup) {
+        Map<Integer, car> car_group = new HashMap<>();
+        for (int i = 0; i < carGroup.size(); i++) {
+            car_group.put(i+1, car.of(carGroup.get(i)));
+        }
+
+        return new carGroup(car_group);
+    }
+
+    public Integer findSize() {
+        return this.car_group.size();
+    }
+
+    public car findCarByOrder(Integer order) {
+        return this.car_group.get(order);
+    }
 }
