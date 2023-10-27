43090d8b4ce22709621bb0fea62a4d6efad0477a
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 02:34:36 +0900

    test(InputTest) : 테스트 메소드명 수정

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class InputTest {
     @Test
     @DisplayName("입력값이 빈 문자열이면 예외 발생, 그렇지 않으면 정상 종료")
-    void validateInputBlankExceptionTest() {
+    void validateInputBlank_ExceptionTest() {
         assertThatCode(() -> new InputView().validateInputBlank(""))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatCode((() -> new InputView().validateInputBlank("name1,name2,name3")))
