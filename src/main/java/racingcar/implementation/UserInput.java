c22ad6107cac78f1fdc539fa1232d83a3e0a999b
Namyejun <yejun1148@gmail.com>
2023-10-27 12:36:51 +0900

    중복과 알파벡틱에 대한 유효성 검사 로직 추가

--- src/main/java/racingcar/implementation/UserInput.java
+++ src/main/java/racingcar/implementation/UserInput.java
@@ -8,9 +8,11 @@ public class UserInput {
 
     public List<String> getCarList() {
         String input = Console.readLine();
-        List<String> carList = ValidChecker.carListCheck(input);
+        List<String> carList = ValidChecker.carListCheck(input); // 5글자 이하에 대한 유효성 검사
+        ValidChecker.isDuplicated(carList); // 중복에 대한 유효성 검사
 
         for (int i = 0; i < carList.size(); i++) {
+            ValidChecker.isAlphabetic(carList.get(i)); // 알파베틱한 것에 대한 유효성 검사
             carList.set(i, carList.get(i).concat(" : "));
         }
 
