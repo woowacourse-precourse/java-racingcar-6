3ef179c17899e099835d42a676a2d8ba2382871d
hellomatia <shuaigejp80@gmail.com>
2023-10-27 21:32:07 +0900

    feat(view): OutputView 클래스 추가
    - 이 클래스는 자동차 레이스 게임의 결과를 출력하는 기능을 제공합니다.
    - 레이스 결과, 최종 우승자 등을 출력하여 사용자에게 게임 결과를 보여줍니다.

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,43 @@
+package racingcar.view;
+
+import racingcar.domain.RaceResultDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+    private static final String RACE_RESULT_MESSAGE = "실행 결과";
+    private static final String NAME_POSITION_SEPARATOR_MESSAGE = " : ";
+    private static final String ONE_STEP_MESSAGE = "-";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String WINNER_SEPARATOR_MESSAGE = ", ";
+
+    public void printResultMessage() {
+        System.out.println(RACE_RESULT_MESSAGE);
+    }
+    public void printRaceResult(RaceResultDTO raceResultDTO) {
+        raceResultDTO.names()
+                .stream()
+                .forEach(name -> printNameAndPosition(name, raceResultDTO.position()));
+        System.out.println();
+    }
+
+    private void printNameAndPosition(String name, Map<String, Integer> positions) {
+        System.out.print(name);
+        System.out.print(NAME_POSITION_SEPARATOR_MESSAGE);
+        printPosition(positions.get(name));
+    }
+
+    private void printPosition(int position) {
+        while (position-- > 0) {
+            System.out.print(ONE_STEP_MESSAGE);
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<String> names) {
+        System.out.print(FINAL_WINNER_MESSAGE);
+        String winners = String.join(WINNER_SEPARATOR_MESSAGE, names);
+        System.out.println(winners);
+    }
+}
