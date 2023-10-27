bdc8c9e7b23022341d8a40f6eb0b4ed0fc5178b4
Chenowoo <cjsgusdn14@naver.com>
2023-10-28 00:14:47 +0900

    feat(InputValue): 게임 시작 초기 입력값을 받는 메서드 기능 구현 자동차 수, 자동차 이름, 이동 회수를 입력 받는 기능

--- src/main/java/racingcar/InputValue.java
+++ src/main/java/racingcar/InputValue.java
@@ -2,7 +2,19 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static racingcar.PrintUtil.*;
+
 public class InputValue {
+    public static void InputSettingValue() {
+        printNumberOfCarsInputMsg();
+        int inputNumberOfCars = InputNumberOfCars();
+
+        printNameOfCarInputMsg();
+        String[] carNameList = InputNameOfCars();
+
+        printMoveCountInputMsg();
+        int movingCount = InputMovingCount();
+    }
 
     public static int InputNumberOfCars() {
         int numberOfCars = Integer.parseInt(Console.readLine());
@@ -13,9 +25,11 @@ public class InputValue {
     public static String[] InputNameOfCars() {
         String userInputCarName = Console.readLine();
         InputValidation.validateSeparator(userInputCarName);
-        String[] nameOfCars = userInputCarName.split(",");
+
+        String[] nameOfCars = userInputCarName.split(MessageConst.STANDARD_SAPARATOR);
         InputValidation.validateNameOfCarsRange(nameOfCars);
         InputValidation.validateDuplicateName(nameOfCars);
+
         return nameOfCars;
     }
 
