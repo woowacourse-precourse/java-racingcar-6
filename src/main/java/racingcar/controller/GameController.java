e68005d4ef5077c3040b408072e5ca4c68e98ac4
song windows <dkclasltmf@naver.com>
2023-10-27 20:43:37 +0900

    feat: GameController에 InputHandler 의존 주입

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,15 +1,21 @@
 package racingcar.controller;
 
+import racingcar.handler.InputHandler;
 import racingcar.view.GameView;
+
+import java.util.List;
 
 public class GameController {
     private final GameView gameView;
+    private final InputHandler inputHandler;
 
-    public GameController(GameView gameView) {
+    public GameController(GameView gameView, InputHandler inputHandler) {
         this.gameView = gameView;
+        this.inputHandler = inputHandler;
     }
 
     public void startGame() {
-        
+        String carNames = gameView.inputCarNames();
+
     }
 }
