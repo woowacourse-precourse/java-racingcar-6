e75b57eb92bd01a1d1a0769d3b637cf57a263d0b
wns312 <wns312@gmail.com>
2023-10-27 09:40:13 +0900

    feat(InputView): 이동 시도 횟수 입력 및 검증 기능 구현
    변경내용
    - readAttemptCount 메소드에서 입력 및 입력 문자열 출력
    - parseInt 메소드에서 변환 및 검증

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -3,24 +3,23 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
 
 public class RacingGameController {
 
     InputView inputView = new InputView();
 
-    private Car createCarByCarName(String carName) {
-        return new Car(carName);
-    }
-
     private Cars createCarsByCarNames(List<String> carNames) {
-        List<Car> cars = carNames.stream().map(this::createCarByCarName).toList();
+        List<Car> cars = carNames.stream().map(Car::new).toList();
         return new Cars(cars);
     }
 
     public void run() {
         List<String> carNames = inputView.readCarNames();
         Cars cars = createCarsByCarNames(carNames);
+        int attemptCount = inputView.readAttemptCount();
+
 
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class InputView {
     private static final String READ_CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String READ_ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String READ_ATTEMPT_COUNT_ERROR_MESSAGE = "숫자만 입력해 주세요";
 
     private List<String> convertCarNames(String input) {
         return Arrays.stream(input.split(",")).map(String::trim).toList();
@@ -15,5 +17,19 @@ public class InputView {
         System.out.println(READ_CAR_NAMES_INPUT_MESSAGE);
         String input = Console.readLine();
         return convertCarNames(input);
+    }
+
+    private int parseInt(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(READ_ATTEMPT_COUNT_ERROR_MESSAGE);
+        }
+    }
+
+    public int readAttemptCount() {
+        System.out.println(READ_ATTEMPT_COUNT_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return parseInt(input);
     }
 }
