d0facbc5645814ec2899f51e51e12d60a380d6c5
slowcloud <sju9417@gmail.com>
2023-10-27 11:27:10 +0900

    feat: Car에서 한 칸 이동하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,14 +3,20 @@ package racingcar.domain;
 public class Car {
 
     private final String name;
+    private Integer distance;
 
     public Car(String name) {
         validateLength(name);
         this.name = name;
+        this.distance = 0;
     }
 
     private static void validateLength(String name) {
         if(name.length() > 5) throw new IllegalArgumentException();
+    }
+
+    public void move() {
+        distance++;
     }
 
 }
