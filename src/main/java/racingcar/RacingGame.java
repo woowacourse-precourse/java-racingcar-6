fa1a6fff52877543f820845fbb4ac0b9c5bd0f76
박정현 <ros28081@naver.com>
2023-10-27 22:12:33 +0900

    fix: 상수명 스타일 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -8,16 +8,16 @@ public class RacingGame {
 
     private static final int MAX_NAME_SIZE = 5;
 
-    private static final String startingPhrase = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String attemptingPhrase = "시도할 회수는 몇회인가요?";
-    private static final String resultPhrase = "실행 결과";
-    private static final String winnerPhrase = "최종 우승자 : ";
+    private static final String STARTING_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPTING_PHRASE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_PHRASE = "실행 결과";
+    private static final String WINNER_PHRASE = "최종 우승자 : ";
 
     LinkedHashMap<String, Integer> racingCars;
     int attemptingNum;
 
     public void startGame() {
-        System.out.println(startingPhrase);
+        System.out.println(STARTING_PHRASE);
     }
 
     public void playGame() {
@@ -49,7 +49,7 @@ public class RacingGame {
     }
 
     public void getAttemptingNum() {
-        System.out.println(attemptingPhrase);
+        System.out.println(ATTEMPTING_PHRASE);
         attemptingNum = Integer.parseInt(Console.readLine());
     }
 
