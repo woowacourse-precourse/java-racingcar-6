5cb576f6a8fe29223ceceade39fbac634742df18
YongJOoOo <we072200@naver.com>
2023-10-27 14:41:46 +0900

    feat: 정해진 이동 횟수만큼 전체 게임 실행하는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import racingcar.domain.Car;
 import racingcar.domain.CarService;
 import racingcar.view.UserInput;
 
@@ -11,6 +13,31 @@ public class Application {
         // 사용자 입력값
         UserInput input = inputUser();
         CarService carService = new CarService(input);
+        gameStart(carService); //게임 규격 생성
+    }
+
+    //전체 게임 횟수만큼 게임 반복 및 실행 start 기능
+    static void gameStart(CarService carService) {
+        int count = carService.input.gameCount;
+        ArrayList<Car> carsList = carService.carList;
+        //자동차 객체 임시 컨트롤
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < carsList.size(); j++) {
+                //전진, 멈춤 판별
+                boolean flag = carService.judgeRunOrStop();
+                //판별된 상태에 따른 상태 세팅
+                carService.setCurrentCar(flag, carsList.get(j));
+                //현재의 게임 객체들 상태 출력
+                carService.printCurrentCar(carsList);
+            }
+        }
+        //모든 게임 완료된 상테에서 최종 우승자 가리기
+        int winScore = carService.getMaxScore(carsList);
+        ArrayList<String> winnerName = carService.getWinnerName(carsList, winScore);
+        System.out.println("이긴 사람");
+        for (int i = 0; i < winnerName.size(); i++) {
+            System.out.print(winnerName.get(i) + ", ");
+        }
     }
 
     //1) 사용자 입력 처리 기능
