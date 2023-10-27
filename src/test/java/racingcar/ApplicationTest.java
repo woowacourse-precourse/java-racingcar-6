5451713fd5148d529f3e3c1bae5bf896c2ccc2bd
박승찬 <seungchan141414@gmail.com>
2023-10-27 22:54:01 +0900

    [Test(racing)] TDD 작성 자동차의 이동을 제어해준다.  RaceCar#movementControlCar

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.control.GameProcess;
 import racingcar.domain.CreateRandomNum;
@@ -9,6 +10,9 @@ import racingcar.domain.GameHost;
 import racingcar.domain.JudgeStandard;
 import racingcar.domain.RaceCar;
 import racingcar.view.GameStart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -18,6 +22,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static String[] nameArr;
+    private static List<RaceCar> raceCarList;
+
     private GameProcess gameProcess;
     private CreateRandomNum createRandomNum;
     private GameHost gameHost;
@@ -33,6 +40,13 @@ class ApplicationTest extends NsTest {
         judgeStandard = new JudgeStandard();
         raceCar = new RaceCar();
         gameStart = new GameStart();
+
+        nameArr = new String[]{"one", "two", "three"};
+
+        raceCarList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            raceCarList.add(new RaceCar(nameArr[i]));
+        }
     }
 
 
@@ -56,6 +70,30 @@ class ApplicationTest extends NsTest {
     }
 
     // 추가 - 박승찬
+    // ================ RaceCar.class ================
+    @DisplayName("자동차의 이동을 제어해준다.")
+    @Test
+    public void movementControlCarTest() throws Exception {
+        // given
+
+
+        // when
+        raceCarList.get(0).movementControlCar();
+        raceCarList.get(0).movementControlCar();
+        raceCarList.get(0).movementControlCar();
+        raceCarList.get(1).movementControlCar();
+        raceCarList.get(1).movementControlCar();
+        raceCarList.get(2).movementControlCar();
+
+        Integer result_one = raceCarList.get(0).getCntMovementOfCar();
+        Integer result_two = raceCarList.get(1).getCntMovementOfCar();
+        Integer result_three = raceCarList.get(2).getCntMovementOfCar();
+
+        // then
+        assertThat(result_one).isEqualTo(3);
+        assertThat(result_two).isEqualTo(2);
+        assertThat(result_three).isEqualTo(1);
+    }
 
 
     // 추가 완료 - 박승찬
