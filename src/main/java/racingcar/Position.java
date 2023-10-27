e1754476063efbf974a6163d743d6c5ed55d3120
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 16:36:47 +0900

    refactor(Position): 생성자 추가

--- src/main/java/racingcar/Position.java
+++ src/main/java/racingcar/Position.java
@@ -3,6 +3,13 @@ package racingcar;
 public class Position {
     private int value;
 
+    public Position() {
+    }
+
+    public Position(int value) {
+        this.value = value;
+    }
+
     void goForward() {
         this.value++;
     }
