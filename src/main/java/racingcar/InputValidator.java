589deebb85e60654c1ac57b0edf7a5a395bfe8d1
GaBaljaintheroom <junsu1222@naver.com>
2023-10-27 15:42:29 +0900

    refactor(InputValidator): 자동차 이름 구분자 판단 메서드 수정

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,16 +1,16 @@
 package racingcar;
 
 import racingcar.constants.ErrorMessage;
+import racingcar.constants.InputPattern;
 import racingcar.exception.DivisionCarNamesError;
 
 public class InputValidator {
 
-    private static final String DIVISION = ",";
+    private InputValidator() {
+    }
 
-    private InputValidator() {}
-
-    public static void validateDivisionCarNames(String carNames){
-        if (!carNames.contains(DIVISION)){
+    public static void validateDivisionCarNames(String carNames) {
+        if (InputPattern.isNotMatchCarNamesPattern(carNames)) {
             throw new DivisionCarNamesError(ErrorMessage.DIVISION_CAR_NAME_ERROR.getMessage());
         }
     }
