ff3c76e8947970dc22223f76bec841925762b96f
SUJI-J <suzy852@naver.com>
2023-10-27 23:38:11 +0900

    feat: 게임 시작 문구 출력

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import static racingcar.GameMessage.INPUT_CAR_MESSAGE;
+
+public class OutputView {
+    public static void printStart() {
+        System.out.println(INPUT_CAR_MESSAGE);
+    }
+}
