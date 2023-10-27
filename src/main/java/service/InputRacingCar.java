369e6abb89a34fb0a6b7b49abd6e7fb1e53e2654
sinyebin <dpqls0625@naver.com>
2023-10-27 22:44:33 +0900

    feat: 레이싱카 예외처리 레이싱 카 이름 5자 초과할 때 에러처리

--- src/main/java/service/InputRacingCar.java
+++ src/main/java/service/InputRacingCar.java
@@ -3,6 +3,7 @@ package service;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import view.ErrorMessage;
 import vo.RacingCar;
 import vo.RacingCars;
 
@@ -11,6 +12,7 @@ public class InputRacingCar {
     public static RacingCars setInputRacingCars() {
         String input = Console.readLine();
         List<String> racingCarsNames = nameSeparator(input);
+        checkLength(racingCarsNames);
         RacingCars racingCars = new RacingCars(racingCarsNames);
         return racingCars;
     }
@@ -19,5 +21,13 @@ public class InputRacingCar {
         List<String> racingCarsNames = new ArrayList<>();
         racingCarsNames = List.of(input.split(","));
         return racingCarsNames;
+    }
+
+    public static void checkLength(List<String> racingCarsNames){
+        for(String name:racingCarsNames){
+            if(name.length()>5){
+                throw new IllegalArgumentException(ErrorMessage.INPUT_LENGTH_EXCEEDED_ERROR);
+            }
+        }
     }
 }
---
+++ src/main/java/view/ErrorMessage.java
@@ -0,0 +1,6 @@
+package view;
+
+public class ErrorMessage {
+    public static String EMPTY_NAME_ERROR="이름에 빈칸있습니다.";
+    public static String INPUT_LENGTH_EXCEEDED_ERROR="5자 초과하였습니다.";
+}
