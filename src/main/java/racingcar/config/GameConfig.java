9e8e8bb3827287a1c00c661e3dddcffc0f482f5c
jinny-l <jinny.l.dev@gmail.com>
2023-10-27 20:54:30 +0900

    ✨feat: Cars 클래스 구현
    - 여러 대의 자동차를 생성하는 기능
    - 자동차들을 이동하게 하는 기능

--- src/main/java/racingcar/config/GameConfig.java
+++ src/main/java/racingcar/config/GameConfig.java
@@ -5,6 +5,7 @@ public abstract class GameConfig {
     public static final String CAR_NAME_DELIMITER = ",";
     public static final String CAR_NAME_DELIMITER_KR = "쉼표";
     public static final int CAR_NAME_MAX_LENGTH = 5;
+    public static final int CARS_MIN_SIZE = 2;
     public static final int ATTEMPTS_MIN_VALUE = 1;
     public static final int MOVING_FORWARD = 4;
     public static final int MOVING_MIN_VALUE = 0;
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,41 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.config.GameConfig;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<String> carNames) {
+        validateMinLength(carNames);
+        validateUniqueCarNames(carNames);
+        return new Cars(carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList()));
+    }
+
+    private static void validateMinLength(List<String> carNames) {
+        if (carNames.size() < GameConfig.CARS_MIN_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateUniqueCarNames(List<String> carNames) {
+        if (carNames.size() != carNames.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void moveAll(List<Integer> positions) {
+        for (int i = 0; i < cars.size(); i++) {
+            Car car = cars.get(i);
+            car.moveForward(positions.get(i));
+        }
+    }
+}
