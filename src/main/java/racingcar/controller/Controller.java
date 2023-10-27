40d3feda25f463de5815cc63873cb7a78596110a
hellomatia <shuaigejp80@gmail.com>
2023-10-27 21:33:08 +0900

    feat(controller): Controller 클래스 추가
    - 이 클래스는 자동차 레이스 게임을 제어하고 실행합니다.
    - 사용자로부터 자동차 이름 및 레이스 횟수를 입력받고, 레이스를 실행하며 결과를 출력합니다.

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.domain.RaceResultDTO;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private Car car;
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        car = new Car(inputView.readNames());
+        carRace(inputView.readNumberOfRaces());
+    }
+
+    private void carRace(int numberOfRaces) {
+        outputView.printResultMessage();
+        for (int raceCount = 0; raceCount < numberOfRaces; raceCount++) {
+            RaceResultDTO raceResultDTO = car.oneCarRace();
+            outputView.printRaceResult(raceResultDTO);
+        }
+        outputView.printWinners(car.getWinners());
+    }
+}
