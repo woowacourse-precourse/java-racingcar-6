a50fe1043a8c3dea5139247f1ec6cf783261d737
김영준 <rladudwns164@gmail.com>
2023-10-27 22:15:43 +0900

    feat(Validator) : Validator 클래스 구현
    -Validator 클래스는 이 레이싱 게임 도중 발생할 수 있는 모든 예외를 검사합니다.
    -InputRaceCount()은 게임횟수를 입력했을 때 숫자만을 입력했는지 검사합니다.
    -InputName()은 자동차 이름을 입력했을 떄 중복된 이름이 있는지, 이름이 5자를 초과하는지 검사합니다.

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,34 @@
+package racingcar.util;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import racingcar.domain.Constants;
+
+public class Validator {
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("[0-9]+");
+
+    public static void InputRaceCount(String input) {
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자를 입력하세요");
+        }
+    }
+
+    public static void InputName(List<String> cars) {
+        if (!InputNameLength(cars)) {
+            throw new IllegalArgumentException("이름이 5자를 초과를 초과하지마세요");
+        }
+        if (!InputNameDuplicatin(cars)) {
+            throw new IllegalArgumentException("중복된 이름을 입력하지마세요");
+        }
+    }
+
+    private static boolean InputNameLength(List<String> cars) {
+        return cars.stream()
+                .noneMatch(name -> name.length() > Constants.NAME_SIZE);
+    }
+
+    private static boolean InputNameDuplicatin(List<String> cars) {
+        return cars.stream().distinct().count() == cars.size();
+    }
+
+}
