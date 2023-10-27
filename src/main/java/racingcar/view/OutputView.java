dd1c61dd888c32aaaa8b3743bf5f549a3e891bf6
Haeun Jeong <gkdms4248@gmail.com>
2023-10-27 20:19:26 +0900

    feat(view): 시도할 회수 요청 메세지 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,14 @@
 package racingcar.view;
 
 public class OutputView {
-    final static String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String REQUEST_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void requestCarNamesMessage() {
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
+    }
+
+    public void requestNumberOfTryMessage() {
+        System.out.println(REQUEST_NUMBER_OF_TRY_MESSAGE);
     }
 }
