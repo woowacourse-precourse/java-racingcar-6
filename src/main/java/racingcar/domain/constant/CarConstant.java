34a6a6de99d7014e8124b8de5dda19f09789bb94
gosu <q63530@naver.com>
2023-10-27 22:38:45 +0900

    refactor(domain): 상수 클래스 이름 변경
    상수를 Car클래스에서만 사용하고 있어서 이름을 변경함
    기존
    - DomainConstant
    변경
    - CarConstant

--- src/main/java/racingcar/domain/constant/CarConstant.java
+++ src/main/java/racingcar/domain/constant/CarConstant.java
@@ -1,6 +1,6 @@
 package racingcar.domain.constant;
 
-public class DomainConstant {
+public class CarConstant {
     public static final int CAR_MOVE_CONDITION = 4;
     public static final int CAR_INIT_POSITION = 0;
 
