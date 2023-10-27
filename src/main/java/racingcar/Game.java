a72a842fe789cdc77482aeb6d2b63b910ac31147
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-27 13:00:23 +0900

    feat : 메인에 게임스타트 생성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Game {
 
-    private void gameStart(){
+    public void gameStart(){
         InputValidation inputValidation = new InputValidation();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
