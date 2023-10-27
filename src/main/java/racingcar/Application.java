65f530548ea74815cb26eb32a86618e94dceca93
jhyii <83736896+jhyii@users.noreply.github.com>
2023-10-27 17:41:01 +0900

    feat : 추가 입력받기 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,16 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 class Car{
 	
-	public static String name;
-	public static int step;
-	public static String show;
+	String name;
+	int step;
+	String show;
 	
 	public Car(String n) {
 		name = n;
@@ -14,7 +18,7 @@ class Car{
 		show = "";
 	}
 
-	public static void go() {
+	public void go() {
 		int randomNumber = Randoms.pickNumberInRange(1, 9);
 		
 		if(randomNumber >= 4) {
@@ -23,13 +27,25 @@ class Car{
 		}
     }
 	
-	public static void print() {
+	public void print() {
 		System.out.printf("%s : %s", name, show);
 	}
 }
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = sc.nextLine();
+        sc.close();
+        
+        ArrayList<String> carNames = new ArrayList<String>();
+        Collections.addAll(carNames, input.split(","));
+        int N = carNames.size();
+        
+        Car[] cars = new Car[N];
+        for(int i = 0; i < N; i++) {
+        	cars[i] = new Car(carNames.get(i));
+        }
     }
 }
