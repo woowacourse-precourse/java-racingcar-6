6c01c15404e85434350533b3f267f0065bcaaba6
U-keun Song <sukeun319@gmail.com>
2023-10-28 00:13:26 +0900

    Feat : 어플리케이션 실행 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.controller.RacingGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGameController racingGameController = Controller.generateRacingGameController();
+        racingGameController.gameStart();
     }
 }
