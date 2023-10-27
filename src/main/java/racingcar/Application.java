edef003af268c8a78201ec7ab28d3ca00631dbaa
Henry-Cha <sungwon4728@naver.com>
2023-10-27 22:46:30 +0900

    feat: main실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,8 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InOutputView view = new InOutputView();
+        RaceController controller = new RaceController(view);
+        controller.start();
     }
 }
