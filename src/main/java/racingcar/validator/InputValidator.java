b8e14d8d0af173429d772a29c4efd605de2c963f
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-27 20:32:12 +0900

    feat: 중복되는 이름이 있을 때 제거하는 기능

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,13 +1,17 @@
 package racingcar.validator;
 
+import java.util.Arrays;
+
 public class InputValidator {
     private static final String COMMA_EXCEPTION_MESSAGE = "이름은 쉼표(,)로 구분되어야 합니다";
     private static final String CARS_LENGTH_EXCEPTION_MESSAGE = "자동차는 1개 이상이어야 합니다";
+    private static final String DUPLICATED_NAME_EXCEPTION_MESSAGE = "중복되는 이름이 있습니다";
     private static final int MIN_CARS_LENGTH = 1;
 
     public static void validateCarNames(String names) {
         validateDividedByComma(names);
         validateNamesLength(names);
+        validateDuplicatedName(names);
     }
 
     private static void validateDividedByComma(String names) {
@@ -20,6 +24,13 @@ public class InputValidator {
         String[] nameArray = names.split(",");
         if (nameArray.length < MIN_CARS_LENGTH) {
             throw new IllegalArgumentException(CARS_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateDuplicatedName(String names) {
+        String[] nameArray = names.split(",");
+        if (Arrays.asList(nameArray).stream().distinct().count() != nameArray.length) {
+            throw new IllegalArgumentException(DUPLICATED_NAME_EXCEPTION_MESSAGE);
         }
     }
 
