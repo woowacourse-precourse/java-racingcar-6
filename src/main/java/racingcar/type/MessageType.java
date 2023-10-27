e72c08b90e60fdded0830c9a6a2ef5ceb4e299ce
YoonbeomSo <ybum1224@naver.com>
2023-10-27 22:06:38 +0900

    feat: 출력 메세지 정의 Enum 구현

---
+++ src/main/java/racingcar/type/MessageType.java
@@ -0,0 +1,25 @@
+package racingcar.type;
+
+/**
+ * @Enum : 자동차 경주의 출력 메세지 타입
+ */
+public enum MessageType {
+
+    START_RACING("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ASK_ATTEMPTS("시도할 회수는 몇회인가요?"),
+    RESULT("실행 결과"),
+    COLON(" : "),
+    ADVANCE("-"),
+    FINAL_WINNER("최종 우승자 : "),
+    COMMA(", ");
+
+    private final String message;
+
+    MessageType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
