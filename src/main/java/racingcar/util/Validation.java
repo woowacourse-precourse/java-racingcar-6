845fed220b0329f8d487c5196849be5bab7b7917
hellomatia <shuaigejp80@gmail.com>
2023-10-27 21:20:31 +0900

    feat(util): 입력값 유효성 검증 클래스 Validation 추가
    - 이 클래스는 사용자의 입력이 유효한지 검증합니다.
    - 만약 유효하지 않는 값이 입력 되었다면 IllegalArgumentException을 발생시키고 애플리케이션이 종료됩니다.

---
+++ src/main/java/racingcar/util/Validation.java
@@ -0,0 +1,52 @@
+package racingcar.util;
+
+import java.util.List;
+
+import static racingcar.domain.CarConstants.MAX_CAR_NAME_LENGTH;
+import static racingcar.domain.CarConstants.MIN_NUMBER_OF_RACES;
+
+public class Validation {
+
+    private static final String OVER_RANGE_LENGTH_MESSAGE = "5자리 이하 이름를 입력해주세요.";
+    private static final String NOT_UNIQUE_NAME_MESSAGE = "중복되지 않는 이름을 입력해주세요.";
+    private static final String NOT_NUMBER_MESSAGE = "숫자를 입력해주세요.";
+    private static final String BELOW_RANGE_NUMBER_MESSAGE = "1이상 숫자를 입력해주세요.";
+
+
+    public static void validateCarNames(List<String> names) {
+        if (isOverRangeLength(names)) {
+            throw new IllegalArgumentException(OVER_RANGE_LENGTH_MESSAGE);
+        }
+        if (isUniqueName(names)) {
+            throw new IllegalArgumentException(NOT_UNIQUE_NAME_MESSAGE);
+        }
+    }
+
+    private static boolean isOverRangeLength(List<String> names) {
+        return names.stream()
+                .anyMatch(name -> name.length() > MAX_CAR_NAME_LENGTH);
+    }
+
+    private static boolean isUniqueName(List<String> names) {
+        return names.stream()
+                .distinct()
+                .count() != names.size();
+    }
+
+    public static void validateNumberOfRaces(String inputString) {
+        if (isNotNumber(inputString)) {
+            throw new IllegalArgumentException(NOT_NUMBER_MESSAGE);
+        }
+        if (isBelowRange(inputString)) {
+            throw new IllegalArgumentException(BELOW_RANGE_NUMBER_MESSAGE);
+        }
+    }
+
+    private static boolean isNotNumber(String inputString) {
+        return !inputString.matches("\\d+");
+    }
+
+    private static boolean isBelowRange(String inputString) {
+        return Integer.parseInt(inputString) < MIN_NUMBER_OF_RACES;
+    }
+}
