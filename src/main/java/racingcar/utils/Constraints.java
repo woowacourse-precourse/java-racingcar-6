87afe23dcdc9fcb373f2c5795cb2f0f0e9950f4e
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-27 17:08:40 +0900

    feat(enum): 새로운 값 추가
    랜덤숫자 범위 최솟값0, 최댓값9, 기준4 추가

--- src/main/java/racingcar/utils/Constraints.java
+++ src/main/java/racingcar/utils/Constraints.java
@@ -1,11 +1,14 @@
 package racingcar.utils;
 
 public enum Constraints {
-    MAX_SIZE(5);
+    MAX_SIZE(5),
+    MIN_NUM(0),
+    MAX_NUM(9),
+    FORWARD_STANDARD(4);
 
     private final int value;
 
-    private Constraints(int value) {
+    Constraints(int value) {
         this.value = value;
     }
 
