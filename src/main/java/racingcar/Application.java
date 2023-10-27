50f5416ddef9e6f6ce662d9f392224f0b90e4373
parkchangeon <jg9870@naver.com>
2023-10-27 21:58:44 +0900

    feat(racingcar): 메인 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> carNames = InputView.getCarNames();
+        int racingRound = InputView.getRacingRound();
+
+        Race race = new Race(carNames, racingRound);
+        race.start();
+
+        ResultView.printWinners(race.getCars());
     }
 }
