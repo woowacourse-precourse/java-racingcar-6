de69d35bf67f39f2413179b5b21c6bb020d40abb
Junobee25 <wnsdhqo@naver.com>
2023-10-27 21:48:07 +0900

    feat: createRandomNumber() 메서드 구현

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,5 +1,17 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
 
+    RandomNumber randomNumber = new RandomNumber();
+
+    public int createRandomNumber() {
+        int randomNumberValue = Randoms.pickNumberInRange(0,9);
+        return randomNumberValue;
+    }
+
+    public void setRandomNumberValueToRandomNumber() {
+        randomNumber.setRandomNumber(createRandomNumber());
+    }
 }
