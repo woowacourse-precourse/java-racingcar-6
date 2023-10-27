1d47d3c31c3abe3d2758364c9683b271a2f9023a
kongji <kongji4092@gmail.com>
2023-10-27 20:12:19 +0900

    feat: 게임 시도 회수 문구 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,8 +2,13 @@ package racingcar.view;
 
 public class OutputView {
     public static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ASK_GAME_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+
     public static void printStartGame(){
         System.out.println(START_GAME_MESSAGE);
     }
 
+    public static void printAskGameRound(){
+        System.out.println(ASK_GAME_ROUND_MESSAGE);
+    }
 }
