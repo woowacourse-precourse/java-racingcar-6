bda6ce9c20dc474f8eafa677b3bb977e61808cb3
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 20:19:35 +0900

    refactor(RacingController) : startGame() 게임안의 로직들을 역할에 맞게 메서드로 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,32 +2,47 @@ package racingcar.controller;
 
 import racingcar.domain.CarFactory;
 import racingcar.domain.RacingCars;
+import racingcar.domain.RacingResult;
 import racingcar.util.Convertor;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
+    private RacingResult racingResult;
+    private RacingCars racingCars;
 
     public void startGame() {
-        OutputView.printCarNameInputMessage();
-        String carNames = InputView.inputCarNames();
-        RacingCars racingCars = new RacingCars(CarFactory.generateParticipationCarList(carNames));
-        OutputView.printTryCountInputMessage();
-        int tryCount = Convertor.convertStringToInt(InputView.inputTryCount());
-        racing(racingCars, tryCount);
-        OutputView.printWinner(racingCars.getWinner());
+        setParticipationCars();
+        racing(setTryCount());
+        OutputView.printWinner(racingResult.getWinner());
     }
 
-    private void racing(RacingCars racingCars, int tryCount) {
+    private void setParticipationCars() {
+        OutputView.printCarNameInputMessage();
+        String carNames = InputView.inputCarNames();
+        racingCars = new RacingCars(CarFactory.generateParticipationCarList(carNames));
+    }
+
+    private int setTryCount() {
+        OutputView.printTryCountInputMessage();
+        return Convertor.convertStringToInt(InputView.inputTryCount());
+    }
+
+    private void racing(int tryCount) {
         OutputView.printExecutionResultMessage();
         while (isNotRacingFinished(tryCount)) {
             racingCars.move();
-            OutputView.printResult(racingCars.getCarStatuses());
+            OutputView.printResult(getRacingResult(racingCars));
             tryCount--;
         }
     }
 
     private boolean isNotRacingFinished(int tryCount) {
         return tryCount > 0;
+    }
+
+    private RacingResult getRacingResult(RacingCars racingCars) {
+        racingResult = racingCars.createRacingResult();
+        return racingResult;
     }
 }
