86883f6329ae2513506c76c675f8a40874c7fad4
Bisi3asi <kueeng8008@naver.com>
2023-10-28 03:25:45 +0900

    test(unit): 기능별 테스트 추가
    1. 전진 정지
        * 일반 유닛
        * 모든 유닛의 전진 없이 동시 우승
        * 모든 유닛이 각 회차 모두 전진해 동시 우승
    2. 이름 예외 처리
        * 입력값 길이 초과
        * 입력값이 중복인 경우
        * 입력값이 공백인 경우
        * 구분자 이후 입력이 없는 경우
    3. 회수 예외 처리
        * 입력값이 1 이상이 아닌 경우
        * 입력값이 숫자가 아닌경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -15,18 +15,69 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("bisi,basi", "1");
+                    assertThat(output()).contains("bisi : -", "basi : ", "최종 우승자 : bisi");
+                },
+                MOVING_FORWARD, STOP // 두 자동차의 1회차 이동 난수 지정
+        );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("bisi,basi", "1");
+                    assertThat(output()).contains("bisi : ", "basi : ", "최종 우승자 : bisi, basi");
+                },
+                STOP, STOP // 두 자동차의 1회차 이동 난수 지정
+        );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("bisi,basi,bibi", "3");
+                    assertThat(output()).contains("bisi : ---", "basi : ---", "bibi : ---",
+                            "최종 우승자 : bisi, basi, bibi");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,// 세 자동차의 1회차 이동 난수 지정
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,// 세 자동차의 2회차 이동 난수 지정
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD // 세 자동차의 3회차 이동 난수 지정
         );
     }
 
     @Test
     void 이름에_대한_예외_처리() {
+        // 이름 length 5 초과
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                assertThatThrownBy(() -> runException("bisi,bassssi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        // 이름이 중복인 경우
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bisi,bisi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        // 이름이 공백인 경우
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bisi,  ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",basi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        // 구분자 이후 입력이 없는 경우
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bisi,basi,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 회수에_대한_예외_처리() {
+        // 1 이하의 숫자가 입력된 경우
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bisi,basi", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        // 숫자가 아닌 값이 입력된 경우
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bisi,basi", "hello!"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
