bffb4a1df30b3bc4fdf320f667cb249612cb926f
gyungchan Jo <namejgc@naver.com>
2023-10-27 13:37:41 +0900

    feat(InputView): 자동차 이름을 입력받는 기능 구현

--- src/main/java/racingcar/view/validator/InputValidator.java
+++ src/main/java/racingcar/view/validator/InputValidator.java
@@ -4,7 +4,7 @@ import org.junit.platform.commons.util.StringUtils;
 
 public class InputValidator {
 
-    protected static void validateInput(String input) {
+    public static void validateInput(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException("입력은 공백일 수 없습니다.");
         }
