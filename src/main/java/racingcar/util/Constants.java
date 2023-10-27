478b7539312f6204d359c3ba6a81fda95901fd05
doha <dohaahi@naver.com>
2023-10-28 01:10:02 +0900

    feat: 입/출력, 오류 메세지 상수로 작성

---
+++ src/main/java/racingcar/util/Constants.java
@@ -0,0 +1,15 @@
+package racingcar.util;
+
+public class Constants {
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String GAME_RESULT_MESSAGE = "실행 결과";
+    public static final String GAME_WINNER_MESSAGE = "최종 우승자 :";
+
+    public static final String WRONG_INPUT_CAR_NAME_CHARACTER_MASSAGE = "자동차 이름은 문자로 입력해주세요.";
+    public static final String WRONG_INPUT_CAR_NAME_LENGTH_MESSAGE = "자동차 이름은 5자 이하로 입력해주세요,";
+    public static final String WRONG_INPUT_COMMA_MESSAGE = "자동차 이름은 ','로 구분되게 입력해주세요.";
+    public static final String WRONG_INPUT_CAR_NAME_DUPLICATION_MESSAGE = "자동차 이름이 중복되지 않게 입력해주세요.";
+    public static final String WRONG_INPUT_TRY_COUNT_CHARACTER_MESSAGE = "시도 횟수는 숫자로 입력해주세요.";
+    public static final String WRONG_INPUT_TRY_COUNT_ZERO_MESSAGE = "시도 횟수는 0이 아닌 다른 숫자로 입력해주세요.";
+}
