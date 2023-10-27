13915c89bcfd88c59dc90e21778a139f28c76a90
HamoHamoHamo <hamo222@naver.com>
2023-10-27 22:54:57 +0900

    Feat : view/InputView 기능 구현
    inputName() - 자동차 이름 입력
    inputRepeat() - 시도할 회수 입력

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_REPEAT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static String inputName() {
+        System.out.println(INPUT_NAME_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputRepeat() {
+        System.out.println(INPUT_REPEAT_MESSAGE);
+        return Console.readLine();
+    }
+
+}
