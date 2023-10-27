84ccd1698ee9f1c66d02d6e89aed06cc5beceb0c
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-27 15:34:42 +0900

    feat: add a feature to get an input from console
    get lap number

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,7 @@ public class Application {
         machines.getInput();
 
         lap.showMessage();
+
+        lap.getInput();
     }
 }
--- src/main/java/racingcar/Lap.java
+++ src/main/java/racingcar/Lap.java
@@ -1,10 +1,19 @@
 package racingcar;
 
-public class Lap implements Showable{
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Lap implements Showable, Gettable {
     private static final String lapInstruction = "시도할 회수는 몇회인가요?";
+
+    private String lap;
 
     @Override
     public void showMessage() {
         System.out.println(lapInstruction);
+    }
+
+    @Override
+    public String getInput() {
+        return lap = readLine();
     }
 }
