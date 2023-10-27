f6d731e1dd8b56ce249fd520c2ece2a0d1a0b08a
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-27 19:10:41 +0900

    fix: 경주 서비스 메서드 이름 수정

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,15 +1,13 @@
 package racingcar.service;
 
 import racingcar.domain.participant.Participant;
-import racingcar.domain.race.Race;
 
 import java.util.List;
 
 public interface RaceService {
     void validateCarName(String input);
     void validateRaceCount(String input);
-
-    List<Participant> parseToParticipantList(String carNames);
+    List<Participant> parseToParticipantList(String input);
     void runRace(List<Participant> participants);
     List<Participant> filterWinner(List<Participant> participants);
 }
