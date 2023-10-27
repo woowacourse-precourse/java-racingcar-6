7bc6669157a8aa17e1166997e1909b3a3cb9409d
nahyeon99 <nahyeonee99@gmail.com>
2023-10-28 01:08:17 +0900

    feat: 자동차 경주 게임 관리 클래스 및 메인 실행 구현 1. RacingCarGame.java : 게임 시작부터 종료까지 게임 관리 기능 구현 2. Application.main() : 게임 실행 메서드 호출 구현

---
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -0,0 +1,82 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import racingcar.Console.Constants;
+import racingcar.Console.Input;
+import racingcar.Console.Output;
+import racingcar.util.CarFactory;
+
+public class RacingCarGame {
+
+    private static final  int MAX_GAME_TRY_COUNTS = 15;
+
+    private List<Car> cars;
+
+    {
+        this.cars = new ArrayList<>();
+    }
+
+    public void playRacingCarGame() {
+        this.cars = CarFactory.createCars(Input.receiveCarNames());
+        int tryCounts = validateTryCounts(Input.receiveGameTryCounts());
+        Output.printExecutionResultsMessage();
+
+        int idx = 0;
+        while (idx < tryCounts) {
+            moveRacingCar();
+            printExecutionResult();
+            idx++;
+        }
+
+        printFinalResult();
+    }
+
+    private void moveRacingCar() {
+        for (Car car : this.cars) {
+            car.tryMove();
+        }
+    }
+
+    private static int validateTryCounts(String counts) {
+        int tryCounts = 0;
+        try {
+            tryCounts = Integer.parseInt(counts);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Constants.EXCEPTION_GAME_TRY_COUNTS_FORMAT_MESSAGE);
+        }
+
+        if (tryCounts > MAX_GAME_TRY_COUNTS) {
+            throw new IllegalArgumentException(Constants.EXCEPTION_GAME_TRY_COUNTS_SIZE_MESSAGE);
+        }
+        return tryCounts;
+    }
+
+    private void printExecutionResult() {
+        Map<String, Integer> executionResultMap = cars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getMoveCounts));
+
+        Output.printExecutionResult(executionResultMap);
+    }
+
+    private void printFinalResult() {
+
+        List<String> winnerNames = new ArrayList<>();
+
+        this.cars = this.cars.stream()
+                .sorted(Comparator.comparing(Car::getMoveCounts).reversed())
+                .collect(Collectors.toList());
+
+        int maxMoveCounts = this.cars.get(0).getMoveCounts();
+
+        for(Car car : this.cars) {
+            if (car.getMoveCounts() == maxMoveCounts) {
+                winnerNames.add(car.getName());
+            }
+        }
+        Output.printFinalResult(winnerNames);
+    }
+}
