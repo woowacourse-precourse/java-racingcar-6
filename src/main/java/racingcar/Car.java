7fdbae2db38e22e6d4ca8b815f91ef48925d7b0c
devhoya97 <diajbh123@gmail.com>
2023-10-27 11:53:36 +0900

    feat(racingcar) : 자동차 객체가 스스로 이름 유효성 검사하도록 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,19 @@ package racingcar;
 
 public class Car {
     private static final String LOCATION = "-";
+    private static final int MAX_NAME_LENGTH = 5;
     private String name;
     private String location = "";
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 이름이 " + MAX_NAME_LENGTH + "보다 깁니다.");
+        }
     }
 
     public void move() {
