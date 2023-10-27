11936a53d68a36b46729afabf92278d706be0cf2
noxknow <chris2769@naver.com>
2023-10-27 17:35:20 +0900

    feat(RacingCar) : 자동차 이름에 적합한지에 대한 유효성 검사 기능

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,12 +1,27 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static racingcar.handler.ErrorHandler.INVALID_ENGLISH_KOREAN;
 
 public class RacingCar {
 
     private final List<String> carNames;
 
     public RacingCar(List<String> carNames) {
+        validateCarName(carNames);
+
         this.carNames = carNames;
+    }
+
+    public void validateCarName(List<String> carNames) {
+        String regex = "^[a-z|A-Z|ㄱ-ㅎ|가-힣]+$";
+
+        for (String carName : carNames) {
+            if (!Pattern.matches(regex, carName)) {
+                throw INVALID_ENGLISH_KOREAN.getException();
+            }
+        }
     }
 }
--- src/main/java/racingcar/handler/ErrorHandler.java
+++ src/main/java/racingcar/handler/ErrorHandler.java
@@ -3,6 +3,7 @@ package racingcar.handler;
 public enum ErrorHandler {
 
     INVALID_NUMBER("유효하지 않은 숫자입니다."),
+    INVALID_ENGLISH_KOREAN("영어와 한글만 사용할 수 있습니다."),
     INVALID_INPUT("자동차 이름은 쉼표로 구분해야 합니다.");
 
     private final String errorMessage;
