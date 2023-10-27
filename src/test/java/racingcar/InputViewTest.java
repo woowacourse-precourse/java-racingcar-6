e01393e6103b26a50c2fc292d3be06f73723c531
minjun7410 <minjun7410@gmail.com>
2023-10-27 13:32:34 +0900

    fix(InputViewTest): 입력값 변경
    * 5자 초과 예외처리 테스트가 아니므로 5자가 넘어가는 테스트 문자를 5자 이하로 변경했다.

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -19,24 +19,24 @@ public class InputViewTest {
 
     @Test
     void 자동차이름_올바른_입력_테스트() {
-        setSystemInToCustomInput("minjun, dahyeong , jason");
+        setSystemInToCustomInput("song, jone , jason");
         assertThatNoException().isThrownBy(() -> inputView.getCarList());
     }
     @Test
     void 자동차이름_입력값_공백_예외처리_테스트() {
-        setSystemInToCustomInput("minjun, , jason");
+        setSystemInToCustomInput("song, , jason");
         assertThatThrownBy(() -> inputView.getCarList()).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차이름_입력값_분리_테스트() {
-        List<String> inputViewList = inputView.splitInputByComma("minjun, dahyeong, jason");
+        List<String> inputViewList = inputView.splitInputByComma("song, jone, jason");
         assertThat(inputViewList.size()).isEqualTo(3);
     }
 
     @Test
     void 자동차이름_공백_제거_테스트() {
-        List<String> inputViewList = inputView.splitInputByComma(" minjun , dahyeong, jason ");
+        List<String> inputViewList = inputView.splitInputByComma(" song , jone, jason ");
         assertThat(inputViewList.get(0).length()).isEqualTo(6);
     }
 }
