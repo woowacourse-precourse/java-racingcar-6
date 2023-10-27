3b8bf2f3224f0546ed88abe944cdab1993903e99
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-27 20:21:28 +0900

    feat: 게임 기능 구현을 위한 클래스, 인터페이스 작성 - Game의 기능을 가지는 Game 인터페이스 - Game을 implements하는 RacingCarGame 클래스

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,6 @@
+package game;
+
+public interface Game {
+    void start();
+    void end();
+}
---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import game.Game;
+
+public class RacingCarGame implements Game {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+}
