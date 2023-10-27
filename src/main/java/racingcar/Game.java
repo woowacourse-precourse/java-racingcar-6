7ea53f63356ecc16e0a1159c9e894b5ac5c683a7
bnfkim <kgr3819@gmail.com>
2023-10-27 19:40:43 +0900

    Feat: createCars ArrayList method

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,11 +1,30 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Game {
+    Message message = new Message();
+    private static ArrayList<Car> cars;
+    private Integer gameRound;
 
     public void play() {
+        readyGame();
+    }
 
+    public void readyGame() {
+        String inputCarName = message.inputCarName();
+        gameRound = Integer.parseInt(message.inputGameRound());
+        cars = createCars(inputCarName);
+    }
+
+    public ArrayList<Car> createCars(String input) {
+        ArrayList<Car> cars = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(input, ",");
+        while (st.hasMoreTokens()){
+            cars.add(new Car(st.nextToken(), 0));
+        }
+        return cars;
     }
 
     public void startGame() {
@@ -13,14 +32,6 @@ public class Game {
     }
 
     public void endGame() {
-
-    }
-
-    public void inputCarName(){
-
-    }
-
-    public void inputGameRound(){
 
     }
 
@@ -33,6 +44,6 @@ public class Game {
     }
     public ArrayList<String> findWinner(){
         ArrayList<String> winners = new ArrayList<>();
-
+        return winners;
     }
 }
