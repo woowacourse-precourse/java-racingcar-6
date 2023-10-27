df9faf760228eca95841247d4c97c6913b33aaa7
zjacom <eephus625@icloud.com>
2023-10-27 17:47:18 +0900

    test(racingCar): 자동차 주행 기능 및 자동차 전진 기능 테스트 완료

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,9 +1,9 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
@@ -12,11 +12,23 @@ public class CarTest {
     @Test
     void 랜덤숫자_생성기_테스트() {
         int randomNumber = car.createRandomNumber();
+        // 랜덤 숫자가 0에서 9 사이에 있는 숫자인지 확인
         assertThat(randomNumber).isLessThan(10);
         assertThat(randomNumber).isGreaterThan(-1);
     }
 
-    private class Car {
+    @Test
+    void 자동차_주행_테스트() {
+        for (int i = 0; i < 5; i++) {
+            car.driving();
+        }
+        // 주행을 5번 시도했을 때 "-"를 포함하는지 확인
+        // 주행거리 문자열의 길이가 5보다 작은지 확인
+        assertThat(car.distanceDriven).as("운이 나빠서 '-'가 포함되지 않았어요!").contains("-");
+        assertThat(car.distanceDriven).hasSizeLessThanOrEqualTo(5);
+    }
+
+    public class Car {
         private String carName;
         private String distanceDriven = "";
         public Car(String carName) {
@@ -25,6 +37,17 @@ public class CarTest {
 
         public int createRandomNumber() {
             return Randoms.pickNumberInRange(0,9);
+        }
+
+        public void moveForward(int randomNum) {
+            if (randomNum >= 4) {
+                this.distanceDriven += "-";
+            }
+        }
+
+        public void driving() {
+            int randomNum = createRandomNumber();
+            moveForward(randomNum);
         }
     }
 }
