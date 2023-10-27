f6d8058164527ed67c7d9135ffba4e49415ee63b
Hoo-sung <gntjd135@gmail.com>
2023-10-27 11:02:56 +0900

    feat(GameManager): Race에 참여하는 Cars 초기화하는 initRaceCars 함수 생성

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
@@ -8,5 +9,16 @@ public class GameManager {
 
     private final RoundManager roundManager = new RoundManager();
 
+    private List<Car> cars = new ArrayList<>();
+
+    private int attempts;
+
+    private void initRaceCars(){
+        List<String> carNames = inputView.getCarNames();
+        for(String carName: carNames){
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+    }
 
 }
