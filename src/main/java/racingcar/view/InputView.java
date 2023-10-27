39c551965043a78428fcc35e20cd36a5323ab6fd
U-keun Song <sukeun319@gmail.com>
2023-10-27 13:17:39 +0900

    Feat : 입력 유효성 검사 기능 추가
    생성한 InputValidator를 이용하여 입력의 유효성을 검사하는 코드를 추가했다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,27 +1,31 @@
 package racingcar.view;
 
-import static racingcar.view.Constants.INPUT_CARS_NAME;
-import static racingcar.view.Constants.TRY_COUNT;
+import static racingcar.view.Constants.INPUT_CARS_NAME_MESSAGE;
+import static racingcar.view.Constants.TRY_COUNT_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.util.InputValidator;
+import racingcar.util.Validator;
 
 public class InputView {
+    private final InputValidator inputValidator = Validator.generateInputValidator();
 
     public List<String> getUserInputName() {
-        System.out.println(INPUT_CARS_NAME);
+        System.out.println(INPUT_CARS_NAME_MESSAGE);
         String input = Console.readLine();
 
-        return Arrays.stream(input.split(","))
+        List<String> nameList =  Arrays.stream(input.split(","))
                 .collect(Collectors.toList());
+        inputValidator.isValidList(nameList);
+
+        return nameList;
     }
 
     public Integer getUserInputCount() {
-        System.out.println(TRY_COUNT);
-        String input = Console.readLine();
-
-        return Integer.parseInt(input);
+        System.out.println(TRY_COUNT_MESSAGE);
+        return inputValidator.isNumber(Console.readLine());
     }
 }
