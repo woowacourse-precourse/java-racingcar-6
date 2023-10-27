190fcf93e30b58c061643bc8c8cffe0919db2155
shin5774 <babo5774@naver.com>
2023-10-28 01:45:13 +0900

    refactor: 중복확인 기능 수정
    - Set이 아닌 stream의 distinct로 중복을 제거해서 길이를 비교하는 방식으로 수정함.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,69 +1,69 @@
 package racingcar;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RacingGame {
-    public static final String COMMA=",";
-    private static final Pattern INTEGER=Pattern.compile("[0-9]+");
+    public static final String COMMA = ",";
+    private static final Pattern INTEGER = Pattern.compile("[0-9]+");
 
-    public RacingGame(){
+    public RacingGame() {
     }
 
-    public List<Car> inputCarNames(String input){
-        List<String> names=Arrays.stream(splitNames(input))
+    public List<Car> inputCarNames(String input) {
+        List<String> names = Arrays.stream(splitNames(input))
                 .map(this::removeWhiteSpace)
                 .collect(Collectors.toList());
 
         checkHasDuplicates(names);
 
         return names.stream()
-                .map(name->new Car(new Name(name),new Position()))
+                .map(name -> new Car(new Name(name), new Position()))
                 .collect(Collectors.toList());
     }
 
-    private String[] splitNames(String names){
+    private String[] splitNames(String names) {
         return names.split(COMMA);
     }
 
-    private String removeWhiteSpace(String name){
-        return name.replace(" ","");
+    private String removeWhiteSpace(String name) {
+        return name.replace(" ", "");
     }
 
-    private void checkHasDuplicates(List<String> names){
-        Set<String> uniqueName=new HashSet<>(names);
+    private void checkHasDuplicates(List<String> names) {
+        int uniqueSize = names.stream()
+                .distinct().toList().size();
 
-        if(names.size()!=uniqueName.size()){
+        if (names.size() != uniqueSize) {
             throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 
-    public int inputAttemptCount(String input){
+    public int inputAttemptCount(String input) {
         checkAttemptCountIsInteger(input);
         return toInt(input);
     }
 
-    private void checkAttemptCountIsInteger(String input){
-        if(!INTEGER.matcher(input).matches()){
+    private void checkAttemptCountIsInteger(String input) {
+        if (!INTEGER.matcher(input).matches()) {
             throw new IllegalArgumentException("정수를 입력해야합니다.");
         }
     }
-    private int toInt(String input){
+
+    private int toInt(String input) {
         return Integer.parseInt(input);
     }
 
-    public String printCarName(List<Car> cars){
-        StringBuilder result=new StringBuilder();
+    public String printCarName(List<Car> cars) {
+        StringBuilder result = new StringBuilder();
 
-        for(Car car:cars){
+        for (Car car : cars) {
             result.append(" ").append(car.getNameValue()).append(",");
         }
         result.deleteCharAt(0);
-        result.deleteCharAt(result.length()-1);
+        result.deleteCharAt(result.length() - 1);
 
         return result.toString();
     }
