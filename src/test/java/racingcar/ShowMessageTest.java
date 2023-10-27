491e51f489cfc982992904f4060f68fadff9f3e5
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-27 13:02:11 +0900

    test: add a test to check the showing a message feature

---
+++ src/test/java/racingcar/ShowMessageTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class ShowMessageTest {
+    private final ByteArrayOutputStream outForTest = new ByteArrayOutputStream();
+    private final PrintStream outOriginal = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outForTest));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(outOriginal);
+    }
+
+    @Test
+    public void machineNameInputInstructionMessage() {
+        Machines machines = new Machines();
+
+        machines.showMessage();
+
+        assertThat(outForTest.toString()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
