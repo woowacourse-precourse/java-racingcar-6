ce8e96613b671bb1eb8f796b3c496ce70dbf5caf
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-27 13:47:05 +0900

    feat : 자동차 객체 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", position=" + position +
+                '}';
+    }
+}
