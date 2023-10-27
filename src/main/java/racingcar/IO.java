483783a4b81cdbf0f87b393f460f41b4362ca9f1
dmdwns2 <ssreki1@naver.com>
2023-10-28 04:08:54 +0900

    [Feat] 메시지를 출력하는 Output 클래스와 그 함수들 작성

--- src/main/java/racingcar/IO.java
+++ src/main/java/racingcar/IO.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Map;
+
 public class IO {
     public String[] inputName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -23,5 +25,36 @@ public class IO {
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void outputIntro() {
+        System.out.println("실행 결과");
+    }
+
+    public void outputStatus(String[] nameArr, Map<String, Integer> nameMap) {
+        for (String s : nameArr) {
+            System.out.print(s + " : ");
+            for (int j = 0; j < nameMap.get(s); j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void outputResult(int maxVal, String[] nameArr, Map<String, Integer> nameMap) {
+        System.out.print("최종 우승자 : ");
+        boolean jointWinnerFlag = false;
+        for (String s : nameArr) {
+            if (jointWinnerFlag && nameMap.get(s) == maxVal) {
+                System.out.print(", ");
+                jointWinnerFlag = false;
+            }
+            if (nameMap.get(s) == maxVal) {
+                System.out.print(s);
+                jointWinnerFlag = true;
+            }
+        }
+        System.out.println();
     }
 }
