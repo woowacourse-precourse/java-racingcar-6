d6ac1e26ffb1eec1ec8c271c046f5bd4969ec52b
SeongUk52 <rkfka7849@gmail.com>
2023-10-27 19:11:01 +0900

    feat(OutputHandler): 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,9 +1,12 @@
 package racingcar.controller;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.TryRemains;
 import racingcar.service.CarMover;
+import racingcar.service.WinnerChecker;
 import racingcar.view.InputHandler;
 import racingcar.view.OutputHandler;
 
@@ -14,8 +17,9 @@ public class RaceController {
     public RaceController(List<Car> carList) {
         this.carList = carList;
         createTryRemains(InputHandler.inputNumberOfTry());
+        OutputHandler.printRaceProgressLabel();
         doTry();
-        // TODO 2023 10 27 최종 우승자 출력
+        printFinalWinner();
     }
 
     private void doTry() {
@@ -24,22 +28,27 @@ public class RaceController {
         }
         tryRemains.doTry();
         CarMover.move(carList);
-        printRaceProgress(carList);
-        // TODO 2023 10 27 레이스 진행상황 출력
+        printRaceProgress();
         doTry();
     }
 
-    private void printRaceProgress(List<Car> carList) {
+    private void printRaceProgress() {
         OutputHandler.printRaceProgress(carList.stream()
+                        .map(Car::getNameToString)
+                        .toList(),
+                carList.stream()
+                        .map(Car::getProgressToInt)
+                        .toList());
+    }
+
+    private void printFinalWinner() {
+        List<Car> winnerList = WinnerChecker.findWinner(carList);
+        OutputHandler.printFinalWinner(winnerList.stream()
                 .map(Car::getNameToString)
-                .toList(), carList.stream()
-                .map(Car::getProgressToInt)
-                .toList());
+                .collect(joining(", ")));
     }
 
     private void createTryRemains(String userInput) {
         tryRemains = new TryRemains(Integer.parseInt(userInput));
     }
-
-    //TODO 2023-10-27 12:37:21 CarController에 List<Car>를 반환하는 메서드를 만들고 그 리스트를 받아온다.
 }
--- src/main/java/racingcar/view/OutputHandler.java
+++ src/main/java/racingcar/view/OutputHandler.java
@@ -7,14 +7,21 @@ import java.util.stream.IntStream;
 import racingcar.system.SystemMessage;
 
 public class OutputHandler {
+    public static void printRaceProgressLabel() {
+        System.out.print(SystemMessage.RACE_RESULT.getMessage());
+    }
+
     public static void printRaceProgress(List<String> nameList, List<Integer> progressList) {
         int listSize = nameList.size();
-        System.out.print(SystemMessage.RACE_RESULT.getMessage());
         IntStream.range(0, listSize)
                 .forEach(i -> System.out.println(nameList.get(i) + " : "
                         + IntStream.range(0, progressList.get(i))
                         .mapToObj(j -> "-")
                         .collect(joining())));
         System.out.println();
+    }
+
+    public static void printFinalWinner(String winnerListString) {
+        System.out.print(SystemMessage.FINAL_WINNER.getMessage() + winnerListString);
     }
 }
