d572fe6d542a3af71351905593ade1f90ad478a1
liarreez <liarreez@gmail.com>
2023-10-27 13:08:41 +0900

    test : 무작위 값이 4이상일 경우 자동차가 한칸 전진, 4미만일 경우 자동차가 이동하지 않는 테스트 작성

--- src/test/java/racingcar/cars/CarsTest.java
+++ src/test/java/racingcar/cars/CarsTest.java
@@ -26,4 +26,24 @@ public class CarsTest {
         assertThrows(IllegalArgumentException.class,
                 () -> new Cars("lion,leopard,cow"));
     }
+
+    @Test
+    @DisplayName("무작위 값이 4이상일 경우 자동차가 한칸 전진")
+    public void testCarMovesIfRandomNumberIsGreaterThanEqual4() {
+        Cars cars = new Cars("tiger,eagle,bear");
+
+        int randomNumber = 5;
+
+        Assertions.assertThat(cars.getDistanceMoved(randomNumber)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("무작위 값이 4미만일 경우 자동차가 이동하지 않음")
+    public void testCarMovesIfRandomNumberIsLessThen4() {
+        Cars cars = new Cars("tiger,eagle,bear");
+
+        int randomNumber = 3;
+
+        Assertions.assertThat(cars.getDistanceMoved(randomNumber)).isEqualTo(0);
+    }
 }
