25195c92841998071786fa5c273f27c1964159e1
newh08 <newh08@gmail.com>
2023-10-28 06:54:53 +0900

    ✨FEAT: CarPosition, Cars 검증 추가
    CarPosition: position 에 음수값이 들어가지 못하도록 검증
    Cars: 자동차 리스트가 빈값으로 들어오지 못하도록 검증

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -7,10 +7,12 @@ import java.util.stream.Collectors;
 public class Cars {
     private static final String PREFIX = "";
     private static final String DELIMITER_AND_SUFFIX = "\n";
+    private static final String WRONG_CARS_MESSAGE = "자동차가 비어있습니다.";
 
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validateCarsIsNotEmpty();
         this.cars = cars;
     }
 
@@ -29,5 +31,11 @@ public class Cars {
         return cars.stream()
                 .map(Car::makeMoveResultMessage)
                 .collect(Collectors.joining(DELIMITER_AND_SUFFIX, PREFIX, DELIMITER_AND_SUFFIX));
+    }
+
+    private void validateCarsIsNotEmpty() {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException(WRONG_CARS_MESSAGE);
+        }
     }
 }
--- src/main/java/racingcar/domain/car/carcomponent/carposition/CarPosition.java
+++ src/main/java/racingcar/domain/car/carcomponent/carposition/CarPosition.java
@@ -2,14 +2,14 @@ package racingcar.domain.car.carcomponent.carposition;
 
 import static racingcar.Constant.ONE;
 
-import racingcar.Constant;
-
 public class CarPosition {
     private static final String RESULT_MESSAGE_COMPONENT = "-";
+    private static final String WRONG_CAR_POSITION_MESSAGE = "자동차의 위치에 음수가 들어왔습니다.";
 
     private final int position;
 
     public CarPosition(int position) {
+        validatePositionIsPositive();
         this.position = position;
     }
 
@@ -28,6 +28,12 @@ public class CarPosition {
     }
 
     public String makePositionResultMessage() {
-        return RESULT_MESSAGE_COMPONENT.repeat(Math.max(Constant.ZERO, position));
+        return RESULT_MESSAGE_COMPONENT.repeat(position);
+    }
+
+    private void validatePositionIsPositive() {
+        if (position < 0) {
+            throw new IllegalArgumentException(WRONG_CAR_POSITION_MESSAGE);
+        }
     }
 }
