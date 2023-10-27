6a374456207db663b37ce00320adcb60e5d97763
Dowon <3120kang@gmail.com>
2023-10-28 01:10:45 +0900

    Feat(Judgment): 랜덤 숫자가 4이상인지 판별하는 기능 구현

--- src/main/java/racingcar/domain/Judgment.java
+++ src/main/java/racingcar/domain/Judgment.java
@@ -6,11 +6,13 @@ public class Judgment {
 
     private static final int FIRST_VALUE=0;
     private static final int LAST_VALUE=0;
-    public int createRandomNumber() {
-        return Randoms.pickNumberInRange(FIRST_VALUE,LAST_VALUE);
-    }
+    private static final int FORWARD_CONDITION=4;
 
     public boolean isNumberFourOrMore() {
-        return false;
+        return createRandomNumber()>=FORWARD_CONDITION;
+    }
+
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(FIRST_VALUE,LAST_VALUE);
     }
 }
