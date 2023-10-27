892c4382d23d0fa6c8a3d0e2503f5654e5c7c418
plastichong <zakkslash18@naver.com>
2023-10-27 22:03:22 +0900

    feat: 전진횟수 입력 받기

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -11,6 +11,8 @@ public class GameManager {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNameList = UserInputReceiver.receiveCarNameAndGetCarNameList(Console.readLine());
         CarFactory.createCars(carNameList);
+
+        int tryTimes = UserInputReceiver.receiveTryTimes(Console.readLine());
     }
 
 }
--- src/main/java/racingcar/UserInputReceiver.java
+++ src/main/java/racingcar/UserInputReceiver.java
@@ -13,9 +13,35 @@ public class UserInputReceiver {
         return split;
     }
 
+    public static int receiveTryTimes(String rawInput) {
+
+        int inputNumber = tryParseInt(rawInput);
+
+        numberRangeValidate(inputNumber);
+
+        return inputNumber;
+    }
+
+    private static void numberRangeValidate(int inputNumber) {
+        if(inputNumber <= 0 ){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int tryParseInt(String rawInput) {
+        int parsedInt;
+        try {
+            parsedInt = Integer.parseInt(rawInput);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+        return parsedInt;
+    }
+
     private static void elementLengthValidate(List<String> split) {
-        split.forEach(e->{
-            if(e.length()>=5) throw new IllegalArgumentException();
+        split.forEach(e -> {
+            if (e.length() >= 5) throw new IllegalArgumentException();
         });
     }
 
