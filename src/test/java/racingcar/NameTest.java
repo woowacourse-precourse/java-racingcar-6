e7b5fa9ce3835e99055ab5c9fec1b7a42a7a3ea9
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-27 11:10:26 +0900

    test: 자동차 이름 테스트

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -9,18 +9,6 @@ import racingcar.model.Name;
 public class NameTest {
 
     @Test
-    @DisplayName("자동차 이름이 올바르게 입력된 경우")
-    void carNameTest() throws Exception{
-        //given
-        String value =  "pobi";
-
-        //when
-        Name name = new Name(value);
-
-        //then
-        assertThat(name.getName()).isEqualTo("pobi");
-    }
-    @Test
     @DisplayName("자동차 이름이 띄워쓰기와 함께 입력된 경우")
     void carNameTest2() throws Exception{
         //given
@@ -31,5 +19,42 @@ public class NameTest {
 
         //then
         assertThat(name.getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 띄워쓰기 여러개와 함께 입력된 경우")
+    void carNameTest3() throws Exception{
+        //given
+        String value =  "  a ";
+
+        //when
+        Name name = new Name(value);
+
+        //then
+        assertThat(name.getName()).isEqualTo("a");
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 6글자 이상인 경우")
+    void carNameTest4() throws Exception{
+        //given
+        String value =  "abcdef";
+
+        //when, then
+        assertThatThrownBy(() -> {
+            Name name = new Name(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자 이하인 경우")
+    void carNameTest5() throws Exception{
+        //given
+        String value =  "abcdef";
+
+        //when, then
+        assertThatThrownBy(() -> {
+            Name name = new Name(value);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
