eabcce53d3cae99a309f58f30c80f7450adee713
U-keun Song <sukeun319@gmail.com>
2023-10-27 13:52:31 +0900

    Comment : 상수에 주석 추가
    어디에 쓰이는 상수인지에 대한 설명을 붙여두었다.

--- src/main/java/racingcar/view/Constants.java
+++ src/main/java/racingcar/view/Constants.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
 public enum Constants {
+    /*
+    입력 안내 메시지
+     */
     INPUT_CARS_NAME_MESSAGE("경주할 자동차 이름을 입력하세.(이름은 쉼표(,) 기준으로 구분)"),
     TRY_COUNT_MESSAGE("시도할 횟수는 몇회인가요?");
     private static String message;
