7a2de3c3f5f6e63b99d3cb0e8c35fc9a23f0c30b
freekyr7529 <freekyr7529@gmail.com>
2023-10-28 06:51:17 +0900

    feat: 공백 라인 출력 메서드 추가
    공백 라인을 출력하는 printEmptyLine 메서드 추가

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -6,6 +6,10 @@ public class View {
         System.out.println(output);
     }
 
+    public void printEmptyLine() {
+        printMsg("");
+    }
+
     public void inputNamesMsg() {
         printMsg("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
