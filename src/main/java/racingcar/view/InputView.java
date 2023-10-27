da073846589f6e10b96383fc820d5cc7bd94e097
doha <dohaahi@naver.com>
2023-10-28 02:58:51 +0900

    feat: 자동차 이름, 시도 횟수 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,38 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.util.Constants;
+import racingcar.validator.InputValidator;
+
+public class InputView {
+    public List<String> inputCarNames() {
+        System.out.println(Constants.INPUT_CAR_NAMES_MESSAGE);
+        String input = readLine().trim();
+
+        InputValidator.validateNotInputComma(input);
+        InputValidator.validateInputCarNameCharacter(input);
+        InputValidator.validateInputCarNameLength(input);
+        InputValidator.validateDuplicationCarNames(input);
+
+        return converterCarNames(input);
+    }
+
+    public int inputTryCount() {
+        System.out.println(Constants.INPUT_TRY_COUNT_MESSAGE);
+        String input = readLine().trim();
+
+        InputValidator.validateInputTryCountCharacter(input);
+        InputValidator.validateInputTryCountZero(input);
+
+        return Integer.parseInt(input);
+    }
+
+    private List<String> converterCarNames(final String carNames) {
+        return Arrays.stream(readLine()
+                        .split(","))
+                .toList();
+    }
+}
