dfd65b83ac6a98e9e620cc2bd602c821d4b29b37
jaegyeongkim <dongji_11@naver.com>
2023-10-27 17:20:46 +0900

    feat(Cars): 자동차들을 컬렉션으로 갖고있는 클래스 생성

---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
