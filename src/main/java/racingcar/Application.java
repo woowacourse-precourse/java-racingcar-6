17f82c4864a8a62cfc6a79ba483b7c252d27316b
SeongJun Lee <ekdma3259@gmail.com>
2023-10-27 20:49:35 +0900

    feat: 게임 시작 전 안내문 출력
    안내문 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	
     }
+    
+	//게임 시작전 안내문 출력
+    public static void startGame() {
+    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    
 }
