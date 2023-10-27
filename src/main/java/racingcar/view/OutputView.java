cd29d436c5296b198046212e7075b3ed7d3d91fb
YongHyeon <pyh4616@naver.com>
2023-10-28 03:36:49 +0900

    Feat: (OutputView 클래스), "실행 결과" 메시지를 출력하는 기능을 printExecutionResultMessage로 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String INPUT_RACING_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
     public void printInputRacingCarNamesMessage() {
         System.out.println(INPUT_RACING_CAR_NAMES_MESSAGE);
@@ -11,5 +12,9 @@ public class OutputView {
 
     public void printInputAttemptCountMessage() {
         System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
+    }
+
+    public void printExecutionResultMessage() {
+        System.out.println(EXECUTION_RESULT_MESSAGE);
     }
 }
