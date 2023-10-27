0c4319b0f223d5e1ea8da9e15006df0e60e59a25
yeseong31 <yeseong31@naver.com>
2023-10-27 13:06:14 +0900

    refactor(Cars): 입력 값 확인 메서드 이름 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -31,18 +31,18 @@ public class Car {
     }
 
     private void validate(String name) {
-        validateInput(name);
-        validateLength(name);
+        checkInput(name);
+        checkLength(name);
     }
     
-    private void validateLength(String name) {
+    private void checkLength(String name) {
         int length = name.length();
         if (length < MINIMUM_CAR_NAME_LENGTH || length > MAXIMUM_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
         }
     }
-    
-    private void validateInput(String name) {
+
+    private void checkInput(String name) {
         if (hasBlank(name)) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
         }
