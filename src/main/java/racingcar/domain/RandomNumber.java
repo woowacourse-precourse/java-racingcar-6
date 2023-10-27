f1a399d5b25e8a41e0f2eb26d371750d2d0cf346
slowcloud <sju9417@gmail.com>
2023-10-27 20:04:23 +0900

    feat: RandomNumber에서 랜덤한 숫자를 가지는 기능 구현

---
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+
+    public static final int RANDOM_NUMBER_FROM = 0;
+    public static final int RANDOM_NUMBER_TO = 9;
+    private final Integer number;
+
+    private RandomNumber(Integer number) {
+        this.number = number;
+    }
+
+    private static RandomNumber createRandomNumber() {
+        return new RandomNumber(getRandomNumber());
+    }
+
+    private static int getRandomNumber() {
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_FROM, RANDOM_NUMBER_TO);
+    }
+
+}
