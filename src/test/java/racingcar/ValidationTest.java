6c138b24ec22f8616e7b4acc97e25c1d37c082d0
sohyun9527 <sohyun95277@gmail.com>
2023-10-27 20:41:30 +0900

    style(Validation): 패키지 이동
    util 패키지 내부로 파일 이동

--- src/test/java/racingcar/ValidationTest.java
+++ src/test/java/racingcar/ValidationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.util.Validation;
 
 class ValidationTest {
     Validation validation = new Validation();
