51cc193bbac57b0d1abc96e27cac08d3252a114f
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 14:48:43 +0900

    feat(Position): 자동차의 현재 위치를 저장, 증가, 반환하는 클래스 작성

---
+++ src/main/java/racingcar/Position.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class Position {
+    private int value;
+
+    void goForward() {
+        this.value++;
+    }
+
+    int getValue() {
+        return this.value;
+    }
+}
