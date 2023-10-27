4cd088213b3a64d2c558cc63ce193cc3a145069f
문고은 <>
2023-10-27 17:51:52 +0900

    feat: 실행 시 자동차 이름 입력 메시지 출력
    OutputTest에 출력 메시지 테스트 추가, ConsoleOutput에 자동차 이름 입력 메시지 출력 메서드 추가

---
+++ src/main/java/racingcar/ConsoleOutput.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class ConsoleOutput {
+    public void printInputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
---
+++ src/test/java/game/OutputTest.java
@@ -0,0 +1,32 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+import racingcar.RaceGame;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OutputTest {
+    @Test
+    void output_실행_시_자동차_이름_입력_메시지_출력(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        PrintStream originalOut = System.out;
+
+        try{
+            System.setOut(printStream);
+
+            RaceGame raceGame = new RaceGame();
+            raceGame.startGame();
+
+            String printedMessage = outputStream.toString();
+
+            assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", printedMessage.trim());
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
+}
