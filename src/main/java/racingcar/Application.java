26d26e3bf710ec2fcc3d9e8117712aaa7037e892
DH Jeong <wjdehgns97@gmail.com>
2023-10-27 18:59:06 +0900

    feat: n대의 자동차 이름을 쉼표(,)로 구분하여 저장하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.script.Script;
+
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Script.LINEUP.print();
+        HashMap<String, Integer> entry = lineUp();
+        // hi,my,name,is,hello,world
+
+        System.out.println(entry);
+    }
+
+    public static HashMap<String, Integer> lineUp() {
+        HashMap<String, Integer> lineUp = new HashMap<>();
+        String[] lineUpArr = Console.readLine().split(",");
+
+        for (String name : lineUpArr) {
+            lineUp.put(name, 0);
+        }
+
+        return lineUp;
     }
 }
