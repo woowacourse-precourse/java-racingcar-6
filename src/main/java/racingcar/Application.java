1a51aa29512f480206bcfe4995f9aa1789737418
신동현 <donghyeun02@gmail.com>
2023-10-27 16:48:31 +0900

    docs : 기능 명세서 및 Flow 추가(README.md)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+/**
+ * Pseudo Code
+ * 자동차에 이름 부여 가능 ( 자동차가 전진 시 이름을 같이 출력, 이름은 쉼표(,)로 구분하며 이름은 5자 이하 )
+ * 사용자가 몇 번 이동할 지 선택 ( 주어진 횟수 동안 n대의 자동차는 전진 또는 멈춤. )
+ * 전진하는 조건 : 0 ~ 9 사이 무작위 값 중 값이 4 이상 일 경우 ( 각 자동차마다 )
+ * 게임 종료 후 누가 우승했는 지 알려줌. 우승자는 >= 1
+ * 우승자가 여러 명일 경우 쉼표(,)로 구분
+ * 사용자가 잘못된 값 입력 시 : IllegalArgumentException 발생 후 애플리케이션 종료
+ * 예시 ( 각 차수별 실행 결과 )
+ * pobi : --
+ * woni : ----
+ * jun : ---
+ *
+ */
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
