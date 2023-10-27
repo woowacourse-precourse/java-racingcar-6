dae8cb0f2c0c9a0b9467b11745c9b611cece3955
devholic22 <hyunjoon.tech@gmail.com>
2023-10-27 12:29:37 +0900

    feat: Name 객체 생성
    - 이름을 의미하는 Name 객체 생성
    - 각종 예외 처리 구현 완료 (5자 이하, 공백 제거, 중복 제거)

---
+++ src/main/java/racingcar/model/Name.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Name {
+
+    private static final String NAME_SPLITTER = ",";
+    private static final int MAXIMUM_LENGTH = 5;
+
+    private final String name;
+
+    private Name(final String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    public static Name from(final String name) {
+        return new Name(name);
+    }
+
+    public static List<Name> createCarNames(final String name) {
+        String[] splitNames = name.split(NAME_SPLITTER);
+        List<String> names = List.of(splitNames);
+
+        return names.stream()
+                .map(String::trim)
+                .distinct()
+                .map(Name::from)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
