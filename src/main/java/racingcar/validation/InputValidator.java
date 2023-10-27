1aeffb453b09e6edca91689540587341cc215b52
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-27 17:24:07 +0900

    feat: 자동차 이름의 길이를 검사하는 기능 추가

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -1,6 +1,14 @@
 package racingcar.validation;
 
+import racingcar.constant.GameConfig;
+
 public class InputValidator {
 
     public InputValidator() {}
+
+    public static void validateInputNameLength(String name) {
+        if(name.length() > GameConfig.MAXIMUM_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
