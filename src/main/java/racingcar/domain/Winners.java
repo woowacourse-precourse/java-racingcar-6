29fe71b61199e400b6a5795e7ebc384fd8fb72ea
SongJwans <yhh5916@naver.com>
2023-10-28 00:30:10 +0900

    feat: 우승자를 찾고 정보를 저장하는 기능을 담당하는 Winners 구현

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winners {
+    private final List<Car> winners;
+    private final int maxDistance;
+
+    public Winners(List<Car> cars) {
+        maxDistance = calculateMaxDistance(cars);
+        winners = findWinningCars(cars, maxDistance);
+    }
+
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+    public List<String> getNames() {
+        return winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int calculateMaxDistance(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+    }
+
+    private List<Car> findWinningCars(List<Car> cars, int maxDistance) {
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
+}
