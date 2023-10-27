4a4fd30da0291599513c0ba896303cb47862bec0
shin5774 <babo5774@naver.com>
2023-10-27 16:16:27 +0900

    feat: 자동차 이름 입력 기능 구현
    - 콘솔에서 이름을 입력받는 것 부터 Car 객체에 Name이 담기는 것까지 구현함.
    - Stream을 사용하여 가시성을 향상시킴.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class Car {
+    private final Name name;
+    private final Position position;
+
+    public Car(Name name,Position position){
+        this.name=name;
+        this.position=position;
+    }
+
+    public String getNameValue(){
+        return name.getName();
+    }
+
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,14 +1,19 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class RacingGame {
     public static final String COMMA=",";
 
     private static final Pattern INTEGER=Pattern.compile("[0-9]+");
 
-    public RacingGame(){
+    private final View view;
 
+    public RacingGame(){
+        view=new View();
     }
 
     public String[] splitNames(String names){
@@ -19,5 +24,11 @@ public class RacingGame {
         if(!INTEGER.matcher(input).matches()){
             throw new IllegalArgumentException("정수를 입력해야합니다.");
         }
+    }
+
+    public List<Car> inputCarNames(){
+        return Arrays.stream(splitNames(view.inputNames()))
+                .map(name->new Car(new Name(name),new Position()))
+                .collect(Collectors.toList());
     }
 }
