cca48aaf6bf080be78960ac65259232344c8f201
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-27 15:31:49 +0900

    test: add a test for lap query message

--- src/test/java/racingcar/ShowMessageTest.java
+++ src/test/java/racingcar/ShowMessageTest.java
@@ -31,4 +31,13 @@ public class ShowMessageTest {
 
         assertThat(outForTest.toString().trim()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    @Test
+    public void lapInstructionMessage() {
+        Lap lap = new Lap();
+
+        lap.showMessage();
+
+        assertThat(outForTest.toString().trim()).isEqualTo("시도할 회수는 몇회인가요?");
+    }
 }
