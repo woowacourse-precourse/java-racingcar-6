a48a572b56d1f6a7f51f53a19ad2b66a2d914749
JAY-Winter <rubat0@naver.com>
2023-10-27 23:17:06 +0900

    feat: 랜덤 수

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -1,2 +1,22 @@
-package racingcar;public class RandomNumber {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+
+    private final int RANDOM_NUMBER_MIN = 0;
+    private final int RANDOM_NUMBER_MAX = 9;
+    private int number;
+
+    public RandomNumber() {
+        this.number = generateRandomNumber();
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
