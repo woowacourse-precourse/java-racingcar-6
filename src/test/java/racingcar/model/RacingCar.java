1dda30d732ce5c5f2e817f74112787d891a134a0
Jiyoung Choi <12201817@inha.edu>
2023-10-27 14:11:13 +0900

    feat(RacingCar) : 생성자 함수 추가 자동차 이름, 이동 거리 초기화

---
+++ src/test/java/racingcar/model/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class RacingCar {
+    private final String name;
+    private int moveDistance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.moveDistance  = 0;
+    }
+}
