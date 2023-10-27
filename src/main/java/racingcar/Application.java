dc09b440f9a00b82eeca4cf134a48c37a281f96b
totocm <typesapk8445@gmail.com>
2023-10-28 07:58:54 +0900

    Start : input -> convert -> List -> 출력 성공

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  private static IoService io = new IoService();
+  public static void main(String[] args) {
+    String input = io.gameStartInputMsg();
+    List<String> isNmaes = io.nameChangeNames(input);
+    System.out.println(isNmaes);
+    //경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)
+    //pibo,tototot,boblkd
+    //[pibo, tototot, boblkd]
+
+
+  }
 }
--- src/main/java/racingcar/IoService.java
+++ src/main/java/racingcar/IoService.java
@@ -1,17 +1,39 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class IoService {
 
   public IoService(){}
 
-  public void gameStartMsg(){
+
+  public String gameStartInputMsg(){
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String input = Console.readLine();
+    return input;
+  }public List<String> nameChangeNames(String isName){
+    List<String> isNmaes = Arrays.asList(isName.split(","));
+    return isNmaes;
+    //디버깅 완료
+  }public boolean nameExceptionCheck(){
+
+
+    return false;
   }
-  public void tryChoiceGameMsg(){
+  public String tryChoiceGameMsg(){
     System.out.println("시도할 회수는 몇회인가요?");
+    String input = Console.readLine();
+    return input;
   }
+  public Object tryGams(){
+
+    return null;
+  }
+
+
   public void resultChoiceGameMsg(){
     System.out.println("실행 결과");
   }
