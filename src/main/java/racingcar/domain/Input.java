29aa3548de00e308d35efe6851262961df9105bb
yechop <channetattoo@gmail.com>
2023-10-27 20:24:25 +0900

    feat: 중복된 이름 입력시 예외 발생

--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -3,7 +3,9 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Input {
 
@@ -15,15 +17,27 @@ public class Input {
     public List<String> getCarNames() {
         userInput = Console.readLine();
         List<String> carNames = Arrays.asList(userInput.split(","));
-        validateNameLength(carNames);
+        validateCarNames(carNames);
         return carNames;
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        validateNameLength(carNames);
+        validateDuplication(carNames);
     }
 
     private void validateNameLength(List<String> carNames) {
         for (String name : carNames) {
             if (!(name.length() <= CAR_NAME_MAX_LENGTH && name.length() >= CAR_NAME_MIN_LENGTH)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("이름은 1~5자 길이로 입력해주세요.");
             }
+        }
+    }
+
+    private void validateDuplication(List<String> carNames) {
+        Set<String> carNamesToSet = new HashSet<>(carNames);
+        if(carNamesToSet.size()!= carNames.size()){
+            throw new IllegalArgumentException("중복된 이름이 있습니다.");
         }
     }
 
