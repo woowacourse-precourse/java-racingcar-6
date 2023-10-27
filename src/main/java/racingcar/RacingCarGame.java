218cc65bff622435fb491382dc98663f3da62ee0
진환 정 <joung.jinhawn@gmail.com>
2023-10-27 14:09:29 +0900

    test(MyTest.java) 시도 횟수 입력 파트 테스트 작성

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -100,6 +100,11 @@ public class RacingCarGame {
         }
     }
 
+    public void checkMovementAmountTest(String movementAmountTest) {
+        checkMovementAmount(movementAmountTest);
+    }
+
+
     private void inputCarNameMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
--- src/test/java/racingcar/MyTest.java
+++ src/test/java/racingcar/MyTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
@@ -13,5 +14,15 @@ public class MyTest {
         testToInputSplit.splitCarNamesTest(testInput);
 
         assertThat(testToInputSplit.getCarNames()).contains("t1", "t2", "t3", "t4", "t5");
+    }
+
+    @Test
+    void 시도_횟수_입력_파트_테스트() {
+        RacingCarGame testToInputMovementAmount = new RacingCarGame();
+        String testInput = "five";
+
+        assertThatThrownBy(() -> {
+            testToInputMovementAmount.checkMovementAmountTest(testInput);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
