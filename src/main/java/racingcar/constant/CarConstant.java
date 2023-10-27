47afe649cebe8658cb5d8eb168a29369d9f5f080
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-27 20:31:11 +0900

    feat(constant):자동차 경주 게임에 대한 정보 상수화

--- src/main/java/racingcar/constant/CarConstant.java
+++ src/main/java/racingcar/constant/CarConstant.java
@@ -1,7 +1,14 @@
 package racingcar.constant;
 
 public class CarConstant {
-    public static final int MIN_CAR_SIZE = 1;
-    public static final int MAX_CAR_SIZE = 5;
-    public static final String CAR_NAME_REGEX = "^[0-9a-zA-Zㄱ-ㅎ가-힣]*$";
+    public static final int MIN_NAME_SIZE = 1;
+    public static final int MAX_NAME_SIZE = 5;
+    public static final String NAME_REGEX = "^[0-9a-zA-Zㄱ-ㅎ가-힣]*$";
+
+    public static final int MIN_MOVE_COUNT = 0;
+    public static final int MAX_MOVE_COUNT = 10000;
+    public static final int MOVE_COUNT_INITIAL = 0;
+    public static final int MAX_MOVE_CONDITION_NUMBER = 9;
+    public static final int MIN_MOVE_CONDITION_NUMBER = 0;
+    public static final int STANDARD_MOVE_CONDITION_NUMBER = 4;
 }
---
+++ src/main/java/racingcar/constant/GameMessage.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class GameMessage {
+    public final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final String REQUEST_MOVE_COUNT = "시도할 회수는 몇회인가요?";
+    public final String EXECUTION_RESULT = "실행 결과";
+    public final String WINNER = "최종 우승자";
+}
