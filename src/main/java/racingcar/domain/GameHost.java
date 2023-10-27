14a0241d7f405b0d70aa1ff8a492dc3fa347bae7
박승찬 <seungchan141414@gmail.com>
2023-10-28 06:14:06 +0900

    [Feat(racing)] 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려주는 기능 추가.

--- src/main/java/racingcar/domain/GameHost.java
+++ src/main/java/racingcar/domain/GameHost.java
@@ -1,15 +1,25 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-// TODO: 10/27/23 게임을 진행하는 게임 진행자를 생성하는 클래스.
 public class GameHost {
+    /**
+     * 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다.
+     *
+     * @param progressCarList   현제 진행중인 RaceCarList
+     * @param numOfWinCondition 승리를 위한 조건 (입력 진행숫자)
+     * @return
+     */
+    public List<RaceCar> winRaceCar(List<RaceCar> progressCarList, Integer numOfWinCondition) {
+        List<RaceCar> winRaceCarList = new ArrayList<>();
 
-    // TODO: 10/27/23 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다.
-    public List<RaceCar> winRaceCar(List<RaceCar> progressCarList){
-//        - [ ] 우승자는 한 명 이상일 수 있다.
-//        전진 카운터가 입력한만큼있으면
-//        - [ ] "-" 가 시도할 횟수만큼 있으면 우승
-        return null;
+        for (RaceCar raceCar : progressCarList) {
+            // 전진 카운터가 입력한만큼있으면
+            if (raceCar.getCntMovementOfCar() == numOfWinCondition) {
+                winRaceCarList.add(raceCar);
+            }
+        }
+        return winRaceCarList;
     }
 }
