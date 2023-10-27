07c3e99f9ea9bb8be430786dfd9c316ac27f31a7
YongHyeon <pyh4616@naver.com>
2023-10-27 18:29:29 +0900

    Feat: (OutputView 클래스), 시도할 횟수의 입력을 요청하는 메시지를 출력하는 기능을 printInputAttemptCountMessage 메서드로 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,13 @@ package racingcar.view;
 public class OutputView {
 
     private static final String INPUT_RACING_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printInputRacingCarNamesMessage() {
         System.out.println(INPUT_RACING_CAR_NAMES_MESSAGE);
+    }
+
+    public void printInputAttemptCountMessage() {
+        System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
     }
 }
