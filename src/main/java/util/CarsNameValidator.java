076676abc4db46438dbcda46bd39e7338e5a11dc
leesangjun <hubls@naver.com>
2023-10-27 23:36:12 +0900

    feat: 자동차 이름 중복 시 검증 기능

--- src/main/java/util/CarsNameValidator.java
+++ src/main/java/util/CarsNameValidator.java
@@ -1,14 +1,24 @@
 package util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarsNameValidator {
     public static void checkValidCarsName(List<String> cars) {
-
+        checkEmptyCars(cars);
+        checkDuplicateCars(cars);
     }
 
     private static void checkEmptyCars(List<String> cars) {
         if (cars.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkDuplicateCars(List<String> cars) {
+        Set<String> set = new HashSet<>(cars);
+        if (set.size() != cars.size()) {
             throw new IllegalArgumentException();
         }
     }
