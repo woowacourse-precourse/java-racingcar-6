3b0295e7b3b481779b52d4a92c4dff572c638e44
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-27 20:21:14 +0900

    Feat: 실행결과 형식에 맞게 Racing클래스의 toString 재정의

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,11 +1,46 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Validator;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Racing {
-    private final List<Car> cars;
+import static racingcar.util.Validator.validateCarNames;
 
-    public Racing(List<Car> cars) {
-        this.cars = cars;
+public class Racing {
+    private final List<Car> carList;
+    private Racing(List<Car> cars) {
+        this.carList = cars;
+    }
+
+    public Racing registerCarList(){
+        String names = Console.readLine();
+        validateCarNames(names);
+
+        List<Car> cars = makeCarList(names.split(","));
+
+        return new Racing(cars);
+    }
+
+    private List<Car> makeCarList(String[] names){
+        List<Car> cars = new ArrayList<>();
+        for(String name : names){
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for(Car car : carList){
+            sb.append(car.toString());
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
