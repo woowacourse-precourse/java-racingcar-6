a7a3a768f96ac674039211736562e9c9fb7a45de
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-27 13:55:58 +0900

    refactor: arranged

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,32 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.drive.Drive;
-import racingcar.race.Racing;
 import racingcar.driver.Driver;
+import racingcar.judge.Judge;
+import racingcar.opportunityvalidator.OpportunityValidator;
+import racingcar.race.RaceSimulator;
+
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        Driver driver = new Driver();
-        Drive drive = new Drive();
-        Racing racing = new Racing();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Driver driver = new Driver();
         String drivers = Console.readLine();
-        System.out.println("시도활 회수는 몇 회 인가요?");
+        Map<String, String> driverMap = driver.makeDriverRoster(drivers);
+
+        System.out.println("시도할 회수는 몇 회인가요?");
         String opportunity = Console.readLine();
-        System.out.println("최종 우승자 : ");
+        OpportunityValidator opportunityValidator = new OpportunityValidator();
+        opportunityValidator.validateOpportunity(opportunity);
+
+        System.out.println("실행 결과");
+        RaceSimulator raceSimulator = new RaceSimulator();
+        raceSimulator.simulateRace(driverMap, Integer.parseInt(opportunity));
+
+        Judge judge = new Judge();
+        String winners = judge.calculateWinners(driverMap);
+        System.out.println("최종 우승자 : " + winners);
     }
 }
