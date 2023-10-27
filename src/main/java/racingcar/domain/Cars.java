5884ab0cb0fe2928a3029456f5dd7f847cf8d246
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-27 20:36:23 +0900

    refactor - 책임 분리

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.dto.CarsGenerateDto;
 import racingcar.service.RandomNumberGenerator;
 import racingcar.service.Rule;
 
@@ -8,10 +10,7 @@ public class Cars {
 
     private final List<Car> cars;
 
-    public Cars(List<Car> cars, String[] carNames) {
-        for (String car : carNames) {
-            cars.add(new Car(car));
-        }
+    public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
@@ -20,5 +19,17 @@ public class Cars {
             int randomNumber = randomNumberGenerator.generateRandomNumber();
             car.move(rule, randomNumber);
         }
+    }
+
+    public static Cars generateCars(CarsGenerateDto carsGenerateDto) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carsGenerateDto.carsName()) {
+            carList.add(new Car(carName));
+        }
+        return new Cars(carList);
+    }
+
+    public List<Car> getCarsList() {
+        return new ArrayList<>(cars);
     }
 }
