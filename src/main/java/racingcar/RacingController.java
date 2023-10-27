7c25e0fb035f53318ac9b41ab6be14136481ac62
Bisi3asi <kueeng8008@naver.com>
2023-10-27 10:34:34 +0900

    feat : 1-2. 자동차 이름 검증
    1. 입력받은 값을 "," 단위로 split
    2. 이름 유효성 검증
       * 중복이 없는지
       * 길이가 5 이하인지
    3. 유효하지 않을 시 예외 발생
    4. 유효할 시 자동차 이름 리스트 리턴

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -9,13 +9,16 @@ public class RacingController {
     void launch() {
         // 1. 자동차값 입력 → 검증 → 저장
         String names = inputName();
-
-        // 2. 시도 횟수 입력 기능
-        // 3. 실행 결과 출력 기능
-        // 4. 최종 우승자 출력 기능
+        List<String> validNameList = racingService.namesTest(names);
+        // 2. 자동차 이동 횟수(차수) 값 입력, 검증, 저장
+        // 3. 차수별 경주 실행 및 결과 출력
+        // 4. 우승자 출력
     }
 
-    // 1-1. 자동차 이름 입력 기능
+    /**
+     * 1-1. 자동차 이름 입력
+     * @return 사용자 자동차 이름 입력값
+     */
     String inputName(){
         System.out.println("경주할 자동차 이름을 입력하세요.");
         return Console.readLine();
---
+++ src/main/java/racingcar/RacingService.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingService {
+    RacingRepository racingRepository = new RacingRepository();
+
+    /**
+     * 1-2. 자동차 이름 검증
+     * @param names 사용자의 자동차 이름들 입력값
+     * @return 정제된 각 자동차의 이름 리스트
+     * @throws IllegalArgumentException
+     */
+    List<String> namesTest(String names) throws IllegalArgumentException {
+        String[] namesArr = names.split(",");
+
+        boolean isInvalidLength = Arrays.stream(namesArr)
+                .anyMatch(name -> name.length() > 5);
+        boolean hasDuplicates = Arrays.stream(namesArr)
+                .distinct().count() != namesArr.length;
+
+        if (isInvalidLength || hasDuplicates) {
+            throw new IllegalArgumentException();
+        }
+        return Arrays.stream(namesArr).toList();
+    }
+}
