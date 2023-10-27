4bc9fbfb1e69a0f65fdfec2b4b233e6cfe148335
SewoongPark <lympsw12@naver.com>
2023-10-27 20:29:30 +0900

    입력 받은 자동차 이름으로 객체 생성 함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,12 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private StringBuilder progress;
+
+    public Car(String name){
+        this.name = name;
+        progress = new StringBuilder();
+    }
+
 }
--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class RaceController {
+
+    ArrayList<Car> cars = new ArrayList<>();
+
+    public void initializeCars(String inputCar){
+        String[] inputCars = inputCar.split(",");
+        for (String car : inputCars) {
+            cars.add(new Car(car));
+        }
+    }
+
+
 }
