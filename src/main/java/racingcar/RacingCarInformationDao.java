1df75940fcb646dfda82c21ac3f1fd64730d1037
junyoun99 <juny2486@naver.com>
2023-10-27 16:47:46 +0900

    feat: 자동차의 이름과 이동 횟수 정보를 담을 RacingCarInformationDao 클래스 추가

---
+++ src/main/java/racingcar/RacingCarInformationDao.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RacingCarInformationDao {
+    private Map<String, Integer> racingCarInfoDao = new HashMap<>();
+
+
+    public void put(String name, int forward) {
+        racingCarInfoDao.put(name, forward);
+    }
+
+    public int get(String name) {
+        return racingCarInfoDao.get(name);
+    }
+
+    public int size() {
+        return racingCarInfoDao.size();
+    }
+
+    public Map<String, Integer> getAll() {
+        return racingCarInfoDao;
+    }
+}
