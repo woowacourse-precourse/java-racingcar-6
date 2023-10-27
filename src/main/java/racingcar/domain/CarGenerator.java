fca9036c559d2abc41759882c8c6bd9110b95f73
Junobee25 <wnsdhqo@naver.com>
2023-10-27 16:50:00 +0900

    feat: createCarList() 메서드 구현

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -2,13 +2,19 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.car.Car;
+import java.util.HashMap;
 
 public class CarGenerator {
 
     Car car = new Car();
 
     public void createCarList() {
-
+        HashMap<String,String> carList = new HashMap<>();
+        String [] cars = splitStringByCommas();
+        for(String car : cars) {
+            carList.put(car,"");
+        }
+        car.setCarList(carList);
     }
 
     public String[] splitStringByCommas() {
