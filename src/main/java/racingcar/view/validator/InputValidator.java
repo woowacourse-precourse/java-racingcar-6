18c4343a0e92010ed73aabb5d3c5e66c331a8d3c
gyungchan Jo <namejgc@naver.com>
2023-10-27 14:10:21 +0900

    feat(InputValidator): 경주를 진행할 횟수가 숫자인지 검증하는 로직 구현

--- src/main/java/racingcar/view/validator/InputValidator.java
+++ src/main/java/racingcar/view/validator/InputValidator.java
@@ -2,11 +2,27 @@ package racingcar.view.validator;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
+    private static Pattern onlyNumbers = Pattern.compile("-?[0-9]+");
 
     public static void validateInput(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException("입력은 공백일 수 없습니다.");
         }
+    }
+
+    public static void validateRaceCount(String input) {
+        validateInput(input);
+
+        if (isNotDigit(input)) {
+            throw new IllegalArgumentException("경주를 시도할 횟수에 대한 입력은 숫자만 가능합니다.");
+        }
+    }
+
+    private static boolean isNotDigit(String input) {
+        return !onlyNumbers.matcher(input)
+                .matches();
     }
 }
--- src/test/java/racingcar/view/validator/InputValidatorTest.java
+++ src/test/java/racingcar/view/validator/InputValidatorTest.java
@@ -3,6 +3,7 @@ package racingcar.view.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -15,5 +16,14 @@ class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.validateInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "!", "12A"})
+    @DisplayName("입력이 숫자가 아니면 예외가 발생한다.")
+    void validateNumber(String input) {
+        assertThatThrownBy(() -> InputValidator.validateRaceCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("경주를 시도할 횟수에 대한 입력은 숫자만 가능합니다.");
     }
 }
