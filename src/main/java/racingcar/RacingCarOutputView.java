8b5f10b869fe4931d317a5d3d56af1ad0397a63c
kunsanglee <gosmdochee2@gmail.com>
2023-10-27 17:29:41 +0900

    feat(RacingCarOutputView): 각 차수별 실행 결과를 출력하는 기능 구현

--- src/main/java/racingcar/RacingCarOutputView.java
+++ src/main/java/racingcar/RacingCarOutputView.java
@@ -4,12 +4,24 @@ package racingcar;
 public class RacingCarOutputView {
 
     public static final String RACING_EXECUTE_MESSAGE = "실행 결과";
+    public static final String EACH_RACING_RESULT_FORMAT = "%s : %s";
+    public static final String POSITION_PROGRESS = "-";
 
     public void printStartMessage() {
         System.out.println(RACING_EXECUTE_MESSAGE);
     }
 
-    // 횟수는 밖에서 조절, Cars(Car들이 들어있는 일급컬렉션)를 넘겨받아 매 회차 레이싱 결과를 출력.
-    public void printEachRacingResult() {
+    public void printEachRacingResult(Cars cars) {
+        for (Car car : cars.getReadOnlyCarList()) {
+            String carName = car.getName();
+            int position = car.getPosition();
+            String positionProgress = convertPositionValue(position);
+            String result = String.format(EACH_RACING_RESULT_FORMAT, carName, positionProgress);
+            System.out.println(result);
+        }
+    }
+
+    private String convertPositionValue(int position) {
+        return POSITION_PROGRESS.repeat(position);
     }
 }
