710327ea86d057dd2aaf35f97f3caf643e16673f
yubin <dbqls2821@gmail.com>
2023-10-27 16:37:10 +0900

    feat: 이름의 요구사항 중 '1이상 5이하'의 상수를 enum으로 분리

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -3,8 +3,20 @@ package racingcar;
 record Name(String name) {
 
     Name {
-        if (name.length() < 1 || name.length() > 5) {
+        if (name.length() < racingcar.Name.NameRule.MIN_LENGTH.length || name.length() > racingcar.Name.NameRule.MAX_LENGTH.length) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    enum NameRule {
+        MIN_LENGTH(1),
+        MAX_LENGTH(5),
+        ;
+
+        final int length;
+
+        NameRule(int length) {
+            this.length = length;
         }
     }
 }
