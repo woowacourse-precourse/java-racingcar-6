08ff5617cd448b6be676d28b8e6343ddde7cbe0b
dltjdn <hana40004@naver.com>
2023-10-28 04:48:17 +0900

    feat(Car): 자동차 클래스와 생성자 정의
    * 자동차의 이름과 전진 횟수 저장

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private final int forwardCount;
+
+    public Car(String name){
+        this.name = name;
+        this.forwardCount = 0;
+    }
+
+}
