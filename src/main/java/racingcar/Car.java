e486af1f8c71b03075ae6ac818258a413c422a1e
zangsu <zangsu_@naver.com>
2023-10-27 16:31:54 +0900

    feat : create Car Generator using String name

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,14 @@
 package racingcar;
 
 public class Car {
+    String name;
+    int location;
+
+    public Car(String name){
+        this.name = name;
+        this.location = 0;
+    }
+
     //TODO : boolean tryMove()
 
     //TODO : @Override String toString()
