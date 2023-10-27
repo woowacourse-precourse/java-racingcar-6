19fb258603a976c9b55d2f05c8329288a7efe6e2
Hoo-sung <gntjd135@gmail.com>
2023-10-27 17:33:47 +0900

    feat(Referee): 경주에서 최대 이동거리를 계산하는 findMaxDistance 함수 생성

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,6 +1,15 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Referee {
 
-
+    private int findMaxDistance(List<Car> cars){
+        int maxDistance = 0;
+        for(Car car : cars){
+            if(maxDistance< car.getTotalMovedDistance())
+                maxDistance = car.getTotalMovedDistance();
+        }
+        return maxDistance;
+    }
 }
