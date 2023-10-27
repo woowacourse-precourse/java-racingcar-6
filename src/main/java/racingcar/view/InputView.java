a4d15b038e5a9130fad83eef186a623c0170ecc2
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-27 16:02:22 +0900

    feat(InputView) : inputCarName(), inputRounds() 메소드 추가 InputView 클래스 내 자동차이름, 라운드수 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,50 @@
 package racingcar.view;
 
-public class InputView {
+import static racingcar.constant.AllConstants.*;
+import static racingcar.constant.ExceptionMessage.*;
+import static racingcar.constant.RaceIOMessage.*;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class InputView {
+    public Map<String, Integer> inputCarName() {
+        System.out.println(CAR_NAME_INPUT);
+        String nameInput = Console.readLine();
+        // 자동차 이름 입력 관련 1차 유효성 검사
+        if (nameInput.contains(DELIMETER)) {
+            if (nameInput.charAt(0) == DELIMETER.charAt(0)
+                    || nameInput.charAt(nameInput.length()-1) == DELIMETER.charAt(0)) {
+                throw new IllegalArgumentException(NAME_LENGTH_ZERO + nameInput);
+            }
+            if (nameInput.contains(DELIMETER+DELIMETER)) {
+                throw new IllegalArgumentException(NAME_LENGTH_ZERO + nameInput);
+            }
+        }
+        // 자동차 이름 입력 관련 2차 유효성 검사
+        Map<String, Integer> carList = new LinkedHashMap<>();
+        for (String carName : nameInput.split(DELIMETER)) {
+            if (carName.length() > NAME_LENGTH_MAX) {
+                throw new IllegalArgumentException(OVER_NAME_LENGTH + carName);
+            }
+            if (carList.containsKey(carName)) {
+                throw new IllegalArgumentException(NAME_DUPLICATED + carName);
+            }
+            carList.put(carName, 0);
+        }
+        return carList;
+    }
+    public int inputRounds() {
+        System.out.println(NUMBER_INPUT);
+        String countInput = Console.readLine();
+        // 시도 횟수 유효성 검사
+        int count;
+        try {
+            count = Integer.parseInt(countInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC + countInput);
+        }
+        return count;
+    }
 }
