1247839eefea12beec37fec0b2ac264793e1e2a9
devholic22 <hyunjoon.tech@gmail.com>
2023-10-27 17:46:02 +0900

    test: 이름에 아랍어 입력 시 예외 테스트
    이름에 아랍어가 입력될 시 예외 테스트 진행

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -110,6 +110,16 @@ public class NameTest {
     }
 
     @Test
+    void 아랍어는_입력되면_안_된다_예외() {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Name name = Name.from("حسن");
+        });
+        // then
+        assertThat(exception.getMessage()).isEqualTo(CAR_NAMV_VALUE_EXCEPTION.toString());
+    }
+
+    @Test
     void 특수기호는_입력되면_안_된다() {
         // given & when
         Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
