b6f7773e86c8c0df20060d9739a09e920fbf7f7f
Hoo-sung <gntjd135@gmail.com>
2023-10-27 18:07:11 +0900

    feat(OutputView) : 우승자 출력 메시지 만드는 makeGameResultMessage 함수 생성

--- src/main/java/racingcar/domain/OutputView.java
+++ src/main/java/racingcar/domain/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
 
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
+    private final String WINNER_OUTPUT_MESSAGE = "최종 우승자 : ";
+
+    private final String DELIMITER = ", ";
+
     public void printRoundResult(List<Car> cars){
         for(Car car : cars){
             String messge = "";
@@ -16,5 +20,13 @@ public class OutputView {
             }
             System.out.println(messge);
         }
+    }
+    private String makeGameResultMessage(List<String> cars){
+        String message = WINNER_OUTPUT_MESSAGE;
+        for(int i=0;i< cars.size()-1;i++){
+            message += (cars.get(i) + DELIMITER);
+        }
+        message +=cars.get(cars.size()-1);
+        return message;
     }
 }
