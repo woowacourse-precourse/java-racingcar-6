9d8d94d0d543d2b4e2c530983aec0449475552bd
unifolio0 <boho82ha@gmail.com>
2023-10-27 21:48:16 +0900

    feat(Car) : 생성자와 전진 기능 구현

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,14 @@
+package racingcar.Model;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void moveForward() {
+        this.position += 1;
+    }
+}
