624f7b1a2c38aef8b50ea5f8b70ea37b98db2df0
brightfanta <brightfanta@gmail.com>
2023-10-27 21:31:07 +0900

    refactor: Application 클래스와 GameController 클래스에서 중복되는 변수 할당 과정 삭제 feat: 인스턴스변수 getter&setter 설정

--- src/main/java/racingcar/domain/GameController.java
+++ src/main/java/racingcar/domain/GameController.java
@@ -9,14 +9,21 @@ public class GameController {
     private Integer TryMoveForwardRandomNumber;
     private List<Car> carList;
 
+    public Integer getTryMoveForwardRandomNumber() {
+        return TryMoveForwardRandomNumber;
+    }
+
+    public void setTryMoveForwardRandomNumber(Integer tryMoveForwardRandomNumber) {
+        TryMoveForwardRandomNumber = tryMoveForwardRandomNumber;
+    }
+
     public String getCarNames() {
 
         return Console.readLine();
     }
 
-    public void splitCarNameInput(String carNamesInput, List<Car> carList) {
+    public void splitCarNameInput(String[] carNamesArray, List<Car> carList) {
 
-        String[] carNamesArray = carNamesInput.split(",");
         for (int carIndex = 0; carIndex < carNamesArray.length; carIndex++) {
             Car car = carList.get(carIndex);
             car.setName(carNamesArray[carIndex]);
