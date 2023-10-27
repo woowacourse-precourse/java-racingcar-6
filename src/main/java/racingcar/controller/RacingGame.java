e2cfa4433184f4350e8b409c16e14830a022b386
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-27 19:36:24 +0900

    Feat: 게임 기초 세팅 - 사용자 입력 및 예외처리

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,2 +1,35 @@
-package racingcar.controller;public class RacingGame {
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Game;
+import racingcar.util.Validator;
+import racingcar.view.InputView;
+
+import java.util.StringTokenizer;
+
+public class RacingGame {
+    Game game;
+    int repetitions=0;
+
+    public void startGame(){
+        game = new Game();
+
+        StringTokenizer st = new StringTokenizer(InputView.inputCalNames());
+        while (st.hasMoreTokens()){
+            String carName = st.nextToken(",");
+            Validator.validateCarLength(carName);
+            game.addCar(new Car(carName, ""));
+        }
+        String inputRepetitions = InputView.inputRepetitions();
+        Validator.validateIsNumber(inputRepetitions);
+        repetitions = Integer.parseInt(inputRepetitions);
+
+        for(int i=0;i<repetitions;i++){
+            playGame();
+        }
+    }
+
+    public void playGame(){
+        //todo 게임 시작
+    }
 }
