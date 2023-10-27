1561c44b01eaaa17a4d487a27bbad4eafb114c21
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2023-10-27 19:59:17 +0900

    feat(InputView) : 자동차 이름 입력받는 기능 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+
+public class InputView {
+    public static final int NAME_SIZE = 5;
+
+    public static String carInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String[] InputToArray(String input) throws IllegalArgumentException {
+        String[] carsName = input.split(",");
+
+        if (!isNotDuplicate(carsName)) {
+            throw new IllegalArgumentException("중복되지 않는 이름을 입력해주세요");
+        }
+
+        for (String names : carsName) {
+            if (names.length() < 1 || names.length() > NAME_SIZE) {
+                throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하여야 합니다.");
+            }
+        }
+
+        return carsName;
+    }
+
+    public static boolean isNotDuplicate(String[] s) {
+        return Arrays.stream(s).distinct().count() == s.length;
+    }
+
+}
---
+++ src/main/java/racingcar/MyTest.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MyTest {
+    @Test
+    public void ex중복여부검사함수작동() {
+        assertThat(InputView.isNotDuplicate(new String[] {"abc", "abc"}))
+                .isEqualTo(false);
+        assertThat(InputView.isNotDuplicate(new String[] {"abc", "bc"}))
+                .isEqualTo(true);
+    }
+    @Test
+    public void 공백입력예외검사작동() {
+        boolean exceptionCatched = false;
+
+        try {
+            // 오류 유발 코드
+            InputView.InputToArray("");
+
+        } catch (IllegalArgumentException e) {
+            exceptionCatched = true;
+        }
+        assertThat(exceptionCatched)
+                .isEqualTo(true);
+    }
+    @Test
+    public void 글자5자이상예외검사작동() {
+        boolean exceptionCatched = false;
+
+        try {
+            // 여기에 오류 유발 코드를 구현한다.
+            InputView.InputToArray("abcdef,agdwrh");
+
+        } catch (IllegalArgumentException e) {
+            exceptionCatched = true;
+        }
+        assertThat(exceptionCatched)
+                .isEqualTo(true);
+    }
+
+}
