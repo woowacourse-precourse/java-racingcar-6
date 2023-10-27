12df79900d6c2d7e5e3a4a64cac41f0f6ae46af1
minseo  jo <liging12@naver.com>
2023-10-27 22:16:48 +0900

    feat: 구분자 콤마(',') 를 상수로 관리
    추후 구분자가 ':', '-' 등으로 바뀔 수 있음, 또한 콤마를 여러 클래스에서 많이 쓰므로 인터페이스를 이용해 공동관리
    
    인터페이스는
    멤버 변수는 항상 `public static final` 이다.

---
+++ src/main/java/racingcar/constant/Separator.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public interface Separator {
+
+    char SEPARATOR = ',';
+    String REGEX_SEPARATOR = ",";
+    String CONSECUTIVE_SEPARATOR = ",,";
+    String KOREAN_BRACKET_SEPARATOR = "콤마(',')";
+}
