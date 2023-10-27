ae136370abc0da0b066641309e6f0aab40a1e75b
Junobee25 <wnsdhqo@naver.com>
2023-10-27 21:41:04 +0900

    feat: RandomNumber 클래스 getter/setter 추가

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,4 +1,13 @@
 package racingcar.domain;
 
 public class RandomNumber {
+    private int randomNumber;
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
 }
