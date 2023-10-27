0655caa488c66683afd4f360820139bc56286a95
noxknow <chris2769@naver.com>
2023-10-27 18:56:35 +0900

    test(RacingCarTest) : 자동차 이름이 5글자가 넘어가는 경우 예외가 발생한다

--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -2,11 +2,15 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.handler.ErrorHandler;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class RacingCarTest {
@@ -27,5 +31,14 @@ public class RacingCarTest {
         assertThatThrownBy(() -> new RacingCar(List.of(inputString.split(","))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorHandler.DUPLICATE_NUMBER.getException().getMessage());
+    }
+
+    @DisplayName("자동차 이름이 5글자가 넘어가는 경우 예외가 발생한다")
+    @ParameterizedTest(name = "[{index}] input : {0} ")
+    @ValueSource(strings = {"pobiii,pobi,woni", "pobi,BUGATTI,woni,현대"})
+    void createCarNamesWithInvalidLength(String inputString) {
+        assertThatThrownBy(() -> new RacingCar(List.of(inputString.split(","))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorHandler.INVALID_LENGTH.getException().getMessage());
     }
 }
