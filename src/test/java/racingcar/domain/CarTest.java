7c7b732888c6300dd412d74157303cc887a29606
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-27 16:44:05 +0900

    Test: 전진해도 되는지 확인하는 메소드 테스

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,7 +9,6 @@ class CarTest {
 
     @Test
     @DisplayName("toString 재정의, 전진 테스트")
-
     public void toStringTest(){
         Car pobi = new Car("pobi");
         String result = "pobi : --";
@@ -18,5 +17,20 @@ class CarTest {
 
         assertThat(pobi.toString()).isEqualTo(result);
     }
+
+    @Test
+    @DisplayName("랜덤한 수에 따라 전진해도 되는지 확인")
+    public void isOkayToGoForwardTest(){
+        Car car = new Car("철수");
+
+        assertThat(car.isOkayToGoForward(6))
+                .isEqualTo(true);
+        assertThat(car.isOkayToGoForward(3))
+                .isEqualTo(false);
+    }
+
+
+
+
 
 }
