9394beb5fb5b06191dc8ddf644ca45db8bca0f8c
zangsu <zangsu_@naver.com>
2023-10-27 15:49:30 +0900

    feat : GameBuilder.getCarsName()

---
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -0,0 +1,7 @@
+package racingcar.constant;
+
+public class ExceptionMessage {
+    public static final String CAR_NAME_CANT_NOT_BLANK = "자동차 이름은 빈 값일 수 없습니다.";
+    public static final String CAR_NAME_IS_TOO_LONG = "자동차 이름은 다섯 글자를 넘을 수 없습니다.";
+    public static final String CAR_NAME_DUPLICATED = "중복된 자동차 이름이 존재합니다.";
+}
