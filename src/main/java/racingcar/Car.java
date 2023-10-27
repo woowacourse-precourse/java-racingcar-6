cdbc8a7a4d51b2ccd3247d1eb437b8dfae0000f9
Bisi3asi <kueeng8008@naver.com>
2023-10-27 23:57:13 +0900

    mod : refactor, Car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,17 +2,21 @@ package racingcar;
 
 public class Car {
     private final String name;
-    private int score; // 이동 누적 거리 == 점수
+    private int totalMove = 0; // 이동 누적 거리 == 점수
 
-    Car(String name){
+    Car(String name) {
         this.name = name;
     }
 
-    String getName(){
-        return this.name;
+    String getName() {
+        return name;
     }
 
-    int getScore(){
-        return this.score;
+    int getTotalMove() {
+        return totalMove;
+    }
+
+    void addScore(int move) {
+        totalMove += move;
     }
 }
