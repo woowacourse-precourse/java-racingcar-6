8d07780720e85806451a20a931c3608a1f892623
Hoo-sung <gntjd135@gmail.com>
2023-10-27 17:36:53 +0900

    feat(Referee): Winner들을 판단하는 determineWinner 함수 생성

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,9 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Referee {
 
+    public List<String> determineWinner(List<Car> cars){
+        List<String> winners = new ArrayList<>();
+        int maxDistance = findMaxDistance(cars);
+        for(Car car : cars){
+            if(car.getTotalMovedDistance() == maxDistance)
+                winners.add(car.getName());
+        }
+        return winners;
+    }
     private int findMaxDistance(List<Car> cars){
         int maxDistance = 0;
         for(Car car : cars){
