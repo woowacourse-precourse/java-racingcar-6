706074774ad3bec2c529d39fd64698b11d265129
brightfanta <brightfanta@gmail.com>
2023-10-27 21:22:47 +0900

    feat: 1.입력받은 자동차 이름에서 자동차 대수를 계산하고 2. 대수 만큼 자동차 객체 생성 3. 자동차리스트 콜렉션, getter&setter 생성

--- src/main/java/racingcar/domain/GameData.java
+++ src/main/java/racingcar/domain/GameData.java
@@ -1,11 +1,34 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameData {
 
     private Integer tryRepetitionNumber;
     private List<Car> winnerList;
+    private String CarNamesInput;
+    private List<Car> carList;
+
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(int carNumber) {
+        this.carList = new ArrayList<>();
+        for (int i = 0; i < carNumber; i++) {
+            carList.add(new Car());
+        }
+    }
+
+    public String getCarNamesInput() {
+        return CarNamesInput;
+    }
+
+    public void setCarNamesInput(String carNamesInput) {
+        CarNamesInput = carNamesInput;
+    }
 
     public Integer getTryRepetitionNumber() {
         return tryRepetitionNumber;
