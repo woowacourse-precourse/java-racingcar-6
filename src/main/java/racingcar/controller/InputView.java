c62be35bce94c6824be8cdfd4d9d35820ab0fffe
slowcloud <sju9417@gmail.com>
2023-10-27 11:52:19 +0900

    feat: InputView에서 시도 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/controller/InputView.java
+++ src/main/java/racingcar/controller/InputView.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Attempt;
 import racingcar.domain.Car;
 
 import java.util.List;
 
 public class InputView {
+
+    private static final String ERR_NOT_NUMBER = "입력값이 숫자가 아닙니다.";
 
     public List<Car> getCars() {
         String line = Console.readLine();
@@ -19,6 +22,18 @@ public class InputView {
 
     private static List<String> splitNames(String line) {
         return List.of(line.split(""));
+    }
+
+    public Attempt getAttempt() {
+        String line = Console.readLine();
+        validateNumber(line);
+        return new Attempt(Integer.parseInt(line));
+    }
+
+    private static void validateNumber(String line) {
+        for(char c : line.toCharArray()) {
+            if(!Character.isDigit(c))throw new IllegalArgumentException(ERR_NOT_NUMBER);
+        }
     }
 
 }
