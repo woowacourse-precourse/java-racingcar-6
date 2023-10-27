dafa478a7dba6fd828826d2294993a4093d76597
kyoo0115 <internetkyoomin@gmail.com>
2023-10-28 01:26:28 +0900

    feat: Car 클래스 생성 및 기능 구현

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,10 +1,30 @@
 package racingcar.Model;
 
+import racingcar.Util;
+
 public class Car {
     private String name;
     private int position = 0;
 
     public Car(String name){
         this.name = name;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void go(){
+        if( Util.getRandomNum() >= 4){
+            position++;
+        }
+    }
+
+    public boolean isWinner(int winDistance){
+        return position == winDistance;
     }
 }
