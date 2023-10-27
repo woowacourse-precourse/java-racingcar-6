39d22e55fb39d1351b9587ffa8b7a9d4045d3f7d
YongJOoOo <we072200@naver.com>
2023-10-27 13:05:41 +0900

    refactor: 사용자 입력값 처리 클래스 및 메소드에 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,19 +3,34 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        inputUser();
+        // 사용자 입력값
+        UserInput input = inputUser();
     }
 
-    //1) 사용자 입력처리 기능
-    static void inputUser() {
+    //1) 사용자 입력 처리 기능
+    static UserInput inputUser() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
-        //입력값 검증 호출
-        validateInput(carName);
+
         System.out.println("시도할 회수는 몇회인가요?");
         int gameCount = Integer.parseInt(Console.readLine());
+
+        return new UserInput(carName, gameCount);
+    }
+
+}
+
+class UserInput {
+    String userInputCarName;
+    int gameCount;
+
+    UserInput(String userInputCarName, int gameCount) {
+        validateInput(userInputCarName);//여기서 확인 후 담기
+        this.userInputCarName = userInputCarName;
+        this.gameCount = gameCount;
     }
 
     //2) 사용자 입력에 대한 예외처리
@@ -33,5 +48,4 @@ public class Application {
             }
         }
     }
-
 }
