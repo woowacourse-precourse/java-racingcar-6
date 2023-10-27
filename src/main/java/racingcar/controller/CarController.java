697ecb9e13a988be385111377bdcd362db2a3924
peppone-choi <peppone.choi@gmail.com>
2023-10-27 16:17:55 +0900

    feat: CarController 메소드 분리

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -8,11 +8,14 @@ public class CarController {
 
     public List<Car> makeCar(String name) {
         ArrayList<Car> cars = new ArrayList<>();
+        makeList(name, cars);
+        return cars;
+    }
 
+    private void makeList(String name, ArrayList<Car> cars) {
         for (String s : name.split(",")) {
             Car car = new Car(s);
             cars.add(car);
         }
-        return cars;
     }
 }
