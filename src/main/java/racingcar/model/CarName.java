bf116efe1adfb79dc220009ec742334fea703159
kkkkimtaehyeon <kth1927@naver.com>
2023-10-27 20:05:06 +0900

    [model] ㄴ[CarName]
    [CarNamesToList]
    입력 받은 자동차 이름을 구분자를 기준으로 나눠서 ArrayList에 삽입

--- src/main/java/racingcar/model/CarName.java
+++ src/main/java/racingcar/model/CarName.java
@@ -1,12 +1,21 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
 
 public class CarName {
     static String carNames;
-
+    static ArrayList<String> carList= new ArrayList<>();
     public void InputCarNames(){
         carNames = Console.readLine();
     }
 
+    public void CarNamesToList(){
+        StringTokenizer cars = new StringTokenizer(carNames,",");
+        while(cars.hasMoreTokens()){
+            carList.add(cars.nextToken());
+        }
+    }
 }
