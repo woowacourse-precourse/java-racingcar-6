a3f327419467606bb2b9256368f6dbf654294890
le2sky <leehaneul990623@gmail.com>
2023-10-27 20:11:44 +0900

    feat : 이름 생성 기능 구현

---
+++ src/main/java/racingcar/entity/Name.java
@@ -0,0 +1,52 @@
+package racingcar.entity;
+
+import java.util.Objects;
+
+class Name {
+
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String UNKNOWN_NAME_MESSAGE = "알 수 없는 이름입니다.";
+    private static final String EXCEED_NAME_LENGTH_MESSAGE = "이름은 최대 " + MAX_NAME_LENGTH + "글자까지 입력할 수 있습니다.";
+
+    private final String name;
+
+    private Name(String name) {
+        this.name = name;
+    }
+
+    public static Name from(String name) {
+        validate(name);
+
+        return new Name(name);
+    }
+
+    private static void validate(String name) {
+        checkNameNonNull(name);
+        checkNameLength(name);
+    }
+
+    private static void checkNameNonNull(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException(UNKNOWN_NAME_MESSAGE);
+        }
+    }
+
+    private static void checkNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(EXCEED_NAME_LENGTH_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/racingcar/entity/NameTest.java
@@ -0,0 +1,35 @@
+package racingcar.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @DisplayName("생성 테스트")
+    @Test
+    void create() {
+        Name name = Name.from("pobi");
+        Name otherName = Name.from("pobi");
+
+        assertThat(name).isEqualTo(otherName);
+    }
+
+    @DisplayName("알 수 없는 이름(null)으로 객체를 생성할 수 없다.")
+    @Test
+    void createWithNull() {
+        assertThatThrownBy(() -> Name.from(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("알 수 없는 이름입니다.");
+    }
+
+    @DisplayName("이름의 길이는 최대 5글자이다.")
+    @Test
+    void checkNameLength() {
+        assertThatThrownBy(() -> Name.from("*".repeat(6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 최대 5글자까지 입력할 수 있습니다.");
+    }
+}
