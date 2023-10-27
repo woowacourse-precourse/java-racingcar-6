d35a11218399ecea8519bbb86ef6ca037e848928
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-27 22:31:08 +0900

    feat/racingcar: racingcar 클래스 생성, 필드,생성자,getter 작성

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class RacingCar {
+
+    private String carName;
+    private int distance;
+
+    public RacingCar(final String carName){
+        this.carName = carName;
+        this.distance = 0;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
