313ded7c110f3bf33218a54b9d82f109f7f57b8c
unifolio0 <boho82ha@gmail.com>
2023-10-27 22:35:43 +0900

    fear(Validator) : 이름이 빈칸인지 체크

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,5 +1,15 @@
 package racingcar;
 
 public class Validator {
-    
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateEmptyName(String name) {
+        if (name.length() < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
