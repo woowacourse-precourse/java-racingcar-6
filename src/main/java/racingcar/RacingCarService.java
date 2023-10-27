004ca0ad1397382e84eb65a49d473806915e0359
wooyong99 <wulovesk@naver.com>
2023-10-27 16:55:21 +0900

    feat : 이동 횟수 입력 기능

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -7,15 +7,23 @@ import racingcar.domain.RacingCars;
 
 public class RacingCarService {
     private static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private final static RacingCars cars = new RacingCars();
+    private static Integer input_cnt;
 
     public void exec() {
         createRacingCar();
+        getInputCount();
     }
 
     private void createRacingCar() {
         System.out.println(START_GAME_MESSAGE);
         String[] input_car = Console.readLine().split(",");
         Arrays.stream(input_car).forEach(e -> cars.add(new RacingCar(e)));
+    }
+
+    private void getInputCount() {
+        System.out.println(INPUT_COUNT_MESSAGE);
+        input_cnt = Integer.parseInt(Console.readLine());
     }
 }
