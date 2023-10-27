d608ce454b26d6d76abea1cbf22fa1367978bbed
elyudwo <dudqk9696@naver.com>
2023-10-27 13:25:58 +0900

    [Feat] : 자동차 이동 기능 구현
    파라미터로 들어온 무작위 값이 4 이상일 경우 위치를 1 증가시킨다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,9 +2,17 @@ package racingcar.domain;
 
 public class Car {
 
+    private int distance;
+
     public void validateCarName(String name) {
         if(name.isBlank() || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름의 길이는 1~5 사이입니다.");
+        }
+    }
+
+    public void moveCar(int randomNumber) {
+        if(randomNumber >= 4) {
+            distance++;
         }
     }
 }
