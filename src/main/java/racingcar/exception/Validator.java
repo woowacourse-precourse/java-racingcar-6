e32dde87a68b4f4a6e43023b1c956f385f6371e9
youngjunkim <kyj0703@konkuk.ac.kr>
2023-10-27 15:50:35 +0900

    feat(Validator): 자동차 이름 및 시도횟수 입력 검증 기능 추가

--- src/main/java/racingcar/exception/Validator.java
+++ src/main/java/racingcar/exception/Validator.java
@@ -2,13 +2,19 @@ package racingcar.exception;
 
 public class Validator {
 
-    public static boolean validateCarName(String carName) {
+    public static void validateCarName(String carName) {
 
-        return false;
+        if(carName.length()>5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
     }
 
-    public static boolean validateTryCount(String tryCount) {
+    public static void validateTryCount(String tryCount) {
 
-        return false;
+        try {
+            Integer.parseInt(tryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
     }
 }
