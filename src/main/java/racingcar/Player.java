d53f375ea63f24fa8c8a4c5dd0f31ef15915c43e
CJ <choijun0627@konkuk.ac.kr>
2023-10-27 16:48:37 +0900

    Feat : 자동차 이름 입력 확인하는 기능 구현
    자동차 이름 입력이 쉼표를 기준으로 구분되어 있는지 확인하는 기능
    void checkInputFormat(String) 메소드 구현
    쉼표가 연속되어 있는지, 쉼표가 입력의 마지막에 있는지 확인

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -7,9 +7,23 @@ public class Player {
     public Player() {
     }
 
-    private String inputCarName(){
-        String carName= Console.readLine();
+    private String inputCarName() {
+        String carName = Console.readLine();
         checkInputFormat(carName);
         return carName;
+    }
+
+    private void checkInputFormat(String carName) {
+        int lastStringIndex = carName.length() - 1;
+
+        for (int i = 0; i < lastStringIndex; i++) {
+            if (carName.charAt(i) == ',' && carName.charAt(i + 1) == ',') {
+                throw new IllegalArgumentException("쉼표 다음에 쉼표가 나온 잘못된 입력입니다.");
+            }
+        }
+
+        if (carName.charAt(lastStringIndex) == ',') {
+            throw new IllegalArgumentException("쉼표가 마지막에 나온 잘못된 입력입니다.");
+        }
     }
 }
