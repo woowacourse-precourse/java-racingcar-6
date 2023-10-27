af5066266743dc7ad9a7d22555dde042e9d0f059
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 01:59:13 +0900

    feat: 형식에 맞게 출력하도록 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.controller.dto.RacingResultResponse;
 import racingcar.model.Cars;
 import racingcar.util.Converter;
 import racingcar.util.NumberGenerator;
@@ -21,23 +22,28 @@ public class RacingController {
         Cars cars = getCars();
         int tryCount = getTryCount();
         NumberGenerator numberGenerator = new RandomNumberGenerator();
+        outputView.printResultMessage();
         while (tryCount-- > 0) {
             play(cars, numberGenerator);
         }
     }
 
     private Cars getCars() {
-        outputView.printInputMessage();
+        outputView.printCarNamesInputMessage();
         String carNames = inputView.readInput();
         return Cars.createFromCarNames(Converter.convertCommaSeparatedStringToList(carNames));
     }
 
     private int getTryCount() {
+        outputView.printTryCountInputMessage();
         String count = inputView.readInput();
         return Converter.convertStringToPositiveInt(count);
     }
 
     private void play(final Cars cars, final NumberGenerator numberGenerator) {
         cars.moveAll(numberGenerator);
+        cars.getCars()
+                .forEach(car -> outputView.printResult(RacingResultResponse.from(car)));
+        outputView.printNewLine();
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,38 @@
 package racingcar.view;
 
+import java.util.stream.IntStream;
+import racingcar.controller.dto.RacingResultResponse;
+
 public class OutputView {
 
-    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
+    private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
-    public void printInputMessage() {
-        System.out.println(INPUT_MESSAGE);
+    public void printCarNamesInputMessage() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+    }
+
+    public void printTryCountInputMessage() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+    }
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printResult(final RacingResultResponse result) {
+        System.out.print(result.getCarName() + " : ");
+        printPositionResult(result.getCarPosition());
+    }
+
+    private void printPositionResult(final int count) {
+        IntStream.range(0, count)
+                .forEach(i -> System.out.print("-"));
+        printNewLine();
+    }
+
+    public void printNewLine() {
+        System.out.println();
     }
 }
