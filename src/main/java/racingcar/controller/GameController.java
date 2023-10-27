f4d4e21de5dd6b7a80fee8121e89644798c1ba83
song windows <dkclasltmf@naver.com>
2023-10-27 19:29:27 +0900

    feat: GameController 클래스 생성

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.view.GameView;
+
+public class GameController {
+    private final GameView gameView;
+
+    public GameController(GameView gameView) {
+        this.gameView = gameView;
+    }
+
+    public void startGame() {
+        
+    }
+}
