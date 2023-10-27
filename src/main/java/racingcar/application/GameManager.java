8c8ca9eb011ed864a809ca979d25512dcededb8a
SeoJiWon <sjiwon4491@gmail.com>
2023-10-27 17:30:51 +0900

    feat: 레이싱 자동차, 시도 횟수 Input 로직 구현

--- src/main/java/racingcar/application/GameManager.java
+++ src/main/java/racingcar/application/GameManager.java
@@ -1,9 +1,13 @@
 package racingcar.application;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.DefaultFuelFactory;
 import racingcar.domain.FuelFactory;
 import racingcar.domain.GameTry;
+import racingcar.io.InputProcessor;
+
+import java.util.List;
 
 public class GameManager {
     private static final FuelFactory FUEL_FACTORY = new DefaultFuelFactory();
@@ -12,5 +16,23 @@ public class GameManager {
     private GameTry gameTry;
 
     public void run() {
+        readRacingCars();
+        readGameTryCount();
+    }
+
+    private void readRacingCars() {
+        final List<String> participants = InputProcessor.readParticipateCars();
+        cars = Cars.from(applyCars(participants));
+    }
+
+    private List<Car> applyCars(final List<String> participants) {
+        return participants.stream()
+                .map(Car::new)
+                .toList();
+    }
+
+    private void readGameTryCount() {
+        final int attempCount = InputProcessor.readAttemptCount();
+        gameTry = GameTry.from(attempCount);
     }
 }
