3b7dc8a77672fa9b03296153cc9caf8564c71664
alswp006 <alswp006@gmail.com>
2023-10-28 15:40:16 +0900

    사용자에게 차량 이동에 대한 결과를 출력해주는 메소드 구현

--- src/main/java/racingcar/view/PrintOutput.java
+++ src/main/java/racingcar/view/PrintOutput.java
@@ -1,4 +1,20 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+
 public class PrintOutput {
+    public static void printMove(int move){
+        for (int i = 0; i<move; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+    public static void printAllMove(List<Car> car_list){
+        for (int i = 0; i<car_list.size(); i++){
+            System.out.print(car_list.get(i).getName() + " : ");
+            printMove(car_list.get(i).getMove());
+        }
+    }
 }
