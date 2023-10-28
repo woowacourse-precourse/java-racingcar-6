3a9e66052e8e56a7ff6fb9a9c8982f6d4c9b04c9
gusdn0108 <gusdn6671@naver.com>
2023-10-28 14:06:50 +0900

    feat(InputView) : 사용자 입력기능 생성

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String userInput() {
+        return Console.readLine();
+    }
+
+}
