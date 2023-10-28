df13f992ced5e6b808b41362f6a44c9187a54bc9
dong2ast <kdh01132006@naver.com>
2023-10-28 20:22:13 +0900

    ♻️ refactor: progress 변수를 Integer가 아닌 String으로 변경 Racing 진행중에 바로 결과를 출력할 수 있도록 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,16 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final String name;
-    private Integer progress;
+    private String progress;
 
     public Car(String name) {
         this.name = name;
-        this.progress = 0;
+        this.progress = "";
     }
 
     public void checkForward() {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            this.progress += 1;
+            this.progress += "-";
         }
     }
 
@@ -21,7 +21,7 @@ public class Car {
         return name;
     }
 
-    public Integer getProgress() {
+    public String getProgress() {
         return progress;
     }
 }
