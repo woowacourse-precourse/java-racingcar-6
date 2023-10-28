1121b93a373d0a78d7078a7af2723d61990ad7e0
KimDongEon <gold3987@naver.com>
2023-10-29 00:49:00 +0900

    feat : 경기 진행 기능 구현
    자동차 초기화
    - 자동차 전진 횟수를 저장하는 1차원 배열 forwardArr 생성
    - 실행 결과를 표시할 String List lainList 생성
    
    경기 진행
    - 입력 받은 숫자 만큼 진행
    - 랜덤 숫자를 입력 받아 4이상이면 전진, 미만이면 제자리
    - 전진이면 해당 자동차의 forwardArr +1, lainList에 - 추가
    - 매 라운드가 끝나면 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -10,22 +11,68 @@ public class Application {
 
   // 자동차 이름을 담을 List
   static List<String> carList;
-
   // 진행할 round 갯수
   static int round;
 
+  // 자동차 갯수
+  static int n;
+
+  // 각 자동차의 경주 상태
+  static int[] forwardArr;
+  static List<String> lainList;
+
+
   public static void main(String[] args) {
     // TODO: 프로그램 구현
-
-    carList = new ArrayList<>();
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
     // 자동차 이름 입력
+    carList = new ArrayList<>();
     carList = inputCar();
+
+    System.out.println("시도할 회수는 몇회인가요?");
+
     // 경기 진행 횟수 입력
     round = inputRound();
 
+    n = carList.size();
+
+    // 경기 진행
+
+    // 자동차 경로 초기화
+    forwardArr = new int[n];
+    lainList = new ArrayList<>();
+    for(int i = 0; i < n; i++) {
+      String lain = carList.get(i) + " : ";
+      lainList.add(lain);
+    }
+
+    // 경기 시작
+    System.out.println("실행 결과");
+    for(int i = 0; i < round; i++) {
+      race();
+    }
+
+
+
 
   }
+
+  // 한 라운드 경기 진행
+  public static void race() {
+    for(int i = 0; i < n; i++) {
+      int number = Randoms.pickNumberInRange(0,9);
+      // 4 이상일 경우 전진
+      if(number >= 4) {
+        forwardArr[i] += 1;
+        lainList.set(i, lainList.get(i) + "-");
+      }
+      System.out.println(lainList.get(i));
+    }
+    System.out.println();
+  }
+
+
 
   // 자동차 이름 입력
   public static List<String> inputCar() {
