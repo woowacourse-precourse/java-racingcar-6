ff4cfac9860d53d4d1ee178fdb1d72ca7cfb216e
Koo-EunSung <ku903@naver.com>
2023-10-28 23:48:53 +0900

    Feat: 입력받을 때의 문구 출력

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class InputView {
+    public void carName() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void tryCnt() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
+}
