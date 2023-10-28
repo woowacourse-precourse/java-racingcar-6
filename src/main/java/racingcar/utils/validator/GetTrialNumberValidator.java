80b99075a56a006b3998a82cb3526cf2e0c89696
suhyun9764 <suhyun9764@naver.com>
2023-10-28 19:43:53 +0900

    refactor(GetTrialNumberValidator): 예외 문구 추가

--- src/main/java/racingcar/utils/validator/GetTrialNumberValidator.java
+++ src/main/java/racingcar/utils/validator/GetTrialNumberValidator.java
@@ -4,7 +4,7 @@ public class GetTrialNumberValidator {
 
     public static void validateNumberRange(int trialNumber) {
         if(trialNumber<1||trialNumber>100){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1부터 100까지의 숫자만 입력해주세요");
         }
     }
 
