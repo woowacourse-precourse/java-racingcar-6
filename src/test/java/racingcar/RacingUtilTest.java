55854f2e4ee6667a08162934d5865e3a561fc0d0
Oseuseu <lakingsung@gmail.com>
2023-10-29 01:10:50 +0900

    test: createCarsFromNames 메서드 테스트 코드 작성

--- src/test/java/racingcar/RacingUtilTest.java
+++ src/test/java/racingcar/RacingUtilTest.java
@@ -2,7 +2,11 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingUtilTest {
     @Test
@@ -10,5 +14,16 @@ public class RacingUtilTest {
         int result = RacingUtil.generateRandomPower();
 
         assertThat(result).isBetween(0, 9);
+    }
+
+    @Test
+    void createCarsFromNames_테스트() {
+        List<String> inputList = Arrays.asList("red", "green", "blue");
+
+        List<Car> resultList = RacingUtil.createCarsFromNames(inputList);
+
+        assertEquals(inputList.get(0), resultList.get(0).getName());
+        assertEquals(inputList.get(1), resultList.get(1).getName());
+        assertEquals(inputList.get(2), resultList.get(2).getName());
     }
 }
