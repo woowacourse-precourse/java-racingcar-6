ebb7c8ecb01b0dd012c760c185402e1a1951521d
2gyeong <knk222@naver.com>
2023-10-28 15:09:02 +0900

    feat : 우승자 출력 완료

--- src/main/java/racingcar/domain/Play.java
+++ src/main/java/racingcar/domain/Play.java
@@ -24,6 +24,8 @@ public class Play {
             printCarPositions(carPositions);
             System.out.println();
         }
+        List<String> winners = getWinners(carPositions);
+        printWinners(winners);
 
     }
 
@@ -53,5 +55,22 @@ public class Play {
         }
     }
 
+    public static List<String> getWinners(Map<String, Integer> carPositions) {
+        int maxPosition = carPositions.values().stream().mapToInt(Integer::intValue).max().orElse(0);
+        return carPositions.entrySet().stream()
+                .filter(entry -> entry.getValue() == maxPosition)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.print("최종 우승자: ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i));
+            if (i < winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
 
 }
