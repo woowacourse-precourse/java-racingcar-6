c3c1ff2dcb75ac3cc0e91807278db9be4775b895
김연진 <love_homebody@naver.com>
2023-10-28 19:25:13 +0900

    feat(CarController): 최종 우승자 계산 및 출력 기능 구현

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,6 +1,6 @@
 package racingcar.controller;
 
-import racingcar.domain.RacingCarGame;
+import racingcar.domain.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -10,18 +10,19 @@ public class CarController {
 
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
-    private RacingCarGame racingCarGame;
+    private Cars cars;
 
     public void run() {
-        racingCarGame = new RacingCarGame(readNames());
+        cars = new Cars(readNames());
         startRacing(readTryCount());
+        outputView.printWinner(cars.getWinner());
     }
 
     private void startRacing(int tryCount) {
         outputView.printResultMessage();
         for (int i = 0; i < tryCount; i++) {
-            racingCarGame.race();
-            outputView.printRacingResult(racingCarGame.getCars());
+            cars.race();
+            outputView.printRacingResult(cars);
         }
     }
 
