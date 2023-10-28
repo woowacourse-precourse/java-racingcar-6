fbb8240a0034894bb906b421569195790e315e90
PUNGZI <ashyoo1367@gmail.com>
2023-10-28 17:00:07 +0900

    feat: 몇 번의 이동을 할 것인지 지정하는 기능 구현

--- src/main/java/racingcar/domain/CarMoveCount.java
+++ src/main/java/racingcar/domain/CarMoveCount.java
@@ -1,4 +1,10 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CarMoveCount {
+    public final int inputCarMoveCount() {
+        int CarMoveCount = Integer.parseInt(Console.readLine());
+        return CarMoveCount;
+    }
 }
