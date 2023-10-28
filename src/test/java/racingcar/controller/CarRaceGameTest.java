16c8892ce992ae35d3a45a11f40f27d26106b70d
yooooonshine <hl3470@naver.com>
2023-10-29 01:01:42 +0900

    refactor: 자동차이름테스트 여러 파라미터 받도록 변경

--- src/test/java/racingcar/controller/CarRaceGameTest.java
+++ src/test/java/racingcar/controller/CarRaceGameTest.java
@@ -2,17 +2,24 @@ package racingcar.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Application;
+import racingcar.view.CarRaceGameView;
 
 class CarRaceGameTest {
     @ParameterizedTest
     @ValueSource(strings = {",,",  "abcdef,abc", "abc,de,f,"})
     void 자동차이름테스트(String carNames) {
-        Assertions.assertThatThrownBy().isInstanceOf(IllegalArgumentException.class);
+        CarRaceGame carRaceGame = new CarRaceGame();
+        Assertions.assertThatThrownBy(
+                () -> carRaceGame.carNameSetting(carNames)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
+
 
 }
