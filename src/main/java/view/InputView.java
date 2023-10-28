9e6ae0b02237eec7454484ba134a4cb5783c555e
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 21:09:38 +0900

    refactor: view에서 데이터 입력을 받는 것이 아닌 controller에서 데이터 입력을 받도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,15 +5,11 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
     private static final String INPUT_NUM_OF_ATTEMPT = "시도할 회수는 몇회인가요?";
-    public static String printUserInputCarNameMessage() {
+    public static void printUserInputCarNameMessage() {
         System.out.println(INPUT_CAR_NAME);
-        String input = readLine();
-        return input;
     }
 
-    public static Integer printUserInputNumOfAttemptMessage() {
+    public static void printUserInputNumOfAttemptMessage() {
         System.out.println(INPUT_NUM_OF_ATTEMPT);
-        Integer numOfAttempt = Integer.parseInt(readLine());
-        return numOfAttempt;
     }
 }
