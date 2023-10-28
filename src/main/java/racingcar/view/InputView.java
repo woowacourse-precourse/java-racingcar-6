29a650101b3147f7adbade86a95a257bebc94846
otaeho <dhxogh123123@naver.com>
2023-10-28 22:38:05 +0900

    refactor(InputView): 자동차 이름 리스트 변환 메서드 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -15,12 +15,7 @@ public class InputView {
             validateNull(carNamesRawData);
 
             String[] splitCarNamesByComma = carNamesRawData.split(DELIMITER);
-            List<String> carNames = new ArrayList<>();
-            for (int idx = 0; idx < splitCarNamesByComma.length; idx++) {
-                validateCarNameLength(splitCarNamesByComma[idx]);
-                carNames.add(splitCarNamesByComma[idx]);
-            }
-            return carNames;
+            return carNamesArrToList(splitCarNamesByComma);
         } catch (Exception e) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_INPUT_EXCEPTION.getMessage());
         }
@@ -32,8 +27,18 @@ public class InputView {
         }
     }
 
+    private List<String> carNamesArrToList(String[] splitCarNamesByComma) {
+        List<String> carNames = new ArrayList<>();
+        for (int idx = 0; idx < splitCarNamesByComma.length; idx++) {
+            validateCarNameLength(splitCarNamesByComma[idx]);
+            carNames.add(splitCarNamesByComma[idx]);
+        }
+
+        return carNames;
+    }
+
     private void validateCarNameLength(String carName) {
-        if (carName.length() > 5) {
+        if (carName.length() > 5 || carName.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_EXCEPTION.getMessage());
         }
     }
