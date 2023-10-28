790e7a151bcedeecc7ea5ddfc4c2ee18a9186a5d
jeondui <jeondui@gmail.com>
2023-10-28 22:00:52 +0900

    feat(RandomNumberGenerator): 무작위 숫자 생성 기능

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    private static final int NUMBER_MIN_VALUE = 0;
+    private static final int NUMBER_MAX_VALUE = 9;
+
+    public int generate() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
