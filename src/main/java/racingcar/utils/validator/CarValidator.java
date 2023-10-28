6da58b3853a1a0e7a910f54a2086ae10948f4b66
상원 최 <swchoi1@swm.ai>
2023-10-29 01:33:16 +0900

    feat(자동차 이름 검증) : 자동차 이름 검증 로직 추가
    자동차 이름이 1~5글자 사이에 있는지, null이나 빈 이름을 전달하지 않았는지 검증하는 Validator 추가

---
+++ src/main/java/racingcar/utils/validator/CarValidator.java
@@ -0,0 +1,30 @@
+package racingcar.utils.validator;
+
+import racingcar.utils.types.StringUtils;
+
+public class CarValidator implements Validator<String>{
+
+    private static final int START_LENGTH = 1;
+    private static final int END_LENGTH = 5;
+
+    public CarValidator() {
+    }
+
+    @Override
+    public void validate(final String value) {
+        this.validateNameNotEmpty(value);
+        this.validateNameLength(value);
+    }
+
+    private void validateNameNotEmpty(final String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameLength(final String value) {
+        if (!StringUtils.isLengthRange(value, START_LENGTH, END_LENGTH)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/main/java/racingcar/utils/validator/Validator.java
@@ -0,0 +1,7 @@
+package racingcar.utils.validator;
+
+public interface Validator<T> {
+
+    void validate(final T value);
+
+}
