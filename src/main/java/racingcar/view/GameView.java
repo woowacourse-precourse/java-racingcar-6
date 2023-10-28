a2e888f233ae48dfcabcc81e8c8071358bdfaad1
seokmyungham <hellotjraud@gmail.com>
2023-10-29 08:09:39 +0900

    feat(GameView): inputCarNames 메서드 추가
    inputCarNames: 자동차 이름을 입력받는 메서드입니다.

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -1,9 +1,16 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public void printCarNamesMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
+    }
+
+    public String inputCarNames() {
+        String carNames = Console.readLine();
+        return carNames;
     }
 }
