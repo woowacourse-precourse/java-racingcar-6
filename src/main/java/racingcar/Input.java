bea909bea5d7ff1ce308896e801bf3474bfd23d6
BINBEAN777 <peanutchan@naver.com>
2023-10-28 12:36:21 +0900

    feat : Input 클래스 생성 / getCarName 메소드 생성 자동차 게임을 할 인원의 이름을 받고 받은 이름으로 문자열 배열을 생성하였다.

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,4 +1,15 @@
 package racingcar;
 
+import java.util.HashMap;
+import  java.util.Map;
+import camp.nextstep.edu.missionutils.Console;
 public class Input {
-}
+
+    public static String[] getCarName(){
+        String getCarAllName = Console.readLine();
+        String [] carName = getCarAllName.split(",");
+        return carName;
+        }
+    }
+
+
