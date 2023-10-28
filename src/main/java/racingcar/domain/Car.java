b6b6519ef271a8e2f4812b058fd692cfd99edeb7
leeedohyun <ldh19738@gmail.com>
2023-10-28 16:40:23 +0900

    style: name.length() -> name.isEmpty()로 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class Car {
 
-    private static final int NAME_MIN_LENGTH = 1;
     private static final int NAME_MAX_LENGTH = 5;
     private static final int FORWARD_BASE_NUMBER = 4;
     private static final String CAR_NAME_LENGTH_EXCEPTION_MESSAGE = "5자 이하의 이름만 가능합니다.";
@@ -57,7 +56,7 @@ public class Car {
     }
 
     private void validateCarNameLength(String name) {
-        if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
+        if (name.isEmpty() || name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION_MESSAGE);
         }
     }
