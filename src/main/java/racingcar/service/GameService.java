cbddcfe8ebc5c0323c218caaa3283a7022e79dbb
dong2ast <kdh01132006@naver.com>
2023-10-28 20:02:56 +0900

    ✨ feat: 이름 list를 통해 자동차 객체 list 생성

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -3,18 +3,20 @@ package racingcar.service;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.Car;
 
 public class GameService {
 
     public static void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
-        List<String> carList = new ArrayList<>();
+        List<String> carNameList = new ArrayList<>();
         for (String s : carNames.split(",")) {
-            if (s.contains(" ") || s.length() > 5 || carList.contains(s)) {
+            if (s.contains(" ") || s.length() > 5 || carNameList.contains(s)) {
                 throw new IllegalArgumentException();
             }
-            carList.add(s);
+            carNameList.add(s);
         }
+        List<Car> carList = carNameList.stream().map(Car::new).toList();
     }
 }
