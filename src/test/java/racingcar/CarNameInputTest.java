6a2fdd4e5787cbcaeab7f10f37a26b3133c8e85d
Zeus6768 <akaps6768@gmail.com>
2023-10-29 00:07:14 +0900

    test(요구사항2): 중복된 이름을 입력할 경우의 테스트 작성

--- src/test/java/racingcar/CarNameInputTest.java
+++ src/test/java/racingcar/CarNameInputTest.java
@@ -35,6 +35,25 @@ class CarNameInputTest {
         assertThat(race.getCars()).isEqualTo(expected);
     }
 
+    @Test
+    void 중복된_이름_입력() {
+        // given
+        String actual = "자동차1,자동차2,자동차1,자동차2";
+        setInput(actual);
+
+        Set<Car> expected = Set.of(
+                new Car("자동차1"),
+                new Car("자동차2")
+        );
+        // when
+        String[] carNames = inputView.askCarName().split(",");
+        for (String carName : carNames) {
+            race.add(new Car(carName));
+        }
+        // then
+        assertThat(race.getCars()).isEqualTo(expected);
+    }
+
     void setInput(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
