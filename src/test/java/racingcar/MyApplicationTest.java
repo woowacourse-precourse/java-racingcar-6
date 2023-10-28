9cd0970cacf64a8f5b7b1ec902791efb1459a38e
kimdeokyoung <dudejrss@naver.com>
2023-10-28 20:04:52 +0900

    test: 입력이 유효한지 검증하는 기능 테스트 (#2)
    * test: 자동차 이름을 빈 문자열로 주면 IllegalArgumentException을 발생시키고 종료한다
    * test: 자동차 이름이 0글자 이면 IllegalArgumentException을 발생시키고 종료한다
    * test: 자동차 이름이 6자 이상이면 IllegalArgumentException을 발생시키고 종료한다
    * test: 시도할 횟수가 0 이하의 값이라면 IllegalArgumentException을 발생시키고 종료한다
    * test: 시도할 횟수가 정수형 입력이 아니라면 IllegalArgumentException을 발생시키고 종료한다
    * docs: 수행한 기능목록 표시

--- src/test/java/racingcar/MyApplicationTest.java
+++ src/test/java/racingcar/MyApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -9,6 +10,7 @@ import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -26,6 +28,8 @@ public class MyApplicationTest {
     private static final InputStream systemIn = System.in;
     private static final String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIALS = "시도할 회수는 몇회인가요?";
+    private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자";
     private ByteArrayOutputStream outBuffer;
     private PrintStream outputStream;
     private List<String> outputs;
@@ -39,11 +43,57 @@ public class MyApplicationTest {
                 .isEqualTo(INPUT_NAMES);
 
     }
+
     @Test
     void 자동차_이름_입력_문구와_시도할_횟수_입력_문구_출력() {
-        run("pobi,woni", "10","1");
+        run("pobi,woni", "10", "1");
         assertThat(outputs())
                 .contains(INPUT_NAMES, INPUT_TRIALS);
+    }
+    @Test
+    void 자동차_이름을_빈_문자열로_주면_프로그램_종료() {
+        assertThatProgramExitsWithIllegalArgumentException(()->{
+            run("", "10");
+
+        });
+    }
+
+    @Test
+    void 자동차_이름이_0글자이면_프로그램_종료() {
+        assertThatProgramExitsWithIllegalArgumentException(()->{
+            run("pobi,,woni", "10");
+
+        });
+    }
+    @Test
+    void 자동차_이름이_6글자_이상이면_프로그램_종료() {
+        assertThatProgramExitsWithIllegalArgumentException(()->{
+            run("pobi,sebastian,woni", "10");
+
+        });
+    }
+
+    @Test
+    void 시도할_횟수가_0_이하의_값이면_프로그램_종료() {
+        assertThatProgramExitsWithIllegalArgumentException(()->{
+            run("pobi,woni", "-15");
+
+        });
+    }
+    @Test
+    void 시도할_횟수가_정수형이_아닌_값이면_프로그램_종료() {
+        assertThatProgramExitsWithIllegalArgumentException(()-> {
+            run("pobi,woni", "hello");
+        });
+
+    }
+
+    void assertThatProgramExitsWithIllegalArgumentException(ThrowingCallable callable) {
+        assertThatThrownBy(callable)
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(outputs())
+                .doesNotContain(INPUT_TRIALS, EXECUTION_RESULT)
+                .noneMatch(s -> s.contains(FINAL_WINNER));
     }
 
 
@@ -59,8 +109,8 @@ public class MyApplicationTest {
         Application.main(new String[]{});
     }
 
-    void resetInputStream(String... args){
-        byte[] inBuffer = String.join("\n",args).getBytes();
+    void resetInputStream(String... args) {
+        byte[] inBuffer = String.join("\n", args).getBytes();
         System.setIn(new ByteArrayInputStream(inBuffer));
     }
 
