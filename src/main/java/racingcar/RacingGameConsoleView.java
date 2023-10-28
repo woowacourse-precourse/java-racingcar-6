9c8f016dc4a93b3e4509abe0a3237133872837c9
dukcode <duk9741@gmail.com>
2023-10-28 19:39:17 +0900

    feat: 레이싱 결과 제목 출력 구현

--- src/main/java/racingcar/RacingGameConsoleView.java
+++ src/main/java/racingcar/RacingGameConsoleView.java
@@ -10,6 +10,7 @@ public class RacingGameConsoleView {
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MOVE_COUNT_REQUEST_MESSAGE =
             "시도할 회수는 몇회인가요?";
+    private static final String RACING_RESULT_TITLE_MESSAGE = "실행 결과";
 
     public MoveCountDto inputMoveCount() {
         System.out.println(MOVE_COUNT_REQUEST_MESSAGE);
@@ -19,5 +20,9 @@ public class RacingGameConsoleView {
     public RacingCarNameRegistryDto inputRacingCarNames() {
         System.out.println(RACING_CAR_NAME_REQUEST_MESSAGE);
         return new RacingCarNameRegistryDto(Console.readLine());
+    }
+
+    public void printResultTitle() {
+        System.out.println(RACING_RESULT_TITLE_MESSAGE);
     }
 }
