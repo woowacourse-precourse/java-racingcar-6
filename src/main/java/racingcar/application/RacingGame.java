ce1a3d9b793d8844187ec87cfaa41c6c3da3b14c
yummygyudon <bang2brew@gmail.com>
2023-10-29 01:56:21 +0900

    FEAT : 레이싱 게임 기능 객체 RacingGame 클래스 구현
    - 실질적인 게임 기능을 수행하는 역할
      - 시도 횟수만큼 서킷을 돌도록 하는 역할
      - Race에 대한 결과를 출력하는 역할
      - Race 객체 생성 및 활용 또한 외부에서 제어하는 것이 아닌 RacingGame 객체에서만 제어하도록 설계

---
+++ src/main/java/racingcar/application/RacingGame.java
@@ -0,0 +1,56 @@
+package racingcar.application;
+
+import racingcar.domain.Race;
+import racingcar.domain.RacingCar;
+import racingcar.util.channel.Printer;
+import racingcar.util.constant.ConstVariable;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class RacingGame {
+    private final Race race;
+    private final Long trial;
+
+    public RacingGame(Long trial, List<String> carNames) {
+        this.trial = trial;
+        this.race = new Race(carNames);
+    }
+
+    public void start() {
+        Printer.printLine(ConstVariable.RESULT_START_MESSAGE);
+
+        for (int round = 1; round <= trial; round++) {
+            race.runCircuit();
+            viewCircuitResult(race);
+            Printer.printBlankLine();
+        }
+        viewWinners(race);
+    }
+
+    private void viewCircuitResult(Race race) {
+        for (RacingCar car : race.getRacingCars()) {
+            StringJoiner carResult = new StringJoiner(ConstVariable.RESULT_SEPARATOR);
+            carResult.add(car.getName());
+            carResult.add(ConstVariable.RESULT_MOVEMENT.repeat(car.getDistance().intValue()));
+
+            Printer.printLine(carResult.toString());
+        }
+    }
+
+    private void viewWinners(Race race) {
+        StringJoiner winnerResult = new StringJoiner(ConstVariable.RESULT_WINNER_SEPARATOR);
+
+        Long highestScore = race.getRacingCars().stream()
+                .map(RacingCar::getDistance)
+                .max(Comparator.comparingInt(Long::intValue))
+                .get();
+        List<RacingCar> winners = race.getRacingCars().stream()
+                .filter(car -> car.getDistance().equals(highestScore))
+                .toList();
+        winners.forEach(car -> winnerResult.add(car.getName()));
+        Printer.printLine(ConstVariable.RESULT_WINNER_MESSAGE + winnerResult);
+    }
+
+}
