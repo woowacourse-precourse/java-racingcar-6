fd4daef41205954e63a9b14ce8b3b7aeb6a9d128
KIVSH <kihongkim3@hotmail.com>
2023-10-28 20:36:55 +0900

    feat: compose Car class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,45 @@
+package racingcar;
+
+import java.util.List;
+
+public class Car {
+    private String name;
+    private List<String> goOrStopRecord;
+    private int totalGoTimes;
+    private boolean isWinner;
+
+    public Car(String name, List<String> goOrStopRecord, int totalGoTimes, boolean isWinner) {
+        this.name = name;
+        this.goOrStopRecord = goOrStopRecord;
+        this.totalGoTimes = totalGoTimes;
+        this.isWinner = isWinner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getGoOrStopRecord() {
+        return goOrStopRecord;
+    }
+
+    public void setGoOrStopRecord(List<String> goOrStopRecord) {
+        this.goOrStopRecord = goOrStopRecord;
+    }
+
+    public int getTotalGoTimes() {
+        return totalGoTimes;
+    }
+
+    public void setTotalGoTimes(int totalGoTimes) {
+        this.totalGoTimes = totalGoTimes;
+    }
+
+    public boolean isWinner() {
+        return isWinner;
+    }
+
+    public void setWinner(boolean winner) {
+        isWinner = winner;
+    }
+}
