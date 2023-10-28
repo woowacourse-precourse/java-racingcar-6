45ae8faa624df6f7fe89653e9f691885c681ac37
minisyu <iswater.kim@gmail.com>
2023-10-28 14:55:23 +0900

    refactor(InputConverter): convertStringToCars 메소드 수정

--- src/main/java/racingcar/converter/InputConverter.java
+++ src/main/java/racingcar/converter/InputConverter.java
@@ -1,10 +1,16 @@
 package racingcar.converter;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.game.Car;
 import racingcar.domain.game.Cars;
 
 public class InputConverter {
     public Cars convertStringToCars(String input) {
-        return Cars.of(input);
+        List<Car> cars = Arrays.stream(input.split(","))
+                .map(Car::of)
+                .toList();
+        return Cars.of(cars);
     }
 
     public int covertStringToInteger(String input) {
