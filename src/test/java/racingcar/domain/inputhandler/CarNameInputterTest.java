36266d4a5b91799d9b3e5ca8f8a0a497c1a5a0bb
Picka <marmong@naver.com>
2023-10-28 18:56:40 +0900

    test: add tests for 경주할 자동차 이름 유효성 검사 기능

--- src/test/java/racingcar/domain/inputhandler/CarNameInputterTest.java
+++ src/test/java/racingcar/domain/inputhandler/CarNameInputterTest.java
@@ -3,15 +3,25 @@ package racingcar.domain.inputhandler;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CarNameInputterTest {
 
     @Test
-    public void CarNameInputter() {
+    void Input_자동차_입력_확인() {
         CarNameInputter carNameInputter = new CarNameInputter();
         String carNames = "pobi,woni,jun";
-        List<String> carNameList = carNameInputter.divineCarName(carNames);
+        List<String> carNameList = carNameInputter.splitCarNames(carNames);
         assertEquals(Arrays.asList("pobi", "woni", "jun"), carNameList);
+    }
+
+    @Test
+    void Valid_자동차_이름_유효성_확인() {
+        CarNameInputter carNameInputter = new CarNameInputter();
+        List<String> carNameList = Arrays.asList("pobi", "woni", "jun");
+        boolean isValidCarName = carNameInputter.validateCarName(carNameList);
+        assertTrue(isValidCarName);
     }
 }
