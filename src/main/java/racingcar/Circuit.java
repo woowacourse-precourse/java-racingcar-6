9ec06582bd103c16111c063dcc79672b1e9ce324
minsang-alt <mazinger97@naver.com>
2023-10-28 14:08:09 +0900

    feat: implement game features
    - Added generate random numbers
    - Added extract winners
    - Added all cars to run

---
+++ src/main/java/racingcar/Circuit.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import racingcar.model.Car;
+
+public class Circuit {
+    private List<Car> cars;
+
+    public Circuit(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void run() {
+        cars = cars.stream().map((car) -> car.runAndReturnNewCar(createRandomNumber())).toList();
+    }
+
+
+    private Long createRandomNumber() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        return (long) randomNum;
+    }
+
+    public String getWinner() {
+        Long maxPosition = cars.stream().mapToLong(Car::getPosition).max().orElseThrow(NoSuchElementException::new);
+
+        return cars.stream()
+                .filter(car -> car.getPosition().equals(maxPosition))
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", "));
+    }
+}
