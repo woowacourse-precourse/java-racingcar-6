e9dd3ecc452d180c694b00930233b96679feef1b
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-28 22:17:32 +0900

    feat: 입력 회수만큼 게임 진행 및 게임 1회 진행 기능 개발
    - 입력 회수 만큼 게임 진행
    - playSingleGame 메소드를 추가하여 게임 1회 진행 로직을 분리하고 가독성 향상

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,10 +1,14 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.model.TrialNumber;
 import racingcar.utils.Parser;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class Controller {
     Cars cars;
@@ -27,5 +31,24 @@ public class Controller {
         String inputNumber = Console.readLine();
         System.out.println();
         trialNumber = new TrialNumber(inputNumber);
+    }
+
+    /**
+     * 입력 회수만큼 게임 진행
+     */
+    private void playGame() {
+        OutputView.printResultHeader();
+        int rounds = trialNumber.getTrialNumber();
+        for (int count = 0; count < rounds; count++) {
+            playSingleGame(cars.getCars());
+        }
+    }
+
+    /**
+     * 게임 1회 진행
+     */
+    private void playSingleGame(List<Car> cars) {
+        cars.forEach(Car::move);
+        OutputView.printResult(cars);
     }
 }
