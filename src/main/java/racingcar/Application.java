16d7500bb957bb76145759ccdf6eef4266488115
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-10-28 23:04:53 +0900

    feat: 우승자들 출력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -111,6 +111,14 @@ public class Application {
     }
 
     public static void printWinners(List<Car> winners){
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i<winners.size(); i++){
+            System.out.print(winners.get(i).getName());
 
+            if(i != winners.size() - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
