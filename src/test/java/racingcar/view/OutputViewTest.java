612fe2ff4db37ff966076d19135b93bb12fb3762
박준영 <wnsdud6969@naver.com>
2023-10-28 18:29:22 +0900

    test(OutputViewTest): add test to verify user output view
    Add new event to OutputViewTest
    - add ByteArrayOutputStream instance to check console output
    - add OutputView instance
    - setup console output environment using @BeforeEach
    - return normal console output environment using @AfterEach
    - call promptCarNames() and compare message

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -2,10 +2,31 @@ package racingcar.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class OutputViewTest {
 
+    private static ByteArrayOutputStream outputMessage;
+    private OutputView outputView;
+
+    @BeforeEach
+    void beforeEach() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.setOut(System.out);
+    }
+
+    public OutputViewTest() {
+        this.outputView = OutputView.getInstance();
+    }
 
     @Test
     public void OutputView() {
@@ -13,5 +34,11 @@ class OutputViewTest {
         OutputView outputView2 = OutputView.getInstance();
 
         assertEquals(outputView1, outputView2);
+    }
+
+    @Test
+    public void promptCarNames() {
+        outputView.promptCarNames();
+        assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n", outputMessage.toString());
     }
 }
