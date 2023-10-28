5bc8ea3c32c48b2c0506db5027ad5d6766909f04
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-28 20:42:49 +0900

    feat(util): InputValidator 클래스 추가
    입력 값을 검증하는 역할을 수행합니다.
    - isValidCarNames() 메소드는 자동차 이름 입력을 검증합니다.
    - isValidRound() 메소드는 라운드 입력을 검증합니다.

---
+++ src/main/java/racingcar/util/InputValidator.java
@@ -0,0 +1,31 @@
+package racingcar.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputValidator {
+    public static boolean isValidCarNames(String carNames) {
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        Set<String> uniqueCarNames = new HashSet<>(carNameList);
+
+        if (carNameList.size() != uniqueCarNames.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+        }
+
+        if (carNameList.stream().anyMatch(carName -> carName.length() > 5)) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+
+        return true;
+    }
+
+    public static boolean isValidRound(String round) {
+        if (!round.matches("[0-9]+")) {
+            throw new IllegalArgumentException("시도할 회수는 숫자만 가능합니다.");
+        }
+
+        return true;
+    }
+}
