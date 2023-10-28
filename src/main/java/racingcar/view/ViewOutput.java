5f2a20fc939ec94099b0cc14417a95efbc5d4386
GumaLee <rkdxhd97@naver.com>
2023-10-29 00:00:30 +0900

    feature: 우승자 출력기능 추가

--- src/main/java/racingcar/view/ViewOutput.java
+++ src/main/java/racingcar/view/ViewOutput.java
@@ -13,6 +13,16 @@ public class ViewOutput {
             stringBuilder.append("\n");
         }
         System.out.println(stringBuilder);
+        stringBuilder.setLength(0);
     }
 
+    public void printWinner(List<Car> winnerCars) {
+        stringBuilder.append("최종 우승자 : ");
+        for(Car car : winnerCars) {
+            stringBuilder.append(car.getName() + ", ");
+        }
+        stringBuilder.setLength(stringBuilder.length() - 2);
+
+        System.out.println(stringBuilder);
+    }
 }
