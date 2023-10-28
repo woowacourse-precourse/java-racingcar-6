2edb11d79caf61e033364faa57095fce66308419
hyeongjun im <limbs713@naver.com>
2023-10-28 13:16:04 +0900

    :sparkles: GameController class 추가
    game의 flow을 담당하는 controller
    실행을 의미하는 run()의 구조만 생성되어있음.

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.model.service.GameService;
+
+public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    public void run(){
+
+    }
+}
