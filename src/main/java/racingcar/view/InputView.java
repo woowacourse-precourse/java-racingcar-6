79a154ca9cbeec932e70e2429d470b65480406ec
sehee <tpgml701@naver.com>
2023-10-29 02:32:06 +0900

    feat: InputView 클래스 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,9 @@
 package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class InputView {
 
-    public String getNumberInput() {
+    public String getInput() {
         return Console.readLine();
-    }
-
-    public List<String> getUserInput(String input) {
-        return Arrays.stream(input.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
     }
 }
