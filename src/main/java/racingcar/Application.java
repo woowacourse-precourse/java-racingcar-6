3e5694380fc5df4a14865a382f5e8666fcde551a
이준희 <juni416@naver.com>
2023-10-28 16:24:28 +0900

    bugfix : racingCar리스트 생성 오류 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -7,7 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         RacingManager racingManager = new RacingManager();
-        List<RacingCar> racingCars = List.of();
+        List<RacingCar> racingCars = new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = readLine();
@@ -20,6 +21,8 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         racingManager.setLap(Integer.parseInt(readLine()));
 
+        System.out.println("실행 결과");
+        racingManager.race(racingCars);
 
     }
 }
