3e7dab751e19ea8689d2ae0e2ea395d003abfa64
yummygyudon <bang2brew@gmail.com>
2023-10-29 01:40:06 +0900

    FIX : RacingCar 도메인 내부 데이터 추가/편집하는 메서드 & 생성자 default 접근제어자 적용
    - 같은 패키지에 있는 도메인인 Race 객체에서만 호출할 수 있도록 설계
    - 시도횟수에 대한 상한선이 없기 때문에 `distance` 필드 Long 타입으로 지정

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,22 +2,22 @@ package racingcar.domain;
 
 public class RacingCar {
     private final String name;
-    private Long score;
+    private Long distance;
 
-    public RacingCar(String name) {
+    RacingCar(String name) {
         this.name = name;
-        this.score = 0L;
+        this.distance = 0L;
     }
 
     public String getName() {
         return name;
     }
 
-    public Long getScore() {
-        return score;
+    public Long getDistance() {
+        return distance;
     }
 
-    public void move() {
-        this.score += 1;
+    void move() {
+        this.distance += 1;
     }
 }
