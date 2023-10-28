04ee5ba8432d4d2bd7365ce64df3faeaaa7753f5
HoeSeong123 <ghltjd3@gmail.com>
2023-10-28 13:03:43 +0900

    feat(InputView) : 문구 출력 후 자동차 이름 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class InputView {
     public static List<String> readRacingCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String racingCarName = Console.readLine();
         return Arrays.stream(racingCarName.split(","))
                 .map(String::trim)
