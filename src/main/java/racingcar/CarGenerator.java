e0021245a6cdf33f0c108900834a4357d211363a
dk7648 <dk7648@korea.ac.kr>
2023-10-28 10:43:13 +0900

    feat(CarGenerator): splitNames메소드 추가

--- src/main/java/racingcar/CarGenerator.java
+++ src/main/java/racingcar/CarGenerator.java
@@ -1,10 +1,13 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarGenerator {
     public List<String> splitNames(String names) {
-        return null;
+        String[] splitNames = names.split(",");
+        return new ArrayList<>(Arrays.asList(splitNames));
     }
 
     public List<Car> createCars(List<String> nameList) {
