8c32ca5a26e7fbbe70c89d1f593d791487ec18c5
jinchiim <nellroll@syuin.ac.kr>
2023-10-29 05:29:42 +0900

    feat: Input Error 추가
    Input Error 추가
    - 1개 이하일 경우의 Input error 추가

--- src/main/java/racingcar/domain/input/error/InputError.java
+++ src/main/java/racingcar/domain/input/error/InputError.java
@@ -5,6 +5,8 @@ public enum InputError {
     EXCEEDED_MAXIMUM_ERROR("입력할 수 있는 최대를 넘었습니다."),
     MUST_BE_NUMBER("입력값은 숫자여야 합니다."),
 
+    NOT_EXCEED_MINIMUM_AMOUNT("이름을 2개 이상은 입력해야 합니다."),
+
     DOES_NOT_MATCH_WITH_TRIAL_AMOUNT("입력한 이름의 개수가 게임 ");
 
     final String message;
