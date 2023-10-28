94a7c2d8141396363ae5858bac32d7daca4b2a79
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-28 15:52:38 +0900

    feat: 경주할 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.StringTokenizer;
+
 public class Application {
+    private static final int INIT_DISTANCE = 0;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // HashMap의 순서를 보장하기 위해 LinkedHashMap 사용
+        Map<String, Integer> memberStates = new LinkedHashMap<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String memberInput = Console.readLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(memberInput, ",");
+        while (stringTokenizer.hasMoreTokens()) {
+            String name = stringTokenizer.nextToken();
+            memberStates.put(name, INIT_DISTANCE);
+        }
     }
 }
