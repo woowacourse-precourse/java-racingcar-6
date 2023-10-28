bb5152971804c878751dab84bbbee480d24089ba
huhdy32 <mong3268@gmail.com>
2023-10-28 13:59:22 +0900

    refactor(main/Controller): 리턴타입 수정

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,9 +1,11 @@
 package racingcar.controller;
 
+import racingcar.model.Player;
+
 import java.util.List;
 
 public interface Controller {
     boolean addPlayer();
     boolean removePlayer();
-    List<String> getPlayers();
+    List<Player> getPlayers();
 }
