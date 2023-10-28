eb435eb8689e49edc3b2aa180f78d5a1b78f61e1
hyo-limilimee <1001lily@naver.com>
2023-10-29 02:51:40 +0900

    feat: make StringTest code

--- src/main/java/racingcar/Validator/StringValidator.java
+++ src/main/java/racingcar/Validator/StringValidator.java
@@ -2,7 +2,7 @@ package racingcar.Validator;
 
 public class StringValidator {
     private final static int INPUT_VALUE_MAX_LENGTH = 5;
-    private final static String INVALID_INPUT_LENGTH_MESSAGE = "입력 길이는 5자 이하입니다."
+    private final static String INVALID_INPUT_LENGTH_MESSAGE = "입력 길이는 5자 이하입니다.";
 
     public static void validateCarNameLength(String input){
         if(input.length() > INPUT_VALUE_MAX_LENGTH){
--- src/test/java/racingcar/StringTest.java
+++ src/test/java/racingcar/StringTest.java
@@ -1,4 +1,51 @@
 package racingcar;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class StringTest {
+    @Test
+    void split_valuesWithMethod() {
+        // BUILD
+       String input = "pobi,woni";
+       // OPERATE
+       String[] result = input.split(",");
+        //CHECK
+       assertThat(result).contains("woni", "pobi");
+       assertThat(result).containsExactly("pobi", "woni");
+   }
+
+    @Test
+    void split_valuesIfNoSeperator()
+   {    // BUILD
+       String input = "pobi";
+       // OPERATE
+       String[] result = input.split(",");
+        //CHECK
+       Assertions.assertThat(result).contains("pobi");
+   }
+
+   @Test
+    void check_stringLengthInRange() {
+        // BUILD
+        String input = "pobi";
+        // OPERATE
+        int maxLength = 5;
+        // CHECK
+        assertThat(input.length()).isLessThanOrEqualTo(maxLength);
+    }
+
+    @Test
+    void check_stringLengthExceedsMax() {
+        // BUILD
+        String input = "hyolim";
+        // OPERATE
+        int maxLength = 5;
+        // CHECK
+        assertThat(input.length()).isGreaterThan(maxLength);
+    }
+
 }
