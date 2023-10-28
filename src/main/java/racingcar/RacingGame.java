2e548417a16ebfd98bec56093cf4af4814750064
gombasan <xogmlwkdi@naver.com>
2023-10-28 14:26:22 +0000

    RacingGame UI 클래스 분리
    - 사용자의 입출력과 자동차 게임 진행의 역할을 분리하고자, UI 를 클래스로 분리하였습니다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,32 +2,29 @@ package racingcar;
 
 import java.util.List;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class RacingGame {
-	private final CarManger carManger = new CarManger();
+	private final CarManger carManger;
+	private final GameUI ui;
+
+	public RacingGame(GameUI ui) {
+		this.carManger = new CarManger();
+		this.ui = ui;
+	}
 
 	public void run() {
-		String[] carNames = Console.readLine().split(",");
-		carManger.managedCar(carNames);
-
-		int repeat = Integer.parseInt(Console.readLine());
+		String[] carNames = ui.inputCarNames();
+		carManger.createCars(carNames);
+		int repeat = ui.inputRepeat();
 		racing(repeat);
 		List<String> winners = carManger.findWinner();
-		result(winners);
+		ui.printResult(winners);
 	}
 
 	public void racing(int repeat) {
 		for (int i = 0; i < repeat; i++) {
-			carManger.moveCar();
-			System.out.println();
-		}
-	}
-
-	public void result(List<String> winnerName) {
-		System.out.print("최종 우승자 : ");
-		for (String s : winnerName) {
-			System.out.print(s);
+			carManger.moveCars();
+			ui.printHistory(carManger.getPositionHistory());
+			ui.enter();
 		}
 	}
 }
