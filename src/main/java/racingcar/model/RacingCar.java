303471ff387285553265d0490683f91a1fbb3e90
yoonjuho <dbswn990@naver.com>
2023-10-29 03:31:13 +0900

    feat(RacingCar): 자동차는 전진 또는 멈출 수 있다.

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 public class RacingCar {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int FORWARD_CONDITION = 4;
     private final String name;
 
     public RacingCar(String name) {
@@ -13,5 +14,12 @@ public class RacingCar {
         if(name.isEmpty() || name.length() > MAX_NAME_LENGTH ){
             throw new IllegalArgumentException("이름은 5자 이하의 구분 가능한 문자로 이루어져야 합니다.");
         }
+    }
+
+    public CarGear apply(int condition){
+        if(condition >= FORWARD_CONDITION){
+            return CarGear.FORWARD;
+        }
+        return CarGear.STOP;
     }
 }
--- src/test/java/racingcar/model/RacingCarTest.java
+++ src/test/java/racingcar/model/RacingCarTest.java
@@ -1,14 +1,18 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class RacingCarTest {
 
@@ -20,5 +24,17 @@ class RacingCarTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new RacingCar(name.trim()));
     }
+
+    @DisplayName("자동차는 전진 또는 멈출 수 있다")
+    @ParameterizedTest
+    @CsvSource(value = {"0,4", "1,5", "2,6", "3,7", "3,8", "2,9"})
+    void applyGear(int stop, int forward){
+        RacingCar racingCar = new RacingCar("TEST");
+        Assertions.assertAll(
+                () -> assertThat(racingCar.apply(stop)).isEqualTo(CarGear.STOP),
+                () -> assertThat(racingCar.apply(forward)).isEqualTo(CarGear.FORWARD)
+        );
+    }
+
 
 }
