2c981ade4a8e1ab76623e9578be7c204518f1f6b
CodingApe9 <tlqkf6974@>
2023-10-28 21:54:52 +0900

    refactor(InputValidator) : 자동차 이름 전체에 대한 검증만 수행
    각 자동차의 이름의 대한 검증은 따로 한다.

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,20 +1,11 @@
 package racingcar;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class InputValidator {
     public static void validateCarNames(String carNames) {
         if (carNames == null
                 || carNames.isBlank()
                 || carNames.isEmpty()) {
             throw new IllegalArgumentException("검증할 자동차 이름 목록이 없습니다.");
-        }
-
-        String[] carNameArray = carNames.split(",");
-
-        for (String carName : carNameArray) {
-//            validateCarName(carName);
         }
     }
 }
