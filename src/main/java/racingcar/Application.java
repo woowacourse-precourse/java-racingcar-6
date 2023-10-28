135644731a9e24108e396523e61a15d9c9c083bd
김영관 <kimyogun78@naver.com>
2023-10-28 23:17:37 +0900

    style: Changed print message

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,13 +8,14 @@ import java.util.Arrays;
 public class Application {
 
     public static String[] inputCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String str = Console.readLine();
         String[] names = str.split(",");
         return names;
     }
 
     public static int inputNumber(){
+        System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
