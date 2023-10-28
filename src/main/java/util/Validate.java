4974acf6656e2bacd8a2fe159eebd28a7b9b6f38
AJN <buzz781@gmail.com>
2023-10-28 15:26:41 +0900

    유효성검사기능수정

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -7,8 +7,9 @@ public class Validate {
 	public static void inputPlayerDateValidate(String inputDate) {
 		String[] inputDate_arr = inputDate.split(",");
 		
-		//쉼표(,)가 없을경우
-		if(!inputDate.contains(",")) throw new IllegalArgumentException("게임 진행을 위해 쉼표를 넣어주세요!");
+		//쉼표(,)가 없을경우(자동차 1개만으로도 진행 할 경우의 수를 고려해서 주석처리
+		//if(!inputDate.contains(",")) throw new IllegalArgumentException("게임 진행을 위해 쉼표를 넣어주세요!");
+		
 		//이름 5자이하로 제한.
 		for(String s : inputDate_arr) if(s.length()>NAME_LIMIT) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다!");
 		//숫자 포함하면 안됨.
