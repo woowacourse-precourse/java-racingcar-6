51c8bad42b90f46fa8a4ebd4110d56b3a89c2ad3
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 19:57:58 +0900

    feat : 실제 경주를 하는 객체의 전진한 횟수를 변경하는 메소드 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import static racingcar.exception.CarNameValidator.validateCarName;
+
+public class Car {
+
+    private static final Integer INIT_STEP = 0;
+
+    private final String name;
+    private Integer step;
+
+    public Car(final String name) {
+        validateCarName(name);
+        this.name = name;
+        this.step = INIT_STEP;
+    }
+
+    public void move() {
+        this.step = Status.move() + this.step;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getStep() {
+        return step;
+    }
+}
