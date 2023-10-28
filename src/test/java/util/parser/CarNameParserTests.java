0bd92a69caad8aab33a94f76a37099d1dd62de78
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-28 11:35:34 +0900

    test(CarNameParser): 자동차 이름 사용자 입력값의 마지막이 공백인 경우에도 참가자로 인식하는 테스트 케이스 추가
    - 시험 중 마지막에 , 후 공백인 경우 이를 참가자로 인식하지 못하는 오류를 발견하여 테스트 케이스 추가

--- src/test/java/util/parser/CarNameParserTests.java
+++ src/test/java/util/parser/CarNameParserTests.java
@@ -26,4 +26,20 @@ public class CarNameParserTests {
         for ( int i = 0; i < expect.length; i++ )
             assertThat(result.get(i)).isEqualTo(expect[i]);
     }
+
+    @Test
+    @DisplayName("마지막 참가자가 이름이 없는 경우에도 빈 문자열로 변환된다")
+    void test2() {
+        // given
+        String input = "첫번째,두번째,세번째,네번째,,";
+
+        // when
+        List<String> result = carNameParser.parse(input);
+
+        // then
+        String[] expect = {"첫번째", "두번째", "세번째","네번째","",""};
+        assertThat(result.size()).isEqualTo(6);
+        for ( int i = 0; i < expect.length; i++ )
+            assertThat(result.get(i)).isEqualTo(expect[i]);
+    }
 }
