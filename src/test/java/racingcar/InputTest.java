3f80e6f5aa7fe2a2d4cf9a93f4d34f2d1408d316
taehoon <xogns1514@gmail.com>
2023-10-28 16:04:02 +0900

    test: 자동차이름 검증 테스트 코드 추가
    - 자동차이름 0초과 5자 이하인지 검증
    - 공백 포함시, 에러발생 검증

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class InputTest {
+
+    @Test
+    public void 테스트_모두_유효한_자동차이름() {
+        // given
+        String inputCarNames = "우아,한,테크,코스";
+        //then
+        assertDoesNotThrow(() -> ValidationManager.validateCarNames(inputCarNames));
+    }
+
+    @Test
+    public void 테스트_5자_초과_자동차이름() {
+        // given
+        String inputCarNames = "우아,하아안,테크으으으으,코스으으으으";
+        // then
+        assertThrows(IllegalArgumentException.class, () -> ValidationManager.validateCarNames(inputCarNames));
+    }
+
+    @Test
+    public void 테스트_공백포함() {
+        //given
+        String inputCarNames = "우아, ,한,테크";
+        // then
+        assertThrows(IllegalArgumentException.class, () -> ValidationManager.validateCarNames(inputCarNames));
+    }
+}
