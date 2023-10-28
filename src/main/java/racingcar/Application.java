b14852ddb645699d4d057d9978613ac9f870294f
le2sky <leehaneul990623@gmail.com>
2023-10-29 01:38:27 +0900

    feat : 자동차 랜덤 이동 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,11 @@
 package racingcar;
 
+import racingcar.entity.policy.RandomMovePolicy;
+
 public class Application {
 
     public static void main(String[] args) {
-        Controller controller = new Controller(() -> true);
+        Controller controller = new Controller(new RandomMovePolicy());
 
         controller.run();
     }
