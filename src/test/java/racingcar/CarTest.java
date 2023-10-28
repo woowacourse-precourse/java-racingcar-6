3a4fd058a15195acc208ebbf0da2de06c36c2a58
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-28 21:15:21 +0900

    test : 자동차 생성, 전진 테스트

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    void 자동차_생성_테스트() {
+        Car car = new Car("pobi");
+        Assertions.assertThat(car.getCarName())
+                .isNotNull()
+                .isEqualTo("pobi");
+    }
+
+    @Test
+    void 자동차_전진_테스트() {
+        Car car = new Car("pobi");
+        car.moveForward();
+
+        Assertions.assertThat(1)
+                .isEqualTo(car.getPosition());
+    }
+}
