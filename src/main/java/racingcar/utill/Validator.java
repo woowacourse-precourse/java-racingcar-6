2815eaf3c84e58a3d891cd1e010c23b114da6208
kylim99 <zxzx05050@naver.com>
2023-10-28 12:29:02 +0900

    feat: 자동차가 1대일 경우 에러 처리 기능 구현

--- src/main/java/racingcar/utill/Validator.java
+++ src/main/java/racingcar/utill/Validator.java
@@ -8,13 +8,20 @@ public class Validator {
     public void validationCarName(String input){
         List<String> carNames = Arrays.stream(input.split(",")).toList();
         validationCarNamesLength(carNames);
+        validationCarNamesListLength(carNames);
     }
 
     private void validationCarNamesLength(List<String> carNames){
-        carNames.stream().filter(name -> name.length() > 5 ).findAny()
+        carNames.stream().filter(name -> name.length() > 5 )
+                .findAny()
                 .ifPresent(name -> {
                     throw new IllegalArgumentException();
                 });
+    }
+    private void validationCarNamesListLength(List<String> carNames){
+        if(carNames.size() < 2){
+            throw  new IllegalArgumentException();
+        }
     }
 
 }
