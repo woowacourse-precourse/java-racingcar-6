7e8bee103ef587a11ff206ae535d1f1832390581
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-28 20:22:46 +0900

    feat(controller): 경주할 자동차 이름을 입력받는 기능 추가

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,7 +1,10 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingGame {
     public void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
     }
 }
