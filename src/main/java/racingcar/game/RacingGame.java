570e771d68dbf34292e5ee5bffb783807583cec3
Seol_JY <wlsdud5654@gmail.com>
2023-10-28 19:28:09 +0900

    feat(view): 시도할 횟수를 입력받는 기능 구현
    사용자 입력 및 정수로 파싱
    - 파싱이 불가능한 경우 `IllegalArgumentException` 예외 발생

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -6,6 +6,7 @@ import racingcar.view.Input;
 public class RacingGame {
     public void play() {
         List<String> carNames = Input.getCarNames();
-
+        int totalRace = Input.getTotalRace();
+        
     }
 }
--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Input {
     private static final String GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GET_TOTAL_RACE_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private Input() {
     }
@@ -16,12 +17,25 @@ public class Input {
         return parseCarNames(userInput);
     }
 
+    public static int getTotalRace() {
+        String userInput = readLineWithPrompt(GET_TOTAL_RACE_MESSAGE);
+        return tryParseTotalRace(userInput);
+    }
+
     private static List<String> parseCarNames(String input) {
         List<String> carNames = new ArrayList<>();
         for (String name : input.split(",")) {
             carNames.add(name.trim());
         }
         return carNames;
+    }
+
+    private static Integer tryParseTotalRace(String userInput) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static String readLineWithPrompt(String prompt) {
