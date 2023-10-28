c182b290927d62a2c41f0150d126a48235513455
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-28 18:08:23 +0900

    feat: 1-2. 이름 입력 예외처리

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -4,12 +4,20 @@ import racingcar.view.InputView;
 
 public class RacingcarController {
     private static final InputView inputView = InputView.getInstance();
+    private String[] cars;
 
     public void run() {
         start();
     }
 
     private void start() {
-        inputView.getCarNames();
+        cars = inputView.getCarNames().split(",");
+
+        for (String car : cars) {
+            if(car.length() > 5){
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하입니다.");
+            }
+        }
+
     }
 }
