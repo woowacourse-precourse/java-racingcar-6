320d4377aa1c8d9468d654717cf5ea03263b5a68
SeongUk52 <rkfka7849@gmail.com>
2023-10-28 19:36:20 +0900

    test(WinnerChecker): 다중 우승자 테스트

--- src/test/java/racingcar/service/WinnerCheckerTest.java
+++ src/test/java/racingcar/service/WinnerCheckerTest.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
@@ -17,12 +18,33 @@ public class WinnerCheckerTest {
         Car car1 = new Car("붕붕이");
         Car car2 = new Car("부릉이");
         Car car3 = new Car("부와앙");
+
         carList.add(car1);
         carList.add(car2);
         carList.add(car3);
         carList.get(1).move();
         carList.get(1).move();
 
-        assertEquals(WinnerChecker.findWinner(carList).get(0).getNameToString(), car2.getNameToString());
+        assertEquals(WinnerChecker.findWinner(carList).get(0).getNameToString(), "부릉이");
+    }
+
+    @DisplayName("다중 우승자")
+    @Test
+    void testMultipleWinner() {
+        List<Car> carList = new ArrayList<>();
+        Car car1 = new Car("트럭");
+        Car car2 = new Car("버스");
+        Car car3 = new Car("고장난차");
+
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        carList.get(1).move();
+        carList.get(0).move();
+
+        assertThat(WinnerChecker.findWinner(carList).stream()
+                .map(Car::getNameToString)
+                .toList())
+                .contains("트럭", "버스");
     }
 }
