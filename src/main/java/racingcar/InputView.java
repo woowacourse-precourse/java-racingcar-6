72c099247d06f54baad3d5f95b6b91235392b2b1
HyungHoKim <hkim1109@naver.com>
2023-10-28 10:39:30 +0900

    feat(main): 2번 기능 구현
    InputView 클래스 안에 이동 횟수를 입력받는 timesToTry 메서드 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TIMES_TO_TRY_REQUEST = "시도할 횟수는 몇회인가요?";
+
     public static String carName() {
         System.out.println(CAR_NAME_REQUEST);
         String carNamesString;
@@ -13,5 +15,16 @@ public class InputView {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_LENGTH_OVERED);
         }
         return carNamesString;
+    }
+
+    public static int timesToTry() {
+        System.out.println(TIMES_TO_TRY_REQUEST);
+        int timesToTry;
+        try {
+            timesToTry = Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_TYPE_MISMATCH);
+        }
+        return timesToTry;
     }
 }
