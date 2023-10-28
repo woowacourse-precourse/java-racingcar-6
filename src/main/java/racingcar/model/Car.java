8d25a387c8305542818eb9a2d65b928bcadc792d
sehee <tpgml701@naver.com>
2023-10-28 21:56:52 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    String carName;
+    int position;
+
+    public Car(String carName, int position) {
+        this.carName = carName;
+        this.position = position;
+    }
+}
