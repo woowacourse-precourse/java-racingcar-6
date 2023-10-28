248a0ab89afe004aaf0175677fa6bc661e555ee0
junest66 <chlwndks333@gmail.com>
2023-10-29 02:57:27 +0900

    refactor(CarTest): mock NumberGenerator 추가
    - NumberGenerator 의존성 mock 객체를 추가.
    - setUp() 메서드 내에서 mockNumberGenerator 초기화 및 Car 객체 생성 시 주입
    - 랜덤 숫자 생성 로직을 mock을 통해 제어하여 테스트 케이스 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,23 +11,29 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CarTest {
 
     private Car car;
+    private NumberGenerator mockNumberGenerator;
 
     @BeforeEach
     void setUp() {
-        car = new Car("TestCar");
+        mockNumberGenerator = mock(NumberGenerator.class);
+        car = new Car("TestCar", mockNumberGenerator);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void 랜덤숫자가_4이상일떄_이동횟수_증가(int randomNumber) {
-        car.move(randomNumber);
-        assertThat(car.getMoveNum()).isEqualTo(1);
+    void 랜덤숫자가_4이상일떄_자동차_전진(int randomNumber) {
+        int expectedMoveNum = 1;
+        when(mockNumberGenerator.createRandomNumber()).thenReturn(randomNumber);
+        car.move();
+        assertThat(car.getMoveNum()).isEqualTo(expectedMoveNum);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    void 랜덤숫자가_4미만일때_이동횟수_변화없음(int randomNumber) {
-        car.move(randomNumber);
-        assertThat(car.getMoveNum()).isEqualTo(0);
+    void 랜덤숫자가_4미만일때_자동차_멈춤(int randomNumber) {
+        int expectedMoveNum = 0;
+        when(mockNumberGenerator.createRandomNumber()).thenReturn(randomNumber);
+        car.move();
+        assertThat(car.getMoveNum()).isEqualTo(expectedMoveNum);
     }
 }
