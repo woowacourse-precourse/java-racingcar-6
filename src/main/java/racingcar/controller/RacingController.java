6b3381f2713c6fbb5d1566474a3ef838d84544bd
JH-TT <leejongho9803@gmail.com>
2023-10-29 02:00:47 +0900

    feat(RacingController) - 레이싱을 총괄하는 객체 생성
    - 준비과정 기능 구현
        - 플레이어 이름 입력받고
        - 진행횟수 입력받고
        - 레이싱 시작
        - 시도할때 마다 현재 상태 출력
        - 최종 우승자 출력

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,55 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import racingcar.model.Player;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final static InputView inputView = new InputView();
+    private final static OutputView outputView = new OutputView();
+    private final LinkedList<Player> players = new LinkedList<>();
+    private static int MAX = 0;
+    public void racingReady() {
+        outputView.inputNames();
+        String[] names = inputView.inputPlayers();
+        savePlayers(names);
+        outputView.inputTimes();
+        racingStart(inputView.inputTimes());
+        outputView.printWinner(winner());
+    }
+
+    private ArrayList<String> winner() {
+        ArrayList<String> winner = new ArrayList<>();
+        for (Player player : players) {
+            if (!player.isWinner(MAX)) continue;
+            winner.add(player.getName());
+        }
+        return winner;
+    }
+
+    private void racingStart(final int inputTimes) {
+        outputView.printResult();
+        for (int i = 0; i < inputTimes; i++) {
+            System.out.println();
+            move();
+        }
+    }
+
+    private void move() {
+        boolean flag = false;
+        for (Player player : players) {
+            flag |= player.ForwardOrNot();
+            outputView.printPlayerStatus(player.getStatus());
+        }
+        if (flag) MAX++;
+    }
+
+    private void savePlayers(String[] names) {
+        for (String name : names) {
+            players.add(new Player(name));
+        }
+    }
+}
