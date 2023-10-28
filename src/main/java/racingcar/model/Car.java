c1207a249ac28a644e0e0f01d5e4a8dd541f37ca
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-28 18:36:06 +0900

    feat: 자동차 전진 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 public class Car {
 
+    private static final int MOVE_NUMBER = 4;
     private String name;
     private int position = 0;
 
@@ -11,5 +12,19 @@ public class Car {
 
     public static Car from(String name) {
         return new Car(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward(int randomNumber) {
+        if (randomNumber >= MOVE_NUMBER) {
+            position++;
+        }
     }
 }
