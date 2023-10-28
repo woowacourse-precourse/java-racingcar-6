34040e34484e4d9086a9c1fe42b944464c6ecc55
pjaehyun <show6756@gmail.com>
2023-10-28 18:19:54 +0900

    feat: 추가 stringToCarList 함수

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -11,6 +11,17 @@ public class Game {
     public int frequency;
 
     public Game(String participants, String frequency) {
+        this.participants = stringToCarList(participants);
+    }
 
+    public List<Car> stringToCarList(String data) {
+        List<Car> res = new ArrayList<>();
+        String[] splitData = data.split(",");
+
+        for(String name: splitData) {
+            Car car = new Car(name);
+            res.add(car);
+        }
+        return res;
     }
 }
