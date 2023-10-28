2b68f6586c53d1e4f934e3d2623b209066a12cc2
Mr.Kim <joajy98@gmail.com>
2023-10-28 23:07:58 +0900

    style: 예외 사항별 에러 출력문 추가

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -9,6 +9,9 @@ public class CarValidator {
     private static final int CHECK_IF_TRY_COUNT_LESS_THAN_ZERO = 0;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String STRING_REGEX = "^[\\w]*$";
+    private static final String CAR_NAME_LENGTH_ERROR = "자동차의 이름은 1글자 이상 5글자 이하여야 합니다.";
+    private static final String CAR_NAME_TYPE_ERROR = "자동차의 이름은 공백 없는 문자열이어야 합니다.";
+    private static final String TRY_COUNT_ERROR = "시도 횟수는 0회 이상이어야 합니다.";
 
     public static void checkAllException(List<Car> carList){
         for(Car carName : carList){
@@ -19,19 +22,19 @@ public class CarValidator {
 
     public static void checkCarNameLength(String carName) {
         if (carName.length() > MAX_CAR_NAME_LENGTH || carName.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR);
         }
     }
 
     public static void checkCarNameType(String carName) {
         if (!carName.matches(STRING_REGEX)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_TYPE_ERROR);
         }
     }
 
     public static void checkTryCount(int tryCount) {
         if (tryCount < CHECK_IF_TRY_COUNT_LESS_THAN_ZERO) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(TRY_COUNT_ERROR);
         }
     }
 }
