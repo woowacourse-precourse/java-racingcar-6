cd3902114b80770a91e01ad1c370176157ed15d6
donghoony <aru0504@naver.com>
2023-10-29 01:37:37 +0900

    feat(GameClientIO): 사용자로부터 총 이동 횟수를 입력받는 기능 구현
    - 주어진 문자열이 모두 숫자로 이뤄져있는지 확인한 뒤, 범위 내에 있는지 확인한다.

--- src/main/java/racingcar/GameClientIO.java
+++ src/main/java/racingcar/GameClientIO.java
@@ -4,10 +4,48 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class GameClientIO {
+    private static final int MIN_TOTAL_MOVES = 1;
+    private static final int MAX_TOTAL_MOVES = 1_000;
     private static final String DELIMITER = ",";
 
     public List<String> getNames() {
         String input = Console.readLine();
         return List.of(input.split(DELIMITER));
+    }
+
+    public int getTotalMoves() {
+        String input = Console.readLine();
+        validateContainingDigits(input);
+        int moves = convertTotalMoves(input);
+        return moves;
+    }
+
+    private int convertTotalMoves(String input) {
+        int moves = Integer.parseInt(input);
+        validateMovesInRange(moves);
+        return moves;
+    }
+
+    private void validateMovesInRange(int moves) {
+        validateLowerRange(moves);
+        validateUpperRange(moves);
+    }
+
+    private void validateLowerRange(int moves) {
+        if (moves < MIN_TOTAL_MOVES) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateUpperRange(int moves) {
+        if (moves > MAX_TOTAL_MOVES) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateContainingDigits(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
