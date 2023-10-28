31747bce442dee0c070e526105509718b62bb9ed
nohheeyeon <heeyeon1213@gmail.com>
2023-10-28 18:47:04 +0900

    feat : 최종 우승자를 출력하는 로직 구현

---
+++ src/main/java/ui/ResultView.java
@@ -0,0 +1,18 @@
+package ui;
+
+import domain.Car;
+
+import java.util.List;
+
+public class ResultView {
+    public static void printWinners(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            if (i < winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+}
