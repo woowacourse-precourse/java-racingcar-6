995b8e1e3c6e66e97611c88b3797baeeb7606a41
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-28 22:17:30 +0900

    refactor(domain):일급컬렉션을 위한 클래스 생성

---
+++ src/main/java/racingcar/domain/view/CarNames.java
@@ -0,0 +1,15 @@
+package racingcar.domain.view;
+
+import java.util.List;
+
+public class CarNames {
+    private List<String> carNames;
+
+    public CarNames(List<String> carNames) {
+        this.carNames = carNames;
+    }
+
+    public List<String> getCarNames() {
+        return carNames;
+    }
+}
---
+++ src/main/java/racingcar/domain/view/MoveCount.java
@@ -0,0 +1,17 @@
+package racingcar.domain.view;
+
+/*
+ *   입력받는 이동 횟수 정보를 담당
+ * */
+
+public class MoveCount {
+    private int moveCount;
+
+    public MoveCount(int moveCount) {
+        this.moveCount = moveCount;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
