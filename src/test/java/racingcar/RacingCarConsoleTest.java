03fc0d74967f84a98be1bb27c37dd93359082b6a
CodingApe9 <tlqkf6974@>
2023-10-28 20:19:48 +0900

    test(RacingCarConsoleTest) : 자동차 이름 입력 기능에 대한 테스트 작성

---
+++ src/test/java/racingcar/RacingCarConsoleTest.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarConsoleTest {
+
+    @Test
+    void 자동차_이름_입력받기_빈문자열_예외처리() {
+        Throwable result = catchThrowable(
+                () -> {
+                    String input = "";
+                    System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+                    RacingCarConsole racingCarConsole = new RacingCarConsole();
+                    racingCarConsole.readCarNames();
+                }
+        );
+
+        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름_입력받기_개행_예외_발생하지_않음() {
+        Throwable result = catchThrowable(
+                () -> {
+                    String input = "\n";
+                    System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+                    RacingCarConsole racingCarConsole = new RacingCarConsole();
+                    racingCarConsole.readCarNames();
+                }
+        );
+
+        assertThat(result).doesNotThrowAnyException();
+    }
+}
