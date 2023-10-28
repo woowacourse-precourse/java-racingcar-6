52dfb224186ed898d3d5e8919971d440774ba42c
donghoony <aru0504@naver.com>
2023-10-29 01:51:43 +0900

    test(RandomNumber): 주어진 크기의 무작위 수를 만들어주는 기능 테스트 작성
    - 주어진 크기만큼의 무작위 수를 만들어내는지 테스트한다.

---
+++ src/test/java/racingcar/RandomNumberGeneratorTest.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RandomNumberGeneratorTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10})
+    @DisplayName("주어진 크기만큼 랜덤 값을 생성한다.")
+    public void getRandomBySize(int size){
+        List<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumbers(size);
+        assertThat(randomNumbers).hasSize(size);
+    }
+}
