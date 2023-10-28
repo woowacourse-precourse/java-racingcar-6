6e80248149fb350901be6c0dcecbbfc7d73c32a7
unknown <gpfls981220@gmail.com>
2023-10-29 02:04:21 +0900

    feat: implement a function to create an object with the car name and current location

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,24 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 차 이름 입력받기
         OutputView.outputCar();
         String inputCar = InputView.InputCar();
+        
+        // TODO : 차 이름 예외상황 처리
+        
+        // 쉼표 기준으로 나누고 객체 생성
+        String[] carName = inputCar.split(",");
+        Car[] car = new Car[carName.length];
+        for (int i = 0; i < carName.length; i++) {
+            //System.out.println(carName[i]);
+            //car[i].name = carName[i];
+            //car[i].location = 0;
+            car[i] = new Car(carName[i]);
+            System.out.println(car[i].location + car[i].name);
+        }
+        
+        // 횟수 입력받기
         OutputView.outputTimes();
         String inputTimes = InputView.InputTimes();
         int times = HandleException.times(inputTimes);
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int location;
+    
+    Car(String name) {
+        this.location = 0;
+        this.name = name;
+    }
+}
