2bb118695a5170650e9b3068067a57b13a9d7986
2021110806 <2021110806@dgu.ac.kr>
2023-10-28 21:58:45 +0900

    feat(RacingGame.java): add getWinner method Cars 리스트에서 location 값이 가장 높은 Car 들의 name 리스트를 만들어 리턴하는 기능을 구현했습니다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class RacingGame {
@@ -36,5 +36,24 @@ public class RacingGame {
              currStatus.put(car.getName(), car.getLocation());
          }
          return currStatus;
+     }
+
+     ArrayList<String> getWinner() {
+        int maxLocation = -1;
+        HashMap<Integer, ArrayList<String>> carsClassifiedByLocation = new HashMap<>();
+        for (Car car : Cars) {
+            int currentLocation = car.getLocation();
+            if (currentLocation > maxLocation) {
+                maxLocation = currentLocation;
+            }
+            if (carsClassifiedByLocation.containsKey(currentLocation)) {
+                carsClassifiedByLocation.get(currentLocation).add(car.getName());
+                continue;
+            }
+            ArrayList<String> carsName = new ArrayList<>();
+            carsName.add(car.getName());
+            carsClassifiedByLocation.put(currentLocation, carsName);
+        }
+        return carsClassifiedByLocation.get(maxLocation);
      }
 }
