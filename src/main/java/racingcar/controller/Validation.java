80fdc772afe7c3f35c718d1d31070bd9bf368e7b
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 19:12:50 +0900

    feat : 자동차 이름이 5자 초과일 경우 에러

--- src/main/java/racingcar/controller/Validation.java
+++ src/main/java/racingcar/controller/Validation.java
@@ -1,6 +1,15 @@
 package racingcar.controller;
 
+import java.util.List;
+
 public class Validation {
 
+    public static void validateLength(List<String> cars) {
+        for (String car : cars) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
 }
