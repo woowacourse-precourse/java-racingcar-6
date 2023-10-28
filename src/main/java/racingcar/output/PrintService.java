9a2325aa441e7bce164d06b177e229dd34985694
cousim46 <wjdghldns24@naver.com>
2023-10-29 01:14:16 +0900

    feat : 회차별 결과를 출력하는 메서드 추가

--- src/main/java/racingcar/output/PrintService.java
+++ src/main/java/racingcar/output/PrintService.java
@@ -8,4 +8,8 @@ public class PrintService {
     public static void askAttemtCount() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
+
+    public static void show(String carName, int straightCount) {
+        System.out.printf("%s : %s\n",carName,"-".repeat(straightCount));
+    }
 }
