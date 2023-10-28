6a3624db97b630659e00d46f0091b7c662601cda
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 14:20:10 +0900

    test(Validator): 문자가 아닌 숫자가 포함된 예외처리 테스트 코드 작성

--- src/test/java/racingcar/util/validator/ValidatorTest.java
+++ src/test/java/racingcar/util/validator/ValidatorTest.java
@@ -25,4 +25,20 @@ public class ValidatorTest {
 	void checkNomalOperation1(String str) {
 		Validator.validateOutOfLength(str);
 	}
+	
+	@DisplayName("숫자가 포함되어 있다면 예외를 발생시킨다.")
+	@ValueSource(strings = { "jaa22", "122asa" })
+	@ParameterizedTest
+	void checkContainNumber(String str) {
+		assertThatThrownBy(() -> Validator.validateContainNumber(str))
+				.isInstanceOf(IllegalArgumentException.class)
+						.hasMessageContaining(ErrorMessage.CONTAIN_NUMBER_ERROR.getMessage());
+	}
+	
+	@DisplayName("숫자가 아닌 문자로만 구성되었을 때 정상적으로 작동하는 지 확인한다.")
+	@ValueSource(strings = { "nana", "jjaaa" })
+	@ParameterizedTest
+	void checkNomalOperation2(String str) {
+		Validator.validateContainNumber(str);
+	}
 }	
