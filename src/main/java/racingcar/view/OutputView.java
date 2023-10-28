7d53ce5317aeabb5a3702a555c5bb5aaf0d4bf05
minjun7410 <minjun7410@gmail.com>
2023-10-28 13:19:28 +0900

    feat(OutputView): 실행 결과 문구 출력 기능 구현
    * 경주를 시작할 때 처음 뜨는 결과 문구 출력 기능을 추가했다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,9 +3,13 @@ package racingcar.view;
 import racingcar.domain.TrialNumber;
 
 public class OutputView {
+    private static final String FIRST_RESULT_TEXT = "실행 결과\n";
     private static final String TRIAL_NUMBER_INPUT_TEXT = "시도할 회수는 몇회인가요?\n";
     private static final String CAR_NAME_INPUT_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
 
+    public static void printFirstResultText() {
+        System.out.println(FIRST_RESULT_TEXT);
+    }
     public static void printTrialNumberInputText() {
         System.out.println(TRIAL_NUMBER_INPUT_TEXT);
     }
