de8721199dfdd034cf67e477571fd0ef23955576
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:57:58 +0900

    style: 자바 컨벤션에 맞게 띄어쓰기 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -5,13 +5,13 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String inputCarName = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
     private static final String inputNumOfAttempt = "시도할 회수는 몇회인가요?";
-    public static String printUserInputCarNameMessage(){
+    public static String printUserInputCarNameMessage() {
         System.out.println(inputCarName);
         String canName = readLine();
         return canName;
     }
 
-    public static Integer printUserInputNumOfAttemptMessage(){
+    public static Integer printUserInputNumOfAttemptMessage() {
         System.out.println(inputCarName);
         Integer numOfAttempt = Integer.parseInt(readLine());
         return numOfAttempt;
--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -3,11 +3,11 @@ package View;
 public class OutputView {
     private static final String resultMessage = "실행 결과";
     private static final String winnerMessage = "최종 우승자 : ";
-    public static void printResultMessage(){
+    public static void printResultMessage() {
         System.out.println(resultMessage);
     }
 
-    public static void printWinnerMessage(){
+    public static void printWinnerMessage() {
         System.out.print(winnerMessage);
     }
 
