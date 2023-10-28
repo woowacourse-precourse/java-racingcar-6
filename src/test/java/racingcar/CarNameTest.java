398bc58bbdc8232d4114f7ba3c9d9b89557ecbbf
LEE-sh1673 <lsh901673@gmail.com>
2023-10-28 23:37:57 +0900

    test: 자동차 이름의 길이에 대한 검증 테스트 코드를 추가한다

--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -20,9 +21,17 @@ public class CarNameTest {
     }
 
     @Test
-    @DisplayName("글자 수가 5개를 초과하면 예외가 발생한다.")
+    @DisplayName("다섯 글자를 초과한 이름이 입력되면 예외가 발생한다.")
     void exceedFiveLetters_Then_ExceptionOccurs() {
         assertThatThrownBy(() -> CarName.create("123456"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "car", "car3", "12345"})
+    @DisplayName("다섯 글자 이하의 이름이 입력되면 예외가 발생하지 않는다.")
+    void lessThenFiveLetters_Then_NoExceptionOccurs(final String name) {
+        assertThatCode(() -> CarName.create(name))
+                .doesNotThrowAnyException();
     }
 }
