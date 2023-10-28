16c68291c8a7686824e3e40ca47bd50ba583dff2
jhsseonn <hs125bt@gmail.com>
2023-10-28 20:55:54 +0900

    feat: 경주 할 자동차 이름 입력받아 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 경주 할 자동차 이름 입력받기
+        String cars = Console.readLine();
+        String[] carArray = cars.split(",");
+
+
     }
 }
