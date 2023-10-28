69a45119de0b4fecfbd18f7c72b468d216ef3c37
JHyun0302 <wogustkfkd11@gmail.com>
2023-10-28 12:06:23 +0900

    :recycle: [refactor] playRacingGame 수정
    1. 'constantMessage' 변경
    2. 'indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다.' - 요구사항 만족을 위해 로직을 메서드로 추출

--- src/main/java/racingcar/service/PlayRacingGame.java
+++ src/main/java/racingcar/service/PlayRacingGame.java
@@ -1,30 +1,29 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import static racingcar.constant.constantMessage.RESULT_MESSAGE;
+
 import java.util.List;
 import racingcar.domain.Car;
-import racingcar.domain.Standard;
 
 public class PlayRacingGame {
     public void play(int moveCount, List<Car> cars) {
-        System.out.println();
-        System.out.println("실행 결과");
+        System.out.println("\n" + RESULT_MESSAGE);
 
         while (moveCount-- > 0) {
-            for (Car car : cars) {
-                int randomNum = Randoms.pickNumberInRange(0, 9);
-                // 랜덤 숫자가 4보다 크다면 이동!
-//                System.out.println("randomNum = " + randomNum);
-                if (randomNum >= Standard.goeFour.getValue()) {
-                    car.move();
-                }
-            }
+            moveCars(cars);
             printCarStatus(cars);
         }
     }
 
+    private void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            if (car.canMove()) {
+                car.move();
+            }
+        }
+    }
+
     private void printCarStatus(List<Car> cars) {
-        // 출력
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + car.getDistance());
         }
