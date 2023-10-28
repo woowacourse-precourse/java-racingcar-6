f8baea42937163694883772bcf24b007532abc2d
HYUNJUN\sajun <sajun28@naver.com>
2023-10-28 16:26:49 +0900

    Refactor: "이름 길이 변수 설정"

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,14 +1,18 @@
 package racingcar.model;
 
+import org.mockito.internal.configuration.FieldAnnotationProcessor;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class Name {
+
+    public static final int CAR_NAME_LENGTH = 5;
     public void isValid(List player) {
         for (Object playerName : player){
-            if (playerName.toString().length() > 5){
+            if (playerName.toString().length() > CAR_NAME_LENGTH){
                 throw new IllegalArgumentException();
             }
         }
