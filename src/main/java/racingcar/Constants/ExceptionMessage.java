2645dafbb1692aaec354bec680d237877f522513
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-10-29 02:55:20 +0900

    feat(ExceptionMessage): 에러 메세지 및 생성자 추가
    docs: ExceptionMassage 생성자 완료 체크

---
+++ src/main/java/racingcar/Constants/ExceptionMessage.java
@@ -0,0 +1,16 @@
+package racingcar.Constants;
+
+public enum ExceptionMessage {
+    OUT_OF_LENGTH("1~5글자 사이의 이름을 입력 하십시오"),
+    DUPLICATE_NAME("중복되지 않는 이름을 입력하세요"),
+    NONE_INPUT("값을 입력 하세요"),
+    NUMBER_ONLY("시도 횟수는 숫자만 입력 가능합니다"),
+    NUMBER_OVERFLOW("int의 범위를 벗어났습니다.");
+
+    private final String exceptionMassage;
+    ExceptionMessage(String exceptionMessage){
+        this.exceptionMassage = exceptionMessage;
+    }
+
+
+}
