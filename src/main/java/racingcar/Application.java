b6bf99f0b2b956212eeabda96b09535e95145442
changhyun9 <ckdgus1713@naver.com>
2023-10-28 23:41:51 +0900

    feat : add function to print guide of input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,9 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
