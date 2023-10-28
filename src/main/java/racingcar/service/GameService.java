13446b88fbc1a65fbf753b89c8a4c7a039907ea7
King's meow <mingee641@gmail.com>
2023-10-28 20:38:21 +0900

    feat: 입력값의 예외처리 기능 추가

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,11 +1,13 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import racingcar.model.Car;
+import racingcar.utils.Validation;
 
 public class GameService {
-
+  public Car[] car;
   public int tryNum;
+  Validation validation = new Validation();
 
 
   public void startGame() {
@@ -18,13 +20,29 @@ public class GameService {
   public void getInputCar() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     String input = Console.readLine();
+    splitInputName(input);
   }
 
+  // 입력받은 이름을 나눈다.
+  public void splitInputName(String input) {
 
+    String[] splited = input.split(",");
+    int carNum = splited.length;
+
+    car = new Car[carNum];
+
+    for(int i=0;i<car.length;i++) {
+      validation.isBlank(splited[i]);
+      validation.isLengthError(splited[i]);
+      validation.isDuplicated(splited[i], car);
+
+      car[i] = new Car(splited[i]);
+    }
+  }
 
   public void getInputTryNum() {
     System.out.println("시도할 횟수는 몇회인가요?");
-    tryNum = Console.readLine();
+    tryNum = validation.isNumber(Console.readLine());
   }
 
 }
---
+++ src/main/java/racingcar/utils/Validation.java
@@ -0,0 +1,34 @@
+package racingcar.utils;
+
+import racingcar.model.Car;
+
+public class Validation {
+
+  public void isBlank(String name) {
+    if(name == null || name.trim().isEmpty()) throw new IllegalArgumentException("공백이나 스페이스바가 있어요");
+  }
+
+  public void isLengthError(String name) {
+    if(name.length() > 5) throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+  }
+
+  public void isDuplicated(String name, Car[] car) {
+    for (Car c : car) {
+      if (c != null && c.getName().equals(name)) {
+        throw new IllegalArgumentException("동일한 이름이 있다.");
+      }
+    }
+  }
+
+  public int isNumber(String inputNum) {
+    try {
+      int num = Integer.parseInt(inputNum);
+      return num;
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("숫자가 아닙니다.");
+    }
+  }
+
+
+
+}
