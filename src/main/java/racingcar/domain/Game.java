7256abf9b0c5e5c01847324a4b0ffe80610eda0e
chjcoder <chjcoder@gmail.com>
2023-10-29 00:45:14 +0900

    feat: 자동차 이동 결과를 출력하는 기능 구현
    - Game클래스
    	- carPosition을 구하는 기능 추가
    	- carPosition을 화면에 출력하는 기능 추가

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -8,20 +8,23 @@ import java.util.List;
 public class Game {
     public void start() {
         /**
-         * 1. 차 이름 입력 받기
+         * 1. 차 객체 만들기
          * 2. 시도 횟수 입력 받기
          * 3. 실행
          * 4. 실행 결과 표시
          */
         List<Car> carList = makeCar();
         int numberOfAttempts = getNumberOfAttempts();
+        System.out.println("실행 결과");
         for (int count = 0; count < numberOfAttempts; count++) {
             play(carList);
-            showResult();
+            showResult(carList);
         }
+
     }
 
     private List<Car> makeCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         CarName carName = new CarName(Console.readLine());
         List<String> carNameList = carName.getCarNameStrList();
         List<Car> carList = new ArrayList<>();
@@ -33,16 +36,32 @@ public class Game {
     }
 
     private int getNumberOfAttempts() {
+        System.out.println("시도할 회수는 몇회인가요?");
         Attemp attemp = new Attemp(Console.readLine());
+        System.out.println();
         return attemp.getNumber();
     }
 
     private void play(List<Car> carList) {
-
+        for (Car car : carList) {
+            car.move();
+        }
 
     }
 
-    private void showResult() {
+    private void showResult(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.print(car.getCarName()+" : ");
+            System.out.println(carPosition(car.getCarPosition()));
+        }
+        System.out.println();
+    }
 
+    private String carPosition(int position) {
+        String positionStr = "";
+        for (int count = 0; count < position; count++) {
+            positionStr += "-";
+        }
+        return positionStr;
     }
 }
