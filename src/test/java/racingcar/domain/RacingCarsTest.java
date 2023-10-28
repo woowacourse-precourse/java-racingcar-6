154b746d6c5a2fcb9e628c29665660bf031c15c1
김승진 <ohksj77@gmail.com>
2023-10-28 21:20:26 +0900

    test(RacingCarsTest): 자동차 개수 반환 기능 테스트

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -38,12 +39,17 @@ class RacingCarsTest {
 
 
     @Test
+    @DisplayName("자동차 개수를 요청하면 올바른 개수를 반환하는가")
     void numOfElement() {
         // given
+        final List<RacingCar> elements = List.of(new RacingCar(new Name("abc")), new RacingCar(new Name("def")));
+        final RacingCars racingCars = new RacingCars(elements);
 
         // when
+        final int result = racingCars.numOfElement();
 
         // then
+        Assertions.assertThat(result).isEqualTo(elements.size());
     }
 
     @Test
