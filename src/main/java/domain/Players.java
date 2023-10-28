fcf22c4f6746de80538a070d6e929b1b153f2a9c
BullChallenger <am23am45@naver.com>
2023-10-29 01:18:36 +0900

    [FEAT] 기능 분리를 위한 Players 클래스 생성
    Players 클래스는 게임에 참여하는 자동차의 일급 컬렉션이며, 경기 진행 및 우승자 선출의 책임을 담당한다
    
    기존에 RacingCarService에서 담당하던 역할이 이전 되었으므로 RacingCarService의 코드 또한 변경되었다

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,49 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import util.RandomGenerator;
+
+public class Players {
+
+    private final List<RacingCar> players;
+
+    private static final String COLON_WITH_WHITESPACE = " : ";
+
+    public Players(List<RacingCar> players) {
+        this.players = players;
+    }
+
+    public String processRacing() {
+        StringBuilder builder = new StringBuilder();
+        for (RacingCar racingCar : players) {
+            if (racingCar.isMoveForward(RandomGenerator.generateNumber())) {
+                racingCar.moveForward();
+            }
+            builder.append(racingCar.getName()).append(COLON_WITH_WHITESPACE).append(racingCar.getLocation())
+                    .append("\n");
+        }
+        builder.append("\n");
+
+        return builder.toString();
+    }
+
+    public List<String> findWinners() {
+        int maxDistance = Integer.MIN_VALUE;
+        List<String> winners = new ArrayList<>();
+
+        for (RacingCar racingCar : players) {
+            int distance = racingCar.getLocation().length();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                winners.clear();
+                winners.add(racingCar.getName());
+            } else if (distance == maxDistance) {
+                winners.add(racingCar.getName());
+            }
+        }
+
+        return winners;
+    }
+
+}
--- src/main/java/service/RacingCarService.java
+++ src/main/java/service/RacingCarService.java
@@ -1,56 +1,39 @@
 package service;
 
+import domain.Players;
 import domain.RacingCar;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import util.RandomGenerator;
+import java.util.stream.Collectors;
 
 public class RacingCarService {
 
-    private List<RacingCar> racingCarList;
+    private Players players;
 
-    public void process(Set<String> carNameSet) {
-        racingCarList = generateRacingCar(carNameSet);
+    private static final String COMMA_WITH_WHITESPACE = ", ";
+    private static final String FINAL_WINNERS = "최종 우승자 : ";
+
+    public void generateRacingCar(Set<String> carNameSet) {
+        players = new Players(generate(carNameSet));
     }
 
-    private List<RacingCar> generateRacingCar(Set<String> carNameSet) {
-        List<RacingCar> racingCarList = new ArrayList<>();
-        for (String carName : carNameSet) {
-            racingCarList.add(new RacingCar(carName));
-        }
-
-        return racingCarList;
+    private static List<RacingCar> generate(Set<String> carNameSet) {
+        return carNameSet.stream().map(carName -> new RacingCar(carName))
+                                  .collect(Collectors.toList());
     }
 
-    public void startRacing(int cycle) {
-        // TODO: 메시지 View 로 전달하는 방식으로 변경하기
-        System.out.println("실행 결과");
+    public String racing(int cycle) {
+        StringBuilder builder = new StringBuilder();
+
         for (int i = 0; i < cycle; i++) {
-            for (RacingCar racingCar : racingCarList) {
-                if (racingCar.isMoveForward(RandomGenerator.generateNumber())) {
-                    racingCar.moveForward();
-                }
-                System.out.println(racingCar.getName() + " : " + racingCar.getLocation());
-            }
-            System.out.println();
+            builder.append(players.processRacing());
         }
 
-        // TODO: 승자 판정 로직 분리하기
-        List<String> winners = new ArrayList<>();
-        int maxDistance = Integer.MIN_VALUE;
-        for (RacingCar racingCar : racingCarList) {
-            int distance = racingCar.getLocation().length();
-            if (distance > maxDistance) {
-                maxDistance = distance;
-                winners.clear();
-                winners.add(racingCar.getName());
-            } else if (distance == maxDistance) {
-                winners.add(racingCar.getName());
-            }
-        }
+        return builder.toString();
+    }
 
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    public String judgeWinners() {
+        return FINAL_WINNERS + String.join(COMMA_WITH_WHITESPACE, players.findWinners());
     }
 
 }
