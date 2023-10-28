1d95afbc2d24ea693db8cea60e30b3866819b5ce
gihwanJang <gihwan319@gmail.com>
2023-10-29 06:00:28 +0900

    feat : (출력 기능) 우승자는 한 명 이상일 수 있음 기능

--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -9,59 +9,53 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-    String[] array = {"i", "my", "me"};
+    String[] array = { "i", "my", "me" };
     Game game = new Game(Arrays.asList(array), 3);
-    
+
     @Test
     void 주어진_횟수_동안_n대의_자동차는_전진_또는_멈춤() {
         assertRandomNumberInRangeTest(
-            () -> {
-                game.step();
-                Assertions.assertThat(
-                    "i : \nmy : \nme : "
-                    ).isEqualTo(game.getRaceInfo());
-                game.step();
-                Assertions.assertThat(
-                    "i : -\nmy : -\nme : -"
-                    ).isEqualTo(game.getRaceInfo());
-                game.step();
-                Assertions.assertThat(
-                    "i : -\nmy : -\nme : --"
-                    ).isEqualTo(game.getRaceInfo());
-            },
-            1,2,3, 
-            4,5,6, 
-            1,2,4
-        );
+                () -> {
+                    game.step();
+                    Assertions.assertThat(
+                            "i : \nmy : \nme : ").isEqualTo(game.getRaceInfo());
+                    game.step();
+                    Assertions.assertThat(
+                            "i : -\nmy : -\nme : -").isEqualTo(game.getRaceInfo());
+                    game.step();
+                    Assertions.assertThat(
+                            "i : -\nmy : -\nme : --").isEqualTo(game.getRaceInfo());
+                },
+                1, 2, 3,
+                4, 5, 6,
+                1, 2, 4);
     }
 
     @Test
     void 자동차_경주_게임을_완료한_후_누가_우승했는지를_알려줌() {
         assertRandomNumberInRangeTest(
-            () -> {
-                while(!game.isDone()) {
-                    game.step();
-                }
-                assertThat(game.getWinnersList()).contains("me");
-            },
-            1,2,3, 
-            4,5,6, 
-            1,2,4
-        );
+                () -> {
+                    while (!game.isDone()) {
+                        game.step();
+                    }
+                    assertThat(game.getWinnersList()).contains("me");
+                },
+                1, 2, 3,
+                4, 5, 6,
+                1, 2, 4);
     }
 
     @Test
     void 우승자는_한_명_이상일_수_있음() {
         assertRandomNumberInRangeTest(
-            () -> {
-                while(!game.isDone()) {
-                    game.step();
-                }
-                assertThat(game.getWinnersList()).contains("me", "my");
-            },
-            1,2,3, 
-            4,5,6, 
-            1,4,4
-        );
+                () -> {
+                    while (!game.isDone()) {
+                        game.step();
+                    }
+                    assertThat(game.getWinnersList()).contains("me", "my");
+                },
+                1, 2, 3,
+                4, 5, 6,
+                1, 4, 4);
     }
 }
