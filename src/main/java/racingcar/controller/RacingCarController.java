b186ddc3ae09d64301040a659949d7593551f0b1
lusate <rayrayr@naver.com>
2023-10-28 17:23:16 +0900

    자동차 경주 진행 로직 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,42 @@
 package racingcar.controller;
 
 
+import racingcar.model.Cars;
+import racingcar.model.TryCount;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
 public class RacingCarController {
+    private Cars cars;
+    private TryCount tryCount;
 
     public void run() {
+        Output.inputCarName();
+        Input.getCarName(); //pobi,woni,jun
 
+        Output.printNumber_of_attempts(); // 시도할 횟수는 몇회?
+        Input.getNumber_of_attemps();
+
+        //경주 실행
+        race();
+
+        //경주 끝 최종 우승자
+        theEnd();
+    }
+
+    private void race() {
+        Output.printExecutionResult(); // 실행 결과
+
+        int nowTrialCnt = 0;
+        while(tryCount.isSame(nowTrialCnt++)){
+            cars.moveByRandomNumber();
+            Output.printCarScore(cars.getCars());
+        }
     }
 
 
+
+    private void theEnd() {
+        Output.printWinner(cars.getWinners());
+    }
 }
