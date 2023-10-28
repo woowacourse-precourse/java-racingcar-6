0068b9f60973a79eaf62ce53c694a8d4cbe9526d
박준영 <wnsdud6969@naver.com>
2023-10-28 18:31:01 +0900

    feat(OutputView): add new feature
    Add new feature
    - prompt user to input number of game attempts

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,11 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String PROMPT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_NUMBER_OF_GAME_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private static OutputView outputView;
 
@@ -18,5 +21,9 @@ public class OutputView {
 
     public void promptCarNames() {
         System.out.println(PROMPT_CAR_NAMES_MESSAGE);
+    }
+
+    public void promptNumberOfGameAttempts() {
+        System.out.println(PROMPT_NUMBER_OF_GAME_ATTEMPTS_MESSAGE);
     }
 }
