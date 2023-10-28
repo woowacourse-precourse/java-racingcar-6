69a638ad08acb698df4606fdfde2cb7919758c1d
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 17:46:29 +0900

    refactor: GameResult(게임 결과) 생성자 유효성 검증 추가

--- src/main/java/racingcar/model/GameResult.java
+++ src/main/java/racingcar/model/GameResult.java
@@ -6,10 +6,15 @@ import java.util.Objects;
 public record GameResult(List<CarProgressResponse> progressResponses, List<String> winners) {
 
     public GameResult {
-        Objects.requireNonNull(progressResponses, "게임 결과는 비어있을 수 없습니다.");
+        Objects.requireNonNull(progressResponses, "게임 결과는 비어 있을 수 없습니다.");
+        Objects.requireNonNull(progressResponses, "우승자는 비어 있을 수 없습니다.");
 
-        if(progressResponses.size() < 1) {
-            throw new IllegalArgumentException("프로그램 결과는 하나 이상이어야 합니다.");
+        if(progressResponses.isEmpty()) {
+            throw new IllegalArgumentException("게임 결과는 하나 이상 이어야 합니다.");
+        }
+
+        if(winners.isEmpty()) {
+            throw new IllegalArgumentException("우승자는 한명 이상 이어야 합니다.");
         }
     }
 }
