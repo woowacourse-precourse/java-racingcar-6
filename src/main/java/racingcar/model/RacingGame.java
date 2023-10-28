6808dce1287d6b365950082294689c6b18504fbf
dltjdn <hana40004@naver.com>
2023-10-28 18:11:49 +0900

    feat(RacingGame): 클래스 및 생성자 정의
    * 경주할 자동차들과 시도 횟수를 가지고 자동차 경주 게임을 실행하는 클래스

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+public class RacingGame {
+
+    private final Cars cars;
+    private final int tryCount;
+
+    public RacingGame(Cars cars, int tryCount){
+        this.cars = cars;
+        this.tryCount = tryCount;
+    }
+
+}
