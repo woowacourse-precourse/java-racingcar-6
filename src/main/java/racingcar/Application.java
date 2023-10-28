5b148c6683039c2c30b13297b3f71835fb023c9f
rakyun1 <dbskim234@naver.com>
2023-10-27 22:20:06 -0600

    Feat: add checkCarName()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,33 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static java.lang.Integer.parseInt;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+
+    /**
+     *  user의 차이름 입력을 받아서 유효한 값인지 확인하고 String[]로 각 유저의 이름을 쪼개서 리턴
+     * @param users
+     * @return String[] carNames
+     */
+    private static String[] checkCarName(String users){
+        String[] carNames = users.split(",");
+        for (String name : carNames) {
+            if (name.trim().isEmpty()){
+                throw new IllegalArgumentException("정확한 이름을 입력해주세요");
+            }
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+            }
+        }
+        return carNames;
     }
 }
