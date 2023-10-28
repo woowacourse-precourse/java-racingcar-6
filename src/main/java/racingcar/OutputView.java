fc03e91bc9d9bea105e2da71cdcdb5dbfabaa84d
SUJI-J <suzy852@naver.com>
2023-10-28 21:30:50 +0900

    style: 메소드명 변경 printStart -> printRequestCar

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,9 +1,32 @@
 package racingcar;
 
+import java.util.List;
+
 import static racingcar.GameMessage.INPUT_CAR_MESSAGE;
+import static racingcar.GameMessage.INPUT_ATTEMPT_MESSAGE;
+import static racingcar.GameMessage.OUTPUT_RESULT_MESSAGE;
+import static racingcar.GameMessage.OUTPUT_WINNER_MESSAGE;
 
 public class OutputView {
-    public static void printStart() {
+
+    public static void printRequestCar() {
         System.out.println(INPUT_CAR_MESSAGE);
+    }
+
+    public static void printRequestAttempt() {
+        System.out.println(INPUT_ATTEMPT_MESSAGE);
+    }
+
+    public static void printGameResult() {
+        System.out.println(OUTPUT_RESULT_MESSAGE);
+    }
+
+    public static void printEnter() {
+        System.out.println();
+    }
+
+    public static void printGameWinner(List<String> winner) {
+        String result = String.join(",", winner);
+        System.out.println(OUTPUT_WINNER_MESSAGE + result);
     }
 }
