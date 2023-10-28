f1869207902de969e32587ba4ea5018bd193e393
LEM <ppoppo9802@naver.com>
2023-10-29 04:03:01 +0900

    feat: 회수를 입력하는 기능에 예외 처리 추가

--- src/main/java/racingcar/input/Count.java
+++ src/main/java/racingcar/input/Count.java
@@ -6,7 +6,30 @@ public class Count {
     public int inputCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String input = readLine();
+        checkExeption(input);
         int count = Integer.parseInt(input);
         return count;
+    }
+    public void checkExeption(String input) {
+        checkIsPositive(input);
+        checkEmpty(input);
+        checkIsInteger(input);
+    }
+    public void checkIsPositive(String count) {
+        if (Integer.parseInt(count) < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkEmpty(String count) {
+        if (count.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkIsInteger(String count) {
+        try {
+            Integer.parseInt(count);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
