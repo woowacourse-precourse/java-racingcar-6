61b2479a78979837472d4955b63fb3682552415b
Sully <2dcoder@naver.com>
2023-10-28 23:28:28 +0900

    feat(CarNameValidator): carName에 영문자와 쉼표가 아닌 다른 입력이 들어온다면 예외가 발생하는 메서드의 테스트 코드 추가
    통과되는 메서드는 하나만 필요하다.

--- src/test/java/racingcar/domain/CarNameValidatorTest.java
+++ src/test/java/racingcar/domain/CarNameValidatorTest.java
@@ -7,6 +7,18 @@ import org.junit.jupiter.api.Test;
 
 class CarNameValidatorTest {
 
+    @DisplayName("모든 경우의 수를 따져서 올바른 carName일 경우에는 예외가 발생하지 않는다.")
+    @Test
+    void validateAllCasesDoesNotThrowAnyException() {
+        // given
+        String validCarName = "sully";
+
+        // when
+        // then
+        assertThatCode(() -> new CarNameValidator(validCarName))
+                .doesNotThrowAnyException();
+    }
+
     @DisplayName("validateCarNameExists()로 자동차 이름을 입력하지 않으면 ''으로 저장되기 때문에 예외가 발생한다.")
     @Test
     void validateCarNameExistsWithBlank() {
@@ -20,16 +32,17 @@ class CarNameValidatorTest {
                 .hasMessage("자동차 이름을 입력해주세요.");
     }
 
-    @DisplayName("validateCarNameExists()로 자동차 이름을 입력하면 예외가 발생하지 않는다.")
+    @DisplayName("validateAlphanumericAndCommaCarName()로 자동차 이름이 영숫자와 콤마로만 구성되지 않는 경우 예외가 발생한다.")
     @Test
-    void validateCarNameExists() {
+    void validateAlphanumericAndCommaCarNameWithInvalidCarName() {
         // given
-        String validCarName = "sully";
+        String invalidCarName = "sully^^";
 
         // when
         // then
-        assertThatCode(() -> new CarNameValidator(validCarName))
-                .doesNotThrowAnyException();
+        assertThatThrownBy(() -> new CarNameValidator(invalidCarName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 영숫자와 콤마로만 구성되어야 합니다.");
     }
 
 }
