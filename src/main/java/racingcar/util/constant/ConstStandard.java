e2c8019cc3e2a4fc52295c2d81fc1226ee778db6
yummygyudon <bang2brew@gmail.com>
2023-10-29 01:27:18 +0900

    FEAT : 요구 사항 기준 값 및 명령어 값 등 상수값 구현 및 추상 클래스로 관리
    - `ConstStandard` : 기준 값 / 비교 값 상수 저장
    - `ConstVariable` : 절대적으로 출력되어야 하는 출력 값 / 활용 값 상수 저장

---
+++ src/main/java/racingcar/util/constant/ConstStandard.java
@@ -0,0 +1,13 @@
+package racingcar.util.constant;
+
+public abstract class ConstStandard {
+
+    public static final int CRITERIA_MOVE = 4;
+
+    public static final int CRITERIA_NAME_UPPER_LIMIT = 5;
+
+    public static final String CRITERIA_SEPARATE = ",";
+
+    public static final String INVALID_SEPARATES = "<>\"'[]{}~!@#$%^&*()\\-_+=:;.";
+
+}
---
+++ src/main/java/racingcar/util/constant/ConstVariable.java
@@ -0,0 +1,9 @@
+package racingcar.util.constant;
+
+public abstract class ConstVariable {
+    public static final String RESULT_SEPARATOR = " : ";
+    public static final String RESULT_START_MESSAGE = "실행 결과";
+    public static final String RESULT_MOVEMENT = "-";
+    public static final String RESULT_WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String RESULT_WINNER_SEPARATOR = ", ";
+}
