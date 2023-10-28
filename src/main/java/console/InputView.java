20430877dd095bdcc92aca39f930a458c1b7f3ac
DK <laancer4@gmail.com>
2023-10-28 18:35:50 +0900

    feat : InputView 구현

---
+++ src/main/java/console/InputView.java
@@ -0,0 +1,19 @@
+package console;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String NON_NUMERIC_INPUT = "숫자만 입력할 수 있다.";
+    public String getInput(){
+        return Console.readLine();
+    }
+
+    public int getNumericInput(){
+        String userInput = Console.readLine().trim(); // trim 해서 공백 제거
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(NON_NUMERIC_INPUT);
+        }
+    }
+}
