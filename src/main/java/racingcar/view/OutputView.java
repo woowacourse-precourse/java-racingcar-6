7b5380a13e7f04536d6837c8341839b1b1fadd7b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 17:24:16 +0900

    feat(OutputView): 시도 수만큼 자동차들의 이름과 위치를 출력하는 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
+import racingcar.util.message.ProgressMessage;
+
+public class OutputView {
+	
+	public void printInputCarName() {
+		System.out.println(ProgressMessage.INPUT_CARNAME.getMessage());
+	}
+	
+	public void printInputTryNumber() {
+		System.out.println(ProgressMessage.INPUT_TRY_NUMBER.getMessage());
+	}
+	
+	public void printExecutionResult(String result, int tryNumber) {
+		StringJoiner strJoiner = new StringJoiner(System.lineSeparator());
+		
+		IntStream.range(0, tryNumber).forEach(x -> strJoiner.add(result));
+		
+		System.out.println(ProgressMessage.EXECUTION_RESULT.getMessage() + "\n" + strJoiner.toString());
+	}
+}
