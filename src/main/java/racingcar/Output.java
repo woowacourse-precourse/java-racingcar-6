b83c8f8ae149b040633465c13a438b7c6284cf06
2021110806 <2021110806@dgu.ac.kr>
2023-10-28 22:11:31 +0900

    feat(Output.java): add printCarNameInputRequest method 경주할 자동차 이름 입력을 요청하는 문구를 출력하는 기능을 구현했습니다.

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Output {
+    private final String CAR_NAME_INPUT_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String TRY_NUMBER_INPUT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String RESULT_TITLE = "실행 결과";
+
+    void printCarNameInputRequest() {
+        System.out.println(CAR_NAME_INPUT_REQUEST_MESSAGE);
+    }
+}
