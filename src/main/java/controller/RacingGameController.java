848b3ef4b9c2f7e375d29357ab378dab46ae2dcf
WonyuChoi <phillip07290@naver.com>
2023-10-28 22:52:47 +0900

    feat: 컨트롤러에 심판 객체를 주입

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,7 +1,12 @@
 package controller;
 
 import controller.dto.CarNames;
+import controller.dto.MoveResult;
+import java.util.List;
+import model.Car;
 import model.Cars;
+import model.RandomReferee;
+import model.Referee;
 import model.TryCount;
 import view.InputView;
 import view.OutputView;
@@ -13,9 +18,21 @@ public class RacingGameController {
     private Cars cars;
     private TryCount tryCount;
 
-    public RacingGameController(final OutputView outputView, final InputView inputView) {
+    public RacingGameController(final OutputView outputView, final InputView inputView,
+        final Referee referee) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.referee = referee;
+    }
+
+    public static RacingGameController createDefault(final OutputView outputView,
+        final InputView inputView) {
+        return new RacingGameController(outputView, inputView, new RandomReferee());
+    }
+
+    public static RacingGameController createControllerWithReferee(final OutputView outputView,
+        final InputView inputView, final Referee referee) {
+        return new RacingGameController(outputView, inputView, referee);
     }
 
     public void run() {
