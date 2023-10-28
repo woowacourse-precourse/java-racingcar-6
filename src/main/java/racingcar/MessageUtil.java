690e90cb6691c79b36248e69f85a06907bc47f50
minw0095 <kmu6898@naver.com>
2023-10-28 21:26:30 +0900

    feat: 메세지 유틸 관련 구현

---
+++ src/main/java/racingcar/MessageUtil.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class MessageUtil {
+    final static String Carname_Message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    final static String Try_Message = "시도할 회수는 몇회인가요?";
+
+}
