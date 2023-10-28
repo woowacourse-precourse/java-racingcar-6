c262d8053c1267cec1397a5fdb725c3a8b49302d
JoonKyo <joonkyolee@gmail.com>
2023-10-29 00:20:46 +0900

    feat(InputView): 시도 회수를 입력받는 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String ASK_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String askCarName(){
         System.out.println(ASK_CAR_NAME_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String askAttemptNumber(){
+        System.out.println(ASK_ATTEMPT_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
