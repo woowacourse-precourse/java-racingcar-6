a493b272c47951c6bee31215d2611ca9d8061fd3
JongMinCh0i <wonderland314592@gmail.com>
2023-10-28 21:03:54 +0900

    feat(InputView): 이동시도 횟수 입력 문구 출력 기능

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String NOT_PERMIT_BLANK = "공백은 허용하지 않습니다.";
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String HOW_MANY_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
@@ -18,6 +19,10 @@ public class InputView {
             throw new IllegalArgumentException(NOT_PERMIT_BLANK);
         }
         return input;
+    }
+
+    public void inputHowManyTry() {
+        System.out.println(HOW_MANY_TRY_MESSAGE);
     }
 
 }
