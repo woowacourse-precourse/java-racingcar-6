4c1eba81c077a1d623ee6e3ba3dcccca4d755e25
sunwooiljoon <wnsdl1659@naver.com>
2023-10-28 14:07:32 +0900

    feat : RacingCarGame.requestCarNames 구현
    사용자 입력을 받은 후 split하고 RacingCar 생성하여 List에 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RacingCarGame {
@@ -24,6 +26,11 @@ public class RacingCarGame {
 
     private void requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();
+        String[] carNames = carNameInput.split(",");
+        Arrays.stream(carNames).forEach(carName -> {
+            racingCarList.add(new RacingCar(carName));
+        });
     }
 
     private void requestTryNumber() {
