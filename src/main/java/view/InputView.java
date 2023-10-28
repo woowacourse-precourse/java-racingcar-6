d6adbefba1efa3ad7c22b50c56c9d5f6f94f0c03
star-candy <el0710@naver.com>
2023-10-28 22:45:36 +0900

    fix(racingcar): 입출력 요구사항에 따른 출력값 수정, 주석 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,6 @@ public class InputView {
         return Console.readLine();
     }
     public static String inputNumberOfMatch() {
-        return Console.readLine(); //model에서 int로 변환할 것
+        return Console.readLine();
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,21 +8,20 @@ public class OutputView {
     }
     public static void printNumberOfRacing() {
         System.out.println("시도할 회수는 몇회인가요?");
-
     }
     public static void printRacingResult(List<String>carName, List<Integer>raceResult) {
         System.out.println("실행 결과");
         for(int i = 0; i < carName.size(); i++) {
-        System.out.print(carName.get(i));
-            while(i < raceResult.size()) {
+            String stringName = carName.get(i);
+            System.out.printf("%s : ", stringName);
+            for(int j = 0; j < raceResult.get(i); j++) {
                 System.out.print('-');
             }
             System.out.println();
         }
-
     }
     public static void printRacingWinner(List<String>winnerName) {
     String winner = String.join(", ", winnerName);
-    System.out.println(winner);
+    System.out.printf("최종 우승자 : %s", winner);
     }
 }
