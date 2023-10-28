001ac96939658e9a0693073f34fa134976c39017
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 21:43:46 +0900

    style: 메소드명 변경

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import model.GroupOfCar;
+import model.Race;
 import view.InputView;
 import model.Car;
 import validator.InputValidator;
@@ -10,18 +11,26 @@ public class Controller {
     private final InputValidator inputValidator = new InputValidator();
     private final GroupOfCar groupOfCar = new GroupOfCar();
 
-    public void run(){ //프로그램의 시작
-        setCarName(parseCarName(getCarNameByInput()));
+    public void run() { //프로그램의 시작
+        createCar(parseCarName(getCarNameByInput()));
 
     }
 
-    private void setCarName(String[] carName) {
+    private void startRace() {
+        Race race = new Race(getNumOfAttemptByInput());
+    }
+
+    private int getNumOfAttemptByInput() {
+        return Integer.parseInt(readLine());
+    }
+
+    private void createCar(String[] carName) {
         for(String name: carName) {
             groupOfCar.addCar(new Car(name));
         }
     }
 
-    private String[] parseCarName(String input){
+    private String[] parseCarName(String input) {
         String carNames = getCarNameByInput();
         return carNames.split(",");
     }
