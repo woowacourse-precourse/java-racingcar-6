2e93b203354f853d8f3e56bf45c87028597eebba
eora21 <eora21@naver.com>
2023-10-28 17:39:59 +0900

    feat: 🎸 (IOView) 추가 View의 구현체에서 차들의 이름을 받아 반환하는 메서드 작성

---
+++ src/main/java/racingcar/view/IOView.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class IOView implements View {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public IOView(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    @Override
+    public List<String> getCarNames() {
+        outputView.requestCarNames(inputView.getDelimiterDescription(), inputView.getDelimiter());
+        return inputView.enterCarNames();
+    }
+}
