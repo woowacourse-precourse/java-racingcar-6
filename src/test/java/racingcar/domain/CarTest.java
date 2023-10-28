8245569241e1a1adb2472604606c9a34161728ab
unknown <y2kf9a0q@gmail.com>
2023-10-29 00:11:56 +0900

    test: CarTest.java - getters

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
     String name;
     Car car;
@@ -15,10 +17,14 @@ class CarTest {
 
     @Test
     void getDistance() {
+        int result = car.getDistance();
+        assertThat(result).isEqualTo(0);
     }
 
     @Test
     void getName() {
+        String result = car.getName();
+        assertThat(result).isEqualTo("default");
     }
 
     @Test
