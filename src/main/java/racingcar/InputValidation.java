d8ae574998f830f092537743f0c659d391052e50
Chenowoo <cjsgusdn14@naver.com>
2023-10-28 15:51:40 +0900

    feat(InputValidation): 자동차의 수와 입력받은 이름의 수가 맞는지 확인하는 기능 구현 자동차의 수(n)와 입력받은 이름의 수가 맞는지 확인하고 에러를 발생시키는 기능 구현

--- src/main/java/racingcar/InputValidation.java
+++ src/main/java/racingcar/InputValidation.java
@@ -5,35 +5,41 @@ import java.util.Arrays;
 public class InputValidation {
 
     public static void validateNumberOfCarsRange(int numberOfCars) {
-        if(numberOfCars < NumberConst.MIN_NUMBER_OF_CAR && numberOfCars > NumberConst.MAX_NUMBER_OF_CAR) {
+        if(numberOfCars < NumberConst.MIN_NUMBER_OF_CAR || numberOfCars > NumberConst.MAX_NUMBER_OF_CAR) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void validateSeparator(String userInputCarName) {
+    public static void validateInputCorrectSeparator(String userInputCarName) {
         String deleteAllWords = userInputCarName.replaceAll("[가-힣a-zA-Z0-9,]", "");
         if(deleteAllWords.length() != 0) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void validateNameOfCarsRange(String[] nameOfCars) {
-        for(String name : nameOfCars) {
-            if(name.length() < NumberConst.MIN_NAME_OF_CAR_LENGTH && name.length() > NumberConst.MAX_NAME_OF_CAR_LENGTH) {
+    public static void validateInputEnoughNames(int numberOfCars, String[] namesOfCars) {
+        if(namesOfCars.length != numberOfCars) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateNameOfCarsRange(String[] namesOfCars) {
+        for(String name : namesOfCars) {
+            if(name.length() < NumberConst.MIN_NAME_OF_CAR_LENGTH || name.length() > NumberConst.MAX_NAME_OF_CAR_LENGTH) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    public static void validateDuplicateName(String[] nameOfCars) {
-        Object[] newNameOfCars = Arrays.stream(nameOfCars).distinct().toArray();
-        if(newNameOfCars.length != nameOfCars.length) {
+    public static void validateDuplicateName(String[] namesOfCars) {
+        Object[] newNameOfCars = Arrays.stream(namesOfCars).distinct().toArray();
+        if(newNameOfCars.length != namesOfCars.length) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateMoveCountRange(int carMovingCount) {
-        if(carMovingCount < NumberConst.MOVING_MIN_COUNT && carMovingCount > NumberConst.MOVING_MAX_COUNT) {
+        if(carMovingCount < NumberConst.MOVING_MIN_COUNT || carMovingCount > NumberConst.MOVING_MAX_COUNT) {
             throw new IllegalArgumentException();
         }
     }
