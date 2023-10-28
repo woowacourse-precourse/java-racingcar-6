917da9aaa3232ad3d76378979e1789312a2d7ede
soozzang <soozzang@inha.edu>
2023-10-28 16:05:02 +0900

    Feat(OutputView): printStartMessage(), printRoundNumMessage() 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+public class OutputView {
+    public void printStartMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void printRoundNumMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printRoundResult() {
+
+    }
+
+    public void printFinalResult() {
+
+    }
+}
