4213b094869d578aab37ef76c4ea941ddf0086a6
donghoony <aru0504@naver.com>
2023-10-29 02:41:40 +0900

    test(RacingCarStatus): 자동차의 상태를 문자열로 변환하는 기능 테스트 작성
    - `toString()`이 올바르게 작동하는지에 대한 테스트 작성

---
+++ src/test/java/racingcar/RacingCarStatusTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCarStatusTest {
+
+    @Test
+    @DisplayName("자동차의 상태를 정확하게 문자열로 변환한다.")
+    public void toStringTest(){
+        RacingCarStatus status = RacingCarStatus.of("abc", 5);
+        String statusString = status.toString();
+
+        assertThat(statusString).isEqualTo("abc : -----");
+    }
+}
