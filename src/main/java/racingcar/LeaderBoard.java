0e174b979e9154f9f0e4cd2aceec95caab393782
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-28 17:16:10 +0900

    feat: add a feature to save generated random numbers into a list for test

--- src/main/java/racingcar/LeaderBoard.java
+++ src/main/java/racingcar/LeaderBoard.java
@@ -1,25 +1,31 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class LeaderBoard {
+    private final List<Integer> paceMapValueList = new ArrayList<>();
 
     public void showRace(Machines machines, Lap lap, PaceComputer paceComputer) {
 
         System.out.println();
         System.out.println("실행 결과");
 
-        LinkedHashMap<String, Integer> paceMap = paceComputer.createDefaultPaceMap(machines);
+        LinkedHashMap<String, Integer> currentPaceMap = paceComputer.createDefaultPaceMap(machines);
 
         for (int i = 0; i < Integer.parseInt(lap.getLap()); i++) {
-            paceComputer.updatePaceMap(paceMap);
+            paceComputer.updatePaceMap(currentPaceMap);
+            paceMapValueList.addAll(currentPaceMap.values());
 
-            paceMap.forEach((machineName, driveSuccessNumber) ->
+            currentPaceMap.forEach((machineName, driveSuccessNumber) ->
                     System.out.println(machineName + " : " + driveSuccessNumber));
 
             System.out.println();
         }
+    }
+
+    public List<Integer> getPaceMapValueList() {
+        return paceMapValueList;
     }
 }
