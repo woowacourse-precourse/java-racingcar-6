a62e6fc919e3765b6db466c7ea33f425f73a151c
dong2ast <kdh01132006@naver.com>
2023-10-28 21:43:56 +0900

    ✨ feat: 예외 처리하는 객체 생성 및 기존 로직 분리

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -3,6 +3,7 @@ package racingcar.service;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.exception.Exception;
 
 public class GameService {
 
@@ -11,22 +12,16 @@ public class GameService {
         checkCarNameAndMakeCar(InteractService.getCarName(), carList);
 
         int tryNum = InteractService.getTryNum();
-        checkPositive(tryNum);
+        Exception.checkPositive(tryNum);
 
         InteractService.printResult(carList, tryNum);
-    }
-
-    private static void checkPositive(int tryNum) {
-        if (tryNum <= 0) {
-            throw new IllegalArgumentException();
-        }
     }
 
     private static void checkCarNameAndMakeCar(String carNames, List<Car> carList) {
         List<String> carNameList = new ArrayList<>();
 
         for (String s : carNames.split(",")) {
-            if (checkNameValidation(carNameList, s)) {
+            if (Exception.checkNameValidation(carNameList, s)) {
                 throw new IllegalArgumentException();
             }
             carNameList.add(s);
@@ -34,7 +29,5 @@ public class GameService {
         }
     }
 
-    private static boolean checkNameValidation(List<String> carNameList, String s) {
-        return s.contains(" ") || s.length() > 5 || carNameList.contains(s);
-    }
+
 }
