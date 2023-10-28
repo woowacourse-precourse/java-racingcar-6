9197184cbd9106034f1cc4f0fa3e7b93cb9b0a01
unknown <y2kf9a0q@gmail.com>
2023-10-29 00:06:47 +0900

    test: CarTest.java - setUp()

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+    String name;
+    Car car;
 
     @BeforeEach
     void setUp() {
+        name = "default";
+        car = new Car(name);
     }
 
     @Test
