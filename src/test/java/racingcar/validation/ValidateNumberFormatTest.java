52b952c6c00f44fb935c64bdeb17e69b208f4d63
usnim <sms897@naver.com>
2023-10-29 00:15:33 +0900

    [feat] 시도할 횟수 입력 기능 test : 시도할 횟수 입력에 대한 검증 기능, 예외 발생 test

---
+++ src/test/java/racingcar/validation/ValidateNumberFormatTest.java
@@ -0,0 +1,22 @@
+package racingcar.validation;
+
+import org.junit.jupiter.api.Test;
+import racingcar.exception.InvalidBlankNameException;
+import racingcar.exception.InvalidNumberFormatException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateNumberFormatTest {
+
+    ValidateNumberFormat validateNumberFormat;
+    @Test
+    void validate_이름이_공백일때_예외처리() {
+        String input = "숫자";
+
+        assertThatThrownBy(() -> validateNumberFormat.validate(input))
+                .isInstanceOf(InvalidNumberFormatException.class)
+                .hasMessageContaining("[에러] 입력된 값이 숫자형식이 아닙니다.");
+    }
+
+}
