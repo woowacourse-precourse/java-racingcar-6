d9c079d6c6437b754278f12233c26e4acaca0c3b
CodingApe9 <tlqkf6974@>
2023-10-28 22:03:14 +0900

    feat(InputValidator) : 하나의 자동차 이름을 검증하는 기능 추가

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -8,4 +8,11 @@ public class InputValidator {
             throw new IllegalArgumentException("검증할 자동차 이름 목록이 없습니다.");
         }
     }
+
+    public static void validateCarName(String carName) {
+        String carNamePattern = "\"^[a-zA-Z가-힣]{1,5}$\"";
+        if (!carName.matches(carNamePattern)) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자의 한글, 영어 대소문자로 구성됩니다.");
+        }
+    }
 }
