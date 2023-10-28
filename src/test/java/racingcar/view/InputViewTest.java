ff34e455430bfaf1547e87aaa424b18a76f1e77a
gihwanJang <gihwan319@gmail.com>
2023-10-29 00:59:58 +0900

    test : (입력기능) 자동차 이름이 5글자보다 긴 경우 예외 발생 테스트 작성

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -20,5 +21,17 @@ public class InputViewTest {
         List<String> carNameList = inputView.inputCarNames();
         // then
         assertThat(carNameList).contains("Morning", "Tico");
+    }
+
+    @Test
+    void 자동차_이름이_5글자보다_긴_경우_예외_발생() {
+        // given
+        String userInput = "Morning,Tico";
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+        // when & then
+        assertThatThrownBy(() -> inputView.inputCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5글자 이하여야 합니다.");
     }
 }
