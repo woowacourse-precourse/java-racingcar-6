3527186cc1307fba71e8fd31109884ea0cf4f526
BINBEAN777 <peanutchan@naver.com>
2023-10-28 13:03:04 +0900

    feat : Output.java 클래스 생성 / tellStartAndGetName,tellGroupName 메소드 생성 게임의 시작을 알리는 멘트 출력 , 입력받은 이름들 출력

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,4 +1,13 @@
 package racingcar;
 
 public class Output {
-}
+
+    public static void tellStartAndGetName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    };
+
+    public static void tellGroupName(String [] groupName){
+        System.out.println(String.join(",",groupName));
+        }
+    }
+
