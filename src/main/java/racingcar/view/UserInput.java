ea78741eeb0702b63b4ee8c2e38444844f8ceb74
Dawin <dawin002@naver.com>
2023-10-28 17:09:29 +0900

    feat(UserInput): 자동차 이름을 입력 받는 기능 추가
    자동차 이름을 입력받아 반환하는 inputCarNames() 메서드 추가.
    camp.nextstep.edu.missionutils.Console API의 readLine() 메서드로 사용자 입력 받음.
    String 타입으로 반환.

---
+++ src/main/java/racingcar/view/UserInput.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class UserInput {
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
+}
