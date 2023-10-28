bef9a3b20dcf28566ff20007a0128fbc39c3f9b5
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-28 21:00:13 +0900

    refactor(domain):GamePlayer 클래스 정적 팩토리 메서드 적용

--- src/main/java/racingcar/domain/GamePlayer.java
+++ src/main/java/racingcar/domain/GamePlayer.java
@@ -1,7 +1,5 @@
 package racingcar.domain;
 
-import static racingcar.constant.CarConstant.INITIAL_BEST_RECORD;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,23 +9,15 @@ import java.util.List;
 
 public class GamePlayer {
     private final List<Car> cars = new ArrayList<>();
-    private int bestRecord = INITIAL_BEST_RECORD;
 
-    public GamePlayer() {
-    }
-
-    public void addCar(Car car) {
-        cars.add(car);
-    }
-
-    //모든 이름을 리스트로 가져오는 것 중에 이름의 리스트를 클래스로 설정해주는것이 좋은가?
-    //테스트 에서만 사용하는데 의미가 있을까?
-    public List<String> getAllCarNames() {
-        List<String> names = new ArrayList<>();
-        for (Car car : cars) {
-            car.addNameToList(names);
+    private GamePlayer(List<String> carNames) {
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
         }
-        return names;
+    }
+
+    public static GamePlayer from(List<String> carNames) {
+        return new GamePlayer(carNames);
     }
 
     public void moveCarsByCount(int count) {
@@ -41,13 +31,6 @@ public class GamePlayer {
         for (Car car : cars) {
             car.move();
         }
-    }
-
-    @Override
-    public String toString() {
-        return "GamePlayer{" +
-                "cars=" + cars +
-                '}';
     }
 
     public GameRank toGameRank() {
