52027a0ffefaf7407e5d2d8e2e5c9ada1a3347ff
JHyun0302 <wogustkfkd11@gmail.com>
2023-10-28 12:01:38 +0900

    :recycle: [refactor] userInput 수정
    'constantMessage' 변경

--- src/main/java/racingcar/service/UserInput.java
+++ src/main/java/racingcar/service/UserInput.java
@@ -1,18 +1,21 @@
 package racingcar.service;
 
+import static racingcar.constant.constantMessage.CAR_NAME_INPUT_MESSAGE;
+import static racingcar.constant.constantMessage.TRY_COUNT_INPUT_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
     public String[] inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
         String input = Console.readLine();
         String[] names = input.split(",");
 
         return names;
     }
 
-    public int tryCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
+    public String tryCount() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        return Console.readLine();
     }
 }
