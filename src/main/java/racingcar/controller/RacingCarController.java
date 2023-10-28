d03a963d4de84337e99f0246902e3f5d3cfb9d6d
yuseonjun <seonjun0906@gmail.com>
2023-10-28 17:18:44 +0900

    feat: 만든 domain과 view를 연결시켜 프로그램의 흐름을 제어하는 Controller 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,9 +2,28 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.domain.Cars;
+import racingcar.domain.NumberGenerator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingCarController {
+
+    private final NumberGenerator numberGenerator;
+
+    public RacingCarController(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void start() {
+        Cars cars = generateCar();
+        int tryCount = InputView.readTryCount();
+        OutputView.printPlayResult();
+        for (int i = 0; i < tryCount; i++) {
+            cars.move(numberGenerator);
+            OutputView.printResult(cars);
+        }
+        OutputView.printWinner(cars);
+    }
 
     private Cars generateCar() {
         List<String> cars = InputView.readCarNames();
