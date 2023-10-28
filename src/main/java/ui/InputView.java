b43b0fe44379b041b236eb0a759ace6c64e5a8a1
nohheeyeon <heeyeon1213@gmail.com>
2023-10-28 19:13:22 +0900

    feat : 자동차 이름 유효성 검사 기능 추가

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -19,8 +19,14 @@ public class InputView {
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
-
     private static List<String> splitByComma(String input) {
         return Arrays.asList(input.split(","));
+    }
+    private static void validateCarNames(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
     }
 }
