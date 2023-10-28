4fac9187685ddd04ac0141d8b668b83236b6f266
hyuna <monicajo0@naver.com>
2023-10-28 21:38:37 +0900

    feat: 시도횟수 입력 받기 및 유효성 검사 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -8,12 +8,18 @@ import java.util.List;
 public class Game {
     private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final InputValidator inputValidator = new InputValidator();
+    private static final String TRIAL_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private final List<Car> cars = new ArrayList<>();
+
+    private int trialNumber;
 
     public void start() {
         printMessage(START_MESSAGE);
         setCarNames();
+
+        printMessage(TRIAL_NUMBER_MESSAGE);
+        setTrialNumber();
     }
 
     private void printMessage(String message) {
@@ -27,5 +33,11 @@ public class Game {
             inputValidator.validCarNameLength(carName);
             cars.add(new Car(carName));
         }
+    }
+
+    private void setTrialNumber(){
+        String input = Console.readLine();
+        inputValidator.validTrialNumber(input);
+        trialNumber = Integer.parseInt(input);
     }
 }
--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -3,11 +3,31 @@ package racingcar;
 public class InputValidator {
     private static final int CAR_NAME_MIN_LENGTH = 1;
     private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final String ONLY_NUMBER_REGEX = "[0-9]+";
     private static final String CAR_NAME_LENGTH_EXCEPTION = "자동차 이름은 1자 이상 5자 이하만 가능합니다.";
+    private static final String BLANK_EXCEPTION = "입력값이 존재하지 않습니다.";
+    private static final String NOT_ONLY_NUMBER_EXCEPTION = "숫자만 입력해야 합니다.";
 
     public void validCarNameLength(String carName) {
         if (carName.length() < CAR_NAME_MIN_LENGTH || carName.length() > CAR_NAME_MAX_LENGTH) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION);
+        }
+    }
+
+    public void validTrialNumber(String input){
+        validNotEmpty(input);
+        validOnlyNumber(input);
+    }
+
+    private void validNotEmpty(String input) {
+        if(input.isBlank()){
+            throw new IllegalArgumentException(BLANK_EXCEPTION);
+        }
+    }
+
+    private void validOnlyNumber(String input) {
+        if (!input.matches(ONLY_NUMBER_REGEX)) {
+            throw new IllegalArgumentException(NOT_ONLY_NUMBER_EXCEPTION);
         }
     }
 }
