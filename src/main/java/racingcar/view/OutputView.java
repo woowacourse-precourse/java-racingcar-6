1e6b12ad0de9428cbaf4507a2bad70b635b46ef6
CJY <c0512jy@naver.com>
2023-10-29 01:13:49 +0900

    feat: 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void start() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void printResult(List<String> winnerList) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winnerList));
+    }
+
+    public void printStanding(List<Car> cars) {
+        cars.forEach(System.out::println);
+        System.out.println();
+    }
 }
