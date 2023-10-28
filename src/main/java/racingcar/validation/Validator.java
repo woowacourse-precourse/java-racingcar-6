89ef1f6a06ee805330e73126883362a356ced977
cheolhyeon <tghy4@naver.com>
2023-10-28 15:38:09 +0900

    refact : 코드 구조 개선

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,18 +1,17 @@
 package racingcar.validation;
 
 
+import java.lang.invoke.StringConcatFactory;
 import racingcar.User;
 
 public class Validator {
 
-    public void checkCarNameCount(User user) {
-        String[] carNames = user.inputCarNames();
+    public void checkCarNameCount(String[] carNames) {
         if (carNames.length <= 1) {
-            throw new IllegalArgumentException("??? ??? ???? ?? 2? ?????.");
+            throw new IllegalArgumentException("경주할 수 있는 최소 자동차는 2대 이상 입니다.");
         }
     }
-    public void checkCarNameLength(User user) {
-        String[] carNames = user.inputCarNames();
+    public void checkCarNameLength(String[] carNames) {
         for (String carName : carNames) {
             carNameMaxLength(carName);
         }
@@ -20,7 +19,7 @@ public class Validator {
 
     private static void carNameMaxLength(String carName) {
         if (carName.trim().length() > 5) {
-            throw new IllegalArgumentException("??? ??? 5?? ???? ???.");
+            throw new IllegalArgumentException("자동차 이름의 글자 길이는 5글자 이하 입니다.");
         }
     }
 }
