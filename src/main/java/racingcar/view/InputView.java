246f17d8e85f0eddc43482592b0c1dd3047ea8e3
gihwanJang <gihwan319@gmail.com>
2023-10-29 00:44:05 +0900

    feat : (입력기능) 자동차 이름에 대한 입력을 쉼표로 구분 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,9 +1,20 @@
 package racingcar.view;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private List<String> splitCarNames(String userInput) {
+        return Arrays.stream(userInput.split(","))
+                     .map(String::trim)
+                     .collect(Collectors.toList());
+    }
+
     public List<String> inputCarNames() {
-        return null;
+        String userInput = Console.readLine();
+        return splitCarNames(userInput);
     }
 }
