fee7487d8ac01b2814b4cdc8d281755a1ed3ef99
Annjieun <dkslwdms2121@naver.com>
2023-10-28 20:02:26 +0900

    feat(InputView): 시도할 횟수를 물어보는 메세지를 출력하는 printRequestAttempt() 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,9 +1,18 @@
 package racingcar.view;
 
 import static racingcar.constant.GameMessage.RACING_START;
+import static racingcar.constant.GameMessage.REQUEST_ATTEMPT_COUNT;
 
 public class InputView {
     public static void showRequestNames() {
         System.out.println(RACING_START);
+    }
+
+    public static void printRequestAttempt() {
+        System.out.println(REQUEST_ATTEMPT_COUNT);
+    }
+
+    public static void printLineChanging() {
+        System.out.println();
     }
 }
