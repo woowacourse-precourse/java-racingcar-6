7ee6db39fa5790e73e4bc18823a99ec2b185e25a
huhdy32 <mong3268@gmail.com>
2023-10-28 14:09:54 +0900

    feat(main/RacingPlayer): equals() 오버라이딩 - 플레이어들의 이름 중복 확인

--- src/main/java/racingcar/model/RacingPlayer.java
+++ src/main/java/racingcar/model/RacingPlayer.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import java.util.Objects;
+
 public class RacingPlayer implements Player{
     private final String name;
     private int score = 0;
@@ -17,5 +19,17 @@ public class RacingPlayer implements Player{
     @Override
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        RacingPlayer player = (RacingPlayer) obj;
+        return Objects.equals(name, player.getName());
     }
 }
