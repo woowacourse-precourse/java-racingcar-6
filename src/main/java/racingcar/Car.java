855dca27d7105d3e7c223bfb4d1d2b691cd79af1
Jae-Young98 <ygy0102@gmail.com>
2023-10-29 00:03:33 +0900

    feat(Car): 자동차 이동 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private String name;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+    private static final int CAN_FORWARD_NUMBER = 4;
+
+    private final String name;
 
     private int position;
 
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+        if (randomNumber >= CAN_FORWARD_NUMBER) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
