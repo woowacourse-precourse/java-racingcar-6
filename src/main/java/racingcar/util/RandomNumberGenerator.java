a2b9c5abc87d4072db73d7f0f3216069935d453b
minisyu <iswater.kim@gmail.com>
2023-10-28 14:48:30 +0900

    feat(RandomNumberGenerator): 랜덤 값 생성에 대한 인터페이스 생성

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,18 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private final int startInclusive;
+    private final int endInclusive;
+
+    public RandomNumberGenerator(int startInclusive, int endInclusive) {
+        this.startInclusive = startInclusive;
+        this.endInclusive = endInclusive;
+    }
+
+    @Override
+    public int generateNumber() {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+}
