634ababaeb21f78beebb9bccccde011576a7cfe5
Brian <bwook9908@gmail.com>
2023-10-28 14:26:39 +0900

    feat: 자동차 객체 Car 구현
    - 무작위 숫자가 4 이상이면, 자동차의 전진거리를 1 추가함

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,18 @@
 package racingcar.model;
 
 public class Car {
+    private final String carName;
+    private int distance;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    public void go() {
+        Engine engine = new Engine();
+        if (engine.getPower() >= 4) {
+            distance += 1;
+        }
+    }
 
 }
