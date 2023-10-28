4b736eafe9ee9cfcf8393a356fdc5bac99f9d811
leesangjun <hubls@naver.com>
2023-10-28 17:11:55 +0900

    refactor: 미사용 변수 제거

--- src/main/java/model/AttemptNumberConverter.java
+++ src/main/java/model/AttemptNumberConverter.java
@@ -3,7 +3,6 @@ package model;
 import util.AttemptValidator;
 
 public class AttemptNumberConverter {
-    private int attemptNumber;
 
     public static int convertStringToInteger(String attemptNumber) {
         AttemptValidator.checkValidAttemptNumber(attemptNumber);
