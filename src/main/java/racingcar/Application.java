3bee0dd613707afba602f2043e051b7b13016167
dong_hy <kimadonghyon99@gmail.com>
2023-10-28 23:59:21 +0900

    feat : 메인함수부분 게임구현 (테스트케이스 작성필요)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Cars cars = new Cars();
+
+        GameView.gameStart();
+        cars.createCar(Input.inputCarName());
+        GameView.gameCount();
+        cars.moveCount = Input.inputGameCount();
+
+        GameView.gameMonitoring((HashMap<String, Integer>) cars.carMap,cars.moveCount);
+        //차량생성까지 끝 게임 시작 ~
+
+        for(int i=0;i < cars.moveCount;i++){
+            cars.moveCar();
+            GameView.gameCurrent((HashMap<String, Integer>) cars.carMap);
+        }
+
+        GameView.gameResult(cars.referee());
     }
 }
