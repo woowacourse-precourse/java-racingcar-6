1ff478be4018fd63e82f09e140dc204964826d28
jun <ggprgrkjh@naver.com>
2023-10-28 23:19:25 +0900

    feat(cars): car 전체 전진 moveAll 메소드 추가

--- src/main/java/racingcar/controller/CarGameController.java
+++ src/main/java/racingcar/controller/CarGameController.java
@@ -1,9 +1,11 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import net.bytebuddy.dynamic.scaffold.MethodGraph.Linked;
 import racingcar.model.Car;
+import racingcar.model.Cars;
 import racingcar.model.Game;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -11,35 +13,45 @@ import racingcar.view.OutputView;
 public class CarGameController {
 
     private Game game;
+    private Cars cars;
 
-    public CarGameController(){
+    public CarGameController() {
         this.game = new Game();
     }
 
-    public void start(){
+    public void start() {
         OutputView.printRequestCarNameMessage();
         String input = InputView.readCarName();
-        List<String> carNameList= game.splitCarName(input);
+        List<String> carNameList = game.splitCarName(input);
         validateNameLength(carNameList);
+        cars = new Cars(createCars(carNameList));
         OutputView.printRequestTryNumberMessage();
         int tryNumber = Integer.parseInt(InputView.readTryNumber());
         OutputView.printResultStartMessage();
 
 
-
-
-
     }
 
 
-    public static void validateNameLength(List<String> carNameList){
-        for(int i = 0 ; i < carNameList.size();i++){
-            if(carNameList.get(i).length()>5){
+    public static void validateNameLength(List<String> carNameList) {
+        for (int i = 0; i < carNameList.size(); i++) {
+            if (carNameList.get(i).length() > 5) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
+
+    public List<Car> createCars(List<String> carNameList) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNameList) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+        return cars;
+
+    }
 
 
 }
--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
     }
+
+    public List<Car> moveAll() {
+        for (Car car : cars) {
+            int randomNumber = car.createRandomNumber();
+            if (randomNumber >= 4) {
+                car.move();
+            }
+        }
+        return cars;
+    }
+
 
 }
