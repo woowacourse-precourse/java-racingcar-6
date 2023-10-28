0363955cd65812d56d1f3c561ae0c40f2e6f4f2a
zaysverse <zeze@jbnu.ac.kr>
2023-10-29 01:32:31 +0900

    feat : 시도할 횟수 입력 기능 추가
    사용자로부터 시도할 횟수를 입력받는다.
    
    - round에 입력받은 정수를 저장한다.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -15,17 +15,23 @@ public class Game {
     }
 
     public void run() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         createCar();
-        System.out.println("시도할 회수는 몇회인가요?");
+        getRound();
+
     }
 
-    public void createCar() {
+    void createCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] names = input.split(",");
         for (String name : names) {
             carRepository.add(new Car(name));
         }
+    }
+
+    void getRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        round = Integer.valueOf(Console.readLine());
     }
 
 }
