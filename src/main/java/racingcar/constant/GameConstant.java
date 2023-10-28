f4fef6c612ea9d78dd907d07672a3e1cbdc2219c
Flash-KIM142 <topqr123q@gmail.com>
2023-10-28 22:22:03 +0900

    feat: 사용자 입력 기능 구현
    자동차 이름 입력, 이동 횟수 입력, 이동 횟수 형식 검증

---
+++ src/main/java/racingcar/constant/GameConstant.java
@@ -0,0 +1,7 @@
+package racingcar.constant;
+
+public class GameConstant {
+
+    public static final String NUMBER_REGEX = "^[0-9]+$";
+    public static final String ZERO_REGEX = "^0$";
+}
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,34 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.GameConstant;
+
+public class InputView {
+
+    public String getCarNames(){
+        String carNames = Console.readLine();
+        return carNames;
+    }
+
+    public Integer getTrialNumber(){
+        String trialString = Console.readLine();
+
+        isNumber(trialString);
+        isZero(trialString);
+
+        Integer trialNumber = Integer.parseInt(trialString);
+        return trialNumber;
+    }
+
+    private void isNumber(String trialString){
+        if(!trialString.matches(GameConstant.NUMBER_REGEX)){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+
+    private void isZero(String trialString){
+        if(trialString.matches(GameConstant.ZERO_REGEX)){
+            throw new IllegalArgumentException("1이상의 숫자를 입력해주세요.");
+        }
+    }
+}
