a62dddf7efaccfc0620c6a5e3625cc18cd4fb94b
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-28 22:59:25 +0900

    test(TryNumberValidator, CarNameValidator): 자동차 이름, 시도 회수 입력값에 대한 테스트 코드 추가

---
+++ src/test/java/racingcar/domain/CarNameValidatorTest.java
@@ -0,0 +1,65 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.CarNameValidator;
+import racingcar.domain.validator.exception.CarNameException;
+
+public class CarNameValidatorTest {
+
+    private CarNameValidator carNameValidator;
+
+    @BeforeEach
+    void setup() {
+        carNameValidator = new CarNameValidator();
+    }
+
+    @Test
+    void testCarNameValidatorWithValidInput() {
+        Assertions.assertDoesNotThrow(() -> carNameValidator.validate("Car1,Car2,Car3"));
+    }
+
+    @Test
+    void testCarNameValidatorWithEmptyInput() {
+        String emptyInput = "";
+
+        IllegalArgumentException emptyInputException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> carNameValidator.validate(emptyInput));
+
+        Assertions.assertEquals(
+                CarNameException.NO_VALUE_NAME.getMessage(),
+                emptyInputException.getMessage()
+        );
+    }
+
+    @Test
+    void testCarNameValidatorWithTooLongName() {
+        String tooLongName = "LongNamingIsNotValidatedInThisGame";
+
+        IllegalArgumentException tooLongNameException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> carNameValidator.validate(tooLongName));
+
+        Assertions.assertEquals(
+                CarNameException.INVALID_TOO_LONG_NAME.getMessage(),
+                tooLongNameException.getMessage()
+        );
+    }
+
+    @Test
+    void testCarNameValidatorWithTooShortName() {
+        String tooShortName = "C";
+
+        IllegalArgumentException tooShortNameException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> carNameValidator.validate(tooShortName)
+        );
+
+        Assertions.assertEquals(
+                CarNameException.INVALID_TOO_SHORT_NAME.getMessage(),
+                tooShortNameException.getMessage()
+        );
+    }
+}
---
+++ src/test/java/racingcar/domain/TryNumberValidatorTest.java
@@ -0,0 +1,81 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.TryNumberValidator;
+import racingcar.domain.validator.exception.TryNumberException;
+
+public class TryNumberValidatorTest {
+    private TryNumberValidator tryNumberValidator;
+
+    @BeforeEach
+    void setup() {
+        tryNumberValidator = new TryNumberValidator();
+    }
+
+    @Test
+    void testTryNumberValidatorWithValidInput() {
+        Assertions.assertDoesNotThrow(() -> tryNumberValidator.validate("5"));
+    }
+
+    @Test
+    void testTryNumberValidatorWithEmptyInput() {
+        String emptyInput = "";
+
+        IllegalArgumentException emptyInputException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> tryNumberValidator.validate(emptyInput)
+        );
+
+        Assertions.assertEquals(
+                TryNumberException.NO_VALUE_TRY_NUMBER.getMessage(),
+                emptyInputException.getMessage()
+        );
+    }
+
+    @Test
+    void testTryNumberValidatorWithNonNumericInput() {
+        String nonNumericInput = "abc";
+
+        IllegalArgumentException nonNumericException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> tryNumberValidator.validate(nonNumericInput)
+        );
+
+        Assertions.assertEquals(
+                TryNumberException.INVALID_INTEGER_VALUE.getMessage(),
+                nonNumericException.getMessage()
+        );
+    }
+
+    @Test
+    void testTryNumberValidatorWithTooBigInput() {
+        String tooBigInput = "11";
+
+        IllegalArgumentException tooBigNumberException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> tryNumberValidator.validate(tooBigInput)
+        );
+
+        Assertions.assertEquals(
+                TryNumberException.INVALID_TOO_BIG_TRY_NUMBER.getMessage(),
+                tooBigNumberException.getMessage()
+        );
+    }
+
+    @Test
+    void testTryNumberValidatorWithTooSmallInput() {
+        String tooSmallInput = "0";
+
+        IllegalArgumentException tooSmallNumberException = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> tryNumberValidator.validate(tooSmallInput)
+        );
+
+        Assertions.assertEquals(
+                TryNumberException.INVALID_TOO_SMALL_TRY_NUMBER.getMessage(),
+                tooSmallNumberException.getMessage()
+        );
+    }
+}
