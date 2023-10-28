aa5ea351b6821412265ca9c56a60586133116aaf
jongheonleee <qwefghnm1212@gmail.com>
2023-10-28 22:12:53 +0900

    [feat] Cars 클래스의 예외 처리 문구 작성

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -46,7 +46,7 @@ public class Cars {
     private void validateDuplicateName(List<String> names) {
         if (names.stream()
                 .collect(Collectors.toSet()).size() != names.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 중복된 이름으로 차를 생성할 수 없습니다.");
         }
     }
 
