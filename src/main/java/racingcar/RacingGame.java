f573d8de39552f62d77c6e36a874f58ad13afa1a
BINBEAN777 <peanutchan@naver.com>
2023-10-28 15:12:05 +0900

    feat : start , checkStopAndGo 메소드 생성 start 메소드에서는 hashmap 컬렉션으로 이름과 전진 상황을 표현하게 했다. checkStopAndGo 메소드에서는 만든 해쉬맵과 그룹이름으로 전진할때마다 "-" 를 해쉬맵 밸류값에 추가했다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import java.util.HashMap;
+
 public class RacingGame {
     public void start(){
+        Output.tellStartAndGetName();
+        String [] carName = Input.getCarName();
+        Output.tellGroupName(carName);
+        HashMap groupCarName = new HashMap();
+        for(String name : carName){
+            groupCarName.put(name,"");
+        }
+
+        int currentRoundNum=0;
+        int gameRoundNum = Input.getGameRoundNum();
+
+        while(gameRoundNum!=currentRoundNum){
+            checkStopAndGo(groupCarName,carName);
+            Output.tellCurrent(groupCarName,carName);
+            currentRoundNum+=1;
+        }
+
+        }
+
+        void checkStopAndGo(HashMap name,String[] groupName){
+        for(String carName : groupName)
+            name.put(carName,name.get(carName)+"-");
+        }
 
     }
-}
+
+
+
