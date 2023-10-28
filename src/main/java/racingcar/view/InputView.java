a34502f29c30508c83a40be5093c1f27bba3c7ff
박준영 <wnsdud6969@naver.com>
2023-10-28 19:17:10 +0900

    feat(InputView): add InputView Class to manage user input view
    Add new feature to verify singleton
    - private static InputView instance
    - private constructor
    - public static getInstance()

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public class InputView {
+
+    private static InputView inputView;
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        if (inputView == null) {
+            inputView = new InputView();
+        }
+        return inputView;
+    }
+}
