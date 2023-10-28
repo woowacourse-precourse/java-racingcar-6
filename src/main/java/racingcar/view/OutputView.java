2e3506c5a327eff1ec84361526fd9acb09e6bc63
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 15:17:40 +0900

    feat(OutputView) : 실행결과 출력 기능 추가 OutputView 클래스 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import static racingcar.constant.AllPunctuationMarks.*;
+import static racingcar.constant.RaceIOMessage.RACE_WINNER;
+
+import java.util.List;
+
 public class OutputView {
+    public void printRoundResult(String carName, int moves) {
+        System.out.println(makeResultToString(carName, moves));
+    }
+    private String makeResultToString(String carName, int moves) {
+        return carName + COLONS + PROGRESS.repeat(Math.max(0, moves));
+    }
+    public void printWinnerList(List<String> winnerName) {
+        StringBuilder winner = new StringBuilder(RACE_WINNER + winnerName.get(0));
+        for (int i = 1; i < winnerName.size(); i++) {
+            winner.append(COMMA+BLANK).append(winnerName.get(i));
+        }
+        System.out.println(winner);
+    }
 }
