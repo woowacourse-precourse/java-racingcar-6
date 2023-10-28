d1b4166a4969953dbbf215dd56c83a012ca5b785
minisyu <iswater.kim@gmail.com>
2023-10-28 14:43:40 +0900

    refactor: RandomNumberPicker -> RandomMoveStrategy로 변경

---
+++ src/main/java/racingcar/util/RandomMoveStrategy.java
@@ -0,0 +1,16 @@
+package racingcar.util;
+
+public class RandomMoveStrategy implements MoveStrategy {
+    private static final int THRESHOLD = 4;
+    private final NumberGenerator numberGenerator;
+
+    public RandomMoveStrategy(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    @Override
+    public boolean isMovable() {
+        int randomNumber = numberGenerator.generateNumber();
+        return randomNumber >= THRESHOLD;
+    }
+}
