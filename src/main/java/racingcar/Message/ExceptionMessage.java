ee9bf217ff5c897c7f3f9c8dae921cf0c432dc50
coenffl <>
2023-10-28 23:42:43 +0900

    Feat: generate exception message

--- src/main/java/racingcar/Message/ExceptionMessage.java
+++ src/main/java/racingcar/Message/ExceptionMessage.java
@@ -1,4 +1,12 @@
 package racingcar.Message;
 
 public class ExceptionMessage {
+    final String ERROR_MESSAGE = "[ERROR] ";
+    final String FIVE_LENGTH_ERROR = "이름은 5자 이하만 가능합니다";
+    final String IS_SPACE_ERROR = "이름에 공백이 존재할 수 없습니다.";
+    final String ONE_CAR_ERROR = "자동차 경주는 두 대 이상 가능합니다.";
+    final String SAME_NAME_ERROR = "동일한 이름은 존재할 수 없습니다.";
+    final String IS_NUMBER_ERROR = "숫자를 입력해주세요.";
+    final String IS_INT_ERROR = "정수를 입력해주세요";
+    final String ZERO_OR_LESS_ERROR = "1 이상의 값을 입력해주세요.";
 }
