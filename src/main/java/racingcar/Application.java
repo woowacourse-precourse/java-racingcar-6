df262df130c8a052bae7a664a9290d417433fdbe
inwoo <korean5078@gmail.com>
2023-10-28 21:57:10 +0900

    feat(RacingCar): 무작위 숫자가 4부터 9가 나오면 전진하는 기능 구현 1. RacingCar의 randomNumber가 4이상인 경우 전진하는 함수 goOrStop 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         RacingCar racingCar = new RacingCar("pobi");
-//        racingCar.makeRandomNumber();
-//        System.out.println(racingCar.randomNumber);
+        racingCar.makeRandomNumber();
+        racingCar.goOrStop();
+        System.out.println(racingCar.randomNumber + " " + racingCar.distance);
     }
 }
--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -4,13 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
     private String name;
-    private int randomNumber;
+    public int randomNumber;
+    public int distance;
 
     public RacingCar(String name) {
         this.name = name;
     }
 
-    private void makeRandomNumber() {
+    public void makeRandomNumber() {
         this.randomNumber = Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void goOrStop() {
+        if (this.randomNumber >= 4) {
+            this.distance++;
+        }
     }
 }
