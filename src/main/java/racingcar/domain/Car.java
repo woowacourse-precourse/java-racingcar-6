db758910341ec55008c25f11351495313648b442
김연진 <love_homebody@naver.com>
2023-10-28 18:07:16 +0900

    feat(Car): 랜덤 숫자가 4이상일 경우 자동차의 위치 이동 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,16 +6,26 @@ public class Car {
 
     private static final int START_NUMBER = 0;
     private static final int END_NUMBER = 9;
+    private static final int MOVE_NUMBER = 4;
 
     private final String name;
-    private final int position;
+    private int position;
 
     public Car(String name) {
         this.name = name;
         this.position = 0;
     }
 
-    public int pickRandomNumber() {
+    public void race() {
+        if (isOverMoveNumber()) {
+            position++;
+        }
+    }
+
+    private boolean isOverMoveNumber() {
+        return pickRandomNumber() >= MOVE_NUMBER;
+    }
+    private int pickRandomNumber() {
         return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 }
