96a253f33056d73e68901add64b084cb8365c426
suhyun__ <thebbbb1206@gmail.com>
2023-10-29 00:21:51 +0900

    feat: 자동차 입력 문구 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        return Console.readLine();
+    }
+}
