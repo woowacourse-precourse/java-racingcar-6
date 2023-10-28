bf89bda8af204fb7878f232d02c1f16e4cc0e18a
hyuna <monicajo0@naver.com>
2023-10-28 19:33:43 +0900

    feat: 자동차 이름 입력받는 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private String location;
+
+    public Car(String name){
+        this.name = name;
+    }
+}
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private final List<Car> cars = new ArrayList<>();
+
+    public void start() {
+        printMessage(START_MESSAGE);
+
+        setCarNames();
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private void setCarNames() {
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+            System.out.println(carName);
+        }
+    }
+}
