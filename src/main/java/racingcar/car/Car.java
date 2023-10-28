b6652e647f7e6a24ba51eebb4402f99ffde57726
JooDukHan <bfinecpa1@naver.com>
2023-10-28 21:43:36 +0900

    feat: 자동차 이름을 부여한다.
    자동차 이름을 받아 객체를 생성한다.

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,21 @@
+package racingcar.car;
+
+public class Car {
+
+    public static final int FORWARD_MOVE_INIT_COUNT = 0;
+    private int forwardMoveCount;
+    private final String name;
+
+    private Car(String name) {
+        this.name = name;
+        this.forwardMoveCount = FORWARD_MOVE_INIT_COUNT;
+    }
+
+    public static Car of (String name) {
+        return new Car(name);
+    }
+
+
+
+
+}
