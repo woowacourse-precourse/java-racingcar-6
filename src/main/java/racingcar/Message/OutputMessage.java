e7c7b64ec1051c7d1cd584481a7689f274cba715
coenffl <>
2023-10-28 23:16:55 +0900

    Feat: generate output message

--- src/main/java/racingcar/Message/OutputMessage.java
+++ src/main/java/racingcar/Message/OutputMessage.java
@@ -1,4 +1,9 @@
 package racingcar.Message;
 
 public class OutputMessage {
+    final String CAR_DELIMITER = ":";
+    final String RACE_MARK = "-";
+    final String RESULT_MESSAGE = "실행결과";
+    final String WINNER_MESSAGE = "가 최종 우승했습니다.";
+    final String WINNER_DELIMITER = ", ";
 }
