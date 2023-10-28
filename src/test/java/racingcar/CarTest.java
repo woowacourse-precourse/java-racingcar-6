b1ab4162eec462183c1a8876cd3d08a8edf7e911
junest66 <chlwndks333@gmail.com>
2023-10-29 04:36:31 +0900

    refactor(CarTest): Mockito 제거 및 TestNumberGenerator 도입
    - mock을 사용한 테스트에서 TestNumberGenerator를 사용한 테스트로 변경
    - 각 테스트 케이스마다 Car 객체를 새로 생성하여 의존성 주입

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,29 +1,21 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.utils.NumberGenerator;
 
 class CarTest {
 
     private Car car;
-    private NumberGenerator mockNumberGenerator;
-
-    @BeforeEach
-    void setUp() {
-        mockNumberGenerator = mock(NumberGenerator.class);
-        car = new Car("TestCar", mockNumberGenerator);
-    }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     void 랜덤숫자가_4이상일떄_자동차_전진(int randomNumber) {
         int expectedMoveNum = 1;
-        when(mockNumberGenerator.createRandomNumber()).thenReturn(randomNumber);
+        NumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumber);
+        car = new Car("TestCar", testNumberGenerator);
         car.move();
         assertThat(car.getMoveNum()).isEqualTo(expectedMoveNum);
     }
@@ -32,7 +24,8 @@ class CarTest {
     @ValueSource(ints = {0, 1, 2, 3})
     void 랜덤숫자가_4미만일때_자동차_멈춤(int randomNumber) {
         int expectedMoveNum = 0;
-        when(mockNumberGenerator.createRandomNumber()).thenReturn(randomNumber);
+        NumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumber);
+        car = new Car("TestCar", testNumberGenerator);
         car.move();
         assertThat(car.getMoveNum()).isEqualTo(expectedMoveNum);
     }
