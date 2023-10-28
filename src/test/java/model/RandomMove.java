01ec8b76dbe92a380d14efd79a06ba43c2775fa9
sehee <tpgml701@naver.com>
2023-10-29 00:37:23 +0900

    feat: 4~9까지 랜덤 수 이동 기능 구현

--- src/test/java/model/RandomMove.java
+++ src/test/java/model/RandomMove.java
@@ -6,16 +6,6 @@ import java.util.Map;
 
 public class RandomMove {
     boolean isMove(){
-        if(Randoms.pickNumberInRange(0,9)>=4) {
-            return true;
-        }
-        return false;
-    }
-
-    boolean isMove1(Map<String, Integer> cars){
-        if(Randoms.pickNumberInRange(0,9)>=4) {
-
-        }
-        return false;
+        return (Randoms.pickNumberInRange(0, 9) >= 4);
     }
 }
