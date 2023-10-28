e301efd622beae97c73ee66fbab8a749bb6f0959
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-10-28 17:45:32 +0900

    refactor(CarService): Controller의 일부 CarService로 이동
    `RacingGameController`에 있던 내용 중 일부가 `CarService`에 있는 것이 맞다고 판단되어 `getCarNameAndCheckValidation` 메서드와  `getTryCountAndCheckValidation` 메서드를 `CarService`로 이동 시킴.

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,27 +1,11 @@
 package racingcar.controller;
 
 import racingcar.service.CarService;
-import racingcar.util.validator.InputValidator;
-import racingcar.view.InputView;
-
-import java.util.List;
 
 public class RacingGameController {
-    InputView inputView = new InputView();
     CarService carService = new CarService();
-    InputValidator inputValidator = new InputValidator();
 
     public void gameStart() {
-        getCarNameAndCheckValidation();
-        getTryCountAndCheckValidation();
-    }
-
-    private void getCarNameAndCheckValidation() {
-        List<String> carNameList = carService.splitCarNames(inputView.getCarNames());
-        inputValidator.checkCarNameInputValidation(carNameList);
-    }
-
-    private void getTryCountAndCheckValidation() {
-        inputValidator.checkTryCountInputValidation(inputView.getTryCount());
+        carService.getCarNameAndTryCount();
     }
 }
--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,10 +1,31 @@
 package racingcar.service;
 
+import racingcar.util.validator.InputValidator;
+import racingcar.view.InputView;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class CarService {
-    public List<String> splitCarNames(String carNames) {
+
+    InputView inputView = new InputView();
+    InputValidator inputValidator = new InputValidator();
+
+    public void getCarNameAndTryCount() {
+        getCarNameAndCheckValidation();
+        getTryCountAndCheckValidation();
+    }
+
+    private void getCarNameAndCheckValidation() {
+        List<String> carNameList = splitCarNames(inputView.getCarNames());
+        inputValidator.checkCarNameInputValidation(carNameList);
+    }
+
+    private void getTryCountAndCheckValidation() {
+        inputValidator.checkTryCountInputValidation(inputView.getTryCount());
+    }
+
+    private List<String> splitCarNames(String carNames) {
         return Arrays.stream(carNames.split(",")).toList();
     }
 }
