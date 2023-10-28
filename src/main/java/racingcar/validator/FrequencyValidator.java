a1cc1d7c827d52306fcf1d4d7a496ade4576cf2e
qncj5224 <qncj5224@naver.com>
2023-10-28 21:08:24 +0900

    fix : static 추가

--- src/main/java/racingcar/validator/FrequencyValidator.java
+++ src/main/java/racingcar/validator/FrequencyValidator.java
@@ -2,7 +2,7 @@ package racingcar.validator;
 
 public class FrequencyValidator {
 
-  public void isCorrectFrequencyNumber(String frequency) throws IllegalArgumentException {
+  public static void isCorrectFrequencyNumber(String frequency) throws IllegalArgumentException {
     for (char c : frequency.toCharArray()) {
       if (!Character.isDigit(c)) {
         throw new IllegalArgumentException("숫자를 입력해주세요.");
@@ -10,7 +10,7 @@ public class FrequencyValidator {
     }
   }
 
-  public void isNegativeNumber(String frequency) throws IllegalArgumentException {
+  public static void isNegativeNumber(String frequency) throws IllegalArgumentException {
     if(Integer.parseInt(frequency) < 1) {
       throw new IllegalArgumentException("1보다 큰 수를 입력해주세요.");
     }
