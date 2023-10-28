ee65543622217d353cba153cfc2bfc8da87c8432
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-28 19:44:14 +0900

    feat : 시도 횟수 입력시 입력이 없을떄에 대한 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,25 +1,35 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.view.Validator;
 
-public class InputView{
+public class InputView {
 
-    public String inputCarNames(){
+    public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputData = Console.readLine();
         exceptInputCarNames(inputData);
         return inputData;
     }
 
-    public String inputRaceTimes(){
+    public String inputRaceTimes() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        String inputData = Console.readLine();
+        exceptInputRaceTimes(inputData);
+        return inputData;
     }
 
-    public void exceptInputCarNames(String inputData){
-        if(Validator.validateInputExist(inputData)){
+    public void exceptInputCarNames(String inputData) {
+        if (Validator.validateInputExist(inputData)) {
             throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    public void exceptInputRaceTimes(String inputData) {
+        if (Validator.validateInputExist(inputData)) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+        if (Validator.validateInputNotNum(inputData)) {
+            throw new IllegalArgumentException("입력값은 정수여야 합니다.");
         }
     }
 
--- src/main/java/racingcar/view/Validator.java
+++ src/main/java/racingcar/view/Validator.java
@@ -1,7 +1,16 @@
 package racingcar.view;
 
 public class Validator {
-    public static boolean validateInputExist(String inputValue) {
-        return inputValue.isEmpty();
+    public static boolean validateInputExist(String inputData) {
+        return inputData.isEmpty();
+    }
+
+    public static boolean validateInputNotNum(String inputData) {
+        for (char ch : inputData.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
