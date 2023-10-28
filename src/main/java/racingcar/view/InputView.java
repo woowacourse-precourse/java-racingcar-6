0c196718d91ecfcfce72fc4c8eb0567cda00b903
usnim <sms897@naver.com>
2023-10-28 16:27:04 +0900

    [refactor] InputView 클래스 내부 메소드 수정 : getCarsName() 메소드 분리 및 메소드명 변경
    before :
    1. (메소드명 변경) carsName()
    2. carsName() 내부에 있던 입력 메세지 출력 기능
    3. 리턴 타입 void carsName() 메소드
    
    after :
    1. getCarsName()
    2. readInputMessage() 별도 메소드로 분리
    3. splitCarsName() 메소드 생성으로 getCarsName() 반환 타입 String[] 로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,14 +7,22 @@ import racingcar.validation.ValidateStringLength;
 public class InputView {
     private static final String carsNameInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public void carsName() {
+    public void readInputMessage() {
         System.out.println(carsNameInputMessage);
+    }
+
+    public String[] getCarsName() {
         String input = Console.readLine();
         validateInputValue(input);
+        return splitCarsName(input);
     }
 
     private void validateInputValue(String value) {
         ValidateBlankNameFormat.validate(value);
         ValidateStringLength.validate(value);
+    }
+
+    private String[] splitCarsName(String carsName) {
+        return carsName.split(",");
     }
 }
