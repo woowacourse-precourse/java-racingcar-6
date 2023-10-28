861c88f3bbbba2ab6140978cfbc928dd2a7d6bda
김효준 <khyojun1104@gmail.com>
2023-10-28 20:16:03 +0900

    feat(CarRace): OutputView -> 게임 시도 횟수 입력 전 출력문에 대한 기능 구현!

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -17,4 +17,8 @@ public class OutputView {
     public void printInitGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     }
+
+    public void printGameCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
