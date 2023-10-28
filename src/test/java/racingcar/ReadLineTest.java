4847ef12f18d9b94e9db622a1181fdfd8ca7424e
takoyakimchi <c4nyou@gmail.com>
2023-10-28 20:15:38 +0900

    refactor(game_test): rename & all test case have separate test
    Renamed the class name
    : GameTest -> ReadLineTest
    This will only be used for testing methods which contains Console.readLine()

---
+++ src/test/java/racingcar/ReadLineTest.java
@@ -0,0 +1,124 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReadLineTest {
+
+    @AfterEach
+    void afterEach() {
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("정상 이름 입력(여러 개)")
+    void readCarNames_goodInputs() {
+        // given
+        String input = "jason,bob";
+
+        // when
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        List<Car> cars = Game.readCarNames();
+
+        // then
+        assertThat(cars.get(0).getName()).isEqualTo("jason");
+        assertThat(cars.get(1).getName()).isEqualTo("bob");
+    }
+
+    @Test
+    @DisplayName("정상 이름 입력(한 개)")
+    void readCarNames_goodInput() {
+        // given
+        String input = "peter";
+
+        // when
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        List<Car> cars = Game.readCarNames();
+        Console.close();
+
+        // then
+        assertThat(cars.get(0).getName()).isEqualTo("peter");
+    }
+
+    @Test
+    @DisplayName("비정상 이름 입력(쉼표만)")
+    void readCarNames_onlyCommas() {
+        // given
+        String onlyCommas = ",,,,";
+
+        // when - then
+        System.setIn(new ByteArrayInputStream(onlyCommas.getBytes()));
+        assertThatThrownBy(Game::readCarNames)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("비정상 이름 입력(앞뒤 쉼표)")
+    void readCarNames_badCommaPosition() {
+        // given
+        String badCommaPosition = ",lee,park,kim,";
+
+        // when - then
+        System.setIn(new ByteArrayInputStream(badCommaPosition.getBytes()));
+        assertThatThrownBy(Game::readCarNames)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("비정상 이름 입력(이름 6글자)")
+    void readCarNames_sixLettersName() {
+        // given
+        String sixLettersName = "joshua,josh";
+
+        // when - then
+        System.setIn(new ByteArrayInputStream(sixLettersName.getBytes()));
+        assertThatThrownBy(Game::readCarNames)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("정상 게임 횟수 입력")
+    void readGameCount_one() {
+        // given
+        String legal = "1";
+
+        // when - then
+        System.setIn(new ByteArrayInputStream(legal.getBytes()));
+        int countLegal = Game.readGameCount();
+        assertThat(countLegal).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("비정상 게임 횟수 입력(0)")
+    void readGameCount_zero() {
+        checkGameCount("0");
+    }
+
+    @Test
+    @DisplayName("비정상 게임 횟수 입력(-1)")
+    void readGameCount_minusOne() {
+        checkGameCount("-1");
+    }
+
+    @Test
+    @DisplayName("비정상 게임 횟수 입력(숫자 아닌 문자 포함)")
+    void readGameCount_noDigit() {
+        checkGameCount("12a");
+    }
+
+    private static void checkGameCount(String input) {
+        // given
+        String illegal = input;
+
+        // when - then
+        System.setIn(new ByteArrayInputStream(illegal.getBytes()));
+        assertThatThrownBy(Game::readGameCount)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
