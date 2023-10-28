7c31baf381a328cdacaaaafd94048b7e574dc369
김효준 <khyojun1104@gmail.com>
2023-10-28 15:06:02 +0900

    feat(CarRace) : InputView -> Input 1번 항목에 대한 기능 구현!

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     private static InputView instance;
@@ -14,4 +16,7 @@ public class InputView {
     }
 
 
+    public String inputCarName() {
+        return Console.readLine();
+    }
 }
