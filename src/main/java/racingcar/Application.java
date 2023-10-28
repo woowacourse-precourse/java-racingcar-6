4e4bf9532dda7b8ce5f033ba19a07b93eebbb9b6
chansoo <chansoo38@gmail.com>
2023-10-28 14:47:20 +0900

    <Refactor> Controller, Service 변경에 따른 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,12 @@
 package racingcar;
 
 import racingcar.Controller.Controller;
+import racingcar.Domain.ProgressBoard;
+import racingcar.Service.Service;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Controller controller = new Controller(new Service(ProgressBoard.getInstance()));
         controller.run();
     }
 }
