d5c1cefa4606724983b753c935fe9969a97c5443
YongJOoOo <we072200@naver.com>
2023-10-28 12:07:53 +0900

    refactor: 메소드 이름 변경 & 코드 정리
    기존의 사용자 입력을 받던 inputUser() 메소드를 더 명확한 이름인 askInputUser() 이름으로 변경 및 코드 정리

--- src/main/java/racingcar/view/UserInput.java
+++ src/main/java/racingcar/view/UserInput.java
@@ -25,18 +25,16 @@ public class UserInput {
         return gameCount;
     }
 
-    //1) 사용자 입력 처리해서 객체에도 세팅
-    public void inputUser(UserInput userInput) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carName = Console.readLine();
-
-        validateInput(carName);
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        int gameCount = Integer.parseInt(Console.readLine());
-
-        userInput.setUserInputCarName(carName);
-        userInput.setGameCount(gameCount);
+    // -이름 중복될 경우
+    static void validateDuplicate(String[] names) {
+        //3) 이름이 중복될 경우 예외
+        String tmp = "";
+        for (String x : names) {
+            if (tmp.contains(x)) {
+                throw new IllegalArgumentException("경주할 자동차 이름이 중복되면 안됩니다.");
+            }
+            tmp += x; //생각해보니까. 포함되는 문자열이 생길 수 도 있으니 다시 작성하자. :TODO
+        }
     }
 
     //validate 메소드 분리 - 길이
@@ -59,16 +57,18 @@ public class UserInput {
         }
     }
 
-    // -이름 중복될 경우
-    static void validateDuplicate(String[] names) {
-        //3) 이름이 중복될 경우 예외
-        String tmp = "";
-        for (String x : names) {
-            if (tmp.contains(x)) {
-                throw new IllegalArgumentException("경주할 자동차 이름이 중복되면 안됩니다.");
-            }
-            tmp += x;
-        }
+    //1) 사용자 입력 처리해서 객체에도 세팅
+    public void askInputUser(UserInput userInput) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+
+        validateInput(carName);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int gameCount = Integer.parseInt(Console.readLine());
+
+        userInput.setUserInputCarName(carName);
+        userInput.setGameCount(gameCount);
     }
 
     //2) 사용자 입력에 대한 예외처리
