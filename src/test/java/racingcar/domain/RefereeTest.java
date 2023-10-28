cc5ec751f614ca1c378cb7c011e61ab6a0d99548
leedohyun <lucky77795@naver.com>
2023-10-28 17:27:37 +0900

    test(racingCar) : 우승자를 판별하는 심판 테스트

---
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -0,0 +1,53 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RefereeTest {
+
+    private Referee referee = new Referee();
+    @Test
+    void 우승자가_여러명일_경우_테스트() {
+        List<Car> cars = new ArrayList<>();
+
+        Car car1 = new Car("dobi", 4);
+        Car car2 = new Car("pobi", 4);
+        Car car3 = new Car("apple", 3);
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+
+        Cars carList = new Cars(cars);
+
+        List<String> result = new ArrayList<>();
+        result.add("dobi");
+        result.add("pobi");
+
+        Assertions.assertThat(referee.checkResult(carList)).isEqualTo(result);
+    }
+
+    @Test
+    void 우승자가_한명일_경우_테스트() {
+        List<Car> cars = new ArrayList<>();
+
+        Car car1 = new Car("dobi", 4);
+        Car car2 = new Car("pobi", 2);
+        Car car3 = new Car("apple", 3);
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+
+        Cars carList = new Cars(cars);
+
+        List<String> result = new ArrayList<>();
+        result.add("dobi");
+
+        Assertions.assertThat(referee.checkResult(carList)).isEqualTo(result);
+    }
+}
