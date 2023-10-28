86f9a40a3be3f7c728bd4ce5e5f2a1cee9dbee41
박정현 <ros28081@naver.com>
2023-10-28 18:04:33 +0900

    feat: 게임 실행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import static racingcar.RacingGame.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.startGame();
+
+        Car[] cars = racingGame.getInput();
+        getAttemptingNum();
+
+        moveCars(cars, attemptingNum);
+        printRacingGameResults(cars);
+
+        String winners = findWinners(cars);
+        printWinnerPhrase();
+        System.out.println(winners);
     }
 }
