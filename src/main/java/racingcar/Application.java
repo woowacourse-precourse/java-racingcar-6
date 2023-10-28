0f5d33ead42af2902ce9a4d4a091d8842a889a42
IE-MangChi <hsm983278@gmail.com>
2023-10-28 20:59:39 +0900

    feat: 게임시작 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.CarRaceGameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        CarRaceGameController carRaceGameController = new CarRaceGameController();
+        carRaceGameController.gameStart();
     }
 }
