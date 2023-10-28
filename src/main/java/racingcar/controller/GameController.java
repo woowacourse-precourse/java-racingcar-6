a4d125fdba1c12ccaf5de2d141cc0e924b4cbe28
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-29 01:40:00 +0900

    refactor: Race 클래스의 기능으로 변경된 서드 삭제
    docs: Race 생성자 설명 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,14 +1,11 @@
 package racingcar.controller;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.constant.CommonSymbol;
-import racingcar.constant.GameConfig;
-import racingcar.model.Car;
+import racingcar.model.Race;
 import racingcar.validation.InputValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -19,60 +16,17 @@ public class GameController {
 	}
 
 	public void startGame() {
-		List<Car> cars = createCars(readCarNames());
+		Race race = new Race(readCarNames());
 		int attemptCount = readAttemptCount();
 
 		OutputView.printExecutionResultMessage();
+		
 		for (int i = 0; i < attemptCount; i++) {
-			moveAllCarsForward(cars);
-			displayAllCarsPosition(cars);
+			race.moveAllCarsForward();
+			race.displayAllCarsPosition();
 		}
 
-		List<Car> winners = findWinners(cars);
-		displayWinnerNames(winners);
-	}
-
-	private List<Car> findWinners(List<Car> cars) {
-		List<Car> sortedCars = sortCarsByPosition(cars);
-		int maxPosition = sortedCars.get(0).getPosition();
-
-		return sortedCars.stream()
-			.filter(car -> car.getPosition() == maxPosition)
-			.collect(Collectors.toList());
-	}
-
-	private void displayWinnerNames(List<Car> winners) {
-		OutputView.printWinner(winners.stream()
-			.map(Car::getName)
-			.collect(Collectors.toList()));
-	}
-
-	private List<Car> sortCarsByPosition(List<Car> cars) {
-		return cars.stream()
-			.sorted(Comparator.comparingInt(Car::getPosition).reversed())
-			.collect(Collectors.toList());
-	}
-
-	private void displayAllCarsPosition(List<Car> cars) {
-		for (Car car : cars) {
-			OutputView.printCarPosition(car.getName(), car.getPosition());
-		}
-
-		OutputView.printNewLine();
-	}
-
-	private void moveAllCarsForward(List<Car> cars) {
-		for (Car car : cars) {
-			moveForward(car);
-		}
-	}
-
-	private void moveForward(Car car) {
-		int randomValue = Randoms.pickNumberInRange(GameConfig.RANDOM_MINIMUM_RANGE, GameConfig.RANDOM_MAXIMUM_RANGE);
-
-		if (randomValue >= GameConfig.MINIMUM_NUMBER_FOR_FORWARD) {
-			car.forward();
-		}
+		race.displayWinnerNames();
 	}
 
 	private int readAttemptCount() {
@@ -81,12 +35,6 @@ public class GameController {
 		InputValidator.validateNumericInput(inputCount);
 
 		return Integer.parseInt(inputCount);
-	}
-
-	private List<Car> createCars(List<String> names) {
-		return names.stream()
-			.map(Car::new)
-			.collect(Collectors.toList());
 	}
 
 	private List<String> readCarNames() {
