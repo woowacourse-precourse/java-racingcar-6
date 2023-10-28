5da2cb5d9ab536da50b9d92ce6b225de7d415de6
cousim46 <wjdghldns24@naver.com>
2023-10-29 03:17:54 +0900

    fix : static 메서드 생성

--- src/main/java/racingcar/input/InputService.java
+++ src/main/java/racingcar/input/InputService.java
@@ -3,11 +3,11 @@ package racingcar.input;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
-    public  String writeCarName() {
+    public static  String writeCarName() {
         return Console.readLine();
     }
 
-    public int attemptCount() {
+    public static int attemptCount() {
         return Integer.parseInt(Console.readLine());
     }
 }
