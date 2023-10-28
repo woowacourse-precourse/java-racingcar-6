67dff68b07e2084b6bc831e3ada4fab24d1f5e09
kg9585 <kg9585@naver.com>
2023-10-29 00:39:37 +0900

    feat(CarController): 횟수를 입력 받을 TryCount 메소드 생성

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,19 +1,34 @@
 package racingcar.controller;
 
+import java.io.IOException;
 import java.util.List;
-import racingcar.model.Car;
+import java.util.stream.Collectors;
+import racingcar.model.CarDto;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
 
 
 public class CarController {
 
-    private final List<Car> cars;
+    private Cars cars;
+    private final InputView inputView;
 
-    public CarController(List<Car> cars) {
+    public CarController(Cars cars, InputView inputView) {
         this.cars = cars;
-
+        this.inputView = inputView;
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public void play()  {
+        cars = new Cars(inputView.getPlayerNamesInput());
+        getTryCountFromInput();
     }
+    public List<CarDto> getCarDtos() {
+        return cars.getCars().stream()
+                .map(car -> new CarDto(car.getName(), car.getPosition()))
+                .collect(Collectors.toList());
+    }
+    private void getTryCountFromInput() {
+        inputView.inputTryCount();
+    }
+
 }
