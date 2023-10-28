92baf13e87494b487056a7ff51e493bbb1b7cb80
BullChallenger <am23am45@naver.com>
2023-10-28 20:13:06 +0900

    [REFACTOR] 현재까지 작성된 로직들로 임시 클라이언트 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,16 @@
 package racingcar;
 
-import domain.RacingCar;
-import util.RandomGenerator;
+import controller.RacingCarController;
 import util.View;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarController racingCarController = new RacingCarController();
+
         View.printStartMessage();
-        RacingCar racingCar = new RacingCar(View.input());
+        racingCarController.extractName(View.input());
         View.printCycleInputMessage();
-        int N = Integer.parseInt(View.input());
-
-        for (int i = 0; i < N; i++) {
-            if (racingCar.isMoveForward(RandomGenerator.generateNumber())) {
-                racingCar.moveForward();
-            };
-        }
-
-        System.out.println(racingCar.getLocation());
+        racingCarController.startRacing(View.input());
     }
 }
