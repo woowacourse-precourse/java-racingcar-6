0585d7cf4a0bc3eb0c8d8d46860d7a0b9d51f337
xogus3492 <wkdxogus3492@gmail.com>
2023-10-28 13:44:43 +0900

    Fix(InputView): 입력 메시지 오타 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final int INITIAL_COUNT_VALUE = 0;
     private static final String RACING_CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String TRIAL_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회안가요?";
+    private static final String TRIAL_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public Map<String, Integer> inputCarName() {
         System.out.println(RACING_CAR_NAME_INPUT_MESSAGE);
