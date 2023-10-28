d317f83b9883dc9dc2e315b1e8a1cebc279d1958
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 09:59:32 +0900

    feat(InputValidator): 숫자로 구성되어있는지 검증

--- src/main/java/racingcar/io/InputValidator.java
+++ src/main/java/racingcar/io/InputValidator.java
@@ -3,6 +3,7 @@ package racingcar.io;
 import racingcar.constants.ErrorMessage;
 import racingcar.constants.InputPattern;
 import racingcar.exception.DivisionCarNamesException;
+import racingcar.exception.InputNumericException;
 
 public class InputValidator {
 
@@ -12,6 +13,12 @@ public class InputValidator {
     public static void validateDivisionCarNames(String carNames) {
         if (InputPattern.isNotMatchCarNamesPattern(carNames)) {
             throw new DivisionCarNamesException(ErrorMessage.DIVISION_CAR_NAME_ERROR.getMessage());
+        }
+    }
+
+    public static void validateNumeric(String attemptCount) {
+        if (InputPattern.isNotNumeric(attemptCount)) {
+            throw new InputNumericException(ErrorMessage.INPUT_NUMERIC_ERROR.getMessage());
         }
     }
 }
