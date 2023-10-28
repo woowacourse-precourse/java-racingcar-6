f8b18c5c5122b8d80713fb77db8f0793918bb810
KIVSH <kihongkim3@hotmail.com>
2023-10-28 23:03:29 +0900

    feat: add two method about output

--- src/main/java/racingcar/InputAndOutput.java
+++ src/main/java/racingcar/InputAndOutput.java
@@ -7,6 +7,14 @@ public class InputAndOutput {
         System.out.println(message);
     }
 
+    static void outputRaceResult(List<Car> carList) {
+
+    }
+
+    static void outputRaceWinner(List<Car> carList) {
+
+    }
+
     static List<String> inputCarNames() throws IllegalArgumentException {
         return null;
     }
