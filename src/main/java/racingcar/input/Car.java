ea55c82ab8deabfe42f7f99fd55656da9d58814b
LEM <ppoppo9802@naver.com>
2023-10-28 21:08:39 +0900

    feat: 자동차 이름 입력에 대한 예외 처리 기능 추가

--- src/main/java/racingcar/input/Car.java
+++ src/main/java/racingcar/input/Car.java
@@ -6,6 +6,11 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int NAME_IS_EMPTY = 0;
+    private static final int NAME_IS_ONE = 1;
+
     public List<String> inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNames = new ArrayList<>();
@@ -13,15 +18,36 @@ public class Car {
         for (String name: input.split(",")) {
             carNames.add(name);
         }
-        checkNameLength(carNames);
+        checkException(carNames);
         return carNames;
+    }
+    public void checkException(List<String> carNames) {
+        checkEmpty(carNames);
+        checkNameLength(carNames);
+        checkDuplication(carNames);
+        checkNameIsOne(carNames);
     }
     public void checkNameLength(List<String> carNames) {
         for (int i = 0; i < carNames.size(); i++) {
             String name = carNames.get(i);
-            if (name.length() > 5 || name.length() < 1) {
+            if (name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+    public void checkEmpty(List<String> carNames) {
+        if (carNames.size() == NAME_IS_EMPTY) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkDuplication(List<String> carNames) {
+        if (carNames.stream().distinct().count() != carNames.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkNameIsOne(List<String> carNames) {
+        if (carNames.size() == NAME_IS_ONE) {
+            throw new IllegalArgumentException();
         }
     }
 }
