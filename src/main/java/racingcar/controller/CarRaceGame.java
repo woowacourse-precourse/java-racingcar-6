7f625bcbda94dbb0a2f99157b56092989df5b928
yooooonshine <hl3470@naver.com>
2023-10-29 00:31:08 +0900

    feat: 자동차이름에 대한 입력을 받아 저장하는 메소드 구현

--- src/main/java/racingcar/controller/CarRaceGame.java
+++ src/main/java/racingcar/controller/CarRaceGame.java
@@ -1,18 +1,37 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.Car;
 import racingcar.view.CarRaceGameView;
 
 public class CarRaceGame {
     private static CarRaceGameView carRaceGameView = CarRaceGameView.getCarRaceGameView();
+    private static Car car;
 
     public CarRaceGame() {}
 
     public void run() {
-        carRaceGameView.startGameView();
         startGame();
     }
 
     public void startGame() {
-
+        String carNames = carRaceGameView.startGameView();
+        List<String> carNameList = CarNamesToList(carNames);
+        car = Car.getCar();
+        car.setCarQuantity(carNameList.size());
+        for (String carName:carNameList) {
+            car.addCarList(carName);
+        }
     }
+
+    private List<String> CarNamesToList(String carNames) {
+        return Arrays.stream(carNames.split(",")).toList();
+    }
+
+
+
+
+
+
 }
