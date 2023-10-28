859afb64769aa174955a3161c0d7e7cff5e069a3
zzawang <hobin0116@naver.com>
2023-10-28 10:18:24 +0900

    refactor: 매직넘버들을 상수로 정의
    - SEPARATOR: 자동차들의 이름을 구분하는 구분자이다.
    - BLANK_SPACE: 자동차 이름 공백 검사에서 사용되는 공백 문자이다.
    - MAX_CAR_NAME_LENGTH: 자동차 이름의 최대 길이이다.
    - DUPLICATE_CAR_NAME_MESSAGE: 자동차 이름 중복 검사에서 예외 발생 시 사용되는 문구이다.
    - BLANK_CAR_NAME_MESSAGE: 자동차 이름 공백 검사에서 예외 발생 시 사용되는 문구이다.
    - OVER_MAX_LENGTH_MESSAGE: 자동차 이름 길이 검사에서 예외 발생 시 사용되는 문구이다.

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Constant {
+    public static final String SEPARATOR = ",";
+    public static final String BLANK_SPACE = " ";
+    public static final int MAX_CAR_NAME_LENGTH = 5; // 자동차 이름의 최대 길이
+    public static final String DUPLICATE_CAR_NAME_MESSAGE = "자동차들의 이름 중 중복된 이름이 존재합니다. 게임이 종료됩니다.";
+    public static final String BLANK_CAR_NAME_MESSAGE = "자동차 이름에 공백이 포함되어 있습니다. 게임이 종료됩니다.";
+    public static final String OVER_MAX_LENGTH_MESSAGE = "자동차 이름이 5자 이상입니다. 게임이 종료됩니다.";
+}
