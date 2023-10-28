13e03ef396360192f823eb5420ad14c17bd2f8f9
bin <gimgyubin1225@gmail.com>
2023-10-28 16:31:50 +0900

    Test: 자동차 전진 기능 테스트

--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -1,5 +1,14 @@
 package study;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
 public class CarTest {
 
+    @Test
+    void 자동차_전진_기능(){
+        Car car = new Car("car1");
+        Assertions.assertEquals(1,  car.movesForward());
+    }
 }
