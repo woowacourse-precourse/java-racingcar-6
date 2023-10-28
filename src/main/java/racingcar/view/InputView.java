23aa51cdc6d88ac9b5e355b273d3ad86bab5a691
Yoonseo <jessicadbstj@gmail.com>
2023-10-28 13:01:18 +0900

    fix: 자동차이름 입력받는 함수명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 import static constant.MessgeList.INPUT_MESSAGE;
 
 public class InputView {
-    public String getInputFromPlayer(){
+    public String getCarNameFromPlayer(){
         System.out.println(INPUT_MESSAGE);
         return Console.readLine();
     }
