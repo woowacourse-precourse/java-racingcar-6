567f4e5991d57efc0c232162fc9f6614073fe974
liarreez <liarreez@gmail.com>
2023-10-28 16:52:32 +0900

    style : 불필요한 상수 제거
    가독성을 높이기 위해 기존의 상수가 한번만 쓰이고 의미가 변수명이 없어도 알 수 있기에 메서드에서 바로 사용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,16 +3,15 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printInputCarsNameMessage() {
-        System.out.println(INPUT_CARS_NAME_MESSAGE);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printInputTotalRoundMessage() {
-        System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
+        System.out.println("시도할 회수는 몇회인가요?");
     }
+
     public String inputCarsName() {
         printInputCarsNameMessage();
         return Console.readLine();
@@ -21,7 +20,7 @@ public class InputView {
     public int inputTotalRound() {
         printInputTotalRoundMessage();
         String inputRound = Console.readLine();
-        if(isNotNumeric(inputRound)) {
+        if (isNotNumeric(inputRound)) {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(inputRound);
