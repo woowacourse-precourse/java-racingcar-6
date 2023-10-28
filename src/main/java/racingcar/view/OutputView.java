fa53bd6f7b823f6dfa316013628c1d20a7d9d8f0
KangGeonyoung <kky6335@gmail.com>
2023-10-29 01:28:01 +0900

    feat(OutputView): 게임 결과 출력 - printGameResult(Car car): car의 이름과 전진 상태를 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,13 @@ import racingcar.model.Car;
 
 public class OutputView {
 
+    public static void printGameResult(Car car) {
+        System.out.print(car.getCarName() + " : ");
+        for (int i = 0; i < car.getForwardCount(); i++) {
+            System.out.print("-");
+        }
+    }
+
     public static void printBeforeResult() {
         System.out.println();
         System.out.println("실행 결과");
