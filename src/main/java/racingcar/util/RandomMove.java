54d2a2085292278f77ee740bd97253495471cafa
sehee <tpgml701@naver.com>
2023-10-29 02:39:53 +0900

    feat: 랜덤이동 판별 기능 구현

--- src/main/java/racingcar/util/RandomMove.java
+++ src/main/java/racingcar/util/RandomMove.java
@@ -1,2 +1,9 @@
-package racingcar.util;public class RandomMove {
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMove {
+    public boolean isMove(){
+        return (Randoms.pickNumberInRange(0, 9) >= 4);
+    }
 }
