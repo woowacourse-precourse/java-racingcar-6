c45e7963b16dd3b9cfd0506aedf362073fc88768
qncj5224 <qncj5224@naver.com>
2023-10-28 13:40:15 +0900

    feat: 전진 혹은 정지를 정하는 메서드 구현

--- src/main/java/racingcar/model/CarDistanceManager.java
+++ src/main/java/racingcar/model/CarDistanceManager.java
@@ -1,10 +1,22 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class CarDistanceManager {
 
-  private int createRandomNumber() {
+  public static int createRandomNumber() {
     return Randoms.pickNumberInRange(0, 9);
   }
+
+  public static void selectAction(List<Integer> distance) {
+    for (int i = 0; i < distance.size(); i++) {
+      if (createRandomNumber() >= 4) {
+        int currentDistance = distance.get(i);
+        distance.set(i, currentDistance + 1);
+      }
+    }
+  }
+
+
 }
