7373ac73ce559615bde59d3ffa1d42af2713f5ad
HyungHoKim <hkim1109@naver.com>
2023-10-28 11:01:06 +0900

    feat(main): ExceptionMessage 클래스 생성
    예외 메세지를 담은 클래스 생성

---
+++ src/main/java/racingcar/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class ExceptionMessage {
+    public static final String INPUT_LENGTH_OVERED = "입력 길이 초과";
+    public static final String CAR_NAME_LENGTH_OVERED = "자동차 이름 길이 초과";
+    public static final String CAR_NAME_DUPLICATION = "자동차 이름 중복";
+    public static final String INPUT_TYPE_MISMATCH = "입력 형식 오류";
+    public static final String INPUT_NUMBER_RANGE_MISMATCH = "입력 숫자 범위 오류";
+}
