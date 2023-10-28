5a6d7cd374b8089650d13a226f06b471bbf7c13b
KIVSH <kihongkim3@hotmail.com>
2023-10-28 20:26:59 +0900

    feat: compose InputAndOutput class

---
+++ src/main/java/racingcar/InputAndOutput.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.List;
+
+public class InputAndOutput {
+    void outputMessage(String message) {
+    }
+
+    List<String> inputCarNames() throws IllegalArgumentException {
+        return null;
+    }
+
+    int inputTryTimes() throws IllegalArgumentException {
+        return 0;
+    }
+}
