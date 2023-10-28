b59f83761498cf1a43a7fc10b78b65b7733faecf
Seol_JY <wlsdud5654@gmail.com>
2023-10-28 20:14:30 +0900

    feat(RacingGame): 자동차 이름을 통해 Cars 객체를 생성하는 메서드 구현
    1. 사용자 입력으로부터 Name객체를 생성 후, Car 객체를 생성
    2. Car 객체 리스트를 통해 Cars 생성

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -1,12 +1,26 @@
 package racingcar.game;
 
 import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Name;
 import racingcar.view.Input;
 
 public class RacingGame {
     public void play() {
         List<String> carNames = Input.getCarNames();
         int totalRace = Input.getTotalRace();
-        
+
+        createCars(carNames);
+
+    }
+
+    private Cars createCars(List<String> carNames) {
+        List<Car> cars = carNames.stream()
+                .map(Name::from)
+                .map(Car::from)
+                .toList();
+
+        return Cars.from(cars);
     }
 }
