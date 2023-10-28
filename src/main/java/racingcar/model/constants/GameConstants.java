022ff3bf3e136eeef3c837a19c5fc0f4a1da5d7d
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2023-10-29 04:17:22 +0900

    refactor(constants): 상수 파일 이동 및 기존 constant 파일 삭제
    상수를 새로운 파일로 이동하고, 기존의 constant 파일을 삭제하였습니다.

--- src/main/java/racingcar/model/constants/GameConstants.java
+++
@@ -1,7 +0,0 @@
-package racingcar.model.constants;
-
-public class GameConstants {
-
-    static final String inputNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    static final String inputTryNums = "시도할 회수는 몇회인가요?";
-}
--- src/main/java/racingcar/model/constants/ResultConstants.java
+++
@@ -1,7 +0,0 @@
-package racingcar.model.constants;
-
-public class ResultConstants {
-    static final String playResult = "실행 결과";
-    static final String winner = "최종 우승자 :";
-
-}
