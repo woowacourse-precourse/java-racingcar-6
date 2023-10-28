2c1e6b8e735eaca74cb29333eb346c553f1fe3c9
unknown <y2kf9a0q@gmail.com>
2023-10-28 16:15:12 +0900

    feat: skeleton of Logs.java

---
+++ src/main/java/racingcar/domain/Logs.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+public class Logs {
+    private Logs() {
+
+    }
+
+    public static void inputCarNames() {
+
+    }
+
+    public static void inputGameTurns() {
+
+    }
+
+    public static void newLine() {
+
+    }
+
+    public static void getGameResult() {
+
+    }
+
+    public static void car(Car car) {
+
+    }
+
+    public static void result(String result) {
+
+    }
+}
