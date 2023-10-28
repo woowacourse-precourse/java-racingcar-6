f0fa15aa621ff39abc6f81651ea9a57d73de09ae
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-29 05:03:50 +0900

    feat(CarNames): 입력받은 이름을 콤마 기준으로 쪼개기
    공백여부와 상관없이 콤마 기준으로 쪼개는 기능

--- src/main/java/racingcar/validation/CarNames.java
+++ src/main/java/racingcar/validation/CarNames.java
@@ -1,9 +1,16 @@
 package racingcar.validation;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarNames {
     void checkNullInput(String in) throws IllegalArgumentException {
         if (in == null || in.isEmpty()) {
             throw new IllegalArgumentException("아무 입력도 들어오지 않았습니다.");
         }
+    }
+
+    List<String> splitNames(String in) {
+        return Arrays.asList(in.split(",", -1));
     }
 }
--- src/test/java/racingcar/validation/ValidationTest.java
+++ src/test/java/racingcar/validation/ValidationTest.java
@@ -1,7 +1,10 @@
 package racingcar.validation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ValidationTest {
@@ -10,5 +13,18 @@ public class ValidationTest {
         String input = "";
         assertThatThrownBy(() -> new CarNames().checkNullInput(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력받은_이름을_쪼개기() {
+        List<String> inputs = Arrays.asList("pobi,woni,jun", " ,  ",
+                "hi", ",,,", ",a,", "  ");
+        List<List<String>> results = Arrays.asList(Arrays.asList("pobi", "woni", "jun"),
+                Arrays.asList(" ", "  "), Arrays.asList("hi"), Arrays.asList("", "", "", ""),
+                Arrays.asList("", "a", ""), Arrays.asList("  "));
+
+        for (int i = 0; i < inputs.size(); i++) {
+            assertThat(new CarNames().splitNames(inputs.get(i))).isEqualTo(results.get(i));
+        }
     }
 }
