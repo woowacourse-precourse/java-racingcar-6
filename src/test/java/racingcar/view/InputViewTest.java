f5748b30e3f9b62d40e499e15f4ca6e3b24fcb98
박준영 <wnsdud6969@naver.com>
2023-10-28 20:16:46 +0900

    test(InputViewTest): add test to verify user input view
    Add new event to InputViewTest
    - add ByteArrayInputStream
    - add InputView instance
    - add InputStream instance
    - set InputStream of System.in
    - call inputCarNames() and compare List<String>

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,9 +2,25 @@ package racingcar.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
+
+    private InputView inputView;
+    private String userInput;
+    private InputStream inputStream;
+
+    public InputViewTest() {
+        this.inputView = InputView.getInstance();
+
+    }
+
+    public static InputStream parseUserInputToInputStream(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
 
     @Test
     public void InputView() {
@@ -12,5 +28,17 @@ class InputViewTest {
         InputView inputView2 = InputView.getInstance();
 
         assertEquals(inputView1, inputView2);
+    }
+
+    @Test
+    public void inputCarNames() {
+        userInput = "pobi,woni,jun\n";
+        inputStream = parseUserInputToInputStream(userInput);
+        System.setIn(inputStream);
+
+        List<String> expectedCarNames = List.of("pobi", "woni", "jun");
+        List<String> carNames = inputView.inputCarNames();
+
+        assertEquals(expectedCarNames, carNames);
     }
 }
