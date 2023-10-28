2d45eac87df156618b7e84ad9e1a91e8d68be0f0
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 11:22:45 +0900

    feat(RacingCarController): 게임 실행 기능 작성

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,30 @@
+package racingcar.controller;
+
+import racingcar.io.InputManager;
+import racingcar.io.OutputView;
+import racingcar.model.Cars;
+import racingcar.service.RacingCarService;
+
+public class RacingCarController {
+
+    private final InputManager inputManager;
+    private final OutputView outputView;
+    private final RacingCarService racingCarService;
+
+    public RacingCarController() {
+        this.inputManager = new InputManager();
+        this.outputView = new OutputView();
+        this.racingCarService = new RacingCarService();
+    }
+
+    void run() {
+        outputView.printRacingCarNamesMessage();
+        startRacing();
+    }
+
+    private void startRacing() {
+        Cars cars = inputManager.inputCarNames();
+        int attemptCount = inputManager.inputAttemptCount();
+        racingCarService.startRacing(cars, attemptCount);
+    }
+}
