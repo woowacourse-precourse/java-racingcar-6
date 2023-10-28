52821bd1fa7b57d6fdf77e516d1511d45b08270d
minisyu <iswater.kim@gmail.com>
2023-10-28 11:44:43 +0900

    feat(Cars): Car 일급 컬렉션 생성

--- src/main/java/racingcar/domain/game/Cars.java
+++ src/main/java/racingcar/domain/game/Cars.java
@@ -1,7 +1,11 @@
 package racingcar.domain.game;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.domain.dto.CarDto;
+import racingcar.domain.dto.CarsDto;
+import racingcar.util.NumberPicker;
 
 public class Cars {
     private final List<Car> cars;
@@ -18,5 +22,14 @@ public class Cars {
         return Arrays.stream(input.split(","))
                 .map(Car::of)
                 .toList();
+    }
+
+    public CarsDto moveOnce(NumberPicker numberPicker) {
+        List<CarDto> carDtoList = new ArrayList<>();
+        for (Car car : cars) {
+            CarDto carDto = car.moveOnce(numberPicker);
+            carDtoList.add(carDto);
+        }
+        return new CarsDto(carDtoList);
     }
 }
