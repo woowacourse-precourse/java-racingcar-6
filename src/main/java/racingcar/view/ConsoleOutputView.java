719bf14260f19cad879922c700086102b387266e
eora21 <eora21@naver.com>
2023-10-28 17:45:05 +0900

    feat: 🎸 추가 사용자에게 콘솔 출력을 통해 자동차 이름을 입력받을 수 있도록 알려주는 메서드 구현

---
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+public class ConsoleOutputView implements OutputView {
+
+    private static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.";
+    private static final String SEPARATED_BY = "(이름은 %s(%s) 기준으로 구분)";
+
+    @Override
+    public void requestCarNames(String delimiterDescription, String delimiter) {
+        String separateMessage = createSeparateMessage(delimiterDescription, delimiter);
+        System.out.println(REQUEST_CAR_NAMES + separateMessage);
+    }
+
+    private String createSeparateMessage(String delimiterDescription, String delimiter) {
+        return String.format(SEPARATED_BY, delimiterDescription, delimiter);
+    }
+}
