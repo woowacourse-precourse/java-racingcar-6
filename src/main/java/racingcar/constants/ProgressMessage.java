fec93d482457f43fce789b9bc4ad738bb72a172b
GaBaljaintheroom <junsu1222@naver.com>
2023-10-29 00:57:32 +0900

    feat(ProgressMessage): 최종 우승자 메시지 생성

--- src/main/java/racingcar/constants/ProgressMessage.java
+++ src/main/java/racingcar/constants/ProgressMessage.java
@@ -3,7 +3,8 @@ package racingcar.constants;
 public enum ProgressMessage {
     INPUT_RACING_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_ATTEMPT_COUNT("시도할 회수는 몇회인가요?"),
-    EXECUTION_RESULT("실행 결과");
+    EXECUTION_RESULT("실행 결과"),
+    FINAL_WINNERS("최종 우승자");
     private final String message;
 
     ProgressMessage(String message) {
