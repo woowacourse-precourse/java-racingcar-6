48100377508ba643d0b2d52745ac886d48f5ded6
Laniel Park <Laniel8177@gmail.com>
2023-10-28 20:11:21 +0900

    feat : add Seperator Class & move Seperator Constant
    Sepearator Class로 Message Class에 있던 Seperator 상수 이동

---
+++ src/main/java/racingcar/constant/Separator.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class Separator {
+    private Separator() {
+    }
+
+    public static final String INPUT_CAR_SEPARATOR = ",";
+    public static final String OUTPUT_CAR_SEPARATOR = ", ";
+    public static final String KEY_VALUE_SEPARATOR = " : ";
+}
