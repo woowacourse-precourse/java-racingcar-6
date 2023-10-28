0a37a62981a8596b8086f22ba68f474384142046
SUJI-J <suzy852@naver.com>
2023-10-28 21:26:12 +0900

    feat: 자동차 전진 기능 함수 호출 구현

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -1,12 +1,18 @@
 package racingcar;
 
+import java.util.Map;
+
 public class RacingCarController {
+
     public void run() {
-        OutputView.printStart();
         gameStart();
     }
 
     private void gameStart() {
-        InputView.inputCarNames();
+        Map<String, String> carNamesMap = InputView.CarNames();
+        int attemptNumber = InputView.AttemptNumber();
+        OutputView.printEnter();
+        OutputView.printGameResult();
+        GameProgress.racing(carNamesMap, attemptNumber);
     }
 }
