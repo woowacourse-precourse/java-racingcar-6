3b98d26af7358b15478e763423444acdfdb5544e
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-29 00:47:13 +0900

    refactor: 기호 상수 적용

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,95 +1,99 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constant.CommonSymbol;
 import racingcar.constant.GameConfig;
 import racingcar.model.Car;
 import racingcar.validation.InputValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class GameController {
 
-    public GameController() {}
+	public GameController() {
+	}
 
-    public void startGame() {
-        List<Car> cars = createCars(inputCarNames());
-        int attemptCount = inputAttemptCount();
+	public void startGame() {
+		List<Car> cars = createCars(inputCarNames());
+		int attemptCount = inputAttemptCount();
 
-        OutputView.printExecutionResultMessage();
-        for (int i = 0; i < attemptCount; i++) {
-            moveAllCarsForward(cars);
-            displayAllCarsPosition(cars);
-        }
+		OutputView.printExecutionResultMessage();
+		for (int i = 0; i < attemptCount; i++) {
+			moveAllCarsForward(cars);
+			displayAllCarsPosition(cars);
+		}
 
-        List<Car> winners = findWinners(cars);
-        displayWinnerNames(winners);
-    }
+		List<Car> winners = findWinners(cars);
+		displayWinnerNames(winners);
+	}
 
-    private List<Car> findWinners(List<Car> cars) {
-        List<Car> sortedCars = sortCarsByPosition(cars);
-        int maxPosition = sortedCars.get(0).getPosition();
+	private List<Car> findWinners(List<Car> cars) {
+		List<Car> sortedCars = sortCarsByPosition(cars);
+		int maxPosition = sortedCars.get(0).getPosition();
 
-        return sortedCars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .collect(Collectors.toList());
-    }
+		return sortedCars.stream()
+			.filter(car -> car.getPosition() == maxPosition)
+			.collect(Collectors.toList());
+	}
 
-    private void displayWinnerNames(List<Car> winners) {
-        OutputView.printWinner(winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.toList()));
-    }
+	private void displayWinnerNames(List<Car> winners) {
+		OutputView.printWinner(winners.stream()
+			.map(Car::getName)
+			.collect(Collectors.toList()));
+	}
 
-    private List<Car> sortCarsByPosition(List<Car> cars) {
-        return cars.stream()
-                .sorted(Comparator.comparingInt(Car::getPosition).reversed())
-                .collect(Collectors.toList());
-    }
+	private List<Car> sortCarsByPosition(List<Car> cars) {
+		return cars.stream()
+			.sorted(Comparator.comparingInt(Car::getPosition).reversed())
+			.collect(Collectors.toList());
+	}
 
-    private void displayAllCarsPosition(List<Car> cars) {
-        for (Car car : cars) {
-            OutputView.printCarPosition(car.getName(), car.getPosition());
-        }
+	private void displayAllCarsPosition(List<Car> cars) {
+		for (Car car : cars) {
+			OutputView.printCarPosition(car.getName(), car.getPosition());
+		}
 
-        OutputView.printNewLine();
-    }
+		OutputView.printNewLine();
+	}
 
-    private void moveAllCarsForward(List<Car> cars) {
-        for (Car car : cars) {
-            moveForward(car);
-        }
-    }
+	private void moveAllCarsForward(List<Car> cars) {
+		for (Car car : cars) {
+			moveForward(car);
+		}
+	}
 
-    private void moveForward(Car car) {
-        int randomValue = Randoms.pickNumberInRange(GameConfig.RANDOM_MINIMUM_RANGE, GameConfig.RANDOM_MAXIMUM_RANGE);
+	private void moveForward(Car car) {
+		int randomValue = Randoms.pickNumberInRange(GameConfig.RANDOM_MINIMUM_RANGE, GameConfig.RANDOM_MAXIMUM_RANGE);
 
-        if (randomValue >= GameConfig.MINIMUM_NUMBER_FOR_FORWARD) {
-            car.forward();
-        }
-    }
+		if (randomValue >= GameConfig.MINIMUM_NUMBER_FOR_FORWARD) {
+			car.forward();
+		}
+	}
 
-    private int inputAttemptCount() {
-        String inputCount = InputView.inputAttemptCount();
+	private int inputAttemptCount() {
+		String inputCount = InputView.inputAttemptCount();
 
-        InputValidator.validateNumericInput(inputCount);
+		InputValidator.validateNumericInput(inputCount);
 
-        return Integer.parseInt(inputCount);
-    }
+		return Integer.parseInt(inputCount);
+	}
 
-    private List<Car> createCars(List<String> names) {
-        return names.stream()
-                    .map(Car::new)
-                    .collect(Collectors.toList());
-    }
+	private List<Car> createCars(List<String> names) {
+		return names.stream()
+			.map(Car::new)
+			.collect(Collectors.toList());
+	}
 
-    private List<String> inputCarNames() {
-        String inputNames = InputView.inputCarNames();
+	private List<String> inputCarNames() {
+		String inputNames = InputView.inputCarNames();
 
-        return Arrays.stream(inputNames.split(","))
-                .peek(InputValidator::validateInputNameLength)
-                .collect(Collectors.toList());
-    }
+		return Arrays.stream(inputNames.split(CommonSymbol.SYMBOL_COMMA))
+			.peek(InputValidator::validateInputNameLength)
+			.collect(Collectors.toList());
+	}
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,15 +5,16 @@ import racingcar.constant.Message;
 
 public class InputView {
 
-    public InputView() {}
+	public InputView() {
+	}
 
-    public static String inputCarNames() {
-        System.out.println(Message.CAR_NAME_INPUT_MESSAGE);
-        return Console.readLine();
-    }
+	public static String inputCarNames() {
+		System.out.println(Message.CAR_NAME_INPUT_MESSAGE);
+		return Console.readLine();
+	}
 
-    public static String inputAttemptCount() {
-        System.out.println(Message.ATTEMPT_COUNT_MESSAGE);
-        return Console.readLine();
-    }
+	public static String inputAttemptCount() {
+		System.out.println(Message.ATTEMPT_COUNT_MESSAGE);
+		return Console.readLine();
+	}
 }
