a8168b546fa550d0bef53922279d2c482c98b793
hyuna <monicajo0@naver.com>
2023-10-29 00:41:39 +0900

    test(GameManagerTest): 실행 결과 출력 테스트 추가

--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -2,12 +2,17 @@ package racingcar;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 
 public class GameManagerTest {
     private static final int MOVING_NUMBER = 4;
     private static final int STOP_NUMBER = 2;
+    private static final String CAR_NAME = "test";
 
     private GameManager gameManager;
     private Car car;
@@ -15,11 +20,11 @@ public class GameManagerTest {
     @BeforeEach
     void setUp() {
         gameManager = new GameManager();
-        car = new Car("test");
+        car = new Car(CAR_NAME);
     }
 
     @Test
-    void 전진(){
+    void 전진() {
         assertRandomNumberInRangeTest(
                 () -> {
                     gameManager.moveCar(car);
@@ -30,11 +35,41 @@ public class GameManagerTest {
     }
 
     @Test
-    void 정지(){
+    void 정지() {
         assertRandomNumberInRangeTest(
                 () -> {
                     gameManager.moveCar(car);
                     assertThat(car.getLocation()).isEqualTo(0);
+                },
+                STOP_NUMBER
+        );
+    }
+
+
+    @Test
+    void 자동차_전진_결과_생성() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(car);
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    gameManager.moveCar(car);
+                    gameManager.moveCar(car);
+                    assertThat(gameManager.makeResult(cars)).isEqualTo(CAR_NAME + " : --\n");
+                },
+                MOVING_NUMBER, MOVING_NUMBER
+        );
+    }
+
+    @Test
+    void 자동차_정지_결과_생성() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(car);
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    gameManager.moveCar(car);
+                    assertThat(gameManager.makeResult(cars)).isEqualTo(CAR_NAME + " : \n");
                 },
                 STOP_NUMBER
         );
