b9b40ae11ce8931bdac72de123f0ad7583f89d7b
GumaLee <rkdxhd97@naver.com>
2023-10-28 20:45:37 +0900

    feature: 사용자 입력 기능 구현

---
+++ src/main/java/racingcar/view/ViewInput.java
@@ -0,0 +1,31 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ViewInput {
+
+    private static StringTokenizer carsToken;
+
+    public List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        carsToken = new StringTokenizer(Console.readLine(), ",");
+
+        List<String> cars = new ArrayList<>();
+        while(carsToken.hasMoreTokens()) {
+            cars.add(carsToken.nextToken().trim());
+        }
+
+        return cars;
+    }
+
+    public int getNumberOfRounds() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int roundNumbers = Integer.parseInt(Console.readLine().trim());
+
+        return roundNumbers;
+    }
+}
---
+++ src/test/java/racingcar/ViewInputTest.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ViewInputTest {
+
+}
