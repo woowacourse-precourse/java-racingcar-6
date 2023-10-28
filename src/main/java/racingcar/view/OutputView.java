59ef821c595e5b2a572c9db2fa632b67fdc614cf
Yoonseo <jessicadbstj@gmail.com>
2023-10-28 14:32:38 +0900

    feat: 공동 우승자 안내 문구 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,20 +1,29 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static constant.MessgeList.EXECUTION_RESULT_MESSAGE;
 import static constant.MessgeList.WINNER_MESSAGE;
 
 public class OutputView {
-    public void printExecutionResultMessage(){
+    public void printExecutionResultMessage() {
         System.out.println(EXECUTION_RESULT_MESSAGE);
     }
-    public void printExecutionResult(String carName, int movingForwardNumber){
-        System.out.print(carName+" : ");
+
+    public void printExecutionResult(String carName, int movingForwardNumber) {
+        System.out.print(carName + " : ");
         for (int i = 0; i < movingForwardNumber; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
-    public void printSoleWinner(String winner){
-        System.out.println(WINNER_MESSAGE+winner);
+
+    public void printSoleWinner(String winner) {
+        System.out.println(WINNER_MESSAGE + winner);
+    }
+
+    public void printJointWinner(List<String> winners) {
+        System.out.println(WINNER_MESSAGE + winners.stream().map(String::valueOf).collect(Collectors.joining(", ")));
     }
 }
