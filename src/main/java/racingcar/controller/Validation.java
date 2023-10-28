db5ea3f7df80df0f7e28b45a7426d4871204ba43
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 23:12:02 +0900

    refactor : 하나의 작업만 수행하도록 수정

--- src/main/java/racingcar/controller/Validation.java
+++ src/main/java/racingcar/controller/Validation.java
@@ -9,34 +9,39 @@ public class Validation {
     public static void validateLength(List<String> cars) {
         for (String car : cars) {
             if (car.length() > 5) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
             }
         }
     }
 
     public static void validateEmtpy(List<String> cars) {
         if (cars.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("자동차의 이름을 입력해주세요.");
         }
     }
 
     public static void validateDuplicate(List<String> cars) {
-        Set<String> checkForDuplicates = new HashSet<>();
-
-        for (String car : cars) {
-            String lowercase = car.toLowerCase();
-            checkForDuplicates.add(lowercase);
-        }
+        Set<String> checkForDuplicates = makeLowercase(cars);
 
         if (checkForDuplicates.size() != cars.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복된 자동차가 있습니다.");
         }
+    }
+
+    public static Set<String> makeLowercase(List<String> cars) {
+        Set<String> lowercaseCars = new HashSet<>();
+
+        for (String car : cars) {
+            lowercaseCars.add(car.toLowerCase());
+        }
+
+        return lowercaseCars;
     }
 
     public static void validateCharacters(List<String> cars) {
         for (String car : cars) {
             if (!car.matches("[a-zA-Z]")) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("자동차의 이름은 알파벳으로만 구성되야 합니다.");
             }
         }
     }
