735273fa08524586d32c2529e1db32153bd8c078
Seol_JY <wlsdud5654@gmail.com>
2023-10-28 19:05:50 +0900

    feat(domain): 자동차 이름을 저장하고 검증하는 기능 구현
    Name 클래스 구현:
    - Car에서 사용 할 이름 저장
    - toString 오버라이딩
    - 이름에 대한 Validataion 지원 (이름 길이가 min과 max length 사이인지 검증)

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,4 +1,30 @@
 package racingcar.domain;
 
 public class Name {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+
+    private final String value;
+
+    private Name(String value) {
+        validateLength(value);
+        this.value = value;
+    }
+
+    public static Name from(String value) {
+        return new Name(value);
+    }
+
+    private static void validateLength(String value) {
+        int length = value.length();
+
+        if (length < MIN_LENGTH || length > MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
