7ba460f75237e3704ecd13fb15cc40df64fa7fe6
DK <laancer4@gmail.com>
2023-10-28 18:51:14 +0900

    feat : OutputView 구현, Util class 로 사용하는 것이 옳을까?

---
+++ src/main/java/console/OutputView.java
@@ -0,0 +1,48 @@
+package console;
+
+import car.Car;
+import car.Cars;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String ROUND_RESULT = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자: ";
+    private static final String PROGRESS_PREFIX = " : ";
+    private static final String SPLIT_DELIMITER = ", ";
+    private static final String LOCATION_COUNTER = "-";
+
+    private OutputView() { // util class
+        throw new IllegalArgumentException();
+    }
+
+    public static void printRoundResult(Cars cars) {
+        System.out.println(ROUND_RESULT);
+        for (Car car : cars.getCarList()) {
+            printCarProgress(car);
+        }
+        System.out.println();
+    }
+
+    private static void printCarProgress(Car car) {
+        StringBuilder progress = new StringBuilder(car.getName() + PROGRESS_PREFIX);
+        for (int i = 0; i < car.getLocation(); i++) {
+            progress.append(LOCATION_COUNTER);
+        }
+        System.out.println(progress);
+    }
+
+    public static void printWinners(List<Car> winners) {
+        System.out.print(FINAL_WINNER);
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            printDelimiterIfNotLast(winners, i);
+        }
+    }
+
+    private static void printDelimiterIfNotLast(List<Car> winners, int i) {
+        if (i < winners.size() - 1) {
+            System.out.print(SPLIT_DELIMITER);
+        }
+    }
+}
