61de2a2797bd7d16c42b1fbb8f14318f87201179
SUJI-J <suzy852@naver.com>
2023-10-28 21:18:58 +0900

    style: 최종 우승자 메세지 공백 추가

--- src/main/java/racingcar/GameMessage.java
+++ src/main/java/racingcar/GameMessage.java
@@ -3,9 +3,9 @@ package racingcar;
 public class GameMessage {
 
     public static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String INPUT_ATTEMPT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String OUTPUT_RESULT_MESSAGE = "실행 결과";
-    public static final String OUTPUT_WINNER_MESSAGE = "최종 우승자 :";
+    public static final String OUTPUT_WINNER_MESSAGE = "최종 우승자 : ";
     public static final String INPUT_DUPLICATE_EXCEPTION_MESSAGE = "자동차 이름은 중복없이 입력해주세요.";
     public static final String INPUT_LENGTH_EXCEPTION_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
 }
