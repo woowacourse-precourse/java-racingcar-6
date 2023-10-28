944aa691102358b0d700a7a975d5eb19c052067a
KimDabomi <cdabomi@nate.com>
2023-10-28 16:59:28 +0900

    test: Car method

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -9,6 +9,28 @@ class CarTest {
     @Test
     void Car_인스턴스_생성확인() {
         Car car = new Car("kim");
-        assertThat(car.carName).isEqualTo("kim");
+    }
+
+    @Test
+    void getCarName_동작확인() {
+        Car car = new Car("da");
+        String carName = car.getCarName();
+        assertThat(carName).isEqualTo("da");
+    }
+
+    @Test
+    void getPosition_동작확인() {
+        Car car = new Car("bo");
+        int position = car.getPosition();
+        assertThat(position).isEqualTo(0);
+    }
+
+    @Test
+    void move_동작확인() {
+        Car car = new Car("mi");
+        int prevPosition = car.getPosition();
+        car.move(new MoveStatus());
+        int nextPosition = car.getPosition();
+        assertThat(nextPosition).isGreaterThanOrEqualTo(prevPosition);
     }
 }
