52e7713f14ee47dce8e4fccbb270e57f1cdb4285
Juhyorim <joohr1234@gmail.com>
2023-10-29 04:32:33 +0900

    test(racingcar): 이동 횟수 입력값 검증 테스트 작성

--- src/test/java/racingcar/domain/InputValidatorTest.java
+++ src/test/java/racingcar/domain/InputValidatorTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class InputValidatorTest {
+    private InputValidator inputValidator = new InputValidator();
+
     @DisplayName("자동차 이름 검증")
     @Nested
     class ValidateCarNames {
@@ -78,6 +80,49 @@ class InputValidatorTest {
             assertThatThrownBy(() -> inputValidator.validateCarNames(input))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("자동차 이름은 2개 이상 입력되어야 합니다");
+        }
+    }
+
+    @DisplayName("이동 횟수 검증")
+    @Nested
+    class ValidateMoveCount {
+        @Test
+        @DisplayName("정상 동작")
+        void good_case() {
+            //when
+            String goodMoveCountInput = "3";
+
+            //then
+            assertDoesNotThrow(() -> {
+                inputValidator.validateMoveCount(goodMoveCountInput);
+            });
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"-3", "-1", "ㅇ", "#", "c"})
+        @DisplayName("자연수가 아닐 때 예외 발생")
+        void 자연수_아닐때_예외_발생(String input) {
+            assertThatThrownBy(() -> inputValidator.validateMoveCount(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이동횟수는 자연수여야합니다.");
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"0","00","000"})
+        @DisplayName("0일 때 예외 발생")
+        void zero_예외_발생(String input) {
+            assertThatThrownBy(() -> inputValidator.validateMoveCount(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이동횟수는 0이어서는 안됩니다.");
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"1000000000","20000000000000","1000000001"})
+        @DisplayName("자릿수 초과 예외 발생")
+        void 자릿수_초과_예외_발생(String input) {
+            assertThatThrownBy(() -> inputValidator.validateMoveCount(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이동 횟수는 아홉 자릿수 이내여야 합니다.");
         }
     }
 }
