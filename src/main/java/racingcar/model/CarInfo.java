3b3022c430d311b350139a7ad868f0d20dfd9ae2
goragoraki <gorakio0126@gmail.com>
2023-10-28 23:20:24 +0900

    feat(CarInfo): add isMove function
    Add isMove function that creates random number and return true if the random number is over 4

--- src/main/java/racingcar/model/CarInfo.java
+++ src/main/java/racingcar/model/CarInfo.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.util.validation;
 
 public class CarInfo {
@@ -13,5 +15,9 @@ public class CarInfo {
         for (String s : this.carName) {
             validation.checkCarName(s);
         }
+    }
+    public boolean isMove() {
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        return randomNum >= 4;
     }
 }
