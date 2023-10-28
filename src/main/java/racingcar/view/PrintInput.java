d841cb590df34c5c34e9b32c4a45cf3ce071667c
alswp006 <alswp006@gmail.com>
2023-10-28 15:39:40 +0900

    사용자에게 input을 받을 때 출력할 내용을 담을 PrintInput 클래스 구현

--- src/main/java/racingcar/view/PrintInput.java
+++ src/main/java/racingcar/view/PrintInput.java
@@ -8,18 +8,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PrintInput {
-    public List<String> inputCar() {
+    public static void inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] car = Console.readLine().split(",");
-        ValidCheck.namesize_check(car);
-
-        return new ArrayList<>(Arrays.asList(car));
     }
 
-    public int inputNumberOfMove(){
+    public static void inputNumberOfMove(){
         System.out.println("시도할 회수는 몇회인가요?");
-        String input = Console.readLine();
-
-        return ValidCheck.moveTypeCheck(input);
     }
 }
