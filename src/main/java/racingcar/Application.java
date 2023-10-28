f0ab13fbaaa8c8d93c8d70fa15ff3c64d1b72f8d
honee8583 <honee8583@naver.com>
2023-10-28 22:01:03 +0900

    feat(carname): 사용자로부터 자동차이름을 입력받는 기능 구현
    사용자로부터 쉽표 단위로 여러개의 자동차이름을 입력받고 저장하는 기능 구현:
    
    - camp.nextstep.edu.missionutils.Console 라이브러리의 readLine()을 사용해서 사용자로부터 자동차이름 입력받고 저장
    - 이름이 5자를 넘어가는 경우 IllegalArgumentException 예외 발생
    - 중복되는 이름이 들어오는 경우 IllegalArgumentException 예외 발생
    - 입력받은 이름들에 공백이 섞여 있는 경우 trim()을 사용해 공백 제거
    - 입력 받은 자동차를 Map에 key값으로 이름을, 아직 전진하지 않은 상태이므로 value값으로 공백 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+    static Map<String, String> forwardMap;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        forwardMap = new HashMap<>();
+        // 1. 자동차 이름 입력받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        getCars();
+    }
+
+    static void getCars() {
+        String carNames = Console.readLine();
+        for (String car : carNames.split(",")) {
+            String carName = car.trim();
+            if (carName.length() > 5 || forwardMap.containsKey(carName)) {
+                throw new IllegalArgumentException();
+            }
+            forwardMap.put(carName, "");
+        }
     }
 }
