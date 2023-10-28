0e1e3d2284661af653628ff09e364686f90ff17b
kunsanglee <gosmdochee2@gmail.com>
2023-10-28 14:40:29 +0900

    feat(RacingCarOutputView): 최종 우승자를 출력하는 기능 구현 - printEachRacingResult()의 로직을 일부 수정했다. 매개변수로 전체 경기 결과가 들어있는 RacingResult를 받아서 내부에 있는 각 회차 결과인 RacingRoundResult를 출력하도록 수정했다. - 최종 우승자를 출력하는 기능인 printWinner()를 추가했다. RacingResult를 받아서 최종 우승자 리스트를 얻고, 우승자가 한 명 이상일 경우 쉼표를 구분자로 문자열을 만들어 출력하도록 했다.

--- src/main/java/racingcar/RacingCarOutputView.java
+++ src/main/java/racingcar/RacingCarOutputView.java
@@ -1,17 +1,29 @@
 package racingcar;
 
 
+import java.util.List;
+
 public class RacingCarOutputView {
     public static final String RACING_EXECUTE_MESSAGE = "실행 결과";
     public static final String EACH_RACING_RESULT_FORMAT = "%s : %s";
     public static final String POSITION_PROGRESS = "-";
+    public static final String DELIMITER = ", ";
+    public static final String FINAL_WINNERS_MESSAGE = "최종 우승자 : ";
 
     public void printStartMessage() {
         System.out.println(RACING_EXECUTE_MESSAGE);
     }
 
-    public void printEachRacingResult(Cars cars) {
-        for (Car car : cars.getReadOnlyCarList()) {
+    public void printEachRacingResult(RacingResult racingResult) {
+        List<RacingRoundResult> racingRoundResults = racingResult.getRacingRoundResults();
+        for (RacingRoundResult r : racingRoundResults) {
+            printCarsNameAndPosition(r);
+        }
+    }
+
+    private void printCarsNameAndPosition(RacingRoundResult r) {
+        List<Car> cars = r.getCars();
+        for (Car car : cars) {
             String carName = car.getName();
             int position = car.getPosition();
             String positionProgress = convertPositionValue(position);
@@ -22,5 +34,11 @@ public class RacingCarOutputView {
 
     private String convertPositionValue(int position) {
         return POSITION_PROGRESS.repeat(position);
+    }
+
+    public void printWinner(RacingResult racingResult) {
+        List<String> finalWinners = racingResult.getFinalWinners();
+        String result = String.join(DELIMITER, finalWinners);
+        System.out.println(FINAL_WINNERS_MESSAGE + result);
     }
 }
