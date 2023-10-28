383624bee0e8e4eec93899f8dc3b0f485ea65701
Picka <marmong@naver.com>
2023-10-28 19:51:33 +0900

    feat: add 시도할 횟수(전진하는 횟수)를 입력 받는 기능

---
+++ src/main/java/racingcar/domain/inputhandler/AttemptNumberInputter.java
@@ -0,0 +1,34 @@
+package racingcar.domain.inputhandler;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AttemptNumberInputter {
+
+    public Integer getAttemptNumber() {
+        printInputMessage();
+        return transformAttemptNumberToInt(readLine());
+    }
+
+    private void printInputMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public boolean isValidNumber(String attemptNumber) {
+        try {
+            Integer.parseInt(attemptNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public Integer transformAttemptNumberToInt(String attemptNumber) {
+        if (isValidNumber(attemptNumber)) {
+            return Integer.parseInt(attemptNumber);
+        }
+        throw new IllegalArgumentException("잘못된 인수를 입력하였습니다. " + attemptNumber);
+    }
+}
