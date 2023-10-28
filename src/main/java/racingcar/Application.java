595443ded26437882f7ce66e739b106df2a6cec8
gombasan <xogmlwkdi@naver.com>
2023-10-28 10:03:22 +0000

    GameUI 인스턴스 추가
    - 레이싱 게임의 메시지 출력과 사용자의 입력을 담당하는 클래스인 GameUI 를 추가하여 생성하는 인스턴스롤 추가하였습니다.
    - UI 는 언제든지 새로 만들어져 사용될 가능성이 있다고 생각하여 외부에서 의존성을 주입하도록 하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
+        GameUI gameUI = new GameUI();
+        RacingGame racingGame = new RacingGame(gameUI);
         racingGame.run();
     }
 }
