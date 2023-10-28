3a6727c0cb2ae3523cb98275e8efc990da33fdac
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-29 01:11:38 +0900

    test : 자동차 이름 입력 예외 처리 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,15 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.controller.Validation;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,11 +18,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -29,6 +32,38 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 이름_입력안한_경우_처리() {
+        List<String> input = new ArrayList<>();
+
+        assertThatThrownBy(() -> Validation.validateEmtpy(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름_길이_예외_처리() {
+        List<String> input = List.of("abc", "cde", "abcdef");
+
+        assertThatThrownBy(() -> Validation.validateLength(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름_중복_예외_처리() {
+        List<String> input = List.of("Abc", "abc");
+
+        assertThatThrownBy(() -> Validation.validateDuplicate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름_알파벳_이외_문자_예외_처리() {
+        List<String> input = List.of("abd", "qAz", "aq1");
+        
+        assertThatThrownBy(() -> Validation.validateCharacters(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
