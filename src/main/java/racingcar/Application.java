964cd3cb34ca07cad59af2a5a5b052ae41800b1f
Minjeong63 <kmj06030@naver.com>
2023-10-28 17:56:55 +0900

    Feat: 게임 진행하는 메소드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.game.GameService;
+import racingcar.game.GameServiceImpl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameService gameService = new GameServiceImpl();
+        gameService.playGame();
     }
 }
