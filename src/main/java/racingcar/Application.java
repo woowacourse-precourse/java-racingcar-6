450182e11de59f625ea243b212c1b0b21e0967e7
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 22:31:30 +0900

    feat: Application 메인 메소드 개발  - 모든 테스트 케이스 통과  - var 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.handler.InputConvertor;
+import racingcar.handler.InputHandler;
+import racingcar.handler.InputValidator;
+import racingcar.service.RacingCarService;
+import racingcar.service.random.DefaultRandomNumberProvider;
+import racingcar.view.OutputView;
+import racingcar.view.input.ConsoleInputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        var inputHandler = new InputHandler(new InputConvertor(), new InputValidator(),
+            new ConsoleInputView());
+
+        var racingCarController = new RacingCarController(inputHandler, new OutputView(),
+            new RacingCarService(new DefaultRandomNumberProvider()));
+
+        racingCarController.run();
     }
 }
