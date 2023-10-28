41ce55a5ffda9baf607d6c1319d20bc8d86b88b2
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 17:15:54 +0900

    feat(MoveCarsService): 무작위 값이 4 이상일 경우 자동차들을 전진시키는 기능 구현

---
+++ src/main/java/racingcar/service/MoveCarsService.java
@@ -0,0 +1,28 @@
+package racingcar.service;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.util.generator.NumberGenerator;
+
+public class MoveCarsService {
+	
+	private static final NumberGenerator GENERATOR = new NumberGenerator();
+
+	public void moveCars(Cars cars) {
+		for(Car car : cars.getCars()) {
+			int randomNumber = GENERATOR.generate();
+			
+			moveCar(car, randomNumber);
+		}
+	}
+	
+	private void moveCar(Car car, int number) {
+		if(checkMoveCondition(number)) {
+			car.move();
+		}
+	}
+	
+	private boolean checkMoveCondition(int number) {
+		return number >= 4;
+	}
+}
