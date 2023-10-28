02238a3f47df19ba333cf95502fc8c4f50c4fc69
euniiiii <hee9920@naver.com>
2023-10-28 19:44:19 +0900

    feat(utils): 자동차 이름 입력 시 이름 중복을 확인하는 기능 추가

--- src/main/java/racingcar/utils/InputException.java
+++ src/main/java/racingcar/utils/InputException.java
@@ -2,6 +2,10 @@ package racingcar.utils;
 
 import racingcar.domain.PrintMessage;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputException {
 
     public static void isContainComma(String input) {
@@ -12,5 +16,11 @@ public class InputException {
     public static void validateNameSize(String name) {
         if (name.isEmpty() || name.length() > 5)
             throw new IllegalArgumentException(PrintMessage.FIVE_OR_LESS);
+    }
+
+    public static void validateNameDuplication(List<String> carList) {
+        Set<String> set = new HashSet<>(carList);
+        if (carList.size() != set.size())
+            throw new IllegalArgumentException(PrintMessage.NOT_DUPLICATE_NAME);
     }
 }
