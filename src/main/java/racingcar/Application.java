1dd6f642d2e0b406447987db53a3923a869b8e47
Jokyo0612 <jgh010612@gmail.com>
2023-10-28 15:55:00 +0900

    feat : 배열에서 최댓값을 고르는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -91,6 +91,17 @@ public class Application {
         System.out.print("\n");
     }
 
+    public static int max(int[] num) {
+        int max = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > max) {
+                max = num[i];
+            }
+        }
+        return max;
+    }
+
+
 
     public static void main(String[] args) {
 
