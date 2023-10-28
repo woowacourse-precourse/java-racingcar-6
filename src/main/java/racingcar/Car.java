743e7fb7c89289cad62c7233b313524b0c1284bc
dk7648 <dk7648@korea.ac.kr>
2023-10-28 10:59:11 +0900

    feat(Car): 생성자 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,11 @@ public class Car {
     private String name;
     private int distance;
 
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
     public boolean canMove() {
         return false;
     }
