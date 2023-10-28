75f0b4def0cbc455502eb2bb85429bedfeb8d6ee
BurningFalls <sherlocky31415@gmail.com>
2023-10-28 17:12:04 +0900

    style: 실행 결과 예시의 출력 문구 삽입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,13 +14,27 @@ public class Application {
     }
 
     public static void receiveRaceCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String raceCarNamesinput = Console.readLine();
         raceCarNames = new RaceCarNames(raceCarNamesinput);
     }
 
     public static void receiveTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String tryCountInput = Console.readLine();
         tryCount = new TryCount(tryCountInput);
+    }
+
+    public static void calculateWhoAreTheWinners() {
+        //
+    }
+
+    public static void printCurrentRacingResult() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printRaceWinners() {
+        System.out.println("최종 우승자 : ");
     }
 
     // test용 코드
