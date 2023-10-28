a90499b9ba97bcba8d54a335e75f1ee38ae713fa
sehee <tpgml701@naver.com>
2023-10-28 22:24:21 +0900

    feat: 자동차 관리 DTO 기능 구현

---
+++ src/main/java/racingcar/dto/CarsDto.java
@@ -0,0 +1,37 @@
+package racingcar.dto;
+
+
+
+import racingcar.model.Car;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CarsDto {
+    static Map<String, Car> cars = new HashMap();
+
+    public void enrollCar(String carName) {
+        if(findCarByName(carName).equals(Optional.empty())) {
+            cars.put(carName,new Car(carName,0));
+        }
+    }
+
+    public Optional<Integer> findPositionByName(String name) {
+        return Optional.ofNullable(cars.get(name).getPosition());
+    }
+
+    public Optional<Car> findCarByName(String name) {
+        return Optional.ofNullable(cars.get(name));
+    }
+
+    public Map<String, Integer> findAll() {
+        return cars.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().getPosition()));
+    }
+
+    public void increaseCarPosition(String carName) {
+        findCarByName(carName).ifPresent(car -> car.setPosition(car.getPosition() + 1));
+    }
+}
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,19 @@
 package racingcar.model;
 
 public class Car {
-    String carName;
-    int position;
+     private String carName;
+     private int position;
 
     public Car(String carName, int position) {
         this.carName = carName;
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
         this.position = position;
     }
 }
