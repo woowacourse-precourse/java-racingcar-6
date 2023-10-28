473578654453817d3f223187a55de12b14e6a97c
yuseonjun <seonjun0906@gmail.com>
2023-10-28 16:56:17 +0900

    test: Cars의 move 메서드는 랜덤한 숫자를 뽑아 결과에 따라 위치가 증가하거나 증가하지 않는다.

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,35 @@ class CarsTest {
         List<String> cars = List.of("pobi", "woni", "jun");
 
         assertThat(Cars.from(cars)).isInstanceOf(Cars.class);
+    }
+
+    @Test
+    void Cars의_move_메서드는_모든_자동차가_랜덤한_숫자를_뽑아_4이상이면_위치가_증가한다() {
+        NumberGenerator numberGenerator = new IncreasePositionNumber();
+
+        Car firstCar = new Car("pobi");
+        Car secondCar = new Car("woni");
+
+        Cars cars = new Cars(List.of(firstCar, secondCar));
+
+        cars.move(numberGenerator);
+
+        assertEquals(1, firstCar.getPosition());
+        assertEquals(1, secondCar.getPosition());
+    }
+
+    @Test
+    void Cars의_move_메서드는_모든_자동차가_랜덤한_숫자를_뽑아_4보다_작으면_위치가_증가하지_않는다() {
+        NumberGenerator numberGenerator = new NotIncreasePositionNumber();
+
+        Car firstCar = new Car("pobi");
+        Car secondCar = new Car("woni");
+
+        Cars cars = new Cars(List.of(firstCar, secondCar));
+
+        cars.move(numberGenerator);
+
+        assertEquals(0, firstCar.getPosition());
+        assertEquals(0, secondCar.getPosition());
     }
 }
