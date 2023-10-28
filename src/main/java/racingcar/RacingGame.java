cc7727c6a55f5f83c2eb2920fb01411de0e30479
mmihye <smilehye910@naver.com>
2023-10-28 14:38:00 +0900

    FEAT/사용자에게 자동차 입력받기

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,11 +2,18 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class RacingGame {
+    private ArrayList<Car> carList = new ArrayList<>();
 
     public void getCarsName(){
         System.out.println(GameMessage.REQUEST_ENTER_CAR_NAME_MESSAGE);
-
+        String carNames = Console.readLine();
+        String[] carListTest = carNames.split(",");
+        for(String car : carListTest){
+            carList.add(new Car(car));
+        }
     }
 }
 
