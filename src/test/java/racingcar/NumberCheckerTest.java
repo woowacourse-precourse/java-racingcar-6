521f66020a848bd531c8a97173cd04c8b1987a06
SeongHun <tjdgns5506@gmail.com>
2023-10-28 16:36:49 +0900

    test: 입력 받은 횟수가 입력 요구 사항에 맞는지 확인하는 기능 테스트

---
+++ src/test/java/racingcar/NumberCheckerTest.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NumberCheckerTest {
+    @Test
+    public void testValidNumber() {
+        String validNumber = "12345";
+        assertTrue(Application.checkNumber(validNumber));
+    }
+
+    @Test
+    public void testInvalidNumber() {
+        String invalidNumber = "abc123";
+        assertFalse(Application.checkNumber(invalidNumber));
+
+        invalidNumber = "";
+        assertFalse(Application.checkNumber(invalidNumber));
+
+        invalidNumber = "-1";
+        assertFalse(Application.checkNumber(invalidNumber));
+
+        invalidNumber = "h";
+        assertFalse(Application.checkNumber(invalidNumber));
+    }
+}
