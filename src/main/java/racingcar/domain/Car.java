2f2e9d783552b79c623678436e1a38a2cd0a6ea6
seokmyungham <hellotjraud@gmail.com>
2023-10-29 08:22:03 +0900

    feat(Car): moveForward 메서드 추가
    moveForward: 랜덤 숫자를 생성하고, 숫자가 4 이상인 경우 move를 1 증가시키는 메서드입니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private Name name;
     private Move move;
@@ -7,5 +9,13 @@ public class Car {
     public Car(Name name, Move move) {
         this.name = name;
         this.move = move;
+    }
+
+    public void moveForward() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+        if (randomNumber >= 4) {
+            move.increaseMove();
+        }
     }
 }
