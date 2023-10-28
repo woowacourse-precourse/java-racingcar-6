24d5d1aae0eaa31caf0f2238b62b08e789a158b6
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 19:20:45 +0900

    test: startRace 테스트  - 공통 숫자 클래스 상수화

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -1,17 +1,20 @@
 package racingcar.service;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 import racingcar.model.Cars;
+import racingcar.model.RandomNumberProvider;
 
 class RacingCarServiceTest {
-    private final RacingCarService racingCarService = new RacingCarService();
+    private final int racingTime = 5;
+    private final int moveForwardNumber = 5;
+    private RacingCarService racingCarService = new RacingCarService(() -> moveForwardNumber);
     private final List<String> carNameList = List.of("car", "benz", "audi", "fox");
+
     @Test
     void prepareCars_결과_이름_동일() {
         Cars cars = racingCarService.prepareCars(carNameList);
@@ -26,7 +29,42 @@ class RacingCarServiceTest {
     }
 
     @Test
-    void startRace() {
+    void startRace_전진() {
+
+        this.racingCarService = new RacingCarService(() -> moveForwardNumber);
+        Cars cars = racingCarService.prepareCars(carNameList);
+
+        Cars startedRace = racingCarService.startRace(cars, racingTime);
+        
+        Assertions.assertAll(
+            () -> assertThat(startedRace.getWinner()).isEqualTo(carNameList),
+            () -> assertThat(startedRace.getWinner().size()).isEqualTo(carNameList.size()),
+            () -> {
+                for (Car car : startedRace.getCars()) {
+                    assertThat(car.getProgress()).isEqualTo(racingTime);
+                }
+            }
+        );
+    }
+    @Test
+    void startRace_전진_못함() {
+        final int noMoveNumber = 2;
+        final int initialProgress = 0;
+
+        this.racingCarService = new RacingCarService(() -> noMoveNumber);
+        Cars cars = racingCarService.prepareCars(carNameList);
+
+        Cars startedRace = racingCarService.startRace(cars, racingTime);
+
+        Assertions.assertAll(
+            () -> assertThat(startedRace.getWinner()).isEqualTo(carNameList),
+            () -> assertThat(startedRace.getWinner().size()).isEqualTo(carNameList.size()),
+            () -> {
+                for (Car car : startedRace.getCars()) {
+                    assertThat(car.getProgress()).isEqualTo(initialProgress);
+                }
+            }
+        );
     }
 
     @Test
