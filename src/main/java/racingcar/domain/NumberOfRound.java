ec34348ebd6c338e83d5bc70a740637b23e14cba
waveofmymind <sjun0913@gmail.com>
2023-10-28 19:23:50 +0900

    refactor: 라운드를 계속 진행할 때, 반환값으로 새로운 객체를 생성한다.

--- src/main/java/racingcar/domain/NumberOfRound.java
+++ src/main/java/racingcar/domain/NumberOfRound.java
@@ -8,12 +8,16 @@ public class NumberOfRound {
         this.value = value;
     }
 
+    public int getValue() {
+        return value;
+    }
+
     public boolean hasRoundsLeft() {
         return value > 0;
     }
 
-    public void consumeRound() {
-        value--;
+    public NumberOfRound consumeRound() {
+        return new NumberOfRound(value - 1);
     }
 
     private void validate(int value) {
