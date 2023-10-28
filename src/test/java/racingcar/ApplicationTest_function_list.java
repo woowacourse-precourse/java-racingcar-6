87bc2d55ad05e8bf9e02b4890a691ecaadae2a5c
idingg <48832274+idingg@users.noreply.github.com>
2023-10-28 05:04:18 +0000

    test: 추가 기능목록_테스트_시작_문자열_출력
    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" 출력 테스트

---
+++ src/test/java/racingcar/ApplicationTest_function_list.java
@@ -0,0 +1,137 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.Assert;
+
+class ApplicationTest_function_list extends NsTest {
+
+    @Test
+    void 기능목록_테스트_시작_문자열_출력() {
+        assertSimpleTest(() -> {
+            run("pobi,woni,jun", "1");
+            assertThat(output().split("\n")[0]).startsWith("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        });
+    }
+
+    @Test
+    void 기능목록_테스트_자동차_이름_입력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_입력_문자열_분할() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_자동차_이름_입력_오류_처리() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_null_검사() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_빈_문자열_검사() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_1_5자_검사() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_자동차_이름_저장() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_경주() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_시도_횟수_입력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_시도_횟수_질문_문자열_출력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_시도_횟수_입력_오류_처리() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_숫자_자연수() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_시도_횟수만큼_전체_순환() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_전체_자동차_순환() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_한_자동차_진행() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_0_9_무작위_값_생성() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_4이상_인지_검사() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_값에_따라_자동차_전진_정지() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_현재_상태_출력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_경기_상황_출력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_우승자_목록을_형식에_맞춰_출력() {
+
+    }
+
+    @Test
+    void 기능목록_테스트_우승자_계산() {
+
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[] {});
+    }
+}
