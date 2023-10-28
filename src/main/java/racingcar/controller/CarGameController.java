8fedb03e721296388fae260e1d06968caee1f278
sehee <tpgml701@naver.com>
2023-10-29 04:18:32 +0900

    feat: 예외 기능 추가

--- src/main/java/racingcar/controller/CarGameController.java
+++ src/main/java/racingcar/controller/CarGameController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import racingcar.service.CarGameService;
 import racingcar.util.Parser;
+import racingcar.util.Validator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -12,22 +13,24 @@ public class CarGameController {
     InputView inputView;
     CarGameService carGameService;
     Parser parser;
+    Validator validator;
 
     public CarGameController() {
         this.outputView = new OutputView();
         this.inputView = new InputView();
         this.carGameService = new CarGameService();
         this.parser = new Parser();
+        this.validator = new Validator();
     }
 
     public void run() {
         outputView.carNameMessage();
         String userInput = inputView.getInput();
-
         List<String> participant = parser.parseUserInputToList(userInput);
 
         outputView.numberMessage();
         String numberInput = inputView.getInput();
+        validator.validParseInt(numberInput);
         int times = parser.ParseInt(numberInput);
 
         outputView.resultInfoMessage();
