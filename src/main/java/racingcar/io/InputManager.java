82c004dacc1bd6abb3a180a82e4f05b140a81b39
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 10:02:40 +0900

    feat(InputManager): 시도할 횟수 입력 관리 기능 작성

--- src/main/java/racingcar/io/InputManager.java
+++ src/main/java/racingcar/io/InputManager.java
@@ -17,4 +17,9 @@ public class InputManager {
         return inputMapper.toCars(carNames);
     }
 
+    public int inputAttemptCount() {
+        String attemptCount = inputView.inputAttemptCount();
+        return inputMapper.toInt(attemptCount);
+    }
+
 }
