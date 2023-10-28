8406b99e2ebe6825f755248af6b41743d89f23f8
formoin <alsrud869@gmail.com>
2023-10-29 04:17:24 +0900

    get car names from a user

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,35 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
+import static java.lang.System.exit;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        try {
+            String[] names = getCarNames();
+        }
+        catch (IllegalArgumentException e){
+            e.printStackTrace();
+            exit(0);
+        }
+    }
+
+    public static String[] getCarNames() throws IllegalArgumentException{
+        String name;
+
+        name = Console.readLine();
+        String[] nameslist = name.split(",");
+
+        for(int i=0; i<nameslist.length; i++){
+            if(nameslist[i].length() >5){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return nameslist;
     }
 }
