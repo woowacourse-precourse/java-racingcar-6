98830f5e39cc5a2dffc009937b62f552d755c454
Jiyoung Choi <12201817@inha.edu>
2023-10-28 15:55:32 +0900

    feat(CarRacing) : 자동차 경주 게임의 전반적인 흐름 구현

---
+++ src/main/java/racingcar/controller/CarRacing.java
@@ -0,0 +1,62 @@
+package racingcar.controller;
+
+import racingcar.InputValidator;
+import racingcar.model.RacingCars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class CarRacing {
+    private RacingCars racingCars;
+    private int count;
+
+    public void race() {
+        inputRacingCars();
+        inputCount();
+        playCountTimes();
+        showFinalWinners();
+    }
+
+    private void inputRacingCars() {
+        List<String> racingCarsName = inputRacingCarsName();
+        racingCars = new RacingCars(racingCarsName);
+    }
+
+    private List<String> inputRacingCarsName() {
+        return validateRacingCarsName(InputView.inputRacingCarsName());
+    }
+
+    private List<String> validateRacingCarsName(String inputRacingCarsName) {
+        return InputValidator.validateRacingCarsName(inputRacingCarsName);
+    }
+
+    private void inputCount() {
+        count = validateCount(InputView.inputCount());
+    }
+
+    private int validateCount(String inputCount) {
+        return InputValidator.validateCount(inputCount);
+    }
+
+    private void playCountTimes() {
+        showResultMessage();
+        for (int i = 0; i < count; ++i) {
+            racingCars.playForEachCount();
+            showResultForEachCount();
+        }
+    }
+
+    private void showResultMessage() {
+        OutputView.printResult();
+    }
+
+    private void showResultForEachCount() {
+        OutputView.printMove(racingCars.makeResultMap());
+    }
+
+    private void showFinalWinners() {
+        List<String> winners = racingCars.getWinners();
+        OutputView.printFinalWinners(winners);
+    }
+}
