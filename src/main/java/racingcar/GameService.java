d1899700f49af840be427001061267e71a56fb80
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-28 23:17:25 +0900

    feat : 자동차의 위치 최대값 찾는 기능, 최종 우승자 찾는 기능 추가

--- src/main/java/racingcar/GameService.java
+++ src/main/java/racingcar/GameService.java
@@ -2,7 +2,12 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameService {
+    private int carGameWinner = 0;
+
     public void move(Car car) {
         moveForward(car);
         StringBuilder moveCount = new StringBuilder();
@@ -15,6 +20,28 @@ public class GameService {
     public void moveForward(Car car) {
         if (isMoveForward()) {
             car.plusMoveForward();
+        }
+    }
+
+    public void getMaxPosition(List<Car> carInfos) {
+        for (Car carInfo : carInfos) {
+            findMaxPosition(carInfo);
+        }
+    }
+
+    public List<String> findGameWinner(List<Car> carInfos) {
+        List<String> gameWinners = new ArrayList<>();
+        for (Car carInfo : carInfos) {
+            if (carGameWinner == carInfo.getPosition()) {
+                gameWinners.add(carInfo.getCarName());
+            }
+        }
+        return gameWinners;
+    }
+
+    private void findMaxPosition(Car carInfo) {
+        if (carGameWinner < carInfo.getPosition()) {
+            carGameWinner = carInfo.getPosition();
         }
     }
 
