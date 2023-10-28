ab11be6592978681cc6d0f5441571ec7a399ca7e
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 16:43:07 +0900

    feat: Car 객체 data transfer object 생성

---
+++ src/main/java/racingcar/model/CarProgressResponse.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public final class CarProgressResponse {
+    private final String name;
+    private final int result;
+
+    public CarProgressResponse(String name, int result) {
+        this.name = name;
+        this.result = result;
+    }
+}
