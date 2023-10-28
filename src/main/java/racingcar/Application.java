181eda3a99a4718a875c9cd30da5b5ebc70180c8
SeongUk52 <rkfka7849@gmail.com>
2023-10-28 19:21:55 +0900

    refactor(Application): 미사용 지역 변수 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,6 @@ import racingcar.controller.RaceController;
 public class Application {
     public static void main(String[] args) {
         CarController carController = new CarController();
-        RaceController raceController = new RaceController(carController.getCarList());
+        new RaceController(carController.getCarList());
     }
 }
