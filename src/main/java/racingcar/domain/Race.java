fdee75847e7b6ba91aaa8457e64c10b81ab97403
lzlkolo <lzlko1209@gmail.com>
2023-10-29 07:55:16 +0900

    refactor: Remove test print statements

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -24,8 +24,6 @@ public class Race {
             int randomNumber = numberGenerator.createRandomNumber();
             randomNumbers.add(randomNumber);
         }
-        System.out.println(randomNumbers);
-
         return randomNumbers;
     }
 }
