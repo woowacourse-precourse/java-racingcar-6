fa57b23e491b7db834e518727325188c0b1ed24a
daeni-dang <de021@naver.com>
2023-10-28 19:31:12 +0900

    feat : 스트링 배열 List로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,13 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] cars = Console.readLine().split(",");
+        List<String> cars = Arrays.asList(Console.readLine().split(","));
         System.out.println("시도할 회수는 몇회인가요?");
         int num = Integer.parseInt(Console.readLine());
 
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,20 +1,25 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
 
-    private String[] cars;
-    private int num;
+    private final List<String> cars;
+    private final int num;
+    private Car[] car;
 
-    public Game(String[] cars, int num) {
+    public Game(List<String> cars, int num) {
         this.cars = cars;
         this.num = num;
     }
 
     // 차량 객체 이름 지정 및 이동 거리 0으로 초기화
     void initCar() {
-        Car[] car = new Car[this.num];
-        for (int i = 0; i < this.num; i++) {
-            car[i] = new Car(this.cars[i], 0);
+        car = new Car[cars.size()];
+        for (int i = 0; i < cars.size(); i++) {
+            car[i] = new Car(cars.get(i), 0);
         }
     }
 
