ac5bb572eea7f3a75aa9356b9042bb1c161270bf
JisooPyo <audwl326@gmail.com>
2023-10-29 04:22:24 +0900

    feat(game): 사용자에게 실행 결과를 보여준다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,17 +2,29 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class RacingGame {
 	CarNameValidator carNameValidator = new CarNameValidator();
+	Car car = new Car();
+	List<StringBuilder> steps = new ArrayList<>();
 
 	public RacingGame() {
-		String[] cars = inputcarName();
-		int tryCount = inputTryCount();
+		gameStart();
 	}
 
-	private String[] inputcarName() {
+	private void gameStart() {
+		String[] cars = inputCarName();
+		makeCarStepList(cars.length);
+		int tryCount = inputTryCount();
+		showTheResults(cars, tryCount);
+	}
+
+	private String[] inputCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String carNames = Console.readLine();
 		String[] carArray = carNames.split(",");
 
@@ -21,13 +33,31 @@ public class RacingGame {
 		return carArray;
 	}
 
-	private int inputTryCount() {
-		String tryCount = Console.readLine();
+	private void makeCarStepList(int length) {
+		for (int i = 0; i < length; i++) {
+			steps.add(new StringBuilder());
+		}
+	}
 
+	private int inputTryCount() {
+		System.out.println("시도할 횟수는 몇 회인가요?");
+		String tryCount = Console.readLine();
+		System.out.println();
 		if (!Pattern.matches("^[0-9]+$", tryCount)) {
 			throw new IllegalArgumentException("숫자만 입력하실 수 있습니다.");
 		}
 
 		return Integer.parseInt(tryCount);
+	}
+
+	private void showTheResults(String[] cars, int tryCount) {
+		for (int i = 0; i < tryCount; i++) {
+			steps = car.moveForward(steps);
+
+			for (int j = 0; j < cars.length; j++) {
+				System.out.println(cars[j] + " : " + steps.get(j));
+			}
+			System.out.println();
+		}
 	}
 }
