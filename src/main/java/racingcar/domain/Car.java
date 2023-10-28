100e278029c1ed784fea2a3d5ffa0ab78b7dec8a
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-28 23:01:30 +0900

    FEAT / create Car

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int moving;
+
+    public Car(String name) {
+        this.name = name;
+        this.moving = 0;
+    }
+}
