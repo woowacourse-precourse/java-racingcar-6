554bc53c425c6d5d9c1918818009d2630c0b63b8
GumaLee <rkdxhd97@naver.com>
2023-10-28 21:47:07 +0900

    Refactor: 입력 검증 기능

--- src/main/java/racingcar/view/InputVerification.java
+++ src/main/java/racingcar/view/InputVerification.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public class InputVerification {
 
-    public boolean verifyCarNamesInput(List<String> cars) {
-        for (String car : cars) {
-            if (car.length() == 0 || car.length() > 5) {
-                throw new IllegalArgumentException("옳지 않은 입력 길이입니다.");
-            }
+    public boolean verifyCarNamesInput(String car) {
+        if (car.length() == 0 || car.length() > 5) {
+            throw new IllegalArgumentException("옳지 않은 입력 길이입니다.");
         }
         return true;
     }
