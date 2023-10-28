c84185537d87a9da3eb8ed55dc355b2f03734f13
dlrjs2360 <lhg3670@naver.com>
2023-10-28 23:21:11 +0900

    :sparkles: feat: 단순 입력 클래스 구현
    - 사용자의 단순한 입력을 받는 InputView 클래스를 생성했습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,11 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.constants.Messages;
 
 public class InputView {
+    public static String inputCarNames() {
+        System.out.println(Messages.INPUT_CAR_NAMES.getMessage());
+        return Console.readLine();
+    }
 }
