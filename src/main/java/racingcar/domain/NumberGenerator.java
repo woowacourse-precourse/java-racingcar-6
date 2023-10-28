1376a22f880d35a97b9c3b1342233ce91a45cce8
jinsu868 <mjinsu098@gmail.com>
2023-10-28 20:25:36 +0900

    refactor(number_generator): 상수값 지정, MIN_NUMBER, MAX_NUMBER

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -3,8 +3,11 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     public static int createRandomNumber() {
-        int createdNumber = Randoms.pickNumberInRange(0, 9);
+        int createdNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
         return createdNumber;
     }
 }
