ae6015fdb9057f3a401e94effbd5bb233a486772
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-28 19:43:44 +0900

    feat: add a feature to throw illegalargumentexception if lap input is not a number

--- src/main/java/racingcar/Lap.java
+++ src/main/java/racingcar/Lap.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.function.Predicate;
+
 public class Lap implements Showable, Gettable {
     private static final String lapInstruction = "시도할 회수는 몇회인가요?";
 
@@ -14,7 +16,22 @@ public class Lap implements Showable, Gettable {
 
     @Override
     public String getInput() {
-        return lap = readLine();
+        lap = readLine();
+
+        Predicate<String> isNumeric = str -> {
+            try {
+                Double.parseDouble(str);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+
+        if (!isNumeric.test(lap)) {
+            throw new IllegalArgumentException();
+        }
+
+        return lap;
     }
 
     public String getLap() {
