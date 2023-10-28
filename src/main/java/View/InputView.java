53280879e5a4217a57e11deb1e26c8357cffa72a
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:39:57 +0900

    refactor : 불필요한 import 제거

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,6 +1,5 @@
 package View;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String inputCarName = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
     private static final String inputNumOfAttempt = "시도할 회수는 몇회인가요?";
