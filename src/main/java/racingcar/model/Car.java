3d7000c0898305994b01dca594c521b30cf965ab
박성춘 <songchon100@gmail.com>
2023-10-28 13:20:48 +0900

    feat : 0~9사이의 난수를 가져오는 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,18 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private CarInform carInform;
+    private Car(String carName) {
+        carInform = CarInform.getInstance(carName);
+    }
+
+    public static Car getInstance(String carName) {
+        return new Car(carName);
+    }
+
+    public void getRandNum() {
+        int randNum = Randoms.pickNumberInRange(0, 9);
+    }
 }
