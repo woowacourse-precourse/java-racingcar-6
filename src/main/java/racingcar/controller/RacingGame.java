26453e417edc72d7626aaaac3510d71fc5b433f6
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-10-28 21:29:13 +0900

    feat: 입력받은 car 객체를 생성 후 carList에 추가 구현

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,14 +1,27 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.view.CarName;
 import racingcar.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private List<Car> carList = new ArrayList<>();
     OutputView outputView = new OutputView();
     CarName carName = new CarName();
     public void play() {
         outputView.racingStart();
         String[] carNames = carName.input().split(",");
+        init(carNames);
         outputView.rotateCount();
+    }
+
+    private void init(String[] carNames){
+        for(int index=0; index<carNames.length; index++){
+            Car car = new Car(carNames[index],"");
+            carList.add(car);
+        }
     }
 }
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,28 @@
 package racingcar.model;
 
+import racingcar.controller.RacingGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
+    private String name;
+    private String move;
+
+    public Car(String name, String move) {
+        this.name = name;
+        this.move = move;
+    }
+
+    public void setMove(String move) {
+        this.move = move;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMove() {
+        return move;
+    }
 }
