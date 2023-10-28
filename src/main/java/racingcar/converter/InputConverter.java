8b8e18daf154061bb8d5f1d2e424d26bd95466fc
minisyu <iswater.kim@gmail.com>
2023-10-28 15:48:54 +0900

    refactor(InputConverter): convertStringToCars 메소드 변경

--- src/main/java/racingcar/converter/InputConverter.java
+++ src/main/java/racingcar/converter/InputConverter.java
@@ -3,13 +3,16 @@ package racingcar.converter;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.game.Car;
+import racingcar.domain.game.CarName;
 import racingcar.domain.game.Cars;
 
 public class InputConverter {
     public Cars convertStringToCars(String input) {
         List<Car> cars = Arrays.stream(input.split(","))
+                .map(CarName::from)
                 .map(Car::of)
                 .toList();
+
         return Cars.of(cars);
     }
 
