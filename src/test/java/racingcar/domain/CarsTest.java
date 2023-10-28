15d250b5f970e67c2f03f1f15da4f83f32cb3156
leeedohyun <ldh19738@gmail.com>
2023-10-28 17:22:05 +0900

    test: 모든 자동차 움직이는 테스트 제거

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,52 +3,8 @@ package racingcar.domain;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarsTest {
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    public void 모든_자동차_움직이지_않는_기능_테스트(int randomNumber) {
-        // given
-        final Cars cars = new Cars(List.of(new Car("pobi"), new Car("woni"), new Car("jun")));
-
-        // when
-        List<String> locationInformation = cars.moveCars(randomNumber);
-
-        // then
-        Assertions.assertThat(locationInformation).hasSize(3)
-                .contains("pobi : ", "woni : ", "jun : ");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    public void 모든_자동차_움직이는_기능_테스트(int randomNumber) {
-        // given
-        final Cars cars = new Cars(List.of(new Car("pobi"), new Car("woni"), new Car("jun")));
-
-        // when
-        List<String> locationInformation = cars.moveCars(randomNumber);
-
-        // then
-        Assertions.assertThat(locationInformation).hasSize(3)
-                .contains("pobi : -", "woni : -", "jun : -");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    public void 우승자_테스트(int randomNumber) {
-        // given
-        final Cars cars = new Cars(List.of(new Car("pobi"), new Car("woni")));
-
-        // when
-        cars.moveCars(randomNumber);
-        String winnerCars = cars.findWinnerCars();
-
-        // then
-        Assertions.assertThat(winnerCars).isEqualTo("pobi, woni");
-    }
 
     @Test
     public void 이름_중복_테스트() {
