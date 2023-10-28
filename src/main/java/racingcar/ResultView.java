52baf8e9281f893389488bca3229e8ba40eb5f72
박찬건 <jg9870@naver.com>
2023-10-28 21:18:24 +0900

    refactor(racingcar): 시도할 회수 안내 문구 함수 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -23,4 +23,8 @@ public class ResultView {
     public static void printEnterNameGuide() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public static void printRoundQuestion() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
