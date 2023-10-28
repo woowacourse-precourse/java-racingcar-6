d11242441c0a53153f507f6e8c071c9dd496d1bc
acycoco <pink10257@naver.com>
2023-10-29 05:03:59 +0900

    feat(WinnerPicker): 우승자를 고르는 기능 추가

--- src/main/java/racingcar/domain/WinnerPicker.java
+++ src/main/java/racingcar/domain/WinnerPicker.java
@@ -1,4 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WinnerPicker {
+
+    public List<String> pickWinner(RacingCars racingCars) {
+        List<String> winnersName = new ArrayList<>();
+        int winnerDistance = racingCars.getMaxForwardDistance();
+
+        racingCars.getRacingCars().stream()
+                .filter(racingCar -> racingCar.getForwardDistance() == winnerDistance)
+                .map(RacingCar::getName)
+                .forEach(winnersName::add);
+
+        return winnersName;
+    }
 }
