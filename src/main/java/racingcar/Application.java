2bba0968a839d0739a9003a64dd166252fc42d82
le2sky <leehaneul990623@gmail.com>
2023-10-29 01:39:55 +0900

    refactor : 자동차 이동 정책 생성 로직 메서드로 추출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,17 @@
 package racingcar;
 
+import racingcar.entity.MovePolicy;
 import racingcar.entity.policy.RandomMovePolicy;
 
 public class Application {
 
     public static void main(String[] args) {
-        Controller controller = new Controller(new RandomMovePolicy());
+        Controller controller = new Controller(movePolicy());
 
         controller.run();
+    }
+
+    private static MovePolicy movePolicy() {
+        return new RandomMovePolicy();
     }
 }
