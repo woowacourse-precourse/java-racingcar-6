6bead32d1f2f27f529d41654932213f442dfb1c1
anneu <b.imnuj@gmail.com>
2023-10-28 18:30:55 +0900

    refactor: RacingCars 객체로 경주 역할 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,6 +1,5 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.domain.RacingCars;
 import racingcar.view.Input;
@@ -10,9 +9,6 @@ import java.util.List;
 
 public class RacingController {
 
-    private static final int RANDOM_NUMBER_START = 0;
-    private static final int RANDOM_NUMBER_END = 9;
-    private static final int MIN_NUMBER = 4;
     private static final String NUMBER_FORMAT_EXCEPTION = "숫자를 입력해야 합니다.";
 
     private final Output output = new Output();
@@ -28,12 +24,7 @@ public class RacingController {
 
         output.showResultMessage();
         for (int i = 0; i < count; i++) {
-            for (Car car : racingCars.getCars()) {
-                if (canMove()) {
-                    car.move();
-                }
-            }
-            output.showResult(racingCars.getCars());
+            output.showResult(racingCars.race());
         }
 
         output.showWinners(findWinners(racingCars.getCars()));
@@ -45,14 +36,6 @@ public class RacingController {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION);
         }
-    }
-
-    private boolean canMove() {
-        return MIN_NUMBER <= makeRandomNumber();
-    }
-
-    private int makeRandomNumber() {
-        return Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END);
     }
 
     private List<Car> findWinners(List<Car> cars) {
--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,11 +1,16 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class RacingCars {
 
     private static final String CAR_NAME_DELIMITER = ",";
+    private static final int MIN_NUMBER = 4;
+    private static final int RANDOM_NUMBER_START = 0;
+    private static final int RANDOM_NUMBER_END = 9;
 
     private final List<Car> cars;
 
@@ -18,6 +23,23 @@ public class RacingCars {
                 .map(Car::new)
                 .toList();
         return new RacingCars(cars);
+    }
+
+    public List<Car> race() {
+        for (Car car : cars) {
+            if (canMove()) {
+                car.move();
+            }
+        }
+        return cars;
+    }
+
+    private boolean canMove() {
+        return MIN_NUMBER <= makeRandomNumber();
+    }
+
+    private int makeRandomNumber() {
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END);
     }
 
     public List<Car> getCars() {
