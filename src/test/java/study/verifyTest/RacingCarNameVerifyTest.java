6f2f2be000185d858b3543d1ad4214fa37a78434
junyoun99 <juny2486@naver.com>
2023-10-28 19:28:04 +0900

    feat: 자동차 이름 입력이 조건에 부합하는지 검증하는 RacingCarNameVerify 클래스와 이를 테스트하는 RacingCarNameVerifyTest 추가

---
+++ src/test/java/study/verifyTest/RacingCarNameVerifyTest.java
@@ -0,0 +1,21 @@
+package study.verifyTest;
+
+import java.io.ByteArrayInputStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.service.RacingCarNameScanService;
+import racingcar.verify.RacingCarNameVerify;
+
+public class RacingCarNameVerifyTest {
+
+    @Test
+    void verifyTest() {
+        RacingCarNameScanService racingCarNameScanService = new RacingCarNameScanService();
+        String example = "car1,car2,car3";
+        System.setIn(new ByteArrayInputStream(example.getBytes()));
+        racingCarNameScanService.readRacingCarNames();
+
+        RacingCarNameVerify racingCarNameVerify = new RacingCarNameVerify(racingCarNameScanService);
+        Assertions.assertThat(racingCarNameVerify.verify()).isEqualTo(true);
+    }
+}
