c759dfb95ee3f27b214d54aec42478cc8c6894fc
voyager003 <miraclex000@gmail.com>
2023-10-28 16:05:57 +0900

    refactor: isCarMoved를 static 메서드로 변경

--- src/main/java/racingcar/RacingGameExecutor.java
+++ src/main/java/racingcar/RacingGameExecutor.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingGameExecutor {
 
-    public boolean isCarMoved() {
+    public static boolean isCarMoved() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         return randomNumber >= 4;
     }
