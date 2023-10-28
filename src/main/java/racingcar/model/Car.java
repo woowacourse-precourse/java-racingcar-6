5665a67874765c9cff5b4ee7c6a7da4eb31892a6
Haeun Jeong <gkdms4248@gmail.com>
2023-10-28 16:29:27 +0900

    feat(model): Car 이름 1-5자 유효성 검사 기능 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,9 +4,21 @@ public class Car {
     private String carName;
     private int position;
 
+    private final static String NOT_VALID_CAR_NAME = "자동차 이름의 길이가 1-5자 사이가 아닙니다.";
+    private final static int VALID_MINIMUM_NUMBER = 1;
+    private final static int VALID_MAXIMUM_NUMBER = 5;
+
+
     public Car(String carName) {
+        validateCarName(carName);
         this.carName = carName;
         this.position = 0;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() < VALID_MINIMUM_NUMBER || carName.length() > VALID_MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException(NOT_VALID_CAR_NAME);
+        }
     }
 
     public int getPosition() {
