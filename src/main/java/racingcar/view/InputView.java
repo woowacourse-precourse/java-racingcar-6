e16bfc3c66da60037ff0029698d863f9f4cf9364
sangchu <didi1484@gmail.com>
2023-10-28 23:02:22 +0900

    feat : 자동차의 이름을 입력받는 askCarName 메소드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.view.constants.Notice.ASK_ATTEMPT_NUMBER;
+import static racingcar.view.constants.Notice.ASK_CAR_NAME;
+
 public class InputView {
+    public static String askCarName(){
+        System.out.println(ASK_CAR_NAME);
+        return readLine();
+    }
 }
