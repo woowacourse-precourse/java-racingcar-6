37751c385fe8206f403eb71fcb7c3f3380a35a42
Jinsu Kim <akrn112@naver.com>
2023-10-28 19:35:12 +0900

    feat: 자동차 이름 입력 기능 추가

---
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -0,0 +1,23 @@
+package racingcar.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Model.Car;
+import racingcar.View.ConsolePrint;
+
+public class RaceController {
+
+    private List<Car> carList = new ArrayList<Car>();
+
+    public void start() {
+        String[] names = ConsolePrint.readCars().split(",");
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+        System.out.println(carList);
+
+
+    }
+
+
+}
---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,28 @@
+package racingcar.Model;
+
+public class Car {
+    private String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void incrementDistance() {
+        this.distance++;
+    }
+
+
+}
