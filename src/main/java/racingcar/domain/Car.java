f49c9b75d5a7434f4df3610bced2fcbc890408f6
Seol_JY <wlsdud5654@gmail.com>
2023-10-28 19:31:56 +0900

    feat(domain): Car 객체 초안 작성
    레이싱 주체가 되는 Car 클래스 작성
    - Name 객체를 가짐
    - step은 이동 거리
    - Comparable를 implements

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,49 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Car implements Comparable<Car> {
+    private static final int INITIAL_STEP = 0;
+
+    private final Name name;
+    private int step;
+
+    private Car(Name name) {
+        this.name = name;
+        this.step = INITIAL_STEP;
+    }
+
+    public static Car from(Name name) {
+        return new Car(name);
+    }
+
+    public void moveForward() {
+        step++;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return Integer.compare(this.step, car.step);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return step == car.step;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(step);
+    }
+}
