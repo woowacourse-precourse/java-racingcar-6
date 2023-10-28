1236d864743eea02534b37448272ecd5de81a77b
daeni-dang <de021@naver.com>
2023-10-28 18:36:11 +0900

    feat : 차량 객체 생성 및 초기화

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -10,7 +10,15 @@ public class Game {
         this.num = num;
     }
 
-    void startGame() {
+    // 차량 객체 이름 지정 및 이동 거리 0으로 초기화
+    void initCar() {
+        Car[] car = new Car[this.num];
+        for (int i = 0; i < this.num; i++) {
+            car[i] = new Car(this.cars[i], 0);
+        }
+    }
 
+    void startGame() {
+        initCar();
     }
 }
