542d7379a4c7c2ef6dd098cae4503529b6845d1b
SeongIl <biuld1234@gmail.com>
2023-10-28 23:40:14 +0900

    style: 주석 제거 - 주석을 감싼 채, 커밋되어 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -13,13 +13,12 @@ public class Application {
         // 1. 자동차 이름 입력
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        String[] userInput = Console.readLine().split(",");
+        String[] userInput = Console.readLine().split(","); //수정 필요
         //String[] carNames = userInput.split(","); //배열대신 컬렉션 변경하기
         List<String> carNames = new ArrayList<>();
 
         for (int i=0; i< userInput.length; i++) {
             carNames.add(userInput[i]);
-
         }
 
         for (String carName : carNames) {
@@ -43,7 +42,7 @@ public class Application {
             }
         }
 
-        // 이름 중복시 => 예외처리
+        // 이름 중복시 => 예외 처리
 
         if (carNames.size() > carNames.stream().distinct().toList().size()) {
             throw new IllegalArgumentException("중복되지 않은 이름을 입력해 주세요.");
@@ -58,69 +57,69 @@ public class Application {
             System.out.print("\"" + carName + "\"");
         }
 
-//        // 2. 시도 횟수 입력
-//        System.out.println("시도할 회수는 몇회인가요?");
-//        userInput = Console.readLine();
-//        int roundCount;
-//
-//        try {
-//            roundCount = Integer.parseInt(userInput);
-//        } catch (NumberFormatException e) {
-//            throw new IllegalArgumentException("숫자만 입력하세요.");
-//        }
-//
-//        if (roundCount <= 0) {
-//            throw new IllegalArgumentException("1이상의 숫자를 입력하세요.");
-//        }
-//
-//        // 3. 이동 로직 구현
-//        System.out.println("실행 결과");
-//
-//        String[] movedCount = new String[carNames.length]; // 배열 대신 컬렉션으로 교체 하기, 초기화 방법고려
-//
-//        for (int i = 0; i < movedCount.length; i++) {
-//            movedCount[i] = "";
-//        }
-//
-//        for (int i = 0; i < roundCount; i++) {
-//
-//            for (int j = 0; j < carNames.length; j++) {
-//                // 3도 상수로 변환하기
-//                if (Randoms.pickNumberInRange(0, 9) >= 4) {
-//                    movedCount[j] += "-";
-//                }
-//            }
-//
-//            for (int k = 0; k < carNames.length; k++) {
-//                System.out.println(carNames[k] + " : " + movedCount[k]);
-//            }
-//
-//            System.out.println();
-//        }
-//
-//        // 4. 우승자 출력
-//        int max = 0;
-//
-//        for (int i = 0; i < carNames.length; i++) {
-//            if (max <= movedCount[i].length()) {
-//                max = movedCount[i].length();
-//            }
-//        }
-//
-//        System.out.print("최종 우승자 : ");
-//        int count = 0;
-//
-//        for (int i = 0; i < carNames.length; i++) {
-//
-//            if (max == movedCount[i].length()) {
-//
-//                if (count++ != 0) {
-//                    System.out.print(", ");
-//                }
-//
-//                System.out.print(carNames[i]);
-//            }
-//        }
+        // 2. 시도 횟수 입력
+        System.out.println("시도할 회수는 몇회인가요?");
+        userInput = Console.readLine();
+        int roundCount;
+
+        try {
+            roundCount = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력하세요.");
+        }
+
+        if (roundCount <= 0) {
+            throw new IllegalArgumentException("1이상의 숫자를 입력하세요.");
+        }
+
+        // 3. 이동 로직 구현
+        System.out.println("실행 결과");
+
+        String[] movedCount = new String[carNames.length]; // 배열 대신 컬렉션으로 교체 하기, 초기화 방법고려
+
+        for (int i = 0; i < movedCount.length; i++) {
+            movedCount[i] = "";
+        }
+
+        for (int i = 0; i < roundCount; i++) {
+
+            for (int j = 0; j < carNames.length; j++) {
+                // 3도 상수로 변환하기
+                if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                    movedCount[j] += "-";
+                }
+            }
+
+            for (int k = 0; k < carNames.length; k++) {
+                System.out.println(carNames[k] + " : " + movedCount[k]);
+            }
+
+            System.out.println();
+        }
+
+        // 4. 우승자 출력
+        int max = 0;
+
+        for (int i = 0; i < carNames.length; i++) {
+            if (max <= movedCount[i].length()) {
+                max = movedCount[i].length();
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        int count = 0;
+
+        for (int i = 0; i < carNames.length; i++) {
+
+            if (max == movedCount[i].length()) {
+
+                if (count++ != 0) {
+                    System.out.print(", ");
+                }
+
+                System.out.print(carNames[i]);
+            }
+        }
 
     }
 }
