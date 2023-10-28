7591b0befe07876a3a2edc392fcb70879b4fdd24
Jiyun <alex68566856@gmail.com>
2023-10-28 17:12:10 +0900

    feat: Cars 클래스를 기반으로 랜덤하게 전진/ 우승자 계산 기능 추가

---
+++ src/main/java/racingcar/model/CarRace.java
@@ -0,0 +1,41 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.OptionalInt;
+
+public class CarRace {
+    private final static int MIN_NUMBER = 1;
+    private final static int MAX_NUMBER = 9;
+
+    public CarRace() {}
+
+    public void playOneTurn(Cars cars) {
+        List<Car> raceCars = cars.getCars();
+        raceCars.forEach(this::forwardRandomStep);
+    }
+
+    public void forwardRandomStep(Car car) {
+        int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
+        if (randomNumber >= 4) {
+            int currentStep = car.getForwardSteps();
+            car.setForwardSteps(currentStep+1);
+        }
+    }
+
+    public List<String> getRaceWinner(Cars cars) {
+        List<Car> raceCars = cars.getCars();
+
+        int winnerSteps = raceCars.stream()
+                .mapToInt(Car::getForwardSteps)
+                .max()
+                .orElse(0);
+
+        List<String> winnersName = raceCars.stream()
+                .filter(car -> car.getForwardSteps()==winnerSteps)
+                .map(Car::getCarName)
+                .toList();
+        return winnersName;
+    }
+}
