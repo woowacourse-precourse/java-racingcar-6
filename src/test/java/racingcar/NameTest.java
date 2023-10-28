6d77d610cc28c491bd27891eeabfe5dedf142f91
dukcode <duk9741@gmail.com>
2023-10-28 19:06:12 +0900

    test: Name 생성 시 5글자 넘으면 예외 발생 테스트 추가

---
+++ src/test/java/racingcar/NameTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+class NameTest {
+
+    @Test
+    public void Name_생성_시_이름이_다섯_글자가_넘으면_예외를_발생시킨다() throws Exception {
+        // given
+        String name = "moreThan5";
+
+        // when
+        //then
+        Assertions.assertThatThrownBy(() -> {
+            new Name(name);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
