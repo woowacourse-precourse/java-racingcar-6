f26dd83486095f4d7867b66990a762677869bb70
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:52:43 +0900

    refactor : InputView 에서 데이터를 입력 받는 기능 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,13 +1,19 @@
 package View;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
     private static final String inputCarName = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
     private static final String inputNumOfAttempt = "시도할 회수는 몇회인가요?";
-    public static void printUserInputCarNameMessage(){
+    public static String printUserInputCarNameMessage(){
         System.out.println(inputCarName);
+        String canName = readLine();
+        return canName;
     }
 
-    public static void printUserInputNumOfAttemptMessage(){
+    public static Integer printUserInputNumOfAttemptMessage(){
         System.out.println(inputCarName);
+        Integer numOfAttempt = Integer.parseInt(readLine());
+        return numOfAttempt;
     }
 }
