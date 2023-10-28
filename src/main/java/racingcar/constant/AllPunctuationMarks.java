2bac18627e0c718b8ae143445cfe8cbfe8ba5118
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 15:25:02 +0900

    refactor : 문장부호 관련 상수 클래스 추가

---
+++ src/main/java/racingcar/constant/AllPunctuationMarks.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class AllPunctuationMarks {
+    public static final String COMMA = ",";
+    public static final String COLONS = " : ";
+    public static final String PROGRESS = "-";
+    public static final String BLANK = " ";
+}
