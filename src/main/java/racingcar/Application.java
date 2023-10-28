19b7eaa386daf06de8ca437a8368703df472ce29
sejung2 <twindrop@naver.com>
2023-10-28 16:16:15 +0900

    기능목록추가 및 전진여부파악기능구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,14 @@ package racingcar;
 
 import racingcar.domain.GenerateRandomNum;
 import racingcar.domain.InputCarName;
+import racingcar.domain.StopOrGo;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//        StopOrGo go = new StopOrGo();
 //        GenerateRandomNum randomNum = new GenerateRandomNum();
-//        System.out.println(randomNum.createRandomNum());
+//        System.out.println(go.move(randomNum.createRandomNum()));
 //        InputCarName carName = new InputCarName();
 //        System.out.println(carName.createCar());
 
--- src/main/java/racingcar/domain/StopOrGo.java
+++ src/main/java/racingcar/domain/StopOrGo.java
@@ -1,4 +1,10 @@
 package racingcar.domain;
 
 public class StopOrGo {
+    public boolean move(int number) {
+        if (number > 4) {
+            return true;
+        }
+        return false;
+    }
 }
