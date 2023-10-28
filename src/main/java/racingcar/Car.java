d797f625cf1a6dd8ab8844fa68166f520b1763d8
Jaewon <choijawon99@gmail.com>
2023-10-29 00:08:58 +0900

    [FEAT] Car.java 기본 변수, 메서드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,23 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name){
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int distance){
+        position += distance;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
