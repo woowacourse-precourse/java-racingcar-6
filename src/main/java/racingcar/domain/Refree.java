65e4303bfe7b1cd83131083d2ef0cbe58013d5ee
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 02:14:25 +0900

    feat(Refree): simulate에 대한 코드 추가

--- src/main/java/racingcar/domain/Refree.java
+++ src/main/java/racingcar/domain/Refree.java
@@ -1,8 +1,19 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Refree {
 
-    public static void simulate() {
+    public static void simulate(RacingCars racingcars) {
 
+        for (int i = 0; i < racingcars.size(); i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            racingcars.get(i).move();
+        }
+
+    }
+
+    public static RacingCars calculateWinner(RacingCars racingcars) {
+        return new RacingCars();
     }
 }
