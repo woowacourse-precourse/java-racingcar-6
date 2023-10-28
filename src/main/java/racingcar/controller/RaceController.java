ee8824d89a5c3b578201239eef7d3efc19d1a37a
msung99 <msung6924@naver.com>
2023-10-28 15:57:19 +0900

    feat: TryCount 도메인 개발

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -8,6 +8,5 @@ public class RaceController {
 
     public void runGame() {
         CarNames carNames = CarNames.from(inputView.readCarNames());
-
     }
 }
--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -1,4 +1,29 @@
 package racingcar.domain;
 
 public class TryCount {
+    private final int tryCount;
+    private static final String NUMBER_FORMAT_ERROR_MESSAGE = "올바를 숫자 형식을 입력해야합니다.";
+    private static final String NUMBER_PARSE_ERROR_MESSAGE = "시도 횟수는 최소 1이상으로 입력해야합니다.";
+    private static final int MINIMUM_TRY_COUNT = 1;
+
+    public TryCount(String tryCount){
+        validateParse(tryCount);
+        int parsedCount = Integer.parseInt(tryCount);
+        validateRange(parsedCount);
+        this.tryCount = parsedCount;
+    }
+
+    private void validateParse(String tryCount){
+        try{
+            Integer.parseInt(tryCount);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateRange(int tryCount){
+        if(tryCount < MINIMUM_TRY_COUNT){
+            throw new IllegalArgumentException(NUMBER_PARSE_ERROR_MESSAGE);
+        }
+    }
 }
