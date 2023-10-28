ee3057600ff98b3fe6f253645fde8328a69ee91f
sehee <tpgml701@naver.com>
2023-10-29 02:48:59 +0900

    feat: Paser 이름 Parser 수정

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Paser {
+public class Parser {
     public int ParseInt(String input) {
         return Integer.parseInt(input);
     }
