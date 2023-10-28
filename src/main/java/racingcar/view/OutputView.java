ed56a9adc1cf60d1e90452acd5f9c96ae5acec10
Mr.Kim <joajy98@gmail.com>
2023-10-29 01:52:10 +0900

    feat: 실행별 결과 출력 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,10 +2,20 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String EXECUTE_RESULT = "실행 결과";
     private static final String WINNER_LIST = "최종 우승자 : ";
+
+    public static void printResult(List<Car> carList){
+        System.out.println(EXECUTE_RESULT);
+        for (Car car : carList) {
+            printCurrentDistance(car);
+        }
+        System.out.println();
+    }
 
     public static void printCurrentDistance(Car car){
         System.out.print(car.getCarName() + " : ");
@@ -15,4 +25,5 @@ public class OutputView {
         }
         System.out.println();
     }
+
 }
