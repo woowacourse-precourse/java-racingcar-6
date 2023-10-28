f6d723a21c357c7b378e910d23a4f9042f710cb3
inpink <inpink@kakao.com>
2023-10-28 14:18:36 +0900

    test: TDD(테스트 주도 개발). MemoryCarRepositoryTest의 새로운멤버저장 테스트 코드 작성
    실제로 구현하기 앞서 테스트 코드를 먼서 작성함.
    현재는 컴파일 에러가 뜨고 있음.
    이 테스트 코드가 통과될 수 있도록 코드를 구현할 예정.

---
+++ src/test/java/racingcar/repository/MemoryCarRepositoryTest.java
@@ -0,0 +1,28 @@
+package racingcar.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+public class MemoryCarRepositoryTest {
+
+    private final CarRepository memoryCarRepository = new MemoryCarRepository();
+
+    @Test
+    public void 새로운멤버저장() {
+        //given
+        String name = "Car1";
+        final Car car = Car.create(name);
+
+        //when
+        final Car result = memoryCarRepository.save(car);
+
+        //then
+        assertThat(result.getId()).isNotNull(); //TODO: ID는 save() 로직 내부에서 자동으로 할당해줌
+        assertThat(result.getName()).isEqualTo("car1");
+    }
+
+    //TODO: ID테스트_1번부터 1씩 커지는 순서로 생성되는지(멤버 2개 만들어서 id값 확인)
+    //TODO: get~~(getPosition)같은것을 쓸경우 이거를 위한 테스트 따로하기(위치 이동시킨다음  위치값 저장 잘되는지)
+}
