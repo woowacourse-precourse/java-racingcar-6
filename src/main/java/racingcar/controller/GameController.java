b9179da0b2bdc8df35624f5b2c939979a46b398a
song windows <dkclasltmf@naver.com>
2023-10-28 14:20:42 +0900

    refactor: CarService 관련 의존성 제거
    CarService가 삭제됨에따라 관련 의존성을 삭제함

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,7 +2,6 @@ package racingcar.controller;
 
 import racingcar.handler.InputHandler;
 import racingcar.model.Cars;
-import racingcar.service.CarService;
 import racingcar.view.GameView;
 
 import java.util.List;
@@ -10,21 +9,20 @@ import java.util.List;
 public class GameController {
     private final GameView gameView;
     private final InputHandler inputHandler;
-    private final CarService carService;
 
-    public GameController(GameView gameView, InputHandler inputHandler, CarService carService) {
+    public GameController(GameView gameView, InputHandler inputHandler) {
         this.gameView = gameView;
         this.inputHandler = inputHandler;
-        this.carService = carService;
     }
 
     public void startGame() {
         String carNames = gameView.inputCarNames();
         List<String> nameList = inputHandler.convertNamesToNameList(carNames);
-        Cars cars = carService.createCars(nameList);
 
         String inputAttempts = gameView.inputAttempts();
         int attempts = inputHandler.convertAttemptsToInt(inputAttempts);
+
+        Cars cars = Cars.of(nameList);
 
         gameView.showRaceResult();
         for (int i = 0; i < attempts; i++) {
