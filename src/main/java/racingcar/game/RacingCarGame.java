e037ce7eca0c241e217e332e4794f6d862d63e18
JooDukHan <bfinecpa1@naver.com>
2023-10-28 22:48:32 +0900

    feat: 자동차 전지 시키는 기능 추가
    랜덤 숫자를 이용해서 자동차 전진 시킨다.

--- src/main/java/racingcar/game/RacingCarGame.java
+++ src/main/java/racingcar/game/RacingCarGame.java
@@ -3,15 +3,30 @@ package racingcar.game;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.car.Car;
+import racingcar.utils.randomGenerator.RandomNumberGenerator;
 
 public class RacingCarGame {
 
     private List<Car> cars;
+    private final RandomNumberGenerator randomNumberGenerator;
+
+    public RacingCarGame(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
 
     public void play(List<String> carNames, int attemptCount) {
         makeCars(carNames);
         for (int round = 0; round < attemptCount; round++) {
+            moveCarsForward();
+        }
+    }
 
+    private void moveCarsForward() {
+        for (Car car : cars) {
+            int randomNumber = randomNumberGenerator.generateRandomSingleDigit();
+            if (randomNumber>=4) {
+                car.move();
+            }
         }
     }
 
