a4e0036689a39c58fa2f483e079adacfe38982ec
박준영 <wnsdud6969@naver.com>
2023-10-28 20:28:39 +0900

    test(InputViewTest): add test to verify user input view
    Add new event to InputViewTest
    - success : call inputNumberOfGameAttempts() and compare number
    - fail : call inputNumberOfGameAttempts() and throw IllegalArgumentException

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -4,18 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
 
     private InputView inputView;
-    private String userInput;
     private InputStream inputStream;
 
     public InputViewTest() {
         this.inputView = InputView.getInstance();
+        inputStream = createInputStreams();
+        System.setIn(inputStream);
+    }
 
+    private InputStream createInputStreams() {
+        List<InputStream> inputStreams = Arrays.asList(
+            parseUserInputToInputStream("pobi,woni,jun\n"),
+            parseUserInputToInputStream("2\n"),
+            parseUserInputToInputStream("non digit\n")
+        );
+
+        return new SequenceInputStream(Collections.enumeration(inputStreams));
     }
 
     public static InputStream parseUserInputToInputStream(String input) {
@@ -32,13 +45,24 @@ class InputViewTest {
 
     @Test
     public void inputCarNames() {
-        userInput = "pobi,woni,jun\n";
-        inputStream = parseUserInputToInputStream(userInput);
-        System.setIn(inputStream);
-
         List<String> expectedCarNames = List.of("pobi", "woni", "jun");
         List<String> carNames = inputView.inputCarNames();
 
         assertEquals(expectedCarNames, carNames);
+    }
+
+    @Test
+    public void inputNumberOfGameAttempts_success() {
+        int expectedNumberOfGameAttempts = 2;
+        int numberOfGameAttempts = inputView.inputNumberOfGameAttempts();
+
+        assertEquals(expectedNumberOfGameAttempts, numberOfGameAttempts);
+    }
+
+    @Test
+    public void inputNumberOfGameAttempts_fail() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputView.inputNumberOfGameAttempts();
+        });
     }
 }
