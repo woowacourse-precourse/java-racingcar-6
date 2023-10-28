14f73e92845a1dcd7f3ed0cedc65d60af749ca99
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-29 04:29:47 +0900

    feat: 게임 메시지 추가

--- src/main/java/racingcar/config/GameMessages.java
+++ src/main/java/racingcar/config/GameMessages.java
@@ -2,4 +2,19 @@ package racingcar.config;
 
 public class GameMessages {
 
+    /**
+     * 게임 시작 메시지
+     * 자동차 이름 입력 요청 메시지
+     * */
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    /** 시도 횟수 입력 요청 메시지 */
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+
+    /** 실행 결과 메시지 */
+    public static final String EXECUTION_RESULT = "실행 결과";
+
+    /** 최종 우승자 메시지 */
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+
 }
