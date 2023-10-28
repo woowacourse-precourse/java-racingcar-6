b4c4cccf8093ec6b2b3edc6f3703d2136f442343
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-28 21:15:58 +0900

    [refactor] : add static to method

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,12 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String inputCarNames() {
+    public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    public String inputTryCount() {
+    public static String inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
     }
