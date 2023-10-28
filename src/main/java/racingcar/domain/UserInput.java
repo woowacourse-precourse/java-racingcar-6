ae1e07d35cd2d334077ba3dfbb9fb198642f03d5
suuven1 <jschoi9160@naver.com>
2023-10-28 14:21:36 +0900

    [추가] 자동차 이름사용자 오기입시 예외처리 구현

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -1,18 +1,34 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInput {
     public String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = Console.readLine();
         String[] carNames = inputCarNames.split(",");
+        List<String> dupCheckList = new ArrayList<>();
+        for (String carName : carNames) {
+            if (dupCheckList.contains(carName)) {
+                throw new IllegalArgumentException("자동차 이름은 중복없이 설정해주세요.");
+            }
+            dupCheckList.add(carName);
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해주세요.");
+            }
+        }
         return carNames;
     }
 
     public int getTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int tryNumber = Integer.parseInt(Console.readLine());
-        return tryNumber;
+        try {
+            int tryNumber = Integer.parseInt(Console.readLine());
+            return tryNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자로 입력해주세요.");
+        }
     }
 }
