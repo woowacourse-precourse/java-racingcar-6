9685f27800461021db1b4430410cc5148ad9d30a
kcw0360 <kcw0360@naver.com>
2023-10-29 01:04:40 +0900

    Feat: 정규표현식을 이용해 공백이 있는지 확인하는 기능 구현(예외처리)

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,24 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private String[] inputNames;
+    private final Pattern nameValidatePattern = Pattern.compile("([\\S]{1,})");
 
     public void setInputNames() {
         String input = userInput();
+        validateSpace(input);
         String[] names = input.split(",");
         this.inputNames = names;
+    }
+
+    private void validateSpace(String input) {
+        if (!nameValidatePattern.matcher(input).matches()) {
+            throw new IllegalArgumentException("공백을 제거해주세요.");
+        }
     }
 
     public String[] getInputNames() {
@@ -18,5 +27,12 @@ public class InputView {
 
     private static String userInput() {
         return Console.readLine();
+    }
+
+    //test method
+    public void setInputNames(String input) {;
+        validateSpace(input);
+        String[] names = input.split(",");
+        this.inputNames = names;
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,18 +2,42 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
 
+    InputView inputView;
+
+    @BeforeEach
+    void beforeEach() {
+        this.inputView = new InputView();
+    }
+
     @Test
     @DisplayName("입력한 값을 split으로 배열로 만들기")
     void 입력_테스트() {
         //직접 입력하는 것으로 Console.readline() 대체
-        String input = "pobi,woni,jun";
-        String[] names = input.split(",");
+        inputView.setInputNames("pobi,woni,jun");
 
-        assertThat(names).isEqualTo(new String[]{"pobi", "woni", "jun"});
+        assertThat(inputView.getInputNames()).isEqualTo(new String[]{"pobi", "woni", "jun"});
+    }
+    
+    @Test
+    void 예외_공백_확인() {
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.setInputNames("pobi,woni, jun"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백을 제거해주세요.");
+
+        assertThatThrownBy(() -> inputView.setInputNames("po bi,woni,jun"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백을 제거해주세요.");
+
+        assertThatThrownBy(() -> inputView.setInputNames("pobi, ,jun"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백을 제거해주세요.");
     }
 }
