702881679c26ae57c71aa6b47fdeebf4da4f3907
takoyakimchi <c4nyou@gmail.com>
2023-10-28 21:27:23 +0900

    fix(valid): add error message

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -6,12 +6,15 @@ public class Validation {
 
     public static void validateCarNames(List<String> names) {
         if (names.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 이름이 없습니다.");
         }
 
         for (String name : names) {
-            if (name.isBlank() || name.length() > 5) {
-                throw new IllegalArgumentException();
+            if (name.isBlank()) {
+                throw new IllegalArgumentException("빈 이름은 입력할 수 없습니다.");
+            }
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 최대 5자입니다.");
             }
         }
     }
@@ -21,10 +24,10 @@ public class Validation {
             int inputNum = Integer.parseInt(input);
 
             if (inputNum <= 0) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("자연수만 입력할 수 있습니다.");
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
     }
 }
