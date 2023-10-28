c8fc92c31c9c70dc89e2eb35adba0e53b3c81ad6
penrose15 <seongju99@naver.com>
2023-10-28 16:01:54 +0900

    Feat: 입/출력 클래스 생성

---
+++ src/main/java/racingcar/global/input/InputView.java
@@ -0,0 +1,10 @@
+package racingcar.global.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String input() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/global/output/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.global.output;
+
+public class OutputView {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
