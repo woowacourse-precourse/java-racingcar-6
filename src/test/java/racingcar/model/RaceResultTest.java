0acfbf93c8181cab0fe9f1a20862d11deb9c8bb8
kim <hwiyul_g@daum.net>
2023-10-28 14:46:03 +0900

    Test(RaceResult) : 우승자 목록 생성을 위한 레이스 결과 테스트 케이스 추가

---
+++ src/test/java/racingcar/model/RaceResultTest.java
@@ -0,0 +1,63 @@
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class RaceResultTest {
+
+    @Test
+    void 모든_자동차_거리가_다른_경우_winner() {
+        List<Car> cars = Arrays.asList(
+            new Car("a", 1),
+            new Car("b", 2),
+            new Car("c", 3)
+        );
+        RaceResult raceResult = new RaceResult(cars);
+        List<String> result = raceResult.getWinners();
+        List<String> expected = Arrays.asList("c");
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 모든_자동차_거리가_같은_경우_winner() {
+        List<Car> cars = Arrays.asList(
+            new Car("a", 1),
+            new Car("b", 1),
+            new Car("c", 1)
+        );
+        RaceResult raceResult = new RaceResult(cars);
+        List<String> result = raceResult.getWinners();
+        List<String> expected = Arrays.asList("a","b","c");
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 일부_자동차_거리가_같은_경우_winner() {
+        List<Car> cars = Arrays.asList(
+            new Car("a", 3),
+            new Car("b", 1),
+            new Car("c", 3)
+        );
+        RaceResult raceResult = new RaceResult(cars);
+        List<String> result = raceResult.getWinners();
+        List<String> expected = Arrays.asList("a", "c");
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 일부_자동차_거리가_같은_경우_winner2() {
+        List<Car> cars = Arrays.asList(
+            new Car("a", 3),
+            new Car("b", 1),
+            new Car("c", 1)
+        );
+        RaceResult raceResult = new RaceResult(cars);
+        List<String> result = raceResult.getWinners();
+        List<String> expected = Arrays.asList("a");
+        assertEquals(expected, result);
+    }
+
+}
