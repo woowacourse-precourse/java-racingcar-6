9277435950373cd95281dbb311e0bb862890c666
34suuuuu <psh7492@gmail.com>
2023-10-28 16:48:56 +0900

    feat: 시도 횟수 validator 클래스 생성

--- src/main/java/racingcar/car/TryCntValidator.java
+++ src/main/java/racingcar/car/TryCntValidator.java
@@ -1,2 +1,22 @@
-package racingcar.car;public class TryCntValidator {
+package racingcar.car;
+
+import racingcar.Constants;
+
+public class TryCntValidator {
+
+    public final String TRY_COUNT;
+
+    public TryCntValidator(String tryCnt) {
+        this.TRY_COUNT = tryCnt;
+    }
+
+    public void validate(){
+        isInteger();
+    }
+
+    public void isInteger() {
+        if (!Constants.TRY_NUM_PATTERN.matcher(TRY_COUNT).matches()) {
+            throw new IllegalArgumentException(Constants.ERROR + Constants.INPUT_ONLY_INTEGER_ERROR_MESSAGE);
+        }
+    }
 }
