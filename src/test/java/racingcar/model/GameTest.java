6fdf4ebf490c7455db8229e408b3c5d3300b9e1a
flown222 <jangu3384@gmail.com>
2023-10-28 19:00:06 +0900

    test: 동일한 초기화 BeforeEach로 추출

--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -2,23 +2,36 @@ package racingcar.model;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.util.MoveResolver;
 
 class GameTest {
+    private final int MOVING_DISTANCE = 1;
+
     MoveResolver moveResolver = mock(MoveResolver.class);
     MoveResolver nonMoveResolver = mock(MoveResolver.class);
-    private final int MOVING_DISTANCE = 1;
+
+    Game game;
+    List<Car> cars;
+
+    @BeforeEach
+    void setEnvionment() {
+        game = new Game();
+        cars = List.of(new Car("테스트"), new Car("테스트1"), new Car("테스트2"));
+        cars.forEach(car -> game.addCar(car));
+        setGivenBehaviorMoveResolver(moveResolver,nonMoveResolver);
+    }
+
+
+
 
     @Test
     void 가장_먼_거리를_이동한_자동차를_기준으로_우승한_자동차들을_구할_수_있다_단일_우승자() {
-        setGivenBehaviorMoveResolver(moveResolver,nonMoveResolver);
-
-        List<Car> cars = List.of(new Car("테스트"), new Car("테스트1"), new Car("테스트2"));
-        Game game = new Game(cars);
 
         cars.get(0).move(MOVING_DISTANCE,nonMoveResolver);
         cars.get(0).move(MOVING_DISTANCE,nonMoveResolver);
@@ -29,14 +42,11 @@ class GameTest {
 
         List<Car> winners = game.getWinnerCars();
         assertThat(winners).extracting(Car::getName).containsOnly("테스트2");
+        assertThatThrownBy(() -> winners.add(new Car("dd"))).isInstanceOf(UnsupportedOperationException.class);
     }
 
     @Test
     void 가장_먼_거리를_이동한_자동차를_기준으로_우승한_자동차들을_구할_수_있다_복수_우승자() {
-        setGivenBehaviorMoveResolver(moveResolver,nonMoveResolver);
-
-        List<Car> cars = List.of(new Car("테스트"), new Car("테스트1"), new Car("테스트2"));
-        Game game = new Game(cars);
 
         cars.get(0).move(MOVING_DISTANCE,nonMoveResolver);
         cars.get(0).move(MOVING_DISTANCE,nonMoveResolver);
