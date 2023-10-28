11f2ca202d5f76c9fe98e9dcf3af6b2aaedb31e9
cyPark95 <parkcy1007@gmail.com>
2023-10-28 23:10:14 +0900

    feat: 자동차 이름 검증 및 생성 구현 & 테스트
    - 자동차 이름에 대한 유효성 검증 기능 구현
        - 자동차 이름이 null 인지 검증
        - 자동차 이름의 글자 수가 1이상 5이하 인지 검증
    - 테스트 케이스
        - null 예외 발생
        - 빈 문자나 5글자 초과 예외 발생

---
+++ src/main/java/racingcar/common/response/ErrorCode.java
@@ -0,0 +1,19 @@
+package racingcar.common.response;
+
+public enum ErrorCode {
+
+    CAR_NAME_NULL("자동차 이름을 입력해 주세요."),
+    CAR_NAME_INVALID_LENGTH("1글자 이상, 5글자 이하의 자동차 이름을 입력해 주세요."),
+    CAR_NAME_DUPLICATION("중복되지 않는 자동차 이름을 입력해 주세요."),
+    ;
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import racingcar.common.exception.CustomErrorException;
+
+import java.util.Objects;
+
+import static racingcar.common.response.ErrorCode.*;
+
+public record CarName(String name) {
+
+    private static final int MIN_NAME_SIZE = 1;
+    private static final int MAX_NAME_SIZE = 5;
+    public static final String SPACE = " ";
+
+    public CarName {
+        validateName(name);
+    }
+
+    private static void validateName(String name) {
+        if (isNull(name)) {
+            throw new CustomErrorException(CAR_NAME_NULL);
+        }
+
+        if (isInvalidLength(name)) {
+            throw new CustomErrorException(CAR_NAME_INVALID_LENGTH);
+        }
+    }
+
+    private static boolean isNull(String name) {
+        return Objects.isNull(name);
+    }
+
+    private static boolean isInvalidLength(String name) {
+        return name.length() < MIN_NAME_SIZE || name.length() > MAX_NAME_SIZE;
+    }
+}
