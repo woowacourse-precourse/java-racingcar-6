5fb41b89ff5f2ecff219eca2a504ebbc2faf23c0
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 03:57:48 +0900

    fix(InputParser): Car와 CarName 객체를 생성하고 반환하게 메서드 수정
    carsSplit 메서드를 변경하여 입력 문자열을 파싱하고 Car와 CarName 객체를 생성하도록 수정하였습니다

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -2,14 +2,17 @@ package racingcar.util;
 
 import java.util.Arrays;
 import java.util.List;
-
+import racingcar.domain.Car;
+import racingcar.domain.CarName;
 public class InputParser {
     public static int tryCountParser(String input) {
         return Integer.parseInt(input);
     }
 
-    public static List<String> carsParser(String input) {
-        String[] cars = input.split(",");
-        return Arrays.stream(cars).toList();
+    public static List<Car> carsSplit(String input) {
+        return Arrays.stream(input.split(","))
+                .map(CarName::new)
+                .map(Car::new)
+                .toList();
     }
 }
