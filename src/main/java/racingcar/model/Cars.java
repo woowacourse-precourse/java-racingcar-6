84434aa1572fa9e9c043130f5b1b0c725cb34f8e
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 01:51:52 +0900

    feat(Cars): Car가 중복될 경우 예외 메시지 출력하는 기능 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import java.util.List;
+import racingcar.constants.ExceptionMessage;
 
 public class Cars {
 
@@ -8,6 +9,20 @@ public class Cars {
 
     public Cars(final List<Car> cars) {
         this.cars = cars;
+    }
+
+    private void validateCars(final List<Car> cars) {
+        validateDuplicateName(cars);
+    }
+
+    private void validateDuplicateName(final List<Car> cars) {
+        if (hasDuplicateName(cars)) {
+            ExceptionMessage.NOT_INPUT_CARS_DUPLICATE.throwException();
+        }
+    }
+
+    private boolean hasDuplicateName(final List<Car> cars) {
+        return cars.stream().distinct().count() != cars.size();
     }
 
 }
