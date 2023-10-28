abe875aa66a9fec1d5fff4113f367374555d7ac3
nohheeyeon <heeyeon1213@gmail.com>
2023-10-28 19:00:10 +0900

    feat : 자동차 객체를 초기화하는 로직 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
