f9d26ce07af566dbd0c064b14cb3c7731f483f35
leedohyun <lucky77795@naver.com>
2023-10-28 16:26:12 +0900

    feat(racingCar) : 시도 횟수 입력값 예외 처리 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -3,9 +3,16 @@ package racingcar.validator;
 public class InputValidator {
 
     private final String CarNameRegex = "\\w+(\\s+\\w+)*(,\\w+(\\s+\\w+)*)*"; // ,뒤 공백은 비허용/이름 사이 공백은 허용
+    private final String TryCountRegex = "[1-9]\\d*";
 
     public void validateInputCarName(String carName) {
         if (!carName.matches(CarNameRegex)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateInputTryCount(String tryCount) {
+        if (!tryCount.matches(TryCountRegex)) {
             throw new IllegalArgumentException();
         }
     }
