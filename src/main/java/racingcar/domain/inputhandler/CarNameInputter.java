aeda033e0a19f43b96fed6a3a0350fe75a61f71c
Picka <marmong@naver.com>
2023-10-28 17:19:59 +0900

    feat: 경주할 자동차를 입력 받는 기능 구현 (이름은 쉼표(,) 기준으로 구분)

---
+++ src/main/java/racingcar/domain/inputhandler/CarNameInputter.java
@@ -0,0 +1,27 @@
+package racingcar.domain.inputhandler;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarNameInputter {
+    public String getCarNames() {
+        printInputMessage();
+        return readCarNames();
+    }
+
+    private void printInputMessage() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    private String readCarNames() {
+        return readLine();
+    }
+
+    public List<String> divineCarName(String carNames) {
+        List<String> carName = Arrays.asList(carNames.split(","));
+        return carName;
+    }
+
+}
