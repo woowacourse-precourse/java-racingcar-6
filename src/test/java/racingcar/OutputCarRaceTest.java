8c78146e5052a5f1b69d8358071c83e88f7cd377
OikimiO <etkim02@naver.com>
2023-10-28 14:17:28 +0900

    test: 출력 기능 테스트 추가
    - PrintTest: 출력을 테스트하기 위한 추상클래스 추가
    - OutputCarRaceTest: 시스템 출력을 위한 inner class 및 테스트 메서드 추가

---
+++ src/test/java/racingcar/OutputCarRaceTest.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class OutputCarRaceTest extends PrintTest{
+    enum MessageType{
+        INPUT_CAR_NAME_PRINT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        private final String value;
+
+        MessageType(String value){
+            this.value = value;
+        }
+
+        public String getValue(){
+            return value;
+        }
+    }
+
+    static class OutputCarRace{
+        public static OutputCarRace print(final MessageType messageType){
+            return new OutputCarRace(messageType);
+        }
+
+        private OutputCarRace(){}
+
+        private OutputCarRace(final MessageType messageType){
+            System.out.println(messageType.getValue());
+        }
+    }
+
+    @Test
+    void 자동차_이름입력멘트_출력_정상(){
+        OutputCarRace.print(MessageType.INPUT_CAR_NAME_PRINT);
+
+        assertThat(output()).contains(MessageType.INPUT_CAR_NAME_PRINT.getValue());
+    }
+}
