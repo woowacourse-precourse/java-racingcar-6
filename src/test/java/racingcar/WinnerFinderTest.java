a12cc97e893547d79f61d20d4eecb750a7e68124
junest66 <chlwndks333@gmail.com>
2023-10-29 05:39:31 +0900

    test(WinnerFinderTest): cars List의 최대 이동 횟수 찾는 테스트 추가
    - 최대 이동 횟수 찾는 테스트 추가
    - 아무도 움직이지 않았을때 테스트 추가

---
+++ src/test/java/racingcar/WinnerFinderTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class WinnerFinderTest {
+
+    @Test
+    void 최대이동횟수_찾기() {
+        Car car1 = new Car("pobi", new TestNumberGenerator(4));
+        Car car2 = new Car("woni", new TestNumberGenerator(5));
+        Car car3 = new Car("jun", new TestNumberGenerator(3));
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+        Game game = new Game(cars, 5);
+        game.moveAllCarsOnce();
+        game.moveAllCarsOnce();
+        int maxMoveNum = WinnerFinder.getMaxMoveNum(cars);
+        assertThat(maxMoveNum).isEqualTo(2);
+    }
+
+    @Test
+    void 아무도_움직이지_않았을때_최대이동횟수() {
+        Car car1 = new Car("pobi", new TestNumberGenerator(1));
+        Car car2 = new Car("woni", new TestNumberGenerator(2));
+        Car car3 = new Car("jun", new TestNumberGenerator(3));
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+        Game game = new Game(cars, 5);
+        game.moveAllCarsOnce();
+        game.moveAllCarsOnce();
+        int maxMoveNum = WinnerFinder.getMaxMoveNum(cars);
+        assertThat(maxMoveNum).isEqualTo(0);
+    }
+}
