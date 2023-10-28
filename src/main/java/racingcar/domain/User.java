20173b1861f9c09928f86117907eb1f991f62cff
YeaChan05 <qkenrdl05@gmail.com>
2023-10-28 20:32:49 +0900

    [FIX] 도메인 병합(User+RacingCar)

--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -1,18 +1,50 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 /**
  * 경주에 참여하는 주체
  */
 public class User {
-    private String name;
-    private RacingCar car;
+    private final String name;
+    private int proceedCount;
 
-    private User(String name, RacingCar car) {
+    private User(String name) {
         this.name = name;
-        this.car = car;
+        this.proceedCount = 0;
     }
 
-    public static User of(String name, RacingCar car) {
-        return new User(name, car);
+    public static User of(String name) {
+        return new User(name);
+    }
+
+
+    public String visualizeProgress() {
+        return name + " : " + visualizedCount();
+    }
+
+    private StringBuilder visualizedCount() {
+        StringBuilder sb=new StringBuilder();
+        return sb.append("-".repeat(Math.max(0, proceedCount)));
+    }
+
+    public void proceedCar() {
+        this.proceedCount++;
+    }
+
+    /**
+     * 차가 움직일지 결정
+     */
+    public void decideProceed() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) proceedCar();
+    }
+
+    public int getProceedCount() {
+        return proceedCount;
+    }
+
+    public String getName() {
+        return name;
     }
 }
