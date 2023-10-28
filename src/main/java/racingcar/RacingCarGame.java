d885657722bffd3dda8fa18549540abb5d948a2d
sunwooiljoon <wnsdl1659@naver.com>
2023-10-28 13:54:06 +0900

    feat : 게임 시작 출력
    게임 시작 시 `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)` 출력

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarGame {
@@ -7,10 +8,22 @@ public class RacingCarGame {
     private Integer tryNumber;
     List<RacingCar> racingCarList;
 
+    public RacingCarGame() {
+        this.tryNumber = 0;
+        this.racingCarList = new ArrayList<>();
+    }
+
     public void play() {
+        requestCarNames();
+        requestTryNumber();
+        for (int i = 0; i < tryNumber; i++) {
+            tryOnce();
+        }
+        printWinners();
     }
 
     private void requestCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     private void requestTryNumber() {
