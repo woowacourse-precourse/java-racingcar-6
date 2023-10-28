08543bb27ea14756609baba4462014c9f1a1b652
JH-TT <leejongho9803@gmail.com>
2023-10-29 02:15:23 +0900

    feat - 레이싱게임 시작하는 코드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.Arrays;
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController();
+        racingController.racingReady();
     }
 }
