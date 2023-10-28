fe320cef4136f8badb673bd208ce9a968abdede2
elyudwo <dudqk9696@naver.com>
2023-10-28 17:10:08 +0900

    [Feat] : CarDto 객체 getter 추가
    name, distance 필드 getter 추가

---
+++ src/main/java/racingcar/domain/CarDto.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+public class CarDto {
+
+    private String name;
+    private int distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+}
