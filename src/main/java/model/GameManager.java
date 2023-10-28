936b6691953cc537e1a19391a15159ffae1a98ea
Kim Jeong Hyeon <kr100475@gmail.com>
2023-10-29 02:39:10 +0900

    [feat] 각각의 자동차에 대해서 N번의 이동 로직 구현\n[[refactor] 사용자 입력 리스트로 수정

--- src/main/java/model/GameManager.java
+++ src/main/java/model/GameManager.java
@@ -1,4 +1,46 @@
 package model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class GameManager {
+    List<String> carName;
+    int num;
+    public GameManager(List<String> carName, int num) {
+        this.carName = carName;
+        this.num = num;
+    }
+
+    //게임 로직
+    public boolean gameStart() {
+        int[] carMoveCount = new int[carName.size()];
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("실행 결과").append("\n");
+
+        for(int i=0; i<num; i++) {
+
+            for(int j=0; j<carName.size(); j++) {
+                boolean flag = isMove();
+                if(flag) carMoveCount[j]++;
+
+                sb.append(carName.get(j)).append(" : ").append("-".repeat(carMoveCount[j])).append("\n");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+        return true;
+    }
+
+    public boolean isMove() {
+        int randomNum = Randoms.pickNumberInRange(0,9);
+
+        if(randomNum >= 4) return true;
+        return false;
+    }
+
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,13 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import model.GameManager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     private static String firstMent = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String secondMent = "시도할 회수는 몇회인가요?";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -13,11 +18,15 @@ public class Application {
         //이름 입력
         String nameInput = Console.readLine();
 
-        String[] carName = nameInput.split(",");
+        List<String> carNameList = Arrays.asList(nameInput.split(","));
 
+        System.out.println(secondMent);
         int num = Integer.parseInt(Console.readLine());
 
-        new GameManager(carName, num);
+        //입력값 세팅
+        GameManager gameManager = new GameManager(carNameList, num);
+
+        gameManager.gameStart();
 
     }
 }
