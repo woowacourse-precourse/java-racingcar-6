ad0e3fa2fe9a170b53f2ba72533490c6943436c8
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-28 17:27:33 +0900

    refactor: 기능별로 메서드 구분

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,30 +12,65 @@ import java.util.StringTokenizer;
 public class Application {
     private static final int INIT_DISTANCE = 0;
     private static final int MOVING_FORWARD = 4;
+    private static final String GAME_RESULT = "\n실행 결과";
 
     public static void main(String[] args) {
-        // HashMap의 순서를 보장하기 위해 LinkedHashMap 사용
         Map<String, Integer> memberStates = new LinkedHashMap<>();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String memberInput = Console.readLine();
+        List<String> winner = new ArrayList<>();
 
+        memberStates = getMemberState();
+
+        int tryCount = getTryCount();
+
+        System.out.println(GAME_RESULT);
+
+        gameStart(tryCount, memberStates);
+
+        winner = getWinner(memberStates);
+
+        printWinnerView(winner);
+    }
+
+    public static Map<String, Integer> getMemberState() {
+        // HashMap의 순서를 보장하기 위해 LinkedHashMap 사용
+        Map<String, Integer> memberStates = new LinkedHashMap<>();
+        String memberInput = carNameInputView();
         StringTokenizer stringTokenizer = new StringTokenizer(memberInput, ",");
         while (stringTokenizer.hasMoreTokens()) {
             String name = stringTokenizer.nextToken();
-            // * 이름 예외 처리
-            // 이름 5자 이하가 아닐 경우
-            if (name.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-
+            nameException(name);
             memberStates.put(name, INIT_DISTANCE);
         }
 
+        return memberStates;
+    }
+
+    public static String carNameInputView() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String memberInput = Console.readLine();
+        return memberInput;
+    }
+
+    public static void nameException(String name) {
+        // * 이름 예외 처리
+        // 이름 5자 이하가 아닐 경우
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int getTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String tryInput = Console.readLine();
         int tryCount = Integer.parseInt(tryInput);
 
+        tryCountException(tryCount);
+
+        return tryCount;
+    }
+
+    public static void tryCountException(int tryCount) {
         // * 시도 횟수 예외 처리
         // 입력 값이 숫자가 아닌 경우
         if (Character.isDigit(tryCount)) {
@@ -46,49 +81,37 @@ public class Application {
         if (tryCount <= 0) {
             throw new IllegalArgumentException();
         }
+    }
 
-        System.out.println();
-        System.out.println("실행 결과");
+    public static void gameStart(int tryCount, Map<String, Integer> memberStates) {
         // 게임 시작
         for (int i = 0; i < tryCount; i++) {
-            // 한 게임씩 시작
-            for (String name : memberStates.keySet()) {
-
-                // 무작위 값 뽑기
-                int randomNumber = Randoms.pickNumberInRange(0, 9);
-
-                // 전진할 경우
-                if (randomNumber >= MOVING_FORWARD) {
-                    memberStates.replace(name, memberStates.get(name) + 1);
-                }
-
-                // 각 차수별 실행 결과
-                System.out.println(name + " : " + changeDash(memberStates.get(name)));
-            }
+            memberStates = startEachGame(memberStates);
+            printEachGameResultView(memberStates);
             System.out.println();
         }
+    }
 
-        // 최종 우승자 결정
-
-        // 최대 이동값 구하기
-        int maxNumber = 0;
-        List<String> winner = new ArrayList<>();
+    public static Map<String, Integer> startEachGame(Map<String, Integer> memberStates) {
+        // 한 차수씩 시작
         for (String name : memberStates.keySet()) {
-            if (memberStates.get(name) > maxNumber) {
-                maxNumber = memberStates.get(name);
-                winner.clear();
-                winner.add(name);
-            }
-            if (memberStates.get(name) == maxNumber) {
-                if (winner.contains(name)) {
-                    continue;
-                }
-                winner.add(name);
+
+            // 무작위 값 뽑기
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            // 전진할 경우
+            if (randomNumber >= MOVING_FORWARD) {
+                memberStates.replace(name, memberStates.get(name) + 1);
             }
         }
-        // 결과 출력
-        String result = String.join(",", winner);
-        System.out.println("최종 우승자 : " + result);
+        return memberStates;
+    }
+
+    public static void printEachGameResultView(Map<String, Integer> memberStates) {
+        for (String name : memberStates.keySet()) {
+            // 각 차수별 실행 결과
+            System.out.println(name + " : " + changeDash(memberStates.get(name)));
+        }
     }
 
     // 이동거리를 대쉬(-)로 변경
@@ -100,5 +123,36 @@ public class Application {
         }
 
         return stringBuilder;
+    }
+
+    public static List<String> getWinner(Map<String, Integer> memberStates) {
+        // 최종 우승자 결정
+
+        // 최대 이동값 구하기
+        int maxNumber = getMaxNumber(memberStates);
+        List<String> winner = new ArrayList<>();
+        for (String name : memberStates.keySet()) {
+            if (memberStates.get(name) == maxNumber) {
+                winner.add(name);
+            }
+        }
+
+        return winner;
+    }
+
+    public static int getMaxNumber(Map<String, Integer> memberStates) {
+        int maxNumber = 0;
+        for (String name : memberStates.keySet()) {
+            if (memberStates.get(name) > maxNumber) {
+                maxNumber = memberStates.get(name);
+            }
+        }
+        return maxNumber;
+    }
+
+    public static void printWinnerView(List<String> winner) {
+        // 결과 출력
+        String result = String.join(",", winner);
+        System.out.println("최종 우승자 : " + result);
     }
 }
