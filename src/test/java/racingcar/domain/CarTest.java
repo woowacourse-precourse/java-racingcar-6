dae5241ba78dba06059f8edfa98b4d6c1ce6a5b4
cousim46 <wjdghldns24@naver.com>
2023-10-29 01:53:06 +0900

    test : 자동차 객체 생성에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차 이름이 비어있으면 예외가 발생한다.")
+    void generateExceptionCarNameIsBlank() {
+        //given
+        String carName = "";
+
+        // when && then
+        assertThatThrownBy(() -> Car.createForStart(carName))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자를 초과 할 경우 예외가 발생한다.")
+    void generateExceptionCarNameLengthExceedFive() {
+        //given
+        String carName = "a".repeat(6);
+
+        //when && then
+        assertThatThrownBy(() -> Car.createForStart(carName))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자 이하이고 비어있지않으면 자동차 객체가 생성이 된다.")
+    void createSuccess() {
+        //given
+        String carName = "test";
+
+        //when
+        Car car = Car.createForStart(carName);
+
+        //then
+        assertThat(car.getName()).isEqualTo(carName);
+        assertThat(car.getStraightCount()).isEqualTo(0);
+    }
+}
