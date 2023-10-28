8d83ce05099b52f8316ca85307e99a728d76788e
taehoon <xogns1514@gmail.com>
2023-10-28 18:51:19 +0900

    style: inputView 클래스명 변경
    - inputView -> InputView로 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class inputView {
+public class InputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
