61a7e3de5049c7f4baa25fcbc4a83ee73929581f
doka <posmane5@gmail.com>
2023-10-28 18:55:44 +0900

    chore(CarController) : CarController 클래스 주석수정

--- src/main/java/racingcar/domain/CarController.java
+++ src/main/java/racingcar/domain/CarController.java
@@ -6,17 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarController {
 
-//- [ ] 주어진 횟수(세트수) 동안 n대의 자동차는 전진 또는 멈출 수 있다. - carControler()
-//    - [ ] n대의 자동차 - cars[]
-//    - [ ] 전진 혹은 멈추는 조건 확인 - isForward
-//      - [ ] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다. isForward()
-//        - [ ] 0~9 사이의 무작위값 구현 - makeRandomNumbers()
-//        - [ ] 4를 기준으로 전진 조건 확인  - isBiggerThanFour()
-//    - [ ] 1세트 경주 결과 기록후 해시맵 반환 - gameStart
-//      - [ ] 결과 기록 해시맵 - resultRacing HashMap<String, Integer>
-//      - [ ] 경주 이후 결과 해시맵 에 기입. gameStart()
-
-//    진행과정
+// 진행과정
 // 자동차 리스트를 가지고 와서
 // 레이싱 1세트 진행 시키고
 // 차1대의 전진이 끝날때마다 해시맵에 기록
