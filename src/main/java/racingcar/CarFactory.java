56d782d830149ccdcc37770772fbd2e0e216a99b
박찬건 <jg9870@naver.com>
2023-10-28 16:04:36 +0900

    refactor(racingcar): 함수 이름 변경, 함수 더 작은 단위로 쪼개기

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -1,18 +1,42 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarFactory {
-    public static List<Car> from(String names) {
-        String[] nameArr = names.split(",");
+    public static List<Car> createCarsFromInput(String names) {
+        String[] nameArr = splitNames(names);
+        validateUniqueNames(nameArr);
+        return createCars(nameArr);
+    }
+
+    private static String[] splitNames(String names) {
+        return names.split(",");
+    }
+
+    private static void validateUniqueNames(String[] nameArr) {
+        Set<String> nameSet = new HashSet<>();
+        for (String name : nameArr) {
+            if (!nameSet.add(name.trim())) {
+                throw new IllegalArgumentException("중복된 이름이 있습니다.");
+            }
+        }
+    }
+
+    private static List<Car> createCars(String[] nameArr) {
         List<Car> carList = new ArrayList<>();
         for (String name : nameArr) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
+            validateNameLength(name);
             carList.add(new Car(name.trim()));
         }
         return carList;
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
     }
 }
