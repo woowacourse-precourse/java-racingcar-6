d1ec47ecb9c6163dfd351e24e99886fdfbac633c
hyerimmy <heyrim2010@naver.com>
2023-10-29 03:09:09 +0900

    feat: Car 모델 클래스 정의

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,15 @@
 package racingcar.model;
 
 public class Car {
+    public String name;
+    public int position;
+
+    public void Car(String name){
+        this.name=name;
+        position=0;
+    }
+
+    public void move(){
+        ++position;
+    }
 }
