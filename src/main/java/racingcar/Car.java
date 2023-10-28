6ed899170e624cbed0d1432bcaec4f1fd5310e3d
psy3720 <qkrtmddyd12@naver.com>
2023-10-28 12:40:41 +0900

    feat: 자동차 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int movingCount;
+
+    public Car(String name, int movingCount) {
+        this.name = name;
+        this.movingCount = movingCount;
+    }
+
+    public void moveAccordingToInput(int number) {
+        if(number >= 4) {
+            this.moving();
+        }
+    }
+
+    private void moving() {
+        this.movingCount++;
+    }
+
+    public int getMovingCount() {
+        return this.movingCount;
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    void 값이_4이상인경우_1번전진한다() {
+        // given
+        Car car = new Car("pobi", 0);
+
+        // when
+        car.moveAccordingToInput(4);
+
+        // then
+        assertThat(car.getMovingCount()).isEqualTo(1);
+    }
+
+    @Test
+    void 값이_3이하인경우_정지한다() {
+        // given
+        Car car = new Car("pobi", 0);
+
+        // when
+        car.moveAccordingToInput(3);
+
+        // then
+        assertThat(car.getMovingCount()).isEqualTo(0);
+    }
+}
