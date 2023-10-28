fbcc974e1d0550c09a78888d4a5360b157fda65a
JP-company <wjsdj2008@gmail.com>
2023-10-28 21:06:33 +0900

    기능 1. 자동차 이름 입력 기능 [수정1_테스트 클래스 이름 변경]

--- src/test/java/racingcar/InputCarNamesTest.java
+++ src/test/java/racingcar/InputCarNamesTest.java
@@ -2,19 +2,17 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 import camp.nextstep.edu.missionutils.Console;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 
-public class MethodTest {
+public class InputCarNamesTest {
 
     @Test
-    void inputCarNames_정상_입력시_리스트반환() {
+    void 정상_입력시_리스트반환() {
         Console.close();
 
         String testInput = " Jun , Alex , David ";
@@ -24,7 +22,7 @@ public class MethodTest {
     }
 
     @Test
-    void inputCarNames_자동차이름_공백값_포함시_예외발생() {
+    void 자동차이름_공백값_포함시_예외발생() {
         String[] testInputList = {
                 "Jun,, Alex",
                 "Jun,  , Alex",
@@ -44,13 +42,13 @@ public class MethodTest {
     }
 
     @Test
-    void inputCarNames_자동차이름_5자_이상_입력시_예외발생() {
+    void 자동차이름_5자_이상_입력시_예외발생() {
         String testInput = "Jun, Alex, JeongPyo";
         carNameInputErrorOccurs(testInput, "자동차 이름의 길이는 5자리 이하 이어야 합니다.");
     }
 
     @Test
-    void inputCarNames_자동차이름_중복시_예외발생() {
+    void 자동차이름_중복시_예외발생() {
         String testInput = "Jun, Alex, Alex";
         carNameInputErrorOccurs(testInput, "자동차 이름은 중복될 수 없습니다.");
     }
