2e22ab14b398844fc486679dccd466119a59fd73
qncj5224 <qncj5224@naver.com>
2023-10-28 14:32:11 +0900

    fix: 배열들을 리스트로 변환

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -1,19 +1,20 @@
 package racingcar.validator;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CarNameValidator {
 
-  public void isNameLengthValid(String[] carNamesList) throws IllegalArgumentException {
+  public void isNameLengthValid(List<String> carNamesList) throws IllegalArgumentException {
     for (String carName : carNamesList) {
-      if (carName.length() > 5 ) {
+      if (carName.length() > 5) {
         throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
       }
     }
   }
 
-  public void isNameDuplicate(String[] carNamesList) throws IllegalArgumentException {
+  public void isNameDuplicate(List<String> carNamesList) throws IllegalArgumentException {
     Set<String> uniqueElements = new HashSet<>();
     for (String element : carNamesList) {
       if (uniqueElements.contains(element)) {
@@ -23,13 +24,13 @@ public class CarNameValidator {
     }
   }
 
-  public void isNumberOfCarsValid(String[] carNamesList) throws IllegalArgumentException {
-    if(carNamesList.length < 2) {
+  public void isNumberOfCarsValid(List<String> carNamesList) throws IllegalArgumentException {
+    if (carNamesList.size() < 2) {
       throw new IllegalArgumentException("2개 이상의 이름을 입력해주세요.");
     }
   }
 
-  public void isNameValid(String[] carNamesList) throws IllegalArgumentException {
+  public void isNameValid(List<String> carNamesList) throws IllegalArgumentException {
     for (String element : carNamesList) {
       if (element != null && element.trim().isEmpty()) {
         throw new IllegalArgumentException("공백을 입력하지 마세요.");
