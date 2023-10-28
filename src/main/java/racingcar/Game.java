809eb90f31793a3ef86e2f197e8b521549e8fe02
junest66 <chlwndks333@gmail.com>
2023-10-29 06:09:36 +0900

    feat(Game): 게임 진행 확인 기능 추가
    - 현재 라운드가 전체 시도 횟수보다 작은 경우에 게임을 계속 진행

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -22,6 +22,10 @@ public class Game {
         return currentRound;
     }
 
+    public boolean isGameOngoing() {
+        return currentRound < attemptCount;
+    }
+
     public void playOneRound() {
         moveAllCarsOnce();
         increaseRound();
