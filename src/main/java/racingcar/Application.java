515159c29db8bb951d0ebc023076ccf6566b5088
김서현 <kimseohyun@gimseohyeons-MacBook-Pro.local>
2023-10-28 16:13:55 +0900

    refactor: make car class in Model

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.model.Car;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
