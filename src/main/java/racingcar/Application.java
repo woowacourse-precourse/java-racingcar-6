07ca5e251d813321080f755985f43a148ca41bc6
nyeroni <nir2y@naver.com>
2023-10-28 21:22:24 +0900

    Update: 게임 실행 코드

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.controller.CarGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarGameController controller = new CarGameController();
     }
 }
