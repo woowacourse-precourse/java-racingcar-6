fd5e1331b61749f06f4797bd5367068c53d6db92
gusdn0108 <gusdn6671@naver.com>
2023-10-28 14:06:06 +0900

    feat(Car) : 차의 움직임 기능구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+
+    public int move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            position++;
+        }
+        return randomNumber;
+    }
+
+
+
+}
