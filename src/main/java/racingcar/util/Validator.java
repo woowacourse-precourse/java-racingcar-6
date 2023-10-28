cb985a1fa06a9977dd874d7840b8f28e28e98091
sehee <tpgml701@naver.com>
2023-10-29 04:06:39 +0900

    feat: Validator 입력 예외 처리 클래스 제작

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,2 +1,22 @@
-package racingcar.util;public class Validator {
+package racingcar.util;
+
+import org.assertj.core.api.ThrowableAssert;
+
+public class Validator {
+
+    public void validParseInt(String input) throws IllegalArgumentException {
+        try {
+            int inputInt = Integer.parseInt(input);
+            validRangeNumber(inputInt);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자 값을 입력해주세요.");
+        }
+    }
+
+    public void validRangeNumber(int input) throws IllegalArgumentException {
+        if (0 >= input) {
+            throw new IllegalArgumentException("1~2147483647의 값을 입력해주세요.");
+        }
+    }
+
 }
