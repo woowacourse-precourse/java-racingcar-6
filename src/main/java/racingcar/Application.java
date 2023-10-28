8b37c3a47c107aaa2e054c3532706c446101abbd
AJN <buzz781@gmail.com>
2023-10-28 14:59:13 +0900

    1.01 메인함수 코드축약

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,9 +8,8 @@ public class Application {
     public static void main(String[] args) {
     	//게임준비
         ArrayList<Car> carListForGameReady = Game.gameReady();
-        int player_input_gameTRY = Game.gameTRYCount();
         //게임시작
-        Game.gameStart(player_input_gameTRY, carListForGameReady);
+        Game.gameStart(Game.gameTRYCount(), carListForGameReady);
         //우승자(엔딩)
         Game.gameWinner(carListForGameReady);
     }
