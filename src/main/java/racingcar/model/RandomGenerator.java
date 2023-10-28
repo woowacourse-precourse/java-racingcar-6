3d8ad66b83a30ff88fba96c37cc06021612912e8
Dawin <dawin002@naver.com>
2023-10-28 19:25:18 +0900

    feat(RandomGenerator): 랜덤 숫자 생성 기능 추가
    랜덤 숫자를 생성해 반환하는 createNumber() 기능 추가
    - camp.nextstep.edu.missionutils의 Randoms API 사용
    - pickNumberInRange(1, 9) 메서드로 생성한 1에서 9 사이의 랜덤값 반환

---
+++ src/main/java/racingcar/model/RandomGenerator.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGenerator {
+    public static int createNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+}
