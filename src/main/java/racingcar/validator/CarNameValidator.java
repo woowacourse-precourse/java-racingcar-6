a76c7789d08728f2f0fa35b77ba14155584a0998
qncj5224 <qncj5224@naver.com>
2023-10-28 14:17:52 +0900

    feat: 자동차 이름이 2개 이상인지 확인하는 메서드 구현

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -12,4 +12,22 @@ public class CarNameValidator {
       }
     }
   }
+
+  public void isNameDuplicate(String[] carNamesList) throws IllegalArgumentException {
+    Set<String> uniqueElements = new HashSet<>();
+    for (String element : carNamesList) {
+      if (uniqueElements.contains(element)) {
+        throw new IllegalArgumentException("중복된 이름이 있습니다.");
+      }
+      uniqueElements.add(element);
+    }
+  }
+
+  public void isNumberOfCarsValid(String[] carNamesList) throws IllegalArgumentException {
+    if(carNamesList.length < 2) {
+      throw new IllegalArgumentException("2개 이상의 이름을 입력해주세요.");
+    }
+  }
+
+
 }
