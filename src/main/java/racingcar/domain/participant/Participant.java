81d4230f323f72015e6cb95dc74db08a75e8702e
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-28 11:20:41 +0900

    style(Participant): 기본 생성자 명시 제거 및 carName 변수 final 적용
    - carName을 강제하는 protected 생성자가 있으므로 기본 생성자는 명시하지 않아도 된다고 판단

--- src/main/java/racingcar/domain/participant/Participant.java
+++ src/main/java/racingcar/domain/participant/Participant.java
@@ -1,14 +1,12 @@
 package racingcar.domain.participant;
 
 public class Participant {
-    private Participant() {}
-
     protected Participant(String carName) {
         this.carName = carName;
         this.distance = 0;
     }
 
-    private String carName;
+    private final String carName;
     private int distance;
 
     public String getCarName() {
