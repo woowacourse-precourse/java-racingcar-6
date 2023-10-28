36ac56599bd9b198d06d9a10021d755e8b102a04
SeongHun <tjdgns5506@gmail.com>
2023-10-28 21:52:22 +0900

    fix: 테스트 클래스 이름 변경

--- src/test/java/racingcar/CarNamesCheckerTest.java
+++ src/test/java/racingcar/CarNamesCheckerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CarNamesTest {
+public class CarNamesCheckerTest {
     @Test
     public void testValidCarNames() {
         String validCarNames = "Car1,Car2,Car3";
