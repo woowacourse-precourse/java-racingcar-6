a856278ab1ecd94cffaf52ee3600b7f1cea8aa60
김효준 <khyojun1104@gmail.com>
2023-10-28 15:14:02 +0900

    feat(CarRace): OutputView, GameController ->  Output관련 초기 출력 관련 기능 구현!

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.service.CarGameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -7,18 +8,23 @@ public class GameController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final CarGameService carGameService;
 
     public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.carGameService = new CarGameService();
     }
 
     public void start() {
+
         initGame();
+
     }
 
     private void initGame() {
         inputView.inputCarName();
+        outputView.printInitGame();
 
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -14,4 +14,7 @@ public class OutputView {
         return instance;
     }
 
+    public void printInitGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    }
 }
