9a8f2e6aa6e7cca4fbf6169d07fef3b3419c053c
Dawin <dawin002@naver.com>
2023-10-28 18:53:12 +0900

    feat(Car): 자동차를 한 칸 전진하는 기능 추가
    자동차를 한 칸 전진하는 moveForward() 기능 추가.
    자동차 객체의 상태 중 자동차가 이동한 거리 distance를 1 증가시킴.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,8 +2,15 @@ package racingcar.model;
 
 public class Car {
     private String name;
-    private int distance = 0;
+    private int distance;
     public Car(String name) {
         this.name = name;
+        distance = 0;
     }
+
+    public void moveForward() {
+        distance += 1;
+    }
+
+
 }
