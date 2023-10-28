8211bc64c2a95f6ab4b748ba07675e101a7edb26
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-28 20:39:58 +0900

    refactor - 반복문 -> @RepeatedTest로 변경

--- src/test/java/racingcar/service/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/service/RandomNumberGeneratorTest.java
@@ -1,18 +1,18 @@
 package racingcar.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class RandomNumberGeneratorTest {
 
     @DisplayName("0~9까지의 난수를 생성한다.")
     @Test
+    @RepeatedTest(10)
     void generateRandomValue() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        for (int i = 0; i < 10; i++) {
             int actual = randomNumberGenerator.generateRandomNumber();
             assert (actual >= 0 && actual <= 9);
-        }
     }
 }
