e801327280764df19785683d062cb535ebe3a860
EvelynKimm <evekimmy09@gmail.com>
2023-10-29 02:27:51 +0900

    feat: add validate number input method
    사용자의 round 숫자 입력이 올바른 형식으로 기입되었는지 검증하는 기능 추가

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -8,4 +8,15 @@ public class Validation {
             }
         }
     }
+    public static void validateNumber(int input) {
+        if (input < 0) {
+            throw new IllegalArgumentException("음수는 입력 불가합니다.");
+        }
+
+        try {
+            Integer.parseInt(String.valueOf(input)); // 문자열로 변환하고 정수로 다시 변환 시도
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+    }
 }
