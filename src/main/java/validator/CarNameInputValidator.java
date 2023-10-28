2b0b66f5216e16646dff2d86d7e901de5bbded56
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 20:48:13 +0900

    refactor: 입력이 쉼표(,)로만 이루어져 있을 때 검사 및 쉼표(,)로 파싱한 뒤 이름이 조건에 맞는지 확인하는 방식으로 변경

--- src/main/java/validator/CarNameInputValidator.java
+++ src/main/java/validator/CarNameInputValidator.java
@@ -4,10 +4,17 @@ import util.NameConstant;
 import java.util.regex.Pattern;
 
 public class CarNameInputValidator {
-    private final String REGEXP_PATTERN_SPACE_NO_CHAR = "^[^\\s]+$";
+    private final String regexpPatternSpaceNoChar = "^[^\\s]+$";
+    private final String regexPatternOnlyComma = "^,+$";
+
     public void validate(String input) {
-        isNameBelowFiveLetters(input);
-        isNameContainBlank(input);
+        isNameOnlyComma(input);
+        String[] carNames = input.split(",");
+        for(String name: carNames) {
+            System.out.printf(name);
+            isNameBelowFiveLetters(input);
+            isNameContainBlank(input);
+        }
     }
 
     public void isNameBelowFiveLetters(String input) {
@@ -17,8 +24,14 @@ public class CarNameInputValidator {
     }
 
     public void isNameContainBlank(String input){
-        if(!Pattern.matches(REGEXP_PATTERN_SPACE_NO_CHAR,input)){
+        if(!Pattern.matches(regexpPatternSpaceNoChar,input)){
             throw new IllegalArgumentException("공백이 포함되어 있거나 빈 문자열입니다.");
+        }
+    }
+
+    public void isNameOnlyComma(String input){
+        if (Pattern.matches(regexPatternOnlyComma, input)) {
+            throw new IllegalArgumentException("이름에 대한 정보가 없습니다.");
         }
     }
 }
