bf5841a6885d447e77b1630d86e4e952345b6ec8
pjaehyun <show6756@gmail.com>
2023-10-28 14:48:35 +0900

    게임 메세지 상수 클래스 추가

---
+++ src/main/java/constants/RacingCarConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class RacingCarConstants {
+    public static String PARTICIPANTS_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n";
+    public static String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요? \n";
+}
