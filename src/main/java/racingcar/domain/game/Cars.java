006ebf0205ba85c70d59dcae68011a4b56c6bad6
minisyu <iswater.kim@gmail.com>
2023-10-28 14:56:00 +0900

    refactor(Cars): 생성자 및 정적 팩토리 메소드 수정

--- src/main/java/racingcar/domain/game/Cars.java
+++ src/main/java/racingcar/domain/game/Cars.java
@@ -1,33 +1,28 @@
 package racingcar.domain.game;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.dto.CarDto;
 import racingcar.domain.dto.CarsDto;
-import racingcar.util.NumberPicker;
+import racingcar.util.MoveStrategy;
 
 public class Cars {
     private final List<Car> cars;
 
-    private Cars(String input) {
-        this.cars = convertToCarList(input);
+    private Cars(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public static Cars of(String input) {
-        return new Cars(input);
+    public static Cars of(List<Car> cars) {
+        return new Cars(cars);
     }
 
-    private List<Car> convertToCarList(String input) {
-        return Arrays.stream(input.split(","))
-                .map(Car::of)
-                .toList();
-    }
-
-    public CarsDto moveOnce(NumberPicker numberPicker) {
+    public CarsDto moveOnce(MoveStrategy moveStrategy) {
         List<CarDto> carDtoList = new ArrayList<>();
+        // ref. 일급 콜렉션에서 객체를 꺼내서 차량을 움직이는 비지니스 로직을 구현하고 있다
+        //  일급 콜렉션에세 메시지를 보내 차량을 이동시키도록 바꿔볼까?
         for (Car car : cars) {
-            CarDto carDto = car.moveOnce(numberPicker);
+            CarDto carDto = car.moveOnce(moveStrategy);
             carDtoList.add(carDto);
         }
         return new CarsDto(carDtoList);
