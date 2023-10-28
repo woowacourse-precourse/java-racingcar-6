8bf179a7339fd9d7e456d115d5ca47d3a0c725c5
minpyo <horaee@naver.com>
2023-10-28 17:24:25 +0900

    feat : 자동차 이름 입력 및 예외처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static String inputCarNames;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputCarNames = Console.readLine();
+        try {
+            String[] carNames = carNames(inputCarNames);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+
+    }
+
+    public static String[] carNames(String input) {
+        String[] carNames =  input.split(",");
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이내이어야 합니다.");
+            }
+        }
+        return carNames;
     }
 }
