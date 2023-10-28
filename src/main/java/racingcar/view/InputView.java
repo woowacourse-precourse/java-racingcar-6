9a5d00440b1185b9d9f94a18930980bb555b2dc2
김연진 <love_homebody@naver.com>
2023-10-28 17:44:08 +0900

    feat(InputView): 이름 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+
+    private static final String SPLIT_REGEX = ",";
+    public List<String> readNames() {
+        String input = Console.readLine();
+        return List.of(input.split(SPLIT_REGEX));
+    }
+}
