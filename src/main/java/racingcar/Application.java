7995cc751ec9191f134b4cc6f58b9da0fb4172bb
J-petto <hminji4565@gmail.com>
2023-10-28 20:30:26 +0900

    feat: 자동차 이름 및 게임 횟수 받아오기 & 자동차 이름 길이 검수.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
+        InputGame input = new InputGame();
+        input.setCarName();
+        input.setGameNumber();
+
+        Game game = new Game();
+        game.playGame(input.getCarName(), input.getGameCount());
     }
 }
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Game {
+    public void playGame(ArrayList<String> name, int count){
+        System.out.println(name.get(0));
+        System.out.println(name.get(1));
+        System.out.println(name.get(2));
+        System.out.println(count);
+    }
+}
