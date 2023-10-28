47795aa8c6659dbdb007c1416a7cea309a49fb9e
scarlet0o0 <ghd7673@naver.com>
2023-10-28 19:50:39 +0900

    Feat : 전체적인 출력문 수정

--- src/main/java/racingcar/view/OutView.java
+++ src/main/java/racingcar/view/OutView.java
@@ -1,6 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.dto.CarDto;
+
 public class OutView {
+
+    private final static String DEFAULT_TRAVEL_LENGTH = "-";
 
     public static void printCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -10,12 +16,20 @@ public class OutView {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void printRaceResults() {
+    public static void printRaceResults(List<CarDto> carDtoList) {
+        for(CarDto carDto : carDtoList){
+            System.out.printf("%s : %s \n", carDto.name(), DEFAULT_TRAVEL_LENGTH.repeat(carDto.travelDistance()));
+        }
+    }
+
+    public static void printFinalWinner(List<CarDto> carDtoList){
+        String winners = carDtoList.stream()
+                .map(CarDto::name)
+                .collect(Collectors.joining(", "));
+        System.out.printf("최종 우승자 : %s", winners);
+    }
+
+    public static void printResults() {
         System.out.println("실행 결과");
     }
-
-    public static void printFinalWinner(){
-        System.out.println("최종 우승자: ");
-    }
-
 }
