aa59868d1a046653dce6ff1011a0dfd3dbb20f5f
psy3720 <qkrtmddyd12@naver.com>
2023-10-28 13:39:58 +0900

    feat: 자동차 유효성 검사 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,26 @@
 package racingcar;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public class Car {
     private String name;
     private int movingCount;
 
     public Car(String name) {
+        validateName(name);
+
         this.name = name;
+        this.movingCount = 0;
+    }
+
+    private void validateName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Car(String name, int movingCount) {
@@ -14,7 +29,7 @@ public class Car {
     }
 
     public void moveAccordingToInput(int number) {
-        if(number >= 4) {
+        if (number >= 4) {
             this.moving();
         }
     }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,33 +1,44 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
     @Test
+    @DisplayName("값이 4이상이면 1번 전진한다")
     void 값이_4이상인경우_1번전진한다() {
-        // given
         Car car = new Car("pobi", 0);
 
-        // when
         car.moveAccordingToInput(4);
 
-        // then
         assertThat(car.getMovingCount()).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("값이 3이하이면 정지한다")
     void 값이_3이하인경우_정지한다() {
-        // given
         Car car = new Car("pobi", 0);
 
-        // when
         car.moveAccordingToInput(3);
 
-        // then
         assertThat(car.getMovingCount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차이름이 5자를 초과하면 에러가 발생한다.")
+    void 자동차이름_5자를_초과하면_에러발생() {
+        assertThatThrownBy(() -> new Car("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차이름을 입력하지 않으면 에러가 발생한다")
+    void 자동차이름을_입력하지_않으면_에러가_발생한다() {
+        assertThatThrownBy(() -> new Car(""))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
