f8a82d5d05b99ccaf1bfcc252b809d5bd6ed77e9
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-28 23:58:59 +0900

    refactor(TryNumberValidator): validate 내부로직을 메서드로 분리

--- src/main/java/racingcar/domain/validator/TryNumberValidator.java
+++ src/main/java/racingcar/domain/validator/TryNumberValidator.java
@@ -5,24 +5,33 @@ import racingcar.domain.validator.exception.TryNumberException;
 public class TryNumberValidator implements InputValidator {
     @Override
     public void validate(String input) {
-        int intValueInput = 0;
+        checkNull(input);
+        checkAmount(getIntValueFromInput(input));
+    }
 
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException(TryNumberException.NO_VALUE_TRY_NUMBER.getMessage());
-        }
-
-        try {
-            intValueInput = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(TryNumberException.INVALID_INTEGER_VALUE.getMessage());
-        }
-
+    private void checkAmount(int intValueInput) {
         if (intValueInput >=10) {
             throw new IllegalArgumentException(TryNumberException.INVALID_TOO_BIG_TRY_NUMBER.getMessage());
         }
 
         if (intValueInput <=1) {
             throw new IllegalArgumentException(TryNumberException.INVALID_TOO_SMALL_TRY_NUMBER.getMessage());
+        }
+    }
+
+    private int getIntValueFromInput(String input) {
+        int intValueInput;
+        try {
+            intValueInput = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(TryNumberException.INVALID_INTEGER_VALUE.getMessage());
+        }
+        return intValueInput;
+    }
+
+    private void checkNull(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(TryNumberException.NO_VALUE_TRY_NUMBER.getMessage());
         }
     }
 }
