edadfb4238421a36cae6b60dc1300ca32c20baca
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 01:54:38 +0900

    test : verify test for value is contain space

--- src/test/java/study/ValidationTest.java
+++ src/test/java/study/ValidationTest.java
@@ -18,4 +18,17 @@ public class ValidationTest {
             ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력값이 공백일 수는 없습니다.");
     }
+
+    @Test
+    void 입력_값_공백_포함_검증_로직_테스트(){
+        //given
+        String target = "pobi,woni, jun";
+
+        //when
+        assertThatThrownBy(()->{
+                    InputValidation.verifyForSpaceValue(target);
+                }
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력값에 공백이 포함되었습니다.");
+    }
 }
