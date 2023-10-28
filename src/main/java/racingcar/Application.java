0993972600abb3fb01b1017482ceb598ce364a5e
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-10-28 21:06:00 +0900

    feat: 추가 출력용 상수 메시지
    출력용으로 사용될 상수 메시지를 정의함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,15 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
+    private static final String INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRYTIME_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String FORWARD_MESSAGE = "-";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
