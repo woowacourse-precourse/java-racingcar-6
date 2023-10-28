e21344f3bc9bc3cb599b36ecf5217d6e9ca2f1b5
jhw1015 <jhw1015@bank-mall.co.kr>
2023-10-29 00:50:45 +0900

    feat : 시도, 자동차 이름을 요청하는 문구 출력하는 메서드 추가ˆ

--- src/main/java/racingcar/output/PrintService.java
+++ src/main/java/racingcar/output/PrintService.java
@@ -1,5 +1,11 @@
 package racingcar.output;
 
 public class PrintService {
+    public static void inputRequest() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
 
+    public static void askAttemtCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
