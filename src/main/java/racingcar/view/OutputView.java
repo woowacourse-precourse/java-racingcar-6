6ee3ed386278e986d84f2dc1d5b1f7d509dc5e16
doha <dohaahi@naver.com>
2023-10-28 22:21:16 +0900

    feat: 실행 결과 문구 출력 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import java.util.Map;
+import racingcar.domain.Car;
+import racingcar.util.Constants;
+
+public class OutputView {
+    public void printGameResult() {
+        System.out.println(Constants.GAME_RESULT_MESSAGE);
+    }
+
+    public void printMoveResult(Map<Car, Integer> moveResults) {
+        for (Car car : moveResults.keySet()) {
+            String position = converterPosition(car.getPosition());
+            System.out.printf("%s : %s\n", car.getName(), position);
+        }
+        System.out.println();
+    }
+
+    private String converterPosition(final int position) {
+        return "-".repeat(position);
+    }
+}
