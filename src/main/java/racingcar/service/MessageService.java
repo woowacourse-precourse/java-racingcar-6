ac26c55fc7e8bd02ba120fdc19c2bbd7414157dc
heejae <hjhjhjhj3770@naver.com>
2023-10-28 12:46:41 +0900

    Feat: MessageService 기능추가(service/MessageService)
    공통으로 출력되는 부분과 아닌부분으로 나눠 메서드를 만들었습니다.
    아닌부분은 사용자의 입력값에 따라 달라지며 나중에 입력값을 파라미터로 받게 수정할 것입니다.

--- src/main/java/racingcar/service/MessageService.java
+++ src/main/java/racingcar/service/MessageService.java
@@ -1,4 +1,24 @@
 package racingcar.service;
 
 public class MessageService {
+
+    // 공통처리
+    public void questionCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void questionTryNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    // 값마다 다른것
+    public void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printWinner() {
+        System.out.println("최종 우승자 :");
+    }
+
+
 }
