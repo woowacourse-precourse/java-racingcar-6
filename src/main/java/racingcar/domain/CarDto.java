9fd29828e52c3a3c9a0d1533ea1b7f3d23a486c5
elyudwo <dudqk9696@naver.com>
2023-10-28 17:15:51 +0900

    [Feat] : CarDto 객체 정적 팩토리 메서드 구현
    자동차 이름, 위치를 파라미터로 받아 CarDto 객체를 생성해 반환

--- src/main/java/racingcar/domain/CarDto.java
+++ src/main/java/racingcar/domain/CarDto.java
@@ -5,6 +5,15 @@ public class CarDto {
     private String name;
     private int distance;
 
+    private CarDto(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public static CarDto convertCarDto(String name, int distance) {
+        return new CarDto(name, distance);
+    }
+
     public String getName() {
         return name;
     }
