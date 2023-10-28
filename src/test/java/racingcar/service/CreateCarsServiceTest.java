27d2b882ad9317c75911b89865695559e463c650
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 14:44:47 +0900

    feat(CreateCarsService): 자동차들을 생성하는 기능 테스트 코드 작성

---
+++ src/test/java/racingcar/service/CreateCarsServiceTest.java
@@ -0,0 +1,26 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+public class CreateCarsServiceTest {
+	
+	
+	@DisplayName(", 구분으로 자동차의 이름들을 가진 Cars객체인지 확인한다.")
+	@ValueSource(strings = { "car1,car2,car3"})
+	@ParameterizedTest
+	void checkCreateCars(String str) {
+		CreateCarsService service = new CreateCarsService();
+		Cars cars = service.createCars(str);
+		
+		for(Car car : cars.getCars()) {
+			if(!str.contains(car.getCarName())) {
+				throw new IllegalArgumentException();
+			}
+		}		
+	}
+}
