bf2742d624dd06c381e87280902f4b6489855c46
hYe807n <rlagkdus55@gmail.com>
2023-10-28 21:44:07 +0900

    Feat: racing 반복 회수 설정 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,22 +1,28 @@
 package racingcar.controller;
 
-import static racingcar.view.InputView.inputCarName;
-import static racingcar.view.OutputView.printCarNameDemand;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.model.CarModel;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
     private List<CarModel> carModelList = new ArrayList<>();
+    private int repeatNumber;
 
     public void inputCarNames() {
-        printCarNameDemand();
-        List<String> carList = Arrays.asList(inputCarName().split(","));
+        OutputView.printCarNameDemand();
+        List<String> carList = Arrays.asList(InputView.inputCarName().split(","));
 
         for (String s : carList) {
             this.carModelList.add(new CarModel(s));
         }
     }
+
+    public void inputRepeatNumber() {
+        OutputView.printRepeatNumberDemand();
+        this.repeatNumber = Integer.parseInt(InputView.inputRepeatNumber());
+    }
+
 }
