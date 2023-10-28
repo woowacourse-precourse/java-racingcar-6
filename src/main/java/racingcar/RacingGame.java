bf781713a9627e003ef8634ba4dfa9a265454753
mmihye <smilehye910@naver.com>
2023-10-28 16:16:00 +0900

    FEAT/RacingGame 초기화

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,8 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static java.lang.Integer.parseInt;
 
@@ -10,5 +12,11 @@ public class RacingGame {
     private ArrayList<Car> carList = new ArrayList<>();
     private int gameNumber;
 
+    public RacingGame(){
+        InputValue inputValue = new InputValue();
+        carList = inputValue.getCarsName();
+        gameNumber = inputValue.getGameNumber();
+    }
+    
 }
 
