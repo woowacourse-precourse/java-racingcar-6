e7d81239fc8267d45a89785426c32b1cd89d9f27
WonyuChoi <phillip07290@naver.com>
2023-10-28 20:47:06 +0900

    feat: 게임 초기화 시에 입력을 받은 try count, cars 초기화

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import controller.dto.CarNames;
+import model.Cars;
+import model.TryCount;
 import view.InputView;
 import view.OutputView;
 
@@ -7,9 +10,31 @@ public class RacingGameController {
 
     private final OutputView outputView;
     private final InputView inputView;
+    private Cars cars;
+    private TryCount tryCount;
 
     public RacingGameController(final OutputView outputView, final InputView inputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        initGame();
+    }
+
+    private void initGame() {
+        initCars();
+        initTryCount();
+    }
+
+    private void initCars() {
+        outputView.askCarNames();
+        CarNames carNames = CarNames.of(inputView.readLine());
+        cars = carNames.toCars();
+    }
+
+    private void initTryCount() {
+        outputView.askTryCount();
+        tryCount = TryCount.from(inputView.readLine());
     }
 }
