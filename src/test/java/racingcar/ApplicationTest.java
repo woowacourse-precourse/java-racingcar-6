f91986d89f2600aa71f2c5c313595863802dd499
donghoony <aru0504@naver.com>
2023-10-29 03:21:41 +0900

    test(ApplicationTest): 단독 우승 및 다중 우승자 전체 테스트 작성
    - 한 명만 우승하는 경우, 여러 명이 우승하는 경우에 대해 모두 테스트 작성
    - 랜덤한 값을 고정시키기 위해 `assertRandomNumberInRangeTest`를 사용

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,11 +16,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -28,6 +29,31 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("단독 우승자 테스트")
+    public void testOneWinner() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("a,b", "2");
+                    assertThat(output()).contains("a : -", "b : ", "a : --", "b : -", "최종 우승자 : a");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    @DisplayName("다중 우승자 테스트")
+    public void testMultipleWinner() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("a,b,c", "2");
+                    assertThat(output()).contains("a : -", "b : -", "c : -", "a : --", "b : --", "c : --",
+                            "최종 우승자 : a, b, c");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
         );
     }
 
