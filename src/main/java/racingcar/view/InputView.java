8ddb2067c34e61132bfc2adc11b7e84f9b2071ac
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-29 02:49:04 +0900

    feat: 자동차 이름을 입력받는 method 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.InputValidator;
+
+public class InputView {
+
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public String inputCarNames() {
+        String input = Console.readLine();
+        checkEmptyAndBlank(input);
+        return input;
+    }
+
+    private void checkEmptyAndBlank(String input) {
+        inputValidator.validateInputEmpty(input);
+        inputValidator.validateInputBlank(input);
+    }
+}
