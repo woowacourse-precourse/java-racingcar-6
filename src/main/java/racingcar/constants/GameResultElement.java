a679766bed94383b599853cf8a76e8d4d77e1a65
GaBaljaintheroom <junsu1222@naver.com>
2023-10-29 00:56:38 +0900

    feat(GameResultElement): 게임 결과 요소 컴마 추가

--- src/main/java/racingcar/constants/GameResultElement.java
+++ src/main/java/racingcar/constants/GameResultElement.java
@@ -3,12 +3,17 @@ package racingcar.constants;
 public enum GameResultElement {
     COLON(" : "),
     DASH("-"),
+    COMMA(", "),
     ENTER("\n");
 
     private final String element;
 
     GameResultElement(String element) {
         this.element = element;
+    }
+
+    public String getElement() {
+        return element;
     }
 
     @Override
