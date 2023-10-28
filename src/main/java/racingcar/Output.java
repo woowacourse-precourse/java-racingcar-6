651134856843f80f6ef0d95d4a0b5f40c426892a
2021110806 <2021110806@dgu.ac.kr>
2023-10-29 01:02:04 +0900

    feat(Output.java): add is_last, printWinner method 최종 우승자 안내 문구를 출력하는 기능을 구현하였습니다. 우승자가 여러 명일때는 반점을 출력하지 않습니다.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,11 +1,13 @@
 package racingcar;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Output {
     private final String CAR_NAME_INPUT_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String TRY_NUMBER_INPUT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
     private final String RESULT_TITLE = "실행 결과";
+    private final String WINNERS = "최종 우승자 : ";
 
     void printCarNameInputRequest() {
         System.out.println(CAR_NAME_INPUT_REQUEST_MESSAGE);
@@ -22,6 +24,21 @@ public class Output {
     void printRacingMap(ArrayList<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName()+" : "+"-".repeat(car.getLocation()));
+        }
+    }
+
+    boolean is_last(String winner, ArrayList<String> winners) {
+        return winner.equals(winners.get(winners.size() - 1));
+    }
+
+    void printWinners(ArrayList<String> winners) {
+        System.out.print(WINNERS);
+        for (String winner : winners) {
+            if (is_last(winner, winners)) {
+                System.out.print(winner);
+                continue;
+            }
+            System.out.print(winner+", ");
         }
     }
 }
