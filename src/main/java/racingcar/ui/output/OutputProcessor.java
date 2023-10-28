98fec3efe59d1373aea39541510eda0c4e45f7a9
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 16:39:15 +0900

    feat(ui): 기능 별 print함수 추가

--- src/main/java/racingcar/ui/output/OutputProcessor.java
+++ src/main/java/racingcar/ui/output/OutputProcessor.java
@@ -1,15 +1,30 @@
 package racingcar.ui.output;
 
+import racingcar.ui.Message;
+
 public class OutputProcessor {
     public void printInput(String input) {
         System.out.println(input);
     }
 
+    public void printStartMessageCarNames() {
+        System.out.println(Message.START_MESSAGE_CAR_NAMES);
+    }
+
+    public void printStartMessageTryCount() {
+        System.out.println(Message.START_MESSAGE_TRY_COUNT);
+    }
+
+    public void printResult() {
+        System.out.println(Message.RESULT);
+    }
+
     public void printResult(String carName, int forwardNumber) {
-        System.out.println(carName+" "+": "+"-".repeat(forwardNumber));
+        System.out.println(carName + " : " + "-".repeat(forwardNumber));
     }
 
-    public void printStartMessage() {
-
+    public void printFinalWinner(String carNames) {
+        System.out.println(Message.FINAL_WINNER + carNames);
     }
+
 }
