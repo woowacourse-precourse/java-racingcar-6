c6ebc751a075330ab456bdd74278367aa6898ded
dltjdn <hana40004@naver.com>
2023-10-28 16:34:08 +0900

    feat(Car): 자동차 전진 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,8 +1,12 @@
 package racingcar.model;
 
+import racingcar.RandomNumberGenerator;
+
 public class Car {
-    private final String name;
-    private final int forwardCount;
+    private String name;
+    private int forwardCount;
+    private final RandomNumberGenerator generator = new RandomNumberGenerator();
+    private static final int FORWARD_LOWER_INCLUSIVE = 4;
     private static final int MAX_NAME_LENGTH = 5;
     private static final String NAME_LENGTH_ERROR = "이름은 5자 이하여야 합니다.";
 
@@ -15,6 +19,12 @@ public class Car {
     private void validateLength(String name) {
         if(name.length() > MAX_NAME_LENGTH){
             throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
+    }
+
+    private void forward(){
+        if(generator.generate() > FORWARD_LOWER_INCLUSIVE){
+            forwardCount++;
         }
     }
 
