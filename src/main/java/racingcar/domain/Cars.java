afa13537f20fd94c3a84b2b46df6673f3468956e
LastCow9000 <sys19912002@hanmail.net>
2023-10-29 00:47:03 +0900

    feat: 레이스 결과 문자열로 얻는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,10 +2,15 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class Cars {
     private static final String DUPLICATE_ERROR_MESSAGE = "이미 존재하는 자동차 이름입니다.";
+    private static final String INIT_VALUE = "";
+    private static final String SEPARATOR = " : ";
+    private static final String POSITION_SYMBOL = "-";
+    private static final String LINE_FEED = "\n";
     private final List<Car> cars;
 
     public Cars() {
@@ -19,6 +24,11 @@ public class Cars {
 
     public void move() {
         cars.forEach(Car::move);
+    }
+
+    public String getStringOfRaceResult() {
+        return cars.stream()
+                .reduce(INIT_VALUE, getParsedResultBiFunction(), String::join);
     }
 
     public int getMaxPosition() {
@@ -43,5 +53,16 @@ public class Cars {
                 .ifPresent(result -> {
                     throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
                 });
+    }
+
+    private BiFunction<String, Car, String> getParsedResultBiFunction() {
+        return (String result, Car car) ->
+                new StringBuilder()
+                        .append(result)
+                        .append(car.getName())
+                        .append(SEPARATOR)
+                        .append(POSITION_SYMBOL.repeat(car.getPosition()))
+                        .append(LINE_FEED)
+                        .toString();
     }
 }
