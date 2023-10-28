cc060dba52a68f5211d6322d5c7b75a058cd2628
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 13:33:03 +0900

    feat(ProgressMessage): 실행 결과 메시지 생성

--- src/main/java/racingcar/constants/ProgressMessage.java
+++ src/main/java/racingcar/constants/ProgressMessage.java
@@ -2,7 +2,8 @@ package racingcar.constants;
 
 public enum ProgressMessage {
     INPUT_RACING_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_ATTEMPT_COUNT("시도할 회수는 몇회인가요?");
+    INPUT_ATTEMPT_COUNT("시도할 회수는 몇회인가요?"),
+    EXECUTION_RESULT("실행 결과");
     private final String message;
 
     ProgressMessage(String message) {
