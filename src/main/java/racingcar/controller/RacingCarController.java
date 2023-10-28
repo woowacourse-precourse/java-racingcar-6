6605a08c887ba360e0a010c6fd9a43605c4ee22b
GaBaljaintheroom <junsu1222@naver.com>
2023-10-29 00:58:23 +0900

    feat(RacingCarController): 최종 우승자 출력 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -34,5 +34,6 @@ public class RacingCarController {
             outputView.printGameResult(cars.statusCarsPosition());
         }
 
+        outputView.printFinalWinners(cars.getFinalWinners());
     }
 }
