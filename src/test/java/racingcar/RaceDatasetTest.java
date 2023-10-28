b88dd903673d4c0186249e80c8426bc06acda063
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-28 15:16:57 +0900

    test: add a test to check if the dataset is correctly updated in order with random numbers

--- src/test/java/racingcar/RaceDatasetTest.java
+++ src/test/java/racingcar/RaceDatasetTest.java
@@ -4,13 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RaceDatasetTest {
-    String mockInput = "alpha,bravo,charlie";
+    String mockInput = "alpha,bravo,charlie\n5";
     private final InputStream inOriginal = System.in;
 
     @BeforeEach
@@ -31,9 +34,24 @@ public class RaceDatasetTest {
 
         machines.getInput();
 
-        LinkedHashMap<String, Integer> test = paceComputer.createDefaultPaceMap(machines);
+        LinkedHashMap<String, Integer> testMap = paceComputer.createDefaultPaceMap(machines);
 
-        assertThat(test.keySet()).containsExactly("alpha", "bravo", "charlie");
-        assertThat(test.values()).containsExactly(0, 0, 0);
+        assertThat(testMap.keySet()).containsExactly("alpha", "bravo", "charlie");
+        assertThat(testMap.values()).containsExactly(0, 0, 0);
+    }
+
+    @Test
+    public void updatePaceMapInOneLap() {
+        Machines machines = new Machines();
+        PaceComputer paceComputer = new PaceComputer();
+
+        machines.getInput();
+
+        LinkedHashMap<String, Integer> paceMap = paceComputer.updatePaceMap(machines);
+        List<Integer> valuesInMap = new ArrayList<>(paceMap.values());
+
+        List<Integer> generatedNumbers = paceComputer.getRandomNumberList();
+
+        assertThat(valuesInMap).isEqualTo(generatedNumbers);
     }
 }
