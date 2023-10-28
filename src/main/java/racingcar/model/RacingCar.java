0e1a71b4553d5a1103b2228a108abbeb85e955f7
hansg931 <hansg0509@cau.ac.kr>
2023-10-28 22:58:49 +0900

    feat: 자동차 속성 처리 메소드 작성

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,4 +1,26 @@
 package racingcar.model;
 
+import racingcar.util.Constant;
+
 public class RacingCar {
+	private final String name;
+	private int position = 0;
+
+	public RacingCar(String name) {
+		this.name = name;
+	}
+
+	public void moveForward() {
+		if (RandomMoveGenerator.isMovable()) {
+			position++;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
 }
