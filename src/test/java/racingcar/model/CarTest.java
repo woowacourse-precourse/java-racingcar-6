7aa39450565b0da0c14f9691ad4c5c4d41cdb674
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 16:16:59 +0900

    test: 테스트 코드 수정  - StringTest와 같이 컨벤션 맞춰, 상세한 테스트로 문제가 생긴 부분을 찾기 쉽게 변경  - docs변경

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,43 +3,50 @@ package racingcar.model;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
     private final String name = "벤츠";
+    private final Car benz = Car.from(name);
+    private static final int notMovedBenzProgress = 0;
 
     @Test
-    void moveSuccess() {
+    void move_4이상_전진() {
         final int moveNumber = 5;
-        final int notMoveNumber = 2;
-
-        final Car benz = Car.from(name);
         final Car movedBenz = benz.move(moveNumber);
+        final int movedBenzProgress = 1;
+
+        assertAll(
+            () -> assertThat(benz.getProgress()).isEqualTo(notMovedBenzProgress),
+            () -> assertThat(movedBenz.getProgress()).isEqualTo(movedBenzProgress),
+            () -> assertThat(benz).isNotSameAs(movedBenz)
+        );
+    }
+
+    @Test
+    void move_4미만_정지() {
+        final int notMoveNumber = 2;
         final Car notMovedBenz = benz.move(notMoveNumber);
 
-        final int movedBenzProgress = 1;
-        final int notMovedBenzProgress = 0;
-
         assertAll(
-            () -> assertThat(movedBenz.getProgress()).isEqualTo(movedBenzProgress),
             () -> assertThat(benz.getProgress()).isEqualTo(notMovedBenzProgress),
             () -> assertThat(notMovedBenz.getProgress()).isEqualTo(notMovedBenzProgress),
-            () -> assertThat(benz).isNotSameAs(movedBenz),
             () -> assertThat(benz).isNotSameAs(notMovedBenz)
         );
-
-
     }
-    @Test
-    void moveFail() {
-        Car benz = Car.from(name);
 
+    @Test
+    void move_0보다_작은_숫자인_경우_예외_발생() {
         final int negativeNumber = -1;
+
+        assertThatIllegalArgumentException().isThrownBy(() -> benz.move(negativeNumber));
+    }
+
+    @Test
+    void move_9보다_큰_숫자인_경우_예외_발생() {
         final int overRangeNumber = 10;
 
-        assertAll(
-            () -> assertThatIllegalArgumentException().isThrownBy(() -> benz.move(negativeNumber)),
-            () -> assertThatIllegalArgumentException().isThrownBy(() -> benz.move(overRangeNumber))
-        );
+        assertThatIllegalArgumentException().isThrownBy(() -> benz.move(overRangeNumber));
     }
 }
