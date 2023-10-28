7fbbedcd083715af24580fb2c86e54b13168ba84
OhJongMin <jmoh7480@gmail.com>
2023-10-28 17:12:09 +0900

    feat(racingcar):경주 자동차 객체 생성

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,20 +1,38 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
-    public final static int defaultValue = 0;
-    private String car;
+    private final String name;
     private int move;
     private int randomNumber;
 
-    public RacingCar(String car){
-        this.car = car;
-        this.move = defaultValue;
+    public RacingCar(String name){
+        this.name = name;
+        this.move = 0;
     }
 
-    public void makeRandomNumber(){
+    public String getCarName(){
+        return this.name;
     }
 
     public void moveForward(){
+        makeRandomNumber();
+        if (isOverFour(randomNumber)){
+            move += 1;
+        }
+    }
+
+    public void makeRandomNumber(){
+        randomNumber = Randoms.pickNumberInRange(Rule.minNumber, Rule.maxNumber);
+    }
+
+    public boolean isOverFour(int value){
+        if (value >= Rule.standardValue){
+            return true;
+        } else{
+            return false;
+        }
     }
 
     public int getMove(){
