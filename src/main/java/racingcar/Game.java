f6e14e36bf5384471e485edb932796432298d398
junest66 <chlwndks333@gmail.com>
2023-10-29 05:12:45 +0900

    feat(Game): 한 라운드마다 모든 자동차를 움직이려는 기능 추가

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.List;
+
+public class Game {
+
+    private List<Car> cars;
+    private int attemptCount;
+    private int currentRound;
+
+    public Game(List<Car> cars, int attemptCount) {
+        this.cars = cars;
+        this.attemptCount = attemptCount;
+        this.currentRound = 0;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void moveAllCarsOnce() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+}
