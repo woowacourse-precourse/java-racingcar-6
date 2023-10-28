d6b450396f214d9115a4b3d83fab446a9f3de034
hyeongjun im <limbs713@naver.com>
2023-10-28 13:01:37 +0900

    :sparkles: InputView class 변경
    parsing을 처리한 후 return하도록 변경.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,24 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_INPUT_MESSAGE ="시도할 회수는 몇회인가요?";
 
-    public static String inputName(){
+    public static List<String> inputName(){
         System.out.println(NAME_INPUT_MESSAGE);
-        return Console.readLine();
+        String[] split = Console.readLine().split(",");
+        return Arrays.stream(split)
+                .collect(Collectors.toList());
     }
 
-    private static String inputAttempt() {
+    public static int inputAttempt() {
         System.out.println(ATTEMPT_COUNT_INPUT_MESSAGE);
-        return Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 
 }
