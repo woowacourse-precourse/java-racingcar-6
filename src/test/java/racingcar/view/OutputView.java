b26239c8c065783b126b5faf697e14f389243057
hYe807n <rlagkdus55@gmail.com>
2023-10-28 20:03:30 +0900

    Feat: 자동차 이름을 입력받기 위한 출력문 기능

---
+++ src/test/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+    public static final String DEMAND_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static void printCarNameDemand() {
+        System.out.println(DEMAND_CAR_NAME);
+    }
+
+}
