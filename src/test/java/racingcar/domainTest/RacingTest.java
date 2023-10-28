56257108a51872daa4ee0d61644b4a8e591da6ca
lzlkolo <lzlko1209@gmail.com>
2023-10-28 15:09:58 +0900

    test: Update RacingTest class by game logic change

--- src/test/java/racingcar/domainTest/RacingTest.java
+++ src/test/java/racingcar/domainTest/RacingTest.java
@@ -2,12 +2,10 @@ package racingcar.domainTest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import racingcar.Car;
 import racingcar.domain.Race;
-import racingcar.util.NumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,38 +14,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class RacingTest {
-    @InjectMocks
+    @Mock
     private Race race;
 
     @Mock
-    private NumberGenerator numberGenerator;
+    private Car car;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         MockitoAnnotations.openMocks(this);
+        car = new Car("tayo");
     }
 
     @Test
     void 이동_횟수만큼_경주_진행() {
         List<Car> cars = new ArrayList<>();
+        int moveCount = 5;
+
         cars.add(new Car("tayo"));
         cars.add(new Car("gani"));
 
-        int moveCount = 5;
-        race.racing(cars, moveCount);
-        numberGenerator = mock(NumberGenerator.class);
-        verify(numberGenerator, times(5));
+        for (int i = 0; i < moveCount; i++) {
+            race.racing(cars);
+        }
+
+        verify(race, times(5)).racing(cars);
     }
 
     @Test
     void 랜덤번호가_4_이상인_경우_전진() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("tayo"));
-
-        when(numberGenerator.createRandomNumber()).thenReturn(1, 2, 3, 4, 5);
-        race.racing(cars, 5);
-        for (Car car : cars) {
-            assertEquals(2, car.getStatus().length());
-        }
+        car.move(4);
+        assertEquals("-", car.getStatus());
     }
 }
