7aa097371e295b5f8ff1306b7da7f3fe27877c8c
bin <gimgyubin1225@gmail.com>
2023-10-28 17:43:32 +0900

    Test: 자동차 전진 기능 테스트 수정
    전진기능 수정으로 인한 테스트 수정

--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -7,8 +7,14 @@ import racingcar.domain.Car;
 public class CarTest {
 
     @Test
-    void 자동차_전진_기능(){
+    void 자동차_전진_기능_4미만이면_정지() {
         Car car = new Car("car1");
-        Assertions.assertEquals(1,  car.movesForward());
+        Assertions.assertEquals(0, car.moves(0));
+    }
+
+    @Test
+    void 자동차_전진_기능_4이상이면_전진() {
+        Car car = new Car("car1");
+        Assertions.assertEquals(1, car.moves(9));
     }
 }
