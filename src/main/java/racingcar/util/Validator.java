8d41be1f92190ab1b19b593d6dd07a69e60dcd7d
Lee Yeon Su <yslee0540@gmail.com>
2023-10-28 17:39:08 +0900

    feat(Validator): 자동차 이름 공백 확인기능 추가

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -7,19 +7,32 @@ import java.util.List;
 public class Validator {
     private static final int CAR_NAME_LENGTH_MIN = 1;
     private static final int CAR_NAME_LENGTH_MAX = 5;
-    private final static String WRONG_LENGTH_ERROR_MESSAGE = "1~5글자인 자동차 이름만 입력 가능합니다.";
+    private static final String WRONG_LENGTH_ERROR_MESSAGE = "1~5글자인 자동차 이름만 입력 가능합니다.";
+    private static final String SPACE = " ";
+    private static final String SPACE_ERROR_MESSAGE = "자동차 이름에는 공백이 있으면 안됩니다.";
 
     public List<String> convertStringToList(String input) {
         ArrayList<String> carNames = new ArrayList<>(Arrays.asList(input.split(",")));
-        isRightNameLength(carNames);
+        validate(carNames);
         return carNames;
     }
 
-    public void isRightNameLength(List<String> carNames) {
+    public void validate(List<String> carNames) {
         for (String carName : carNames) {
-            if (carName.length() < CAR_NAME_LENGTH_MIN || carName.length() > CAR_NAME_LENGTH_MAX) {
-                throw new IllegalArgumentException(WRONG_LENGTH_ERROR_MESSAGE);
-            }
+            isRightNameLength(carName);
+            hasSpace(carName);
+        }
+    }
+
+    public void isRightNameLength(String carName) {
+        if (carName.length() < CAR_NAME_LENGTH_MIN || carName.length() > CAR_NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException(WRONG_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    public void hasSpace(String carName) {
+        if (carName.contains(SPACE)) {
+            throw new IllegalArgumentException(SPACE_ERROR_MESSAGE);
         }
     }
 
