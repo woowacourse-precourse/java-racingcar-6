884f3d48b3afabad84c19f8cbfbed79928d4ea81
WinterHana <haha920@naver.com>
2023-10-28 18:37:16 +0900

    feat(Car) : moveRandom() 추가 및 STEP_PERCENTAGE 상수 추가
    - 일정 확률로 이동을 가능하게 하는 moveRandom() 추가
    - 확률을 조정해주는 상수 STEP_PERCENTAGE 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     String name;
     int distance;
+    final int STEP_PERCENTAGE = 4;  // 자동차가 이동할 확률 조정. 0~9까지 1씩 오를수록 10% 확률 감소.
 
     Car(String name) {
         this.name = name;
@@ -17,5 +20,12 @@ public class Car {
             distanceStr.append("-");
         }
         return distanceStr.toString();
+    }
+
+    void moveRandom() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        if(randomNumber >= STEP_PERCENTAGE) {
+            distance = distance + 1;
+        }
     }
 }
