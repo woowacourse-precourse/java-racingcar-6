4fedcdfa900d85a17d4c7cf9fbeacbb5b7d79fe4
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 13:45:06 +0900

    feat(ErrorMessage): 자동차 최대 위치값 에러 메시지 생성

--- src/main/java/racingcar/constants/ErrorMessage.java
+++ src/main/java/racingcar/constants/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     DIVISION_CAR_NAME_ERROR("이름은 쉼표(,) 기준으로 구분해야 합니다."),
     CAR_NAME_LENGTH_ERROR("자동차 이름은 5자 이하만 가능합니다."),
     CARS_COUNT_ERROR("자동차 이름을 2개 이상 입력해주세요"),
-    INPUT_NUMERIC_ERROR("숫자를 입력해 주세요");
+    INPUT_NUMERIC_ERROR("숫자를 입력해 주세요"),
+    NOT_FOUND_MAX_CAR_POSITION("레이싱카의 최대 위치를 찾지 못했습니다.");
 
     private final String message;
 
