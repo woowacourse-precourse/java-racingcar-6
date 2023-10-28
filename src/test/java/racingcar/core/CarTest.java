b29d618fba00e91301c617b4b2230909c0a56f43
gengminy <kls1998@naver.com>
2023-10-28 15:42:23 +0900

    test: car 테스트 정의

--- src/test/java/racingcar/core/CarTest.java
+++ src/test/java/racingcar/core/CarTest.java
@@ -1,2 +1,61 @@
-package racingcar.core;public class CarTest {
+package racingcar.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void car_생성자_테스트() {
+        String name = "pobyy";
+        Car car = new Car(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void car_생성자_이름_null_대입시_예외_반환() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car(null);
+        });
+    }
+
+    @Test
+    void car_생성자_이름_길이_5_초과시_예외_반환() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car("abcdefg");
+        });
+    }
+
+    @Test
+    void car_생성자_이름_길이_0_이하시_예외_반환() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car("");
+        });
+    }
+
+    @Test
+    void car_생성자_이름_공백시_예외_반환() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car("   ");
+        });
+    }
+
+    @Test
+    void move_메서드_테스트() {
+        Car car = new Car("pobi");
+        for (int i = 0; i < 50000; i++) {
+            car.move();
+        }
+        assertThat(car.getForwardCount()).isGreaterThan(0);
+    }
+
+    @Test
+    void getCurrentStatusString_메서드_테스트() {
+        Car car = new Car("pobi");
+        for (int i = 0; i < 50000; i++) {
+            car.move();
+        }
+        assertThat(car.getCurrentStatusString()).contains("pobi", "-");
+    }
 }
