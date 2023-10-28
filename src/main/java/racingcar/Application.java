dbfba14b8bf2f5888b57d8c44c51fc9c5782e4b4
DryLeonhard <ygangmin@gmail.com>
2023-10-29 00:16:46 +0900

    Test : 자동차 생성까지 테스트

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.game.GameService;
+
 public class Application {
+    private static final GameService gameService = GameService.getInstance();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameService.runRace();
     }
 }
