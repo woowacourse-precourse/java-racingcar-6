1d52ce6309d28c80914811518a3228a5665d41ba
김민정 <mjeong@sookmyung.ac.kr>
2023-10-28 10:58:36 +0900

    feat: 무작위 값이 전진 조건에 부합하면 전진하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private String distance = "";
+
+    private void moveOnRandomChance() {
+        int randomNumber = generateRandomNumber();
+        if (isForward(randomNumber)) {
+            distance += "-";
+        }
+    }
 
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
