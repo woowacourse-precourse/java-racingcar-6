e29e381cc4748130b52dae8e880f5bdcb102c35a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 03:40:30 +0900

    feat(Name): 자동차 이름 유효성 검사 기능 구현

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,2 +1,24 @@
-package racingcar.domain.car;public class Name {
+package racingcar.domain.car;
+
+import java.util.regex.Pattern;
+
+public class Name {
+    private static final String NAME_REGEX = "^[a-zA-Z가-힣]{1,5}$";
+    private static final Pattern pattern = Pattern.compile(NAME_REGEX);
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (isNotMatchValidNameCondition(name)) {
+            throw new IllegalArgumentException("이름은 공백 없는 1~5자의 영어 또는 한글이어야 합니다.");
+        }
+    }
+
+    private boolean isNotMatchValidNameCondition(String name) {
+        return !pattern.matcher(name).matches();
+    }
 }
