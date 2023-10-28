b4776b15cb916bdef9447fda53a02feaf5936b20
hyeongjun im <limbs713@naver.com>
2023-10-28 14:30:37 +0900

    :sparkles: run 메소드에 기능 추가
    attemptNum의 값을 inputview를 통해 입력받음
    attemptNum의 값만큼 playGame 메소드 호출

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import racingcar.model.service.GameService;
+import racingcar.view.InputView;
 
 public class GameController {
     private final GameService gameService;
@@ -9,7 +10,13 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    public void run(){
+    public void run() {
+        int attemptNum = InputView.inputAttempt();
+        gameService.createCars();
 
+        while (attemptNum == 0) {
+            gameService.playGame();
+            attemptNum--;
+        }
     }
 }
