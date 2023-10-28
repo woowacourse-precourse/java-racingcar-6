66c908c8012aeba41c8b08cb66d6e3751a4cc40d
waveofmymind <sjun0913@gmail.com>
2023-10-28 19:25:49 +0900

    feat: 게임을 진행하는 비즈니스 로직의 클래스를 구현한다.

---
+++ src/main/java/racingcar/application/RacingCarService.java
@@ -0,0 +1,29 @@
+package racingcar.application;
+
+import racingcar.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarService {
+
+    private final Navigator navigator;
+
+    public RacingCarService(Navigator navigator) {
+        this.navigator = navigator;
+    }
+
+    public GameResult gameStart(final List<String> names, final int tryCount) {
+        Cars cars = CarFactory.createCars(names);
+        NumberOfRound numberOfRound = new NumberOfRound(tryCount);
+
+        List<RoundHistory> gameHistories = new ArrayList<>();
+
+        for (int i = 0; i < numberOfRound.getValue(); i++) {
+            cars.moveAllCars(navigator);
+            RoundHistory roundHistory = new RoundHistory(new NumberOfRound(i + 1), cars.getCurrentStatus());
+            gameHistories.add(roundHistory);
+        }
+        return new GameResult(gameHistories);
+    }
+}
