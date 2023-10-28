0b2ba89ee8b8bf8fae0f1e2584560bcc5dd574e0
박정현 <ros28081@naver.com>
2023-10-28 17:30:35 +0900

    fix: 불필요 기능 삭제

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,8 +6,6 @@ import java.util.LinkedHashMap;
 
 public class RacingGame {
 
-    private static final int MAX_NAME_SIZE = 5;
-
     private static final String STARTING_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPTING_PHRASE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_PHRASE = "실행 결과";
@@ -20,42 +18,13 @@ public class RacingGame {
         System.out.println(STARTING_PHRASE);
     }
 
-    public void playGame() {
-
-    }
-
-    public String getInput() {
-        String input = Console.readLine().trim();
-        return input;
-    }
-
-    public LinkedHashMap<String, Integer> addHashMapWithNames(String input) {
-        String[] names = input.split(",");
-        racingCars = new LinkedHashMap<>();
-
-        for (String name : names) {
-            name = name.trim();
-            checkValidCarName(name);
-            racingCars.put(name, 0);
-        }
-
-        return racingCars;
-    }
-
-    public void checkValidCarName(String name) {
-        if(name.length() > MAX_NAME_SIZE) {
-            throw new IllegalArgumentException("이름은 5글자 이하여야 합니다.");
-        }
+    public Car[] getInput() {
+        String[] carNames = Console.readLine().split(",");
     }
 
     public void getAttemptingNum() {
         System.out.println(ATTEMPTING_PHRASE);
         attemptingNum = Integer.parseInt(Console.readLine());
-    }
-
-    public int generateRandomNumber() {
-        int randomNum = Randoms.pickNumberInRange(0, 9);
-        return randomNum;
     }
 
 }
