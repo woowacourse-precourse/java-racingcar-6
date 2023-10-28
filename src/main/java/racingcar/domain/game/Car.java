a3fe03840797d71cc5cc98aa08f3f09ef22329e9
minisyu <iswater.kim@gmail.com>
2023-10-28 15:52:59 +0900

    refactor(Car): 멤버 변수 String -> CarName으로 변경, 검증 로직 제거

--- src/main/java/racingcar/domain/game/Car.java
+++ src/main/java/racingcar/domain/game/Car.java
@@ -1,31 +1,14 @@
 package racingcar.domain.game;
 
-import racingcar.domain.dto.CarDto;
-import racingcar.util.NumberPicker;
-
 public class Car {
-    private final String name;
-    private int position = 0;
+    private final CarName carName;
+    private final int position = 0;
 
-    private Car(String name) {
-        validateNumberLength(name);
-        this.name = name;
+    private Car(CarName carName) {
+        this.carName = carName;
     }
 
-    public static Car of(String name) {
-        return new Car(name);
-    }
-
-    private void validateNumberLength(String name) {
-        if (name == null || name.isEmpty() || name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-        }
-    }
-
-    public CarDto moveOnce(NumberPicker numberPicker) {
-        if (numberPicker.pick() >= 4) {
-            position++;
-        }
-        return new CarDto(name, position);
+    public static Car of(CarName carName) {
+        return new Car(carName);
     }
 }
