22e3e13182bc8f0a34648a310a75943fbac60b75
leedohyun <lucky77795@naver.com>
2023-10-28 18:06:50 +0900

    refactor(racingCar) : inputView에서 inputValidator를 사용하도록 수정.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,27 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
 
 public class InputView {
-    public String inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
-        return Console.readLine();
+
+    private InputValidator inputValidator;
+
+    public InputView() {
+        this.inputValidator = new InputValidator();
     }
 
-    public String inputTryCount() {
+    public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+        inputValidator.validateInputCarName(carName);
+        return carName;
+    }
+
+    public Integer inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        String tryCount = Console.readLine();
+        inputValidator.validateInputTryCount(tryCount);
+        return Integer.parseInt(tryCount);
     }
 }
