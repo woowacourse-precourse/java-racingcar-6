09bbf6e56a3383397e3fe0a4203c9534efc47942
Chenowoo <cjsgusdn14@naver.com>
2023-10-28 16:31:05 +0900

    feat(Carracing): 자동차들의 초기값을 설정하는 기능 추가  - 자동차들의 이름과 초기값을 Map에 저장

--- src/main/java/racingcar/Carracing.java
+++ src/main/java/racingcar/Carracing.java
@@ -2,13 +2,16 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static racingcar.NumberConst.*;
 import static racingcar.InputValue.*;
+import static racingcar.PrintUtil.*;
 
 public class Carracing {
+
+    public static Map<String, String> raceScore;
     public static void startRacing() {
 
         InputSettingValue();
@@ -18,16 +21,26 @@ public class Carracing {
         //carNameList
     }
 
-    public static void goStop(String[] carNameList) {
-
-        Map<String, String> raceScore = new HashMap<>();
+    public static void goStop(Map<String, String> raceScore) {
 
         for(int i=0; i<carNameList.length; i++){
             int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE_NUMBER, FINAL_INCLUSIVE_NUMBER);
 
             if (randomNumber >= STANDARD_MOVING_NUMBER) {
-                raceScore.put(carNameList[i], raceScore.getOrDefault(carNameList[i], "") + "-");
+                raceScore.put(carNameList[i], raceScore.get(carNameList[i]) + "-");
             }
         }
+        printGameResultMsg(raceScore);
+    }
+
+    public static Map<String, String> setInitStartPoint(String[] carNameList) {
+
+        raceScore = new LinkedHashMap<>();
+
+        for(int i=0; i<carNameList.length; i++) {
+            raceScore.put(carNameList[i], "");
+        }
+
+        return raceScore;
     }
 }
