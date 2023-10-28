a97698cd667a7d8ff5c19b95763ff4e7a0b9aa65
jinsu868 <mjinsu098@gmail.com>
2023-10-28 16:54:23 +0900

    feat(referee): 우승자를 반환하는 기능 구현

---
+++ src/main/java/racingcar/domain/Referee.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Referee {
+    public List<Car> getWinner(List<Car> cars) {
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = getMaxPosition(cars);
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+        return maxPosition;
+    }
+}
