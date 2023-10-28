9b96eaa73338f89af65e8645216898782312e90c
GumaLee <rkdxhd97@naver.com>
2023-10-28 23:49:01 +0900

    feature: 게임진행 기능

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.ViewInput;
+import racingcar.view.ViewOutput;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Game {
+
+    private ViewOutput viewOutput = new ViewOutput();
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public List<Car> playOneRound(List<Car> cars) {
+        for(Car car : cars) {
+            car.move(getRandomNumber());
+        }
+        return cars;
+    }
+
+    public List<Car> playAllRounds(List<Car> cars, int numberOfRounds) {
+        for(int gameCount = 0; gameCount < numberOfRounds; gameCount++) {
+            viewOutput.printRoundResult(playOneRound(cars));
+        }
+        System.out.println();
+        return cars;
+    }
+
+    public List<Integer> makeLocationList(List<Car> cars) {
+        List<Integer> carLocations = new ArrayList<>();
+
+        for(Car car : cars) {
+            carLocations.add(car.getCurrentLocation());
+        }
+        return carLocations;
+    }
+
+    public List<Car> findWinner(List<Car> cars) {
+        List<Integer> carLocations = makeLocationList(cars);
+        int maxLocation = Collections.max(carLocations);
+
+        List<Car> winnerCars = new ArrayList<>();
+
+        for(Car car: cars) {
+            if(car.getCurrentLocation() == maxLocation)
+                winnerCars.add(car);
+        }
+
+        return cars;
+    }
+}
