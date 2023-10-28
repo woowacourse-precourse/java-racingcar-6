71758474d9aa44bdd2696b8b6834f2c68e55792b
yuseonjun <seonjun0906@gmail.com>
2023-10-28 17:00:48 +0900

    feat: 시도횟수 만큼 결과를 출력한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,19 @@
 package racingcar.view;
 
+import racingcar.domain.Cars;
+
 public class OutputView {
+
+    private static final String MOVE_MESSAGE = "-";
+    private static final String LINE_BREAK = System.lineSeparator();
 
     public static void printPlayResult() {
         System.out.println("실행 결과");
+    }
+
+    public static void printResult(Cars cars) {
+        cars.getCars()
+                .forEach(car -> System.out.println(car.getName() + " : " + MOVE_MESSAGE.repeat(car.getPosition())));
+        System.out.print(LINE_BREAK);
     }
 }
