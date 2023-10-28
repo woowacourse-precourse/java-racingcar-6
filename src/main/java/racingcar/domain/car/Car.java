413222278a0a03deaf1f8f2eec0de10af100eb96
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 03:52:52 +0900

    feat(Car): 자동차 이름 반환하는 기능 구현

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,2 +1,15 @@
-package racingcar.domain.car;public class Car {
+package racingcar.domain.car;
+
+public class Car {
+    private final Name name;
+    private final int position;
+
+    public Car(String name) {
+        this.name = new Name(name);
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name.toString();
+    }
 }
