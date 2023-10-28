34aded195454b4c620307c4e8cf61297483ab2dd
dong2ast <kdh01132006@naver.com>
2023-10-28 19:48:57 +0900

    ✨ feat: 게임 실행 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.service.GameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameService.run();
     }
 }
