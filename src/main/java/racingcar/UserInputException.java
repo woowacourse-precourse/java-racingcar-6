be3d49928b87af75766c1cb639682726ae985650
zzawang <hobin0116@naver.com>
2023-10-28 10:05:07 +0900

    refactor: 자동차 이름 중복 검사 개선
    - 자동차 이름의 중복을 검사하는 duplicateCarName 메소드를 개선하였다.
        - 전: 자동차의 이름이 중복될 때 마다 개수를 세어 반환
        - 후: 자동차의 이름이 중복되는 즉시 true를 반환
    - 이에 따라 반환하는 자료형을 long에서 boolean으로 변경하였다.
    - 이에 따라 주석 또한 맞게 변경하였다.
    - 이에 따라 duplicateCarName을 호출하는 validateUniqueCarName의 if문을 수정하였다.
        - 전: 반환값이 0 이상이면 IllegalArgumentException 발생
        - 후: 반환값이 True면 IllegalArgumentException 발생
    
    아래는 개선된 duplicateCarName 메소드의 중복 검사 부분이다.
    
    Before:
    
    return Arrays.stream(carNames)
            .filter(carName -> !uniqueCarNames.add(carName))
            .count();
    
    After:
    
    return Arrays.stream(carNames)
            .anyMatch(carName -> !uniqueCarNames.add(carName));

--- src/main/java/racingcar/UserInputException.java
+++ src/main/java/racingcar/UserInputException.java
@@ -7,18 +7,17 @@ import java.util.Set;
 public class UserInputException {
     public void validateUniqueCarName(String[] carNames) {
         // 자동차 이름 중복 검사
-        if (duplicateCarName(carNames) > 0) {
+        if (duplicateCarName(carNames)) {
             throw new IllegalArgumentException("자동차들의 이름 중 중복된 이름이 존재합니다. 게임이 종료됩니다.");
         }
     }
 
-    private long duplicateCarName(String[] carNames) {
+    private boolean duplicateCarName(String[] carNames) {
         Set<String> uniqueCarNames = new HashSet<>();
 
-        // 중복되는 자동차 이름의 개수를 센다.
+        // 중복된 값이 Set에 추가되지 않으면 true를 반환
         return Arrays.stream(carNames)
-                .filter(carName -> !uniqueCarNames.add(carName))
-                .count();
+                .anyMatch(carName -> !uniqueCarNames.add(carName));
     }
 
     public void validateBlankCarName(String[] carNames) {
