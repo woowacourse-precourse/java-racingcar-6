4e31095ffb2419961db85912e0dd95d6c8a04999
Cycrypto <jh01love@naver.com>
2023-10-28 21:49:58 +0900

    feature/game-flow:레이싱카 클래스에 거리 속성 추가

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -2,7 +2,11 @@ package racingcar.model;
 
 public class RacingCar {
     private String name;
+    private int distance;
     public RacingCar(String name){
         this.name = name;
+    }
+    public void moveForward(int distance){
+        this.distance += distance;
     }
 }
