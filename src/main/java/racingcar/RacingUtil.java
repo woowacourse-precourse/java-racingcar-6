636875c0393ca1ef985b1897ab9089bc98771dfe
Oseuseu <lakingsung@gmail.com>
2023-10-29 01:17:46 +0900

    feat: createCarsFromNames 메서드 구현

--- src/main/java/racingcar/RacingUtil.java
+++ src/main/java/racingcar/RacingUtil.java
@@ -2,8 +2,19 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingUtil {
     public static int generateRandomPower() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public static List<Car> createCarsFromNames(List<String> nameList) {
+        List<Car> carList = new ArrayList<>();
+        for (String name : nameList) {
+            carList.add(new Car(name));
+        }
+        return carList;
     }
 }
