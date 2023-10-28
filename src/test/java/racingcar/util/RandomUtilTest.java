c12fcbd8cc9aed4fac252a252a783558c4a02ff8
otaeho <dhxogh123123@naver.com>
2023-10-28 16:38:16 +0900

    refactor(RandomUtilTest): 변경된 상수 클래스명 적용

--- src/test/java/racingcar/util/RandomUtilTest.java
+++ src/test/java/racingcar/util/RandomUtilTest.java
@@ -1,18 +1,18 @@
 package racingcar.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.constant.Constant;
+import racingcar.constant.TestConstant;
 
 class RandomUtilTest {
     @Test
     @DisplayName("0과 9 사이의 정수 생성 테스트")
     void createRandomNumberInRangeTest() {
-        int rndNum = RandomUtil.createRandomNumberInRange(Constant.MIN_RANDOM_NUMBER, Constant.MAX_RANDOM_NUMBER);
+        int rndNum = RandomUtil.createRandomNumberInRange(TestConstant.MIN_RANDOM_NUMBER,
+                TestConstant.MAX_RANDOM_NUMBER);
 
-        assertTrue(Constant.MIN_RANDOM_NUMBER <= rndNum && rndNum <= Constant.MAX_RANDOM_NUMBER);
+        assertTrue(TestConstant.MIN_RANDOM_NUMBER <= rndNum && rndNum <= TestConstant.MAX_RANDOM_NUMBER);
     }
 }
