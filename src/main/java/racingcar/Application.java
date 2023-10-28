98b18047377d752fd719a6b015fabaa986bc767d
Jinnie Kim <jinniek48@gmail.com>
2023-10-28 22:16:14 +0900

    feat(racingcar): 자동차 경주 게임 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import racingcar.domain.Leaderboard;
+import racingcar.domain.NamesManager;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        NamesManager namesManager = new NamesManager();
+        List<String> namesList = namesManager.separateNamesString(Console.readLine());
+
+        Leaderboard leaderboard = new Leaderboard(namesList);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        int trialCount = Integer.parseInt(Console.readLine());
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        while (trialCount>0){
+
+            for (String name : namesList){
+                if (new Car().engine()){
+                    leaderboard.board.put(name, leaderboard.board.get(name)+1);
+                }
+            }
+
+            leaderboard.status();
+            trialCount--;
+        }
+
+        leaderboard.winner();
     }
+
 }
