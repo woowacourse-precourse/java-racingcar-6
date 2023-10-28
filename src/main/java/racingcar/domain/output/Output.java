190ba99ae9db0ef453bc74bdac5fd196a147e011
jinchiim <nellroll@syuin.ac.kr>
2023-10-29 05:07:46 +0900

    feat: Output 도메인 구현
    Output 도메인 구현
    - Output이 출력이라는 일만 하도록 구현 1주차 피드백 적용
    - enum으로 메시지 관리

--- src/main/java/racingcar/domain/output/Output.java
+++ src/main/java/racingcar/domain/output/Output.java
@@ -2,7 +2,19 @@ package racingcar.domain.output;
 
 public class Output {
 
-    public void printMessage(OutputMessage outputMessage){
-        System.out.println(outputMessage);
+    public static void printMessage(OutputMessage outputMessage){
+        System.out.println(outputMessage.message);
+    }
+
+    public static void printMessage(OutputMessage outputMessage, String messageValue){
+        System.out.printf((outputMessage.message) + "%n", messageValue);
+    }
+
+    public static void printResultMessage(OutputMessage outputMessage, String name, String distance){
+        System.out.printf((outputMessage.message) + "%n", name, distance);
+    }
+
+    public static void printNewLine(){
+        System.out.println();
     }
 }
--- src/main/java/racingcar/domain/output/OutputMessage.java
+++ src/main/java/racingcar/domain/output/OutputMessage.java
@@ -4,10 +4,11 @@ public enum OutputMessage {
 
     PLEASE_ENTER_CAR_NAME("경주할 %s 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     HOW_MANY_TIME("시도할 회수는 몇회인가요?"),
-    PLAY_RESULT("실행 결과"),
-    FINAL_WINNER("최종 우승자 : ");
+    EXECUTION_RESULT("실행 결과"),
+    FINAL_WINNER("최종 우승자 : %s"),
+    PROGRESS_RESULT("%s : %s");
 
-    private final String message;
+    final String message;
 
     OutputMessage(String message){
         this.message = message;
