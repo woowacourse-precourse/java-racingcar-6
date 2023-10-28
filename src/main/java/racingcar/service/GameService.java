0c22f8e1cc7e4e4a16e7d36a41c67b7945a6dcb1
dong2ast <kdh01132006@naver.com>
2023-10-28 21:16:56 +0900

    ♻️ refactor: 차 이름 유효성 검증과 동시에 객체 생성하도록 변경

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -10,11 +10,10 @@ public class GameService {
     public static void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
-        List<String> carNameList = new ArrayList<>();
+        List<Car> carList = new ArrayList<>();
 
-        addValidCarName(carNames, carNameList);
+        addValidCarName(carNames, carList);
 
-        List<Car> carList = carNameList.stream().map(Car::new).toList();
         System.out.println("시도할 회수는 몇회인가요?");
         try {
             int tryNum = Integer.parseInt(Console.readLine());
@@ -31,12 +30,15 @@ public class GameService {
         }
     }
 
-    private static void addValidCarName(String carNames, List<String> carNameList) {
+    private static void addValidCarName(String carNames, List<Car> carList) {
+        List<String> carNameList = new ArrayList<>();
+
         for (String s : carNames.split(",")) {
             if (checkNameValidation(carNameList, s)) {
                 throw new IllegalArgumentException();
             }
             carNameList.add(s);
+            carList.add(new Car(s));
         }
     }
 
