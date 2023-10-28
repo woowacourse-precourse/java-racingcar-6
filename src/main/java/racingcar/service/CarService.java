50e782558a8de68f85ea4a6fb5e194cfe99c74e0
박성춘 <songchon100@gmail.com>
2023-10-28 13:28:42 +0900

    feat : Car name을 이용하여 Car와 Cars 객체 생성기능 구현

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,4 +1,33 @@
 package racingcar.service;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarService {
+    private Cars cars;
+
+    private CarService() {
+
+    }
+
+    public static CarService getInstance() {
+        return new CarService();
+    }
+
+    public void generateCarAndCarsInstanceFromCarNames(String carNames) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : splitCarNames(carNames)) {
+            carList.add(Car.getInstance(carName));
+        }
+        cars = Cars.getInstance(carList);
+    }
+
+    private String[] splitCarNames(String carNames) {
+        return carNames.split(",");
+    }
+
+
 }
