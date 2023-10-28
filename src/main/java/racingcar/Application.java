817bb99f11a4032162f6362b2b545d7d14d90179
Jokyo0612 <jgh010612@gmail.com>
2023-10-28 14:57:44 +0900

    feat : 문자열 입력 후 리스트화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
+
+    public static List<String> nameCut(String name) {
+        String[] carArray = name.split(",");
+        return List.of(carArray);
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputName = Console.readLine();
+        List<String> carName = nameCut(inputName);
+
     }
 }
