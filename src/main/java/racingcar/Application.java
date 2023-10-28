3cff3263b7fff40068c526297962e73735b31db0
이종경 <gyoung3063413@naver.com>
2023-10-28 12:12:42 +0900

    [추가] 사용자 등록 메서드  및 유효성 검사 메서드

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,39 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		Game racingcar = new Game();
+	}
+}
+
+class Game {
+	private static String[] userArray;
+
+	Game() {
+		this.setGameUsers();
+	}
+
+	private void checkUsername() {
+		for (int i = 0; i < userArray.length; i++) {
+			if (userArray[i].length() > 5) {
+				throw new IllegalArgumentException();
+			} else {
+				userArray[i] = userArray[i].replaceAll(" ", ""); // 공백제거
+			}
+		}
+	}
+
+	private String[] setGameUsers() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String userInput = readLine();
+
+		userArray = userInput.split(","); // 사용자 등록
+		this.checkUsername(); //유효성 검사
+
+		return userArray;
+	}
+
+	
 }
