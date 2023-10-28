3ce26409da3f53a752e5c7ae3c6ca8d8329e983c
jinsu868 <mjinsu098@gmail.com>
2023-10-28 15:57:24 +0900

    feat(view): 우승자를 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.domain.Car;
 
 public class OutputView {
@@ -9,5 +10,17 @@ public class OutputView {
             System.out.print("-");
         }
         System.out.println();
+    }
+
+    public void printWinner(List<Car> cars) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < cars.size(); i++) {
+            if (i == cars.size() - 1) {
+                stringBuilder.append(cars.get(i).getName() + "\n");
+            } else {
+                stringBuilder.append(cars.get(i).getName() + ", ");
+            }
+        }
+        System.out.println(stringBuilder.toString());
     }
 }
