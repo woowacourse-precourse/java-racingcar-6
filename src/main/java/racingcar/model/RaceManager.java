df769088eb2d213bbeeaf30a73ceaa419d20be95
JUNG GYUWON <milestogo0905@naver.com>
2023-10-28 23:30:38 +0900

    feat: 이동 횟수 입력값 유효성 검사 추가
    입력값이 숫자가 아니거나 음수인 경우에 IllegalArgumentException이 발생하도록 에러처리

--- src/main/java/racingcar/model/RaceManager.java
+++ src/main/java/racingcar/model/RaceManager.java
@@ -3,6 +3,16 @@ package racingcar.model;
 public class RaceManager {
 
     public void validateNumberOfMove(String playerInput) {
+        if (toNumber(playerInput) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 
+    private int toNumber(String playerInput) {
+        try {
+            return Integer.parseInt(playerInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
