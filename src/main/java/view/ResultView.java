6b3ea7151c44aa10ebecfdf67e3fd9c0052f04a2
leesangjun <hubls@naver.com>
2023-10-28 17:18:46 +0900

    refactor: 하드코딩 제거

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class ResultView {
     private static final String RESULT_MESSAGE = "최종 우승자 : ";
+    private static final String STANDARD_DELIMITER = ", ";
 
     public static void printResult(List<String> winner) {
         System.out.print(RESULT_MESSAGE);
-        System.out.println(String.join(", ", winner));
+        System.out.println(String.join(STANDARD_DELIMITER, winner));
     }
 }
