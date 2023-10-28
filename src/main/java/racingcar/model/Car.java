56911def3687507d66df60f517f5bab190c671a2
dlrjs2360 <lhg3670@naver.com>
2023-10-28 23:22:32 +0900

    :sparkles: feat: 자동차와 자동차 리스트 모델 생성
    - 자동차 객체의 틀인 Car 클래스를 생성했습니다.
    - 자동차 객체 리스트를 저장하는 CarList 클래스를 생성했습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,4 +2,9 @@ package racingcar.model;
 
 public class Car {
 
+    private final String car;
+
+    public Car(String car) {
+        this.car = car;
+    }
 }
--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -1,5 +1,12 @@
 package racingcar.model;
 
-public class CarList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CarList {
+    private final List<Car> carList = new ArrayList<>();
+
+    public CarList(List<String> carNames) {
+        carNames.forEach(carName -> carList.add(new Car(carName)));
+    }
 }
