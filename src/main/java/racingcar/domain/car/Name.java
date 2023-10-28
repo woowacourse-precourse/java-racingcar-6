e4db900dd2482ba58ea9ef442a9a78bfe3173778
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 03:50:57 +0900

    feat(Name): toString() 오버라이딩을 통해 이름 반환하는 기능 구현

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -12,6 +12,11 @@ public class Name {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
     private void validate(String name) {
         if (isNotMatchValidNameCondition(name)) {
             throw new IllegalArgumentException("이름은 공백 없는 1~5자의 영어 또는 한글이어야 합니다.");
