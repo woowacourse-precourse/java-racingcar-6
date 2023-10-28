e4e7c3d75ee64f756c98f1f5881c2d3ee260187a
D0ri123 <lsh81550@gmail.com>
2023-10-29 01:42:33 +0900

    feat: 콘솔 출력 문구 기능 구현 - 2차 구현
    게임에서 사용되는 콘솔 문구를 출력하는 역할을 담당합니다.

---
+++ src/main/java/racingcar/GameUI.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class GameUI {
+    public void requestNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void requestTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+}
