167d27d7eca7321d2170452febf59167d7afd9bb
scarlet0o0 <ghd7673@naver.com>
2023-10-28 10:53:53 +0900

    Feat : Car 객체 생성자와 이동 메소드 구현
    Car에 생성자와 이동 메소드를 구현 하였습니다. 이동메소드는 엔진조건에 따라 움직입니다.

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -2,6 +2,19 @@ package racingcar.car;
 
 public class Car {
     private Name name;
-    private Engine engine;
+    private final Engine engine;
+    private int travelDistance;
+
+    public Car(String name) {
+        this.name = new Name(name);
+        this.engine = new Engine();
+        this.travelDistance = 0;
+    }
+
+    public void move() {
+        if(engine.isForward()){
+            travelDistance += 1;
+        }
+    }
 
 }
