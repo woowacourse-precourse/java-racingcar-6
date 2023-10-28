71186ac081033cc920ab43b52a14901e5234d061
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 17:14:11 +0900

    feat(service): 입력 값 초기화 함수 추가

--- src/main/java/racingcar/service/Game.java
+++ src/main/java/racingcar/service/Game.java
@@ -1,7 +1,32 @@
 package racingcar.service;
 
+import racingcar.domain.validation.InputValidator;
+import racingcar.ui.Message;
+import racingcar.ui.input.InputProcessor;
+import racingcar.ui.output.OutputProcessor;
+
 public class Game {
+    private InputValidator inputValidator = new InputValidator();
+    private InputProcessor inputProcessor = new InputProcessor();
+    private OutputProcessor outputProcessor = new OutputProcessor();
+    private String carNames;
+    private String tryCount;
+
     public void playGame() {
 
+    }
+
+    private String getInput(String message) {
+        outputProcessor.printMessage(message);
+        return inputProcessor.readInput();
+    }
+    private void initCarNames() {
+        carNames = getInput(Message.START_MESSAGE_CAR_NAMES.getMessage());
+        inputValidator.validateCarNames(carNames);
+    }
+
+    private void initTryCount() {
+        tryCount = getInput(Message.START_MESSAGE_TRY_COUNT.getMessage());
+        inputValidator.validateTryCount(tryCount);
     }
 }
