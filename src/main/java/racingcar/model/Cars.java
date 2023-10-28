8c8487b8b8c8f899291717f0e978c0537a0c0d76
chosunghyun18 <krismon@naver.com>
2023-10-28 16:25:09 +0900

    feat : add compare and get top

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
@@ -8,22 +10,46 @@ public class Cars {
     public Cars(List<Car> cars) {
         this.cars = cars;
     }
+    public Integer getTopPosition() {
+        int topPosition= 0 ;
+        for(Car car : cars) {
+            PairCompareResult cp = this.comparePosition(car);
+            if(cp.state().equals(ComparePositionState.FRONT)) {
+                topPosition = Integer.max(topPosition, cp.position());
+            }
+            if(cp.state().equals(ComparePositionState.SAME)) {
+                return cp.position();
+            }
+        }
+        return topPosition;
+    }
 
     public PairCompareResult comparePosition(Car givenCar) {
         int topLank = 0 ;
-        PairCompareResult out = new PairCompareResult(ComparePositionState.BACK ,topLank);
+        List<ComparePositionState> csList  = new ArrayList<>();
         for(Car car : cars) {
             PairCompareResult compareResult = givenCar.comparePosition(car);
             if(compareResult.state().equals(ComparePositionState.FRONT)) {
                 topLank = Integer.max(topLank,compareResult.position());
-                out = new PairCompareResult(ComparePositionState.FRONT,topLank);
+                csList.add(ComparePositionState.FRONT);
             }
             if(compareResult.state().equals(ComparePositionState.SAME)) {
                 topLank = Integer.max(topLank,compareResult.position());
-                out = new PairCompareResult(ComparePositionState.SAME,topLank);
+                csList.add(ComparePositionState.SAME);
+            }
+            if(compareResult.state().equals(ComparePositionState.BACK)){
+                csList.add(ComparePositionState.BACK);
             }
         }
-        return out;
-    }
 
+        if(csList.contains(ComparePositionState.FRONT)) {
+            return new PairCompareResult(ComparePositionState.FRONT,topLank);
+        }
+
+        if(csList.contains(ComparePositionState.BACK)){
+            return new PairCompareResult(ComparePositionState.BACK ,topLank);
+        }
+
+        return new PairCompareResult(ComparePositionState.SAME,topLank);
+    }
 }
