557b256179462b617f7a451093e05fe31dc4ce43
Youngwoo Choi <allnstar@naver.com>
2023-10-29 02:28:06 +0900

    feat(Event): add playGame

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,22 +8,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         Event carRacing = new Event();
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carRacing.filterName();
-        carRacing.filterTimes();
-
-        System.out.println("");
-        System.out.println("실행 결과");
-
-        int l = 0;
-        while (l < carRacing.moveTimes) {
-            carRacing.moveForward();
-            System.out.println("");
-            l++;
-        }
-
-        carRacing.announceWinner();
+        carRacing.playGame();
     }
 }
 
@@ -39,7 +24,6 @@ class Event {
         if ( !(inputName.contains(",")) ) {
             throw new IllegalArgumentException("이름은 쉼표로 구분되어야 합니다.");
         }
-
         for (int i = 0; i < players.length; i++) {
             if (players[i].length() > 5) {
                 throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
@@ -57,18 +41,15 @@ class Event {
         catch (Exception e) {
             throw new IllegalArgumentException("회수가 올바르지 않습니다.");
         }
-
     }
 
     void moveForward() {
         int i = 0;
         while (i < players.length ) {
             int movePoint = Randoms.pickNumberInRange(0, 9);
-
             if (movePoint >= 4) {
                 scoreBoard[i] = scoreBoard[i] + movePoint;
             }
-
             i++;
         }
 
@@ -78,7 +59,6 @@ class Event {
             for (int k = 0; k < scoreBoard[j]; k++) {
                 System.out.print("-");
             }
-
             System.out.print("\n");
         }
     }
@@ -113,4 +93,20 @@ class Event {
         }
     }
 
+    void playGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        filterName();
+        filterTimes();
+        System.out.println("");
+        System.out.println("실행 결과");
+
+        int l = 0;
+        while (l < moveTimes) {
+            moveForward();
+            System.out.println("");
+            l++;
+        }
+
+        announceWinner();
+    }
 }
