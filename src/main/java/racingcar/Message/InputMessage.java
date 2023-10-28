22f82bc53bca4e895fc8dab0f332b9f13493dd64
coenffl <>
2023-10-28 23:17:55 +0900

    Refactor: change input message

--- src/main/java/racingcar/Message/InputMessage.java
+++ src/main/java/racingcar/Message/InputMessage.java
@@ -1,6 +1,6 @@
 package racingcar.Message;
 
 public class InputMessage {
-    final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    final String INPUT_TIMES = "시도할 회수는 몇회인가요?";
+    final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    final String TIMES_MESSAGE = "시도할 회수는 몇회인가요?";
 }
