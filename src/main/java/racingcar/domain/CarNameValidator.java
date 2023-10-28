153b357d089ec9a7d919a6c8b1ab492aa6786748
Sully <2dcoder@naver.com>
2023-10-28 22:27:11 +0900

    feat(CarNameValidator): carName에 영문자와 쉼표가 아닌 다른 입력이 들어온다면 예외가 발생하는 메서드 추가

--- src/main/java/racingcar/domain/CarNameValidator.java
+++ src/main/java/racingcar/domain/CarNameValidator.java
@@ -7,11 +7,18 @@ public class CarNameValidator {
     public CarNameValidator(String carName) {
         this.carName = carName;
         validateCarNameExists();
+        validateAlphanumericAndCommaCarName();
     }
 
     public void validateCarNameExists() {
         if (carName.isBlank()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    public void validateAlphanumericAndCommaCarName() {
+        if (!carName.matches("[a-zA-Z0-9,]+")) {
+            throw new IllegalArgumentException("자동차 이름은 영숫자와 콤마로만 구성되어야 합니다.");
         }
     }
 
