b44c05938765bf038b4f97d028bd6fa2fc88a7ce
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-28 20:33:08 +0900

    test(AttemptCountValidatorTest) : 객체를 클래스에서 한번만 생성하도록 수정

--- src/test/java/racingcar/AttemptCountValidatorTest.java
+++ src/test/java/racingcar/AttemptCountValidatorTest.java
@@ -10,12 +10,12 @@ import racingcar.util.BlankValidator;
 
 public class AttemptCountValidatorTest {
     BlankValidator blankValidator = new BlankValidator();
+    AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
 
     @DisplayName("시도횟수 입력 - 정상입력")
     @Test
     void checkAttempNoramInt() {
         String attempCount = "21";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertDoesNotThrow(() -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -23,7 +23,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempOutOfRangePlusInt() {
         String attempCount = "1231231241351351351325134123413431";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -31,7 +30,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempOutOfRangeMinusInt() {
         String attempCount = "-1231231241351351351325134123413431";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -39,7 +37,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempNotInt() {
         String attempCount = "!222A";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -47,7 +44,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempdecimal() {
         String attempCount = "3.5";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -55,7 +51,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempMinus() {
         String attempCount = "-6";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -63,7 +58,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempZero() {
         String attempCount = "0";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 
@@ -71,7 +65,6 @@ public class AttemptCountValidatorTest {
     @Test
     void checkAttempInternalSpaces() {
         String attempCount = "1 4";
-        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getAttemptCount(attempCount));
     }
 }
