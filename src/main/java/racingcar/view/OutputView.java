1642b9e24738926fbe046d9169db8ee04dd8b232
jongheonleee <qwefghnm1212@gmail.com>
2023-10-28 15:36:06 +0900

    [feat] 자동차 위치 출력 메서드 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,10 +6,32 @@ import racingcar.domain.Cars;
 public class OutputView {
 
     private static final String OUTPUT_GAME_RESULT_MESSAGE = "실행 결과";
+
+    private static final String OUTPUT_CAR_POSITION_MESSAGE_FORMAT = "%s : %s";
     private final StringBuilder outputBuilder;
 
     public OutputView() {
         this.outputBuilder = new StringBuilder();
+    }
+
+
+    public void outputCarsPosition(Cars cars) {
+        for (Car car : cars.getCars()) {
+            drawCarPosition(car);
+        }
+    }
+
+    private void drawCarPosition(Car car) {
+        System.out.printf(OUTPUT_CAR_POSITION_MESSAGE_FORMAT, car.getName(), createCarGraph(car));
+    }
+
+    private String createCarGraph(Car car) {
+        initOutputBuilder();
+        for (int i=0; i<car.getPosition(); i++) {
+            outputBuilder.append("-");
+        }
+        outputBuilder.append("\n");
+        return outputBuilder.toString();
     }
 
 
