0a4779ab5a0d8ecc7ea1fafb9e7f3d8b6f621d7c
EvelynKimm <evekimmy09@gmail.com>
2023-10-29 02:10:14 +0900

    feat: first upload Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.control.Control;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Control RacingcarGame = new Control();
+        RacingcarGame.playGame();
     }
 }
