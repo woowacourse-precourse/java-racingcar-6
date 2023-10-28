3f210a3b12e069ac154c89bb4fb39c027d6e8f7a
mango <esudatcom@gmail.com>
2023-10-28 11:32:28 +0900

    test(Car): 전진과 멈춤 기능 테스트

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarTest {
+    @Test
+    void 전진_테스트 (){
+        Car car = new Car("test");
+        car.move(true);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 멈춤_테스트(){
+        Car car = new Car("test");
+        car.move(false);
+        assertThat(car.getPosition()).isEqualTo(0);    }
+
+}
