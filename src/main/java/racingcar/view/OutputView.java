f5662c2f688edad83b48e6b232f76a266f26cdc7
SeungjunNA <nsjjsng1@gmail.com>
2023-10-28 23:16:32 +0900

    feat : GameUtil클래스 자동차 이름이랑 위치 출력, 우승자 출력 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,10 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class OutputView {
     public void gameResult(){
         System.out.println("실행 결과");
@@ -9,5 +14,13 @@ public class OutputView {
     }
     public void inputCount(){
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+    public void gameSituation(ArrayList<Car> carArrayList){
+        for(int i=0;i<carArrayList.size();i++){
+            System.out.println(carArrayList.get(i).toString());
+        }
+    }
+    public void winner(String winner){
+        System.out.println("최종 우승자 : " + winner);
     }
 }
