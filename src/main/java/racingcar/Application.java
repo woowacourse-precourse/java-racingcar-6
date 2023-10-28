150c29b1936454f9306435fb621827da2f8b5543
Kim Jeong Hyeon <kr100475@gmail.com>
2023-10-29 02:18:31 +0900

    [feat] 각 자동차의 이름 및 이동횟수 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import model.GameManager;
+
 public class Application {
+    private static String firstMent = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println(firstMent);
+
+        //이름 입력
+        String nameInput = Console.readLine();
+
+        String[] carName = nameInput.split(",");
+
+        int num = Integer.parseInt(Console.readLine());
+
+        new GameManager(carName, num);
+
     }
 }
