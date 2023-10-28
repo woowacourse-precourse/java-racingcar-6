0904df9e90a60242efa8a7d0180e8c84fcb0b605
prao <pch5521@gmail.com>
2023-10-28 22:49:52 +0900

    feat: Console 생성
    - racingcar 내에서 사용하는 print, println, readLine을 static 메서드로 정의

---
+++ src/main/java/racingcar/utils/Console.java
@@ -0,0 +1,19 @@
+package racingcar.utils;
+
+public class Console {
+
+    private Console() {
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    public static String readLine() {
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
+}
