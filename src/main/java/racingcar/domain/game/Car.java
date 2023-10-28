d895729f196c08d84b9e224eb5d6e16312f6538f
minisyu <iswater.kim@gmail.com>
2023-10-28 11:44:19 +0900

    feat(Car): 자동차 이름 검증, 전진 기능 구현

--- src/main/java/racingcar/domain/game/Car.java
+++ src/main/java/racingcar/domain/game/Car.java
@@ -1,8 +1,11 @@
 package racingcar.domain.game;
 
+import racingcar.domain.dto.CarDto;
+import racingcar.util.NumberPicker;
+
 public class Car {
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     private Car(String name) {
         validateNumberLength(name);
@@ -17,5 +20,12 @@ public class Car {
         if (name == null || name.isEmpty() || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
+    }
+
+    public CarDto moveOnce(NumberPicker numberPicker) {
+        if (numberPicker.pick() >= 4) {
+            position++;
+        }
+        return new CarDto(name, position);
     }
 }
