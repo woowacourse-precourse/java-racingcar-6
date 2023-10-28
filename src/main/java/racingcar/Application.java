c5e0fee02af35f69f19434b18b37a4a01eefb3ce
Seol_JY <wlsdud5654@gmail.com>
2023-10-28 19:21:15 +0900

    feat(game): RacingGame 객체 초안 작성
    controller 역할을 수행
    - 게임 주요 흐름 구성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.game.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
     }
 }
---
+++ src/main/java/racingcar/game/RacingGame.java
@@ -0,0 +1,11 @@
+package racingcar.game;
+
+import java.util.List;
+import racingcar.view.Input;
+
+public class RacingGame {
+    public void play() {
+        List<String> carNames = Input.getCarNames();
+
+    }
+}
