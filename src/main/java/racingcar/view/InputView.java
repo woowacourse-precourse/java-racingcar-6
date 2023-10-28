be3b74050bb344b1e1154f7a5ede5046b14c6feb
hil679 <hil679922@gmail.com>
2023-10-28 14:39:24 +0900

    Feat(InputView): Create skeleton code
    - InputCarName method
    - InputTryNum method

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String InputCarName(){
+        return "";
+    }
+
+    public int InputTryNum(){
+        return 0;
+    }
+}
