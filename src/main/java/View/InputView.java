f66255d44e4eb0a44c83382431ffd4f87d93de67
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:32:21 +0900

    refactor : 시도 횟수 입력 기능 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -3,10 +3,16 @@ package View;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String inputCarName = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
-
+    private static final String inputNumOfAttempt = "시도할 회수는 몇회인가요?";
     public static String printUserInputCarName(){
         System.out.println(inputCarName);
         String input = readLine();
         return input;
+    }
+
+    public static Integer printUserInputNumOfAttempt(){
+        System.out.println(inputCarName);
+        Integer numOfAttempt = Integer.parseInt(readLine());
+        return numOfAttempt;
     }
 }
