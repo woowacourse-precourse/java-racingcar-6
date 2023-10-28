f3be629ee6a2c1218bf9bbdb1ce8b839c3f91f09
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-28 12:13:00 +0900

    refactor(RacingController) : 하나의 메소드가 하나의 일만 하도록 수정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -7,26 +7,46 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-    BlankValidator blankValidator = new BlankValidator();
-    Cars cars = new Cars(blankValidator);
-    Racing racing = new Racing(cars, blankValidator);
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final BlankValidator blankValidator = new BlankValidator();
+    private final Cars cars = new Cars(blankValidator);
+    private final Racing racing = new Racing(cars, blankValidator);
 
     public void raceStart() {
-        cars.addCars(inputView.printCarNameRequest());
-        racing.validateAttemptCount(inputView.printAttemptCountRequest());
-        System.out.println();
-        outputView.printResultText();
-        executeRaces();
-        outputView.printFinalWinner(cars.determineFinalWinner());
+        setUpRace();
+        runRace();
+        printRaceResults();
     }
 
-    private void executeRaces() {
+    private void setUpRace() {
+        addCarsToRace();
+        setAttemptCountForRace();
+    }
+
+    private void addCarsToRace() {
+        String carNames = inputView.printCarNameRequest();
+        cars.addCars(carNames);
+    }
+
+    private void setAttemptCountForRace() {
+        String attemptCount = inputView.printAttemptCountRequest();
+        racing.validateAttemptCount(attemptCount);
+        System.out.println();
+    }
+
+    private void runRace() {
         for (int i = 0; i < racing.getAttempCount(); i++) {
             racing.race();
             outputView.printExecutionResult(cars.getCars());
             System.out.println();
         }
     }
+
+    private void printRaceResults() {
+        outputView.printResultText();
+        outputView.printFinalWinner(cars.determineFinalWinner());
+    }
+
+
 }
