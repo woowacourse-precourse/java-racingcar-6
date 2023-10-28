f0fad9e962d9b158a73b02dda50a82a885ad5a3f
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 11:12:52 +0900

    feat(OutputView): 게임 출력 화면 생성

---
+++ src/main/java/racingcar/io/OutputView.java
@@ -0,0 +1,14 @@
+package racingcar.io;
+
+import racingcar.constants.ProgressMessage;
+
+public class OutputView {
+
+    public void printRacingCarNamesMessage() {
+        System.out.println(ProgressMessage.INPUT_RACING_CAR_NAMES);
+    }
+
+    public void printAttemptCountMessage() {
+        System.out.println(ProgressMessage.INPUT_ATTEMPT_COUNT);
+    }
+}
