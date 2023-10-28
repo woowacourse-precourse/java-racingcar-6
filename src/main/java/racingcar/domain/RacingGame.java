63e08a148ae39bd3fd0c48c0d38dd3935100c6b3
jinsu868 <mjinsu098@gmail.com>
2023-10-28 18:37:30 +0900

    refactor(RacingGame, OutputView): 자동차 이동 메서드로 추출(RacingGame), 개행 메서드 추가(OutputView)

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -6,15 +6,21 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGame {
-    public void run() {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        Judgment judgment = new Judgment();
-        Referee referee = new Referee();
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Judgment judgment;
+    private final Referee referee;
 
+    public RacingGame() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        judgment = new Judgment();
+        referee = new Referee();
+    }
+
+    public void run() {
         outputView.printCarInputMessage();
         final List<String> carNames = inputView.askCarNames();
-
 
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
@@ -26,16 +32,23 @@ public class RacingGame {
 
         outputView.printResultMessage();
         for (int i = 0; i < tryCount; i++) {
+            moveCars(cars);
             for (Car car : cars) {
-                if (judgment.canMove(NumberGenerator.createRandomNumber())) {
-                    car.move();
-                }
                 outputView.printCarInfo(car);
             }
-            System.out.println();
+
+            outputView.printNewLine();
         }
 
         List<Car> winners = referee.getWinner(cars);
         outputView.printWinner(winners);
+    }
+
+    private void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            if (judgment.canMove(NumberGenerator.createRandomNumber())) {
+                car.move();
+            }
+        }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -36,4 +36,8 @@ public class OutputView {
     public void printResultMessage() {
         System.out.println("실행 결과");
     }
+
+    public void printNewLine() {
+        System.out.println();
+    }
 }
