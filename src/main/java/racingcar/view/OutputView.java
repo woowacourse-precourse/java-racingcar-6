4384efb2b81a0e2501a31ce6e8ac37d05d010dbb
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-28 21:21:08 +0900

    [feat] : add final winner message method

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void resultMessage() {
         System.out.println("실행 결과");
     }
+
+    public static void finalWinnerMessage() {
+        System.out.println("최종 우승자 :");
+    }
 }
