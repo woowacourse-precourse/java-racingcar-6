ac16b06f105ee8a91a33587a261e76d75aac5654
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:37:31 +0900

    refactor : 시도 횟수 입력 기능 -> 입력 메세지만 출력으로 변경

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -4,15 +4,11 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String inputCarName = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분";
     private static final String inputNumOfAttempt = "시도할 회수는 몇회인가요?";
-    public static String printUserInputCarName(){
+    public static void printUserInputCarNameMessage(){
         System.out.println(inputCarName);
-        String input = readLine();
-        return input;
     }
 
-    public static Integer printUserInputNumOfAttempt(){
+    public static void printUserInputNumOfAttemptMessage(){
         System.out.println(inputCarName);
-        Integer numOfAttempt = Integer.parseInt(readLine());
-        return numOfAttempt;
     }
 }
