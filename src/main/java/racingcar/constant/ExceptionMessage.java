800dbe93337004759cd3b00d866f371d0c7b18ab
Annjieun <dkslwdms2121@naver.com>
2023-10-28 20:52:01 +0900

    refactor(ExceptionMessage): 매직 넘버 제거

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -1,11 +1,14 @@
 package racingcar.constant;
 
+import static racingcar.constant.GameNumber.ATTEMPT_MINIMUM;
+import static racingcar.constant.GameNumber.NAME_LENGTH_LIMIT;
+
 public class ExceptionMessage {
     public static final String INVALID_NAME_EXCEPTION = "이름이 유효하지 않습니다.";
     public static final String BLANK_EXCEPTION = "공백을 입력하였습니다.";
-    public static final String LENGTH_EXCEPTION = "이름은 5자 이내로 입력해주세요.";
+    public static final String LENGTH_EXCEPTION = "이름은 " + NAME_LENGTH_LIMIT + "자 이내로 입력해주세요.";
     public static final String DUPLICATE_EXCEPTION = "중복된 이름이 있습니다.";
     public static final String NON_NUMBER_EXCEPTION = "숫자를 입력해주세요.";
     public static final String DECIMAL_EXCEPTION = "소수를 입력하였습니다.";
-    public static final String NON_POSITIVE_NUMBER_EXCEPTION = "1이상의 정수를 입력해주세요.";
+    public static final String NON_POSITIVE_NUMBER_EXCEPTION = ATTEMPT_MINIMUM + " 이상의 정수를 입력해주세요.";
 }
---
+++ src/main/java/racingcar/constant/GameNumber.java
@@ -0,0 +1,6 @@
+package racingcar.constant;
+
+public class GameNumber {
+    public static final Integer NAME_LENGTH_LIMIT = 5;
+    public static final Integer ATTEMPT_MINIMUM = 1;
+}
