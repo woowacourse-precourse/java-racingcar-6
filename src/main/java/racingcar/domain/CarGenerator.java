6f85daa3bca9dab6633ddff5566d9431244256a9
BH946 <kobh2895@gmail.com>
2023-10-28 20:39:53 +0900

    feat: 자동차 정보 기록위해 객체 생성

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -1,2 +1,18 @@
-package racingcar.domain;public class CarGenerator {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CarGenerator {
+    private String[][] names;
+    public CarGenerator() {
+        String[] inStr = Console.readLine().split(",");
+        String[][] inNames = new String[inStr.length][2];
+        for(int i=0; i<inStr.length;i++) {
+            inNames[i][0] = inStr[i];
+            inNames[i][1] = "";
+        }
+        this.names = inNames;
+    }
+
+    public String[][] getNames() { return this.names; }
 }
