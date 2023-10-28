1e6773ab8d24ed2630d6d5479733852138400e9c
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 01:42:05 +0900

    feat(Car): 자동차의 이름과 위치를 저장하는 Car 클래스 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+
+public class Car {
+
+    private final CarName name;
+    private Position position = new Position(0);
+
+    public Car(final CarName name) {
+        this.name = name;
+    }
+
+    public CarName getName() {
+        return this.name;
+    }
+
+    public void moveToCar() {
+        this.position = position.move();
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+}
