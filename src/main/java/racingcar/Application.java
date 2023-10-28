bf84d77507f87c64a1a1e51e1ca45946e082ff00
julie0005 <julie0005@ajou.ac.kr>
2023-10-28 21:17:32 +0900

    feat : 사용자가 시도횟수 입력할 수 있는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = new ArrayList<>();
+
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        for (String name : carNames) cars.add(new Car(name));
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
     }
 }
