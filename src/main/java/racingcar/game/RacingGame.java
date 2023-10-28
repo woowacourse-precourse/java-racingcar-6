a104e8c4b7be600d52eed402322da4d940d55a11
Seol_JY <wlsdud5654@gmail.com>
2023-10-29 00:34:48 +0900

    feat(RacingGame): 우승 자동차 결정 후 출력할 수 있는 기능 구현
    자동차 승리 결정
    - 사용: race.determineWinners
    Output 클래스를 통해 승리한 자동차 이름 출력
    - 사용: Output.printWinnersName

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -5,14 +5,23 @@ import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.Name;
 import racingcar.view.Input;
+import racingcar.view.Output;
 
 public class RacingGame {
     public void play() {
         List<String> carNames = Input.getCarNames();
         int totalRace = Input.getTotalRace();
 
-        createCars(carNames);
+        Cars cars = createCars(carNames);
+        Race race = Race.of(cars, totalRace);
 
+        while (race.hasRemainingRaces()) {
+            race.performRaceStep();
+//            race.getRaceStatus();
+        }
+
+        List<Car> car = race.determineWinners();
+        Output.printWinnersName(car);
     }
 
     private Cars createCars(List<String> carNames) {
