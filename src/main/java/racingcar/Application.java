3bfc729e3de1ee0a8e3e3895fe903dd4a5144b39
wlghks960 <98100778+wlghks960@users.noreply.github.com>
2023-10-28 23:25:52 +0900

    feat : 자동차 경주 코드 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,52 +11,74 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Application {
+    public static LinkedHashMap<String, Integer> racingCars;
+
     public static void main(String[] args) throws IOException {
-        LinkedHashMap<String, Integer> racingCars = new LinkedHashMap<String,Integer>();
+        racingCars = new LinkedHashMap<String, Integer>();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] cars = in.readLine().split(",");
-        for(int i=0; i<cars.length; i++){
-            racingCars.put(cars[i],0);
+
+        for (int i = 0; i < cars.length; i++) {
+            racingCars.put(cars[i], 0);
         }
+
         System.out.println("시도할 회수는 몇회인가요?");
+
         int gameCount = Integer.parseInt(in.readLine());
         System.out.println("실행 결과");
-        while(gameCount-->0){
-            for(Map.Entry<String, Integer> car : racingCars.entrySet()){
-                int moveCount = moveCountInit();
 
+        raceStart(gameCount);
+
+        int max = racingCarsMaxValue();
+        String winners = whoWinner(max);
+        for (Map.Entry<String, Integer> car : racingCars.entrySet()) {
+            if (max == car.getValue()) {
+                winners = String.join(",", car.getKey());
+            }
+        }
+        System.out.println("최종 우승자 : " + winners);
+    }
+
+    public static void raceStart(int gameCount){
+        while (gameCount-- > 0) {
+            for (Map.Entry<String, Integer> car : racingCars.entrySet()) {
+                int moveCount = moveCountInit();
                 String move = concatMove("-", moveCount);
                 System.out.println(String.format("%s : %s", car.getKey(), move));
-                car.setValue(car.getValue()+moveCount);
+                car.setValue(car.getValue() + moveCount);
             }
-                System.out.println();
+            System.out.println();
         }
-        System.out.println(racingCars);
+    }
+    public static String whoWinner(int max) {
+        String winners = "";
+        for (Map.Entry<String, Integer> car : racingCars.entrySet()) {
+            if (max == car.getValue()) {
+                winners = String.join(",", car.getKey());
+            }
+        }
+        return winners;
+    }
+
+    public static int racingCarsMaxValue() {
         int max = Integer.MIN_VALUE;
-        for(Map.Entry<String, Integer> car : racingCars.entrySet()){
+        for (Map.Entry<String, Integer> car : racingCars.entrySet()) {
             max = Math.max(max, car.getValue());
         }
-        String answer = "";
-        for(Map.Entry<String, Integer> car : racingCars.entrySet()){
-            if(max == car.getValue()){
-                answer = String.join(",",car.getKey());
-            }
-        }
-        System.out.println("최종 우승자 : "+answer);
+        return max;
     }
 
-    public static int moveCountInit(){
-        return Randoms.pickNumberInRange(0,9);
+    public static int moveCountInit() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
-    public static String concatMove(String move, int moveCount){
+    public static String concatMove(String move, int moveCount) {
         StringBuilder sb = new StringBuilder(move.length() * moveCount);
 
-        for(int i=1; i<=moveCount; i++){
+        for (int i = 1; i <= moveCount; i++) {
             sb.append(move);
         }
-
         return sb.toString();
     }
 }
