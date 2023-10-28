1ce7a9ec4dd2110e39959dab1119a9d361869b46
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 03:44:33 +0900

    feat(InputValidator): 사용자의 입력에 대한 검증하는 코드 추가

--- src/main/java/racingcar/view/validator/InputValidator.java
+++ src/main/java/racingcar/view/validator/InputValidator.java
@@ -1,7 +1,48 @@
 package racingcar.view.validator;
 
-public class InputValidator {
-    public void validateCars(String s){
+import racingcar.view.constants.Numbers;
 
+public class InputValidator {
+    public static void validateCarName(String carName) {
+        if (carName == null || carName.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < carName.length(); i++) {
+            char currentLetter = carName.charAt(i);
+            if (currentLetter == ',') {
+                continue;
+            }
+            if (currentLetter < 'a' || currentLetter > 'z') {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        String[] carNames = carName.split(",");
+        if (carNames.length == 0 || carNames.length > Numbers.MAX_GAME_PLAYER.getNumber()) {
+            throw new IllegalArgumentException();
+        }
+        for (String name : carNames) {
+            if (name.isEmpty() || name.length() > Numbers.MAX_NAME_LENGTH.getNumber()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static void validatePlayTime(String playtime) {
+        if (playtime == null || playtime.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < playtime.length(); i++) {
+            char currentLetter = playtime.charAt(i);
+
+            if (currentLetter < '0' || currentLetter > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+        int number = Integer.parseInt(playtime);
+        if (number <= 0 || number > Numbers.MAX_GAME_PLAY.getNumber()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
