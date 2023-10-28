fb3a5d69658027e432b2d0d1e5acc543493d77d5
ohhyeon2 <dhgus1235@gmail.com>
2023-10-28 14:18:20 +0900

    feat: comma 제거
    유저의 자동차 개 수를 파악하고, 자동차의 이동 또는 멈춤을 비교하기 위해 유저가 comma를 기준으로 입력한 문자열의 comma를 제거한 후 리스트에 삽입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,11 +7,23 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        inputCarName();
+        String userInput = inputCarName();
+        List<String> deleteComma = deleteComma(userInput);
+        System.out.println(deleteComma);
     }
 
-    private static void inputCarName() {
+    private static String inputCarName() {
         String inputCarName = Console.readLine();
+        return inputCarName;
+    }
+
+    private static List<String> deleteComma(String inputCarName) {
+        List<String> userCarName = new ArrayList<>();
+        String[] userCarNameArr = inputCarName.split(",");
+        for (String car : userCarNameArr) {
+            userCarName.add(car.replace(",", ""));
+        }
+        return userCarName;
     }
 
     private static int inputTryCount() {
