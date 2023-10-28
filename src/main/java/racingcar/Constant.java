558dc53dcc82e8e944d54c6f036a1cbe4e83de38
zzawang <hobin0116@naver.com>
2023-10-28 22:35:30 +0900

    feat: 상수 파일에 새로운 상수 추가
    - MIN_ATTEMPT_COUNT: 시도 횟수의 최솟값이다.
    - INVALID_NUMERIC_INPUT_MESSAGE: 시도할 횟수를 Integer형으로 변환할 때 예외 발생 시 사용되는 문구이다.
    - MINIMUM_ATTEMPT_COUNT_REQUIRED_MESSAGE: 시도할 횟수가 최소 시도할 횟수보다 작은 경우 사용되는 예외 발생 문구이다.

--- src/main/java/racingcar/Constant.java
+++ src/main/java/racingcar/Constant.java
@@ -4,7 +4,10 @@ public class Constant {
     public static final String SEPARATOR = ",";
     public static final String BLANK_SPACE = " ";
     public static final int MAX_CAR_NAME_LENGTH = 5; // 자동차 이름의 최대 길이
+    public static final int MIN_ATTEMPT_COUNT = 1; // 최소 시도할 횟수
     public static final String DUPLICATE_CAR_NAME_MESSAGE = "자동차들의 이름 중 중복된 이름이 존재합니다. 게임이 종료됩니다.";
     public static final String BLANK_CAR_NAME_MESSAGE = "자동차 이름에 공백이 포함되어 있습니다. 게임이 종료됩니다.";
     public static final String OVER_MAX_LENGTH_MESSAGE = "자동차 이름이 5자 이상입니다. 게임이 종료됩니다.";
+    public static final String INVALID_NUMERIC_INPUT_MESSAGE = "시도할 횟수가 올바른 숫자가 아닙니다. 게임이 종료됩니다.";
+    public static final String MINIMUM_ATTEMPT_COUNT_REQUIRED_MESSAGE = "시도할 횟수는 최소 1 이상이어야 합니다. 게임이 종료됩니다.";
 }
