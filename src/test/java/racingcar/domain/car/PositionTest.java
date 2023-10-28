c3c21d27a3435f396ee1437ddccec0df5bbab309
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 04:02:48 +0900

    test(PositionTest): 위치 반환 기능 테스트 구현

--- src/test/java/racingcar/domain/car/PositionTest.java
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -1,2 +1,16 @@
-package racingcar.domain.car;public class PositionTest {
+package racingcar.domain.car;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+    @Test
+    void 위치_반환_테스트() {
+        //given
+        Position position = new Position();
+
+        //then
+        assertThat(position.getPosition()).isEqualTo(0);
+    }
 }
