31532c99b763acb8a3aac323a56b665c7e2f662f
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-28 19:52:09 +0900

    style:테스트 클래스 생성

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -11,6 +11,7 @@ import racingcar.Application;
 import racingcar.domain.GamePlayer;
 
 public class GameServiceTest extends NsTest {
+    private static final int MOVE_COUNT = 1;
 
     @Test
     public void 자동차_이름으로_자동차와_플레이어_생성() {
@@ -19,12 +20,34 @@ public class GameServiceTest extends NsTest {
         GameService gameService = new GameService();
         //when
         GamePlayer gamePlayer = gameService.initializePlayer(names);
-        gamePlayer.moveCarsByCount(1);
+        gamePlayer.moveCarsByCount(MOVE_COUNT);
         //then
         assertThat(output()).contains("name1", "name2", "name3");
         assertThat(output()).doesNotContain("name4");
     }
+//  랜덤값을 어떻게 테스트할건지 생각해보기
+//    @Test
+//    public void 모든_자동차_지정된_횟수만큼_랜덤으로_이동() {
+//        //given
+//
+//        //when
+//
+//        //then
+//
+//    }
 
+    @Test
+    public void 플레이어_정보로_게임등수_생성() {
+        //given
+        List<String> names = new ArrayList(Arrays.asList("name1", "name2", "name3"));
+        GameService gameService = new GameService();
+        //when
+        GamePlayer gamePlayer = gameService.initializePlayer(names);
+        gamePlayer.moveCarsByCount(MOVE_COUNT);
+        //then
+        assertThat(output()).contains("name1", "name2", "name3");
+        assertThat(output()).doesNotContain("name4");
+    }
 
     @Override
     public void runMain() {
