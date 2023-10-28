a011c2e11930e423186140daa9bca5d4e4d29955
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-29 00:09:33 +0900

    feat(service): 자동차 전진 게임 로직 구현

--- src/main/java/racingcar/service/Game.java
+++ src/main/java/racingcar/service/Game.java
@@ -1,25 +1,33 @@
 package racingcar.service;
 
+import racingcar.domain.model.Car;
+import racingcar.domain.util.Judgement;
+import racingcar.domain.util.NumberGenerator;
 import racingcar.domain.validation.InputValidator;
 import racingcar.ui.Message;
 import racingcar.ui.input.InputProcessor;
 import racingcar.ui.output.OutputProcessor;
 
 public class Game {
-    private InputValidator inputValidator = new InputValidator();
-    private InputProcessor inputProcessor = new InputProcessor();
-    private OutputProcessor outputProcessor = new OutputProcessor();
+    private final Car cars = new Car();
+    private final NumberGenerator numberGenerator = new NumberGenerator();
+    private final Judgement judgement = new Judgement();
+    private final InputValidator inputValidator = new InputValidator();
+    private final InputProcessor inputProcessor = new InputProcessor();
+    private final OutputProcessor outputProcessor = new OutputProcessor();
     private String carNames;
     private String tryCount;
 
     public void playGame() {
-
+        init();
     }
 
-    private String getInput(String message) {
-        outputProcessor.printMessage(message);
-        return inputProcessor.readInput();
+    private void init() {
+        initCarNames();
+        initTryCount();
+        initCars();
     }
+
     private void initCarNames() {
         carNames = getInput(Message.START_MESSAGE_CAR_NAMES.getMessage());
         inputValidator.validateCarNames(carNames);
@@ -28,5 +36,26 @@ public class Game {
     private void initTryCount() {
         tryCount = getInput(Message.START_MESSAGE_TRY_COUNT.getMessage());
         inputValidator.validateTryCount(tryCount);
+    }
+
+    private void initCars() {
+        cars.addCar(carNames);
+    }
+
+    private String getInput(String message) {
+        outputProcessor.printMessage(message);
+        return inputProcessor.readInput();
+    }
+
+    private void inGame() {
+        outputProcessor.printResult();
+        for (int i = 0; i < Integer.parseInt(tryCount); i++) {
+            for (String carName : cars.carList.keySet()) {
+                if (judgement.isMoving(numberGenerator.createNumber())) {
+                    cars.updateCar(carName);
+                }
+            }
+        }
+        outputProcessor.printResult(cars.carList);
     }
 }
