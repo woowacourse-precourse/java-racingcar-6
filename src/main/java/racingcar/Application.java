d985f0687b7045dc605530d48e403ba72e35d31f
mango <esudatcom@gmail.com>
2023-10-28 10:31:08 +0900

    feat(InputView): 사용자 입력의 유효성 검증하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,13 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.*;
+import racingcar.view.InputView;
 
 public class Application {
-
-    private static final int MAX_NAME_LENGTH = 5;
-    private static final int MOVE_CRITERIA = 4;
-    private static final String READLINE = Console.readLine();
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+//        String[] carNames = inputView.getCarNames();
+        int AttemptCount = inputView.getAttemptCount();
     }
 }
