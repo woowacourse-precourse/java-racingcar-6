9b65194fd99712e1d551d1d6be61c8de976c6430
inpink <inpink@kakao.com>
2023-10-28 14:26:51 +0900

    feat: CarRepository interface 생성
    save()는 Car 객체를 저장소에 저장하는 역할.
    findById()와 findByName()은 저장소에서 Id와 Name으로 Car 객체를 찾아옴. 없는 id 또는 없는 Name일 수 있으므로 Optional 적용.
    findAll()은 저장되어있는 모든 Car 객체를 받아옴.

---
+++ src/main/java/racingcar/repository/CarRepository.java
@@ -0,0 +1,13 @@
+package racingcar.repository;
+
+import java.util.List;
+import java.util.Optional;
+import racingcar.domain.Car;
+
+public interface CarRepository {
+
+    Car save(Car car);
+    Optional<Car> findById(Long id);
+    Optional<Car> findByName(String name);
+    List<Car> findAll();
+}
