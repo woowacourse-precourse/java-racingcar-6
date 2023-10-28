c00ba6989059395eb022ab9b05a96a3ec09c8dc9
JisooPyo <audwl326@gmail.com>
2023-10-29 03:30:45 +0900

    feat(game): 사용자에게 시도할 횟수를 받는다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,17 +2,32 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class RacingGame {
 	CarNameValidator carNameValidator = new CarNameValidator();
+
 	public RacingGame() {
-		start();
+		String[] cars = inputcarName();
+		int tryCount = inputTryCount();
 	}
 
-	private void start() {
+	private String[] inputcarName() {
 		String carNames = Console.readLine();
 		String[] carArray = carNames.split(",");
 
 		carNameValidator.validateCarName(carArray);
 
+		return carArray;
+	}
+
+	private int inputTryCount() {
+		String tryCount = Console.readLine();
+
+		if (!Pattern.matches("^[0-9]+$", tryCount)) {
+			throw new IllegalArgumentException("숫자만 입력하실 수 있습니다.");
+		}
+
+		return Integer.parseInt(tryCount);
 	}
 }
