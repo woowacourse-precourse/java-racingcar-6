f6e713f6163d020e250c0d0d1836a91f040993c3
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-29 01:02:39 +0900

    feat: Race 클래스에 List에 담긴 자동차를 전진시키는 기능 추가

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -2,11 +2,26 @@ package racingcar.model;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constant.GameConfig;
+
 public class Race {
 
 	private final List<Car> cars;
 
 	public Race(List<Car> cars) {
 		this.cars = cars;
+	}
+
+	private void moveAllCarsForward(List<Car> cars) {
+		int randomValue = 0;
+
+		for (Car car : cars) {
+			randomValue = Randoms.pickNumberInRange(GameConfig.RANDOM_MINIMUM_RANGE, GameConfig.RANDOM_MAXIMUM_RANGE);
+
+			if (randomValue >= GameConfig.MINIMUM_NUMBER_FOR_FORWARD) {
+				car.forward();
+			}
+		}
 	}
 }
