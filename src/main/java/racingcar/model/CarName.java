77d6f8147889731ba1375720422b8b63595982d1
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 01:31:37 +0900

    feat(CarName): CarName 클래스 추가 및 객체 생성시 검증 기능 추가

---
+++ src/main/java/racingcar/model/CarName.java
@@ -0,0 +1,34 @@
+package racingcar.model;
+
+import java.util.Objects;
+import racingcar.constants.ExceptionMessage;
+
+public class CarName {
+
+    private final String name;
+
+    public CarName(final String name) {
+        if (Objects.isNull(name)) {
+            ExceptionMessage.NONE_INPUT_CARS.throwException();
+        }
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
+        validateCarNameIsBlank(name);
+        validateLengthCarName(name);
+    }
+
+    private void validateCarNameIsBlank(final String name) {
+        if (name.isBlank()) {
+            ExceptionMessage.NONE_INPUT_CARS.throwException();
+        }
+    }
+
+    private void validateLengthCarName(final String name) {
+        if (name.length() < 1 || name.length() > 5) {
+            ExceptionMessage.CAR_NAME_LIMIT_EXCEEDED.throwException();
+        }
+    }
+}
