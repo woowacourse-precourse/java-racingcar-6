78eb49ec7520e7e8fd6317da976480747e06c1cc
gengminy <kls1998@naver.com>
2023-10-28 15:19:51 +0900

    feat: Application 설정 정의

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.core.Racing;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final Racing racing = new Racing();
+        racing.play();
     }
 }
