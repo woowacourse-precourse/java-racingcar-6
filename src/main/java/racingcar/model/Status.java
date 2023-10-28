7a23aef6b8e39aad998638aa8b974b9dc1de3583
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 18:48:30 +0900

    feat : 자동차가 출발할지 멈출지 결정하는 기능 구현

---
+++ src/main/java/racingcar/model/Status.java
@@ -0,0 +1,30 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public enum Status {
+
+    GO(1), STOP(0);
+
+    private static final Integer FIRST_NUMBER = 0;
+    private static final Integer LAST_NUMBER = 9;
+    private static final Integer THRESHOLD = 4;
+
+    private Integer forward;
+
+    Status(Integer forward) {
+        this.forward = forward;
+    }
+
+    public static Integer move() {
+        Integer randomNumber = Randoms.pickNumberInRange(FIRST_NUMBER, LAST_NUMBER);
+        if (randomNumber >= THRESHOLD) {
+            return GO.getForward();
+        }
+        return STOP.getForward();
+    }
+
+    public Integer getForward() {
+        return forward;
+    }
+}
