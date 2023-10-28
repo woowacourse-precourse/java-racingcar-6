3f7b233f4bf84a72b3cceed8edb720af308447ee
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 11:11:46 +0900

    feat(ProgressMessage): 게임 진행 메시지 생성

---
+++ src/main/java/racingcar/constants/ProgressMessage.java
@@ -0,0 +1,16 @@
+package racingcar.constants;
+
+public enum ProgressMessage {
+    INPUT_RACING_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_ATTEMPT_COUNT("시도할 회수는 몇회인가요?");
+    private final String message;
+
+    ProgressMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
