c4afa0446dbc735f29f10675cd13add499dd5df5
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 16:43:52 +0900

    feat: 게임의 결과를 전달하는 GameResult 생성

---
+++ src/main/java/racingcar/model/GameResult.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+public final class GameResult {
+    private final List<CarProgressResponse> progressResponses;
+    private final List<String> winners;
+
+    public GameResult(List<CarProgressResponse> progressResponses, List<String> winners) {
+        this.progressResponses = progressResponses;
+        this.winners = winners;
+    }
+}
