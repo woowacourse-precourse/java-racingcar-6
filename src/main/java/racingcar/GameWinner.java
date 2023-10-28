b705ea35ba44bfb83d34f649e795b51e3fe5fa2c
SUJI-J <suzy852@naver.com>
2023-10-28 21:21:29 +0900

    refactor: 우승 자동차 검증 로직 재구성

--- src/main/java/racingcar/GameWinner.java
+++ src/main/java/racingcar/GameWinner.java
@@ -1,27 +1,29 @@
 package racingcar;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class GameWinner {
 
     public static void getWinner(Map<String, String> carNamesMap) {
-        String maxCarValue = "0";
-        for (String carName : carNamesMap.keySet()) {
-            List<String> winner = isWinner(carNamesMap, carName, maxCarValue);
-        }
-        OutputView.
+        int maxCarValue = 0;
+        List<String> winner = new ArrayList<>();
+        winner = isWinner(carNamesMap, maxCarValue, winner);
+        OutputView.printGameWinner(winner);
     }
 
-    private static List<String> isWinner(Map<String, String> carNamesMap, String carName, String maxCarValue) {
-        List<String> winner = Arrays.asList(carName);
-        if (maxCarValue.compareTo(carNamesMap.get(carName)) == 0) {
-            winner.add(carName);
-        }
-        if (maxCarValue.compareTo(carNamesMap.get(carName)) < 0) {
-            winner.clear();
-            winner.add(carName);
+    private static List<String> isWinner(Map<String, String> carNamesMap, int maxCarValue, List<String> winner) {
+        for (String carName : carNamesMap.keySet()) {
+            if (maxCarValue == carNamesMap.get(carName).length()) {
+                maxCarValue = carNamesMap.get(carName).length();
+                winner.add(carName);
+            }
+            if (maxCarValue < carNamesMap.get(carName).length()) {
+                maxCarValue = carNamesMap.get(carName).length();
+                winner.clear();
+                winner.add(carName);
+            }
         }
         return winner;
     }
