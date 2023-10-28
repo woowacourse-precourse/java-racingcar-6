89541482c7e889ae4882a6ed33722942741de9f7
unknown <lsy5881@gmail.com>
2023-10-28 16:32:31 +0900

    Feat: Game, Car, Dice 클래스 추가

---
+++ src/main/java/racingcar/Dice.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Dice {
+    int min;
+    int max;
+
+    public Dice(int min, int max){
+        this.min = min;
+        this.max = max;
+    }
+}
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import java.util.LinkedList;
+
+public class Game {
+    private LinkedList<Car> cars = new LinkedList<>();
+    int turns;
+    Dice dice = new Dice(0, 9);
+}
