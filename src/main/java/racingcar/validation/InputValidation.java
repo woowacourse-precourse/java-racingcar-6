c343e366a1f1bc214a772de1369289bc5e6060c8
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 00:25:24 +0900

    feat : add verify logic for value contains space

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -6,4 +6,11 @@ public abstract class InputValidation {
             throw new IllegalArgumentException("[ERROR] 입력값이 공백일 수는 없습니다.");
         }
     }
+
+    public static void verifyForSpaceValue(String inputValue){
+        String eliminateSpaceValue = inputValue.replaceAll(" ", "");
+        if (!inputValue.equals(eliminateSpaceValue)){
+            throw new IllegalArgumentException("[ERROR] 입력값에 공백이 포함되었습니다.");
+        }
+    }
 }
