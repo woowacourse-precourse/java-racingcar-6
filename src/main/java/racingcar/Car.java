61345ad2ed4c1f4d7d4171e4634539b79d940cb6
junest66 <chlwndks333@gmail.com>
2023-10-29 02:53:39 +0900

    refactor(Car): NumberGenerator 의존성 주입
    - Car 클래스에 움직임 결정을 위한 NumberGenerator 의존성 추가
    - 생성자 변경으로 NumberGenerator를 받을 수 있게 수정
    - move() 메서드 내에서 NumberGenerator를 활용하여 랜덤 움직임 결정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,18 +4,25 @@ public class Car {
 
     private String name;
     private int moveNum;
+    private NumberGenerator numberGenerator;
 
-    public Car(String name) {
+
+    public Car(String name, NumberGenerator numberGenerator) {
         this.name = name;
         this.moveNum = 0;
+        this.numberGenerator = numberGenerator;
     }
 
     public int getMoveNum() {
         return moveNum;
     }
 
-    public void move(int randomNumber) {
-        if (randomNumber >= 4) {
+    public String getName() {
+        return name;
+    }
+
+    public void move() {
+        if (numberGenerator.createRandomNumber() >= 4) {
             moveNum++;
         }
     }
