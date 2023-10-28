5230af56664eba4180e4fdbe2b0ba122e9354794
song windows <dkclasltmf@naver.com>
2023-10-28 19:54:33 +0900

    refactor: Car 클래스 내 매직넘버를 상수로 치환

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,18 +3,20 @@ package racingcar.model;
 import static racingcar.constants.ExceptionMessageConstants.*;
 
 public class Car {
+    public static final int MAX_LENGTH = 5;
+    public static final int MIN_NUMBER = 4;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
-        if (name.length()>5) {
+        if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH);
         }
         this.name = name;
     }
 
     public void moveForward(int randomNumber) {
-        if (randomNumber>=4) {
+        if (randomNumber >= MIN_NUMBER) {
             position++;
         }
     }
