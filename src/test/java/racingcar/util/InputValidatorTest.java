edb2aadafe283adcbaaebe3ad8492876bf36ffd4
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-28 22:02:35 +0900

    test(util): 자동차 이름 중복 검증 테스트 추가

--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -11,4 +11,11 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
+
+    @Test
+    void 자동차_이름_중복_검증() {
+        assertThatThrownBy(() -> InputValidator.isValidCarNames("pobi,pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 자동차 이름이 존재합니다.");
+    }
 }
