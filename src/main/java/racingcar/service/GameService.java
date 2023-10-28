d4afa150a619b61ebb72b20a4cd929cf2b99b48f
takealook97 <takealook97@naver.com>
2023-10-29 03:40:45 +0900

    feat: GameService에 게임 진행 메서드 구현
    게임진행과 관련된 메서드를 구현하였다. 컨트롤러에서 playGame이 호출될 것이고 라운드 횟수를 기반으로 게임을 진행하며 repository에 저장한다. 전진 여부는 isForward 메서드를 통해 얻는다.

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,5 +1,12 @@
 package racingcar.service;
 
+import static racingcar.utility.RandomNumberGenerator.*;
+import static racingcar.utility.constant.NumberConstant.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.domain.Car;
 import racingcar.dto.GameDto;
 import racingcar.repository.GameRepository;
 
@@ -12,5 +19,26 @@ public class GameService {
 
 	public void createGame(GameDto gameDto) {
 		gameRepository.saveGame(gameDto);
+	}
+
+	public void playGame(Long roundCount) {
+		while (roundCount-- > 0) {
+			playRound();
+		}
+	}
+
+	public void playRound() {
+		List<Car> forwardCars = new ArrayList<>();
+		List<Car> cars = gameRepository.findAll();
+		for (Car car : cars) {
+			if (isForward()) {
+				forwardCars.add(car);
+			}
+		}
+		gameRepository.moveForward(forwardCars);
+	}
+
+	public boolean isForward() {
+		return getRandomNumber() >= FORWARD_POINT.getValue();
 	}
 }
