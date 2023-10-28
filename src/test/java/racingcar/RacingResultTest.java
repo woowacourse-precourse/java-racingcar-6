95d04327ac7d9557f1e35f79a1ccc3b8aa0528a9
kunsanglee <gosmdochee2@gmail.com>
2023-10-28 14:32:37 +0900

    test(RacingResultTest): 깊은 복사하여 변경 불가능한 리스트를 반환하는 테스트코드 작성

--- src/test/java/racingcar/RacingResultTest.java
+++ src/test/java/racingcar/RacingResultTest.java
@@ -1,16 +1,51 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class RacingResultTest {
+
+    @DisplayName("getRacingRoundResult()를 통해 얻어낸 읽기전용 리스트를 조작하려고 하면 에러가 발생한다.")
+    @Test
+    void getRacingRoundResultsManipulateFailTest() {
+        RacingResult racingResult = new RacingResult();
+        RacingRoundResult racingRoundResult = new RacingRoundResult(List.of(
+                setCar("pobi", 3),
+                setCar("woni", 3)
+        ));
+        racingResult.addResult(racingRoundResult);
+        List<RacingRoundResult> racingRoundResults = racingResult.getRacingRoundResults();
+        assertThatCode(
+                () -> racingRoundResults.add(new RacingRoundResult(List.of(setCar("pobi", 4), setCar("woni", 4)))))
+                .isInstanceOf(UnsupportedOperationException.class);
+        assertThatCode(() -> racingRoundResults.remove(0))
+                .isInstanceOf(UnsupportedOperationException.class);
+        assertThatCode(() -> racingRoundResults.clear())
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @DisplayName("getRacingRoundResult()를 통해 얻어낸 읽기전용 리스트를 조작하려고 하면 에러가 발생한다.")
+    @Test
+    void getRacingRoundResultsReadSuccessTest() {
+        RacingResult racingResult = new RacingResult();
+        RacingRoundResult racingRoundResult = new RacingRoundResult(List.of(
+                setCar("pobi", 3),
+                setCar("woni", 3)
+        ));
+        racingResult.addResult(racingRoundResult);
+        List<RacingRoundResult> racingRoundResults = racingResult.getRacingRoundResults();
+        assertThatCode(() -> racingRoundResults.get(0))
+                .doesNotThrowAnyException();
+    }
 
     @DisplayName("최종 라운드의 승자를 반환한다.")
     @ParameterizedTest
