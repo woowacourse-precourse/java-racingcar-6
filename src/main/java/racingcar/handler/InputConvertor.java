aa05f9067fea5818db32d875bb21da4b7b442015
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 20:50:30 +0900

    fix: 사용자 메시지 trim 하라는 요구사항이 없어서 수정

--- src/main/java/racingcar/handler/InputConvertor.java
+++ src/main/java/racingcar/handler/InputConvertor.java
@@ -4,15 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputConvertor {
-    private static final String COMMA_DELIMITER = ",";
+    static final String COMMA_DELIMITER = ",";
 
     public static List<String> splitByComma(String input) {
         return Arrays.stream(input.split(COMMA_DELIMITER))
-            .map(String::trim)
             .toList();
     }
 
     public static int convertToInt(String input) {
-        return Integer.parseInt(input.trim());
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/racingcar/handler/InputConvertorTest.java
+++ src/test/java/racingcar/handler/InputConvertorTest.java
@@ -1,5 +1,6 @@
 package racingcar.handler;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public class InputConvertorTest {
 
     @Test
     public void 쉼표로_문자열을_분리하면_올바르게_분리된_리스트가_반환되어야_한다() {
-        String input = "car1,car2 , car3";
+        String input = "car1,car2,car3";
         List<String> expected = List.of("car1", "car2", "car3");
 
         List<String> result = InputConvertor.splitByComma(input);
@@ -21,7 +22,7 @@ public class InputConvertorTest {
 
     @Test
     public void 유효한_문자열을_정수로_변환하면_올바른_정수가_반환되어야_한다() {
-        String input = " 12345 ";
+        String input = "12345";
 
         int result = InputConvertor.convertToInt(input);
 
@@ -32,6 +33,6 @@ public class InputConvertorTest {
     public void 유효하지_않은_문자열을_정수로_변환하려고_하면_NumberFormatException이_발생해야_한다() {
         String input = "invalid";
 
-        assertThrows(NumberFormatException.class, () -> InputConvertor.convertToInt(input));
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> InputConvertor.convertToInt(input));
     }
 }
