10ce63c3a05823f75533b414967a69d4225c4c19
dltjdn <hana40004@naver.com>
2023-10-28 17:42:16 +0900

    feat(Cars): 차수별 실행 결과 스트링 생성 기능

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,10 +1,10 @@
 package racingcar.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
     private final List<Car> cars;
+    private static final String NEW_LINE = "\n";
 
     public Cars(List<String> names){
         cars = names.stream()
@@ -16,5 +16,14 @@ public class Cars {
         for(Car car : cars){
             car.forward();
         }
+    }
+
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        for(Car car : cars){
+            result.append(car.toString());
+            result.append(NEW_LINE);
+        }
+        return result.toString();
     }
 }
