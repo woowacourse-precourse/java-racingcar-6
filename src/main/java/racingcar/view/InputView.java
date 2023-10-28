457f5b8d0afe769c01e10eb0ab461551c7bb8c53
“EvelynKimm” <evekimmy09@gmail.com>
2023-10-29 02:22:25 +0900

    feat: add request round(trial) number method
    사용자에게 경주 횟수 요청하는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,9 +7,15 @@ public class InputView {
     // Car 입력받기
     // Round 입력받기
     private static final String CarNameQuestion = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TrialQuestion = "시도할 회수는 몇회인가요?";
 
     public static String requestCarName() {
         System.out.println(CarNameQuestion);
+        return Console.readLine();
+    }
+
+    public static String requestRoundNumber() {
+        System.out.println(TrialQuestion);
         return Console.readLine();
     }
 }
