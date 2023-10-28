7d07cf1c0848c93da9fd79492a10b35e53f7f5a7
suhyun9764 <suhyun9764@naver.com>
2023-10-28 17:42:18 +0900

    feat(Car) : [ ] 랜덤값 생성하기 (0~9) 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int currentPosition;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private int getRandomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
     }
 
 
