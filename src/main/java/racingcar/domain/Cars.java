71854ce58c30697756047bf85fbf02ddbb1dfc02
elyudwo <dudqk9696@naver.com>
2023-10-28 18:09:12 +0900

    [Feat] : Cars 객체 기본 생성자 추가
    List 필드 ArrayList 로 초기화

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -7,6 +8,10 @@ import static java.util.stream.Collectors.toList;
 public class Cars {
 
     private List<Car> cars;
+
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
 
     public void makeCars(List<String> carNames) {
         cars.addAll(carNames.stream()
