3ae9f7318f712079374826dc82dcf07454ce1058
heiler <threepebbles@naver.com>
2023-10-29 00:58:04 +0900

    refactor: 메소드명 변경(suffix: message)

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,11 @@
 package racingcar.view;
 
 public class InputView {
-    public static String enterCarNames() {
+    public static String enterCarNamesMessage() {
         return "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     }
 
-    public static String enterNumberOfRounds() {
+    public static String enterNumberOfRoundsMessage() {
         return "시도할 회수는 몇회인가요?";
     }
 }
