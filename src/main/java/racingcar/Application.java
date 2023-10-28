6d30184d3bf2a573867f5a3abaa8c2677cd0da45
euni <kongeuni1004@gmail.com>
2023-10-28 16:04:41 +0900

    Feat($CarNameInput) : 경주할 자동차 이름 입력
    - 자동차이름 "," 기준으로 입력받기
    - 입력받은 문자열 ","기준으로 나눈후 오류판별
    - 통과한 배열을 hashmap에 이름을 key, value는 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarNameInput carNameInput = new CarNameInput();
+
+        HashMap<String,Integer> userResult = carNameInput.inputCarName();
+
     }
 }
