54383cbd2096a3558e786280a229d1f72b21e05b
김연진 <love_homebody@naver.com>
2023-10-28 17:58:08 +0900

    feat(Car): 무작위 값 생성 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,11 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
 
     private final String name;
     private final int position;
@@ -8,5 +13,9 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public int pickRandomNumber() {
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 }
