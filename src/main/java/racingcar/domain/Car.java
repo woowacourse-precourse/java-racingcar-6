b29055e2ba6864147dbd2f51c07898d7a0fcaf1d
김연진 <love_homebody@naver.com>
2023-10-28 17:52:29 +0900

    feat(Car): 자동차 객체 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private final int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
