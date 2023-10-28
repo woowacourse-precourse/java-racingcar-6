0e0959d8f8c0f65c5be1b80a5da18034d56dd4e2
olsohee <dlthgml0108@daum.net>
2023-10-29 00:09:16 +0900

    feat(OutputView): OutputView 클래스 생성, 출력문 상수 처리

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public final class OutputView {
+
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자";
+    private static final String COLON = " : ";
+    private static final String HYPHEN = "-";
+}
