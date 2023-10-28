592d4b3bdbf14860ace629a63622e5f5e16ba8a4
박준영 <wnsdud6969@naver.com>
2023-10-28 17:54:00 +0900

    feat(OutputView): add OutputView Class to manage user output view
    Add new feature to verify singleton
    - private static OutputView instance
    - private constructor
    - public static getInstance()

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+
+public class OutputView {
+
+    private static OutputView outputView;
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        if (outputView == null) {
+            outputView = new OutputView();
+        }
+        return outputView;
+    }
+}
