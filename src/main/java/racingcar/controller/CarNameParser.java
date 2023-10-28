d1be2de93ded652d3c56ceb00e6a0d60dbebb2c1
qncj5224 <qncj5224@naver.com>
2023-10-28 13:46:29 +0900

    feat: 배열을 리스트로 변환하는 메서드 구현

--- src/main/java/racingcar/controller/CarNameParser.java
+++ src/main/java/racingcar/controller/CarNameParser.java
@@ -1,9 +1,15 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarNameParser {
   public  String[] carNamesToArray(String carNames) {
     return carNames.split(",");
   }
 
-
+  public static List<String> carNameToList(String[] carNamesArray) {
+    List<String> carNamesList = Arrays.asList(carNamesArray);
+    return carNamesList;
+  }
 }
