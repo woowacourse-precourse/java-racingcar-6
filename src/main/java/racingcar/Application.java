7abd9c707c12b760bbd32a15c08b160e8d5e1438
SungWoo <sungwoo166@gmail.com>
2023-10-28 23:24:58 +0900

    feat: Implement main entry point for racing car game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RaceGameController;
+import racingcar.model.InputValidator;
+import racingcar.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View();
+        InputValidator inputValidator = new InputValidator();
+        RaceGameController raceGameController = new RaceGameController(view, inputValidator);
+
+        raceGameController.start();
     }
 }
