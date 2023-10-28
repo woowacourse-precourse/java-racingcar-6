09b9269bf8394d5bf29eda345a0a0645cb7fbd1a
donghoony <aru0504@naver.com>
2023-10-28 23:13:06 +0900

    feat(RacingCarList): 자동차 리스트를 초기화하고 검증하는 기능 구현
    - 자동차의 이름에는 중복된 값이 존재해서는 안 됨
    - 자동차는 1대 이상 10대 이하가 주어져야 함
    - 주어지는 자동차 이름에 대한 검증은 해당 클래스에서 논외이므로 다루지 않음

--- src/main/java/racingcar/RacingCarList.java
+++ src/main/java/racingcar/RacingCarList.java
@@ -9,4 +9,41 @@ public class RacingCarList {
 
     private final List<RacingCar> racingCars = new ArrayList<>();
 
+    public void addRacingCarsByNames(List<String> names) {
+        validateNameList(names);
+        names.stream()
+                .map(RacingCar::new)
+                .forEach(racingCars::add);
+    }
+
+    private void validateNameList(List<String> names) {
+        validateDistinctName(names);
+        validateNameCount(names);
+    }
+
+    private void validateDistinctName(List<String> names) {
+        long distinctNameCount = names.stream()
+                .distinct()
+                .count();
+        if (distinctNameCount < names.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameCount(List<String> names) {
+        validateNameCountLargerThanMinimumValue(names);
+        validateNameCountSmallerThanMaximumValue(names);
+    }
+
+    private void validateNameCountLargerThanMinimumValue(List<String> names) {
+        if (names.size() < MIN_LIST_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameCountSmallerThanMaximumValue(List<String> names) {
+        if (names.size() > MAX_LIST_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
