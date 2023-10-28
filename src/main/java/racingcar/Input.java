5c23cfb88ff1c56a0c166375523fc993ac8291c5
2021110806 <2021110806@dgu.ac.kr>
2023-10-28 17:53:03 +0900

    feat(Input.java): Add getCarName method 자동차의 이름을 사용자로부터 입력받는 기능을 추가하였습니다.

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    static String getCarName() {
+        String carNameInput = Console.readLine();
+        return (carNameInput);
+    }
+}
