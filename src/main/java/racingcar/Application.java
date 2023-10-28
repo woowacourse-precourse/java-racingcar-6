4a6e75bca25b9db2a26a3eaff3c03d26f51e303d
noxknow <chris2769@naver.com>
2023-10-28 18:08:34 +0900

    feat(RacingCarGameController) : ConsoleOutput에 대한 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,13 +2,16 @@ package racingcar;
 
 import racingcar.controller.RacingCarGameController;
 import racingcar.handler.InputHandler;
+import racingcar.handler.OutputHandler;
 import racingcar.view.ConsoleInput;
+import racingcar.view.ConsoleOutput;
 
 public class Application {
     public static void main(String[] args) {
 
         final InputHandler inputHandler = new ConsoleInput();
+        final OutputHandler outputHandler = new ConsoleOutput();
 
-        new RacingCarGameController(inputHandler).run();
+        new RacingCarGameController(inputHandler, outputHandler).run();
     }
 }
--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -3,15 +3,18 @@ package racingcar.controller;
 import racingcar.domain.RacingCar;
 import racingcar.domain.TryCount;
 import racingcar.handler.InputHandler;
+import racingcar.handler.OutputHandler;
 
 import java.util.List;
 
 public class RacingCarGameController {
 
     private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
 
-    public RacingCarGameController(InputHandler inputHandler) {
+    public RacingCarGameController(InputHandler inputHandler, OutputHandler outputHandler) {
         this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
     }
 
     public void run() {
@@ -20,12 +23,14 @@ public class RacingCarGameController {
     }
 
     public RacingCar loadCarNames() {
+        outputHandler.printInputCarNameMessage();
         List<String> carNames = inputHandler.inputCarNames();
 
         return new RacingCar(carNames);
     }
 
     public TryCount loadTryCount() {
+        outputHandler.printInputTryCountMessage();
         int tryCount = inputHandler.inputTryCount();
 
         return new TryCount(tryCount);
