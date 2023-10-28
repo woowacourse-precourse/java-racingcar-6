135a226ff08279b95c6736033237a1a53ea16fa1
Haeun Jeong <gkdms4248@gmail.com>
2023-10-28 15:44:05 +0900

    test(model): 자동차 전진 기능 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,16 +3,33 @@ package racingcar.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("pobi");
+    }
 
     @DisplayName("위치 반환 기능 테스트")
     @Test
     void testGetPosition() {
         int expected = 0;
-        Car car = new Car("pobi");
+
+        assertEquals(expected, car.getPosition());
+    }
+
+    @DisplayName("자동차 전진 기능 테스트")
+    @Test
+    void testMoveForward() {
+        int expected = 2;
+
+        car.moveForward();
+        car.moveForward();
 
         assertEquals(expected, car.getPosition());
     }
