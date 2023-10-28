2edbe2164d515d4b9548ca6b4a22101689aed838
yooooonshine <hl3470@naver.com>
2023-10-28 22:45:04 +0900

    feat: 상수 클래스 구현

---
+++ src/main/java/racingcar/constant/GameConstants.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public class GameConstants {
+    private GameConstants() {
+    }
+
+
+    public static String START_GAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+}
