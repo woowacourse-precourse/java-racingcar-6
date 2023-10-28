c1d8d5ee3aea76b7ea8844eab3f2ddc4b4d6f608
hansg931 <hansg0509@cau.ac.kr>
2023-10-28 22:04:54 +0900

    feat: 유저 입력 메소드 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,24 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Constant;
+import racingcar.util.Message;
+
+import java.util.List;
+import java.util.Arrays;
+
 public class InputView {
+	public static List<String> inputCarName() {
+		OutputView.printMessage(Message.INPUT_CAR_NAME_MESSAGE);
+		String userInput = Console.readLine();
+
+		return Arrays.asList(userInput.split(Constant.INPUT_NAME_DELIMITER));
+	}
+
+	public static int inputTryNumber() {
+		OutputView.printMessage(Message.INPUT_TRY_NUMBER_MESSAGE);
+		String userInput = Console.readLine();
+
+		return Integer.parseInt(userInput);
+	}
 }
