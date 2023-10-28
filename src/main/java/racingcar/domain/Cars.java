4bc6831362f427bc78b63007c21975c1c59c7a6b
LastCow9000 <sys19912002@hanmail.net>
2023-10-28 21:47:56 +0900

    feat: 자동차 리스트에 추가하는 기능 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static final String DUPLICATE_ERROR_MESSAGE = "이미 존재하는 자동차 이름입니다.";
+    private final List<Car> cars;
+
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void add(String name) {
+        validateDuplicate(name);
+        cars.add(Car.getNewCar(name));
+    }
+
+    private void validateDuplicate(String newName) {
+        cars.stream()
+                .map(Car::getName)
+                .filter(name -> name.equals(newName))
+                .findAny()
+                .ifPresent(result -> {
+                    throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+                });
+    }
+}
