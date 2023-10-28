69c0854fbc3caeccb015fbe0b7ed3ce2494a3766
BH946 <kobh2895@gmail.com>
2023-10-28 21:01:35 +0900

    fix: 예외 테스트 실패로 인해 오류 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,7 +14,13 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         CarGenerator cars = new CarGenerator();
         System.out.println("시도할 회수는 몇회인가요?");
-        Integer count = Integer.parseInt(Console.readLine());
+        Integer count=0;
+        try {
+            count = Integer.parseInt(Console.readLine());
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력하세요. 예외 강제 발생.");
+        }
 
         //run
         System.out.println("\n실행 결과");
--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -12,6 +12,11 @@ public class CarGenerator {
             inNames[i][1] = "";
         }
         this.names = inNames;
+        // 예외처리 추가확인 - 글자수 5이하
+        for(int i=0; i<inStr.length; i++) {
+            if(inStr[i].length()>5) throw new IllegalArgumentException("5자 이하만 입력 가능합니다.");
+        }
+
     }
 
     public String[][] getNames() { return this.names; }
