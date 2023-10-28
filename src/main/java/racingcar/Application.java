f9b5dd000172f829258b68d8985a32ea030434ec
yunjeong <stac_cato@naver.com>
2023-10-29 03:12:19 +0900

    feat: 자동차 m개의 이름과 n번의 경주 시도 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<String> askCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String input = readLine();
+
+        List<String> names = new ArrayList<>();
+        for (String name : input.split(",")) {
+            names.add(String.valueOf(name));
+        }
+        return names;
+    }
+
+    public static int askRound(){
+        String input = readLine();
+        int round = Integer.parseInt(input);
+        return round;
     }
 }
