c852010206e881b5248d4b51d771dba7a31eea17
Jiyoung Choi <12201817@inha.edu>
2023-10-28 15:42:42 +0900

    refactor(InputValidator) : 자동차 이름의 유효성을 검사하는 함수 수정

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -12,25 +12,27 @@ public class InputValidator {
     private final static int MAX_LENGTH_OF_CAR_NAME= 5;
     private final static int MIN_SIZE_OF_COUNT = 0;
 
-    public static void validateRacingCarsName(String racingCarsName){
+    public static List<String> validateRacingCarsName(String racingCarsName){
         if(isEmpty(racingCarsName)){
             ExceptionMessage.INPUT_NO_CAR_NAME_MESSAGE.throwException();
         }
         if(isOutOfRange(convertStringToList(racingCarsName))){
             ExceptionMessage.INPUT_WRONG_CAR_LENGTH_MESSAGE.throwException();
         }
+        return convertStringToList(racingCarsName);
     }
 
-    public static void validateCount(String count){
+    public static int validateCount(String count){
         if(isEmpty(count)){
             ExceptionMessage.INPUT_NO_COUNT_MESSAGE.throwException();
         }
         if(!isNumber(count)){
             ExceptionMessage.INPUT_NOT_NUMBER_MESSAGE.throwException();
         }
-        if(!isOverZero(count)){
+        if(!isOverZero(convertStringToInt(count))){
             ExceptionMessage.INPUT_WRONG_RANGE_OF_COUNT_MESSAGE.throwException();
         }
+        return convertStringToInt(count);
     }
 
     private static boolean isEmpty(String inputString) {
@@ -38,18 +40,22 @@ public class InputValidator {
     }
 
     private static boolean isOutOfRange(List<String> racingCarsName) {
-        return racingCarsName.stream().allMatch(racingCarName -> racingCarName.length()<=MAX_LENGTH_OF_CAR_NAME);
+        return racingCarsName.stream().anyMatch(racingCarName -> racingCarName.length()>MAX_LENGTH_OF_CAR_NAME);
     }
 
     private static boolean isNumber(String count) {
         return count.chars().allMatch(Character::isDigit);
     }
 
-    private static boolean isOverZero(String count) {
-        return Integer.parseInt(count)>MIN_SIZE_OF_COUNT;
+    private static boolean isOverZero(int count) {
+        return count>MIN_SIZE_OF_COUNT;
     }
 
     private static List<String> convertStringToList(String racingCarsName){
         return Arrays.asList(racingCarsName.replace(spaceString, blankString).split(commaString));
+    }
+
+    private static int convertStringToInt(String count) {
+        return Integer.parseInt(count);
     }
 }
