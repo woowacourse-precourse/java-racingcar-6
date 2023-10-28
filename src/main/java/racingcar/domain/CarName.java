f4e9a1508031c83431b99f72b5c46c4e9a60a378
msung99 <msung6924@naver.com>
2023-10-28 09:01:05 +0900

    refactor: Name 의 유효성 검증, 기타 기능 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -7,21 +7,24 @@ public class CarName {
     private final String name;
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
+    private static final String SPACE_INCLUDED_ERROR_MESSAGE = "이름에 공백이 포함될 수 없습니다.";
+    private static final String LENGTH_ERROR_INCLUDE = "이름은 1글자 이상, 5글자 이하를 입력해야 합니다.";
 
     public CarName(String name){
-        validateLength();
-        validateEmptySpaceInclude();
+        validateLength(name);
+        validateEmptySpaceInclude(name);
+        this.name = name;
     }
 
     private void validateLength(String name){
         int nameLength = name.length();
         if(nameLength < MIN_LENGTH || nameLength > MAX_LENGTH){
-            throw new IllegalArgumentException("이름은 1글자 이상, 5글자 이하를 입력해야 합니다.");
+            throw new IllegalArgumentException(LENGTH_ERROR_INCLUDE);
         }
     }
 
     private void validateEmptySpaceInclude(String name){
-        if(name.contains()){
+        if(name.contains(SPACE_INCLUDED_ERROR_MESSAGE)){
             throw new IllegalArgumentException("이름에 공백이 포함될 수 없습니다.");
         }
     }
