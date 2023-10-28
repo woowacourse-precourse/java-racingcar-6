663c6f5187af4e616ce7dee8204414b0a13a48b7
enduf7686 <enduf768640@gmail.com>
2023-10-28 17:17:51 +0900

    refactor(car): change magic number to constant

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import static racingcar.constant.RandomNumberConstant.MAXIMUM_PICK_RANDOM_NUMBER;
+import static racingcar.constant.RandomNumberConstant.MINIMUM_PICK_RANDOM_NUMBER;
+import static racingcar.constant.RandomNumberConstant.MOVE_FORWARD;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
@@ -10,7 +14,6 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
-        this.distance = 0;
     }
 
     public String getName() {
@@ -22,8 +25,8 @@ public class Car {
     }
 
     public void moveForward() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            distance += 1;
+        if (Randoms.pickNumberInRange(MINIMUM_PICK_RANDOM_NUMBER, MAXIMUM_PICK_RANDOM_NUMBER) >= MOVE_FORWARD) {
+            distance++;
         }
     }
 }
---
+++ src/main/java/racingcar/constant/RandomNumberConstant.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public class RandomNumberConstant {
+
+    public static final int MOVE_FORWARD = 4;
+    public static final int MINIMUM_PICK_RANDOM_NUMBER = 0;
+    public static final int MAXIMUM_PICK_RANDOM_NUMBER = 9;
+
+}
