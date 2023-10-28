929359753533a94f6f3aa2f6cb0f6975ede336f0
hansg931 <hansg0509@cau.ac.kr>
2023-10-28 21:24:43 +0900

    feat: 상수 처리

--- src/main/java/racingcar/util/Constant.java
+++ src/main/java/racingcar/util/Constant.java
@@ -1,4 +1,10 @@
 package racingcar.util;
 
 public class Constant {
+	public static final int NAME_LENGTH_LIMIT = 5;
+	public static final int RANDOM_NUMBER_MIN = 0;
+	public static final int RANDOM_NUMBER_MAX = 9;
+	public static final int RANDOM_NUMBER_THRESHOLD = 4;
+	public static final String INPUT_NAME_DELIMITER = ",";
+	public static final String CAR_MOVE_SIGN = "-";
 }
