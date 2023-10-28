e7697040f897c646ce42011a77708b82e8619e8a
Mr.Kim <joajy98@gmail.com>
2023-10-28 20:17:09 +0900

    feat: 모든 자동차에 대해 예외를 확인하는 메서드

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -1,10 +1,21 @@
 package racingcar.validator;
 
+import racingcar.entity.Car;
+
+import java.util.List;
+
 public class CarValidator {
 
     private static final int CHECK_IF_TRY_COUNT_LESS_THAN_ZERO = 0;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String STRING_REGEX = "^[\\w]*$";
+
+    public static void checkAllException(List<Car> carList){
+        for(Car carName : carList){
+            checkCarNameLength(carName.getCarName());
+            checkCarNameType(carName.getCarName());
+        }
+    }
 
     public static void checkCarNameLength(String carName) {
         if (carName.length() > MAX_CAR_NAME_LENGTH || carName.isEmpty()) {
