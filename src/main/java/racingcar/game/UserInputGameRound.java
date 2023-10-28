1343250ccabc65305bda5aa154ff30f3e9cd0c3a
혜민 <alddlv81@gmail.com>
2023-10-28 19:37:19 +0900

    feat: 횟수 입력 예외 - 입력값이 0~9 사이가 아닐 경우 오류

--- src/main/java/racingcar/game/UserInputGameRound.java
+++ src/main/java/racingcar/game/UserInputGameRound.java
@@ -7,4 +7,14 @@ public class UserInputGameRound {
         System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
     }
+
+    private static int checkIsNumber(String gameRound) {
+        for (char digit : gameRound.toCharArray()) {
+            int num = digit - '0';
+            if (num < 0 || num > 9) {
+                throw new IllegalArgumentException("0부터 9까지의 숫자를 입력해주세요.");
+            }
+        }
+        return Integer.parseInt(gameRound);
+    }
 }
