6bfa7b0e9ba9faace770f14a562efb82c47adc76
cousim46 <wjdghldns24@naver.com>
2023-10-29 01:11:38 +0900

    fix : 메서드이름 변경

--- src/main/java/racingcar/output/PrintService.java
+++ src/main/java/racingcar/output/PrintService.java
@@ -1,7 +1,7 @@
 package racingcar.output;
 
 public class PrintService {
-    public static void inputRequest() {
+    public static void inputCarNameRequest() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
