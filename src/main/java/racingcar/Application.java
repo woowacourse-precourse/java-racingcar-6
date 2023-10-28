56040821bc47b6cd51fa537838dc629d40c492b0
Mr.Kim <joajy98@gmail.com>
2023-10-29 01:46:25 +0900

    feat: 메인에 경주 게임 시작 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingController.start();
     }
 }
