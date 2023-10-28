86d604f6efc463c68ce51b01f73d0bdcf08db062
hYe807n <rlagkdus55@gmail.com>
2023-10-29 02:04:51 +0900

    Fix: winnerCheck() 우승차 판별 기능 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.model.CarModel;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController();
+
+        racingController.carNamesProcess();
+        racingController.repeatNumberProcess();
+        racingController.racingStart();
+        racingController.resultWinner();
     }
 }
