c5b0f2feead2aa756adf5ae9cf5ef3ccaa1f14fa
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-28 20:01:55 +0900

    test(CarNameValidatorTest) : 시도횟수입력 검증 - 양수int범위초과 입력 테스트 추가

--- src/test/java/racingcar/CarNameValidatorTest.java
+++ src/test/java/racingcar/CarNameValidatorTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,13 @@ public class CarNameValidatorTest {
         String attempCount = "21";
         AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
         assertDoesNotThrow(() -> attemptCountValidator.getValidatedAttemptCount(attempCount));
+    }
+
+    @DisplayName("시도횟수 입력 - 양수int범위 초과")
+    @Test
+    void checkAttempOutOfRangePlusInt() {
+        String attempCount = "1231231241351351351325134123413431";
+        AttemptCountValidator attemptCountValidator = new AttemptCountValidator(blankValidator);
+        assertThrows(IllegalArgumentException.class, () -> attemptCountValidator.getValidatedAttemptCount(attempCount));
     }
 }
