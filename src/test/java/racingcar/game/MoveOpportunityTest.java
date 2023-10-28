b0816b33b9f735aeebc210ab47ac8c7cff5e6e15
dukcode <duk9741@gmail.com>
2023-10-28 22:05:31 +0900

    test: MoveOpportunity 0이하에서 move 시 예외 발생 테스트 추가

--- src/test/java/racingcar/game/MoveOpportunityTest.java
+++ src/test/java/racingcar/game/MoveOpportunityTest.java
@@ -1,20 +1,35 @@
 package racingcar.game;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 
 class MoveOpportunityTest {
 
     @Test
-    public void moveCount가_0_이하면_예외를_발생시킨다() throws Exception {
+    public void MoveOpportunity_객체_생성_시_moveOpportunity가_0_이하면_예외를_발생시킨다() throws Exception {
         // given
         int moveCount = -1;
 
         // when
         //then
-        Assertions.assertThatThrownBy(() -> {
+        assertThatThrownBy(() -> {
             new MoveOpportunity(moveCount);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void move_메서드_호출_시_moveOpporotunity가_0_이하면_예외를_발생시킨다() throws Exception {
+        // given
+        int moveCount = 1;
+        MoveOpportunity moveOpportunity = new MoveOpportunity(moveCount);
+
+        // when
+        //then
+        assertThatThrownBy(() -> {
+            MoveOpportunity moveOpportunity0 = moveOpportunity.move();
+            moveOpportunity0.move();
+        }).isInstanceOf(IllegalStateException.class);
     }
 
 }
