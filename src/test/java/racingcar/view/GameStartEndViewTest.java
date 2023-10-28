e7fdf70cea9cbf65b1b082ccc1e853e356dce61f
uhanuu <hwryu990927@gmail.com>
2023-10-28 14:24:15 +0900

    test: view test

--- src/test/java/racingcar/view/GameStartEndViewTest.java
+++ src/test/java/racingcar/view/GameStartEndViewTest.java
@@ -3,6 +3,10 @@ package racingcar.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.OutputTestSupport;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.RaceCars;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static racingcar.constants.MessageConstants.ATTEMPT_COUNT_MESSAGE;
@@ -29,4 +33,18 @@ class GameStartEndViewTest extends OutputTestSupport {
         // then
         assertThat(outputWithoutTrim()).isEqualTo(ATTEMPT_COUNT_MESSAGE);
     }
+
+    @Test
+    @DisplayName("최종 우승한 자동차의 이름을 출력할 수 있다.")
+    public void printWinningCarNames() {
+        // given
+        String name = "haen";
+        RaceCars raceCars = new RaceCars(List.of(new Car(name)));
+        String winningCarNames = raceCars.getWinningCarNames();
+        // when
+        gameStartEndView.printWinningCarNames(winningCarNames);
+        // then
+        assertThat(outputWithoutTrim()).isEqualTo("최종 우승자 : " + name);
+    }
+
 }
