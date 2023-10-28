880fc5ad8cc84cd5b2677e122b9fbd82cc54f07b
liarreez <liarreez@gmail.com>
2023-10-28 17:29:12 +0900

    style : 불필요한 import 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,9 @@
 package racingcar;
 
 import racingcar.controller.RacingcarController;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
-import racingcar.view.InputView;
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         RacingcarController racingcarController = appConfig.racingcarController();
 
