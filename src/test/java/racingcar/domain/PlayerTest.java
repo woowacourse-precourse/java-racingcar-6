52c6c5a691fc5cff42bf15030e0628589d11a9b4
leeedohyun <ldh19738@gmail.com>
2023-10-28 12:52:29 +0900

    test: 중복된 이름 예외 테스트

--- src/test/java/racingcar/domain/PlayerTest.java
+++ src/test/java/racingcar/domain/PlayerTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
@@ -28,5 +29,18 @@ class PlayerTest {
 
         // then
         assertThat(strings).contains("pobi");
+    }
+
+    @Test
+    public void 중복된_이름이_있으면_예외_발생() {
+        // given
+        Player player = new Player("pobi,woni,pobi");
+
+        // when
+        String[] namesOfCars = player.splitNamesOfCars();
+
+        // then
+        Assertions.assertThatThrownBy(() -> player.validateDuplicationName(namesOfCars))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
