ccb5172b13d1fb3ad3f07c2f455c21b9c10d48ab
donghoony <aru0504@naver.com>
2023-10-28 23:08:55 +0900

    feat(RacingCarList): 클라이언트에서 자동차 관리를 위해 사용할 클래스 구현
    - 게임 로직에서 자동차가 직접 드러나면 캡슐화가 약해진다
    - 최대한 숨길 수 있는 것을 숨기기 위해 클래스를 사용해 래핑
    - 모든 자동차에게 전진 신호, 각 자동차들의 상태들을 가져오는 메서드 등을 구현 예정

---
+++ src/main/java/racingcar/RacingCarList.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarList {
+    private static final int MIN_LIST_LENGTH = 1;
+    private static final int MAX_LIST_LENGTH = 10;
+
+    private final List<RacingCar> racingCars = new ArrayList<>();
+
+}
