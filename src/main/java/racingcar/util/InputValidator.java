8462ebe1895f625f62bcec90b214d02e47da6fed
SeolHuiGwan9478 <shg5977@naver.com>
2023-10-28 23:32:48 +0900

    feature(util/InputValidator): 다양한 유효성 검사 함수 구현
    - imsMinLengthValid -> 입력한 이름의 길이가 최소 길이인 1이상인지 확인
    - isNumericValid -> 입력이 숫자인지 유효성 검사
    - isMinValueValid -> 이동 횟수가 최소 값인 1 이상인지 확인
    
    Breaks isNumericValue는 IllegalArgumentException 예외를 던지도록 처리하였음.

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,8 +1,30 @@
 package racingcar.util;
 
 public class InputValidator {
+    // 이동 횟수 관련 상수
+    private static final int MIN_VALUE = 1;
+    // 문자열 길이 관련 상수
+    private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
-    public Boolean isMaxLengthValid(String input){
-        return input.length() <= MAX_LENGTH;
+
+    public Boolean isMinLengthValid(String name){
+        return name.length() >= MIN_LENGTH;
+    }
+
+    public Boolean isMaxLengthValid(String name){
+        return name.length() <= MAX_LENGTH;
+    }
+
+    public Boolean isNumericValid(String moveCount){
+        try{
+            Integer.parseInt(moveCount);
+            return true;
+        }catch(Exception e){
+            throw new IllegalArgumentException("Input Error: not numeric");
+        }
+    }
+    // 유효성 검사: 이동 횟수가 1이상인지
+    public Boolean isMinValueValid(Integer moveCount){
+        return moveCount >= MIN_VALUE;
     }
 }
