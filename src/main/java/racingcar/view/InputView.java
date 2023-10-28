d44cbd56e7d084173995862c83906af50d603cf0
JH-TT <leejongho9803@gmail.com>
2023-10-29 01:42:34 +0900

    feat(InputView) - 상수값 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String LENGTHERROR = "이름은 5자 이하만 가능합니다.";
+    private static final String LENGTH_ERROR = "이름은 5자 이하만 가능합니다.";
+    private static final String IS_NOT_NUMBER = "숫자를 입력해 주세요.";
+    private static final String IS_NOT_NATURAL_NUMBER = "전진횟수는 자연수여야 합니다.";
+    private static final Character ZERO = '0';
+    private static final Character NINE = '9';
     public String[] inputPlayers() {
         String players = Console.readLine();
         return splitCommaAndValidate(players);
