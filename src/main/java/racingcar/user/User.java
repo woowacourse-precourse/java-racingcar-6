7584e7d2994bf14cf9ecf75c5bfb21fb95f37500
ajou20658 <kwy1379@ajou.ac.kr>
2023-10-28 13:33:11 +0900

    Validator를 상속받는 User객체 생성 사용자로부터의 입력과 사용자에게의 출력을 담당함.

---
+++ src/main/java/racingcar/user/User.java
@@ -0,0 +1,35 @@
+package racingcar.user;
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.car.Car;
+
+import java.util.List;
+
+public class User extends Validator{
+    public String[] carNameInput(){
+        System.out.println(MessageConstant.GAME_START_MESSAGE.getMessage());
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        for(String i:carNames){
+            carNameCheck(i);
+        }
+        return carNames;
+    }
+    public int gameRoundInput(){
+        System.out.println(MessageConstant.ROUND_SELECT_MESSAGE.getMessage());
+        String input = Console.readLine();
+        gameRoundCheck(input);
+        return Integer.parseInt(input);
+    }
+    public void gameRoundOutput(List<Car> carList){
+        for(Car i : carList){
+            String printFormat = i.getName()+" : "+"-".repeat(i.getScore());
+            System.out.println(printFormat);
+        }
+        System.out.println();
+    }
+    public void gameWinnerOutput(List<String> winnerList){
+        String result = String.join(", ", winnerList);
+        System.out.println(MessageConstant.WINNER_MESSAGE.getMessage()+result);
+    }
+
+}
