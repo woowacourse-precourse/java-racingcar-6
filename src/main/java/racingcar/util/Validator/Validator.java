901348e5ec4272f7d5f1fa1e896dc27db8abf5d3
jinchiim <nellroll@syuin.ac.kr>
2023-10-29 05:28:42 +0900

    refactor: Validator 수정
    Validator 수정
    - 요구사항에 맞도록 이름이 0개일 경우 Error
    - 추가 요구사항으로 이름이 1개 이하일 경우 게임이 불가능하기 때문에 Error 발생

--- src/main/java/racingcar/util/Validator/Validator.java
+++ src/main/java/racingcar/util/Validator/Validator.java
@@ -9,11 +9,21 @@ public class Validator {
     private static final int MAXIMUM_FORWARD_NUMBER = 4;
 
     public static final int MAXIMUM_NAME_LENGTH = 5;
+    public static final int MINIMUM_NAME_LENGTH = 1;
+
+
+    private static void validateNameEmpty(List<String> names) {
+        if (names.size() <= 1) {
+            throw new InputException(InputError.NOT_EXCEED_MINIMUM_AMOUNT);
+        }
+    }
 
     public static void validateNameLength(List<String> names) {
 
+        validateNameEmpty(names);
+
         long countNames = names.stream()
-                .filter(name -> name.length() < MAXIMUM_NAME_LENGTH)
+                .filter(name -> name.length() < MAXIMUM_NAME_LENGTH && name.length() != MINIMUM_NAME_LENGTH)
                 .count();
 
         if (countNames != names.size()) {
