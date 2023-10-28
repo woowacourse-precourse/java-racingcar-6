f837b5064555252ceaef25a0bc10220996b40d9c
34suuuuu <psh7492@gmail.com>
2023-10-28 16:48:01 +0900

    feat: Car 클래스 생성

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,2 +1,24 @@
-package racingcar.car;public class Car {
+package racingcar.car;
+
+import racingcar.Constants;
+import racingcar.Util;
+
+public class Car {
+    private final String NAME;
+    private int position = 0;
+
+    public Car(String name) {
+        this.NAME = name;
+        this.position = 0;
+    }
+
+    public void go(){
+        if (Util.getRandomNum() >= Constants.GO_NUM) {
+            position++;
+        }
+    }
+
+
+
+
 }
