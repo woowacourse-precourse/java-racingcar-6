0613f7b8e983c9404693bb3c8619b770f2937d7e
kim <hwiyul_g@daum.net>
2023-10-28 17:51:27 +0900

    refactor(RaceManager) : 비지니스 로직을 담당하는 manager 클래스에 상태 제거

--- src/main/java/racingcar/manager/RaceManager.java
+++ src/main/java/racingcar/manager/RaceManager.java
@@ -1,26 +1,19 @@
 package racingcar.manager;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.model.Car;
 import racingcar.model.RaceResult;
 
 public class RaceManager {
 
-    private final List<Car> cars;
-    // TODO : attemptCount의 책임을 고려할 것.
-    private int attemptCount;
 
-    public RaceManager(List<Car> cars, int attemptCount) {
-        this.cars = cars;
-        this.attemptCount = attemptCount;
+    public RaceManager() {
     }
 
-    public RaceResult playRound() {
+    public RaceResult playRound(List<Car> cars) {
         for (Car car : cars) {
             car.attemptMove();
         }
-        attemptCount--;
         return new RaceResult(cars);
     }
 
