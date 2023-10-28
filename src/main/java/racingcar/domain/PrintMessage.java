397972f44180839d3bedf0a66f93620d46814138
euniiiii <hee9920@naver.com>
2023-10-28 20:10:34 +0900

    feat(domain): PrintMessage 클래스 추가

---
+++ src/main/java/racingcar/domain/PrintMessage.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class PrintMessage {
+
+    private PrintMessage() { }
+
+    public static final String INPUT_RACING_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+    public static final String PRINT_EXECUTION_RESULT = "실행 결과";
+    public static final String PRINT_FINAL_WINNER = "최종 우승자 : ";
+    public static final String ONLY_SPLIT_COMMA = "차 이름은 쉽표로 구분해야합니다.";
+    public static final String FIVE_OR_LESS = "차 이름은 5자 이하로 해야합니다.";
+    public static final String NOT_DUPLICATE_NAME = "중복된 이름이 있습니다.";
+}
