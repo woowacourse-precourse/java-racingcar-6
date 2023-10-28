452d96c292ae21535ccaf27ac79b4d5dad83818d
HeeHeeg <qmffndoden@naver.com>
2023-10-28 20:38:31 +0900

    git commit -m "feat(racingcar): 0에서 9 사이의 무작위 값을 구하고 그 값이 4 이상일 경우 전진하고 아니면 멈춰있는 기능 구현"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String inputName = Console.readLine();
         List<String> names = Arrays.asList(inputName.split(","));
 
@@ -17,9 +19,20 @@ public class Application {
             carList.add(new Car(name));
         }
 
-        for (Car car : carList) {
-            String printCarName = car.printCarName();
-            System.out.println(printCarName);
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNumber = Console.readLine();
+        int attemptsNumber = Integer.parseInt(inputNumber);
+
+        for (int i = 0; i < attemptsNumber; i++) {
+            for (Car car : carList) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                if (randomNumber >= 4) {
+                    car.move();
+                }
+
+                System.out.println(car.printCarName());
+            }
+            System.out.println();
         }
 
         System.out.println("시도할 회수는 몇회인가요?");
