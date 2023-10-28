46ca3ff127fb911063aad53b65390e9874bf6081
이진모 <jinmlee210@naver.com>
2023-10-28 20:30:13 +0900

    feat: checkInputCar 사용자의 자동차 이름 입력값이 이상없는지 확인하는 메서드 기능구현

--- src/main/java/racingcar/util/User.java
+++ src/main/java/racingcar/util/User.java
@@ -2,13 +2,28 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class User {
     public List<String> inputCar() {
         String input = Console.readLine();
         List<String> carName = Arrays.asList(input.split(","));
+        checkInputCar(carName);
         return carName;
+    }
+
+    public void checkInputCar(List<String> carName){
+        HashSet<String> DuplicateName = new HashSet<>();
+        DuplicateName.addAll(carName);
+        if(carName.size() != DuplicateName.size()){
+            throw new IllegalArgumentException();
+        }
+        for(int i = 0; i < carName.size(); i++){
+            if(carName.get(i).length() > 5){
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public int inputTryNumber(){
