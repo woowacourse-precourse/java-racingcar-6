c020216bd45efc88596f51481ca3bfa9b1ad5a46
sehee <tpgml701@naver.com>
2023-10-29 00:38:51 +0900

    feat: String 분리, String -> int 변환 유틸 구현

--- src/main/java/racingcar/util/Paser.java
+++ src/main/java/racingcar/util/Paser.java
@@ -1,2 +1,17 @@
-package racingcar.util;public class Paser {
+package racingcar.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Paser {
+    public int ParseInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public List<String> parseUserInputToList(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
 }
