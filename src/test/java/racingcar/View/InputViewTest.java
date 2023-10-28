719fe79059a4126a36dcdf0d2e32460781f214d5
sami355-24 <ddung355@gmail.com>
2023-10-28 22:59:17 +0900

    test(View): given-when-then 추가
    가독성을 위해 행동을 추가하였음

--- src/test/java/racingcar/View/InputViewTest.java
+++ src/test/java/racingcar/View/InputViewTest.java
@@ -11,17 +11,27 @@ class InputViewTest {
 
     @Test
     void inputCarName() {
+        //given
         String testCarNames = "test1,test2,test3";
         System.setIn(new ByteArrayInputStream(testCarNames.getBytes()));
+
+        //when
         String result = getCarName();
+
+        //then
         assertEquals("test1,test2,test3", result);
     }
 
     @Test
     void inputNumberOfRound() {
+        //given
         String testNumberOfRound = "5";
         System.setIn(new ByteArrayInputStream(testNumberOfRound.getBytes()));
+
+        //when
         String result = getNumberOfRound();
+
+        //then
         assertEquals("5", result);
     }
 }
