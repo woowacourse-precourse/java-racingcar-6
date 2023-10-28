f2c889cc7edefaf4ac7e33b5f2e0964d114c91af
kes02 <hijin1220@naver.com>
2023-10-28 18:42:04 +0900

    Feat: Game Result print
    게임이 시작되고 콘솔창으로 보이는 실행결과 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printException(String message) {
         System.out.println(message);
     }
+
+    public static void printPlayResult() {
+        System.out.println("실행 결과");
+    }
 }
