f6d0d949d53f4ba938c0285007d271185df5e4fa
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-28 17:15:38 +0900

    feat : 자동차 이름 입력 받기 기능 추가

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,18 @@
+package racingcar.controller;
+import racingcar.view.InputView;
+public class GameController {
+    private final InputView inputView;
+
+    public GameController() {
+        this.inputView = new InputView();
+    }
+
+    public void runGame(){
+        initGame();
+    }
+
+
+    public void initGame(){
+        String userCarNames = inputView.inputCarNames();
+    }
+}
