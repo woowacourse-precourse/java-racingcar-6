23f0d8a96f70cf6ceda49591f8114abd05a24062
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 22:38:12 +0900

    feat : 무작위 숫자 4 이상인지 확인

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -2,4 +2,8 @@ package racingcar.controller;
 
 public class RacingCarGame {
 
+    public static boolean checkNumberIs4Over(int number) {
+        return number >= 4;
+    }
+    
 }
--- src/main/java/racingcar/model/Number.java
+++ src/main/java/racingcar/model/Number.java
@@ -3,7 +3,6 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
-
     public static int generateNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
