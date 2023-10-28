1ef681762593446d9698c84c93acc60e253283f8
KIVSH <kihongkim3@hotmail.com>
2023-10-28 21:18:46 +0900

    feat: compose RacingLogic class

---
+++ src/main/java/racingcar/RacingLogic.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import java.util.List;
+
+public class RacingLogic {
+    void repeatRaceAsTryTimes(List<Car> carList, int tryTime) {
+
+    }
+
+    List<Integer> decideGoOrStopByRandoms() {
+        return null;
+    }
+
+    List<Boolean> isGreaterThanOrEqualToFour(List<Integer> randomNumberList) {
+        return null;
+    }
+
+    void saveRaceData(List<Car> carList, List<Boolean> goOrStopList) {
+
+    }
+
+    List<Integer> checkWinner(List<Car> carList) {
+        return null;
+    }
+
+    void saveWinnerDate(List<Integer> winnerList) {
+
+    }
+}
