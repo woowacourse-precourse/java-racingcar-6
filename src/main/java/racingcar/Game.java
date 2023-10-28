1be9350bba4380e9a4181d120f3b6449297f65f0
takoyakimchi <c4nyou@gmail.com>
2023-10-28 20:17:11 +0900

    fix(game): remove Console.close()
    it causes error

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -17,7 +17,6 @@ public class Game {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String input = Console.readLine();
-        Console.close();
 
         List<String> names = Arrays.asList(input.split(","));
         Validation.validateCarNames(names);
@@ -33,7 +32,6 @@ public class Game {
         System.out.println("시도할 회수는 몇회인가요?");
 
         String input = Console.readLine();
-        Console.close();
 
         Validation.validateGameCount(input);
         return Integer.parseInt(input);
