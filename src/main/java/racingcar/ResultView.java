af16d75da74c98ac4681003dce1808f269e06185
박찬건 <jg9870@naver.com>
2023-10-28 16:02:00 +0900

    feat(racingcar): 최종 우승자 출력 형식 변경

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -14,7 +14,7 @@ public class ResultView {
 
     public static void printWinners(List<Car> cars) {
         String winners = findWinners(cars);
-        System.out.println("최종 우승자 : " + winners);
+        System.out.print("최종 우승자 : " + winners);
     }
 
     private static String findWinners(List<Car> carList) {
