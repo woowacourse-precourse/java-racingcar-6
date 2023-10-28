75b34202ceeb0f674024bab64fdb03773da238fd
김효준 <khyojun1104@gmail.com>
2023-10-28 22:40:36 +0900

    feat(CarRace): OutputView - 실행결과를 위한 출력 메서드 구현!

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static OutputView instance;
@@ -15,10 +17,21 @@ public class OutputView {
     }
 
     public void printInitGame() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printBeforeInputGameCount() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printRoundResult(List<String> carResults) {
+        for(int i =0; i<carResults.size(); i++){
+           System.out.println(carResults.get(i));
+        }
+        System.out.println();
+    }
+
+    public void notifyBeforeResult() {
+        System.out.println("\n실행결과");
     }
 }
