c0624a129b65a91f96e748a896a8d4f4ec5b6858
sami355-24 <ddung355@gmail.com>
2023-10-28 21:03:47 +0900

    feat(Message): 자동차 이름 관련한 예외 발생시 출력한 문구들을 모아놓은 enum 클래스 생성

---
+++ src/main/java/Message/CarNameExceptionPrompt.java
@@ -0,0 +1,18 @@
+package Message;
+
+public enum CarNameExceptionPrompt {
+
+    LENGTHMESSAGE("자동차 이름은 1글자 이상 5자 이하만 가능합니다.\n"),
+    WHITESPACEMESSAGE("자동차 이름에 공백이 포함되어 있습니다.\n"),
+    DUPLICATEMESSAGE("자동차 이름이 중복되었습니다.\n");
+
+    private String message;
+
+    CarNameExceptionPrompt(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
