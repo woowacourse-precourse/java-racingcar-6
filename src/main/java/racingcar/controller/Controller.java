6c3136b12bd4811d018dc48906352a90ab51dc57
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-28 22:12:31 +0900

    feat: 시도 회수 입력 처리 기능 개발
    - InputView를 통해 시도 회수 요청 및 입력
    - 입력값을 통해 TrialNumber 객체 초기화

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -2,11 +2,13 @@ package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.Cars;
+import racingcar.model.TrialNumber;
 import racingcar.utils.Parser;
 import racingcar.view.InputView;
 
 public class Controller {
     Cars cars;
+    TrialNumber trialNumber;
 
     /**
      * 자동차 이름을 입력 받기
@@ -15,5 +17,15 @@ public class Controller {
         InputView.requestCarsName();
         String carsName = Console.readLine();
         cars = new Cars(Parser.parseCarsName(carsName));
+    }
+
+    /**
+     * 시도 회수 입력 받기
+     */
+    private void getTrialNumberByUserInput() {
+        InputView.requestTrialNumber();
+        String inputNumber = Console.readLine();
+        System.out.println();
+        trialNumber = new TrialNumber(inputNumber);
     }
 }
