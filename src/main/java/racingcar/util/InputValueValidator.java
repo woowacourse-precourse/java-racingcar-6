b48f68b8cf1ce365613f3cfa8344a45e2c152352
jeondui <jeondui@gmail.com>
2023-10-28 21:58:09 +0900

    feat(InputValueValidator): 자동차 이름 입력값의 유효성 검사 기능

--- src/main/java/racingcar/util/InputValueValidator.java
+++ src/main/java/racingcar/util/InputValueValidator.java
@@ -7,6 +7,18 @@ import java.util.Set;
 
 public class InputValueValidator {
 
+    public List<String> validateName(String inputCarNames) {
+        List<String> carNames = converStringToList(inputCarNames);
+
+        // TODO: Null 값, 공백 미포함 유효성 검사 추가
+        if (!isDuplicateName(carNames)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 이름은 중복을 허용하지 않습니다.");
+        } else if (!isNameLength(carNames)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 이름 길이는 5자 이하만 가능합니다.");
+        }
+        return carNames;
+    }
+
     private boolean isDuplicateName(List<String> carNames) {
         Set<String> set = new HashSet<>(carNames);
         return set.size() == carNames.size();
