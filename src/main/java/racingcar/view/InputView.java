b1a0d3dac66f9571d936aca31c9a72d13e090b71
qncj5224 <qncj5224@naver.com>
2023-10-28 14:41:41 +0900

    feat: 회수 입력 받을때 예외처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,15 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.FrequencyValidator;
 
 public class InputView {
+
+  private static FrequencyValidator frequencyValidator;
+
+  public InputView(){
+    frequencyValidator = new FrequencyValidator();
+  }
   public String inputCarName() {
     OutputView.printInputNameMessage();
     return Console.readLine();
@@ -11,6 +18,8 @@ public class InputView {
   public int inputFrequency() {
     OutputView.printInputFrequencyMessage();
     String frequency = Console.readLine();
+    frequencyValidator.isCorrectFrequencyNumber(frequency);
+    frequencyValidator.isNegativeNumber(frequency);
     return Integer.parseInt(frequency);
   }
 
