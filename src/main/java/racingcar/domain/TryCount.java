54f764bed1f2e080b46b8c3183d9deb9dc6790e8
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-28 16:05:15 +0900

    [feat] : add try count class get try count method

---
+++ src/main/java/racingcar/domain/TryCount.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class TryCount {
+    private int tryCount = 0;
+
+    public TryCount(int tryCount) {
+        this.tryCount = tryCount;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+}
