c0c7ed1c7756b9ca1d06f962d6854993893c653b
SUJI-J <suzy852@naver.com>
2023-10-28 19:07:03 +0900

    feat: 문자열을 해시맵으로 변경 후 키값 중복 체크 기능 구현

--- src/main/java/racingcar/InputConvertor.java
+++ src/main/java/racingcar/InputConvertor.java
@@ -1,12 +1,23 @@
 package racingcar;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static racingcar.GameMessage.INPUT_DUPLICATE_EXCEPTION_MESSAGE;
 
 public class InputConvertor {
 
-    public static List<String> toList(String inputCarNames) {
-        return Arrays.asList(inputCarNames.split(","));
+    public static Map<String, String> toMap(String inputCarNames) {
+        Map<String, String> carNamesMap = new HashMap<>();
+        String[] tempStr = inputCarNames.split(",");
+        for (String str : tempStr) {
+            if (carNamesMap.containsKey(str)) {
+                throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MESSAGE);
+            }
+            carNamesMap.put(str, "");
+        }
+        return carNamesMap;
     }
 
     public static int toInt(String inputNumber) {
