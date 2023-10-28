4843caa5c0b980a65bfda0cf738b257dda9c5367
bin <gimgyubin1225@gmail.com>
2023-10-28 16:15:43 +0900

    Feat: 자동차 이름 부여 기능 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int distance;
+    private static final int LIMIT_NAME_LENGTH = 5;
+
+    public Car(String name) {
+        if(name.length() > LIMIT_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+}
