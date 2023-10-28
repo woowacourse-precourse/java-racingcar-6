bb745a4827b9654a54926813832576137634ac5d
WooYoungDoo <duden5000@naver.com>
2023-10-29 00:50:15 +0900

    feat (InputView) : 입력 받는 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputCarName() {
+        return Console.readLine();
+    }
+
+    public String inputNumber() {
+        return Console.readLine();
+    }
+}
