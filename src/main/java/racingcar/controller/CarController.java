099ae6d1f5cdc40ba56bad63fa29cfde87408115
박성춘 <songchon100@gmail.com>
2023-10-28 14:31:39 +0900

    feat : Car Names를 통해 필요 인스턴스 생성

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,4 +1,40 @@
 package racingcar.controller;
 
+import racingcar.service.CarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class CarController {
+    private InputView inputView;
+    private OutputView outputView;
+    private CarService carService;
+
+    public CarController(InputView inputView, OutputView outputView, CarService carService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.carService = carService;
+    }
+
+    public static CarController getInstance() {
+        return new CarController(InputView.getInstance(), OutputView.getInstance(), CarService.getInstance());
+    }
+
+    public void run() {
+        String carNames = inputView.readCarNames();
+        Integer tryNum = inputView.readTryNum();
+        makeCars(carNames);
+        racingStart(tryNum);
+    }
+
+    public void makeCars(String carNames) {
+        carService.generateCarsInstance(carNames);
+    }
+
+    public void racingStart(Integer tryNum) {
+        while(tryNum --> 0) {
+
+        }
+    }
+
+
 }
