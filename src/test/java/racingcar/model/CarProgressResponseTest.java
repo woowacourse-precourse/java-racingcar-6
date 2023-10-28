c8dc64a34b57e709a69d2c6d0b88430fea2fedc5
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-28 17:45:03 +0900

    test: 차량 정보 객체 테스트 케이스 작성

---
+++ src/test/java/racingcar/model/CarProgressResponseTest.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CarProgressResponseTest {
+    private final int result = 3;
+    @Test
+    void constructor_성공() {
+        final String name = "benz";
+
+        CarProgressResponse response = new CarProgressResponse(name, result);
+
+        assertAll(
+            () -> assertThat(response.name()).isEqualTo(name),
+            () -> assertThat(response.result()).isEqualTo(result)
+        );
+    }
+    @Test
+    void constructor_차량_이름은_필수() {
+        assertThatNullPointerException()
+            .isThrownBy(() -> new CarProgressResponse(null, result));
+    }
+}
