c1f503cba0ba80ddfeac51cbe3d3ea437f5e606d
Mr.Kim <joajy98@gmail.com>
2023-10-29 01:52:37 +0900

    feat: 최종 우승자 목록을 출력하는 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
@@ -26,4 +27,32 @@ public class OutputView {
         System.out.println();
     }
 
+    public static void printWinnerList(List<Car> carList){
+        System.out.print(WINNER_LIST);
+        int winnerDistance = findMaximumDistance(carList);
+        List<Car> winnerList = findWinnerList(carList, winnerDistance);
+        int size = winnerList.size();
+        for (int i = 0; i < size - 1; ++i) {
+            if(carList.get(i).getCarPosition() < winnerDistance) continue;
+            System.out.print(winnerList.get(i).getCarName() + ", ");
+        }
+        System.out.println(winnerList.get(size - 1).getCarName());
+    }
+
+    private static List<Car> findWinnerList(List<Car> carList, int winnerDistance) {
+        List<Car> winnerList = new ArrayList<>();
+        for (Car car : carList) {
+            if(car.getCarPosition() < winnerDistance) continue;
+            winnerList.add(new Car(car.getCarName()));
+        }
+        return winnerList;
+    }
+
+    public static int findMaximumDistance(List<Car> carList){
+        int maxDistance = 0;
+        for (Car car : carList) {
+            maxDistance = Math.max(maxDistance, car.getCarPosition());
+        }
+        return maxDistance;
+    }
 }
