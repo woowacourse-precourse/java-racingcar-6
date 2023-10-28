12e716b0af4c14125cdcbdff804d9986e4f516c5
unknown <y2kf9a0q@gmail.com>
2023-10-29 00:26:46 +0900

    test: CarTest.java - move forward and stop

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,8 +29,20 @@ class CarTest {
         assertThat(result).isEqualTo("default");
     }
 
-    @Test
-    void move() {
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void move_forward(int parameter) {
+        int before = car.getDistance();
+        car.move(parameter);
+        assertThat(car.getDistance()).isEqualTo(before + 1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void move_stop(int parameter) {
+        int before = car.getDistance();
+        car.move(parameter);
+        assertThat(car.getDistance()).isEqualTo(before);
     }
 
     @Test
