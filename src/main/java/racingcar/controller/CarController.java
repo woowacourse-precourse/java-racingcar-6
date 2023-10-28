4041a259b6f8b56c370bf93facb63eea2a12e49b
kg9585 <kg9585@naver.com>
2023-10-28 22:04:42 +0900

    feat(CarController): 자동차의 위치와 이름 메소드 생성

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,23 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.util.CarHandler;
+
+public class CarController {
+    private final Car car;
+    private final CarHandler carHandler;
+    public CarController(Car car, CarHandler carHandler) {
+        this.car = car;
+        this.carHandler = carHandler;
+    }
+    public String getCarName() {
+        return car.getName();
+    }
+    public int getCarPosition() {
+        return car.getPosition();
+    }
+
+    public void moveCar(int randomValue) {
+        carHandler.go(randomValue);
+    }
+}
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -11,5 +11,16 @@ public class Car {
         this.name = name;
         this.position = 0;
     }
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 
 }
