d498caf56348973cb7587d03fe42f250acf01a79
soyeonii <dingdingcrong@naver.com>
2023-10-28 13:43:28 +0900

    refactor(GameView): 메서드명을 더 명확하고 가독성 있게 변경

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -4,19 +4,19 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class GameView {
-    public void displayUserInputCarNamePrompt() {
+    public void promptForCarNames() {
         displayMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void displayUserInputRoundPrompt() {
+    public void promptForRoundCount() {
         displayMessage("시도할 회수는 몇회인가요?");
     }
 
-    public void displayResultMessage() {
+    public void displayExecutionResults() {
         displayMessage("실행 결과");
     }
 
-    public void displayOneRoundResultMessage(List<Car> cars) {
+    public void displayRoundResults(List<Car> cars) {
         for (Car car : cars) {
             car.printStatus();
         }
