d90f58359a5b9275f24c0b1f055b2d1a4921683d
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 23:02:16 +0900

    feat : 게임 기본 출력 메시지 추가

---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+public class Output {
+
+    public static void printStartMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printAttemptNumberMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printWinners() {
+        System.out.println();
+    }
+}
