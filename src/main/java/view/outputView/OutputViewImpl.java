090a52510c3b3838a0701da2cb703f636288446a
hongseokhodev <hongseokhodev@gmail.com>
2023-10-28 14:14:02 +0900

    feat: 시도 횟수 입력 메시지를 출력한다

--- src/main/java/view/outputView/OutputViewImpl.java
+++ src/main/java/view/outputView/OutputViewImpl.java
@@ -3,11 +3,16 @@ package view.outputView;
 import java.util.List;
 
 public class OutputViewImpl {
+    private static final String READ_ATTEMPT_MESSAGE = "시도할 회수는 몇회인가요?";
     public void printWinners(List<String> winners) {
         System.out.println(String.join(", ", winners));
     }
 
     public void printResults(List<String> results) {
         results.forEach(System.out::println);
+    }
+
+    public void printReadAttemptMessage() {
+        System.out.println(READ_ATTEMPT_MESSAGE);
     }
 }
