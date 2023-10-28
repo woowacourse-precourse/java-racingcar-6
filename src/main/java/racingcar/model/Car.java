e9457a689bf0a6941cacf2c0086e162d8de83373
yoonseo choi <choiyoonseo02@gmail.com>
2023-10-29 04:20:24 +0900

    feat: 자동차 이름을 쉼표로 구분하는 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,23 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.view.InputView;
+
 public class Car {
+    private List<String> name;
+
+    public Car() {
+        name = splitCarNames();
+    }
+
+    private static List<String> splitCarNames() {
+        List<String> carNameList = new ArrayList<>();
+        String carName = InputView.inputCarName();
+
+        carNameList.addAll(Arrays.asList(carName.split(",")));
+
+        return carNameList;
+    }
 }
