2ec7ac04f0cadd0b399f9b309d52d9444eea669c
deohyunpark <qwertyshsh@naver.com>
2023-10-28 15:58:03 +0900

    refactor: make magic number to static final

--- src/main/java/racingcar/validation/RacingCarValidation.java
+++ src/main/java/racingcar/validation/RacingCarValidation.java
@@ -6,6 +6,7 @@ import racingcar.domain.Car;
 public class RacingCarValidation {
     public static final String CAR_LIST_IS_NOT_AVAIL = "최소 두 개 이상의 차 이름을 입력해주세요.";
     public static final String CAR_NAME_LENGTH_IS_NOT_AVAIL = "차 이름은 다섯글자로 입력해주세요";
+    public static final int CAR_NAME_LIMIT_LENGTH = 5;
 
     public static void isNotAvailCarList(List<Car> carList) {
         int size = carList.size();
@@ -15,7 +16,7 @@ public class RacingCarValidation {
     }
 
     public static void isNotAvailCarName(List<Car> carList) {
-        boolean invalidCarNameExists = carList.stream().anyMatch(car -> car.getName().length() > 5);
+        boolean invalidCarNameExists = carList.stream().anyMatch(car -> car.getName().length() > CAR_NAME_LIMIT_LENGTH);
         if (invalidCarNameExists) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_IS_NOT_AVAIL);
         }
