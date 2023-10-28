784c328a3922f50d848b397678d3b839e84d283f
SeongUk52 <rkfka7849@gmail.com>
2023-10-28 19:44:33 +0900

    fix(WinnerChecker): 스트림을 사용한 메서드 롤백
    다중 우승자 처리 불가능해서 원래 버전으로 되돌림

--- src/main/java/racingcar/service/WinnerChecker.java
+++ src/main/java/racingcar/service/WinnerChecker.java
@@ -1,16 +1,23 @@
 package racingcar.service;
 
-import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toList;
 
 import java.util.List;
 import racingcar.domain.Car;
 
 public class WinnerChecker {
-
     public static List<Car> findWinner(List<Car> carList) {
+        int maxProgress = getMaxProgress(carList);
         return carList.stream()
-                .max(comparingInt(Car::getProgressToInt))
-                .stream()
-                .toList();
+                .filter(i -> i.getProgressToInt() == maxProgress)
+                .collect(toList());
+    }
+
+    // TODO 2023 10 28 두 메서드 대신 하나의 스트림으로 구현할 방법 찾기
+    private static int getMaxProgress(List<Car> carList) {
+        return carList.stream()
+                .mapToInt(Car::getProgressToInt)
+                .max()
+                .getAsInt();
     }
 }
