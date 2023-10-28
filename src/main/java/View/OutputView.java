e50738f956d6ad9c9d3472dc6dc0910363b8e267
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:54:21 +0900

    refactor : 우승자 문구 출력 기능 추가

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -2,9 +2,13 @@ package View;
 
 public class OutputView {
     private static final String resultMessage = "실행 결과";
-
+    private static final String winnerMessage = "최종 우승자 : ";
     public static void printResultMessage(){
         System.out.println(resultMessage);
+    }
+
+    public static void printWinnerMessage(){
+        System.out.print(winnerMessage);
     }
 
 }
