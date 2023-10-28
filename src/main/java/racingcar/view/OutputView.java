c46802a4255fe54bb30ce6468a83edd45990023e
DaeYoungK <hanguk830@naver.com>
2023-10-28 19:32:40 +0900

    fix(OutputView): 반복출력되는 실행 결과 제거를 위해 메서드 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,8 +5,11 @@ public class OutputView {
     private final String EXECUTION_RESULT = "실행 결과";
     private final String WINNER = "최종 우승자 : ";
 
-    public void printExecutionResult(String carList) {
+    public void printExecutionResult() {
         System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printCarRacingResult(String carList) {
         System.out.println(carList);
     }
 
