047da23ea0bc4834aa47b781c6acc042fe09c25b
jhw1015 <jhw1015@bank-mall.co.kr>
2023-10-29 00:59:32 +0900

    feat : 자동차 이름에 대한 검증 메서드 추가

--- src/main/java/racingcar/validate/RacingCarValidate.java
+++ src/main/java/racingcar/validate/RacingCarValidate.java
@@ -2,4 +2,10 @@ package racingcar.validate;
 
 public class RacingCarValidate {
 
+    public static void validateNameLength(String carName) {
+        if(carName.isBlank() || carName.length() > 5){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
