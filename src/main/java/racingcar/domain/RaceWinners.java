9ac9b97916aac61593233fbeeec3a87b68e8e372
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-28 15:32:37 +0900

    feat: RaceWinners maxPosition을 찾고 같은 position인 리스트 리턴

--- src/main/java/racingcar/domain/RaceWinners.java
+++ src/main/java/racingcar/domain/RaceWinners.java
@@ -7,10 +7,28 @@ public class RaceWinners {
     private final List<String> winners;
 
     private RaceWinners(RaceParticipants raceParticipants) {
+        this.winners = determineWinners(raceParticipants);
     }
 
     public static RaceWinners from(RaceParticipants raceParticipants) {
         return new RaceWinners(raceParticipants);
+    }
+
+    private List<String> determineWinners(RaceParticipants raceParticipants) {
+        List<RacingCar> racingCarList = raceParticipants.getRacingCarList();
+        int maxPosition = findMaxPosition(racingCarList);
+        return findWinningCarNames(racingCarList, maxPosition);
+    }
+
+    private List<String> findWinningCarNames(List<RacingCar> racingCarList, int maxPosition) {
+        return racingCarList.stream()
+                .filter(racingCar -> racingCar.getPosition() == maxPosition)
+                .map(RacingCar::getName)
+                .toList();
+    }
+
+    private int findMaxPosition(List<RacingCar> racingCarList) {
+        return racingCarList.stream().mapToInt(RacingCar::getPosition).max().orElse(0);
     }
 
     public List<String> getWinners() {
