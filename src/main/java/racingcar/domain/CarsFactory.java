1e6d5b45a3e2b1239816573412628e092ac6c5e6
Annjieun <dkslwdms2121@naver.com>
2023-10-28 18:47:44 +0900

    feat(domain): 자동차 이름을 입력받고, 쉼표로 구분하여 반환하는 inputCarNames() 구현

--- src/main/java/racingcar/domain/CarsFactory.java
+++ src/main/java/racingcar/domain/CarsFactory.java
@@ -1,10 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarsFactory {
     private final List<Car> cars = new ArrayList<>();
+
+    public List<String> inputCarNames() {
+        String inputNames = Console.readLine();
+        List<String> carNames = Arrays.asList(inputNames.split(","));
+        validateCarNames(carNames);
+
+        return carNames;
+    }
 
     public List<Car> getCars() {
         return cars;
