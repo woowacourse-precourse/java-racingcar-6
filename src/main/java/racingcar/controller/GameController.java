d09ce9eb099c4136820498de7b54998c73202be3
김효준 <khyojun1104@gmail.com>
2023-10-28 22:41:23 +0900

    feat(CarRace): 앞선 실행결과를 출력하기 위한 Controller 로직 작성!

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,7 +1,10 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.service.CarGameService;
+import racingcar.service.RaceService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -10,11 +13,13 @@ public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
     private final CarGameService carGameService;
+    private final RaceService raceService;
 
     public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.carGameService = new CarGameService();
+        this.raceService = new RaceService();
     }
 
     public void start() {
@@ -22,10 +27,19 @@ public class GameController {
         List<String> carNames = carGameService.extractSeperator(inputView.inputCarName());
         outputView.printBeforeInputGameCount();
         Integer gameCount = carGameService.convertGameCountToNumber(inputView.inputGameCount());
+        List<Car> cars = new ArrayList<>();
+        carGameService.initCarInfo(carNames, cars);
+        outputView.notifyBeforeResult();
+        for(int count=0; count<gameCount; count++){
+            List<String> roundResult = raceService.raceRoundResult(cars);
+            printRoundResult(roundResult);
+        }
 
 
+    }
 
-
+    private void printRoundResult(List<String> result) {
+        outputView.printRoundResult(result);
     }
 
 }
