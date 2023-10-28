5bfe215c45866e937e841569901ffbfe72381f51
jeongjihun <wlgns2234@naver.com>
2023-10-28 11:34:48 +0900

    feat: 랜덤숫자가 0~9가 아니라면 예외를 발생하는 기능 구현

--- src/test/java/racingcar/model/RandomNumberTest.java
+++ src/test/java/racingcar/model/RandomNumberTest.java
@@ -1,12 +1,21 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 class RandomNumberTest {
+
+    @DisplayName("RandomNumber 범위 0~9 테스트 : 0~9 사이의 숫자가 아니라면 IllegalArgumentException가 발생한다.")
+    @Test
+    void random_number_range_0_to_9() {
+        assertThatThrownBy(() -> new RandomNumber(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("랜덤 생성 숫자는 0~9사이의 숫자여야 합니다.");
+    }
 
     @DisplayName("create 생성 테스트 : RandomNumber 생성 시 특정 숫자를 부여할 수 있다.")
     @Test
