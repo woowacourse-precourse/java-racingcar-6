6d79ca5cb8f4419306fb26cabc14fcd9fe7bf011
kunsanglee <gosmdochee2@gmail.com>
2023-10-28 14:43:48 +0900

    feat(Application): 자동차 경주 게임 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController(
+                new RacingCarInputView(),
+                new RacingCarOutputView(),
+                new RacingCarInputValidator(),
+                new RacingCarService(),
+                new RacingCarAssemblyService(),
+                new RandomCarEngine()
+        );
+        racingController.run();
     }
 }
