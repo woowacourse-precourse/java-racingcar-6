5513572a545fa03827553e6960f4cadb43a067ed
pjaehyun <show6756@gmail.com>
2023-10-28 19:00:43 +0900

    feat: 추가 게임 시작 함수

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -15,6 +15,34 @@ public class Game {
         this.frequency = stringToInt(frequency);
     }
 
+    public void start(){
+        // 게임이 시작되면 frequency 만큼 반복문을 돌게된다.
+        int maxDistanceCar = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (int i = 0; i < frequency; i++){
+            for (Car car : participants) {
+                car.movingForward();
+                int distance = car.getDistance();
+                if (maxDistanceCar < distance) {
+                    maxDistanceCar = distance;
+                }
+            }
+
+            for (Car participant : participants) {
+                System.out.println(participant.getName() + " : " + "-".repeat(participant.getDistance()));
+            }
+            System.out.println();
+        }
+
+        for (Car participant : participants) {
+            if (participant.getDistance() >= maxDistanceCar){
+                winners.add(participant.getName());
+            }
+        }
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
     public List<Car> stringToCarList(String data) {
         List<Car> res = new ArrayList<>();
         String[] splitData = data.split(",");
