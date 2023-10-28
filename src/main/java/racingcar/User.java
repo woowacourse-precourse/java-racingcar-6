dfed778af821d1beb64fa5058fd4a420f01f10ac
unknown <hakie2kim@gmail.com>
2023-10-28 23:33:20 +0900

    feat(src/main/java/racingcar/User.java): set the trial number of car racing game

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class User {
     private List<String> carName;
+    private int trial;
 
     void setCar() throws IllegalArgumentException {
         String input = Console.readLine();
@@ -24,5 +25,16 @@ public class User {
         }
 
         this.carName = carName;
+    }
+
+    void setTrial() throws IllegalArgumentException {
+        int input = Integer.parseInt(Console.readLine()); // 정수값으로 변환 안될 경우 NumberFormatException 발생
+        Console.close();
+
+        if (input <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.trial = input;
     }
 }
