3f56de67e28139329c01ae221afe671a617d083e
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-29 05:26:29 +0900

    feat(CarNames): 이름이 공백인 경우 예외 처리
    - '아무 입력도 들어오지 않은 경우 예외처리'함수를 제거, 이 기능에 포함
    - 이름의 길이가 0이거나 공백으로만 이루어진 경우 예외 처리

--- src/main/java/racingcar/validation/CarNames.java
+++ src/main/java/racingcar/validation/CarNames.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarNames {
-    void checkNullInput(String in) throws IllegalArgumentException {
-        if (in == null || in.isEmpty()) {
-            throw new IllegalArgumentException("아무 입력도 들어오지 않았습니다.");
-        }
-    }
-
     List<String> splitNames(String in) {
         return Arrays.asList(in.split(",", -1));
+    }
+
+    void checkBlankInput(String in) throws IllegalArgumentException {
+        List<String> names = splitNames(in);
+        names.stream().forEach(name -> {
+            if (isBlankString(name)) {
+                throw new IllegalArgumentException();
+            }
+        });
+    }
+
+    boolean isBlankString(String in) {
+        return in.trim().isEmpty();
     }
 }
--- src/test/java/racingcar/validation/ValidationTest.java
+++ src/test/java/racingcar/validation/ValidationTest.java
@@ -9,13 +9,6 @@ import org.junit.jupiter.api.Test;
 
 public class ValidationTest {
     @Test
-    void 아무_입력도_들어오지_않은_경우_예외_처리() {
-        String input = "";
-        assertThatThrownBy(() -> new CarNames().checkNullInput(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
     void 입력받은_이름을_쪼개기() {
         List<String> inputs = Arrays.asList("pobi,woni,jun", " ,  ",
                 "hi", ",,,", ",a,", "  ");
@@ -26,5 +19,14 @@ public class ValidationTest {
         for (int i = 0; i < inputs.size(); i++) {
             assertThat(new CarNames().splitNames(inputs.get(i))).isEqualTo(results.get(i));
         }
+    }
+
+    @Test
+    void 이름이_공백으로만_이루어진_경우_예외처리() {
+        List<String> inputs = Arrays.asList("", " ", "    ,  ", ",,,,");
+        inputs.stream().forEach(input -> {
+            assertThatThrownBy(() -> new CarNames().checkBlankInput(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
     }
 }
