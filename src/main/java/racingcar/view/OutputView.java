1c0741d41b68620ece8e68101a170adac4f05285
hansg931 <hansg0509@cau.ac.kr>
2023-10-28 22:00:57 +0900

    feat: 콘솔 출력 메소드 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
+import racingcar.util.Message;
+
 public class OutputView {
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+
+	public static void printInputCarNameMessage() {
+		printMessage(Message.INPUT_CAR_NAME_MESSAGE);
+	}
+
+	public static void printInputTryNumberMessage() {
+		printMessage(Message.INPUT_TRY_NUMBER_MESSAGE);
+	}
+
+	public static void printGameStartMessage() {
+		printMessage(Message.GAME_START_MESSAGE);
+	}
+
+	public static void printWinnerMessage(String winner) {
+		printMessage(Message.WINNER_MESSAGE + winner);
+	}
 }
