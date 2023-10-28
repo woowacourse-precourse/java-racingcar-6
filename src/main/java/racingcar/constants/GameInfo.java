5191515e18ee6926babb506abb8073c8e42e04d8
dlrjs2360 <lhg3670@naver.com>
2023-10-28 23:20:37 +0900

    :sparkles: feat: 상수 파일 생성
    - 프로젝트에서 사용하는 view 메시지를 Enum으로 관리합니다.
    - 게임의 설정 요소들을 하나의 클래스로 관리합니다.
    Enum으로도 구분할 수 있으나 여러 데이터 타입이 포함될 수 있음을 고려하여 일반 클래스로 생성했습니다.

--- src/main/java/racingcar/constants/GameInfo.java
+++ src/main/java/racingcar/constants/GameInfo.java
@@ -1,5 +1,9 @@
 package racingcar.constants;
 
 public class GameInfo {
-
+    public static final String SEPARATOR = ",";
+    public static final String SEPARATOR_NAME = "쉼표";
+    public static final int MIN_CAR_NAME_LENGTH = 1;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static String CAR_NAME_REGEX = "^[^\\s,]{1,5},[^\\s,]{1,5},[^\\s,]{1,5}$";
 }
--- src/main/java/racingcar/constants/Messages.java
+++ src/main/java/racingcar/constants/Messages.java
@@ -1,5 +1,18 @@
 package racingcar.constants;
 
-public class Messages {
+public enum Messages {
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요. (이름은 " + GameInfo.SEPARATOR_NAME + "(" + GameInfo.SEPARATOR
+        + ") 기준으로 구분)"),
+    INPUT_CAR_NAMES_ERROR("자동차 이름은 " + GameInfo.SEPARATOR_NAME + "로 구분되며 공백은 허용되지 않습니다.");
 
+
+    private final String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
