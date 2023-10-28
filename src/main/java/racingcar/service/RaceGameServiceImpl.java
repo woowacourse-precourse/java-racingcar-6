1836cc55ba3a5701158db00fd68e48384d368a02
IE-MangChi <hsm983278@gmail.com>
2023-10-28 20:58:26 +0900

    feat: 자동차숫자확인 후 움직이기 기능, 우승자 찾기 기능 구현

---
+++ src/main/java/racingcar/service/RaceGameServiceImpl.java
@@ -0,0 +1,61 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RaceGameServiceImpl implements RaceGameService{
+    @Override
+    public void carMoveCheckAndGo(Car car) {
+        // 전진하는 조건은 0~9사이에서 무작위 값을 구한 후 무작위 값이 4이상일 경우
+        List<Integer> carPositions = car.getCarPosition();
+        carMoveCheck(car, carPositions);
+    }
+
+    private static void carMoveCheck(Car car, List<Integer> carPositions) {
+        for (int i=0; i<carPositions.size(); i++) {
+            if (getRandomValue() > 4) {
+                car.carMove(i);
+            }
+        }
+    }
+
+    private static int getRandomValue() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    @Override
+    public List<Integer> findWinner(Car car) {
+        int maxPosition = findMaxPosition(car);
+        return findWinnerIndex(car, maxPosition);
+    }
+
+    private static List<Integer> findWinnerIndex(Car car, int maxPosition) {
+        List<Integer> carPositions = car.getCarPosition();
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i=0; i<carPositions.size(); i++) {
+            if (carPositions.get(i) == maxPosition) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    private static int findMaxPosition(Car car) {
+        List<Integer> carPositions = car.getCarPosition();
+
+        int maxPosition = 0;
+
+        for (Integer carPosition : carPositions) {
+            if (carPosition > maxPosition) {
+                maxPosition = carPosition;
+            }
+        }
+        return maxPosition;
+    }
+}
