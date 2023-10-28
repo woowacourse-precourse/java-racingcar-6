e99ac6a8f714648924e7dc4b3be65a79a2053d0e
dong2ast <kdh01132006@naver.com>
2023-10-28 21:30:23 +0900

    ♻️ refactor: 사용자에게서 자동차 이름 받아오는 로직 분리

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -8,11 +8,9 @@ import racingcar.domain.Car;
 public class GameService {
 
     public static void run() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
         List<Car> carList = new ArrayList<>();
 
-        checkCarNameAndMakeCar(carNames, carList);
+        checkCarNameAndMakeCar(InteractService.getCarName(), carList);
 
         System.out.println("시도할 회수는 몇회인가요?");
         try {
