4e2ee11f27d076c4e8b6e4eb1df879ee10b7c085
kimD0ngjun <chickentasty0112@gmail.com>
2023-10-28 16:10:38 +0900

    [refactoring] 자동자 전진 기능 메소드 수정

--- src/main/java/racingcar/model/CarEngine.java
+++ src/main/java/racingcar/model/CarEngine.java
@@ -13,18 +13,19 @@ public class CarEngine {
         return false;
     }
 
-    // 전진 반복하는 만큼 "-" 더하기
-    public String countMovement(int count) {
-        String movement = " ";
-        for(int i = 1; i <= count; i++) {
-            if(moveCar()) {
-                movement = movement + "-";
-            }
+    // 전진 반복하는 만큼 "-" 더하기(인자는 이전 턴의 이동 거리)
+    public String countMovement(String movement) {
+        if(moveCar()) {
+            movement = movement + "-";
         }
         return movement;
     }
 
-    public void finishRace() {
-
+    // 이동 횟수 충족(즉 결승선 도달) 검증(인자는 턴마다의 이동 거리와 사용자 입력값)
+    public boolean finishRace(String movement, int count) {
+        if(movement.length() == count - 1) {
+            return true;
+        }
+        return false;
     }
 }
