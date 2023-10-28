88a3266feaedaa3d2dce5b9cbb583d874ec73b02
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 14:51:59 +0900

    feat(Validator): 숫자가 아닌 문자를 입력했을 때 예외처리 구현

--- src/main/java/racingcar/util/message/ErrorMessage.java
+++ src/main/java/racingcar/util/message/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
 
 	CAR_NAME_LENGTH_ERROR("차 이름의 길이는 5이하여야 합니다."),
 	CONTAIN_NUMBER_ERROR("숫자가 아닌 문자만 입력가능합니다."),
+	INPUT_LETTER_ERROR("문자가 아닌 숫자만 입력해야합니다."),
 	PREFIX("[ERROR] ");
 
 	private final String message;	
--- src/main/java/racingcar/util/validator/Validator.java
+++ src/main/java/racingcar/util/validator/Validator.java
@@ -1,11 +1,14 @@
 package racingcar.util.validator;
 
+import java.util.regex.Pattern;
+
 import racingcar.util.message.ErrorMessage;
 
 public class Validator {
 
+	private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
 	private static final int LENGTH = 5;
-
+	
 	public static void validateOutOfLength(String carName) {
 		if (carName.length() > LENGTH) {
 			throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_ERROR.getMessage());
@@ -15,6 +18,12 @@ public class Validator {
 	public static void validateContainNumber(String carName) {
 		if (carName.matches(".*[0-9].*")) {
 			throw new IllegalArgumentException(ErrorMessage.CONTAIN_NUMBER_ERROR.getMessage());
+		}
+	}
+	
+	public static void validateNumberFormat(String inputValue) {
+		if (!NUMBER_PATTERN.matcher(inputValue).matches()) {
+			throw new IllegalArgumentException(ErrorMessage.INPUT_LETTER_ERROR.getMessage());
 		}
 	}
 }
