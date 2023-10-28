33e6962bc29facae075550f83fae2b4186083527
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-28 21:26:18 +0900

    [refactor] : change location to show car name and position method

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,17 @@
 package racingcar.view;
 
+import racingcar.controller.ViewController;
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
-    public static void resultMessage() {
-        System.out.println("실행 결과");
+
+    public static void showCarNameAndPosition(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + ViewController.resultPosition(car));
+        }
+        System.out.println("\n");
     }
 
     public static void finalWinnerMessage() {
