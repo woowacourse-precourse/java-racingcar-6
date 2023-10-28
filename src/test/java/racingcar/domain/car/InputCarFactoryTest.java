c68528c29ef2426fbb8c3f7849946e981f5b4802
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-29 04:07:31 +0900

    test: 입력받은 자동차 이름이 5이하가 아닐 경우 예외 처리 기능 테스트

--- src/test/java/racingcar/domain/car/InputCarFactoryTest.java
+++ src/test/java/racingcar/domain/car/InputCarFactoryTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -44,6 +45,20 @@ class InputCarFactoryTest  {
         //then
         assertThat(cars.size()).isEqualTo(3);
 
+    }
+
+    @Test
+    void 입력받은_자동차_이름이_5글이_이하가_아니라면_에러를_던진다() {
+        //given
+        CarFactory carFactory = new InputCarFactory();
+
+        //when
+        command("aaaa, bbbb, cccccc");
+
+        //then
+        assertThatThrownBy(() -> carFactory.createCars())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
 
     private void command(final String... args) {
