663a6173a2f454e10610cc10a8330c1f90fb0090
slowcloud <sju9417@gmail.com>
2023-10-28 10:29:38 +0900

    test: Car 생성자에 대한 테스트 코드 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,16 +3,24 @@ package racingcar.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CarTest {
 
     @Test
     void Car_다섯자_초과_이름으로_생성() {
-
         String name = "123456789";
-
         Assertions.assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @Test
+    void Car_다섯자_이하_이름으로_생성() {
+        String name = "12345";
+        assertDoesNotThrow(
+                () -> new Car(name)
+        );
     }
 
 }
