0958ba63865f00c3b94e00c35f27ea3f8d0f9769
PUNGZI <ashyoo1367@gmail.com>
2023-10-28 16:49:06 +0900

    feat: 각 자동차에 이름을 부여하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import racingcar.domain.CarName;
-
 public class Application {
     public static void main(String[] args) {
     }
--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class CarName {
+    public final List<String> inputCarName() {
+        String inputCarNames = Console.readLine();
+        return Arrays.asList(inputCarNames.split(","));
+    }
 }
