c6c1c83de112c311881ffea47194e45815f21d7a
zaysverse <zeze@jbnu.ac.kr>
2023-10-29 01:28:34 +0900

    style : 게임 동작 함수 따로 추가
    게임 동작 흐름을 run() 함수에서 주관한다.
    
    - 생성자는 초기화만 해준다.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -11,8 +11,13 @@ public class Game {
     private List<Car> carRepository;
 
     public Game() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carRepository = new ArrayList<>();
+    }
 
+    public void run() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        createCar();
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 
     public void createCar() {
