d44225d24446316e4dcb7e7286ad5d21e097f5e1
leechiyun <wychis28@gmail.com>
2023-10-28 18:51:36 +0900

    feat: 경주할 자동차 이름, 시행 횟수 입력 및 예외처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,18 +9,62 @@ import racingcar.domain.Player;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        inputNames();
 
-        List<Player> players = Arrays.asList(new Player("honi"), new Player("jun"), new Player("woni"));
+        List<Player> players = new ArrayList<>();
+        inputNames().forEach(name -> players.add(new Player(name)));
+
+        int num = inputNumber();
+
+        int max = Integer.MIN_VALUE;
 
         System.out.println("실행 결과");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < num; i++) {
             for (Player player : players) {
                 player.run();
+
+                if (player.getRunCount() > max) {
+                    max = player.getRunCount();
+                }
+
                 player.print();
             }
             System.out.println();
         }
+
+        List<Player> winner = new ArrayList<>();
+        for (Player player : players) {
+            if (player.getRunCount() == max) {
+                winner.add(player);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("최종 우승자 : ");
+        for (int i = 0; i < winner.size(); i++) {
+            sb.append(winner.get(i).getName());
+
+            if(i < winner.size() - 1){
+                sb.append(", ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int inputNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        int num = 0;
+        try {
+            num = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+
+            if(num < 0) {
+                throw new IllegalArgumentException("유효하지 않은 입력입니다, 1 이상의 자연수를 입력해 주세요");
+            }
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("유효하지 않은 입력입니다, 1 이상의 자연수를 입력해 주세요");
+        }
+        return num;
     }
 
     public static List<String> inputNames() {
@@ -47,12 +91,12 @@ public class Application {
 
     private static void validateNames(List<String> names) {
         if (names.isEmpty()) {
-            throw new IllegalArgumentException("이름을 입력해주세요.");
+            throw new IllegalArgumentException("유효하지 않은 입력입니다, 이름을 입력해주세요.");
         }
 
         names.stream().forEach(name -> {
             if (name.length() > 5) {
-                throw new IllegalArgumentException("이름이 5글자가 넘습니다.");
+                throw new IllegalArgumentException("유효하지 않은 입력입니다, 이름이 5글자가 넘습니다.");
             }
         });
     }
--- src/main/java/racingcar/domain/Player.java
+++ src/main/java/racingcar/domain/Player.java
@@ -4,6 +4,22 @@ public class Player {
     private String name;
     private int runCount;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRunCount() {
+        return runCount;
+    }
+
+    public void setRunCount(int runCount) {
+        this.runCount = runCount;
+    }
+
     public Player(String name){
         this.name = name;
         this.runCount = 0;
