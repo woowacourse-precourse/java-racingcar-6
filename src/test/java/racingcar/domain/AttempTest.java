dcfa797ff7c89ade7ff80692d1289b5ba8f38dc3
chjcoder <chjcoder@gmail.com>
2023-10-28 22:43:03 +0900

    test: 자동차 이동 횟수 받아오는 Attemp 클래스 테스트 코드 구현
    - 빈 값 예외처리
    - 숫자가 아닌 값 예외 처리
    - 숫자 0보다 작은 값 예외 처리

---
+++ src/test/java/racingcar/domain/AttempTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AttempTest {
+    @Test
+    void 빈_값_예외_처리() {
+        String input = "";
+
+        Assertions.assertThatThrownBy(() -> new Attemp(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("빈칸입니다.");
+    }
+
+    @Test
+    void 숫자가_아닌_값_예외_처리() {
+        String input = "abc";
+
+        Assertions.assertThatThrownBy(() -> new Attemp(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자를 입력해 주세요.");
+    }
+
+    @Test
+    void 숫자_0_보다_작은_값_예외_처리() {
+        String input = "0";
+
+        Assertions.assertThatThrownBy(() -> new Attemp(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("0보다 큰 수를 입력해 주세요.");
+    }
+}
