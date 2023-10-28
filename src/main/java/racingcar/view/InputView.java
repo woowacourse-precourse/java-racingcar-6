ce4e1177394bbf3395dd8ed912e9c9446d8799d2
Yoonseo <jessicadbstj@gmail.com>
2023-10-28 13:05:32 +0900

    feat: 시도할 횟수 입력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,11 +2,16 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static constant.MessgeList.INPUT_MESSAGE;
+import static constant.MessgeList.INPUT_ATTEMPT_NUMBER_MESSAGE;
+import static constant.MessgeList.INPUT_CAR_NAME_MESSAGE;
 
 public class InputView {
     public String getCarNameFromPlayer(){
-        System.out.println(INPUT_MESSAGE);
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        return Console.readLine();
+    }
+    public String getNumberFromPlayer(){
+        System.out.println(INPUT_ATTEMPT_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
