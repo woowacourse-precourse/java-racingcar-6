3f9cb6e4a2604c9310bcfe7e447421a343ca0e5f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-29 03:54:32 +0900

    feat(model): Car 모델 생성
    - Car 클래스 생성
    - id, carName, carPosition 필드 생성
    - 생성자 추가
    - 게터 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+public class Car {
+    private long id;
+    private String carName;
+    private int carPosition;
+
+    public Car(long id, String carName, int carPosition) {
+        this.id = id;
+        this.carName = carName;
+        this.carPosition = carPosition;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getCarPosition() {
+        return carPosition;
+    }
+}
