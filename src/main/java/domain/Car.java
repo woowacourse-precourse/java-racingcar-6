93d519955f244d35f9d76b87f4b0d7d38c6f535b
song-jae-wan <sjo9810@gmail.com>
2023-10-28 10:56:30 +0900

    Refactor: Car 클래스 주요 메소드 접근제어자 변경
    다른 패키지에서도 사용가능하도록 주요 메소드 접근제어자 public으로 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,24 +5,24 @@ public class Car implements Comparable<Car> {
     private final String name;
     private int distance;
 
-    Car(final String name, int distance) {
+    public Car(final String name, int distance) {
         this.name = name;
         this.distance = distance;
     }
 
-    Car(final String name) {
+    public Car(final String name) {
         this.name = name;
     }
 
 
-    void move(final int value) {
+    public void move(final int value) {
         if(value > 3) {
             distance++;
         }
     }
 
-    int getDistance() { return distance; }
-    String getName() { return name; }
+    public int getDistance() { return distance; }
+    public String getName() { return name; }
 
     @Override
     public int compareTo(Car o) {
