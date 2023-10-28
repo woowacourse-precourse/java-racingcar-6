b17061b0f061953b1ffe68c24176b87a1fd4decf
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 00:30:48 +0900

    feat(OutputView): askCarName 메소드 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import racingcar.view.constants.Messages;
+
+public class OutputView {
+
+    public void askCarName() {
+        System.out.println(Messages.ASK_CAR_NAME.getMessage());
+    }
+
+
+}
