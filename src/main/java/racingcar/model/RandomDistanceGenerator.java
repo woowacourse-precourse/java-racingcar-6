c8905675868c23b9eead94acb946bd47992ed25d
SungWoo <sungwoo166@gmail.com>
2023-10-28 09:19:44 +0900

    feat(model): Implement RandomDistanceGenerator

---
+++ src/main/java/racingcar/model/RandomDistanceGenerator.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomDistanceGenerator {
+
+    private static final int MIN_RANDOM_VALUE = 0;
+    private static final int MAX_RANDOM_VALUE = 9;
+    private static final int MOVE_LIMIT = 4;
+
+    public int generateRandomValue() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+    }
+
+    public int moveDistance() {
+        int randomValue = generateRandomValue();
+        return randomValue >= MOVE_LIMIT ? randomValue : MIN_RANDOM_VALUE;
+    }
+}
