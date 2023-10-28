cb37ad8aab3c404878f96bc03fb6b3b30dadb34a
JUNG GYUWON <milestogo0905@naver.com>
2023-10-28 15:34:39 +0900

    test: 문자열을 , 기준으로 자르는 test 코드 변경
    입력된 문자열이 null이 아닌 빈 문자열일때를 test 하는 코드로 수정

--- src/test/java/racingcar/model/CarCreatorTest.java
+++ src/test/java/racingcar/model/CarCreatorTest.java
@@ -2,12 +2,9 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -18,12 +15,14 @@ public class CarCreatorTest {
     private static CarCreator carCreator;
 
     @BeforeAll
-    public static void setup(){ carCreator = new CarCreator();}
+    public static void setup() {
+        carCreator = new CarCreator();
+    }
 
     @ParameterizedTest
     @DisplayName("문자열을 , 기준으로 자릅니다")
     @MethodSource("playerInputAndExpectedListProvider")
-    public void 문자열_쉼표_기준_자르기(String playerInput, List<String> expectedList){
+    public void 문자열_쉼표_기준_자르기(String playerInput, List<String> expectedList) {
         //when
         List<String> actualList = carCreator.splitPlayerInput(playerInput);
 
@@ -31,11 +30,11 @@ public class CarCreatorTest {
         assertThat(actualList).isEqualTo(expectedList);
     }
 
-    static Stream<Arguments> playerInputAndExpectedListProvider(){
+    static Stream<Arguments> playerInputAndExpectedListProvider() {
         return Stream.of(
-                Arguments.arguments("", Collections.emptyList()),
-                Arguments.arguments("pobbi", Arrays.asList("pobbi")),
-                Arguments.arguments("pobbi,wonni,jun", Arrays.asList("pobbi", "wonni", "jun"))
+                Arguments.arguments("", List.of("")),
+                Arguments.arguments("pobbi", List.of("pobbi")),
+                Arguments.arguments("pobbi,wonni,jun", List.of("pobbi", "wonni", "jun"))
         );
     }
 }
