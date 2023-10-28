f026545e8cbade132e988e2b2ccad8f0074e4855
beomsic <qjatjr29@khu.ac.kr>
2023-10-28 13:11:43 +0900

    :test_tube: Test: Update Car Init Test
    - @ParameterizedTest 를 통해서 여러 케이스의 생성 테스트를 하도록 코드 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,18 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.Car;
 
 @DisplayName("자동차 모델에 대한 테스트")
 class CarTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"beom", "seok", "kevin", "test"})
     @DisplayName("자동차 생성 테스트")
-    void initCar() {
-        // given
-        String carName = "beom";
-
+    void initCar(String carName) {
+        // give
         // when
         Car car = new Car(carName);
 
@@ -23,24 +23,25 @@ class CarTest {
         assertThat(car.getName()).isEqualTo(carName);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"beomsic", "beomseok", "kobeomseok", "testname"})
     @DisplayName("자동차의 이름의 길이가 5글자 초과라면 생성하지 않고 예외를 호출한다.")
-    void init_CarNameLengthThan5_ThrownException() {
+    void init_CarNameLengthThan5_ThrownException(String carName) {
         // given
-        String carName = "beomsic";
-
         // when
         // then
         assertThatThrownBy(() -> new Car(carName)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "   ", "    ", "     "})
     @DisplayName("자동차의 이름으로 빈 문자열이 들어오면 예외를 호출한다.")
     void init_CarNameIsEmpty_ThrownException() {
         // given
         String carName = "";
 
         // when
+
         // then
         assertThatThrownBy(() -> new Car(carName)).isInstanceOf(IllegalArgumentException.class);
     }
