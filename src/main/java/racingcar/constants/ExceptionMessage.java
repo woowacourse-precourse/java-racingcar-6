0bb007155119749d7555a335c0c0bfb2bb9e7b88
pwrwpw <dlgustjr9912@gmail.com>
2023-10-28 23:58:22 +0900

    fix(ExceptionMessage): 의미없는 예외 메시지 삭제
    ,로 슬라이스 하게 될경우 잘못된 쉼표는 "" 문자열을 반환해서 오류 출력이 될 것으로 생각됨
    docs: 의미 없는 예외 메시지 출력 기능 삭제

--- src/main/java/racingcar/constants/ExceptionMessage.java
+++ src/main/java/racingcar/constants/ExceptionMessage.java
@@ -5,9 +5,6 @@ public enum ExceptionMessage {
     CAR_NAME_LIMIT_EXCEEDED("자동차 이름은 5자 이하만 가능합니다."),
     NOT_INPUT_CARS_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
     CARS_LESS_THAN_TWO("자동차 이름은 2개 이상이어야 합니다."),
-    EXCESSIVE_COMMAS_ERROR("쉼표가 연속으로 입력되었습니다."),
-    START_COMMA_ERROR("쉼표로 시작하면 안됩니다."),
-    END_COMMA_ERROR("쉼표로 끝나면 안됩니다."),
     INVALID_CHARACTER_INPUT_ERROR("문자,쉼표만 입력 가능 합니다."),
     INVALID_INPUT_ERROR_MESSAGE("유효하지 않은 입력입니다. 0보다 큰 숫자만 입력 가능합니다.");
     private static final String ERROR_TAG = "[ERROR] ";
