45519e8d5793b36f162b48b2c3b936cca070e9a3
Jaewon <choijawon99@gmail.com>
2023-10-29 00:18:59 +0900

    [FEAT] Game.java 기본 변수, 메서드 생성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,23 @@
 package racingcar;
 
+import java.util.List;
+
 public class Game {
+    private int trial;
+    private int done_trial;
+    private List<Car> cars;
+
+    public Game(List <Car> cars, int trial){
+        this.cars = cars;
+        this.trial = trial;
+        this.done_trial = 0;
+    }
+
+    public List<Car> getCars(){
+        return cars;
+    }
+
+    public void playGame(){
+        this.done_trial++;
+    }
 }
