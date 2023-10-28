b9c79c550c36c129948c6c758b93b4371b814375
kimminsu <xjfcnfw3@naver.com>
2023-10-28 12:56:46 +0900

    fix: 빈 자동차 이름 목록이 검증되지 않는 버그 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
     private static final String COMMA = ",";
 
     public List<String> getCarNames() {
-        return Arrays.stream(Console.readLine().split(COMMA)).toList();
+        List<String> carNames = new ArrayList<>();
+        for (String name : Console.readLine().split(COMMA)) {
+            if (!name.isBlank() && !name.isEmpty()) {
+                carNames.add(name);
+            }
+        }
+        return carNames;
     }
 
     public String getRacingTrial() {
