bccb6acf22eaf9081b7d871774a82217c46ed933
Gukin Han <gukin.dev@gmail.com>
2023-10-28 22:38:41 +0900

    feat: 경주 회수를 입력받고 검증하는 메서드 추가

--- src/main/java/racingcar/Utils.java
+++ src/main/java/racingcar/Utils.java
@@ -10,6 +10,11 @@ import java.util.stream.Collectors;
 
 public class Utils {
     private static final String MESSAGE_TO_INQUIRE_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MESSAGE_TO_INQUIRE_HOW_MANY_TRIES = "시도할 회수는 몇회인가요?";
+
+    private static final Pattern INTEGER_PATTERN = Pattern.compile("^-?\\d+$");
+    private static final String ERROR_NOT_INTEGER = "정수를 입력해주세요.";
+    private static final String ERROR_LESS_THAN_ONE = "0보다 큰 값을 입력해야합니다.";
 
     public static String getCarNamesWithCommaSeparated() {
         System.out.println(MESSAGE_TO_INQUIRE_CARS_NAME);
@@ -22,5 +27,22 @@ public class Utils {
                 .map(String::strip)
                 .filter(name -> !name.isEmpty())
                 .collect(Collectors.toList());
+    }
+
+    public static int getHowManyTries() throws IllegalArgumentException{
+        System.out.println(MESSAGE_TO_INQUIRE_HOW_MANY_TRIES);
+
+        String userInput = Console.readLine();
+        if (!isInteger(userInput)) throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+
+        int howManyTries = Integer.parseInt(userInput);
+        if (howManyTries < 0) throw new IllegalArgumentException(ERROR_LESS_THAN_ONE);
+
+        return howManyTries;
+    }
+
+    public static boolean isInteger(String str) {
+        Matcher matcher = INTEGER_PATTERN.matcher(str);
+        return matcher.matches();
     }
 }
