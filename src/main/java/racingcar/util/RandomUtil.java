9fe2cfdf27dbae3119ffc7a5cf4a7325574db17e
jinchiim <nellroll@syuin.ac.kr>
2023-10-28 22:21:16 +0900

    feat: RandomUtil 생성
    Random Util 생성
    - 테스트 코드가 원활하도록 1주차 코드 리뷰 반영
    - MIN_NUMBER MAX_NUMBER 상수화
    - 랜덤한 숫자를 만들도록 생성

---
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -0,0 +1,19 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtil {
+
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 9;
+
+    Randoms randoms;
+
+    public RandomUtil(Randoms randoms){
+        this.randoms = randoms;
+    }
+
+    public int generateRandomNumber(){
+        return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+    }
+}
