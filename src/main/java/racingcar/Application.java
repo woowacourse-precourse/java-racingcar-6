862a0959b3dcd1a51d341ecfb76d438721a6c13f
KIVSH <kihongkim3@hotmail.com>
2023-10-28 22:37:32 +0900

    feat: compose main method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputAndOutput.outputMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNameList = InputAndOutput.inputCarNames();
+        List<Car> carList = Car.makeCarList(carNameList);
+
+        InputAndOutput.outputMessage("시도할 회수는 몇회인가요?");
+        int tryTimes = InputAndOutput.inputTryTimes();
+
+        InputAndOutput.outputMessage("\n실행 결과");
+        RacingLogic.repeatRaceAsTryTimes(carList, tryTimes);
     }
 }
