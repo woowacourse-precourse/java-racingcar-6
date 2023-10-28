d9411b037c45acded29a727a02111e850f413f41
Hegale <juyeon@gimjuyeon-ui-MacBookAir.local>
2023-10-29 01:37:49 +0900

    fetch: car, game 클래스 및 생성자 추가
    Car 클래스
    - 필드 : 차량의 이름, 현재 위치
    - 생성자 : 필드 초기화
    
    Game 클래스
    - 필드 : 차량들의 이름, 차량의 수
    - 생성자 : 유저의 입력을 인자로 받아 ','를 기준으로 분리해 필드 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Car {
+    String name;
+    int position;
+    Car(String name) {
+        this.name = name;
+        position = 0;
+    }
+}
+
+class Game {
+    String[] carNames;
+    int carCount;
+    Game(String carName) {
+        carsNames = carName.split(",");
+        carCount = carsNames.length;
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
