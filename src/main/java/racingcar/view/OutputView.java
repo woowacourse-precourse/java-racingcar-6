ea8a8dc4b7a8cd1d54db18e2e966b3e3c37324e7
LastCow9000 <sys19912002@hanmail.net>
2023-10-29 01:23:52 +0900

    feat: 이동 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,10 +3,19 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
+    private static final String RESULT_MESSAGE = "\n실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
     private static final String SEPARATOR = ", ";
 
     private OutputView() {
+    }
+
+    public static void printPlayResult() {
+        print(RESULT_MESSAGE);
+    }
+
+    public static void printRaceResult(String result) {
+        print(result);
     }
 
     public static void printWinner(List<String> winners) {
