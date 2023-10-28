427b8d84c851498c256fcf739936b52492396283
JP-company <wjsdj2008@gmail.com>
2023-10-28 19:40:39 +0900

    기능 1. 자동차 이름 입력 기능 [추가]

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,79 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNameList = inputCarNames();
+        System.out.println("carNameList = " + carNameList);
+    }
+
+
+    public static List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarNamesString = Console.readLine().trim();
+        return makeCarNameList(inputCarNamesString);
+    }
+
+    private static List<String> makeCarNameList(String inputCarNamesString) {
+        verifyIfStartOrEndWithComma(inputCarNamesString);
+
+        List<String> carNameList = new ArrayList<>();
+        Arrays.stream(inputCarNamesString.split(","))
+                .forEach(carName -> verifyCarName(carNameList, carName));
+
+        return carNameList;
+    }
+
+    private static void verifyIfStartOrEndWithComma(String inputCarNameString) {
+        if (inputCarNameString.startsWith(",") || inputCarNameString.endsWith(",")) {
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+    }
+
+    private static void verifyCarName(List<String> carNameList, String carName) {
+        String carNameTrim = carName.trim();
+
+        if (carNameTrim.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+
+        if (carNameTrim.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 5자리 이하 이어야 합니다.");
+        }
+
+        if (carNameList.contains(carNameTrim)) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+
+        carNameList.add(carNameTrim);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/racingcar/MethodTest.java
+++ src/test/java/racingcar/MethodTest.java
@@ -1,2 +1,66 @@
-package racingcar;public class MethodTest {
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+
+public class MethodTest {
+
+    @Test
+    void inputCarNames_정상_입력시_리스트반환() {
+        Console.close();
+
+        String testInput = " Jun , Alex , David ";
+        System.setIn(new ByteArrayInputStream(testInput.getBytes()));
+
+        assertThat(Application.inputCarNames()).containsAll(Arrays.asList("Jun", "Alex", "David"));
+    }
+
+    @Test
+    void inputCarNames_자동차이름_공백값_포함시_예외발생() {
+        String[] testInputList = {
+                "Jun,, Alex",
+                "Jun,  , Alex",
+                ",Jun, Alex",
+                "Jun, Alex,",
+                ",",
+                ",,",
+                ",  ,",
+                "   ,,",
+                ",,   ",
+                "   ,,   "
+        };
+
+        for (String testInput : testInputList) {
+            carNameInputErrorOccurs(testInput, "자동차 이름은 공백일 수 없습니다.");
+        }
+    }
+
+    @Test
+    void inputCarNames_자동차이름_5자_이상_입력시_예외발생() {
+        String testInput = "Jun, Alex, JeongPyo";
+        carNameInputErrorOccurs(testInput, "자동차 이름의 길이는 5자리 이하 이어야 합니다.");
+    }
+
+    @Test
+    void inputCarNames_자동차이름_중복시_예외발생() {
+        String testInput = "Jun, Alex, Alex";
+        carNameInputErrorOccurs(testInput, "자동차 이름은 중복될 수 없습니다.");
+    }
+
+    private void carNameInputErrorOccurs(String testInput, String errorMessage) {
+        Console.close();
+        System.setIn(new ByteArrayInputStream(testInput.getBytes()));
+
+        assertThatThrownBy(Application::inputCarNames)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(errorMessage);
+    }
 }
