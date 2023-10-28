5ea05f9be197c734bee2d97ea130e3d76b07bee9
DK <laancer4@gmail.com>
2023-10-28 18:26:10 +0900

    refactor : Cars 클래스 1. 원시값 포장 2. trim + 길이제한 매서드로 공백 판별 가능하므로 isBlank 로 인한 validation 제거 3. validateUserInput 매서드 stream 사용하도록 변경

--- src/main/java/car/Cars.java
+++ src/main/java/car/Cars.java
@@ -1,13 +1,14 @@
 package car;
 
-import org.junit.platform.commons.util.StringUtils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
     private final List<Car> carList;
+    private static final int CAR_NAME_LIMIT = 5;
+    private static final String NAME_LENGTH_INVALID = "이름은 5자 이하만 가능하다.";
+    private static final String SPLIT_DELIMITER = ",";
 
     public Cars(String userInput) {
         this.carList = this.initByStringInput(userInput);
@@ -20,13 +21,13 @@ public class Cars {
     public List<String> getCarNames(){
         return carList.stream().map(Car::getName).toList();
     }
+
     public void moveForTest(){ // 테스트만을 위한 매서드가 존재해도 무방할까?
         this.carList.forEach(Car::addLocation);
     }
 
-    // TODO: 2023/10/28 VALIDATION 을 강화할 필요가 있을까? 고민을 좀 더 해봐야 함.
     private List<Car> initByStringInput(String userInput){
-        String[] carNames = userInput.split(",");
+        String[] carNames = userInput.split(SPLIT_DELIMITER);
         validateUserInput(carNames);
         return Arrays.stream(carNames)
                 .map(String::trim) // 양옆 공백을 제거한다.
@@ -34,23 +35,13 @@ public class Cars {
                 .toList();
     }
 
-    // TODO: 2023/10/28 private 매소드가 과도하게 많으면 클래스를 옮기는 것을 고려하라. 는 조언이 떠오른다.
     private void validateUserInput(String[] carNames){
-        for (String carName : carNames) {
-            validateLength(carName);
-            validateNotBlank(carName);
-        }
-    }
-
-    private void validateNotBlank(String carName) {
-        if (StringUtils.isBlank(carName)){
-            throw new IllegalArgumentException("공백은 입력할 수 없다");
-        }
+        Arrays.stream(carNames).forEach(this::validateLength);
     }
 
     private void validateLength(String carName) {
-        if (carName.length() > 5){
-            throw new IllegalArgumentException("이름은 5자 이하만 가능하다.");
+        if (carName.length() > CAR_NAME_LIMIT){
+            throw new IllegalArgumentException(NAME_LENGTH_INVALID);
         }
     }
 }
