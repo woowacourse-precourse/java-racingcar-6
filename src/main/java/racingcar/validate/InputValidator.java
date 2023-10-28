1e7d1270cd7f1201e5eb8c186b2ae4e2d949f0f2
Minjae-An <mj3242@naver.com>
2023-10-28 17:35:14 +0900

    feat : 차 이름 패턴 검증 기능 구현

--- src/main/java/racingcar/validate/InputValidator.java
+++ src/main/java/racingcar/validate/InputValidator.java
@@ -1,10 +1,13 @@
 package racingcar.validate;
 
+import java.util.regex.Pattern;
 import racingcar.global.GameConfig;
 import racingcar.global.exception.CarGameException;
 import racingcar.global.exception.ErrorMessage;
 
 public abstract class InputValidator {
+    private static final String INPUT_PATTERN="^[a-zA-Z0-9,가-힣]+$";
+
     private static boolean isEmpty(final String input){
         return input.isEmpty();
     }
@@ -13,15 +16,26 @@ public abstract class InputValidator {
         return name.length() <= GameConfig.MAX_CAR_NAME_LENGTH;
     }
 
+    private static boolean isValidInput(final String input){
+        return Pattern.matches(INPUT_PATTERN, input);
+    }
+
     private static boolean isNumber(final String number){
         return number
                 .chars()
                 .allMatch(Character::isDigit);
     }
 
+
     public static void validateEmptyInput(final String input){
         if(isEmpty(input)){
             throw CarGameException.of(ErrorMessage.EMPTY_VALUE);
+        }
+    }
+
+    public static void validateInput(final String input){
+        if(!isValidInput(input)){
+            throw CarGameException.of(ErrorMessage.INVALID_INPUT);
         }
     }
 
