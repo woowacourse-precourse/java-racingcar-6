0b608f1302bf22c0be3a8a2407b21a56a06aaab2
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 12:17:00 +0900

    test(Car): printCarStatus 메소드의 메세지 결과를 테스트한다.

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+	@DisplayName("printCarStatus메소드 출력 형태를 확인한다.")
+	@Test
+	void checkPrintCarStatusMethodPrintMessage() {
+		Car car = new Car("pobi");
+		car.move();
+		car.move();
+
+		String carStatusMessage = car.printCarStatus();
+
+		assertThat(carStatusMessage).isEqualTo("pobi : --");
+	}
+}
