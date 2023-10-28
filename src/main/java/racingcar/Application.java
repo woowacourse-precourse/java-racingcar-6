43957a5859fe8ab8b1bba875d8ba2de5fca6f92f
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-10-28 22:29:08 +0900

    feat: 추가 사용자 입력 처리 기능
    자동차 이름, 회수에 대한 사용자 입력을 받고, 예외처리를 추가함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,10 @@ package racingcar;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     private static final String INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRYTIME_MESSAGE = "시도할 회수는 몇회인가요?";
@@ -11,6 +15,51 @@ public class Application {
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 사용자 입력 처리
+        System.out.println(INPUT_CARNAME_MESSAGE);
+        String inputNames = readLine();
+        List<String> carNames = Arrays.asList(inputNames.split(","));
+        if (!isNotEmpty(carNames)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isUnderSixDigit(carNames)) {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println(INPUT_TRYTIME_MESSAGE);
+        String inputTime = readLine();
+        if (!isNumberInteger(inputTime)) {
+            throw new IllegalArgumentException();
+        }
+        int tryTime = Integer.parseInt(inputTime);
+
+        // TODO: 자동차 목록 생성
+
+
+        // TODO: 실행 결과 처리
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public static boolean isNumberInteger(String input) {
+        String regularExpression = "^[0-9]+$";
+        return input.matches(regularExpression);
+
+    }
+
+    public static boolean isUnderSixDigit(List<String> inputs) {
+        String regularExpression = "^[.]{5}$";
+        for (String input : inputs) {
+            if (!input.matches(regularExpression)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNotEmpty(List<String> inputs) {
+        if(inputs.size() == 0){
+            return false;
+        }
+        return inputs.contains(" ");
     }
 }
