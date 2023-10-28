5a275aa1078f0aea516b007e3d8039978d204cb3
sosojean <thwlsdl0989@gmail.com>
2023-10-28 11:14:46 +0900

    우승자 출력

--- src/main/java/racingcar/PrintFunction.java
+++ src/main/java/racingcar/PrintFunction.java
@@ -11,4 +11,23 @@ public class PrintFunction {
         }
         System.out.println();
     }
+
+    void printWinner(List<Car> carList) {
+        List<String> winnerName = new ArrayList<>();
+        int max = 0;
+        for (Car car : carList) {
+            if (max < car.score) {
+                max = car.score;
+            }
+        }
+
+        for (Car car : carList) {
+            if (car.score == max) {
+                winnerName.add(car.name);
+            }
+        }
+        String result = String.join(", ", winnerName);
+        System.out.print("최종 우승자 : ");
+        System.out.println(result);
+    }
 }
