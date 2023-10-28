a10d122a22514106adc507559871ad5632c4f046
BH946 <kobh2895@gmail.com>
2023-10-28 21:31:09 +0900

    feat: 테스트 코드 작성위해 자잘한 수정

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CarGenerator {
     private String[][] names;
-    public CarGenerator() {
-        String[] inStr = Console.readLine().split(",");
+    public CarGenerator(String[] inStr) {
         String[][] inNames = new String[inStr.length][2];
         for(int i=0; i<inStr.length;i++) {
             inNames[i][0] = inStr[i];
