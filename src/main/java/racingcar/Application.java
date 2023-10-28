f4afa7170d378fc7c126c531a4fd8dd8ac710235
baksakcci <pshsh910@gmail.com>
2023-10-29 01:08:37 +0900

    refactor($RacingGameController): add dependency injection

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RacingGameController;
+import racingcar.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingGameController racingGameController = new RacingGameController();
+        RacingGameController racingGameController = new RacingGameController(new InputView());
         racingGameController.startGame();
     }
 }
--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -3,9 +3,12 @@ package racingcar.controller;
 import racingcar.view.InputView;
 
 public class RacingGameController {
+    private final InputView inputView;
+    public RacingGameController(InputView inputView) {
+        this.inputView = inputView;
+    }
 
     public void startGame() {
-        InputView inputView = new InputView();
         String names = inputView.inputCarNames();
         String attempts = inputView.inputAttempts();
         System.out.println("자동차 이름: " + names + "시도할 회수: " + attempts);
