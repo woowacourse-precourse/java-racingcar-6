964962af92ccbe6b79a1485d793166120c7b8343
olsohee <dlthgml0108@daum.net>
2023-10-28 17:35:37 +0900

    feat(Cars): Cars 클래스에 생성자 생성

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,8 +1,17 @@
 package racingcar.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
 
     private List<Car> cars;
+
+    public Cars(String inputNames) {
+
+        String[] names = inputNames.split(",");
+        cars = Arrays.stream(names)
+                .map(name -> new Car(name))
+                .toList();
+    }
 }
