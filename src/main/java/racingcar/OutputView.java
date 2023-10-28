660d8356668cedbce88940b0417b067fa3d68810
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-28 19:58:55 +0900

    feat : 메시지 상수처리 및 실행결과 문구 출력기능, 줄바꿈 메소드 추출 기능 추가

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,12 +1,24 @@
 package racingcar;
 
 public class OutputView {
+    private static final String REQUEST_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_INPUT_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
     public void printRequestInputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(REQUEST_INPUT_CAR_NAME_MESSAGE);
     }
 
     public void printRequestInputTryNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(REQUEST_INPUT_TRY_NUMBER_MESSAGE);
+    }
+
+    public void printPlayResult() {
+        space();
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void space() {
+        System.out.println();
     }
 }
