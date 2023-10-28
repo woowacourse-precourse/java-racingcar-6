31754bcaf3f5eadef85f449db68a2db3602ea5ea
San Kim <rlatks1217@gmail.com>
2023-10-28 12:56:07 +0900

    feat GameProcess 클래스 생성

--- src/main/java/racingcar/EntryPoint.java
+++ src/main/java/racingcar/EntryPoint.java
@@ -6,10 +6,12 @@ public class EntryPoint {
 
 	private ArrayList<InformationOfCar> carList;
 	private final UserInput userInput;
+	private final GameProcess gameProcess;
 
 	public EntryPoint() {
 		this.carList = new ArrayList<InformationOfCar>();
 		this.userInput = new UserInput(carList);
+		this.gameProcess = new GameProcess();
 	}
 
 	public void startGame() {
