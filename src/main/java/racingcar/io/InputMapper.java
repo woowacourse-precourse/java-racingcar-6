a3f5309ca47f38d2362f35109ac91449a2b4ac77
GaBaljaintheroom <junsu1222@naver.com>
2023-10-28 10:01:17 +0900

    feat(InputMapper): 문자열에서 Int형 변환

--- src/main/java/racingcar/io/InputMapper.java
+++ src/main/java/racingcar/io/InputMapper.java
@@ -7,4 +7,8 @@ public class InputMapper {
     public Cars toCars(String carNames) {
         return new Cars(carNames);
     }
+
+    public int toInt(String attemptCount) {
+        return Integer.parseInt(attemptCount);
+    }
 }
