9f7ce3158d6910d6332bf21a746a4c70dc2ba090
박성춘 <songchon100@gmail.com>
2023-10-28 12:21:11 +0900

    feat : 사용자로부터 차 이름과 이동 횟수 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,27 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MOVE_NUM_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    private InputView() {
+
+    }
+
+    public static InputView getInstance() {
+        return new InputView();
+    }
+
+    public String readNames() {
+        System.out.println(INPUT_NAME_MESSAGE);
+        // 입력 요구사항에 대해선 이쪽에서 유효성 검사를 해야할듯함.
+        return Console.readLine();
+    }
+
+    public Integer readMoveNum() {
+        System.out.println(INPUT_MOVE_NUM_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
 }
