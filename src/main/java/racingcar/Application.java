67ad40647f8bf4b2d3afcdd2d4127c535aa0ff64
JW0321 <digneotn123@gmail.com>
2023-10-29 03:55:21 +0900

    Feat : 입력된 횟수 만큼 실행 반복 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,12 +6,18 @@ import java.util.Comparator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String name = Console.readLine();
-        Input.inputName(name);
+        String names = Console.readLine();
+        Car cars = Input.makingLineup(names);
+
         System.out.println("시도할 회수는 몇회인가요?");
         String number = Console.readLine();
-        Input.inputNumber(number);
+        int repeatTimes = Input.inputNumber(number);
+        for (int i=0; i<repeatTimes; i++){
+            cars.moveCar();
+            System.out.println();
+        }
 
     }
 }
