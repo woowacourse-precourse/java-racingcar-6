faac821197992217691085f35a166704f7865c62
anneu <b.imnuj@gmail.com>
2023-10-28 19:21:37 +0900

    chore: 자동차 이름 길이 예외 메세지 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,7 +5,7 @@ public class Car {
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
     private static final int CAN_MOVE_CONDITION = 4;
-    private static final String NAME_LENGTH_EXCEPTION = "자동차 이름은 %d ~ %d자 이하만 가능합니다.";
+    private static final String NAME_LENGTH_EXCEPTION = "자동차 이름은 %d ~ %d글자만 가능합니다.";
     private static final String NAME_LETTER_EXCEPTION = "자동차 이름에 특수문자나 숫자는 포함될 수 없습니다.";
 
     private final String name;
