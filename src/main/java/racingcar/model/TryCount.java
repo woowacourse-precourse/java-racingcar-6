74f27b8f0fb2dcab7b320fbcef305ef14b5ef551
lusate <rayrayr@naver.com>
2023-10-28 17:21:50 +0900

    입력한 시도 횟수 객체 생성 수정

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -7,6 +7,11 @@ public class TryCount {
         this.tryCount = convertStringToInt(stringTryCount);
     }
 
+    // 입력한 숫자와 현재 진행 중인 시도 횟수가 같아지면 끝냄
+    public boolean isSame(int tryCount) {
+        return this.tryCount == tryCount;
+    }
+
     private int convertStringToInt(String input) {
         return Integer.parseInt(input);
     }
