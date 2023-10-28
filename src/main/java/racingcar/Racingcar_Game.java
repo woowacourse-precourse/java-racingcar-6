50dd40b266973810167dcaf34f3a20acc20f6148
jsj <tkdlek4186@naver.com>
2023-10-28 13:24:08 +0900

    docs : README 수정

---
+++ src/main/java/racingcar/Racingcar_Game.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class function {
+    public static List<String> userInput(){
+        List<String> carName = new ArrayList<>();
+
+        String input=readLine();
+    }
+}
