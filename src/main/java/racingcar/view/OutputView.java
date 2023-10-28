f70e4bbe33530751801ab87cacd743300c47d29f
박성춘 <songchon100@gmail.com>
2023-10-28 15:06:31 +0900

    feat : 각 레이싱 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    private OutputView() {}
+
+    public static OutputView getInstance() {
+        return new OutputView();
+    }
+
+    public void printRacingResult() {
+        System.out.println(GAME_RESULT_MESSAGE);
+    }
+
+    public void printCarRacingStatus(String racingResult) {
+        System.out.println(racingResult);
+    }
+
+    public void printWinner() {
+
+    }
+
 }
