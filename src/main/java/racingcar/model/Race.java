d196f0a3574ea9b68aab987b7d7fa14fc9564a4d
Dawin <dawin002@naver.com>
2023-10-28 17:55:46 +0900

    feat(Race): 이동 회수 설정 기능 제거
    이동 회수를 설정하는 setRound() 기능을 Race 클래스에서 제거.
    이동 회수는 Race 클래스에서 관리되어야 하지 않는다고 판단해 이를 제거, 다른 클래스로 이동.

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class Race {
     private List<Car> cars;
-    private int round;
-    private String winner;
 
     public Race() {
         cars = new ArrayList<>();
@@ -21,10 +19,5 @@ public class Race {
             Car newCar = new Car(carName);
             cars.add(newCar);
         }
-    }
-
-    public void setRound(String inputString) {
-        // inputString에 대한 검사
-        round = Integer.parseInt(inputString);
     }
 }
