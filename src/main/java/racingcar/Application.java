4a4bee29775809aa00a0d3c7c2984619be3c6f27
KimDongEon <gold3987@naver.com>
2023-10-28 16:18:57 +0900

    feat : 입력단 기능 구현 및 테스트 코드 작성
    입력단 기능 추가
    - 자동차 이름 입력
    - 경기 진행 횟수 입력
    
    테스트 코드 작성
    - 자동차 이름 입력 테스트
    - 자동차 이름 입력 예외 테스트 (5글자 이상)
    - 경지 진행 횟수 입력 테스트
    - 경기 진행 횟수 입력 예외 테스트 (숫자가 아닌게 있을 때)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,58 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-    static List<String> carList;
+  // 자동차 이름을 담을 List
+  static List<String> carList;
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+  // 진행할 round 갯수
+  static int round;
 
-        carList = new ArrayList<>();
-        
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
 
+    carList = new ArrayList<>();
+
+    // 자동차 이름 입력
+    carList = inputCar();
+    // 경기 진행 횟수 입력
+    round = inputRound();
+
+
+  }
+
+  // 자동차 이름 입력
+  public static List<String> inputCar() {
+    String[] inputCarList = Console.readLine().split(",");
+    carList = new ArrayList<>();
+    for (String name : inputCarList) {
+      // 이름은 다섯 글자 이상이면 에외 처리
+      if (name.length() > 5) {
+        throw new IllegalArgumentException();
+      }
+      carList.add(name);
     }
+    return carList;
+  }
+
+  // 경기 진행 횟수 입력
+  public static int inputRound() {
+    String readLine = Console.readLine();
+
+    // 숫자가 아닌게 들어오면 예외 발생
+    for(char c : readLine.toCharArray()) {
+      if(!Character.isDigit(c)) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    return Integer.parseInt(readLine);
+  }
+
 }
---
+++ src/test/java/racingcar/MethodTest.java
@@ -0,0 +1,90 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 기능 구현 함수들 테스트
+ */
+public class MethodTest extends NsTest {
+
+
+    @Test
+    void 자동차_이름_입력_테스트() {
+        // given
+        String input = "pobi,woni";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // when
+        List<String> result = Application.inputCar();
+
+        // then
+        List<String> inputStringList = new ArrayList<>();
+        String[] split = input.split(",");
+        for(String data : split) {
+            inputStringList.add(data);
+        }
+        assertThat(result).isEqualTo(inputStringList);
+    }
+
+    @Test
+    void 자동차_이름_입력_예외_테스트() {
+        // given
+        String input = "5charmore15charmore2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // when, then
+        assertThatThrownBy(() -> Application.inputCar())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 경기_진행_횟수_입력_테스트() {
+        // given
+        String input = "5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // when
+        int result = Application.inputRound();
+
+        // then
+        assertThat(result).isEqualTo(Integer.parseInt(input));
+    }
+
+    @Test
+    void 경기_진행_횟수_입력_예외_테스트() {
+        // given
+        String input = "1c";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // when, then
+        assertThatThrownBy(() -> Application.inputRound())
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+
+
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
