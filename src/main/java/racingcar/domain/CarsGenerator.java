62f1d3bc64a103d9d4dd17583236a021f19e6927
m5s3 <devmyoungsoo2@gmail.com>
2023-10-28 16:47:34 +0900

    rafector(racingcar): generateCars 를 static 으로 수정

--- src/main/java/racingcar/domain/CarsGenerator.java
+++ src/main/java/racingcar/domain/CarsGenerator.java
@@ -6,23 +6,11 @@ import java.util.stream.Collectors;
 
 public class CarsGenerator {
 
-    private static CarsGenerator carsGenerator;
-
-    private CarsGenerator() {
-    }
-
-    public List<Car> generateCars(String input) {
+    public static List<Car> generateCars(String input) {
         List<String> carNames = List.of(input.split(","));
         return carNames
                 .stream()
                 .map(Car::of)
                 .collect(Collectors.toList());
-    }
-
-    public static CarsGenerator getInstance() {
-        if (carsGenerator == null) {
-            carsGenerator = new CarsGenerator();
-        }
-        return carsGenerator;
     }
 }
--- src/test/java/racingcar/domain/CarsGeneratorTest.java
+++ src/test/java/racingcar/domain/CarsGeneratorTest.java
@@ -8,17 +8,10 @@ import org.junit.jupiter.api.Test;
 
 class CarsGeneratorTest {
 
-    private CarsGenerator carsGenerator;
-
-    @BeforeEach()
-    void init() {
-        carsGenerator = CarsGenerator.getInstance();
-    }
-
     @Test
     void 주어진_값으로_자동차_생성() {
         String input = "pobi,woni,jun";
-        List<Car> cars = carsGenerator.generateCars(input);
+        List<Car> cars = CarsGenerator.generateCars(input);
         assertThat(cars).contains(Car.of("pobi"), Car.of("woni"), Car.of("jun"));
         assertThat(cars.size()).isEqualTo(3);
     }
@@ -27,7 +20,7 @@ class CarsGeneratorTest {
     void 주어진_값으로_자동차_생성시_이름이_6자_글자_이상이면_예외발생() {
         String input = "pobibibi,woni,jun";
 
-        assertThatThrownBy(() -> carsGenerator.generateCars(input))
+        assertThatThrownBy(() -> CarsGenerator.generateCars(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
