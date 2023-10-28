559ef542c468b27ab9fabe05da7e304c0d7761a2
goragoraki <gorakio0126@gmail.com>
2023-10-29 01:23:02 +0900

    feat(OutputView): add printWinner function
    Add printWinner function to print winner name

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     public static void printExecResult() {
         System.out.println("실행 결과");
     }
-
+    public static void printWinner(List<String> winnerName) {
+        int winnerSize = winnerName.size();
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i < winnerSize - 1; i++) {
+            System.out.print(winnerName.get(i));
+            System.out.print(", ");
+        }
+        System.out.println(winnerName.get(winnerSize - 1));
+    }
 }
