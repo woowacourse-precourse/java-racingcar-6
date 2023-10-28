bc425976c5854a844d91b27ce6a520bd1c519226
hYe807n <rlagkdus55@gmail.com>
2023-10-28 20:06:18 +0900

    Feat: 반복 회수 입력받기 위한 출력문 기능

--- src/test/java/racingcar/view/OutputView.java
+++ src/test/java/racingcar/view/OutputView.java
@@ -1,10 +1,17 @@
 package racingcar.view;
 
+import java.security.PublicKey;
+
 public class OutputView {
     public static final String DEMAND_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String DEMAND_REPEAT_NUMBER = "시도할 회수는 몇회인가요?";
 
     public static void printCarNameDemand() {
         System.out.println(DEMAND_CAR_NAME);
+    }
+
+    public static void printRepeatNumberDemand() {
+        System.out.println(DEMAND_REPEAT_NUMBER);
     }
 
 }
