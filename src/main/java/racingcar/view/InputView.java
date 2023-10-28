163ea3876e02b359582ac18853b1ff936d726f86
CJY <c0512jy@naver.com>
2023-10-29 01:05:48 +0900

    feat: 입력 기능 구현
    - 경주할 자동차 이름 입력 받기 -InputView#inputCarNames()
      - 자동차 이름은 쉼표(,)를 기준으로 구분
        - 자동차 이름은 5자 이하만 가능
    - 시도할 횟수 입력 받기 -InputView#InputTryCount
      - 시도 횟수는 숫자만 가능
      - 시도 횟수는 1이상만 가능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,36 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
+
+    public List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String[] split = Console.readLine().split(",");
+        for (String name : split) {
+            validateCarName(name);
+        }
+        return List.of(split);
+    }
+
+    public int inputTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+        validateRange(tryCount);
+        return tryCount;
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    private void validateRange(int tryCount) {
+        if (tryCount < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
 }
