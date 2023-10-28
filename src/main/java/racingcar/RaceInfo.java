deea94f0dee83864aa96a3d916d064f61e74eceb
sohyun9527 <sohyun95277@gmail.com>
2023-10-28 16:53:58 +0900

    feat(RaceInfo): 사전 정보 세팅을 위한 기능 구현
    문서에 작성한 세부사항대로 클래스 분리

---
+++ src/main/java/racingcar/RaceInfo.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import racingcar.util.Printer;
+import racingcar.util.UserInput;
+
+public class RaceInfo {
+    private final Printer printer = new Printer();
+    private final UserInput userInput = new UserInput();
+
+    private List<String> carNames;
+    private int gameRound;
+    private Map<String, StringBuilder> scoreBoard;
+
+    public RaceInfo getRaceInfo() {
+        this.carNames = inputCarNames();
+        this.gameRound = inputGameRound();
+        this.scoreBoard = makeScoreBoard();
+
+        return this;
+    }
+    private List<String> inputCarNames() {
+        printer.printStartMessage();
+
+        return userInput.getCarNames();
+    }
+
+    private int inputGameRound() {
+        printer.printResultMessage();
+
+        return userInput.getGameRound();
+    }
+
+    private Map<String, StringBuilder> makeScoreBoard() {
+        Map<String, StringBuilder> scoreBoard = new LinkedHashMap<>();
+
+        for (String name : carNames) {
+            scoreBoard.put(name, new StringBuilder());
+        }
+        return scoreBoard;
+    }
+
+    public List<String> getCarNames() {
+        return carNames;
+    }
+
+    public int getGameRound() {
+        return gameRound;
+    }
+
+    public Map<String, StringBuilder> getScoreBoard() {
+        return scoreBoard;
+    }
+}
