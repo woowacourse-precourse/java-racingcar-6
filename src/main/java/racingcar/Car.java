dff70986fc4bd4f93f5190968855fe7e50fe4df2
siho-song <thdtlgh234@naver.com>
2023-10-28 20:44:57 +0900

    [Feat] 함수 추가 및 함수 구조변경
    1. 각 필드의 getter 추가
    2. toString() 구조변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,16 +4,27 @@ public class Car {
     private String name;
     private int movingCount = 0;
 
-    public void setName(String name){
-        this.name = name;
-    }
-    public void increaseMovingCount(int movingCount){
-        this.movingCount ++;
+    public void increaseMovingCount() {
+        this.movingCount++;
     }
 
-    @Override
-    public String toString(){
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
         return this.name;
     }
 
+    public int getMovingCount() {
+        return this.movingCount;
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return "null";
+        }
+        return this.name;
+    }
 }
