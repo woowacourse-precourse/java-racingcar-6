e60272714af225a2e0b46c43e9656c5064ea4cf4
김효준 <khyojun1104@gmail.com>
2023-10-28 23:30:50 +0900

    feat(CarRace): WinnerService -> 최종우승자를 뽑기 위한 서비스 기능 구현!

---
+++ src/main/java/racingcar/service/WinnerService.java
@@ -0,0 +1,38 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+
+public class WinnerService {
+
+    public String findWinners(List<Car> cars) {
+        int max=0;
+        List<String> winners = new ArrayList<>();
+        max = getMaxMove(cars, max);
+        makeWinnersOutput(cars, max, winners);
+        return String.join(", ", winners);
+    }
+
+    private void makeWinnersOutput(List<Car> cars, int max, List<String> winners) {
+        for (Car car : cars) {
+            if(max == car.getMoveCount()){
+                winners.add(car.getName());
+            }
+        }
+    }
+
+    private int getMaxMove(List<Car> cars, int max) {
+        for (Car car : cars) {
+            max = getMax(max, car);
+        }
+        return max;
+    }
+
+    private int getMax(int max, Car car) {
+        if(max < car.getMoveCount()){
+            max = car.getMoveCount();
+        }
+        return max;
+    }
+}
