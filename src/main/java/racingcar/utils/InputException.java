58789872c0c8775fd79aff47b51d1feef086dd6e
euniiiii <hee9920@naver.com>
2023-10-28 19:33:51 +0900

    feat(utils): 자동차 이름 입력 시 이름의 길이를 확인하는 기능 추가

--- src/main/java/racingcar/utils/InputException.java
+++ src/main/java/racingcar/utils/InputException.java
@@ -8,4 +8,9 @@ public class InputException {
         if (!input.contains(","))
             throw new IllegalArgumentException(PrintMessage.ONLY_SPLIT_COMMA);
     }
+
+    public static void validateNameSize(String name) {
+        if (name.isEmpty() || name.length() > 5)
+            throw new IllegalArgumentException(PrintMessage.FIVE_OR_LESS);
+    }
 }
