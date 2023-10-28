3d05bb1a5d194d6734e717643ac5e4219c14a334
HeeHeeg <qmffndoden@naver.com>
2023-10-28 13:48:49 +0900

    feat(racingcar): 자동차에 이름을 부여할 수 있는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+        String inputName = Console.readLine();
+        Car car = new Car(inputName);
+        System.out.println(car);
 
     }
 }
