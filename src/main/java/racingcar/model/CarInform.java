12b168fa68c444c4b668952c183434c30c16379c
박성춘 <songchon100@gmail.com>
2023-10-28 12:54:57 +0900

    feat : Car의 정보를 가지고 있는 CarInform 객체 생성

---
+++ src/main/java/racingcar/model/CarInform.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+public class CarInform {
+    private String carName;
+    private String dash;
+
+    private CarInform(String carName) {
+        this.carName = carName;
+        this.dash = "";
+    }
+
+    public static CarInform getInstance(String carName) {
+        return new CarInform(carName);
+    }
+}
