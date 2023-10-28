32f9210691a7a2d87a12512dd2574dd96659d982
kimtaesoo <kimtaesoo7@naver.com>
2023-10-28 11:55:27 +0900

    test: Equals & getter 테스트 추가

--- src/test/java/racingcar/model/NameTest.java
+++ src/test/java/racingcar/model/NameTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,5 +18,25 @@ class NameTest {
         // when & then
         assertThatThrownBy(() -> new Name(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_같으면_같은_값으로_판단한다() {
+        // given
+        Name result = new Name("abc");
+        Name expected = new Name("abc");
+
+        // when & then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 이름을_반환한다() {
+        // given
+        String input = "abc";
+        Name result = new Name(input);
+
+        // when & then
+        assertThat(result.getName()).isEqualTo(input);
     }
 }
--- src/test/java/racingcar/model/PositionTest.java
+++ src/test/java/racingcar/model/PositionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -35,5 +36,25 @@ class PositionTest {
 
         // then
         assertThat(position.getPosition()).isEqualTo(expected);
+    }
+
+    @Test
+    void 이름이_같으면_같은_값으로_판단한다() {
+        // given
+        Position result = new Position(0);
+        Position expected = new Position(0);
+
+        // when & then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 위치를_반환한다() {
+        // given
+        int input = 4;
+        Position result = new Position(input);
+
+        // when & then
+        assertThat(result.getPosition()).isEqualTo(input);
     }
 }
