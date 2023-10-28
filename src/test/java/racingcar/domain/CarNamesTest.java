c5b463fc3ad6a32a32ababca3372ed006a7b29c9
msung99 <msung6924@naver.com>
2023-10-28 15:17:32 +0900

    style: 불필요한 선언문 제거

--- src/test/java/racingcar/domain/CarNamesTest.java
+++ src/test/java/racingcar/domain/CarNamesTest.java
@@ -2,11 +2,8 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Car;
 
 public class CarNamesTest {
     @Test
