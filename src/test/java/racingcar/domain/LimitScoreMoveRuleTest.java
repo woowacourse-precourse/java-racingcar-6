80ce1bb704007f15a3d1857d5ebd67c83b922f28
howudong <tjdvy963@naver.com>
2023-10-28 15:02:25 +0900

    refactor : 테스트코드 리팩토링

--- src/test/java/racingcar/domain/LimitScoreMoveRuleTest.java
+++ src/test/java/racingcar/domain/LimitScoreMoveRuleTest.java
@@ -1,12 +1,11 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LimitScoreRacingRuleTest {
+class LimitScoreMoveRuleTest {
     private NumberGenerator getTargetNumber(int number) {
         return new RandomNumberGenerator() {
             @Override
@@ -21,9 +20,9 @@ class LimitScoreRacingRuleTest {
     void 숫자4_1_반환() {
         //given
         NumberGenerator numberGenerator = getTargetNumber(4);
-        RacingRule racingRule = new LimitScoreRacingRule(numberGenerator);
+        MoveRule moveRule = new LimitScoreMoveRule(numberGenerator);
         //when
-        int distance = racingRule.tryMove();
+        int distance = moveRule.tryMove();
         //then
         assertThat(distance).isEqualTo(1);
     }
@@ -33,9 +32,9 @@ class LimitScoreRacingRuleTest {
     void 숫자5_1_반환() {
         //given
         NumberGenerator numberGenerator = getTargetNumber(5);
-        RacingRule racingRule = new LimitScoreRacingRule(numberGenerator);
+        MoveRule moveRule = new LimitScoreMoveRule(numberGenerator);
         //when
-        int distance = racingRule.tryMove();
+        int distance = moveRule.tryMove();
         //then
         assertThat(distance).isEqualTo(1);
     }
@@ -45,19 +44,10 @@ class LimitScoreRacingRuleTest {
     void 숫자3_0_반환() {
         //given
         NumberGenerator numberGenerator = getTargetNumber(3);
-        RacingRule racingRule = new LimitScoreRacingRule(numberGenerator);
+        MoveRule moveRule = new LimitScoreMoveRule(numberGenerator);
         //when
-        int distance = racingRule.tryMove();
+        int distance = moveRule.tryMove();
         //then
         assertThat(distance).isZero();
-    }
-
-    @RepeatedTest(value = 20)
-    @DisplayName("0 ~ 9 사이의 숫자가 나오면 성공한다.")
-    void RandomNumberGeneratorTest() {
-        //given
-        NumberGenerator numberGenerator = new RandomNumberGenerator();
-        //when,then
-        assertThat(numberGenerator.generate()).isBetween(0, 9);
     }
 }
