ca21c2432f11622c6a0bd6d7fb320886bbbbb7ff
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 16:56:09 +0900

    feat : 자동차 이름 입력에 대한 안내 메시지 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String CAR_NAME_INPUT_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL_COUNT_INPUT_MSG = "시도할 회수는 몇회인가요?";
+    private static final String LINE = "\n";
+    private static final String RESULT_MSG = LINE + "실행 결과";
+
+    public void printRequestCarNameMessage() {
+        System.out.println(CAR_NAME_INPUT_MSG);
+    }
+}
