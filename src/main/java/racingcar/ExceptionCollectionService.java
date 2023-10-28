f80cfe2ac37b748e31bdb3a99d473169732d84b6
totocm <typesapk8445@gmail.com>
2023-10-28 11:19:19 +0900

    예외처리 기능 구현

--- src/main/java/racingcar/ExceptionCollectionService.java
+++ src/main/java/racingcar/ExceptionCollectionService.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public class ExceptionCollectionService {
   public ExceptionCollectionService(){}
-  // 컨트롤러 만든 곳에서 return 시켜 종료하면
-  // 메인까지 return이 안나갈것같은데..
-  // 컨트롤러 대신 메인을 사용해야할듯..
-  // 아니면 컨트롤러를 기능조합용으로만 하고 ㅇㅇ 메인에서 실행하는 것으로 땅땅
-  public boolean isNmaesCheck(List<String> names){
+
+  // isNameCheck 는 컨트롤에서 만들고 여긴 기능들만
+  public boolean isLengthOverCheck(List<String> names){
     for(String name : names){
       if (name.length() > 5){
         return false;
@@ -16,12 +14,20 @@ public class ExceptionCollectionService {
     }
     return true;
   }
-  public boolean isCoiceCheck(String choice){
-    try{
-      Integer.parseInt(choice);
-      return true;
-    }catch (NumberFormatException e){
-      return false;
-    }
+  //isCoiceCheck 는 컨트롤에서 사용하고 이 메서드는 따로 이름
+  // Exception 레벨별로 구분
+
+  // isNumberParsingCheck -> int 범위초과, 문자열을 분리하려 했으나 기술 부족
+  public boolean isNumberParsingCheck(String choice){
+    try
+    { Integer.parseInt(choice); return true;
+    }catch (NumberFormatException e){ return false;}
   }
+  public boolean isPositiveIntegerCheck(int number){
+    return number > 0;
+  }
+
+
+
+
 }
