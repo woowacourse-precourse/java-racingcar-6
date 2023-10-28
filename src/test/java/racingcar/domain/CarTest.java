cbb765a0dbaa831e95c268964c8a50fb9d111c99
unknown <y2kf9a0q@gmail.com>
2023-10-28 21:35:46 +0900

    test: skeleton of CarTest.java

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
