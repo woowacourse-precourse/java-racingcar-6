07b4614beba3b7e7df35a4cb1c7f11f87b57b821
hyungyeom <rlagusrua3687@gmail.com>
2023-10-28 14:38:02 +0900

    feat : SystemMessage 기능 구현

---
+++ src/main/java/racingcar/SystemMessage.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public enum SystemMessage {
+    SET_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    private final String message;
+
+    SystemMessage(String message) {
+        this.message = message;
+    }
+
+    // 메시지 반환하기
+    public String print(){
+        return message;
+    }
+}
---
+++ src/main/java/racingcar/view/OuputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+import racingcar.SystemMessage;
+
+public class OuputView {
+
+    public static void printSetCarNameMessage(){
+        System.out.println(SystemMessage.SET_CAR_NAME_MESSAGE.print());
+    }
+}
