b96118df1358df23c8231dc09ea1d4a8153534bb
Jinnie Kim <jinniek48@gmail.com>
2023-10-28 16:41:49 +0900

    feat(racingcar): 전진하는지 아닌지 판단하는 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class Car {
+
+    public boolean engine(){
+        int power = Randoms.pickNumberInRange(0,9);
+
+        if (power>=4){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
