6e510257493ab19313714d72b5fb96ac8730fe55
suhyun__ <thebbbb1206@gmail.com>
2023-10-29 02:27:11 +0900

    feat: 아무것도 입력하지 않은 경우 검증

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,12 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
 
 public class InputView {
-    public String inputCarName() {
+    public InputValidator inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        return Console.readLine();
+        return new InputValidator(Console.readLine().trim());
     }
 }
