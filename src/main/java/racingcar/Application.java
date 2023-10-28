fda98a9b82e99387119164cb4e38c22ab931c055
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-28 20:42:30 +0900

    refactor - 생성자 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import racingcar.controller.GameController;
+import racingcar.controller.InputController;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController gameController = new GameController();
+        GameController gameController = new GameController(new InputController(), new OutputView());
         gameController.play();
     }
 }
