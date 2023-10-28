63753b30ee4afa4f40fc741cc820945fc2cecf6b
kichang_98 <richkc2006@naver.com>
2023-10-28 19:59:07 +0900

    구현 1. 사용자에게 입력을 요구하는 문장 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 사용자 입력 요구
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
