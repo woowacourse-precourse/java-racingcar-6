1d6d9d7ee85add1cb65c2ec1c1666516325c18a1
mmihye <smilehye910@naver.com>
2023-10-28 13:45:00 +0900

    FEAT/자동차 입력 요청 메시지

---
+++ src/main/java/racingcar/GameMessage.java
@@ -0,0 +1,6 @@
+package racingcar;
+
+public class GameMessage {
+
+    public static final String REQUEST_ENTER_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+}
---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingGame {
+
+    public void getCarsName(){
+        System.out.println(GameMessage.REQUEST_ENTER_CAR_NAME_MESSAGE);
+
+    }
+}
+
