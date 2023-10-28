abd0f5c733d0bac0b5cd5d70d7813d82c93f837a
takealook97 <takealook97@naver.com>
2023-10-29 02:28:28 +0900

    feat: InputValidation 클래스 생성
    입력의 유효성 검증을 위한 클래스를 생성하였다. 메서드는 테스트코드 작성 후 구현할 것이다.

---
+++ src/main/java/racingcar/utility/InputValidation.java
@@ -0,0 +1,11 @@
+package racingcar.utility;
+
+public class InputValidation {
+	public static String validateName(String name) {
+		return null;
+	}
+
+	public static Long validateNumber(String input) {
+		return null;
+	}
+}
