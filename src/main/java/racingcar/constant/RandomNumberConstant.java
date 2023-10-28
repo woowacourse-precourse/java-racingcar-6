c07820a7ef9aea9cf33a99a2f75ab5fcbe612d08
enduf7686 <enduf768640@gmail.com>
2023-10-28 17:45:34 +0900

    docs(README): edit docs/readme

--- src/main/java/racingcar/constant/RandomNumberConstant.java
+++ src/main/java/racingcar/constant/RandomNumberConstant.java
@@ -2,7 +2,8 @@ package racingcar.constant;
 
 public enum RandomNumberConstant {
 
-    MOVE_FORWARD(4), MINIMUM_PICK_RANDOM_NUMBER(0), MAXIMUM_PICK_RANDOM_NUMBER(9);
+    MOVE_FORWARD(4),
+    MINIMUM_PICK_RANDOM_NUMBER(0), MAXIMUM_PICK_RANDOM_NUMBER(9);
 
     private final int number;
 
