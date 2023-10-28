10c9af8d2b39d062eafe00a4e8ccda5a8cdaee45
jyajoo <jyajoo1020@gmail.com>
2023-10-28 23:24:19 +0900

    feat: 입력값이 5자 이하인지 검증
    - validateLengthInput 메서드에서 5자 이상인 경우 예외 처리한다

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,6 +1,9 @@
 package racingcar.service;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RaceService {
     public void createCars(String carNamesInput) {
         validateCarNamesInput(carNamesInput);
@@ -9,6 +12,8 @@ public class RaceService {
     public void validateCarNamesInput(String carNamesInput) {
         validateEmptyInput(carNamesInput);
         validateNullInput(carNamesInput);
+        List<String> carNames = Arrays.asList(carNamesInput.split(","));
+        validateLengthInput(carNames);
     }
 
     private void validateEmptyInput(String carNamesInput) {
@@ -19,6 +24,16 @@ public class RaceService {
 
     private void validateNullInput(String carNamesInput) {
         if (carNamesInput == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLengthInput(List<String> carNames) {
+        boolean isInvalidCarName = carNames
+                .stream()
+                .anyMatch(carName -> carName.trim().length() > 5);
+
+        if (isInvalidCarName) {
             throw new IllegalArgumentException();
         }
     }
