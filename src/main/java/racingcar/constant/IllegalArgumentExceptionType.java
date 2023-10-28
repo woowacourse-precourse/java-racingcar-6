0d1857c69fd0b3aed964b2051276f14e838a1d0a
wns312 <wns312@gmail.com>
2023-10-28 16:23:23 +0900

    refactor(InputView): IllegalArgumentExceptionType 리팩토링에서 누락된 항목 적용
    변경사항
    - InputView의 IllegalArgumentException을 enum 으로 이동
    - IDE에서의 리턴문 강제에 따른 에러 발생 방식 변경
    - 이를 위한 IllegalArgumentExceptionType에 getMessage 메소드 추가
    
    catch문에서 Type의 에러 발생 메소드를 통한 에러 발생이 적절한지 의문이 생깁니다. 이렇게 IDE의 오류 검출을 피하기 위한 것이 올바른 설계인지 고민됩니다.

--- src/main/java/racingcar/constant/IllegalArgumentExceptionType.java
+++ src/main/java/racingcar/constant/IllegalArgumentExceptionType.java
@@ -5,12 +5,17 @@ public enum IllegalArgumentExceptionType {
     CAR_NAME_MAX_LENGTH_ERROR_MESSAGE("차량 이름의 길이는 5와 같거나 작아야 합니다."),
     EMPTY_CARS_ERROR_MESSAGE("차량은 한 대 이상이어야 합니다."),
     DUPLICATED_CARS_ERROR_MESSAGE("차량 이름이 중복되었습니다."),
-    ATTEMPT_COUNT_INIT_ERROR_MESSAGE("최소 1회의 시도 횟수를 입력해 주세요.");
+    ATTEMPT_COUNT_INIT_ERROR_MESSAGE("최소 1회의 시도 횟수를 입력해 주세요."),
+    READ_ATTEMPT_COUNT_ERROR_MESSAGE("숫자만 입력해 주세요");
 
     private final String message;
 
     IllegalArgumentExceptionType(String message) {
         this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     public void throwException() {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,12 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.constant.IllegalArgumentExceptionType;
 
 public class InputView {
     private static final String CAR_NAMES_SPLITTING_REGEX = ",";
     private static final String READ_CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String READ_ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String READ_ATTEMPT_COUNT_ERROR_MESSAGE = "숫자만 입력해 주세요";
 
     private List<String> convertCarNames(String input) {
         return Arrays.stream(input.split(CAR_NAMES_SPLITTING_REGEX))
@@ -27,7 +27,10 @@ public class InputView {
         try {
             return Integer.parseInt(string);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(READ_ATTEMPT_COUNT_ERROR_MESSAGE);
+            // IDE 리턴 문법 오류 회피를 위한 에러 발생 별도 처리
+            throw new IllegalArgumentException(
+                    IllegalArgumentExceptionType.READ_ATTEMPT_COUNT_ERROR_MESSAGE.getMessage()
+            );
         }
     }
 
