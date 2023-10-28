caf6048e16e4ca9733ebd3f6d1dac2122df40e3e
unknown <coguddlf1000@naver.com>
2023-10-28 10:44:10 +0900

    feat : 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.CarManufacturer;
+import racingcar.input.InputManager;
+import racingcar.output.OutputManager;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final OutputManager outputManager = new OutputManager();
+        final InputManager inputManager = new InputManager();
+        final CarManufacturer carManufacturer = new CarManufacturer();
     }
 }
--- src/main/java/racingcar/input/InputManager.java
+++ src/main/java/racingcar/input/InputManager.java
@@ -1,6 +1,25 @@
 package racingcar.input;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputManager {
+
+    public List<String> inputNames() {
+        final String names = Console.readLine();
+        final List<String> nameList = Arrays
+                .stream(names.split(","))
+                .toList();
+        validateNames(nameList);
+        return nameList;
+    }
+
+    private void validateNames(List<String> nameList) {
+        for (String name : nameList) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
