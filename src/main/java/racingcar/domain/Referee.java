8ae76da1cd39307c23592c13ddef94a3198df1b8
leedohyun <lucky77795@naver.com>
2023-10-28 17:20:17 +0900

    feat(racingCar) : 최대 거리를 가지는 자동차들의 이름(우승자들)을 구하는 기능 구현.

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -6,9 +6,23 @@ import java.util.stream.Collectors;
 
 public class Referee {
 
+    public List<String> checkResult(Cars cars) {
+        Collection<Car> carList = cars.getCars();
+
+        Car maxDistanceCar = findMaxDistanceCar(carList);
+        return findWinnersCarName(carList, maxDistanceCar);
+    }
+
     private Car findMaxDistanceCar(Collection<Car> carList) {
         return carList.stream()
                 .max(Car::compareTo)
                 .orElse(null);
+    }
+
+    private List<String> findWinnersCarName(Collection<Car> carList, Car maxPositionCar) {
+        return carList.stream()
+                .filter(maxPositionCar::hasSameDistance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
