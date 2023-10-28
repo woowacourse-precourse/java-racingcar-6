5592a83db37fa987fc6deb510a04925ffaa5696a
Gukin Han <gukin.dev@gmail.com>
2023-10-28 22:35:06 +0900

    feat: Car 클래스 toString 메서드 오버라이드하여 필드값을 이용하여 String 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -36,4 +36,11 @@ public class Car {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return name +
+                " : " +
+                mileage.toString();
+    }
 }
