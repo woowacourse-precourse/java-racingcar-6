4635b4fa2abee23add7b1dfc28becf9077f19255
cousim46 <wjdghldns24@naver.com>
2023-10-29 03:18:21 +0900

    feat : 개행 추가 메서드, 우승자 출력하는 메서드 추가

--- src/main/java/racingcar/output/PrintService.java
+++ src/main/java/racingcar/output/PrintService.java
@@ -1,6 +1,10 @@
 package racingcar.output;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class PrintService {
+
     public static void inputCarNameRequest() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
@@ -9,7 +13,15 @@ public class PrintService {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void show(String carName, int straightCount) {
-        System.out.printf("%s : %s\n",carName,"-".repeat(straightCount));
+    public static void show(Car car) {
+        System.out.printf("%s : %s\n", car.getName(), "-".repeat(car.getStraightCount()));
+    }
+
+    public static void result(List<String> racings) {
+        String winners = String.join(", ", racings);
+        System.out.printf("최종 우승자 : %s", winners);
+    }
+    public static void nextLine() {
+        System.out.println();
     }
 }
