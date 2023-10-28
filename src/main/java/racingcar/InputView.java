a87e0b6a04c48cf625855505b756238b821ea994
박찬건 <jg9870@naver.com>
2023-10-28 21:04:44 +0900

    refactor(racingcar): 함수 이름 더 명확하게 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class InputView {
     public static List<Car> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");  // todo 같은 이름 입력하면?
+
+    public static List<Car> getCars() {
+        ResultView.printEnterNameGuide();
         return CarFactory.createCarsFromInput(Console.readLine());
     }
 
