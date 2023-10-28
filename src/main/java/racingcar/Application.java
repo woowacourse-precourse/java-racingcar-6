4d8da47f0c03ff6460d4f183e2002f5c528ca4ba
혜민 <alddlv81@gmail.com>
2023-10-28 16:23:11 +0900

    feat: 자동차 이름 쉼표 기준으로 구분, 이름 앞, 뒤 공백 제거 후 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         User user = new User();
-        user.inputName();
+        user.splitByComma(user.inputName());
     }
 }
--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -2,9 +2,28 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     String inputName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    List<Car> splitByComma(String inputCarName){
+        List<Car> carList = new ArrayList<>();
+        String[] cars = inputCarName.split(",");
+        for (String carName : cars) {
+            carList.add(createCar(carName));
+        }
+        System.out.println(carList);
+        return carList;
+    }
+
+    Car createCar(String carName){
+        return new Car(carName.trim());
+    }
+
+
 }
