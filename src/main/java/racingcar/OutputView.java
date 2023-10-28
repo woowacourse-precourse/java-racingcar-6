516cf28d550b03deca2b987424ff602f08b259dc
unknown <gpfls981220@gmail.com>
2023-10-28 21:48:32 +0900

    feat: implementation of a function to output a comment to input the number of times

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -5,4 +5,7 @@ public class OutputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
     
+    static void outputTimes() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
