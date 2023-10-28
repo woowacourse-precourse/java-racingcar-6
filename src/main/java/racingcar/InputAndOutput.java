6b93ec247e3abc693a75cc5e746d41d8e8b4a0e7
KIVSH <kihongkim3@hotmail.com>
2023-10-28 21:38:55 +0900

    refactor: change all method to static in InputAndOutput class

--- src/main/java/racingcar/InputAndOutput.java
+++ src/main/java/racingcar/InputAndOutput.java
@@ -3,15 +3,15 @@ package racingcar;
 import java.util.List;
 
 public class InputAndOutput {
-    void outputMessage(String message) {
+    static void outputMessage(String message) {
         System.out.println(message);
     }
 
-    List<String> inputCarNames() throws IllegalArgumentException {
+    static List<String> inputCarNames() throws IllegalArgumentException {
         return null;
     }
 
-    int inputTryTimes() throws IllegalArgumentException {
+    static int inputTryTimes() throws IllegalArgumentException {
         return 0;
     }
 }
