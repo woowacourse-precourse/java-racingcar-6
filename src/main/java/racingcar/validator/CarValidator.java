2d35bc58af134305f5ff7ffd1f739642e51f9dd1
Mr.Kim <joajy98@gmail.com>
2023-10-28 20:14:04 +0900

    feat: 시도 횟수가 0회보다 적을 경우를 예외처리하는 메서드

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -5,4 +5,10 @@ public class CarValidator {
     private static final int CHECK_IF_TRY_COUNT_LESS_THAN_ZERO = 0;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String STRING_REGEX = "^[\\w]*$";
+
+    public static void checkTryCount(int tryCount) {
+        if (tryCount < CHECK_IF_TRY_COUNT_LESS_THAN_ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
