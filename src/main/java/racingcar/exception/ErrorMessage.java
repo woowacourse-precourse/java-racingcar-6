4bbe985af6a76cce2171836cd64b827d2288dae8
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-28 14:59:49 +0900

    feat: ErrorMessage 자동차 상수 이용, 시도 횟수 관련 에러 메세지 작성

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,6 +1,5 @@
 package racingcar.exception;
 
-
 import racingcar.domain.RacingCar;
 
 public enum ErrorMessage {
@@ -8,8 +7,14 @@ public enum ErrorMessage {
     INPUT_NOT_A_NUMBER("입력값은 숫자여야 합니다."),
     INPUT_NOT_POSITIVE_NUMBER("입력값은 자연수여야 합니다."),
 
+    CAR_NAMES_NOT_EMPTY("자동차 이름은 비어있을 수 없습니다."),
     INVALID_CAR_NAME_LENGTH(
-            String.format("자동차 이름은 %d~%d자만 가능합니다.", RacingCar.NAME_MIN_LENGTH, RacingCar.NAME_MAX_LENGTH));
+            String.format("자동차 이름은 %d~%d자만 가능합니다.", RacingCar.NAME_MIN_LENGTH, RacingCar.NAME_MAX_LENGTH)),
+    CAR_NAME_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
+
+    INVALID_RACE_ATTEMPTS("시도 횟수는 1이상이어야 합니다."),
+    RACE_ATTEMPTS_NOT_EMPTY("시도 횟수는 비어있을 수 없습니다."),
+    RACE_ATTEMPTS_NOT_NUMBER("시도 횟수는 숫자여야 합니다.");
 
     private final String message;
 
