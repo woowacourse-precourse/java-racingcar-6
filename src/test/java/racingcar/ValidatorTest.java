7e7db8299d03100a7abe21509ee243cda1d83e82
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-29 00:13:37 +0900

    test: 진행 라운드 예외 처리 테스트 코드 생성
    숫자가 아닌 문자 입력 시 예외 처리

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -2,12 +2,15 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 import racingcar.validator.CarNameValidator;
+import racingcar.validator.RoundValidator;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidatorTest  {
 
     private CarNameValidator carNameValidator;
+    private RoundValidator roundValidator;
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test()
@@ -33,6 +36,13 @@ public class ValidatorTest  {
     @Test()
     void 차_이름_5자_초과_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hyun,pobi,wooteco"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test()
+    void 진행_라운드_숫자_외_문자_입력_예외_처리() {
+        assertThatThrownBy(() -> roundValidator = new RoundValidator("asd"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
