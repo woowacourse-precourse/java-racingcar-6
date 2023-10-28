afc9b8cfd7458d7325f5444b68ee1ec03640426c
lusate <rayrayr@naver.com>
2023-10-28 15:59:55 +0900

    입력한 자동차 이름 콤마로 구분

---
+++ src/main/java/racingcar/util/StringUtils.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+import java.util.stream.IntStream;
+
+public class StringUtils {
+    public static String[] splitByComma(String str, String comma) {
+        return str.split(comma);
+    }
+
+
+}
