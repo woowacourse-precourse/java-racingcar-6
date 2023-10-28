524553dea9bfd3c2c6714263c9302c450fefd2dc
leedohyun <lucky77795@naver.com>
2023-10-28 17:46:01 +0900

    refactor(racingCar) : 상수 네이밍 수정

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -2,17 +2,17 @@ package racingcar.validator;
 
 public class InputValidator {
 
-    private final String CarNameRegex = "\\w+(\\s+\\w+)*(,\\w+(\\s+\\w+)*)*"; // ,뒤 공백은 비허용/이름 사이 공백은 허용
-    private final String TryCountRegex = "[1-9]\\d*";
+    private final String CAR_NAME_REGEX = "\\w+(\\s+\\w+)*(,\\w+(\\s+\\w+)*)*"; // ,뒤 공백은 비허용/이름 사이 공백은 허용
+    private final String TRY_COUNT_REGEX = "[1-9]\\d*";
 
     public void validateInputCarName(String carName) {
-        if (!carName.matches(CarNameRegex)) {
+        if (!carName.matches(CAR_NAME_REGEX)) {
             throw new IllegalArgumentException();
         }
     }
 
     public void validateInputTryCount(String tryCount) {
-        if (!tryCount.matches(TryCountRegex)) {
+        if (!tryCount.matches(TRY_COUNT_REGEX)) {
             throw new IllegalArgumentException();
         }
     }
