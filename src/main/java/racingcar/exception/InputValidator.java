137f5eb424e07a2796e953def0fabdab3e1e3ed2
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 16:59:44 +0900

    feat : 사용자가 입력한 시도 횟수에 대한 유효성 검증 기능 구현 - 빈 값이나 문자 입력시 예외 발생

--- src/main/java/racingcar/exception/InputValidator.java
+++ src/main/java/racingcar/exception/InputValidator.java
@@ -3,6 +3,7 @@ package racingcar.exception;
 public class InputValidator {
 
     private static final String EMPTY_INPUT_ERROR_MSG = "입력값이 비어있습니다.";
+    private static final String CHARACTERS_IN_INPUT_ERROR_MSG = "입력값에 문자가 포함되어 있습니다";
 
     private InputValidator() {
     }
@@ -10,6 +11,24 @@ public class InputValidator {
     public static void validateInputCarNames(String carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MSG);
+        }
+    }
+
+    public static void validateInputTrialCount(String number) {
+        if (number.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MSG);
+        }
+        if (!isNumeric(number)) {
+            throw new IllegalArgumentException(CHARACTERS_IN_INPUT_ERROR_MSG);
+        }
+    }
+
+    private static boolean isNumeric(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
