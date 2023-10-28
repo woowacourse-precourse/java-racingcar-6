e8b6e06648095719cf04a6ea2d1e4459bfd2d99f
jyajoo <jyajoo1020@gmail.com>
2023-10-28 23:11:43 +0900

    feat: 빈 입력값 검증
    - validateCarNamesInput 메서드에서 검증이 이루어진다
    - validateEmptyInput 메서드에서 빈 입력값은 예외 처리 된다

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -3,6 +3,17 @@ package racingcar.service;
 
 public class RaceService {
     public void createCars(String carNamesInput) {
-        
+        validateCarNamesInput(carNamesInput);
     }
+
+    public void validateCarNamesInput(String carNamesInput) {
+        validateEmptyInput(carNamesInput);
+    }
+
+    private void validateEmptyInput(String carNamesInput) {
+        if (carNamesInput.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
