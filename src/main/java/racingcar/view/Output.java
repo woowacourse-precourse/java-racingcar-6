883ff2783aca5016bcb9bebeb68a929259d17e5e
lusate <rayrayr@naver.com>
2023-10-28 16:49:22 +0900

    자동차에 대한 실행 결과 출력

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -2,6 +2,8 @@ package racingcar.view;
 
 import racingcar.model.Car;
 
+import java.util.List;
+
 public class Output {
     private static final String inputCarName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String number_of_attempts = "시도할 회수는 몇회인가요?";
@@ -10,7 +12,7 @@ public class Output {
     private static final String distance_mark = "-";
 
 
-    public static void printCarName() {
+    public static void inputCarName() {
         System.out.println(inputCarName);
     }
 
@@ -18,14 +20,15 @@ public class Output {
         System.out.println(number_of_attempts);
     }
 
-    public static void printResult() {
+    public static void printExecutionResult() {
         System.out.println(execution_result);
     }
 
-    private static void printCarScore(Car car) {
-        System.out.print(car.getName() + " : ");
-        printPosition(car);
-        System.out.println();
+    private static void printCarScore(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : "); // 차 이름
+            printPosition(car);
+        }
     }
 
     private static void printPosition(Car car) {
@@ -39,15 +42,6 @@ public class Output {
         System.out.println("최종 우승자 : ");
         // 우승자 이름 출력
         System.out.println();
-
-        //만약 우승자가 한명이면
-        /*if(car == only){
-            System.out.println(); 차 이름 출력
-        }
-
-        else{
-            sout; // 차름 여러 개 출력
-        }
-        */
     }
+
 }
