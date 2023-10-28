122fc6e935556d8104514924739e1c64be66449c
Jiyun <alex68566856@gmail.com>
2023-10-29 03:28:40 +0900

    refactor: 람다함수 방식 변경 map(carName->new Car(carName)) -> map(Car::new)

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -3,20 +3,16 @@ package racingcar.model;
 import java.util.List;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
     public Cars(List<String> stringCars) {
         this.cars = stringCars.stream()
-                .map(carName->new Car(carName))
+                .map(Car::new)
                 .toList();
     }
 
     public List<Car> getCars() {
         return cars;
-    }
-
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
     }
 
     @Override
