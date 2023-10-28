6d6d735a91a89db6903626305a69504f88b0572a
hlmg <devhlmg@gmail.com>
2023-10-29 00:59:29 +0900

    feat(Participants): 시도 횟수만큼 레이스하는 기능 구현

--- src/main/java/racingcar/domain/Participants.java
+++ src/main/java/racingcar/domain/Participants.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Participants {
     private final List<Car> cars;
@@ -9,5 +10,21 @@ public class Participants {
         cars = names.stream()
                 .map(Car::new)
                 .toList();
+    }
+
+    public void raceNTimes(int times) {
+        validate(times);
+        IntStream.range(0, times)
+                .forEach(unused -> race());
+    }
+
+    private void validate(int times) {
+        if (times < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다");
+        }
+    }
+
+    private void race() {
+        cars.forEach(Car::moveForward);
     }
 }
