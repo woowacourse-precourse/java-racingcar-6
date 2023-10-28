b7878b493ea28f0da72927408617bedbd999c34e
donghoony <aru0504@naver.com>
2023-10-29 02:32:33 +0900

    feat(GameClientOutput): 입력 전 안내 메시지 출력 기능 구현
    - 사용자로부터 입력을 받기 전에, 자동차 이름 및 시도 횟수를 묻는 기능

--- src/main/java/racingcar/GameClientOutput.java
+++ src/main/java/racingcar/GameClientOutput.java
@@ -3,7 +3,17 @@ package racingcar;
 import java.util.List;
 
 public class GameClientOutput {
+    private static final String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MOVES_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String WINNERS_MESSAGE = "최종 우승자 : ";
+
+    public void printForAskingNames() {
+        System.out.println(INPUT_NAMES_MESSAGE);
+    }
+
+    public void printForAskingMoves(){
+        System.out.println(INPUT_MOVES_MESSAGE);
+    }
 
     public void printWinners(List<String> winners) {
         System.out.print(WINNERS_MESSAGE);
