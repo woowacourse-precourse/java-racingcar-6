730d3dc628ac7afc87d1c92ce401e6403aef2b50
dltjdn <hana40004@naver.com>
2023-10-28 18:19:06 +0900

    feat(RacingGame): 자동차 경주 게임 실행 및 결과 만들기
     * 주어진 횟수만큼 자동차 경주 게임을 실행시키고 차수별 실행 결과를 조합하는 함수

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -5,9 +5,34 @@ public class RacingGame {
     private final Cars cars;
     private final int tryCount;
 
+    private static final String NEW_LINE = "\n";
+
     public RacingGame(Cars cars, int tryCount){
         this.cars = cars;
         this.tryCount = tryCount;
     }
+
+    public String makeStatus(){
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<tryCount; i++){
+            cars.forward();
+            result.append(cars.toString());
+            result.append(NEW_LINE);
+        }
+        return result.toString();
+    }
+
+    public void judgeFinalWinner(){
+        // TODO 최종 우승자
+    }
+
+
+
+
+
+
+
+
+
 
 }
