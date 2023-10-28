4dd68efe3cd50cb2047233cc04c2060d94ef01c1
박준영 <wnsdud6969@naver.com>
2023-10-28 18:36:58 +0900

    feat(OutputView): add new features
    Add new feature
    - print game result message to user
    - print new empty line

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,11 @@
 package racingcar.view;
 
-import java.util.List;
-
 public class OutputView {
 
     private static final String PROMPT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PROMPT_NUMBER_OF_GAME_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+
 
     private static OutputView outputView;
 
@@ -25,5 +25,14 @@ public class OutputView {
 
     public void promptNumberOfGameAttempts() {
         System.out.println(PROMPT_NUMBER_OF_GAME_ATTEMPTS_MESSAGE);
+    }
+
+    public void printGameResult() {
+        printNewEmptyLine();
+        System.out.println(GAME_RESULT_MESSAGE);
+    }
+
+    private void printNewEmptyLine() {
+        System.out.println();
     }
 }
