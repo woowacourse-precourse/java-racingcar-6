b52e290069a28381cca7d540cbb3b4eebb3b70bc
taehoon <xogns1514@gmail.com>
2023-10-28 16:11:52 +0900

    fix: 자동차이름 공백 검증 추가
    - 자동차이름 공백 테스트를 통해 수정

--- src/main/java/racingcar/ValidationManager.java
+++ src/main/java/racingcar/ValidationManager.java
@@ -5,6 +5,7 @@ public class ValidationManager {
     public static void validateCarNames(String inputCarNames) {
         for (String carName : inputCarNames.split(",")) {
             validateCarNameLength(carName);
+            validateCarNameEmpty(carName);
         }
     }
 
@@ -12,8 +13,16 @@ public class ValidationManager {
         if(!isValidCarNameLength(inputCarName)) throw new IllegalArgumentException();
     }
 
+    private static void validateCarNameEmpty(String inputCarName) {
+        if(isNullOrEmpty(inputCarName)) throw new IllegalArgumentException();
+    }
+
     private static boolean isValidCarNameLength(String inputCarName) {
         return inputCarName.length() > 0 && inputCarName.length() <= 5;
+    }
+
+    private static boolean isNullOrEmpty(String inputCarName) {
+        return inputCarName == null || inputCarName.trim().isEmpty();
     }
 
 }
