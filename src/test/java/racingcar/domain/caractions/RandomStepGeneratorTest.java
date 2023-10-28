75cb6f9912be9a2f629d54fab05f495be16372f9
Picka <marmong@naver.com>
2023-10-28 21:57:27 +0900

    test: add tests for 시도할 횟수(전진하는 횟수)를 입력 받는 기능

---
+++ src/test/java/racingcar/domain/caractions/RandomStepGeneratorTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain.caractions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+class RandomStepGeneratorTest {
+    private RandomStepGenerator stepGenerator;
+
+    @BeforeEach
+    public void setUp() {
+        stepGenerator = new RandomStepGenerator();
+    }
+
+    @Test
+    void getStepTest() {
+        int step = stepGenerator.getStep();
+        assertTrue(step == 0 || step >= 4);
+    }
+}
