4880d6e1e4fc07d1b22b00b8ccbb7248c5c137d0
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-29 02:54:43 +0900

    feat(game): implement moveForward method for car movement in Car class

--- src/main/java/racingcar/game/Car.java
+++ src/main/java/racingcar/game/Car.java
@@ -2,8 +2,14 @@ package racingcar.game;
 
 public class Car {
     private String name;
+    private String distance;
 
     public Car(String name) {
         this.name = name;
+        this.distance = "";
+    }
+
+    public void moveForward() {
+        distance += "-";
     }
 }
