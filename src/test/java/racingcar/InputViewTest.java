07f122a36000adb49c33e907dbc99fe2fa245bf6
KimDabomi <cdabomi@nate.com>
2023-10-29 00:32:56 +0900

    test: Init InputView

---
+++ src/test/java/racingcar/InputViewTest.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    @Test
+    void InputView_인스턴스_생성확인() {
+        InputView inputView = new InputView();
+    }
+}
