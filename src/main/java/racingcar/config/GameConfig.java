d52311d2cb4a3b477d90f8344ba6b4b9c910979d
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-28 21:51:27 +0900

    feat: 기본 설정값 작성

--- src/main/java/racingcar/config/GameConfig.java
+++ src/main/java/racingcar/config/GameConfig.java
@@ -1,4 +1,36 @@
 package racingcar.config;
 
 public enum GameConfig {
+    FORWARD_CONDITION(4),
+    START_OF_RANGE(0),
+    END_OF_RANGE(9),
+    MAX_TRY_COUNT(Integer.MAX_VALUE),
+    MAX_CAR_NAME_LENGTH(5),
+    SPLIT_REGEX(",");
+
+    private final Integer intValue;
+    private final String stringValue;
+
+    GameConfig(Integer intValue) {
+        this.intValue = intValue;
+        this.stringValue = null;
+    }
+
+    GameConfig(String stringValue) {
+        this.intValue = null;
+        this.stringValue = stringValue;
+    }
+
+    GameConfig(String stringValue, Integer intValue) {
+        this.stringValue = stringValue;
+        this.intValue = intValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
 }
