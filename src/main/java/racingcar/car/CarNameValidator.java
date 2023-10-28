2944ac7b156bdaaa15c28341c7ff913a0363d2f3
34suuuuu <psh7492@gmail.com>
2023-10-28 16:48:36 +0900

    feat: 자동차 이름 validator 클래스 생성

--- src/main/java/racingcar/car/CarNameValidator.java
+++ src/main/java/racingcar/car/CarNameValidator.java
@@ -1,2 +1,33 @@
-package racingcar.car;public class CarNameValidator {
+package racingcar.car;
+
+import racingcar.Constants;
+import racingcar.Util;
+
+import java.util.ArrayList;
+
+public class CarNameValidator {
+    public final String NAMES;
+    public final ArrayList<String> NAMELIST;
+
+    public CarNameValidator(String carNames) {
+        this.NAMES = carNames;
+        this.NAMELIST = Util.carNamesToArrayList(carNames);
+    }
+
+    public void validate(){
+        isBlank();
+        isRightNameLength();
+    }
+
+    public void isBlank() {
+        if (NAMELIST.stream().anyMatch(name -> name.isEmpty())) {
+            throw new IllegalArgumentException(Constants.INPUT_WRONG_ERROR_MESSAGE);
+        }
+    }
+
+    public void isRightNameLength() {
+        if (NAMELIST.stream().anyMatch(name -> name.length() > Constants.NAME_SIZE)) {
+            throw new IllegalArgumentException(Constants.CAR_NAME_SIZE_ERROR_MESSAGE);
+        }
+    }
 }
