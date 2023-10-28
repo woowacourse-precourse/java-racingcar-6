c34b68efb906717f78a24cc8cb427e2127f03908
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 18:01:55 +0900

    feat : InputView에서 입력한 값을 변환해주는 기능 구현

---
+++ src/main/java/racingcar/util/StringConverter.java
@@ -0,0 +1,24 @@
+package racingcar.util;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.exception.InputValidator;
+
+public class StringConverter {
+    private static final String DELIMITER = ",";
+
+    private StringConverter() {
+    }
+
+    public static List<String> stringToListByDelimiter(String carNames) {
+        InputValidator.validateInputCarNames(carNames);
+        return Arrays.stream(carNames.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+
+    public static Integer stringToInteger(String trialCount) {
+        InputValidator.validateInputTrialCount(trialCount);
+        return Integer.parseInt(trialCount);
+    }
+}
