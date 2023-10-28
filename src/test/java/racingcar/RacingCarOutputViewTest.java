74cc7838cfa6b3ac4914bc24094b7333330771df
kunsanglee <gosmdochee2@gmail.com>
2023-10-28 14:41:19 +0900

    test(RacingCarOutputViewTest): 매 회차별 경기 결과 출력, 최종 승자 반환하는 테스트코드 작성

--- src/test/java/racingcar/RacingCarOutputViewTest.java
+++ src/test/java/racingcar/RacingCarOutputViewTest.java
@@ -23,14 +23,9 @@ public class RacingCarOutputViewTest {
         System.setOut(new PrintStream(OUTPUT));
     }
 
-    /**
-     * - printStartMessage(): 실행 결과를 출력한다. - [ ] `"실행 결과"` 출력.
-     * <p>
-     * - printEachRacingResult(): `사용자가 입력한 횟수`만큼 각 차수별 실행 결과를 출력한다. - [ ] 경주할 자동차의 이름 순서에 맞게 `이름 : -` 형태로 출력. - [ ]
-     * 줄바꿈을 한다.
-     * <p>
-     * - printWinner(): 최종 우승자를 출력한다. - [ ] 우승자가 2명 이상일 경우 `,`를 구분한다. - [ ] `"최종 우승자 : pobi, jun"` 형태로 출력한다.
-     */
+    private static Car setCar(String carName, boolean engineCanMove, int position) {
+        return new Car(carName, new FixedCarEngine(engineCanMove), new Position(position));
+    }
 
     @DisplayName("실행 결과 문자열을 출력한다.")
     @Test
@@ -43,21 +38,23 @@ public class RacingCarOutputViewTest {
     @DisplayName("매 회차마다 자동차 경주의 결과를 출력한다.")
     @ParameterizedTest
     @MethodSource("providePrintEachRacingResultTestArguments")
-    void printEachRacingResult(Cars cars, String expectedMessage) {
+    void printEachRacingResult(RacingRoundResult racingRoundResult, String expectedMessage) {
         RacingCarOutputView racingCarOutputView = new RacingCarOutputView();
-        racingCarOutputView.printEachRacingResult(cars);
+        RacingResult racingResult = new RacingResult();
+        racingResult.addResult(racingRoundResult);
+        racingCarOutputView.printEachRacingResult(racingResult);
         assertThat(OUTPUT.toString()).isEqualTo(expectedMessage);
     }
 
     static Stream<Arguments> providePrintEachRacingResultTestArguments() {
         return Stream.of(
-                arguments(new Cars(List.of(setCar("pobi", true, 1),
+                arguments(new RacingRoundResult(List.of(setCar("pobi", true, 1),
                                 setCar("woni", false, 0))),
                         """
                                 pobi : -
                                 woni :\s
                                 """),
-                arguments(new Cars(List.of(setCar("pobi", true, 2),
+                arguments(new RacingRoundResult(List.of(setCar("pobi", true, 2),
                                 setCar("woni", false, 4),
                                 setCar("jun", true, 3))),
                         """
@@ -68,7 +65,35 @@ public class RacingCarOutputViewTest {
         );
     }
 
-    private static Car setCar(String carName, boolean engineCanMove, int position) {
-        return new Car(carName, new FixedCarEngine(engineCanMove), new Position(position));
+    @DisplayName("최종 우승자를 출력한다.")
+    @ParameterizedTest
+    @MethodSource("providePrintWinnerTestArguments")
+    void printWinnerTest(RacingRoundResult racingRoundResult, String expectedMessage) {
+        RacingCarOutputView racingCarOutputView = new RacingCarOutputView();
+        RacingResult racingResult = new RacingResult();
+        racingResult.addResult(racingRoundResult);
+        racingCarOutputView.printWinner(racingResult);
+        assertThat(OUTPUT.toString()).isEqualTo(expectedMessage);
+    }
+
+    static Stream<Arguments> providePrintWinnerTestArguments() {
+        return Stream.of(
+                arguments(new RacingRoundResult(List.of(
+                                setCar("pobi", true, 2),
+                                setCar("woni", false, 4),
+                                setCar("jun", true, 3))),
+                        "최종 우승자 : woni\n",
+                        new RacingRoundResult(List.of(
+                                setCar("pobi", true, 4),
+                                setCar("woni", false, 4),
+                                setCar("jun", true, 3))),
+                        "최종 우승자 : pobi, woni\n",
+                        new RacingRoundResult(List.of(
+                                setCar("pobi", true, 1),
+                                setCar("woni", false, 1),
+                                setCar("jun", true, 1))),
+                        "최종 우승자 : pobi, woni, jun\n"
+                )
+        );
     }
 }
