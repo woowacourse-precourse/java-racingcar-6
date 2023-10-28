16d1c25df049d971029bfea35b113f21d3946231
Hoo-sung <gntjd135@gmail.com>
2023-10-28 10:33:39 +0900

    feat(AttemptsNumVerifier): Verifier 구현한 AttemptsNumVerifier 클래스 생성

---
+++ src/main/java/racingcar/verifier/AttemptsNumVerifier.java
@@ -0,0 +1,9 @@
+package racingcar.verifier;
+
+public class AttemptsNumVerifier implements Verifier{
+
+    @Override
+    public void check(String input) throws IllegalArgumentException {
+
+    }
+}
