0410b82ece463818d40645202701a9e847616971
otaeho <dhxogh123123@naver.com>
2023-10-28 17:11:52 +0900

    feat(Cars): 각 자동차의 전진 횟수를 기준으로 가장 많이 전진한 자동차를 구한다

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> carList;
+
+    public Cars(List<String> carNames) {
+        this.carList = carNames.stream().map(Car::new).collect(Collectors.toList());
+    }
+
+    public List<String> getMostForwardedCars() {
+        sortCarsByForwards();
+
+        int maxForwards = this.carList.get(0).getForwards();
+        return this.carList.stream().filter(car -> car.getForwards() == maxForwards).map(car -> car.getName())
+                .collect(Collectors.toList());
+    }
+    
+    public void sortCarsByForwards() {
+        Collections.sort(carList, (car1, car2) -> car2.getForwards() - car1.getForwards());
+    }
+}
