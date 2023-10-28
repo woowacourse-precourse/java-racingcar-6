28a7a36761c3d06f85eea7932da92b332428f82e
hYe807n <rlagkdus55@gmail.com>
2023-10-28 20:08:33 +0900

    Feat: "실행 결과" 문구 출력 기능

--- src/test/java/racingcar/view/OutputView.java
+++ src/test/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ import java.security.PublicKey;
 public class OutputView {
     public static final String DEMAND_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String DEMAND_REPEAT_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_PRINT = "실행 결과";
 
     public static void printCarNameDemand() {
         System.out.println(DEMAND_CAR_NAME);
@@ -14,4 +15,7 @@ public class OutputView {
         System.out.println(DEMAND_REPEAT_NUMBER);
     }
 
+    public static void printResult(){
+        System.out.println(RESULT_PRINT);
+    }
 }
