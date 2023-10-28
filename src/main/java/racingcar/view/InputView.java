e82ccf59285d5f41f9fe7e7c4b297cfcd6990338
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 02:00:20 +0900

    fix(InputView): String을 반환하도록 기능 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,10 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import racingcar.util.InputParser;
-import racingcar.validation.InputValidation;
 
 public class InputView {
 
-    public List<String> inputCars() {
-        String input = Console.readLine();
-        return InputValidation.validateCars(input);
+    public String inputCars() {
+        return Console.readLine();
     }
 }
