509e528bb873b3426249d857197693f05cbd4c41
SUJI-J <suzy852@naver.com>
2023-10-28 19:12:24 +0900

    refactor: 중복값 검증 코드 제거

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,30 +1,23 @@
 package racingcar;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
 import static racingcar.GameMessage.INPUT_LENGTH_EXCEPTION_MESSAGE;
-import static racingcar.GameMessage.INPUT_DUPLICATE_EXCEPTION_MESSAGE;
 
 public class Validator {
-    public static List<String> inputCarNames(String inputCarNames) {
-        List<String> carNamesList = InputConvertor.toList(inputCarNames);
-        isValidLength(carNamesList);
-        isValidDuplicate(carNamesList);
-        return carNamesList;
+
+    public static Map<String, String> inputCarNames(Map<String, String> carNamesMap) {
+        isValidLength(carNamesMap);
+        return carNamesMap;
     }
 
-    private static void isValidLength(List<String> carNamesList) {
-        for (String carName : carNamesList) {
+    private static void isValidLength(Map<String, String> carNamesMap) {
+        for (String carName : carNamesMap.keySet()) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException(INPUT_LENGTH_EXCEPTION_MESSAGE);
             }
-        }
-    }
-
-    private static void isValidDuplicate(List<String> carNamesList) {
-        Set<String> validDuplicate = new HashSet<>(carNamesList);
-        if (validDuplicate.size() != carNamesList.size()) {
-            throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MESSAGE);
         }
     }
 }
