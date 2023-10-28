85ef14b519093a50981c8cfe04038557d3d87327
HeeHeeg <qmffndoden@naver.com>
2023-10-28 15:36:53 +0900

    feat(racingcar): 사용자가 몇 번의 이동을 할 것인지를 입력할 수 있는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,11 +9,14 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         String inputName = Console.readLine();
-        Car car = new Car(inputName);
 
+        Car car = new Car(inputName);
         List<Car> cars = Arrays.asList(car);
-        String result =  cars.stream().map(Car::getName).collect(Collectors.joining(","));
-        System.out.println(result);
+        String result = cars.stream().map(Car::getName).collect(Collectors.joining(","));
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNumber = Console.readLine();
+        int attemptsNumber = Integer.parseInt(inputNumber);
 
     }
 }
