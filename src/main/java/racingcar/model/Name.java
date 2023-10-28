89f2762924000e658c678683ac88fc4a44a08ba2
Chan <33cks1423@naver.com>
2023-10-28 16:14:37 +0900

    feat(name): class Name create

---
+++ src/main/java/racingcar/model/Name.java
@@ -0,0 +1,45 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public final class Name {
+    private final String name;
+
+    private Name(String name) {
+        this.name = name;
+    }
+
+    public static Name of(String name) {
+        checkValid(name);
+        return new Name(name);
+    }
+
+    private static void checkValid(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("자동차 이름은 null 이 될 수 없다.");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없다.");
+        }
+        if (isOverNumberFive(name)) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
+        }
+    }
+
+    private static boolean isOverNumberFive(String name) {
+        return name.length() > 5;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/racingcar/model/NameTest.java
@@ -0,0 +1,41 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameTest {
+
+    @Test
+    @DisplayName("이름 생성 테스트")
+    void create() {
+        Name name = Name.of("pobi");
+
+        assertThat(name).isEqualTo(Name.of("pobi"));
+    }
+
+    @Test
+    @DisplayName("자동차 이름은 5자를 초과할 수 없다.")
+    void createNameOver5() {
+        assertThatThrownBy(() -> Name.of("pobicon"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름은 1자 이상이어야 한다.")
+    void createNameUnder1() {
+        assertThatThrownBy(() -> Name.of(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름은 null이 될 수 없다.")
+    void createNameBlank() {
+        assertThatThrownBy(() -> Name.of(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
