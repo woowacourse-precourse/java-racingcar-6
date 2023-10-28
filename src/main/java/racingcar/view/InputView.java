a344cad272c4109b5021e4b5f41e0bf2b2642aa9
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-29 02:37:31 +0900

    feat: 입력 관련 로직을 처리하는 InputView 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.domain.validator.InputValidator;
+import racingcar.util.StringParser;
+
+public class InputView {
+
+    private static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_NUMBER_OF_ROUNDS = "시도할 회수는 몇회인가요?";
+
+    private final InputValidator<String> roundNumberValidator;
+    private final InputValidator<List<String>> carNamesValidator;
+
+    public InputView(InputValidator<String> roundNumberValidator, InputValidator<List<String>> carNamesValidator) {
+        this.roundNumberValidator = roundNumberValidator;
+        this.carNamesValidator = carNamesValidator;
+    }
+
+    public List<String> readCarNames() {
+        System.out.println(REQUEST_CAR_NAMES);
+        String carNamesString = Console.readLine();
+        List<String> carNames = StringParser.parseToList(carNamesString);
+        carNamesValidator.validate(carNames);
+        return carNames;
+    }
+
+    public Integer readNumberOfRounds() {
+        System.out.println(REQUEST_NUMBER_OF_ROUNDS);
+        String roundNumberString = Console.readLine();
+        roundNumberValidator.validate(roundNumberString);
+        return Integer.parseInt(roundNumberString);
+    }
+}
