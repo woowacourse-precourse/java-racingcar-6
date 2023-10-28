e0265b287483e28549d3e7aea8de5ea6b934c113
HeeHeeg <qmffndoden@naver.com>
2023-10-28 14:07:04 +0900

    feat(racingcar): 자동차 이름을 쉼표(,) 기준으로 구분하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,18 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         String inputName = Console.readLine();
         Car car = new Car(inputName);
-        System.out.println(car);
+
+        List<Car> cars = Arrays.asList(car);
+        String result =  cars.stream().map(Car::getName).collect(Collectors.joining(","));
+        System.out.println(result);
 
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,10 @@ package racingcar;
 public class Car {
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
     public Car(String name) {
         this.name = name;
     }
