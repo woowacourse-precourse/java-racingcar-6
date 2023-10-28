c7854b7178e1c308673273c4140b8210330b2b98
Ramel <dev_20@naver.com>
2023-10-28 21:37:40 +0900

    feat: 각 자동차에 할당할 이름 생성

--- src/main/java/racingcar/domain/NameGenerator.java
+++ src/main/java/racingcar/domain/NameGenerator.java
@@ -1,7 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NameGenerator {
-    public String inputCarName(){
-        return null;
+    public ArrayList<String> carNameGenerate() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
     }
 }
