e0ea8801bf30f8edbff4ba2fe6f994282e74bf64
yang <ywj9811@gmail.com>
2023-10-29 03:52:49 +0900

    feat : 출력을 위한 Constant와 클래스 생성

---
+++ src/main/java/racingcar/constant/OutputConstant.java
@@ -0,0 +1,14 @@
+package racingcar.constant;
+
+public enum OutputConstant {
+    INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"), HOW_MANY_TIME("시도할 회수는 몇회인가요?");
+    final String output;
+
+    OutputConstant(String output) {
+        this.output = output;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import static racingcar.constant.OutputConstant.*;
+
+public class OutputView {
+    public void inputCarName() {
+        System.out.println(INPUT_CAR_NAME.getOutput());
+    }
+
+    public void howManyTime() {
+        System.out.println(HOW_MANY_TIME.getOutput());
+    }
+}
