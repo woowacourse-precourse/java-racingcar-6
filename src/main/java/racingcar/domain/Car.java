dea8d2ae4e07431966c8a6f3c47b875169d7559d
yooooonshine <hl3470@naver.com>
2023-10-29 00:30:41 +0900

    feat: Car객체 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,38 @@
 package racingcar.domain;
 
-public class Car {
-    private int movementCounter = 0;
+import java.util.HashMap;
+import racingcar.view.CarRaceGameView;
 
-    public int getMovementCounter() {
-        return movementCounter;
+public class Car {
+    private static Car car;
+    private int carQuantity;
+    private HashMap<String,Integer> carList = new HashMap<String, Integer>();
+
+
+    public static Car getCar() {
+        if(car == null){
+            car = new Car();
+        }
+        return car;
     }
 
-    public void setMovementCounter(int movementCounter) {
-        this.movementCounter = movementCounter;
+    public int getCarQuantity() {
+        return carQuantity;
+    }
+
+    public void setCarQuantity(int carQuantity) {
+        this.carQuantity = carQuantity;
+    }
+
+    public HashMap<String, Integer> getCarList() {
+        return carList;
+    }
+
+    public void addCarList(String name) {
+            carList.put(name, 0);
+    }
+
+    public void updateCarList(String name, int advanceNumber) {
+        carList.replace(name,advanceNumber);
     }
 }
