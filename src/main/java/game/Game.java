a9ebc35928cbeac1b7a25df59a0303433fe6e5b5
pjaehyun <show6756@gmail.com>
2023-10-28 18:18:28 +0900

    feat: 추가 Game 클래스 생성자 파라미터(participants, frequency)

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,16 @@
 package game;
 
+import car.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
+    public List<Car> participants;
+    public int frequency;
+
+    public Game(String participants, String frequency) {
+
+    }
 }
