7e7dde4a7f1f3e58c48c3b4ce2b1ecdfdcd8fb43
박찬건 <jg9870@naver.com>
2023-10-28 21:15:46 +0900

    refactor(racingcar): 기능에 맞는 클래스로 이동

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -12,24 +12,8 @@ public class ResultView {
     }
 
     public static void printWinners(List<Car> cars) {
-        String winners = findWinners(cars);
+        String winners = Race.findWinners(cars);
         System.out.print("최종 우승자 : " + winners);
-    }
-
-    private static String findWinners(List<Car> carList) {
-        int maxPositionLength = 0;
-        for (Car car : carList) {
-            if (car.getPositionLength() > maxPositionLength) {
-                maxPositionLength = car.getPositionLength();
-            }
-        }
-        List<String> winners = new ArrayList<>();
-        for (Car car : carList) {
-            if (car.getPositionLength() == maxPositionLength) {
-                winners.add(car.getName());
-            }
-        }
-        return String.join(", ", winners);
     }
 
     public static void printResultNotification() {
