a595650d3a4ba78c93154219cd165ea12d87c1f0
slowcloud <sju9417@gmail.com>
2023-10-28 10:49:05 +0900

    refactor: CarDistanceMessageTest의 메소드명 중 testToString을 toString으로 수정

--- src/test/java/racingcar/domain/CarDistanceMessageTest.java
+++ src/test/java/racingcar/domain/CarDistanceMessageTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarDistanceMessageTest {
 
     @Test
-    void testToString_메시지_생성() {
+    void toString_메시지_생성() {
         CarDistanceMessage carDistanceMessage = new CarDistanceMessage("foo", 5);
         assertEquals(carDistanceMessage.toString(), "foo : -----");
     }
