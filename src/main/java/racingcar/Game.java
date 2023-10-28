68696390b48177270f9497d1ef001481073ec6c0
happyryan <tjdgks120813@gmail.com>
2023-10-28 13:44:28 +0900

    feat(racingcar GameMessage): add enum for managing game messages
    Implemented the GameMessage enum to manage constants related to game messages for improved code organization and maintenance.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -15,9 +15,9 @@ public class Game {
     }
 
     public void play() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(GameMessage.GAME_START.message);
         System.out.println(JoinComma.playerInput(this.players));
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(GameMessage.TRY_COUNT.message);
         String moveNumber = User.moveNumberInput();
         System.out.println(moveNumber + "\n");
 
@@ -34,7 +34,7 @@ public class Game {
         Map<String, Integer> playerDistanceMap = Create.playerDistanceMapping(playerObjectArray);
 
         int maxDist = Collections.max(playerDistanceMap.values());
-        System.out.print("최종 우승자 : ");
+        System.out.print(GameMessage.FINAL_WINNER.message);
         List<String> winners = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : playerDistanceMap.entrySet()) {
             if (entry.getValue() == maxDist) {
---
+++ src/main/java/racingcar/GameMessage.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public enum GameMessage {
+    GAME_START("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    TRY_COUNT("시도할 회수는 몇회인가요?"),
+    FINAL_WINNER("최종 우승자 : ");
+
+    final String message;
+
+    GameMessage(final String message) {
+        this.message = message;
+    }
+}
