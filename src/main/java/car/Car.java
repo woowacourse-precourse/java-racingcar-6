5cea91beb3a3af8460d3af9c432d42caf2b34709
pjaehyun <show6756@gmail.com>
2023-10-28 17:24:21 +0900

    feat: 추가 0-9까지 랜덤번호 생성 함수

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -1,5 +1,7 @@
 package car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     public int distance;
     public String name;
@@ -14,5 +16,9 @@ public class Car {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0,9);
     }
 }
