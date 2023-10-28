a99a5b0812cf4217e07885921f111acf9295175b
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 17:28:15 +0900

    test : 자동차 이름 하나에 대한 비즈니스 검증 테스트 추가 - 6자 이하인 경우 예외 발생,  미만인 경우 예외 발생 X

---
+++ src/test/java/racingcar/exception/CarNameValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.exception;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarNameValidatorTest {
+
+    private static final Integer CAR_NAME_MAX_LENGTH = 5;
+    private static final String INVALID_CAR_NAME_LENGTH_ERROR_MSG = "자동차 이름은 " + CAR_NAME_MAX_LENGTH + "자 이하만 가능합니다.";
+
+
+    @DisplayName("자동차 이름이 최대 길이 이하인 경우 예외가 발생하지 않는다.")
+    @Test
+    void validCarName_test() {
+        String validCarName = "C".repeat(CAR_NAME_MAX_LENGTH - 1);
+
+        assertDoesNotThrow(() -> {
+            CarNameValidator.validateCarName(validCarName);
+        });
+    }
+
+    @DisplayName("자동차 이름이 최대 길이보다 긴 경우 예외가 발생해야 한다.")
+    @Test
+    void testInvalidCarName() {
+        String invalidCarName = "C".repeat(CAR_NAME_MAX_LENGTH);
+
+        try {
+            CarNameValidator.validateCarName(invalidCarName);
+        } catch (IllegalArgumentException e) {
+            assertEquals(INVALID_CAR_NAME_LENGTH_ERROR_MSG, e.getMessage());
+        }
+    }
+}
