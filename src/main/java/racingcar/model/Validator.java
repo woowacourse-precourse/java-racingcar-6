7df383fe4237f7c16c1196272742e03ce3dacf45
Yoonseo <jessicadbstj@gmail.com>
2023-10-28 12:09:16 +0900

    feat: 자동차이름 입력값 검증

---
+++ src/main/java/racingcar/model/Validator.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+public class Validator {
+    //콤마를 기준으로 이름이 1자 이상 5자 이하가 아닌 경우
+    public Validator() {
+    }
+
+    public boolean isCarNameFromPlayerValidate(String carName) {
+        String[] carNameArray = carName.split(",");
+        for (int i = 0; i < carNameArray.length; i++) {
+            if (!(carNameArray[i].length() >= 1 && carNameArray[i].length() <= 5)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
