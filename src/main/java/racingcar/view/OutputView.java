502d49ac73c2f051b865179fe366c3d8a0f8527f
seminss <2007ksm@naver.com>
2023-10-28 10:52:03 +0900

    feat: 사용자 입력을 받음
    InputView: 사용자 입력을 받음
    OutputView: 메서드들을 static 메서드로 변경하여 InputView에서 사용할 수 있도록 함

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,16 +2,16 @@ package racingcar.view;
 import static racingcar.view.ConstantView.*;
 
 public class OutputView {
-    public void printInputCarNameMessage(){
+    public static void printInputCarNameMessage(){
         System.out.println(INPUT_CAR_NAMES.getMessage());
     }
-    public void printInputTriesCountMessage(){
+    public static void printInputTriesCountMessage(){
         System.out.println(INPUT_TRIES_COUNT);
     }
-    public void printResultMessage(){
+    public static void printResultMessage(){
         System.out.println(RESULT);
     }
-    public void printNewLine(){
+    public static void printNewLine(){
         System.out.println();
     }
 }
