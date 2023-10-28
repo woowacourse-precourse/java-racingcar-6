8dcfa54b67ea151d0f4eb435172a8f9c1dca45b9
jyajoo <jyajoo1020@gmail.com>
2023-10-28 22:44:20 +0900

    feat: InputView 추가
    - 사용자의 입력을 받기 위해 InputView의 getInput()메서드 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,16 +1,20 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RaceController {
 
+    private final InputView inputView;
     private final OutputView outputView;
 
     public RaceController() {
+        this.inputView = new InputView();
         this.outputView = new OutputView();
     }
 
     public void run() {
         outputView.carNamesOutput();
+        String carNamesInput = inputView.getInput();
     }
 }
