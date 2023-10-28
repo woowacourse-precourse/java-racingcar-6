49f57ff2ade24283b7fc9a3b735a88e70ed1d4da
yoonseo choi <choiyoonseo02@gmail.com>
2023-10-29 03:32:33 +0900

    feat: 경주할 자동차 이름 입력 받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import static racingcar.view.OutputView.startInputMessage;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startInputMessage();
     }
 }
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+import static racingcar.view.InputView.inputCarName;
+
+public class OutputView {
+    public static void startInputMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputCarName();
+    }
+}
