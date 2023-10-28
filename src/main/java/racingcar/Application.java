f6e20036989034ca313104e8fec2100ecd284c4d
minisyu <iswater.kim@gmail.com>
2023-10-28 11:47:58 +0900

    feat(Application): 1차 구현 완료, 1차 리팩토링 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.converter.InputConverter;
+import racingcar.util.InputValidator;
+import racingcar.util.NumberPicker;
+import racingcar.util.RandomNumberPicker;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+        InputConverter inputConverter = new InputConverter();
+        InputView inputView = new InputView(inputValidator, inputConverter);
+        OutputView outputView = new OutputView();
+        NumberPicker randomNumberPicker = new RandomNumberPicker(0, 9);
+        GameController gameController = new GameController(inputView, outputView, randomNumberPicker);
+        gameController.run();
     }
 }
