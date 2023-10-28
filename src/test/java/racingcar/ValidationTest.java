dbe8d550cf259eafbf6f2420f89f3dd79243aeb0
takoyakimchi <c4nyou@gmail.com>
2023-10-28 18:00:39 +0900

    style(valid_test): remove unused import

--- src/test/java/racingcar/ValidationTest.java
+++ src/test/java/racingcar/ValidationTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
