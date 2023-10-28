631c29b142c30b04621f250021f3e9288e17574f
noxknow <chris2769@naver.com>
2023-10-28 17:03:20 +0900

    feat(RacingCarGameService) : 차수별 게임을 진행한다

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,4 +1,21 @@
 package racingcar.service;
 
+import racingcar.domain.RacingCar;
+import racingcar.domain.RandomNumber;
+
+import java.util.List;
+
 public class RacingCarGameService {
+
+    public void RacingCarGameProgress(RacingCar racingCar) {
+        List<String> carNames = racingCar.getCarNames();
+
+        for (int i = 0; i < carNames.size(); i++) {
+            RandomNumber randomNumber = new RandomNumber();
+
+            if (randomNumber.isMovePossible()) {
+
+            }
+        }
+    }
 }
