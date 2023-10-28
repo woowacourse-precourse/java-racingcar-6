b4b802d620e8ed48a42fca8616f9b2ab015b1b06
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-10-28 14:57:08 +0900

    Feat: playGame, conductRound 메서드 구현
    입력한 횟수만큼 회차를 진행하는 conductRound 메서드,
    게임 전반적인 부분을 실행시키는 playGame 메서드 구현

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -1,18 +1,45 @@
 package racingcar.Controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Model.CarList;
 import racingcar.View.InputView;
+import racingcar.View.OutputView;
 
 public class GameController {
-	public int inputAttemptCount() {
+
+	public void playGame() {
+
+		CarList carList = CarListMaker.makeList();
+		int attemptCount = inputAttemptCount();
+
+		System.out.println();
+		OutputView.printResult();
+
+		conductRound(attemptCount, carList);
+
+		String winnerList = carList.findWinner();
+
+		OutputView.printWinner(winnerList);
+
+	}
+
+	private int inputAttemptCount() {
 
 		InputView.printAttemptCount();
 		String inputAttemptCount = Console.readLine();
+
 		return Integer.parseInt(inputAttemptCount);
 
 	}
 
+	private void conductRound(int attemptCount, CarList carList) {
 
+		for (int i = 0; i < attemptCount; i++) {
+
+			carList.moveCarList();
+			OutputView.printEachRound(carList);
+
+		}
+	}
 
 }
