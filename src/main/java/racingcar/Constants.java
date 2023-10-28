82506d0506b89ea3f060d23b52aaa02aa6aa0581
34suuuuu <psh7492@gmail.com>
2023-10-28 16:45:31 +0900

    feat: 상수 추가

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -1,2 +1,20 @@
-package racingcar;public class Constants {
+package racingcar;
+
+import java.util.regex.Pattern;
+
+public class Constants {
+    public static final int NAME_SIZE = 5;
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+    public static final int GO_NUM = 4;
+
+    public static final Pattern TRY_NUM_PATTERN = Pattern.compile("^[1-9][0-9]*$");
+
+    public static final String DELIMETER = ",";
+    public static final String ERROR = "ERROR: ";
+    public static final String CAR_NAME_SIZE_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String INPUT_WRONG_ERROR_MESSAGE = "입력값이 존재하지 않습니다.";
+    public static final String INPUT_ONLY_INTEGER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
+
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
 }
