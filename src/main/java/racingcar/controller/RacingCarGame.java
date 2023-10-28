028239a50dc9ca1274c480e15ef819611cb2040e
Yoonseo <jessicadbstj@gmail.com>
2023-10-28 18:32:33 +0900

    feat: 사용자로부터 입력받은 자동차이름을 콤마 기준으로 나누는 기능 구현

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -4,13 +4,26 @@ import racingcar.model.MovingCar;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarGame {
     private MovingCar movingCar;
 
     public RacingCarGame() {
         movingCar = new MovingCar();
     }
-    public
+
+    public List<String> splitCarNamesByComma() {
+        String carName = InputView.getCarNameFromPlayer();
+        String[] carNameArray = carName.split(",");
+
+        List<String> carNameList = new ArrayList<>();
+        for (int i = 0; i < carNameArray.length; i++) {
+            carNameList.add(carNameArray[i]);
+        }
+        return carNameList;
+    }
 
     public void moveCarForward() {
         if (movingCar.isCarMovingForward() == true) {
