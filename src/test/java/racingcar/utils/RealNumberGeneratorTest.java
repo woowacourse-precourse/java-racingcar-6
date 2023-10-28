331e77a439e97170924873d485f0e3ef03f0c7d5
junest66 <chlwndks333@gmail.com>
2023-10-29 04:33:17 +0900

    refactor(RealNumberGeneratorTest): NumberGenerator를 인터페이스로 변경함으로 인한 테스트 코드 수정
    - NumberGenerator 인터페이스 도입에 따라 구체적인 구현체인 RealNumberGenerator를 사용하여 테스트 진행
    - 테스트 클래스 명도 RealNumberGeneratorTest로 변경

--- src/test/java/racingcar/utils/RealNumberGeneratorTest.java
+++ src/test/java/racingcar/utils/RealNumberGeneratorTest.java
@@ -1,14 +1,14 @@
-package racingcar;
+package racingcar.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.RepeatedTest;
 
-class NumberGeneratorTest {
+class RealNumberGeneratorTest {
 
     @RepeatedTest(100)
     void 랜덤숫자_0부터_9_사이인지_확인() {
-        NumberGenerator numberGenerator = new NumberGenerator();
+        NumberGenerator numberGenerator = new RealNumberGenerator();
         int randomNumber = numberGenerator.createRandomNumber();
         assertThat(randomNumber).isBetween(0, 9);
     }
