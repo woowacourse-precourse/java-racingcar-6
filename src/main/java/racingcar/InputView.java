e89050d7fafbc2bfe118cb0cc1eac4e8bf5a69a7
JongMinCh0i <wonderland314592@gmail.com>
2023-10-28 20:58:24 +0900

    feat(InputView): 경주할 자동차 이름 입력 검증 기능 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,10 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String NOT_PERMIT_BLANK = "공백은 허용하지 않습니다.";
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public void inputCarNames() {
+    public String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        return validInputBlank(Console.readLine());
     }
+
+    // 문자열에 공백이 포함되어 있거나, 아무것도 입력받지 않았을 때 예외가 발생합니다.
+    private String validInputBlank(String input) {
+        if ((input.isEmpty() || input.contains(" "))) {
+            throw new IllegalArgumentException(NOT_PERMIT_BLANK);
+        }
+        return input;
+    }
+
 }
