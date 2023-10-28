bc96afbc3144d184df05d702cf3ec7495f6f99ab
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 23:30:21 +0900

    refactor(domain): carList 데이터 타입 변경

--- src/main/java/racingcar/domain/model/Car.java
+++ src/main/java/racingcar/domain/model/Car.java
@@ -2,10 +2,11 @@ package racingcar.domain.model;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
 public class Car {
-    HashMap<String, Integer> carList = new HashMap<>();
+    public HashMap<String, Integer> carList = new LinkedHashMap<>();
 
     public void addCar(String carNames) {
         carList = Arrays.stream(carNames.split(","))
@@ -13,7 +14,7 @@ public class Car {
                         key -> key,
                         value -> 0,
                         (version1, version2) -> version1,
-                        HashMap::new
+                        LinkedHashMap::new
                 ));
     }
 
