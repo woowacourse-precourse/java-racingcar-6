e2c1f633a22da7977cad025dbca46749e5a49c68
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-28 21:14:56 +0900

    [refactor] : change logic

--- src/main/java/racingcar/controller/ViewController.java
+++ src/main/java/racingcar/controller/ViewController.java
@@ -1,17 +1,19 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.TryCount;
 import racingcar.utils.StringUtils;
+import racingcar.view.InputView;
 
 public class ViewController {
 
-    public String[] createCar() {
-        String carNames = InputCarName();
+    public static String[] InputCarName() {
+        String carNames = InputView.inputCarNames();
         return StringUtils.splitCarNames(carNames);
     }
 
-    private String InputCarName() {
-        String carList = Console.readLine();
-        return carList;
+    public static int InputTryCount() {
+        TryCount tryCount = new TryCount(InputView.inputTryCount());
+        return tryCount.getTryCount();
     }
 }
