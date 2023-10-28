e78a91ea19909bb87b500e9b55f6d20e89f939c6
CJY <c0512jy@naver.com>
2023-10-29 01:09:50 +0900

    feat: 자동차 도메인
    - 이름과 위치를 가지고 있음
    - [x]: 자동차 이름과 현재 위치를 출력 -Car#toString()
    - [x]: 자동차는 전진 또는 멈출 수 있음 -Car#move()
    - [x]: 자동차 이름 5자 이하 한번 더 확인

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,47 @@
 package racingcar.domain;
 
 public class Car {
+
+    private final String name;
+    private int position = 0;
+    private static final int MAX_NAME_SIZE = 5;
+    private static final int MIN_FORWARDABLE_NUMBER = 4;
+
+    private Car(String name) {
+        this.name = name;
+    }
+
+    public static Car of(String name) {
+        validateNameSize(name);
+        return new Car(name);
+    }
+
+    public void race(int number) {
+        if (isForwardable(number)) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " : " + "-".repeat(getPosition());
+    }
+
+    private static void validateNameSize(String name) {
+        if (name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    private boolean isForwardable(int number) {
+        return number >= MIN_FORWARDABLE_NUMBER;
+    }
 }
