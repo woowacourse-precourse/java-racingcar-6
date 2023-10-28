5254e3d3a868ccf64f5800abcb5adad1abc75d63
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-10-28 16:38:58 +0900

    feat: Car 클래스 멤버 내용 완료 & 메서드 틀 짜기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,65 @@
 package racingcar;
+import java.util.*;
 
 public class Application {
+    public static class Car{
+        private final String name;
+        private int position = 0;
+        public Car(String name){
+            this.name = name;
+        }
+
+        public void tryMove(int randomNumber){
+            if(randomNumber>=4){
+                this.position++;
+            }
+        }
+
+        public int getPosition(){
+            return this.position;
+        }
+        public String getName(){
+            return this.name;
+        }
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List <Car> makeCars(){
+
+        List<Car> cars = new ArrayList<>();
+
+        return cars;
+    }
+
+    public static int getAtteptsNum(){
+        int attemptsNum = 0;
+
+        return attemptsNum;
+    }
+
+    public static void runningRace(List<Car> cars, int attemptsNum){
+
+    }
+
+    public static void printRoundResult(Car car){
+
+    }
+
+    public static List<Car> getWinners(List<Car> cars){
+        List<Car> winners = new ArrayList<>();
+
+        return winners;
+    }
+
+    public static int getMaxPosition(List<Car> cars){
+        int maxPosition = 0;
+
+        return maxPosition;
+    }
+
+    public static void printWinners(List<Car> winners){
+
     }
 }
