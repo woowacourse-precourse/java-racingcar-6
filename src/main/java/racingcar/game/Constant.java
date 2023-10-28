880f4f9bffb5fa9252a9a2219f383e1b994613d8
minseo  jo <liging12@naver.com>
2023-10-28 11:49:21 +0900

    refactor: 가독성을 위해 위치 변경 + 메인 구분자에 대한 설명(주석) 추가

--- src/main/java/racingcar/game/Constant.java
+++ src/main/java/racingcar/game/Constant.java
@@ -2,16 +2,21 @@ package racingcar.game;
 
 public interface Constant {
 
+    String LINE = "\n";
+    int MAX_INT_VALUE = 2_147_483_647;
+    int MIN_INT_VALUE = 1;
+
+    String HYPHEN = "-";
+    String COLON_DELIMITER = " : ";
+
+    /**
+     * 자동차 이름을 구분하는 주요 구분자이다.
+     * 아래 상수를 바꿀 경우, 자동차 이름을 구분하는 구분자가 모두 변경됨
+     * ex) ',' 를 ':' 로 바꾸는경우 (메인 구분자를 바꿀 경우 이름도 바꿔야 할 듯, 또는 MAIN_DELIMITER 로 상수명을 바꿔도 될 듯)
+     * "pobi,woni" -> "pobi:woni"
+     */
     char COMMA_DELIMITER = ',';
     String COMMA_REGEX_DELIMITER= ",";
     String CONSECUTIVE_COMMA_DELIMITER = ",,";
     String KOREAN_COMMA_BRACKET_DELIMITER = "콤마(',')";
-
-    String COLON_DELIMITER = " : ";
-    String HYPHEN = "-";
-    String LINE = "\n";
-
-    int MAX_INT_VALUE = 2_147_483_647;
-    int MIN_INT_VALUE = 1;
-
 }
