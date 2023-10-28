770ff46689d58c409a25be6cb03abef2a48fa0f0
jongheonleee <qwefghnm1212@gmail.com>
2023-10-28 15:53:49 +0900

    [feat] GameController 클래스의 필드 정의

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,18 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+
+    public GameController(InputView inputView , OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
 
 }
