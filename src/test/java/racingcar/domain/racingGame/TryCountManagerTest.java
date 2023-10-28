6adf6da56ad7e8fd6b0dd0ea442d707ea970ec3f
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-29 04:24:14 +0900

    test: 사용자에게 시도 횟수 입력 기능 테스트

--- src/test/java/racingcar/domain/racingGame/TryCountManagerTest.java
+++ src/test/java/racingcar/domain/racingGame/TryCountManagerTest.java
@@ -1,7 +1,25 @@
 package racingcar.domain.racingGame;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
-class TryCountManagerTest {
+import org.junit.jupiter.api.Test;
+import racingcar.IOTest;
 
+class TryCountManagerTest extends IOTest {
+    @Test
+    void 시도_횟수_입력받은후_해당_횟수만큼_람다_호출() {
+        //given
+        TryCountManager tryCountManager = new TryCountManager();
+
+        //when
+        int tryCount = 3;
+        String message = "message";
+        command(tryCount+"");
+        tryCountManager.makeTryCount();
+        tryCountManager.invokeForTryCount(() -> System.out.print(message));
+
+        //then
+        assertThat(output()).contains("시도할 회수는 몇회인가요?", message.repeat(tryCount));
+    }
 }
