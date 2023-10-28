9f1f2de84b21f676eedbf6141bfd2e614a73aa17
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 00:33:25 +0900

    feat(OutputView): askPlayTime 메소드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,15 @@ public class OutputView {
     public void askCarName() {
         System.out.println(Messages.ASK_CAR_NAME.getMessage());
     }
+    public void askPlayTime() {
+        System.out.println(Messages.ASK_PLAY_TIME.getMessage());
+    }
+
+    public void showResult() {
+//        RESULT("실행 결과"),
+//        FINAL_WINNER("최종 우승자 : ");
+    }
+
 
 
 }
