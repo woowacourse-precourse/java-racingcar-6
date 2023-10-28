b481894c6e384e243439c9b72cb5a693591a2fac
takealook97 <takealook97@naver.com>
2023-10-29 01:25:09 +0900

    feat: 라운드 결과 get, stack 메서드 구현
    라운드 결과를 리스트를 기반으로 만들어서 string형태로 반환하는 get 메서드를 구현하였다. 추가적으로, 이를 기반으로 StringBuilder에 쌓는 역할을 하는 stack 메서드를 구현하였다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import java.util.List;
 
+import racingcar.domain.Car;
 import racingcar.view.constant.OutputConstant;
 
 public class OutputView {
@@ -10,6 +11,23 @@ public class OutputView {
 	public OutputView() {
 		this.gameResult = new StringBuilder();
 		gameResult.append(OutputConstant.GAME_RESULT.getRequestMessage());
+	}
+
+	public String getRoundResult(List<Car> cars) {
+		StringBuilder roundResult = new StringBuilder();
+		for (Car car : cars) {
+			roundResult.append(car.getName()).append(OutputConstant.SEPERATOR.getRequestMessage());
+			for (int count = 0; count < car.getForwardCount(); count++) {
+				roundResult.append(OutputConstant.FORWARD_MARK.getRequestMessage());
+			}
+			roundResult.append("\n");
+		}
+		roundResult.append("\n");
+		return roundResult.toString();
+	}
+
+	public void stackRoundResult(String roundResult) {
+		gameResult.append(roundResult);
 	}
 
 	public void printGameResult() {
