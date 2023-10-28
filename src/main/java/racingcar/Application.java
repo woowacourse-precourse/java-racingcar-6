3dab93d430968b4092f80716eef3ec93accac3d2
jsj <tkdlek4186@naver.com>
2023-10-28 14:29:06 +0900

    feat: 메인함수 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.Racingcar_Game.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> carName = new ArrayList<>();
+        carName = userInput();
+        playGame(carName);
     }
 }
