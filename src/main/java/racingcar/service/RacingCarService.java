9dfe72be533b73437c839b545df2627dad114e13
5jeong <wjdgns4385@naver.com>
2023-10-29 02:46:21 +0900

    [Refactor] 매직넘버 상수로 변경, 메소드명 수정, 주석제거

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -3,32 +3,30 @@ package racingcar.service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
-import javax.print.DocFlavor.READER;
 import racingcar.model.Cars;
 import racingcar.util.ConverterUtil;
 import racingcar.util.RandomNumUtil;
 
 public class RacingCarService {
-    private static final String FORWARD_MOVING = "-";
+    private static final String MOVE_FORWARD_STRING = "-";
+    private static final int MIN_FORWARD_NUMBER = 4;
     private static Cars cars;
 
     private static HashMap<String, String> gameResult = new HashMap<>();
 
-    public HashMap<String, String> getGameResult() {
-        return gameResult;
-    }
-
-    public void createCars(String carNames) {
+    public void generateCars(String carNames) {
         List<String> carsInfos = ConverterUtil.covertStringToList(carNames);
         cars = new Cars(carsInfos);
-        gameResult = cars.initCarsMap();
+        gameResult = cars.initGameResult();
+    }
+
+    public int generateRandomNum() {
+        return RandomNumUtil.generateRandomNum();
     }
 
     public boolean judgeMove(int randomNum) {
-//        int num = RandomNumUtil.generateRandomNum();
         boolean moving = false;
-        if (randomNum >= 4) {
+        if (randomNum >= MIN_FORWARD_NUMBER) {
             moving = true;
             return moving;
         }
@@ -36,11 +34,11 @@ public class RacingCarService {
     }
 
     public HashMap<String, String> judgeResult() {
-        int randomNum = RandomNumUtil.generateRandomNum();
 
         for (String car : gameResult.keySet()) {
+            int randomNum = generateRandomNum();
             if (judgeMove(randomNum)) {
-                gameResult.put(car, gameResult.get(car) + FORWARD_MOVING);
+                gameResult.put(car, gameResult.get(car) + MOVE_FORWARD_STRING);
             }
         }
         return gameResult;
@@ -49,9 +47,7 @@ public class RacingCarService {
     public int mostMovementCount() {
         int maxLen = Integer.MIN_VALUE;
         for (String car : gameResult.keySet()) {
-            if (maxLen < gameResult.get(car).length()) {
-                maxLen = gameResult.get(car).length();
-            }
+            maxLen = Math.max(maxLen, gameResult.get(car).length());
         }
         return maxLen;
     }
