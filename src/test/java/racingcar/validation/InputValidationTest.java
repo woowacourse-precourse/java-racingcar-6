77c21de7e44dc59ba80944655b20fcc83a6d56b7
IE-MangChi <hsm983278@gmail.com>
2023-10-28 17:41:36 +0900

    test: 자동차 이름 검증 테스트

---
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -0,0 +1,38 @@
+package racingcar.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InputValidationTest {
+
+    @Test
+    @DisplayName("자동차 이름이 5자 이하이면 입력값 반환")
+    void inputValidateOKTest() {
+        // given
+        List<String> input = Stream.of("pobi","woni","jun")
+                .collect(Collectors.toList());
+        // when
+        List<String> result = InputValidation.inputValidate(input);
+        // then
+        Assertions.assertThat(result).isEqualTo(input);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자 초과이면 오류 반환")
+    void inputValidateFailTest() {
+        // given
+        List<String> input = Stream.of("pobipobi","woni","jun")
+                .collect(Collectors.toList());
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidation.inputValidate(input);
+        });
+    }
+}
