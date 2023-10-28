4922df201d463978b3f3d411ebe1b0f7f8c97b80
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 17:56:55 +0900

    feat : 사용자의 입력을 받고 받은 값을 넘겨주는 컨트롤러 추가

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        
+    }
+}
