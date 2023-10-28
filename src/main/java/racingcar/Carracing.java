7ed5c901d655a0157945209cc022ebb490d1610f
Chenowoo <cjsgusdn14@naver.com>
2023-10-28 15:53:55 +0900

    feat(Carracing): 자동차를 전진 또는 스탑하는 기능 구현  - 랜덤한 숫자를 뽑고 뽑은 숫자가 4이상이면 전진, 4보다 작으면 스탑하는 기능 구현  - Map에 자동차의 이름과, 전진한 만큼 "-" 값을 추가

--- src/main/java/racingcar/Carracing.java
+++ src/main/java/racingcar/Carracing.java
@@ -1,6 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static racingcar.NumberConst.*;
+import static racingcar.InputValue.*;
+
 public class Carracing {
-    public Carracing() {
+    public static void startRacing() {
+
+        InputSettingValue();
+
+        //inputNumberOfCars
+        //movingCount
+        //carNameList
+    }
+
+    public static void goStop(String[] carNameList) {
+
+        Map<String, String> raceScore = new HashMap<>();
+
+        for(int i=0; i<carNameList.length; i++){
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE_NUMBER, FINAL_INCLUSIVE_NUMBER);
+
+            if (randomNumber >= STANDARD_MOVING_NUMBER) {
+                raceScore.put(carNameList[i], raceScore.getOrDefault(carNameList[i], "") + "-");
+            }
+        }
     }
 }
