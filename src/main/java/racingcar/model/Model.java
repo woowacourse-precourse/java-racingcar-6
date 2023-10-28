3d7945f68cbc89b135647a2d5827282c4afd5142
seungtoctoc <dltmxor31@naver.com>
2023-10-29 01:09:20 +0900

    feat: 레이스를 1턴 진행하는 메서드 구현 레이스가 1턴(1회) 진행되는 메서드를 추가했습니다.

--- src/main/java/racingcar/model/Model.java
+++ src/main/java/racingcar/model/Model.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import racingcar.model.Car;
 import java.util.List;
 import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Model {
     public List<Car> MakeCarList(String[] namesOfCars){
@@ -12,7 +13,17 @@ public class Model {
             Car newCar = new Car(nameOfCar);
             carList.add(newCar);
         }
-        
+
         return carList;
+    }
+
+    public void racingForOneTurn(List<Car> carList) {
+        for (Car currentCar : carList) {
+            int randomNum = Randoms.pickNumberInRange(0,9);
+
+            if(randomNum >= 4) {
+                currentCar.increaseDistance();
+            }
+        }
     }
 }
