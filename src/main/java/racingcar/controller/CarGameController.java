94b0421b29eb8f43852be27f6980296497c15aee
sehee <tpgml701@naver.com>
2023-10-29 03:07:26 +0900

    feat: CarGameController 게임 전체 흐름 Controller 구현

--- src/main/java/racingcar/controller/CarGameController.java
+++ src/main/java/racingcar/controller/CarGameController.java
@@ -1,8 +1,36 @@
 package racingcar.controller;
 
+import racingcar.service.CarGameService;
+import racingcar.util.Parser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class CarGameController {
+    OutputView outputView;
+    InputView inputView;
+    CarGameService carGameService;
+    Parser parser;
+
+    public CarGameController() {
+        this.outputView = new OutputView();
+        this.inputView = new InputView();
+        this.carGameService = new CarGameService();
+        this.parser = new Parser();
+    }
 
     public void run() {
-        //
+        outputView.carNameMessage();
+        String userInput = inputView.getInput();
+        List<String> participant = parser.parseUserInputToList(userInput);
+
+        outputView.numberMessage();
+        String numberInput = inputView.getInput();
+        int times = parser.ParseInt(numberInput);
+
+        outputView.resultInfoMessage();
+        List<String> winner = carGameService.playGame(participant, times);
+        outputView.winnerMessage(winner);
     }
 }
