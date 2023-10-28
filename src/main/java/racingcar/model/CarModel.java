dcc6f9c829449637e49c1039c2b0639a6633917f
hYe807n <rlagkdus55@gmail.com>
2023-10-28 22:02:55 +0900

    Feat: carModel의 전진 기능 구현

--- src/main/java/racingcar/model/CarModel.java
+++ src/main/java/racingcar/model/CarModel.java
@@ -2,8 +2,15 @@ package racingcar.model;
 
 public class CarModel {
     private final String name;
+    private int location;
 
     public CarModel(String carName) {
         this.name = carName;
+        this.location = 0;
+    }
+
+    public int increaseLocation() {
+        this.location++;
+        return this.location;
     }
 }
