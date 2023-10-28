bbb546ac33f86690318bb3e62b6585f17865a1c7
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 02:46:33 +0900

    feat(Game): 중간 중간에 출력 형식에 맞는 적절한 메시지를 출력하는 코드 추가.

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -11,7 +11,9 @@ public class Game {
     public void start() {
         OutputView.askCarName();
         RacingCars racingcars = TypeCasting.stringToRacingCars(InputView.askCarName());
+        OutputView.askPlayTime();
         final int playTime = TypeCasting.stringToInteger(InputView.askPlayTime());
+        OutputView.showResultMessage();
         for (int i = 0; i < playTime; i++) {
             Refree.simulate(racingcars);
             OutputView.showCurrentDistance(racingcars);
