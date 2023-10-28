de0238219270844f5d3b7a2d806ef7e57372ee26
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 16:33:07 +0900

    refactor(ui): 변수 이름 변경

--- src/main/java/racingcar/ui/Message.java
+++ src/main/java/racingcar/ui/Message.java
@@ -1,9 +1,9 @@
 package racingcar.ui;
 
 public enum Message {
-    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_TRY_COUNT("시도할 회수는 몇회인가요?"),
-    RESULT_HEADER("실행 결과"),
+    START_MESSAGE_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    START_MESSAGE_TRY_COUNT("시도할 회수는 몇회인가요?"),
+    RESULT("실행 결과"),
     FINAL_WINNER("최종 우승자 : ");
 
     private final String message;
