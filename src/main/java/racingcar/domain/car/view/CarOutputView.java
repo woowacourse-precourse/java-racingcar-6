881bcdc3b391a4ffe80a31479d9066cc2d148288
02ggang9 <02ggang9@gmail.com>
2023-10-28 19:42:22 +0900

    chore(CarOutputView): EndGameMessage를 출력하는 메서드 삭제

--- src/main/java/racingcar/domain/car/view/CarOutputView.java
+++ src/main/java/racingcar/domain/car/view/CarOutputView.java
@@ -6,9 +6,6 @@ import racingcar.domain.util.OutputMessage;
 import java.util.List;
 
 public class CarOutputView {
-    public static void printEndGame() {
-        System.out.print(OutputMessage.PRINT_FINAL_WINNER.getMessage());
-    }
 
     public void printResult() {
         System.out.println(OutputMessage.PRINT_ROUND_RESULT.getMessage());
