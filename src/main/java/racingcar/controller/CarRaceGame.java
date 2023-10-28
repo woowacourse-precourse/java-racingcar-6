ba2484ec4d17452d77427ff6fc8b51270ac35d60
yooooonshine <hl3470@naver.com>
2023-10-28 22:10:42 +0900

    feat: 게임 시작 기능 구현

---
+++ src/main/java/racingcar/controller/CarRaceGame.java
@@ -0,0 +1,14 @@
+package racingcar.controller;
+
+public class CarRaceGame {
+
+    public CarRaceGame() {}
+
+    public void run() {
+        startGame();
+    }
+
+    public void startGame() {
+
+    }
+}
