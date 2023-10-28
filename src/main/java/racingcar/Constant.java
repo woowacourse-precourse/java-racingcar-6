52e69001ec17af143d41cd59435d6e208a9052c7
HeoManBo <iovesssr__@naver.com>
2023-10-29 00:14:55 +0900

    style 불필요한 코드 삭제

--- src/main/java/racingcar/Constant.java
+++ src/main/java/racingcar/Constant.java
@@ -7,7 +7,6 @@ public final class Constant {
     public final static String EXCEPTION_MESSAGE = "유효하지 않는 문자열입니다.";
     public final static String OVER_LENGTH_CAR_NAME = "5자 이하의 이름만 가능합니다.";
     public final static String IS_NOT_POSITIVE_INTEGER = "주어진 입력은 양의 정수가 아닙니다.";
-    public final static String NOT_ALLOW_NEGATIVE_OR_ZERO_ROUNT = "라운드에 음수나, 0을 입력할 수 없습니다.";
 
     private Constant(){}
 }
