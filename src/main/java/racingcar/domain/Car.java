9ea69359f5278f63e88463f3ecf9af6fb3c9743c
yooooonshine <hl3470@naver.com>
2023-10-28 22:57:51 +0900

    feat: Car 클래스 카운터 저장 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Car {
+    private int movementCounter = 0;
+
+    public int getMovementCounter() {
+        return movementCounter;
+    }
+
+    public void setMovementCounter(int movementCounter) {
+        this.movementCounter = movementCounter;
+    }
+}
