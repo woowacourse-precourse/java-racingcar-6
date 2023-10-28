d48f9ae52295b0afe564ecaf3cdc955c81b7efe5
scarlet0o0 <ghd7673@naver.com>
2023-10-28 21:02:08 +0900

    Feat : 에러 메시지를 보관하는 클래스 생성

---
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -0,0 +1,6 @@
+package racingcar.exception;
+
+public class ErrorMessage {
+    public static final String CAR_NAME_MAX_LENGTH = "자동차 이름은 5글자 이하여야 합니다.";
+    public static final String CAR_NAME_MIN_LENGTH = "자동차 이름은 1글자 이상이여야 합니다.";
+}
