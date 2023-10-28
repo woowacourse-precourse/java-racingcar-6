19feeb94e6b9e85c5442cb92a005c34a8c316fcd
sami355-24 <ddung355@gmail.com>
2023-10-28 23:02:37 +0900

    feat(View): 출력을 담당하는 OutputView 클래스 작성
    - TDD를 통한 개발을 진행하기 위해 로직이 담기지 않은 메서드만 추가

---
+++ src/main/java/racingcar/View/OutPutView.java
@@ -0,0 +1,12 @@
+package racingcar.View;
+
+import racingcar.Domain.CarGroup;
+
+public class OutPutView {
+
+    public static void showRoundResult(CarGroup carGroup) {
+    }
+
+    public static void showFinalWinner(CarGroup carGroup) {
+    }
+}
