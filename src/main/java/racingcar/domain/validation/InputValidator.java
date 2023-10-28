2d0c6e002b6d2018d96be000328a421d0f295cb4
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 17:13:10 +0900

    feat(domain): 시도 회수 검증 함수 추가

--- src/main/java/racingcar/domain/validation/InputValidator.java
+++ src/main/java/racingcar/domain/validation/InputValidator.java
@@ -3,10 +3,18 @@ package racingcar.domain.validation;
 import java.util.Arrays;
 
 public class InputValidator {
-    public void validateInput(String carNames) {
+    public void validateCarNames(String carNames) {
         if (Arrays.stream(carNames.split(",")).allMatch(s -> s.length() <= 5)) {
             return;
         }
         throw new IllegalArgumentException();
+    }
+
+    public void validateTryCount(String tryCount) {
+        try {
+            Integer.parseInt(tryCount);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
