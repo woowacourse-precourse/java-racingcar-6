aa23021c1e9ff3f21f35dad0b2606c0aa1e91eb2
slowcloud <sju9417@gmail.com>
2023-10-28 15:22:57 +0900

    test: GameTest에서 Game의 forwardCars를 테스트하는 코드 추가

---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,44 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+
+class GameTest {
+
+    public static MockedStatic<Randoms> randoms;
+
+    @BeforeAll
+    public static void beforeAll() {
+        randoms = Mockito.mockStatic(Randoms.class);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        randoms.close();
+    }
+
+    @Test
+    void forwardCars_메시지_반환() {
+
+        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(7);
+        List<Car> cars = List.of(new Car("foo"), new Car("bar"));
+        Game game = new Game(cars);
+
+        List<CarDistanceMessage> carDistanceMessages = game.forwardCars();
+        List<String> messages = carDistanceMessages.stream().map(CarDistanceMessage::toString).toList();
+
+        Assertions.assertThat(messages).contains("foo : -", "bar : -");
+
+    }
+
+}
