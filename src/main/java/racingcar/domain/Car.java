9119a9bb0147f6c22e152e0c72e4f99f191c90f4
jhsseonn <hs125bt@gmail.com>
2023-10-28 20:34:38 +0900

    feat: 자동차 객체 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int aheadCount;
+
+    public Car(String name){
+        this.name = name;
+        this.aheadCount = 0;
+    }
+}
