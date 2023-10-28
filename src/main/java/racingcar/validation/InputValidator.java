fb6e7642376200e35c61dc59eb5d29830c18d2f7
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-29 02:25:50 +0900

    feat: 입력이 blank와 empty인지 검사하는 method 추가

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -4,13 +4,36 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
+    private static final String INVALID_TRY_COUNT_MESSAGE = "시도할 횟수는 숫자만 입력 가능합니다.";
+    private static final String INPUT_NOT_BLANK_MESSAGE = "입력은 공백일 수 없습니다.";
+    private static final String INPUT_NOT_EMPTY_MESSAGE = "입력은 비어있을 수 없습니다.";
+
+
     private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
 
-    private static final String INVALID_TRY_COUNT_MESSAGE = "시도할 횟수는 숫자만 입력 가능합니다.";
+    public void validateInputEmpty(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException(INPUT_NOT_EMPTY_MESSAGE);
+        }
+    }
+
+    public void validateInputBlank(String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException(INPUT_NOT_BLANK_MESSAGE);
+        }
+    }
     public void validateTryCount(String tryCount) {
         if (isMatchNumber(tryCount)) {
             throw new IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE);
         }
+    }
+
+    private boolean isEmpty(String input) {
+        return !(input.isEmpty());
+    }
+
+    private boolean isBlank(String input) {
+        return !input.isBlank();
     }
 
     private boolean isMatchNumber(String tryCount) {
