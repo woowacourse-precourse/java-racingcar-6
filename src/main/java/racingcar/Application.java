8799105df924e719677f3da7c5a88c876dc73a4e
JW0321 <digneotn123@gmail.com>
2023-10-29 01:36:17 +0900

    Feat : 게임 시작 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Comparator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String name = Console.readLine();
+        Input.inputName(name);
+        System.out.println("시도할 회수는 몇회인가요?");
+        String number = Console.readLine();
+        Input.inputNumber(number);
+
     }
 }
