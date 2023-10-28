ad7a6386e3f1cfd8adff9c7c877b2463e6008e7b
takealook97 <takealook97@naver.com>
2023-10-29 01:12:54 +0900

    style: NULL_ERROR 에서 EMPTY_ERROR로 enum 수정
    입력을 하지않고 엔터를 누르면 null 이 아닌 ""가 입력되므로 그에 맞게 EMPTY로 네이밍을 수정하였다.

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-	NULL_ERROR("입력을 하지 않았습니다."),
+	EMPTY_ERROR("입력을 하지 않았습니다."),
 	LENGTH_ERROR(" -> 이름은 1 ~ 5 글자여야 합니다."),
 	SPACE_ERROR(" -> 이름에 공백만 포함할 수 없습니다."),
 	DUPLICATED_ERROR(" -> 중복된 이름을 입력할 수 없습니다."),
