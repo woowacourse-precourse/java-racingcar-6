7622a8d5014b9c04bca8ec9047dfeda07d05c9b1
D0ri123 <lsh81550@gmail.com>
2023-10-29 01:48:05 +0900

    feat: Car 클래스 분리 - 2차 구현
    1차 구현에서 정의한 핵심 도메인 Car를 새로운 클래스 파일로 생성했습니다.
    Car 클래스는 이름, 이동거리 정보를 가지고 있습니다.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Car {
+    String name;
+    int meter;
+
+    Map<Integer, Integer> movingLog;
+
+    public Car(String name) {
+        this.name = name;
+        this.meter = 0;
+    }
+
+    public void settingLog(int chance) {
+        movingLog = new HashMap<>();
+        for (int i = 1; i <= chance; i++) {
+            movingLog.put(i, 0);
+        }
+    }
+
+    public String printResult(int round) {
+        return name + " : " + "-".repeat(movingLog.get(round));
+    }
+}
