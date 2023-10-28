dca309a4eef9eff01549d8eef7a33eac370c2acb
inpink <inpink@kakao.com>
2023-10-28 14:21:53 +0900

    feat: 각 자동차를 담을 Car class 생성
    추상화 없이 일반 class 사용으로 충분함.
    멤버 필드는 id, name, 현재 위치, 현재까지 숫자가 뭐뭐가 나왔는지 담을 List<Integer>가 필요.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+
+    int id;
+    String name;
+    int currentPosition = 0;
+    List<Integer> pickedNumbers = new ArrayList<>();
+
+
+
+
+}
