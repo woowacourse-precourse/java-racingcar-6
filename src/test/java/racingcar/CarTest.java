2c344a56a5812384342a489e041b7b9fbb9323ae
minjun7410 <minjun7410@gmail.com>
2023-10-28 11:52:42 +0900

    test(CarTest): 자동차 최대 개수 예외처리 기능 테스트 코드 구현
    * 자동차의 최대 개수를 넘어섰을 때 예외처리를 하는지 테스트하는 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,9 +5,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.RandomNumberGenerator;
 
+import java.util.Arrays;
+
 public class CarTest {
+    @Test
+    void 자동차_최대_개수_예외처리_테스트() {
+        assertThatThrownBy(() -> new Cars(Arrays.asList("mj", "dh", "pobi", "jason"))).isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     void 자동차_이름_5자초과_예외처리_테스트() {
         assertThatThrownBy(() -> new Car("송민준입니다")).isInstanceOf(IllegalArgumentException.class);
