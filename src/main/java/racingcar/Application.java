c45d585252932795388d0f857a203f9a68ab56fb
juhwan5 <lavistrike770@gmail.com>
2023-10-28 22:11:16 +0900

    Car_1

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,4 +4,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+
+    public class Car{
+        String car_name;
+        int current_location;
+
+        Car(String car_name){
+            this.car_name = car_name;
+            this.current_location = 0;
+        }
+    }
 }
