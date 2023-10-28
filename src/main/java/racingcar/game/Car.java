f760e3a0248f0fc67dbe212f50d882e51ffcaf66
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-29 02:55:37 +0900

    feat(game): implement isMovable method for random movement in Car class

--- src/main/java/racingcar/game/Car.java
+++ src/main/java/racingcar/game/Car.java
@@ -1,5 +1,7 @@
 package racingcar.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private String distance;
@@ -11,5 +13,13 @@ public class Car {
 
     public void moveForward() {
         distance += "-";
+    }
+
+    public boolean isMovable() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
     }
 }
