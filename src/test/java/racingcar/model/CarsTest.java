fc78cc74575cade052d4e5101a06c771415a1889
Chan <33cks1423@naver.com>
2023-10-28 22:01:29 +0900

    test(cars): README.md와 test 추가

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -14,4 +14,29 @@ class CarsTest {
 
         assertThat(cars).isEqualTo(Cars.of("pobi,crong,honux"));
     }
+
+    @Test
+    @DisplayName("자동차들을 받아서 자동차들을 이동할 수 있다. - 모두 한칸식 이동")
+    void move() {
+        Cars cars = Cars.of("pobi,crong,honux");
+        Cars movedCars = cars.moves(() -> 4);
+        assertThat(movedCars).isEqualTo(Cars.ofWithCount("pobi,crong,honux", 1));
+    }
+
+    @Test
+    @DisplayName("실행 결과에서 우승자를 구한다.")
+    void winners() {
+        Cars cars = Cars.of("pobi,crong,honux");
+        Cars movedCars = cars.moves(() -> 4);
+        assertThat(movedCars.winners()).isEqualTo("pobi,crong,honux");
+    }
+
+    @Test
+    @DisplayName("자동차들의 이름과 위치를 반환한다.")
+    void toStringTest() {
+        Cars cars = Cars.of("pobi,crong,honux");
+        Cars movedCars = cars.moves(() -> 4);
+        assertThat(movedCars.toString()).isEqualTo("pobi : -\ncrong : -\nhonux : -");
+    }
+
 }
