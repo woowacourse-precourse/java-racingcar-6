fdde1f26a38fe3b2a51ad0c63295d1e700ef7efa
Mr.Kim <joajy98@gmail.com>
2023-10-28 20:13:10 +0900

    feat: 예외 사항 검증을 위한 클래스 및 상수 생성

---
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -0,0 +1,8 @@
+package racingcar.validator;
+
+public class CarValidator {
+
+    private static final int CHECK_IF_TRY_COUNT_LESS_THAN_ZERO = 0;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String STRING_REGEX = "^[\\w]*$";
+}
