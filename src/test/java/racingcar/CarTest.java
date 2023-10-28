3e8b4eb873279fe2d7dddc036010f2e6e9e8af40
Young1 <dlduddnjs198@naver.com>
2023-10-28 13:24:36 +0900

    test : 자동차 전진 확인 테스트 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -29,7 +29,7 @@ class CarTest {
     @Test
     public void 자동차_이름_쉼표구분_성공() {
         String carNames = "pobi,woni,jun";
-        Game game = new Game(carNames, 5);
+        Game game = new Game(carNames, "5");
 
         List<Car> cars = game.getCars();
         assertThat(cars.size()).isEqualTo(3);
@@ -43,8 +43,22 @@ class CarTest {
         String wrongCarNames = "pobi woni,jun";
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Game(wrongCarNames, 5))
+                .isThrownBy(() -> new Game(wrongCarNames, "5"))
                 .withMessage("자동차 이름이 적합하지 않습니다.");
+    }
+
+    @Test
+    public void 전진_확인_4이상() {
+        Car car = new Car("pobi");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1); // 4 이상이므로 1 전진
+    }
+
+    @Test
+    public void 전진_확인_3이하() {
+        Car car = new Car("pobi");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0); // 3 이하이므로 0 전진
     }
 
 
