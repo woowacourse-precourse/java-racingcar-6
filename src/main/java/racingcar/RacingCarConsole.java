9b5624b72cb9cda1aeab737db31e6dae4c1d82b8
CodingApe9 <tlqkf6974@>
2023-10-28 20:18:33 +0900

    feat(RacingCarConsole) : 자동차 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/RacingCarConsole.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class RacingCarConsole {
+    public String readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("자동차의 이름을 정해주세요.");
+        }
+    }
+}
