59d4892d2bc4b4582da31685f29868ccb8c8013c
Hajuuu <hpt0127@naver.com>
2023-10-28 17:01:41 +0900

    feat(main) : 입력(경주 할 자동차) 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Map<String, Integer> userMap = userCar();
+
+    }
+
+    public static Map<String, Integer> userCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputString = readLine();
+
+        StringTokenizer st = new StringTokenizer(inputString, ",");
+        Map<String, Integer> map = new LinkedHashMap<>();
+        while (st.hasMoreTokens()) {
+            map.put(st.nextToken(), 0);
+        }
+
+        return map;
     }
 }
