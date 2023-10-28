f02c48290c4265a5d2b29d2b1f2892ce7cf95ebd
34suuuuu <psh7492@gmail.com>
2023-10-28 16:46:21 +0900

    feat: 랜덤수 생성, 최대 주행거리 리턴, 결과값 출력 메서드 구현

--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -1,2 +1,31 @@
-package racingcar;public class Util {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static racingcar.Constants.MAX_RANDOM_NUM;
+import static racingcar.Constants.MIN_RANDOM_NUM;
+
+public class Util {
+
+    public static ArrayList<String> carNamesToArrayList(String carNames) {
+        return new ArrayList<>(Arrays.asList(carNames.split(Constants.DELIMETER)));
+    }
+
+    public static int getRandomNum(){
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
+    }
+
+    public static int getMax(ArrayList<Integer> positions){
+        return Collections.max(positions);
+    }
+
+    public static void printResult() {
+        System.out.println(Constants.WINNER_MESSAGE);
+
+    }
+
 }
