80872f448e38e42c086ff79228639153f7efae13
hyeongjun im <limbs713@naver.com>
2023-10-28 15:50:29 +0900

    :sparkles: outputWinner 메소드 추가
    우승한 자동차의 이름을 출력하는 메소드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,15 +1,17 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.model.domain.Car;
 
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
-    public static void outputResult(){
+
+    public static void outputResult() {
         System.out.println(RESULT_MESSAGE);
     }
 
-    public static void outputForward(Car car){
+    public static void outputForward(Car car) {
         int forwardCnt = car.getForward();
         System.out.print(car.getName() + " : ");
         for (int i = 0; i < forwardCnt; i++) {
@@ -18,7 +20,13 @@ public class OutputView {
         System.out.println("");
     }
 
-    public static void outputWinner(){
+    public static void outputWinner(List<String> winners) {
         System.out.print(WINNER_MESSAGE);
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i));
+            if (i != winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
