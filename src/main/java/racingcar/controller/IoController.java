5e208e953fb785f53b575e5a7e3ade8ebe757814
Brian <bwook9908@gmail.com>
2023-10-28 14:47:20 +0900

    feat: input, output을 관리하는 IoController 구현

--- src/main/java/racingcar/controller/IoController.java
+++ src/main/java/racingcar/controller/IoController.java
@@ -1,4 +1,38 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+
 public class IoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public IoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public ArrayList<String> raceCarNameInput() {
+        ArrayList<String> carNames = inputView.carNameInput();
+        return carNames;
+    }
+
+    public int trialCountInput() {
+        return inputView.trialCountInput();
+    }
+
+    public void notifyExecutionResult() {
+        outputView.executionResult();
+    }
+
+    public void notifyOneRoundCarResult(String result) {
+        outputView.showResult(result);
+    }
+
+    public void notifyWinners(String result) {
+        outputView.showWinner(result);
+    }
+
 }
