ba3f1ef072dc65c4b13d5a601b4a5e1e855cc219
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-28 23:39:09 +0900

    refactor(domain): createNumber 리턴 타입 변경

--- src/main/java/racingcar/domain/util/NumberGenerator.java
+++ src/main/java/racingcar/domain/util/NumberGenerator.java
@@ -2,8 +2,14 @@ package racingcar.domain.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class NumberGenerator {
-    public int createNumber() {
-        return Randoms.pickNumberInRange(0,9);
+    public List<Integer> createNumber(int totalCarNumber) {
+        return IntStream.range(0, totalCarNumber)
+                .mapToObj(number -> Randoms.pickNumberInRange(0,9))
+                .collect(Collectors.toList());
     }
 }
