fc8403e7b36f8c850579447157733014f17d1d8a
34suuuuu <psh7492@gmail.com>
2023-10-28 17:05:40 +0900

    feat: 입력 관련 상수, 메서드 추가

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
     public static final int MAX_RANDOM_NUM = 9;
     public static final int GO_NUM = 4;
 
+    public static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final Pattern TRY_NUM_PATTERN = Pattern.compile("^[1-9][0-9]*$");
 
     public static final String DELIMETER = ",";
@@ -16,5 +18,6 @@ public class Constants {
     public static final String INPUT_WRONG_ERROR_MESSAGE = "입력값이 존재하지 않습니다.";
     public static final String INPUT_ONLY_INTEGER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 
+    public static final String ROUND_RESULT = "실행 결과\n";
     public static final String WINNER_MESSAGE = "최종 우승자 : ";
 }
--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -1,13 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static racingcar.Constants.MAX_RANDOM_NUM;
-import static racingcar.Constants.MIN_RANDOM_NUM;
+import static racingcar.Constants.*;
 
 public class Util {
 
@@ -19,13 +19,29 @@ public class Util {
         return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
     }
 
-    public static int getMax(ArrayList<Integer> positions){
-        return Collections.max(positions);
+    public static String carNamesInput(){
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        return Console.readLine();
     }
 
-    public static void printResult() {
-        System.out.println(Constants.WINNER_MESSAGE);
+    public static String tryCntInput() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 
+    public static int getMax(ArrayList<Integer> positions){
+        return Collections.max(positions);
+
+    }
+
+    public static void printRoundResult(ArrayList<String> roundResults) {
+        roundResults.forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printWinner(ArrayList<String> winners) {
+        System.out.print(Constants.WINNER_MESSAGE);
+        System.out.println(String.join(DELIMETER, winners));
     }
 
 }
