79e5c4969ed3b0f37671e866da987f55dc8fc7dd
Hegale <juyeon@gimjuyeon-ui-MacBookAir.local>
2023-10-29 04:32:12 +0900

    fetch: 차량의 전진 여부를 결정하는 클래스 추가
    raceDecisionMaker class 추가
    - 0~9 사이의 랜덤값을 추출
    - 추출한 랜덤값이 4 이상이면 'true' 반환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,26 @@
 package racingcar;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+class raceDecisionMaker {
+    boolean raceDecision() {
+        int randomNumber = pickNumber();
+        return randomNumber >= 4;
+    }
+
+    private int pickNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
 
 class Car {
     String name;
     int position;
+
     Car(String name) {
         this.name = name;
         position = 0;
@@ -16,17 +31,19 @@ class Game {
     List<Car> cars;
     List<String> carNames;
     int carCount;
+
     Game(String input) {
         String[] tokens = input.split(",");
         carNames = Arrays.asList(tokens);
-        carCount = this.carsNames.length;
+        carCount = this.carNames.length;
         initCar();
     }
+
     void initCar() {
         cars = new ArrayList<Car>();
         for (int i = 0; i < carCount; ++i) {
             // 입력받은 이름을 기반으로 차례대로 차량 생성
-            car = new Car(carNames.get(i));
+            Car car = new Car(carNames.get(i));
             cars.add(car);
         }
     }
