dfe82fb0294acff112acf9ce3ecbf163a0d0a769
cloudchamb3r <jizon0123@protonmail.com>
2023-10-29 00:22:40 +0900

    refactor. clarify position => mileage, compareTo => compareMileage

--- src/main/java/racingcar/game/domain/Car.java
+++ src/main/java/racingcar/game/domain/Car.java
@@ -1,37 +1,36 @@
 package racingcar.game.domain;
 
-public class Car implements Comparable<Car> {
+public class Car {
     private final String name;
     private final CarEngine engine;
-    private int position;
+    private int mileage;
 
     public Car(String name, CarEngine engine) {
         this.name = name;
         this.engine = engine;
-        this.position = 0;
+        this.mileage = 0;
     }
 
-    public int getPosition() {
-        return position;
+    public int getMileage() {
+        return mileage;
     }
 
     public String getName() {
         return name;
     }
 
+    public int compareMileage(Car o) {
+        return mileage - o.getMileage();
+    }
+
     public void go() {
         if (engine.checkWorking()) {
-            this.position += 1;
+            this.mileage += 1;
         }
     }
 
     @Override
     public String toString() {
-        return name + " : " + "-".repeat(position);
-    }
-
-    @Override
-    public int compareTo(Car o) {
-        return position - o.getPosition();
+        return name + " : " + "-".repeat(mileage);
     }
 }
