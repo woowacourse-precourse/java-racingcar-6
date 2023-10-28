d522bcbd6c2de1c8c23ec63d2a6b250108aaabff
김효준 <khyojun1104@gmail.com>
2023-10-28 17:02:58 +0900

    test(CarRace) : 게임 횟수 입력에 대한 case들 테스트 추가!

--- src/test/java/racingcar/input/GameInputTest.java
+++ src/test/java/racingcar/input/GameInputTest.java
@@ -1,14 +1,13 @@
-package racingcar.inputest;
+package racingcar.input;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.service.CarGameService;
 
-public class InputTest {
+public class GameInputTest {
 
     private CarGameService carGameService;
 
@@ -19,7 +18,7 @@ public class InputTest {
 
     @ParameterizedTest
     @DisplayName("자동차 이름 입력에 따른 예외 처리! 입력 길이 5 초과인 경우")
-    @CsvSource({"123223", "123456,222,333", "123123123,23232323,44444"})
+    @ValueSource(strings = {"123223", "123456,222,333", "123123123,23232323,44444"})
     void 자동차이름_길이_5초과(String input){
         Assertions.assertThatThrownBy(() ->
             carGameService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class);
@@ -29,9 +28,17 @@ public class InputTest {
     @ParameterizedTest
     @DisplayName("자동차 이름 입력에 따른 예외 처리! 입력 길이 0인 경우")
     @ValueSource(strings = {"1,,,3", ",,,"})
-    void 자동차입름_길이_0인경우_(String input){
+    void 자동차이름_길이_0인경우_(String input){
         Assertions.assertThatThrownBy(() ->
             carGameService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("게임 횟수 숫자인지 검증!")
+    @ValueSource(strings = {"--=", "abd", "ddd", ""})
+    void 게임횟수_입력_검증(String input){
+        Assertions.assertThatThrownBy(()-> carGameService.convertGameCountToNumber(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
 
