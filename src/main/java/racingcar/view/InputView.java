693bd7cc8b98d25a4bcdc3fca558c4f4bd980e09
olsohee <dlthgml0108@daum.net>
2023-10-28 23:47:34 +0900

    feat(askCarName): 자동차 이름을 입력받는 askCarName 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,14 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public final class InputView {
 
     private static final String ASK_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_PLAY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static String askCarName() {
+        System.out.println(ASK_CAR_NAME_MESSAGE);
+        return Console.readLine();
+    }
 }
