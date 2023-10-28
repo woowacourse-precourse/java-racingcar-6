b1a057f88470c9d2748ba8257766389423a20247
DaeYoungK <hanguk830@naver.com>
2023-10-28 17:26:53 +0900

    feat(Car,CarList): 경주할 자동차 목록을 받아 정리하는 로직 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,4 +6,18 @@ public class Car {
     private int level;
     private String log;
 
+
+    private Car(String carName, int level, String log) {
+        this.carName = carName;
+        this.level = level;
+        this.log = log;
+    }
+
+    public static void setCar(String carName) {
+        CarList carList = CarList.getInstance();
+        Car car = new Car(carName, 0, "");
+        carList.getCars().add(car);
+    }
+
+
 }
--- src/main/java/racingcar/domain/CarList.java
+++ src/main/java/racingcar/domain/CarList.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class CarList {
 
+    private static CarList instance = new CarList();
+
+    private CarList(){}
+
+    public static CarList getInstance() {
+        return instance;
+    }
+
     private List<Car> cars = new ArrayList<>();
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
