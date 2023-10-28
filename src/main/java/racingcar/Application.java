5bb976355a1e3fb6167f052b9c5544e1411dc9e1
cloudchamb3r <jizon0123@protonmail.com>
2023-10-29 00:10:55 +0900

    feat. update main function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,13 @@
 package racingcar;
 
-import java.util.stream.Collectors;
+import racingcar.game.Game;
+import racingcar.game.GameManager;
+import racingcar.game.car.RacingCarGame;
 
 public class Application {
     public static void main(String[] args) {
-        var players = Input.inputPlayers();
-        var phase = Input.inputGamePhase();
-
-        var cars = players.stream().map(player -> new Car(
-                player,
-                new RandomizedCarEngine()
-        )).collect(Collectors.toList());
-
-        var game = new RacingCarGame(cars, phase);
-        game.start();
-        game.end();
+        Game game = new RacingCarGame();
+        GameManager gameManager = new GameManager(game);
+        gameManager.start();
     }
 }
