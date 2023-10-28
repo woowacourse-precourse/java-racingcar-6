75254ab7358cfc975b4bb5fda1a63a906e48752b
olsohee <dlthgml0108@daum.net>
2023-10-28 23:45:52 +0900

    feat(InputView): InputView 클래스 생성, 출력문 상수 처리

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,7 @@
+package racingcar.view;
+
+public final class InputView {
+
+    private static final String ASK_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_PLAY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+}
