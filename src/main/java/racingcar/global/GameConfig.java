aa7f42f520bfba86fbc71ba09db5cdbb52caaeae
Minjae-An <mj3242@naver.com>
2023-10-28 16:31:00 +0900

    feat : 이동 표현 기호 상수 추가

--- src/main/java/racingcar/global/GameConfig.java
+++ src/main/java/racingcar/global/GameConfig.java
@@ -2,7 +2,8 @@ package racingcar.global;
 
 public abstract class GameConfig {
 
-    public static final int RANDOM_NUMBER_START_INCLUSIVE=0;
-    public static final int RANDOM_NUMBER_END_INCLUSIVE=9;
-    public static final int MAX_CAR_NAME_LENGTH=5;
+    public static final int RANDOM_NUMBER_START_INCLUSIVE = 0;
+    public static final int RANDOM_NUMBER_END_INCLUSIVE = 9;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final String MOVE_SYMBOL = "-";
 }
