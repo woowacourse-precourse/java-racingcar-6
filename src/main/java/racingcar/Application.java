2b9e49a3564c4c95e38a6b318ed1674d177f63e7
euni <kongeuni1004@gmail.com>
2023-10-28 17:18:23 +0900

    Feat($AttemptsInput) : 시도횟수 입력
    - 시도횟수 문자열 입력
    - 입력받은 문자열 문자,숫자 판별
    - 숫자인경우 return

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,12 @@ package racingcar;
 import java.util.HashMap;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        CarNameInput carNameInput = new CarNameInput();
 
-        HashMap<String,Integer> userResult = carNameInput.inputCarName();
+        HashMap<String, Integer> userResult = new CarNameInput().inputCarName();
 
+        int attemptTimes = new AttemptsInput().inputAttempts();
     }
 }
