07e985a75e20d1e8f4e3189cf61b2d43b403a0bd
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 02:13:27 +0900

    feat(TypeCasting): validator를 제외한 코드 추가

--- src/main/java/racingcar/domain/TypeCasting.java
+++ src/main/java/racingcar/domain/TypeCasting.java
@@ -1,13 +1,18 @@
 package racingcar.domain;
 
 public class TypeCasting {
-    public static RacingCars stringToRacingCars(String cars) {
+    public static RacingCars stringToRacingCars(String stringCars) {
+        // validate(cars);
         RacingCars result = new RacingCars();
+        String[] stringArrayCars = stringCars.split(",");
+        for (String car : stringArrayCars) {
+            result.add(new RacingCar(car));
+        }
         return result;
     }
 
-    public static int stringToInteger(String cars) {
-        int result = 0;
-        return result;
+    public static int stringToInteger(String number) {
+        // validate(number);
+        return Integer.parseInt(number);
     }
 }
