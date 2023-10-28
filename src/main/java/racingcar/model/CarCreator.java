3ee2e9859f08d02724167bae95c0d5c9e3f2bd70
JUNG GYUWON <milestogo0905@naver.com>
2023-10-28 15:40:29 +0900

    feat: 문자열을 자동차의 이름으로 분리하는 메서드 추가
    자동차의 이름을 얻기 위하여 문자열을 자동차 이름으로 분리하는 메서드를 구현

--- src/main/java/racingcar/model/CarCreator.java
+++ src/main/java/racingcar/model/CarCreator.java
@@ -1,10 +1,15 @@
 package racingcar.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CarCreator {
 
-    public List<String> splitPlayerInput(String playerInput){
-        return null;
+    public List<String> splitPlayerInput(String playerInput) {
+        return arrayToList(playerInput.split(","));
+    }
+
+    private List<String> arrayToList(String[] array) {
+        return Arrays.asList(array);
     }
 }
