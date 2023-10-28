f423a2e3c0f144f6fb7f7716eeff7fb9b9e3a109
jsj <tkdlek4186@naver.com>
2023-10-28 13:26:59 +0900

    feat: 자동차이름 입력함수 구현

--- src/main/java/racingcar/Racingcar_Game.java
+++ src/main/java/racingcar/Racingcar_Game.java
@@ -5,10 +5,30 @@ import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-public class function {
+public class Racingcar_Game {
     public static List<String> userInput(){
         List<String> carName = new ArrayList<>();
 
         String input=readLine();
+        String name="";
+
+        for(int i =0;i<input.length();i++){
+            if(input.charAt(i)==','){
+                if(checkNameLength(name)){
+                    carName.add(name);
+                    name="";
+                };
+            }
+            else name+=input.charAt(i);
+        }
+        carName.add(name);
+
+        return carName;
+    }
+
+    public static boolean checkNameLength(String name){
+        if(name.length() > 5)
+            throw new IllegalArgumentException("Name is too long");
+        return true;
     }
 }
