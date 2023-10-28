8e9a10022de0fd3799f49636bdd6b04a6b151946
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-28 23:05:01 +0900

    feat(CarNameValidator): 자동차 이름 입력에 대한 검증 구현 클래스 추가

---
+++ src/main/java/racingcar/domain/validator/CarNameValidator.java
@@ -0,0 +1,33 @@
+package racingcar.domain.validator;
+
+import racingcar.domain.StringManipulator;
+import racingcar.domain.validator.exception.CarNameException;
+
+import java.util.List;
+
+public class CarNameValidator implements InputValidator {
+    @Override
+    public void validate(String input) {
+        check_null(input);
+
+        List<String> trimmedInputs = StringManipulator.split(input);
+
+        trimmedInputs.forEach(this::check_length);
+    }
+
+    private void check_null(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(CarNameException.NO_VALUE_NAME.getMessage());
+        }
+    }
+
+    private void check_length(String sentence) {
+        if (sentence.length() >=10) {
+            throw new IllegalArgumentException(CarNameException.INVALID_TOO_LONG_NAME.getMessage());
+        }
+
+        if (sentence.length() <=2) {
+            throw new IllegalArgumentException(CarNameException.INVALID_TOO_SHORT_NAME.getMessage());
+        }
+    }
+}
