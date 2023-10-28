ab52b679d27f9b270ed6559278a943021665fe79
leedohyun <lucky77795@naver.com>
2023-10-28 18:17:32 +0900

    feat(racingCar) : 이름 중복 검증 기능 추가.

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,5 +1,7 @@
 package racingcar.validator;
 
+import java.util.Arrays;
+
 public class InputValidator {
 
     private final String CAR_NAME_REGEX = "\\w+(\\s+\\w+)*(,\\w+(\\s+\\w+)*)*"; // ,뒤 공백은 비허용/이름 사이 공백은 허용
@@ -7,6 +9,19 @@ public class InputValidator {
 
     public void validateInputCarName(String carName) {
         if (!carName.matches(CAR_NAME_REGEX)) {
+            throw new IllegalArgumentException();
+        }
+
+        checkDuplicateCarName(carName);
+    }
+
+    private void checkDuplicateCarName(String carName) {
+        String[] names = carName.split(",");
+        long nonDuplicateNameSize = Arrays.stream(names)
+                .distinct()
+                .count();
+
+        if (nonDuplicateNameSize != names.length) {
             throw new IllegalArgumentException();
         }
     }
