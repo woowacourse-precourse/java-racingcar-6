c842ad2e68353cca2f8c28a1eff806bb3625c8c0
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-28 13:05:00 +0900

    feat(InputView): n대의 자동차이름 입력기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	public String inputCarNames() {
+		String inputValue = Console.readLine();
+
+		return inputValue;
+	}
+}
