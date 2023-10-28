ad760a682a262cd2d4ba4e9007d05eda446cdd81
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 17:06:41 +0900

    feat : 자동차 이름에 대한 비즈니스 검증 기능 구현 - 자동차 이름의 길이가 5 초과인 경우 예외 발생

---
+++ src/main/java/racingcar/exception/CarNameValidator.java
@@ -0,0 +1,20 @@
+package racingcar.exception;
+
+public class CarNameValidator {
+
+    private static final Integer CAR_NAME_MAX_LENGTH = 5;
+    private static final String INVALID_CAR_NAME_LENGTH_ERROR_MSG = "자동차 이름은 " + CAR_NAME_MAX_LENGTH + "자 이하만 가능합니다.";
+
+    private CarNameValidator() {
+    }
+
+    public static void validateCarName(String carName) {
+        if (isCarNameLongerThanMaxLength(carName)) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH_ERROR_MSG);
+        }
+    }
+
+    private static boolean isCarNameLongerThanMaxLength(String carName) {
+        return carName.length() > CAR_NAME_MAX_LENGTH;
+    }
+}
