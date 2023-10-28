23b622fc27bf5099ff1358a86ed7bd8b9b2a8649
DK <laancer4@gmail.com>
2023-10-28 18:51:32 +0900

    refactor : util 클래스로 변경

--- src/main/java/console/InputView.java
+++ src/main/java/console/InputView.java
@@ -4,11 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String NON_NUMERIC_INPUT = "숫자만 입력할 수 있다.";
-    public String getInput(){
+
+    private InputView() { // util class
+        throw new IllegalArgumentException();
+    }
+    public static String getInput(){
         return Console.readLine();
     }
 
-    public int getNumericInput(){
+    public static int getNumericInput(){
         String userInput = Console.readLine().trim(); // trim 해서 공백 제거
         try {
             return Integer.parseInt(userInput);
