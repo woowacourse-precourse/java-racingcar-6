3f9c060cc4aeac6fc81ff85171c3b9b6c455ad13
penrose15 <seongju99@naver.com>
2023-10-28 16:20:44 +0900

    Feat: 자동차 이름을 입력 받으면 자동차를 생성하는 Generator 생성

---
+++ src/main/java/racingcar/domain/car/CarGenerator.java
@@ -0,0 +1,25 @@
+package racingcar.domain.car;
+
+import racingcar.domain.car.dto.CarsNameDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarGenerator {
+    public List<Car> cars;
+    public RandomUtil randomUtil;
+
+    public CarGenerator(RandomUtil randomUtil) {
+        this.cars = new ArrayList<>();
+        this.randomUtil = randomUtil;
+    }
+
+    public void generateCars(CarsNameDto names) {
+        String carNames = names.names();
+        String[] carNamesArr = carNames.split(",");
+        for (String carName : carNamesArr) {
+            Car car = new Car(carName, randomUtil);
+            cars.add(car);
+        }
+    }
+}
