e3004e97168844d9a455a79e3c53c71991cc4fd9
seokmyungham <hellotjraud@gmail.com>
2023-10-28 09:34:03 +0900

    feat(Car): Car 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private Name name;
+    private Move move;
+
+    public Car(Name name, Move move) {
+        this.name = name;
+        this.move = move;
+    }
+}
