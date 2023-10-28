4c2e2dd5073ca975ab8aebb7c9f41538b499cbbc
eunseob <seobseob0810@naver.com>
2023-10-28 20:41:25 +0900

    feat(Car): moveForward 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     String carName;
     Long totalMovements;
@@ -15,5 +17,16 @@ public class Car {
 
     public Long getToTalMovements() {
         return totalMovements;
+    }
+
+    public boolean isGo() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        return random >= 4;
+    }
+
+    public void moveForward() {
+        if (isGo()) {
+            totalMovements += 1;
+        }
     }
 }
