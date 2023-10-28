2a09d915abb3210bcd4f2f5f68987833d350f0a9
Hooni <gkxogns21@gmail.com>
2023-10-28 18:57:50 +0900

    feat: implement input & make racer's list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.*;
+
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        // Implement input
+        // 경주 할 자동차 이름 입력
+        String racerNames = scanner.nextLine();
+        // ","을 기준으로 하여 각 경주 참가자들을 Array에 임시 저장
+        String[] racerArray = racerNames.split(",");
+        // racer들의 이름을 List<Stirng>에 저장
+        List<String> racerList = new ArrayList<>();
+        for (int i = 0; i < racerArray.length; i++) {
+            racerList.add(racerArray[i]);
+        }
+        // 시도할 횟수 입력
+        int num = scanner.nextInt();
+
+
     }
 }
