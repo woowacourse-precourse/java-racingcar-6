229cf9e3493dc670f55bdbad4c3da53ee57bee06
doha <dohaahi@naver.com>
2023-10-28 16:57:57 +0900

    feat: 자동차 객체 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import racingcar.util.Constants;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    private Car(final String name) {
+        this.name = name;
+    }
+
+    public static Car of(final String name) {
+        return new Car(name);
+    }
+
+    public void move() {
+        int randomNumber = randomNumberGenerator();
+
+        if (isMoreThanFour(randomNumber)) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    private int randomNumberGenerator() {
+        return pickNumberInRange(Constants.START_INCLUSIVE, Constants.END_INCLUSIVE);
+    }
+
+    private boolean isMoreThanFour(final int pickedNumber) {
+        return pickedNumber > 4;
+    }
+}
--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String GAME_RESULT_MESSAGE = "실행 결과";
     public static final String GAME_WINNER_MESSAGE = "최종 우승자 :";
+    public static final int START_INCLUSIVE = 0;
+    public static final int END_INCLUSIVE = 0;
 
     public static final String WRONG_INPUT_CAR_NAME_CHARACTER_MASSAGE = "자동차 이름은 문자로 입력해주세요.";
     public static final String WRONG_INPUT_CAR_NAME_LENGTH_MESSAGE = "자동차 이름은 5자 이하로 입력해주세요,";
