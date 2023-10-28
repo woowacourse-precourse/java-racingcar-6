1be4c0c3a57ce1ac2b0ae0abb5a5109e984589e0
seokmyungham <hellotjraud@gmail.com>
2023-10-29 08:10:44 +0900

    feat(GameView): printAttemptCountMessage 메서드 추가
    printAttemptCountMessage: 시도할 회수를 입력받는 메시지를 출력합니다.

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printCarNamesMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -12,5 +13,9 @@ public class GameView {
     public String inputCarNames() {
         String carNames = Console.readLine();
         return carNames;
+    }
+
+    public void printAttemptCountMessage() {
+        System.out.println(ATTEMPT_COUNT_INPUT_MESSAGE);
     }
 }
