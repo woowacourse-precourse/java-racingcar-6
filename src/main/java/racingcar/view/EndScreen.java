d7594482578967c7e45de7b8ac5f12915552aec4
chae401 <chae401@sju.ac.kr>
2023-10-29 05:05:24 +0900

    fix(EndScreen): resolve wrong printing
    마지막 우승자뒤에 가 추가되는 문제 해결

--- src/main/java/racingcar/view/EndScreen.java
+++ src/main/java/racingcar/view/EndScreen.java
@@ -1,15 +1,16 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.dto.CarDTO;
 
 public class EndScreen {
     private static final String WINNER_IS = "최종 우승자 : ";
     private static final String WINNER_SEPARATOR = ", ";
     public static void printWinner(List<CarDTO> winners){
-        System.out.print(WINNER_IS);
-        winners.forEach(winner -> {
-            System.out.print(winner.getName() + WINNER_SEPARATOR);
-        });
+        String winnerNames = winners.stream()
+                .map(winner -> winner.getName())
+                .collect(Collectors.joining(WINNER_SEPARATOR));
+        System.out.println(WINNER_IS + winnerNames);
     }
 }
