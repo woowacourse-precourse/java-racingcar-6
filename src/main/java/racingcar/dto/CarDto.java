7dafbbd400e3f0cdad5f552866ddafec7b9badc9
olsohee <dlthgml0108@daum.net>
2023-10-29 00:14:22 +0900

    feat(CarDto): CarDto 생성

---
+++ src/main/java/racingcar/dto/CarDto.java
@@ -0,0 +1,15 @@
+package racingcar.dto;
+
+public class CarDto {
+
+    private String name;
+    private int position;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
