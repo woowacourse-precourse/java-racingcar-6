37a9d53c42f39c8eaa5bc70c71cf63e12359a7f7
SungWoo <sungwoo166@gmail.com>
2023-10-28 21:45:56 +0900

    feat(model): Implement CarRace logic

---
+++ src/main/java/racingcar/model/CarRace.java
@@ -0,0 +1,47 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarRace {
+    private final List<RacingCar> racingCars;
+    private final int targetTurn;
+    private final RandomDistanceGenerator randomDistanceGenerator;
+
+    public CarRace(ArrayList<RacingCar> racingCars, int targetTurn, RandomDistanceGenerator randomDistanceGenerator) {
+        this.racingCars = racingCars;
+        this.targetTurn = targetTurn;
+        this.randomDistanceGenerator = randomDistanceGenerator;
+    }
+
+    public List<RacingCar> race() {
+        moveCars();
+        return racingCars;
+    }
+
+    public int getTargetTurn() {
+        return targetTurn;
+    }
+
+    public List<RacingCar> getWinners() {
+        int maxDistance = getMaxDistance();
+        return racingCars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
+
+    private void moveCars() {
+        for (RacingCar racingCar : racingCars) {
+            racingCar.moveForward(randomDistanceGenerator.moveDistance());
+        }
+    }
+
+    private int getMaxDistance() {
+        return racingCars.stream()
+                .mapToInt(RacingCar::getDistance)
+                .max()
+                .orElse(0);
+    }
+
+}
