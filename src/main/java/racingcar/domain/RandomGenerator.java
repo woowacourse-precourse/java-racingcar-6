118fd03d71a271ba97dea0e1e71cde07e5f22ab5
김영준 <rladudwns164@gmail.com>
2023-10-28 20:12:19 +0900

    refactor(RandomGenerator) : 인스턴스 삭제
    -private int number 삭제

--- src/main/java/racingcar/domain/RandomGenerator.java
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator {
-    private int number;
 
     public int generate() {
         return Randoms.pickNumberInRange(Constants.MIN_RANDOM_NUM, Constants.MAX_RANDOM_NUM);
