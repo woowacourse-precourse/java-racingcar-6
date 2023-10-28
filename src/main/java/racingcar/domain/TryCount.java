6683d14f7bf4bdb91ca5ea2aedd30a1c88defb26
noxknow <chris2769@naver.com>
2023-10-28 16:16:34 +0900

    feat(TryCount) : user가 입력한 값이 1 이상의 수 인지 확인한다

---
+++ src/main/java/racingcar/domain/TryCount.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import static racingcar.handler.ErrorHandler.INVALID_RANGE;
+
+public class TryCount {
+
+    private final int tryCount;
+
+    public TryCount(int tryCount) {
+        validateRange(tryCount);
+
+        this.tryCount = tryCount;
+    }
+
+    public void validateRange(int tryCount) {
+        if (tryCount < 1) {
+            throw INVALID_RANGE.getException();
+        }
+    }
+}
--- src/main/java/racingcar/handler/ErrorHandler.java
+++ src/main/java/racingcar/handler/ErrorHandler.java
@@ -2,10 +2,11 @@ package racingcar.handler;
 
 public enum ErrorHandler {
 
-    INVALID_NUMBER("유효하지 않은 숫자입니다."),
+    INVALID_NUMBER("숫자가 아닌 값은 입력하실 수 없습니다."),
     INVALID_INPUT("자동차 이름은 쉼표로 구분해야 합니다."),
     INVALID_ENGLISH_KOREAN("영어와 한글만 사용할 수 있습니다."),
     INVALID_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
+    INVALID_RANGE("시도하는 회수는 1 이상의 값입니다."),
     DUPLICATE_NUMBER("중복된 이름은 들어갈 수 없습니다.");
 
     private final String errorMessage;
