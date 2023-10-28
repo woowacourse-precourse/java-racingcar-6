6bbb4140e918bc137fbf4679bcd655b1443e4996
박정현 <ros28081@naver.com>
2023-10-28 17:10:00 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private static final int MAX_NAME_SIZE = 5;
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+        this.name = name;
+        this.position = 0;
+    }
+}
