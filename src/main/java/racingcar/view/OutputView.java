6c2c53be03bafcbc6a5b898f8b9231b2e53fc21a
ParkDH0809 <znxn1237@gmail.com>
2023-10-28 11:59:00 +0900

    feat(output): 최종 우승 결과 메시지 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String OUTPUT_EXECUTION_RESULT = "\n실행 결과";
     private static final String OUTPUT_MOVE_RESULT = "%s : %s\n";
     private static final String OUTPUT_BLANK = "\n";
+    private static final String OUTPUT_RACE_RESULT = "최종 우승자 : %s";
 
     public void outputExecutionResult() {
         System.out.println(OUTPUT_EXECUTION_RESULT);
@@ -14,5 +17,8 @@ public class OutputView {
     }
     public void outputBlank() {
         System.out.printf(OUTPUT_BLANK);
+    }
+    public void outputRaceResult(List<String> names) {
+        System.out.printf(OUTPUT_RACE_RESULT, String.join(", ", names));
     }
 }
