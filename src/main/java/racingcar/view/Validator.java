e0f080f01a4e446b4cf0893641cf10b5139f960b
yoonseo choi <choiyoonseo02@gmail.com>
2023-10-29 05:02:49 +0900

    feat: 자동차 이름에 문자와 쉼표 이외의 값이 있는지 검증

--- src/main/java/racingcar/view/Validator.java
+++ src/main/java/racingcar/view/Validator.java
@@ -1,11 +1,14 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import racingcar.model.Car;
 
 public class Validator {
-    private static boolean isCarNameOverFiveCharacters(){
-        List<String> carNameList = Car.getCarName();
+    private static final List<String> carNameList = Car.getCarName();
+
+    private static boolean isCarNameOverFiveCharacters() {
         boolean fiveCharacters = true;
         for (String carName : carNameList) {
             if (carName.length() > 5) {
@@ -13,5 +16,20 @@ public class Validator {
             }
         }
         return fiveCharacters;
+    }
+
+    private static boolean validateDelimiter() {
+        boolean validateDelimiter = true;
+
+        String specialCharacters = "[^\\\\w,]+";
+        Pattern p = Pattern.compile(specialCharacters);
+        for (String carName : carNameList) {
+            Matcher m = p.matcher(carName);
+            if (m.find()) {
+                validateDelimiter = false;
+            }
+        }
+
+        return validateDelimiter;
     }
 }
