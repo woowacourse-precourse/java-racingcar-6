4db2bddaeebdc50e5339ea58d60b4e9bb4729c26
sehee <tpgml701@naver.com>
2023-10-28 22:45:25 +0900

    feat: InputView 참가자 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,19 @@
 package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
-    public String getUserInput() {
+    public String getNumberInput() {
         return Console.readLine();
+    }
+
+    public List<String> getUserInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
