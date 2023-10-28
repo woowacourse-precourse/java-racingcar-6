ee7f5e1fd234c4a18a0365621f53a99a7c2be6be
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 21:27:46 +0900

    refactor: 사용자로부터 데이터를 입력 받고 car 인스턴스를 생성하는 기능 추가 및 프로그램을 시작하는 run 메소드 생성

--- src/main/java/controller/CarRace.java
+++ src/main/java/controller/CarRace.java
@@ -1,6 +1,7 @@
 package controller;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import model.GroupOfCar;
 import view.InputView;
 import model.Car;
 import java.util.ArrayList;
@@ -9,8 +10,24 @@ import java.util.List;
 import validator.InputValidator;
 
 public class CarRace {
-    private List<Car> cars = new ArrayList<Car>();
-    private InputValidator inputValidator = new InputValidator();
+    private final InputValidator inputValidator = new InputValidator();
+    private final GroupOfCar groupOfCar = new GroupOfCar();
+
+    public void run(){ //프로그램의 시작
+        setCarName(parseCarName(getCarNameByInput()));
+
+    }
+
+    private void setCarName(String[] carName) {
+        for(String name: carName) {
+            groupOfCar.addCar(new Car(name));
+        }
+    }
+
+    private String[] parseCarName(String input){
+        String carNames = getCarNameByInput();
+        return carNames.split(",");
+    }
 
     private String getCarNameByInput() {
         InputView.printUserInputCarNameMessage();
@@ -25,11 +42,6 @@ public class CarRace {
         } catch (IllegalArgumentException e){
             throw e;
         }
-    }
-
-    private List<String> parseCarName(String input) {
-        List<String> carName = Arrays.asList(input);
-        return carName;
     }
 
 
