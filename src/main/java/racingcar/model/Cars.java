ee8f80adaaccc75cda432b608b651fe65e76b28e
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-28 20:18:31 +0900

    feat : 컬렉션 필드를 순회하여 Car들의 전진한 횟수를 변경하는 기능 구현

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import racingcar.exception.CarNamesValidator;
+
+public class Cars {
+
+    private final List<Car> carList;
+
+    public Cars(final List<String> carNames) {
+        CarNamesValidator.validateCarNames(carNames);
+
+        List<Car> cars = new ArrayList<>();
+        carNames.forEach(carName -> cars.add(new Car(carName)));
+
+        carList = Collections.unmodifiableList(cars);
+    }
+
+    public void move() {
+        carList.stream().forEach(car -> car.move());
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+}
