c883e568924dd7c30338bfa43b171a582b720bf2
goragoraki <gorakio0126@gmail.com>
2023-10-28 23:38:27 +0900

    feat(CarInfo): add getCarStatus
    Add getCarStatus function to return the number of times the car moves

--- src/main/java/racingcar/model/CarInfo.java
+++ src/main/java/racingcar/model/CarInfo.java
@@ -5,10 +5,12 @@ import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.w3c.dom.ls.LSInput;
 import racingcar.util.validation;
 
 public class CarInfo {
     private final List<String> carName = new ArrayList<>();
+    private final List<Integer> carStatus = new ArrayList<>();
     public void setCarName(String carName) {
         String[] splitStr = carName.split(",");
         Collections.addAll(this.carName, splitStr);
@@ -20,4 +22,16 @@ public class CarInfo {
         int randomNum = Randoms.pickNumberInRange(0,9);
         return randomNum >= 4;
     }
+    public void updateCarStatus() {
+        for (int i = 0; i < carName.size(); i++) {
+            if(isMove()){
+                int newValue = carStatus.get(i) + 1;
+                carStatus.set(i, newValue);
+            }
+        }
+    }
+    public List<Integer> getCarStatus() {
+        return carStatus;
+    }
+
 }
