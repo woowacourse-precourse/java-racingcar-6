d1a13f96bda8de5ed08f455b8369d9c5e99ae2ef
scarlet0o0 <ghd7673@naver.com>
2023-10-28 21:02:35 +0900

    Feat : 이름이 0일 경우 에러 발생

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,12 +1,17 @@
 package racingcar.domain.car;
 
+import racingcar.exception.ErrorMessage;
+
 public class Name {
     private final static int NAME_SIZE = 5;
     private final String  value;
 
     public Name(String name) {
         if (name.length() > NAME_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_MAX_LENGTH);
+        }
+        if(name.length() == 0){
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_MIN_LENGTH);
         }
         this.value = name;
     }
