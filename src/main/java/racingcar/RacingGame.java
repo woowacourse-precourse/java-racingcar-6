582c519999e99e61fdd38e4537fa7a356605a5b3
WinterHana <haha920@naver.com>
2023-10-28 19:16:08 +0900

    feat(RacingGame) : RacingGame 클래스 추가
    - 게임을 진행하기 위한 클래스 추가

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    List<Car> racingCars = new ArrayList<Car>();
+
+    RacingGame(String cars) {
+        
+    }
+}
