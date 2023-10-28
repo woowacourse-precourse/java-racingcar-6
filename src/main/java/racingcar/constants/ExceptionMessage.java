458fe4181f52c89b0c879b9b6d2c9c896b5c5b7b
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 01:09:40 +0900

    fix(ExceptionMessage): 예외메시지 수정
    docs: 예외메시지 수정

--- src/main/java/racingcar/constants/ExceptionMessage.java
+++ src/main/java/racingcar/constants/ExceptionMessage.java
@@ -2,7 +2,7 @@ package racingcar.constants;
 
 public enum ExceptionMessage {
     NONE_INPUT_CARS("자동차 이름은 공백이 될 수 없습니다."),
-    CAR_NAME_LIMIT_EXCEEDED("자동차 이름은 5자 이하만 가능합니다."),
+    CAR_NAME_LIMIT_EXCEEDED("자동차 이름은 1자 이상 5자 이하만 가능합니다."),
     NOT_INPUT_CARS_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
     CARS_LESS_THAN_TWO("자동차 이름은 2개 이상이어야 합니다."),
     INVALID_CHARACTER_INPUT_ERROR("문자,쉼표만 입력 가능 합니다."),
