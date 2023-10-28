9ff5a8353c5ae4208c36fda2abdc52953fe7483f
kim <hwiyul_g@daum.net>
2023-10-28 22:21:44 +0900

    refactor : 이름 목록을 입력받아 Cars로 변경하는 것은 Parser가 아닌 Cars에 역할에 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -3,23 +3,32 @@ package racingcar.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
+
     private final List<Car> cars;
 
-    public Cars(List<Car> cars){
+    public Cars(List<Car> cars) {
         this.cars = new ArrayList<>(cars);
     }
 
-    public Cars playRound(){
+    public static Cars fromNames(List<String> carNames) {
+        List<Car> cars = carNames.stream()
+            .map(carName -> new Car(carName, 0))
+            .collect(Collectors.toList());
+        return new Cars(cars);
+    }
+
+    public Cars playRound() {
         List<Car> movedCars = new ArrayList<>();
-        for(Car car : cars){
+        for (Car car : cars) {
             movedCars.add(car.attemptMove());
         }
         return new Cars(movedCars);
     }
 
-    public List<Car> toList(){
+    public List<Car> toList() {
         return Collections.unmodifiableList(cars);
     }
 
--- src/main/java/racingcar/utils/InputParser.java
+++ src/main/java/racingcar/utils/InputParser.java
@@ -2,19 +2,11 @@ package racingcar.utils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import racingcar.model.Car;
 
 public class InputParser {
 
     public static List<String> parseCarNames(String input) {
         return Arrays.asList(input.split(","));
-    }
-
-    public static List<Car> convertNamesToCars(List<String> carNames){
-        return carNames.stream()
-            .map(carName -> new Car(carName, 0))
-            .collect(Collectors.toList());
     }
 
 }
