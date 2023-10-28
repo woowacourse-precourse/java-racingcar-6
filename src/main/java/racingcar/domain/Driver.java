a8d304bc4c62a9f09a662174c1597fdf4d36c5cf
liarreez <liarreez@gmail.com>
2023-10-28 14:46:57 +0900

    style : 의미를 분명히하기 위해drivingSkill메서드명을 calculateDrivingSkill으로 변경

--- src/main/java/racingcar/domain/Driver.java
+++ src/main/java/racingcar/domain/Driver.java
@@ -7,10 +7,10 @@ public class Driver {
     private final int RANDOM_NUMBER_MAX_VALUE = 9;
 
     public void drive(Car car) {
-        car.move(drivingSkill());
+        car.move(calculateDrivingSkill());
     }
 
-    private int drivingSkill() {
+    private int calculateDrivingSkill() {
         return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_VALUE, RANDOM_NUMBER_MAX_VALUE);
     }
 }
