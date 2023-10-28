c9ea0080df73ea1be2d14bb5b427165fd5d0c953
JW0321 <digneotn123@gmail.com>
2023-10-29 03:26:58 +0900

    Feat : 전진 여부 판단 및 전진 실행 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,8 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 
 public class Car {
     private final HashMap<String, Integer> racingLineup;
@@ -10,6 +11,24 @@ public class Car {
         this.racingLineup = new HashMap<>();
         for (String name : names) {
             this.racingLineup.put(name, 0);
+        }
+    }
+
+    void moveCar(Car cars) {
+        Set<String> carNames = this.racingLineup.keySet();
+        for (String name : carNames) {
+            increaseCarPosition(name);
+        }
+    }
+
+    boolean checkCarMove() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        return randomNumber > 3;
+    }
+
+    void increaseCarPosition(String name) {
+        if (checkCarMove()) {
+            this.racingLineup.put(name, this.racingLineup.get(name) + 1);
         }
     }
 
