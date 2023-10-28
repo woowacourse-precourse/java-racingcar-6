1acbcd4fc23b77d83cd670433ed1af338e4f9a4c
chansoo <chansoo38@gmail.com>
2023-10-28 14:14:30 +0900

    <Feat> Controller - 게임을 진행하는 클래스를 구현한다.

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.Utils.InputValidator;
+
+public class Controller {
+    private final InputValidator inputValidator;
+    private final Service service;
+    private int tryCount;
+
+    public Controller() {
+        inputValidator = new InputValidator();
+        service = new Service();
+    }
+
+    public void run() {
+        initGame();
+        startGame();
+        endGame();
+    }
+
+    private void initGame() {
+        String inputCars = service.receiveCars();
+        List<String> cars = inputValidator.validateCarName(inputCars);
+        service.registerCars(cars);
+
+        String inputTryCount = service.receiveTrialCount();
+        tryCount = inputValidator.validateTryCount(inputTryCount);
+    }
+
+    private void startGame() {
+        service.printResultMessage();
+        while (tryCount > 0) {
+            service.executeOneGame();
+            service.showProgress();
+            tryCount--;
+        }
+    }
+
+    private void endGame() {
+        service.showFinalWinner();
+    }
+
+}
