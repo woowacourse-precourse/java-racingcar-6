d5188c364879319de9c610287ff8ae4661204af5
Hanjaemo <hjm2530@gmail.com>
2023-10-28 21:30:59 +0900

    feat(Car): 자동차 이름 검증 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,8 +6,15 @@ public class Car {
     private int forwardCount;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
         this.forwardCount = 0;
+    }
+
+    private void validate(String name) {
+        if (!(1 <= name.length() && name.length() <= 5)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
     }
 
     public int moveForward(int value) {
