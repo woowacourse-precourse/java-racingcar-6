41eda43e8b65a1cdfe8ce560e749a289cf648858
hyeongjun im <limbs713@naver.com>
2023-10-28 14:25:56 +0900

    :sparkles: carForward 메소드 추가
    모든 car 객체의 전진 여부를 결정하고 값을 증가시키는 메소드를 추가한다.

--- src/main/java/racingcar/model/repository/CarRepository.java
+++ src/main/java/racingcar/model/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package racingcar.model.repository;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.domain.Car;
@@ -7,11 +8,23 @@ import racingcar.model.domain.Car;
 public class CarRepository {
     List<Car> cars;
 
-    public CarRepository(){
+    public CarRepository() {
         cars = new ArrayList<>();
     }
 
-    public void save(String name){
+    public void save(String name) {
         cars.add(new Car(name));
+    }
+
+    public void carForward() {
+        cars.forEach(car -> {
+            if (makeRandomNumber() >= 4) {
+                car.setForward(car.getForward() + 1);
+            }
+        });
+    }
+
+    private int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
