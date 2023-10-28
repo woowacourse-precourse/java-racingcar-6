de9380215ca2f5ab0387b8d201f521cadc23def9
김민정 <mjeong@sookmyung.ac.kr>
2023-10-28 11:12:16 +0900

    feat: 주어진 횟수만큼 경주 게임을 수행하는 기능 구현

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RaceManager {
-
     List<Car> raceCarList = new ArrayList<>();
+    private int attempts = 0;
+
+    private void runRace() {
+        for (int i = 0; i < attempts; i++) {
+            runRaceRound();
+        }
+    }
 
     private void runRaceRound() {
         for (Car car : raceCarList) {
