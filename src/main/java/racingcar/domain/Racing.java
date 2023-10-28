159444dd9e5bbb1c3c1f50b9d9aad7b099dc47de
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-10-29 02:51:23 +0900

    Feat: 0~9에서 무작위 값 추출 기능 추가
    - 0~9에서 무작위 값을 추출할 수 있다. getRanNum()

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,17 +1,23 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static java.lang.Integer.parseInt;
 
+
 public class Racing {
+    List<String> carNames;
+    List<String> carPoints;
     private Racing(){
 
     }
     public List<String> getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
-        List<String> carNames = Arrays.asList(Console.readLine().split(","));
+        carNames = Arrays.asList(Console.readLine().split(","));
         checkNameLength(carNames);
 
         return carNames;
@@ -22,11 +28,27 @@ public class Racing {
                 throw new IllegalArgumentException("each racing car's name must be shorter than 6");
             }
         }
-        return 0;
-
+        return carNames.size();
     }
     public int getRoundCount(){
         System.out.println("시도할 회수를 입력하세요");
-        return parseInt(Console.readLine());
+        try{return parseInt(Console.readLine());}
+        catch(IllegalArgumentException e){
+            e.printStackTrace(); //예외정보 출력 (호출스택(Call Stack)에 있었던 메서드의 정보와 예외 메세지를 콘솔화면에 출력)
+            throw e;
+        }
+
     }
+    public int playRound(){
+
+    }
+   private List<Integer> getRanNUM(){
+        List<Integer> ranNumList;
+        ranNumList= new ArrayList<Integer>();
+        for(int i=0; i<carNames.size(); i++){
+            ranNumList.add(Randoms.pickNumberInRange(0,9));
+        }
+        return ranNumList;
+    }
+
 }
