e8377fc6ac775819ab9086a5dad54d71bbe4926a
hyungyeom <rlagusrua3687@gmail.com>
2023-10-28 14:54:38 +0900

    feat : CarList 일급 컬렉션 생성

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Cars {
+    CarList carList = new CarList();
+
+    public void addCar(String carNames){
+        for (String carName : carNames.split(",")){
+            carList.add(carName);
+        }
+    }
+}
