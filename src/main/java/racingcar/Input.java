2bd7305a9c517e2f1c6ccd975de0fd90809b3788
JW0321 <digneotn123@gmail.com>
2023-10-29 01:31:09 +0900

    Feat : 게임 횟수 입력 기능 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,5 +5,8 @@ public class Input {
         String[] names = name.split(",");
         Car cars = new Car(names);
     }
+    int inputNumber(String number) {
+        return Integer.parseInt(number);
+    }
 
 }
