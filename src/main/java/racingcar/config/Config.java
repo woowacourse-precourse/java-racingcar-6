1ec2608d0c92d9c4d5df3aca4c55eb58c6bd32c2
King's meow <mingee641@gmail.com>
2023-10-28 22:01:53 +0900

    feat: 랜덤 생성을 위한 상수값 선언

---
+++ src/main/java/racingcar/config/Config.java
@@ -0,0 +1,20 @@
+package racingcar.config;
+
+public class Config {
+  private static int MIN_VALUE = 0;
+  private static int MAX_VALUE = 9;
+  private static int SET_VALUE = 4;
+
+  public int getMinValue() {
+    return MIN_VALUE;
+  }
+
+  public int getMaxValue() {
+    return MAX_VALUE;
+  }
+
+  public int getSetValue() {
+    return SET_VALUE;
+  }
+
+}
