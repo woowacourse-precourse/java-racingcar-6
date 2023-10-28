1213adc938bb6ed04f5a4990149d6f342b4a7484
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 21:52:24 +0900

    refactor: 자동차의 전진 여부 판단 메소드 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,13 +1,26 @@
 package model;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Car {
     private String name;
     private int distance = 0;
+    private final int MIN_NUM = 0;
+    private final int MAX_NUM = 9;
 
     Car() {
     }
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move(){
+        if(canGoFoward()){
+            this.distance += 1;
+        }
+    }
+
+    private boolean canGoFoward(){
+        return pickNumberInRange(MIN_NUM,MAX_NUM) >= 4;
     }
 }
