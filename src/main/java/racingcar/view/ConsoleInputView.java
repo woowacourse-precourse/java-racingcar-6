7ab08c57ad418d47880bedbe9206beff0cff6e37
eora21 <eora21@naver.com>
2023-10-28 17:46:47 +0900

    feat: 🎸 추가 콘솔을 통해 입력받은 이름들을 구분자 기준으로 나눠 반환하는 메서드 작성

---
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+
+public class ConsoleInputView implements InputView {
+    public static final String DELIMITER_DESCRIPTION = "쉼표";
+    public static final String DELIMITER = ",";
+
+    @Override
+    public List<String> enterCarNames() {
+        return List.of(
+                readLine().split(getDelimiter())
+        );
+    }
+
+    @Override
+    public String getDelimiterDescription() {
+        return DELIMITER_DESCRIPTION;
+    }
+
+    @Override
+    public String getDelimiter() {
+        return DELIMITER;
+    }
+}
