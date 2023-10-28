56c2a2877b7acbd33abb9e7a2ecd1efdf51c7af8
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 17:29:23 +0900

    refactor : ','를 기준으로 이름 입력받기

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,12 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Input {
 
 
-    public static String inputCarName(){
-        return Console.readLine();
+    public static List<String> inputCarName(){
+        String userInput = Console.readLine();
+        List<String> cars = new ArrayList<>();
+
+        for (String car : userInput.split(",")) {
+            cars.add(car);
+        }
+
+        return cars;
     }
 }
