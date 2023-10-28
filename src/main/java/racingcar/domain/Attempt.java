4761aa69eaff5585c8e1741b5965f60d96381b60
Annjieun <dkslwdms2121@naver.com>
2023-10-28 20:02:59 +0900

    feat(Attempt): 시도 횟수를 입력받아 저장하는 startMakeAttemptNumber() 구현

--- src/main/java/racingcar/domain/Attempt.java
+++ src/main/java/racingcar/domain/Attempt.java
@@ -1,7 +1,21 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.InputView;
+
 public class Attempt {
     private int count;
+
+    public void startMakeAttemptNumber() {
+        InputView.printRequestAttempt();
+        String attemptInput = Console.readLine();
+        input(attemptInput);
+        InputView.printLineChanging();
+    }
+
+    public void input(String attemptInput) {
+        validateAttemptNumber(attemptInput);
+    }
 
     public int getCount() {
         return count;
