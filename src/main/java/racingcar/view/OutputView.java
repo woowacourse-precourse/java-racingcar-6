a3ab43a82aea18c2d19eeaf3dbcdde85e5a08377
KangGeonyoung <kky6335@gmail.com>
2023-10-29 01:25:46 +0900

    feat(OutputView): 게임 진행 문구 출력 기능 - printBeforeResult(): "실행 결과" 출력 - printLine(): 빈 한줄 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+public class OutputView {
+
+    public static void printBeforeResult() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public static void printLine() {
+        System.out.println();
+    }
+}
