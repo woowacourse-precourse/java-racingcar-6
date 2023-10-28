cc33a0f9ea1fba67b16bbd7b7bcaac18b0a72aa1
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-10-28 17:49:02 +0900

    Style: " : " 상수 처리
    " : " 를 MESSAGE_SEPARATOR 변수를 통해 상수 처리

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -5,7 +5,8 @@ import racingcar.Model.CarList;
 public class OutputView {
 	final static String GRAPH_UNIT = "-";
 	final static String RESULT_MESSAGE = "실행 결과";
-	final static String WINNER_MESSAGE = "최종 우승자 : ";
+	final static String WINNER_MESSAGE = "최종 우승자";
+	final static String MESSAGE_SEPARATOR = " : ";
 
 	public static void printResult() {
 		System.out.println(RESULT_MESSAGE);
@@ -14,16 +15,14 @@ public class OutputView {
 	public static void printEachRound(CarList carList) {
 
 		for (int i = 0; i < carList.size(); i++) {
-
-			System.out.println(carList.getEachCar(i) + " : " + carList.getEachCar(i).makeDistanceGraph(GRAPH_UNIT));
-
+			System.out.println(carList.getEachCar(i) + MESSAGE_SEPARATOR + carList.getEachCar(i).makeDistanceGraph(GRAPH_UNIT));
 		}
 
 		System.out.println();
 	}
 
 	public static void printWinner(String winnerList) {
-		System.out.println(WINNER_MESSAGE + winnerList);
+		System.out.println(WINNER_MESSAGE + MESSAGE_SEPARATOR + winnerList);
 	}
 
 }
