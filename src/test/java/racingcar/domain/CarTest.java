beb9be4209de900f9c281ca75de2b5df18eaa578
unknown <y2kf9a0q@gmail.com>
2023-10-29 00:05:12 +0900

    docs: README.md - test requirements

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,8 +3,6 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CarTest {
 
     @BeforeEach
