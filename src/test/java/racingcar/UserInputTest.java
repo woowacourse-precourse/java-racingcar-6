488ddc3c5f359b6fcc09ea30ea0608691ebc5d14
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-10-28 20:47:04 +0900

    test: 추가 UserInputTest.java
    사용자 입력 처리 테스트를 담당할 테스트 코드 파일을 추가함.

---
+++ src/test/java/racingcar/UserInputTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserInputTest extends NsTest {
+    @Test
+    void 입력된_회수가_숫자가_맞나요(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,lkh,be", "abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
