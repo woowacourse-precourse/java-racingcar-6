73bbb6610b580a8caf70908b1c3602837f1d3169
junest66 <chlwndks333@gmail.com>
2023-10-29 06:26:03 +0900

    feat(OutputView): 출력 메세지 상수 추가
    - 게임 시작, 이동횟수, 결과, 우승자 메시지 및 카운트 심볼 상수 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_MESSAGE = "시도할 회수는 몇 회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String COUNT_SYMBOL = "-";
+}
