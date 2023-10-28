de5906de0f3586d58e3d46f1371678f3b62d4ce0
doha <dohaahi@naver.com>
2023-10-28 22:14:49 +0900

    fix: 랜덤 숫자 뽑을 때 사용하는 END_INCLUSIVE 상수 수정

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String GAME_RESULT_MESSAGE = "실행 결과";
     public static final String GAME_WINNER_MESSAGE = "최종 우승자 :";
     public static final int START_INCLUSIVE = 0;
-    public static final int END_INCLUSIVE = 0;
+    public static final int END_INCLUSIVE = 9;
 
     public static final String WRONG_INPUT_CAR_NAME_CHARACTER_MASSAGE = "자동차 이름은 문자로 입력해주세요.";
     public static final String WRONG_INPUT_CAR_NAME_LENGTH_MESSAGE = "자동차 이름은 5자 이하로 입력해주세요,";
