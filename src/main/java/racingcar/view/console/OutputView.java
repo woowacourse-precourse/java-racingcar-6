2dbcf50733c1d6ed037e7626ce95f02fc1729dad
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-28 14:26:55 +0900

    feat: OutputView printResultMessage, printRaceProgress, println, printRaceWinners 기본 코드 작성

--- src/main/java/racingcar/view/console/OutputView.java
+++ src/main/java/racingcar/view/console/OutputView.java
@@ -1,4 +1,24 @@
 package racingcar.view.console;
 
+import java.util.List;
+
 public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNERS_MESSAGE = "최종 우승자 : ";
+
+    public void printResultMessage() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printRaceProgress() {
+    }
+
+    public void println() {
+        System.out.println();
+    }
+
+    public void printRaceWinners(List<String> raceWinners) {
+        System.out.println(WINNERS_MESSAGE + String.join(", ", raceWinners));
+    }
 }
