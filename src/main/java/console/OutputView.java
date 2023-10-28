51629504b12f9678738a87bb57714dbd922110ba
DK <laancer4@gmail.com>
2023-10-28 19:39:15 +0900

    refactor : 우승자 출력을 위한 로직 변경(cars 객체에게 상태를 물어보는 방법)

--- src/main/java/console/OutputView.java
+++ src/main/java/console/OutputView.java
@@ -7,10 +7,19 @@ import java.util.List;
 
 public class OutputView {
     private static final String ROUND_RESULT = "실행 결과";
-    private static final String FINAL_WINNER = "최종 우승자: ";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
     private static final String PROGRESS_PREFIX = " : ";
     private static final String SPLIT_DELIMITER = ", ";
     private static final String LOCATION_COUNTER = "-";
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRIALS = "시도할 회수는 몇회인가요?";
+    public void printGameStart(){
+        System.out.println(INPUT_CAR_NAMES);
+    }
+
+    public void printTrials(){
+        System.out.println(INPUT_TRIALS);
+    }
 
     public void printRoundResult(Cars cars) {
         System.out.println(ROUND_RESULT);
@@ -28,17 +37,10 @@ public class OutputView {
         System.out.println(progress);
     }
 
-    public void printWinners(List<Car> winners) {
-        System.out.print(FINAL_WINNER);
-        for (int i = 0; i < winners.size(); i++) {
-            System.out.print(winners.get(i).getName());
-            printDelimiterIfNotLast(winners, i);
-        }
+    public void printWinners(Cars cars){
+        List<String> winners = cars.getWinnersNames();
+        String winnerName = FINAL_WINNER + String.join(SPLIT_DELIMITER, winners);
+        System.out.println(winnerName);
     }
 
-    private void printDelimiterIfNotLast(List<Car> winners, int i) {
-        if (i < winners.size() - 1) {
-            System.out.print(SPLIT_DELIMITER);
-        }
-    }
 }
