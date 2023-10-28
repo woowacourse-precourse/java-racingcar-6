a45d64c70be60136cb9c0650b641f1f704621c21
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-28 23:53:02 +0900

    refactor: 상수 이름 변경

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -6,10 +6,10 @@ public class InputValidator {
 
     private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
 
-    private static final String INVALID_TRY_NUMBER_MESSAGE = "시도할 횟수는 숫자만 입력 가능합니다.";
+    private static final String INVALID_TRY_COUNT_MESSAGE = "시도할 횟수는 숫자만 입력 가능합니다.";
     public void validateTryCount(String tryCount) {
         if (isMatchNumber(tryCount)) {
-            throw new IllegalArgumentException(INVALID_TRY_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE);
         }
     }
 
