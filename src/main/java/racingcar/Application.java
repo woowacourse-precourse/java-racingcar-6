91ca8e756f9dea222f01c40c47ffae22d698bf6b
DohunHyun <dohun.hyun@gmail.com>
2023-10-28 15:07:30 +0900

    Feat : 결과값 출력
    경기 후 우승자 출력 부분 구현.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,11 +7,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.Car;
 
 public class Application {
+    static ArrayList<Car> carList;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] input = Console.readLine().split(",");
-        ArrayList<Car> carList = new ArrayList<>();
+        carList = new ArrayList<>();
         for(String name : input) {
             if(name.length() > 5) {
                 throw new IllegalArgumentException("이름은 5자 이내로 작성해주세요.");
@@ -36,6 +37,42 @@ public class Application {
                 System.out.println();
             }
         }
+
+        // 경기 결과 출력
+        printResult();
+    }
+
+    static void printWinner(int maxDistance) {
+        ArrayList<Car> winnerList = new ArrayList<>();
+        for(Car car : carList) {
+            if(car.getCount() == maxDistance) {
+                winnerList.add(car);
+            }
+        }
+
+        String result = null;
+        for(Car winner : winnerList) {
+            result = String.join(", ", winner.getName());
+        }
+        System.out.println(result);
+    }
+
+    static int getMaxDistance() {
+        int maxDistance = 0;
+        for(Car car : carList) {
+            if(car.getCount() > maxDistance) {
+                maxDistance = car.getCount();
+            }
+        }
+        return maxDistance;
+    }
+
+    static void printResult() {
+        // 최대거리 구하기
+        int maxDistance = getMaxDistance();
+
+        // 출력하기
+        printWinner(maxDistance);
     }
 
     static void randomDrive(ArrayList<Car> carList) {
