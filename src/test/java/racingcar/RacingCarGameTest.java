47b72dd4af743941d4f89c336930a91c85e0bbd3
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-28 17:29:36 +0900

    test: 게임 라운드 수 입력값 검증 기능

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.RacingCarGame;
+import racingcar.view.InputView;
+import java.io.ByteArrayInputStream;
 import java.util.stream.Stream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -23,5 +27,19 @@ public class RacingCarGameTest {
                 Arguments.of((Object) new String[] {"jy", "", "wb"}),
                 Arguments.of((Object) new String[] {"jy", " ", "wb"})
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"+4", "ss", "-1", "0"})
+    void 라운드_수_입력값_검증_테스트(String input) {
+        generateInput(input);
+        assertThatThrownBy(InputView::inputRoundNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+
+        Console.close();
+    }
+
+    private void generateInput(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
     }
 }
