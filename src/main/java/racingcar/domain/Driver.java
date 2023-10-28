3240c01b349e5e2aec5917738c26d66c857458bc
liarreez <liarreez@gmail.com>
2023-10-28 16:55:27 +0900

    refactor : 랜덤 숫자의 최솟값과 최댓값 변수에 static 추가

--- src/main/java/racingcar/domain/Driver.java
+++ src/main/java/racingcar/domain/Driver.java
@@ -3,8 +3,8 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Driver {
-    private final int RANDOM_NUMBER_MIN_VALUE = 0;
-    private final int RANDOM_NUMBER_MAX_VALUE = 9;
+    private static final int RANDOM_NUMBER_MIN_VALUE = 0;
+    private static final int RANDOM_NUMBER_MAX_VALUE = 9;
 
     public void drive(Car car) {
         car.move(calculateDrivingSkill());
