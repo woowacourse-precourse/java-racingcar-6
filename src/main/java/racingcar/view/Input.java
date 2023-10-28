3d257c0ec3567e263ac744ed5a83518908f2dcb3
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-28 16:51:54 +0900

    feat : 사용자로부터 자동차 입력 받기 추가

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+
+
+    public static String inputCarName(){
+        return Console.readLine();
+    }
+}
