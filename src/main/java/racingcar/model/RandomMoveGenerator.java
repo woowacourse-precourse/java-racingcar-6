fb668568df0a0554d31752a3aceb14e37e6b62ce
hansg931 <hansg0509@cau.ac.kr>
2023-10-28 22:09:22 +0900

    feat: 전진을 판단하는 무작위 값 생성 메소드 작성

--- src/main/java/racingcar/model/RandomMoveGenerator.java
+++ src/main/java/racingcar/model/RandomMoveGenerator.java
@@ -1,4 +1,10 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.util.Constant;
+
 public class RandomMoveGenerator {
+	public static boolean isMovable() {
+		return Randoms.pickNumberInRange(Constant.RANDOM_NUMBER_MIN, Constant.RANDOM_NUMBER_MAX) >= Constant.RANDOM_NUMBER_THRESHOLD;
+	}
 }
