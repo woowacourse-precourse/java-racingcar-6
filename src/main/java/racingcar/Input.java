2ea7f64fa29aba8e26eac3031c80c0c9bc5c394b
JW0321 <digneotn123@gmail.com>
2023-10-29 06:40:43 +0900

    Feat : 예외 발생 구현
    지정된 형식의 예외가 발생하도록 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,12 +1,35 @@
 package racingcar;
 
 public class Input {
-    void inputName(String name) {
+    static Car makingLineup(String name) {
         String[] names = name.split(",");
-        Car cars = new Car(names);
+        validateNames(names);
+        return new Car(names);
     }
-    int inputNumber(String number) {
-        return Integer.parseInt(number);
+    static void validateNames(String[] names) {
+        for (String name : names) {
+            checkNameLengthLimit(name);
+        }
+    }
+    static void checkNameLengthLimit(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("입력되는 이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    static int inputNumber(String number) throws IllegalArgumentException{
+        try {
+            int repeatNumber = Integer.parseInt(number);
+            validateNumberAtLeast(repeatNumber);
+            return repeatNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1 이상의 숫자만 입력해야 합니다.");
+        }
+    }
+    static void validateNumberAtLeast(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("1 이상의 숫자만 입력해야 합니다.");
+        }
     }
 
 }
