78e9ec935734f1b887543c54743433cb9ce8e3ac
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 21:16:17 +0900

    refactor: 자동차 이름을 입력 받는 기능 추가

--- src/main/java/controller/CarRace.java
+++ src/main/java/controller/CarRace.java
@@ -1,19 +1,36 @@
 package controller;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import view.InputView;
-
+import model.Car;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import validator.InputValidator;
 
 public class CarRace {
+    private List<Car> cars = new ArrayList<Car>();
+    private InputValidator inputValidator = new InputValidator();
+
     private String getCarNameByInput() {
-        InputView inputView = new InputView();
-        String carName = inputView.printUserInputCarNameMessage();
-        return carName;
+        InputView.printUserInputCarNameMessage();
+        String input = readLine();
+        return checkCarName(input);
+    }
+
+    private String checkCarName(String input) {
+        try {
+            inputValidator.checkCarNameValidate(input);
+            return input;
+        } catch (IllegalArgumentException e){
+            throw e;
+        }
     }
 
     private List<String> parseCarName(String input) {
         List<String> carName = Arrays.asList(input);
         return carName;
     }
+
+
 }
