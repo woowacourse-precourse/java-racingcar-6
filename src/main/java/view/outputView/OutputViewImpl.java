aa26e88c35fed63cd8afe1840680d053e093a8d2
hongseokhodev <hongseokhodev@gmail.com>
2023-10-28 16:59:01 +0900

    feat: 자동차 이름을 입력하라는 메시지를 출력한다

--- src/main/java/view/outputView/OutputViewImpl.java
+++ src/main/java/view/outputView/OutputViewImpl.java
@@ -3,6 +3,7 @@ package view.outputView;
 import java.util.List;
 
 public class OutputViewImpl {
+    private static final String INSERT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
     private static final String INSERT_ATTEMPT_MESSAGE = "시도할 회수는 몇회인가요?";
     public void printWinners(List<String> winners) {
         System.out.println(String.join(", ", winners));
@@ -10,6 +11,10 @@ public class OutputViewImpl {
 
     public void printResults(List<String> results) {
         results.forEach(System.out::println);
+    }
+
+    public void printInsertCarNamesMessage() {
+        System.out.println(INSERT_CAR_NAMES_MESSAGE);
     }
 
     public void printInsertAttemptMessage() {
