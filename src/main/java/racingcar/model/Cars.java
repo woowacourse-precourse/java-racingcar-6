285bc4db1d3ccc9a1948ee5a727e0c6760d4ef4a
pwrwpw <dlgustjr9912@gmail.com>
2023-10-29 01:51:00 +0900

    feat(Cars): Car의 객체를 List로 저장하는 Cars 클래스 생성

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+}
