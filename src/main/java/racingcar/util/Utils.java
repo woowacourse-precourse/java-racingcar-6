b74101282e359c70d80d84c3c2f83f2dd04187d8
elyudwo <dudqk9696@naver.com>
2023-10-28 20:37:03 +0900

    [Feat] : 랜덤 숫자 발급 기능 구현

---
+++ src/main/java/racingcar/util/Utils.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Utils {
+
+    public static int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
