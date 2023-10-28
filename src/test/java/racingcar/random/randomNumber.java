aeca17b3e6ce1a103a5ce2673668e119e8ae0514
hYe807n <rlagkdus55@gmail.com>
2023-10-28 20:29:52 +0900

    Feat: 0 ~ 9 사이의 무작위 정수 생성 기능

---
+++ src/test/java/racingcar/random/randomNumber.java
@@ -0,0 +1,11 @@
+package racingcar.random;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class randomNumber {
+
+    public static int randomNumberGenerator() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
