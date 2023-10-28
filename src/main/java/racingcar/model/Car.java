1cf08bc3b83feb1346d4545b4d4ceae45ca4e5ef
WooYoungDoo <duden5000@naver.com>
2023-10-29 00:56:15 +0900

    feat (Car) : 자동차 객체

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Car {
+
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
