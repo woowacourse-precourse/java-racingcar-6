88b3a1d50a4b7f1e5ae430e28c69cb39f0b11810
c1ce <chalicenuclear@gmail.com>
2023-10-28 17:12:24 +0900

    feat : 게임진행 출력과 최종우승자 출력코드 작성

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,4 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Output {
+    private ArrayList<String> goalPlayer;
+    public void printRound(ArrayList<String> player,ArrayList<Integer> playerResult){
+        for (int i = 0; i < player.size(); i++) {
+            System.out.println(player.get(i)+" : "+"-".repeat(playerResult.get(i)));
+        }
+    }
+    public void goal(ArrayList<String> goalPlayer) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < goalPlayer.size(); i++) {
+            System.out.print(goalPlayer.get(i)+", ");
+        }
+    }
 }
