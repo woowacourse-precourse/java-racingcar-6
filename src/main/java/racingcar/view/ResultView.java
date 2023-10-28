5ae8b37e81d8759cedf8332667fd186180b1e3b2
hyerimmy <heyrim2010@naver.com>
2023-10-29 03:33:15 +0900

    feat: View 출력문 함수 작성

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
 public class ResultView {
+    private static final String RESULT = "실행 결과";
+    private static final String WINNER = "최종 우승자";
+    private static final String DELIMITER = " : ";
+    private static final String DASH = "-";
+    private static final String COMMAS = ", ";
+
+    public void printResultTitle(){
+        System.out.println(RESULT);
+    }
+
+    public void printCarState(String name, int position){
+        System.out.println(name+DELIMITER+DASH.repeat(position));
+    }
+
+    public void printWinner(String[] names){
+        System.out.print(WINNER+DELIMITER+String.join(COMMAS, names));
+    }
 }
--- src/main/java/racingcar/view/SettingView.java
+++ src/main/java/racingcar/view/SettingView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
 public class SettingView {
+    private static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND_NUMBER_MSG = "시도할 회수는 몇회인가요?";
+
+    public void printInputCarNameMsg(){
+        System.out.println(INPUT_CAR_NAME_MSG);
+    }
+
+    public void printInputRoundNumberMsg(){
+        System.out.println(INPUT_ROUND_NUMBER_MSG);
+    }
 }
