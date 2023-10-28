c1f26c3b4bf7969fa640baf6a47f538036a4eb89
chosunghyun18 <krismon@naver.com>
2023-10-28 14:47:52 +0900

    test : 두개의 차량의 위치를 비교한다.

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -28,7 +28,6 @@ class CarTest {
     @Test
     @DisplayName("stop by stop position")
     public void carSamePostionTest(){
-        car = new Car();
         int initPostion = 0 ;
         int targetNumber = initPostion+0 ;
         assertThat(car.moveCarByState(MovementState.STOP)).isEqualTo(targetNumber);
@@ -36,10 +35,49 @@ class CarTest {
     @Test
     @DisplayName("move by move position")
     public void carPostionPlusTest(){
-        car = new Car();
         int initPostion = 0 ;
         int targetNumber = initPostion+1 ;
         assertThat(car.moveCarByState(MovementState.MOVE)).isEqualTo(targetNumber);
     }
+    @Test
+    @DisplayName("compare in when both is same Position")
+    public void carSamePositionTest(){
+        Car other = new Car();
+        assertThat(car.isFront(other)).isFalse();
+        assertThat(car.isSamePosition(other)).isTrue();
+    }
 
+    @Test
+    @DisplayName("compare one is over other  Position")
+    public void carDiffPostionPlusTest(){
+        Car other = new Car();
+        other.moveCarByState(MovementState.MOVE);
+        assertThat(other.isFront(car)).isTrue();
+    }
+    @Test
+    @DisplayName("compare two Cars  Position")
+    public void carDiffPostionCompareTest() {
+
+        Car other = new Car();
+        other.moveCarByState(MovementState.MOVE);
+        other.moveCarByState(MovementState.MOVE);
+
+        assertThat(other.comparePosition(car)).isEqualTo(ComparePositionState.FRONT);
+    }
+    @Test
+    @DisplayName("compare two Cars  PositionSame")
+    public void carDiffPostionSameCompareTest() {
+
+        Car other = new Car();
+        assertThat(other.comparePosition(car)).isEqualTo(ComparePositionState.SAME);
+    }
+
+    @Test
+    @DisplayName("compare two Cars  Position")
+    public void carDiffPositionCompareWhenBackTest() {
+        Car other = new Car();
+        other.moveCarByState(MovementState.MOVE);
+        other.moveCarByState(MovementState.MOVE);
+        assertThat(car.comparePosition(other)).isEqualTo(ComparePositionState.BACK);
+    }
 }
