7a9e69229a237106ddb4c637764a4dbb3a6b64ad
qncj5224 <qncj5224@naver.com>
2023-10-28 20:57:49 +0900

    feat: 자동차 이름 개수 테스트 구현

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -4,10 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 
+import org.junit.jupiter.api.Assertions;
 import racingcar.validator.CarNameValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputTest {
@@ -21,4 +24,14 @@ public class InputTest {
       CarNameValidator.isNameLengthValid(input);
     });
   }
+
+  @DisplayName("자동자 이름 개수 테스트")
+  @Test
+  void 자동차_이름_개수_테스트() {
+    final List<String> input = Collections.singletonList("a");
+
+    assertThatThrownBy(() -> CarNameValidator.isNumberOfCarsValid(input))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
 }
