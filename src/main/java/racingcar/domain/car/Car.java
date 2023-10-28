c2f8d25f1b277d8105c6839d1233b6ff58b1a45c
penrose15 <seongju99@naver.com>
2023-10-28 15:10:51 +0900

    Feat: Car 도메인 생성
    이름, 움직이는 횟수 저장

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,31 @@
+package racingcar.domain.car;
+
+public class Car {
+    private final String name;
+    private int moveCount;
+    private RandomUtil randomUtil;
+
+    public Car(String name, RandomUtil randomUtil) {
+        this.name = name;
+        this.moveCount = 0;
+        this.randomUtil = randomUtil;
+    }
+
+    public void moveCar() {
+        if(canMoveCar()) {
+            this.moveCount += 1;
+        }
+    }
+
+    public boolean canMoveCar() {
+        return randomUtil.moveForward();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
---
+++ src/main/java/racingcar/domain/car/RandomMove.java
@@ -0,0 +1,11 @@
+package racingcar.domain.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMove implements RandomUtil{
+    @Override
+    public boolean moveForward() {
+        int number = Randoms.pickNumberInRange(0, 9);
+        return number >= 4;
+    }
+}
