6586e7279692f2b62a2b784daae1cf9dcb473e68
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-28 16:45:57 +0900

    refactor(OutputView) : 실행 결과 출력 기능 수정 및 메소드 분리 printTotalResult() 추가, printProgressOfCar()에서 convertProgressToString() 메소드 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,21 +1,26 @@
 package racingcar.view;
 
 import static racingcar.constant.AllPunctuationMarks.*;
+import static racingcar.constant.RaceIOMessage.RACE_RESULT;
 import static racingcar.constant.RaceIOMessage.RACE_WINNER;
 
 import java.util.List;
 
 public class OutputView {
-    public void printRoundResult(String carName, int moves) {
-        System.out.println(makeResultToString(carName, moves));
+    public void printTotalResult() {
+        System.out.println(RACE_RESULT);
     }
-    private String makeResultToString(String carName, int moves) {
+    public void printProgressOfCar(String carName, int moves) {
+        System.out.println(convertProgressToString(carName, moves));
+    }
+    private String convertProgressToString(String carName, int moves) {
         return carName + COLONS + PROGRESS.repeat(Math.max(0, moves));
     }
     public void printWinnerList(List<String> winnerName) {
         StringBuilder winner = new StringBuilder(RACE_WINNER + winnerName.get(0));
         for (int i = 1; i < winnerName.size(); i++) {
-            winner.append(COMMA+BLANK).append(winnerName.get(i));
+            winner.append(COMMA+BLANK);
+            winner.append(winnerName.get(i));
         }
         System.out.println(winner);
     }
