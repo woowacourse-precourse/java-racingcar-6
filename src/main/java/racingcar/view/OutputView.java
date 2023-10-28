a0462409c3300b10db1cc5fea101b87fb4da5cce
JH-TT <leejongho9803@gmail.com>
2023-10-29 02:20:58 +0900

    style - 변수에 static 추가
    - 상수를 static final로 선언하면
        - 변경이 불가능하다
        - 메모리를 효율적으로 사용하게된다
    
    따라서 상수는 static final로 선언하는것이 좋다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,13 @@
 package racingcar.view;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class OutputView {
-    private final String INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String INPUT_TIMES = "시도할 회수는 몇회인가요?";
-    private final String PRINT_RESULT = "실행 결과";
-    private final String PRINT_WINNER = "최종 우승자 : ";
-    private final String DELIMITER = ", ";
+    private static final String INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TIMES = "시도할 회수는 몇회인가요?";
+    private static final String PRINT_RESULT = "실행 결과";
+    private static final String PRINT_WINNER = "최종 우승자 : ";
+    private static final String DELIMITER = ", ";
 
     public void inputNames() {
         System.out.println(INPUT_NAME);
