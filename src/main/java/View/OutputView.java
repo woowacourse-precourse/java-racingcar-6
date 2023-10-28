e5a9c90536df3238ef80f417882bbd4639077bbc
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-28 17:40:23 +0900

    refactor : 경주 결과에 대한 내용 출력 기능 추가

---
+++ src/main/java/View/OutputView.java
@@ -0,0 +1,10 @@
+package View;
+
+public class OutputView {
+    private static final String resultMessage = "실행 결과";
+
+    public static void printResultMessage(){
+        System.out.println(resultMessage);
+    }
+
+}
