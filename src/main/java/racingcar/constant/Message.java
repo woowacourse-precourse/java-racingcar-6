c2cca37a35dfc652061d2547c1db2da240dafcd4
Laniel Park <Laniel8177@gmail.com>
2023-10-28 18:21:11 +0900

    feat: Constant.Message Class

---
+++ src/main/java/racingcar/constant/Message.java
@@ -0,0 +1,15 @@
+package racingcar.constant;
+
+public class Message {
+    private Message() {
+    }
+
+    public static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
+    public static final String ENTER_TRY_COUNT = "시도할 회수는 몇회인가요?";
+
+    public static final String CAR_SEPARATOR = ",";
+    public static final String KEY_VALUE_SEPARATOR = " : ";
+
+    public static final String RACE_RESULT_HEADER = "실행 결과";
+    public static final String RACE_WINNER = "최종 우승자" + KEY_VALUE_SEPARATOR;
+}
