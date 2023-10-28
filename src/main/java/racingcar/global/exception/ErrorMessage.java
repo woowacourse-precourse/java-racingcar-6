390bbffd3127dd23d2ec00d7b209ccbad9fbb3c4
Minjae-An <mj3242@naver.com>
2023-10-28 15:51:46 +0900

    feat : 숫자 입력 에러 메시지 추가

--- src/main/java/racingcar/global/exception/ErrorMessage.java
+++ src/main/java/racingcar/global/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.global.exception;
 public enum ErrorMessage {
     EMPTY_VALUE("빈 값을 입력할 수 없습니다."),
     INVALID_INPUT_VALUE("잘못된 입력 형식입니다."),
-    INVALID_LENGTH("차 이름은 5자를 초과할 수 없습니다.");
+    INVALID_LENGTH("차 이름은 5자를 초과할 수 없습니다."),
+    NOT_A_NUMBER("단일 숫자를 입력해야 합니다.");
 
     private final String message;
 
