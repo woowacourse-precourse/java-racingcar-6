b6abc0e80279d7eeb74296183f99ff95415055c7
CJY <c0512jy@naver.com>
2023-10-29 01:12:48 +0900

    feat: 자동차 경주 도메인
    - 경주 자동차 목록을 가지고 있음
      - 경주 진행 -Race#process
      - 게임을 진행한 후 우승자들을 반환 -Race#winners()

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,4 +1,42 @@
 package racingcar.domain;
 
+import racingcar.generator.NumberGenerator;
+
+import java.util.List;
+
 public class Race {
+
+    private final List<Car> cars;
+    private final NumberGenerator numberGenerator;
+
+    public Race(List<String> strings, NumberGenerator numberGenerator) {
+        this.cars = strings.stream()
+                .map(Car::of)
+                .toList();
+        this.numberGenerator = numberGenerator;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void process() {
+        cars.forEach(car -> car.race(numberGenerator.generate()));
+    }
+
+    public List<String> winners() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("최대 위치를 찾을 수 없습니다."));
+    }
+
 }
