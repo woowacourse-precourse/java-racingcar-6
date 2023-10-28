39306457267d9899b85768f77a0a2c79cd5f457a
sami355-24 <ddung355@gmail.com>
2023-10-28 19:22:09 +0900

    feat(Exception): 게임에서 발생할 수 있는 예외를 이너 클래스들로 작성해 관리
    - CarNameException: 자동차 이름과 관련한 예외를 의미
    - RoundException: 시도 횟수와 관련한 예외를 의미

---
+++ src/main/java/Exception/GameException.java
@@ -0,0 +1,16 @@
+package Exception;
+
+public class GameException extends RuntimeException {
+
+    public static class CarNameException extends RuntimeException {
+        public CarNameException(String message) {
+            super(message);
+        }
+    }
+
+    public static class RoundException extends RuntimeException {
+        public RoundException(String message) {
+            super(message);
+        }
+    }
+}
