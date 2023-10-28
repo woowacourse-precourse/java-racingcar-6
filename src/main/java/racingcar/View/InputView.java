44019300c45a1e9338577d61bf5dbc8e06520848
sami355-24 <ddung355@gmail.com>
2023-10-28 21:44:33 +0900

    feat(View): 입력을 담당하는 InputView 클래스 생성
    - TDD를 통한 개발을 진행하기 위해 로직이 담기지 않은 메서드만 추가

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.View;
+
+public class InputView {
+    public String inputCarName() {
+        return null;
+    }
+
+    public Integer inputNumberOfRound() {
+        return null;
+    }
+}
