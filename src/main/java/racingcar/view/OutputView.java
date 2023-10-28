009ebd6bb7071f9faae4f1470229cc025b8f711b
jinsu868 <mjinsu098@gmail.com>
2023-10-28 20:51:34 +0900

    refactor(view): 출력 메시지 상수로 지정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,17 +4,26 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class OutputView {
+    private final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String RESULT_MESSAGE = "실행 결과";
+    private final String WINNER_START_NOTIFICATION_MESSAGE = "최종 우승자 : ";
+    private final String MOVE_NOTIFICATION = "-";
+
     public void printCarInfo(Car car) {
-        System.out.print(car.getName() + " : ");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(car.getName() + " : ");
         for (int i = 0; i < car.getPosition(); i++) {
-            System.out.print("-");
+            stringBuilder.append(MOVE_NOTIFICATION);
         }
-        System.out.println();
+
+        stringBuilder.append("\n");
+        System.out.println(stringBuilder.toString());
     }
 
     public void printWinner(List<Car> cars) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("최종 우승자 : ");
+        stringBuilder.append(WINNER_START_NOTIFICATION_MESSAGE);
         for (int i = 0; i < cars.size(); i++) {
             if (i == cars.size() - 1) {
                 stringBuilder.append(cars.get(i).getName() + "\n");
@@ -22,19 +31,20 @@ public class OutputView {
                 stringBuilder.append(cars.get(i).getName() + ", ");
             }
         }
+
         System.out.println(stringBuilder.toString());
     }
 
     public void printCarInputMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_MESSAGE);
     }
 
     public void printMoveCountInputMessage() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
     }
 
     public void printResultMessage() {
-        System.out.println("실행 결과");
+        System.out.println(RESULT_MESSAGE);
     }
 
     public void printNewLine() {
