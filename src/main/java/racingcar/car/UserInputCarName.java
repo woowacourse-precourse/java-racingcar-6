c29f9a7ffd0424c3e695cfb8316b94cb242fe3f1
혜민 <alddlv81@gmail.com>
2023-10-28 17:48:46 +0900

    refactor: 입력값 쉼표 기준으로 구분, 구분 후 List에 값 추가 기능 분리

--- src/main/java/racingcar/car/UserInputCarName.java
+++ src/main/java/racingcar/car/UserInputCarName.java
@@ -6,22 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserInputCarName {
-    public String inputName(){
+    public String inputName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    public List<CarInfo> splitByComma(String inputCarName){
-        List<CarInfo> carList = new ArrayList<>();
-        String[] cars = inputCarName.split(",");
-        for (String carName : cars) {
-            carList.add(createCar(carName));
-        }
-        System.out.println(carList);
-        return carList;
+    public String[] splitByComma(String inputCarName) {
+        return inputCarName.split(",");
     }
 
-    CarInfo createCar(String carName){
+    private CarInfo createCar(String carName) {
         return new CarInfo(carName.trim());
+    }
+
+    public List<CarInfo> addCarList(String[] cars){
+        List<CarInfo> carList = new ArrayList<>();
+        for (String carName : cars) {
+            carList.add(createCar(carName.trim()));
+        }
+        return carList;
     }
 }
