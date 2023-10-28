3490a60036c9cbe3db48d98add05fe1e63e13e20
lee jihyeon <lejinhy@gmail.com>
2023-10-28 15:10:45 +0900

    feat: MagicNumber enum 추가 랜덤값 최소&최대, 이동 가능한 최소 랜덤값,자동차 이름 최대길이
    - feat (feature) : 새로운 기능을 추가한 경우
    - fix (bug fix) : 버그를 고친 경우
    - docs (documentation) : 문서를 수정한 경우
    - design : CSS 등 사용자 UI 디자인 변경
    - style (formatting, missing semi colons, …) : 코드 포맷 변경, 세미 콜론 누락 등 기능적 변경이 없는 경우
    - refactor : 코드 리팩토링을 한 경우
    - comment: 필요한 주석 추가 및 변경
    - test (when adding missing tests) : 테스트 추가, 테스트 리팩토링을 한 경우
    - chore (maintain) : 빌드, 패키지 매니저 설정을 변경한 경우
    - rename : 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    - remove : 파일을 삭제하는 작업만 수행한 경우

---
+++ src/main/java/racingcar/MagicNumber.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public enum MagicNumber {
+    RANDOM_MIN(0),
+    RANDOM_MAX(9),
+    CAM_MOVE_MIN(4),
+    CAR_NAME_LENGTH_MAX(5);
+
+    private int magicNumber;
+
+    MagicNumber(int magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+}
