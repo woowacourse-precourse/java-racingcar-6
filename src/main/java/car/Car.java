e407a850884d0c618e46326b94d547b837c432ff
pjaehyun <show6756@gmail.com>
2023-10-28 14:50:53 +0900

    Car 클래스 생성(생성자, Getter)

---
+++ src/main/java/car/Car.java
@@ -0,0 +1,18 @@
+package car;
+
+public class Car {
+    public int distance;
+    public String name;
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
