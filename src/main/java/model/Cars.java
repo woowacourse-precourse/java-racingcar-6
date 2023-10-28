26ffd6614db0d7c92a70432ffb8296b11e936761
WonyuChoi <phillip07290@naver.com>
2023-10-28 20:49:34 +0900

    refactor: 매개 변수에 final 키워드 추가

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -6,11 +6,11 @@ public class Cars {
 
     private final List<Car> cars;
 
-    private Cars(List<Car> cars) {
+    private Cars(final List<Car> cars) {
         this.cars = cars;
     }
 
-    public static Cars from(List<String> values) {
+    public static Cars from(final List<String> values) {
         List<Car> cars = values.stream()
             .map(Car::from)
             .toList();
