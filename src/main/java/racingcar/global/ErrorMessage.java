b7faf94898c8c518116cc991e3ef1a6eedba5dc0
Gyeongho Yang <chchch1213@naver.com>
2023-10-29 18:21:58 +0900

    feat(global): 자동차 이름 길이 제한 에러 메세지 추가

--- src/main/java/racingcar/global/ErrorMessage.java
+++ src/main/java/racingcar/global/ErrorMessage.java
@@ -1,6 +1,9 @@
 package racingcar.global;
 
+import static racingcar.global.Constant.MAX_CAR_NAME_LENGTH;
+
 public enum ErrorMessage {
+    EXCEED_CAR_NAME_LENGTH(String.format("%d자리 이상인 자동차 이름이 존재 합니다.", MAX_CAR_NAME_LENGTH)),
     DUPLICATED_CAR_NAME("중복된 자동차 이름이 존재 합니다."),
     EMPTY_CAR_NAME("공백인 자동차 이름이 존재 합니다.");
 
