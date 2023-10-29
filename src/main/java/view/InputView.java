2ee1c6b8938ca9cf2a716db7625b96ff5758a8cb
(han songyi) <(hhan1218@naver.com)>
2023-10-29 16:56:12 +0900

    feat: create InputView Class

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,14 @@
+package view;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public String inputNameOfCar() {
+        String inputNameOfCar = readLine();
+    }
+
+    public int inputTryNumber() {
+        int inputTryNumber = Int(readLine());
+    }
+}
