dee9c64c4478e84a00c0e6e1ebe15d9524ddbf53
YongHyeon <pyh4616@naver.com>
2023-10-30 02:21:01 +0900

    Test: (RacingCarGameServiceTest 클래스), 이동이 모두 끝난 결과를 통해 우승자를 뽑는 기능을 테스트

--- src/test/java/racingcar/service/RacingCarGameServiceTest.java
+++ src/test/java/racingcar/service/RacingCarGameServiceTest.java
@@ -2,7 +2,10 @@ package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,11 +14,42 @@ public class RacingCarGameServiceTest {
     @ParameterizedTest(name = "[테스트 케이스 {index}] 랜덤 숫자 = {0}, 전진 결과 = {1}")
     @CsvSource({"3,false", "4,true", "5,true"})
     @DisplayName("4이상은 전진하고, 3이하는 정지상태를 유지한다.")
-    public void should_MovingForward_When_FourOrMore(int randomNumber, boolean expected) {
+    public void should_MoveForward_When_FourOrMore(int randomNumber, boolean expected) {
         RacingCarGameService racingCarGameService = new RacingCarGameService();
 
         boolean actual = racingCarGameService.isMovingForward(randomNumber);
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("가장 많이 이동한 자동차를 우승자로 뽑는다.")
+    void getOnlyWinner() {
+        RacingCarGameService racingCarGameService = new RacingCarGameService();
+        LinkedHashMap<String, Integer> finalResult = new LinkedHashMap<>();
+        finalResult.put("park", 2);
+        finalResult.put("yong", 4);
+        finalResult.put("hyeon", 3);
+
+        List<String> winners = racingCarGameService.getWinners(finalResult);
+
+        assertThat(winners.size()).isEqualTo(1);
+        assertThat(winners.get(0)).isEqualTo("yong");
+    }
+
+    @Test
+    @DisplayName("가장 많이 이동한 자동차를 우승자로 뽑되, 동점자가 있다면, 동점자들 모두를 우승자로 뽑는다.")
+    void getWinners() {
+        RacingCarGameService racingCarGameService = new RacingCarGameService();
+        LinkedHashMap<String, Integer> finalResult = new LinkedHashMap<>();
+        finalResult.put("park", 2);
+        finalResult.put("yong", 4);
+        finalResult.put("hyeon", 4);
+
+        List<String> winners = racingCarGameService.getWinners(finalResult);
+
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(winners.get(0)).isEqualTo("yong");
+        assertThat(winners.get(1)).isEqualTo("hyeon");
     }
 }
