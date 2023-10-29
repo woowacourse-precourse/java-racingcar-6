4a78f31057b350a94d097e152b32ae21b0e95d39
waveofmymind <sjun0913@gmail.com>
2023-10-29 20:16:41 +0900

    feat: 게임 결과에 우승자 목록도 포함한다.

--- src/main/java/racingcar/domain/GameResult.java
+++ src/main/java/racingcar/domain/GameResult.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import java.util.List;
 
 public record GameResult(
-        List<RoundHistory> roundHistory
+        List<RoundHistory> roundHistory,
+
+        List<Name> winners
 ) {
 }
