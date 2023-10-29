d51fc4d9f896c0c93cc2631cb57b4ff80cb3dade
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-29 18:15:10 +0900

    FEAT / modify to Singlethon

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,4 +1,14 @@
 package racingcar.service;
 
 public class GameService {
+    public GameService() {
+    }
+
+    public static GameService getInstance() {
+        return GameService.LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final GameService INSTANCE = new GameService();
+    }
 }
