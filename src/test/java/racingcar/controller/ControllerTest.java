4e360c1ab3494ec47186c4bf2347cb581831e7f5
ddangme <ssilver121@nav>
2023-10-30 01:16:09 +0900

    test: 자동차 이름으로 생성한 game의 car정보 테스트 코드

--- src/test/java/racingcar/controller/ControllerTest.java
+++ src/test/java/racingcar/controller/ControllerTest.java
@@ -2,8 +2,12 @@ package racingcar.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.Game;
 import racingcar.service.InitService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,32 +20,48 @@ class ControllerTest {
         Game game = new Game();
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("가,가");
+            game.validationCarNameAndSetCar("가,가");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("가,,가");
+            game.validationCarNameAndSetCar("가,,가");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("가,다,");
+            game.validationCarNameAndSetCar("가,다,");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("");
+            game.validationCarNameAndSetCar("");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("가나다라마");
+            game.validationCarNameAndSetCar("가나다라마");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("가나다라마바사, 아자");
+            game.validationCarNameAndSetCar("가나다라마바사, 아자");
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validationCarNameAndSetGame("간ㄴ, 아자");
+            game.validationCarNameAndSetCar("간ㄴ, 아자");
         });
+
+    }
+    
+    @DisplayName("Game 객체에 정상적으로 Car정보가 저장되는지 확인")
+    @Test
+    void Game_객체_Car_이름_확인() {
+        String[] carNames = {"가나", "다라"};
+
+        String carNameListString = carNames[0] + "," + carNames[1];
+
+        Game game = new Game();
+        game.validationCarNameAndSetCar(carNameListString);
+
+        for (int i = 0; i < game.getCarList().size(); i++) {
+            assertEquals(carNames[i], game.getCarList().get(i).getName());
+        }
 
     }
 
