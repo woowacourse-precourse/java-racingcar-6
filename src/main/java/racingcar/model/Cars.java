5820ac608411b642d7a2b0f7b82568ce3dd56fff
“EvelynKimm” <evekimmy09@gmail.com>
2023-10-29 12:03:39 +0900

    feat: add put object method
    car model에 입력받은 리스트의 객체 추가하는 메서드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,17 +1,23 @@
 package racingcar.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
+    public static List<Car> cars;
+    public static String[] carNames;
     private static final String DELIMITER = ",";
+
     public static String[] makeCarList(String input) {
-        String[] list = input.split(DELIMITER);
-        return list;
+        return input.split(DELIMITER);
     }
-    // 이건 객체지향
-//    public static void makeCarListtmp(String[] list) {
-//        List<Car> cars = new ArrayList<>();
-//        for (String carName : list) {
-//            cars.add(new Car(carName));
-//        }
-//    }
+
+    public static void putObject() {
+        cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
 }
