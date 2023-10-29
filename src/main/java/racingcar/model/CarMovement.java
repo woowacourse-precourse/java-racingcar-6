3ca86d583f5e826022ebc9b821c1640a5e6a37ec
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-30 00:30:40 +0900

    feat: 랜덤 숫자에 따라 차를 전진시키는 코드 추가

--- src/main/java/racingcar/model/CarMovement.java
+++ src/main/java/racingcar/model/CarMovement.java
@@ -4,6 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarMovement {
 
+    public static void moveCar(Car car){
+        int randomNumber = createRandomNumber();
+
+        if (randomNumber >= 4){
+            car.updateDistance();
+        }
+    }
+
     private static int createRandomNumber(){
         int randomNumber = Randoms.pickNumberInRange(0, 9);
 
