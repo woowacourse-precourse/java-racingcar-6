5846bf9ce867d4bf1ef78c1d4fb72f1117966802
ohjuyoungg <ejrm12@naver.com>
2023-10-29 20:28:47 +0900

    feat(race game) : InputValidation 클래스 내에 사용자가 아무 값을 입력하지 않은 경우와 6자 이상의 이름을 입력한 경우 예외 처리 메소드 validationName 구현

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -1,24 +1,40 @@
 package racingcar.validation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidation {
 
-    private static void validationBlankName() {
-        // 사용자가 아무 값을 입력하지 않은 경우
+    private static final String COMMA = ",";
+    private static final int MAX_STRING = 5;
+    private static final int MIN_STRING = 1;
+
+
+    public static List<String> validationName(String carName) {
+        if (carName == null || carName.isBlank() || carName.length() < MIN_STRING || carName.length() > MAX_STRING) {
+            throw new IllegalArgumentException("자동차 이름은 1 ~ 5 자리로 입력해야 합니다.");
+        }
+        List<String> carNamesList = Arrays.asList(carName.split(COMMA));
+        return carNamesList;
     }
 
-    private static void validationSixCharOrMore() {
-        // 6 자 이상의 이름을 입력할 경우
+    public static List<String> validationCommaSeparate(List<String> carNames) {
+        return null;
     }
 
-    private static void validationCommaSeparate() {
-        // 쉼표로 구분하여 입력하지 않은 경우
+    public static List<String> validateAndSplit(String name) {
+        return null;
     }
 
-    private static void validationTwoOrMore() {
-        // 2 이상의 자동차 이름을 입력 하지 않은 경우
+    // 자동차 이름이 같은 경우 예외 처리
+    public static void carSameNameValidation(List<String> carNames) {
     }
 
+    // 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리
     private static void carNumericValidation() {
-        // 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리
     }
 }
+
