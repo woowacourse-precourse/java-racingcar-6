994fd7e7655c197031c960883ca206acb6ab9bb0
Hyo Gyeong <cdkrcd8@gmail.com>
2023-10-29 20:51:28 +0900

    chore: remove redundant initializer

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
         CheckValidation checkValidation = new CheckValidation();
-        ArrayList<String> carNameList = new ArrayList<>();
+        ArrayList<String> carNameList;
         String input;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
