117ee58cff1bc1fd955b3be35809697de4925adb
otaeho <dhxogh123123@naver.com>
2023-10-29 21:03:54 +0900

    refactor(Cars): 인스턴스 필드 getter 추가
    경주에 참여하는 자동차 정보들을 얻기 위한 인스턴스 필드 getter 생성

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -22,4 +22,8 @@ public class Cars {
     private void sortCarsByForwards() {
         Collections.sort(carList, (car1, car2) -> car2.getForwards() - car1.getForwards());
     }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
 }
