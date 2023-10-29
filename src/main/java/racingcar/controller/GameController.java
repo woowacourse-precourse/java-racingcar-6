0eee25ba5a079c97292d524544bca8263da596a7
chs98412 <chs98412@gmail.com>
2023-10-29 18:03:48 +0900

    feat:GameController 뼈대

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+public class GameController {
+
+    public void gameStart() {
+
+    }
+    private void gameInit() {
+
+    }
+
+    private void gameProcess() {
+
+    }
+
+    private void gameResult() {
+
+    }
+}
