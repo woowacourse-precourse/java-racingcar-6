d883fb89c5207f22ba94312044cf07aa89941410
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-10-29 16:24:56 +0900

    test: 추가,수정 UserInputTest 예외 처리 메서드
    메서드 이름을 통일시킴.
    void 회수가_0보다_작은_경우_예외_처리() 메서드를 추가함.
     void 이름이_공백인_경우_예외_처리() 코드를 변경함.

--- src/test/java/racingcar/UserInputTest.java
+++ src/test/java/racingcar/UserInputTest.java
@@ -1,17 +1,14 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserInputTest extends NsTest {
     @Test
-    void 회수가_숫자_아님_예외처리() {
+    void 회수가_숫자_아닌_경우_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,lkh,be", "abc"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -19,13 +16,29 @@ public class UserInputTest extends NsTest {
     }
 
     @Test
-    void 이름이_공백_예외처리() {
-        List<String> names = Arrays.asList(" ", "happy", "123");
-        List<String> emptyName = Arrays.asList(" ");
-
-        assertThat(!Application.isNotEmpty(names));
-        assertThat(!Application.isNotEmpty(emptyName));
+    void 회수가_0보다_작은_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, sam", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,sam", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
+
+    @Test
+    void 이름이_공백인_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,abc", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
