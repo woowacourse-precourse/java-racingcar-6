4bd6a3338cd65a7d2ea5320870277e074b442613
juone00 <juwon0208@naver.com>
2023-10-29 18:37:24 +0900

    feat(racingcar): 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,11 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        playRacingCar();
+    }
+
+    private static void playRacingCar() {
+
+
     }
 }
