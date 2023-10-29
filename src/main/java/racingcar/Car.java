fbe7b0579240a969aa3870ef9e0e124db4d1579a
ImGrooott <20184456@chosun.ac.kr>
2023-10-29 12:17:47 +0900

    add skeleton code

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int odometer;
+
+    public void moveForward() {
+        this.odometer++;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
