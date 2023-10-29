35517470c2785229e3117d7746050a82ea5242ab
cyberjam <jamsince1994@naver.com>
2023-10-30 08:19:14 +0900

    feat: add Car Class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -17,6 +17,11 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = readLine();
         String[] carNames = carNamesInput.split(",");
+        int carCount = carNames.length;
+        Car[] cars = new Car[carCount];
+        for (int index = 0; index < carCount; index++) {
+            cars[index] = new Car(carNames[index], 0);
+        }
         System.out.println(Arrays.toString(carNames));
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int distance;
+
+    public Car(String name, int distance){
+        this.name = name;
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+}
