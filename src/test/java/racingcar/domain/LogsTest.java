4fa2ca8d19f7335ce9e09faf486b26609acb0523
unknown <y2kf9a0q@gmail.com>
2023-10-29 14:00:24 +0900

    test: skeleton of LogsTest.java

---
+++ src/test/java/racingcar/domain/LogsTest.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void inputCarNames() {
+    }
+
+    @Test
+    void inputGameTurns() {
+    }
+
+    @Test
+    void newLine() {
+    }
+
+    @Test
+    void getGameResult() {
+    }
+
+    @Test
+    void car() {
+    }
+
+    @Test
+    void string() {
+    }
+}
