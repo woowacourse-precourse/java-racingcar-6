3bfde1f4018435da343c40e71c58c1e76562cc6a
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-29 23:57:56 +0900

    refafctor(Validator): validateInputIsNumber 메서드 수정

--- src/main/java/racingcar/Controller/Module/Validator.java
+++ src/main/java/racingcar/Controller/Module/Validator.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Validator {
-    public static int validateInputIsNumber(int input){
-        if(input > 0){
-            return input;
-        }else{
-            throw new IllegalArgumentException("잘못된 입력값 입니다.");
+    public static int validateInputIsNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number > 0) {
+                return number;
+            } else {
+                throw new IllegalArgumentException("입력값은 양의 정수여야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자로 변환할 수 없는 문자열입니다.", e);
         }
     }
+
 
     public static ArrayList<String> validateNameInputContainsComma(String input){
         if(input.contains(",")){
