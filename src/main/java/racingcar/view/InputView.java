f719ddadb0ab2d4a6102f46008cf743814103c2b
jihyuk <kwlgur0416@gmail.com>
2023-10-29 14:46:54 +0900

    feat : 시도 횟수 기능 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,16 @@ public class InputView {
 
     private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
+    private static final String ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
     public static String inputCarNames() {
         System.out.println(CAR_NAMES_MESSAGE);
         return getInput();
+    }
+
+    public static int inputAttemptCount() {
+        System.out.println(ATTEMPT_COUNT_MESSAGE);
+        return Integer.parseInt(getInput());
     }
 
     private static String getInput() {
