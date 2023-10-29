bb04cb5c91600f02cef489c58aa4e6515f0a6590
junest66 <chlwndks333@gmail.com>
2023-10-30 02:44:37 +0900

    refactor(RealNumberGeneratorTest): 랜덤숫자 상수로 지정

--- src/test/java/racingcar/domain/RealNumberGeneratorTest.java
+++ src/test/java/racingcar/domain/RealNumberGeneratorTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.RepeatedTest;
 
 class RealNumberGeneratorTest {
 
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     @RepeatedTest(100)
     void 랜덤숫자_0부터_9_사이인지_확인() {
         NumberGenerator numberGenerator = new RealNumberGenerator();
         int randomNumber = numberGenerator.createRandomNumber();
-        assertThat(randomNumber).isBetween(0, 9);
+        assertThat(randomNumber).isBetween(MIN_NUMBER, MAX_NUMBER);
     }
 }
