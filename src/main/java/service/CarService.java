3fcacaf5b928310e93eaedce189f80efb71da1e1
sangeun-b <baek0007@algonquinlive.com>
2023-10-30 00:20:54 +0900

    refactor: class 이름 변경

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RacingCarService {
+public class CarService {
 
     public List<String> stringToList(String carName) {
         List<String> carNames = Arrays.asList(carName.split(","));
