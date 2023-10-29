536ecc2ea58f9ab5cfc69798a4638e7c5a43ac92
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 17:52:05 +0900

    test(CarMapperTest): 문자열 이름 목록 자동차 리스트로 변환하는 기능 테스트 구현

---
+++ src/test/java/racingcar/mapper/CarMapperTest.java
@@ -0,0 +1,23 @@
+package racingcar.mapper;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+
+public class CarMapperTest {
+    @DisplayName("문자열로 이름을 입력 받으면 자동차 리스트로 변환해야 한다.")
+    @Test
+    void 문자열_자동차_리스트로_변환_기능_테스트() {
+        //given
+        String names = "jun,seo,park";
+
+        //when
+        List<Car> cars = CarMapper.convertToCars(names);
+
+        //then
+        assertThat(cars.size()).isEqualTo(3);
+    }
+}
