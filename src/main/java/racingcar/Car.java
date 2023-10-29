2ba0a9797e3fde62ccc1c6f33cbbf76d971f8d8c
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-29 15:41:04 +0900

    feat: 자동차 상태 변화 메서드 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int state;
+
+    public Car(String name) {
+        Validator.nameLengthCheck(name);
+        this.name = name;
+        this.state = 0;
+    }
+
+    public void move() {
+        this.state++;
+    }
+}
