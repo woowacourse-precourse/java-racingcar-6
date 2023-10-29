c7bd738ca861310610ce3feaf6845b06e5512b7f
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-30 01:38:19 +0900

    feat: 실행결과 출력 기능 추가

--- src/main/java/racingcar/controller/Gamecontroller.java
+++ src/main/java/racingcar/controller/Gamecontroller.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import racingcar.model.Car;
 import racingcar.view.PlayerinputView;
+import racingcar.view.PlayeroutputView;
 
 import java.util.List;
 
@@ -11,5 +12,15 @@ public class Gamecontroller {
         String[] names = PlayerinputView.getNames();
         List<Car> cars = PlayerinputView.saveNames(names);
         int round = PlayerinputView.getRound();
+        gameResult(round, cars);
     }
+
+    private static void gameResult(int round, List<Car> cars){
+        System.out.println("\n실행결과");
+        for (int i = 0; i < round; i++) {
+            PlayeroutputView.updateRound(cars);
+            PlayeroutputView.round(cars);
+        }
+    }
+
 }
