6161c646bde95d297110d739d233b170c684d707
eora21 <eora21@naver.com>
2023-10-29 23:43:44 +0900

    refactor: 💡 수정 자동차 이름 길이를 자동차 스스로가 검증하도록 변경
    CarFactory에서 검증 메서드를 들고 있으면, 이름 제한을 넘는 차량이 만들어질 수도 있다. 해당 요구사항을 제대로 지킬 수
    있도록 Car 자체에서 검증하도록 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,12 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int driveCount;
 
     public Car(String name) {
+        verifyNameLength(name);
         this.name = name;
         this.driveCount = 0;
     }
@@ -15,5 +17,11 @@ public class Car {
 
     public CarStatus getStatus() {
         return new CarStatus(name, driveCount);
+    }
+
+    private static void verifyNameLength(String name) {
+        if (MAX_NAME_LENGTH < name.length()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
