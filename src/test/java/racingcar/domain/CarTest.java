e1b184cd3ef024d5b47791115ae77934cb0b5316
hunko <rhwlgns4386@naver.com>
2023-10-29 15:25:05 +0900

    test(Car) : 이동 실패 테스트 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,11 +8,20 @@ class CarTest {
 
     @Test
     public void 난수가_4이상이면_이동() {
-        Car car = new Car();
+        Car car = new Car(new TestNumberGenerator(4));
 
         car.move();
 
-        assertThat(car.getLocation()).isEquals(1);
+        assertThat(car.getLocation()).isEqualTo(1);
+    }
+
+    @Test
+    public void 난수가_4미만이면_이동하지_않는다(){
+        Car car = new Car(new TestNumberGenerator(3));
+
+        car.move();
+
+        assertThat(car.getLocation()).isEqualTo(0);
     }
 
 }
