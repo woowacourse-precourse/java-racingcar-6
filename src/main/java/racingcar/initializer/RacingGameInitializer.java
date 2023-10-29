aa03d5146fc40038523ce15c1159ba4249c03c2b
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:58:20 +0900

    feat(initializer): RacingGameInitializer 구현

---
+++ src/main/java/racingcar/initializer/RacingGameInitializer.java
@@ -0,0 +1,21 @@
+package racingcar.initializer;
+
+import racingcar.domain.system.game.Game;
+import racingcar.domain.system.game.RacingGame;
+import racingcar.domain.system.game.round.host.Host;
+import racingcar.domain.system.game.round.host.RandomSignalHost;
+import racingcar.domain.system.manager.car.CarManager;
+import racingcar.domain.system.manager.car.InMemoryCarManager;
+import racingcar.domain.system.manager.position.CarPositionManager;
+import racingcar.domain.system.manager.position.InMemoryCarPositionManager;
+
+public class RacingGameInitializer {
+
+    public Game getGame() {
+        ConsoleGameFactory factory = new ConsoleGameFactory();
+        Host host = new RandomSignalHost();
+        CarManager carManager = new InMemoryCarManager();
+        CarPositionManager carPositionManager = new InMemoryCarPositionManager();
+        return new RacingGame(factory, host, carManager, carPositionManager);
+    }
+}
