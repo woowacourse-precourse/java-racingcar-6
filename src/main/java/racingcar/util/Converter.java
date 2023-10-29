094a4dd9a5283a92771218648d6411fad1a44507
Jae-Young98 <ygy0102@gmail.com>
2023-10-30 02:17:20 +0900

    refactor: 매직 리터럴을 상수 문자열로 교체
    Converter, InputValidator, InputView, OutputView 내의 매직 리터럴을 상수로 교체

--- src/main/java/racingcar/util/Converter.java
+++ src/main/java/racingcar/util/Converter.java
@@ -3,9 +3,9 @@ package racingcar.util;
 import java.util.Arrays;
 import java.util.List;
 
-public class Converter {
-    private static final String DELIMITER = ",";
+import static racingcar.constant.StringConstant.DELIMITER;
 
+public class Converter {
     public static List<String> getCarNames(String inputNames) {
         return Arrays.asList(inputNames.split(DELIMITER));
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,24 +5,30 @@ import racingcar.domain.Cars;
 
 import java.util.List;
 
+import static racingcar.constant.StringConstant.COLON;
+import static racingcar.constant.StringConstant.DASH;
+import static racingcar.constant.StringConstant.JOIN_DELIMITER;
+import static racingcar.constant.StringConstant.RESULT_MESSAGE;
+import static racingcar.constant.StringConstant.WINNER_MESSAGE;
+
 public class OutputView {
     public void showResult() {
-        System.out.println("\n실행 결과");
+        System.out.println(RESULT_MESSAGE);
     }
     public void showPosition(Cars cars) {
         List<Car> carList = cars.getCars();
         for (Car car : carList) {
-            System.out.println(car.getName() + " : " + makeDelimiter(car.getPosition()));
+            System.out.println(car.getName() + COLON + makeDelimiter(car.getPosition()));
         }
         System.out.println();
     }
 
     private String makeDelimiter(int position) {
-        return "-".repeat(position);
+        return DASH.repeat(position);
     }
 
     public void showWinner(List<String> winner) {
-        String winnerName = String.join(", ", winner);
-        System.out.print("최종 우승자 : " + winnerName);
+        String winnerName = String.join(JOIN_DELIMITER, winner);
+        System.out.print(WINNER_MESSAGE + winnerName);
     }
 }
