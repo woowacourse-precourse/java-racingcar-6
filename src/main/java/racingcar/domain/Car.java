226e69006ba910e7ca5c518bc72bcb0e9e930a27
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-29 23:38:26 +0900

    🚀refector(domain / Car) : Map 변경 취소

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -10,27 +10,26 @@ import java.util.Map;
 * */
 public class Car {
     private String carName;
-    private Map<String, Integer> car;
+    private int location = 0;
+
     public Car(String carName) {
-        validateDuplicate(carName);
         this.carName = carName;
+        validateDuplicate();
     }
 
     // 전진 기능
     public void forward() {
-        int location = car.get(carName);
-        car.put(carName,++location);
+        this.location++;
     }
 
     // 후진 기능
     public void backward() {
-        int location = car.get(carName);
-        car.put(carName,--location);
+        this.location--;
     }
 
     // car 중복 검사
-    public void validateDuplicate(String carName) {
-        if (Cars.cars.contains(car)) {
+    public void validateDuplicate() {
+        if (Cars.cars.contains(carName)) {
             throw new IllegalArgumentException("중복된 이름의 자동차는 동시에 경기할 수 없습니다.");
         }
     }
