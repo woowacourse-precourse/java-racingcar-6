24463fbfbd26e085466c24e5e63356fb20665bdb
m2nseop <pms246@naver.com>
2023-10-29 23:01:54 +0900

    feat: 입력값 검증 기능(숫자로된 회차수, 차동차이름 길이) validator 추가

--- src/main/java/racingcar/Controller/RacingCarController.java
+++ src/main/java/racingcar/Controller/RacingCarController.java
@@ -1,12 +1,15 @@
 package racingcar.Controller;
 
 import java.util.List;
+import racingcar.Util.InputValidator;
 import racingcar.View.InputView;
 
 public class RacingCarController {
 
     public void initRacingCarGame(){
         List<String> carNameList = InputView.inputRacingCarName();
+        InputValidator.checkCarNameLength(carNameList);
         String racingRoundTimes = InputView.inputRacingRoundTimes();
+        InputValidator.checkIsNumber(racingRoundTimes);
     }
 }
---
+++ src/main/java/racingcar/Util/InputValidator.java
@@ -0,0 +1,25 @@
+package racingcar.Util;
+
+import java.util.List;
+
+public class InputValidator {
+
+    public static void checkCarNameLength(List<String> inputStrList){
+        for (String str : inputStrList) {
+            if (str.length() > 5) {
+                throw new IllegalArgumentException("자동차의 이름은 5글자 이하여야 합니다.");
+            }
+        }
+    }
+    public static void checkIsNumber(String inputStr){
+        System.out.println(inputStr);
+        if (inputStr == null || inputStr.isEmpty()) {
+            throw new IllegalArgumentException("원하는 시도 횟수를 입력해주세요!");
+        }
+        for (char c : inputStr.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("원하는 시도 횟수를 숫자로 입력해주세요!");
+            }
+        }
+    }
+}
