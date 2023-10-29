1f5a070ccbf163cd0e7cdc8b1be6024915b4524f
seminss <2007ksm@naver.com>
2023-10-29 20:44:22 +0900

    feat: 자동차 이동 시도 횟수 입력 검증
    TriesCountInputValidator: 검증 후 자동차 이동 시도 횟수를 정수로 변환해 반환

---
+++ src/main/java/racingcar/validator/TriesCountInputValidator.java
@@ -0,0 +1,36 @@
+package racingcar.validator;
+
+public class TriesCountInputValidator {
+    public int validateAndGetTriesCount(String userInput) {
+        userInput = removeWhiteSpace(userInput);
+        validateNotEmpty(userInput);
+        int triesCount = parseValidInteger(userInput);
+        validatePositive(triesCount);
+        return triesCount;
+    }
+
+    private void validateNotEmpty(String userInput){
+        if(userInput.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int parseValidInteger(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validatePositive(int number){
+        if(number<=0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String removeWhiteSpace(String userInput) {
+        userInput = userInput.replace(" ","");
+        return userInput;
+    }
+}
