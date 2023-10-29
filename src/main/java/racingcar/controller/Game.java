a78fb7e20c09ec0dd67f43e548a8097da08368b7
lotuxsoo <tndus5383@gmail.com>
2023-10-30 00:25:21 +0900

    feat: InputView에서 사용자에게 n대의 자동차 이름을 입력받음, 리드미 업데이트

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,9 +1,11 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Game {
     public void start() {
         OutputView.printStartMessage();
+        InputView.getRacingCars();
     }
 }
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static void getRacingCars() {
+        String input = Console.readLine();
+        // validateCarLength();
+        // validateCarsFormat();
+    }
+}
