1d6d4be66a7776cd0dbaee2c0b699234ba422722
leemiyeon <lemingul7@gmail.com>
2023-10-30 04:35:29 +0900

    refactor: block instantiation of Utils class 1. RacingConfig 2. Validator

--- src/main/java/racingcar/domain/RacingConfig.java
+++ src/main/java/racingcar/domain/RacingConfig.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
 public class RacingConfig {
+    private RacingConfig() {
+        throw new AssertionError("RacingConfig class should not be instantiated.");
+    }
     public static final String NAME_SEPARATOR = ",";
     public static final String NAME_PATTERN = "^([a-zA-Z\\p{IsHangul}]+,)*[a-zA-Z\\p{IsHangul}]+$";
-    public static final int MAX_LENGTH = 5;
+    public static final String FORWARD_HISTORY_MARK = "-";
+    public static final int MAX_NAME_LENGTH = 5;
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
     public static final int FORWARD_NUMBER = 4;
