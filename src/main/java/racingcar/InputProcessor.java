4fa6fe87492241c2bcee5a068e710ea36b54343d
harandal24601 <devchaehokim@gmail.com>
2023-10-30 01:01:14 +0900

    FEAT : Add car name Input method
    Add method which returns array of split input data.

--- src/main/java/racingcar/InputProcessor.java
+++ src/main/java/racingcar/InputProcessor.java
@@ -5,5 +5,10 @@ import java.util.Collections;
 
 public class InputProcessor {
 
-
+    public static ArrayList<String> carNameInputProcess(String input) {
+        ArrayList<String> names = new ArrayList<>();
+        String[] splitInput = input.split(",");
+        Collections.addAll(names, splitInput);
+        return names;
+    }
 }
