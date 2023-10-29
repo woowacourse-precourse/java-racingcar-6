4fe251c24985347d318803a218957e51fdcc4da4
YongHyeon <pyh4616@naver.com>
2023-10-30 02:15:47 +0900

    Feat: (RacingCarGameService 클래스), 이동이 모두 끝난 결과를 통해 우승자를 뽑는 기능을 getWinners 메서드로 구현

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,6 +1,9 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RacingCarGameService {
 
@@ -15,5 +18,30 @@ public class RacingCarGameService {
 
     public void moveForward(LinkedHashMap<String, Integer> progressStatus, String carName) {
         progressStatus.put(carName, progressStatus.get(carName) + 1);
+    }
+
+    public List<String> getWinners(LinkedHashMap<String, Integer> finalResult) {
+        int maxMovingValue = Integer.MIN_VALUE;
+        for (int movingValue : finalResult.values()) {
+            maxMovingValue = getMaxMovingValue(movingValue, maxMovingValue);
+        }
+        List<String> winners = new ArrayList<>();
+        for (Map.Entry<String, Integer> finalResultEntry : finalResult.entrySet()) {
+            addWinner(finalResultEntry, maxMovingValue, winners);
+        }
+        return winners;
+    }
+
+    private int getMaxMovingValue(int movingValue, int maxMovingValue) {
+        if (movingValue > maxMovingValue) {
+            return movingValue;
+        }
+        return maxMovingValue;
+    }
+
+    private void addWinner(Map.Entry<String, Integer> finalResultEntry, int maxMovingValue, List<String> winners) {
+        if (finalResultEntry.getValue() == maxMovingValue) {
+            winners.add(finalResultEntry.getKey());
+        }
     }
 }
