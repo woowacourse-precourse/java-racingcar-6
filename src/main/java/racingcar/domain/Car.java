21bdc466a96b067598b623c99713e054f0cc8d04
JudyYeon <dev.judykim@gmail.com>
2023-10-29 20:52:57 +0900

    feat(Car): 자동차가 전진하는 기능, 자동차 이름 부여 기능 구현
    - 사용자에게 입력받은 자동차 String[] 이름에 따라 Car 인스턴스를 생성하도록 구현했습니다.
    - NumberGenerator 로 임의의 수를 생성하고, 전진 조건을 만족하는지 체크하여 인스턴스의 position 을 증가하도록 구현했습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     private static final int MOVE_CONDITION = 4;
-    private String name;
+    private final String name;
     private int position;
 
     public Car(String name) {
@@ -19,13 +22,24 @@ public class Car {
         return position;
     }
 
-    public void move(int speed){
-
+    public void move(){
+        if (NumberGenerator.createRandomNumber() >= MOVE_CONDITION) {
+            position++;
+        }
     }
 
-    public static Car createCar(String name) {
-        return null;
+    public static List<Car> createCars(String[] names) {
+
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : names) {
+            cars.add(createCar(name));
+        }
+        return cars;
     }
 
+    private static Car createCar(String name) {
+        return new Car(name);
+    }
 
 }
