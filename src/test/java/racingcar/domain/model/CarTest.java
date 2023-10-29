75a21918b0652fbd7b0e8a02eb2c7196c49e6c81
wnsgh12s <junho1273@gmail.com>
2023-10-30 08:01:29 +0900

    test:  move가_호출될_시_position_이_증가되는지  테스트

---
+++ src/test/java/racingcar/domain/model/CarTest.java
@@ -0,0 +1,23 @@
+package racingcar.domain.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CarTest {
+
+
+    @Test
+    void move가_호출될_시_position_이_증가되는지() {
+        Car car = new Car("pobi");
+        int initPosition = car.getPostion();
+        car.move();
+        assertEquals(car.getPostion(), initPosition + 1);
+    }
+
+}
