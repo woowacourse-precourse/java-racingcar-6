a14d042f189d08f3be2f238564b6ecf5e1299aef
jyajoo <jyajoo1020@gmail.com>
2023-10-29 17:22:10 +0900

    feat: 시도 횟수를 묻는 countOutput 추가

--- src/main/java/racingcar/constant/RaceMessage.java
+++ src/main/java/racingcar/constant/RaceMessage.java
@@ -1,7 +1,8 @@
 package racingcar.constant;
 
 public enum RaceMessage {
-    INIT_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    INIT_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INIT_COUNT_MESSAGE("시도할 회수는 몇회인가요?");
 
     private final String message;
 
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     public void carNamesOutput() {
         System.out.println(INIT_CAR_NAMES_MESSAGE.getMessage());
     }
+
+    public void countOutput() {
+        System.out.println(INIT_COUNT_MESSAGE);
+    }
 }
