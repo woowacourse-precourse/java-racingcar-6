006eaf012e506afb4f19f3d785354382348c9336
ohjuyoungg <ejrm12@naver.com>
2023-10-29 21:42:08 +0900

    feat(race game) : 사용자가 같은 자동차 이름을 입력 할 경우 예외 처리하는 carSameNameValidation 메소드 구현

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -17,9 +17,9 @@ public class InputValidation {
         if (carName == null || carName.isBlank() || carName.length() < MIN_STRING || carName.length() > MAX_STRING) {
             throw new IllegalArgumentException("자동차 이름은 1 ~ 5 자리로 입력해야 합니다.");
         }
-        List<String> carNamesList = Arrays.asList(carName.split(COMMA));
-        return carNamesList;
+        return Arrays.asList(carName);
     }
+
 
     public static List<String> validationNameSeparate(List<String> carNames) {
         List<String> validatedNames = new ArrayList<>();
@@ -27,7 +27,9 @@ public class InputValidation {
         for (String name : carNames) {
             validatedNames.addAll(validationName(name));
         }
+
         carSameNameValidation(validatedNames);
+
         return validatedNames;
     }
 
@@ -37,10 +39,15 @@ public class InputValidation {
         if (namesList.size() < 2) {
             throw new IllegalArgumentException("자동차 이름을 쉼표로 구분하여 2개 이상 입력해야 합니다.");
         }
+        return namesList;
     }
 
     // 자동차 이름이 같은 경우 예외 처리
     public static void carSameNameValidation(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        if (uniqueNames.size() < carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 자동차마다 다르게 입력해야 합니다.");
+        }
     }
 
     // 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리
