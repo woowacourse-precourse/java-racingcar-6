aca91b7b6eacf5c160016edef625707f85b7845e
acs0209 <annjae4676@naver.com>
2023-10-29 17:19:09 +0900

    feat: add car class

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+}
