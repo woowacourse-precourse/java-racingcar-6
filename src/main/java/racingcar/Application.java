848bdb8bd0b58998fc6bcf6d1a6413980bebdb97
songyi <song21677@naver.com>
2023-10-29 19:58:58 +0900

    feat: 쉼표로 구분한 자동차 이름 앞 뒤 공백 제거 후 리스트에 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,5 +11,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+
+        List<String> carNameList  = Arrays.stream(input.split(","))
+                                        .map(carName -> carName.strip())
+                                        .toList();
     }
 }
