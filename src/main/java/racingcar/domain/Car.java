d392761527ca4609c92185e311e26f5848c3beb4
heogeonho <geonh1016@gmail.com>
2023-10-30 04:16:09 +0900

    feat: 자동차 클래스 생성자 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,6 +6,10 @@ public class Car {
     public String name;
     public int position;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     public void moveForward() {
         int value = Randoms.pickNumberInRange(0,9);
         if (value >= 4) {
