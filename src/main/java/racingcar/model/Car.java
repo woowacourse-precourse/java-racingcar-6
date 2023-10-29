4c00831c4a18de8cf9c04386b57e716bff668809
JeongHunHui <deca3248@gmail.com>
2023-10-30 01:14:09 +0900

    feat: 자동차의 이름이 5글자 이하인지 확인

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,11 +2,19 @@ package racingcar.model;
 
 public class Car {
 
+    private static final Integer MAX_NAME_LENGTH = 5;
     private final String name;
     private Integer moveCount;
 
     public Car(String name) {
+        validateNameLength(name.length());
         this.name = name;
         this.moveCount = 0;
+    }
+
+    private void validateNameLength(Integer nameLength) {
+        if (nameLength > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 }
