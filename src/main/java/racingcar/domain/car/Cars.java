e508d709fc1ccd48d8a6e77f86b20df56fd368dc
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 17:40:47 +0900

    Revert "refactor(CarMapper): 문자열을 자동차 리스트로 변환하는 기능 Mapper 클래스로 분리"
    This reverts commit dcaeb5bc90fa82f26997f79937fe52504555488a.

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -4,17 +4,24 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import racingcar.mapper.CarMapper;
 
 public class Cars {
+    private static final String DELIMITER = ",";
     private List<Car> cars;
 
     public Cars(String cars) {
-        List<Car> targetCars = CarMapper.stringToCarList(cars);
+        List<Car> targetCars = convertStringToCar(cars);
         this.cars = targetCars;
     }
 
     public List<Car> getCars() {
         return Collections.unmodifiableList(cars);
+    }
+
+    private List<Car> convertStringToCar(String cars) {
+        String[] tokens = cars.split(DELIMITER);
+        return Arrays.stream(tokens)
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
     }
 }
