03a18f3594e8c76133aaf2707e89c9b3cec94039
acs0209 <annjae4676@naver.com>
2023-10-29 17:33:09 +0900

    feat: add race control logic
    race를 제어하는 로직을 추가

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -8,10 +8,19 @@ public class User {
 
     public void play() {
         save();
+        race();
     }
 
     public void save() {
         carController.save();
+    }
+
+    public void race() {
+        int raceCount = carController.getRaceCount();
+        int round = 1;
+        while (round <= raceCount) {
+            round++;
+        }
     }
 
 }
