b9869759dd6a126d207b5d3b8fc6282267d0eb4b
Jinseo Jeong <js020315@gmail.com>
2023-10-29 18:42:36 +0900

    [FEAT] OutputView 클래스 생성
    경주 결과, 우승자 출력

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class OutputView {
+    public void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printWinners() {
+        System.out.print("최종 우승자 : ");
+    }
+}
