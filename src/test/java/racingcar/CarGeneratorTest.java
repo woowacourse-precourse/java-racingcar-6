c111f12bb0564e2ab6c8d3223ddeab497e3a96f9
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 22:50:35 +0900

    test(carGenerator): @ParameterizedTest로 변경

--- src/test/java/racingcar/CarGeneratorTest.java
+++ src/test/java/racingcar/CarGeneratorTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarGeneratorTest {
     private final CarGenerator carGenerator = new CarGenerator();
@@ -15,18 +17,11 @@ public class CarGeneratorTest {
                 .containsExactly(new Car("pobi"), new Car("woni"));
     }
 
-    @Test
-    void 이름이_5글자_초과인_경우_예외() {
+    @ParameterizedTest(name = "이름이 {0}인 경우 예외")
+    @ValueSource(strings = {"javaji", ""})
+    void 이름이_5글자_초과인_경우_예외(String invalidName) {
         assertThatThrownBy(
-                () -> carGenerator.generate(List.of("ppppobi", "woni")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 5자 이하, 1자 이상만 가능합니다.");
-    }
-
-    @Test
-    void 이름이_0글자인_경우_예외() {
-        assertThatThrownBy(
-                () -> carGenerator.generate(List.of("", "woni")))
+                () -> carGenerator.generate(List.of(invalidName)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5자 이하, 1자 이상만 가능합니다.");
     }
