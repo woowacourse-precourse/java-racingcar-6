0b9a0c268b89317e0484aee2a30292d6c0a73cb4
Hayoon <hy97@sju.ac.kr>
2023-10-29 17:21:49 +0900

    feat(ProductConstant): ProductCode에서 Integer 값을 상수로 선언한다

--- src/main/java/racingcar/util/constant/ProductConstant.java
+++ src/main/java/racingcar/util/constant/ProductConstant.java
@@ -4,4 +4,13 @@ public class ProductConstant {
     public static String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
     public static String GAME_RESULT = "실행 결과";
+
+    public static int START_NUMBER = 0;
+    public static int LAST_NUMBER = 9;
+    public static int MINIMUM_VALUE = 4;
+    public static int PLUS_TIME = 1;
+
+    public static String MOVE_ONE_SPACE = "-";
+    public static String FIRST_LOCATION = "";
+    public static String FINAL_WINNERS = "최종 우승자";
 }
