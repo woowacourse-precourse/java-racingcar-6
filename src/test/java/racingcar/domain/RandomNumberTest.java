4ab1b78668bd283a02b4c5c2ce138f06499def71
slowcloud <sju9417@gmail.com>
2023-10-29 10:23:30 +0900

    refactor(RandomNumberTest): 테스트 코드를 간결하게 변경

--- src/test/java/racingcar/domain/RandomNumberTest.java
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -1,46 +1,21 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.BDDMockito.given;
 
 class RandomNumberTest {
 
-    private static MockedStatic<Randoms> randoms;
-
-    @BeforeAll
-    public static void beforeAll() {
-        randoms = Mockito.mockStatic(Randoms.class);
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        randoms.close();
-    }
-
     @Test
     void isMovable_4_이상이면_참() {
-        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(7);
-
-        RandomNumber randomNumber = RandomNumber.createRandomNumber();
-
+        RandomNumber randomNumber = new RandomNumber(4);
         assertTrue(randomNumber.isMovable());
     }
 
     @Test
     void isMovable_4_미만이면_거짓() {
-        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(3);
-
-        RandomNumber randomNumber = RandomNumber.createRandomNumber();
-
+        RandomNumber randomNumber = new RandomNumber(3);
         assertFalse(randomNumber.isMovable());
     }
 
