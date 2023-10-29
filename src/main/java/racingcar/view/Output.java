3b063a43bb4462b7367f2aeb54239872abf24624
ts3083 <ts04031@gmail.com>
2023-10-29 17:28:07 +0900

    feat: 게임 결과 최종 우승자 출력

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,10 +1,17 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class Output {
 
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String GAME_WINNER = "최종 우승자 : ";
 
     public void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printGameWinner(List<String> winners) {
+        System.out.println(GAME_WINNER + String.join(", ", winners));
     }
 }
