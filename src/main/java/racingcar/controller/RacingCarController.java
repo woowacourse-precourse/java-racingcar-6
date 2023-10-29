75cee61737bc19660267aa5c811b3a4dc6a92053
tam31 <wxogud3377@naver.com>
2023-10-29 16:40:35 +0900

    Change(RacingCarController) : 입력받은 자동차이름 대입

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,15 +1,26 @@
 package racingcar.controller;
 
 import racingcar.model.Car;
+import racingcar.model.Cars;
 import racingcar.view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarController {
+    Cars cars;
     InputView inputView = new InputView();
-    Car car = new Car();
 
     public void run() {
-        car.inputCars(inputView.carName());
-        System.out.println(car.getMovingCars());
-        System.out.println(inputView.playNumber());
+        List<Car> collectCars = new ArrayList<>();
+        String[] carNames = inputView.carName();
+        for (String carName : carNames) {
+            collectCars.add(new Car(carName));
+        }
+        cars = new Cars(collectCars);
+
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getCarName()+" "+car.getPosition());
+        }
     }
 }
