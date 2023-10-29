0d3159a9b8c1a249603bbc6f4b8c6abc1560a7b7
hayannn <dlgkdis801@naver.com>
2023-10-29 16:55:12 +0900

    feat(TryCountNumberException): 시도 횟수 예외 처리 중 "[E2] 숫자가 아닌 문자인 경우 체크" 기능 구현

--- src/main/java/racingcar/Exception/TryCountNumberException.java
+++ src/main/java/racingcar/Exception/TryCountNumberException.java
@@ -3,10 +3,27 @@ package racingcar.Exception;
 public class TryCountNumberException {
     public void validateTryCountInput(String tryCountNumInput) throws IllegalArgumentException {
         checkTryCountInputIsEmpty(tryCountNumInput);
+        checkTryCountIsCharacter(tryCountNumInput);
     }
+
     private void checkTryCountInputIsEmpty(String tryCountNumInput) {
         if (tryCountNumInput.isEmpty()) {
             throw new IllegalArgumentException("값을 입력해주세요.");
+        }
+    }
+
+    private void checkTryCountIsCharacter(String tryCountNumInput) {
+        if (!IntegerTypeInput(tryCountNumInput)) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자여야 합니다.");
+        }
+    }
+
+    private boolean IntegerTypeInput(String tryCountNumInput) {
+        try {
+            Integer.parseInt(tryCountNumInput);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
