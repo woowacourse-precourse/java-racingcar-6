16126909714b822f81a4efaab2fc0bcfa008d5ed
Jongwoo Han <jongwooo.han@gmail.com>
2023-10-30 02:01:07 +0900

    feat: implement the RandomNumberGenerator class

---
+++ src/main/java/racingcar/domain/generator/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.domain.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+  private static final int MINIMUM_NUMBER = 0;
+  private static final int MAXIMUM_NUMBER = 9;
+
+  @Override
+  public int generate() {
+    return Randoms.pickNumberInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER);
+  }
+}
