128a7417a5ed2204087646207d3bda84dd6f34bc
JeongMin <ajhappy12@gamil.com>
2023-10-29 14:49:55 +0900

    feat: 생성자 및 name validation 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,8 +1,18 @@
 package racingcar;
 
 public class RacingCar {
+    private final String name;
     private int moveCount;
-    private String name;
 
-    public RacingCar(String name) {}
+    public RacingCar(String name) {
+        validationCarName(name);
+        this.name = name;
+        this.moveCount = 0;
+    }
+
+    private void validationCarName(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("차량 이름은 1~5의 글자 수만 허용 됩니다.");
+        }
+    }
 }
