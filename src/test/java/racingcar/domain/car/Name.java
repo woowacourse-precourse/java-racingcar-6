15f3af13765a6e9300dc4da923157f03d92eebaf
jae woo Jung <awfjol2008@naver.com>
2023-10-30 02:50:22 +0900

    feat: Name 클래스 생성자에 자동차 이름 검증 로직 추가

--- src/test/java/racingcar/domain/car/Name.java
+++ src/test/java/racingcar/domain/car/Name.java
@@ -1,9 +1,12 @@
 package racingcar.domain.car;
 
+import racingcar.validator.CarNameValidator;
+
 public class Name {
     private final String name;
 
     public Name(String name) {
+        CarNameValidator.validateForEachName(name);
         this.name = name;
     }
 }
