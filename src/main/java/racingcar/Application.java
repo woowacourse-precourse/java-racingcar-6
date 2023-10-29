cc08d37c85f298314501a3686d02574f0689e635
songyi <song21677@naver.com>
2023-10-29 19:57:02 +0900

    feat: 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,6 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
     }
 }
