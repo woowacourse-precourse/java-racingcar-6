89056cefd92e28c24365bd29ee5b9afb21c60566
wonjun <wonjon0930@daum.net>
2023-10-29 22:00:31 +0900

    refactor: rename class ErrorMessage to CarNamesInputErrorMessage

--- src/main/java/racingcar/validator/CarNamesInputValidator.java
+++ src/main/java/racingcar/validator/CarNamesInputValidator.java
@@ -1,7 +1,7 @@
 package racingcar.validator;
 
+import racingcar.constant.CarNamesInputErrorMessage;
 import racingcar.constant.Constant;
-import racingcar.constant.ErrorMessage;
 
 public class CarNamesInputValidator {
     public void validate(String userInput) {
@@ -12,13 +12,13 @@ public class CarNamesInputValidator {
 
     private void isNotEmpty(String userInput) {
         if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_CAR_NAME_EMPTY);
+            throw new IllegalArgumentException(CarNamesInputErrorMessage.INPUT_CAR_NAME_EMPTY);
         }
     }
 
     private void containsDelimiter(String userInput) {
         if (!userInput.contains(Constant.INPUT_CAR_NAMES_DELIMITER)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_CAR_NAMES_NOT_CONTAIN_DELIMITER);
+            throw new IllegalArgumentException(CarNamesInputErrorMessage.INPUT_CAR_NAMES_NOT_CONTAIN_DELIMITER);
         }
     }
 
@@ -28,10 +28,10 @@ public class CarNamesInputValidator {
         for (String carName : carNames) {
             trimmedCarName = carName.trim();
             if (trimmedCarName.length() > Constant.CAR_NAME_MAX_LENGTH) {
-                throw new IllegalArgumentException(ErrorMessage.INPUT_CAR_NAME_LENGTH_EXCEEDS_LIMIT);
+                throw new IllegalArgumentException(CarNamesInputErrorMessage.INPUT_CAR_NAME_LENGTH_EXCEEDS_LIMIT);
             }
             if (trimmedCarName.isEmpty()) {
-                throw new IllegalArgumentException(ErrorMessage.INPUT_CAR_NAME_EMPTY);
+                throw new IllegalArgumentException(CarNamesInputErrorMessage.INPUT_CAR_NAME_EMPTY);
             }
         }
     }
