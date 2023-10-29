14eb51b1ebf9a0ab1cbadce9049c19684337d4be
Oseuseu <lakingsung@gmail.com>
2023-10-30 08:07:32 +0900

    refactor: Cars 클래스 구현
    CarList 클래스의 기능들을 구현
    + 기존 게임 진행상황 출력을 위해 List<Car>을 인수로 받던 방법에서
    getCarsStatus 메서드로 상태를 Map<String, Integer> 형태로 반환하도록 변경

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,58 @@
+package racingcar;
+
+import java.util.*;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> carList) {
+        validateCarNamesUnique(carList);
+        this.cars = carList;
+    }
+
+    public void moveRandomEachCars() {
+        for (Car car : cars) {
+            int power = RacingUtil.generateRandomPower();
+            car.move(power);
+        }
+    }
+
+    public Map<String, Integer> getCarsStatus() {
+        Map<String, Integer> status = new HashMap<>();
+        for (Car car : cars) {
+            status.put(car.getName(), car.getPosition());
+        }
+        return status;
+    }
+
+    public int findMaxPosition() {
+        int max = cars.get(0).getPosition();
+        for (Car car : cars) {
+            if (max < car.getPosition()) {
+                max = car.getPosition();
+            }
+        }
+        return max;
+    }
+
+    public List<String> findMaxPositionCarNames() {
+        List<String> names = new ArrayList<>();
+        int max = findMaxPosition();
+        for (Car car : cars) {
+            if (car.getPosition() == max) {
+                names.add(car.getName());
+            }
+        }
+        return names;
+    }
+
+    private void validateCarNamesUnique(List<Car> carList) {
+        Set<String> seen = new HashSet<>();
+        for (Car car : carList) {
+            if (!seen.add(car.getName())) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+}
