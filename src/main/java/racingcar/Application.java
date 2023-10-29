c8458f3a27fb19e04ae7dd4c0da171c83fdd7c7e
박찬건 <jg9870@naver.com>
2023-10-29 23:12:44 +0900

    refactor(racingcar): 테스트하기 유용하도록 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<Car> carNames = InputView.getCars();
-        int racingRound = InputView.getRacingRound();
+        List<Car> carNames = InputFactory.getCars(Console.readLine());
+        int racingRound = InputFactory.getRacingRound(Console.readLine());
 
         Race race = new Race(carNames, racingRound);
         race.start();
