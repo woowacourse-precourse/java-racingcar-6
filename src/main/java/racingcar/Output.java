10d4d5bf3f72e36b3de06fe198410be459359691
happyryan <tjdgks120813@gmail.com>
2023-10-29 21:00:36 +0900

    feat(racingcar Output): add printPlayersStatus method to the Output class
    Added the printPlayersStatus method to the Output class for displaying the status of all players during the game.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,29 +1,43 @@
 package racingcar;
 
-import utils.JoinComma;
+import player.Player;
+import utils.StringUtils;
 
 import java.util.*;
 
 final class Output {
     private Output() {
     }
+
     static void printGameStartMessage() {
         System.out.println(GameMessage.GAME_START.message);
     }
+
     static void printPlayersName(List<String> players) {
-        System.out.println(JoinComma.playerInput(players));
+        System.out.println(StringUtils.joinWithoutBlank(players));
     }
+
     static void printTryCountMessage() {
         System.out.println(GameMessage.TRY_COUNT.message);
     }
+
     static void printTryCount(String moveNumber) {
         System.out.println(moveNumber + "\n");
     }
-    static void printRaceCar(String playerName, int distance){
+
+    static void printPlayerStatus(String playerName, int distance) {
         System.out.println(playerName + " : " + "-".repeat(distance));
     }
+
+    static void printPlayersStatus(List<Player> players) {
+        for (Player player : players) {
+            Output.printPlayerStatus(player.getPlayerName(), player.getDistance());
+        }
+        System.out.println();
+    }
+
     static void printFinalWinner(List<String> winners) {
         System.out.print(GameMessage.FINAL_WINNER.message);
-        System.out.println(JoinComma.winnerOutput(winners));
+        System.out.println(StringUtils.joinWithBlank(winners));
     }
 }
