651c29045a6770c5c43881698d323a15d8df4c4b
eunseob <seobseob0810@naver.com>
2023-10-29 20:49:35 +0900

    fix(User): 메소드 이름 리팩터링

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -7,23 +7,24 @@ import java.util.List;
 public class User {
     Validation validation = new Validation();
 
-    public List<String> GiveCarName() {
+    public List<String> giveCarName() {
         List<String> carNames;
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInsert = Console.readLine();
         carNames = Arrays.asList(userInsert.split(","));
-        if (!validation.isLessThanSix(carNames)) {
-            throw new IllegalArgumentException("5글자 이하로 입력해주세요.");
+
+        if (validation.isValidName(carNames)) {
+            return carNames;
         }
-        return carNames;
+        throw new IllegalArgumentException("게임이 종료됩니다.");
     }
 
-    public int NumberOfRepeats() {
+    public Long numberOfRepeats() {
         System.out.println("시도할 횟수는 몇회인가요?");
         String input = Console.readLine();
-        int number;
-        if (validation.isNumber(input)) {
-            number = Integer.parseInt(input);
+        Long number;
+        if (validation.isValidNumber(input)) {
+            number = Long.parseLong(input);
             return number;
         }
         throw new IllegalArgumentException("횟수 입력이 잘못되었습니다");
