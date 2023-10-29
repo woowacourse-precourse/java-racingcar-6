b4298a7818e1d5768732eaff8d9d3eeb5c4787fe
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-29 18:31:10 +0900

    FEAT / modify to Singlethon

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
 public class InputView {
+    public InputView() {
+    }
+
+    public static InputView getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final InputView INSTANCE = new InputView();
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,15 @@
 package racingcar.view;
 
 public class OutputView {
+    public OutputView() {
+    }
+
+    public static OutputView getInstance() {
+
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final OutputView INSTANCE = new OutputView();
+    }
 }
