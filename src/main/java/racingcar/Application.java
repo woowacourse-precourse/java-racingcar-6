903e4acb3058b42302b4ac6e4e1e1b56ec641513
WOONBE <kepull2918@naver.com>
2023-10-29 16:46:12 +0900

    [feat] 프로그램 실행을 담당하는 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.PlayRacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayRacingGame playRacingGame = new PlayRacingGame();
+        playRacingGame.racingGame();
     }
 }
