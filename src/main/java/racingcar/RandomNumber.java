893e4601c885b9c03b73068717914861004bc8dd
aammddkkzxc <ideal0427@naver.com>
2023-10-29 21:08:32 +0900

    feat: 4 이상의 수 판별 기능 구현

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -5,12 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumber {
     public static final int RANDOM_NUMBER_MIN_VALUE = 0;
     public static final int RANDOM_NUMBER_MAX_VALUE = 9;
+    public static final int REFERENCE_NUMBER_VALUE = 4;
 
     public int generate() {
         return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_VALUE, RANDOM_NUMBER_MAX_VALUE);
     }
 
-    public void judge() {
-
+    public boolean judge(int number) {
+        return number >= REFERENCE_NUMBER_VALUE;
     }
 }
