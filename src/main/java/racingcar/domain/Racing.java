e21df71cfa30db5eb5547cd8122ba7cfd4d28a9a
dsadara <chaehyun.forest@gmail.com>
2023-10-29 13:21:42 +0900

    feat: 레이싱 생성 정적 메소드 작성

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +31,19 @@ public class Racing {
     }
 
 
-    public static void createRacingWithInput() {
+    public static Racing createRacingWithInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carArr = Console.readLine().split(",");
 
+        System.out.println("시도할 회수는 몇회인가요?");
+        int turn = Integer.valueOf(Console.readLine());
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : carArr) {
+            cars.add(new Car(name));
+        }
+
+        return new Racing(cars, turn);
     }
 
     private static void validateCreateRacingInput() {
