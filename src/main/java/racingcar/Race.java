3891009d0ca14aac83f50ddaca1adc2155f54efc
Happeeee <cmg118@naver.com>
2023-10-30 04:22:26 +0900

    Feat: 레이스 흐름 제어 기능 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,19 @@
 package racingcar;
 
-public class Race {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Race {
+    private final List<Car> carList = new ArrayList<>();
+    private final List<String> carNameList;
+    private final int tryCount;
+
+    public Race(List<String> carNameList, int tryCount) {
+        this.carNameList = carNameList;
+        this.tryCount = tryCount;
+    }
+
+    public void start() {
+        return;
+    }
 }
