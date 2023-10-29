0cd44f1828e273731e21d97e986b856dd61328b0
Annjieun <dkslwdms2121@naver.com>
2023-10-29 16:28:02 +0900

    test(ValidationTest): 시도 횟수 예외 처리 테스트 추가

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -2,14 +2,11 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import racingcar.constant.ExceptionMessage;
 import racingcar.domain.Attempt;
 import racingcar.domain.CarsFactory;
 
@@ -34,34 +31,10 @@ public class GameTest {
     }
 
     @Test
-    void 자동차_이름_검증하기() {
-        List<String> carNames1 = Arrays.asList("pobi", "woni", "jun");
-        List<String> carNames2 = Arrays.asList("pobi", "woni", "");
-
-        assertThatCode(() -> carsFactory.validateCarNames(carNames1)).doesNotThrowAnyException();
-        assertThatThrownBy(() ->
-                carsFactory.validateCarNames(carNames2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ExceptionMessage.INVALID_NAME_EXCEPTION);
-    }
-
-    @Test
     void 시도_횟수_입력받아_저장하기() {
         command("5");
 
         assertThatCode(() -> attempt.saveAttemptNumber()).doesNotThrowAnyException();
-    }
-
-    @Test
-    void 시도_횟수_검증하기() {
-        String attemptNumber1 = "5";
-        String attemptNumber2 = "0";
-
-        assertThatCode(() -> attempt.validateAttemptNumber(attemptNumber1)).doesNotThrowAnyException();
-        assertThatThrownBy(() ->
-                attempt.validateAttemptNumber(attemptNumber2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ExceptionMessage.NON_POSITIVE_NUMBER_EXCEPTION);
     }
 
     private void command(final String... args) {
--- src/test/java/domain/ValidationTest.java
+++ src/test/java/domain/ValidationTest.java
@@ -1,13 +1,36 @@
 package domain;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
+import racingcar.constant.ExceptionMessage;
+import racingcar.domain.Attempt;
+import racingcar.domain.CarsFactory;
 
 public class ValidationTest extends NsTest {
+    private CarsFactory carsFactory;
+    private Attempt attempt;
+
+    @BeforeEach
+    public void setUp() {
+        carsFactory = new CarsFactory();
+        attempt = new Attempt();
+    }
+
+    @Test
+    void 자동차_이름_검증하기() {
+        List<String> carNames1 = Arrays.asList("pobi", "woni", "jun");
+
+        assertThatCode(() -> carsFactory.validateCarNames(carNames1)).doesNotThrowAnyException();
+    }
+
     @Test
     void 이름_예외_처리_빈_문자열() {
         assertSimpleTest(() ->
@@ -49,14 +72,48 @@ public class ValidationTest extends NsTest {
     }
 
     @Test
+    void 시도횟수_검증() {
+        String attemptNumber = "5";
+
+        assertThatCode(() -> attempt.validateAttemptNumber(attemptNumber)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 시도횟수_예외_처리_숫자가_아닌_문자() {
+        String attemptNumber = "a";
+
+        assertThatThrownBy(() ->
+                attempt.validateAttemptNumber(attemptNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NON_INTEGER_EXCEPTION);
+    }
+
+    @Test
+    void 시도횟수_예외_처리_소수() {
+        String attemptNumber = "5.2";
+
+        assertThatThrownBy(() ->
+                attempt.validateAttemptNumber(attemptNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NON_INTEGER_EXCEPTION);
+    }
+
+    @Test
+    void 시도횟수_예외_처리_0이하의_수() {
+        String attemptNumber = "-5";
+
+        assertThatThrownBy(() ->
+                attempt.validateAttemptNumber(attemptNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NON_POSITIVE_NUMBER_EXCEPTION);
+    }
+
+    @Test
     void 숫자_예외_처리_공백() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException(" "))
                         .isInstanceOf(IllegalArgumentException.class));
     }
-
-
-
 
     @Override
     public void runMain() {
