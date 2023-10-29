b40ceeb9265a91f0efe395013167b0353925f78b
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-29 15:16:38 +0900

    feat: 공백 입력에 대한 검사 기능 구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
 public class Validator {
     public static void nameLengthCheck(String name) {
@@ -15,6 +16,12 @@ public class Validator {
         HashSet<String> deduplicationSet = new HashSet<>(names);
         if (initLength != deduplicationSet.size()) {
             throw new IllegalArgumentException("중복되지 않은 이름 입력");
+        }
+    }
+
+    public static void blankCheck(String input) {
+        if (Objects.equals(input, "")) {
+            throw new IllegalArgumentException("공백 입력");
         }
     }
 }
