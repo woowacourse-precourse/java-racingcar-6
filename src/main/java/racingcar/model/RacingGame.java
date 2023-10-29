af256e3994e84b8acc61949a59a070b76c658eee
sinminseok12 <sinminseok12@naver.com>
2023-10-30 00:57:50 +0900

    feat(RacingGame): 레이싱에 포함된 자동차들 중 우승 자동차 이름을 찾는 기능 구현

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -21,4 +21,8 @@ public class RacingGame {
     public boolean isPlayable() {
         return count.isAbleTry();
     }
+
+    public List<String> findWinCars() {
+        return cars.findWinnerNames();
+    }
 }
