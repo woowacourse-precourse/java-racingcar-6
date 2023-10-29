b6c6434fd704594f5e9f5c2975715dcc30243138
IMWoo94 <leemeawoo94@gmail.com>
2023-10-29 15:04:18 +0900

    test : 게임문구, 에러메시지 테스트 AssertAll 로 그룹화 테스트로 변경
    게임문구, 에러메시지 테스트 AssertAll 로 그룹화 테스트로 변경

--- src/test/java/racingcar/constants/ExceptionCommentsTest.java
+++ src/test/java/racingcar/constants/ExceptionCommentsTest.java
@@ -1,6 +1,7 @@
 package racingcar.constants;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,16 @@ class ExceptionCommentsTest {
     @Test
     @DisplayName("예외 문구 테스트")
     void ExceptionCommentsTest() {
-        assertThat(ExceptionComments.INPUT_NULL_VALUE_COMMENT).isEqualTo("NULL 이 입력 되었습니다.");
-        assertThat(ExceptionComments.INPUT_EMPTY_VALUE_COMMENT).isEqualTo("빈값 이 입력 되었습니다.");
-        assertThat(ExceptionComments.INPUT_VALUE_SIZE_OVER).isEqualTo(
-                "이름의 크기가 " + Config.CAR_NAME_SIZE + "를 초과 하였습니다.");
-        assertThat(ExceptionComments.INPUT_TYPE_NON_NUMBER).isEqualTo("숫자가 아닙니다.");
-        assertThat(ExceptionComments.INPUT_VALUE_INCORRECT_COMMENT).isEqualTo("잘못된 값이 입력되었습니다.");
-        assertThat(ExceptionComments.INPUT_CAR_NAME_DUPLICATED).isEqualTo("중복 자동차 이름이 입력되었습니다.");
+        assertAll(
+                "예외 문구 테스트",
+                () -> assertThat(ExceptionComments.INPUT_NULL_VALUE_COMMENT).isEqualTo("NULL 이 입력 되었습니다."),
+                () -> assertThat(ExceptionComments.INPUT_EMPTY_VALUE_COMMENT).isEqualTo("빈값 이 입력 되었습니다."),
+                () -> assertThat(ExceptionComments.INPUT_VALUE_SIZE_OVER).isEqualTo(
+                        "이름의 크기가 " + Config.CAR_NAME_SIZE + "를 초과 하였습니다."),
+                () -> assertThat(ExceptionComments.INPUT_TYPE_NON_NUMBER).isEqualTo("숫자가 아닙니다."),
+                () -> assertThat(ExceptionComments.INPUT_VALUE_INCORRECT_COMMENT).isEqualTo("잘못된 값이 입력되었습니다."),
+                () -> assertThat(ExceptionComments.INPUT_CAR_NAME_DUPLICATED).isEqualTo("중복 자동차 이름이 입력되었습니다.")
+        );
     }
 
 }
--- src/test/java/racingcar/constants/RacingCarGameCommentsTest.java
+++ src/test/java/racingcar/constants/RacingCarGameCommentsTest.java
@@ -1,6 +1,7 @@
 package racingcar.constants;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,14 @@ class RacingCarGameCommentsTest {
     @Test
     @DisplayName("자동차 경주 게임 문구 테스트")
     void racingCarGameCommentsTest() {
-        assertThat(RacingCarGameComments.OPENING_COMMENT).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        assertThat(RacingCarGameComments.TRY_COMMENT).isEqualTo("시도할 회수는 몇회인가요?");
-        assertThat(RacingCarGameComments.RESULT_COMMENT).isEqualTo("실행 결과");
-        assertThat(RacingCarGameComments.WINNER_COMMENT).isEqualTo("최종 우승자 : ");
+        assertAll(
+                "자동차 경주 게임 문구 테스트",
+                () -> assertThat(RacingCarGameComments.OPENING_COMMENT).isEqualTo(
+                        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+                () -> assertThat(RacingCarGameComments.TRY_COMMENT).isEqualTo("시도할 회수는 몇회인가요?"),
+                () -> assertThat(RacingCarGameComments.RESULT_COMMENT).isEqualTo("실행 결과"),
+                () -> assertThat(RacingCarGameComments.WINNER_COMMENT).isEqualTo("최종 우승자 : ")
+        );
     }
 
 }
