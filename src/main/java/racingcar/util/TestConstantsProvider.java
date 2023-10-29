390ca31fa3f90ea83b1463b1e3cd49b5b5fe9901
leesh <qwqw8019@naver.com>
2023-10-29 20:39:54 +0900

    test: 시도 회수 입력 기능 테스트 추가

--- src/main/java/racingcar/util/TestConstantsProvider.java
+++ src/main/java/racingcar/util/TestConstantsProvider.java
@@ -9,6 +9,7 @@ import static racingcar.constant.TestConstants.EXPECTED_PLAY_FIVE_TIMES;
 import static racingcar.constant.TestConstants.EXPECTED_PLAY_ONE_TIME;
 import static racingcar.constant.TestConstants.EXPECTED_THREE_CAR_NAMES;
 import static racingcar.constant.TestConstants.LONGER_THAN_FIVE_STRING;
+import static racingcar.constant.TestConstants.NOT_NUMBER_STRING;
 import static racingcar.constant.TestConstants.NULL_STRING;
 import static racingcar.constant.TestConstants.ONE_CAR_NAME;
 import static racingcar.constant.TestConstants.PLAY_FIVE_TIMES;
@@ -48,8 +49,12 @@ public class TestConstantsProvider {
 
     public static Stream<Arguments> provideInvalidPlayNumberConstants() {
         return Stream.of(
+                arguments(named("null", NULL_STRING)),
+                arguments(named("빈 문자열", EMPTY_STRING)),
+                arguments(named("공백 문자열", BLANK_STRING)),
                 arguments(named("0", PLAY_NO_TIME)),
-                arguments(named("0보다 작은 값", PLAY_NEGATIVE_TIME))
+                arguments(named("0보다 작은 값", PLAY_NEGATIVE_TIME)),
+                arguments(named("숫자가 아닌 값", NOT_NUMBER_STRING))
         );
     }
 
