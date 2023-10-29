ae9bf591724524fb2034e737cda9a8e1fc44c02f
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-29 23:17:43 +0900

    refactor(inputView): displayPlayCountMessage 메서드 수정

--- src/main/java/racingcar/View/inputView.java
+++ src/main/java/racingcar/View/inputView.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class inputView {
     private static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_PLAY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
     public String displayGameStartMessage(){
         System.out.println(INPUT_CARS_NAME_MESSAGE);
         return Console.readLine();
     }
 
-    public void displayPlayCountMessage(){
-        System.out.println("시도할 회수는 몇회인가요?");
+    public String displayPlayCountMessage(){
+        System.out.println(INPUT_PLAY_COUNT_MESSAGE);
+        return Console.readLine();
     }
 }
