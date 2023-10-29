8d9104a1e33be3406bb5eecf18c5bd4283bbcddb
suhyun9764 <suhyun9764@naver.com>
2023-10-29 22:34:14 +0900

    test(GetNameValidatorTest): 수정된 메서드에 맞게 테스트 코드 수정

--- src/test/java/racingcar/utils/validator/GetNameValidatorTest.java
+++ src/test/java/racingcar/utils/validator/GetNameValidatorTest.java
@@ -2,54 +2,20 @@ package racingcar.utils.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 class GetNameValidatorTest {
 
 
     @Test
-    void 이름_5글자_이상_예외처리() {
-        String input = "abc,asdfas,dfdf";
-
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("5");
-    }
-
-    @Test
     void 이름_공백_예외처리() {
         String input = "abc,asd fas,df df";
 
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
+        assertThatThrownBy(() -> GetNameValidator.validateStringNames(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("공백");
-    }
-
-    @Test
-    void 이름_중복_예외처리() {
-        String input = "abc,abc,dfdf";
-
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("중복");
-    }
-
-    @Test
-    void 이름_2개이하_예외처리() {
-        String input = "abc";
-
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("2개");
-    }
-
-    @Test
-    void 이름_입력없음_예외처리() {
-        String input = "abc";
-
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("2개");
     }
 
     @Test
@@ -57,25 +23,63 @@ class GetNameValidatorTest {
         String input1 = "abc,";
         String input2 = ",abc";
 
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input1))
+        assertThatThrownBy(() -> GetNameValidator.validateStringNames(input1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("위치");
 
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input2))
+        assertThatThrownBy(() -> GetNameValidator.validateStringNames(input2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("위치");
 
 
     }
+
     @Test
     void 이름_지정되지_않은문자_예외처리() {
         String input = "@#ab,2322,!@312";
 
-        assertThatThrownBy(() -> GetNameValidator.validateCarsName(input))
+        assertThatThrownBy(() -> GetNameValidator.validateStringNames(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("문자, 숫자");
     }
 
+    @Test
+    void 이름_5글자_이상_예외처리() {
+        String[] stringArray = {"abc","asdfas","dfdf"};
+        ArrayList<String> stringArrayList = Lists.newArrayList(stringArray);
+
+        assertThatThrownBy(() -> GetNameValidator.validateArrayListNames(stringArrayList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5");
+    }
+
+    @Test
+    void 이름_중복_예외처리() {
+        String[] stringArray = {"abc","abc","dfdf"};
+        ArrayList<String> stringArrayList = Lists.newArrayList(stringArray);
+        assertThatThrownBy(() -> GetNameValidator.validateArrayListNames(stringArrayList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복");
+    }
+
+    @Test
+    void 이름_2개이하_예외처리() {
+        String[] stringArray = {"abc"};
+        ArrayList<String> stringArrayList = Lists.newArrayList(stringArray);
+        assertThatThrownBy(() -> GetNameValidator.validateArrayListNames(stringArrayList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("2개");
+    }
+
+    @Test
+    void 이름_입력없음_예외처리() {
+        String[] stringArray = {""};
+        ArrayList<String> stringArrayList = Lists.newArrayList(stringArray);
+
+        assertThatThrownBy(() -> GetNameValidator.validateArrayListNames(stringArrayList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("2개");
+    }
 
 
 }
