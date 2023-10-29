dcdb9e3e9983a03e9872d3db7e8df18fe86a58bd
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 23:31:37 +0900

    feat: isRandomForwardMove 메소드 작성
    random 값을 생성하고 random 값이 4이상일 시 true, 아닐 시 false를 반환하는 isRandomForwardMove 메소드를 작성했습니다.

--- src/main/java/racingcar/utils/RacingCarGame.java
+++ src/main/java/racingcar/utils/RacingCarGame.java
@@ -1,10 +1,17 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCarGame {
     private static int farthestDistance;
 
     public RacingCarGame() {
         farthestDistance = 0;
+    }
+
+    private static boolean isRandomForwardMove() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        return randomValue >= 4;
     }
 
 }
