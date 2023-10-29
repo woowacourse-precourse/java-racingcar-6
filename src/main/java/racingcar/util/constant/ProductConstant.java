b9555be8be3a7273047153c1f772d7951760b28c
Hayoon <hy97@sju.ac.kr>
2023-10-29 16:32:41 +0900

    feat(ProductConstant): ProductCode에서 String 값을 상수로 선언한다

--- src/main/java/racingcar/util/constant/ProductConstant.java
+++ src/main/java/racingcar/util/constant/ProductConstant.java
@@ -1,4 +1,7 @@
 package racingcar.util.constant;
 
 public class ProductConstant {
+    public static String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+    public static String GAME_RESULT = "실행 결과";
 }
