a1907ac00b9646524ecbceb811e58af2b818dbef
minisyu <iswater.kim@gmail.com>
2023-10-29 20:46:42 +0900

    refactor(Application): 1차 리팩토링 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,21 +2,23 @@ package racingcar;
 
 import racingcar.controller.GameController;
 import racingcar.converter.InputConverter;
+import racingcar.domain.game.move.RandomMoveStrategy;
+import racingcar.domain.game.move.RandomNumberGenerator;
 import racingcar.util.InputValidator;
-import racingcar.util.NumberPicker;
-import racingcar.util.RandomNumberPicker;
+import racingcar.util.MoveStrategy;
+import racingcar.util.NumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         InputValidator inputValidator = new InputValidator();
         InputConverter inputConverter = new InputConverter();
         InputView inputView = new InputView(inputValidator, inputConverter);
         OutputView outputView = new OutputView();
-        NumberPicker randomNumberPicker = new RandomNumberPicker(0, 9);
-        GameController gameController = new GameController(inputView, outputView, randomNumberPicker);
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator(0, 9);
+        MoveStrategy randomMoveStrategy = new RandomMoveStrategy(randomNumberGenerator);
+        GameController gameController = new GameController(inputView, outputView, randomMoveStrategy);
         gameController.run();
     }
 }
