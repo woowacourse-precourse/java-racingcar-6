9432629926b3c3eb02bc5349bba5f445c12a8250
devpanic <yeon.dev.8@gmail.com>
2023-10-29 12:36:50 +0900

    feat(movingCount) : Add movingCount to Car Object 1. for Getting Winner 2. for Getting Moving Path

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,18 +3,21 @@ package racingcar;
 public class Car {
     private String carName;
     private String carPath;
+    private int movingCount;
     public void makeCar(String carName){
         this.carName = carName;
         this.carPath = "";
+        this.movingCount = 0;
     }
     public void movingCar(){
-        carPath += "-";
+        this.carPath += "-";
+        this.movingCount++;
     }
     public String getCarName(){
-        return carName;
+        return this.carName;
     }
 
     public String getCarPath(){
-        return carPath;
+        return this.carPath;
     }
 }
