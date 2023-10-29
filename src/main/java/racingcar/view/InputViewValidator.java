f90fdfe55dbbe14770fc75d816682287408a832b
maark1106 <mark1106@naver.com>
2023-10-29 14:57:30 +0900

    feat(InputViewValidator) : 차 이름의 길이가 5보다 큰 경우 검증하는 기능 구현

--- src/main/java/racingcar/view/InputViewValidator.java
+++ src/main/java/racingcar/view/InputViewValidator.java
@@ -15,6 +15,20 @@ public class InputViewValidator {
         if(isCarNameEmpty(carNames)){
             throw new IllegalArgumentException("[ERROR] : 차 이름이 빈 값일 수 없습니다");
         }
+        if(isCarNameCorrectLength(carNames)){
+            throw new IllegalArgumentException("[ERROR] : 차 이름은 길이가 5이하여야 합니다.");
+        }
+    }
+
+    public boolean isCarNameCorrectLength(String carNames) {
+        String[] carNameList = carNames.split("");
+
+        for (String carName : carNameList) {
+            if(carName.length() > 5){
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean isCarNameEmpty(String carNames) {
