be280e448dec7ab0f838d5b377474148ecb9e246
unknown <y2kf9a0q@gmail.com>
2023-10-29 14:21:38 +0900

    test: LogsTest.java - inputCarNames()

--- src/test/java/racingcar/domain/LogsTest.java
+++ src/test/java/racingcar/domain/LogsTest.java
@@ -3,16 +3,25 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LogsTest {
+    OutputStream out;
 
     @BeforeEach
     void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
     }
 
     @Test
     void inputCarNames() {
+        String log = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" + System.lineSeparator();
+        Logs.inputCarNames();
+        assertThat(out.toString()).isEqualTo(log);
     }
 
     @Test
