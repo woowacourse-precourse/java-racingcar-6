4ab063ea89afb119873ef3845ef5fb3c1210ed2a
Hoo-sung <gntjd135@gmail.com>
2023-10-29 22:14:58 +0900

    test(RandomNumberGeneratorTest): 0과 9 포함해 랜덤값 반환하는지 테스트하는 케이스

---
+++ src/test/java/racingcar/domain/RandomNumberGeneratorTest.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class RandomNumberGeneratorTest {
+
+    private  final int START_INCLUSIVE = 0;
+    private final int MIDDLE_NUM = 4;
+    private  final int END_INCLUSIVE = 9;
+
+    @Test
+    @DisplayName("0과 9포함한 랜덤값으로 1자리르 구성하는지 체크")
+    void is_singel_digit_random_value() {
+        assertRandomNumberInRangeTest(
+                ()-> {
+                    assertThat(RandomNumberGenerator.randomNumGenerate())
+                            .isEqualTo(START_INCLUSIVE);
+                    assertThat(RandomNumberGenerator.randomNumGenerate())
+                            .isEqualTo(MIDDLE_NUM);
+                    assertThat(RandomNumberGenerator.randomNumGenerate())
+                            .isEqualTo(END_INCLUSIVE);
+                },
+                START_INCLUSIVE,MIDDLE_NUM,END_INCLUSIVE
+        );
+    }
+}
