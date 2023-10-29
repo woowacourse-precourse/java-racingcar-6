f05cd9027023495acf3102baceef12eae6dc7d42
pjaehyun <show6756@gmail.com>
2023-10-29 15:03:58 +0900

    feat: 수정 CarCollection 추가로 인해 Game Class 자동차 관리 함수 제거 및 참가자 생성함수 수정

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -12,17 +12,18 @@ import static constants.RacingCarConstants.PARTICIPANTS_INPUT_MESSAGE;
 
 public class Game {
 
-    private List<Car> participants;
+    private CarCollection participants;
     private int frequency;
 
     public Game() {
+        participants = new CarCollection();
         initializeParticipants();
         initializeFrequency();
     }
 
     private void initializeParticipants() {
         String participantsInput = InputUtil.getUserInput(PARTICIPANTS_INPUT_MESSAGE);
-        participants = stringToCarList(participantsInput);
+        addCarsFromInput(participantsInput);
     }
 
     private void initializeFrequency() {
@@ -34,68 +35,27 @@ public class Game {
     public void start(){
 
         for (int i = 0; i < frequency; i++){
-            moveCarsForward(participants);
-            printCarsDistance(participants);
+            participants.moveCarsForward();
+            participants.printCarsDistance();
         }
 
-        int maxDistance = getMaxDistance(participants);
+        List<String> winners = participants.getWinners();
 
-        List<String> winners = getWinners(participants, maxDistance);
-
-        printWinners(winners);
+        participants.printWinners(winners);
     }
 
-    public void moveCarsForward(List<Car> participants) {
-        for (Car car : participants) {
-            car.movingForward();
+    private void addCarsFromInput(String input) {
+        String[] data = input.split(",");
+
+        for (String name : data) {
+            participants.addCar(new Car(name));
         }
     }
 
-    public void printCarsDistance(List<Car> participants) {
-        for (Car car : participants) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
-        }
-        System.out.println();
-    }
-
-    public int getMaxDistance(List<Car> participants) {
-        int maxDistance = 0;
-
-        for (Car car : participants) {
-            maxDistance = Math.max(maxDistance, car.getDistance());
-        }
-        return maxDistance;
-    }
-
-    public List<String> getWinners(List<Car> participants, int maxDistance) {
-        List<String> winners = new ArrayList<>();
-
-        for (Car car : participants) {
-            if (car.getDistance() >= maxDistance){
-                winners.add(car.getName());
-            }
-        }
-        return winners;
-    }
-
-
-    public void printWinners(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
-    }
-
-    public List<Car> stringToCarList(String data) {
-        List<Car> res = new ArrayList<>();
-        String[] splitData = data.split(",");
-
-        for(String name: splitData) {
-            Car car = new Car(name);
-            res.add(car);
-        }
-        return res;
-    }
-
-    public int stringToInt(String data) {
+    private int stringToInt(String data) {
         return Integer.parseInt(data);
     }
+
+
 
 }
