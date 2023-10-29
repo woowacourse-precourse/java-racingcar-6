f29438e3b23d5ccb5ddea81e105b4e26651ce2de
Minjae-An <mj3242@naver.com>
2023-10-29 16:24:17 +0900

    feat : 불필요한 INVALID_INPUT_VALUE 삭제

--- src/main/java/racingcar/global/exception/ErrorMessage.java
+++ src/main/java/racingcar/global/exception/ErrorMessage.java
@@ -2,7 +2,6 @@ package racingcar.global.exception;
 
 public enum ErrorMessage {
     EMPTY_VALUE("빈 값을 입력할 수 없습니다."),
-    INVALID_INPUT_VALUE("잘못된 입력 형식입니다."),
     INVALID_LENGTH("차 이름은 5자를 초과할 수 없습니다."),
     NOT_A_NUMBER("단일 숫자를 입력해야 합니다."),
     INVALID_INPUT("이름은 영소문자, 숫자, 한글로 이루어질 수 있으며 ,로 구분되어야 합니다.");
