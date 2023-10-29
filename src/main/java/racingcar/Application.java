694811ba93d5a25c06345f9584fbc88016ccad52
Hyo Gyeong <cdkrcd8@gmail.com>
2023-10-29 19:18:10 +0900

    implement: input car and validation 1. get car name and split by comma 2. check input car validation 2-1. count car, if space, if duplicated, check length 3. return car array list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CheckValidation checkValidation = new CheckValidation();
+        ArrayList<String> carNameList = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        carNameList = checkValidation.checkCarName(input);
+
     }
 }
---
+++ src/main/java/racingcar/CheckValidation.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+
+public class CheckValidation {
+
+    public static ArrayList<String> checkCarName(String input) {
+        ArrayList<String> carNameList = new ArrayList<>();
+        String[] carNameArr = input.split(",");
+
+        if (carNameArr.length > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("입력 개수 오류");
+        }
+        for (String carName : carNameArr) {
+            if (carName.contains(" ")) {
+                throw new IllegalArgumentException("공백 입력 오류");
+            }
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("입력값 길이 오류");
+            }
+            if (!carNameList.contains(carName)) {
+                carNameList.add(carName);
+            } else {
+                throw new IllegalArgumentException("중복 입력 오류");
+            }
+        }
+        return carNameList;
+    }
+}
