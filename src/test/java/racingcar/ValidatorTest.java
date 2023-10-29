52eca3e8d09b83821d4e2b1d6fc79c813c877d81
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 01:08:29 +0900

    test(Validator): 입력한 숫자가 정수인지 검증하는 함수 테스트

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,4 +1,34 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.Validator.*;
+
 class ValidatorTest {
-  
+
+    @Test
+    void 입력한_숫자가_정수인지_검증_성공_케이스() {
+        // given
+        String input = "7";
+
+        // when
+        boolean result = isInteger(input);
+
+        // then
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    void 입력한_숫자가_정수인지_검증_실패_케이스() {
+        // given
+        String input = "number";
+
+        // when
+        boolean result = isInteger(input);
+
+        // then
+        Assertions.assertThat(result).isFalse();
+    }
 }
