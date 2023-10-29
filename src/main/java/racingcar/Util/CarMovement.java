bbe9cbf369c96b603b839a01c75e330a798845f0
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-10-29 17:33:50 +0900

    refactor(CarMovement): movingStatus 메서드의 if문 간소화

--- src/main/java/racingcar/Util/CarMovement.java
+++ src/main/java/racingcar/Util/CarMovement.java
@@ -8,9 +8,6 @@ public class CarMovement {
     private static final int FORWARD_MIN_NUMBER = 4;
 
     public static boolean movingStatus(){
-        if (Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END) >= FORWARD_MIN_NUMBER){
-            return true;
-        }
-        return false;
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END) >= FORWARD_MIN_NUMBER;
     }
 }
