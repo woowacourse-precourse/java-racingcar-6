e0eeb55371555893a4c8d19ee2dc2677efd3f3bd
WooYoungDoo <duden5000@naver.com>
2023-10-29 14:38:53 +0900

    feat (InputValidator) : 차 이름 글자수 검증

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -3,11 +3,23 @@ package racingcar.validator;
 public class InputValidator {
 
     private static final String INPUT_NUMBER_CORRECT_TYPE = "[0-9]+";
+    private static final int INPUT_NAME_MAXIMUM_LENGTH = 5;
+
+    public void validateInputCarName(String inputCarName) {
+        if (isInputCarNameOverLength(inputCarName)) {
+            throw new IllegalArgumentException();
+        }
+
+    }
 
     public void validateInputNumber(String inputNumber) {
         if (isInputNumberNotDigit(inputNumber)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isInputCarNameOverLength(String inputCarName) {
+        return (inputCarName.length() > INPUT_NAME_MAXIMUM_LENGTH);
     }
 
     private boolean isInputNumberNotDigit(String inputNumber) {
