7d500af4b7b6d5796566e8fc43eb68525d173806
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-29 19:11:23 +0900

    feat: getRandomNumber를 static으로 변경 - RacingCarPlayerModel에서 사용하기 위함

--- src/main/java/util/RacingCarRandoms.java
+++ src/main/java/util/RacingCarRandoms.java
@@ -3,7 +3,7 @@ package util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCarRandoms{
-    public int getRandomNumber(){
+    public static int getRandomNumber(){
         return Randoms.pickNumberInRange(0,9);
     }
 }
