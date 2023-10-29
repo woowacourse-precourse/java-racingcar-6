a030f8f9c74b8c697f62f1bf20ac29f436b2924b
jongmee <ra123221@gmail.com>
2023-10-30 02:47:22 +0900

    test: DTO로 경주차 입력 값을 전달 받아 경주차를 생성하는 서비스 기능 테스트하기

---
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -0,0 +1,36 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.CarRepository;
+import racingcar.dto.request.CarNamesDto;
+
+class RacingServiceTest {
+
+    private final CarRepository carRepository = new CarRepository();
+    private final RacingService racingService = new RacingService(carRepository);
+
+    @AfterEach
+    public void clearRepository() {
+        carRepository.deleteAll();
+    }
+
+    @Test
+    void 경주차_생성하기() {
+        // given
+        CarNamesDto carNamesDto = new CarNamesDto("jong,gari,meoru");
+
+        // when
+        racingService.createCars(carNamesDto);
+
+        // then
+        List<Car> carList = carRepository.findAll();
+        assertThat(carList).hasSize(3)
+            .extracting(Car::getName)
+            .containsAll(carNamesDto.getNames());
+    }
+}
