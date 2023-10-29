f4dcab9c63a9c7ffb5f219c2a2b97168dd5c0000
kiwkim <atthenoon@gmail.com>
2023-10-30 07:58:17 +0900

    refactor($InputView): change the datatype of `inputCarNames` to String Array.
    Using a simple array looks better than List.

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -2,18 +2,13 @@ package racingcar.View;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
 public class InputView {
     private static final String MSG_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MSG_COUNT = "시도할 회수는 몇회인가요?";
 
-    public List<String> inputCarNames(){
+    public String[] inputCarNames(){
         System.out.println(MSG_NAME);
-        List<String> carNameList = new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
-        return carNameList;
+        return Console.readLine().split(",");
     }
     public String inputCounts(){
         System.out.println(MSG_COUNT);
