acf08a974406d0e6376971464486c6e4e99f8f46
상원 최 <swchoi1@swm.ai>
2023-10-29 14:02:56 +0900

    test(자동차 이름 검증) : 사용자가 입력한 자동차 이름들 검증하는 로직 테스트
    빈값을 입력하지 않았는지, 중복된 차량 이름을 입력하지 않았는지 테스트 로직 추가

---
+++ src/test/java/racingcar/utils/validator/CarManagerValidatorTest.java
@@ -0,0 +1,80 @@
+package racingcar.utils.validator;
+
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@Nested
+@DisplayName("[Car Manager Validator]")
+class CarManagerValidatorTest {
+
+    @ParameterizedTest
+    @MethodSource("validCarName")
+    @DisplayName("자동차들 이름 검증 - 올바른 이름 입력")
+    public void isValidCarNames(final String cars) {
+        Assertions.assertDoesNotThrow(() -> new CarManagerValidator().validate(cars));
+    }
+
+    static Stream<Arguments> validCarName() {
+        return Stream.of(
+                arguments("abc, cds, aaa"),
+                arguments("abc, cds, a.aa"),
+                arguments("abc,cds,aaa"),
+                arguments("abc,cds,aa a"),
+                arguments("abc , cds , aaa"),
+                arguments("abc , cds ,        aaa")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyCarName")
+    @DisplayName("자동차들 이름 검증 - 자동차 이름 입력 시 비어있는 값 입력")
+    public void enterEmptyCarName(final String cars) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new CarManagerValidator().validate(cars));
+    }
+
+    static Stream<Arguments> emptyCarName() {
+        return Stream.of(
+                arguments(", ,"),
+                arguments(""),
+                arguments((Object) null),
+                arguments("       "),
+                arguments(", cds, aaa"),
+                arguments(",cds,aaa"),
+                arguments(" , cds , aaa"),
+                arguments("abc, , aaa"),
+                arguments("abc,,aaa"),
+                arguments("abc ,  , aaa"),
+                arguments("abc, cds, "),
+                arguments("abc,cds,"),
+                arguments("abc,cds,      "),
+                arguments("abc , cds , ")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("duplicateCarName")
+    @DisplayName("자동차들 이름 검증 - 중복된 차량 이름 입력")
+    public void enterDuplicateCarName(final String cars) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new CarManagerValidator().validate(cars));
+    }
+
+    static Stream<Arguments> duplicateCarName() {
+        return Stream.of(
+                arguments("abc,abc"),
+                arguments("abc, abc"),
+                arguments("abc   , abc, ddd")
+        );
+    }
+
+}
