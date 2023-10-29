f5b40e48e0b2ffdd2be97095e8eb25ca3c7819f8
JeongMin <ajhappy12@gamil.com>
2023-10-29 16:14:52 +0900

    feat: 경주 이동 카운트 초기화 로직 추가

--- src/main/java/racingcar/RacingManager.java
+++ src/main/java/racingcar/RacingManager.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 public class RacingManager {
     private List<RacingCar> racingCars;
+    private int moveCount;
 
     public void initRace() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
         racingCars = createRacingCars(carNamesInput);
+
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String moveCountInput = Console.readLine();
+        moveCount = parseMoveCountInput(moveCountInput);
     }
 
     public List<RacingCar> createRacingCars(String carNamesInput) {
@@ -28,6 +33,26 @@ public class RacingManager {
     private void validateCarNames(String carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("자동차의 이름들을 입력해주세요.");
+        }
+    }
+
+    public int parseMoveCountInput(String moveCountInput) {
+        validateMoveCount(moveCountInput);
+        return Integer.parseInt(moveCountInput);
+    }
+
+    private void validateMoveCount(String moveCount) {
+        if (moveCount.isEmpty()) {
+            throw new IllegalArgumentException("시도할 회수를 입력해주세요.");
+        }
+
+        try {
+            int count = Integer.parseInt(moveCount);
+            if (count <= 0) {
+                throw new IllegalArgumentException("시도할 회수는 1이상이어야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 회수는 숫자여야 합니다.");
         }
     }
 }
