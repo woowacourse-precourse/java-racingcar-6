469389f606462e74462e6a248630b0cb97b1b847
sami355-24 <ddung355@gmail.com>
2023-10-30 03:11:59 +0900

    test(Model): 자동차 이름을 전처리하는 메서드 로직 수정
    - 단순 EqualTo를 사용하면 객체자체를 비교하는 코드가 되어 이를 하나씩 꺼내어 비교하는 코드로 수정

--- src/test/java/racingcar/Model/CarModelTest.java
+++ src/test/java/racingcar/Model/CarModelTest.java
@@ -3,8 +3,10 @@ package racingcar.Model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.Domain.Car;
 import racingcar.Domain.CarGroup;
 
 class CarModelTest {
@@ -17,12 +19,23 @@ class CarModelTest {
         //given
         String testCarNames = "car1,car2,car3";
         CarGroup expected = CarGroup.of(List.of("car1", "car2", "car3"));
+        CarGroup expectedCarGroup = CarGroup.of(List.of("car1", "car2", "car3"));
 
         //when
         carModel.preProcessing(testCarNames);
+        CarGroup resultCarGroup = carModel.preProcessing(testCarNames);
 
         //then
-        assertEquals(expected, carModel.preProcessing(testCarNames));
+        Integer expectedSize = expectedCarGroup.findSize();
+        Integer resultSize = resultCarGroup.findSize();
+        assertEquals(expectedSize, resultSize);
+
+        for (int order = 1; order <= expectedSize; order++) {
+            Car expectedCar = expectedCarGroup.findCarByOrder(order);
+            Car resultCar = resultCarGroup.findCarByOrder(order);
+            assertEquals(expectedCar.findName(), resultCar.findName());
+        }
+
     }
 
     @Test
