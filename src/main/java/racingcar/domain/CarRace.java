684d1b21e456a59e5958b5e3b9f3839020c925da
yunjeong <stac_cato@naver.com>
2023-10-29 16:10:23 +0900

    feat: 전 출력값에 이어서 경기 결과 출력해주는 기능 추가

--- src/main/java/racingcar/domain/CarRace.java
+++ src/main/java/racingcar/domain/CarRace.java
@@ -2,8 +2,11 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRace {
-    public static String printTrack() {
+    public String computeTrack() {
         int number = Randoms.pickNumberInRange(0, 9);
 
         if (number >= 4) {
@@ -12,4 +15,17 @@ public class CarRace {
             return "";
         }
     }
+
+    public void printTrack(List<String> carNames, List<String> tracks) {
+        for (int i = 0; i < carNames.size(); i++) {
+            System.out.print(carNames.get(i) + " : ");
+
+            String track = tracks.get(i);
+            track += computeTrack();
+            tracks.set(i, track);
+
+            System.out.print(track + "\n");
+        }
+    }
+
 }
