bc6dd60bb40e8a203c2d2130867aff5fbab87676
kiwkim <atthenoon@gmail.com>
2023-10-30 05:18:10 +0900

    feat($OutputView): make OutputView.
    - printRaceProgress: print each Car's Progress.
    - printFinalWinners: print determined Winners.

---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,19 @@
+package racingcar.View;
+
+import java.util.List;
+import racingcar.Model.Car;
+
+public class OutputView {
+    private static final String MSG_RESULT = "\n실행 결과";
+
+    public void printRaceProgress(Car car){
+        String carName = car.getCarName();
+        String carProgress = car.getProgress();
+        System.out.println(carName + " : " + carProgress);
+    }
+
+    public void printFinalWinners(List<String> finalWinners){
+        String result = String.join(", ", finalWinners);
+        System.out.println("최종 우승자 : " + result);
+    }
+}
