fb0b8f66875cbb7922149c20813e9948f4de9c98
tam31 <wxogud3377@naver.com>
2023-10-29 22:41:56 +0900

    Feat(InputValidation) : 입력받은 값의 숫자 검사 기능 구현 Docs : 이동횟수가 숫자가 아니라면 오류발생 완료 체크

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -6,10 +6,16 @@ import java.util.Map;
 public class InputValidation {
     private static final String IS_NULL_CAR_NAME_ERROR_MESSAGE = "입력하지 않았습니다.";
     private static final String IS_NOT_RANGE_NAME_ERROR_MESSAGE = "5자리 이하가 아닙니다.";
+    private static final String IS_NOT_NUMBER_ERROR_MESSAGE = "숫자가 아닙니다.";
 
     public String[] checkedInputValidation(String carNames){
         isNullText(carNames);
         return collectCarNames(carNames);
+    }
+
+    public void checkedInputNumberValidation(String number){
+        isNullText(number);
+        isCheckedNumber(number);
     }
 
     private void isNullText(String carNames){
@@ -29,6 +35,14 @@ public class InputValidation {
     private void isCheckedNameRange(String carName){
         if(carName.length()>5){
             throw new IllegalArgumentException(IS_NOT_RANGE_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    private void isCheckedNumber(String number) {
+        for (int i = 0; i<number.length(); i++){
+            if(!Character.isDigit(number.charAt(i))){
+                throw new IllegalArgumentException(IS_NOT_NUMBER_ERROR_MESSAGE);
+            }
         }
     }
 }
