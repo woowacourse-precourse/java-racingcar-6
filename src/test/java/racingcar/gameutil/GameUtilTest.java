5acc10f54f132f517bae33bc232365af2774a49f
june-777 <wlwhswnsrl96@gmail.com>
2023-10-29 22:20:59 +0900

    refactor(GameUtil): utils 패키지로 이동

--- src/test/java/racingcar/gameutil/GameUtilTest.java
+++ src/test/java/racingcar/gameutil/GameUtilTest.java
@@ -3,7 +3,7 @@ package racingcar.gameutil;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import racingcar.GameUtil;
+import racingcar.utils.GameUtil;
 
 class GameUtilTest {
 
