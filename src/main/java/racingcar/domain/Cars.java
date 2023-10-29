70b6f76ef1de70bd0a8196da0f94312d93e78e04
elyudwo <dudqk9696@naver.com>
2023-10-29 20:56:22 +0900

    [Feat] : 우승자 선정 기능 구현
    1. findFarthestCar 가장 멀리있는 자동차 반환
    2. findSameDistanceCar 가장 멀리있는 자동차와 같은 위치에 있는 자동차 이름 반환

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,6 +4,7 @@ import racingcar.util.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toUnmodifiableList;
@@ -30,5 +31,23 @@ public class Cars {
         return cars.stream()
                 .map(car -> Car.makeCarByNameDistance(car.getName(), car.getDistance()))
                 .collect(toUnmodifiableList());
+    }
+
+    public List<String> findWinner() {
+        Car farthestCar = findFarthestCar();
+        return findSameDistanceCar(farthestCar);
+    }
+
+    private Car findFarthestCar() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("차량 리스트가 비어있습니다."));
+    }
+
+    private List<String> findSameDistanceCar(Car maxPositionCar) {
+        return cars.stream()
+                .filter(maxPositionCar::isSameDistance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
