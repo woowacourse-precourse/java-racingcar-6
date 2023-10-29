a05dbf73593888e020b878db96b270b092ef20fd
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-10-30 01:12:56 +0900

    Feat: validateName 메서드 조건 추가
    validateName 조건 추가,
    조건문 속 5 상수 처리

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -12,8 +12,10 @@ public class Car {
 	}
 
 	public void validateName() {
-		if(name.length() > 5) {
-			throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+		final int MAX_NAME_LENGTH = 5;
+
+		if(name.length() > MAX_NAME_LENGTH || name.isBlank()) {
+			throw new IllegalArgumentException("5자 이하의 이름있는 자동차만 넣을 수 있습니다.");
 		}
 	}
 
