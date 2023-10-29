7bd563515da7966713e0aa31d952c1efe494da4b
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-29 17:20:20 +0900

    feat: 초기 시작 뷰 추가

---
+++ src/main/java/racingcar/view/SettingView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+public class SettingView {
+
+   public static void getCarNames() {
+       System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+   }
+   public static void getTrialNums() {
+       System.out.println("시도할 회수는 몇회인가요?");
+   }
+}
