5190e3faae9fa2a2acbb5f32c374aa90f009846b
ahma0 <dsd932@naver.com>
2023-10-29 18:26:18 +0900

    feat: 시도할 회수를 입력받을 때 문장을 출력하는 기능

--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -5,4 +5,8 @@ public class RacingView {
     public void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public void printReceiveNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
