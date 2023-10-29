ad5f6750cc88f7462be4130d378ffba20a19fa1b
goragoraki <gorakio0126@gmail.com>
2023-10-29 12:21:19 +0900

    feat(Application): create racingGame object and exec racingStart function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.Controller.RacingGame;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.racingStart();
     }
 }
