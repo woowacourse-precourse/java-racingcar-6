4a91bc4f3a661c919ace5234ae60501725090a56
bamm2 <xxknu88@naver.com>
2023-10-30 02:00:47 +0900

    feat: 이름에 공백만 존재하는 경우 예외발생기능

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,41 +1,67 @@
 package racingcar.validator;
 
+import static racingcar.constant.Constant.CAR_NAME_MAX_SIZE;
+import static racingcar.constant.ExceptionMessage.DUPLICATE_CAR_NAME;
+import static racingcar.constant.ExceptionMessage.EMPTY_CAR_NAME;
+import static racingcar.constant.ExceptionMessage.OVER_SIZE_CAR_NAME;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import camp.nextstep.edu.missionutils.Console;
-import racingcar.constant.ExceptionMessage;
+import racingcar.view.Input;
 
 public class InputValidator {
 
     private List<String> validateCarNames;
+    private Input input = new Input();
 
     public InputValidator(String validateCarNames) {
         convertValidateList(validateCarNames);
-        try{
-            hasDuplicatedName();
-        }catch (IllegalArgumentException e){
-            Console.close();
-            e.printStackTrace();
-        }
+        removeSpaceInCarName();
+        hasDuplicatedName();
+        isOverCarNameSize();
+        validateEmptyCarName();
     }
 
     private void convertValidateList(String carNames) {
         this.validateCarNames = Arrays.stream(carNames.split(",")).toList();
     }
 
+    protected List<String> removeSpaceInCarName() {
+        this.validateCarNames = validateCarNames.stream().map(carName -> carName.replaceAll("\\s", ""))
+                .collect(Collectors.toList());
+        return validateCarNames;
+    }
+
     private void hasDuplicatedName() {
         HashSet<String> duplicateValidSet = new HashSet<>(validateCarNames);
         if (duplicateValidSet.size()!=this.validateCarNames.size()) {
-            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_CAR_NAME.getMessage());
+            input.close();
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME.getMessage());
         }
     }
 
-    protected List<String> removeSpaceInCarName(){
-        return this.validateCarNames.stream().map(carName->carName.replaceAll("\\s",""))
-                .collect(Collectors.toList());
+    private void isOverCarNameSize() {
+        List<String> unAvailCarNames = validateCarNames.stream()
+                .filter(carName -> carName.length() > CAR_NAME_MAX_SIZE)
+                .toList();
+        if (!unAvailCarNames.isEmpty()) {
+            input.close();
+            throw new IllegalArgumentException(OVER_SIZE_CAR_NAME.getMessage());
+        }
     }
 
+    private void validateEmptyCarName() {
+        System.out.println(validateCarNames);
+        validateCarNames.stream()
+                .filter(carName -> carName.length()==0)
+                .findAny()
+                .ifPresent(carName -> {
+                    input.close();
+                    throw new IllegalArgumentException(EMPTY_CAR_NAME.getMessage());
+                }
+        );
+    }
 }
