7e6826c2b08385557462289df92440f689cc511c
junseoks-99 <angec5645@naver.com>
2023-10-29 21:35:49 +0900

    feat: 게임 시작 후 입력 문구 출력 기능 구현

---
+++ src/main/java/racingcar/domain/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.domain.view;
+
+public class InputView {
+
+    public static void printStartMessage() {
+        System.out.println(InputMessages.START.getMessage());
+    }
+
+    public static void printAttemptMessage() {
+        System.out.println(InputMessages.ATTEMPT.getMessage());
+    }
+}
