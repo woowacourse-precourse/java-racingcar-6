8137e91e5328b816d8ef327384cfee835a6f6f81
twinson37 <twinson37@hanyang.ac.kr>
2023-10-29 18:48:10 +0900

    Refactor: 게임 컨트롤러 게임시작 함수 수정

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -1,2 +1,35 @@
-package racingcar.Controller;public class GameController {
+package racingcar.Controller;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import racingcar.Model.GameModel;
+
+public class GameController {
+
+    private final GameModel gameModel;
+    private final CarController carController;
+
+    public GameController(GameModel gameModel, CarController carController) {
+        this.gameModel = gameModel;
+        this.carController = carController;
+    }
+
+
+    public void gameStart(){
+
+        String[] carList;
+        int coinNumber;
+
+        System.out.println("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+        carList = readLine().split(",");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        coinNumber = Integer.parseInt(readLine());
+
+        gameModel.setCarNumber(carList.length);
+        gameModel.setCoinNumber(coinNumber);
+
+        carController.setCarModels(carList);
+    }
+
 }
