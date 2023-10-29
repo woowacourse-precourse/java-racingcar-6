4c4ce43166126667c46cf84ce17ed13031b04866
KDT live <dnjsxo0616@gmail.com>
2023-10-29 19:02:10 +0900

    feat(User): 사용자 입력 유효성 검사 기능 추가
    - 사용자로부터 입력 받은 자동차 이름의 길이를 유효성 검사하는 `carNameVaildation` 메서드 추가
    - 최대 길이(`CARNAME_MAXSIZE`) 및 최소 길이(`CARNAME_MINSIZE`) 상수 정의
    - 길이가 범위를 벗어나는 경우 예외를 던지도록 구현
    - 유효성 검사 결과 예외 처리 추가

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class User {
 
+    private static final Integer CARNAME_MAXSIZE = 5;
+    private static final Integer CARNAME_MINSIZE = 1;
+
     public List<String> carNames = new ArrayList<>();
 
     public void carNameSplit(String userInput){
@@ -12,6 +15,18 @@ public class User {
 
         for(String carName : carNameArray) {
             carNames.add(carName.trim());
+        }
+    }
+
+    public void carNameVaildation(List<String> carNames){
+        for(String carName : carNames){
+            if(carName.length() > CARNAME_MAXSIZE){
+                throw new IllegalArgumentException("입력한 이름은 5자 이하만 가능합니다.");
+            }
+
+            if(carName.length() < CARNAME_MINSIZE){
+                throw new IllegalArgumentException("이름을 1자 이상 5자 이하로 입력해 주세요.");
+            }
         }
     }
 
