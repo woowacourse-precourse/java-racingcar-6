a108e0b10c18e78cb36295fbaaae9e0f01f6befa
freekyr7529 <freekyr7529@gmail.com>
2023-10-29 18:24:37 +0900

    test: 게임 진행에 대한 테스트 케이스 작성 및 통과
    - 생성된 난수가 4이상 이면 전진 그렇지 않으면 위치가 변하지 않는 것을 검증하기 위해 테스트 케이스를 작성 후 통과하였다.
    - 차수가 모두 끝난 후 선두에 있는 자동차들을 구하는 기능을 검증하기 위해 테스트 케이스를 작성 후 통과하였다.

---
+++ src/test/java/racingcar/GameProcessTest.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Cars;
+
+public class GameProcessTest extends NsTest {
+    private static final int ROUND = 5;
+
+    @Test
+    void 난수가_4이상일_경우_자동차_위치_업데이트_O() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "2");
+                    assertThat(output()).contains("pobi : --", "최종 우승자 : pobi");
+                },
+                4, 4
+        );
+    }
+
+    @Test
+    void 난수가_4미만일_경우_자동차_위치_업데이트_X() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "2");
+                    assertThat(output()).contains("pobi : ", "최종 우승자 : pobi");
+                },
+                3, 3
+        );
+    }
+
+    @Test
+    void 게임_진행_후_선두의_자동차들을_계산() {
+        Cars cars = new Cars("pobi,woni,jun");
+        cars.roundStatus(5);
+        assertThat(cars.findWinners()).isNotNull();
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
