0591a2df9488330b9096d672665853906e7c98b3
Tiger <skdlghwns@gmail.com>
2023-10-29 23:09:13 +0900

    feat: CarGenerator 클래스 추가, 경주 자동차 이름 입력, 자동차 이름 부여후 생성 기능 구현 - domain/CarGenerator Class 추가하였다. - 경주 자동차 이름 입력기능을 CarGenerator#askCarNames로 구현하였다. - 자동차 이름을 부여하여 자동차를 생성하는 기능을   CarGenerator#createCars로 구현하였다.

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -1,2 +1,28 @@
-package racingcar.domain;public class CarGenerator {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class CarGenerator {
+    public CarGenerator(){}
+    public List<String> askCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carName = Arrays.asList(Console.readLine().split(","));
+        for(String name : carName){
+            if(name.length()>5){
+                throw new IllegalArgumentException();
+            }
+        }
+        return carName;
+    }
+    public List<Car> createCars(List<String> carNames){
+        List<Car> racingCarList = new ArrayList<>();
+        for(String name : carNames){
+            Car newCar = new Car(name);
+            racingCarList.add(newCar);
+        }
+        return racingCarList;
+    }
 }
