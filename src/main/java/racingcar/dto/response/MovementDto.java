39ad72ba4aee862bf69f48d44ee9cba007c893bc
jongmee <ra123221@gmail.com>
2023-10-30 02:43:27 +0900

    feat: 각 경주 실행 시 중간 결과의 출력 값을 생성하는 DTO 구현하기
    - 한 경주 차의 이동 결과 메세지를 생성한다.

---
+++ src/main/java/racingcar/dto/response/MovementDto.java
@@ -0,0 +1,26 @@
+package racingcar.dto.response;
+
+import racingcar.domain.Car;
+
+public class MovementDto {
+    private static final String RAIL = "-";
+    private static final String DELIMITER = " : ";
+
+    private final String movementMessage;
+
+    public MovementDto(Car car) {
+        this.movementMessage = createMessage(car);
+    }
+
+    public String createMessage(Car car) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(car.getName())
+                .append(DELIMITER)
+                .append(RAIL.repeat(car.getForwardCount()));
+        return builder.toString();
+    }
+
+    public String getMovementMessage() {
+        return movementMessage;
+    }
+}
