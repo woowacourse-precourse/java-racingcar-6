c0c4dfec5e2fec6b058365a884d16355cabd82d6
gusdn0108 <gusdn6671@naver.com>
2023-10-30 02:42:03 +0900

    feat(Cars): 일급컬렉션으로 리팩토링 시작

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+
+
+}
