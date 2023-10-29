7fb7f993042c747bfbae93860e0fc5d381912c96
siyeonSon <siyeon0209@naver.com>
2023-10-29 17:33:15 +0900

    chore(car): 자동차의 이름은 5자 이하만 가능하다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,21 @@
 package racingcar;
 
 public class Car {
+    private static final int LENGTH_LIMITER = 5;
+
     private final String name;
     private int point;
 
     public Car(String name) {
+        validateLength(name);
         this.name = name;
         this.point = 0;
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > LENGTH_LIMITER) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다");
+        }
     }
 
     public String getName() {
