6bfd282309293e52f0b40ba1163be7a40ec628c6
hayannn <dlgkdis801@naver.com>
2023-10-29 19:05:55 +0900

    feat(RacingcargameController): 게임을 시작하고, 플레이어의 유효한 입력 발생 시 차수별 경주를 시작하는 기능 구현

--- src/main/java/racingcar/Controller/RacingcargameController.java
+++ src/main/java/racingcar/Controller/RacingcargameController.java
@@ -1,10 +1,16 @@
 package racingcar.Controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.Domain.Car;
+import racingcar.Domain.Game;
 import racingcar.Exception.CarNameException;
 import racingcar.Exception.TryCountNumberException;
 import racingcar.Repository.CarRepository;
 import racingcar.Service.GameFlowManagementService;
 import racingcar.Util.StringToList;
+import racingcar.View.InputView;
+import racingcar.View.OutputView;
 
 public class RacingcargameController {
     private final GameFlowManagementService gameFlowManagementService;
@@ -19,5 +25,53 @@ public class RacingcargameController {
         this.carNameException = new CarNameException();
         this.tryCountNumberException = new TryCountNumberException();
         this.carRepository = new CarRepository();
+    }
+
+    public void startRacingCarGame() {
+        while (true) {
+            if (playRacingCarGame()) {
+                break;
+            }
+        }
+    }
+
+    public boolean playRacingCarGame() {
+        String playerCarNames = getPlayerCarNames();
+        String tryCountNumber = getTryCountNumber();
+
+        try {
+            List<String> carNames = stringToList.analysisCarNames(playerCarNames);
+            int tryCount = stringToList.analysisTryCountNumber(tryCountNumber);
+
+            saveCarNamesToRepo(carNames);
+            manageRacingCarGame(tryCount);
+            return true;
+        } catch (IllegalArgumentException e) {
+            OutputView.showCarNameAndTryCountException(e.getMessage());
+            return false;
+        }
+    }
+
+    private void manageRacingCarGame(int tryCount) {
+    }
+    //입력받을 자동차 이름과 시도 횟수에 대한 메서드
+    private void saveCarNamesToRepo(List<String> carNames) {
+        for (String carName : carNames) {
+            carRepository.saveCarName(new Car(carName));
+        }
+    }
+
+    private String getPlayerCarNames() {
+        InputView.showCarNameInputPrompt();
+        String playerCarNames = Console.readLine();
+        carNameException.validateCarNameInput(playerCarNames); // 입력 유효성 검사
+        return playerCarNames;
+    }
+
+    private String getTryCountNumber() {
+        InputView.showTryCountNumPrompt();
+        String tryCountNumber = Console.readLine();
+        tryCountNumberException.validateTryCountInput(tryCountNumber); // 입력 유효성 검사
+        return tryCountNumber;
     }
 }
