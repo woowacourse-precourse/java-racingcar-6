7a0dbeb2b2dbd39f95ee92e05116b4124bc8742c
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-30 02:28:39 +0900

    feat: 자동차를 관리할 Car 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Car {
+    private final List<String> carList;
+
+    public Car(List<String> carList) {
+        this.carList = carList;
+    }
+}
