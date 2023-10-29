e3448e5fc544095eaa6a79e57e1fa592a07d5013
PARKHWANYEONG <welcome0828@naver.com>
2023-10-29 09:21:52 +0900

    test(service): 임의의 숫자로 게임 진행 결과 테스트 추가
    -임의의 숫자를 사용하여 게임을 2회 진행한 결과를 테스트해 게임의 로직이 정확하게 동작하는지 확인.

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.util.TestNumberGenerator;
 
 import java.util.List;
 
@@ -16,7 +17,8 @@ class RacingGameServiceTest {
 
     @BeforeEach
     void beforeEach() {
-        racingGameService = new RacingGameService();
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(new int[]{1,6,4,7});
+        racingGameService = new RacingGameService(testNumberGenerator);
     }
     @Test
     void 입력받은_이름으로_Car_생성() {
@@ -31,6 +33,29 @@ class RacingGameServiceTest {
         assertThat(carList)
                 .extracting(c -> c.getName())
                 .containsExactly(carNames);
+    }
+
+    /**
+     *  테스트 숫자가 1,6,4,7 이므로
+     *  자동차가 2개 기준 게임을 2번 반복했을때
+     *  1번 자동차는 1 -> 정지 , 4 -> 전진
+     *  2번 자동차는 6 -> 전진 , 7 -> 전진 이다.
+     */
+    @Test
+    void 임의의_테스트_숫자로_게임진행_결과_테스트() {
+        //given
+        String[] carNames = {"hwan","looloo"};
+        racingGameService.init(carNames);
+
+        //when
+        racingGameService.playGame(2);
+
+        //then
+        List<Car> carList = racingGameService.getCarList();
+        Car hwan = carList.get(0);
+        Car looloo = carList.get(1);
+        assertThat(hwan.getPosition()).isEqualTo(1);
+        assertThat(looloo.getPosition()).isEqualTo(2);
     }
 
 }
