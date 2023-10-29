d291f1f87a519783321933852616590c9118c285
olsohee <dlthgml0108@daum.net>
2023-10-29 12:26:02 +0900

    feat(Application): 게임 시작 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RaceController raceController = new RaceController();
+        raceController.play();
     }
 }
