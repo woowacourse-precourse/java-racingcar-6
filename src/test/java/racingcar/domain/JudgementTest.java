0bde38d625e3488120aa5d029e778530d2d0e6c0
hoiae <donald819@naver.com>
2023-10-30 01:30:00 +0900

    test: Judgement.class getPlayCountFromUser()테스트코드 작성

--- src/test/java/racingcar/domain/JudgementTest.java
+++ src/test/java/racingcar/domain/JudgementTest.java
@@ -7,14 +7,25 @@ import org.junit.jupiter.api.Test;
 class JudgementTest {
 
     @Test
-    void getNamesFromUser5글자이하의이름을입력한경우정상실행() {
-        Assertions.assertThat(Judgement.getNamesFromUser("강아지,고양이,너구리")).containsExactly("강아지","고양이","너구리");
+    void getNamesFromUser5글자이하의이름을입력한경우() {
+        Assertions.assertThat(Judgement.getNamesFromUser("강아지,고양이,너구리")).containsExactly("강아지", "고양이", "너구리");
     }
 
     @Test
-    void getNamesFromUser5글자를초과한이름을입력한경우에러발생() {
+    void getNamesFromUser5글자를초과한이름을입력한경우() {
         Assertions.assertThatThrownBy(() -> Judgement.getNamesFromUser("티라노사우르스,오리너구리"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getPlayCountFromUser숫자를입력하지않은경우() {
+        Assertions.assertThatThrownBy(() -> Judgement.getPlayCountFromUser("숫자"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getPlayCountFromUser올바른입력을한경우(){
+        Assertions.assertThat(Judgement.getPlayCountFromUser("10")).isEqualTo(10);
     }
 
 }
