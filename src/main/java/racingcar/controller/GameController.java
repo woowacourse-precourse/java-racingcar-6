3ec54aea749e90697a892c96831c69702888bfe4
jinkshower <askthem@naver.com>
2023-10-29 10:48:53 +0900

    feat(GameController): 프로그램 설계 pseudocode로 표기

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,2 +1,19 @@
-package racingcar.controller;public class GameController {
+package racingcar.controller;
+
+public class GameController {
+    public void proceed() {
+        /*
+         *  get user input for car
+         *  create car objects with names
+         *  get user input for rounds
+         *  - one round
+         *      throw dice for each car
+         *      determine whether car move forward (dice >= 4)
+         *      print result
+         *  loop rounds according to user input
+         *  decide winner
+         *  print winner
+         *  exit00
+         */
+    }
 }
