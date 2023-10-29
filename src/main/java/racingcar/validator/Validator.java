dd4c52596cceb8a553762ad18d7f0c985d07e7db
yooooonshine <hl3470@naver.com>
2023-10-29 12:39:17 +0900

    refactor: 자동차이름 입력 예외처리 수정

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,16 +1,27 @@
 package racingcar.validator;
 
 public class Validator {
-    public void carNameStringLength(String carName) {
+    public static void carNameStringLength(String carName) {
         if (carName.length() <= 5) {
             return;
         }
         throw new IllegalArgumentException("[ERROR] 이름 길이 5초과");
     }
 
-    public void isNull(String carName) {
+    public static void isNull(String carName) {
         if (carName == "") {
-            throw new IllegalArgumentException("[ERROR] 이름이 존재하지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 입력이 존재하지 않습니다.");
+        }
+    }
+
+    //콤마 개수랑 이름 개수가 안 맞으면 throw
+    public static void carNamesComma(String carNames) {
+        int commaCount = (int) carNames.chars()
+                .filter(c -> c == ',')
+                .count();
+        int nameCount = carNames.split(",").length;
+        if (commaCount + 1 != nameCount) {
+            throw new IllegalArgumentException("[ERROR] 콤마의 사용이 부적절합니다.");
         }
     }
 }
