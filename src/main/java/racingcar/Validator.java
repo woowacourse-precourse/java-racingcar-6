a75f3aa3789b9d7b464f275a1177f9da08d2796e
SUJI-J <suzy852@naver.com>
2023-10-29 17:41:56 +0900

    feat: 자동차 이름 중복값 예외 기능 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -4,20 +4,25 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
+import static racingcar.GameMessage.INPUT_DUPLICATE_EXCEPTION_MESSAGE;
 import static racingcar.GameMessage.INPUT_LENGTH_EXCEPTION_MESSAGE;
 
 public class Validator {
 
-    public static Map<String, String> inputCarNames(Map<String, String> carNamesMap) {
-        isValidLength(carNamesMap);
-        return carNamesMap;
+    public static Map<String, String> inputCarNames(Map<String, String> racingCars) {
+        isValidLength(racingCars);
+        return racingCars;
     }
 
-    private static void isValidLength(Map<String, String> carNamesMap) {
-        for (String carName : carNamesMap.keySet()) {
+    private static void isValidLength(Map<String, String> racingCars) {
+        for (String carName : racingCars.keySet()) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException(INPUT_LENGTH_EXCEPTION_MESSAGE);
             }
         }
+    }
+
+    public static void  isDuplicateNames() {
+        throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MESSAGE);
     }
 }
