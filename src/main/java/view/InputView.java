a82870239780ac4da457afc88d877e1a2e7d02e6
sangeun-b <baek0007@algonquinlive.com>
2023-10-29 23:01:54 +0900

    feat: 자동차 이름 입력 메소드 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,18 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public List<String> inputCarName() {
+        System.out.println(INPUT_CAR_NAME);
+        String carName = Console.readLine();
+        List<String> carNames = Arrays.asList(carName.split(","));
+        return carNames;
+    }
+}
