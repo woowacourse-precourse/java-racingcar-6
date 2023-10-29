4586105065d4b63e56239c7f29ae9f60e59d8613
sangdol <priceelees@gmail.com>
2023-10-29 13:03:14 +0900

    feat(Cars): 자동차 리스트를 저장하며, 경주를 진행하고 우승자를 얻어내는 기능

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import java.util.Collections;
+import java.util.List;
+import racingcar.util.CarListMaker;
+
+public record Cars(List<Car> carList) {
+    public static Cars getCarsFromNames(String names) {
+        return new Cars(CarListMaker.makeCarList(names));
+    }
+
+    public Cars race() {
+        List<Car> result = carList.stream()
+                .map(Car::moveForward)
+                .toList();
+
+        return new Cars(result);
+    }
+
+    public List<Car> findWinners() {
+        return carList.stream()
+                .filter(this::isWinner)
+                .toList();
+    }
+
+    private int getMaxLocation() {
+        return carList.stream()
+                .mapToInt(Car::location)
+                .max()
+                .orElse(0);
+    }
+
+    private boolean isWinner(Car car) {
+        return car.location() == getMaxLocation();
+    }
+}
