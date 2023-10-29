3a8847df235e5e5f10e6035d0a86def7a782391e
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 21:09:30 +0900

    feat(RacingCarService): 차를 달리게하고 우승자를 찾는 기능 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,23 +1,21 @@
 package racingcar.service;
 
+import racingcar.converter.DtoConverter;
 import racingcar.domain.*;
-import racingcar.domain.dto.RunningCarRecord;
-import racingcar.domain.dto.WinnerRecord;
+import racingcar.domain.dto.RunningCarDto;
+import racingcar.domain.dto.WinnerDto;
 import racingcar.domain.validator.InputValidator;
 import racingcar.util.StringManipulator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarService {
     private final Cars cars;
     private int tryNumber;
-    private final Judge judge;
-    private final RandomNumberGenerator randomNumberGenerator;
 
-    public RacingCarService(Cars cars, Judge judge, RandomNumberGenerator randomNumberGenerator) {
+    public RacingCarService(Cars cars) {
         this.cars = cars;
-        this.judge = judge;
-        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void inputValidate(String input, InputValidator inputValidator) {
@@ -31,5 +29,23 @@ public class RacingCarService {
 
     public void insertTryNumber(String inputNumber) {
         tryNumber = Integer.parseInt(inputNumber);
+    }
+
+    public List<RunningCarDto> rushCars() {
+        List<Car> carList = cars.rushCars();
+        return carList.stream()
+                .map(DtoConverter::toRunningCarDto)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isRaceOver() {
+        return tryNumber == 0;
+    }
+
+    public List<WinnerDto> getWinner() {
+        List<Car> winners = cars.findWinners();
+        return winners.stream()
+                .map(DtoConverter::toWinnerDto)
+                .collect(Collectors.toList());
     }
 }
