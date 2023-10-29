1a733e9aa1024325fcf889b0a7bc83a65b689976
maark1106 <mark1106@naver.com>
2023-10-29 15:42:36 +0900

    feat(Cars) : car들의 집합인 Cars 클래스 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars = new ArrayList<>();
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
