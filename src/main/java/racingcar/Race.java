0a747845b599fbd089d1eb5db18ea73a234c433c
SCY <helenason@naver.com>
2023-10-29 20:09:08 +0900

    feat: 사용자 입력 종료 후 경주 시작

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import com.sun.security.jgss.GSSUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ public class Race {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         inputCarNames();
         inputGameCount();
+        play();
     }
 
     private void inputCarNames() {
@@ -49,5 +51,9 @@ public class Race {
                 .filter(car -> car.isEqualName(carName))
                 .count();
         return countEqualNames > NOT_HAVE_SAME_NAMES;
+    }
+
+    private void play() {
+        System.out.println("\n실행 결과");
     }
 }
