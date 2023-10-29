257225a6e7dae6bb8561e66dea60a4601ca6c9cd
Mr.Kim <joajy98@gmail.com>
2023-10-29 10:28:40 +0900

    feat: 자동차 이름 입력받는 메서드

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,10 +1,13 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.validator.CarValidator.*;
 import static racingcar.view.InputView.*;
 import static racingcar.view.OutputView.*;
 
@@ -22,5 +25,24 @@ public class RacingController {
             printResult(carList);
         }
         printWinnerList(carList);
+    }
+
+    public static void getCarInput(List<Car> carList){
+        String carName = getCarName();
+        splitCarList(carList, carName);
+        checkAllException(carList);
+    }
+
+    public static String getCarName() {
+        getCarNameNotification();
+        String carNameList = readLine();
+        return carNameList;
+    }
+
+    public static void splitCarList(List<Car> carList, String carNames) {
+        String[] carNameList = carNames.split(DELIMITER);
+        for (String carName : carNameList) {
+            carList.add(new Car(carName));
+        }
     }
 }
