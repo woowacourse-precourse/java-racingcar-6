7649c3ff290eacb049aea45f2bdba988b8126395
little6523 <little6523@naver.com>
2023-10-30 01:32:20 +0900

    feat : 0~9사이의 무작위 값 추출 기능 구현  - docs/README.md의 2번 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,44 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
-//    private List<String> CarNames = new ArrayList<>();
+    private static List<String> carNames = new ArrayList<>();
+    private static int carMovingCount;
 
     private static void displayStartText() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
     }
 
-    //    private static void inputCarNames() {
-//
-//    }
-    private static int inputCarMovingCount() {
-        return Integer.parseInt(Console.readLine());
+/*    private static void inputCarNames() {
+        String carName = Console.readLine();
+        carNames = Arrays.asList(carName.split(","));
+    }*/
+
+/*    private static void isNameLengthOverFive(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }*/
+
+    private static void inputCarMovingCount() {
+        carMovingCount = Integer.parseInt(Console.readLine());
+    }
+
+    private static int randomPickingNumberZeroToNine() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         displayStartText();
         inputCarMovingCount();
+/*        inputCarNames();
+        for (int i = 0; i < carNames.size(); i++) {
+            isNameLengthOverFive(carNames.get(i));
+        }*/
     }
 }
