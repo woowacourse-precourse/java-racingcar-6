84d694d6796730b5271c4176a06b884375bd38ba
Jin Geonwoo <jinkonu99@gmail.com>
2023-10-29 15:43:07 +0900

    feat: ErrorConstant 추가 및 적용

--- src/main/java/racingcar/service/InputService.java
+++ src/main/java/racingcar/service/InputService.java
@@ -2,7 +2,9 @@ package racingcar.service;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static java.lang.Long.*;
-import static racingcar.domain.NumberConstant.MAX_RACINGCAR_NAME_LENGTH;
+import static racingcar.domain.ErrorConstant.*;
+import static racingcar.domain.NumberConstant.*;
+import static racingcar.domain.StringConstant.*;
 
 // 고정 출력 문자열 상수 도메인으로
 // validator 클래스 따로
@@ -14,7 +16,7 @@ public class InputService {
 
 
     public static String[] convertToStringArray(String line) {
-        String[] carNames = line.split(",");
+        String[] carNames = line.split(CARS_SPLIT_JOIN_DELIMITER);
 
         for (String carName : carNames)
             validateCarNameLength(carName);
@@ -24,7 +26,7 @@ public class InputService {
 
         private static void validateCarNameLength(String carName) {
             if (carName.length() > MAX_RACINGCAR_NAME_LENGTH)
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR);
         }
 
 
@@ -38,7 +40,7 @@ public class InputService {
             try {
                 parseLong(line);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("횟수는 숫자로만 입력해주세요.");
+                throw new IllegalArgumentException(TIMES_NUMBER_TYPE_ERROR);
             }
         }
 }
