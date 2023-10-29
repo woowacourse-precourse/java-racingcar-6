11ebfc947d2fc1586de13d4286174ebeba0fabd9
Junobee25 <wnsdhqo@naver.com>
2023-10-29 16:38:34 +0900

    refactor: CarGenerator 매개변수 수정

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -2,31 +2,27 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.LinkedHashMap;
-import java.util.regex.*;
+
 
 public class CarGenerator {
 
-    Car car = new Car();
 
-    public LinkedHashMap<String, String> createCarList() {
+
+    public LinkedHashMap<String, String> createCarList(String stringCarList) {
         LinkedHashMap<String, String> carList = new LinkedHashMap<>();
-        String[] cars = splitStringByCommas();
+        String[] cars = splitStringByCommas(stringCarList);
         for (String car : cars) {
             carList.put(car, "");
         }
         return carList;
     }
 
-    public String[] splitStringByCommas() {
-        String[] carList = car.getStringCarList().split(",");
+    public String[] splitStringByCommas(String stringCarList) {
+        String[] carList = stringCarList.split(",");
         return carList;
     }
 
-    public void inputCarList() {
-        car.setStringCarList(Console.readLine());
+    public String inputCarList() {
+        return Console.readLine();
     }
-
-
-
-
 }
