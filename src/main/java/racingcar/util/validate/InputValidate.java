cd8e5651f395353807dae57caf39e39ad5d1a30b
Hayoon <hy97@sju.ac.kr>
2023-10-29 17:21:15 +0900

    feat(InputValidate): 예외처리 Integer 값을 상수로 치환한다

--- src/main/java/racingcar/util/validate/InputValidate.java
+++ src/main/java/racingcar/util/validate/InputValidate.java
@@ -1,7 +1,5 @@
 package racingcar.util.validate;
 
-import racingcar.util.constant.ExceptionConstant;
-
 import static racingcar.util.constant.ExceptionConstant.*;
 
 public class InputValidate {
@@ -20,13 +18,13 @@ public class InputValidate {
     }
 
     static public void validateInputNumberRange(int number) {
-        if(number < 1) {
+        if(number < MINIMUM_NUMBER_VALUE) {
             throw new IllegalArgumentException(INPUT_VALID_NUMBER_RANGE);
         }
     }
 
     static public void validateNumberLength(String name) {
-        if(name.length() > 5) {
+        if(name.length() > MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(INPUT_VALID_NAME_LENGTH);
         }
     }
