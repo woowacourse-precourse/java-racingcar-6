b1568fe67c3115105807a4c6c46329e5e1f0ea5a
hyunjin <guswlsdl0121@gmail.com>
2023-10-29 23:40:29 +0900

    refactor: DTO변환 메서드를 팩토리로 생성

---
+++ src/main/java/racingcar/common/util/DTOFactory.java
@@ -0,0 +1,15 @@
+package racingcar.common.util;
+
+import java.util.List;
+import racingcar.dto.output.RoundDTO;
+import racingcar.dto.output.WinnerDTO;
+
+public class DTOFactory {
+    public static RoundDTO createRoundDTO(String name, int distance) {
+        return new RoundDTO(name, distance);
+    }
+
+    public static WinnerDTO createWinnerDTO(List<String> winnerNames) {
+        return new WinnerDTO(winnerNames);
+    }
+}
