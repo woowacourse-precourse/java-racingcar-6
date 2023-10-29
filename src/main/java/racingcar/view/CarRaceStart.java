c9305611c192856fd7dc2491b44a99da195bf4cc
M1NS1K <rlaalstlr2001@gmail.com>
2023-10-29 09:41:43 +0900

    CarRaceStart 구현

--- src/main/java/racingcar/view/CarRaceStart.java
+++ src/main/java/racingcar/view/CarRaceStart.java
@@ -1,11 +1,17 @@
-package racingcar.controller;
+package racingcar.view;
 
+import racingcar.controller.CarAction;
 import racingcar.model.CarObject;
+import racingcar.model.Message;
 
 public class CarRaceStart {
     public static void exe() {
         CarAction car = new CarAction();
+
+        System.out.println(Message.INPUT_CAR_QUESTION);
         car.inputCarName();
+
+        System.out.println(Message.INPUT_EPOCH_QUESTION);
         car.inputEpoch();
 
         for(int i = 0; i < CarObject.epoch; i++) {
