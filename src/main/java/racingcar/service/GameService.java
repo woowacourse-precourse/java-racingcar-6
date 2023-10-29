805163e5efbed23e0e687aa74c99c8c13d0651c0
chs98412 <chs98412@gmail.com>
2023-10-29 18:53:50 +0900

    feat: Game init 작성

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,6 +1,10 @@
 package racingcar.service;
 
+import racingcar.entity.Game;
 import racingcar.repository.GameRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GameService {
 
@@ -10,5 +14,13 @@ public class GameService {
     public GameService(GameRepository gameRepository, CarService carService) {
         this.gameRepository = gameRepository;
         this.carService = carService;
+    }
+
+    public void initGame(String nameInput, Integer playCount) {
+        Game game = gameRepository.save(playCount);
+        List<String> nameList = Arrays.stream(nameInput.split(",")).toList();
+        for (String name : nameList) {
+            game.addCar(carService.carCreate(name));
+        }
     }
 }
