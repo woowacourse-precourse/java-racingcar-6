315f96df04f6d95da442615914efd01c97c1a3a1
leesh <qwqw8019@naver.com>
2023-10-29 18:12:15 +0900

    test: 시도 회수 입력 기능 테스트

--- src/main/java/racingcar/util/TestConstantsProvider.java
+++ src/main/java/racingcar/util/TestConstantsProvider.java
@@ -5,10 +5,16 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static racingcar.constant.TestConstants.BLANK_STRING;
 import static racingcar.constant.TestConstants.EMPTY_STRING;
 import static racingcar.constant.TestConstants.EXPECTED_ONE_CAR_NAME;
+import static racingcar.constant.TestConstants.EXPECTED_PLAY_FIVE_TIMES;
+import static racingcar.constant.TestConstants.EXPECTED_PLAY_ONE_TIME;
 import static racingcar.constant.TestConstants.EXPECTED_THREE_CAR_NAMES;
 import static racingcar.constant.TestConstants.LONGER_THAN_FIVE_STRING;
 import static racingcar.constant.TestConstants.NULL_STRING;
 import static racingcar.constant.TestConstants.ONE_CAR_NAME;
+import static racingcar.constant.TestConstants.PLAY_FIVE_TIMES;
+import static racingcar.constant.TestConstants.PLAY_NEGATIVE_TIME;
+import static racingcar.constant.TestConstants.PLAY_NO_TIME;
+import static racingcar.constant.TestConstants.PLAY_ONE_TIME;
 import static racingcar.constant.TestConstants.THREE_CAR_NAMES;
 
 import java.util.stream.Stream;
@@ -32,4 +38,19 @@ public class TestConstantsProvider {
                 arguments(named("5자 이상의 문자열", LONGER_THAN_FIVE_STRING))
         );
     }
+
+    public static Stream<Arguments> provideValidPlayNumberConstants() {
+        return Stream.of(
+                arguments(named("1", PLAY_ONE_TIME), EXPECTED_PLAY_ONE_TIME),
+                arguments(named("5", PLAY_FIVE_TIMES), EXPECTED_PLAY_FIVE_TIMES)
+        );
+    }
+
+    public static Stream<Arguments> provideInvalidPlayNumberConstants() {
+        return Stream.of(
+                arguments(named("0", PLAY_NO_TIME)),
+                arguments(named("0보다 작은 값", PLAY_NEGATIVE_TIME))
+        );
+    }
+
 }
--- src/test/java/racingcar/ConsoleInputTest.java
+++ src/test/java/racingcar/ConsoleInputTest.java
@@ -4,7 +4,9 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.util.TestConstantsProvider.provideInvalidCarNameConstants;
+import static racingcar.util.TestConstantsProvider.provideInvalidPlayNumberConstants;
 import static racingcar.util.TestConstantsProvider.provideValidCarNameConstants;
+import static racingcar.util.TestConstantsProvider.provideValidPlayNumberConstants;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
@@ -62,4 +64,46 @@ public class ConsoleInputTest {
             System.out.println(ConsoleInput.inputCarNames());
         }
     }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    @DisplayName("시도 회수 입력 테스트")
+    class PlayNumberTest extends NsTest {
+
+        @ParameterizedTest(name = "시도 회수 {0}을 입력한다.")
+        @DisplayName("성공 테스트")
+        @MethodSource("provideValidPlayNumber")
+        void successTest(String validPlayNumber, String expectedPlayNumber) {
+            assertSimpleTest(() -> {
+                        run(validPlayNumber);
+                        assertThat(output()).isEqualTo(expectedPlayNumber);
+                    }
+            );
+        }
+
+        @ParameterizedTest(name = "{0}을 입력하면 예외가 발생한다.")
+        @DisplayName("예외 테스트")
+        @MethodSource("provideInvalidPlayNumber")
+        void exceptionTest(String invalidPlayNumber) {
+            assertSimpleTest(
+                    () -> assertThatThrownBy(() -> runException(invalidPlayNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+
+        private Stream<Arguments> provideValidPlayNumber() {
+            return provideValidPlayNumberConstants();
+        }
+
+        private Stream<Arguments> provideInvalidPlayNumber() {
+            return provideInvalidPlayNumberConstants();
+        }
+
+        @Override
+        protected void runMain() {
+            System.out.println(ConsoleInput.inputPlayNumber());
+        }
+    }
+
 }
