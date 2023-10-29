b96488184ef7444dd91979a87c7b22e2bcb38793
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:38:31 +0900

    feat(domain/system): CarManager 선언

---
+++ src/main/java/racingcar/domain/system/manager/car/CarManager.java
@@ -0,0 +1,16 @@
+package racingcar.domain.system.manager.car;
+
+import java.util.List;
+import racingcar.domain.core.car.Car;
+import racingcar.domain.core.car.CarName;
+
+public interface CarManager {
+
+    SavedCar save(Car car);
+
+    List<SavedCar> saveAll(List<Car> cars);
+
+    SavedCar findByName(CarName carName);
+
+    List<SavedCar> findAll();
+}
