1dcfc546900e0fa887c20ae976debc2bc3b6cc67
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 00:52:41 +0900

    feat(GameController): 자동차들의 이름을 입력받아 Cars 객체 생성 메서드 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,8 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.util.InputParser;
 import racingcar.view.InputView;
@@ -8,6 +11,12 @@ import racingcar.view.OutputView;
 public class GameController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+
+    private Cars generateCars() {
+        final String carName = inputView.inputCars();
+        final List<Car> cars = InputParser.carsSplit(carName);
+        return new Cars(cars);
+    }
 
     private TryCount generateTryCount() {
         final String input = inputView.inputTryCount();
