17ecae178b15c3616935478a8ecb1033693eb825
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-29 22:15:01 +0900

    feat: 중복된 자동차 이름 확인하는 코드 추가

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -1,6 +1,8 @@
 package racingcar.validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarNameValidator {
 
@@ -9,6 +11,14 @@ public class CarNameValidator {
             if (name.length() >= 6){
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    public static void checkDuplicated(List<String> carNameList){
+        Set<String> carNameSet = new HashSet<>(carNameList);
+
+        if (carNameList.size() != carNameSet.size()){
+            throw new IllegalArgumentException();
         }
     }
 }
