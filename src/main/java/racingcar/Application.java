62321f9acdae64a80d404563dabdde68e1f0ead3
song-jae-wan <sjo9810@gmail.com>
2023-10-29 15:24:42 +0900

    Refactor: main에서 RacingGame 객체 관리
    main에서 게임 실행을 의미하는 객체의 생성과 실행, 삭제 모두 관리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,8 @@ import game.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame game = new RacingGame();
+        RacingGame game = RacingGame.getInstance();
         game.run();
+        game.close();
     }
 }
