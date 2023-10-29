917e22d286cc9c768d1efd258ea2007b4f411b41
Lee ChanHui <ing3616062@naver.com>
2023-10-29 17:23:28 +0900

    feat: Player 객체 생성
    \n
    Player 객체 생성

---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class RacingCarGame {
+
+    public void run(){
+
+    }
+}
---
+++ src/main/java/racingcar/domain/Player.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class Player {
+
+    private String name;
+    private int runCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRunCount() {
+        return runCount;
+    }
+
+    public void setRunCount(int runCount) {
+        this.runCount = runCount;
+    }
+}
