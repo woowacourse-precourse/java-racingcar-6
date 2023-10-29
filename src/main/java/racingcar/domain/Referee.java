b31904143c5ed558a34978e7d56a3f7dad183fce
PUNGZI <ashyoo1367@gmail.com>
2023-10-29 19:41:40 +0900

    style: 함수가 한 가지 일만 하도록 수정

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -9,14 +9,17 @@ public class Referee {
     public String callWinnerCar() {
         Racing racing = new Racing();
         Map<String, Integer> racingResult = racing.racingMoveCount();
+        return "최종 우승자 : " + String.join(", ", winnerCar(racingResult));
+    }
+
+    public List<String> winnerCar(Map<String, Integer> racingResult) {
         List<String> winnerCar = new ArrayList<>();
         int racingResultMax = Collections.max(racingResult.values());
-
         for (String key : racingResult.keySet()) {
             if (racingResult.get(key) == racingResultMax) {
                 winnerCar.add(key);
             }
         }
-        return "최종 우승자 : " + String.join(", ", winnerCar);
+        return winnerCar;
     }
 }
