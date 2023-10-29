5f25f1f178c6b3673e09d396f65f72f988970abe
seohyun kwon <seohyunkwon98@gmail.com>
2023-10-29 23:47:56 +0900

    Refactor: RacingController 패키지 변경

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,64 @@
+package racingcar.controller;
+
+import racingcar.utils.InputValidator;
+import racingcar.utils.RandomNumberGenerater;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingController {
+    private final Cars cars;
+    private Race race;
+    private final RandomNumberGenerater randomNumberGenerater;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final InputValidator validator;
+
+    public RacingController(Cars cars, Race race, RandomNumberGenerater randomNumberGenerater, InputView inputView, OutputView outputView, InputValidator validator) {
+        this.cars = cars;
+        this.race = race;
+        this.randomNumberGenerater = randomNumberGenerater;
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.validator = validator;
+    }
+
+    public void start() {
+        cars.generateCars(getCars());
+        int times = getTimes();
+        race = new Race(cars, times);
+        while (race.run()) {
+            printProcess();
+        }
+        List<String> winnerList = race.findWinner();
+        String winners = winnerList.stream()
+                        .collect(Collectors.joining(", "));
+        outputView.printWinner(winners);
+    }
+
+    public void printProcess() {
+        List<Car> carList = cars.getCars();
+        StringBuilder process = new StringBuilder();
+        carList.forEach(car -> process.append(car.info()));
+        outputView.printProcess(process.toString());
+    }
+
+    public String getCars() {
+        outputView.printInputCars();
+        String carsInput = inputView.inputCars();
+        validator.validateCars(carsInput);
+        return carsInput;
+    }
+
+    public int getTimes() {
+        outputView.printInputTimes();
+        String timesInput = inputView.inputTimes();
+        validator.validateTimes(timesInput);
+        return Integer.parseInt(timesInput);
+    }
+}
