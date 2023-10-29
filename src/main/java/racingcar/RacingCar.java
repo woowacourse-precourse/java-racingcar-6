605461efb6647c95ce15e75f51bfd3f5ebf64125
남수연 <rnrmf0721@gmail.com>
2023-10-29 23:37:05 +0900

    style(RacingCar): 9번째 줄 개행 삭제

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -5,8 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import static racingcar.constant.Constant.DISTANCE_DRIVER;
 
 public class RacingCar extends Car {
-
-
+    
     public RacingCar(String name) {
         super(name);
     }
