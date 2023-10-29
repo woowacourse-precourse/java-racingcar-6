411a16f24d04af9aa3b32a7b308250c899fa437c
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-29 23:17:37 +0900

    🎯feat(domain / Car) : 중복 검사 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -12,6 +12,7 @@ public class Car {
     private String carName;
     private Map<String, Integer> car;
     public Car(String carName) {
+        validateDuplicate(carName);
         this.carName = carName;
     }
 
@@ -25,5 +26,12 @@ public class Car {
     public void backward() {
         int location = car.get(carName);
         car.put(carName,--location);
+    }
+
+    // car 중복 검사
+    public void validateDuplicate(String carName) {
+        if (Cars.cars.contains(car)) {
+            throw new IllegalArgumentException("중복된 이름의 자동차는 동시에 경기할 수 없습니다.");
+        }
     }
 }
