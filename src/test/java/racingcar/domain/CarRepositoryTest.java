ff8ae4a8718e33eb1f3da46ae35bc7e043047a8b
jongmee <ra123221@gmail.com>
2023-10-30 02:18:25 +0900

    test: 레포지토리 테스트하기
    - 중복되는 코드를 줄이고 다양한 parameter로 테스트하고자 ParameterizedTest를 도입하였다.

---
+++ src/test/java/racingcar/domain/CarRepositoryTest.java
@@ -0,0 +1,77 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CarRepositoryTest {
+
+    private final CarRepository carRepository = new CarRepository();
+
+    @AfterEach
+    public void clearRepository() {
+        carRepository.deleteAll();
+    }
+
+    @ParameterizedTest
+    @MethodSource("carNamesAndCount")
+    void 경주차_여러개_생성하기(List<String> nameList, Integer expectedCount) {
+        // given & when
+        for(String name: nameList) {
+            carRepository.save(name);
+        }
+
+        // then
+        assertEquals(expectedCount, carRepository.getCarCount());
+    }
+
+    @ParameterizedTest
+    @MethodSource("carNamesAndCount")
+    void 경주차_여러개_생성하고_모두_조회하기(List<String> nameList, Integer expectedCount) {
+        // given
+        for(String name: nameList) {
+            carRepository.save(name);
+        }
+
+        // when
+        List<Car> carList = carRepository.findAll();
+
+        // then
+        assertThat(carList).hasSize(expectedCount)
+            .extracting(Car::getName)
+            .containsAll(nameList);
+    }
+
+    static Stream<Arguments> carNamesAndCount() {
+        return Stream.of(
+            Arguments.arguments(List.of("jongmi", "meoru"), 2),
+            Arguments.arguments(List.of("gari"), 1),
+            Arguments.arguments(List.of("gari", "meoru", "zzol"), 3)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void 경주차_전진횟수_수정하기(int repetition) {
+        // given
+        Car car = carRepository.save("jongmi");
+
+        // when
+        for(int i = 0; i < repetition; i++) {
+            car.updateForwardCount();
+        }
+        carRepository.update(car);
+
+        // then
+        Car updatedCar = carRepository.findById(car.getId());
+        assertEquals(repetition, updatedCar.getForwardCount());
+    }
+
+}
