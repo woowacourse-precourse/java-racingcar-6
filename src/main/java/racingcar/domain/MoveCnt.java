4387fa694270e51d958a796769f9357d2201f143
YuJeongYun <yunyj99@naver.com>
2023-10-30 00:55:56 +0900

    feat: 이동 횟수 원시값 포장

---
+++ src/main/java/racingcar/domain/MoveCnt.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import racingcar.constant.ErrorMessage;
+
+public class MoveCnt {
+    private int moveCnt;
+
+    public MoveCnt() {
+    }
+    public MoveCnt(String moveCnt) {
+        validateMoveCnt(moveCnt);
+        this.moveCnt = Integer.parseInt(moveCnt);
+    }
+
+    public void validateMoveCnt(String str) {
+        if (str.length() == 0 || str == null) { // null 또는 공백 입력할 경우 예외 발생
+            throw new IllegalArgumentException(ErrorMessage.MOVE_CNT_BLANK_ERR_MESSAGE);
+        }
+        if (isNotNumber(str)) { // 숫자 아니면 예외 발생
+            throw new IllegalArgumentException(ErrorMessage.MOVE_CNT_NOT_NUMBER_ERR_MESSAGE);
+        }
+        if(Integer.parseInt(str) < 0) { // 음수이면 예외 발생
+            throw new IllegalArgumentException(ErrorMessage.MOVE_CNT_NEGATIVE_ERR_MESSAGE);
+        }
+    }
+
+    public boolean isNotNumber(String str) {
+        try {
+            int i = Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
+        return false;
+    }
+}
