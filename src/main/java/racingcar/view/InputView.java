7d8150e7ce169ce20410f5dff70a8af80c066db1
DongKey777 <higgs95@naver.com>
2023-10-29 20:59:14 +0900

    feat: 시도할 횟수 입력받는 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -14,4 +14,18 @@ public class InputView {
         }
         return carNames;
     }
+
+    public static int readRacingTryCount() {
+        String count = readLine();
+        validateNumber(count);
+        return Integer.parseInt(count);
+    }
+
+    private static void validateNumber(String input) {
+        for (int k = 0; k < input.length(); k++) {
+            if (Character.isDigit(input.charAt(k))) {
+                throw new IllegalArgumentException("숫자가 아닌 문자가 입력되었습니다.");
+            }
+        }
+    }
 }
