f59704350f32c44a8e8d26dce93e37393dfa1066
strongcookdas <goodjav141592@gmail.com>
2023-10-30 02:03:23 +0900

    feat : 차 이름이 담긴 배열을 Map으로 변환하는 convertToCarMap 메서드 구현 - 차이름 유효성 체크 - String 배열에서 Map으로 변환

--- src/main/java/racingcar/view/InputManager.java
+++ src/main/java/racingcar/view/InputManager.java
@@ -1,6 +1,8 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class InputManager {
     private final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -21,4 +23,37 @@ public class InputManager {
     }
 
 
+    public Boolean isValidateCarName(String carName, Map<String, Integer> carPositions) {
+        if (isBlankCarName(carName) || isInvalidCarNameLength(carName) || isContainedCarName(carName, carPositions)) {
+            return false;
+        }
+        return true;
+    }
+
+    private Boolean isBlankCarName(String carName) {
+        String pattern = "^\\s*$";
+        return carName.matches(pattern);
+    }
+
+    private Boolean isInvalidCarNameLength(String carName) {
+        if (carName.length() < 1 || carName.length() > 5) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean isContainedCarName(String carName, Map<String, Integer> carPositions) {
+        return carPositions.containsKey(carName);
+    }
+
+    public Map<String, Integer> convertToCarMap(String[] carNames) {
+        Map<String, Integer> carMap = new LinkedHashMap<>();
+        for (String carName : carNames) {
+            if (!isValidateCarName(carName, carMap)) {
+                throw new IllegalArgumentException("Invalid CarName");
+            }
+            carMap.put(carName, 0);
+        }
+        return carMap;
+    }
 }
