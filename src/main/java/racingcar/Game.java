ad13faa2340724a9c888e6fb406eef9a42e03034
siyeonSon <siyeon0209@naver.com>
2023-10-29 18:00:05 +0900

    feat(game): 사용자가 입력한 횟수만큼 전진한다

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -6,12 +6,22 @@ import java.util.stream.Stream;
 
 public class Game {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
     private static final String SEPARATOR = ",";
 
     public void play() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String input = Console.readLine();
         List<Car> cars = splitToCarList(input);
+
+        System.out.println(INPUT_COUNT_MESSAGE);
+        int count = Integer.parseInt(Console.readLine());
+        System.out.println();
+
+        for (int i = 0; i < count; i++) {
+           cars.forEach(Car::forward);
+        }
     }
 
     private List<Car> splitToCarList(String input) {
