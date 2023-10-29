1cee2a0cd81e3b702728ce5d14be49f796ad331d
leemiyeon <lemingul7@gmail.com>
2023-10-29 20:12:41 +0900

    refactor: 차수별 실행 결과 출력 1. RacingMessage: RacingMessage,ResultMessage 통합 - 사용하지 않는 enum 제거

--- src/main/java/racingcar/view/enums/RacingMessage.java
+++ src/main/java/racingcar/view/enums/RacingMessage.java
@@ -2,7 +2,9 @@ package racingcar.view.enums;
 
 public enum RacingMessage {
     ASK_FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    ASK_FOR_ROUNDS("시도할 회수는 몇회인가요?");
+    ASK_FOR_ROUNDS("시도할 회수는 몇회인가요?"),
+    RACING_RESULT("실행 결과"),
+    RACING_WINNER("최종 우승자 : %s");
 
     private final String message;
 
