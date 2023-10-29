1cb9f5bc5d7e4e4d539146886d4b942c25fc85aa
becooq81 <becooq81@gmail.com>
2023-10-30 05:38:07 +0900

    refactor(ValidatorTest): made each test to use the more general method, validateCarNamesInput, instead of the specific methods so that the lower-level methods of Validator class can be protected through encapsulation

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,8 +1,10 @@
 package racingcar.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorTest extends NsTest {
@@ -10,48 +12,44 @@ public class ValidatorTest extends NsTest {
     Validator validator = new Validator();
     @Test
     void 자동차_이름_입력값_비어있음_오류() {
-        assertThatThrownBy(() -> validator.validateInputIsNotEmpty(""))
+        assertThatThrownBy(() -> validator.validateCarNamesInput(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_쉼표로_끝남_오류() {
-        assertThatThrownBy(() -> validator.validateInputDoesNotEndWithComma("a,"))
+        assertThatThrownBy(() -> validator.validateCarNamesInput("a,"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이릅_입력값_공백_있음_오류() {
-        assertThatThrownBy(() -> validator.validateInputDoesNotContainSpace("a b"))
+        assertThatThrownBy(() -> validator.validateCarNamesInput("a b"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_중복_오류() {
-        assertThatThrownBy(() -> validator.validateInputDoesNotContainDuplicate("a,b,a"))
+        assertThatThrownBy(() -> validator.validateCarNamesInput("a,b,a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_5자_초과_오류() {
-        assertThatThrownBy(() -> validator.validateInputIsNotOverLength("123456"))
+        assertThatThrownBy(() -> validator.validateCarNamesInput("123456"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_연속_쉼표_오류() {
-        assertThatThrownBy(() -> validator.validateInputDoesNotContainContinuousComma("a,,b"))
+        assertThatThrownBy(() -> validator.validateCarNamesInput("a,,b"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_성공() {
-        validator.validateInputIsNotEmpty("a");
-        validator.validateInputDoesNotEndWithComma("a");
-        validator.validateInputDoesNotContainSpace("a");
-        validator.validateInputDoesNotContainDuplicate("a");
-        validator.validateInputIsNotOverLength("a");
-        validator.validateInputDoesNotContainContinuousComma("a");
+        List<String> successfulCarNameList = validator.validateCarNamesInput("a,b,c");
+        assertThat(successfulCarNameList).containsExactly("a", "b", "c");
     }
 
     @Override
