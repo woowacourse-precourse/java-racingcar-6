9e19a5b884ebf264e095d2737e0037980e9cac8b
minisyu <iswater.kim@gmail.com>
2023-10-29 14:05:18 +0900

    refactor(InputValidatorTest): 쉼표로 구분된 문자열 검증 로직 및 메소드명 변경

--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,17 +15,15 @@ class InputValidatorTest {
         inputValidator = new InputValidator();
     }
 
-    @DisplayName("쉼표 기준으로 구분된 문자열 검증 성공")
-    @Test
-    void should_Validate_Comma_Separated_String() {
-        // given
-        String input1 = "z,x,c,v";
-        String input2 = "kim,lee,park";
-
+    @ParameterizedTest
+    @DisplayName("쉼표 기준으로 구분된 문자열이 아니면 예외 발생")
+    @ValueSource(strings = {"aaabbbccc", "qq.ee.rrr", "kk|ddd|fff"})
+    void should_Throw_Exception_For_Invalid_Comma_Separated_String(String input) {
         // when
         // then
-        inputValidator.validateInputFormat(input1);
-        inputValidator.validateInputFormat(input2);
+        assertThatThrownBy(() -> inputValidator.validateInputFormat(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Err.INVALID_CAR_NAMES_FORMAT.getMessage());
     }
 
     @ParameterizedTest
@@ -37,7 +34,7 @@ class InputValidatorTest {
         // then
         assertThatThrownBy(() -> inputValidator.validateNonEmpty(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력 문자열은 빈 칸 또는 공백 문자를 포함하면 안됩니다.");
+                .hasMessage(Err.NON_EMPTY_STRING.getMessage());
     }
 
     @ParameterizedTest
@@ -48,6 +45,6 @@ class InputValidatorTest {
         // then
         assertThatThrownBy(() -> inputValidator.validateNumericString(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("1이상의 양수로 된 문자열만 입력할 수 있습니다.");
+                .hasMessage(Err.NUMERIC_STRING_ONLY.getMessage());
     }
 }
