b790f8d86c4673a776899c1bfaea255f3714ab71
WooYoungDoo <duden5000@naver.com>
2023-10-29 18:00:53 +0900

    feat (RacingCarGame) : 게임 회수 증감 기능

--- src/main/java/racingcar/model/RacingCarGame.java
+++ src/main/java/racingcar/model/RacingCarGame.java
@@ -4,7 +4,15 @@ public class RacingCarGame {
 
     private int count;
 
+    public int getCount() {
+        return count;
+    }
+
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public void minusOneCount() {
+        this.count--;
     }
 }
