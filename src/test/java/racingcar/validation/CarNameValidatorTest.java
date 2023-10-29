a0db76a4c4347cb90c8ac6f0657986ee4a109439
Nara Park <skfk6352@naver.com>
2023-10-29 23:05:34 +0900

    <test>: test for car name exceeds 5 characters

---
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -0,0 +1,18 @@
+package racingcar.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CarNameValidatorTest {
+
+    CarNameValidator carNameValidator = new CarNameValidator();
+
+    @Test
+    void 자동차_이름이_5자를_초과할경우_예외발생() {
+        assertThrows(IllegalArgumentException.class,
+                () -> carNameValidator.validateCarName(List.of("lovejava")));
+
+    }
+}
