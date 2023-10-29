df2184d3b3edd59b7c4dbb130cbf6f3296e099a3
k9want <zndwlrnjr26@daum.net>
2023-10-30 07:35:54 +0900

    test: 가장 많이 전진한 자동차 계산하기

--- src/test/java/racingcar/controller/RacingControllerTest.java
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -1,8 +1,12 @@
 package racingcar.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,16 +16,39 @@ class RacingControllerTest {
     @DisplayName("랜덤한 수별로 이동 처리하기")
     public void testManageCarMovement() {
         // Given
-        RacingController controller = new RacingController();
         Car car1 = new Car("Car1");
         Car car2 = new Car("Car2");
 
         // When
-        controller.manageCarMovement(car1, 4); // move
-        controller.manageCarMovement(car2, 3); // stop
+        RacingController.manageCarMovement(car1, 4); // move
+        RacingController.manageCarMovement(car2, 3); // stop
 
         // Then
         assertThat(car1.getPosition()).isEqualTo(1);
         assertThat(car2.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("가장 많이 전진한 자동차 계산하기")
+    public void testComputeRacingWinner() {
+        //given
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("CarA"));
+        cars.add(new Car("CarB"));
+        cars.add(new Car("CarC"));
+        cars.add(new Car("CarD"));
+
+        for (int i = 0; i < 3; i++) {
+            cars.get(0).moveCar();
+            cars.get(2).moveCar();
+        }
+        cars.get(1).moveCar();
+
+        //when
+        List<String> result = RacingController.computeRacingWinner(cars);
+
+        //then
+        List<String> expected = List.of("CarA", "CarC");
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
