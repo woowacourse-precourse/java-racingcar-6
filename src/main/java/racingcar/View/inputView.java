76ccfdf1ffcb6ec31ef38d73dc1b0aa4042dcd76
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-29 23:16:55 +0900

    refactor(inputView): displayGameStartMessage 메서드 수정

--- src/main/java/racingcar/View/inputView.java
+++ src/main/java/racingcar/View/inputView.java
@@ -1,8 +1,12 @@
 package racingcar.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class inputView {
-    public void displayGameStartMessage(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    private static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public String displayGameStartMessage(){
+        System.out.println(INPUT_CARS_NAME_MESSAGE);
+        return Console.readLine();
     }
 
     public void displayPlayCountMessage(){
