a12a0250af902772282b12b6e73690840ccf0455
HeeHeeg <qmffndoden@naver.com>
2023-10-29 17:41:15 +0900

    git commit -m "refactor(racingcar): 사용자는 몇 번의 이동을 할 것인지를 입력 받는 메서드 분리와 메서드 순서변경"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,14 +14,7 @@ public class Application {
 
         List<Car> carList = createCars(names);
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        String inputNumber = Console.readLine();
-        int attemptsNumber;
-        try {
-            attemptsNumber = Integer.parseInt(inputNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 횟수는 숫자만 가능합니다.");
-        }
+        int attemptsNumber = getAttemptsNumber();
 
         System.out.println();
         System.out.println("실행 결과");
@@ -33,6 +26,13 @@ public class Application {
         String winners = String.join(",", result);
         System.out.println("최종 우승자 : " + winners);
     }
+
+    private static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputName = Console.readLine();
+        return Arrays.asList(inputName.split(","));
+    }
+
 
     private static List<Car> createCars(List<String> names) {
         List<Car> carList = new ArrayList<>();
@@ -49,10 +49,14 @@ public class Application {
         }
     }
 
-    private static List<String> getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputName = Console.readLine();
-        return Arrays.asList(inputName.split(","));
+    private static int getAttemptsNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNumber = Console.readLine();
+        try {
+            return Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자만 가능합니다.");
+        }
     }
 
     private static void racing(List<Car> carList) {
