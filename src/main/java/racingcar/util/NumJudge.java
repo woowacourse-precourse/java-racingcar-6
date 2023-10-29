89c07ed8aa11895326c9f286f766b16861099467
soozzang <soozzang@inha.edu>
2023-10-29 21:41:35 +0900

    Refactor(Numjudge): NumJudge 삭제

--- src/main/java/racingcar/util/NumJudge.java
+++
@@ -1,9 +0,0 @@
-package racingcar.util;
-import racingcar.util.RaceNumGenerator;
-public class NumJudge {
-    RaceNumGenerator raceNumGenerator = new RaceNumGenerator();
-    public boolean judgeNum(){
-        int randomNum = raceNumGenerator.generateNumber();
-        return randomNum >= 4;
-    }
-}
