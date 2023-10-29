cba135bea6c93abddc85c34957b26d21c3e84888
ChaeAg <dydkglel123@gmail.com>
2023-10-29 14:40:50 +0900

    refactor(Cars): 자동차들의 정보를 가지고 게임 결과를 판정하는 클래스 생성

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,44 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static final int MIN_FORWARD_NUMBER = 4;
+    private static final List<Car> carList = new ArrayList<>();
+    private static final List<Car> winner = new ArrayList<>();
+    private static int maxForward = 0;
+
+    public void addCars(List<Car> inputCarList) {
+        carList.addAll(inputCarList);
+    }
+
+    public void oneResult(Car car, int randomNumber) {
+        winner.clear();
+        if(randomNumber >= MIN_FORWARD_NUMBER) {
+            car.upForwardCount();
+        }
+
+        if(car.getForwardCount() > maxForward) {
+            maxForward = car.getForwardCount();
+        }
+
+        setWinner();
+    }
+
+    public List<Car> getWinner() {
+        return winner;
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    private void setWinner() {
+        for(Car car : carList) {
+            if(car.getForwardCount() == maxForward) {
+                winner.add(car);
+            }
+        }
+    }
+}
