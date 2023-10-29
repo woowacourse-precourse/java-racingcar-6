5742215388b3dfe4a1fcaf7cd0b6717c17a7076a
Junobee25 <wnsdhqo@naver.com>
2023-10-29 16:59:43 +0900

    style: 클래스 정의 와 인스턴스 변수 사이 줄바꿈 수정

--- src/main/java/racingcar/service/Move.java
+++ src/main/java/racingcar/service/Move.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Move {
+
     Car car = new Car();
 
     public void moveTheCar(LinkedHashMap<String, String> carList, List<Integer> randomNumberList) {
