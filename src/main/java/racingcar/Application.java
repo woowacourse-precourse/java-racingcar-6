a2360b852898ed56047ebb6b774fec1d38e04433
jwtech05 <takartin@gmail.com>
2023-10-29 16:15:29 +0900

    경주용 자동차 등록 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racers = Console.readLine();
+        String[] racersList = racers.split(",");
+
+        RacingCar[] RacingCars = new RacingCar[racersList.length];
+
+        for(int i = 0; i< racersList.length; i++){
+            RacingCar car = new RacingCar(racersList[i],0);
+            RacingCars[i] = car;
+        }
+
     }
 }
---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class RacingCar {
+    String name;
+    int distance;
+
+    public RacingCar(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+}
