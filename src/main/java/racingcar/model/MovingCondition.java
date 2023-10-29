c0d0790ec6115709c4e62d138b086f3a6e0b9101
GaBaljaintheroom <junsu1222@naver.com>
2023-10-29 11:26:53 +0900

    feat(MovingCondition): MOVING_CONDITION 적용

--- src/main/java/racingcar/model/MovingCondition.java
+++ src/main/java/racingcar/model/MovingCondition.java
@@ -1,15 +1,15 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constants.RacingCarRole;
 
 public class MovingCondition {
 
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
-    private static final int MOVING_CONDITION = 4;
 
     public boolean canMove() {
         int number = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-        return number >= MOVING_CONDITION;
+        return number >= RacingCarRole.MOVING_CONDITION.getValue();
     }
 }
