299b8c60fa5dc6493f15352b7459dc88becf951f
sujnkim <blackout3210@naver.com>
2023-10-29 14:53:21 +0900

    feat: add Car class

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final static int MOVING_STANDARD = 4;
+
+    private final String name;
+    private int location = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+
+    private void move(){
+        int randomNumber = createRandomNumber();
+        if(isMoveFront(randomNumber)){
+            location++;
+        }
+    }
+
+    private int createRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    private boolean isMoveFront(int randomNumber){
+        return randomNumber >= MOVING_STANDARD;
+    }
+
+}
