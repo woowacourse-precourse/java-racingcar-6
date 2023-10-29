b65608b6d0cc401fd33b74183a4c61bd49756347
jeondui <jeondui@gmail.com>
2023-10-29 13:12:38 +0900

    feat(RacingGame): 자동차 경주 게임의 우승자를 저장하는 모델 생성

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,14 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private List<String> winners;
+
+    public RacingGame() {
+        this.winners = new ArrayList<>();
+    }
+
+}
