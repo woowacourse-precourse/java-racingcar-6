387c33041c3f842819c0942a98df0730ad0fe0d6
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-29 22:45:32 +0900

    feat(Car): Car 클래스 생성

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,15 @@
+package racingcar.Model;
+
+public class Car {
+    private String name;
+    private int count;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
