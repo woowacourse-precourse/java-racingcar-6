eb1f21581397e9fc93cbf95fa61a523332308c52
JongMinCh0i <wonderland314592@gmail.com>
2023-10-30 01:23:59 +0900

    feat(Car): 자동차 이름에 대한 검증 로직 작성
    자동차 이름은 5글자 이하만 가능합니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,23 @@ package racingcar.domain;
 
 public class Car {
 
+    private final static int LIMIT_NAME_LENGTH = 6;
+    private final static String CAR_NAME_LENGTH_FIVE_UNDER = "자동차의 이름은 5글자 이하만 가능합니다.";
+
     private final String name;
     private Integer distance = 0;
     private String mileage = "";
 
     public Car(String name) {
         this.name = name;
+        validationNameLength(name);
+    }
+
+    // 자동차 이름은 5글자 이하만 가능합니다.
+    public void validationNameLength(final String name) {
+        if (name.length() >= LIMIT_NAME_LENGTH) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_FIVE_UNDER);
+        }
     }
 
     @Override
