a7822275292b8f53c09731abb16f852ed2e84618
k9want <zndwlrnjr26@daum.net>
2023-10-30 07:45:45 +0900

    test: 출력 - 최종 우승자 : 자동차 이름 표시 (,으로 구분)

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -2,7 +2,10 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class OutputViewTest extends NsTest {
 
@@ -18,6 +21,34 @@ class OutputViewTest extends NsTest {
 
         //then
         Assertions.assertThat(roundResultMessage).isEqualTo("CarA : ---");
+    }
+
+    @Test
+    @DisplayName("출력(여러 명일 경우) - 최종 우승자 : 자동차 이름 표시 (,으로 구분)")
+    public void testPrintRacingWinnerMany() {
+        //given
+        List<String> carNames = List.of("CarA", "CarB");
+
+        //when
+        OutputView.printRacingWinner(carNames);
+        String racingWinnerMessage = output();
+
+        //then
+        Assertions.assertThat(racingWinnerMessage).isEqualTo("최종 우승자 : CarA, CarB");
+    }
+
+    @Test
+    @DisplayName("출력(한 명일 경우) - 최종 우승자 : 자동차 이름 표시 (,으로 구분)")
+    public void testPrintRacingWinnerOne() {
+        //given
+        List<String> carNames = List.of("CarA");
+
+        //when
+        OutputView.printRacingWinner(carNames);
+        String racingWinnerMessage = output();
+
+        //then
+        Assertions.assertThat(racingWinnerMessage).isEqualTo("최종 우승자 : CarA");
     }
 
     @Override
