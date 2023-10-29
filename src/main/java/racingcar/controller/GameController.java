cd4c52a24971779216df3345c6af41e4df1b9014
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-29 18:10:10 +0900

    FEAT / modify to Singlethon

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,14 @@
 package racingcar.controller;
 
 public class GameController {
+    public GameController() {
+    }
+
+    public static GameController getInstance() {
+        return GameController.LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final GameController INSTANCE = new GameController();
+    }
 }
