cba4ccdda6fe4e36b8e0401e4a0c815bbb0814bd
win9 <krrr883@gmail.com>
2023-10-29 23:44:42 +0900

    [Feat]: Car 클래스 생성자 정리
    기본 생성자를 막고 이름 파라미터를 입력항 생성하도록 정리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,4 +2,15 @@ package racingcar.domain;
 
 public class Car {
     private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    protected Car(){
+    }
+
+    public String getName() {
+        return name;
+    }
 }
