2c417202967d42c0a70191b4767005118b4c8a46
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 22:22:09 +0900

    feat(Converter): 문자열을 양의 정수로 변환하는 기능 추가

--- src/main/java/racingcar/converter/RacingConverter.java
+++ src/main/java/racingcar/converter/RacingConverter.java
@@ -9,11 +9,19 @@ public class RacingConverter {
         return List.of(input.split(DELIMITER));
     }
 
-    public int convertToInt(String input) {
+    public int convertToPositiveInt(String input) {
+        int number = convertToInt(input);
+        if (number < 1) {
+            throw new IllegalArgumentException("0보다 커야 합니다.");
+        }
+        return number;
+    }
+
+    private int convertToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("입력값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
 }
