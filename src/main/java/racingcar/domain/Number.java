c618787befe3b2d4016de1c32037156148ee5c6b
jinkshower <askthem@naver.com>
2023-10-29 11:21:38 +0900

    feat(Number): 랜덤 숫자 생성기능 구현
    이후 해당 클래스에 필요한 내용 주석처리

--- src/main/java/racingcar/domain/Number.java
+++ src/main/java/racingcar/domain/Number.java
@@ -1,10 +1,28 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Number {
-    public Number generateRandomNumber() {
-        return new Number();
+    private int number;
+
+    private Number(int randomNumber) {
+        this.number = randomNumber;
     }
-    public boolean isEqualOrGreaterThanFour(Number generatedNumber) {
+    public static Number generateRandomNumber() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        return new Number(randomNumber);
+    }
+    // For later Usage
+    //    private Number(String userInput) {
+//        validate userInput
+//        this.number = convertedNumber
+//    }
+//    public static Number inputPlayerNumber() {
+//        String userInput = askHowManyRoudns();
+//        return new Number(userInput);
+//    }
+
+    public boolean isEqualOrGreaterThanFour(Number generatedRandomNumber) {
         return false;
     }
 }
