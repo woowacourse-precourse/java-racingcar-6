fe8104fe6b1149a89e884bc2f9def7c688a70614
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-30 05:01:32 +0900

    fix(ConsoleOutputView): 자동차 이름 입력 요청 메시지 오타 수정

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 import racingcar.dto.CarDto;
 
 public class ConsoleOutputView implements OutputView{
-    private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_GAME_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
