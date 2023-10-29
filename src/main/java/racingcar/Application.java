5a7ba94cb7117db5b6cc9662aff3dc6a29420744
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-30 01:53:26 +0900

    feat : Application 에서 RaceController에 RaceService 주입 후, raceGame 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.race.RaceController;
+import racingcar.domain.race.RaceService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceController raceController = RaceController.createRaceController(RaceService.getInstance());
+
+        raceController.startRaceGame();
+        Console.close();
     }
 }
