5af70aa4f724b9d836b927754a282200533bbfcf
HyunShik <hshong0907@gmail.com>
2023-10-29 18:20:53 +0900

    feat: Car move에 따른 우선순위 큐 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -3,10 +3,12 @@ package domain;
 import util.CarList;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Cars {
 
     private CarList<Car> cars;
+    private PriorityQueue<Car> carMoveCountPQ;
 
     public Cars(CarList<Car> cars){
         this.cars = cars;
@@ -14,5 +16,13 @@ public class Cars {
 
     public CarList<Car> getCars(){
         return cars;
+    }
+
+    public void setCarMoveCountPQ(PriorityQueue<Car> carMoveCountPQ){
+        this.carMoveCountPQ = carMoveCountPQ;
+    }
+
+    public PriorityQueue<Car> getCarMoveCountPQ(){
+        return carMoveCountPQ;
     }
 }
