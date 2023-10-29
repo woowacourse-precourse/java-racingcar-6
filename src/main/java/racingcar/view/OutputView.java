7595b203462d93a7dcda59aac03676edc72cb97e
seona-moon <fhflwhwl5@gmail.com>
2023-10-29 16:25:46 +0900

    feat: 사용자의 input을 안내하기 위한 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,10 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+public class OutputView {
+  public void displayCarName(){
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+  }
+  public void displayGameCount(){
+    System.out.println("시도할 회수는 몇회인가요?");
+  }
 }
