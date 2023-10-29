4f697f6d92345757b8a4ca2a922448a00c920b02
박승민 <tmddudals369@naver.com>
2023-10-30 04:17:44 +0900

    refactor(randomNumberGenerator): 생성자 및 메서드의 접근 제어자 수정
    유틸리티 클래스의 객체 생성 제한 및 정적 메서드 제공

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -7,7 +7,10 @@ public class RandomNumberGenerator {
     private static final int RANDOM_NUMBER_MIN_VALUE = 0;
     private static final int RANDOM_NUMBER_MAX_VALUE = 9;
 
-    public int generate() {
+    private RandomNumberGenerator() {
+    }
+
+    public static int generate() {
         return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_VALUE, RANDOM_NUMBER_MAX_VALUE);
     }
 }
