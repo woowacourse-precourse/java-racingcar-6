0c2a718c704d74f03ee0ccb124a65b56e0fdd0ef
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 23:28:33 +0900

    test(announcer): 자동차들의 현재 위치가 정상적으로 출력되는지 테스트

---
+++ src/test/java/racingcar/AnnouncerTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class AnnouncerTest extends NsTest {
+    private final Announcer announcer = new Announcer();
+    private final Car pobi = new Car("pobi");
+    private final Car woni = new Car("woni");
+
+
+    @Test
+    void 자동차들의_위치를_정상적으로_출력() {
+        // given
+        pobi.moveForward();
+        woni.moveForward();
+        woni.moveForward();
+
+        // when -> announceRoundResult()
+
+        // then
+        assertSimpleTest(() -> {
+            announcer.announceRoundResult(List.of(pobi, woni));
+            assertThat(output()).contains("pobi : -", "woni : --");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        announcer.announceRoundResult(List.of(pobi, woni));
+    }
+}
