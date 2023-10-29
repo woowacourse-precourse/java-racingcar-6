d7380ea8609ac778329fc0cbea7584cb8b5f9f39
jaegyeongkim <dongji_11@naver.com>
2023-10-29 15:26:22 +0900

    feat(Cars): String 배열을 Car 객체 리스트로 변환하는 기능 구현

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -7,10 +7,14 @@ import utils.Utils;
 
 public class Cars {
     private List<Car> cars;
-    Utils utils= new Utils();
+    Utils utils = new Utils();
 
     public Cars(String input) {
-        cars = Arrays.stream(utils.splitByDelimiter(input))
+        cars = convertStringArrayToCars(input);
+    }
+
+    public List<Car> convertStringArrayToCars(String input) {
+        return Arrays.stream(utils.splitByDelimiter(input))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
