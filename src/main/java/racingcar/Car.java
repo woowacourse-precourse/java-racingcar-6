a343d023a24606117560fff813d982e4e47c08f4
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-29 20:53:41 +0900

    feat(Car) : create car class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+public class Car {
+    String name;
+    Integer location;
+    Integer currentValue;
+    Boolean isWinner;
+    Car(String name){
+        this.name = name;
+        location = 0;
+        currentValue = 0;
+        isWinner = false;
+    }
+    public void advance(Integer movingDistance){
+        location += movingDistance;
+    }
+    public Integer getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCurrentValue(Integer currentValue) {
+        this.currentValue = currentValue;
+    }
+    public Integer getCurrentValue() {
+        return currentValue;
+    }
+}
