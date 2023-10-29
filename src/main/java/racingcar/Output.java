f060d16388ac930f25dc72a1730d018d67ea79be
2021110806 <2021110806@dgu.ac.kr>
2023-10-29 15:55:39 +0900

    feat(Output.java): change printRacingMap method's input type printRacingMap 의 입력 인자를 ArrayList<Car> 에서 LinkedHashMap<String, Integer>로 변경하였습니다. 함수들을 static 으로 변경하였습니다.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -2,36 +2,38 @@ package racingcar;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Output {
-    private final String CAR_NAME_INPUT_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String TRY_NUMBER_INPUT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
-    private final String RESULT_TITLE = "실행 결과";
-    private final String WINNERS = "최종 우승자 : ";
+    private static final String CAR_NAME_INPUT_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_NUMBER_INPUT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_TITLE = "실행 결과";
+    private static final String WINNERS = "최종 우승자 : ";
 
-    void printCarNameInputRequest() {
+    static void printCarNameInputRequest() {
         System.out.println(CAR_NAME_INPUT_REQUEST_MESSAGE);
     }
 
-    void printTryNumberInputRequest() {
+    static void printTryNumberInputRequest() {
         System.out.println(TRY_NUMBER_INPUT_REQUEST_MESSAGE);
     }
 
-    void printResultTitle() {
+    static void printResultTitle() {
         System.out.println(RESULT_TITLE);
     }
 
-    void printRacingMap(ArrayList<Car> cars) {
-        for (Car car : cars) {
-            System.out.println(car.getName()+" : "+"-".repeat(car.getLocation()));
+    static void printRacingMap(LinkedHashMap<String, Integer> cars) {
+        for (String carName : cars.keySet()) {
+            System.out.println(carName+" : "+"-".repeat(cars.get(carName)));
         }
+        System.out.println();
     }
 
-    boolean is_last(String winner, ArrayList<String> winners) {
+    static boolean is_last(String winner, ArrayList<String> winners) {
         return winner.equals(winners.get(winners.size() - 1));
     }
 
-    void printWinners(ArrayList<String> winners) {
+    static void printWinners(ArrayList<String> winners) {
         System.out.print(WINNERS);
         for (String winner : winners) {
             if (is_last(winner, winners)) {
