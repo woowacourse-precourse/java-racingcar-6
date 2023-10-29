fc24ddceff84dec0d1e630a43b141930a94fe9d1
minisyu <iswater.kim@gmail.com>
2023-10-29 13:31:22 +0900

    test/refactor(CarNameTest): 테스트 메소드명 변경

--- src/test/java/racingcar/domain/game/CarNameTest.java
+++ src/test/java/racingcar/domain/game/CarNameTest.java
@@ -12,7 +12,7 @@ class CarNameTest {
     @ParameterizedTest
     @DisplayName("자동차 이름이 null 또는 길이가 0인 경우, CarName 객체 생성 시 예외 발생")
     @ValueSource(strings = {"", " "})
-    void checkCarName(String name) {
+    void should_Throw_Exception_For_Null_Or_Empty_CarName(String name) {
         // when
         // then
         assertThatThrownBy(() -> CarName.from(name))
@@ -23,7 +23,7 @@ class CarNameTest {
     @ParameterizedTest
     @DisplayName("자동차 이름의 길이가 5글자를 넘어가면 CarName 객체 생성 시 예외 발생")
     @ValueSource(strings = {"rrrrrrrr", "pobiii", "yehhhhhhhhhhhhhhhh"})
-    void checkCarNameMaxLength(String name) {
+    void should_Throw_Exception_For_Exceeding_CarName_Max_Length(String name) {
         // when
         // then
         assertThatThrownBy(() -> CarName.from(name))
