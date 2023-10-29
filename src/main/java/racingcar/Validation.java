d054e10fbd28b2477a9dc44c001a370df806cda8
dk7648 <dk7648@korea.ac.kr>
2023-10-29 09:44:44 +0900

    feat(Validation): validLength 스켈레톤 코드 추가

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
 public class Validation {
     public static boolean validNames(String names) {
+        return false;
+    }
+    public static boolean validLength(List<String> nameList) {
+        //각 이름이 5자 이하로 되어 있는지 확인
         return false;
     }
 
