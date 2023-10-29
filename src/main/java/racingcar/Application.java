11bda0fe4f2f52616ffe85b056b6562b7ba1a35f
minseokKim6823 <alstjr6823@naver.com>
2023-10-29 16:41:24 +0900

    시도 횟수 입력시 숫자가 아닐 때 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import racingcar.controller.InputTries;
+
 import java.util.List;
 
 import static racingcar.controller.InputParticipantes.InputParticipantes;
@@ -10,6 +12,10 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> participantes=InputParticipantes();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tries=InputTries.getTries();
+        System.out.println();
+        System.out.println("실행 결과");
     }
 }
 //pobi,woni,jun
--- src/main/java/racingcar/controller/InputTries.java
+++ src/main/java/racingcar/controller/InputTries.java
@@ -1,2 +1,15 @@
-package racingcar.controller;public class InputTries {
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputTries {
+    public static int getTries() {
+        String input = Console.readLine();
+        int tries=Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 정수를 입력하세요.");
+        }
+    }
 }
