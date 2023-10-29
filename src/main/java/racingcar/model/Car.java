1b38b1b4dfc0ceb569790f08e00c8e741f0a4760
beomsic <qjatjr29@khu.ac.kr>
2023-10-29 10:09:50 +0900

    :sparkles: Feat: Add move method according to moving conditions
    - 무작위 값 생성 코드 추가
    - 생성된 무작위 값이 움직일 수 있는 기준에 만족하는지 체크하는 함수 추가
    - 기준에 만족한다면 해당 자동차의 움직인 횟수 증가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,20 +2,43 @@ package racingcar.model;
 
 import static java.util.Objects.isNull;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private static final int MAX_LENGTH_OF_NAME = 5;
+    private static final int MIN_RANDOM_VALUE_TO_MOVE = 4;
+    private static final int MIN_RANDOM_VALUE = 0;
+    private static final int MAX_RANDOM_VALUE = 9;
 
     private final String name;
+    private Integer movedCount;
 
     public Car(final String name) {
         validateEmptyName(name);
         validateNameLength(name);
         this.name = name;
+        this.movedCount = 0;
+    }
+
+    public void move() {
+        if(canMove()) movedCount++;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getMovedCount() {
+        return movedCount;
+    }
+
+    private boolean canMove() {
+        return generateRandomValue() >= MIN_RANDOM_VALUE_TO_MOVE;
+    }
+
+    private int generateRandomValue() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
     }
 
     private void validateNameLength(final String name) {
