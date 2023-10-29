4b50aa5e0a0609763f2deb6a2b406ae4f553edf2
Zeus6768 <akaps6768@gmail.com>
2023-10-29 18:16:31 +0900

    test(요구사항5-2): 이름 입력 예외 발생 기능 테스트

--- src/test/java/racingcar/CarNameInputTest.java
+++ src/test/java/racingcar/CarNameInputTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.TestUtil.setInput;
 import static racingcar.message.ExceptionMessage.DUPLICATE_NAME;
+import static racingcar.message.ExceptionMessage.LENGTH_LIMIT_EXCEEDED;
 import static racingcar.util.NameSplitter.split;
 
 import java.util.Arrays;
@@ -31,24 +32,36 @@ class CarNameInputTest {
         // when
         String[] carNames = split(inputView.askCarNames());
         List<Car> carList = Arrays.stream(carNames).map(Car::from).toList();
+
         // then
         assertThat(carList).isEqualTo(expected);
     }
 
     @Test
-    void 중복된_이름_입력() {
+    void 이름이_5글자를_넘는_경우_예외_발생() {
+        // given
+        String input = "메르세데스벤츠";
+        setInput(input);
+
+        // when
+        String carName = inputView.askCarNames();
+
+        // then
+        assertThatThrownBy(() -> Car.from(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LENGTH_LIMIT_EXCEEDED.getMessage());
+    }
+
+    @Test
+    void 중복된_이름을_입력한_경우_예외_발생() {
         // given
         String input = "자동차1,자동차2,자동차3,자동차1,자동차2";
         setInput(input);
 
-        List<Car> expected = List.of(
-                Car.from("자동차1"),
-                Car.from("자동차2"),
-                Car.from("자동차3")
-        );
         // when
         String[] carNames = split(inputView.askCarNames());
         List<Car> carList = Arrays.stream(carNames).map(Car::from).toList();
+
         // then
         assertThatThrownBy(() -> Cars.from(carList))
                 .isInstanceOf(IllegalArgumentException.class)
