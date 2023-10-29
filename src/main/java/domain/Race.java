02ee1b6b0f8e52e33f8bed57050849ddeed415a7
mumpuris <hst990124@naver.com>
2023-10-30 00:29:07 +0900

    경주할 자동차 이름을 입력하는 기능 작성

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -41,6 +41,12 @@ public class Race {
         inputMoveCount();
     }
 
+    private void inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = UserInput.inputCarNames();
+        initializeCars(carNames);
+    }
+
 
 
 }
