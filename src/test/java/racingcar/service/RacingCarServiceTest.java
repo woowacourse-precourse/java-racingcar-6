5e58d3df305241f2ed41f04b4f1068656f0b3ed5
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 17:15:13 +0900

    test(RacingCarServiceTest): 테스트의 이름을 검증으로 수정

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -3,27 +3,30 @@ package racingcar.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import racingcar.Configuration;
 import racingcar.domain.validator.CarNameValidator;
 import racingcar.domain.validator.TryNumberValidator;
 
 
 public class RacingCarServiceTest {
-    private final RacingCarService racingCarService = new RacingCarService();
+    private final RacingCarService racingCarService = Configuration.racingCarService();
     private final TryNumberValidator tryNumberValidator = new TryNumberValidator();
     private final CarNameValidator carNameValidator = new CarNameValidator();
 
     @Test
-    public void 시도_회수_입력_성공() {
+    public void 시도_회수_입력_검증_성공() {
         String validInput = "5";
 
         Assertions.assertDoesNotThrow(() -> racingCarService.inputValidate(validInput,tryNumberValidator));
     }
 
     @Test
-    public void 자동차_이름_입력_성공() {
+    public void 자동차_이름_입력_검증_성공() {
         String validInput = "김명준";
 
         Assertions.assertDoesNotThrow(() -> racingCarService.inputValidate(validInput,carNameValidator));
     }
+
+
 }
 
