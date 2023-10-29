ac0986ed6c5102151401f4c71a42bf4587a12db0
jongheonleee <qwefghnm1212@gmail.com>
2023-10-29 21:59:50 +0900

    [test] isCarMove() 멈춤/전진 로직 테스트

--- src/test/java/racingcar/domain/MovingStrategyTest.java
+++ src/test/java/racingcar/domain/MovingStrategyTest.java
@@ -8,6 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MovingStrategyTest {
 
+    @Test
+    void isCarMove_멈춤_로직_테스트() {
+        // 4 보다 작은 숫자인 경우 멈춤
+        MovingStrategy movingStrategy = new MovingStrategy(() -> 3);
+        assertEquals(false, movingStrategy.isCarMove());
+    }
+
+    @Test
+    void isCarMove_전진_로직_테스트() {
+        // 4 이상의 숫자인 경우 전진
+        MovingStrategy movingStrategy = new MovingStrategy(() -> 4);
+        assertEquals(true, movingStrategy.isCarMove());
+
+    }
 
 
 }
