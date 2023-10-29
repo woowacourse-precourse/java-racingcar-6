5fc43b1ce3bf9f7d0fcbc0172a14d1eeb551e034
chaehuichan <eqholic1125@gmail.com>
2023-10-29 22:11:31 +0900

    feat: RacingCar 이름 길이 유효성 검사 기능 구현

--- src/main/java/racingcar/car/RacingCar.java
+++ src/main/java/racingcar/car/RacingCar.java
@@ -2,12 +2,20 @@ package racingcar.car;
 
 public class RacingCar {
     private static final Integer MOVE_COUNT_INITIAL_NUMBER = 0;
-    private String name;
+    private static final Integer MAX_NAME_LENGTH = 5;
 
-    private Integer moveCount;
+    private final String name;
+    private final Integer moveCount;
 
     public RacingCar(String name) {
+        validateNameLength(name);
         this.name = name;
         this.moveCount = MOVE_COUNT_INITIAL_NUMBER;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 }
