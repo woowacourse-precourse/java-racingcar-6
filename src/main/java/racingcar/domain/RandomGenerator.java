8490cb9120af9b1d3d27dd50e5a22609079fa19a
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-29 18:09:15 +0900

    난수를 생성해서 리턴하는 메서드

--- src/main/java/racingcar/domain/RandomGenerator.java
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -5,7 +5,10 @@ import racingcar.constant.CarRaceConstant;
 
 public class RandomGenerator {
 
+    private final int MIN_LENGTH = CarRaceConstant.MIN_LENGTH.getConstant();
+    private final int MAX_LENGTH = CarRaceConstant.MAX_LENGTH.getConstant();
+
     public int createRandomNumber() {
-//        return Randoms.pickNumberInRange(CarRaceConstant.MIN_LENGTH, CarRaceConstant.MAX_LENGTH);
+        return Randoms.pickNumberInRange(MIN_LENGTH, MAX_LENGTH);
     }
 }
