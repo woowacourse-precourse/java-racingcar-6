b7cc8ab8e16d6257521c53dcbe341bbf97283582
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-10-29 17:46:02 +0900

    컨트롤러 부분 코드 정리

--- src/main/java/racingcar/controller/RacingCar.java
+++ src/main/java/racingcar/controller/RacingCar.java
@@ -12,19 +12,36 @@ import javax.xml.crypto.Data;
 
 public class RacingCar {
     public static void playing () {
-        String inputNames = Input.printNameToBeInput();
-        Errors.nameInputThrowError(inputNames);
-        String[] splitNames = Logics.splitNames(inputNames);
-
-        String inputNumber = Input.printNumToBeInput();
-        Errors.numberInputThrowError(inputNumber);
-        int progressNum = Integer.parseInt(inputNumber);
+        String[] splitNames = inputNames();
+        int progressNum = inputNumber();
 
         Output.printResultMsg();
-        DataObj[] Datas = new DataObj[splitNames.length];
-        for (int j = 0; j < splitNames.length; j++) {
-            Datas[j] = new DataObj();
+        DataObj[] Datas = initDataObj(splitNames.length);
+
+        printSteps(splitNames, Datas, progressNum);
+    }
+
+    public static String[] inputNames () {
+        String names = Input.printNameToBeInput();
+        Errors.nameInputThrowError(names);
+        return Logics.splitNames(names);
+    }
+
+    public static int inputNumber () {
+        String inputNumber = Input.printNumToBeInput();
+        Errors.numberInputThrowError(inputNumber);
+        return Integer.parseInt(inputNumber);
+    }
+
+    public static DataObj[] initDataObj (int length) {
+        DataObj[] Datas = new DataObj[length];
+        for(int i = 0; i < Datas.length; i++) {
+            Datas[i] = new DataObj();
         }
+        return Datas;
+    }
+
+    public static void printSteps (String[] splitNames, DataObj[] Datas, int progressNum) {
         for(int i = 0; i < progressNum; i++) {
             for(int j = 0; j < splitNames.length; j++) {
                 Datas[j].setData(splitNames[j], Logics.forwardOrNot());
@@ -33,6 +50,5 @@ public class RacingCar {
             System.out.println();
         }
         Output.printWinner(Logics.winners(Datas));
-
     }
 }
