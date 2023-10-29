264528c11a2d651fe2f3c2527f8ce9b542f27f99
Hayoon <hy97@sju.ac.kr>
2023-10-29 15:38:37 +0900

    feat(Application): 자동차 경주 게임을 실행할 new RacingCarGame() 인스턴스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.console.RacingCarGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new RacingCarGame().playGame();
     }
 }
