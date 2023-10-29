cbd1da77137c1d2d79af99fedd2c42a89ac8d59b
Mr.Kim <joajy98@gmail.com>
2023-10-29 10:34:50 +0900

    feat: 시도 횟수에 숫자 이외의 값이 들어오는 예외를 검증하는 메서드

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -10,10 +10,12 @@ public class CarValidator {
     private static final int CHECK_IF_TRY_COUNT_LESS_THAN_ZERO = 0;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String STRING_REGEX = "^[\\w]*$";
+    public static final String NUM_REGEX = "^[1-9]*$";
     private static final String CAR_NAME_LENGTH_ERROR = "자동차의 이름은 1글자 이상 5글자 이하여야 합니다.";
     private static final String CAR_NAME_TYPE_ERROR = "자동차의 이름은 공백 없는 문자열이어야 합니다.";
     private static final String TRY_COUNT_ERROR = "시도 횟수는 0회 이상이어야 합니다.";
     private static final String DUPLICATE_CAR_NAME_ERROR = "자동차의 이름은 중복될 수 없습니다.";
+    private static final String TRY_INPUT_TYPE_ERROR = "시도 횟수는 0부터 9 사이의 숫자로만 구성되어야 합니다.";
 
     public static void checkAllException(List<Car> carList){
         for(Car carName : carList){
@@ -42,9 +44,20 @@ public class CarValidator {
         }
     }
 
+    public static void checkAllTryCountException(String tryCount){
+        checkTryCount(Integer.parseInt(tryCount));
+        checkTryCountType(tryCount);
+    }
+
     public static void checkTryCount(int tryCount) {
         if (tryCount < CHECK_IF_TRY_COUNT_LESS_THAN_ZERO) {
             throw new IllegalArgumentException(TRY_COUNT_ERROR);
+        }
+    }
+    
+    public static void checkTryCountType(String tryCount){
+        if(!tryCount.contains(NUM_REGEX)){
+            throw new IllegalArgumentException(TRY_INPUT_TYPE_ERROR);
         }
     }
 }
