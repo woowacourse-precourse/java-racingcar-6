f50752183e075f31b724d48e534b3203b5e864c3
박준영 <wnsdud6969@naver.com>
2023-10-30 00:53:39 +0900

    test(CarsTest): add test
    Add new event to CarsTest
    - test attemptToMoveCars() and getMovingDistances()
    - test getCarNames()
    - test getMaximumMovingDistance();

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -16,4 +16,44 @@ class CarsTest {
         });
     }
 
+    @Test
+    public void attemptToMoveCars() {
+        List<String> carNames = List.of("pobi", "woni", "jun");
+        Cars cars = new Cars(carNames);
+
+        CarAccelerator carAccelerator = new CarAccelerator();
+        carAccelerator.setFakeRandoms();
+        cars.attemptToMoveCars(carAccelerator);
+
+        List<Integer> expectedMovingDistances = List.of(0, 0, 0);
+        List<Integer> realMovingDistances = cars.getMovingDistances();
+
+        assertEquals(expectedMovingDistances, realMovingDistances);
+    }
+
+    @Test
+    public void getCarNames() {
+        List<String> carNames = List.of("pobi", "woni", "jun");
+        Cars cars = new Cars(carNames);
+
+        List<String> realCarNames = cars.getCarNames();
+
+        assertEquals(carNames, realCarNames);
+    }
+
+    @Test
+    public void getMaximumMovingDistance() {
+        List<String> carNames = List.of("pobi", "woni", "jun");
+        Cars cars = new Cars(carNames);
+
+        CarAccelerator carAccelerator = new CarAccelerator();
+        carAccelerator.setFakeRandoms();
+        cars.attemptToMoveCars(carAccelerator);
+
+        int expectedMaximumMovingDistances = 0;
+        int realMaximumMovingDistances = cars.getMaximumMovingDistance();
+
+        assertEquals(expectedMaximumMovingDistances, realMaximumMovingDistances);
+    }
+
 }
