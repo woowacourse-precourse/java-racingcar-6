edebf7bee365711575fe154fa65c9935442b902e
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 18:02:59 +0900

    feat(Car): 자동차 이름 validation 체크 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,7 +5,14 @@ public class Car {
     private int movedCount;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.movedCount = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
