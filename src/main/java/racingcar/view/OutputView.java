e2e728e083570bef19a244702fa0d66e057f644c
Dawin <dawin002@naver.com>
2023-10-29 23:20:03 +0900

    refactor(OutputView): 결과 출력 메서드 파라미터 변경
    게임 결과 출력 메서드 printRoundResult()의 파라미터 변경:
    (Race race) -> (List<String> carNames, List<Integer> roundResult)
    우승자 출력 메서드 printGameWinners()의 파라미터 변경:
    (Race race) -> (List<String> winners)
    변경 사유: 객체 사이에 전달하는 정보의 범위를 줄이는 것이 View와 Model의 역할을 명확히 구분하는 것이라 판단함.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,6 @@
 package racingcar.view;
 
-import racingcar.model.Race;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
@@ -9,31 +8,25 @@ public class OutputView {
         System.out.println("\n실행 결과");
     }
 
-    public static void printRoundResult(Race race) {
-        List<String> carNames = race.getCarNames();
-        List<Integer> roundResult = race.getRoundResult();
-
-        String result = buildResultString(carNames, roundResult);
-
-        System.out.println(result);
-    }
-
-    private static String buildResultString(List<String> carNames, List<Integer> roundResult) {
-        StringBuilder stringBuilder = new StringBuilder();
-
+    public static void printRoundResult(List<String> carNames, List<Integer> roundResult) {
+        List<String> resultList = new ArrayList<>();
         for (int index = 0; index < carNames.size(); index++) {
-            int distance = roundResult.get(index);
-            stringBuilder.append(carNames.get(index));
-            stringBuilder.append(" : ");
-            stringBuilder.append("-".repeat(distance));
-            stringBuilder.append("\n");
+            String resultString = makeResultString(index, carNames, roundResult);
+            resultList.add(resultString);
         }
-
-        return stringBuilder.toString();
+        String result = String.join("\n", resultList);
+        System.out.println(result + '\n');
     }
 
-    public static void printGameWinners(Race race) {
-        List<String> winners = race.getWinners();
+    private static String makeResultString(Integer index, List<String> carNames, List<Integer> roundResult) {
+        String carName = carNames.get(index);
+        int distance = roundResult.get(index);
+        String carMove = "-".repeat(distance);
+
+        return carName + " : " + carMove;
+    }
+
+    public static void printGameWinners(List<String> winners) {
         String result = String.join(", ", winners);
         System.out.println("최종 우승자 : " + result);
     }
