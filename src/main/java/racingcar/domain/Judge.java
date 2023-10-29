7d4386d5401fbcc47975ce18c649a6a1e5cfab33
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 21:06:47 +0900

    remove(Judge): static 메서드로 두기에 적합하지 않다 판단하여 car에 합병

--- src/main/java/racingcar/domain/Judge.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-public class Judge {
-    private static final int JUDGE_CAN_GO_NUMBER = 4;
-
-    public static boolean canRush() {
-        return RandomNumberGenerator.createRandomNumber() >= JUDGE_CAN_GO_NUMBER;
-    }
-}
