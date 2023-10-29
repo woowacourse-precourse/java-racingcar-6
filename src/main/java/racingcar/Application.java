154a419ffa2c268a5fa9bacae5b632d28692ca7a
minseokKim6823 <alstjr6823@naver.com>
2023-10-29 14:41:12 +0900

    자동차 경주 게임 README 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str=readLine();
+
+
+
     }
 }
