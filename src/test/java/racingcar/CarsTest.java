7adc5beed89db0049f49a53e8625205a70d4b725
devhoya97 <diajbh123@gmail.com>
2023-10-29 21:49:20 +0900

    test(racingcar) : Cars 클래스가 이전에 변경됐으므로 이에 맞게 테스트 코드 수정
    Cars 클래스의 메서드들에 대해 적절한 테스드 작성함

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -3,40 +3,72 @@ package racingcar;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
-import org.junit.jupiter.api.Test;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
-import racingcar.domain.OutputView;
 
-class CarsTest extends NsTest {
+class CarsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static final int MOVEMENT_OCCURRED = 1;
+    private static final int MOVEMENT_NOT_OCCURRED = 0;
+    private Car pobiCar;
+    private Car woniCar;
+    private Cars cars;
 
-    public static void main(String[] args) {
-        Car pobi = new Car("pobi");
-        Car woni = new Car("woni");
-        Car jun = new Car("jun");
-
-        Cars cars = new Cars(Arrays.asList(pobi, woni, jun));
-        cars.move();
-        OutputView.printCarsLocation(cars);
-    }
-
-    @Test
-    void printCars_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    main(new String[]{});
-                    assertThat(output()).contains("pobi : ", "woni : -", "jun : -"); // output() 메서드의 trim() 적용됨을 유의
-                },
-                STOP, MOVING_FORWARD, MOVING_FORWARD
+    private static Stream<Arguments> testMoveCountAndRandomNumber() {
+        return Stream.of(
+                Arguments.of(MOVEMENT_NOT_OCCURRED, MOVEMENT_NOT_OCCURRED, STOP, STOP),
+                Arguments.of(MOVEMENT_OCCURRED, MOVEMENT_NOT_OCCURRED, MOVING_FORWARD, STOP),
+                Arguments.of(MOVEMENT_NOT_OCCURRED, MOVEMENT_OCCURRED, STOP, MOVING_FORWARD),
+                Arguments.of(MOVEMENT_OCCURRED, MOVEMENT_OCCURRED, MOVING_FORWARD, MOVING_FORWARD)
         );
     }
 
-    @Override
-    public void runMain() {
-        main(new String[]{});
+    @BeforeEach
+    void setUp() {
+        pobiCar = new Car("pobi");
+        woniCar = new Car("woni");
+        cars = new Cars(Arrays.asList(pobiCar, woniCar));
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMoveCountAndRandomNumber")
+    void move_테스트(int expectedPobiMoveCount, int expectedWoniMoveCount, int pobiRandomNumber, int woniRandomNumber) {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    cars.move();
+                    int pobiMoveCount = pobiCar.getMoveCount();
+                    int woniMoveCount = woniCar.getMoveCount();
+                    assertThat(pobiMoveCount).isEqualTo(expectedPobiMoveCount);
+                    assertThat(woniMoveCount).isEqualTo(expectedWoniMoveCount);
+                },
+                pobiRandomNumber, woniRandomNumber
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMoveCountAndRandomNumber")
+    void getMoveCountMap_테스트(int pobiMoveCount, int woniMoveCount, int pobiRandomNumber, int woniRandomNumber) {
+        //given
+        Map<String, Integer> expectedMap = new LinkedHashMap<>();
+        expectedMap.put("pobi", pobiMoveCount);
+        expectedMap.put("woni", woniMoveCount);
+        //when, then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    cars.move();
+                    Map<String, Integer> moveCountMap = cars.getMoveCountMap();
+                    assertThat(moveCountMap).isEqualTo(expectedMap);
+                },
+                pobiRandomNumber, woniRandomNumber
+        );
     }
 }
