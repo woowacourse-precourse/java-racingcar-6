302ac6eb117177afbfbcad5a2e9dd81cb6fba450
seona-moon <fhflwhwl5@gmail.com>
2023-10-29 16:29:53 +0900

    feat: 게임 결과를 출력하는 메서드 틀 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,4 +7,16 @@ public class OutputView {
   public void displayGameCount(){
     System.out.println("시도할 회수는 몇회인가요?");
   }
+  public void displayStart(){
+    System.out.println("실행 결과");
+  }
+  public void displayResult(){
+    System.out.println("pobi : -\n"
+        + "woni : \n"
+        + "jun : -"); //추후 모델로부터 값을 받아오기
+  }
+
+  public void displayWinner(){
+    System.out.println("최종 우승자 : "+"pobi, jun"); //추후 모델로부터 값을 받아오기
+  }
 }
