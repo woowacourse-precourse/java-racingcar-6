2a1dd33f0a524ac380d19ddfcdff97eaa2e7cbe3
noeyodeel <sko8044@gmail.com>
2023-10-29 23:56:39 +0900

    feat : 자동차 이름 입력 예외처리
    자동차 이름 입력 받았을때, 5자 이하 입력 예외 처리 구현함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,10 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
 
+//    자동차 이름 입력
       CarName car = new CarName();
       System.out.println(car.Car());
+      
+      
     }
 }
--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -5,20 +5,25 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarName {
-  public List<String> Car(){
-    
+  public List<String> Car() {
+
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     String input = Console.readLine();
     List<String> Car_name = new ArrayList<>();
-    
+
     String[] nameArray = input.split(",");
     for (String name : nameArray) {
-      Car_name.add(name.trim()); 
+      Car_name.add(name.trim());
 
     }
+    exception_length(input);
     return Car_name;
-    
-    
 
+  }
+  
+  public void exception_length(String s) {
+    if(s.length() > 5) {
+      throw new IllegalArgumentException("5자 이하로 입력하세요.");
+    }
   }
 }
