e82db6d6884041155f7d3628ca1284652320b671
DongKey777 <higgs95@naver.com>
2023-10-29 20:44:54 +0900

    feat: 자동차 이름 입력 테스트

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,9 +2,11 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Scanner;
+
 public class InputView {
 
-    public static String[] readCarNames() {
+    public static String[] readCarNames(Scanner scanner) {
         String names = readLine();
         String[] carNames = new String[]{};
         try {
---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    Scanner scanner = new Scanner(System.in);
+
+    @Test
+    void split_메서드로_주어진_값을_구분() {
+        String input = "lion,cat,dog";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        scanner = new Scanner(System.in);
+        String[] expectedResult = new String[]{"lion", "cat", "dog"};
+
+        assertThat(InputView.readCarNames(scanner)).containsExactly(expectedResult);
+    }
+}
