b17f06901a5a8ccbe8527813b095c879e3777f06
beomsic <qjatjr29@khu.ac.kr>
2023-10-29 10:08:04 +0900

    :test_tube: Test: Test move method based on moving conditions
    - 생성된 무작위 값에 따라 자동차가 움직이는지 테스트하는 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,13 +2,17 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 import racingcar.model.Car;
 
 @DisplayName("자동차 모델에 대한 테스트")
@@ -46,6 +50,40 @@ class CarTest {
         // when
         // then
         assertThatThrownBy(() -> new Car(carName)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("무작위 값이 정해진 값보다 크거나 같은 경우 자신이 움직인 횟수의 카운트를 증가시킨다.")
+    void move_RandomValueMoreThanCriteria_IncreaseMoveCount(int value) {
+        // given
+        Car car = new Car("beom");
+        MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class);
+
+        //when
+        when(Randoms.pickNumberInRange(0, 9)).thenReturn(value);
+        car.move();
+
+        // then
+        assertThat(car.getMovedCount()).isEqualTo(1);
+        mockRandoms.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("무작위값이 정해진 값보다 작은 경우 움직이지 않는다.")
+    void move_RandomValueLessThanCriteria(int value) {
+        // given
+        Car car = new Car("beom");
+        MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class);
+
+        //when
+        when(Randoms.pickNumberInRange(0, 9)).thenReturn(value);
+        car.move();
+
+        // then
+        assertThat(car.getMovedCount()).isZero();
+        mockRandoms.close();
     }
 
     private static Stream<Arguments> carNameProvider() {
