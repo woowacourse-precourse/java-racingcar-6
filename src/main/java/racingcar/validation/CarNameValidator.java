78bc3ece8fbfff2df1717cc387cda7476c9a694c
Nara Park <skfk6352@naver.com>
2023-10-29 23:09:09 +0900

    <feat>: add exception messages about car name
    carnamevalidator 클래스에서 발생하는 예외에 대한 메시지 추가

--- src/main/java/racingcar/validation/CarNameValidator.java
+++ src/main/java/racingcar/validation/CarNameValidator.java
@@ -9,10 +9,16 @@ public class CarNameValidator {
     private static final int CAR_NAME_MIN_SIZE = 1;
     private static final int CAR_NAME_MAX_SIZE = 5;
     //private static final String CAR_NAME_REGEX = "^[\\w]*$";
+    private static final String CAR_NAME_LENGTH_EXCEPTION_MESSAGE = "자동차의 이름은 1자 이상, 5자 이하여야 합니다.";
+    private static final String CAR_NAME_DUPLICATION_EXCEPTION_MESSAGE = "자동차의 이름은 중복될 수 없습니다.";
 
     public void validateCarName(List<String> carNameList) {
-        if (!(validateNameSize(carNameList) || validateNameDuplication(carNameList))) {
-            throw new IllegalArgumentException();
+        if (!validateNameSize(carNameList)) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+
+        if (!validateNameDuplication(carNameList)) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATION_EXCEPTION_MESSAGE);
         }
     }
 
--- src/test/java/racingcar/domain/CarTest.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
-import org.junit.jupiter.api.Test;
-
-class CarTest {
-}
