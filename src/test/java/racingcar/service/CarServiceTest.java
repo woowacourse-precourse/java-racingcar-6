a8206c079d78bf91add8b9049be54435725da03e
acs0209 <annjae4676@naver.com>
2023-10-29 23:51:06 +0900

    test: add function to create winner output

--- src/test/java/racingcar/service/CarServiceTest.java
+++ src/test/java/racingcar/service/CarServiceTest.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.constant.MessageConst.WINNER_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
@@ -38,4 +39,36 @@ class CarServiceTest {
         assertThat(woni.getScore()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("최종 우승자 생성 시 정해진 출력값 형식과 다르면 오류 발생")
+    void createWinnerTest() {
+        // given
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+        CarRepository carRepository = new CarRepository();
+        CarService carService = new CarService();
+
+        // when
+        pobi.updateScore();
+        pobi.updateScore();
+        jun.updateScore();
+        jun.updateScore();
+        carRepository.save(pobi);
+        carRepository.save(woni);
+        carRepository.save(jun);
+
+        List<Car> cars = carRepository.getCars();
+        int highestScore = carService.findHighestScore(cars);
+        List<Car> carsWithHighestScore = carService.createCarsWithHighestScore(cars, highestScore);
+        StringBuilder winner = new StringBuilder();
+        winner.append(WINNER_MESSAGE).append(" : ");
+        for (Car car : carsWithHighestScore) {
+            winner.append(car.createWinner());
+        }
+        winner = carService.formatOutput(winner);
+
+        // then
+        assertThat(winner.toString()).isEqualTo("최종 우승자 : pobi, jun");
+    }
 }
