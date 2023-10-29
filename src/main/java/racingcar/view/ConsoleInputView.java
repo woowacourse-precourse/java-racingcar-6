e2ce7a72eb2d8e2ed0028dd01150053e74af47d3
inpink <inpink@kakao.com>
2023-10-29 22:59:17 +0900

    refactor: InputView와 OutputView를 interface로 추상화

---
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import racingcar.util.InputUtil;
+
+public class ConsoleInputView implements InputView {
+
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    @Override
+    public String inputCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        return InputUtil.inputString();
+    }
+
+    @Override
+    public String inputTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+        return InputUtil.inputString();
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,8 @@
 package racingcar.view;
 
-import racingcar.util.InputUtil;
+public interface InputView {
 
-public class InputView {
+    String inputCarNames();
 
-    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-
-    public static String inputCarNames() {
-        System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        return InputUtil.inputString();
-    }
-
-    public static String inputTryCount() {
-        System.out.println(INPUT_TRY_COUNT_MESSAGE);
-        return InputUtil.inputString();
-    }
+    String inputTryCount();
 }
