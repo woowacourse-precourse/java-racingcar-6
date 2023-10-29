91fcd811abd089dc9f4acf5000468bf6060f666b
yeop0740 <ssucgoo@gmail.com>
2023-10-29 21:05:36 +0900

    feat: add Race constructor with round and cars

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.List;
+
+public class Race {
+
+    private int round;
+    private List<Car> cars;
+
+    public Race(int round, List<Car> cars) {
+        this.round = round;
+        this.cars = cars;
+    }
+
+}
