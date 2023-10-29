64df1fad56bca7d246cb342f996b697ca627e522
xogns4909 <xogns4909@naver.com>
2023-10-29 14:37:21 +0900

    Feat : 문자열에서 문자열 리스트로 변환 기능 추가

---
+++ src/main/java/racingcar/util/convertor/StringToStringListConvertor.java
@@ -0,0 +1,12 @@
+package racingcar.util.convertor;
+import java.util.List;
+
+public class StringToStringListConvertor {
+
+    private static final String DELIMITER = ",";
+
+    public static List<String> convert(String value) {
+        return List.of(value.split(DELIMITER));
+    }
+
+}
---
+++ src/test/java/convert/StringToStringListTest.java
@@ -0,0 +1,30 @@
+package convert;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.util.convertor.StringToStringListConvertor;
+
+public class StringToStringListTest {
+
+    @ParameterizedTest
+    @DisplayName("문자열 문자열 리스트로 변환 검증")
+    @MethodSource("provideStringToStringListCases")
+    void testConvert(String input, List<String> expected) {
+        List<String> result = StringToStringListConvertor.convert(input);
+        Assertions.assertThat(expected).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> provideStringToStringListCases() {
+        return Stream.of(
+                Arguments.of("a,b,c", List.of("a", "b", "c")),
+                Arguments.of("Test1,Test2,Test3", List.of("Test1", "Test2", "Test3")),
+                Arguments.of("1,2,3", List.of("1", "2", "3"))
+        );
+    }
+
+}
