8435ea71f13fe58dcb18cfc0accc3bd03e42c632
KimSehoon <shggm2000@gmail.com>
2023-10-30 00:39:52 +0900

    feat: printGameProgress method 구현
    - 게임 진행도를 자동차 이름과 "-"로 표현하는 printGameProgress method 구현
    - ex) pobi : --

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,6 +1,8 @@
 package racingcar.view;
 
 
+import java.util.Map;
+
 public class Output {
     public static void printInputCarMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -14,7 +16,21 @@ public class Output {
         System.out.println("실행 결과");
     }
 
-    public static void printGameWinnerMessage(String winners){
+    public static void printGameWinnerMessage(String winners) {
         System.out.println("최종 우승자 : " + winners);
+    }
+
+    public static void printGameProgress(Map<String, Integer> carMap) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(String car : carMap.keySet()){
+            stringBuilder.append(car+ " : ");
+            for(int forwardCount = 0; forwardCount < carMap.get(car); forwardCount++){
+                stringBuilder.append("-");
+            }
+            stringBuilder.append("\n");
+        }
+
+        System.out.println(stringBuilder);
     }
 }
