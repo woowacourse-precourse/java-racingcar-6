03090f65b224d445aec54eb4380f55c88a618f5e
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-30 02:24:22 +0900

    test(validation): 입력 타당성 검토 테스트 작성
    - 자동차 이름 입력 예외에 대한 테스트
    - 실행 횟수 입력 예외에 대한 테스트

--- src/test/java/racingcar/validation/ValidationTest.java
+++ src/test/java/racingcar/validation/ValidationTest.java
@@ -1,5 +1,6 @@
 package racingcar.validation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.validation.CarNames.BLANK_INPUT_EXCEPTION;
 import static racingcar.validation.CarNames.NAME_LENGTH_EXCEPTION;
 import static racingcar.validation.CarNames.SAME_NAMES_EXCEPTION;
@@ -8,7 +9,6 @@ import static racingcar.validation.ExecutionNumber.NUMBER_RANGE_EXCEPTION;
 
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ValidationTest {
@@ -18,11 +18,9 @@ public class ValidationTest {
     void 이름이_공백으로만_이루어진_경우_예외처리() {
         List<String> inputs = Arrays.asList("", " ", "    ,  ", ",,,,");
         inputs.stream().forEach(input -> {
-            try {
-                carNames.validateCarNames(input);
-            } catch (IllegalArgumentException e) {
-                Assertions.assertEquals(BLANK_INPUT_EXCEPTION, e.getMessage());
-            }
+            assertThatThrownBy(() -> carNames.validateCarNames(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(BLANK_INPUT_EXCEPTION);
         });
     }
 
@@ -30,11 +28,9 @@ public class ValidationTest {
     void 이름_길이가_5이하가_아닌_경우_예외처리() {
         List<String> inputs = Arrays.asList("12, 01-, 123210", "pobi,pororo,crong", "ham bur ger");
         inputs.stream().forEach(input -> {
-            try {
-                carNames.validateCarNames(input);
-            } catch (IllegalArgumentException e) {
-                Assertions.assertEquals(NAME_LENGTH_EXCEPTION, e.getMessage());
-            }
+            assertThatThrownBy(() -> carNames.validateCarNames(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NAME_LENGTH_EXCEPTION);
         });
     }
 
@@ -42,11 +38,9 @@ public class ValidationTest {
     void 같은_이름이_들어온_경우_예외처리() {
         List<String> inputs = Arrays.asList("a,a,a", "pobi,crong,pobi", "*,!,!");
         inputs.stream().forEach(input -> {
-            try {
-                carNames.validateCarNames(input);
-            } catch (IllegalArgumentException e) {
-                Assertions.assertEquals(SAME_NAMES_EXCEPTION, e.getMessage());
-            }
+            assertThatThrownBy(() -> carNames.validateCarNames(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(SAME_NAMES_EXCEPTION);
         });
     }
 
@@ -57,11 +51,9 @@ public class ValidationTest {
     void 시도횟수가_숫자가_아닌경우_예외처리() {
         List<String> inputs = Arrays.asList("", "$@", "abc", "  ");
         inputs.stream().forEach(input -> {
-            try {
-                number.validateExcutionNumber(input);
-            } catch (IllegalArgumentException e) {
-                Assertions.assertEquals(NOT_A_NUMBER_EXCEPTION, e.getMessage());
-            }
+            assertThatThrownBy(() -> number.validateExcutionNumber(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NOT_A_NUMBER_EXCEPTION);
         });
     }
 
@@ -69,11 +61,9 @@ public class ValidationTest {
     void 시도횟수의_숫자_범위가_잘못된_경우_예외처리() {
         List<String> inputs = Arrays.asList("0", "-12");
         inputs.stream().forEach(input -> {
-            try {
-                number.validateExcutionNumber(input);
-            } catch (IllegalArgumentException e) {
-                Assertions.assertEquals(NUMBER_RANGE_EXCEPTION, e.getMessage());
-            }
+            assertThatThrownBy(() -> number.validateExcutionNumber(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NUMBER_RANGE_EXCEPTION);
         });
     }
 }
