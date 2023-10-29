e637d0a9caa722e7e868456e4af49387ca0baf68
WonyuChoi <phillip07290@naver.com>
2023-10-29 19:45:21 +0900

    refactor: 메서드명 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -16,7 +16,7 @@ public class OutputView {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void informBeforeResult() {
+    public void informBeforeShowMove() {
         System.out.println("실행 결과");
     }
 
