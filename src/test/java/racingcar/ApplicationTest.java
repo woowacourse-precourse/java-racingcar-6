0a38c943ef0cc8c6281b52159f13f1febcc087c2
Junobee25 <wnsdhqo@naver.com>
2023-10-29 22:43:45 +0900

    test: splitStringByCommas 메서드 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -62,6 +62,32 @@ class ApplicationTest extends NsTest {
 
     }
 
+    @Test
+    void splitStringByCommas_쉼표기준_분리_테스트() {
+        //given
+        final CarGenerator T = new CarGenerator();
+        final String case1 = "pobi";
+        final String case2 = "pobi,jason";
+        final String case3 = "pobi,jason,juno";
+        final String case4 = "pobi,jason,juno,hyun";
+        final String case5 = "pobi,jason,juno,hyun,minseo";
+
+        //when
+        final String[] result1 = T.splitStringByCommas(case1);
+        final String[] result2 = T.splitStringByCommas(case2);
+        final String[] result3 = T.splitStringByCommas(case3);
+        final String[] result4 = T.splitStringByCommas(case4);
+        final String[] result5 = T.splitStringByCommas(case5);
+
+        //then
+        assertThat(result1).containsExactly("pobi");
+        assertThat(result2).containsExactly("pobi", "jason");
+        assertThat(result3).containsExactly("pobi", "jason", "juno");
+        assertThat(result4).containsExactly("pobi", "jason", "juno", "hyun");
+        assertThat(result5).containsExactly("pobi", "jason", "juno", "hyun", "minseo");
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
