4981b269d720ec638b4e216e544c4c71b7bd7ba7
YuJeongYun <yunyj99@naver.com>
2023-10-30 01:33:06 +0900

    test: 우승자 출력 기능 테스트 코드 추가

---
+++ src/test/java/racingcar/model/WinnerTest.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+class WinnerTest {
+    @Test
+    void 우승자_이름_출력() {
+        // given
+        Winner winner = new Winner();
+        List<Car> raceResult = Arrays.asList(new Car("car1",2),
+                new Car("car2",0), new Car("car3", 3));
+
+        // when
+        String winnerResult = winner.selectWinner(raceResult);
+
+        // then
+        assertThat(winnerResult).isEqualTo("car3");
+    }
+
+}
