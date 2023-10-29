f7491cdd75acdd1306b51f77c508d899348ed020
jae woo Jung <awfjol2008@naver.com>
2023-10-30 04:33:25 +0900

    feat: 경주 시도 횟수 입력값이 정수인지 검증하는 기능 구현

--- src/test/java/racingcar/validator/GameCountValidator.java
+++ src/test/java/racingcar/validator/GameCountValidator.java
@@ -1,10 +1,19 @@
 package racingcar.validator;
 
+import static java.lang.Character.*;
+import static java.lang.Integer.*;
+
 public class GameCountValidator {
 
     private void validateIsEmpty(String gameCount) {
         if (gameCount.isEmpty()) {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
+
+    private void validateIsDigit(String gameCount) {
+        if (!isDigit(parseInt(gameCount))) {
+            throw new IllegalArgumentException("입력값은 정수이어야 합니다.");
         }
     }
 }
