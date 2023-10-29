d5bf67b9c41a64d62556cdeb788e48d3b35f070b
Lee junmin <terabyte4096@gmail.com>
2023-10-29 22:38:39 +0900

    feat: add getCarsFromInput() converts given string to car list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static List<String> inputCarNameList() {
@@ -10,7 +11,18 @@ public class Application {
         return List.of(stringArray);
     }
 
-    public static void main(String[] args) {
+    public static List<Car> getCarsFromInput() {
         List<String> carNameList = inputCarNameList();
+        boolean isLengthExceed = carNameList.stream().anyMatch(carName -> carName.length() > 5);
+        if (isLengthExceed) {
+            throw new IllegalArgumentException();
+        }
+        return carNameList.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Car> cars = getCarsFromInput();
     }
 }
