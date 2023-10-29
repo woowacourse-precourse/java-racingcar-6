f57c1cd4cede3484b20128ef079954b24ddb0ff4
juone00 <juwon0208@naver.com>
2023-10-30 01:18:50 +0900

    style(racingcar): Input 테스트 코드 개발을 위한 함수 추출

--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -10,15 +10,13 @@ public class Input {
 
     /**
      * 경주 할 자동차 한줄로 입력 받은 후,
-     * 예외 처리 후,
-     * 쉽표를 기준으로 나눠서 List에 저장 후 return
+     * 쉽표를 기준으로 나눠서 List에 저장 후,
+     * 예외 처리 후 return
      */
     public List<String> createNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String line = Console.readLine();
-        List<String> nameList = List.of(line.split(","));
-        exception.checkName(nameList);
-        return nameList;
+        return getNames(line);
     }
 
     /**
@@ -28,6 +26,18 @@ public class Input {
     public int moveCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String line = Console.readLine();
+        return getMoveCount(line);
+    }
+
+    // 쉼표를 기준으로 나누고 예외 처리
+    public List<String> getNames(String line) {
+        List<String> nameList = List.of(line.split(","));
+        exception.checkName(nameList);
+        return nameList;
+    }
+
+    // 예외 처리 후 int 형으로 변환
+    public int getMoveCount(String line) {
         exception.checkNumber(line);
         return Integer.parseInt(line);
     }
