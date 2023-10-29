b04abad57286d316f02732f409e842c36f04a030
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 23:18:59 +0900

    feat: printCarNameInputMessage 메소드 작성
    자동차 이름들을 입력 받기 위한 메시지를 출력하는 printCarNameInputMessage 메소드를 작성했습니다.

---
+++ src/main/java/racingcar/utils/RacingCarGameOutput.java
@@ -0,0 +1,8 @@
+package racingcar.utils;
+
+public class RacingCarGameOutput {
+    public static void printCarNameInputMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+    }
+
+}
