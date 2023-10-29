62c75fa446bf7bc1c28a0a4745921e190b9fb815
sujnkim <blackout3210@naver.com>
2023-10-29 20:37:50 +0900

    feat: OutputView 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String PROCESS_PRINT_START = "실행 결과";
+
+
+    public static void processPrintStartMessage() {
+        System.out.println(PROCESS_PRINT_START);
+    }
+
+
+    public static void currentProcessPrint() {
+
+    }
+
+
+    public static void printWinner() {
+
+    }
+}
