02ca166c8f0f450d51cef34200844273277c5081
ChaeAg <dydkglel123@gmail.com>
2023-10-29 14:08:37 +0900

    feat(InputView): 사용자의 입력을 받는 클래스 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String carNamesInput() {
+        return Console.readLine();
+    }
+
+    public String timesInput() {
+        return Console.readLine();
+    }
+}
