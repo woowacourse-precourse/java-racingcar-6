d7bd2a0a62cfcab2d73076e00a662e23335a9a10
namhoon-kim97 <kskcmh5723@gmail.com>
2023-10-30 02:05:00 +0900

    feat: 경주 할 자동차 이름 입력기능 구현

--- src/main/java/racingcar/domain/InputCarName.java
+++ src/main/java/racingcar/domain/InputCarName.java
@@ -1,4 +1,30 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputCarName {
+    public List<String> getCarNames(){
+        String inputCarNames = Console.readLine().trim();
+        String[] carNameArray = inputCarNames.split(",");
+        Set<String> uniqueCarNames = new HashSet<>();
+
+        for (String carName : carNameArray) {
+            if (carName.length() > 5 || !carName.matches("^[a-z]+$")) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하의 알파벳 소문자만 가능합니다.");
+            }
+        }
+
+        for (String carName : carNameArray) {
+            if (uniqueCarNames.contains(carName)) {
+                throw new IllegalArgumentException("자동차 이름이 중복되었습니다.");
+            }
+            uniqueCarNames.add(carName);
+        }
+
+        return Arrays.asList(carNameArray);
+    }
 }
