cc3eb3a40b471b6d7ff57c2d15995aaca6dd0e7c
3juhwan <13selfesteem91@naver.com>
2023-10-30 00:27:06 +0900

    feat(application): 사용자 차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = askCarNames();
     }
+
+    public static List<String> askCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Arrays.stream(readLine().split(","))
+                .map(carName -> {
+                    if (isValidCarName(carName)) {
+                        return carName;
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                })
+                .toList();
+    }
+
+    public static Boolean isValidCarName(String carName) {
+        return null;
+    }
+
 }
