5bb786c493f57be0e6eceb89f5c5649e961d02e7
Hoo-sung <gntjd135@gmail.com>
2023-10-29 19:22:36 +0900

    test(InputViewTest) : NsTest를 상속받은 InputViewTest 생성

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+class InputViewTest extends NsTest {
+
+    private final List<String> answer = Arrays.asList("pobi","woni","jun");
+    private final InputView inputView = new InputView();
+
+    @Override
+    protected void runMain() {
+
+    }
+}
