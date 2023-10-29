985248af6eb3b630416bc4c58e007195b2c78801
Sully <2dcoder@naver.com>
2023-10-29 21:12:32 +0900

    feat(CarNameValidator): carName의 길이가 0에서 5글자가 아니라면 예외가 발생하는 메서드 추가
    String이니 0은 판별해줄 필요가 없다.

--- src/main/java/racingcar/domain/CarNameValidator.java
+++ src/main/java/racingcar/domain/CarNameValidator.java
@@ -8,6 +8,7 @@ public class CarNameValidator {
         this.carName = carName;
         validateCarNameExists();
         validateAlphanumericAndCommaCarName();
+        validateCarNameLength();
     }
 
     public void validateCarNameExists() {
@@ -19,6 +20,12 @@ public class CarNameValidator {
     public void validateAlphanumericAndCommaCarName() {
         if (!carName.matches("[a-zA-Z0-9,]+")) {
             throw new IllegalArgumentException("자동차 이름은 영숫자와 콤마로만 구성되어야 합니다.");
+        }
+    }
+
+    public void validateCarNameLength() {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
         }
     }
 
