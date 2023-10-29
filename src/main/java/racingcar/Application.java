e4503a5b57e13c57e5e1edbe639cf98a195f2d28
ssunnykku <ssunnykku@gmail.com>
2023-10-29 15:39:01 +0900

    feat(NumberGenerator): 0에서 9사이에 무작위 값을 구하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,12 @@
 package racingcar;
 
+import racingcar.domain.NumberGenerator;
 import racingcar.domain.RacingCar;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         RacingCar racingCar = new RacingCar();
-        String input = racingCar.input();
-        racingCar.naming(input);
 
     }
 }
