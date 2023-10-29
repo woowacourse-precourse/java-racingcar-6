8fa16e9894ec682e22a47161c7e278a1b00232dd
bamm2 <xxknu88@naver.com>
2023-10-29 23:03:08 +0900

    feat: 입력에 중복된 이름 검증 기능

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,7 +1,11 @@
 package racingcar.validator;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.ExceptionMessage;
 
 public class InputValidator {
 
@@ -9,10 +13,23 @@ public class InputValidator {
 
     public InputValidator(String validateCarNames) {
         convertValidateList(validateCarNames);
+        try{
+            hasDuplicatedName();
+        }catch (IllegalArgumentException e){
+            Console.close();
+            e.printStackTrace();
+        }
     }
 
-    private void convertValidateList(String carNames){
+    private void convertValidateList(String carNames) {
         this.validateCarNames = Arrays.stream(carNames.split(",")).toList();
+    }
+
+    private void hasDuplicatedName() {
+        HashSet<String> duplicateValidSet = new HashSet<>(validateCarNames);
+        if (duplicateValidSet.size()!=this.validateCarNames.size()) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_CAR_NAME.getMessage());
+        }
     }
 
 }
