affc59e3b00e02387717cf95db145427c482bdbc
Seungjida <esg0107@pusan.ac.kr>
2023-10-30 00:41:38 +0900

    feat: 필요한 값을 생성하고 판단하는 부가 기능을 담은 클래스 생성

---
+++ src/main/java/racingcar/Utils.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Utils {
+    public static int generateRandomNumber() {
+        return 0;
+    }
+
+    public static boolean isMove(int movingNumber) {
+        return false;
+    }
+}
