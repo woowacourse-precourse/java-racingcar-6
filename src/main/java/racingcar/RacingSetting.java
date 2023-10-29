d300b38f8c1d635a2f2a4ced73e6926d699ff094
minkyole <7719s@nave.com>
2023-10-29 16:25:56 +0900

    feat(RacingSetting.java): RacingSetting Class 생성 및 getName() 매소드 구현
    - RacingGame을 진행하며 사용할 세팅값을 보관할 RacingSetting Class 생성
    - Racing Car 이름을 입력받을 getName() 메소드 구현

---
+++ src/main/java/racingcar/RacingSetting.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class RacingSetting {
+
+    private void getName() {
+        String inputCarName = readLine();
+    }
+}
