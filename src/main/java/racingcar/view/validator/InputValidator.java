6a4acbc7d108aa2690345ead08e9e321b54ff6a4
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 18:51:47 +0900

    refactor(InputValidator): 각 기능을 메소드별로 분리, 기능을 분담하여 로직이 보이도록 가독성을 높임. validate 하는데 쓰이는 두 메소드만 public으로 접근이 가능하고, 나머지는 private으로 설정함

--- src/main/java/racingcar/view/validator/InputValidator.java
+++ src/main/java/racingcar/view/validator/InputValidator.java
@@ -1,48 +1,64 @@
 package racingcar.view.validator;
 
+import racingcar.view.constants.Messages;
 import racingcar.view.constants.Numbers;
 
 public class InputValidator {
-    public static void validateCarName(String carName) {
-        if (carName == null || carName.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
 
-        for (int i = 0; i < carName.length(); i++) {
-            char currentLetter = carName.charAt(i);
-            if (currentLetter == ',') {
-                continue;
-            }
-            if (currentLetter < 'a' || currentLetter > 'z') {
-                throw new IllegalArgumentException();
+    private static void handleIsNullOrEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(Messages.NULL_OR_EMPTY_ERROR.getMessage());
+        }
+    }
+
+    private static void handleOnlyContainsValidLetter(String input, String validLetters) {
+        for (int i = 0; i < input.length(); i++) {
+            char currentLetter = input.charAt(i);
+            if (validLetters.indexOf(currentLetter) < 0) {
+                throw new IllegalArgumentException(Messages.INVALID_LETTER_ERROR.getMessage());
             }
         }
+    }
 
+    private static void handleLengthOfCarName(String carName) {
         String[] carNames = carName.split(",");
-        if (carNames.length == 0 || carNames.length > Numbers.MAX_GAME_PLAYER.getNumber()) {
-            throw new IllegalArgumentException();
-        }
         for (String name : carNames) {
             if (name.isEmpty() || name.length() > Numbers.MAX_NAME_LENGTH.getNumber()) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(Messages.LENGTH_OF_CAR_NAME_ERROR.getMessage());
             }
         }
+    }
+
+    private static void handleNumberOfCarNames(String carName) {
+        String[] carNames = carName.split(",");
+        if (carNames.length == 0 || carNames.length > Numbers.MAX_GAME_PLAYER.getNumber()) {
+            throw new IllegalArgumentException(Messages.NUMBER_OF_CARS_ERROR.getMessage());
+        }
+
+
+    }
+
+
+    private static void handlePlayTimeInRange(String playtime) {
+        int intTypePlayTime = Integer.parseInt(playtime);
+        if (intTypePlayTime <= 0 || intTypePlayTime > Numbers.MAX_GAME_PLAY.getNumber()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateCarName(String carName) {
+        handleIsNullOrEmpty(carName);
+        handleOnlyContainsValidLetter(carName, Messages.VALID_CAR_NAME.getMessage());
+        handleLengthOfCarName(carName);
+        handleNumberOfCarNames(carName);
+
     }
 
     public static void validatePlayTime(String playtime) {
-        if (playtime == null || playtime.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        for (int i = 0; i < playtime.length(); i++) {
-            char currentLetter = playtime.charAt(i);
-
-            if (currentLetter < '0' || currentLetter > '9') {
-                throw new IllegalArgumentException();
-            }
-        }
-        int number = Integer.parseInt(playtime);
-        if (number <= 0 || number > Numbers.MAX_GAME_PLAY.getNumber()) {
-            throw new IllegalArgumentException();
-        }
+        handleIsNullOrEmpty(playtime);
+        handleOnlyContainsValidLetter(playtime, Messages.VALID_PLAYTIME.getMessage());
+        handlePlayTimeInRange(playtime);
     }
+
+
 }
