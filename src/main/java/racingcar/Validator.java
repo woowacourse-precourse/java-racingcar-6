7388c5f311be8d3ea0bf003a4df9907ef70a5c9f
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 01:38:32 +0900

    feat(Validator): 콤마가 연속으로 입력되어 있는지 검증한다.

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Validator {
     public static boolean isInteger(String input) {
         try {
@@ -17,5 +20,12 @@ public class Validator {
 
     public static boolean hasNoWhitespace(String input) {
         return !input.contains(" ");
+    }
+
+    public static boolean hasNoConsecutiveCommas(String input) {
+        // 정규 표현식 패턴: 연속된 쉼표(,) 검색
+        Pattern pattern = Pattern.compile(",{2,}");
+        Matcher matcher = pattern.matcher(input);
+        return !matcher.find();
     }
 }
