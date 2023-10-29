f01e10979c81057fd32e1788a8efd7d387812593
vega <vega99c@naver.com>
2023-10-29 17:56:14 +0900

    test(CarTest): 자동차 객체 생성 시 이름 유효성 검사
    - 자동차 객체 생성 테스트
    - 자동차 이름 길이 확인 테스트
    - 자동차 이름 공백포함 여부 확인 테스트

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    void 자동차_객체_생성() {
+        Car carObj = new Car("vega");
+
+        assertThat(carObj).isNotNull();
+        assertThat(carObj.getCarName()).isEqualTo("vega");
+    }
+
+    @Test
+    void 자동차이름_길이제한() {
+        Car carObj = new Car("vega");
+        int limitNameLength = 5;
+
+        assertThat(carObj.getCarName().length()).isLessThanOrEqualTo(limitNameLength);
+    }
+
+    @Test
+    void 자동차이름_공백검사() {
+        Car carObj = new Car("vega");
+
+        assertThat(carObj.getCarName()).doesNotContainAnyWhitespaces();
+    }
+}
