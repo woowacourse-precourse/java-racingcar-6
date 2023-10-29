71fb8e3ff301a1d1feada613b9364983d24131fd
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-29 18:23:16 +0900

    [Refactor] RandomGenerator 메서드 static으로 변경 다른 클래스에서 바로 호출할 수 있게 변경했다

--- src/main/java/racingcar/domain/RandomGenerator.java
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -5,10 +5,10 @@ import racingcar.constant.CarRaceConstant;
 
 public class RandomGenerator {
 
-    private final int MIN_LENGTH = CarRaceConstant.MIN_LENGTH.getConstant();
-    private final int MAX_LENGTH = CarRaceConstant.MAX_LENGTH.getConstant();
+    private final static int MIN_LENGTH = CarRaceConstant.MIN_LENGTH.getConstant();
+    private final static int MAX_LENGTH = CarRaceConstant.MAX_LENGTH.getConstant();
 
-    public int createRandomNumber() {
+    public static int createRandomNumber() {
         return Randoms.pickNumberInRange(MIN_LENGTH, MAX_LENGTH);
     }
 }
