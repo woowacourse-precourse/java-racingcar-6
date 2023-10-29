d7acb2e34889b757459784dd577558c993511385
yeop0740 <ssucgoo@gmail.com>
2023-10-29 21:35:19 +0900

    test: add test code for Race#proceedRound

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,6 +10,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RaceTest {
+
+    public static final int MOVING_FORWARD = 4;
+    public static final int STOP = 3;
 
     @Test
     public void 레이스_생성_테스트() throws Exception {
@@ -25,5 +29,57 @@ class RaceTest {
         Assertions.assertThat(race.getCars()).isEqualTo(expectedCars);
     }
 
+    @Nested
+    class proceedRound_메서드는 {
+        @Test
+        public void 전진일_때_모두_전진() throws Exception {
+            Car car1 = new Car("alpha");
+            Car car2 = new Car("bravo");
+            Car car3 = new Car("char");
+
+            int expectedRound = 3;
+            List<Car> expectedCars = Arrays.asList(car1, car2, car3);
+
+            Race race = new Race(3, List.of(car1, car2, car3));
+
+            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+                    () -> {
+                        for (int i = 0; i < expectedRound; i++) {
+                            race.proceedRound();
+                        }
+                        Assertions.assertThat(race.getCars().get(0).getDistance()).isEqualTo(expectedRound);
+                        Assertions.assertThat(race.getCars().get(1).getDistance()).isEqualTo(expectedRound);
+                        Assertions.assertThat(race.getCars().get(2).getDistance()).isEqualTo(expectedRound);
+                    },
+                    MOVING_FORWARD,
+                    MOVING_FORWARD
+            );
+        }
+
+        @Test
+        public void 정지일때_모두_정지() throws Exception {
+            Car car1 = new Car("alpha");
+            Car car2 = new Car("bravo");
+            Car car3 = new Car("char");
+
+            int expectedRound = 3;
+            List<Car> expectedCars = Arrays.asList(car1, car2, car3);
+
+            Race race = new Race(3, List.of(car1, car2, car3));
+
+            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+                    () -> {
+                        for (int i = 0; i < expectedRound; i++) {
+                            race.proceedRound();
+                        }
+                        Assertions.assertThat(race.getCars().get(0).getDistance()).isEqualTo(0);
+                        Assertions.assertThat(race.getCars().get(1).getDistance()).isEqualTo(0);
+                        Assertions.assertThat(race.getCars().get(2).getDistance()).isEqualTo(0);
+                    },
+                    STOP,
+                    STOP
+            );
+        }
+    }
 
 }
