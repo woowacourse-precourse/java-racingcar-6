4509976ceeba20b3eec1b8ae38b2475c91b51920
edgar6bf <edgar6bf@gmail.com>
2023-10-29 22:30:39 +0900

    feat: 정수를 입력받는 기능 구현
    사용자로부터 경주를 진행할 횟수를 입력받기 위해 정수를 입력받는 기능을 구현함.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,10 +9,24 @@ public class InputView {
 
     private static final String INPUT_DELIMITER = ",";
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표("+ INPUT_DELIMITER + ") 기준으로 구분)";
+    private static final String INPUT_RACE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static List<String> inputStringsByDelimiter() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         return Arrays.stream(Console.readLine().split(INPUT_DELIMITER))
                 .toList();
+    }
+
+    public static int inputNumber() {
+        System.out.println(INPUT_RACE_COUNT_MESSAGE);
+        return convertStringToInt(Console.readLine());
+    }
+
+    private static int convertStringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력할 수 있습니다.");
+        }
     }
 }
