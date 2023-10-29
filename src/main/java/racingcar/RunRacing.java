f3c20d209346d7e0475e66a7d85558d513f0dce1
8x15yz <8x15yz@gmail.com>
2023-10-29 15:44:38 +0900

    feat : StartRacing(), RaceIndividually(), MoveForward() 구현 완료

--- src/main/java/racingcar/RunRacing.java
+++ src/main/java/racingcar/RunRacing.java
@@ -2,6 +2,7 @@ package racingcar;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -11,8 +12,11 @@ public class RunRacing {
 
     public static void run() {
         RunRacing GetMembersAndTurn = new RunRacing();
+        RunRacing StartRacing = new RunRacing();
         System.out.println("되냐?");
         GetMembersAndTurn.GetMembersAndTurn();
+        StartRacing.StartRacing();
+        System.out.println(members);
     }
     public void GetMembersAndTurn() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -21,7 +25,24 @@ public class RunRacing {
         }
         System.out.println("시도할 회수는 몇회인가요?");
         turn = Integer.parseInt(Console.readLine());
-        System.out.println(members);
-        System.out.println(turn);
     }
+    public void StartRacing() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < turn; i++) {
+            RaceIndividually();
+        }
+    }
+    public void RaceIndividually() {
+        Iterator<String> keys = members.keySet().iterator();
+        while(keys.hasNext()){
+            MoveForward(keys.next());
+        }
+    }
+    public void MoveForward(String member) {
+        int pickedNum = Randoms.pickNumberInRange(0,9);
+        if (pickedNum >= 4) {
+            members.put(member, members.get(member)+1);
+        }
+    }
+
 }
