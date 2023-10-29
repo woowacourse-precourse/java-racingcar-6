1f20be35338921b4ea300638af39e4dd0626f807
Jongwoo Han <jongwooo.han@gmail.com>
2023-10-30 02:02:57 +0900

    feat: implement the CarFactory class

---
+++ src/main/java/racingcar/domain/car/CarFactory.java
@@ -0,0 +1,11 @@
+package racingcar.domain.car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarFactory {
+
+  public static List<Car> createCars(List<String> carNames) {
+    return carNames.stream().map(Car::new).collect(Collectors.toList());
+  }
+}
