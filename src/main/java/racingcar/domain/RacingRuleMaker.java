f53bf75429ce2f919cfb35716d2c89e2975c6b98
doka <posmane5@gmail.com>
2023-10-29 16:21:10 +0900

    feat(RacingRuleMaker) : RacingRuleMaker 클래스 생성 RacingRuleMaker 는 입력을 통해서 자동차들의 이름, 게임 세트수(몇번 이동할것인가?)를 지정한다.

---
+++ src/main/java/racingcar/domain/RacingRuleMaker.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+//- RacingRuleMaker
+//        - [ ] 각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
+//            - [ ] 자동차에 이름 부여 - namingCar()
+//                - [ ] 자동차의 이름을 입력받기 - Console 활용
+//                - [ ] 입력받은 문자열을 리스트로 변환 - split() 활용
+//            - [ ] n대의 자동차 리스트 - List<String> cars
+//        - [ ] 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
+//            - [ ] 몇번의 이동을 할것인가 입력 (주어진 횟수(게임세트수)) int roundCount
+public class RacingRuleMaker {
+
+
+
+}
