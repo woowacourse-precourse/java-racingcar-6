dc0494fa931a502239723531caf4d135c68d0f29
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-10-29 22:16:08 +0900

    feat: 각 자동차 이름이 5자 이하가 아니면 IllegalArgumentException 발생시키는 기능 구현

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,4 +1,22 @@
 package racingcar;
 
-public class Car { // 자동차 객체, 5글자 이하의 이름만 설정 가능
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String ErrorMessage = "경주할 자동차의 이름은 5자 이하로 작성해 주세요.";
+    private final String car;
+
+    public Car(String car) {
+        this.car = car;
+    }
+
+    public static Car makeCar(String splitInput) {
+        validateCar(splitInput);
+        return new Car(splitInput);
+    }
+
+    private static void validateCar(String splitInput) {
+        if (splitInput.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage);
+        }
+    }
 }
