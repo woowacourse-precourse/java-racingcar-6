7949da2fc9b6b3bf4199b8e84364122f369d74d9
DaehoSon <thseogh135@gmail.com>
2023-10-29 14:27:34 +0900

    feat: 테스트 코드 전진_정지에 해당하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Game game = new Game();
+        game.run();
     }
 }
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,21 +1,48 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
 
     public final String ANNOUNCEMENT_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final String ANNOUNCEMENT_INPUT_MOVE_COUNT = "시도할 회수는 몇회인가요?";
+    public final String ANNOUNCEMENT_EXECUTION_RESULT = "실행 결과";
 
     public String[] carNameArray;
+    public int[] carLocationArray;
+    public int numberOfCar;
     public int moveCount;
+    private int maxLocation;
 
     public Game() {
         init();
     }
 
+    public void run() {
+        System.out.println(this.ANNOUNCEMENT_EXECUTION_RESULT);
+        for (int i = 0; i < this.moveCount; i++) {
+            carRuns();
+            System.out.println();
+        }
+        System.out.println("최종 우승자 : " + "pobi");
+    }
+
+    private void carRuns() {
+        for (int i = 0; i < this.numberOfCar; i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                this.carLocationArray[i]++;
+            }
+            int currentLocation = this.carLocationArray[i];
+            System.out.println(this.carNameArray[i] + " : " + "-".repeat(currentLocation));
+        }
+    }
+
     private void init() {
         this.carNameArray = getCarNameArrayFromInput();
+        this.numberOfCar = this.carNameArray.length;
+        this.carLocationArray = new int[this.numberOfCar];
         this.moveCount = getMoveCountFromInput();
     }
 
