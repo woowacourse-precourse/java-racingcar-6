998c5f8e04bd8f774270553e1d35c9270bd0b5f2
Tiger <skdlghwns@gmail.com>
2023-10-30 01:00:11 +0900

    feat(racing): 자동차를 움직여서 실행 결과에 반영 구현 - Racing에서 각 차의 위치를 저장하기 위해 carsPosition사용한다. - 1회 시도할 때 마다 applyPosition을 호출하여 carsPosition을 수정한다.

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -8,9 +8,12 @@ import java.util.Map;
 public class Racing {
     private static int count;
     private static List<Car> racingCars;
-
+    private static Map<Car, Integer> carsPosition;
     public Racing(List<Car> racingCars){
         this.racingCars = racingCars;
+        for(Car car : racingCars){
+            carsPosition.put(car,0);
+        }
     }
     public int askRacingCount(){
         System.out.println("시도할 회수는 몇회인가요?");
@@ -22,5 +25,11 @@ public class Racing {
         }
         System.out.println();
         return count;
+    }
+    public static void applyPosition(Car racingCar){
+        if(racingCar.tryMove()){
+            int position = carsPosition.get(racingCar);
+            carsPosition.put(racingCar, ++position);
+        }
     }
 }
