1bbb2e6ff1cb38f95b20ba112bf71a959f1a38e7
unknown <y2kf9a0q@gmail.com>
2023-10-29 18:54:22 +0900

    test: skeleton of GameTest.java

---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,49 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void finish() {
+    }
+
+    @Test
+    void inputCarNames() {
+    }
+
+    @Test
+    void inputCarNames_ThrownByIllegalArgumentException() {
+    }
+
+    @Test
+    void inputGameTurns() {
+    }
+
+    @Test
+    void inputGameTurns_ThrownByIllegalArgumentException() {
+    }
+
+    @Test
+    void findWinners() {
+    }
+
+    @Test
+    void winnersToString() {
+    }
+}
