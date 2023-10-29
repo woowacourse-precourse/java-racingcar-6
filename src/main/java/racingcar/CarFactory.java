04ce788fa45e0951ef81c78f0587bf253ef50f61
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-30 03:21:12 +0900

    feat : 자동차 공장 테스트 코드 & 기능 구현
    TDD 원칙에 준수하나 , 단순 객체 생성 후 return 이 끝이므로
    테스트 와 기능 구현 동시에 진행
    docs/README.md 에 이모지 수정

---
+++ src/main/java/racingcar/CarFactory.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class CarFactory {
+    public static Car getNewCarInstance(){
+        return new Car();
+    }
+}
---
+++ src/test/java/racingcar/CarFactoryTest.java
@@ -0,0 +1,14 @@
+
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarFactoryTest {
+    @Test
+    void 새로운_자동차_인스턴스를_생성한다(){
+        Car car1 = CarFactory.getNewCarInstance();
+        Car car2 = CarFactory.getNewCarInstance();
+        Assertions.assertNotEquals(car1,car2);
+    }
+}
