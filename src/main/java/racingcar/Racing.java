976e50cc120b6a3c234fa249b7746ef0d82bbfb1
KDT live <dnjsxo0616@gmail.com>
2023-10-30 03:05:59 +0900

    feat(Racing): 자동차 전진 가능 여부 검사 및 업데이트 메서드 추가
    - 자동차가 전진 가능 여부를 검사하고 상황을 업데이트하는 `forwardValidation` 메서드 추가
    - 전진 조건 기준인 `CARFORWARD_NUMBER` 상수 정의
    - 각 자동차에 대한 전진 여부 검사 및 상황 업데이트

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Racing {
+    private static final Integer CARFORWARD_NUMBER = 4;
     public List<String> raceSituation = new ArrayList<>();
     public List<Integer> raceRandomNumbers = new ArrayList<>();
 
@@ -17,6 +18,16 @@ public class Racing {
     public void raceRandomNumberList(List<String> carNames){
         for(int i=0; i<carNames.size(); i++) {
             raceRandomNumbers.add(Computer.CreateRandomNumber());
+        }
+    }
+
+    public void forwardValidation(List<Integer> raceRandomNumbers){
+        for(int i=0; i<raceRandomNumbers.size(); i++){
+            if(raceRandomNumbers.get(i) >= CARFORWARD_NUMBER){
+                String currentString = raceSituation.get(i);
+                currentString += "-";
+                raceSituation.set(i, currentString);
+            }
         }
     }
 
