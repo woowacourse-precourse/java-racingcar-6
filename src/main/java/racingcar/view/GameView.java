a29380d18b0cd4d81625a9a9d1c0aa9784ae1f46
JAY-Winter <rubat0@naver.com>
2023-10-29 17:28:14 +0900

    refactor: 게임 메시지 출력

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -3,22 +3,22 @@ package racingcar.view;
 public class GameView {
 
     public void displayStartGame() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(GameMessages.START_GAME.getMessage());
     }
 
     public static void displayAskRounds() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(GameMessages.ASK_ROUNDS.getMessage());
     }
 
     public void displayResultMessage() {
-        System.out.println("실행결과");
+        System.out.println(GameMessages.RESULT_MESSAGE.getMessage());
     }
 
     public void displayFinalWinner(String winner) {
-        System.out.println("최종 우승자 : " + winner);
+        System.out.println(GameMessages.FINAL_WINNER.format(winner));
     }
 
     public void displayCurrentCarPosition(String currentParticipantCarPosition) {
-        System.out.println(currentParticipantCarPosition + "\n");
+        System.out.println(GameMessages.CURRENT_POSITION.format(currentParticipantCarPosition));
     }
 }
