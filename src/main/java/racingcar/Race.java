d7eab3e2cbad4d4ecf00dc05dfa883fa960f6f9e
박찬건 <jg9870@naver.com>
2023-10-29 23:18:28 +0900

    Revert "refactor(racingcar): cars 반환하는 함수 추가"
    This reverts commit 98188efb7e46ee1eb2ddee5074247e6d07c1235e.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,19 +27,22 @@ public class Race {
         }
     }
 
-    public void moveCarsRandomly() {
+    private void moveCarsRandomly() {
         for (Car car : cars) {
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            moveCarRandomly(car, randomNumber);
+            moveCarRandomly(car);
             ResultView.printCarPosition(car);
         }
         ResultView.printLineBreak();
     }
 
-    public void moveCarRandomly(Car car, int randomNumber) {
-        if (randomNumber >= 4) {
+    private void moveCarRandomly(Car car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
             car.moveForward();
         }
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
     }
 
     public static String findWinners(List<Car> carList) {
@@ -47,14 +51,14 @@ public class Race {
         return String.join(", ", winners);
     }
 
-    public static int calculateMaxPositionLength(List<Car> carList) {
+    private static int calculateMaxPositionLength(List<Car> carList) {
         return carList.stream()
                 .mapToInt(Car::getPositionLength)
                 .max()
                 .orElse(0);
     }
 
-    public static List<String> getWinningCarNames(List<Car> carList, int maxPositionLength) {
+    private static List<String> getWinningCarNames(List<Car> carList, int maxPositionLength) {
         return carList.stream()
                 .filter(car -> car.isWinner(maxPositionLength))
                 .map(Car::getName)
