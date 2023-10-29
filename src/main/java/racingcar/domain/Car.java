3b192e729ab8893fe08e5264cedbcb60af2cf84c
MingyeomKim <mingyum119@naver.com>
2023-10-30 01:19:39 +0900

    feat(Car): Car 객체 생성 시 이름을 저장
    생성자를 구현하여 자동차의 이름 저장 시 자동으로 전진한 횟수를 0으로 초기화한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,4 +3,9 @@ package racingcar.domain;
 public class Car {
     private String name;
     private Integer count;
+
+    private Car(String name) {
+        this.name = name;
+        this.count = 0;
+    }
 }
