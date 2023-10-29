709f94c565f22a5bfa7b36938193a7579b1b20cb
Hajuuu <hpt0127@naver.com>
2023-10-29 19:11:33 +0900

    feat(User): 사용자의 입력을 확인하는 inputCheck 메서드 추가

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,13 +1,28 @@
 package racingcar;
 
+import java.util.List;
+
 public class User {
 
     String name;
     int length;
 
+    public User() {
+
+    }
+
     public User(String name, int length) {
         this.name = name;
         this.length = length;
+    }
+
+    public static void inputCheck(List<User> users) {
+        for (User u : users) {
+            String carName = u.name;
+            if (carName.length() > 5 || carName.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
 }
