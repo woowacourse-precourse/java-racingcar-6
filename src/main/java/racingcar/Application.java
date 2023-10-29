06ff10c65799dd3ee19eaa0bc2ebc626a3b9ee36
74059 <shyeone@naver.com>
2023-10-29 23:46:41 +0900

    feat: 받은 값 ,을 기준으로 split

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,9 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
+        List<String> names_list = List.of(names.split(","));
     }
 }
