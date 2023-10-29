23ef6e85adf083453fbff2fdf07353bb5489b8f6
skj4244 <skj4244@naver.com>
2023-10-29 12:25:20 +0900

    feat: application start

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,12 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int maxNameLength = 5;
+        int forwardCriterion = 4;
+        int lowerBoundRandomNumber = 0;
+        int upperBoundRandomNumber = 9;
+
+        RacingGame racingGame = new RacingGame(maxNameLength, forwardCriterion, lowerBoundRandomNumber, upperBoundRandomNumber);
+        racingGame.play();
     }
 }
