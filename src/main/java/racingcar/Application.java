70d7377bffa954a397810204ec952e016d3db419
김찬규 <kjkj5868@gmail.com>
2023-10-29 14:03:59 +0900

    feat : implement Application.main()
    자동자 경주 게임을 구현. 경주할 자동차 이름을 입력 받고, 시도할 횟수를 입력받아 각 시도 차수에 대한 전진_정지 결과를 출력하고, 최종 우승 결과를 출력하는 기능 구현
    
    TODO : 사용자 입력 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,60 @@
 package racingcar;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	static List<Car> participants;
+
+	public static void main(String[] args) {
+		participants = new ArrayList<>();
+		// 차 이름 입력 받기
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String[] carNameInput = Console.readLine().split(","); // 공백 입력 처리 필요
+		for (String name : carNameInput) {
+			participants.add(new Car(name));
+		}
+		System.out.println("시도할 회수는 몇회인가요?");
+		int tryCount = Integer.parseInt(Console.readLine()); // 예외 처리 필요
+		System.out.println(); // 한칸 띄우고
+		System.out.println("실행 결과");
+		while (tryCount-- > 0) {
+			participants.forEach(Car::rollDice);
+			participants.forEach(car -> {
+				try {
+					car.printStatus();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			});
+			System.out.println();
+		}
+
+		System.out.print("최종 우승자 : ");
+		participants.sort((o1, o2) -> o2.advance - o1.advance);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Car firstWinner = participants.get(0);
+
+		try {
+			bw.write(firstWinner.name);
+			if (participants.size() > 1) {
+				for (int i = 1; i < participants.size(); i++) {
+					if (Objects.equals(participants.get(i).advance, firstWinner.advance)) {
+						bw.write(", ");
+						bw.write(participants.get(i).name);
+					}
+				}
+			}
+			bw.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println();
+	}
 }
