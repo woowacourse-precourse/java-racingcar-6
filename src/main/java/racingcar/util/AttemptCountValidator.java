52e3086d97382fc1b3074dc73d1156734ae6b0bb
Somin Park <ps4708@naver.com>
2023-10-29 19:04:43 +0900

    refactor: 입력값이 숫자인지 확인하는 메서드 분리

--- src/main/java/racingcar/util/AttemptCountValidator.java
+++ src/main/java/racingcar/util/AttemptCountValidator.java
@@ -4,10 +4,14 @@ public class AttemptCountValidator {
 
     private final static String INVALID_ATTEMPT_COUNT_MESSAGE = "시도 횟수는 숫자로만 입력해야 합니다";
 
-    public static void validateAttemptCount(String input) {
-        if (!input.matches("\\d+")) {
+    public void validateAttemptCount(String input) {
+        if (!isNumeric(input)) {
             throw new IllegalArgumentException(INVALID_ATTEMPT_COUNT_MESSAGE);
         }
+    }
+
+    private boolean isNumeric(String input) {
+        return input.matches("\\d+");
     }
 
 }
