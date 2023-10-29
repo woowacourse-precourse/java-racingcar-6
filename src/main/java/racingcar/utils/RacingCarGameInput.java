f1607e6e3a21f21093f9bb83d649f56961a4da4c
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 21:10:05 +0900

    feat: 이동 횟수가 조건에 맞는지 검증하는 메소드 추가
    validateNumber: 숫자인지 확인하는 메소드이다.
    validateMoveCount: 이동 횟수가 1이상인지 확인하는 메소드이다.
    조건에 맞지 않는 경우 IllegalArgumentException을 던지게 되어 있다.

--- src/main/java/racingcar/utils/RacingCarGameInput.java
+++ src/main/java/racingcar/utils/RacingCarGameInput.java
@@ -7,6 +7,8 @@ import racingcar.dto.RacingCar;
 
 public class RacingCarGameInput {
     public static String ERROR_MESSAGE_BIGGER_THAN_5CHAR = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+    public static String ERROR_MESSAGE_NOT_NUMBER = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+    public static String ERROR_MESSAGE_NOT_POSITIVE = "[ERROR] 시도 횟수는 1 이상이어야 합니다.";
 
     public static List<RacingCar> readCarNames() {
         String[] arr = Console.readLine().split(",");
@@ -25,4 +27,21 @@ public class RacingCarGameInput {
         }
         return true;
     }
+
+    public static boolean validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_NUMBER);
+        }
+        return true;
+    }
+
+    public static boolean validateMoveCount(int moveCount) {
+        if (moveCount < 1) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_POSITIVE);
+        }
+        return true;
+    }
+
 }
