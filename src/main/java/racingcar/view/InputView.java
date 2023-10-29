a970bdfa295961adbd1130eadf301afecfe4afd6
sujnkim <blackout3210@naver.com>
2023-10-29 20:19:26 +0900

    feat: modify InputView
    view는 사용자에게 보여지는 것이 주 역할이므로
    입력값 null 검증도 다른 곳에서 하는 게 맞을 것 같다고 생각

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,32 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
-    private final static String GET_CAR_NAMES_SENTENCE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final static String GET_TRIAL_NUMBER_SENTENCE = "시도할 회수는 몇회인가요?";
+    private static final String GET_CAR_NAMES_SENTENCE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GET_TRIAL_NUMBER_SENTENCE = "시도할 회수는 몇회인가요?";
 
-    public String inputCarNames() {
+    public static String inputCarNames() {
         System.out.println(GET_CAR_NAMES_SENTENCE);
-        String input = bringUserInput();
-        return input;
+        return Console.readLine();
     }
 
-
-    public String inputTrialNumber() {
+    public static String inputTrialNumber() {
         System.out.println(GET_TRIAL_NUMBER_SENTENCE);
-        String input = bringUserInput();
-        return input;
+        return Console.readLine();
     }
 
-
-    private String bringUserInput() throws IllegalStateException {
-        String input = Console.readLine();
-        if (isInputNull(input)) {
-            throw new IllegalStateException();
-        }
-        return input;
-    }
-
-    private boolean isInputNull(String userInput) {
-        return userInput == null;
-    }
 }
