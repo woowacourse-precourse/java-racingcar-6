65c04266c820b469ff5f711335d9d51ea90af91a
박승민 <tmddudals369@naver.com>
2023-10-30 04:10:00 +0900

    feat(randomNumberGenerator): 0부터 9까지의 랜덤한 수를 생성하는 기능 구현

---
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    private static final int RANDOM_NUMBER_MIN_VALUE = 0;
+    private static final int RANDOM_NUMBER_MAX_VALUE = 9;
+
+    public int generate() {
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_VALUE, RANDOM_NUMBER_MAX_VALUE);
+    }
+}
