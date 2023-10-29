777e6eb0efcba656f1fae10a413fa215fb0c0e13
qudcks8084 <https://github.com/qudcks8084>
2023-10-29 18:31:19 +0900

    Car클래스 내부변수 선언 및 생성자 선언

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Car {
+
+    // Class Variable
+    public static final String MOVEMENT = "-";
+    public static final int ZERO = 0;
+    private ArrayList<String> userName;
+    private ArrayList<Integer> userScore;
+    private ArrayList<String> winner;
+
+    // Constructor
+    public Car() {
+        this.userName = new ArrayList<>();
+        this.userScore = new ArrayList<>();
+        this.winner = new ArrayList<>();
+    }
+
+    // Method
+    public void start() {
+        System.out.println("Hello !");
+    }
+}
