da6c701769ce74da568f0293f2077270249319e7
youjae <youjae0803@gmail.com>
2023-10-29 15:11:53 +0900

    feat: constructor and getter

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,26 @@
 package racingcar;
 
 public class Car {
-    String name;
-    int pos = 0;
+    private String name;
+    private int pos = 0;
 
     public Car(String name) {
         this.name = name;
     }
+    public Car(String name, int pos) {
+        this.name = name;
+        this.pos = pos;
+    }
 
     public void move(){
         pos++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPos() {
+        return pos;
     }
 }
