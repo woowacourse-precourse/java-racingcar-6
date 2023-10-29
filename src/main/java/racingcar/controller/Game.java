c51162d563b1ddb9edbc1ed401bd62e5a1233114
sangchu <didi1484@gmail.com>
2023-10-29 18:36:49 +0900

    fix : 메소드 printResult 수정 2

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
+import static racingcar.view.InputView.askAttemptNumber;
 import static racingcar.view.InputView.askCarName;
+import static racingcar.view.OutputView.printResult;
 
 import java.util.List;
 import racingcar.domain.Car;
@@ -10,8 +12,14 @@ public class Game {
     public void start() {
         Cars cars = new Cars();
         List<Car> carList = cars.createCarList(askCarName());
-        for (Car car : carList) {
-            System.out.println(car.getName());
+        int attemptNumber = askAttemptNumber();
+        printResult();
+        for (int i = 0; i < attemptNumber; i++) {
+            for (Car car : carList) {
+                car.moveForward(car.generateRandomNumber());
+                printResult(car);
+            }
+            System.out.println();
         }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,14 @@
 package racingcar.view;
 
+import static racingcar.view.constants.Notice.RESULT;
+
 import racingcar.domain.Car;
 
 public class OutputView {
+    public static void printResult() {
+        System.out.println(RESULT);
+    }
+
     public static void printResult(Car car) {
         System.out.println(car.toString());
     }
