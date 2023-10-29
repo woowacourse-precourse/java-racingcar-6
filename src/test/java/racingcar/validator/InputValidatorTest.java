7910d90190f0fc3f7c76a6dfa4420442605a5b6c
bamm2 <xxknu88@naver.com>
2023-10-30 00:09:12 +0900

    test: 자동차 이름내 공백 제거 확인 테스트

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -3,8 +3,11 @@ package racingcar.validator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static racingcar.constant.ExceptionMessage.DUPLICATE_CAR_NAME;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class InputValidatorTest {
 
@@ -18,4 +21,20 @@ class InputValidatorTest {
             assertThat(DUPLICATE_CAR_NAME).isEqualTo(e);
         }
     }
+
+    @Test
+    @DisplayName("문자열 내에 공백 제거 확인 테스트")
+    public void 자동차_이름_공백_제거(){
+        String carNames = "w  o,a   c, u      s, Second \t Week   \n ! , \t";
+        InputValidator inputValidator =new InputValidator(carNames);
+        List<String> carNameList = inputValidator.removeSpaceInCarName();
+        assertThat(carNameList.get(0)).isEqualTo("wo");
+        assertThat(carNameList.get(1)).isEqualTo("ac");
+        assertThat(carNameList.get(2)).isEqualTo("us");
+        assertThat(carNameList.get(3)).isEqualTo("SecondWeek!");
+        assertThat(carNameList.get(4)).hasSize(0);
+        assertThat(carNameList.size()).isEqualTo(5);
+    }
+
+
 }
