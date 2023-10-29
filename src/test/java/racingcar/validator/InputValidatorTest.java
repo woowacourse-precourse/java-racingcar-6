bbae1cf505eaa2acf6194d99817f9ac13d0b5ae7
bamm2 <xxknu88@naver.com>
2023-10-30 02:00:13 +0900

    test: 이름에 공백만 입력했을 경우 테스트

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -4,15 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import static racingcar.constant.ExceptionMessage.DUPLICATE_CAR_NAME;
-import static racingcar.constant.ExceptionMessage.OVER_SIZE_CAR_NAME;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-
-import racingcar.view.Input;
 
 class InputValidatorTest {
 
@@ -34,14 +30,14 @@ class InputValidatorTest {
     @Test
     @DisplayName("문자열 내에 공백 제거 확인 테스트")
     public void 자동차_이름_공백_제거() {
-        carNames = "w  o,a   c, u      s, Second \t Week   \n ! , \t";
+        carNames = "w  o,a   c, u      s, fi \t  ve  \n ! , \t b ";
         inputValidator = new InputValidator(carNames);
         List<String> carNameList = inputValidator.removeSpaceInCarName();
         assertThat(carNameList.get(0)).isEqualTo("wo");
         assertThat(carNameList.get(1)).isEqualTo("ac");
         assertThat(carNameList.get(2)).isEqualTo("us");
-        assertThat(carNameList.get(3)).isEqualTo("SecondWeek!");
-        assertThat(carNameList.get(4)).hasSize(0);
+        assertThat(carNameList.get(3)).isEqualTo("five!");
+        assertThat(carNameList.get(4)).hasSize(1);
         assertThat(carNameList.size()).isEqualTo(5);
     }
 
@@ -52,5 +48,15 @@ class InputValidatorTest {
         assertThrows(IllegalArgumentException.class, () -> new InputValidator(carNames)
         , "자동차 이름 중 5글자 초과한 자동차가 없습니다.");
     }
+
+    @Test
+    @DisplayName("자동차 이름으로 공백을 사용할 수 없습니다.")
+    public void 자동차_이름_공백_테스트() {
+        carNames = "    , super,power, h    i , h `d , '\t ";
+        assertThrows(IllegalArgumentException.class, () -> new InputValidator(carNames)
+        , "모든 자동차 이름이 정상적으로 입력되었습니다.");
+    }
+
+
 
 }
