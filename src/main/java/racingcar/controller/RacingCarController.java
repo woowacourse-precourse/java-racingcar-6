26a2fc9e3e8de23ab2ce502ab6cde4459c934c14
WooYoungDoo <duden5000@naver.com>
2023-10-29 18:00:24 +0900

    feat (RacingCarController) : 게임 진행 프로세스

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -5,7 +5,12 @@ import racingcar.service.ValidateService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.List;
+import java.util.Map;
+
 public class RacingCarController {
+
+    private static final int ZERO_VALUE = 0;
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
@@ -19,6 +24,32 @@ public class RacingCarController {
         String inputNumber = numberInputOutputProcess();
         setCount(inputNumber);
 
+        printExecutionResultMessage();
+        gameProcess();
+
+    }
+
+    private void printExecutionResultMessage() {
+        outputView.printExecutionResultMessage();
+    }
+
+    private void printRoundResult(String carName, int distance) {
+        outputView.printRoundResult(carName, distance);
+    }
+    private void printBlankLine() {
+        outputView.printBlankLine();
+    }
+
+    private void gameProcess() {
+        while (racingCarService.getCount() != ZERO_VALUE) {
+            racingCarService.gameProcess();
+            List<String> carNames = racingCarService.getCarNames();
+            Map<String, Integer> situation = racingCarService.currentSituation();
+            for (String carName : carNames) {
+                printRoundResult(carName, situation.get(carName));
+            }
+            printBlankLine();
+        }
     }
 
     private void setCount(String inputNumber) {
