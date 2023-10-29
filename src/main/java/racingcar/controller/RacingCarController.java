e19475cd0b1e8518804afaf915c49238b26b5d93
eunhee <dmsgml9903@gmail.com>
2023-10-30 00:13:26 +0900

    feat(RacingCarView): 경주 시작할 때 시작 문구 출력 기능 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -6,10 +6,18 @@ import java.util.List;
 import racingcar.model.Car;
 import racingcar.validation.CarValidator;
 import racingcar.validation.UserInputValidator;
+import racingcar.view.RacingCarView;
 
 public class RacingCarController {
 
+    private final RacingCarView racingCarView;
+
+    public RacingCarController() {
+        this.racingCarView = new RacingCarView();
+    }
+
     public void start() {
+        racingCarView.printStartRacingCar();
         String userInput = askCarNames();
         UserInputValidator.validateUserInput(userInput);
 
---
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class RacingCarView {
+
+    private static final String RACING_CAR_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public void printStartRacingCar() {
+        System.out.println(RACING_CAR_START);
+    }
+
+
+}
