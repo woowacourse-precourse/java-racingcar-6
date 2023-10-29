2ee31128384950ebc8c4b3c9030e3107e2dc93cf
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-30 00:00:15 +0900

    feat: 입력 검증(is Unique Name)

---
+++ src/main/java/racingcar/utils/validator/UniqueNameValidator.java
@@ -0,0 +1,14 @@
+package racingcar.utils.validator;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueNameValidator {
+
+    public static boolean validate(List<String> inputs) {
+        Set<String> uniqueNames = new HashSet<>(inputs);
+        return uniqueNames.size() == inputs.size();
+    }
+}
