bb5bde9ab02446bab2f099e22c92f518f63e5597
lee jihyeon <lejinhy@gmail.com>
2023-10-29 21:42:53 +0900

    feat: SymbolType enum에 " : " 기호 추가

--- src/main/java/racingcar/constant/SymbolType.java
+++ src/main/java/racingcar/constant/SymbolType.java
@@ -3,11 +3,16 @@ package racingcar.constant;
 public enum SymbolType {
     DELIMITER_SPLIT_CARS(","),
     DELIMITER_PRINT_CARS(", "),
+    CAR_RESULT(" : "),
     TRACE_RACE_RESULT("-");
 
     private String symbol;
 
     SymbolType(String symbol) {
         this.symbol = symbol;
+    }
+
+    public String getValue() {
+        return symbol;
     }
 }
