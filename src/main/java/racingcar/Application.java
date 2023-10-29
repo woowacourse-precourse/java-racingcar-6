03b9f69703d4e73cf0629e738f90a7890c1aa272
doka <posmane5@gmail.com>
2023-10-29 18:33:00 +0900

    feat(Application) : RacingRuleMaker 기능 클래스와 CarController 기능 클래스를 활용해서 프로그램 기능 구성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.domain.CarController;
+import racingcar.domain.RacingRuleMaker;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingRuleMaker racingRuleMaker = new RacingRuleMaker();
+        CarController carController = new CarController(racingRuleMaker.getCars());
+
+        for (int roundCount = 0; roundCount < racingRuleMaker.getRoundCount(); roundCount++) {
+            carController.gameStart();
+
+        }
+
     }
 }
