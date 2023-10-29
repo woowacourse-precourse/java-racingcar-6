fa58721b5237ffb2a00d21e7a2a38b41d9ccdab1
Hoo-sung <gntjd135@gmail.com>
2023-10-29 14:59:00 +0900

    test(AttemptsNumVerifierTest): 시도횟수가 숫자가 아닌 입력이 주어진 경우 테스트 케이스 추가

--- src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
+++ src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
@@ -4,6 +4,7 @@ package racingcar.verifier;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.system.ExceptionMessage;
 
 class AttemptsNumVerifierTest {
     
@@ -19,5 +20,12 @@ class AttemptsNumVerifierTest {
     void 정상입력이_주어진경우() {
         Assertions.assertThatCode(()->attemptsNumVerifier.check("56"))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 숫자가아닌입력이_주어진경우() {
+        Assertions.assertThatThrownBy(()->attemptsNumVerifier.check("35k"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NOT_NUMERIC);
     }
 }
