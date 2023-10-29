1a2ebc97956edef03ab7f1b4faaca61309db9c4a
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-29 13:23:09 +0900

    test: OutputView 테스트
    라운드 결과를 적절히 출력하는지 테스트
    단일 우승자를 적절히 출력하는지 테스트
    복수 우승자를 적절히 출력하는지 테스트

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,73 @@
+package racingcar.view;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+import racingcar.domain.power.DefaultPowerGenerator;
+
+@DisplayName("뷰 로직 테스트")
+class OutputViewTest {
+
+    private OutputView outputView;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputView = new OutputView();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+
+    @DisplayName("라운드 결과 출력 테스트")
+    @Test
+    void testShowRoundResult() {
+        List<Car> cars = Arrays.asList(
+                new Car("car1", 3, new DefaultPowerGenerator()),
+                new Car("car2", 5, new DefaultPowerGenerator()),
+                new Car("car3", 2, new DefaultPowerGenerator())
+        );
+
+        String expectedOutput = "car1 : ---\ncar2 : -----\ncar3 : --";
+
+        outputView.showRoundResult(cars);
+        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
+    }
+
+    @DisplayName("복수 우승자 출력 테스트")
+    @Test
+    void testShowGameResult() {
+        List<Car> cars = Arrays.asList(
+                new Car("car1", 3, new DefaultPowerGenerator()),
+                new Car("car2", 5, new DefaultPowerGenerator()),
+                new Car("car3", 5, new DefaultPowerGenerator())
+        );
+
+        String expectedOutput = "최종 우승자 : car2,car3";
+
+        outputView.showGameResult(cars);
+        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
+    }
+
+    @DisplayName("단일 우승자 출력 테스트")
+    @Test
+    void testShowGameResultWithSingleWinner() {
+        List<Car> cars = Arrays.asList(
+                new Car("car1", 3, new DefaultPowerGenerator()),
+                new Car("car2", 5, new DefaultPowerGenerator()),
+                new Car("car3", 2, new DefaultPowerGenerator())
+        );
+
+        String expectedOutput = "최종 우승자 : car2";
+
+        outputView.showGameResult(cars);
+        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
+    }
+}
