f9f875a3cb9b76e304eb773d6319777885d7bb75
jinkshower <askthem@naver.com>
2023-10-29 16:29:37 +0900

    test(RaceTest): 자동차 생성 및 출력 기능 검증 테스트 추가

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,2 +1,14 @@
-package racingcar;public class RaceTest {
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+public class RaceTest {
+    @Test
+    void 자동차_생성_검증() {
+        var car = Car.createCar("pobi");
+        assertThat(car.printPosition()).isEqualTo("pobi : ----");
+    }
 }
