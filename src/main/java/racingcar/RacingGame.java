cde7ccb65522a924807d2ab99ae4649f86a495ee
naahnoey <naahyin20@sookmyung.ac.kr>
2023-10-29 16:42:53 +0900

    feat: 사용자로부터 입력받은 이동 횟수만큼 전진 결정 여부 확인 반복

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,18 +9,24 @@ public class RacingGame {
 
     public RacingGame() {
         retrieveCarList();
-        // 이동 횟수 불러오기
-        /*
-         * 이동 횟수만큼 아래 과정 반복
-         *
-         * 1. 각 자동차별로 0~9사이의 무작위 값 구하기
-         * 2. 해당 값이 4보다 큰지 확인
-         * 3. 4이상이면 전진
-         */
+        retrieveCount();
+
+        for (int i=0; i<count; i++) {
+            /*
+             * 1. 각 자동차별로 0~9사이의 무작위 값 구하기
+             * 2. 해당 값이 4보다 큰지 확인
+             * 3. 4이상이면 전진
+             */
+        }
     }
 
     private void retrieveCarList() {
         CarList carlist = new CarList();
+    }
+
+    private void retrieveCount() {
+        User user = new User();
+        count = user.initCount();
     }
 
 }
