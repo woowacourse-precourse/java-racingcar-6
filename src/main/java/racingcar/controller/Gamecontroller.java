acd8df5e1e78e1c9ae455951f42d2dc8bcecaf49
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-30 03:38:10 +0900

    feat: 최종 우승자 결과 출력 기능 추가

--- src/main/java/racingcar/controller/Gamecontroller.java
+++ src/main/java/racingcar/controller/Gamecontroller.java
@@ -6,6 +6,8 @@ import racingcar.view.PlayeroutputView;
 
 import java.util.List;
 
+import static racingcar.view.PlayeroutputView.resultPrize;
+
 public class Gamecontroller {
     private final PlayerinputView playerinputView = new PlayerinputView();
     public static void start(){
@@ -13,6 +15,8 @@ public class Gamecontroller {
         List<Car> cars = PlayerinputView.saveNames(names);
         int round = PlayerinputView.getRound();
         gameResult(round, cars);
+        int prize = PlayeroutputView.result(cars);
+        gamePrize(cars, prize);
     }
 
     private static void gameResult(int round, List<Car> cars){
@@ -21,6 +25,11 @@ public class Gamecontroller {
             PlayeroutputView.updateRound(cars);
             PlayeroutputView.round(cars);
         }
+    }
+
+    private static void gamePrize(List<Car> cars, int prize) {
+        System.out.print("최종 우승자 : ");
+        resultPrize(cars, prize);
     }
 
 }
