573a70ff05c5c2ec619b7cee29c2f53a8bbdef6f
김찬규 <kjkj5868@gmail.com>
2023-10-29 15:17:25 +0900

    feat : add Enum class ErrorMessage, SystemMessage
    콘솔 출력 메시지들을 편하게 관리하기 위한 Enum 클래스 구현

---
+++ src/main/java/racingcar/ErrorMessage.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public enum ErrorMessage {
+    NOT_AN_INTEGER_OVER_ZERO("[ERROR] 0이상의 정수를 입력하여야 합니다."),
+    CAR_NAME_LENGTH_MUST_BE_UNDER_5("[ERROR] 자동차 이름의 길이는 5 이하여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
