8a0fece7367b8b8d2139e6c13e08347c53804cc7
sangchu <didi1484@gmail.com>
2023-10-29 17:15:03 +0900

    feat : 0~9 사이의 무작위 정수값 발생시키는 generateRandomNumber 메소드 추가

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,7 +1,17 @@
 package racingcar.controller;
 
-public class Game {
-    public void start(){
+import static racingcar.view.InputView.askCarName;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+public class Game {
+    public void start() {
+        Cars cars = new Cars();
+        List<Car> carList = cars.createCarList(askCarName());
+        for (Car car : carList) {
+            System.out.println(car.getName());
+        }
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,20 @@
 package racingcar.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
     private String name;
     private int distance;
     private int randomNumber;
+    public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int MAX_RANDOM_NUMBER = 9;
 
     public String getName() {
         return name;
+    }
+
+    public int generateRandomNumber() {
+        return pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 
     Car(String name) {
