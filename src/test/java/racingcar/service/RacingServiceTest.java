34959ee84d3edc191675a068584623397e4e0044
jongmee <ra123221@gmail.com>
2023-10-30 02:50:14 +0900

    test: 우승자를 구하는 서비스 기능 테스트하기

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -2,12 +2,18 @@ package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.Car;
 import racingcar.domain.CarRepository;
 import racingcar.dto.request.CarNamesDto;
+import racingcar.dto.response.WinnersDto;
 
 class RacingServiceTest {
 
@@ -48,5 +54,44 @@ class RacingServiceTest {
         assertThat(carList).hasSize(2)
             .extracting(Car::getForwardCount)
             .allMatch(count -> count >= 0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("전진횟수와_우승자_설정")
+    void 우승자_결정하기(List<String> carNames, List<Integer> forwardCounts, String winner) {
+        // given
+        List<Car> savedCars = 경주차_저장하기(carNames);
+        for(int i = 0; i < savedCars.size(); i++) {
+            특정_횟수만큼_전진하기(savedCars.get(i), forwardCounts.get(i));
+        }
+
+        // when
+        WinnersDto winnersDto = racingService.findWinners();
+
+        // then
+        assertThat(winnersDto.getNames()).contains(winner);
+    }
+
+    static Stream<Arguments> 전진횟수와_우승자_설정() {
+        return Stream.of(
+            Arguments.arguments(List.of("jong", "meoru"), List.of(5, 4), "jong"),
+            Arguments.arguments(List.of("gari"), List.of(1), "gari"),
+            Arguments.arguments(List.of("gari", "meoru", "zzol"), List.of(1, 2, 3), "zzol")
+        );
+    }
+
+    private List<Car> 경주차_저장하기(List<String> carNames) {
+        List<Car> savedCars = new ArrayList<>();
+        for(String name: carNames) {
+            savedCars.add(carRepository.save(name));
+        }
+        return savedCars;
+    }
+
+    private void 특정_횟수만큼_전진하기(Car car, Integer count) {
+        for(int i = 0; i < count; i++) {
+            car.updateForwardCount();
+        }
+        carRepository.update(car);
     }
 }
