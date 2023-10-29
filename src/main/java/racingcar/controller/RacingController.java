0702b5f0b85500f12c4b1c96aa2711ca28aeae13
JH-TT <leejongho9803@gmail.com>
2023-10-29 14:51:28 +0900

    style - LinkedList에서 ArrayList로 변경
    - 단순 삽입만 하니까 Linked로 구현할 이유가 없다고 판단

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,8 +1,6 @@
 package racingcar.controller;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import racingcar.model.Player;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -10,14 +8,11 @@ import racingcar.view.OutputView;
 public class RacingController {
     private final static InputView inputView = new InputView();
     private final static OutputView outputView = new OutputView();
-    private final LinkedList<Player> players = new LinkedList<>();
-    private static int MAX = 0;
-    public void racingReady() {
-        outputView.inputNames();
-        String[] names = inputView.inputPlayers();
-        savePlayers(names);
-        outputView.inputTimes();
-        racingStart(inputView.inputTimes());
+    private final ArrayList<Player> players = new ArrayList<>();
+
+    public void racingProcess() {
+        savePlayers();
+        racingStart(times());
         outputView.printWinner(winner());
     }
 
@@ -29,32 +24,32 @@ public class RacingController {
     private ArrayList<String> winner() {
         ArrayList<String> winner = new ArrayList<>();
         for (Player player : players) {
-            if (!player.isWinner(MAX)) continue;
+            if (!player.isWinner()) continue;
             winner.add(player.getName());
         }
         return winner;
     }
 
-    private void racingStart(final int inputTimes) {
+    private void racingStart(final int times) {
         outputView.printResult();
-        for (int i = 0; i < inputTimes; i++) {
+        for (int i = 0; i < times; i++) {
             System.out.println();
             move();
         }
     }
 
     private void move() {
-        boolean flag = false;
         for (Player player : players) {
-            flag |= player.ForwardOrNot();
+            player.ForwardOrNot();
             outputView.printPlayerStatus(player.getStatus());
         }
-        if (flag) MAX++;
     }
 
-    private void savePlayers(String[] names) {
-        for (String name : names) {
-            players.add(new Player(name));
+    private void savePlayers() {
+        outputView.inputNames();
+        for (String name : inputView.inputPlayers()) {
+            Player p = new Player(name);
+            players.add(p);
         }
     }
 }
