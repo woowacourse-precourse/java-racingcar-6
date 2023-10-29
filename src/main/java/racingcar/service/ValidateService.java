68ae5edf77274d6862575435cf5ef61bdf7359af
WooYoungDoo <duden5000@naver.com>
2023-10-29 15:30:57 +0900

    feat (ValidateService) : 차 이름 글자수 검증 기능 서비스

--- src/main/java/racingcar/service/ValidateService.java
+++ src/main/java/racingcar/service/ValidateService.java
@@ -6,6 +6,10 @@ public class ValidateService {
 
     private final InputValidator inputValidator = new InputValidator();
 
+    public void validateInputCarName(String carName) {
+        inputValidator.validateInputCarName(carName);
+    }
+
     public void validateInputNumber(String inputNumber) {
         inputValidator.validateInputNumber(inputNumber);
     }
