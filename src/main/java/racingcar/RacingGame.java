f3d309414357ee49071a78476b09e20ea3f732f4
daneng4 <daneng4@naver.com>
2023-10-29 12:24:27 +0900

    Fix: 실행 결과 String 변수 이름 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,7 +6,7 @@ public class RacingGame {
 
     private static final String REQUIRE_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUIRE_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String PROCESS_RESULT = "실행결과";
+    private static final String PROGRESS_RESULT = "실행결과";
     private Player player = new Player();
     private HashMap<String, Integer> cars;
     private int attemptNumber;
@@ -26,7 +26,7 @@ public class RacingGame {
     }
 
     public void gameProgress(int attemptNumber){
-        printMessage(PROCESS_RESULT);
+        printMessage(PROGRESS_RESULT);
         for(int i = 0; i<attemptNumber; i++){
             for(int j = 0; j<carNumber; j++){
                 eachCarPickRandomNumber(j);
