cf2dfdf5811b29206d71202b58759220a5a3546b
CodingApe9 <tlqkf6974@>
2023-10-29 13:25:10 +0900

    feat(CarRacing) : 최종 우승 자동차들을 선정하는 기능 추가

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -1,8 +1,38 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class CarRacing {
+    private final List<Car> cars;
+    private final int iterationNumBer;
+
+    public CarRacing(List<Car> cars, int iterationNumBer) {
+        this.cars = cars;
+        this.iterationNumBer = iterationNumBer;
+    }
+
+    private List<Car> getChampionCars() {
+        Integer maxLocation = cars.stream()
+                .map(Car::getLocation)
+                .max(Integer::compare)
+                .get();
+
+        List<Car> championCars = cars.stream()
+                .filter(car -> car.getLocation() == maxLocation)
+                .toList();
+
+        return championCars;
+    }
+
+    private void progressOneStep() {
+        for (Car car : cars) {
+            if (isCarMoveForward()) {
+                car.moveForward();
+            }
+        }
+    }
+
     private boolean isCarMoveForward() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         return randomNumber >= 4;
