2eeb35bf80093fdb99d68e080f737174293a964e
Dain Lee <dlekdls529@gmail.com>
2023-10-29 15:23:15 +0900

    feat: 횟수만큼 이동하고 결과를 출력하는 기능

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,2 +1,30 @@
-package racingcar.model;public class Race {
+package racingcar.model;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Race {
+    private List<Integer> carScoreList;
+
+    public void racingCar(int count, List<String> carNameList) {
+        carScoreList = new ArrayList<>(
+                Collections.nCopies(carNameList.size(),0)
+        );
+
+        for (int i = 0; i < count; i++) {
+            Forward forward = new Forward();
+            carScoreList = forward.moveForward(carScoreList);
+            print(carNameList);
+        }
+    }
+
+    private void print(List<String> carNameList){
+        for (int i=0;i<carNameList.size();i++){
+            System.out.println(carNameList.get(i) + " : " + "-".repeat(carScoreList.get(i)));
+        }
+        System.out.println();
+    }
+
 }
