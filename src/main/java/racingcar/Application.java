5d1d374b5a016af407a3de00d0bb0120d68d650a
Yu JeongMin <polar522@naver.com>
2023-10-29 15:51:33 +0900

    기능 3-1. 자동차 이름을 쉼표를 기준으로 구분하여 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,7 @@ public class Application {
             // 기능 1-1. 자동차의 이름을 입력 받아 몇 대(n)의 자동차로 경기를 할 것인지 입력 받기
             String Input = getInput();
             // 기능 2-1. 자동차의 이름을 저장하여 각 라운드마다 자동차의 이름과 진행 상황을 프린트하는 함수인 playRound() 메서드 구현
+            // 기능 3-1. 자동차 이름을 쉼표를 기준으로 구분하여 저장
             String[] cars = Input.split(",");
             // 자동차의 수만큼 0을 초기값으로 갖는 배열 생성
             int[] location = new int[cars.length];
