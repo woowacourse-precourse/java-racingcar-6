0ebe05b1ad3025e95a873f705e7eec33076b4c25
PUNGZI <ashyoo1367@gmail.com>
2023-10-29 18:52:55 +0900

    feat: 한 번 이동할 때마다 각 자동차의 이동횟수를 출력하는 기능 구현

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -8,9 +8,19 @@ public class Racing {
         Car car = new Car();
 
         int raceCount = racingCount.inputRacingCount();
+        System.out.println();
+        System.out.println("실행 결과");
         Map<String, Integer> result = null;
         for (int i=0; raceCount>i; i++) {
             result = car.racingCarCount();
+            result.forEach((key, value)->{
+                System.out.print(key + " : ");
+                for (int j = 0; value > j; j++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            });
+            System.out.println();
         }
         return result;
     }
