101a8fac39f312eed1b093974a0fbd05cd2824e1
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 19:47:55 +0900

    feat(OutputView): 사용자에게 결과와 현황을 알려주는 InputView 클래스 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,33 @@
+package racingcar.view;
+
+import racingcar.domain.dto.RunningCarDto;
+import racingcar.domain.dto.WinnerDto;
+import racingcar.util.StringManipulator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private static final String INTRODICE_WINNER = "최종 우승자 : ";
+    private static final String RESULT = "\n실행 결과";
+
+    public void notifyResultRotate() {
+        System.out.println(RESULT);
+    }
+
+    public void notifyCarRunning(List<RunningCarDto> runningCarDtos) {
+        runningCarDtos.stream()
+                .map(runningCarDto -> runningCarDto.name() + " : " + "-".repeat(runningCarDto.location() * 2))
+                .forEach(System.out::println);
+    }
+
+    public void notifyWinner(List<WinnerDto> winners) {
+        System.out.print(INTRODICE_WINNER);
+
+        List<String> resultWinner = winners.stream()
+                .map(WinnerDto::Name)
+                .collect(Collectors.toList());
+
+        System.out.println(StringManipulator.join(resultWinner));
+    }
+}
