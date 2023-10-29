02b8a4e4092c5ed08f776e30ec3e6d42a53335b2
Nara Park <skfk6352@naver.com>
2023-10-29 20:42:31 +0900

    <feat>: create validation method for length of car name
    자동차 이름의 길이에 대한 검증 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,11 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Car {
 
+    private static final int CAR_NAME_MIN_SIZE = 1;
+    private static final int CAR_NAME_MAX_SIZE = 5;
     private final String name;
     private int position;
 
@@ -12,5 +16,10 @@ public class Car {
 
     public static Car nameFrom(String name) {
         return new Car(name, 0);
+    }
+
+    private boolean validateNameSize(List<String> carNameList) {
+        return carNameList.stream()
+                .allMatch(carName -> CAR_NAME_MIN_SIZE <= carName.length() && carName.length() < CAR_NAME_MAX_SIZE);
     }
 }
---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+}
