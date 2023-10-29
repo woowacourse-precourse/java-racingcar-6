42ec95f2482c891f878f9d71d7573abd9f4777ef
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 20:24:57 +0900

    feat(DtoConverter): Car객체를 WinnerDto로 변환하는 toWinnerDto 메소드 추가

--- src/main/java/racingcar/converter/DtoConverter.java
+++ src/main/java/racingcar/converter/DtoConverter.java
@@ -2,10 +2,15 @@ package racingcar.converter;
 
 import racingcar.domain.Car;
 import racingcar.domain.dto.RunningCarDto;
+import racingcar.domain.dto.WinnerDto;
 
 public class DtoConverter {
 
-    public static RunningCarDto toRunningCarRecord(Car car) {
+    public static RunningCarDto toRunningCarDto(Car car) {
         return new RunningCarDto(car.getName(), car.getLocation());
+    }
+
+    public static WinnerDto toWinnerDto(Car car) {
+        return new WinnerDto(car.getName());
     }
 }
