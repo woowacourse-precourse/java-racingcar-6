3139b0ef6e50da41ed92a19cfe36e0af2da093db
이종경 <gyoung3063413@naver.com>
2023-10-30 01:31:04 +0900

    [수정] Game class 수정

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -11,7 +11,7 @@ public class Game {
 	private static int[] moveCount;
 	private static ArrayList<Integer> maxIndices = new ArrayList<>();
 
-	private String[] checkUsername(String[] input) {
+	public String[] checkUsername(String[] input) {
 		for (int i = 0; i < input.length; i++) {
 			if (input[i].length() > 5) {
 				throw new IllegalArgumentException();
@@ -22,16 +22,16 @@ public class Game {
 		return userArray = input;
 	}
 
-	public String[] setGameUsers() {
+	private String[] setGameUsers() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String userInput = readLine();
 
 		userArray = userInput.split(","); // 사용자 등록
-		
+
 		return this.checkUsername(userArray); // 유효성 검사
 	}
 
-	private int intValidator(String userInput) {
+	public int intValidator(String userInput) {
 		try {
 			return Integer.parseInt(userInput);
 		} catch (NumberFormatException e) {
@@ -40,11 +40,11 @@ public class Game {
 
 	}
 
-	public int setGameCount() {
+	private void setGameCount() {
 		System.out.println("시도할 회수는 몇회인가요?");
 		String userInput = readLine();
 
-		return count = this.intValidator(userInput);// 이동할 회수 등록
+		count = this.intValidator(userInput);// 이동할 회수 등록
 	}
 
 	public void forwardOrStop() {
@@ -87,7 +87,7 @@ public class Game {
 		String winner = "";
 
 		this.findWinnerIndex();
-		
+
 		for (int i = 0; i < maxIndices.size(); i++) {
 			if (i == maxIndices.size() - 1) {
 				winner += userArray[maxIndices.get(i)];
