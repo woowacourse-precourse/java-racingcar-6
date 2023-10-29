9c0c90b927b36af9bb3f5b58d8aba272e861a741
yu-teacher <yusm1231@gmail.com>
2023-10-29 17:10:03 +0900

    refactor : 기능 축약
    validateNameLength -> X
    
    사유 : validateNameLength 메서드의 역할의 사용처가 하나밖에 없어 삭제하였습니다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,25 +6,22 @@ public class Car {
     private int move;
 
     public Car(String name) {
-        this.name = validateNameLength(name);
+        name = name.trim();
+        if (name.length() > 5 || name.length() == 0) {
+            throw new IllegalArgumentException("글자수 문제 발생! 5글자가 넘거나 비어있습니다.");
+        }
+        this.name = name;
         this.move = 0;
     }
 
     public String getName() {
         return this.name;
     }
-
     public int getMove() {
         return this.move;
     }
-
     public void plusMove() {
         this.move++;
     }
 
-    private String validateNameLength(String name) {
-        if (name.length() > 5)
-            throw new IllegalArgumentException();
-        return name;
-    }
 }
