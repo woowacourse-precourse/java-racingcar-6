319be6685cb9e3a313a931d278ecf6bb04caa531
SUJI-J <suzy852@naver.com>
2023-10-29 17:34:22 +0900

    refactor: 중복값 검증 기능 Validator로 이전

--- src/main/java/racingcar/InputConvertor.java
+++ src/main/java/racingcar/InputConvertor.java
@@ -1,23 +1,20 @@
 package racingcar;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
-import static racingcar.GameMessage.INPUT_DUPLICATE_EXCEPTION_MESSAGE;
 
 public class InputConvertor {
 
     public static Map<String, String> toMap(String inputCarNames) {
-        Map<String, String> carNamesMap = new HashMap<>();
+        Map<String, String> racingCars = new HashMap<>();
         String[] tempStr = inputCarNames.split(",");
         for (String str : tempStr) {
-            if (carNamesMap.containsKey(str)) {
-                throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MESSAGE);
+            if (racingCars.containsKey(str)) {
+                Validator.isDuplicateNames();
             }
-            carNamesMap.put(str, "");
+            racingCars.put(str, " ");
         }
-        return carNamesMap;
+        return racingCars;
     }
 
     public static int toInt(String inputNumber) {
