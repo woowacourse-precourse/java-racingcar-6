14c5bccdba7c246744f36c04726e19bf040eb698
SeYeol00 <dmot0126@gmail.com>
2023-10-30 01:34:37 +0900

    reafactor: 결과값 전까지 각 컴포넌트 결합 및 아웃풋 클래스 리드미에 정의

--- src/main/java/racingcar/service/game/RacingGame.java
+++ src/main/java/racingcar/service/game/RacingGame.java
@@ -1,19 +1,48 @@
 package racingcar.service.game;
 
 import racingcar.domain.Cars;
+import racingcar.dto.input.TimeInputDto;
+import racingcar.dto.output.CarsOutputDto;
 import racingcar.service.input.Input;
 import racingcar.service.input.InputView;
+import racingcar.service.output.Output;
+import racingcar.service.output.OutputView;
+import racingcar.service.race.Racing;
+import racingcar.service.race.RandomLogicRace;
 
 public class RacingGame implements GameInterface{
+    private static final int END_TIME = 0;
     private final Input input;
-    public RacingGame() {
+    private final Output output;
+    private final Racing racing;
+    public RacingGame(Output output, Racing racing) {
+        this.output = new OutputView();
+        this.racing = new RandomLogicRace();
         this.input = new InputView();
     }
 
     @Override
     public void play() {
-        Cars.of(input.getCarInput());
-        
+        Cars cars = Cars.of(input.getCarInput());
+        int time = getTime();
+        competing(time, cars);
+
+    }
+
+    private void competing(int time, Cars cars) {
+        while(isCounting(time)){
+            racing.race(cars);
+            output.showPlaying(new CarsOutputDto(cars));
+            time--;
+        }
+    }
+
+    private static boolean isCounting(int time) {
+        return time > END_TIME;
+    }
+
+    private int getTime() {
+        return input.getTimeInput().time();
     }
 
 
