4a4645a379e5400ab52ced3f7493506c88a63277
formoin <alsrud869@gmail.com>
2023-10-29 15:23:40 +0900

    자동차 수 만큼 랜덤 수 발생 및 전진 여부 결정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,46 +1,66 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 
-import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 import static java.lang.System.exit;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String[] namelist = null;
+        int[] scorelist;
+        List<String> winner = new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         try {
-            String[] names = getCarNames();
+            namelist = getCarNames();
         }
         catch (IllegalArgumentException e){
             e.printStackTrace();
             exit(0);
         }
+
         System.out.println("시도할 회수는 몇회인가요?");
-        int trial = getTrialNum();
+        int N = getTrialNum();
+        scorelist = new int[namelist.length];
 
-        for(int i=0; i<trial; i++){
-
+//        입력받은 횟수만큼 반복
+//        - 자동차 이름과 _로 전진 횟수 화면 출력
+        for(int i=0; i<N; i++){
+            judgeRunning(namelist, scorelist);
+            for(int j=0; j<namelist.length; j++){
+                System.out.println(namelist[j] + " : " + "_".repeat(scorelist[j]));
+            }
+            System.out.println();
         }
+
+
+
 
     }
 
     public static String[] getCarNames() throws IllegalArgumentException{
-        String name;
 
-        name = Console.readLine();
-        String[] nameslist = name.split(",");
+        String name = Console.readLine();
+        String[] names = name.split(",");
 
-        for(int i=0; i<nameslist.length; i++){
-            if(nameslist[i].length() >5){
+        for(int i=0; i<names.length; i++){
+// fortest  System.out.println(names[i]);
+            if(names[i].length() >5){
                 throw new IllegalArgumentException();
             }
         }
 
-        return nameslist;
+        return names;
     }
 
     public static int getTrialNum(){
@@ -57,6 +77,18 @@ public class Application {
 //        if(isInteger()){
 //            throw new IllegalArgumentException();
 //        }
+
+    }
+    //          - 자동차 마다 1~9 범위의 무작위 값을 추출
+    //              - 4이상일때 전진
+    //              - 3이하일 때 멈춤
+    //              - 자동차별 전진 횟수 리스트 저장
+    public static void judgeRunning(String[] namelist, int[] scorelist){
+
+        for(int i=0; i<namelist.length; i++){
+            int random = Randoms.pickNumberInRange(0,9);
+            if(random>=4) scorelist[i]++;
+        }
 
     }
 }
