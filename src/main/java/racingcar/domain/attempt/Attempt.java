42cf35fadfb58b7e6f5318e2263936a508a551a3
wwan13 <wwan13@naver.com>
2023-10-29 20:08:55 +0900

    feat : `Attempt` 기능 개발 - 입력 받은 전체 시도 횟수를 저장한다. - 입력받은 값이 1보다 작으면 IllegalArgumentException을 발생한다. - 남아있는 시도 횟수를 1회 사용한다. - 남아있는 시도가 있는지 알려준다.

---
+++ src/main/java/racingcar/domain/attempt/Attempt.java
@@ -0,0 +1,33 @@
+package racingcar.domain.attempt;
+
+public class Attempt {
+
+    private static final int INPUT_MINIMUM_VALUE = 1;
+    private static final int ATTEMPT_MINIMUM_VALUE = 0;
+    private static final String ERROR_MESSAGE_TOO_LOW = "최소 1 이상의 값을 입력해 주세요.";
+
+    private int value;
+
+    public Attempt(int value) {
+        validateValue(value);
+        this.value = value;
+    }
+
+    private void validateValue(int input) {
+        if (input < INPUT_MINIMUM_VALUE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_TOO_LOW);
+        }
+    }
+
+    public void use() {
+        hasNext();
+        value -= 1;
+    }
+
+    public boolean hasNext() {
+        if (value <= INPUT_MINIMUM_VALUE) {
+            return false;
+        }
+        return true;
+    }
+}
