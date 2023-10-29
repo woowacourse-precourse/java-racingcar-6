fdd38868da44de429c5f4736396ab1f8332a727c
HSBODo <vinsulill@gmaile.com>
2023-10-29 16:23:49 +0900

    Feat: 기능구현 전 메서드 분리 네이밍 및 분리
    구현할 메서드들 정의
    - inputCarName()
    - inputCarNameValidation(String inputCarName)
    - cars(String inputCarName)
    - inputAttempts()
    - inputAttemptsValidation(String inputAttempts)
    - randomNumber()
    - movementCount(Map<String,Integer> cars)
    - progressDisplay(Map<String,Integer> cars)
    - winnerDisplay(Map<String,Integer> cars)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,47 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        // Commit 테스트
+
+
+
+    }
+    public String inputCarName(){
+      return null;
+    }
+
+    public String inputCarNameValidation(String inputCarName){
+      return null;
+    }
+
+    public Map<String,Integer> cars(String inputCarName){
+      return null;
+    }
+
+    public String inputAttempts(){
+      return null;
+    }
+
+    public String inputAttemptsValidation(String inputAttempts){
+      return null;
+    }
+
+    public int randomNumber(){
+      return 0;
+    }
+
+    public Map<String,Integer> movementCount(Map<String,Integer> cars){
+      return null;
+    }
+
+    public void progressDisplay(Map<String,Integer> cars){
+
+    }
+
+    public void winnerDisplay(Map<String,Integer> cars){
 
     }
 }
