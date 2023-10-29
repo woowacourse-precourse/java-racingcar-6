d1c892f3ad3048342881545f075e51594839eebf
HyunmoBae <tsi0520@naver.com>
2023-10-29 20:32:58 +0900

    feat(racingcar) : add showWinner function

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,8 +1,20 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Referee {
 
-    public String showWinner() {
-        return null;
+    public String showWinner(List<Car> carList) {
+        String winner = "";
+        int position = 0;
+        for (int i = 0; i < carList.size(); i++) {
+            if (position < carList.get(i).carPosition) {
+                position = carList.get(i).carPosition;
+                winner = carList.get(i).carName;
+            } else if (position == carList.get(i).carPosition) {
+                winner += ", " + carList.get(i).carName;
+            }
+        }
+        return "최종 우승자 : " + winner;
     }
 }
