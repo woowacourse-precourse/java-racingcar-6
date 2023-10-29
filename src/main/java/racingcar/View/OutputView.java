c67da044022dada10717c387af42518b77128180
YejiGong <yejigong00@gmail.com>
2023-10-30 03:51:16 +0900

    Feat: 출력View 구현

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -2,16 +2,41 @@ package racingcar.View;
 
 import java.util.List;
 import java.util.Map;
+import racingcar.Global.Constants;
 
 public class OutputView {
-    public void printEachStageResult(Map<String, Integer> results){
+    public static final String stepSymbol = "-";
 
+    public void printEachStageResult(Map<String, Integer> results) {
+        results.forEach((name, step) -> {
+            System.out.println(makeResultState(name, step));
+        });
     }
 
-    public void printString(String str){
-
+    private String makeResultState(String name, int step) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name);
+        stringBuilder.append(" : ");
+        for (int i = 0; i < step; i++) {
+            stringBuilder.append(stepSymbol);
+        }
+        return stringBuilder.toString();
     }
-    public void printWinner(List<String> name){
 
+    public void printString(String str) {
+        System.out.println(str);
+    }
+
+    public void printWinner(List<String> names) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(Constants.gameWinnerState);
+        stringBuilder.append(" :");
+        for (String name : names) {
+            stringBuilder.append(" ");
+            stringBuilder.append(name);
+            stringBuilder.append(",");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        System.out.println(stringBuilder.toString());
     }
 }
