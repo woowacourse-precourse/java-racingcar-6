f14fd5de70a82bb654829bab4ec13f65a1968c54
stopmin <geemin0@naver.com>
2023-10-30 02:06:26 +0900

    feat: 자동차를 생성해주는 controller생성(CarFactory)

---
+++ src/main/java/racingcar/controller/CarFactory.java
@@ -0,0 +1,25 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public List<Car> createCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+
+        return cars;
+    }
+
+}
