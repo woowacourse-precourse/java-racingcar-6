55c3891afc4061450bdcb425beaab259c1124dc9
JIHYE <jdh324@naver.com>
2023-10-29 17:36:39 +0900

    test: 자동차 이름 구분 기능 테스트 코드 작성

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+
+    @Test
+    void 자동차_이름_구분() {
+        // given
+        String car1 = "wiz";
+        String car2 = "JiHye";
+        String input = car1 + "," + car2;
+
+        // when
+        Map<String, Integer> racingCars = Application.validateCarName(input);
+
+        // then
+        assertThat(racingCars).containsKey(car1);
+        assertThat(racingCars).containsKey(car2);
+    }
+
+    @Test
+    void 자동차_이름_구분시_자동차_이름이_5자를_초과할_때_예외_발생() {
+        // given
+        String car1 = "wisdom";
+
+        // when & then
+        assertThatThrownBy(() -> Application.validateCarName(car1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+}
