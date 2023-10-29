d2c1b75c9026ca71897e6a589f844fecc2b56bbb
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 02:12:54 +0900

    test(Validator): 이름에 허용되는 문자만 포함되어있는지 검증하는 함수 테스트 코드 작성

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -3,7 +3,6 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static racingcar.Validator.*;
 
 class ValidatorTest {
@@ -99,6 +98,30 @@ class ValidatorTest {
 
         // when
         boolean result = hasNoConsecutiveCommas(input);
+
+        // then
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    void 이름에_허용되는_문자만_포함되어_있는_검증_성공_케이스() {
+        // given
+        String input = "성겸,성민12,성우a";
+
+        // when
+        boolean result = validateNames(input);
+
+        // then
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    void 이름에_허용되는_문자만_포함되어_있는지_검증_실패_케이스() {
+        // given
+        String input = "성겸@,성민,성우";
+
+        // when
+        boolean result = validateNames(input);
 
         // then
         Assertions.assertThat(result).isFalse();
