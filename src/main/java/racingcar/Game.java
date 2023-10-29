d8637198d2e64e1b97791efafcb0c51ab70615ba
StrangeMin <mim0107@nate.com>
2023-10-30 05:57:25 +0900

    feat : Game 구성
    추가한 기능:
    - 자동차 이름 입력받기, 경기 진행 횟수 입력받기, 라운드 진행, 우승자
      선정 기능들을 조합하여 Game 구성
    - main 문에서 Game이 실행될 수 있도록 구성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,11 +2,33 @@ package racingcar;
 
 public class Game {
 
-    private Cars cars;
+    private Cars cars = new Cars();
     private Integer roundCount;
 
-    public void setRoundCount(String userInput){
-        roundCount = Integer.parseInt(userInput);
+    public void start(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Input.getCarNames();
+        cars.set(carNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        setRoundCount(Input.getRoundCount());
+        System.out.println();
+        System.out.println("실행 결과");
+        startRound();
+
+        Winner winner = new Winner(cars.getWinners());
+        winner.printWinner();
+    }
+
+    public void startRound(){
+        for(int i=0; i<roundCount; i++){
+            cars.moveAll();
+            cars.printCarPositions();
+        }
+    }
+
+    public void setRoundCount(Integer roundCount){
+        this.roundCount = roundCount;
     }
 
     public Integer getRoundCount(){
