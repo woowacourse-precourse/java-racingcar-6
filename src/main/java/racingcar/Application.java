194642067bcb91346bedbe25e27d3fd119c6b7d4
songyi <song21677@naver.com>
2023-10-29 20:15:06 +0900

    feat: 자동차 이름 중복 검증

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -32,5 +32,10 @@ public class Application {
                 throw new IllegalArgumentException("이름은 공백 포함 5자 이하여야 합니다.");
             }
         }
+
+        long sizeAfterDistinct = carList.stream().distinct().count();
+        if (sizeAfterDistinct != carList.size()) {
+            throw new IllegalArgumentException("중복된 이름입니다.");
+        }
     }
 }
