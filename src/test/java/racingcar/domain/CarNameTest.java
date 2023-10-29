ec2f89d73c97ea4078298e321bd5d6b3a5457ddd
김강년 <64246800+ekek54@users.noreply.github.com>
2023-10-29 17:08:59 +0900

    test(CarNameTest): 5자 초과 CarName 실패 테스트 추가
    CarName 테스트 추가:
    "자동차 이름은 5자 이하만 가능하다." 예외처리 확인을 위한 실패 테스트

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,16 @@ class CarNameTest {
         CarName carName = new CarName(name);
         //then
         assertThat(carName.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void CarName_생성_실패_테스트() {
+        // given
+        String name = "javaji";
+        //when
+        //then
+        assertThatThrownBy(() -> new CarName(name))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
     }
 }
