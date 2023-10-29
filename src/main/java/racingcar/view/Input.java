7f9f6a3e0d7b4a31a81732fea0f2565afe816d16
ts3083 <ts04031@gmail.com>
2023-10-29 17:20:35 +0900

    feat: 시도할 횟수 입력

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,10 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-    private static final String askRacingCarNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
 
     public String getCarNames() {
-        System.out.println(askRacingCarNames);
+        System.out.println(ASK_RACING_CAR_NAMES);
+        return Console.readLine();
+    }
+
+    public String getNumberOfAttempts() {
+        System.out.println(ASK_NUMBER_OF_ATTEMPTS);
         return Console.readLine();
     }
 }
