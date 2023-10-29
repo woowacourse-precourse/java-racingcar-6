b63afe3ebe365e5c0ec59616ba66a9cdddecd65a
정찬영 <jcy0342@gmail.com>
2023-10-29 16:13:21 +0900

    refactor: CarTest와 CarsTest 분리 및 테스트 코드 폴더 구조 main 이랑 일치
    폴더 구조를 프로덕션 코드와 일치시키면 찾기 수월하다고 생각하여 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,31 +1,11 @@
-package racingcar;
+package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.model.Car;
-import racingcar.model.Cars;
 
 public class CarTest {
-
-    @Test
-    void 자동차_이름이_콤마로_구분되어_저장된다() {
-        //Given
-        Cars cars = new Cars();
-        String inputString = "포비, 워니, 차니";
-
-        //When
-        cars.setCarList(inputString);
-        List<Car> carList = cars.getCarList();
-
-        //Then
-        assertEquals("포비", carList.get(0).getName());
-        assertEquals("워니", carList.get(1).getName());
-        assertEquals("차니", carList.get(2).getName());
-    }
-
     @Test
     void 이름이_NULL이거나_길이가_범위를_벗어나면_예외발생() {
         //Given
@@ -50,6 +30,7 @@ public class CarTest {
         //Then
         assertEquals(1, car.getDistance());
     }
+
     @Test
     void 무작위_값이_기준값_이하일_때_1칸_이동() {
         //Given
---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+    @Test
+    void 자동차_이름이_콤마로_구분되어_저장된다() {
+        //Given
+        Cars cars = new Cars();
+        String inputString = "포비, 워니, 차니";
+
+        //When
+        cars.setCarList(inputString);
+        List<Car> carList = cars.getCarList();
+
+        //Then
+        assertEquals("포비", carList.get(0).getName());
+        assertEquals("워니", carList.get(1).getName());
+        assertEquals("차니", carList.get(2).getName());
+    }
+}
