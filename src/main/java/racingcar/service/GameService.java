4ba72c194512526ed35f91b440161f347862ab09
King's meow <mingee641@gmail.com>
2023-10-29 15:30:28 +0900

    refator: 비교, 결과출력 기능 추가

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,6 +1,8 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.config.Config;
 import racingcar.model.Car;
 import racingcar.utils.RandomUtil;
@@ -55,19 +57,44 @@ public class GameService {
     tryNum = Validation.isNumber(Console.readLine());
   }
 
+
   public void carForward() {
+    System.out.println("실행 결과");
     for(int i=0;i<tryNum;i++) {
       for(int j=0;j<car.length;j++) {
         forwardNum = RandomUtil.getRandomNumber(minValue, maxValue);
         if(forwardNum >= setValue) {
           car[j].increaseForward();
         }
+        System.out.print(car[j].getName() + " : ");
+        for(int k=0;k<car[j].getForward();k++)
+          System.out.print("-");
+        System.out.println();
+      }
+      System.out.println();
+    }
+
+    List<Car> winnerList = new ArrayList<Car>();
+    int max= -1;
+
+    for(int i=0;i<car.length;i++) {
+      if(car[i].getForward() > max) {
+        max = car[i].getForward();
+        winnerList.clear(); // 새로운 최대값을 찾았으므로 이전 우승자 목록을 지웁니다.
+        winnerList.add(car[i]); // 현재 자동차를 우승자로 설정
+      } else if(car[i].getForward() == max) {
+        winnerList.add(car[i]); // 중복된 경우 우승자 목록에 추가
       }
     }
 
-    System.out.println("최종결과");
-    for(Car c: car) {
-      System.out.println(c.getName() + " : " + c.getForward());
+    System.out.print("최종 우승자 : ");
+    for (int i = 0; i < winnerList.size(); i++) {
+      Car winner = winnerList.get(i);
+      System.out.print(winner.getName());
+
+      if (i < winnerList.size() - 1) {
+        System.out.print(", ");
+      }
     }
   }
 
