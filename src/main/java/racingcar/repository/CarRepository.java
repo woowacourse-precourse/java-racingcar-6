f604b2eb82721a9430c4f9de3ee8c163fca8f341
yang <ywj9811@gmail.com>
2023-10-30 05:09:08 +0900

    feat : repository 생성

---
+++ src/main/java/racingcar/repository/CarRepository.java
@@ -0,0 +1,25 @@
+package racingcar.repository;
+
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.constant.NumberConstant.MOVE_FORWARD_VALUE;
+
+public class CarRepository {
+    public List<Car> save(String[] inputNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : inputNames) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    public void moveForward(Car car, int value) {
+        if (value >= MOVE_FORWARD_VALUE.getNumber()) {
+            car.moveForward();
+        }
+    }
+}
