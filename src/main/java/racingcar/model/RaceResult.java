05e4c86bdfda1577b544c62055999c7ba36a64a1
unknown <khd128k@gmail.com>
2023-10-29 21:49:42 +0900

    feat: model/RaceResult

--- src/main/java/racingcar/model/RaceResult.java
+++ src/main/java/racingcar/model/RaceResult.java
@@ -1,4 +1,12 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
 public class RaceResult {
+    public static List<Car> getWinners(List<Car> cars) {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
 }
