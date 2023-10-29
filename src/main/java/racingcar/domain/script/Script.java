d1b689a7500bf48aaf2325cc96c09dfdb908191f
jdohun <wjdehgns97@naver.com>
2023-10-29 12:42:03 +0900

    refactor: LINEUP -> REGISTER 로 변경

--- src/main/java/racingcar/domain/script/Script.java
+++ src/main/java/racingcar/domain/script/Script.java
@@ -1,7 +1,7 @@
 package racingcar.domain.script;
 
 public enum Script {
-    LINEUP("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
+    REGISTER("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
     LAPS("시도할 회수는 몇회인가요?\n"),
     RESULT("실행 결과\n"),
     PRIZE_CEREMONY("최종 우승자 : ");
