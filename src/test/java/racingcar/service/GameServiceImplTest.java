9108c1ba8cd43caeb95643a554ee2e66523345da
inpink <inpink@kakao.com>
2023-10-29 21:41:06 +0900

    test: "이름을 1개 이상 입력하지 않으면 잘못된 입력" 기능 단위 테스트
    아무것도 입력하지 않거나, 공백만 입력하거나, 콤마만 입력하는 경우에 예외 발생해야함.

--- src/test/java/racingcar/service/GameServiceImplTest.java
+++ src/test/java/racingcar/service/GameServiceImplTest.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ public class GameServiceImplTest {
     }
 
     @Test
-    public void Car들_한줄에_입력받아_저장하기() {
+    public void Car_Name들_한줄에_입력받아_저장하기() {
         // given
         String input = "pobi,woni,jun";
 
@@ -28,5 +29,44 @@ public class GameServiceImplTest {
 
         // then
         assertThat(memoryCarRepository.findAll()).hasSize(3);
+    }
+
+    @Test
+    public void Car_Name_미입력시_예외발생() {
+        // given
+        String input = "";
+
+        //when
+        final RuntimeException result = assertThrows(IllegalArgumentException.class,
+                () -> gameService.processCarNamesInput(input));
+
+        //then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    public void Car_Name_공백입력시_예외발생() {
+        // given
+        String input = " ";
+
+        //when
+        final RuntimeException result = assertThrows(IllegalArgumentException.class,
+                () -> gameService.processCarNamesInput(input));
+
+        //then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    public void Car_Name_콤마만_입력시_예외발생() {
+        // given
+        String input = ", , ,,";
+
+        //when
+        final RuntimeException result = assertThrows(IllegalArgumentException.class,
+                () -> gameService.processCarNamesInput(input));
+
+        //then
+        assertThat(result).isNotNull();
     }
 }
