1250b0ead9429de1ba7da891a9cfa222d0b12a64
HYUNJUN\sajun <sajun28@naver.com>
2023-10-29 23:47:16 +0900

    Feat: "시도 횟수 기능 추가"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -8,13 +8,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameController {
+    private int randomNum, attemptsNum;
     private final InputView inputView = new InputView();
     private final Name name = new Name();
     private final Move move = new Move();
     public void start() {
         List<String> player = Arrays.asList(inputView.name().split(","));
         name.isValid(player);
-        int randomNum = move.generateRandom();
+        attemptsNum = Integer.parseInt(inputView.attemptsNum());
+        randomNum = move.generateRandom();
         System.out.println(randomNum);
+        System.out.println(attemptsNum);
     }
 }
