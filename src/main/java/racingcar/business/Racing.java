535cd2816a218667dffcf35147100b4563aea64c
gumgu <bienlee@naver.com>
2023-10-29 20:27:00 +0900

    Feat : 이름 입력받기 기능 구현

--- src/main/java/racingcar/business/Racing.java
+++ src/main/java/racingcar/business/Racing.java
@@ -1,12 +1,18 @@
 package racingcar.business;
 
+import racingcar.ui.InputView;
 import racingcar.ui.Output;
 
+import java.util.List;
+
 public class Racing {
+
+    InputView inputView = new InputView();
 
     public void doRacing() {
 
         Output.GET_NAMES.print();
+        List<String> names = inputView.getNames();
 
 
     }
--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -9,6 +9,18 @@ public class InputView {
 
     public static final String SPLITTER = ",";
 
+    public List<String> getNames() {
+        String input = getInput();
+
+        Validator.validateInput(input);
+
+        List<String> names = splitNames(input);
+
+        Validator.validateNameList(names);
+
+        return names;
+    }
+
     private List<String> splitNames(String input) {
         //TODO String[], List<String>중 어느것이 더 적절할까?
         return Arrays.asList(input.split(SPLITTER));
