d5a71a206eb22cd8909a92a79c631a01956a8e8c
sinminseok12 <sinminseok12@naver.com>
2023-10-29 19:43:44 +0900

    feat(InputView): 시도 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class Parser {
     private static final String COMMA = ",";
+
+    public static Integer parseInteger(String input) {
+        return Integer.parseInt(input);
+    }
+
     public static List<String> parseStrings(String input) {
         return List.of(input.split(COMMA));
     }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,16 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
-import static racingcar.utils.Parser.parseStrings;
+import static racingcar.utils.Parser.*;
 
 public class InputView {
 
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String input = Console.readLine();
         //todo 입력값 겁증
         return parseStrings(input);
+    }
+
+    public Integer inputTryCount() {
+        System.out.println(INPUT_TRY_COUNT);
+        String input = Console.readLine();
+        //todo 입력값 검증
+        return parseInteger(input);
     }
 }
