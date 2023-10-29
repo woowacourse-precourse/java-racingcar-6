760a2eb3b876303ad983588a83d3f8f05bce0292
Jiwon <wldnjs980409@naver.com>
2023-10-29 18:25:55 +0900

    Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.PlayGameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        PlayGameController playGameController = new PlayGameController();
+
+        playGameController.gameStart();
     }
 }
