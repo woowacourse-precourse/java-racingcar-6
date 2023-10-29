d5281d6fc33ffb5244e7479d91053165424d6c6f
sonjeongdong <sjd0219@gmail.com>
2023-10-30 02:01:40 +0900

    Feat: 자동차 이름을 입력받는 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        printInputCarName();
+
     }
 
     public static void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static String getCarName() {
+        printInputCarName();
+        String input = Console.readLine();
+        return input;
     }
 }
