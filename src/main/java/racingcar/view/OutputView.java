0e73978ae35a640bd0675e5f618ffb2acbaa4575
sujnkim <blackout3210@naver.com>
2023-10-29 21:50:13 +0900

    feat(view): 우승자의 이름을 출력하는 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String PROCESS_PRINT_START = "실행 결과";
+    private static final String PROCESS_BAR = "-";
+    private static final String WINNER_SENTENCE = "최종 우승자 : ";
+    private static final String COMMA = ", ";
 
 
     public static void processPrintStartMessage() {
@@ -14,7 +19,14 @@ public class OutputView {
     }
 
 
-    public static void printWinner() {
+    public static void printWinner(List<String> winners) {
+        StringBuilder message = new StringBuilder();
 
+        message.append(WINNER_SENTENCE);
+        winners.stream().forEach(name -> message.append(name).append(COMMA));
+        message.delete(message.length()-2, message.length()-1);
+
+        System.out.println(message);
     }
+
 }
