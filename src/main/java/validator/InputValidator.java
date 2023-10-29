6aee664ff838233337711fde4a034ea01059f8c6
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-29 13:27:47 +0900

    style: 잘못된 변수명 수정 및 throw구문 수정

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -8,17 +8,16 @@ public class InputValidator {
     private final String REGEXP_PATTERN_ONLY_COMMA = "^,+$";
 
 
-    public void checkCarNameValidate(String input) throws IllegalArgumentException {
+    public void checkCarNameValidate(String input) {
         isNameOnlyComma(input);
         String[] carNames = input.split(",");
         for(String name: carNames) {
-            System.out.printf(name);
-            isNameBelowFiveLetters(input);
-            isNameContainBlank(input);
+            isNameBelowFiveLetters(name);
+            isNameContainBlank(name);
         }
     }
 
-    public void checkNumOfAttempt(String input) throws IllegalArgumentException {
+    public void checkNumOfAttempt(String input) {
         if(!Pattern.matches(REGEXP_PATTERN_SPACE_NO_CHAR,input)){
             throw new IllegalArgumentException("공백이 포함되어 있거나 빈 문자열입니다.");
         }
