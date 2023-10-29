5540e2072d2e5ef6194c80e6be005c7da60bbb86
howudong <tjdvy963@naver.com>
2023-10-29 15:57:26 +0900

    feat : InputView, OutputView 인터페이스 추가

---
+++ src/main/java/racingcar/view/inputview/InputView.java
@@ -0,0 +1,7 @@
+package racingcar.view.inputview;
+
+import java.util.Map;
+
+public interface InputView {
+    void read(Map<String, Object> readEntities);
+}
---
+++ src/main/java/racingcar/view/outputview/OutputView.java
@@ -0,0 +1,7 @@
+package racingcar.view.outputview;
+
+import java.util.Map;
+
+public interface OutputView {
+    void display(Map<String, Object> displayEntities);
+}
