ad64201f021d7683a9cf327172f3d99d7c254794
Haeun Jeong <gkdms4248@gmail.com>
2023-10-29 19:05:26 +0900

    feat(model): 랜덤 숫자 생성 기능 추가

---
+++ src/main/java/racingcar/model/Generator.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Generator {
+    private final static int MIN_NUMBER = 0;
+    private final static int MAX_NUMBER = 9;
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
