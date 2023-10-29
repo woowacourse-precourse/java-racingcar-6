f3ce1a5c153dd7edfd4c5bf02408e8fd03554412
jaegyeongkim <dongji_11@naver.com>
2023-10-29 16:15:25 +0900

    feat(Utils): 이동 횟수 검증 기능 구현

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,11 +1,6 @@
 package utils;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import model.Car;
-import model.Cars;
 
 public class Utils {
     final static int MAX_INPUT_LENGTH = 5;
@@ -13,6 +8,8 @@ public class Utils {
     final static String INPUT_HAS_SPACE = "입력은 공백이 포함될 수 없습니다.";
     final static String INPUT_OVER_LENGTH = "입력은 5자 이하여야 합니다.";
     final static String DUPLICATION_NAME = "자동차의 이름은 중복되지 않은 고유한 값이어야 합니다.";
+    final static String NOT_NUMBER = "시도 횟수는 0 이상의 정수이어야 합니다.";
+    final static String NEGATIVE_NUMBER = "시도 횟수는 음수가 될 수 없습니다.";
 
     public void hasSpace(String input) {
         if (input.contains(" ")) {
@@ -34,9 +31,7 @@ public class Utils {
 
     public void duplicateCarName(String input) {
 
-        boolean duplicationName = Arrays.stream(input.split(","))
-                .distinct()
-                .count() != input.split(",").length;
+        boolean duplicationName = Arrays.stream(input.split(",")).distinct().count() != input.split(",").length;
 
         if (duplicationName) {
             throw new IllegalArgumentException(DUPLICATION_NAME);
@@ -46,4 +41,20 @@ public class Utils {
     public String[] splitByDelimiter(String carNames) {
         return carNames.split(",");
     }
+
+    public void convertStringToInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER);
+        }
+    }
+
+    public void isPositiveNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER);
+        }
+    }
+
+
 }
