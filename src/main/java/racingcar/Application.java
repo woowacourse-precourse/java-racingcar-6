9c213f0024215fd517ddda30bcaed55741ac3224
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 00:56:30 +0900

    feat(Application): 테스트 코드 삭제 및 playGame 메서드 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        OutputView outputView = new OutputView();
-        outputView.requestCarNames();
+        GameController gameController = new GameController();
+        gameController.playGame();
     }
 }
