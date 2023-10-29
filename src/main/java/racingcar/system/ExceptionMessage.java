ef615e29924588a1ae2a6c3ea6bc2d34a01cbd82
Hoo-sung <gntjd135@gmail.com>
2023-10-29 14:52:57 +0900

    feat(AttemptsNumVerifier) : 시도 횟수가 양의 정수인지 체크하는 메서드 생성

--- src/main/java/racingcar/system/ExceptionMessage.java
+++ src/main/java/racingcar/system/ExceptionMessage.java
@@ -3,4 +3,5 @@ package racingcar.system;
 public final class ExceptionMessage {
     public static final String NOT_NUMERIC = "입력된 값은 숫자가 아닙니다";
     public static final String INVALID_CAR_NAME = "자동차 이름이 5자 이하여야 합니다.";
+    public static final String INVALID_NUM_OF_ATTEMPTS = "시도할 횟수는 양의 정수이어야 합니다.";
 }
--- src/main/java/racingcar/verifier/AttemptsNumVerifier.java
+++ src/main/java/racingcar/verifier/AttemptsNumVerifier.java
@@ -7,6 +7,7 @@ public class AttemptsNumVerifier implements Verifier{
     @Override
     public void check(String input) throws IllegalArgumentException {
         checkNumeric(input);
+        checkRange(input);
     }
     private void checkNumeric(String input) {
         try {
@@ -14,5 +15,11 @@ public class AttemptsNumVerifier implements Verifier{
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_NUMERIC);
         }
+    }
+
+    private void checkRange(String input){
+        int attempts = Integer.parseInt(input);
+        if(attempts <=0)
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NUM_OF_ATTEMPTS);
     }
 }
