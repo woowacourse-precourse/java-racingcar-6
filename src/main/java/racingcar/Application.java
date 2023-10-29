3a0871e7a2f0065b459cbabbf27aa0f655fdfa2b
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-10-29 19:57:28 +0900

    feat(racingcar - Race ): 각 경주 결과(1회) 구현 & readme 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,23 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        int trialNumber;
+        List<String> carNamesArrayList = new ArrayList<>();
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        UserInput.getCarNames();
+        carNamesArrayList = UserInput.getCarNames();
 
         System.out.println("시도할 회수는 몇회인가요?");
-        UserInput.getTrialNumber();
+        trialNumber = UserInput.getTrialNumber();
+
+        System.out.println("실행 결과");
+
 
 
 
