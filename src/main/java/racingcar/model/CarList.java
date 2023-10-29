cdf1ebaf0a2241c576a8b163594a71d8d79240e2
myeongju00 <j51677767@gmail.com>
2023-10-29 19:01:43 +0900

    [feat] : 입력받은 자동차 이름으로 자동차 생성

--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -1,16 +1,19 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarList {
     List<Car> carList = new ArrayList<>();
 
     public CarList(String carNames) {
-        String[] carNameList = carNames.split(",");
-        for (String carName : carNameList) {
-            carList.add(new Car(carName));
-        }
+        List<String> carNameList = Arrays.stream(carNames.split(",")).toList();
+        carNameList.forEach(carName -> {
+            Car car = new Car(carName);
+            isDuplicate(car);
+            carList.add(car);
+        });
     }
 
     private void isDuplicate(Car car) {
