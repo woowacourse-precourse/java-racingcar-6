18ded5ec0e04c94211dfc48fdb98939b4284768f
KiminSon <kimin0331@naver.com>
2023-10-29 15:06:09 +0900

    feat(Application): 자동차 입력 구현 완료
    자동차의 이름들을 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCar = readLine();
     }
 }
