ca520bbff490be9761323864bd72e5402826b577
jeongjihun <wlgns2234@naver.com>
2023-10-29 17:45:01 +0900

    test: 예외를 발생시키지 않고 성공하는 테스트 코드 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -18,7 +19,7 @@ class CarTest {
     private static final int MOVE_THRESHOLD = 4;
 
 
-    @DisplayName("move() 테스트 : 랜덤으로 생성된 값이 기준값보다 크거나 같으면 전진한다.")
+    @DisplayName("moveUsingRandomNumber() 테스트 : 랜덤으로 생성된 값이 기준값보다 크거나 같으면 전진한다.")
     @ParameterizedTest(name = "생성된 랜덤값이 {0}일 때 0에서 1로 전진한다.")
     @ValueSource(ints = {4, 5, 9})
     void moveUsingRandomNumber_램덤값이_4보다_클때(int random) {
@@ -32,7 +33,7 @@ class CarTest {
                 .isEqualTo(new Car(ANONYMOUS_NAME, 1));
     }
 
-    @DisplayName("move() 테스트 : 랜덤으로 생성된 값이 기준값보다 작으면 가만히 있는다.")
+    @DisplayName("moveUsingRandomNumber() 테스트 : 랜덤으로 생성된 값이 기준값보다 작으면 가만히 있는다.")
     @ParameterizedTest(name = "생성된 랜덤값이 {0}일 때 0에서 전진하지 않는다.")
     @ValueSource(ints = {0, 1, 3})
     void moveUsingRandomNumber_랜덤값이_4보다_작을때(int random) {
@@ -59,7 +60,7 @@ class CarTest {
     }
 
     @DisplayName("isFrontOf() 테스트 :Car 객체가 다른 Car 보다 더 앞에 있으면 true를 반환한다.")
-    @ParameterizedTest(name = "내 차가(5) 다른차 {0}보다 앞에 있으므로 true를 반환한다.")
+    @ParameterizedTest(name = "내 차가(5) 다른차({0})보다 앞에 있으므로 true를 반환한다.")
     @ValueSource(ints = {0, 3, 4})
     void isFrontOf_다른차보다_앞에있을때_true(int otherPosition) {
         Car car = new Car("홍길동", 5);
@@ -70,7 +71,7 @@ class CarTest {
     }
 
     @DisplayName("isFrontOf() 테스트 :Car 객체가 다른 Car 보다 같거나 뒤에있으면 false를 반환한다.")
-    @ParameterizedTest(name = "내 차가(5) 다른차 {0}와 같거나 뒤에 있으므로 false를 반환한다.")
+    @ParameterizedTest(name = "내 차가(5) 다른차({0})와 같거나 뒤에 있으므로 false를 반환한다.")
     @ValueSource(ints = {5, 6, 7})
     void isFrontOf_다른차보다_뒤에있을때_false(int otherPosition) {
         Car car = new Car("홍길동", 5);
@@ -126,7 +127,7 @@ class CarTest {
                 .isEqualTo(new Car("홍길동", 0));
     }
 
-    @DisplayName("Car(name, position) 생성 테스트 : Car 생성 시 이름과 초기위치값을 부여할 수 있다.")
+    @DisplayName("Car(name, position) 생성 테스트 : Car 생성 시 이름과 초기 위치값을 부여할 수 있다.")
     @Test
     void create_생성자에_이름과_위치값이_주어짐() {
         Car car = new Car("홍길동", 5);
@@ -136,19 +137,43 @@ class CarTest {
                 .isEqualTo(new Car("홍길동", 5));
     }
 
-    @DisplayName("name 사이즈 예외 테스트 : 5이하가 아니라면 IllegalArgumentException가 발생한다.")
+    @DisplayName("name 사이즈 예외 테스트 : 5초과라면 IllegalArgumentException가 발생한다.")
     @Test
     void 예외발생_이름크기가_5자_초과인경우() {
-        assertThatThrownBy(() -> new Car("5글자를초과하는이름"))
+        assertThatThrownBy(() -> new Car("여섯글자여섯"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 이름 크기는 5이하여야 합니다.");
     }
 
-    @DisplayName("position 값 0이상 테스트 : 0이상이 아니라면 IllegalArgumentException가 발생한다.")
+    @DisplayName("name 사이즈 예외 테스트 : 5이하라면 성공적으로 Car객체가 생성된다.")
+    @Test
+    void 예외발생하지않음_이름크기가_5자_이하인경우() {
+        assertAll(
+                () -> assertThatCode(() -> new Car("다섯글자다"))
+                        .doesNotThrowAnyException(),
+                () -> assertThatCode(() -> new Car("세글자"))
+                        .doesNotThrowAnyException(),
+                () -> assertThatCode(() -> new Car("한"))
+                        .doesNotThrowAnyException()
+        );
+    }
+
+    @DisplayName("position 값 0이상 테스트 : 0보다 작다면 IllegalArgumentException가 발생한다.")
     @Test
     void 예외발생_위치값이_0보다_작은_경우() {
         assertThatThrownBy(() -> new Car("홍길동", -1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 초기위치값은 0보다 작을 수 없습니다.");
+    }
+
+    @DisplayName("position 값 0이상 테스트 : 0이상이라면 성공적으로 Car객체가 생성된다.")
+    @Test
+    void 예외발생하지않음_위치값이_0_이상인_경우() {
+        assertAll(
+            () -> assertThatCode(() -> new Car("세글자", 0))
+                    .doesNotThrowAnyException(),
+            () -> assertThatCode(() -> new Car("세글자", 1))
+                    .doesNotThrowAnyException()
+        );
     }
 }
