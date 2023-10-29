13302b2f897a0dd7ed4e47e63aa0d09d8a8938fa
jae woo Jung <awfjol2008@naver.com>
2023-10-30 02:08:58 +0900

    refector: 자동차 이름 목록으로 입력된 문자열을 검증하는 메소드명 변경
    문자열을 검증하는 메소드를 리스트를 검증하는 메소드와 구분하기 위해 메소드명을 명시적으로 변경

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -3,19 +3,20 @@ package racingcar.validator;
 public class CarNameValidator {
 
     public static void validateForString(String carName) {
-        validateEmpty(carName);
-        validateContainsComma(carName);
+        validateEmptyForString(carName);
+        validateContainsCommaForString(carName);
     }
 
-    private static void validateEmpty(String carName) {
+    private static void validateEmptyForString(String carName) {
         if (carName.isEmpty()) {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
         }
     }
 
-    private static void validateContainsComma(String carName) {
+    private static void validateContainsCommaForString(String carName) {
         if (!carName.contains(",")) {
             throw new IllegalArgumentException("입력값은 쉼표(,)를 기준으로 구분되어야 합니다.");
         }
     }
+
 }
