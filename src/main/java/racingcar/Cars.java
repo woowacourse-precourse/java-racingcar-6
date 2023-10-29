d0bb4430147d31ce41400aac0f790a48bbb435d4
freekyr7529 <freekyr7529@gmail.com>
2023-10-29 13:20:48 +0900

    refactor: 메서드 분리를 통한 인덴트 깊이 감소
    기존 메서드를 분리하여 메서드 마다 한 가지의 역할을 담당하도록 하였다. 이를 통해 인덴트 깊이를 감소시킬 수 있었다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,6 +8,7 @@ import java.util.stream.Collectors;
 public class Cars {
 
     private final List<Car> cars;
+    private final View view = new View();
 
     public Cars(String names) {
         String[] splitNames = names.split(",");
@@ -30,15 +32,22 @@ public class Cars {
 
     public void roundStatus(int round) {
         for (int i = 0; i < round; i++) {
-            for (Car car : cars) {
-                int randNum = Randoms.pickNumberInRange(0, 9);
-                if (randNum >= 4) {
-                    car.updatePosition();
-                }
+            printRoundResult();
+        }
+    }
 
-                view.carResultMsg(car.getName(), car.process());
-            }
-            view.printEmptyLine();
+    private void printRoundResult() {
+        for (Car car : cars) {
+            updatePositionComparedToRandNum(car);
+            view.carResultMsg(car.getName(), car.process());
+        }
+        view.printEmptyLine();
+    }
+
+    private void updatePositionComparedToRandNum(Car car) {
+        int randNum = Randoms.pickNumberInRange(0, 9);
+        if (randNum >= 4) {
+            car.updatePosition();
         }
     }
 }
