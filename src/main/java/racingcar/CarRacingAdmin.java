b2c5c13f27cd1a5f801e7ced51cd09af0b48e3b3
CodingApe9 <tlqkf6974@>
2023-10-29 15:31:52 +0900

    feat(CarRacingAdmin) : 경주를 준비, 시작하고 우승한 자동차를 출력하는 기능 추가

--- src/main/java/racingcar/CarRacingAdmin.java
+++ src/main/java/racingcar/CarRacingAdmin.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarRacingAdmin {
+    public void startRace() {
+        String carNames = RacingCarConsole.readCarNames();
+        List<Car> cars = getReadyCars(carNames);
+
+        int iterationNumBer = RacingCarConsole.readIterationNumBer();
+        InputValidator.validateIterationNumBer(iterationNumBer);
+
+        CarRacing carRacing = new CarRacing(cars, iterationNumBer);
+        List<CarRacingResult> championCars = carRacing.start();
+        RacingCarConsole.printChampionCars(championCars);
+    }
+
     private List<Car> getReadyCars(String carNames) {
         InputValidator.validateCarNames(carNames);
         String[] carNameArray = carNames.split(",");
