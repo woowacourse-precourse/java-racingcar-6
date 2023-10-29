1e1b5120028e75a7a78b140fc9495dac6ad88b5b
sonjeongdong <sjd0219@gmail.com>
2023-10-30 01:35:23 +0900

    Feat: 자동차 이름 입력 요구문을 출력하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,10 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        printInputCarName();
+    }
+
+    public static void printInputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
