e1275dc2f3b6fad00f7b4c1aabd93161017da276
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-29 16:54:10 +0900

    refactor(racingcar): use camp.nextstep.edu.missionutils.Randoms

--- src/main/java/racingcar/domain/CreateRandomNumber.java
+++ src/main/java/racingcar/domain/CreateRandomNumber.java
@@ -1,11 +1,11 @@
 package racingcar.domain;
 
-import java.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class CreateRandomNumber {
     public int generateRandomNumber(){
-        int number = new Random().nextInt(9) + 1;
-        return number;
+        int randomNumber  = Randoms.pickNumberInRange(0, 9);
+        return randomNumber;
     }
 }
 
