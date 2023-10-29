65c86830882445a8170ccbd904bfd00e778ff20c
DaehoSon <thseogh135@gmail.com>
2023-10-29 13:54:21 +0900

    feat: 요구사항에서 요구한 차 이름의 자리수를 초과할 경우 Exception 처리

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+
+    public final String ANNOUNCEMENT_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final int MAX_CAR_NAME_LENGTH = 5;
+    private final String INPUT_WRONG_VALUE = "잘못된 입력값입니다.";
+
+    public Game() {
+        init();
+    }
+
+    private void init() {
+        System.out.println(this.ANNOUNCEMENT_INPUT_CAR_NAMES);
+        String[] carNameArray = Console.readLine().split(",");
+        for (String s : carNameArray) {
+            if (s.length() > this.MAX_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException(this.INPUT_WRONG_VALUE);
+            }
+        }
+    }
+
+}
