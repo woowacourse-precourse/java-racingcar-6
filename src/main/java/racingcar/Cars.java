5a39be69ca40a8e3a95dc4fb8231562851cbcfa6
yush <yush2202@gmail.com>
2023-10-30 03:37:02 +0900

    test : CarTest 수정 CarTest에 포함된 거리를 가져오는 askDistance와 이름을 가져오는 getName을 테스트하는 코드를 수정했습니다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -23,4 +23,5 @@ public class Cars {
     public int askDistance() {
         return this.distance;
     }
+
 }
--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -1,14 +1,19 @@
 package study;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.Cars;
+
 public class CarTest {
 
-//    @Test
-//    void n회_전진했을경우_거리측정() {
-//        Cars james = new Cars("james");
-//        for (int i = 0; i < 5; i++) {
-//            james.goForward();
-//        }
-//        assertThat(james.askDistance()).isEqualTo(5);
-//    }
-
+    @Test
+    void n회_전진했을경우_객체테스트() {
+        Cars james = new Cars("james");
+        for (int i = 0; i < 5; i++) {
+            james.goOneStep();
+        }
+        assertThat(james.askDistance()).isEqualTo(5);
+        assertThat(james.getName()).isEqualTo("james");
+    }
 }
