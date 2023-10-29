ee80c4dc12c5cae6d4de6b0398425949be7477dc
yunjeong <stac_cato@naver.com>
2023-10-29 14:36:34 +0900

    feat: 잘못된 사용자 input 예외 발생 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,19 +11,44 @@ public class Application {
     }
 
     public static List<String> askCarName(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String input = readLine();
+        checkCarNameAsCommas(input);
 
         List<String> names = new ArrayList<>();
         for (String name : input.split(",")) {
+            checkCarNameAsLength(name);
             names.add(String.valueOf(name));
         }
         return names;
     }
 
     public static int askRound(){
+        System.out.print("시도할 회수는 몇회인가요?\n");
         String input = readLine();
+        checkRound(input);
+
         int round = Integer.parseInt(input);
         return round;
     }
+
+    public static void checkCarNameAsCommas(String input){
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("자동차 이름은 2개 이상, 쉼표로 구분되어야 합니다.");
+        }
+    }
+
+    public static void checkCarNameAsLength(String input){
+        if (input.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static void checkRound(String input) {
+        if (input.isEmpty() || !input.matches("\\d+")) {
+            throw new IllegalArgumentException("숫자를 입력하십시오.");
+        }
+    }
 }
+
+
