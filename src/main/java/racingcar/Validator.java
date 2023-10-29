4ce0e9ea19b89f7fc70c08203b69ba0531bf0c11
SUJI-J <suzy852@naver.com>
2023-10-29 19:02:02 +0900

    feat: 자동차 이름 공백 여부 기능 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,15 +1,15 @@
 package racingcar;
 
 import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
 
 import static racingcar.GameMessage.INPUT_DUPLICATE_EXCEPTION_MESSAGE;
 import static racingcar.GameMessage.INPUT_LENGTH_EXCEPTION_MESSAGE;
+import static racingcar.GameMessage.INPUT_BLANK_EXCEPTION_MESSAGE;
 
 public class Validator {
 
     public static Map<String, String> inputCarNames(Map<String, String> racingCars) {
+        isEmptyName(racingCars);
         isValidLength(racingCars);
         return racingCars;
     }
@@ -22,7 +22,15 @@ public class Validator {
         }
     }
 
-    public static void  isDuplicateNames() {
+    private static void isEmptyName(Map<String, String> racingCars) {
+        for (String carName : racingCars.keySet()) {
+            if (carName.isBlank()) {
+                throw new IllegalArgumentException(INPUT_BLANK_EXCEPTION_MESSAGE);
+            }
+        }
+    }
+
+    public static void DuplicateNames() {
         throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MESSAGE);
     }
 }
