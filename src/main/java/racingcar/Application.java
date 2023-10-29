a22af54de924a025c7a1e33eb925f93df3d7e774
WooJJam <jaemin5548@naver.com>
2023-10-29 14:29:51 +0900

    [Feat] 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        GameController gameController = new GameController();
+        gameController.inputComputerName();
+
     }
 }
