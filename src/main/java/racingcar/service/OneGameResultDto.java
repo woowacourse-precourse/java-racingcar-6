b37f5019b14127569ff22cc1326c6ae5622fdf1b
june-777 <wlwhswnsrl96@gmail.com>
2023-10-29 20:26:26 +0900

    feat(OneGameResultDto): 자동차들을 움직임 횟수 리스트로 매핑하는 기능

--- src/main/java/racingcar/service/OneGameResultDto.java
+++ src/main/java/racingcar/service/OneGameResultDto.java
@@ -6,9 +6,11 @@ import racingcar.domain.Car;
 
 public class OneGameResultDto {
     private final List<String> names;
+    private final List<Long> totalMoveCounts;
 
     public OneGameResultDto(final List<Car> cars) {
         this.names = mappingToNames(cars);
+        this.totalMoveCounts = mappingToMoveCounts(cars);
     }
 
     private List<String> mappingToNames(List<Car> endCars) {
@@ -17,5 +19,13 @@ public class OneGameResultDto {
             names.add(endCar.getName());
         }
         return names;
+    }
+
+    private List<Long> mappingToMoveCounts(List<Car> endCars) {
+        List<Long> moveCounts = new ArrayList<>();
+        for (Car endCar : endCars) {
+            moveCounts.add(endCar.getTotalMoveCount());
+        }
+        return moveCounts;
     }
 }
