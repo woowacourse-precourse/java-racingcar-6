02fbff938d7723df21b7b9adb75ce6e3cf9e109f
mmalfoy <stpmalfoy@gmail.com>
2023-10-29 22:41:45 +0900

    Feat : 입력Class 시도할 회수 저장 멤버 추가

--- src/main/java/racingcar/domain/Inputs.java
+++ src/main/java/racingcar/domain/Inputs.java
@@ -6,13 +6,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Inputs {
     private static List<String> cars = new ArrayList<>();
-
+    private static int count;
 
     public static List<String> getCars(){
         return cars;
     }
 
-
+    public static int getCount(){
+        return count;
+    }
 
 
 
