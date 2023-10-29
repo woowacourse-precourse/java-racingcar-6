8cef40a1336bb231a7fe0a23f0f2bfb3b840f743
Dawin <dawin002@naver.com>
2023-10-29 22:17:01 +0900

    feat(OutputView): 결과 출력 기능 수정
    결과를 출력하는 printRoundResult() 메서드 내 결과 문자열을 생성하는 코드를 buildResultString() 메서드로 분리.
    결과 출력 시작 메시지를 출력하는 메서드 이름 변경:
    printResultStart() -> printResultStartMessage()
    우승자를 출력하는 메서드 이름 변경:
    printWinners() -> printGameWinners()

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,26 @@
 package racingcar.view;
 
+import racingcar.model.Race;
+
 import java.util.List;
 
 public class OutputView {
-
-    public static void printResultStart() {
+    public static void printResultStartMessage() {
         System.out.println("\n실행 결과");
     }
-    public static void printRoundResult(List<String> carNames, List<Integer> roundResult) {
+
+    public static void printRoundResult(Race race) {
+        List<String> carNames = race.getCarNames();
+        List<Integer> roundResult = race.getRoundResult();
+
+        String result = buildResultString(carNames, roundResult);
+
+        System.out.println(result);
+    }
+
+    private static String buildResultString(List<String> carNames, List<Integer> roundResult) {
         StringBuilder stringBuilder = new StringBuilder();
+
         for (int index = 0; index < carNames.size(); index++) {
             int distance = roundResult.get(index);
             stringBuilder.append(carNames.get(index));
@@ -16,13 +28,14 @@ public class OutputView {
             stringBuilder.append("-".repeat(distance));
             stringBuilder.append("\n");
         }
-        String result = stringBuilder.toString();
-        System.out.println(result);
+
+        return stringBuilder.toString();
     }
 
-    public static void printWinners(List<String> winners) {
+    public static void printGameWinners(Race race) {
+        List<String> winners = race.getWinners();
         String result = String.join(", ", winners);
-        System.out.println(result);
+        System.out.println("최종 우승자 : " + result);
     }
 
 }
