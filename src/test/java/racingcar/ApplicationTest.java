c1107b20bec7921a63a03940c2cf8a62862657de
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 20:12:06 +0900

    test) validateCarNames 메소드를 사용시 자동차 이름이 5자가 넘어가는 경우 예외 발생하는지 확인하는 테스트 작성
    validateCarNames 메소드를 사용시 자동차 이름이 5자가 넘어가는 경우 IllegalArgumentException이 일어나고 ERROR_MESSAGE_BIGGER_THAN_5CHAR가 예외문에 포함되는지 확인해주는 테스트를 작성했습니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.RacingCar;
+import racingcar.utils.RacingCarGameInput;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,6 +14,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void validateCarNames_메소드를_사용시_자동차_이름이_5자가_넘어가는_경우_예외_발생() {
+        List<RacingCar> carList = List.of(
+            new RacingCar("exception")
+        );
+        assertThatThrownBy(() -> RacingCarGameInput.validateCarNames(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_BIGGER_THAN_5CHAR);
+    }
 
     @Test
     void 전진_정지() {
