b35621e6a84c7affd0be9d6a0ef047c298960d5d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 17:34:50 +0900

    fix(viewer): inputCarNames() 제거 및 input()메서드 private으로 접근 제한

--- src/main/java/racingcar/Viewer.java
+++ src/main/java/racingcar/Viewer.java
@@ -1,24 +1,31 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Viewer {
     private final String CAR_NAME_DELIMITER = ",";
     private final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이하만 가능합니다.";
-    private final String INVALID_CAR_NAME_DELIMITER = "쉼표(,)로 구분해주세요.";
+    private final String INVALID_INPUT_TYPE = "입력은 자동차 이름과 구분자(,)만 가능합니다.";
     private final Integer MIN_TRY_COUNT = 1;
     private final String TRY_COUNT_ONLY_NUMBER = "시도 횟수는 숫자여야 합니다.";
     private final String MINIMUM_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
 
-    public List<String> inputCarNames() {
-        List<String> carNameList = Arrays.asList(readLine().split(CAR_NAME_DELIMITER));
-        carNameList.forEach(this::validateCarName);
+    private String input() {
+        String input = readLine();
+        validateInputType(input);
+        return input;
+    }
+
+    public List<String> readCarNames() {
+        List<String> carNameList = Arrays.asList(input().split(CAR_NAME_DELIMITER));
+        carNameList.forEach(this::validateCarNameLength);
         return carNameList;
     }
+
     public int inputTryCount() {
         try {
             int tryCount = Integer.parseInt(readLine());
@@ -29,21 +36,18 @@ public class Viewer {
         }
     }
 
-    private void validateCarName(String carName) {
-        validateCarNameLength(carName);
-        validateCarNameType(carName);
-    }
-
     private void validateCarNameLength(String carName) {
         if (carName.length() > 5) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
         }
     }
-    private void validateCarNameType(String carName) {
-        if (Pattern.matches("^[a-zA-Z]*$", carName)) {
-            throw new IllegalArgumentException(INVALID_CAR_NAME_DELIMITER);
+
+    private void validateInputType(String carName) {
+        if (!Pattern.matches("^[a-zA-Z,]*$", carName)) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE);
         }
     }
+
     private void validateTryCount(int tryCount) {
         if (tryCount < MIN_TRY_COUNT) {
             throw new IllegalArgumentException(MINIMUM_TRY_COUNT);
