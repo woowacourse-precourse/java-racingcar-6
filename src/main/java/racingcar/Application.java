803197aafa8716b1f3ffafaa84933f5bb35cb367
juone00 <juwon0208@naver.com>
2023-10-29 19:07:26 +0900

    feat(racingcar): Input 자동차 이름, 시도할 횟수 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import racingcar.domain.Input;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -8,6 +12,13 @@ public class Application {
 
     private static void playRacingCar() {
 
+        Input input = new Input();
 
+        List<String> names = input.createNames();
+        for (String name : names) {
+            System.out.println("name = " + name);
+        }
+        int moveCount = input.moveCount();
+        System.out.println("moveCount = " + moveCount);
     }
 }
--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -1,4 +1,26 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Input {
+
+    /**
+     * 경주 할 자동차 한줄로 입력 받은 후,
+     * 쉽표를 기준으로 나눠서 List에 저장 후 return
+     */
+    public List<String> createNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        return List.of(names.split(","));
+    }
+
+    /**
+     * 시도 할 횟수 입력 받은 후 return
+     */
+    public int moveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
