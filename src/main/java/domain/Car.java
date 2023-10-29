e0faaee0039ddfe8c37d315094b7b4a19bc15db1
sangeun-b <baek0007@algonquinlive.com>
2023-10-30 00:12:08 +0900

    feat: car 도메인 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,6 +5,21 @@ import java.util.List;
 
 public class Car {
 
+    private String name;
+    private int position;
+
+    public Car(String carName){
+        this.name = carName;
+        this.position = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getPosition(){
+        return position;
+    }
+
     public List<String> convertCarName(String carName) {
         List<String> carNames = Arrays.asList(carName.split(","));
         return carNames;
