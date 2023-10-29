029f2de4d668bcf60ecec6ae501bf42f8a676f9e
Dain Lee <dlekdls529@gmail.com>
2023-10-29 15:41:09 +0900

    우승자 구하기 구현

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -6,9 +6,11 @@ import java.util.Collections;
 
 
 public class Race {
+    private List<String> carNameList;
     private List<Integer> carScoreList;
 
-    public void racingCar(int count, List<String> carNameList) {
+    public void racingCar(int count, List<String> carNameArr) {
+        carNameList = carNameArr;
         carScoreList = new ArrayList<>(
                 Collections.nCopies(carNameList.size(),0)
         );
@@ -16,15 +18,30 @@ public class Race {
         for (int i = 0; i < count; i++) {
             Forward forward = new Forward();
             carScoreList = forward.moveForward(carScoreList);
-            print(carNameList);
+            print();
         }
+        findingWinner();
     }
 
-    private void print(List<String> carNameList){
+    private void print(){
         for (int i=0;i<carNameList.size();i++){
             System.out.println(carNameList.get(i) + " : " + "-".repeat(carScoreList.get(i)));
         }
         System.out.println();
+    }
+
+    private void findingWinner(){
+        int max = Collections.max(carScoreList);
+
+        List<String> winner = new ArrayList<>();
+        for(int i=0;i<carScoreList.size();i++){
+            if(carScoreList.get(i) == max){
+                winner.add(carNameList.get(i));
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winner));
     }
 
 }
