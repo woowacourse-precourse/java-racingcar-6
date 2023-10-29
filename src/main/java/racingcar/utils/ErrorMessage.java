81ce0d60269774568734e60300e9026f355bbf38
lcqff <yeonnu82g@gmail.com>
2023-10-29 21:33:30 +0900

    refactor: enum 필드 final 추가

--- src/main/java/racingcar/utils/ErrorMessage.java
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     INVALID_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
     NON_DIGIT_ATTEMP_NUM("시도 횟수는 숫자여야 합니다.");
 
-    private String message;
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
