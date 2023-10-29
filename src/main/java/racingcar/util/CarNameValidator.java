d91270e98f86fc2af58913caa70fb1e9090bcf0c
Somin Park <ps4708@naver.com>
2023-10-29 19:57:42 +0900

    feat: 입력한 자동차 이름을 검증하는 기능 추가

---
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -0,0 +1,26 @@
+package racingcar.util;
+
+public class CarNameValidator {
+
+    private final static String CAR_NAME_LENGTH_ERROR = "자동차의 이름은 5글자 이하로 입력해야 합니다.";
+    private final static int MAX_CAR_NAME_LENGTH = 5;
+
+    public static String[] validateCarNames(String input){
+        String[] carNames = splitCarNames(input);
+        isLengthValid(carNames);
+        return carNames;
+    }
+
+    private static String[] splitCarNames(String input) {
+        return input.split(",");
+    }
+
+    private static void isLengthValid(String[] carNames) {
+
+        for (String carName : carNames) {
+            if (carName.length() >= MAX_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR);
+            }
+        }
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,19 +2,20 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.util.AttemptCountValidator;
+import racingcar.util.CarNameValidator;
 
 public class InputView {
-
-    public static String readCarNames(){
-        OutputView.printCarNamePrompt();
-        String input = Console.readLine();
-        return input;
-    }
 
     public static String readAttemptCount(){
         OutputView.printAttemptCountPrompt();
         String input = Console.readLine();
-        AttemptCountValidator.validateAttemptCount(input);
         return input;
     }
+
+    public static String[] readCarNames(){
+        OutputView.printCarNamePrompt();
+        String input = Console.readLine();
+        return CarNameValidator.validateCarNames(input);
+    }
+
 }
