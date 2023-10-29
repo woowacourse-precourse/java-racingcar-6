0947131a143eb88bc02b61bfcf3dd7fcf698687b
Jae-Young98 <ygy0102@gmail.com>
2023-10-29 16:41:58 +0900

    feat(InputView): 자동차 이름, 횟수 입력을 받는 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public String inputNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputAttemptNumber() {
+        System.out.println(INPUT_ATTEMPT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+}
