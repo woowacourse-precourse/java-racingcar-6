996b4c7733cfcb8de8656e2fb112f15e41de704f
Yu JeongMin <polar522@naver.com>
2023-10-29 15:25:27 +0900

    기능 2-1. 자동차의 이름을 저장하여 각 라운드마다 자동차의 이름과 진행 상황을 프린트하는 함수인 playRound() 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,10 +10,14 @@ public class Application {
         // TODO: 프로그램 구현
             // 기능 1-1. 자동차의 이름을 입력 받아 몇 대(n)의 자동차로 경기를 할 것인지 입력 받기
             String Input = getInput();
-            // 입력 받은 자동차 이름들을 배열에 저장
+            // 기능 2-1. 자동차의 이름을 저장하여 각 라운드마다 자동차의 이름과 진행 상황을 프린트하는 함수인 playRound() 메서드 구현
             String[] cars = Input.split(",");
+            // 자동차의 수만큼 0을 초기값으로 갖는 배열 생성
+            int[] location = new int[cars.length];
             // 기능 1-2. 주어진 횟수를 입력받기(몇 번의 라운드를 진행할 지 입력 받기)
             int rounds = getNumberOfRounds();
+            System.out.println("\n실행 결과");
+
     }
 
     // 기능 1-1. 자동차의 이름을 입력 받아 몇 대(n)의 자동차로 경기를 할 것인지 입력 받기
@@ -34,10 +38,20 @@ public class Application {
             return rounds;
     }
 
-    public static void playRound(String[] cars){
+    // 기능 2-1. 자동차의 이름을 저장하여 각 라운드마다 자동차의 이름과 진행 상황을 프린트하는 함수인 playRound() 메서드 구현
+    public static void playRound(String[] cars, int rounds, int[] location){
+
+            int numOfCars = cars.length;
+            int[][] positions = new int[numOfCars][rounds];
+            int index = 0;
+
             for (String car : cars) {
-                int randomNumber = new Randoms.pickNumberInRange(0, 9);
-                System.out.
-        }
+                    System.out.print(car + ": ");
+                    for(int i = 0; i < location[index]; i++){
+                        System.out.print("-");
+                    }
+                    System.out.println();
+            }
     }
+
 }
