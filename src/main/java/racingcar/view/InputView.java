b140764f662d823f70e48c3abddbb9b43898eb4a
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-29 22:00:42 +0900

    refactor ($InputView) : startGame함수 안 List 변환 함수 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,9 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputView {
-    public List<String> startGame(){
+    public String startGame(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarName = Console.readLine();
+        return Console.readLine();
+    }
+    public List<String> inputCarNameToList(String inputCarName){
         return List.of(inputCarName.split(","));
     }
     public Integer inputTryCount(){
