d4e24fa4491086161fb7bfdf8d5cd59d34e20fbf
sangdol <priceelees@gmail.com>
2023-10-29 13:18:16 +0900

    refactor(CarListMaker): 예외 메시지를 enum으로 분리

--- src/main/java/racingcar/util/CarListMaker.java
+++ src/main/java/racingcar/util/CarListMaker.java
@@ -1,5 +1,7 @@
 package racingcar.util;
 
+import static racingcar.util.CarListMaker.ErrorMessage.*;
+
 import java.util.Arrays;
 import java.util.List;
 import racingcar.Car;
@@ -16,13 +18,13 @@ public final class CarListMaker {
 
     private static void validateNames(String names) {
         if (names.isEmpty()) {
-            throw new IllegalArgumentException("입력값이 존재하지 않습니다.");
+            throw new IllegalArgumentException(NOT_EXIST_EXCEPTION.getMessage());
         }
         if (hasInvalidLength(names)) {
-            throw new IllegalArgumentException("이름은 1글자부터 5글자까지 가능합니다.");
+            throw new IllegalArgumentException(INVALID_LENGTH_EXCEPTION.getMessage());
         }
         if (isInvalidFormat(names)) {
-            throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException(INVALID_FORMAT_EXCEPTION.getMessage());
         }
     }
 
@@ -32,6 +34,22 @@ public final class CarListMaker {
     }
 
     private static boolean isInvalidFormat(String names) {
+        // 입력의 형태가 조근의 형식대로 이루어졌는지 확인
         return !names.matches("([\\w가-힣]{1,5},?)+");
+    }
+
+    enum ErrorMessage {
+        NOT_EXIST_EXCEPTION("입력값이 존재하지 않습니다."),
+        INVALID_LENGTH_EXCEPTION("이름은 1글자부터 5글자까지 가능합니다."),
+        INVALID_FORMAT_EXCEPTION("입력 형식이 올바르지 않습니다.");
+
+        private final String message;
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        private String getMessage() {
+            return message;
+        }
     }
 }
