9c18bafd0f5e33cecacc1637b510c43432360092
nylonmusk <2912rla@gmail.com>
2023-10-29 14:52:37 +0900

    refactor : 매직넘버 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,14 @@
 package racingcar.domain;
 
+import racingcar.constant.CarPosition;
+
 public class Car {
     private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
-        this.position = 0;
+        this.position = CarPosition.INITIAL_POSITION.getValue();
     }
 
     public void move() {
