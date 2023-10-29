8701d84f50a377b02fcd5b327458821818bc11c5
혜민 <alddlv81@gmail.com>
2023-10-30 00:07:02 +0900

    feat: 경주 횟수 반환

--- src/main/java/racingcar/game/UserInputGameRound.java
+++ src/main/java/racingcar/game/UserInputGameRound.java
@@ -3,6 +3,11 @@ package racingcar.game;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInputGameRound {
+    public static int getGameRound() {
+        String inputRound = inputGameRound();
+        return processInput(inputRound);
+    }
+
     private static String inputGameRound() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
