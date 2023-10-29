30f0c8f1c7255df53efe72e5a0c7af73e1636bc3
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-29 12:08:17 +0900

    test: 자동차 등록, 경주횟수 등록에 대한 테스트 추가
    - 입력받은 문자열로 자동차가 등록 되는지 여부 테스트
    - 입력받은 문자열로 경주 횟수가 등록 되는지 여부 테스트

---
+++ src/test/java/racingcar/service/RacingGameTest.java
@@ -0,0 +1,39 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingGameTest {
+
+    private RacingGame racingGame = new RacingGame();
+
+    @Test
+    void 자동차등록_입력받은문자열 () {
+        String playerInput = "자동차1,자동차2,자동차3";
+        racingGame.setCarList(playerInput);
+
+        assertAll(
+                () -> assertEquals(racingGame.getCarList().get(0).getName(), "자동차1"),
+                () -> assertEquals(racingGame.getCarList().get(1).getName(), "자동차2"),
+                () -> assertEquals(racingGame.getCarList().get(2).getName(), "자동차3")
+        );
+    }
+
+    @Test
+    void 경주횟수등록_입력받은문자열 () {
+        String playerInput = "100";
+        racingGame.setRaceCount(playerInput);
+
+        assertThat(Integer.parseInt(playerInput)).isEqualTo(racingGame.getRaceCount());
+    }
+}
