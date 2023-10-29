db0aca58e64101e02e23f11abd53cdd1c560cde8
Hayoon <hy97@sju.ac.kr>
2023-10-29 16:27:44 +0900

    feat(ExceptionConstant): 예외처리 메세지를 ExceptionConstant로 분리하여 상수 처리한다

---
+++ src/main/java/racingcar/util/constant/ExceptionConstant.java
@@ -0,0 +1,8 @@
+package racingcar.util.constant;
+
+public class ExceptionConstant {
+    public static String EMPTY_NAME = "공란입니다. 이름을 입력해주세요.";
+    public static String INPUT_VALID_NUMBER = "숫자를 입력해주세요.";
+    public static String INPUT_VALID_NUMBER_RANGE = "1 이상의 숫자를 입력해주세요.";
+    public static String INPUT_VALID_NAME_LENGTH = "이름은 5자 이하로 입력해주세요.";
+}
