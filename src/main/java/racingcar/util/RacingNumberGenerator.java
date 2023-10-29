6f37fed236f9b0a26bb277e796aaac9d9b1e606a
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-29 12:32:05 +0900

    test(RacingNumberGenerator) : 매직넘버 상수화

--- src/main/java/racingcar/util/RacingNumberGenerator.java
+++ src/main/java/racingcar/util/RacingNumberGenerator.java
@@ -3,9 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingNumberGenerator implements RandomNumberGenerator{
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE= 9;
 
     @Override
     public int generate() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
     }
 }
