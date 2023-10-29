848dc03f4812cd036f921e581488a1ad63ef840b
unknown <khd128k@gmail.com>
2023-10-29 21:42:52 +0900

    feat: model/Race

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,4 +1,25 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void raceOnce() {
+        for (Car car : cars) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.move();
+            }
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
