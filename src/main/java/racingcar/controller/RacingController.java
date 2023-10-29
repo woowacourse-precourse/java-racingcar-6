732a51fc510eb16fa45a87c8fbc216c332419ed6
Jinseo Jeong <js020315@gmail.com>
2023-10-29 20:03:30 +0900

    [FEAT] RacingController
    InputCarsByUser() 자동차 이름 입력받고 쉼표로 구분
    InputAttemptsByUser() 시도할 횟수 입력받기

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,26 @@
 package racingcar.controller;
 
-public class RacingController {
-    public void GameStart() {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+    List<String> racingCars = new ArrayList<>();
+
+    public void GameProgress() {
+
+    }
+
+    public void InputCarsByUser() {
+        InputView.inputCars();
+        String inputCars = Console.readLine();
+        racingCars = List.of(inputCars.split(","));
+    }
+
+    public void InputAttemptsByUser() {
+        InputView.inputAttemptsNum();
+        int inputAttempts = Integer.parseInt(Console.readLine());
     }
 }
