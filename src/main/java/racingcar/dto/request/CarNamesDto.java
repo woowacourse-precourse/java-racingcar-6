aa4fb9c7da5361b2e3fa17eafdb7f4b883caf8d3
jongmee <ra123221@gmail.com>
2023-10-30 02:29:39 +0900

    feat: 경주차 이름의 길이가 5자를 초과할 경우 IllegalArgumentException을 발생시키기

--- src/main/java/racingcar/dto/request/CarNamesDto.java
+++ src/main/java/racingcar/dto/request/CarNamesDto.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class CarNamesDto {
     private static final String DELIMITER = ",";
+    private static final int NAME_SIZE = 5;
 
     private final List<String> names;
 
     public CarNamesDto(String carNames) {
         this.names = Arrays.asList(carNames.split(DELIMITER));
+        validateNameLength(names);
+    }
+
+    private void validateNameLength(List<String> names) {
+        for(String name: names) {
+            if(name.length() > NAME_SIZE || name.length() == 0) {
+                throw new IllegalArgumentException("length of numbers must be below " + NAME_SIZE);
+            }
+        }
     }
 
     public List<String> getNames() {
