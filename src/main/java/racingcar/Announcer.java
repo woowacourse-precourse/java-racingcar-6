6a17b36ebb01d4c35d52d1822eef4a6f35c4fbf8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 23:12:08 +0900

    feat(announcer): 자동차 이름을 입력하도록 멘트 출력하는 메서드 추가

--- src/main/java/racingcar/Announcer.java
+++ src/main/java/racingcar/Announcer.java
@@ -1,4 +1,13 @@
 package racingcar;
 
 public class Announcer {
+    private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+
+    public void announceToInputCarName() {
+        print(INPUT_CAR_NAME);
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+    }
 }
