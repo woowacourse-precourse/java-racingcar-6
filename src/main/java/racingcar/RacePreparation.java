62dc777e7c89a0a8f0160d1da58be632f01dbbe6
sohyun9527 <sohyun95277@gmail.com>
2023-10-29 12:19:29 +0900

    refactor(RacePreparation): 클래스 분리를 위해 점수판 만들기 기능만 담당
    출력은 RaceGame이 맡도록 하고, 게임 관련 클래스들은 값을 수정하기만 한다.

--- src/main/java/racingcar/RacePreparation.java
+++ src/main/java/racingcar/RacePreparation.java
@@ -3,51 +3,15 @@ package racingcar;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import racingcar.util.Printer;
-import racingcar.util.UserInput;
 
 public class RacePreparation {
-    private final Printer printer = new Printer();
-    private final UserInput userInput = new UserInput();
 
-    private List<String> carNames;
-    private int gameRound;
-    private Map<String, StringBuilder> scoreBoard;
-
-    public RacePreparation getRaceInformation() {
-        this.carNames = inputCarNames();
-        this.gameRound = inputGameRound();
-        this.scoreBoard = makeScoreBoard();
-
-        return this;
-    }
-
-    private List<String> inputCarNames() {
-        printer.printStartMessage();
-
-        return userInput.getCarNames();
-    }
-
-    private int inputGameRound() {
-        printer.printRequestRoundMessage();
-
-        return userInput.getGameRound();
-    }
-
-    private Map<String, StringBuilder> makeScoreBoard() {
+    public Map<String, StringBuilder> makeScoreBoard(List<String> carNames) {
         Map<String, StringBuilder> scoreBoard = new LinkedHashMap<>();
 
         for (String name : carNames) {
             scoreBoard.put(name, new StringBuilder());
         }
-        return scoreBoard;
-    }
-
-    public int getGameRound() {
-        return gameRound;
-    }
-
-    public Map<String, StringBuilder> getScoreBoard() {
         return scoreBoard;
     }
 }
