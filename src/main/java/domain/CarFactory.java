7b11cc8aa2807913cd3d8e19d0b69897387120a5
DH CHOI <bluetifulc@gmail.com>
2023-10-29 15:48:52 +0900

    feature: CarFactory에 이름 중복 검사 로직 추가

--- src/main/java/domain/CarFactory.java
+++ src/main/java/domain/CarFactory.java
@@ -3,7 +3,10 @@ package domain;
 import domain.Car;
 import domain.CarEngine;
 import infra.RandomCarEngine;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CarFactory {
 
@@ -14,6 +17,14 @@ public class CarFactory {
     }
 
     public List<Car> generateCars(final List<String> carNames) {
+        validateNoDuplicatedNames(carNames);
         return carNames.stream().map(name -> new Car(name, carEngine)).toList();
+    }
+
+    private void validateNoDuplicatedNames(final List<String> carNames) {
+        final Set<String> setNames = new HashSet<>(carNames);
+        if (carNames.size() != setNames.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
