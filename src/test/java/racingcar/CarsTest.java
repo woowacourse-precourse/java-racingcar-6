8f819f6186e726826f970288a355b22b63e8e435
Oseuseu <lakingsung@gmail.com>
2023-10-30 07:55:25 +0900

    test: findMaxPosition 메서드 테스트 코드 추가

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -8,17 +8,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CarsTest {
+    private List<Car> carList;
     private Cars cars;
 
     @BeforeEach
     void setUp() {
         List<String> carNames = Arrays.asList("red", "green", "blue");
-        List<Car> carList = new ArrayList<>();
+        carList = new ArrayList<>();
         for (String name : carNames) {
             carList.add(new Car(name));
         }
@@ -27,13 +27,24 @@ public class CarsTest {
 
     @Test
     void getCarsStatus_테스트() {
+        carList.get(0).move();
+        carList.get(0).move();
+
         Map<String, Integer> result = cars.getCarsStatus();
 
         assertTrue(result.containsKey("red"));
-        assertEquals(0, result.get("red"));
+        assertEquals(2, result.get("red"));
         assertTrue(result.containsKey("green"));
         assertEquals(0, result.get("green"));
         assertTrue(result.containsKey("blue"));
         assertEquals(0, result.get("blue"));
+    }
+
+    @Test
+    void findMaxPosition_테스트() {
+        carList.get(1).move();
+        carList.get(1).move();
+
+        assertEquals(2, cars.findMaxPosition());
     }
 }
