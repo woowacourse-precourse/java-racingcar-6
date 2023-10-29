fa06bc4ddda004a51a4b3ea92b1597c7b9090886
ioyxxnstream <ioyxxnstream@naver.com>
2023-10-29 19:39:47 +0900

    car class와  forward 함수 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+
 public class Application {
+
+    // 전진 함수
+    public static boolean forward(){
+        int condition = pickNumberInRange(0, 9);
+
+        if(condition >= 4 ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int distance;
+
+    public Car(String name, int distance){
+        this.name = name;
+        this.distance = distance;
+    }
+}
