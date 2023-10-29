f6df72bbac04c58957a3057c576416de58451aae
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-10-30 03:43:35 +0900

    feat(validation): InputException 클래스 생성
    - 예외 체크 메소드 선언
      - carNameValidation()
        - blank()
        - contiansBlank()
        - noCarName()
        - carNameLengthOver()
        - carNameDuplicate()
      - numberValidation()
        - blank()
        - notNumber()
        - underZero()
    - 클래스에 예외 처리 반영
      - UserInput.java

--- src/main/java/racingcar/utils/UserInput.java
+++ src/main/java/racingcar/utils/UserInput.java
@@ -1,14 +1,16 @@
 package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.InputException;
 
-// TODO : 예외 입히기
 public final class UserInput {
     public static String[] racingCarsName() {
-        return Console.readLine().trim().split(",");
+        System.out.println(Phrases.start);
+        return InputException.carNameValidation(Console.readLine()).split(",");
     }
 
-    public static Integer racingTimes() {
-        return Integer.parseInt(Console.readLine());
+    public static int racingTimes() {
+        System.out.println(Phrases.times);
+        return Integer.parseInt(InputException.numberValidation(Console.readLine()));
     }
 }
