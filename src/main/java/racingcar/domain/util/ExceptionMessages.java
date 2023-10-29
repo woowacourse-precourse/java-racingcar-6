2637191f9decdfd42ab4f60a5a8c22178958992b
junseoks-99 <angec5645@naver.com>
2023-10-30 00:32:35 +0900

    feat: 자동차 이름이 중복되는지 확인하는 기능 구현

--- src/main/java/racingcar/domain/util/ExceptionMessages.java
+++ src/main/java/racingcar/domain/util/ExceptionMessages.java
@@ -2,7 +2,8 @@ package racingcar.domain.util;
 
 public enum ExceptionMessages {
 
-    LENGTH("자동차의 이름은 1~5 글자 이어야 합니다.");
+    LENGTH("자동차의 이름은 1~5 글자 이어야 합니다."),
+    DUPLICATION("자동차 이름이 중복됩니다.");
 
     private final String exceptionMessage;
 
--- src/main/java/racingcar/domain/util/ValidationName.java
+++ src/main/java/racingcar/domain/util/ValidationName.java
@@ -1,6 +1,8 @@
 package racingcar.domain.util;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ValidationName {
 
@@ -10,11 +12,27 @@ public class ValidationName {
                 .forEach(carName -> checkLength(carName));
     }
 
+    public static void validateDuplication(String carNames) {
+        int beforeSize = carNames.split(",").length;
+        List<String> nameList = makeUnduplicateNameList(carNames);
+        int afterSize = nameList.size();
+        if (beforeSize != afterSize) {
+            throw new IllegalArgumentException(ExceptionMessages.DUPLICATION.getExceptionMessage());
+        }
+    }
+
     private static void checkLength(String carName) {
         int length = carName.length();
         if (length < NameLengthCondition.MIN_LENGTH.getLength()
                 || length > NameLengthCondition.MAX_LENGTH.getLength()) {
             throw new IllegalArgumentException(ExceptionMessages.LENGTH.getExceptionMessage());
         }
+    }
+
+    private static List<String> makeUnduplicateNameList(String carNames) {
+        return Arrays.stream(carNames.split(","))
+                .map(carName -> carName.trim())
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
