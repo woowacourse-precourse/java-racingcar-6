470832a9b82288d1fc20434062eb02b0cd5c33ca
soozzang <soozzang@inha.edu>
2023-10-29 16:27:37 +0900

    Feat(OutputView): 단순 메시지 print 메서드들 내용 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,28 @@
 package racingcar.view;
 
+import org.mockito.internal.verification.InOrderWrapper;
+
+import java.util.ArrayList;
+
 public class OutputView {
+
     public void printStartMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printRoundNumMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void printRoundResult() {
-
+    public void printWinnerMessage() {
+        System.out.print("최종 우승자 : ");
     }
 
-    public void printFinalResult() {
+    public void printWinnerResult(String carName) {
+        System.out.print(carName+", ");
+    }
 
+    public void printLastWinnerResult(String carName) {
+        System.out.print(carName);
     }
 }
