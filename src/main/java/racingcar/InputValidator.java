196b9fa59218c08dbf2d1d52858e630839a9e9bd
hyuna <monicajo0@naver.com>
2023-10-29 12:13:50 +0900

    refactor: 숫자 입력 검증 기능의 성능 개선

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputValidator {
     private static final int CAR_NAME_MIN_LENGTH = 1;
     private static final int CAR_NAME_MAX_LENGTH = 5;
@@ -7,6 +10,7 @@ public class InputValidator {
     private static final String CAR_NAME_LENGTH_EXCEPTION = "자동차 이름은 1자 이상 5자 이하만 가능합니다.";
     private static final String BLANK_EXCEPTION = "입력값이 존재하지 않습니다.";
     private static final String NOT_ONLY_NUMBER_EXCEPTION = "숫자만 입력해야 합니다.";
+    private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile(ONLY_NUMBER_REGEX);
 
     public void validCarNameLength(String carName) {
         if (carName.length() < CAR_NAME_MIN_LENGTH || carName.length() > CAR_NAME_MAX_LENGTH) {
@@ -26,7 +30,9 @@ public class InputValidator {
     }
 
     private void validOnlyNumber(String input) {
-        if (!input.matches(ONLY_NUMBER_REGEX)) {
+        Matcher matcher = ONLY_NUMBER_PATTERN.matcher(input);
+
+        if (!matcher.matches()) {
             throw new IllegalArgumentException(NOT_ONLY_NUMBER_EXCEPTION);
         }
     }
