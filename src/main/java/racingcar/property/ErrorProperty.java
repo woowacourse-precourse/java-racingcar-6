119fc6b4b74ecb257de9f685fab7151b10e42bdf
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 16:55:12 +0900

    refactor: modify attach variable final keyword cause will not modify

--- src/main/java/racingcar/property/ErrorProperty.java
+++ src/main/java/racingcar/property/ErrorProperty.java
@@ -2,7 +2,7 @@ package racingcar.property;
 
 public class ErrorProperty {
 
-    public static String VALUE_IS_EMPTY="[ERROR] 입력값이 공백일 수는 없습니다.";
-    public static String VALUE_CONTAINS_SPACE="[ERROR] 입력값에 공백이 포함되었습니다.";
-    public static String NAME_VALUE_IS_NOT_CORRECT_FORM="[ERROR] 이름은 한글 혹은 숫자 값만 들어가야 합니다.";
+    public static final String VALUE_IS_EMPTY="[ERROR] 입력값이 공백일 수는 없습니다.";
+    public static final String VALUE_CONTAINS_SPACE="[ERROR] 입력값에 공백이 포함되었습니다.";
+    public static final String NAME_VALUE_IS_NOT_CORRECT_FORM="[ERROR] 이름은 한글 혹은 숫자 값만 들어가야 합니다.";
 }
