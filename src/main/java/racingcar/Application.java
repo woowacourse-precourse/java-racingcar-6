e62dfc5913da8c0b80a5a133d92c78f0de328b37
sinminseok12 <sinminseok12@naver.com>
2023-10-29 19:41:59 +0900

    feat(RacingController): 레이싱 게임 흐름을 제어할 RacingController 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        RacingController racingController = new RacingController(inputView);
+        racingController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingController {
+    private final InputView inputView;
+
+    public RacingController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+
+    }
+}
