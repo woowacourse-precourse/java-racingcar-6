994d6540a05ea215bcb07e43dbe95befef2ffcee
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-29 23:06:33 +0900

    [refactor] : change for logic to stream logic

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,70 +1,32 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
-import racingcar.printer.GameGuidePrinter;
+import racingcar.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameController {
 
-    public void start() {
-        GameGuidePrinter.getCarNameInputMessage();
-        String carList = carNameInput();
-        String[] cars = groupCarsByName(carList);
-        List<Car> cars1 = createCar(cars);
-        GameGuidePrinter.getTryCountMessage();
-        int tryCount = tryCountInput();
-        GameGuidePrinter.getResultMessage();
+    public List<Car> createCar(String[] carNames) {
 
+        return Arrays.stream(carNames)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
 
-        for (int i = 0; i < tryCount; i++) {
-            moveCar(cars1);
-            GameGuidePrinter.showCarNameAndPosition(cars1);
+    public void playRacing(int tryCount,List<Car> cars) {
+        for (int i=0; i<tryCount; i++) {
+            moveCar(cars);
+            OutputView.showCarNameAndPosition(cars);
         }
-    }
-
-    private String carNameInput() {
-        String carList = Console.readLine();
-        return carList;
-    }
-
-    private String[] groupCarsByName(String carList) {
-        String[] cars = carList.split(",");
-        return cars;
-    }
-
-    private List<Car> createCar(String[] cars) {
-        List<Car> cars1 = new ArrayList<>();
-
-        for (String carName : cars) {
-            Car car = new Car();
-            car.setName(carName);
-            cars1.add(car);
-        }
-
-        return cars1;
-    }
-
-    private int tryCountInput() {
-        String tryCount = Console.readLine();
-        return Integer.parseInt(tryCount);
     }
 
     private void moveCar(List<Car> cars) {
         for (Car car : cars) {
-            verification(car);
-        }
-    }
-
-    private void verification(Car car) {
-        int randomNumber = Randoms.pickNumberInRange(1, 9);
-
-        if (randomNumber > 4) {
             car.move();
         }
     }
-
 }
