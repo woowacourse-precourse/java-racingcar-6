9426a1ae3c7735b5330f2d6f460a263ddc237489
ssuzyn <asj7090@gmail.com>
2023-10-29 18:05:11 +0900

    :sparkles: [feat] : 각 차수별 랜덤숫자에 따른 전진, 정지 기능 추가

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.view.InputView;
@@ -14,6 +15,7 @@ public class Game {
         tryCount = InputView.readTryCount();
 
         generateCar(car);
+        start();
     }
 
     public void generateCar(String[] car){
@@ -22,5 +24,28 @@ public class Game {
         for(String name: car){
             carList.add(new Car(name));
         }
+    }
+
+    public void start(){
+        while(tryCount > 0){
+            gameByOrder();
+            tryCount--;
+        }
+    }
+
+    public void gameByOrder(){
+        for(Car car: carList){
+            int forward = moveForwardStep(getRandomNumber());
+            car.setForward(forward);
+        }
+    }
+
+    public int getRandomNumber (){
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    public int moveForwardStep(int num){
+        if(num >= 4 && num <= 9) return 1;
+        return 0;
     }
 }
