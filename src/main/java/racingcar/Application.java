b26f345d36d9e8cb6036ad7b719b8b7542943805
unknown <winternrabbit@gmail.com>
2023-10-29 21:46:26 +0900

    레이싱 게임 실행을 위한 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,25 +5,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Input input = new Input();
-        Car car = new Car();
-        Validator validator = new Validator();
+        Game game = new Game();
 
-        String inputCarNames = input.carName();
-        validator.hasSpace(inputCarNames);
-
-        List<String> carNames = car.splitByComma(inputCarNames);
-        /*System.out.println(inputCarNames);
-        System.out.println(carNames);*/
-
-        String inputNumber = input.tryNumber();
-        validator.isNumber(inputNumber);
-
-        int tryNumber = Integer.parseInt(inputNumber);
-
-        List<Integer> carMoveDistance = car.nowRacing(carNames, tryNumber);
-
-        String winner = car.printWinner(carNames, carMoveDistance);
-        System.out.println(winner);
+        game.run();
     }
 }
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,2 +1,27 @@
-package racingcar;public class Game {
+package racingcar;
+
+import java.util.List;
+
+public class Game {
+    public void run() {
+        Input input = new Input();
+        Car car = new Car();
+        Validator validator = new Validator();
+
+        String inputCarNames = input.carName();
+        validator.hasSpace(inputCarNames);
+
+        List<String> carNames = car.splitByComma(inputCarNames);
+        validator.isLengthOverFive(carNames);
+
+        String inputNumber = input.tryNumber();
+        validator.isNumber(inputNumber);
+
+        int tryNumber = Integer.parseInt(inputNumber);
+
+        List<String> carMove = car.racingResult(carNames, tryNumber);
+        List<Integer> carMoveDistance = car.countCarMove(carMove);
+
+        car.printWinner(carNames, carMoveDistance);
+    }
 }
