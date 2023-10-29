ccc7c4552365ad740631315586179a5abd186259
m2nseop <pms246@naver.com>
2023-10-30 01:30:41 +0900

    feat: 각 회차별 랜덤숫자 생성 및 전진상태 저장

--- src/main/java/racingcar/Controller/RacingCarGameController.java
+++ src/main/java/racingcar/Controller/RacingCarGameController.java
@@ -1,5 +1,6 @@
 package racingcar.Controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.Model.Car;
@@ -8,20 +9,49 @@ import racingcar.View.InputView;
 
 public class RacingCarGameController {
 
-    public void initRacingCarGame(){
+    public void initRacingCarGame() {
         List<String> carNameList = InputView.inputRacingCarName();
         InputValidator.checkCarNameLength(carNameList);
         String racingRoundTimes = InputView.inputRacingRoundTimes();
         InputValidator.checkIsNumber(racingRoundTimes);
 
+        List<Car> carList = createRacingCar(carNameList);
+
+        roundProgress(carList, Integer.parseInt(racingRoundTimes)); // 게임 진행
         createRacingCar(carNameList);
     }
 
-    public void createRacingCar(List<String> carNameList){
+    public List<Car> createRacingCar(List<String> carNameList) {
         List<Car> carList = new ArrayList<>();
 
         for (String carName : carNameList) {
             carList.add(new Car(carName));
         }
+        return carList;
+    }
+
+    public int makeRandomNumber() { // 랜덤번호 생성
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void roundProgress(List<Car> carList, int racingRoundTimes){
+        for(int i=0; i<racingRoundTimes; i++){
+            for (Car car : carList) {
+                if(checkRandomNumber(makeRandomNumber())){
+                    car.plusStepCount();
+                }
+            }
+
+        }
+        for (Car car : carList) {
+            System.out.println(car.getName() + ": " + car.getStepCount());
+        }
+    }
+
+    public boolean checkRandomNumber(int num){
+        if (num >= 4){
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -2,6 +2,7 @@ package racingcar.Model;
 
 public class Car {
     private String carName;
+    private int stepCount = 0;
 
     public Car(String carName) {
         this.carName = carName;
@@ -9,5 +10,13 @@ public class Car {
 
     public String getName(){
         return this.carName;
+    }
+
+    public int getStepCount(){
+        return this.stepCount;
+    }
+
+    public void plusStepCount(){
+        this.stepCount = this.stepCount + 1;
     }
 }
