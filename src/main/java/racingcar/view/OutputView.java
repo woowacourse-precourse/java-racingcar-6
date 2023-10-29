6f5ef1b5a8c8fd874a94d924b53d5567b87ff409
Cycrypto <jh01love@naver.com>
2023-10-29 17:40:07 +0900

    feature/game-process-flow:진행상황 출력 매서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,17 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.RacingCar;
+
 public class OutputView {
     public static void gameResultMessge(){
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println("실행 결과");
+    }
+    public static void moveDistanceMessage(RacingCar racingCar){
+        System.out.println(racingCar.getName()+" : "+ racingCar.getDistanceAsBar());
+    }
+    public static void printFinalWinner(List<String> winners){
+        String result = String.join(",", winners);
+        System.out.println("최종 우승자 : "+result);
     }
 }
