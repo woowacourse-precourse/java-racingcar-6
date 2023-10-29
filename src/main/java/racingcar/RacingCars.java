39c22639188155b98e8119c6fc3e391fd19fe90b
Jinseo Jeong <js020315@gmail.com>
2023-10-29 18:35:08 +0900

    [FEAT] RacingCars 객체 생성
    자동차들의 이름

---
+++ src/main/java/racingcar/RacingCars.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.List;
+
+public class RacingCars {
+    List<String> racingCars;
+
+    public RacingCars(List<String> racingCars) {
+        this.racingCars = racingCars;
+    }
+}
