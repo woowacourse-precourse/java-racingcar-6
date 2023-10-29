982dbe38bb9a0bdaa7e30ba2b8791be7e61246a7
seona-moon <fhflwhwl5@gmail.com>
2023-10-29 20:25:28 +0900

    feat: 자동차 이름 입력값, 경주시도에 대한 예외처리 (공백, 5자 초과, 중복값, 숫자 여부)

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,5 +1,36 @@
-package racingcar.view;
+package racingcar.validation;
+
+import java.util.Collections;
+import java.util.List;
 
 public class Validator {
 
+  public void validateNullValue(Object value) {
+    if (value == "" || value == null) {
+      throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+    }
+  }
+
+  public void validateNameLength(List<String> nameList) {
+    for (int i = 0; i < nameList.size(); i++) {
+      if(nameList.get(i).length()>5) {
+        throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH.getMessage());
+      }
+    }
+  }
+
+  public void validateNum(String count) {
+    if(!count.matches("^[0-9]+$")){
+      throw new IllegalArgumentException(ErrorMessage.CONTAIN_LETTER.getMessage());
+    }
+  }
+
+  public void validateDuplicatedName(List<String> nameList) {
+    for (int i = 0; i < nameList.size(); i++) {
+      int count = Collections.frequency(nameList, nameList.get(i));
+      if(count > 1) {
+        throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NAME.getMessage());
+      }
+    }
+  }
 }
