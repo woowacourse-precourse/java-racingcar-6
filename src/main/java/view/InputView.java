b319e4559f259a6b2a402c4ece4f27ecab2f99cb
dd <12202340@inha.edu>
2023-10-29 16:53:06 +0900

    Feat: 입력 받은 문자열을 구분자 기준으로 파싱하는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,21 @@
-package view;public class InputView {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    public static final String DELIMITER = ",";
+
+    public static List<String> inputCarNames() {
+        OutputView.printInputCarName();
+        return splitWords(Console.readLine(), DELIMITER);
+    }
+
+    private static List<String> splitWords(String string, String delimiter) {
+        return Arrays.stream(string.split(delimiter))
+                .toList();
+    }
+
 }
