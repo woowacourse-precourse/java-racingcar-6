685cd1edbf856f53fbfbc8fc7d4ac06658ce95a7
sunghyun <shlim0287@naver.com>
2023-10-30 01:04:31 +0900

    feat : Car 도메인 추가.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,40 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
+    private String carName;
+
+    private List<String> carNameList;
+
+    private int position;
+
+    public Car(){
+        carNameList=new ArrayList<>();
+    }
+
+    public Car(String carName){
+        validateCarNameLength(carName);
+        this.carName=carName;
+        this.position=0;
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if(carName.length()>5){
+            throw new IllegalArgumentException("이름은 5글자 이하만 가능합니다.");
+        }
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public List<String> getCarNameList() {
+        return carNameList;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
