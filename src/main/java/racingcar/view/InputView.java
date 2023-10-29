c36389277b0371454100cacd97afc64352cd8945
junseoks-99 <angec5645@naver.com>
2023-10-30 02:58:15 +0900

    feat: 입력 문자열 검증 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,22 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.ValidationName;
+import racingcar.util.ValidationNumber;
 
 public class InputView {
 
     public static String inputCarNames() {
         printStartMessage();
         String carNames = input();
+        ValidationName.validateCarNames(carNames);
         return carNames;
     }
 
     public static String inputAttemptNumber() {
         printAttemptMessage();
         String attemptNumber = input();
+        ValidationNumber.validateAttemptNumber(attemptNumber);
         return attemptNumber;
     }
 
