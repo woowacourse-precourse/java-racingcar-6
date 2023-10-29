554f4c53a7de985880b572c180b7581e9b58daa0
kongji <kongji4092@gmail.com>
2023-10-29 18:06:18 +0900

    feat: 게임 라운드마다 자동차 이동

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,16 +1,34 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.Validator;
+import racingcar.controller.dto.RoundResponseDto;
 import racingcar.domain.Car;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
     GameService gameService = new GameService();
+    Validator validator = new Validator();
+
     public void run() {
         OutputView.printStartGame();
         String[] input = InputView.inputCarList();
         List<Car> carList = gameService.registerCarListFromInput(input);
+        int gameRound = validator.validateGameRound(InputView.inputGameRound());
+
+        OutputView.printRoundStart();
+        for (int i = 0; i < gameRound; i++) {
+            // 1개 라운드
+            for (Car tempCar : carList) {
+                tempCar.move();
+            }
+            // 게임 결과 출력
+            OutputView.printGameStatus(new RoundResponseDto(carList));
+        }
+
+
     }
 
 }
+
