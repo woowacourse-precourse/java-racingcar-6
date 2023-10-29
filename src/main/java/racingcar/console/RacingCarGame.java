a5d81bdd4a85e93ee669ebbda910933ca903b965
Hayoon <hy97@sju.ac.kr>
2023-10-29 15:20:42 +0900

    feat(RacingCarGame): 게임콘솔 생성 및 입력 메서드(initInput) 구현

---
+++ src/main/java/racingcar/console/RacingCarGame.java
@@ -0,0 +1,37 @@
+package racingcar.console;
+
+import racingcar.domain.RacingCar;
+import racingcar.service.RacingCarService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class RacingCarGame {
+    public final int start = 0;
+    public RacingCarService racingCarService;
+
+    public void playGame() {
+
+        racingCarService = new RacingCarService();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        ArrayList<RacingCar> racingCars = racingCarService.initRacingCarMembers();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int number = racingCarService.initNumberOfAttempts();
+
+        System.out.println("실행 결과");
+
+        while (start < number) {
+            /**
+             * pobi : -
+             * woni :
+             * jun : -
+             */
+            racingCarService.movingForward(racingCars);
+        }
+    }
+}
