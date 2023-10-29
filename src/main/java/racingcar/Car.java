2ab6e64c29e06237a0d99b7f9921e04f5e3e142a
booyaChael <chaeeul2022@gmail.com>
2023-10-29 11:29:41 +0900

    feat(Game): racingCars 생성하기(setRacingCars)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,8 @@
 package racingcar;
 
 public class Car {
+	private final String name;
+	Car(String name){
+		this.name = name;
+	}
 }
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import racingcar.View.InputView;
 import racingcar.View.OutputView;
 
 public class Game {
@@ -19,12 +22,42 @@ public class Game {
 
 	}
 
-	private void setRacingCars(){}
+	private void setRacingCars(){
+		String input = InputView.getCarNamesInput();
+		validateCarNamesInput(input);
+		this.racingCars = makeCarNamesInputToCarList(input);
+	}
 
 	private void setRoundCount(){}
 
 	private Car findWinner(){
-		Car winner = new Car();
+		Car winner = new Car("name");
 		return winner;
+	}
+
+	private void validateCarNamesInput(String input){
+		if(!input.contains(",")){
+			throw new IllegalArgumentException("이름은 쉼표(,)로 구분해야 합니다.");
+		}
+		String[] carsArray = input.split(",");
+		for(String car : carsArray){
+			if(car.length() > 5){
+				throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+			}
+		}
+	}
+
+	private List<Car> makeCarNamesInputToCarList(String input){
+		List<Car> racingCars = new ArrayList<>();
+		String[] carNamesArray = input.split(",");
+		for(String carName : carNamesArray){
+			Car car = makeCarInstance(carName);
+			racingCars.add(car);
+		}
+		return racingCars;
+	}
+
+	private Car makeCarInstance(String name){
+		return new Car(name);
 	}
 }
