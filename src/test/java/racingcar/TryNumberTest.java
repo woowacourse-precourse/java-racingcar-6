51f9cb54aa9d5676725778664460be31674cff14
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-29 15:47:39 +0900

    test: TryNumber 테스트코드 추가

--- src/test/java/racingcar/TryNumberTest.java
+++ src/test/java/racingcar/TryNumberTest.java
@@ -1,7 +1,8 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import racingcar.model.TryNumber;
@@ -9,32 +10,35 @@ import racingcar.model.TryNumber;
 public class TryNumberTest {
     @Test
     void 시도횟수_1보다_작을_때_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    TryNumber tryNumber = new TryNumber("0");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            TryNumber tryNumber = new TryNumber("0");
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 시도횟수_음수일_때_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    TryNumber tryNumber = new TryNumber("-1");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            TryNumber tryNumber = new TryNumber("-1");
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 시도횟수_소수일_때_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    TryNumber tryNumber = new TryNumber("1.2");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            TryNumber tryNumber = new TryNumber("1.2");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력_시도횟수_보다_경주안했을_때_확인() {
+        TryNumber tryNumber = new TryNumber("3");
+        assertFalse(tryNumber.isOver(1));
+    }
+
+    @Test
+    void 입력_시도횟수만큼_경주했을_때_확인() {
+        TryNumber tryNumber = new TryNumber("3");
+        assertTrue(tryNumber.isOver(3));
     }
 
 }
