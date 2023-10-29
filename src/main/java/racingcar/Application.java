9832a23c8138fb774c5f720978fe9aa69185c399
dk7648 <dk7648@korea.ac.kr>
2023-10-29 11:43:07 +0900

    refactor(Application): Todo주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         CarGenerator generator = new CarGenerator();
         Printer printer = new Printer();
         Referee referee = new Referee();
