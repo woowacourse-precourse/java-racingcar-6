69ee6ea1209d1adb86f8bc2a79cf8c0dff6e413c
penrose15 <seongju99@naver.com>
2023-10-29 16:10:51 +0900

    Refactor: 메서드 추출(인덴트 2이하로)

--- src/main/java/racingcar/domain/car/dto/CarsNameDto.java
+++ src/main/java/racingcar/domain/car/dto/CarsNameDto.java
@@ -1,6 +1,7 @@
 package racingcar.domain.car.dto;
 
 public record CarsNameDto(String names) {
+    private static final String COMMA = ",";
 
     public CarsNameDto {
         validateNamesIsNull(names);
@@ -10,30 +11,28 @@ public record CarsNameDto(String names) {
     //dto의 유효성 검증 범위를 어디까지 해야 하는가?
 
 
-    public void validateNamesIsNull(String names) {
+    private void validateNamesIsNull(String names) {
         if (names == null) {
             throw new IllegalArgumentException("names cannot be null");
         }
     }
-    public void validateOneOfNameIsNull(String names) {
+    private void validateOneOfNameIsNull(String names) {
         String first = names.substring(0,1);
         String last = names.substring(names.length()-1);
 
-        if(first.equals(",") || last.equals(",")) {
+        if(first.equals(COMMA) || last.equals(COMMA)) {
             throw new IllegalArgumentException("이름 입력시 처음과 끝이 , 로 끝나면 안됩니다.");
         }
     }
 
-    public void validateCommasContinuousInput(String names) {
+    private void validateCommasContinuousInput(String names) {
         char[] namesArr = names.toCharArray();
 
         int commasCnt = 0;
         for (char c : namesArr) {
             commasCnt = countCommas(commasCnt, c);
 
-            if(commasCnt > 1) {
-                throw new IllegalArgumentException("이름 입력시 처음과 끝이 , 로 끝나면 안됩니다.");
-            }
+            checkCommasCount(commasCnt);
         }
     }
 
@@ -44,6 +43,12 @@ public record CarsNameDto(String names) {
             commasCnt = 0;
         }
         return commasCnt;
+    }
+
+    private void checkCommasCount(int commasCnt) {
+        if(commasCnt > 1) {
+            throw new IllegalArgumentException("이름 입력시 처음과 끝이 , 로 끝나면 안됩니다.");
+        }
     }
 
 }
