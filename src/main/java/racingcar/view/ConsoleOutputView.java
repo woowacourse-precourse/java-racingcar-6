a5988bb1e4eac0b74103d4d8e7dfb58399a50fe7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 23:29:38 +0900

    feat(ConsoleOutputView): 게임 횟수 입력 요청 메시지 출력 기능 구현

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 public class ConsoleOutputView implements OutputView{
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String REQUEST_GAME_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     private static ConsoleOutputView instance;
 
     private ConsoleOutputView() {}
@@ -20,7 +21,7 @@ public class ConsoleOutputView implements OutputView{
 
     @Override
     public void printRequestGameRound() {
-
+        System.out.println(REQUEST_GAME_ROUND_MESSAGE);
     }
 
     @Override
