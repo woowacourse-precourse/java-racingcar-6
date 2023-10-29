ba49907ab201de1fc45ae1ea7315382a32cac7e1
Jiwon <wldnjs980409@naver.com>
2023-10-29 18:23:07 +0900

    [feat] Output View

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printResult(){
+        System.out.println("실행 결과");
+    }
+
+    public void printWinner(){
+        System.out.print("최종 우승자 : ");
+    }
+}
