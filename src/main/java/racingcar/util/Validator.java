9d7282d1ef6e0e7e93737b7c0982fe973e408a8d
xogus3492 <wkdxogus3492@gmail.com>
2023-10-29 19:23:23 +0900

    Feat(Validator): 시도 횟수가 1 이상인지 검증하는 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class Validator {
     private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int COUNT_MIN_VALUE = 1;
     private static final String CAR_NAME_LENGTH_EXCESS_ERROR_MESSAGE = "자동차 이름의 길이가 5를 초과했습니다.";
     private static final String EXIST_SPACE_IN_CAR_NAME_ERROR_MESSAGE = "자동차 이름에 공백이 존재합니다.";
     private static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "자동차 이름이 중복됐습니다.";
+    private static final String COUNT_NOT_GREATER_THAN_ONE_ERROR_MESSAGE = "시도 횟수는 1이상이어야 합니다.";
     private static final String SPACE = " ";
 
     public static void verifyCarNames(List<String> carNameList) {
@@ -18,7 +20,8 @@ public class Validator {
     }
 
     public static void verifyTrialCount(String input) {
-        InputParser.parseTrialCountToInt(input);
+        int count = InputParser.parseTrialCountToInt(input);
+        verifyGreaterThanOne(count);
     }
 
     private static void verifyCarNameLength(String name) {
@@ -39,6 +42,12 @@ public class Validator {
                 .count();
         if (distinctCount < carNameList.size()) {
             throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    private static void verifyGreaterThanOne(int count) {
+        if (count < COUNT_MIN_VALUE) {
+            throw new IllegalArgumentException(COUNT_NOT_GREATER_THAN_ONE_ERROR_MESSAGE);
         }
     }
 }
