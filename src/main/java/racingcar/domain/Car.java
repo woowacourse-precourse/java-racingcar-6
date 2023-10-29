568430703546b20a1db139e228bf52bc3621a4b4
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 04:47:17 +0900

    feat(Car) : Car클래스의 메소드 구현
    addDistance,toString(오버라이드)
     getToTalDistance -> 이 함수는 테스트용으로 추후 변경가능성이 있다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,10 +3,22 @@ package racingcar.domain;
 public class Car {
     private String name;
     private int totalDistance;
-    
-    Car(String name, int totalDistance){
+
+    public Car(String name) {
         this.name = name;
-        this.totalDistance = totalDistance;
+        this.totalDistance = 0;
     }
 
+    public void addDistance() {
+        ++this.totalDistance;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
