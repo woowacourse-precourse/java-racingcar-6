808458ca9f588eae8a604b729f9cd9ee0b9183d8
gumgu <bienlee@naver.com>
2023-10-29 16:57:05 +0900

    Feat : 입력값 검증기능 구현 - 빈문자인 경우

---
+++ src/main/java/racingcar/utility/Validator.java
@@ -0,0 +1,11 @@
+package racingcar.utility;
+
+import java.util.List;
+
+public class Validator {
+
+    public static boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+}
