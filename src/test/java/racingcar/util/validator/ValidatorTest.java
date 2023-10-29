9964606c6c5983c04dea05225cf35e0e75419bd8
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-29 14:10:32 +0900

    test(Validator): 이름의 길이 초과 예외처리 테스트 코드의 파라미터 수정

--- src/test/java/racingcar/util/validator/ValidatorTest.java
+++ src/test/java/racingcar/util/validator/ValidatorTest.java
@@ -10,8 +10,8 @@ import racingcar.util.message.ErrorMessage;
 
 public class ValidatorTest {
 	
-	@DisplayName("길이가 5초과하면 예외를 발생시킨다.")
-	@ValueSource(strings = { "jajass", "nananana" })
+	@DisplayName(",로 구분된 이름의 길이가 5초과하면 예외를 발생시킨다.")
+	@ValueSource(strings = { "jaaa,aaaabb", "nannnnn,nanan" })
 	@ParameterizedTest
 	void checkOutOfLength(String str) {
 		assertThatThrownBy(() -> Validator.validateOutOfLength(str))
@@ -19,8 +19,8 @@ public class ValidatorTest {
 						.hasMessageContaining(ErrorMessage.CAR_NAME_LENGTH_ERROR.getMessage());
 	}
 
-	@DisplayName("길이가 5이하일때 정삭적으로 작동하는 지 확인한다.")
-	@ValueSource(strings = { "nana", "jjaaa" })
+	@DisplayName(",로 구분된 이름의 길이가 5이하일때 정삭적으로 작동하는 지 확인한다.")
+	@ValueSource(strings = { "naan,jjaa", "jjaa" })
 	@ParameterizedTest
 	void checkNomalOperation1(String str) {
 		Validator.validateOutOfLength(str);
