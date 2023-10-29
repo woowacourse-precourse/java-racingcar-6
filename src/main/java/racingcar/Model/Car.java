732f7b1d6d68d3370c21157868e8f82a288e040c
kiwkim <atthenoon@gmail.com>
2023-10-30 04:06:45 +0900

    feat($Car): add `progressCounter` as member.

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -3,10 +3,12 @@ package racingcar.Model;
 public class Car {
     private String name;
     private String currentProgress;
+    private int progressCounter;
 
     public Car(String name){
         this.name = name;
         this.currentProgress = "";
+        this.progressCounter = 0;
     }
 
     public void setCarName(String input){
@@ -17,8 +19,12 @@ public class Car {
     }
     public void addProgress(){
         this.currentProgress = this.currentProgress + '-';
+        this.progressCounter++;
     }
     public String getProgress(){
         return this.currentProgress;
+    }
+    public int getProgressCounter(){
+        return this.progressCounter;
     }
 }
