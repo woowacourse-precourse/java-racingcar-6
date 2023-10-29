7557496f660239a87f2198983a82f8905d6a9526
junest66 <chlwndks333@gmail.com>
2023-10-30 02:19:29 +0900

    refactor(OutputView): 입력 안내 메세지 InputView로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,19 +5,9 @@ import racingcar.Car;
 
 public class OutputView {
 
-    private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String PROMPT_MESSAGE = "시도할 회수는 몇 회인가요?";
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
     private static final String COUNT_SYMBOL = "-";
-
-    public static void printGameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
-    }
-
-    public static void printPromptMessage() {
-        System.out.println(PROMPT_MESSAGE);
-    }
 
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
