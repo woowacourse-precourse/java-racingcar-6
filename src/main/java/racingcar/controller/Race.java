995772acbe23c6c4abf64e9fe6a5424709fecfdd
gusdn0108 <gusdn6671@naver.com>
2023-10-30 00:25:49 +0900

    feat(Race): 차 목록 생성하기

--- src/main/java/racingcar/controller/Race.java
+++ src/main/java/racingcar/controller/Race.java
@@ -1,9 +1,35 @@
 package racingcar.controller;
 
-public class Race {
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
-    public void run(){
-        System.out.println("hello Race");
+public class Race {
+    private final List<Car> cars = new ArrayList<>();
+    private int moveCnt;
+    public void run() {
+        OutputView.printRacingStart();
+        callCarNames();
+        OutputView.printAskCount();
+        moveCnt = getMoveCount();
+    }
+
+    private static int getMoveCount() {
+        String userInput = InputView.userInput();
+        return Integer.parseInt(userInput);
+    }
+
+
+    private void callCarNames() {
+        String input = InputView.userInput();
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
     }
 
 }
+
