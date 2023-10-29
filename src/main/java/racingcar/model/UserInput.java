17ba46285a50a9674a9a7f9bcc417e0c6fa90893
toychip <dlawnsgud427@naver.com>
2023-10-29 20:37:07 +0900

    feat: 사용자 입력 String -> List<String>으로 return 메서드 추가

---
+++ src/main/java/racingcar/model/UserInput.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.view.InputView;
+
+public class UserInput {
+
+    public void process() {
+        List<String> userInputList = toArray();
+    }
+
+    public List<String> toArray() {
+        String userInput = InputView.getUserInput();
+        String[] split = userInput.split(",");
+        return Arrays.asList(split);
+    }
+}
