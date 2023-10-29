65a1cfad40075609388bade082b793518293ad28
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-10-29 17:03:19 +0900

    refactor(GameEngineValidatorTest): 자바 컨벤션에 맞게 수정
    Arrays.asList() 보단 Collections.singletonList을 추천받음
     https://www.korecmblog.com/blog/arrays-asList-alternative-Collections-singletonList
    Junit5에서는 테스트의 접근제한를 디폴트 접근자로 해야한다고 함

--- src/test/java/racingcar/engine/GameEngineValidatorTest.java
+++ src/test/java/racingcar/engine/GameEngineValidatorTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 class GameEngineValidatorTest {
-    private static int MAX_SIZE = 5;
+    private static final int MAX_SIZE = 5;
 
     private static String[] 생성이_안되는_케이스() {
         return new String[]{" ".repeat(MAX_SIZE + 1), "123456789"};
@@ -19,30 +19,30 @@ class GameEngineValidatorTest {
     }
 
     @Test
-    public void 플레이어이름이_null값이면_예외를던진다() {
+    void 플레이어이름이_null값이면_예외를던진다() {
         Assertions.assertThatCode(() -> new GameEngineValidator().validateIsNotNull(null))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력값은 null입니다.");
     }
 
     @Test
-    public void 플레이어이름이_null값이아니면_예외를던지지_않는다() {
+    void 플레이어이름이_null값이아니면_예외를던지지_않는다() {
         Assertions.assertThatCode(() -> new GameEngineValidator().validateIsNotNull(""))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @MethodSource("생성이_안되는_케이스")
-    public void 플레이어이름이_6이상이면_예외를_던진다(String readLine) {
-        Assertions.assertThatCode(() -> new GameEngineValidator().validatePlayerNames(Arrays.asList(readLine)))
+    void 플레이어이름이_6이상이면_예외를_던진다(String readLine) {
+        Assertions.assertThatCode(() -> new GameEngineValidator().validatePlayerNames(Collections.singletonList(readLine)))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("현재 입력된 플레이어 이름의 길이는 %d, %d이하로 해주세요.", readLine.length(), MAX_SIZE);
     }
 
     @ParameterizedTest
     @MethodSource("생성이_되는_케이스")
-    public void 플레이어이름이_5이하이면_예외를_던진다(String readLine) {
-        Assertions.assertThatCode(() -> new GameEngineValidator().validatePlayerNames(Arrays.asList(readLine)))
+    void 플레이어이름이_5이하이면_예외를_던진다(String readLine) {
+        Assertions.assertThatCode(() -> new GameEngineValidator().validatePlayerNames(Collections.singletonList(readLine)))
                 .doesNotThrowAnyException();
     }
 
