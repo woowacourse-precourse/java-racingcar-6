576267c7cdc801d208728bbdaee8926687488bbb
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 22:38:20 +0900

    feat(Main): Dependency 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.converter.RacingConverter;
+import racingcar.factory.CarFactory;
+import racingcar.generator.RandomNumberGenerator;
+import racingcar.service.RacingService;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final InputView inputView = new InputView(new InputValidator());
+        final OutputView outputView = new OutputView();
+        final RacingConverter converter = new RacingConverter();
+        final RacingService racingService = new RacingService(new RandomNumberGenerator());
+        final CarFactory carFactory = new CarFactory();
+        final RacingController racingController = new RacingController(inputView, outputView, racingService, converter, carFactory);
+
+        racingController.run();
     }
 }
