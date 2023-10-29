280678a8023fc56c4d8b9d709470703f3522c4bc
Dawin <dawin002@naver.com>
2023-10-29 23:22:12 +0900

    refactor(Game): GameProgress 클래스 이름 변경
    게임 진행을 관리하는 클래스 이름 변경:
    GameProgress -> Game
    변경 사유: 더 직관적인 이름으로 변경하는 것이 이해하기 쉬운 코드를 짜는 방식이라 판단함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,11 @@
 package racingcar;
 
-import racingcar.controller.GameController;
+import racingcar.controller.Game;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController game = new GameController();
+        Game game = new Game();
         game.run();
     }
 }
