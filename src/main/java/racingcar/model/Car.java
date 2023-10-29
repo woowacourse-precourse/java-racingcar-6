c1679f058db9c833268a33bf6abd82dd2aa32681
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-29 14:48:50 +0900

    feat : add model for user

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package main.java.racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void forward() {
+        if () {
+            this.position += 1;
+        }
+    }
+
+    public int getPosition(){
+        return position;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
