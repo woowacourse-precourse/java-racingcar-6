6b508a96f6bf1bfbe78e9830d7a7289c354074e6
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-29 23:57:07 +0900

    🎯feat(domain / Cars) : Cars 클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import java.util.List;
+
+import static racingcar.Input.InputView.trialNumber;
+
+/* Car 전부 관리하는 클래스
+* List<Car> 을 관리
+*  최대 5개까지
+*  중복 검사
+*  */
+public class Cars {
+    public static List<Car> cars;
+    public Cars(Car car) {
+    }
+}
