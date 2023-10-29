dab94d75af00bc19606589be72594034ae2d7cce
kongji <kongji4092@gmail.com>
2023-10-29 15:30:44 +0900

    feat: 최종 우승자 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,12 @@
 package racingcar.view;
 
+import racingcar.controller.dto.GameResultDto;
+import racingcar.controller.dto.RoundResponseDto;
+
 public class OutputView {
     public static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ASK_GAME_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String START_GAME_ROUND_MESSAGE = "실행결과";
 
     public static void printStartGame(){
         System.out.println(START_GAME_MESSAGE);
@@ -21,4 +25,7 @@ public class OutputView {
         System.out.println(output);
     }
 
+    public static void printGameWinner(GameResultDto gameResultDto) {
+        System.out.print("최종 우승자 : " + gameResultDto.convertOutput());
+    }
 }
