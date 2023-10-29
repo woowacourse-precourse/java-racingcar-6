59a4861910a960fbaef3a0d4d588ca6933020f1c
Sun! <87172228+GangSSun@users.noreply.github.com>
2023-10-29 18:11:19 +0900

    설계 내용 변경

---
+++ src/main/java/racingcar/GameData.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+/*
+    게임 진행 시 필요한 데이터 처리, 저장 클래스(데이터 private)
+    자동차 명
+    자동차 전진 거리
+    우승자 명
+ */
+public class GameData {
+}
---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+/*
+    게임 진행 클래스
+    - 게임 시작 부터 종료까지 별도 클래스들로 진행 되도록 구현
+ */
+public class RacingCarGame {
+}
