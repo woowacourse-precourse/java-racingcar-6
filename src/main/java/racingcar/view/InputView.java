fe1bd7c7dac936ae22684972828f856dcaf51ad1
tam31 <wxogud3377@naver.com>
2023-10-29 16:29:08 +0900

    Change(InputView) : 입력받은 데이터 검사 접근법 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,18 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.InputValidation;
+
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE= "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_PLAY_NUMBER = "시도할 회수는 몇회인가요?";
 
-    public String carName(){
+    InputValidation inputValidation = new InputValidation();
+
+    public String[] carName(){
         System.out.println(INPUT_CAR_NAME_MESSAGE);
-        return Console.readLine();
+        String questionAnswer = Console.readLine();
+        return inputValidation.checkedInputValidation(questionAnswer);
     }
 
     public String playNumber(){
