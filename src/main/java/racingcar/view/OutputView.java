d18677e0e87b229473684c3e3b6dc88cbd52819d
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 02:57:38 +0900

    refactor: 메서드 간 공백 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,10 +4,12 @@ public class OutputView {
     public void requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
     }
+
     public void requestTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
-    public void printStartResult(){
+
+    public void printStartResult() {
         System.out.println("실행 결과");
     }
 }
