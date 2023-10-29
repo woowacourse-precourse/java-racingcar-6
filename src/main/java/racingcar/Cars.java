205a5a08f4caa23c3311c0150543d3bd441d7404
freekyr7529 <freekyr7529@gmail.com>
2023-10-29 11:17:34 +0900

    feat: 자동차 이름에 대한 입력값 검증 기능 구현
    - 사용자가 입력한 이름 중 5자리를 넘는 이름이 존재하는 경우
    - 사용자가 값을 입력하지 않은 경우
    
    - 위 두 가지 상황을 검증하는 기능을 구현하였다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -13,5 +13,18 @@ public class Cars {
         cars = Arrays.stream(splitNames)
                 .map(name -> new Car(name.strip()))
                 .collect(Collectors.toList());
+        validateCarNames();
+    }
+
+    private void validateCarNames() {
+        if (cars.size() == 1 && firstElement().getName() == "") {
+            throw new IllegalArgumentException("어떠한 이름도 입력되지 않았습니다!");
+        }
+
+        for (Car car : cars) {
+            if (car.isNameLongerThenCriteria(5)) {
+                throw new IllegalArgumentException("5자리 이하의 이름을 입력하시오!");
+            }
+        }
     }
 }
