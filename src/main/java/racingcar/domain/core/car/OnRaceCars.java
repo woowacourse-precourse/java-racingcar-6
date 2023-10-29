235020d6cd97d7c459ec1410096a4a1ba9cee248
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:21:49 +0900

    feat(domain/core): OnRaceCars 구현

---
+++ src/main/java/racingcar/domain/core/car/OnRaceCars.java
@@ -0,0 +1,27 @@
+package racingcar.domain.core.car;
+
+import java.util.Iterator;
+import java.util.List;
+import racingcar.exceptions.runtime.NotFoundSourceException;
+
+public class OnRaceCars implements Iterable<OnRaceCar> {
+
+    private final List<OnRaceCar> cars;
+
+    public OnRaceCars(List<OnRaceCar> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public Iterator<OnRaceCar> iterator() {
+        return cars.iterator();
+    }
+
+    public Long getPosition(OnRaceCar car) {
+        return cars.stream()
+            .filter(onRaceCar -> onRaceCar.equals(car))
+            .map(OnRaceCar::getPosition)
+            .findFirst()
+            .orElseThrow(NotFoundSourceException::new);
+    }
+}
