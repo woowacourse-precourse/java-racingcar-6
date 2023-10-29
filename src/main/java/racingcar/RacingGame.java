8f15d233933229d49aae956e1f43adce4ffab6bf
daneng4 <daneng4@naver.com>
2023-10-29 12:51:14 +0900

    Fix: 변수 선언 위치 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,8 +9,8 @@ public class RacingGame {
     private static final String PROGRESS_RESULT = "실행결과";
     private Player player = new Player();
     private HashMap<String, Integer> cars;
-    private int attemptNumber;
-    private int carNumber;
+    private int attemptNumber, carNumber, valueOfCar, randomNumber, updateCarValue;
+    private String carName;
 
     public void GameStart(){
         printMessage(REQUIRE_CAR_NAME_MESSAGE);
@@ -27,19 +27,21 @@ public class RacingGame {
 
     public void gameProgress(int attemptNumber){
         printMessage(PROGRESS_RESULT);
+        carName = "";
         for(int i = 0; i<attemptNumber; i++){
             for(int j = 0; j<carNumber; j++){
-                String carName = player.getCarName(j);
+                carName = player.getCarName(j);
                 eachCarPickRandomNumber(carName);
                 printProgress(carName);
             }
             System.out.println();
         }
+        checkWinner(carName);
     }
 
     public void printProgress(String carName){
         System.out.printf("%s : ", carName);
-        int valueOfCar = cars.get(carName);
+        valueOfCar = cars.get(carName);
         for(int i = 0; i<valueOfCar; i++){
             System.out.print("-");
         }
@@ -47,11 +49,15 @@ public class RacingGame {
     }
 
     public void eachCarPickRandomNumber(String carName){
-        int randomNumber = player.getRandomNumber();
+        randomNumber = player.getRandomNumber();
         if (randomNumber >= 4){
-            int updateCarValue = cars.get(carName) + 1;
+            updateCarValue = cars.get(carName) + 1;
             cars.put(carName,updateCarValue);
         }
+    }
+
+    public void checkWinner(String carName){
+        
     }
 
     public void printMessage(String message){
