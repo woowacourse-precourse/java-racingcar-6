12dd478bac48ceb14f5a8e231d81c3fb9c46b28f
ldw <leehan02@hanyang.ac.kr>
2023-10-30 04:19:01 +0900

    fix(racingcar): 자동차 이름의 유효성을 검증하는 메서드에 입력된 이름이 공백인지 검사하는 로직 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,19 +2,35 @@ package racingcar;
 
 public class Car {
     private final String name;
-    private int countMove;
     private static final String MOVE = "-";
-
     private String totalMove = "";
+    private static final int LIMIT_NAME_LEN = 5;
 
     public Car(String name) {
         this.name = name;
+        validateCarName();
     }
 
-    public void Move(boolean isMove) {
+    public String getName() {
+        return this.name;
+    }
+
+    public int getTotalMove() {
+        return totalMove.length();
+    }
+
+    public void validateCarName() {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessages.BLANK_NAME_ERROR_MESSAGE);
+        }
+        if (name.length() > LIMIT_NAME_LEN) {
+            throw new IllegalArgumentException(ErrorMessages.TOO_LONG_CAR_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    public void moveOrStop(boolean isMove) {
         System.out.print(name + " : ");
         if (isMove) {
-            countMove++;
             totalMove += MOVE;
         }
 
--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,21 +1,22 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class InputValidator {
 
     private static final int LIMIT_NAME_LEN = 5;
     private static final int MINIMUM_TRY_NUM = 1;
-//    public static void validateCarName(String[] carsName) {
-//        for (String name : carsName) {
-//            if (name.length() > LIMIT_NAME_LEN)
-//                throw new IllegalArgumentException();
-//        }
-//    }
+//    private static final String TOO_LONG_CAR_NAME_ERROR_MESSAGE = "자동차 이름은 5자 이하여야 합니다.";
+//    private static final String TOO_LOW_TRY_NUM_ERROR_MESSAGE = "시도 횟수는 최소 1회 이상이어야 합니다.";
+//    private static final String NOT_NUMBER_ERROR_MESSAGE = "시도 횟수는 숫자만 입력해야 합니다.";
+//    private static final String BLANK_NAME_ERROR_MESSAGE = "자동차 이름은 최소 한 글자 이상의 공백이 아닌 문자여야 합니다.";
 
-    public static void validateCarName(String carName) {
-        if (carName.length() > LIMIT_NAME_LEN) {
-            throw new IllegalArgumentException();
+
+    public static void validateCarsName(String[] carsName) {
+        for (String carName : carsName) {
+            if (carName.isBlank()) {
+                throw new IllegalArgumentException(ErrorMessages.BLANK_NAME_ERROR_MESSAGE);
+            }
+            if (carName.length() > LIMIT_NAME_LEN)
+                throw new IllegalArgumentException();
         }
     }
 
@@ -24,11 +25,11 @@ public class InputValidator {
         try {
             tryNum = Integer.parseInt(stringTryNum);
             if (tryNum < MINIMUM_TRY_NUM) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ErrorMessages.TOO_LOW_TRY_NUM_ERROR_MESSAGE);
             }
         }
         catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessages.NOT_NUMBER_ERROR_MESSAGE);
         }
         return tryNum;
     }
