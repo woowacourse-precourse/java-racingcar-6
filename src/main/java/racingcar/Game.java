7460ec8f8ca6888d90e4855b590fd9b3179ed279
Hajuuu <hpt0127@naver.com>
2023-10-29 19:42:11 +0900

    feat(Game): 무작위 값이 4 이상일 경우 전진하는 함수 moveForward 작성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,12 +1,23 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Game {
 
-    public int randomNumber() {
+    private int randomNumber() {
         int randomNum = Randoms.pickNumberInRange(0, 9);
         return randomNum;
+    }
+
+    private void moveForward(List<User> users) {
+        for (User u : users) {
+            int randomNum = randomNumber();
+            if (randomNum < 4) {
+                continue;
+            }
+            u.length += 1;
+        }
     }
 
 
