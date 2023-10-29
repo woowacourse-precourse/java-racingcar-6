4a98fa7dce7a67447c7c6eb9b0da6945776477ed
Gyeongho Yang <chchch1213@naver.com>
2023-10-29 18:03:41 +0900

    feat(global): 공백인 자동차 이름이 있을 때 출력할 에러 메세지 추가

--- src/main/java/racingcar/global/ErrorMessage.java
+++ src/main/java/racingcar/global/ErrorMessage.java
@@ -1,7 +1,8 @@
 package racingcar.global;
 
 public enum ErrorMessage {
-    DUPLICATED_CAR_NAME("중복된 자동차 이름이 존재 합니다.");
+    DUPLICATED_CAR_NAME("중복된 자동차 이름이 존재 합니다."),
+    EMPTY_CAR_NAME("공백인 자동차 이름이 존재 합니다.");
 
     private final String message;
 
