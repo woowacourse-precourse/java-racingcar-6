f0f77a2007988e665e9e492c901627d7fbd6edbf
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-29 21:25:03 +0900

    feat : 검증자 요구사항 1번 기능 구현 완료
    TDD 기법에 따라 테스트 코드에 대한 기능 구현 ( BLUE )
    두 가지 테스트 케이스 통과
    docs/README.md 에 이모지 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,6 +1,21 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
     public void validateUserInputIsCorrectFormat(String userInput){
+        List<String> toValidateRacerNameList = Arrays.asList(userInput.split(","));
+        for (String validateRacerName : toValidateRacerNameList){
+            validateRacerNameIsContainSpecificSymbol(validateRacerName);
+        }
+    }
+    private void validateRacerNameIsContainSpecificSymbol(String racerName){
+        for (char element : racerName.toCharArray()){
+            if (Character.isLetterOrDigit(element)){
+                continue;
+            }
+            throw new IllegalArgumentException("입력된 값이 형식에 맞지 않습니다!");
+        }
     }
 }
