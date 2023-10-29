9a4aee038ba594e065b04d05b941802547a20854
WooYoungDoo <duden5000@naver.com>
2023-10-29 18:01:30 +0900

    feat (OutputView) : 게임 진행 시 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,27 @@ package racingcar.view;
 
 public class OutputView {
 
+    private static final String DISTANCE_STRING_VALUE = "-";
+    private static final String BLANK_LINE = "";
+
     public void printInputCarNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printInputNumberMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printExecutionResultMessage() {
+        printBlankLine();
+        System.out.println("실행 결과");
+    }
+
+    public void printRoundResult(String carName, int distance) {
+        System.out.printf("%s : %s\n", carName, DISTANCE_STRING_VALUE.repeat(distance));
+    }
+
+    public void printBlankLine() {
+        System.out.println(BLANK_LINE);
     }
 }
