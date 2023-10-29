2f8ade5660bdded2d47faa1bd83a15e267c43d14
irerin07 <counterchord89@gmail.com>
2023-10-30 01:00:39 +0900

    #8 애플리케이션 코드 수정 - 게임 흐름대로 진행되도록 애플리케이션 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,33 +1,32 @@
 package racingcar;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import racingcar.input.CarNamesInputReader;
-import racingcar.input.NumberOfMovementInputReader;
-import racingcar.racingcar.Car;
-import racingcar.racingcar.RacingCar;
+import racingcar.ui.input.CarNamesInputReader;
+import racingcar.ui.input.NumberOfMovementInputReader;
+import racingcar.playrule.RandomNumberRacingRule;
+import racingcar.racing.RacingManager;
+import racingcar.racingcar.RacingParticipants;
+import racingcar.ui.result.ResultViewer;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         CarNamesInputReader carNamesInputReader = new CarNamesInputReader();
+        NumberOfMovementInputReader numberOfMovementInputReader = new NumberOfMovementInputReader();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<Car> carList = Stream.of(carNamesInputReader.readLine()
-                        .split(",", -1))
-                .map(e -> new Car(e, 0))
-                .collect(Collectors.toList());
+        RacingManager racingManager = new RacingManager(
+                new RacingParticipants(carNamesInputReader.readLine()),
+                numberOfMovementInputReader.readLine(),
+                new RandomNumberRacingRule()
+        );
 
+        ResultViewer resultViewer = new ResultViewer(racingManager);
 
-        NumberOfMovementInputReader number = new NumberOfMovementInputReader();
+        resultViewer.printResult();
+        while (!racingManager.isEnd()) {
+            racingManager.processRacing();
+            resultViewer.printCurrentPositions();
+        }
 
-        System.out.println("시도할 회수는 몇회인가요?");
-
-        RacingCar racingCar = new RacingCar(carList, Integer.parseInt(number.readLine()));
-
-        System.out.println("실행 결과");
+        resultViewer.printWinner();
 
     }
 }
