6fea4421dffbc7964dd3f2e4dadc239126d3a68c
JongukYang <didwhddnr123@naver.com>
2023-10-29 18:05:48 +0900

    feat: RacingCar 클래스에 현재 위치를 알 수 있는 변수 추가
    랜덤 수 값을 통해 얼마나 이동할지에 대한 메서드도 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
+import racingcar.utils.RandomNumberGenerator;
+
 public class RacingCar {
     private final String name;
+    private int position = 0;
 
     public RacingCar(String name) {
         this.name = name;
@@ -9,5 +12,15 @@ public class RacingCar {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int number) {
+        if (number >= 4) {
+            position++;
+        }
     }
 }
