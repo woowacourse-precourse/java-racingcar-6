17e13fb8aacf87c9b17340fc62cc5d5af82740de
IE-MangChi <hsm983278@gmail.com>
2023-10-29 17:15:57 +0900

    feat: 게임셋팅 기능 추가

--- src/main/java/racingcar/service/RaceGameService.java
+++ src/main/java/racingcar/service/RaceGameService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface RaceGameService {
 
+    public Car startGame(List<String> input);
     public void carMoveCheckAndGo(Car car);
     public List<Integer> findWinner(Car car);
 }
