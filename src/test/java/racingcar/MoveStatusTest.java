6f9d1842bc273dcd4236eadfa0c7cfc73a960efa
KimDabomi <cdabomi@nate.com>
2023-10-29 16:07:00 +0900

    test: Init MoveStatus

---
+++ src/test/java/racingcar/MoveStatusTest.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+class MoveStatusTest {
+    @Test
+    void MoveStatus_인스턴스_생성확인() {
+        MoveStatus moveStatus = new MoveStatus();
+    }
+}
