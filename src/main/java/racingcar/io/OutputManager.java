c2dacac99c75a2c56a7b4ba7c65bcfdf87e2702b
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-29 23:55:51 +0900

    feat: 게임 기본 메시지 관련 출력 메소드 추가

--- src/main/java/racingcar/io/OutputManager.java
+++ src/main/java/racingcar/io/OutputManager.java
@@ -1,4 +1,25 @@
 package racingcar.io;
 
+import racingcar.config.GameMessages;
+
 public class OutputManager {
+    public void printCarNameInputMessage() {
+        printMessage(GameMessages.INPUT_CAR_NAME);
+    }
+    public void printTryCountInputMessage() {
+        printMessage(GameMessages.INPUT_TRY_COUNT);
+    }
+
+    public void printExecutionResultMessage() {
+        printMessage(GameMessages.EXECUTION_RESULT);
+    }
+
+    public void printFinalWinnerMessage(String winnerNames) {
+        printMessage(GameMessages.FINAL_WINNER + winnerNames);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
 }
