452fd3e0647713d12dbe557f7c5694599f1996d7
KiminSon <kimin0331@naver.com>
2023-10-29 15:40:06 +0900

    feat(Application): 이동 횟수 입력 구현
    makePlayCount함수에서 이동할 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,14 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
+    public static int makePlayCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputCount = readLine();
+        int playCount = Integer.parseInt(inputCount);
+
+        return playCount;
+    }
+
     public static void findCarListError(String[] carList) {
         Set<String> uniqueCar = new HashSet<>();
         for (int i = 0; i < carList.length; i++) {
@@ -31,5 +39,6 @@ public class Application {
 
     public static void main(String[] args) {
         String[] carList = makeCarList();
+        int playCount = makePlayCount();
     }
 }
