c1ce2067edd9cf1d0ae46151d54372a95c45df68
jae woo Jung <awfjol2008@naver.com>
2023-10-30 02:11:03 +0900

    refector: 자동차 이름 목록으로 입력된 문자열을 검증하는 메소드 파라미터명 변경
    자동차 이름 목록으로 입력된 문자열은 여러 개의 이름을 포함하므로, carName이란 변수명을 carNames로 변경

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -2,19 +2,19 @@ package racingcar.validator;
 
 public class CarNameValidator {
 
-    public static void validateForString(String carName) {
-        validateEmptyForString(carName);
-        validateContainsCommaForString(carName);
+    public static void validateForString(String carNames) {
+        validateEmptyForString(carNames);
+        validateContainsCommaForString(carNames);
     }
 
-    private static void validateEmptyForString(String carName) {
-        if (carName.isEmpty()) {
+    private static void validateEmptyForString(String carNames) {
+        if (carNames.isEmpty()) {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
         }
     }
 
-    private static void validateContainsCommaForString(String carName) {
-        if (!carName.contains(",")) {
+    private static void validateContainsCommaForString(String carNames) {
+        if (!carNames.contains(",")) {
             throw new IllegalArgumentException("입력값은 쉼표(,)를 기준으로 구분되어야 합니다.");
         }
     }
