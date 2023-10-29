55f9191df8f340a1d9adcff7d6863fbb55c2c8ae
yu-teacher <yusm1231@gmail.com>
2023-10-29 17:11:05 +0900

    refactor : 가독성 문제 해결
    Output 상수의 삭제로 문자열을 직접 출력하도록 바꾸었습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,17 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(Output.START.getText());
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
 
-        System.out.println(Output.TIME.getText());
+        System.out.println("시도할 회수는 몇회인가요?");
         String time = Console.readLine();
 
         Race race = new Race(names);
-        System.out.println(Output.RESULT.getText());
+        System.out.println("\n실행 결과");
         race.playGame(time);
 
-        System.out.print(Output.WINNER.getText());
-        System.out.println(race.findWinner());
+        System.out.print("최종 우승자 : ");
+        System.out.println(race.findWinnerToString());
     }
 }
