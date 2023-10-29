800230f8d4c4a281ccc70c6df9d15cf1968f52ae
seaniiio <cnc4750@naver.com>
2023-10-29 14:34:08 +0900

    feat(User): 사용자에게 자동차를 입력받아 List에 저장하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
+import racingcar.domain.User;
 
 public class Application {
     public static void main(String[] args) {
-
+        User user = new User();
     }
 }
--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -1,7 +1,19 @@
 package racingcar.domain;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class User {
-    public void User() {
+    public List<String> cars = new ArrayList<>();
 
+    public User() {
+        InputCarName();
+    }
+    public void InputCarName() {
+        cars = Arrays.asList(Console.readLine().split(","));
+        System.out.println(cars);
     }
 }
+
