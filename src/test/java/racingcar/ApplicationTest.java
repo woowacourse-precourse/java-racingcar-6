7da02dddef47d0352325431ce05aef4d6b51631a
StrangeMin <mim0107@nate.com>
2023-10-30 00:46:04 +0900

    test : 자동차 이름 입력받기 테스트 코드 작성
    테스트 목록:
    - 5글자를 초과 하는 이름 존재 시 에러 발생 여부 확인
    - 1글자 미만인 이름 존재 시 에러 발생 여부 확인
    - 유효한 이름들로만 구성됐을 경우 정상적으로 분할되는지 확인
    - List에 들어있는 이름들을 LinkedHashMap에 정상적으로 저장되는지 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,10 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -29,6 +34,56 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void processCarNames_5_글자_초과_하는_이름_존재할_때_예외_발생(){
+        Game game = new Game();
+        String carNames = "abcdef,ab";
+
+        assertThatThrownBy(() -> game.processCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void processCarNames_1_글자_미만인_이름_존재할_때_예외_발생(){
+        Game game = new Game();
+        String carNames = ",ab";
+
+
+        assertThatThrownBy(() -> game.processCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void processCarNames_유효한_이름들일_때_예외_발생_안함(){
+        Game game = new Game();
+        String carName = "abc,de";
+
+        assertDoesNotThrow(() -> game.processCarNames(carName));
+    }
+
+    @Test
+    void splitAndConvertToList_유효한_사용자_입력일_때_쉼표를_기준으로_분할(){
+        Game game = new Game();
+        String carNamesInput = "pobi,woni,jun";
+        List<String> carList = game.splitAndConvertToList(carNamesInput, ",");
+
+        assertThat(carList).contains("woni", "pobi", "jun");
+    }
+
+    @Test
+    void storeCarNames_List에_들어_있는_값들_LinkedHashMap에_저장(){
+        Game game = new Game();
+        List<String> carList = Arrays.asList("pobi", "woni");
+        game.storeCarNames(carList);
+
+        LinkedHashMap<String, Integer> cars = game.getCars();
+        assertThat(cars.size()).isEqualTo(2);
+        assertThat(cars.containsKey("pobi"));
+        assertThat(cars.containsKey("woni"));
+        assertThat(cars.get("pobi")).isEqualTo(0);
+        assertThat(cars.get("woni")).isEqualTo(0);
     }
 
     @Override
