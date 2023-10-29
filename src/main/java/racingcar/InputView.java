e1d2ede2d37a85ff20b95381ee2a561894a2a35e
KimDabomi <cdabomi@nate.com>
2023-10-29 14:49:54 +0900

    feat: Add car duplicate value exception

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,6 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputView {
     MagicVariable magicVariable = new MagicVariable();
@@ -11,14 +14,21 @@ public class InputView {
         System.out.println(magicVariable.RACE_START_MESSAGE);
         String carNameInput = Console.readLine();
         String[] carNames = carNameInput.split(",");
+        checkCarNameExceptions(carNames);
+        return String.join(",", carNames);
+    }
+
+    private void checkCarNameExceptions(String[] carNames) {
+        Set<String> uniqueNames = new HashSet<>(Arrays.asList(carNames));
+        if (uniqueNames.size() != carNames.length) {
+            throw new IllegalArgumentException(magicVariable.CAR_DUPLICATE_ERROR_MESSAGE);
+        }
 
         for (String carName : carNames) {
             if (!carName.matches(magicVariable.CAR_NAME_RULE)) {
                 throw new IllegalArgumentException(magicVariable.CAR_NAME_ERROR_MESSAGE);
             }
         }
-
-        return String.join(",", carNames);
     }
 
     public int getTryCount() {
--- src/main/java/racingcar/MagicVariable.java
+++ src/main/java/racingcar/MagicVariable.java
@@ -6,6 +6,7 @@ public class MagicVariable {
     public final int FORWARD_NUM = 4;
     public final String RACE_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final String CAR_NAME_RULE = "^[a-z]{1,5}$";
+    public final String CAR_DUPLICATE_ERROR_MESSAGE = "자동차 이름에 중복된 값이 있습니다.";
     public final String CAR_NAME_ERROR_MESSAGE = "자동차 이름은 공백없이 1자 이상 5자 이하의 소문자만 가능합니다.";
     public final String ASK_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     public final String TRY_COUNT_RANGE_ERROR_MESSAGE = "시도할 횟수는 1회 이상이어야 합니다.";
