bdd2abae6e8966900594c3471c1111244c9b7941
남수연 <rnrmf0721@gmail.com>
2023-10-29 10:08:06 +0900

    feat(Constant): 입출력되는 텍스트를 상수로 선언

---
+++ src/test/java/racingcar/Constant.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public final class Constant {
+
+    static final String INIT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String INIT_NUMBER = "시도할 회수는 몇회인가요?";
+    static final String EXECUTION_REAULT = "실행 결과";
+    static final String FINAL_WINNER = "최종 우승자";
+    static final String COLON = " : ";
+    static final String DISTANCE_DRIVER = "-";
+}
