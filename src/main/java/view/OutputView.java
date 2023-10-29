9103d7f61bfa9da822159523d9f97ea2e0e9de7a
yeahdy <yejine98@naver.com>
2023-10-29 19:26:27 +0900

    feat(OutputView): 클래스 생성, 모듈 개발

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,19 @@
+package view;
+
+/**
+ * Model의 데이터를 사용자에게 표시하는 클래스
+ *
+ * @author Ellie
+ */
+public class OutputView {
+
+    /***
+     *사용자에게 메세지를 출력하는 메소드
+     *
+     * @param message
+     */
+    public void print(String message){
+        System.out.println(message);
+    }
+
+}
