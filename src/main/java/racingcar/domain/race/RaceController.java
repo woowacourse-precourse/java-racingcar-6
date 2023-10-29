eeafc6bfac1405ba3eba156f1b21143de4f1dc6e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-30 01:55:44 +0900

    feat : RaceController > 게임 진행 후 우승자 구하기 호출

--- src/main/java/racingcar/domain/race/RaceController.java
+++ src/main/java/racingcar/domain/race/RaceController.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.domain.race;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
@@ -24,17 +24,25 @@ public class RaceController {
 
     public void startRaceGame() {
         // 자동차 이름들 입력 안내 뷰
-        RaceView.guideRaceCarInput();
+        System.out.println(RaceView.printGuideRaceCarInput());
 
         // 입력에 따른 자동차들 생성
         String raceCarNames = Console.readLine();
         List<Car> cars = raceService.getCarsFromInput(raceCarNames);
 
         // 시도횟수 입력 안내 뷰
-        RaceView.guideRoundInput();
+        System.out.println(RaceView.printGuideRoundInput());
 
         // 입력에 따른 시도횟수 생성
         String roundInput = Console.readLine();
         int round = raceService.getRoundFromInput(roundInput);
+
+        // 시도 횟수만큼 게임 진행
+        System.out.println(RaceView.printGuideResult());
+        raceService.raceForRounds(cars, round);
+
+        // 우승자 구하기
+        List<Car> winners = raceService.getWinners(cars);
+        System.out.println(RaceView.printWinners(winners));
     }
 }
