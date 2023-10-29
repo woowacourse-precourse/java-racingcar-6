1979bd2b3c0cd557256dae6fa140f659fa9634e6
ChaeAg <dydkglel123@gmail.com>
2023-10-29 14:42:10 +0900

    refactor(Application): Application에서 수행하던 로직을 컨트롤러에서 수행하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,77 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.validate.InvalidInputException;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-
-import java.util.HashMap;
-import java.util.Map;
+import racingcar.controller.RacingCarController;
 
 public class Application {
-    private static final int RANDOM_START_NUMBER = 0;
-    private static final int RANDOM_LAST_NUMBER = 9;
-    private static final int MIN_FORWARD_NUMBER = 4;
-    private static final OutputView outputView = new OutputView();
-    private static final InputView inputView = new InputView();
-    private static final InvalidInputException invalidInputException = new InvalidInputException();
-    private static final Map<String, Integer> carMap = new HashMap<>();
-
     public static void main(String[] args) {
-        // 자동차 입력 안내 메세지 출력
-        outputView.carNameInputMessage();
-
-        // 자동차 이름 입력
-        String input = inputView.carNamesInput();
-        invalidInputException.checkInput(input);
-
-        String[] inputCarNames = input.split(",");
-        for (String carName : inputCarNames) {
-            carMap.put(carName, 0);
-        }
-
-        // 시도할 횟수 입력 안내 메세지 출력
-        outputView.timesInputMessage();
-
-        // 시도할 횟수 입력
-        String times = inputView.timesInput();
-        invalidInputException.checkTimes(times);
-
-        // '실행 결과' 메세지 출력
-        outputView.resultMessage();
-
-        // 우승자 판정 변수 선언
-        int maxForward = 0;
-
-        // 경주 시작
-        for (int i = 0; i < Integer.parseInt(times); i++) {
-            for (int k = 0; k < inputCarNames.length; k++) {
-                int randomNumber = Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_LAST_NUMBER);
-                if (randomNumber >= MIN_FORWARD_NUMBER) {
-                    carMap.put(inputCarNames[k], carMap.get(inputCarNames[k]) + 1);
-                }
-                if (carMap.get(inputCarNames[k]) > maxForward) {
-                    maxForward = carMap.get(inputCarNames[k]);
-                }
-
-                outputView.CarNameOutput(inputCarNames[k]);
-                outputView.resultCountOutput(carMap.get(inputCarNames[k]));
-            }
-            outputView.printOutput();
-        }
-
-        // '최종 우승자 : ' 메세지 출력
-        outputView.winnerMessage();
-
-        // 최종 우승자 출력
-        int i = 0;
-        for (String carNames : inputCarNames) {
-            if (carMap.get(carNames) == maxForward) {
-                if (i == 0) outputView.Winner(carNames);
-                else outputView.Winners(carNames);
-                i++;
-            }
-        }
-        outputView.printOutput();
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,94 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.utils.RandomUtils;
+import racingcar.validate.InvalidInputException;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingCarController {
+    private static final int RANDOM_START_NUMBER = 0;
+    private static final int RANDOM_LAST_NUMBER = 9;
+    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final InvalidInputException invalidInputException = new InvalidInputException();
+    private static final Cars cars = new Cars();
+    private static final RandomUtils randomUtils = new RandomUtils();
+
+    public void run() {
+        // 자동차 입력 안내 메세지 출력
+        outputView.carNameInputMessage();
+
+        // 자동차 이름 입력
+        String inputCarNames = carNamesInput();
+
+        // 자동차 객체 리스트 생성
+        createCars(inputCarNames);
+
+        // 시도할 횟수 입력 안내 메세지 출력
+        outputView.timesInputMessage();
+
+        // 시도할 횟수 입력
+        int times = timesInput();
+
+        // '실행 결과' 메세지 출력
+        outputView.resultMessage();
+
+        // 경주 시작
+        for(int i=0; i<times; i++) {
+            startRacing();
+        }
+
+        // '최종 우승자 : ' 메세지 출력
+        outputView.winnerMessage();
+
+        // 최종 우승자 출력
+        printFinalWinner(cars.getWinner());
+    }
+
+    private void createCars(String inputCarNames) {
+        List<Car> inputCarList = Arrays.stream(inputCarNames.split(","))
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        cars.addCars(inputCarList);
+    }
+
+    private void startRacing() {
+        for (Car car : cars.getCarList()) {
+            cars.oneResult(car, randomUtils.getRandomNumber(RANDOM_START_NUMBER, RANDOM_LAST_NUMBER));
+            outputView.CarNameOutput(car.getName());
+            outputView.resultCountOutput(car.getForwardCount());
+        }
+        outputView.printOutput();
+    }
+
+    private String carNamesInput() {
+        String input = inputView.carNamesInput();
+        invalidInputException.checkInput(input);
+        return input;
+    }
+
+    private int timesInput() {
+        String times = inputView.timesInput();
+        invalidInputException.checkTimes(times);
+        return Integer.parseInt(times);
+    }
+
+    private void printFinalWinner(List<Car> winnerList) {
+        for(int i = 0; i< winnerList.size(); i++) {
+            if(i == 0) {
+                outputView.Winner(winnerList.get(i).getName());
+            }
+            if(i > 0) {
+                outputView.Winners(winnerList.get(i).getName());
+            }
+        }
+        outputView.printOutput();
+    }
+}
