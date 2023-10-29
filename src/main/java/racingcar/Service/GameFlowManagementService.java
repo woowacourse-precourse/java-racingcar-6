c825fc33a7c6766b1cafc62075b854e4a6c9483f
hayannn <dlgkdis801@naver.com>
2023-10-29 15:39:57 +0900

    feat(GameFlowManagementService): 게임 진행 관리 기능 구현

--- src/main/java/racingcar/Service/GameFlowManagementService.java
+++ src/main/java/racingcar/Service/GameFlowManagementService.java
@@ -1,4 +1,23 @@
 package racingcar.Service;
 
+import racingcar.Domain.Car;
+import racingcar.Domain.Game;
+import racingcar.Repository.GameRepository;
+import racingcar.Util.RandomNumberGenerator;
+
 public class GameFlowManagementService {
+    private final GameRepository gameRepository = new GameRepository();
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    private static final int SET_DEFAULT_INT = -1;
+    public void playRacingCarGameRound(Game game) {
+        randomDriveForward(game);
+        game.incrementCurrentTryCount();
+    }
+    private void randomDriveForward(Game game) {
+        for (Car car : game.getCarList()) {
+            if (RandomNumberGenerator.DRIVEABLE_VALUE <= randomNumberGenerator.getRandomNumber()) {
+                car.driveForward(1);
+            }
+        }
+    }
 }
