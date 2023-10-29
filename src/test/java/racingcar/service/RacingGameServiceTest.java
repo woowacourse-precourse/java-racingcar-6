7f277f73cd517ae077d710c42ab576760798086d
PARKHWANYEONG <welcome0828@naver.com>
2023-10-29 09:44:53 +0900

    test(service): 최종 우승자를 찾는 기능 테스트 추가
    -최종 우승자가 한 명일 때와 여러 명일 때 반환받는 문자열의 형식이 다르므로 두 가지 경우의 테스트 코드를 추가하였습니다.

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -58,4 +58,38 @@ class RacingGameServiceTest {
         assertThat(looloo.getPosition()).isEqualTo(2);
     }
 
+    /**
+     *  임의로 생성한 테스트 숫자가 1,6,4,7 이었을 때
+     *  첫번째 숫자 1 빼고 나머지는 모두 숫자 4 이상으로
+     *  2,3,4번째 자동차만 전진 조건(숫자 4 이상일 경우)에 해당하여
+     *  position이 1씩 증가하여 2,3,4번째 자동차가 공동으로 최종 우승하게 된다.
+     */
+    @Test
+    void 최종_우승자를_찾는_테스트_우승자가_여러_명일_경우() {
+        //given
+        String[] carNames = {"hwan","looloo","woo","hee"};
+        racingGameService.init(carNames);
+        racingGameService.playGame(1);
+
+        //when
+        String winningCarStr = racingGameService.getWinningCar();
+
+        //then
+        assertThat(winningCarStr).isEqualTo("looloo, woo, hee");
+    }
+
+    @Test
+    void 최종_우승자를_찾는_테스트_우승자가_한_명일_경우() {
+        //given
+        String[] carNames = {"hwan","looloo"};
+        racingGameService.init(carNames);
+        racingGameService.playGame(1);
+
+        //when
+        String winningCarStr = racingGameService.getWinningCar();
+
+        //then
+        assertThat(winningCarStr).isEqualTo("looloo");
+    }
+
 }
