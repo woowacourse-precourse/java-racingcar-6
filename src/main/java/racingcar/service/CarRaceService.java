3f47ad58ef9efe1f26b8465ade626fc4b6c8bb1d
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-29 18:25:20 +0900

    feat:난수를 받아 자동차가 전진할지말지 판단하는 메서드

--- src/main/java/racingcar/service/CarRaceService.java
+++ src/main/java/racingcar/service/CarRaceService.java
@@ -1,17 +1,31 @@
 package racingcar.service;
 
 import racingcar.domain.Cars;
+import racingcar.domain.RandomGenerator;
 
+/**
+ * 애플리케이션 로직을 담당하는 클래스
+ */
 public class CarRaceService {
+
     private final Cars cars;
 
     public CarRaceService(Cars cars) {
         this.cars = cars;
     }
 
+
     public void addCars(String input) {
         cars.saveCars(input);
     }
 
+    public void moveCars() {
+
+    }
+
+    public boolean shouldMoveForward() {
+        int randomNumber = RandomGenerator.createRandomNumber();
+        return randomNumber >= 4;
+    }
 
 }
