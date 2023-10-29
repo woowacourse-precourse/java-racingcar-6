9024100a8c6b29eb8e8ada473a1949dbd1584abc
yeon409 <yeon000409@gmail.com>
2023-10-29 16:12:58 +0900

    feature : add calculate winner and print logic
    desc : calculate max distance from racing cars and filter the winners

--- src/main/java/racingcar/printer/RacingCarPrinter.java
+++ src/main/java/racingcar/printer/RacingCarPrinter.java
@@ -1,5 +1,10 @@
 package racingcar.printer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.assertj.core.util.VisibleForTesting;
+import racingcar.service.RacingCar;
+
 public class RacingCarPrinter {
     public static void askRacingCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +12,38 @@ public class RacingCarPrinter {
 
     public static void askHowManyNumberToPlay() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printMessageBeforePlay() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printCurrentDistanceStatus(List<RacingCar> racingCars) {
+        racingCars.forEach(racingCar ->
+                System.out.println(getCurrentStatusWithBar(racingCar)));
+        System.out.println();
+    }
+
+    @VisibleForTesting
+    protected static String getCurrentStatusWithBar(RacingCar racingCar) {
+        return racingCar.getName() + " : " + convertDistanceToBar(racingCar.getAdvanceDistance());
+    }
+
+    @VisibleForTesting
+    protected static String convertDistanceToBar(int advanceDistance) {
+        return "-".repeat(Math.max(0, advanceDistance));
+    }
+
+    public static void printWinnerOfPlay(List<RacingCar> calculateWinner) {
+        String message = "최종 우승자 : ";
+        message += convertListToNamesOfOneLine(calculateWinner);
+        System.out.println(message);
+    }
+
+    @VisibleForTesting
+    protected static String convertListToNamesOfOneLine(List<RacingCar> calculateWinner) {
+        return calculateWinner.stream()
+                .map(RacingCar::getName)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,10 +1,15 @@
 package racingcar.service;
 
 import static racingcar.input.RacingCarInputReader.getRacingCarsFromConsole;
+import static racingcar.printer.RacingCarPrinter.printCurrentDistanceStatus;
 import static racingcar.printer.RacingCarPrinter.printMessageBeforePlay;
+import static racingcar.printer.RacingCarPrinter.printWinnerOfPlay;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.assertj.core.util.VisibleForTesting;
 import racingcar.input.RacingCarInputReader;
 
 public class RacingCarGameService {
@@ -15,14 +20,14 @@ public class RacingCarGameService {
         List<RacingCar> racingCars = getRacingCarsFromConsole();
         int numberToPlay = RacingCarInputReader.getPlayNumberFromConsole();
         advanceRacingCarsUntilPlayNumber(racingCars, numberToPlay);
+        printWinnerOfPlay(calculateWinner(racingCars));
     }
 
     private static void advanceRacingCarsUntilPlayNumber(List<RacingCar> racingCars, int numberToPlay) {
         printMessageBeforePlay();
         while(numberToPlay-- > 0) {
             advanceEachRacingCars(racingCars);
-            // TODO : implement below
-//            printCurrentDistanceStatus(racingCars);
+            printCurrentDistanceStatus(racingCars);
         }
     }
 
@@ -31,5 +36,25 @@ public class RacingCarGameService {
             racingCar.advanceByNumber(
                     Randoms.pickNumberInRange(
                             RANDOM_GENERATE_START_NUM, RANDOM_GENERATE_END_NUM)));
+    }
+
+    @VisibleForTesting
+    protected static List<RacingCar> calculateWinner(List<RacingCar> racingCars) {
+        int maxDistance = calculateMaxDistance(racingCars);
+        return racingCars.stream()
+                .filter(racingCar -> racingCar.getAdvanceDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
+
+    @VisibleForTesting
+    protected static int calculateMaxDistance(List<RacingCar> racingCars) {
+        if (racingCars.isEmpty())
+            return 0;
+
+        return racingCars.stream()
+                .map(RacingCar::getAdvanceDistance)
+                .sorted(Comparator.reverseOrder())
+                .toList()
+                .get(0);
     }
 }
