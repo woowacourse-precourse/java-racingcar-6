221e5efdd8d65bbbbcdd1f12ecc4b4c9a78d11a9
DaehoSon <thseogh135@gmail.com>
2023-10-29 14:18:47 +0900

    refact: 입력 값의 유효성 검사를 하는 코드 메소드 분리
    [내용]
    - init()에서 처리하던 코드를 각각 분리
      - private String[] getCarNameArrayFromInput()
      - private int getMoveCountFromInput()
    - 각 메소드 함수에서 입력 받고 유효성 검사한 뒤 반환해줌

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,20 +7,30 @@ public class Game {
     public final String ANNOUNCEMENT_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final String ANNOUNCEMENT_INPUT_MOVE_COUNT = "시도할 회수는 몇회인가요?";
 
+    public String[] carNameArray;
+    public int moveCount;
+
     public Game() {
         init();
     }
 
     private void init() {
-        System.out.println(this.ANNOUNCEMENT_INPUT_CAR_NAMES);
-        String[] carNameArray = Console.readLine().split(",");
-        Validator.validateCarNameArray(carNameArray);
+        this.carNameArray = getCarNameArrayFromInput();
+        this.moveCount = getMoveCountFromInput();
+    }
 
+    private String[] getCarNameArrayFromInput() {
+        System.out.println(this.ANNOUNCEMENT_INPUT_CAR_NAMES);
+        String[] inputCarNameArray = Console.readLine().split(",");
+        Validator.validateCarNameArray(inputCarNameArray);
+        return inputCarNameArray;
+    }
+
+    private int getMoveCountFromInput() {
         System.out.println(this.ANNOUNCEMENT_INPUT_MOVE_COUNT);
         String inputMoveCountString = Console.readLine();
         Validator.validateMoveCount(inputMoveCountString);
+        return Integer.parseInt(inputMoveCountString);
     }
-
-
 
 }
