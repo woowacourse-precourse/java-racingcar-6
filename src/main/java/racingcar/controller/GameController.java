640a0eaad5d57bf793ac6014875cca919ae6b754
Infikei <hckhong@naver.com>
2023-10-30 01:30:35 +0900

    feat: 입력받은 자동차 이름들을 쉼표 기준으로 구분하는 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,7 +1,20 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GameController {
     public void playGame() {
-        // TODO: 게임 진행 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        List<String> racingCarNames = splitRacingCarNames(inputView.inputRacingCarNames());
+    }
+
+    static public List<String> splitRacingCarNames(String racingCarNames) {
+        return Arrays.asList(racingCarNames.split(","));
     }
 }
