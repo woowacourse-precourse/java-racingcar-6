bbd29b5f63aa5d5b4402f14c0116da37da9c589d
bamm2 <xxknu88@naver.com>
2023-10-29 18:54:44 +0900

    feat: 상수클래스 생성

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public final class Constant {
+    public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String GAME_COUNT_QUESTION = "시도할 회수는 몇회인가요?";
+    public static final String GAME_RESULT_MESSAGE = "실행 결과";
+    public static final String GAME_WINNER_MESSAGE = "최종 우승자 : ";
+    public static final Integer RANDOM_NUMBER_MIN = 0;
+    public static final Integer RANDOM_NUMBER_MAX = 9;
+    public static final Integer MOVE_LOWER_BOUND = 4;
+
+    private Constant() {
+    }
+}
