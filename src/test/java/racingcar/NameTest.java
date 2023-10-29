da9725b3a85cf4d5937c03b04ced03e839d52152
dd <12202340@inha.edu>
2023-10-29 21:32:10 +0900

    Refactor: NameTest에서 NameValidator를 검증하도록 수정

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -5,6 +5,7 @@ import domain.CarName;
 import domain.Cars;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import validator.NameValidator;
 import view.InputView;
 
 import java.io.ByteArrayInputStream;
@@ -16,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NameTest {
 
-    @DisplayName("문자열을 구분하는 기능")
+    @DisplayName("문자열을 구분하고 자동차 객체를 저장하는 기능")
     @Test
     void inputCarNamesTest() {
         System.setIn(new ByteArrayInputStream("pobi,woni,jun".getBytes()));
@@ -27,48 +28,29 @@ public class NameTest {
                 add("jun");
             }
         };
+        Cars cars = new Cars();
 
-        List<String> nameList = InputView.inputCarNames();
-
-        assertThat(answer).isEqualTo(nameList);
-    }
-
-    @DisplayName("자동차 저장 테스트")
-    @Test
-    void insertCarTest() {
-        List<String> names = new ArrayList<>() {
-            {
-                add("pobi");
-                add("woni");
-                add("jun");
-            }
-        };
-        Cars answer = new Cars() {
-            {
-                insertCar(new Car(new CarName("pobi")));
-                insertCar(new Car(new CarName("woni")));
-                insertCar(new Car(new CarName("jun")));
-            }
-        };
-
-        Cars result = new Cars();
-        names.forEach(name -> result.insertCar(new Car(new CarName(name))));
-
-        List<String> answerCarNames = answer.getCars().stream()
+        InputView.inputCarNames()
+                .forEach(name -> cars.insertCar(new Car(new CarName(name))));
+        List<String> carNames = cars.getCars().stream()
                 .map(Car::getName)
                 .toList();
-        List<String> resultCarNames = answer.getCars().stream()
-                .map(Car::getName)
-                .toList();
-        assertThat(answerCarNames).isEqualTo(resultCarNames);
+
+        assertThat(answer).isEqualTo(carNames);
     }
 
     @DisplayName("[예외] 자동차 이름 글자수 예외 테스트")
     @Test
     void validateLengthTest() {
-        System.setIn(new ByteArrayInputStream("pobiii,woni,jun".getBytes()));
+        List<String> nameList = new ArrayList<>() {
+            {
+                add("pobiii");
+                add("woni");
+                add("jun");
+            }
+        };
 
-        assertThatThrownBy(() -> InputView.inputCarNames())
+        assertThatThrownBy(() -> NameValidator.validateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 1자 이상, 5자 이하의 이름을 입력해 주세요.");
     }
@@ -76,9 +58,15 @@ public class NameTest {
     @DisplayName(("[예외] 허용되지 않는 문자 포함 예외 테스트"))
     @Test
     void validateContainTest() {
-        System.setIn(new ByteArrayInputStream("po bi,woni,jun".getBytes()));
+        List<String> nameList = new ArrayList<>() {
+            {
+                add("po bi");
+                add("woni");
+                add("jun");
+            }
+        };
 
-        assertThatThrownBy(() -> InputView.inputCarNames())
+        assertThatThrownBy(() -> NameValidator.validateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 포함될 수 없는 문자가 포함되어 있습니다.");
     }
@@ -86,9 +74,16 @@ public class NameTest {
     @DisplayName("[예외] 중복된 이름 예외 테스트")
     @Test
     void validateNameDupTest() {
-        System.setIn(new ByteArrayInputStream("pobi,pobi,woni,jun".getBytes()));
+        List<String> nameList = new ArrayList<>() {
+            {
+                add("pobi");
+                add("pobi");
+                add("woni");
+                add("jun");
+            }
+        };
 
-        assertThatThrownBy(() -> InputView.inputCarNames())
+        assertThatThrownBy(() -> NameValidator.validateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 자동차 이름은 중복될 수 없습니다.");
     }
