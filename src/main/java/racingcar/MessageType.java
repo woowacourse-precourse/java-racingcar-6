a34dee85e7424e0735fe40eb510b463103bac2f0
OikimiO <etkim02@naver.com>
2023-10-29 15:03:16 +0900

    feat: 입력했던 값에 대한 출력 추가

--- src/main/java/racingcar/MessageType.java
+++ src/main/java/racingcar/MessageType.java
@@ -3,7 +3,8 @@ package racingcar;
 public enum MessageType{
     INPUT_CAR_NAME_PRINT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_CAR_RACING_COUNT_PRINT("시도할 회수는 몇회인가요?"),
-
+    INPUT_DATA_PRINT("%s"),
+    
     OUPUT_RACE_RESULT_PRINT("실행 결과"),
     OUPUT_IMPLEMENTATION_RESULT_PRINT("%s : %s");
 
