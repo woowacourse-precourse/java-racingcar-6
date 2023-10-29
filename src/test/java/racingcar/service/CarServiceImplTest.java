6319f7ebae9c8ee924681fc194fb0414508d00f9
inpink <inpink@kakao.com>
2023-10-29 19:42:27 +0900

    test: Car 이름의 길이가 5를 초과하면 IllegalArgumentException 예외 발생하는지 테스트
    길이가 5를 초과하는 Car 이름으로 Car 객체를 만들고, carSerivce.join했을 때 IllegalArgumentException가 발생해야함.

--- src/test/java/racingcar/service/CarServiceImplTest.java
+++ src/test/java/racingcar/service/CarServiceImplTest.java
@@ -2,6 +2,7 @@ package racingcar.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,8 +48,20 @@ public class CarServiceImplTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    public void Car_이름의_길이가_5초과하면_예외발생() {
+        //given
+        final Car car1 = Car.create("abcdef");
+
+        //when
+        final RuntimeException result = assertThrows(IllegalArgumentException.class,
+                () -> carService.join(car1));
+
+        //then
+        assertThat(result).isNotNull();
+    }
 
     //TODO: Car에서 get~~(getPosition)같은것을 쓸경우 이거를 위한 테스트 따로하기(위치 이동시킨다음  위치값 저장 잘되는지)
-    // 이건 Car Model이 아닌 Service에서 하는게 맞을듯. Model의 method들도 테스트해야할지 고민하기
+    // 이건 Car Model이 아닌 Service에서 하는게 맞을듯.
 
 }
