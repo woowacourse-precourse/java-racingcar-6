51c2b1e6f743ccef93f23c44138ccaaa4b4100c1
sangeun-b <baek0007@algonquinlive.com>
2023-10-30 01:55:13 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,8 +1,5 @@
 package domain;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Car {
 
     private String name;
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,9 +2,6 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class InputView {
 
     public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
