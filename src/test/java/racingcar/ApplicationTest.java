0b6305b3fe4e565c85a248259a12fc311bc9a32c
booyaChael <chaeeul2022@gmail.com>
2023-10-29 10:47:12 +0900

    test(ApplicationTest): 입력값(이름, 시도횟수)에 대한 예외처리 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -23,11 +25,30 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("이름이 6자 이상이면 예외가 발생한다.")
     @Test
-    void 이름에_대한_예외_처리() {
+    void 이름에_대한_예외_처리_1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("이름이 쉼표로 구분되어 있지 않으면 예외가 발생한다.")
+    @Test
+    void 이름에_대한_예외_처리_2() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("시도 횟수에 숫자가 아닌 값을 입력하면 예외가 발생한다.")
+    @Test
+    void 시도횟수에_대한_예외_처리() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi javaji", "k"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
