9e80f62e9dcc02c21eeaea758f6db6442ecc55dc
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 23:38:50 +0900

    feat: moveCarsForward 메소드 작성
    isRandomForwardMove 메소드를 통해 알맞는 조건이 달성되면 해당 자동차를 앞으로 전진시키는 moveCarsForward 메소드를 작성했습니다.

--- src/main/java/racingcar/utils/RacingCarGame.java
+++ src/main/java/racingcar/utils/RacingCarGame.java
@@ -1,6 +1,7 @@
 package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.dto.RacingCar;
 
 public class RacingCarGame {
     private static int farthestDistance;
@@ -12,6 +13,12 @@ public class RacingCarGame {
     private static boolean isRandomForwardMove() {
         int randomValue = Randoms.pickNumberInRange(0, 9);
         return randomValue >= 4;
+    }
+
+    public static void moveCarsForward(RacingCar car) {
+        if (isRandomForwardMove()) {
+            car.moveForword();
+        }
     }
 
 }
