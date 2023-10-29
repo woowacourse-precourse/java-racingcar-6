4f9778e2ab46c663849e003d359b0d570082f4e0
happyryan <tjdgks120813@gmail.com>
2023-10-29 15:17:57 +0900

    refactor(user User): rename methods for clarity
    Changed method names for improved clarity and readability. Renamed 'playerNameInput' to 'inputPlayerName' and 'TryNumberInput' to 'inputTryNumber'. The new method names better represent the functionality of the methods, enhancing code understanding.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class User {
-    public static List<String> playerNameInput() {
+    public static List<String> inputPlayerName() {
         String players = Console.readLine();
 
         List<String> playersArray = new ArrayList<>(Arrays.asList(players.split(",")));
@@ -16,7 +16,7 @@ public class User {
         return playersArray;
     }
 
-    public static String tryNumberInput() {
+    public static String inputTryNumber() {
         String moveNumber = Console.readLine();
 
         ExceptionHandler.rasieMoveNumberInputException(moveNumber);
