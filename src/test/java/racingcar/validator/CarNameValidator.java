a4546e6b6391b6871c3146d1cd69411de534c407
jae woo Jung <awfjol2008@naver.com>
2023-10-30 02:03:03 +0900

    feat: 자동차 이름 목록 입력값에 쉼표(, )가 포함되어 있는지 검증하는 기능 구현

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -7,4 +7,10 @@ public class CarNameValidator {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
         }
     }
+
+    private static void validateContainsComma(String carName) {
+        if (!carName.contains(",")) {
+            throw new IllegalArgumentException("입력값은 쉼표(,)를 기준으로 구분되어야 합니다.");
+        }
+    }
 }
