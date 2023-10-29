234c3ee19b9ae9315414ca550388de546a2c3db1
cyberjam <jamsince1994@naver.com>
2023-10-30 08:08:48 +0900

    feat: add method generating cars

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        generateCars();
+    }
+
+    private static void generateCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = readLine();
+        String[] carNames = carNamesInput.split(",");
+        System.out.println(Arrays.toString(carNames));
     }
 }
