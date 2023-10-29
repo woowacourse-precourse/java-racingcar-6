7218ab4fe713980a57c555ab16726edee3f8f4a4
hil679 <hil679922@gmail.com>
2023-10-30 02:58:30 +0900

    Feat(Validator): Create skeleton code

---
+++ src/main/java/racingcar/validator/Validator.java
@@ -0,0 +1,24 @@
+package racingcar.validator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Validator {
+
+    public boolean isTryNumInteger(String tryNum) {
+
+    }
+
+    public boolean isTryNumPositiveNum(Integer tryNum) {
+
+    }
+
+    public boolean checkCarNameLength(String carName) {
+
+    }
+
+    public boolean checkDuplicateCarName(List<String> carNameList) {
+
+    }
+}
