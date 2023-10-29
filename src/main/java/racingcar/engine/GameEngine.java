20afc2305fab873a7aa07ae2ef6de4783b794022
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-10-29 19:59:38 +0900

    feat(GameEngine): 플레이어가 전진하는지를 판단한다
    GameEngineValidator# 음수이거나 숫자가아닌지 검증한다.
    NumberGenerator# 숫자 만드는 것을 부른다.
    숫자를 판단한다
      0부터3까지 멈춤
      4부터 9까지는 전진

--- src/main/java/racingcar/engine/GameEngine.java
+++ src/main/java/racingcar/engine/GameEngine.java
@@ -9,8 +9,13 @@ import java.util.List;
 public class GameEngine {
     private static final String PLAYER_NAME_DELIMITER = ",";
     private static final Long INIT_SCORE = 0L;
+    private static final Long PLUS_SCORE = 1L;
+    private static final int NUMBER_GENERATOR_START_INCLUSIVE = 0;
+    private static final int NUMBER_GENERATOR_END_INCLUSIVE = 9;
+    private static final int ADD_SCORE_POINT = 4;
     private final List<Score> scores;
     private final NumberGenerator numberGenerator;
+    private final GameEngineValidator gameEngineValidator;
 
     public GameEngine(String readLine, NumberGenerator numberGenerator, GameEngineValidator gameEngineValidator) {
         gameEngineValidator.validateIsNotNull(readLine);
@@ -18,6 +23,22 @@ public class GameEngine {
         gameEngineValidator.validatePlayerNames(playerNames);
         this.scores = playerNames.stream().map(playerName -> new Score(playerName, INIT_SCORE)).toList();
         this.numberGenerator = numberGenerator;
+        this.gameEngineValidator = gameEngineValidator;
     }
 
+    public void run(String playCount) {
+        gameEngineValidator.validateIsPositiveNumber(playCount);
+        long playEndTime = Long.parseLong(playCount);
+        for (long playTime = 0; playTime < playEndTime; playTime++) {
+            updateScore();
+        }
+    }
+
+    private void updateScore() {
+        for (Score score : scores) {
+            if (numberGenerator.generate(NUMBER_GENERATOR_START_INCLUSIVE, NUMBER_GENERATOR_END_INCLUSIVE) < ADD_SCORE_POINT)
+                continue;
+            score.addScore(PLUS_SCORE);
+        }
+    }
 }
