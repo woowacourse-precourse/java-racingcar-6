98a386f6929a82bdc2b62a19613897dc05c90ae5
jongheonleee <qwefghnm1212@gmail.com>
2023-10-29 17:53:38 +0900

    [refacotr] 테스트 코드 작성을 위한 구조 개선

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,11 +2,12 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements RandomUtil{
 
     private static final Integer START_NUMBER = 0;
     private static final Integer END_NUMBER = 9;
 
+    @Override
     public int generate() {
         return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
---
+++ src/test/java/racingcar/domain/MovingStrategyTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+import racingcar.util.RandomNumberGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovingStrategyTest {
+
+    @Test
+    void 랜덤_숫자_생성_테스트() {
+        // given
+        // when
+        // then
+
+    }
+
+
+}
