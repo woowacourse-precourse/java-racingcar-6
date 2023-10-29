0985bbd8c8debff0a59bf92c8fe0c694f2a73bb3
SeongUk52 <rkfka7849@gmail.com>
2023-10-29 17:23:58 +0900

    test(Car): Car 클래스의 나머지 메서드에 대한 테스트 코드 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -15,4 +15,18 @@ public class CarTest {
         car.move();
         assertEquals(car.getProgressToInt(), 2);
     }
+
+    @DisplayName("getNameToString(): 이름을 Stirng으로 반환하는 getter")
+    @Test
+    void getNameTest() {
+        Car car = new Car("붕붕이");
+        assertEquals(car.getNameToString(), "붕붕이");
+    }
+
+    @DisplayName("getProgressToInt(): 진행도를 int로 반환하는 getter")
+    @Test
+    void getProgressTest() {
+        Car car = new Car("붕붕이");
+        assertEquals(car.getProgressToInt(), 0);
+    }
 }
