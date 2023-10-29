57f9cb78fbadcb7aaa465c32b8c0c32e893421c8
YongHyeon <pyh4616@naver.com>
2023-10-30 00:23:26 +0900

    Feat: (RacingGameService 클래스), 자동차를 전진시키는 기능 구현

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,5 +1,7 @@
 package racingcar.service;
 
+import java.util.LinkedHashMap;
+
 public class RacingCarGameService {
 
     private static final int MOVING_FORWARD_STANDARD = 4;
@@ -9,5 +11,9 @@ public class RacingCarGameService {
             return true;
         }
         return false;
+    }
+
+    public void moveForward(LinkedHashMap<String, Integer> progressStatus, String carName) {
+        progressStatus.put(carName, progressStatus.get(carName) + 1);
     }
 }
