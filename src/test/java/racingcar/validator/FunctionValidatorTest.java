3830069d0ff874524fa1a733c0b5f247b0a0e70d
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-29 14:32:25 +0900

    test: 기능 목록에 대한 테스트 코드 추가

--- src/test/java/racingcar/validator/FunctionValidatorTest.java
+++ src/test/java/racingcar/validator/FunctionValidatorTest.java
@@ -1,4 +1,81 @@
 package racingcar.validator;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import validator.InputValidator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
+
 public class FunctionValidatorTest {
+    private final InputValidator validator = new InputValidator();
+
+    @Test
+    void 자동차이름_빈칸_입력() {
+        assertThatThrownBy(() -> validator.checkCarNameValidate(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+    }
+
+    @Test
+    void 시도회수_빈칸_입력() {
+        assertThatThrownBy(() -> validator.checkNumOfAttempt(""))
+                .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+    }
+
+    @Test
+    void 자동차이름_0자일_경우() {
+        assertThatThrownBy(() -> validator.checkCarNameValidate(",a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+
+        assertThatThrownBy(() -> validator.checkCarNameValidate("abc,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+
+        assertThatThrownBy(() -> validator.checkCarNameValidate(",,,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+    }
+
+    @Test
+    void 자동차이름_5자_초과한_경우() {
+        assertThatThrownBy(() -> validator.checkCarNameValidate("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.CAR_NAME_EXCEED_FIVE_WORDS);
+    }
+
+    @Test
+    void 자동차이름_공백_포함된_경우() {
+        assertThatThrownBy(() -> validator.checkCarNameValidate(" a,bc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+
+        assertThatThrownBy(() -> validator.checkCarNameValidate("abc "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+
+        assertThatThrownBy(() -> validator.checkCarNameValidate(" , , "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.INPUT_CONTAIN_BLANK);
+    }
+
+    void 시도횟수_숫자가_아닌_문자_포함된_경우() {
+        assertThatThrownBy(() -> validator.checkNumOfAttempt("123a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.ATTEMPT_NUMBER_CONTAIN_CHAR_OTHER_THAN_NUMBER);
+
+        assertThatThrownBy(() -> validator.checkNumOfAttempt("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.ATTEMPT_NUMBER_CONTAIN_CHAR_OTHER_THAN_NUMBER);
+
+        assertThatThrownBy(() -> validator.checkNumOfAttempt("five"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(validator.ATTEMPT_NUMBER_CONTAIN_CHAR_OTHER_THAN_NUMBER);
+
+
+    }
 }
