b702dfd0a76dba0f397a9e65f2270eb16d96d6c1
OikimiO <etkim02@naver.com>
2023-10-29 17:01:01 +0900

    test: 차량 출발 메서드 구분 진행
    - 입력/비교/출력 메서드 구분

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,53 +1,76 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.CheckMoveAndStopTest.CheckMoveAndStop;
 
 public class RacingCarTest {
-    private List<StringBuilder> winCount = new ArrayList<>();
-    private int maxCount = 0;
 
-    @Test
-    void gameStart(){
-        OutputCarRace.print(MessageType.INPUT_CAR_NAME_PRINT);
-        InputCarRace.inputCarNames("차이름1,차이름2,차이름3,차이름4");
+    static class RacingCar{
+        private List<StringBuilder> winCount = new ArrayList<>();
+        private int maxCount = 0;
 
-        OutputCarRace.printf(MessageType.INPUT_CAR_RACING_COUNT_PRINT);
-        InputCarRace.inputMove("2");
-        System.out.println();
-
-        OutputCarRace.print(MessageType.OUTPUT_RACE_RESULT_PRINT);
-
-        for (String carName : InputCarRace.getCarNumbers()) {
-            this.winCount.add(new StringBuilder());
+        public void startGame(){
+            inputData();
+            compareCarName();
+            printWinner();
         }
 
-        for (int i = 0; i < InputCarRace.getMoveCount(); i++) {
-            for (int j = 0; j < InputCarRace.getCarNumbers().size(); j++) {
-                if(CheckMoveAndStop.isMove()){
-                    this.winCount.set(j,this.winCount.get(j).append("-"));
+        private void inputData() {
+            InputCarRace.init();
+            OutputCarRace.print(MessageType.INPUT_CAR_NAME_PRINT);
+            InputCarRace.inputCarNames(readLine());
 
-                    if(this.maxCount < winCount.get(j).length()){
-                        this.maxCount = winCount.get(j).length();
-                    }
-                }
-
-                OutputCarRace.printf(MessageType.OUTPUT_IMPLEMENTATION_RESULT_PRINT, InputCarRace.getCarNumbers().get(j), this.winCount.get(j).toString());
-            }
+            OutputCarRace.printf(MessageType.INPUT_CAR_RACING_COUNT_PRINT);
+            InputCarRace.inputMove(readLine());
+            setWinCount();
             System.out.println();
         }
 
-        StringBuilder winners = new StringBuilder();
-
-        for (int i = 0; i < winCount.size(); i++) {
-            if(this.maxCount == winCount.get(i).length()){
-                winners.append(InputCarRace.getCarNumbers().get(i)).append(", ");
+        private void setWinCount(){
+            for (String carName : InputCarRace.getCarNumbers()) {
+                this.winCount.add(new StringBuilder());
             }
         }
 
-        System.out.println("최종 우승자 : " + winners.substring(0,winners.length()-2));
+        private void compareCarName() {
+            OutputCarRace.print(MessageType.OUTPUT_RACE_RESULT_PRINT);
 
+            for (int i = 0; i < InputCarRace.getMoveCount(); i++) {
+                for (int j = 0; j < InputCarRace.getCarNumbers().size(); j++) {
+                    if(CheckMoveAndStop.isMove()){
+                        this.winCount.set(j,this.winCount.get(j).append("-"));
+
+                        if(this.maxCount < winCount.get(j).length()){
+                            this.maxCount = winCount.get(j).length();
+                        }
+                    }
+
+                    OutputCarRace.printf(MessageType.OUTPUT_IMPLEMENTATION_RESULT_PRINT,
+                            InputCarRace.getCarNumbers().get(j),
+                            this.winCount.get(j).toString());
+                }
+                System.out.println();
+            }
+        }
+
+        private void printWinner() {
+            StringBuilder winners = new StringBuilder();
+
+            for (int i = 0; i < winCount.size(); i++) {
+                if(this.maxCount == winCount.get(i).length()){
+                    winners.append(InputCarRace.getCarNumbers().get(i)).append(", ");
+                }
+            }
+
+            OutputCarRace.printf(MessageType.OUTPUT_WINNERS, winners.substring(0,winners.length()-2));
+        }
+    }
+
+    @Test
+    void startGame(){
+        new RacingCar().startGame();
     }
 }
