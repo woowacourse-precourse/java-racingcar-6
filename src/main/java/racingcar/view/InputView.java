a3864eb7979d0a3cf6677413e7f15dd36e0e450c
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 21:08:48 +0900

    remove(InputView): println을 통해 상수에서 사용된 \n 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,11 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final  String INSERT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
-    private static final  String INSERT_TRY_NUMBER = "시도할 회수는 몇회인가요?\n";
+    private static final  String INSERT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final  String INSERT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
 
     public String readCarNames() {
-        System.out.print(INSERT_NAMES);
+        System.out.println(INSERT_NAMES);
         return Console.readLine();
     }
 
