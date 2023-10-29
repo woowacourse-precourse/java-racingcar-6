9400fdcbe50b7888dbfa55bd64ac40610cc85cdf
Sully <2dcoder@naver.com>
2023-10-29 21:53:00 +0900

    feat(TryCountValidator): 시도할 횟수가 정수가 아니라면 예외가 발생하는 메서드 추가

--- src/main/java/racingcar/domain/TryCountValidator.java
+++ src/main/java/racingcar/domain/TryCountValidator.java
@@ -7,11 +7,20 @@ public class TryCountValidator {
     public TryCountValidator(String tryCount) {
         this.tryCount = tryCount;
         validateTryCountExists();
+        validateTryCountIsInteger();
     }
 
     public void validateTryCountExists() {
         if (tryCount.isBlank()) {
             throw new IllegalArgumentException("시도할 횟수를 입력해주세요.");
+        }
+    }
+
+    public void validateTryCountIsInteger() {
+        try {
+            Integer.parseInt(tryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 정수여야 합니다.");
         }
     }
 
