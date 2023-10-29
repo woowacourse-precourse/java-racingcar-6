2bf069e0e1813a6a0f4ee92b2717a67d1550146a
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-29 15:16:11 +0900

    feat: 중복된 이름 입력에 대한 검사 기능 구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,9 +1,20 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
     public static void nameLengthCheck(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("잘못된 입력");
+        }
+    }
+
+    public static void nameDuplicatenCheck(List<String> names) {
+        int initLength = names.size();
+        HashSet<String> deduplicationSet = new HashSet<>(names);
+        if (initLength != deduplicationSet.size()) {
+            throw new IllegalArgumentException("중복되지 않은 이름 입력");
         }
     }
 }
