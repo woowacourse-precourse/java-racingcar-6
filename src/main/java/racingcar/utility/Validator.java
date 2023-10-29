986f39be89115e9f337c024f31ff0f227c508369
gumgu <bienlee@naver.com>
2023-10-29 17:08:11 +0900

    Feat : 입력값 검증 기능 구현 - 입력된 이름의 글자수 범위(5자 이하) 검증

--- src/main/java/racingcar/utility/Validator.java
+++ src/main/java/racingcar/utility/Validator.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 public class Validator {
 
+    private static final int MAX_NAME_LENGTH = 5;
+
     public static boolean isBlank(String input) {
         return input == null || input.isBlank();
+    }
+
+    public static boolean isInNameRange(String input) {
+        return input.length() <= 5;
     }
 
 }
