17ba2fa46647ad854663c6692b59a9205f71a546
honee8583 <honee8583@naver.com>
2023-10-29 17:35:44 +0900

    test(ApplicationTest): add test case of inputting car names

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,16 +1,33 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void getCars_자동차이름_저장() {
+        String carNames = "andy,cindy,suzy";
+        Map<String, String> forwardMap = new HashMap<>();
+        for (String car : carNames.split(",")) {
+            forwardMap.put(car.trim(), "");
+        }
+
+        assertEquals(forwardMap.size(), 3);
+        for (String key: forwardMap.keySet()) {
+            assertEquals(forwardMap.get(key), "");
+        }
+    }
 
     @Test
     void 전진_정지() {
