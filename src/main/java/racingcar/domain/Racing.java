731e8ff0d52642d4b3556216f1a6e84f0ac4db1e
dsadara <chaehyun.forest@gmail.com>
2023-10-29 15:17:06 +0900

    feat: 레이싱 생성시 입력값 검증 로직 추가
    feat: 쉼표로 구분 된 문자열 공백 제거
    refactor: 배열 대신 리스트 사용

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -2,8 +2,8 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Racing {
     private List<Car> cars = new ArrayList<>();
@@ -33,20 +33,35 @@ public class Racing {
 
     public static Racing createRacingWithInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carArr = Console.readLine().split(",");
+        List<String> carList = Arrays.stream(Console.readLine().split(","))
+                .map(String::trim).collect(Collectors.toList());
+        if (!validateCreateRacingCarInput(carList)) {
+            throw new IllegalArgumentException("중복되지 않는 자동차 이름을 입력해주세요.");
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
-        int turn = Integer.valueOf(Console.readLine());
+        int turn = Integer.parseInt(Console.readLine());
+        if (turn <= 0) {
+            throw new IllegalArgumentException("0보다 큰 회수를 입력해주세요.");
+        }
 
         List<Car> cars = new ArrayList<>();
-        for (String name : carArr) {
+        for (String name : carList) {
             cars.add(new Car(name));
         }
 
         return new Racing(cars, turn);
     }
 
-    private static void validateCreateRacingInput() {
-
+    private static boolean validateCreateRacingCarInput(List<String> carList) {
+        // 자동차 이름 중복 체크
+        Set<String> set = new HashSet<>();
+        for (String carStr : carList) {
+            if (set.contains(carStr)) {
+                return false;
+            }
+            set.add(carStr);
+        }
+        return true;
     }
 }
