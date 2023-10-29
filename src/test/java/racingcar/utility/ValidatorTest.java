d79cc5d9eeb1e60778636fa423eba8defe984697
gumgu <bienlee@naver.com>
2023-10-29 17:35:05 +0900

    Test : 입력값 검증기능 테스트 - 입력된 이름이 같은 경우 검증 테스트

--- src/test/java/racingcar/utility/ValidatorTest.java
+++ src/test/java/racingcar/utility/ValidatorTest.java
@@ -3,6 +3,9 @@ package racingcar.utility;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,6 +42,34 @@ class ValidatorTest {
         assertThat(fiveLength).isTrue();
         assertThat(oneLength).isTrue();
         assertThat(zeroLength).isTrue();
+    }
+
+    @Test
+    void 전달받은_이름들_중_중복된_값이_있는_경우_true_반환() {
+        List<String> koreanNames = Arrays.asList("가", "나", "가", "다");
+        List<String> englishNames = Arrays.asList("pobi", "woni", "jun", "pobi");
+
+        assertThat(Validator.hasSameName(koreanNames)).isTrue();
+        assertThat(Validator.hasSameName(englishNames)).isTrue();
+    }
+
+    @Test
+    void 전달받은_이름들_중_중복된_값이_없는_경우_false_반환() {
+        List<String> koreanNames = Arrays.asList("가", "나", "다", "라");
+        List<String> englishNames = Arrays.asList("pobi", "woni", "jun", "gugi");
+
+        assertThat(Validator.hasSameName(koreanNames)).isFalse();
+        assertThat(Validator.hasSameName(englishNames)).isFalse();
+    }
+
+    @Test
+    void 중복값_체크_다양한_예시() {
+        List<String> notSame = Arrays.asList("가", "", "    ", "가나다", "^%&");
+        List<String> same = Arrays.asList("1", "quankei", "!%&(", "1", "0", "0");
+
+        assertThat(Validator.hasSameName(notSame)).isFalse();
+        assertThat(Validator.hasSameName(same)).isTrue();
+
     }
 
 
