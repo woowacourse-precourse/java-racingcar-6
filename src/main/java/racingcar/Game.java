a8d5201c179ec0d3bc05564e17ed2ee457cfb9b6
vega <vega99c@naver.com>
2023-10-29 19:34:21 +0900

    feat(Game): Car인스턴스 생성 메서드 구현
    - inputCarsName메서드가 정상적으로 다 통과되었을 시 createCarList호출

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -10,6 +11,11 @@ public class Game {
     final int COUNT_MINIMUM_CAR = 2;
     final String SEPARATOR_CAR_NAME = ",";
     final String MESSAGE_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    List<Car> carList;
+
+    public Game() {
+        carList = new ArrayList<Car>();
+    }
 
     public void startGame() {
         System.out.print(MESSAGE_START);
@@ -24,13 +30,21 @@ public class Game {
         }
 
         String[] arrayNames = inputNames.split(SEPARATOR_CAR_NAME);
-        List<String> namesResult = Arrays.asList(arrayNames);
+        List<String> carNameList = Arrays.asList(arrayNames);
 
-        if (!validateCarNamesLength(namesResult)) {
+        if (!validateCarNamesLength(carNameList)) {
             throw new IllegalArgumentException(String.format("%d대 이상의 이름을 입력해야 합니다", COUNT_MINIMUM_CAR));
         }
-        if (!validateCarNameDuplication(namesResult)) {
+        if (!validateCarNameDuplication(carNameList)) {
             throw new IllegalArgumentException("중복되는 이름이 있습니다.");
+        }
+
+        createCarList(carNameList);
+    }
+
+    private void createCarList(List<String> carNameList) {
+        for (String carName : carNameList) {
+            carList.add(new Car(carName));
         }
     }
 
