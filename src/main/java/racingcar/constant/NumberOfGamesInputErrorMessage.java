c2ab84e8937d2cda8043832e77b72ad1e0aeb25d
wonjun <wonjon0930@daum.net>
2023-10-29 22:17:21 +0900

    feat: add feature to check user input about number of games is not empty

--- src/main/java/racingcar/constant/NumberOfGamesInputErrorMessage.java
+++ src/main/java/racingcar/constant/NumberOfGamesInputErrorMessage.java
@@ -1,6 +1,7 @@
 package racingcar.constant;
 
 public class NumberOfGamesInputErrorMessage {
+    public static final String INPUT_IS_EMPTY = "입력값 오류 : 시도 횟수는 적어도 한 자리 수 이상의 입력이어야 합니다.";
     public static final String INPUT_IS_NOT_NUMERIC_TYPE = "입력값 오류 : 시도 횟수는 숫자 형식의 입력이어야 합니다.";
-
+    
 }
--- src/main/java/racingcar/validator/NumberOfGamesInputValidator.java
+++ src/main/java/racingcar/validator/NumberOfGamesInputValidator.java
@@ -4,7 +4,14 @@ import racingcar.constant.NumberOfGamesInputErrorMessage;
 
 public class NumberOfGamesInputValidator {
     public void validate(String userInput) {
+        isNotEmpty(userInput);
         isNumeric(userInput);
+    }
+
+    private void isNotEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException(NumberOfGamesInputErrorMessage.INPUT_IS_EMPTY);
+        }
     }
 
     private void isNumeric(String userInput) {
