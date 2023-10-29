674837ef82bdb2e7ad32e818229f2d38de956de8
YuJeongYun <yunyj99@naver.com>
2023-10-29 22:12:13 +0900

    refactor: 자동차(Car) 클래스에 dist 변수 setter 추가
    이동 거리는 계속해서 바뀌므로 dist를 변경할 수 있도록 setter를 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -13,12 +13,14 @@ public class Car {
     public Car() {
 
     }
+
     public Car(String name) {
         name = name.trim();
         checkCarNameCondition(name);
 
-        this.name=name;
+        this.name = name;
     }
+
     public Car(String name, int dist) {
         this(name);
         this.dist = dist;
@@ -32,13 +34,17 @@ public class Car {
         return dist;
     }
 
-    private void checkCarNameCondition(String name){
+    public void setDist(int dist) {
+        this.dist = dist;
+    }
+
+    private void checkCarNameCondition(String name) {
         // 자동차 이름이 길이 제한을 초과했을 경우 예외 발생
-        if(name.length() > GameMessage.CAR_NAME_LENGTH_MAX_SIZE) {
+        if (name.length() > GameMessage.CAR_NAME_LENGTH_MAX_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_OVER_ERR_MESSAGE);
         }
         // 자동차 이름이 공백일 경우 예외 발생
-        if(name.length() == 0) {
+        if (name.length() == 0) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_BLANK_ERR_MESSAGE);
         }
     }
