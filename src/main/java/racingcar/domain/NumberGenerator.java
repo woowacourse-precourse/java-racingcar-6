d48521492c18e4a6421d512c77f42f4542d221a8
JudyYeon <dev.judykim@gmail.com>
2023-10-29 20:56:21 +0900

    refactor(NumberGenerator): Number Range 매직넘버 수정
    랜덤 수의 범위를 지정할 때 매직넘버 대신 Constance 를 활용하도록 리팩토링했습니다.

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -3,10 +3,13 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+
     private NumberGenerator(){
     }
 
     public static int createRandomNumber(){
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 }
