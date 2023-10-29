242cee319aac09885885795a6785d07dfc804134
lee jihyeon <lejinhy@gmail.com>
2023-10-29 11:21:23 +0900

    feat: 레이싱 실행결과 보여주는 메소드 추가

--- src/main/java/racingcar/ConsolePrint.java
+++ src/main/java/racingcar/ConsolePrint.java
@@ -1,19 +1,24 @@
 package racingcar;
 
 public class ConsolePrint {
+    private StringBuilder moveTrace = new StringBuilder();
     public void requestCarNameInput() {
-        System.out.println(Message.CAR_NAME_REQUEST.getMessage());
+        System.out.println(Message.CAR_NAME_REQUEST.getValue());
     }
 
     public void requestTryCountInput() {
-        System.out.println(Message.TRY_COUNT_REQUEST.getMessage());
+        System.out.println(Message.TRY_COUNT_REQUEST.getValue());
     }
     public void informRacingResult() {
-        System.out.println(Message.RESULT_RACING_INFO.getMessage());
+        System.out.println(Message.RESULT_RACING_INFO.getValue());
     }
 
-    public void racingTryResult(String name, int status) {
-
+    public void racingTryResult(Car name, CarStatus status) {
+        moveTrace.append(name).append(" : ");
+        if (status.equals(CarStatus.MOVE)) {
+            moveTrace.append("-");
+        }
+        System.out.println(moveTrace.toString());
     }
 
     public void finalWinner() {
