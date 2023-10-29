2c5084268c0d03cf1fc3318017e5d3fc22fde1be
pjaehyun <show6756@gmail.com>
2023-10-29 15:32:35 +0900

    feat: 추가 횟수에 숫자가 아닌 다른 값이 들어갔을 때 예외 처리 함수

--- src/main/java/validate/InputValidation.java
+++ src/main/java/validate/InputValidation.java
@@ -1,4 +1,10 @@
 package validate;
 
 public class InputValidation {
+
+    public static void validateNumericInput(String input) {
+        if (!input.matches("^[0-9]+$")){
+            throw new IllegalArgumentException("입력된 값에 문자열이 포함되어있습니다.");
+        }
+    }
 }
