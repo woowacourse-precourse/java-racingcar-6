7f1b9aaebbb439d8c34943fd376657f14ca86237
ts3083 <ts04031@gmail.com>
2023-10-29 17:23:30 +0900

    feat: 실행 결과 메시지 출력

---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class Output {
+
+    private static final String RESULT_MESSAGE = "실행 결과";
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+}
