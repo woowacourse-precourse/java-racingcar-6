31efd52fd6ebb17cd20a830e9be7082182f54abf
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 15:22:36 +0900

    test: verify logic test for value is contain english or korean

--- src/test/java/study/ValidationTest.java
+++ src/test/java/study/ValidationTest.java
@@ -1,9 +1,11 @@
 package study;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.property.ErrorProperty;
 import racingcar.validation.InputValidation;
+import racingcar.validation.NameValidation;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ValidationTest {
@@ -33,4 +35,12 @@ public class ValidationTest {
                 .hasMessage(ErrorProperty.VALUE_CONTAINS_SPACE);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"pob1","pobi@","포비-"})
+    void 이름_입력_값_한글_혹은_영어_검증_로직_테스트(String inputValue){
+        assertThatThrownBy(()->{
+            NameValidation.verifyForRacerNameIsEngOrKor(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorProperty.NAME_VALUE_IS_NOT_CORRECT_FORM);
+    }
 }
