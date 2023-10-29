d37b5672e914310a3ffaba4894c5740d96f3e942
honee8583 <honee8583@naver.com>
2023-10-29 19:51:56 +0900

    test(ApplicationTest): add test case of getting winners list
    최종우승자를 문자열로 반환하는 테스트 케이스 생성:
    
    - Application.java의 getWinners() 메소드를 검증하기 위한 테스트 케이스로 마지막에 winners 리스트를 sort하여 결과값을 비교

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,13 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -20,11 +20,40 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    private static Map<String, String> forwardMap;
+    private static final String[] carNames = new String[]{"andy", "cindy", "suzy"};
+    private static final String[] result = new String[]{"" ,"--", "--"};
+
+    @BeforeAll
+    static void setup() {
+        forwardMap = new HashMap<>();
+        for (int i = 0; i < carNames.length; i++) {
+            forwardMap.put(carNames[i], result[i]);
+        }
+    }
+
+    @Test
+    void 최종우승자_선별() {
+        int max = Integer.MIN_VALUE;
+        List<String> winners = new ArrayList<>();
+        for (String car : forwardMap.keySet()) {
+            int forward = forwardMap.get(car).length();
+            if (forward > max) {
+                max = forward;
+                winners.clear();
+                winners.add(car);
+            } else if (forward == max) {
+                winners.add(car);
+            }
+        }
+        Collections.sort(winners);
+        String result = String.join(",", winners);
+
+        assertEquals(result, "cindy,suzy");
+    }
+
     @Test
     void move_자동차_이동() {
-        String[] carNames = new String[]{"andy", "cindy", "suzy"};
-        String[] result = new String[]{"" ,"--", "--"};
-
         Map<String, String> forwardMap = new HashMap<>();
         for (int i = 0; i < carNames.length; i++) {
             forwardMap.put(carNames[i], "");
