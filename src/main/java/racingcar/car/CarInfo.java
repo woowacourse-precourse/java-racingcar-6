a5bc2f5e0dd4f279483d99616c14ceee1ee66ac1
혜민 <alddlv81@gmail.com>
2023-10-30 00:07:54 +0900

    remove: 자동차 정보 변경 - 불필요한 변수 삭제

--- src/main/java/racingcar/car/CarInfo.java
+++ src/main/java/racingcar/car/CarInfo.java
@@ -2,13 +2,11 @@ package racingcar.car;
 
 public class CarInfo {
     String name;
-    boolean isForward;
     int moveCount;
 
 
     public CarInfo(String name){
         this.name = name;
-        this.isForward = false;
         this.moveCount = 0;
     }
 
