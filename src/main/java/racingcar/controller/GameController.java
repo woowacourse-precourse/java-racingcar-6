077c08404ff8b9aba1c21976f8fd45c5410c6699
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 02:20:37 +0900

    fix(GameController): 객체 변경으로 인한 로직 수정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,7 +3,6 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
-import racingcar.domain.GameData;
 import racingcar.domain.TryCount;
 import racingcar.util.InputParser;
 import racingcar.view.InputView;
@@ -12,17 +11,19 @@ import racingcar.view.OutputView;
 public class GameController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+    private Cars cars;
+    private TryCount tryCount;
 
     public void playGame() {
+        initializeGame();
+
+    }
+
+    private void initializeGame() {
         outputView.requestCarNames();
         final Cars cars = generateCars();
         outputView.requestTryCount();
         final TryCount tryCount = generateTryCount();
-        GameData gameData = initializeGameData(cars, tryCount);
-    }
-
-    private GameData initializeGameData(final Cars cars, final TryCount tryCount) {
-        return new GameData(cars, tryCount);
     }
 
     private Cars generateCars() {
