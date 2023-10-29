117c54cec5b96fd2995893a0bf4a0fecc0c147fa
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 23:04:54 +0900

    test(car): 자동차가 이동하면 위치가 1만큼 증가하는지 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,9 +4,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class CarTest {
+class CarTest {
     @Test
     void 자동차_초기_위치는_0() {
         assertThat(new Car("pobi").getPosition()).isZero();
+    }
+
+    @Test
+    void 자동차가_이동하면_위치는_1만큼_증가() {
+        Car car = new Car("pobi");
+        int past = car.getPosition();
+
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(past + 1);
     }
 }
