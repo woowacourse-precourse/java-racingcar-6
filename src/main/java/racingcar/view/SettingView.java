4201ff2abfc99ee6609290468bcde946e9b9e5aa
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-29 17:24:21 +0900

    style: final 변수 활용

--- src/main/java/racingcar/view/SettingView.java
+++ src/main/java/racingcar/view/SettingView.java
@@ -3,10 +3,13 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 public class SettingView {
 
+    private static final String GET_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String GET_TRIALS = "시도할 회수는 몇회인가요?";
+
    public static void getCarNames() {
-       System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+       System.out.println(GET_NAME);
    }
    public static void getTrialNums() {
-       System.out.println("시도할 회수는 몇회인가요?");
+       System.out.println(GET_TRIALS);
    }
 }
