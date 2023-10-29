48fc84beb3cdb103d338ad9b87ef5280ed30d249
kiwkim <atthenoon@gmail.com>
2023-10-30 04:56:56 +0900

    feat($InputView): make inputCounts.
    make a view that receives input from the user on the number of times to run.

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,18 +1,18 @@
 package racingcar.View;
 
-import racingcar.Model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String MSG_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MSG_COUNT = "시도할 회수는 몇회인가요?";
 
     public String[] inputCarNames(){
-        List<Car> cars = new ArrayList<>();
-
         System.out.println(MSG_NAME);
-        return camp.nextstep.edu.missionutils.Console.readLine().split(",");
+        return Console.readLine().split(",");
+
+    }
+    public String inputCounts(){
+        System.out.println(MSG_COUNT);
+        return Console.readLine();
     }
 }
