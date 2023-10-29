666df2fc80bc3476d2d8df869e5544d06dc02c28
Happeeee <cmg118@naver.com>
2023-10-30 03:26:03 +0900

    Feat: 입력받은 문자열 타입 변환 기능 구현(String -> List)

--- src/main/java/racingcar/ManageRace.java
+++ src/main/java/racingcar/ManageRace.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageRace {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -8,5 +10,13 @@ public class ManageRace {
     private String inputCarName() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         return Console.readLine();
+    }
+
+    private List<String> stringToList(String carNames) {
+        List<String> carNameList = new ArrayList<>();
+        for (String carName : carNames.split(",")) {
+            carNameList.add(carName);
+        }
+        return carNameList;
     }
 }
