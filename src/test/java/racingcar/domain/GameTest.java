60c4e5f6d55dbf7f5a36720590086cf0caefaf7e
slowcloud <sju9417@gmail.com>
2023-10-29 10:28:51 +0900

    refactor(GameTest): RandomNumber와 관련된 코드를 간결하게 변경

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,44 +1,31 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
 
 class GameTest {
-
-    private static MockedStatic<Randoms> randoms;
-
-    @BeforeAll
-    public static void beforeAll() {
-        randoms = Mockito.mockStatic(Randoms.class);
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        randoms.close();
-    }
 
     @Test
     void forwardCars_메시지_반환() {
 
-        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(7);
         List<Car> cars = List.of(new Car("foo"), new Car("bar"));
         Game game = new Game(cars);
 
-        List<CarDistanceMessage> carDistanceMessages = game.forwardCars();
-        List<String> messages = carDistanceMessages.stream().map(CarDistanceMessage::toString).toList();
+        try (MockedStatic<RandomNumber> randomNumber = mockStatic(RandomNumber.class)) {
+            given(RandomNumber.createRandomNumber()).willReturn(new RandomNumber(4));
 
-        assertThat(messages).contains("foo : -", "bar : -");
+            List<CarDistanceMessage> carDistanceMessages = game.forwardCars();
+            List<String> messages = carDistanceMessages.stream().map(CarDistanceMessage::toString).toList();
+
+            assertThat(messages).contains("foo : -", "bar : -");
+        }
 
     }
 
