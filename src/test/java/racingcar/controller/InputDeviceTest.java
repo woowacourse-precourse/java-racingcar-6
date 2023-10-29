a82a771347a24248e5ff46af01cdba91c5147aee
huiseung <whtjddn75@gmail.com>
2023-10-30 02:54:34 +0900

    test : InputDeviceTest 자동차 이름 테스트 추가
    - 자동차 이름 글자 수 확인
    - 자동차 이름 공백 시작 확인

---
+++ src/test/java/racingcar/controller/InputDeviceTest.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputDeviceTest  extends NsTest {
+    @Test
+    void 자동차_이름_글자수_확인() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("javaji"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_공백시작_확인() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ja"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
