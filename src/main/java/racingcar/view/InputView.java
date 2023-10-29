c02c325858db21007a3c6bbce3f911ef3b56fe9f
OhJongMin <jmoh7480@gmail.com>
2023-10-29 17:02:38 +0900

    feat(racingcar):사용자 입력시 Exception 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,21 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.Service.InputException;
 
 public class InputView {
-    public void inputCarNames(){
+    InputException ex = new InputException();
+
+    public List<String> inputCarNames(){
         String userInput = Console.readLine();
+        List<String> names = ex.divideCarNames(userInput);
+        ex.validCarName(names);
+        return names;
     }
 
-    public void inputTryCount(){
+    public int inputTryCount(){
         String userInput = Console.readLine();
+        return ex.changeStringToInt(userInput);
     }
 }
