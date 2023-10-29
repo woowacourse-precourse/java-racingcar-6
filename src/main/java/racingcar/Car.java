c0c222850c2ad1edf48a1924504c6b4d79038a49
Happeeee <cmg118@naver.com>
2023-10-30 05:05:17 +0900

    Refactor: 자동차 name 필드 final로 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,7 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private String name;
+    private final String name;
     private int goCount = 0;
 
     public Car(String name) {
