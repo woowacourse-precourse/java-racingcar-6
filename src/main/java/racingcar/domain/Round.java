ba54a0a12c99326241a295c261db90c7b0797589
SJ70 <seojae70@gmail.com>
2023-10-29 22:20:38 +0900

    Feat: Round 클래스 추가

---
+++ src/main/java/racingcar/domain/Round.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Round {
+
+    private final int rounds;
+    private int currentRound = 0;
+
+    public Round(int rounds) {
+        this.rounds = rounds;
+    }
+
+}
