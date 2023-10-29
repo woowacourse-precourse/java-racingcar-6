3a32cbf40d528d1a933afced7532bb1978f7831b
JH-TT <leejongho9803@gmail.com>
2023-10-29 14:51:55 +0900

    style - 시작 메서드 이름 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,6 @@ import racingcar.view.InputView;
 public class Application {
     public static void main(String[] args) {
         RacingController racingController = new RacingController();
-        racingController.racingReady();
+        racingController.racingProcess();
     }
 }
