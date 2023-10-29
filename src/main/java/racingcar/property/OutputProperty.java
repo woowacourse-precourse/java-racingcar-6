ffabc7295e2662502edd10509d9312404e32f9a6
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 04:37:07 +0900

    feat: output logic use to output property

---
+++ src/main/java/racingcar/property/OutputProperty.java
@@ -0,0 +1,5 @@
+package racingcar.property;
+
+public class OutputProperty {
+    public static final String GAME_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+import racingcar.property.OutputProperty;
+
+public class OutputView {
+    public void outputForGameStart(){
+        System.out.println(OutputProperty.GAME_START);
+    }
+}
