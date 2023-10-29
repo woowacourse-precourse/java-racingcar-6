b655347efde5803814293a4679d3e548b54eadca
Seungjida <esg0107@pusan.ac.kr>
2023-10-30 00:27:31 +0900

    refactor: 기능에 알맞는 메서드 부여

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,29 +1,30 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
     private static int attemptNumber = 0;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        CarRaceManager newRacingCars = new CarRaceManager();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         // 자동차 이름 받아서 쉼표 기준으로 나누고 리스트에 저장(static으로 할 지 말 지 고민되네에베네)
-        CarManager newCarList = new CarManger();
-        newCarList.createCarList();
+        newRacingCars.createCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
-        // 시도 횟수 받아서 시도할 회수에 저장
         attemptNumber = getAttemptNumber();
 
-        while (attemptNumber) {
-            Referee.nthAttemptRace();
+        for(int i = 0; i < attemptNumber; i++) {
+            newRacingCars.nthAttemptRace();
+            newRacingCars.printAttemptResult();
         }
 
-        final String winner = Referee.winnerJudgment();
-        printWinner();
-    }
-
-    private static void printWinner() {
+        newRacingCars.winnerJudgment();
+        newRacingCars.printWinner();
+        Console.close();
     }
 
     private static int getAttemptNumber() {
