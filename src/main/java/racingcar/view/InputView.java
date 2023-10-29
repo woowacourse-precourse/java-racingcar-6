d6a2ffb8aa2d377be4874c6deaac4eab5127860c
Lee ChanHui <ing3616062@naver.com>
2023-10-29 17:41:45 +0900

    feat: View 기능 구현 InputView, ResultView 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
 public class InputView {
+
+    public void inputName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void inputRun(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
 }
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import racingcar.domain.Player;
+
+import java.util.List;
+
 public class ResultView {
+
+    public void winView(List<Player> winners){
+        System.out.println("최종 우승자 : ");
+
+        for (int i=0; i<winners.size()-1; i++) {
+            System.out.print(winners.get(i).getName() + ", ");
+        }
+
+        System.out.println(winners.get(winners.size()).getName());
+    }
 }
