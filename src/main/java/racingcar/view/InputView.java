71a4f5b6f8e88c784d4334d3b8fb1b5848b4ade2
haiseong <wjdgotjd9908@gmail.com>
2023-10-30 00:23:08 +0900

    docs: 클래스 설명 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,11 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * 입력을 위한 클래스다.
+ *
+ * @author haiseong
+ */
 public class InputView {
     public static String readLine() {
         return Console.readLine();
