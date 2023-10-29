8e97165a4c2f09144bcbcb0cd8e21ef34534dcaa
HyunmoBae <tsi0520@naver.com>
2023-10-29 19:27:38 +0900

    test(racingcar) : add showCarsScore test

--- src/test/java/racingcar/domain/ScoreBoardTest.java
+++ src/test/java/racingcar/domain/ScoreBoardTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ScoreBoardTest {
@@ -31,5 +33,15 @@ class ScoreBoardTest {
         car.carPosition = 5;
         String result = scoreBoard.getCarPosition(car);
         assertThat(result).isEqualTo("-----");
+    }
+
+    @Test
+    void showCarsScoreTest() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            carList.add(new Car("" + i));
+        }
+        scoreBoard.showCarsScore(carList, 3);
     }
 }
