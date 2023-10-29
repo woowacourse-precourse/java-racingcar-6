a4e1de6cb6d5e9f6fca7ebc4c2df945b10a04af8
yuseok0215 <elwlahstmxjf@naver.com>
2023-10-29 17:35:55 +0900

    feat: 자동차 이름 입력알림문구 알려주는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,11 @@
 package racingcar;
 
 public class Application {
+
+    public void informInputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
