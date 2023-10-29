8160c1aea7066295ece3238dce6864d7fa701f94
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 22:32:12 +0900

    test(carGenerator): 자동차의 이름이 5글자인 경우 예외 테스트

--- src/test/java/racingcar/CarGeneratorTest.java
+++ src/test/java/racingcar/CarGeneratorTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,13 @@ public class CarGeneratorTest {
     void 이름_목록으로부터_자동차_생성() {
         assertThat(carGenerator.generate(List.of("pobi", "woni")))
                 .containsExactly(new Car("pobi"), new Car("woni"));
+    }
+
+    @Test
+    void 이름이_5글자_초과인_경우_예외() {
+        assertThatThrownBy(
+                () -> carGenerator.generate(List.of("ppppobi", "woni")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
     }
 }
