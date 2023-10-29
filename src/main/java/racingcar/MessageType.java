0838213608e0a96a261f5d977ca9ee7938094b1e
OikimiO <etkim02@naver.com>
2023-10-29 13:35:41 +0900

    feat: 출력 format 기능 추가

--- src/main/java/racingcar/MessageType.java
+++ src/main/java/racingcar/MessageType.java
@@ -2,7 +2,10 @@ package racingcar;
 
 public enum MessageType{
     INPUT_CAR_NAME_PRINT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_CAR_RACING_COUNT("시도할 회수는 몇회인가요?");
+    INPUT_CAR_RACING_COUNT_PRINT("시도할 회수는 몇회인가요?"),
+
+    OUPUT_RACE_RESULT_PRINT("실행 결과"),
+    OUPUT_IMPLEMENTATION_RESULT_PRINT("%s :");
 
     private final String value;
 
--- src/main/java/racingcar/OutputCarRace.java
+++ src/main/java/racingcar/OutputCarRace.java
@@ -1,13 +1,31 @@
 package racingcar;
 
+import static java.lang.String.format;
+
 public class OutputCarRace{
     public static OutputCarRace print(final MessageType messageType){
         return new OutputCarRace(messageType);
     }
 
+    public static OutputCarRace printf(final MessageType messageType, final Object... inputNames){
+        return new OutputCarRace(messageType, inputNames);
+    }
+
     private OutputCarRace(){}
 
     private OutputCarRace(final MessageType messageType){
-        System.out.println(messageType.getValue());
+        String message = messageType.getValue();
+
+        printMessage(message);
+    }
+
+    private OutputCarRace(final MessageType messageType, final Object... inputNames){
+        String message = messageType.getValue();
+
+        printMessage(format(message, inputNames));
+    }
+
+    private static void printMessage(String message){
+        System.out.println(message);
     }
 }
