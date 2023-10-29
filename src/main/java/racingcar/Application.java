c8dc69e2ad3bd5c3399c6dcec0799c6e8e1ef99d
jy3026 <jy_3026@naver.com>
2023-10-29 18:52:44 +0900

    feat : 우승자 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         String[] cars = carNameInput();// 자동차 이름 입력 받기
@@ -13,6 +16,8 @@ public class Application {
         System.out.println("실행 결과");
 
         playGame(cars, retryCount, results); // 게임 시작
+
+        determineWinners(cars, results); // 우승자 출력
     }
 
     private static String[] carNameInput() {
@@ -20,7 +25,7 @@ public class Application {
         String input = Console.readLine();
         String[] cars = input.split(",");
 
-        for(String car : cars) {
+        for (String car : cars) {
             String carName = car.trim();
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
@@ -47,21 +52,40 @@ public class Application {
         for (int i = 0; i < result.length; i++) {
             result[i] = "";
         }
+
         return result;
     }
 
     private static void playGame(String[] cars, int retryCount, String[] results) {
-        for(int i = 0; i< retryCount; i++){
+        for (int i = 0; i < retryCount; i++) {
             updateCarResults(cars, results); // 이동 결과 출력
             System.out.println();
         }
     }
 
     private static void updateCarResults(String[] cars, String[] results) {
-        for(int i = 0; i < cars.length ; i++) {
+        for (int i = 0; i < cars.length; i++) {
             int randomNumber = Randoms.pickNumberInRange(0, 9);
-            if(randomNumber >= 4) results[i] += "-";
+            if (randomNumber >= 4) results[i] += "-";
             System.out.println(cars[i] + " : " + results[i]);
         }
+    }
+
+    private static void determineWinners(String[] cars, String[] results) {
+        int maxDistance = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (int i = 0; i < cars.length; i++) {
+            int distance = results[i].length();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                winners.clear();
+                winners.add(cars[i]);
+            } else if (distance == maxDistance) {
+                winners.add(cars[i]);
+            }
+        }
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
