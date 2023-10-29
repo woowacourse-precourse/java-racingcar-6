f9d2c9ce603984bde4f7727c44c2b48898139330
acs0209 <annjae4676@naver.com>
2023-10-29 18:52:46 +0900

    feat: add forward function in carservice

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
@@ -12,6 +13,18 @@ public class CarService {
         carNames.stream()
                 .map(Car::new)
                 .forEach(carRepository::save);
+    }
+
+    public void forward() {
+        List<Car> cars = carRepository.getCars();
+        for (Car car : cars) {
+            int randomNum = createRandomNum();
+            updateCarScore(car, randomNum);
+        }
+    }
+
+    private int createRandomNum() {
+        return Randoms.pickNumberInRange(0,9);
     }
 
     public void updateCarScore(Car car, int randomNum) {
