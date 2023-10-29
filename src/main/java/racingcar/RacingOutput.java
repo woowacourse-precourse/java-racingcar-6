e4068305017adf70d8f34de8cfd2d9613b22b40c
su-giana <giananews@gmail.com>
2023-10-29 21:48:22 +0900

    # 출력 구현
    # 제목
    '실행 결과' 출력 메소드 구현
    
    ### 이슈번호 (Optional.)
    - 이슈 번호 #11 구현

--- src/main/java/racingcar/RacingOutput.java
+++ src/main/java/racingcar/RacingOutput.java
@@ -14,17 +14,22 @@ public class RacingOutput {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
     }
 
-    private void putNameInputStatement() throws IOException
+    public void putNameInputStatement() throws IOException
     {
         bw.write("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     }
 
-    private void putTryInputStatement() throws IOException
+    public void putTryInputStatement() throws IOException
     {
         bw.write("시도할 회수는 몇회인가요?");
     }
 
-    private void putResultOfEachCar(int cnt) throws IOException
+    public void putResultStatement() throws IOException
+    {
+        bw.write("실행 결과");
+    }
+
+    public void putResultOfEachCar(int cnt) throws IOException
     {
         for (int i = 0 ; i<cnt; i++)
         {
@@ -32,7 +37,7 @@ public class RacingOutput {
         }
     }
 
-    private void putResultOfMovement(int n, List<Integer> carNames, List<Integer> carDis) throws IOException
+    public void putResultOfMovement(int n, List<Integer> carNames, List<Integer> carDis) throws IOException
     {
         for (int i = 0 ; i<n ; i++)
         {
@@ -43,7 +48,7 @@ public class RacingOutput {
         bw.write("\n");
     }
 
-    private List<String> getResultOfWinner(int n, List<String> carName, List<Integer> carDis) throws IOException
+    public List<String> getResultOfWinner(int n, List<String> carName, List<Integer> carDis) throws IOException
     {
         if (n==0)    return new ArrayList<>();
 
@@ -66,14 +71,14 @@ public class RacingOutput {
         return winnerList;
     }
 
-    private void putResultOfWinner(List<String> winnerNames) throws IOException
+    public void putResultOfWinner(List<String> winnerNames) throws IOException
     {
         bw.write("최종 우승자 : ");
         String winners = String.join(", ", winnerNames);
         bw.write(winners);
     }
 
-    private void closeInput() throws IOException
+    public void closeInput() throws IOException
     {
         bw.flush();
         bw.close();
