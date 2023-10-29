2e070f7602ef597786240696dd025e7e5f360dc3
gonixxxx <akdntm946@gmail.com>
2023-10-29 22:21:08 +0900

    feat: add random number generator

---
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int START_NUM = 0;
+    private static final int END_NUM = 9;
+
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(START_NUM, END_NUM);
+    }
+
+}
