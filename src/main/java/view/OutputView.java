11a15f12cac452df8add622868177140fb75a6e3
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-29 12:48:52 +0900

    refactor: 각 자동차들의 진행 상태를 출력하는 메소드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,9 +3,20 @@ package view;
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String ONE_SPACE ="-";
+
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
     }
+
+    public static void printCurrentLocationOfCar(String name, int distance){
+        System.out.printf("%s : ",name);
+        for(int i=0; i < distance; i++) {
+            System.out.print(ONE_SPACE);
+        }
+        System.out.println();
+    }
+
 
     public static void printWinnerMessage() {
         System.out.print(WINNER_MESSAGE);
