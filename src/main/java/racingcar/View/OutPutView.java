b659f9ee351d1c8a5c3ebebc268e10f0c52716f2
sami355-24 <ddung355@gmail.com>
2023-10-29 18:59:41 +0900

    test(Domain): CarGroup에서 최종 우승자를 순서에 맞게 출력하는 기능에 대한 테스트 코드 추가

--- src/main/java/racingcar/View/OutPutView.java
+++ src/main/java/racingcar/View/OutPutView.java
@@ -13,6 +13,7 @@ public class OutPutView {
     }
 
     public static void showFinalWinner(CarGroup carGroup) {
+
     }
 
 
--- src/test/java/racingcar/Domain/CarGroupTest.java
+++ src/test/java/racingcar/Domain/CarGroupTest.java
@@ -3,6 +3,7 @@ package racingcar.Domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarGroupTest {
@@ -43,5 +44,19 @@ class CarGroupTest {
         assertThat(testInstance.findCarByOrder(1).findName()).isEqualTo(testCarNames.get(0));
         assertThat(testInstance.findCarByOrder(2).findName()).isEqualTo(testCarNames.get(1));
         assertThat(testInstance.findCarByOrder(3).findName()).isEqualTo(testCarNames.get(2));
+    }
+
+    @Test
+    @DisplayName("최종 우승자 검색하기")
+    void findFinalWinnerTest(){
+        //given
+        List<String> testCarNames = List.of("testCar1", "testCar2", "testCar3");
+        CarGroup testInstance = CarGroup.of(testCarNames);
+
+        //when
+        CarGroup finalWinner = testInstance.findFinalWinner();
+
+        //then
+        assertThat(finalWinner.findSize()).isEqualTo(testCarNames.size());
     }
 }
