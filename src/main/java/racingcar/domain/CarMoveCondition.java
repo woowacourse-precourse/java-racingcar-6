330ef150beef216b81157c277bd21b1d20d53102
daheeParkk <qkrekgml7414@naver.com>
2023-10-29 21:11:58 +0900

    test : 4 이상의 숫자가 들어오면 전진하는 테스트 수정

---
+++ src/main/java/racingcar/domain/CarMoveCondition.java
@@ -0,0 +1,2 @@
+package racingcar.domain;public interface CarMoveCondition {
+}
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,18 +2,18 @@ package racingcar;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.domain.Car;
 
 public class CarTest {
 
     @DisplayName("4 이상의 숫자가 들어오면 전진하는 테스트")
-    @Test
-    @CsvSource(value = {"1:0", "3:0", "4:1", "5:1"}, delimiter = ',')
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "3:0", "4:1", "5:1"}, delimiter = ':')
     public void moveTest(int randomNumber, int result) {
         Car car = new Car("테스트");
-        if (car.isMoreThanCodition(randomNumber)) {
+        if (car.isMoreThanCondition(randomNumber)) {
             car.move();
         }
         Assertions.assertThat(car.getDistance()).isEqualTo(result);
