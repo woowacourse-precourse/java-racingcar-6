230bfabcebd8c2f9a67597dbd9e551a75e59b752
HYUNJUN\sajun <sajun28@naver.com>
2023-10-29 23:42:15 +0900

    Feat: "랜덤 숫자 생성 기능 추가"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.model.Move;
 import racingcar.model.Name;
 import racingcar.view.InputView;
 
@@ -9,8 +10,11 @@ import java.util.List;
 public class GameController {
     private final InputView inputView = new InputView();
     private final Name name = new Name();
+    private final Move move = new Move();
     public void start() {
         List<String> player = Arrays.asList(inputView.name().split(","));
         name.isValid(player);
+        int randomNum = move.generateRandom();
+        System.out.println(randomNum);
     }
 }
