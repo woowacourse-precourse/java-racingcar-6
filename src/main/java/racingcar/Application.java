4782d354ad0ee7b9a23eb383b76e4d8aed1d6212
jdohun <wjdehgns97@naver.com>
2023-10-29 11:19:47 +0900

    refactor: 자동차 이름을 저장하는 기능 이름 변경 및 패키지 구분

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,28 +1,19 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import racingcar.script.Script;
+import racingcar.domain.script.Script;
 
 import java.util.HashMap;
+
+import static racingcar.domain.reception.ReceptionDest.registering;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Script.LINEUP.print();
-        HashMap<String, Integer> entry = lineUp();
+        HashMap<String, Integer> entry = registering();
         // hi,my,name,is,hello,world
 
         System.out.println(entry);
     }
 
-    public static HashMap<String, Integer> lineUp() {
-        HashMap<String, Integer> lineUp = new HashMap<>();
-        String[] lineUpArr = Console.readLine().split(",");
-
-        for (String name : lineUpArr) {
-            lineUp.put(name, 0);
-        }
-
-        return lineUp;
-    }
 }
--- src/main/java/racingcar/domain/script/Script.java
+++ src/main/java/racingcar/domain/script/Script.java
@@ -1,4 +1,4 @@
-package racingcar.script;
+package racingcar.domain.script;
 
 public enum Script {
     LINEUP("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
