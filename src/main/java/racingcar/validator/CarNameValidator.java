66ae2755f67afc6ab634db6f6a1dcf19a3da29ba
minseo  jo <liging12@naver.com>
2023-10-29 19:26:44 +0900

    refactor: 예외 처리 변경
    자동차 명을 ','로 시작하는 경우는 예외를 뱉지만, ','로 끝나는 경우는 예외를 안뱉음.
    
    1번 상황 - ",pobi,woni" -> 예외 X
    2번 상황 - "pobi,woni," -> 예외 O
    Car 객체를 생성할 떄 예외 이므로, 1번 상황인 경우는 첫 자동차 이름이 공백으로 들어가지만, 2번의 경우는 ['pobi','woni']만 있으므로 예외가 안터진다.
    
    1번, 2번의 상황모두 예외를 터치고 싶으면 InputValidator 를 만들던가 해야할 듯.

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -12,8 +12,6 @@ public class CarNameValidator {
     private static final String ERROR = "[ERROR] 자동차 이름은 ";
     private static final String ERROR_BLANK = ERROR + "공백을 허용하지 않습니다.";
     private static final String ERROR_MAX_LENGTH = ERROR + "최대 " + MAX_LENGTH + "글자 입니다.";
-//    private static final String ERROR_COMMA_START = ERROR + KOREAN_COMMA_BRACKET_DELIMITER + "로 시작할 수 없습니다.";
-//    private static final String ERROR_COMMA_END = ERROR  + KOREAN_COMMA_BRACKET_DELIMITER + "로 끝날수 없습니다.";
     private static final String ERROR_CONSECUTIVE_COMMA = ERROR + "연속된 " + KOREAN_COMMA_BRACKET_DELIMITER + "을 허용하지 않습니다.";
 
     private CarNameValidator() {}
@@ -21,8 +19,6 @@ public class CarNameValidator {
     public static void validate(String input) {
         validateCarNamesBlankException(input);
         validateCarNamesLengthGreaterThanFive(input);
-//        validateCarNamesStartWithCommaException(input);
-//        validateCarNamesEndWithCommaException(input);
         validateCarNamesConsecutiveCommaException(input);
     }
 
@@ -41,18 +37,6 @@ public class CarNameValidator {
                     throw new IllegalArgumentException(ERROR_MAX_LENGTH);
                 });
     }
-
-//    private static void validateCarNamesStartWithCommaException(String input) {
-//        if (input.charAt(0) == COMMA_DELIMITER) {
-//            throw new IllegalArgumentException(ERROR_COMMA_START);
-//        }
-//    }
-//
-//    private static void validateCarNamesEndWithCommaException(String input) {
-//        if (input.charAt(input.length() - 1) == COMMA_DELIMITER) {
-//            throw new IllegalArgumentException(ERROR_COMMA_END);
-//        }
-//    }
 
     private static void validateCarNamesConsecutiveCommaException(String input) {
         if (input.contains(CONSECUTIVE_COMMA_DELIMITER)) {
