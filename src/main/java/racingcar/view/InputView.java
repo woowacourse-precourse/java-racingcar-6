126f107a2cbcf4415089c5dea70deaa7e97bab4e
elyudwo <dudqk9696@naver.com>
2023-10-29 22:40:39 +0900

    [Refactor] : InputView 메서드 개행문자 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,13 +13,13 @@ public class InputView {
     }
 
     public List<String> insertCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
         return utils.convertStringToList(names);
     }
 
     public int insertTryTimes() {
-        System.out.println("시도할 회수는 몇회인가요?\n");
+        System.out.println("시도할 회수는 몇회인가요?");
         String tryNumber = Console.readLine();
         return utils.convertStringToNumber(tryNumber);
     }
