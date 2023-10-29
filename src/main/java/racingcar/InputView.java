5cb7ecc7f6087c77c87d7d8b5650991e622b6f49
aammddkkzxc <ideal0427@naver.com>
2023-10-29 20:51:28 +0900

    feat: 쉼표로 자동차 이름 자르기 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,17 +1,22 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     static final String CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String COMMA = ",";
 
     public static String inputCarNames() {
         System.out.println(CAR_NAMES_REQUEST_MESSAGE);
         return Console.readLine();
     }
 
-    public void splitNames() {
-
+    public static List<String> divideCarNames(String carNames) {
+        String[] dividedCarNames = carNames.split(COMMA);
+        return new ArrayList<>(Arrays.asList(dividedCarNames));
     }
 
     public void inputAttempts() {
