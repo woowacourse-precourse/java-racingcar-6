c639e08c452c2b76df3cfa07d71d11694e4370b6
unknown <y2kf9a0q@gmail.com>
2023-10-30 01:22:35 +0900

    refactor: GameTest.java

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -11,10 +12,14 @@ import static org.mockito.Mockito.*;
 
 class GameTest {
     Game game;
+    MockedStatic<Console> consoleMockedStatic;
+    MockedStatic<RandomMaker> randomMakerMockedStatic;
 
     @BeforeEach
     void setUp() {
         game = new Game();
+        consoleMockedStatic = mockStatic(Console.class);
+        randomMakerMockedStatic = mockStatic(RandomMaker.class);
     }
 
     @Test
@@ -34,74 +39,62 @@ class GameTest {
         String input = "pobi,woni,jun";
         String[] output = {"pobi", "woni", "jun"};
 
-        try (MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThat(game.inputCarNames()).isEqualTo(output);
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThat(game.inputCarNames()).isEqualTo(output);
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
     void inputCarNames_Exception_EmptyName() {
         String input = "pobi,,jun";
 
-        try (MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThatThrownBy(() -> game.inputCarNames())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Not valid input: car names");
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThatThrownBy(() -> game.inputCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Not valid input: car names");
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
     void inputCardNames_Exception_MoreThanFiveCharacters() {
         String input = "pobi,woni,jun,dongjin";
 
-        try (MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThatThrownBy(() -> game.inputCarNames())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Not valid input: car names");
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThatThrownBy(() -> game.inputCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Not valid input: car names");
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
     void inputGameTurns() {
         String input = "10";
 
-        try(MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThatCode(() -> game.inputGameTurns()).doesNotThrowAnyException();
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThatCode(() -> game.inputGameTurns()).doesNotThrowAnyException();
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
     void inputGameTurns_Exception_NotNumber() {
         String input = "...";
 
-        try(MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThatThrownBy(() -> game.inputGameTurns())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Not valid input: game turn number");
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThatThrownBy(() -> game.inputGameTurns())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Not valid input: game turn number");
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
     void inputGameTurns_Exception_NegativeNumber() {
         String input = "-1";
 
-        try (MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
-            when(Console.readLine()).thenReturn(input);
-            assertThatThrownBy(() -> game.inputGameTurns())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Not valid input: game turn number");
-            mockedStatic.verify(Console::readLine, times(1));
-        }
+        when(Console.readLine()).thenReturn(input);
+        assertThatThrownBy(() -> game.inputGameTurns())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Not valid input: game turn number");
+        consoleMockedStatic.verify(Console::readLine, times(1));
     }
 
     @Test
@@ -113,20 +106,17 @@ class GameTest {
         final int STOP = 3;
         Integer[] randoms = {STOP, GO, GO, STOP, GO, GO, GO, GO, GO, GO, GO, GO, GO, GO};
 
-        try (MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
-             MockedStatic<RandomMaker> randomMakerMockedStatic = mockStatic(RandomMaker.class)) {
-            when(Console.readLine()).thenReturn(carNames, gameTurns);
-            when(RandomMaker.makeRandomNumber()).thenReturn(GO, randoms);
+        when(Console.readLine()).thenReturn(carNames, gameTurns);
+        when(RandomMaker.makeRandomNumber()).thenReturn(GO, randoms);
 
-            assertThatCode(() -> {
-                game.init();
-                game.run();
-                game.findWinners();
-            }).doesNotThrowAnyException();
+        assertThatCode(() -> {
+            game.init();
+            game.run();
+            game.findWinners();
+        }).doesNotThrowAnyException();
 
-            consoleMockedStatic.verify(Console::readLine, times(2));
-            randomMakerMockedStatic.verify(RandomMaker::makeRandomNumber, times(15));
-        }
+        consoleMockedStatic.verify(Console::readLine, times(2));
+        randomMakerMockedStatic.verify(RandomMaker::makeRandomNumber, times(15));
     }
 
     @Test
@@ -139,18 +129,21 @@ class GameTest {
 
         String expected = "최종 우승자 : pobi";
 
-        try (MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
-             MockedStatic<RandomMaker> randomMakerMockedStatic = mockStatic(RandomMaker.class)) {
-            when(Console.readLine()).thenReturn(carNames, gameTurns);
-            when(RandomMaker.makeRandomNumber()).thenReturn(GO, STOP);
+        when(Console.readLine()).thenReturn(carNames, gameTurns);
+        when(RandomMaker.makeRandomNumber()).thenReturn(GO, STOP);
 
-            game.init();
-            game.run();
-            game.findWinners();
-            assertThat(game.winnersToString()).isEqualTo(expected);
+        game.init();
+        game.run();
+        game.findWinners();
+        assertThat(game.winnersToString()).isEqualTo(expected);
 
-            consoleMockedStatic.verify(Console::readLine, times(2));
-            randomMakerMockedStatic.verify(RandomMaker::makeRandomNumber, times(2));
-        }
+        consoleMockedStatic.verify(Console::readLine, times(2));
+        randomMakerMockedStatic.verify(RandomMaker::makeRandomNumber, times(2));
+    }
+
+    @AfterEach
+    void tearDown() {
+        consoleMockedStatic.close();
+        randomMakerMockedStatic.close();
     }
 }
