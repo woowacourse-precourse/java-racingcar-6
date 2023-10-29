a93c4e133566a7e9a6d6de0e62d067df6ab80c8c
hlmg <devhlmg@gmail.com>
2023-10-29 18:17:51 +0900

    feat(OutputView): 차수별 실행 결과 출력하는 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.RaceHistory;
+
+public class OutputView {
+    public void printRaceHistory(RaceHistory raceHistory) {
+        List<String> carNames = raceHistory.getCarNames();
+        List<List<Integer>> positionHistory = raceHistory.getPositionHistory();
+
+        for (List<Integer> positions : positionHistory) {
+            printRaceSnapshot(carNames, positions);
+        }
+    }
+
+    private void printRaceSnapshot(List<String> carNames, List<Integer> positions) {
+        for (int j = 0; j < positions.size(); j++) {
+            System.out.println(carNames.get(j) + " : " + "-".repeat(positions.get(j)));
+        }
+        System.out.println();
+    }
+}
