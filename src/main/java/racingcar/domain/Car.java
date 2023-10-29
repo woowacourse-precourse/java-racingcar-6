3cfbb6fc00a6c26a0b7087cafa137054f700d316
jongmee <ra123221@gmail.com>
2023-10-30 02:11:07 +0900

    feat: 경주차(Car) 엔티티 생성하기
    - 경주차는 이름, 전진 횟수를 속성으로 갖는다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+public class Car {
+
+    private Integer id;
+    private String name;
+    private Integer forwardCount;
+
+    public Car(final Integer id, final String name) {
+        this.id = id;
+        this.name = name;
+        this.forwardCount = 0;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getForwardCount() {
+        return forwardCount;
+    }
+}
