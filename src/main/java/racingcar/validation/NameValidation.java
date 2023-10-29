3927b12946d7d3cc6aa8eb8c5b0253ef94be65b2
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 18:41:18 +0900

    feat: verify logic for racer name value length is over length standard

--- src/main/java/racingcar/validation/NameValidation.java
+++ src/main/java/racingcar/validation/NameValidation.java
@@ -1,12 +1,19 @@
 package racingcar.validation;
 
 import racingcar.property.ErrorProperty;
+import racingcar.property.ValidateProperty;
 
 public class NameValidation extends InputValidation{
 
     public static void verifyForRacerNameIsEngOrKor(String inputValue){
         if (!inputValue.equals("^[a-zA-Z가-힣]*$")){
             throw new IllegalArgumentException(ErrorProperty.NAME_VALUE_IS_NOT_CORRECT_FORM);
+        }
+    }
+
+    public static void verifyForRacerNameIsLengthNotOver(String inputValue){
+        if (inputValue.length()> ValidateProperty.NAME_LENGTH_STANDARD){
+            throw new IllegalArgumentException(ErrorProperty.NAME_VALUE_LENGTH_IS_OVER);
         }
     }
 }
