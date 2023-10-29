7c4a2ebb90d8d0bfd6ed7f8f892760866211fc3d
sangeun-b <baek0007@algonquinlive.com>
2023-10-29 23:10:18 +0900

    feat: 시도 횟수 입력 메소드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,11 +8,17 @@ import java.util.List;
 public class InputView {
 
     public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_TIME = "시도할 회수는 몇회인가요?";
 
     public List<String> inputCarName() {
         System.out.println(INPUT_CAR_NAME);
         String carName = Console.readLine();
         List<String> carNames = Arrays.asList(carName.split(","));
         return carNames;
+    }
+    public int inputTryTime() {
+        System.out.println(INPUT_TRY_TIME);
+        int tryTime = Integer.parseInt(Console.readLine());
+        return tryTime;
     }
 }
