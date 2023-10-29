b0e52971714f5f9638cf574801af4737b4468334
daheeParkk <qkrekgml7414@naver.com>
2023-10-29 21:53:55 +0900

    feat : 콤마로 이름을 구분하는 Cars 생성

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,2 +1,30 @@
-package racingcar.domain;public class Cars {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final String names;
+    private final List<Car> cars = new ArrayList<>();
+
+    public Cars(String names) {
+        this.names = names;
+        String[] separateName = separateName();
+        createCars(separateName);
+    }
+
+    private void createCars(String[] separateName) {
+        for (String name : separateName) {
+            cars.add(new Car(name));
+        }
+    }
+
+    private String[] separateName() {
+        return names.split(",");
+    }
+
+    public Car getCar(int index) {
+        return cars.get(index);
+    }
 }
