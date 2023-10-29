301763bee51da9d6608340dec940f1a92225972a
권장순 <jsoonworld@gmail.com>
2023-10-29 13:10:53 +0900

    feat(racingcar) : 기능 요구사항에서 레이싱의 우승자를 알려준다(Racing#result())에서 Racing#winner()로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
 
-        RacingResult racingResult = new RacingResult();
-        racingResult.result();
+
 
     }
 }
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,11 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Racing {
 
     // 입력 받기
+    public String input() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return input;
+    }
+
     // 실행 결과
-    // 우승자 출
-    public void result() {
+    public String racing() {
         System.out.println("실행 결과");
         CreateCar createCar = new CreateCar();
         int carCount = createCar.carCount(createCar.carName(createCar.input()));
@@ -25,5 +32,33 @@ public class Racing {
             }
             System.out.println();
         }
+        return null;
     }
+    // 우승자 출력
+    public String winner() {
+        System.out.println("최종 우승자: pobi");
+        return null;
+    }
+
+//    public void result() {
+//        System.out.println("실행 결과");
+//        CreateCar createCar = new CreateCar();
+//        int carCount = createCar.carCount(createCar.carName(createCar.input()));
+//        String[] carName = createCar.carName(createCar.input());
+//
+//        RacingRound racingRound = new RacingRound();
+//        int attemptCount = racingRound.attemptCount();
+//
+//        for (int i = 0; i < attemptCount; i++) {
+//            for (int j = 0; j < carCount; j++) {
+//                Forward forward = new Forward();
+//                if (forward.canForward()) {
+//                    System.out.println(carName[j] + " : " + "-");
+//                } else {
+//                    System.out.println(carName[j] + " : " + "");
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
 }
