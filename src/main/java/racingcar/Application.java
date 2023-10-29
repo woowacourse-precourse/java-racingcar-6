a0b95cd6fd0b03b1bd69bfb782a79fed74f96462
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-29 16:27:27 +0900

    feat : 게임 시작 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.startGame();
     }
 }
