8aadc2c454ff8cd8b8c680001ba14377fa27086e
HyunmoBae <tsi0520@naver.com>
2023-10-29 21:11:05 +0900

    refactor(racingcar) : modify NumberGeneratorTest

--- src/test/java/racingcar/domain/NumberGeneratorTest.java
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -1,17 +1,30 @@
 package racingcar.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
 class NumberGeneratorTest {
+
     @Test
-    void randomNumber() {
-        for (int i = 0; i < 10; i++){
+    void randomNumber1() {
         NumberGenerator numberGenerator = new NumberGenerator();
-        int num = numberGenerator.createRandomNumber();
-        System.out.print(num);
-        }
-        System.out.println();
+        int result = numberGenerator.createRandomNumber();
+        assertThat(result).isBetween(1, 9);
     }
+
+    @Test
+    void randomNumber2() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        int result = numberGenerator.createRandomNumber();
+        assertThat(result).isBetween(1, 9);
+    }
+
+    @Test
+    void randomNumber3() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        int result = numberGenerator.createRandomNumber();
+        assertThat(result).isBetween(1, 9);
+    }
+
 }
