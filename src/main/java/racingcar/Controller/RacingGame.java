760ea1daf950ce595c76f6e114473640c6a993f1
goragoraki <gorakio0126@gmail.com>
2023-10-29 12:19:54 +0900

    feat(RacingGame): add racingStart function
    Add racingStart function to start racing game.

--- src/main/java/racingcar/Controller/RacingGame.java
+++ src/main/java/racingcar/Controller/RacingGame.java
@@ -5,10 +5,27 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 import racingcar.util.validation;
 
-public class RacingGame {
+import java.util.List;
 
+public class RacingGame {
+    private final CarInfo carInfo = new CarInfo();
+    public void racingStart() {
+        int tryNum;
+
+        carInfo.setCarName(InputView.receiveCarName());
+        carInfo.initCarStatus();
+        tryNum = convertStringToInt(InputView.receiveTryNumber());
+
+        OutputView.printExecResult();
+        for(int i = 0; i < tryNum; i++) {
+            carInfo.updateCarStatus();
+            OutputView.printCarStatus(carInfo.getCarName(), carInfo.getCarStatus());
+        }
+        OutputView.printWinner(carInfo.getWinnerName());
+    }
     public int convertStringToInt(String num) {
         validation.checkTryNumber(num);
         return Integer.parseInt(num);
     }
+
 }
