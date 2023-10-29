7816d10c3a1bed487b4c5c394838307dec07c022
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-29 22:04:52 +0900

    refactor: 문자열을 리스트로 변환하는 기능을 한 함수로 축약
    문자열을 리스트로 바꾸기 위해
    배열로 바꾸는 함수와
    배열을 리스트로 바꾸는 함수로 나뉘었으나,
    Arrays.stream구문을 통해 한 문장으로 축약하여 함수를 하나로 간략화함

--- src/main/java/racingcar/utils/StringChanger.java
+++ src/main/java/racingcar/utils/StringChanger.java
@@ -1,16 +1,12 @@
 package racingcar.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class StringChanger {
-    public static String[] stringToTrimmedStringArray(String string) {
-        return Stream.of(string.split(",", -1)).map(String::trim).toArray(String[]::new);
-    }
-
-    public static List<String> stringArrayToStringList(String[] strings) {
-        return new ArrayList<>(Arrays.asList(strings));
+    public static List<String> stringToStringList(String string) {
+        return Arrays.stream(string.split(",", -1))
+                .map(String::trim).collect(Collectors.toList());
     }
 }
