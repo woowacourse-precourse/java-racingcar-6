3a9784d400723a14d0bf5c93ee3b73f1a94abd1f
lee jihyeon <lejinhy@gmail.com>
2023-10-29 21:44:48 +0900

    feat: 자동차들 중 최종 우승자를 가려내는 메소드 완성

--- src/main/java/racingcar/util/Calculation.java
+++ src/main/java/racingcar/util/Calculation.java
@@ -1,8 +1,13 @@
-package racingcar;
+package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Entity.Car;
 import racingcar.constant.CarStatus;
 import racingcar.constant.MagicNumber;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Calculation {
 
@@ -15,5 +20,19 @@ public class Calculation {
             return CarStatus.MOVE;
         }
         return CarStatus.STOP;
+    }
+
+    public List<Car> judgeWhoIsWinner(List<Car> cars) {
+        List<Car> winners = new ArrayList<>();
+        Collections.sort(cars, (o1, o2) -> o2.getMoveCount() - o1.getMoveCount());
+        int maxMoveCount = cars.get(0).getMoveCount();
+
+        for (Car car : cars) {
+            if (car.getMoveCount() != maxMoveCount) {
+                return winners;
+            }
+            winners.add(car);
+        }
+        return winners;
     }
 }
