243b42859b1b81652d119e637dc03137cb8cf980
chjcoder <chjcoder@gmail.com>
2023-10-29 11:15:56 +0900

    rename: 파일명 수정
    - Attemp -> Attempt
    - AttempTest -> AttemptTest

--- src/main/java/racingcar/domain/Attempt.java
+++ src/main/java/racingcar/domain/Attempt.java
@@ -3,12 +3,13 @@ package racingcar.domain;
 import racingcar.util.Utils;
 import racingcar.validation.Validation;
 
-public class Attemp {
+public class Attempt {
     private final int number;
-    public Attemp(String attempStr) {
-        Validation.validateEmptyInput(attempStr);
-        Validation.validateInteger(attempStr);
-        number = Utils.stringToInt(attempStr);
+
+    public Attempt(String attemptStr) {
+        Validation.validateEmptyInput(attemptStr);
+        Validation.validateInteger(attemptStr);
+        number = Utils.stringToInt(attemptStr);
         Validation.validateRange(number);
     }
 
--- src/test/java/racingcar/domain/AttemptTest.java
+++ src/test/java/racingcar/domain/AttemptTest.java
@@ -3,12 +3,12 @@ package racingcar.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AttempTest {
+public class AttemptTest {
     @Test
     void 빈_값_예외_처리() {
         String input = "";
 
-        Assertions.assertThatThrownBy(() -> new Attemp(input))
+        Assertions.assertThatThrownBy(() -> new Attempt(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("빈칸입니다.");
     }
@@ -17,7 +17,7 @@ public class AttempTest {
     void 숫자가_아닌_값_예외_처리() {
         String input = "abc";
 
-        Assertions.assertThatThrownBy(() -> new Attemp(input))
+        Assertions.assertThatThrownBy(() -> new Attempt(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자를 입력해 주세요.");
     }
@@ -26,7 +26,7 @@ public class AttempTest {
     void 숫자_0_보다_작은_값_예외_처리() {
         String input = "0";
 
-        Assertions.assertThatThrownBy(() -> new Attemp(input))
+        Assertions.assertThatThrownBy(() -> new Attempt(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("0보다 큰 수를 입력해 주세요.");
     }
