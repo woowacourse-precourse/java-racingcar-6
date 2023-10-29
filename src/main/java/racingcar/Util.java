318d49726503bec9f0bf0e96f50131f040ca99ef
oosedus <seayeon0729@gmail.com>
2023-10-29 16:27:02 +0900

    feat: 0~9까지 랜덤 숫자 생성 기능 구현

---
+++ src/main/java/racingcar/Util.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Util {
+
+    int makeRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+}
