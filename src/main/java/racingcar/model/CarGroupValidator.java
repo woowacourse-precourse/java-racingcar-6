11e0711c59f7ea504b5c81f4162bcaaef432de5a
sinminseok12 <sinminseok12@naver.com>
2023-10-30 01:30:49 +0900

    feat(CarGroupValidator): 중복되는 자동차 이름이 있을 경우 예외처리 기능 구현

--- src/main/java/racingcar/model/CarGroupValidator.java
+++ src/main/java/racingcar/model/CarGroupValidator.java
@@ -1,10 +1,13 @@
 package racingcar.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarGroupValidator {
     private static final Integer MIN_CAR_COUNT = 1;
     private static final String CAR_COUNT_ERROR = "최소 1개 이상의 자동차를 입력해야 합니다.";
+    private static final String DUPLICATE_CAR_ERROR = "중복된 자동차 이름이 존재합니다.";
 
     public static void validate(List<String> names) {
         validateCarCount(names);
@@ -13,6 +16,13 @@ public class CarGroupValidator {
     private static void validateCarCount(List<String> names) {
         if (names.size() < MIN_CAR_COUNT) {
             throw new IllegalArgumentException(CAR_COUNT_ERROR);
+        }
+    }
+
+    private static void validateDuplicate(List<String> names) {
+        Set<String> uniqueNames = new HashSet<>(names);
+        if (uniqueNames.size() != names.size()) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_ERROR);
         }
     }
 }
