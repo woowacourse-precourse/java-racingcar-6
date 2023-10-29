3f546f50cf4088696d41df6339d206de679692e9
IE-MangChi <hsm983278@gmail.com>
2023-10-29 17:15:40 +0900

    refactor: 변경된 메소드에 맞게 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,6 @@ public class Application {
         // TODO: 프로그램 구현
 
         CarRaceGameController carRaceGameController = new CarRaceGameController();
-        carRaceGameController.gameStart();
+        carRaceGameController.run();
     }
 }
