40106d0fa189446ee386b1138f37d3051ecc335a
pjaehyun <show6756@gmail.com>
2023-10-29 15:06:06 +0900

    feat: 수정 출력문 상수 추가로 출력 구문 변경

--- src/main/java/car/CarCollection.java
+++ src/main/java/car/CarCollection.java
@@ -3,6 +3,8 @@ package car;
 import java.util.ArrayList;
 import java.util.List;
 
+import static constants.RacingCarConstants.*;
+
 public class CarCollection {
 
     private final List<Car> cars;
@@ -23,7 +25,7 @@ public class CarCollection {
 
     public void printCarsDistance() {
         for (Car car : cars) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+            System.out.println(car.getName() + " : " + PRINT_DISTANCE_UNIT.repeat(car.getDistance()));
         }
         System.out.println();
     }
@@ -48,6 +50,6 @@ public class CarCollection {
     }
 
     public void printWinners(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        System.out.println(PRINT_WINNER_MESSAGE + String.join(", ", winners));
     }
 }
