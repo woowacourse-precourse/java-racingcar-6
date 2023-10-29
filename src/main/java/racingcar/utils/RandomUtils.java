9f8406153d29735d5cf0862d1619beeb93b0be02
ChaeAg <dydkglel123@gmail.com>
2023-10-29 14:38:27 +0900

    refactor(RandomUtils): 랜덤 정수 생성 로직 클래스로 분리

---
+++ src/main/java/racingcar/utils/RandomUtils.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtils {
+    public static int getRandomNumber(int randomStartNumber, int randomLastNumber) {
+        if(randomStartNumber == randomLastNumber) {
+            return randomStartNumber;
+        }
+        return Randoms.pickNumberInRange(randomStartNumber, randomLastNumber);
+    }
+}
