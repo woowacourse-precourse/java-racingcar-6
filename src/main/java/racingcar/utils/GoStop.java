9b50184fb0dc9bf7587ea4c7a06d40a377b2ffc3
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-29 15:43:35 +0900

    style: final변수를 활용해 bound 설정

--- src/main/java/racingcar/utils/GoStop.java
+++ src/main/java/racingcar/utils/GoStop.java
@@ -7,11 +7,12 @@ import main.java.racingcar.model.Car;
 
 public class GoStop {
 
+    private static final int FORWARD_BOUND = 4;
     public static void decideGoStop (Car car) {
         car.forward();
     }
     private static boolean decisionMaker(int n) {
-        return n >= 4;
+        return n >= FORWARD_BOUND;
     }
     private static int generateNum() {
         int num;
