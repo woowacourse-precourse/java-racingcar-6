a0dd557313bb2ba842287a2301c2a297fbb31134
SEOWONLEEE <9952sun@jj.ac.kr>
2023-10-29 23:25:31 +0900

    feat: 자동차 동작

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getPositionString() {
+        return "-".repeat(position);
+    }
+}
