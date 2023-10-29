c2c1f31e1d559e9ba84abff7c933cf3efbf869b2
yeonjae02 <0yeonjae2@naver.com>
2023-10-30 03:15:54 +0900

    refactor: Modify gameStart()
    변수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 	public int tryNumber;
+	public int playerNumber;
 	public class PlayerInfo{
 		private String name;
 		private int loc;
@@ -19,11 +20,12 @@ public class Application {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String input = Console.readLine();
 		StringTokenizer st = new StringTokenizer(input, ",");
-		
+		playerNumber = st.countTokens();
+
 		System.out.println("시도할 회수는 몇회인가요?");
 		String num = Console.readLine();
 		tryNumber = Integer.parseInt(num);
-		PlayerInfo[] data = new PlayerInfo[st.countTokens()];
+		PlayerInfo[] data = new PlayerInfo[playerNumber];
 		
 		int i=0;
 		while (st.hasMoreElements()) {
