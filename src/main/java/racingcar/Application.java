a4ba8e96b93d54a29dafad4fe911e084d65cf28e
yunjeong <stac_cato@naver.com>
2023-10-29 15:07:40 +0900

    chore: checkRound 예외 간단한 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,8 +9,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
-
-    public static List<String> askCarName(){
+    
+    public static List<String> askCarName() {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String input = readLine();
         checkCarNameAsCommas(input);
@@ -23,7 +23,7 @@ public class Application {
         return names;
     }
 
-    public static int askRound(){
+    public static int askRound() {
         System.out.print("시도할 회수는 몇회인가요?\n");
         String input = readLine();
         checkRound(input);
@@ -32,21 +32,21 @@ public class Application {
         return round;
     }
 
-    public static void checkCarNameAsCommas(String input){
+    public static void checkCarNameAsCommas(String input) {
         if (!input.contains(",")) {
             throw new IllegalArgumentException("자동차 이름은 2개 이상, 쉼표로 구분되어야 합니다.");
         }
     }
 
-    public static void checkCarNameAsLength(String input){
-        if (input.length() > 5){
+    public static void checkCarNameAsLength(String input) {
+        if (input.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 
     public static void checkRound(String input) {
-        if (input.isEmpty() || !input.matches("\\d+")) {
-            throw new IllegalArgumentException("숫자를 입력하십시오.");
+        if (input.isEmpty() || !input.matches("\\d+") || Integer.parseInt(input) < 1) {
+            throw new IllegalArgumentException("자연수를 입력하십시오.");
         }
     }
 }
