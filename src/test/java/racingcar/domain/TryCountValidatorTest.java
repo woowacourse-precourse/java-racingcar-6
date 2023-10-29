c2b630db65098f460b58d258d33c689f5d4acbb1
Sully <2dcoder@naver.com>
2023-10-29 21:45:50 +0900

    feat(TryCountValidator): 시도할 횟수를 입력하지 않았을 때 예외가 발생하는 메서드의 테스트 코드 작성
    혹시 테스트 코드가 추가될지도 모르므로 모든 경우의 수에 대한 테스트 코드도 추가했다.

---
+++ src/test/java/racingcar/domain/TryCountValidatorTest.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TryCountValidatorTest {
+
+    @DisplayName("모든 경우의 수를 따져서 올바른 tryCount일 경우에는 예외가 발생하지 않는다.")
+    @Test
+    void validateAllCasesDoesNotThrowAnyException() {
+        // given
+        String validTryCount = "2";
+
+        // when
+        // then
+        assertThatCode(() -> new TryCountValidator(validTryCount))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("validateTryCountExists()로 시도할 횟수를 입력하지 않으면 ''으로 저장되기 때문에 예외가 발생한다.")
+    @Test
+    void validateTryCountExistsWithBlank() {
+        // given
+        String invalidTryCount = "";
+
+        // when
+        // then
+        assertThatThrownBy(() -> new TryCountValidator(invalidTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도할 횟수를 입력해주세요.");
+    }
+
+}
