cf64fd6429fa2516bb274cfb013bb38c399707a2
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 18:17:31 +0900

    feat(Cars): 중복된 이름 유무 판별하는 유효성 검사 기능 구현

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,7 +1,10 @@
 package racingcar.domain.car;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import racingcar.mapper.CarMapper;
 
 public class Cars {
@@ -9,6 +12,7 @@ public class Cars {
 
     public Cars(String names) {
         List<Car> targetCars = CarMapper.convertToCars(names);
+        validateDuplicateName(targetCars);
         this.cars = targetCars;
     }
 
@@ -22,5 +26,20 @@ public class Cars {
 
     public int size() {
         return cars.size();
+    }
+
+    private void validateDuplicateName(List<Car> cars) {
+        if (isContainDuplicateName(cars)) {
+            throw new IllegalArgumentException("중복되는 자동차 이름이 존재합니다.");
+        }
+    }
+
+    private boolean isContainDuplicateName(List<Car> cars) {
+        List<Car> target = cars.stream()
+                .map(Car::getName)
+                .distinct()
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
+        return target.size() != cars.size();
     }
 }
