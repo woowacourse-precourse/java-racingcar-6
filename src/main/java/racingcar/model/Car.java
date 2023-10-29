9286025f03745e06836e35f16666969f435abeed
jihun <pjh2996@naver.com>
2023-10-29 22:02:07 +0900

    feature : 자동차 이름을 쉼표(,)를 기준으로 구분하는 기능 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,21 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Car {
-    private List<String> car_name;
+    private List<String> car_name = new ArrayList<>();
 
     public void setCar_name(String name) {
+        divideName(name);
+    }
 
+    private void divideName(String name) {
+        StringTokenizer tokenizer = new StringTokenizer(name,",");
+
+        while (tokenizer.hasMoreTokens()) {
+            car_name.add(tokenizer.nextToken().trim());
+        }
     }
 }
