10fd92d1148b08c5d5927f281519fc71b5009908
acs0209 <annjae4676@naver.com>
2023-10-29 14:35:54 +0900

    feat: add message constant

---
+++ src/main/java/racingcar/constant/MessageConst.java
@@ -0,0 +1,19 @@
+package racingcar.constant;
+
+/**
+ * 메시지 관련 상수 관리 클래스
+ */
+public class MessageConst {
+
+    public static final String LENGTH_MESSAGE = "자동차 이름의 길이는 1 ~ 5 여야합니다.";
+
+    public static final String DUPLICATE_MESSAGE = "중복된 값을 입력할 수 없습니다.";
+
+    public static final String SPACE_MESSAGE = "이름에 공백이 들어가면 안됩니다.";
+
+    public static final String EMPTY_MESSAGE = "이름에 빈 값이 들어올 수 없습니다.";
+
+    public static final String NULL_MESSAGE = "값에 null이 들어올 수 없습니다.";
+
+    public static final String COMMA_MESSAGE = "자동차 이름들은 , 로 구분 되어야 합니다.";
+}
