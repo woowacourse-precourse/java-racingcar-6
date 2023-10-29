0440a7b72e423da74e3a4f82aac09e73c9bfbcfc
sunghyun <shlim0287@naver.com>
2023-10-30 01:21:25 +0900

    feat : 사용자에게 차 이름,시도 회수 입력 요청 메소드 추가, 입력값 예외처리 메소드 추가.

--- src/main/java/racingcar/Service/RaceService.java
+++ src/main/java/racingcar/Service/RaceService.java
@@ -1,4 +1,52 @@
 package racingcar.Service;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import racingcar.domain.Car;
+
 public class RaceService {
+
+    Car car=new Car();
+    private int requestAttempts(){
+        String attemptsInput = Console.readLine();
+        return validateConvertToInt(attemptsInput);
+    }
+
+    private static int validateConvertToInt(String attemptsInput) {
+        try {
+            return Integer.parseInt(attemptsInput);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자를 입력하십시오.");
+        }
+    }
+
+    private List<String> requestCarNames(){
+        String carListInput = Console.readLine();
+        StringTokenizer st=new StringTokenizer(carListInput,",");
+        Set<String> tokenSet=new HashSet<>();
+        while(st.hasMoreTokens()){
+            String carName = validateDelimiter(st);
+            validateDuplication(tokenSet, carName);
+            car.addCarName(carName);
+        }
+        return car.getCarNameList();
+    }
+
+    private static void validateDuplication(Set<String> tokenSet, String carName) {
+        if(!tokenSet.add(carName)){
+            throw new IllegalArgumentException("차 이름이 중복되었습니다.");
+        }
+    }
+
+    private static String validateDelimiter(StringTokenizer st) {
+        String carName = st.nextToken();
+        String[] token=carName.split("[^a-zA-Z0-9]+");
+        if(token.length>1){
+            throw new IllegalArgumentException("쉼표(,)를 기준으로 구분하여 입력하십시오.");
+        }
+        return carName;
+    }
 }
