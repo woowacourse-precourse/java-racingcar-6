bd92e72253fbd993f88e264ea6b4a3ced663f653
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 22:11:35 +0900

    feat(carGenerator): 차 이름의 길이는 CarGenerator에서 검증하도록 변경

--- src/main/java/racingcar/CarGenerator.java
+++ src/main/java/racingcar/CarGenerator.java
@@ -3,13 +3,22 @@ package racingcar;
 import java.util.List;
 
 public class CarGenerator {
-    public Car generate(String name) {
-        return new Car(name);
+    private final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이하만 가능합니다.";
+
+    public Car generate(String carName) {
+        validateCarNameLength(carName);
+        return new Car(carName);
     }
 
     public List<Car> generate(List<String> names) {
         return names.stream()
                 .map(this::generate)
                 .toList();
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
+        }
     }
 }
--- src/main/java/racingcar/Viewer.java
+++ src/main/java/racingcar/Viewer.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 public class Viewer {
     private final String CAR_NAME_DELIMITER = ",";
-    private final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이하만 가능합니다.";
     private final String INVALID_INPUT_TYPE = "입력은 자동차 이름과 구분자(,)만 가능합니다.";
     private final Integer MIN_TRY_COUNT = 1;
     private final String TRY_COUNT_ONLY_NUMBER = "시도 횟수는 숫자여야 합니다.";
@@ -20,10 +19,8 @@ public class Viewer {
         return input;
     }
 
-    public List<String> readCarNames() {
-        List<String> carNameList = Arrays.asList(input().split(CAR_NAME_DELIMITER));
-        carNameList.forEach(this::validateCarNameLength);
-        return carNameList;
+    public List<String> inputCarNames() {
+        return Arrays.asList(input().split(CAR_NAME_DELIMITER));
     }
 
     public int inputTryCount() {
@@ -33,12 +30,6 @@ public class Viewer {
             return tryCount;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(TRY_COUNT_ONLY_NUMBER);
-        }
-    }
-
-    private void validateCarNameLength(String carName) {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
         }
     }
 
@@ -53,5 +44,4 @@ public class Viewer {
             throw new IllegalArgumentException(MINIMUM_TRY_COUNT);
         }
     }
-
 }
