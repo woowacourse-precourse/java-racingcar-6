896402fd236d14c8bf0602f45eaca25f55ebf884
DESKTOP-FH4O6OD\syt11 <syt1128@gmail.com>
2023-10-30 00:09:28 +0900

    :fix: Package 오타 수정
    오타가 있던 veiw에 있던 RacingGameOutputs를 새 패키지 view로 옮김

--- src/main/java/racingcar/view/RacingGameOutputs.java
+++ src/main/java/racingcar/view/RacingGameOutputs.java
@@ -1,6 +1,6 @@
-package racingcar.veiw;
+package racingcar.view;
 
-public class racingGameOutputs {
+public class RacingGameOutputs {
     final public static String pleaseInuptCarNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     final public static String inputNumberOfAttempt = "시도할 회수는 몇회인가요?";
     final public static String attemptResult = "실행 결과";
