bcad93f0d1a1364698d6045035d161acac8ca0bc
honee8583 <honee8583@naver.com>
2023-10-29 19:08:05 +0900

    test(ApplicationTest): add test case of moving cars

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,8 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -14,6 +19,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void move_자동차_이동() {
+        String[] carNames = new String[]{"andy", "cindy", "suzy"};
+        String[] result = new String[]{"" ,"--", "--"};
+
+        Map<String, String> forwardMap = new HashMap<>();
+        for (int i = 0; i < carNames.length; i++) {
+            forwardMap.put(carNames[i], "");
+        }
+
+        int move = 3;
+        List<Integer>[] moveList = new List[]{
+                Arrays.asList(1, 4, 7),
+                Arrays.asList(1, 2, 8),
+                Arrays.asList(1, 4, 2)};
+        for (int i = 0; i < move; i++) {
+            List<Integer> moves = moveList[i];
+            for (int j = 0; j < moveList[i].size(); j++) {
+                if (moves.get(j) >= 4) {
+                    forwardMap.put(carNames[j], forwardMap.get(carNames[j]) + "-");
+                }
+            }
+        }
+
+        for (int i = 0; i < carNames.length; i++) {
+            assertEquals(result[i], forwardMap.get(carNames[i]));
+        }
+    }
 
     @Test
     void getCars_자동차이름_저장() {
