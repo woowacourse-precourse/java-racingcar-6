21bcd5b6247bd34150745e04a455aad886067bb3
elyudwo <dudqk9696@naver.com>
2023-10-29 21:32:15 +0900

    [Feat] : 자동차 이름 1~5 사이 아닐경우 예외 발생 기능 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,11 +1,24 @@
 package racingcar.validator;
 
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class InputValidator {
 
     public void validateCarNameEmptyOrNull(String names) {
         if (names == null || names.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름은 비어있으면 안됩니다.");
         }
+    }
+
+    public void validateCarNameLength(String names) {
+        Stream<String> nameStream = Arrays.stream(names.split(",")).map(String::trim);
+
+        nameStream.forEach(name -> {
+            if (name.length() < 1 || name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름의 길이는 1에서 5 사이여야 합니다.");
+            }
+        });
     }
 }
