2050da9a8cb24ffa8886ca2edfc45aba82bc2790
HaiSeong <wjdgotjd9908@gmail.com>
2023-10-29 12:34:56 +0900

    test: RacingCar 테스트 클래스 작성

---
+++ src/test/java/study/model/RacingCarTest.java
@@ -0,0 +1,27 @@
+package study.model;
+
+import org.junit.jupiter.api.Test;
+import racingcar.model.RacingCar;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RacingCarTest {
+
+    @Test
+    void test_정상적인경우() {
+        // given
+        RacingCar racingCar = new RacingCar();
+
+        // when
+        for (int i = 0; i < 5; i++) {
+            racingCar.excuteRound();
+        }
+        int distance = racingCar.getDistance();
+
+        // then
+        assertThat(distance).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(5);
+    }
+}
