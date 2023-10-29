1e1493e62aae2a0f5053c98ef1d2343d0045c576
Oseuseu <lakingsung@gmail.com>
2023-10-29 23:35:28 +0900

    test: getAttemptInput 메서드 테스트 코드 작성

--- src/test/java/racingcar/RaceViewTest.java
+++ src/test/java/racingcar/RaceViewTest.java
@@ -1,20 +1,49 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RaceViewTest {
+    private RaceView view;
+    private InputStream originalIn;
+
+    @BeforeEach
+    void setUp() {
+        view = new RaceView();
+        originalIn = System.in;
+    }
+
+    @AfterEach
+    void restoreIn() {
+        System.setIn(originalIn);
+        Console.close();
+    }
+
     @Test
-    void getCarInput_테스트() {
+    void getCarListInput_테스트() {
         String input = "test";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        RaceView view = new RaceView();
-
         String result = view.getCarListInput();
+
+        assertEquals(input, result);
+    }
+
+    @Test
+    void getAttemptInput_테스트() {
+        String input = "5";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+
+        String result = view.getAttemptInput();
+
         assertEquals(input, result);
     }
 }
