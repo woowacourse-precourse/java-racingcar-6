bb40516fcb6c2ce63118ef2a8786895727b4ddf7
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-29 21:59:00 +0900

    feat(JudgeMachine) : create JudgeMachine to get winner Cars

---
+++ src/main/java/racingcar/JudgeMachine.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JudgeMachine {
+    private void judge(List<Car> carList) {
+        Integer winningDistance = 0;
+        for (Car car : carList) {
+            if (car.getLocation() > winningDistance) {
+                winningDistance = car.getLocation();
+            }
+        }
+        for (Car car : carList) {
+            if (car.getLocation().equals(winningDistance)) {
+                car.setWinner();
+            }
+        }
+    }
+
+    public List<Car> getWinningCars(List<Car> carList) {
+        judge(carList);
+        List<Car> winningCars = new ArrayList<>();
+        for (Car car : carList) {
+            if (car.isWinner()) {
+                winningCars.add(car);
+            }
+        }
+        return winningCars;
+    }
+}
