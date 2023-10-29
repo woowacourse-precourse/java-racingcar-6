a0a4c0108d4bdbb094b37fae3504bbec803e5acd
OikimiO <etkim02@naver.com>
2023-10-29 15:37:54 +0900

    test: 게임 시작 기능 구현
    - 입/출력을 이용해 게임 시작기능을 구현
    - 출력값중 누락된 부분 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -7,6 +7,7 @@ import racingcar.CheckMoveAndStopTest.CheckMoveAndStop;
 
 public class RacingCarTest {
     private List<StringBuilder> winCount = new ArrayList<>();
+    private int maxCount = 0;
 
     @Test
     void gameStart(){
@@ -15,23 +16,38 @@ public class RacingCarTest {
 
         OutputCarRace.printf(MessageType.INPUT_CAR_RACING_COUNT_PRINT);
         InputCarRace.inputMove("2");
+        System.out.println();
+
         OutputCarRace.print(MessageType.OUPUT_RACE_RESULT_PRINT);
 
         for (String carName : InputCarRace.getCarNumbers()) {
-            winCount.add(new StringBuilder());
+            this.winCount.add(new StringBuilder());
         }
 
         for (int i = 0; i < InputCarRace.getMoveCount(); i++) {
             for (int j = 0; j < InputCarRace.getCarNumbers().size(); j++) {
                 if(CheckMoveAndStop.isMove()){
-                    winCount.set(j,winCount.get(j).append("-"));
+                    this.winCount.set(j,this.winCount.get(j).append("-"));
+
+                    if(this.maxCount < winCount.get(j).length()){
+                        this.maxCount = winCount.get(j).length();
+                    }
                 }
 
-                OutputCarRace.printf(MessageType.OUPUT_IMPLEMENTATION_RESULT_PRINT, InputCarRace.getCarNumbers().get(j), winCount.get(j).toString());
+                OutputCarRace.printf(MessageType.OUPUT_IMPLEMENTATION_RESULT_PRINT, InputCarRace.getCarNumbers().get(j), this.winCount.get(j).toString());
             }
             System.out.println();
         }
 
+        StringBuilder winners = new StringBuilder();
+
+        for (int i = 0; i < winCount.size(); i++) {
+            if(this.maxCount == winCount.get(i).length()){
+                winners.append(InputCarRace.getCarNumbers().get(i)).append(", ");
+            }
+        }
+
+        System.out.println("최종 우승자 : " + winners.substring(0,winners.length()-2));
 
     }
 }
