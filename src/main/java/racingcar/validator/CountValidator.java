d4c5a2fc1efbe6a1506b254c535c6f609aa852d7
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-29 22:48:41 +0900

    refactor: 시도 횟수 입력 예외 발생 시 메시지 출력하도록 변경

--- src/main/java/racingcar/validator/CountValidator.java
+++ src/main/java/racingcar/validator/CountValidator.java
@@ -2,12 +2,15 @@ package racingcar.validator;
 
 public class CountValidator {
 
+    private static final String NOT_NUMBER = "숫자가 아닙니다.";
+    private static final String IS_NEGATIVE_NUMBER = "0이상 입력해주세요.";
+
     public static int checkNumber(String count){
         int countNumber;
         try{
             countNumber = Integer.parseInt(count);
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_NUMBER);
         }
 
         return countNumber;
@@ -15,7 +18,7 @@ public class CountValidator {
 
     public static void checkNegativeNumber(int countNumber){
         if (countNumber < 0){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(IS_NEGATIVE_NUMBER);
         }
     }
 }
