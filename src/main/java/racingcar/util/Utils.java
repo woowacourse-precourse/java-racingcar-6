2d9fba90b217f84f732d91eb2fa64ddd6444eea5
elyudwo <dudqk9696@naver.com>
2023-10-29 22:08:23 +0900

    [Feat] : 자동차 이름 입력 문자열 List 변환 기능 구현

--- src/main/java/racingcar/util/Utils.java
+++ src/main/java/racingcar/util/Utils.java
@@ -1,8 +1,26 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.validator.InputValidator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
+
+    private final InputValidator inputValidator;
+
+    public Utils() {
+        inputValidator = new InputValidator();
+    }
+
+    public List<String> convertStringToList(String names) {
+        inputValidator.validateCarName(names);
+        return Arrays.stream(names.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
 
     public static int makeRandomNumber() {
         return Randoms.pickNumberInRange(0,9);
