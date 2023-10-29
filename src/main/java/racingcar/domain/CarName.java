5684045bed234a651dbcec1b746f161f6afe97e9
SJ70 <seojae70@gmail.com>
2023-10-29 18:09:26 +0900

    Feat & Test: CarName 길이 검증

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -2,10 +2,23 @@ package racingcar.domain;
 
 public class CarName {
 
+    private final int MIN_LENGTH = 1;
+    private final int MAX_LENGTH = 5;
+
     private final String name;
 
     public CarName(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        int nameLength = name.length();
+        boolean isLengthBelow = nameLength < MIN_LENGTH;
+        boolean isLengthOver = nameLength > MAX_LENGTH;
+        if (isLengthBelow || isLengthOver) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
---
+++ src/test/java/racingcar/car/CarNameLengthValidateTest.java
@@ -0,0 +1,38 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.CarName;
+
+public class CarNameLengthValidateTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1", "22", "333", "4444", "55555"
+    })
+    @DisplayName("차 이름 생성 시 길이 기준 이내의 문자열을 허용한다.")
+    void carNameLengthValidateSuccessTest(String name) {
+        // then
+        Assertions.assertDoesNotThrow(() -> {
+            new CarName(name);
+        });
+    }
+
+    @Test
+    @DisplayName("차 이름 생성 시 길이 기준 미만/초과의 문자열을 불허한다.")
+    void carNameLengthValidateFailTest_Below() {
+        // given
+        String name_belowLength = "";
+        String name_overLength = "666666";
+
+        // then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new CarName(name_belowLength);
+            new CarName(name_overLength);
+        });
+    }
+
+}
