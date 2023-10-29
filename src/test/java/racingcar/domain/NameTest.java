e7390d4816913d4c66353c8d64d417593ace7a9b
Seol_JY <wlsdud5654@gmail.com>
2023-10-29 15:06:15 +0900

    refactor(NameTest): 테스트에 주석 추가
    given, when, then 주석을 추가

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -11,33 +11,50 @@ class NameTest {
     @ParameterizedTest
     @ValueSource(strings = {"ab", "cde", "fghi"})
     public void 중간_길이_이름_테스트(String validName) {
+        // When
         Name name = Name.from(validName);
+
+        // Then
         assertEquals(validName, name.toString());
     }
 
     @Test
     public void 최소_길이_이름_테스트() {
+        // Given
         String validName = "a";
+
+        // When
         Name name = Name.from(validName);
+
+        // Then
         assertEquals(validName, name.toString());
     }
 
     @Test
     public void 최대_길이_이름_테스트() {
+        // Given
         String validName = "David";
+
+        // When
         Name name = Name.from(validName);
+
+        // Then
         assertEquals(validName, name.toString());
     }
 
     @Test
     public void 잘못된_짧은_이름_예외_처리() {
+        // Given
         String invalidName = "";
+
+        // When, Then
         assertThrows(IllegalArgumentException.class, () -> Name.from(invalidName));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Adolph", "Wolverine", "Christopher"}) // 여러 정책에 대해 테스트
     public void 잘못된_긴_이름_예외_처리(String invalidName) {
+        // When, Then
         assertThrows(IllegalArgumentException.class, () -> Name.from(invalidName));
     }
 }
