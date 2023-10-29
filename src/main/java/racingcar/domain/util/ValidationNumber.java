a66b2d0b8d827a8422d752c2f263ecb34c26814e
junseoks-99 <angec5645@naver.com>
2023-10-30 00:55:37 +0900

    feat: 시도할 횟수의 입력이 숫자가 아닌지 확인하는 기능 구현

--- src/main/java/racingcar/domain/util/ValidationNumber.java
+++ src/main/java/racingcar/domain/util/ValidationNumber.java
@@ -2,10 +2,21 @@ package racingcar.domain.util;
 
 public class ValidationNumber {
 
+    private ValidationNumber() {}
+
+    private static final String removeNumberRegex = "[0-9]";
+
     public static void validateIsEmpty(String attemptNumber) {
         attemptNumber = attemptNumber.trim();
         if (attemptNumber.isEmpty()) {
             throw new IllegalArgumentException(ExceptionMessages.EMPTY.getExceptionMessage());
+        }
+    }
+
+    public static void validateIsNumber(String attemptNumber) {
+        String removedNumberString = attemptNumber.replaceAll(removeNumberRegex, "");
+        if (removedNumberString.isEmpty() == false) {
+            throw new IllegalArgumentException(ExceptionMessages.NOT_NUMBER.getExceptionMessage());
         }
     }
 }
