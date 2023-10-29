b9ada27f082b568adbcd3e859449c0b726f2fcab
ikjo39 <chi4321@naver.com>
2023-10-29 19:29:55 +0900

    refactor: StringFormatter 추가에 따른 리팩토링
    - 자동차 위치와 구분하기 위해 필드명 변경
    - 우승자 찾는 로직 중 중복 로직 제거
    - String Format 책임 분리

--- src/main/java/racingcar/model/CarsPosition.java
+++ src/main/java/racingcar/model/CarsPosition.java
@@ -2,14 +2,15 @@ package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import racingcar.util.StringFormatter;
 
 public final class CarsPosition {
-    private final Map<String, Integer> position;
+    private final Map<String, Integer> carsPosition;
 
     private CarsPosition(Map<String, Integer> position) {
-        this.position = position;
+        this.carsPosition = position;
     }
 
     public static CarsPosition createPositionWithRacingCars(RacingCars racingCars) {
@@ -18,34 +19,34 @@ public final class CarsPosition {
 
     public String getEachRaceResult() {
         StringBuilder sb = new StringBuilder();
-        for (String key : position.keySet()) {
+        for (String key : carsPosition.keySet()) {
             checkStepForward(key);
-            sb.append(getNameAndPosition(key));
+            String signs = convertPositionToSigns(carsPosition.get(key));
+            sb.append(StringFormatter.raceResultFormat(key, signs));
         }
         return sb.toString();
     }
 
     public String getWinner() {
-        int max = Collections.max(position.values());
-        List<String> winners = position.keySet()
-                .stream()
-                .filter(name -> position.get(name).equals(max))
-                .toList();
-        return String.format("최종 우승자 : %s", String.join(", ", winners));
+        int maxValue = Collections.max(carsPosition.values());
+        return StringFormatter.winnerFormat(getWinnerToMaxValue(maxValue));
     }
 
     private void checkStepForward(String key) {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
-            position.put(key, position.get(key) + 1);
+            carsPosition.put(key, carsPosition.get(key) + 1);
         }
     }
 
-    private String getNameAndPosition(String key) {
-        return String.format("%s : %s%n", key, convertPositionToSign(key));
+    private String convertPositionToSigns(int position) {
+        return "-".repeat(position);
     }
 
-    private String convertPositionToSign(String key) {
-        return "-".repeat(position.get(key));
+    private String getWinnerToMaxValue(int maxValue) {
+        return carsPosition.keySet()
+                .stream()
+                .filter(name -> carsPosition.get(name).equals(maxValue))
+                .collect(Collectors.joining(", "));
     }
 }
