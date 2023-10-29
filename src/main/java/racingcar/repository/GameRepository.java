a7e33fd8737e7cd921496dfa90a14a9c347f30ab
becooq81 <becooq81@gmail.com>
2023-10-30 06:34:17 +0900

    feat(GameRepository): completed game repository class to save and view game objects in hashmap

--- src/main/java/racingcar/repository/GameRepository.java
+++ src/main/java/racingcar/repository/GameRepository.java
@@ -1,2 +1,24 @@
-package racingcar.repository;public class GameRepository {
+package racingcar.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import racingcar.model.Car;
+import racingcar.model.Game;
+
+public class GameRepository {
+    private Long id = 0L;
+    private Map<Long, Game> games = new HashMap<>();
+
+    public Long save(Game game) {
+        games.put(id, game);
+        return id++;
+    }
+
+    public Game findById(Long id) {
+        return games.get(id);
+    }
+
+    public Map<Long, Game> findAll() {
+        return games;
+    }
 }
