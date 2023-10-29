637405cb6a20d3c58285d0be087f17b3bfa309c3
Jiyoung Choi <12201817@inha.edu>
2023-10-29 20:35:51 +0900

    refactor(ExceptionMessage) : 유효성 검사 조건 추가 & 수정

--- src/main/java/racingcar/util/ExceptionMessage.java
+++ src/main/java/racingcar/util/ExceptionMessage.java
@@ -1,10 +1,10 @@
 package racingcar.util;
 
 public enum ExceptionMessage {
-    INPUT_NO_CAR_NAME_MESSAGE("경주할 자동차의 이름을 ,로 구분하여 입력해야 합니다."),
-    INPUT_WRONG_CAR_LENGTH_MESSAGE("길이가 5이하인 자동차 이름을 입력해야 합니다."),
+    INPUT_NO_CAR_NAME_MESSAGE("1개 이상의 자동차 이름을 입력해야 합니다."),
+    INPUT_DUPLICATE_CAR_NAME_MESSAGE("모두 다른 자동차 이름을 입력해야 합니다."),
+    INPUT_WRONG_CAR_NAME_LENGTH_MESSAGE("길이가 1이상 5이하인 자동차 이름을 입력해야 합니다."),
     INPUT_NO_COUNT_MESSAGE("시도 횟수를 입력해야 합니다."),
-    INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
     INPUT_WRONG_RANGE_OF_COUNT_MESSAGE("시도 횟수는 1이상이어야 합니다.");
 
     private final String exceptionMessage;
@@ -13,7 +13,7 @@ public enum ExceptionMessage {
         this.exceptionMessage = exceptionMessage;
     }
 
-    public void throwException(){
+    public void throwException() {
         throw new IllegalArgumentException(exceptionMessage);
     }
 }
