bc3545ca3d1fc01f00b12ff26054889209772234
Nara Park <skfk6352@naver.com>
2023-10-30 00:36:25 +0900

    <feat>: create method for validating game count
    게임 횟수에 대한 입력값이 자연수인지 검증하는 메서드 구현

--- src/main/java/racingcar/validation/CarNameValidator.java
+++ src/main/java/racingcar/validation/CarNameValidator.java
@@ -9,7 +9,7 @@ public class CarNameValidator {
 
     private static final int CAR_NAME_MIN_SIZE = 1;
     private static final int CAR_NAME_MAX_SIZE = 5;
-    private static final Pattern CAR_NAME_REGEX = Pattern.compile("^[0-9a-zA-Zㄱ-ㅎ가-힣]*$");
+    private static final Pattern CAR_NAME_TYPE_REGEX = Pattern.compile("^[0-9a-zA-Zㄱ-ㅎ가-힣]*$");
     private static final String CAR_NAME_LENGTH_EXCEPTION_MESSAGE = "자동차의 이름은 1자 이상, 5자 이하여야 합니다.";
     private static final String CAR_NAME_DUPLICATION_EXCEPTION_MESSAGE = "자동차의 이름은 중복될 수 없습니다.";
     private static final String CAR_NAME_TYPE_EXCEPTION_MESSAGE = "자동차의 이름은 특수문자를 제외한 문자와 숫자만 허용합니다.";
@@ -41,7 +41,7 @@ public class CarNameValidator {
 
     private boolean validateNameType(List<String> carNameList) {
         for (String carName : carNameList) {
-            if (!CAR_NAME_REGEX.matcher(carName).matches()) {
+            if (!CAR_NAME_TYPE_REGEX.matcher(carName).matches()) {
                 return false;
             }
         }
---
+++ src/main/java/racingcar/validation/GameCountValidator.java
@@ -0,0 +1,19 @@
+package racingcar.validation;
+
+import java.util.regex.Pattern;
+
+public class GameCountValidator {
+
+    private static final Pattern GAME_COUNT_REGEX = Pattern.compile("[1-9]+");
+    private static final String GAME_COUNT_TYPE_EXCEPTION_MESSAGE = "게임 횟수는 자연수여야 한다.";
+
+    public void validateGameCount(String userInput) {
+        if (!validateCountType(userInput)) {
+            throw new IllegalArgumentException(GAME_COUNT_TYPE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean validateCountType(String userInput) {
+        return GAME_COUNT_REGEX.matcher(userInput).matches();
+    }
+}
