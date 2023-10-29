61722512ee10f8378ba404e2e096f4ffcb992b8f
little6523 <little6523@naver.com>
2023-10-30 01:06:36 +0900

    feat : 게임 시작 문구 출력 기능  - docs/README.md 9번 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
 public class Application {
+//    private List<String> CarNames = new ArrayList<>();
+
+    private static void displaySrtartText() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+//    private static void inputCarNames() {
+//
+//    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        displayStartText();
     }
 }
