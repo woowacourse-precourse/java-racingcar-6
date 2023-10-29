9ada342971797fb043a3845d54443e7e420c6ae4
PARK9898 <wjddjs21@naver.com>
2023-10-29 23:41:45 +0900

    "refactor: Getter 삭제 후 compareTo Overriding"

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,14 @@
 package racingcar.domain;
 
-public class Car {
-
+public class Car implements Comparable<Car>{
+    private static final int MOVE_CONDITION = 4;
     private final String name;
     private int position;
 
-    public Car(String name) {
+    public Car(String name, int position) {
         validateCarName(name);
         this.name = name;
-        this.position = 0;
+        this.position = position;
     }
 
     private void validateCarName(String name) {
@@ -18,14 +18,13 @@ public class Car {
     }
 
     public void moveOrStop(int number) {
-        if(number >= 4) {
+        if(number >= MOVE_CONDITION) {
             position++;
         }
     }
 
-    public int getPosition() {
-        return position;
+    @Override
+    public int compareTo(Car other) {
+        return this.position - other.position;
     }
-
-
 }
