8b3b0a5d90d331f525d93ae1e891b4ef8ae0d2ee
yubin <dbqls2821@gmail.com>
2023-10-29 23:11:51 +0900

    fix: 정적 팩토리 -> 생성자로 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -11,19 +11,19 @@ final class CarTest {
 
     @Test
     void 이름이_null인_차는_인스턴스화_될때_에러가_발생한다() {
-        assertThrows(IllegalArgumentException.class, () -> Car.create(null));
+        assertThrows(IllegalArgumentException.class, () -> new Car(null));
     }
 
     @Test
     void 요구사항에_맞는_이름으로_인스턴스화할시_예외가_발생하지_않는다() {
-        assertDoesNotThrow(() -> Car.create("woowa"));
+        assertDoesNotThrow(() -> new Car("woowa"));
     }
 
     @Test
     void 동일한_이름을_가진_차는_동일한_객체로_인식한다() {
         String sameName = "동일한이름";
-        Car car1 = Car.create(sameName);
-        Car car2 = Car.create(sameName);
+        Car car1 = new Car(sameName);
+        Car car2 = new Car(sameName);
 
         assertEquals(car1, car2);
     }
@@ -31,8 +31,8 @@ final class CarTest {
     @Test
     void 동일한_이름을_가진_차는_Hash_자료구조에서도_동일한_객체로_인식된다() {
         String sameName = "동일한이름";
-        Car car1 = Car.create(sameName);
-        Car car2 = Car.create(sameName);
+        Car car1 = new Car(sameName);
+        Car car2 = new Car(sameName);
         Set<Car> set = new HashSet<>();
 
         set.add(car1);
@@ -44,7 +44,7 @@ final class CarTest {
     @Test
     void 자신의_현재상태를_출력형식에_맞는_String으로_반환한다() {
         String expected = "woowa : --";
-        Car car = Car.create("woowa");
+        Car car = new Car("woowa");
         car.move();
         car.move();
 
