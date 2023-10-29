ef0077df6d959e66f5a66c480e6cd81c7e0cb004
Sully <2dcoder@naver.com>
2023-10-29 21:55:50 +0900

    feat(TryCountValidator): 시도할 횟수가 양의 정수가 아니라면 예외가 발생하는 메서드 추가

--- src/main/java/racingcar/domain/TryCountValidator.java
+++ src/main/java/racingcar/domain/TryCountValidator.java
@@ -8,6 +8,7 @@ public class TryCountValidator {
         this.tryCount = tryCount;
         validateTryCountExists();
         validateTryCountIsInteger();
+        validateTryCountIsPositive();
     }
 
     public void validateTryCountExists() {
@@ -21,6 +22,12 @@ public class TryCountValidator {
             Integer.parseInt(tryCount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도할 횟수는 정수여야 합니다.");
+        }
+    }
+
+    public void validateTryCountIsPositive() {
+        if (Integer.parseInt(tryCount) <= 0) {
+            throw new IllegalArgumentException("시도할 횟수는 양의 정수여야 합니다.");
         }
     }
 
