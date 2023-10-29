b7914b9df9a18bbf9d13e8c57136aecfef20c2ea
박승민 <tmddudals369@naver.com>
2023-10-30 04:54:13 +0900

    feat(car): 생성된 수에 따른 자동차 움직임 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,10 +3,18 @@ package racingcar.domain;
 import racingcar.utils.RandomNumberGenerator;
 
 public class Car {
+    private static final int MOVING_CONDITION = 4;
     private final String name;
     private int movingCount;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move() {
+        int randomNumber = RandomNumberGenerator.generate();
+        if (randomNumber >= MOVING_CONDITION) {
+            movingCount++;
+        }
     }
 }
