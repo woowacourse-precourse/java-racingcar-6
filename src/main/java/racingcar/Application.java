ede2022769da869d87cbe0b6f3066072f356fbef
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 21:11:39 +0900

    refactor(Application): Controller를 호출하여 전반적인 게임이 진행되도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarController racingCarController = Configuration.racingCarController();
+        racingCarController.inputCarNames();
+        racingCarController.inputTryNumber();
+        racingCarController.racingGameRun();
+        racingCarController.outputWinner();
     }
 }
