db05d6e6a85f8f6ef5111426f18e097cead0ad6b
Junobee25 <wnsdhqo@naver.com>
2023-10-29 17:01:26 +0900

    style: 메서드와 로직 사이 줄바꿈 수정

--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -17,6 +17,7 @@ public class RacingGame {
 
 
     public void RacingGameStart() {
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String stringCarList = carGenerator.inputCarList();
         car.setStringCarList(stringCarList);
