5ceab5fe692457871104b429e69dfafb79c4db4d
Soobin-Park <jrkd2165@gmail.com>
2023-10-29 17:11:15 +0900

    :sparkles: feat: 사용자에게 경주할 자동차 이름, 게임 시도 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    public static void game() throws IllegalArgumentException{
+
+        //사용자에게 입력값 (경주할 자동차 이름, 게임을 시도할 회수) 입력받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String stringCars = readLine();
+        List<String> cars = List.of(stringCars.split(","));
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numberOfGames = Integer.parseInt(readLine());
+
+
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        game();
     }
 }
