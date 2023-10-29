c8f5798f624441415768fc90a51241e5fc88a1a9
2021110806 <2021110806@dgu.ac.kr>
2023-10-29 21:35:29 +0900

    test(InputTest.java): add getCarNameLengthExceptionTest 사용자로부터 자동차 이름을 입력받는 기능의 경주할 자동차의 이름이 5자가 넘는 예외 경우에 대한 테스트를 추가하였습니다.

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,8 +1,11 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static racingcar.Exception.INVALID_CAR_NAME_LENGTH_MESSAGE;
 import static racingcar.Input.getCarName;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.io.ByteArrayInputStream;
@@ -21,5 +24,18 @@ public class InputTest {
         InputStream userInputStream = generateByteArrayInputStream(userInput);
         System.setIn(userInputStream);
         assertArrayEquals(answer,getCarName());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"car1,car2,overFiveCharacters"})
+    void getCarNameLengthExceptionTest(String userInput) {
+    InputStream userInputStream = generateByteArrayInputStream(userInput);
+    System.setIn(userInputStream);
+    try {
+        getCarName();
+    } catch (IllegalArgumentException illegalArgumentException) {
+        Assertions.assertEquals(INVALID_CAR_NAME_LENGTH_MESSAGE, illegalArgumentException.getMessage());
+    }
+
     }
 }
