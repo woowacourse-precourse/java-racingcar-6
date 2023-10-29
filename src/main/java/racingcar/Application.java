da781c80ff00bec277c5acca40652fc5a300208a
Hajuuu <hpt0127@naver.com>
2023-10-29 18:58:56 +0900

    refactor(userCar): Map을 List로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,26 +2,26 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Application {
     public static void main(String[] args) {
-        Map<String, Integer> userMap = userCar();
+        List<User> users = userCar();
 
     }
 
-    public static Map<String, Integer> userCar() {
+    public static List<User> userCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputString = readLine();
 
         StringTokenizer st = new StringTokenizer(inputString, ",");
-        Map<String, Integer> map = new LinkedHashMap<>();
+        List<User> users = new ArrayList<>();
         while (st.hasMoreTokens()) {
-            map.put(st.nextToken(), 0);
+            users.add(new User(st.nextToken(), 0));
         }
 
-        return map;
+        return users;
     }
 }
