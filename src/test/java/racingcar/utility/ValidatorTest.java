f314241bbe4484ba42bcc7710fb4a6cb23c25d50
gumgu <bienlee@naver.com>
2023-10-29 21:10:19 +0900

    Style : 테스트 코드 메서드명에 테스트하는 기능의 메서명 붙여줌

--- src/test/java/racingcar/utility/ValidatorTest.java
+++ src/test/java/racingcar/utility/ValidatorTest.java
@@ -1,31 +1,31 @@
 package racingcar.utility;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
 
     @Test
-    void 입력값이_공백인_경우_true_반환() {
+    void isBlank_입력값이_공백인_경우_true_반환() {
 
         boolean stringNull = Validator.isBlank(null);
         boolean blank = Validator.isBlank(" ");
         boolean empty = Validator.isBlank("");
+        boolean normal = Validator.isBlank("가나 다라");
 
         assertThat(stringNull).isTrue();
         assertThat(blank).isTrue();
         assertThat(empty).isTrue();
+        assertThat(normal).isFalse();
 
     }
 
     @Test
-    void 전달받은_이름이_5글자_이상인_경우_false_반환() {
+    void isInNameRange_전달받은_이름이_5글자_이상인_경우_false_반환() {
         boolean nineLength = Validator.isInNameRange("123456789");
         boolean sixLength = Validator.isInNameRange("123456");
 
@@ -34,7 +34,7 @@ class ValidatorTest {
     }
 
     @Test
-    void 전달받은_이름이_5글자_이하인_경우_true_반환() {
+    void isInNameRange_전달받은_이름이_5글자_이하인_경우_true_반환() {
         boolean fiveLength = Validator.isInNameRange("12345");
         boolean oneLength = Validator.isInNameRange("1");
         boolean zeroLength = Validator.isInNameRange("");
@@ -45,7 +45,7 @@ class ValidatorTest {
     }
 
     @Test
-    void 전달받은_이름들_중_중복된_값이_있는_경우_true_반환() {
+    void hasSameName_전달받은_이름들_중_중복된_값이_있는_경우_true_반환() {
         List<String> koreanNames = Arrays.asList("가", "나", "가", "다");
         List<String> englishNames = Arrays.asList("pobi", "woni", "jun", "pobi");
 
@@ -54,7 +54,7 @@ class ValidatorTest {
     }
 
     @Test
-    void 전달받은_이름들_중_중복된_값이_없는_경우_false_반환() {
+    void hasSameName_전달받은_이름들_중_중복된_값이_없는_경우_false_반환() {
         List<String> koreanNames = Arrays.asList("가", "나", "다", "라");
         List<String> englishNames = Arrays.asList("pobi", "woni", "jun", "gugi");
 
@@ -63,13 +63,12 @@ class ValidatorTest {
     }
 
     @Test
-    void 중복값_체크_다양한_예시() {
+    void hasSameName_중복값_체크_다양한_예시() {
         List<String> notSame = Arrays.asList("가", "", "    ", "가나다", "^%&");
         List<String> same = Arrays.asList("1", "quankei", "!%&(", "1", "0", "0");
 
         assertThat(Validator.hasSameName(notSame)).isFalse();
         assertThat(Validator.hasSameName(same)).isTrue();
-
     }
 
     @Test
