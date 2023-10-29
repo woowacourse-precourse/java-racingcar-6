ad52ebde40ddc66b9362c10e95d90e69c8ab5778
Junobee25 <wnsdhqo@naver.com>
2023-10-29 23:30:01 +0900

    test: domain 영역 테스트 코드 분리 BeforeEach 사용 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.CarGenerator;
 
 import java.io.ByteArrayOutputStream;
@@ -23,11 +24,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -37,54 +38,6 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-    }
-
-    @Test
-    void splitStringByCommas_쉼표기준_분리_테스트() {
-        //given
-        final CarGenerator T = new CarGenerator();
-        final String case1 = "pobi";
-        final String case2 = "pobi,jason";
-        final String case3 = "pobi,jason,juno";
-        final String case4 = "pobi,jason,juno,hyun";
-        final String case5 = "pobi,jason,juno,hyun,minseo";
-
-        //when
-        final String[] result1 = T.splitStringByCommas(case1);
-        final String[] result2 = T.splitStringByCommas(case2);
-        final String[] result3 = T.splitStringByCommas(case3);
-        final String[] result4 = T.splitStringByCommas(case4);
-        final String[] result5 = T.splitStringByCommas(case5);
-
-        //then
-        assertThat(result1).containsExactly("pobi");
-        assertThat(result2).containsExactly("pobi", "jason");
-        assertThat(result3).containsExactly("pobi", "jason", "juno");
-        assertThat(result4).containsExactly("pobi", "jason", "juno", "hyun");
-        assertThat(result5).containsExactly("pobi", "jason", "juno", "hyun", "minseo");
-    }
-
-    @Test
-    void createCarList_자동차리스트_값_테스트() {
-        //given
-        final CarGenerator T = new CarGenerator();
-        final String case1 = "povi1";
-        final String case2 = "povi1,povi2";
-        final String case3 = "povi1,povi2,povi3";
-
-        //when
-        final LinkedHashMap<String, String> result1 = T.createCarList(case1);
-        final LinkedHashMap<String, String> result2 = T.createCarList(case2);
-        final LinkedHashMap<String, String> result3 = T.createCarList(case3);
-
-        //then
-        assertThat(result1).containsKey("povi1");
-        assertThat(result2).containsKey("povi1");
-        assertThat(result2).containsKey("povi2");
-        assertThat(result3).containsKey("povi1");
-        assertThat(result3).containsKey("povi2");
-        assertThat(result3).containsKey("povi3");
-
     }
 
     @Override
