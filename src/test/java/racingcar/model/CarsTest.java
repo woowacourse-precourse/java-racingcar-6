cce6abafe562bb8d52e380a73a2fb9bbd11542e2
정찬영 <jcy0342@gmail.com>
2023-10-29 16:47:19 +0900

    refactor: CarsTest의 Cars 객체가 반복 사용되므로 클래스의 필드 변수로 선언

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -7,10 +7,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
+    private Cars cars = new Cars();
+
     @Test
     void 자동차_이름이_콤마로_구분되어_저장된다() {
         //Given
-        Cars cars = new Cars();
         String inputString = "포비, 워니, 차니";
 
         //When
@@ -27,7 +28,6 @@ public class CarsTest {
     void 시도_횟수_저장() {
         //Given
         String tryCount = "6";
-        Cars cars = new Cars();
 
         //When
         cars.setTryCount(tryCount);
@@ -41,7 +41,6 @@ public class CarsTest {
         //Given
         String negativeNumber = "-1";
         String zero = "0";
-        Cars cars = new Cars();
 
         //When & Then
         assertThrows(IllegalArgumentException.class, () -> cars.setTryCount(negativeNumber));
@@ -52,7 +51,6 @@ public class CarsTest {
     void 시도할_횟수가_숫자가_아니라면_예외발생() {
         //Given
         String inputValue = "sd";
-        Cars cars = new Cars();
 
         //When & Then
         assertThrows(IllegalArgumentException.class, () -> cars.setTryCount(inputValue));
