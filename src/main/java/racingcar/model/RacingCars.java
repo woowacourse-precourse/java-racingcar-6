87fa392fcc40c7f3fd03e241127e5023601b2768
ChanUi <hcu55@naver.com>
2023-10-29 19:47:24 +0900

    feat: 자동차 전진 조건 판단 및 전진 기능 구현

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class RacingCars {
 
+    private static final int FORWARD_JUDGE_NUMBER = 4;
+
     private final List<Car> racingCars;
 
     public RacingCars(List<Car> racingCars) {
@@ -14,6 +16,15 @@ public class RacingCars {
         for (String carName : inputCarName.split(",")) {
             Car generatedCar = new Car(carName);
             racingCars.add(generatedCar);
+        }
+    }
+
+    public void forwardJudge() {
+        for (Car racingCar  : racingCars) {
+            int randomNumber = GenerateRandomNumber.generate();
+            if (randomNumber >= FORWARD_JUDGE_NUMBER) {
+                racingCar.goForward();
+            }
         }
     }
 }
