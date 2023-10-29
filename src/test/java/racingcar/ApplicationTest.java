558f882f3dd6dad28354a10b5c35134afec12dcb
hil679 <hil679922@gmail.com>
2023-10-29 23:48:49 +0900

    Test(OutputView): 차수별 실행 결과 출력 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,6 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -43,6 +47,43 @@ class ApplicationTest extends NsTest {
             assertThat(car.getForwardNum()).isEqualTo(1);
         });
 
+    }
+
+    @Test
+    void 경주_출력_확인_테스트1(){
+        OutputView outputView = new OutputView();
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("coni");
+        car1.increaseForwardNum();
+        car1.increaseForwardNum();
+        List<Car> carList = new ArrayList<>(List.of(car1, car2, car3));
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    outputView.printRacingResult(carList);
+                    assertThat(output()).contains("pobi : --", "woni : ", "coni :");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 경주_출력_확인_테스트2(){
+        OutputView outputView = new OutputView();
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        car2.increaseForwardNum();
+        car2.increaseForwardNum();
+        List<Car> carList = new ArrayList<>(List.of(car1, car2));
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    outputView.printRacingResult(carList);
+                    assertThat(output()).contains("pobi : ", "woni : --");
+                },
+                MOVING_FORWARD, STOP
+        );
     }
 
     @Override
