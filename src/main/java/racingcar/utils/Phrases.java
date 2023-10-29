92f8ec0db027c8d17a33ee24bffcb4d0274240ae
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-10-29 18:20:50 +0900

    feat(utils): Phrases 클래스 생성
    - 필요 문구 정의
      - 시작
      - 시도 횟수
      - 실행 결과
      - 최종 우승자

---
+++ src/main/java/racingcar/utils/Phrases.java
@@ -0,0 +1,11 @@
+package racingcar.utils;
+
+public final class Phrases {
+    public static final String start = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static final String times = "시도할 회수는 몇회인가요?";
+
+    public static final String racing = "실행 결과";
+
+    public static final String winner = "최종 우승자 : ";
+}
