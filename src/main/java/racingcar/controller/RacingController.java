c0a8c9da11511912658eac82eff4eb5995c41e2a
kcw0360 <kcw0360@naver.com>
2023-10-29 22:01:01 +0900

    Refactor: RacingController 메소드 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -10,52 +10,84 @@ public class RacingController {
 
     private final InputView inputView;
     private final List<Car> cars = new ArrayList<>();
+    private int attemptCount = 0;
+    private int maxPosition = 0;
+    private String winner = "";
 
     public RacingController(InputView inputView) {
         this.inputView = inputView;
     }
 
     public void play() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        inputView.setInputNames();
-        System.out.println("시도할 회수는 몇회인가요?");
-        inputView.setNumberOfAttempts();
+        startInput();
 
-        for (String name : inputView.getInputNames()) {
-            Car car = new Car(name);
-            cars.add(car);
-        }
+        carsSetting();
 
         System.out.println();
         System.out.println("실행 결과");
 
-        int count = 0;
-        int maxPosition = 0;
-        while (count < inputView.getNumberOfAttempts()) {
-            for (Car car : cars) {
-                int number = RandomNumberGenerator.createRandomNumber();
-                if (number >= 4) {
-                    car.move();
-                }
-                maxPosition = Math.max(maxPosition, car.getPosition());
-                String result = car.getName() + " : " + "-".repeat(car.getPosition());
-                System.out.println(result);
-            }
-            System.out.println();
-            count++;
-        }
+        driveRepeatExecution();
 
-        String winner = "";
-        for (Car car : cars) {
-            if (car.getPosition() == maxPosition) {
-                if (winner.equals("")) {
-                    winner = car.getName();
-                } else {
-                    winner += ", " + car.getName();
-                }
-            }
-        }
+        findWinners();
 
         System.out.println("최종 우승자 : " + winner);
+    }
+
+    private void findWinners() {
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                makeListOfWinners(car);
+            }
+        }
+    }
+
+    private void makeListOfWinners(Car car) {
+        if (winner.isEmpty()) {
+            winner = car.getName();
+        } else {
+            winner += ", " + car.getName();
+        }
+    }
+
+    private void driveRepeatExecution() {
+        while (attemptCount < inputView.getNumberOfAttempts()) {
+            for (Car car : cars) {
+                isMove(car);
+                isMaxPostion(car);
+                executionResult(car);
+            }
+            System.out.println();
+            attemptCount++;
+        }
+    }
+
+    private static void executionResult(Car car) {
+        String result = car.getName() + " : " + "-".repeat(car.getPosition());
+        System.out.println(result);
+    }
+
+    private void isMaxPostion(Car car) {
+        maxPosition = Math.max(maxPosition, car.getPosition());
+    }
+
+    private static void isMove(Car car) {
+        int number = RandomNumberGenerator.createRandomNumber();
+        if (number >= 4) {
+            car.move();
+        }
+    }
+
+    private void carsSetting() {
+        for (String name : inputView.getInputNames()) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+    }
+
+    private void startInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputView.setInputNames();
+        System.out.println("시도할 회수는 몇회인가요?");
+        inputView.setNumberOfAttempts();
     }
 }
