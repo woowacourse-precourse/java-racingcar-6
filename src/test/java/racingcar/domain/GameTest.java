50775c7e5cb81586833b6f1babfe43becfe6fbce
unknown <y2kf9a0q@gmail.com>
2023-10-29 19:32:26 +0900

    test: GameTest.java - inputCardNames()

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,14 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
 
 class GameTest {
+    Game game;
 
     @BeforeEach
     void setUp() {
+        game = new Game();
     }
 
     @Test
@@ -25,10 +31,22 @@ class GameTest {
 
     @Test
     void inputCarNames() {
+        String input = "pobi,woni,jun";
+        String[] output = {"pobi", "woni", "jun"};
+
+        try (MockedStatic<Console> mockedStatic = mockStatic(Console.class)) {
+            when(Console.readLine()).thenReturn(input);
+            assertThat(game.inputCarNames()).isEqualTo(output);
+        }
     }
 
     @Test
-    void inputCarNames_ThrownByIllegalArgumentException() {
+    void inputCarNames_Exception_EmptyName() {
+    }
+
+    @Test
+    void inputCardNames_Exception_MoreThanFiveCharacters() {
+
     }
 
     @Test
