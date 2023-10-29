7d7b75abc8e601cc9e84245f18d9df20a9ab1155
junest66 <chlwndks333@gmail.com>
2023-10-29 19:08:06 +0900

    feat(InputView): 사용자에게 입력받는 자동차이름들을 ',' 구분자로 나누는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String NAME_DELIMITER = ",";
+
     public static String inputCarNames() {
         return Console.readLine();
+    }
+
+    public static String[] splitCarNames(String carNames) {
+        return carNames.split(NAME_DELIMITER);
     }
 }
