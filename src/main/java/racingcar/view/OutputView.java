39e8306e95d09006cddcf9efb3314e2d73db080a
ykmxxi <ykmxxi97@gmail.com>
2023-10-29 16:16:49 +0900

    refactor(OutputView): 반복 사용되는 콜론 구분자 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     private static final String NAMES_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRIAL_INPUT_GUIDE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_GUIDE = "실행 결과";
+    private static final String COLON_DELIMITER = " : ";
     private static final String HYPHEN = "-";
     private static final String WINNERS_GUIDE = "최종 우승자";
     private static final String COMMA_DELIMITER = ", ";
@@ -32,7 +33,7 @@ public class OutputView {
         for (RoundResult roundResult : roundResults) {
             String carName = roundResult.name();
             String currentPosition = replaceTo(roundResult.position());
-            System.out.println(String.join(" : ", carName, currentPosition));
+            System.out.println(String.join(COLON_DELIMITER, carName, currentPosition));
         }
         System.out.println();
     }
@@ -42,7 +43,7 @@ public class OutputView {
     }
 
     public void printWinnersName(List<String> winnersName) {
-        System.out.println(String.join(" : ", WINNERS_GUIDE, divideByComma(winnersName)));
+        System.out.println(String.join(COLON_DELIMITER, WINNERS_GUIDE, divideByComma(winnersName)));
     }
 
     private String divideByComma(List<String> winnersName) {
