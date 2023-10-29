54b06b070a9b96f1d790b78cf8ba7c311049c812
noyes5 <noyes5@naver.com>
2023-10-30 00:37:43 +0900

    fix: 테스트 DisplayName 수정

--- src/test/java/racingcar/util/PositionTest.java
+++ src/test/java/racingcar/util/PositionTest.java
@@ -9,19 +9,21 @@ import racingcar.domain.Position;
 
 public class PositionTest {
     @Test
-    @DisplayName("랜덤 숫자가 범위 내에 있는지 테스트")
+    @DisplayName("position이 범위 3이하 이면 스톱 테스트")
     void 생성() {
         Position position = new Position(3);
         assertThat(position).isEqualTo(new Position(3));
     }
 
     @Test
+    @DisplayName("position이 범위 4이상 이면 전진 테스트")
     void 이동() {
         Position position = new Position(3);
         assertThat(position.move()).isEqualTo(new Position(4));
     }
 
     @Test
+    @DisplayName("position이 음수 값이 주어질때 예외 테스트")
     void 음수일때_예외() {
         assertThatThrownBy(() -> {
             new Position(-1);
