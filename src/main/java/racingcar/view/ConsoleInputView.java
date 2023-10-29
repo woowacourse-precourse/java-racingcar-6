eac69fc11d214a14be0ed5d83fda7ee7a27ded42
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 22:52:46 +0900

    feat(InputView): 사용자 입력 관련 인터페이스 구현

---
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class ConsoleInputView implements InputView{
+    @Override
+    public String readCarNames() {
+        return null;
+    }
+
+    @Override
+    public int readGameRound() {
+        return 0;
+    }
+}
