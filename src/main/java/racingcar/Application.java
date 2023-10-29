966423c4e1d8440c07e4aa1555ecdad5ddf91a2b
Junobee25 <wnsdhqo@naver.com>
2023-10-29 16:54:15 +0900

    feat: Racing Game 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.service.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.RacingGameStart();
     }
 }
