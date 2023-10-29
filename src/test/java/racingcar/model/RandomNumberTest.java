dc2c40f7e87fa060e77ef06aca0e82ba81810603
jeongjihun <wlgns2234@naver.com>
2023-10-30 00:36:25 +0900

    test: 예외 발생과 발행하지않는 경우로 테스트 분리

--- src/test/java/racingcar/model/RandomNumberTest.java
+++ src/test/java/racingcar/model/RandomNumberTest.java
@@ -1,18 +1,21 @@
 package racingcar.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class RandomNumberTest {
 
-    @DisplayName("RandomNumber 값이 특정 값 이상이지 테스트 : 값이 4 이상이라면 true, 미만이라면 false 반환.")
+    @DisplayName("isGreaterThan() 테스트 : 값이크면 true 작으면 false를 반환한다.")
     @Test
-    void isGreaterThan_4() {
+    void isGreaterThan_특정값보다_큰지_확인() {
         RandomNumber randomNumber1 = new RandomNumber(4);
         RandomNumber randomNumber2 = new RandomNumber(3);
 
@@ -25,17 +28,26 @@ class RandomNumberTest {
         );
     }
 
-    @DisplayName("RandomNumber 범위 0~9 테스트 : 0~9 사이의 숫자가 아니라면 IllegalArgumentException가 발생한다.")
-    @Test
-    void random_number_range_0_to_9() {
-        assertThatThrownBy(() -> new RandomNumber(-1))
+    @DisplayName("RandomNumber 범위 0~9 예외 테스트 : 0~9 사이의 숫자가 아니라면 IllegalArgumentException가 발생한다.")
+    @ParameterizedTest(name = "{0}는 0~9범위가 아니므로 예외가 발생한다.")
+    @ValueSource(ints = {-1, 10, 999})
+    void 예외발생_범위가_0_to_9_가_아니라면(int number) {
+        assertThatThrownBy(() -> new RandomNumber(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("랜덤 생성 숫자는 0~9사이의 숫자여야 합니다.");
     }
 
-    @DisplayName("create 생성 테스트 : RandomNumber 생성 시 특정 숫자를 부여할 수 있다.")
+    @DisplayName("RandomNumber 범위 0~9 예외 테스트 : 0~9 사이의 숫자라면 성공적으로 RandomNumber 객체가 생성된다.")
+    @ParameterizedTest(name = "{0}는 0~9범위가 이므로 성공적으로 RandomNumber 객체가 생성된다.")
+    @ValueSource(ints = {0, 5, 9})
+    void 예외발생하지않음_0_to_9_라면(int number) {
+        assertThatCode(() -> new RandomNumber(number))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("RandomNumber(number) 생성 테스트 : RandomNumber 생성 시 특정 숫자를 부여할 수 있다.")
     @Test
-    void create() {
+    void create_생성자에_숫자가_주어ㅅ() {
         RandomNumber randomNumber = new RandomNumber(5);
 
         assertThat(randomNumber).usingRecursiveComparison()
