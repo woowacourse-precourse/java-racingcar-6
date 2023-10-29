0fbd2136af322efe28745bb350d7f7c60e330436
Arachneee <jhg2819@naver.com>
2023-10-29 14:52:25 +0900

    fix: 숫자가 아닌 문자 입력받는 오류 수정
    정규표현식 오류를 수정했습니다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,7 +12,7 @@ import racingcar.exception.RacingGameException;
 public class InputView {
 
     private static final String CARS_SPLIT_STRING = ",";
-    private static final Pattern NUMBER = Pattern.compile("[0-9]+");
+    private static final Pattern NUMBER = Pattern.compile("^[0-9]+$");
     private static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)\n";
 
     private static final String ASK_ROTATE_NUMBER = "시도할 회수는 몇회인가요?";
