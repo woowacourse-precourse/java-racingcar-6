88788401768f9dc7cf7189675e8fcb8cdb6934ba
HyunmoBae <tsi0520@naver.com>
2023-10-29 18:57:02 +0900

    refactor(racingcar) : modify NumberGenerator

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
 
-    public int createRandomNumber(){
-        int randomNum = Randoms.pickNumberInRange(1,9);
+    public int createRandomNumber() {
+        int randomNum = Randoms.pickNumberInRange(1, 9);
         return randomNum;
     }
 }
