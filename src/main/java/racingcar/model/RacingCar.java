f9d4a9964f68a99a02e6e26f192795e4a895548c
eunhee <dmsgml9903@gmail.com>
2023-10-30 02:17:04 +0900

    feat(RacingCar): 랜덤 값을 생성한 뒤 그 값을 기반으로 자동차를 이동시키는 기능 구현

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RacingCar {
@@ -10,5 +11,13 @@ public class RacingCar {
     public RacingCar(List<Car> cars, Integer numberOfRetry) {
         this.cars = cars;
         this.numberOfRetry = numberOfRetry;
+    }
+
+    private void moveRandomAllCar() {
+        cars.forEach(car -> car.moveForward(createRandomNumber()));
+    }
+
+    private int createRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
