222e8a28a82976770b6334ccc57e5a853aa0f64a
kim <hwiyul_g@daum.net>
2023-10-29 20:09:25 +0900

    feat(InputValidator) : null 검사와 중복 이름 검사 추가

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -1,24 +1,44 @@
 package racingcar.utils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.junit.platform.commons.util.StringUtils;
 
 public class InputValidator {
 
     private static final int CAR_NAME_LENGTH = 5;
-    private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 0자 이상 5자 이하만 가능합니다.";
+    private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+    private static final String INVALID_CAR_NAME_MESSAGE = "자동차 이름은 null 또는 비어 있을 수 없습니다.";
+    private static final String DUPLICATE_CAR_NAME_MESSAGE = "자동차 이름은 중복될 수 없습니다.";
     private static final String NON_INTEGER_MESSAGE = "시도 횟수는 정수여야 합니다.";
     private static final String NON_POSITIVE_MESSAGE = "시도 횟수는 0보다 커야 합니다.";
 
 
     public static void validateCarNames(List<String> carNames) {
         for (String carName : carNames) {
+            validateCarNameNotNullOrBlank(carName);
             validateCarNameLength(carName);
         }
+        validateDuplicateCarName(carNames);
     }
 
     private static void validateCarNameLength(String carName) {
-        if (carName.length() > CAR_NAME_LENGTH || carName.trim().isEmpty()) {
+        if (carName.length() > CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateCarNameNotNullOrBlank(String carName){
+        if(StringUtils.isBlank(carName)){
+            throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
+        }
+    }
+
+    private static void validateDuplicateCarName(List<String> carNames) {
+        Set<String> set = new HashSet<>(carNames);
+        if(set.size() != carNames.size()){
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_MESSAGE);
         }
     }
 
