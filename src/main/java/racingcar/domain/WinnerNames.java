07688dcc1adef1e1a0321a5ce6cd0d7df2da02f4
eora21 <eora21@naver.com>
2023-10-29 23:41:22 +0900

    feat: 🎸 (WinnerNames) 추가 우승자들의 이름을 지니는 DTO 클래스 작성

---
+++ src/main/java/racingcar/domain/WinnerNames.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class WinnerNames {
+    private final List<String> winnerNames;
+
+    public WinnerNames(List<String> winnerNames) {
+        this.winnerNames = winnerNames;
+    }
+}
