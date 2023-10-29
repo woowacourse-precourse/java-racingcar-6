370e9ca82c2004c522072c1ecc1f0c61d8a2d37c
김강년 <64246800+ekek54@users.noreply.github.com>
2023-10-29 20:40:54 +0900

    test(ControllerTest, InputViewTest): 입력 테스트 동시 수행시 No Line Found 예외 발생 해결
    문제 상황:
      - 여러개의 입력 테스트 동시 수행시 No Line Found 예외 발생
    문제 원인:
      - 미션용 라이브러리의
    Console은 System.in으로 Scanner를 생성해 싱글톤으로 관리한다.
      - 입력 테스트를 위해
    InputStream을 mocking해주고 있는다.
      - 하지만, 각 테스트에서 mocking을 수행해도 Scanner는
    싱글톤으로 관리된다.
      - 따라서, mocking한 InputStream에 대한 Scanner가 각 테스트별로 생성되지
    않는다.
    해결 방안:
     - 테스트 수행 후 Console.close()를 수행해 Scanner를 초기화 해주는 작업을 수행한다.

--- src/test/java/racingcar/ControllerTest.java
+++ src/test/java/racingcar/ControllerTest.java
@@ -2,13 +2,21 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputView;
 
 class ControllerTest {
     private Controller controller = new Controller(new InputView());
+
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
 
     @Test
     void 사용자_입력으로_경주_생성() {
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,17 +1,24 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
     private InputView inputView = new InputView();
 
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
+
     @Test
     void 쉽표를_기준으로_구분하여_입력받는다() {
         // given
-        String inputString = "pobi,woni,jun";
+        String inputString = "pobi,woni,jun\n";
         // when
         System.setIn(new ByteArrayInputStream(inputString.getBytes()));
         List<String> carNames = inputView.inputCarNameStrings();
