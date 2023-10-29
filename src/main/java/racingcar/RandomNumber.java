bf1c114954910a398dac4664ee521277562401d7
aammddkkzxc <ideal0427@naver.com>
2023-10-29 21:04:45 +0900

    feat: 무작위 수 생성 기능 구현

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -1,8 +1,13 @@
 package racingcar;
 
-public class RandomNumber {
-    public void generate() {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class RandomNumber {
+    public static final int RANDOM_NUMBER_MIN_VALUE = 0;
+    public static final int RANDOM_NUMBER_MAX_VALUE = 9;
+
+    public int generate() {
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_VALUE, RANDOM_NUMBER_MAX_VALUE);
     }
 
     public void judge() {
