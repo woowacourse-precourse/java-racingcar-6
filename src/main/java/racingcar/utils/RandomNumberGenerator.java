78e78dc6875694858db53d890f1f006c203b87a4
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 22:39:33 +0900

    refactor(NumberGenerator): 숫자 생성기 인터페이스 구현

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator{
     private static final int MINIMUM_NUMBER_RANGE = 0;
     private static final int MAXIMUM_NUMBER_RANGE = 9;
     private static RandomNumberGenerator instance;
