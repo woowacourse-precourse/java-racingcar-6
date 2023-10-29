1db347110b93160112c80b1b446f06a704fae833
heesane <tees3359@gmail.com>
2023-10-29 17:19:59 +0900

    feat : Cars Model 선언 및 toString Override

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public Integer getCarsLength(){
+        return cars.size();
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : cars) {
+            sb.append(car.getName()).append(" : ");
+            sb.append("-".repeat(Math.max(0, car.getPosition())));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
