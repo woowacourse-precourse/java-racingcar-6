c46235f8cf59ded79f9f56e73db16fd8f758941c
yhcho0405 <yhcho0405@kakao.com>
2023-10-29 23:14:39 +0900

    feat: 사용자로 부터 자동차 이름과 라운드 수 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+
 public class Application {
     public static void main(String[] args) {
         RaceController raceController = new RaceController();
--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,10 @@
 package racingcar.controller;
 
 public class RaceController {
+
+    public void start() {
+        String carNames = InputView.getCarNames();
+        int rounds = InputView.getRounds();
+
+    }
 }
