85e31c1256988ae5788500b2f80fca0a2de927e4
suhyun__ <thebbbb1206@gmail.com>
2023-10-30 04:05:09 +0900

    feat: 경주에 참여하는 자동차의 Car 객체 리스트 생성

--- src/main/java/racingcar/controller/CarGame.java
+++ src/main/java/racingcar/controller/CarGame.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.validator.CarValidator;
 import racingcar.validator.InputValidator;
 import racingcar.view.InputView;
@@ -15,8 +16,9 @@ public class CarGame {
         inputView = new InputView();
     }
 
-    private void start() {
+    public void start() {
         String input = getCarNamesInput();
+        List<Car> carList = getCarList(splitCarNames(input));
     }
 
     private String getCarNamesInput() {
@@ -35,6 +37,7 @@ public class CarGame {
             CarValidator.checkCarNameLength(carNameList);
             CarValidator.checkDuplicateCarName(carNameList);
         } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
             getCarNamesInput();
         }
         return input;
@@ -44,5 +47,13 @@ public class CarGame {
         String[] nameArray = input.split(",");
         List<String> nameList = new ArrayList<>(Arrays.stream(nameArray).toList());
         return nameList;
+    }
+
+    private List<Car> getCarList(List<String> carNameList) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carNameList) {
+            carList.add(new Car(carName, 0));
+        }
+        return carList;
     }
 }
