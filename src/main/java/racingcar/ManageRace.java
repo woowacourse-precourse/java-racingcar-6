4cb45bcc49d6aeb7ea4bd9026a9fa6315cf851e6
Happeeee <cmg118@naver.com>
2023-10-30 04:21:36 +0900

    Refactor: 디버깅 코드 삭제

--- src/main/java/racingcar/ManageRace.java
+++ src/main/java/racingcar/ManageRace.java
@@ -10,13 +10,17 @@ public class ManageRace {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public void test() {
-        String carName = inputCarName();
-        System.out.println(carName);
-        List<String> carNames = stringToList(carName);
-        checkValidNames(carNames);
-
+    public void setRace() {
+        String carNames = inputCarName();
+        List<String> carNameList = stringToList(carNames);
+        checkValidNames(carNameList);
         int tryCount = inputTryCount();
+
+        startRace(new Race(carNameList, tryCount));
+    }
+
+    private void startRace(Race race) {
+        race.start();
     }
 
     private String inputCarName() {
