a81a601d3c3f4b949ebf5fe51d654bf6290152c8
Gukin Han <gukin.dev@gmail.com>
2023-10-29 19:17:08 +0900

    refactor: 필드 이름을 좀더 descriptive하게 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,14 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private String name;
     private StringBuilder mileage;
-    private static final int MOVABLE_CRITERIA = 4;
-    private static final String ONE_MOVE = "-";
+    private static final int MOVABLE_LOWER_BOUND = 4;
+    private static final String INDICATOR_PER_MOVE = "-";
 
 
     private Car(String name) {
         this.name = name;
         this.mileage = new StringBuilder();
-
     }
 
     public static Car createCarByName(String name) {
@@ -21,11 +20,13 @@ public class Car {
 
     public void moveForwardOrStop() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
-        if (isOkayToMove(randomNumber)) mileage.append(ONE_MOVE);
+        if (isOkayToMove(randomNumber)) {
+            mileage.append(INDICATOR_PER_MOVE);
+        }
     }
 
     private static boolean isOkayToMove(int randomNumber) {
-        return randomNumber >= MOVABLE_CRITERIA;
+        return randomNumber >= MOVABLE_LOWER_BOUND;
     }
 
 
