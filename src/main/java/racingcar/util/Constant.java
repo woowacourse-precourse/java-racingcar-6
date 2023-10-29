c424e38e32dd36ddead2963efb9f1661ae1646a9
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-30 00:13:01 +0900

    FEAT / add SPLIT_UNIT

--- src/main/java/racingcar/util/Constant.java
+++ src/main/java/racingcar/util/Constant.java
@@ -3,4 +3,5 @@ package racingcar.util;
 public class Constant {
     public static final int MAX_CAR_NAME = 5;
     public static final int MIN_CAR_NAME = 1;
+    public static final String SPLIT_UNIT = ",";
 }
