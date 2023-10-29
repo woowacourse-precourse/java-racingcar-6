dd3d6118a734882a5759ae3adb1dcf522b3a9383
Haeun Jeong <gkdms4248@gmail.com>
2023-10-29 23:29:05 +0900

    feat(model): Race 한 경기 진행 기능 추가

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -2,7 +2,9 @@ package racingcar.model;
 
 public class Race {
     private int numberOfRace;
+    private final Generator generator = new Generator();
     private final static String TOO_MUCH_NUMBER_OF_RACE = "너무 많은 회수를 입력하였습니다.";
+    private final static int REFERENCE_NUMBER = 4;
 
     public Race(int numberOfRace) {
         validateNumberOfRace(numberOfRace);
@@ -13,5 +15,16 @@ public class Race {
         if (numberOfRace > 100) {
             throw new IllegalArgumentException(TOO_MUCH_NUMBER_OF_RACE);
         }
+    }
+
+    public void runSingleRace(Car car) {
+        if (generator.generateRandomNumber() >= REFERENCE_NUMBER) {
+            car.moveForward();
+        }
+        this.numberOfRace--;
+    }
+
+    public int getNumberOfRace() {
+        return this.numberOfRace;
     }
 }
