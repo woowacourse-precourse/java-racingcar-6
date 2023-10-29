b5618b627d1d83dc685c654fc9a168d98abfae2c
IM-GYURI <kgulr0517@gmail.com>
2023-10-30 02:59:06 +0900

    Feat: 추가 Car 클래스

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        position++;
+    }
+}
