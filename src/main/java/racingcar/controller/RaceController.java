9692e0d1bd48336e9af299e3e4d6df8faff70ab2
hansg931 <hansg0509@cau.ac.kr>
2023-10-29 13:51:02 +0900

    feat: 경주의 진행을 관리하는 클래스 작성

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,32 @@
 package racingcar.controller;
 
+import racingcar.model.RaceStatus;
+import racingcar.model.Cars;
+import racingcar.model.Round;
+
 public class RaceController {
+	private final Cars cars;
+	private final Round round;
+
+	public RaceController(Cars cars, Round round) {
+		this.cars = cars;
+		this.round = round;
+	}
+
+	public RaceStatus getRaceStatus() {
+		cars.moveForward();
+		round.proceedRound();
+		return new RaceStatus(cars.getCars());
+	}
+
+	public RaceStatus getFinalRaceStatus() {
+		while (!round.isEndRound()) {
+			getRaceStatus();
+		}
+		return new RaceStatus(cars.getFirstPlaceCars());
+	}
+
+	public boolean isEndRace() {
+		return round.isEndRound();
+	}
 }
