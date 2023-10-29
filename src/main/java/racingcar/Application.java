8d2dc6b7dcca7eecd70992ce882a3fcf672570ba
Montep95 <sewd1224@gmail.com>
2023-10-29 22:56:36 +0900

    [feat] Start the game
    Connect to class(Stadium) of Package(Controller)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.Stadium;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Stadium race = new Stadium();
+        race.start();
     }
 }
