f909a25726b9bf32a690ca0cdb470f225d8d00f3
lee jihyeon <lejinhy@gmail.com>
2023-10-29 21:41:56 +0900

    feat: 경주 결과 출력, 최종 우승자까지 완성

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,6 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.Entity.Car;
+import racingcar.constant.CarStatus;
+import racingcar.util.Calculation;
+import racingcar.util.ConsolePrint;
+import racingcar.util.Exception;
+import racingcar.util.Parsing;
 
 import java.util.List;
 
@@ -8,6 +14,7 @@ public class RacingController {
     private Exception exception = new Exception();
     private ConsolePrint consolePrint = new ConsolePrint();
     private Parsing parsing = new Parsing();
+    private Calculation calculation = new Calculation();
     public void startGame() {
         consolePrint.requestCarNameInput();
         String carNameInput = Console.readLine();
@@ -25,6 +32,21 @@ public class RacingController {
         exception.isGreaterThanZero(tryCount);
 
         consolePrint.informRacingResult();
+        int i = 0;
+        while(i++ <= tryCount) {
+            for (Car car : cars) {
+                int randomNumber = calculation.generateRandomNumberInRange();
+                CarStatus carStatus = calculation.judgeCarMoveOrStop(randomNumber);
+                car.setMoveCount(carStatus);
+                car.setNowMoveTrace(carStatus);
 
+                consolePrint.racingTryResult(car,carStatus);
+            }
+            System.out.println();
+        }
+
+        List<Car> winners = calculation.judgeWhoIsWinner(cars);
+
+        consolePrint.finalWinner(winners);
     }
 }
