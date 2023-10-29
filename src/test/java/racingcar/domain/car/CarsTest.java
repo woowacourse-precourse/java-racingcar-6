ec72b1fbba313ab24c9f377af7c09c63e71424f2
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 18:00:40 +0900

    test(CarsTest): 자동차 리스트 및 인덱스별 자동차 반환 기능 테스트 구현

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -1,2 +1,37 @@
-package racingcar.domain.car;public class CarsTest {
+package racingcar.domain.car;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+    @DisplayName("자동차 리스트를 반환하면 불변 리스트로 반환해야 한다.")
+    @Test
+    void 자동차_불변_리스트_반환_테스트() {
+        //given
+        Cars cars = new Cars("jun,seo,park");
+
+        //when
+        List<Car> unmodifiableList = cars.getCars();
+
+        //then
+        assertThrows(UnsupportedOperationException.class, () -> unmodifiableList.add(new Car("test")));
+        assertThat(unmodifiableList.size()).isEqualTo(3);
+    }
+
+    @DisplayName("인덱스를 전달하면 그에 맞는 자동차를 반환해야 한다.")
+    @Test
+    void 자동차_인덱스로_반환_테스트() {
+        //given
+        Cars cars = new Cars("jun,seo,park");
+
+        //when
+        Car car = cars.findCarByIndex(2);
+
+        //then
+        assertThat(car.getName()).isEqualTo("park");
+    }
 }
