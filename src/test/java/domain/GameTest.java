1924ae341f395dce4dfbd7201330013d689368d5
Annjieun <dkslwdms2121@naver.com>
2023-10-29 16:40:04 +0900

    test(GameTest): 무작위 값 생성 테스트 추가

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -9,15 +10,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Attempt;
 import racingcar.domain.CarsFactory;
+import racingcar.domain.Movement;
 
 public class GameTest {
     private CarsFactory carsFactory;
     private Attempt attempt;
+    private Movement movement;
 
     @BeforeEach
     public void setUp() {
         carsFactory = new CarsFactory();
         attempt = new Attempt();
+        movement = new Movement();
     }
 
     @Test
@@ -35,6 +39,16 @@ public class GameTest {
         command("5");
 
         assertThatCode(() -> attempt.saveAttemptNumber()).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 무작위_값_생성하기() {
+        int RANDOM_NUMBER_MINIMUM = 0;
+        int RANDOM_NUMBER_MAXIMUM = 9;
+
+        int randomValue = movement.createRandomNumber();
+
+        assertTrue(randomValue >= RANDOM_NUMBER_MINIMUM && randomValue <= RANDOM_NUMBER_MAXIMUM);
     }
 
     private void command(final String... args) {
