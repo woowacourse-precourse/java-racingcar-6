ccec5999d65705091b2efbf8acccfa10aa478149
coenffl <>
2023-10-29 18:44:14 +0900

    Feat: create car object

--- src/main/java/racingcar/Domain/Car.java
+++ src/main/java/racingcar/Domain/Car.java
@@ -1,5 +1,30 @@
 package racingcar.Domain;
 
-public class Car {
+import static racingcar.Domain.RaceNumberGenerator.*;
 
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public boolean move() {
+        int randomNumber = generateRandomNumber();
+        if (randomNumber >= 4) {
+            position++;
+            return true;
+        }
+        else return false;
+    }
 }
