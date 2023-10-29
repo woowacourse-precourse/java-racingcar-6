9657a51001ad941673c1c0bc87c1b88a56cda7d9
haero77 <shineecard5@gmail.com>
2023-10-29 20:29:06 +0900

    test: 입력받은 이동 횟수가 숫자가 아니거나, 0 이하의 정수라면 예외가 발생한다.

--- src/test/java/racingcar/io/input/InputManagerTest.java
+++ src/test/java/racingcar/io/input/InputManagerTest.java
@@ -1,5 +1,6 @@
 package racingcar.io.input;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +20,47 @@ class InputManagerTest {
 
         // when & then
         assertThatThrownBy(() -> inputManager.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("양의 정수로 이루어진 이동 횟수를 입력받을 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "300", "999999"})
+    void readMovingCount(String input) {
+        // given
+        Reader reader = new FakeReader(input);
+        InputManager inputManager = new InputManager(reader);
+
+        // when
+        long movingCount = inputManager.readMovingCount();
+
+        // then
+        assertThat(movingCount).isEqualTo(Long.parseLong(input));
+    }
+
+    @DisplayName("입력받은 이동 횟수가 숫자가 아니라면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "1a", ",", " ,"})
+    void readMovingCountFail(String movingCountStringLiteral) {
+        // given
+        Reader reader = new FakeReader(movingCountStringLiteral);
+        InputManager inputManager = new InputManager(reader);
+
+        // when & then
+        assertThatThrownBy(() -> inputManager.readMovingCount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력받은 이동 횟수가 0 이하의 정수라면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1"})
+    void readMovingCountFail2(String input) {
+        // given
+        Reader reader = new FakeReader(input);
+        InputManager inputManager = new InputManager(reader);
+
+        // when & then
+        assertThatThrownBy(() -> inputManager.readMovingCount())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
