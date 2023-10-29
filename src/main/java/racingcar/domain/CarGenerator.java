3b2435fe2fa5d5a269231ab5b565d17499f30d7e
Junobee25 <wnsdhqo@naver.com>
2023-10-29 15:23:44 +0900

    fix: 입력 받은 문자열은 Car 클래스에서 get 으로 수정

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -10,26 +10,25 @@ public class CarGenerator {
 
     public LinkedHashMap<String, String> createCarList() {
         LinkedHashMap<String, String> carList = new LinkedHashMap<>();
-        String[] cars = splitStringByCommas(inputCarList());
+        String[] cars = splitStringByCommas();
         for (String car : cars) {
             carList.put(car, "");
         }
         return carList;
     }
 
-    public String[] splitStringByCommas(String inputCarList) {
-        String[] carList = inputCarList.split(",");
+    public String[] splitStringByCommas() {
+        String[] carList = car.getStringCarList().split(",");
         return carList;
     }
 
-    public String inputCarList() {
-        String carList = Console.readLine();
-        return carList;
+    public void inputCarList() {
+        car.setStringCarList(Console.readLine());
     }
 
-    public void checkContainsConsecutiveCommas(String inputCarList) {
+    public void checkContainsConsecutiveCommas() {
         Pattern pattern = Pattern.compile(",,+");
-        Matcher matcher = pattern.matcher(inputCarList);
+        Matcher matcher = pattern.matcher(car.getStringCarList());
 
         if (matcher.find()) {
             throw new IllegalArgumentException("연속적인 쉼표가 발견되었습니다.");
