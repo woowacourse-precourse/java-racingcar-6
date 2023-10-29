b8aef22e5a2f0f02b5a9bb49ae1bf61d6ee1f305
Kite_Z <duswl339@ajou.ac.kr>
2023-10-29 21:54:57 +0900

    refactor: modify RacingCarGameConfig
    - enum -> class

--- src/main/java/racingcar/global/RacingCarGameConfig.java
+++ src/main/java/racingcar/global/RacingCarGameConfig.java
@@ -1,14 +1,9 @@
 package racingcar.global;
 
-public enum RacingCarGameConfig {
-    CAR_NAME_MAX_LENGTH(5),
-    RANDOM_MAX_NUMBER(9),
-    RANDOM_MIN_NUMBER(1),
-    STANDARD_NUMBER(4);
-
-    private final int value;
-
-    RacingCarGameConfig(int value) {
-        this.value = value;
-    }
+public class RacingCarGameConfig {
+    private RacingCarGameConfig() {}
+    public static int CAR_NAME_MAX_LENGTH = 5;
+    public static int RANDOM_MAX_NUMBER = 9;
+    public static int RANDOM_MIN_NUMBER = 1;
+    public static int STANDARD_NUMBER = 4;
 }
