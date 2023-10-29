dec92f5d25bce6433513663b61639bda3b88d839
JongukYang <didwhddnr123@naver.com>
2023-10-29 14:24:15 +0900

    feat: 사용자 입력값 예외처리 로직 구현

---
+++ src/main/java/racingcar/validator/InputRacingCarNameValidator.java
@@ -0,0 +1,22 @@
+package racingcar.validator;
+
+import racingcar.utils.ErrorMessage;
+
+public class InputRacingCarNameValidator {
+    public void validate(String input) {
+        validateRacingCarNameIsBlank(input);
+        validateRacingCarNameLength(input);
+    }
+
+    public void validateRacingCarNameIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.RACING_CAR_NAME_DOES_NOT_BLANK.getMessage());
+        }
+    }
+
+    public void validateRacingCarNameLength(String input) {
+        if (input.length() > 5) {
+            throw new IllegalArgumentException(ErrorMessage.RACING_CAR_NAME_TOO_LONG.getMessage());
+        }
+    }
+}
