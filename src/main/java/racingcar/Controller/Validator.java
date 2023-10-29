2c20f5074f9359726a78d6c9f4b66ec1c6f503ea
kiwkim <atthenoon@gmail.com>
2023-10-30 06:43:43 +0900

    feat($Validator): make `Validator` for CarName.
    - validNameMaxLength : validate that each Car's name is limited to 5 characters.
    - validNameIsUnique : check for duplicate names.
    - validNameIsContainSpace: check if there are any spaces in the name.
    - validNameStringIsEmpty: Ensure that the name is not empty.
    - validListIsEmpty: Validate that the Car list is not empty.

---
+++ src/main/java/racingcar/Controller/Validator.java
@@ -0,0 +1,58 @@
+package racingcar.Controller;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+public class Validator {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private final static String ERR_NAME_LENGTH = "자동차의 이름은 5자 이하여야 합니다.";
+    private final static String ERR_NAME_ALREADY_USED = "이미 등록된 자동차 이름입니다.";
+    private final static String ERR_NAME_SPACE_CHAR = "자동차 이름에 공백문자를 사용할수는 없습니다.";
+    private final static String ERR_LIST_IS_EMPTY = "최소 1개의 자동차 이름을 입력하세요.";
+    private final static String ERR_NAME_IS_EMPTY = "자동차의 이름을 입력하세요.";
+    private Set<String> carNameSet;
+
+    public Validator(){
+        this.carNameSet = new HashSet<>();
+    }
+    public void validCarName(String carName){
+        validNameMaxLength(carName);
+        validNameIsContainSpace(carName);
+        validNameIsUnique(carName);
+        validNameStringIsEmpty(carName);
+    }
+    private void validNameMaxLength(String carName){
+        if(carName.length() > MAX_CAR_NAME_LENGTH){
+            throw new IllegalArgumentException(ERR_NAME_LENGTH);
+        }
+    }
+
+    private void validNameIsUnique(String carName){
+        if(carNameSet.contains(carName)){
+            throw new IllegalArgumentException(ERR_NAME_ALREADY_USED);
+        }
+        carNameSet.add(carName);
+    }
+
+    private void validNameIsContainSpace(String carName){
+        for(char spell : carName.toCharArray()){
+            if (Character.isWhitespace(spell)){
+                throw new IllegalArgumentException(ERR_NAME_SPACE_CHAR);
+            }
+        }
+    }
+
+    private  void validNameStringIsEmpty(String carName){
+        if(carName.isEmpty()){
+            throw new IllegalArgumentException((ERR_NAME_IS_EMPTY));
+        }
+    }
+
+    public void validListIsEmpty(List<String> carNameList){
+        if(carNameList.isEmpty()){
+            throw new IllegalArgumentException(ERR_LIST_IS_EMPTY);
+       }
+    }
+}
+
