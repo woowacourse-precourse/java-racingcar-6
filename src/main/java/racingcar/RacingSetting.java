d76eabfc5b695bdcace9d37bc5d100c313c6b84a
minkyole <7719s@nave.com>
2023-10-29 19:22:24 +0900

    feat(RacingSetting.java : 안내메시지 출력 구현)
    - getName(), getMoveNum()입력 안내메시지 출력 구현
    - 기능 구현목록 체크 완료

--- src/main/java/racingcar/RacingSetting.java
+++ src/main/java/racingcar/RacingSetting.java
@@ -10,6 +10,7 @@ public class RacingSetting {
         String inputCarName;
         String[] carNameParsing;
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         inputCarName = Console.readLine();
         carNameParsing = inputCarName.split(",");
         carNum = new CarNum(carNameParsing);
@@ -19,6 +20,7 @@ public class RacingSetting {
     void getMoveNum() {
         String inputMoveNum;
 
+        System.out.println("시도할 회수는 몇회인가요?");
         inputMoveNum = Console.readLine();
         moveNum = new MoveNum(inputMoveNum);
     }
