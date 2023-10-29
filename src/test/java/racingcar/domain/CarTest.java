37186bb8809667a575dad60202024784ffa0500a
HyunmoBae <tsi0520@naver.com>
2023-10-29 21:07:59 +0900

    test(racingcar) : add tryMoveCar test

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +9,30 @@ class CarTest {
     @Test
     void tryMoveCarTest1() {
         Car car = new Car();
-        for (int i = 0; i < 5; i++ ){
-        car.tryMoveCar();
-        System.out.println(car.carPosition);
+        for (int i = 0; i < 5; i++) {
+            car.tryMoveCar();
         }
+        int result = car.carPosition;
+        assertThat(result).isBetween(1, 5);
+    }
+
+    @Test
+    void tryMoveCarTest2() {
+        Car car = new Car();
+        for (int i = 0; i < 10; i++) {
+            car.tryMoveCar();
+        }
+        int result = car.carPosition;
+        assertThat(result).isBetween(1, 10);
+    }
+
+    @Test
+    void tryMoveCarTest3() {
+        Car car = new Car();
+        for (int i = 0; i < 20; i++) {
+            car.tryMoveCar();
+        }
+        int result = car.carPosition;
+        assertThat(result).isBetween(1, 20);
     }
 }
