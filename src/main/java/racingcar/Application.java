ec17aca89e2ccd6e89ecdb135760b000fcfe2da3
songhee1 <ghenrhkwk88@naver.com>
2023-10-29 19:12:48 +0900

    [feature] 구현할 기능목록 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 public class Application {
+    private static final String INIT_COMMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL_COMMENT = "시도할 회수는 몇회인가요?;";
+    private static final String
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
