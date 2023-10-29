ee0ab564b9a09b3ab90970b325fdbbd00d3814f8
BonSik <rnqhstlr2297@gmail.com>
2023-10-29 22:30:19 +0900

    feat: 자동차 전진 조건 판별 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,12 @@
 package racingcar.domain;
 
+import racingcar.util.NumberGenerator;
+
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int FORWARD_CONDITION_NUMBER = 4;
+    private static final int RANDOM_RANGE_START_NUMBER = 0;
+    private static final int RANDOM_RANGE_END_NUMBER = 9;
     private static final String EXCEED_NAME_LENGTH_MESSAGE = "자동차 이름은 5자 이하만 가능 합니다.";
 
     private final String name;
@@ -11,6 +16,15 @@ public class Car {
         checkNameLength(name);
         this.name = name;
         this.movementCount = 0;
+    }
+
+    public Boolean isPossibleForward() {
+        int randomNumber = NumberGenerator.generateRandomNumberInRange(RANDOM_RANGE_START_NUMBER,
+                RANDOM_RANGE_END_NUMBER);
+        if (randomNumber >= FORWARD_CONDITION_NUMBER) {
+            return true;
+        }
+        return false;
     }
 
     private void checkNameLength(String name) {
