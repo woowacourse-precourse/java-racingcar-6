c99802c7a6b0fe916ef67d85dae915f0a8d4f5d5
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-10-29 17:38:09 +0900

    경주 차량 객체

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,5 +3,33 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    
+    private String name;
+    private int forwardCount;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getForward() {
+        return this.forwardCount;
+    }
+
+    private void randomNumber() {
+        int num = Randoms.pickNumberInRange(0,9);
+        if (num >= 4) {
+            forwardCount++;
+        }
+    }
+
+    private void currentLocation() {
+        System.out.print(name + " : ");
+        for(int i = 0; i < forwardCount; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
