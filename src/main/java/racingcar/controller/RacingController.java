f60d522903ed23b7320ae3489ca4ad792ad05583
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 22:37:57 +0900

    refactor(RacingController): 레이싱을 시작하는 기능 추가

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,52 @@
+package racingcar.controller;
+
+import java.util.List;
+
+import racingcar.converter.RacingConverter;
+import racingcar.domain.Car;
+import racingcar.dto.RacingResult;
+import racingcar.factory.CarFactory;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+    private final RacingConverter converter;
+    private final CarFactory carFactory;
+
+    public RacingController(InputView inputView, OutputView outputView, RacingService racingService, RacingConverter converter,
+            CarFactory carFactory) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+        this.converter = converter;
+        this.carFactory = carFactory;
+    }
+
+    public void run() {
+        List<Car> cars = createCars();
+        int tryCount = createTryCount();
+
+        outputView.printResultMessage();
+        while (tryCount > 0) {
+            List<RacingResult> racingResults = racingService.race(cars);
+            outputView.printRacingResult(racingResults);
+            tryCount--;
+        }
+        List<String> winners = racingService.selectWinners(cars);
+        outputView.printWinners(winners);
+    }
+
+    private List<Car> createCars() {
+        String carNames = inputView.inputCarNames();
+        return carFactory.createCars(converter.convertToListByDelimiter(carNames));
+    }
+
+    private int createTryCount() {
+        String tryCount = inputView.inputTryCount();
+        return converter.convertToPositiveInt(tryCount);
+    }
+}
