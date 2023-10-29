6ce13dd98a1076823e5e4d65b0bca8d1ac1aaea2
박준영 <wnsdud6969@naver.com>
2023-10-30 01:41:34 +0900

    feat(Round): add Round class to manage round of racing car game
    Add new features to Round
    - add field round
    - add constructor
    - validate negative round
    - validate overflow

---
+++ src/main/java/racingcar/model/Round.java
@@ -0,0 +1,30 @@
+package racingcar.model;
+
+public class Round {
+
+    private static final int MAXIMUM_INTEGER_VALUE = 0x7fffffff;
+    private static final int ZERO = 0;
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+    private static final String ERROR_NEGATIVE_ROUND = "라운드는 1 미만일 수 없습니다.";
+    private static final String ERROR_INTEGER_OVERFLOW = "오버플로우가 발생했습니다.";
+
+    private int round;
+
+    public Round(int round) {
+        validateNegativeRound(round);
+        validateIntegerOverflow(round);
+        this.round = round;
+    }
+
+    private void validateNegativeRound(int round) {
+        if (round <= ZERO) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_NEGATIVE_ROUND);
+        }
+    }
+
+    private void validateIntegerOverflow(int round) {
+        if (round > MAXIMUM_INTEGER_VALUE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_INTEGER_OVERFLOW);
+        }
+    }
+}
