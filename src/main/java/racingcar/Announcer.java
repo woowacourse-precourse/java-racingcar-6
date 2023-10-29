ac1d11ee571764917eae04c36755cefab29988aa
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 23:12:46 +0900

    feat(announcer): 시도 횟수를 입력하도록 멘트 출력하는 메서드 추가

--- src/main/java/racingcar/Announcer.java
+++ src/main/java/racingcar/Announcer.java
@@ -2,9 +2,14 @@ package racingcar;
 
 public class Announcer {
     private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public void announceToInputCarName() {
         print(INPUT_CAR_NAME);
+    }
+
+    public void announceToInputTryCount() {
+        print(INPUT_TRY_COUNT);
     }
 
     public void print(String message) {
