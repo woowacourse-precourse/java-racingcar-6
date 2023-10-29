9b1f3feef442bf7a8f5ad96658e7dc2bd10db166
freekyr7529 <freekyr7529@gmail.com>
2023-10-29 13:47:08 +0900

    refactor: throw문 메서드로 분리
    클래스 내부에서 반복적으로 사용되는 throw문을 별도의 메서드로 분리하였다

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -5,25 +5,29 @@ import java.util.Objects;
 public class InputValidator {
     public void nameNonInputValidate(int carsSize, String firstElementName) {
         if (carsSize == 1 && Objects.equals(firstElementName, "")) {
-            throw new IllegalArgumentException("어떠한 이름도 입력되지 않았습니다!");
+            printException("어떠한 이름도 입력되지 않았습니다! 값을 입력해주세요!");
         }
     }
 
     public void nameLengthValidate(Car car) {
         if (car.isNameLongerThenCriteria(5)) {
-            throw new IllegalArgumentException("5자리 이하의 이름을 입력하시오!");
+            printException("5자리 이하의 이름을 입력하시오!");
         }
     }
 
     public void roundNonInputValidate(String input) {
         if (Objects.equals(input, "")) {
-            throw new IllegalArgumentException("어떠한 것도 입력되지 않았습니다! 값을 입력해주세요!");
+            printException("어떠한 것도 입력되지 않았습니다! 값을 입력해주세요!");
         }
     }
 
     public void roundRegExValidate(String input) {
         if (!input.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException("입력에 숫자가 아닌 다른 문자가 포함되어 있습니다!");
+            printException("입력에 숫자가 아닌 다른 문자가 포함되어 있습니다!");
         }
+    }
+
+    private static void printException(String msg) {
+        throw new IllegalArgumentException(msg);
     }
 }
