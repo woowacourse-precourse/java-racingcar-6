5c420a47f52ebdec3d3c3f5250487021545e5db3
jihyuk <kwlgur0416@gmail.com>
2023-10-29 15:50:58 +0900

    feat : 시도 횟수 입력의 예외 처리 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,11 @@
 package racingcar.view;
 
+import static racingcar.exception.InputErrorCode.BLANK_INPUT;
+import static racingcar.exception.InputErrorCode.NEGATIVE_INTEGER_INPUT;
+import static racingcar.exception.InputErrorCode.NOT_INTEGER_INPUT;
+
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.InputException;
 
 public class InputView {
 
@@ -15,11 +20,38 @@ public class InputView {
 
     public static int inputAttemptCount() {
         System.out.println(ATTEMPT_COUNT_MESSAGE);
-        return Integer.parseInt(getInput());
+
+        final int attemptCount = convertToInteger(getInput());
+        validateNotNegative(attemptCount);
+
+        return attemptCount;
     }
 
     private static String getInput() {
-        return Console.readLine();
+        final String input = Console.readLine();
+        validateNotBlank(input);
+
+        return input;
+    }
+
+    private static void validateNotBlank(final String input) {
+        if (input == null || input.isBlank()) {
+            throw new InputException(BLANK_INPUT);
+        }
+    }
+
+    private static void validateNotNegative(final int value) {
+        if (value < 0) {
+            throw new InputException(NEGATIVE_INTEGER_INPUT);
+        }
+    }
+
+    private static int convertToInteger(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            throw new InputException(NOT_INTEGER_INPUT);
+        }
     }
 
 }
