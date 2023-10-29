1aedd40a469a0d7f14021ca9814d88a3b94e762d
남수연 <rnrmf0721@gmail.com>
2023-10-29 23:35:53 +0900

    feat(RacingCar): 부모클래스의 생성자를 통해 name의 값을 초기화

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 import static racingcar.constant.Constant.DISTANCE_DRIVER;
 
 public class RacingCar extends Car {
+
+
+    public RacingCar(String name) {
+        super(name);
+    }
+
     @Override
     public void moveCar() {
         int number = Randoms.pickNumberInRange(0,9);
