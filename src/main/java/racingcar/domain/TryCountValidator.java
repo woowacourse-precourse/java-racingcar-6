64d43df22a98c5b0e3237467e6e68aea881dc1ea
Sully <2dcoder@naver.com>
2023-10-29 21:34:19 +0900

    feat(TryCountValidator): 시도할 횟수를 입력받을 때 생기는 예외를 처리하기 위한 클래스 생성
    필드로 tryCount를 갖고 있는다.

---
+++ src/main/java/racingcar/domain/TryCountValidator.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class TryCountValidator {
+
+    private final String tryCount;
+
+    public TryCountValidator(String tryCount) {
+        this.tryCount = tryCount;
+    }
+
+}
