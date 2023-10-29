b3b17935ec5b9c8edcb307662dc3dfa745d37dd7
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-30 03:10:01 +0900

    refactor: Car 멤버 변경

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain.car;
+
+import java.util.List;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
