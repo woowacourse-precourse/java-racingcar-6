1b35dac54baabfd507124970e57142e5838addf4
heogeonho <geonh1016@gmail.com>
2023-10-30 02:47:48 +0900

    feat: Car 뼈대 코드 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    public String name;
+    public int position;
+
+    public void moveForward() {
+    }
+}
