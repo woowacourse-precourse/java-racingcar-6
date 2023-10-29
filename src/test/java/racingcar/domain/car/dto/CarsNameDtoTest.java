fa27dbfc81c23ae72b43c8654082b96fc4ff36ae
penrose15 <seongju99@naver.com>
2023-10-29 16:05:04 +0900

    Test: CarsNameDto 유효성 검증 테스트 추가

--- src/test/java/racingcar/domain/car/dto/CarsNameDtoTest.java
+++ src/test/java/racingcar/domain/car/dto/CarsNameDtoTest.java
@@ -8,12 +8,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsNameDtoTest {
 
-    @DisplayName("이름 null 입력 시 NPE 확인")
+    @DisplayName("이름 null 입력 시 IllegalArgumentException 확인")
     @Test
-    void carsNameDtoThrowNPE() {
+    void carsNameDtoThrowExceptionWhenNamesIsNull() {
 
         assertThatThrownBy(() -> new CarsNameDto(null))
-                .isInstanceOf(NullPointerException.class);
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("이름 하나가 null 입력 시 IllegalArgumentException 확인")
+    @Test
+    void carsNameDtoThrowExceptionOneOfNameIsNull() {
+        String names = "다오,";
+
+        assertThatThrownBy(() -> new CarsNameDto(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("쉼표 연속으로 나오는 경우 IllegalArgumentException 확인")
+    @Test
+    void carsNameDtoThrowExceptionCommasContinuousInput() {
+        String names = "다오,,버찌";
+
+        assertThatThrownBy(() -> new CarsNameDto(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
