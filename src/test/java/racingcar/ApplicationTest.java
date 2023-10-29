ad0c99ff06810134fa5955109b15ad775361d19c
KangGeonyoung <kky6335@gmail.com>
2023-10-30 00:42:37 +0900

    test(ApplicationTest): 자동차 이름 입력 값에 대한 테스트 - 자동차_이름_숫자포함일경우 - 자동차_이름_숫자일경우 - 자동차_이름_5자_초과일경우 - 자동차_이름_공백일경우 - 자동차_이름_빈줄일경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.controller.RaceGameController;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -12,14 +17,17 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    private Car car = new Car("kgy");
+    private RaceGameController raceGameController = new RaceGameController();
+
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -30,6 +38,48 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+
+    @Test
+    void 자동차_이름_숫자포함일경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("kang1,geon,young", "3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_숫자일경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("kang,geon,13", "4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_5자_초과일경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("kang,geonyoung", "2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_공백일경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ", "4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_빈줄일경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
