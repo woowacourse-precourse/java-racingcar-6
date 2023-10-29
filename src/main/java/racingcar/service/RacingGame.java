314320acdc7b81163815eccc558ab1f20cd2b75d
Junobee25 <wnsdhqo@naver.com>
2023-10-29 15:29:44 +0900

    feat: 게임 시작 시 예외 케이스 적용

--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -19,7 +19,13 @@ public class RacingGame {
 
     public void RacingGameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carGenerator.inputCarList();
+        car.checkContainsConsecutiveCommas();
         car.setCarList(carGenerator.createCarList());
+        car.checkCarNameLength();
+        car.checkCarNameDuplicate();
+        car.checkCarNameFirstLetterIsSpace();
+        car.checkCarNameLastLetterIsSpace();
         LinkedHashMap<String, String> carList = car.getCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
