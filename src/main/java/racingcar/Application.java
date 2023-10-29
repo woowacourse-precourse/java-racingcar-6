7a1ff5027a2909225b1e85663e6e9f1564626837
prao <pch5521@gmail.com>
2023-10-29 16:49:58 +0900

    feat: 구현 및 테스트 완료(성공)
    - 구현 및 테스트 완료
    - 추후 리팩토링 사항
      - 메서드 별로 하나의 기능을 담당할 것
      - 확장에 열려있도록 상수처리할 것
      - 복잡한 로직은 stream을 사용하여 가독성과 성능을 향상시킬 것

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarController racingCarController = new RacingCarController(new InputView(), new OutputView());
+        racingCarController.startRace();
     }
 }
