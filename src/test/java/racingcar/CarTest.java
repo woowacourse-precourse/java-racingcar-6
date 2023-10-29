ebf9a4452b1f086daf136413d44c660e0c8c44ab
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 04:43:19 +0900

    test(CarTest) : addDistance()를 테스트하는 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -10,7 +10,7 @@ public class CarTest {
 
     @Test
     @DisplayName("생성자를 통해서 변수 초기화가 되는가?")
-    void testCarConstructor(){
+    void testCarConstructor() {
         Car car1 = new Car("페라리");
         Car car2 = new Car("우테코");
 
@@ -23,5 +23,18 @@ public class CarTest {
         assertThat(result1_TotalDistance).isEqualTo(0);
         assertThat(result2_Name).isEqualTo("우테코");
         assertThat(result2_TotalDistance).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("addDistance()로 totalDistance ++시키는 기능 테스트")
+    void testAddDistance() {
+        Car car = new Car("우테코");
+        int expectedDistance = 5;
+
+        for (int i = 0; i < expectedDistance; i++) {
+            car.addDistance();
+        }
+
+        assertThat(car.getTotalDistance()).isEqualTo(expectedDistance);
     }
 }
