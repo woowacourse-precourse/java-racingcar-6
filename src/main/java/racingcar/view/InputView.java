d217088a56bb2d7d2a6557617d9262b4f04c89a5
Yang-soeun <soeun8636@naver.com>
2023-10-29 15:24:03 +0900

    [feat]: inputView (,)를 기준으로 이름 나누는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,14 +2,28 @@ package racingcar.view;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     private final static String inputCarNameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String inputTryNumberMessage = "시도할 횟수는 몇회인가요?";
 
-    public static String inputCarName(){
+    public static List<String> inputCarName(){
         System.out.println(inputCarNameMessage);
-        return Console.readLine();
+        String names = Console.readLine();
+        return splitNames(names);
+    }
+
+    private static List<String> splitNames(String names) {
+        String[] namesArray = names.split(",");
+        List<String> nameList = new ArrayList<>();
+
+        for (int i = 0; i < namesArray.length; i++) {
+            nameList.add(namesArray[i]);
+        }
+
+        return nameList;
     }
 
     public static int inputTryNumber(){
