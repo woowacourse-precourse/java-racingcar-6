928618d17a3a677c7fc6311c961759a591eac7f7
doha <dohaahi@naver.com>
2023-10-29 13:50:10 +0900

    fix: 요구 사항에 맞게 우승자 출력 문구 수정

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String GAME_RESULT_MESSAGE = "실행 결과";
-    public static final String GAME_WINNER_MESSAGE = "최종 우승자 :";
+    public static final String GAME_WINNER_MESSAGE = "최종 우승자 : ";
     public static final int START_INCLUSIVE = 0;
     public static final int END_INCLUSIVE = 9;
 
