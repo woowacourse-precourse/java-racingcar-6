ede99c46dca96cf002046ff74a39fc5ecf120afc
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 21:02:51 +0900

    remove(RandomNumberGeneratorTest): 외부 라이브러리에서 제공해주는 메서드이기 때문에 제거

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class RandomNumberGenerator {
-    public static Integer createRandomNumber() {
-        return Randoms.pickNumberInRange(1, 9);
-    }
-}
