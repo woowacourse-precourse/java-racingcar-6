fb3fa929514198472b9b2cc9e0283862f6d5f972
maark1106 <mark1106@naver.com>
2023-10-29 20:13:18 +0900

    feat(RacingCarGame) : 0 ~ 9 까지 무작위 숫자 생성하는 기능 구현

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.Map;
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -13,6 +16,25 @@ public class RacingCarGame {
 
     public void run(){
         carInitialize();
+        int tryNumber = inputView.InputTryNumber();
+        playRacingGame(tryNumber);
+    }
+
+    public void playRacingGame(int tryNumber) {
+        for(int tryCount = 0; tryCount < tryNumber;tryCount++){
+            pickEachCarRandomNumber();
+        }
+    }
+
+    public void pickEachCarRandomNumber() {
+        Map<String, Car> carsMap = cars.getCars();
+        for (Car car : carsMap.values()) {
+            int randomNumber = pickRandomNumber();
+        }
+    }
+
+    public int pickRandomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     public void carInitialize() {
--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
