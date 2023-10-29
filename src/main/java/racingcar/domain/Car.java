2903bd739ee2df015a41ad3ddf6226e80e862a86
박승민 <tmddudals369@naver.com>
2023-10-30 04:42:52 +0900

    feat(car): 게임 수행 주체가 되는 도메인 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import racingcar.utils.RandomNumberGenerator;
+
+public class Car {
+    private final String name;
+    private int movingCount;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
