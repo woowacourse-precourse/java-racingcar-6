99df2170a0d69bf99ed6e734b65c97728a55b06c
(han songyi) <(hhan1218@naver.com)>
2023-10-29 17:04:56 +0900

    feat: create ViewMessage Class and fix OutputView

---
+++ src/main/java/message/ViewMessage.java
@@ -0,0 +1,6 @@
+public class ViewMessage {
+    public static final PRINT_NAME_OF_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final PRINT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final PRINT_GAME_RESULT = "실행 결과";
+    public static final PRINT_WINNER = "최종 우승자 : ";
+}
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,21 +1,16 @@
 package view;
+import message.ViewMessage;
 
 public class OutputView {
-    private static final PRINT_NAME_OF_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final PRINT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
-    private static final PRINT_GAME_RESULT = "실행 결과";
-
     public void printNameOfCar() {
-        System.out.println(PRINT_NAME_OF_CAR);
+        System.out.println(ViewMessage.PRINT_NAME_OF_CAR);
     }
 
     public void printTryNumber() {
-        System.out.println(PRINT_TRY_NUMBER);
+        System.out.println(ViewMessage.PRINT_TRY_NUMBER);
     }
 
     public void printGameResult() {
-        System.out.println(PRINT_GAME_RESULT);
+        System.out.println(ViewMessage.PRINT_GAME_RESULT);
     }
-
-    
 }
