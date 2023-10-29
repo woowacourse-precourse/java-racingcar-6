021350f425b5d234d5c2b41b2750f948ed4cdc88
minkyole <7719s@nave.com>
2023-10-30 00:55:52 +0900

    feat(CarNum.java): CarNum 유효성 검사 기능 구현
    - CarNum 유효성 검사 기능 구현
    1. 자동차 개수가 최대 개수를 넘는지 검사
    2. 자동차 개수가 최소 1개 이상인지 검사
    - 기능 구현목록 체크 완료

--- src/main/java/racingcar/CarNum.java
+++ src/main/java/racingcar/CarNum.java
@@ -1,13 +1,24 @@
 package racingcar;
 
 public class CarNum {
+    final static int MAX_CAR_NUM = 10;
     int num;
 
     CarNum(String[] carNameParsing) {
+        isVaildCarNum(carNameParsing);
         this.num = carNameParsing.length;
     }
 
     boolean isInRange(int currentNum) {
         return (num > currentNum);
+    }
+
+    void isVaildCarNum(String[] carNameParsing) throws IllegalArgumentException {
+        if (carNameParsing.length > MAX_CAR_NUM) {
+            throw new IllegalArgumentException("Error : 자동차 개수가 너무 많습니다 최대개수 " + MAX_CAR_NUM);
+        }
+        if (carNameParsing.length == 1 && carNameParsing[0].isBlank()) {
+            throw new IllegalArgumentException("Error : 자동차 개수가 너무 적습니다 최소 하나라도 입력해 주세요");
+        }
     }
 }
