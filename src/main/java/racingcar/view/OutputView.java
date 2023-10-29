159369603d9c5a461a427e892491d6931beb94be
ykmxxi <ykmxxi97@gmail.com>
2023-10-29 15:31:06 +0900

    feat(OutputView): 각 차수별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,16 @@
 package racingcar.view;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import racingcar.dto.CarsDto;
+
 public class OutputView {
 
     private static final String NAMES_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRIAL_INPUT_GUIDE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_GUIDE = "실행 결과";
+    private static final String HYPHEN = "-";
 
     public void printNamesGuide() {
         System.out.println(NAMES_INPUT_GUIDE);
@@ -11,6 +18,23 @@ public class OutputView {
 
     public void printTrialGuide() {
         System.out.println(TRIAL_INPUT_GUIDE);
+    }
+
+    public void printResultGuide() {
+        System.out.println(RESULT_GUIDE);
+    }
+
+    public void printRoundResult(final CarsDto carsDto) {
+        Map<String, Integer> roundResult = carsDto.carsStatus();
+        for (Entry<String, Integer> entry : roundResult.entrySet()) {
+            String name = entry.getKey();
+            String position = replaceTo(entry.getValue());
+            System.out.println(String.join(" : ", name, position));
+        }
+    }
+
+    private String replaceTo(final int position) {
+        return HYPHEN.repeat(position);
     }
 
 }
