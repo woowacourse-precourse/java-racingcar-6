975c59815bdad9c061be188ede6d734804ba9e2c
M1NS1K <rlaalstlr2001@gmail.com>
2023-10-30 07:17:29 +0900

    progressView 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.view.progressView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        progressView.view().print();
     }
 }
--- src/main/java/racingcar/view/progressView.java
+++ src/main/java/racingcar/view/progressView.java
@@ -1,23 +1,61 @@
 package racingcar.view;
 
+import racingcar.controller.Input;
 import racingcar.controller.Output;
-import racingcar.model.CarObject;
 
 public class progressView {
-    private final Output car = new Output();
+    private final Output carOut = new Output();
+    private final Input carIn = new Input();
+
+    private progressView() {}
 
     //car string : distance 출력 함수
-    //epoch 만큼 반복해서 출력해주는 함수
-    //input string 함수
-    //input epoch 함수
+    public void printDistance(int distanceSize) {
+        for(int i = 0; i < distanceSize; i++) {
+            System.out.print("-");
+        }
+    }
 
     public void printProgress() {
-
-        for(int i = 0; i < car.getSize(); i++) {
-
-
-            car.getService().insertCarDistance();
-            car.getInstance().updateDistance();
+        for(int i = 0; i < carOut.getSize(); i++) {
+            System.out.println(
+                    carOut.getInstance().getComponentIndexOf(i).getName() + " : " );
+            printDistance(carOut.getInstance().getComponentIndexOf(i).getDistance());
         }
+    }
+
+    //epoch 만큼 반복해서 출력해주는 함수
+    public void printAllProgress() {
+        for(int i = 0; i < carOut.getEpoch(); i++) {
+            printProgress();
+            carOut.getService().insertCarDistance();
+            carOut.getInstance().updateDistance();
+        }
+    }
+
+    //input string 함수
+    public void inputCarString() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carIn.inputName();
+    }
+
+    //input epoch 함수
+    public void inputCarEpoch() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        carIn.inputEpoch();
+    }
+
+    public void print() {
+        inputCarString();
+        inputCarEpoch();
+        printAllProgress();
+    }
+
+    private static class SingleInstanceHolder {
+        private static final progressView INSTANCE = new progressView();
+    }
+
+    public static progressView view() {
+        return SingleInstanceHolder.INSTANCE;
     }
 }
