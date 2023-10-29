b0ef57b99b475c0546c4237da42952e25c506f90
KimSehoon <shggm2000@gmail.com>
2023-10-29 23:24:05 +0900

    feat: checkMoveForward method 구현
    - 자동차 이름을 인자로 받아 무작위 값이 4 이상이면 carMap에 전진 횟수를 1 증가 시키는 checkMoveForward method 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,8 +1,17 @@
 package racingcar;
 
 public class Game {
+    GameManager gameManager = new GameManager();
+    GameElements gameElements = new GameElements();
 
-    public void startGame(){
-        
+    public void startGame() {
+        gameManager.gameSetting();
+    }
+    
+    private void checkMoveForward(String car) {
+        if (gameManager.createRandomNumber() >= 4) {
+            int forwardCount = gameElements.getCarMap().get(car) + 1;
+            gameElements.getCarMap().replace(car, forwardCount);
+        }
     }
 }
