b64684c9f0f64181fc8491d4b7e6146294ca7eff
becooq81 <becooq81@gmail.com>
2023-10-30 05:57:44 +0900

    refactor(Parser, ParserTest): separated the role of parser class and validator class - the parser class using validator class's methods before parsing integrates the two classes, which is an unecessary process since the methods can be called separately in a class that handles logic - to avoid the dependency on each other, I removed the validation process from parser class

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -5,15 +5,11 @@ import java.util.List;
 
 public class Parser {
 
-    Validator validator = new Validator();
-
     public int parseNumTrial(String numTrialInput) {
-        validator.validateNumTrialInput(numTrialInput);
         return Integer.parseInt(numTrialInput);
     }
 
     public List<String> parseCarNames(String carNamesInput) {
-        validator.validateCarNamesInput(carNamesInput);
         return Arrays.asList(carNamesInput.split(","));
     }
 }
--- src/test/java/racingcar/util/ParserTest.java
+++ src/test/java/racingcar/util/ParserTest.java
@@ -12,28 +12,17 @@ public class ParserTest extends NsTest{
     Parser parser = new Parser();
 
     @Test
-    void 횟수_검증_성공_후_횟수_숫자_반환() {
+    void 횟수_숫자_반환() {
         int numTrial = parser.parseNumTrial("5");
         assertThat(numTrial).isEqualTo(5);
     }
 
     @Test
-    void 자동차_이름_검증_성공_후_자동차_이름_리스트_반환() {
+    void 자동차_이름_리스트_반환() {
         List<String> carNames = parser.parseCarNames("a,b,c");
         assertThat(carNames).containsExactly("a", "b", "c");
     }
 
-    @Test
-    void 자동차_이름_오류_발생시_예외_발생() {
-        assertThatThrownBy(() -> parser.parseCarNames("a,,b"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 횟수_오류_발생시_예외_발생() {
-        assertThatThrownBy(() -> parser.parseNumTrial("0"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @Override
     protected void runMain() {
