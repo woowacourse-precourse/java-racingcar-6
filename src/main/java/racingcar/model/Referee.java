de7e19cfcaddd6ad3606b1b1fa8ef9facc618eb6
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-29 15:25:35 +0900

    style : 의미없는 공백 라인 제거

--- src/main/java/racingcar/model/Referee.java
+++ src/main/java/racingcar/model/Referee.java
@@ -11,7 +11,7 @@ public class Referee {
     private static final Integer START_POINT = 0;
 
     private List<CarName> winnerNames;
-    
+
     public List<CarName> getWinnerNames() {
         return winnerNames;
     }
@@ -31,7 +31,6 @@ public class Referee {
                 .map(car -> car.getCarName())
                 .collect(Collectors.toList());
     }
-
 
     private CarDistance getMaxDistance(List<Car> cars) {
         Integer maxDistance = cars.stream()
