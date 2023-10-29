0dd678b04a244d80d7a1211159ef8e9716b1f011
junseoks-99 <angec5645@naver.com>
2023-10-29 22:06:28 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/domain/view/InputView.java
+++ src/main/java/racingcar/domain/view/InputView.java
@@ -1,6 +1,14 @@
 package racingcar.domain.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String inputCarNames() {
+        printStartMessage();
+        String carNames = input();
+        return carNames;
+    }
 
     public static void printStartMessage() {
         System.out.println(InputMessages.START.getMessage());
@@ -8,5 +16,10 @@ public class InputView {
 
     public static void printAttemptMessage() {
         System.out.println(InputMessages.ATTEMPT.getMessage());
+    }
+
+    public static String input() {
+        String inputString = Console.readLine();
+        return inputString;
     }
 }
