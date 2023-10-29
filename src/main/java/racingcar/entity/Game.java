84e4c253fb1ded73ca53eef01d0bc193f58f74e0
chs98412 <chs98412@gmail.com>
2023-10-29 18:27:03 +0900

    feat: GameRepository save 기능 작성

--- src/main/java/racingcar/entity/Game.java
+++ src/main/java/racingcar/entity/Game.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    public Game(Integer playCount) {
+        this.playCount = playCount;
+    }
 
     private List<Car> carList = new ArrayList<>();
 
--- src/main/java/racingcar/repository/GameRepository.java
+++ src/main/java/racingcar/repository/GameRepository.java
@@ -1,13 +1,20 @@
 package racingcar.repository;
 
-
 import racingcar.entity.Game;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class GameRepository {
-    Map<String, Game> gameMap = new HashMap<String, Game>();
+    public GameRepository() {
+        this.id = 0;
+    }
 
-
+    Map<Integer, Game> gameMap = new HashMap<Integer, Game>();
+    Integer id;
+    public Game save(Integer playCount) {
+        Game saveGame = new Game(playCount);
+        gameMap.put(id++, saveGame);
+        return saveGame;
+    }
 }
