6418c59e35ffb2b8aec559f8745a0299b12020ee
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-29 15:31:21 +0900

    refactor(ExecutionNumber): 타당성 검토 이후 정수를 반환하도록 수정
    타당성 검토를 하면서 문자열을 정수로 변환하였으므로 이를 반환함으로써 불필요한 숫자 변환을 없앰

--- src/main/java/racingcar/validation/ExecutionNumber.java
+++ src/main/java/racingcar/validation/ExecutionNumber.java
@@ -4,7 +4,7 @@ public class ExecutionNumber {
     static final String NOT_A_NUMBER_EXCEPTION = "숫자가 아닙니다.";
     static final String NUMBER_RANGE_EXCEPTION = "숫자 범위가 올바르지 않습니다.";
 
-    public void validateExcutionNumber(String in) throws IllegalArgumentException {
+    public int validateExcutionNumber(String in) throws IllegalArgumentException {
         int number;
         try {
             number = Integer.parseInt(in);
@@ -12,6 +12,7 @@ public class ExecutionNumber {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_A_NUMBER_EXCEPTION);
         }
+        return number;
     }
 
     private void checkRange(int number) throws IllegalArgumentException {
