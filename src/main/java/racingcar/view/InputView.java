07d4578506090c1351e51403d7fd19659c6b2559
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-29 23:48:04 +0900

    FEAT / add getCarNames function

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Validator;
+
 public class InputView {
     public InputView() {
     }
@@ -10,5 +13,12 @@ public class InputView {
 
     private static class LazyHolder {
         private static final InputView INSTANCE = new InputView();
+    }
+
+    public String getCarNames() {
+        String input = Console.readLine();
+        Validator.validateCarNames(input);
+
+        return input;
     }
 }
