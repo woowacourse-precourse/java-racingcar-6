bdef4d278872d1530c9e55a577b99cd8a638e60d
Isaac Han <isaachan233@gmail.com>
2023-10-29 19:37:14 +0900

    study: String api 공부하기

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -48,4 +48,73 @@ public class StringTest {
                 .hasMessageContaining("String index out of range: 5");
     }
 
+    @Test
+    void compareTo_A_B_비교하기() {
+        String a = "abc";
+        String b = "bcd";
+        assertThat(a.compareTo(b)).isEqualTo(-1);
+    }
+
+    @Test
+    void compareTo_A_B_비교하기_2() {
+        String a = "abc";
+        String b = "bcd";
+        assertThat(b.compareTo(a)).isEqualTo(1);
+    }
+    @Test
+    void contains_포함되어_있는지_체크() {
+        String a = "abc";
+        String b = "b";
+
+        assertThat(a.contains(b)).isEqualTo(true);
+    }
+
+    @Test
+    void contains_포함되어_있는지_체크_2() {
+        String a = "abc";
+        String b = "abcdef";
+
+        assertThat(a.contains(b)).isEqualTo(false);
+    }
+
+    @Test
+    void startsWith_대상으로_시작하는_체크() {
+        String a = "abcd";
+        String b = "a";
+
+        assertThat(a.startsWith(b)).isEqualTo(true);
+    }
+
+    @Test
+    void endsWith_대상으로_끝나는지_체크() {
+        String a = "abcd";
+        String b = "d";
+
+        assertThat(a.endsWith(b)).isEqualTo(true);
+    }
+
+    @Test
+    void equals_두_대상의_동등성_비교() {
+        String a = "abcd";
+        String b = "abcd";
+
+        assertThat(a.equals(b)).isEqualTo(true);
+    }
+
+    @Test
+    void equals_두_대상의_동등성_비교_2() {
+        String a = "abcd";
+        String b = "abc";
+
+        assertThat(a.equals(b)).isEqualTo(false);
+    }
+
+    @Test
+    void equals_두_대상의_동등성_비교_예외상황() {
+        String a = null;
+        String b = "abc";
+
+        assertThatThrownBy(() -> a.equals(b)).isInstanceOf(NullPointerException.class).hasMessageContaining("\"a\" is null");
+    }
+
 }
