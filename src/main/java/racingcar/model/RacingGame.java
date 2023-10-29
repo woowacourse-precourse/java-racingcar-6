83035f7e2a9624308dea95d67f49caee9bff42d5
IToriginal <gitshineit@gmail.com>
2023-10-29 18:52:24 +0900

    :sparkles: [feature] #2 각 플레이어는 랜덤 숫자를 배정 받고 조건에 만족하면 전진하는 메서드 기능 구현

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,7 +1,34 @@
 package racingcar.model;
 
+import static racingcar.util.constant.GameRule.END_INCLUSIVE;
+import static racingcar.util.constant.GameRule.MOVING_FORWARD_CONDITION;
+import static racingcar.util.constant.GameRule.START_INCLUSIVE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashMap;
+
 public class RacingGame {
 
-    // TODO: 게임 서비스에 대한 로직 구현
+    public void start(HashMap<String, Integer> players) {
+        movePlayers(players);
+    }
+
+    private void movePlayers(HashMap<String, Integer> players) {
+        for (String player : players.keySet()) {
+            moveConditionCheck(players, player);
+        }
+    }
+
+    private void moveConditionCheck(HashMap<String, Integer> players, String player) {
+        int myNumber = getRandomNumber();
+        if (myNumber >= MOVING_FORWARD_CONDITION.getValue()) {
+            int currentPosition = players.get(player);
+            players.put(player, currentPosition + 1);
+        }
+    }
+
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE.getValue(), END_INCLUSIVE.getValue());
+    }
 
 }
--- src/main/java/racingcar/util/constant/GameRule.java
+++ src/main/java/racingcar/util/constant/GameRule.java
@@ -1,7 +1,10 @@
 package racingcar.util.constant;
 
 public enum GameRule {
-    MAX_NICKNAME_LENGTH(4);
+    MAX_NICKNAME_LENGTH(4),
+    START_INCLUSIVE(0),
+    END_INCLUSIVE(9),
+    MOVING_FORWARD_CONDITION(4);
 
     private final Integer value;
 
