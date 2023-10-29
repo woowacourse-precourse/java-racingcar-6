862ce8872c4f6c3478809a7108073a656bbcc9ac
dsadara <chaehyun.forest@gmail.com>
2023-10-29 12:58:48 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int movedDist;
 
-    public Car(String name, int movedDist) {
+    public Car(String name) {
         this.name = name;
-        this.movedDist = movedDist;
+        movedDist = 0;
     }
 
     public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (4 <= randomNumber) {
+            movedDist++;
+        }
     }
 }
