197de04f7a580511ff2e5ab596875123b36d1aa8
KimSehoon <shggm2000@gmail.com>
2023-10-29 23:04:59 +0900

    feat: GameElements class 내 getter method 추가

--- src/main/java/racingcar/GameElements.java
+++ src/main/java/racingcar/GameElements.java
@@ -7,6 +7,14 @@ public class GameElements {
     private List<String> carNameList = new ArrayList<>();
     private int tryCount = 0;
 
+    public List<String> getCarNameList() {
+        return carNameList;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
     public void carNamesSetting(List<String> carNameListByUser) {
         carNameList = carNameListByUser;
     }
