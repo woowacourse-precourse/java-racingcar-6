ad31c6d78ac10fded5237c2224bbeed824e4b8f5
kihwn <kihwn98@gmail.com>
2023-10-29 14:20:18 +0900

    feat:입출력 메서드 추가. 사용자에게 차 이름, 시도횟수 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carInput = Console.readLine();
+        String[] carArray = carInput.split(",");
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNumber = Integer.parseInt(Console.readLine());
+
+
     }
 }
