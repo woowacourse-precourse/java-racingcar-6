07032eb6a2ada1dc09047db55ed9c7de1b14c8c1
hyuna <monicajo0@naver.com>
2023-10-30 00:57:17 +0900

    style: GameManager 필요없는 import 삭제

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
