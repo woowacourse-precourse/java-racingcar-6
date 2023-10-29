b717b4a347482de6624c5f50612194a408576975
doka <posmane5@gmail.com>
2023-10-29 18:22:01 +0900

    feat(RacingRuleMaker) : RacingRuleMaker 기능 클래스 완성
    "각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다." 기능 완성
    
    "사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다." 기능 완성
    
    nameingCar() - 경주 자동차들 이름 입력 메소드
    inputRacingRound() - 경주 시도할 회수 입력 메소드
    RacingRuleMaker() - 입력을 통해서 레이싱 정보를 기입하는 생성자 함수

--- src/main/java/racingcar/domain/RacingRuleMaker.java
+++ src/main/java/racingcar/domain/RacingRuleMaker.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //- RacingRuleMaker
@@ -15,12 +17,27 @@ public class RacingRuleMaker {
 
     public List<String> cars = new ArrayList<>();
     public int roundCount;
-    public RacingRuleMaker () {
+
+    public RacingRuleMaker() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        namingCar();
+        System.out.println("시도할 회수는 몇회인가요?");
+        inputRacingRound();
 
     }
-    public List<String> namingCar(String carsInput) {
 
-        return null;
+    public List<String> namingCar() {
+        String carNames = Console.readLine();
+
+        String[] carsNamesArray = carNames.split(",");
+        cars = Arrays.asList(carsNamesArray);
+        return cars;
+    }
+
+    public void inputRacingRound() {
+        String inputRacingRound = Console.readLine();
+
+        roundCount = Integer.parseInt(inputRacingRound);
     }
 
 
