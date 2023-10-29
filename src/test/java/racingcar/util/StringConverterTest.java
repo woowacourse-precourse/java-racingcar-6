e363115c38c1d16d3bb2a973ffa4aeddf9dcb637
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-29 21:51:16 +0900

    test : String을 TryNumber로 변환하는 테스트 수정

--- src/test/java/racingcar/util/StringConverterTest.java
+++ src/test/java/racingcar/util/StringConverterTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.vo.CarName;
+import racingcar.vo.TryNumber;
 
 class StringConverterTest {
 
@@ -15,7 +16,7 @@ class StringConverterTest {
 
     @DisplayName("유효한 문자열을 구분자로 나누어 리스트로 변환한다.")
     @Test
-    void stringToListByDelimiter_test() {
+    void valid_stringToCarNameListByDelimiter_test() {
         // given
         String input = "Car1, Car2, Car3";
         List<CarName> expectedList = Arrays.asList(new CarName("Car1"), new CarName("Car2"), new CarName("Car3"));
@@ -33,7 +34,7 @@ class StringConverterTest {
         String invalidInput = "";
 
         try {
-            StringConverter.stringToInteger(invalidInput);
+            StringConverter.stringToTryNumber(invalidInput);
         } catch (IllegalArgumentException e) {
             assertEquals(EMPTY_INPUT_ERROR_MESSAGE, e.getMessage());
         }
@@ -41,26 +42,26 @@ class StringConverterTest {
 
     @DisplayName("유효한 문자열을 정수로 변환한다.")
     @Test
-    void stringToInteger_test() {
+    void valid_StringToTryNumber_test() {
         // given
         String input = "123";
-        Integer result = StringConverter.stringToInteger(input);
+        TryNumber result = StringConverter.stringToTryNumber(input);
 
         // when
         Integer expectedResult = 123;
 
         // then
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult, result.number());
     }
 
 
     @DisplayName("유효하지 않은 문자열을 정수로 변환하면 예외가 발생한다.")
     @Test
-    void invalidStringToInteger_exception_test() {
+    void invalid_StringToTryNumber_exception_test() {
         String invalidInput = "a";
 
         try {
-            StringConverter.stringToInteger(invalidInput);
+            StringConverter.stringToTryNumber(invalidInput);
         } catch (IllegalArgumentException e) {
             assertEquals(CHARACTERS_IN_INPUT_ERROR_MESSAGE, e.getMessage());
         }
