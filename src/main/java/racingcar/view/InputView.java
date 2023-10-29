a4e70b5f27f12eb793fd31d97be9f3394916e837
kcw0360 <kcw0360@naver.com>
2023-10-29 17:30:28 +0900

    Feat: 시도할 횟수 입력 기능 구현과 1이상의 숫자가 입력 되었는지 검증하는 메소드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,13 +6,35 @@ import java.util.regex.Pattern;
 public class InputView {
 
     private String[] inputNames;
+    private int numberOfAttempts;
+
     private static final Pattern nameValidatePattern = Pattern.compile("([\\S]{1,})");
+    private static final Pattern numberValidatePattern = Pattern.compile("([\\d]{1,})");
 
     public void setInputNames() {
         String input = userInput();
         String[] names = input.split(",");
         validateNames(names);
         this.inputNames = names;
+    }
+
+    public void setNumberOfAttempts() {
+        String input = userInput();
+        validateNumber(input);
+        validateNumberZero(input);
+        this.numberOfAttempts = Integer.parseInt(input);
+    }
+
+    private static void validateNumberZero(String input) {
+        if (input.equals("0")) {
+            throw new IllegalArgumentException("1이상의 숫자만 입력해주세요.");
+        }
+    }
+
+    private static void validateNumber(String input) {
+        if (!validatePattern(input, numberValidatePattern)) {
+            throw new IllegalArgumentException("1이상의 숫자만 입력해주세요.");
+        }
     }
 
     private static void validateNames(String[] names) {
@@ -29,27 +51,37 @@ public class InputView {
     }
 
     private static void validateSpace(String input) {
-        if (!isNotSpace(input)) {
+        if (!validatePattern(input, nameValidatePattern)) {
             throw new IllegalArgumentException("공백을 제거해주세요.");
         }
     }
 
-    private static boolean isNotSpace(String input) {
-        return nameValidatePattern.matcher(input).matches();
+    private static boolean validatePattern(String input, Pattern pattern) {
+        return pattern.matcher(input).matches();
     }
 
     public String[] getInputNames() {
         return inputNames;
     }
 
+    public int getNumberOfAttempts() {
+        return numberOfAttempts;
+    }
+
     private static String userInput() {
         return Console.readLine();
     }
 
-    //test method
+    // 아래는 test를 위한 method
     public void setInputNames(String input) {;
         String[] names = input.split(",");
         validateNames(names);
         this.inputNames = names;
+    }
+
+    public void setNumberOfAttempts(String numberOfAttempts) {
+        validateNumber(numberOfAttempts);
+        validateNumberZero(numberOfAttempts);
+        this.numberOfAttempts = Integer.parseInt(numberOfAttempts);
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -17,16 +17,16 @@ class InputViewTest {
 
     @Test
     @DisplayName("입력한 값을 split으로 배열로 만들기")
-    void 입력_테스트() {
+    void 이름_입력_테스트() {
         //직접 입력하는 것으로 Console.readline() 대체
-        inputView.setInputNames("pobi,woni,jun");
+        inputView.setInputNames("pobi,woni,jun1,!@");
 
-        assertThat(inputView.getInputNames()).isEqualTo(new String[]{"pobi", "woni", "jun"});
+        assertThat(inputView.getInputNames()).isEqualTo(new String[]{"pobi", "woni", "jun1", "!@"});
     }
     
     @Test
     @DisplayName("이름에 공백이 존재하는 경우 예외 처리")
-    void 예외_테스트1() {
+    void 이름_예외_테스트1() {
         assertThatThrownBy(() -> inputView.setInputNames("pobi,woni, jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("공백을 제거해주세요.");
@@ -50,10 +50,47 @@ class InputViewTest {
 
     @Test
     @DisplayName("이름의 길이가 5글자를 넘어서는 경우")
-    void 예외_테스트2() {
+    void 이름_예외_테스트2() {
         assertThatThrownBy(() -> inputView.setInputNames("qwerasdf"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이름은 1~5글자로 작성해주세요.");
 
+    }
+
+    @Test
+    @DisplayName("시도할 횟수 입력 테스트")
+    void 시도횟수_입력_테스트() {
+        //직접 입력하는 것으로 Console.readline() 대체
+        inputView.setNumberOfAttempts("123456789");
+
+        assertThat(inputView.getNumberOfAttempts()).isEqualTo(123456789);
+    }
+
+    @Test
+    @DisplayName("1이상의 양의정수가 아닌 숫자를 입력한 경우")
+    void 시도횟수_예외_테스트() {
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts("qwerasdf"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
+
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts("-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
+
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
+
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts("1 "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
+
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts("-2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
+
+        assertThatThrownBy(() -> inputView.setNumberOfAttempts("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1이상의 숫자만 입력해주세요.");
     }
 }
