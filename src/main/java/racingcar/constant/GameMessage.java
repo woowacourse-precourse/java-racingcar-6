68610f03827b378aa6cf3d9fc44ffb51193b5ad8
YuJeongYun <yunyj99@naver.com>
2023-10-30 01:35:46 +0900

    refactor: 게임 메세지에서 실행 결과 상수 앞에 개행 문자 추가

--- src/main/java/racingcar/constant/GameMessage.java
+++ src/main/java/racingcar/constant/GameMessage.java
@@ -7,7 +7,7 @@ public final class GameMessage {
     public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String GAME_MOVE_CNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public static final String GAME_RESULT_MESSAGE = "실행 결과";
+    public static final String GAME_RESULT_MESSAGE = "\n실행 결과";
     public static final String EACH_MOVE_RESULT_MESSAGE = "%s : %s\n";
     public static final String GAME_WINNER_MESSAGE = "최종 우승자 : %s";
     public static final String DASH = "-";
