cda3969bac82383c46e4756ff489b7cd05dc1c2d
jeongjihun <wlgns2234@naver.com>
2023-10-29 14:18:51 +0900

    refactor: ","를 상수로 표현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,12 +10,15 @@ public class InputView {
 
     private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIAL_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    
+    private static final String DELIMITER_COMMA = ",";
+
 
     public static List<String> inputNames() {
         System.out.println(INPUT_NAME_MESSAGE);
         try {
             String names = Console.readLine();
-            return Arrays.asList(names.split(","));
+            return Arrays.asList(names.split(DELIMITER_COMMA));
         } catch (Exception e) {
             throw new IllegalArgumentException("[Error] 잘못된 형식의 자동차 이름이 입력되었습니다.", e);
         }
