58de9ec14f157a4d5be156c56135e3c2262b25a1
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 20:35:05 +0900

    refactor(NumberGenerator): 메서드명 변경

--- src/main/java/racingcar/generator/NumberGenerator.java
+++ src/main/java/racingcar/generator/NumberGenerator.java
@@ -1,5 +1,5 @@
 package racingcar.generator;
 
 public interface NumberGenerator {
-    int generate();
+    int generateNumber();
 }
--- src/main/java/racingcar/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/generator/RandomNumberGenerator.java
@@ -3,8 +3,11 @@ package racingcar.generator;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     @Override
-    public int generate() {
-        return Randoms.pickNumberInRange(0, 9);
+    public int generateNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
