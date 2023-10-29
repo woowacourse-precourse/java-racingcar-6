d2491b0cd95c051f3835a32400db3a925d739bc8
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 12:44:01 +0900

    test(RacingResultTest): 테스트코드 내에서 사용하는 메서드명 변경 RacingRoundResult를 생성시 List<Car> 타입에서 List<CarInfo> 타입으로 변경했기 때문에, 테스트코드 내에서 사용하는 생성 테스트명 변경.

--- src/test/java/racingcar/domain/RacingResultTest.java
+++ src/test/java/racingcar/domain/RacingResultTest.java
@@ -11,75 +11,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.dto.CarInfo;
 
 public class RacingResultTest {
-
-    static Stream<Arguments> provideGetFinalWinnerTestArguments() {
-        return Stream.of(
-                arguments(List.of(
-                        new RacingRoundResult(List.of(
-                                setCar("pobi", 1),
-                                setCar("woni", 1),
-                                setCar("jun", 0)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 2),
-                                setCar("woni", 2),
-                                setCar("jun", 0)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 2),
-                                setCar("woni", 2),
-                                setCar("jun", 0)
-                        ))
-                ), List.of("pobi", "woni")),
-                arguments(List.of(
-                        new RacingRoundResult(List.of(
-                                setCar("pobi", 1),
-                                setCar("woni", 1),
-                                setCar("jun", 0)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 2),
-                                setCar("woni", 2),
-                                setCar("jun", 0)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 3),
-                                setCar("woni", 2),
-                                setCar("jun", 0)
-                        ))
-                ), List.of("pobi")),
-                arguments(List.of(
-                        new RacingRoundResult(List.of(
-                                setCar("pobi", 1),
-                                setCar("woni", 1),
-                                setCar("jun", 0)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 2),
-                                setCar("woni", 2),
-                                setCar("jun", 1)
-                        )), new RacingRoundResult(List.of(
-                                setCar("pobi", 2),
-                                setCar("woni", 2),
-                                setCar("jun", 2)
-                        ))
-                ), List.of("pobi", "woni", "jun"))
-        );
-    }
-
-    private static Car setCar(String carName, int position) {
-        return new Car(carName, new FixedCarEngine(true), new Position(position));
-    }
 
     @DisplayName("getRacingRoundResult()를 통해 얻어낸 읽기전용 리스트를 조작하려고 하면 에러가 발생한다.")
     @Test
     void getRacingRoundResultsManipulateFailTest() {
         RacingResult racingResult = new RacingResult();
         RacingRoundResult racingRoundResult = new RacingRoundResult(List.of(
-                setCar("pobi", 3),
-                setCar("woni", 3)
+                setCarInfo("pobi", 3),
+                setCarInfo("woni", 3)
         ));
         racingResult.addResult(racingRoundResult);
         List<RacingRoundResult> racingRoundResults = racingResult.getRacingRoundResults();
         assertThatCode(
-                () -> racingRoundResults.add(new RacingRoundResult(List.of(setCar("pobi", 4), setCar("woni", 4)))))
+                () -> racingRoundResults.add(
+                        new RacingRoundResult(List.of(setCarInfo("pobi", 4), setCarInfo("woni", 4)))))
                 .isInstanceOf(UnsupportedOperationException.class);
         assertThatCode(() -> racingRoundResults.remove(0))
                 .isInstanceOf(UnsupportedOperationException.class);
@@ -92,8 +40,8 @@ public class RacingResultTest {
     void getRacingRoundResultsReadSuccessTest() {
         RacingResult racingResult = new RacingResult();
         RacingRoundResult racingRoundResult = new RacingRoundResult(List.of(
-                setCar("pobi", 3),
-                setCar("woni", 3)
+                setCarInfo("pobi", 3),
+                setCarInfo("woni", 3)
         ));
         racingResult.addResult(racingRoundResult);
         List<RacingRoundResult> racingRoundResults = racingResult.getRacingRoundResults();
@@ -111,5 +59,59 @@ public class RacingResultTest {
         }
         List<String> finalWinners = racingResult.getFinalWinners();
         assertThat(finalWinners).isEqualTo(expectedWinners);
+    }
+
+    static Stream<Arguments> provideGetFinalWinnerTestArguments() {
+        return Stream.of(
+                arguments(List.of(
+                        new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 1),
+                                setCarInfo("woni", 1),
+                                setCarInfo("jun", 0)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 2),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 0)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 2),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 0)
+                        ))
+                ), List.of("pobi", "woni")),
+                arguments(List.of(
+                        new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 1),
+                                setCarInfo("woni", 1),
+                                setCarInfo("jun", 0)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 2),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 0)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 3),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 0)
+                        ))
+                ), List.of("pobi")),
+                arguments(List.of(
+                        new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 1),
+                                setCarInfo("woni", 1),
+                                setCarInfo("jun", 0)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 2),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 1)
+                        )), new RacingRoundResult(List.of(
+                                setCarInfo("pobi", 2),
+                                setCarInfo("woni", 2),
+                                setCarInfo("jun", 2)
+                        ))
+                ), List.of("pobi", "woni", "jun"))
+        );
+    }
+
+    private static CarInfo setCarInfo(String carName, int position) {
+        return new CarInfo(carName, position);
     }
 }
