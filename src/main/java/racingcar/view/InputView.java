f657ba509cf8f81c18147fe7c76f25ac7cd277ea
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-29 22:06:31 +0900

    feat: 자동차 이름 입력 ,에 따라 분할하는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,16 +2,31 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
     private static final String CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT = "시도할 회수는 몇회인가요?";
+    private static final String DELIMITER = ",";
+
 
     public void inputCarName(){
         System.out.println(CAR_INPUT);
         String carName = Console.readLine();
 
+        List<String> carNameList = splitCarName(carName);
+
         //유효성 검증
+    }
+
+    public List<String> splitCarName(String carName){
+        List<String> carNameList = Arrays.
+                stream(carName.split(DELIMITER)).
+                toList();
+
+        return carNameList;
     }
 
     public void inputCount(){
