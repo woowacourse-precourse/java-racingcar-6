d253b57e44d26943e825d330eece61ec2e2d7c5d
JaeY0ungJung <hanol5216@gmail.com>
2023-10-29 14:30:41 +0900

    feat: add constant for output message

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Constant {
+
+    static final String ASK_TRYCOUNT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String ASK_CARS_NAME_MESSAGE = "시도할 회수는 몇회인가요?";
+    static final String MOVECOUNT_RESULT_MESSAGE = "실행 결과";
+    static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
+    static final String UNIT_MOVE_BLOCK = "-";
+}
