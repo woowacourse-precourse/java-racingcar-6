bbd4df220f5ac3f094bbb8f151ffd1101e00727c
PUNGZI <ashyoo1367@gmail.com>
2023-10-29 19:07:10 +0900

    style: indent depth를 줄이기 위해 메소드 분리

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -6,22 +6,29 @@ public class Racing {
     public Map<String, Integer> racingMoveCount() {
         RacingCount racingCount = new RacingCount();
         Car car = new Car();
-
         int raceCount = racingCount.inputRacingCount();
         System.out.println();
         System.out.println("실행 결과");
         Map<String, Integer> result = null;
         for (int i=0; raceCount>i; i++) {
             result = car.racingCarCount();
-            result.forEach((key, value)->{
-                System.out.print(key + " : ");
-                for (int j = 0; value > j; j++) {
-                    System.out.print("-");
-                }
-                System.out.println();
-            });
+            printRacingCar(result);
             System.out.println();
         }
         return result;
+    }
+
+    public void printRacingCar(Map<String, Integer> result) {
+        result.forEach((key, value)->{
+            System.out.print(key + " : ");
+            printMoveCount(value);
+            System.out.println();
+        });
+    }
+
+    public void printMoveCount(int moveCount) {
+        for (int i = 0; moveCount > i; i++) {
+            System.out.print("-");
+        }
     }
 }
