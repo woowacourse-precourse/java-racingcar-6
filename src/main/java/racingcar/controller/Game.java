2613d77ce7ad8156cd59fda572c9f4b74b21b900
JUNG GYUWON <milestogo0905@naver.com>
2023-10-29 14:22:36 +0900

    feat: Prompt 메시지 출력 기능 추가

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -6,18 +6,22 @@ import racingcar.model.Car;
 import racingcar.model.CarCreator;
 import racingcar.model.RaceManager;
 import racingcar.view.View;
+import racingcar.view.constant.PromptMessage;
 
 public class Game {
 
     public static void start() {
+        View.printMessage(PromptMessage.CAR_NAME_INPUT);
         String carNamesString = View.getInput();
         List<String> carNames = CarCreator.splitCarNamesString(carNamesString);
         CarCreator.validateCarNames(carNames);
         List<Car> cars = CarCreator.createCars(carNames);
 
+        View.printMessage(PromptMessage.NUMBER_OF_MOVE_INPUT);
         String numberOfMoveInput = View.getInput();
         int numberOfMove = RaceManager.validateNumberOfMove(numberOfMoveInput);
 
+        View.printMessage(PromptMessage.RACE_RESULT);
         startRace(cars, numberOfMove);
 
         List<String> winnerList = RaceManager.decideWinner(cars);
--- src/main/java/racingcar/view/constant/PromptMessage.java
+++ src/main/java/racingcar/view/constant/PromptMessage.java
@@ -3,7 +3,7 @@ package racingcar.view.constant;
 public enum PromptMessage implements Message {
     CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     NUMBER_OF_MOVE_INPUT("시도할 회수는 몇회인가요?"),
-    RACE_RESULT("실행 결과");
+    RACE_RESULT("\n실행 결과");
 
     private final String message;
 
