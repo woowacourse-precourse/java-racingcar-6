058f46b289f1a464991757b0c05d9457002c3edc
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-10-29 20:56:32 +0900

    feat(Ui): 시작문구를 출력한다.

---
+++ src/main/java/racingcar/ui/Ui.java
@@ -0,0 +1,10 @@
+package racingcar.ui;
+
+public class Ui {
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public void start() {
+        System.out.println(START_MESSAGE);
+    }
+
+}
---
+++ src/test/java/racingcar/ui/UiTest.java
@@ -0,0 +1,32 @@
+package racingcar.ui;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class UiTest {
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void 시작문구를_출력한다() {
+        String expectMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        new Ui().start();
+        Assertions.assertThat(outputStreamCaptor.toString().trim()).isEqualTo(expectMessage);
+    }
+
+}
