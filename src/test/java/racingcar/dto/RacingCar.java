951d7289ae90981977d64468c44cbd30fc0724b7
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 16:37:13 +0900

    feat: RacingCar 생성 및 생성자 생성
    name 인자 값으로 name을 초기화해주고 distance를 0으로 초기화해주는 생성자를 만들었습니다.

---
+++ src/test/java/racingcar/dto/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar.dto;
+
+public class RacingCar {
+    private String name;
+    private int distance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        distance = 0;
+    }
+}
