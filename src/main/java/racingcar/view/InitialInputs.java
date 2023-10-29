9d1b91c753594331527f43a8514b04bdec4b7607
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-29 15:35:05 +0900

    refactor(InitialInputs): 입력받은 이름들을 List로 반환하도록 수정

--- src/main/java/racingcar/view/InitialInputs.java
+++ src/main/java/racingcar/view/InitialInputs.java
@@ -2,14 +2,16 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.List;
 import racingcar.validation.CarNames;
 
 public class InitialInputs {
-    public static String readCarNames() {
+    public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         CarNames checkCarNames = new CarNames();
-        checkCarNames.validateCarNames(input);
-        return input;
+        return checkCarNames.validateCarNames(input);
     }
+
+
 }
