83cc658079844a8a0e2a296c57f4bdcbe4e3a289
정영록 <s9285s@naver.com>
2023-10-29 22:04:39 +0900

    feat : 자동차 이름 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,50 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Map;
+
 public class Application {
+
+    static String[] carNameList;
+    static Map<String, Integer> carInfo;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        startRacing();
+        inputCarNames();
+    }
+
+    public static void startRacing() {
+
+        inputCarNames();
+    }
+
+    public static void inputCarNames() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+
+        String carNames = Console.readLine();
+        carNameList = carNames.split(",");
+
+        if (carNameList.length < 2) {
+
+            throw new IllegalArgumentException("자동차 이름을 2개 이상 입력해주세요.");
+        }
+
+        for (String inputName : carNameList) {
+
+            String carName = inputName.replaceAll("\\s", "");
+            if (carName.length() > 5 || carName.length() == 0) {
+                throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    public static void createCar() {
+
+        for (String carName : carNameList) {
+            carInfo.put(carName, 0);
+        }
     }
 }
