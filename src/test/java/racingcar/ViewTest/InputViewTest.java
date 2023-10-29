6473d1b453a4e32e947a165dbb3c15256a3dc49a
Firedrago95 <yonghwa95@naver.com>
2023-10-29 11:11:59 +0900

    refactor : InputViewTest 리팩토링, System 초기화 추가

--- src/test/java/racingcar/ViewTest/InputViewTest.java
+++ src/test/java/racingcar/ViewTest/InputViewTest.java
@@ -1,33 +1,47 @@
 package racingcar.ViewTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import racingcar.View.InputView;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class InputViewTest {
+
+    private InputStream originalSystemIn;
+
+    @BeforeEach
+    public void setUp() {
+        originalSystemIn = System.in;
+    }
+
+    @AfterEach
+    public void restoreSystemIn() {
+        System.setIn(originalSystemIn);
+    }
 
     @Test
     @DisplayName("자동차명 입력 테스트")
     public void requestCarNamesTest() {
-        ByteArrayInputStream carNamesInput = new ByteArrayInputStream("ray,tico".getBytes());
-        System.setIn(carNamesInput);
+        setInput("ray,tico");
         String inputedCarNames = InputView.requestCarNames();
-        System.setIn(System.in);
-
-        assertEquals("ray,tico",inputedCarNames);
+        assertEquals("ray,tico", inputedCarNames);
     }
 
     @Test
     @DisplayName("라운드 입력 테스트")
     public void requestRoundsTest() {
-        ByteArrayInputStream roundsInput = new ByteArrayInputStream("5".getBytes());
-        System.setIn(roundsInput);
+        setInput("5");
         String inputedRounds = InputView.requestRounds();
-        System.setIn(System.in);
+        assertEquals("5", inputedRounds);
+    }
 
-        assertEquals("5",inputedRounds);
+    private void setInput(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
     }
 }
