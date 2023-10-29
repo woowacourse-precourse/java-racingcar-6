4406de77ed8e84d58f10a1b63d128bdfc581e305
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-30 03:03:28 +0900

    feat : 자동차 요구사항 1번 테스트 코드 & 기능 구현
    TDD 기법에 준수하나 ,
    private 에서 protected 로 변경에 따라 테스트 추가 구현

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -18,6 +18,13 @@ public class CarTest {
         car = Mockito.mock(Car.class);
     }
     @Test
+    void 최소값과_최대값사이_랜덤한_숫자를_생성한다(){
+        Mockito.when(car.generateRandomSpeed()).thenCallRealMethod();
+        int speed = car.generateRandomSpeed();
+        Assertions.assertTrue(speed>= 1 && speed<=9);
+    }
+
+    @Test
     void 숫자가_일정값을_넘으면_전진할수있다(){
         Mockito.when(car.generateRandomSpeed()).thenReturn(5);
         Mockito.when(car.checkCanMoveForwardIfSpeedIsExceedThreshold()).thenCallRealMethod();
