bd13e1e68dd79865807fb8203ada79d3ba40d191
BurningFalls <sherlocky31415@gmail.com>
2023-10-29 20:22:36 +0900

    refactor: raceCarNamesInput 전역변수에서 지역변수로 바꿈
    receiveRaceCarNames 메서드에서 carList를 얻는데만 사용하는 변수이기 때문에, 메서드 내의 지역 변수로 선언

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
-    private static RaceCarNames raceCarNames;
     private static TryCount tryCount;
     private static CarList carList;
 
@@ -33,7 +32,7 @@ public class Application {
     public static void receiveRaceCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String raceCarNamesInput = Console.readLine();
-        raceCarNames = new RaceCarNames(raceCarNamesInput);
+        RaceCarNames raceCarNames = new RaceCarNames(raceCarNamesInput);
         carList = new CarList();
 
         List<String> carNameList = raceCarNames.parseCarNamesFromRaceCarInput();
