177e03ffdcd53b12de42880777017baf84df06d1
devery59 <kunhee9045@gmail.com>
2023-10-29 20:22:34 +0900

    feat: Get Input value and check its lenght under 5

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputCarName = readLine();
+        List<String> carNameList = utils.convertToList(inputCarName);
+        utils.checkCarNameUnderFiveLength(carNameList);
     }
 }
---
+++ src/main/java/racingcar/utils.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class utils {
+    public static List<String> convertToList(String value) {
+        try {
+            return Arrays.asList(value.split(","));
+        } catch (Exception e){
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+        }
+    }
+
+    public static void checkCarNameUnderFiveLength(List<String> strings){
+        for (String s: strings){
+            if (s.length() > 5){
+                throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+            }
+        }
+    }
+}
