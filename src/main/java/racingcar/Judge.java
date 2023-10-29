dd59c2ef1830143a8a60ad6b7348a69e7900dd58
parking0 <p980710@gmail.com>
2023-10-29 20:36:25 +0900

    refactor : Judge 함수 작게 나누기

--- src/main/java/racingcar/Judge.java
+++ src/main/java/racingcar/Judge.java
@@ -4,25 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Judge {
+    List<String> winner = new ArrayList<>();
 
     public List<String> findWinner(List<Car> cars) {
         Car car = cars.get(0);
         int maxMove = car.getMove();
-        List<String> winner = new ArrayList<>();
-        winner.add(car.getName());
+
+        addWinner(car.getName());
 
         for (int i = 1; i < cars.size(); i++) {
             car = cars.get(i);
             int move = car.getMove();
-            if (maxMove < move) {
+
+            if (isLongest(move,maxMove)) {
                 maxMove = move;
                 winner.clear();
-                winner.add(car.getName());
-            } else if (maxMove == move) {
-                winner.add(car.getName());
+                addWinner(car.getName());
+            }
+            else if (isWinner(move,maxMove)) {
+                addWinner(car.getName());
             }
         }
+
         return winner;
+    }
+
+    private boolean isLongest(int move, int maxMove){
+        return move > maxMove;
+    }
+
+    private boolean isWinner(int move, int maxMove){
+        return move == maxMove;
+    }
+
+    private void addWinner(String name){
+        winner.add(name);
     }
 
 }
