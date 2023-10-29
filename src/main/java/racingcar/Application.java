5cc4b31fef81fb48cadb9627b9f586f6d9a52c87
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-29 15:55:54 +0900

    feat(racingcar): implement the whole game and print out the final winner
    - get keys that have the max value and make it to list
    - print out the element of the max value key list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import racingcar.domain.ImplementGame;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ImplementGame implementGame = new ImplementGame();
+
+        Map<String, Integer> numMoveMap = implementGame.playGame();
+        List<String> winnerList = getWinner(numMoveMap);
+        String prizeWinner = String.join(",", winnerList);
+        System.out.println("최종우승자: " + prizeWinner);
+    }
+    public static List<String> getWinner(Map<String, Integer> numMap) {
+        int maxValue = Collections.max(numMap.values());
+        List<String> maxValueKeys = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : numMap.entrySet()) {
+            if (entry.getValue() == maxValue) {
+                maxValueKeys.add(entry.getKey());
+            }
+        }
+        return maxValueKeys;
+
     }
 }
