e9883b06e0a32780076ecfaf1d7d040d5132b5c0
YuJeongYun <yunyj99@naver.com>
2023-10-29 22:04:59 +0900

    feat: 자동차(Car) 클래스에 예외 처리 기능 추가
    자동차 이름 길이가 조건을 만족하지 않으면 예외가 발생되도록 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,8 @@
 package racingcar.domain;
 
+import racingcar.constant.ErrorMessage;
+import racingcar.constant.GameMessage;
+
 /**
  * 자동차 이름과 이동 거리를 가지고 있는 클래스
  */
@@ -10,9 +13,14 @@ public class Car {
     public Car() {
 
     }
+    public Car(String name) {
+        name = name.trim();
+        checkCarNameCondition(name);
 
+        this.name=name;
+    }
     public Car(String name, int dist) {
-        this.name = name;
+        this(name);
         this.dist = dist;
     }
 
@@ -22,5 +30,16 @@ public class Car {
 
     public int getDist() {
         return dist;
+    }
+
+    private void checkCarNameCondition(String name){
+        // 자동차 이름이 길이 제한을 초과했을 경우 예외 발생
+        if(name.length() > GameMessage.CAR_NAME_LENGTH_MAX_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_OVER_ERR_MESSAGE);
+        }
+        // 자동차 이름이 공백일 경우 예외 발생
+        if(name.length() == 0) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_BLANK_ERR_MESSAGE);
+        }
     }
 }
