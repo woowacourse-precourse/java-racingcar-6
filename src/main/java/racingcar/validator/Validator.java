bbab0fc7f1c018c3a02197300a0592804d1c5d2a
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-29 21:34:14 +0900

    chore(validator):가독성을 위한 네이밍 처리

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -9,59 +9,58 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-    public static void stringsLengthInRange(List<String> strings, int minSize, int maxSize) {
-        for (String string : strings) {
-            stringLengthInRange(string, minSize, maxSize);
+    public static void commandsLengthInRange(List<String> commands, int minSize, int maxSize) {
+        for (String command : commands) {
+            commandLengthInRange(command, minSize, maxSize);
         }
     }
 
-    public static void stringLengthInRange(String string, int minSize, int maxSize) {
-        if (string.length() < minSize || string.length() > maxSize) {
+    public static void commandLengthInRange(String command, int minSize, int maxSize) {
+        if (command.length() < minSize || command.length() > maxSize) {
             throw new IllegalArgumentException("문자열의 길이가 범위 내에 속하지 않습니다");
         }
     }
 
     //에러메세지를 constant로 묶어서 매개변수로 받아서쓰는것
-    //일일이 메시지를 정해준다면 정해진 게임에서의 정확한 에러를 찾기 힘들다.
+    //일일이 메시지를 고정시키면 정해진 게임에서의 정확한 에러를 찾기 힘들다.
     //예를 들어서 문자열 리스트 중에 중복된 문자열이 존재합니다.
     //를 자동차 이름들중 중복된 값이 존재합니다. 로 변경
-    public static void stringsNotDuplicated(List<String> strings) {
-        long count = strings.stream().distinct().count();
-        if (strings.size() != count) {
+    public static void commandsNotDuplicated(List<String> commands) {
+        long count = commands.stream().distinct().count();
+        if (commands.size() != count) {
             throw new IllegalArgumentException("문자열 리스트 중에 중복된 문자열이 있습니다.");
         }
     }
 
-    public static void stringsContainsOnlyRegex(List<String> strings, String regex) {
-        for (String string : strings) {
-            stringContainsOnlyRegex(string, regex);
+    public static void commandsFollowRegex(List<String> commands, String regex) {
+        for (String command : commands) {
+            commandFollowRegex(command, regex);
         }
     }
 
-    public static void stringContainsOnlyRegex(String string, String regex) {
-        if (!Pattern.matches(regex, string)) {
+    public static void commandFollowRegex(String command, String regex) {
+        if (!Pattern.matches(regex, command)) {
             throw new IllegalArgumentException("정해진 정규식을 벗어났습니다.");
         }
 
     }
 
-    public static void stringsSizeBiggerThan(int size, List<String> carNames) {
-        if (carNames.size() <= size) {
+    public static void commandsSizeBiggerThan(int size, List<String> commands) {
+        if (commands.size() <= size) {
             throw new IllegalArgumentException("리스트가 정해진 크기에 맞지않습니다.");
         }
     }
 
-    public static void stringToInteger(String string) {
+    public static void commandToInteger(String command) {
         try {
-            Integer.parseInt(string);
+            Integer.parseInt(command);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("너무 광범위한 숫자이거나 올바른 숫자 형식이 아닙니다.");
         }
     }
 
-    public static void integerInRange(Integer integer, int minValue, int maxValue) {
-
-        if (integer < minValue || integer > maxValue) {
+    public static void commandValueInRange(Integer commandValue, int minValue, int maxValue) {
+        if (commandValue < minValue || commandValue > maxValue) {
             throw new IllegalArgumentException("값이 정해진 범위내에 속하지 않습니다");
         }
     }
