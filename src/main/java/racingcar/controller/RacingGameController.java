4c41ff0e4ff6fb075b436163eb70c70e01c4a0f2
Jae-Young98 <ygy0102@gmail.com>
2023-10-29 22:44:56 +0900

    feat(RacingGameController): 게임을 진행하는 기능 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,32 +1,48 @@
 package racingcar.controller;
 
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.util.Converter;
 import racingcar.util.InputValidator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.List;
 
 public class RacingGameController {
     private final InputView inputView = new InputView();
-    private final InputValidator inputValidator = new InputValidator();
-    private final Converter converter = new Converter();
+    private final OutputView outputView = new OutputView();
+    private final Cars cars = new Cars();
+    private int attemptNumber;
 
     public void run() {
         initialize();
+        start();
     }
 
     private void initialize() {
         String inputNames = inputView.inputNames();
-        List<String> carNames = converter.getCarNames(inputNames);
-        inputValidator.validateCarNames(carNames);
-        List<Car> cars = carNames.stream()
-                .map(Car::new)
-                .toList();
+        List<String> carNames = Converter.getCarNames(inputNames);
+        InputValidator.validateCarNames(carNames);
+        for (String name : carNames) {
+            cars.addCar(new Car(name));
+        }
 
         String inputNumber = inputView.inputAttemptNumber();
-        if (inputValidator.validateAttemptNumber(inputNumber)) {
-            int attemptNumber = Integer.parseInt(inputNumber);
+        if (InputValidator.validateAttemptNumber(inputNumber)) {
+            attemptNumber = Integer.parseInt(inputNumber);
         }
+    }
+
+    private void start() {
+        outputView.showResult();
+
+        int currentAttempt = 0;
+        while (currentAttempt < attemptNumber) {
+            cars.move();
+            outputView.showPosition(cars);
+            currentAttempt++;
+        }
+
     }
 }
