82262d40661841d2ec63efdfbf6a91f30e7c08e8
Juhyorim <joohr1234@gmail.com>
2023-10-30 04:06:23 +0900

    fix(racingcar): ConsoleInput 자동차 이름 저장 시 공백처리 추가

--- src/main/java/racingcar/domain/ConsoleInput.java
+++ src/main/java/racingcar/domain/ConsoleInput.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConsoleInput implements Input {
     private InputValidator inputValidator;
@@ -16,7 +17,16 @@ public class ConsoleInput implements Input {
         String carNames = Console.readLine();
         inputValidator.validateCarNames(carNames);
 
-        return new ArrayList<>(List.of(carNames.split(",")));
+        return splitCarNames(carNames);
+    }
+
+    private List<String> splitCarNames(String carNames) {
+        List<String> nameList = new ArrayList<>(List.of(carNames.split(",")));
+        List<String> trimedNameList = nameList.stream()
+                .map(name -> name.strip())
+                .collect(Collectors.toList());
+
+        return trimedNameList;
     }
 
     @Override
