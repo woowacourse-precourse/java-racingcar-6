64f6f316de66e0fe690efa21fc16e9b334f4c2d7
sw801733 <sw801733@naver.com>
2023-10-29 16:04:31 +0900

    feat : 시도할 횟수 입력 추가 및 레이싱 게임 기능 구현
    - 시도할 횟수 입력 후 입력값 만큼 게임 진행 반복
    - 자동차 별로 랜덤한 숫자 생성 후 4 이상이 나올 경우 위치 1씩 증가
    - 시도 횟수가 모두 끝나면 가장 큰 위치 값을 가진 자동차를 출력 (중복 가능)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,12 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Application {
     
@@ -27,25 +30,86 @@ public class Application {
         }
     }
 
+    // 자동차 전진 혹은 정지
+    // - 랜덤한 숫자 생성 후 4 이상일 경우 해당 자동차의 위치 1 증가
+    public static void moveForwardCars() {
+        for(String carName : carState.keySet()) {
+            int currentPosition = carState.get(carName);
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            if (randomNumber >= 4) {
+                carState.put(carName, currentPosition + 1);
+            }
+        }
+    }
+
+    // 실행 결과 출력
+    public static void printRacingResult() {
+        for(String carName : carState.keySet()) {
+            int currentPosition= carState.get(carName);
+
+            System.out.print(carName + " : ");
+            for (int i = 0; i < currentPosition; i++) {
+                System.out.print("-");
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    // 우승자 출력
+    public static void printWinner() {
+
+        int maxPosition = 0;
+        List<String> winners = new ArrayList<>();
+
+        for(String carName : carState.keySet()) {
+            int currentPosition = carState.get(carName);
+            if (maxPosition < currentPosition)
+                maxPosition = currentPosition;
+        }
+
+        for(String carName : carState.keySet()) {
+            int currentPosition = carState.get(carName);
+            if (maxPosition == currentPosition)
+                winners.add(carName);
+        }
+
+        System.out.print("최종 우승자 : ");
+        String winner = String.join(", ", winners);
+        System.out.print(winner);
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
 
         // 사용자로부터 자동차 이름 입력을 받는 부분
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = readLine();
         String[] carNameArr = carNames.split(",");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int attempts = Integer.parseInt(readLine());
+
 
         // 사용자 입력 확인
         checkCarNameLength(carNameArr);
 
         // CarState에 초기화
         initializeCarState(carNameArr);
-        
-        for(String key : carState.keySet()) {
-            int value = carState.get(key);
-            System.out.println(key + " : " + value);
+
+        System.out.println("실행 결과");
+
+        // 시도 횟수 만큼 레이싱 게임 진행과 결과 출력
+        for(int i = 0; i < attempts; i++) {
+            moveForwardCars();
+            printRacingResult();
         }
 
-
+        printWinner();
     }
 }
