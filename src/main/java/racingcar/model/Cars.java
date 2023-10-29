2d839889f3f0272a241522ad937dc717c6be92cf
tam31 <wxogud3377@naver.com>
2023-10-29 23:00:09 +0900

    Feat(Cars) : 이동횟수 추가 Docs : 이동횟수 추가 완료 체크

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 public class Cars {
     private List<Car> cars;
+    private int playNumbers;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
+    public int getPlayNumbers() {
+        return playNumbers;
+    }
+
     public List<Car> getCars() {
         return cars;
+    }
+
+    public void playNumberTimes(int number){
+        playNumbers = number;
     }
 }
