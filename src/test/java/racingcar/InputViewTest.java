0cd8176f58a24f7ff6261c4f4b9ac23c0ccff60c
KimDabomi <cdabomi@nate.com>
2023-10-29 15:24:13 +0900

    test: InputView Exceptions

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,13 +1,17 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import camp.nextstep.edu.missionutils.Console;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.DataInput;
 import java.io.InputStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
@@ -46,5 +50,55 @@ class InputViewTest {
         InputView inputView = new InputView();
         int result = inputView.getTryCount();
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void checkCarNameExceptions_중복확인() {
+        String[] inputArr = {"da","da","bo","mi"};
+        InputView inputView = new InputView();
+        MagicVariable magicVariable = new MagicVariable();
+        assertThatThrownBy(() ->  inputView.checkCarNameExceptions(inputArr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(magicVariable.CAR_DUPLICATE_ERROR_MESSAGE);
+    }
+
+    @Test
+    void checkCarNameExceptions_대문자불가확인() {
+        String[] inputArr = {"Da","da","bo","mi"};
+        InputView inputView = new InputView();
+        MagicVariable magicVariable = new MagicVariable();
+        assertThatThrownBy(() ->  inputView.checkCarNameExceptions(inputArr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(magicVariable.CAR_NAME_ERROR_MESSAGE);
+    }
+
+    @Test
+    void checkCarNameExceptions_숫자불가확인() {
+        String[] inputArr = {"11","da","bo","mi"};
+        InputView inputView = new InputView();
+        MagicVariable magicVariable = new MagicVariable();
+        assertThatThrownBy(() ->  inputView.checkCarNameExceptions(inputArr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(magicVariable.CAR_NAME_ERROR_MESSAGE);
+    }
+
+    @Test
+    void checkCarNameExceptions_길이확인() {
+        String[] inputArr = {"kimdabomi","da","bo","mi"};
+        InputView inputView = new InputView();
+        MagicVariable magicVariable = new MagicVariable();
+        assertThatThrownBy(() ->  inputView.checkCarNameExceptions(inputArr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(magicVariable.CAR_NAME_ERROR_MESSAGE);
+    }
+
+    @Test
+    void checkTryCountExceptions_범위확인() {
+        int tryCount = 0;
+        InputView inputView = new InputView();
+        MagicVariable magicVariable = new MagicVariable();
+        assertThatThrownBy(() ->  inputView.checkTryCountExceptions(tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(magicVariable.TRY_COUNT_RANGE_ERROR_MESSAGE);
     }
 }
