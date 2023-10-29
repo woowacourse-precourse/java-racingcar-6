178bb753cc5dc29d16679d90c9ed674903ca3cf7
SJ70 <seojae70@gmail.com>
2023-10-29 22:24:16 +0900

    Feat & Test: 라운드 값이 양수인지 검증

---
+++ src/test/java/racingcar/round/RoundValidateIsPositiveTest.java
@@ -0,0 +1,36 @@
+package racingcar.round;
+
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.DisplayName;
+        import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.params.ParameterizedTest;
+        import org.junit.jupiter.params.provider.CsvSource;
+        import racingcar.domain.Round;
+
+public class RoundValidateIsPositiveTest {
+
+    @Test
+    @DisplayName("라운드 횟수는 양수를 허용한다.")
+    void validateRoundIsPositiveSuccessTest() {
+        // given
+        int value = 1;
+
+        // then
+        Assertions.assertDoesNotThrow(() -> {
+            new Round(value);
+        });
+    }
+
+    @ParameterizedTest
+    @DisplayName("라운드 횟수는 0과 음수를 불허한다.")
+    @CsvSource(value = {
+            "0", "-1"
+    })
+    void validateRoundIsPositiveFailTest(int value) {
+        // then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Round(value);
+        });
+    }
+
+}
