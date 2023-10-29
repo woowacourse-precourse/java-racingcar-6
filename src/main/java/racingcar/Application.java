4a906f3d868bdf555e84a4730caebcfcd05f9e40
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-10-29 16:57:53 +0900

    feat(Application): 게임 진행 횟수 입력 기능 구현
    getPlayCount 구현
    - 게임 진행 횟수를 입력받아 반환하는 함수 구현
    - 에러 조건 (IllegalArgumentException 발생)
      - 입력한 문자가 숫자로 변환이 안될 때
      - 입력한 숫자가 1 ~ 10 사이가 아닌 경우

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,8 +10,10 @@ public class Application {
         // TODO: 프로그램 구현
 
         Set<String> carNames = getCarNames();
+        int playCount = getPlayCount();
 
         System.out.println(carNames);
+        System.out.println(playCount);
 
     }
 
@@ -43,5 +45,23 @@ public class Application {
         }
 
         return carNames;
+    }
+
+    static int getPlayCount() {
+        int playCount = 0;
+
+        try {
+            String inputCount = readLine();
+            playCount = Integer.parseInt(inputCount);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        if (playCount < 1 || playCount > 10) {
+            throw new IllegalArgumentException("플레이 횟수는 1~10 사이 입니다.");
+        }
+
+        return playCount;
     }
 }
