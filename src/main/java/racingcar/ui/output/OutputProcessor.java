38d12ad8795891df17bd9eee934996e1bfe4230a
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-29 20:58:20 +0900

    refactor(ui): printFinalWinner함수 출력 내용 변경

--- src/main/java/racingcar/ui/output/OutputProcessor.java
+++ src/main/java/racingcar/ui/output/OutputProcessor.java
@@ -2,29 +2,34 @@ package racingcar.ui.output;
 
 import racingcar.ui.Message;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class OutputProcessor {
     public void printMessage(String message) {
         System.out.println(message);
-    }
-
-    public void printStartMessageCarNames() {
-        System.out.println(Message.START_MESSAGE_CAR_NAMES.getMessage());
-    }
-
-    public void printStartMessageTryCount() {
-        System.out.println(Message.START_MESSAGE_TRY_COUNT.getMessage());
     }
 
     public void printResult() {
         System.out.println(Message.RESULT.getMessage());
     }
 
-    public void printResult(String carName, int forwardNumber) {
-        System.out.println(carName + " : " + "-".repeat(forwardNumber));
+    public void printResult(HashMap<String, Integer> carList) {
+        carList.forEach((carName, forwardNumber) ->
+                System.out.println(carName + " : " + "-".repeat(forwardNumber)));
+        System.out.println();
     }
 
-    public void printFinalWinner(String carNames) {
-        System.out.println(Message.FINAL_WINNER.getMessage() + carNames);
+    public void printFinalWinner(HashMap<String, Integer> carList) {
+        System.out.print(Message.FINAL_WINNER.getMessage());
+        Optional<Integer> maxValue = carList.values().stream().max(Integer::compareTo);
+        String result = carList.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(maxValue.orElse(null)))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
     }
 
 }
