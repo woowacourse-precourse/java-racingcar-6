2ebd93a3be3eb87e3d0b6e740c4a828416263b59
lee jihyeon <lejinhy@gmail.com>
2023-10-29 16:56:52 +0900

    feat: 이동 경로에 "-" 기호를 더하는 메소드 완성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,27 +1,34 @@
 package racingcar;
 
+import racingcar.constant.CarStatus;
+import racingcar.constant.SymbolType;
+
 public class Car {
-    private String name;
-    private StringBuilder moveTrace;
-    private int moveCount;
+    private String name;    // 자동차 이름
+    private StringBuilder nowMoveTrace; // 현재 까지의 이동 경로 StringBuilder
+    private int moveCount;  // 총 이동 횟수
 
     Car(String name) {
         this.name = name;
     }
 
-    public void setMoveTrace() {
-
+    public void setNowMoveTrace(CarStatus status) {
+        nowMoveTrace.append(SymbolType.TRACE_RACE_RESULT);
     }
 
-    public void setMoveCount() {
+    public void setMoveCount(CarStatus status) {
+        if (status.equals(CarStatus.MOVE))
+            moveCount++;
     }
 
     public String getName() {
         return name;
     }
-    public StringBuilder getMoveTrace() {
-        return moveTrace;
+
+    public StringBuilder getNowMoveTrace() {
+        return nowMoveTrace;
     }
+
     public int getMoveCount() {
         return moveCount;
     }
