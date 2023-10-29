40e8be434d07aa25fe5846e3ef264ff79da48c61
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:32:48 +0900

    feat(domain/system): Round 구현

---
+++ src/main/java/racingcar/domain/system/game/round/Round.java
@@ -0,0 +1,32 @@
+package racingcar.domain.system.game.round;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.core.car.OnRaceCar;
+import racingcar.domain.core.car.OnRaceCars;
+import racingcar.domain.system.game.round.host.Host;
+
+public class Round {
+
+    private final Host host;
+    private final OnRaceCars cars;
+
+    private Round(Host host, OnRaceCars cars) {
+        this.host = host;
+        this.cars = cars;
+    }
+
+    public static Round start(Host host, OnRaceCars cars) {
+        return new Round(host, cars);
+    }
+
+    public RoundResult play() {
+        List<OnRaceCar> movedCars = new ArrayList<>();
+        for (OnRaceCar car : cars) {
+            OnRaceCar movedCar = host.signal(car);
+            movedCars.add(movedCar);
+        }
+        OnRaceCars movedOnRaceCars = new OnRaceCars(movedCars);
+        return new RoundResult(movedOnRaceCars);
+    }
+}
