73976718eb346a8c772c6b72aa677970fadbfc5d
jeondui <jeondui@gmail.com>
2023-10-29 13:24:37 +0900

    feat(RacingGame): 경주 자동차들 중 우승자를 찾아내는 기능

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,7 +1,9 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
 
@@ -9,6 +11,21 @@ public class RacingGame {
 
     public RacingGame() {
         this.winners = new ArrayList<>();
+    }
+
+    public List<String> getWinners(List<Car> carList) {
+        this.winners = carList.stream()
+                .filter(car -> car.isLocationEqual(getMaxLocation(carList)))
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        return winners;
+    }
+
+    public int getMaxLocation(List<Car> carList) {
+        return carList.stream()
+                .max(Comparator.comparingInt(Car::getLocation))
+                .get().getLocation();
     }
 
 }
