082d694505ea3b8716e0c4905b4c1c1f31901463
Kite_Z <duswl339@ajou.ac.kr>
2023-10-29 21:54:25 +0900

    feat: add moveCar, getCarMoveCount method for RacingCar Class

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,14 +1,29 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static racingcar.global.RacingCarGameConfig.*;
+
 public class RacingCar {
     public String name;
     private int carMoveCount;
 
-    RacingCar(String name) {
+    public RacingCar(String name) {
         this.name = name;
     }
 
     public void incrementCarMoveCount() {
         this.carMoveCount++;
+    }
+
+    public int getCarMoveCount() {
+        return this.carMoveCount;
+    }
+
+    public void moveCar() {
+        int moveFlagNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER);
+        if (moveFlagNumber >= STANDARD_NUMBER) {
+            this.incrementCarMoveCount();
+        }
     }
 }
