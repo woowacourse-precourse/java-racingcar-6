ce9ab70dddb6d3680dcc0e44dfd171015afa9d5b
kylim99 <zxzx05050@naver.com>
2023-10-29 13:26:55 +0900

    feat: 자동차이름 중복 검사 기능 추가

--- src/main/java/racingcar/utill/ExceptionMessage.java
+++ src/main/java/racingcar/utill/ExceptionMessage.java
@@ -5,7 +5,8 @@ public enum ExceptionMessage {
 
     INVALID_NOT_NUMERIC("자연수만 입력 가능합니다."),
     INVALID_NUMBER_OF_CARS("자동차 경주는 두 대 이상부터 가능합니다."),
-    INVALID_CAR_NAME_LENGTH("자동차의 이름은 5글자 이하만 가능합니다.");
+    INVALID_CAR_NAME_LENGTH("자동차의 이름은 1글자 이상 5글자 이하만 가능합니다."),
+    INVALID_CAR_NAME_DUPLICATE("중복된 자동차 이름이 있습니다.");
 
     public static final String BASE_MESSAGE = "[ERROR] %s";
     private final String message;
--- src/main/java/racingcar/utill/Validator.java
+++ src/main/java/racingcar/utill/Validator.java
@@ -3,16 +3,18 @@ package racingcar.utill;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Validator {
 
     public void validationCarName(String input) {
         List<String> carNames = Arrays.stream(input.split(",")).toList();
         validationCarNamesLength(carNames);
         validationCarNamesListLength(carNames);
+        validationDuplicate(carNames);
     }
 
     private void validationCarNamesLength(List<String> carNames) {
-        carNames.stream().filter(name -> name.length() > 5)
+        carNames.stream().filter(name -> name.length() > 5 || name.isEmpty())
                 .findAny()
                 .ifPresent(name -> {
                     throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME_LENGTH.getMessage());
@@ -22,6 +24,13 @@ public class Validator {
     private void validationCarNamesListLength(List<String> carNames) {
         if (carNames.size() < 2) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_OF_CARS.getMessage());
+        }
+    }
+
+    private void validationDuplicate(List<String> carNames) {
+        List<String> nonDuplicateList = carNames.stream().distinct().toList();
+        if (carNames.size() != nonDuplicateList.size()) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME_DUPLICATE.getMessage());
         }
     }
 
