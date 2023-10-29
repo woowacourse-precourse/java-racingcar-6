5a86da682cd2d8ae42a3b2a9c0693cb4671b578a
BonSik <rnqhstlr2297@gmail.com>
2023-10-29 17:58:06 +0900

    feat: 시도 횟수 입력 메서드 구현

--- src/main/java/racingcar/view/InputDevice.java
+++ src/main/java/racingcar/view/InputDevice.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public final class InputDevice {
     private final static String DELIMITER = ",";
+    private final static String INVALID_NUMBER_MESSAGE = "숫자 외에 값은 입력할 수 없습니다.";
 
     private InputDevice() {
     }
@@ -13,6 +14,15 @@ public final class InputDevice {
     public static List<String> inputNamesWithCommaDelimiter() {
         String[] names = Console.readLine().split(DELIMITER, -1);
         return Arrays.asList(names);
+    }
+
+    public static int inputAttemptCount() {
+        String attemptCount = Console.readLine();
+        try {
+            return Integer.parseInt(attemptCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
     }
 
 }
