9465fca4454fe2ac79f590f1630807d8b709006b
Soobin-Park <jrkd2165@gmail.com>
2023-10-29 18:29:34 +0900

    :sparkles: feat: 자동차 이동하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,33 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
+
+    //차가 움직일지 멈춰있을지 판단
+    public static boolean isMove() {
+        int randomNumber = pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    //차 이동 기록
+    public static List<Integer> moveCars(int sizeOfCars, List carsStatus) {
+            for (int i = 0; i < sizeOfCars; i++) {
+                boolean movementOrNot = isMove();
+                if (movementOrNot){
+                    carsStatus.set(i, (int) carsStatus.get(i) + 1);
+                }
+            }
+            return carsStatus;
+    }
+
     public static void game() throws IllegalArgumentException {
 
         //사용자에게 입력값 (경주할 자동차 이름, 게임을 시도할 회수) 입력받기
@@ -27,15 +46,23 @@ public class Application {
         String stringNumberOfGames = readLine();
 
         //입력값이 숫자인지 확인
+        int numberOfGames = 0;
         try {
-            int numberOfGames = Integer.parseInt(stringNumberOfGames);
-        } catch (NumberFormatException e){
+            numberOfGames = Integer.parseInt(stringNumberOfGames);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
 
+        System.out.println("실행 결과");
 
+        List<Integer> carsStatus = new ArrayList<>();
+        for(int i=0;i< cars.size();i++){
+            carsStatus.add(0);
+        }
 
-
+        for(int i=0;i<numberOfGames;i++) {
+            carsStatus= moveCars(cars.size(),carsStatus);
+        }
     }
 
     public static void main(String[] args) {
