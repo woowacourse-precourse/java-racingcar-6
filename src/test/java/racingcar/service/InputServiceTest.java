c54f5335612d7eb19cd2462d4e05f078d0ca1237
Jin Geonwoo <jinkonu99@gmail.com>
2023-10-29 13:52:12 +0900

    comment: InputServiceTest에 GIVEN, WHEN, THEN 각주 추가

--- src/test/java/racingcar/service/InputServiceTest.java
+++ src/test/java/racingcar/service/InputServiceTest.java
@@ -9,10 +9,13 @@ import static racingcar.service.InputService.*;
 class InputServiceTest {
     @Test
     void convertToStringArray_쉼표로_연결된_문자열을_자동차_이름_문자열_배열로_반환() {
+        // GIVEN
         String line = "긴,난,보,이,즈";
 
+        // WHEN
         String[] stringArray = convertToStringArray(line);
 
+        // THEN
         assertThat(stringArray.length).isEqualTo(5);
 
         for (int i = 0; i < stringArray.length; i++)
@@ -21,8 +24,10 @@ class InputServiceTest {
 
     @Test
     void convertToStringArray_쉼표_사이에_5자_이상의_입력값에_대해_예외_발생() {
+        // GIVEN
         String line = "펄잼,너바나,사운드가든,앨리스인체인스";
 
+        // WHEN, THEN
         assertThatThrownBy(() -> convertToStringArray(line))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
@@ -30,15 +35,19 @@ class InputServiceTest {
 
     @Test
     void convertToLong_문자열을_Long형으로_변환() {
+        // GIVEN
         String line = "987654321";
 
+        // WHEN, THEN
         assertThat(convertToLong(line)).isInstanceOf(Long.class);
     }
 
     @Test
     void convertToLong_숫자가_아닌_문자열에_대해_예외_발생() {
+        // GIVEN
         String line = "브루트포스";
 
+        // WHEN, THEN
         assertThatThrownBy(() -> convertToLong(line))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("횟수는 숫자로만 입력해주세요.");
