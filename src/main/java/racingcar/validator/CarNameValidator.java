84351432cd064836d15f154a2f88281863b6816e
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-29 22:47:17 +0900

    refactor: 자동차 입력 예외 발생 시 메시지 출력하도록 변경

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -6,10 +6,13 @@ import java.util.Set;
 
 public class CarNameValidator {
 
+    private static final String TOO_LONG_NAME = "이름은 5자 이하만 가능합니다.";
+    private static final String DUPLICATE_NAME = "중복된 이름이 있습니다.";
+
     public static void checkLength(List<String> carNameList){
         for (String name : carNameList){
             if (name.length() >= 6){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(TOO_LONG_NAME);
             }
         }
     }
@@ -18,7 +21,7 @@ public class CarNameValidator {
         Set<String> carNameSet = new HashSet<>(carNameList);
 
         if (carNameList.size() != carNameSet.size()){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_NAME);
         }
     }
 }
