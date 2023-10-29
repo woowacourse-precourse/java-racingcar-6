b66e4747ada76e117aa9dc22874661ff08d0f710
freekyr7529 <freekyr7529@gmail.com>
2023-10-29 11:33:46 +0900

    feat: 차수에 대한 입력값 검증 기능 구현
    - 차수에 대해 값을 입력하는 과정에서 발생할 수 있는 예외 상황을 검증하는 기능을 구현하였다. 목록은 아래와 같다.
    
    - 숫자 이외에 다른 값이 들어왔는지 검증하는 기능
    - 사용자가 값을 입력하지 않았는지를 검증하는 기능

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Objects;
 
 public class InputHandler {
 
@@ -9,6 +10,18 @@ public class InputHandler {
     }
 
     public int inputRound() {
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        validateRound(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateRound(String input) {
+        if (Objects.equals(input, "")) {
+            throw new IllegalArgumentException("어떠한 것도 입력되지 않았습니다! 값을 입력해주세요!");
+        }
+
+        if (!input.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("입력에 숫자가 아닌 다른 문자가 포함되어 있습니다!");
+        }
     }
 }
