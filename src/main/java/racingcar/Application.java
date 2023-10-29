0ae2ddefbb54e254668c1a32221106256c087548
Joyce <by_eunae@naver.com>
2023-10-29 10:41:38 +0900

    feat : 레이싱카 이름 입력 기능 추가
    - 레이싱카 이름 입력 받아오기
    - 쉼표를 기준으로 이름 나누기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        readNames();
     }
+
+    public static String[] readNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] names = input.split(",");
+
+        //TODO: 검증 로직 추가
+
+        return names;
+    }
+
+
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,11 +19,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -30,6 +34,33 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("readNames 함수에서 입력값을 쉼표로 분리해서 출력")
+    void readNames함수_쉼표로_분리() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("pobi,woni,june");
+            //when
+            String[] result = Application.readNames();
+            //then
+            assertThat(result).isEqualTo(new String[]{"pobi", "woni", "june"});
+        }
+    }
+
+    @Test
+    @DisplayName("readNames 함수에서 입력 값에 쉼표가 없을 때 그대로 출력")
+    void readNames함수_쉼표가_없을_때_그대로_출력() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("pobi");
+            //when
+            String[] result = Application.readNames();
+            //then
+            assertThat(result).isEqualTo(new String[]{"pobi"});
+        }
+    }
+
 
     @Override
     public void runMain() {
