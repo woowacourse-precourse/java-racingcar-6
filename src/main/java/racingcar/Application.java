f33544bb4c7f2986c0fbe4a0a27dec09b6df34d9
kwonkeonhyeong <rjsgud19566@gmail.com>
2023-10-30 03:08:13 +0900

    feat : 경주에 참여할 자동차 이름 입력기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import javax.xml.catalog.Catalog;
+import racingcar.controller.GameController;
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView ss = new InputView();
+        System.out.println(ss.inputCarName());
     }
 }
---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+public class Car {
+    public Car() {
+
+    }
+
+
+}
