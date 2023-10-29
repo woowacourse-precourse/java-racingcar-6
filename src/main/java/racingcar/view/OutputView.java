1a88c202b20a26896fe9983b1f2826060e386499
Jeonwoojae <dnwo0719@icloud.com>
2023-10-29 22:09:25 +0900

    feat: 우승자 출력 기능 구현
    - raceService에서 우승자 리스트를 받아 구분자 추가 후 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,26 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
+import racingcar.service.RaceService;
 
 public class OutputView {
+    private RaceService raceService = new RaceService();
+
     public void displayPerResults(List<Car> cars) {
-        for(Car car : cars) {
+        for (Car car : cars) {
             System.out.print(car.getName() + " : ");
-            for(int i = 0; i < car.getLocation(); i++) {
+            for (int i = 0; i < car.getLocation(); i++) {
                 System.out.print("-");
             }
             System.out.println();
         }
+    }
+
+    public void displayResults(List<Car> cars) {
+        List<String> winnersNames = raceService.getWinner(cars).stream().map(Car::getName).collect(Collectors.toList());
+        String winnerNamesMessage = String.join(", ", winnersNames);
+        System.out.print("최종 우승자 : " + winnerNamesMessage);
     }
 }
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import racingcar.Application;
 import racingcar.model.Car;
 
@@ -13,7 +14,31 @@ public class OutputViewTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
 
     @Test
-    public void 주행결과_출력_테스트() {
+    void 우승결과_출력_두명() {
+        List<Car> cars = List.of(new Car("pobi"), new Car("woni"));
+        for(Car car : cars) {
+            car.goOrStop(MOVING_FORWARD);
+            car.goOrStop(MOVING_FORWARD);
+        }
+
+        outputView.displayResults(cars);
+        assertThat(output()).contains("최종 우승자 : pobi, woni");
+    }
+
+    @Test
+    void 우승결과_출력_한명() {
+        List<Car> cars = List.of(new Car("pobi"), new Car("woni"));
+        Car pobi = cars.get(0);
+        pobi.goOrStop(MOVING_FORWARD);
+        pobi.goOrStop(MOVING_FORWARD);
+
+        outputView.displayResults(cars);
+        assertThat(output()).contains("최종 우승자 : pobi");
+    }
+
+
+    @Test
+    void 주행결과_출력_테스트() {
         List<Car> cars = List.of(new Car("pobi"), new Car("woni"));
         for(Car car : cars) {
             car.goOrStop(MOVING_FORWARD);
