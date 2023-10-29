77ddf62979adfa5812d151d2665421777a6c0c4a
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-29 18:02:23 +0900

    CarRaceGame들의 상수를 관리하는 기능

---
+++ src/main/java/racingcar/constant/CarRaceConstant.java
@@ -0,0 +1,13 @@
+package racingcar.constant;
+
+public enum CarRaceConstant {
+    MIN_LENGTH(0),
+    MAX_LENGTH(9);
+
+    final int constant;
+
+    CarRaceConstant(int constant) {
+        this.constant = constant;
+    }
+
+}
