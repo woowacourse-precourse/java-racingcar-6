ea1a5038f09349c33a5eb50ec5e61ceaba947779
june-777 <wlwhswnsrl96@gmail.com>
2023-10-29 16:33:24 +0900

    feat(GameController): 자동차들을 경주 준비하는 기능

---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.view.Input;
+import racingcar.view.View;
+
+public class GameController {
+    private final View view;
+    private final Input input;
+
+    public GameController(View view, Input input) {
+        this.view = view;
+        this.input = input;
+    }
+
+    private RacingCars readyToCarRace() {
+        view.printGameStartMessage();
+        String carNamesInput = input.inputCarNames();
+        Validator.validate(carNamesInput);
+        List<String> carNames = GameUtil.splitByCommas(carNamesInput);
+        return new RacingCars(carNames);
+    }
+}
