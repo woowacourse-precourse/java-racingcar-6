4fd388a32e464b32553ddd1883e19ec4c7e6dfac
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-10-30 01:19:46 +0900

    feat: running program

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,25 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.controller.Game;
+import racingcar.service.CarMove;
 import racingcar.service.CarName;
+import racingcar.service.RandomNumber;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        CarName car = new CarName();
 
 
+        CarName carName = new CarName();
+        RandomNumber randomNumber = new RandomNumber();
+        CarMove carMove = new CarMove(carName, randomNumber);
+        Game game = new Game(carMove, carName, randomNumber);
 
+        game.start();
 
     }
+
 }
+
+
