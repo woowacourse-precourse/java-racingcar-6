085ee96ef3672f75c66d7d533d036808ce5cc0cb
YuJeongYun <yunyj99@naver.com>
2023-10-30 01:00:42 +0900

    refactor: 출력 뷰에서 리스트를 받아 출력하도록 수정
    레이스 결과를 리스트로 받아오므로, 리스트를 매개변수로 받아 결과를 출력하도록 메서드 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import racingcar.constant.GameMessage;
 import racingcar.domain.Car;
 
+import java.util.List;
+
 public class OutputView {
     public void printResultMsg() {
         System.out.println(GameMessage.GAME_RESULT_MESSAGE);
@@ -12,7 +14,13 @@ public class OutputView {
         System.out.printf(GameMessage.GAME_WINNER_MESSAGE, winner.getName());
     }
 
-    public void printEachRaceResult(Car car) {
+    public void printEachRaceResult(List<Car> raceResult) {
+        for(Car car : raceResult) {
+            printEachRaceCarResult(car);
+        }
+    }
+
+    public void printEachRaceCarResult(Car car) {
         System.out.printf(GameMessage.EACH_MOVE_RESULT_MESSAGE, car.getName(),
                 GameMessage.DASH.repeat(car.getDist()));
     }
