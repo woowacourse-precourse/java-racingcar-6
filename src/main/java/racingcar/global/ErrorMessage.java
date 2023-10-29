139ed8b7ffc4ab4daf7d2a3ae55335ba091471bd
Gyeongho Yang <chchch1213@naver.com>
2023-10-29 19:15:15 +0900

    feat(global): 시도할 횟수가 숫자가 아닐 때의 에러 메세지 상수 추가

--- src/main/java/racingcar/global/ErrorMessage.java
+++ src/main/java/racingcar/global/ErrorMessage.java
@@ -5,7 +5,8 @@ import static racingcar.global.Constant.MAX_CAR_NAME_LENGTH;
 public enum ErrorMessage {
     EXCEED_CAR_NAME_LENGTH(String.format("%d자리 이상인 자동차 이름이 존재 합니다.", MAX_CAR_NAME_LENGTH)),
     DUPLICATED_CAR_NAME("중복된 자동차 이름이 존재 합니다."),
-    EMPTY_CAR_NAME("공백인 자동차 이름이 존재 합니다.");
+    EMPTY_CAR_NAME("공백인 자동차 이름이 존재 합니다."),
+    NOT_NUMERIC_PLAY_COUNT_STRING("시도할 횟수가 숫자가 아닙니다.");
 
     private final String message;
 
