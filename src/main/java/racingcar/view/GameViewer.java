b1d089692e314b49d266a655e20513a65c05987a
stopmin <geemin0@naver.com>
2023-10-30 02:06:37 +0900

    feat: 결과를 보여주는 Viewer생성

---
+++ src/main/java/racingcar/view/GameViewer.java
@@ -0,0 +1,38 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class GameViewer {
+    private List<Car> cars;
+
+    public GameViewer(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void displayGameResult() {
+
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + getCarPositionString(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private String getCarPositionString(int position) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    public void displayGameWinner(String winner) {
+        if (winner.isEmpty()) {
+            // 에러 처리
+        } else {
+            System.out.println("최종 우승자: " + winner);
+        }
+    }
+
+}
