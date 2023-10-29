d30ebe39003ff025226c37fe43fd24c307657b12
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-29 22:28:11 +0900

    feat(car): Car 클래스에 equals(), hashCode() 재정의

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Car {
     private final String name;
     private Integer position;
@@ -19,5 +21,22 @@ public class Car {
 
     public Integer getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
