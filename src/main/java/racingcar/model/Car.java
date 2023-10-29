0215eb766635258796e3d45b6b34e0023c50805a
WooYoungDoo <duden5000@naver.com>
2023-10-29 18:02:24 +0900

    feat (Car) : 자동차 겍체 랜덤값 및 getter,setter 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,9 +4,32 @@ public class Car {
 
     private String name;
     private int distance;
+    private int randomNumber;
 
-    public Car(String name) {
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void initCar(String name) {
         this.name = name;
         this.distance = 0;
+        this.randomNumber = 0;
     }
+
+    public void setCarRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public void addOneDistance() {
+        this.distance++;
+    }
+
 }
