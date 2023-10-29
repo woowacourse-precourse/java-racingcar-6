ea799031b723383b2dc5cf1f7031229ed840f2e3
seongha <11pi885@gmail.com>
2023-10-29 15:22:57 +0900

    feat : racingCar 클래스 필드, 생성자, getter 작성

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public class RacingCar {
+
+    private String name;
+
+    private int count;
+
+    public RacingCar(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+}
