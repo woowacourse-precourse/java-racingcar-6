ea2fbb2fa60252c9e8c769be27071b2a7375ef84
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 19:57:44 +0900

    feat(Car): 자동차 이름 길이 상수화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
     private String name;
     private int movedCount;
 
@@ -11,7 +12,7 @@ public class Car {
     }
 
     private void validateName(String name) {
-        if (name.length() > 5) {
+        if (name.length() > CAR_NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
