259a387aa88299363a9ab12482831b63d3dee2d7
박성춘 <songchon100@gmail.com>
2023-10-29 21:52:25 +0900

    feat : 예외처리를 위한 enum 작성

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -1,4 +1,20 @@
 package racingcar.util;
 
 public enum ErrorMessage {
+    OVER_LENGTH_ERROR("차 이름은 5자 이하로 해주세요."),
+    NOT_COMMA_ERROR("이름은 쉽표(,)로 구분해주세요."),
+    TRY_NUM_NOT_NUMBER_ERROR("이동 횟수는 숫자를 입력해주세요"),
+    EMPTY_INPUT_ERROR("공백을 입력하지 마세요."),
+    CAR_NUM_ERROR("차는 두대 이상 입력해주세요.");
+
+    private String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
 }
