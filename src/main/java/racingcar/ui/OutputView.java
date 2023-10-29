993d582523f22be6697607f5784c17c9201609fc
gumgu <bienlee@naver.com>
2023-10-29 15:26:15 +0900

    Style : 출력문 오타 수정

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -2,7 +2,7 @@ package racingcar.ui;
 
 public class OutputView {
 
-    private final static String GET_NAMES= "겅주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String GET_NAMES= "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String GET_GAME_COUNT = "시도할 횟수는 몇회인가요?";
     private final static String RESULT_COMMENT = "실행 결과";
     private final static String SEPARATOR = " : ";
