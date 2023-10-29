1a9f6adbda52eb6efb41167e76787c7fa55dee79
kylim99 <zxzx05050@naver.com>
2023-10-29 12:49:09 +0900

    test: 우승자가 여러명일 경우 테스트, 횟수 입력시 숫자가 아닌경우 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.utill.ExceptionMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,6 +30,26 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 우승자가_여러명일_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("car1,car2,car3", "1");
+                    Assertions.assertThat(output()).contains("car1 : ", "car2 : ","car3 : ", "최종 우승자 : car1, car2, car3");
+                },
+                1,2
+        );
+    }
+
+    @Test
+    void 횟수_입력이_숫자가_아닌_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("car1,car2,car3", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ExceptionMessage.INVALID_NOT_NUMERIC.getMessage())
         );
     }
 
