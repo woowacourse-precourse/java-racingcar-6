5996d946cafbba2aebe4d96ffa43b7b302746fbd
sami355-24 <ddung355@gmail.com>
2023-10-29 20:00:17 +0900

    fix(Exception): 사용자 정의 예외가 상속하는 예외를 작은 범위의 예외로 변경
    - 좁은 범위를 가진 예외로 변경하여 예외의 범위를 축소
    - CarNameException이 상속하는 예외를 RunTimeException에서 IllegalArgumentException으로 수정
    - RoundException이 상속하는 예외를 RunTimeException에서 IllegalArgumentException으로 수정

--- src/main/java/racingcar/Exception/GameException.java
+++ src/main/java/racingcar/Exception/GameException.java
@@ -2,13 +2,13 @@ package racingcar.Exception;
 
 public class GameException extends RuntimeException {
 
-    public static class CarNameException extends RuntimeException {
+    public static class CarNameException extends IllegalArgumentException {
         public CarNameException(String message) {
             super(message);
         }
     }
 
-    public static class RoundException extends RuntimeException {
+    public static class RoundException extends IllegalArgumentException {
         public RoundException(String message) {
             super(message);
         }
