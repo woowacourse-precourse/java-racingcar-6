33c48d13e23dd3b5bba53055aa81aa74ff82acea
KiminSon <kimin0331@naver.com>
2023-10-29 16:59:00 +0900

    feat(RacingGame): 자동차 이동 구현
    moveCar함수에서 0에서 9사이의 무작위 값을 통해 자동차 이동 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,7 +1,30 @@
 package racingcar;
 
-public class RacingGame {
-    public static void playGame(String[] carList, int playCount) {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class RacingGame {
+
+    private static String[] carList;
+    private static int[] gameResultCount;
+    private static int carListLength;
+    private static int winCount;
+
+    public static void moveCar() {
+        for (int i = 0; i < carListLength; i++) {
+            if (Randoms.pickNumberInRange(0, 9) > 3) {
+                gameResultCount[i]++;
+            }
+        }
+    }
+
+    public static void playGame(String[] inputCarList, int playCount) {
+        carList = inputCarList;
+        carListLength = carList.length;
+        gameResultCount = new int[carListLength];
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < playCount; i++) {
+            moveCar();
+        }
     }
 }
