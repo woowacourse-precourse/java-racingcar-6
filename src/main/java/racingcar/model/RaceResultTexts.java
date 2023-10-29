e7d24fdd1d8a3f46ba6fec8bfc00cab0ff086ee0
ikjo39 <chi4321@naver.com>
2023-10-30 01:22:26 +0900

    feat: 경주 결과 문구 리스트를 가질 레코드 추가
    - 각 자동차별 경주 결과 리스트를 하나로 묶는 메서드 추가
    - 이동 횟수만큼 모든 자동차의 경주 결과를 하나로 묶는 메서드 추가

---
+++ src/main/java/racingcar/model/RaceResultTexts.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+public record RaceResultTexts(List<String> results) {
+    public String convertOneLine() {
+        return String.join("", results);
+    }
+
+    public String convertTotalOneLine() {
+        return String.join("\n", results);
+    }
+}
