bb71a1a3b409acd257a1539e28e732ef775a60d3
hayannn <dlgkdis801@naver.com>
2023-10-29 19:13:34 +0900

    feat(main): 컨트롤러로부터 자동차 경주 게임이 시작되도록 Application 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.Controller.RacingcargameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingcargameController racingcargameController = new RacingcargameController();
+        racingcargameController.startRacingCarGame();
     }
 }
