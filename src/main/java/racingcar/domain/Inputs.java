5b3e233e46e52aa4909e5399b23e54de355dcb7a
mmalfoy <stpmalfoy@gmail.com>
2023-10-29 22:41:18 +0900

    Feat : 입력Class 경주할 자동차 이름 저장 멤버 추기

--- src/main/java/racingcar/domain/Inputs.java
+++ src/main/java/racingcar/domain/Inputs.java
@@ -1,7 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Inputs {
+    private static List<String> cars = new ArrayList<>();
+
+
+    public static List<String> getCars(){
+        return cars;
+    }
+
+
+
 
 
 }
