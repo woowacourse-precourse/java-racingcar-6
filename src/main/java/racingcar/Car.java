96edc69f0715b22be9eed39af8b23b9b3dc048e6
pyounani <yndbsk9372@gmail.com>
2023-10-29 22:24:54 +0900

    feat: Car 클래스 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class Car {
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
