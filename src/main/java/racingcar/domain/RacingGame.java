f7c5f6a3b9b6077cf95b94aee603fb834ce4ba48
JongukYang <didwhddnr123@naver.com>
2023-10-29 18:32:12 +0900

    feat: 실행 결과 출력

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
 import racingcar.utils.RandomNumberGenerator;
+import racingcar.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
@@ -9,17 +11,22 @@ public class RacingGame {
     private final List<RacingCar> racingCars;
     private final int tryCount;
     private final RandomNumberGenerator randomNumberGenerator;
+    private final OutputView outputView;
 
     public RacingGame(List<RacingCar> racingCars, int tryCount) {
         this.racingCars = racingCars;
         this.tryCount = tryCount;
         this.randomNumberGenerator = new RandomNumberGenerator();
+        this.outputView = new OutputView();
     }
 
     public void start() {
+        outputView.printResult();
         for (int i = 0; i < tryCount; i++) {
             moveRacingCars();
+            outputView.printAllRacingCarsPosition(racingCars);
         }
+        outputView.printWinners(findWinners());
     }
 
     private void moveRacingCars() {
@@ -27,5 +34,31 @@ public class RacingGame {
             int randomNumber = randomNumberGenerator.generateRandomNumber();
             car.move(randomNumber);
         }
+    }
+
+    private List<RacingCar> findWinners() {
+        int maxPosition = findMaxPosition();
+
+        List<RacingCar> winners = new ArrayList<>();
+
+        for (RacingCar racingCar : racingCars) {
+            if (racingCar.getPosition() == maxPosition) {
+                winners.add(racingCar);
+            }
+        }
+
+        return winners;
+    }
+
+    private int findMaxPosition() {
+        int max = 0;
+
+        for (RacingCar racingCar : racingCars) {
+            if (racingCar.getPosition() > max) {
+                max = racingCar.getPosition();
+            }
+        }
+
+        return max;
     }
 }
