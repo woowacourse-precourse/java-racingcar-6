e7c7d4a22505eb87258f9af1c5134ffbcd10d0e4
leemiyeon <lemingul7@gmail.com>
2023-10-30 04:40:56 +0900

    style: separate methods 1. RacingView

--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -1,10 +1,10 @@
 package racingcar.view;
 
-import static racingcar.utils.ConvertListStringToString.convertListStringToString;
+import static racingcar.domain.RacingConfig.NAME_SEPARATOR;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-import racingcar.domain.RacingRoundResult;
+import racingcar.domain.SingleRoundResult;
 import racingcar.view.enums.RacingMessage;
 
 public class RacingView {
@@ -15,16 +15,18 @@ public class RacingView {
     public void displayRacingMessage(RacingMessage racingMassage) {
         System.out.println(racingMassage.getMessage());
     }
-    public void displayResults(List<RacingRoundResult> results) {
-        results.stream()
-                .map(RacingRoundResult::getCarMoveHistories)
-                .forEach(histories -> {
-                    histories.forEach(System.out::println);
-                    System.out.println();
-                });
+
+    public void displayResults(List<SingleRoundResult> results) {
+        results.forEach(this::displaySingleRoundResult);
     }
+    private void displaySingleRoundResult(SingleRoundResult singleRoundResult) {
+        displayRacingMessage(RacingMessage.RACING_RESULT);
+        singleRoundResult.getCarMoveHistories().forEach(System.out::println);
+        System.out.println();
+    }
+
     public void displayWinnerMessage(List<String> results) {
-        String formattingName = String.join(", ", results);
+        String formattingName = String.join(NAME_SEPARATOR , results);
         String result = String.format(RacingMessage.RACING_WINNER.getMessage(), formattingName);
         System.out.println(result);
     }
