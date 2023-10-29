1fe5b990d397396c7bebdc453df56ca69d0d59da
daneng4 <daneng4@naver.com>
2023-10-29 11:46:33 +0900

    Feat: 자동차 이름 입력 받는 기능 추가

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -1,5 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+
 public class Player {
 
+    private HashMap<String,Integer> cars;
+    private String[] inputCarNames;
+
+    public void setCarName(){
+        inputCarNames = Console.readLine().split(",");
+        for(int i = 0; i < inputCarNames.length; i++){
+            cars.put(inputCarNames[i], 0);
+        }
+    }
+
+    public HashMap<String, Integer> getCarName(){
+        return cars;
+    }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,9 +3,13 @@ package racingcar;
 public class RacingGame {
 
     private static final String REQUIRE_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUIRE_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private Player player = new Player();
 
     public void GameStart(){
         printMessage(REQUIRE_CAR_NAME_MESSAGE);
+        player.setCarName();
+        printMessage(REQUIRE_ATTEMPT_NUMBER_MESSAGE);
     }
 
     public void printMessage(String message){
