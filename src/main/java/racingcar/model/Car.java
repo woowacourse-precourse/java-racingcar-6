4073ed1daa698f20a22102526089d79789919046
unknown <khd128k@gmail.com>
2023-10-29 21:35:12 +0900

    feat: model/Car

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,19 @@
 package racingcar.model;
 
 public class Car {
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPosition() {
+        return position;
+    }
+    public void move() {
+        this.position++;
+    }
 }
