18b9c64501f507ce5e020920db365f90c15845ee
Dawin <dawin002@naver.com>
2023-10-29 22:17:44 +0900

    refactor(InputView): 이동 회수 입력 메서드 이름 변경
    이동 회수를 입력하는 메서드 이름을 변경:
    readRound() -> readRoundNumber()

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
-    public static String readRound() {
+    public static String readRoundNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
