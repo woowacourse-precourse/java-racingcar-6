9ec2fa1f3e71612caba9ba8d10eae4ff884a6437
lee jihyeon <lejinhy@gmail.com>
2023-10-29 11:22:37 +0900

    refactor: 한번에 예외처리 하는 메소드 삭제, private 메소드 public으로 수정

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -1,30 +1,19 @@
 package racingcar;
 
 public class Exception {
-
-    public void isCarNameValid(String name) {
-        isNameNull(name);
-        isNameInRange(name);
-    }
-
-    public void isTryCountValid(String count) {
-        isInteger(count);
-        isGreaterThanZero(count);
-    }
-
-    private void isNameNull(String name) {
+    public void isNameNull(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름을 입력해 주세요.");
         }
     }
 
-    private void isNameInRange(String name) {
+    public void isNameInRange(String name) {
         if (name.length() > MagicNumber.CAR_NAME_LENGTH_MAX.getValue()) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하로 작성해 주세요.");
         }
     }
 
-    private void isInteger(String count) {
+    public void isInteger(String count) {
         try {
             Integer.parseInt(count);
         } catch (NumberFormatException e) {
@@ -34,8 +23,8 @@ public class Exception {
         }
     }
 
-    private void isGreaterThanZero(String count) {
-        if (count.compareTo("0") == 1) {
+    public void isGreaterThanZero(int count) {
+        if (count < 1) {
             throw new IllegalArgumentException("[ERROR] 1 이상의 정수를 입력해 주세요.");
         }
     }
