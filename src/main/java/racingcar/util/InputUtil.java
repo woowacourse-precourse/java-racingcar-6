841279cc0d7cf03a5bb1a6284673ac9090a638cb
inpink <inpink@kakao.com>
2023-10-29 17:25:13 +0900

    feat: 입력받는 기능 InputUtil class에 구현

---
+++ src/main/java/racingcar/util/InputUtil.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+
+    public static String inputString() {
+        return Console.readLine();
+    }
+}
