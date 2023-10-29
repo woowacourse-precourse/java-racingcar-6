633ba5acb51ce4979f9f7ab11ddd1686ef320799
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-30 02:02:31 +0900

    test : RaceViewTest > 최종 우승자 결과 뷰 검증

---
+++ src/test/java/racingcar/domain/race/RaceViewTest.java
@@ -0,0 +1,47 @@
+package racingcar.domain.race;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+
+class RaceViewTest {
+
+    @Test
+    @DisplayName("한 번의 이동 이후, 자동차들의 현재 상태를 출력합니다.")
+    void printCarsStatus() {
+        // GIVEN
+        List<Car> cars = new ArrayList<>();
+        cars.add(Car.createCar("pobi", 2));
+        cars.add(Car.createCar("woni", 4));
+        cars.add(Car.createCar("jun", 3));
+        String expectedStatus = "pobi : --\n"
+                + "woni : ----\n"
+                + "jun : ---\n";
+
+        // WHEN
+        String actualStatus = RaceView.printCarsStatus(cars);
+
+        // THEN
+        assertEquals(expectedStatus, actualStatus);
+    }
+
+    @Test
+    @DisplayName("우승자에 대한 결과를 츨력합니다.")
+    void printWinners() {
+        // GIVEN
+        List<Car> cars = new ArrayList<>();
+        cars.add(Car.createCar("pobi", 4));
+        cars.add(Car.createCar("woni", 4));
+        String expectedResult = "최종 우승자 : pobi, woni";
+
+        // WHEN
+        String actualResult = RaceView.printWinners(cars);
+
+        // THEN
+        assertEquals(expectedResult, actualResult);
+    }
+}
