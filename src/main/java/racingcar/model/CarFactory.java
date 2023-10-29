189f3d7be5710593509869829c97247ea76e4b8f
edgar6bf <edgar6bf@gmail.com>
2023-10-29 19:31:58 +0900

    refactor: CarFactory의 Car 인스턴스 생성 로직 수정
    Car 클래스에 새로운 필드가 추가되면서 생성자 파라미터가 변경되었기 때문에 CarFactory의 Car 인스턴스 생성 로직을 이에 맞춰 수정함.

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -1,12 +1,18 @@
 package racingcar.model;
 
+import racingcar.util.ProductionRandomNumberUtil;
+import racingcar.util.RandomNumberUtil;
+
 import java.util.List;
 
 public class CarFactory {
 
+    private static final int INITIAL_CAR_POSITION = 0;
+    private static final RandomNumberUtil randomNumberUtil = new ProductionRandomNumberUtil();
+
     public static List<Car> initCars(List<String> carNames) {
         return carNames.stream()
-                .map(Car::new)
+                .map(carName -> new Car(randomNumberUtil, carName, INITIAL_CAR_POSITION))
                 .toList();
     }
 }
