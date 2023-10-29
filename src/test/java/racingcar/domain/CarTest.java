17ab4a2767d06dfe32ea1f95f8d5f61f3175cf8c
kkw2758 <dtc3229@gmail.com>
2023-10-29 13:06:32 +0900

    test : 자동차 이름 검증 로직 테스트 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,25 +1,19 @@
 package racingcar.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     @Test
-    void 자동차의_이름은_5자_이하만_가능하다() {
+    void 자동차의_이름은_1자_이상_5자_이하만_가능하다() {
         assertThatThrownBy(() -> new Car("ABCDEF"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름은 이름은 5자 이하만 가능합니다.");
-    }
+                .hasMessageContaining("자동차의 이름은 이름은 1자 이상 5자 이하만 가능합니다.");
 
-    @Test
-    void 자동차의_이름은_공백이_될_수_없습니다() {
         assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름은 공백이 될 수 없습니다.");
+                .hasMessageContaining("자동차의 이름은 이름은 1자 이상 5자 이하만 가능합니다.");
     }
-
 }
