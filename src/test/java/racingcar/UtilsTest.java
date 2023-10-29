2e3308fa15faf4083917f923465e5562a1e3112b
Seungjida <esg0107@pusan.ac.kr>
2023-10-30 02:08:22 +0900

    fix: int를 처리하기 위해 @ValueSource 대신 @CsvSource을 사용하도록 변경

--- src/test/java/racingcar/UtilsTest.java
+++ src/test/java/racingcar/UtilsTest.java
@@ -5,30 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class UtilsTest {
-    @Test
     @ParameterizedTest
-    @ValueSource(strings = {"4", "5", "6", "7", "8", "9"})
-    void isMoveTest_True(String input) {
+    @CsvSource({"4", "5", "6", "7", "8", "9"})
+    void isMoveTest_True(int input) {
         // given
-        int randomMovingNumber = Integer.valueOf(input);
+
         // when
 
         // then
-        assertTrue(Utils.isMove(randomMovingNumber));
+        assertTrue(Utils.isMove(input));
     }
 
-    @Test
     @ParameterizedTest
-    @ValueSource(strings = {"0", "1", "2", "3"})
-    void isMoveTest_False(String input) {
+    @CsvSource({"0", "1", "2", "3"})
+    void isMoveTest_False(int input) {
         // given
-        int randomMovingNumber = Integer.valueOf(input);
+
         // when
 
         // then
-        assertFalse(Utils.isMove(randomMovingNumber));
+        assertFalse(Utils.isMove(input));
     }
 }
