7a4eb02cd7adf0ee46109233ca2d73d7efa7c934
SeongIl <biuld1234@gmail.com>
2023-10-29 21:49:51 +0900

    refactor: remove 불필요한 주석

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,7 +11,7 @@ import static racingcar.Computer.printWinner;
 public class Application {
 
     public static void main(String[] args) {
-        // 1. 자동차 이름 입력
+
         Car car = new Car(createCarNames(getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")));
 
         car.move(createRoundCount(getInput("시도할 회수는 몇회인가요?")));
