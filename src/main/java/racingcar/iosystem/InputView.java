48210c4ecb97b5425d6ecbddce197dfd2611b736
EHCTH <lees90896@gmail.com>
2023-10-29 17:15:16 +0900

    InputView

--- src/main/java/racingcar/iosystem/InputView.java
+++ src/main/java/racingcar/iosystem/InputView.java
@@ -2,25 +2,30 @@ package racingcar.iosystem;
 
 import racingcar.errorCheck.ErrorCheck;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Scanner;
 
 public class InputView {
-    static Scanner sc = new Scanner(System.in);
     private String[] cars;
     private int[] carsMove;
     private int tryCnt;
 
-    public InputView(String[] cars) {
-        ErrorCheck.isCarNameFive(cars);
-        ErrorCheck.isDuplicationNameCar(cars);
-        this.cars = cars;
-        this.carsMove = new int[cars.length];
-        this.tryCnt = Integer.valueOf(inputTry());
+    public InputView() {
+        String[] carList = setCarsName(inputCar());
+        int cnt = Integer.valueOf(inputTry());
 
+        ErrorCheck.isCarNameFive(carList);
+        ErrorCheck.isDuplicationNameCar(carList);
+
+        ErrorCheck.isZeroValues(cnt);
+
+        this.cars = carList;
+        this.carsMove = new int[cars.length];
+        this.tryCnt = cnt;
     }
+
     public String[] getCars() {
         return cars;
     }
+
     public int[] getCarMove() {
         return carsMove;
     }
@@ -28,22 +33,20 @@ public class InputView {
     public int getTryCnt() {
         return tryCnt;
     }
-    public static String[] setCarsName(String input) {
+
+    public String[] setCarsName(String input) {
         String[] carList = input.split(",");
         return carList;
     }
 
-    public static String inputCar() {
+    public String inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-//        String a = "pobi,woni,jun";
         return Console.readLine();
-//        return a;
     }
 
-    public static String inputTry() {
-        System.out.println("시도할 횟수 입력하세여");
+    public String inputTry() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
-//        return "5";
     }
 
 }
