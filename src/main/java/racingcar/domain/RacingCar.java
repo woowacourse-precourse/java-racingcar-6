9cdb969efb286d7599ed333527e6189f45417624
JongukYang <didwhddnr123@naver.com>
2023-10-29 14:53:39 +0900

    feat: RacingCar 클래스

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class RacingCar {
+    private final String name;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
