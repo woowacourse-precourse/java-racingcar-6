cf868f1319e914def3eaf0616076a5d0e2873956
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-29 16:27:02 +0900

    refactor : 자료형 수정

--- src/main/java/racingcar/controller/Validation.java
+++ src/main/java/racingcar/controller/Validation.java
@@ -3,27 +3,31 @@ package racingcar.controller;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import racingcar.model.Car;
 
 public class Validation {
 
-    public static void validateLength(List<String> cars) {
-        for (String car : cars) {
+    public static void validateLength(Car cars) {
+        List<String> carName = cars.getName();
+        for (String car : carName) {
             if (car.length() > 5) {
                 throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
             }
         }
     }
 
-    public static void validateEmtpy(List<String> cars) {
-        if (cars.isEmpty()) {
+    public static void validateEmtpy(Car cars) {
+        List<String> carName = cars.getName();
+        if (carName.isEmpty()) {
             throw new IllegalArgumentException("자동차의 이름을 입력해주세요.");
         }
     }
 
-    public static void validateDuplicate(List<String> cars) {
-        Set<String> checkForDuplicates = makeLowercase(cars);
+    public static void validateDuplicate(Car cars) {
+        List<String> carName = cars.getName();
+        Set<String> checkForDuplicates = makeLowercase(carName);
 
-        if (checkForDuplicates.size() != cars.size()) {
+        if (checkForDuplicates.size() != carName.size()) {
             throw new IllegalArgumentException("중복된 자동차가 있습니다.");
         }
     }
@@ -38,12 +42,20 @@ public class Validation {
         return lowercaseCars;
     }
 
-    public static void validateCharacters(List<String> cars) {
-        for (String car : cars) {
-            if (!car.matches("[a-zA-Z]")) {
+    public static void validateCharacters(Car cars) {
+        List<String> carName = cars.getName();
+        for (String car : carName) {
+            if (!car.matches("[a-zA-Z]*")) {
                 throw new IllegalArgumentException("자동차의 이름은 알파벳으로만 구성되야 합니다.");
             }
         }
+    }
+
+    public static void validateCarName(Car cars) {
+        validateCharacters(cars);
+        validateLength(cars);
+        validateDuplicate(cars);
+        validateEmtpy(cars);
     }
 
     public static void validateNumber(String inputNumber) {
