1cdb8a1fa9ac1ae520992abee967acf4b3ab730b
minseokKim6823 <alstjr6823@naver.com>
2023-10-29 17:41:30 +0900

    README 순서 변경ㅜㅜ

--- src/main/java/racingcar/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/generator/RandomNumberGenerator.java
@@ -1,2 +1,7 @@
-package racingcar.generator;public class RandomNumberGenerator {
+package racingcar.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    int num= Randoms.pickNumberInRange(0,9);
 }
