b0ef181b26281b97aabc60731d98c2a7a330483e
Ukj0ng <xb253q@gmail.com>
2023-10-29 18:58:02 +0900

    feat(Car): 자동차 객체 구현
    자동차:
    - 이름
    - 이동한 횟수

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,2 +1,23 @@
-package racingcar.model;public class Car {
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int move;
+
+    public Car(String name) {
+        this.name = name;
+        this.move = 0;
+    }
+
+    public void moveForward(int number) {
+        if (number >= 4) move++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMove() {
+        return move;
+    }
 }
