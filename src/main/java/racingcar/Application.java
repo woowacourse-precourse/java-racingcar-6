264dd724e436a7f9062ac3b0cafa601e51f9295c
master <cj855695@gmail.com>
2023-10-29 15:33:18 +0900

    전진 기능 구현 - straightCars(), addStraight()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,14 @@
 package racingcar;
 
 public class Application {
+    static CarsManage carsManage = new CarsManage();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        
+        carsManage.inputCars();
+        carsManage.straightCars();
+        for (Car car: carsManage.cars
+             ) {
+            System.out.println(car.getName() + " "+car.getStraight());
+        }
     }
 }
--- src/main/java/racingcar/CarsManage.java
+++ src/main/java/racingcar/CarsManage.java
@@ -19,4 +19,24 @@ public class CarsManage {
             cars.add(car);
         }
     }
+
+    public void straightCars (){
+        int n = Integer.parseInt(Console.readLine());
+
+        for (int i = 0; i < n; i++) {
+            addStraight();
+            //출력문 들어갈 자리
+        }
+        //전체 결과 출력 자리
+    }
+
+    public void addStraight(){
+        int n;
+        for (Car car:cars) {
+            n = Randoms.pickNumberInRange(0,9);
+            if(n>=4){
+                car.addStraight();
+            }
+        }
+    }
 }
