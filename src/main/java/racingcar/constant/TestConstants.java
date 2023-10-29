57f6b84a96ff61e259a5b5237b8ab8170c00a119
leesh <qwqw8019@naver.com>
2023-10-29 20:39:25 +0900

    feat: 테스트 문자열 추가

--- src/main/java/racingcar/constant/TestConstants.java
+++ src/main/java/racingcar/constant/TestConstants.java
@@ -11,10 +11,12 @@ public class TestConstants {
     public static final List<String> EXPECTED_THREE_CAR_NAMES = List.of(THREE_CAR_NAMES.split(","));
 
 
-    // Invalid Car Name Constants
+    // Invalid Constants for Car Name and Play Number
     public static final String NULL_STRING = null;
     public static final String EMPTY_STRING = "";
     public static final String BLANK_STRING = " ";
+
+    // Invalid Car Name Constants
     public static final String LONGER_THAN_FIVE_STRING = "Pororo";
 
 
@@ -29,4 +31,5 @@ public class TestConstants {
     // Invalid Play Number Constants
     public static final String PLAY_NO_TIME = "0";
     public static final String PLAY_NEGATIVE_TIME = "-1";
+    public static final String NOT_NUMBER_STRING = "a";
 }
