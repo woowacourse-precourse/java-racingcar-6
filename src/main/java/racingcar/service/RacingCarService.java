fa39b57e7a99c1f71af09128b1f2c8c5549b66bd
WooYoungDoo <duden5000@naver.com>
2023-10-29 18:03:35 +0900

    feat (RacingCarService) : 게임 진행 프로세스 및 우승자 선정

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -2,15 +2,23 @@ package racingcar.service;
 
 import racingcar.model.Car;
 import racingcar.model.RacingCarGame;
+import racingcar.util.RandomUtil;
 import racingcar.util.SeparationUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RacingCarService {
 
+    private static final int MIN_VALUE = -1;
+
+    private final RandomUtil randomUtil = new RandomUtil();
     private final SeparationUtil separationUtil = new SeparationUtil();
     private final RacingCarGame racingCarGame = new RacingCarGame();
+
+    private final ValidateService validateService = new ValidateService();
 
     List<Car> cars = new ArrayList<>();
 
@@ -18,9 +26,15 @@ public class RacingCarService {
         List<String> separatedCarNames = separateCarName(carNames);
 
         for (String separatedCarName : separatedCarNames) {
-            Car car = new Car(separatedCarName);
+            validateService.validateInputCarName(separatedCarName);
+            Car car = new Car();
+            car.initCar(separatedCarName);
             cars.add(car);
         }
+    }
+
+    public int getCount() {
+        return racingCarGame.getCount();
     }
 
     public void setCount(String inputNumber) {
@@ -35,4 +49,61 @@ public class RacingCarService {
         return separationUtil.separationCarName(carNames);
     }
 
+    public void gameProcess() {
+        for (Car car : cars) {
+            setRandomNumberToCar(car);
+            compareNumber(car);
+        }
+        roundEnd();
+    }
+
+    public Map<String, Integer> currentSituation() {
+        Map<String, Integer> situations = new HashMap<>();
+
+        for (Car car : cars) {
+            situations.put(car.getName(), car.getDistance());
+        }
+        return situations;
+    }
+
+    public List<String> getCarNames() {
+        List<String> carNames = new ArrayList<>();
+        cars.forEach(car -> carNames.add(car.getName()));
+        return carNames;
+    }
+
+    private void setRandomNumberToCar(Car car) {
+        car.setCarRandomNumber(randomUtil.createRandomNumber());
+    }
+
+    private void compareNumber(Car car) {
+        if (car.getRandomNumber() >= 4) {
+            car.addOneDistance();
+        }
+    }
+
+    private void roundEnd() {
+        racingCarGame.minusOneCount();
+    }
+
+    public List<String> getWinners() {
+        int maxDistance = getMaxDistance();
+        return getWinnerCarsName(maxDistance);
+    }
+
+    private List<String> getWinnerCarsName(int maxDistance) {
+        return cars.stream().filter(car -> car.getDistance() == maxDistance)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int getMaxDistance() {
+        int max = MIN_VALUE;
+
+        for (Car car : cars) {
+            if (max < car.getDistance()) max = car.getDistance();
+        }
+
+        return max;
+    }
 }
