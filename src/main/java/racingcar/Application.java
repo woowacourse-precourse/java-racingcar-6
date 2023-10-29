8e7df030840e3e849b0e595475a2102291781855
D0ri123 <lsh81550@gmail.com>
2023-10-29 17:05:38 +0900

    feat: main 메서드에 분리한 클래스 추가 구현  - 3차 구현
    main 메서드에 OutputController 관련 메서드 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,18 @@
 package racingcar;
 
 import racingcar.controller.GameInputController;
+import racingcar.controller.GameOutputController;
 
 public class Application {
 
     public static void main(String[] args) {
-        GameInputController controller = new GameInputController();
-        controller.settingController();
-        controller.requestNames();
-        controller.requestTryNumber();
+        GameInputController inputController = new GameInputController();
+        inputController.settingController();
+        inputController.requestNames();
+        inputController.requestTryNumber();
 
-//        // 게임을 진행시킨다.
-//        game.running();
+        GameOutputController outputController = new GameOutputController();
+        outputController.readyForGame(inputController.sendGameInfo());
+        outputController.startGame();
     }
 }
