0b0d6ac124a83c8363927b23fa936e4c8bc426e7
brightfanta <brightfanta@gmail.com>
2023-10-29 18:11:09 +0900

    refactor: 메서드에서 한가지 기능만 한도록 메서드 분리 및 return type 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,21 +3,21 @@ package racingcar.domain;
 public class Car {
     private String name;
     private Integer successMoveForwardCount = 0;
-
-
-
     private String moveForwardStatus = "";
 
     public String getMoveForwardStatus() {
         return moveForwardStatus;
     }
 
-    public void setMoveForwardStatus() {
-        String moveForwardCountTemp = "";
-        for (int i = 0; i < this.successMoveForwardCount; i++) {
-            moveForwardCountTemp += "-";
-            this.moveForwardStatus = moveForwardCountTemp;
+    public void setMoveForwardStatus(String moveForwardStatus) {
+        this.moveForwardStatus = moveForwardStatus;
+    }
+    public String makeMoveForwardStatus(Integer successMoveForwardCount) {
+        String moveForwardStatus = "";
+        for (int i = 0; i < successMoveForwardCount; i++) {
+            moveForwardStatus += "-";
         }
+        return moveForwardStatus;
     }
 
     public String getName() {
