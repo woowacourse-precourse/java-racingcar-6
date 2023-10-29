342d4bd1e26f15fe0b76c8ad6d5309518ee0dc4e
twinson37 <twinson37@hanyang.ac.kr>
2023-10-29 19:17:12 +0900

    Refactor: 전진 거리 출력 함수 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.Controller.CarController;
+import racingcar.Controller.GameController;
+import racingcar.Model.GameModel;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        GameController gameController = new GameController(new GameModel(),new CarController());
+
+        gameController.gameStart();
     }
 }
--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -38,6 +38,8 @@ public class GameController {
 
             throwDiceAndGoForward();
         }
+
+        carController.printDistance();
     }
 
     private void throwDiceAndGoForward() {
