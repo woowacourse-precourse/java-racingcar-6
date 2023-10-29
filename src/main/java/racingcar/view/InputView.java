232e2198a3e8f6475dc711c1f55b9dace7330b22
PARKHWANYEONG <welcome0828@naver.com>
2023-10-29 10:31:03 +0900

    refactor(view): InputView 클래스의 출력 메시지 상수로 분리
    - 가독성을 위해 출력 문구 메시지를 따로 상수로 분리하였습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String ENTER_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String ENTER_ATTEMPT_COUNT_MESSAGE  = "시도할 회수는 몇회인가요?";
+
     public static String carNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(ENTER_CAR_NAMES_MESSAGE);
         return Console.readLine();
     }
 
     public static String askForAttemptCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(ENTER_ATTEMPT_COUNT_MESSAGE);
         return Console.readLine();
     }
 }
