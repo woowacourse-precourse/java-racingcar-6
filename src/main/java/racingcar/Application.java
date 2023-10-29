bd28bd69bbc40f75cff0f92408b9c42b9a9f5388
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-10-29 16:52:04 +0900

    feat(Application) : 자동차 이름 입력 기능 구현
    getCarNames() 구현
    - 중복이 안되게 Set 자료형으로 받음
    - 에러 조건
    - , 이 없거나 이름 길이가 맞지 않거나, 중복된 이름이 있거나, 자동차 이름의 갯수가 맞지 않으면 IllegalArgumentException 을 발생하도록 만듬

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,47 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Set<String> carNames = getCarNames();
+
+        System.out.println(carNames);
+
+    }
+
+    static Set<String> getCarNames() {
+        Set<String> carNames = new HashSet<>();
+
+        String inputNames = readLine();
+
+        if (!inputNames.contains(",")) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        String[] splitNames = inputNames.split(",");
+
+        for (String carName : splitNames) {
+            if ( 1 > carName.length() || carName.length() > 5) {
+                throw new IllegalArgumentException("이름 길이는 1 ~ 5자 이내 입니다.");
+            }
+
+            if (carNames.contains(carName)) {
+                throw new IllegalArgumentException("중복된 이름이 있습니다.");
+            }
+
+            carNames.add(carName);
+        }
+
+        if (carNames.size() == 0 || carNames.size() > 5) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        return carNames;
     }
 }
