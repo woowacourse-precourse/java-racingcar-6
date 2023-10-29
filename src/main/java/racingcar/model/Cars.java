667eb0f676786bc81a20542a991b7bd22ee952e2
Isaac Han <isaachan233@gmail.com>
2023-10-29 23:37:58 +0900

    refactor: 클래스 외부에서 형식에 맞춰 값을 만들어서 집어 넣는것 대신 값이 내부적으로 생성

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,18 +1,18 @@
 package racingcar.model;
 
+import racingcar.constants.Constants;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> carList = new ArrayList<>();
 
-    public void addCar(String name) {
-        carList.add(new Car(name));
-    }
-
-    public void addCars(List<String> names) {
-        for (String name : names) {
-            addCar(name);
+    public Cars(String carNames) {
+        String[] carNameArray = carNames.split(Constants.SEPARATOR.getStringValue());
+        for (String carName : carNameArray) {
+            carList.add(new Car(carName));
         }
     }
 
