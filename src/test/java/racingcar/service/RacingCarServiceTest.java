7cb19870433bf7b3bde4d4f18655ba2eeee29898
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 11:38:32 +0900

    test(RacingCarServiceTest): 레이싱 시작 전 조립하는 로직으로 테스트코드 수정

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -11,57 +11,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
 import racingcar.domain.FixedCarEngine;
-import racingcar.domain.Position;
 import racingcar.domain.RacingResult;
 
 public class RacingCarServiceTest {
 
-    static Stream<Arguments> provideRaceTestArguments() {
-        return Stream.of(
-                arguments(new Cars(List.of(
-                        setCar("pobi", true, 0),
-                        setCar("woni", false, 0),
-                        setCar("jun", false, 0)
-                )), 3, List.of("pobi")),
-                arguments(new Cars(List.of(
-                        setCar("pobi", true, 0),
-                        setCar("woni", true, 0),
-                        setCar("jun", false, 0)
-                )), 3, List.of("pobi", "woni")),
-                arguments(new Cars(List.of(
-                        setCar("pobi", true, 0),
-                        setCar("woni", true, 0),
-                        setCar("jun", true, 0)
-                )), 3, List.of("pobi", "woni", "jun")),
-                arguments(new Cars(List.of(
-                        setCar("pobi", false, 0),
-                        setCar("woni", false, 0),
-                        setCar("jun", false, 0)
-                )), 10, List.of("pobi", "woni", "jun"))
-        );
-    }
-
-    private static Car setCar(String carName, boolean engineCanMove, int position) {
-        return new Car(carName, new FixedCarEngine(engineCanMove), new Position(position));
-    }
-
     @DisplayName("RacingCarService 객체를 생성한다.")
     @Test
     void createTest() {
-        assertThatCode(() -> new RacingCarService())
+        assertThatCode(() -> new RacingCarService(new RacingCarAssemblyService(), new FixedCarEngine(true)))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("레이싱을 시작해 최종 우승자를 반환한다.")
     @ParameterizedTest
     @MethodSource("provideRaceTestArguments")
-    void raceTest(Cars cars, int attemptCounts, List<String> expectedWinners) {
-        RacingCarService racingCarService = new RacingCarService();
-        RacingResult racingResult = racingCarService.race(cars, attemptCounts);
+    void raceTest(List<String> carNames, int attemptCounts, List<String> expectedWinners) {
+        RacingCarService racingCarService = new RacingCarService(new RacingCarAssemblyService(),
+                new FixedCarEngine(true));
+        RacingResult racingResult = racingCarService.race(carNames, attemptCounts);
         List<String> finalWinners = racingCarService.determineWinner(racingResult);
         assertThat(finalWinners).isEqualTo(expectedWinners);
+    }
+
+    static Stream<Arguments> provideRaceTestArguments() {
+        return Stream.of(
+                arguments(List.of("pobi", "woni", "jun"), 3, List.of("pobi", "woni", "jun")),
+                arguments(List.of("pobi", "woni", "jun"), 10, List.of("pobi", "woni", "jun"))
+        );
     }
 }
