f02f92df9f7c89c1d90f676452eb6353bfba5af8
Jiyoung Choi <12201817@inha.edu>
2023-10-29 20:52:52 +0900

    refactor(OutputView) : 해당 회차의 실행 결과 출력 기능 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,27 +1,18 @@
 package racingcar.view;
 
 import java.util.List;
-import java.util.Map;
 import java.util.StringJoiner;
 
 public class OutputView {
-    private final static String moveUnit = "-";
     private final static String resultMessage = "\n실행 결과";
-    private final static String printMoveFormat = "%s : %s\n";
     private final static String printFinalWinnersFormat = "최종 우승자 : %s";
 
-    public static void printResult() {
+    public static void printResultMessage() {
         System.out.println(resultMessage);
     }
 
-    public static void printMove(Map<String, Integer> resultMap) {
-        for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
-            String carName = entry.getKey();
-            int moveDistance = entry.getValue();
-            String moveUnits = moveUnit.repeat(moveDistance);
-            System.out.printf(printMoveFormat, carName, moveUnits);
-        }
-        System.out.println();
+    public static void printMove(String resultMap) {
+        System.out.println(resultMap);
     }
 
     public static void printFinalWinners(List<String> finalWinnersList) {
