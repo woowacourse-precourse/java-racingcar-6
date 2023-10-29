922530cbf48ec92720e6229128339d1b57e6d4a1
jaegyeongkim <dongji_11@naver.com>
2023-10-29 16:15:42 +0900

    test(UtilsTest): 이동 횟수 검증 기능 테스트

--- src/test/java/utils/UtilsTest.java
+++ src/test/java/utils/UtilsTest.java
@@ -58,4 +58,18 @@ public class UtilsTest {
 
         assertThat(utils.splitByDelimiter(input)).isEqualTo(expectString);
     }
+    @Test
+    public void 이동횟수가_숫자가_아닌_경우_예외_발생(){
+        String input = "1.6";
+        assertThatThrownBy(() -> utils.convertStringToInt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 0 이상의 정수이어야 합니다.");
+    }
+    @Test
+    public void 이동횟수가_음수인_경우_예외_발생(){
+        int input = -1;
+        assertThatThrownBy(() -> utils.isPositiveNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 음수가 될 수 없습니다.");
+    }
 }
