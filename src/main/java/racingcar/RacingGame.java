f87a716442ed7e301210aa7c591b0be4517f508a
elephantLikesApple <elephant890@naver.com>
2023-10-29 16:59:18 +0900

    feat : 경주 횟수를 입력받는다

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 public class RacingGame {
     List<Car> participant = new ArrayList<>();
+    int times;
     public void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         participant = readParticipantCars(readLine());
 
-        // TODO : 경주 횟수를 입력받는다
+        System.out.println("시도할 회수는 몇회인가요?");
+        times = readRacingTimes(readLine());
+
         // TODO : n개의 자동차가 각자 전진한다
         // TODO : 각 차수별 실행 결과를 출력한다
         // TODO : 우승한 자동차 리스트를 구한다
@@ -29,5 +32,13 @@ public class RacingGame {
         }
 
         return cars;
+    }
+
+    private int readRacingTimes(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요. (입력 : " + input + ")");
+        }
     }
 }
