386df4078e2b05e9640c48f2d9702e0c2ec2b008
Gyeongho Yang <chchch1213@naver.com>
2023-10-29 18:04:47 +0900

    feat(domain): 일급 컬렉션 중복 및 공백 체크 validate 함수 추가

--- src/main/java/racingcar/domain/CarGroup.java
+++ src/main/java/racingcar/domain/CarGroup.java
@@ -9,20 +9,31 @@ import java.util.Map;
 public class CarGroup {
     private Map<String, Integer> cars;
 
-    // General constructor
-    public CarGroup(Map<String, Integer> cars) {
+    private CarGroup(Map<String, Integer> cars) {
         this.cars = cars;
     }
 
-    // Factory method creating object
+    // Creating instance method for factory pattern
     public static CarGroup from(String carNamesCombinedWithComma) {
         Map<String, Integer> cars = new HashMap<>();
         for (String carName : carNamesCombinedWithComma.split(",")) {
-            if (cars.containsKey(carName)) {
-                throw RaceException.of(ErrorMessage.DUPLICATED_CAR_NAME);
-            }
-            cars.put(carName, 0);
+            String trimmedCarName = carName.trim();
+            validateDuplicate(cars, trimmedCarName);
+            validateEmpty(trimmedCarName);
+            cars.put(trimmedCarName, 0);
         }
         return new CarGroup(cars);
+    }
+
+    private static void validateDuplicate(Map<String, Integer> cars, String carName) {
+        if (cars.containsKey(carName)) {
+            throw RaceException.of(ErrorMessage.DUPLICATED_CAR_NAME);
+        }
+    }
+
+    private static void validateEmpty(String carName) {
+        if (carName.isEmpty()) {
+            throw RaceException.of(ErrorMessage.DUPLICATED_CAR_NAME);
+        }
     }
 }
