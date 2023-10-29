869c0b3869270e42286187cd523ab0d89ff8aa88
dlrjs2360 <lhg3670@naver.com>
2023-10-29 22:29:46 +0900

    :sparkles: feat: 시도 횟수 입력 view 추가
    - InputView에 시도 횟수를 입력받는 view 메서드를 생성했습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         System.out.println(Messages.INPUT_CAR_NAMES.getMessage());
         return Console.readLine();
     }
+
+    public static int inputTryCount() {
+        System.out.println(Messages.INPUT_TRY_COUNT.getMessage());
+        return Integer.parseInt(Console.readLine());
+    }
 }
