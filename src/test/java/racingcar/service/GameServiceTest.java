69053f32822b48f881d6261a7bcd46194c12dd3c
Jin Geonwoo <jinkonu99@gmail.com>
2023-10-29 15:12:13 +0900

    refactor: playGameForTimes() 메서드에 대한 테스트 제거

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -39,19 +39,22 @@ class GameServiceTest {
             assertThat(racingCars1.get(i).getName()).isEqualTo(racingCars2.get(i).getName());
     }
 
-    @DisplayName("일정한 횟수만큼 게임 진행")
-    @Test
-    void playGameForTimes_입력값에_맞는_횟수만큼_게임_진행() {
-        // GIVEN, WHEN
-        long maxLocation = GameService.playGameForTimes((long)10, racingCars1);
-        long maxLocationInRacingCars1 = 0;
-
-        for (RacingCar racingCar : racingCars1)
-            maxLocationInRacingCars1 = Math.max(maxLocationInRacingCars1, racingCar.getLocation());
-
-        // THEN
-        assertThat(maxLocation).isEqualTo(maxLocationInRacingCars1);
-    }
+    // 도저히 메서드의 로직이 제대로 돌아가는지 확인할 방법이 없다.
+    // playGameForTimes()는 아무런 데이터도 반환하지 않는다.
+    // 게다가, 0부터 9까지의 수를 임의로 뽑기 때문에, 지속적으로 0이 나올 경우 "자동차가 움직였는가"를 가지고 로직을 테스트할 수도 없는 노릇이다.
+//    @DisplayName("일정한 횟수만큼 게임 진행")
+//    @Test
+//    void playGameForTimes_입력값에_맞는_횟수만큼_게임_진행() {
+//        // GIVEN, WHEN
+//        GameService.playGameForTimes((long)10, racingCars1);
+//        long maxLocationInRacingCars1 = 0;
+//
+//        for (RacingCar racingCar : racingCars1)
+//            maxLocationInRacingCars1 = Math.max(maxLocationInRacingCars1, racingCar.getLocation());
+//
+//        // THEN
+//        assertThat().isEqualTo(maxLocationInRacingCars1);
+//    }
 
     @DisplayName("최종 우승자 반환")
     @Test
@@ -63,7 +66,7 @@ class GameServiceTest {
             racingCars1.get(0).putForward();
 
         // WHEN
-        List<RacingCar> winners = GameService.findWinners(maxLocation, racingCars1);
+        List<RacingCar> winners = GameService.findWinners(racingCars1);
 
         // THEN
         assertThat(winners.size()).isEqualTo(1);
