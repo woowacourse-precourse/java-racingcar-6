bb3de2ed8663957a1120327649d75af43b9822aa
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-10-29 19:49:04 +0900

    feat : 자동차 클래스 선언

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,23 @@
 package racingcar;
 
+class Car {
+    private String name;
+    private Integer move;
+
+    public Car(String name) {
+        this.name = name;
+        this.move = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getMove() {
+        return move;
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
