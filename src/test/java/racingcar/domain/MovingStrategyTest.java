181ce5a96f124764a04dfc8475262415846d0bbb
jongheonleee <qwefghnm1212@gmail.com>
2023-10-29 22:21:52 +0900

    [chore] 테스트 작성 형식 맞춰주기

--- src/test/java/racingcar/domain/MovingStrategyTest.java
+++ src/test/java/racingcar/domain/MovingStrategyTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.util.RandomNumberGenerator;
 
@@ -9,14 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class MovingStrategyTest {
 
     @Test
-    void isCarMove_멈춤_로직_테스트() {
+    @DisplayName("4 보다 작은 숫자인 경우 False 반환")
+    void isCarMove_멈춤_테스트() {
         // 4 보다 작은 숫자인 경우 멈춤
         MovingStrategy movingStrategy = new MovingStrategy(() -> 3);
         assertEquals(false, movingStrategy.isCarMove());
     }
 
     @Test
-    void isCarMove_전진_로직_테스트() {
+    @DisplayName("4 이상의 숫자인 경우 true 반환")
+    void isCarMove_전진_테스트() {
         // 4 이상의 숫자인 경우 전진
         MovingStrategy movingStrategy = new MovingStrategy(() -> 4);
         assertEquals(true, movingStrategy.isCarMove());
