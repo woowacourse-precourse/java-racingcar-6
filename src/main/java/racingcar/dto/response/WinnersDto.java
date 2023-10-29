120d36016c9d6c02002570e014270a5e086e12eb
jongmee <ra123221@gmail.com>
2023-10-30 02:38:50 +0900

    feat: 경주 우승자의 이름을 저장하고 우승자에 대한 출력 메세지를 생성하는 DTO 구현하기
    - 서비스 로직에서 우승 경주차의 리스트를 건네면 이름을 리스트로 저장한다.
    - 내부에 정의된 createMessage 메서드로 출력 메세지를 생성한다.

---
+++ src/main/java/racingcar/dto/response/WinnersDto.java
@@ -0,0 +1,32 @@
+package racingcar.dto.response;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+
+public class WinnersDto {
+    private static final String DELIMITER = ", ";
+
+    private final List<String> names;
+
+    public WinnersDto(final List<Car> cars) {
+        this.names = cars.stream().map(Car::getName).collect(Collectors.toList());
+    }
+
+    public String createMessage() {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for(; i < names.size()-1; i++) {
+            builder.append(names.get(i)).append(DELIMITER);
+        }
+        builder.append(names.get(i));
+        return builder.toString();
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+}
