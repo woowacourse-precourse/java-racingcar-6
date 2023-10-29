18c601075eaa5312019dc9a2ff9a558dca343cb0
minisyu <iswater.kim@gmail.com>
2023-10-29 20:46:03 +0900

    refactor(GameController): 주석 제거

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -19,21 +19,15 @@ public class GameController {
     }
 
     public void run() {
-        // 1. 자동차 이름 입력 받기
         Cars cars = inputView.getCarsFromUserInput();
-        // 2. 총 라운드 수 입력 받기
         int trial = inputView.getTrialFromUserInput();
-        // 3. 총 라운드 수 만큼 반복 == (1게임 - 실행 - 출력) x 총 라운드 수
         WinnersDto winnersDto = playGame(trial, cars);
-        //마지막 CarsDto로 최종 우승자 출력
         outputView.printGameWinner(winnersDto);
     }
 
     private WinnersDto playGame(int trial, Cars cars) {
         for (int i = 1; i <= trial; i++) {
-            // 모든 자동차는 전진 조건에 따라 움직이고, 게임 결과인 CarsDto를 반환
             CarsMovementDto carsMovementDto = cars.moveOnce(moveStrategy);
-            // view의 인자로 CarsDto를 전달하고, 출력
             outputView.printGameResult(carsMovementDto);
         }
         return cars.toWinnerDto();
