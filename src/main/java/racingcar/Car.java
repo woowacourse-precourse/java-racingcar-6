0be448ac8f3410aa5db55a6295dfe7ebe0ea9116
unknown <lsy5881@gmail.com>
2023-10-29 23:12:52 +0900

    Feat: Car에 move, stay, getters 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,7 +4,25 @@ public class Car {
     private String name;
     private int position = 0;
 
-    public Car(String name){
+    public Car(String name) {
         this.name = name;
     }
+
+    private void move() {
+        position += 1;
+    }
+
+    private void stay() {
+        // 아무것도 하지않음
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
+
 }
