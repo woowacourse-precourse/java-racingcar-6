7580a6e23c36ad466ee8feee0a6018e185133ab3
Sunhee Kim <sunni372@icloud.com>
2023-10-29 19:12:04 +0900

    feat: Car 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
