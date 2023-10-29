73525434f4dcfbb49b11a920826c402458368872
Jinseo Jeong <js020315@gmail.com>
2023-10-29 18:32:24 +0900

    [FEAT] Car 객체 생성
    이름, 이동한 거리

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+        move();
+    }
+
+    public void move() {
+        distance++;
+    }
+}
