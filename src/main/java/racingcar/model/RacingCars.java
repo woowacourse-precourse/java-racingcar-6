eed7a1d1e566792a3c4726924d0e94fc752c420f
Jinseo Jeong <js020315@gmail.com>
2023-10-29 21:36:38 +0900

    [REFECTOR] RacingCars
    RacingController에 있던 랜덤 정수 생성, 자동차 움직임 조건 코드 RacingCars로 이동

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,11 +1,33 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
-    List<String> racingCars;
+    List<Car> racingCarsList = new ArrayList<>();
 
-    public RacingCars(List<String> racingCars) {
-        this.racingCars = racingCars;
+    // carNames 리스트의 각 이름에 대해 반복하면서 Car 객체 생성한 후 racingCarsList 리스트에 추가
+    public RacingCars(List<String> carNames) {
+        for (String carName : carNames) {
+            racingCarsList.add(new Car(carName));
+        }
+    }
+
+    public void MoveCars() {
+        for (Car car : racingCarsList) {
+            if (CreateRandomNum() >= 4) {
+                car.Move();
+            }
+        }
+    }
+
+    public int CreateRandomNum() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    public List<Car> GetRacingCars() {
+        return racingCarsList;
     }
 }
