e89d292232bb2dc43f6b222da3e67d09509b3e9f
ohhyeon2 <dhgus1235@gmail.com>
2023-10-29 11:06:10 +0900

    perf: mostMovingForwardLength 함수
    같은 작업을 두 번 하는 것을 수정하기 위해 List에 Map value의 길이를 전부 담아서 따로 가장 큰 값을 추출하는 것이 아닌 가장 큰 값을 구한 후 List에 가장 큰 값을 가진 Map의 Key를 담아서 리턴함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -34,19 +34,23 @@ public class Application {
         System.out.println();
     }
 
-    private static HashMap<String, String> referee(HashMap<String, String> userCarName) {
+    private static void referee(HashMap<String, String> userCarName) {
         for (String key : userCarName.keySet()) {
             String value = userCarName.get(key);
             if (randomNumber() >= 4) {
                 userCarName.put(key, value + "-");
             }
         }
-        return userCarName;
     }
 
-    private static HashMap<String, String> userCarNameSave(String inputCarName) {
+    private static HashMap<String, String> userCarNameSave(final String inputCarName) {
         HashMap<String, String> createUserCar = new HashMap<>();
         String[] userCarNameArr = inputCarName.split(",");
+        for (String userCars : userCarNameArr) {
+            if (userCars.length() > 5) {
+                throw new IllegalArgumentException("자동차의 이름은 5글자를 초과할 수 없습니다.");
+            }
+        }
         for (String car : userCarNameArr) {
             createUserCar.put(car, "");
         }
@@ -61,32 +65,29 @@ public class Application {
 
     private static int inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int tryCount = Integer.parseInt(Console.readLine());
+        final int tryCount = Integer.parseInt(Console.readLine());
         return tryCount;
     }
 
     private static int randomNumber() {
-        int random = Randoms.pickNumberInRange(0, 9);
+        final int random = Randoms.pickNumberInRange(0, 9);
         return random;
     }
 
-    private static int mostMovingForwardLength(HashMap<String,String> carMoveStatus) {
-        List<Integer> forwardLength = new ArrayList<>();
-        for (String carCount : carMoveStatus.keySet()) {
-            forwardLength.add(carMoveStatus.get(carCount).length());
+    private static List<String> mostMovingForwardCar(HashMap<String,String> carMoveStatus) {
+        List<String> winnerList = new ArrayList<>();
+        String mostForward = Collections.max(carMoveStatus.values());
+
+        for (String winner : carMoveStatus.keySet()) {
+            if (mostForward.equals(carMoveStatus.get(winner))) {
+                winnerList.add(winner);
+            }
         }
-        return Collections.max(forwardLength);
+        return winnerList;
     }
 
     private static void winner(HashMap<String,String> carMoveStatus) {
-        int mostForwardLength = mostMovingForwardLength(carMoveStatus);
-
-        String winner = "";
-        for (String userCarName : carMoveStatus.keySet()) {
-            if (carMoveStatus.get(userCarName).length() == mostForwardLength) {
-                winner += userCarName;
-            }
-        }
-        System.out.println("최종 우승자 : " + winner);
+        List<String> test = mostMovingForwardCar(carMoveStatus);
+        System.out.println(test);
     }
 }
