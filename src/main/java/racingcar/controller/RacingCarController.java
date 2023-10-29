aa2fa30bc38e405688ae4d93ccd2c4436dfcd97b
JongukYang <didwhddnr123@naver.com>
2023-10-29 17:09:25 +0900

    refactor: InputView와 RacingCarController에 해당하는 책임에 맞게 코드 리팩토링

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,27 +1,53 @@
 package racingcar.controller;
 
 import racingcar.domain.RacingCar;
+import racingcar.view.InputPreprocessor;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import racingcar.validator.InputRacingCarNameValidator;
+import racingcar.validator.InputTryCountValidator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarController {
-
     private final InputView inputView;
+    private final InputPreprocessor preprocessor;
+    private final InputRacingCarNameValidator racingCarNameValidator;
+    private final InputTryCountValidator tryCountValidator;
 
     public RacingCarController() {
         this.inputView = new InputView(new OutputView());
+        this.preprocessor = new InputPreprocessor();
+        this.racingCarNameValidator = new InputRacingCarNameValidator();
+        this.tryCountValidator = new InputTryCountValidator();
     }
 
     public void play() {
-        List<String> racingCarNames = inputView.inputRacingCarNames();
+        List<String> racingCarNames = getValidatedRacingCarNames();
         List<RacingCar> racingCars = createRacingCars(racingCarNames);
-
-        int tryCount = inputView.inputTryCount();
+        int tryCount = getValidatedTryCount();
 
         // 게임 시작 로직
+    }
+
+    private List<String> getValidatedRacingCarNames() {
+        String input = inputView.receiveRacingCarNamesInput();
+        String[] racingCarNames = preprocessor.splitInputByComma(input);
+        List<String> racingCarNamesList = new ArrayList<>();
+
+        for (String racingCarName : racingCarNames) {
+            racingCarName = preprocessor.trimInput(racingCarName);
+            racingCarNameValidator.validate(racingCarName);
+            racingCarNamesList.add(racingCarName);
+        }
+
+        return racingCarNamesList;
+    }
+
+    private int getValidatedTryCount() {
+        String input = preprocessor.trimInput(inputView.receiveTryCountInput());
+        return tryCountValidator.validateAndConvert(input);
     }
 
     private List<RacingCar> createRacingCars(List<String> racingCarNames) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,47 +1,21 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.validator.InputRacingCarNameValidator;
-import racingcar.validator.InputTryCountValidator;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class InputView {
     private final OutputView outputView;
-    private final InputPreprocessor preprocessor;
-    private final InputRacingCarNameValidator racingCarNameValidator;
-    private final InputTryCountValidator tryCountValidator;
 
     public InputView(OutputView outputView) {
         this.outputView = outputView;
-        this.preprocessor = new InputPreprocessor();
-        this.racingCarNameValidator = new InputRacingCarNameValidator();
-        this.tryCountValidator = new InputTryCountValidator();
     }
 
-    public List<String> inputRacingCarNames() {
+    public String receiveRacingCarNamesInput() {
         outputView.printRacingCarInputMessage();
-        String input = Console.readLine();
-
-        String[] racingCarNames = preprocessor.splitInputByComma(input); // ','에 따른 분리
-        List<String> racingCarNamesList = new ArrayList<>();
-
-        for (String racingCarName : racingCarNames) {
-            racingCarName = preprocessor.trimInput(racingCarName); // 공백 제거
-
-            racingCarNameValidator.validate(racingCarName); // 예외처리
-
-            racingCarNamesList.add(racingCarName);
-        }
-
-        return racingCarNamesList;
+        return Console.readLine();
     }
 
-    public int inputTryCount() {
+    public String receiveTryCountInput() {
         outputView.printTryCountInputMessage();
-        String input = preprocessor.trimInput(Console.readLine());
-
-        return tryCountValidator.validateAndConvert(input);
+        return Console.readLine();
     }
 }
