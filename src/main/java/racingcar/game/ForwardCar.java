bacba3ed11cabb107eb529b4c65b494180f914fe
혜민 <alddlv81@gmail.com>
2023-10-30 00:27:27 +0900

    feat: 랜덤값 4 이상시 전진 여부 파악

--- src/main/java/racingcar/game/ForwardCar.java
+++ src/main/java/racingcar/game/ForwardCar.java
@@ -4,8 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.Constant;
 
 public class ForwardCar {
-
     private static int randomNumber(){
         return Randoms.pickNumberInRange(Constant.MIN_VALUE,Constant.MAX_VALUE);
+    }
+
+    private static boolean isForward(int number){
+        return number >= Constant.FORWARD_CONDITION;
     }
 }
