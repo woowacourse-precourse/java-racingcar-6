a382319374468a0e35e4aba0d1a19258fdee00ec
Picka <marmong@naver.com>
2023-10-30 00:16:55 +0900

    feat: 프로그램 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.caractions.Car;
+import racingcar.domain.caractions.CarFactory;
+import racingcar.domain.inputhandler.AttemptNumberInputter;
+import racingcar.domain.inputhandler.CarNameInputter;
+import racingcar.domain.outputhandler.CarStateOutputter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarNameInputter carNameInputter = new CarNameInputter();
+        List<String> carNameList = carNameInputter.splitCarNames(carNameInputter.getCarNames());
+        carNameInputter.validateCarName(carNameList);
+        List<Car> cars = CarFactory.createCarsFromNames(carNameList);  // 수정된 부분
+        CarStateOutputter carStateOutputter = new CarStateOutputter(cars);
+        carStateOutputter.printCarNames();
+        AttemptNumberInputter attemptNumberInputter = new AttemptNumberInputter();
+        int attemptNumber = attemptNumberInputter.getAttemptNumber();
+        carStateOutputter.startRace(attemptNumber);
+        carStateOutputter.printChampion();
     }
 }
