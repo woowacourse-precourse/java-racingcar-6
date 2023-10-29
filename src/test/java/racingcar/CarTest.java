8e458ef9a86bd44d8e9d414aac97290f9c5e702b
ImGrooott <20184456@chosun.ac.kr>
2023-10-29 15:23:24 +0900

    add : test skeleton code 추가.

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getOdometer() {
+    }
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/racingcar/GameHostTest.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameHostTest {
+
+    @Test
+    void playOneTurn() {
+    }
+
+    @Test
+    void addCars() {
+    }
+
+    @Test
+    void announceWinner() {
+    }
+}
