d40255b2a4d72784089b67ab3d8b30cae40bdb35
km9404 <kmh940408@gmail.com>
2023-10-30 00:26:36 +0900

    style: 자동차 이름 입력 메시지 변경

--- src/main/java/racingcar/input/InputUtil.java
+++ src/main/java/racingcar/input/InputUtil.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class InputUtil {
     public void inputCarName(HashMap<String, Integer> carName) {
-        System.out.println("경주할 자동차 이름을 입력하세요.");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String carNames = Console.readLine();
         List<String> carNameList = List.of(carNames.split(","));
