522b1b5122ebf4ebc446b5c9f234b22b6d2fa444
Haeun Jeong <gkdms4248@gmail.com>
2023-10-29 23:29:27 +0900

    test(model): Race 한 경기 진행 기능 테스트 추가

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -2,9 +2,11 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class RaceTest {
 
@@ -17,6 +19,18 @@ public class RaceTest {
     @DisplayName("race 회수 유효성 검사 exception 테스트")
     @Test
     void testValidateNumberOfRaceThrow() {
-        assertThrows(IllegalArgumentException.class,() -> new Race(101));
+        assertThrows(IllegalArgumentException.class, () -> new Race(101));
     }
+
+    @DisplayName("한 경기 진행 기능")
+    @Test
+    void testRunSingleRace() {
+        Car car = new Car("pobi");
+        Race race = new Race(1);
+
+        race.runSingleRace(car);
+
+        assertTrue(car.getPosition() == 0 || car.getPosition() == 1);
+    }
+
 }
