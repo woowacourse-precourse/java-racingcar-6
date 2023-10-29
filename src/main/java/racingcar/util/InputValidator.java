340ee324262df19398bb56320e3e1c80049b8043
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-29 19:34:48 +0900

    refactor(util): 에러 메시지 및 출력 메시지 문자열 상수로 변경
    기존 여러 상수나 변수를 + 연산 후 출력하던 방식에서 형식화된 출력으로 변경했습니다.
    - 예시: println(변수1 + 상수 + 변수2); -> printf(상수, 변수1, 변수2);

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -8,28 +8,28 @@ import java.util.Set;
 public class InputValidator {
     public static void validateCarNames(String carNames) {
         if (carNames == null || carNames.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+            throw new IllegalArgumentException(Constants.INVALID_CAR_NAMES_EMPTY);
         }
 
-        List<String> carNameList = Arrays.asList(carNames.split(","));
+        List<String> carNameList = Arrays.asList(carNames.split(Constants.CAR_NAME_DELIMITER));
         Set<String> uniqueCarNames = new HashSet<>(carNameList);
 
         if (carNameList.size() != uniqueCarNames.size()) {
-            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+            throw new IllegalArgumentException(Constants.INVALID_CAR_NAMES_DUPLICATE);
         }
 
         if (carNameList.stream().anyMatch(carName -> carName.length() > 5)) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException(Constants.INVALID_CAR_NAMES_LENGTH);
         }
     }
 
     public static void validateRound(String round) {
         if (round == null || round.isEmpty()) {
-            throw new IllegalArgumentException("시도할 회수를 입력해주세요.");
+            throw new IllegalArgumentException(Constants.INVALID_ROUND_EMPTY);
         }
 
-        if (!round.matches("[0-9]+")) {
-            throw new IllegalArgumentException("시도할 회수는 숫자만 가능합니다.");
+        if (!round.matches(Constants.REGEX_NUMBER) || Integer.parseInt(round) < 1) {
+            throw new IllegalArgumentException(Constants.INVALID_ROUND_ONE_OR_MORE);
         }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,36 +1,36 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
+import racingcar.util.Constants;
 
 import java.util.List;
 
 public class OutputView {
-    private static final String RACING_RESULT_MESSAGE = "실행 결과";
-    private static final String RACING_WINNER_MESSAGE = "최종 우승자";
-    private static final String RESULT_DELIMITER = " : ";
-    private static final String PROGRESS_DELIMITER = "-";
-
     public static void printRacingResult() {
-        System.out.println(System.lineSeparator() + RACING_RESULT_MESSAGE);
+        System.out.println(Constants.RACING_RESULT_MESSAGE);
     }
 
     public static void printProgress(List<Car> cars) {
+        StringBuilder progressBuilder = new StringBuilder();
+
         for (Car car : cars) {
             int carPosition = car.getPosition();
             String progressString = generateProgressString(carPosition);
 
-            System.out.println(car.getName() + RESULT_DELIMITER + progressString);
+            progressBuilder.append(String.format(Constants.PROGRESS_MESSAGE, car.getName(), progressString));
         }
+
+        System.out.println(progressBuilder);
     }
 
     private static String generateProgressString(int position) {
-        return PROGRESS_DELIMITER.repeat(Math.max(0, position));
+        return Constants.PROGRESS_DELIMITER.repeat(Math.max(0, position));
     }
 
     public static void printRacingWinner(List<Car> winners) {
         String winnerNames = getWinnerNamesToString(winners);
 
-        System.out.println(RACING_WINNER_MESSAGE + RESULT_DELIMITER + winnerNames);
+        System.out.printf(Constants.RACING_WINNER_MESSAGE, winnerNames);
     }
 
     private static String getWinnerNamesToString(List<Car> winners) {
@@ -38,6 +38,6 @@ public class OutputView {
                 .map(Car::getName)
                 .toList();
 
-        return String.join(",", winnerNames);
+        return String.join(Constants.CAR_NAME_DELIMITER, winnerNames);
     }
 }
