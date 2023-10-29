61663470623a84a1d122ee79809dabe79d6cc14d
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-29 21:34:39 +0900

    chore(validator):클래스 수정으로 인한 코드 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,7 +4,7 @@ package racingcar.view;
  *  사용자에게 진행상황과 요청사항을 알려주는 담당
  * */
 
-import racingcar.domain.GameRank;
+import racingcar.domain.GameWinner;
 
 public class OutputView {
     private static final String REQUEST_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -30,7 +30,7 @@ public class OutputView {
     }
 
 
-    public void gameWinner(GameRank gameRank) {
-        System.out.print(WINNER_DEFAULT_MESSAGE + gameRank.winnersNameToString());
+    public void gameWinner(GameWinner gameWinner) {
+        System.out.print(WINNER_DEFAULT_MESSAGE + gameWinner.namesToString());
     }
 }
