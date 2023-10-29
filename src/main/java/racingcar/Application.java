871e1458d8143f728df5e1fd0fc862e38397b975
WonyuChoi <phillip07290@naver.com>
2023-10-29 19:43:27 +0900

    refactor: cotroller 객체 생성 방식 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        RacingGameController controller = RacingGameController.createDefault(outputView, inputView);
+        RacingGameController controller = new RacingGameController(outputView, inputView);
         controller.run();
     }
 }
