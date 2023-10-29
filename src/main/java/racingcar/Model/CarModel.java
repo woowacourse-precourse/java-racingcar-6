b79ce3b1c85dfa1b3b42b18cf505620e6ce8ac21
sami355-24 <ddung355@gmail.com>
2023-10-30 03:14:31 +0900

    feat(Model): 자동차와 관련한 로직 구현
    - 자동차 이름이 문자열로 들어왔을때 이를 구분자로 나누는 메서드 추가
    - 자동차 이름에 대해서 검증하고 문제 없을씨 이를 CarGroup으로 만들어 주는 메서드 추가

--- src/main/java/racingcar/Model/CarModel.java
+++ src/main/java/racingcar/Model/CarModel.java
@@ -2,13 +2,24 @@ package racingcar.Model;
 
 import java.util.List;
 import racingcar.Domain.CarGroup;
+import racingcar.Validator.CarNameValidator;
 
 public class CarModel {
 
+    public final CarNameValidator carNameValidator = new CarNameValidator();
+
     public CarGroup preProcessing(String carNames) {
-        return null;
+        List<String> carNameList = divideCarNameByDelimiter(carNames);
+        for (String carName : carNameList) {
+            carNameValidator.isValidLength(carName);
+            carNameValidator.hasWhiteSpace(carName);
+        }
+        carNameValidator.hasDuplicateName(carNameList);
+        return CarGroup.of(carNameList);
     }
     public List<String> divideCarNameByDelimiter(String carName) {
-        return null;
+        String DELIMITER = ",";
+        return List.of(carName.split(DELIMITER));
     }
 }
+
