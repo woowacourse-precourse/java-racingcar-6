2bb3be4eae6ac3f8ffdfeb5d9359586c615a2f7d
Cycrypto <jh01love@naver.com>
2023-10-29 17:40:24 +0900

    refactor:코드 컨벤션에 맞게 수정

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -3,14 +3,24 @@ package racingcar.model;
 public class RacingCar {
     private String name;
     private int distance;
-    public RacingCar(String name){
+
+    public RacingCar(String name) {
         this.name = name;
-        this.distance = -1;
     }
-    public void moveForward(int distance){
+
+    public void moveForward(int distance) {
         this.distance += distance;
     }
-    public int getDistance(){   //  테스트용 코도
+
+    public int getDistance() {   //  테스트용 코도
         return this.distance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDistanceAsBar() {
+        return "-".repeat(this.distance);
     }
 }
