a30c5c403ca17ead72f3484c9f34129e83c6b571
seonghoo1217 <seonghoo1217@naver.com>
2023-10-29 14:50:15 +0900

    refactor : separate error message on the class

---
+++ src/main/java/racingcar/property/ErrorProperty.java
@@ -0,0 +1,7 @@
+package racingcar.property;
+
+public class ErrorProperty {
+
+    public static String VALUE_IS_EMPTY="[ERROR] 입력값이 공백일 수는 없습니다.";
+    public static String VALUE_CONTAINS_SPACE="[ERROR] 입력값에 공백이 포함되었습니다.";
+}
--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -1,16 +1,18 @@
 package racingcar.validation;
 
+import racingcar.property.ErrorProperty;
+
 public abstract class InputValidation {
     public static void verifyForEmptyValue(String inputValue){
         if (inputValue.equals("")){
-            throw new IllegalArgumentException("[ERROR] 입력값이 공백일 수는 없습니다.");
+            throw new IllegalArgumentException(ErrorProperty.VALUE_IS_EMPTY);
         }
     }
 
     public static void verifyForSpaceValue(String inputValue){
         String eliminateSpaceValue = inputValue.replaceAll(" ", "");
         if (!inputValue.equals(eliminateSpaceValue)){
-            throw new IllegalArgumentException("[ERROR] 입력값에 공백이 포함되었습니다.");
+            throw new IllegalArgumentException(ErrorProperty.VALUE_CONTAINS_SPACE);
         }
     }
 }
