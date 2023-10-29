09e7cdc504487dcd4000bbdbaffe53e97e61d3b3
김영준 <rladudwns164@gmail.com>
2023-10-29 16:23:58 +0900

    feat(Application) : Applicatin 클래스 변경
    -Application 클래스는 게임실행을 시작합니다.
    -Controller에 InputView, OutputView를 추가합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RacingGameController mainController = new RacingGameController(InputView.getInstance(),
+                OutputView.getInstance());
+
+        mainController.run();
     }
 }
