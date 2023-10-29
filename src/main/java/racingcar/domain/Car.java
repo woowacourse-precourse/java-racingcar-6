a9e3f91f6e5e454ae0f7e324627645308fc5ef39
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-29 22:35:44 +0900

    feat: 자동차 전진 추가 자동차 경주 게임에서 필요한 전진 조건을 이용하여 전진 기능을 추가하였습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,8 @@
 package racingcar.domain;
 
+import racingcar.resource.CarGameValue;
+import racingcar.util.RandomUtil;
+
 public class Car {
 
     private final String name;
@@ -7,5 +10,19 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move() {
+        if (moveCheck()) {
+            this.position++;
+        }
+    }
+
+    private boolean moveCheck() {
+        int randomNumber = RandomUtil.getRandomNumberRange(
+                CarGameValue.CAR_MOVE_MIN_NUMBER.getValue(),
+                CarGameValue.CAR_MOVE_MAX_NUMBER.getValue()
+        );
+        return randomNumber >= CarGameValue.CAR_MOVE_MORE_THAN_NUMBER.getValue();
     }
 }
