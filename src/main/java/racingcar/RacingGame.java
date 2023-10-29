3c5b673af53a12abacf3e91af737048d2936dc35
Ashmin <dbswoals0512@gmail.com>
2023-10-29 18:26:21 +0900

    자동차 이동 시도 횟수 입력 구현
    사용자로부터 몇 번의 이동을 할 것인지 입력받는 기능을 구현한다.
    
    RacingGame 클래스
    
    생성자
    - targetAttemps 값을 상숫값 0으로 초기화
    
    변수
    - targetAttemps 이동 시도 횟수의 목푯값
    
    상수
    - MIN_TARGET_ATTEMPS 이동 시도 횟수 목푶값의 최솟값 (초기화 값)
    
    유효성 검사
    - validateTargetAttemps 메서드를 통해 사용자로부터 입력 받은 값이 양수인지 검사한다. 값이 음수라면 IllegalArgumentException을 발생시킨다.
    
    메서드
    public
    - void start()
    	기능 추가
    	- 사용자에게 몇 번의 이동을 할지 묻고 값을 입력 받는다.
    	- 그리고 유효성 검사를 진행한다.
    private
    - void printTargetAttempsInputMessage()
    	- 사용자에게 시도 횟수를 묻는다.
    - int getTargetAttemps()
    	- 사용자로부터 값을 문자열 형식으로 입력 받아 숫자 값으로 리턴한다.
    	- 만약 읽은 값이 숫자 형식이 아닐 경우 IllegalArgumentException을 발생시킨다.
    - void validateTargetAttemps(int targetAttemps)
    	- 사용자로부터 입력 받은 시도 횟수 목푯값의 유효성을 검사한다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,20 +7,27 @@ import java.util.List;
 import java.util.Objects;
 
 public class RacingGame {
+    Integer targetAttempts;
+    private static final int MIN_TARGET_ATTEMPS = 0;
 
     public RacingGame() {
-
+        targetAttempts = MIN_TARGET_ATTEMPS;
     }
 
-    public void start() {
+    public void start() throws IllegalArgumentException {
         printCarNameInputMessages();
         List<String> carNames = getCarNames();
+
+        printTargetAttemptsInputMessage();
+        targetAttempts = getTargetAttempts();
+        validateTargetAttempts(targetAttempts);
 
     }
 
     private void printCarNameInputMessages() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
     private List<String> getCarNames() throws IllegalArgumentException {
         String strNames = Console.readLine();
         if (Objects.isNull(strNames)) {
@@ -29,4 +36,22 @@ public class RacingGame {
         return Arrays.asList(strNames.split(","));
     }
 
+    private int getTargetAttempts() throws IllegalArgumentException {
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수가 올바른 숫자 형식 값이 아닙니다.");
+        }
+    }
+
+    private void printTargetAttemptsInputMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    private void validateTargetAttempts(final int targetAttempts) throws IllegalArgumentException {
+        if (targetAttempts < MIN_TARGET_ATTEMPS) {
+            throw new IllegalArgumentException("시도 횟수는 최소 " + MIN_TARGET_ATTEMPS + "이상이어야 합니다");
+        }
+    }
 }
