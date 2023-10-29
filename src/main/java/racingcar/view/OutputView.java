7f5d2230fc0c7613c94743f99959f3e119c5853b
박승민 <tmddudals369@naver.com>
2023-10-30 03:57:14 +0900

    feat(outputView): 게임 진행 횟수 입력을 알리는 메세지 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,15 @@ package racingcar.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_GAME_PROGRESS_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printTempMessage() {
+
+        System.out.println(INPUT_GAME_PROGRESS_COUNT_MESSAGE);
+
     }
 }
