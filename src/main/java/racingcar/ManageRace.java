ca39f71c995d12332a35f55b915d0addcf6c257b
Happeeee <cmg118@naver.com>
2023-10-30 03:56:25 +0900

    Feat: 시도 횟수 입력, 예외 처리 기능 구현
    숫자가 아니거나, 0으로 시작하거나, 음수이거나

--- src/main/java/racingcar/ManageRace.java
+++ src/main/java/racingcar/ManageRace.java
@@ -8,6 +8,16 @@ import java.util.Set;
 
 public class ManageRace {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public void test() {
+        String carName = inputCarName();
+        System.out.println(carName);
+        List<String> carNames = stringToList(carName);
+        checkValidNames(carNames);
+
+        int tryCount = inputTryCount();
+    }
 
     private String inputCarName() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -31,6 +41,24 @@ public class ManageRace {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    private int inputTryCount() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        String tryCount = Console.readLine();
+        checkValidCount(tryCount);
+        return Integer.parseInt(tryCount);
+    }
+
+    private void checkValidCount(String tryCount) {
+        for (char c : tryCount.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+        }
+        if (tryCount.charAt(0) == '0') {
+            throw new IllegalArgumentException();
         }
     }
 }
