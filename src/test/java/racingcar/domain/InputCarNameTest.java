a0ed2d253a998503bc4d67f8f7bc4f5022f8cf09
JINU-CHANG <jenny010925@naver.com>
2023-10-30 01:50:23 +0900

    Test : 자동차이름 글자수에 대한 예외 테스트

--- src/test/java/racingcar/domain/InputCarNameTest.java
+++ src/test/java/racingcar/domain/InputCarNameTest.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +13,15 @@ public class InputCarNameTest {
 
 		assertThat(inputCarName.getCarNameList()).hasSize(3)
 			.containsExactlyInAnyOrder("pobi","woni","jun");
+	}
+
+	@Test
+	void 자동차이름이_5글자보다크면_예외를_발생시킨다() {
+		String input = "pobi,woniii,jun";
+
+		assertThatThrownBy(() -> new InputCarName(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("자동차 이름은 5글자 이하만 가능합니다.");
 	}
 
 }
