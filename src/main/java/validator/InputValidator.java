c4126e5efca5bd7c66f5fcd4133eed30dc27e2db
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-29 14:30:30 +0900

    refactor: 오류 메세지 상수 변수로 변경

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -7,7 +7,9 @@ public class InputValidator {
     private final String REGEXP_PATTERN_SPACE_NO_CHAR = "^[^\\s]+$";
     private final String REGEXP_PATTERN_ONLY_COMMA = "^,+$";
     private final String REGEXP_PATTERN_ONLY_NUMBER ="^[0-9]*S";
-
+    public final String INPUT_CONTAIN_BLANK ="공백이 포함되어 있거나 빈 문자열입니다.";
+    public final String CAR_NAME_EXCEED_FIVE_WORDS ="다섯 글자이하의 입력값이 아닙니다.";
+    public final String ATTEMPT_NUMBER_CONTAIN_CHAR_OTHER_THAN_NUMBER = "시도 회수는 0이상의 정수만 입력하세요.";
     public void checkCarNameValidate(String input) {
         isNameOnlyComma(input);
         String[] carNames = input.split(",");
@@ -24,17 +26,17 @@ public class InputValidator {
 
     private void isNumOnlyComposeNumber(String input) {
         if(!Pattern.matches(REGEXP_PATTERN_ONLY_NUMBER,input)) {
-            throw new IllegalArgumentException("시도 회수는 0이상의 정수만 입력하세요.");
+            throw new IllegalArgumentException(ATTEMPT_NUMBER_CONTAIN_CHAR_OTHER_THAN_NUMBER);
         }
     }
     private void isNumContainBlank(String input) {
         if(!Pattern.matches(REGEXP_PATTERN_SPACE_NO_CHAR,input)) {
-            throw new IllegalArgumentException("공백이 포함되어 있거나 빈 문자열입니다.");
+        throw new IllegalArgumentException(INPUT_CONTAIN_BLANK);
         }
     }
     private void isNameBelowFiveLetters(String input) {
         if(input.length()>NameConstant.MAX_LENGTH){
-            throw new IllegalArgumentException("다섯 글자이하의 입력값이 아닙니다.");
+            throw new IllegalArgumentException(CAR_NAME_EXCEED_FIVE_WORDS);
         }
     }
 
@@ -46,7 +48,7 @@ public class InputValidator {
 
     private void isNameOnlyComma(String input){
         if (Pattern.matches(REGEXP_PATTERN_ONLY_COMMA, input)) {
-            throw new IllegalArgumentException("이름에 대한 정보가 없습니다.");
+            throw new IllegalArgumentException(INPUT_CONTAIN_BLANK);
         }
     }
 }
