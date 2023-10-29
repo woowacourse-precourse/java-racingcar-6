a9276cf60b35eadc06fbc2ea0cbd68d8bd32b7a8
SeungGwa123 <skwan123@naver.com>
2023-10-29 16:15:12 +0900

    Feat: 사용자에게 자동차 이름을 입력받는 carName함수 작성
    사용자에게 자동차 이름을 입력받는 carName함수를 작성하였지만 carDistance, checkCarName이 필요한 부분이 생김
    일단 현재 carName을 작성하고 commit후 필요한 기능이 있는 함수들 작성하고 다시 carName마저 작성하러 돌아올 예정

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.Map;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Input {
+    public Map<String, Integer> carName(){
+        Map<String,Integer> parkingLot = null;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carlist = readLine();
+        // checkCarName 넣어서 맞는 입력 값인지 확인
+        // carDistance 넣어서 거리 0으로 초기화
+        return parkingLot;
+    }
+
+    public Map<String,Integer> carDistance(Map<String,Integer> parkingLot,String carlist){
+        return parkingLot;
+    }
+
+}
