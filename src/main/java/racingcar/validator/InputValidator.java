29e4a3c0726f656f151c39a7c4be090accb2d092
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 19:39:22 +0900

    feat(Validator): 입력값에 공백이 포함되어 있는지 검증하는 기능 추가

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -6,4 +6,10 @@ public class InputValidator {
             throw new IllegalArgumentException("입력값이 비어있습니다.");
         }
     }
+
+    private void validateNotContainSpace(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException("입력값에 공백이 포함되어 있습니다.");
+        }
+    }
 }
