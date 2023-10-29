2e7f64abc1386846d17d2a49f8e17a30abb73eaf
hunko <rhwlgns4386@naver.com>
2023-10-29 15:26:52 +0900

    refactor(RandomNumberGenerator) : generator 인터페이스를 상속

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -3,12 +3,13 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements Generator{
 
     private static final int MIN = 0;
     private static final int MAX = 9;
     private static final int SIZE = 1;
 
+    @Override
     public Integer generate() {
         List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(MIN, MAX, SIZE);
         return randomNumber.get(0);
