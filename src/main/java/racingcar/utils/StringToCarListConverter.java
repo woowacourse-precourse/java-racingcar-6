cf0cad6218879b14c6713b30a6ff4bc966f2efd5
gyungchan Jo <namejgc@naver.com>
2023-10-29 22:01:47 +0900

    fix(StringToCarListConverter): 문자열을 기준으로 자동차 이름을 분리하는 기능 수정
    [pobi,,,]와 같이 입력할 경우 자동차 이름이 공백인 자동차가 있으므로 예외가 발생해야하지만,
    예외가 발생하지 않던 버그 해결

--- src/main/java/racingcar/utils/StringToCarListConverter.java
+++ src/main/java/racingcar/utils/StringToCarListConverter.java
@@ -8,10 +8,11 @@ import java.util.stream.Collectors;
 
 public class StringToCarListConverter implements Converter<String, List<Car>> {
     private static final String DELIMITER = ",";
+    private static final int LIMIT = -1;
 
     @Override
     public List<Car> convert(String source) {
-        return Arrays.stream(source.split(DELIMITER))
+        return Arrays.stream(source.split(DELIMITER, LIMIT))
                 .map(String::trim)
                 .map(Car::new)
                 .collect(Collectors.toList());
