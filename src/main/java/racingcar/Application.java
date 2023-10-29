091f59b930b9fe7958f49ea36ec0cc2102998886
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-30 01:03:45 +0900

    feat: 사용자에게 자동차 이름을 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	private static Map<String, Integer> carMap = new LinkedHashMap<>();
+
+	public static void main(String[] args) {
+		setCarMap();
+		System.out.println(carMap);
+	}
+
+	public static void setCarMap() {
+		String input = readLine().trim();
+		for (String carName : input.split(",")) {
+			if (carName.length() == 0) {
+				throw new IllegalArgumentException("자동차 이름이 올바르지 않습니다.");
+			}
+			carMap.put(carName, 0);
+		}
+	}
 }
