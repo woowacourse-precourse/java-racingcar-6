4227b75474346c3a23bcce77ca98037db69ffd13
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-29 21:21:49 +0900

    feat(RacingCarPlayer) : add generateCars func and playARound func

--- src/main/java/racingcar/RacingCarPlayer.java
+++ src/main/java/racingcar/RacingCarPlayer.java
@@ -2,8 +2,8 @@ package racingcar;
 
 import java.util.List;
 
-public class RacingCarPlayer implements GamePlayer{
-    List<String> carNames;
+public class RacingCarPlayer implements GamePlayer {
+    List<Car> carList;
     Integer numberOfCars;
     Integer numberOfRounds;
     Configurations configurations;
@@ -11,17 +11,40 @@ public class RacingCarPlayer implements GamePlayer{
     Scanner scanner;
     Printer printer;
     RandomNumbersGenerator randomNumbersGenerator;
-
-
     @Override
     public void run() {
-
+        generateCars(scanner.inputCarNames());
+        numberOfRounds = scanner.inputNumberOfRound();
+        randomNumbersGenerator = new RandomNumbersGenerator(
+                configurations.getMinimumOfRange(), configurations.getMaximumOfRange(), carList.size());
+        printer.printRoundStateMessage();
+        for (int round = 1; round < numberOfRounds; round++) {
+            playARound();
+            printer.printCurrent(carList);
+        }
     }
-    RacingCarPlayer(){
+
+    RacingCarPlayer() {
         configurations = new Configurations();
-        advancingMachine = new AdvancingMachine(configurations.getControlValue(),configurations.getMovingDistance());
+        advancingMachine = new AdvancingMachine(configurations.getControlValue(), configurations.getMovingDistance());
         scanner = new Scanner(configurations);
         printer = new Printer();
     }
 
+    private void generateCars(List<String> carNames) {
+        for (String name : carNames) {
+            Car car = new Car(name);
+            carList.add(car);
+        }
+    }
+
+    private void playARound() {
+        List<Integer> randomNumberList = randomNumbersGenerator.generate();
+        for (int i = 0; i < numberOfCars; i++) {
+            Integer currentValue = randomNumberList.get(i);
+            Car car = carList.get(i);
+            car.setCurrentValue(currentValue);
+            advancingMachine.Advance(car);
+        }
+    }
 }
