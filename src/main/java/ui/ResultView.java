a5c83301c32c2b57d0f05303688ec49add2722e7
nohheeyeon <heeyeon1213@gmail.com>
2023-10-29 17:59:46 +0900

    refactor : 우승자 출력 코드 depth 줄이기

--- src/main/java/ui/ResultView.java
+++ src/main/java/ui/ResultView.java
@@ -3,16 +3,17 @@ package ui;
 import domain.Car;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class ResultView {
     public static void printWinners(List<Car> winners) {
         System.out.print("최종 우승자 : ");
-        for (int i = 0; i < winners.size(); i++) {
-            System.out.print(winners.get(i).getName());
-            if (i < winners.size() - 1) {
-                System.out.print(", ");
-            }
+        StringJoiner winnerJoiner = new StringJoiner(", ");
+
+        for (Car car : winners) {
+            winnerJoiner.add(car.getName());
         }
-        System.out.println();
+
+        System.out.println(winnerJoiner);
     }
 }
