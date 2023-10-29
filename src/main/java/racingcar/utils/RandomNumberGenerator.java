85d8f6d5f8afbca3f68fce4ed6e64a1ecf257154
gonixxxx <akdntm946@gmail.com>
2023-10-29 22:25:12 +0900

    style: code formatting

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -3,6 +3,7 @@ package racingcar.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+
     private static final int START_NUM = 0;
     private static final int END_NUM = 9;
 
