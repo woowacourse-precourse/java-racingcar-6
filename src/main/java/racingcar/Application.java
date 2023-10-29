04beb1e9a4d071c4c71fd5681281b4896fc4e2b1
KiminSon <kimin0331@naver.com>
2023-10-29 15:19:19 +0900

    feat(Application): 공백 입력 예외 처리 구현
    ""입력 혹은 " "문자 입력 시 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,30 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
+
+    public static void findCarListError(String[] carList) {
+        Set<String> uniqueCar = new HashSet<>();
+    }
+
+    public static String[] makeCarList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCar = readLine();
+        if (inputCar == "" || inputCar.contains(" ")) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] carList = inputCar.split(",");
+        findCarListError(carList);
+
+        return carList;
+    }
+
+    public static void main(String[] args) {
+        String[] carList = makeCarList();
     }
 }
