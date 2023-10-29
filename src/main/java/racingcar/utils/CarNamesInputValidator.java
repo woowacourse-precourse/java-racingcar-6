dfb6f07ab1f0206b679c46ff370e861c2c4e8391
june-777 <wlwhswnsrl96@gmail.com>
2023-10-29 23:15:07 +0900

    feat(CarNamesInputValidator): 입력 맨 처음 글자 콤마 여부 검증 기능

--- src/main/java/racingcar/utils/CarNamesInputValidator.java
+++ src/main/java/racingcar/utils/CarNamesInputValidator.java
@@ -3,11 +3,18 @@ package racingcar.utils;
 public class CarNamesInputValidator {
     public static void validate(String target) {
         validateIsNotBlank(target);
+        validateFirstCharacterIsNotComma(target);
     }
 
     private static void validateIsNotBlank(String target) {
         if (target.isBlank()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private static void validateFirstCharacterIsNotComma(String target) {
+        if (target.charAt(0) == ',') {
+            throw new IllegalArgumentException("첫 번째 문자로 콤마(,)를 입력하면 안됩니다.");
         }
     }
 }
