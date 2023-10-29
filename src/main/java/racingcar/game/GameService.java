7e1fd09cc04322ef7b91c40c56ae01ae9b3b760d
DryLeonhard <ygangmin@gmail.com>
2023-10-29 22:24:25 +0900

    Feat : 입력 처리, 횟수만큼 차가 전진, 트랙의 상황 출력, 1등 출력

--- src/main/java/racingcar/game/GameService.java
+++ src/main/java/racingcar/game/GameService.java
@@ -9,18 +9,30 @@ public class GameService {
 
     private static final Track tracks = Track.getInstance();
 
-    private GameService(){
+    private GameService() {
 
     }
 
-    public static GameService getInstance(){
+    public static GameService getInstance() {
         return instance;
     }
 
-    public void runRace(){
+    public void runRace() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String str = Console.readLine();
         tracks.setCars(InputUtil.inputNames(str));
-        tracks.printCars();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = Integer.parseInt(Console.readLine());
+
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < count; i++) {
+            tracks.runCars();
+            tracks.printCars();
+        }
+
+        tracks.print1st();
     }
 
 
