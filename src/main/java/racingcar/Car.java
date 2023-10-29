3f8a7ab13136dcff6eae98f9a638ad4ab707e360
Happeeee <cmg118@naver.com>
2023-10-30 04:32:59 +0900

    Refactor: 결과 출력 메소드에 개행 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,23 +5,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private String name;
     private int goCount = 0;
+
     public Car(String name) {
         this.name = name;
     }
 
     public void goOrStop() {
-        if(Randoms.pickNumberInRange(0,9) >= 4) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
             goCount++;
         }
     }
-    
+
     public void printNameAndCount() {
         System.out.print(name + " : ");
-        for(int i = 0; i < goCount; i++) {
+        for (int i = 0; i < goCount; i++) {
             System.out.print("-");
             if (i != goCount - 1) {
                 System.out.print(" ");
             }
         }
+        System.out.println();
     }
 }
