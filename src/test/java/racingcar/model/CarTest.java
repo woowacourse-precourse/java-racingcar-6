a4b658d02a2a61895f0768b9cb2873c519622f7e
sinminseok12 <sinminseok12@naver.com>
2023-10-29 23:41:03 +0900

    test(CarTest): 자동차 이동거리 비교 기능 테스트 구현

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,7 +1,12 @@
 package racingcar.model;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +18,23 @@ public class CarTest {
         Car car = new Car("자동차");
         car.go(goValue);
         assertThat(car.getPosition()).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCarTestArgument")
+    void comparePosition_메서드는_자신과_다른_자동차_객체를_비교해_position_의_대소관계를_비교한다(Integer myMove, Integer compareMove, Integer result) {
+        Car myCar = new Car("내자동차");
+        Car compareCar = new Car("상대자동차");
+        myCar.go(myMove);
+        compareCar.go(compareMove);
+        assertThat(myCar.comparePosition(compareCar)).isEqualTo(result);
+    }
+
+    static List<Arguments> provideCarTestArgument() {
+        return Arrays.asList(
+                Arguments.of(4, 0, 1),
+                Arguments.of(0, 4, -1),
+                Arguments.of(4, 4, 0)
+        );
     }
 }
