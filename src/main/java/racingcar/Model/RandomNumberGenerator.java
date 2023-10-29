4c3410e5b567d469828db713b0acb928857af3df
YejiGong <yejigong00@gmail.com>
2023-10-30 03:32:39 +0900

    Feat: 랜덤 숫자 생성기 구현

--- src/main/java/racingcar/Model/RandomNumberGenerator.java
+++ src/main/java/racingcar/Model/RandomNumberGenerator.java
@@ -1,7 +1,12 @@
 package racingcar.Model;
 
-public class RandomNumberGenerator {
-    public int getRandomNumberResult() {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class RandomNumberGenerator {
+    public static int minRange = 0;
+    public static int maxRange = 9;
+
+    public int getRandomNumberResult() {
+        return Randoms.pickNumberInRange(minRange, maxRange);
     }
 }
