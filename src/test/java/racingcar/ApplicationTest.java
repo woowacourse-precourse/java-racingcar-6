f30e6267f5e7d6e30d3e119b60fddf80cda97829
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 20:49:14 +0900

    test(ApplicationTest): 입력에 대한 테스트코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -38,6 +38,48 @@ class ApplicationTest extends NsTest {
 
 
     @Test
+    void 이름_글자_수_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobiqwrwr", "1")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+    @Test
+    void 영어_소문자가_아닌_문자열_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException(" ,wt, !", "1")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+    @Test
+    void 연속된_구분자에_대한_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("qrr,,rr", "1")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+
+    @Test
+    void 중복된_이름에_대한_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("abc,abc", "1")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+    @Test
+    void 게임_횟수가_올바른_문자인지에_대한_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("abc,def", "a")).isInstanceOf(
+                IllegalArgumentException.class));
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("abc,def", "-1")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+    @Test
+    void 게임_횟수가_범위_안에_있는지에_대한_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("abc,def", "0")).isInstanceOf(
+                IllegalArgumentException.class));
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("abc,def", "1001")).isInstanceOf(
+                IllegalArgumentException.class));
+    }
+
+
+    @Test
     void 중복된_이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("abc,abc", "1"))
@@ -71,21 +113,16 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 전진_정지() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-                },
-                MOVING_FORWARD, STOP
-        );
+        assertRandomNumberInRangeTest(() -> {
+            run("pobi,woni", "1");
+            assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+        }, MOVING_FORWARD, STOP);
     }
 
     @Test
     void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobi,javaji", "1")).isInstanceOf(
+                IllegalArgumentException.class));
     }
 
     @Override
