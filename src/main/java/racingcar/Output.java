850ece9e55ee5fc863d14b7619edf0bdd1e2b4fc
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-29 15:49:16 +0900

    feat: 최종 게임 결과 출력 메서드 추가

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
@@ -9,5 +10,15 @@ public class Output {
             System.out.println(car.currentStatus());
         }
         System.out.println();
+    }
+
+    public static void viewResultOfGame(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        List<String> names = new ArrayList<>();
+        for (Car winner : winners) {
+            names.add(winner.getName());
+        }
+        String result = String.join(", ", names);
+        System.out.println(result);
     }
 }
