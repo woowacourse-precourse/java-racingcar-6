c8bf1223eab75b4bffc818e56260a5996208bd87
xogns4909 <xogns4909@naver.com>
2023-10-30 02:00:12 +0900

    Feat : 라운드 결과 출력에 맞게 포멧 및 테스트 추가

---
+++ src/main/java/domain/formatter/RoundResultFormatter.java
@@ -0,0 +1,32 @@
+package domain.formatter;
+
+import dto.RoundResultDto;
+import java.util.List;
+
+public class RoundResultFormatter {
+
+    public String format(List<RoundResultDto> carResults) {
+        StringBuilder resultBuilder = new StringBuilder();
+
+        for (RoundResultDto result : carResults) {
+            formatSingleResult(resultBuilder, result);
+            resultBuilder.append("\n");
+        }
+
+        return resultBuilder.toString();
+    }
+
+    private static void formatSingleResult(StringBuilder builder, RoundResultDto result) {
+        builder.append(result.carName())
+                .append(" : ")
+                .append(generateDash(result.carLocation()));
+    }
+
+    private static String generateDash(int location) {
+        StringBuilder dashBuilder = new StringBuilder();
+        for (int i = 0; i < location; i++) {
+            dashBuilder.append("-");
+        }
+        return dashBuilder.toString();
+    }
+}
