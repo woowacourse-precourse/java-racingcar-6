d02809502d12328f968aebea8038521342f7f364
hyo-limilimee <1001lily@naver.com>
2023-10-30 00:50:31 +0900

    feat: make OutputView and display position menthod

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,4 +1,21 @@
 package racingcar.View;
 
+import java.util.List;
+import racingcar.Model.Car;
+
 public class OutputView {
+
+    public static void displayPositions(List<Car> cars) {
+        for (Car car : cars) {
+            displayPosition(car.carName, car.carPosition);
+        }
+    }
+    private static void displayPosition(String carName, int position) {
+        System.out.print(carName + " : ");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
 }
