ed60700071eaa0f7874ae27b2a6be04310473360
4444ki <qkrwogh423@gmail.com>
2023-10-29 16:52:32 +0900

    주석 제거 및 README.md 수정
    docs : 커밋 단위와 순서도 수정
    refacor : Game 함수 내에 기능들을 분리하여서 함수로 표현하였습니다.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -4,58 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    public void tryGame(List<String> members, int TryNumber){
+    public void tryGame(List<String> members, int TryNumber) {
         MoveChecker moveChecker = new MoveChecker();
         WinnerChecker winnerChecker = new WinnerChecker();
+        System.out.println();
         System.out.println("실행 결과");
         List<Integer> count = new ArrayList<>();
-        for(int i=0;i<members.size();i++)
+        for (int i = 0; i < members.size(); i++)
             count.add(0);
-        for(int i=0;i<TryNumber;i++){
+        for (int i = 0; i < TryNumber; i++) {
             count = moveChecker.checkMoveOrStop(members, count);
-            System.out.println("");
+            System.out.println();
         }
         winnerChecker.checkWinner(members, count);
     }
-//    public List<Integer> CheckMoveOrStop(List<String> members, List<Integer> count){
-//        int MembersSize = members.size();
-//        String result = "";
-//        for(int i=0;i<MembersSize;i++){
-//            int RandomNumber = Randoms.pickNumberInRange(0,9);
-//            if(RandomNumber >= 4){
-//                count.set(i, count.get(i)+1);
-//            }
-//            result = CheckResult(count.get(i));
-//            System.out.println(members.get(i) + " : " + result);
-//        }
-//        return count;
-//    }
-//    public String CheckResult(int count){
-//        String result = "";
-//        for(int i =0; i<count;i++){
-//            result += "-";
-//        }
-//        return result;
-//    }
-//    public void CheckWinner(List<String> members, List<Integer> count){
-//        int MaxIndex = FindMaxIndex(count);
-//        int CountSize = count.size();
-//        int max = count.get(MaxIndex);
-//        System.out.print("최종 우승자 : " + members.get(MaxIndex));
-//        for(int i=0;i<CountSize;i++){
-//            if(i == MaxIndex)
-//                continue;
-//            if(max == count.get(i))
-//                System.out.print(", " + members.get(i));
-//        }
-//    }
-//    public int FindMaxIndex(List<Integer> count){
-//        int index = 0;
-//        int size = count.size();
-//        for(int i = 0; i<size;i++){
-//            if(count.get(index) < count.get(i))
-//                index = i;
-//        }
-//        return index;
-//    }
 }
