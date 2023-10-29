d63c9996977718c1bc76b7f98a7cd85287a20907
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:27:16 +0900

    test(domain/core): CarTest 구현

---
+++ src/test/java/racingcar/domain/core/car/CarTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain.core.car;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차는 이름을 가진다.")
+    void create() {
+        // given
+        String name = "name";
+
+        // when
+        Car car = new Car(new CarName(name));
+
+        // then
+        assertEquals(name, car.getName());
+    }
+}
