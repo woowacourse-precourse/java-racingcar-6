27758f0b10c6ec28a16eec768bfd335da9f29202
chs98412 <chs98412@gmail.com>
2023-10-29 15:50:01 +0900

    feat: Game 엔티티 설계

--- src/main/java/racingcar/entity/Game.java
+++ src/main/java/racingcar/entity/Game.java
@@ -1,4 +1,19 @@
 package racingcar.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private List<Car> carList = new ArrayList<>();
+
+    private Integer playCount;
+
+    public void addCar(Car car) {
+        carList.add(car);
+    }
+
+    public void setPlayCount(Integer playCount) {
+        this.playCount = playCount;
+    }
 }
