0ad557c80373bf6f5bcf2713d6dd062294eb368f
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-10-30 00:48:10 +0900

    feat(Car) : Car 클래스의 기본 속성 및 메서드 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int moveCount;
+
+    public Car(String name){
+        this.name = name;
+        this.moveCount = 0;
+    }
+}
