b7a9bfe47cc136534dc6932940c224f553be2a7e
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 17:39:14 +0900

    feat(InputView): 자동차 이름 입력 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputCarNames() {
+        String carNames = Console.readLine();
+        // TODO validation
+        return carNames;
+    }
+}
