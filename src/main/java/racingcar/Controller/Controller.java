827f6852df24abf21909dca960ee2433dae8a2ce
YejiGong <yejigong00@gmail.com>
2023-10-30 03:10:21 +0900

    Feat: 컨트롤러 구현

--- src/main/java/racingcar/Controller/Controller.java
+++ src/main/java/racingcar/Controller/Controller.java
@@ -1,7 +1,9 @@
 package racingcar.Controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import racingcar.Global.Constants;
 import racingcar.Model.Game;
 import racingcar.View.InputView;
 import racingcar.View.OutputView;
@@ -12,15 +14,24 @@ public class Controller {
     private Game game;
 
     public void init() {
-        outputView.printString();
-        List<String> carNames = splitString(inputView.getCarsName());
-        outputView.printString();
-        int stageNumber = StringToInt(inputView.getStageNumber());
+        List<String> carNames = getCarName();
+        int stageNumber = getGameStageNumber();
         game = new Game(carNames, stageNumber);
     }
 
+    public List<String> getCarName() {
+        outputView.printString(Constants.getCarsNameState);
+        return splitString(inputView.getCarsName());
+    }
+
+    public int getGameStageNumber() {
+        outputView.printString(Constants.getGameStageNumberState);
+        return StringToInt(inputView.getStageNumber());
+    }
+
     public void run() {
-        outputView.printString();
+        init();
+        outputView.printString(Constants.gameResultStartingState);
         while (!game.isGameEnd()) {
             Map<String, Integer> result = game.runStage();
             outputView.printEachStageResult(result);
@@ -28,11 +39,26 @@ public class Controller {
         outputView.printWinner(game.findWinner());
     }
 
-    public int StringToInt(String string) {
+    public Boolean isStringNumber(String string) {
+        return string.matches("\\d+");
+    }
 
+    public int StringToInt(String string) {
+        if (!isStringNumber(string)) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(string);
+    }
+
+    public Boolean isStringSplitBySymbol(String string, String symbol) {
+        return string.split(symbol).length >= 2;
     }
 
     public List<String> splitString(String string) {
-
+        if (!isStringSplitBySymbol(string, Constants.splitSymbol)) {
+            throw new IllegalArgumentException();
+        }
+        List<String> result = Arrays.asList(string.split(Constants.splitSymbol));
+        return result;
     }
 }
--- src/main/java/racingcar/Global/Constants.java
+++ src/main/java/racingcar/Global/Constants.java
@@ -1,4 +1,9 @@
 package racingcar.Global;
 
 public class Constants {
+    public static final String getCarsNameState = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String getGameStageNumberState = "시도할 회수는 몇회인가요?";
+    public static final String gameResultStartingState = "실행 결과";
+    public static final String gameWinnerState = "최종 우승자";
+    public static final String splitSymbol = ",";
 }
