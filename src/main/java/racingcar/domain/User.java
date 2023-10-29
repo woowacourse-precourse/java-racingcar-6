da5d07a4d51b535438efda032ac35f0f72dbfd6d
seaniiio <cnc4750@naver.com>
2023-10-29 14:41:18 +0900

    feat(User): 게임 시도 횟수 입력받고 저장

--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -7,13 +7,19 @@ import java.util.List;
 
 public class User {
     public List<String> cars = new ArrayList<>();
+    public int gameNumber;
 
     public User() {
         InputCarName();
+        InputGameNumber();
     }
     public void InputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         cars = Arrays.asList(Console.readLine().split(","));
-        System.out.println(cars);
+    }
+    public void InputGameNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        gameNumber = Integer.parseInt(Console.readLine());
     }
 }
 
