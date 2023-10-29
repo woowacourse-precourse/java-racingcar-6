2c6247bb7dc1e63babbc5b09d238bb867a1bb5f1
SehyeonKang <gedfgrdt@naver.com>
2023-10-30 00:28:21 +0900

    feat: 자동차가 이동하는 기능 추가
    Car 클래스에 이동 상황을 나타내는 필드, 이동 기능을 하는 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,8 +3,24 @@ package racingcar.domain;
 public class Car {
 
     private String name;
+    private int progressCount;
 
     public Car(String name) {
         this.name = name;
+        progressCount = 0;
+    }
+
+    public void move(int randomNum) {
+        if (isMoveForward(randomNum)) {
+            moveForward();
+        }
+    }
+
+    private boolean isMoveForward(int randomNum) {
+        return false;
+    }
+
+    private void moveForward() {
+        progressCount++;
     }
 }
