5ea955c464f27bbc84175a9daf8f4c415e3146e4
eora21 <eora21@naver.com>
2023-10-29 17:53:51 +0900

    feat: 🎸 추가 시도할 횟수 입력에 대한 콘솔 메시지 출력 메서드 작성

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -4,6 +4,7 @@ public class ConsoleOutputView implements OutputView {
 
     private static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.";
     private static final String SEPARATED_BY = "(이름은 %s(%s) 기준으로 구분)";
+    private static final String REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     @Override
     public void requestCarNames(String delimiterDescription, String delimiter) {
@@ -13,5 +14,10 @@ public class ConsoleOutputView implements OutputView {
 
     private String createSeparateMessage(String delimiterDescription, String delimiter) {
         return String.format(SEPARATED_BY, delimiterDescription, delimiter);
+    }
+
+    @Override
+    public void requestTryCount() {
+        System.out.println(REQUEST_TRY_COUNT);
     }
 }
