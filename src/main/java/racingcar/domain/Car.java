42c8c12be2a07c96ea717509ca47021736f8718c
mksystem98 <mksystem98@gmail.com>
2023-10-30 00:56:14 +0900

    feat: Car 클래스 추가
    name과 position 필드, 생성자, getter 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+
+    }
+}
