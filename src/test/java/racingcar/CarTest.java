a8d5ada4d10181250ad6ecfbec0ddd52dcddbf4f
ImGrooott <20184456@chosun.ac.kr>
2023-10-29 23:00:12 +0900

    test : CarTest 코드 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,18 +11,23 @@ class CarTest {
 
     @Test
     void getName() {
-
+        Car car = new Car("name");
+        assertThat(car.getName()).contains("name");
     }
 
     @Test
     void getOdometer() {
+        // 랜덤으로 테스트
     }
 
     @Test
     void moveForward() {
+        //랜덤으로 테스트
     }
 
     @Test
     void testToString() {
+        Car car = new Car("name");
+        assertThat(car.toString()).contains("name : ");
     }
 }
