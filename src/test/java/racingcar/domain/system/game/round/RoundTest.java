a538fef8e27f1a3257b9bd4c466fa9caaf469ac6
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:33:18 +0900

    test(domain/system): RoundTest 구현

---
+++ src/test/java/racingcar/domain/system/game/round/RoundTest.java
@@ -0,0 +1,36 @@
+package racingcar.domain.system.game.round;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.core.car.Car;
+import racingcar.domain.core.car.CarName;
+import racingcar.domain.core.car.OnRaceCar;
+import racingcar.domain.core.car.OnRaceCars;
+import racingcar.domain.system.game.round.host.RandomSignalHost;
+
+class RoundTest {
+
+    @Test
+    @DisplayName("라운드 시작")
+    void start() {
+        // given
+        RandomSignalHost host = new RandomSignalHost();
+        OnRaceCar car1 = new OnRaceCar(new Car(new CarName("a")), 0L);
+        OnRaceCar car2 = new OnRaceCar(new Car(new CarName("b")), 0L);
+        OnRaceCar car3 = new OnRaceCar(new Car(new CarName("c")), 0L);
+        OnRaceCars cars = new OnRaceCars(List.of(car1, car2, car3));
+
+        Round round = Round.start(host, cars);
+
+        // when
+        RoundResult roundResult = round.play();
+
+        // then
+        for (OnRaceCar car : roundResult.getAllCars()) {
+            assertTrue(car.getPosition() >= 0L);
+        }
+    }
+}
