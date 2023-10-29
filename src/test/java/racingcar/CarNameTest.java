014de66274b95e121b32b10f69dc7b292359b2a5
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-29 15:52:15 +0900

    refactor: 자동차 이름 테스트 코드에서 불필요한 부분 삭제

--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -10,42 +9,34 @@ import racingcar.model.Name;
 public class CarNameTest {
     @Test
     void 이름_자리수_초과_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    Name name = new Name("123456");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            Name name = new Name("123456");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 빈_이름_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    Name name = new Name("");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            Name name = new Name("");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_입력값_쉼표로_구분되었는지() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    Cars cars = new Cars("a.b.c");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            Cars cars = new Cars("a.b.c");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_이름_두개_이상_받았는지() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> {
-                    Cars cars = new Cars(",");
-                })
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> {
+            Cars cars = new Cars(",");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
