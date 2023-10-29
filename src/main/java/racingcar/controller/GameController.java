e2f76fae891e97b3137609aeab03fcc5e68c5bc7
jinkshower <askthem@naver.com>
2023-10-29 17:13:31 +0900

    feat(GameControlloer): 게임 셋업 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,11 +1,27 @@
 package racingcar.controller;
 
+import static racingcar.view.InputView.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Number;
+
 public class GameController {
-    public void proceed() {
+    private List<Car> cars = new ArrayList<>();
+    private Number round;
+    private void setUp() {
+        List<String> userInput = askCarNames();
+        for (String s : userInput) {
+            cars.add(Car.createCar(s));
+        }
+        round = Number.inputUserRounds();
+    }
+
+    public void play() {
+        setUp();
+    }
         /*
-         *  get user input for car
-         *  create car objects with names
-         *  get user input for rounds
          *  - one round
          *      throw dice for each car
          *      determine whether car move forward (dice >= 4)
@@ -15,5 +31,4 @@ public class GameController {
          *  print winner
          *  exit00
          */
-    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,14 @@
 package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.util.Utils.splitString;
+
+import java.util.List;
 
 public class InputView {
-    public static String askCarNames() {
+    public static List<String> askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return readLine();
+        return splitString(readLine());
     }
 
     public static String askHowManyRounds() {
