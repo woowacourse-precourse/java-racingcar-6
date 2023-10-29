cab75f0f449f6eeefcb4bce72b1cf05941b0076f
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-29 11:51:14 +0900

    feat : 각 차수별 자동차 상대 출력 및 최종 우승자 출력 기능 추가

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,12 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
 public class GameController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final Player player = new Player();
+    private final GameService gameService = new GameService();
 
     public void play() {
         racingCarGamePlayerInputInfo();
+        player.settingCarNames(player.getCarNameStore());
+        printCarStatus();
+        finishGameResult();
     }
 
     private void racingCarGamePlayerInputInfo() {
@@ -16,19 +22,46 @@ public class GameController {
         settingPlayerTryNumber();
     }
 
+    private void printRequestInputCarName() {
+        outputView.printRequestInputCarName();
+    }
+
     private void settingPlayerCarNames() {
         player.settingPlayerCarNameStore(inputView.inputPlayerCarName());
+    }
+
+    private void printRequestInputTryNumber() {
+        outputView.printRequestInputTryNumber();
     }
 
     private void settingPlayerTryNumber() {
         player.settingPlayerTryNumber(inputView.inputPlayerTryNumber());
     }
 
-    private void printRequestInputCarName() {
-        outputView.printRequestInputCarName();
+    private void printCarStatus() {
+        printPlayResult();
+        carStatusLoop();
     }
 
-    private void printRequestInputTryNumber() {
-        outputView.printRequestInputTryNumber();
+    private void printPlayResult() {
+        outputView.printPlayResult();
+    }
+
+    private void carStatusLoop() {
+        for (int i = 0; i < player.getTryNumber(); i++) {
+            for (Car car : getPlayerCarInfo()) {
+                gameService.move(car);
+            }
+            outputView.space();
+        }
+    }
+
+    private List<Car> getPlayerCarInfo() {
+        return player.getCarInfo();
+    }
+
+    private void finishGameResult() {
+        gameService.getMaxPosition(getPlayerCarInfo());
+        outputView.printCarGameWinnerName(gameService.findGameWinner(getPlayerCarInfo()));
     }
 }
