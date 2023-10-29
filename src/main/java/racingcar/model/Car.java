2fd21ee3fa70353c89a40eb5456f0ce0ca1bfb2c
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-30 00:18:37 +0900

    feat: 자동차 정보 저장 클래스 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+public class Car {
+
+    private static final int DISTANCE_INITIALIZE = 0;
+    private final String carName;
+    private final int distance;
+
+    public Car(String carName){
+        this.carName = carName;
+        this.distance = DISTANCE_INITIALIZE;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+        public int getDistance() {
+        return distance;
+    }
+}
