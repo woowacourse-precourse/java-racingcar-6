086e2958fc8d29e38c60c2d48b59a3412b3dfbb3
혜민 <alddlv81@gmail.com>
2023-10-30 00:21:52 +0900

    add: 상수 파일 추가

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Constant {
+    public static final int CAR_NAME_START_LENGTH = 1;
+    public static final int CAR_NAME_LAST_LENGTH = 5;
+
+    public static final int MIN_VALUE = 0;
+    public static final int MAX_VALUE = 9;
+}
