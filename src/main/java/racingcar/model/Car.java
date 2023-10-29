4a5f7204d83cf9e7484f181a42e14abe9112d5cd
sujnkim <blackout3210@naver.com>
2023-10-29 16:56:04 +0900

    feat: add Car이름의 길이의 유효성을 확인하는 method

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,12 +3,13 @@ package racingcar.model;
 
 public class Car {
     private final static int MOVING_STANDARD = 4;
+    private final static int MAX_NAME_LENGTH = 5;
 
     private final String name;
     private int location = 0;
 
     public Car(String name) {
-        this.name = name;
+        this.name = validateNameLength(name);
     }
 
 
@@ -23,4 +24,11 @@ public class Car {
         return randomNumber >= MOVING_STANDARD;
     }
 
+
+    private String validateNameLength(String name) throws IllegalStateException {
+        if(name != null && name.length() <= MAX_NAME_LENGTH){
+            return name;
+        }
+        throw new IllegalStateException();
+    }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -5,16 +5,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
     private final static int MOVING_STANDARD = 4;
     private Car car;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         car = new Car("test");
     }
 
+    @Test
+    void Car_이름길이5이상_인스턴스생성실패() {
+        String wrongName = "longerthan5";
+        assertThatThrownBy(() ->
+                new Car(wrongName)).isInstanceOf(IllegalStateException.class);
+    }
 
     @Test
     void move_기준값이상일때_1칸전진() {
