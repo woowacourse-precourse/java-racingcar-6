022ea3339599eb5fab0478b9e6a37a26fd1b56c5
NahyeChoi <skgp0628@naver.com>
2023-10-29 15:29:07 +0900

    feat(racingcar):drive a car

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -7,21 +7,28 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
+	public int totalCar;
+	public int attemptNumbers;
+	public List<String> carName = new ArrayList();
+	
 	public void inputCarName(){
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		List<String> carName = new ArrayList();
 		String registerCarName = Console.readLine();
 		carName = Arrays.asList(registerCarName.split(","));
 		
+		totalCar = carName.size();
+	}
+	
+	public List<Integer> carRecord() {
 		List<Integer> car = new ArrayList<Integer>();
-		while(car.size() < carName.size()) {
+		while(car.size() < totalCar) {
 			car.add(0);
 		}
+		return car;
 	}
 
 	public void inputAttemptNumbers() {
 		System.out.println("시도할 회수는 몇회인가요?");
-		int attemptNumbers = Integer.parseInt(Console.readLine());
-		System.out.println(attemptNumbers);
+		attemptNumbers = Integer.parseInt(Console.readLine());
 	}
 } // end of Player
