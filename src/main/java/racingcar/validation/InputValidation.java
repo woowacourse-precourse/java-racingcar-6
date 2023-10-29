ad19d9b9b1a729d81d41eeef2e813166e2da071b
ohjuyoungg <ejrm12@naver.com>
2023-10-29 22:08:44 +0900

    feat(race game) : 게임을 시도할 횟수 입력 받은 후, 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리 하는 carNumericValidation 메소드 구현

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import racingcar.view.Inputview;
 
 public class InputValidation {
 
     private static final String COMMA = ",";
     private static final int MAX_STRING = 5;
     private static final int MIN_STRING = 1;
+    private static final String CAR_NAME_LENGTH_VALIDATION_EXCEPTION = "자동차 이름은 1 ~ 5 자리로 입력해야 합니다.";
 
 
     public static List<String> validationName(String carName) {
         if (carName == null || carName.isBlank() || carName.length() < MIN_STRING || carName.length() > MAX_STRING) {
-            throw new IllegalArgumentException("자동차 이름은 1 ~ 5 자리로 입력해야 합니다.");
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_VALIDATION_EXCEPTION);
         }
         return Arrays.asList(carName);
     }
@@ -42,16 +44,21 @@ public class InputValidation {
         return namesList;
     }
 
-    // 자동차 이름이 같은 경우 예외 처리
     public static void carSameNameValidation(List<String> carNames) {
         Set<String> uniqueNames = new HashSet<>(carNames);
         if (uniqueNames.size() < carNames.size()) {
             throw new IllegalArgumentException("자동차 이름은 자동차마다 다르게 입력해야 합니다.");
         }
+        Inputview.tryGameNumber();
     }
 
     // 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리
-    private static void carNumericValidation() {
+    public static int carNumericValidation(String input) {
+        try {
+            return Integer.parseInt(String.valueOf(input));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력 해 주세요.");
+        }
     }
 }
 
