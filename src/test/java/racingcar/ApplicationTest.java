628fabc635de362f75a0617dfc550adb7a20695b
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 20:50:25 +0900

    test(ApplicationTest): 입력에 대한 테스트코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -12,30 +12,6 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
-    @Test
-    void 이름_글자_수_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobiqwrwr", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 영어_소문자가_아닌_문자열_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException(" ,wt, !", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 연속된_구분자에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("qrr,,rr", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
 
     @Test
     void 이름_글자_수_예외_처리() {
@@ -78,38 +54,6 @@ class ApplicationTest extends NsTest {
                 IllegalArgumentException.class));
     }
 
-
-    @Test
-    void 중복된_이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc,abc", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 게임_횟수가_올바른_문자인지에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc,def", "a"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc,def", "-1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 게임_횟수가_범위_안에_있는지에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc,def", "0"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc,def", "1001"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
 
     @Test
     void 전진_정지() {
