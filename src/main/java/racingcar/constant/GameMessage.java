78cf3407b0024074cfb569877814a385d03b0b43
nylonmusk <2912rla@gmail.com>
2023-10-29 15:46:42 +0900

    feat : 실행결과 enum 생성

--- src/main/java/racingcar/constant/GameMessage.java
+++ src/main/java/racingcar/constant/GameMessage.java
@@ -3,6 +3,7 @@ package racingcar.constant;
 public enum GameMessage {
     REQUEST_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     REQUEST_ROUNDS("시도할 회수는 몇회인가요?"),
+    EXECUTION_RESULT("실행 결과"),
     FINAL_WINNER("최종 우승자 : "),
     COLON(" : "),
     DASH("-");
