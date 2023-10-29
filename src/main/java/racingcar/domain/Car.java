a73aaba62f977534d8e38cba4939fc3fcd889575
lcqff <yeonnu82g@gmail.com>
2023-10-29 20:27:00 +0900

    feat: 예외 처리-자동차 이름 5자 이하만 가능하다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,16 @@
 package racingcar.domain;
 
+import static racingcar.utils.ErrorMessage.*;
+
 public record Car(String name, int progress) {
     public Car(String name) {
         this(name,0);
+        validateCarName(name);
+    }
+
+    private void validateCarName(String name) {
+        if(name.length() > 5){
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH.getMessage());
+        }
     }
 }
--- src/main/java/racingcar/utils/ErrorMessage.java
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -1,7 +1,8 @@
 package racingcar.utils;
 
 public enum ErrorMessage {
-    INVALID_SPLIT_CHAR("구분자로 쉼표(,)를 사용해야 합니다.");
+    INVALID_SPLIT_CHAR("구분자로 쉼표(,)를 사용해야 합니다."),
+    INVALID_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다.");
 
     private String message;
 
