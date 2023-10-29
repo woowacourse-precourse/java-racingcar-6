794d153dbcb102181105b8f8e6d6368ab0ed0eac
xogus3492 <wkdxogus3492@gmail.com>
2023-10-29 18:26:35 +0900

    Feat(InputParser): 자동차 이름을 List<이름, 전진 횟수> 객체로 반환하는 기능 구현

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -1,13 +1,26 @@
 package racingcar.util;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class InputParser {
+    private static final int INITIAL_COUNT_VALUE = 0;
 
     public static List<String> parseCarNameList(String input) {
         return Arrays.stream(input.split(","))
                 .collect(Collectors.toList());
+    }
+
+    public static Map<String, Integer> parseCarMap(String input) {
+        return Arrays.stream(input.split(","))
+                .collect(Collectors.toMap(
+                        key -> key,
+                        value -> INITIAL_COUNT_VALUE,
+                        (exitingValue, newValue) -> exitingValue,
+                        HashMap::new
+                ));
     }
 }
