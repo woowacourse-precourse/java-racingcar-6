3f2aaad0b9d613b3828bfd94a7649062310f7f7a
Jae-Young98 <ygy0102@gmail.com>
2023-10-29 18:58:31 +0900

    feat(RacingGameController): 게임 진행을 위한 자동차, 횟수 세팅 기능 구현

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,32 @@
+package racingcar.controller;
+
+import racingcar.Car;
+import racingcar.util.Converter;
+import racingcar.util.InputValidator;
+import racingcar.view.InputView;
+
+import java.util.List;
+
+public class RacingGameController {
+    private final InputView inputView = new InputView();
+    private final InputValidator inputValidator = new InputValidator();
+    private final Converter converter = new Converter();
+
+    public void run() {
+        initialize();
+    }
+
+    private void initialize() {
+        String inputNames = inputView.inputNames();
+        List<String> carNames = converter.getCarNames(inputNames);
+        inputValidator.validateCarNames(carNames);
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .toList();
+
+        String inputNumber = inputView.inputAttemptNumber();
+        if (inputValidator.validateAttemptNumber(inputNumber)) {
+            int attemptNumber = Integer.parseInt(inputNumber);
+        }
+    }
+}
