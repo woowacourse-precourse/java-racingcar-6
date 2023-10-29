2cf436a3c5940670f56f3cd5c3b2f5b972e99996
jcw1031 <jcw001031@gmail.com>
2023-10-29 22:12:35 +0900

    test: 이동 횟수 입력 테스트
    사용자가 1 이상의 정수를 이동 횟수로 입력했을 때 테스트

--- src/test/java/racingcar/view/InputViewTeste.java
+++ src/test/java/racingcar/view/InputViewTeste.java
@@ -13,18 +13,34 @@ class InputViewTeste {
 
     @ParameterizedTest
     @ValueSource(strings = {"Car A,car B", " Car A,car B "})
-    void 자동차_이름_입력(String carsName) {
+    void 자동차_이름_입력(String inputCarsName) {
         // given
-        command(carsName);
+        command(inputCarsName);
 
         // when
-        List<String> inputCarsName = InputView.enterCarsName();
+        List<String> carsName = InputView.enterCarsName();
 
         // then
         Assertions.assertAll(
-                () -> assertThat(inputCarsName).isNotNull(),
-                () -> assertThat(inputCarsName).hasSize(2),
-                () -> assertThat(inputCarsName.get(0)).isNotEmpty()
+                () -> assertThat(carsName).isNotNull(),
+                () -> assertThat(carsName).hasSize(2),
+                () -> assertThat(carsName.get(0)).isNotEmpty(),
+                () -> assertThat(carsName.get(0)).isEqualTo(inputCarsName.split(",")[0].trim())
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 10})
+    void 이동_횟수_입력(int inputMovesNumber) {
+        // given
+        command(String.valueOf(inputMovesNumber));
+
+        // when
+        int movesNumber = InputView.enterMovesNumber();
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(movesNumber).isEqualTo(inputMovesNumber)
         );
     }
 
