786de727df5be9831582429b4fe162d277d522a2
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-10-29 18:29:29 +0900

    feat : move the racing cars

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import view.InputView;
 
 public class Application {
+    static int moveCondition = 4;
     public static void main(String[] args) {
         String[] racingCarNames = InputView.inputRacingCarNames();
         int moveCount = InputView.inputMoveCount();
+        int[] distances = new int[racingCarNames.length];
+        for (int i = 0; i < moveCount; ++i) {
+            move(distances);
+        }
+    }
+
+    static void move(int[] distances) {
+        for (int i = 0; i < distances.length; ++i) {
+            int randomNumber = Randoms.pickNumberInRange(0,9);
+            if (randomNumber >= moveCondition) {
+                ++distances[i];
+            }
+        }
     }
 }
