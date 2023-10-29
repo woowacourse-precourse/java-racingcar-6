b22cf0cb464271f0f48ece1b34d2bdfae30f4229
odiohead <txpc03@gmail.com>
2023-10-29 18:41:48 +0900

    create Winner class for calculation.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -13,7 +13,7 @@ public class Application {
         count = Input_data.input_number();
 
         Play_game.racing_game(racer_name, distance, count);
-        calculate_winner(racer_name, distance, winner);
+        Winner.calculate_winner(racer_name, distance, winner);
         
 
     }
@@ -22,22 +22,6 @@ public class Application {
 
 
 
-    public static void calculate_winner(ArrayList<String> name, ArrayList<String> distance, ArrayList<String> winner){
-        int best_record = 0;
-        for(int i=0; i<name.size(); i++){
-            if(distance.get(i).length() > best_record) {
-                best_record = distance.get(i).length();
-            }
-        }
-
-        for(int j=0; j<name.size(); j++) {
-            if(distance.get(j).length() == best_record) {
-                winner.add(name.get(j));
-            }
-        }
-
-        String result_str = String.join(", ",winner);
-        System.out.println("최종 우승자 : " + result_str);
-    }
+    
     
 }
