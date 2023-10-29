f1ee7f3613e0af2d09d62fc84dde2595265936cd
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 03:31:39 +0900

    feat: verify logic for racer name list is duplicate

--- src/main/java/racingcar/validation/NameValidation.java
+++ src/main/java/racingcar/validation/NameValidation.java
@@ -3,6 +3,10 @@ package racingcar.validation;
 import racingcar.property.ErrorProperty;
 import racingcar.property.ValidateProperty;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class NameValidation extends InputValidation{
 
     public static void verifyForRacerNameIsEngOrKor(String inputValue){
@@ -14,6 +18,13 @@ public class NameValidation extends InputValidation{
     public static void verifyForRacerNameIsLengthNotOver(String inputValue){
         if (inputValue.length()> ValidateProperty.NAME_LENGTH_STANDARD){
             throw new IllegalArgumentException(ErrorProperty.NAME_VALUE_LENGTH_IS_OVER);
+        }
+    }
+
+    public static void verifyForRacerNameIsDuplicate(String racerNameList){
+        List<String> nameList = Arrays.asList(racerNameList.split(","));
+        if (nameList.size() != new HashSet<>(nameList).size()){
+            throw new IllegalArgumentException(ErrorProperty.NAME_VALUE_IS_DUPLICATE);
         }
     }
 }
