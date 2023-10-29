85910b8c00aa5cea234fc8cd34680433f2f25ed5
su-giana <giananews@gmail.com>
2023-10-29 19:37:13 +0900

    # 출력 구현
    # 제목
    시도 횟수 요청 문구 출력
    
    ## 부가설명
    '시도할 횟수'라는 문구를 출력한다
    
    ### 연관된 이슈 (Optional)
    - 이슈 번호 #04 해결

--- src/main/java/racingcar/RacingOutput.java
+++ src/main/java/racingcar/RacingOutput.java
@@ -16,4 +16,9 @@ public class RacingOutput {
     {
         bw.write("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     }
+
+    private void putTryInputStatement() throws IOException
+    {
+        bw.write("시도할 회수는 몇회인가요?");
+    }
 }
