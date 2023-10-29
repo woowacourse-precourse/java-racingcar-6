eabe5d54b80a35cc80acb114dd03d19908c7cee0
leesc0839 <leesc0839@naver.com>
2023-10-29 23:53:45 +0900

    refactor(test.racingcar): carListFactory에서 길이 5이상의 에러인풋이 들어오면 에러 생성 확인
    assertThrows를 이용하여 길이 5이상의 에러 인풋이 들어오면 IllegalArgumentException 생성 발생 확인

--- src/test/java/racingcar/domain/CarListFactoryTest.java
+++ src/test/java/racingcar/domain/CarListFactoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class CarListFactoryTest {
@@ -18,7 +19,7 @@ class CarListFactoryTest {
 
     @Test
     @DisplayName("정상입력시_carList_객체_생성")
-    void createCarList() {
+    void normalInput_CreateCarList_Success() {
         //given
         String normalInput = "a,b,c";
         String expected = "a : \n" +
@@ -31,5 +32,15 @@ class CarListFactoryTest {
         //then
         String actual = carList.toString();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("길이5초과시_carList_객체생성실패")
+    void errorInput_CreateCarList_Fail() {
+        //given
+        String errorInput = "aaaaaa,c";
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> carListFactory.create(errorInput));
     }
 }
