5e6c8c24d38f852eeb3b874a1b7e8d2cc969f950
unknown <khd128k@gmail.com>
2023-10-29 22:17:35 +0900

    feat: main game controller

--- src/main/java/racingcar/controller/Application.java
+++ src/main/java/racingcar/controller/Application.java
@@ -1,7 +1,33 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.model.RaceResult;
+import racingcar.view.InputHandler;
+import racingcar.view.OutputHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputHandler.getCarNames();
+        int attemptCount = InputHandler.getAttemptCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+
+        Race race = new Race(cars);
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < attemptCount; i++) {
+            race.raceOnce();
+            OutputHandler.printRaceState(cars);
+        }
+
+        List<Car> winners = RaceResult.getWinners(cars);
+        OutputHandler.printWinners(winners);
     }
 }
