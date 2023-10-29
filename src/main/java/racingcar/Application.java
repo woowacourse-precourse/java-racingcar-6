8ff1b19cfcadfcffe7a363a502a7bbca95a4bda6
JIHYE <jdh324@naver.com>
2023-10-29 17:36:01 +0900

    feat: 자동차 이름 구분 기능 구현
    입력받은 자동차 이름을 쉼표 기준으로 구분하고 이름이 5자를 초과하면 예외를 발생하는 기능을 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,29 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         String input = inputCarName();
+        Map<String, Integer> racingCars = validateCarName(input);
     }
 
     private static String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
+    }
+
+    public static Map<String, Integer> validateCarName(String input) {
+        Map<String, Integer> racingCars = new HashMap<>();
+        String[] cars = input.split(",");
+        for (String car : cars) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            racingCars.put(car, 0);
+        }
+        return racingCars;
     }
 }
