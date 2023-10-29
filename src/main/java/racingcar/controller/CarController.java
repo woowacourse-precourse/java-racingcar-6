94db59131ad4d8eafde59bde0779d840b63571a6
acs0209 <annjae4676@naver.com>
2023-10-29 17:14:34 +0900

    feat: add save function in CarController

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,24 @@
+package racingcar.controller;
+
+import static racingcar.constant.MessageConst.SAVE_CAR_NAMES_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Stream;
+import racingcar.service.CarService;
+
+public class CarController {
+
+    private final CarService carService = new CarService();
+
+    public void save() {
+        System.out.println(SAVE_CAR_NAMES_MESSAGE);
+        List<String> carNames = stringToCollection(Console.readLine());
+        carService.save(carNames);
+    }
+
+    private List<String> stringToCollection(String input) {
+        return Stream.of(input.split(","))
+                .toList();
+    }
+}
