80c92c40102e03437482828f46c586111d8ade5a
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-10-29 21:59:25 +0900

    feat: 경주 자동차 이름, 시도 회수 입력 받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        racingGame();
+
     }
+
+    private static void racingGame () {
+        String carNames = inputCarNames();
+        int trialTimes = inputTrialTimes();
+
+
+    }
+
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return Console.readLine();
+    }
+
+    private static int inputTrialTimes() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+
 }
