b34b3321a714d0c390fcf12246a87b6317a26712
ohjuyoungg <ejrm12@naver.com>
2023-10-29 14:57:56 +0900

    (feat(race game) : 자동차 경주 validation 패키지 InputValidation 클래스 뼈대 만들기)

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -1,2 +1,24 @@
-package racingcar.validation;public class InputValidation {
+package racingcar.validation;
+
+public class InputValidation {
+
+    private static void validationBlankName() {
+        // 사용자가 아무 값을 입력하지 않은 경우
+    }
+
+    private static void validationSixCharOrMore() {
+        // 6 자 이상의 이름을 입력할 경우
+    }
+
+    private static void validationCommaSeparate() {
+        // 쉼표로 구분하여 입력하지 않은 경우
+    }
+
+    private static void validationTwoOrMore() {
+        // 2 이상의 자동차 이름을 입력 하지 않은 경우
+    }
+
+    private static void carNumericValidation() {
+        // 사용자가 숫자가 아닌 다른 값을 입력했을 경우 예외 처리
+    }
 }
