87818b3e21b4767b901b8a13b816a89b7f85d128
Gyutae Kim <kiimwf0819@gmail.com>
2023-10-29 16:02:52 +0900

    test: 랜덤값이 4 이상일때 전진값이 증가하는지에 대한 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-    Car car = new Car("testCar");
-
     @Test
     void 자동차위치_초기값0 () {
+        Car car = new Car("testCar");
         assertThat(0).isEqualTo(car.getPosition());
+    }
+
+    @Test
+    void 랜덤값4이상_자동차전진증가 () {
+        Car car = new Car("testCar");
+        int randomNumber = 4;
+        car.setPosition(randomNumber);
+
+        assertThat(1).isEqualTo(car.getPosition());
     }
 }
