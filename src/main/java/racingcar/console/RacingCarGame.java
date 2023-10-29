8953f0a77cb2afe9c8736957eacc8c68e7d859fe
Hayoon <hy97@sju.ac.kr>
2023-10-29 16:31:45 +0900

    refactor(RacingCarGame): ProductConstant에 선언한 상수로 교체한다

--- src/main/java/racingcar/console/RacingCarGame.java
+++ src/main/java/racingcar/console/RacingCarGame.java
@@ -2,12 +2,14 @@ package racingcar.console;
 
 import racingcar.domain.RacingCar;
 import racingcar.service.RacingCarService;
+import racingcar.util.constant.ProductConstant;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.util.constant.ProductConstant.*;
 
 public class RacingCarGame {
     public int start = 0;
@@ -17,15 +19,15 @@ public class RacingCarGame {
 
         racingCarService = new RacingCarService();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_NAMES);
         ArrayList<RacingCar> racingCars = racingCarService.initRacingCarMembers();
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        int number = racingCarService.initNumberOfAttempts();
+        System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
+        int attempts = racingCarService.initNumberOfAttempts();
 
-        System.out.println("실행 결과");
+        System.out.println(GAME_RESULT);
 
-        while (start < number) {
+        while (start < attempts) {
             racingCars = racingCarService.movingForward(racingCars);
             racingCarService.getRaceResult(racingCars);
 
