c4adf3f590090dc920f1198e326c3d16375464af
김승진 <ohksj77@gmail.com>
2023-10-29 19:16:48 +0900

    feat(Number): 숫자 도메인을 통해 이동 여부 확인 기능 구현

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -8,7 +8,9 @@ public enum ErrorMessage {
     INVALID_MAX_CALCULATION("최대 이동 거리를 구하다 오류가 발생했습니다."),
     INVALID_RACING_CARS_NAME("자동차 이름 입력값이 잘못되었습니다."),
     INVALID_WINNER_NUM("우승자가 존재하지 않습니다."),
-    NO_LEFT_TRY_COUNT("잔여 시도 횟수가 없습니다.");
+    NO_LEFT_TRY_COUNT("잔여 시도 횟수가 없습니다."),
+    INVALID_NUMBER("숫자의 범위가 벗어났습니다."),
+    INVALID_NUMBER_NUM("숫자의 개수가 잘못되었습니다.");
 
     private final String message;
 
