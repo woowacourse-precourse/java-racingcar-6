78c7e9b068d183ee9ae58db5024e2a6571d4f7f4
Chenowoo <cjsgusdn14@naver.com>
2023-10-29 16:22:46 +0900

    test(InputValueTest): 이동 횟수 입력값에 대한 테스트 기능 구현  - 이동 횟수가 100이하의 자연수로 입력되고 변수에 저장되었는지 확인하는 테스트

--- src/test/java/racingcar/InputValueTest.java
+++ src/test/java/racingcar/InputValueTest.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,12 +23,25 @@ class InputValueTest {
         inputValue = new InputValue();
     }
 
+    @AfterEach
+    void close(){
+        Console.close();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = "pobi,woni,jun")
     void 자동차_이름_입력(String input) {
         generateUserInput(input);
         String[] names = inputValue.InputNameOfCars();
         assertThat(names).isEqualTo(new String[]{"pobi", "woni", "jun"});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "4")
+    void 이동_횟수_입력(String input) {
+        generateUserInput(input);
+        int moveCount = inputValue.InputMovingCount();
+        assertThat(moveCount).isEqualTo(4);
     }
 
     private static void generateUserInput(String input) {
