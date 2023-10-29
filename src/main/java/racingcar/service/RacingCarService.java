e1eb42e8dd7477e8b98c3470c83b9a3c7aa6a71c
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-29 17:20:34 +0900

    feat(RacingCarService): 자동차 이름을 입력받고 가공 후에 인스턴스 변수로 할당하는 insertCarNames 메서드 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,10 +1,30 @@
 package racingcar.service;
 
+import racingcar.domain.*;
+import racingcar.domain.dto.RunningCarRecord;
+import racingcar.domain.dto.WinnerRecord;
 import racingcar.domain.validator.InputValidator;
+import racingcar.util.StringManipulator;
+
+import java.util.List;
 
 public class RacingCarService {
+    private final Cars cars;
+    private final Judge judge;
+    private final RandomNumberGenerator randomNumberGenerator;
+
+    public RacingCarService(Cars cars, Judge judge, RandomNumberGenerator randomNumberGenerator) {
+        this.cars = cars;
+        this.judge = judge;
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
 
     public void inputValidate(String input, InputValidator inputValidator) {
         inputValidator.validate(input);
+    }
+
+    public void insertCarNames(String carNames) {
+        List<String> names = StringManipulator.split(carNames);
+        cars.insertCarsNames(names);
     }
 }
