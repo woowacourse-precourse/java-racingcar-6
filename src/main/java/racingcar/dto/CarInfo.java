eb827918277bc26dd8a09bdae5a621330b748d4c
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 16:08:59 +0900

    refactor(CarInfo): 자동차의 진척도를 반환하는 기능 추가

--- src/main/java/racingcar/dto/CarInfo.java
+++ src/main/java/racingcar/dto/CarInfo.java
@@ -3,8 +3,13 @@ package racingcar.dto;
 import racingcar.domain.Car;
 
 public record CarInfo(String name, int position) {
+    public static final String POSITION_PROGRESS = "-";
 
     public static CarInfo of(Car car) {
         return new CarInfo(car.getName(), car.getPosition());
+    }
+
+    public String convertPositionValue() {
+        return POSITION_PROGRESS.repeat(this.position);
     }
 }
