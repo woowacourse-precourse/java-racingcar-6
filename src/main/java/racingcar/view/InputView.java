43f4cb66baaf8925fe5aa3ab1a962e57243e26a0
elyudwo <dudqk9696@naver.com>
2023-10-29 22:20:49 +0900

    [Feat] : 자동차 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Utils;
+
+import java.util.List;
+
+public class InputView {
+    private final Utils utils;
+
+    public InputView() {
+        utils = new Utils();
+    }
+
+    public List<String> insertCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String names = Console.readLine();
+        return utils.convertStringToList(names);
+    }
+}
