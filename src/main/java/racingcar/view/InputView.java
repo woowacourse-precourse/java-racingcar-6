e557de8d30d1472be4d5b820594e1004de114197
Dawin <dawin002@naver.com>
2023-10-29 17:41:03 +0900

    refactor(InputView): UserInput 클래스 이름 변경
    클래스 이름 변경: UserInput -> InputView

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,12 @@
 package racingcar.view;
 
-public class UserInput {
-    public static String inputCarNames() {
+public class InputView {
+    public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
-    public static String inputRound() {
+    public String inputRound() {
         System.out.println("시도할 회수는 몇회인가요?");
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
