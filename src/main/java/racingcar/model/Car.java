effae84f2ea54ed7bc806bb7dcf0c29f4b985c64
eunhee <dmsgml9903@gmail.com>
2023-10-30 01:29:50 +0900

    feat(Car): 자동차가 1씩 이동하는 함수 구현
    - 매개변수가 4 미만이면 그대로 두고 4이상이면 1씩 이동거리를 증가시키는 로직 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,10 +4,22 @@ import racingcar.validation.CarValidator;
 
 public class Car {
 
-    private String carName;
+    private final String carName;
+    private Integer movingDistance;
+
+    private static final int STARTING_LINE = 0;
+    private static final int MOVE_CONDITION_MIN_NUMBER = 4;
+    private static final int MOVE_ONE_STEP = 1;
 
     public Car(String carName) {
         CarValidator.validateCarName(carName);
         this.carName = carName;
+        this.movingDistance = STARTING_LINE;
+    }
+
+    public void moveForward(int randomNumber) {
+        if (randomNumber >= MOVE_CONDITION_MIN_NUMBER) {
+            this.movingDistance += MOVE_ONE_STEP;
+        }
     }
 }
