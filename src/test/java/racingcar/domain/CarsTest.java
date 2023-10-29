9329028ff5604895cfad07327bfacb022ba099bb
elyudwo <dudqk9696@naver.com>
2023-10-29 18:29:26 +0900

    [Test] : 자동차 이동 메서드 테스트 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -2,11 +2,15 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.util.Utils;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 class CarsTest {
 
@@ -38,5 +42,28 @@ class CarsTest {
 
         // then
         assertNotSame(cars.getCars(), carList);
+    }
+
+    @DisplayName("자동차가 랜덤 숫자에 따라 움직이거나 멈춘다.")
+    @Test
+    void moveCars() {
+        // given
+        List<String> nameList = List.of("pobi", "lee", "kim");
+        List<Integer> resultDistance = List.of(1,1,0);
+        Cars cars = new Cars();
+        cars.makeCars(nameList);
+        mockStatic(Utils.class);
+        when(Utils.makeRandomNumber()).thenReturn(7,5, 2);
+
+        // when
+        cars.moveCars();
+        List<Car> carList = cars.getCars();
+
+        // then
+        IntStream.range(0, carList.size())
+                .forEach(i -> {
+                    Car car = carList.get(i);
+                    assertThat(car.getDistance()).isEqualTo(resultDistance.get(i));
+                });
     }
 }
