9fdc01eb1c4bafe1a78220658cd5aae00a42f9fb
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-29 22:16:41 +0900

    🎯feat: Car 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+/* Car 하나하나 관리 하는 클래스
+*  이름
+*  위치
+*  전진 기능
+*  후진 기능
+* */
+public class Car {
+    private String carName;
+    public Car(String carName) {
+        this.carName = carName;
+    }
+}
