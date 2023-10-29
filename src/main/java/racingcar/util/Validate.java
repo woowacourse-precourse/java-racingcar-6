9406f27c057f53c59173f14436478ad69cbf388f
mdgome <mdgome3@gmail.com>
2023-10-29 17:38:54 +0900

    Refacotr: 함수 이름 변경(코딩 컨벤션)
    함수 이름 첫 문자가 대문자로 코딩 컨벤션에 따라 소문자로 변경

--- src/main/java/racingcar/util/Validate.java
+++ src/main/java/racingcar/util/Validate.java
@@ -3,46 +3,42 @@ package racingcar.util;
 import java.util.List;
 
 public class Validate {
-    public static void CarNameNumeric(List<String> list) {
-        for(String name: list) {
-            if (name.chars().anyMatch(Character::isDigit)) {
-                throw new IllegalArgumentException("자동차 이름에 숫자는 넣을 수 없습니다.");
-            }
+    public static void carNameNumeric(String name) {
+        if (name.chars().anyMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("자동차 이름에 숫자는 넣을 수 없습니다.");
         }
     }
-    public static void LastCharIsComma(String name) {
+    public static void lastCharIsComma(String name) {
         if(! name.endsWith(",")) {
             throw new IllegalArgumentException("잘못된 입력 : \",\" 뒤는 자동차 이름이 들어가야합니다.");
         }
     }
 
-    public static void CarNameLength(List<String> list) {
-        for(String name: list) {
-            if(name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
-            }
+    public static void carNameLength(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
         }
     }
 
-    public static void CarNameIsOne(List<String> list) {
+    public static void carNameIsOne(List<String> list) {
         if(list.size() == 1) {
             throw new IllegalArgumentException("자동차 이름은 2개 이상 입력해야 합니다.");
         }
     }
 
-    public static void InputStringNotContainsComma(String input) {
+    public static void inputStringNotContainsComma(String input) {
         if(input.contains(",")) {
             throw new IllegalArgumentException("자동차 이름을 구분짓는 구분자는 쉼표(\",\")를 입력해야 합니다.");
         }
     }
 
-    public static void InputIsNull(String input) {
+    public static void inputIsNull(String input) {
         if(input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("문자를 입력해주세요");
+            throw new IllegalArgumentException("입력 값을 확인해주세요");
         }
     }
 
-    public int MoveIsNumericAndReturnInteger(String move) {
+    public static int moveIsNumericAndReturnInteger(String move) {
         try {
             return Integer.parseInt(move);
         } catch(NumberFormatException exception) {
