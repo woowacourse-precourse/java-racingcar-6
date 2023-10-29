567263020ded252c32c12b44c96c448296038624
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-29 16:04:24 +0900

    test : View

---
+++ src/test/java/racingcar/View/InputViewTest.java
@@ -0,0 +1,61 @@
+package racingcar.View;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import racingcar.View.InputView;
+class InputViewTest {
+    public static InputStream setReadLine(String readLine) {
+        return new ByteArrayInputStream(readLine.getBytes());
+    }
+
+    @Test
+    void askCarName(){
+        InputStream readLine = setReadLine("Pobi,NANa,Kane");
+        System.setIn(readLine);
+
+
+        String expectCarName = "Pobi,NANa,Kane";
+        String resultCarName = InputView.askCarName();
+
+        assertEquals(expectCarName,resultCarName);
+    }
+
+
+
+
+    @Test
+    void askTryCount(){
+        InputStream readLine = setReadLine("5");
+        System.setIn(readLine);
+
+
+        int inputTryCount = 5;
+        int intTrtCount = InputView.askTryCount();
+
+        assertEquals(inputTryCount,intTrtCount);
+    }
+
+    @Test
+    void storeCarName(){
+        InputStream readLine = setReadLine("Pobi,NANa,Kane");
+        System.setIn(readLine);
+
+        Map<String,Integer> carCountMapTest = new HashMap<String,Integer>();
+        carCountMapTest.put("Pobi",0);
+        carCountMapTest.put("NANa",0);
+        carCountMapTest.put("Kane",0);
+
+        Map<String,Integer> carCountMap = InputView.storeCarName();
+        assertEquals(carCountMapTest,carCountMap);
+
+
+    }
+}
