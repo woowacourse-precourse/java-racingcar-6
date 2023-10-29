2ff9e320a206bc79c9c69f3084e06cbc4fa4335a
JudyYeon <dev.judykim@gmail.com>
2023-10-29 23:05:17 +0900

    feat: 자동차 경주 게임 Application 기능 구현
    자동차 경주 게임에 필요한 객체들을 조합하여 Application을 동작하는 main 기능을 구현하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+
+        RacingGame game = RacingGame.getInstance();
+        game.addCar(carNames);
+
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String rounds = Console.readLine();
+        game.play(rounds);
+
+        System.out.printf("최종 우승자 : %s", game.getWinners());
+
     }
 }
