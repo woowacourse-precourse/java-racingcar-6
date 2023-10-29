8e4138157a7c308607cb0ce573c677ed7aa10605
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-29 22:08:46 +0900

    refactor(racingcar): make sure a method has one duty
    - Take print message out of askCar method

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -9,11 +9,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class UserInput {
 
     public List<String> askCar() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
         List<String> carTypeList = new ArrayList<String>();
-        for (String carType: input.split(",")){
+        for (String carType : input.split(",")) {
             checkLength(carType);
             carTypeList.add(String.valueOf(carType));
         }
@@ -21,18 +20,17 @@ public class UserInput {
 
     }
 
-    public void checkLength(String carType){
-        if (carType.length() > 5){
+    public void checkLength(String carType) {
+        if (carType.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5글자 보다 적어야 합니다.");
         }
     }
 
-    public int askNumberGame(){
+    public int askNumberGame() throws IllegalArgumentException {
         System.out.println("시도할 횟수는 얼마인가요?");
         int numGame = Integer.parseInt(Console.readLine());
         return numGame;
     }
-
 
 
 }
