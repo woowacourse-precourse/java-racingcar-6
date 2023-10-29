8e743b228c04a8cb182ee9285ea295c3602f253a
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-29 15:21:41 +0900

    refactor(InitalInputs): 이름 입력 시 예외 처리 기능 추가

--- src/main/java/racingcar/view/InitialInputs.java
+++ src/main/java/racingcar/view/InitialInputs.java
@@ -2,10 +2,14 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import racingcar.validation.CarNames;
+
 public class InitialInputs {
     public static String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
+        CarNames checkCarNames = new CarNames();
+        checkCarNames.validateCarNames(input);
         return input;
     }
 }
