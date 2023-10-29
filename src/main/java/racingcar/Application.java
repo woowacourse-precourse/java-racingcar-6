e01d8875b5433f1801b684cb04f4712b44713077
gyu_hyeok <jjookki33@gmail.com>
2023-10-30 01:39:05 +0900

    feat:게임 입력 기능 구현 - 자동차 이름 입력 기능 - 시행할 게임 횟수 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(START_INPUT_MESSAGE);
+        String racingCar = Console.readLine();
+
+        System.out.println(INPUT_TRY_NUMBER);
+        String gameNumber = Console.readLine();
     }
+
+    private static final String START_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_NUMBER = "시도할 횟수는 몇회인가요?";
+
+
 }
