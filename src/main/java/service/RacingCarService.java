d4dbcd0193fdbc2019073c9be5eeaf286b33f5a9
sangeun-b <baek0007@algonquinlive.com>
2023-10-30 00:51:20 +0900

    feat: 제일 빠른 차의 전진거리 구하기

--- src/main/java/service/RacingCarService.java
+++ src/main/java/service/RacingCarService.java
@@ -1,6 +1,8 @@
 package service;
 import domain.Car;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RacingCarService {
@@ -14,4 +16,11 @@ public class RacingCarService {
         int position = car.getPosition();
         car.setPosition(position+randomNumber);
     }
-}
+
+    public int findMaxPosition(List<Car> cars) {
+        int maxPosition = 0;
+        for(Car car: cars){
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
