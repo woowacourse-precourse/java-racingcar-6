e0cc1c9c7163515985e3bb2bea31d8ddb2569f55
yeondori <yeonseo.jung0@gmail.com>
2023-10-29 19:56:00 +0900

    feat: requestRacers 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    public static HashMap<String, StringBuilder> requestRacers(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] input = readLine().split(",");
+
+        if (input.length<2) throw new IllegalArgumentException();
+
+        Map<String, StringBuilder> racers = Stream.of(input)
+                .peek(name -> {
+                    if (name.length() > 5) {
+                        throw new IllegalArgumentException("Name should be 5 characters or less: " + name);
+                    }
+                })
+                .collect(Collectors.toMap(name -> name, name -> new StringBuilder()));
+
+        return new HashMap<>(racers);
+    }
+
 }
