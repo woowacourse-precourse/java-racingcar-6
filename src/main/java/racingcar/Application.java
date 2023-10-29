276b1a8fe949275cafdce78e481a7301de109249
sung-yeop <jjabc9459@naver.com>
2023-10-29 16:40:56 +0900

    feat: 예외 처리 기능
    - 이름의 길이가 5를 넘어갈 경우 IllegalArgumentException 발생
    - 이름에_대한_예외_처리 테스트 통과

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Application application = new Application();
+        String input = application.input_name();
+        String[] split = application.devide_name(input);
+        application.exceptionOfNameLength(split);
+
     }
 
     public String input_name(){
@@ -17,6 +23,11 @@ public class Application {
         return split;
     }
 
-
-
+    public void exceptionOfNameLength(String[] split){
+        for (String s : split) {
+            if (s.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
