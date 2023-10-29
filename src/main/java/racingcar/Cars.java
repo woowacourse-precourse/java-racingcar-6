0a8026d14fc660dfe65577104f70bbb0228400c0
yubin <dbqls2821@gmail.com>
2023-10-29 23:54:11 +0900

    refactor: 스트림에서 forEach로 변경

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 final class Cars {
 
@@ -21,15 +22,32 @@ final class Cars {
     }
 
     void tryMove() {
-        cars.stream()
-                .filter(car -> roulette.spin() >= 4)
-                .forEach(Car::move);
+        cars.forEach(car -> {
+            if(roulette.spin() >= 4) car.move();
+        });
     }
-
 
     private void validateNames(String[] names) {
         var uniqueNameCount = Arrays.stream(names).distinct().count();
         if (names.length < 1 || names.length != uniqueNameCount) throw new IllegalArgumentException();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars target = (Cars) o;
+        if (Objects.equals(cars, target.cars)) return true;
+        if (cars.size() != target.cars.size()) return false;
+        for (int i = 0; i < cars.size(); i++) {
+            if(!cars.get(i).equals(target.cars.get(i))) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars, roulette);
     }
 
     enum Constant {
