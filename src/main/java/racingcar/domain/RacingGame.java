83396060b7ba2ef1dd726c1ba4ec161e5e530c61
JudyYeon <dev.judykim@gmail.com>
2023-10-29 22:45:12 +0900

    feat(RacingGame): 자동차에 이름을 부여하는 기능추가
    게임의 흐름을 주관하는 RacingGame에서 입력된 이름대로 Car 인스턴스를 만드는 addCar 기능을 구현했습니다.

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,25 +1,55 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
+    private static RacingGame instance;
+    private final List<Car> cars;
+
     private RacingGame() {
+        cars = new ArrayList<>();
     }
 
     public static RacingGame getInstance() {
-        return null;
+        if (instance == null) {
+            instance = new RacingGame();
+        }
+        return instance;
     }
 
-    public void initialize(List<String> carNames) {
+    public void addCar(String input) {
+
+        String[] carNames = input.split(",");
+
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            Car car = new Car(name);
+            cars.add(car);
+        }
+    }
+
+    public void play(String input) {
+
 
     }
 
-    public void play(int rounds) {
 
-    }
+    public String getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(-1);
 
-    public List<Car> getWinners() {
-        return null;
+        List<String> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        return String.join(", ", winners);
     }
 
 }
