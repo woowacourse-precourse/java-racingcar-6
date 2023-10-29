123cf58c99f6dee5344818e36d60198069f55b65
harandal24601 <devchaehokim@gmail.com>
2023-10-30 01:16:07 +0900

    CHORE : Add throw
    Change 'names' to 'carNames'.
    Add process code if car name is more than 5 character.

--- src/main/java/racingcar/InputProcessor.java
+++ src/main/java/racingcar/InputProcessor.java
@@ -6,9 +6,14 @@ import java.util.Collections;
 public class InputProcessor {
 
     public static ArrayList<String> carNameInputProcess(String input) {
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> carNames = new ArrayList<>();
         String[] splitInput = input.split(",");
-        Collections.addAll(names, splitInput);
-        return names;
+        for (String carName : splitInput) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            carNames.add(carName);
+        }
+        return carNames;
     }
 }
