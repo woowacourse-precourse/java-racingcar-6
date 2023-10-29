1675707c9a1b13039b8b3d72c138c561b8fb6983
jdohun <wjdehgns97@naver.com>
2023-10-29 12:42:30 +0900

    feat: 이동할 횟수를 입력받고 숫자인지 검증하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import racingcar.domain.establisher.Establisher;
 import racingcar.domain.script.Script;
 
 import java.util.HashMap;
@@ -9,11 +10,14 @@ import static racingcar.domain.reception.ReceptionDest.registering;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Script.LINEUP.print();
-        HashMap<String, Integer> lineUp = registering();
+//        Script.REGISTER.print();
+//        HashMap<String, Integer> lineUp = registering();
         // hi,my,name,is,hello,world
 
-        System.out.println(lineUp);
+//        System.out.println(lineUp);
+        Script.LAPS.print();
+        int laps = Establisher.setLaps();
+        System.out.println(laps + "회");
     }
 
 }
