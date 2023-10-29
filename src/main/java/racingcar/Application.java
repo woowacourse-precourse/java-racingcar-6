f15a9463ec62a2b97fb439a4712f636afc0c170c
Yu JeongMin <polar522@naver.com>
2023-10-29 14:06:06 +0900

    기능 1-2. 주어진 횟수를 입력받기(몇 번의 라운드를 진행할 지 입력 받기) 를 getNumberOfRounds()함수를 만들어 구현함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,10 @@ public class Application {
         // TODO: 프로그램 구현
             // 기능 1-1. 자동차의 이름을 입력 받아 몇 대(n)의 자동차로 경기를 할 것인지 입력 받기
             String Input = getInput();
+            // 입력 받은 자동차 이름들을 배열에 저장
+            String[] cars = Input.split(",");
+            // 기능 1-2. 주어진 횟수를 입력받기(몇 번의 라운드를 진행할 지 입력 받기)
+            int rounds = getNumberOfRounds();
     }
 
     // 기능 1-1. 자동차의 이름을 입력 받아 몇 대(n)의 자동차로 경기를 할 것인지 입력 받기
@@ -19,5 +23,13 @@ public class Application {
             String input = scanner.nextLine();
             scanner.close();
             return input;
+    }
+    // 기능 1-2. 주어진 횟수를 입력받기(몇 번의 라운드를 진행할 지 입력 받기)
+    public static int getNumberOfRounds(){
+        int rounds = 0;
+        boolean validInput = false;
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+        return rounds;
     }
 }
