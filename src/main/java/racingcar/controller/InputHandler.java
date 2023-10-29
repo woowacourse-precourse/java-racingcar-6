45b68680c03842b90f9c2581864270fe979dc01a
Ukj0ng <xb253q@gmail.com>
2023-10-29 18:52:52 +0900

    feat(InputHandler): 구현 기능 테스트
    테스트할 기능:
    - 입력받은 자동차 이름 파싱
    - 시도 횟수 입력받기

---
+++ src/main/java/racingcar/controller/InputHandler.java
@@ -0,0 +1,33 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.view.InputView;
+
+public class ViewController {
+
+    public static List<String> parseCarName(String input) {
+        List<String> carNames = new ArrayList<>();
+//        if (isValidInput()) {
+//            splitInput(carNames, input);
+//        }
+        splitInput(carNames, input);
+
+        return carNames;
+    }
+
+    public static int triesCount(String input) {
+        int tries = 0;
+
+        //validateInput();
+        tries = Integer.parseInt(input);
+        return tries;
+    }
+
+    private static void splitInput(List<String> carNames, String input) {
+        String[] cars = input.split(",");
+        for (String car : cars) {
+            carNames.add(car.trim());
+        }
+    }
+}
