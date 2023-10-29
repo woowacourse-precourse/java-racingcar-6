d23f1b6a944c3cb3249fd1d62a6bf19d734c0921
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 11:41:14 +0900

    refactor(RacingController): 자동차 조립 로직 제거 사용자에게 자동차 이름을 입력받아서 자동차를 조립하는 RacingCarAssemblyService의 로직이 컨트롤러로 Cars 객체를 반환하기 때문에 컨트롤러로 도메인이 노출되었다. 도메인이 외부로 노출되는 것을 막기 위해 자동차 경주를 책임지는 RacingCarService 내에 자동차 조립을 책임지는 RacingCarAssemblyService를 주입하여 노출을 막았다.

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,10 +1,7 @@
 package racingcar.controller;
 
 import java.util.List;
-import racingcar.domain.CarEngine;
-import racingcar.domain.Cars;
 import racingcar.domain.RacingResult;
-import racingcar.service.RacingCarAssemblyService;
 import racingcar.service.RacingCarService;
 import racingcar.view.RacingCarInputValidator;
 import racingcar.view.RacingCarInputView;
@@ -15,27 +12,21 @@ public class RacingController {
     private final RacingCarOutputView racingCarOutputView;
     private final RacingCarInputValidator racingCarInputValidator;
     private final RacingCarService racingCarService;
-    private final RacingCarAssemblyService racingCarAssemblyService;
-    private final CarEngine carEngine;
 
     public RacingController(RacingCarInputView racingCarInputView, RacingCarOutputView racingCarOutputView,
-                            RacingCarInputValidator racingCarInputValidator, RacingCarService racingCarService,
-                            RacingCarAssemblyService racingCarAssemblyService, CarEngine carEngine) {
+                            RacingCarInputValidator racingCarInputValidator, RacingCarService racingCarService) {
         this.racingCarInputView = racingCarInputView;
         this.racingCarOutputView = racingCarOutputView;
         this.racingCarInputValidator = racingCarInputValidator;
         this.racingCarService = racingCarService;
-        this.racingCarAssemblyService = racingCarAssemblyService;
-        this.carEngine = carEngine;
     }
 
     public void run() {
         List<String> carNames = getCarNamesFromUser();
         int attemptCounts = getAttemptCountsFromUser();
-        Cars assembledCars = racingCarAssemblyService.assemble(carNames, carEngine);
 
         racingCarOutputView.printStartMessage();
-        RacingResult racingResult = racingCarService.race(assembledCars, attemptCounts);
+        RacingResult racingResult = racingCarService.race(carNames, attemptCounts);
 
         racingCarOutputView.printEachRacingResult(racingResult);
         racingCarOutputView.printWinner(racingResult);
