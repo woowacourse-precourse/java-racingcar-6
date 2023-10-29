282353eff47d995a962d7614a9f616dd266a0694
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-29 16:01:52 +0900

    test: 어플리케이션 정상 동작 테스트 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+
 public class Application {
     public static void main(String[] args) {
+        RacingGameController racingGameController = new RacingGameController();
+        racingGameController.run();
         // TODO: 프로그램 구현
     }
 }
