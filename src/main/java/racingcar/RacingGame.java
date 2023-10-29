ad39036ed8101c5e39e7997f1a696f8d8fe55dfb
KiminSon <kimin0331@naver.com>
2023-10-29 17:05:18 +0900

    feat(RacingGame): 자동차 이동 출력 구현
    printMoveResult함수에서 회차마다 모든 자동차의 이동 출력하는 것을 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,12 +9,22 @@ public class RacingGame {
     private static int carListLength;
     private static int winCount;
 
+    public static void printMoveResult(String carName, int moveCount) {
+        System.out.print(carName + " : ");
+        for (int i = 0; i < moveCount; i++) {
+            System.out.print("-");
+        }
+        System.out.print("\n");
+    }
+
     public static void moveCar() {
         for (int i = 0; i < carListLength; i++) {
             if (Randoms.pickNumberInRange(0, 9) > 3) {
                 gameResultCount[i]++;
             }
+            printMoveResult(carList[i], gameResultCount[i]);
         }
+        System.out.print("\n");
     }
 
     public static void playGame(String[] inputCarList, int playCount) {
