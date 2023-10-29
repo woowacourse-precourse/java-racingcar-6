bfd0e4d3bb5d73cf4d6c45e8e4a16126e68b1b4e
Yoon Jiho <zlhh3842@nate.com>
2023-10-29 19:06:31 +0900

    시도할 횟수 입력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,10 +11,23 @@ public class Application {
         }
     }
 
+    private static int checkTurn(String turnInput){
+        try {
+            return Integer.parseInt(turnInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
         // 1. 자동차 이름 입력
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] inputs = Console.readLine().split(",");
         checkCarNames(inputs);      // 예외 처리
+
+        // 2. 시도할 횟수 입력
+        System.out.println("시도할 회수는 몇회인가요?");
+        String turnInput = Console.readLine();
+        int turns = checkTurn(turnInput);       // 예외 처리 및 횟수 값
     }
 }
