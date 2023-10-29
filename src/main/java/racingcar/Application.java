854285953c08c5f505082fe7b6c377ab5467059b
yeondori <yeonseo.jung0@gmail.com>
2023-10-29 20:15:16 +0900

    feat: race 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,15 +11,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+
+        HashMap<String, StringBuilder> racers = requestRacers();
         int num = requestNum();
-        System.out.println(num);
+
+        System.out.println("\n실행 결과");
+        for (; num > 0; num--) {
+            race(racers);
+        }
     }
 
-    public HashMap<String, StringBuilder> requestRacers(){
+    public static HashMap<String, StringBuilder> requestRacers() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] input = readLine().split(",");
 
-        if (input.length<2) throw new IllegalArgumentException();
+        if (input.length < 2) throw new IllegalArgumentException();
 
         Map<String, StringBuilder> racers = Stream.of(input)
                 .peek(name -> {
@@ -38,6 +46,12 @@ public class Application {
         return Integer.parseInt(input);
     }
 
+    public static void race(HashMap<String, StringBuilder> racers) {
 
-
+        for (String racer : racers.keySet()) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {racers.get(racer).append("-");}
+            System.out.println(racer + " : " + racers.get(racer));
+        }
+        System.out.println("");
+    }
 }
