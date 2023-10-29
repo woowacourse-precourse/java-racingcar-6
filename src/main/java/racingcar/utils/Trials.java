a9f81c939b4892391c1324cedcaec1456d69e04d
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-29 17:19:29 +0900

    feat: 시도 회수 입력 및 유효성 검사 추가

---
+++ src/main/java/racingcar/utils/Trials.java
@@ -0,0 +1,31 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import main.java.racingcar.model.Car;
+import net.bytebuddy.pool.TypePool;
+
+public class Trials {
+
+    private static final int MIN_BOUND = 1;
+    public static int getTrials() {
+        String trials = Console.readLine();
+        trialIsNum(trials);
+        trialAboveZero(Integer.parseInt(trials));
+        return Integer.parseInt(trials);
+    }
+    // 0회 미만, 입력받은 값이 수가 아닌 경우
+    private static void trialIsNum (String trials) {
+        try {
+            Integer.parseInt(trials);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private static void trialAboveZero(int trials) {
+        if (trials < MIN_BOUND) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
