e51c5b0a2b7cd38f80e2414a482c8c9ea6c956ab
jae woo Jung <awfjol2008@naver.com>
2023-10-30 02:06:26 +0900

    feat: 자동차 이름 목록으로 입력된 문자열을 1차적으로 검증하는 기능 구현

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -2,6 +2,11 @@ package racingcar.validator;
 
 public class CarNameValidator {
 
+    public static void validateForString(String carName) {
+        validateEmpty(carName);
+        validateContainsComma(carName);
+    }
+
     private static void validateEmpty(String carName) {
         if (carName.isEmpty()) {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
