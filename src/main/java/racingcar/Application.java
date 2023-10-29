8c8d36b861f92ee9880bc2d5242d83efb2cb792d
seaniiio <cnc4750@naver.com>
2023-10-29 15:15:15 +0900

    feat(Application): 사용자로부터 자동차 목록을 받아 Car인스턴스를 요소의 자료형으로 하는 cars 리스트 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,24 @@
 package racingcar;
+import racingcar.domain.Car;
 import racingcar.domain.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<String> carsList = new ArrayList<>();;
+    static List<Car> cars = new ArrayList<>();;
+
     public static void main(String[] args) {
         User user = new User();
+        carsList = user.getCars();
+        createCars(carsList);
+    }
+    public static void createCars(List<String> carsList) {
+        for(int i=0; i<carsList.size(); i++) {
+            Car car = new Car();
+            car.carName = carsList.get(i);
+            cars.add(car);
+        }
     }
 }
