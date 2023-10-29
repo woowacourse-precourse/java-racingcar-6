a76ac27c725321427018d73786a1bd497d90d7f4
OhJongMin <jmoh7480@gmail.com>
2023-10-29 18:09:06 +0900

    feat(racingcar):자동차 게임 객체를 묶어 게임 실행 객체 생성

---
+++ src/main/java/racingcar/Controller/GameContoller.java
@@ -0,0 +1,35 @@
+package racingcar.Controller;
+
+import java.util.List;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameContoller {
+    private InputView input = new InputView();
+    private OutputView output = new OutputView();
+
+    public void startGame(){
+        List<String> names = makeRacingCars();
+        RacingGame game = new RacingGame(names);
+        int tryCount = makeTryCount();
+        output.printTryMessage();
+        for(int i = 0; i < tryCount; i++){
+            game.moveCarForward();
+            output.printTryResult(game);
+        }
+        output.printWinnerMessage();
+        output.printWinnerNames(game);
+    }
+
+    public List<String> makeRacingCars(){
+        output.printNameMessage();
+        return input.inputCarNames();
+    }
+
+    public int makeTryCount(){
+        output.printCountMessage();
+        return input.inputTryCount();
+    }
+}
