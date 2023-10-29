f44f3f1a34114c512d9816a7c4fe07d7a7f91d36
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-10-29 17:14:59 +0900

    feat(Controller): 레이싱의 주행 상태를 출력 하는 기능 완성
    docs: 레이싱의 주행 상태를 출력 하는 기능 추가 완료 체크

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -16,7 +16,13 @@ public class Controller {
         trialNumber = inputView.printTrial();
         outputView.printResultStart();
     }
-    
+
+    private void playRacing(int trialNumber){
+        for(int i = 0; i < trialNumber; i++){
+            cars.racingOne();
+            racingResult();
+        }
+    }
 
     private void racingResult(){
         for (int i = 0; i < cars.carsLength(); i++){
