5eb806d848d960722d4f45ee2af39cdd22fde065
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-29 22:44:06 +0900

    feat(racingcar): save constants
    - save constants so that it's easier to change when needed

---
+++ src/main/java/racingcar/domain/Constants.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+public class Constants {
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static final String ATTEMP_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static final String RESULT_OF_GAME = "실행 결과";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String ERROR_CAR_NAME_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+
+    public static final String CAR_NAME_SEPARATOR = ",";
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+}
