dd0e978769c04ca5d26366ccc4d3efea836b1cbd
kimdohyung <pdohyung@gmail.com>
2023-10-29 20:09:37 +0900

    feat(Car): 자동차를 정의하는 car 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
