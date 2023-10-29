e1fcd8dce5e1ab5a906cf10532040ab2d47a076b
sinminseok12 <sinminseok12@naver.com>
2023-10-29 23:36:04 +0900

    feat(Car): 자동차 위치 이동 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 public class Car {
     private static final Integer INIT_POSITION = 0;
+    private static final Integer MOVE_STANDARD = 4;
 
     private final String name;
     private Integer position;
@@ -9,5 +10,11 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = INIT_POSITION;
+    }
+
+    public void go(Integer value) {
+        if (MOVE_STANDARD <= value) {
+            position++;
+        }
     }
 }
