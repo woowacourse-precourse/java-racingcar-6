5e4796296cb6f7da3e4224447be9fd09d1f49bd1
kssumin <201566@jnu.ac.kr>
2023-10-30 03:00:05 +0900

    feat : 랜덤 값을 생성하는 책임을 가진 RandomGenerator 객체 구현

---
+++ src/main/java/racingcar/RandomGenerator.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGenerator {
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 9;
+	public int generate() {
+		return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+	}
+}
+
+
