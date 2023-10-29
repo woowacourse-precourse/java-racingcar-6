963f341cd7b97f305f0f2591187946328bbfda81
SeYeol00 <dmot0126@gmail.com>
2023-10-30 01:41:48 +0900

    feat: 아웃풋에서 결과값 출력하는 메서드 및 기능 추가  및 일급 컬렉션 ResultList 생성
    ResultList의 경우 일급 컬렉션 객체로 사용

--- src/main/java/racingcar/service/output/Output.java
+++ src/main/java/racingcar/service/output/Output.java
@@ -4,5 +4,7 @@ import racingcar.domain.Cars;
 import racingcar.dto.output.CarsOutputDto;
 
 public interface Output {
-    void show(CarsOutputDto carsOutputDto);
+    void showPlaying(CarsOutputDto carsOutputDto);
+    void showResult(CarsOutputDto carsOutputDto);
+
 }
--- src/main/java/racingcar/service/output/OutputView.java
+++ src/main/java/racingcar/service/output/OutputView.java
@@ -1,13 +1,38 @@
 package racingcar.service.output;
 
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.dto.output.CarOutputDto;
 import racingcar.dto.output.CarsOutputDto;
 
 public class OutputView implements Output{
     @Override
-    public void show(CarsOutputDto carsOutputDto) {
+    public void showPlaying(CarsOutputDto carsOutputDto) {
         showCarNameAndPosition(carsOutputDto);
+    }
+
+    @Override
+    public void showResult(CarsOutputDto carsOutputDto) {
+        Cars cars = carsOutputDto.cars();
+        int max = cars.getCarList().get(0).getCarPosition();
+        ResultList resultList = new ResultList();
+        for(Car car : cars.getCarList()){
+            if(car.getCarPosition()>max){
+                max = car.getCarPosition();
+                resultList.getResults().clear();
+                resultList.getResults().add(car.getCarName());
+            }else if(car.getCarPosition()==max){
+                resultList.getResults().add(car.getCarName());
+            }
+        }
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i < resultList.getResults().size();i++){
+            System.out.print(resultList.getResults().get(i));
+            if (i < resultList.getResults().size()-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 
     private static void showCarNameAndPosition(CarsOutputDto carsOutputDto) {
