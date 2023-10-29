084ba6c8019dc98eca91fc33b5353f49b4c2e1d1
sangeun-b <baek0007@algonquinlive.com>
2023-10-30 00:33:16 +0900

    feat: 레이싱 카 이동 메소드 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,15 +8,19 @@ public class Car {
     private String name;
     private int position;
 
-    public Car(String carName){
+    public Car(String carName) {
         this.name = carName;
         this.position = 0;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public int getPosition(){
+    public int getPosition() {
         return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 }
--- src/main/java/service/RacingCarService.java
+++ src/main/java/service/RacingCarService.java
@@ -1,4 +1,6 @@
 package service;
+import domain.Car;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RacingCarService {
@@ -6,5 +8,10 @@ public class RacingCarService {
     public int generateRandomNumber() {
         int randomNumber = pickNumberInRange(1,9);
         return randomNumber;
+    }
+
+    public void move(Car car, int randomNumber) {
+        int position = car.getPosition();
+        car.setPosition(position+randomNumber);
     }
 }
