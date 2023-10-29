5a3a07a8e168e9e1bdb7c5bd9e8b991f5101831c
rakyun1 <dbskim234@naver.com>
2023-10-29 04:48:27 -0600

    Refactor: add validateAndParseTry()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,17 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static java.lang.Integer.parseInt;
 
 public class Application {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * 유저의 차 이름 입력을 받아 유효성을 검사하고 쉼표를 기준으로 나눠 String[]로 리턴
+     * @return String[] carNames
+     */
     public static String[] validateCarName(){
         String carInput = readLine();
         if (carInput == null || carInput.isEmpty()) {
@@ -32,5 +38,24 @@ public class Application {
             }
         }
         return carNames;
+    }
+
+    /**
+     * 유저의 게임을 시도할 횟수를 입력 받아 유효성을 검사하고 int로 바꿔 리턴
+     * @return int tires
+     */
+    public static int validateAndParseTry(){
+        String tryInput = readLine();
+        int tries;
+        if (tryInput == null || tryInput.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            tries = parseInt(tryInput);
+        } catch (Exception e){
+            throw new IllegalArgumentException("숫자를 입력하세요");
+        }
+        return tries;
     }
 }
