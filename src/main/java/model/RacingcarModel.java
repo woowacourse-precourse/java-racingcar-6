fa7e7d80f34c5d9045656d27e2df6f2f0fc3cccf
MoonJaeGyeong <bblbblan69@gmail.com>
2023-10-29 20:14:58 +0900

    <feat> : <car_names 입력값 처리>
    car_names 입력 값을 , 을 기준으로 나눠서 객체에 저장

--- src/main/java/model/RacingcarModel.java
+++ src/main/java/model/RacingcarModel.java
@@ -1,26 +1,25 @@
 package model;
 
 public class RacingcarModel {
-    String car_names;
+    String[] car_names;
     int repeat;
 
     public RacingcarModel(String car_names, String repeat){
         ErrorCheck(car_names, repeat);
-        this.car_names = car_names;
+        this.car_names = SliceByComma(car_names);
         this.repeat = Integer.parseInt(repeat);
     }
 
     private void ErrorCheck(String car_names, String repeat){
-        car_names = car_names.replaceAll("\\s",""); // 문자열 공백 제거
-        String[] car_name = car_names.split(","); // ',' 을 기준으로 문자열 자르기
+        String[] car_name = SliceByComma(car_names);
         for(int i=0; i<car_name.length; i++){
             if(isCorrectLength(car_name[i])){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("자동차 이름 입력 길이가 잘못됨.");
             }
         }
 
         if(!isNumeric(repeat)){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("반복횟수가 숫자가 아님.");
         }
     }
     private boolean isCorrectLength(String car_name){
@@ -29,6 +28,11 @@ public class RacingcarModel {
 
     private boolean isNumeric(String user_number) {
         return user_number.matches("\\d+");
+    }
+
+    private String[] SliceByComma(String car_names){
+        car_names = car_names.replaceAll("\\s", "");
+        return car_names.split(",");
     }
 
 }
