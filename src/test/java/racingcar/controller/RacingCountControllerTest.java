212398a5678cf157f262d704f33f99d24c46c333
k9want <zndwlrnjr26@daum.net>
2023-10-30 06:30:34 +0900

    test: 랜덤한 수별로 이동 처리하기

--- src/test/java/racingcar/controller/RacingCountControllerTest.java
+++ src/test/java/racingcar/controller/RacingCountControllerTest.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +19,21 @@ class RacingCountControllerTest {
 
         //then
         assertThat(result).isEqualTo(100);
+    }
+
+    @Test
+    public void testManageCarMovement() {
+        // Given
+        RacingController controller = new RacingController();
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+
+        // When
+        controller.manageCarMovement(car1, 4); // move
+        controller.manageCarMovement(car2, 3); // stop
+
+        // Then
+        assertThat(car1.getPosition()).isEqualTo(1);
+        assertThat(car2.getPosition()).isEqualTo(0);
     }
 }
