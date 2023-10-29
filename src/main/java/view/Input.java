16606c9b6d4fbc6382152879ca5c1c14e3197b06
Knight <kangrack333@gmail.com>
2023-10-30 02:19:09 +0900

    feat : 자동차 이름입력 메서드 추가         - n 대 이름 입력가능         - 이름은 , 로 구분         - 자동차 이름은 5자 이하

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -6,18 +6,15 @@ import java.util.List;
 
 public class Input {
 
-
     public static List<String> carName() {
+
         String[] carNames = Console
                 .readLine()
                 .split(",");
         Exception.nameVerification(carNames);
         System.out.println(Arrays.toString(carNames));
         return Arrays.asList(carNames);
-    }
 
-    public static void main(String[] args) {
-        carName();
     }
 
 }
