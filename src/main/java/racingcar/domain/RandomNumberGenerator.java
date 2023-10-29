1065107678d99e44ae1b0d8f975d153677b1fe85
PARK9898 <wjddjs21@naver.com>
2023-10-29 22:07:33 +0900

    "feat: 랜덤 숫자 생성 class 구현"

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int MAX_NUMBER = 9;
+    private static final int MIN_NUMBER = 0;
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
