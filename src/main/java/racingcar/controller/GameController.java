7363295fbf8ef709e5a8e5bd7aa363803030a71d
suihub <qkdcrt@gmail.com>
2023-10-29 15:22:12 +0900

    <feat>: "Controller의 게임 진행, 변환 기능 구현"
    컨트롤러의 역할에 맞도록 다음과 같은 기능을 추가.
    
    1. service의 체크 결과에 따라 차를 전진시키는 기능 구현
    2. 자동차들의 이름을 배열로 변환시켜 반환하는 기능 구현
    3. 자동차들의 위치가 "--"된 결과를 배열에 담아 반환하는 기능 구현("--" 변환은 domain이 담당)
    4. 주어진 시도 횟수까지 위 메서드들을 호출하는 진행 담당 메서드 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,7 +3,9 @@ package racingcar.controller;
 import racingcar.domain.Car;
 import racingcar.exception.CountException;
 import racingcar.exception.NameException;
+import racingcar.service.GameService;
 import racingcar.view.PlayerInput;
+import racingcar.view.ResultOutput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.List;
 public class GameController {
     private final List<Car> carList = new ArrayList<>();
     private final PlayerInput input = new PlayerInput();
+    private final ResultOutput output = new ResultOutput();
+    private final GameService service = new GameService();
     private Integer tryCount;
 
     public void start() {
@@ -31,5 +35,41 @@ public class GameController {
         }
 
         this.tryCount = Integer.parseInt(number);
+
+        gameProcess();
+    }
+
+    private void gameProcess() {
+        int count = 0;
+
+        output.printResult();
+
+        while (count < tryCount) {
+            carMove();
+            String[] names = getCarNames();
+            String[] locations = getCarLocations();
+            output.printRacing(names, locations);
+            count += 1;
+        }
+    }
+
+    private void carMove() {
+        for (Car car : carList) {
+            if (service.checkMovePossible()) {
+                car.moveForward();
+            }
+        }
+    }
+
+    private String[] getCarNames() {
+        return carList.stream()
+                .map(Car::getName)
+                .toArray(String[]::new);
+    }
+
+    private String[] getCarLocations() {
+        return carList.stream()
+                .map(Car::getLocationResult)
+                .toArray(String[]::new);
     }
 }
