aa5edb8cb8f626201419d413ad5301d28c2fafa5
ikjo39 <chi4321@naver.com>
2023-10-29 18:35:28 +0900

    feat: 예외 메세지 enum 생성

---
+++ src/main/java/racingcar/constant/InputError.java
@@ -0,0 +1,13 @@
+package racingcar.constant;
+
+public enum InputError {
+    BLANK_CAR_NAME("자동차 이름은 비어있을 수 없습니다."),
+    EXCEEDED_LIMIT_CAR_LENGTH("자동차 이름은 제한 길이를 초과할 수 없습니다."),
+    NOT_DIGIT_MOVING_COUNT("이동 횟수는 숫자만 가능합니다.");
+
+    private final String message;
+
+    InputError(String message) {
+        this.message = message;
+    }
+}
