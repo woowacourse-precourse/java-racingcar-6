0df9f9cf5b112439f0cbbcd0ecc33fad70f229f9
lzlkolo <lzlko1209@gmail.com>
2023-10-30 08:33:38 +0900

    test: Use parameterized test

--- src/test/java/racingcar/utilTest/InputValidationTest.java
+++ src/test/java/racingcar/utilTest/InputValidationTest.java
@@ -4,54 +4,57 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.util.InputValidation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class InputValidationTest {
-    @Test
-    void 정상_입력() {
-        assertDoesNotThrow(() -> InputValidation.validateInput("tayo"));
-        assertDoesNotThrow(() -> InputValidation.validateInput("tayo,gani"));
+    @ParameterizedTest
+    @ValueSource(strings = {"tayo", "gani"})
+    void 자동차_이름_정상_입력_테스트(String test) {
+        assertDoesNotThrow(() -> InputValidation.validateInput(test));
     }
 
-    @Test
-    void 문자열_처음에_숫자_입력_시_예외_처리() {
-        assertThatThrownBy(() -> InputValidation.validateInput("2tayo"))
+    @ParameterizedTest
+    @ValueSource(strings = {"tayo1", "1tayo", "ta1yo"})
+    void 자동차_이름에_숫자가_입력된_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateInput(test))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 문자열_사이에_숫자_입력_시_예외_처리() {
-        assertThatThrownBy(() -> InputValidation.validateInput("ta2yo"))
+    @ParameterizedTest
+    @ValueSource(strings = {"tayo!", "tayo*", "tayo-", "ta_yo", "tayo~", "?"})
+    void 자동차_이름에_쉼표_외_특수문자가_입력된_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateInput(test))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 문자열_마지막에_숫자_입력_시_예외_처리() {
-        assertThatThrownBy(() -> InputValidation.validateInput("tayo2"))
+    @ParameterizedTest
+    @ValueSource(strings = "")
+    void 자동차_이름에_공백이_입력된_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateInput(test))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 쉼표_외_특수문자_입력_시_예외_처리() {
-        assertThatThrownBy(() -> InputValidation.validateInput("tayo!"))
+    @ParameterizedTest
+    @ValueSource(strings = {"tayooo", "gani"})
+    void 자동차_이름이_5자_초과한_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateCarNames(List.of(test)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 공백_입력_시_예외_처리() {
-        assertThatThrownBy(() -> InputValidation.validateInput(""))
+    @ParameterizedTest
+    @ValueSource(strings = {"tayo", "tayo"})
+    void 자동차_이름이_중복되는_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateCarNames(List.of(test)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 자동차_이름이_5자_초과_시_예외_처리() {
-        List<String> test = new ArrayList<>();
-        test.add("tayooo");
-        assertThatThrownBy(() -> InputValidation.validateCarNames(test))
+    @ParameterizedTest
+    @ValueSource(strings = "tayo")
+    void 자동차_이름을_하나만_입력한_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateCarNames(List.of(test)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -59,6 +62,13 @@ class InputValidationTest {
     @ValueSource(strings = {"1"})
     void 이동횟수_정상_입력_테스트(String test) {
         assertDoesNotThrow(() -> InputValidation.validateMoveCount(test));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"two"})
+    void 이동횟수_입력_시_숫자가_아닌_경우_예외_처리(String test) {
+        assertThatThrownBy(() -> InputValidation.validateMoveCount(test))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
