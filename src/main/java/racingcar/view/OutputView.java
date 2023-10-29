5bd0cffcfeea47cbdf4bfe689c5edd053b1e1e15
Gyeongho Yang <chchch1213@naver.com>
2023-10-30 07:45:14 +0900

    feat(view): 실행 결과 제목과 자동차 상태를 표시 하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,12 +2,19 @@ package racingcar.view;
 
 import racingcar.global.GeneralMessage;
 
+import static racingcar.global.GeneralMessage.EXECUTION_RESULT;
+
 public class OutputView {
     public static void printGeneralMessage(GeneralMessage generalMessage) {
         System.out.println(generalMessage.getMessage());
     }
 
-    public static void printMessage(String message) {
-        System.out.println(message);
+    public static void printResultTitle() {
+        System.out.println();
+        printGeneralMessage(EXECUTION_RESULT);
+    }
+
+    public static void printStatus(String status) {
+        System.out.println(status + "\n");
     }
 }
