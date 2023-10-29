97dfa01e80365f82da0b71e8f94fd2e145f77a50
DESKTOP-FH4O6OD\syt11 <syt1128@gmail.com>
2023-10-30 00:25:36 +0900

    :fix: Applicaton 객체 생성 및 기능 구현
    Application에 GamePlaying 의 객체를 생성하고 GamePlaying에서 전체적인 게임의 진행을 담당하는 playing() 메소드를 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+
+import racingcar.controller.GamePlaying;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GamePlaying gamePlaying = new GamePlaying();
+        gamePlaying.playing();
     }
 }
