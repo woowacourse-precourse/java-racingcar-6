ead80a72dcea2781ecd615fcd3d79582e15c2deb
beomsic <qjatjr29@khu.ac.kr>
2023-10-30 02:26:10 +0900

    :test_tube: Test: Add test Result of cars when racing game is played
    - 레이싱 게임 play시 참여한 모든 자동차가 움직이거나 정지한 결과에 대한 Result 객체로 Return하는지 테스트하는 코드 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -5,12 +5,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
+import racingcar.model.Result;
 
 @DisplayName("레이싱 게임 테스트")
 class RacingGameTest {
@@ -40,5 +44,35 @@ class RacingGameTest {
 
         // then
         assertThatThrownBy(() -> RacingGame.of("")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputCarNames")
+    @DisplayName("Racing Game play시 레이싱 게임에 참여하는 모든 자동차가 움직이거나 정지한 결과인 Result를 return 한다.")
+    void play_AllCarsHaveMovedOrStopped(String inputNameString, int count, String[] names) {
+        // given
+        RacingGame racingGame = RacingGame.of(inputNameString);
+
+        // when
+        List<Result> results = racingGame.play();
+
+        List<String> resultNames = results.stream()
+                .map(Result::name)
+                .toList();
+
+        // then
+        assertAll(
+                () -> assertThat(results).isNotEmpty(),
+                () -> assertThat(results).hasSize(count),
+                () -> assertThat(resultNames).containsExactlyInAnyOrder(names)
+        );
+    }
+
+    private static Stream<Arguments> inputCarNames() {
+        return Stream.of(
+                Arguments.of("beom,pobi,woni", 3, new String[] {"beom", "pobi", "woni"}),
+                Arguments.of("beom,pobi,woni,jun", 4, new String[] {"beom", "pobi", "woni", "jun"}),
+                Arguments.of("beom", 1, new String[] {"beom"})
+        );
     }
 }
