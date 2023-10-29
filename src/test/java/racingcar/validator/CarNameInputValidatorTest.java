13a9dca9bcadcc691fe0a9968c69bd5712ded3ae
seminss <2007ksm@naver.com>
2023-10-29 20:20:39 +0900

    test: 자동차 이름 입력 검증 테스트
    CarNameInputValidator: 각 예외 상황에 대한 긍정 부정 테스트 진행

---
+++ src/test/java/racingcar/validator/CarNameInputValidatorTest.java
@@ -0,0 +1,67 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarNameInputValidatorTest {
+
+    private final CarNameInputValidator validator = new CarNameInputValidator();
+
+    /**
+     * positive test
+     */
+    @Test
+    void validateAndGetCarNames_공백_없이_쉼표_구분() {
+        String userInput = "pobi,woni,jun";
+        List<String> carNames = validator.validateAndGetCarNames(userInput);
+        System.out.println(carNames);
+        assertThat(carNames).containsExactly("pobi", "woni", "jun");
+    }
+
+    @Test
+    void validateAndGetCarNames_공백_있고_쉼표_구분() {
+        String userInput = "pobi     , woni,   jun";
+        List<String> carNames = validator.validateAndGetCarNames(userInput);
+        assertThat(carNames).containsExactly("pobi", "woni", "jun");
+    }
+
+    @Test
+    void validateAndGetCarNames_한글이름() {
+        String userInput = "안녕,나는,세민";
+        List<String> carNames = validator.validateAndGetCarNames(userInput);
+        System.out.println(carNames);
+        assertThat(carNames).containsExactly("안녕", "나는", "세민");
+    }
+
+    /**
+     * negative test
+     */
+    @Test
+    void validateAndGetCarNames_입력이_없을때_검증() {
+        String userInput = "";
+        assertThrows(IllegalArgumentException.class, () -> validator.validateAndGetCarNames(userInput));
+    }
+
+    @Test
+    void validateAndGetCarNames_잘못된_구분자_검증() {
+        String userInput = "hello/my/name/is/semin";
+        assertThrows(IllegalArgumentException.class, () -> validator.validateAndGetCarNames(userInput));
+    }
+
+    @Test
+    void validateAndGetCarNames_잘못된_이름_길이_검증() {
+        String userInput = "77777777,333,999999999";
+        assertThrows(IllegalArgumentException.class, () -> validator.validateAndGetCarNames(userInput));
+    }
+
+    @Test
+    void validateAndGetCarNames_중복된_이름_검증() {
+        String userInput = "semin,semin,hi";
+        assertThrows(IllegalArgumentException.class, () -> validator.validateAndGetCarNames(userInput));
+    }
+
+}
