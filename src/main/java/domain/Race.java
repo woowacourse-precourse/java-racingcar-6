f9cf49c4d8783c19ca4e7242c5f70aeca7983a2b
mumpuris <hst990124@naver.com>
2023-10-29 23:28:00 +0900

    Race 클래스 싱글톤 패턴 적용

---
+++ src/main/java/domain/Race.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private static Race instance = new Race();
+
+    private List<Car> cars;
+    private int moveCount;
+
+    private Race() {
+        this.cars = new ArrayList<>();
+        this.moveCount = 0;
+    }
+
+    public static Race getInstance() {
+        if (instance == null) {
+            instance = new Race();
+        }
+        return instance;
+    }
+}
