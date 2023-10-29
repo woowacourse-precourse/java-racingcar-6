22b9509d9cbb8218bc60c7f0864c1e2b0e0d492c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-30 01:27:00 +0900

    feat(Winner): 우승자를 반환하는 기능 구현

---
+++ src/main/java/racingcar/domain/game/Winner.java
@@ -0,0 +1,37 @@
+package racingcar.domain.game;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+
+public class Winner {
+    private static final String DELIMITER = ",";
+    private final String winners;
+
+    public Winner(Cars cars) {
+        this.winners = judgeWinner(cars);
+    }
+
+    public String judgeWinner(Cars cars) {
+        int maxPosition = getMaxPosition(cars);
+        List<Car> target = cars.getCars();
+        return target.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.joining(DELIMITER));
+    }
+
+    public int getMaxPosition(Cars cars) {
+        List<Car> target = cars.getCars();
+        return target.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    @Override
+    public String toString() {
+        return winners;
+    }
+}
