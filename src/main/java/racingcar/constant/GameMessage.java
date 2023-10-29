e7cc7a99c21f2e50036d50a63646f160846332b6
hgh1472 <hgh1472@naver.com>
2023-10-29 17:33:30 +0900

    feat : 상수 메세지 추가

---
+++ src/main/java/racingcar/constant/GameMessage.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class GameMessage {
+    private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+}
