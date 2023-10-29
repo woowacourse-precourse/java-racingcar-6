d6971cbf2bc9aa5d28e76c4f71bef45c60a1dccc
becooq81 <becooq81@gmail.com>
2023-10-30 06:28:08 +0900

    feat(Game): added necessary methods to pass the test

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,4 +1,27 @@
 package racingcar.model;
 
-public class Trial {
+import java.util.List;
+
+public class Game {
+    private int order = 0;
+    private final List<Car> cars;
+
+    private final int numGames;
+
+    public Game(int numGames, List<Car> cars) {
+        this.cars = cars;
+        this.numGames = numGames;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void increaseGameOrder() {
+        this.order++;
+    }
+
+    public boolean checkGamesExhausted() {
+        return this.order >= this.numGames;
+    }
 }
