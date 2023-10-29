78f3a11e4bb7c051c4e0d93f3c8966853607091d
soozzang <soozzang@inha.edu>
2023-10-29 16:16:30 +0900

    Feat(RacingException): validateCarNameLen() 추가

---
+++ src/main/java/racingcar/exception/RacingException.java
@@ -0,0 +1,14 @@
+package racingcar.exception;
+
+import java.util.ArrayList;
+
+public class RacingException {
+
+        public void validateCarNameLen(String carNames) {
+            for (String s : carNames.split(",")) {
+                if (s.length() > 5) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+}
