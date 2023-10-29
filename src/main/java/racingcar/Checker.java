2f81f1b74bff7660f05daf39c4d756d28e2a5d51
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-10-30 00:20:33 +0900

    feat(judgeWinners): 기능 - 4 구현 및 클래스 - 2 완료
    기능 - 4 구현
    - 클래스 - 2에 추가
    - judgeWinners() 구현
    - judgeWinners() 테스트 코드 작성

--- src/main/java/racingcar/Checker.java
+++ src/main/java/racingcar/Checker.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Checker {
     public static Boolean isNameAllCorrect(List<String> names) {
@@ -11,5 +13,21 @@ public class Checker {
         }
 
         return true;
+    }
+
+    public static List<String> judgeWinners(List<Car> cars) {
+        Integer highestScore = cars.stream()
+                .mapToInt(car -> car.getScore())
+                .max().orElseThrow(() -> new NoSuchElementException());
+
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getScore() == highestScore) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
     }
 }
--- src/test/java/racingcar/CheckerTest.java
+++ src/test/java/racingcar/CheckerTest.java
@@ -16,4 +16,15 @@ public class CheckerTest {
         assertThat(Checker.isNameAllCorrect(correctNames)).isTrue();
         assertThat(Checker.isNameAllCorrect(incorrectNames)).isFalse();
     }
+
+    @Test
+    void score가_가장_높은_car의_이름_리턴() {
+        List<Car> cars = Arrays.asList(new Car("car1", 3), new Car("car2", 3), new Car("car3", 1));
+
+        List<String> winners = Checker.judgeWinners(cars);
+
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(winners.contains("car1")).isTrue();
+        assertThat(winners.contains("car2")).isTrue();
+    }
 }
