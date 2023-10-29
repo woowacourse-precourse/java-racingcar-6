e567b3f616fd440311a4156712e08874ad15fc65
JHyun0302 <wogustkfkd11@gmail.com>
2023-10-29 12:17:14 +0900

    :sparkles: [Feat] 정적 메시지 모음

---
+++ src/main/java/racingcar/constant/constantMessage.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class constantMessage {
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String Not_Integer_VALUE_ERROR_MESSAGE = "시도할 회수는 숫자로만 입력 가능합니다.";
+    public static final String NEGATIVE_VALUE_ERROR_MESSAGE = "시도할 회수는 0이상의 숫자여야 합니다.";
+    public static final String RESULT_MESSAGE = "실행 결과";
+}
