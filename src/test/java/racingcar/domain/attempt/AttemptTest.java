f34459216032c5d5142b13ff51178e9dc9bea2f0
wwan13 <wwan13@naver.com>
2023-10-29 20:09:55 +0900

    test : `Attempt` 테스트 코드 작성 - 정수(시도 횟수)를 통해 생성할 수 있다. - 입력받은 값이 1보다 작으면 IllegalArgumentException을 발생한다. - use() 가 한 번의 시도 횟수를 사용한다. - hasNext() 가 시도 횟수가 남아있다면 true를 반환한다. - hasNext() 가 남은 시도 횟수가 없다면 false를 반환한다.

---
+++ src/test/java/racingcar/domain/attempt/AttemptTest.java
@@ -0,0 +1,82 @@
+package racingcar.domain.attempt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AttemptTest {
+
+    @Test
+    @DisplayName("정수(시도 횟수)를 통해 생성할 수 있다.성")
+    void canConstructByInt() {
+        // given
+        int value = 3;
+
+        // when
+        Attempt attempt = new Attempt(value);
+
+        // then
+        assertThat(attempt).isExactlyInstanceOf(Attempt.class);
+    }
+
+    @ParameterizedTest(name = "[{index}] : {0}")
+    @ValueSource(ints = {0, -1, -2})
+    @DisplayName("입력이 1 이하일 경우 IllegalArgumentException을 발생한다.")
+    void throwsIllegalArgumentException_whenInputIsUnder1(final int input) {
+        final String ERROR_MESSAGE_TOO_LOW = "최소 1 이상의 값을 입력해 주세요.";
+
+        // when, then
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+            () -> new Attempt(input));
+        assertThat(e.getMessage()).isEqualTo(ERROR_MESSAGE_TOO_LOW);
+    }
+
+    @Test
+    @DisplayName("use() 가 한 번의 시도 횟수를 사용한다.")
+    void useOneAttempt_whenUse() {
+        // given
+        int value = 1;
+        Attempt attempt = new Attempt(value);
+
+        // when
+        attempt.use();
+
+        // then
+        assertThat(attempt.hasNext()).isFalse();
+    }
+
+    @Test
+    @DisplayName("hasNext() 가 시도 횟수가 남아있다면 true를 반환한다.")
+    void hasNextReturnTrue_whenAttemptRemain() {
+        // given
+        int value = 3;
+        Attempt attempt = new Attempt(value);
+        attempt.use();
+
+        // when
+        boolean result = attempt.hasNext();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("hasNext() 가 남은 시도 횟수가 없다면 false를 반환한다.")
+    void hasNextReturnFalse_whenAttemptNotRemain() {
+        // given
+        int value = 2;
+        Attempt attempt = new Attempt(value);
+        attempt.use();
+        attempt.use();
+
+        // when
+        boolean result = attempt.hasNext();
+
+        // then
+        assertThat(result).isFalse();
+    }
+}
