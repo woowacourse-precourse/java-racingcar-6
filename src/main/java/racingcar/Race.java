084b19911b7ec0165353761030744f265718337d
eunjin3481 <eunjin3481@gmail.com>
2023-10-29 17:57:30 +0900

    feat(Race): 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Race {
     private int moveNum;
 
@@ -16,7 +18,11 @@ public class Race {
         displayWinner();
     }
 
-    private void inputCarNames() {
+    private String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = readLine();
+        return input.split(",");
+    }
 
     }
 
