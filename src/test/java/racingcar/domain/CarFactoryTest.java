708d38f641eafdf3c1758a3ca9e44df839501452
DH CHOI <bluetifulc@gmail.com>
2023-10-29 15:48:59 +0900

    test: 이름 중복 검사 로직 테스트

--- src/test/java/racingcar/domain/CarFactoryTest.java
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -1,9 +1,10 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import domain.CarFactory;
 import domain.Car;
+import domain.CarFactory;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.support.StubCarEngine;
@@ -23,5 +24,18 @@ public class CarFactoryTest {
         // then
         final List<String> 차_이름 = cars.stream().map(Car::getName).toList();
         assertThat(차_이름).containsExactlyInAnyOrder(첫번째_차_이름, 두번째_차_이름);
+    }
+
+
+    @Test
+    void 중복된_이름이_있다면_예외를_던진다() {
+        // given
+        final String 첫번째_차_이름 = "차_이름1";
+        final String 두번째_차_이름 = "차_이름1";
+        final CarFactory carFactory = new CarFactory(new StubCarEngine());
+
+        // when & then
+        assertThatThrownBy(() -> carFactory.generateCars(List.of(첫번째_차_이름, 두번째_차_이름)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
