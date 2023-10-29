b28817fe7a3b30a6fc26b2b18f79ee9495b5abc7
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-29 15:49:29 +0900

    feat(racingcar): outline the whole process of game
    - for the number of gameNumber decide whether to go one step or stop and save it to a log

--- src/main/java/racingcar/domain/ImplementGame.java
+++ src/main/java/racingcar/domain/ImplementGame.java
@@ -1,4 +1,29 @@
 package racingcar.domain;
+
+import racingcar.domain.CreateMoveMap;
+import racingcar.domain.UserInput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.Scanner;
 public class ImplementGame {
+    UserInput userInput = new UserInput();
+    CreateMoveMap createMoveMap = new CreateMoveMap();
+
+    public Map<String, Integer> playGame(){
+        List<String> carList = userInput.askCar();
+        int gameNumber = userInput.askNumberGame();
+        System.out.println("경기 실행 결과:");
+        Map<String, Integer> numMoveMap = createMoveMap.initMap(carList);
+        for (int i = 0; i < gameNumber; i++){
+            numMoveMap = createMoveMap.decideIncMove(carList, numMoveMap);
+        }
+        return numMoveMap;
+    }
 
 }
