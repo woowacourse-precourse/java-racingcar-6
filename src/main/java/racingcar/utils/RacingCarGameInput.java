5d12599be057f67e7af2b23258337ce2c59f74bc
alscjf1329 <alscjf1329@gmail.com>
2023-10-29 20:07:15 +0900

    feat: validateCarNames 메소드 추가
    자동차 이름이 5자 넘으면 IllegalArgumentException을 던져주는 메소드를 추가했습니다.

--- src/main/java/racingcar/utils/RacingCarGameInput.java
+++ src/main/java/racingcar/utils/RacingCarGameInput.java
@@ -6,10 +6,20 @@ import java.util.List;
 import racingcar.dto.RacingCar;
 
 public class RacingCarGameInput {
+    public static String ERROR_MESSAGE_BIGGER_THAN_5CHAR = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
     public static List<RacingCar> readCarNames() {
         String[] arr = Console.readLine().split(",");
         return Arrays.stream(arr)
                 .map(RacingCar::new)
                 .toList();
+    }
+
+    public static boolean validateCarNames(List<RacingCar> carList) {
+        for (RacingCar car : carList) {
+            if (car.getName().length() > 5) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_BIGGER_THAN_5CHAR);
+            }
+        }
+        return true;
     }
 }
