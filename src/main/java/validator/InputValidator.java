9147908eae6f1e4841c07c0ff605e1cc494ed20d
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-29 14:33:27 +0900

    fix: 문자열의 길이가 0보다 클 때만 실행되게 수정

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -54,7 +54,7 @@ public class InputValidator {
     }
 
     private void isNameEndWithComma(String input) {
-        if(input.charAt(input.length()-1) == ',') {
+        if(input.length() > 0 && input.charAt(input.length()-1) == ',') {
             throw new IllegalArgumentException(INPUT_CONTAIN_BLANK);
         }
     }
