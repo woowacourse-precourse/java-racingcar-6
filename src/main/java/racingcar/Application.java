a69965fe8d8ad5ed4f5a043b06c254279e4a243a
ETERNAL <fldzm7905@gmail.com>
2023-10-29 22:11:16 +0900

    feat: 최종 우승자 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        OutputView outputView = new OutputView();
+        outputView.printWinners(Arrays.asList("pobi", "jun"));
     }
 }
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,20 +1,28 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputView {
-
-    public String printStartMessage() {
-        return null;
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_NUM_OF_ATTEMPTS_MESSGE = "시도할 회수는 몇회인가요?";
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
     }
 
-    public String askNumOfAttemptsMessage() {
-        return null;
+    public void askNumOfAttemptsMessage() {
+        System.out.println(ASK_NUM_OF_ATTEMPTS_MESSGE);
     }
 
-    public String printProgress() {
-        return null;
+    public void printProgress() {
     }
 
-    public void printWinners() {
+    public void printWinners(List<String> winners) {
+        String win = "";
+        System.out.print("최종 우승자 : ");
+        for (String winner : winners) {
+            win +=  winner;
+        }
     }
 
 }
