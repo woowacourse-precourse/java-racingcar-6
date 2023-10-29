a3fb764ac4093e6121c9b940fb2f028422259515
edgar6bf <edgar6bf@gmail.com>
2023-10-29 19:53:17 +0900

    test: CarFactoryTest & CarTest 로직 수정
    기존 CarFactory에서 수행하던 자동차 객체 생성과 생성 예외 테스트는 CarTest에서 진행하는 게 더 적절하다고 판단하여 테스트 로직을 이전함.
    CarFactory의 자동차 초기화 테스트에서 기대 부분에 생성된 자동차 리스트의 요소 필드 값을 검증하는 것보다는 리스트 길이를 검증하는 게 더 적절하다고 판단되어 테스트 로직을 수정함.

--- src/test/java/racingcar/model/CarFactoryTest.java
+++ src/test/java/racingcar/model/CarFactoryTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class CarFactoryTest {
 
-    @DisplayName("자동차들의 이름을 넘기면, 각각의 이름을 가진 Car 객체를 생성한다.")
+    @DisplayName("자동차 이름 목록을 전달하면, 목록 길이만큼의 자동차 객체를 생성한다.")
     @Test
     void Given_CarNames_When_InitCars_Then_Cars() throws Exception {
         // Given
@@ -17,33 +17,8 @@ class CarFactoryTest {
 
         // When
         List<Car> cars = CarFactory.initCars(carNames);
-        String firstCarName = cars.get(0).getName();
 
         // Then
-        assertThat(firstCarName).isEqualTo(carNames.get(0));
-    }
-
-    @DisplayName("공백의 자동차 이름을 넘기면, 예외가 발생한다.")
-    @Test
-    void Given_EmptyCarName_When_InitCars_Then_ThrowException() throws Exception {
-        // Given
-        List<String> carNames = List.of("", "elmo", "kermit");
-
-        // When & Then
-        assertThatThrownBy(() -> CarFactory.initCars(carNames))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효하지 않은 자동차 이름입니다.");
-    }
-
-    @DisplayName("최대 유효 길이를 초과하는 자동차 이름을 넘기면, 예외가 발생한다.")
-    @Test
-    void Given_SoLongCarName_When_InitCars_Then_ThrowException() throws Exception {
-        // Given
-        List<String> carNames = List.of("soLongLongName", "elmo", "kermit");
-
-        // When & Then
-        assertThatThrownBy(() -> CarFactory.initCars(carNames))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효하지 않은 자동차 이름입니다.");
+        assertThat(cars).hasSize(carNames.size());
     }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,11 +2,59 @@ package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.CarStatus;
+import racingcar.util.ProductionRandomNumberUtil;
 import racingcar.util.RandomNumberUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
+
+    @DisplayName("자동차 이름을 넘기면, 그 이름을 가진 자동차 객체가 생성된다.")
+    @Test
+    void Given_CarName_When_InitCar_Then_CarCreate() throws Exception {
+        // Given
+        String carName = "edgar";
+        RandomNumberUtil randomNumberUtil = new ProductionRandomNumberUtil();
+        int initialPosition = 0;
+
+        // When
+        Car car = new Car(randomNumberUtil, carName, initialPosition);
+        CarStatus status = car.getStatus();
+
+        // Then
+        assertThat(status.carName()).isEqualTo(carName);
+        assertThat(status.position()).isEqualTo(initialPosition);
+    }
+
+    @DisplayName("공백의 자동차 이름을 넘기면, 예외가 발생한다.")
+    @Test
+    void Given_EmptyCarName_When_InitCars_Then_ThrowException() throws Exception {
+        // Given
+        String emptyCarName = "";
+        RandomNumberUtil randomNumberUtil = new ProductionRandomNumberUtil();
+        int initialPosition = 0;
+
+        // When & Then
+        assertThatThrownBy(() -> new Car(randomNumberUtil, emptyCarName, initialPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 자동차 이름입니다.");
+    }
+
+    @DisplayName("최대 유효 길이를 초과하는 자동차 이름을 넘기면, 예외가 발생한다.")
+    @Test
+    void Given_SoLongCarName_When_InitCars_Then_ThrowException() throws Exception {
+        // Given
+        String soLongCarName = "abcdefghizklmkdjefefekfjekfjekfjekfjekfjek";
+        RandomNumberUtil randomNumberUtil = new ProductionRandomNumberUtil();
+        int initialPosition = 0;
+
+        // When & Then
+        assertThatThrownBy(() -> new Car(randomNumberUtil, soLongCarName, initialPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 자동차 이름입니다.");
+    }
 
     @DisplayName("자동차가 전진하면, 자동차의 위치값이 증가한다.")
     @Test
