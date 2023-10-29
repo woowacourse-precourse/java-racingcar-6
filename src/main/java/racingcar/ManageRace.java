1dada31d6abb85f504cfb5766a8a723a11e1f62e
Happeeee <cmg118@naver.com>
2023-10-30 03:27:14 +0900

    Feat: 자동차 이름 예외 검사 기능 구현
    중복된 이름, 5자 이상

--- src/main/java/racingcar/ManageRace.java
+++ src/main/java/racingcar/ManageRace.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ManageRace {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -18,5 +20,17 @@ public class ManageRace {
             carNameList.add(carName);
         }
         return carNameList;
+    }
+
+    private void checkValidNames(List<String> carNameList) {
+        Set<String> set = new HashSet(carNameList);
+        if (carNameList.size() != set.size()) {
+            throw new IllegalArgumentException();
+        }
+        for (String carName : set) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
