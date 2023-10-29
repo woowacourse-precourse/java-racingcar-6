8869a8ced7327840ec2e85943232bc8a0f37ed9a
minseo  jo <liging12@naver.com>
2023-10-30 01:13:49 +0900

    refactor: 상수 명 변경

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -5,13 +5,13 @@ import racingcar.util.RandomNumberGenerator;
 public class Race {
 
     private static final int MIN_FORWARD_THRESHOLD = 4;
-    private static final int FORWARD_COUNT = 1;
+    private static final int ONE_STEP = 1;
 
     public void runOneRound(final Cars cars) {
         for (Car car : cars.cars()) {
             int randomValue = RandomNumberGenerator.generateRandomNumber();
             if (randomValue >= MIN_FORWARD_THRESHOLD) {
-                car.forward(FORWARD_COUNT);
+                car.forward(ONE_STEP);
             }
         }
     }
