53a5ee8f0caaff2bdb4ac13ff4a5ba016e63f828
박현수 <pm6567@naver.com>
2023-10-30 02:58:56 +0900

    feat : 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.startGame();
+    }
+
+    private static final String CarNamesAlert = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String NumberTryAlert = "시도할 회수는 몇 회인가요?";
+    private Map<String, String> carStatus;
+
+    public void startGame(){
+        System.out.println(CarNamesAlert);
+        String name =readLine();
+        String[] carNames = name.split(",");
     }
 }
