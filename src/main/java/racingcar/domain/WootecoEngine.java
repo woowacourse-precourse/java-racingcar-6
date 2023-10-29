777de45e6fceebe9beb8e61f01cbdfac4d6588f0
june-777 <wlwhswnsrl96@gmail.com>
2023-10-29 21:42:23 +0900

    feat(WootecoEngine): Engine 인터페이스를 구현한 구현체. 0 ~ 9 사이의 무작위 값을 생성하는 기능

---
+++ src/main/java/racingcar/domain/WootecoEngine.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class WootecoEngine implements Engine {
+    private int startInclusive = 0;
+    private int endInclusive = 9;
+
+    @Override
+    public int generateNumber() {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+}
