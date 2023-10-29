92faf3d27d7ac8f82c9e56c47930b201348d2e8e
bbbbooo <qlqlzbxkzl@naver.com>
2023-10-29 22:17:01 +0900

    test($CarNameValidationTest) : 테스트 추가
    사용자명을 입력하지 않는 경우 에외처리 테스트 추가함

--- src/test/java/racingcar/utils/CarNameValidationTest.java
+++ src/test/java/racingcar/utils/CarNameValidationTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.utils.CarNameValidation;
@@ -18,7 +19,7 @@ class CarNameValidationTest {
         carNameValidation = new CarNameValidation();
     }
 
-    @DisplayName("사용자가 자동차명을 5글자를 초과하여 입력했을때 예외를 발생하는지")
+    @DisplayName("사용자가 자동차명을 5글자를 초과하여 입력했을 때, 예외를 발생하는지")
     @ParameterizedTest
     @ValueSource(strings = {"pobidd", "pobibb", "pobiee", "pobirr,bbbbooo"})
     void validateUserInput5CharacterTest(String inputCarName) {
@@ -38,5 +39,18 @@ class CarNameValidationTest {
                 () -> carNameValidation.validateUserInput(inputCarName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이름에 숫자가 들어갈 수 없습니다.");
+    }
+
+    @DisplayName("사용자가 자동차명을 입력하지 않았을 때, 예외를 발생하는지")
+    @Test
+    void validateUserInputBlank() {
+        // given
+        String inputCarName = "";
+
+        // when && then
+        assertThatThrownBy(
+                () -> carNameValidation.validateUserInput(inputCarName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값이 존재하지 않습니다.");
     }
 }
