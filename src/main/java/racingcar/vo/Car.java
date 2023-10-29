795f36c597675cca7d283c00a552a9650d41f547
seminss <2007ksm@naver.com>
2023-10-29 21:12:25 +0900

    refactor: 자동차의 전진 횟수를 변수로 저장

--- src/main/java/racingcar/vo/Car.java
+++ src/main/java/racingcar/vo/Car.java
@@ -5,12 +5,24 @@ import java.util.List;
 public class Car {
     private final String name;
     private final List<Boolean> movementFlags;
+    private final int totalForward;
     private int completedMoves;
 
     public Car(String name, List<Boolean> movementFlags){
         this.name=name;
         this.movementFlags = movementFlags;
         this.completedMoves = 0;
+        this.totalForward = calculateTotalForward(movementFlags);
+    }
+
+    private int calculateTotalForward(List<Boolean> movementFlags){
+        return (int) movementFlags.stream()
+                .filter(flag->flag)
+                .count();
+    }
+
+    public int getTotalForward(){
+        return totalForward;
     }
 
     public void incrementMoveCount(){
