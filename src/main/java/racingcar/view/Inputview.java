ac6bcbf9c6485c06ab778219a1062f2f1f5869e2
ohjuyoungg <ejrm12@naver.com>
2023-10-29 14:50:23 +0900

    (feat(race game) : 자동차 경주 Inputview 패키지 Inputview 클래스 뼈대 만들기)

--- src/main/java/racingcar/view/Inputview.java
+++ src/main/java/racingcar/view/Inputview.java
@@ -1,2 +1,25 @@
-package racingcar.view;public class Inputview {
+package racingcar.view;
+
+import java.util.List;
+
+public class Inputview {
+
+    private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GAME_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+
+    public static void startGame() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+    public List<String> inputCarName() {
+        return null;
+    }
+
+    public static void tryGameNumber() {
+        System.out.println(GAME_NUMBER_OF_ATTEMPTS);
+    }
+
+    public int carNumericValue() {
+        return 0;
+    }
 }
