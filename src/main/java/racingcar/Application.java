d1bb9fea541540b15d3f33c56342fce6480832b5
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-30 01:17:38 +0900

    feat: 사용자에게 자동차 이동 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,10 +7,13 @@ import java.util.Map;
 
 public class Application {
 	private static Map<String, Integer> carMap = new LinkedHashMap<>();
+	private static int turnCount;
 
 	public static void main(String[] args) {
 		setCarMap();
 		System.out.println(carMap);
+		setTurnCount();
+		System.out.println(turnCount);
 	}
 
 	public static void setCarMap() {
@@ -20,6 +23,15 @@ public class Application {
 				throw new IllegalArgumentException("자동차 이름이 올바르지 않습니다.");
 			}
 			carMap.put(carName, 0);
+		}
+	}
+
+	public static void setTurnCount() {
+		String input = readLine().trim();
+		try {
+			turnCount = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("숫자만 입력 가능합니다.");
 		}
 	}
 }
