3cb5c3213bd6b3f55c34284ed3469f19b908a4e3
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-29 23:58:51 +0900

    🎯feat(domain / Cars) : 자동차 수 validation 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -10,7 +10,16 @@ import static racingcar.Input.InputView.trialNumber;
 *  중복 검사
 *  */
 public class Cars {
+    public static final int MAX_CAR_NUMBER = 5;
     public static List<Car> cars;
     public Cars(Car car) {
+        validateCarNumbers();
+    }
+
+    // car 5대 이하 check
+    public void validateCarNumbers() {
+        if (cars.size() > MAX_CAR_NUMBER) {
+            throw new IllegalArgumentException("최대 5대까지 참가 가능합니다.");
+        }
     }
 }
