dff9392c17aabb2cb2d7b14ca2d7255f97a6cfa3
jaegyeongkim <dongji_11@naver.com>
2023-10-29 16:29:00 +0900

    feat(GeneratedRandomNumber): 랜덤 변수 생성 기능 구현

---
+++ src/main/java/utils/GenerateRandomNumber.java
@@ -0,0 +1,9 @@
+package utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class GenerateRandomNumber {
+    public static int pick(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
