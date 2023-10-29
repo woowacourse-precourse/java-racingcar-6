7e81c903c7d70db04892dec13113005ace1d09ca
Hoo-sung <gntjd135@gmail.com>
2023-10-29 12:36:22 +0900

    refactor(CarNameVerifierTest): 테스트 메서드 이름 보기 좋게 수정

--- src/test/java/racingcar/verifier/CarNameVerifierTest.java
+++ src/test/java/racingcar/verifier/CarNameVerifierTest.java
@@ -10,13 +10,13 @@ class CarNameVerifierTest {
     private final Verifier carNameVerifier = new CarNameVerifier();
 
     @Test
-    void 정상입력이주어진경우() {
+    void 정상입력이_주어진경우() {
        Assertions.assertThatCode(()-> carNameVerifier.check("pobi,woni,jun"))
                .doesNotThrowAnyException();
     }
 
     @Test
-    void 자동차이름5자이하가아닌경우() {
+    void 자동차이름이_5자이하가_아닌경우() {
         Assertions.assertThatThrownBy(()-> carNameVerifier.check("pobi,woni,123456,ifj"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.INVALID_CAR_NAME);
