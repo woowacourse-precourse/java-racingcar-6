851d0cdd2a2dc2837b61cc27348faff88a86bb88
OhJongMin <jmoh7480@gmail.com>
2023-10-29 18:05:44 +0900

    feat(racingcar):경주 자동차 최대 이름 길이 변수 생성

--- src/main/java/racingcar/domain/Rule.java
+++ src/main/java/racingcar/domain/Rule.java
@@ -5,4 +5,5 @@ public class Rule {
     public final static int standardValue = 4;
     public final static int minNumber = 0;
     public final static int maxNumber = 9;
+    public final static int maxCarName = 5;
 }
