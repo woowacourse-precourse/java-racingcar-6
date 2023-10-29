54423427b440939adfcf27185248dcaee69d8df4
권장순 <jsoonworld@gmail.com>
2023-10-29 09:52:37 +0900

    feat(racingcar): readme file에 있는 요구사항에 있는  RacingRound#input() 를 RacingRound#attemptCount()로 변경하고 RacingRound#attemptCount()를 구현함

--- src/main/java/racingcar/RacingRound.java
+++ src/main/java/racingcar/RacingRound.java
@@ -1,9 +1,20 @@
 package racingcar;
-
+import camp.nextstep.edu.missionutils.Console;
 /**
  * F1 - 44 ~ 78 Lap Round -> Lap
  *
  */
 public class RacingRound {
+    public int attemptCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int attemptCount = Integer.parseInt(Console.readLine());
+        return attemptCount;
+    }
+
+
+
+
+
+
 
 }
