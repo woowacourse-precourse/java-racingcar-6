12663d1fe153dbaf6ee3ecf535e5b1c4f623ac79
otaeho <dhxogh123123@naver.com>
2023-10-29 21:04:43 +0900

    feat(OutputView): 차수별 실행 결과를 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,10 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
+    private static final String EACH_FORWARD_INFORMATION = "-";
     private static final String INPUT_CAR_NAMES_INFO = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_MOVEMENT_TIME_INFO = "시도할 회수는 몇회인가요?";
 
@@ -10,5 +14,19 @@ public class OutputView {
 
     public void printMovementInfoInput() {
         System.out.println(INPUT_MOVEMENT_TIME_INFO);
+    }
+
+    public void printExecutionResultsOrder(List<Car> cars) {
+        StringBuilder executionResult = new StringBuilder();
+        for (Car car : cars) {
+            printForwardInfo(car, executionResult);
+        }
+        System.out.println(executionResult);
+    }
+
+    private void printForwardInfo(Car car, StringBuilder executionResult) {
+        executionResult.append(
+                        String.format("%s : %s", car.getName(), EACH_FORWARD_INFORMATION.repeat(car.getForwards())))
+                .append('\n');
     }
 }
