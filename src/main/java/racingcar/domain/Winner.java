1418cf048071f2563115058a3f6a1e392dbb770d
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-10-29 21:41:30 +0900

    Feat: 우승자 자동차 리스트 생성 함수

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -5,7 +5,14 @@ import java.util.List;
 
 public class Winner {
     public void car(List<String> carNames, List<String> progressResults) {
+        List<String> winner = new ArrayList<>();
         int maxNum = checkMax(progressResults);
+
+         for (int r = 0; r < progressResults.size(); r++) {
+             if(progressResults.get(r).length() == maxNum) {
+                 winner.add(carNames.get(r));
+             }
+         }
     }
 
     public int checkMax(List<String> progressResults) {
