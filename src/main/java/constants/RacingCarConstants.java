2607ccc8f2e1843040f6b5e5409eddec2545d353
pjaehyun <show6756@gmail.com>
2023-10-29 15:04:34 +0900

    feat: 추가 출력문 상수

--- src/main/java/constants/RacingCarConstants.java
+++ src/main/java/constants/RacingCarConstants.java
@@ -3,4 +3,7 @@ package constants;
 public class RacingCarConstants {
     public static String PARTICIPANTS_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n";
     public static String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요? \n";
+
+    public static String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
+    public static String PRINT_DISTANCE_UNIT = "-";
 }
