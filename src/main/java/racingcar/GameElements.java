1ec640c05c89c93c3b8f770af5e2a2f46e79427c
KimSehoon <shggm2000@gmail.com>
2023-10-29 23:20:36 +0900

    feat: GameElements class 내 carMap 생성
    - 자동차의 이름과 전진 횟수를 저장할 carMap 생성
    - carMap에 대한 getCarMap method 구현

--- src/main/java/racingcar/GameElements.java
+++ src/main/java/racingcar/GameElements.java
@@ -1,11 +1,15 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameElements {
     private List<String> carNameList = new ArrayList<>();
     private int tryCount = 0;
+
+    private Map<String, Integer> carMap = new HashMap<>();
 
     public List<String> getCarNameList() {
         return carNameList;
@@ -13,6 +17,10 @@ public class GameElements {
 
     public int getTryCount() {
         return tryCount;
+    }
+
+    public Map<String, Integer> getCarMap() {
+        return carMap;
     }
 
     public void carNamesSetting(List<String> carNameListByUser) {
