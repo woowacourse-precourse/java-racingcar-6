75b9f0fde0a096fdb999474b2a8f7abe7f320312
JongMinCh0i <wonderland314592@gmail.com>
2023-10-30 02:22:39 +0900

    feat(Referee): 레이싱 경기의 최종 우승자를 출력하는 기능

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Referee {
+    private final static String FINAL_WINNER = "최종 우승자 : ";
 
     // Distance 를 기준으로 내림차순 정렬하는 기능
     public List<Car> selectWinnerList(List<Car> list) {
@@ -17,5 +18,18 @@ public class Referee {
             return o1.getDistance().compareTo(o2.getDistance());
         });
         return list;
+    }
+
+    // 우승자 이름을 반환한다.
+    public String winnerName(List<Car> list) {
+        String name = list.get(0).getName();
+        Integer Score = list.get(0).getDistance();
+
+        for (int i = 1; i < list.size(); i++) {
+            if (Score.equals(list.get(i).getDistance())) {
+                name += ", " + list.get(i).getName();
+            }
+        }
+        return FINAL_WINNER + name;
     }
 }
