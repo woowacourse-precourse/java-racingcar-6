af386e4f1cf54c133d2b3b18362005efcc9802b7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-29 17:17:30 +0900

    feat(Cars): 문자열을 Car 리스트로 변환하는 기능 구현

---
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -0,0 +1,22 @@
+package racingcar.domain.car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private static final String DELIMITER = ",";
+    private List<Car> cars;
+
+    public Cars(String cars) {
+        List<Car> targetCars = convertStringToCar(cars);
+        this.cars = targetCars;
+    }
+
+    private List<Car> convertStringToCar(String cars) {
+        String[] tokens = cars.split(DELIMITER);
+        return Arrays.stream(tokens)
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
+    }
+}
