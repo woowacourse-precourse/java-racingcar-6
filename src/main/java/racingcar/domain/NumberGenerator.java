99fe76cc6af6c35c042f66f4b8c9eea3a51c8949
ssunnykku <ssunnykku@gmail.com>
2023-10-29 15:39:01 +0900

    feat(NumberGenerator): 0에서 9사이에 무작위 값을 구하기

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,5 +1,10 @@
 package racingcar.domain;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
-    
+    public int createRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
 }
