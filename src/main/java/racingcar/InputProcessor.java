c8c312ab360868477f49b315971a88a611f391f5
harandal24601 <devchaehokim@gmail.com>
2023-10-30 02:52:47 +0900

    Chore : Refactor code.
    Add and edit condition comparison.
     - check  null status
     - check seperator ',' exist
     - seperate string size check code to method

--- src/main/java/racingcar/InputProcessor.java
+++ src/main/java/racingcar/InputProcessor.java
@@ -1,19 +1,32 @@
 package racingcar;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class InputProcessor {
 
     public static ArrayList<String> carNameInputProcess(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new NullPointerException("String is null or empty. \nerrorVar : " + input);
+        }
         ArrayList<String> carNames = new ArrayList<>();
-        String[] splitInput = input.split(",");
-        for (String carName : splitInput) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        if(input.contains(",")) {
+            String[] splitInput = input.split(",");
+            for (String carName : splitInput) {
+                carName = verifyCarNameOver5(carName);
+                carNames.add(carName);
             }
-            carNames.add(carName);
+        } else {
+            input = verifyCarNameOver5(input);
+            carNames.add(input);
         }
         return carNames;
     }
+
+    public static String verifyCarNameOver5(String input) {
+        if (input.length() > 5) {
+            throw new StringIndexOutOfBoundsException("String index out of range: 5 \nerrorVar : " + input);
+        }
+        return input;
+    }
+    
 }
