e44e4f14f441aeedd9d4bb6fd5a7f74b5d6d4181
Sanghwi Lee <sanghwi47@naver.com>
2023-10-29 12:14:57 +0900

    refactor(CheckNumber) 메소드당 하나의 작업을 수행하도록 코드 구조를 개선

--- src/main/java/racingcar/function/CheckNumber.java
+++ src/main/java/racingcar/function/CheckNumber.java
@@ -2,18 +2,24 @@ package racingcar.function;
 
 public class CheckNumber {
     public static int validate(String inputNumber) {
+        validateOnlyDigits(inputNumber);
+        return validatePositiveNumber(inputNumber);
+    }
+
+    private static void validateOnlyDigits(String inputNumber) {
         for (char character : inputNumber.toCharArray()) {
             if (!Character.isDigit(character)) {
-//                throw new IllegalArgumentException("숫자로 입력 하세요.");
-                throw new IllegalArgumentException("input Number!!!");
+                throw new IllegalArgumentException("숫자로 입력 하세요.");
+//                throw new IllegalArgumentException("input Number!!!");
             }
         }
+    }
+    private static int validatePositiveNumber(String inputNumber){
         int number = Integer.parseInt(inputNumber);
         if (number <= 0) {
-//            throw new IllegalArgumentException("0 이상의 값을 입력하세요");
-            throw new IllegalArgumentException("more than 1!!!!!");
+            throw new IllegalArgumentException("0 이상의 값을 입력하세요");
+//            throw new IllegalArgumentException("more than 1!!!!!");
         }
-
         return number;
     }
 }
