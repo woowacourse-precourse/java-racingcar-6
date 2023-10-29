332bca164a727c83e4ee1aca80a7022df489f36f
ldw <leehan02@hanyang.ac.kr>
2023-10-30 04:03:18 +0900

    feat(racingcar) : 에러메세지들을 상수로 정의한 클래스 구현

---
+++ src/main/java/racingcar/ErrorMessages.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class ErrorMessages {
+    public static final String TOO_LONG_CAR_NAME_ERROR_MESSAGE = "자동차 이름은 5자 이하여야 합니다.";
+    public static final String TOO_LOW_TRY_NUM_ERROR_MESSAGE = "시도 횟수는 최소 1회 이상이어야 합니다.";
+    public static final String NOT_NUMBER_ERROR_MESSAGE = "시도 횟수는 숫자만 입력해야 합니다.";
+    public static final String BLANK_NAME_ERROR_MESSAGE = "자동차 이름은 최소 한 글자 이상의 공백이 아닌 문자여야 합니다.";
+}
