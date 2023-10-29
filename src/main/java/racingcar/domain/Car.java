3f55d965a7177f9c8754657ff61691ab816ac6ff
kkw2758 <dtc3229@gmail.com>
2023-10-29 13:03:58 +0900

    refactor : 자동차 이름 검증 메서드 단순화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -22,19 +22,12 @@ public class Car {
     }
 
     private void validateCarNameLength(String carName) {
-        if (isCarNameLengthOverThan5(carName)) {
-            throw new IllegalArgumentException("자동차의 이름은 이름은 5자 이하만 가능합니다.");
-        }
-        if (isCarNameLengthEqualZero(carName)) {
-            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+        if (!isStringLengthInRange(carName)) {
+            throw new IllegalArgumentException("자동차의 이름은 이름은 1자 이상 5자 이하만 가능합니다.");
         }
     }
 
-    private boolean isCarNameLengthOverThan5(String carName) {
-        return carName.length() > 5;
-    }
-
-    private boolean isCarNameLengthEqualZero(String carName) {
-        return carName.length() == 0;
+    private boolean isStringLengthInRange(String string) {
+        return string.length() <= 5 && string.length() != 0;
     }
 }
