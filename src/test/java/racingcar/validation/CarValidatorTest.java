050f3fcebcd035f24ddb2d274f98e807d94c1a8f
eunhee <dmsgml9903@gmail.com>
2023-10-30 00:01:33 +0900

    test(CarValidatorTest): 자동차 이름에 대한 검증 로직 테스트
    아래 내용을 테스트:
    - 자동차 이름이 0이하면 예외 발생 테스트
    - 자동차 이름이 5초과면 예외 발생 테스트
    - 자동차 이름이 숫자/영어/한글 외의 문자로 구성되면 예외 발생 테스트

--- src/test/java/racingcar/validation/CarValidatorTest.java
+++ src/test/java/racingcar/validation/CarValidatorTest.java
@@ -23,58 +23,35 @@ class CarValidatorTest {
                 .hasMessage(CarValidator.DUPLICATED_CAR_NAME_ERROR);
     }
 
-    @DisplayName("리스트에 담긴 자동차 이름 중 중복값이 없으면 검증 성공")
+    @DisplayName("자동차 이름의 문자열의 길이가 5 초과면 예외 발생")
     @Test
-    void notDuplicatedCarNameInNameListThrowException() {
-        List<String> nameList = new ArrayList<>();
-        nameList.add("pobi");
-        nameList.add("pobi");
-        nameList.add("woni");
+    void carNameLengthOverFiveThrowException() {
+        String carNameOverLengthFive = "pobiii";
 
         assertThatThrownBy(
-                () -> CarValidator.validateCarNameList(nameList))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(CarValidator.DUPLICATED_CAR_NAME_ERROR);
-    }
-
-    @DisplayName("리스트에 담긴 자동차 이름 중 문자열의 길이가 5 초과면 예외 발생")
-    @Test
-    void lengthOverFiveThrowException() {
-        List<String> nameList = new ArrayList<>();
-        nameList.add("pobiii");
-        nameList.add("pobi");
-        nameList.add("woni");
-
-        assertThatThrownBy(
-                () -> CarValidator.validateCarNameList(nameList))
+                () -> CarValidator.validateCarName(carNameOverLengthFive))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CarValidator.LENGTH_MAX_FIVE_ERROR);
     }
 
-    @DisplayName("리스트에 담긴 자동차 이름 중 문자열의 길이가 1 미만이면 예외 발생")
+    @DisplayName("리스트에 담긴 자동차 이름의 문자열의 길이가 1 미만이면 예외 발생")
     @Test
-    void lengthUnderOneThrowException() {
-        List<String> nameList = new ArrayList<>();
-        nameList.add("");
-        nameList.add("pobi");
-        nameList.add("woni");
+    void carNameLengthUnderOneThrowException() {
+        String carNameLengthUnderOne = "";
 
         assertThatThrownBy(
-                () -> CarValidator.validateCarNameList(nameList))
+                () -> CarValidator.validateCarName(carNameLengthUnderOne))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CarValidator.LENGTH_MIN_ONE_ERROR);
     }
 
-    @DisplayName("리스트에 담긴 자동차 이름 중 숫자/한글/영어 외의 문자면 예외 발생.")
+    @DisplayName("자동차 이름이 숫자/한글/영어 외의 문자열이면 예외 발생.")
     @Test
-    void inputEmptyStringThrowException() {
-        List<String> nameList = new ArrayList<>();
-        nameList.add("pobi#");
-        nameList.add("123한글");
-        nameList.add("woni");
+    void carNameNotOnlyHasCharacterOrNumber() {
+        String notOnlyHasCharacterOrNumber = "#pobi";
 
         assertThatThrownBy(
-                () -> CarValidator.validateCarNameList(nameList))
+                () -> CarValidator.validateCarName(notOnlyHasCharacterOrNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CarValidator.ONLY_HAS_CHARACTER_OR_NUMBER_ERROR);
     }
