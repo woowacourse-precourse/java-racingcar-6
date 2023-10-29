1c8c4bbb576a1817be6a84148c8995a65e6d298a
kiwkim <atthenoon@gmail.com>
2023-10-30 02:38:56 +0900

    feat($Car): make model `Car`.

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,16 @@
+package racingcar.Model;
+
+public class Car {
+    private String carName;
+    private String currentProgress = "";
+
+    public void setCarName(String input){
+        this.carName = input;
+    }
+    public String getCarName(){
+        return this.carName;
+    }
+    public void addProgress(){
+        this.currentProgress = this.currentProgress + '-';
+    }
+}
