9d59ba51196845245b4c8d33cb69f0049613ac7b
Nara Park <skfk6352@naver.com>
2023-10-30 01:20:59 +0900

    <feat>: create method for generating random number
    게임 전진을 위한 랜덤 넘버 생성 후 랜덤값이 4 이상일 경우 참인 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,15 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Car {
 
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+    private static final int GAME_PROCEED_STANDARD = 4;
     private final String name;
     private int position;
 
@@ -16,5 +20,9 @@ public class Car {
 
     public static Car nameFrom(String name) {
         return new Car(name, 0);
+    }
+
+    private boolean checkProceed() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER) >= GAME_PROCEED_STANDARD;
     }
 }
