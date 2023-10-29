5ae2e8dcfe047f5af24d94b09122e7e676099f4b
devhoya97 <diajbh123@gmail.com>
2023-10-29 17:27:25 +0900

    test(racingcar) : CarTest 코드 전체적으로 리팩토링
    기존의 CarTest 코드는 main 메서드를 테스트코드 안에 따로 두고 구현했는데
    좀 더 효율적인 방법인 ParameterizedTest를 사용하여 테스트코드 리팩토링 함

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,41 +2,67 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.Car;
 
-class CarTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+class CarTest {
+    private static final int MAX_NAME_LENGTH = 5;
 
-    public static void main(String[] args) {
-        Car pobi = new Car("pobi");
-        Car woni = new Car("woni");
-
-        pobi.moveIfSatisfy();
-        pobi.moveIfSatisfy();
-        woni.moveIfSatisfy();
-        woni.moveIfSatisfy();
-
-        System.out.println(pobi.getMoveCount());
-        System.out.println(woni.getMoveCount());
-    }
-
-    @Test
-    void getMoveCount_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    main(new String[]{});
-                    assertThat(output()).contains("1", "2"); // output() 메서드의 trim() 적용됨을 유의
-                },
-                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD
+    private static Stream<Arguments> testMoveCountAndRandomNumber() {
+        return Stream.of(
+                Arguments.of(0, 0),
+                Arguments.of(0, 1),
+                Arguments.of(0, 2),
+                Arguments.of(0, 3),
+                Arguments.of(1, 4),
+                Arguments.of(1, 5),
+                Arguments.of(1, 6),
+                Arguments.of(1, 7),
+                Arguments.of(1, 8),
+                Arguments.of(1, 9)
         );
     }
 
-    @Override
-    public void runMain() {
-        main(new String[]{});
+    @ParameterizedTest
+    @MethodSource("testMoveCountAndRandomNumber")
+    void getMoveCount_테스트(int expectedMoveCount, int randomNumber) {
+        //given
+        Car pobi = new Car("pobi");
+
+        //when, then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    pobi.moveIfSatisfy();
+                    int moveCount = pobi.getMoveCount();
+                    assertThat(moveCount).isEqualTo(expectedMoveCount);
+                },
+                randomNumber
+        );
+    }
+
+    @Test
+    void 자동차_이름_정상동작() {
+        // given
+        String carName = "pobi";
+        // when
+        Car car = new Car(carName);
+        // then
+        assertThat(car.getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    void 자동차_이름_예외처리() {
+        // given
+        String carName = "pobiii";
+        // when, then
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 이름이 " + MAX_NAME_LENGTH + "보다 깁니다.");
     }
 }
