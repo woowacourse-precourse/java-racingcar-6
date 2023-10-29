5b8ee77b6ccdc47ec0ae1958df1cfe57c435b552
qmzo553 <qmzo553@gmail.com>
2023-10-29 13:06:12 +0900

    feat: make Racing.java

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,2 +1,40 @@
-package racingcar;public class Racing {
+package racingcar;
+
+import java.util.List;
+
+public class Racing {
+
+    private List<Car> cars;
+    private Judge judge;
+
+    public void racingRun(String[] inputs, int repeatCount) {
+
+        createCars(inputs);
+        racingRepeatRun(repeatCount);
+    }
+
+    private void createCars(String[] inputs) {
+
+        for(int i = 0; i < inputs.length; i++) {
+
+            cars.add(new Car(inputs[i]));
+        }
+    }
+
+    private void racingRepeatRun(int repeatCount) {
+
+        for(int i = 0; i < repeatCount; i++) {
+
+            for(int j = 0; j < cars.size(); j++) {
+
+                cars.get(j).go();
+            }
+        }
+    }
+
+    private List<Car> checkResult() {
+
+        judge = new Judge();
+        return judge.judgeResult();
+    }
 }
