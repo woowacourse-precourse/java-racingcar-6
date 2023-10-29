5f6df3a79df57414ceeb33f3a31a9e0aef611244
lee jihyeon <lejinhy@gmail.com>
2023-10-29 12:14:19 +0900

    feat(ConsolePrint): 최종 우승자 출력 메소드 완성

--- src/main/java/racingcar/ConsolePrint.java
+++ src/main/java/racingcar/ConsolePrint.java
@@ -1,7 +1,8 @@
 package racingcar;
 
+import java.util.List;
+
 public class ConsolePrint {
-    private StringBuilder moveTrace = new StringBuilder();
     public void requestCarNameInput() {
         System.out.println(Message.CAR_NAME_REQUEST.getValue());
     }
@@ -14,6 +15,7 @@ public class ConsolePrint {
     }
 
     public void racingTryResult(Car name, CarStatus status) {
+        StringBuilder moveTrace = new StringBuilder();
         moveTrace.append(name).append(" : ");
         if (status.equals(CarStatus.MOVE)) {
             moveTrace.append("-");
@@ -21,7 +23,11 @@ public class ConsolePrint {
         System.out.println(moveTrace.toString());
     }
 
-    public void finalWinner() {
-
+    public void finalWinner(List<Car> winners) {
+        StringBuilder winnerString = new StringBuilder();
+        System.out.println(Message.FINAL_WINNER_INFO.getValue());
+        for (Car winner : winners) {
+            winnerString.append(winner.getName()).append(Message.DELIMITER_PRINT_CARS.getValue());
+        }
     }
 }
