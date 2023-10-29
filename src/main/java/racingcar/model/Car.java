d697be7bd3ae916cadd4f94bd5c0f50ce938668d
“EvelynKimm” <evekimmy09@gmail.com>
2023-10-29 11:53:19 +0900

    feat: add car model
    model.car 파일 수정

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
