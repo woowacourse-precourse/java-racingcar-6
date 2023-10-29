9c316879bfb407093ed907a58c6623230e4caf6c
little6523 <little6523@naver.com>
2023-10-30 01:09:47 +0900

    feat : 자동차가 이동을 시도할 횟수를 입력하는 기능  - docs/README.md 1번 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 //    private List<String> CarNames = new ArrayList<>();
 
@@ -7,13 +9,16 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
     }
 
-//    private static void inputCarNames() {
+    //    private static void inputCarNames() {
 //
 //    }
-
+    private static int inputCarMovingCount() {
+        return Integer.parseInt(Console.readLine());
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         displayStartText();
+        inputCarMovingCount();
     }
 }
