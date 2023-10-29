7027e0f8204036eb36fbce161b2c92adf8ae1ec8
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 04:08:39 +0900

    feat(Car) : 생성자 완성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,5 +3,10 @@ package racingcar.domain;
 public class Car {
     private String name;
     private int totalDistance;
+    
+    Car(String name, int totalDistance){
+        this.name = name;
+        this.totalDistance = totalDistance;
+    }
 
 }
