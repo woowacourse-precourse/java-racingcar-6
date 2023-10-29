a8674afaec3923abeb22a5c18e0d3ebdb3414511
SJ70 <seojae70@gmail.com>
2023-10-29 18:25:37 +0900

    Feat & Test: 차의 위치값을 증감하는 메소드

--- src/main/java/racingcar/domain/CarPosition.java
+++ src/main/java/racingcar/domain/CarPosition.java
@@ -4,4 +4,12 @@ public class CarPosition {
 
     private int position = 0;
 
+    public void move(int value) {
+        position = position + value;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
 }
---
+++ src/test/java/racingcar/car/CarPositionMoveTest.java
@@ -0,0 +1,34 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.CarPosition;
+
+public class CarPositionMoveTest {
+
+    private CarPosition carPosition;
+
+    @BeforeEach()
+    void setup() {
+        carPosition = new CarPosition();
+    }
+
+    @ParameterizedTest
+    @DisplayName("차의 위치값이 증감값에 대해 올바르게 이동한다.")
+    @CsvSource(value = {
+            "1,1,2", "0,0,0", "-1,-1,-2", "1,-1,0"
+    })
+    void carPositionMoveTest(int value1, int value2, int expectedPosition) {
+        // when
+        carPosition.move(value1);
+        carPosition.move(value2);
+        int position = carPosition.getPosition();
+
+        // then
+        Assertions.assertEquals(position, expectedPosition);
+    }
+
+}
