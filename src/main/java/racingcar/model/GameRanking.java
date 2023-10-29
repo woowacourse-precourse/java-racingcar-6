2249337fa44d34d76b0ae301a75fa14492b03c5d
yoonjuho <dbswn990@naver.com>
2023-10-29 18:15:26 +0900

    feat(GameRanking): 자동차 경주 게임 현황을 알려준다.

---
+++ src/main/java/racingcar/model/GameRanking.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Map;
+
+public class GameRanking {
+    private Map<RacingCar, String> participants;
+
+    public GameRanking(Map<RacingCar, String> participants) {
+        this.participants = participants;
+    }
+
+    public Map<RacingCar, String> progressRound(){
+        for(RacingCar racingCar : participants.keySet()){
+            participants.replace(racingCar,participants.get(racingCar)
+                    + racingCar.apply((Randoms.pickNumberInRange(0,9))).getOutput());
+        }
+        return participants;
+    }
+
+
+}
