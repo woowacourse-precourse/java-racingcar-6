af302b7d0afbe0486fe5037b150ea1df3546ca2a
WooJJam <jaemin5548@naver.com>
2023-10-29 15:25:42 +0900

    [Refacotr] 자동차 이름 검증과정 메소드로 분할

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -7,25 +7,42 @@ import java.util.Set;
 
 public class CarNameValidator {
 
+    Set<String> uniqueWords = new HashSet<>();
+    List<String> carName = new ArrayList<>();
+
     public List<String> validateInputCarName(String inputCarName) {
-        Set<String> uniqueWords = new HashSet<>();
-        List<String> carName = new ArrayList<>();
 
         for (String word : inputCarName.split(",")) {
             System.out.println("word = " + word);
-            if (!uniqueWords.add(word)) {
-                throw new IllegalArgumentException("중복된 단어가 있습니다.");
-            } else if (word.isEmpty()) {
-                throw new IllegalArgumentException("빈 문자는 입력할 수 없습니다.");
-            } else if (word.contains(" ")) {
-                throw new IllegalArgumentException("자동차 이름에 공백은 포함될 수 없습니다.");
-            } else if (!word.matches("^[A-Za-z0-9]+$")) {
-                throw new IllegalArgumentException("자동차 이름에 특수 문자는 포함될 수 없습니다.");
-            } else if (!(word.length() > 5)) {
-                throw new IllegalArgumentException("자동차 이름은 5글자를 이하만 가능합니다.");
-            }
+            validateDuplicate(word);
+            validateBlank(word);
+            validateSpecialCharacter(word);
+            validateLength(word);
             carName.add(word);
         }
         return carName;
+    }
+
+    private void validateDuplicate(String word) {
+        if (!uniqueWords.add(word)) {
+            throw new IllegalArgumentException("중복된 단어가 있습니다.");
+        }
+    }
+    private void validateBlank(String word) {
+        if (word.contains(" ")) {
+            throw new IllegalArgumentException("자동차 이름에 공백은 포함될 수 없습니다.");
+        }
+    }
+
+    private void validateSpecialCharacter(String word) {
+        if (!word.matches("^[A-Za-z0-9]+$")) {
+            throw new IllegalArgumentException("자동차 이름에 특수 문자는 포함될 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String word) {
+        if (word.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 이하만 가능합니다.");
+        }
     }
 }
