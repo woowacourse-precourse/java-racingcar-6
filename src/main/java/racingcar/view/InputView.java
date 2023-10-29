e42563b5b0db402c6b79b7b832023363b81d176b
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-30 00:03:53 +0900

    🎯feat(view / InputView) : 자동차 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/* 자동차 입력 받는 클래스
+*  입력만 받기
+* */
+public class InputView {
+   private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"; // 삼수
+
+    // 자동차 입력 받기
+    public  String inputCarName() {
+        System.out.println(INPUT_CAR_NAME);
+        return Console.readLine();
+    }
+}
