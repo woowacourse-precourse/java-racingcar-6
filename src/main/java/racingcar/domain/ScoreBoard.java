6adf83d6a3002fa782132e31a28ef4530aad003f
HyunmoBae <tsi0520@naver.com>
2023-10-29 19:26:59 +0900

    feat(racingcar) : add showCarsScore function

--- src/main/java/racingcar/domain/ScoreBoard.java
+++ src/main/java/racingcar/domain/ScoreBoard.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class ScoreBoard {
 
     public String getCarPosition(Car car) {
@@ -8,5 +10,15 @@ public class ScoreBoard {
             carPosition += "-";
         }
         return carPosition;
+    }
+
+    public void showCarsScore(List<Car> carList, int tryNum) {
+        for (int i = 0; i < tryNum; i++) {
+            for (int j = 0; j < carList.size(); j++) {
+                carList.get(j).tryMoveCar();
+                System.out.println(carList.get(j).carName + " : " + getCarPosition(carList.get(j)));
+            }
+            System.out.println();
+        }
     }
 }
