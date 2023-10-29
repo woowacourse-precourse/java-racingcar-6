0154073566259f97fec8f419ce85c08d69d13bdd
Jinseo Jeong <js020315@gmail.com>
2023-10-29 20:53:56 +0900

    [FEAT] CreateRandomNum
    0~9 사이의 무작위 정수 생성
    정수가 4보다 클 경우 자동차 이동

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,13 +1,16 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
 import racingcar.view.InputView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingController {
-    List<String> racingCars = new ArrayList<>();
+    List<String> racingCarsList = new ArrayList<>();
+    Car car = new Car();
 
     public void GameProgress() {
 
@@ -16,11 +19,21 @@ public class RacingController {
     public void InputCarsByUser() {
         InputView.inputCars();
         String inputCars = Console.readLine();
-        racingCars = List.of(inputCars.split(","));
+        racingCarsList = List.of(inputCars.split(","));
     }
 
     public void InputAttemptsByUser() {
         InputView.inputAttemptsNum();
         int inputAttempts = Integer.parseInt(Console.readLine());
+    }
+
+    public int CreateRandomNum() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    public void IsMoreThan() {
+        if (CreateRandomNum() >= 4) {
+            car.move();
+        }
     }
 }
