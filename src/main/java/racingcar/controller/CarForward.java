04721c2941e01144933755cc7226b78aeec36831
minseokKim6823 <alstjr6823@naver.com>
2023-10-29 18:36:50 +0900

    자동차 전진

--- src/main/java/racingcar/controller/CarForward.java
+++ src/main/java/racingcar/controller/CarForward.java
@@ -1,2 +1,14 @@
-package racingcar.controller;public class CarForward {
+package racingcar.controller;
+
+import racingcar.generator.RandomNumberGenerator;
+
+public class CarForward {
+    int randomnumber=RandomNumberGenerator.RandomNumber();
+    public void forward(int location){
+        if(randomnumber>=4) {
+            location += 1;
+        }
+    }
+
+
 }
