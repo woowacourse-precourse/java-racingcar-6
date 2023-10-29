696867cd5db2a10bfe73658d420d5a7b9d7aa65b
YeaChan05 <qkenrdl05@gmail.com>
2023-10-29 16:10:43 +0900

    [ADD] 기능 통합

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,18 @@
 package racingcar;
 
+import racingcar.domain.Stadium;
+import racingcar.utils.InputManager;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 2023-10-27 오후 3:41 시도 횟수 입력
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String > userNames = (List<String>) InputManager.INPUT_NAME.input();
+        System.out.println("시도할 회수는 몇회인가요?");
+        Integer moveCount = (Integer)InputManager.INPUT_MOVE_COUNT.input();
+        Stadium stadium= Stadium.of(moveCount, userNames);
+        stadium.startRace();
+        stadium.confirmWinner();
     }
 }
