0e64851d69454272329ae5ee2f6399b17f54e639
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 20:25:11 +0900

    Refactor(Refree): 매직넘버를 상수로 변경

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -3,16 +3,18 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Referee {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int PROBABILITY = 4;
 
     public static void simulate(RacingCars racingcars) {
 
         for (int i = 0; i < racingcars.size(); i++) {
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            if (randomNumber >= 4) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+            if (randomNumber >= PROBABILITY) {
                 racingcars.get(i).move();
             }
         }
-
     }
 
     public static RacingCars calculateWinner(RacingCars racingcars) {
