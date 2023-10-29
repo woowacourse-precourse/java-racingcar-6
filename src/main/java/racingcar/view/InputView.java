869a46df92e565c499fd64f4a6d055245b888b82
yang <ywj9811@gmail.com>
2023-10-30 05:27:23 +0900

    feat : InputView 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import static racingcar.constant.OutputConstant.*;
+
+public class InputView {
+    static public void inputCarName() {
+        System.out.println(INPUT_CAR_NAME.getOutput());
+    }
+
+    static public void howManyTime() {
+        System.out.println(HOW_MANY_TIME.getOutput());
+    }
+}
