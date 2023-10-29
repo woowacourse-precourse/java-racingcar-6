7dfd83ed73fb7924db5575a5d73a7c84e3624c4f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-30 03:02:24 +0900

    feat : 자동차 요구사항 2번 기능 구현 완료
    TDD 기법에 따라 테스트 코드에 대한 기능 구현 ( BLUE )
    docs/README.md 에 이모지 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,11 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    public boolean moveForwardIfSpeedIsExceedThreshold(){
-        generateRandomSpeed();
-        return true;
+    public boolean checkCanMoveForwardIfSpeedIsExceedThreshold(){
+        int randomSpeed = generateRandomSpeed();
+        if (randomSpeed >= 4){
+            return true;
+        }
+        return false;
     }
-    private int generateRandomSpeed(){
-        return 1;
+    protected int generateRandomSpeed(){
+        int randomSpeed = Randoms.pickNumberInRange(0,9);
+        return randomSpeed;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,24 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import org.mockito.Mockito;
+
+
 public class CarTest {
     private Car car;
+
+
     @BeforeEach
     void setUp(){
-        car = new Car();
+        car = Mockito.mock(Car.class);
     }
     @Test
     void 숫자가_일정값을_넘으면_전진할수있다(){
-        Boolean forwardStatus = car.moveForwardIfSpeedIsExceedThreshold();
-        Assertions.assertEquals(forwardStatus,true);
+        Mockito.when(car.generateRandomSpeed()).thenReturn(5);
+        Mockito.when(car.checkCanMoveForwardIfSpeedIsExceedThreshold()).thenCallRealMethod();
+        Assertions.assertEquals(car.checkCanMoveForwardIfSpeedIsExceedThreshold(),true);
     }
     @Test
     void 숫자가_일정값을_넘지못하면_전진할수없다(){
-        Boolean forwardStatus = car.moveForwardIfSpeedIsExceedThreshold();
-        Assertions.assertEquals(forwardStatus,false);
+        Mockito.when(car.generateRandomSpeed()).thenReturn(1);
+        Mockito.when(car.checkCanMoveForwardIfSpeedIsExceedThreshold()).thenCallRealMethod();
+        Assertions.assertEquals(car.checkCanMoveForwardIfSpeedIsExceedThreshold(),false);
     }
 
 }
