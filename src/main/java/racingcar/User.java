298a3948a1e40612489f1b4fc6f58bc46bcef75c
Hajuuu <hpt0127@naver.com>
2023-10-29 18:53:21 +0900

    feat(User): User 클래스 추가

---
+++ src/main/java/racingcar/User.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class User {
+
+    String name;
+    int length;
+
+    public User(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+}
