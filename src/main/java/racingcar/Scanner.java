8c519d2058cc48fd5149ba3f31f9958d06c01414
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-29 20:14:01 +0900

    add(Scanner) : add inputCarNames func to input Car's Names

--- src/main/java/racingcar/Scanner.java
+++ src/main/java/racingcar/Scanner.java
@@ -1,4 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Scanner {
+    private int minimumOfRange;
+    private int maximumOfRange;
+    private int numberOfCars;
+    private String delimiter;
+
+    Scanner(Configurations configurations){
+        minimumOfRange = configurations.getMinimumOfRange();
+        maximumOfRange = configurations.getMaximumOfRange();
+        numberOfCars = configurations.getNumberOfNumbers();
+        delimiter = configurations.getDelimiter();
+    }
+    List<String> inputCarNames(){
+        List<String>carNames = new ArrayList<>();
+        String inputString = Console.readLine();
+        StringTokenizer tokenizedInputString = new StringTokenizer(inputString,delimiter);
+        while (tokenizedInputString.hasMoreTokens()) {
+            String nextCar = tokenizedInputString.nextToken();
+            carNames.add(nextCar);
+        }
+        return carNames;
+    }
 }
