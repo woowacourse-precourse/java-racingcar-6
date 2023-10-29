72571e7189f6359494a03f8d6ac7ebd80e43eca9
msung99 <msung6924@naver.com>
2023-10-29 18:31:34 +0900

    refactor: Position 테스트 코드 BeforeEach 를 활용하여 재사용성 개선

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -1,20 +1,28 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PositionTest {
+
+    private Position initPosition;
+
+    @BeforeEach
+    void setUp() {
+        initPosition = new Position(0);
+    }
+
     @Test
-    void createPositionWithIncrease(){
-        int initPosition = 0;
-        Position position = Position.getIncreasedPosition(initPosition);
+    void createPositionWithIncrease() {
+        Position position = Position.getIncreasedPosition(initPosition.getPosition());
         assertThat(position).isEqualTo(new Position(1));
     }
 
     @Test
-    void createPositionWithPause(){
-        int initPosition = 0;
-        Position position = Position.getPausedPosition(initPosition);
+    void createPositionWithPause() {
+        Position position = Position.getPausedPosition(initPosition.getPosition());
         assertThat(position).isEqualTo(new Position(0));
     }
 }
+
