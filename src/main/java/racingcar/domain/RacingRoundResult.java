f0dd1bcdf0788cf551bf442fb0600c19f7bdb91c
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 12:38:47 +0900

    refactor(RacingRoundResult): 필드로 List<CarInfo> DTO를 갖도록 수정

--- src/main/java/racingcar/domain/RacingRoundResult.java
+++ src/main/java/racingcar/domain/RacingRoundResult.java
@@ -2,30 +2,31 @@ package racingcar.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.dto.CarInfo;
 
 public class RacingRoundResult {
     public static final String NOT_AVAILABLE_CAR_LIST_EXCEPTION = "올바르지 않은 자동차 리스트입니다.";
-    private final List<Car> cars;
+    private final List<CarInfo> carInfoList;
 
-    public RacingRoundResult(List<Car> readOnlyCarList) {
-        this.cars = readOnlyCarList;
+    public RacingRoundResult(List<CarInfo> carInfoList) {
+        this.carInfoList = carInfoList;
     }
 
-    public List<Car> getCars() {
-        return this.cars.stream()
-                .map(car -> new Car(car.getName(), car.getCarEngine(), new Position(car.getPosition())))
+    public List<CarInfo> getCarInfoList() {
+        return this.carInfoList.stream()
+                .map(carInfo -> new CarInfo(carInfo.name(), carInfo.position()))
                 .collect(Collectors.toUnmodifiableList());
     }
 
     public List<String> getWinners() {
-        int maxPosition = cars.stream()
-                .mapToInt(Car::getPosition)
+        int maxPosition = carInfoList.stream()
+                .mapToInt(CarInfo::position)
                 .max()
                 .orElseThrow(() -> new IllegalArgumentException(NOT_AVAILABLE_CAR_LIST_EXCEPTION));
 
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .map(Car::getName)
+        return carInfoList.stream()
+                .filter(carInfo -> carInfo.position() == maxPosition)
+                .map(CarInfo::name)
                 .collect(Collectors.toList());
     }
 }
