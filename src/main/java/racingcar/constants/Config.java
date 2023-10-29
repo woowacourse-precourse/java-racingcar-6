7f844651a28ae928f5cf643503cc6024370974fc
IMWoo94 <leemeawoo94@gmail.com>
2023-10-29 16:06:13 +0900

    fix : 랜덤 숫자 생성 범위 오타 수정
    랜덤 숫자 생성 범위 오타 수정

--- src/main/java/racingcar/constants/Config.java
+++ src/main/java/racingcar/constants/Config.java
@@ -3,7 +3,7 @@ package racingcar.constants;
 public class Config {
 
     public static final int BEGIN_RANGE = 0;
-    public static final int END_RANGE = 0;
+    public static final int END_RANGE = 9;
     public static final int CAR_NAME_SIZE = 5;
     public static final int MOVING_FORWARD = 4;
 
