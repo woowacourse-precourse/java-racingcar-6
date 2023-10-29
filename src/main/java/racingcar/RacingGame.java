9bba7220f3795623da4680724bdadeb39ae5cddb
sangdol <priceelees@gmail.com>
2023-10-29 13:03:58 +0900

    feat(RacingGame): 게임을 관리하는 기능

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class RacingGame {
+    private final RaceResult raceResult = new RaceResult();
+    private Cars cars;
+
+    public void startGame() {
+        String names = receiveCarNames();
+        int raceCount = receiveRaceCount();
+        cars = Cars.getCarsFromNames(names);
+
+        playGame(raceCount);
+    }
+
+    private void playGame(int raceCount) {
+        for (int i = 0; i < raceCount; i++) {
+            cars = cars.race();
+            System.out.println(raceResult.getRaceProgress(cars.carList()));
+        }
+        List<Car> winnerList = cars.findWinners();
+        System.out.println(raceResult.getWinners(winnerList));
+    }
+
+    private int receiveRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private String receiveCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+}
