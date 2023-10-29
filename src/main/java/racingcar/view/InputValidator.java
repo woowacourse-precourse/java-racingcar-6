324b5256add99b117565c13aa876c09d97fe9a12
sinminseok12 <sinminseok12@naver.com>
2023-10-29 23:30:32 +0900

    feat(InputValidator): ,가 마지막에 들어온 경 예외처리 기능 구현

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -1,15 +1,21 @@
 package racingcar.view;
 
+import static racingcar.utils.Parser.COMMA;
+
 public class InputValidator {
 
     private static final String COMMA_PATTERN = "^[,\\p{L}\\p{N}\\s]*$";
     private static final String COMMA_ERROR = ",가 아닌 다른 특수문자는 허용하지 않습니다.";
+    private static final String LAST_COMMA_ERROR = "입력값 마지막에 ,를 허용하지 않습니다.";
 
     public static void validateList(String input) {
         validateSplitComma(input);
     }
 
     private static void validateSplitComma(String input) {
+        if (input.endsWith(COMMA)) {
+            throw new IllegalArgumentException(LAST_COMMA_ERROR);
+        }
         if (!input.matches(COMMA_PATTERN)) {
             throw new IllegalArgumentException(COMMA_ERROR);
         }
