5e2a491df85635b21641058ffc37f44ee7deace5
yush <yush2202@gmail.com>
2023-10-30 04:03:25 +0900

    style : 주석 추가 GameModel의 decideFinalWinner 메서드에 주석을 추가했습니다.

--- src/main/java/racingcar/GameModel.java
+++ src/main/java/racingcar/GameModel.java
@@ -15,6 +15,10 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Cars 객체에서 askDistance로 길이를 반환받은 후 가장 먼 거리를 나아간 객체의 이름을 getName으로 반환받아서 ArrayList로 반환하는 메서드입니다. 최초에 maxDistance를
+     * 음수로 설정한 후 Cars 객체를 하나씩 순회하면서 가장 긴 거리가 나올때마다 maxDistance를 갱신시킵니다.
+     */
     public ArrayList<String> decideFinalWinner(ArrayList<Cars> players) {
         ArrayList<String> winners = new ArrayList<>();
         int maxDistance = -1;
@@ -38,5 +42,4 @@ public class GameModel {
             }
         }
     }
-
 }
--- src/test/java/study/ValidationTest.java
+++ src/test/java/study/ValidationTest.java
@@ -11,16 +11,16 @@ public class ValidationTest {
     void name_0자미만_6자이상_array입력() {
         String name = "안녕하세요반갑습니다.";
         Assertions.assertThatThrownBy(() -> Validation.validateLengthOfName(name))
-                .isInstanceOf(IllegalArgumentException.class) // 여기서 YourExpectedException을 실제 사용하는 예외로 대체해야 합니다.
-                .hasMessageContaining("잘못된 입력입니다."); // 예외 메시지도 필요하다면 지정할 수 있습니다.
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력입니다.");
     }
 
     @Test
     void name_숫자가_아닌_값() {
         String s = "k";
         Assertions.assertThatThrownBy(() -> Validation.isNotNumber(s))
-                .isInstanceOf(IllegalArgumentException.class) // 여기서 YourExpectedException을 실제 사용하는 예외로 대체해야 합니다.
-                .hasMessageContaining("잘못된 입력입니다."); // 예외 메시지도 필요하다면 지정할 수 있습니다.
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력입니다.");
     }
 }
 
