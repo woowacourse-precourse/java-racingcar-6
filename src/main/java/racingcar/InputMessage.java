125596c217bddd42ac690b3eb052ec37c2874ba5
KDT live <dnjsxo0616@gmail.com>
2023-10-29 18:15:09 +0900

    feat(InputMessage): 사용자 입력 게임 횟수 입력 메서드 추가
    - 입력 안내 메시지 출력을 위한 `GAME_NUMBER` 상수 정의
    - 사용자에게 게임 횟수를 입력받는 `gameNumber` 메서드 추가

--- src/main/java/racingcar/InputMessage.java
+++ src/main/java/racingcar/InputMessage.java
@@ -6,8 +6,15 @@ public class InputMessage {
 
     private static final String CARNAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
+    private static final String GAME_NUMBER = "시도할 횟수는 몇 회인가요?";
+
     public static String carName(){
         System.out.println(CARNAME_INPUT);
+        return Console.readLine();
+    }
+
+    public static String gameNumber(){
+        System.out.println(GAME_NUMBER);
         return Console.readLine();
     }
 
