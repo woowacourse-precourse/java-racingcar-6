22bc49681f64fc43addb29e23bd575ca4055ae63
tmdduq500 <osy9907@gmail.com>
2023-10-29 22:20:22 +0900

    feat: 레이싱 플레이 기능 구현

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -2,12 +2,13 @@ package racingcar;
 
 import static racingcar.InputView.inputCarNames;
 import static racingcar.InputView.inputTryNumber;
+import static racingcar.OutputView.printFinalWinner;
 import static racingcar.OutputView.printPlayResult;
 
 public class Controller {
     ValidatorCarName validatorCarName = new ValidatorCarName();
     ValidatorTryNumber validatorTryNumber = new ValidatorTryNumber();
-    MakeCar makecar = new MakeCar();
+    CarFuntion makecar = new CarFuntion();
 
     public void racingStart(){
         String carNames = inputCarNames();
@@ -16,9 +17,11 @@ public class Controller {
 
         String tryNumber = inputTryNumber();
         validatorTryNumber.checkNum(tryNumber);
-        validatorTryNumber.getTryNumber();
 
         printPlayResult();
+        PlayRacing.forwardRecord(validatorTryNumber.getTryNumber(), makecar.getCar());
+        printFinalWinner();
+        PlayRacing.selectWinner(makecar.getCar());
 
     }
 }
