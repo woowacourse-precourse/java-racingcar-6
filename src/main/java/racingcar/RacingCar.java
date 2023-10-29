a528a08bcd1730bd6dcaef2245f876dd97af1dd5
Silver <leeeun5423@naver.com>
2023-10-29 19:23:45 +0900

    feat : 시작 안내문 출력하기 (car name, iteration 숫자 입력)

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class RacingCar {
+    public static final String carName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String gameNum = "시도할 회수는 몇회인가요?";
     List<String> members;
     public Player players;
     public int games;
@@ -15,11 +17,13 @@ public class RacingCar {
 
     public void getUser(){
         players = new Player();
+        System.out.println(carName);
         members = players.getMember(Console.readLine());
 //        players.printMembers();
     }
 
     public int getGameNum(){
+        System.out.println(gameNum);
         String num = Console.readLine();
         return Integer.parseInt(num);
     }
