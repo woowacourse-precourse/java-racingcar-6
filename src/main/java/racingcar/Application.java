4ba7d37222f73677cdc11b7db58b371ac1ad22f7
Soobin-Park <jrkd2165@gmail.com>
2023-10-29 17:16:06 +0900

    :sparkles: feat: 사용자에게 입력받은 값이 잘못된 값이며 오류 반환하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,18 +9,35 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void game() throws IllegalArgumentException{
+    public static void game() throws IllegalArgumentException {
 
         //사용자에게 입력값 (경주할 자동차 이름, 게임을 시도할 회수) 입력받기
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String stringCars = readLine();
         List<String> cars = List.of(stringCars.split(","));
 
+        //입력값이 5자 이하인지 확인
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         System.out.println("시도할 회수는 몇회인가요?");
-        int numberOfGames = Integer.parseInt(readLine());
+        String stringNumberOfGames = readLine();
+
+        //입력값이 숫자인지 확인
+        try {
+            int numberOfGames = Integer.parseInt(stringNumberOfGames);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+
 
 
     }
+
     public static void main(String[] args) {
         game();
     }
