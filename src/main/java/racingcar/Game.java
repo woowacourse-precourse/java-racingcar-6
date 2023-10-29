fd10bac4ed15f4f8c8c6cf33c60518898187250e
yonggwonk <gwonluck@dgu.ac.kr>
2023-10-29 21:16:02 +0900

    feat: Generate Game class
    게임을 진행하기 위한 클래스 생성, 외부로 부터 입력받을 자동차의 이름, 시도 횟수를 받아와 객체를 생성하고 게임을 진행하는 로직 구현

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private final List<Car> cars;
+    private final int tryCount;
+
+    public Game(List<String> carNames, int tryCount) {
+        this.cars = createCars(carNames);
+        this.tryCount = tryCount;
+    }
+
+    private List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    public void play() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryCount; i++) {
+            moveCars();
+            System.out.println();
+        }
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    private void moveCars() {
+        for (Car car : cars) {
+            car.move();
+            System.out.println(car);
+        }
+    }
+
+    private int getMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
+}
