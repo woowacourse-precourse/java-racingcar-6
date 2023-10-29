b74d26602e1bef125546d992df1b38a19625eef2
sami355-24 <ddung355@gmail.com>
2023-10-29 21:07:38 +0900

    test(Validator): 입력하는 시도 횟수에 대한 테스트 코드 추가
    - 시도 횟수가 문자열이 아닌지 검증하는 테스트 코드 추가
    - 시도 횟수가 실수가 아닌지 검증하는 테스트 코드 추가
    - 시도 횟수가 범위안에 존재하는지 검증

---
+++ src/main/java/racingcar/Validator/RoundValidator.java
@@ -0,0 +1,13 @@
+package racingcar.Validator;
+
+public class RoundValidator {
+
+    public static void isNotString(String round) {
+    }
+
+    public static void isNotRealNumber(String round) {
+    }
+
+    public static void isValidRange(String round) {
+    }
+}
---
+++ src/test/java/racingcar/Validator/RoundValidatorTest.java
@@ -0,0 +1,42 @@
+package racingcar.Validator;
+
+import static racingcar.Message.RoundExceptionPrompt.RANGEPROMPT;
+import static racingcar.Message.RoundExceptionPrompt.REALNUMBERPROMPT;
+import static racingcar.Message.RoundExceptionPrompt.STRINGPROMPT;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RoundValidatorTest {
+
+    @Test
+    @DisplayName("시도 횟수가 문자열이 아닌지 검증")
+    void isNotString() {
+        String testRound = "test";
+
+        Assertions.assertThatThrownBy(() -> RoundValidator.isNotString(testRound))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(STRINGPROMPT.getMessage(testRound));
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 실수가 아닌지 검증")
+    void isNotRealNumber() {
+        String testRound = "1.1";
+
+        Assertions.assertThatThrownBy(() -> RoundValidator.isNotRealNumber(testRound))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(REALNUMBERPROMPT.getMessage(testRound));
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 범위안에 존재하는 검증")
+    void isValidRange() {
+        String testRound = "0";
+
+        Assertions.assertThatThrownBy(() -> RoundValidator.isValidRange(testRound))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(RANGEPROMPT.getMessage(testRound));
+    }
+}
