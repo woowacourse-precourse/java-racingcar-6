9b98e6d7a6879fdcc0c7bde9538fe6ba65ef8233
slowcloud <sju9417@gmail.com>
2023-10-29 10:31:23 +0900

    style(AttemptTest): 짧은 코드의 개행 제거

--- src/test/java/racingcar/domain/AttemptTest.java
+++ src/test/java/racingcar/domain/AttemptTest.java
@@ -21,9 +21,7 @@ class AttemptTest {
     @Test
     void decrease_1에서_decrease했을_때_거짓_반환() {
         Attempt attempt = new Attempt(1);
-
         attempt.decrease();
-
         assertFalse(attempt.isExist());
     }
 
