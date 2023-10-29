d4c51465fde56f506e20098787e927265e8143a9
OikimiO <etkim02@naver.com>
2023-10-29 14:59:37 +0900

    test: 입력했던 값에 대한 출력 추가

--- src/test/java/racingcar/OutputCarRaceTest.java
+++ src/test/java/racingcar/OutputCarRaceTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 
 public class OutputCarRaceTest extends PrintTest{
     private static final String 차이름 = "차이름";
+    private static final String 차이름들 = "pobi1,pobi2,pobi3,pobi4";
 
     enum MessageType{
         INPUT_CAR_NAME_PRINT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
         INPUT_CAR_RACING_COUNT_PRINT("시도할 회수는 몇회인가요?"),
+        INPUT_DATA_PRINT("%s"),
 
         OUPUT_RACE_RESULT_PRINT("실행 결과"),
         OUPUT_IMPLEMENTATION_RESULT_PRINT("%s : %s");
@@ -81,5 +83,13 @@ public class OutputCarRaceTest extends PrintTest{
         OutputCarRace.printf(MessageType.OUPUT_IMPLEMENTATION_RESULT_PRINT, 차이름, "");
 
         assertThat(outputf(차이름)).contains(format);
+    }
+
+    @Test
+    void 입력했던_값_출력(){
+        String format = format(MessageType.INPUT_DATA_PRINT.getValue(), 차이름들);
+        OutputCarRace.printf(MessageType.INPUT_DATA_PRINT, 차이름들);
+
+        assertThat(outputf(차이름들)).contains(format);
     }
 }
