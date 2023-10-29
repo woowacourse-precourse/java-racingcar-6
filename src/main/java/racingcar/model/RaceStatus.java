4026ac633377f7ca7438026ac0d0dcdab719bfa7
hansg931 <hansg0509@cau.ac.kr>
2023-10-29 11:38:50 +0900

    feat: 각 라운드의 상태를 처리하는 클래스 작성

--- src/main/java/racingcar/model/RaceStatus.java
+++ src/main/java/racingcar/model/RaceStatus.java
@@ -1,4 +1,16 @@
 package racingcar.model;
 
+import java.util.Collections;
+import java.util.List;
+
 public class RaceStatus {
+	private final List<RacingCar> cars;
+
+	public RaceStatus(List<RacingCar> cars) {
+		this.cars = Collections.unmodifiableList(cars);
+	}
+
+	public List<RacingCar> getCars() {
+		return cars;
+	}
 }
