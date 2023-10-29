5b6cd0a7698b307290812a5fd516a1cb2b843857
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-29 12:49:23 +0900

    refactor(CarsTest) : Given-When-Then패턴 도입

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -20,9 +20,13 @@ public class CarsTest {
     @DisplayName("자동차명 리스트가 입력되면 모든 자동차가 추가되어야한다.")
     @Test
     void addCarsTest() {
+        // Given
         cars.addCars(Arrays.asList("pobi", "woni", "jun"));
+
+        // When
         HashMap<String, Integer> carPositions = cars.getCars();
 
+        // Then
         assertThat(carPositions).hasSize(3);
         assertThat(carPositions.get("pobi")).isEqualTo(0);
         assertThat(carPositions.get("woni")).isEqualTo(0);
@@ -32,11 +36,15 @@ public class CarsTest {
     @DisplayName("특정 자동차를 이동시키면 값이 1증가해야 한다.")
     @Test
     void moveCarTest() {
+        // Given
         cars.addCars(Arrays.asList("pobi", "woni", "jun"));
-        cars.moveCar("pobi");
+        String carName = "pobi";
 
+        // When
+        cars.moveCar(carName);
         HashMap<String, Integer> carPositions = cars.getCars();
 
+        // Then
         assertThat(carPositions.get("pobi")).isEqualTo(1);
         assertThat(carPositions.get("woni")).isEqualTo(0);
         assertThat(carPositions.get("jun")).isEqualTo(0);
@@ -45,22 +53,32 @@ public class CarsTest {
     @DisplayName("값이 같다면, 해당 차량은 모두 우승자로 결정되어야 한다.")
     @Test
     void determineMultipleWinnersTest() {
+        // Given
         cars.addCars(Arrays.asList("pobi", "woni", "jun"));
         cars.moveCar("pobi");
         cars.moveCar("woni");
 
-        assertThat(cars.determineFinalWinner()).containsExactlyInAnyOrder("pobi", "woni");
+        // When
+        var winners = cars.determineFinalWinner();
+
+        // Then
+        assertThat(winners).containsExactlyInAnyOrder("pobi", "woni");
     }
 
     @Test
     @DisplayName("가장 큰 값을 가진 자동차들이 우승자로 결정되어야 한다.")
     void determineFinalWinnerTest() {
+        // Given
         cars.addCars(Arrays.asList("pobi", "woni", "jun"));
         cars.moveCar("pobi");
         cars.moveCar("pobi");
         cars.moveCar("jun");
 
-        assertThat(cars.determineFinalWinner()).containsExactlyInAnyOrder("pobi");
+        // When
+        var winners = cars.determineFinalWinner();
+
+        // Then
+        assertThat(winners).containsExactlyInAnyOrder("pobi");
     }
 
 }
