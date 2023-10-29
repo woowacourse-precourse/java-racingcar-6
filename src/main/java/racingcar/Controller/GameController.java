8b2b3fdd5cac712b54a87c5da889cfd92f98707d
kiwkim <atthenoon@gmail.com>
2023-10-30 07:17:59 +0900

    1. feat($Validator): make `getValidNumber`.
    2. feat($Controller): join the Controller with Validator.
    
    3. feat($Controller): make simple `raceOnce`.
    
    If `getRandomNumber` is more than four, the Car's progress is added.

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -5,26 +5,37 @@ import racingcar.Model.CarList;
 import racingcar.View.InputView;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.View.OutputView;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class GameController {
     private InputView inputView;
+    private OutputView outputView;
     private CarList carList;
     private Validator validator;
 
     public GameController(){
         this.inputView = new InputView();
+        this.outputView = new OutputView();
         this.carList = new CarList();
         this.validator = new Validator();
     }
 
     public void runGame(){
+        String tryCount;
+        int validCountNum;
+
         List<String> carNameList = new ArrayList<>(this.inputView.inputCarNames());
         validator.validListIsEmpty(carNameList);
-        String tryCount = this.inputView.inputCounts();
+
+        tryCount = this.inputView.inputCounts();
+        validCountNum = validator.getValidNumber(tryCount);
+
         fillCarList(carNameList);
+        this.outputView.printResultInit();
+
     }
 
     private void fillCarList(List<String> carNameList){
@@ -34,13 +45,14 @@ public class GameController {
             carList.addCar(newCar);
         }
     }
-    public void raceOnce(){
-
+    public void raceOnce(Car car){
+        if(getRandomNumber() >= 4){
+            car.addProgress();
+        }
     }
 
     public int getRandomNumber(){
         int genNumber = Randoms.pickNumberInRange(0, 9);
         return genNumber;
     }
-
 }
--- src/main/java/racingcar/Controller/Validator.java
+++ src/main/java/racingcar/Controller/Validator.java
@@ -11,6 +11,8 @@ public class Validator {
     private final static String ERR_NAME_SPACE_CHAR = "자동차 이름에 공백문자를 사용할수는 없습니다.";
     private final static String ERR_LIST_IS_EMPTY = "최소 1개의 자동차 이름을 입력하세요.";
     private final static String ERR_NAME_IS_EMPTY = "자동차의 이름을 입력하세요.";
+    private final static String ERR_NUM_IS_NOT_NUM = "실행횟수에 숫자를 입력하세요.";
+    private final static String ERR_NUM_IS_BELOW_ONE = "실행횟수에 1이상의 숫자를 입력하세요.";
     private Set<String> carNameSet;
 
     public Validator(){
@@ -53,6 +55,19 @@ public class Validator {
         if(carNameList.isEmpty()){
             throw new IllegalArgumentException(ERR_LIST_IS_EMPTY);
        }
+    }
+
+    public int getValidNumber(String tryString){
+        try{
+            int intValue = Integer.parseInt(tryString);
+            if (intValue < 1) {
+                throw new IllegalArgumentException(ERR_NUM_IS_BELOW_ONE);
+            }
+            return intValue;
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(ERR_NUM_IS_NOT_NUM);
+        }
+
     }
 }
 
