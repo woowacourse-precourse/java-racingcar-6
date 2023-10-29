09e1f131e102c3f9636f00e9165b21f676631044
권장순 <jsoonworld@gmail.com>
2023-10-29 09:58:22 +0900

    feat(racingcar): main 함수에 지금까지 구현한 메서드들을 테스트해보며 확인

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,10 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CreateCar createCar = new CreateCar();
+        createCar.createdCar(createCar.input());
+        RacingRound racingRound = new RacingRound();
+        racingRound.attemptCount();
+
     }
 }
