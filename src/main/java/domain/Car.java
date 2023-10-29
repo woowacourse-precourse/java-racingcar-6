23d166cd7d36a08b328558eef84828dbf216c37b
songsunkook <songsunkook@gmail.com>
2023-10-29 21:04:28 +0900

    feat: 전진과 멈춤 test 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Car {
     private final String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
@@ -9,5 +10,17 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void go() {
+        position++;
+    }
+
+    public void stop() {
+        return;
     }
 }
