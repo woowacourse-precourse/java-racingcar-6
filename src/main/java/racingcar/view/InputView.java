4ba49a03a1c690f927984e5ae4a8e0fda2979bb1
bbbbooo <qlqlzbxkzl@naver.com>
2023-10-29 16:10:29 +0900

    refactor : 게임 시작 시, 자동차 이름 및 이동횟수 문구 상수화
    자동차 이름 및 시도 회수 상수화
    - START_SENTENCE 추가
    - MOVEMENT_SENTENCE 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,13 +13,17 @@ public class InputView {
 
     private MovementValidation movementValidation;
 
+    private final String START_SENTENCE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private final String MOVEMENT_SENTENCE = "시도할 회수는 몇회인가요?";
+
     public InputView() {
         carNameValidation = new CarNameValidation();
         movementValidation = new MovementValidation();
     }
 
     public List<RacingCar> inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(START_SENTENCE);
         String inputCarName = Console.readLine();
         carNameValidation.validateUserInput(inputCarName);
 
@@ -28,7 +32,7 @@ public class InputView {
 
     public Integer inputMovement() {
         try {
-            System.out.println("시도할 회수는 몇회인가요?");
+            System.out.println(MOVEMENT_SENTENCE);
             Integer movement = Integer.parseInt(Console.readLine());
             movementValidation.validateMovement(movement);
 
