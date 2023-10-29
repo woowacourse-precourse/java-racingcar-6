79cc98117e2294d4f8ca32a0ee960ea8fd5bd8a4
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-10-29 15:38:08 +0900

    Test: 추가 inputCarName 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,6 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,12 +18,12 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 전진_정지() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+        assertRandomNumberInRangeTest( // values 3으로 이뤄진 배열이 됨. 따라서 첫번째 pobi는 4, woni는 3의 값을 가짐.
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -30,6 +34,22 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 이름을_알맞게_나누는지() {
+        CallNsTestMethod testMethod = new CallNsTestMethod();
+        String inputName = "aaa,bbb,ccc,ddd, cc.dd";
+        testMethod.myCommand(inputName);
+        RacingGame racingGame = new RacingGame();
+        List<String> nameList = racingGame.inputCarName();
+        assertThat(nameList.get(0)).isEqualTo("aaa");
+        assertThat(nameList.get(1)).isEqualTo("bbb");
+        assertThat(nameList.get(2)).isEqualTo("ccc");
+        assertThat(nameList.get(3)).isEqualTo("ddd");
+        assertThat(nameList.get(4)).isEqualTo("cc.dd");
+    }
+
+
 
     @Override
     public void runMain() {
