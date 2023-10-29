e765ee7a56413a5c73cff29de7baa98b2c6439a0
jae woo Jung <awfjol2008@naver.com>
2023-10-30 04:28:24 +0900

    feat: 경주 시도 횟수 입력값이 빈 문자열인지 검증하는 기능 구현

--- src/test/java/racingcar/validator/GameCountValidator.java
+++ src/test/java/racingcar/validator/GameCountValidator.java
@@ -1,4 +1,10 @@
 package racingcar.validator;
 
 public class GameCountValidator {
+
+    private void validateIsEmpty(String gameCount) {
+        if (gameCount.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
 }
