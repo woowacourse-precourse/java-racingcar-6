a71d79fceb010fce8f237e3be543e5a7201c0642
rjsdn9025 <rjsdn9025@naver.com>
2023-10-30 00:02:19 +0900

    feat : 차수별 실행결과  및 우승결과출력 기능 추가

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -19,12 +19,12 @@ public class Controller {
         saveCars(parser.parseInputCarNames(getCarNamesByInput()));
         OutputView.printExecutionResult();
         attemptNum = getAttemptNumberByInput();
-        for (int i = 0; i < attemptNum; i++)
-        {
+        for (int i = 0; i < attemptNum; i++) {
             decideMovement();
             OutputView.printCarScore(carRepository);
             System.out.println();
         }
+        OutputView.printWinners(getWinner());
     }
 
     private void saveCars(List<String> carNames) {
@@ -32,6 +32,7 @@ public class Controller {
             carRepository.add(new Car(carName));
         }
     }
+
     private String getCarNamesByInput() {
         InputView.requestCarName();
         String input = Console.readLine();
@@ -43,11 +44,26 @@ public class Controller {
         int attemptNum = Integer.parseInt(Console.readLine());
         return attemptNum;
     }
+
     private void decideMovement() {
         for (int i = 0; i < carRepository.size(); i++) {
             if (Randoms.pickNumberInRange(0, 9) >= 4) {
                 carRepository.get(i).moveForward(1);
             }
         }
+    }
+
+    private List<String> getWinner() {
+        List<String> winner_name = carRepository.stream()
+                .filter(i -> i.getPosition() == maxPosition())
+                .map(i -> i.getName()).collect(Collectors.toList());
+        return winner_name;
+    }
+
+    private int maxPosition() {
+        int max_position = carRepository.stream()
+                .map(i -> i.getPosition())
+                .max(Integer::compare).get();
+        return max_position;
     }
 }
