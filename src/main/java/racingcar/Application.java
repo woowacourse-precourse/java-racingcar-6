d8d61e582d9efdc541cd50c845faa9cc69a88ae6
JIHYE <jdh324@naver.com>
2023-10-29 17:22:19 +0900

    feat: 자동차 이름 입력 기능 구현
    Console.readLine()을 이용하여 사용자에게 자동차 이름을 입력 받을 수 있는 기능을 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = inputCarName();
+    }
+
+    private static String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 }
