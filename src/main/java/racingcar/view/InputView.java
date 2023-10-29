04e43556a6216d02d3b50dec6ad65247adfbf8a2
Jinseo Jeong <js020315@gmail.com>
2023-10-29 20:02:05 +0900

    [REFECTOR] InputView
    Console.readLine(); 제거
    메소드 static으로 선언

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,11 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class InputView {
-    public void inputCars() {
+    public static void inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Console.readLine();
     }
 
-    public void inputTryNum() {
+    public static void inputAttemptsNum() {
         System.out.println("시도할 회수는 몇 회인가요?");
-        Console.readLine();
     }
 }
