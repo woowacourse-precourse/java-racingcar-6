a96a7af13af996ce459ca79fcd289cda36220393
YongJOoOo <we072200@naver.com>
2023-10-29 19:24:12 +0900

    refactor: CarController의 gameStart() 메소드 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -14,27 +14,12 @@ public class GameController {
     private List<Car> carList;
     private int gameCount;
 
-    //전체 게임 횟수만큼 게임 반복 및 실행 start 기능
     public void gameStart() {
         askInputUser();
-
-        //자동차 객체 임시 컨트롤
-        for (int i = 0; i < gameCount; i++) {
-            for (int j = 0; j < carList.size(); j++) {
-                //전진, 멈춤 판별
-                boolean flag = carService.judgeRunOrStop();
-                //판별된 상태에 따른 상태 세팅
-                carService.setCurrentCar(flag, carList.get(j));
-            }
-            output = new Output(carService);
-            //(1) 출력 : 현재의 게임 객체들 상태 출력
-            output.printCurrentCar(carList);
-        }
-        //(2) 출력 : 최종 우승자 출력
+        doGame();
         output.printFinalWinner(carList);
     }
 
-    //1) 사용자 입력 처리해서 객체에도 세팅
     private void askInputUser() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         carList = changeUserInputToCarList(userInput.readCarNames());
@@ -43,8 +28,25 @@ public class GameController {
         gameCount = userInput.readGameCount();
     }
 
-    //1) 입력받은 자동차 이름별 Car 클래스 생성 및 리스트에 담기
     private List<Car> changeUserInputToCarList(List<String> carNames) {
         return carNames.stream().map(Car::new).toList();
+    }
+
+    private void doGame() {
+        for (int i = 0; i < gameCount; i++) {
+            doStep();
+            output = new Output(carService);
+            //(1) 출력 : 현재의 게임 객체들 상태 출력
+            output.printCurrentCar(carList);
+        }
+    }
+
+    private void doStep() {
+        for (int j = 0; j < carList.size(); j++) {
+            //전진, 멈춤 판별
+            boolean flag = carService.judgeRunOrStop();
+            //판별된 상태에 따른 상태 세팅
+            carService.setCurrentCar(flag, carList.get(j));
+        }
     }
 }
