207ea8ee892e5d9f76fb01d207d4b78b16bb2c6d
ykmxxi <ykmxxi97@gmail.com>
2023-10-29 11:07:37 +0900

    feat(Car): 자동차 이름 검증 기능 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,11 +1,30 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @ValueSource(strings = {"k", "stark"})
+    @DisplayName(("자동차 이름 검증 통과"))
+    void givenName_whenCreateCar_thenNoException(String name) {
+        assertThatNoException().isThrownBy(() -> new Car(name));
+    }
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @ValueSource(strings = {"", "  ", "pooobi", " pobi", "pobi ", "po bi"})
+    @DisplayName("자동차 이름 검증 실패")
+    void givenName_whenCreateCar_thenThrowException(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("랜덤 값이 4 이상이면 자동차 전진")
