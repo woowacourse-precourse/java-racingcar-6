37d50c689bd9cc32917e495a88be7f3d468b0669
Nara Park <skfk6352@naver.com>
2023-10-30 01:04:27 +0900

    <refactor>: modify method for validating game count
    게임 횟수에 대한 입력값이 0으로 시작할 경우 예외가 발생하도록 구현 및 그 외의 자리는 0~9까지의 숫자 허용

--- src/main/java/racingcar/validation/GameCountValidator.java
+++ src/main/java/racingcar/validation/GameCountValidator.java
@@ -4,16 +4,20 @@ import java.util.regex.Pattern;
 
 public class GameCountValidator {
 
-    private static final Pattern GAME_COUNT_REGEX = Pattern.compile("[1-9]+");
+    private static final Pattern GAME_COUNT_REGEX = Pattern.compile("[0-9]+");
     private static final String GAME_COUNT_TYPE_EXCEPTION_MESSAGE = "게임 횟수는 자연수여야 한다.";
 
     public void validateGameCount(String userInput) {
-        if (!validateCountType(userInput)) {
+        if (!validateCountType(userInput) || validateStartZero(userInput)) {
             throw new IllegalArgumentException(GAME_COUNT_TYPE_EXCEPTION_MESSAGE);
         }
     }
 
     private boolean validateCountType(String userInput) {
         return GAME_COUNT_REGEX.matcher(userInput).matches();
+    }
+
+    private boolean validateStartZero(String userInput) {
+        return userInput.charAt(0) == '0';
     }
 }
