8ad2e129ea0b60bbab1ef5484b3a781a8648af56
master <cj855695@gmail.com>
2023-10-29 15:13:09 +0900

    Cars.java -> CarsManage.java & Car 클래스 추가 & inputCars 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,8 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        
     }
 }
--- src/main/java/racingcar/CarsManage.java
+++ src/main/java/racingcar/CarsManage.java
@@ -1,21 +1,22 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Cars {
-    List<String> cars = new ArrayList<>();
-    public List<String> inputCars() {
+public class CarsManage {
+    List<Car> cars = new ArrayList<>();
+    public void inputCars() {
         String carInput = Console.readLine();
         StringTokenizer stringTokenizer = new StringTokenizer(carInput, ",");
 
         while (stringTokenizer.hasMoreTokens()) {
-            cars.add(stringTokenizer.nextToken());
+            Car car = new Car();
+            car.setName(stringTokenizer.nextToken());
+            cars.add(car);
         }
-        return cars;
     }
-
 }
