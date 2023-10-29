2d50a7ccefe2c2675fc7d1f73446cb839c7e767a
IMWoo94 <leemeawoo94@gmail.com>
2023-10-29 15:46:58 +0900

    test : racingCars 테스터 작성
    racingCars 테스터 작성

---
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RacingCarsTest {
+
+    RacingCars racingCars;
+
+    int movingCount = 5;
+
+    @BeforeEach
+    void setUp() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("toby"));
+        cars.add(new Car("ben"));
+        cars.add(new Car("lee"));
+
+        racingCars = new RacingCars(cars);
+
+        for (int i = 0; i < movingCount; i++) {
+            racingCars.MovingCars();
+        }
+    }
+
+    @Test
+    void movingCarsTest() {
+        String[] winners = racingCars.getRacingWinners();
+
+        assertThat(winners.length).isGreaterThan(0);
+        assertThat(winners).contains("toby", "ben", "lee");
+    }
+
+}
