c3760d31f0529a85216ee7e15bb5a3c841270d3f
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-30 00:32:20 +0900

    add(Scanner) : add func to validate input

--- src/main/java/racingcar/Scanner.java
+++ src/main/java/racingcar/Scanner.java
@@ -6,29 +6,46 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Scanner {
-    private int minimumOfRange;
-    private int maximumOfRange;
-    private int numberOfCars;
     private String delimiter;
+    private int maximumNameLength;
 
-    Scanner(Configurations configurations){
-        minimumOfRange = configurations.getMinimumOfRange();
-        maximumOfRange = configurations.getMaximumOfRange();
-        numberOfCars = configurations.getNumberOfNumbers();
+    Scanner(Configurations configurations) {
         delimiter = configurations.getDelimiter();
+        maximumNameLength = configurations.getMaximumNameLength();
     }
-    List<String> inputCarNames(){
-        List<String>carNames = new ArrayList<>();
+
+    List<String> inputCarNames() {
+        List<String> carNames = new ArrayList<>();
         String inputString = Console.readLine();
-        StringTokenizer tokenizedInputString = new StringTokenizer(inputString,delimiter);
+        StringTokenizer tokenizedInputString = new StringTokenizer(inputString, delimiter);
         while (tokenizedInputString.hasMoreTokens()) {
-            String nextCar = tokenizedInputString.nextToken();
-            carNames.add(nextCar);
+            String nextCarName = tokenizedInputString.nextToken();
+            validate(isInvalidCarName(nextCarName));
+            carNames.add(nextCarName);
         }
         return carNames;
     }
-    Integer inputNumberOfRound(){
-        Integer numberOfRound = Integer.parseInt(Console.readLine());
-        return numberOfRound;
+
+    Integer inputNumberOfRound() {
+        String inputString = Console.readLine();
+        validate(isInvalidNumber(inputString));
+        return Integer.parseInt(inputString);
+    }
+
+    private Boolean isInvalidCarName(String carName) {
+        return (carName.length() > maximumNameLength);
+    }
+
+    private Boolean isInvalidNumber(String inputString) {
+        if (inputString.isBlank()) {
+            return true;
+        }
+        return (inputString.charAt(0) < '0' || inputString.charAt(0) > '9');
+    }
+
+    private void validate(Boolean condition) {
+        if (condition) {
+            throw new IllegalArgumentException();
+        }
     }
 }
