22e4855b47279d8ba28f308e50503dea40f9b5de
minisyu <iswater.kim@gmail.com>
2023-10-29 13:07:18 +0900

    test(CarNameTest): CarName 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/game/CarNameTest.java
@@ -0,0 +1,34 @@
+package racingcar.domain.game;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.util.Err;
+
+class CarNameTest {
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름이 null 또는 길이가 0인 경우, CarName 객체 생성 시 예외 발생")
+    @ValueSource(strings = {"", " "})
+    void checkCarName(String name) {
+        // when
+        // then
+        assertThatThrownBy(() -> CarName.from(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Err.INVALID_CAR_NAME_MIN_LENGTH.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름의 길이가 5글자를 넘어가면 CarName 객체 생성 시 예외 발생")
+    @ValueSource(strings = {"rrrrrrrr", "pobiii", "yehhhhhhhhhhhhhhhh"})
+    void checkCarNameMaxLength(String name) {
+        // when
+        // then
+        assertThatThrownBy(() -> CarName.from(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Err.INVALID_CAR_NAME_MAX_LENGTH.getMessage());
+    }
+
+}
