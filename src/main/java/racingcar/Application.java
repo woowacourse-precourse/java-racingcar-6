b2d002d535dc5af1c8ad56c4fb8ab656cb3db4f9
yeonjae02 <0yeonjae2@naver.com>
2023-10-30 03:08:59 +0900

    feat: Implement gameStart()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,38 @@
 package racingcar;
 
+import java.util.StringTokenizer;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+	public int tryNumber;
+	public class PlayerInfo{
+		private String name;
+		private int loc;
+		public PlayerInfo(String name, int loc) {
+			this.name = name;
+			this.loc = loc;
+		}
+	}
+	
+	public void gameStart() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String input = Console.readLine();
+		StringTokenizer st = new StringTokenizer(input, ",");
+		
+		System.out.println("시도할 회수는 몇회인가요?");
+		String num = Console.readLine();
+		tryNumber = Integer.parseInt(num);
+		PlayerInfo[] data = new PlayerInfo[st.countTokens()];
+		
+		int i=0;
+		while (st.hasMoreElements()) {
+			data[i] = new PlayerInfo(st.nextToken(), 0);
+			i++;
+		}
+	}
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	Application game = new Application();
+    	game.gameStart();
     }
 }
