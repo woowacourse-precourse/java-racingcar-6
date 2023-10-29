dfa344f846bb54a46f820b687e213f18dfe670ec
OhJongMin <jmoh7480@gmail.com>
2023-10-29 18:06:36 +0900

    feat(racingcar):경주 자동차 이름 중복 확인 객체 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,13 @@ import java.util.List;
 import racingcar.Service.InputException;
 
 public class InputView {
-    InputException ex = new InputException();
+    private InputException ex = new InputException();
 
     public List<String> inputCarNames(){
         String userInput = Console.readLine();
         List<String> names = ex.divideCarNames(userInput);
-        ex.validCarName(names);
+        ex.validCarNameRange(names);
+        ex.validCarNameDuplicated(names);
         return names;
     }
 
