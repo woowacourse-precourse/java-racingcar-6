283c9d504dfd6732b75d41bb012ec4e7a0dffb3e
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-10-30 03:58:50 +0900

    feat(InputView) : 자동차 몇 번의 이동을 할 것인지에 대해 입력 기능 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,10 +1,12 @@
 package racingcar.controller;
 
+
 import java.util.Arrays;
 import java.util.List;
-import racingcar.view.InputView;
+
 import racingcar.domain.Car;
 import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
 
 public class RacingGameController {
     private final RacingCarService racingGameService;
@@ -12,9 +14,16 @@ public class RacingGameController {
     public RacingGameController() {
         this.racingGameService = new RacingCarService();
     }
-    public void playRacingGame(){
+
+    public void playRacingGame() {
         String input = InputView.inputCarsName();
         List<String> cars = Arrays.asList(input.split(","));
         List<Car> carList = racingGameService.participateCar(cars);
+
+        int moveCount = InputView.inputMovesCount();
+        while (moveCount-- > 0) {
+            RacingCarService.moveCar();
+        }
+
     }
 }
--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -10,15 +10,16 @@ public class RacingCarService {
     private static final int RANDOM_MIN_NUMBER = 0;
     private static final int RANDOM_MAX_NUMBER = 9;
     private static final int MOVE_CONDITION = 4;
-    private final List<Car> carList = new ArrayList<>();
+    private static final List<Car> carList = new ArrayList<>();
 
     public List<Car> participateCar(List<String> carsName) {
-        for(String name : carsName){
+        for (String name : carsName) {
             carList.add(new Car(name));
         }
         return carList;
     }
-    public void moveCar() {
+
+    public static void moveCar() {
         List<Integer> randomNumbers = generateRandomNumbers();
         for (int i = 0; i < carList.size(); i++) {
             if (randomNumbers.get(i) >= MOVE_CONDITION) {
@@ -26,7 +27,8 @@ public class RacingCarService {
             }
         }
     }
-    private List<Integer> generateRandomNumbers() {
+
+    private static List<Integer> generateRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
         for (int i = 0; i < carList.size(); i++) {
             randomNumbers.add(Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER));
