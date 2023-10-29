e3a9805c60289c20a5c003461fcab6b246b79982
jae woo Jung <awfjol2008@naver.com>
2023-10-30 01:37:25 +0900

    feat: 자동차 이름 목록 입력값이 빈 문자열인지 검증하는 기능 구현

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -1,4 +1,10 @@
 package racingcar.validator;
 
 public class CarNameValidator {
+
+    private static void validateEmpty(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
 }
