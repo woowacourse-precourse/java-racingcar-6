06741d498377777edda1369dc7c3fecdee5a74ce
becooq81 <becooq81@gmail.com>
2023-10-30 05:53:50 +0900

    feat(ParserTest): completed list of tests to be achieved once parser is completed - successful test cases for parsing valid inputs for car names and number of trials - if error occurs during validation process, error should occur in parser class too

--- src/test/java/racingcar/util/ParserTest.java
+++ src/test/java/racingcar/util/ParserTest.java
@@ -1,2 +1,41 @@
-package racingcar.util;public class ParserTest {
+package racingcar.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest extends NsTest{
+
+    Parser parser = new Parser();
+
+    @Test
+    void 횟수_검증_성공_후_횟수_숫자_반환() {
+        int numTrial = Parser.parseNumTrial("5");
+        assertThat(numTrial).isEqualTo(5);
+    }
+
+    @Test
+    void 자동차_이름_검증_성공_후_자동차_이름_리스트_반환() {
+        List<String> carNames = Parser.parseCarNames("a,b,c");
+        assertThat(carNames).containsExactly("a", "b", "c");
+    }
+
+    @Test
+    void 자동차_이름_오류_발생시_예외_발생() {
+        assertThatThrownBy(() -> Parser.parseCarNames("a,,b"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 횟수_오류_발생시_예외_발생() {
+        assertThatThrownBy(() -> Parser.parseNumTrial("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    protected void runMain() {
+    }
 }
