cd31502381a4124ebb36f988654337d0c82f3ba5
김강년 <64246800+ekek54@users.noreply.github.com>
2023-10-29 17:50:55 +0900

    feat(view.InputView): 자동차 이름을 쉼표로 구분하여 입력 받는다
    View 입력 코드 작성
    테스트 코드 작성

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_NAMES_INPUT_DELIMITER = ",";
+
+    public List<String> inputCarNames() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        String inputString = Console.readLine();
+        String[] splitByDelimiter = inputString.split(CAR_NAMES_INPUT_DELIMITER);
+        return Arrays.stream(splitByDelimiter).toList();
+    }
+}
---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    private InputView inputView = new InputView();
+
+    @Test
+    void 쉽표를_기준으로_구분하여_입력받는다() {
+        // given
+        String inputString = "pobi,woni,jun";
+        // when
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        List<String> carNames = inputView.inputCarNames();
+        // then
+        Assertions.assertThat(carNames).containsExactly("pobi", "woni", "jun");
+    }
+}
