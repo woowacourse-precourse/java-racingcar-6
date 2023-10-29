86707c1d98500ce24515a382162ad172117f4db8
minseo  jo <liging12@naver.com>
2023-10-29 19:23:46 +0900

    refactor: 멤버 변수 명 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,25 +7,25 @@ public class Car {
     private static long count = 0L; // ID 를 위한 정적 변수 (생성자를 호출할 때 마다, 카운트 증가 → 각각의 자동차 ID)
 
     private final long id; // 중복 이름을 고려한, 자동차 ID
-    private final CarName carName;
-    private int forwardCount;
+    private final CarName name;
+    private int position;
 
     public Car(final String name) {
         this.id = count++;
-        this.carName = new CarName(name);
-        this.forwardCount = 0;
+        this.name = new CarName(name);
+        this.position = 0;
     }
 
     public String getName() {
-        return carName.toString();
+        return name.toString();
     }
 
     public int getForwardCount() {
-        return forwardCount;
+        return position;
     }
 
     public void forward() {
-        forwardCount++;
+        position++;
     }
 
     // 테스트 용 코드
