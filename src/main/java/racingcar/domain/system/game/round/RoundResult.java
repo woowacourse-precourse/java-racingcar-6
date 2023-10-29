306e9ed78468f62c383c3cd086000abec9f5a463
belljun3395 <195850@jnu.ac.kr>
2023-10-29 23:32:40 +0900

    test(domain/system): RoundResult 구현

---
+++ src/main/java/racingcar/domain/system/game/round/RoundResult.java
@@ -0,0 +1,22 @@
+package racingcar.domain.system.game.round;
+
+import racingcar.domain.core.car.OnRaceCar;
+import racingcar.domain.core.car.OnRaceCars;
+
+public class RoundResult {
+
+    private final OnRaceCars cars;
+
+    public RoundResult(OnRaceCars cars) {
+        this.cars = cars;
+    }
+
+
+    public OnRaceCars getAllCars() {
+        return cars;
+    }
+
+    public Long getPosition(OnRaceCar car) {
+        return cars.getPosition(car);
+    }
+}
