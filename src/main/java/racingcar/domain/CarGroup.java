47424148dba171676a39bec2d6b34d2988cfb136
Gyeongho Yang <chchch1213@naver.com>
2023-10-29 18:22:28 +0900

    feat(global): 자동차 이름 길이를 제한 하는 validate 함수 추가

--- src/main/java/racingcar/domain/CarGroup.java
+++ src/main/java/racingcar/domain/CarGroup.java
@@ -6,6 +6,8 @@ import racingcar.global.RaceException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static racingcar.global.Constant.MAX_CAR_NAME_LENGTH;
+
 public class CarGroup {
     private Map<String, Integer> cars;
 
@@ -18,11 +20,18 @@ public class CarGroup {
         Map<String, Integer> cars = new HashMap<>();
         for (String carName : carNamesCombinedWithComma.split(",")) {
             String trimmedCarName = carName.trim();
+            validateLength(trimmedCarName);
             validateDuplicate(cars, trimmedCarName);
             validateEmpty(trimmedCarName);
             cars.put(trimmedCarName, 0);
         }
         return new CarGroup(cars);
+    }
+
+    private static void validateLength(String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw RaceException.of(ErrorMessage.EXCEED_CAR_NAME_LENGTH);
+        }
     }
 
     private static void validateDuplicate(Map<String, Integer> cars, String carName) {
