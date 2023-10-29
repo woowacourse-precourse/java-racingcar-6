b869bd47684f0ef497f73a642fca62e6d3fa2e1d
HyunShik <hshong0907@gmail.com>
2023-10-29 19:24:47 +0900

    feat: car List sort 후 우승자 collection 반환 메소드 구현

---
+++ src/main/java/service/CarRacingWinnerService.java
@@ -0,0 +1,32 @@
+package service;
+
+import domain.Car;
+import domain.Cars;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class CarRacingWinnerService {
+
+    public List<Car> selectWinner(Cars cars){
+        ArrayList<Car> carList = cars.getCars();
+        sort(carList);
+        int lastIndex = findLastIndex(carList);
+        return carList.subList(0,lastIndex);
+    }
+
+    private void sort(ArrayList<Car> carList){
+        carList.sort((a, b) -> b.getMoveCount() - a.getMoveCount());
+    }
+
+    //정렬된 배열에서 우승자까지의 index 반환
+    private int findLastIndex(ArrayList<Car> carList){
+        int topMoveCountValue = carList.get(0).getMoveCount();
+        return IntStream.range(1, carList.size())
+                .filter(index -> carList.get(index).getMoveCount() != topMoveCountValue)
+                .findFirst()
+                .orElse(-1);
+    }
+}
