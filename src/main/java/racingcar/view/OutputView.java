a5d1c55f2f28a2439590ec4e547db369fc0e2254
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 04:44:51 +0900

    refactor: modify method and variable name for fit purpose

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,6 @@ import racingcar.property.OutputProperty;
 
 public class OutputView {
     public void outputForRaceStart(){
-        System.out.println(OutputProperty.GAME_START);
+        System.out.println(OutputProperty.RACE_START);
     }
 }
