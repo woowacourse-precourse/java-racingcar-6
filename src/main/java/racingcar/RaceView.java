ced9ca5ba3b2e1a15e474bf9f8b9240649755bd4
Oseuseu <lakingsung@gmail.com>
2023-10-30 03:24:43 +0900

    feat: printAttemptInputMessage 메서드 구현

--- src/main/java/racingcar/RaceView.java
+++ src/main/java/racingcar/RaceView.java
@@ -34,4 +34,8 @@ public class RaceView {
     public void printCarListInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public void printAttemptInputMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
