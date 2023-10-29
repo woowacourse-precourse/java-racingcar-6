2fa7d31856583a7aee6fa2ed9823ffed7bdbd9e3
OhJongMin <jmoh7480@gmail.com>
2023-10-29 16:38:33 +0900

    feat(racingcar):사용자 input 객체

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public void inputCarNames(){
+        String userInput = Console.readLine();
+    }
+
+    public void inputTryCount(){
+        String userInput = Console.readLine();
+    }
+}
