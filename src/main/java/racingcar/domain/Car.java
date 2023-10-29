fc512e86f934913c6347c527939e990d3104a8de
NAM GIBEOM <rlqja0523@naver.com>
2023-10-29 18:01:59 +0900

    feat(Car): 자동차 클래스 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int movedCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.movedCount = 0;
+    }
+}
