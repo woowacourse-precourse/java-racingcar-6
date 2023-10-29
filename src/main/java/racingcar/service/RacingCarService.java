c5e1f9d5a2ee8a1eeb6c78ec9e842291e64899c7
kunsanglee <gosmdochee2@gmail.com>
2023-10-29 11:34:00 +0900

    refactor(RacingCarService): 자동차 조립을 레이싱 시작시 하도록 수정 - 기존에는 컨트롤러에서 사용자에게 입력받은 이름으로 자동차 조립을 했는데, 자동차 이름과 시도 횟수를 모두 입력받은 후 RacingCarService의 레이싱을 시작하는 race() 메서드 내에서 레이싱 시작 전 조립을 하여 시도 횟수만큼 하는 것으로 로직을 수정했다.

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -2,13 +2,22 @@ package racingcar.service;
 
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.domain.CarEngine;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingResult;
 import racingcar.domain.RacingRoundResult;
 
 public class RacingCarService {
+    private final RacingCarAssemblyService racingCarAssemblyService;
+    private final CarEngine carEngine;
 
-    public RacingResult race(Cars cars, int attemptCounts) {
+    public RacingCarService(RacingCarAssemblyService racingCarAssemblyService, CarEngine carEngine) {
+        this.racingCarAssemblyService = racingCarAssemblyService;
+        this.carEngine = carEngine;
+    }
+
+    public RacingResult race(List<String> carNames, int attemptCounts) {
+        Cars cars = racingCarAssemblyService.assemble(carNames, carEngine);
         RacingResult racingResult = new RacingResult();
         for (int i = 0; i < attemptCounts; i++) {
             cars.move();
