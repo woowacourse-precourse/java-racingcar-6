b9c4d923590ef8269e72883156bf15fc78a67ffc
sami355-24 <ddung355@gmail.com>
2023-10-30 03:26:21 +0900

    test(Model): 시도 횟수 전처리에 대한 테스트 코드 이름 수정
    - 한글로 표시되게 수정

--- src/test/java/racingcar/Model/RoundModelTest.java
+++ src/test/java/racingcar/Model/RoundModelTest.java
@@ -2,12 +2,14 @@ package racingcar.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RoundModelTest {
 
     private final RoundModel roundModel = new RoundModel();
     @Test
+    @DisplayName("시도 횟수에 대한 전처리 확인")
     void preProcessing() {
         //given
         String round = "5";
