aa1bdae23076667ee5f15224ed58437bff310947
jihwankim128 <jihwan1535@nate.com>
2023-10-29 22:15:31 +0900

    feat: 경기횟수 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,15 @@ package racingcar;
 
 import java.io.Console;
 import java.util.List;
+import racingcar.controller.RacingCupController;
 import racingcar.view.GameView;
 
 public class Application {
     public static void main(String[] args) {
         GameView gameView = new GameView();
         List<String> carNames = gameView.getCarNames();
-        int totalRounds = gameView.getTotalRounds();
+
+        RacingCupController racingCupController = new RacingCupController(gameView.getTotalRounds());
+
     }
 }
