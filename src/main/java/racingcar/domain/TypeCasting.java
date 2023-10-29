978fa9604b9d5553146fc948fa4c54b3f0e46cca
최우종 Choi Woojong <dwax1324@gmail.com>
2023-10-29 19:07:30 +0900

    reafactor(TypeCasting): stringToInteger 메소드 삭제, Integer.parseInt 사용

--- src/main/java/racingcar/domain/TypeCasting.java
+++ src/main/java/racingcar/domain/TypeCasting.java
@@ -1,10 +1,8 @@
 package racingcar.domain;
 
-import racingcar.view.validator.InputValidator;
-
 public class TypeCasting {
     public static RacingCars stringToRacingCars(String stringCars) {
-        InputValidator.validateCarName(stringCars);
+//        InputValidator.validateCarName(stringCars);
         RacingCars racingcars = new RacingCars();
         String[] stringArrayCars = stringCars.split(",");
         for (String car : stringArrayCars) {
@@ -13,8 +11,4 @@ public class TypeCasting {
         return racingcars;
     }
 
-    public static int stringToInteger(String number) {
-        // validate(number);
-        return Integer.parseInt(number);
-    }
 }
