d1ceabe6ef1630cbf8410f5af333f1e7c2ac1aae
suhyun__ <thebbbb1206@gmail.com>
2023-10-30 01:11:20 +0900

    feat: 입력값에 쉼표 외 특수 기호 혹은 숫자가 포함된 경우 검증

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,6 +1,7 @@
 package racingcar.validator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputValidator {
     private String input;
@@ -13,6 +14,12 @@ public class InputValidator {
     private void isNullOrIsEmpty() {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("아무 것도 입력하지 않았습니다.");
+        }
+    }
+
+    private void includeNumberOrSymbol() {
+        if (!Pattern.matches("^[a-z|A-Z|,]*$", input)) {
+            throw new IllegalArgumentException("쉼표 외 특수 기호 혹은 숫자가 포함되어 있습니다.");
         }
     }
 }
