f371539865333f14409d522afc81a71127fbae97
BINBEAN777 <peanutchan@naver.com>
2023-10-29 16:17:37 +0900

    <refactor> : 전진상황 출력 메소드 변경 showHowMuchGo 메소드에서 전진상황을 출력하지 않고 String 배열만 넘겨줌으로써 tellCurrent 메소드에서 출력할 수 있도록 변경하였다.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,28 +4,32 @@ import java.util.HashMap;
 
 public class Output {
 
-    public static void tellStartAndWantName(){
+    public static void tellStartAndWantName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    };
-
-    public static void tellGroupName(String [] groupName){
-        System.out.println(String.join(",",groupName));
-        }
-
-   public static void tellCurrent(HashMap map,String [] groupCar){
-        for(String name : groupCar){
-            showHowMuchGo((Integer)map.get(name));
-        }
-   }
-   public static void showHowMuchGo(int howMuchGoNum){
-       String line = "";
-        for(int i = 0; i <howMuchGoNum; i++){
-            line+="-";
-        }
-        System.out.println(line);
-   }
-
     }
+
+    ;
+
+    public static void tellGroupName(String[] groupName) {
+        System.out.println(String.join(",", groupName));
+    }
+
+    public static void tellCurrent(HashMap map, String[] groupCar) {
+        for (String name : groupCar) {
+            String line = showHowMuchGo((Integer) map.get(name));
+            System.out.println(name + " : " + line);
+        }
+    }
+
+    public static String showHowMuchGo(int howMuchGoNum) {
+        String line = "";
+        for (int i = 0; i < howMuchGoNum; i++) {
+            line += "-";
+        }
+        return line;
+    }
+
+}
 
 
 
