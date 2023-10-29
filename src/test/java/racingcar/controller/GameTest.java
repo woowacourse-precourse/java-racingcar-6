22201e5ccf9de3e19290bb7750fec8047afad516
chae401 <chae401@sju.ac.kr>
2023-10-29 17:40:41 +0900

    test(GameTest): 실행 횟수가 0이하일 경우

--- src/test/java/racingcar/controller/GameTest.java
+++ src/test/java/racingcar/controller/GameTest.java
@@ -1,6 +1,5 @@
 package racingcar.controller;
 
-import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.RacingcarGame;
@@ -9,45 +8,37 @@ import racingcar.domain.dto.InitDTO;
 class GameTest {
     private static final String RIGHT_INPUT_ABOUT_NAMES = "toto,hoho,koko";
     private static final String RIGHT_INPUT_ABOUT_ROUNDS = "4";
-    @Test
-    void 조건에_맞는_차량이름과_실행횟수_입력(){
-        Assertions.assertThatCode(() -> {
-                    InitDTO initDTO = new InitDTO(RIGHT_INPUT_ABOUT_NAMES, RIGHT_INPUT_ABOUT_ROUNDS);
 
-                    new RacingcarGame(initDTO);
-                })
+    @Test
+    void 조건에_맞는_차량이름과_실행횟수_입력() {
+        Assertions.assertThatCode(
+                        () -> new RacingcarGame(new InitDTO(RIGHT_INPUT_ABOUT_NAMES, RIGHT_INPUT_ABOUT_ROUNDS)))
                 .doesNotThrowAnyException();
 
     }
 
     @Test
-    void 차량이름의_크기_5초과(){
+    void 차량이름의_크기_5초과() {
         // given
         String names = "tototo,hoho";
 
         // then
-        Assertions.assertThatThrownBy(() ->
-            new RacingcarGame(new InitDTO(names, RIGHT_INPUT_ABOUT_ROUNDS))
-                )
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차의 이름은 5글자 이하 여야 합니다.");
+        Assertions.assertThatThrownBy(() -> new RacingcarGame(new InitDTO(names, RIGHT_INPUT_ABOUT_ROUNDS)))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("자동차의 이름은 5글자 이하 여야 합니다.");
     }
 
     @Test
-    void 실행횟수_0이하(){
+    void 실행횟수_0이하() {
         // given
         String rounds = "-1";
 
         // then
-        Assertions.assertThatThrownBy(() -> {
-            InitDTO initDTO = new InitDTO(RIGHT_INPUT_ABOUT_NAMES, rounds);
-            new RacingcarGame(initDTO);
-        })
-
+        Assertions.assertThatThrownBy(() -> new RacingcarGame(new InitDTO(RIGHT_INPUT_ABOUT_NAMES, rounds)))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("실행 횟수를 양수로 입력해 주세요.");
     }
 
     @Test
-    void 실행횟수에_문자_포함(){
+    void 실행횟수에_문자_포함() {
 
     }
 
