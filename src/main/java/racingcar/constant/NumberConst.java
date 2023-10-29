0a85c279f19f85b3ccdf8517b996683e86488d73
acs0209 <annjae4676@naver.com>
2023-10-30 00:19:20 +0900

    feat: add number constant
    숫자 관련 상수 관리 클래스 생성

---
+++ src/main/java/racingcar/constant/NumberConst.java
@@ -0,0 +1,21 @@
+package racingcar.constant;
+
+/**
+ * 숫자 관련 상수 관리 클래스
+ */
+public class NumberConst {
+
+    public static final int CAR_NAME_MIN_LENGTH = 1;
+
+    public static final int CAR_NAME_MAX_LENGTH = 5;
+
+    public static final int RACE_MIN_ROUND = 1;
+
+    public static final int RANDOM_MIN_NUMBER = 4;
+
+    public static final int RANDOM_MIN_RANGE = 0;
+
+    public static final int RANDOM_MAX_RANGE = 9;
+
+    public static final int FORMAT_NUMBER = 2;
+}
