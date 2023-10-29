5357fdef3c377efe4c5c246362365a9184535062
minseo  jo <liging12@naver.com>
2023-10-29 22:09:11 +0900

    refactor: 매직넘버 상수로 변경

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
 
+    private final static int NUMBER_MIN_VALUE = 0;
+    private final static int NUMBER_MAX_VALUE = 9;
+
     private RandomNumberGenerator() {}
 
     static public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(NUMBER_MIN_VALUE, NUMBER_MAX_VALUE);
     }
 }
