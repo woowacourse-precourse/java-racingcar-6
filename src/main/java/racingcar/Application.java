dc2b4aef9373c51fb1acbf1807ca7d1a17a963a5
mumpuris <hst990124@naver.com>
2023-10-31 00:36:13 +0900

    프로그램 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import domain.Race;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Race race = Race.getInstance();
+        race.start();
+
     }
 }
