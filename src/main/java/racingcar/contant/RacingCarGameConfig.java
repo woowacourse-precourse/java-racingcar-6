25317189f04c4b9552d99ab130c06a82cedab36d
Kite_Z <duswl339@ajou.ac.kr>
2023-10-30 19:49:44 +0900

    delete: remove global package and move RacingCarGameConfig

--- src/main/java/racingcar/contant/RacingCarGameConfig.java
+++ src/main/java/racingcar/contant/RacingCarGameConfig.java
@@ -1,8 +1,9 @@
-package racingcar.global;
+package racingcar.contant;
 
 public class RacingCarGameConfig {
     private RacingCarGameConfig() {}
     public static int CAR_NAME_MAX_LENGTH = 5;
+    public static int CAR_NAME_MIN_LENGTH = 1;
     public static int RANDOM_MAX_NUMBER = 9;
     public static int RANDOM_MIN_NUMBER = 1;
     public static int STANDARD_NUMBER = 4;
