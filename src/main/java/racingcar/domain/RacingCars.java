4f312ff35230ace7f5d8b0b894114ff75c82e4ed
jiixon <jr0107380@gmail.com>
2023-10-31 02:51:23 +0900

    feat: carNames (,)로 파싱 후 리스트변환

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,2 +1,22 @@
-package racingcar.domain;public class RacingCars {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCars {
+    private List<Car> racingCars;
+
+    public RacingCars(String carNames) {
+       initRacingCars(carNames);
+    }
+
+    public void initRacingCars(String carNames){
+        String[] carNameIndex = carNames.split(",");
+        this.racingCars = new ArrayList<>();
+
+        for(String car : carNameIndex){
+            this.racingCars.add(new Car(car));
+        }
+    }
+
 }
