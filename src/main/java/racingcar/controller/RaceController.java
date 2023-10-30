410de782bf1951f579620f6e6e8500d551c0770f
yhcho0405 <yhcho0405@kakao.com>
2023-10-30 15:17:15 +0900

    refactor: 서비스 레이어 추가, 구조 변경

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,12 +1,29 @@
 package racingcar.controller;
 
+import racingcar.service.RaceService;
 import racingcar.view.InputView;
 
 public class RaceController {
+    private RaceService raceService;
+
+    public RaceController() {
+        this.raceService = new RaceService();
+    }
 
     public void start() {
+        initializeRace();
+        executeRace();
+        printResults();
+    }
+
+    private void initializeRace() {
         String carNames = InputView.getCarNames();
         int rounds = InputView.getRounds();
+    }
 
+    private void executeRace() {
+    }
+
+    private void printResults() {
     }
 }
