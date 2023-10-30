23ca35a51e0716162fe4a16030f4ee75841bd548
유서린 <sprout3082@gmail.com>
2023-10-31 02:00:33 +0900

    [feat] Course Unit 구현

---
+++ src/main/java/racingcar/game/unit/Course.java
@@ -0,0 +1,26 @@
+package racingcar.game.unit;
+
+import java.util.List;
+
+public class Course {
+
+	private int attempt;
+
+	public static Course of(int attempt) {
+		if (attempt < 0) {
+			throw new IllegalArgumentException("시도 횟수는 0 이상이어야 합니다.");
+		}
+		return new Course(attempt);
+	}
+
+	private Course(int attempt) {
+		this.attempt = attempt;
+	}
+
+	public void racingStart(List<Car> cars, Referee referee) {
+		for (int i = 0; i < attempt; i++) {
+			cars.forEach(Car::startEngine);
+			referee.broadcast(cars);
+		}
+	}
+}
