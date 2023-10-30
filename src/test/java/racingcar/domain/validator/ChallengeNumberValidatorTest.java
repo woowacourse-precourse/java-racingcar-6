2c0fd75c71928c015e1f4d1006c1e3ac6c61036b
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-30 18:21:30 +0900

    test(ChallengeNumberValidatorTest): add ChallengeNumberValidatorTest
    ChallengeNumberValidatorTest 작성

---
+++ src/test/java/racingcar/domain/validator/ChallengeNumberValidatorTest.java
@@ -0,0 +1,45 @@
+package racingcar.domain.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ChallengeNumberValidatorTest {
+    @Test
+    public void 정상적인_입력() {
+        String number = "3";
+
+        ChallengeNumberValidator.validate(number);
+    }
+
+    @Nested
+    class 예외_발생 {
+        @Test
+        public void 문자로_시작() {
+            String input = "d12";
+
+            exception_generator(input);
+        }
+
+        @Test
+        public void 숫자로_시작하는데_중간에_문자가_있는_경우() {
+            String input = "1a3";
+
+            exception_generator(input);
+        }
+
+        @Test
+        public void Integer_범위를_벗어나는_경우() {
+            String input = "123456789123456789123456789123456789";
+
+            exception_generator(input);
+        }
+
+        private static void exception_generator(String input1) {
+            assertThatThrownBy(() -> ChallengeNumberValidator.validate(input1))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Invalid number of attempts input");
+        }
+    }
+}
