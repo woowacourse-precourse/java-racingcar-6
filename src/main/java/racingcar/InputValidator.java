71a0eb332985802bd34b489e1ffb8d038ef26ef8
eunjin3481 <eunjin3481@gmail.com>
2023-10-31 00:44:51 +0900

    feat(InputValidator): 이름 길이를 초과할 경우 예외 처리 구현

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -3,7 +3,6 @@ package racingcar;
 import java.util.List;
 
 public class InputValidator {
-    public static void nameLength(String name) {
     public static void validateNamesIsEmpty(String[] input) {
         for (String name : input) {
             if (name.trim().isEmpty()) {
@@ -20,6 +19,12 @@ public class InputValidator {
         }
     }
 
+    public static void validateNameLength(String[] input) {
+        for (String name : input) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름의 길이가 5초과 됩니다. 프로그램 종료");
+            }
+        }
     }
 
     public static void duplicateName(List<Car> cars) {
