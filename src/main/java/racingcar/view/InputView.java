ca0a7ba5cdf3084e2263c473c741fb530a17e534
JiEung2 <https://github.com/JiEung2>
2023-10-30 23:00:25 +0900

    Style: Java 코드 컨벤션에 맞게 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,53 +1,56 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
 
-    public List<String> insertCarName(){
+    public List<String> insertCarName() {
         String input = Console.readLine();
         List<String> names = toStringList(input);
         return names;
     }
 
-    public List<String> toStringList(String name){
+    public List<String> toStringList(String name) {
         List<String> names = Arrays.asList(name.split(","));
         validateName(names);
         return names;
     }
 
-    public void validateName(List<String> names){
-        if(!isAllFiveOrLess(names))
+    public void validateName(List<String> names) {
+        if (!isAllFiveOrLess(names)) {
             throw new IllegalArgumentException("자동차 이름은 5자 이내로 입력해주세요.");
+        }
     }
 
-    public boolean isAllFiveOrLess(List<String> names){
-        for(String name : names){
-            if(name.length()>5) return false;
+    public boolean isAllFiveOrLess(List<String> names) {
+        for (String name : names) {
+            if (name.length() > 5) {
+                return false;
+            }
         }
         return true;
     }
 
-    public int insertAttemptCount(){
+    public int insertRoundNumber() {
         String input = Console.readLine();
         validateNumber(input);
 
         return convertToNumber(input);
     }
 
-    public void validateNumber(String input){
-        if(!isNumber(input))
+    public void validateNumber(String input) {
+        if (!isNumber(input)) {
             throw new IllegalArgumentException("숫자를 입력하세요");
+        }
     }
 
     public boolean isNumber(String input) {
         return input.matches("^[0-9]*$");
     }
 
-    public int convertToNumber(String input){
+    public int convertToNumber(String input) {
         return Integer.parseInt(input);
     }
 }
