484206c5ac4b4df0e41d46483f80489428af4634
yeeuniii <anna862700@gmail.com>
2023-10-31 02:22:20 +0900

    Feat: Race 실행 함수 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,5 +16,18 @@ public class Race {
 
         for (String name : names)
             cars.add(new Car(name));
+    }
+
+    public void run() {
+        System.out.println("\n실행 결과");
+        for (int idx = 0; idx < time; idx++) {
+            playOneRound();
+        }
+    }
+
+    private void playOneRound() {
+        for (Car car : cars) {
+            car.moveFoward();
+        }
     }
 }
