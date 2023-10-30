820a7abf2a4abf80ee2448d4f5a7df4d4db19c91
Terry <t3rryahn.dev@gmail.com>
2023-10-31 04:43:17 +0900

    fix(Car): 생성자에서 name 누락 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,13 +9,12 @@ public class Car {
         if (name == null || name.length() == 0 || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 이하로 설정해 주세요.");
         }
+        this.name = name;
     }
     public void goForward() {
         distance++;
     }
 
-
-    // getter
     public int getDistance() {
         return distance;
     }
