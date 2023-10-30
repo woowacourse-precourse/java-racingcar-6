0d0fb3b383778dd0a9712e0272a9ee4c2b4bf2f0
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 15:48:29 +0900

    feat(GameConstant) : 랜덤 최대,최솟값 및 전진하는 최소 임계값 상수 추가

--- src/main/java/racingcar/consts/GameConstant.java
+++ src/main/java/racingcar/consts/GameConstant.java
@@ -3,7 +3,10 @@ package racingcar.consts;
 public enum GameConstant {
     MIN_LENGTH_CAR_NAME(1),
     MAX_LENGTH_CAR_NAME(5),
-    MIN_NUMBER_TYPES_CAR(2);
+    MIN_NUMBER_TYPES_CAR(2),
+    MIN_NUMBER_RANDOM_RANGE(0),
+    MAX_NUMBER_RANDOM_RANGE(9),
+    MIN_FORWARD_THRESHOLD(4);
 
     private final int number;
 
