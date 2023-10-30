e94d457db520533cd6554d1f887146963e73e812
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 14:06:10 +0900

    refactor(Car) : 자동차 이동거리 변수명 변경
    movingCount에서 distance로 변수명 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,25 +4,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private String name;
-    private Long movingCount;
+    private Long distance;
 
     public Car(String name) {
         this.name = name;
-        movingCount = 0L;
+        distance = 0L;
     }
 
     public void moveOrStay() {
         int decisionNumber = Randoms.pickNumberInRange(0, 9);
 
         if (decisionNumber >= 4) {
-            movingCount++;
+            distance++;
         }
     }
 
     public String printResult() {
         StringBuilder hyphen = new StringBuilder();
 
-        for (long i = 0; i < movingCount; i++) {
+        for (long i = 0; i < distance; i++) {
             hyphen.append("-");
         }
 
