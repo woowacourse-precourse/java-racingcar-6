8a8443a703d3c676a2400649755334e784abc8e8
JoonKyo <joonkyolee@gmail.com>
2023-10-30 21:19:23 +0900

    feat(CarNameValidator): 입력 값에 띄어쓰기가 존재하는지 확인하는 메서드 추가

--- src/main/java/racingcar/model/CarNameValidator.java
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -7,4 +7,17 @@ public class CarNameValidator {
     private static final String INPUT_HAS_ZERO_LENGTH = "입력 값에 길이가 0인 자동차가 존재합니다.";
     private static final String INPUT_HAS_OVER_FIVE_LENGTH = "입력 값에 길이가 5가 넘는 자동차가 존재합니다.";
     private static final String INPUT_HAS_DUPLICATED_NAME = "입력 값에 중복 값이 존재합니다.";
+    private static final String SPACE = " ";
+    private List<String> carName;
+
+    public List<String> validateCarName(String carNames){
+        if(hasSpace(carNames)){
+            throw new IllegalArgumentException(INPUT_HAS_SPACE);
+        }
+        return carName;
+    }
+
+    private static boolean hasSpace(String carNames){
+        return carNames.contains(SPACE);
+    }
 }
