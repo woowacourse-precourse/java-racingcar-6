9b76524f65f06b61cca45f87b01045ef73f41563
junest66 <chlwndks333@gmail.com>
2023-10-31 02:30:33 +0900

    test(AttemptCountReaderTest): 시도횟수 입력 후 숫자 반환 기능 테스트 추가

---
+++ src/test/java/racingcar/utils/AttemptCountReaderTest.java
@@ -0,0 +1,43 @@
+package racingcar.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AttemptCountReaderTest {
+
+    private ByteArrayInputStream inputStream;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream originalSystemOut;
+
+    @BeforeEach
+    public void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        originalSystemOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalSystemOut);
+    }
+
+    @Test
+    public void 시도횟수_반환테스트() {
+        String input = "5\n";
+        inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+        System.setIn(inputStream);
+
+        int attemptCount = AttemptCountReader.get();
+        assertThat(attemptCount).isEqualTo(5);
+
+        String outputContent = outputStream.toString().trim();
+        assertThat(outputContent).isEqualTo("시도할 횟수는 몇 회인가요?");
+    }
+}
