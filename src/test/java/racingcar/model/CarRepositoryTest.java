9dde69bb21621638a3a9bf98af3bea337bfd21e8
Haeun Jeong <gkdms4248@gmail.com>
2023-10-30 17:42:18 +0900

    test(model): 우승자 찾는 기능 테스트 추가

--- src/test/java/racingcar/model/CarRepositoryTest.java
+++ src/test/java/racingcar/model/CarRepositoryTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,32 +15,38 @@ public class CarRepositoryTest {
     @BeforeEach
     void setUp() {
         carRepository = new CarRepository();
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+
+        carRepository.save(car1);
+        carRepository.save(car2);
+        carRepository.save(car3);
     }
 
     @DisplayName("Car 저장 기능 테스트")
     @Test
     void testSave() {
-        int expected = 2;
-        Car car1 = new Car("pobi");
-        Car car2 = new Car("woni");
+        int expected = 3;
 
-        carRepository.save(car1);
-        carRepository.save(car2);
-
-        assertEquals(expected, carRepository.getCars().size());
+        assertEquals(expected, carRepository.getCarDto().size());
     }
 
     @DisplayName("Car 저장 중 이름 중복 검사 기능 테스트")
     @Test
     void testSaveValidateDuplicateName() {
-        int expected = 2;
-        Car car1 = new Car("pobi");
-        Car car2 = new Car("woni");
-        Car car3 = new Car("pobi");
+        Car car4 = new Car("pobi");
 
-        carRepository.save(car1);
-        carRepository.save(car2);
+        assertThrows(IllegalArgumentException.class, () -> carRepository.save(car4));
+    }
 
-        assertThrows(IllegalArgumentException.class, () -> carRepository.save(car3));
+    @DisplayName("winner 찾는 기능 테스트")
+    @Test
+    void testGetWinner() {
+        List<String> expected = List.of("pobi", "woni", "jun");
+
+        List<String> winner = carRepository.getWinner();
+
+        assertEquals(expected, winner);
     }
 }
