b8230459d087b8cd3b83ec9ff73d3ff5e2403c42
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 15:15:44 +0900

    refactor: 자동차 이름의 책임 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,27 +4,13 @@ import java.util.Objects;
 
 public class Car {
     private static final String DISTANCE_ICON = "-";
-    private static final int NAME_LENGTH_LIMIT = 5;
     private static final int MOVING_FORWARD_VALUE = 4;
-    private final String name;
+
+    private final CarName name;
     private int distance;
 
     Car(String name) {
-        validateNullOrEmpty(name);
-        validateLengthLimit(name);
-        this.name = name;
-    }
-
-    private void validateNullOrEmpty(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
-        }
-    }
-
-    private void validateLengthLimit(String name) {
-        if (name.length() > NAME_LENGTH_LIMIT) {
-            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
-        }
+        this.name = new CarName(name);
     }
 
     public void go(RandomNumber randomNumber) {
@@ -38,11 +24,11 @@ public class Car {
     }
 
     public String getName() {
-        return name;
+        return name.toString();
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name + " : " + DISTANCE_ICON.repeat(distance);
     }
 
