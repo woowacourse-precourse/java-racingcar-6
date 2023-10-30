f327787254960ef05b2e661edc5aff22cce4b5db
donghyun <kdhdh001@naver.com>
2023-10-31 02:37:49 +0900

    feat: 음수 입력시 예외처리 구현

--- src/main/java/racingcar/validation/TryCountValidator.java
+++ src/main/java/racingcar/validation/TryCountValidator.java
@@ -5,6 +5,7 @@ public class TryCountValidator {
     public static int validateTryCount(String input) {
         validateInputEmpty(input);
         int tryCount = parseInputToInteger(input);
+        validateTryCountBiggerThanZero(tryCount);
         return tryCount;
     }
 
@@ -12,13 +13,19 @@ public class TryCountValidator {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 다른 문자를 입력하였습니다. 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("정수를 입력하세요.");
         }
     }
 
     private static void validateInputEmpty(String input) {
         if (input == null || input.length() == 0) {
             throw new IllegalArgumentException("시도할 횟수를 입력하지 않았습니다. 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void validateTryCountBiggerThanZero(int tryCount) {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도할 횟수는 0보다 커야 합니다.");
         }
     }
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.validation.CarNameValidator;
+import racingcar.validation.TryCountValidator;
 
 public class InputView {
     private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -11,13 +13,16 @@ public class InputView {
     public static List<String> inputCarNames() {
         System.out.println(CAR_NAME_PROMPT);
         String input = Console.readLine();
-        return Arrays.asList(input.split(","));
+        List<String> carNames = Arrays.asList(input.split(","));
+        CarNameValidator.validateCarNames(carNames);
+        return carNames;
     }
 
     public static int inputTryCount() {
         System.out.println(TRY_COUNT_PROMPT);
         String input = Console.readLine();
-        return Integer.parseInt(input);
+        int validatedTryCount = TryCountValidator.validateTryCount(input);
+        return validatedTryCount;
     }
 
 }
