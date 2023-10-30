fdab3436431cb8c8284a32b57f29d0608cc9bb2f
DevFrog92 <yongsu1481@gmail.com>
2023-10-30 17:24:49 +0900

    refactor: add abstract to PatternUtil for prevent inheritance

--- src/main/java/racingcar/util/PatternUtil.java
+++ src/main/java/racingcar/util/PatternUtil.java
@@ -3,7 +3,7 @@ package racingcar.util;
 /**
  * 유효성 검사에 필요한 정규 표현식을 정의한 util 클래스 입니다.
  */
-public class PatternUtil {
+public abstract class PatternUtil {
         /**
          * 양수만 입력할 수 있는 규칙입니다.
          */
