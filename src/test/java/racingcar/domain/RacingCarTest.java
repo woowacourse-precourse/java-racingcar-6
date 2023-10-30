702a35fe66ccccd5706d661a0b008aa7f8bd5173
ParkDH0809 <znxn1237@gmail.com>
2023-10-31 00:18:40 +0900

    test: 이름 반환 테스트

---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarTest {
+    RacingCars racingCars;
+    @BeforeEach
+    void initRacingCars() {
+        List<String> list = List.of("pobi", "java");
+        racingCars = new RacingCars(list);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 올바르게 반환되는지 테스트")
+    void getName() {
+        List<String> testList = List.of("pobi", "java");
+        List<RacingCar> racingCarsList = racingCars.carList;
+        for (int i = 0; i < racingCarsList.size(); i++) {
+            assertThat(racingCarsList.get(i).getName()).isEqualTo(testList.get(i));
+        }
+    }
+}
