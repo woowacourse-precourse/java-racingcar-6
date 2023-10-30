e975606afed1b79f5f04a1994c05d0a2479c3a71
bamm2 <xxknu88@naver.com>
2023-10-30 21:42:58 +0900

    "feat: 입력 관련 기능"

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,26 +1,46 @@
 package racingcar.view;
 
+import static racingcar.constant.Constant.GAME_START_MESSAGE;
+import static racingcar.constant.Constant.PLAY_TIME_QUESTION;
+
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
 
 public class Input {
+
+    private InputValidator inputValidator;
 
     public Input() {
     }
 
-    public void printGameStartMessage(String message) {
-        System.out.println(message);
+    public void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
-    public void printPlayTimeQuestion(String message) {
-        System.out.println(message);
+    public void printPlayTimeQuestion() {
+        System.out.println(PLAY_TIME_QUESTION);
     }
 
-    public String writeCarNames() {
-        return Console.readLine();
+    public List<String> writeCarNames() {
+        validateCarNames(Console.readLine());
+        return inputValidator.convertList();
+    }
+
+    private void validateCarNames(String carNames) {
+        inputValidator = new InputValidator(carNames);
+        inputValidator.validateCarNames();
     }
 
     public Integer writePlayTime() {
-        return Integer.parseInt(Console.readLine());
+        validatePlayTime(Console.readLine());
+        return inputValidator.getPlayTime();
+    }
+
+    private void validatePlayTime(String playTime) {
+        inputValidator = new InputValidator();
+        inputValidator.validateTime(playTime);
     }
 
     public void close() {
