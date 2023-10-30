35e848b37a36e40553538c3c64d9266a19c2d5d7
김효준 <khyojun1104@gmail.com>
2023-10-30 16:52:22 +0900

    chore(CarRace): 기호 전용 Enum 클래스 생성!

--- src/main/java/racingcar/constant/Sign.java
+++ src/main/java/racingcar/constant/Sign.java
@@ -1,2 +1,18 @@
-package racingcar.constant;public enum Sign {
+package racingcar.constant;
+
+public enum Sign {
+    COLONE(" : "),
+    TRACE("-");
+
+
+    private String sign;
+
+
+    Sign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getSign() {
+        return sign;
+    }
 }
