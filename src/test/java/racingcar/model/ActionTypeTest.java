a45e9c7d8a146f2bdcf14bf632fbaeff9c8f6125
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-30 16:25:27 +0900

    test : 액션 타입에 관한 테스트 코드 작성

--- src/test/java/racingcar/model/ActionTypeTest.java
+++ src/test/java/racingcar/model/ActionTypeTest.java
@@ -3,17 +3,19 @@ package racingcar.model;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ActionTypeTest {
 
+    @DisplayName("반드시 정수 1또는 정수 0을 리턴해야 한다.")
     @Test
     void randomAction_test() {
         for (int i = 0; i < 1000; i++) {
             Integer action = ActionType.randomAction();
             assertNotNull(action);
-            assertTrue(action == ActionType.GO);
+            assertTrue(action == ActionType.GO.getAction()
+                    || action == ActionType.STOP.getAction());
         }
     }
-
 }
