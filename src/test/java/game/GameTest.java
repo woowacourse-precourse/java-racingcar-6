a88c925dd1605d9492c153deef1dddacf7d20c89
sh990920 <sh990920@gmail.com>
2023-10-30 15:49:47 +0900

    test: addPerson 메소드 테스트추가

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -1,6 +1,7 @@
 package game;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,14 @@ public class GameTest {
         List<String> result = game.addPerson("user1,user2");
 
         assertThat(result).containsExactly("user1", "user2");
+    }
+
+    @Test
+    public void testAddPersonException(){
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.addPerson("user1,userLongName"))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
