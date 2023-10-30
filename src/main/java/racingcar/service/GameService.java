23895a55f30de9c0dc4fe505244603e6d05eeaa2
깃wooqqq <sge0225@naver.com>
2023-10-31 02:24:12 +0900

    feat: 각 자동차의 위치 비교하여 우승자를 찾는 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -12,6 +12,7 @@ public class GameService {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
     private static final int FORWARD_NUMBER = 4;
+    private static final int INITIAL_MAX_VALUE = -1;
 
     // 쉼표(,)를 기준으로 입력된 자동차 이름 분해 후 List에 저장
     public List<String> parseCarName(String input) {
@@ -21,6 +22,13 @@ public class GameService {
     public void play(Game game) {
         moveForwardOfGame(game);
         game.increaseTriedNum();
+    }
+
+    public List<String> getWinner(Game game) {
+        List<Car> cars = game.getCars();
+        // maxPosition 뽑기
+        int maxPosition = getMaxPosition(cars);
+        return getMaxPositionCars(cars, maxPosition);
     }
 
     private void moveForwardOfGame(Game game) {
@@ -39,5 +47,27 @@ public class GameService {
     // 자동차 별 무작위 값을 구함
     private Integer pickRandomNumber() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+    // position이 가장 큰 자동차의 이름 배열에 저장
+    private List<String> getMaxPositionCars(List<Car> cars, int maxPosition) {
+        List<String> carNames = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                carNames.add(car.getName());
+            }
+        }
+        return carNames;
+    }
+
+    // 가장 큰 position 값 구하기
+    private int getMaxPosition(List<Car> cars) {
+        int max = INITIAL_MAX_VALUE;
+        for (Car car : cars) {
+            if (max < car.getPosition()) {
+                max = car.getPosition();
+            }
+        }
+        return max;
     }
 }
