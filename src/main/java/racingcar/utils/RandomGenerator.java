8f4e1bad45fc4745e82f7474de37ff7ec53eddeb
Isaac Han <isaachan233@gmail.com>
2023-10-30 11:18:26 +0900

    refactor: 난수 생성하는 메서드를 외부에서 인자를 받아 사용하게 하여 재사용성을 높임

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -1,13 +1,12 @@
 package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.constants.Constants;
 
 /*
  * Car랑 generateRandomNumber는 관련이 없는거 같아서 나누었습니다.
  * */
 public class RandomGenerator {
-    public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(Constants.MIN_NUMBER.getIntValue(), Constants.MAX_NUMBER.getIntValue());
+    public int generateRandomNumber(int min, int max) {
+        return Randoms.pickNumberInRange(min, max);
     }
 }
