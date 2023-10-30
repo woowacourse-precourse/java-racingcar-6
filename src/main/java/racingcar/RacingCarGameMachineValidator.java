3e9551b1125d7a64882cf19df7ec86b8c77e9528
seminchoi <choicco89@gmail.com>
2023-10-30 23:38:03 +0900

    feat(racingcar.RacingCarGameMachineValidator) : 레이싱카 이름 길이, 중복 여부 유효성을 검증하는 기능 구현

--- src/main/java/racingcar/RacingCarGameMachineValidator.java
+++ src/main/java/racingcar/RacingCarGameMachineValidator.java
@@ -1,10 +1,39 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RacingCarGameMachineValidator {
 
+    private final int MIN_RACING_CAR_COUNT = 2;
+    private final int MAX_RACING_CAR_COUNT = 5;
+
     public void validateNameList(List<String> racingCarNameList) {
+        validateNameCount(racingCarNameList);
+        validateDuplicateName(racingCarNameList);
+    }
+
+    private void validateNameCount(List<String> racingCarNameList) {
+        int size = racingCarNameList.size();
+        if (isInvalidNameCount(size)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidNameCount(int size) {
+        return size < MIN_RACING_CAR_COUNT || size > MAX_RACING_CAR_COUNT;
+    }
+
+    private void validateDuplicateName(List<String> racingCarNameList) {
+        Set<String> uniqueNames = new HashSet<>(racingCarNameList);
+        if (hasDuplicate(racingCarNameList, uniqueNames)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean hasDuplicate(List<String> racingCarNameList, Set<String> uniqueNames) {
+        return racingCarNameList.size() != uniqueNames.size();
     }
 
     public void validateRoundCount(int roundCount) {
