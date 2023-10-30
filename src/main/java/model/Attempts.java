c0afff4826780c095b357e09b7f79239c10d3c09
kimwanyoung <dhks2869@gmail.com>
2023-10-30 16:10:23 +0900

    feat : Attempts 문자 입력 검증 구현

--- src/main/java/model/Attempts.java
+++ src/main/java/model/Attempts.java
@@ -4,9 +4,11 @@ public class Attempts {
 
     private int attempts;
     private static final String EMPTY_MESSAGE = "공백 혹은 빈 문자열은 입력 할 수 없습니다.";
+    private static final String ONLY_NUMBER_INPUT_MESSAGE = "숫자만 입력 가능합니다.";
 
     public Attempts(String attempts) {
         validateEmpty(attempts);
+        validateIsNumericInput(attempts);
         this.attempts = 1;
     }
 
@@ -18,5 +20,13 @@ public class Attempts {
 
     private boolean isEmpty(String attempts) {
         return attempts.isEmpty() || attempts.isBlank();
+    }
+
+    private void validateIsNumericInput(String attempts) {
+        try {
+            Integer.parseInt(attempts);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ONLY_NUMBER_INPUT_MESSAGE);
+        }
     }
 }
