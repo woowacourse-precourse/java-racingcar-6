8d44754a3e6a25438b6599f1d9b4016aa569e7f6
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 12:15:23 +0900

    test: 최종 우승자를 계산하는 기능 테스트

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -13,6 +13,7 @@ class RacingGameServiceTest {
 
     static final Long MOVE = 1L;
     static final Long STOP = 0L;
+    static final int CANT_MOVE_EDGE_CONDITION = 3;
     RacingGameService racingGameService = new RacingGameService();
 
     @Test
@@ -26,6 +27,46 @@ class RacingGameServiceTest {
         Assertions.assertThat(resultDto.getTotalMoveCounts()).containsExactly(MOVE, STOP, MOVE);
     }
 
+    @Test
+    @DisplayName("최종 우승자 판별 기능 테스트. 경주 1회 진행")
+    void calculateFinalWinnersTest1() {
+        // given
+        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"), new CustomEngine2());
+        // when
+        racingGameService.playOneTimeCarRace(racingCars);
+        List<String> winners = racingGameService.calculateFinalWinners(racingCars);
+        // then
+        Assertions.assertThat(winners).containsExactly("H", "yp", "b 0y");
+    }
+
+    @Test
+    @DisplayName("최종 우승자 판별 기능 테스트. 경주 3회 진행")
+    void calculateFinalWinnersTest2() {
+        // given
+        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"), new CustomEngine2());
+        int totalRound = 3;
+        // when
+        for (int round = 0; round < totalRound; round++) {
+            racingGameService.playOneTimeCarRace(racingCars);
+        }
+        List<String> winners = racingGameService.calculateFinalWinners(racingCars);
+        // then
+        Assertions.assertThat(winners).containsExactly("H", "b 0y");
+    }
+
+    @Test
+    @DisplayName("최종 우승자 판별 기능 테스트. 모든 자동차가 움직이지 못하는 경우")
+    void calculateFinalWinnersTest3() {
+        // given
+        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"),
+                () -> CANT_MOVE_EDGE_CONDITION);
+        // when
+        racingGameService.playOneTimeCarRace(racingCars);
+        List<String> winners = racingGameService.calculateFinalWinners(racingCars);
+        // then
+        Assertions.assertThat(winners).containsExactly("L!!O", "new", "j2AnS", "H", "yp", "b 0y");
+    }
+
     static class CustomEngine implements Engine {
 
         List<Integer> enginePowers = Lists.newArrayList(4, 3, 8);
@@ -33,6 +74,24 @@ class RacingGameServiceTest {
         @Override
         public int generateNumber() {
             return enginePowers.remove(0);
+        }
+    }
+
+    static class CustomEngine2 implements Engine {
+
+        List<Integer> enginePowers = Lists.newArrayList(1, 2, 3, 4, 5, 6);
+        List<Integer> enginePowers2 = Lists.newArrayList(0, 0, 9, 6, 3, 4);
+        List<Integer> enginePowers3 = Lists.newArrayList(4, 3, 7, 9, 1, 8);
+
+        @Override
+        public int generateNumber() {
+            if (!enginePowers.isEmpty()) {
+                return enginePowers.remove(0);
+            }
+            if (!enginePowers2.isEmpty()) {
+                return enginePowers2.remove(0);
+            }
+            return enginePowers3.remove(0);
         }
     }
 }
