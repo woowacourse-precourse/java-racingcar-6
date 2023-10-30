58790b067225c5ad78b242ce87a5245419e8011f
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-30 16:39:47 +0900

    feat : Race class - 게임 진행 및 결과 생성 기능 추가

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,52 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private final List<Car> cars;
+    private final int trials;
+    private final List<Car> winners;
+
+    public Race(List<Car> cars, int trials) {
+        this.cars = cars;
+        this.trials = trials;
+        this.winners = new ArrayList<>();
+    }
+
+    public void executeRace() {
+        OutputManager.displayRaceHeader();
+
+        for (int i = 0; i < trials; i++) {
+            for (Car car : cars) {
+                car.move();
+                OutputManager.displayCarPosition(car);
+            }
+            OutputManager.displayBlankLine();
+        }
+
+    }
+
+    private void determineWinners() {
+        int maxPosition = getMaxPosition();
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Car> getWinners() {
+        return winners;
+    }
+
+}
