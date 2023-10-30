68806971f2b55bd282714536e5eafd55e3a08839
dltjdn <hana40004@naver.com>
2023-10-31 01:43:07 +0900

    fix(RandomNumberGenerator): range 값 오류 수정

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberGenerator implements NumberGenerator {
 
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
+    private static final int RANDOM_UPPER_INCLUSIVE = 9;
 
     @Override
     public int generate() {
