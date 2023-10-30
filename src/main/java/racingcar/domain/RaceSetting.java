97938eaa317d8ac3df2f827441cc5c6b1fabd958
신용대 <fshiel8165@naver.com>
2023-10-30 17:55:10 +0900

    RaceSettingTest 추가

--- src/main/java/racingcar/domain/RaceSetting.java
+++ src/main/java/racingcar/domain/RaceSetting.java
@@ -6,19 +6,21 @@ public class RaceSetting {
     public String[] regCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = Console.readLine();
-        return carValidCheck(cars);
+        String[] carList = splitCars(cars);
+
+        carValidCheck(carList);
+        return carList;
     }
 
     public String[] splitCars(String cars) {
-        return cars.split(",");
+        // 쉼표 뒤에 나오는 공백을 포함하여 split
+        return cars.split(",\\s*");
     }
 
-    public String[] carValidCheck(String cars) {
-        String[] carList = splitCars(cars);
+    public void carValidCheck(String[] carList) {
         for (String car : carList) {
             if (car.length() > 5) throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
         }
-        return carList;
     }
 
     public int playNumber() {
---
+++ src/test/java/racingcar/domain/PlayRacingTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayRacingTest {
+
+    @Test
+    void judgementGoStop() {
+
+    }
+
+    @Test
+    void raceResult() {
+    }
+
+    @Test
+    void playRace() {
+    }
+
+    @Test
+    void getMaxScore() {
+    }
+
+    @Test
+    void addWinner() {
+    }
+
+    @Test
+    void getWinnerList() {
+    }
+}
