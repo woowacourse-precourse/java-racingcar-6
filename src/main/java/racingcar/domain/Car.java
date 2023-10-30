2516e0404558c36dadf392704d6c1aa2b75f9fab
BellWin98 <jongseunghan@naver.com>
2023-10-30 18:21:54 +0900

    feat(racingcar): 자동차가 동작(전진 또는 정지)하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,27 @@
 package racingcar.domain;
 
 public class Car {
-
-    public Car(){}
+    private final String name;
+    private final StringBuilder trace = new StringBuilder();
+    private int score = 0;
+    public Car(String name){
+        this.name = name;
+    }
 
     public void move(){
-
+        score++;
+        trace.append("-");
+        System.out.println(name+" : "+trace);
     }
 
     public void stop(){
+        System.out.println(name+" : "+trace);
+    }
 
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
     }
 }
