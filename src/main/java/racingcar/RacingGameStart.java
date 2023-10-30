4e66dcff88c514c59f923ece2acb44553ad42a3a
jwAhn1020 <takartin@naver.com>
2023-10-30 15:33:20 +0900

    refactor : 자동차 이름 split, createRacingCars 안으로 이동

--- src/main/java/racingcar/RacingGameStart.java
+++ src/main/java/racingcar/RacingGameStart.java
@@ -7,8 +7,7 @@ public class RacingGameStart {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
 
-        String[] carsList = carNames.split(",");
-        RacingCar[] racingCars = createRacingCars(carsList);
+        RacingCar[] racingCars = createRacingCars(carNames);
 
         System.out.println("시도할 횟수는 몇회인가요?");
         int num = Integer.parseInt(Console.readLine());
@@ -20,11 +19,12 @@ public class RacingGameStart {
         printWinners(racingCars);
     }
 
-    private static RacingCar[] createRacingCars(String[] carNames) {
-        RacingCar[] racingCars = new RacingCar[carNames.length];
-        for (int i = 0; i < carNames.length; i++) {
-            checkNameLength(carNames[i]);
-            racingCars[i] = new RacingCar(carNames[i].trim(), 0);
+    private static RacingCar[] createRacingCars(String carNames) {
+        String[] carNamesList = carNames.split(",");
+        RacingCar[] racingCars = new RacingCar[carNamesList.length];
+        for (int i = 0; i < carNamesList.length; i++) {
+            checkNameLength(carNamesList[i]);
+            racingCars[i] = new RacingCar(carNamesList[i].trim(), 0);
         }
         return racingCars;
     }
