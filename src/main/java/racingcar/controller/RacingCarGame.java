94aa0b9a416a041aff251771518e1378e2095f21
oxdjww <oxdjww@gmail.com>
2023-10-30 17:40:14 +0900

    feat : RacingCarGame run() 스켈레톤 코드 구현 완료

---
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import static racingcar.util.ConvertUtils.convertStringToInt;
+import static racingcar.util.ConvertUtils.convertStringToList;
+import static racingcar.view.InputView.getCarNamesFromPlayer;
+import static racingcar.view.InputView.getNumberOfRoundsFromPlayer;
+
+import racingcar.model.Cars;
+import racingcar.model.RacingCarGameConfig;
+
+public class RacingCarGame {
+
+  private final RacingCarGameConfig gameConfig;
+
+  private RacingCarGame() {
+    this.gameConfig = RacingCarGameConfig.createRacingCarGameConfig(null, 5, 0, 9);
+  }
+
+  public static RacingCarGame createRacingCarGame() {
+    return new RacingCarGame();
+  }
+
+  public void run() {
+    String inputCarName = getCarNamesFromPlayer();
+
+    Cars cars = Cars.createFromCarNames(convertStringToList(inputCarName));
+
+    int numberOfRounds = convertStringToInt(getNumberOfRoundsFromPlayer());
+
+    for (int i = 0; i < numberOfRounds; i++) {
+      cars.playRacingGame();
+    }
+
+    printRacingCarGameWinner(cars);
+  }
+
+  private void printRacingCarGameWinner(Cars cars) {
+
+  }
+}
