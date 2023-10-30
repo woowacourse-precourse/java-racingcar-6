0491800465ca0ff0ce0f8153324ff212650d47b7
leedohyun <lucky77795@naver.com>
2023-10-30 19:23:31 +0900

    refactor(racingCar) : 네이밍 수정

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumber implements RandomUtil{
+public class RandomNumberGenerator implements RandomUtil{
     @Override
     public int generate(int min, int max) {
         return Randoms.pickNumberInRange(min, max);
