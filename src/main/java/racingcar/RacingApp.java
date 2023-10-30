10d12f0c04a37fe6fa23866c65e1a4f04fa215c3
seongha <11pi885@gmail.com>
2023-10-30 16:33:11 +0900

    refactor : RacingCarList 로 클래스 분리하기

--- src/main/java/racingcar/RacingApp.java
+++ src/main/java/racingcar/RacingApp.java
@@ -2,19 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RacingApp {
 
     private int executeCount;
-    private List<RacingCar> racingCarList = new ArrayList<>();
+
+    private RacingCarList racingCarList;
 
     public void run() {
         String input = getPlayerList();
-        createRacingCarList(input);
+        this.racingCarList = new RacingCarList(input);
         getExecuteCount();
         play();
         printRacingResult();
@@ -23,44 +21,14 @@ public class RacingApp {
     private void play() {
         System.out.println("실행 결과");
         for (int i = 0; i < executeCount; i++) {
-            executeOneStep();
-            printRacingCarsCurrentStatus();
+            racingCarList.executeOneStep();
+            racingCarList.printRacingCarsCurrentStatus();
         }
     }
-
-    private void executeOneStep() {
-        racingCarList.forEach(racingCar -> {
-            int number = Randoms.pickNumberInRange(0, 9);
-            if (number >= 4) {
-                racingCar.moveOneBlock();
-            }
-        });
-    }
-
-    private void printRacingCarsCurrentStatus() {
-        racingCarList.forEach(racingCar ->
-                System.out.println(racingCar.getName() + " : " +
-                        "-".repeat(racingCar.getCount())));
-    }
-
     private void printRacingResult() {
-        int maxForward = getMaxForward();
-        List<String> winnerList = getWinnerList(maxForward);
+        int maxForward = racingCarList.getMaxForward();
+        List<String> winnerList = racingCarList.getWinnerList(maxForward);
         System.out.print("최종 우승자 : " + String.join(", ", winnerList));
-    }
-
-    private List<String> getWinnerList(int maxCount) {
-        return racingCarList.stream()
-                .filter(racingCar -> racingCar.getCount() == maxCount)
-                .map(RacingCar::getName)
-                .toList();
-    }
-
-    private int getMaxForward() {
-        return racingCarList.stream()
-                .mapToInt(RacingCar::getCount)
-                .max()
-                .orElse(0);
     }
 
     private void getExecuteCount() {
@@ -88,19 +56,6 @@ public class RacingApp {
     private String getPlayerList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
-    }
-
-    private void createRacingCarList(String input) {
-        List<String> nameList = getNameListFromInput(input);
-        createRacingCarListByNameList(nameList);
-    }
-
-    private void createRacingCarListByNameList(List<String> nameList) {
-        nameList.forEach(name -> racingCarList.add(new RacingCar(name)));
-    }
-
-    private List<String> getNameListFromInput(String input) {
-        return Arrays.stream(input.split(",")).collect(Collectors.toList());
     }
 
 }
