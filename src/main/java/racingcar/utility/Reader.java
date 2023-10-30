4ade06a177b417fb421b444f9ace7f2425dec75c
jongmee <ra123221@gmail.com>
2023-10-30 14:32:10 +0900

    feat: 입, 출력을 담당하는 utility class 생성하기
    - System.out.print로 출력하고 Scanner로 입력 받는다.
    - 확장과 리팩토링이 쉽도록 따로 클래스로 빼서 관리한다.

---
+++ src/main/java/racingcar/utility/Reader.java
@@ -0,0 +1,9 @@
+package racingcar.utility;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Reader {
+    public static String readLine() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/utility/Writer.java
@@ -0,0 +1,11 @@
+package racingcar.utility;
+
+public class Writer {
+    public static void print(Object message) {
+        System.out.print(message);
+    }
+
+    public static void println(Object message) {
+        System.out.println(message);
+    }
+}
