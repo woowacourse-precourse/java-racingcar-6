6f30351b8a1c1be4ed237576bfdce04db4da1e17
황중섭 <jshwang9709@gmail.com>
2023-10-31 02:55:02 +0900

    refactor(test): 테스트 코드 중복 로직 메서드로 추출

--- src/test/java/racingcar/game/domain/CarTest.java
+++ src/test/java/racingcar/game/domain/CarTest.java
@@ -9,16 +9,12 @@ class CarTest {
 
     @Test
     void 자동차_생성() {
-        Car car = Car.create("hwang");
-        assertThat(car).isNotNull();
+        assertSuccessfullyCreate("hwang");
     }
 
     @Test
     void 이름이_6자_이상이면_자동차_생성X() {
-        assertThatThrownBy(() -> Car.create("avante"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름이 1~5자 사이가 아닙니다.");
-
+        assertExceptionCase("avante", "자동차의 이름이 1~5자 사이가 아닙니다.");
     }
 
     @Test
@@ -35,33 +31,34 @@ class CarTest {
 
     @Test
     void 자동차_이름_중간에_공백이_포함돼도_자동차_생성_가능() {
-        Car car = Car.create("ju ng");
-        assertThat(car).isNotNull();
+        assertSuccessfullyCreate("ju ng");
     }
 
     @Test
     void 자동차_이름_양끝에_공백이_포함되면_자동차_생성X() {
-        assertThatThrownBy(() -> Car.create(" jung"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름의 양 끝에는 공백이 올 수 없습니다.");
-
-        assertThatThrownBy(() -> Car.create("jung "))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름의 양 끝에는 공백이 올 수 없습니다.");
+        assertExceptionCase(" jung", "자동차 이름의 양 끝에는 공백이 올 수 없습니다.");
+        assertExceptionCase("jung ", "자동차 이름의 양 끝에는 공백이 올 수 없습니다.");
     }
 
     @Test
     void 자동차_이름이_null_이면_자동차_생성X() {
-        assertThatThrownBy(() -> Car.create(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름은 필수값입니다.");
+        assertExceptionCase(null, "자동차의 이름은 필수값입니다.");
     }
 
     @Test
     void 자동차_이름이_빈_문자열이면_자동차_생성X() {
-        assertThatThrownBy(() -> Car.create(""))
+        assertExceptionCase("", "자동차의 이름이 1~5자 사이가 아닙니다.");
+    }
+
+    private void assertSuccessfullyCreate(String carName) {
+        Car car = Car.create(carName);
+        assertThat(car).isNotNull();
+    }
+
+    private void assertExceptionCase(String carName, String errorMessage) {
+        assertThatThrownBy(() -> Car.create(carName))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름이 1~5자 사이가 아닙니다.");
+                .hasMessageContaining(errorMessage);
     }
 
 }
