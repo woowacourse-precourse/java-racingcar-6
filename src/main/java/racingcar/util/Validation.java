5c53bb6d242dd49b66463f9607017ad0eb31eb7e
“EvelynKimm” <evekimmy09@gmail.com>
2023-10-30 21:12:28 +0900

    feat: add validate string null method
    차 이름을 입력받았을 때 빈칸인지 검증하는 메서드

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -1,24 +1,16 @@
 package racingcar.util;
 
 import java.util.List;
+import racingcar.model.Cars;
 
 public class Validation {
-    public static void validateNameInput(List<String> list) {
-        for (String input : list) {
-            if (input.length() > 5) {
-                throw new IllegalArgumentException("5자 이하의 이름만 입력 가능합니다");
-            }
-        }
-    }
-    public static void validateNumber(int input) {
-        if (input < 0) {
-            throw new IllegalArgumentException("음수는 입력 불가합니다.");
-        }
+    // indent depth = 2인 메서드임
+    // 글자(CarNames) 입력 검증
 
-        try {
-            Integer.parseInt(String.valueOf(input)); // 문자열로 변환하고 정수로 다시 변환 시도
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
+    private static void validateStringNull(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("빈칸입니다.");
         }
     }
+
 }
