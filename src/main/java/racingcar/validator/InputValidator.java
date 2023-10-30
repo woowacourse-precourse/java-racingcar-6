a1f60a25b40fd640156f7b07312c2b085e86af20
mainfn <_@mainfn.dev>
2023-10-31 00:42:25 +0900

    fix: 정규 표현식 오류 수정

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -7,7 +7,7 @@ import racingcar.exception.InvalidMaxRoundInputException;
 public final class InputValidator {
 
     private static final String CAR_NAMES_REGEX = "^(\\w+,)+\\w+$";
-    private static final String MAX_ROUND_REGEX = "^\\d$";
+    private static final String MAX_ROUND_REGEX = "^\\d+$";
 
     public void validateCarNamesInput(final String input) {
         if (!Pattern.matches(CAR_NAMES_REGEX, input)) {
