b65eb114e3da16f1e6644f315714c369cafa01d8
SungWoo <sungwoo166@gmail.com>
2023-10-31 00:22:58 +0900

    fix(view): Typo in CarRaceMessages

--- src/main/java/racingcar/view/CarRaceMessages.java
+++ src/main/java/racingcar/view/CarRaceMessages.java
@@ -2,7 +2,7 @@ package racingcar.view;
 
 public enum CarRaceMessages {
     START_RACE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    REQUEST_RACE_ROUNDS("시도할 회수는 몇회인가요?"),
+    REQUEST_RACE_ROUNDS("시도할 횟수는 몇회인가요?"),
     RACE_RESULT_MESSAGE("실행 결과"),
     RACE_WINNERS("최종 우승자 : ");
 
@@ -11,7 +11,7 @@ public enum CarRaceMessages {
     CarRaceMessages(String message) {
         this.message = message;
     }
-    
+
     @Override
     public String toString() {
         return message;
