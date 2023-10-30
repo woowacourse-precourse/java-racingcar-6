f623c4ab1a22e3b780fa6dfecaeeeffacaf8672a
kimtaesoo <kimtaesoo7@naver.com>
2023-10-30 23:45:17 +0900

    test: 불필요한 테스트 제거

--- src/test/java/racingcar/model/PositionTest.java
+++ src/test/java/racingcar/model/PositionTest.java
@@ -5,38 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class PositionTest {
-
-    @CsvSource({"4, 1", "7, 1", "9, 1"})
-    @ParameterizedTest
-    void 주어진_값이_4이상일_때_위치가_이동한다(int number, int expected) {
-        // given
-        Position position = Position.createDefault();
-
-        // when
-        position.move(number);
-
-        // then
-        assertThat(position.getPosition()).isEqualTo(expected);
-    }
-
-    @CsvSource({"2, 0", "1, 0"})
-    @ParameterizedTest
-    void 매개변수로_들어온_값이_4미만이면_위치가_그대로이다(int number, int expected) {
-        // given
-        Position position = Position.createDefault();
-
-        // when
-        position.move(number);
-
-        // then
-        assertThat(position.getPosition()).isEqualTo(expected);
-    }
 
     @Test
     void 동일한_위치를_가진_Position_인스턴스는_동등하다() {
@@ -54,7 +26,7 @@ class PositionTest {
         // given
         int expected = 1;
         Position position = Position.createDefault();
-        position.move(4);
+        position.move();
 
         // when
         int result = position.getPosition();
