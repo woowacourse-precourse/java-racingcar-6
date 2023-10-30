8463b4e2472f58841d6d28207ed3deba7b2ecdc6
col-ini <dobbychoi99@gmail.com>
2023-10-30 11:36:56 +0900

    feat : 시도할 횟수 입력(예외처리 포함)
    자동차 경주를 반복하는 횟수를 입력받는 변수와 정수인지, 0 이상의 정수인지 판별하는 예외처리 메소드 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class RacingCarGame {
     public List<String> carName = new ArrayList<String>();
     public List<Integer> movementSoFar = new ArrayList<Integer>();
+    Integer numberOfTries;
 
     public String inputString() {
         String inputStr = Console.readLine();
@@ -27,13 +28,34 @@ public class RacingCarGame {
         // 자동차 이름 입력 완료
         stringToList(strCarName);
         setMovement();
-//        System.out.println(carName);
-//        System.out.println(movementSoFar);
+        System.out.println(carName);
+        System.out.println(movementSoFar);
     }
 
     public void setMovement() {
         for (int i = 0; i < carName.size(); i++) {
             movementSoFar.add(0);
+        }
+    }
+
+    public void numberOfTries() {
+        String strTries = inputString();
+        isNotInteger(strTries);
+        lessThenZero(strTries);
+        numberOfTries = Integer.parseInt(strTries);
+    }
+
+    public void isNotInteger(String strTries) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(strTries);
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("정수를 입력하지 않아서 프로그램을 종료합니다.");
+        }
+    }
+
+    public void lessThenZero(String strTries) throws IllegalArgumentException {
+        if (Integer.parseInt(strTries) < 0) {
+            throw new IllegalArgumentException("횟수는 음수가 될 수 없으므로 프로그램을 종료합니다");
         }
     }
 }
