9aa6826e5afdbf4f9d7b30f7b2803c8868488f61
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 21:40:37 +0900

    refactor: [Output] 상수화 및 경주 우승자 출력하는 함수의 로직 분리

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,12 +1,16 @@
 package racingcar;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Output {
     private static final String IS = " : ";
+    private static final String BAR = "-";
+    private static final String COMMA_BLANK = ", ";
+    private static final String FINAL_WINNER = "최종우승자" + IS;
 
     public void printRacingProcess(Map<String, Integer> carNameAndMoving) {
         for (String carName : carNameAndMoving.keySet()) {
@@ -17,26 +21,35 @@ public class Output {
     }
 
     public void printRacingWinner(Map<String, Integer> carNameAndMoving) {
-        List<String> winnerList = new ArrayList<>();
-        int maxMoving = 0;
-
-        for (int moving : carNameAndMoving.values()) {
-            maxMoving = Math.max(maxMoving, moving);
-        }
-        for (String carName : carNameAndMoving.keySet()) {
-            if (carNameAndMoving.get(carName) == maxMoving) {
-                winnerList.add(carName);
-            }
-        }
-        String winners = String.join(", ", winnerList);
-        System.out.println("최종 우승자 : " + winners);
+        int maxMoving = getCarWithsMaxMoving(carNameAndMoving);
+        List<String> winnerList = getWinnerList(carNameAndMoving, maxMoving);
+        String winners = String.join(COMMA_BLANK, winnerList);
+        System.out.println(FINAL_WINNER + winners);
     }
 
     private StringBuilder appendBar(int count) {
         StringBuilder bar = new StringBuilder();
         for (int i = 0; i < count; i++) {
-            bar.append("-");
+            bar.append(BAR);
         }
         return bar;
+    }
+
+    private int getCarWithsMaxMoving(Map<String, Integer> carNameAndMoving) {
+        int maxMoving = 0;
+        for (int moving : carNameAndMoving.values()) {
+            maxMoving = Math.max(maxMoving, moving);
+        }
+        return maxMoving;
+    }
+
+    private List<String> getWinnerList(Map<String, Integer> carNameAndMoving, int maxMoving) {
+        List<String> winnerList = new ArrayList<>();
+        for (String carName : carNameAndMoving.keySet()) {
+            if (carNameAndMoving.get(carName) == maxMoving) {
+                winnerList.add(carName);
+            }
+        }
+        return winnerList;
     }
 }
