269e7ed4185603793302db0cb1b612eaa1dd0fbf
leorb159 <ldgsocial159@naver.com>
2023-10-31 01:11:21 +0900

    feat(Output): 시도할 횟수를 입력하라는 문구 출력 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -2,9 +2,12 @@ package racingcar;
 
 public class Output {
     private String enterNameOfCar = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private String enterTryTime = "시도할 회수는 몇회인가요?";
     public void printEnterNameOfCar(){
         System.out.println(enterNameOfCar);
     }
 
-
+    public void printEnterTryTime(){
+        System.out.println(enterTryTime);
+    }
 }
