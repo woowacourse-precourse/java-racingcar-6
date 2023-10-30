f3217b5e4e512ce36693f3dee90111057edeb82f
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-10-30 19:12:50 +0900

    refactor: 함수 수정 및 스트림 변경

--- src/main/java/racingcar/model/service/RacingCarServiceImp.java
+++ src/main/java/racingcar/model/service/RacingCarServiceImp.java
@@ -2,8 +2,8 @@ package racingcar.model.service;
 
 import static racingcar.model.constants.Rule.MOVE_VALUE;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.domain.Car;
 import racingcar.model.domain.Game;
 import racingcar.utils.Generator;
@@ -11,44 +11,33 @@ import racingcar.utils.Generator;
 public class RacingCarServiceImp implements RacingCarService {
     @Override
     public void moveCars(Game game) {
-        for (Car car : game.getCars()) {
-            if (MOVE_VALUE.getValue() <= getRandomNumber()) {
+        game.getCars().forEach(car -> {
+            if (isCarMove()) {
                 car.move();
             }
-        }
+        });
         increaseTrial(game);
     }
 
     @Override
     public List<String> getWinner(Game game) {
-        int maxLocation = getMaxLocation(game.getCars());
-        return getMaxLocationCars(game.getCars(), maxLocation);
+        return game.getCars().stream()
+                .filter(car -> car.getLocation() == getMaxLocation(game))
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 
-    private static List<String> getMaxLocationCars(List<Car> cars, int maxLocation) {
-        List<String> carNames = new ArrayList<>();
-        for (Car car : cars) {
-            if (car.getLocation() == maxLocation) {
-                carNames.add(car.getName());
-            }
-        }
-        return carNames;
+    private static int getMaxLocation(Game game) {
+        return game.getCars().stream()
+                .mapToInt(Car::getLocation)
+                .max()
+                .orElse(0);
     }
 
-    private static int getMaxLocation(List<Car> cars) {
-        int max = 0;
-        for (Car car : cars) {
-            if (max < car.getLocation()) {
-                max = car.getLocation();
-            }
-        }
-        return max;
+    private static boolean isCarMove() {
+        return Generator.generateRandomNumber() >= MOVE_VALUE.getValue();
     }
-
-    private static int getRandomNumber() {
-        return Generator.generateRandomNumber();
-    }
-
+    
     private static void increaseTrial(Game game) {
         game.increaseCount();
     }
