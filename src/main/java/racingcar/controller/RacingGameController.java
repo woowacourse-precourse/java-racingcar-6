4136d9bfc02779f332b02b30475d00796bb9676d
ohhyeon2 <dhgus1235@gmail.com>
2023-10-30 19:51:21 +0900

    refactor: userCarNameSave 함수 기능 분리
    좀 더 작은 단위의 함수를 만들기 위해 ","를 기준으로 배열을 생성하는 함수와 배열을 입력 받아 HashMap에 key, value로 넣어주는 함수 두 개로 분리
    
    carNameAndForwardStatus에 두 함수를 넣어주고 예외가 없다면 생성된 HashMap<String, String> personalCar를 반환

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import racingcar.Constant;
 
 public class RacingGameController {
 
@@ -17,19 +18,31 @@ public class RacingGameController {
         }
     }
 
-    public static HashMap<String, String> userCarNameSave(final String inputCarName) {
+    public static String[] commaBasedSplitCarName(String carName) {
+        String[] carNameArr = carName.split(Constant.COMMA);
+
+        ExceptionController.carNameMaxLengthException(carNameArr);
+
+        return carNameArr;
+    }
+
+    public static HashMap<String, String> makePersonalCarAndForwardStatus(final String[] carNameArr) {
         HashMap<String, String> createUserCar = new HashMap<>();
-        String[] userCarNameArr = inputCarName.split(",");
 
-        ExceptionController.carNameMaxLengthException(userCarNameArr);
-
-        for (String car : userCarNameArr) {
+        for (String car : carNameArr) {
             createUserCar.put(car, "");
         }
 
-        ExceptionController.carNameDuplicateException(createUserCar, userCarNameArr);
-
         return createUserCar;
+    }
+
+    public static HashMap<String, String> carNameAndForwardStatus(final String carName) {
+        String[] carNameArr = commaBasedSplitCarName(carName);
+        HashMap<String, String> personalCar = makePersonalCarAndForwardStatus(carNameArr);
+
+        ExceptionController.carNameDuplicateException(personalCar, carNameArr);
+
+        return personalCar;
     }
 
     public static List<String> mostMovingForwardCar(HashMap<String,String> carMoveStatus) {
@@ -41,6 +54,7 @@ public class RacingGameController {
                 winnerList.add(winner);
             }
         }
+
         return winnerList;
     }
 
