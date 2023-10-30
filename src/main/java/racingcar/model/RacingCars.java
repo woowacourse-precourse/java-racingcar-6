78c1cf1c021f35bf42e218e813728c14d4dca9b1
Ukj0ng <xb253q@gmail.com>
2023-10-30 19:03:09 +0900

    feat(RacingCars): 경주에 참여할 차들
    경주에 참여할 차들을 관리할 클래스:
    - 자동차를 경주에 참여시킴

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,2 +1,16 @@
-package racingcar.model;public class RacingCars {
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCars {
+    private final List<Car> racingCars;
+
+    public RacingCars() {
+        this.racingCars = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        racingCars.add(car);
+    }
 }
