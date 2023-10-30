a07299f1b9bb4cd040594948ad0f9d1f00276c67
gundorit <packdev937@gmail.com>
2023-10-30 19:30:51 +0900

    feat (Car) : 자동차 이름이 5자 이내 인지 검증한다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,10 +3,12 @@ package racingcar.domain;
 public class Car {
 
     private final int MOVING_FORWARD_CRITERION = 4;
+    private final int NAME_LENGTH_CRITERION = 5;
     private String name;
     private int position;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
         this.position = 0;
     }
@@ -22,6 +24,12 @@ public class Car {
     public void moveForward(int number) {
         if (number >= MOVING_FORWARD_CRITERION) {
             this.position++;
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > NAME_LENGTH_CRITERION) {
+            throw new IllegalArgumentException("이름은 5자 이내로 작성해야 합니다");
         }
     }
 }
