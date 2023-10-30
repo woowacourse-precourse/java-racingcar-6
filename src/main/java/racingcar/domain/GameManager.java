cb958453537fcb54a50156e6c040b8720c0fd155
khsrla9806 <kimhunsope@naver.com>
2023-10-30 20:58:53 +0900

    feat: 경주할 자동차의 이름을 입력받는 메서드 구현

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -1,9 +1,11 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
+    private static final String CAR_NAME_DELIMIT = ",";
     private List<Car> cars;
     private StringBuilder gameLog;
     private StringBuilder gameWinner;
@@ -15,4 +17,13 @@ public class GameManager {
         this.gameWinner = new StringBuilder();
         this.attemptCount = 0;
     }
+
+    public void readCarNames() {
+        String enteredCarNames = Console.readLine();
+        String[] carNames = enteredCarNames.split(CAR_NAME_DELIMIT);
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
 }
