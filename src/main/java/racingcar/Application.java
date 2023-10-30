8b42df0110df4698bd3cc0fad30a28540953e381
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 14:46:34 +0900

    feat: 레이싱 경주 실행 코드

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameIO.printCarNamesInputMessage();
+        String carNames = Console.readLine();
+        Cars cars = new Cars(carNames);
+
+        GameIO.printLapInputMessage();
+        String lapSize = Console.readLine();
+        Lap lap = new Lap(lapSize);
+
+        RacingGame racingGame = new RacingGame(cars, lap);
+        racingGame.play();
     }
 }
