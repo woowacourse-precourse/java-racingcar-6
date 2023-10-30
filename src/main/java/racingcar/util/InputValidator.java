0b2f90ea5365e6bdf1e8f9afe8c3ae12fdf28668
jiwoo <zbqmgldjfh@gmail.com>
2023-10-30 17:45:03 +0900

    refactor(InputValidator): InputValidator 내부 로직 메서드 분리로 depth 줄이기

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 public class InputValidator {
 
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int MIN_CAR_NAME_LENGTH = 1;
+
     public static boolean isValidCarNameLength(String input) {
         if(isEmptyInput(input)) return false;
         if(isInvalidNameLength(input)) return false;
@@ -17,23 +20,16 @@ public class InputValidator {
     }
 
     public static boolean hasDuplicateCarName(List<String> carNames) {
-        final String DUPLICATE_CAR_NAME = "[ERROR] 자동차 이름은 중복될 수 없습니다.";
-
-        Set<String> sets = Set.copyOf(carNames);
-        if(sets.size() == carNames.size()) {
+        if(Set.copyOf(carNames).size() == carNames.size()) {
             return false;
         }
-
         return true;
     }
 
     public static boolean isValidAllCarNameLength(List<String> carNames) {
         for (String carName : carNames) {
-            if(!isValidCarNameLength(carName)) {
-                return false;
-            }
+            if (!isValidCarNameLength(carName)) return false;
         }
-
         return true;
     }
 
@@ -45,7 +41,7 @@ public class InputValidator {
     }
 
     private static boolean isInvalidNameLength(String input) {
-        if(input.length() < 1 || input.length() > 5) return true;
+        if(input.length() < MIN_CAR_NAME_LENGTH || input.length() > MAX_CAR_NAME_LENGTH) return true;
         return false;
     }
 }
