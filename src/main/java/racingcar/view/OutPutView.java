b31a41131fdc872725316986e2dc040cef050b0e
beomsic <qjatjr29@khu.ac.kr>
2023-10-30 13:02:53 +0900

    :sparkles: Feat: Add output racing results
    - 레이싱 게임을 진행하며 결과를 출력하는 기능 추가

--- src/main/java/racingcar/view/OutPutView.java
+++ src/main/java/racingcar/view/OutPutView.java
@@ -1,5 +1,11 @@
 package racingcar.view;
 
+import static racingcar.view.IOMessage.PRINT_FINAL_WINNER;
+
+import java.util.List;
+import java.util.StringJoiner;
+import racingcar.model.Result;
+
 public class OutPutView {
 
     private static final String FORWARD_STEP = "-";
@@ -10,6 +16,20 @@ public class OutPutView {
 
     public static void println() {
         System.out.println();
+    }
+
+    public static void printResult(Result result) {
+        String resultMessage = result.name() + " : " + FORWARD_STEP.repeat(result.movedCount());
+        System.out.println(resultMessage);
+    }
+
+    public static void printWinner(List<String> winnerList) {
+
+        if(winnerList.isEmpty()) throw new IllegalArgumentException();
+
+        StringJoiner joiner = new StringJoiner(", ");
+        winnerList.forEach(joiner::add);
+        System.out.println(PRINT_FINAL_WINNER.getMessage() + joiner.toString());
     }
 
 }
