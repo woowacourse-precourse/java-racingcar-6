4af0828c89c79fecade5943c331fd273b307f877
hee9841 <lee98417716@gmail.com>
2023-10-31 08:04:22 +0900

    test(racingcar.validation): InputValidation 예외 처리의 메시지 확인 테스트 코드 추가

--- src/test/java/racingcar/validation/InputValidationTest.java
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -25,47 +25,55 @@ class InputValidationTest {
     void 자동차_이름_예외처리() {
         assertThatThrownBy(
                 () -> inputValidation.validateCarNameSize("", NAME_SIZE)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is blank");
 
         assertThatThrownBy(
                 () -> inputValidation.validateCarNameSize(" ", NAME_SIZE)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is blank");
 
         assertThatThrownBy(
                 () -> inputValidation.validateCarNameSize(null, NAME_SIZE)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is null");
         assertThatThrownBy(
                 () -> inputValidation.validateCarNameSize("\n", NAME_SIZE)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is blank");
 
         assertThatThrownBy(
                 () -> inputValidation.validateCarNameSize("123456", NAME_SIZE)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : size of input value exceeds " + NAME_SIZE);
     }
 
     @Test
     void 이동_차수_입력_유효성_확인() {
         inputValidation.validateMovingCount("7");
-
     }
 
     @Test
     void 이동_차수_입력_예외() {
         assertThatThrownBy(
                 () -> inputValidation.validateMovingCount("")
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is blank");
 
         assertThatThrownBy(
                 () -> inputValidation.validateMovingCount(null)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is null");
 
         assertThatThrownBy(
                 () -> inputValidation.validateMovingCount("\n")
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Input invalid : input value is blank");
 
         assertThatThrownBy(
                 () -> inputValidation.validateMovingCount("pobi")
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid input String:", "For input string:", "\"pobi\"");
 
     }
 
