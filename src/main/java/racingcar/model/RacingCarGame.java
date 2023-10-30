07e314973efcb61e0b33c948a082de4eee90a91f
Hwang In Tae <sjobs66@gmail.com>
2023-10-31 00:28:45 +0900

    feat: RacingCarGame goAhead 메서드 구현
    그리고 goAhead의 기준인 4를 static 변수로 선언함.

--- src/main/java/racingcar/model/RacingCarGame.java
+++ src/main/java/racingcar/model/RacingCarGame.java
@@ -4,10 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RacingCarGame implements SettingGame {
+    private static final int criterion = 4;
     private final Map<String, Object> settings = new HashMap<>();
 
     @Override
     public void saveSettings(String attribute, Object value) {
         settings.put(attribute, value);
+    }
+
+    public void goAhead(Car car, int randomNumber) {
+        if (randomNumber >= criterion) {
+            car.move();
+        }
     }
 }
