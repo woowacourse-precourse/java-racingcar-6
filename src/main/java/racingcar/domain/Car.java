60f03f7acf0e5bab3de638509c8dfbcba3c90709
DaeyeonCho <daecho8@gmail.com>
2023-10-31 01:59:10 +0900

    refactor: 게임 클래스, 자동차 클래스 리팩토링
    게임 클래스의 기능들을 작은 단위의 메소드로 나누고 우승자 관련 기능들을 자동차 클래스에 옮겼습니다.
    자동차 이름 예외처리를 자동차 클래스에 옮겼습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,13 +2,26 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Car {
 
+    private static final int NAME_STANDARD = 5;
+    private static final int RUN_STANDARD = 4;
     private final String name;
     private String location = "";
 
     public Car(String name) {
         this.name = name;
+        validateName(this.name);
+    }
+
+    private void validateName(String name) {
+        if (name.length() > NAME_STANDARD) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getName() {
@@ -23,11 +36,33 @@ public class Car {
         if (checkRunCondition()) {
             this.location = location + "-";
         }
+        System.out.println(this.getName() + " : " + this.getLocation());
     }
 
     private boolean checkRunCondition() {
         int runCondition = Randoms.pickNumberInRange(0,9);
-        return runCondition >= 4;
+        return runCondition >= RUN_STANDARD;
+    }
+
+    public static List<String> giveWinner(List<Car> cars) {
+        List<String> winners = new ArrayList<>();
+        sortByLocation(cars);
+        addWinner(cars, winners);
+        return winners;
+    }
+    private static void addWinner(List<Car> cars, List<String> winners) {
+        winners.add(cars.get(0).getName());
+        for (int i = 1; i < cars.size(); i++) {
+            if (cars.get(i).getLocation().length() != cars.get(0).getLocation().length()) {
+                break;
+            } else {
+                winners.add(cars.get(i).getName());
+            }
+        }
+    }
+    private static void sortByLocation(List<Car> cars) {
+        Comparator<Car> cp = (o1, o2) -> o2.getLocation().length() - o1.getLocation().length();
+        cars.sort(cp);
     }
 
 }
--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -9,55 +9,41 @@ import java.util.List;
 public class Game {
 
     public void start() {
-        Car[] cars = this.settingCars();
-        int terms = this.settingTerms();
-        this.play(cars, terms);
-        this.result(cars, terms);
+        List<Car> cars = this.giveCars();
+        int count = this.giveCount();
+        this.play(cars, count);
+        this.result(cars);
     }
 
-    private Car[] settingCars() {
+    private List<Car> giveCars() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
         String nameInput = Console.readLine();
         String[] carNames = nameInput.split(",");
-        for (String name : carNames) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        Car[] cars = new Car[carNames.length];
-        for (int index = 0; index < carNames.length; index++) {
-            cars[index] = new Car(carNames[index]);
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
         }
         return cars;
     }
 
-    private int settingTerms() {
+    private int giveCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
-    private void play(Car[] cars, int terms) {
-        while (terms > 0) {
-            for (Car carObj : cars) {
-                carObj.run();
-                System.out.println(carObj.getName() + " : " + carObj.getLocation());
+    private void play(List<Car> cars, int count) {
+        System.out.print("\n");
+        System.out.println("실행 결과");
+        for (int i = 0; i < count; i++) {
+            for (Car car : cars) {
+                car.run();
             }
-            System.out.println();
-            terms--;
+            System.out.print("\n");
         }
     }
 
-    private void result(Car[] cars, int terms) {
-        System.out.println("실행 결과");
-        List<String > winners = new ArrayList<>();
-        for (Car carObj : cars) {
-            if (carObj.getLocation().length() == terms) {
-                winners.add(carObj.getName());
-            }
-        }
-
+    private void result(List<Car> cars) {
         System.out.print("최종 우승자 : ");
-        System.out.println(String.join(", ", winners));
+        System.out.println(String.join(", ", Car.giveWinner(cars)));
     }
 }
