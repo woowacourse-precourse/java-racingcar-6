c998665e132fc18f0c17a41050a64bfbb1cbd73d
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 02:20:59 +0900

    feat: 횟수 입력에 대한 검증 추가

--- src/main/java/racingcar/validation/RacingCarValidation.java
+++ src/main/java/racingcar/validation/RacingCarValidation.java
@@ -9,4 +9,9 @@ public class RacingCarValidation {
             throw new IllegalArgumentException("해당 이름은 크키가 5 초과입니다.");
     }
 
+    public void illegalValidation(int count){
+        if(count < 1 || count > 10000)
+            throw new IllegalArgumentException("해당 값은 범위 내에서 실행할 수 없는 값입니다. (1 ~ 10000)");
+    }
+
 }
