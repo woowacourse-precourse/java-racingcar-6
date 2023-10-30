c6f812e7980d9d586473f11d9aed94def12b6d8f
ohjuyoungg <ejrm12@naver.com>
2023-10-31 00:42:24 +0900

    test(InputValidation) : 자동차 경주 게임 시도 횟수 검증 테스트 작성

--- src/test/java/racingcar/validation/InputValidationTest.java
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class InputValidationTest {
 
     @Test
-    void validationName_자동차_이름을_검증_성공한다() {
+    void validateName_자동차_이름을_검증_성공한다() {
         // given : 테스트를 진행하기 위해 필요한 값 세팅 및 설정
         String carName = "pobi";
 
@@ -25,45 +26,87 @@ public class InputValidationTest {
 
     @ParameterizedTest
     @MethodSource("validationName_자동차_이름을_검증_실패한다")
-    void validationName_자동차_이름을_검증_실패한다(String carName) {
+    void validateName_자동차_이름을_검증_실패한다(String carName) {
         assertThatThrownBy(() -> InputValidation.validateName(carName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(InputValidation.CAR_NAME_LENGTH_VALIDATION_EXCEPTION);
     }
 
-    private static Stream<Arguments> validationName_자동차_이름을_검증_실패한다() {
+    private static Stream<Arguments> validateName_자동차_이름을_검증_실패한다() {
         return Stream.of(
             Arguments.of("     "),
             Arguments.of(""),
             Arguments.of("abcdefghijkl")
         );
     }
+
     @Test
-    void validateAndSplit_자동차_콤마구분_검증_성공한다() {
+    void validateNameAndSplit_자동차_콤마구분_검증_성공한다() {
         String carName = "pobi,crong";
         String[] result = carName.split(",");
         assertThat(result).contains("pobi", "crong");
         assertThat(result).containsExactly("pobi", "crong");
+        assertDoesNotThrow(() -> InputValidation.validateNameAndSplit(carName));
     }
 
     @ParameterizedTest
-    @MethodSource("validateAndSplit_자동차_콤마구분_검증_실패한다")
+    @MethodSource("validateNameAndSplit_자동차_콤마구분_검증_실패한다")
     void validateAndSplit_자동차_콤마구분_검증_실패한다(String carName) {
         assertThatThrownBy(() -> InputValidation.validateNameAndSplit(carName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(InputValidation.CAR_NAME_SEPARATE_COMMA_EXCEPTION);
     }
 
-    private static Stream<Arguments> validateAndSplit_자동차_콤마구분_검증_실패한다() {
+    private static Stream<Arguments> validateNameAndSplit_자동차_콤마구분_검증_실패한다() {
         return Stream.of(
-            Arguments.of("pobi",".","crong"),
+            Arguments.of("pobi", ".", "crong"),
             Arguments.of("pobi", "/", "crong")
         );
     }
 
     @Test
-    void carSameNameValidation_자동차_이름이_다를경우_성공한다() {
-        List<String> carName = List.of("pobi", "crong");
+    void validateDuplicatedName_자동차_이름이_다를경우_성공한다() {
+        List<String> carNames = List.of("pobi", "crong");
+        assertThat(carNames).contains("pobi", "crong");
+        assertDoesNotThrow(() -> InputValidation.validateDuplicatedName(carNames));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validateDuplicatedName_자동차_이름이_같을경우_실패한다")
+    void validateDuplicateName_자동차_이름이_같을경우_실패한다(List<String> carNames) {
+        assertThatThrownBy(() -> InputValidation.validateDuplicatedName(carNames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(InputValidation.CAR_SAME_NAME_EXCEPTION);
+    }
+
+    private static Stream<Arguments> validateDuplicatedName_자동차_이름이_같을경우_실패한다() {
+        return Stream.of(
+            Arguments.of(Arrays.asList("pobi", "pobi")),
+            Arguments.of(Arrays.asList("crong", "crong")),
+            Arguments.of(Arrays.asList("a", "a"))
+        );
+    }
+
+    @Test
+    void validateIsNumber_자동차_경주_게임_시도_횟수가_숫자일경우_성공한다() {
+        String number = String.valueOf(1);
+        assertDoesNotThrow(() -> InputValidation.validateIsNumber(number));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validateIsNumber_자동차_경주_게임_시도_횟수가_숫자가_아닐경우_실패한다")
+    void validateIsNumber_자동차_경주_게임_시도_횟수가_숫자가_아닐경우_실패한다(String number) {
+        assertThatThrownBy(() -> InputValidation.validateIsNumber(number))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(InputValidation.RACE_GAME_TRY_NUMBER);
+    }
+
+    private static Stream<Arguments> validateIsNumber_자동차_경주_게임_시도_횟수가_숫자가_아닐경우_실패한다() {
+        return Stream.of(
+            Arguments.of("a"),
+            Arguments.of("."),
+            Arguments.of("/")
+        );
     }
 }
 
