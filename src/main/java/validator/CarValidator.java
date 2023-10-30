2fe1b1a7ae013e64df81cc74211b3ee22dcc2f92
dbstn1325 <dbstn1325@naver.com>
2023-10-30 15:24:31 +0900

    feat(CarValidator): 자동차 이름이 공백을 포함한 경우 예외처리 기능 구현

--- src/main/java/validator/CarValidator.java
+++ src/main/java/validator/CarValidator.java
@@ -7,4 +7,10 @@ public class CarValidator {
             throw new IllegalArgumentException("자동차 이름이 5글자를 초과하였습니다. 애플리케이션을 종료합니다.");
         }
     }
+
+    public static void validateIncludeEmpty(String carName) {
+        if (carName.contains(" ")) {
+            throw new IllegalArgumentException("자동차 이름이 공백을 포함하고 있습니다. 애플리케이션을 종료합니다.");
+        }
+    }
 }
