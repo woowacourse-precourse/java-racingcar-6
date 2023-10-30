92fe30fb3ec8feaff019558d412d0bd4438f9395
jang010505 <jang010505@inu.ac.kr>
2023-10-31 02:47:04 +0900

    test: validateCarNameInput 테스트 작성

---
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -0,0 +1,33 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.constant.MessageConstant.*;
+
+class ValidatorTest {
+
+    @Test
+    void 공백_입력_예외처리() {
+        assertThatThrownBy(() -> Validator.validateCarNameInput(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAR_NAME_INPUT_COUNT_ERROR.getMessage());
+    }
+
+    @Test
+    void 이름에_대한_중복_예외처리() {
+        assertThatThrownBy(() -> Validator.validateCarNameInput("pobi,pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAR_NAME_INPUT_DUPLICATE_ERROR.getMessage());
+    }
+
+    @Test
+    void 검증_후_리스트_반환_확인() {
+        List<String> result = Validator.validateCarNameInput("pobi,woni");
+        List<String> answer = List.of("pobi", "woni");
+        assertThat(result).isEqualTo(answer);
+    }
+}
