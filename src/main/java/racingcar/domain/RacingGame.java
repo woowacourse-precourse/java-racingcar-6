abbd6247f0d5ee8e212f26d06bb54da10868e69f
khsrla9806 <kimhunsope@naver.com>
2023-10-30 21:02:56 +0900

    feat: 게임 매니저와 연관을 맺고, 게임 흐름을 제어하는 게임 클래스 생성

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import racingcar.view.View;
+
+public class RacingGame {
+    private final GameManager gameManager;
+
+    private RacingGame(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+    public static RacingGame create() {
+        return new RacingGame(new GameManager());
+    }
+
+    public void run() {
+        View.printStartMessage();
+        gameManager.readCarNames();
+
+        View.printEnterAttemptCountMessage();
+        gameManager.readAttemptCount();
+
+        gameManager.startGame();
+        View.printGameProcess(gameManager.getGameLog());
+
+        gameManager.calculateWinner();
+        View.printGameResult(gameManager.getGameWinner());
+    }
+}
