6ee3c31d0ae9944bd8b14bec2a9e6c8373689fcd
juns1s <juns1s@naver.com>
2023-10-31 00:53:54 +0900

    feat: 에러 메시지 정의

---
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -0,0 +1,16 @@
+package racingcar.message;
+
+public enum ErrorMessage {
+    INVALID_CAR_NAME("차의 이름은 5자 이하만 가능"),
+    INVALID_TIMES("레이스 진행 횟수는 자연수만 가능");
+
+    private String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
