e7057e0ebbac999b28eeef5ccc80bea0220dbdc7
YongHyeon <pyh4616@naver.com>
2023-10-30 21:18:59 +0900

    Refactor: (RacingCarName 클래스), 매직 넘버를 상수로 선언, validateLength 메서드의 if문 조건식을 isInvalidLength 메서드로 분리

--- src/main/java/racingcar/model/RacingCarName.java
+++ src/main/java/racingcar/model/RacingCarName.java
@@ -2,16 +2,23 @@ package racingcar.model;
 
 public class RacingCarName {
 
+    private static final int MAX_LENGTH = 5;
+    private static final int MIN_LENGTH = 1;
+
     private final String racingCarName;
 
     public RacingCarName(String racingCarName) {
-        validate(racingCarName);
+        validateLength(racingCarName);
         this.racingCarName = racingCarName;
     }
 
-    private void validate(String racingCarName) {
-        if ((racingCarName.length() > 5) || (racingCarName.length() < 1)) {
+    private void validateLength(String racingCarName) {
+        if (isInvalidLength(racingCarName)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isInvalidLength(String racingCarName) {
+        return (racingCarName.length() > MAX_LENGTH) || (racingCarName.length() < MIN_LENGTH);
     }
 }
