6f533b9483a5a78c5607f6e8ea51fafba6245fc5
Jo TaeHyeong <wpg.jth@gmail.com>
2023-10-30 23:09:05 +0900

    refactor(ErrorMessage): 에러 메시지 수정

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -1,7 +1,7 @@
 package racingcar.util;
 
 public enum ErrorMessage {
-    NAME_NULL_OR_EMPTY("등록할_이름이_존재하지_않습니다."),
+    NAME_NULL_OR_EMPTY("자동차 이름이 누락되었습니다. 경주를 진행하려면 유효한 이름을 입력해주세요."),
     NAME_MAX_LENGTH("자동차_이름은 최대 5자 이하만 가능합니다."),
     NAME_NO_WHITESPACE("자동차 이름에서 공백은 허용하지 않습니다."),
     NAME_IS_NOT_UNIQUE("중복된 자동차 이름이 존재합니다."),
