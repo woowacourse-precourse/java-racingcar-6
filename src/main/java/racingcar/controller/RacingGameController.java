bcb201ba5dd9a0dcad073ef393fbf5450ffb25b0
U-keun Song <sukeun319@gmail.com>
2023-10-30 13:08:58 +0900

    Refactor : gameStart() 메서드 분리
    gameStart() 메서드에서 사용되던 로직을 분리해서 private 메서드를 몇 개 추가했다.

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -18,21 +18,38 @@ public class RacingGameController {
     }
 
     public void gameStart() {
-        List<String> participants = inputView.getUserInputName();
-        int tryCount = inputView.getUserInputCount();
+        List<String> participants = getParticipantsFromUser();
+        List<Car> cars = generateCarList(participants);
 
+        updateAndRecordCarStatus(cars);
+
+        judgeAndRecordWinner(cars);
+
+        outputView.printResult();
+    }
+
+    private List<String> getParticipantsFromUser() {
+        return inputView.getUserInputName();
+    }
+
+    private List<Car> generateCarList(List<String> participants) {
         List<Car> cars = new ArrayList<>();
         for (String name:participants) {
             cars.add(Model.generateCar(name));
         }
+        return cars;
+    }
 
+    private void updateAndRecordCarStatus(List<Car> cars) {
+        int tryCount = inputView.getUserInputCount();
         while (tryCount-- > 0) {
             cars.forEach(Car::moveOrStop);
             outputView.recordStatus(cars);
         }
+    }
 
+    private void judgeAndRecordWinner(List<Car> cars) {
         Rank rank = Model.generateRank(cars);
         outputView.recordWinner(rank.getWinnerList());
-        outputView.printResult();
     }
 }
