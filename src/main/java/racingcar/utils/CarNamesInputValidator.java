9542e3098733e043a6fceff07df54f4fc4e5b115
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 15:57:25 +0900

    refactor(CarNamesInputValidator): 예외 메시지를 ExceptionMessage에서 가져오도록 변경

--- src/main/java/racingcar/utils/CarNamesInputValidator.java
+++ src/main/java/racingcar/utils/CarNamesInputValidator.java
@@ -13,26 +13,26 @@ public class CarNamesInputValidator {
 
     private static void validateIsNotBlank(String target) {
         if (target.isBlank()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+            throw new IllegalArgumentException(ExceptionMessage.BLANK.getError());
         }
     }
 
     private static void validateFirstCharacterIsNotComma(String target) {
         if (target.charAt(0) == ',') {
-            throw new IllegalArgumentException("첫 번째 문자로 콤마(,)를 입력하면 안됩니다.");
+            throw new IllegalArgumentException(ExceptionMessage.FIRST_CHARACTER_COMMA.getError());
         }
     }
 
     private static void validateLastCharacterIsNotComma(String target) {
         if (target.charAt(target.length() - 1) == ',') {
-            throw new IllegalArgumentException("마지막 문자로 콤마(,)를 입력하면 안됩니다.");
+            throw new IllegalArgumentException(ExceptionMessage.LAST_CHARACTER_COMMA.getError());
         }
     }
 
     private static void validateEachElementIsNotBlank(String[] targets) {
         for (String target : targets) {
             if (target.isBlank()) {
-                throw new IllegalArgumentException("공백 문자로만 입력하면 안됩니다.");
+                throw new IllegalArgumentException(ExceptionMessage.ELEMENT_BLANK.getError());
             }
         }
     }
@@ -40,7 +40,7 @@ public class CarNamesInputValidator {
     private static void validateEachElementCorrectLength(String[] targets) {
         for (String target : targets) {
             if (target.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+                throw new IllegalArgumentException(ExceptionMessage.OUT_OF_LENGTH.getError());
             }
         }
     }
