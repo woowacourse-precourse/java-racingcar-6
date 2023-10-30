941bf3a022ed9754219ab8bff5bde2b777533c67
혜민 <alddlv81@gmail.com>
2023-10-30 10:55:20 +0900

    rename: 파일 이름 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import racingcar.car.CarInfo;
 import racingcar.car.UserInputCarName;
-import racingcar.game.ForwardCar;
+import racingcar.game.processGame;
 import racingcar.game.UserInputGameRound;
 
 import java.util.List;
@@ -11,6 +11,6 @@ public class Application {
     public static void main(String[] args) {
         List<CarInfo> carList = UserInputCarName.getCarList();
         int gameRound = UserInputGameRound.getGameRound();
-        ForwardCar.playGame(carList, gameRound);
+        processGame.playGame(carList, gameRound);
     }
 }
