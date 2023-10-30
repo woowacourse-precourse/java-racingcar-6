ed693350633f87ac42b5d8e7c67eb5729033484b
BlueArcticFox <jominhyeok0103@gmail.com>
2023-10-30 21:21:19 +0900

    add carRepository Method

--- src/main/java/racingcar/model/CarRepository.java
+++ src/main/java/racingcar/model/CarRepository.java
@@ -1,22 +1,52 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.view.OutputView;
 
-public class CarRepository {
+public enum CarRepository {
+    INSTANCE;
     private List<Car> carList = new ArrayList<Car>();
+
+    public void addCar(List<String> nameList) {
+        for(String name : nameList) {
+            carList.add(new Car(name));
+        }
+    }
+
+    public void go(int numberExcution) {
+        for(int i=0; i<numberExcution; i++) {
+            for(Car car : carList) {
+                car.go();
+            }
+        }
+    }
 
     public List<Car> getCarList() {
         return carList;
     }
 
     public List<Car> getWinners() {
+        int numberWin = winnerScore();
         List<Car> winners = new ArrayList<Car>();
+
         for (Car car : carList) {
-            if (car.getGo() == carList.size()) {
+            if (car.getGo() == numberWin) {
                 winners.add(car);
             }
         }
         return winners;
+    }
+
+    private int winnerScore() {
+        int result = 0;
+        for(Car car : carList) {
+            int carGo = car.getGo();
+            if(result < carGo) {
+                result = carGo;
+            }
+        }
+        return result;
     }
 }
