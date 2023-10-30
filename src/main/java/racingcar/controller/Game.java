ef7e2cd37d31a2cd04b74c59f00fe46883bbf51d
Dawin <dawin002@naver.com>
2023-10-31 04:45:46 +0900

    refactor(ValidateInput): 입력값 유효성 검사 기능 클래스 분리
    입력값의 유효성을 검사하는 기능을 Game 클래스에서 ValidateInput 클래스로 분리
    이동한 메서드:
    - validateNamesInput()
    - checkNameEmpty()
    - checkNameLength()
    - validateRoundInput()
    - checkIsNumber()
    - checkRoundRange()

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -8,8 +8,6 @@ import racingcar.view.OutputView;
 import java.util.List;
 
 public class Game {
-
-    private final static int MAX_NAME_LENGTH = 5;
     private final Race race;
     private final Round round;
 
@@ -20,11 +18,11 @@ public class Game {
 
     public void run() {
         String namesInput = InputView.readCarNames();
-        validateNamesInput(namesInput);
+        ValidateInput.validateNamesInput(namesInput);
         race.setCarList(namesInput);
 
         String roundInput = InputView.readRoundNumber();
-        validateRoundInput(roundInput);
+        ValidateInput.validateRoundInput(roundInput);
         round.setRound(roundInput);
 
         OutputView.printResultStartMessage();
@@ -41,48 +39,6 @@ public class Game {
 
         List<String> winners = race.getWinners();
         OutputView.printGameWinners(winners);
-    }
-
-    private void validateNamesInput(String namesInput) {
-        String[] inputSplit = namesInput.split(",");
-        List<String> nameList = List.of(inputSplit);
-        for (String name : nameList) {
-            checkNameEmpty(name);
-            checkNameLength(name);
-        }
-    }
-
-    private void checkNameEmpty(String name) throws IllegalArgumentException {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("빈 이름이 존재함니다.");
-        }
-    }
-
-    private void checkNameLength(String name) throws IllegalArgumentException {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("5글자를 초과한 이름이 입력되었습니다.");
-        }
-    }
-
-    private void validateRoundInput(String roundInput) {
-        int number = checkIsNumber(roundInput);
-        checkRoundRange(number);
-    }
-
-    private int checkIsNumber(String roundInput) throws IllegalArgumentException {
-        int number;
-        try {
-            number = Integer.parseInt(roundInput);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("정수가 아닌 값이 입력되었습니다.");
-        }
-        return number;
-    }
-
-    private void checkRoundRange(int number) throws IllegalArgumentException {
-        if (number < 1 ) {
-            throw new IllegalArgumentException("시도할 회수가 1보다 작습니다.");
-        }
     }
 
 }
