e7bfa1ca72be4d53a562c7ae70d452f11b4261de
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-30 23:23:19 +0900

    [FEAT] car의 name을 저장해주는 함수 구현
    중복을 제거하기 위해 LinkedHashMap 사용

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,9 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
 
 public class User {
-
     private String input(){
         return Console.readLine();
     }
@@ -12,5 +12,13 @@ public class User {
     }
     private int stringToInteger(String input){
         return Integer.parseInt(input);
+    }
+    public LinkedHashMap<String, Integer> carReset(String[] splitInput){
+        LinkedHashMap<String, Integer> racingCar = new LinkedHashMap<>();
+        splitInput = splitInput(input());
+        for (String car : splitInput){
+            racingCar.put(car, 0);
+        }
+        return racingCar;
     }
 }
