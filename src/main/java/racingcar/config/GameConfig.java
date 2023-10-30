230a83d87e48201fcbadb1d1761eac86ac5c4b2c
kmw2378 <rlarla677@gmail.com>
2023-10-30 15:41:33 +0900

    feat(racingcar): 게임 설정값을 상수로 저장하는 클래스 추가 요구사항에 필요한 리터럴 값을 저장하는 클래스를 추가하였습니다.

---
+++ src/main/java/racingcar/config/GameConfig.java
@@ -0,0 +1,17 @@
+package racingcar.config;
+
+public final class GameConfig {
+    private GameConfig() {
+
+    }
+
+    public static final String MOVE_VALUE = "-";
+    public static final String INPUT_NAMES_SEPARATOR = ",";
+    public static final String WINNER_NAMES_RESULT_SEPARATOR = ", ";
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MIN_TO_MOVE = 4;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final int INITIAL_DISTANCE = 0;
+    public static final int MIN_TOTAL_ROUND = 1;
+}
