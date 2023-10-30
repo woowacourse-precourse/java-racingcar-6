caa262b5eb523d833b84072c644b08cbd9470610
devholic22 <hyunjoon.tech@gmail.com>
2023-10-31 00:00:40 +0900

    test: Cars 정상 생성 테스트
    중복된 이름을 가진 자동차가 없을 시 Cars 정상 생성 테스트

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,12 +1,20 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static racingcar.model.exception.ExceptionMessage.DUPLICATE_CAR_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
+
+    @Test
+    @DisplayName("자동차 이름이 모두 독립적이면 정상 생성된다.")
+    void createCarsTest() {
+        // when & then
+        assertDoesNotThrow(() -> Cars.from("john,bob,alice"));
+    }
 
     @Test
     @DisplayName("같은 이름을 가진 자동차가 있으면 예외가 발생한다.")
