07753e952133da17b1504e71af09b072382b1fbc
devholic22 <hyunjoon.tech@gmail.com>
2023-10-30 12:39:54 +0900

    feat: Cars 객체 생성
    - Car들을 관리하는 일급 컬렉션 Cars 생성
    - 각 Car들이 경주하도록 race 메서드 생성

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,32 @@
+package racingcar.model;
+
+import racingcar.model.randomnumber.RandomNumber;
+import racingcar.util.NameFactory;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    private Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(final String names) {
+        List<Name> carNames = NameFactory.createNames(names);
+        List<Car> cars = carNames.stream()
+                .map(Car::createDefault)
+                .toList();
+
+        return new Cars(cars);
+    }
+
+    public void race(final RandomNumber randomNumber) {
+        cars.forEach(car -> {
+            int number = randomNumber.pickNumber();
+            if (car.isSatisfiedMove(number)) {
+                car.move();
+            }
+        });
+    }
+}
