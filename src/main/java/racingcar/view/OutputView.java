d4692b2da935f4758f7b708b2c592c9986ae1ad0
D0ri123 <lsh81550@gmail.com>
2023-10-31 02:42:32 +0900

    refactor: 콘솔 출력 문구를 상수화 - 4차 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class OutputView {
-    private static final String RESULT_LOG = "실행 결과";
+    private static final String RESULT_MESSAGE = "실행 결과";
     private static final String MOVING_MARK = "-";
     private static final String ANNOUNCEMENT_OF_WINNER = "최종 우승자 : %s";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String RESULT_LOG = "%s : %s \n";
 
     public void showResultLog() {
-        System.out.println();
-        System.out.println(RESULT_LOG);
+        System.out.print(NEW_LINE);
+        System.out.println(RESULT_MESSAGE);
     }
 
     public void showOneRound(Map<String, Integer> gameResult) {
         for(String name : gameResult.keySet()) {
             Integer result = gameResult.get(name);
-            System.out.println(name + " : " + MOVING_MARK.repeat(result));
+            System.out.printf(RESULT_LOG, name, MOVING_MARK.repeat(result));
         }
-        System.out.println();
+        System.out.print(NEW_LINE);
     }
 
     public void showWinners(ArrayList<String> winners) {
