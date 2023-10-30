11cdae6af133e3fbc39577317301e73a3ee5d73c
kimwanyoung <dhks2869@gmail.com>
2023-10-30 11:21:47 +0900

    feat : 프로젝트 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,6 @@
 package racingcar;
 
-import controller.RacingController;
-
 public class Application {
     public static void main(String[] args) {
-        RacingController racingController = new RacingController();
-
-        racingController.playGame();
     }
 }
