7bb0cda41559d28ff52e507bfcd9cafe4325b3d9
gilik154 <okk080200@gmail.com>
2023-10-30 18:14:00 +0900

    Feat : Car의 name valid 관련 로직 수정
    - Car.java 에서 name 을 검증하는 로직을 empty일때도 검증하게 수정
    - CarTest.java 에서 Car 관련 테스트 코드 작성

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -15,9 +15,17 @@ public class Car {
     }
 
     private void valid(String name) {
-        if (name.length() > NAME_LENGTH) {
-            throw new IllegalArgumentException("차의 이름은 5글자 이하여야 합니다.");
+        if (isOver(name) || isEmpty(name)) {
+            throw new IllegalArgumentException("이름을 확인해주세요.");
         }
+    }
+
+    private boolean isOver(String name) {
+        return name.length() < NAME_LENGTH;
+    }
+
+    private boolean isEmpty(String name) {
+        return name.isEmpty();
     }
 
     public void move(DriveRule driveRule) {
@@ -31,6 +39,6 @@ public class Car {
     }
 
     public String printPosition() {
-        return name + "-".repeat(Math.max(0, position));
+        return name + " : " + "-".repeat(Math.max(0, position));
     }
 }
---
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -0,0 +1,73 @@
+package racingcar.domain.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.policy.DriveRule;
+import racingcar.domain.policy.RandomDriveRule;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CarTest {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
+
+    @Test
+    @DisplayName("Car은 정상적으로 생성 되어야 한다.")
+    void Car_정상_생성_테스트() {
+        Car car = new Car("test");
+
+        assertNotNull(car);
+    }
+
+    @Test
+    @DisplayName("Car의 이름이 5글자를 넘으면 오류가 발생한다.")
+    void Car_Name_6글자_이상이면_익셉션_발생() {
+        assertThrows(IllegalArgumentException.class, () -> new Car("abcdef"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("Car은 5 이상의 값이 나오면 전진 할 수 있어야 한다.")
+    void Car은_정상적으로_전진한다(int value) {
+        DriveRule driveRule = mock(RandomDriveRule.class);
+        when(driveRule.generateValue(MIN_RANGE, MAX_RANGE)).thenReturn(value);
+
+        Car car = new Car("test");
+        car.move(driveRule);
+        String position = car.printPosition();
+
+        assertEquals("test : -", position);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("Car은 2회 전진할 수 있어야 한다.")
+    void Car은_정상적으로_전진한다_2회() {
+        DriveRule driveRule = mock(RandomDriveRule.class);
+        when(driveRule.generateValue(MIN_RANGE, MAX_RANGE)).thenReturn(5);
+
+        Car car = new Car("test");
+        car.move(driveRule);
+        car.move(driveRule);
+        String position = car.printPosition();
+
+        assertEquals("test : --", position);
+    }
+
+    @Test
+    @DisplayName("Car은 4 미만의 값이 나오면 전진 할 수 없다.")
+    void Car은_전진하지_못한다() {
+        DriveRule driveRule = mock(RandomDriveRule.class);
+        when(driveRule.generateValue(MIN_RANGE, MAX_RANGE)).thenReturn(3);
+
+        Car car = new Car("test");
+        car.move(driveRule);
+        String position = car.printPosition();
+
+        assertEquals("test : ", position);
+    }
+}
