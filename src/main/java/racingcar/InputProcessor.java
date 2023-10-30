a55a5cff45d985717af4887520256ccd27443503
Finn <wjstodsss@naver.com>
2023-10-30 21:21:02 +0900

    feat: 사용자 입력을 받는 기능 추가

---
+++ src/main/java/racingcar/InputProcessor.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputProcessor {
+    public static String readLine() {
+        return Console.readLine();
+    }
+
+    public static void close() { Console.close();}
+
+}
