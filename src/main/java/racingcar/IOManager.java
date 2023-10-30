69043c5ef67038101659411f4db4272dba8a881c
Jonghyeok Lee <endb1228@gmail.com>
2023-10-30 21:16:03 +0900

    IOManager 싱글톤 구현

--- src/main/java/racingcar/IOManager.java
+++ src/main/java/racingcar/IOManager.java
@@ -1,4 +1,16 @@
 package racingcar;
 
 public class IOManager {
+
+    private static IOManager ioManager;
+
+    private IOManager() {
+    }
+
+    public static IOManager getInstance() {
+        if (ioManager == null) {
+            ioManager = new IOManager();
+        }
+        return ioManager;
+    }
 }
