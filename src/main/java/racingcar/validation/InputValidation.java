0fa54762fd0a0aff8ad81b9639c4dc0d6818b290
hee9841 <lee98417716@gmail.com>
2023-10-31 06:25:02 +0900

    feat(racingcar): 입력관련 유효성 체크 기능 구현

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -2,4 +2,47 @@ package racingcar.validation;
 
 
 public class InputValidation {
+
+    public void validateCarNameSize(String carName, int size) {
+
+        if (isBlank(carName)) {
+            throw new IllegalArgumentException("Input invalid : input value is blank");
+        }
+        if (carName.length() > size) {
+            throw new IllegalArgumentException("Input invalid : size of input value exceeds " + size);
+        }
+
+    }
+
+    public void validateMovingCount(String MovingCount) {
+        if (isBlank(MovingCount)) {
+            throw new IllegalArgumentException("Input invalid : input value is blank");
+        }
+        validateStringIsNumeric(MovingCount);
+    }
+
+
+    private boolean isBlank(String str) {
+
+        try {
+            return str.isBlank();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("Input invalid : input value is null");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Input invalid : " + e.getMessage());
+        }
+    }
+
+    private void validateStringIsNumeric(String str) {
+
+        try {
+            Integer.parseInt(str);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+    }
+
 }
+
+
