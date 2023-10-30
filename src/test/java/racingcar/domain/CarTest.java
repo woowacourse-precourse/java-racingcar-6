ff281da4be77bb620258a39d742ae1ffdf200c6e
SongJwans <yhh5916@naver.com>
2023-10-30 21:20:22 +0900

    test: test_move(), test_generatedRandomNumber() 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class CarTest {
@@ -12,6 +13,23 @@ public class CarTest {
 
         assertEquals("poni", car.getName());
         assertEquals(0, car.getDistance());
+    }
+
+    @Test
+    public void test_move() {
+        Car car = new Car("test");
+        car.move();
+        car.move();
+
+        assertEquals(2, car.getDistance());
+    }
+
+    @Test
+    public void test_generatedRandomNumber() {
+        Car car = new Car("test");
+        int randomNumber = car.generatedRandomNumber();
+
+        assertTrue(randomNumber >= 0 && randomNumber <= 9);
     }
 }
 
