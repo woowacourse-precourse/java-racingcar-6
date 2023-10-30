1f1003ddee295a650a655839f9f11d6be9fa6b42
1Bronze <khdkkhdd@gmail.com>
2023-10-30 17:44:42 +0900

    refactor(input): move input logic from service to controller

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,14 +1,26 @@
 package racingcar.controller;
 
+import static racingcar.common.Constants.CAR_LIST_DIVIDER;
+import static racingcar.common.Constants.CAR_NAME_REQUEST_MESSAGE;
+
 import java.util.List;
 import racingcar.service.RacingCarService;
+import racingcar.util.StringUtil;
 import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingCarController {
     private final RacingCarService racingCarService = new RacingCarService();
 
     public void start() {
-        List<String> carList = racingCarService.inputCarList();
+        List<String> carList = inputCarList();
         InputValidator.validateCarList(carList);
+    }
+
+    public List<String> inputCarList() {
+        OutputView.printInfo(CAR_NAME_REQUEST_MESSAGE);
+        List<String> carList = StringUtil.splitByDelimiter(InputView.input(), CAR_LIST_DIVIDER);
+        return carList;
     }
 }
