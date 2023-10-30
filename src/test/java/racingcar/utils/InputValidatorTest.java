0833b22eed2c24905d03f2484cbed5eb9c764623
isyoo1213 <moethns@gmail.com>
2023-10-30 18:30:14 +0900

    test: InputValidatorTest 자동차이름 입력테스트 추가 InputValidatorTest - validateCorrectInputTest()   - 자동차 이름 유효성 테스트 - 정상 데이터 - 성공 - validateInputWithoutSeparatorTest()   - 자동차 이름 유효성 테스트 - 구분자 없는 데이터 - 오류 - validateInputWrongRangeTest()   - 자동차 이름 유효성 테스트 - 5자 초과 데이터 - 오류 - validateInputDuplicatedTest()   - 자동차 이름 유효성 테스트 - 중복되는 데이터 - 오류

--- src/test/java/racingcar/utils/InputValidatorTest.java
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -39,5 +41,40 @@ public class InputValidatorTest {
     @ParameterizedTest
     void preprocessSpacingWIthEmptyInputTest(String names) {
         assertThat(inputValidator.preprocessInputString(names)).isEqualTo("pobi,");
+    }
+
+    @DisplayName("자동차 이름 유효성 테스트 - 정상 데이터 - 성공")
+    @ValueSource(strings = {"pobi,inss,yh"})
+    @ParameterizedTest
+    void validateCorrectInputTest(String names) {
+        List<String> resultNames = Arrays.asList("pobi", "inss", "yh");
+        assertThat(inputValidator.validateCarsNameInput(names)).isEqualTo(resultNames);
+    }
+
+    @DisplayName("자동차 이름 유효성 테스트 - 구분자 없는 데이터 - 오류")
+    @ValueSource(strings = {"pobiinssyh"})
+    @ParameterizedTest
+    void validateInputWithoutSeparatorTest(String names) {
+        assertThatThrownBy(() -> inputValidator.validateCarsNameInput(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 1~5자로, 구분자는 쉼표(,)를 사용해주세요.");
+    }
+
+    @DisplayName("자동차 이름 유효성 테스트 - 5자 초과 데이터 - 오류")
+    @ValueSource(strings = {"pobi,inssyh"})
+    @ParameterizedTest
+    void validateInputWrongRangeTest(String names) {
+        assertThatThrownBy(() -> inputValidator.validateCarsNameInput(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 1~5자로 입력해주세요.");
+    }
+
+    @DisplayName("자동차 이름 유효성 테스트 - 중복되는 데이터 - 오류")
+    @ValueSource(strings = {"pobi,pobi"})
+    @ParameterizedTest
+    void validateInputDuplicatedTest(String names) {
+        assertThatThrownBy(() -> inputValidator.validateCarsNameInput(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복되는 이름은 입력할 수 없습니다.");
     }
 }
