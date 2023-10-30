02733c0374e5414b616aa26bb998417485a936a7
alscjf1329 <alscjf1329@gmail.com>
2023-10-30 13:25:37 +0900

    style: 우테코 코드 컨벤션으로 스타일 변경
     우테코 프리코스 readme에 적혀 있는 코드 컨벤션으로 스타일을 변경했습니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -19,7 +19,7 @@ class ApplicationTest extends NsTest {
     @Test
     void validateCarNames_메소드를_사용시_자동차_이름이_5자가_넘어가는_경우_예외_발생() {
         List<RacingCar> carList = List.of(
-            new RacingCar("exception")
+                new RacingCar("exception")
         );
         assertThatThrownBy(() -> RacingCarGameInput.validateCarNames(carList))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -41,10 +41,10 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void printResultMessage_메소드를_사용시_실행_결과들이_출력(){
+    void printResultMessage_메소드를_사용시_실행_결과들이_출력() {
         List<RacingCar> carList = List.of(
-            new RacingCar("pobi"),
-            new RacingCar("woni")
+                new RacingCar("pobi"),
+                new RacingCar("woni")
         );
         carList.get(0).moveForword();
         RacingCarGameOutput.printGameProgress(carList);
@@ -52,16 +52,16 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void printWinnerMessage_메소드를_사용시_1대의_우승한_자동차_출력(){
+    void printWinnerMessage_메소드를_사용시_1대의_우승한_자동차_출력() {
         List<RacingCar> carList = List.of(
-            new RacingCar("woni")
+                new RacingCar("woni")
         );
         RacingCarGameOutput.printWinnersMessage(carList);
         assertThat(output()).contains("최종 우승자 : woni");
     }
 
     @Test
-    void printWinnerMessage_메소드를_사용시_2대이상의_우승한_자동차들_출력(){
+    void printWinnerMessage_메소드를_사용시_2대이상의_우승한_자동차들_출력() {
         List<RacingCar> carList = List.of(
                 new RacingCar("pobi"),
                 new RacingCar("woni"),
@@ -74,11 +74,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
