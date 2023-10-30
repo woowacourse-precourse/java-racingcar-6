71ce8eba509a7774a364d65e7e93b40f7a5c3c85
Seyeon <wjdtpdus828@naver.com>
2023-10-30 23:53:09 +0900

    feat(Views): 시도할 횟수에 대한 메세지를 출력하는 printInputTrialMessage 메소드 구현

--- src/main/java/racingcar/view/Views.java
+++ src/main/java/racingcar/view/Views.java
@@ -2,7 +2,12 @@ package racingcar.view;
 
 public class Views {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
     public void printInputCarMessage(){
         System.out.println(INPUT_CAR_NAME_MESSAGE);
+    }
+
+    public void printInputTrialMessage(){
+        System.out.println(INPUT_TRIAL_NUMBER);
     }
 }
