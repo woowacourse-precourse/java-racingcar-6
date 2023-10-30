42363e945875a1289a46362d731ca28f8cf96151
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-30 16:25:45 +0900

    FEAT: 레이싱게임 클래스 추가 - Race 클래스 - 레이싱 플레이를 구현할 클래스

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Race {
+    public void race(){
+        RacingInfo racingInfo = new RacingInfo();
+        racingInfo.setNamesAndNum();
+        racingInfo.getNames();
+        racingInfo.getNum();
+    }
+}
