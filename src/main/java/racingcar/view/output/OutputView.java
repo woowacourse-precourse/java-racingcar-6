c65a636a6f2a755d41ad69e5c9ae1a38d3bf701c
Kite_Z <duswl339@ajou.ac.kr>
2023-10-30 19:13:59 +0900

    refactor: modify OutputView
    1. add printWinner method
    2. add printMovedCar method

--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -1,16 +1,30 @@
 package racingcar.view.output;
 
 import racingcar.contant.GameNotice;
+import racingcar.model.RacingCar;
 import utils.Console;
 
+import java.util.List;
+
+import static racingcar.contant.GameNotice.NOTICE_WINNER_RESULT;
+
 public class OutputView {
+    public OutputView() {}
     public static void printGameNotice(GameNotice notice) {
         Console.print(notice.getMessage());
     }
-//    public static void printMiddleResult() {}
-//
-//    public static void printWinner() {
-//        printGameNotice(GameNotice.NOTICE_WINNER_RESULT);
-//    }
 
+    public void printMovedCar(RacingCar racingCar) {
+        String moved = "";
+        for(int i = 0; i < racingCar.getCarMoveCount(); i++) {
+            moved = moved.concat("-");
+        }
+
+        Console.println(racingCar.name + " : " + moved);
+    }
+
+    public void printWinner(List<String> winnerList) {
+        printGameNotice(NOTICE_WINNER_RESULT);
+        Console.print(String.join(", ", winnerList));
+    }
 }
