8b100b89cd0861cc902f8ddc8b58ed4a5daa2927
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-30 13:13:48 +0900

    feat: 시도 횟수 묻는 문구 출력 기능 추가
    시도 횟수 묻는 문구 출력 기능 추가
    - ASK_ATTEMPTS 상수 추가
    - 시도 횟수 묻는 구문 출력하는 askAttempts 메서드 추가

--- src/main/java/racingcar/Writer.java
+++ src/main/java/racingcar/Writer.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public final class Writer {
     private static final String OPENING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_ATTEMPTS = "시도할 회수는 몇회인가요?";
+
 
     private Writer() {
         throw new UnsupportedOperationException();
@@ -9,6 +11,10 @@ public final class Writer {
 
     public static void opening() {
         System.out.println(OPENING);
+    }
+
+    public static void askAttempts() {
+        System.out.println(ASK_ATTEMPTS);
     }
 
 
