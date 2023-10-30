4638b2deb8699101f767371a270e83438ddc345f
siwol025 <cyk99115695@gmail.com>
2023-10-30 15:07:52 +0900

    refactor(racingcar): validateCarNamesSize메서드에서 사용한 매직넘버를 상수로 변경

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -3,8 +3,10 @@ package racingcar.util;
 import java.util.List;
 
 public class InputValidator {
+    private static final int MIN_CAR_NAMES_SIZE = 2;
+
     public void validateCarNamesSize(List<String> carNames) {
-        if (carNames.size() < 2) {
+        if (carNames.size() < MIN_CAR_NAMES_SIZE) {
             throw new IllegalArgumentException("경주에 참여할 자동차의 이름 수가 모자릅니다. 적어도 2개 이상의 이름을 입력해주세요.");
         }
     }
