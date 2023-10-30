bd736b0fedeccad5019676404cb2b6627b6d00bc
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 17:58:10 +0900

    feat: 예외처리 메세지 추가

---
+++ src/main/java/racingcar/util/VerificationMessage.java
@@ -0,0 +1,6 @@
+package racingcar.util;
+
+public class VerificationMessage {
+    public static final String IS_NOT_NUMBER = "숫자만 입력해 주세요.";
+    public static final String OVER_SIZE = "이름은 5자 이하로 입력해 주세요.";
+}
