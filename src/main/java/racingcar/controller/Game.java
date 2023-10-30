a65bbf4221b7709f4b0379b1b4040168ed8d91fc
sangchu <didi1484@gmail.com>
2023-10-30 17:49:14 +0900

    feat: 잘못된 횟수 입력시 예외 발생 메서드 추가

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -10,23 +10,36 @@ import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class Game {
+    private int attemptNumber;
+
     public void start() {
         Cars cars = new Cars();
         List<Car> carList = cars.createCarList(askCarName());
-        int attemptNumber = askAttemptNumber();
+        attemptNumber = askAttemptNumber();
+        validate();
         printResult();
-        repeat(carList, attemptNumber);
+        repeat(carList);
         List<String> winners = cars.findWinner();
         printWinner(winners);
     }
 
-    private void repeat(List<Car> carList, int attemptNumber) {
+    private void repeat(List<Car> carList) {
         for (int i = 0; i < attemptNumber; i++) {
             for (Car car : carList) {
                 car.moveForward(car.generateRandomNumber());
                 printResult(car);
             }
             System.out.println();
+        }
+    }
+
+    public boolean isValidNumber() {
+        return attemptNumber > 0;
+    }
+
+    public void validate() {
+        if (!isValidNumber()) {
+            throw new IllegalArgumentException("입력하신 횟수가 올바르지 않습니다.");
         }
     }
 }
