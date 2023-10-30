79f3021c5dfc21960ef143b857ec75b9740b5dbc
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 01:39:50 +0900

    feat: add print drive result function to output view

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,33 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class OutputView {
     private final static String RESULT = "실행 결과";
+    private final static String COLON = " : ";
 
     public void printResult() {
         System.out.println(RESULT);
+    }
+
+    public void printDrive(List<String> carNameList, List<Integer> carDriveCountList) {
+        int carListSize = carNameList.size();
+        String carName;
+        int carDriveCount;
+        String driveCountVisual;
+
+        for (int i = 0; i < carListSize; i++) {
+            carName = carNameList.get(i);
+            carDriveCount = carDriveCountList.get(i);
+            driveCountVisual = driveCountToString(carDriveCount);
+
+            System.out.println(carName + COLON + driveCountVisual);
+
+        }
+    }
+
+    private String driveCountToString(int driveCount) {
+        return "-".repeat(driveCount);
     }
 }
