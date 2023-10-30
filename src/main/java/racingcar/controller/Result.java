9f620b3d0646109769224dbc3deb2725b7f18605
chaniii999 <psc0729@naver.com>
2023-10-30 21:58:35 +0900

    feat: 결과 출력 프로세스 기능 구현

---
+++ src/main/java/racingcar/controller/Result.java
@@ -0,0 +1,38 @@
+package racingcar.controller;
+
+
+import static racingcar.view.OutputString.printStatus;
+
+import java.util.ArrayList;
+import racingcar.model.Car;
+import racingcar.model.FindWinner;
+import racingcar.view.OutputString;
+
+public class Result {
+
+
+    public Result(ArrayList<Car> classList, int count) {
+        OutputString.printResult(); // 실행결과
+
+        printRound(classList, count); // 라운드출력
+        FindWinner findWinner = new FindWinner(classList);
+        OutputString.printWinner(findWinner.getWinner());
+    }
+
+    private void printRound(ArrayList<Car> classList, int count) {
+        for (int i = 0; i < count; i++) {
+            round(classList);
+            System.out.println();
+        }
+    }
+
+    private void round(ArrayList<Car> classList) {
+        for (Car car : classList) {
+            car.moveOrStop();
+            printStatus(car.getName(), car.getPosition());
+        }
+    }
+
+
+
+}
