bc14030c26a0a591164c110bdb4e1867e7b625b8
kchranger <kchranger@gmail.com>
2023-10-30 23:55:48 +0900

    feature(app) : race time validation check

--- src/main/java/racingcar/GameInput.java
+++ src/main/java/racingcar/GameInput.java
@@ -8,9 +8,22 @@ public class GameInput {
         return Console.readLine();
     }
 
-    public static int raceTime() {
+    public static int raceTime() throws IllegalArgumentException {
         String raceTimeString = Console.readLine();
+        if (!isStringConvertToInteger(raceTimeString)) {
+            throw new IllegalArgumentException();
+        }
         return Integer.valueOf(raceTimeString);
+    }
+
+    private static boolean isStringConvertToInteger(String raceTimeString) {
+        char[] raceTimeCharArray = raceTimeString.toCharArray();
+        for (char raceTimeChar : raceTimeCharArray) {
+            if (raceTimeChar < '0' || raceTimeChar > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
