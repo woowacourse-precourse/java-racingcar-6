7cd7f787ebf2fd67578d69e9962fca0379cc0533
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:37:02 +0900

    feat(racingcar): OutputView에 질문, 게임 시작 메시지 출력 메소드 추가 입력 전 질문 내용, 게임 진행 관련 출력을 하는 static 메소드를 추가하였습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,24 @@
 package racingcar.view;
 
+import static racingcar.view.message.GameMessage.CAR_NAME_INPUT_QUESTION_MESSAGE;
+import static racingcar.view.message.GameMessage.GAME_RESULT_PREFIX_MESSAGE;
+import static racingcar.view.message.GameMessage.TOTAL_ROUND_INPUT_QUESTION_MESSAGE;
+
 public final class OutputView {
     private OutputView() {
 
+    }
+
+    public static void printCarNameInputQuestion() {
+        System.out.println(CAR_NAME_INPUT_QUESTION_MESSAGE);
+    }
+
+    public static void printTotalRoundInputQuestion() {
+        System.out.println(TOTAL_ROUND_INPUT_QUESTION_MESSAGE);
+    }
+
+    public static void printGameStartMessage() {
+        System.out.println();
+        System.out.println(GAME_RESULT_PREFIX_MESSAGE);
     }
 }
