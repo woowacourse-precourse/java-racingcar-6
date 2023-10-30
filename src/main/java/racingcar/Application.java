0d928d976d524a6e97ef847c677bd98c9263848c
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-10-30 12:51:47 +0900

    checkCarEligibility 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 
@@ -11,6 +12,17 @@ public class Application {
 
     public int inputNumber() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public void checkCarEligibility(String[] cars) {
+        for(int i = 0; i<cars.length; i++) {
+            System.out.print(cars[i] + ": ");
+            int eligibility = Randoms.pickNumberInRange(0, 9);
+
+            if (eligibility > 4) {
+                System.out.println("-");
+            }
+        }
     }
     public static void main(String[] args) {
     }
