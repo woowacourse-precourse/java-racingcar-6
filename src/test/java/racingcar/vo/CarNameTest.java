6b51e8b2fb64bfd54d3e83b93cc041b1bbf71a52
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-30 15:29:08 +0900

    test : 자동차 이름에 대한 레코드의 테스트 코드 작성

--- src/test/java/racingcar/vo/CarNameTest.java
+++ src/test/java/racingcar/vo/CarNameTest.java
@@ -1,5 +1,7 @@
 package racingcar.vo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,9 +9,40 @@ import org.junit.jupiter.api.Test;
 
 class CarNameTest {
 
+    private static final Integer CAR_NAME_MAX_LENGTH = 5;
+
+    @DisplayName("유효한 자동차 이름인 경우 예외가 발생하지 않고 객체가 정상적으로 생성된다.")
+    @Test
+    void validCarName_test() {
+        String validName = "a".repeat(CAR_NAME_MAX_LENGTH);
+        CarName carName = new CarName(validName);
+
+        assertEquals(validName, carName.name());
+    }
+
     @DisplayName("자동차 이름이 6자 이상인 경우 예외가 발생한다.")
     @Test
-    void constructorInvalidCarName_exception_test() {
-        assertThrows(IllegalArgumentException.class, () -> new CarName("abcdef"));
+    void invalidCarName_exception_test() {
+        String invalidName = "a".repeat(CAR_NAME_MAX_LENGTH + 1);
+
+        assertThrows(IllegalArgumentException.class, () -> new CarName(invalidName));
+    }
+
+    @DisplayName("필드값이 동일하면 레코드는 동일하다.")
+    @Test
+    void carName_equal_test() {
+        CarName carName1 = new CarName("pobi");
+        CarName carName2 = new CarName("pobi");
+
+        assertEquals(carName1, carName2);
+    }
+
+    @DisplayName("필드값이 동일하지 않으면 레코드는 동일하지 않다.")
+    @Test
+    void carName_not_equal_test() {
+        CarName carName1 = new CarName("pobi");
+        CarName carName2 = new CarName("kdo");
+
+        assertNotEquals(carName1, carName2);
     }
 }
