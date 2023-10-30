b0dd761bb1172b47dfd9dfd566bd18d8b7df0bb7
fromitive <fromitive@gmail.com>
2023-10-30 18:28:42 +0900

    feat: OutputView 구현

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+public class OutputView implements RacingCarGameText {
+
+    private static void printOutput(String outputMessage) {
+        System.out.println(outputMessage);
+    }
+
+    public static void printRequestMultipleCarName() {
+        printOutput(RACINGCAR_REQUEST_MULTIPLE_CAR_NAME);
+    }
+
+    public static void printRequestTryCount() {
+        printOutput(RACINGCAR_REQUEST_TRY_COUNT);
+    }
+
+    public static void printExecuteResult() {
+        printOutput(RACINGCAR_EXECUTE_RESULT);
+    }
+
+    public static void printFormatFinalWinner(String winner) {
+        printOutput(String.format(RACINGCAR_FORMAT_FINAL_WINNER, winner));
+    }
+
+    public static void printFormatStatus(String carName, String carDistance) {
+        if (carDistance.isBlank()){
+            carDistance = RACINGCAR_DOES_NOT_MOVE;
+        }
+        printOutput(String.join(RACINGCAR_FORMAT_STATUS, carName, carDistance));
+    }
+}
---
+++ src/main/java/racingcar/RacingCarGameText.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public interface RacingCarGameText {
+    String RACINGCAR_REQUEST_MULTIPLE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    String RACINGCAR_REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    String RACINGCAR_EXECUTE_RESULT = "실행 결과";
+    String RACINGCAR_FORMAT_STATUS = "%s : %s";
+    String RACINGCAR_FORMAT_FINAL_WINNER = "최종 우승자 : %s";
+    String RACINGCAR_ERROR_INVALID_NUMBER_OF_NAMECHARACTER = "자동차 이름은 [%s]자리 이상 넘을 수 없습니다";
+    String RACINGCAR_ERROR_INVALID_TRY_COUNT_NUMBER = "회수는 자연수를(1 이상의 정수) 입력해야 합니다";
+    String RACINGCAR_DOES_NOT_MOVE = "\u200B"; // zero-width-space trim()에 지워지지 않는 값.
+    String RACINGCAR_LOCATION_SYMBOL = "-";
+}
