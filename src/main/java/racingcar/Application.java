6876e864ae41bd8efecea054bb560ae77d8fe33c
LooanCheong <jjyejun@naver.com>
2023-10-30 21:59:37 +0900

    <feat>: 자동차 이름 입력 받기
    자동차 이름을 ','를 기준으로 split하여 입력받은 리스트를 생성합니다.
    자동차 이름이 5자를 초과하면 IllegalArgumentException 오류를 발생시킵니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        //자동차 이름 입력 받기
+        List<String> cars = List.of(br.readLine().split(","));
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하입니다.");
+            }
+        }
+
+        
     }
+
+
 }
