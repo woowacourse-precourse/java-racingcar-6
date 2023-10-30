2342c3a8e618b30bbfebc738ed9b531c7bd365c8
gmu1026 <curiouscomponent@gmail.com>
2023-10-31 01:22:20 +0900

    TEST: 복수의 차량 생산 테스트 추가 및 차생산 클래스 분리 적용

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,23 +3,48 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.car.Car;
+import racingcar.car.CarFactory;
 
 public class CarTest {
     @Test
     void 차_생성_테스트() {
         String testCarName = "찬";
-        Car car = Car.createCar(testCarName);
+        List<Car> cars = CarFactory.createCar(testCarName);
 
-        assertThat(car.getName()).isEqualTo(testCarName);
+        assertThat(cars).hasSize(1);
+        assertThat(cars.get(0).getName()).isEqualTo(testCarName);
+    }
+
+    @Test
+    void 복수의_차_생성_테스트() {
+        List<String> carNames = Arrays.asList("java", "spring", "gradle");
+
+        String testCarNames = String.join(",", carNames);
+        List<Car> cars = CarFactory.createCar(testCarNames);
+
+        assertThat(cars).hasSize(3);
+        assertThat(cars.get(0).getName()).isEqualTo(carNames.get(0));
+        assertThat(cars.get(1).getName()).isEqualTo(carNames.get(1));
+        assertThat(cars.get(2).getName()).isEqualTo(carNames.get(2));
     }
 
     @Test
     void 공백이_포함된_차_이름_입력값_테스트() {
         String testCarName = "우 아";
 
-        assertThatThrownBy(() -> Car.createCar(testCarName))
+        assertThatThrownBy(() -> CarFactory.createCar(testCarName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 특수문자가_포함된_차_이름_입력값_테스트() {
+        String testCarName = "차!";
+
+        assertThatThrownBy(() -> CarFactory.createCar(testCarName))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
