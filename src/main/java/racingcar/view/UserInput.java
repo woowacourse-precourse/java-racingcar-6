4d05e3fada6f7845034b47b81b976bdcab7e6802
YongJOoOo <we072200@naver.com>
2023-10-30 13:39:03 +0900

    feat: UserInput에 추가로 validate() 메소드 구현
    -gameCount 입력 예외처리를 위해 validate 메소드 추가로 구현

--- src/main/java/racingcar/view/UserInput.java
+++ src/main/java/racingcar/view/UserInput.java
@@ -95,6 +95,8 @@ public class UserInput {
         validateGameCountNotNumber(gameCount);
         //범위 벗어난 입력
         validateGameCountOutOfRange(gameCount);
+        //0으로 시작하는 입력
+        validateGameCountStartWithZero(gameCount);
     }
 
     private void validateGameCountNotNumber(String gameCount) throws IllegalArgumentException {
@@ -109,6 +111,12 @@ public class UserInput {
         int num = Integer.parseInt(gameCount);
         if (num <= 0) {
             throw new IllegalArgumentException("게임 실행 횟수는 양수만 입력해야 합니다.");
+        }
+    }
+
+    private void validateGameCountStartWithZero(String gameCount) throws IllegalArgumentException {
+        if (gameCount.startsWith("0")) {
+            throw new IllegalArgumentException("게임 실행 횟수 입력은 0으로 시작하면 안됩니다.");
         }
     }
 
