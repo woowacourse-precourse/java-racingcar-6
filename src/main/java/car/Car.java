2b8663145906ca3cd65107047de4fadfa30fb5f9
sh990920 <sh990920@gmail.com>
2023-10-30 14:49:15 +0900

    feat

---
+++ src/main/java/car/Car.java
@@ -0,0 +1,20 @@
+package car;
+
+public class Car {
+    private String name;
+
+    private int count;
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+}
