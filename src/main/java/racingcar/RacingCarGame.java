706334fe614cbdf8b409dd1bd270455a8f0a5864
kang yeon joo <rkdduswndi@naver.com>
2023-10-30 11:21:59 +0900

    fix: 사용자 입력 부분 InputManager에 책임 분리

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -8,16 +8,17 @@ import java.util.List;
 public class RacingCarGame {
     public void play() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input_str = Console.readLine();
-        makeRacingCars(input_str);
+
+        List<RacingCar> racingCars = InputManager.get_input_car_names();
+        int play_num = InputManager.get_input_play_num();
+
+        for(int i=0;i<play_num;i++){
+            move(racingCars);
+        }
     }
 
-    private List<RacingCar> makeRacingCars(String inputStr) {
-        String[] split_str = inputStr.split(",");
-        List<RacingCar> cars = new ArrayList<RacingCar>();
-        for(int i=0;i<split_str.length;i++){
-            cars.add(new RacingCar(split_str[i]));
-        }
-        return cars;
+    public void move(List<RacingCar> cars){
+
     }
+
 }
