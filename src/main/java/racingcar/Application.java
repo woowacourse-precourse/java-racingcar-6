4db1a8ffded86b6c4ce3d812720d2d7c05e5376f
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-30 14:24:02 +0900

    refactor: 게임 작동 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,6 @@ package racingcar;
 import racingcar.controller.Gamecontroller;
 
 public class Application {
-    private final Gamecontroller gamecontroller = new Gamecontroller();
     public static void main(String[] args) {
         Gamecontroller.start();
     }
