ab546174b30629e7273a76e8bbe431edfda2d391
belljun3395 <195850@jnu.ac.kr>
2023-10-30 17:00:25 +0900

    test(domain/system): 공동우승자 경우 테스트 추가

--- src/test/java/racingcar/domain/system/game/RacingGameTest.java
+++ src/test/java/racingcar/domain/system/game/RacingGameTest.java
@@ -6,8 +6,8 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.core.car.MoveSignal;
-import racingcar.domain.core.car.OnRaceCar;
 import racingcar.domain.core.car.MovingCar;
+import racingcar.domain.core.car.OnRaceCar;
 import racingcar.domain.system.host.Host;
 import racingcar.domain.system.manager.car.InMemoryCarManager;
 import racingcar.domain.system.manager.car.value.SavedCar;
@@ -17,6 +17,7 @@ import racingcar.domain.system.manager.position.value.SavedCarPosition;
 import racingcar.domain.system.reader.CarNameReader;
 import racingcar.domain.system.reader.TryCountReader;
 import racingcar.initializer.ConsoleGameFactory;
+import racingcar.initializer.GameFactory;
 
 class RacingGameTest {
 
@@ -26,6 +27,10 @@ class RacingGameTest {
     private static final int CAR_A_POSITION = 3;
     private static final int CAR_B_POSITION = 2;
     private static final int CAR_C_POSITION = 1;
+
+    private static final int COWINNER_CAR_A_POSITION = 3;
+    private static final int COWINNER_CAR_B_POSITION = COWINNER_CAR_A_POSITION;
+    private static final int COWINNER_CAR_C_POSITION = 1;
     private static final Long MOVE = 5L;
     private static final Long STOP = 1L;
 
@@ -33,8 +38,16 @@ class RacingGameTest {
      * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(2), C(1)
      */
     private static final Long[] MOVE_DATA = {MOVE, STOP, STOP, MOVE, MOVE, STOP, MOVE, MOVE, MOVE};
+
+    /**
+     * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(3), C(1)
+     */
+    private static final Long[] COWINNER_MOVE_DATA = {MOVE, MOVE, STOP, MOVE, MOVE, STOP, MOVE,
+        MOVE, MOVE};
     private static final int[] EXPECTED_POSITIONS = {CAR_A_POSITION, CAR_B_POSITION,
         CAR_C_POSITION};
+    private static final int[] COWINNER_EXPECTED_POSITIONS = {COWINNER_CAR_A_POSITION,
+        COWINNER_CAR_B_POSITION, COWINNER_CAR_C_POSITION};
 
     @Test
     @DisplayName("게임을 시작한다.")
@@ -42,8 +55,8 @@ class RacingGameTest {
         // given
         TestCarNameReader carNameReader = new TestCarNameReader();
         TestTryCountReader tryCountReader = new TestTryCountReader();
-        TestHost host = new TestHost();
-        ConsoleGameFactory factory = new ConsoleGameFactory();
+        Host host = new TestHost();
+        GameFactory factory = new ConsoleGameFactory();
         InMemoryCarManager carManager = new InMemoryCarManager();
         CarPositionManager carPositionManager = new InMemoryCarPositionManager();
 
@@ -59,6 +72,32 @@ class RacingGameTest {
         int idx = 0;
         for (SavedCarPosition position : positions) {
             assertEquals(EXPECTED_POSITIONS[idx++], position.getPosition());
+        }
+    }
+
+    @Test
+    @DisplayName("공동 우승자가 있는 게임을 시작한다.")
+    void coWinnerStart() {
+        // given
+        TestCarNameReader carNameReader = new TestCarNameReader();
+        TestTryCountReader tryCountReader = new TestTryCountReader();
+        Host host = new CoWinnerTestHost();
+        GameFactory factory = new ConsoleGameFactory();
+        InMemoryCarManager carManager = new InMemoryCarManager();
+        CarPositionManager carPositionManager = new InMemoryCarPositionManager();
+
+        Game game = new RacingGame(carNameReader, tryCountReader, factory.getViewer(), host,
+            carManager, carPositionManager);
+
+        // when
+        game.start();
+
+        // then
+        List<SavedCar> cars = carManager.findAll();
+        List<SavedCarPosition> positions = carPositionManager.findAll(cars);
+        int idx = 0;
+        for (SavedCarPosition position : positions) {
+            assertEquals(COWINNER_EXPECTED_POSITIONS[idx++], position.getPosition());
         }
     }
 
@@ -90,6 +129,21 @@ class RacingGameTest {
         @Override
         protected MoveSignal getSignal() {
             return MoveSignal.of(MOVE_DATA[idx++]);
+        }
+    }
+
+    private static class CoWinnerTestHost extends Host {
+
+        private static int idx = 0;
+
+        @Override
+        public OnRaceCar signal(MovingCar car) {
+            return car.move(getSignal());
+        }
+
+        @Override
+        protected MoveSignal getSignal() {
+            return MoveSignal.of(COWINNER_MOVE_DATA[idx++]);
         }
     }
 }
