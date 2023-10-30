d9096d345a206ce47a242dda7cf6ecd24e4b18ca
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 14:45:53 +0900

    feat: 레이싱 게임 추가

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class RacingGame {
+
+    private final Cars cars;
+    private final Lap lap;
+
+    RacingGame(Cars cars, Lap lap) {
+        this.cars = cars;
+        this.lap = lap;
+    }
+
+    public void play() {
+        GameIO.printResultMessage();
+
+        int currentLapSize = 1;
+        Lap currentLap = new Lap(currentLapSize);
+        while (!lap.isFinish(currentLap)) {
+            cars.race();
+            cars.printRaceStatus();
+            currentLap = new Lap(++currentLapSize);
+        }
+
+        GameIO.printWinners(cars.getWinner());
+    }
+
+}
+
