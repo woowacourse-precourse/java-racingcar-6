0ca8af42547bb2fd05eefe6bcb62303ef81184e6
belljun3395 <195850@jnu.ac.kr>
2023-10-30 12:03:13 +0900

    refactor(domain/system): Carkey를 키로 사용하여 저장관리 하도록 수정 및 TreeMap을 구현채로 사용하여 정렬된 상태로 관리

--- src/main/java/racingcar/domain/system/manager/car/InMemoryCarManager.java
+++ src/main/java/racingcar/domain/system/manager/car/InMemoryCarManager.java
@@ -1,11 +1,13 @@
 package racingcar.domain.system.manager.car;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.TreeMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import racingcar.domain.core.car.Car;
 import racingcar.domain.core.car.CarName;
 import racingcar.exceptions.runtime.DuplicatedCarNameException;
@@ -13,44 +15,47 @@ import racingcar.exceptions.runtime.NotFoundSourceException;
 
 public class InMemoryCarManager implements CarManager {
 
-    private static final Comparator<? super CarName> keyComparator =
-        Comparator.comparing(CarName::getName);
-    private final Map<CarName, Car> cars = new TreeMap<>(keyComparator);
+    /**
+     * Carkey를 기준으로 오름차순 정렬
+     */
+    private final Map<CarKey, SavedCar> cars = new TreeMap<>();
 
 
     public SavedCar save(Car car) {
         validate(car);
-        cars.put(car.getCarName(), car);
-        return new SavedCar(car);
+        CarKey key = CarKey.of(car.getCarName());
+        SavedCar savedCar = new SavedCar(car, key);
+        cars.put(key, savedCar);
+        return savedCar;
     }
 
     private void validate(Car car) {
-        Collection<Car> sources = cars.values();
+        Collection<SavedCar> sources = cars.values();
         validateDuplicateCar(sources, car);
     }
 
-    private void validateDuplicateCar(Collection<Car> sources, Car car) {
-        sources.stream()
-            .filter(source -> source.getName().equals(car.getName()))
-            .findAny()
+    private void validateDuplicateCar(Collection<SavedCar> sources, Car car) {
+        sources.stream().filter(source -> source.getName().equals(car.getName())).findAny()
             .ifPresent(c -> {
                 throw new DuplicatedCarNameException();
             });
     }
 
     public List<SavedCar> saveAll(List<Car> cars) {
-        return cars.stream().map(this::save).toList();
+        return cars.stream().map(this::save).sorted(Comparator.comparing(SavedCar::getKey))
+            .collect(Collectors.toList());
     }
 
     public SavedCar findByName(CarName carName) {
-        Car car = cars.get(carName);
-        if (Objects.isNull(car)) {
-            throw new NotFoundSourceException();
-        }
-        return new SavedCar(car);
+        return cars.values().stream().filter(carNameFilter(carName)).findAny()
+            .orElseThrow(NotFoundSourceException::new);
+    }
+
+    private Predicate<SavedCar> carNameFilter(CarName carName) {
+        return car -> car.getName().equals(carName.getName());
     }
 
     public List<SavedCar> findAll() {
-        return List.copyOf(cars.values().stream().map(SavedCar::new).toList());
+        return cars.values().stream().toList();
     }
 }
