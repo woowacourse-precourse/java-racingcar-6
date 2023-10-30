24eb543cd84c4e7a6b7c8a28a821a6019df15cf2
JAY-Winter <rubat0@naver.com>
2023-10-30 16:20:39 +0900

    test: 경기 횟수 입력

--- src/test/java/round/RoundTest.java
+++ src/test/java/round/RoundTest.java
@@ -9,30 +9,28 @@ import racingcar.validator.RoundValidator;
 
 public class RoundTest {
 
-    private RoundValidator roundValidator;
-
-    @BeforeEach
-    void setUp() {
-        this.roundValidator = new RoundValidator();
-    }
-
     @Test
     void 경기_횟수는_1이상_이어야한다() {
 
         String acceptedRound = "1";
         String unAcceptedRound1 = "0";
         String unAcceptedRound2 = "-1";
+        String unAcceptedRound3 = "가나다";
 
         assertDoesNotThrow(() ->
-            roundValidator.validateRounds(acceptedRound)
+            RoundValidator.validateRounds(acceptedRound)
         );
 
         assertThrows(IllegalArgumentException.class,
-            () -> roundValidator.validateRounds(unAcceptedRound1)
+            () -> RoundValidator.validateRounds(unAcceptedRound1)
         );
 
         assertThrows(IllegalArgumentException.class,
-            () -> roundValidator.validateRounds(unAcceptedRound2)
+            () -> RoundValidator.validateRounds(unAcceptedRound2)
+        );
+
+        assertThrows(IllegalArgumentException.class,
+            () -> RoundValidator.validateRounds(unAcceptedRound3)
         );
     }
 
@@ -41,7 +39,7 @@ public class RoundTest {
         String unAcceptedRound = "Error";
 
         assertThrows(IllegalArgumentException.class,
-            () -> roundValidator.validateRounds(unAcceptedRound)
+            () -> RoundValidator.validateRounds(unAcceptedRound)
         );
     }
 }
