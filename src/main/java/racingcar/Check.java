cabc93408131c9d1c83500e0121875e52806f632
SeungGwa123 <skwan123@naver.com>
2023-10-31 03:29:08 +0900

    Feat: update checkCarName
    컴마 갯수를 세어주는 함수, 이름 길이 측정해주는 함수를 추가하여 checkCarName을 완전하게 작성

--- src/main/java/racingcar/Check.java
+++ src/main/java/racingcar/Check.java
@@ -8,9 +8,10 @@ public class Check {
         if(noBlank.length()!=carlist.trim().length()) result = false;
 
         String[] car_list = noBlank.split(",");
-        // 컴마 갯수 세어주는 함수(컴마 한개 있으면 이름이 두개있어야 하기 때문입니다)
+        int comma = countComma(noBlank);
         // 컴마 갯수 +1 == car_list.length() 인지 체크
-        // car_list 넣어주면 이름길이 0<이름<=5인지 체크해주는 함수
+        if(comma+1!=car_list.length) result = false;
+        if(!measureNameLength(car_list)) result = false;
         return result;
     }
     public boolean measureNameLength(String[] car_list){
