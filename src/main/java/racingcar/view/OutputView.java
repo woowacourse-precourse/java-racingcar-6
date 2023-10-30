d4df37ddcd72e7c1b4e625f339695f28e7003795
jiwoo <zbqmgldjfh@gmail.com>
2023-10-30 13:24:01 +0900

    feat(OutputView): OutputView 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void writeInlineMessage(String message) {
+        System.out.print(message);
+    }
+    public void writeNewlineMessage(String message) {
+        System.out.println(message);
+    }
+}
