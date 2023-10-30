11b031ae839752b6d2eb660803af0a1c687b08cb
zzawang <hobin0116@naver.com>
2023-10-30 19:44:06 +0900

    feat: 게임 시작을 위한 기능 추가
    - 사용자가 입력한 자동차들의 이름으로 자동차 객체들을 생성하여 리스트로 저장한다.
    - 저장된 자동차 리스트와 시도 횟수로 RacingGame 객체를 생성한다.
    - RacingGame의 gameStart 메소드를 통해 레이싱 게임을 시작한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,5 +14,12 @@ public class Application {
         inputView = new InputView(userInputException);
         carNames = inputView.getCarName();
         attemptCount = inputView.getAttemptCount();
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+        RacingGame racingGame = new RacingGame(cars, attemptCount);
+        racingGame.gameStart();
     }
 }
