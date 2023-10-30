ca09816eb501b3d50ed73bfb83d7cd6654b360be
Jiwon Lee <jiwon718@users.noreply.github.com>
2023-10-30 12:38:35 +0900

    feat: 자동차 이름 지정하는 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car {
+
+    private static String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
