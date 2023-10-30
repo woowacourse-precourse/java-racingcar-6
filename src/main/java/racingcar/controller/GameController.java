dc285d3acd4d87727f072f186c30b3baf1b10320
minsub <chrkb1569@naver.com>
2023-10-30 21:46:08 +0900

    feat : 게임이 시작되는 경우, 자동차 이름을 입력 받기 위한 메세지가 출력된다.

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,9 @@
+package racingcar.controller;
+
+// 사용자로부터 값을 입력받고 결과를 출력하기 위한 클래스
+public class GameController {
+    public void startGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        // read carNameArr
+    }
+}
