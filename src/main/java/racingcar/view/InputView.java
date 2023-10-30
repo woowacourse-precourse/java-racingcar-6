3caf0add705f848c4eef85bbd8db40d1184147f4
Yeriimii <seojs0511@naver.com>
2023-10-30 16:27:34 +0900

    feat(InputView): 자동차 이름 5글자 검증 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,8 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private enum InputMessage {
-        REQUEST_INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        REQUEST_INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        EXCEED_NAME_SIZE_LIMIT("자동차 이름은 5글자 이하여야 합니다.");
 
         private final String message;
 
@@ -19,15 +20,22 @@ public class InputView {
     }
 
     private static final String SEPARATOR = ",";
-
+    private static final int NAME_SIZE_LIMIT = 5;
 
     public List<String> inputName() {
         System.out.println(InputMessage.REQUEST_INPUT_CAR_NAME.message);
         String input = Console.readLine();
         List<String> names = Arrays.asList(input.split(SEPARATOR));
+        validateFiveCharacter(names);
 
         return names.stream()
                 .map(String::trim)
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    public void validateFiveCharacter(List<String> names) {
+        if (names.stream().anyMatch(name -> name.length() > NAME_SIZE_LIMIT)) {
+            throw new IllegalArgumentException(InputMessage.EXCEED_NAME_SIZE_LIMIT.message);
+        }
     }
 }
