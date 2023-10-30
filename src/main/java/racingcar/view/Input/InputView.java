a1d2a3d0cd94f3321089fdac1a43c0d253e70401
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-30 23:22:52 +0900

    🚀refector(view / InputView) : return type 변경

--- src/main/java/racingcar/view/Input/InputView.java
+++ src/main/java/racingcar/view/Input/InputView.java
@@ -1,8 +1,11 @@
 package racingcar.view.Input;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
 import racingcar.view.Input.InputParser;
 import racingcar.view.Input.InputValidator;
+
+import java.util.List;
 
 /* 자동차 입력 받는 클래스
 *  입력만 받기
@@ -10,7 +13,6 @@ import racingcar.view.Input.InputValidator;
 public class InputView {
     private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"; // 삼수
     private final String  INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?"; //  삼수
-    public static int trialNumber; // 인스턴스 변수로 필요한지 고민
 
     private InputParser inputParser;
     private InputValidator inputValidator;
@@ -20,11 +22,12 @@ public class InputView {
         this.inputValidator = inputValidator;
     }
     // 자동차 입력 받기
-    public void inputCarName() {
+    public List<Car> inputCarName() {
         System.out.println(INPUT_CAR_NAME);
         String input = Console.readLine();
         inputValidator.validateCarString(input.replace(" ",""));
-        inputParser.parseCarString(input);
+
+        return inputParser.parseCarString(input);
     }
 
     // 시도 횟수 입력 받기
