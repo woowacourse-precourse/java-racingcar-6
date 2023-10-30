fd06117b8a8cd0ee5d9de9d9dbd83bddc8b226ae
lss0809 <ippnss@gmail.com>
2023-10-30 20:22:19 +0900

    refactor(InputHandler): 인풋값들을 리턴하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.domain.InputHandler;
+import racingcar.domain.TrialRepeat;
+
+import java.util.List;
+
 public class Application {
+    public static List<String> names;
+    public static int trialToDo;
+    public static int trialCurrent = 0;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler input = new InputHandler();
+        names = input.inputName();
+        trialToDo = input.inputTrial();
     }
 }
--- src/main/java/racingcar/domain/InputHandler.java
+++ src/main/java/racingcar/domain/InputHandler.java
@@ -1,2 +1,25 @@
-package racingcar;public class InputHandler {
+package racingcar.domain;
+
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    final String NAME_INPUT_TEXT =
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    final String TRIAL_INPUT_TEXT = "시도할 회수는 몇회인가요?";
+
+    public InputHandler() {
+    }
+
+    public List<String> inputName() {
+        System.out.println(NAME_INPUT_TEXT);
+        String namesString = Console.readLine();
+        return List.of(namesString.split(","));
+    }
+
+    public int inputTrial() {
+        System.out.println(TRIAL_INPUT_TEXT);
+        String trialString = Console.readLine();
+        return Integer.parseInt(trialString);
+    }
 }
