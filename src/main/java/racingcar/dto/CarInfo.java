1dc951cab6698b24a878973855320be077315893
kunsanglee <gosmdochee2@gmail.com>
2023-10-30 23:45:01 +0900

    refactor(CarInfo): 자동차의 이름을 가져올 때 CarName을 가져오도록 수정

--- src/main/java/racingcar/dto/CarInfo.java
+++ src/main/java/racingcar/dto/CarInfo.java
@@ -6,7 +6,7 @@ public record CarInfo(String name, int position) {
     public static final String POSITION_PROGRESS = "-";
 
     public static CarInfo of(Car car) {
-        return new CarInfo(car.getName(), car.getPosition());
+        return new CarInfo(car.getCarName(), car.getPosition());
     }
 
     public String convertPositionValue() {
