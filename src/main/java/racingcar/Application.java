ed7d7378a49cd0338899b9bff50ec58b3df5c9f4
jjaegii <hn06038@gmail.com>
2023-10-30 22:50:02 +0900

    feat: Apllication 클래스 내 main 메소드 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager();
+        OutputManager outputManager = new OutputManager();
+
+        List<String> carNames = inputManager.getCarNames();
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        int rounds = inputManager.getTryCount();
+
+        RaceManager raceManager = new RaceManager(cars, rounds);
+        raceManager.race();
+
+        outputManager.printWinners(raceManager.getWinners());
     }
 }
