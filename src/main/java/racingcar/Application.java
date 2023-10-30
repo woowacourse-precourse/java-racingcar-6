9eb83e3b07133d84fbac8b377998e737bff8daac
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-30 23:12:36 +0900

    refactor : 생성자 주입을 통한 main 메소드 리펙토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,12 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Player player = new Player();
+        GameService gameService = new GameService();
+
+        GameController gameController = new GameController(inputView, outputView, player, gameService);
         gameController.play();
     }
 }
