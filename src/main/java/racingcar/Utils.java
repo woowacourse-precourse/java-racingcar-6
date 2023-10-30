3ef156ffffd17877783c25fa1dac9e17e387f2e0
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-30 20:16:37 +0900

    2023.10.25_test : Utils dashRepeat 메서드 생성

--- src/main/java/racingcar/Utils.java
+++ src/main/java/racingcar/Utils.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.IntStream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -18,5 +19,11 @@ public class Utils {
 
     public static int getRandomNumber() {
         return Randoms.pickNumberInRange(Constants.MIN_NUM, Constants.MAX_NUM);
+    }
+
+    public static String dashRepeat(int num) {
+        StringBuilder dashes = new StringBuilder();
+        IntStream.range(0, num).forEach(i -> dashes.append("-"));
+        return dashes.toString();
     }
 }
