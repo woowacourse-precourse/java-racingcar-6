393cb5f06394b1f5c3410113f4abc86cc7f7ac0e
mangwonKing <aud1035@naver.com>
2023-10-30 19:59:18 +0900

    refactor(racingcar): main의 depth 3인 부분 gameStart, roundStart 메소드로 구분

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -27,19 +27,25 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         gameCount = user.inputGameCount();
         System.out.println("\n실행 결과");
-        while (gameCount > 0){
-
-            for (int index = 0; index < carList.size();index++){
-                int randomNumber = numberGenerator.createRandomNumber();
-                if(user.decideMove(randomNumber)){
-                    car.moveForward(carList.get(index),userInfomation);
-                }
-                System.out.println(carList.get(index) + " : "+ userInfomation.get(carList.get(index)));
-            }
-            System.out.println("");
-            gameCount--;
-        }
+        gameStart(gameCount);
         winners = judgement.compareDistance(userInfomation,carList);
         referee.printWinner(winners);
+    }
+    static void gameStart(int gameCount){
+        while (gameCount > 0){
+            roundStart();
+            gameCount--;
+        }
+    }
+    static void roundStart(){
+        for (int index = 0; index < carList.size();index++){
+            int randomNumber = numberGenerator.createRandomNumber();
+            if(user.decideMove(randomNumber)){
+                car.moveForward(carList.get(index),userInfomation);
+            }
+            System.out.println(carList.get(index) + " : "+ userInfomation.get(carList.get(index)));
+        }
+        System.out.println("");
+
     }
 }
