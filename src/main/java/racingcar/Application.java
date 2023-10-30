b4572e369b41ce3f78d7508dc1b134b92da31745
mango <esudatcom@gmail.com>
2023-10-30 19:11:51 +0900

    feat(Application): 최종 우승자 찾는 메소드 생성 후 합치기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,29 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
+        String[] carNames = InputView.getCarNames();
+        int attemptCount = InputView.getAttemptCount();
 
+        System.out.println();
+        System.out.println("실행 결과");
+
+        Race race = new Race(carNames);
+        for (int i = 0; i < attemptCount; i++) {
+            race.moveCars();
+            ResultView.printRace(race.getCars());
+        }
+        List<Car> winners = findWinners(race.getCars());
+        ResultView.printWinners(winners);
+    }
+
+    public static List<Car> findWinners(List<Car> cars){
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
     }
 }
