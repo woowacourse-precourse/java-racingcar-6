ca803bfe5615650a3e006381c39ac3745daace31
tam31 <wxogud3377@naver.com>
2023-10-30 12:19:31 +0900

    Feat(Winner) : 최대이동 거리구하기 기능 구현 Docs : 최대이동 거리 구하기 완료체크

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -1,4 +1,26 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Winner {
+
+    private List<String> winnerName;
+
+    public List<String> getWinnerName() {
+        return winnerName;
+    }
+
+    public void findWinnerName(List<Car> cars) {
+        winnerName = new ArrayList<>();
+        int maxForwardDistance = maxDistance(cars);
+    }
+
+    private int maxDistance(List<Car> cars){
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(car.getPosition(),max);
+        }
+        return max;
+    }
 }
