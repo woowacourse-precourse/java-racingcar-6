1b75331d57308a359c7f051e29ac81f5ad523ce7
seokmyungham <hellotjraud@gmail.com>
2023-10-31 07:07:03 +0900

    feat(GameView): printRaceResult 메서드 추가
    printRaceResult: "실행 결과" 를 출력하는 메서드입니다.

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class GameView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RACE_RESULT = "실행 결과";
 
     public void printCarNamesMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -22,5 +23,9 @@ public class GameView {
     public String inputAttemptCount() {
         String attemptCount = Console.readLine();
         return attemptCount;
+    }
+
+    public void printRaceResult() {
+        System.out.println(RACE_RESULT);
     }
 }
