9911fcd80fc617fb29a70c2fc521466c4ccd4d49
Hanjaemo <hjm2530@gmail.com>
2023-10-31 00:45:03 +0900

    refactor(NumberOfAttempts): 매직 넘버 상수화

--- src/main/java/racingcar/domain/NumberOfAttempts.java
+++ src/main/java/racingcar/domain/NumberOfAttempts.java
@@ -2,6 +2,10 @@ package racingcar.domain;
 
 public class NumberOfAttempts {
 
+    private static final int MIN_NUMBER_OF_ATTEMPTS = 1;
+    private static final int MAX_NUMBER_OF_ATTEMPTS = 100;
+    private static final int ZERO = 0;
+
     private int numberOfAttempts;
 
     public NumberOfAttempts(int numberOfAttempts) {
@@ -10,13 +14,18 @@ public class NumberOfAttempts {
     }
 
     private void validateNumberOfAttempts(int numberOfAttempts) {
-        if (!(1 <= numberOfAttempts && numberOfAttempts <= 100)) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1 이상 100 이하만 가능합니다.");
+        if (isOutOfRange(numberOfAttempts)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 %d 이상 %d 이하만 가능합니다."
+                    .formatted(MIN_NUMBER_OF_ATTEMPTS, MAX_NUMBER_OF_ATTEMPTS));
         }
     }
 
+    private boolean isOutOfRange(int numberOfAttempts) {
+        return !(MIN_NUMBER_OF_ATTEMPTS <= numberOfAttempts && numberOfAttempts <= MAX_NUMBER_OF_ATTEMPTS);
+    }
+
     public boolean hasRemainingAttempts() {
-        return numberOfAttempts > 0;
+        return numberOfAttempts > ZERO;
     }
 
     public void decreaseNumberOfAttempts() {
