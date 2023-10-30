61b8a8a14079ce4fc74d085794fab60159628531
Jiwon Lee <jiwon718@users.noreply.github.com>
2023-10-30 19:47:06 +0900

    feat: 경주를 진행하고 각 차수마다 진행 결과 출력하는 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -8,22 +8,27 @@ public class Game {
     public static List<Car> cars = new ArrayList<>();
     public static Player player = new Player();
 
-    public void play() {
+    public static void play() {
         start();
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < player.getRaceCount(); i++) {
+            race();
+            printMidTermResult();
+        }
     }
 
-    public void start() {
+    public static void start() {
         askCarNames();
         nameCars();
         askRaceCount();
     }
 
-    public void askCarNames() {
+    public static void askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         player.inputCarNames();
     }
 
-    public void nameCars() {
+    public static void nameCars() {
         List<String> carNames = player.getCarNames();
 
         for (String carName : carNames) {
@@ -31,7 +36,21 @@ public class Game {
         }
     }
 
-    public void askRaceCount() {
+    public static void askRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
         player.inputRaceCount();
+    }
+
+    public static void race() {
+        for (Car car : cars) {
+            car.moveForward();
+        }
+    }
+
+    public static void printMidTermResult() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getMoveForwardCount()));
+        }
+        System.out.println();
     }
 }
