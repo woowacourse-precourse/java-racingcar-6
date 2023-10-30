1177d93a7bc5fecd47d386d0395fe0df2cb52b05
taehyeon <kth0373@naver.com>
2023-10-31 04:37:19 +0900

    test(racingcar): 자동차 이름이 중복일 경우 예외 발생 테스트

--- src/test/java/racingcar/domain/InputValidationTest.java
+++ src/test/java/racingcar/domain/InputValidationTest.java
@@ -2,6 +2,9 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,5 +16,16 @@ class InputValidationTest {
         assertThatThrownBy(() -> InputValidation.checkCharacter(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("잘못된 입력입니다.");
+    }
+
+    @Test
+    void 자동차_이름이_중복일_경우_예외_발생() {
+        List<String> carNames = new ArrayList<>();
+        carNames.add("pobi");
+        carNames.add("pobi");
+
+        assertThatThrownBy(() -> InputValidation.checkOverlap(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 이름이 있습니다.");
     }
 }
