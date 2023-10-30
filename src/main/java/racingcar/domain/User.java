695247c1ef6d748ba607916095da81704b923f14
Dowon <3120kang@gmail.com>
2023-10-30 21:51:53 +0900

    Refactor(User): 코드 정렬

--- src/main/java/racingcar/domain/User.java
+++ src/main/java/racingcar/domain/User.java
@@ -6,18 +6,18 @@ import java.util.List;
 
 public class User {
 
-    private static final int MAX_NAME_SIZE=5;
-    private static final int MIN_CAR_NUMBER=2;
+    private static final int MAX_NAME_SIZE = 5;
+    private static final int MIN_CAR_NUMBER = 2;
 
     public List<Car> nameForCar(String names) {
-        List<String> carsName=separateNames(names);
-        List<Car> cars=new ArrayList<>();
-        carsName.stream().forEach(name->cars.add(new Car(name.trim())));
+        List<String> carsName = separateNames(names);
+        List<Car> cars = new ArrayList<>();
+        carsName.stream().forEach(name -> cars.add(new Car(name.trim())));
         return cars;
     }
 
     public boolean validateName(String name) {
-        return name.length()<=MAX_NAME_SIZE;
+        return name.length() <= MAX_NAME_SIZE;
     }
 
     public List<String> separateNames(String names) {
@@ -27,8 +27,8 @@ public class User {
         return cars;
     }
 
-    public void validateGame(String[] cars){
-        if(cars.length<MIN_CAR_NUMBER){
+    public void validateGame(String[] cars) {
+        if (cars.length < MIN_CAR_NUMBER) {
             throw new IllegalArgumentException("게임을 진행하기 위한 자동차의 개수는 최소 2대 입니다.");
         }
     }
