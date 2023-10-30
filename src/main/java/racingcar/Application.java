c248169e78e76304fbaf8497541cd21333a71c50
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-10-31 00:59:06 +0900

    Car 객체 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,44 +1,9 @@
 package racingcar;
 
+import racingcar.model.Car;
 import java.util.ArrayList;
 import java.util.List;
-import camp.nextstep.edu.missionutils.Randoms;
 import static camp.nextstep.edu.missionutils.Console.readLine;
-
-class Car {
-    private String name;
-    private int forwardCount;
-
-    public Car(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
-        }
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getForwardCount() {
-        return this.forwardCount;
-    }
-
-    void move() {
-        int num = Randoms.pickNumberInRange(0,9);
-        if (num >= 4) {
-            forwardCount++;
-        }
-    }
-
-    void currentLocation() {
-        System.out.print(name + " : ");
-        for(int i = 0; i < forwardCount; i++){
-            System.out.print("-");
-        }
-        System.out.println();
-    }
-}
 
 class Game {
     private List<Car> cars = new ArrayList<>();
---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int forwardCount;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getForwardCount() {
+        return this.forwardCount;
+    }
+
+    public void move() {
+        int num = Randoms.pickNumberInRange(0,9);
+        if (num >= 4) {
+            forwardCount++;
+        }
+    }
+
+    public void currentLocation() {
+        System.out.print(name + " : ");
+        for(int i = 0; i < forwardCount; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
