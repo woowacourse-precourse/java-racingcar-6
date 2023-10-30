ace84ae1e4dc43e28ecb31e61aa87dc1068e1e51
tfer2442 <tfer2442@gmail.com>
2023-10-30 19:56:17 +0900

    feat(Constants): 상수값을 담는 클래스 생성

---
+++ src/main/java/racingcar/Constants.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Constants {
+    public static final int DISTANCE = 1;
+    public static final String SEPARATOR = ",";
+    public static final int MIN_CAR_NAME_LEN = 1;
+    public static final int MAX_CAR_NAME_LEN = 5;
+}
