de390dc8107e92415f3e5627f94dad3b22b23b3b
gundorit <packdev937@gmail.com>
2023-10-30 19:02:34 +0900

    test (Car) : 자동차 전진 기능을 테스트 한다

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,24 +1,36 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Car 클래스")
 class CarTest {
 
+    private Car car;
+
+    @BeforeEach
+    void setup() {
+        car = new Car("pobi");
+    }
+
     @Test
-    void 이름으로_자동차를_생성할_수_있다() {
-        // given
-        String name = "pobi";
+    void 이름으로_자동차를_생성() {
+        assertThat(car.getName()).isEqualTo("pobi");
+    }
 
-        // when
-        Car car = new Car(name);
+    @Test
+    void 무작위_값이_4_이상이면_전진() {
+        car.moveForward(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 
-        // then
-        assertThat(car.getName()).isEqualTo(name);
+    @Test
+    void 무작위_값이_4_미만이면_정지() {
+        car.moveForward(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+
     }
 }
