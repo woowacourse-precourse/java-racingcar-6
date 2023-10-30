5a2a9d3dbf0bcc850d52819085c118afd419df4a
sujnkim <blackout3210@naver.com>
2023-10-31 01:40:32 +0900

    fix(test): Car에서 삭제한 메서드에 해당하는 test 삭제

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -16,13 +16,6 @@ class CarTest {
     }
 
     @Test
-    void Car_이름길이5이상이면_인스턴스생성실패() {
-        String wrongName = "longerthan5";
-        assertThatThrownBy(() ->
-                new Car(wrongName)).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
     void move_기준값이상일때_1칸전진() {
         car.move(4);
         assertThat(car.getLocation()).isEqualTo(1);
