f6538905dd48af52e9858a7911f4c85984aadd11
gyu_hyeok <jjookki33@gmail.com>
2023-10-31 02:01:56 +0900

    feat:게임 실행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println(START_INPUT_MESSAGE);
-        String racingCar = Console.readLine();
-
-        System.out.println(INPUT_TRY_NUMBER);
-        String gameNumber = Console.readLine();
+        GameController gameController = new GameController();
+        gameController.run();
     }
-
-    private static final String START_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_TRY_NUMBER = "시도할 횟수는 몇회인가요?";
-
-
 }
