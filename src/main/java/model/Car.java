1a654227e50b11ee2ff338ea3ebad13a324e2219
nayeon Lee <skdus4190@gmail.com>
2023-10-31 04:58:20 +0900

    Feat: Car 객체 추가
    Car.java에 [자동차 이름, 전진 횟수] 를 가지는 Car 객체 정의

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Car {
+
+    private String name;
+    private int go;
+
+    public Car(String name) {
+
+        this.name = name;
+        this.go = 0;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGo() {
+        return go;
+    }
+
+}
