619180da85ee1846f229de308d666f77a4be61b7
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:26:12 +0900

    [feat] Add InputException class checkExistCar method

--- src/main/java/racingcar/controller/InputException.java
+++ src/main/java/racingcar/controller/InputException.java
@@ -2,6 +2,9 @@ package racingcar.controller;
 
 import static racingcar.utility.Constants.ONLYINT;
 
+import racingcar.model.Car;
+import racingcar.model.CarStorage;
+
 public class InputException {
     private InputException() {}
     public static void checkNameLength(String newCarName) { // 이름 길이 5자 초과 예외처리
@@ -12,6 +15,15 @@ public class InputException {
     public static void checkOnlyInt(String newCarName) { // 숫자로만 이루어진 이름 예외처리
         if (newCarName.matches(ONLYINT)) {
             throw new IllegalArgumentException();
+        }
+    }
+    public static void checkExistCar(String newCarName) { // 같은 이름의 자동차 사용 예외처리
+        String carName;
+        for (Car car : CarStorage.cars()) {
+            carName = car.name();
+            if (newCarName.equals(carName)) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
