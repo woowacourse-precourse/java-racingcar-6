5e17fc9f5138babeaea60e0eb753889ea0d45781
3juhwan <13selfesteem91@naver.com>
2023-10-30 10:58:35 +0900

    feat(application): 예외 처리(범위 밖 길이의 사용자 이름이 입력되는 경우)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,18 +14,15 @@ public class Application {
     public static List<String> askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Arrays.stream(readLine().split(","))
-                .map(carName -> {
-                    if (isValidCarName(carName)) {
-                        return carName;
-                    } else {
-                        throw new IllegalArgumentException();
-                    }
-                })
+                .map(Application::isValidCarName)
                 .toList();
     }
 
-    public static Boolean isValidCarName(String carName) {
-        return null;
+    public static String isValidCarName(String carName) {
+        if (carName.length() < 1 || carName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        return carName;
     }
 
     public static Integer askAttemptNumber() {
