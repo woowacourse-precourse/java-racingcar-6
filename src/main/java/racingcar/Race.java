9e1b173ac2752c8204db300bfec9349c36d84a6b
BH kang <rkdqudgjs1127@gmail.com>
2023-10-30 17:31:23 +0900

    feat: 사용자에게 입력받아 자동차 생성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,23 @@
 package racingcar;
 
+import racingcar.car.Car;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Race {
 
+    private List<Car> allCars = new ArrayList<>();
+
+    private void inputCars(){
+
+       String s = Console.readLine();
+       String[] cars = s.split(",");
+
+        for (String carName : cars) {
+            Car car = new Car(carName,0);
+            allCars.add(car);
+        }
+    }
 }
--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -3,4 +3,9 @@ package racingcar.car;
 public class Car {
     public String carName;
     public int distance;
+
+    public Car(String carName, int distance) {
+        this.carName = carName;
+        this.distance = distance;
+    }
 }
