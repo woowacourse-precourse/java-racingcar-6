bfe18415123099a4aedb08f42b933c6a59beca2d
Donggeon Moon <nullsafer@naver.com>
2023-10-30 18:49:23 +0900

    feat: 자동차 경주 게임 객체

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,88 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.NoSuchElementException;
+import racingcar.enums.Message;
+import racingcar.enums.Symbol;
+
+public class RacingGame {
+    private final RacingCars racingCars;
+    private final int totalRound;
+    private int currentRound;
+    private RacingCars winners;
+
+    private RacingGame(RacingCars racingCars, int totalRound) {
+        this.racingCars = racingCars;
+        this.totalRound = totalRound;
+    }
+
+    public static RacingGame of(String inputCarNames, String inputTotalRound) {
+        return new RacingGame(
+                RacingCars.from(inputCarNames),
+                Integer.parseInt(inputTotalRound)
+        );
+    }
+
+    public boolean isNotFinished() {
+        return currentRound < totalRound;
+    }
+
+    public void increaseCurrentRound() {
+        currentRound++;
+    }
+
+    public String getRunResultMessage() {
+        StringBuilder stringBuilder = new StringBuilder();
+        this.racingCars
+                .getRacingCars()
+                .forEach(racingCar -> {
+                    stringBuilder.append(racingCar.getName());
+                    stringBuilder.append(Symbol.KEY_VALUE_SEPARATOR.getValue());
+                    stringBuilder.append(racingCar.getPositionMarkers());
+                    stringBuilder.append("\n");
+                });
+        stringBuilder.append("\n\n\n\n\n");
+
+        return stringBuilder.toString();
+    }
+
+    public String getWinnerMessage() {
+        return Message.FINAL_WINNER.getMessage()
+                + Symbol.KEY_VALUE_SEPARATOR.getValue()
+                + winners.toNames()
+                + "\n";
+    }
+
+
+    public void runRound() {
+        racingCars.getRacingCars()
+                .forEach(this::decideWhetherToMoveForward);
+    }
+
+    private void decideWhetherToMoveForward(RacingCar racingCar) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            racingCar.moveForward();
+        }
+    }
+
+    public void decideWinner() {
+        this.winners = getRacingCarsWithMaximumPosition();
+    }
+
+    private RacingCars getRacingCarsWithMaximumPosition() {
+        List<RacingCar> racingCarsWithMaximumPosition = racingCars.getRacingCars()
+                .stream()
+                .filter(racingCar -> racingCar.getPosition() == getMaximumPosition())
+                .toList();
+        return RacingCars.from(racingCarsWithMaximumPosition);
+    }
+
+    private int getMaximumPosition() {
+        return racingCars.getRacingCars()
+                .stream()
+                .mapToInt(RacingCar::getPosition)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
