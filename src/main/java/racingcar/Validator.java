05b7ee39838c448cab2a3926d493e074f1bd6628
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 13:04:27 +0900

    feat: Validator 입력값이 영문자,한글,쉼표로 구성되지 않으면 예외

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -6,8 +6,8 @@ public class Validator {
         if (input.length() > 10000000) {
             throw new IllegalArgumentException("입력값이 너무 깁니다. 입력값의 길이는 최대 1000만자 입니다.");
         }
-//        if (input.matches("^[가-힣a-zA-Z]+$")) {
-//            throw new IllegalArgumentException("")
-//        }
+        if (input.matches("^[가-힣a-zA-Z,]+$")) {
+            throw new IllegalArgumentException("자동차 이름을 영문자,한글로만 구성하고 쉼표로만 구분해주세요.");
+        }
     }
 }
