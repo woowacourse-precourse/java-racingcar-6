8034bc541fcb4872fea5e0d57ec0760ab60c6379
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 16:12:41 +0900

    feat(Car): 이동거리를 증가시킨다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -9,6 +9,10 @@ public class Car {
         this.distance = 0;
     }
 
+    public void incrementDistance() {
+        this.distance++;
+    }
+
     public String getName() {
         return name;
     }
