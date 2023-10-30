f0cf9686d220f11adbdbac027a150926f2f09d99
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-30 14:37:33 +0900

    feat : ValidationManager class - 잘못된 입력 필터링 기능 추가

---
+++ src/main/java/racingcar/ValidationManager.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class ValidationManager {
+
+    public static void validateCarNames(String[] names) {
+        for (String name : names) {
+            if (name.trim().length() > 5 || name.trim().isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static void validateTrials(String trialInput) {
+        try{
+            int trials = Integer.parseInt(trialInput);
+            if (trials <= 0){
+                throw new IllegalArgumentException();
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+}
