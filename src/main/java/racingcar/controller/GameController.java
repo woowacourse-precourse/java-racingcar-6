b97d06eec6cfbabb81d6673871958a885a23cc08
devholic22 <hyunjoon.tech@gmail.com>
2023-10-30 12:43:06 +0900

    feat: 컨트롤러에 RandomNumber 연결
    컨트롤러에 RandomNumber 연결하여 게임 진행 시 사용할 수 있도록 설정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.model.randomnumber.RandomNumber;
 import racingcar.view.input.InputView;
 import racingcar.view.output.Outputview;
 
@@ -7,10 +8,12 @@ public class GameController {
 
     private final InputView inputView;
     private final Outputview outputView;
+    private final RandomNumber randomNumber;
 
-    public GameController(final InputView inputView, final Outputview outputView) {
+    public GameController(final InputView inputView, final Outputview outputView, final RandomNumber randomNumber) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.randomNumber = randomNumber;
     }
 
     public void play() {
