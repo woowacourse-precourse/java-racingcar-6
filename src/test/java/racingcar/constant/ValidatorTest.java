6f36b05826addca5ae0bea0079cbf04fb8e49163
itsmo1031 <itsmo1031@gmail.com>
2023-10-31 00:52:04 +0900

    feat(constant): 입력 값 유효성 검증을 위한 Validator Enum Class 작성

---
+++ src/test/java/racingcar/constant/ValidatorTest.java
@@ -0,0 +1,27 @@
+package racingcar.constant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+    @Test
+    void 예외처리() {
+        assertAll(
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Validator.NUMBER.validate("-1")),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Validator.NAME.validate("aa bb")),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Validator.NAME.validate("abcdef"))
+        );
+    }
+
+    @Test
+    void 올바른_입력() {
+        assertAll(
+                () -> assertThatCode(() -> Validator.NUMBER.validate("12")).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> Validator.NAME.validate("pobi")).doesNotThrowAnyException()
+        );
+    }
+}
