ce9794b6b37240a2345d8d13758fed7215ce44d2
dunowljj <uuufg3322@gmail.com>
2023-10-31 00:18:21 +0900

    feat : InputView 입력 요구 메시지 출력 기능 구현

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -5,5 +5,7 @@ import racingcar.view.InputView;
 public class RacingCarGame {
 
     public void play() {
+        InputView.printDemandCarNames();
+        InputView.printDemandAttemptCount();
     }
 }
