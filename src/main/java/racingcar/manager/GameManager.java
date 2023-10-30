046b75bd70004a2e032685fa2ba329cf9e8fb730
bitesnail <rkwoal216@gmail.com>
2023-10-30 23:19:42 +0900

    feat: implementation for P-01

--- src/main/java/racingcar/manager/GameManager.java
+++ src/main/java/racingcar/manager/GameManager.java
@@ -1,5 +1,6 @@
 package racingcar.manager;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.utils.Validator;
@@ -22,8 +23,21 @@ public class GameManager {
         return Integer.parseInt(numberOfMoves);
     }
 
+    private void moveCars(List<Car> cars){
+        for(Car car : cars){
+            if(Randoms.pickNumberInRange(0,9) >= 4) {
+                car.move();
+                continue;
+            }
+            car.pause();
+        }
+    }
+
     public void start(){
         List<Car> cars = getCars();
         int numberOfMoves = getNumberOfMoves();
+        for(int i=0; i<numberOfMoves; i++){
+            moveCars(cars);
+        }
     }
 }
