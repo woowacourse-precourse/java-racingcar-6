597d040cea382524701b7b433fe39b68cfcc73ff
naayoung <ask5046a@gmail.com>
2023-10-31 01:31:46 +0900

    refactor: play and finalWinner

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,36 +6,48 @@ import java.util.List;
 import java.util.Objects;
 
 public class Application {
+    // 입력값 받기
+    static InputValue inputValue  = new InputValue();
+    static InputValue inputTime  = new InputValue();
+    static String[] value;
+    static Integer time;
+
+    //랜덤 거리
+    static RandomDistance randomDis = new RandomDistance();
+    static List<Integer> userDis = new ArrayList<>();
+    //최종 우승자 저장
+    static List<String> answer = new ArrayList<>();
+
     public static void main(String[] args) {
-        // 입력값 받기
-        InputValue inputValue  = new InputValue();
-        InputValue inputTime  = new InputValue();
-        //랜덤 거리
-        RandomDistance randomDis = new RandomDistance();
-        List<Integer> userDis = new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] value = inputValue.getInputValue();
+        value = inputValue.getInputValue();
         System.out.println("시도할 회수는 몇회인가요?");
-        Integer time = inputTime.getInputTime();
+        time = inputTime.getInputTime();
         System.out.println("실행 결과");
 
         for (int i=0; i<time; i++){
-            for(int j=0; j<value.length;j++){
-                userDis.add(j,randomDis.getDistance(j));
-                //길이 값을 문자로 변환
-                String dis = "-".repeat(userDis.get(j));
-                System.out.println(value[j] + " :" + dis);
-            }
+            play();
         System.out.println(" ");
         }
-        //최종 우승자 구하기
-        List<String> answer = new ArrayList<>();
+        System.out.println("최종 우승자 : " + finalWinner());
+    }
+    //플레이
+    public static void play(){
+        for(int j=0; j<value.length;j++){
+            userDis.add(j,randomDis.getDistance(j));
+            //길이 값을 문자로 변환
+            String dis = "-".repeat(userDis.get(j));
+            System.out.println(value[j] + " :" + dis);
+        }
+    }
+    //최종 우승자 구하기
+    public static List<String> finalWinner(){
         for (int i=0; i<value.length; i++){
             if (Objects.equals(userDis.get(i), Collections.max(userDis))){
                 answer.add(value[i]);
             }
         }
-        System.out.println("최종 우승자 : " + answer);
+        return answer;
     }
 }
