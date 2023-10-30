5ef0e3d61e2524115142103195f3ac224eb73891
beomsic <qjatjr29@khu.ac.kr>
2023-10-30 11:57:31 +0900

    :sparkles: Feat: Add Class for Output
    - 출력을 위한 view class 추가

---
+++ src/main/java/racingcar/view/OutPutView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class OutPutView {
+
+    private static final String FORWARD_STEP = "-";
+
+    public static void printIOMessage(IOMessage message) {
+        System.out.println(message.getMessage());
+    }
+
+    public static void println() {
+        System.out.println();
+    }
+
+}
