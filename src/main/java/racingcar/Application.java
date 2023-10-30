ad6281b4d557ea65e9910aa1808d6c815f579500
jihun-24k <gi020477@gmail.com>
2023-10-30 20:55:05 +0900

    refactor: 클래스 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,28 +1,8 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        String[] carNames = input.split(",");
-
-        for (String carName : carNames) {
-            validateCarName(carName);
-        }
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        input = Console.readLine();
-        int count = Integer.parseInt(input);
-
-        new RacingGame(count);
-    }
-
-    private static void validateCarName(String carName) {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-        }
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,9 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingGame {
 
     private int count;
-    public RacingGame(int count) {
-        this.count = count;
+
+    public void start() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        for (String carName : carNames) {
+            validateCarName(carName);
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        input = Console.readLine();
+        count = Integer.parseInt(input);
+
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
