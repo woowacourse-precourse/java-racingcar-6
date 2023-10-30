a4ad68d7f492a193161a88a97c56819daab652dc
leorb159 <ldgsocial159@naver.com>
2023-10-31 01:57:29 +0900

    feat(Game): 자동차 이름 입력 처리 적용

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,12 +1,21 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 
 public class Game {
     private Output myOutput;
+    private Input myInput;
+
     public Game(){
         myOutput = new Output();
+        myInput = new Input();
     }
     public void run(){
         myOutput.printEnterNameOfCar();
+        String input = Console.readLine();
+        ArrayList<String> carNameList = myInput.getNameList(input);
+
 
         myOutput.printEnterTryTime();
     } 
