18807fba18a19ee88adc9ea46138cbec43e3a965
jifrozen0110 <answldjs1836@gmail.com>
2023-10-30 14:19:14 +0900

    feat(SystemMessage) : InputValidator 에러 메시지 추가

--- src/main/java/racingcar/common/consts/SystemMessage.java
+++ src/main/java/racingcar/common/consts/SystemMessage.java
@@ -7,4 +7,9 @@ public class SystemMessage {
     public static final String EXECUTION_RESULT_MESSAGE="실행 결과";
 
     public static final String FINAL_WINNER_MESSAGE="최종 우승자 : ";
+
+
+    //에러 메시지
+    public static final String INVALID_INPUT_NUMBER_ERROR="입력한 시도 횟수가 유효하지 않습니다.";
+    public static final String INVALID_INPUT_NAME_ERROR="입략한 이름이 유효하지 않습니다.";
 }
