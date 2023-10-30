2ef26cb502082b74688c8b5417f4d3c1206966e7
seminchoi <choicco89@gmail.com>
2023-10-31 00:09:40 +0900

    refactor(racingcar.RacingCarGameFactoryImpl) : 추가된 인터페이스 메소드 구현

--- src/main/java/racingcar/RacingCarGameFactoryImpl.java
+++ src/main/java/racingcar/RacingCarGameFactoryImpl.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-public class RacingCarGameFactoryImpl implements RacingCarGameFactory{
+public class RacingCarGameFactoryImpl implements RacingCarGameFactory {
     @Override
     public OutputView outputView() {
         return new OutputViewImpl();
@@ -9,6 +9,16 @@ public class RacingCarGameFactoryImpl implements RacingCarGameFactory{
     @Override
     public InputView inputView() {
         return new InputViewImpl();
+    }
+
+    @Override
+    public RacingCarGameMachineValidator racingCarGameMachineValidator() {
+        return new RacingCarGameMachineValidator();
+    }
+
+    @Override
+    public RacingCarValidator racingCarValidator() {
+        return new RacingCarValidator();
     }
 
     @Override
