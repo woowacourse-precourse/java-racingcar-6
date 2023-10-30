3e8b9f17023194bf6b5038378b5426c3afa728ad
ay-eonii <naayen@naver.com>
2023-10-31 02:21:42 +0900

    feat: 자동차 이름 입력 메시지 출력 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,10 @@
 package racingcar.view;
 
 public class OutputView {
+    private final static String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
     public void displayCarNameInputMessage() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
     }
 
     public void displayTryCountInputMessage() {
