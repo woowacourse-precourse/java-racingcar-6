10145ea554fda170d3853613ade8c702d3dd44dd
dbstn1325 <dbstn1325@naver.com>
2023-10-30 18:20:28 +0900

    test(TryCountValidatorTest): 시도 횟수가 빈 값인 경우 예외처리가 발생하는지 테스트

--- src/test/java/racingcar/TryCountValidatorTest.java
+++ src/test/java/racingcar/TryCountValidatorTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import validator.TryCountValidator;
@@ -13,5 +14,16 @@ public class TryCountValidatorTest {
         Assertions.assertThatThrownBy(() -> TryCountValidator.validateString(tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("시도 횟수는 숫자만 가능합니다. 애플리케이션을 종료합니다.");
+    }
+
+    @Test
+    void 시도_횟수가_빈_값인_경우_예외처리_발생_테스트() {
+        //given
+        String emptyTryCount = "";
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> TryCountValidator.validateEmpty(emptyTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수가 빈 값입니다. 애플리케이션을 종료합니다.");
     }
 }
