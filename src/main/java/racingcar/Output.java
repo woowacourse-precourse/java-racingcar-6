41dcb53fbee6c4a7615f02c84ef90a4dfd3d688d
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 16:20:36 +0900

    feat: [Output] 우승자 출력하기 기능 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -19,6 +19,18 @@ public class Output {
     }
 
     public void printRacingWinner(Map<String, Integer> carNameAndMoving) {
+        List<String> winnerList = new ArrayList<>();
+        int maxMoving = 0;
 
+        for (int moving : carNameAndMoving.values()) {
+            maxMoving = Math.max(maxMoving, moving);
+        }
+        for (String carName : carNameAndMoving.keySet()) {
+            if (carNameAndMoving.get(carName) == maxMoving) {
+                winnerList.add(carName);
+            }
+        }
+        String winners = String.join(", ", winnerList);
+        System.out.println("최종 우승자 : " + winners);
     }
 }
