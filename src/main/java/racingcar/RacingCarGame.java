8a11cd9a5fbf2056030b42190333e37346d92e6d
1027bestlife <1027bestlife@gmail.com>
2023-10-30 21:59:30 +0900

    feat: 게임 결과값 비교 하는 메서드 추가, 단독 우승자 출력 하는 메서드 추가, 공동 우승자 출력 하는 메서드 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -8,7 +8,7 @@ public class RacingCarGame {
 
     public static void play(List<String> carNames, int tries) {
         List<Integer> distance = new ArrayList<Integer>();
-        for ( int i = 0 ; i < carNames.size(); i++ ){
+        for (int i = 0; i < carNames.size(); i++) {
             distance.add(0);
         }
         for (int i = 0; i < tries; i++) {
@@ -24,24 +24,64 @@ public class RacingCarGame {
             }
             showRoundResults(carNames, distance);
         }
+
+        List<Integer> resultIndex = compareResults(distance);
+        if (resultIndex.size() == 1) {
+            showSoloWinner(carNames.get(resultIndex.get(0)));
+        } else {
+            showMultipleWinners(carNames, resultIndex);
+        }
     }
 
-    public static void showRoundResults(List<String> carNames, List<Integer> distance){
+    public static List<Integer> compareResults(List<Integer> distance) {
+        int maxDistance = Integer.MIN_VALUE;
+        List<Integer> maxIndex = new ArrayList<Integer>();
+
+        for (int i = 0; i < distance.size(); i++) {
+            int currentDistance = distance.get(i);
+            if (currentDistance > maxDistance) {
+                maxDistance = currentDistance;
+                maxIndex.clear();
+                maxIndex.add(i);
+            } else if (currentDistance == maxDistance) {
+                maxIndex.add(i);
+            }
+        }
+
+        return maxIndex;
+    }
 
 
-        for ( int i = 0; i< carNames.size(); i++ ){
+    public static void showRoundResults(List<String> carNames, List<Integer> distance) {
+
+        for (int i = 0; i < carNames.size(); i++) {
             System.out.println(carNames.get(i) + " : " + generateHyphens(distance.get(i)));
 
         }
         System.out.println();
     }
 
-    public static String generateHyphens(int distance){
+    public static String generateHyphens(int distance) {
         String hyphens = "";
-        for( int i = 0; i < distance; i++){
+        for (int i = 0; i < distance; i++) {
             hyphens = hyphens + "-";
         }
         return hyphens;
+    }
+
+    public static void showSoloWinner(String carName) {
+        System.out.println("최종 우승자 : " + carName);
+    }
+
+    public static void showMultipleWinners(List<String> carNames, List<Integer> resultIndex) {
+        List<String> resultWinners = new ArrayList<>();
+
+        for (int i = 0; i < resultIndex.size(); i++) {
+            resultWinners.add(carNames.get(resultIndex.get(i)));
+        }
+        String result = "최종 우승자 : " + String.join(", ", resultWinners);
+
+        System.out.println(result);
     }
 }
 
