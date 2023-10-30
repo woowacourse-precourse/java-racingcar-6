33a12758f9fdb296dfe509cb4789bbf3bf607037
Heejun-0219 <heejun0219@gmail.com>
2023-10-31 00:54:07 +0900

    feat(run): Application.java에서 컨트롤러 생성 및 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.Controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final RacingCarController car = new RacingCarController();
+        car.run();
     }
 }
