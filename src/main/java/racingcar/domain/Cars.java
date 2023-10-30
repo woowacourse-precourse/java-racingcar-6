b51c3442c962ec6e84a4802f3524750e1b8a9520
cyPark95 <parkcy1007@gmail.com>
2023-10-30 21:17:02 +0900

    refactor: 자동차 이름 목록 파라미터 타입 변경
    - 자바에서 제공하는 API 를 활용하기 위해 String 배열에서 String 리스트로 파라미터 타입 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import static racingcar.common.response.ErrorCode.CAR_NAME_DUPLICATION;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,16 +11,14 @@ import racingcar.common.exception.CustomErrorException;
 
 public class Cars {
 
-    private static final String SEPARATOR = ",";
     private static final int MIN_RANDOM_NUMBER = 0;
     private static final int MAX_RANDOM_NUMBER = 9;
 
     private final List<Car> cars;
 
-    public Cars(String input) {
-        String[] names = input.split(SEPARATOR);
+    public Cars(List<String> names) {
         validateNames(names);
-        this.cars = Arrays.stream(names)
+        this.cars = names.stream()
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
@@ -37,14 +34,14 @@ public class Cars {
         return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 
-    private void validateNames(String[] names) {
+    private void validateNames(List<String> names) {
         if (isDuplicatedNames(names)) {
             throw new CustomErrorException(CAR_NAME_DUPLICATION);
         }
     }
 
-    private boolean isDuplicatedNames(String[] names) {
-        Set<String> set = new HashSet<>(Arrays.asList(names));
-        return names.length != set.size();
+    private boolean isDuplicatedNames(List<String> names) {
+        Set<String> set = new HashSet<>(names);
+        return names.size() != set.size();
     }
 }
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,11 +1,12 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.common.response.ErrorCode.CAR_NAME_DUPLICATION;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("자동차 목록 검증")
 class CarsTest {
@@ -13,7 +14,7 @@ class CarsTest {
     @Test
     void 자동차_목록_이름_중복_예외() {
         // given
-        String input = "car,car,name";
+        List<String> input = List.of("car", "car", "name");
 
         // when
         // then
@@ -25,7 +26,7 @@ class CarsTest {
     @Test
     void 자동차_목록_생성() {
         // given
-        String input = "car,name,myCar";
+        List<String> input = List.of("car", "name", "myCar");
 
         // when
         Cars result = new Cars(input);
