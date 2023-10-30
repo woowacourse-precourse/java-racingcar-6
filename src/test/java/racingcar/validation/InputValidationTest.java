0eb17fb81feb9b4593a579782c87fba0c39b166f
ohjuyoungg <ejrm12@naver.com>
2023-10-31 01:13:47 +0900

    test(InputValidation) : validateNameList 자동차 이름 검증 테스트 구현

--- src/test/java/racingcar/validation/InputValidationTest.java
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -13,32 +12,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class InputValidationTest {
-
-    @Test
-    void validateName_자동차_이름을_검증_성공한다() {
-        // given : 테스트를 진행하기 위해 필요한 값 세팅 및 설정
-        String carName = "pobi";
-
-        // when : 언제? 테스트 대상 메서드 실행
-        // then : 실질적으로 검증하는 과정
-        assertDoesNotThrow(() -> InputValidation.validateName(carName));
-    }
-
-    @ParameterizedTest
-    @MethodSource("validationName_자동차_이름을_검증_실패한다")
-    void validateName_자동차_이름을_검증_실패한다(String carName) {
-        assertThatThrownBy(() -> InputValidation.validateName(carName))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(InputValidation.CAR_NAME_LENGTH_VALIDATION_EXCEPTION);
-    }
-
-    private static Stream<Arguments> validateName_자동차_이름을_검증_실패한다() {
-        return Stream.of(
-            Arguments.of("     "),
-            Arguments.of(""),
-            Arguments.of("abcdefghijkl")
-        );
-    }
 
     @Test
     void validateNameAndSplit_자동차_콤마구분_검증_성공한다() {
@@ -65,29 +38,6 @@ public class InputValidationTest {
     }
 
     @Test
-    void validateDuplicatedName_자동차_이름이_다를경우_성공한다() {
-        List<String> carNames = List.of("pobi", "crong");
-        assertThat(carNames).contains("pobi", "crong");
-        assertDoesNotThrow(() -> InputValidation.validateDuplicatedName(carNames));
-    }
-
-    @ParameterizedTest
-    @MethodSource("validateDuplicatedName_자동차_이름이_같을경우_실패한다")
-    void validateDuplicateName_자동차_이름이_같을경우_실패한다(List<String> carNames) {
-        assertThatThrownBy(() -> InputValidation.validateDuplicatedName(carNames))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(InputValidation.CAR_SAME_NAME_EXCEPTION);
-    }
-
-    private static Stream<Arguments> validateDuplicatedName_자동차_이름이_같을경우_실패한다() {
-        return Stream.of(
-            Arguments.of(Arrays.asList("pobi", "pobi")),
-            Arguments.of(Arrays.asList("crong", "crong")),
-            Arguments.of(Arrays.asList("a", "a"))
-        );
-    }
-
-    @Test
     void validateIsNumber_자동차_경주_게임_시도_횟수가_숫자일경우_성공한다() {
         String number = String.valueOf(1);
         assertDoesNotThrow(() -> InputValidation.validateIsNumber(number));
@@ -106,6 +56,36 @@ public class InputValidationTest {
             Arguments.of("a"),
             Arguments.of("."),
             Arguments.of("/")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validateNameList_자동차_이름_검증에_성공한다")
+    void validateNameList_자동차_이름_검증에_성공한다(List<String> carNames) {
+        assertDoesNotThrow(() -> InputValidation.validateNameList(carNames));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validateNameList_자동차_이름_검증에_실패한다")
+    void validateNameList_자동차_이름_검증에_실패한다(List<String> carNames, String failureMessage) {
+        assertThatThrownBy(() -> InputValidation.validateNameList(carNames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(failureMessage);
+    }
+
+    private static Stream<Arguments> validateNameList_자동차_이름_검증에_성공한다() {
+        return Stream.of(
+            Arguments.of(List.of("pobi")),
+            Arguments.of(List.of("pobi", "crong"))
+        );
+    }
+
+    private static Stream<Arguments> validateNameList_자동차_이름_검증에_실패한다() {
+        return Stream.of(
+            Arguments.of(List.of(""), InputValidation.CAR_NAME_LENGTH_VALIDATION_EXCEPTION),
+            Arguments.of(List.of("     "), InputValidation.CAR_NAME_LENGTH_VALIDATION_EXCEPTION),
+            Arguments.of(List.of("aaaabbbccccccdddeeee"), InputValidation.CAR_NAME_LENGTH_VALIDATION_EXCEPTION),
+            Arguments.of(List.of("a", "a", "b"), InputValidation.CAR_SAME_NAME_EXCEPTION)
         );
     }
 }
