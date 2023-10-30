df62015a71e34b51de60e97e56d719d177154dc1
NamJihyun99 <namjihyun9936@gmail.com>
2023-10-30 17:18:38 +0900

    feat:> Car 클래스 정의

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int status;
+
+    public Car(String name) {
+        this.name = name;
+        this.status = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void move() {
+        this.status++;
+    }
+}
