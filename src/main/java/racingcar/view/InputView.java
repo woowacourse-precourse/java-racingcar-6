6d5b4ddd38957598a3d031b7f4ab3aa08558f374
GS_song <20003204@sju.ac.kr>
2023-10-30 15:41:37 +0900

    refactor: InputView의 역할 최소화
    InputView에서는 값을 받아오는 역할만 수행하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,22 +4,16 @@ import static racingcar.constant.Constant.READ_CAR_NAME_MESSAGE;
 import static racingcar.constant.Constant.READ_MOVE_COUNT_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.validation.CarNameValidation;
-import racingcar.validation.CountValidation;
 
 public final class InputView {
 
     public static String readCarName() {
         System.out.println(READ_CAR_NAME_MESSAGE);
-        String carName = Console.readLine();
-        CarNameValidation.isEmptyInput(carName);
-        return carName;
+        return Console.readLine();
     }
 
     public static String readMoveCount() {
         System.out.println(READ_MOVE_COUNT_MESSAGE);
-        String moveCount = Console.readLine();
-        CountValidation.isEmptyInput(moveCount);
-        return moveCount;
+        return Console.readLine();
     }
 }
