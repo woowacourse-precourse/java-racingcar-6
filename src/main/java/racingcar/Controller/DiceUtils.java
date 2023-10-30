1e63498f2e76bf3c17b74ab0a1ac3757ec2868d9
Yoon Jiho <zlhh3842@nate.com>
2023-10-31 02:16:38 +0900

    리펙토링 최적화, 예외처리 추가

--- src/main/java/racingcar/Controller/DiceUtils.java
+++ src/main/java/racingcar/Controller/DiceUtils.java
@@ -3,7 +3,7 @@ package racingcar.Controller;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class DiceUtils {
-    public static int getDiceRool(int min, int max) {
+    public static int getDiceRoLl(int min, int max) {
         return Randoms.pickNumberInRange(min,max);
     }
 }
--- src/main/java/racingcar/Controller/InputConverter.java
+++ src/main/java/racingcar/Controller/InputConverter.java
@@ -3,13 +3,13 @@ package racingcar.Controller;
 import java.util.List;
 
 public class InputConverter {
-    public static List<String> nameInputCvt(String input){
+    public static List<String> convertStringToNames(String input){
         List<String> inputs = List.of(input.split(","));
         InputValidator.checkCarNames(inputs);
         return inputs;
     }
 
-    public static int turnsInputCnt(String input) {
+    public static int convertStringToTurns(String input) {
         int turns = InputValidator.checkTurn(input);
         return turns;
     }
