cfc59ae21e366c02b8f5e1707975e786fa1697c4
harandal24601 <devchaehokim@gmail.com>
2023-10-30 18:54:49 +0900

    Feat : Add CarMotionController
    Create carMotion Controller

---
+++ src/main/java/racingcar/CarMotionController.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CarMotionController {
+    public static String carMotion(int randomNumber) {
+        if (randomNumber >= 4) {
+            return "-";
+        }
+        return "";
+    }
+    
+}
