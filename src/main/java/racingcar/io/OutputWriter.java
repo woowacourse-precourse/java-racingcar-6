9e9e3c6882c7b075b69f562a7640a868725e2807
Jiihyun <cwlgusc@gmail.com>
2023-10-31 04:06:54 +0900

    feat(OutputView): 각 라운드당 실행 결과 출력

--- src/main/java/racingcar/io/OutputWriter.java
+++ src/main/java/racingcar/io/OutputWriter.java
@@ -3,6 +3,7 @@ package racingcar.io;
 public class OutputWriter {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
     private static final String INPUT_TRY_TIMES = "시도할 회수는 몇회인가요?\n";
+    private static final String GAME_RESULT = "\n실행 결과";
 
     public void writeInputNameMsg() {
         System.out.println(INPUT_CAR_NAMES);
@@ -10,5 +11,9 @@ public class OutputWriter {
 
     public void writeInputTryTimesMsg() {
         System.out.println(INPUT_TRY_TIMES);
+    }
+
+    public void writeGameResultMsg() {
+        System.out.println(GAME_RESULT);
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,36 @@
 package racingcar.view;
 
+import racingcar.domain.dto.output.CarDto;
+import racingcar.domain.dto.output.CarsDto;
+import racingcar.io.OutputWriter;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String DASH = "-";
+    private final OutputWriter writer;
+
+    public OutputView(OutputWriter writer) {
+        this.writer = writer;
+    }
+
+    public void printGameResultMsg() {
+        writer.writeGameResultMsg();
+    }
+
+    public void printGameResult(CarsDto carsDto) {
+        List<CarDto> carDtoList = carsDto.carDtoList();
+        for (CarDto carDto : carDtoList) {
+            String carGameResult = String.format("%s : %s",
+                    carDto.name(),
+                    convertToDashes(carDto.position()));
+            System.out.println(carGameResult);
+        }
+        System.out.println();
+    }
+
+    private String convertToDashes(int position) {
+        return DASH.repeat(Math.max(0, position));
+    }
+
 }
