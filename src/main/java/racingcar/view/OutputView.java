a39826a8ed4ec6e18111563df5c48161f07e097d
Donghun Won <wonhun1225@gmail.com>
2023-10-30 19:37:29 +0900

    feat(view) : OutputView 클래스 작성
    결과안내문 출력
    각 라운드별 결과값 출력
    이동횟수에 따라 "-" 출력하는 메서드

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.Car;
+
+public class OutputView {
+
+    public void showResult(){
+        System.out.println("\n실행 결과");
+    }
+    public void showRoundResult(List<Car> carList){
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            String name = car.getName();
+            int move = car.getTotalMove();
+            System.out.print(name + " : ");
+            moveBar(move);
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+
+    private void moveBar(int move){
+        for (int i = 0; i < move; i++) {
+            System.out.print("-");
+        }
+    }
+}
