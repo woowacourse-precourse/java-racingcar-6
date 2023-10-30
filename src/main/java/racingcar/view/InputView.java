275eb2d465858e00612a2514d55536ef7b104aa5
MingyeomKim <mingyum119@naver.com>
2023-10-30 16:36:00 +0900

    feat(InputView): 입력 단에서 빈 문자열의 입력을 검증
    공백 문자열이 입력된 경우를 InputView에서 검증한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,4 +6,19 @@ public class InputView {
     public String readInput() {
         return Console.readLine();
     }
+
+    public String readCarNames() {
+        String carNames = Console.readLine();
+        validateCarNames(carNames);
+    }
+
+    private void validateCarNames(String carNames) {
+        validateBlankInput(carNames);
+    }
+
+    private void validateBlankInput(String carNames) {
+        if (carNames.isBlank()) {
+            throw new IllegalArgumentException("공백이 입력되었습니다.");
+        }
+    }
 }
