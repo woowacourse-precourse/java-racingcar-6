109fa6c03dc10f5847e9c9b3fd32a0659979ee81
username <57055730+Jake-huen@users.noreply.github.com>
2023-10-30 10:47:24 +0900

    refactor : 단독 우승자와 공동 우승자 분리

--- src/main/java/racingcar/view/RacingGameView.java
+++ src/main/java/racingcar/view/RacingGameView.java
@@ -6,15 +6,18 @@ import java.util.List;
 
 public class RacingGameView {
 
+    public static final String INPUT_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_FOR_ATTEMPTS = "시도할 회수는 몇회인가요?";
+
     public void inputForCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_FOR_CAR_NAMES);
     }
 
     public void inputForAttempts(){
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(INPUT_FOR_ATTEMPTS);
     }
 
-    public void printCars(List<Car> cars) {
+    public void printRoundResults(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + car.getPositionString());
         }
@@ -23,6 +26,18 @@ public class RacingGameView {
 
     public void printWinners(List<Car> winners) {
         System.out.print("최종 우승자 : ");
+        if (winners.size() == 1) {
+            printSingleWinner(winners.get(0));
+        } else {
+            printMultipleWinners(winners);
+        }
+    }
+
+    private void printSingleWinner(Car winner) {
+        System.out.print(winner.getName());
+    }
+
+    private void printMultipleWinners(List<Car> winners) {
         for (int i = 0; i < winners.size(); i++) {
             System.out.print(winners.get(i).getName());
             if (i < winners.size() - 1) {
