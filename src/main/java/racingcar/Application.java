16ec77dd6cfa659be3798109f1f77c78b9090b39
donghoonyeom <dua5410773@naver.com>
2023-10-31 01:03:22 +0900

    feat(Application): getNumberOfAttempts() 메서드 추가
    다음과 같은 기능을 수행하는 getNumberOfAttempts() 메서드를 Application 클래스에 추가했습니다:
    - ConsoleView를 통해 사용자로부터 시도할 회수를 입력받습니다.
    - 입력된 회수를 정수로 변환하여 반환합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -13,4 +13,9 @@ public class Application {
         consoleView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
     }
+
+    private static int getNumberOfAttempts(ConsoleView consoleView) {
+        consoleView.printMessage("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
