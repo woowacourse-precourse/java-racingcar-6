9c55af9b1f6d5c4ccd5e572e2dd4587535601c45
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-30 22:51:01 +0900

    car Class 작성

--- src/main/java/racingcar/domain/car.java
+++ src/main/java/racingcar/domain/car.java
@@ -1,8 +1,18 @@
 package racingcar.domain;
 
 public class car {
+    int position = 0;
 
+    public void go() {
+        position++;
+    }
 
+    public void stop() {
+    }
+
+    public int getPosition() {
+        return position;
+    }
 
 
 }
