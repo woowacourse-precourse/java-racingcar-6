fe2599379e4f68bc17dddadec9e913602c4acdc2
WinterHana <haha920@naver.com>
2023-10-30 22:42:39 +0900

    refactor(Car, ConstantNumber) : 상수에 대한 변경
    - Car에 있던 상수 STEP_PERCENTAGE 삭제
    - 그에 따라 ConstantNumber에 있는 상수 사용

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car implements Comparable<Car> {
     String name;
     int distance;
-    final int STEP_PERCENTAGE = 4;  // 자동차가 이동할 확률 조정. 0~9까지 1씩 오를수록 10% 확률 감소.
 
     Car(String name) {
         this.name = name;
@@ -24,7 +23,7 @@ public class Car implements Comparable<Car> {
 
     void moveRandom() {
         int randomNumber = Randoms.pickNumberInRange(0,9);
-        if(randomNumber >= STEP_PERCENTAGE) {
+        if(randomNumber >= ConstantNumber.STEP_PERCENTAGE) {
             distance = distance + 1;
         }
     }
