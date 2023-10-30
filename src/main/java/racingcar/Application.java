0b434af87e7c1ae18026dfca29cd6d6bf2fb6409
sequaa <asl06194@gmail.com>
2023-10-31 02:12:43 +0900

    feat: Application 기능 추가 Car 클래스와 Race 클래스를 이용하여 경주 기능을 추가했습니다. winners 리스트를 선언하여 그 길이에 따라 최종 우승자를 출력하는 기능을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +14,19 @@ public class Application {
 
         int tryCount = Integer.parseInt(Console.readLine());
 
+        List<Car> cars = new ArrayList<>();
+        for (String carName: carNames) {
+            cars.add(new Car(carName));
+        }
 
+        Race race = new Race(cars,tryCount);
+        race.start();
+
+        List<String> winners = race.getWinners();
+        if (winners.size() > 1) {
+            System.out.println("최종 우승자 : " + String.join(",",winners));
+        } else {
+            System.out.println("최종 우승자 : " + winners.get(0));
+        }
     }
 }
