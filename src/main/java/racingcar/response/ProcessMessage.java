d428eb1f2a4a00aa9cb2542090e086be0ae40f9c
mikekks <thdalsrb79@naver.com>
2023-10-30 21:26:42 +0900

    [feat] 게임 진행을 위한 매직리터럴 enum 구현

---
+++ src/main/java/racingcar/response/ProcessMessage.java
@@ -0,0 +1,16 @@
+package racingcar.response;
+
+public enum ProcessMessage {
+    REQUIRE_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    REQUIRE_TRY_NUMBER_MESSAGE("시도할 회수는 몇회인가요? ");
+
+    private final String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    ProcessMessage(String msg) {
+        this.msg = msg;
+    }
+}
