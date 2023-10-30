8f9d68838c14a9476b5b06a878b35d9d06ee917b
Sanghwi Lee <sanghwi47@naver.com>
2023-10-30 11:42:26 +0900

    refactor(CheckNumber) error message를 상수 처리í

--- src/main/java/racingcar/function/CheckNumber.java
+++ src/main/java/racingcar/function/CheckNumber.java
@@ -1,6 +1,9 @@
 package racingcar.function;
 
 public class CheckNumber {
+    private static final String ERROR_INVALID_DIGITS = "숫자로 입력 하세요.";
+    private static final String ERROR_NOT_POSITIVE_NUMBER = "0 이상의 값을 입력 하세요";
+
     public static int validate(String inputNumber) {
         validateOnlyDigits(inputNumber);
         return validatePositiveNumber(inputNumber);
@@ -9,7 +12,7 @@ public class CheckNumber {
     private static void validateOnlyDigits(String inputNumber) {
         for (char character : inputNumber.toCharArray()) {
             if (!Character.isDigit(character)) {
-                throw new IllegalArgumentException("숫자로 입력 하세요.");
+                throw new IllegalArgumentException(ERROR_INVALID_DIGITS);
             }
         }
     }
@@ -17,7 +20,7 @@ public class CheckNumber {
     private static int validatePositiveNumber(String inputNumber) {
         int number = Integer.parseInt(inputNumber);
         if (number <= 0) {
-            throw new IllegalArgumentException("0 이상의 값을 입력하세요");
+            throw new IllegalArgumentException(ERROR_NOT_POSITIVE_NUMBER);
         }
         return number;
     }
