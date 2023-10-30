9db14a90d7fd9f5ddcbc0d062ad00a2753544ff0
DESKTOP-UPSCALB\tkddn <thwlsdl0989@gmail.com>
2023-10-30 11:28:27 +0900

    클래스 이름 변경 printFunction -> print

--- src/main/java/racingcar/Print.java
+++ src/main/java/racingcar/Print.java
@@ -3,16 +3,16 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintFunction {
+public class Print {
 
-    void printScore(List<Car> carList) {
+    void Score(List<Car> carList) {
         for (Car car : carList) {
             System.out.println(car.name + " : " + "-".repeat(car.score));
         }
         System.out.println();
     }
 
-    void printWinner(List<Car> carList) {
+    void Winner(List<Car> carList) {
         List<String> winnerName = new ArrayList<>();
         int max = 0;
         for (Car car : carList) {
@@ -30,4 +30,8 @@ public class PrintFunction {
         System.out.print("최종 우승자 : ");
         System.out.println(result);
     }
+
+
+
+
 }
