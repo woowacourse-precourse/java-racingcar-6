8cd0cf1137af3a6f22fb844196140869e67c3b5c
mksystem98 <mksystem98@gmail.com>
2023-10-30 16:28:20 +0900

    feat: move(), display() 메서드 추가
    move() : 값이 4이상이면 position을 증가시킴
    display() : position 값에 따라 이름 뒤에 '-'를 추가하는 기능

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
+
 public class Car {
+
+    private static final int MINIMUM_MOVE_VALUE = 4;
 
     private String name;
     private int position = 0;
@@ -9,12 +12,25 @@ public class Car {
         this.name = name;
     }
 
+    public void move(int randomValue) {
+        if (randomValue >= MINIMUM_MOVE_VALUE) {
+            position++;
+        }
+    }
+
     public String getName() {
         return name;
     }
 
     public int getPosition() {
         return position;
+    }
 
+    public String display() {
+        StringBuilder display = new StringBuilder(name + " : ");
+        for (int i = 0; i< position; i++) {
+            display.append("-");
+        }
+        return display.toString();
     }
 }
