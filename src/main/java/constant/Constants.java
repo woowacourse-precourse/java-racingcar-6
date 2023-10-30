dbe8bb444b8ed4dc6d19ed962f882d2faffbf4f1
dltmddus <tmddus4671@gmail.com>
2023-10-30 21:35:16 +0900

    feat: Constants 목록 정리

---
+++ src/main/java/constant/Constants.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class Constants {
+    public static String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    public static String INPUT_NUMBERS = "시도할 회수는 몇회인가요?";
+    public static String RESULT = "실행 결과";
+    public static String FINAL_WINNER = "최종 우승자";
+}
---
+++ src/main/java/racingcarGame/Game.java
@@ -0,0 +1,12 @@
+package racingcarGame;
+
+
+public class Game {
+    public Game(){
+
+    }
+
+    public static void run(){
+        Game game;
+    }
+}
