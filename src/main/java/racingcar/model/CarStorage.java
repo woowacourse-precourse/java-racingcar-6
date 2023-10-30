960b545abac9a2ef15e460dac00c98adb99b0bc7
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:11:48 +0900

    [feat] Add CarStorage class and fields

---
+++ src/main/java/racingcar/model/CarStorage.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class CarStorage {
+    private static List<Car> winners;
+    private static List<Car> cars;
+    private static List<Integer> stepsList;
+}
