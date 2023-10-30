564dc292e55d016bcb8106dbea41dece1b1b21fe
34suuuuu <psh7492@gmail.com>
2023-10-30 18:27:45 +0900

    style: ROUND_RESULT 줄바꿈 제거

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final String ROUND_RESULT_DELIMETER = " : ";
     public static final String SCORE = "-";
 
-    public static final Pattern CAR_NAMES_PATTERN = Pattern.compile("^([\\w\\d]{1,5},)+[\\w\\d]{1,5}$\n");
+    public static final Pattern CAR_NAMES_PATTERN = Pattern.compile("^[가-힣\\w]+[가-힣\\w" + DELIMITER + "]*[가-힣\\w]$");
     public static final Pattern TRY_COUNT_PATTERN = Pattern.compile("^[1-9][0-9]*$");
 
     public static final String ERROR = "ERROR: ";
@@ -24,6 +24,6 @@ public class Constants {
     public static final String INPUT_WRONG_ERROR_MESSAGE = "입력값이 존재하지 않습니다.";
     public static final String INPUT_ONLY_INTEGER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 
-    public static final String ROUND_RESULT = "실행 결과\n";
+    public static final String ROUND_RESULT = "실행 결과";
     public static final String WINNER_MESSAGE = "최종 우승자";
 }
