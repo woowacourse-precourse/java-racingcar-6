0fa3340709fb1014dacc8c3dc30c63b8c24ea708
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-30 19:19:02 +0900

    test: @BeforeEach를 이용하여 CarTest 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,36 +1,49 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.utils.numbergenerators.NumberGenerator;
 import racingcar.utils.numbergenerators.TestNumberGenerator;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarTest {
-    @Test
-    void 기준넘지않으면_이동불가() {
-        TestNumberGenerator testGenerator = new TestNumberGenerator(1);
-        Car car = new Car("CarName", testGenerator);
-        car.move();
+    private Car movableCar;
+    private Car nonmovableCar;
 
-        assertThat(car.getDistance()).isEqualTo(0);
+    @BeforeEach
+    void setUp() {
+        //이동가능한 자동차
+        NumberGenerator movableNumberGenerator = new TestNumberGenerator(4);
+        movableCar = new Car("CarName", movableNumberGenerator);
+        //이동 불가능한 자동차
+        NumberGenerator nonmovableNumberGenerator = new TestNumberGenerator(1);
+        nonmovableCar = new Car("CarName", nonmovableNumberGenerator);
     }
 
     @Test
-    void 기준이상이면_이동가능() {
-        TestNumberGenerator testGenerator = new TestNumberGenerator(4);
-        Car car = new Car("CarName", testGenerator);
-        car.move();
+    @DisplayName("이동불가_기준넘지않음")
+    void nonmovableCarShouldNotMove() {
+        nonmovableCar.move();
 
-        assertThat(car.getDistance()).isEqualTo(1);
+        assertThat(nonmovableCar.getDistance()).isEqualTo(0);
     }
 
     @Test
-    void 최대거리True반환() {
-        TestNumberGenerator testGenerator = new TestNumberGenerator(6);
-        Car car = new Car("CarName", testGenerator);
-        car.move();
+    @DisplayName("이동가능_기준이상")
+    void movableCarShouldMove() {
+        movableCar.move();
 
-        assertThat(car.isMaxDistance(1)).isEqualTo(true);
-        assertThat(car.isMaxDistance(2)).isEqualTo(false);
+        assertThat(movableCar.getDistance()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("최대거리True반환")
+    void returnTrueForMaximunDistance() {
+        movableCar.move();
+
+        assertThat(movableCar.isMaxDistance(1)).isTrue();
+        assertThat(movableCar.isMaxDistance(0)).isFalse();
     }
 }
