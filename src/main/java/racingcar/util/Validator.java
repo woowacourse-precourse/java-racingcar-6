334a6afc4d3601197c5f50027501e0694eac101a
박성춘 <songchon100@gmail.com>
2023-10-30 13:23:23 +0900

    feat : 공통 예외처리 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,4 +1,12 @@
 package racingcar.util;
 
 public abstract class Validator {
+    public abstract void validate(String inputString);
+
+    // 공백인 경우
+    static void validateEmptyInput(String carNames) {
+        if (carNames.isBlank()) throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT_ERROR.getErrorMessage());
+    }
+
+
 }
