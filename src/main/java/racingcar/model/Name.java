7dafaae6a8d896cc46edba9756f88847a6987d58
devholic22 <hyunjoon.tech@gmail.com>
2023-10-30 14:29:30 +0900

    refactor: Name 예외 메시지 상수 처리
    길이 예외 발생 시 메시지 출력에 상수 이용되도록 수정

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 public class Name {
 
+    private static final int MINIMUM_LENGTH = 1;
     private static final int MAXIMUM_LENGTH = 5;
 
     private final String name;
@@ -16,9 +17,11 @@ public class Name {
     }
 
     private static void validate(final String name) {
-        if (name.isEmpty() || name.length() > MAXIMUM_LENGTH) {
-            throw new IllegalArgumentException("이름 길이는 1자 이상 5자 이하여야 합니다.");
+        if (name.length() < MINIMUM_LENGTH || name.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException("이름 길이는 " + MINIMUM_LENGTH + "자 이상 "
+                    + MAXIMUM_LENGTH + "자 이하여야 합니다.");
         }
+
         if (name.contains(" ")) {
             throw new IllegalArgumentException("공백이 있으면 안 됩니다.");
         }
