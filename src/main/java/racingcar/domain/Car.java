e862297260c0d349b033d381b11e2f31ba8e250c
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-31 03:39:17 +0900

    fix: Car 클래스 생성자 생성
    - Car 클래스의 생성자를 추가하였음

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,6 +4,11 @@ public class Car {
     private String name;
     private int location = 0;
 
+    public Car(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
