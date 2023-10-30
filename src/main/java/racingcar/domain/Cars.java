644d178a8819b86326adc4d81d21da3d40038de8
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 14:49:20 +0900

    refactor: 잘못된 임포트 삭제

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import racingcar.constants.ExceptionMessage;
 import racingcar.util.NumberGenerator;
 
