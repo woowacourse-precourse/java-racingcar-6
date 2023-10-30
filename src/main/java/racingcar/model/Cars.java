e4b32a5f5914335b015d7988be12787eee144793
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 16:05:33 +0900

    feat(Cars): Car 리스트 중 가장 이동거리가 높은 Car List를 반환한다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,7 +1,9 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Cars {
     private final List<Car> cars;
@@ -12,5 +14,28 @@ public class Cars {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public List<Car> getCarsWithHighestDistance() {
+        List<Car> carsWithHighestDistance = new ArrayList<>();
+
+        if (cars.isEmpty()) {
+            return carsWithHighestDistance;
+        }
+
+        // 가장 높은 distance 값 찾기
+        Integer highestDistance = cars.stream()
+                .map(Car::getDistance)
+                .max(Integer::compareTo)
+                .orElse(0);
+
+        // 가장 높은 distance를 가진 모든 Car 객체 찾기
+        cars.forEach(car -> {
+            if (car.getDistance().equals(highestDistance)) {
+                carsWithHighestDistance.add(car);
+            }
+        });
+
+        return carsWithHighestDistance;
     }
 }
