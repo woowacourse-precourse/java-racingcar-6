9cac6e7646163180d6ff0a55e2620cf19e93793c
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-30 19:38:43 +0900

    test: 여러개 입력 테스트 수행 시 오류 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,12 +1,16 @@
 package racingcar.view;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.validation.InputValidator;
 
 class InputViewTest {
@@ -18,38 +22,47 @@ class InputViewTest {
         inputView = new InputView(new InputValidator());
     }
 
-    @Test
+    @AfterEach
+    void clean() {
+        Console.close();
+    }
+
     @DisplayName("경주할 자동차 입력받는다. 단, 비어있지 않다.")
-    void testInputCarNamesEmpty() {
-        String inputEmpty = "\n";
-        InputStream in = new ByteArrayInputStream(inputEmpty.getBytes());
+    @ParameterizedTest
+    @ValueSource(strings = {"\n"})
+    void testInputCarNamesEmpty(String input) {
+        InputStream in = generateInput(input);
         System.setIn(in);
 
         assertThrows(IllegalArgumentException.class,
                 () -> inputView.inputCarNames());
     }
 
-    @Test
     @DisplayName("경주할 자동차를 입력받는다. 단, 공백일 수 없다.")
-    void testInputCarNamesBlank() {
-        String inputBlank = " ";
-        InputStream in = new ByteArrayInputStream(inputBlank.getBytes());
+    @ParameterizedTest
+    @ValueSource(strings = {" "})
+    void testInputCarNamesBlank(String input) {
+        InputStream in = generateInput(input);
         System.setIn(in);
 
         assertThrows(IllegalArgumentException.class,
                 () -> inputView.inputCarNames());
     }
 
-    @Test
     @DisplayName("시도할 횟수를 입력받는다. 단, 숫자만 허용한다.")
-    void testInputTryCount() {
-        String inputTryCount = "a";
-        InputStream in = new ByteArrayInputStream(inputTryCount.getBytes());
+    @ParameterizedTest
+    @ValueSource(strings = {"a"})
+    void testInputTryCount(String input) {
+        InputStream in = generateInput(input);
         System.setIn(in);
 
         assertThrows(IllegalArgumentException.class,
                 () -> inputView.inputTryCount());
 
+    }
+
+    private static InputStream generateInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
     }
 
 }
