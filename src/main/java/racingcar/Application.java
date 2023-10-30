b3414d85850c307c51577b7878af52572585ea7c
sansan20535 <20san20@daum.net>
2023-10-30 22:28:21 +0900

    Define Class Car and README.md Feature List

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,14 @@
 package racingcar;
 
+class Car{
+    private String name;
+    private String distance;
+
+    Car(String name, String distance){
+        this.name = name;
+        this.distance = distance;
+    }
+}
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
