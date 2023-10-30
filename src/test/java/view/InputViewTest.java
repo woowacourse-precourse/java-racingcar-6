f0b5fb5ddd97974a9c4d7f8302bed43849a8ec4d
kimwanyoung <dhks2869@gmail.com>
2023-10-31 07:47:13 +0900

    test : InputView 입력 테스트

---
+++ src/test/java/view/InputViewTest.java
@@ -0,0 +1,40 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputViewTest {
+
+    void provideInput(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
+    @AfterEach
+    void closeConsole() {
+        Console.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,lisa", "json,jiny"})
+    @DisplayName("자동차 이름 입력 정상 동작하는지 테스트")
+    public void 이름_입력_테스트(String input) {
+        //given
+        List<String> expectedName = Arrays.asList(input.split(","));
+
+        //when
+        provideInput(input);
+        List<String> result = InputView.getCarsNameFromUserInput();
+
+        //then
+        Assertions.assertThat(result).isEqualTo(expectedName);
+    }
+}
