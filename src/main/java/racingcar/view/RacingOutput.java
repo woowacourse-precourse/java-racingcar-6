f80bc71336346f2f79ff0d6e10c02d1fa7f53fd5
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2023-10-30 22:13:30 +0900

    feat 경주 진행 Input 생성, 실행결과/우승자 Output 추가

--- src/main/java/racingcar/view/RacingOutput.java
+++ src/main/java/racingcar/view/RacingOutput.java
@@ -1,19 +1,29 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class RacingOutput {
     public static void printStartRacing() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void printTryNumber() {
+    public static void printInputTryNumberMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void printProcessResult() {
+    public static void printProcessResultMessage() {
         System.out.println("실행 결과");
     }
 
-    public static void printWinner() {
-        System.out.print("최종 우승자 : ");
+    public static void printWinnerMessage(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public static void printProcessResult(List<Car> racingCar) {
+        for (var car : racingCar) {
+            System.out.println(car.getName() + " : " + car.getPosition());
+        }
+        System.out.println();
     }
 }
