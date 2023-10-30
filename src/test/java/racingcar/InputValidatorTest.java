15dee33d10348d2e2c92f5c4ec581ce0cff0eb0b
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 15:19:46 +0900

    test(InputValidatorTest) : 시행횟수 입력에 대한 유효성 검사의 테스트 코드 작성

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -2,8 +2,6 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.consts.ErrorMessages;
@@ -15,9 +13,9 @@ public class InputValidatorTest {
     @Test
     @DisplayName("자동차 이름 입력에 중복이 있는 경우")
     void testValidateCarNames1() {
-        List<String> carNames = Arrays.asList("car1", "car1", "car2");
+        String playerInput = "car1,car1,car3";
 
-        assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> inputValidator.validateCarNames(playerInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.DUPLICATION_CAR_NAME_MESSAGE.get());
     }
@@ -25,9 +23,9 @@ public class InputValidatorTest {
     @Test
     @DisplayName("자동차 종류가 2가지 이상이 아닐 경우")
     void testValidateCarNames2() {
-        List<String> carNames = Arrays.asList("car1");
+        String playerInput = "car1";
 
-        assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> inputValidator.validateCarNames(playerInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.WRONG_CAR_TYPES_MESSAGE.get());
     }
@@ -35,14 +33,38 @@ public class InputValidatorTest {
     @Test
     @DisplayName("자동차 이름이 1글자 이상 5글자 이하가 아닌 경우")
     void testValidateCarNames3() {
-        List<String> carNames1 = Arrays.asList("carNamesExample", "car1", "car2");
-        List<String> carNames2 = Arrays.asList("", "car1", "car2");
+        String playerInput1 = "carNamesExample,car1";
+        String playerInput2 = ",car1,car2";
 
-        assertThatThrownBy(() -> inputValidator.validateCarNames(carNames1))
+        assertThatThrownBy(() -> inputValidator.validateCarNames(playerInput1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.WRONG_CAR_NAME_LENGTH_MESSAGE.get());
-        assertThatThrownBy(() -> inputValidator.validateCarNames(carNames2))
+        assertThatThrownBy(() -> inputValidator.validateCarNames(playerInput2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.WRONG_CAR_NAME_LENGTH_MESSAGE.get());
+    }
+
+    @Test
+    @DisplayName("시행 횟수 입력이 정수형태가 아닌 경우")
+    void testValidateNumberOfAttempt1() {
+        String playerInput1 = "숫자아닌입력";
+        String playerInput2 = "0012";
+
+        assertThatThrownBy(() -> inputValidator.validateNumberOfAttempt(playerInput1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
+        assertThatThrownBy(() -> inputValidator.validateNumberOfAttempt(playerInput2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
+    }
+
+    @Test
+    @DisplayName("시행 횟수가 1회 이상이 아닌 경우")
+    void testValidateNumberOfAttempt2() {
+        String playerInput = "0";
+
+        assertThatThrownBy(() -> inputValidator.validateNumberOfAttempt(playerInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
     }
 }
