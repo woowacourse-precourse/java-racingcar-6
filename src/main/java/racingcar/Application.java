6125fd8ea573664d7d1701900e9f23c86207d38a
sonjeongdong <sjd0219@gmail.com>
2023-10-30 19:35:21 +0900

    Feat: 자동차 이름 입력에 대하여 입력 라인식 예외 사항 처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,28 +1,43 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println(saveCarName(getCarNames()));
-
+        System.out.println(checkInputLineCarNames(getCarNames()));
     }
 
-    public static void printInputCarName() {
+    public static void printInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
     public static String getCarNames() {
-        printInputCarName();
-        String input = Console.readLine();
-        return input;
-    }
-    public static List<String> saveCarName(String names) {
-        List<String> carNames = Arrays.asList(names.split(","));
+        printInputCarNames();
+        String carNames = Console.readLine();
         return carNames;
     }
+    public static List<String> saveCarNames(String carNames) {
+        List<String> cars = Arrays.asList(carNames.split(","));
+        return cars;
+    }
+    public static String checkInputLineCarNames(String inputCarNames) {
+        checkStartWithInputLine(inputCarNames);
+        checkLastIndexInputLine(inputCarNames);
+        return inputCarNames;
+    }
+    public static void checkStartWithInputLine(String inputCarNames) {
+        if(inputCarNames.startsWith(",")){
+            throw new IllegalArgumentException();
+        }
+    }
+    public static void checkLastIndexInputLine(String inputCarNames) {
+        if(inputCarNames.lastIndexOf(",") == inputCarNames.length()-1){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
