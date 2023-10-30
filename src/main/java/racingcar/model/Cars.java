6f7698960e10967b3520be38b3f88eb41f48b8a6
Han <yulmam1@naver.com>
2023-10-30 14:57:37 +0900

    style(Cars) : 변수명과 함수명 변경
    carsName에서 carNames로 변경
    carsScore에서 carScores로 변경
    변수명과 관련된 함수명도 같이 변경

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -4,39 +4,51 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class Cars {
-    List<String> carsName;
-    Map<String, Integer> carsScore;
+    private List<String> carNames;
+    private Map<String, Integer> carScores;
 
     private static final int CAR_NAME_MAX_LENGTH = 5;
 
     public Cars() {
-        carsScore = new HashMap<String, Integer>();
+        carScores = new HashMap<String, Integer>();
     }
 
     public void addCar(String carsName){
-        this.carsName = Arrays.asList(carsName.split(","));
-        validate(this.carsName);
-        this.carsName.forEach(carName -> carsScore.put(carName, 0));
+        this.carNames = Arrays.asList(carsName.split(","));
+        validate(this.carNames);
+        this.carNames.forEach(carName -> carScores.put(carName, 0));
     }
 
+    public List<String> getCarNames() {
+        return carNames;
+    }
+
+    public Map<String, Integer> getCarScores() {
+        return carScores;
+    }
+
+    private void increaseCarScore(String carName) {
+        int score = carScores.get(carName) + 1;
+        carScores.put(carName, score);
+    }
 
     private void validate(List<String> carsName) {
-        validateCarsNameLength(carsName);
-        validateCarsNameNotNull(carsName);
-        validateCarsNameNotDuplication(carsName);
+        validateCarNamesLength(carsName);
+        validateCarNamesNotNull(carsName);
+        validateCarNamesNotDuplication(carsName);
     }
 
-    private void validateCarsNameLength(List<String> carsName) {
+    private void validateCarNamesLength(List<String> carsName) {
         if(carsName.stream().anyMatch(carName -> carName.length() > CAR_NAME_MAX_LENGTH))
             throw new IllegalArgumentException("자동차 이름의 길이가 5를 넘어갔습니다.");
     }
 
-    private void validateCarsNameNotNull(List<String> carsName) {
+    private void validateCarNamesNotNull(List<String> carsName) {
         if(carsName.stream().anyMatch(carName -> carName == null))
             throw new IllegalArgumentException("자동차의 이름이 null 입니다.");
     }
 
-    private void validateCarsNameNotDuplication(List<String> carsName) {
+    private void validateCarNamesNotDuplication(List<String> carsName) {
         HashSet<String> Set = new HashSet<>(carsName);
 
         if (Set.size() < carsName.size()) {
