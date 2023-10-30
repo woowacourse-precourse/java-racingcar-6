0568cbaefab8dc34bda1586e7633be848ff1d5d8
박승찬 <seungchan141414@gmail.com>
2023-10-30 18:09:20 +0900

    [Test(racing)] Utill Test

---
+++ src/test/java/racingcar/utill/UtillTest.java
@@ -0,0 +1,83 @@
+package racingcar.utill;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class UtillTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @DisplayName("숫자 변환 테스트")
+    @Test
+    void parseInteger() {
+
+    }
+
+    @DisplayName("string을 regex의 기준으로 구분을 하여 배열로 반환 테스트")
+    @Test
+    void makeSplitArrFromString() {
+
+    }
+
+    @DisplayName("print new Line 테스트")
+    @Test
+    void printNewLine() {
+        // given
+
+        // when
+
+        // than
+    }
+
+    @DisplayName("print 메서드 테스트")
+    @Test
+    void print() {
+        // given
+
+        // when
+
+        // than
+    }
+
+    @DisplayName("배열을 리스트로 만들어준다. 테스트")
+    @Test
+    void makeListFromArr() {
+        // given
+
+        // when
+
+        // than
+    }
+
+    @DisplayName("num_1 과 num_2의 숫자가 같으면 true 테스트")
+    @Test
+    void isSameNum() {
+        // given
+
+        // when
+
+        // than
+    }
+
+    @DisplayName("값이 1이상인지 확인한다. 테스트")
+    @Test
+    void valueGreaterThanEqualOne() {
+        // given
+
+        // when
+
+        // than
+    }
+}
