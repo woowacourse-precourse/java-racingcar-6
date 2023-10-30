e0a3c2ac172eff725731c5d1d32641d38da55f94
lotuxsoo <tndus5383@gmail.com>
2023-10-30 10:56:15 +0900

    feat: InputValidator에서 테스트를 기반으로 사용자 입력 유효성 검증

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,11 +5,18 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     public static void validateCarsFormat(String input) {
-
+        // 정규식 패턴: 문자 또는 쉼표 외의 문자가 하나이상 있는지 확인
+        Pattern pattern = Pattern.compile("[^a-zA-Z,]+");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            throw new IllegalArgumentException("[ERROR] 입력에 허용되지 않는 문자가 포함되어있어요.");
+        }
     }
 
     public static void validateCarLength(String input) {
-
+        if (input.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하로 작성해주세요.");
+        }
     }
 }
 
