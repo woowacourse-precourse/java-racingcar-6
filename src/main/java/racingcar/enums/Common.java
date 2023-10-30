a3a0ef2cdc7ac477ad82776e3c6663542e65c042
Isaac Han <isaachan233@gmail.com>
2023-10-30 10:54:23 +0900

    refactor: 공통적으로 쓰인다는 의미로 이름 변경

--- src/main/java/racingcar/enums/Common.java
+++ src/main/java/racingcar/enums/Common.java
@@ -1,16 +1,16 @@
-package racingcar.constants;
+package racingcar.enums;
 
-public enum Constants {
+public enum Common {
     STANDARD_NUMBER(4), MIN_NUMBER(0), MAX_NUMBER(9), MAX_LENGTH(5), SEPARATOR(",");
 
     private int intValue;
     private String stringValue;
 
-    private Constants(int value) {
+    private Common(int value) {
         this.intValue = value;
     }
 
-    private Constants(String value) {
+    private Common(String value) {
         this.stringValue = value;
     }
 
