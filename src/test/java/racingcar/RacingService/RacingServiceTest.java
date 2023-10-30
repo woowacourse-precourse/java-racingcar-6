26e6168bb343a9ad55a4df7bebd93c7e4dab65f5
Maljaaa <Maljaaa@github.com>
2023-10-30 15:12:51 +0900

    test(RacingServiceTest): 분리된 레이싱할 자동차 등록 기능 테스트

--- src/test/java/racingcar/RacingService/RacingServiceTest.java
+++ src/test/java/racingcar/RacingService/RacingServiceTest.java
@@ -2,6 +2,8 @@ package racingcar.RacingService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.utils.RandomNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,7 +27,7 @@ class RacingServiceTest {
     @Test
     public void 자동차_이름_세팅_테스트() throws Exception {
         //given
-        List<String> expected = new ArrayList<>(Arrays.asList("car1", "car2", "car3"));
+        List<String> expected = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
 
         doReturn(expected).when(racingService).getCarNameList();
 
@@ -50,6 +52,28 @@ class RacingServiceTest {
 
         //then
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void 레이싱할_자동차_등록() throws Exception {
+        //given
+        String expectedPobi = "pobi";
+        String expectedWoni = "woni";
+        String expectedJun = "jun";
+
+        List<String> carNames = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
+        doReturn(carNames).when(racingService).getCarNameList();
+
+        racingService.readyCarName();
+
+        //when
+        racingService.readyCars();
+
+        //then
+
+        assertEquals(expectedPobi, racingService.getCars().get(0).getName());
+        assertEquals(expectedWoni, racingService.getCars().get(1).getName());
+        assertEquals(expectedJun, racingService.getCars().get(2).getName());
     }
 
 }
