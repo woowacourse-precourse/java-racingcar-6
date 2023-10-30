0c4c052f6e356793d8014fed62de3ae4740a12c8
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-31 00:27:27 +0900

    feat: 시도횟수에 대한 예외 메세지 추가

--- src/main/java/racingcar/util/VerificationMessage.java
+++ src/main/java/racingcar/util/VerificationMessage.java
@@ -4,4 +4,5 @@ public class VerificationMessage {
     public static final String IS_NOT_NUMBER = "숫자만 입력해 주세요.";
     public static final String OVER_SIZE = "이름은 5자 이하로 입력해 주세요.";
     public static final String DUPLICATE_NAME = "이미 입력한 자동차 이름 입니다.";
+    public static final String NOT_ZERO = "시도 횟수는 1 이상이어야 합니다.";
 }
