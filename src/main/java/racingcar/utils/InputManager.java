46ccb36d13e52192120dc57004d5bdcd75f9c713
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-30 23:42:23 +0900

    InputManager 구현 수정

--- src/main/java/racingcar/utils/InputManager.java
+++ src/main/java/racingcar/utils/InputManager.java
@@ -1,10 +1,12 @@
 package racingcar.utils;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
 
-    public int inputNumber(String numString){
-        Integer number;
+    public int Number(){
+        String numString = Console.readLine();
+        Integer number = null;
         try {
              number = Integer.parseInt(numString);
         } catch (NumberFormatException e) {
@@ -12,5 +14,20 @@ public class InputManager {
         }
         return number;
     }
+    //todo validator 분리
+
+    public String Cars() {
+        String cars = Console.readLine();
+        String[] carNames = cars.split(",");
+        for (String name:
+             carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return cars;
+    }
+
+    //todo validator 분리
 
 }
