60f2d7c006eb966c8c544fd2bf523c272e00c0f9
kmw2378 <rlarla677@gmail.com>
2023-10-30 16:06:15 +0900

    feat(racingcar): Car 클래스에 1부터 9까지 랜덤 수 생성 및 랜덤 수에 따라 거리를 증가시키는 메소드 추가 1부터 9까지 랜덤 수를 발생시키고 이 값이 4 이상인 경우 거리를 증가시키는 메소드를 추가하였습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,11 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import static racingcar.config.GameConfig.INITIAL_DISTANCE;
+import static racingcar.config.GameConfig.MAX_RANDOM_VALUE;
+import static racingcar.config.GameConfig.MIN_RANDOM_VALUE;
+import static racingcar.config.GameConfig.MIN_TO_MOVE;
 
 public class Car {
     private final String name;
@@ -9,5 +14,16 @@ public class Car {
     public Car(final String name) {
         this.name = name;
         this.distance = INITIAL_DISTANCE;
+    }
+
+    public void move() {
+        final int randomNumber = createRandomNumber();
+        if (randomNumber >= MIN_TO_MOVE) {
+            distance++;
+        }
+    }
+
+    private int createRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
     }
 }
