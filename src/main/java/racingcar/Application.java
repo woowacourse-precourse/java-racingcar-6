eaeb20ebc4f1d0f6f1d46d2b0e5c7007c502f03d
Hooni07 <gkxogns21@gmail.com>
2023-10-30 14:02:04 +0900

    feat: make racing() function and additional function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -29,27 +29,78 @@ class Car {
     }
 }
 
-public class Application {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        // Implement input
-        // 경주 할 자동차 이름 입력
-        String racerNames = scanner.nextLine();
-        // ","을 기준으로 하여 각 경주 참가자들을 Array에 임시 저장
-        String[] racerArray = racerNames.split(",");
-        // racer들의 이름을 List<Stirng>에 저장
-        List<String> racerList = new ArrayList<>();
+
+public class Application {
+    private Scanner scanner;
+    List<Car> racerList = new ArrayList<>();
+
+    public void checkNamelength(String[] racerArray){
         for (int i = 0; i < racerArray.length; i++) {
             // name의 길이가 5를 넘어가는 경우 -> 예외 발생
             if (racerArray[i].length() > 5) {
                 throw new IllegalArgumentException();
             }
-            racerList.add(racerArray[i]);
+            racerList.add(new Car(racerArray[i]));
         }
+    }
+
+    public int findMaxPosition(){
+        int maxPosition = 0;
+        for (Car car : racerList) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
+
+
+    public void racing() {
+        scanner = new Scanner(System.in);
+        // Implement input
+        // 경주 할 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racerNames = scanner.nextLine();
+        // ","을 기준으로 하여 각 경주 참가자들을 Array에 임시 저장
+        String[] racerArray = racerNames.split(",");
+        checkNamelength(racerArray);    // racer들의 이름을 List<Stirng>에 저장
         // 시도할 횟수 입력
-        int num = scanner.nextInt();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = scanner.nextInt();
 
+        System.out.println("실행 결과");
+        for (int i=0; i < tryCount; i++){
+            for (Car car : racerList) {
+                car.move();
+                System.out.printf("%s : ", car.getName());
+                for (int j = 0; j < car.getPosition(); j++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
 
+        // 제일 멀리 간 position 확인
+        int maxPosition = findMaxPosition();
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : racerList) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        System.out.println(maxPosition);
+        System.out.print("최종 우승자: ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i));
+            if (i < winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Application race = new Application();
+        race.racing();
+        race.scanner.close(); // 프로그램 종료 시 scanner를 닫아줍니다.
     }
 }
