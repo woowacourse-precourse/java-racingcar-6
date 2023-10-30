e0a61add3257be0f045ee7c0be697232ac5694cf
minsub <chrkb1569@naver.com>
2023-10-31 01:28:58 +0900

    feat : 게임 결과를 출력한다.

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -6,6 +6,7 @@ import racingcar.util.NumberFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // 게임을 진행하기 위한 클래스
 public class GameService {
@@ -14,7 +15,15 @@ public class GameService {
 
         for(int i = 0; i < userInput.frequency(); i++) {
             moveForward(carList);
+            printGameResult(carList);
         }
+
+        int maxValue = getMaxValue(carList);
+
+        String winnerName = getWinnerName(carList, maxValue);
+
+        System.out.print("최종 우승자 : ");
+        System.out.println(winnerName);
     }
 
     // 사용자가 입력한 값을 토대로 Car Entity List를 생성
@@ -36,5 +45,28 @@ public class GameService {
             int randomNumber = NumberFactory.getRandomNumber();
             car.moveForward(randomNumber);
         }
+    }
+
+    // 게임을 진행 결과를 출력
+    private void printGameResult(List<Car> carList) {
+        System.out.println("실행 결과");
+        for(Car car : carList) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
+    // 가장 멀리 간 자동차의 위치를 반환
+    private int getMaxValue(List<Car> carList) {
+        return carList.stream()
+                .map(Car::getLocation)
+                .max(Integer::compare).get();
+    }
+
+    // 우승한 자동차의 이름을 반환, 우승한 자동차가 여러 대 존재하는 경우 ','를 통해 여러 대의 자동차 이름을 반환
+    private String getWinnerName(List<Car> carList, int maxValue) {
+        return carList.stream().filter(car -> car.getLocation() == maxValue)
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", "));
     }
 }
