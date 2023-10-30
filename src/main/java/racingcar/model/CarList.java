e10747d1c4f18452babab3455f46567e2e85b126
jugpark <hjjk0721@gmail.com>
2023-10-30 13:59:16 +0900

    feat: setup carlist function

--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -1,4 +1,23 @@
 package racingcar.model;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.views.SystemMessage;
+
 public class CarList {
+    private List<String> carList;
+
+    SystemMessage systemMessage = new SystemMessage();
+
+    public List<String> getCarList() {
+        return this.carList;
+    }
+
+    public List<String> setCarList() {
+        return this.convertToList(systemMessage.getCarList());
+    }
+
+    private List<String> convertToList(String input) {
+        return Arrays.asList(input.split(","));
+    }
 }
--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,4 +1,13 @@
 package racingcar.service;
 
+import java.util.List;
+import racingcar.model.CarList;
+
 public class GameService {
+    CarList carList = new CarList();
+
+    public void runGame() {
+        List<String> A = carList.setCarList();
+        System.out.println(A);
+    }
 }
