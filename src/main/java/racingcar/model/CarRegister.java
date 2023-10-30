0fd5c91894cfc20c9351ee89ab2ce7db340c4f8c
IM-GYURI <kgulr0517@gmail.com>
2023-10-30 20:34:22 +0900

    Feat: 추가 CarRegister 클래스

---
+++ src/main/java/racingcar/model/CarRegister.java
@@ -0,0 +1,16 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRegister {
+    private final List<String> carNames;
+
+    public CarRegister() {
+        this.carNames = new ArrayList<>();
+    }
+
+    public void registerCar(String carName) {
+        carNames.add(carName);
+    }
+}
