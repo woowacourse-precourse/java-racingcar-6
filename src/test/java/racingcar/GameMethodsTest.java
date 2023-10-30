7f4f4f45292edc0b376fa261d2bc45724931582c
Jihye Lee <zerom980706@gmail.com>
2023-10-30 17:57:04 +0900

    test(gameMethods): make a test testing winner deciding feature

--- src/test/java/racingcar/GameMethodsTest.java
+++ src/test/java/racingcar/GameMethodsTest.java
@@ -2,9 +2,11 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.Index.atIndex;
+import static racingcar.GameMethods.decideWinner;
 import static racingcar.GameMethods.goOrStop;
 import static racingcar.GameMethods.makeRandNum;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class GameMethodsTest {
@@ -32,4 +34,15 @@ class GameMethodsTest {
                 .contains("-", atIndex(2));
     }
 
+    @Test
+    void decideWinnerTest() {
+        String[] movedDistance = {"---", "----", "", "----"};
+        String[] carNames = {"do", "re", "mi", "fa"};
+        ArrayList<String> winningCandidate = decideWinner(movedDistance, carNames);
+
+        assertThat(winningCandidate)
+                .contains("re", atIndex(0))
+                .contains("fa", atIndex(1))
+                .hasSize(2);
+    }
 }
