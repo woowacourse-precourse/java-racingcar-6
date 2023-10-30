0e4c8494e45458ba39f6f7466e78b7e6f7574eb6
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-31 00:06:39 +0900

    feat(game): 경주에 참여하는 자동차를 주입받는 init 메서드 생성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,9 +1,11 @@
 package racingcar;
 
+import java.util.List;
 import racingcar.view.Announcer;
 import racingcar.view.Viewer;
 
 public class Game {
+    private List<Car> carList;
     private final Announcer announcer;
     private final CarGenerator carGenerator;
     private final RandomNumberGenerator randomNumberGenerator;
@@ -17,4 +19,9 @@ public class Game {
         this.viewer = viewer;
     }
 
+    public void init() {
+        announcer.announceToInputCarName();
+        carList = carGenerator.generate(viewer.inputCarNames());
+        announcer.announceToInputTryCount();
+    }
 }
