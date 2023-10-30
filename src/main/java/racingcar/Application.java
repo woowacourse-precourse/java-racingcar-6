c11ade7ff412f1dc25f0971f03387eca24c7d6ab
parkhyejin <vpfmti227@gmail.com>
2023-10-30 23:59:01 +0900

    자동차경주게임완성본

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ");
+        String[] carNames = scanner.nextLine().split(",");
+
+        System.out.print("시도할 회수는 몇회인가요? ");
+        int rounds = scanner.nextInt();
+
+        Race race = new Race(carNames, rounds);
+        race.start();
     }
 }
