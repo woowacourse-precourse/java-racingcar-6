6bb854d666632aea453c0b8bdf7291e238356758
Dowon <3120kang@gmail.com>
2023-10-30 21:02:14 +0900

    Feat(Car): 이름에 대한 예외처리 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class Car {
 
+    private static final int MAX_NAME_SIZE=5;
     private String name;
     private String distance="";
 
     public Car(String name){
+        validateName(name);
         this.name=name;
     }
 
@@ -28,6 +30,15 @@ public class Car {
 
     public String getName(){
         return name;
+    }
+
+    public void validateName(String name){
+        if(name.length()>MAX_NAME_SIZE){
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
+        }
+        if(name.length()==0){
+            throw new IllegalArgumentException("자동차의 이름은 최소 1자 이상 입력해야 합니다.");
+        }
     }
 
     public void clear(){
