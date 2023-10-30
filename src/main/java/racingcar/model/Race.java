f548d62649ee053da0995e1c21c035c7b95c68e7
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 00:27:09 +0900

    feat: add race constructor

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,4 +1,29 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Race {
+
+    private final int totalTrial;
+    private int currentTrial;
+    private List<Car> carList;
+
+    public Race(int totalTrial, List<String> carNameList) {
+        this.totalTrial = totalTrial;
+        this.currentTrial = 0;
+        this.carList = initCarList(carNameList);
+
+    }
+
+    public List<Car> initCarList(List<String> carNameList) {
+        List<Car> carList = new ArrayList<>();
+        Car car;
+        for (String carName : carNameList) {
+            car = new Car(carName);
+            carList.add(car);
+        }
+        return carList;
+    }
+
 }
