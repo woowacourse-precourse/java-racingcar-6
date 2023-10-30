2de08522c17426f28b84e0aeed2d3b443cda2ab1
pjm2476 <pjm2476@gmail.com>
2023-10-31 00:21:16 +0900

    feat: 실행 결과 출력 안내 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,8 @@ public class OutputView {
 
     private static final String ENTER_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ENTER_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ONE_STEP = "-";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
     public static void enterCarNames() {
         System.out.println(ENTER_CAR_NAME_MESSAGE);
@@ -13,4 +15,7 @@ public class OutputView {
         System.out.println(ENTER_ATTEMPTS_MESSAGE);
     }
 
+    public static void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
 }
