3d4b025e9798966c4e444c9fa14189b34287332b
gyu_hyeok <jjookki33@gmail.com>
2023-10-31 01:47:17 +0900

    feat:String 배열 리스트로 변환 기능 구현

---
+++ src/main/java/racingcar/service/ListConvert.java
@@ -0,0 +1,11 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListConvert {
+    public List<String> convertToList(String[] args){
+        return Arrays.asList(args);
+    }
+}
