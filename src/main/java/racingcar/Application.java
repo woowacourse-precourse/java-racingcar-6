5445e0889c5d81ccf26c8b38c5f9d5c277aa4a41
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-30 16:54:22 +0900

    feat : Main - 전체 기능 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = InputManager.getCars();
+        int trials = InputManager.getTrials();
+        Race race = new Race(cars, trials);
+
+        race.executeRace();
+
+        OutputManager.displayWinners(race.getWinners());
     }
 }
