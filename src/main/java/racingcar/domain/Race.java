995677b3a7fea751d4f58d21bae2a4b010769887
고동환\eurdr <eurdream98@naver.com>
2023-10-30 18:06:43 +0900

    feat($Race) : Add numberOfGameRound method
    -시도할 횟수를 묻는 문구 출력
    -사용자가 시도할 횟수를 입력
    -게임의 횟수를 return

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Race {
+    public static Integer numberOfGameRound(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+        Integer countOfGame = Integer.valueOf(Console.readLine());
+        return countOfGame;
+    }
+
 }
