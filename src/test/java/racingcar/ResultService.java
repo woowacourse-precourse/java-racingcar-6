eae26b5e2aa714dab2c3e5983e3aaac7ff13d4aa
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-10-30 12:07:58 +0900

    feat: HashMap을 돌면서 자동차당 전진 횟수를 "-"를 이용해 출력하는 기능 구현

--- src/test/java/racingcar/ResultService.java
+++ src/test/java/racingcar/ResultService.java
@@ -1,4 +1,23 @@
 package racingcar;
 
 public class ResultService {
+    public static void printNumberOfMovingForward(Cars cars) {
+        for (Object car : cars.keySet()) {
+            System.out.printf("%s : ", car);
+            int numberOfMovingForward = (int) cars.get(car);
+            printDashes(numberOfMovingForward);
+        }
+    }
+
+    public static void printDashes(int numberOfMovingForward) {
+        if (numberOfMovingForward == 0) {
+            System.out.print("\n");
+        } else {
+            for (int i = 1; i <= numberOfMovingForward; i++) {
+                System.out.print("-");
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
