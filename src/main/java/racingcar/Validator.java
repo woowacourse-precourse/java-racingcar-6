78c970a6da30a2eb4f77f459d0ebff2abaf86089
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 14:29:49 +0900

    feat: [Validator] 자동차 이름 입력값이 영문자,한글,쉼표로만 이루어져있는지 기능

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -7,10 +7,7 @@ import java.util.Set;
 public class Validator {
 
     public void validateInput(String input) {
-        if (input.length() > 10000000) {
-            throw new IllegalArgumentException("입력값이 너무 깁니다. 입력값의 길이는 최대 1000만자 입니다.");
-        }
-        if (input.matches("^[가-힣a-zA-Z,]+$")) {
+        if (!input.matches("^[가-힣a-zA-Z,]+$")) {
             throw new IllegalArgumentException("자동차 이름을 영문자,한글로만 구성하고 쉼표로만 구분해주세요.");
         }
     }
