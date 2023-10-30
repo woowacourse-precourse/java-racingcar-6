30cffc1a24180d35b0b7ae393aecc2bbe712cabf
elyudwo <dudqk9696@naver.com>
2023-10-30 14:29:30 +0900

    [Feat] : 차수별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,8 +1,11 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
 import racingcar.util.Utils;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.stream.IntStream;
 
 
 public class RaceController {
@@ -15,6 +18,15 @@ public class RaceController {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.utils = new Utils();
+    }
+
+    private void startMove(Cars cars, int tryNumber) {
+        outputView.printExecutionResult();
+        IntStream.range(0, tryNumber)
+                .forEach(i -> {
+                    cars.moveCars();
+                    outputView.printCarDistanceAll(cars.getCars());
+                });
     }
 
 }
