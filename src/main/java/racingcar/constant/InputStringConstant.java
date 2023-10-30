190fc84c1e49f3a86921c03122416afb220dd1fc
Bosub Kim <grapeflavoredapple@gmail.com>
2023-10-30 23:00:37 +0900

    feat: 문자열 상수 정의
    - Input, Output 에서 사용할 문자열을 상수화 시킴

--- src/main/java/racingcar/constant/InputStringConstant.java
+++ src/main/java/racingcar/constant/InputStringConstant.java
@@ -1,4 +1,18 @@
 package racingcar.constant;
 
 public enum InputStringConstant {
+
+    GET_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    GET_ROUND_NUM("시도할 회수는 몇회인가요?"),
+    BLANK(" ");
+
+    private String value;
+
+    InputStringConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
--- src/main/java/racingcar/constant/OutputStringConstant.java
+++ src/main/java/racingcar/constant/OutputStringConstant.java
@@ -1,4 +1,18 @@
 package racingcar.constant;
 
 public enum OutputStringConstant {
+
+    INIT_SHOW_RESULT("실행 결과"),
+    SHOW_WINNER("최종 우승자 : "),
+    BLANK(" ");
+
+    private String value;
+
+    OutputStringConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
