b63cf087479e5b9e759d0e4fca921d311545460e
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 05:38:11 +0900

    chore : Car에 있던 기능 Cars로 옮김

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,11 +9,4 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CarTest {
 
-    @Test
-    void 이름() {
-        Cars car = new Cars();
-        List name = car.parsingCarName("pobi,juni,jun");
-        assertThat(name).isEqualTo(Arrays.asList("pobi", "woni", "jun"));
-        System.out.println(name);
-    }
 }
