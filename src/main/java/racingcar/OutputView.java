837f118937c067daa58b8c40fef26e13553bc4ff
aammddkkzxc <ideal0427@naver.com>
2023-10-30 18:55:31 +0900

    feat: 차수별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,9 +1,16 @@
 package racingcar;
 
-public class OutputView {
-    public void printResult() {
+import java.util.List;
 
+public class OutputView {
+    public static final String COLON = " : ";
+    public static final String ENTER = "/n";
+
+    public static void printResult(Car car, List<String> distance) {
+        System.out.println(car.getName() + COLON + distance);
+        System.out.println(ENTER);
     }
+
     public void printWinners() {
 
     }
