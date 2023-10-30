268cd33f3384f4dd3c8a8a8fd9e85bcf27f46848
고동환\eurdr <eurdream98@naver.com>
2023-10-30 21:13:43 +0900

    feat($Application) : Add trycatch
    -makeRacingCarList에 예외처리를 해놓아 만약 입력한 자동차의 이름이 5글자 이상이면 IllegalArgumentException을 catch하여 예외처리할 수 있도록 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,10 @@ import racingcar.domain.Race;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-
+        try {
+            Race.againRacingByRandomNumber();
+        }catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
     }
 }
