95f191b9db5417384c1d94ea012109699a3a36d5
junghunim07 <dlawndgns9@naver.com>
2023-10-30 23:10:34 +0900

    feat(Race): makeCars 메소드 static 수정

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -6,11 +6,7 @@ import java.util.List;
 public class Race {
     static List<Car> carRace = new ArrayList<>();
 
-    public Race(List<String> list) {
-        makeCars(list);
-    }
-
-    private void makeCars(List<String> list) {
+    static void makeCars(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             Car car = new Car(list.get(i));
             carRace.add(car);
