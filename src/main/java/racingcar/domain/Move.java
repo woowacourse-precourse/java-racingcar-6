a2feb1df38feb105581f80374db7e4c70037efea
seokmyungham <hellotjraud@gmail.com>
2023-10-30 09:36:08 +0900

    feat(Move): toString 메서드 오버라이드
    toString: move를 문자열로 반환하도록 오버라이딩 하였습니다

--- src/main/java/racingcar/domain/Move.java
+++ src/main/java/racingcar/domain/Move.java
@@ -14,4 +14,9 @@ public class Move {
     public void print() {
         System.out.print(move.toString());
     }
+
+    @Override
+    public String toString() {
+        return move.toString();
+    }
 }
