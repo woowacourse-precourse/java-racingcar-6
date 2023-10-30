5fa50c47923e09ed8e31f79ff797f4416b64d0a3
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 17:50:37 +0900

    feat(InputView): 예외처리 추가
    - 이름을 구분하는 기준이 쉼표가 아닌 경우 예외처리
    - 구분자를 이용해서 변환을 한 뒤, 변환한 자동차 리스트의 길이가 2 이하인 경우 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,14 +3,35 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.util.Constants;
 
 public class InputView {
     private final static String SEPARATOR = ",";
+    private final static String INVALID_SEPARATOR_MESSAGE = "자동차를 구분하는 기준은 쉼표(,)여야 합니다.";
+    private final static String INVALID_NAME_COUNT_MESSAGE = "자동차 이름은 2개 이상이어야 합니다.";
     public static List<String> inputNames() {
         OutputView.printNameInputMessage();
+
         String input = Console.readLine();
+
+        validateInputWithSeparator(input);
+
         List<String> names = splitToStringList(input);
+
+        validateInputCount(names);
+
         return names;
+    }
+    private static void validateInputWithSeparator(String input) {
+        if (!input.contains(SEPARATOR)) {
+            throw new IllegalArgumentException(INVALID_SEPARATOR_MESSAGE);
+        }
+    }
+
+    private static void validateInputCount(List<String> names) {
+        if (names.size() < Constants.MIN_INPUT_COUNT) {
+            throw new IllegalArgumentException(INVALID_NAME_COUNT_MESSAGE);
+        }
     }
 
     public static String inputRound() {
