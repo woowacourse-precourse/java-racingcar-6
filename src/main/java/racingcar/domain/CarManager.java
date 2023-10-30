618e7ab3606eb98a0d3ec85ab7847bc9d0801ebf
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-10-30 20:01:49 +0900

    Feat(CarManager): 입력값으로 자동차 이름 리스트 생성하는 기능 구현

--- src/main/java/racingcar/domain/CarManager.java
+++ src/main/java/racingcar/domain/CarManager.java
@@ -1,9 +1,21 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarManager {
     public List<String> giveName(String inputNames) {
-        return null;
+
+        Verification verification = new Verification();
+
+        List<String> cars = new ArrayList<>();
+        for (String car : inputNames.split(",")) {
+            if (verification.isLessThanFive(car)) {
+                cars.add(car);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return cars;
     }
 }
