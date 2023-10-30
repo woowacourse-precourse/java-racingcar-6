f58651921b56165f477e632d7ee0adfe66e15117
DESKTOP-UPSCALB\tkddn <thwlsdl0989@gmail.com>
2023-10-30 12:36:41 +0900

    변수 이름 변경 finalWinner -> finallWinnerComment

--- src/main/java/racingcar/Print.java
+++ src/main/java/racingcar/Print.java
@@ -8,7 +8,7 @@ public class Print {
     String startingComment = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     String askHowManyTurn = "시도할 회수는 몇회인가요?";
     String resultComment = "실행 결과";
-    String finalWinner = "최종 우승자 : ";
+    String finalWinnerComment = "최종 우승자 : ";
 
     void Score(List<Car> carList) {
         for (Car car : carList) {
@@ -22,11 +22,7 @@ public class Print {
         List<String> winnerName = gameFunction.getWinner(carList);
         String result = String.join(", ", winnerName);
 
-        System.out.print(finalWinner);
+        System.out.print(finalWinnerComment);
         System.out.println(result);
     }
-
-
-
-
 }
