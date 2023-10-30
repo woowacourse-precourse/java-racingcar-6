c3db1b4f9967a8a60977adf5eba2eef231c8e22f
jwAhn1020 <takartin@naver.com>
2023-10-30 15:14:42 +0900

    refactor : 자동차 배열 생성 및 초기화 기능  메서드화

--- src/main/java/racingcar/RacingGameStart.java
+++ src/main/java/racingcar/RacingGameStart.java
@@ -3,13 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGameStart {
-
     public static void run(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
         String[] carsList = carNames.split(",");
 
-        RacingCar[] RacingCars = new RacingCar[carsList.length];
+        RacingCar[] RacingCars = createRacingCars(carsList);
 
         for(int i = 0; i< carsList.length; i++){
             RacingCar car = new RacingCar(carsList[i].trim(),0);
@@ -33,5 +32,13 @@ public class RacingGameStart {
         PrintWinner pW = new PrintWinner(RacingCars);
         System.out.print(pW.winnerListPrint());
 
+    }
+
+    private static RacingCar[] createRacingCars(String[] carNames) {
+        RacingCar[] racingCars = new RacingCar[carNames.length];
+        for (int i = 0; i < carNames.length; i++) {
+            racingCars[i] = new RacingCar(carNames[i].trim(), 0);
+        }
+        return racingCars;
     }
 }
