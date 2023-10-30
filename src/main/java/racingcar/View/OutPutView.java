b902f46417b22aeabe76b22736fa242c47e8473d
sami355-24 <ddung355@gmail.com>
2023-10-30 10:57:11 +0900

    fix(View): outputView에서 형식에 맞게 출력하도록 수정
    - "실행결과" 출력하도록 수정
    - 라운드별 결과 출력하고 개행문자 출력하도록 수정
    - "최종 우승자 : "라는 문구 출력하게끔 수정
    - OutputView에서만 쓰이는 메서드의 접근제어자 private로 수정

--- src/main/java/racingcar/View/OutPutView.java
+++ src/main/java/racingcar/View/OutPutView.java
@@ -5,7 +5,12 @@ import racingcar.Domain.CarGroup;
 
 public class OutPutView {
 
+    public void showPrompt() {
+        System.out.print("\n실행 결과");
+    }
+
     public void showRoundResult(CarGroup carGroup) {
+        System.out.println();
         for (int order = 1; order <= carGroup.findSize(); order++) {
             Car findCar = carGroup.findCarByOrder(order);
             System.out.println(findCar.findName() + " : " + makeIndividualResult(findCar.findPosition()));
@@ -14,7 +19,7 @@ public class OutPutView {
 
     public void showFinalWinner(CarGroup carGroup) {
         CarGroup finalWinners = carGroup.findFinalWinner();
-        System.out.print("최종 우승자 : ");
+        System.out.print("\n최종 우승자 : ");
         for (int order = 1; order <= finalWinners.findSize(); order++) {
             Car findCar = finalWinners.findCarByOrder(order);
             System.out.print(findCar.findName());
@@ -25,7 +30,7 @@ public class OutPutView {
     }
 
 
-    public String makeIndividualResult(Integer position) {
+    private String makeIndividualResult(Integer position) {
         return "-".repeat(Math.max(0, position));
     }
 }
