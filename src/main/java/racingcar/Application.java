e027bf243d7feada0f0184d263b1bdf3615fff71
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:58:29 +0900

    feat(racingcar): main 메소드에 구현한 기능 반영

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.GameManagerController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameManagerController gameManagerController = new GameManagerController();
+        gameManagerController.start();
     }
 }
