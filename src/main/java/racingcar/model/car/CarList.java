9335bb68b0fbe47756eb1b11c5b62ec24a97ba5e
IM-GYURI <kgulr0517@gmail.com>
2023-10-31 03:26:32 +0900

    Feat: moveAllCarListOnce(), getCurrentResult(), getWinners(), getMaxPosition() 함수 추가

--- src/main/java/racingcar/model/car/CarList.java
+++ src/main/java/racingcar/model/car/CarList.java
@@ -2,7 +2,9 @@ package racingcar.model.car;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.model.manager.CarMoveManager;
 import racingcar.util.CarNameValidator;
+import racingcar.util.RandomNumberGenerator;
 
 public class CarList {
     private final List<Car> carList;
@@ -19,6 +21,27 @@ public class CarList {
         CarNameValidator.validate(carNames);
     }
 
+    public void moveAllCarListOnce(CarMoveManager carMoveManager) {
+        carList.forEach(car -> car.move(carMoveManager.isMove(RandomNumberGenerator.getRandomNumber())));
+    }
 
+    public List<Car> getCurrentResult() {
+        return List.copyOf(carList);
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        return carList.stream()
+                .filter(car -> car.isSame(maxPosition))
+                .map(Car::getName)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private int getMaxPosition() {
+        return carList.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
 
 }
