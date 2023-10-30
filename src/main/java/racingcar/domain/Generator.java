6e4d2b7efa89d42755671ad70f0758898d47fb46
muyaaho <skanthsu715@gmail.com>
2023-10-30 17:32:00 +0900

    remove(racingcar): 기존 domain 디렉토리 삭제

--- src/main/java/racingcar/domain/Generator.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class Generator {
-    public int generateRandomNum() {
-        return Randoms.pickNumberInRange(1,9);
-    }
-}
--- src/main/java/racingcar/domain/Judgment.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-public class Judgment {
-    public boolean isGo(int randomNumber){
-        if (randomNumber >= 4)
-            return true;
-        return false;
-    }
-}
