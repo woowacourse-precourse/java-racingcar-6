f3d9c85bb176ddf2f10b3207d8d8f3fb3ddf85b6
stone <109290247+astrataraxia@users.noreply.github.com>
2023-10-30 23:41:28 +0900

    feat :  print car status and move
    - 입력값을 토대로 자동차의 포지션 상태가 출력되어 나오는 기능
    - Car class에 현재 포지션 상태를 출력하고, 움직이는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +18,11 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         String inputTryCount = Console.readLine();
         int tryCount = parsingTryCount(inputTryCount);
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < tryCount; i++) {
+            printCarStatus(cars);
+        }
     }
 
     private static List<Car> parsingCarNames(String inputCarNames) {
@@ -28,5 +34,20 @@ public class Application {
     private static int parsingTryCount(String inputTryCount) {
         Validate.number(inputTryCount);
         return Integer.parseInt(inputTryCount);
+    }
+
+    private static void printCarStatus(List<Car> cars) {
+        for (Car car : cars) {
+            car.move(Randoms.pickNumberInRange(0, 9));
+        }
+        printCurrentStatus(cars);
+        System.out.println();
+    }
+
+    private static void printCurrentStatus(List<Car> cars) {
+        cars.forEach(car -> {
+            System.out.print(car.getName() + ": ");
+            System.out.println(car.getPositionString());
+        });
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,10 +3,27 @@ package racingcar;
 public class Car {
 
     private final String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this.name = name;
     }
 
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPositionString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
 }
