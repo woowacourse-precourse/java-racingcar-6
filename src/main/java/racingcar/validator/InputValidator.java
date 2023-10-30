ee716782a260738daa8b689de1a99db4865ab05e
geunsu <geunsu23@naver.com>
2023-10-30 09:58:41 +0900

    add validator

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,24 @@
+package racingcar.validator;
+
+import static racingcar.model.Constants.MAX_NAME_LENGTH;
+
+public class InputValidator {
+    public static void checkRepetitionIsNumber(String userRepeat) throws IllegalArgumentException {
+        if(!userRepeat.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    public static void checkRepetitionIsPositive(String userRepeat) throws IllegalArgumentException {
+        if(Integer.parseInt(userRepeat) < 0) {
+            throw new IllegalArgumentException("양수만 입력해주세요.");
+        }
+    }
+
+    public static void checkNameLength(String carName) throws IllegalArgumentException {
+        if (carName.isEmpty() || carName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("5자 이하의 이름만 입력하세요.");
+        }
+    }
+
+}
