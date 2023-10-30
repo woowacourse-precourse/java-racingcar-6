3c3b9dc39f900e6aea130d0badd0c5ca434459f9
Sanghwi Lee <sanghwi47@naver.com>
2023-10-30 11:05:39 +0900

    refactor(CheckName) error message를 상수처리, chore(CheckName) 불필요한 주석 제거

--- src/main/java/racingcar/function/CheckName.java
+++ src/main/java/racingcar/function/CheckName.java
@@ -1,6 +1,10 @@
 package racingcar.function;
 
 public class CheckName {
+    private static final String ERROR_NULL_OR_EMPTY = "자동차 이름을 입력해 주세요.";
+    private static final String ERROR_LENGTH = "이름을 4글자 이내로 입력해 주세요: ";
+    private static final String ERROR_INVALID_CHARACTERS = "영어로 입력해 주세요: ";
+
     public static void validate(String inputNames) {
         nameIsNotNull(inputNames);
         correctNameCheck(inputNames);
@@ -8,7 +12,7 @@ public class CheckName {
 
     private static void nameIsNotNull(String inputNames) {
         if (inputNames == null || inputNames.trim().isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
+            throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY);
         }
     }
 
@@ -22,37 +26,13 @@ public class CheckName {
 
     private static void lengthCheck(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름을 4글자 이내로 입력해 주세요: " + name);
+            throw new IllegalArgumentException(ERROR_LENGTH + name);
         }
     }
 
     private static void nameCharacterCheck(String name) {
         if (!name.matches("[a-z]+")) {
-            throw new IllegalArgumentException("영어로 입력해 주세요: " + name);
+            throw new IllegalArgumentException(ERROR_INVALID_CHARACTERS + name);
         }
     }
 }
-
-
-//public class CheckName {
-//    public static void validate(String inputNames) {
-//        if (inputNames == null || inputNames.trim().isEmpty()) {
-//            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
-//        }
-//
-//        String[] names = inputNames.split(",");
-//        for (String name : names) {
-//            validateSingleName(name);
-//        }
-//    }
-//
-//    private static void validateSingleName(String name) {
-//        if (name.length() > 5) {
-//            throw new IllegalArgumentException("이름을 4글자 이내로 입력해 주세요: " + name);
-//        }
-//
-//        if (!name.matches("[a-z]+")) {
-//            throw new IllegalArgumentException("영어로 입력해 주세요: " + name);
-//        }
-//    }
-//}
