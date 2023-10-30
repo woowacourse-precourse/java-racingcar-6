0ee3aeaef927f8d4b1f51cd5860159b5fd955653
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-30 12:45:38 +0900

    feat: add racing game method logic

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,7 +1,37 @@
 package racingcar.controller;
 
-public class RacingGame {
-    public void start(){
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.NumberGenerator;
+import racingcar.model.RacingCar;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
+public class RacingGame {
+    private final NumberGenerator numberGenerator = new NumberGenerator();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void start(){
+        outputView.printRacingCarNameInputMessage();
+        String[] carNames = inputView.readRacingCarName();
+
+        outputView.printNumberOfTryInputMessage();
+        int numberOfTry = inputView.readNumberOfTry();
+
+        List<RacingCar> racingCars = new ArrayList<>();
+
+        for(int i = 0; i < carNames.length; i++){
+            racingCars.add(new RacingCar(carNames[i]));
+        }
+
+        for(int i = 0; i < numberOfTry; i++){
+            for(int k = 0; k < racingCars.size(); k++){
+                racingCars.get(k).moveRandom(numberGenerator.generateNumber());
+            }
+            outputView.printResult();
+        }
+
+        outputView.printWinner();
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.RacingCar;
+
 public class OutputView {
     public void printRacingCarNameInputMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -16,4 +19,6 @@ public class OutputView {
     public void printWinner(){
         System.out.println("최종 우승자 : ");
     }
+
+
 }
