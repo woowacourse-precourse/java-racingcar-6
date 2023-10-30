31b8a57451f9d7aa49ef88ada6a12bf2c7a4596e
jwAhn1020 <takartin@naver.com>
2023-10-30 15:12:13 +0900

    update : 레이싱게임 시작 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,33 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        String[] carsList = carNames.split(",");
-
-        RacingCar[] RacingCars = new RacingCar[carsList.length];
-
-        for(int i = 0; i< carsList.length; i++){
-            RacingCar car = new RacingCar(carsList[i].trim(),0);
-            car.checkNameLength(car.getName());
-            RacingCars[i] = car;
-        }
-
-        System.out.println("시도할 횟수는 몇회인가요?");
-        int num = Integer.parseInt(Console.readLine());
-
-        System.out.println();
-        System.out.println("실행 결과");
-        for(int i=0; i<num; i++){
-            for(int j=0; j< carsList.length; j++){
-                RapsResult rR = new RapsResult(RacingCars[j]);
-                System.out.println(rR.forwardOrNot());
-            }
-            System.out.println();
-        }
-
-        PrintWinner pW = new PrintWinner(RacingCars);
-        System.out.print(pW.winnerListPrint());
-
+        RacingGameStart.run();
     }
 }
