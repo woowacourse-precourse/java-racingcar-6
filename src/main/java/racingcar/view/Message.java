31358b8609529200fa86a9e263b002727b2afa56
DongHyun <kimdh0315@naver.com>
2023-10-30 15:21:22 +0900

    feat(src/main/view/Message) : define messages of userInput and Output

---
+++ src/main/java/racingcar/view/Message.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class Message {
+    /* Strings for user Input */
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_EXECUTION_NUMBER = "시도할 회수는 몇회인가요?";
+
+    /* Strings for user Output */
+    public static final String EXECUTION_RESULT = "실행 결과";
+    public static final String WINNER_NAMES = "최종 우승자 : ";
+}
