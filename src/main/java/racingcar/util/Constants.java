73ad81a4afa4f001c2d6c9da9784c97f6c8ab573
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 17:01:33 +0900

    feat(Constants): 상수 추가
    - 최대 입력값을 의미하는 상수 추가

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public final static int MIN_INPUT_RANGE = 0;
     public final static int MAX_INPUT_RANGE = 9;
     public final static int MIN_MOVE_THRESHOLD = 4;
+    public final static int MAX_INPUT_SIZE = 5;
 }
