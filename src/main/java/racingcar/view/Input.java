0e688dc2e5cbfeccce8475303d98f11fe291d7c8
namtndus <rnrmf0721@gmail.com>
2023-10-30 23:43:00 +0900

    fix(Input): 테스트 함수 실행한 후, 발견한 오류 수정
    input에 " "가 있으면 오류 발생 이므로 '!' 제거
    carName의 길이가 6 이상일 때 오류 발생이므로 5보다 크면 오류 발생으로 수정

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -12,14 +12,14 @@ public class Input {
     public String[] InputCarName(){
         System.out.println(INIT_CAR_NAME);
         String input = Console.readLine();
-        if(!input.contains(" "))
+        if(input.contains(" "))
             throw new IllegalArgumentException("쉽표(,)를 통해 구분해 주세요.");
 
         String[] cars = input.split(",");
         if(cars.length == 1)
             throw new IllegalArgumentException("자동차는 2대 이상이 경기에 참여해야 합니다");
         for(String carName : cars){
-            if(carName.length() < 6){
+            if(carName.length()  > 5){
                 throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
             }
         }
