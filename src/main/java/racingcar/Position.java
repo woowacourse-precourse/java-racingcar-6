001f3258f44b19cd899836ec06dd829c47c09d10
choiejae <ejae8319@gmail.com>
2023-10-30 23:04:20 +0900

    feat(Position): RacingCar 의 멤버 변수 래퍼 클래스
    (Position): RacingCar 의 현재 위치를 담는 멤버 변수, int curPosition 을 wrapping 한 클래스

---
+++ src/main/java/racingcar/Position.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public record Position(int currentPosition) {
+
+    private static final String POSITION_PATTERN = "-";
+
+    public String expressPosition() {
+        return POSITION_PATTERN.repeat(currentPosition);
+    }
+
+    @Override
+    public String toString() {
+        return expressPosition();
+    }
+}
---
+++ src/test/java/racingcar/PositionTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    private static final String POSITION_PATTERN = "-";
+
+    @Test
+    void 현재_위치만큼_특정_문자로_나타낼_수_있어야_한다() {
+        // given
+        int curPosition = 3;
+        Position position = new Position(curPosition);
+
+        // when
+        String position_pattern = position.expressPosition();
+
+        // then
+        String expectedPatterns = POSITION_PATTERN.repeat(curPosition);
+        assertEquals(expectedPatterns, position_pattern);
+    }
+}
