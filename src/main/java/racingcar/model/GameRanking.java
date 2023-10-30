2a9e610091ae99c97a5465ab3cb8bccbb7a4a348
yoonjuho <dbswn990@naver.com>
2023-10-30 11:43:21 +0900

    feat(GameRanking): 게임을 완료한 후 누가 우승했는지 알 수 있다.

--- src/main/java/racingcar/model/GameRanking.java
+++ src/main/java/racingcar/model/GameRanking.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 
 public class GameRanking {
@@ -12,12 +14,24 @@ public class GameRanking {
         this.participants = participants;
     }
 
-    public Map<RacingCar, String> progressRound(){
-        for(RacingCar racingCar : participants.keySet()){
-            participants.replace(racingCar,participants.get(racingCar)
-                    + racingCar.apply((Randoms.pickNumberInRange(0,9))).getOutput());
+    public Map<RacingCar, String> progressRound() {
+        for (RacingCar racingCar : participants.keySet()) {
+            participants.replace(racingCar, participants.get(racingCar)
+                    + racingCar.apply((Randoms.pickNumberInRange(0, 9))).getOutput());
         }
         return Collections.unmodifiableMap(participants);
+    }
+
+    public List<RacingCar> getWinner() {
+        String winnerScore = participants.entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .max(Comparator.comparing(String::length)).get();
+        return participants.entrySet()
+                .stream()
+                .filter(entry -> winnerScore.length() == entry.getValue().length())
+                .map(Map.Entry::getKey)
+                .toList();
     }
 
 
--- src/test/java/racingcar/model/GameRankingTest.java
+++ src/test/java/racingcar/model/GameRankingTest.java
@@ -1,21 +1,28 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class GameRankingTest {
-    private static Map<RacingCar, String> participants;
+    private Map<RacingCar, String> participants;
 
-    @BeforeAll
-    static void setUp() {
+    @BeforeEach
+    void setUp() {
         participants = new HashMap() {{
             put(new RacingCar("pobi"), CarStatus.STOP.getOutput());
             put(new RacingCar("woni"), CarStatus.STOP.getOutput());
@@ -28,14 +35,49 @@ class GameRankingTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "woni", "jun"})
     void progressRound(String name) {
-        RacingCar racingCar = new RacingCar(name);
         GameRanking gameRanking = new GameRanking(participants);
         Map<RacingCar, String> result = gameRanking.progressRound();
+        RacingCar racingCar = new RacingCar(name);
         Assertions.assertAll(
                 () -> assertThat(result.containsKey(racingCar)).isTrue(),
                 () -> assertThat(result.get(racingCar)).isEqualTo(participants.get(racingCar))
         );
-
     }
 
+    @DisplayName("우승자가 한명인 경우 누가 우승했는지 알 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"---:pobi", "--:woni", "-:jun"}, delimiter = ':')
+    void getWinner(String score, String winner) {
+        RacingCar winnerRacingCar = new RacingCar(winner);
+        participants.put(winnerRacingCar, score);
+        GameRanking gameRanking = new GameRanking(participants);
+        List<RacingCar> result = gameRanking.getWinner();
+        Assertions.assertAll(
+                () -> assertThat(result.size()).isEqualTo(1),
+                () -> assertThat(result.get(0)).isEqualTo(winnerRacingCar)
+        );
+    }
+
+    @DisplayName("우승자가 여러명인 경우 누가 우승했는지 알 수 있다.")
+    @ParameterizedTest
+    @MethodSource("winnerData")
+    void getMultipleWinner(String score, List<RacingCar> winners) {
+        for (RacingCar winner : winners) {
+            participants.put(winner, score);
+        }
+        GameRanking gameRanking = new GameRanking(participants);
+        List<RacingCar> result = gameRanking.getWinner();
+        Assertions.assertAll(
+                () -> assertThat(result.size()).isEqualTo(winners.size()),
+                () -> assertThat(result).containsAll(winners)
+        );
+    }
+
+    static Stream<Arguments> winnerData() {
+        return Stream.of(
+                arguments("---", Arrays.asList(new RacingCar("pobi"), new RacingCar("woni"))),
+                arguments("--", Arrays.asList(new RacingCar("pobi"), new RacingCar("jun")),
+                        arguments("---", Arrays.asList(new RacingCar("pobi"), new RacingCar("jun"), new RacingCar("woni")))
+                ));
+    }
 }
