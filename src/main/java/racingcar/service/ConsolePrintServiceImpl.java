09e6710a13412883cb10fb3ba1a1bbdaf85d0df3
LimGayoung <ygy920@gmail.com>
2023-10-30 21:58:32 +0900

    refactor: 상수 public → private
    ConsolePrintService에서만 사용하는 것인데 test를 위해서 public으로 뒀었음. test에서는 String으로 검증하고 상수는 private로 변경함.

--- src/main/java/racingcar/service/ConsolePrintServiceImpl.java
+++ src/main/java/racingcar/service/ConsolePrintServiceImpl.java
@@ -4,10 +4,10 @@ import java.util.List;
 import racingcar.model.Car;
 
 public class ConsolePrintServiceImpl implements PrintService{
-    public static final String INPUT_NAME_NOTICE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_COUNT_NOTICE = "시도할 회수는 몇회인가요?";
-    public static final String RESULT_NOTICE = "실행 결과";
-    public static final String GAME_WINNER_NOTICE = "최종 우승자 : ";
+    private static final String INPUT_NAME_NOTICE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_COUNT_NOTICE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_NOTICE = "실행 결과";
+    private static final String GAME_WINNER_NOTICE = "최종 우승자 : ";
 
     @Override
     public void printInputNameNotice() {
