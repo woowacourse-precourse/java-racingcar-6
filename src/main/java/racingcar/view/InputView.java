03b20b69385fba31e7b6649f7d91b24454dddab4
SungKyum Kim <sungkyum1@naver.com>
2023-10-31 00:09:51 +0900

    feat(InputView): 경주할 자동차 이름을 입력받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,20 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Validator;
+
+import static racingcar.Validator.hasNoConsecutiveCommas;
+import static racingcar.Validator.validateNames;
+
+public class InputView {
+    public static String[] displayCarNamesInputView() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputString = Console.readLine();
+
+        if (!(hasNoConsecutiveCommas(inputString) && validateNames(inputString))) {
+            throw new IllegalArgumentException();
+        }
+
+        return inputString.split(",");
+    }
 }
