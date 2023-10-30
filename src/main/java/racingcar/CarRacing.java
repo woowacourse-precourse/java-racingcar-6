cfcf3b66781f7ecabd832111d2bcf1f14995f9de
dmswl6310 <heunji6310@gmail.com>
2023-10-30 23:43:27 +0900

    feat: 자동차 이름 입력에 대한 문구 출력

---
+++ src/main/java/racingcar/CarRacing.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class CarRacing {
+    int attempCount;
+
+    CarRacing() {
+        attempCount = 0;
+    }
+
+    void execute() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
