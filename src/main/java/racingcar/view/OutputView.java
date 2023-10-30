74464a6e7e0041338574c131e6ddd9819efc50ae
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-10-30 11:14:26 +0900

    feat (OutputView) : 위치 현황의 타이틀 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.Objects;
+import racingcar.view.printer.ConsolePrinter;
+import racingcar.view.printer.Printer;
+
+public class OutputView {
+
+    private static final String LOCATION_RESULT_TITLE = String.format("%n실행 결과");
+
+    private final Printer printer;
+
+    OutputView(Printer printer) {
+        this.printer = Objects.requireNonNull(printer);
+    }
+
+    public static OutputView createConsoleView() {
+        return new OutputView(new ConsolePrinter());
+    }
+
+    public void printLocationResultTitle() {
+        print(LOCATION_RESULT_TITLE);
+    }
+
+    private void print(String message) {
+        printer.print(message);
+    }
+}
---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.view.printer.MockPrinter;
+
+class OutputViewTest {
+
+    @Test
+    @DisplayName("위치 현황의 타이틀 출력 기능 테스트")
+    void printLocationResultTitleTest() {
+        MockPrinter printer = new MockPrinter();
+        OutputView outputView = new OutputView(printer);
+        String expectedMessage = String.format("%n실행 결과");
+
+        outputView.printLocationResultTitle();
+
+        assertThat(printer.getPrintedMessage()).isEqualTo(expectedMessage);
+    }
+}
