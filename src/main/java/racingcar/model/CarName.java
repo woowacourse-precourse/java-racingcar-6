2ef39b6a7b2a552f81b6952d49daf1accc00491f
pokycookie <0cookieboy0@gmail.com>
2023-10-30 15:14:42 +0900

    refactor: 매직넘버 및 매직리터럴 상수화

--- src/main/java/racingcar/model/CarName.java
+++ src/main/java/racingcar/model/CarName.java
@@ -2,10 +2,12 @@ package racingcar.model;
 
 public class CarName {
     private final String name;
+    private final int MAX_LENGTH = 5;
+    private final String ERROR_MESSAGE = "[Error]: 자동차 이름이 5자를 넘음";
 
     public CarName(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("[Error]: 자동차 이름이 5자를 넘음");
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         this.name = name;
     }
