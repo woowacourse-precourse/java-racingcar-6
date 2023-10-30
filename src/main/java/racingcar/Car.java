0cd9d049fb90747cc881fb9e54b649da5b83a533
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 16:50:27 +0900

    refactor: move메서드의 내부를 분리

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -15,22 +15,29 @@ public class Car {
     }
 
     public void move(List<String> carNames, int movingNumber) {
-        Map<String, Integer> carNameAndMoving =
-                carNames.stream().collect(Collectors.toMap(
-                        carName -> carName,
-                        carName -> 0)
-                );
+        Map<String, Integer> carNameAndMoving = listToMap(carNames);
 
         while (movingNumber > 0) {
-            for (String carName : carNames) {
-                if (isMovable()) {
-                    carNameAndMoving.put(carName, carNameAndMoving.getOrDefault(carName, 0) + 1);
-                }
-            }
+            moveCars(carNames, carNameAndMoving);
             output.printRacingProcess(carNameAndMoving);
             movingNumber--;
         }
         output.printRacingWinner(carNameAndMoving);
+    }
+
+    private Map<String, Integer> listToMap(List<String> carNames) {
+        return carNames.stream().collect(Collectors.toMap(
+                carName -> carName,
+                carName -> 0
+        ));
+    }
+
+    private void moveCars(List<String> carNames, Map<String, Integer> carNameAndMoving) {
+        for (String carName : carNames) {
+            if (isMovable()) {
+                carNameAndMoving.put(carName, carNameAndMoving.getOrDefault(carName, 0) + 1);
+            }
+        }
     }
 
     private boolean isMovable() {
