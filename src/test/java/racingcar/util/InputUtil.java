6e2bcc488a19deb9dbbbb09cd5ed3d9d2a36c56b
jae woo Jung <awfjol2008@naver.com>
2023-10-30 21:57:27 +0900

    feat: 자동차 이름 목록 입력값을 리스트로 바꿔주는 기능 구현

--- src/test/java/racingcar/util/InputUtil.java
+++ src/test/java/racingcar/util/InputUtil.java
@@ -1,4 +1,12 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputUtil {
+
+    public static List<String> convertInputStringToList(String input) {
+        return Arrays.stream(input.split(","))
+                .toList();
+    }
 }
