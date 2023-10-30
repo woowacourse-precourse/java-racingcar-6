36c9610b125f2ea056ca34d3b486685f09a92e12
yshong1998 <yshong1998@gmail.com>
2023-10-31 02:48:50 +0900

    Style(SystemMessage): 메세지 변경

--- src/main/java/racingcar/system/SystemMessage.java
+++ src/main/java/racingcar/system/SystemMessage.java
@@ -6,7 +6,7 @@ public enum SystemMessage {
     INPUT_REPEAT_TIMES_MESSAGE("시도할 회수는 몇회인가요?"),
     GAME_WINNER("최종 우승자 : "),
     INPUT_OUT_OF_RANGE("입력이 범위를 초과했습니다."),
-    ONLY_NUMBER_INPUT_AVAILABLE("숫자 이외에는 입력할 수 없습니다.");
+    INPUT_POSITIVE_INTEGER_ONLY("양의 정수만 입력할 수 있습니다.");
 
     private final String message;
 
