356c78b2911d6729227ace4dae5f2d39d07c98b9
LeeJaeHong <0915jhlee@daum.net>
2023-10-30 14:45:36 +0900

    feat: 자동차 이름 입력 함수 추가
    차량 이름을 입력 후 Car 객체 List에 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,13 @@
 package racingcar;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
 
@@ -8,6 +16,14 @@ public class Application {
     public void printStartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
-
+    public List<Car> inputCarNames(){
+        String s = readLine();
+        String[] carNames = s.split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
 
 }
