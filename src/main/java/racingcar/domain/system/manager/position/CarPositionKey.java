556a35e0a57003b01eca15b369a4c9b20caed78e
belljun3395 <195850@jnu.ac.kr>
2023-10-30 12:08:12 +0900

    feat(domain/system): CarPositionKey 추가

---
+++ src/main/java/racingcar/domain/system/manager/position/CarPositionKey.java
@@ -0,0 +1,25 @@
+package racingcar.domain.system.manager.position;
+
+import racingcar.domain.core.car.CarName;
+
+public class CarPositionKey implements Comparable<CarPositionKey> {
+
+    private final String key;
+
+    public CarPositionKey(CarName key) {
+        this.key = key.getName() + "_position";
+    }
+
+    public static CarPositionKey of(CarName key) {
+        return new CarPositionKey(key);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public int compareTo(CarPositionKey o) {
+        return key.compareTo(o.key);
+    }
+}
