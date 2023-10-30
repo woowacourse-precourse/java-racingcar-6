60272a231b5a215f5cdf414a75e4ad4cee41a0cc
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 14:50:48 +0900

    fix(ErrorMessages) : 시도횟수 에러메세지 변경

--- src/main/java/racingcar/consts/ErrorMessages.java
+++ src/main/java/racingcar/consts/ErrorMessages.java
@@ -5,8 +5,7 @@ public enum ErrorMessages {
     WRONG_CAR_NAME_LENGTH_MESSAGE("[ERROR] 차량 이름은 1글자 이상 5글자 이하 입니다."),
     DUPLICATION_CAR_NAME_MESSAGE("[ERROR] 차량 이름에 중복이 있습니다."),
 
-    NOT_NUMERIC_MESSAGE("[ERROR] 시도 횟수는 정수형태로 입력해주세요."),
-    NOT_MORE_THAN_MIN_NUMBER_OF_ATTEMPT("[ERROR] 시도 횟수는 1회 이상이여야 합니다.");
+    WRONG_NUMBER_OF_ATTEMPT_MESSAGE("[ERROR] 시도 횟수는 1 이상의 정수를 입력해주세요.");
 
     private final String errorMessage;
 
