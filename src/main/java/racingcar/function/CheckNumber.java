ae30847409983907082657b294c1a11034fa3fc1
Sanghwi Lee <sanghwi47@naver.com>
2023-10-30 17:12:23 +0900

    fix(CheckNumber) 시행 횟수로 공백이 입력되었을때 예외처리 구현하여 예외 수정

--- src/main/java/racingcar/function/CheckNumber.java
+++ src/main/java/racingcar/function/CheckNumber.java
@@ -3,10 +3,19 @@ package racingcar.function;
 public class CheckNumber {
     private static final String ERROR_INVALID_DIGITS = "숫자로 입력 하세요.";
     private static final String ERROR_NOT_POSITIVE_NUMBER = "0 이상의 값을 입력 하세요";
+    private static final String ERROR_IS_NULL = "값을 입력 하세요";
+
 
     public static int validate(String inputNumber) {
+        validateNotNull(inputNumber);
         validateOnlyDigits(inputNumber);
         return validatePositiveNumber(inputNumber);
+    }
+
+    private static void validateNotNull(String inputNumber) {
+        if (inputNumber == null) {
+            throw new IllegalArgumentException(ERROR_IS_NULL);
+        }
     }
 
     private static void validateOnlyDigits(String inputNumber) {
