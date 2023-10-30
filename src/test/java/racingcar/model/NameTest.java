fd5677e9f9f4219b696a398b1235e89e41f75c1b
devholic22 <hyunjoon.tech@gmail.com>
2023-10-30 19:38:27 +0900

    refactor: NameTest 작성법 교정
    - 별도의 데이터가 필요 없으므로 when 삭제
    - 별도의 데이터가 필요 없으므로 람다식에서 객체 생성을 하지 않도록 수정
    - 체이닝 개행 수정

--- src/test/java/racingcar/model/NameTest.java
+++ src/test/java/racingcar/model/NameTest.java
@@ -15,11 +15,8 @@ public class NameTest {
     @ValueSource(strings = {"  bob", "bob  ", "b o b"})
     @DisplayName("공백이 포함되면 안 된다.")
     void haveBlankExceptionTest(final String value) {
-        // given & when & then
-        assertThatThrownBy(() -> {
-            Name name = Name.from(value);
-        })
-                .isInstanceOf(IllegalArgumentException.class)
+        // when & then
+        assertThatThrownBy(() -> Name.from(value)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NAME_VALUE_BLANK_EXCEPTION.getMessage());
     }
 
@@ -27,11 +24,8 @@ public class NameTest {
     @ValueSource(strings = {"nickname", ""})
     @DisplayName("이름 길이는 1자 이상 5자 이하여야 한다.")
     void lengthExceptionTest(final String value) {
-        // given & when & then
-        assertThatThrownBy(() -> {
-            Name name = Name.from(value);
-        })
-                .isInstanceOf(IllegalArgumentException.class)
+        // when & then
+        assertThatThrownBy(() -> Name.from(value)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NAME_LENGTH_EXCEPTION.getMessage());
     }
 
@@ -39,9 +33,7 @@ public class NameTest {
     @ValueSource(strings = {"bob", "123", "john", "12a"})
     @DisplayName("공백이 없고 길이가 5자 이하면 정상 생성 된다.")
     void correctNameValueTest(final String value) {
-        // given & when & then
-        assertDoesNotThrow(() -> {
-            Name name = Name.from(value);
-        });
+        // when & then
+        assertDoesNotThrow(() -> Name.from(value));
     }
 }
