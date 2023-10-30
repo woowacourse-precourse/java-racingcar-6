b1b0f79efda1a316b30d3a9a6f1a8ca41c4abff0
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 15:22:52 +0900

    refactor : advancePosition 으로 메서드 이름 변경

--- src/main/java/model/CarPosition.java
+++ src/main/java/model/CarPosition.java
@@ -3,13 +3,14 @@ package model;
 public class CarPosition {
     private final int INITIAL_POS = 0;
     private int pos;
+    private final int DISTANCE = 1;
 
     public CarPosition() {
         this.pos = INITIAL_POS;
     }
 
-    public void moveByDistance(int distance) {
-        pos += distance;
+    public void advancePosition() {
+        pos += DISTANCE;
     }
 
     public int toInt() {
