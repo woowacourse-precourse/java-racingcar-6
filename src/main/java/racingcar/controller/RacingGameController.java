e16c8ead139bb5e2e72cd6c50c66dc0888d80cdf
leejaehun <ldyjka@gmail.com>
2023-10-31 05:45:49 +0900

    refactor: 시도횟수 입력 기능 함수로 분리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,16 +1,27 @@
 package racingcar.controller;
 
+import static java.lang.Integer.parseInt;
+
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.CarNameList;
 
 public class RacingGameController {
     private static CarNameList carNameList;
+    private static Integer tryNumber;
     public void init(){
-        carNameList = CarNameList.getinstance(InputCarString());
+        carNameList = CarNameList.getinstance(getCarString());
         carNameList.PrintCarName();
+        tryNumber = getTryNumber();
     }
-    public String[] InputCarString(){
+    public void progress(){
+
+    }
+    public String[] getCarString(){
         String carName = Console.readLine();
         return carName.split(",");
+    }
+    public Integer getTryNumber(){
+        Integer inputNumber = parseInt(Console.readLine());
+        return inputNumber;
     }
 }
