13dbe32d8fd0ccc68fc0dee82aa84c24507e2abd
DaeYoungK <hanguk830@naver.com>
2023-10-30 10:02:30 +0900

    fix(ApplicationTest): 테스트 출력 케이스 변경

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -50,7 +50,7 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni", "3");
-                    assertThat(output()).contains("pobi : --", "woni : ---", "최종 우승자 : pobi,woni");
+                    assertThat(output()).contains("pobi : ---", "woni : ---", "최종 우승자 : pobi,woni");
                 },
                 MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
         );
