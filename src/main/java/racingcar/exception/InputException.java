0a23325ed241e7307671ac12ef0af919b32cda0c
opixxx <opix0306@naver.com>
2023-10-30 15:56:39 +0900

    feat: 중복된 이름이 있을 경우 예외 처리 기능 구현
    Set을 이용해서 중복된 이름이 들어오면 예외 처리 하도록 하였음

--- src/main/java/racingcar/exception/InputException.java
+++ src/main/java/racingcar/exception/InputException.java
@@ -1,12 +1,19 @@
 package racingcar.exception;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputException {
     public static void validateInputName(List<String> names) {
+        Set<String> nameSet = new HashSet<>();
         for (String name : names) {
+            if (!nameSet.add(name)) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+
             if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능");
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
         }
     }
