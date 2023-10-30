fe8de8bd3f272db532555fb9f9782e9e25dc650a
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-30 13:56:19 +0900

    test(domain): Car 클래스 테스트 코드 수정
    자동차 생성 테스트에서 직접 생성 후 테스트 진행하도록 수정합니다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,17 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
-    private Car testCar;
     private String testCarName;
 
     @BeforeEach
     void setUp() {
         testCarName = "pobi";
-        testCar = new Car(testCarName);
     }
 
     @Test
     void 자동차_생성_테스트() {
+        Car testCar = new Car(testCarName);
+
         assertThat(testCar).isNotNull();
         assertEquals(testCarName, testCar.getName());
         assertEquals(0, testCar.getPosition());
@@ -27,6 +27,7 @@ class CarTest {
     void 자동차_전진_테스트() {
         Car testCar = new Car(testCarName);
         testCar.move();
+
         assertEquals(1, testCar.getPosition());
     }
 }
