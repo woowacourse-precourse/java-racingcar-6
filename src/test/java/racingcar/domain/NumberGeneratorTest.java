4368bf2fa58433501bc8b2dfbf5abac41d426b1b
BellWin98 <jongseunghan@naver.com>
2023-10-30 18:30:22 +0900

    test(racingcar): test폴더 추가

---
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+}
