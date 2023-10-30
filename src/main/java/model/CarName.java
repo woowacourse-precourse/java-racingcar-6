e9500b048f5ccf4f7cff27f029da25c50c45a776
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 14:06:42 +0900

    feat : 자동차 이름 길이 검증
    - 1글자 미만, 5글자 초과시 에러

--- src/main/java/model/CarName.java
+++ src/main/java/model/CarName.java
@@ -1,11 +1,21 @@
 package model;
 
 public class CarName {
+    private final int MIN_NAME_LENGTH = 1;
+    private final int MAX_NAME_LENGTH = 5;
     private final String name;
 
 
+
     public CarName(String name) {
+        validateCarNameLength(name);
         this.name = name;
+    }
+
+    private void validateCarNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 
 
--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -4,6 +4,8 @@ import model.CarName;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarNameTest {
 
@@ -13,4 +15,22 @@ public class CarNameTest {
         CarName name = new CarName("mycar");
         Assertions.assertThat(name).isNotNull();
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {""})
+    @DisplayName("이름 1글자 미만 입력 테스트")
+    void 자동차_이름_1글자_미만_입력(String string){
+        Assertions.assertThatThrownBy(()->new CarName(string))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcedf"})
+    @DisplayName("이름 5글자 초과 입력 테스트")
+    void 자동차_이름_5글자_초과_입력(String string){
+        Assertions.assertThatThrownBy(()->new CarName(string))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
