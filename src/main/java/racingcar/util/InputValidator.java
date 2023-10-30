3307299a5ab39c7cc0fc175c89676c996b5e124e
siwol025 <cyk99115695@gmail.com>
2023-10-30 14:14:49 +0900

    feat(racingcar): 입력받은 자동차 이름의 개수를 검사하는 기능 구현

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,15 +1,19 @@
 package racingcar.util;
 
+import java.util.List;
+
 public class InputValidator {
-    public void validateCarNamesSize() throws IllegalArgumentException{
+    public void validateCarNamesSize(List<String> carNames) {
+        if (carNames.size() < 2) {
+            throw new IllegalArgumentException("경주에 참여할 자동차의 이름 수가 모자릅니다. 적어도 2개 이상의 이름을 입력해주세요.");
+        }
+    }
+
+    public void validateCarNameLength() {
 
     }
 
-    public void validateCarNameLength() throws IllegalArgumentException{
-
-    }
-
-    public void validateMoveCountRange() throws IllegalArgumentException{
+    public void validateMoveCountRange() {
 
     }
 }
