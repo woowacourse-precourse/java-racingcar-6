ddf039ea557b51f62356b699292f1f25a3ad89a3
Hyeon-hak Kim <07ily@naver.com>
2023-10-31 08:28:19 +0900

    feat: Game.getNumberOfRounds
    - Expect to get a non-negative integer as a String
    - Parse int from a String
    - Throw exception for negative values

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -6,33 +6,24 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.List;
 
 public class Game implements Rule{
-    /**
-     * @return 쉼표로 구분된 자동차 이름을 분할한 리스트
-     */
+
     @Override
     public List<String> getCarNames() {
         return List.of(readLine().split(","));
     }
 
-    /**
-     * @return
-     */
     @Override
-    public Integer getNumberOfRounds() {
-        return null;
+    public Integer getNumberOfRounds() throws IllegalArgumentException {
+        int res = Integer.parseInt(readLine());
+        if (res < 0) throw new IllegalArgumentException();
+        return res;
     }
 
-    /**
-     *
-     */
     @Override
     public void printCurrentPerformance() {
 
     }
 
-    /**
-     *
-     */
     @Override
     public void printBestPerformers() {
 
