6bf5f34b5915b63fbf3b11e12b00f885a1ccf0ac
heogeonho <geonh1016@gmail.com>
2023-10-30 20:11:54 +0900

    refactor: 검증 코드 Validator로 분리
    Name 검증 코드 분리(세분화): isValidInput(), isValidLength(), isDuplicate() 메서드 추가 및 활용

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -11,7 +10,7 @@ public class RacingGame {
     public void startGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
-        validateInputName(inputName);
+        Validator.validateName(inputName);
 
         String[] inputArray = inputName.split(",");
         List<Car> carList = new ArrayList<>();
@@ -21,7 +20,7 @@ public class RacingGame {
 
         System.out.println("시도할 회수는 몇회인가요?");
         String inputRound = Console.readLine();
-        validateInputRound(inputRound);
+        Validator.validateRound(inputRound);
 
         System.out.println("실행 결과");
         int repeatNumber = Integer.parseInt(inputRound);
@@ -50,47 +49,8 @@ public class RacingGame {
 
     }
 
-    private static void validateInputName(String input) {
-        input = input.trim();
-        if (input.startsWith(",") || input.endsWith(",")) {
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
-        }
-        String[] nameArray = input.split(",");
-        for (String name : nameArray) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차의 이름은 5자 이하로 만들어야 합니다.");
-            }
-            if (name.isEmpty()) {
-                throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
-            }
-        }
-        List<String> nameList = Arrays.asList(nameArray);
-        if (nameList.size() != nameList.stream().distinct().count()) {
-            throw new IllegalArgumentException("자동차의 이름은 중복된 값이 있을 수 없습니다.");
-        }
-    }
-
     private static Car generateCar(String name) {
         return new Car(name);
-    }
-
-    private static void validateInputRound(String round) {
-        isNumeric(round);
-        isPositive(round);
-    }
-
-    private static void isNumeric(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 회수는 숫자이어야 합니다.");
-        }
-    }
-
-    private static void isPositive(String input) {
-        if (Integer.parseInt(input)<=0) {
-            throw new IllegalArgumentException("시도할 회수는 1회 이상을 입력해주세요.");
-        }
     }
 
     private static String createPositionPrint(int position) {
---
+++ src/main/java/racingcar/domain/Validator.java
@@ -0,0 +1,52 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Validator {
+    public static void validateName(String input) {
+        input = input.trim();
+        String[] nameArray = input.split(",");
+        List<String> nameList = Arrays.asList(nameArray);
+
+        isValidInput(input);
+        isValidLength(nameArray);
+        isDuplicate(nameList);
+    }
+    public static void validateRound(String input) {
+        isNumeric(input);
+        isPositive(input);
+    }
+    private static void isValidInput(String input) {
+        if (input.startsWith(",") || input.endsWith(",")) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+    }
+    private static void isValidLength(String[] nameArray) {
+        for (String name : nameArray) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하로 만들어야 합니다.");
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+            }
+        }
+    }
+    private static void isDuplicate(List<String> nameList) {
+        if (nameList.size() != nameList.stream().distinct().count()) {
+            throw new IllegalArgumentException("자동차의 이름은 중복된 값이 있을 수 없습니다.");
+        }
+    }
+    private static void isNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 회수는 숫자이어야 합니다.");
+        }
+    }
+    private static void isPositive(String input) {
+        if (Integer.parseInt(input)<=0) {
+            throw new IllegalArgumentException("시도할 회수는 1회 이상을 입력해주세요.");
+        }
+    }
+}
