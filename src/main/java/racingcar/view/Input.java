ed509e7a7dddfa7114b67cac4cd96f72c13fa951
mdgome <mdgome3@gmail.com>
2023-10-30 23:59:39 +0900

    Refactor: 코딩 컨벤션 및 가독성 목적 수정
    코딩 컨벤션:
     ApplicationTest import 문 분리를 위하여 공백 추가
    
    가독성 목적 수정:
     Input.java 변수 선언 및 console 출력간 공백 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -8,6 +8,7 @@ public class Input {
     public Input() {}
     public String promptForCarNames() {
         String cars;
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         try {
