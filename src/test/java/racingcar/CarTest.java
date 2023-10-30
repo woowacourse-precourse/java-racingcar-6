1e573e43883aeb4befee43c66d384b0447dd727c
jjaegii <hn06038@gmail.com>
2023-10-30 23:19:20 +0900

    feat: CarTest 클래스 내 move_forward_when_random_number_is_greater_than_or_eqaul_to_4 메소드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 public class CarTest {
+    @Test
+    void move_forward_when_random_number_is_greater_than_or_eqaul_to_4() {
+        int canMove = 4;
+        assertRandomNumberInRangeTest(
+            () -> {
+                Car car = new Car("testCar");
+                int prevPosition = car.getPosition();
+                car.move();
+                assertThat(car.getPosition() - prevPosition).isEqualTo(1);
+            },
+            canMove
+        );
+
+    }
 }
