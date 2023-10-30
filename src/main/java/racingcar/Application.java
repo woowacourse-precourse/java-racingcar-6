386fa4a56e5f9667fb552e5099a10d53cda5d0cf
Park Jae Won <ploi9@naver.com>
2023-10-31 03:02:16 +0900

    코드 가독성 향상 및 README파일 수정, 테스트 코드 정교화 과정 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -22,12 +22,12 @@ public class Application {
 
         Map<String, String> carsAndMoves = app.addCountWhenMove(racingGameCount, carNameList);
 
-        String winner = app.determineMoveTheFurthest(carsAndMoves, carNameList);
+        String winner = app.determineWhichMovesTheFurthest(carsAndMoves, carNameList);
         System.out.println(winner);
 
     }
 
-    private void checkInput(List<String> carNameList) { // 각 자동차의 이름은 5글자 이하, 쉼표로만 구
+    protected void checkInput(List<String> carNameList) { // 각 자동차의 이름은 5글자 이하, 쉼표로만 구
 
         for (int i = 0; i < carNameList.size(); i++) {
             String carName = carNameList.get(i);
@@ -36,19 +36,24 @@ public class Application {
                 throw new IllegalArgumentException("레이싱 카 이름이 5글자를 초과하였습니다.");
             }
 
-            if (Pattern.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*", carName)) { // 한글이 들어간 경우 예외처리
-                throw new IllegalArgumentException("레이싱 카 이름에 한글이 포함되어 있습니다.");
+            if (!Pattern.matches("^[a-zA-Z]+$", carName)) { // 영어 이외의 문자(공백포함)가 포함될 시 예외 처리
+                throw new IllegalArgumentException("레이싱 카 이름이 영어로 이루어져 있지 않습니다.");
             }
 
             if (validateDuplication(carNameList, carName, i)) { // 이름이 중복될 경우 예외 처리
                 throw new IllegalArgumentException(carName + " 이름이 중복됩니다.");
             }
 
+
         }
 
     }
 
-    private boolean validateDuplication(List<String> carNameList, String carName, int index) {
+    private boolean validateDuplication(
+            List<String> carNameList,
+            String carName,
+            int index
+    ) {
         for (int i = index + 1; i < carNameList.size(); i++) {
             if (carNameList.get(i).equals(carName)) {
                 return true;
@@ -58,26 +63,30 @@ public class Application {
 
     }
 
-    private Map<String, String> addCountWhenMove(int racingGameCount, List<String> carNameList){
+    protected Map<String, String> addCountWhenMove(int racingGameCount, List<String> carNameList) {
         Map<String, String> carsAndMoves = new HashMap<>();
 
         System.out.println("실행 결과");
 
         for (int i = 0; i < racingGameCount; i++) {
             for (int j = 0; j < carNameList.size(); j++) {
-                int randomNumber = Randoms.pickNumberInRange(0, 9);
 
-                preventNullValue(carsAndMoves, carNameList, randomNumber, j);
+                addCountAndPreventNullValue(carsAndMoves, carNameList, j);
 
-                System.out.println(carNameList.get(j)+" : "+carsAndMoves.get(carNameList.get(j)));
+                System.out.println(carNameList.get(j) + " : " + carsAndMoves.get(carNameList.get(j)));
             }
             System.out.println();
         }
         return carsAndMoves;
     }
 
-    private void preventNullValue(Map<String, String> carsAndMoves ,List<String> carNameList, int randomNumber, int index) {
+    private void addCountAndPreventNullValue(
+            Map<String, String> carsAndMoves,
+            List<String> carNameList,
+            int index
+    ) {
         String moveCount = carsAndMoves.get(carNameList.get(index));
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
 
         if (randomNumber >= 4) {
 
@@ -98,7 +107,7 @@ public class Application {
         }
     }
 
-    private String determineMoveTheFurthest(Map<String, String> carsAndMoves, List<String> carNameList) {
+    private String determineWhichMovesTheFurthest(Map<String, String> carsAndMoves, List<String> carNameList) {
         List<String> result = new ArrayList<>();
         int temp = 0;
 
@@ -117,7 +126,9 @@ public class Application {
 
         }
 
-        return "최종 우승자" + " : " + result.toString().replaceAll("\\[|\\]", "");
+        String resultToString = result.toString();
+
+        return "최종 우승자" + " : " + resultToString.replaceAll("\\[|\\]", "");
     }
 
 }
