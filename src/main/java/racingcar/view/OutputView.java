d5d3da831d747f6b9615d902a72b0771dff83ad2
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:25:50 +0900

    feat(OutputView) : InputView 클래스에서 입력받은 자동차와 게임 시도 횟수와 연결되어 자동차를 출력하는 printGameResult 메소드와 printNumberOfAttempts 메소드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,46 @@
-package racingcar.view;public class Outputview {
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final String COLON = " : ";
+    private static final String ADVANCED_ICON = "-";
+    private static final String NEW_LINE = "\n";
+
+    public static void printGameResult(List<Car> cars, int numberOfAttempts) {
+        for (int attempt = 0; attempt < numberOfAttempts; attempt++) {
+            printNumberOfAttempts(attempt);
+        }
+    }
+
+
+    public static void printNumberOfAttempts(int numberOfAttempts) {
+        if (numberOfAttempts < numberOfAttempts - 1) {
+            System.out.println();
+        }
+    }
+
+    public static void printCurrentPositions(List<Car> cars) {
+        StringBuilder result = new StringBuilder();
+        for (Car car : cars) {
+            result.append(car.getName()).append(COLON).append(generateAdvancedIcon(car.getPosition())).append(NEW_LINE);
+        }
+        System.out.println(result);
+    }
+
+    private static String generateAdvancedIcon(int position) {
+        StringBuilder icon = new StringBuilder();
+        for (int index = 0; index < position; index++) {
+            icon.append(ADVANCED_ICON);
+        }
+        return icon.toString();
+    }
+
+    public static void printWinners(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.println("최종 우승자: " + winnerNames);
+    }
 }
