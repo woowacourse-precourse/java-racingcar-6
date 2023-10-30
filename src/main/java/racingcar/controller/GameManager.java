bfac330f71ee3f9dfd6ca61d0c4ba359e6dfa276
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:29:29 +0900

    [feat] Add GameManager class constructor

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -6,7 +6,10 @@ public class GameManager {
     // 타 클래스 변수
     private UserView userView;
     private User user;
-
     // GameManager 변수
     private Integer tryCount;
+    public GameManager() {
+        userView = new UserView();
+        user = new User();
+    }
 }
