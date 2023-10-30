885517205bbcc9ececaff7de1bb307616397e378
05AM <anytime0224@gmail.com>
2023-10-31 00:32:59 +0900

    refactor: Class CarNameDto, RaceCountDto -> Record로 변경

--- src/main/java/racingcar/controller/dto/request/CarNameDto.java
+++ src/main/java/racingcar/controller/dto/request/CarNameDto.java
@@ -2,14 +2,5 @@ package racingcar.controller.dto.request;
 
 import java.util.List;
 
-public class CarNameDto {
-    private final List<String> carNameList;
-
-    public CarNameDto(List<String> carNameList) {
-        this.carNameList = carNameList;
-    }
-
-    public List<String> getCarNameList() {
-        return carNameList;
-    }
+public record CarNameDto(List<String> carNameList) {
 }
--- src/main/java/racingcar/controller/dto/request/RaceCountDto.java
+++ src/main/java/racingcar/controller/dto/request/RaceCountDto.java
@@ -1,13 +1,4 @@
 package racingcar.controller.dto.request;
 
-public class RaceCountDto {
-    int cnt;
-
-    public RaceCountDto(int cnt) {
-        this.cnt = cnt;
-    }
-
-    public int getCnt() {
-        return cnt;
-    }
+public record RaceCountDto(int cnt) {
 }
