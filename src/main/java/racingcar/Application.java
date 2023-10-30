362a20bc57cdceaadeb8043e6ac74c4e75481d6f
moonyeju <moonyeju8510@gmail.com>
2023-10-31 00:22:13 +0900

    FEAT: 자동차 이름 입력 기능 추가 - 예외 처리 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        processInputValues();
+    }
+
+    private static void processInputValues() {
+            System.out.print("자동차 이름 입력: ");
+            String values = Console.readLine();
+
+            // 쉼표로 분리
+            String[] carNames = values.split(",");
+
+            // 예외 처리: 값의 길이가 5 이상인 경우
+            checkCarNameLength(carNames);
+    }
+
+    private static void checkCarNameLength(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.trim().length() >= 5) {
+                throw new IllegalArgumentException("값의 길이가 5자 이상입니다.");
+            }
+        }
     }
 }
