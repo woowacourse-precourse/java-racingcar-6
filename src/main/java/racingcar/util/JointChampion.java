cc07d1dbdf49dbe4b38948bd9e746b9e283f92ab
Knight <kangrack333@gmail.com>
2023-10-30 21:59:26 +0900

    feat : 우승자 출력 및 메서드 구현

--- src/main/java/racingcar/util/JointChampion.java
+++ src/main/java/racingcar/util/JointChampion.java
@@ -1,4 +1,13 @@
 package racingcar.util;
 
-public class JointChampion {
+import java.util.List;
+
+public class JointChampion implements Champion{
+    @Override
+    public void finalWinnerOutput(List<String> winnerInformation) {
+        System.out.println("두명이상우승");
+        System.out.println(winnerInformation);
+
+
+    }
 }
