ec0805c33408b13f6928cba34e11aa8e14ee4201
whxogus215 <whxogus215@naver.com>
2023-10-30 20:33:52 +0900

    refactor: 자동차 이름과 관련된 검증 로직 분리
    자동차 이름과 관련된 검증 로직은 CarStore에서 하도록 변경

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -2,14 +2,6 @@ package racingcar.utils;
 
 public class InputValidator {
 
-    public static void carNameValidate(String[] carNames) {
-        for (String name : carNames) {
-            if (name.isBlank() || name.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
     public static void gameCountValidate(String input) {
         if (isNotInteger(input) || input.isBlank()) {
             throw new IllegalArgumentException();
--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -11,11 +11,8 @@ public class RacingCarView {
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = inputStringLine();
-        String[] splitInputCars = cars.split(",");
 
-        carNameValidate(splitInputCars);
-
-        return splitInputCars;
+        return cars.split(",");
     }
 
     public static int inputGameCount() {
