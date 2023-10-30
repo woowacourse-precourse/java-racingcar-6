07eb1c7a70322f45475f305a5f0e50797a101397
icebear0111 <wltjq0901@naver.com>
2023-10-31 00:55:04 +0900

    refactor: separate name input method and number of moves input method

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -2,16 +2,16 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-
-
 public class User {
-    public void initGame() {
+    public String[] initGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String nameInput = Console.readLine();
-        String[] name = nameInput.split(",");
+        return nameInput.split(",");
+    }
+
+    public int setMoves() {
         System.out.println("시도할 회수는 몇회인가요?");
         String userInput = Console.readLine();
-        int numberOfMoves = Integer.parseInt(userInput);
+        return Integer.parseInt(userInput);
     }
 }
