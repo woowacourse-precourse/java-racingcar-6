944b250766d956256ee950f6f8766124437fe5be
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 03:23:03 +0900

    feat: 진행 중인 경기 상황을 보여주는 View 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,15 +1,27 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarList;
+
 public class RacingCarView {
 
-    public void carNameListInputPrint(){
+    private final String LOCATION = "-";
+
+    public void carNameListInputPrint() {
         System.out.println("경주할 자동차 이름을 입력하시오");
     }
 
-    //시도할 회수는 몇회인가요? -service, print - view
-    public void readCountPrint(){
+
+    public void readCountPrint() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public void gameProgressPrint(CarList carList) {
+        for (Car car : carList.getCarList()) {
+            int location = car.getLocation();
+            System.out.println(car.getName() + " : " + LOCATION.repeat(location));
+        }
+        System.out.println();
+    }
 
 }
