150c941f1d5a94a5a73f42b007e4af2cf71a62b4
IM-GYURI <kgulr0517@gmail.com>
2023-10-31 03:27:10 +0900

    Feat: 추가 prepareCarList() 함수

--- src/main/java/racingcar/model/car/CarRegister.java
+++ src/main/java/racingcar/model/car/CarRegister.java
@@ -2,6 +2,7 @@ package racingcar.model.car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarRegister {
     private final List<String> carNames;
@@ -12,5 +13,11 @@ public class CarRegister {
 
     public void registerCar(String carName) {
         carNames.add(carName);
+    }
+
+    public CarList prepareCarList(){
+        return new CarList(carNames.stream()
+                .map(carName -> new Car(carName))
+                .collect(Collectors.toUnmodifiableList()));
     }
 }
