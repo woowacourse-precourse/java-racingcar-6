190e7503008e8a885e581ba62bc144f0635dce31
Leehyoju97 <dlgywn2420@gmail.com>
2023-10-30 17:11:06 +0900

    feat: 입력한 자동차 값을 Car list에 넣어주는 기능

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,10 +1,18 @@
 package racingcar;
 
+import racingcar.domain.Car;
 import racingcar.domain.Setting;
+import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RacingGame {
+    private static int numberOfAttempts = 0;
+    static List<Car> cars;
 
     public static void run() {
-        Setting.initCar();
+        cars = Setting.initCar();
+        numberOfAttempts = Setting.initNumberOfAttempt();
+        OutputView.executionMessage();
     }
 }
--- src/main/java/racingcar/domain/Setting.java
+++ src/main/java/racingcar/domain/Setting.java
@@ -1,5 +1,8 @@
 package racingcar.domain;
 
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +10,24 @@ public class Setting {
     static List<Car> cars = new ArrayList<>();
 
     public static List<Car> initCar() {
-        List<String> carNames = Input.getSplitList(Input.inputString());
+        List<String> carNames = InputValidator.filterCarName(initCarName());
         for (String carName : carNames) {
             Car car = new Car(carName);
             cars.add(car);
         }
         return cars;
+    }
+
+    private static List<String> initCarName() {
+        List<String> carNameList = new ArrayList<>();
+        String[] carNames = InputView.inputCarName();
+        for (String carName : carNames) {
+            carNameList.add(carName);
+        }
+        return carNameList;
+    }
+
+    public static int initNumberOfAttempt() {
+        return InputValidator.filterNumberOfAttempt(InputView.inputNumberOfAttempts());
     }
 }
