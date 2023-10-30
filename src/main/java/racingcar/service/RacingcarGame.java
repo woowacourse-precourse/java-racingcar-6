f229f8d5f7bd243a7d41cdea94d9fbe9020dfbda
Minju Ji <hinote444@gmail.com>
2023-10-31 00:52:43 +0900

    feat(RacingcarGame): 가장 많이 전진한 자동차의 이름을 리스트에 담아 반환하는 기능 구현

--- src/main/java/racingcar/service/RacingcarGame.java
+++ src/main/java/racingcar/service/RacingcarGame.java
@@ -1,6 +1,8 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.domain.Car;
 
 public class RacingcarGame {
@@ -8,6 +10,22 @@ public class RacingcarGame {
     private static final int NUMBER_FOUR = 4;
     private static final int NUMBER_ZERO = 0;
     private static final int NUMBER_NINE = 9;
+
+    public List<String> findMaxMoveForwardCount(List<Car> cars) {
+        List<String> winner = new ArrayList<>();
+        int max = 0;
+        for (Car car : cars) {
+            if (car.getMoveForwardCount() > max) {
+                max = car.getMoveForwardCount();
+            }
+        }
+        for (Car car : cars) {
+            if (car.getMoveForwardCount() == max) {
+                winner.add(car.getCarName());
+            }
+        }
+        return winner;
+    }
 
     public void increaseMoveForwardCount(Car car) {
         if (isGreaterThanOrEqualFour()) {
