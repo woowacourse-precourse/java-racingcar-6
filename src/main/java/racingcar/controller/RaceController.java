2f88718c53fb0bf5ae34a8f617103d4988b1b0d4
siwol025 <cyk99115695@gmail.com>
2023-10-30 15:57:19 +0900

    feat(racingcar): 자동차 이름들을 쉼표(,)를 기준으로 구분하는 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,9 +1,13 @@
 package racingcar.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.util.InputValidator;
 
 public class RaceController {
+    public static final String DELIMITER = ",";
+
     public void run() {
 
     }
@@ -16,8 +20,12 @@ public class RaceController {
         return 0;
     }
 
-    public List<String> divideCarNames() {
-        return null;
+    public List<String> divideCarNames(String inputNames) {
+        List<String> carNames = Arrays.asList(inputNames.split(DELIMITER));
+        InputValidator.validateCarNamesSize(carNames);
+        InputValidator.validateCarNameLength(carNames);
+
+        return carNames;
     }
 
     public List<Car> createCars() {
