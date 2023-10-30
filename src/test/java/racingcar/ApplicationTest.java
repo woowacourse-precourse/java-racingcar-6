c4aac0018ab92f68528a781e312bc2d49872b192
hyeongjun im <limbs713@naver.com>
2023-10-31 00:09:54 +0900

    :white_check_mark: 중복 우승자에 대한 test 추가
    우승자가 여러 명일 때 출력 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,18 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.repository.CarRepository;
+import racingcar.model.service.GameService;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.view.OutputView.outputWinner;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,11 +21,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -31,6 +37,23 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    @DisplayName("복수의 우승자에 대한 출력 검사")
+    void multiWinnerTest() {
+        //given
+        CarRepository carRepository = new CarRepository();
+        GameService gameService = new GameService(carRepository);
+        List<String> players = new ArrayList<>(List.of("faker", "chovy", "bdd"));
+        gameService.createCars(players);
+
+        //when
+        carRepository.findByName("faker").setForward(5);
+        carRepository.findByName("chovy").setForward(5);
+        carRepository.findByName("bdd").setForward(4);
+        //then
+        outputWinner(gameService.resultGame());
+        assertThat(output()).contains("faker, chovy");
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
