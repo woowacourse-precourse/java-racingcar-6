aec20ff6cd632805c888a8d5dc99f97edf277c60
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:50:23 +0900

    feat(racingcar): GameManagerController에서 View를 통해 자동차 이름을 가져오는 메소드 추가 InputView, OutputView의 static 메소드를 통해 자동차 이름을 가져오는 메소드를 추가하였습니다.

--- src/main/java/racingcar/controller/GameManagerController.java
+++ src/main/java/racingcar/controller/GameManagerController.java
@@ -1,4 +1,14 @@
 package racingcar.controller;
 
+import racingcar.dto.request.CarNameRequest;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class GameManagerController {
+    private List<CarNameRequest> getCarNames() {
+        OutputView.printCarNameInputQuestion();
+        return InputView.requestCarNames();
+    }
 }
