2ac7e4f8a7d661e4036a5aa7662433cb76fd2b45
SiHyun-Lee113 <82255633+SiHyun-Lee113@users.noreply.github.com>
2023-10-30 17:57:35 +0900

    fix : 에러 관리 수정
    - 객체 생성 없이 사용을 위해 static 함수로 수정

--- src/main/java/racingcar/Util/ErrorCollection.java
+++ src/main/java/racingcar/Util/ErrorCollection.java
@@ -2,15 +2,15 @@ package racingcar.Util;
 
 public class ErrorCollection {
 
-    public void throwError() {
+    public static void IllegalEmptyInput() {
         throw  new IllegalArgumentException("올바르지 않은 입력입니다.");
     }
 
-    public void IllegalCarNames() {
-        throw new IllegalArgumentException("올바르지 않은 입력입니다. 이름 사이를 공백 없이 쉼표(,)를 이용해 구분해 주세요.\n올바른 예) pobi,woni,jun\n");
+    public static void IllegalCarNames() {
+        throw new IllegalArgumentException("올바르지 않은 입력입니다. 자동차 이름은 5자 이하만 가능합니다.\n올바른 예) pobi,woni,jun\n");
     }
 
-    public void IllegalAttempts() {
-        throw new IllegalArgumentException("올바르지 않은 입력이다. 숫자만 입력해 주세요.");
+    public static void IllegalAttempts() {
+        throw new IllegalArgumentException("올바르지 않은 입력입니다. 숫자만 입력해 주세요.");
     }
 }
