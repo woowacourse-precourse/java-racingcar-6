80b99e3afaad5706d7782119688fb3a45223a383
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 02:30:51 +0900

    test : 빈칸 여부 테스트코드 작성
    입력 값에서 빈 칸 포함 시 예외 처리 테스트 코드 작성. 빈 칸 포함 안 됐을 때 예외 처리 안 던지는 테스트 코드 작성

--- src/test/java/racingcar/exception/UserInputExceptionTest.java
+++ src/test/java/racingcar/exception/UserInputExceptionTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class UserInputExceptionTest {
     private UserInputException userInputException;
     private static final String LENGTH_ERROR_MESSAGE = "자동차 이름은 1 이상, 5 이하로 입력해 주세요";
+    private static final String BLANK_STRING_MESSAGE = "빈 문자를 입력하셨습니다.";
 
     @Test
     void 이름_길이가_유효할_때() {
@@ -15,10 +16,22 @@ class UserInputExceptionTest {
         assertDoesNotThrow(() -> UserInputException.isLength(input));
     }
     @Test
-    void 이름_길이가_유효하지_않을때() {
-        String input = "";
+    void 이름_길이가_유효하지_않을_때() {
+        String input = "asdfadsfasdf";
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> UserInputException.isLength(input));
         assertEquals(LENGTH_ERROR_MESSAGE, exception.getMessage());
     }
+    @Test
+    void 빈칸이_포함_됐을_때(){
+        String input = "po bi,jundo";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> UserInputException.isBlank(input));
+        assertEquals(BLANK_STRING_MESSAGE, exception.getMessage());
+    }
+    @Test
+    void 빈칸이_포함_안_됐을_때() {
+        String input = "pobi,jundo";
+        assertDoesNotThrow(() -> UserInputException.isBlank(input));
+    }
+
 
 }
