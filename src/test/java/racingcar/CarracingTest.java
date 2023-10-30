f261a516353f5a664f04d684b4736b06eca4b34c
Chenowoo <cjsgusdn14@naver.com>
2023-10-30 15:03:34 +0900

    test(CarracingTest): 자동차의 전진, 스탑을 테스트 하는 기능 구현  - 4이상의 숫자가 들어왔을 때 자동차가 전진했는지 확인  - 4보다 작은 숫자가 들어왔을 때 자동차가 스탑했는지 확인

--- src/test/java/racingcar/CarracingTest.java
+++ src/test/java/racingcar/CarracingTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -14,22 +16,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarracingTest {
+    private final boolean CAR_LOCATE_START_POINT = false;
     private Carracing carracing;
+    private Map<String, String> raceScore;
 
-    @BeforeEach
-    void setUp() {
+    void setInstance() {
         carracing = new Carracing();
+    }
+
+    void setMapInstance() {
+        carracing.raceScore = new HashMap<>();
+        carracing.raceScore.put("pobi", "");
+        carracing.raceScore.put("woni", "");
     }
 
     @ParameterizedTest
     @MethodSource("provideCarName")
     void 자동차_출발점_위치(String[] names) {
+        setInstance();
         Map<String, String> nameList = new LinkedHashMap<>();
         nameList = carracing.setInitStartPoint(names);
 
         boolean checkSetInitStartPoint = checkNull(nameList);
 
-        assertThat(checkSetInitStartPoint).isEqualTo(false);
+        assertThat(checkSetInitStartPoint).isEqualTo(CAR_LOCATE_START_POINT);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"pobi, 4, pobi, -", "pobi, 3, pobi, ''", "woni, 4, pobi, ''"})
+    void 자동차_전진_스탑(String name, int randomNumber, String resultName, String score) {
+        setMapInstance();
+        carracing.move(name, randomNumber);
+
+        assertThat(carracing.raceScore.get(resultName)).isEqualTo(score);
     }
 
     private static boolean checkNull(Map<String, String> nameList) {
