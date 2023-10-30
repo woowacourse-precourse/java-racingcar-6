5884a779033fa5c82c151416fe72962d0270414e
minsub <chrkb1569@naver.com>
2023-10-30 21:48:55 +0900

    feat : 사용자로부터 자동차의 이름을 입력받는다.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,10 +1,13 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 // 사용자로부터 값을 입력받고 결과를 출력하기 위한 클래스
 public class GameController {
     public void startGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        // read carNameArr
+        String[] carNameArr = Console.readLine().split(",");
+
         System.out.println("시도할 회수는 몇회인가요?");
         // read frequency
 
