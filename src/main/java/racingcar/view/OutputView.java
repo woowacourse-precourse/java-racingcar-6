7e7ff8d9b10f21e307c2e38e2727878327e7be2c
ro-el <chung3286@naver.com>
2023-10-31 02:00:37 +0900

    feat: 현재 레이싱 경주 상황 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,31 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String RESULT_PHRASE = "실행 결과";
+    private static final String DISTINCTION = " : ";
+    private static final String ONE_BLOCK = "-";
+
+    public static void printResultPhrase() {
+        System.out.println();
+        System.out.println(RESULT_PHRASE);
+    }
+
+    public static void printRacingGameStatus(List<Car> cars) {
+        for (Car car : cars) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(car.getName() + DISTINCTION);
+
+            int position = car.getPosition();
+            while (position-- > 0) {
+                sb.append(ONE_BLOCK);
+            }
+
+            System.out.println(sb);
+        }
+        System.out.println();
+    }
 }
