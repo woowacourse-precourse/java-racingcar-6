0c862eacd35d98a0e3940562374c9f9ca4a059cd
suddiyo <suddiyo@naver.com>
2023-10-30 18:59:55 +0900

    feat: 자동차 이름 입력 값 검증 로직 구현

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,10 +1,13 @@
 package racingcar.model;
 
+import static racingcar.exception.RacingCarExceptionType.*;
+
 public class RacingCar {
     private final String name;
     private int position = 0;
 
     public RacingCar(String name) {
+        checkNameLength(name);
         this.name = name;
     }
 
@@ -19,4 +22,11 @@ public class RacingCar {
     public void moveForward() {
         position++;
     }
+
+    private void checkNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEEDED.getMessage());
+        }
+    }
+
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,15 @@
 package racingcar.view;
 
+import static racingcar.exception.RacingCarExceptionType.*;
 import static racingcar.view.RacingCarConsole.*;
 import static racingcar.view.RacingCarConstants.*;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
@@ -16,6 +20,7 @@ public class InputView {
         List<String> racingCars = Arrays.stream(carNames)
                 .map(String::trim)
                 .collect(Collectors.toList());
+        validateCarNames(racingCars);
         return racingCars;
     }
 
@@ -23,4 +28,32 @@ public class InputView {
         print(ATTEMPTS_PROMPT);
         return Integer.parseInt(Console.readLine());
     }
+
+    public void validateCarNames(List<String> racingCars) {
+        checkEmptyNames(racingCars);
+        checkDuplicatedNames(racingCars);
+        checkInvalidFormat(racingCars);
+    }
+
+    private void checkEmptyNames(List<String> racingCars) {
+        if (racingCars.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_NAME.getMessage());
+        }
+    }
+
+    private void checkDuplicatedNames(List<String> racingCars) {
+        Set<String> uniqueName = new HashSet<>(racingCars);
+        if (uniqueName.size() != racingCars.size()) throw new IllegalArgumentException(DUPLICATE_NAME.getMessage());
+    }
+
+    private void checkInvalidFormat(List<String> racingCars) {
+        for (String racingCar : racingCars) {
+            Pattern pattern = Pattern.compile("(\\w)+");
+            if (!pattern.matcher(racingCar).matches()) {
+                throw new IllegalArgumentException(INVALID_CAR_NAME_FORMAT.getMessage());
+            }
+        }
+    }
+
+
 }
