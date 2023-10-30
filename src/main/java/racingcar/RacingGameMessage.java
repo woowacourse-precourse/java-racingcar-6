f5c598745b1fb0a393d6215653f488c8014bd37d
tfer2442 <tfer2442@gmail.com>
2023-10-31 02:26:39 +0900

    refactor(RacingGameMessage): 상수명 적절하게 변경

--- src/main/java/racingcar/RacingGameMessage.java
+++ src/main/java/racingcar/RacingGameMessage.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 public class RacingGameMessage {
-    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
-    public static final String RESULT_RUNNING = "실행 결과";
-    public static final String FINAL_WINNER = "최종 우승자 : ";
+    public static final String INPUT_CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRIAL_NUMBER_PROMPT = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_RUNNING_SENTENCE = "실행 결과";
+    public static final String FINAL_WINNER_SENTENCE = "최종 우승자 : ";
 }
