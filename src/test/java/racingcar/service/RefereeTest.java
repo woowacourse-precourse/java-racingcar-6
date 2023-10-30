c4a793f5776cfec5ccf01716dfe0ed99b1af1248
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-30 21:09:46 +0900

    test(RefereeTest): 가장 멀리 이동한 자동차의 거리 반환 기능 테스트 구현

---
+++ src/test/java/racingcar/service/RefereeTest.java
@@ -0,0 +1,33 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingCar;
+
+class RefereeTest {
+
+    @Test
+    void getMaxDistance_자동차_전진하기_전_테스트() {
+        String[] carNames = {"test", "car", "name"};
+        List<RacingCar> racingCars = RacingCarList.generateRacingCarList(carNames);
+
+        int maxDistance = Referee.getMaxDistance(racingCars);
+        assertThat(maxDistance).isEqualTo(0);
+    }
+
+    @Test
+    void getMaxDistance_자동차_한대만_전진_후_테스트() {
+        String[] carNames = {"test", "car", "name"};
+        List<RacingCar> racingCars = RacingCarList.generateRacingCarList(carNames);
+
+        for (int i = 0; i < 100; i++) {
+            racingCars.get(0).moveForward();
+        }
+
+        int maxDistance = Referee.getMaxDistance(racingCars);
+        assertThat(maxDistance).isNotEqualTo(0);
+    }
+
+}
