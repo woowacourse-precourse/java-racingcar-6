e8eaa33eacb4e6698fbfffea30549c8d343efc56
SeungjunNA <nsjjsng1@gmail.com>
2023-10-30 14:17:42 +0900

    test : 입력시 아무것도 입력하지 않을 경우, 중복된 이름, 회수입력시 숫자가 아닌값 입력에 대한 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -15,11 +15,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -32,30 +32,34 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 입력시_아무것도_입력하지_않을_경우_예외_처리(){
+    void 입력시_아무것도_입력하지_않을_경우_예외_처리() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("","1", "pobi,woni","", "",""))
+                assertThatThrownBy(() -> runException("", "1", "pobi,woni", "", "", ""))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
     @Test
-    void 이름_입력시_중복될_경우_예외_처리(){
+    void 이름_입력시_중복될_경우_예외_처리() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,woni,pobi","3", "nsj,ns,sj,nsj","5"))
+                assertThatThrownBy(() -> runException("pobi,woni,pobi", "3", "nsj,ns,sj,nsj", "5"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
     @Test
-    void split_메소드_사용시_이름이_비어있을_경우_예외_처리(){
+    void split_메소드_사용시_이름이_비어있을_경우_예외_처리() {
 
     }
+
     @Test
-    void 회수_입력시_숫자가_아닌_값_입력시_예외_처리(){
+    void 회수_입력시_숫자가_아닌_값_입력시_예외_처리() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("nsj,nam,sjn","1a", "nsj,nam","*", "nsj,nam",""))
+                assertThatThrownBy(() -> runException("nsj,nam,sjn", "1a", "nsj,nam", "*", "nsj,nam", ""))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
