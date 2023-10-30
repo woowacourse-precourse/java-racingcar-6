4b37e9573c7f8cfef01c378920580557e6413366
star-candy <el0710@naver.com>
2023-10-31 03:22:33 +0900

    style(racingcar): code formatting

--- src/main/java/controller/RacingCarGameManager.java
+++ src/main/java/controller/RacingCarGameManager.java
@@ -11,24 +11,42 @@ import utility.ValidateException;
 public class RacingCarGameManager {
     public static void racingStart() {
         List<String> carName;
-        List<Integer> raceResult = new ArrayList<>();
-        String carNameBundle;
-        int numberOfMatch;
+        List<Integer> raceResult;
+        int numberOfRacing;
+        carName = carNameInputProcess();
+        numberOfRacing = numberOfRacingProcess();
+        raceResult = gameLogicProcess(carName, numberOfRacing);
+        racingWinnerOutputProcess(carName, raceResult);
+    }
 
+    public static List<String> carNameInputProcess() {
+        String carNameBundle;
         Output.carNameManuel();
         carNameBundle = Input.carName();
-        carName = Transform.carNameToList(carNameBundle);
+        List<String> carName = Transform.carNameToList(carNameBundle);
         ValidateException.racingCarNameInvalid(carName);
+        return carName;
+    }
+
+    public static int numberOfRacingProcess() {
+        int numberOfRacing;
         Output.numberOfRacingManuel();
-        numberOfMatch = Integer.parseInt(Input.numberOfRacing());
-        raceResult = Transform.resultListReset(carName.size(), raceResult);
-        for (int i = 0; i < numberOfMatch; i++) {
+        numberOfRacing = Integer.parseInt(Input.numberOfRacing());
+        return numberOfRacing;
+    }
+
+    public static List<Integer> gameLogicProcess(List<String> carName, int numberOfRacing) {
+        List<Integer> raceResult = new ArrayList<>();
+        Transform.resultListReset(carName.size(), raceResult);
+        for (int i = 0; i < numberOfRacing; i++) {
             Validation.racingResult(carName, raceResult);
             Output.racingResult(carName, raceResult);
         }
+        return raceResult;
+    }
+
+    public static void racingWinnerOutputProcess(List<String> carName, List<Integer> raceResult) {
         Validation.racingWinner(carName, raceResult);
         Output.racingWinner(carName);
     }
-
-
 }
