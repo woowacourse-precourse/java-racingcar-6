b2ca114f872d01c9e356551c108c5f309e8da7ab
sunghyun <shlim0287@naver.com>
2023-10-30 16:48:36 +0900

    feat : RaceService에서 raceResult 메소드 결과 출력 기능 메소드로 분리 후 view로 옮김.

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 public class Output {
     public static void printRequestCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -9,5 +11,8 @@ public class Output {
     }
     public static void printExecutionResultMessage(){
         System.out.println("실행 결과");
+    }
+    public static void printRaceResult(Car car) {
+        System.out.println(car.getCarName()+" : "+"-".repeat(car.getPosition()));
     }
 }
