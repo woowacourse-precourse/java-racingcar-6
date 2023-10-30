c752d301b064a9842b7ac2d52f21b9a1d2dfc7ac
JoonKyo <joonkyolee@gmail.com>
2023-10-30 22:59:35 +0900

    feat(Car): 자동차를 움직이는 메서드 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,11 +4,13 @@ public class Car {
     private static final int MOVE = 1;
     private static final String MOVE_FORWARD = "-";
     private String name;
+    private String carMovedResult;
     private int carState;
 
     public Car(String name){
         this.name = name;
         carState = 0;
+        carMovedResult = "";
     }
 
     public String getName(){
@@ -21,5 +23,13 @@ public class Car {
 
     public void setCarState(){
         carState = carState + MOVE;
+    }
+
+    public String getCarMovedResult(){
+        return carMovedResult;
+    }
+
+    public void setCarMovedResult(){
+        carMovedResult = carMovedResult + MOVE_FORWARD;
     }
 }
