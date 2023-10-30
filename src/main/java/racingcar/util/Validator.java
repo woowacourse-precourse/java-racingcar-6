c708ed80fd025fda0511963ce82c85e4579d487f
kcw0360 <kcw0360@naver.com>
2023-10-31 00:19:35 +0900

    Refator: 사용자에게 보여지는 출력 메세지를 enum으로 관리

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -9,29 +9,45 @@ public class Validator {
 
     public static void validateInputSpace(String input) {
         if (!validateInputPattern(input, nameValidatePattern)) {
-            throw new IllegalArgumentException("공백을 제거해주세요.");
+            throw new IllegalArgumentException(Message.EXCEPTION_INPUT_NAMES_SPACE.message);
         }
     }
 
     public static void validateInputLength(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 1~5글자로 작성해주세요.");
+            throw new IllegalArgumentException(Message.EXCEPTION_INPUT_NAMES_LENGTH.message);
         }
     }
 
     public static void validateInputNumberZero(String input) {
         if (input.equals("0")) {
-            throw new IllegalArgumentException("1이상의 정수만 입력해주세요.");
+            throw new IllegalArgumentException(Message.EXCEPTION_INPUT_NUMBER_RANGE.message);
         }
     }
 
     public static void validateInputNumber(String input) {
         if (!validateInputPattern(input, numberValidatePattern)) {
-            throw new IllegalArgumentException("1이상의 정수만 입력해주세요.");
+            throw new IllegalArgumentException(Message.EXCEPTION_INPUT_NUMBER_RANGE.message);
         }
     }
 
     private static boolean validateInputPattern(String input, Pattern pattern) {
         return pattern.matcher(input).matches();
+    }
+
+    private enum Message {
+        EXCEPTION_INPUT_NAMES_SPACE("공백을 제거해주세요."),
+        EXCEPTION_INPUT_NAMES_LENGTH("이름은 1~5글자로 작성해주세요."),
+        EXCEPTION_INPUT_NUMBER_RANGE("1이상의 정수만 입력해주세요.");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,22 +3,40 @@ package racingcar.view;
 public class OutputView {
 
     public void printCarNamesInputMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(Message.CAR_NAMES_INPUT_MESSAGE.message);
     }
 
     public void printExecutionNumberInputMessage() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(Message.EXECUTION_NUMBER_INPUT_MESSAGE);
     }
 
     public void printExecutionResultMessage() {
-        System.out.println("실행 결과");
+        System.out.println(Message.EXECUTION_RESULT_MESSAGE.message);
     }
 
     public void printWinnersMessage(String winner) {
-        System.out.println("최종 우승자 : " + winner);
+        System.out.println(Message.WINNER_MESSAGE.message + winner);
     }
 
     public void printSpaceLine() {
         System.out.println();
+    }
+
+    private enum Message {
+
+        CAR_NAMES_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        EXECUTION_NUMBER_INPUT_MESSAGE("시도할 회수는 몇회인가요?"),
+        EXECUTION_RESULT_MESSAGE("실행 결과"),
+        WINNER_MESSAGE("최종 우승자 : ");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
