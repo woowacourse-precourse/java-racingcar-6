37ace8b1dcb995f5572493aa6142d881b8242226
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-31 02:34:31 +0900

    fix : Cars클래스 fix 필요한 cars 생성자를 fix했으며, 중복된 이름을 체크해주는 메소드의 문제점이 있어서 고쳤습니다. for문을 stream을 사용하여 코드의 가독성을 강조했습니다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -9,7 +9,6 @@ public class Cars {
     private List<Car> cars;
 
     public Cars(String carNames) {
-
         this.cars = carList(carNames);
     }
 
@@ -17,24 +16,29 @@ public class Cars {
         this.cars = cars;
     }
 
-    public void carMove(CarMoveCondition carMoveCondition){
+    public List<Car> getCars() {
+        return this.cars;
+    }
 
-        for (Car car : this.cars){
-            if (carMoveCondition.moveCondition()){
+    public void carMove(CarMoveCondition carMoveCondition) {
+
+        for (Car car : this.cars) {
+            if (carMoveCondition.moveCondition()) {
                 car.move();
             }
         }
     }
 
-    public List<Car> carList(String cars){
+    public List<Car> carList(String cars) {
 
         List<Car> carsList = Arrays.stream(cars.split(InputView.COMMA))
                 .map(Car::new)
                 .toList();
 
-        long duplicateNameRemoveLength = carsList.stream().
-                distinct().
-                count();
+        long duplicateNameRemoveLength = carsList.stream()
+                .map(Car::getCarName)
+                .distinct()
+                .count();
 
         DuplicateCarName(carsList, duplicateNameRemoveLength);
 
@@ -43,31 +47,21 @@ public class Cars {
 
     public Cars winners() {
 
-        List<Car> winnersCars = new ArrayList<>(this.cars);
+        final List<Car> winners = new ArrayList<>(this.cars).stream()
+                .sorted(Comparator.comparing(Car::getPosition).reversed())
+                .toList();
 
-        Collections.sort(winnersCars, new Comparator<Car>() {
-            @Override
-            public int compare(Car o1, Car o2) {
-                return Integer.compare(o2.getPosition(), o1.getPosition());
-            }
-        });
+        int winnerPosition = winners.get(0).getPosition();
 
-        int winnerPosition = winnersCars.get(0).getPosition();
-
-        List<Car> winners = new ArrayList<>();
-
-        for (Car car : winnersCars) {
-            if (car.getPosition() == winnerPosition) {
-                winners.add(car);
-            }
-        }
-
-        return new Cars(winners);
+        return new Cars(winners.stream()
+                .filter(car -> car.getPosition() == winnerPosition)
+                .toList());
     }
 
     private void DuplicateCarName(List<Car> cars,
-                                  long size){
-        if (cars.size() != size){
+                                  long size) {
+
+        if (cars.size() != size) {
             throw new IllegalArgumentException();
         }
     }
