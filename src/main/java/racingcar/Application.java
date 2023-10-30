4aa23bfe3cbc8bef2e772ba9f134fc05c758c466
YoungYoon <duddbs1994@naver.com>
2023-10-30 16:26:11 +0900

    자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+    //경주할 자동차 이름 입력
+    public static List<Car> insertCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carName = input.split(",");
+        List<Car> cars = new ArrayList<>();
+        for(String name : carName){
+            if(name.length() > 5){
+                throw new IllegalArgumentException();
+            }
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
