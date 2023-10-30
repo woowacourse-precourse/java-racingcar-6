ab9a9b1285714b0fda64507c597686875229bdde
whereami2048 <whereami2048@gmail.com>
2023-10-30 23:44:20 +0900

    feat: Car 모델 forward 값 증가 함수 및 getter 추가

--- src/main/java/racingcar/model/CarModel.java
+++ src/main/java/racingcar/model/CarModel.java
@@ -3,4 +3,22 @@ package racingcar.model;
 public class CarModel {
     private String name;
     private int forward;
+
+    public CarModel(String name) {
+        this.name = name;
+        this.forward = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getForward() {
+        return forward;
+    }
+
+    public int increaseForward() {
+        this.forward += 1;
+        return this.forward;
+    }
 }
