c1b586fa68168f8ba65041bc4bd37a3a70fcd702
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-30 23:18:28 +0900

    InputManager 구현

---
+++ src/main/java/racingcar/utils/InputManager.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputManager {
+
+    public int inputNumber(String numString){
+        int number = Integer.parseInt(numString);
+
+        return number;
+    }
+
+}
