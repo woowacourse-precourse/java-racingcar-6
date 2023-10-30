36391b0462b7ab85d7460f8518e0da52d5088865
howudong <tjdvy963@naver.com>
2023-10-30 15:35:47 +0900

    feat : 최종 우승자를 출력하는 ResultOutputView 생성

---
+++ src/test/java/racingcar/view/outputview/ResultOutputView.java
@@ -0,0 +1,34 @@
+package racingcar.view.outputview;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.List;
+import java.util.Map;
+
+public class ResultOutputView implements OutputView {
+
+    private static final String WINNER_DELIMITER = " ,";
+
+    @Override
+    public void display(Map<String, Object> displayEntities) {
+        if (displayEntities.get("winners") instanceof Cars cars) {
+            printWinners(cars.getCars());
+        }
+    }
+
+    private void printWinners(List<Car> cars) {
+        System.out.print("최종 우승자 : ");
+        
+        String winners = convertNameToString(cars);
+        System.out.println(winners);
+    }
+
+    private String convertNameToString(List<Car> cars) {
+        List<String> winners = cars.stream()
+                .map(Car::getName)
+                .toList();
+
+        return String.join(WINNER_DELIMITER, winners);
+    }
+}
