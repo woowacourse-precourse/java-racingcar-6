363898e9b8af943658904594a6c571e331b48833
chaerlo127 <monynony0203@gmail.com>
2023-10-31 00:55:27 +0900

    fix: [1] 자동차 이름 저장소 (ArrayList to Map) 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,19 +3,20 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsInput = Console.readLine();
-        List<String> carList = new ArrayList<>(Arrays.asList(carsInput.split(",")));
+        Map<String, String> carList = Arrays.stream(carsInput.split(",")).collect(Collectors.toMap(car -> car, car -> ""));
 
-        for (String car: carList){
+
+        for (String car: carList.values()){
             if (car.length() > 5) throw new IllegalArgumentException("자동차 이름은 5글자 이하로 작성해주세요.");
         }
+
 
         System.out.println("시도할 회수는 몇회인가요?");
         String countInput = Console.readLine();
@@ -26,5 +27,6 @@ public class Application {
         }catch (NumberFormatException e){
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
+
     }
 }
