2c76ae9cbc714598880c6dc656b24ae0f1d33f0a
sangjun <sangjun1389@ajou.ac.kr>
2023-10-31 01:39:11 +0900

    feat(Judge): 입력 받는 메소드 추가
    - makeCarNamesArray() 자동차 이름들 입력받기
    - makeNumberOfAttempts() 시도할 회수 입력받기

--- src/main/java/racingcar/domain/Judge.java
+++ src/main/java/racingcar/domain/Judge.java
@@ -1,2 +1,21 @@
-package racingcar.domain;public class Judge {
+package racingcar.domain;
+
+import racingcar.InputHandler;
+import racingcar.Validator;
+
+public class Judge {
+
+    private String[] makeCarNamesArray() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNamesArray = InputHandler.inputCarNames();
+        Validator.checkValidCarNames(carNamesArray);
+        return carNamesArray;
+    }
+
+    private int makeNumberOfAttempts() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numberOfAttempts = InputHandler.inputNumberOfAttempts();
+        Validator.isValidNumberOfAttempts(numberOfAttempts);
+        return numberOfAttempts;
+    }
 }
