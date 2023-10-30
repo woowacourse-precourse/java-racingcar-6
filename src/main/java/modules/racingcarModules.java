231936b18ff7fdfa44ccb76e2f08565506d27321
74059 <shyeone@naver.com>
2023-10-30 18:48:07 +0900

    refactor: 파일 이름 변경

--- src/main/java/modules/racingcarModules.java
+++ src/main/java/modules/racingcarModules.java
@@ -1,6 +1,6 @@
 package modules;
 
-public class racingcarName {
+public class racingcarModules {
     public static void checked_car_name_len(String[] carNames) {
         for (String carName : carNames) {
             racingcarException.exception_len_5(carName);
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,7 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
-import modules.racingcarName;
+import modules.racingcarModules;
 import modules.racingcarException;
 
 public class Application {
@@ -10,7 +9,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
         String[] names_list = names.split(",");
-        racingcarName.checked_car_name_len(names_list);
+        racingcarModules.checked_car_name_len(names_list);
 
         System.out.println("시도할 회수는 몇회인가요?");
         String move_num = Console.readLine();
