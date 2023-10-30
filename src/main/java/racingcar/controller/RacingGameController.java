64ddfea008514480e009ccc3c0655512ac1a29b5
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-31 02:00:32 +0900

    refactor: 컨트롤러는 요청을 받아서 실행 유도만을 하도록 실행로직 분리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,55 +1,15 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.domain.RoundCount;
-import racingcar.domain.car.Car;
-import racingcar.domain.power.DefaultPowerGenerator;
+import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGameController {
 
-    private final InputView inputView;
-    private final OutputView outputView;
-
-    public RacingGameController(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
-
     public void runRacingGame() {
-        List<Car> cars = prepareRacingCars();
-        RoundCount roundCount = setRoundNumber();
-
-        System.out.println("실행 결과");
-
-        while (roundCount.hasNextRound()) {
-            executeRound(cars);
-            roundCount.consumeRound();
-        }
-        outputView.showGameResult(cars);
-
+        RacingGame racingGame = new RacingGame(new InputView(), new OutputView());
+        racingGame.run();
     }
 
-    private void executeRound(List<Car> cars) {
-        for (Car car : cars) {
-            car.tryDrive();
-        }
-        outputView.showRoundResult(cars);
-    }
 
-    private RoundCount setRoundNumber() {
-        RoundCount roundCount = inputView.readNumberOfRounds();
-        return roundCount;
-    }
-
-    private List<Car> prepareRacingCars() {
-        List<String> carNames = inputView.readCarNames();
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName, 0, new DefaultPowerGenerator()));
-        }
-        return cars;
-    }
 }
---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,54 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.car.Car;
+import racingcar.domain.power.DefaultPowerGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGame {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<Car> cars = prepareRacingCars();
+        RoundCount roundCount = setRoundNumber();
+
+        System.out.println("실행 결과");
+
+        while (roundCount.hasNextRound()) {
+            executeRound(cars);
+            roundCount.consumeRound();
+        }
+        outputView.showGameResult(cars);
+    }
+
+
+    private void executeRound(List<Car> cars) {
+        for (Car car : cars) {
+            car.tryDrive();
+        }
+        outputView.showRoundResult(cars);
+    }
+
+    private RoundCount setRoundNumber() {
+        RoundCount roundCount = inputView.readNumberOfRounds();
+        return roundCount;
+    }
+
+    private List<Car> prepareRacingCars() {
+        List<String> carNames = inputView.readCarNames();
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName, 0, new DefaultPowerGenerator()));
+        }
+        return cars;
+    }
+}
