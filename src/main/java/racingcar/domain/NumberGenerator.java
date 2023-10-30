feede5cbd2389be580bbc8e24c7822ed6364c6fa
siwol025 <cyk99115695@gmail.com>
2023-10-30 16:45:53 +0900

    feat(racingcar): 0 ~ 9까지의 무작위 값을 생성하는 기능 구현

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,7 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
     public int generateRandomNumber() {
-        return 0;
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
