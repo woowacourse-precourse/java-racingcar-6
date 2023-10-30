fb52e973b9b80c8170d23cc5a3941d8f28c3afa4
Woo Joo Chae <enaenen@naver.com>
2023-10-30 23:04:26 +0900

    FEAT: 자동차 이름으로 자동차 등록 책임을 가지는 CarRegistration

--- src/main/java/racingcar/CarRegistration.java
+++ src/main/java/racingcar/CarRegistration.java
@@ -1,39 +1,16 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.platform.commons.util.StringUtils;
-
 public class CarRegistration {
-    private static final int MIN_CAR_NAME_LENGTH = 2;
-    private static final String splitChar = ",";
+    private final RaceParticipants raceParticipants;
+    public CarRegistration(CarNameRegistration carNames) {
+        this.raceParticipants = new RaceParticipants();
 
-    private static List<CarName> carNameList;
-
-    private static void validateInputCarName(String[] splitCarName) throws IllegalArgumentException {
-        if (splitCarName.length < MIN_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.MINIMUM_CAR_NEEDED);
+        for (CarName carName : carNames.getCarNameList()) {
+            raceParticipants.addCar(new Car(carName));
         }
     }
-
-    private static void addToCarNameList(String[] splitCarName) throws IllegalArgumentException {
-        carNameList = new ArrayList<>();
-        for (String carName : splitCarName) {
-            carNameList.add(new CarName(carName));
-        }
+    public RaceParticipants getRaceParticipants() {
+        return raceParticipants;
     }
 
-    public static void input(String inputCarNames) throws IllegalArgumentException {
-        // static import 가능?
-        if (StringUtils.isBlank(inputCarNames)) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_BLANK);
-        }
-        String[] splitCarName = inputCarNames.split(splitChar);
-        validateInputCarName(splitCarName);
-        addToCarNameList(splitCarName);
-    }
-
-    public static List<CarName> getCarNameList() {
-        return carNameList;
-    }
 }
