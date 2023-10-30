114000431c5b1f76ad8bc473a6e29a69cc160910
mikekks <thdalsrb79@naver.com>
2023-10-30 21:27:56 +0900

    [feat] 입력 구현 및 Controller 호출 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,23 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.controller.BasicController;
+import racingcar.response.ProcessMessage;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(ProcessMessage.REQUIRE_CAR_NAME_MESSAGE);
+        String carList = Console.readLine();
 
+        System.out.println(ProcessMessage.REQUIRE_TRY_NUMBER_MESSAGE);
+        String tryInput = Console.readLine();
+        Integer tryNum = Integer.parseInt(tryInput);
+
+        BasicController controller = new BasicController(carList);
+        while(tryNum > 0){
+            controller.tryOneGame();
+            tryNum--;
+        }
     }
 }
