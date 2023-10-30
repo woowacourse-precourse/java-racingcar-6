9a7bb0ec8be58370fea4ec43611b9b63deaeacb9
jae woo Jung <awfjol2008@naver.com>
2023-10-30 21:23:17 +0900

    refector: GameCountValidator 클래스명 및 관련 변수 TryCountValidator로 변경

---
+++ src/test/java/racingcar/validator/TryCountValidator.java
@@ -0,0 +1,31 @@
+package racingcar.validator;
+
+import static java.lang.Character.*;
+import static java.lang.Integer.*;
+
+public class TryCountValidator {
+
+    public static void validate(String tryCount) {
+        validateIsEmpty(tryCount);
+        validateIsDigit(tryCount);
+        validateSign(tryCount);
+    }
+
+    private static void validateIsEmpty(String tryCount) {
+        if (tryCount.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
+
+    private static void validateIsDigit(String tryCount) {
+        if (!isDigit(parseInt(tryCount))) {
+            throw new IllegalArgumentException("입력값은 정수이어야 합니다.");
+        }
+    }
+
+    private static void validateSign(String tryCount) {
+        if (parseInt(tryCount) <= 0) {
+            throw new IllegalArgumentException("입력값은 양의 정수이어야 합니다.");
+        }
+    }
+}
