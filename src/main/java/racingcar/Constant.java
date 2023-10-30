a576807acf235d758701a16d4575098b5b8172de
ohhyeon2 <dhgus1235@gmail.com>
2023-10-30 19:43:48 +0900

    feat: Constant class 생성
    고정적으로 사용되는 문자열, 숫자 값 상수화

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Constant {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String GAME_RESULT = "실행 결과";
+    public static final String WINNER_LIST = "최종 우승자 : ";
+    public static final String MOVE_FORWARD = "-";
+
+    public static final String COMMA = ",";
+    public static final String COMMA_SPACE = ", ";
+
+    public static final int MAX_CAR_NAME_SIZE = 5;
+
+    public static final int MIN_RANDOM_NUMBER_RANGE = 0;
+    public static final int MAX_RANDOM_NUMBER_RANGE = 9;
+
+    public static final int FORWARD_STANDARD = 4;
+}
