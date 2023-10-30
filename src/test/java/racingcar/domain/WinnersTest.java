154312a7df2b0ae173f11245dee277a43d2eb7dc
SongJwans <yhh5916@naver.com>
2023-10-30 21:22:15 +0900

    test: test_EmptyCarsList() 추가

--- src/test/java/racingcar/domain/WinnersTest.java
+++ src/test/java/racingcar/domain/WinnersTest.java
@@ -27,25 +27,38 @@ public class WinnersTest {
 
         List<Car> cars = Arrays.asList(car1, car2, car3);
         Winners winners = new Winners(cars);
-        assertEquals(2, winners.getMaxDistance());
+
+        int expectedMaxDistance = 2;
+        int actualMaxDistance = winners.getMaxDistance();
+
+        assertEquals(expectedMaxDistance, actualMaxDistance);
     }
 
     @Test
-    public void test_WinningCars() {
+    public void test_getWinningCarNames() {
         Car car1 = new Car("Car1");
         car1.move();
+
         Car car2 = new Car("Car2");
         car2.move();
         car2.move();
+
         Car car3 = new Car("Car3");
         car3.move();
         car3.move();
 
         List<Car> cars = Arrays.asList(car1, car2, car3);
         Winners winners = new Winners(cars);
-        List<Car> winningCars = winners.getWinningsCars();
+
+        List<Car> winningCars = winners.getWinningCars();
 
         assertTrue(winningCars.contains(car2));
         assertTrue(winningCars.contains(car3));
+    }
+
+    @Test
+    public void test_EmptyCarsList() {
+        List<Car> cars = Arrays.asList();
+        assertThrows(IllegalArgumentException.class, () -> new Winners(cars));
     }
 }
