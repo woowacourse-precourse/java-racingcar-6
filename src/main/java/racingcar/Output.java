377eb3da629d95413d9d1dfe8adb62ec6ce88a28
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 23:49:40 +0900

    refactor: [Output] getWinners메서드의 구현부분 람다식으로 변경, 우승자 모으는 리스트의 변수 이름 변경

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -2,15 +2,14 @@ package racingcar;
 
 import static java.util.stream.Collectors.toList;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Output {
     private static final String IS = " : ";
     private static final String BAR = "-";
-    private static final String COMMA_BLANK = ", ";
-    private static final String FINAL_WINNER = "최종우승자" + IS;
+    private static final String COMMA_AND_BLANK = ", ";
+    private static final String FINAL_WINNER = "최종 우승자" + IS;
 
     public void printRacingProcess(Map<String, Integer> carNameAndMoving) {
         for (String carName : carNameAndMoving.keySet()) {
@@ -22,9 +21,9 @@ public class Output {
 
     public void printRacingWinner(Map<String, Integer> carNameAndMoving) {
         int maxMoving = getCarWithsMaxMoving(carNameAndMoving);
-        List<String> winnerList = getWinnerList(carNameAndMoving, maxMoving);
-        String winners = String.join(COMMA_BLANK, winnerList);
-        System.out.println(FINAL_WINNER + winners);
+        List<String> winners = getWinners(carNameAndMoving, maxMoving);
+        String winner = String.join(COMMA_AND_BLANK, winners);
+        System.out.println(FINAL_WINNER + winner);
     }
 
     private StringBuilder appendBar(int count) {
@@ -43,13 +42,10 @@ public class Output {
         return maxMoving;
     }
 
-    private List<String> getWinnerList(Map<String, Integer> carNameAndMoving, int maxMoving) {
-        List<String> winnerList = new ArrayList<>();
-        for (String carName : carNameAndMoving.keySet()) {
-            if (carNameAndMoving.get(carName) == maxMoving) {
-                winnerList.add(carName);
-            }
-        }
-        return winnerList;
+    private List<String> getWinners(Map<String, Integer> carNameAndMoving, int maxMoving) {
+        return carNameAndMoving.entrySet().stream()
+                .filter(name -> name.getValue() == maxMoving)
+                .map(Map.Entry::getKey)
+                .collect(toList());
     }
 }
