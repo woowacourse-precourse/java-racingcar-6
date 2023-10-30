f932c97daf633f3d18de15969a5b26ed44d8370d
Young1 <dlduddnjs198@naver.com>
2023-10-30 17:01:54 +0900

    test : 우승자 출력 테스트 작성

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,7 +27,7 @@ class GameTest {
     public void 게임_횟수입력_실패() {
         String carNames = "pobi,woni,jun";
         String attemptsString = "five";  // 숫자가 아닌 문자열
-        String attemptsNegative = "-3";
+        String attemptsNegative = "-3";  // 음수
 
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Game(carNames, attemptsString))
@@ -33,6 +36,38 @@ class GameTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Game(carNames, attemptsNegative))
                 .withMessage("시도 횟수는 양수여야 합니다.");
+    }
+
+    @Test
+    public void 우승자_출력() {
+        Game game = new Game("pobi,woni,jun", "1");
+        game.getCars().get(0).move(4);
+        game.getCars().get(0).move(4);
+
+        String winners = game.printWinner();
+
+        assertThat(winners).isEqualTo("pobi");
+    }
+
+    @Test
+    public void 우승자_여러명_출력() {
+        Game game = new Game("pobi,woni,jun", "1");
+        List<Car> cars = game.getCars();
+        int i=0;
+
+        for(Car car : cars){
+            if(car.getPosition()!=0){
+                game.getCars().get(i).move(4);
+                game.getCars().get(i).move(4);
+            } else{
+                game.getCars().get(i).move(4);
+            }
+            i++;
+        }
+
+        String winners = game.printWinner();
+
+        assertThat(winners).isEqualTo("pobi, woni, jun");
     }
 
 }
