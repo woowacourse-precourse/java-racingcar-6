c94fd32d7aa8bf4ddbdd996b7d81062355be3838
HYOAN <aspqll@naver.com>
2023-10-30 19:19:21 +0900

    feat(racingcar): 자동차의 이름을 입력받고 쉼표로 구분하는 기능 구현, 시도 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -1,13 +1,26 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class UserInput {
     public UserInput() {
     }
-    public String carName() {
-        return "";
+    public List<String> carName() {
+        String input = Console.readLine();;
+        String[] names = input.split(",");
+        List<String> carNames = new ArrayList<>(Arrays.asList(names));
+//        System.out.println(carNames);
+        return carNames;
     }
     public int attemptCount() {
-        return 0;
+        int attemptCount;
+        attemptCount = Integer.parseInt(Console.readLine());
+//        System.out.println(attemptCount);
+        return attemptCount;
     }
     public void processException() {
     }
