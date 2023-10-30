f1dea664fbd70d0d82ea3430ddcea73db56bc3ec
kkw2758 <dtc3229@gmail.com>
2023-10-31 00:12:06 +0900

    test : CarsTest, carTest DisplayName 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constant.Error;
@@ -11,6 +12,7 @@ class CarTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"ABCDEF", ""})
+    @DisplayName("유효하지 않은 자동차 이름 검증 테스트")
     void 자동차의_이름은_길이는_공백과_5자_초과를_허용하지_않는다(String carName) {
         assertThatThrownBy(() -> new Car(carName))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -18,6 +20,7 @@ class CarTest {
     }
 
     @ParameterizedTest
+    @DisplayName("유효한 자동차 이름 검증 테스트")
     @ValueSource(strings = {"벤츠", "아우디"})
     void 자동차의_이름은_1자_이상_5자_이하만_가능하다(String carName) {
         assertDoesNotThrow(() -> new Car(carName));
--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -2,11 +2,14 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.constant.Error;
 
 class CarsTest {
+
     @Test
+    @DisplayName("중복된 자동차의 이름 확인 테스트")
     void 중복된_자동차_이름_확인() {
         String[] stringArray = {"A", "A", "B"};
         assertThatThrownBy(() -> new Cars(stringArray))
