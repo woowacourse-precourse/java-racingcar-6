87633daad15ba464f9f9a4f95b48cedd5320cce1
jaea-kim <jaeakim98@gmail.com>
2023-10-30 21:08:45 +0900

    FEAT: 게임에 필요한 값 enum 타입으로 정의

---
+++ src/main/java/racingcar/GameConfig.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public enum GameConfig {
+    MAX_CAR_COUNT(9),
+    MIN_CAR_COUNT(1),
+    MAX_CAR_NAME(5),
+    MIN_RANDOM_NUMBER(0),
+    MAX_RANDOM_NUMBER(9);
+
+    private final int value;
+
+    GameConfig(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
