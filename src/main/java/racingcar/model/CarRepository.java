e9d02bcc8107d4ca03e81ddcedc85179e8258823
Haeun Jeong <gkdms4248@gmail.com>
2023-10-30 17:42:00 +0900

    feat(model): 우승자 찾는 기능 추가

--- src/main/java/racingcar/model/CarRepository.java
+++ src/main/java/racingcar/model/CarRepository.java
@@ -2,10 +2,12 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarRepository {
     private final List<Car> cars = new ArrayList<>();
-    private final static int maxCarCount = 100;
+    private final static int MAX_CAR_COUNT = 100;
+    private final static String DUPLICATE_NAME_MESSAGE = "같은 이름의 자동차 이름이 존재합니다.";
 
     public void save(Car car) {
         validateDuplicateName(car.getName());
@@ -14,11 +16,28 @@ public class CarRepository {
 
     private void validateDuplicateName(String carName) {
         if (cars.stream().anyMatch(car -> car.getName().equals(carName))) {
-            throw new IllegalArgumentException("같은 이름의 자동차 이름이 존재합니다.");
+            throw new IllegalArgumentException(DUPLICATE_NAME_MESSAGE);
         }
     }
 
-    public List<Car> getCars() {
-        return this.cars;
+    public List<CarDto> getCarDto() {
+        return cars.stream()
+                .map(car -> new CarDto(car))
+                .toList();
+    }
+
+    public List<String> getWinner() {
+        int max = getMaxPosition();
+        return cars.stream()
+                .filter(c -> c.getPosition() == max)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
     }
 }
