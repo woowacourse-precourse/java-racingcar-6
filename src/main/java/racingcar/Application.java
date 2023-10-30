f4bee782283b327989329e2660cdab57bfbb1838
taewan625 <taewan1997625@gmail.com>
2023-10-31 08:21:19 +0900

    Refactor: Fix "Application"
    의미를 파악하기 쉬운 변수명으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,16 +9,17 @@ public class Application {
         RacingGame racingGame = new RacingGame();
 
         print.carName();
-        Map<String, Integer> carNames = input.carNames(); // todo tdd
+        Map<String, Integer> carsData = input.carNames(); // todo tdd
         print.tryCount();
         int tryCount = input.tryCount(); // todo tdd
 
         print.executeInit();
         for (int i = 0; i < tryCount; i++) {
             // todo 게임 수행
-            racingGame.executeCondition(carNames);
-            print.executeProgress(carNames);
+            racingGame.executeCondition(carsData);
+            print.executeProgress(carsData);
         }
-        print.result(tryCount, carNames);
+        int maxValue = racingGame.maxValue(tryCount, carsData);
+        print.result(maxValue, carsData);
     }
 }
