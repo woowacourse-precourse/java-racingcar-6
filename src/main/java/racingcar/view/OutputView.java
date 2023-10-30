173d3cd3a80c62592943f9ce0990c04ee37b9d67
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 08:14:39 +0900

    feat : 실행 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,9 +2,14 @@ package racingcar.view;
 
 public class OutputView {
     private static final String FORWARD_SYMBOL = "-";
+    private static final String GAME_RESULT = "실행결과";
 
     public void printForward(String carName, int forward){
         String forwardSymbol = FORWARD_SYMBOL.repeat(forward);
         System.out.println(carName + " : " + forwardSymbol);
+    }
+    public void printResult(){
+        System.out.println("");
+        System.out.println(GAME_RESULT);
     }
 }
