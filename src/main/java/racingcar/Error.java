b2b997704d7f1f387f59108eb986ef4cb9fac816
JDUB9823 <jeong98231@gmail.com>
2023-10-30 10:44:38 +0900

    feat: 자동차 이름 중복입력 예외처리

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 
 public class Error extends Throwable {
     final public static int INVALID_NAME_LENGTH = 1;
+    final public static int DUPLICATE_NAME = 2;
 
     final static HashMap<Integer, String> errors = new HashMap<>(){{
         put(INVALID_NAME_LENGTH, "이름의 길이는 1 이상 5 이하만 가능합니다.");
+        put(DUPLICATE_NAME, "중복된 이름은 입력 불가능합니다.");
     }};
 
     public static void throwError(int errorCode){
