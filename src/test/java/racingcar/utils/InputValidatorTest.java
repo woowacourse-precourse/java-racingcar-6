d9ea1ccc99d396d88c62560bfee2a784d0a1a0a3
isyoo1213 <moethns@gmail.com>
2023-10-30 16:41:54 +0900

    test: InputValidator 입력값 전처리 기능 테스트 추가 InputValidatorTest - 입력값 전처리 테스트 - Null 데이터 - 오류 - 입력값 전처리 테스트 - 빈 문자열 데이터 - 오류 - 입력값 전처리 테스트 - 공백 포함 데이터 - 성공

---
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class InputValidatorTest {
+
+    InputValidator inputValidator = new InputValidator();
+
+    @Test
+    @DisplayName("입력값 전처리 테스트 - Null 데이터 - 오류")
+    void preprocessNullInputTest() {
+        assertThatThrownBy(() -> inputValidator.preprocessInputString(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력값 전처리 테스트 - 빈 문자열 데이터 - 오류")
+    void preprocessEmptyInputTest() {
+        assertThatThrownBy(() -> inputValidator.preprocessInputString(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력값 전처리 테스트 - 공백 포함 데이터 - 성공")
+    @ValueSource(strings = {"po bi"})
+    @ParameterizedTest
+    void preprocessSpacingInputTest(String names) {
+        assertThat(inputValidator.preprocessInputString(names)).isEqualTo("pobi");
+    }
+}
