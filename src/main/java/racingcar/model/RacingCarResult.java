1e4053fe5ab9910ce2a5ba0d9a2d46308b3e8c31
eunhee <dmsgml9903@gmail.com>
2023-10-30 17:01:47 +0900

    refactor(RacingCarResult): view에 해당하는 RacingCarView 클래스에서 문장을 만드는 로직 분리
    아래 내용을 리팩토링:
    - 문장을 만드는 로직을 RacingCarResult로 분리
    - 뷰에 해당하는 RacingCarView에서 RacingCarView를 이용해서 출력

---
+++ src/main/java/racingcar/model/RacingCarResult.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class RacingCarResult {
+
+    public static final String ONE_STEP = "-";
+    public static final String COLON = ":";
+    private static final String FINAL_WINNER = "최종 우승자";
+
+    public static String carMovementResult(Car car) {
+        String movementDistance = carMovement(car);
+        return String.join(" ", car.toString(), COLON, movementDistance);
+    }
+
+    public static String winnersResult(List<Car> winners) {
+        String winnersString = convertWinnersToString(winners);
+        return String.join(" ", FINAL_WINNER, COLON, winnersString);
+    }
+
+    private static String carMovement(Car car) {
+        return ONE_STEP.repeat(car.getMovingDistance());
+    }
+
+    private static String convertWinnersToString(List<Car> winners) {
+        return String.join(",", winners.toString().replace("[", "").replace("]", ""));
+    }
+}
--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import static racingcar.model.RacingCarResult.carMovementResult;
+import static racingcar.model.RacingCarResult.winnersResult;
+
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.RacingCar;
@@ -9,9 +12,6 @@ public class RacingCarView {
     private static final String RACING_CAR_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASKING_NUMBER_OF_RETRIES = "시도할 회수는 몇회인가요?";
     private static final String RESULT_PER_RACING = "실행 결과";
-    private static final String FINAL_WINNER = "최종 우승자";
-    private static final String COLON = ":";
-    private static final String ONE_STEP = "-";
 
     public void printStartRacingCar() {
         System.out.println(RACING_CAR_START);
@@ -31,18 +31,12 @@ public class RacingCarView {
     }
 
     public void printFinalMessage(List<Car> winners) {
-        String winnersString = convertWinnersToString(winners);
-        System.out.print(String.join(" ", FINAL_WINNER, COLON, winnersString));
-
-    }
-
-    private String convertWinnersToString(List<Car> winners) {
-        return String.join(",", winners.toString().replace("[", "").replace("]", ""));
+        String winnersResult = winnersResult(winners);
+        System.out.print(winnersResult);
     }
 
     private void printCarMovement(Car car) {
-        String movementDistance = ONE_STEP.repeat(car.getMovingDistance());
-        System.out.println(String.join(" ", car.toString(), COLON, movementDistance));
+        String carMovementResult = carMovementResult(car);
+        System.out.println(carMovementResult);
     }
-
 }
