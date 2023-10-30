a4dfb82bc6f3ceef13785652899d49f0cc86a598
kkw2758 <dtc3229@gmail.com>
2023-10-30 19:15:11 +0900

    test : 자동차 이름 검증 테스트 구체화
    - ParameterizedTest를 이용하여 자동차 이름이 5자리 초과인 경우와 공백인 경우 에러 발생 체크 기능추가
    - 자동차 이름이 1자리 이상 5자리 이하 일때 에러가 발생하지 않는 것을 체크 하는 기능 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,20 +1,25 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constant.Error;
 
 class CarTest {
 
-    @Test
-    void 자동차의_이름은_1자_이상_5자_이하만_가능하다() {
-        assertThatThrownBy(() -> new Car("ABCDEF"))
+    @ParameterizedTest
+    @ValueSource(strings = {"ABCDEF", ""})
+    void 자동차의_이름은_길이는_공백과_5자_초과를_허용하지_않는다(String carName) {
+        assertThatThrownBy(() -> new Car(carName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Error.CAR_NAME_LENGTH_ERROR.getMessage());
+    }
 
-        assertThatThrownBy(() -> new Car(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(Error.CAR_NAME_LENGTH_ERROR.getMessage());
+    @ParameterizedTest
+    @ValueSource(strings = {"벤츠", "아우디"})
+    void 자동차의_이름은_1자_이상_5자_이하만_가능하다(String carName) {
+        assertDoesNotThrow(() -> new Car(carName));
     }
 }
