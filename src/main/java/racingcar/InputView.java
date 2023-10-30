5d8f8ce9fa3c667fc4b93f5df9429109e8a5be07
dokkisan <dokkisan.wang@gmail.com>
2023-10-30 19:54:52 +0900

    feat(input-view): 입력 정보 제공을 위한 메시지 출력 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,11 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-	public String askTryCount() {
+	public String askCarName() {
+		final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+		System.out.println(INPUT_MESSAGE);
+
 		return Console.readLine();
 	}
 
-	public String askCarName() {
+	public String askTryCount() {
+		final String INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+		System.out.println(INPUT_MESSAGE);
+
 		return Console.readLine();
 	}
 }
