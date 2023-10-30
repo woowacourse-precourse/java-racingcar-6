787b34b7546df22b96668474072699184cec372a
jaea-kim <jaeakim98@gmail.com>
2023-10-31 00:11:16 +0900

    FEAT: 랜덤값 유틸 함수 구현

---
+++ src/main/java/racingcar/RandomUtil.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtil {
+    public static int getRandomNumber() {
+
+        return Randoms.pickNumberInRange(GameConfig.MIN_RANDOM_NUMBER.getValue(), GameConfig.MIN_RANDOM_NUMBER.getValue());
+    }
+}
