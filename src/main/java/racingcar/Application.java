c571b4378d81de7ee8425e36401c3abd9364c66f
opixxx <opix0306@naver.com>
2023-10-30 17:57:44 +0900

    refactor(Application): 주입 받은 객체를 사용하도록 RacingCarController 수정
    main 메서드에서 InputView, OutputView, CarController 객체를 생성하고 이름 RacingCarGameController 의 생성자로 전달하였다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,18 @@
 package racingcar;
 
+import racingcar.controller.CarController;
 import racingcar.controller.RacingCarGameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarGameController gameController = new RacingCarGameController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CarController carController = new CarController();
+
+        RacingCarGameController gameController = new RacingCarGameController(inputView, outputView, carController);
         gameController.play();
     }
 }
