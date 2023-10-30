c1066f07de8a8896ffb26eb97d8dbfdeaf727393
hyerimmy <heyrim2010@naver.com>
2023-10-31 00:47:13 +0900

    chore: 입력값 예외처리 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -12,41 +12,39 @@ public class GameController {
     private static final ArrayList<Car> carList = new ArrayList<>();
     private static int round;
 
-    public void setGame(){
+    public void setGame() {
         setCars();
         setRoundNumber();
         System.out.println();
     }
 
-    public void printResults(){
+    public void printResults() {
         ResultView.printResultTitle();
-        for(int r=0; r<round; r++){
+        for (int r = 0; r < round; r++) {
             printResultOfRound();
         }
     }
 
-    public void printWinner(){
+    public void printWinner() {
         ArrayList<String> winnerNameList = getWinnerNameList();
         ResultView.printWinner(winnerNameList);
     }
 
-    public static void setCars(){
+    public static void setCars() {
         SettingView.printInputCarNameMsg();
         String carNames = Console.readLine();
-        // TODO : valid check
         setCarList(carNames);
     }
 
-    public static void setRoundNumber(){
+    public static void setRoundNumber() {
         SettingView.printInputRoundNumberMsg();
         String roundNumber = Console.readLine();
-        // TODO : valid check
         round = Integer.parseInt(roundNumber);
     }
 
-    public void printResultOfRound(){
-        for(Car car : carList){
-            if(Randoms.pickNumberInRange(0,9)>=4){
+    public void printResultOfRound() {
+        for (Car car : carList) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
                 car.move();
             }
             ResultView.printCarState(car.name, car.position);
@@ -54,24 +52,31 @@ public class GameController {
         System.out.println();
     }
 
-    public ArrayList<String> getWinnerNameList(){
+    public ArrayList<String> getWinnerNameList() {
         ArrayList<String> winnerNameList = new ArrayList<>();
         int max = 0;
-        for(Car car : carList){
-            if(car.position>max) {
+        for (Car car : carList) {
+            if (car.position > max) {
                 max = car.position;
                 winnerNameList.clear();
             }
-            if(car.position==max){
+            if (car.position == max) {
                 winnerNameList.add(car.name);
             }
         }
         return winnerNameList;
     }
 
-    public static void setCarList(String carNames){
-        for(String carName : carNames.split(",")){
-            carList.add(new Car(carName));
+    public static void setCarList(String carNames) {
+        for (String carName : carNames.split(",")) {
+            if (isValidCarName(carName)) carList.add(new Car(carName));
         }
+    }
+
+    public static boolean isValidCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이내만 가능합니다.");
+        }
+        return true;
     }
 }
