8f3ec7d2008fa439226f453b5e919f3900691470
NamJihyun99 <namjihyun9936@gmail.com>
2023-10-30 20:26:47 +0900

    feat:> Race 객체- 경주 실행 함수

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import java.util.List;
+
+public class Race {
+    private final List<Car> candidates;
+    private final int roundNumber;
+
+    public Race(List<String> carNames, int roundNumber) {
+        this.candidates = carNames.stream().map(Car::new).toList();
+        this.roundNumber = roundNumber;
+    }
+
+    public void play() {
+        System.out.println("실행 결과");
+        for (int round = 0; round < roundNumber; round++) {
+            for (Car car : candidates) {
+                car.race();
+                car.showStatus();
+            }
+            System.out.println();
+        }
+    }
+}
