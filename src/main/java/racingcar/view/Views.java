90bb36a4385f86c187ae75374923ee525bf380c2
Seyeon <wjdtpdus828@naver.com>
2023-10-31 00:26:50 +0900

    feat(Views): "실행 결과" 메세지를 출력하는 printResultMessage 메소드 구현

--- src/main/java/racingcar/view/Views.java
+++ src/main/java/racingcar/view/Views.java
@@ -3,11 +3,15 @@ package racingcar.view;
 public class Views {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_NESSAGE = "실행 결과";
     public void printInputCarMessage(){
         System.out.println(INPUT_CAR_NAME_MESSAGE);
     }
 
     public void printInputTrialMessage(){
         System.out.println(INPUT_TRIAL_NUMBER);
+    }
+    public void printResultMessage(){
+        System.out.println(RESULT_NESSAGE);
     }
 }
