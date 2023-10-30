bfd299e150b56d416ab6212bad7a68a6a2ceffda
HongYeseul <ghd8119@naver.com>
2023-10-31 07:53:54 +0900

    :sparkles: feat: Car 클래스 만들기

--- src/main/java/racingcar/game/Car.java
+++ src/main/java/racingcar/game/Car.java
@@ -1,4 +1,21 @@
 package racingcar.game;
 
 public class Car {
+    private String name;
+    private Integer score;
+
+    public Car(String name){
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void moveForward(){
+        // TODO: 상수 선언
+        this.score += 1;
+    }
+
+    public void stop(){
+        // TODO: 상수 선언
+        this.score += 0;
+    }
 }
