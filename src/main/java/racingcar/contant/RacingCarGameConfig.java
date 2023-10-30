cde9f6c06d8ba88eb1a6634dc2725c599b16d53f
Kite_Z <duswl339@ajou.ac.kr>
2023-10-30 23:41:17 +0900

    refactor: add config number and modify constant

--- src/main/java/racingcar/contant/RacingCarGameConfig.java
+++ src/main/java/racingcar/contant/RacingCarGameConfig.java
@@ -2,9 +2,11 @@ package racingcar.contant;
 
 public class RacingCarGameConfig {
     private RacingCarGameConfig() {}
-    public static int CAR_NAME_MAX_LENGTH = 5;
-    public static int CAR_NAME_MIN_LENGTH = 1;
-    public static int RANDOM_MAX_NUMBER = 9;
-    public static int RANDOM_MIN_NUMBER = 1;
-    public static int STANDARD_NUMBER = 4;
+    public static final int CAR_NAME_MAX_LENGTH = 5;
+    public static final int CAR_NAME_MIN_LENGTH = 1;
+    public static final int RANDOM_MAX_NUMBER = 9;
+    public static final int RANDOM_MIN_NUMBER = 1;
+    public static final int STANDARD_NUMBER = 4;
+    public static final int PARTICIPANT_MIN_NUMBER = 1;
+    public static final int INTEGER_INPUT_MAX = 2147483647;
 }
