c698c8c09cb58594687c4e7f3af2208ea41c5348
Infikei <hckhong@naver.com>
2023-10-30 19:38:46 +0900

    refactor: 오류 메시지 추가
    - IllegalArgumentException 오류가 발생할 때 오류가 발생한 이유를 오류 메시지로 추가

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RacingCars {
+    private static final String EMPTY_NAME_ERROR_MESSAGE = "이름으로 빈 문자열을 입력하여 오류가 발생하였습니다.";
+    private static final String LONG_NAME_ERROR_MESSAGE = "5자 이하가 아닌 이름을 입력하여 오류가 발생하였습니다.";
     private List<RacingCar> racingCars;
 
     private RacingCars(final List<String> names) {
@@ -27,14 +29,14 @@ public class RacingCars {
 
     private static void validateNames(final List<String> names) {
         if (names.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EMPTY_NAME_ERROR_MESSAGE);
         }
 
         for (String name : names) {
             if (name.isEmpty()) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(EMPTY_NAME_ERROR_MESSAGE);
             } else if (name.length() > 5) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(LONG_NAME_ERROR_MESSAGE);
             }
         }
     }
