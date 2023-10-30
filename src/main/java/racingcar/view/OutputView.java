b48997bc6dafd640e5d7eca69b6c5f11584487cb
hunko <rhwlgns4386@naver.com>
2023-10-30 18:59:06 +0900

    feat(OutputView) : printStatus 함수 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,15 +1,26 @@
 package racingcar.view;
 
+import java.util.LinkedHashMap;
+import racingcar.dto.StatusResponse;
+
 public class OutputView {
 
     private static final String PLAY_RESULT_MESSAGE = "실행 결과";
+    private static final String HYPHEN = "-";
 
-    public static void printPlayResultMessage(){
+    public static void printPlayResultMessage() {
         print(PLAY_RESULT_MESSAGE);
+    }
+
+    public static void printStatus(StatusResponse response) {
+        LinkedHashMap<String, Integer> status = response.getStatus();
+        for(String name:status.keySet()){
+            print(String.format("%s : %s",name,HYPHEN.repeat(status.get(name))));
+        }
+        print("");
     }
 
     private static void print(String message) {
         System.out.println(message);
     }
-
 }
