11709ef75fbb6ff150ee1cc8ae763441a2d86e4b
ay-eonii <naayen@naver.com>
2023-10-31 02:41:18 +0900

    feat: 우승자 판단하는 메서드 추가

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,5 +1,8 @@
 package racingcar.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Referee {
     private final static int judgeStandard = 4;
 
@@ -7,9 +10,14 @@ public class Referee {
         return number >= judgeStandard;
     }
 
-    public void judgeNumber() {
-    }
-
-    public void judgeWinner() {
+    public List<String> judgeWinner(List<Car> carList) {
+        int max = carList.stream()
+                .mapToInt(Car::getLocation)
+                .max()
+                .orElse(0);
+        return carList.stream()
+                .filter(car -> car.getLocation() == max)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
