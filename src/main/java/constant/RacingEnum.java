5ff5b9b0c1ff16ec0cef4edf385dfe089c59bda1
Sur0n <skyzephyr333@gmail.com>
2023-10-31 02:01:05 +0900

    feat : Write down RacingEnum

--- src/main/java/constant/RacingEnum.java
+++ src/main/java/constant/RacingEnum.java
@@ -1,4 +1,49 @@
 package constant;
 
-public class RacingEnum {
+//Enum 셋업
+// 0 9 4 magic numbers
+// 경주
+// 시도
+// 결과
+// 진척도 -
+// :
+// 최종우승자
+enum RacingEnums {
+    RANDOM_MIN, RANDOM_MAX, NUM_CHECK_GO,
+    ASK_CAR_NAME, ASK_GAME_TRYS, CAR_NAME_COLON,
+    RACING_RESULT, RACING_DISTANCE, RACING_WINNER
+
 }
+
+enum MagicNumbers {
+    RANDOM_MIN("0"), RANDOM_MAX("9"), NUM_CHECK_GO("4");
+    private final String Numbers;
+
+    MagicNumbers(String Numbers) {
+        this.Numbers = Numbers;
+    }
+
+    String getRandomNumbers() {
+        return Numbers;
+    }
+}
+
+enum GameUserInterface {
+    // %d 등 인수 작업 가능
+    ASK_CAR_NAME("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"),
+    ASK_GAME_TRYS("시도할 회수는 몇회인가요?"),
+    CAR_NAME_COLON(":"),
+    RACING_RESULT("실행 결과"),
+    RACING_DISTANCE("-"),
+    RACING_WINNER("최종 우승자");
+
+    private final String UserInterface;
+
+    GameUserInterface(String UserInterface) {
+        this.UserInterface = UserInterface;
+    }
+    String getRandomNumbers() {
+        return UserInterface;
+    }
+}
+
