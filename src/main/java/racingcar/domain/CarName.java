242b6dd8c5ca7f158acd11d7a8003879b23628f4
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 17:51:47 +0900

    [feat] : add is car name null method

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -4,10 +4,17 @@ public class CarName {
     private String name;
 
     public CarName(String carName) {
-        validateCarName(carName);
+        this.name = carName;
+        validateCarName();
     }
 
-    public void validateCarName(String carName) {
+    public void validateCarName() {
+        isCarNameNull();
+    }
 
+    private void isCarNameNull() {
+        if (name.length() > 0) {
+            throw new IllegalArgumentException("입력 값이 없습니다.");
+        }
     }
 }
