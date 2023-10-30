ab8b23e5d1236f8df65ab7a70f323edee5acf754
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-30 13:54:29 +0900

    feat: Car 추가
    자동차 클래스 Car 추가
    - 이름과 위치를 필드값으로 갖는다
    - 생성자에서 validateNameLength를 통해 이름 길이가 5 이하인지 검증한다
    - 객체 생성 시 위치 초기값은 0이다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public class Car {
+
+    private static final int NAME_LENGTH_LIMIT = 5;
+
+    private String name;
+    private Integer position;
+
+    public Car(String name) {
+        validateNameLength(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
