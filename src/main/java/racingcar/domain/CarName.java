f560ee4ae6c594674cf345bd30b8910ed0ec71cc
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 17:55:14 +0900

    [feat] : add is comma present method

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -10,11 +10,18 @@ public class CarName {
 
     public void validateCarName() {
         isCarNameNull();
+        isCommaPresent();
     }
 
     private void isCarNameNull() {
         if (name.length() > 0) {
             throw new IllegalArgumentException("입력 값이 없습니다.");
+        }
+    }
+
+    private void isCommaPresent() {
+        if (name.contains(",")) {
+            throw new IllegalArgumentException(",(쉼표)로 자동차 이름을 구분해주세요.");
         }
     }
 }
