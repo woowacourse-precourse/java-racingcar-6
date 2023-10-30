cef130f4d5f6dfac2c724d31fa4348813d0739d8
leedohyun <lucky77795@naver.com>
2023-10-30 22:16:32 +0900

    refactor(racingCar) : 예외 메시지 추가.

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,6 +1,9 @@
 package racingcar.validator;
 
+import static racingcar.gameutil.GameConstants.*;
+
 import java.util.Arrays;
+import racingcar.gameutil.GameConstants;
 
 public class InputValidator {
 
@@ -16,15 +19,15 @@ public class InputValidator {
         checkNameLengthOverFive(names);
     }
 
-    private void checkValidatedCarName(String carName) {
-        if (!carName.matches(CAR_NAME_REGEX)) {
-            throw new IllegalArgumentException();
+    public void validateInputTryCount(String tryCount) {
+        if (!tryCount.matches(TRY_COUNT_REGEX)) {
+            throw new IllegalArgumentException("맨 앞의 0은 허용되지 않습니다. 알맞은 숫자를 입력해주세요.");
         }
     }
 
-    public void validateInputTryCount(String tryCount) {
-        if (!tryCount.matches(TRY_COUNT_REGEX)) {
-            throw new IllegalArgumentException();
+    private void checkValidatedCarName(String carName) {
+        if (!carName.matches(CAR_NAME_REGEX)) {
+            throw new IllegalArgumentException(",뒤 공백은 허용되지 않습니다.");
         }
     }
 
@@ -34,20 +37,20 @@ public class InputValidator {
                 .count();
 
         if (nonDuplicatedNameSize != names.length) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("자동차의 이름은 중복될 수 없습니다.");
         }
     }
 
     private void checkParticipantSizeUnderTwo(String[] names) {
-        if (names.length < 2) {
-            throw new IllegalArgumentException();
+        if (names.length < CAR_SIZE_MIN) {
+            throw new IllegalArgumentException("참가하는 자동차는 2대 이상이어야 합니다.");
         }
     }
 
     private void checkNameLengthOverFive(String[] names) {
         for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException();
+            if (name.length() > CAR_NAME_MAX_LEN) {
+                throw new IllegalArgumentException("자동차 이름의 길이는 5자를 넘을 수 없습니다.");
             }
         }
     }
