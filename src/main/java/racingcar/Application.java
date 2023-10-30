a3044e5bd415e72778f908d82b402b68cdc4a49b
Yerinnnn <shinyerin0216@gmail.com>
2023-10-31 01:56:56 +0900

    Refactor: "사용하지 않는 import 정리"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,4 @@
 package racingcar;
-
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
