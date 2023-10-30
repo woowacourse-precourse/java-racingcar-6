11233bde80d3d49070fa896c240422615516f853
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:30:04 +0900

    [feat] Add GameManager class choiceWinner method

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.CarStorage;
 import racingcar.view.UserView;
 
 public class GameManager {
@@ -11,5 +13,17 @@ public class GameManager {
     public GameManager() {
         userView = new UserView();
         user = new User();
+    }
+
+
+    private void choiceWinner() {
+        Integer tempCarStep;
+        Integer maxStep = CarStorage.getMaxSteps();
+        for (Car tempCar : CarStorage.cars()) {
+            tempCarStep = tempCar.steps();
+            if (tempCarStep.equals(maxStep)) {
+                CarStorage.addWinner(tempCar);
+            }
+        }
     }
 }
