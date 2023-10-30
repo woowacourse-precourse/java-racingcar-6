8fc507c0403eb1eb6fe1a163d45f222382bae373
최진원 <etudent39@gmail.com>
2023-10-30 20:30:11 +0900

    Feat : Car class 추가 이름 이동거리 두 개의 인스턴스 변수 설정

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private String distance;
+
+    public Car(String name){
+        this.name = name;
+        this.distance = "";
+
+    }
+    public final String  getName(){
+        return this.name;
+    }
+
+    public final String getDistance(){
+        return this.distance;
+    }
+
+    public final void setDistance(String distance){
+        this.distance = distance;
+    }
+
+}
