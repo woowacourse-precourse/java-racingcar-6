d2a6ddbdd5eff0c67168dcade66c8e0f93943706
Elliot-KJW <eidechse4@hanmail.net>
2023-10-30 20:57:01 +0900

    Feat: Add Const.java

---
+++ src/main/java/racingcar/Const.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Const {
+  public final static int CAR_NAME_LIMIT = 5;
+  public final static int RANDOM_RANGE_BEGIN = 0;
+  public final static int RANDOM_RANGE_END = 9;
+  public final static int FORWARD_MOVE_BOUND = 4;
+}
