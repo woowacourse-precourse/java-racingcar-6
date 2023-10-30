b2aa227d99032d075697a0b6957f1ca2800e918d
silverwest8 <silverwest8@gmail.com>
2023-10-30 14:24:04 +0900

    feat: add RacingGame class

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingGame {
+    public void start() {
+        // 자동차 이름 입력
+        // 시도할 횟수 입력
+        // 실행 결과 출력
+        // 우승자 출력
+    }
+}
+
