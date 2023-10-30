fecccf06872ee0c6491d29a9512f80616d41e498
wook <dong930112@gmail.com>
2023-10-31 02:47:42 +0900

    feat: 입력받은 자동차 이름들을 각각의 이름으로 분리하는 기능 추가

---
+++ src/main/java/racingcar/Model/CarTextModel.java
@@ -0,0 +1,23 @@
+package racingcar.Model;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class CarTextModel {
+
+    public HashMap<String, String> parseCarNames(String carNames) {
+
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        HashMap<String, String> carHashMap = new HashMap<>();
+
+
+        for(int i=0; i<carNameList.size(); i++) {
+            carHashMap.put(carNameList.get(i), "");
+        }
+
+
+        return carHashMap;
+    }
+
+}
