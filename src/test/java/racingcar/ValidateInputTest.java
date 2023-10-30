d77f9a9ff7e5c43a4118c12ffeadd3412b5d8014
sujnkim <blackout3210@naver.com>
2023-10-30 21:03:29 +0900

    fix(test): 테스트 코드 이름 수정

--- src/test/java/racingcar/ValidateInputTest.java
+++ src/test/java/racingcar/ValidateInputTest.java
@@ -15,7 +15,7 @@ class ValidateInputTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {" "})
-    void input이_없으면_IllegalStateException(String input) {
+    void isNotNullOrEmpty_input이_없거나_공백이면_IllegalArgumentException(String input) {
         assertThatThrownBy(() ->
                 ValidateInput.isNotNullOrEmpty(input)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -23,20 +23,20 @@ class ValidateInputTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"aaa", "12a", "-1", "1.2"})
-    void input이_정수형태가_아닌경우_IllegalStateException(String input) {
+    void isNumeric_input이_정수형태가_아닌경우_IllegalArgumentException(String input) {
         assertThatThrownBy(() ->
                 ValidateInput.isNumeric(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 이름에_중복이_있으면_IllegalStateException() {
+    void isCarNameDuplicate_이름에_중복이_있으면_IllegalArgumentException() {
         String[] names = new String[]{"name", "name"};
         assertThatThrownBy(() ->
                 ValidateInput.isRightCarNames(names)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 이름의_길이범위를_초과하면_IllegalStateException() {
+    void isWrongLengthNames_이름의_길이범위를_초과하면_IllegalArgumentException() {
         String[] names = new String[]{"longer"};
         assertThatThrownBy(() ->
                 ValidateInput.isRightCarNames(names)).isInstanceOf(IllegalArgumentException.class);
