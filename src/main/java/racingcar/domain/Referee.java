710faa25929070ace3890d8e9e13720c98fa2018
Kohyeonjeong <toyu7870@naver.com>
2023-10-31 00:31:33 +0900

    Feat: 생성 Referee.java 클래스 생성

---
+++ src/main/java/racingcar/domain/Referee.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+public class Referee {
+    private ArrayList<Car> cars;
+
+    public Referee(ArrayList<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+
+    // 각 자동차마다 값을 랜덤으로 뽑아 Car.position 값 변경
+    public ArrayList<Car> playRound(ArrayList<Car> cars) {
+        for (Car car : cars) {
+            int move = Randoms.pickNumberInRange(0,9);
+            if (move >= 4) {
+                car.carMove(car.getPosition() + 1);
+            }
+        }
+        return cars;
+    }
+
+    public ArrayList<Car> determineWinner(ArrayList<Car> cars) {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        ArrayList<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+}
