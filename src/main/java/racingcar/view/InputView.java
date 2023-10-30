0954f147e7e2b2f1961e1dc80c923adc6915364f
Minju Ji <hinote444@gmail.com>
2023-10-30 19:20:37 +0900

    feat(InputView): 이동 횟수를 입력하는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,8 +5,14 @@ import java.util.List;
 import racingcar.utils.Utils;
 
 public class InputView {
+    public static int inputAttemptCount() {
+        String attemptCount = Console.readLine();
+        return Integer.parseInt(attemptCount);
+    }
+
     public static List<String> inputCarName() {
         String carNames = Console.readLine();
         return Utils.splitAndToList(carNames);
     }
+
 }
