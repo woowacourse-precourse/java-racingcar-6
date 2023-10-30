f968fb00a364d520192df551cf766cb76cdd9d79
bbbig5360 <bbbig5360>
2023-10-30 15:23:44 +0900

    자동차 이름 입력 및 글자수 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    static int carNameRange = 5;
+
+    public static void NameCheck(String carName){
+        if (carName.length() > carNameRange)
+            throw new IllegalArgumentException();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();
+        String[] carNameList = carNameInput.split("\\,");
+
+        for(int i=0; i<carNameList.length; i++){
+            NameCheck(carNameList[i]);
+        }
+        
     }
 }
