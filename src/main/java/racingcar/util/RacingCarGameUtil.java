85be3a66f3c811eaf2d0dc053fa62c92411d6ced
donglee99 <jhnj741@naver.com>
2023-10-30 15:11:39 +0900

    feat : 랜덤 숫자 생성 구현

--- src/main/java/racingcar/util/RacingCarGameUtil.java
+++ src/main/java/racingcar/util/RacingCarGameUtil.java
@@ -1,11 +1,15 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingCarGameUtil {
     private static final int RACING_CAR_NAME_LENGTH = 5;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
     private static final String SEPARATOR = ",";
     private RacingCarGameUtil() {}
 
@@ -28,5 +32,9 @@ public class RacingCarGameUtil {
         String validatedNames = (String) names;
         return Arrays.stream(validatedNames.split(SEPARATOR))
                 .collect(Collectors.toList());
+    }
+
+    public static int getRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
