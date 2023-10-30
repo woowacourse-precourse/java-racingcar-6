0751710ceb47e26e938cc960ab3a0aed0506d02d
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-31 02:36:30 +0900

    fix : InputView Class InputView 클래스를 호출할 때 마다 입력 값을 받는 오류가 생겨서 InputView 안에 메서드를 호출할 때 입력값을 받도록 고쳤습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,24 +5,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static String input = Console.readLine();
-
     public static final int MAX_NAME_LENGTH = 5;
 
     public static final String COMMA = ",";
 
-    public String startGame(){
+    public static String startGame() {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        return input;
+        return Console.readLine();
     }
 
-    public int startGameCount(){
+    public static int startGameCount() {
 
         System.out.println("시도할 회수는 몇회인가요?");
 
-        return Integer.parseInt(input);
-
+        return Integer.parseInt(Console.readLine());
     }
 }
