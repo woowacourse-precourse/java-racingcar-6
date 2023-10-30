0c63e95919d35d22bbdaf7a674cbd1f9782ea865
eora21 <eora21@naver.com>
2023-10-30 14:30:08 +0900

    feat: 🎸 (OutputView) 추가 레이싱 게임 결과를 출력하는 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,11 @@
 package racingcar.view;
 
+import racingcar.domain.RacingGameStatus;
+
 public interface OutputView {
     void requestCarNames(String delimiterDescription, String delimiter);
 
     void requestTryCount();
+
+    void printRacingGameStatus(RacingGameStatus racingGameStatus);
 }
