7b15d47b76b6809fe15cc35e8861b6b1b40a768f
mainfn <_@mainfn.dev>
2023-10-30 16:37:25 +0900

    test: 가독성을 위해 테스트 메소드 명 [함수명_상태_동작] 양식으로 변경

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -10,7 +10,7 @@ final class InputValidatorTest {
 
     @DisplayName(",로 이름이 구분된 문자열 검증 성공")
     @Test
-    void validateSuccess() {
+    void validateCarNamesInput_withStringSeparatedCommas_shouldBeOk() {
         // given
         final String input1 = "a,b,c";
         final String input2 = "pobi,pss";
@@ -21,9 +21,9 @@ final class InputValidatorTest {
         inputValidator.validateCarNamesInput(input2);
     }
 
-    @DisplayName("1자리 숫자로 이루어진 MaxRound 입력 성공")
+    @DisplayName("1자리 숫자로 이루어진 MaxRound 입력 시, 검증 성공")
     @Test
-    void validateMaxRoundSuccess() {
+    void validateMaxRoundInput_singleNumberString_shouldBeOk() {
         // given
         final String input1 = "1";
         final String input2 = "2";
@@ -38,7 +38,7 @@ final class InputValidatorTest {
 
     @DisplayName("1자리 외의 숫자 혹은 공백 및 숫자 이외의 MaxRound 입력 예외 발생")
     @Test
-    void validateMaxRoundFailure() {
+    void validateMaxRoundInput_exceptSingleNumberString_throwInvalidMaxRoundInputException() {
         // given
         final String input1 = "";
         final String input2 = "12";
@@ -64,7 +64,7 @@ final class InputValidatorTest {
 
     @DisplayName(",로 이름이 구분되었으나 공백이 포함되거나 빈 문자열은 검증 실패")
     @Test
-    void validateFailure2() {
+    void validateMaxRoundInput_includesWhiteSpaces_throwInvalidMaxRoundInputException() {
         // given
         final String input1 = "12";
         final String input2 = "12x";
