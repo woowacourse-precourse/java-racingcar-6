0912311e61b20c046d9c6aeb0b44499afc113b1c
seminchoi <choicco89@gmail.com>
2023-10-30 19:31:10 +0900

    feat(racingcar) : RacingCarGameController 클래스, 멤버변수, 생성자 구현

---
+++ src/main/java/racingcar/RacingCarGameController.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class RacingCarGameController {
+    private final RacingCarGameMachine racingCarGameMachine;
+
+    public RacingCarGameController(RacingCarGameMachine racingCarGameMachine) {
+        this.racingCarGameMachine = racingCarGameMachine;
+    }
+}
