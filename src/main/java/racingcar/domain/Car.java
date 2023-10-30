90ce2a6f339919e7a9f0a87bb3dfd59c43d880ce
kangyuseok <kingyuseok@naver.com>
2023-10-31 02:00:15 +0900

    Feat: Car 객체 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,2 +1,13 @@
-package racingcar.domain;public class Car {
+package racingcar.domain;
+
+public class Car {
+    private String carName;
+    private int randomNumber;
+    private int forwarDistance;
+
+    public Car(){}
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
 }
