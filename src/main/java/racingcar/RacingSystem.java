b5b3447ebf2469a9bd341aabeb6998d6d427bbf1
pyounani <yndbsk9372@gmail.com>
2023-10-30 10:42:14 +0900

    rename: InputSystem 클래스 생성 및 옮기기

--- src/main/java/racingcar/RacingSystem.java
+++ src/main/java/racingcar/RacingSystem.java
@@ -1,19 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class RacingSystem {
 
-    private static final String INPUT_CAR_SENTENCE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_CNT_SENTENCE = "시도할 회수는 몇회인가요?";
-    private static final String SEPARATOR = ",";
-    private static final int MAX_COUNT = 5;
-    private static final String NUMBER_REGEX = "^[0-9]*$";
+    private InputSystem inputSystem = new InputSystem();
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
 
@@ -22,6 +16,15 @@ public class RacingSystem {
     private int cnt = 0;
 
     public RacingSystem() {
+    }
+
+    public Winners getWinners() {
+        return winners;
+    }
+
+    public void input() {
+        inputSystem.inputCarName(cars);
+        int cnt = inputSystem.inputTryCount();
     }
 
     public void racingStart() {
@@ -44,82 +47,6 @@ public class RacingSystem {
         if(position == cnt) {
             winners.addWinner(name);
         }
-    }
-
-    public Winners getWinners() {
-        return winners;
-    }
-
-    public void inputCarName() {
-        String input = write(INPUT_CAR_SENTENCE);
-        try {
-            checkValidName(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-    }
-
-    public void inputTryCount() {
-        String input = write(INPUT_CNT_SENTENCE);
-        try{
-            checkNumber(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-
-    }
-
-    private String write(String sentence) {
-        System.out.println(sentence);
-        return Console.readLine();
-    }
-
-    public void checkNumber(String input) {
-        if(!isNumber(input) || !isUnderMax(input)) {
-            throw new IllegalArgumentException();
-        }
-        this.cnt = Integer.parseInt(input);
-    }
-
-    private boolean isNumber(String str) {
-        return Pattern.matches(NUMBER_REGEX, str);
-    }
-
-    private boolean isUnderMax(String str) {
-        return Long.parseLong(str) <= Integer.MAX_VALUE;
-    }
-
-    public void checkValidName(String input) {
-        String[] nameArray = input.split(SEPARATOR, -1);
-        for(String name:nameArray){
-            if(isInvalidName(name)) {
-                throw new IllegalArgumentException();
-            }
-            cars.add(new Car(name));
-        }
-    }
-
-    private boolean isInvalidName(String name) {
-        return isDuplicated(name)
-                || isOverMaxCount(name)
-                || isEmptyString(name);
-    }
-
-    private boolean isDuplicated(String name) {
-        for(Car car: cars) {
-            if(name.equals(car.getName())){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isOverMaxCount(String name) {
-        return name.length() > MAX_COUNT;
-    }
-
-    private boolean isEmptyString(String name) {
-        return name == null || " ".equals(name);
     }
 
 }
--- src/test/java/racingcar/RacingSystemTest.java
+++ src/test/java/racingcar/RacingSystemTest.java
@@ -2,47 +2,13 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RacingSystemTest {
 
-    @Test
-    void 자동차_중복_예외발생() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertThrows(IllegalArgumentException.class,
-                () -> racingSystem.checkValidName("car1,car2,car1"));
-    }
 
-    @Test
-    void 자동차_길이_예외발생() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertThrows(IllegalArgumentException.class,
-                () -> racingSystem.checkValidName("car123,car2, "));
-    }
 
-    @Test
-    void 자동차_공백_예외발생() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertThrows(IllegalArgumentException.class,
-                () -> racingSystem.checkValidName("car1,car2, "));
-    }
-
-    @Test
-    void 올바른_자동차_입력() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertDoesNotThrow(() -> racingSystem.checkValidName("car1, car2, car3"));
-    }
-
-    @Test
-    void 입력_예외발생() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertThrows(IllegalArgumentException.class,
-                () -> racingSystem.checkNumber("3A"));
-    }
-
-    @Test
-    void 올바른_입력_입력() {
-        RacingSystem racingSystem = new RacingSystem();
-        assertDoesNotThrow(() -> racingSystem.checkNumber("123"));
-    }
 }
