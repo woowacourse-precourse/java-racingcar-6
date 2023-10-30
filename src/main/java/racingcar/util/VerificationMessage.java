989e2ee59835b5c355a4172612746f7f871c3ebd
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 21:42:15 +0900

    feat: 중복된 이름 예외 메세지 추가

--- src/main/java/racingcar/util/VerificationMessage.java
+++ src/main/java/racingcar/util/VerificationMessage.java
@@ -3,4 +3,5 @@ package racingcar.util;
 public class VerificationMessage {
     public static final String IS_NOT_NUMBER = "숫자만 입력해 주세요.";
     public static final String OVER_SIZE = "이름은 5자 이하로 입력해 주세요.";
+    public static final String DUPLICATE_NAME = "이미 입력한 자동차 이름 입니다.";
 }
