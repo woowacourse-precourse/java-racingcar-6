f46443c433c172d94f96ab828e3175b70740a6c9
Heejun-0219 <heejun0219@gmail.com>
2023-10-31 00:52:28 +0900

    feat(input): 사용자 입력 받기

--- src/test/java/racingcar/View/InputViewTest.java
+++ src/test/java/racingcar/View/InputViewTest.java
@@ -2,9 +2,7 @@ package racingcar.View;
 
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
     @Test
