58efa60f4e80c8a21f877671fd5f401dbffd8bf8
unknown <hakie2kim@gmail.com>
2023-10-31 00:01:58 +0900

    test(src/test/java/racingcar/): check output separates winners with comma when there are more than one winner

--- src/test/java/racingcar/CarRacingGameTest.java
+++ src/test/java/racingcar/CarRacingGameTest.java
@@ -1,13 +1,27 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarRacingGameTest extends NsTest {
+    @Test
+    void 최종_우승자가_여러명일_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("jun,camel,poro", "1");
+                    assertThat(output()).contains("jun : -", "camel : -", "poro : -", "최종 우승자 : jun, camel, poro");
+                },
+                5, 6, 8
+        );
+    }
+
     @Test
     void 경주할_자동차_설정_중_입력값에_쉼표가_없는_경우() {
         assertSimpleTest(() ->
