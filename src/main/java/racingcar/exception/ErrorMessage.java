5c479c25f2c8bc4b2550dba22612b80c453f278b
jeondui <jeondui@gmail.com>
2023-10-31 03:32:40 +0900

    feat(ErrorMessage): 이름과 시도 횟수가 널값인 경우 에러 메시지 추가

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -5,8 +5,10 @@ public enum ErrorMessage {
     ERROR_NAME_LENGTH("이름 길이는 5자 이하만 가능합니다."),
     ERROR_NAME_NO_SPACES("공백을 제외하고 입력해주세요."),
     ERROR_MINIMUM_NAMES("최소 2개 이상의 이름을 입력해주세요."),
+    ERROR_NAME_IS_NOT_NULL("이름은 필수 입력값입니다."),
     ERROR_MINIMUM_TRY_COUNT("시도 횟수는 최소 1회 이상이어야 합니다."),
-    ERROR_NUMBER_FORMAT_TRY_COUNT("시도 횟수는 숫자만 입력 가능합니다.");
+    ERROR_NUMBER_FORMAT_TRY_COUNT("시도 횟수는 숫자만 입력 가능합니다."),
+    ERROR_TRY_COUNT_IS_NOT_NULL("시도 횟수는 필수 입력값입니다.");
 
     private final String message;
 
