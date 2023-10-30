f4507d780eec27389ebc0f61fc9d56144679478a
eora21 <eora21@naver.com>
2023-10-30 20:57:26 +0900

    test: 💍 추가 자동차 이름 5글자 초과 및 공백 미포함 테스트 작성
    자동차 이름 제한에서 좌우 공백은 포함하지 않기로 결정.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,16 +1,33 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
     @ParameterizedTest
-    @ValueSource(strings = {"다섯글자초과", "123456"})
+    @ValueSource(strings = {"다섯글자초과", "123456", "중간   공백", "   다섯글자초과", "다섯글자초과   ", "  다섯글자초과  "})
     @DisplayName("자동차 이름 5글자 초과 예외 테스트")
     void carNameMaxLengthFailTest(String name) {
         assertThrows(IllegalArgumentException.class, () -> new Car(name));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value =
+            {
+                    "좌공백,   좌공백",
+                    "우공백,우공백   ",
+                    "양쪽공백,  양쪽공백  "
+            }, ignoreLeadingAndTrailingWhitespace = false)
+    @DisplayName("자동차 이름 공백 제거 테스트")
+    void carNameTrimTest(String trimName, String name) {
+        Car car = assertDoesNotThrow(() -> new Car(name));
+        String carName = car.getStatus()
+                .getName();
+        assertThat(carName).isEqualTo(trimName);
     }
 }
