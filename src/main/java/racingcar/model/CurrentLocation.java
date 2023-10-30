f40b5b6ea94ddf1636a652f62a0f626dda8b3ef2
toychip <dlawnsgud427@naver.com>
2023-10-30 09:48:17 +0900

    feat: 자동차의 현재 위치를 나타내는 CurrentLocation 추가

---
+++ src/main/java/racingcar/model/CurrentLocation.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class CurrentLocation {
+    private int current;
+
+    private static final int DEFAULT_LOCATION = 1;
+
+    private CurrentLocation(int current) {
+        this.current = current;
+    }
+
+    public static CurrentLocation createCurrentLocation() {
+        return new CurrentLocation(DEFAULT_LOCATION);
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public void moveLocation() {
+        current++;
+    }
+}
