f8caad4bd9282e51c903e01e1f9ab33725bc99e4
AidenRoh <colohrby@gmail.com>
2023-10-30 21:02:36 +0900

    feat: add Enum class containing messages of input and output

---
+++ src/main/java/racingcar/io/IOMessage.java
@@ -0,0 +1,19 @@
+package racingcar.io;
+
+public enum IOMessage {
+
+    REGISTER_PLAYER("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ATTEMPT_TIME("시도할 회수는 몇회인가요?"),
+    EXECUTION_RESULT("실행 결과"),
+    FINAL_WINNER("최종 우승자 : ");
+
+    private final String message;
+
+    IOMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
