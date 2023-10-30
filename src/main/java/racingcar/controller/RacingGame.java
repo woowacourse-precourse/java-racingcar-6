3651076634dbb10bc3d90cdb81f7b81bf0ab9074
Minsu Choi <cms102510@naver.com>
2023-10-30 11:30:53 +0900

    feat: 자동차 경주 게임 컨트롤러 구현

---
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -0,0 +1,42 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+
+    public void start() {
+        List<Car> cars = createCars();
+        Race race = new Race(cars);
+        int tryCount = getTryCount();
+
+        raceMultipleTimes(tryCount, race);
+
+        List<Car> winners = race.getWinners();
+        OutputView.printWinners(winners);
+    }
+
+    private List<Car> createCars() {
+        return Arrays.stream(InputView.carInput().split(","))
+                .map(String::trim)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private int getTryCount() {
+        return Integer.parseInt(InputView.tryCountInput());
+    }
+
+    private void raceMultipleTimes(int tryCount, Race race) {
+        for (int i = 0; i < tryCount; i++) {
+            race.race();
+            OutputView.printResult(race);
+        }
+    }
+}
