c85373a0c9326c50bc6c623c783e23364f082d64
Yerinnnn <shinyerin0216@gmail.com>
2023-10-31 01:43:40 +0900

    Feat: "Car 클래스 내에 move() 메소드 생성"
    랜덤 숫자를 구하고, 4 이상이면 score가 1점 올라감.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String name;
     private int score;
     final static int MAX_NAME_LENGTH = 5;
+
+    final static int MIN_RANDOM_NUM = 0;
+    final static int MAX_RANDOM_NUM = 9;
+
+    final static int FORWARD_CRITERIA = 4;
 
     public Car(String name) {
         nameValidate(name);
@@ -20,5 +27,11 @@ public class Car {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
         }
+    }
+
+    public void move() {
+        int rand_num = Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
+
+        if (rand_num >= FORWARD_CRITERIA) score += 1;
     }
 }
