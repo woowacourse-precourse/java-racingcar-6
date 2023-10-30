c65aca5e4d68d909a498e974192c9b0e1a4ad850
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-10-31 04:42:27 +0900

    style: 주석 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -11,7 +11,7 @@ class InputViewTest {
     private InputView inputView;
 
     @AfterEach
-    void restoreSystemIn() {
+    void clean() {
         Console.close();
     }
 
@@ -20,20 +20,14 @@ class InputViewTest {
         // given
         inputView = new InputView();
         String input = "Tom,Bob,Lisa";
-
-        // 임시로 System.in을 변경하여 원하는 입력값을 제공
         ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
         // when
         String[] result = inputView.inputCarNames();
 
-        // 테스트 후에는 System.in을 원래대로 복원
-        System.setIn(System.in);
-
         // then
         assertThat(result).containsExactly("Tom", "Bob", "Lisa");
-
     }
 
     @Test
@@ -41,16 +35,11 @@ class InputViewTest {
         // given
         inputView = new InputView();
         String input = "5";
-
-        // 임시로 System.in을 변경하여 원하는 입력값을 제공
         ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
         // when
         String result = inputView.inputTryNumber();
-
-        // 테스트 후에는 System.in을 원래대로 복원
-        System.setIn(System.in);
 
         // then
         assertThat(result).isEqualTo("5");
