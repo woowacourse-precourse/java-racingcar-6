f57a6b90b2333bf003144a1ea3dff77c56be6aec
InJun2 <os2864@naver.com>
2023-10-30 23:39:35 +0900

    :sparkles: RacingCarView 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,4 +1,32 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarView {
+    private final static String START_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String TRY_NUMBER_INPUT = "시도할 회수는 몇회인가요?";
+    private final static String RACING_RESULT_OUTPUT = "실행 결과";
+    private final static String RACING_WINNER_OUTPUT = "최종 우승자 : ";
+
+    public String getCarNameInput() {
+        System.out.println(START_INPUT);
+        return Console.readLine();
+    }
+
+    public String getTryNumberInput() {
+        System.out.println(TRY_NUMBER_INPUT);
+        return Console.readLine();
+    }
+
+    public void racingResultOutput() {
+        System.out.println(RACING_RESULT_OUTPUT);
+    }
+
+    public void racingCarsDistanceOutput(String str) {
+        System.out.println(str);
+    }
+
+    public void racingWinnerOutput(String str) {
+        System.out.println(RACING_WINNER_OUTPUT + str);
+    }
 }
