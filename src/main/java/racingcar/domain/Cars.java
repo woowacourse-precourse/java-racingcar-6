cc66f774b88ebd3a4e3f772e18e28dec87dbcf98
gundorit <packdev937@gmail.com>
2023-10-30 21:47:11 +0900

    feat (Cars) : 자동차가 두 대 이상인지 검증한다

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -10,6 +10,7 @@ public class Cars {
     private final List<Car> cars;
 
     public Cars(List<String> carNames) {
+        validateCarNamesSize(carNames);
         cars = carNames.stream()
             .map(name -> new Car(name))
             .collect(Collectors.toList());
@@ -17,5 +18,11 @@ public class Cars {
 
     public List<Car> getCars() {
         return Collections.unmodifiableList(cars);
+    }
+
+    public void validateCarNamesSize(List<String> carNames) {
+        if (carNames.size() < 2) {
+            throw new IllegalArgumentException("자동차는 두 대 이상 입력해야 합니다.");
+        }
     }
 }
