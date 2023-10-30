82f9ee402c8ed19d6f9848cb6b8e432586f8f206
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-31 01:03:37 +0900

    feat: 게임 진행을 관리하는 PlayRacing 추가

---
+++ src/main/java/racingcar/model/PlayRacing.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import racingcar.util.NumberGenerator;
+
+public class PlayRacing {
+    private final NumberGenerator numberGenerator;
+    private final Cars cars;
+
+    public PlayRacing(NumberGenerator numberGenerator, Cars cars) {
+        this.numberGenerator = numberGenerator;
+        this.cars = cars;
+    }
+
+    private List<Integer> pickNumberOfCars() {
+        return IntStream.range(0, cars.carsSize())
+                .mapToObj(number -> numberGenerator.pickNumber())
+                .toList();
+    }
+}
