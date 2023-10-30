995dcfcc7ad4f6b8f36e66de9a472fdb7fe5e2aa
DESKTOP-UPSCALB\tkddn <thwlsdl0989@gmail.com>
2023-10-30 12:39:14 +0900

    미사용 함수와 패키지 구문 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        UserInput userInput = new UserInput();
+
         GameFunction gameFunction = new GameFunction();
         Print print = new Print();
         List<Car> carList;
@@ -19,5 +19,6 @@ public class Application {
         System.out.println(print.resultComment);
         gameFunction.repeatGame(carList);
         print.Winner(carList);
+
     }
 }
