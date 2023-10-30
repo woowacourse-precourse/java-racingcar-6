8d24b4b5afd66e4a7a791da6abc582b738245a68
KimKyuHwan <vkdls754@gmail.com>
2023-10-30 20:34:48 +0900

    feat(CarRepository): 경주차량들을 저장/조회하기 위한 저장소 인터페이스와 구현체 추가

---
+++ src/main/java/racingcar/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package racingcar.repository;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public interface CarRepository {
+    void save(String name);
+    List<Car> getCarList();
+}
