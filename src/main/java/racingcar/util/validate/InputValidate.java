02d75e2eb17a4b4a1d2fdb7cef32400a19fbc2bb
Hayoon <hy97@sju.ac.kr>
2023-10-30 13:47:08 +0900

    feat(InputValidate): 우승자 출력 형식 검증(validatePrintWinners)

--- src/main/java/racingcar/util/validate/InputValidate.java
+++ src/main/java/racingcar/util/validate/InputValidate.java
@@ -5,10 +5,11 @@ import static racingcar.util.constant.ExceptionConstant.*;
 public class InputValidate {
 
     static public void validateNameFormat(String input) {
-        if(input.isEmpty()) {
+        if (input.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_NAME);
         }
     }
+
     static public int validateInputNumberFormat(String input) {
         try {
             return Integer.parseInt(input);
@@ -18,14 +19,21 @@ public class InputValidate {
     }
 
     static public void validateInputNumberRange(int number) {
-        if(number < MINIMUM_NUMBER_VALUE) {
+        if (number < MINIMUM_NUMBER_VALUE) {
             throw new IllegalArgumentException(INPUT_VALID_NUMBER_RANGE);
         }
     }
 
     static public void validateNumberLength(String name) {
-        if(name.length() > MINIMUM_NAME_LENGTH) {
+        if (name.length() > MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(INPUT_VALID_NAME_LENGTH);
         }
+    }
+
+    static public String validatePrintWinners(String winnerList) {
+        if (winnerList.contains(",")) {
+            throw new IllegalArgumentException("우승자가 2명 이상일 경우 출력은 ', '로 구분해야합니다.");
+        }
+        return winnerList;
     }
 }
