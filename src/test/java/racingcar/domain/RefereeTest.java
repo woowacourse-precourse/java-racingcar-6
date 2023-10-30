c4052515fd092c91076cbb69a0308a315e6ffeb3
JongMinCh0i <wonderland314592@gmail.com>
2023-10-30 10:39:49 +0900

    test(Referee): 자동차의 이동 거리에 따른 우승자 결정 기능 단위 테스트 작성

---
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -0,0 +1,41 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("레이싱 게임 우승자 결정 룰 테스트")
+class RefereeTest {
+
+    @DisplayName("자동차의 이동 거리를 기준으로 내림차순 정렬하여 우승자를 선정한다.")
+    @Test
+    void selectWinnerListByDistanceDescending() {
+        //given
+        List<Car> list = Arrays.asList(new Car("김민수"), new Car("홍길동"), new Car("최종민"));
+        Car car = list.get(1);
+        Referee r = new Referee();
+
+        //when
+        car.accel();
+        List<Car> cars = r.selectWinnerList(list);
+
+        //then
+        Assertions.assertThat(cars.get(0).getName()).isEqualTo("홍길동");
+    }
+
+    @DisplayName("자동차의 이동 거리를 기준으로 내림차순 정렬하여 우승자를 선정한다. 모든 자동차의 이동 거리가 같을 경우 정렬하지 않는다.")
+    @Test
+    void eachDistanceSameNotTry() {
+        //given
+        List<Car> list = Arrays.asList(new Car("김민수"), new Car("홍길동"), new Car("최종민"));
+        Referee r = new Referee();
+
+        //when
+        List<Car> cars = r.selectWinnerList(list);
+
+        //then
+        Assertions.assertThat(cars.get(0).getName()).isEqualTo("김민수");
+    }
+}
