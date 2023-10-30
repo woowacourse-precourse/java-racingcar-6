35d183ffe6eb115ab0da9249e0a77649223320e9
inti0 <142914095+inti0@users.noreply.github.com>
2023-10-30 21:53:41 +0900

    DOCS README.md

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,6 @@
 package racingcar;
-
-import racingcar.controller.MainController;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        MainController mainController = new MainController();
-        mainController.process();
     }
 }
