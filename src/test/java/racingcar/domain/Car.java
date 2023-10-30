cf09484472d62e6fad7b24c1946d65f917ccb528
akskflwn <akskflwn@gmail.com>
2023-10-30 23:23:59 +0900

    feat: 자동차 이름 관련 예외처리 구현

--- src/test/java/racingcar/domain/Car.java
+++ src/test/java/racingcar/domain/Car.java
@@ -5,6 +5,7 @@ public class Car {
     private final String name;
 
     public Car(String name, int position) {
+        validateLengthOf(name);
         this.name = name;
         this.position = position;
     }
@@ -13,10 +14,15 @@ public class Car {
         return new Car(name, position);
     }
 
-    public void move(int i) {
-        if(i > 3) {
-            this.position++;
+    private void validateLengthOf(String name) {
+        if (name.length() > 5 || name.length() < 1) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
         }
     }
 
+    public void move(int i) {
+        if (i > 3) {
+            this.position++;
+        }
+    }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
@@ -31,6 +34,14 @@ public class CarTest {
     void stop() {
         car.move(3);
         assertThat(car).extracting("position").isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"다섯글자초과", ""})
+    void createCarWithInvalidNames(String input) {
+        assertThatThrownBy(() -> {
+            Car.of(input, POSITION);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
