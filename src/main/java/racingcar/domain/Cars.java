9fc89ab4ad4d77e4db31c183a044168f7729c0fc
kmw2378 <rlarla677@gmail.com>
2023-10-30 16:13:18 +0900

    feat(racingcar): 일급 컬렉션 Cars 클래스 추가 사용자가 입력한 이름 개수만큼 Car 리스트를 필드로 갖는 Cars 클래스를 추가하였습니다. 이름 중복검증 및 비지니스 로직을 수행합니다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import racingcar.exception.UserInputException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static racingcar.exception.errorcode.UserInputErrorCode.DUPLICATED_NAME;
+
+public class Cars {
+    private final List<Car> carList;
+
+    public Cars(final List<Car> carList) {
+        validateDuplicate(carList);
+        this.carList = carList;
+    }
+
+    private void validateDuplicate(final List<Car> carList) {
+        final Set<Car> carSet = new HashSet<>(carList);
+        if (carSet.size() != carList.size()) {
+            throw new UserInputException(DUPLICATED_NAME);
+        }
+    }
+}
