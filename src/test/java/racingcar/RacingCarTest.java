abf3a4216664526cad8fb0b79c7e03f5afc3af18
seminchoi <choicco89@gmail.com>
2023-10-30 16:29:33 +0900

    test(racingcar) : RacingCar 클래스에 대한 Test 코드 작성

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarTest {
+    private static final String RACING_CAR_NAME = "semin";
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+    private static final int DISTANCE_WHEN_STOP = 0;
+    private static final String MOVING_BAR_WHEN_STOP = "";
+    private static final int DISTANCE_WHEN_MOVING_FORWARD = 1;
+    private static final String MOVING_BAR_WHEN_MOVING_FORWARD = "-";
+
+    @Test
+    void 전진() {
+        MockRandomNumberGenerator randomNumberGenerator = new MockRandomNumberGenerator(MOVING_FORWARD);
+        RacingCar racingCar = new RacingCar(randomNumberGenerator, RACING_CAR_NAME);
+        racingCar.move();
+
+        assertThat(racingCar.getDistance()).isEqualTo(DISTANCE_WHEN_MOVING_FORWARD);
+        assertThat(racingCar.getMovingBar()).isEqualTo(MOVING_BAR_WHEN_MOVING_FORWARD);
+    }
+
+    @Test
+    void 멈춤() {
+        MockRandomNumberGenerator randomNumberGenerator = new MockRandomNumberGenerator(STOP);
+        RacingCar racingCar = new RacingCar(randomNumberGenerator, RACING_CAR_NAME);
+        racingCar.move();
+
+        assertThat(racingCar.getDistance()).isEqualTo(DISTANCE_WHEN_STOP);
+        assertThat(racingCar.getMovingBar()).isEqualTo(MOVING_BAR_WHEN_STOP);
+    }
+}
