65fd96360de3c2baab67ab8b017c8a2b891ac567
김효준 <khyojun1104@gmail.com>
2023-10-30 14:14:17 +0900

    test(CarRace): 테스트 코드 메시지 검증, 자동차 이름 검증 테스트 케이스 추가!

--- src/test/java/racingcar/service/CarRaceServiceTest.java
+++ src/test/java/racingcar/service/CarRaceServiceTest.java
@@ -13,17 +13,14 @@ import racingcar.domain.Car;
 class CarRaceServiceTest {
 
 
-
-    private static final int TEST_MOVE_COUNT=3;
-    private static final String TRACE="-";
-    private static final String COLONE=" : ";
+    private static final int TEST_MOVE_COUNT = 3;
+    private static final String TRACE = "-";
+    private static final String COLONE = " : ";
     private CarRaceService carRaceService;
 
 
-
-
     @BeforeEach
-    void init(){
+    void init() {
         carRaceService = new CarRaceService();
     }
 
@@ -31,28 +28,33 @@ class CarRaceServiceTest {
     @ParameterizedTest
     @DisplayName("자동차 이름 입력에 따른 예외 처리! 입력 길이 5 초과인 경우")
     @ValueSource(strings = {"123223", "123456,222,333", "123123123,23232323,44444"})
-    void 자동차이름_길이_5초과(String input){
+    void 자동차이름_길이_5초과(String input) {
         Assertions.assertThatThrownBy(() ->
-            carRaceService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class);
+            carRaceService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("자동차 이름의 길이는 5이하이여야 합니다.");
+        ;
     }
 
 
     @ParameterizedTest
     @DisplayName("자동차 이름 입력에 따른 예외 처리! 입력 길이 0인 경우")
-    @ValueSource(strings = {"1,,,3", ",,,", ""})
-    void 자동차이름_길이_0인경우_(String input){
+    @ValueSource(strings = {"1,,,3", ",,,", "", ",,1,2,3", "1,2,,"})
+    void 자동차이름_길이_0인경우_(String input) {
         Assertions.assertThatThrownBy(() ->
-            carRaceService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class);
+            carRaceService.extractSeperator(input)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("자동차 이름을 다음과 같은 형태: (이름1, 이름2, 이름3) 과 같은 형태로 넣어주셔야합니다!")
+        ;
     }
 
 
     @ParameterizedTest
     @DisplayName("게임 횟수 숫자인지 검증!")
     @ValueSource(strings = {"--=", "abd", "ddd", ""})
-    void 게임횟수_입력_검증(String input){
-        Assertions.assertThatThrownBy(()-> carRaceService.convertGameRoundToNumber(input)).isInstanceOf(IllegalArgumentException.class);
+    void 게임횟수_입력_검증(String input) {
+        Assertions.assertThatThrownBy(() -> carRaceService.convertGameRoundToNumber(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("게임 횟수는 숫자가 와야 합니다!");
     }
-
 
 
     @Test
@@ -71,20 +73,18 @@ class CarRaceServiceTest {
         }
 
         //then
-        Assertions.assertThat(recordResult).contains("pobi : --", "woni : --");
+        Assertions.assertThat(recordResult).contains("pobi : ---", "woni : ---");
     }
 
 
     public String madeCarResult(Car car) {
         StringBuilder madeResult = new StringBuilder();
         madeResult.append(car.getName()).append(COLONE);
-        for(int i=0; i<car.getMoveCount(); i++){
+        for (int i = 0; i < car.getMoveCount(); i++) {
             madeResult.append(TRACE);
         }
         return madeResult.toString();
     }
-
-
 
 
 }
