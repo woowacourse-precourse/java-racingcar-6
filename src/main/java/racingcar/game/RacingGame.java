0224d7edc21179e0d36cc944b48d20b5219fe488
sangdol <priceelees@gmail.com>
2023-10-30 15:47:06 +0900

    refactor(RacingGame): 게임 진행 방식 변경
    < description >
    - 테스트를 위해 랜덤 숫자를 리턴하는 객체를 입력받도록 생성자 오버로딩
    - 숫자를 넣어 모든 자동차를 경주시키도록 수정한 내용 반영

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -3,12 +3,21 @@ package racingcar.game;
 import java.util.List;
 import racingcar.car.Car;
 import racingcar.car.Cars;
+import racingcar.util.RandomNumberGenerator;
 
 public final class RacingGame {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
     private final RaceResult raceResult = new RaceResult();
+    private final RandomNumberGenerator randomNumberGenerator;
     private Cars cars;
 
     public RacingGame() {
+        this(new RandomNumberGenerator());
+    }
+
+    public RacingGame(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void startGame() {
@@ -16,17 +25,22 @@ public final class RacingGame {
         String names = inputProvider.receiveCarNames();
         int raceCount = inputProvider.receiveRaceCount();
 
-        cars = Cars.getCarsFromNames(names);
+        cars = Cars.fromNames(names);
 
         playGame(raceCount);
     }
 
     private void playGame(int raceCount) {
-        for (int i = 0; i < raceCount; i++) {
-            cars = cars.race();
+        for (int round = 1; round <= raceCount; round++) {
+            cars.race(getRandomNumbers());
             System.out.println(raceResult.getRaceProgress(cars.carList()));
         }
+
         List<Car> winnerList = cars.findWinners();
         System.out.println(raceResult.getWinners(winnerList));
+    }
+
+    private List<Integer> getRandomNumbers() {
+        return randomNumberGenerator.generateNumbersInRange(MIN_RANGE, MAX_RANGE, cars.totalCars());
     }
 }
