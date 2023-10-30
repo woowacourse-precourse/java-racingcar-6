28399c17719eb0fb851c66fda4244ddcef5ef09f
moong2 <www.castlehi@gmail.com>
2023-10-30 15:29:48 +0900

    test: make test checking Validation of Car's name

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static racingcar.util.GameConstants.CAR_NAME_DELIMITER;
 import static racingcar.util.GameConstants.COUNT_OF_CAR;
 import static racingcar.util.GameConstants.MAXIMUM_CAR_NAME_LENGTH;
 
@@ -30,18 +31,25 @@ public class InputTest {
             char name = (char) ('a' + i);
             strings.add(Character.toString(name));
         }
-        final String input = String.join(",", strings);
+        final String input = String.join(CAR_NAME_DELIMITER, strings);
 
         // when & then
-        assertThatNoException(() -> carNameValidator.validateCount("a,b,c"));
+        assertThatNoException(() -> carNameValidator.validateCount(input));
     }
 
     @Test
     @DisplayName("네 개의 이름을 입력받을 경우 예외를 반환한다.")
     public void 네_개의_이름을_입력받을_경우_예외를_반환한다() {
         // given
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i <= COUNT_OF_CAR; i++) {
+            char name = (char) ('a' + i);
+            strings.add(Character.toString(name));
+        }
+        final String input = String.join(CAR_NAME_DELIMITER, strings);
+
         // when & then
-        assertThatIllegalArgumentException(() -> carNameValidator.validateCarName("a,b,c,d"));
+        assertThatIllegalArgumentException(() -> carNameValidator.validateCount(input));
     }
 
     @Test
@@ -49,15 +57,22 @@ public class InputTest {
     public void 공백을_입력받을_경우_예외를_반환한다() {
         // given
         // when & then
-        assertThatIllegalArgumentException(() -> carNameValidator.validateCarName(""));
+        assertThatIllegalArgumentException(() -> carNameValidator.validateCount(""));
     }
 
     @Test
     @DisplayName("형식을 벗어난 경우 예외를 반환한다.")
     public void 형식을_벗어난_경우_예외를_반환한다() {
         // given
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < COUNT_OF_CAR; i++) {
+            char name = (char) ('a' + i);
+            strings.add(Character.toString(name));
+        }
+        final String input = String.join(CAR_NAME_DELIMITER, strings) + CAR_NAME_DELIMITER;
+
         // when & then
-        assertThatIllegalArgumentException(() -> carNameValidator.validateCarName("a,b,c,"));
+        assertThatIllegalArgumentException(() -> carNameValidator.validateCount(input));
     }
 
     @Test
@@ -94,15 +109,29 @@ public class InputTest {
     @DisplayName("서로 다른 이름을 가졌을 경우 테스트를 통과한다.")
     public void 서로_다른_이름을_가졌을_경우_테스트를_통과한다() {
         // given
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < COUNT_OF_CAR; i++) {
+            char name = (char) ('a' + i);
+            strings.add(Character.toString(name));
+        }
+        final String input = String.join(CAR_NAME_DELIMITER, strings);
+
         // when & then
-        assertThatNoException(() -> carNameValidator.validateDuplicate("a,b,c"));
+        assertThatNoException(() -> carNameValidator.validateDuplicate(input));
     }
 
     @Test
     @DisplayName("서로 같은 이름을 가졌을 경우 예외를 반환한다.")
     public void 서로_같은_이름을_가졌을_경우_예외를_반환한다() {
         // given
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < COUNT_OF_CAR; i++) {
+            char name = 'a';
+            strings.add(Character.toString(name));
+        }
+        final String input = String.join(CAR_NAME_DELIMITER, strings);
+
         // when & then
-        assertThatIllegalArgumentException(() -> carNameValidator.validateDuplicate("a,a,b"));
+        assertThatIllegalArgumentException(() -> carNameValidator.validateDuplicate(input));
     }
 }
