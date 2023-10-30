4cbe9bc57270f6abb6880a26708ff6abf68fee4f
Flash-KIM142 <topqr123q@gmail.com>
2023-10-31 01:45:43 +0900

    refactor: InputView의 메소드 역할 중복 수정
    자동차 이름들을 받는 메소드와 이동횟수를 받는 메소드가 사실상 동일한 역할이므로 하나로 통일

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String getCarNames() {
-        String carNames = Console.readLine();
-        return carNames;
-    }
-
-    public static String getTryCount() {
-        String tryCountString = Console.readLine();
-        return tryCountString;
+    public static String readConsole() {
+        return Console.readLine();
     }
 }
