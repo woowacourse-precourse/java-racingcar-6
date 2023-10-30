4500750a52399695e80257b23802214c63ac5858
SeongIl <biuld1234@gmail.com>
2023-10-30 12:32:47 +0900

    test: modify 2.CountInputTest 0 입력시 예외 테스트 추가

--- src/test/java/racingcar/CountInputTest.java
+++ src/test/java/racingcar/CountInputTest.java
@@ -7,19 +7,24 @@ import org.junit.jupiter.api.Test;
 public class CountInputTest {
 
     @Test
-    void 음수_입력_시_예외_처리(){
+    void 음수_또는_0_입력_시_예외_처리(){
 
         // given
-        String userInput = "-1";
+        String userInputOne = "-1";
+        String userInputTwo = "0";
 
         // then
-        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInput))
+        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputOne))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1이상의 숫자를 입력하세요.");
+
+        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputTwo))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1이상의 숫자를 입력하세요.");
     }
 
     @Test
-    void 문자_공백_입력_시_예외_처리(){
+    void 문자_또는_공백_입력_시_예외_처리(){
 
         // given
         String userInputOne = "3pobi";
