d4d3c78a0346bab6eb396558d0f19403ab17fa73
Yeriimii <seojs0511@naver.com>
2023-10-31 02:22:45 +0900

    feat(Round): 자동차의 이름과 위치를 결과지에 담는 기능 구현

--- src/main/java/racingcar/model/Round.java
+++ src/main/java/racingcar/model/Round.java
@@ -3,11 +3,20 @@ package racingcar.model;
 import racingcar.DTO.CarDTO;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Round {
     private List<CarDTO> roundResult;
 
     protected void startRace(List<Car> cars) {
         cars.forEach(Car::startEngine);
+    }
+
+    public List<CarDTO> generateResult(List<Car> cars) {
+        roundResult = cars.stream()
+                .map(car -> new CarDTO(car.getName(), car.getPosition()))
+                .collect(Collectors.toList());
+
+        return roundResult;
     }
 }
