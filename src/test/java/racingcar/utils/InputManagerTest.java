76c35678d8e041fea1e7476a5fcbaba34d3b703a
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-30 23:42:35 +0900

    InputManagerTest 정상입력, 비정상입력 구현

--- src/test/java/racingcar/utils/InputManagerTest.java
+++ src/test/java/racingcar/utils/InputManagerTest.java
@@ -3,14 +3,28 @@ package racingcar.utils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 class InputManagerTest {
     InputManager inputManager = new InputManager();
 
     @Test
-    public void 숫자가_아닌_수_입력() {
+    public void 시도횟수_정상입력() {
+        String notInteger = "12";
+        InputStream in = new ByteArrayInputStream(notInteger.getBytes());
+        System.setIn(in);
+    }
+
+    @Test
+    public void 시도횟수_문자입력() {
         String notInteger = "가나다라";
-        Assertions.assertThatThrownBy(() -> inputManager.inputNumber(notInteger))
+        InputStream in = new ByteArrayInputStream(notInteger.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(() -> inputManager.Number())
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
+
 }
