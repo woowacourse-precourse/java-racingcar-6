ab38421d449f13a03daba4bab7c75df05372909d
hackertaco <imsa1224@korea.ac.kr>
2023-10-30 18:19:32 +0900

    refactor: IOConsole 이름 변경 반영

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,26 +1,26 @@
 package racingcar.controller;
 
-import racingcar.OutputConsole;
+import racingcar.IOConsole;
 import racingcar.model.Car;
 import racingcar.service.GameService;
 
 import java.util.List;
 
 public class GameController {
-    static private GameService gameService;
+    private GameService gameService;
 
     public void play(){
-        List<Car> cars = OutputConsole.readCarNames().stream().map(Car::new).toList();
-        int trial = OutputConsole.readTrial();
+        List<Car> cars = IOConsole.readCarNames().stream().map(Car::new).toList();
+        int trial = IOConsole.readTrial();
 
         gameService = new GameService(cars, trial);
 
         gameService.playRace();
         end();
     }
-    private static void end(){
+    private void end(){
         List<String> winners = gameService.getWinners();
-        OutputConsole.printWinner(winners);
+        IOConsole.printWinner(winners);
     }
 
 }
