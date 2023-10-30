8b356eab7cf847dbaddf0f339f046fd60f765f33
zangsu <zangsu_@naver.com>
2023-10-30 21:14:02 +0900

    feat : create addCar(Car car) to store instance in collection

--- src/main/java/racingcar/firstclasscollection/PositionMap.java
+++ src/main/java/racingcar/firstclasscollection/PositionMap.java
@@ -2,16 +2,27 @@ package racingcar.firstclasscollection;
 
 import java.util.HashMap;
 import java.util.Map;
+import racingcar.constant.ExceptionMessage;
 import racingcar.model.Car;
 
 public class PositionMap {
+    private static final int INIT_POSITION = 0;
     private Map<Car, Integer> positionMap;
 
     public PositionMap() {
         positionMap = new HashMap<>();
     }
 
-    // TODO : void addCar(Car car)
+    /**
+     * @param car
+     * @throws IllegalArgumentException - 중복된 Car 객체를 저장 시도시
+     */
+    public void addCar(Car car) {
+        if (positionMap.containsKey(car)) {
+            throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_DUPLICATED);
+        }
+        positionMap.put(car, INIT_POSITION);
+    }
 
     // TODO : void moveCar(Car car)
 
