d265e0102e824d3796df1fae58b6ea94d042d029
seokmyungham <hellotjraud@gmail.com>
2023-10-30 13:24:41 +0900

    feat(Car): Set 추가, validateDuplicatedName 메서드 추가, 생성자 수정
    carNames: 중복된 자동차 이름 검사를 위한 Set
    validateDuplicatedName: 중복된 자동차 이름이 존재할 시 에러를 발생합니다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,20 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Car {
     private Name name;
     private Move move;
 
-    public Car(Name name, Move move) {
-        this.name = name;
-        this.move = move;
+    Set<String> carNames = new HashSet<>();
+
+    public Car(String carName) {
+        validateDuplicatedName(carName);
+        this.name = new Name(carName);
+        this.move = new Move();
+        carNames.add(carName);
     }
 
     public void moveForward() {
@@ -20,5 +26,15 @@ public class Car {
     public int generateRandomNumber() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         return randomNumber;
+    }
+
+    private void validateDuplicatedName(String carName) {
+        if (isNameDuplicated(carName)) {
+            throw new IllegalArgumentException("이미 사용중인 자동차 이름입니다.");
+        }
+    }
+
+    private boolean isNameDuplicated(String carName) {
+        return carNames.contains(carName);
     }
 }
