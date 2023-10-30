23ca55980479c46a41a01940c1232a918e12f628
MrKeeplearning <jaeseok1115@gmail.com>
2023-10-30 14:19:36 +0900

    feat(Message): Message enum 클래스 생성
    - feat: 입력과 출력에 사용되는 메시지를 열거 타입으로 생성

---
+++ src/test/java/myrace/Application.java
@@ -0,0 +1,8 @@
+package myrace;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class Application {
+}
