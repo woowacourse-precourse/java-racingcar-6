31e8453b00fd0ba236b33d1f8dcb8d37291e1d29
MingyeomKim <mingyum119@naver.com>
2023-10-31 01:07:33 +0900

    feat(Car): Car 도메인에서 자동차의 이름이 5자 이하임을 검증
    자동차의 이름을 검증하는 로직을 Car 도메인에 위치시켜서, 리스트에서 자동차의 이름을 일일이 추출하지 않고 검증할 수 있도록 수정한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,15 +1,32 @@
 package racingcar.domain;
 
+import static racingcar.global.constants.NumberType.MAX_LENGTH_OF_NUMBER;
+
 public class Car {
     private String name;
     private Integer position;
 
     private Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
     }
 
     public static Car of(String name) {
         return new Car(name);
+    }
+
+    private void validateName(String name) {
+        validateLength(name);
+    }
+
+    private void validateLength(String name) {
+        if (isLengthExceeded(name)) {
+            throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
+        }
+    }
+
+    private boolean isLengthExceeded(String name) {
+        return name.length() > MAX_LENGTH_OF_NUMBER.getValue();
     }
 }
