f69de42df22702e3af990151545fa1c90a01e7f7
gourderased <gourderased@gmail.com>
2023-10-31 02:46:02 +0900

    "feat(racingcar): 자동차 이름의 공백 검증 로직 추가"

--- src/main/java/racingcar/utils/Validater.java
+++ src/main/java/racingcar/utils/Validater.java
@@ -6,5 +6,9 @@ public class Validater {
         if(name.length() > 5) {
             throw new IllegalArgumentException("이름은 5글자 이하로 해야합니다.");
         }
+
+        if(name.isBlank() || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
     }
 }
