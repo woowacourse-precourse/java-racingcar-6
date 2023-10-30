4cc1000b976057522cf255dd52d576f3da90823b
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-10-30 13:19:41 +0900

    feat: 각 경기당 실행 결과 출력하는 기능 구현
    - recordGamdResult 메서드를 통해 자동차당 전진 횟수를 기록하고, 해당 결과를 printResultsPerGame 메서드를 통해 출력합니다.

--- src/test/java/racingcar/ResultService.java
+++ src/test/java/racingcar/ResultService.java
@@ -24,6 +24,21 @@ public class ResultService {
         System.out.println("실행 결과");
     }
 
+    public static void printResultsPerGame(Cars cars, int numberOfMatches) {
+        for (int i = 1; i <= numberOfMatches; i++) {
+            cars = recordGameResult(cars);
+            printNumberOfMovingForward(cars);
+            System.out.println();
+        }
+    }
+
+    private static Cars recordGameResult(Cars cars) {
+        for (Object car : cars.keySet()) {
+            boolean isMovingForward = GameService.movingForwardCheck(GameService.pickRandomNumber());
+            cars = GameService.recordNumberOfMovingForward(cars, (Car) car, isMovingForward);
+        }
+        return cars;
+    }
 
 
 }
