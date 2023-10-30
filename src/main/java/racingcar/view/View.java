f9c90a35789cd98d3206dc1f9a5101544d05c421
eora21 <eora21@naver.com>
2023-10-30 14:29:26 +0900

    feat: 🎸 (View) 추가 레이싱 결과에 대해 처리하는 메서드

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,9 +1,12 @@
 package racingcar.view;
 
 import java.util.List;
+import racingcar.domain.RacingGameStatus;
 
 public interface View {
     List<String> getCarNames();
 
     int getRacingCount();
+
+    void showRacingGameStatus(RacingGameStatus racingGameStatus);
 }
