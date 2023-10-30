eb0549a82d8f126667b65a7686a942a325a1a5a1
박상현 <shawnpark98@naver.com>
2023-10-30 17:38:00 +0900

    feat: Application에 Race 연동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.domain.Race;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Race race = new Race();
+        race.start();
     }
 }
