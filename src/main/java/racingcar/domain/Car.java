161eaa2910341690ba02c19e67c048b1966f111b
gundorit <packdev937@gmail.com>
2023-10-30 19:04:33 +0900

    feat (Car) : 자동차는 4 이상의 무작위 값을 얻으면 전진한다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,13 +2,26 @@ package racingcar.domain;
 
 public class Car {
 
+    private final int MOVING_FORWARD_CRITERION = 4;
     private String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward(int number) {
+        if (number >= MOVING_FORWARD_CRITERION) {
+            this.position++;
+        }
     }
 }
