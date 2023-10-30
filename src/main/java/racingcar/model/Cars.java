2f7e4a090b98e14fc1c2fbb217db6f0bdaed7a30
SeongJunChun <csj991210@naver.com>
2023-10-31 07:14:21 +0900

    FIX  : 예외처리 코드 파일 이동

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,40 +1,27 @@
 package racingcar.model;
 
+import racingcar.util.ExceptionHandling;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cars {
-    public List<String> carList = new ArrayList<>();
-    public List<Integer> countList = new ArrayList<>();
 
-    public Cars(String carName){
+public class Cars {
+    public static List<String> carList = new ArrayList<>();
+    public static List<Integer> countList = new ArrayList<>();
+
+    public Cars(String carName) {
         makeCarList(carName);
     }
+
     public void makeCarList(String carNames) {
         for (String carName : carNames.split(",")) {
-            isCarNameNull(carName);
-            isCarNameOverSize(carName);
-            isCarNameSame(carName, carList);
+            ExceptionHandling.isCarNameNull(carName);
+            ExceptionHandling.isCarNameOverSize(carName);
+            ExceptionHandling.isCarNameSame(carName, carList);
             countList.add(0);
             carList.add(carName);
         }
     }
 
-    public static void isCarNameNull(String carName) throws IllegalArgumentException {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름은 공백이 될수 없습니다.");
-        }
-    }
-
-    public static void isCarNameSame(String carName, List<String> carList) throws IllegalArgumentException {
-        if (carList.contains(carName)) {
-            throw new IllegalArgumentException("자동차 이름은 중복이 될수 없습니다.");
-        }
-    }
-
-    public static void isCarNameOverSize(String carName) throws IllegalArgumentException {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하 이여야 합니다.");
-        }
-    }
 }
