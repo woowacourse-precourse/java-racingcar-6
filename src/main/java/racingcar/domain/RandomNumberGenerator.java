c735c3bd1f0857629403bd3c25bd5e7c8a0672f4
Jiihyun <cwlgusc@gmail.com>
2023-10-30 23:28:58 +0900

    style(RandomNumberGenerator): 필드와 메서드 사이 공백 추가

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberGenerator {
     private static final int START_INCLUSIVE = 0;
     private static final int END_INCLUSIVE = 9;
+
     public int generateRandomNumber() {
         return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
