e7ee3e8d2ecec93c07fcd77a84f3ab30f68254fd
WiFros <39113029+WiFros@users.noreply.github.com>
2023-10-30 22:01:43 +0900

    Game 클래스 생성
    Game을 관리하는 Game 클래스를 생성했습니다.
    - 게임 클래스는 각 차들의 메소드를 이용해서 각 차량들의 진행도를 관리하고, 진행도에 따라서 우승자를 판단하는 기능을 제공합니다.
     - Car List : 자동차 리스트들은 한 게임 내에 존재하기 때문에 자동차의 리스트를 Game 오브젝트에서 생성했습니다.
    - round : 게임 반복 수, 게임의 반복되는 수입니다. 사용자에 입력에 따라서 변화합니다.
    - play() : 게임의 진행을 담당합니다. 라우스 수만큼 반복하며, 각 차의 움직임을 실행합니다. 그리고 진행도를 출력합니다.
    - getWinner() : 승자를 찾는 메소드입니다.

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import java.util.List;
+
+public class Game {
+    private List<Car> cars;
+    private int round;
+
+    public Game(List<Car> cars, int round) {
+        this.cars = cars;
+        this.round = round;
+    }
+
+    public void play() {
+    }
+
+    public String getWinner() {
+        return null;
+    }
+
+    private int getMaximumPosition() {
+        return 0;
+    }
+
+    private String findWinner() {
+        return null;
+    }
+    public List<Car> getCars() {
+        return this.cars;
+    }
+    public int setRound(int round) {
+        this.round = round;
+    }
+}
