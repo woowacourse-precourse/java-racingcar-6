74069ffeb5ba0ae9111e06b549bd62f31a814404
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 14:37:47 +0900

    [refactor] : change location to create car method

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -1,4 +1,14 @@
 package racingcar.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameManager {
+    public List<Car> createCar(String[] carNames) {
+
+        return Arrays.stream(carNames)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
 }
