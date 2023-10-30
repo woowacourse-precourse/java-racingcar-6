26ed8561a6b34350a6182f7a207e2d70714044cb
ahma0 <dsd932@naver.com>
2023-10-31 03:15:25 +0900

    test: 사용자 횟수 입력 테스트

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -18,9 +18,7 @@ public class ValidatorTest extends NsTest {
         //given
         List<String> carNames = new ArrayList<>(List.of("Test1", "Test1", "Test2"));
 
-        //when
-
-        //then
+        //when && then
         assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.NOT_DUPLICATE_NAME);
@@ -33,9 +31,7 @@ public class ValidatorTest extends NsTest {
         //given
         List<String> carNames = new ArrayList<>(List.of("Test1", "", "Test3"));
 
-        //when
-
-        //then
+        //when && then
         assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.NOT_EMPTY_AND_BLANK);
@@ -47,12 +43,24 @@ public class ValidatorTest extends NsTest {
         //given
         List<String> carNames = new ArrayList<>(List.of("TestTest1", "Test2", "Test3"));
 
-        //when
-
-        //then
+        //when && then
         assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.NOT_MORE_THAN_FIVE);
+    }
+
+    @Test
+    @DisplayName("사용자가 잘못된 횟수를 입력하면 실패한다.")
+    void validateUserAnswer() {
+        //given
+        List<String> userAnswers = List.of("가나다", " 테스트", "0", "-5", "6.1");
+
+        //when && then
+        userAnswers.forEach(userAnswer ->
+                assertThatThrownBy(() -> Validator.validateCarTrying(userAnswer))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ExceptionMessage.NOT_NATURAL_NUMBER)
+        );
     }
 
     @Override
