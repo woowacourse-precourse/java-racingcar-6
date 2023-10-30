e79530be3f2090b0717fa560393394f905d8c5fd
chanhoim <cksgh201@naver.com>
2023-10-30 23:28:22 +0900

    test(car): 자동차 이름에 대한 유효성 검사

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,11 +1,11 @@
 package racingcar;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.car.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
@@ -13,7 +13,7 @@ public class CarTest {
     @Test
     void 자동차_전진_성공() {
         //given
-        Car car = new Car() {
+        Car car = new Car("userA") {
             @Override
             protected int getRandomNumber() {
                 return 4;
@@ -33,7 +33,7 @@ public class CarTest {
     @Test
     void 자동차_전진_실패() {
         //given
-        Car car = new Car() {
+        Car car = new Car("userA") {
             @Override
             protected int getRandomNumber() {
                 return 3;
@@ -46,5 +46,43 @@ public class CarTest {
         //then
         assertThat(car.getDistance()).isEqualTo(0);
     }
+
+    @DisplayName("차의 이름의 길이는 1~5 범위에 속해야 한다.")
+    @Test
+    void 자동차_이름_초기화() {
+        //given
+        String validName = "pobi";
+
+        //when
+        Car car = new Car(validName);
+
+        //then
+        assertThat(car.getName()).isEqualTo(validName);
+    }
+
+    @DisplayName("차는 5보다 긴 이름을 가질 수 없다.")
+    @Test
+    void 자동차_이름_길이_초과() {
+        //given
+        String unvalidName = "chanho";
+
+        //when,then
+        assertThatThrownBy(() -> new Car(unvalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 5글자 이하여야 됩니다.");
+    }
+
+    @DisplayName("차의 이름이 비어있으면 안된다.")
+    @Test
+    void 자동차_이름_공백() {
+        //given
+        String unvalidName = "";
+
+        //when,then
+        assertThatThrownBy(() -> new Car(unvalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름이 비어있습니다.");
+    }
+
 
 }
