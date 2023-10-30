58ad58d5a6423fb9d6ebafebb58371f981050d3f
Bosub Kim <grapeflavoredapple@gmail.com>
2023-10-31 00:14:23 +0900

    feat: 라운드 입력을 검증 구현
    - 진행할 라운드 수를 입력받을 때, 발생가능한 예외를 처리하는 기능 구현

---
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -0,0 +1,22 @@
+package racingcar.constant;
+
+import static racingcar.constant.InputNumConstant.MAX_NAME_LENGTH;
+import static racingcar.constant.InputNumConstant.MIN_NAME_LENGTH;
+
+public enum ExceptionMessage {
+
+    NAME_LENGTH_NOT_IN_RANGE(
+            "이름은 %d자 이상, %d자 이하여야 합니다.".formatted(MIN_NAME_LENGTH.getValue(), MAX_NAME_LENGTH.getValue())),
+    ROUND_CONTAIN_NOT_ONLY_NUM("자동차 이동은 숫자로 입력해야 합니다."),
+    ROUND_MUST_BE_POSITIVE("자동차 이동은 양수로 입력해야 합니다.");
+
+    private final String value;
+
+    ExceptionMessage(String vlaue) {
+        this.value = vlaue;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
--- src/main/java/racingcar/validate/InputValidator.java
+++ src/main/java/racingcar/validate/InputValidator.java
@@ -1,6 +1,8 @@
 package racingcar.validate;
 
-import static racingcar.constant.ExceptionConstant.NAME_LENGTH_NOT_IN_RANGE;
+import static racingcar.constant.ExceptionMessage.NAME_LENGTH_NOT_IN_RANGE;
+import static racingcar.constant.ExceptionMessage.ROUND_CONTAIN_NOT_ONLY_NUM;
+import static racingcar.constant.ExceptionMessage.ROUND_MUST_BE_POSITIVE;
 import static racingcar.constant.InputNumConstant.MAX_NAME_LENGTH;
 import static racingcar.constant.InputNumConstant.MIN_NAME_LENGTH;
 
@@ -14,7 +16,30 @@ public class InputValidator {
         }
     }
 
+    public static void validateRoundNum(String roundNum) {
+        if (!checkOnlyNumInString(roundNum)) {
+            throw new IllegalArgumentException(ROUND_CONTAIN_NOT_ONLY_NUM.getValue());
+        }
+
+        if (!checkNumPositive(roundNum)) {
+            throw new IllegalArgumentException(ROUND_MUST_BE_POSITIVE.getValue());
+        }
+     }
+
     private static boolean checkNameLengthInRange(String name) {
         return name.length() >= MIN_NAME_LENGTH.getValue() && name.length() <= MAX_NAME_LENGTH.getValue();
+    }
+
+    private static boolean checkOnlyNumInString(String subject) {
+        try {
+            Integer.parseInt(subject);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkNumPositive(String num) {
+        return Integer.parseInt(num) >= 0;
     }
 }
