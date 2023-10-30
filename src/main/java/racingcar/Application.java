d68060f2a48ac5d254976bc02275dd73bf0e447b
yeonsu <jack0810@kookmin.ac.kr>
2023-10-30 17:27:40 +0900

    feature: input car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,26 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        inputCarName();
+    }
 
+    public static void inputCarName(){
+        String tm = Console.readLine();
+
+        System.out.println(Arrays.toString(parseCarName(tm)));
+    }
+
+    public static String[] parseCarName(String input){
+
+        String[] names = input.split(",");
+
+        return names;
     }
 }
