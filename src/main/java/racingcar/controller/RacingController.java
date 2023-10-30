925a7573dd1faf9208dfa97afe8149b2c885880e
inpink <inpink@kakao.com>
2023-10-30 15:49:34 +0900

    feat: RacingController 생성.
    RacingController가 의존하는 객체들을 DI받음. main에서 racingController.playGame()으로 레이싱 게임 시작함.

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,22 @@
+package racingcar.controller;
+
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+
+    public RacingController(InputView inputView, OutputView outputView, RacingService racingService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+    }
+
+    public void playGame() {
+        // 게임 로직을 구현
+    }
+
+}
