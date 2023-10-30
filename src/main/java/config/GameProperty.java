450177e28a0e587fc5bed0edfbeb774a06bd26c1
yujeonghyeon <dbdb1114@naver.com>
2023-10-30 19:19:10 +0900

    1. 프로퍼티 벨류 반환 방식 getter 적용 2. 프로퍼티 변수명 변경

--- src/main/java/config/GameProperty.java
+++ src/main/java/config/GameProperty.java
@@ -1,8 +1,6 @@
 package config;
 
 import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 public class GameProperty {
@@ -18,29 +16,37 @@ public class GameProperty {
         }
     }
 
-    private final static int MAX_CARNAME_LENGTH = (Integer) properties.get("MAX.CARNAME.LENGTH");
-    private final static int MIN_CARNAME_LENGTH = (Integer) properties.get("MIN.CARNAME.LENGTH");
+    private final static int MAX_CARNAME_LENGTH = Integer.parseInt((String) properties.get("MAX.CARNAME.LENGTH"));
+    private final static int MIN_CARNAME_LENGTH = Integer.parseInt((String) properties.get("MIN.CARNAME.LENGTH"));
     private final static String CARNAME_REG_CONDITION = (String) properties.get("CARNAME.REG.CONDITION");
 
-    private final static int MIN_MOVE_FOR_RANGE = (Integer) properties.get("MIN.RANDOM.RANGE");
-    private final static int MAX_MOVE_FOR_RANGE = (Integer) properties.get("MAX.RANDOM.RANGE");
-    private final static int FOR_MOVE_NUMBER = (Integer) properties.get("FOR.MOVE.NUMBER");
+    private final static int MIN_RANDOM_RANGE = Integer.parseInt((String) properties.get("MIN.RANDOM.RANGE"));
+    private final static int MAX_RANDOM_RANGE = Integer.parseInt((String) properties.get("MAX.RANDOM.RANGE"));
+    private final static int FOR_MOVE_NUMBER = Integer.parseInt((String) properties.get("FOR.MOVE.NUMBER"));
 
-    public static Map<String, Object> getCarNameCondition() {
-        Map<String, Object> carNameCondition = new HashMap<>();
 
-        carNameCondition.put("MAX_LENGTH", MAX_CARNAME_LENGTH);
-        carNameCondition.put("MIN_LENGTH", MIN_CARNAME_LENGTH);
-        carNameCondition.put("CARNAME_REG_CONDITION", CARNAME_REG_CONDITION);
-
-        return carNameCondition;
+    public static int getMaxCarnameLength() {
+        return MAX_CARNAME_LENGTH;
     }
 
-    public static Map<String, Object> getRandomNumberCondition() {
-        Map<String, Object> randomNumberCondition = new HashMap<>();
-        randomNumberCondition.put("MIN_NUM", MIN_MOVE_FOR_RANGE);
-        randomNumberCondition.put("MAX_NUM", MAX_MOVE_FOR_RANGE);
-        randomNumberCondition.put("MOVE_NUM", FOR_MOVE_NUMBER);
-        return randomNumberCondition;
+    public static int getMinCarnameLength() {
+        return MIN_CARNAME_LENGTH;
     }
+
+    public static String getCarnameRegCondition() {
+        return CARNAME_REG_CONDITION;
+    }
+
+    public static int getMinMoveForRange() {
+        return MIN_RANDOM_RANGE;
+    }
+
+    public static int getMaxRandomRange() {
+        return MAX_RANDOM_RANGE;
+    }
+
+    public static int getForMoveNumber() {
+        return FOR_MOVE_NUMBER;
+    }
+
 }
