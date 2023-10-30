e3d2dc4a8ce1cd26a365d6e267d55f27848c336b
pjaehyun <show6756@gmail.com>
2023-10-30 20:17:17 +0900

    refactor: 예외처리 메세지 상수로 관리

--- src/main/java/validate/InputValidation.java
+++ src/main/java/validate/InputValidation.java
@@ -1,36 +1,36 @@
 package validate;
 
-import java.util.List;
+import static constants.RacingCarErrorConstants.*;
 
 public class InputValidation {
 
     public static void validateNumericInput(String input) {
         if (!input.matches("^[0-9]+$")){
-            throw new IllegalArgumentException("입력된 값에 숫자가 아닌 값이 포함되어있습니다.");
+            throw new IllegalArgumentException(NUMERIC_INPUT_ERROR_MESSAGE);
         }
     }
 
     public static void validateInputLengthExceeded(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차의 이름은 5글자를 초과할 수 없습니다.");
+            throw new IllegalArgumentException(INPUT_LENGTH_EXCEEDED_ERROR_MESSAGE);
         }
     }
 
     public static void validateInputIsEmpty(String name) {
         if (name.isEmpty()) {
-            throw new IllegalArgumentException("자동차의 이름은 공백일 수 없습니다.");
+            throw new IllegalArgumentException(INPUT_IS_EMPTY_ERROR_MESSAGE);
         }
     }
 
     public static void validateInputDoesNotContainSpecialCharacters(String name) {
         if (name.matches(".*[ !@#$%^&*(),.?\":{}|<>].*")) {
-            throw new IllegalArgumentException("자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.");
+            throw new IllegalArgumentException(INPUT_DOES_NOT_CONTAIN_SPECIAL_CHARACTERS_ERROR_MESSAGE);
         }
     }
 
     public static void validateListIsEmpty(String[] list) {
         if (list.length == 0) {
-            throw new IllegalArgumentException("입력된 자동차가 없습니다.");
+            throw new IllegalArgumentException(LIST_IS_EMPTY_ERROR_MESSAGE);
         }
     }
 }
