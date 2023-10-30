9ca7c32c43a062a2c812fd2e78df9d8103109c45
KangGeonyoung <kky6335@gmail.com>
2023-10-31 00:38:34 +0900

    style(Application): 주석 추가 - main 안에 주석 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import racingcar.controller.RaceGameController;
 
 public class Application {
     public static void main(String[] args) {
-
+        // 자동차 경주 게임 시작
         RaceGameController raceGameController = new RaceGameController();
         raceGameController.start();
     }
