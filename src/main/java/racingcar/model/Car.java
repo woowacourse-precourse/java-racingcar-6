a530abd7d1072a528898ed3f60ef01431a137d21
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 13:54:51 +0900

    feat(Car) : 자동차가 몇 칸 전진했는지 저장

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,8 +2,10 @@ package racingcar.model;
 
 public class Car {
     private String name;
+    private Long movingCount;
 
     public Car(String name) {
         this.name = name;
+        movingCount = 0L;
     }
 }
