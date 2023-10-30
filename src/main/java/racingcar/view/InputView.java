76e24b618c2a14025f9673fdc3861831cc751855
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 06:55:38 +0900

    feat : 사용자 라운드 반복 입력 값 기능 코드 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final static String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String ROUND_NUMBER = "시도할 횟수는 몇회인가요?";
 
-    public static String createCarNumber(){
+    public static String createCarName(){
         System.out.println(CAR_NAME_INPUT);
+        return Console.readLine();
+    }
+
+    public static String createRoundNumber(){
+        System.out.println(ROUND_NUMBER);
         return Console.readLine();
     }
 }
