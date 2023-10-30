590f2de536501a27c6a95e7e500982571de0cb47
inpink <inpink@kakao.com>
2023-10-30 16:06:33 +0900

    test: AppConfig가 Config의 구현체를 반환하는지 테스트

--- src/test/java/racingcar/configuration/AppConfigTest.java
+++ src/test/java/racingcar/configuration/AppConfigTest.java
@@ -8,15 +8,21 @@ public class AppConfigTest {
 
     @Test
     public void AppConfig가_싱글톤인지() {
-        // given
+        // given && when
         Config config1 = AppConfig.getInstance();
         Config config2 = AppConfig.getInstance();
-
-        //when
 
         // then
         assertThat(config1).isSameAs(config2);
     }
 
+    @Test
+    public void Config의_구현체를_반환하는지() {
+        // given && when
+        Config config1 = AppConfig.getInstance();
+
+        // then
+        assertThat(config1).isInstanceOf(Config.class);
+    }
 
 }
