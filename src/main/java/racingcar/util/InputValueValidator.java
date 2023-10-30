a9863e558c95fffef7118ee64cbea92380e1393a
jeondui <jeondui@gmail.com>
2023-10-30 14:48:28 +0900

    feat(InputValueValidator): 자동차의 최소 개수를 검증하는 기능

--- src/main/java/racingcar/util/InputValueValidator.java
+++ src/main/java/racingcar/util/InputValueValidator.java
@@ -14,6 +14,8 @@ public class InputValueValidator {
             throw new IllegalArgumentException("잘못된 입력입니다. 이름은 중복을 허용하지 않습니다.");
         } else if (!isCarNameLength(carNames)) {
             throw new IllegalArgumentException("잘못된 입력입니다. 이름 길이는 5자 이하만 가능합니다.");
+        } else if(!isMinimumTwoCarNames(carNames)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 최소 2개 이상의 이름을 입력해주세요.");
         }
         return carNames;
     }
@@ -25,6 +27,10 @@ public class InputValueValidator {
 
     private boolean isCarNameLength(List<String> carNames) {
         return carNames.stream().allMatch(name -> name.length() <= 5);
+    }
+
+    private boolean isMinimumTwoCarNames(List<String> carNames) {
+        return carNames.size() > 1;
     }
 
     public int validateTryNumber(int tryNumber) {
