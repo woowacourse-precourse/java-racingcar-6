3b1ebc55128a43e4a754b0e73933f49e2b40e5ca
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 16:17:35 +0900

    test(Cars): 이동거리가 가장 먼 레이싱카 목록 반환 함수의 테스트 코드 작성

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,4 +1,41 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarsTest {
-  
+
+    @Test
+    void 이동거리가_가장_높은_자동자_목록을_반환한다() {
+        // given
+        List<Car> carList = new ArrayList<>();
+
+        Car car1 = new Car("성겸");
+        car1.incrementDistance();
+        car1.incrementDistance();
+
+        Car car2 = new Car("성민");
+        car2.incrementDistance();
+        car2.incrementDistance();
+
+        Car car3 = new Car("성우");
+        car3.incrementDistance();
+
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+
+        Cars cars = new Cars(carList);
+
+        // when
+        List<Car> carsWithHighestDistance = cars.getCarsWithHighestDistance();
+
+        // then
+        Assertions.assertThat(carsWithHighestDistance).contains(car1,car2);
+    }
 }
