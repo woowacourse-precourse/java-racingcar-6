52f0d058f53d2add587a01d1be419d7abaec6fbf
siwol025 <cyk99115695@gmail.com>
2023-10-30 21:03:12 +0900

    feat(racingcar): 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,27 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+
 public class OutputView {
+    private final static String WINNER_MESSAGE = "최종 우승자 : ";
+    private final static String DELIMITER = ", ";
+
     public void showMoveDistance(String carDistance) {
         System.out.println(carDistance);
     }
 
-    public void showWinners() {
+    public void showWinners(List<Car> winners) {
+        StringBuilder raceResult = new StringBuilder();
+        raceResult.append(WINNER_MESSAGE);
 
+        String winnerNames = winners.stream()
+                .map(winner -> winner.getName())
+                .collect(Collectors.joining(DELIMITER));
+
+        raceResult.append(winnerNames);
+
+        System.out.println(raceResult);
     }
 }
