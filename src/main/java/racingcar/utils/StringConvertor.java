3031ea39bb9678455687572354002aaaa69dd4a2
gonixxxx <akdntm946@gmail.com>
2023-10-31 00:14:40 +0900

    refactor(StringConvertor): add method that remove white space

--- src/main/java/racingcar/utils/StringConvertor.java
+++ src/main/java/racingcar/utils/StringConvertor.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class StringConvertor {
 
     public static List<String> convertStringToList(String input) {
-        return Arrays.stream(input.split(",")).toList();
+        return Arrays.stream(removeSpace(input).split(",")).toList();
     }
 
     public static int convertStringToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static String removeSpace(String input) {
+        return input.replaceAll("\\s", "");
     }
 
 }
