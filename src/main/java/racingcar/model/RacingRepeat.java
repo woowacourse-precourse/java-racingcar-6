e8bb2ebfeef9dc845da895bf8dc0f2eb8ea38c09
pokycookie <0cookieboy0@gmail.com>
2023-10-30 15:09:26 +0900

    refactor: 매직넘버 및 매직리터럴 상수화, 입력 에러를 확인하는 메서드 생성자에서 분리

--- src/main/java/racingcar/model/RacingRepeat.java
+++ src/main/java/racingcar/model/RacingRepeat.java
@@ -3,20 +3,31 @@ package racingcar.model;
 public class RacingRepeat {
     private int repeat;
 
+    private final String ERROR_MESSAGE = "[Error]: 잘못된 시도횟수 입력";
+    private final int MINIMUM_REPEAT_RANGE = 1;
+
     public RacingRepeat(String repeatString) {
+        int repeat = parseInt(repeatString);
+        validateIntRange(repeat);
+        this.repeat = repeat;
+    }
+
+    private int parseInt(String intString) {
         try {
-            int repeat = Integer.parseInt(repeatString);
-            if (repeat <= 0) {
-                throw new Exception();
-            }
-            this.repeat = repeat;
+            return Integer.parseInt(intString);
         } catch (Exception e) {
-            throw new IllegalArgumentException("[Error]: 잘못된 시도횟수 입력");
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    private void validateIntRange(int repeat) {
+        if (repeat < MINIMUM_REPEAT_RANGE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
     public boolean decrease() {
-        if (repeat > 0) {
+        if (repeat >= MINIMUM_REPEAT_RANGE) {
             repeat--;
             return true;
         }
