0c06fd4a3dbcc601ec301425ca7998ca588fc68d
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-31 01:04:31 +0900

    feat: 결과를 return하는 RacingResult 추가

---
+++ src/main/java/racingcar/model/RacingResult.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class RacingResult {
+    private Cars cars;
+
+    public RacingResult(Cars cars) {
+        this.cars = cars;
+    }
+
+    public String winners() {
+        StringBuilder sb = new StringBuilder();
+        for (String winner : findWinners()) {
+            sb.append(winner)
+                    .append(SpecialSign.SEPARATOR.getSign())
+                    .append(SpecialSign.SPACE.getSign());
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    private int getMaxDistance() {
+        return cars.maxDistance();
+    }
+
+    private List<String> findWinners() {
+        int maxDistance = cars.maxDistance();
+        return cars.getCars().stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .map(car -> car.getName())
+                .toList();
+    }
+}
