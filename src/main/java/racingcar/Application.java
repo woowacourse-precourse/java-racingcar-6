a0901cd5926a655a8bdaa5eac29f0d2394e12e60
alscjf1329 <alscjf1329@gmail.com>
2023-10-30 13:07:03 +0900

    feat: RacingCarGame 실행 코드 추가
    RacingCarGameLauncher의 launch를 통해 RacingCarGame 실행하는 코드를 작성했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.utils.RacingCarGameInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarGameLauncher launcher = new RacingCarGameLauncher();
+        launcher.launch();
     }
 }
