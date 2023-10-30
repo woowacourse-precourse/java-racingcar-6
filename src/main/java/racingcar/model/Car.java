80bdfc75b48023c30d71c7af3c40e24609897462
kimheekimhee <khd128k@gmail.com>
2023-10-30 12:16:24 +0900

    fix: 이름에 대한 예외 처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,18 +1,30 @@
 package racingcar.model;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
     private String name;
     private int position = 0;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
     }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_LENGTH + "자를 초과할 수 없습니다.");
+        }
+    }
+
     public String getName() {
         return name;
     }
+
     public int getPosition() {
         return position;
     }
+
     public void move() {
         this.position++;
     }
