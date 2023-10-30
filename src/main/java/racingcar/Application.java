f4bba985246c8b3f1f019f916dbf065bb75669b1
junest66 <chlwndks333@gmail.com>
2023-10-31 00:10:44 +0900

    refactor(Application): 프로그램 시작 시 CarListCreator를 사용하여 자동차 목록 생성하도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.utils.CarListCreator;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         RacingController racingController = new RacingController();
-        racingController.play();
+        racingController.play(CarListCreator.generateCarListFromInput());
     }
 }
