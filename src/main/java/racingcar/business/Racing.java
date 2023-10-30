9a34d08fdb36da2f8cc5802dab4ef19ea3ec4849
gumgu <bienlee@naver.com>
2023-10-30 17:32:25 +0900

    Feat : 입력받은 이름에 맞춰 자동차 생성 기능 구현

--- src/main/java/racingcar/business/Racing.java
+++ src/main/java/racingcar/business/Racing.java
@@ -3,19 +3,34 @@ package racingcar.business;
 import racingcar.ui.InputView;
 import racingcar.ui.Output;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Racing {
 
     InputView inputView = new InputView();
 
-    public void doRacing() {
+    List<Car> carList;
 
+    public void doRacing() {
         Output.GET_NAMES.print();
         List<String> names = inputView.getNames();
 
         Output.GET_GAME_COUNT.print();
+        Integer gameCount = inputView.getGameCount();
 
+        System.out.println("gameCount = " + gameCount);
+        System.out.println("names = " + names);
 
+        carList = getCars(names);
+    }
+
+    private static List<Car> getCars(List<String> names) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+        return carList;
     }
 }
