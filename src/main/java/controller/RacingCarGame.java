c20c5a2eb1f5af412bb7fc860a8b831e1a9e5a48
dsa04156 <dsa04156@naver.com>
2023-10-31 00:31:59 +0900

    feat : 랜덤 값 받고 해당 랜덤 값에 따른 전진 횟수 저장

--- src/main/java/controller/RacingCarGame.java
+++ src/main/java/controller/RacingCarGame.java
@@ -1,18 +1,21 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
 import model.Car;
 import model.TryCount;
-import org.junit.platform.commons.function.Try;
 import util.CarNameValidation;
 import util.TryCountValidation;
 import view.InputView;
 import view.OutputView;
 
 public class RacingCarGame {
+    private static final int RANDOM_MIN_NUMBER = 0;
+    private static final int RANDOM_MAX_NUMBER = 9;
+    private static final int CAN_MOVE_NUMBER = 4;
     static ArrayList<Car> carNameList;
     static TryCount tryCount;
+    static String[] carMoveCount;
     private static InputView inputView;
     private static OutputView outputView;
 
@@ -21,13 +24,50 @@ public class RacingCarGame {
         inputView = new InputView();
         outputView = new OutputView();
         carNameList = new ArrayList<>();
+
     }
 
     public void start() {
         outputView.printStartGame();
         setCarNames(inputView.inputCarName());
         setTryCount(inputView.inputTryCount());
+        initCarMoveCount();
 
+        outputView.printResultGame();
+        for (int tc = 0; tc < tryCount.getTryCount(); tc++) {
+            setRandomNumber();
+            printResult();
+        }
+        
+
+    }
+
+    private void initCarMoveCount() {
+        carMoveCount = new String[carNameList.size()];
+        for (int i = 0; i < carMoveCount.length; i++) {
+            carMoveCount[i] = "";
+        }
+    }
+
+    private void printResult() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < carNameList.size(); i++) {
+            sb.append(carNameList.get(i).getCarName() + " : " + carMoveCount[i] + "\n");
+        }
+        outputView.printMoveResult(sb);
+    }
+
+    private void setRandomNumber() {
+        for (int i = 0; i < carNameList.size(); i++) {
+            int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER);
+            isMove(randomNumber, i);
+        }
+    }
+
+    private void isMove(int randomNumber, int carNumber) {
+        if (randomNumber >= CAN_MOVE_NUMBER) {
+            carMoveCount[carNumber] += "-";
+        }
     }
 
     private void setCarNames(String[] carNames) {
@@ -39,7 +79,7 @@ public class RacingCarGame {
 
     private void setTryCount(String tryCount) {
         TryCountValidation.checkTryCountValidation(tryCount);
-        this.tryCount = new TryCount(Integer.parseInt(tryCount));
+        RacingCarGame.tryCount = new TryCount(Integer.parseInt(tryCount));
     }
 
 
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,8 +2,16 @@ package view;
 
 public class OutputView {
 
-    public void printStartGame(){
+    public void printStartGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printResultGame() {
+        System.out.println("실행 결과");
+    }
+
+    public void printMoveResult(StringBuilder sb) {
+        System.out.println(sb);
     }
 
 }
