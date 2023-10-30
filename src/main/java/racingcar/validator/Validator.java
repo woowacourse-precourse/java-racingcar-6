c417a8c6003795092836d7fd0d3ec75013a0db38
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-30 11:02:18 +0900

    refactor(Validator): 검증 메소드 분리

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -10,15 +10,17 @@ import racingcar.exception.OnlyCommaInputException;
 
 public class Validator {
     private static final int CAR_NAME_RANGE = 5;
+    private static final int EMPTY = 0;
+    private static final String DELIMITER = ",";
 
     private Validator() {
     }
 
-    public static void carNameValidator(String carNames) {
-        checkOnlyCommaInput(carNames);
+    public static String[] carNameValidator(String carNames) {
         String[] splitCarNames = splitCarNames(carNames);
         checkCarNameLength(splitCarNames);
         checkDuplicateCarName(splitCarNames);
+        return splitCarNames;
     }
 
     public static void checkEmptyCarName(String splitCarName) {
@@ -34,9 +36,16 @@ public class Validator {
     }
 
     private static void checkCarNameLength(String[] splitCarNames) {
+        checkEmptyCarNameArray(splitCarNames);
         for (String splitCarName : splitCarNames) {
             checkCarNameRange(splitCarName);
             checkEmptyCarName(splitCarName);
+        }
+    }
+
+    private static void checkEmptyCarNameArray(String[] splitCarNames) {
+        if (splitCarNames.length == EMPTY) {
+            throw new OnlyCommaInputException();
         }
     }
 
@@ -47,20 +56,16 @@ public class Validator {
     private static void checkDuplicateCarName(String[] splitCarNames) {
         Set<String> carNameSet = new HashSet<>();
         Arrays.stream(splitCarNames)
-                .forEach(carName -> {
-                    if (!carNameSet.add(trimCarName(carName))) {
-                        throw new DuplicateCarNameException();
-                    }
-                });
+                .forEach(carName -> duplicateCarSet(carName, carNameSet));
+    }
+
+    private static void duplicateCarSet(String carName, Set<String> carNameSet) {
+        if (!carNameSet.add(trimCarName(carName))) {
+            throw new DuplicateCarNameException();
+        }
     }
 
     private static String[] splitCarNames(String carNames) {
-        return carNames.split(",");
-    }
-
-    private static void checkOnlyCommaInput(String carNames) {
-        if (carNames.equals(",")) {
-            throw new OnlyCommaInputException();
-        }
+        return trimCarName(carNames).split(DELIMITER);
     }
 }
