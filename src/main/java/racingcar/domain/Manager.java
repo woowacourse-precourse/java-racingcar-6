c80e338ead3c9d92d40a94bd4eee6766d18969a8
706com <ehdgusjdbswl@gmail.com>
2023-10-30 11:22:18 +0900

    feat : 레이싱 매니저 도메인 객체 생성
    각 Car 마다 누가 전진하고,
    최종 우승자는 누구인지 판별한다.

---
+++ src/main/java/racingcar/domain/Manager.java
@@ -0,0 +1,69 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Manager {
+
+    private List<Car> carList;
+
+    private int maxLine;
+
+    private int moveCount;
+
+    public Manager(int moveCount) {
+        this.carList = new ArrayList<>();
+        this.moveCount = moveCount;
+        this.maxLine = 0;
+    }
+
+    public void registerCar(Car car){
+        this.carList.add(car);
+    }
+
+    public void manageMoveOneCycle(){
+        for(Car car : carList){
+            if(possibleToMove(car)) {
+                car.getMoveChance();
+            }
+            increaseMaxLine(car.getScore());
+        }
+        endOneCycle();
+    }
+
+    public List<String> resultWinner(){
+        List<String> winner = new ArrayList<>();
+        for(Car car : carList){
+            if(car.getScore()==maxLine){
+                winner.add(car.getName());
+            }
+        }
+        return winner;
+    }
+
+    private boolean possibleToMove(Car car){
+        if(car.getMoveCount()>0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void increaseMaxLine(int score){
+        if (maxLine < score) {
+            this.maxLine = score;
+        }
+    }
+
+    private void endOneCycle(){
+        moveCount--;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public List<Car> getCarList() {
+        return new ArrayList<>(carList);
+    }
+
+}
