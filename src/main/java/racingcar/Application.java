11c2298c2802162339af21e5d4fd1e77e30fd49b
gusgh075 <gusgh075@naver.com>
2023-10-30 21:38:10 +0900

    feat(application):시작환경 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import racingcar.domain.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        game.addCarList(Console.readLine().split(","));
+        System.out.println("시도할 회수는 몇회인가요?");
+        game.setMoveCount(Integer.parseInt(Console.readLine()));
+        game.showResult();
+        game.showWinners();
     }
 }
--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -6,25 +6,54 @@ import java.util.List;
 
 public class Game {
     int moveCount;
-    private List<Car> cars=new ArrayList<>();
+    private List<Car> carList = new ArrayList<>();
+
     public void setMoveCount(int moveCount) {
         this.moveCount = moveCount;
     }
-    public void moveCars(){
-        for(Car car:cars){
+
+    public void addCar(Car car) {
+        carList.add(car);
+    }
+
+    public void addCarList(String[] s){
+        for(String name:s){
+            carList.add(new Car(name));
+        }
+    }
+    public void moveCars() {
+        for (Car car : carList) {
             car.move();
         }
     }
-    public List<String> getWinner(){
-        List<String> winners=new ArrayList<>();
-        int max=0;
-        for(Car car:cars){
-            if(car.getLocation()>=max){
-                if(car.getLocation()>max)
+
+    public void showResult() {
+        System.out.println("\n실행결과");
+        for(int i=0;i<moveCount;i++) {
+            moveCars();
+            for (Car car : carList) {
+                System.out.println(car.showLocation());
+            }
+            System.out.println();
+        }
+    }
+
+    public List<String> getWinner() {
+        List<String> winners = new ArrayList<>();
+        int max = 0;
+        for (Car car : carList) {
+            int loc=car.getLocation();
+            if (loc >= max) {
+                if (loc > max)
                     winners.clear();
                 winners.add(car.getName());
+                max=loc;
             }
         }
         return winners;
+    }
+    public void showWinners(){
+        String winners = Arrays.toString(getWinner().toArray()).replace("[", "").replace("]", "");
+        System.out.printf("최종 우승자 : %s",winners);
     }
 }
