57ba09cbc335b5a508b6daa59861c9997baacf12
mksystem98 <mksystem98@gmail.com>
2023-10-30 16:30:30 +0900

    feat: 자동차 경주 게임의 핵심 로직이 담겨있는 클래스

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import racingcar.domain.RacingGame;
+import racingcar.model.RandomGenerator;
+
+public class RacingGameTest {
+
+    private RacingGame racingGame;
+    private RandomGenerator testRandomGenerator;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+}
