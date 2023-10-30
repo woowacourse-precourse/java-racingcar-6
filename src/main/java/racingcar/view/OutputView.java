27787f5c204ef65139b8b064b24c3d89ef1feaa5
youngbeom-Kim <abcd450196@gmail.com>
2023-10-31 05:26:10 +0900

    feat: 레이싱카 게임 결과 domain, controller, view 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,9 @@ public class OutputView {
     private static final String ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String EXECUTION_RESULT = "실행 결과";
     private static final String FINAL_WINNER = "최종 우승자";
+    private static final String COLON = " : ";
 
+    // 상수랑 상수를 출력시키는 함수는 별도로 관리하는게 좋지 않을까용
     public static void carNameInput() {
         System.out.println(CAR_NAME_MESSAGE);
     }
@@ -16,11 +18,14 @@ public class OutputView {
     public static void executionResultView() {
         System.out.println(EXECUTION_RESULT);
     }
-    public static void lastWinnerView() {
-        System.out.println(FINAL_WINNER);
+    public static void finalWinnerView() {
+        System.out.print(FINAL_WINNER);
     }
     public static void blankLine() {
         System.out.println();
+    }
+    public static void colon() {
+        System.out.print(COLON);
     }
 
 }
