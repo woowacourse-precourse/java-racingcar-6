108e9622efd1a5d0d7c52815e378b8e03d290b2f
shining013 <nevertheless113@naver.com>
2023-10-30 23:29:43 +0900

    시도 회수 입력 메소드

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -38,6 +38,13 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
+    public static int enterTryNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryNum = Console.readLine();
+        validateCountNumber(tryNum);
+        return Integer.parseInt(tryNum);
+    }
+
     public static void enterName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
