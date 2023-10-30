c0ae3fe52a6564c157da7c1c07d50c9b59f8cf20
dltmddus <tmddus4671@gmail.com>
2023-10-30 22:50:12 +0900

    feat: inputNumber 메소드 구현, fix: inputCarNames 예외 처리 수정

--- src/main/java/racingcarGame/Game.java
+++ src/main/java/racingcarGame/Game.java
@@ -1,7 +1,9 @@
 package racingcarGame;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static constant.Constants.*;
 
@@ -23,20 +25,43 @@ public class Game {
 
     private void gameStart(){
         inputCarNames();
+        inputNumber();
     }
 
     private void inputCarNames(){
+        System.out.println(INPUT_CAR_NAMES);
         String inputString = Console.readLine();
         String[] inputStringArr = inputString.split(",");
         int arrLength = inputStringArr.length;
         if(arrLength > 5){
             throw new IllegalArgumentException(WRONG_INPUT_SIZE);
         }
+        Set<String> carNameSet = new HashSet<>();
         for(int i = 0; i < arrLength; i++) {
             carNames.add(inputStringArr[i]);
         }
-        for(String name : carNames){
-                System.out.println(name);
+        for(String carName : carNames){
+            if(carNameSet.contains(carName)){
+                throw new IllegalArgumentException(WRONG_INPUT_DUPLICATION);
+            }
+            carNameSet.add(carName);
+//                System.out.println(name);
         }
+//        for (String carName : carNameSet) {
+//            System.out.println(carName);
+//        }
+    }
+
+    private void inputNumber(){
+        System.out.println(INPUT_NUMBER);
+        String inputNumString = Console.readLine();
+        int inputNum = 0;
+        try {
+            inputNum = Integer.parseInt(inputNumString);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
