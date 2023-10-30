aa5e01ee2a97e5ef7686dfdc0769bab47fc47b19
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-31 02:11:14 +0900

    feat(RacingGameController): 레이싱 게임 컨트롤러 생성 및 자동차 목록 추가
    - 레이싱 게임 컨트롤러 생성
    - InputView에서 받은 값으로 Cars 객체 생성 후 cars에 할당

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import racingcar.domain.Cars;
+import racingcar.domain.Round;
+import racingcar.view.InputView;
+
+public class RacingGameController {
+    private Cars cars;
+    private Round round;
+
+    public RacingGameController(){
+    }
+
+    public void playGame() {
+        cars = new Cars(InputView.inputNames());
+    }
+}
