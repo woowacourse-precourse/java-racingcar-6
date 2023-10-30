491ee2de2e9b683235e8ea590132e79b6dbcfd4d
prao <pch5521@gmail.com>
2023-10-30 23:17:35 +0900

    feat: (Enum)ErrorMessage 클래스 추가
    - 에러메시지를 관리하는 enum 클래스 생성

---
+++ src/main/java/racingcar/enums/ErrorMessage.java
@@ -0,0 +1,19 @@
+package racingcar.enums;
+
+public enum ErrorMessage {
+    INVALID_COUNT("%d 이상의 정수만 입력할 수 있습니다."),
+    INPUT_IS_NULL("아무런 값을 입력하지 않으셨습니다."),
+    INVALID_MIN_CARS("최소한 %d개 이상의 자동차 이름을 입력해 주세요."),
+    INVALID_CAR_NAME("자동차 이름은 영문 알파벳으로 작성해 주세요."),
+    INVALID_CAR_NAME_SIZE("자동차 이름은 %d자 이하로 작성해 주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
