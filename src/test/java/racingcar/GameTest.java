ebc177f17996ec47681836a53fb6db4e97bf35ed
pjaehyun <show6756@gmail.com>
2023-10-30 19:43:57 +0900

    feat: 추가 GameTest Class 추가 및 start 함수 기능 테스트

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import game.Game;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest {
+
+    @Test
+    void start_게임_시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    String carsInput = "car1,car2,car3";
+                    String frequencyInput = "1";
+
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    System.setOut(new PrintStream(outputStream));
+                    System.setIn(new ByteArrayInputStream((carsInput + System.lineSeparator() + frequencyInput).getBytes()));
+
+
+                    Game game = new Game();
+                    game.start();
+
+                    String captured = outputStream.toString().trim();
+                    assertThat(captured).contains("car1 : -", "car2 : -", "car3 : ", "최종 우승자 : car1, car2");
+                },
+                4, 4, 3
+        );
+    }
+}
