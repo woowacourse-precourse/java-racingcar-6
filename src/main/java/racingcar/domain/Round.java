093c826be8242d40dc3b88f713dacae5d3ecfb5f
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-31 01:57:16 +0900

    feat(Round): 입력 받은 값이 정수가 아닐 경우 예외처리

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -3,15 +3,25 @@ package racingcar.domain;
 public class Round {
     public final int round;
     private static final String INVALID_INPUT_IS_EMPTY = "입력값이 공백입니다.";
+    private final static String INVALID_INPUT_TYPE = "숫자가 아닌 값이 포함되어있습니다.";
 
     public Round(String input) {
         validateInputNotEmpty(input);
+        validateInteger(input);
         this.round = Integer.parseInt(input);
     }
 
     private void validateInputNotEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(INVALID_INPUT_IS_EMPTY);
+        }
+    }
+
+    private static void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE);
         }
     }
 }
