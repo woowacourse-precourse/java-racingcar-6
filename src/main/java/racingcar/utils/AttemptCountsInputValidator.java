df7649e0287999e8250b04da1071a0e0e9209680
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 16:20:24 +0900

    refactor(AttemptCountsInputValidator): 시도 횟수 입력과 관련한 예외 메시지는 클래스 내부에서 관리하도록 변경

--- src/main/java/racingcar/utils/AttemptCountsInputValidator.java
+++ src/main/java/racingcar/utils/AttemptCountsInputValidator.java
@@ -14,7 +14,7 @@ public class AttemptCountsInputValidator {
 
     private static void validateNumeric(String target) {
         if (!NUMERIC_REGEX.matcher(target).matches()) {
-            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMERIC.getError());
+            throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.NOT_NUMERIC.getError());
         }
     }
 
@@ -22,14 +22,30 @@ public class AttemptCountsInputValidator {
         try {
             Integer.parseInt(target);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_INTEGER_RANGE.getError());
+            throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.OUT_OF_INTEGER_RANGE.getError());
         }
     }
 
     private static void validateRange(String target) {
         int attemptCounts = Integer.parseInt(target);
         if (attemptCounts < 1) {
-            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE.getError());
+            throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.OUT_OF_RANGE.getError());
+        }
+    }
+
+    enum AttemptCountsInputExceptionMessage {
+        NOT_NUMERIC("숫자만 입력해야 합니다."),
+        OUT_OF_INTEGER_RANGE("int 범위를 벗어났습니다."),
+        OUT_OF_RANGE("1회 ~ 2147483647회 만 입력 가능합니다.");
+
+        private final String error;
+
+        AttemptCountsInputExceptionMessage(String error) {
+            this.error = error;
+        }
+
+        String getError() {
+            return error;
         }
     }
 }
