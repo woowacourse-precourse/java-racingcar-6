648073ed1c25d8935e692adb96dedf23cc1f6fd2
howudong <tjdvy963@naver.com>
2023-10-30 15:25:13 +0900

    test : RacingOutputView 테스트코드 추가

---
+++ src/test/java/racingcar/view/outputview/RacingOutputViewTest.java
@@ -0,0 +1,65 @@
+package racingcar.view.outputview;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.MoveRule;
+import racingcar.service.CarService;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class RacingOutputViewTest {
+    private static ByteArrayOutputStream outputMessage;
+    private OutputView outputView = new RacingOutputView();
+    CarService carService;
+
+    private MoveRule createMoveRule(int generateNumber) {
+        return new MoveRule() {
+            @Override
+            public int tryMove() {
+                return generateNumber;
+            }
+        };
+    }
+
+    @BeforeEach
+    void setUpStreams() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @AfterEach
+    void restoresStreams() {
+        System.setOut(System.out); // 원상복귀
+    }
+
+    @Test
+    @DisplayName("출력이 정상적으로 이루어져야한다.")
+    void RacingOutputViewTest() {
+        //given
+        Car car1 = new Car("tobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+
+        car2.move(createMoveRule(2));
+        car3.move(createMoveRule(3));
+
+        Map<String, Object> model = new HashMap<>();
+        //when
+        model.put("cars", new Cars(List.of(car1, car2, car3)));
+        outputView.display(model);
+        //then
+        Assertions.assertThat(outputMessage.toString()).contains("tobi :");
+        Assertions.assertThat(outputMessage.toString()).contains("woni : --");
+        Assertions.assertThat(outputMessage.toString()).contains("jun : ---");
+    }
+
+}
