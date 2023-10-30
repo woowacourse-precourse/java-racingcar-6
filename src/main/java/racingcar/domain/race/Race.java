588657e8aae2772072764441750e88954ac1e4c1
gilik154 <okk080200@gmail.com>
2023-10-30 17:28:09 +0900

    Feat : Race 객체 생성
    - Race.java 로 레이스를 진행하는 로직 작성

---
+++ src/main/java/racingcar/domain/race/Race.java
@@ -0,0 +1,20 @@
+package racingcar.domain.race;
+
+import racingcar.domain.car.Car;
+import racingcar.domain.policy.DriveRule;
+import racingcar.domain.policy.RandomDriveRule;
+
+import java.util.List;
+
+public class Race {
+    private final DriveRule driveRule;
+
+    public Race(RandomDriveRule randomDriveRule) {
+        this.driveRule = randomDriveRule;
+    }
+
+    public void run(List<Car> cars) {
+        cars.forEach(car ->
+                car.move(driveRule));
+    }
+}
