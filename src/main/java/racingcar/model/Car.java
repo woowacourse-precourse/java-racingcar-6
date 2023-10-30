4dacfb07c60f0698fa3563f140622c6726b7a431
Minsu Choi <cms102510@naver.com>
2023-10-30 14:06:06 +0900

    refactor: 상수 치환

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,6 +3,10 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+
+    private static final int MAX_NAMELENGTH = 5;
+    private static final int MOVE_THRESHOLD = 4;
+
     private final String name;
     private int position = 0;
 
@@ -12,13 +16,13 @@ public class Car {
     }
 
     private void validateNameLength(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAMELENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 
     public void tryMove() {
-        if (Randoms.pickNumberInRange(1, 9) >= 4) {
+        if (Randoms.pickNumberInRange(1, 9) >= MOVE_THRESHOLD) {
             position++;
         }
     }
