5e5d20b8996d270e6a5b84123a24bdaba4ee0132
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-30 21:55:33 +0900

    refactor: add constants to racing game config about input validator and split if-conditions to method in input validator

--- src/main/java/racingcar/config/RacingGameConfig.java
+++ src/main/java/racingcar/config/RacingGameConfig.java
@@ -3,7 +3,9 @@ package racingcar.config;
 public enum RacingGameConfig {
     RANDOM_MIN_VALUE(0),
     RANDOM_MAX_VALUE(9),
-    MOVE_CONDITION(4);
+    MOVE_CONDITION(4),
+    NUMBER_OF_TRY_MIN_VALUE(1),
+    CAR_NAME_MAX_LENGTH(5);
 
     private int value;
 
--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -1,23 +1,35 @@
 package racingcar.view;
 
+import racingcar.config.RacingGameConfig;
+
 public class InputValidator {
     public void validateRacingCarName(String[] racingCarNames){
         for(String racingCarName : racingCarNames){
-            if(racingCarName.length() > 5){
+            if(isBiggerMaxLength(racingCarName)){
                 throw new IllegalArgumentException("car name size must be smaller than 6.");
             }
         }
     }
 
     public void validateNumberTypeOfTry(String input){
-        if(!input.matches("[0-9]")){
+        if(!isNumber(input)){
             throw new IllegalArgumentException("number of try must be typed number");
         }
     }
 
     public void validateNumberRangeOfTry(int input){
-        if(input < 1){
+        if(isSmallerMinValue(input)){
             throw new IllegalArgumentException("number range of try must be bigger than 0");
         }
+    }
+
+    private boolean isSmallerMinValue(int input){
+        return input < RacingGameConfig.NUMBER_OF_TRY_MIN_VALUE.getValue();
+    }
+    private boolean isNumber(String input){
+        return input.matches("[0-9]");
+    }
+    private boolean isBiggerMaxLength(String racingCarName){
+        return racingCarName.length() > RacingGameConfig.CAR_NAME_MAX_LENGTH.getValue();
     }
 }
