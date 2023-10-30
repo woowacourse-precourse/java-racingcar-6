90d77c1017b197b95a42d532664240f8cf1f1ef1
Terry <t3rryahn.dev@gmail.com>
2023-10-31 01:57:01 +0900

    feat: Race 클래스 추가.
    car객체 List를 가지고 생성되어 경주를 진행하고 우승자를 판단하는 클래스.
    
    전진 시도, 차량 전진, 경기(한라운드)진행, 우승자 선별, 우승자 전송 기능 구현.

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import racingcar.util.RandomNumberGenerator;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Race {
+    private List<Car> cars;
+    private List<Car> winners = new ArrayList<>();
+
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public boolean canCarGoForward() {
+        int randomNumber = RandomNumberGenerator.getNumber();
+
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public void tryMoveCar(Car car) {
+        if (canCarGoForward()) {
+            car.goForward();
+        }
+    }
+
+    public void runRace() {
+        for (Car car : cars) {
+            tryMoveCar(car);
+        }
+    }
+
+    public void setWinners(List<Car> cars) {
+        winners = (List<Car>) cars.stream()
+                .max(Comparator.comparing(Car::getDistance))
+                .orElseThrow();
+    }
+
+    public List<Car> getWinners() {
+        return winners;
+    }
+
+
+}
