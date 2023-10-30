371ada73159122cb3eb27252825a4bc9339b9484
seminchoi <choicco89@gmail.com>
2023-10-30 21:04:45 +0900

    feat(racingcar.Application) : main 메소드에 게임 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarGameFactory racingCarGameFactory = new RacingCarGameFactoryImpl();
+        RacingCarGameController racingCarGameController = racingCarGameFactory.racingCarController();
+        racingCarGameController.play();
     }
 }
