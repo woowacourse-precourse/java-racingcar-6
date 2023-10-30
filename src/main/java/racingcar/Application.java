0bf43a97962e671b09066394b48f154bbff34bb1
ioyxxnstream <ioyxxnstream@naver.com>
2023-10-30 16:41:18 +0900

    주석 최종 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
@@ -35,14 +34,11 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // 차 이름 입력 안내
         Print.message(1);
 
-        // 차 이름 입력 받기
         String[] carName = Get.carName();
 
         try {
-            // 차 이름이 5자 초과면 예외 발생
             for(String s: carName){
                 if(s.length() > 5){
                     throw new IllegalArgumentException();
@@ -52,38 +48,29 @@ public class Application {
             throw new IllegalArgumentException();
         }
 
-        // 실행 횟수 입력 안내
         Print.message(2);
 
-        // 실행 횟수 입력 받기
         int playCount = Get.playCount();
 
-        // car 리스트 생성
         List<Car> carList = new ArrayList<>();
 
-        // car 객체 생성 후 리스트에 넣기
         for(String s: carName){
             Car c = new Car(s);
             carList.add(c);
         }
 
-        // 실행 결과 출력
         Print.message(3);
 
-        // 실행 횟수 만큼 각 car들을 전진 시키기
         for(int i = 0;i<playCount;i++){
             forward(carList);
             Print.result(carList);
             System.out.println();
         }
 
-        // car 리스트 distance 값 기준으로 정렬하기
         carList = sortCarDistance(carList);
 
-        // 우승자 리스트 생성
         List<String> winners = winnerListGenerator(carList);
 
-        // 우승자 출력
         Print.winner(winners);
 
     }
