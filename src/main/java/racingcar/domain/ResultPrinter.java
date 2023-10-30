42b4bdb2e2fcd0420cf19f502f1733b2c68f169d
junghunim07 <dlawndgns9@naver.com>
2023-10-30 12:11:10 +0900

    feat(ResultPrinter): ResultPrinter 변수 선언

---
+++ src/main/java/racingcar/domain/ResultPrinter.java
@@ -0,0 +1,7 @@
+package racingcar.domain;
+
+public class ResultPrinter {
+    private static String getUserName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String getTry = "시도할 회수는 몇회인가요?";
+    private static String winner = "최종 우승자 : ";
+}
