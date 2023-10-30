cb621992388212a695893a41c1ef054ee77f2680
gumgu <bienlee@naver.com>
2023-10-30 21:14:45 +0900

    Feat : 각 라운드 결과 출력 기능 구현

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -9,7 +9,18 @@ public class OutputView {
     }
 
     public void printMoveResult(Car car) {
-        System.out.println(car.getName() + Output.SEPARATOR.getComment() + car.getDistance());
+        String toHyphen = convertDistanceToHyphen(car.getDistance());
+        System.out.println(car.getName() + Output.SEPARATOR.getComment() + toHyphen);
+    }
+
+    private String convertDistanceToHyphen(Integer distance) {
+        String result = "";
+
+        for (int i = 0; i < distance; i++) {
+            result += "-";
+        }
+
+        return result;
     }
 
 }
