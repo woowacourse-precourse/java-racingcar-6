54aba146b372760d116c17bd60de40a8e08a5672
남수연 <rnrmf0721@gmail.com>
2023-10-30 14:04:40 +0900

    feat(Input): 자동차 경주의 실행 결과를 출력하는 함수 구현

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import static racingcar.constant.Constant.EXECUTION_RESULT;
+
+public class Input {
+
+    public void printExecutionResult(String executionResult){
+        System.out.println(EXECUTION_RESULT);
+        System.out.println(executionResult);
+    }
+}
