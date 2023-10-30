ce3f534db7cc502408b585c6ed225d0434dc6901
wonindev <sangwonparkdev@gmail.com>
2023-10-30 10:44:20 +0900

    feat: 초기 해쉬맵에 put해주는 기능을 생성

--- src/main/java/racingcar/CarMovement.java
+++ src/main/java/racingcar/CarMovement.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class CarMovement {
     public static boolean isMove(int randomNum) {
         return randomNum >= 4;
+    }
+
+    public static void putHM(HashMap<String, String> membersHM, List<String> members, String str) {
+        for (int i = 0; i < members.size(); i++) {
+            membersHM.put(members.get(i), str);
+        }
     }
 }
