0f08c9878a05df4be631b1d07f3e6ffb88fc961e
sleeg00 <slee000220@gmail.com>
2023-10-30 22:36:40 +0900

    Feat: 자동차 이름을 입력 저장
    자동차 이름을 입력받고 예외처리 후 Car객체에 저장했다

---
+++ src/main/java/racingcar/model/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model.domain;
+
+import java.util.Map;
+
+public class Car {
+    private Map<String, Integer> car;
+
+    public void setName(Map<String, Integer> car) {
+        this.car = car;
+    }
+}
+
