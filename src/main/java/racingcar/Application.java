00c1e29fe1eadd5d5dd25b41b5728777532db542
sequaa <asl06194@gmail.com>
2023-10-31 02:23:38 +0900

    feat: Application 기능 추가 실행 결과 예시에 맞게 입력을 유도하는 메시지를 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,10 +8,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = Console.readLine();
         List<String> carNames = Arrays.asList(inputNames.split(","));
 //      List<String> carNames = Arrays.asList(Console.readLine().split(","));
 
+        System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = Integer.parseInt(Console.readLine());
 
         List<Car> cars = new ArrayList<>();
