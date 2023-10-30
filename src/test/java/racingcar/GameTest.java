a367c8a36224847a699678123faf24a28f59675f
Hajuuu <hpt0127@naver.com>
2023-10-30 21:02:38 +0900

    test: findWinner 기능 테스트 코드 추가

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,6 +1,9 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +23,16 @@ class GameTest {
     @DisplayName("가장 멀리 전진한 자동차 찾기")
     @Test
     void findWinner() {
+        //given
+        List<User> users = new ArrayList<>();
+        users.add(new User("abc", 3));
+        users.add(new User("qwer", 5));
+
+        //when
+        int max = Game.findWinner(users);
+
+        //then
+        assertThat(max).isEqualTo(5);
     }
 
     @DisplayName("0~9중 한 값을 반환한다.")
@@ -32,7 +45,7 @@ class GameTest {
         number = Game.randomNumber();
 
         //then
-        Assertions.assertThat(number).isBetween(0, 9);
+        assertThat(number).isBetween(0, 9);
     }
 
     @DisplayName("")
