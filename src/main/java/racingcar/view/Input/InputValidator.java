bfd284f8c5a9aa715199561974e14dcf677d8084
yang <hjyang@epience.com>
2023-10-30 17:27:14 +0900

    📄docs : 기능목록 업데이트

--- src/main/java/racingcar/view/Input/InputValidator.java
+++ src/main/java/racingcar/view/Input/InputValidator.java
@@ -5,7 +5,7 @@ public class InputValidator {
         // [x] 쉼표가 연속해서 있을 경우 >> 정규 표현식 split(",+")
         // [x] 쉼표 사이에 공백이 있을 경우 >> replace(" ", "");
         // [x] 공백일 경우 >> exception
-        // [ ] 5대 초과일 경우
+        // [ ] 5대 초과일 경우 >> Cars에서 해야할 듯
     public void validateCarString(String input) {
         if (input.trim() == null) {
             throw new IllegalArgumentException("");
