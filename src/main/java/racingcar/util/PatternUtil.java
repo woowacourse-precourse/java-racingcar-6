5a87543a4fce1a64b464b646c405dd768b6840ec
DevFrog92 <yongsu1481@gmail.com>
2023-10-30 16:42:00 +0900

    refactor: remove unnecessary method

--- src/main/java/racingcar/util/PatternUtil.java
+++ src/main/java/racingcar/util/PatternUtil.java
@@ -1,9 +1,11 @@
 package racingcar.util;
 
+/**
+ * 유효성 검사에 필요한 정규 표현식을 정의한 util 클래스 입니다.
+ */
 public class PatternUtil {
-        public static final String ONLY_DIGIT_PATTERN = "\\d+";
-        
-        public static String getOnlyDigitNumberPattern() {
-                return ONLY_DIGIT_PATTERN;
-        }
+        /**
+         * 양수만 입력할 수 있는 규칙입니다.
+         */
+        public static final String ONLY_POSITIVE_NUMBER_PATTERN = "^[0-9]\\d*$";
 }
