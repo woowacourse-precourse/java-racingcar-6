d6f4c3c2bbe67c4fd63a5a6dcfd7a3af09e02007
Taehyoun Kim <kth9903@gmail.com>
2023-10-30 16:49:57 +0900

    feat(inputmanager): add method to obtain and validate car names from user input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -91,4 +91,14 @@ class InputValidator{
 
 class InputManger{
 
+    public String[] getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+
+        for(int i=0; i<carNames.length; i++){
+            carNames[i] = carNames[i].strip();
+        }
+        InputValidator.validateCarNames(carNames);
+        return carNames;
+    }
 }
