4aecb58a930fc13b1afa3e203d04850593dbe564
seonghun <sdzx0719@naver.com>
2023-10-30 20:49:13 +0900

    docs(README): 레이싱게임 구현기능 목록 작성 수정 및 클래스 분류

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,7 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
     public static void main(String[] args) {
-        int r = Randoms.pickNumberInRange(0,9);
+        // TODO: 프로그램 구현
     }
 }
