b3c15911e913cecf36726d64b921f46c00f0a799
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-10-30 14:58:18 +0900

    chore : delete unused import statement

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
