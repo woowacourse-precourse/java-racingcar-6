35b538c3112f3f40aebd2086d18524d32314b739
LeeJJ07 <jkj9513@naver.com>
2023-10-30 17:35:59 +0900

    feat : 실행 결과 메세지 출력 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -8,6 +8,10 @@ public class Output {
 	public final String RESULT_MESSAGE = "실행 결과";
 	public final String WINNER_MESSAGE = "최종 우승자";
 	
+	public void resultMessage() {
+		System.out.println(RESULT_MESSAGE);
+	}
+	
 	public void roundResult(List<Car> cars) {
 		for(Car car : cars) {
 			System.out.printf("%s : %s\n", car.getName(), car.getForwardCount());
