a9f05a292c5e486cabc7ecce7be8a52ae22e92e0
bandall <jsm5315@naver.com>
2023-10-30 20:59:08 +0900

    feat: 사용자 입력 및 검증 구현
    InputView와 UserInputValidator 구현
    InputView가 사용자의 입력을 받고 UserInputValidator로 검증한 뒤 반환하도록 구현

---
+++ src/main/java/racingcar/view/input/validator/UserInputValidator.java
@@ -0,0 +1,43 @@
+package racingcar.view.input.validator;
+
+import java.util.List;
+
+public class UserInputValidator {
+
+    public Integer validateRoundCount(String rawInput) {
+        int roundCountInt;
+
+        try {
+            roundCountInt = Integer.parseInt(rawInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+
+        validateNumberUnderZero(roundCountInt);
+
+        return roundCountInt;
+    }
+
+    public void validateCarNames(List<String> carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
+        }
+
+        for (String carName : carNames) {
+            validateNameLength(carName);
+        }
+    }
+
+    private void validateNumberUnderZero(int roundCount) {
+        if (roundCount < 0) {
+            throw new IllegalArgumentException("이동 횟수는 0회 이상입니다.");
+        }
+    }
+
+    private void validateNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 최대 5글자입니다.");
+        }
+    }
+
+}
