d43a982ec07fac31fc8867e1a6abb73fee903a1a
minsub <chrkb1569@naver.com>
2023-10-31 01:23:13 +0900

    feat : 자동차의 정보를 저장하기 위한 Car 클래스 생성

---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,44 @@
+package racingcar.entity;
+
+// 게임에서 사용하는 자동차 정보를 저장하기 위한 클래스
+public class Car {
+    private String carName; // 자동차 이름
+    private int location; // 자동차의 위치
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.location = 0;
+    }
+
+    private Car() {
+    }
+
+    public String getCarName() {
+        return this.carName;
+    }
+
+    public int getLocation() {
+        return this.location;
+    }
+
+    // 특정 조건에 부합하는 경우 전진, 그렇지 않을 경우에는 현재 위치 유지
+    public void moveForward(int resultCount) {
+        if(resultCount < 4) {
+            return;
+        }
+        this.location++;
+    }
+
+    private String convertLocation() {
+        if(this.location == 0) {
+            return "";
+        }
+
+        return "-".repeat(location); // 자동차의 위치에 따른 결과 반환
+    }
+
+    @Override
+    public String toString() {
+        return this.carName + " : " + convertLocation();
+    }
+}
