506822f22d933ac3cb126e272cd3c5d23ad0e26f
whxogus215 <whxogus215@naver.com>
2023-10-30 17:39:13 +0900

    refactor: Console.readLine() private 메서드로 추출

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -6,11 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import racingcar.model.Car;
 
+/**
+ * View는 입력과 출력과 관련된 로직만 있어야 한다.
+ * 입력 값을 검증하는 것은 있어선 안된다. - 입력받은 자동차 이름을 저장하는 일급 컬렉션으로 생성 후 해당 클래스에서 검증로직 수행하도록 변경
+ * Console.readLine() -> private 메서드로 추출하기
+ */
 public class RacingCarView {
 
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String cars = Console.readLine();
+        String cars = inputStringLine();
         String[] splitInputCars = cars.split(",");
 
         carNameValidate(splitInputCars);
@@ -20,7 +25,7 @@ public class RacingCarView {
 
     public static int inputGameCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        String count = Console.readLine();
+        String count = inputStringLine();
 
         gameCountValidate(count);
 
@@ -48,5 +53,9 @@ public class RacingCarView {
                 .toList();
 
         System.out.println("최종 우승자 : " + String.join(", ",winners));
+    }
+
+    private static String inputStringLine() {
+        return Console.readLine();
     }
 }
