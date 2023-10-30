546ed34d11c860882b39bc7aac90d20bdb2c46ee
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-30 16:48:08 +0900

    feat: 우승자 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,6 +9,8 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String ROUND_RESULT = "%s : %s";
     private static final String DISTANCE_RESULT = "-";
+    private static final String WINNER = "최종 우승자 : ";
+    private static final String DELIMITER = ",";
 
     public static void printResultMessage(){
         System.out.println(RESULT_MESSAGE);
@@ -20,5 +22,12 @@ public class OutputView {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static void printWinner(List<String> winnerList){
+        System.out.print(WINNER);
+
+        String winnerResult = String.join(DELIMITER, winnerList);
+        System.out.println(winnerResult);
     }
 }
