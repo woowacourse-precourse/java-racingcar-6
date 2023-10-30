9f4a489784a95612a7486b2220447130b1355b35
gmelon <hsh1769@naver.com>
2023-10-30 13:41:51 +0900

    feat: CarName에 자신이 가진 값을 반환하는 메서드 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class CarName {
 
     private static final int MAXIMUM_LENGTH = 5;
@@ -20,6 +22,27 @@ public class CarName {
         if (valueLength > MAXIMUM_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다. 주어진 이름은 " + valueLength + "글자 입니다.");
         }
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(value, carName.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
 }
