fc4e26e5625f50afd985ae9639c4e640e8e023ea
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-30 14:02:00 +0900

    feat : 기본 출력문 출력 기능 추가

---
+++ src/main/java/racingcar/OutputManager.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class OutputManager {
+
+    public static void promptForCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void promptForTrials() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void displayRaceHeader() {
+        System.out.println("\n실행 결과");
+    }
+
+
+}
