9635ff0af4670aa0458c519489737490beef005a
inpink <inpink@kakao.com>
2023-10-30 19:44:39 +0900

    test: 저장소에 새로운 Racing 저장하는 기능 단위 테스트 코드 작성

---
+++ src/test/java/racingcar/repository/MemoryRacingRepositoryTest.java
@@ -0,0 +1,43 @@
+package racingcar.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.configuration.AppConfig;
+import racingcar.configuration.Config;
+import racingcar.domain.Car;
+import racingcar.domain.Participations;
+import racingcar.domain.Racing;
+import racingcar.domain.Winners;
+
+public class MemoryRacingRepositoryTest {
+
+    Config config = AppConfig.getInstance();
+    private RacingRepository racingRepository = config.racingRepository();
+
+    @BeforeEach
+    public void setUp() {
+        racingRepository.deleteAll();
+    }
+
+    @Test
+    public void 새로운_Racing저장() {
+        // given
+        Participations participations = new Participations();
+        int tryCount = 5;
+        Winners winners = new Winners();
+        Racing racing = new Racing(participations, tryCount, winners);
+
+        // when
+        final Racing result = racingRepository.save(racing);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getParticipations()).isSameAs(participations);
+        assertThat(result.getTryCount()).isEqualTo(tryCount);
+        assertThat(result.getWinners()).isSameAs(winners);
+    }
+
+
+}
