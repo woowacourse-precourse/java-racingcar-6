a79770cc2292c3e434e812aae389bb8f43843011
belljun3395 <195850@jnu.ac.kr>
2023-10-30 12:12:31 +0900

    refactor(domain/system): CarPositionKey를 키로 사용하여 저장관리 하도록 수정, findAll 반환값 수정 및 TreeMap을 구현채로 사용하여 정렬된 상태로 관리

--- src/main/java/racingcar/domain/system/manager/position/InMemoryCarPositionManager.java
+++ src/main/java/racingcar/domain/system/manager/position/InMemoryCarPositionManager.java
@@ -1,47 +1,53 @@
 package racingcar.domain.system.manager.position;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import racingcar.domain.core.car.CarName;
 import racingcar.domain.system.manager.car.SavedCar;
 import racingcar.exceptions.runtime.NotFoundSourceException;
 
 public class InMemoryCarPositionManager implements CarPositionManager {
 
-    private static final Comparator<? super CarName> keyComparator =
-        Comparator.comparing(CarName::getName);
-    private final Map<CarName, SavedCarPosition> positions = new TreeMap<>(keyComparator);
+    /**
+     * Carkey를 기준으로 오름차순 정렬
+     */
+    private final Map<CarPositionKey, SavedCarPosition> positions = new TreeMap<>();
 
     public SavedCarPosition save(SavedCar car, Long position) {
-        positions.put(car.getKey(), new SavedCarPosition(car, position));
-        return positions.get(car.getKey());
+        CarPositionKey key = CarPositionKey.of(car.getKey().getKey());
+        positions.put(key, new SavedCarPosition(car, position, key));
+        return positions.get(key);
     }
 
     public List<SavedCarPosition> saveAll(Map<SavedCar, Long> carPositions) {
         Set<Entry<SavedCar, Long>> sources = carPositions.entrySet();
-        return sources.stream()
-            .map(source -> save(source.getKey(), source.getValue()))
-            .collect(Collectors.toList());
+        return sources.stream().map(saveFunction())
+            .sorted(Comparator.comparing(SavedCarPosition::getKey)).collect(Collectors.toList());
+    }
+
+    private Function<Entry<SavedCar, Long>, SavedCarPosition> saveFunction() {
+        return source -> {
+            SavedCar car = source.getKey();
+            Long position = source.getValue();
+            return save(car, position);
+        };
     }
 
     public SavedCarPosition findByCar(SavedCar car) {
-        SavedCarPosition position = positions.get(car.getKey());
-        if (Objects.isNull(position)) {
-            throw new NotFoundSourceException();
-        }
-        return position;
+        return positions.values().stream().filter(position -> position.getSavedCar().equals(car))
+            .findAny().orElseThrow(NotFoundSourceException::new);
     }
 
-    public Map<CarName, SavedCarPosition> findAll(List<SavedCar> cars) {
-        Map<CarName, SavedCarPosition> results = new TreeMap<>(keyComparator);
+    public List<SavedCarPosition> findAll(List<SavedCar> cars) {
+        List<SavedCarPosition> results = new ArrayList<>();
         for (SavedCar car : cars) {
-            results.put(car.getKey(), findByCar(car));
+            results.add(findByCar(car));
         }
         return results;
     }
