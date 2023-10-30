af06a4d7aaf35d9be3403a6d33dacd05445723c9
hunko <rhwlgns4386@naver.com>
2023-10-30 14:31:42 +0900

    test(RacingGameTest) : 게임 종료 테스트 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -3,21 +3,39 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
 
-    @Test
-    public void 레이싱을_한다() {
+    public static final int TEST_REPEAT = 5;
+    private RacingGame racingGame;
+
+    @BeforeEach
+    public void init() {
         ArrayList<Car> carList = new ArrayList<>();
         carList.add(new Car(new TestNumberGenerator(5)));
         carList.add(new Car(new TestNumberGenerator(3)));
         carList.add(new Car(new TestNumberGenerator(6)));
-        RacingGame gameService = new RacingGame(carList);
+        racingGame = new RacingGame(carList, TEST_REPEAT);
+    }
 
-        gameService.doRace();
+    @Test
+    public void 레이싱을_한다() {
 
-        assertThat(gameService.getStatus()).filteredOn(car -> car.getLocation() != 0).hasSize(2);
+        racingGame.doRace();
+
+        assertThat(racingGame.getStatus()).filteredOn(car -> car.getLocation() != 0).hasSize(2);
+    }
+
+    @Test
+    public void 게임이_끝나면_True를_반환() {
+
+        for (int i = 0; i < TEST_REPEAT; i++) {
+            racingGame.doRace();
+        }
+
+        assertThat(racingGame.isEnd()).isTrue();
     }
 
 }
