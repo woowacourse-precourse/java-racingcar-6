d55d35d771b6e986caf46184608fd9783d4c20f6
LeeJJ07 <jkj9513@naver.com>
2023-10-30 15:06:43 +0900

    Feat : Car model 추가
    자동차의 이름과 전진한 거리를 알 수 있도록 class 생성

---
+++ src/main/java/racingcar/view/Car.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class Car {
+	private String name;
+	private int ForwardCount;
+	
+	public Car(String nm) {
+		this.name = nm;
+		this.ForwardCount = 0;
+	}
+	
+	public int getForwardCount() {
+		return this.ForwardCount;
+	}
+}
