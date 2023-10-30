1c86b4c9d5f134ee35b62865e6ea1f32ab75239b
jinseok <qkrwlstjr0131@naver.com>
2023-10-30 20:01:23 +0900

    [feat]: 유저들의 자동차 이름 설정기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.sql.SQLOutput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        UserCar userCar = new UserCar();
+        userCar.inputUserCar();
+
+        System.out.println("시도할 횟수는 몇번인가요?");
     }
 }
---
+++ src/main/java/racingcar/UserCar.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserCar {
+    private Map<String, Integer> car;
+    public UserCar(){
+    }
+
+    public void inputUserCar(){
+        Map<String, Integer> car = new HashMap<>();
+
+        String str = Console.readLine();
+        String[] arrStr = str.split(",");
+
+        for(int i=0; i<arrStr.length; i++){
+            car.put(arrStr[i],0);
+        }
+        this.car = car;
+    }
+}
+
