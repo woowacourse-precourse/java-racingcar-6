c700ed0572ce4f1a61d53644e6a020b2425f8d51
KDT live <dnjsxo0616@gmail.com>
2023-10-30 23:18:48 +0900

    feat(Controller): 레이스 컨트롤러 및 게임 실행 추가
    - 레이스 컨트롤러 `Controller` 클래스 추가
    - 레이스 게임 실행 및 관련 로직 추가
    - 사용자 입력 및 게임 진행, 결과 출력을 담당하는 `raceStart` 메서드 추가

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import java.util.List;
+
+public class Controller {
+    private static final String resultPrint = "실행 결과";
+    public void raceStart(){
+        User user = new User();
+        user.carNameSplit(InputMessage.carName());
+        user.carNameVaildation(user.carNames);
+        int gameNumber = Integer.parseInt(InputMessage.gameNumber());
+        Racing racing = new Racing();
+        racing.createRace(user.carNames);
+        System.out.println(resultPrint);
+
+        for(int i=0; i<gameNumber; i++){
+            List<Integer> RandomNumberList = racing.raceRandomNumberList(user.carNames);
+            racing.forwardValidation(RandomNumberList);
+            OutputMessage.racePrint(racing.raceSituation);
+            System.out.println();
+        }
+
+        racing.forwardCount(racing.raceSituation);
+        racing.findMaxIndex(racing.forwardCountNumbers);
+        OutputMessage outputMessage = new OutputMessage(racing.maxindexs, user.carNames);
+        outputMessage.winnerPrint();
+    }
+}
