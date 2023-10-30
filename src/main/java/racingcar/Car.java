57ad61fbbdd5773de43b991458462b315005900a
silverwest8 <silverwest8@gmail.com>
2023-10-30 20:25:33 +0900

    feat: implements Comparable<Car> for sorting

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,16 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
+public class Car implements Comparable<Car>{
 
-public class Car {
-    String Name;
+    public String name;
+    public Integer distance;
+    @Override
+    public int compareTo(Car o) {
+        return o.distance - distance;
+    }
+
+    public Car(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
 }
