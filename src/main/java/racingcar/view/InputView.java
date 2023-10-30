016ee9e93f3028e7b2dcf51b1f4e47deb9a20dbb
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-30 18:36:27 +0900

    feat : 시스템 출력 값 생성 게임을 시작할 때 시스템 출력 값과, 사용자가 입력할 수 있는 입력값을 받도록 합니다.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class InputView {
+
+    private static String input = Console.readLine();
+
+    public static final int MAX_NAME_LENGTH = 5;
+
+    public String startGame(){
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        return input;
+    }
+
+    public int startGameCount(){
+
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        return Integer.parseInt(input);
+
+    }
+}
