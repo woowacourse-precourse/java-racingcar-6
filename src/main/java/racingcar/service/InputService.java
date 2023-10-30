12c98f84398b0ba47345650b387d93043f9e915b
minsub <chrkb1569@naver.com>
2023-10-31 02:12:06 +0900

    style : 오류 메시지 변경

--- src/main/java/racingcar/service/InputService.java
+++ src/main/java/racingcar/service/InputService.java
@@ -1,9 +1,11 @@
 package racingcar.service;
 
+import racingcar.exception.GameExceptionMessage;
 import racingcar.exception.NotValidInputException;
 
 import java.util.regex.Pattern;
 
+import static racingcar.exception.GameExceptionMessage.*;
 import static racingcar.util.GameCondition.*;
 
 // 사용자가 입력한 값의 유효성을 판단하는 클래스
@@ -11,12 +13,12 @@ public class InputService {
     private final static String REGEXP_PATTERN_NUMBER = "^[0-9]*$"; // 숫자만 입력 가능
 
     public void checkCarNameValidation(String carName) {
-        checkBlankValidation(carName, "자동차의 이름은 공백이 될 수 없습니다."); // 공백 확인
+        checkBlankValidation(carName, CAR_NAME_CAN_NOT_BE_BLANK.getMessage()); // 공백 확인
         checkLengthValidation(carName); // 자동차의 이름 길이 제약 조건 확인
     }
 
     public void checkCompetitionNumberValidation(String competition) {
-        checkBlankValidation(competition, "사용자의 입력은 공백이 될 수 없습니다."); // 공백 확인
+        checkBlankValidation(competition, USER_INPUT_CAN_NOT_BE_BLANK.getMessage()); // 공백 확인
         checkNumberPatternValidation(competition); // 입력한 횟수가 숫자인지 확인
     }
 
@@ -29,11 +31,11 @@ public class InputService {
         int length = carName.length();
 
         if(length > CONDITION_INPUT_MAX_LENGTH.getCondition() || length < CONDITION_INPUT_MIN_LENGTH.getCondition())
-            throw new NotValidInputException("자동차의 이름은 최소 1자 이상, 5자 이하이어야 합니다.");
+            throw new NotValidInputException(CAR_NAME_LENGTH.getMessage());
     }
 
     private void checkNumberPatternValidation(String competition) {
         if(!Pattern.matches(REGEXP_PATTERN_NUMBER, competition))
-            throw new NotValidInputException("이동 횟수는 숫자만 입력할 수 있습니다.");
+            throw new NotValidInputException(FREQUENCY_ONLY_NUMBER.getMessage());
     }
 }
