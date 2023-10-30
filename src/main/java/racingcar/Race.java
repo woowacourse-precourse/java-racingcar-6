8e96027c8652348b930dd8eae8be87980cb12d90
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-30 23:01:58 +0900

    FEAT: 게임 플레이 과정이 담긴 Race클래스에 실행결과 출력 기능 추가 - RacingInfo 클래스 객체를 생성하여 차량이름이 담긴 names 배열과 횟수를 setter로 입력받고 getter로 받는다 - 실행 결과를 표현하기 위해 results 배열을 만들어 "이름 : " 을 저장 --> 여기에 차가 전진할 때마다 "-"를 붙혀 출력할 것임. - names와 results를 getResult 함수에 넣어준다.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -3,8 +3,19 @@ package racingcar;
 public class Race {
     public void race(){
         RacingInfo racingInfo = new RacingInfo();
+        PlayResult playResult = new PlayResult();
+
         racingInfo.setNamesAndNum();
-        racingInfo.getNames();
-        racingInfo.getNum();
+        int num = racingInfo.getNum();
+        String[] names = racingInfo.getNames();
+
+        String[] results = new String[names.length];
+        for (int i = 0; i < names.length; i++) {
+            results[i] = names[i] + " : ";
+        }
+
+        for (int i = 0; i < num; i++) {
+            playResult.getResult(racingInfo.getNames(), results);
+        }
     }
 }
