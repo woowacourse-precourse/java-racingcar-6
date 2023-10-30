9ae30c3990035961c502a628a7e1307df9c6db0f
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 03:16:44 +0900

    feat: 이름, 횟수 입력을 받기 위한 View 구현

---
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class RacingCarView {
+
+    public void carNameListInputPrint(){
+        System.out.println("경주할 자동차 이름을 입력하시오");
+    }
+
+    //시도할 회수는 몇회인가요? -service, print - view
+    public void readCountPrint(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+
+}
