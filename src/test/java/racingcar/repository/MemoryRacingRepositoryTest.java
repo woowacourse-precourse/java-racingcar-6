50e76bb99cda519e4d08ce7b67f7ae8a37c89121
inpink <inpink@kakao.com>
2023-10-30 22:17:26 +0900

    test: Racing 저장소에 우승자 정보 저장

--- src/test/java/racingcar/repository/MemoryRacingRepositoryTest.java
+++ src/test/java/racingcar/repository/MemoryRacingRepositoryTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.configuration.AppConfig;
 import racingcar.configuration.Config;
-import racingcar.domain.Car;
 import racingcar.domain.Participations;
 import racingcar.domain.Racing;
 import racingcar.domain.Winners;
+
 
 public class MemoryRacingRepositoryTest {
 
     Config config = AppConfig.getInstance();
     private RacingRepository racingRepository = config.racingRepository();
+    Participations participations = new Participations();
+    int tryCount = 5;
+    Winners winners = new Winners();
 
     @BeforeEach
     public void setUp() {
@@ -24,8 +27,6 @@ public class MemoryRacingRepositoryTest {
     @Test
     public void 새로운_Racing저장() {
         // given
-        Participations participations = new Participations();
-        int tryCount = 5;
         Racing racing = Racing.create(participations, tryCount);
 
         // when
@@ -37,6 +38,20 @@ public class MemoryRacingRepositoryTest {
         assertThat(result.getTryCount()).isEqualTo(tryCount);
     }
 
+    @Test
+    public void Winners_업데이트() {
+        // given
+        Racing racing = Racing.create(participations, tryCount);
+        racingRepository.save(racing);
+        Long id = racing.getId();
+
+        // when
+        final Racing result = (Racing) racingRepository.updateWinners(
+                id, winners);
+
+        // then
+        assertThat(result.getWinners()).isSameAs(winners);
+    }
 
 
 }
