6f50569fa7b687d3675202ce31f3b11e6f29d095
minsub <chrkb1569@naver.com>
2023-10-31 01:06:22 +0900

    feat : 사용자로부터 입력 받은 값을 저장하기 위한 레코드 생성

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,18 +1,40 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.dto.UserInput;
 
 // 사용자로부터 값을 입력받고 결과를 출력하기 위한 클래스
 public class GameController {
     public void startGame() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNameArr = Console.readLine().split(",");
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        String frequency = Console.readLine();
-
-        // checkValidation
+        UserInput userInput = readValue();
 
         // implement Logic
     }
+
+    private UserInput readValue() {
+        String[] carNameArr = readCarNameArr();
+        int frequency = readFrequency();
+
+        return new UserInput(carNameArr, frequency);
+    }
+
+    private String[] readCarNameArr() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNameArr = Console.readLine().split(",");
+
+        // checkValidation
+
+        return carNameArr;
+    }
+
+    private int readFrequency() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String frequency = Console.readLine();
+        System.out.println();
+
+        // checkValidation
+
+        return Integer.parseInt(frequency);
+    }
+
 }
