5def102878d9a07fbda0c5e191da51bc2bdc02c1
Knight <kangrack333@gmail.com>
2023-10-30 12:40:58 +0900

    refactor : Exception.java 클래스 메서드명 수정

--- src/main/java/view/Exception.java
+++ src/main/java/view/Exception.java
@@ -4,7 +4,7 @@ public class Exception {
 
     private static final String NAME_WARNING = "자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 1자 이상, 5자 이하만 가능합니다.";
 
-    public static void nameVerification(String[] carNames) {
+    public static void checkName(String[] carNames) {
 
         for (String carName : carNames) {
             checkLength(carName);
