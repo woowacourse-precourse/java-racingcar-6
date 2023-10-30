05fc451642ea8444dfdb4e6c735a84bbea6a1dd6
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-30 14:47:34 +0900

    FEAT: 시도 횟수 입력 추가 - 시도 횟수를 입력 기능 추가 - setNameAndNum 메소드로 이름과 횟수 모두 입력 받음 - 시도 횟수의 getter 생성

--- src/main/java/racingcar/RacingInfo.java
+++ src/main/java/racingcar/RacingInfo.java
@@ -3,12 +3,21 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 public class RacingInfo {
     private String[] names;
+    private int num;
 
     public String[] getNames() {
         return names;
     }
 
-    public void setNames() {
+    public void setNamesAndNum() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         names = Console.readLine().split(",");
+        System.out.print("시도할 회수는 몇회인가요?");
+        num = Integer.parseInt(Console.readLine());
+        System.out.println();
+    }
+
+    public int getNum() {
+        return num;
     }
 }
