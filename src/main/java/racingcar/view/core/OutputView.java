6cc74035b88322cf7013491862214a8b723a921b
JaehongDev <println@kakao.com>
2023-10-30 22:25:11 +0900

    feat: 우승자 조회 컴포넌트 UI 기능 구현

--- src/main/java/racingcar/view/core/OutputView.java
+++ src/main/java/racingcar/view/core/OutputView.java
@@ -5,13 +5,21 @@ import java.util.stream.Collectors;
 import racingcar.view.part.RaceGameComponent.RaceGameResult.TrackPerResult;
 
 public final class OutputView extends ConsoleWriter {
-    public void printTrackPerResult(List<TrackPerResult> trackPerResults) {
+    public void printRaceStartMessage() {
+        this.println("실행 결과");
+    }
 
+    public void printTrackPerResult(List<TrackPerResult> trackPerResults) {
         this.println(trackPerResults.stream()
                 .map((trackPerResult -> String.format("%s : %s", trackPerResult.name(), "-".repeat(trackPerResult.distance()))))
                 .collect(Collectors.joining("\n"))
         );
         this.newLine();
 
+    }
+
+    public void printWinner(List<String> winners) {
+        this.print("최종 우승자 : ");
+        this.print(String.join(",", winners));
     }
 }
