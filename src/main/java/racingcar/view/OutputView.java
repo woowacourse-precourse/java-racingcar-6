6a507161a02ea52f8cf81971a83c4339edd39c97
GS_song <20003204@sju.ac.kr>
2023-10-30 16:33:58 +0900

    feat: 출력을 관리하는 OutputView 생성
    printStartMessage : 게임이 시작되면 나타나는 메시지 출력
    printCarsPosition : 게임 실행 중에 나타나는 실행 결과 출력
    printWinner : 최종 게임의 승자 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,39 @@
+package racingcar.view;
+
+import static racingcar.constant.Constant.END_WINNER_MESSAGE;
+import static racingcar.constant.Constant.MOVE_CHAR;
+import static racingcar.constant.Constant.SEPARATOR_CHAR;
+import static racingcar.constant.Constant.START_GAME_MESSAGE;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import racingcar.model.Car;
+import racingcar.model.RacingCars;
+
+public final class OutputView {
+
+    public static void printStartMessage() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+
+    public static void printCarsPosition(RacingCars racingCars) {
+        racingCars.getCars().forEach(car -> {
+            String moves = IntStream.range(0, car.getPosition())
+                    .mapToObj(i -> MOVE_CHAR)
+                    .collect(Collectors.joining());
+
+            System.out.println(car.getName() + " : " + moves);
+        });
+        System.out.println();
+    }
+
+    public static void printWinner(List<Car> winner) {
+        System.out.print(END_WINNER_MESSAGE);
+        String winnerNames = winner.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(SEPARATOR_CHAR));
+
+        System.out.print(winnerNames);
+    }
+}
