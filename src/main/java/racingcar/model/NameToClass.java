dd784fbee15a191979bfb58930ceacac0bbe478f
chaniii999 <psc0729@naver.com>
2023-10-30 22:01:56 +0900

    feat: car 객체리스트 구현

---
+++ src/main/java/racingcar/model/NameToClass.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+
+public class NameToClass {
+
+    private final ArrayList<Car> classList = new ArrayList<>();
+
+    public NameToClass(ArrayList<String> nameList) {
+        for (String name : nameList) {
+            Car car = new Car(name);
+            classList.add(car);
+        }
+
+    }
+
+    public ArrayList<Car> getClassList() {
+        return classList;
+    }
+
+}
