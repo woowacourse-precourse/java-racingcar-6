1845cc307315b5e822b08a706cb83b6aaeebc097
nayeon Lee <skdus4190@gmail.com>
2023-10-31 03:39:35 +0900

    Feat: 이름 요구 사항 확인 기능 추가
    InputCheck.java에 이름 입력 요구 사항 (쉼표 구분, 5자 이내, 중복 불가) 확인 후 List 반환 기능 추가

--- src/main/java/controller/GameStart.java
+++ src/main/java/controller/GameStart.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.List;
+import model.InputCheck;
 import view.Input;
 
 public class GameStart {
@@ -7,6 +9,8 @@ public class GameStart {
     public static void start(){
 
         String names = Input.name();
+
+        List<String> nameList = InputCheck.name(names);
 
     }
 }
---
+++ src/main/java/model/InputCheck.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputCheck {
+
+    private static final String SEPERATOR = ",";
+
+    public static List<String> name(String input){
+
+        List<String> nameList = Arrays.asList(input.split(SEPERATOR));
+
+        for(String name : nameList){
+            if(name.length() > 5){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        Set<String> nameSet = new HashSet<>(nameList);
+        if(nameSet.size()!= nameList.size()){
+            throw new IllegalArgumentException();
+        }
+
+        return nameList;
+
+    }
+
+}
