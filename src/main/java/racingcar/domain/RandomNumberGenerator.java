ab7da61a37843fa7e163e9927bec7513867f6a6f
Jiihyun <cwlgusc@gmail.com>
2023-10-30 23:06:14 +0900

    feat(RandomNumberGenerator): 0에서 9까지의 정수 중 랜덤으로 한 개의 정수 반환

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    }
+}
