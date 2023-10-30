a6799617fba0662f28a49673b492d9da594186cd
kssumin <201566@jnu.ac.kr>
2023-10-31 03:51:46 +0900

    refactor : 입력한 자동차 이름이 좌우 공백을 제외하고 1 ~ 5글자인지 검증

--- src/main/java/racingcar/support/DividedCarNamesValidator.java
+++ src/main/java/racingcar/support/DividedCarNamesValidator.java
@@ -1,7 +1,8 @@
 package racingcar.support;
 
 public class DividedCarNamesValidator {
-
+	private static final int MIN_NAME_LENGTH = 1;
+	private static final int MAX_NAME_LENGTH = 5;
 	private static final int MIN_DIVIDED_COUNT = 1;
 
 	private DividedCarNamesValidator() {
@@ -10,11 +11,19 @@ public class DividedCarNamesValidator {
 
 	public static void validate(String[] carsName) {
 		validateCount(carsName);
+		validateLength(carsName);
 	}
 
 	private static void validateCount(String[] carsName) {
 		if (carsName.length <= MIN_DIVIDED_COUNT) {
 			throw new IllegalArgumentException("자동차 이름은 2개 이상 입력해야합니다");
+		}
+	}
+	private static void validateLength(String[] carsName) {
+		for (String carName : carsName) {
+			if (carName.length() > MAX_NAME_LENGTH || carName.trim().length() < MIN_NAME_LENGTH) {
+				throw new IllegalArgumentException("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+			}
 		}
 	}
 }
--- src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
+++ src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar.support;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.assertj.core.api.Assertions;
@@ -11,11 +12,31 @@ class DividedCarNamesValidatorTest {
 
 	@DisplayName("자동차 이름은 2개 이상 입력해야 한다.")
 	@ParameterizedTest
-	@ValueSource(strings ="12345")
+	@ValueSource(strings = "12345")
 	void validate_divided_count(String input) {
 		// when & then
 		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessageContaining("자동차 이름은 2개 이상 입력해야합니다");
+	}
+
+	@DisplayName("자동차 이름은 좌우 공백을 제외하고 1 ~ 5 글자로 입력해야 한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"     , "})
+	void validate_car_empty(String input) {
+		// when & then
+		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+	}
+
+	@DisplayName("자동차 이름은 1 ~ 5 글자로 입력해야합니다")
+	@ParameterizedTest
+	@ValueSource(strings = {"12345, 1234567"})
+	void validate_car_length(String input) {
+		// when & then
+		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
 	}
 }
