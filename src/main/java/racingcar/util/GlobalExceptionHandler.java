9658fab1362fc34c0256c0d3f3dfe0613558d1aa
jhsseonn <hs125bt@gmail.com>
2023-10-30 23:05:42 +0900

    fix: 예외처리에서 else문 지양하도록 수정

--- src/main/java/racingcar/util/GlobalExceptionHandler.java
+++ src/main/java/racingcar/util/GlobalExceptionHandler.java
@@ -3,19 +3,17 @@ package racingcar.util;
 public class GlobalExceptionHandler {
 
     public boolean isValidCarArray(String carArray) {
-        if (carArray.contains(",")) {
-            return true;
-        } else {
+        if (!",".contains(carArray)) {
             throw new IllegalArgumentException();
         }
+        return true;
     }
 
     public boolean isValidCarName(String carName) {
-        if (carName.length()<=5) {
-            return true;
-        } else {
+        if (carName.length()>5) {
             throw new IllegalArgumentException();
         }
+        return true;
     }
 
     public boolean isValidRacingCount(String racingCount) {
