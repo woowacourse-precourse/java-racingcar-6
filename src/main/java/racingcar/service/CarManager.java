04087de8d6fbe2af56e0d4a882ea0b1c07b98841
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-31 01:55:51 +0900

    CarManager 필드값 구현

---
+++ src/main/java/racingcar/service/CarManager.java
@@ -0,0 +1,23 @@
+package racingcar.service;
+
+import racingcar.domain.Car;
+import racingcar.utils.RandomCalculator;
+
+
+import java.util.*;
+
+import static racingcar.utils.RandomCalculator.calRandom;
+
+public class CarManager {
+
+    private static List<String> carList;
+    private static HashMap<String, Car> carMap = new HashMap<>();
+
+    private static List<String> winnerList = new ArrayList<>();
+
+
+    private Integer max = 0;
+
+
+
+}
