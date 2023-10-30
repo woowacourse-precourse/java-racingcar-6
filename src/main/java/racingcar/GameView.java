6e8928bccad8983797566762750c6f9c23eb2a2d
dong_hy <kimadonghyon99@gmail.com>
2023-10-30 19:47:00 +0900

    refactor : 상수부분 변수로 리팩토링

--- src/main/java/racingcar/GameView.java
+++ src/main/java/racingcar/GameView.java
@@ -6,20 +6,19 @@ import java.util.List;
 
 public class GameView {
     public static void gameStart(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(Common.strGameStart);
     }
 
     public static void gameCount(){
-        System.out.println("시도할 횟수는 몇회인가요?");
+        System.out.println(Common.strAskTimes);
     }
 
     public static void gameCurrent(HashMap<String,Integer> cars){
         Iterator<String> keys = cars.keySet().iterator();
         while( keys.hasNext() ){
             String key = keys.next();
-//            System.out.println( String.format("키 -> %s, 값 -> %s", key, cars.get(key)) );
 
-            String str = key + " : " + "-".repeat(cars.get(key));
+            String str = key +Common.gameCurrentDivider + Common.gameCurrentProgress.repeat(cars.get(key));
             System.out.println(str);
         }
         System.out.println();
@@ -33,13 +32,7 @@ public class GameView {
             }
             sb.append(winner.get(i));
         }
-        String result = "최종 우승자 : " + sb;
+        String result = Common.resultWinner + sb;
         System.out.println(result);
-    }
-
-    //테스트용 view
-    public static void gameMonitoring(HashMap<String,Integer> cars,int count){
-        System.out.println("차량상태" + cars);
-        System.out.println("시도할횟수" + count);
     }
 }
