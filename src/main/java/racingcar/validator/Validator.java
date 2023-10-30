c8ec06b772e5f8d8d22b7c4d8a67177ffd18bbe2
jang010505 <jang010505@inu.ac.kr>
2023-10-31 02:53:15 +0900

    feat: validateCarNameInput 함수 코드 작성

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,10 +1,34 @@
 package racingcar.validator;
 
+import static racingcar.constant.MessageConstant.CAR_NAME_INPUT_COUNT_ERROR;
+import static racingcar.constant.MessageConstant.CAR_NAME_INPUT_DUPLICATE_ERROR;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
     public static List<String> validateCarNameInput(String input) {
+        validateCarNameInputLength(input);
 
-        return null;
+        List<String> carNames = List.of(input.split(","));
+
+        validateDuplicateCarName(carNames);
+
+        return carNames;
+    }
+
+    private static void validateCarNameInputLength(String input) {
+        if (input.length() == 0) {
+            throw new IllegalArgumentException(CAR_NAME_INPUT_COUNT_ERROR.getMessage());
+        }
+    }
+
+    private static void validateDuplicateCarName(List<String> carNames) {
+        Set<String> carNameSet = new HashSet<>(carNames);
+
+        if (carNames.size() != carNameSet.size()) {
+            throw new IllegalArgumentException(CAR_NAME_INPUT_DUPLICATE_ERROR.getMessage());
+        }
     }
 }
