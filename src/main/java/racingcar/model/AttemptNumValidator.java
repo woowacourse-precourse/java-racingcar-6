71fd05b2e266f0e7991566edda3ad3396f35cad1
JoonKyo <joonkyolee@gmail.com>
2023-10-30 21:48:57 +0900

    refactor(AttemptNumbValidator): 메서드를 static으로 수정

--- src/main/java/racingcar/model/AttemptNumValidator.java
+++ src/main/java/racingcar/model/AttemptNumValidator.java
@@ -4,7 +4,7 @@ public class AttemptNumValidator {
     private static final String INPUT_IS_NOT_NATURAL_NUMBER = "입력값이 자연수가 아닙니다.";
     private static final String ATTEMPT_NUMBER_REGEX = "^[1-9]+$";
 
-    public int validateNumber(String number){
+    public static int validateNumber(String number){
         if(!number.matches(ATTEMPT_NUMBER_REGEX)){
             throw new IllegalArgumentException(INPUT_IS_NOT_NATURAL_NUMBER);
         }
