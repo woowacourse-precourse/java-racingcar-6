f58a1ec717974c62bcd642dd8d63d07f1fe9c04d
taewan625 <taewan1997625@gmail.com>
2023-10-31 07:13:38 +0900

    Feat: Add "Application"
    기능 구현 class를 포함관계로 받아서 메서드로만 구성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Print print = new Print();
+        Input input = new Input();
+        RacingGame racingGame = new RacingGame();
+
+        print.carName();
+        Map<String, Integer> carNames = input.carNames(); // todo tdd
+        print.tryCount();
+        int tryCount = input.tryCount(); // todo tdd
+
+        print.executeInit();
+        for (int i = 0; i < tryCount; i++) {
+            // todo 게임 수행
+            racingGame.executeCondition(carNames);
+            print.executeProgress(carNames);
+        }
+        print.result(tryCount, carNames);
     }
 }
