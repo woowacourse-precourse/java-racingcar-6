868f6dc51123a4b52db99567971bb7b924cad8e6
ay-eonii <naayen@naver.com>
2023-10-31 02:43:06 +0900

    feat: `최종 우승자` 알림 문구 출력 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     private final static String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private final static String RESULT_GUIDE_MESSAGE = "\n" + "실행 결과";
+    private final static String WINNER_GUIDE_MESSAGE = "최종 우승자 : ";
 
     public void displayCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -21,6 +22,7 @@ public class OutputView {
         System.out.print(locationAnnouncement);
     }
 
-    public void displayWinner() {
+    public void displayWinner(String winner) {
+        System.out.print(WINNER_GUIDE_MESSAGE + winner);
     }
 }
