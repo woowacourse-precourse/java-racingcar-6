02e08dc81cba5fc696e5f9dd28a78f94389ff00e
minseo  jo <liging12@naver.com>
2023-10-30 14:38:56 +0900

    feat: View 에 대한 공통 검증 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,10 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String requestCarNameFromUser() {
-        return Console.readLine();
+        return validate(Console.readLine());
     }
 
     public String requestRacingRoundCountFromUser() {
-        return Console.readLine();
+        return validate(Console.readLine());
+    }
+
+    private String validate(String input) {
+        // String 에서 빈 값은 null 이 아니라 "" 빈 공백값으로 치환 되지만, 명시적으로 null 도 체크해준다.
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력은 필수 사항 입니다.");
+        }
+        return input;
     }
 }
