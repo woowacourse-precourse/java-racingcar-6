a25d5c41d6411691857455d3734b7955c07ff9e0
BonSik <rnqhstlr2297@gmail.com>
2023-10-31 00:54:05 +0900

    feat: 콘솔 출력기 클래스 구현

---
+++ src/main/java/racingcar/view/OutputDevice.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+public final class OutputDevice {
+    private final static String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final static String RESULT_MESSAGE = "실행 결과";
+
+    private OutputDevice() {
+    }
+
+    public static void printInputNamesMessage(){
+        System.out.println(INPUT_NAMES_MESSAGE);
+    }
+
+    public static void printInputAttemptCountMessage(){
+        System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
+    }
+
+    public static void printResultMessage(String resultMessage) {
+        System.out.println(RESULT_MESSAGE);
+        System.out.println(resultMessage);
+    }
+
+}
