f0d87568a2085fe4a073118cb87846c5f3bd14dd
nhlee98 <e5slnh9876@gmail.com>
2023-10-30 19:41:33 +0900

    refactor: 자동차 개수 검증 로직을 RacingCars로 이동
    RaceValidator를 없애고 검증 로직은 RacingCars로 이동한다.

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,16 +1,22 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class RaceValidator {
+public final class RacingCars {
     private static final int MIN_CAR_COUNT = 2;
     private static final String CAR_COUNT_EXCEPTION_MESSAGE =
             String.format("Error: 자동차의 개수는 %d대 이상이어야 합니다.", MIN_CAR_COUNT);
 
-    private RaceValidator() {}
+    private final List<Car> racingCars;
 
-    public static void validateCarCount(List<CarName> carNames) {
-        if (carNames.size() < MIN_CAR_COUNT) {
+    public RacingCars(List<Car> racingCars) {
+        validateCarCount(racingCars);
+        this.racingCars = new ArrayList<>(racingCars);
+    }
+
+    public static void validateCarCount(List<Car> cars) {
+        if (cars.size() < MIN_CAR_COUNT) {
             throw new IllegalArgumentException(CAR_COUNT_EXCEPTION_MESSAGE);
         }
     }
---
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class RacingCarsTest {
+    @ParameterizedTest
+    @MethodSource("provideCarsForException")
+    void 자동차의_개수가_2대_이상이_아니라면_예외를_발생시킨다(List<Car> cars) {
+        assertThatThrownBy(() -> RacingCars.validateCarCount(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error: 자동차의 개수는 2대 이상이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCarsForNotException")
+    void 자동차의_개수는_2대_이상이어야_한다(List<Car> cars) {
+        assertThatCode(() -> RacingCars.validateCarCount(cars))
+                .doesNotThrowAnyException();
+    }
+
+    static Stream<Arguments> provideCarsForException() {
+        DriveBehavior behavior = new RandomDriveBehavior(RandomDriveBehavior::generateRandomNumber);
+
+        return Stream.of(
+                Arguments.of(List.of(Car.from(behavior, "a"))),
+                Arguments.of(Collections.emptyList())
+        );
+    }
+
+    static Stream<Arguments> provideCarsForNotException() {
+        DriveBehavior behavior = new RandomDriveBehavior(RandomDriveBehavior::generateRandomNumber);
+
+        return Stream.of(
+                Arguments.of(List.of(Car.from(behavior, "a"), Car.from(behavior, "b"))),
+                Arguments.of(List.of(Car.from(behavior, "a"), Car.from(behavior, "b"), Car.from(behavior,"c")))
+        );
+    }
+}
