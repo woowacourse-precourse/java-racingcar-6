29187a6cea31511711eebe43eb066f90517edf44
lynjun97 <lynjun97@naver.com>
2023-10-30 16:46:24 +0900

    Feat: playGame 메서드 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,5 +1,18 @@
 package racingcar.controller;
 
+import racingcar.service.RacingCarService;
+
+import java.util.List;
+
 public class RacingCarController {
+
+    RacingCarService carService = new RacingCarService();
+
+    public void playGame() {
+
+        //경주 할 자동차 이름
+        List<String> carList = carService.InputCarName();
+
+    }
 
 }
