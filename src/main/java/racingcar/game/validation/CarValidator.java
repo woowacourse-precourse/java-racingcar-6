0775170743bc2ab56944d6dc87754739a36fbd73
황중섭 <jshwang9709@gmail.com>
2023-10-31 02:10:13 +0900

    fix(car): 자동차 이름의 양 끝 공백 유무 검증
    자동차 이름의 중간에 공백이 있는 것은 합리적이나, 이름의 첫글자 혹은
    마지막 글자가 공백인 경우는 합리적이지 못하다고 판단해 이와 같은 검증
    케이스를 추가했다.

--- src/main/java/racingcar/game/validation/CarValidator.java
+++ src/main/java/racingcar/game/validation/CarValidator.java
@@ -5,11 +5,18 @@ public class CarValidator implements Validator{
     @Override
     public void execute(String name) {
         validateNameLength(name);
+        validateSideBlank(name);
     }
 
     private void validateNameLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름이 5자 이하가 아닙니다.");
+        }
+    }
+
+    private void validateSideBlank(String name) {
+        if (name.startsWith(" ") || name.endsWith(" ")){
+            throw new IllegalArgumentException("자동차 이름의 양 끝에는 공백이 올 수 없습니다.");
         }
     }
 }
