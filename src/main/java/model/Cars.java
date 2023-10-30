745919a27dfebb537c28d895ee0ede4658473993
kimwanyoung <dhks2869@gmail.com>
2023-10-30 15:31:22 +0900

    feat : 가장 큰 movingCount를 갖고있는 Car객체 이름 반환 findMaxMovingCount() 구현

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
@@ -8,5 +9,24 @@ public class Cars {
 
     public Cars(List<Car> cars) {
         this.cars = cars;
+    }
+
+
+    public List<String> findMaxMovingCount() {
+        cars.sort(Car::compareTo);
+        List<String> maxMovingCounts = new ArrayList<>();
+
+        Car currentCar = cars.get(0);
+        maxMovingCounts.add(currentCar.toString());
+
+        for (int i = 1; i < cars.size(); i++) {
+            Car nextCar = cars.get(i);
+            if (!currentCar.isSameMovingCount(nextCar)) {
+                break;
+            }
+            maxMovingCounts.add(nextCar.toString());
+        }
+
+        return maxMovingCounts;
     }
 }
