2fad60c24ded64092c8fdedc36b7088f1c20cc0c
bin <gimgyubin1225@gmail.com>
2023-10-30 17:38:42 +0900

    Feat: run 메서드에서 우승자 출력 기능 호출

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -1,19 +1,23 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.view.Input;
 import racingcar.view.Output;
 
 public class MainController {
 
     private RacingController racingController = new RacingController();
-    public void run(){
+
+    public void run() {
         Output.printInputCarNameInstruction();
         List<String> carNames = Input.getCarNames();
 
         Output.printExecutionCountQuestion();
         int NumberOfExecutions = Input.getInteger();
 
-        racingController.race(carNames, NumberOfExecutions);
+        List<Car> winners = racingController.race(carNames, NumberOfExecutions);
+        Output.printWinners(winners);
+
     }
 }
