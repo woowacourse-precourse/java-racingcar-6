80aefcc99e233d97dffebe0528379285a13f411a
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 14:54:03 +0900

    refactor: 랜덤 숫자 범위 상수로 변경

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
 
+    private static final int MINIMUM_VALUE = 0;
+    private static final int MAXIMUM_VALUE = 9;
     private final int number;
 
     RandomNumber() {
-        this.number = Randoms.pickNumberInRange(0, 9);
+        this.number = Randoms.pickNumberInRange(MINIMUM_VALUE, MAXIMUM_VALUE);
     }
 
     RandomNumber(int number) {
