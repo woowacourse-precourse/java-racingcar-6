2872223f76e7fd325ec309c119e25652849cd353
hunko <rhwlgns4386@naver.com>
2023-10-30 19:36:37 +0900

    feat(OutputView) : printWinner 함수 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import racingcar.dto.StatusResponse;
 
 public class OutputView {
@@ -14,10 +15,14 @@ public class OutputView {
 
     public static void printStatus(StatusResponse response) {
         LinkedHashMap<String, Integer> status = response.getStatus();
-        for(String name:status.keySet()){
-            print(String.format("%s : %s",name,HYPHEN.repeat(status.get(name))));
+        for (String name : status.keySet()) {
+            print(String.format("%s : %s", name, HYPHEN.repeat(status.get(name))));
         }
         print("");
+    }
+
+    public static void printWinner(List<String> winner) {
+        print(String.format("%s : %s", "최종 우승자", String.join(",", winner)));
     }
 
     private static void print(String message) {
