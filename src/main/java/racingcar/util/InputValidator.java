c7632944df7158522dc7e5b84fcc14a91595264a
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 15:20:56 +0900

    feat(InputValidator) : 시행 횟수 입력의 유효성 검사 기능 추가

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,18 +1,23 @@
 package racingcar.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import racingcar.consts.ErrorMessages;
 import racingcar.consts.GameConstant;
 
 public class InputValidator {
+    private static final String DELIMITER = ",";
 
-    public void validateCarNames(List<String> carNames) {
+    public void validateCarNames(String playerInput) {
+        List<String> carNames = Arrays.stream(playerInput.split(DELIMITER)).toList();
         if (!isCarNameSizeOneBetweenFive(carNames)) {
             throwIllegalArgumentException(ErrorMessages.WRONG_CAR_NAME_LENGTH_MESSAGE.get());
         } else if (hasDuplicateCarName(carNames)) {
-            throwIllegalArgumentException(ErrorMessages.WRONG_CAR_TYPES_MESSAGE.get());
-        } else if (!isMoreThanTwoCarTypes(carNames)) {
             throwIllegalArgumentException(ErrorMessages.DUPLICATION_CAR_NAME_MESSAGE.get());
+        } else if (!isMoreThanTwoCarTypes(carNames)) {
+            throwIllegalArgumentException(ErrorMessages.WRONG_CAR_TYPES_MESSAGE.get());
         }
     }
 
@@ -28,6 +33,20 @@ public class InputValidator {
         return carNames.stream().allMatch(carName -> carName.length() >= GameConstant.MIN_LENGTH_CAR_NAME.get()
                 && carName.length() <= GameConstant.MAX_LENGTH_CAR_NAME.get());
     }
+
+    public void validateNumberOfAttempt(String playerInput) {
+        if (!isNumeric(playerInput)) {
+            throwIllegalArgumentException(ErrorMessages.WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
+        }
+    }
+
+    private boolean isNumeric(String playerInput) {
+        String regex = "^[1-9][0-9]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(playerInput);
+        return matcher.matches();
+    }
+
 
     private void throwIllegalArgumentException(String errorMessage) {
         throw new IllegalArgumentException(errorMessage);
