07044cf1c01f7089827cb0f65c8eadf4cab5cf7a
leorb159 <ldgsocial159@naver.com>
2023-10-31 05:20:19 +0900

    feat(Game): 자동차 객체 초기화

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -6,17 +6,23 @@ import java.util.ArrayList;
 public class Game {
     private Output myOutput;
     private Input myInput;
-
+    private ArrayList<Car> carList;
     public Game(){
         myOutput = new Output();
         myInput = new Input();
+        carList = new ArrayList<>();
     }
+
     public void run(){
         myOutput.printEnterNameOfCar();
         String input = Console.readLine();
         ArrayList<String> carNameList = myInput.getNameList(input);
-
+        for(int i=0; i<carNameList.size(); i++){
+            Car myCar = new Car(carNameList.get(i));
+            carList.add(myCar);
+        }
 
         myOutput.printEnterTryTime();
+
     } 
 }
