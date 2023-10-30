12644ce04ed6106edeaaccc9d646e0befe21c56e
leorb159 <ldgsocial159@naver.com>
2023-10-31 01:05:39 +0900

    feat(Output): 경주할 자동차 이름 입력하라는 문구 출력 구현

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Output {
+    private String enterNameOfCar = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public void printEnterNameOfCar(){
+        System.out.println(enterNameOfCar);
+    }
+
+
+}
