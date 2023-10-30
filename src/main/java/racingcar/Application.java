fddeb8d98ce6533eb7a7c5e69fa487bc3eb95f6f
jihun-24k <gi020477@gmail.com>
2023-10-30 11:34:15 +0900

    feat: 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] cars = input.split(",");
+
+        // Todo: 5자 예외처리
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        input = Console.readLine();
+        int count = Integer.parseInt(input);
+
+        new RacingGame(count);
     }
 }
---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class RacingGame {
+
+    private int count;
+    public RacingGame(int count) {
+        this.count = count;
+    }
+}
