9621a8427e3cfd414b0f4addf4779348965c1bc5
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-31 04:10:32 +0900

    test : 메인함수를 실행했을 때 사용자의 입력에 따른 예외발생 테스트 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -28,6 +29,51 @@ class ApplicationTest extends NsTest {
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("입력한 이름에 중복이 있는 경우 예외가 발생한다.")
+    @Test
+    void 이름에_대한_중복_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("입력한 이름에 빈 값이 있는 경우 예외가 발생한다.")
+    @Test
+    void 이름에_대한_빈값_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("시도 횟수에 대한 입력이 0인 경우 예외가 발생한다.")
+    @Test
+    void 시도_횟수_0에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pinut,pobi", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("시도 횟수에 대한 입력이 문자인 경우 예외가 발생한다.")
+    @Test
+    void 시도_횟수_문자에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pinut,pobi", "C"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("시도 횟수에 대한 입력이 빈 값인 경우 예외가 발생한다.")
+    @Test
+    void 시도_횟수_빈값에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pinut,pobi", "\n"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
