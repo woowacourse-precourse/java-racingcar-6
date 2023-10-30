0457b2bc998fdf66258d7e5b59f763c61f49fcc6
yuseok0215 <elwlahstmxjf@naver.com>
2023-10-30 22:17:53 +0900

    refactor: 이전 커밋에 대한 변경사항을 적용
    이전 커밋에 대한 변경사항 중 변경하지 않은 부분 수정
    informInputCarNames -> askCarNames
    informInputTryNumber -> askTryNumber
    
    inputCarNames, inputTryNumber 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,23 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String informInputCarNames() {
+    public String askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    public int informInputTryNumber() {
+    public int askTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         String stringNumber = Console.readLine();
         return Integer.parseInt(stringNumber);
-    }
-
-    public String inputCarNames() {
-        return Console.readLine();
-    }
-
-    public int inputTryNumber() {
-        String numberString = Console.readLine();
-        return Integer.parseInt(numberString);
     }
 }
