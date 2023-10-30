8a8eab4ccd57f27d5a41ccb4cd0f28054dffa64f
lee jihyeon <lejinhy@gmail.com>
2023-10-30 17:40:16 +0900

    test: 우승자 판단 메소드 테스트 완료

--- src/test/java/racingcar/util/CalculationTest.java
+++ src/test/java/racingcar/util/CalculationTest.java
@@ -3,7 +3,12 @@ package racingcar.util;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import racingcar.Entity.Car;
 import racingcar.constant.CarStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculationTest {
@@ -40,6 +45,49 @@ class CalculationTest {
         assertThat(result).isEqualTo(CarStatus.MOVE);
     }
 
+    @Test
+    void 우승자_판단_입력_하나인데_0번이동_한명우승() {
+        List<Car> testCars = new ArrayList<>();
+        testCars.add(new Car("one",0));
+        List<Car> result = calculation.judgeWhoIsWinner(testCars);
 
+        assertThat(result).containsAll(testCars);
+    }
+    @Test
+    void 우승자_판단_입력_전부_공동우승() {
+        List<Car> testCars = new ArrayList<>();
+        testCars.add(new Car("one",3));
+        testCars.add(new Car("two",3));
+        testCars.add(new Car("three",3));
+
+        List<Car> result = calculation.judgeWhoIsWinner(testCars);
+
+        assertThat(result).containsAll(testCars);
+    }
+    @Test
+    void 우승자_판단_입력_전부_0번이동이면_공동우승() {
+        List<Car> testCars = new ArrayList<>();
+        testCars.add(new Car("one",0));
+        testCars.add(new Car("two",0));
+        testCars.add(new Car("three",0));
+        List<Car> result = calculation.judgeWhoIsWinner(testCars);
+
+        assertThat(result).containsAll(testCars);
+    }
+    @Test
+    void 우승자_판단_입력_여러명_우승() {
+        List<Car> testCars = new ArrayList<>();
+        testCars.add(new Car("one",1));
+        testCars.add(new Car("two",3));
+        testCars.add(new Car("three",3));
+
+        List<Car> answerCars = new ArrayList<>();
+        testCars.add(new Car("two",3));
+        testCars.add(new Car("three",3));
+
+        List<Car> result = calculation.judgeWhoIsWinner(testCars);
+
+        assertThat(result).containsAll(answerCars);
+    }
 
 }
