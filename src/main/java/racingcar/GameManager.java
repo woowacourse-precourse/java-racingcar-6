b4d1dfc88c4319b959a5d075c37909b3d474d3cd
Jonghyeok Lee <endb1228@gmail.com>
2023-10-30 21:13:55 +0900

    GameManager 싱글톤 구현

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,4 +1,19 @@
 package racingcar;
 
 public class GameManager {
+
+    private static GameManager gameManager;
+
+    private GameManager() {}
+
+    public static GameManager getInstance() {
+        if (gameManager == null) {
+            gameManager = new GameManager();
+        }
+        return gameManager;
+    }
+
+    public void startGame() {
+
+    }
 }
