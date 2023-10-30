d475c74973f7376870269358703ac6c14eec50dc
xogns4909 <xogns4909@naver.com>
2023-10-30 19:14:10 +0900

    Refactor : Cars move 메소드 삭제

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,27 +1,19 @@
 package domain;
 
 import dto.RoundResultDto;
-import dto.WinnerDto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import util.generator.RandomNumberGenerator;
 
 public class Cars {
 
     private final List<Car> cars;
 
+
     public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
-    public void move(){
-        for(Car car: cars){
-            int randomNumber = RandomNumberGenerator.generateRandomNumber();
-            car.move(randomNumber);
-        }
-    }
 
     public List<RoundResultDto> getCarsInformation() {
         List<RoundResultDto> carInfos = new ArrayList<>();
