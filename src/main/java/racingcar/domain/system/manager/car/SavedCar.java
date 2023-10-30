e6ca5cbbf1aa74d2c0e38d05fe57bd838d11cc34
belljun3395 <195850@jnu.ac.kr>
2023-10-30 12:00:11 +0900

    feat(domain/system): 저장된 차의 이름 조회를 위한 getName 메서드 추가

--- src/main/java/racingcar/domain/system/manager/car/SavedCar.java
+++ src/main/java/racingcar/domain/system/manager/car/SavedCar.java
@@ -21,6 +21,10 @@ public class SavedCar {
         return car;
     }
 
+    public String getName() {
+        return car.getName();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
