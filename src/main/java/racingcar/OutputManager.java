48439ea3a9955b1ba5b362a5592f63ea8c13fb06
jjaegii <hn06038@gmail.com>
2023-10-30 22:36:26 +0900

    feat: OutputManager 클래스 내 printRaceResult 메소드 추가

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputManager {
+    public void printRaceResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
