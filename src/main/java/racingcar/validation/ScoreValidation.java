19000744a112da16c186597ba7eeb4043447e288
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 19:00:17 +0900

    refactor: modify variable name for fit purpose

--- src/main/java/racingcar/validation/ScoreValidation.java
+++ src/main/java/racingcar/validation/ScoreValidation.java
@@ -4,8 +4,8 @@ import racingcar.property.ErrorProperty;
 
 public class ScoreValidation extends InputValidation{
 
-    public static void verifyForScoreIsNumericValue(String racerName){
-        if (!racerName.equals("\\d")){
+    public static void verifyForScoreIsNumericValue(String targetScore){
+        if (!targetScore.equals("\\d")){
             throw new IllegalArgumentException(ErrorProperty.SCORE_VALUE_IS_NOT_NUMERIC);
         }
     }
