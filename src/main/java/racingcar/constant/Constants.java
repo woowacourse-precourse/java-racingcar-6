56226631c6d5653f47c857f2f89fdbcd305a93ae
tfer2442 <tfer2442@gmail.com>
2023-10-31 04:08:20 +0900

    feat(RadomManipulator): 매직 넘버 수정

--- src/main/java/racingcar/constant/Constants.java
+++ src/main/java/racingcar/constant/Constants.java
@@ -3,6 +3,11 @@ package racingcar.constant;
 public class Constants {
     public static final int DISTANCE = 1;
     public static final String SEPARATOR = ",";
+
     public static final int MIN_CAR_NAME_LEN = 1;
     public static final int MAX_CAR_NAME_LEN = 5;
+
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int CONDITION_RANDOM_VALUE = 4;
 }
--- src/main/java/racingcar/util/RandomManipulator.java
+++ src/main/java/racingcar/util/RandomManipulator.java
@@ -2,10 +2,12 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import static racingcar.constant.Constants.*;
+
 public class RandomManipulator {
     public static boolean isMove() {
-        int randNum = Randoms.pickNumberInRange(0, 9);
+        int randNum = Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
 
-        return randNum >= 4;
+        return randNum >= CONDITION_RANDOM_VALUE;
     }
 }
