6adc1105fe21b402d839c8bc7ec4da88b4ad3d04
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 08:14:11 +0900

    test : Car 테스트 4 이상일 때 직진 기능 테스트 코드 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,13 +6,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
     private Car car;
+    private static final int MOVE = 4;
     @Test
     void 수가_랜덤_중_4_이상_일_때_직진(){
-        int random = 4;
         Car car = new Car();
-        int result = car.goForward(random);
-        assertThat(result).isEqualTo(1);
-
+        car.goForward(MOVE);
+        assertThat(car.getForward()).isEqualTo(1);
     }
 
 
