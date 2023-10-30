692f7480eb61fa6565f5ae89d8ae92b5aa674447
PARK9898 <wjddjs21@naver.com>
2023-10-30 17:34:18 +0900

    "refactor: RandomNumberGenerator Class 삭제 후 메서드 추출"

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class Game {
+
+    private static final int START_POSITION = 0;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
+    private final List<Car> cars;
+
+    public Game(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+}
