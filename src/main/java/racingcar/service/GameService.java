b5f760d06f03563a5aa76954f4171c4f75d9ea18
Hoony0321 <eogns0321@gmail.com>
2023-10-30 17:43:10 +0900

    feat : 자동차 이름 입력 기능 구현
    * 쉼표(,) 기준으로 자동차 이름 분리하여 GameService 내의 cars 변수에 저장
    * 잘못된 입력이 들어올 경우 예외 처리

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -2,14 +2,24 @@ package racingcar.service;
 
 import racingcar.domain.Car;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import racingcar.view.InputView;
 
 public class GameService {
-    private List<Car> cars;
-    private Integer tryCount;
+    private List<Car> cars = new ArrayList<>();
+    private Integer tryCount = 0;
+    private static final Integer MAX_CAR_NAME_LENGTH = 5;
 
     public void initGame() {
-        // TODO: 게임을 초기화하는 메소드
+        // carNames를 입력받아 cars를 초기화
+        String rawInputStringCarNames = InputView.requestInputCarNames();
+        Set<String> carNames = convertInputToCarNameSet(rawInputStringCarNames);
+        for(String carName : carNames) {
+            cars.add(new Car(carName));
+        }
     }
 
     public void playGame() {
@@ -18,5 +28,25 @@ public class GameService {
 
     public void endGame() {
         // TODO: 게임을 종료하는 메소드
+    }
+
+    private Set<String> convertInputToCarNameSet(String rawInputString) {
+        Set<String> carNameSet = new HashSet<>();
+        String[] splitedInputString = rawInputString.split(",");
+
+        // 올바른 입력인지 검증
+        for(String carName : splitedInputString) {
+            if(carName.length() > MAX_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+
+            if(carNameSet.contains(carName)) {
+                throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+            }
+
+            carNameSet.add(carName);
+        }
+
+        return carNameSet;
     }
 }
