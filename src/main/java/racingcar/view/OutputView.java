847bcbe2ba0e2a84020c67f0e0ff2553cc54cd8a
pjm2476 <pjm2476@gmail.com>
2023-10-30 23:22:03 +0900

    feat: 시도 횟수 입력 요청 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,9 +3,14 @@ package racingcar.view;
 public class OutputView {
 
     private static final String ENTER_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ENTER_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static void enterCarNames() {
         System.out.println(ENTER_CAR_NAME_MESSAGE);
+    }
+
+    public static void enterAttempts(){
+        System.out.println(ENTER_ATTEMPTS_MESSAGE);
     }
 
 }
