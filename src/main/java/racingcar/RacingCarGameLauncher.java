44506230f15fb4b4fba36e3776a835fd28a645f3
alscjf1329 <alscjf1329@gmail.com>
2023-10-30 10:51:09 +0900

    feat: launch 메소드를 생성
    RacingCarGameLauncher의 launch 메소드를 통해 RacingCarGame을 실행하도록 작성했습니다.

---
+++ src/main/java/racingcar/RacingCarGameLauncher.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.dto.RacingCar;
+import racingcar.utils.RacingCarGame;
+import racingcar.utils.RacingCarGameInput;
+import racingcar.utils.RacingCarGameOutput;
+
+public class RacingCarGameLauncher {
+    private RacingCarGame racingCarGame;
+
+    public void launch() {
+        RacingCarGameOutput.printCarNameInputMessage(RacingCarGameOutput.CAR_NAME_INPUT_MESSAGE);
+        List<RacingCar> racingCarList = RacingCarGameInput.readCarNames();
+        RacingCarGameOutput.printMoveCountInputMessage(RacingCarGameOutput.MOVE_COUNT_INPUT_MESSAGE);
+        int moveCount = RacingCarGameInput.readMoveCount();
+        racingCarGame = new RacingCarGame(racingCarList, moveCount);
+        RacingCarGameOutput.printBeforeGameProgress(RacingCarGameOutput.BEFORE_GAME_MESSAGE);
+        racingCarGame.run();
+        RacingCarGameOutput.printWinnersMessage(racingCarGame.getWinners());
+    }
+}
