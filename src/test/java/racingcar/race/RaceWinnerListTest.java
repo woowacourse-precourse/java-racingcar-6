46a58e64e683463d9cfb4d4f76c3727f10221af4
1000zoo <cjswldn99@gmail.com>
2023-10-31 01:32:22 +0900

    test(RaceWinnerList): 수정된 생성자에 맞게 테스트 수정
    - RaceCars의 String을 받는 생성자가 사라졌기에 테스트코드 수정
    - Mockito를 이용하는 테스트로 수정

--- src/test/java/racingcar/race/RaceWinnerListTest.java
+++ src/test/java/racingcar/race/RaceWinnerListTest.java
@@ -1,34 +1,55 @@
 package racingcar.race;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.test.NsTest;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Iterator;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import racingcar.view.NamesInput;
 
-class RaceWinnerListTest extends NsTest {
+class RaceWinnerListTest {
+
+    private static MockedStatic<Randoms> random;
+    private static MockedStatic<Console> console;
+
+    @BeforeAll
+    public static void beforeAll() {
+        random = mockStatic(Randoms.class);
+        console = mockStatic(Console.class);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        random.close();
+        console.close();
+    }
 
     @Test
     void 우승자_명단() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni,jun,jiwoo");
-                    assertThat(output()).contains("woni", "jiwoo");
-                }, 2, 9, 3, 9
-        );
-    }
+        // given
+        random.when(() -> Randoms.pickNumberInRange(0, 9))
+                .thenReturn(2).thenReturn(9).thenReturn(3).thenReturn(9);
+        console.when(Console::readLine).thenReturn("pobi,woni,jun,jiwoo");
+        Iterator<String> answerIterator = List.of("woni", "jiwoo").iterator();
 
-    @Override
-    protected void runMain() {
-        RaceCars raceCars = new RaceCars(Console.readLine());
+        // when
+        RaceCars raceCars = new RaceCars(new NamesInput());
         raceCars.moveForwardEach();
-        RaceWinnerList winnerList = new RaceWinnerList(raceCars);
-        Iterator<RaceCar> iterator = winnerList.getIterator();
 
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+        // then
+        RaceWinnerList winnerList = new RaceWinnerList(raceCars);
+        Iterator<RaceCar> winnerIterator = winnerList.getIterator();
+
+        while (winnerIterator.hasNext()) {
+            RaceCar winnerName = winnerIterator.next();
+            assertThat(winnerName.toString()).isEqualTo(answerIterator.next());
         }
+
     }
 }
