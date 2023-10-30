b97da5b38ae5ed5365866fd9f494bc9cb3fcd525
anneu <b.imnuj@gmail.com>
2023-10-30 18:07:49 +0900

    refactor: 데이터 타입 변환 로직을 별도 클래스로 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,12 +1,11 @@
 package racingcar.controller;
 
 import racingcar.domain.RacingCars;
+import racingcar.utils.Converter;
 import racingcar.view.Input;
 import racingcar.view.Output;
 
 public class RacingController {
-
-    private static final String NUMBER_FORMAT_EXCEPTION = "숫자를 입력해야 합니다.";
 
     private final Output output = new Output();
     private final Input input = new Input();
@@ -17,7 +16,7 @@ public class RacingController {
         RacingCars racingCars = RacingCars.from(carNames);
 
         output.showCountInputMessage();
-        int count = toInteger(input.readCount());
+        int count = Converter.toInt(input.readCount());
 
         output.showResultMessage();
         for (int i = 0; i < count; i++) {
@@ -25,13 +24,5 @@ public class RacingController {
         }
 
         output.showWinners(racingCars.findWinners());
-    }
-
-    private int toInteger(String count) {
-        try {
-            return Integer.parseInt(count);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION);
-        }
     }
 }
