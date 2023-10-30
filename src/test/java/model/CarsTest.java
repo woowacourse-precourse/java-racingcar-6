ba4e1b4237c87e8a63e49c5caac4e759fe0c3351
WonyuChoi <phillip07290@naver.com>
2023-10-30 14:42:20 +0900

    refactor: 와일드 카드 사용하지 않도록 수정

--- src/test/java/model/CarsTest.java
+++ src/test/java/model/CarsTest.java
@@ -1,6 +1,6 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
