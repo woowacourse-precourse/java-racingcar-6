8bea359575f0d8aae50035f592248532c4464fa0
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 13:58:18 +0900

    feat(RacingGame) : 자동차 생성하는 기능 구현
    입력받은 자동차 이름으로 자동차를 만든다.

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,17 +1,21 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.validator.Validator;
 import racingcar.view.InputView;
 
 public class RacingGame {
     public void start() {
         List<String> racingCarNames = InputView.readRacingCarName();
+        List<Car> cars = new ArrayList<>();
 
         Validator.validateIsDuplicate(racingCarNames);
         for (String name : racingCarNames) {
             Validator.validateUnderFiveLength(name);
             Validator.validateIsBlank(name);
+            cars.add(new Car(name));
         }
 
         Long tryCount = InputView.readTryCount();
