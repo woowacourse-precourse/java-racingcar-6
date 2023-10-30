d40dedc2b579df58ee0e248dc86665d837f6a9dc
MingyeomKim <mingyum119@naver.com>
2023-10-30 17:26:42 +0900

    feat(InputView): 입력 단에서 비정상적인 구분자 사용을 검증
    쉼표가 비정상적인 형태로 입력받는 경우를 입력 단에서 예외처리한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,11 @@
 package racingcar.view;
 
+import static racingcar.global.constants.SymbolType.DELIMITER;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     public String readInput() {
         return Console.readLine();
     }
@@ -10,15 +13,43 @@ public class InputView {
     public String readCarNames() {
         String carNames = Console.readLine();
         validateCarNames(carNames);
+        return carNames;
     }
 
-    private void validateCarNames(String carNames) {
-        validateBlankInput(carNames);
+    private void validateCarNames(String names) {
+        validateBlankInput(names);
+        validateInvalidDelimiter(names);
     }
 
-    private void validateBlankInput(String carNames) {
-        if (carNames.isBlank()) {
+    private void validateBlankInput(String names) {
+        if (names.isBlank()) {
             throw new IllegalArgumentException("공백이 입력되었습니다.");
         }
+    }
+
+    private void validateInvalidDelimiter(String names) {
+        if (isEdgeDelimiter(names) || hasContinuousDelimiters(names)) {
+            throw new IllegalArgumentException("쉼표(,)를 올바르게 입력해주세요.");
+        }
+    }
+
+    private boolean hasContinuousDelimiters(String names) {
+        return names.contains(generateContinuousDelimiters());
+    }
+
+    private String generateContinuousDelimiters() {
+        return DELIMITER.getSymbol().repeat(2);
+    }
+
+    private boolean isEdgeDelimiter(String names) {
+        return startWithDelimiter(names) || endsWithDelimiter(names);
+    }
+
+    private boolean startWithDelimiter(String names) {
+        return names.startsWith(DELIMITER.getSymbol());
+    }
+
+    private boolean endsWithDelimiter(String names) {
+        return names.endsWith(DELIMITER.getSymbol());
     }
 }
