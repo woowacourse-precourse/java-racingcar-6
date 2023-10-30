0bc6b741a6d2e22d14c198b185ee9963ecad1de5
HyungHoKim <hkim1109@naver.com>
2023-10-31 00:38:20 +0900

    refactor(main): timesToTry 메서드 분할
    timesToTry 메서드에서 입력이 int 형식이 아니면 오류를 반환하는 부분을 별도의 메서드로 분할

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.ExceptionMessage.INPUT_TYPE_MISMATCH;
 
 public class InputView {
     private static final String CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -15,12 +16,14 @@ public class InputView {
 
     public static int timesToTry() {
         System.out.println(TIMES_TO_TRY_REQUEST);
-        int timesToTry;
-        try {
-            timesToTry = Integer.parseInt(readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_TYPE_MISMATCH);
+        String inputString = readLine();
+        checkInputIsInteger(inputString);
+        return Integer.parseInt(inputString);
+    }
+
+    private static void checkInputIsInteger(String inputString) {
+        if (!inputString.matches("-?\\d+")) {
+            throw new IllegalArgumentException(INPUT_TYPE_MISMATCH);
         }
-        return timesToTry;
     }
 }
