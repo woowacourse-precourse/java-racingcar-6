1b8707b2407b025d9697c0770c317bb47047b220
naayoung <ask5046a@gmail.com>
2023-10-31 00:49:48 +0900

    feat: Change the distance number to '-'

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import net.bytebuddy.agent.builder.AgentBuilder;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +10,7 @@ public class Application {
         InputValue inputTime  = new InputValue();
         //랜덤 거리
         RandomDistance randomDis = new RandomDistance();
+        List<Integer> userDis = new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] value = inputValue.getInputValue();
@@ -19,13 +18,15 @@ public class Application {
         Integer time = inputTime.getInputTime();
         System.out.println("실행 결과");
 
-
-        for(int i=0; i<value.length;i++){
-            String userDis = randomDis.getDistance(time);
-            System.out.println(value[i] + " :" + userDis);
-        }
+        for (int i=0; i<time; i++){
+            for(int j=0; j<value.length;j++){
+                userDis.add(j,randomDis.getDistance(j));
+                //길이 값을 문자로 변환
+                String dis = "-".repeat(userDis.get(j));
+                System.out.println(value[j] + " :" + dis);
+            }
         System.out.println(" ");
-
+        }
         System.out.println("최종 우승자 : ");
     }
 }
