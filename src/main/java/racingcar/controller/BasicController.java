d1ebce33e50f2f7b8e703a6ab8f9644c5b98845e
mikekks <thdalsrb79@naver.com>
2023-10-30 22:34:13 +0900

    [feat] 최종 우승자 출력 구현

--- src/main/java/racingcar/controller/BasicController.java
+++ src/main/java/racingcar/controller/BasicController.java
@@ -4,14 +4,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.response.ProcessMessage;
 
 public class BasicController {
     List<Car> carList;
     private final String BLANK = " : ";
     private final Integer START = 0;
+    private final Integer SINGLE_WINNER = 1;
 
-    public BasicController(String carInput) {
+    private Integer totalRound;
+    private Integer currentRound;
+
+    public BasicController(String carInput, Integer totalRound) {
         carList = new ArrayList<>();
+        this.totalRound = totalRound;
+        currentRound = 0;
         String[] splits = carInput.split(",");
         for(int i=0; i<splits.length; i++){
             carList.add(new Car(splits[i], START));
@@ -24,11 +31,24 @@ public class BasicController {
             Car currentCar = carList.get(i);
             currentCar.tryMove(randomValue);
         }
+        currentRound++;
 
         // 출력
         printRoundResult();
 
+        // 마지막 라운드 확인
+        if(lastRoundCheck()){
+            Integer maxMove = findMaxMove();
+            List<String> winnerList = findWinnerList(maxMove);
+            printFinalResult(winnerList);
+        }
+    }
 
+    private boolean lastRoundCheck(){
+        if(totalRound == currentRound){
+            return true;
+        }
+        return false;
     }
 
     private void printRoundResult(){
@@ -39,6 +59,43 @@ public class BasicController {
                 System.out.print("-");
             }
             System.out.println();
+        }
+        System.out.println();
+    }
+
+    private Integer findMaxMove(){
+        Integer ret = 0;
+        for(int i=0; i<carList.size(); i++){
+            Car currentCar = carList.get(i);
+            if(currentCar.getMoveNum() > ret){
+                ret = currentCar.getMoveNum();
+            }
+        }
+
+        return ret;
+    }
+
+    private List<String> findWinnerList(Integer threshold){
+        List<String> winnerList = new ArrayList<>();
+
+        for(int i=0; i<carList.size(); i++){
+            Car currentCar = carList.get(i);
+            if(currentCar.getMoveNum() == threshold){
+                winnerList.add(currentCar.getName());
+            }
+        }
+        return winnerList;
+    }
+
+    private void printFinalResult(List<String> winnerList){
+        System.out.print(ProcessMessage.PRINT_FINAL_WINNER.getMsg());
+
+        int commaCnt = winnerList.size();
+        for(int i=0; i<winnerList.size(); i++){
+            System.out.print(winnerList.get(i));
+            if(commaCnt != SINGLE_WINNER){
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
--- src/test/java/racingcar/controller/BasicControllerTest.java
+++ src/test/java/racingcar/controller/BasicControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class BasicControllerTest {
     @Test
     void BasicControllerTest(){
-        BasicController controller = new BasicController("pobi,woni,jun");
+        BasicController controller = new BasicController("pobi,woni,jun", 5);
         Assertions.assertThat(controller.carList.get(0).getName()).isEqualTo("pobi");
         Assertions.assertThat(controller.carList.get(1).getName()).isEqualTo("woni");
         Assertions.assertThat(controller.carList.get(2).getName()).isEqualTo("jun");
