7030e4f0094ed152c18fb0e68c6194511c61c701
pjm2476 <pjm2476@gmail.com>
2023-10-30 20:47:20 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        this.position++;
+    }
+}
