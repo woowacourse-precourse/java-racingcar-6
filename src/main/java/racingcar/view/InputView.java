32974b108d37542332c0294e17e9f4ab39be0fbb
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-30 21:14:30 +0900

    fix : comma 상수 split으로 ,를 받는 것 보다 매직넘버를 활용해주기 위해서 추가해주었습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
 
     public static final int MAX_NAME_LENGTH = 5;
 
+    public static final String COMMA = ",";
+
     public String startGame(){
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
