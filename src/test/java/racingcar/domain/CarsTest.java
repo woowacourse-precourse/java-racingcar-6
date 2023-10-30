dc0582e0eaf24ac9fcc2bf1114c439f6766dfd9d
olsohee <dlthgml0108@daum.net>
2023-10-30 11:47:41 +0900

    test(CarsTest): Cars 생성자에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,48 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarsTest {
+
+    @Test
+    @DisplayName("Cars 객체 생성 성공")
+    void CarsTest() {
+        // given
+        String inputNames = "pobi,jason";
+
+        // when
+        Cars cars = new Cars(inputNames);
+
+        // then
+        Assertions.assertThat(cars).isNotNull();
+    }
+
+    @Test
+    @DisplayName("자동차 이름에 공백이 포함되면 실패")
+    void CarsTestFailByBlank() {
+        // given
+        String inputNamesNonSpace = "pobi,";
+        String inputNamesSpace = "pobi, ";
+
+        // then
+        assertThatThrownBy(() -> new Cars(inputNamesNonSpace))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Cars(inputNamesSpace))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 중복되면 실패")
+    void CarsTestFailByDuplicate() {
+        // given
+        String inputNames = "pobi,pobi";
+
+        // then
+        assertThatThrownBy(() -> new Cars(inputNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
