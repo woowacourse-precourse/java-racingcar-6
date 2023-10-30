964848743d768eee555b567a4dbacb3cee25d894
insu3160 <is23193160@gmail.com>
2023-10-30 20:54:42 +0900

    feat: 중복되는 이름 입력 시 예외 처리 기능 추가

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 public class RacingCars {
@@ -8,9 +10,21 @@ public class RacingCars {
     private final List<Car> racingCars;
 
     public RacingCars(List<String> carNames) {
+        validateRacingCars(carNames);
         this.racingCars = carNames.stream()
                 .map(Car::new)
                 .toList();
+    }
+
+    private void validateRacingCars(List<String> carNames) {
+        checkForDuplicates(carNames);
+    }
+
+    private void checkForDuplicates(List<String> carNames) {
+        Set<String> set = new HashSet<>(carNames);
+        if (set.size() < carNames.size()) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR);
+        }
     }
 
     public int getSize() {
--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -2,9 +2,7 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -13,10 +11,20 @@ import org.junit.jupiter.api.Test;
 class RacingCarsTest {
     @Test
     @DisplayName("RacingCars가 잘 생성되는지 확인")
-    public void createRacingCars(){
-        List<String> carNames = Arrays.asList("pobi","dobi");
+    public void createRacingCars() {
+        List<String> carNames = Arrays.asList("pobi", "dobi");
         RacingCars racingCars = new RacingCars(carNames);
         assertThat(racingCars.getSize()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("중복된 이름 입력 시 예외 처리")
+    public void checkForDuplicates() {
+        List<String> carNames = Arrays.asList("pobi", "pobi");
+        assertThatThrownBy(() -> {
+            RacingCars racingCars = new RacingCars(carNames);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[ERROR] : 중복되지 않은 자동차 이름을 입력해주세요.");
     }
 
 }
