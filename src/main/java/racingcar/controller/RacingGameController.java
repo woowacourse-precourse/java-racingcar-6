28d0977079c0f9d0c1d98270bb77b89dafaff15e
ohhyeon2 <dhgus1235@gmail.com>
2023-10-30 20:02:58 +0900

    refactor: RacingGameController 고정 입력 값에 상수 적용

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,39 +1,47 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+
 import racingcar.Constant;
 
 public class RacingGameController {
 
     public static String[] commaBasedSplitCarName(String carName) {
         String[] carNameArr = carName.split(Constant.COMMA);
-
         ExceptionController.carNameMaxLengthException(carNameArr);
-
         return carNameArr;
     }
 
     public static HashMap<String, String> makePersonalCarAndForwardStatus(final String[] carNameArr) {
         HashMap<String, String> createUserCar = new HashMap<>();
-
         for (String car : carNameArr) {
             createUserCar.put(car, "");
         }
-
         return createUserCar;
     }
 
     public static HashMap<String, String> carNameAndForwardStatus(final String carName) {
         String[] carNameArr = commaBasedSplitCarName(carName);
         HashMap<String, String> personalCar = makePersonalCarAndForwardStatus(carNameArr);
-
         ExceptionController.carNameDuplicateException(personalCar, carNameArr);
-
         return personalCar;
+    }
+
+    public static List<String> mostMoveForwardCar(HashMap<String,String> carMoveStatus) {
+        List<String> winnerList = new ArrayList<>();
+        String mostForward = Collections.max(carMoveStatus.values());
+
+        for (String winner : carMoveStatus.keySet()) {
+            if (mostForward.equals(carMoveStatus.get(winner))) {
+                winnerList.add(winner);
+            }
+        }
+        return winnerList;
     }
 
     public static void moveForward(HashMap<String, String> userCarName) {
@@ -45,20 +53,7 @@ public class RacingGameController {
         }
     }
 
-    public static List<String> mostMovingForwardCar(HashMap<String,String> carMoveStatus) {
-        List<String> winnerList = new ArrayList<>();
-        String mostForward = Collections.max(carMoveStatus.values());
-
-        for (String winner : carMoveStatus.keySet()) {
-            if (mostForward.equals(carMoveStatus.get(winner))) {
-                winnerList.add(winner);
-            }
-        }
-
-        return winnerList;
-    }
-
-    public static int randomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+    private static int randomNumber() {
+        return pickNumberInRange(Constant.MIN_RANDOM_NUMBER_RANGE, Constant.MAX_RANDOM_NUMBER_RANGE);
     }
 }
