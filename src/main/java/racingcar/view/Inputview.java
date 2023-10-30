a3447015bcf7cb8c3cd68b07eb26e116dc4ca98d
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:09:07 +0900

    feat(InputView) : 자동차 경주 게임이 시작하는 startGame 메소두 구현

--- src/main/java/racingcar/view/Inputview.java
+++ src/main/java/racingcar/view/Inputview.java
@@ -1,32 +1,23 @@
 package racingcar.view;
 
-import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.GameController;
 import racingcar.validation.InputValidation;
 
 public class Inputview {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String GAME_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+    private static final String EXECUTION_RESULT = "\n실행 결과";
+
+
 
     public static void startGame() {
         System.out.println(GAME_START_MESSAGE);
-        inputCarName();
-    }
-
-    public static List<String> inputCarName() {
-        String input = Console.readLine();
-        return InputValidation.validationNameSeparate(InputValidation.validateAndSplit(input));
-    }
-
-    public static int tryGameNumber() {
-        System.out.println(GAME_NUMBER_OF_ATTEMPTS);
-        String input = String.valueOf(Console.readLine());
-        return InputValidation.carNumericValidation(input);
-    }
-
-    public int carNumericValue() {
-        return 0;
+        List<String> carNames = inputCarName();
+        int numberOfAttempts = tryGameNumber();
+        GameController gameController = new GameController();
+        gameController.playGame(String.join(",", carNames), numberOfAttempts);
     }
 }
