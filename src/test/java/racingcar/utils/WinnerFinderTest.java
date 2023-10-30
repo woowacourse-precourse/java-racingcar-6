bed1871341e52151fa583d9f04d1ef8bf2c22cd1
junest66 <chlwndks333@gmail.com>
2023-10-31 01:08:27 +0900

    refactor(WinnerFinderTest): 게임 객체 생성 방법 변경으로 인한 테스트 수정

--- src/test/java/racingcar/utils/WinnerFinderTest.java
+++ src/test/java/racingcar/utils/WinnerFinderTest.java
@@ -2,8 +2,10 @@ package racingcar.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.Game;
 import racingcar.domain.TestNumberGenerator;
 
@@ -11,9 +13,14 @@ class WinnerFinderTest {
 
     @Test
     void 우승자_이름_찾기() {
-        Game game = new Game("pobi,woni,jun", new TestNumberGenerator(7));
+        List<Car> carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(1)),
+                new Car("woni", new TestNumberGenerator(7)),
+                new Car("jun", new TestNumberGenerator(9))
+        );
+        Game game = new Game(carList);
         game.playOneRound();
-        List<String> winners = WinnerFinder.findWinners(game.getCars()); // pobi, woni, jun
-        assertThat(winners).containsOnly("pobi", "woni", "jun");
+        List<String> winners = WinnerFinder.findWinners(game.getCars()); // woni, jun
+        assertThat(winners).containsOnly("woni", "jun");
     }
 }
