432122b96bfc27a23b91b098b7f334fe36528abf
kang jihyeong <her4386@gmail.com>
2023-10-30 23:34:44 +0900

    [feat] 경주할 자동차 이름 입력

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -1,8 +1,20 @@
 package racingcar.game;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Game {
+    private String carNames;
 
     public Game() {
+        printPhrasesCarName();
+        inputCarName();
+    }
 
+    private void printPhrasesCarName() {
+        System.out.println(GamePhrases.PHRASES_INPUT_CAR_NAME);
+    }
+
+    private void inputCarName() {
+        carNames = readLine();
     }
 }
