e55ecdfe286186d510c59bed94bd678729d00166
lotuxsoo <tndus5383@gmail.com>
2023-10-30 14:31:09 +0900

    fix: RandomNumber 자료형 List<Integer> -> int로 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,13 +1,15 @@
 package racingcar.model;
 
-import java.util.List;
-
 public class Car {
     private String name;
-    private int movement;
+    private int status;
 
-    public Car(String name, int movement) {
+    public Car(String name, int status) {
         this.name = name;
-        this.movement = movement;
+        this.status = status;
+    }
+
+    public void move() {
+        status++;
     }
 }
--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -7,20 +7,21 @@ import java.util.List;
 
 //자동차 이름 개수만큼 랜덤숫자를 생성
 public class RandomNumber {
-    private List<Integer> randomList = new ArrayList<>();
+    private int number;
     private static final int START_RANGE = 0;
     private static final int END_RANGE = 9;
+    private static final int MOVABLE_NUMBER = 4;
 
-    private RandomNumber(List<Integer> list) {
-        this.randomList = list;
+    private RandomNumber(int number) {
+        this.number = number;
     }
 
     public static RandomNumber makeRandomNumber() {
-        List<Integer> tmpRandomList = new ArrayList<>();
-        while (tmpRandomList.size() < 3) { // 일단 3으로 설정
-            int number = pickNumberInRange(START_RANGE, END_RANGE);
-            tmpRandomList.add(number);
-        }
-        return new RandomNumber(tmpRandomList);
+        int number = pickNumberInRange(START_RANGE, END_RANGE);
+        return new RandomNumber(number);
+    }
+
+    public boolean isMovable() {
+        return number >= MOVABLE_NUMBER;
     }
 }
