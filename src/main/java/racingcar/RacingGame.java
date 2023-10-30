6b17101fcce8c626c844d7ae51906b051cc48758
choimung <103022503+choimung@users.noreply.github.com>
2023-10-30 13:20:51 +0900

    feat : 자동차의 이름과 위치를 출력하는 기능 추가 자동차의 이름과 위치를 출력하는 기능을 추가하였습니다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,17 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import racingcar.domain.Car;
 import racingcar.domain.CarList;
 
 public class RacingGame {
-
     private CarList carList;
 
-    public CarList generateCar(String carNames) {
+
+
+    public String printNameAndPosition(List<Car> cars, List<Integer> carsPosition){
+        return IntStream.range(0, cars.size())
+                .mapToObj(index -> cars.get(index).getName() + " : " + "-".repeat(carsPosition.get(index)))
+                .collect(Collectors.joining("\n"));
+    }
+
+
+    public List<Car> generateCar(String carNames) {
         List<Car> cars = Arrays.stream(carNames.split(","))
                 .map(Car::new)
                 .toList();
-        return carList = new CarList(cars);
+        carList = new CarList(cars);
+        return cars;
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,8 +1,12 @@
 package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.contains;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.CarList;
 
 class RacingGameTest {
@@ -11,8 +15,16 @@ class RacingGameTest {
 
     @Test
     void 쉼표를_기준으로_자동차의_이름을_입력받기(){
-        CarList carList = racingGame.generateCar("povi,woni,minu");
-        assertThat(carList.size()).isEqualTo(3);
+        List<Car> cars = racingGame.generateCar("povi,woni,minu");
+        assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 전진하는_자동차_출력테스트(){
+        List<Car> cars = racingGame.generateCar("povi,woni");
+        List<Integer> carsPosition = Arrays.asList(2, 0);
+
+        assertThat(racingGame.printNameAndPosition(cars,carsPosition)).contains("povi : --");
     }
 
 }
