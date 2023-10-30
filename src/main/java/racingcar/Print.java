8694d89d14e73a1327c2990969fc9fc8d8c5df77
wonindev <sangwonparkdev@gmail.com>
2023-10-30 12:30:56 +0900

    feat: 게임 진행 상황을 출력해주는 기능 추가

---
+++ src/main/java/racingcar/Print.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Print {
+    public static void progress(int attemptNum, List<String> members, HashMap<String, String> membersHM) {
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < attemptNum; i++) {
+            CarMovement.putHM(membersHM, members, members.size(), "-");
+            for (int j = 0; j < members.size(); j++) {
+                System.out.println(members.get(j) + " : " + membersHM.get(members.get(j)));
+            }
+            System.out.println();
+        }
+    }
+}
