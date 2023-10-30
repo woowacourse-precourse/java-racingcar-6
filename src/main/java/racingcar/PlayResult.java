9875c1caf595305ae357b196c79bbfcc7d017c5c
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-30 22:55:24 +0900

    FEAT: 실행결과 출력 기능 추가 - 차가 얼마나 이동했는지 이름과 함께 출력 - EX) 이름 : - - 전체적인 게임 플레이를 관장하는 Race 클래스에서 차량 이름인 names 배열과 각 차량 이름에 " : "가 붙은 문자열을 저장한 results 배열을 넘겨 받는다.

---
+++ src/main/java/racingcar/PlayResult.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class PlayResult {
+    public void getResult(String[] names, String[] results){
+        for (int i = 0; i < names.length; i++) {
+            int r = Randoms.pickNumberInRange(0, 9);
+            if (r > 4)
+                results[i] += "-";
+            System.out.println(results[i]);
+        }
+    }
+}
