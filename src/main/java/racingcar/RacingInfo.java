63a565a5790ca6294e0a5bebb224a100b9693071
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-30 16:24:13 +0900

    FEAT: 입력에 따른 예외처리 기능 추가 - 차량이름이 5자를 초과할 경우 - 시도 횟수를 입력할 때 숫자가 아니거나 음수를 입력할 경우 --> IllegalArgumentException 예외처리

--- src/main/java/racingcar/RacingInfo.java
+++ src/main/java/racingcar/RacingInfo.java
@@ -10,14 +10,40 @@ public class RacingInfo {
     }
 
     public void setNamesAndNum() {
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         names = Console.readLine().split(",");
-        System.out.print("시도할 회수는 몇회인가요?");
-        num = Integer.parseInt(Console.readLine());
+        // 이름이 5자를 초과할 때 예외처리
+        for (int i = 0; i < names.length; i++) {
+            if(names[i].length() > 5){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String n = Console.readLine();
+        // 횟수로 숫자가 아닐 경우 예외처리
+        if (!isInteger(n)) {
+            throw new IllegalArgumentException();
+        }
+        num = Integer.parseInt(n);
+
+        // 횟수가 음수인 경우 예외처리
+        if (num < 0){
+            throw new IllegalArgumentException();
+        }
         System.out.println();
     }
 
     public int getNum() {
         return num;
+    }
+
+    public boolean isInteger(String strValue) {
+        try {
+            Integer.parseInt(strValue);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
     }
 }
