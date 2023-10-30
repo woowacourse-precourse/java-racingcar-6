9eded66ec6d238d49ea66a23d5e092fdd289c75b
jang010505 <jang010505@inu.ac.kr>
2023-10-31 02:55:46 +0900

    feat: validateCheckCarName 에러 메시지 추가

--- src/main/java/racingcar/constant/MessageConstant.java
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -2,7 +2,10 @@ package racingcar.constant;
 
 public enum MessageConstant {
     CAR_NAME_INPUT_COUNT_ERROR("1대 이상의 자동차를 입력해주세요."),
-    CAR_NAME_INPUT_DUPLICATE_ERROR("서로 다른 자동차 이름을 입력해주세요.");
+    CAR_NAME_INPUT_DUPLICATE_ERROR("서로 다른 자동차 이름을 입력해주세요."),
+
+    CAR_NAME_LENGTH_ERROR("자동차의 이름은 1자 이상 5자 이하야야합니다."),
+    CAR_NAME_CONTAINS_SPACE_ERROR("자동차의 이름은 공백이 포함되지 말아야합니다.");
 
     private final String message;
 
