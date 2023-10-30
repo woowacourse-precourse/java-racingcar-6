e1daafb9d4dc2972fd2b79a4df8ca21b366a9068
junest66 <chlwndks333@gmail.com>
2023-10-31 01:06:29 +0900

    refactor(GameTest): 게임 객체 생성 방법 변경으로 인한 테스트 수정 및 일부자동차의 전진멈춤 테스트 추가

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -2,31 +2,71 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
     private Game game;
+    private List<Car> carList;
+
+    @BeforeEach
+    void setUp() {
+        carList = new ArrayList<>();
+        game = null;
+    }
 
     @Test
     void 모든_자동차가_전진_확인() {
-        game = new Game("pobi,woni,jun", new TestNumberGenerator(7));
+        carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(7)),
+                new Car("woni", new TestNumberGenerator(8)),
+                new Car("jun", new TestNumberGenerator(9))
+        );
+        game = new Game(carList);
         game.playOneRound();
         assertThat(game.getCars()).allMatch(car -> car.getMoveNum() == 1);
     }
 
     @Test
     void 모든_자동차가_멈춤_확인() {
-        game = new Game("pobi,woni,jun", new TestNumberGenerator(3));
+        carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(1)),
+                new Car("woni", new TestNumberGenerator(2)),
+                new Car("jun", new TestNumberGenerator(3))
+        );
+        game = new Game(carList);
         game.playOneRound();
         assertThat(game.getCars()).allMatch(car -> car.getMoveNum() == 0);
     }
 
     @Test
+    void 일부_전진_멈춤_확인() {
+        carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(1)),
+                new Car("woni", new TestNumberGenerator(7)),
+                new Car("jun", new TestNumberGenerator(9))
+        );
+        game = new Game(carList);
+        game.playOneRound();
+        assertThat(carList.get(0).getMoveNum()).isEqualTo(0);
+        assertThat(carList.get(1).getMoveNum()).isEqualTo(1);
+        assertThat(carList.get(2).getMoveNum()).isEqualTo(1);
+    }
+
+    @Test
     void 우숭자_자동차_리스트_조회하는_테스트() {
-        game = new Game("pobi,woni,jun", new TestNumberGenerator(7));
+        carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(1)),
+                new Car("woni", new TestNumberGenerator(7)),
+                new Car("jun", new TestNumberGenerator(9))
+        );
+        game = new Game(carList);
+        game.playOneRound();
         List<String> winners = game.getWinners();
-        assertThat(winners).containsExactly("pobi", "woni", "jun");
+        assertThat(winners).containsOnly( "woni", "jun");
     }
 }
