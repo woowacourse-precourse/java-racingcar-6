a2b733d0fdf7d2927b1a934a1c4c25aeca37d4da
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-31 08:58:05 +0900

    feat: 공통적으로 사용하는 model key값 상수클래스 구현

---
+++ src/main/java/racingcar/model/ModelConst.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+public interface ModelConst {
+
+    public static final String CAR_NAME = "car name";
+    public static final String ATTEMPT_NUMBER = "attempt number";
+    public static final String TOTAL_GAME_RESULT = "total game result";
+
+}
