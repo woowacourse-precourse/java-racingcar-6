b2e0fe397f424438bf3b6068fcb80eebc89e8450
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:34:23 +0900

    feat(Cars) : 우승자 자동차 데이터만 계산하는 getWinners 메소드와 getWinnersCar 메소드 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,2 +1,23 @@
-package racingcar.model;public class Cars {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    // 우승자 목록
+    public static List<String> getWinners(List<Car> cars, int maxPosition) {
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            getWinnersCar(car, maxPosition, winners);
+        }
+        return winners;
+    }
+
+    private static List<String> getWinnersCar(Car car, int maxPosition, List<String> winners) {
+        if (car.getPosition() == maxPosition) {
+            winners.add(car.getName());
+        }
+        return winners;
+    }
 }
