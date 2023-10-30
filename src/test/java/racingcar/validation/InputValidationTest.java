56ed00dd22fc1a4f73dda0ab34a860b1398ec7f1
hee9841 <lee98417716@gmail.com>
2023-10-31 06:25:59 +0900

    test: 게임 입력관련 유효성 테스트 완료

---
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -0,0 +1,74 @@
+package racingcar.validation;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class InputValidationTest {
+    private static InputValidation inputValidation;
+    private static final int NAME_SIZE = 5;
+
+
+    @BeforeEach
+    void beforeEach() {
+        inputValidation = new InputValidation();
+    }
+
+    @Test
+    void 자동차_이름_형태_유효성_확인() {
+        inputValidation.validateCarNameSize("pobi", NAME_SIZE);
+
+    }
+
+    @Test
+    void 자동차_이름_예외처리() {
+        assertThatThrownBy(
+                () -> inputValidation.validateCarNameSize("", NAME_SIZE)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateCarNameSize(" ", NAME_SIZE)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateCarNameSize(null, NAME_SIZE)
+        ).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(
+                () -> inputValidation.validateCarNameSize("\n", NAME_SIZE)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateCarNameSize("123456", NAME_SIZE)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이동_차수_입력_유효성_확인() {
+        inputValidation.validateMovingCount("7");
+
+    }
+
+    @Test
+    void 이동_차수_입력_예외() {
+        assertThatThrownBy(
+                () -> inputValidation.validateMovingCount("")
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateMovingCount(null)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateMovingCount("\n")
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> inputValidation.validateMovingCount("pobi")
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+
+}
