2d8f9dbfa0141d43017f85b05c1cab7e3e0d383d
jyajoo <jyajoo1020@gmail.com>
2023-10-30 16:32:30 +0900

    test: 자동차 이름의 길이가 일정 값보다 긴 경우, 예외 처리한다.

--- src/test/java/racingcar/service/ValidationServiceTest.java
+++ src/test/java/racingcar/service/ValidationServiceTest.java
@@ -1,8 +1,10 @@
 package racingcar.service;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static racingcar.constant.ErrorMessage.*;
+import static racingcar.constant.RaceSetting.MAX_CAR_NAME_LENGTH;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -31,5 +33,20 @@ class ValidationServiceTest {
                 () -> validationService.validateCarNamesInput(carNamesInput));
 
         assertThat(exception.getMessage()).isEqualTo(EMPTY_INPUT_ERROR.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름의 길이가 일정 값보다 긴 경우, 예외 처리한다.")
+    @CsvSource({"'A', 'AB', 'ABCDE'", "'ABCDEF', 'A', 'ABC'"})
+    void validateCarNamesInputTest3(String carName1, String carName2, String carName3) {
+        ValidationService validationService = new ValidationService();
+        List<String> carNamesInput = List.of(carName1, carName2, carName3);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> validationService.validateCarNamesInput(carNamesInput));
+
+        assertThat(exception.getMessage()).isEqualTo(
+                format(LENGTH_INPUT_ERROR.getMessage(), MAX_CAR_NAME_LENGTH.getValue())
+        );
     }
 }
