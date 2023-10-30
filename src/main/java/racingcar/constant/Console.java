cd28648c500eab532325d27b51064a72695ff5bf
itsmo1031 <itsmo1031@gmail.com>
2023-10-30 23:20:48 +0900

    feat(constant): 콘솔 출력 메세지를 다룰 Console Enum Class 작성

---
+++ src/main/java/racingcar/constant/Console.java
@@ -0,0 +1,20 @@
+package racingcar.constant;
+
+public enum Console {
+    INIT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    TRY("시도할 회수는 몇회인가요?"),
+    RESULT("실행 결과"),
+    WINNER("최종 우승자 : "),
+    SEPARATOR(" : "),
+    PROGRESS("-");
+
+    private final String message;
+
+    Console(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
