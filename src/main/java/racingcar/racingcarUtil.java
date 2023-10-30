23037e873d67ee2af37a0ba1b3e91cc9a5ed2d72
Wangguri <anordinarykorean@gmail.com>
2023-10-30 13:56:58 +0900

    feat(racingcar): 자동차 이름 부여 기능 구현

--- src/main/java/racingcar/racingcarUtil.java
+++ src/main/java/racingcar/racingcarUtil.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 class TryTimes {
 	public static String askTimes() {
@@ -11,4 +13,25 @@ class TryTimes {
 		return tryTimes;
 	}
 
+}
+
+class CarName {
+	
+	public static void inputCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String carName = Console.readLine();
+		String[] result = carName.split("[,]",0);
+		carNameCheck(result);
+		List<String> carNames = Arrays.asList(result);
+		System.out.println(carNames);		
+	}
+	
+	public static void carNameCheck(String[] str) {
+		for(int i = 0; i < str.length; i++) {
+			if(str[i].length() > 5) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
+	
 }
