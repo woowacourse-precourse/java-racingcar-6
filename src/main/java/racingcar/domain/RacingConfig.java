843c66dd3e79b31684feb2ef2f89ca1316ca9312
leemiyeon <lemingul7@gmail.com>
2023-10-30 20:20:21 +0900

    style: code refactoring 1. RacingConfig

--- src/main/java/racingcar/domain/RacingConfig.java
+++ src/main/java/racingcar/domain/RacingConfig.java
@@ -6,9 +6,9 @@ public class RacingConfig {
     }
     public static final String NAME_SEPARATOR = ",";
     public static final String NAME_PATTERN = "^([a-zA-Z\\p{IsHangul}]+,)*[a-zA-Z\\p{IsHangul}]+$";
-    public static final String FORWARD_HISTORY_MARK = "-";
     public static final int MAX_NAME_LENGTH = 5;
+    public static final int MOVE_FORWARD_VALUE = 4;
+    public static final String MOVED_HISTORY_MARK = "-";
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
-    public static final int FORWARD_NUMBER = 4;
 }
