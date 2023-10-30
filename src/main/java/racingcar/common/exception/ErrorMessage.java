84f41979610ea6facbd3afe3664404088ec598a4
beomsic <qjatjr29@khu.ac.kr>
2023-10-30 14:22:31 +0900

    :sparkles: Feat: Add enum class for custom exception
    - 상황에 맞는 예외를 throw 하기 위한 예러 메시지 enum class 추가

---
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -0,0 +1,20 @@
+package racingcar.common.exception;
+
+public enum ErrorMessage {
+
+    NULL_INPUT_STRING("Input String cannot be null."),
+    EMPTY_INPUT_STRING("Input String cannot be empty or blank."),
+    NAME_TOO_LONG("Name is too long."),
+    NULL_NAME("Name cannot be null."),
+    EMPTY_NAME("Name cannot be empty or blank.");
+
+    private final String message;
+
+    ErrorMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
