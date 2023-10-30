a8ede33cc6d286aa3fe29684961ac368b8ea5b0e
Woongbae Yun <devbelly@naver.com>
2023-10-30 19:27:03 +0900

    feat : 정해진 규칙에 따라 자동차가 움직이는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,11 +3,21 @@ package racingcar.domain;
 public class Car {
 
     private static final String CAR_NAME_LENGTH_ERROR = "자동차 이름은 5자 이하만 가능합니다.";
+    private static final int MOVING_FORWARD = 4;
+
     private String carName;
+    private Position position;
 
     Car(String carName) {
         validate(carName);
         this.carName = carName;
+        this.position = new Position();
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVING_FORWARD) {
+            position.shift(1);
+        }
     }
 
     private void validate(String input) {
