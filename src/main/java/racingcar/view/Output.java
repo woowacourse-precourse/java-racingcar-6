30f0caf95abc4d526b2b6f4a37d391c86937f528
bamm2 <xxknu88@naver.com>
2023-10-30 20:59:17 +0900

    "feat: 결과 출력 메서드 & 시작 시 문구 출력"

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,12 +1,31 @@
 package racingcar.view;
 
+import static racingcar.constant.Constant.EXECUTION_RESULT_MESSAGE;
+
+import java.util.Map;
+
+import racingcar.domain.Car;
+
 public class Output {
 
-    public Output(){
+    public Output() {
+        printExecutionMessage();
     }
 
-    private void insertNewLine(){
+    public void printResult(Map<String, Car> fitInRacingCars) {
+        fitInRacingCars.forEach((carName, distance)
+                -> System.out.println(carName + " : " + distance.getMoveDistance()));
+        insertNewLine();
+    }
+
+    private void printExecutionMessage() {
+        insertNewLine();
+        System.out.println(EXECUTION_RESULT_MESSAGE);
+    }
+
+    private void insertNewLine() {
         System.out.println();
     }
+
 
 }
