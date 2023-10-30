574535894cb33f182023384a29f09129173d0042
SeongJunChun <csj991210@naver.com>
2023-10-31 08:22:34 +0900

    FEAT  :  게임 시작 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,22 @@
 package racingcar;
 
 import racingcar.View.InputMessage;
+import racingcar.View.SystemMessage;
+import racingcar.controller.GameController;
+import racingcar.model.CarAction;
 import racingcar.model.Cars;
+import racingcar.model.Referee;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Cars cars = new Cars();
-        cars.makeCarList("");
-        //System.out.println(Cars.carList);
-
+        GameController gameController = new GameController();
+        gameController.playGame();
     }
 }
