d53ec31c88bdc31f6ebeeaf204778e3e7672c217
junghunim07 <dlawndgns9@naver.com>
2023-10-30 16:01:54 +0900

    feat(GameManager): GameManager 클래스 생성자 및 변수 생성

---
+++ src/main/java/racingcar/domain/GameManager.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class GameManager {
+    static List<Car> carRace;
+    static List<String> winner;
+
+    public GameManager() {
+        InputValue inputValue = new InputValue();
+    }
+}
