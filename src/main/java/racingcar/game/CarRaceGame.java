7d9efa527e49c4851182e18f05d8e3518183cfcc
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-30 21:39:24 +0900

    CarRaceServic 와 게임을 진행할 횟수를 저장하는 멤버변수 설정

--- src/main/java/racingcar/game/CarRaceGame.java
+++ src/main/java/racingcar/game/CarRaceGame.java
@@ -1,6 +1,13 @@
 package racingcar.game;
 
+import racingcar.service.CarRaceService;
+
 public class CarRaceGame implements Game {
+    private CarRaceService service;
+    private int gameRound;
+
+
+
     @Override
     public void startGame() {
 
