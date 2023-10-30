bbf47629702425ef076338b4263f05f440ae9161
insu3160 <is23193160@gmail.com>
2023-10-30 20:51:16 +0900

    feat: 랜덤한 리스트를 넘겨 받아 자동차들을 각각 움직이는 기능 추가

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,8 +1,7 @@
 package racingcar.model;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.IntStream;
 
 public class RacingCars {
     private static final String DUPLICATE_CAR_NAME_ERROR = "[ERROR] : 중복되지 않은 자동차 이름을 입력해주세요.";
@@ -13,8 +12,14 @@ public class RacingCars {
                 .map(Car::new)
                 .toList();
     }
-    public int getSize(){
+
+    public int getSize() {
         return racingCars.size();
+    }
+
+    public void moveRacingCars(List<Integer> numbers) {
+        IntStream.range(0, numbers.size())
+                .forEach(index -> racingCars.get(index).moveForward(numbers.get(index)));
     }
 
 }
