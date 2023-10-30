381b3b5eeea03325381cf16bb4d934106e0e7cd8
howudong <tjdvy963@naver.com>
2023-10-30 18:21:34 +0900

    feat : Controller 인터페이스 및 Controller를 Mapping해줄 MainController 생성

---
+++ src/main/java/racingcar/controller/ControlConfig.java
@@ -0,0 +1,29 @@
+package racingcar.controller;
+
+import racingcar.service.CarService;
+import racingcar.view.inputview.InputValidator;
+import racingcar.view.inputview.SettingInputView;
+import racingcar.view.outputview.RacingOutputView;
+import racingcar.view.outputview.ResultOutputView;
+import racingcar.view.outputview.SettingOutputView;
+
+public class ControlConfig {
+    public Controller createSettingController() {
+        return new SettingController(new SettingInputView(new InputValidator()), new SettingOutputView());
+
+    }
+
+    public Controller createRacingController(Object param) {
+        if (param instanceof CarService service) {
+            return new RacingController(new RacingOutputView(), service);
+        }
+        return null;
+    }
+
+    public Controller createResultController(Object param) {
+        if (param instanceof CarService service) {
+            return new ResultController(new ResultOutputView(), service);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/racingcar/controller/MainController.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import racingcar.domain.GameState;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class MainController {
+    private final ControlConfig config;
+    private final Map<GameState, Function<Object, Controller>> adapter;
+
+    public MainController(ControlConfig config) {
+        this.config = config;
+        adapter = initControllerMapping();
+    }
+
+    public void run() {
+        Map<String, String> parameter = new HashMap<>();
+        Map<String, Object> model = new HashMap<>();
+        GameState gameState = GameState.SETTING;
+
+        startLevel(gameState, parameter, model);
+    }
+
+    private GameState startLevel(GameState gameState, Map<String, String> parameter, Map<String, Object> model) {
+        Controller controller = adapter.get(gameState).apply(null);
+        controller.run(parameter, model);
+        return GameState.PLAYING;
+    }
+
+    private Map<GameState, Function<Object, Controller>> initControllerMapping() {
+        return Map.ofEntries(
+                Map.entry(GameState.SETTING, param -> config.createSettingController()),
+                Map.entry(GameState.PLAYING, config::createRacingController),
+                Map.entry(GameState.FINISH, config::createResultController)
+        );
+    }
+
+}
