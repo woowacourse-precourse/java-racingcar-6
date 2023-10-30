5484b41b1ad87b203aceb70fd66a5663458349ba
chaniii999 <psc0729@naver.com>
2023-10-30 21:53:29 +0900

    feat: 횟수입력 기능 추가

---
+++ src/main/java/racingcar/controller/InputCount.java
@@ -0,0 +1,23 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+// 시도횟수 저장 & 검사
+public class InputCount {
+
+    private final int count;
+
+    public InputCount() {
+        try {
+            String input = Console.readLine();
+            count = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 값 입력");
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
