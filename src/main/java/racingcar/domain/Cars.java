35d091bc4ae4a01bf6512964a0d7ed7bd10b7ded
minjun7410 <minjun7410@gmail.com>
2023-10-30 16:17:07 +0900

    refactor(Cars): playTurn 리팩토링
    * moveCars와 printResultOfTurn가 반복문을 도는데 불필요하게 두번 돌 필요가 없다고 생각해 두 메서드를 삭제하고 playTurn으로 합쳤다.
    * cars 변수는 한번 할당되면 바뀌지 않으므로 final을 추가했다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Cars {
     private static final int CAR_MAXIMUM_NUMBER = 10;
-    List<Car> cars;
+    private final List<Car> cars;
 
     public Cars(List<String> carNameList) {
         cars = new ArrayList<>();
@@ -15,24 +15,14 @@ public class Cars {
     }
 
     public void playTurn() {
-        moveCars();
-        printResultOfTurn();
-    }
-
-    public Winners getWinners() {
-        return new Winners(cars);
-    }
-
-    private void printResultOfTurn() {
         for (Car car : cars) {
+            car.moveRandomly();
             car.printPresentMovingDistance();
         }
     }
 
-    private void moveCars() {
-        for (Car car : cars) {
-            car.moveRandomly();
-        }
+    public Winners getWinners() {
+        return new Winners(cars);
     }
 
     private void addCarInCars(List<String> carNameList) {
@@ -49,6 +39,7 @@ public class Cars {
             throw new IllegalArgumentException("차 이름이 중복되었습니다.");
         }
     }
+
     private void validateCarsMaximumNumber() {
         if (cars.size() >= CAR_MAXIMUM_NUMBER) {
             throw new IllegalArgumentException(String.format("차의 개수는 %d 을 넘어갈 수 없습니다.", CAR_MAXIMUM_NUMBER));
