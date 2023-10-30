b91fe0a3c8e53488d0f2e3cadf7c2650074efaf8
Tiger <skdlghwns@gmail.com>
2023-10-30 21:35:01 +0900

    feat(racingcar): 실행결과 출력 구현 - printPosition()으로 자동차의 현제 위치를 시각적으로 표현하였다. - reportRacing()을 사용하면 1회 시도 후 자동차의 위치를 출력한다. - odOneGame()을 사용하여 입력받은 시도 회수 만큼reportRacing을 호출한다.

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -2,13 +2,14 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Racing {
     private static int count;
     private static List<Car> racingCars;
-    private static Map<Car, Integer> carsPosition;
+    private static Map<Car, Integer> carsPosition = new HashMap<Car, Integer>();
     public Racing(List<Car> racingCars){
         this.racingCars = racingCars;
         for(Car car : racingCars){
@@ -30,6 +31,30 @@ public class Racing {
         if(racingCar.tryMove()){
             int position = carsPosition.get(racingCar);
             carsPosition.put(racingCar, ++position);
+        }
+    }
+    public static void printPosition(Car racingCar){
+        System.out.printf("%s : ", racingCar.announceName());
+        for(int i=0; i<carsPosition.get(racingCar); i++){
+            System.out.print('-');
+        }
+        System.out.println();
+    }
+    public static void reportRacing(){
+        for(Car racingCar : racingCars){
+            applyPosition(racingCar);
+            printPosition(racingCar);
+        }
+    }
+    public static void announceWinner(){
+
+    }
+    public void doOneGame(){
+        askRacingCount();
+        System.out.println("실행 결과");
+        for(int i=0; i<count; i++){
+            reportRacing();
+            System.out.println();
         }
     }
 }
