6d5fc905c15ecc9952cac0c314416398afd9787c
gundorit <packdev937@gmail.com>
2023-10-30 19:48:03 +0900

    feat (Try) : 자연수로 시도 횟수 객체를 생성한다

---
+++ src/main/java/racingcar/domain/Try.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Try {
+
+    private int count;
+
+    public Try(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
