07664682c5fbbddec8e0ec0ba6b6eeed633258b6
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:05:49 +0900

    feat(GoStopCar) : 자동차가 전진 멈춤 하는 값을 부여하는 무작위의 값 pickNumberInRange 메소드 기능 구현

--- src/main/java/racingcar/util/GoStopCar.java
+++ src/main/java/racingcar/util/GoStopCar.java
@@ -1,7 +1,12 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class GoStopCar {
+    private static final int RANDOM_START_NUMBER = 0;
+    private static final int RANDOM_END_NUMBER = 9;
     public static int pickNumberInRange() {
-        return 0;
+        int random = Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER);
+        return random;
     }
 }
