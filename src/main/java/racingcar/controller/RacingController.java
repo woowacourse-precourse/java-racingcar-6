b733c53cc7523078f4f282250e821ec55d51fbec
Joohui <joohuii96@gmail.com>
2023-10-31 05:13:22 +0900

    feat: 이름 입력 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
     OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
 
     public void raceStart() {
         outputView.printInputCarNames();
+        String carNames = inputView.carNames();
     }
 }
