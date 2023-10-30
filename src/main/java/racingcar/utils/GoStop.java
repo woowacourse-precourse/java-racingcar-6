3d8869fb81b8d69bb25d12eab06f292e9492c3bb
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-30 16:00:51 +0900

    refactor: 게임 한판을 처리하는 메소드로 교체

--- src/main/java/racingcar/utils/GoStop.java
+++ src/main/java/racingcar/utils/GoStop.java
@@ -1,22 +1,26 @@
 package racingcar.utils;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 import main.java.racingcar.model.Car;
 
 public class GoStop {
 
     private static final int FORWARD_BOUND = 4;
-    public static void decideGoStop (Car car) {
-        car.forward();
+
+    public static void oneTurn(List<Car> Users) {
+        for (Car user: Users) {
+            if(decisionMaker(generateNum())) {
+                user.forward();
+            }
+        }
     }
     private static boolean decisionMaker(int n) {
         return n >= FORWARD_BOUND;
     }
     private static int generateNum() {
-        int num;
-        return num = Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(0,9);
     }
 
 
