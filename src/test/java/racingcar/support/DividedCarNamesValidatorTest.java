a83fa67cd2ca6f258eda253c59e9bc1ecb59cc05
kssumin <201566@jnu.ac.kr>
2023-10-31 03:49:40 +0900

    refactor : Assertions static import 처리로 변경

--- src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
+++ src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
@@ -1,5 +1,7 @@
 package racingcar.support;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,7 +14,7 @@ class DividedCarNamesValidatorTest {
 	@ValueSource(strings ="12345")
 	void validate_divided_count(String input) {
 		// when & then
-		Assertions.assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
+		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessageContaining("자동차 이름은 2개 이상 입력해야합니다");
 	}
