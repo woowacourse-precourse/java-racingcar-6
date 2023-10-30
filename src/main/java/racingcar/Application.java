2b0606504d332422cf27eac0e98f95a8d9737499
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 18:36:28 +0900

    refactor: modify method name for fit purpose

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import racingcar.core.generate.GenerateRacer;
 import racingcar.entity.Racer;
-import racingcar.entity.TargetScore;
 import racingcar.property.ValidateType;
 import racingcar.tool.InputTool;
 import racingcar.validation.ValidateForm;
@@ -14,10 +13,10 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         OutputView.outputForRaceStart();
-        race();
+        raceBefore();
     }
 
-    private static void race(){
+    private static void raceBefore(){
         String inputValue = InputTool.readLineByConsole();
         List<String> racerNameList = Arrays.asList(inputValue.split(" "));
         validateRacerNameList(inputValue);
@@ -35,4 +34,5 @@ public class Application {
                             )
                 );
     }
+
 }
