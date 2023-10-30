6004df1ebbb53901ff7e94f1fbf59a5eaea00f5a
wook <dong930112@gmail.com>
2023-10-31 02:42:38 +0900

    feat: 게임진항을 위한 View 클래스 작성 -inputCarName(): 자동차 이름 입력 기능 -inputAttemptsCount(): 시도횟수 입력 기능 -showTurnResult(): 게임진행 결과 출력 기능 -showGameWinner(): 게임우승자 출력 기능

---
+++ src/main/java/racingcar/View/GamePlayView.java
@@ -0,0 +1,47 @@
+package racingcar.View;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import java.util.List;
+
+public class GamePlayView {
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPTS_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String SHOW_GAME_RESULT = "최종우승자 : ";
+
+
+    public String inputCarName() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        return Console.readLine();
+    }
+
+    public int inputAttemptsCount() {
+        System.out.println(INPUT_ATTEMPTS_COUNT_MESSAGE);
+        String attemptStr = Console.readLine();
+        int attemptsCount = Integer.parseInt(attemptStr);
+
+
+        return attemptsCount;
+    }
+
+    public void showTurnResult(HashMap<String, String> carList) {
+        for(String key : carList.keySet()) {
+            System.out.println(key + " : " +  carList.get(key));
+        }
+    }
+
+    public void showGameWinner(List<String> winnerList) {
+        String winner = winnerList.get(0);
+
+        if(winnerList.size()==1) {
+            System.out.println(SHOW_GAME_RESULT + winner);
+        }
+        else {
+            for(int i=1; i< winnerList.size(); i++) {
+                winner += "," + winnerList.get(i);
+            }
+            System.out.println(SHOW_GAME_RESULT + winner);
+        }
+    }
+
+}
