8f5e551e7c7603373e596b1ee85f2a6871d15f77
minoooo119 <minoth0119@naver.com>
2023-10-30 22:49:56 +0900

    [FEAT] add Winner API to find winners

---
+++ src/main/java/racingcar/Winner.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Winner {
+    public List<String> winners=new ArrayList<>();
+
+    public Winner(Map<String,Integer>CarAndAdvance){
+        findWinner(CarAndAdvance);
+    }
+    public void findWinner(Map<String,Integer>CarAndAdvance){
+        List<String> winners=new ArrayList<>();
+        for (String key : CarAndAdvance.keySet()) {
+            if (winners.isEmpty()) {
+                winners.add(key);
+            } else {
+                turnaroundOrTie(CarAndAdvance,winners,key);
+            }
+        }
+        this.winners=winners;
+    }
+    private void turnaroundOrTie(Map<String,Integer>CarAndAdvance,List<String> originalWinner,String key){
+        int originalWinnersAdvance = CarAndAdvance.get(originalWinner.get(0));
+        if (CarAndAdvance.get(key) > originalWinnersAdvance) {
+            originalWinner.clear();
+            originalWinner.add(key);
+        } else if (CarAndAdvance.get(key) == originalWinnersAdvance) {
+            originalWinner.add(key);
+        }
+    }
+    public void printWinner(){
+        System.out.print("\n최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            if (i != winners.size() - 1) System.out.print(winners.get(i) + ", ");
+            else System.out.println(winners.get(i));
+        }
+    }
+}
