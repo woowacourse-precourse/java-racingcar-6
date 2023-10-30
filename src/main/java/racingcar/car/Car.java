cc462200e35bb711b43431a6e9ca7cd7ed399bb0
gmu1026 <curiouscomponent@gmail.com>
2023-10-31 01:42:57 +0900

    FEAT: 차 이동 기능 구현

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,7 +1,12 @@
 package racingcar.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MOVE_CONDITION_NUMBER = 4;
+
     private final String name;
+    private int moveCount = 0;
 
     public static Car createCar(String name) {
         return new Car(name);
@@ -13,5 +18,12 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public void move() {
+        int moveNumber = Randoms.pickNumberInRange(0,9);
+        if (moveNumber >= MOVE_CONDITION_NUMBER) {
+            moveCount++;
+        }
     }
 }
