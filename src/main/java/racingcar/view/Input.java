fccb5340b625f68d26b637fcae967a90caf60f07
LeeJJ07 <jkj9513@naver.com>
2023-10-30 17:19:43 +0900

    feat : 자동차 이름 입력 기능 구현
    입력 뷰에서 자동차 이름을 입력하는 기능 구현

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+	private final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+
+	public String carNames() {
+		System.out.println(INPUT_CAR_NAMES_MESSAGE);
+		return Console.readLine();
+	}
+
+}
