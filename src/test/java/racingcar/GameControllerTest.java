ceb51f25004a2cc52b7ac7b80d551320dea37d0e
Minjoo <m0522inju@gmail.com>
2023-10-30 18:20:35 +0900

    test: GameController 파라미터 변경, 메서드 이름 변경, 주석 추가

--- src/test/java/racingcar/GameControllerTest.java
+++ src/test/java/racingcar/GameControllerTest.java
@@ -12,15 +12,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.controller.GameController;
-import racingcar.view.ConsoleInputView;
-import racingcar.view.ConsoleOutputView;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class GameControllerTest {
     private GameController gameController;
 
     @BeforeEach
     void init() {
-        gameController = new GameController(new ConsoleInputView(), new ConsoleOutputView());
+        gameController = new GameController(
+                InputView.consoleInputView(),
+                OutputView.consoleOutputView()
+        );
     }
 
     @AfterEach
@@ -28,20 +31,20 @@ public class GameControllerTest {
         Console.close();
     }
 
-    private static Stream<Arguments> nameAndRoundProvider() {
+    private static Stream<Arguments> ProvideNameAndRound() {
         return Stream.of(
                 // 이름
-                Arguments.of("\n", "1"), // 이름 공백
-                Arguments.of(" ", "1"), // 이름 공백
-                Arguments.of("abc", "1"), // 이름 하나
-                Arguments.of("abc,abc", "1"), // 이름 중복
-                Arguments.of("abcdef", "1"), // 이름 5자 초과
+                Arguments.of("\n", "1"), // 1. 이름 공백
+                Arguments.of(" ", "1"), // 2. 이름 공백
+                Arguments.of("abc", "1"), // 3. 이름 하나
+                Arguments.of("abc,abc", "1"), // 4. 이름 중복
+                Arguments.of("abcdef", "1"), // 5. 이름 5자 초과
                 // 라운드
-                Arguments.of("abc,def", "\n"), // 라운드 공백
-                Arguments.of("abc,def", " "), // 라운드 공백
-                Arguments.of("abc,def", "0"), // 라운드 0
-                Arguments.of("abc,def", "abc"), // 라운드 숫자가 아님
-                Arguments.of("abc,def", "-1") // 라운드 범위 벗어남
+                Arguments.of("abc,def", "\n"), // 6. 라운드 공백
+                Arguments.of("abc,def", " "), // 7. 라운드 공백
+                Arguments.of("abc,def", "0"), // 8. 라운드 0
+                Arguments.of("abc,def", "abc"), // 9. 라운드 숫자가 아님
+                Arguments.of("abc,def", "-1") // 10. 라운드 범위 벗어남
         );
     }
     private void command(final String... args) {
@@ -50,7 +53,7 @@ public class GameControllerTest {
     }
 
     @ParameterizedTest
-    @MethodSource("nameAndRoundProvider")
+    @MethodSource("ProvideNameAndRound")
     void 인풋에_잘못된_값이_들어왔을_때_예외_발생(String nameInput, String roundInput) {
         command(nameInput, roundInput);
 
