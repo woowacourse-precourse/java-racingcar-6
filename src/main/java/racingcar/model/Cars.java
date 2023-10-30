0d7d797b751f82bdee4b902cfa368a6febebfec5
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 15:36:28 +0900

    feat(Cars): CarManager를 통해 생성된 Car 리스트를 저장한다.

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,16 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
