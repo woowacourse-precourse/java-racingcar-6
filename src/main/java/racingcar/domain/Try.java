16e2e5a81a5a393a365095df7e4c4864570c3ebf
gundorit <packdev937@gmail.com>
2023-10-30 20:16:58 +0900

    feat (Try) : 시도 횟수가 1 이상 인지 검증한다

--- src/main/java/racingcar/domain/Try.java
+++ src/main/java/racingcar/domain/Try.java
@@ -2,9 +2,12 @@ package racingcar.domain;
 
 public class Try {
 
+    private final int COUNT_CRITERION = 1;
+
     private int count;
 
     public Try(int count) {
+        validateCountValue(count);
         this.count = count;
     }
 
@@ -18,6 +21,12 @@ public class Try {
 
     public boolean isRaceOver() {
         return this.count == 0;
+    }
+
+    public void validateCountValue(int count) {
+        if (count < COUNT_CRITERION) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다");
+        }
     }
 
 }
