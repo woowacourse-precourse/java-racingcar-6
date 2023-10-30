6cc7982287d3c4e81674e41863b53ecb19e569fc
Laniel Park <Laniel8177@gmail.com>
2023-10-30 16:01:11 +0900

    feat : CycleInputValidator (Empty Class)

---
+++ src/main/java/racingcar/util/CycleInputValidator.java
@@ -0,0 +1,12 @@
+package racingcar.util;
+
+import racingcar.constant.Separator;
+
+public class CycleInputValidator {
+
+    private CycleInputValidator(){
+    }
+
+    public static void validate(String input) {
+    }
+}
