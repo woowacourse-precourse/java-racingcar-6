0260b999896734689bb96436d9e0bc1316a5ab33
inti0 <142914095+inti0@users.noreply.github.com>
2023-10-30 22:37:50 +0900

    TEST Car

---
+++ src/test/java/racingcar/car/carTest.java
@@ -0,0 +1,28 @@
+package racingcar.car;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.model.domain.Car;
+
+public class carTest {
+
+    @Test
+    void 자동차_테스트(){
+        Car.of(0,"j");
+        Car.of(0,"jo");
+        Car.of(0,"joh");
+        Car.of(0,"john");
+        Car.of(0,"joHnN");
+    }
+    @Test
+    void 자동차_테스트2(){
+        Assertions.assertThatThrownBy(()->
+                Car.of(0,"")).isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(()->
+                Car.of(0,"a b")).isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(()->
+                Car.of(0,"johnnn")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
