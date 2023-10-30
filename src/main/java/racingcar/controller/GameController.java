5b4eb01a6f834ca2d8ff5bd6881ae95c36f8a377
HYUNJUN\sajun <sajun28@naver.com>
2023-10-31 00:16:53 +0900

    Feat: "Controller 클래스 기능 분리"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -16,6 +16,7 @@ public class GameController {
     private final Attempt attemptNum = new Attempt();
     private final OutputView outputView = new OutputView();
     HashMap<Object, String> distance = new HashMap<Object, String>();
+    private int MAX_ENTRY = 0;
     public void start() {
         List<String> player = Arrays.asList(inputView.name().split(","));
         carName.isValid(player);
@@ -28,7 +29,8 @@ public class GameController {
         for (int i = 0; i < Integer.parseInt(attemptsNum); i++) {
             playGame(player, this.distance);
         }
-        showWinner(this.distance);
+        maxDistance(this.distance);
+        showWinner(this.MAX_ENTRY);
     }
 
     public void playGame(List player, HashMap distance) {
@@ -54,21 +56,24 @@ public class GameController {
         System.out.println();
     }
 
-    public void showWinner(HashMap distance) {
-        int maxEntry = 0;
-        Set<Map.Entry<String, String>> entrySet = distance.entrySet();
+    public void showWinner(int maxEntry) {
+        Set<Map.Entry<Object, String>> entrySet = distance.entrySet();
         List<String> winnerName = new ArrayList<>();
-        for (Map.Entry<String, String> entry : entrySet) {
-            if (entry.getValue().length() > maxEntry) {
-                maxEntry = entry.getValue().length();
-            }
-        }
-        for (Map.Entry<String, String> entry : entrySet) {
+        for (Map.Entry<Object, String> entry : entrySet) {
             if (entry.getValue().length() == maxEntry) {
-                winnerName.add(entry.getKey());
+                winnerName.add((String) entry.getKey());
             }
         }
         String winners = String.join(", ", winnerName);
         outputView.winner(winners);
+    }
+
+    public void maxDistance(HashMap distance) {
+        Set<Map.Entry<String, String>> entrySet = distance.entrySet();
+        for (Map.Entry<String, String> entry : entrySet) {
+            if (entry.getValue().length() > MAX_ENTRY) {
+                MAX_ENTRY = entry.getValue().length();
+            }
+        }
     }
 }
