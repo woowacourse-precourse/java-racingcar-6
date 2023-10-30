d58488f88b83d260bfd9366e8194201a570c2881
kssumin <201566@jnu.ac.kr>
2023-10-30 14:50:56 +0900

    feat : 자동차 이름은 중복될 수 있다 구현

---
+++ src/main/java/racingcar/domain/car/CarStorage.java
@@ -0,0 +1,11 @@
+package racingcar.domain.car;
+
+import java.util.List;
+
+public class CarStorage {
+	private final List<Car> cars;
+
+	public CarStorage(List<Car> cars) {
+		this.cars = cars;
+	}
+}
---
+++ src/test/java/racingcar/domain/car/CarStorageTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain.car;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarStorageTest {
+	@Test
+	@DisplayName("자동차 이름은 중복될 수 있다.")
+	void can_duplicated_car_name() {
+		// given
+		MoveState moveState = new MoveState(()-> 4);
+		Car carNameA1 = new Car(new CarName("A"), moveState);
+		Car carNameA2 = new Car(new CarName("A"), moveState);
+
+		// when & then
+		Assertions.assertDoesNotThrow(()->new CarStorage(List.of(carNameA1, carNameA2)));
+	}
+}
