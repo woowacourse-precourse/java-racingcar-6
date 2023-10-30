b899503726125f9982c2425a6d142f0b242169b1
MingyeomKim <mingyum119@naver.com>
2023-10-30 15:21:11 +0900

    feat(RaceController): 자동차의 이름을 입력받고 Cars 객체를 생성
     앞서 구현한 Cars 생성 메서드를 활용해 자동차의 이름을 입력받고 객체를 생성한다.

--- src/main/java/racingcar/presentation/RaceController.java
+++ src/main/java/racingcar/presentation/RaceController.java
@@ -1,16 +1,21 @@
 package racingcar.presentation;
 
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RaceController {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final OutputView outputView;
+    private final InputView inputView;
 
-    public RaceController(OutputView outputView) {
+    public RaceController(OutputView outputView, InputView inputView) {
         this.outputView = outputView;
+        this.inputView = inputView;
     }
 
     public void startRace() {
         outputView.printlnMessage(CAR_NAME_REQUEST_MESSAGE);
+        Cars cars = Cars.of(inputView.readInput());
     }
 }
