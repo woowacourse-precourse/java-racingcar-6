6fe2c4bf9f6709b21751079c8ec7173926c880be
jang010505 <jang010505@inu.ac.kr>
2023-10-31 02:30:13 +0900

    feat: validateCarNameInput 함수 개형 수정

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -3,7 +3,7 @@ package racingcar.validator;
 import java.util.List;
 
 public class Validator {
-    public List<String> validateCarNameInput() {
+    public static List<String> validateCarNameInput(String input) {
 
         return null;
     }
