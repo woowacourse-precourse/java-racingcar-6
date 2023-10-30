0ecd99c2af72154fcd551a2e5059272a65d0a321
Hwang In Tae <sjobs66@gmail.com>
2023-10-31 01:10:24 +0900

    feat: RacingCarGame validateCarName 구현
    5자 이상일 시 IllegalArgumentException을 발생시킨다.

--- src/main/java/racingcar/model/RacingCarGame.java
+++ src/main/java/racingcar/model/RacingCarGame.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class RacingCarGame implements Game {
     private static final int criterion = 4;
+    private static final int carNameLength = 5;
     private int attempts;
     private final List<Car> carList;
 
@@ -18,6 +19,12 @@ public class RacingCarGame implements Game {
             this.attempts = Integer.parseInt(attempts);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("횟수를 입력해주세요.", e);
+        }
+    }
+
+    public void validateCarName(String carName) {
+        if (carName.length() > carNameLength) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 }
