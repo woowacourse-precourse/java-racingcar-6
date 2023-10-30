d4d9137047c04b69ab2722e320735ec30f99eabc
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 14:45:15 +0900

    feat(Cars): 자동차들의 최고 순위를 계산하여 해당하는 자동차를 반환해주는 메서드 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import racingcar.constants.ExceptionMessage;
 import racingcar.util.NumberGenerator;
 
@@ -47,9 +50,39 @@ public record Cars(List<Car> cars) {
             car.moveToCar(value);
         }
     }
-
     @Override
     public List<Car> cars() {
         return Collections.unmodifiableList(cars);
+    }
+
+    public Map<String, Integer> getRacingStatus() {
+        Map<String, Integer> racingStatus = new HashMap<>();
+
+        for (Car car : cars()) {
+            racingStatus.put(car.getName(), car.getPosition());
+        }
+
+        return racingStatus;
+    }
+
+    public List<String> getTopRacers() {
+        Map<String, Integer> racingStatus = getRacingStatus(); // 레이싱 상태를 가져옴
+        int topPosition = getTopPosition(racingStatus); // 최상위 위치를 계산
+        return getTopRacerNames(racingStatus, topPosition); // 최상위 레이서 이름들을 가져와 반환
+    }
+
+    private int getTopPosition(Map<String, Integer> racingStatus) {
+        int topPosition = Integer.MIN_VALUE;
+        for (int position : racingStatus.values()) {
+            topPosition = Math.max(position, topPosition);
+        }
+        return topPosition;
+    }
+
+    private List<String> getTopRacerNames(Map<String, Integer> racingStatus, int topPosition) {
+        return racingStatus.entrySet().stream()
+                .filter(entry -> entry.getValue() == topPosition)
+                .map(Map.Entry::getKey)
+                .toList();
     }
 }
