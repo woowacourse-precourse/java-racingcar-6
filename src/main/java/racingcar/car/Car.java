ce8565f48cf6515e2bdd34acc9296cb0e759a717
sangdol <priceelees@gmail.com>
2023-10-30 15:29:18 +0900

    refactor(Car) : 자동차의 이름, 위치를 나타내는 원시값을 포장

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,31 +1,25 @@
 package racingcar.car;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-public record Car(String name, int location) {
-    private static final int MIN_NUMBER = 0;
+public record Car(CarName carName, CarLocation carLocation) {
     private static final int START_LOCATION = 0;
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_FOR_MOVE = 4;
-    private static final int MOVE_UNIT = 1;
 
     public Car(String name) {
-        this(name, START_LOCATION);
+        this(new CarName(name), new CarLocation(START_LOCATION));
     }
 
-    public Car moveForward() {
-        int randomNumber = getRandomNumber();
-        if (canMove(randomNumber)) {
-            return new Car(name, location + MOVE_UNIT);
-        }
-        return new Car(name, location);
+    public Car(String name, int location) {
+        this(new CarName(name), new CarLocation(location));
     }
 
-    private int getRandomNumber() {
-        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    public void moveForward(int randomNumber) {
+        carLocation.increaseLocation(randomNumber);
     }
 
-    private boolean canMove(int randomNumber) {
-        return randomNumber >= MIN_FOR_MOVE;
+    public String getCarName() {
+        return carName.name();
+    }
+
+    public int getCarLocation() {
+        return carLocation.getLocation();
     }
 }
