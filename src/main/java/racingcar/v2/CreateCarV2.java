1813b01140e4f83e090565babc95e17a0055e9d0
권장순 <jsoonworld@gmail.com>
2023-10-30 11:13:49 +0900

    feat(racingcar) : 가독성과 재활용성을 위해 예외처리를 메서드로 분리함. CreateCar#checkNameLength, CreateCar#checkCarCount

--- src/main/java/racingcar/v2/CreateCarV2.java
+++ src/main/java/racingcar/v2/CreateCarV2.java
@@ -6,18 +6,21 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 예외 처리를 별도로 빼면 코드 관리가 편하지만 간단한 예외 처리는 메서드 내부에서 처리하는 것이 좋다.
+ * 장단점이 모두 있으니 2주차 목표에 맞게 함수를 분리해서 구현.
+ */
+
 public class CreateCarV2 {
     public String input() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         return input;
     }
-    // TODO: 자동차 이름 예외 처리. 5자 초과, 중복, 공백
+
     public String[] carName(String input) {
         String[] carNames = input.split(",");
-        if (carNames.length > 10 || carNames.length < 2) {
-            throw new IllegalArgumentException("자동차는 2대 이상, 10대 이하만 가능합니다.");
-        }
+        checkCarCount(carNames);
         return carNames;
     }
 
@@ -25,14 +28,30 @@ public class CreateCarV2 {
         String[] carNames = carName(input);
         Map<String, Integer> stateMap = new HashMap<>();
         for (String car : carNames) {
+            checkNameLength(car);
             stateMap.put(car.trim(), 0);
         }
         return stateMap;
+    }
+
+    private void checkCarCount(String[] carNames) {
+        if (carNames.length > 10 || carNames.length < 2) {
+            throw new IllegalArgumentException("자동차는 2대 이상, 10대 이하만 가능합니다.");
+        }
+    }
+
+
+    public void checkNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public int carCount(Map stateMap) {
         int carCount = stateMap.size();
         return carCount;
     }
+
+
 
 }
