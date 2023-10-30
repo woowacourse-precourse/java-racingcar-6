a2912c72f66b49521e2f44bcbb51464db75b213f
shining013 <nevertheless113@naver.com>
2023-10-30 23:12:13 +0900

    이름 입력 검증 메소드1 수정
    -자동차 이름 입력 시 문자, 숫자, 쉼표 외에 다른 것을 입력했는지 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Application {
@@ -36,7 +37,11 @@ public class Application {
 
     public static void validateCarsName(String names) {
         boolean nameCheck = Pattern.matches("^[가-힣\\w]+[가-힣\\w" + "," + "]*[가-힣\\w]$",names);
+        if(!nameCheck) {
+            throw new IllegalArgumentException();
+        }
     }
+
     public static int makeRandomNumber() {
         int random;
         random = Randoms.pickNumberInRange(0,9);
