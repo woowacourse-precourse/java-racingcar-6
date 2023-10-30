234be5bf79d44123501690ea37de62ac52483489
toychip <dlawnsgud427@naver.com>
2023-10-30 09:30:04 +0900

    feat: 제공된 랜덤 함수를 사용하는 클래스 생성

---
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    private final int randomNumber;
+    private static final int MIN_NUM = 0;
+    private static final int MAX_NUM = 9;
+
+    private RandomNumberGenerator(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public RandomNumberGenerator createNumberGenerator() {
+        int randomNumber = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+        return new RandomNumberGenerator(randomNumber);
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+}
