c966283f7446b3379beec84a687430c9e260b2e7
dmdrk1414 <seungchan141414@gmail.com>
2023-10-30 10:04:20 +0900

    [Refator(racing)] 변수 추가 GameHost#winRaceCar

--- src/main/java/racingcar/domain/GameHost.java
+++ src/main/java/racingcar/domain/GameHost.java
@@ -13,21 +13,24 @@ public class GameHost {
     /**
      * 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다.
      *
-     * @param progressCarList   현제 진행중인 RaceCarList
-     * @param numOfWinCondition 승리를 위한 조건 (입력 진행숫자)
+     * @param progressRaceCarList 현제 진행중인 RaceCarList
+     * @param numOfWinCondition   승리를 위한 조건 (입력 진행숫자)
      * @return
      */
-    public List<RaceCar> winRaceCar(List<RaceCar> progressCarList, Integer numOfWinCondition) {
-        List<RaceCar> winRaceCarList = new ArrayList<>();
-        int cntOfMaxMove = getCntMaxMove(progressCarList);
+    public List<RaceCar> winRaceCar(List<RaceCar> progressRaceCarList, Integer numOfWinCondition) {
+        List<RaceCar> raceCarOfWinnerList = new ArrayList<>();
+        int cntOfMaxMove = getCntMaxMove(progressRaceCarList);
+        boolean isVictoryOfRace = true;
 
-        for (RaceCar raceCar : progressCarList) {
+        for (RaceCar raceCarOfCurrentProgress : progressRaceCarList) {
+            isVictoryOfRace = judgeStandard.isVictoryCondition(raceCarOfCurrentProgress, cntOfMaxMove);
+
             // 전진 카운터가 입력한만큼있으면
-            if (judgeStandard.isVictoryCondition(raceCar, cntOfMaxMove)) {
-                winRaceCarList.add(raceCar);
+            if (isVictoryOfRace) {
+                raceCarOfWinnerList.add(raceCarOfCurrentProgress);
             }
         }
-        return winRaceCarList;
+        return raceCarOfWinnerList;
     }
 
     /**
