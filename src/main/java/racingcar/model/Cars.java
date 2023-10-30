cb0ba4b8f54180d2042c2409cbab868dfd653a5f
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-31 02:45:10 +0900

    feat: 가장 높은 위치, 승리자 반환 함수 추가
    getMaxPosition = 가장 높은 위치 반환
    getWinners = getMaxPosition을 이용해 승리자 반환

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -5,6 +5,8 @@ import racingcar.validator.CarNameValidator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
@@ -24,12 +26,26 @@ public class Cars {
         }
     }
 
-    public static HashMap<String, Integer> carStates(){
-        HashMap<String,Integer> carState = new LinkedHashMap<>();
+    public static int getMaxPosition(){
+        return cars.stream()
+                .mapToInt(Car::getCarPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public static List<String> getWinners(){
+        return cars.stream()
+                .filter(car -> car.getCarPosition() == getMaxPosition())
+                .map(Car::getCarName)
+                .collect(Collectors.toList());
+    }
+
+    public static HashMap<String, Integer> carsStates(){
+        LinkedHashMap<String,Integer> carsState = new LinkedHashMap<>();
         for(Car car:cars){
-            carState.put(car.getCarName(), car.getCarPosition());
+            carsState.put(car.getCarName(), car.getCarPosition());
         }
-        return carState;
+        return carsState;
     }
 
 }
