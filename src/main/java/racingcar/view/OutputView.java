0c97443d300f7b72838f15420dfb160e0ad7cd8e
Jung Yunho <a01049048063@gmail.com>
2023-10-30 14:47:25 +0900

    feat(OutputView): 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,11 @@
 package racingcar.view;
 
+import java.util.stream.Collectors;
 import racingcar.constant.GameMessage;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.dto.ResultCar;
+import racingcar.dto.ResultCars;
 
 public class OutputView {
 
@@ -10,6 +13,7 @@ public class OutputView {
     private static final String SPACE = " ";
     private static final String COLON = ":";
     private static final String LINE_BREAK = "\n";
+    private static final String COMMAS = ",";
 
     private static final String VISUALIZED_STATUS = "-";
 
@@ -24,8 +28,11 @@ public class OutputView {
         System.out.println(GameMessage.REPEAT_NUMBER_REQUEST);
     }
 
-    public void printGameStatus(Cars cars) {
+    public void printGameStatusMessage() {
         System.out.println(GameMessage.EXECUTION_RESULT);
+    }
+
+    public void printGameStatus(Cars cars) {
         for (Car car : cars.getCars()) {
             System.out.printf("%s" + SPACE + COLON + SPACE + "%s" + LINE_BREAK, car.getName(),
                     appendStatus(car.getDistance()));
@@ -39,5 +46,15 @@ public class OutputView {
             sb.append(VISUALIZED_STATUS);
         }
         return sb.toString();
+    }
+
+    public void printRacingCarResult(ResultCars resultCars) {
+        System.out.print(GameMessage.FINAL_WINNER + SPACE + COLON + SPACE);
+
+        String resultCarNames = resultCars.getResultCars().stream()
+                .map(ResultCar::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(resultCarNames);
     }
 }
