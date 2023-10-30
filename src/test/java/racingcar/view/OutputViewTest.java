fbbd4a02899ca20c741a034eef239b2cdc97f8f0
k9want <zndwlrnjr26@daum.net>
2023-10-30 18:58:04 +0900

    test: 출력 - 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -10,6 +10,19 @@ import java.util.List;
 class OutputViewTest extends NsTest {
 
     @Test
+    @DisplayName("출력 - 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    public void testPrintCarNameInputMessage() {
+        //given
+        
+        //when
+        OutputView.printCarNameInputMessage();
+        String result = output();
+
+        //then
+        Assertions.assertThat(result).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    
+    @Test
     @DisplayName("출력 - 시도할 회수는 몇회인가요?")
     public void testPrintRacingCountInputMessage() {
         //given
