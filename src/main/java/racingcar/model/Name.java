879fd66101c389fcd7e3afa2601e4177f0a9937b
jeongjihun <wlgns2234@naver.com>
2023-10-30 14:22:19 +0900

    feat: Name을 생성하고 검증하는 기능 구현
    - Name 생성
    - 생성시 이름 크기가 5초과라면 예외발생

---
+++ src/main/java/racingcar/model/Name.java
@@ -0,0 +1,34 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public class Name {
+
+    private static final int MAX_NAME_SIZE = 5;
+
+    private final String name;
+
+    public Name(final String name) {
+        validateSize(name);
+        this.name = name;
+    }
+
+    private void validateSize(final String name) {
+        if (MAX_NAME_SIZE < name.length()) {
+            throw new IllegalArgumentException("[Error] 이름 크기는 5이하여야 합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
