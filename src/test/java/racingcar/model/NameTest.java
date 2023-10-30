223f774701d6f4eba55ea148b9bfd338a5c79dd8
devholic22 <hyunjoon.tech@gmail.com>
2023-10-30 14:58:22 +0900

    test: Name 길이 예외 테스트
    Name 생성 시 길이 예외 발생 테스트

--- src/test/java/racingcar/model/NameTest.java
+++ src/test/java/racingcar/model/NameTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.ExceptionMessage.NAME_LENGTH_EXCEPTION;
 import static racingcar.ExceptionMessage.NAME_VALUE_BLANK_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,5 +20,17 @@ public class NameTest {
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NAME_VALUE_BLANK_EXCEPTION.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"nickname", ""})
+    @DisplayName("이름 길이는 1자 이상 5자 이하여야 한다.")
+    void lengthExceptionTest(final String value) {
+        // given & when & then
+        assertThatThrownBy(() -> {
+            Name name = Name.from(value);
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NAME_LENGTH_EXCEPTION.getMessage());
     }
 }
