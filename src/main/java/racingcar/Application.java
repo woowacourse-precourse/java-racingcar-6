050f1a618267de39d1b1f7cdb3954c8e624bbfd8
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-30 21:05:35 +0900

    feat(application): add prompts for user interaction

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,9 +5,12 @@ import racingcar.game.Racing;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.printf("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String carNames = InputManager.readCarNames();
         Racing racing = new Racing(carNames);
+        System.out.printf("시도할 회수는 몇회인가요?\n");
         int numberOfAttempts = InputManager.readNumberOfAttempts();
+        System.out.printf("\n실행 결과\n");
         for (int i = 0; i < numberOfAttempts; i++) {
             racing.moveAllCarsForward();
         }
