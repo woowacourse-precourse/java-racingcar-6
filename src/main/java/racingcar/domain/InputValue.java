8b3a52a5e8d8c52f3620d24ab544a0f2eb20c4fd
junghunim07 <dlawndgns9@naver.com>
2023-10-30 15:15:51 +0900

    feat(InputValue): 입력값에 따른 예외 처리 checkInputException 메소드

--- src/main/java/racingcar/domain/InputValue.java
+++ src/main/java/racingcar/domain/InputValue.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -16,6 +17,7 @@ public class InputValue {
         ResultPrinter.startMessage();
         inputName = readLine();
         carName = stringToList(inputName);
+        checkInputException(carName);
         ResultPrinter.getTryMessage();
         inputCount = readLine();
         count = Integer.parseInt(inputCount);
@@ -23,5 +25,27 @@ public class InputValue {
 
     private List<String> stringToList(String input) {
         return Arrays.asList(input.split(","));
+    }
+
+    private void checkInputException(List<String> input) {
+        for (int i = 0; i < input.size(); i++) {
+            checkMoreThanFiveChars(input.get(i));
+        }
+
+        checkFrequency(carName);
+    }
+
+    private void checkMoreThanFiveChars(String input) {
+        if (input.length() >= 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkFrequency(List<String> carName) {
+        for (String input : carName) {
+            if (Collections.frequency(carName, input) > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
