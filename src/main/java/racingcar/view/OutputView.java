1ecb4c08f7270eb37a5a5ad88af4cc24f5295bf7
elyudwo <dudqk9696@naver.com>
2023-10-30 13:56:39 +0900

    [Feat] : 자동차 위치 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,26 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static racingcar.util.Utils.formatListToString;
 
 public class OutputView {
+
+    public void printResult(List<Car> cars) {
+        cars.forEach(car -> {
+            System.out.print(car.getName() + " : ");
+            printCarDistance(car);
+        });
+    }
+
+    private void printCarDistance(Car car) {
+        IntStream.range(0, car.getDistance())
+                .forEach(i -> System.out.print("-"));
+        System.out.println();
+    }
 
     public void printWinner(List<String> winners) {
         System.out.print("최종 우승자 : " + formatListToString(winners));
