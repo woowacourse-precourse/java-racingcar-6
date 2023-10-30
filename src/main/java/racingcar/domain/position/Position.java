40edc08de73931ce8557bc3b683a336cfc8a76e4
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-31 00:54:17 +0900

    feat: 자동차 위치 원시값을 포장하는 Position 구현

---
+++ src/main/java/racingcar/domain/position/Position.java
@@ -0,0 +1,46 @@
+package racingcar.domain.position;
+
+import java.util.Objects;
+
+public class Position implements Comparable<Position> {
+
+    private int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public void move(int weight) {
+        this.position += weight;
+    }
+
+    public String resolvePositionToString() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            result.append("-");
+        }
+        return result.toString();
+    }
+
+    @Override
+    public int compareTo(Position o) {
+        return Integer.compare(this.position, o.position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return this.position == position.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
