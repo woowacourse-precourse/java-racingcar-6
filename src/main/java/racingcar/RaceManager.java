bb09d62f9c774166ddb30a6d326be5d4643bcd16
jjaegii <hn06038@gmail.com>
2023-10-30 21:20:26 +0900

    feat: RaceManager 클래스 내 멤버변수, 생성자 추가

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,4 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceManager {
+    private List<Car> cars;
+    private int rounds;
+
+    public RaceManager(List<Car> cars, int rounds) {
+        this.cars = cars;
+        this.rounds = rounds;
+    }
 }
