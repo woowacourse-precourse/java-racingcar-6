f1c182cd41ff2b43aea433d0f298825d12e2b531
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 01:19:42 +0900

    feat: add print result function to output view

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,9 @@
 package racingcar.view;
 
 public class OutputView {
+    private final static String RESULT = "실행 결과";
+
+    public void printResult() {
+        System.out.println(RESULT);
+    }
 }
