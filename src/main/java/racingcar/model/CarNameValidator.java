c89d0a1ff17920647830f1c1f0c13c9c2532cb72
JoonKyo <joonkyolee@gmail.com>
2023-10-30 21:25:04 +0900

    feat(CarNameValidator): 문자열을 리스트로 변환하는 메서드 추가

--- src/main/java/racingcar/model/CarNameValidator.java
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -7,6 +7,7 @@ public class CarNameValidator {
     private static final String INPUT_HAS_ZERO_LENGTH = "입력 값에 길이가 0인 자동차가 존재합니다.";
     private static final String INPUT_HAS_OVER_FIVE_LENGTH = "입력 값에 길이가 5가 넘는 자동차가 존재합니다.";
     private static final String INPUT_HAS_DUPLICATED_NAME = "입력 값에 중복 값이 존재합니다.";
+    private static final String COMMA = ",";
     private static final String SPACE = " ";
     private List<String> carName;
 
@@ -14,7 +15,12 @@ public class CarNameValidator {
         if(hasSpace(carNames)){
             throw new IllegalArgumentException(INPUT_HAS_SPACE);
         }
+        carName = convertCarName(carNames);
         return carName;
+    }
+
+    private List<String> convertCarName(String carNames){
+        return List.of(carNames.split(COMMA));
     }
 
     private static boolean hasSpace(String carNames){
