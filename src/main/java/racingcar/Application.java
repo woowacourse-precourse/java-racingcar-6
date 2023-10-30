9f8c4a1fc94ca4455d1c0c4c6d587eb110bc9522
YoungYoon <duddbs1994@naver.com>
2023-10-30 22:23:27 +0900

    만들어진 기능으로 main 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,13 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            List<Car> cars = insertCarName();
+            int numberOfMoves = insertNumberOfMoves();
+            playGame(cars, numberOfMoves);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
 
     }
     //경주할 자동차 이름 입력
