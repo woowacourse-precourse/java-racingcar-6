1724cf1853c3cff0dd580493f299fd5a5ec447b9
toychip <dlawnsgud427@naver.com>
2023-10-30 10:43:11 +0900

    refactor: 자동차 이름 String -> Name 타입 변경 및 검증 메서드 Name 클래스로 이동

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,39 +4,22 @@ import java.util.List;
 
 public class Car {
 
-    private final String name;
+    private final Name name;
     private final CurrentLocation currentLocation;
 
-    private Car(String name, CurrentLocation currentLocation) {
+    private Car(Name name, CurrentLocation currentLocation) {
         this.name = name;
         this.currentLocation = currentLocation;
     }
 
-    public static Car createCar(String name) {
-        valid(name);
+    public static Car createCar(String inputName) {
+        Name name = Name.createName(inputName);
         CurrentLocation currentLocation = CurrentLocation.createCurrentLocation();
         return new Car(name, currentLocation);
     }
 
-    private static void valid(String userInputList) {
-        validLength(userInputList);
-        validBlankString(userInputList);
-    }
-
-    private static void validLength(String userInputList) {
-        if (userInputList.length() > 5) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validBlankString(String userInputList) {
-        if (userInputList.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     public String getName() {
-        return name;
+        return name.getName();
     }
 
     public int getCurrentLocation(){
