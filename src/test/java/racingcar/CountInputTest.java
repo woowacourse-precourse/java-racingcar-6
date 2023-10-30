dae8574f556cbdfe8d3a957ca2641ce46a70b548
SeongIl <biuld1234@gmail.com>
2023-10-30 12:30:29 +0900

    test: add 2.CountInputTest - 문자_공백_입력_시_예외_처리()

--- src/test/java/racingcar/CountInputTest.java
+++ src/test/java/racingcar/CountInputTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -13,13 +12,29 @@ public class CountInputTest {
         // given
         String userInput = "-1";
 
-        // when
-
         // then
         assertThatThrownBy(()-> Validator.checkPositiveNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1이상의 숫자를 입력하세요.");
     }
+
+    @Test
+    void 문자_공백_입력_시_예외_처리(){
+
+        // given
+        String userInputOne = "3pobi";
+        String userInputTwo = " ";
+
+        // then
+        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputOne))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력하세요.");
+
+        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputTwo))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력하세요.");
+    }
+
 
 
 }
