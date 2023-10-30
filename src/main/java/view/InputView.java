a877c9ec0dd31f41b048cb168d95f5512b3e113e
songsunkook <songsunkook@gmail.com>
2023-10-30 10:22:14 +0900

    feat: InputView 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,21 @@
 package view;
 
-public class InputView {
+import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public List<String> carName() {
+        String input = readLine();
+        return List.of(input.split(","));
+    }
+
+    public int roundNumber() {
+        String input = readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
