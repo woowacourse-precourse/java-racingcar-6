fc1f84c43dbac4d8f95714fbbfbfed39b247a21b
donghyun <kdhdh001@naver.com>
2023-10-31 00:03:25 +0900

    feat: inputTryCount 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,18 @@ import java.util.List;
 
 public class InputView {
     private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
+
     public static List<String> inputCarNames() {
         System.out.println(CAR_NAME_PROMPT);
         String input = Console.readLine();
         return Arrays.asList(input.split(","));
     }
+
+    public static int inputTryCount() {
+        System.out.println(TRY_COUNT_PROMPT);
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
 }
