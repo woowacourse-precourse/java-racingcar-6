ced98c763a30d3854409509566324da51911ff0d
choiejae <ejae8319@gmail.com>
2023-10-30 23:03:35 +0900

    feat(Name): RacingCar 의 멤버 변수 래퍼 클래스
    (Name): RacingCar 의 이름을 담는 멤버 변수, String name 을 wrapping 한 클래스

---
+++ src/main/java/racingcar/Name.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import org.junit.platform.commons.util.StringUtils;
+
+public record Name(String name) {
+
+    private static final int MAX_LENGTH = 5;
+
+    public Name {
+        validateInputName(name);
+    }
+
+    private void validateInputName(final String name) {
+        validateBlank(name);
+        validateOverLength(name);
+    }
+
+    private void validateOverLength(final String name) {
+        if (isOverLength(name)) {
+            throw new IllegalArgumentException("이름이 제한 길이를 초과하였습니다.");
+        }
+    }
+
+    private void validateBlank(final String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("빈 문자열은 이름이 될 수 없습니다.");
+        }
+    }
+
+    private boolean isOverLength(final String name) {
+        return name.length() > MAX_LENGTH;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/test/java/racingcar/NameTest.java
@@ -0,0 +1,65 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+
+    @Test
+    void 게임에서_허용한_길이의_문자열로_이름을_생성할_수_있다() {
+        // given
+        String input = "abcde";
+
+        // when
+        Name name = new Name(input);
+
+        // then
+        assertEquals("abcde", name.name());
+    }
+
+    @Test
+    void 게임에서_허용하지_않은_길이의_문자열로_이름을_생성하면_예외가_발생한다() {
+        // given
+        String input = "abcdef";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름이 제한 길이를 초과하였습니다.");
+    }
+
+    @Test
+    void 빈_문자열로_이름을_생성하면_예외가_발생한다() {
+        // given
+        String input = "";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열은 이름이 될 수 없습니다.");
+    }
+
+    @Test
+    void 공백으로_이름을_생성하면_예외가_발생한다() {
+        // given
+        String input = " ";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열은 이름이 될 수 없습니다.");
+    }
+
+    @Test
+    void null_문자열로_이름을_생성하면_예외가_발생한다() {
+        // given
+        String input = null;
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열은 이름이 될 수 없습니다.");
+    }
+}
