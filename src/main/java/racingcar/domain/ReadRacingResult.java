bac538568af59ce1ae95cb424a1121a6c4e48952
doka <posmane5@gmail.com>
2023-10-30 21:13:00 +0900

    feat(RacingRuleMaker) : readHashMap 메소드 추가
    readHashMap - LinkedHashMap 자료형을 for 반복문을 통해서 Map.Entry를 매번 추출후 문자열 출력하는 구조로 구성. 자동차의 이동거리를 "-" 로 표현하기 위해 toDash 메소드 추가 필요.

---
+++ src/main/java/racingcar/domain/ReadRacingResult.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ReadRacingResult {
+
+    public void readHashMap(LinkedHashMap<String, Integer> racingResult) {
+
+        for (Map.Entry<String, Integer> entry : racingResult.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+    }
+
+
+
+}
