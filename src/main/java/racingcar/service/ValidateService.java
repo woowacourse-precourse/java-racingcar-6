d298bb67c5956d76546d3c70dba71613ca7cd61d
kangyuseok <kingyuseok@naver.com>
2023-10-31 02:03:50 +0900

    Feat: 입력값 유효성 검사에 대한 클래스 생성

--- src/main/java/racingcar/service/ValidateService.java
+++ src/main/java/racingcar/service/ValidateService.java
@@ -1,2 +1,16 @@
-package racingcar.service;public class ValidateService {
+package racingcar.service;
+
+public class ValidateService {
+    public ValidateService(){}
+
+    public static void isEmpty(final String carName){ //아무것도 입력안했을 경우
+        if(carName.isEmpty())throw new IllegalArgumentException();
+    }
+
+
+    public static void isValidLength(final String carName){ //자동차 이름이 1이상 5이하가 아닐 경우
+        int carNameLength = carName.length();
+        if(carNameLength <= 0 || carNameLength > 5)
+            throw new IllegalArgumentException();
+    }
 }
