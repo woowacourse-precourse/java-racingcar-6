a098d9d5c7b404fa6b0d5723989a4021c1061971
olsohee <dlthgml0108@daum.net>
2023-10-30 11:46:16 +0900

    refactor(RandomNumberGenerator): 매직 넘버 제거, 상수 처리

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public final class RandomNumberGenerator {
 
+    private static final int MIN = 0;
+    private static final int MAX = 9;
+
     public static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN, MAX);
     }
 }
