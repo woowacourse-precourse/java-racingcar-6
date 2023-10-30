0ecbafb8ed4b291fd8a4c74ff8d7ff3a6bc18eaa
joyj10 <micool1030@gmail.com>
2023-10-31 07:36:24 +0900

    # 시도 횟수 입력 문구 출력 및 사용자 입력 - 유효성 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,6 +15,10 @@ public class Application {
         PrintUtils.print(Message.GAME_START);
         String carNameListInput = Console.readLine();
         validCarName(carNameListInput);
+
+        PrintUtils.print(Message.TRY_COUNT);
+        String tryCountInput = Console.readLine();
+        validTryCount(tryCountInput);
     }
 
     private static void validCarName(String input) {
@@ -35,6 +39,23 @@ public class Application {
         long invalidCount = Arrays.stream(carNameArr).filter(i -> i.length() > MAX_CAR_NAME_SIZE || i.length() == 0).count();
         if (invalidCount > 0) {
             throw new IllegalArgumentException(ErrorMessage.MAX_CAR_NAME_SIZE_EXCEEDED.getDescription() + MAX_CAR_NAME_SIZE);
+        }
+    }
+
+    private static void validTryCount(String tryCountInput) {
+        if (Strings.isNullOrEmpty(tryCountInput)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_EMPTY.getDescription());
+        }
+
+        int count = 0;
+        try {
+            count = Integer.parseInt(tryCountInput);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER.getDescription());
+        }
+
+        if (count <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.ONE_OVER_NUMBER.getDescription());
         }
     }
 }
