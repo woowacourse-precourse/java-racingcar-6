fde3d67c4a3867486fc6c9274768d5b6f178e5f8
ohjuyoungg <ejrm12@naver.com>
2023-10-31 03:50:18 +0900

    refactor(InputView) : Refactoring code 클래스 명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.GameController;
 import racingcar.validation.InputValidation;
 
-public class Inputview {
+public class InputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String GAME_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
