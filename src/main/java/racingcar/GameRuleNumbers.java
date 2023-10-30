95f57a580fe7dc1299447102ee875615c2c5bae3
Dayoming <jdayoung1204@naver.com>
2023-10-30 20:32:18 +0900

    feat: 게임 내에서 사용할 규칙 숫자와 메세지 작성

---
+++ src/main/java/racingcar/GameRuleNumbers.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class GameRuleNumbers {
+    public static int RANDOM_NUMBER_MIN = 0;
+    public static int RANDOM_NUMBER_MAX = 9;
+    public static int FORWARD_CONDITION = 4;
+    public static int NAME_LENGTH_LIMIT = 5;
+}
