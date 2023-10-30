84ea924c7bfaa4d122024181f744393d55445448
dealim <136411152+dealim@users.noreply.github.com>
2023-10-30 11:45:28 +0900

    [update] README.md 기능 요구사항 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
