3a48d975cc1f517838db4ded6e0e054f78d20aba
bbbbooo <qlqlzbxkzl@naver.com>
2023-10-30 20:31:30 +0900

    refactor(CompareRaceTest) : 테스트 메소드 변경 및 RacingCar 객체 생성 메소드명 변경
    랜덤 숫자가 4 이상일 때 전진하는 것을 확인할려면 Mockito를 써야하는데 최대한 지양하고자 전진하는지 확인하는 것으로 변경
    RacingCar 생성 메소드 의미를 더 명확하게 하기 위하여 createRacingCars로 변경

--- src/test/java/racingcar/service/CompareRaceTest.java
+++ src/test/java/racingcar/service/CompareRaceTest.java
@@ -3,14 +3,13 @@ package racingcar.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.RacingCar;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CompareRaceTest {
 
@@ -21,22 +20,25 @@ class CompareRaceTest {
         compareRace = new CompareRace();
     }
 
-    @DisplayName("자동차가 랜덤한 숫자 중 4 이상일때만 전진하는지")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7 ,8, 9})
-    void moveCarsTest(Integer randomNumber) {
+    @DisplayName("자동차가 정상적으로 전진하는지")
+    @Test
+    void moveCarsTest() {
         // given
         String inputCarNames = "pobi,woni";
-        List<RacingCar> racingCars = RacingCar.fromInputCarName(inputCarNames);
+        List<RacingCar> racingCars = RacingCar.createRacingCars(inputCarNames);
 
         // when
         compareRace.moveCars(racingCars);
 
         // then
         for (RacingCar racingCar : racingCars) {
-            assertTrue(racingCar.isMoveCountLessThanOrEqualTo(randomNumber));
+            String carStatus = racingCar.carStatus();
+            Integer nameLength = racingCar.printCarName().length();
+
+            assertTrue(carStatus.length() - nameLength - 3 >= 0);
         }
     }
+
 
     @DisplayName("이동횟수가 가장 높은 우승차를 찾아내는지")
     @Test
@@ -44,7 +46,8 @@ class CompareRaceTest {
         // given
         String inputCarNames = "pobi,woni,bemo,roro";
         Integer[] carMovement = {2, 3, 1, 3};
-        List<RacingCar> racingCars = RacingCar.fromInputCarName(inputCarNames);
+
+        List<RacingCar> racingCars = RacingCar.createRacingCars(inputCarNames);
 
         IntStream.range(0, racingCars.size()).forEach(i -> {
             for (int j = 0; j < carMovement[i]; j++) {
