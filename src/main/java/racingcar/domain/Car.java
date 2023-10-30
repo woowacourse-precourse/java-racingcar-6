307ec1fb6d29c3956b9f66639ff999718f947ac9
hil679 <hil679922@gmail.com>
2023-10-31 01:46:19 +0900

    Refactor: Change magic number to constant

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final static int FIRST_FORWARD_NUM = 0;
+    private final static int RANDOM_MIN_VALUE = 0;
+    private final static int RANDOM_MAX_VALUE = 9;
 
     private String name;
     private int forwardNum;
@@ -31,6 +33,6 @@ public class Car {
     }
 
     public void initRandomNum() {
-        this.randomNum = Randoms.pickNumberInRange(0, 9);
+        this.randomNum = Randoms.pickNumberInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE);
     }
 }
