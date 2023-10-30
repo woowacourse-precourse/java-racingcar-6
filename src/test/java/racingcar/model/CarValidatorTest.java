51a1bb4d27027b78b5097163ab62831a4e638bb5
sinminseok12 <sinminseok12@naver.com>
2023-10-30 16:34:27 +0900

    refactor(CarValidator): 자동차 이름이 공백일 경우 예외처리 기능 추가

--- src/test/java/racingcar/model/CarValidatorTest.java
+++ src/test/java/racingcar/model/CarValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class CarValidatorTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "123456"})
+    @ValueSource(strings = {" ", "", "123456"})
     void 자동차_이름범위를_벗어났을_경우_예외처리를_한다(String name) {
         Assertions.assertThatThrownBy(() -> CarValidator.validate(name))
                 .isInstanceOf(IllegalArgumentException.class);
