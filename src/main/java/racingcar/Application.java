a2667c6b7c2ad34e68f30f50d9e4fcc1fdf20255
soyoonjeong2328 <soyounjeong6276@gmail.com>
2023-10-30 15:38:34 +0900

    feat: 이름이 5글자 이상일 경우 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputCarName();
+    }
+
+    public static void inputCarName(){
+       String input = Console.readLine();
+       String[] name = input.split(",");
+
+       try {
+           if(!checkLength(name)) throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+       }catch (IllegalArgumentException e){
+           System.out.println(e.getMessage());
+       }
+
+    }
+
+    private static boolean checkLength(String[] arr){
+        boolean flag = false;
+
+        for(String s : arr){
+            flag = s.length() <= 5;
+        }
+        return flag;
     }
 }
