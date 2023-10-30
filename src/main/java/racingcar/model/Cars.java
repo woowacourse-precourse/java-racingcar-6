d8447391270063bd7b860140aee530891c7de7f7
Han <yulmam1@naver.com>
2023-10-30 11:13:32 +0900

    feat(Cars) : 자동차의 이름이 5글자를 넘어갈 경우 예외처리 하는 기능 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -10,12 +10,25 @@ public class Cars {
     List<String> carsName;
     Map<String, Integer> carsScore;
 
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+
     public Cars() {
         carsScore = new HashMap<String, Integer>();
     }
 
     public void addCar(String carsName){
         this.carsName = Arrays.asList(carsName.split(","));
+        validate(this.carsName);
         this.carsName.forEach(carName -> carsScore.put(carName, 0));
+    }
+
+
+    private void validate(List<String> carsName) {
+        validateCarsNameLength(carsName);
+    }
+
+    private void validateCarsNameLength(List<String> carsName) {
+        if(carsName.stream().anyMatch(carName->carName.length() > CAR_NAME_MAX_LENGTH))
+            throw new IllegalArgumentException("문자열의 길이가 5를 넘어갔습니다.");
     }
 }
