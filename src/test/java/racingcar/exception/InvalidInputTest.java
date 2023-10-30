5ca8c7f567076b4f02a8549a5d42793b92433478
Yang-soeun <soeun8636@naver.com>
2023-10-30 15:31:46 +0900

    [test]: 주어진 횟수가 숫자가 아닌 경우 예외 테스트 추가

--- src/test/java/racingcar/exception/InvalidInputTest.java
+++ src/test/java/racingcar/exception/InvalidInputTest.java
@@ -2,12 +2,15 @@ package racingcar.exception;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputView;
 
 class InvalidInputTest {
+    private void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
 
     @DisplayName("자동차 이름이 6글자 이상이면 예외가 발생한다.")
     @Test
@@ -22,14 +25,18 @@ class InvalidInputTest {
                 .hasMessage("이름은 최대 5글자 입니다.");
     }
 
-    @DisplayName("")
+    @DisplayName("주어진 횟수가 숫자가 아니면 예외가 발생한다.")
     @Test
     void NotIntegerValueException(){
         //given
+        InputView inputView = new InputView();
+        systemIn("a");
 
-        //when
+        //when //then
+        assertThatThrownBy(()-> inputView.inputTryNumber())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("횟수는 숫자 여야 합니다.");
 
-        //then
     }
 
 }
