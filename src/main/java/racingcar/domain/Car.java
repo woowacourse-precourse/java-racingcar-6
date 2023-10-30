1143c0214af1b4bfb48b6a1adc0b7c4c6537c31a
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-30 18:37:05 +0900

    feat : 자동차 생성 이 클래스는 자동차를 생성해주는 클라스 입니다. 자동차를 생성할 때, 사용자의 잘못된 입력 값에 대하여 모든 예외처리를 진행해주었습니다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,60 @@
+package racingcar.domain;
+
+import racingcar.view.InputView;
+
+public class Car {
+
+    private String carName;
+
+    private int position;
+
+    public Car(String carName) {
+        isCarNameLengthOver(carName);
+        isNotAlphaBetAndComma(carName);
+        isCarNameBlank(carName);
+        isCarNameNull(carName);
+        this.carName = carName;
+        this.position = 0;
+    }
+
+    public Car(String carName, int position) {
+        isCarNameLengthOver(carName);
+        isNotAlphaBetAndComma(carName);
+        isCarNameBlank(carName);
+        isCarNameNull(carName);
+        this.carName = carName;
+        this.position = position;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    private void isCarNameLengthOver(String carName){
+        if(carName.length() > InputView.MAX_NAME_LENGTH){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isNotAlphaBetAndComma(String carName){
+        if (!carName.matches("[a-z,]")){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCarNameBlank(String carName){
+        if (carName.isBlank()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCarNameNull(String carName){
+        if (carName == null){
+            throw new IllegalArgumentException();
+        }
+    }
+}
