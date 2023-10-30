8e5173c21075518184a2ce147cd1ecdaa721a6fc
PINGPINGYEE <owb7994@nate.com>
2023-10-30 13:23:58 +0900

    RacingSystem Class 최대 이동 거리 확인 후 우승자 선정, 출력

--- src/main/java/racingcar/domain/RacingSystem.java
+++ src/main/java/racingcar/domain/RacingSystem.java
@@ -38,12 +38,41 @@ public class RacingSystem {
         }
     }
 
+    public List<String> makeListOfWinner(int maxMoveState, List<Car> cars) {
+        List<String> listOfWinners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getMoveState() == maxMoveState) {
+                listOfWinners.add(car.getName());
+            }
+        }
+        return listOfWinners;
+    }
+
+    public int findMaxMoveState(int maxMoveState) {
+        for (Car car : cars) {
+            if (car.getMoveState() > maxMoveState) {
+                maxMoveState = car.getMoveState();
+            }
+        }
+        return maxMoveState;
+    }
+
+    public void raceWinner() {
+        int max = -1;
+        int maxMoveState = findMaxMoveState(max);
+
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", makeListOfWinner(maxMoveState, cars)));
+    }
+
     public void startApp() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         this.carNames = user.inputCarName();
         System.out.println("시도할 회수는 몇회인가요?");
         this.movement = user.inputNumberOfCarMovement();
+        System.out.println("실행 결과");
         makeCarsInstance();
         raceProgress();
+        raceWinner();
     }
 }
