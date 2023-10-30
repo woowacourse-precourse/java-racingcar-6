99db411b3746fff2d38a31272f1cbf82af2e1b75
oiler831 <qudwls831@gmail.com>
2023-10-30 15:08:23 +0900

    쉼표를 기준으로 이름을 구분
    쉼표를 기준으로 차량의 이름을 구분하는 메소드를 만들었다. split을 사용하였다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,14 +3,20 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGame {
+    public static String[] carName;
+
     private RacingGame(){
         throw new IllegalArgumentException();
-    };
+    }
     public static void startGame(){
         inputCarName();
     }
     private static void inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameLine = Console.readLine();
+        divideCarNameLine(carNameLine);
+    }
+    private static void divideCarNameLine(String carNameLine){
+        carName = carNameLine.split(",");
     }
 }
