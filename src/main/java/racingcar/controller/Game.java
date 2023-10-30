e90f7611ba52fe75371c4771d6d321047eda8e13
HongYeseul <ghd8119@naver.com>
2023-10-31 07:59:38 +0900

    :sparkles: feat: Game 클래스 내 구상한 메서드 추가

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,4 +1,25 @@
 package racingcar.controller;
 
 public class Game {
+    private Integer round;
+
+    public void run(){
+
+    }
+
+    private void askGameRounds(){
+
+    }
+
+    private void makeRandomNumber(){
+
+    }
+
+    private void mainLogic(){
+
+    }
+
+    private void result(){
+
+    }
 }
