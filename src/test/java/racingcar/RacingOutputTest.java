15ce34fff2d39e4f74cbe30b5f102de8cf16deca
su-giana <giananews@gmail.com>
2023-10-30 17:21:55 +0900

    # 테스트 케이스 제작
    # 제목
    우승자를 뽑는 로직 검증하기
    
    ## 세부 설명
    - 당첨자가 1명일 때, List<String> winnerNames를 반환하는지 확인하는 메소드 구현
    
    - 당첨자가 여러명일 때, List<String> winnerNames를 반환하는지 확인하는 메소드 구현

---
+++ src/test/java/racingcar/RacingOutputTest.java
@@ -0,0 +1,47 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+
+public class RacingOutputTest extends NsTest
+{
+    RacingOutput ro = new RacingOutput();
+
+    @Test
+    public void getResultOfWinner_단일_해답() {
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car("car1"));
+        carList.add(new Car("car2"));
+        carList.add(new Car("car3"));
+        carList.get(0).setDistance(1);
+
+        List<String> ans = ro.getResultOfWinner(carList);
+        assertThat(ans).contains("car1");
+    }
+
+    @Test
+    public void getResultOfWinner_복수_해답() {
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car("car1"));
+        carList.add(new Car("car2"));
+        carList.add(new Car("car3"));
+
+        List<String> ans = ro.getResultOfWinner(carList);
+        assertThat(ans).contains("car1", "car2", "car3");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
