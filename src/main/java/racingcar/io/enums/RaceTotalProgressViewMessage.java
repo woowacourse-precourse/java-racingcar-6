92c921ed61d2ca26bd5d7f9dbf47325669a1ae29
aka-nick <bechalrang@gmail.com>
2023-10-30 23:30:09 +0900

    추가(RaceTotalProgressViewMessage) : RaceTotalProgressView에 대한 enum 클래스 추가
    RaceTotalProgressViewMessage

---
+++ src/main/java/racingcar/io/enums/RaceTotalProgressViewMessage.java
@@ -0,0 +1,40 @@
+package racingcar.io.enums;
+
+import racingcar.generic.RacerProgress;
+
+public enum RaceTotalProgressViewMessage {
+
+    NEW_LINE(System.lineSeparator()),
+    BLANK_AND_NEW_LINE(System.lineSeparator().repeat(2)),
+    MESSAGE_RACE_RESULT("실행 결과"),
+    FORMAT_NAME_AND_MILEAGE("%s : %s"),
+    PROGRESS_MILEAGE("-"),
+    MESSAGE_WINNERS("최종우승자 : "),
+    DELIMITER_WINNERS(", "),
+    ;
+
+    private final String message;
+
+    RaceTotalProgressViewMessage(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return message;
+    }
+
+    public static String makeRacingRecord(RacerProgress racerProgress) {
+        return String.format(FORMAT_NAME_AND_MILEAGE.get(),
+                racerProgress.name(),
+                getProgressBarOn(racerProgress));
+    }
+
+    private static String getProgressBarOn(RacerProgress racerProgress) {
+        return PROGRESS_MILEAGE.get().repeat(racerProgress.mileage());
+    }
+
+    public static String makeWinnersMessage(String totalWinners) {
+        return NEW_LINE.get() + MESSAGE_WINNERS + totalWinners;
+    }
+
+}
