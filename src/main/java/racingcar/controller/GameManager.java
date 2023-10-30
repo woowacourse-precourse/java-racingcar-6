c14cf090fffb627997c229e4d9440e9caa13dced
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:29:01 +0900

    [feat] Add GameManager class fields

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -1,4 +1,12 @@
 package racingcar.controller;
 
+import racingcar.view.UserView;
+
 public class GameManager {
+    // 타 클래스 변수
+    private UserView userView;
+    private User user;
+
+    // GameManager 변수
+    private Integer tryCount;
 }
