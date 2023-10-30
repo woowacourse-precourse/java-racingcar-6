9c1457429f78dd397c75a93a62f978b2f06dbbc3
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 16:43:03 +0900

    refactor: modify method access public -> private

--- src/main/java/racingcar/validation/ValidateForm.java
+++ src/main/java/racingcar/validation/ValidateForm.java
@@ -1,17 +1,31 @@
 package racingcar.validation;
 
+import racingcar.property.ValidateType;
+
 import static racingcar.validation.NameValidation.*;
 
 public class ValidateForm {
 
-    public static void verifyForRacerName(String inputValue){
+    public static void validateForValidateType(ValidateType validateType,String inputValue){
+        if (validateType == ValidateType.NAME){
+            verifyForRacerName(inputValue);
+            return;
+        }
+
+        if (validateType == ValidateType.NAME_LIST){
+            verifyForRacerNameList(inputValue);
+            return;
+        }
+    }
+
+    private static void verifyForRacerName(String inputValue){
         verifyForEmptyValue(inputValue);
         verifyForSpaceValue(inputValue);
         verifyForRacerNameIsLengthNotOver(inputValue);
         verifyForRacerNameIsEngOrKor(inputValue);
     }
 
-    public static void verifyForRacerNameList(String racerNameList){
+    private static void verifyForRacerNameList(String racerNameList){
 
     }
 }
