f8b8d7ba6a45f465b0890f1bd0658df90f601a7a
dmdrk1414 <seungchan141414@gmail.com>
2023-10-30 14:19:15 +0900

    [Style(racing)] 주석 수정

--- src/main/java/racingcar/utill/Utill.java
+++ src/main/java/racingcar/utill/Utill.java
@@ -36,23 +36,12 @@ public class Utill {
     public static Integer inputNum() {
         String inputStr = readLine();
 
-//        - [ ] 공백 입력 검사
-        ValidException.isValidBlankCheck(inputStr);
-
-//        - [ ] 공백 포함 검사
-        ValidException.isValidIncludeBlank(inputStr);
-
-//        - [ ] 문자열 포함 검사
-        ValidException.isValidIncludeString(inputStr);
-
-//        - [ ] 특수문자 포함 검사
-        ValidException.isValidIncludeSpecialString(inputStr);
-
-//        - [ ] 0 입력 검사
-        ValidException.isValidZeroCheck(inputStr);
-
-//        - [ ] 양수 입력 검사
-        ValidException.isValidPositiveCheck(inputStr);
+        ValidException.isValidBlankCheck(inputStr); // 공백 입력 검사
+        ValidException.isValidIncludeBlank(inputStr); // 공백 포함 검사
+        ValidException.isValidIncludeString(inputStr); // 문자열 포함 검사
+        ValidException.isValidIncludeSpecialString(inputStr); // 특수문자 포함 검사
+        ValidException.isValidZeroCheck(inputStr); // 0 입력 검사
+        ValidException.isValidPositiveCheck(inputStr); // 양수 입력 검사
 
         Integer inputNum = parseInteger(inputStr);
         return inputNum;
