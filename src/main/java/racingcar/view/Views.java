f2bd3479c95669f59b0ac8f150b1ea7a1ffc9ec5
Seyeon <wjdtpdus828@naver.com>
2023-10-31 00:38:59 +0900

    feat(Views): 자동차의 이름을 출력해주는 printCarName 메소드와 자동차의 위치를 출력해주는 printLocation 줄바꿈을 출력하는 printLineBreak 메소드 구현

--- src/main/java/racingcar/view/Views.java
+++ src/main/java/racingcar/view/Views.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 public class Views {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
@@ -13,5 +15,16 @@ public class Views {
     }
     public void printResultMessage(){
         System.out.println(RESULT_NESSAGE);
+    }
+    public void printCarName(Car car){
+        System.out.print(car.getCarName() + " : ");
+    }
+    public void printLocation(Car car){
+        for(int i =0; i<car.getCarLocation();i++){
+            System.out.print("-");
+        }
+    }
+    public void printLineBreak(){
+        System.out.println();
     }
 }
