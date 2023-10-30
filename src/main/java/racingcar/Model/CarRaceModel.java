f147bba16fdd086b6a8ee3b54ed2b38993197979
wook <dong930112@gmail.com>
2023-10-31 02:46:33 +0900

    feat: 게임진항을 위한 Model 클래스 작성 -findWinner(): 게임 우승자를 찾는 메서드 -findTopMovement(): 가장 많이 움직인 자동차의 칸 수를 찾는 메서드 -parsCarMovement(): 자동차가 움직인 칸을 움직인 칸 수로 파싱하는 메서드 -showGameWinner(): 자동차가 움직인 칸 수를 저장하는 메서드 -controlCarMovement(): 자동차의 진행과 멈춤을 결정하는 메서드

---
+++ src/main/java/racingcar/Model/CarRaceModel.java
@@ -0,0 +1,70 @@
+package racingcar.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.List;
+
+public class CarRaceModel {
+
+    public List<String> findWinner(HashMap<String, String> carList) {
+        List<String> winner = new ArrayList<>();
+        int topMovement = findTopMovement(carList);
+        HashMap<String, Integer> parsedCarList = parsCarMovement(carList);
+
+        for(Entry<String, Integer> entry : parsedCarList.entrySet()) {
+            if(entry.getValue() == topMovement) {
+                winner.add(entry.getKey());
+            }
+        }
+        return winner;
+    }
+
+    public int findTopMovement(HashMap<String, String> carList) {
+
+        HashMap<String, Integer> parsedCarList = parsCarMovement(carList);
+
+        int winnerValue = 0;
+
+        for(String key : parsedCarList.keySet()){
+            if(parsedCarList.get(key) >= winnerValue) {
+                winnerValue = parsedCarList.get(key);
+            }
+        }
+        return winnerValue;
+    }
+
+    public HashMap<String, Integer> parsCarMovement(HashMap<String, String> carList) {
+        HashMap<String, Integer> parsingCarList = new HashMap<>();
+
+        for(String key : carList.keySet()) {
+            int carMovementValue = carList.get(key).length();
+
+            parsingCarList.put(key, carMovementValue);
+        }
+
+        return parsingCarList;
+    }
+
+    public HashMap<String, String> saveCarMovement(HashMap<String, String> carList) {
+
+        for(Entry<String, String> entry : carList.entrySet()) {
+            if(controlCarMovement()) {
+                String carName = entry.getKey();
+                String carMovement = entry.getValue() + "-";
+
+                carList.put(carName, carMovement);
+            }
+        }
+        return carList;
+    }
+
+    public boolean controlCarMovement() {
+        if(Randoms.pickNumberInRange(0,9)>4) {
+            return true;
+        }
+        return false;
+    }
+
+}
