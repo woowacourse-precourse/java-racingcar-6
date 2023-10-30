b1aa3b037367d38a978f3bfa24b2c0aa8504489a
BDlhj <akk808@naver.com>
2023-10-30 17:21:00 +0900

    feat: Implement getUserInput()

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarGame {
     static void printGameStartMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    static String getUserInput() {
+        return Console.readLine();
     }
 }
