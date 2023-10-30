3dff16e156bb3e412385e949c6df47607a5cb77d
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 16:23:25 +0900

    feat(Car): 자동차 전진 함수 추가
    - 0 ~ 9 사이의 숫자를 랜덤으로 생성하여 생성한 숫자가 4 이상이면 이동거리를 1 증가시키는 함수 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,25 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.util.Constants;
+
 public class Car {
-    private String name;
+    private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void moveForward() {
+        int randomNumber = Randoms.pickNumberInRange(Constants.MIN_INPUT_RANGE, Constants.MAX_INPUT_RANGE);
+        if (randomNumber >= Constants.MIN_MOVE_THRESHOLD) {
+            increasePosition();
+        }
+    }
+
+    public void increasePosition() {
+        this.position++;
     }
 }
