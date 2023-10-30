92ef5a77ec489348af7125f8ee13bfe65ac5ac25
naayoung <ask5046a@gmail.com>
2023-10-30 16:04:32 +0900

    feat: write output statement

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,14 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] value = inputValue.getInputValue();
         System.out.println("시도할 회수는 몇회인가요?");
-        String time = inputTime.getInputTime();
-        
+        Integer time = inputTime.getInputTime();
+        System.out.println("실행 결과");
+
+        for(int i=0; i<time;i++){
+            for(int j=0; j< value.length; j++){
+                System.out.println(value[j] + " :");
+            }
+        }
+        System.out.println("최종 우승자 : ");
     }
 }
