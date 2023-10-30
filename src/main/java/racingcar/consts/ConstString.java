e0d78111d7256e550538ff668a29a58a16837dc9
gyu_hyeok <jjookki33@gmail.com>
2023-10-31 01:33:36 +0900

    feat:상수를 변수로 표현 - 하드코딩을 피하기 위함

---
+++ src/main/java/racingcar/consts/ConstString.java
@@ -0,0 +1,22 @@
+package racingcar.consts;
+
+public class ConstString {
+    public static final String DELIMITER = ",";
+    public static final String DASH = "-";
+    public static final String COLON = " : ";
+
+    public static final String START_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_NUMBER = "시도할 횟수는 몇회인가요?";
+    public static final String MAX_SIZE_ERROR = "입력한 자동차의 길이가 5이상입니다. 게임을 종료합니다.";
+    public static final String MIN_CAR_ERROR = "입력된 차가 두대 미만입니다. 게임을 종료합니다.";
+    public static final String DUPLICATED_VALUE_ERROR = "중복된 값이 존재합니다. 게임을 종료합니다";
+    public static final String WRONG_SEPERATOR_ERROR = "쉼표(, )가 아닌 구분자가 입력되었습니다. 게임을 종료합니다. ";
+    public static final String WRONG_INT_INPUT = "숫자가 아닌 잘못된 값이 입력되었습니다. 게임을 종료합니다.";
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public static final int MAX_SIZE = 5;
+    public static final int MIN_RANDOM_SIZE = 0;
+    public static final int MAX_RANDOM_SIZE = 9;
+    public static final int MOVE_SIZE_TRUE = 4;
+    public static final int GO_AHEAD = 1;
+}
