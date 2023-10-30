4f106870f9945a77677aae537f6bc0b327e5e724
HiiWee <wpdlzhf159@gmail.com>
2023-10-30 18:58:10 +0900

    fix: 오탈자 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
     }
 
     public static String inputRaceCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println("시도할 횟수는 몇회인가요?");
         return Console.readLine()
                 .trim();
     }
