5402467dc6470444f2ac11e6f3d4eba7db70b0a9
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 14:10:21 +0900

    test(CarTest) : Car 클래스에 대한 테스트 구현
    임의의 수에 의해 전진 혹은 정지를 하는 기능에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    void 임의의_수가_4_이상일_경우_전진() {
+        NumberGenerator testNumberGenerator = new NumberGenerator() {
+            @Override
+            public int generateNumber() {
+                return 4;
+            }
+        };
+
+        Car car = new Car("test", testNumberGenerator);
+        car.moveOrStay();
+
+        assertEquals(car.getDistance(), 1L);
+    }
+
+    @Test
+    void 임의의_수가_3_이하일_경우_정지() {
+        NumberGenerator testNumberGenerator = new NumberGenerator() {
+            @Override
+            public int generateNumber() {
+                return 3;
+            }
+        };
+
+        Car car = new Car("test", testNumberGenerator);
+        car.moveOrStay();
+
+        assertEquals(car.getDistance(), 0L);
+    }
+}
