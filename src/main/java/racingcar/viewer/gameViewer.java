64d4ef2749ef1688735ec2048e1055115290d90b
Kim JunHyun <jjag1015@naver.com>
2023-10-31 02:30:05 +0900

    feat(racingcar): console에 출력되는 viewer 기능

--- src/main/java/racingcar/viewer/gameViewer.java
+++ src/main/java/racingcar/viewer/gameViewer.java
@@ -1,24 +1,40 @@
 package racingcar.viewer;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 
-public class gameViewer {
+public class GameViewer {
 
     public void printAskCarNames() {
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printAskTheNumberOfTry() {
-
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void printRaceState() {
+    public void printStartOfResult() {
+        System.out.println("실행 결과");
+    }
 
+    public void printRaceState(List<Car> cars) {
+        for (Car car : cars) {
+            String steps = "";
+            for (int i = 0; i < car.getStep(); i++) {
+                steps += "-";
+            }
+            System.out.println(car.getName() + " : " + steps);
+        }
+        System.out.println();
     }
 
     public void printWinners(List<Car> winners) {
-
+        List<String> names = new ArrayList<>();
+        for (Car car : winners) {
+            names.add(car.getName());
+        }
+        System.out.println("최종 우수자 : " + String.join(", ", names));
     }
 
 }
