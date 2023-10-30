e250740b32301972803eaa3d3abdb3aed78b60e3
InJun2 <os2864@naver.com>
2023-10-31 02:34:24 +0900

    :sparkles: Car, Cars 객체 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,30 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.exception.ExceptionMessage;
+
 public class Car {
+    private final static int MAX_NAME_LENGTH = 5;
+    private final static int NO_MOVE_CAR_NUMBER = 3;
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        validateCarName(name);
+        this.name = name;
+        distance = 0;
+    }
+
+    private void validateCarName(String name) {
+        if(name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_MAX_LENGTH_EXCEEDED
+                    .getErrorMessage());
+        }
+    }
+
+    public void move() {
+        if(Randoms.pickNumberInRange(0, 9) > NO_MOVE_CAR_NUMBER) {
+            this.distance ++;
+        }
+    }
 }
--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,4 +1,15 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class Cars {
+    List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
