b1c5550b275058872c3846695e4537d4c0746ce4
이재현 <fanta4715@naver.com>
2023-10-30 14:18:11 +0900

    feat: CarDto 생성
    CarController가 View와 통신할 때, Model을 직접적으로 주지 않기 위해 생성한 Dto

---
+++ src/main/java/racingcar/CarDto.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class CarDto {
+    private String carName;
+    private int movedDistance;
+
+    public CarDto(String carName, int movedDistance) {
+        this.carName = carName;
+        this.movedDistance = movedDistance;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getMovedDistance() {
+        return movedDistance;
+    }
+}
