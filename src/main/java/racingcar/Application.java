2c1e0fbd46576ae6fa69a81cb0ccd14009820310
oxdjww <oxdjww@gmail.com>
2023-10-30 22:33:32 +0900

    style : 우테코 코드 컨벤션 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,9 @@ package racingcar;
 import racingcar.controller.RacingCarGame;
 
 public class Application {
-    public static void main(String[] args) {
-        RacingCarGame game = RacingCarGame.createRacingCarGame();
-        game.run();
-    }
+
+  public static void main(String[] args) {
+    RacingCarGame game = RacingCarGame.createRacingCarGame();
+    game.run();
+  }
 }
