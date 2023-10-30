b25e429cc6cb8b9e1a339b2c2b14f79669536c7c
Sully <2dcoder@naver.com>
2023-10-30 20:47:56 +0900

    test(UtilsTest): repeatDash()에 대한 dash가 정상적으로 생성되는지 확인하는 테스트 코드 추가

--- src/test/java/racingcar/UtilsTest.java
+++ src/test/java/racingcar/UtilsTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class UtilsTest {
 
-    @DisplayName("정상적으로 ','로 파싱이 된다.")
+    @DisplayName("parseCarName()로 정상적으로 ','로 파싱이 된다.")
     @Test
     void parse_carName() {
         // given
@@ -19,6 +19,19 @@ class UtilsTest {
 
         // then
         assertThat(carNames).containsExactly("i", "am", "sully");
+    }
+
+    @DisplayName("repeatDash()로 position의 수만큼 '-'가 생성된다.")
+    @Test
+    void repeat_dash() {
+        // given
+        int position = 5;
+
+        // when
+        Utils.repeatDash(position);
+
+        // then
+        assertThat(Utils.repeatDash(position)).isEqualTo("-----");
     }
 
 }
