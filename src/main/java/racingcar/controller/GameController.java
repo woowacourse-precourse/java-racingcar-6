13d7bdcc636d88323eb072cef7ac175c9acf41a4
JuSeong1130 <jsmoon1130@naver.com>
2023-10-30 13:13:46 +0900

    refactor(Controller) : run 메서드 리팩토링
    trycount를 RacingGame에게 보내 한번에 진행하는거였지만 한번진행하고 출력 한번진행하고 출력이라는 요구사항으로 인해 레이싱게임에게 보내고 다시 racingGame.isRun()으로 확인하고 진행하는것으로 바꿈

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -21,22 +21,24 @@ public class GameController {
     public void run() {
         List<Car> cars = createCars();
         int tryCount = InputView.askTryCount();
-        racingGame.progress(cars, tryCount);
-        OutputView.printCurrentSituation(cars);
-        OutputView.printWinner(cars);
+        while (racingGame.isRun(tryCount)) {
+            racingGame.progress(cars, tryCount);
+            OutputView.printCurrentSituation(cars);
+        }
+        OutputView.printWinner(racingGame.getWinner(cars));
     }
 
     private List<Car> createCars() {
         String str = InputView.askCarNames();
         validatePattern(str);
-        List<String> carNames = stringToList(str);
+        List<String> carNames = stringToList(str); //currentProgressCount maxProgressCount
         return carNames.stream()
                 .map(name -> new Car(new Name(name)))
                 .collect(Collectors.toList());
     }
 
     private void validatePattern(String str) {
-        String regex = "(\\w{1,})(,\\w)*";
+        String regex = "\\w+(,\\w+)*";
         if (!Pattern.matches(regex, str)) {
             throw new IllegalArgumentException("형식이 맞지 않습니다");
         }
@@ -45,6 +47,5 @@ public class GameController {
     private List<String> stringToList(String str) {
         return Arrays.stream(str.split(","))
                 .collect(Collectors.toList());
-
     }
 }
