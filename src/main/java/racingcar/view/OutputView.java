ccb9c131274c2a5180fccc1847595053fc5bc8ee
lotuxsoo <tndus5383@gmail.com>
2023-10-30 11:28:49 +0900

    fix: OutputView의 매직리터럴 및 접근제어자 실수 처리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,10 @@
 package racingcar.view;
 
 public class OutputView {
-    public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String SHOW_RESULT_STRING = "실행 결과";
+    private static final String SHOW_WINNER_STRING = "실행 결과";
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -13,10 +15,10 @@ public class OutputView {
     }
 
     public static void printShowResultString() {
-        System.out.println("실행 결과");
+        System.out.println(SHOW_RESULT_STRING);
     }
 
     public static void printShowWinnerString() {
-        System.out.print("최종 우승자 : ");
+        System.out.print(SHOW_WINNER_STRING);
     }
 }
