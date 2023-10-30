94ee55930b17009bd047ea722b2806417e0cc33a
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 19:45:11 +0900

    feat: 우승자 판별 메서드
    우승자를 List에 넣어 반환하는 메서드

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,7 +4,6 @@ import racingcar.model.Car;
 import racingcar.model.User;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -29,5 +28,21 @@ public class RacingCarController {
                 carMap.put(car,carMap.getOrDefault(car,0)+1);
             }
         }
+    }
+    private List<String> winner() {
+        List<String> winnerList = new ArrayList<>();
+        int maxResult = Integer.MIN_VALUE;
+        for (String car : carMap.keySet()) {
+            if(carMap.get(car) > maxResult){
+                winnerList.clear();
+                maxResult = Math.max(maxResult,carMap.get(car));
+            }
+            if(isCoincide(maxResult,carMap.get(car)))
+                winnerList.add(car);
+        }
+        return winnerList;
+    }
+    private boolean isCoincide(int maxResult, int currentNumber){
+        return maxResult == currentNumber;
     }
 }
