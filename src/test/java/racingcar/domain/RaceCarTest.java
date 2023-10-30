66daf45f47030a16184d522d3cea05530aa16e61
박승찬 <seungchan141414@gmail.com>
2023-10-30 17:22:05 +0900

    [Test(racing)] 자동차의 이동을 제어한다. RaceCarTest

--- src/test/java/racingcar/domain/RaceCarTest.java
+++ src/test/java/racingcar/domain/RaceCarTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RaceCarTest {
+    private final int MOVE = 4;
+    private final int STOP = 0;
     private RaceCar raceCar;
 
     @BeforeEach
@@ -14,14 +17,22 @@ class RaceCarTest {
         raceCar = new RaceCar("Test");
     }
 
+    private void moveRaceCar(RaceCar raceCar) {
+        raceCar.movementControlCar(MOVE);
+    }
+
     @DisplayName("자동차의 이동을 제어한다.")
     @Test
     void movementControlCar() {
-        // given
-
         // when
+        raceCar.movementControlCar(MOVE);
+
+        int resultNum = raceCar.getCntMovementOfCar();
+        String rasultStr = raceCar.toStringMoveState();
 
         // then
+        assertThat(resultNum).isEqualTo(1);
+        assertThat(rasultStr).isEqualTo("-");
     }
 
     @DisplayName("자동차의 움직여도 되는 조건 이 맞는가. (4이상, 9이하)")
