a2ff20bde9e0657cfa6b3f54a94f62a71a8ef630
hongseokhodev <hongseokhodev@gmail.com>
2023-10-31 00:08:38 +0900

    test: 공통으로 사용하는 변수를 전역으로 추출

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -15,14 +15,14 @@ import racingcar.domain.car.RandomNumberGenerator;
 import racingcar.domain.car.Rule;
 
 public class GameServiceTest {
+    private final Rule rule = new DecideRule();
+    private final Generator generator = new RandomNumberGenerator();
+    private final Decider decider = Decider.of(rule, generator);
     @Test
     void 시도_횟수보다_적게_이동시켰다면_게임을_종료하지_않는다() {
         //given
         Name name = new Name("car");
         Car car = Car.withName(name);
-        Rule rule = new DecideRule();
-        Generator generator = new RandomNumberGenerator();
-        Decider decider = Decider.of(rule, generator);
         Cars cars = Cars.of(List.of(car), decider);
         Attempt attempt = Attempt.withNumber(1);
         GameService gameService = GameService.of(cars, attempt);
@@ -39,9 +39,6 @@ public class GameServiceTest {
         //given
         Name name = new Name("car");
         Car car = Car.withName(name);
-        Rule rule = new DecideRule();
-        Generator generator = new RandomNumberGenerator();
-        Decider decider = Decider.of(rule, generator);
         Cars cars = Cars.of(List.of(car), decider);
         Attempt attempt = Attempt.withNumber(1);
         GameService gameService = GameService.of(cars, attempt);
