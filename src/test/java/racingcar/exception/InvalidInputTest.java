e36e985658d7360c2b69d575428f77c0668aa6e7
Yang-soeun <soeun8636@naver.com>
2023-10-30 15:15:25 +0900

    [test]: 이름이 6글자 이상인 경우 예외 테스트 추가

---
+++ src/test/java/racingcar/exception/InvalidInputTest.java
@@ -0,0 +1,25 @@
+package racingcar.exception;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
+
+class InvalidInputTest {
+
+    @DisplayName("자동차 이름이 6글자 이상이면 예외가 발생한다.")
+    @Test
+    void longNameException(){
+        //given
+        InputView inputView = new InputView();
+        String names = "hellow";
+
+        //when //then
+        assertThatThrownBy(() -> inputView.splitNames(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 최대 5글자 입니다.");
+    }
+
+}
