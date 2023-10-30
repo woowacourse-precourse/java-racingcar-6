860a20dc09e2c05b41bc68430c9f1d6e20054852
kimwanyoung <dhks2869@gmail.com>
2023-10-30 13:51:51 +0900

    feat : Name 객체 5글자 이상 검증 로직 추가

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -6,9 +6,16 @@ public class Name {
 
     public Name(String name) {
         this.name = name;
+        validateOverLength();
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateOverLength() {
+        if (name.length() > 4) {
+            throw new IllegalArgumentException("5글자 미만 이름만 입력 가능합니다.");
+        }
     }
 }
