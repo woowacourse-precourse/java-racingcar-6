4fd7543463786925878cf02b33d8e9baefdd9b60
oxdjww <oxdjww@gmail.com>
2023-10-31 00:52:45 +0900

    refactor : ViewNotice 상수들 public화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,18 @@
 package racingcar.view;
 
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
   public static String getCarNamesFromPlayer() {
-    ViewNotice.printGetCarNameNotice();
+    System.out.println(ViewNotice.GET_CARNAME_NOTICE);
     return Console.readLine();
   }
 
   public static String getNumberOfRoundsFromPlayer() {
-    ViewNotice.printGetRoundNotice();
+    System.out.println(ViewNotice.GET_ROUND_NOTICE);
     return Console.readLine();
   }
 }
+
--- src/main/java/racingcar/view/ViewNotice.java
+++ src/main/java/racingcar/view/ViewNotice.java
@@ -2,19 +2,8 @@ package racingcar.view;
 
 public class ViewNotice {
 
-  private static final String GET_CARNAME_NOTICE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-  private static final String GET_ROUND_NOTICE = "시도할 회수는 몇회인가요?";
+  public static final String GET_CARNAME_NOTICE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+  public static final String GET_ROUND_NOTICE = "시도할 회수는 몇회인가요?";
   public static final String ROUND_RESULT_NOTICE = "실행 결과";
 
-  public static void printGetCarNameNotice() {
-    System.out.println(GET_CARNAME_NOTICE);
-  }
-
-  public static void printGetRoundNotice() {
-    System.out.println(GET_ROUND_NOTICE);
-  }
-
-  public static void printRoundResultNotice() {
-    System.out.println(ROUND_RESULT_NOTICE);
-  }
 }
