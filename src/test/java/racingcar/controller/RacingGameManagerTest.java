8d6a746d82d54df06c1e5139ff93e91f4ce5787b
정찬영 <jcy0342@gmail.com>
2023-10-30 10:47:09 +0900

    refactor: GameManagerTest의 메서드 이름을 영어로 바꾸고 @DisplayName 사용

--- src/test/java/racingcar/controller/RacingGameManagerTest.java
+++ src/test/java/racingcar/controller/RacingGameManagerTest.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import racingcar.View.InputView;
@@ -11,8 +12,9 @@ import racingcar.model.Cars;
 
 public class RacingGameManagerTest {
 
+    @DisplayName("시도 횟수가 1이하인 경우 예외 발생")
     @Test
-    void 시도할_횟수가_1보다_작다면_예외발생() {
+    void tryCountIsNegativeOrZero() {
         //Given
         String inputNegativeNumber = "-1";
         String inputZero = "0";
@@ -23,8 +25,9 @@ public class RacingGameManagerTest {
         assertThrows(IllegalArgumentException.class, () -> racingGameManager.tryCountValidation(inputZero));
     }
 
+    @DisplayName("시도 횟수가 숫자가 아닌 경우 예외 발생")
     @Test
-    void 시도할_횟수가_숫자가_아니라면_예외발생() {
+    void tryCountIsNotInteger() {
         //Given
         String inputValue = "sd";
         RacingGameManager racingGameManager = new RacingGameManager();
@@ -33,8 +36,9 @@ public class RacingGameManagerTest {
         assertThrows(IllegalArgumentException.class, () -> racingGameManager.tryCountValidation(inputValue));
     }
 
+    @DisplayName("시도 횟수만큼 전진 메서드 호출")
     @Test
-    void 시도_횟수_만큼_자동차_이동_함수_호출(){
+    void callMoveMethodByTryCount(){
         // Given
         int tryCount = 3;
         Cars spyCars = Mockito.spy(new Cars());
