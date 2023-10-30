9db1832cba4932cbd45c717e10f78c43146a55e7
DaehoSon <thseogh135@gmail.com>
2023-10-30 10:33:43 +0900

    feat: 전진_정지 테스트 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,18 +2,21 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Game {
 
     public final String ANNOUNCEMENT_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final String ANNOUNCEMENT_INPUT_MOVE_COUNT = "시도할 회수는 몇회인가요?";
     public final String ANNOUNCEMENT_EXECUTION_RESULT = "실행 결과";
+    public final String ANNOUNCEMENT_WINNER = "최종 우승자 : ";
 
     public String[] carNameArray;
     public int[] carLocationArray;
     public int numberOfCar;
     public int moveCount;
-    private int maxLocation;
 
     public Game() {
         init();
@@ -25,7 +28,28 @@ public class Game {
             carRuns();
             System.out.println();
         }
-        System.out.println("최종 우승자 : " + "pobi");
+        String[] winnerArray = getWinnerArray();
+        printWinner(winnerArray);
+    }
+
+    private void printWinner(String[] winnerArray) {
+        System.out.print(this.ANNOUNCEMENT_WINNER);
+        int i = 0;
+        for (; i < winnerArray.length - 1; i++) {
+            System.out.print(winnerArray[i] + ",");
+        }
+        System.out.println(winnerArray[i]);
+    }
+
+    private String[] getWinnerArray() {
+        List<String> winnerList = new ArrayList<>();
+        int maxLocation = Arrays.stream(this.carLocationArray).max().getAsInt();
+        for (int i = 0; i < this.numberOfCar; i++) {
+            if (this.carLocationArray[i] == maxLocation) {
+                winnerList.add(carNameArray[i]);
+            }
+        }
+        return winnerList.toArray(new String[winnerList.size()]);
     }
 
     private void carRuns() {
