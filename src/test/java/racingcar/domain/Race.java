e4db6747e9e99fdc1c7d6df82e2c841254530765
jae woo Jung <awfjol2008@naver.com>
2023-10-30 18:14:46 +0900

    feat: 이동 거리가 서로 같은 경우를 포함하여 최대로 이동한 플레이어를 우승자 리스트를 반환하는 기능 구현

--- src/test/java/racingcar/domain/Race.java
+++ src/test/java/racingcar/domain/Race.java
@@ -3,10 +3,18 @@ package racingcar.domain;
 import racingcar.domain.car.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Race {
 
     private List<Car> cars;
+
+    public List<Car> findWinnerCars() {
+        Car carWithMaxDistance = getCarWithMaxDistance();
+        return cars.stream()
+                .filter(car -> car.isCarWithMaxDistance(carWithMaxDistance))
+                .collect(Collectors.toList());
+    }
 
     private Car getCarWithMaxDistance() {
         Car maxDistanceCar = cars.get(0);
