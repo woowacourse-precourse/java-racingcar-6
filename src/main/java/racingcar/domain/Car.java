13cf7d56219a7dacd2eb6b59d1229634faf3c85d
hee9841 <lee98417716@gmail.com>
2023-10-31 05:59:22 +0900

    feat(racingcar): 자동차 객체 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,5 +2,20 @@ package racingcar.domain;
 
 
 public class Car {
+    private final String name;
+    private int location = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void goForward() {
+        this.location++;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
 
 }
