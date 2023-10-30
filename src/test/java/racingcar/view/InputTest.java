8c766660e4b1e9bd4e666d420e3f9344200396cc
namtndus <rnrmf0721@gmail.com>
2023-10-31 00:14:57 +0900

    test(InputTest): InputCarName함수에서 예외를 발생하는 경우를 테스트

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
@@ -33,5 +34,15 @@ class InputTest {
         int number = input.InputMovingNumber();
 
         assertThat(Integer.parseInt(text)).isEqualTo(number);
+    }
+
+    @Test
+    void Input_자동차의_시도할_횟수에서_예외_발생(){
+        String text = "5sfsfsf7";
+        InputStream in = new ByteArrayInputStream(text.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> input.InputCarName())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
