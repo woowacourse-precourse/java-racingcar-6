207f740f8e6d0bf3bc55a6a516436395ef47b534
Jongwoo Han <jongwooo.han@gmail.com>
2023-10-30 12:54:58 +0900

    refactor: replace term '랜덤값' with '무작위 값'

--- src/test/java/racingcar/domain/formula/MoveFormulaTest.java
+++ src/test/java/racingcar/domain/formula/MoveFormulaTest.java
@@ -9,8 +9,8 @@ import racingcar.domain.generator.MockNumberGenerator;
 public class MoveFormulaTest {
 
   @Test
-  @DisplayName("랜덤값이 4 이상이면 전진한다.")
-  void 랜덤값이_4_이상이면_전진한다() {
+  @DisplayName("무작위 값이 4 이상이면 전진한다.")
+  void 무작위_값이_4_이상이면_전진한다() {
     // given
     var mockNumberGenerator = new MockNumberGenerator(4);
     var moveFormula = new MoveFormula(mockNumberGenerator);
@@ -23,8 +23,8 @@ public class MoveFormulaTest {
   }
 
   @Test
-  @DisplayName("랜덤값이 3 이하이면 멈춘다.")
-  void 랜덤값이_3_이하이면_멈춘다() {
+  @DisplayName("무작위 값이 3 이하이면 멈춘다.")
+  void 무작위_값이_3_이하이면_멈춘다() {
     // given
     var mockNumberGenerator = new MockNumberGenerator(3);
     var moveFormula = new MoveFormula(mockNumberGenerator);
