a84a3830e8c7807b65d4013b6d6d9708a68868c6
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-30 13:23:41 +0900

    refactor(racingcar): get rid of useless library

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import camp.nextstep.edu.missionutils.Console;
 import static racingcar.domain.Constants.*;
