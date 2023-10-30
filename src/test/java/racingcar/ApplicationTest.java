6681f49ff019d9aff582d5b95beb913934096f36
ycis5013@naver.com <ycis5013@naver.com>
2023-10-31 02:02:11 +0900

    test(ApplicationTest) : 각 메서드의 테스트코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,11 +18,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -28,6 +31,50 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도횟수에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_저장() {
+        GameManager gameManager = GameManager.getInstance();
+        List<Car> list = gameManager.setCarList("car1,car2");
+
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(0).name).isEqualTo("car1");
+        assertThat(list.get(1).name).isEqualTo("car2");
+    }
+
+    @Test
+    void 시도횟수_저장() {
+        GameManager gameManager = GameManager.getInstance();
+        int roundNum = gameManager.setRoundNum("3");
+
+        assertThat(roundNum).isEqualTo(3);
+    }
+
+    @Test
+    void 시도횟수_끝났는지_확인() {
+        GameManager gameManager = GameManager.getInstance();
+        assertThat(gameManager.checkGameFinished(0)).isTrue();
+        assertThat(gameManager.checkGameFinished(3)).isFalse();
+    }
+
+    @Test
+    void 우승여부_우승자_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("hay,ohyo", "2");
+                    assertThat(output()).contains("hay : --", "ohyo : -", "최종 우승자 : hay");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, STOP
         );
     }
 
