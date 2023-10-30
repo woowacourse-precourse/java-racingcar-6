97199129b1e4572c8996979425474b19bf0b8484
gundorit <packdev937@gmail.com>
2023-10-30 16:39:45 +0900

    feat (OutputView) : 시도할 횟수를 입력받는 문구를 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,11 @@ package racingcar.view;
 public class OutputView {
 
     private static final String RACE_START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉽표(,) 기준으로 구분)";
-
-    public static void printRaceStartMessage() {
+    private static final String RACE_TRY_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    public static void printRaceStart() {
         System.out.println(RACE_START_MESSAGE);
+    }
+    public static void printRaceTryCount(){
+        System.out.println(RACE_TRY_COUNT_MESSAGE);
     }
 }
