6625b765b14df678c4a17fb9334ba117bd996a17
kimwanyoung <dhks2869@gmail.com>
2023-10-31 08:24:34 +0900

    feat : "최종 결과" 출력 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,11 +6,16 @@ import model.Cars;
 
 public class OutputView {
 
+    private static final String RESULT = "실행 결과";
     private static final String FINAL_WINNER = "최종 우승자";
     private static final String COMMA = ",";
     private static final String STATUS_SEPARATOR = " : ";
 
     private OutputView() {
+    }
+
+    public static void displayResult() {
+        System.out.println(RESULT);
     }
 
     public static void displayFinalWinner(Cars cars) {
