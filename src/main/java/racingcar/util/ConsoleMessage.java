d549c646842bb73770dc9c6c9d5e07f2d9307510
junOTL <naqp99@gmail.com>
2023-10-30 22:54:23 +0900

    refactor: 입력 로직 하드코딩 제거

---
+++ src/main/java/racingcar/util/ConsoleMessage.java
@@ -0,0 +1,6 @@
+package racingcar.util;
+
+public class ConsoleMessage {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_ATTEMPT_COUNT = "시도할 회수는 몇회인가요?";
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,18 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.ConsoleMessage;
 import racingcar.util.Validator;
 
 public class InputView {
     public static String[] inputCarName() {
-        OutputView.printlnConsoleMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        OutputView.printlnConsoleMessage(ConsoleMessage.INPUT_CAR_NAME);
         String[] inputs = splitByComma(Console.readLine());
         return Validator.isValidCarName(inputs);
     }
 
     public static int inputAttemptCount() {
-        OutputView.printlnConsoleMessage("시도할 회수는 몇회인가요?");
+        OutputView.printlnConsoleMessage(ConsoleMessage.INPUT_ATTEMPT_COUNT);
         String input = Validator.isValidAttemptCount(Console.readLine());
         return Integer.parseInt(input);
     }
