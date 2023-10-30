215e2d5749a175ec5a513520e569342e884d8794
yang <hjyang@epience.com>
2023-10-30 15:59:06 +0900

    🎯feat(view/InputView) : 생성자 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 /* 자동차 입력 받는 클래스
 *  입력만 받기
@@ -8,8 +11,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"; // 삼수
     private final String  INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?"; //  삼수
-    public static int trialNumber;
+    public static int trialNumber; // 인스턴스 변수로 필요한지 고민
 
+    private InputParser inputParser;
+    // 생성자
+    public InputView(InputParser inputParser) {
+        this.inputParser = inputParser;
+    }
     // 자동차 입력 받기
     public  String inputCarName() {
         System.out.println(INPUT_CAR_NAME);
