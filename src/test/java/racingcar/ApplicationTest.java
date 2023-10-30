53920cead3bb4aa9639fa5295297b85e51c22324
dsa04156 <dsa04156@naver.com>
2023-10-31 01:04:12 +0900

    feat : 이름이 중복일때 예외처리

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -39,6 +39,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 이름이_중복일_때_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
