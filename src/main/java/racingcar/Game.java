35710b3db2028442c86900f0c807b4829c93292c
김주영 <gimjuyeong0620@gmail.com>
2023-10-30 23:22:37 +0900

    [feature] 시도할 횟수 입력 기능 개발
    1. 시도할 횟수 입력 기능 개발
    2. 입력값이 하나의 숫자가 아닐 경우 에러메세지 출력

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -6,11 +6,24 @@ import java.util.List;
 
 public class Game {
 
-    public void startMessage(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+
+    public Integer playTimes(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String input = Console.readLine();
+        checkPlayTimesInput(input);
+
+        return Integer.parseInt(input);
+    }
+
+    public void checkPlayTimesInput(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("시도할 횟수 입력이 잘못되었습니다.");
+        }
     }
 
     public List<String> playerInput(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         List<String> players = List.of(input.split(","));
         checkPlayersName(players);
