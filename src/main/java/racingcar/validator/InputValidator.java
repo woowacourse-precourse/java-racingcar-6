91c3a828d6ceccead5554c25aa044f2168823474
geunsu <geunsu23@naver.com>
2023-10-30 14:17:25 +0900

    modify exception message

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,7 +1,5 @@
 package racingcar.validator;
 
-import static racingcar.model.Constants.MAX_NAME_LENGTH;
-
 public class InputValidator {
     public static void checkRepetitionIsInteger(String userRepeat) throws IllegalArgumentException {
         if(!userRepeat.matches("^[0-9]*$")) {
@@ -11,13 +9,13 @@ public class InputValidator {
 
     public static void checkRepetitionIsNonNegative(String userRepeat) throws IllegalArgumentException {
         if(Integer.parseInt(userRepeat) < 0) {
-            throw new IllegalArgumentException("음수는 입력이 불가능합니다.");
+            throw new IllegalArgumentException("음수는 입력이 불가능 합니다.");
         }
     }
 
     public static void checkNameLengthIsOverflow(String carName, Integer maxLength) throws IllegalArgumentException {
         if (carName.isEmpty() || carName.length() > maxLength) {
-            throw new IllegalArgumentException("5자 이하의 이름만 입력하세요.");
+            throw new IllegalArgumentException( maxLength +"글자 이하의 이름만 입력하세요.");
         }
     }
 
