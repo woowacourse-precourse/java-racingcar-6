de96fbcdc73dd5c7fb3427bd82e6ae6e6a351a8a
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 04:57:19 +0900

    feat: 실행 결과 문구를 출력해주는 View 구현 및 문구 수정

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -11,13 +11,17 @@ public class RacingCarView {
 
 
     public void carNameListInputPrint() {
-        System.out.println("경주할 자동차 이름을 입력하시오");
+        System.out.println("경주할 자동차 이름을 입력하시오.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void readCountPrint() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public void gameProgressResultPrint(){
+        System.out.println("\n실행 결과");
+    }
+    
     public void gameProgressPrint(CarList carList) {
         for (Car car : carList.getCarList()) {
             int location = car.getLocation();
@@ -28,7 +32,7 @@ public class RacingCarView {
 
     public void printWinner(List<String> winnerList) {
         System.out.print("최종 우승자 : ");
-        System.out.println(String.join(", ", winnerList));
+        System.out.print(String.join(", ", winnerList));
     }
 
 }
