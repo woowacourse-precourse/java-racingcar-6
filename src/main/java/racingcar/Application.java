be6ca1750fd10a373f97602236a2d968d0f4a035
hong <kevin0928@naver.com>
2023-10-30 16:05:22 +0900

    feat:"자동차들의 이름은 쉼표를 기준으로 구분한다" 구현
    - RacingService 클래스는 생성자 주입을 통해 RacingController에게 제공
    - split(",")기능을 통해 쉼표로 구분 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,15 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView(); // InputView를 인스턴스화
+        RacingService racingService = new RacingService(); // RacingService를 인스턴스화
         // TODO: 프로그램 구현
-        RacingController racingController = new RacingController(inputView);
+        RacingController racingController = new RacingController(inputView, racingService);
         racingController.run();
     }
 }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,17 +1,22 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.service.RacingService;
 import racingcar.view.ConsoleOutput;
 import racingcar.view.InputView;
 
 public class RacingController {
     private final InputView inputView;
+    private final RacingService racingService;
 
-    public RacingController(InputView inputView) {
+    public RacingController(InputView inputView, RacingService racingService) {
         this.inputView = inputView;
+        this.racingService = racingService;
     }
 
     public void run() {
         ConsoleOutput.displayMessage(ConsoleOutput.INPUT_MESSAGE);
         String input = inputView.getCarNames();
+        List<String> carNames = racingService.stringToList(input);
     }
 }
