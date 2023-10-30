3fbc798c0b022fba4ebb2c75868d81a4482077fb
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 17:44:15 +0900

    feat: [Validator] 입력값의 마지막으로 쉼표(,)가 올 경우 예외처리 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,14 +1,23 @@
 package racingcar;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Validator {
+    private static final int MIN_MOVING_NUMBER = 1;
+    private static final int MAX_MOVING_NUMBER = 2000000000;
+    private static final int MIN_CAR_NAME_LENGTH = 1;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int MAX_CAR_COUNTING = 2;
 
     public void validateInput(String input) {
         if (!input.matches("^[가-힣a-zA-Z,]+$")) {
             throw new IllegalArgumentException("자동차 이름을 영문자,한글로만 구성하고 쉼표로만 구분해주세요.");
+        }
+        if (input.lastIndexOf(input.length() - 1) == ',') {
+            throw new IllegalArgumentException("입력값의 마지막으로는 쉼표가 올 수 없습니다.");
         }
     }
 
@@ -18,13 +27,13 @@ public class Validator {
         checkCarNameDuplicated(carNames);
     }
 
-    public void validateMovingNumber(String inputMovingNumber) {
+    public void validateMovingNumber(String userInputMovingNumber) {
         try {
-            int movingNumber = Integer.parseInt(inputMovingNumber);
-            if (movingNumber < 1) {
+            int movingNumber = Integer.parseInt(userInputMovingNumber);
+            if (movingNumber < MIN_MOVING_NUMBER) {
                 throw new IllegalArgumentException("1이상의 정수를 입력해야 경주가 시작됩니다.");
             }
-            if (movingNumber > 2000000000) {
+            if (movingNumber > MAX_MOVING_NUMBER) {
                 throw new IllegalArgumentException("이동 횟수는 최대 20억입니다.");
             }
         } catch (NumberFormatException e) {
@@ -34,24 +43,21 @@ public class Validator {
 
     private void checkCarNameLength(List<String> carNames) {
         for (String carName : carNames) {
-            if (carName.length() < 1 || carName.length() > 5) {
+            if (carName.length() < MIN_CAR_NAME_LENGTH || carName.length() > MAX_CAR_NAME_LENGTH) {
                 throw new IllegalArgumentException("자동차 이름은 1~5자로 입력해주세요.");
             }
         }
     }
 
     private void checkCarNameCounting(List<String> carNames) {
-        if (carNames.size() < 2) {
+        if (carNames.size() < MAX_CAR_COUNTING) {
             throw new IllegalArgumentException("경주를 하기 위해서 자동차 이름을 최소 2개는 입력해주세요.");
         }
     }
 
     private void checkCarNameDuplicated(List<String> carNames) {
-        Set<String> duplicacySet = new HashSet<>();
+        Set<String> duplicacySet = new HashSet<>(carNames);
 
-        for (String carName : carNames) {
-            duplicacySet.add(carName);
-        }
         if (carNames.size() != duplicacySet.size()) {
             throw new IllegalArgumentException("자동차 이름이 중복되면 우승자를 확인하는데 어려움이 있으니 중복 없이 입력하세요.");
         }
