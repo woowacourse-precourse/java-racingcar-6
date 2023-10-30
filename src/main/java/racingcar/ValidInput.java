13a0be10ec37adeac50d4f5158e1d71fb186a9aa
san <soh7174@gmail.com>
2023-10-31 00:20:57 +0900

    feat(ValidInput) : ValidInput 클래스 구현

--- src/main/java/racingcar/ValidInput.java
+++ src/main/java/racingcar/ValidInput.java
@@ -6,19 +6,34 @@ public class ValidInput {
     private ValidInput() {                      // 생성자
     }
     public static boolean isValidInput(String inputString) {
-        ;
+        if(true==hasComma(inputString));
+        if(false==hasDoubleComma(inputString));
+        if(false==startsOrEndsWithComma(inputString));
         return true;
     }
     public static boolean hasComma(String inputString) {
-        return true;
+        return inputString.contains(",");
     }
     public static boolean hasDoubleComma(String inputString) {
-        return true;
+        return inputString.contains(",,");
     }
     public static boolean startsOrEndsWithComma(String inputString) {
-        return true;
+        if(inputString.startsWith(",")) {
+            return true;
+        }
+        else if(inputString.endsWith(",")) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     public static boolean isNameLengthLessThanOrEqual5(List<String> NamesList) {
+        for(String name : NamesList) {
+            if(name.length() > 5) {
+                return false;
+            }
+        }
         return true;
     }
 
