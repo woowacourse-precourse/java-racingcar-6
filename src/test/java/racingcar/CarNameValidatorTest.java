d860ddf53890857f666c1842e73ff5df98bc839d
gomudayya <gomudayya@gmail.com>
2023-10-31 02:37:09 +0900

    Test : 자동차 이름 검증 테스트 작성

---
+++ src/test/java/racingcar/CarNameValidatorTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static racingcar.constant.ErrorMessage.CAR_NAME_IS_LONGER;
+
+
+class CarNameValidatorTest {
+
+    @Test
+    void 자동차_이름_검증_테스트() {
+        String longNames = "fobiiiii, geonwoo, park, carr";
+        assertThatThrownBy(() -> CarNameValidator.validate(longNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAR_NAME_IS_LONGER);
+    }
+}
