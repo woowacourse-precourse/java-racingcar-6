8a44d2a853ab3c3ece7dfe0b442656b82dda48e4
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 21:40:56 +0900

    refactor: OutputView 기능을 InputView 에게 위김

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,13 +5,18 @@ import racingcar.utils.AttemptCountsInputValidator;
 import racingcar.utils.CarNamesInputValidator;
 
 public class InputView {
-    public String inputCarNames() {
+    private static final String READ_CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String READ_ATTEMPT_COUNTS_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public String readCarNames() {
+        System.out.println(READ_CAR_INPUT_MESSAGE);
         String carNamesInput = Console.readLine();
         CarNamesInputValidator.validate(carNamesInput);
         return carNamesInput;
     }
 
-    public int inputAttemptCounts() {
+    public int readAttemptCounts() {
+        System.out.println(READ_ATTEMPT_COUNTS_INPUT_MESSAGE);
         String attemptCounts = Console.readLine();
         AttemptCountsInputValidator.validate(attemptCounts);
         return Integer.parseInt(attemptCounts);
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,13 +5,6 @@ import racingcar.dto.OneGameResultsDto;
 import racingcar.dto.ResultDto;
 
 public class OutputView {
-    public void printGameStartMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    }
-
-    public void printAttemptCountsQuestionMessage() {
-        System.out.println("시도할 회수는 몇회인가요?");
-    }
 
     public void printPlayResultText() {
         System.out.println("\n실행 결과");
