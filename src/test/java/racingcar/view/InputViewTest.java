8a16b6e129f24a38a23cc6f88c84950c5e57a343
J0YERIM <chosarah70@gmail.com>
2023-10-30 20:06:49 +0900

    test: Add tests for car name input validation

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,6 +2,47 @@ package racingcar.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
 class InputViewTest {
+
+    @BeforeEach
+    void setUp() {
+        Console.close();
+    }
+
+    @Test
+    void 이름이_5자_이하인_경우_올바르게_이름_목록으로_반환된다() {
+        String input = "car1, car2, car3\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        List<Car> cars = InputView.inputCarNames();
+
+        assertEquals(3, cars.size());
+        assertEquals("car1", cars.get(0).getName());
+        assertEquals("car2", cars.get(1).getName());
+        assertEquals("car3", cars.get(2).getName());
+    }
+
+    @Test
+    void 이름이_5자를_초과할_경우_IllegalArgumentException이_발생한다() {
+        String input = "verylongname, car2, car3\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThrows(IllegalArgumentException.class, InputView::inputCarNames);
+    }
+
+    @Test
+    void 이름이_중복되는_경우_IllegalArgumentException이_발생한다() {
+        String input = "car1, car2, car1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThrows(IllegalArgumentException.class, InputView::inputCarNames);
+    }
 
 }
