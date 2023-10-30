554e0823d2efb88f969e647c65788ac7569c9448
star-candy <el0710@naver.com>
2023-10-31 01:17:45 +0900

    refactor(racingcar): 테스트 로직 수정 및 어노테이션 위치 수정

--- src/test/java/model/ValidateTest.java
+++ src/test/java/model/ValidateTest.java
@@ -5,16 +5,15 @@ import static model.Validation.racingWinner;
 import static model.Validation.randomMoreFour;
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
+import java.util.Arrays;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 class ValidateTest {
-
-    @DisplayName("숫자 랜덤 추출 기능이 작동하는가")
     @Test
+    @DisplayName("숫자 랜덤 추출 기능이 작동하는가")
     void randomMoreFourTest() {
         //given
         //when
@@ -23,31 +22,23 @@ class ValidateTest {
         System.out.printf("랜덤 추출 기능 출력값 : %b \n", testResult);
     }
 
-    @DisplayName("추가된 레이싱 결과값이 List에 저장되는가")
     @Test
+    @DisplayName("추가된 레이싱 결과값이 List에 저장되는가")
     void racingResultTest() {
         //given
-        List<String> carName = Arrays.asList("pobi", "nana");
-        List<Integer> raceResult = Arrays.asList(1, 3);
+        List<String> carName = new ArrayList<>(List.of("pobi"));
+        List<Integer> raceResult = new ArrayList<>(List.of(2));
         //when
         List<Integer> testList = racingResult(carName, raceResult);
         //then
-        System.out.print("레이싱 결과 저장 기능 출력값 :");
-        for (int result : testList) {
-            System.out.printf("%d,", result);
-        }
-        System.out.println();
+        assertThat(testList.get(0)).isBetween(2, 3);
     }
 
-    @DisplayName("결과 값에 따라 우승자를 선정하는가")
     @Test
+    @DisplayName("결과 값에 따라 우승자를 선정하는가")
     void racingWinnerTest() {
         //given
-        List<String> carName = new ArrayList<>();
-        carName.add("pobi");
-        carName.add("nana");
-        carName.add("bubu");
-        carName.add("lala");
+        List<String> carName = new ArrayList<>(Arrays.asList("pobi", "nana", "bubu", "lala"));
         List<Integer> raceResult = Arrays.asList(1, 4, 3, 4);
         //when
         List<String> testResult = racingWinner(carName, raceResult);
