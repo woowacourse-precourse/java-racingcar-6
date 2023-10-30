ab5b9d2e92136c7299073347e5056464664d4dee
IM-GYURI <kgulr0517@gmail.com>
2023-10-31 03:23:30 +0900

    Feat: 추가 play () 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.MainController;
+import racingcar.model.manager.CarMoveManager;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-
+        MainController mainController = new MainController(InputView.getInstance(), OutputView.getInstance(), new CarMoveManager());
+        mainController.play();
     }
 }
