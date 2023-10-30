cbde753ab1d3da32cd94caa60ed469c563238e75
ohhyeonn <ohhyeon120@gmail.com>
2023-10-30 23:22:25 +0900

    feat: add test 자동차_이름_숫자영어_검증
    validate carNames

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,17 +10,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+
+    @Test
+    void 자동차_이름_숫자영어_검증() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("한글,입니다", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+
 
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
