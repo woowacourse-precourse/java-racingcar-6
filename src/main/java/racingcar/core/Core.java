783e41220801c918144b51f89ceea279aa0f846c
aaa <cnj991279@gmail.com>
2023-10-30 19:34:07 +0900

    feat: getCount 메서드 구현
    시도할 회수를 입력받는 메서드

--- src/main/java/racingcar/core/Core.java
+++ src/main/java/racingcar/core/Core.java
@@ -1,5 +1,8 @@
 package racingcar.core;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.Utils;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,4 +11,10 @@ public class Core {
         return Arrays.stream(input.split(",")).toList();
     }
 
+    public int getCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String count = Console.readLine();
+        Utils.validateStringDigit(count);
+        return Integer.parseInt(count);
+    }
 }
