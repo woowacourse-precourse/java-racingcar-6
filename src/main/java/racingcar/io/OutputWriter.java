6fac32ca46d9058d18e8a393a6135a30051a3478
Jiihyun <cwlgusc@gmail.com>
2023-10-31 04:09:44 +0900

    style(OutputWriter): 시도 횟수 포맷 수정

--- src/main/java/racingcar/io/OutputWriter.java
+++ src/main/java/racingcar/io/OutputWriter.java
@@ -2,7 +2,7 @@ package racingcar.io;
 
 public class OutputWriter {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
-    private static final String INPUT_TRY_TIMES = "시도할 회수는 몇회인가요?\n";
+    private static final String INPUT_TRY_TIMES = "시도할 회수는 몇회인가요?";
     private static final String GAME_RESULT = "\n실행 결과";
 
     public void writeInputNameMsg() {
