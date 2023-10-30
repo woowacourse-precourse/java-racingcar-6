60c479f63a6857bcd3956605f8228cbbfb4d0ef1
SongJIH <wlgus7592@gmail.com>
2023-10-30 16:15:35 +0900

    자동차 이름 입력받은 값의 형변환 추가

--- src/main/java/racingcar/Function.java
+++ src/main/java/racingcar/Function.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import camp.nextstep.edu.missionutils.Console;
 public class Function {
 	
@@ -14,8 +18,9 @@ public class Function {
 		return result;
 	}
 	
-	public static String[] inputCarName() {
-		String[] carName= Console.readLine().split(",");
+	public static ArrayList<String> inputCarName() {
+		String[] inputValue= Console.readLine().split(",");
+		ArrayList<String> carName= new ArrayList<String>(Arrays.asList(inputValue));
 		
 		return carName;
 	}
