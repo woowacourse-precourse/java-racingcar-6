5a038aa00e93de9e676ad52e6e484be5d7f321f6
dealim <136411152+dealim@users.noreply.github.com>
2023-10-30 17:30:25 +0900

    [update] <Game> printResult() 실행 결과 출력하기

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,27 +3,34 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
-    private Integer attemptsNumber = 5;
+    private Integer attemptsNumber;
 
     public void start() {
-        while (0 < attemptsNumber) {
-            Console.readLine();
-            attemptsNumber--;
-        }
+        userInputCarName();
+        printResult();
     }
 
     public void userInputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String userInputCarNameString = Console.readLine();
 
         System.out.println("시도할 회수는 몇회인가요?");
 
-        Integer attemptsNumber = Integer.valueOf(Console.readLine());
+        attemptsNumber = Integer.valueOf(Console.readLine());
     }
 
     public void printResult() {
-        System.out.println('실행 결과');
+        System.out.println("실행 결과");
 
+        while (0 < attemptsNumber) {
+            System.out.println("pobi : ");
+            System.out.println("woni : ");
+            System.out.println("jun : ");
+            System.out.println;
+            attemptsNumber--;
+        }
+
+        System.out.println("최종 우승자 : ");
     }
 }
