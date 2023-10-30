aa46fc25b45de5432a1e2cd451e3f0e7daa1ef0e
naayoung <ask5046a@gmail.com>
2023-10-30 15:54:24 +0900

    feat: add InputValue

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력값 받기
+        InputValue inputValue  = new InputValue();
+        InputValue inputTime  = new InputValue();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] value = inputValue.getInputValue();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String time = inputTime.getInputTime();
+        
     }
 }
