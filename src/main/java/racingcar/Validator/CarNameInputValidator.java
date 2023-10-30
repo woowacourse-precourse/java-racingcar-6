74232dabe791209a602423d8e5f29f19986565ea
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-30 15:38:32 +0900

    refactor: make class stateless to other instance

--- src/main/java/racingcar/Validator/CarNameInputValidator.java
+++ src/main/java/racingcar/Validator/CarNameInputValidator.java
@@ -5,48 +5,41 @@ import java.util.HashSet;
 import java.util.List;
 
 public class CarNameInputValidator {
-    private List<String> CarNameInputList;
     private final String ASK_MORE_THAN_TWO = "2개 이상의 차 이름을 입력하세요";
     private final String ASK_NO_DUPLICATION = "중복된 이름을 입력 불가합니다";
     private final String ASK_VALID_NAME_LENGTH = "차 이름은 5자 이하로 입력해주세요";
+
     private int MINIMUM_PARTICIPANT = 2;
     private int MAXIMUM_NAME_LENGTH = 5;
 
 
-    public CarNameInputValidator(String CarNameInput) {
-        this.CarNameInputList = Arrays.asList(CarNameInput.split(","));
+
+    public void validate(String carNameInput) {
+        List<String> carNameInputList = Arrays.asList(carNameInput.split(","))
+        areNamesMoreThanTwo(carNameInputList);
+        isNameLengthBelowFive(carNameInputList);
+        isNameNotDuplicated(carNameInputList);
     }
 
-    public List<String> getCarNameInputList() {
-        validate();
-        return CarNameInputList;
-    }
-
-    public void validate() {
-        areNamesMoreThanTwo();
-        isNameLengthBelowFive();
-        isNameNotDuplicated();
-    }
-
-    public void areNamesMoreThanTwo() throws IllegalArgumentException {
-        if (CarNameInputList.size() < MINIMUM_PARTICIPANT) {
+    public void areNamesMoreThanTwo(List<String> carNameInputList) throws IllegalArgumentException {
+        if (carNameInputList.size() < MINIMUM_PARTICIPANT) {
             throw new IllegalArgumentException(ASK_MORE_THAN_TWO);
         }
     }
 
-    public void isNameLengthBelowFive() throws IllegalArgumentException {
-        for (String carName : CarNameInputList) {
+    public void isNameLengthBelowFive(List<String> carNameInputList) throws IllegalArgumentException {
+        for (String carName : carNameInputList) {
             if (carName.length() > MAXIMUM_NAME_LENGTH) {
                 throw new IllegalArgumentException(ASK_VALID_NAME_LENGTH);
             }
         }
     }
 
-    public void isNameNotDuplicated() throws IllegalArgumentException {
+    public void isNameNotDuplicated(List<String> carNameInputList) throws IllegalArgumentException {
         HashSet<String> carNameSet = new HashSet<String>();
-        carNameSet.addAll(CarNameInputList);
+        carNameSet.addAll(carNameInputList);
 
-        if (carNameSet.size() != CarNameInputList.size()) {
+        if (carNameSet.size() != carNameInputList.size()) {
             throw new IllegalArgumentException(ASK_NO_DUPLICATION);
         }
     }
--- src/main/java/racingcar/Validator/TurnNumInputValidator.java
+++ src/main/java/racingcar/Validator/TurnNumInputValidator.java
@@ -1,28 +1,18 @@
 package racingcar.Validator;
 
 public class TurnNumInputValidator {
-
-    private String turnNumInputString;
     private int turnNumInputInt;
-    private final String ASK_NUMBER_INPUT = "잘못된 입력입니다. 숫자를 입력해야 합니다";
+    private final String ASK_NUMBER_INPUT = "잘못된 입력입니다. 숫자만 입력해야 합니다";
     private final String ASK_POSITIVE_NUM_INPUT = "잘못된 입력입니다. 0이상의 수를 입력해주세요";
     private final int MIN_TURN_NUM = 1;
+    
 
-    public TurnNumInputValidator(String turnNumberInput) {
-        this.turnNumInputString = turnNumberInput;
-    }
-
-    public int getTurnNumberInputInt() {
-        validate();
-        return turnNumInputInt;
-    }
-
-    public void validate() {
-        isInputDigit();
+    public void validate(String turnNumInputString) {
+        isInputDigit(turnNumInputString);
         isPositive();
     }
 
-    public void isInputDigit() {
+    public void isInputDigit(String turnNumInputString) {
         try {
             this.turnNumInputInt = Integer.parseInt(turnNumInputString);
         } catch (NumberFormatException e) {
