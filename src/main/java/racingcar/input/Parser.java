2d994d9a7256e7acd1576384984e62c64494c26c
pgd <rudeh1253@gmail.com>
2023-10-30 15:12:53 +0900

    feat: 사용자 입력 파싱
    - 콤마(,)로 구분된 자동차 이름을 배열에 저장
    - 사용자가 입력한 정수를 int 자료형으로 파싱

---
+++ src/main/java/racingcar/input/Parser.java
@@ -0,0 +1,13 @@
+package racingcar.input;
+
+public class Parser {
+    private static final String CAR_NAME_SEPARATOR = ",";
+
+    public String[] parseCarNames(String line) {
+        return line.split(CAR_NAME_SEPARATOR);
+    }
+
+    public int parseInt(String integer) {
+        return Integer.parseInt(integer);
+    }
+}
---
+++ src/test/java/racingcar/input/ParserTest.java
@@ -0,0 +1,87 @@
+package racingcar.input;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    private Parser parser;
+
+    @BeforeEach
+    void init() {
+        this.parser = new Parser();
+    }
+
+    @Test
+    void testParseCarNames() {
+        String[] testCases = {
+                "pobi,woni,jun",
+                "legacy,mars,common,john",
+                "maine",
+                ""
+        };
+
+        int[] expectedLengths = {
+                3,
+                4,
+                1,
+                1
+        };
+
+        String[][] expectedArrays = {
+                { "pobi", "woni", "jun" },
+                { "legacy", "mars", "common", "john" },
+                { "maine" },
+                { "" }
+        };
+
+        for (int i = 0; i < testCases.length; i++) {
+            String[] names = this.parser.parseCarNames(testCases[i]);
+            Assertions.assertThat(names.length).isEqualTo(expectedLengths[i]);
+            Assertions.assertThat(names).contains(expectedArrays[i]);
+        }
+    }
+
+    @Test
+    void testParseInt_throwsException() {
+        String[] testCases = {
+                "123a",
+                "a56",
+                "b",
+                "13!",
+                "3.14",
+                ""
+        };
+
+        for (String testCase : testCases) {
+            Assertions.assertThatThrownBy(() -> this.parser.parseInt(testCase))
+                    .isInstanceOf(NumberFormatException.class);
+        }
+    }
+
+    @Test
+    void testParseInt_correct() {
+        String[] testCases = {
+                "56",
+                "0",
+                "13",
+                "-1",
+                "-0"
+        };
+
+        int[] expected = {
+                56,
+                0,
+                13,
+                -1,
+                0
+        };
+
+        for (int i = 0; i < testCases.length; i++) {
+            Assertions.assertThat(this.parser.parseInt(testCases[i]))
+                    .isEqualTo(expected[i]);
+        }
+    }
+}
