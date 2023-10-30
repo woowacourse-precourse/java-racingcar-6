a0f0776e071bbb02e76b880a21810eeecd801031
kyumho kim <rlaruagh030@gmail.com>
2023-10-30 14:48:58 +0900

    test : 실행 횟수에 대한 예외 처리 테스트 함수 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 실행_횟수에_대한_예외_처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,jun", "1-"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
