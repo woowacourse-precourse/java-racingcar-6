17d2e3d4686621eca9940d46d69793a6d3728556
ay-eonii <naayen@naver.com>
2023-10-31 02:13:05 +0900

    feat: Output 클래스 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+public class OutputView {
+    public void displayCarNameInputMessage() {
+    }
+
+    public void displayTryCountInputMessage() {
+    }
+
+    public void displayResultGuideMessage() {
+    }
+
+    public void displayLocation() {
+    }
+
+    public void displayWinner() {
+    }
+}
