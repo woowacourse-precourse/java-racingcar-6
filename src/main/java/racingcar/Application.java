e006edbb42821446bac3ea5dce84cc5568c1a0d3
jihun-24k <gi020477@gmail.com>
2023-10-30 18:44:17 +0900

    feat: 자동차 이름 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,14 +7,22 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        String[] cars = input.split(",");
+        String[] carNames = input.split(",");
 
-        // Todo: 5자 예외처리
+        for (String carName : carNames) {
+            validateCarName(carName);
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
         input = Console.readLine();
         int count = Integer.parseInt(input);
 
         new RacingGame(count);
+    }
+
+    private static void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
