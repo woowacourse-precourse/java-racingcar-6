517e650468099a560708e605a1b3ca11224305d7
alscjf1329 <alscjf1329@gmail.com>
2023-10-30 17:39:59 +0900

    test: setInputStream 메소드 작성 적용
    inputstream을 원하는 문자열로 바꾸는 setInputStream 메소드를 작성했습니다. 모든 테스트 이후에 Console.close를 추가해서 scanner를 null로 바꿨습니다.
    
    참고: https://velog.io/@hjm2530/System.in-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1-feat.-NoSuchElementException

--- src/test/java/racingcar/RacingCarGameInputTest.java
+++ src/test/java/racingcar/RacingCarGameInputTest.java
@@ -1,42 +1,59 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import racingcar.dto.RacingCar;
 import racingcar.utils.RacingCarGameInput;
 
 public class RacingCarGameInputTest {
+    @AfterEach
+    private void closeConsole() {
+        Console.close();
+    }
+
+    private void setInputStream(String input) {
+        System.setIn(convertToInputStream(input));
+    }
+
+    private ByteArrayInputStream convertToInputStream(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
     @Test
-    void validateCarNamesInput_메소드를_사용시_자동차_이름_입력에_공백이_들어가는_경우_예외_발생() {
-        String input = "pobi, javaji";
-        assertThatThrownBy(() -> RacingCarGameInput.validateCarNamesInput(input))
+    void 자동차_이름이_5자가_넘어가는_경우_예외_발생() {
+        setInputStream("yang,minjin,cheol");
+        assertThatThrownBy(() -> RacingCarGameInput.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_BIGGER_THAN_5CHAR);
+    }
+
+    @Test
+    void 자동차_이름_입력에_공백이_들어가는_경우_예외_발생() {
+        setInputStream("yang, min");
+        assertThatThrownBy(() -> RacingCarGameInput.readCarNames())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_EMPTY);
     }
 
 
     @Test
-    void validateCarNames_메소드를_사용시_자동차_이름이_5자가_넘어가는_경우_예외_발생() {
-        List<RacingCar> carList = List.of(
-                new RacingCar("exception")
-        );
-        assertThatThrownBy(() -> RacingCarGameInput.validateCarNames(carList))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_BIGGER_THAN_5CHAR);
-    }
-
-    @Test
-    void validateNumber_메소드를_사용시_숫자가_아닌_경우_예외_반환() {
-        assertThatThrownBy(() -> RacingCarGameInput.validateNumber("12ad"))
+    void 시도_횟수가_숫자가_아닌_경우_예외_반환() {
+        setInputStream("12ad");
+        assertThatThrownBy(() -> RacingCarGameInput.readMoveCount())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_NOT_NUMBER);
     }
 
     @Test
-    void validateMoveCount_메소드를_사용시_1이상이_아닌_경우_예외_반환() {
-        assertThatThrownBy(() -> RacingCarGameInput.validateMoveCount(0))
+    void 시도_횟수가_1이상이_아닌_경우_예외_반환() {
+        setInputStream("0");
+        assertThatThrownBy(() -> RacingCarGameInput.readMoveCount())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(RacingCarGameInput.ERROR_MESSAGE_NOT_POSITIVE);
     }
