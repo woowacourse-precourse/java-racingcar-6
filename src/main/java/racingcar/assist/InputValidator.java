f1770994e230d6e0cb8069c1df65504e2c92068e
yshong1998 <yshong1998@gmail.com>
2023-10-31 02:46:29 +0900

    Refactor(InputValidator): 사용자의 입력이 올바른지 검증하는 기능 조건 추가

--- src/main/java/racingcar/assist/InputValidator.java
+++ src/main/java/racingcar/assist/InputValidator.java
@@ -12,10 +12,30 @@ public class InputValidator {
         }
     }
 
+    public void validateRepeatTimesInput(String repeatTimesInput) {
+        int repeatTimes = validateIsInputInteger(repeatTimesInput);
+        validateIsNumberPositive(repeatTimes);
+    }
+
 
     private void validateCarNameOutOfRange(String carName){
         if(carName.length() > 5){
             throw new IllegalArgumentException(INPUT_OUT_OF_RANGE.getMessage());
         }
     }
+
+    private void validateIsNumberPositive(int repeatTimes){
+        if (repeatTimes < 0 ){
+            throw new IllegalArgumentException(INPUT_POSITIVE_INTEGER_ONLY.getMessage());
+        }
+    }
+
+    private int validateIsInputInteger(String repeatTimesInput){
+        try {
+            return Integer.parseInt(repeatTimesInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(INPUT_POSITIVE_INTEGER_ONLY.getMessage());
+        }
+    }
+
 }
