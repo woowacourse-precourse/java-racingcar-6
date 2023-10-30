04e019241e2442c9a5857acb90101f5b36c526f5
eogus0512 <eogus0512@gmail.com>
2023-10-30 23:08:06 +0900

    refactor: Car, RacingCar 주석 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 public class Car {
-    private final String carName;
-    private int position;
+    private final String carName; //자동차 주인 이름
+    private int position; //자동차 위치 (점수)
 
     public Car(String carName) {
         // 이름 검사, 이름이 잘못되면 IllegalArgumentException 발생
@@ -15,6 +15,8 @@ public class Car {
         this.carName = carName;
         this.position = 0;
     }
+
+    //전진 메서드
     public void moveForward() {
         position++;
     }
@@ -22,6 +24,11 @@ public class Car {
         return carName;
     }
 
+    public int getPosition() {
+        return position;
+    }
+
+    //전진 횟수 출력 메서드
     public void showPosition() {
         System.out.print(carName + " : ");
         for (int i=0; i < position; i++) {
--- src/main/java/racingcar/TrafficController.java
+++ src/main/java/racingcar/TrafficController.java
@@ -6,16 +6,19 @@ public class TrafficController {
     private int randomNumber;
 
     public TrafficController() {
-        generateRandomNumber();
+        randomNumber = generateRandomNumber();
     }
 
+    //0~9사이 랜덤 값 리턴
     public int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
+
     public int getRandomNumber() {
         return randomNumber;
     }
 
+    //랜덤 값이 4이상이면 전진(true), 아니면 후진(false)
     public boolean isMoveForward() {
         if (randomNumber >= 4) {
             return true;
