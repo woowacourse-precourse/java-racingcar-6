c42de95885bd36476ba088010e561344f97e3063
alscjf1329 <alscjf1329@gmail.com>
2023-10-30 09:27:32 +0900

    refactor: message들의 상수들을 작성
    나중의 유지 보수를 고려하여 각 메시지 출력하는 메소드들이 인자를 출력하도록 변경하고 RacingCarGameOutput의 상수를 가져와 출력할 수 있도록 수정했습니다.

--- src/main/java/racingcar/utils/RacingCarGameOutput.java
+++ src/main/java/racingcar/utils/RacingCarGameOutput.java
@@ -1,11 +1,18 @@
 package racingcar.utils;
 
+import java.util.List;
+import racingcar.dto.RacingCar;
+
 public class RacingCarGameOutput {
-    public static void printCarNameInputMessage(){
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    public static final String MOVE_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String BEFORE_GAME_MESSAGE = "실행 결과";
+
+    public static void printCarNameInputMessage(String message) {
+        System.out.println(message);
     }
 
-    public static void printMoveCountInputMessage(){
-        System.out.println("시도할 회수는 몇회인가요?");
+    public static void printMoveCountInputMessage(String message) {
+        System.out.println(message);
     }
 }
