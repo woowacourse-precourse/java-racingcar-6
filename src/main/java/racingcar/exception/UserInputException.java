6c02d2caa547db780986df30121f13299b46f9f7
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 06:48:57 +0900

    refactor : 사용자 입력 예외처리 중, 차 두대 이상 입력하는 예외 처리 코드 작성

--- src/main/java/racingcar/exception/UserInputException.java
+++ src/main/java/racingcar/exception/UserInputException.java
@@ -13,6 +13,7 @@ public class UserInputException {
     private static final String BLANK_STRING_MESSAGE = "빈 문자를 입력하셨습니다.";
     private static final String DUPLICATE_MESSAGE = "자동차 이름은 중복되지 않게 입력해주세요";
     private static final String ONLY_COMMA = "구분자는 쉼표로만 작성해주세요";
+    private static final String INPUT_MORE_THAN_TWO = "차는 두 대 이상으로 입력하세요";
 
 
     public static void isLength(String carName) {
@@ -37,6 +38,12 @@ public class UserInputException {
         for (char c : carName.toCharArray())
         if(!(Character.isLetterOrDigit(c) || c == ',' || Character.isWhitespace(c))){
             throw new IllegalArgumentException(ONLY_COMMA);
+        }
+    }
+
+    public static void isMoreThan(List<String> carNameList) {
+        if(carNameList.size() < 2){
+            throw new IllegalArgumentException(INPUT_MORE_THAN_TWO);
         }
     }
 }
