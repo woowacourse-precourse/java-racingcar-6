332924dbec898907c0f7c14559a00acf1f387334
doka <posmane5@gmail.com>
2023-10-30 22:50:39 +0900

    feat(Application) : ReadRacingResult 기능 클래스와 CarController 기능들을 활용하여 프로그램 기능 구성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,23 @@
 package racingcar;
-
+ 
 import racingcar.domain.CarController;
 import racingcar.domain.RacingRuleMaker;
+import racingcar.domain.ReadRacingResult;
 
 public class Application {
 
     public static void main(String[] args) {
+
         RacingRuleMaker racingRuleMaker = new RacingRuleMaker();
         CarController carController = new CarController(racingRuleMaker.getCars());
+        ReadRacingResult readRacingResult = new ReadRacingResult();
 
-        for (int roundCount = 0; roundCount < racingRuleMaker.getRoundCount(); roundCount++) {
+        System.out.println("\n실행 결과");
+        for (int roundCount = 0; roundCount < racingRuleMaker.getAllRoundCount(); roundCount++) {
             carController.gameStart();
+            readRacingResult.readHashMap(carController.racingResult);
 
         }
-
+        readRacingResult.judgmentWinner(carController.racingResult);
     }
 }
