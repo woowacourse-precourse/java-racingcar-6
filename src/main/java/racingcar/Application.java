120e6dd3b119b86ef6c00a3de0db20e6991eb296
bbungjin <leejintwo12@naver.com>
2023-10-30 19:20:19 +0900

    입력 읽기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = readLine();
+        ArrayList<String> car = new ArrayList<String>(List.of(input.split(",")));
+        System.out.println(car);
     }
 }
