c1a77fc5102d1329eac107a2ff6ccaec18da49a1
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-30 20:45:10 +0900

    feat(Referee): 최종 우승자 명단 생성 기능 구현

--- src/main/java/racingcar/service/Referee.java
+++ src/main/java/racingcar/service/Referee.java
@@ -5,6 +5,24 @@ import racingcar.domain.RacingCar;
 
 public class Referee {
 
+    private static final String WINNER_SEPARATOR = ", ";
+
+    public static String generateRaceWinnersList(List<RacingCar> racingCars) {
+        int max = getMaxDistance(racingCars);
+        StringBuilder tmp = new StringBuilder();
+
+        for (RacingCar racingCar : racingCars) {
+            addRacingCarNameIfWinner(racingCar, max, tmp);
+        }
+        return tmp.substring(0, tmp.length() - 2);
+    }
+
+    private static void addRacingCarNameIfWinner(RacingCar racingCar, int max, StringBuilder tmp) {
+        if (racingCar.getDistance() == max) {
+            tmp.append(racingCar.getCarName()).append(WINNER_SEPARATOR);
+        }
+    }
+
     public static int getMaxDistance(List<RacingCar> racingCars) {
         int maxDistance = 0;
         for (RacingCar racingCar : racingCars) {
