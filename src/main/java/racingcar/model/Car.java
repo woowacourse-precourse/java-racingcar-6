f7b536efa2d1223c8f1a77bd22fd6ab87a3a4444
Handwoong <handwoong@gmail.com>
2023-10-31 00:53:17 +0900

    refactor(car): 자동차 전진 시 자동차 model에서 랜덤 숫자 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,19 +1,23 @@
 package racingcar.model;
 
+import racingcar.util.NumberGenerator;
 import racingcar.validation.CarNameValidator;
 
 public class Car {
     private static final int MOVEMENT_THRESHOLD = 4;
+    private final NumberGenerator numberGenerator;
     private final String name;
     private int position = 0;
 
-    public Car(final String name) {
+    public Car(final String name, final NumberGenerator numberGenerator) {
         final CarNameValidator carNameValidator = CarNameValidator.getInstance();
         carNameValidator.validate(name);
         this.name = name;
+        this.numberGenerator = numberGenerator;
     }
 
-    public void moveForward(final int randomNumber) {
+    public void moveForward() {
+        final int randomNumber = numberGenerator.createRandomNumber();
         if (isMoveForward(randomNumber)) {
             position++;
         }
--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -4,15 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import racingcar.dto.CarDto;
-import racingcar.util.NumberGenerator;
 
 public class RacingGame {
     private final List<Car> carList = new ArrayList<>();
-    private final NumberGenerator numberGenerator;
-
-    public RacingGame(final NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
 
     public void addCar(final Car car) {
         carList.add(car);
@@ -20,8 +14,7 @@ public class RacingGame {
 
     public void moveCar(final int carIndex) {
         final Car car = carList.get(carIndex);
-        final int randomNumber = numberGenerator.createRandomNumber();
-        car.moveForward(randomNumber);
+        car.moveForward();
     }
 
     public List<CarDto> getResult() {
