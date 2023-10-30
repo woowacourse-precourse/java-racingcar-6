3e6b1b58da0a1451689c8355032ca0bdf7b5b989
osukhyun00 <osh123chm@gmail.com>
2023-10-30 14:55:53 +0900

    feat: 전진 여부 결정 로직 구현

--- src/main/java/racingcar/domain/Manager.java
+++ src/main/java/racingcar/domain/Manager.java
@@ -1,4 +1,43 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager {
+
+    public void calculateRun(List<Player> playerList) {
+        for (Player player : playerList) {
+            int randomNumber = getRandomNumber();
+
+            if (randomNumber >= 4) {
+                player.addScore();
+            }
+        }
+    }
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    private List<Player> stringListToPlayerList(List<String> players) {
+        List<Player> playerList = new ArrayList<>();
+        for (String name : players) {
+            Player player = new Player(name);
+            playerList.add(player);
+        }
+        return playerList;
+    }
+
+//    private Map<Player, Integer> playerListToMap(List<String> playerList){
+//        Map<Player, Integer> playerMap = new HashMap<>();
+//        for(String name : playerList) {
+//            Player player = new Player(name);
+//            playerMap.put(player, 0);
+//        }
+//
+//        return playerMap;
+//    }
+
+
 }
--- src/main/java/racingcar/domain/Player.java
+++ src/main/java/racingcar/domain/Player.java
@@ -2,4 +2,13 @@ package racingcar.domain;
 
 public class Player {
     public String name;
+    public int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void addScore(){
+        score++;
+    }
 }
