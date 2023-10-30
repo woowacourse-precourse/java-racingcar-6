51300d60ee6254947165e5d77dacfb1bbdad17d4
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-30 19:02:38 +0900

    feat(RacingController): 레이싱 컨트롤러 기능 구현

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.model.Racing;
+import racingcar.validator.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final Racing racing;
+
+    public RacingController() {
+        String[] validateCars = Validator.carNameValidator(InputView.carNamesInput());
+        Cars cars = Cars.createCars(validateCars);
+        String count = InputView.roundCountInput();
+        this.racing = new Racing(cars, count);
+    }
+
+    public void start() {
+        OutputView.printResultMessage();
+        playGame();
+        OutputView.printResult(racing.getWinners());
+    }
+
+    private void playGame() {
+        while (!racing.isEnd()) {
+            racing.playRound();
+            OutputView.printRoundResult(racing.getStatus());
+        }
+    }
+}
