8d5bdd0193724ab3499ba11d2368691d29e06265
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:37:58 +0900

    feat(racingcar): OutputView에 게임 진행 결과를 출력하는 메소드 추가 게임 진행마다 자동차의 거리를 출력하는 static 메소드를 추가하였습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,10 @@
 package racingcar.view;
 
+import racingcar.domain.Cars;
+
+import static racingcar.config.GameConfig.MOVE_VALUE;
 import static racingcar.view.message.GameMessage.CAR_NAME_INPUT_QUESTION_MESSAGE;
+import static racingcar.view.message.GameMessage.GAME_PROGRESS_RESULT_FORMAT;
 import static racingcar.view.message.GameMessage.GAME_RESULT_PREFIX_MESSAGE;
 import static racingcar.view.message.GameMessage.TOTAL_ROUND_INPUT_QUESTION_MESSAGE;
 
@@ -20,5 +24,19 @@ public final class OutputView {
     public static void printGameStartMessage() {
         System.out.println();
         System.out.println(GAME_RESULT_PREFIX_MESSAGE);
+    }
+
+    public static void printCurrentProgress(final Cars cars) {
+        for (int i = 0; i < cars.getSize(); i++) {
+            final String name = cars.getNameFromIndex(i);
+            final String currentDistanceValue = getCurrentDistanceValue(cars, i);
+            System.out.println(String.format(GAME_PROGRESS_RESULT_FORMAT, name, currentDistanceValue));
+        }
+        System.out.println();
+    }
+
+    private static String getCurrentDistanceValue(final Cars cars, final int index) {
+        final int distance = cars.getDistanceFromIndex(index);
+        return MOVE_VALUE.repeat(distance);
     }
 }
