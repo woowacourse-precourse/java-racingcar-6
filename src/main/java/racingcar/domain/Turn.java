98aadf78cfac5adcd89865b41752a694a63a2be6
PgmJun <chltmdwns96@gmail.com>
2023-10-30 15:49:22 +0900

    refactor: 정규표현식 Pattern객체 사용하도록 수정
    https://velog.io/@pgmjun/%EC%9A%B0%ED%85%8C%EC%BD%94-6%EA%B8%B0-%EC%9E%90%EB%B0%94-%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D-%ED%99%9C%EC%9A%A9-%EC%8B%9C-Pattern-%EA%B0%9D%EC%B2%B4%EB%A5%BC-%EB%A7%8C%EB%93%A4%EC%96%B4-%EC%82%AC%EC%9A%A9%ED%95%98%EC%9E%90

--- src/main/java/racingcar/domain/Turn.java
+++ src/main/java/racingcar/domain/Turn.java
@@ -2,11 +2,15 @@ package racingcar.domain;
 
 import static racingcar.constant.RacingCarErrorMessage.TRY_COUNT_NUMBER_FORMAT_ERROR_MESSAGE;
 
+import java.util.regex.Pattern;
+
 public class Turn {
     private final int count;
+    private final static String NUMBER_PATTERN = "\\d+";
+    private final static Pattern NUMBER_FORMAT = Pattern.compile(NUMBER_PATTERN);
 
     public Turn(String count) {
-        validateTryToMoveCount(count);
+        validateTryCount(count);
         this.count = Integer.parseInt(count);
     }
 
@@ -15,13 +19,17 @@ public class Turn {
         return count;
     }
 
-    private void validateTryToMoveCount(String tryToMoveCount) {
-        if (isNotNumberFormat(tryToMoveCount)) {
+    private void validateTryCount(String tryToMoveCount) {
+        validateTryCountFormat(tryToMoveCount);
+    }
+
+    private void validateTryCountFormat(String tryToMoveCount) {
+        if (!isNumberFormat(tryToMoveCount)) {
             throw new IllegalArgumentException(TRY_COUNT_NUMBER_FORMAT_ERROR_MESSAGE);
         }
     }
 
-    private boolean isNotNumberFormat(String text) {
-        return !text.matches("\\d+");
+    private boolean isNumberFormat(String text) {
+        return NUMBER_FORMAT.matcher(text).matches();
     }
 }
