4e2fac03ab7dfdd3ea0fcc93d4f6d3854622670d
bin <gimgyubin1225@gmail.com>
2023-10-30 16:52:27 +0900

    Test: 테스트 형식 수정 (Given when then)

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,19 +2,26 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
 
 public class CarTest {
 
     @Test
     void 자동차_전진_기능_4미만이면_정지() {
-        Car car = new Car("car1");
-        Assertions.assertEquals(0, car.moves(0));
+        Car car = new Car("car");
+        int start = car.getLocation();
+
+        car.moves(0);
+
+        Assertions.assertEquals(start, car.getLocation());
     }
 
     @Test
     void 자동차_전진_기능_4이상이면_전진() {
-        Car car = new Car("car1");
-        Assertions.assertEquals(1, car.moves(9));
+        Car car = new Car("car");
+        int start = car.getLocation();
+
+        car.moves(4);
+
+        Assertions.assertEquals(start+1, car.getLocation());
     }
 }
