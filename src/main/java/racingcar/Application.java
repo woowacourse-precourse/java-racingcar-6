9c9aae170628f9a342513a3e7c680276aff342e1
SongJIH <wlgus7592@gmail.com>
2023-10-30 18:41:24 +0900

    예외처리 및 메인메서드에서 동작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	Function function= new Function();
+    	
+    	int count= function.inputNumber();
+    	ArrayList<String> carName= function.inputCarName();
+    	Map<String, Integer> result = function.run(count, carName);
+    	
+    	function.outputMove(result);
+    	function.outputWinners(function.winners(result));
     }
 }
--- src/main/java/racingcar/Function.java
+++ src/main/java/racingcar/Function.java
@@ -8,12 +8,10 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
-
 import camp.nextstep.edu.missionutils.Console;
 public class Function {
 	
-	public static boolean check() {
+	public  boolean check() {
 		int number= Randoms.pickNumberInRange(0,9);
 		boolean result= false;
 		
@@ -23,26 +21,32 @@ public class Function {
 		return result;
 	}
 	
-	public static ArrayList<String> inputCarName() {
+	protected ArrayList<String> inputCarName() {
+		System.out.println("경주할 자동차의 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
 		String[] inputValue= Console.readLine().split(",");
+		checkString(inputValue);
 		ArrayList<String> carName= new ArrayList<String>(Arrays.asList(inputValue));
 		
 		return carName;
 	}
-	public static int inputNumber() {
-		int number= Integer.parseInt(Console.readLine());
+	protected int inputNumber() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		String inputValue= Console.readLine();
+		checkNumber(inputValue);
+		
+		int number= Integer.parseInt(inputValue);
 		
 		return number;
 	}
 	
-	public static Map<String, Integer> run(int count, ArrayList<String> carName) {
+	protected Map<String, Integer> run(int count, ArrayList<String> carName) {
 		Map<String, Integer> result= new LinkedHashMap<String, Integer>();
 		for(String name:carName) {
 			result.put(name,eachRun(count));
 		}		
 		return result;
 	}
-	public static int eachRun(int count) {
+	protected int eachRun(int count) {
 		int result= 0;
 		for(int i= 0; i<count; i++) {
 			if(check()) {
@@ -52,7 +56,7 @@ public class Function {
 		return result;
 	}
 	
-	public static List<String> winners(Map<String, Integer> result) {
+	protected List<String> winners(Map<String, Integer> result) {
 		Set<String> name= result.keySet();
 		Object[] resultValue=  result.values().toArray();			
 		int winValue= 0;
@@ -71,7 +75,8 @@ public class Function {
 		return winners;
 	}
 
-	public static void outputMove(Map<String, Integer> result) {
+	protected void outputMove(Map<String, Integer> result) {
+		System.out.println("실행 결과");
 		Set<String> name= result.keySet();
 		for(String key: name) {
 			String printStr= key+" : " ;
@@ -82,7 +87,22 @@ public class Function {
 		}
 		
 	}	
-	public static void outputWinners(List<String> winners) {
+	protected void outputWinners(List<String> winners) {
 		System.out.println("최종 우승자 : "+ String.join(", ", winners));
+	}
+	
+	private void checkString(String[] inputValue) {
+		for(int i= 0; i<inputValue.length; i++) {
+			if(inputValue[i].length()>5 || inputValue[i].getClass().getName()!="string") {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
+	private void checkNumber(String inputValue) {
+		String RECEX="[0-9]+";
+		if(!inputValue.matches(RECEX)) {
+			throw new IllegalArgumentException();
+		}
+		
 	}
 }
