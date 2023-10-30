36e9912973adc3e76e7144a161be618dc666d9f8
gumgu <bienlee@naver.com>
2023-10-30 21:38:40 +0900

    Feat : 게임 인스턴스 생성 & 게임 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.business.Racing;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Racing racing = new Racing();
+        racing.doRacing();
     }
 }
