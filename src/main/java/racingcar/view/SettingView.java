4d3a3e9da8823da724f93017190be570a9dc6644
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-30 16:11:57 +0900

    style: 오탈자 수정

--- src/main/java/racingcar/view/SettingView.java
+++ src/main/java/racingcar/view/SettingView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 public class SettingView {
 
-    private static final String GET_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String GET_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String GET_TRIALS = "시도할 회수는 몇회인가요?";
 
    public static void getCarNames() {
