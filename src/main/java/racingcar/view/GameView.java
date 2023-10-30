04407cf524b45da7ac9586d025bce820db11681f
Hwang In Tae <sjobs66@gmail.com>
2023-10-31 01:58:59 +0900

    feat: GameView.java 추가
    GameView Interface 추가함.

---
+++ src/main/java/racingcar/view/GameView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import racingcar.model.Game;
+
+public interface GameView {
+    void showInputRequiredMessage();
+
+    void showProgressOf(Game game);
+
+    void showResultOf(Game game);
+
+}
