c2d4014597faf71fa19c60193e427cb8d42aafd4
Jiwon Lee <jiwon718@users.noreply.github.com>
2023-10-30 18:46:41 +0900

    feat: 게임 플레이어가 입력한 값으로 자동차 3대 이름 지정하는 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -9,11 +9,24 @@ public class Game {
     public static Player player = new Player();
 
     public void play() {
+        start();
+    }
+
+    public void start() {
         askCarNames();
+        nameCars();
     }
 
     public void askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         player.inputCarNames();
+    }
+
+    public void nameCars() {
+        List<String> carNames = player.getCarNames();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
     }
 }
