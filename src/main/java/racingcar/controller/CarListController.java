d609a01ddf59b36e5a7fccb5830ec1ad53ace73a
chaniii999 <psc0729@naver.com>
2023-10-30 21:50:35 +0900

    feat: 자동차리스트컨트롤러 클래스 추가

---
+++ src/main/java/racingcar/controller/CarListController.java
@@ -0,0 +1,37 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import racingcar.model.Car;
+import racingcar.model.InputToName;
+import racingcar.model.NameToClass;
+
+// cars 입력 & 저장
+public class CarListController {
+    static final char Separator = ',';
+
+    final InputToName nameList;
+    final NameToClass classList;
+
+    public CarListController() {
+        String input = Console.readLine();
+        validateInput(input);
+
+        nameList = new InputToName(input);
+
+        classList = new NameToClass(nameList.getNameList());
+    }
+
+    private void validateInput(String input) {
+        if (input.trim().isEmpty() || input.charAt(0) == Separator)
+            throw new IllegalArgumentException();
+
+    }
+
+    public ArrayList<Car> getClassList() {
+        return classList.getClassList();
+    }
+
+}
+
