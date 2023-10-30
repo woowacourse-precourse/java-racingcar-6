482f007eec84b3c42daf85becb2c66eea5718ca6
wonjun <wonjon0930@daum.net>
2023-10-30 16:50:34 +0900

    feat: add feature for Car to move or stop per game

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -6,4 +6,9 @@ public class Constant {
     public static final String INPUT_CAR_NAMES_DELIMITER = ",";
     public static final int CAR_NAME_MAX_LENGTH = 5;
     public static final int NUMBER_OF_GAMES_LOWER_LIMIT = 0;
+    public static final long CAR_INIT_DISTANCE_VALUE = 0L;
+    public static final int RANDOM_VALUE_LOWER_LIMIT = 0;
+    public static final int RANDOM_VALUE_UPPER_LIMIT = 9;
+    public static final int MOVE_OR_STOP_BOUNDARY_VALUE = 4;
+
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,8 +1,35 @@
 package racingcar.domain;
 
-public class Car {
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constant.Constant;
 
+public class Car {
     private String name;
     private Long distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = Constant.CAR_INIT_DISTANCE_VALUE;
+    }
+
+    public void moveOrStop() {
+        int randomValue = Randoms.pickNumberInRange(Constant.RANDOM_VALUE_LOWER_LIMIT,
+                Constant.RANDOM_VALUE_UPPER_LIMIT);
+        if (randomValue >= Constant.MOVE_OR_STOP_BOUNDARY_VALUE && randomValue < Constant.RANDOM_VALUE_UPPER_LIMIT) {
+            move();
+            return;
+        }
+        if (randomValue < Constant.MOVE_OR_STOP_BOUNDARY_VALUE && randomValue >= Constant.RANDOM_VALUE_LOWER_LIMIT) {
+            stop();
+        }
+    }
+
+    private void move() {
+        distance++;
+    }
+
+    private void stop() {
+
+    }
 
 }
