385d7a3e8878e2ec6af6aeaf69c16dcc8b3aaea4
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-30 21:56:51 +0900

    feat(RaceGame): playGame 메서드 생성

--- src/main/java/racingcar/Controller/RaceGame.java
+++ src/main/java/racingcar/Controller/RaceGame.java
@@ -5,6 +5,8 @@ import static racingcar.Controller.Module.Validator.validateInputIsNumber;
 import static racingcar.Controller.Module.Validator.validateNameInput;
 import static racingcar.View.inputView.displayGameStartMessage;
 import static racingcar.View.inputView.displayPlayCountMessage;
+import static racingcar.View.outputView.displayPlayResult;
+import static racingcar.View.outputView.displayWinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +36,26 @@ public class RaceGame {
             }
         }
         return winners;
+    }
+
+    public static void playGame(){
+        ArrayList<String> carNames = getCarsName();
+        int count = getPlayCount();
+        ArrayList<Car> cars = new ArrayList<>();
+
+        for(String carName: carNames){
+            cars.add(new Car(carName));
+        }
+
+        System.out.println("\n실행 결과");
+        for(int i = 0; i< count; i++){
+            for(Car car: cars){
+                car.putCarResult();
+            }
+            displayPlayResult(cars);
+            System.out.println();
+        }
+        displayWinner(getWinners(cars));
     }
 
 }
