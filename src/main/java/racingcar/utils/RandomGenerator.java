342d5510e50b0310b85e717024060f152e6db3c2
Isaac Han <isaachan233@gmail.com>
2023-10-30 20:32:42 +0900

    feat: OCP 원칙 적용해보기

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -3,10 +3,15 @@ package racingcar.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 /*
- * Car랑 generateRandomNumber는 관련이 없는거 같아서 나누었습니다.
+ * Car 랑 generateRandomNumber 는 관련이 없는거 같아서 나누었습니다.
  * */
-public class RandomGenerator {
-    public int generateRandomNumber(int min, int max) {
+public class RandomGenerator implements NumberGenerator{
+    private int generateRandomNumber(int min, int max) {
         return Randoms.pickNumberInRange(min, max);
+    }
+
+    @Override
+    public int generateNumber(int min, int max) {
+        return generateRandomNumber(min, max);
     }
 }
