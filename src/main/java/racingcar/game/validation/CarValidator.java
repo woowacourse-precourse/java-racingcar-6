75d51ed5739b2601ab360c563da59df2ad0d0194
황중섭 <jshwang9709@gmail.com>
2023-10-31 02:18:06 +0900

    fix(car): 자동차 이름이 1자 이상인지 검증

--- src/main/java/racingcar/game/validation/CarValidator.java
+++ src/main/java/racingcar/game/validation/CarValidator.java
@@ -4,12 +4,19 @@ public class CarValidator implements Validator{
 
     @Override
     public void execute(String name) {
+        validateHavingValue(name);
         validateNameLength(name);
         validateSideBlank(name);
     }
 
+    private void validateHavingValue(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("자동차의 이름은 필수값입니다.");
+        }
+    }
+
     private void validateNameLength(String name) {
-        if (name.length() > 5) {
+        if (name.length() < 1 || name.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름이 5자 이하가 아닙니다.");
         }
     }
