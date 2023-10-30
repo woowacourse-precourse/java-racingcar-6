e6af120c065670574d962dac6a0b21ecc3438fd9
dltjdn <hana40004@naver.com>
2023-10-31 01:24:22 +0900

    refactor(OutputView): 출력 형식 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,29 +2,28 @@ package racingcar.view;
 
 import java.util.List;
 import racingcar.model.Car;
-import racingcar.model.Cars;
 
 public class OutputView {
 
-    private static final String GAME_RESULT_TITLE = "실행 결과";
+    private static final String GAME_RESULT_TITLE = "\n실행 결과";
     private static final String GAME_WINNER_FORMAT = "최종 우승자 : %s\n";
     private static final String ONE_STEP = "-";
-    private static final String COLON = "%s : ";
+    private static final String GAME_STATUS_FORMAT = "%s : %s\n";
 
-    public void printResultTitle(){
+    public void printResultTitle() {
         System.out.println(GAME_RESULT_TITLE);
     }
 
-    public void printStatus(List<Car> cars){
-        for(Car car: cars){
-            System.out.printf(COLON, car.getName());
-            System.out.println(ONE_STEP.repeat(car.getStep()));
+    public void printStatus(List<Car> cars) {
+        for (Car car : cars) {
+            String steps = ONE_STEP.repeat(car.getStep());
+            System.out.printf(GAME_STATUS_FORMAT, car.getName(), steps);
         }
-
+        System.out.println();
     }
 
-    public void printWinner(List<String> winners){
-        System.out.printf(GAME_WINNER_FORMAT, String.join(",", winners));
+    public void printWinner(List<String> winners) {
+        System.out.printf(GAME_WINNER_FORMAT, String.join(", ", winners));
     }
 
 }
