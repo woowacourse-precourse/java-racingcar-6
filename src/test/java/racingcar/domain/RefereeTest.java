92fac6095a6e54ad6c63779e5951b075898e3dfb
Hoo-sung <gntjd135@gmail.com>
2023-10-30 10:22:09 +0900

    refactor(RefereeTest) : @ParameterizedTest를 사용하여 단독 우승자 테스트 케이스 리펙토링

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +14,16 @@ class RefereeTest {
 
     @Nested
     class SingleWinnerTests {
-        @Test
-        void 단독우승자인경우() {
+        @ParameterizedTest
+        @CsvSource({"4,3,2,pobi","1,2,3,jun","2,5,4,woni"})
+        void 단독우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
             List<Car> cars = new ArrayList<>();
-            cars.add(new Car("pobi", 4));
-            cars.add(new Car("woni", 3));
-            cars.add(new Car("jun", 2));
+            cars.add(new Car("pobi", pobiDistance));
+            cars.add(new Car("woni", woniDistance));
+            cars.add(new Car("jun", junDistance));
 
             List<String> answer = Referee.determineWinner(cars);
-            List<String> expected = List.of("pobi");
+            List<String> expected = List.of(expectedWinners);
             assertThat(answer).isEqualTo(expected);
         }
     }
