3551c8e0c4503b37bbea0025065e791f39e7bb86
Donggeon Moon <nullsafer@naver.com>
2023-10-30 18:40:21 +0900

    feat: 출력 관련 기능

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import static racingcar.util.PrintUtils.println;
+
+public class OutputView {
+    public void printRunResult(String runResultMessage) {
+        println(runResultMessage);
+    }
+
+    public void printRoundResult(String runResultMessage) {
+        println(runResultMessage);
+        println("");
+    }
+
+    public void printFinalWinner(String finalWinnerMessage) {
+        println(finalWinnerMessage);
+    }
+}
