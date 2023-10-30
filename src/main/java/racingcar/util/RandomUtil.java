eda37691e81f82c339dd6800f95c77e45fd22d01
jum0624 <jum0624@naver.com>
2023-10-30 21:40:15 +0900

    feat: 자동차 움직이기 구현

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -1,2 +1,9 @@
-package racingcar.util;public class RandomUtil {
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtil {
+    public static int createRandom() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
