a1b44a05541aa34a5fade3606d2e49dbd254b65e
Hoony0321 <eogns0321@gmail.com>
2023-10-30 16:34:05 +0900

    feat : GameService 객체 추가
    (cherry picked from commit ec96956d19eccb3e9da838bc470aa36157687f11)

---
+++ src/main/java/racingcar/service/GameService.java
@@ -0,0 +1,22 @@
+package racingcar.service;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class GameService {
+    private List<Car> cars;
+    private Integer tryCount;
+
+    public void initGame() {
+        // TODO: 게임을 초기화하는 메소드
+    }
+
+    public void playGame() {
+        // TODO: 게임을 진행하는 메소드
+    }
+
+    public void endGame() {
+        // TODO: 게임을 종료하는 메소드
+    }
+}
