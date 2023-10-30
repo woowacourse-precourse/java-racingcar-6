c9ef99a744988822e71b4cb1009bf0ca5cc86406
geunsu <geunsu23@naver.com>
2023-10-30 09:58:09 +0900

    add input/outputview

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,20 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void printCarNameNPosition(List<String>carNameList, List<Integer> carPositionList) {
+        for (int i=0;i<carNameList.size();i++) {
+            System.out.println(carNameList.get(i)+" : " + "-".repeat(carPositionList.get(i)));
+        }
+    }
+
+    public static void printWinnerNameList(List<String>winnerNameList) {
+        for (int i=0;i<winnerNameList.size();i++) {
+            System.out.print(winnerNameList.get(i));
+            if (i != winnerNameList.size()-1) { System.out.print(", "); }
+        }
+    }
+
 }
