cd5745d2ec6fe0c01237e81d90a4f960cea98c79
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-10-30 12:43:53 +0900

    feat: inputCarNames 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public String[] inputCarNames() {
+        String[] cars = Console.readLine().split(",");
+        return cars;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
