6a77d06b2b397fdedf57be08bcdc808db50377af
yeonsu <jack0810@kookmin.ac.kr>
2023-10-30 19:10:12 +0900

    feat: input race time, validate input is number

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,25 +3,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     static String[] carNames;
+    static Long raceTime;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try{
-            inputCarName();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        
+        inputCarName();
+        inputRaceTime();
     }
 
     public static void inputCarName() {
-        String tm = Console.readLine();
-        carNames = parseCarName(tm);
+        String input = Console.readLine();
+        carNames = parseCarName(input);
     }
-
     public static String[] parseCarName(String input) {
 
         String[] names = input.split(",");
+
         for (String name : names) {
             validateCarName(name);
         }
@@ -37,4 +34,15 @@ public class Application {
             throw new IllegalArgumentException("자동차의 이름이 5글자 이상입니다.");
         }
     }
+
+    public static void inputRaceTime() {
+        String input = Console.readLine();
+
+        try{
+            raceTime = Long.parseLong(input);
+        }catch (Exception e){
+            throw new IllegalArgumentException("잘못된 횟수 입력입니다.");
+        }
+    }
+
 }
