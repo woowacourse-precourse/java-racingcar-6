fb3fb2d1ff7a234ee55116c4ed2c28692002d667
pjaehyun <show6756@gmail.com>
2023-10-30 18:52:09 +0900

    feat: 추가 printWinners 함수 기능 테스트

--- src/test/java/racingcar/CarCollectionTest.java
+++ src/test/java/racingcar/CarCollectionTest.java
@@ -126,6 +126,26 @@ public class CarCollectionTest {
         );
     }
 
+    @Test
+    void printWinners_자동차들_중_가장_많이_이동한_자동차들을_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car1 = new Car("car1");
+                    Car car2 = new Car("car2");
+                    Car car3 = new Car("car3");
+
+                    CarCollection carCollection = createCarCollection(car1, car2, car3);
+
+                    carCollection.moveCarsForward();
+                    List<String> winners = carCollection.getWinners();
+                    carCollection.printWinners(winners);
+
+                    assertThat(output()).contains("최종 우승자 : car1, car3");
+                },
+                4, 3, 4
+        );
+    }
+
     private CarCollection createCarCollection(Car... cars) {
         CarCollection carCollection = new CarCollection();
 
