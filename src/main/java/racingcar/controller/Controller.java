07fcdca618f4ab1e488943d8a820c0796fb021ae
Seyeon <wjdtpdus828@naver.com>
2023-10-30 15:08:36 +0900

    feat(Controller): 자동차의 이름을 입력받는 getCarNames 메소드 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,4 +1,25 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.Views;
+import racingcar.service.gameService;
+
+import java.util.List;
+
 public class Controller {
+    Views view = new Views();
+    gameService service = new gameService();
+    public void startGame(){
+        makeCars();
+    }
+
+    public void makeCars(){
+        view.printInputCarMessage();
+        List<String> carNames = service.splitCarNames(getCarNames());
+    }
+
+    public String getCarNames(){
+        String input = Console.readLine();
+        return input;
+    }
 }
