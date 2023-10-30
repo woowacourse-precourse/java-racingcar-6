1bbe9aa90e6a0d43bd3e96bed7a97010fa86ff50
kimtaesoo <kimtaesoo7@naver.com>
2023-10-31 02:10:07 +0900

    test: TryCount 테스트 추가

---
+++ src/test/java/racingcar/model/TryCountTest.java
@@ -0,0 +1,44 @@
+package racingcar.model;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.exception.NonPositiveIntException;
+
+class TryCountTest {
+
+    @CsvSource({"-1", "0", "as"})
+    @ParameterizedTest
+    void 입력이_1이상의_정수가_아니라면_예외가_발생한다(String count) {
+        // when & then
+        assertThatThrownBy(() -> TryCount.from(count))
+                .isInstanceOf(NonPositiveIntException.class);
+    }
+
+    @Test
+    void 시도횟수가_감소한다() {
+        // given
+        TryCount tryCount = TryCount.from("1");
+
+        // when
+        tryCount.decreaseCount();
+
+        // then
+        assertThat(tryCount.isEnd()).isTrue();
+    }
+
+    @Test
+    void 동일한_횟수를_가진_TryCount_인스턴스는_동등하다() {
+        // given
+        TryCount result = TryCount.from("1");
+        TryCount expected = TryCount.from("1");
+
+        // When & Then
+        assertThat(result).isEqualTo(expected)
+               .hasSameHashCodeAs(expected);
+    }
+}
--- src/test/java/racingcar/util/ConverterTest.java
+++ src/test/java/racingcar/util/ConverterTest.java
@@ -1,14 +1,9 @@
 package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import racingcar.exception.NonPositiveIntException;
-
 
 class ConverterTest {
 
@@ -22,24 +17,5 @@ class ConverterTest {
 
         // then
         assertThat(result).containsExactly("A", "B", "C");
-    }
-
-    @CsvSource({"10, 10", "100, 100", "999, 999"})
-    @ParameterizedTest
-    void 문자열을_양의_정수로_바꾸어준다(String input, int expected) {
-        // when
-        int result = Converter.convertStringToPositiveInt(input);
-
-        // when & then
-        assertThat(result).isEqualTo(expected);
-    }
-
-
-    @CsvSource({"ABC", "-1", "0"})
-    @ParameterizedTest
-    void 문자열이_잘못된_값으로_들어오면_예외가_발생한다(String input) {
-        // then & then
-        assertThatThrownBy(() -> Converter.convertStringToPositiveInt(input))
-                .isInstanceOf(NonPositiveIntException.class);
     }
 }
