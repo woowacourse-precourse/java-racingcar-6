3c9451d0a36414aba35fc8102ab790178a26b814
jiu6525 <jiu6525@daum.net>
2023-10-30 15:41:57 +0900

    test : startEvent 전진, 정지 테스트

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,16 @@
 package racingcar.controller;
 
+import racingcar.model.RacingGameEvent;
 import racingcar.view.InputView;
+import racingcar.vo.RacingPreference;
 
 public class RacingCarController {
 
     public static void gameStart() {
         InputView inputView = new InputView();
-        inputView.startInfo();
+        RacingPreference racingPreference = inputView.startInfo();
+
+        RacingGameEvent racingGameEvent = new RacingGameEvent(racingPreference.getCars());
+        racingGameEvent.startEvent(racingPreference.getAttempt());
     }
 }
--- src/test/java/racingcar/model/RacingGameEventTest.java
+++ src/test/java/racingcar/model/RacingGameEventTest.java
@@ -1,8 +1,11 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.Map.Entry;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.vo.Car;
 
 class RacingGameEventTest {
 
@@ -12,5 +15,39 @@ class RacingGameEventTest {
         String[] cars = {"pobi", "woni", "jun"};
         RacingGameEvent racingGameEvent = new RacingGameEvent(cars);
         Assertions.assertThat(racingGameEvent.getPositionHistory().size()).isEqualTo(3);
+    }
+
+    @DisplayName("이동 할 경우 테스트")
+    @Test
+    public void startEventMoveTest() {
+        String[] cars = {"pobi"};
+        int attempt = 3;
+        RacingGameEvent racingGameEvent = new RacingGameEvent(cars);
+        for (int i = 0; i < attempt; i++) {
+            for (Entry<Car, List<Integer>> value : racingGameEvent.getPositionHistory().entrySet()) {
+                Car carName = value.getKey();
+                List<Integer> position = value.getValue();
+                position.add(carName.moveForward(true));
+            }
+        }
+        Assertions.assertThat(racingGameEvent.getPositionHistory().values().toArray())
+                .isEqualTo(List.of(List.of(0, 1, 2)).toArray());
+    }
+
+    @DisplayName("이동하지 않을 경우 테스트")
+    @Test
+    public void startEventStopTest() {
+        String[] cars = {"pobi"};
+        int attempt = 3;
+        RacingGameEvent racingGameEvent = new RacingGameEvent(cars);
+        for (int i = 0; i < attempt; i++) {
+            for (Entry<Car, List<Integer>> value : racingGameEvent.getPositionHistory().entrySet()) {
+                Car carName = value.getKey();
+                List<Integer> position = value.getValue();
+                position.add(carName.moveForward(false));
+            }
+        }
+        Assertions.assertThat(racingGameEvent.getPositionHistory().values().toArray())
+                .isEqualTo(List.of(List.of(0, 0, 0)).toArray());
     }
 }
