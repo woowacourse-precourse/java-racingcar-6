7d07a5cdbf82aee51ca55b69be17f441d2bacf09
baksakcci <pshsh910@gmail.com>
2023-10-30 21:34:08 +0900

    test($RacingGameController): test console output

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,5 +1,8 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.CarName;
+import racingcar.model.RacingNumber;
 import racingcar.view.InputView;
 
 public class RacingGameController {
@@ -12,5 +15,9 @@ public class RacingGameController {
         String names = inputView.inputCarNames();
         String attempts = inputView.inputAttempts();
         System.out.println("자동차 이름: " + names + "시도할 회수: " + attempts);
+
+        Car car = Car.makeCar(new CarName(names), new RacingNumber());
+        car.generateRandomNumber();
+        System.out.println(car.toString());
     }
 }
