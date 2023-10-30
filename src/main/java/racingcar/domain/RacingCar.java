6a590fd055974a28880dc81032808d8bba4920f3
Jaeyoung Lim <limjyjustin@gmail.com>
2023-10-30 17:28:51 +0900

    feat(RacingCar): 클래스 생성
    RacingCar 클래스 생성
    멤버 변수 및 생성자 추가

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class RacingCar {
+
+    private String name;
+    private int place;
+
+    public RacingCar(String name, int place) {
+        this.name = name;
+        this.place = 0;
+    }
+
+    // move 메서드
+    // display 메서드
+}
