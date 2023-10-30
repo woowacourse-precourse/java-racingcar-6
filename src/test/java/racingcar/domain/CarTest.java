b910fc408a0270cfd31721f798e7147607396728
sangchu <didi1484@gmail.com>
2023-10-30 15:28:07 +0900

    fix : test 메서드명 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class CarTest {
     @Test
-    void 랜덤_번호가_4_이상이면_전진() {
+    void moveForward_메서드로_랜덤_번호가_4_이상이면_전진() {
         Car car = new Car("상추");
         car.moveForward(3);
         assertThat(car.getDistance()).isEqualTo(0);
