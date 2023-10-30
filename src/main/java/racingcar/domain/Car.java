910323e52e6442f1794abcfb5f8fd733a7b610f3
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 21:20:52 +0900

    [refactor] : change location to result position method

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,20 +1,31 @@
 package racingcar.domain;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Car {
-    private String name;
+    private final CarName name;
     private int position = 0;
 
-    public Car(String carName) {
+    public Car(CarName carName) {
         this.name = carName;
     }
+
     public String getName() {
-        return name;
+        return name.getName();
     }
 
-    public void move() {
-        if (RandomNumber.randomNumber()) {
+    public void move(int randomNumber) {
+        if (RandomNumber.isMove(randomNumber)) {
             this.position++;
         }
+    }
+
+    public String resultPosition() {
+
+        return IntStream.range(0,getPosition())
+                .mapToObj(i -> "-")
+                .collect(Collectors.joining());
     }
 
     public int getPosition() {
