515f0da72ac598c3f9dec988b7b43e4302835de6
noteasymin <noteasymin@gmail.com>
2023-10-30 20:46:49 +0900

    feat: 우승자 출력 기능 구현
    현재 자동차들의 위치를 체크하여 가장 큰 스코어를 저장하고 이 스코어를 가진 자동차를 winner에 저장한다
    winner의 사이즈가 1이면 하나만 출력하고 2 이상인 경우 ", "와 함께 나머지 자동차들도 출력한다.

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import racingcar.domain.Car;
 import racingcar.service.CarService;
 import racingcar.service.UserService;
+import racingcar.view.CarView;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class CarController {
 
         List<Car> cars = CarService.createCars(UserService.getCarNames());
         CarService.startRacing(cars, UserService.getMoveCount());
+        CarView.printWinner(cars);
 
     }
 }
--- src/main/java/racingcar/view/CarView.java
+++ src/main/java/racingcar/view/CarView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarView {
@@ -10,5 +11,43 @@ public class CarView {
             System.out.println(car.getName() + " : " + "-".repeat(car.getLocation()));
         }
         System.out.println();
+    }
+
+    public static void printWinner(List<Car> cars) {
+        List<String> winner = new ArrayList<>();
+        int maxScore = getMaxScore(cars);
+
+        for (Car car : cars) {
+            if (car.getLocation() == maxScore) {
+                winner.add(car.getName());
+            }
+        }
+
+        if (winner.size() == 1) {
+            System.out.println(winner.get(0));
+        } else if (winner.size() >= 2) {
+            printWinners(winner);
+        }
+    }
+
+    private static int getMaxScore(List<Car> cars) {
+        int max_score = 0;
+
+        for (Car car : cars) {
+            if (car.getLocation() > max_score) {
+                max_score = car.getLocation();
+            }
+        }
+
+        return max_score;
+    }
+
+    private static void printWinners(List<String> winners) {
+        System.out.print(winners.get(0));
+        winners.remove(0);
+
+        for (String winner : winners) {
+            System.out.print(", " + winner);
+        }
     }
 }
