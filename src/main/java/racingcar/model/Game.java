a5d53dfac9308e073276220b0afe7f496a740d6c
HYUNJUN\sajun <sajun28@naver.com>
2023-10-31 01:19:07 +0900

    Refactor: "google java code에 따른 수정"

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -3,6 +3,7 @@ package racingcar.model;
 public class Game {
     private final int randomNum;
     private static final int MOVEMENT_NUM = 4;
+
     public Game(int randomNum) {
         this.randomNum = randomNum;
     }
