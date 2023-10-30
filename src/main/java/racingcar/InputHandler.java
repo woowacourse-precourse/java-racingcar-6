932e603fe06a726545d4ff4446ba7b4f56e84baf
Terry <t3rryahn.dev@gmail.com>
2023-10-30 21:26:47 +0900

    feat(입력클래스): InputHandler에 getCarNames, getNumberOfRound 메서드 추가

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,4 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputHandler {
+
+    public List<String> getCarNames() {
+        String userInput = Console.readLine();
+
+        String[] parts = userInput.split(",");
+        List<String> result = new ArrayList<>();
+
+        for (String part : parts) {
+            //공백제거 후 리스트에 추가
+            result.add(part.trim());
+        }
+
+        return result;
+    }
+
+    public int getNumberOfRound() {
+        String userInput = Console.readLine();
+        int result = Integer.parseInt(userInput);
+
+        return result;
+    }
 }
