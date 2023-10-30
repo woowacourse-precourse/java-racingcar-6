fd5f4852deaf911b293554c0e0b60dc053722212
Taehyoun Kim <kth9903@gmail.com>
2023-10-30 17:03:23 +0900

    refactor(libraries): remove unused imports

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,10 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
