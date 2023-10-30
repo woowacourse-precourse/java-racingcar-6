4099f426fa05b0e6c0f6d09ea7dd157a6b5718da
Hyunstone <hyun104467@naver.com>
2023-10-31 02:04:04 +0900

    [Feat] 입력값 유효성 검사 클래스 구현

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
+
+public class InputValidator {
+    public String validateInput(String input) {
+        for (String word: divideByComma(input)) {
+            if (!isCorrectLength(word) && !isAlphabetWord(word)) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return input;
+    }
+
+    private List<String> divideByComma(String input) {
+//        try {
+//            return List.of(input.split(","));
+//        } catch (PatternSyntaxException e) {
+//            throw new IllegalArgumentException(e);
+//        }
+        return List.of(input.split(","));
+    }
+    private boolean isCorrectLength(String word) {
+        return word.length() <= 5;
+    }
+
+    private boolean isAlphabetWord(String word) {
+        for (char c : word.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
