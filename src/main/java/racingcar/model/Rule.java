1e8b4e04a9d6522e4079f9603e0c6392f264204c
Hyeon-hak Kim <07ily@naver.com>
2023-10-31 08:43:53 +0900

    refactor: delete access modifier

--- src/main/java/racingcar/model/Rule.java
+++ src/main/java/racingcar/model/Rule.java
@@ -3,8 +3,8 @@ package racingcar.model;
 import java.util.List;
 
 public interface Rule {
-    public List<String> getCarNames();
-    public Integer getNumberOfRounds();
-    public void printCurrentPerformance();
-    public void printBestPerformers();
+    List<String> getCarNames();
+    Integer getNumberOfRounds();
+    void printCurrentPerformance();
+    void printBestPerformers();
 }
