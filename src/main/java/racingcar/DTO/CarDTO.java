534576450088d7a75a53f643af20625836ef55e8
Yeriimii <seojs0511@naver.com>
2023-10-31 01:04:03 +0900

    feat(CarDTO): 각 자동차의 이름과 상태 저장용 DTO 구현

---
+++ src/main/java/racingcar/DTO/CarDTO.java
@@ -0,0 +1,19 @@
+package racingcar.DTO;
+
+public class CarDTO {
+    private String name;
+    private int position;
+
+    public CarDTO(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
