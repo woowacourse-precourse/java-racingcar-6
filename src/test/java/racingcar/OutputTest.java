85e7936fd9b3278dfde1562ee4ae574267d308b8
choheejin <hjcho9510@gmail.com>
2023-10-30 17:29:33 +0900

    test(output): test for the individual winner output

--- src/test/java/racingcar/OutputTest.java
+++ src/test/java/racingcar/OutputTest.java
@@ -3,13 +3,13 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.constants.GameMessage;
 import racingcar.model.Car;
 import racingcar.view.OutputView;
 
@@ -38,6 +38,22 @@ public class OutputTest {
         OutputView.displayRacingSituation(car);
         String result = "pobi : -" + System.lineSeparator() + "java : --";
 
+        assertThat(result).isEqualTo(outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    @DisplayName("단독 우승자 출력 테스트")
+    void output_단독_우승자() {
+        List<String> names = List.of("pobi", "java");
+        int attempt = 3;
+        List<Integer> mileages = List.of(1, 2);
+
+        Car car = new Car(names, attempt, mileages);
+        car.initWinners();
+
+        OutputView.displayWinner(car);
+
+        String result = GameMessage.OUTPUT_WINNER_ANNOUNCEMENT.getMessage() + "java";
         assertThat(result).isEqualTo(outputStreamCaptor.toString().trim());
     }
 }
