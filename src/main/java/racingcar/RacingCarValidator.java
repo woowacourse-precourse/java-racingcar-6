3974a8007054652ba1d4010716c04124731e5152
seminchoi <choicco89@gmail.com>
2023-10-30 22:03:08 +0900

    feat(racingcar.RacingCarValidator) : RacingCar 생성 검증을 클래스 및 기능 구현

---
+++ src/main/java/racingcar/RacingCarValidator.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class RacingCarValidator {
+    private final int MAX_NAME_LENGTH = 5;
+
+    public void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
