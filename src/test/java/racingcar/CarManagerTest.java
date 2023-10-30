d11f52ed090c33811f0fd003690ea0d54855f846
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 16:37:31 +0900

    test(CarManager): 레이싱카 목록 생성 함수의 테스트 코드 작성

--- src/test/java/racingcar/CarManagerTest.java
+++ src/test/java/racingcar/CarManagerTest.java
@@ -1,4 +1,22 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarManagerTest {
-  
+    CarManager carManager = new CarManager();
+
+    @Test
+    void 입력받은_이름_목록의_레이싱카_목록을_생성한다() {
+        // given
+        String[] names = {"성겸", "성민", "성우"};
+
+        // when
+        carManager.createCars(names);
+
+        // then
+        Assertions.assertThat(carManager.getCars().getCars().size()).isEqualTo(3);
+    }
 }
