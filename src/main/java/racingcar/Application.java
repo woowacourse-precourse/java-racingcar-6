324d5ad3f58855cf838bd1254c958a81171bc8b7
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-30 23:24:56 +0900

    🎯feat(main) : main함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.domain.RacingGame;
+import racingcar.service.RandomGenerator;
+import racingcar.view.Input.InputParser;
+import racingcar.view.Input.InputValidator;
+import racingcar.view.Input.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame(new InputView(new InputParser(), new InputValidator()), new RandomGenerator());
+        racingGame.goOrStop();
     }
 }
