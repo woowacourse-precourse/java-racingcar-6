6b4171fdebf194dec5c06a276e5cbc828a41a3f8
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 15:16:23 +0900

    refactor: 자동차 이름의 책임 변경에 따른 테스트 코드 이동

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,27 +8,6 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
     @Test
-    public void 자동차이름_빈값() {
-        assertThatThrownBy(() -> new Car(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름을 입력해 주세요.");
-    }
-
-    @Test
-    public void 자동차이름_널() {
-        assertThatThrownBy(() -> new Car(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름을 입력해 주세요.");
-    }
-
-    @Test
-    public void 자동차이름_다섯글자초과() {
-        assertThatThrownBy(() -> new Car("ㅁㄴㅇㅁㄴㄴ"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 5글자를 초과할 수 없습니다.");
-    }
-
-    @Test
     public void 자동차_전진성공(){
         Car car = new Car("gj");
         RandomNumber randomNumber = new RandomNumber(4);
