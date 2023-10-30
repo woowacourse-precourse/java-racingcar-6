9f80167038ddbe19582f04a732c5eb7f26e4f5f0
gundorit <packdev937@gmail.com>
2023-10-30 20:58:05 +0900

    test (RandomNumber) : 1이상 9이하 수를 반환 하는지 테스트 한다

---
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("RandomNumber 클래스")
+class RandomNumberTest {
+
+    @Test
+    void 랜덤_값은_1_이상_9_이하_수를_반환() {
+        RandomNumber randomNumber = new RandomNumber();
+        int number = randomNumber.getRandomNumber();
+
+        assertTrue(number >= 1 && number <= 9);
+    }
+}
