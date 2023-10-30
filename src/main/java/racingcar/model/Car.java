3ed8dd338a67fb528e5c365c0dac35f1f70d8a00
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-31 05:37:24 +0900

    feat: Car 클래스 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private final int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+}
