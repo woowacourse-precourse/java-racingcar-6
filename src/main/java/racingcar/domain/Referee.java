494527c4785e0b0ccee278c433eeea49e2ee79ec
mangwonKing <aud1035@naver.com>
2023-10-30 20:02:52 +0900

    refactor(racingcar): Refree 의 출력문구 및 winner구분자 상수로 변경

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -3,17 +3,19 @@ package racingcar.domain;
 import java.util.List;
 
 public class Referee {
+    final String SYSTEM_MESSAGE = "최종 우승자 : ";
+    final String SEPARATE_WINNER = ",";
     public void printWinner(List<String> winner){
         String winners  = new String();
         int totalWinnerNumber = winner.size();
         for(String name : winner){
             winners += name;
             if(totalWinnerNumber > 1){
-                winners +=",";
+                winners += SEPARATE_WINNER;
                 totalWinnerNumber--;
             }
         }
-        System.out.print("최종 우승자 : ");
+        System.out.print(SYSTEM_MESSAGE);
         System.out.println(winners);
     }
 }
