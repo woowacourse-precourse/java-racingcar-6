b7f49537dfea8d95e223051cb98d056094dcf29d
pgd <rudeh1253@gmail.com>
2023-10-30 21:14:06 +0900

    feat: 게임 우승자 추출

--- src/main/java/racingcar/game/GameManager.java
+++ src/main/java/racingcar/game/GameManager.java
@@ -1,18 +1,58 @@
 package racingcar.game;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.input.InputAdaptor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GameManager {
-    private final String[] cars;
-    private final int numOfTries;
+    private final Map<String, Integer> cars;
 
-    public GameManager(String[] cars, int numOfTries) {
-        this.cars = cars;
-        this.numOfTries = numOfTries;
+    public GameManager(String[] carNames) {
+        this.cars = new HashMap<>();
+        for (String car : carNames) {
+            this.cars.put(car, 0);
+        }
     }
 
-    private boolean isSuccessToProceed() {
+    public void proceed(String car) {
+        int previousState = this.cars.get(car);
+        this.cars.put(car, previousState + 1);
+    }
+
+    public int getCarState(String car) {
+        return this.cars.get(car);
+    }
+
+    public List<String> getWinners() {
+        int max = 0;
+        for (int state : cars.values()) {
+            max = getMax(max, state);
+        }
+
+        List<String> winners = new ArrayList<>();
+        for (String car : cars.keySet()) {
+            addWinner(max, winners, car);
+        }
+        return winners;
+    }
+
+    private int getMax(int max, int state) {
+        if (state > max) {
+            max = state;
+        }
+        return max;
+    }
+
+    private void addWinner(int max, List<String> winners, String car) {
+        if (cars.get(car) == max) {
+            winners.add(car);
+        }
+    }
+
+    public static boolean isSuccessToProceed() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
---
+++ src/test/java/racingcar/game/GameManagerTest.java
@@ -0,0 +1,98 @@
+package racingcar.game;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class GameManagerTest {
+    GameManager gameManager;
+    static final String[] CAR_SAMPLES = {
+            "john",
+            "jenny",
+            "doe"
+    };
+
+    @BeforeEach
+    void init() {
+        gameManager = new GameManager(CAR_SAMPLES);
+    }
+
+    @Test
+    void testProceed() {
+        int[] times = {
+                1, 3, 2
+        };
+
+        setting(times);
+
+        int idx = 0;
+        for (String car : CAR_SAMPLES) {
+            Assertions.assertThat(gameManager.getCarState(car)).isEqualTo(times[idx++]);
+        }
+    }
+
+    private void setting(int[] times) {
+        for (int i = 0; i < CAR_SAMPLES.length; i++) {
+            proceed(times[i], CAR_SAMPLES[i]);
+        }
+    }
+
+    private void proceed(int time, String car) {
+        for (int j = 0; j < time; j++) {
+            gameManager.proceed(car);
+        }
+    }
+
+    @Test
+    void testGetWinners_oneWinner() {
+        int[] aheadTimes = {
+                3, 1, 0
+        };
+
+        setting(aheadTimes);
+
+        List<String> winners = gameManager.getWinners();
+        Assertions.assertThat(winners.size()).isEqualTo(1);
+        Assertions.assertThat(winners.get(0)).isEqualTo(CAR_SAMPLES[0]);
+    }
+
+    @Test
+    void testGetWinners_twoWinners() {
+        int[] aheadTimes = {
+                3, 3, 2
+        };
+
+        setting(aheadTimes);
+
+        List<String> winners = gameManager.getWinners();
+        Assertions.assertThat(winners.size()).isEqualTo(2);
+        Assertions.assertThat(winners).contains(CAR_SAMPLES[0], CAR_SAMPLES[1]);
+    }
+
+    @Test
+    void testGetWinners_noProceed() {
+        int[] aheadTimes = {
+                0, 0, 0
+        };
+
+        setting(aheadTimes);
+
+        List<String> winners = gameManager.getWinners();
+        Assertions.assertThat(winners.size()).isEqualTo(3);
+        Assertions.assertThat(winners).contains(CAR_SAMPLES[0], CAR_SAMPLES[1], CAR_SAMPLES[2]);
+    }
+
+    @Test
+    void testIsSuccessToProceed_randomness() {
+        int tryTimes = 10000;
+        boolean[] results = new boolean[tryTimes];
+        for (int i = 0; i < tryTimes; i++) {
+            results[i] = GameManager.isSuccessToProceed();
+        }
+
+        boolean firstValue = results[0];
+        Assertions.assertThat(results).contains(!firstValue);
+    }
+}
