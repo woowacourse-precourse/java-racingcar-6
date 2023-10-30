5aefc4c6f061387413671a9606151825918ed83f
BINBEAN777 <peanutchan@naver.com>
2023-10-30 22:25:30 +0900

    <Feat> : "승자 출력 메소드 추가 "

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -10,38 +10,35 @@ public class RacingGame {
     public void start() {
         Output.tellStartAndWantName();
         String[] carName = Input.getCarName();
-        Output.tellGroupName(carName);
+        // Output.tellGroupName(carName);
         HashMap<String, Integer> userInfo = new HashMap<>(carName.length);
         for (String name : carName) {
             userInfo.put(name, 0);
         }
 
         int currentRoundNum = 0;
+        Output.tellHowManyRoundWant();
         int gameRoundNum = Input.getGameRoundNum();
 
         while (gameRoundNum != currentRoundNum) {
             countGo(userInfo, carName);
             Output.tellCurrent(userInfo, carName);
             currentRoundNum += 1;
+            System.out.println();
         }
+        int numOfWinnerGoNum = Output.findNumOfWinner(userInfo, carName, gameRoundNum);
+        Output.showWinner(Output.findWinner(userInfo, carName, numOfWinnerGoNum));
 
     }
 
     void countGo(HashMap name, String[] groupName) {
         for (String carName : groupName) {
             if (Randoms.pickNumberInRange(0, 9) >= 4) {
-               name.put(carName,(Integer)name.get(carName)+1);
+                name.put(carName, (Integer) name.get(carName) + 1);
             }
         }
     }
 
-
-    void tellWinner(HashMap name, String[] groupName, int gameRoundNum) {
-
-        int totalRoundSize = gameRoundNum;
-        while (containsValue == gameRoundNum)
-
-    }
 
 }
 
--- src/main/java/racingcar/UserInfo.java
+++ src/main/java/racingcar/UserInfo.java
@@ -1,11 +1,7 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 public class UserInfo {
-    int goNum=0;
-public void judgeGoOrStop(){
-    int randomNum = Randoms.pickNumberInRange(0,9);
-    boolean judgeNum;
-    if(randomNum>=4){goNum +=1;}
-}
 
 }
