646829e49a812f0e2de7f1134c7506f7543426e1
kimwanyoung <dhks2869@gmail.com>
2023-10-31 08:29:49 +0900

    test : RacingController 경주 테스트

---
+++ src/test/java/controller/RacingControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+public class RacingControllerTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+    RacingController racingController = new RacingController();
+
+    @Test
+    void 전진_정지() {
+        assertRandomNumberInRangeTest(() -> {
+            run("pobi,woni,jiny", "2");
+            assertThat(output()).contains("pobi : -", "woni : ", "jiny : -", "pobi : --", "woni : ",
+                    "jiny : --", "최종 우승자 : pobi,jiny");
+        }, MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD);
+    }
+
+    @Override
+    public void runMain() {
+        racingController.start();
+    }
+}
