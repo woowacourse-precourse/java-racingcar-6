4665dbe23e97f0cfd5c28e853471b3160206c836
zzawang <hobin0116@naver.com>
2023-10-30 22:07:55 +0900

    refactor: 메인 메소드를 기능 단위로 분리하여 개선
    - init(): UserInputException 객체를 생성하고 InputView와 OutputView를 초기화한다.
        - 메소드를 분리함에 따라 userInputException를 지역변수로 수정하였다.
    - getUserInput(): carNames와 attemptCount를 사용자 입력에 따라 초기화한다.
    - getRacingCars(): carNames에 따라 Car 객체를 생성하고 cars 리스트에 추가한다.
        - 메소드를 분리함에 따라 cars 리스트를 Application의 멤버 변수로 수정하였다.
    - gameStart(): RacingGame 객체를 생성하고 게임을 시작한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,23 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    private static UserInputException userInputException;
     private static InputView inputView;
     private static OutputView outputView;
     private static List<String> carNames;
+    private static List<Car> cars;
     private static int attemptCount;
 
     public static void main(String[] args) {
-        userInputException = new UserInputException();
+        init();
+        getUserInput();
+        getRacingCars();
+        gameStart();
+    }
+
+    private static void init() {
+        UserInputException userInputException = new UserInputException();
         inputView = new InputView(userInputException);
         outputView = new OutputView();
+    }
+
+    private static void getUserInput() {
         carNames = inputView.getCarName();
         attemptCount = inputView.getAttemptCount();
-        List<Car> cars = new ArrayList<>();
+    }
+
+    private static void getRacingCars() {
+        cars = new ArrayList<>();
         for (String carName : carNames) {
             Car car = new Car(carName);
             cars.add(car);
         }
+    }
+
+    private static void gameStart() {
         RacingGame racingGame = new RacingGame(cars, attemptCount, outputView);
         racingGame.gameStart();
     }
