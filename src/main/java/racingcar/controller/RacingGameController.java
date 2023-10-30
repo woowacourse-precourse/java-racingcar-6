20de7aa2e48c38e92a144d139bad1c853403cdd7
eora21 <eora21@naver.com>
2023-10-30 14:53:38 +0900

    feat: 🎸 (RacingGameController) 추가 레이싱 게임을 조작하는 컨트롤러 작성

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,30 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.CarFactory;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGameStatus;
+import racingcar.domain.RacingGame;
+import racingcar.domain.WinnerNames;
+import racingcar.view.View;
+
+public class RacingGameController {
+    private final View view;
+
+    public RacingGameController(View view) {
+        this.view = view;
+    }
+
+    public void play() {
+        List<String> carNames = view.getCarNames();
+        Cars cars = CarFactory.createRacingCars(carNames);
+        RacingGame racingGame = new RacingGame(cars);
+
+        int racingCount = view.getRacingCount();
+        RacingGameStatus racingGameStatus = racingGame.race(racingCount);
+
+        view.showRacingGameStatus(racingGameStatus);
+        WinnerNames winnerNames = racingGame.getWinnerNames();
+        view.showWinnerNames(winnerNames);
+    }
+}
