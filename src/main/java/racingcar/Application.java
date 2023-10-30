b1078df983fce67106d44a9c5d51e7c578cc4fc8
rbgusdlza <rbgusdlza@gmail.com>
2023-10-31 00:20:29 +0900

    feat: 사용자 입력을 요청하는 출력 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        requestCarName();
+        requestCount();
+    }
+
+    static void requestCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    static void requestCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
