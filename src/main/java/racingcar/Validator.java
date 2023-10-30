bfb77b03b91633cc4911baa6ae9c65bf76df5326
SUJI-J <suzy852@naver.com>
2023-10-30 20:19:57 +0900

    refactor: 메소드 인수 재구성

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -9,24 +9,22 @@ import static racingcar.GameMessage.INPUT_BLANK_EXCEPTION_MESSAGE;
 public class Validator {
 
     public static Map<String, String> inputCarNames(Map<String, String> racingCars) {
-        isEmptyName(racingCars);
-        isValidLength(racingCars);
+        for (String carName : racingCars.keySet()) {
+            isEmptyName(carName);
+            isValidLength(carName);
+        }
         return racingCars;
     }
 
-    private static void isValidLength(Map<String, String> racingCars) {
-        for (String carName : racingCars.keySet()) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException(INPUT_LENGTH_EXCEPTION_MESSAGE);
-            }
+    private static void isValidLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(INPUT_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
-    private static void isEmptyName(Map<String, String> racingCars) {
-        for (String carName : racingCars.keySet()) {
-            if (carName.isBlank()) {
-                throw new IllegalArgumentException(INPUT_BLANK_EXCEPTION_MESSAGE);
-            }
+    private static void isEmptyName(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException(INPUT_BLANK_EXCEPTION_MESSAGE);
         }
     }
 
