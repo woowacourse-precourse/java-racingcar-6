315ffb9f8eb84b181483b24447c50d6b35b1ea69
tfer2442 <tfer2442@gmail.com>
2023-10-30 19:29:42 +0900

    feat(RacingGameMessage): 게임 관련 입력, 출력 메시지 생성

---
+++ src/main/java/racingcar/RacingGameMessage.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class RacingGameMessage {
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_RUNNING = "실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+}
