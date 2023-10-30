b09e56c5b43d6df43e5f72e1f7c814b6485ccc47
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-10-31 00:52:24 +0900

    test(Cars): 승자가 여러명일 때 출력 테스트 코드 추가 및 테스트 완료

--- src/test/java/racingcar/Model/CarsTest.java
+++ src/test/java/racingcar/Model/CarsTest.java
@@ -1,12 +1,15 @@
 package racingcar.Model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,7 +28,15 @@ class CarsTest {
 
     @Test
     @DisplayName("차의 총 대수 테스트")
-    void carsLength() {
+    void carsLengthTest() {
         assertEquals(cars.carsLength(), 4);
+    }
+
+    @Test
+    @DisplayName("최종 우승자 이름 가져오기 - 여러 명인 경우")
+    void getWinnerTest() {
+        List<String> expectResult = Arrays.asList("pobi", "woni", "jun", "Joana");
+
+        assertThat(cars.getWinner()).isEqualTo(expectResult);
     }
 }
