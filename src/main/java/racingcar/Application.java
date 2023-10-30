183a5067ec5bf3923f8f423c9251b884ea2c9a16
DevFrog92 <yongsu1481@gmail.com>
2023-10-30 14:20:58 +0900

    refactor: add racing game starter login in main method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.domain.game.Game;
+import racingcar.domain.game.RacingGame;
+import racingcar.domain.player.RacingGameParticipant;
+import racingcar.domain.player.RacingPlayer;
+import racingcar.util.GameConsoleUtil;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingPlayer player = new RacingGameParticipant(GameConsoleUtil.getInstance());
+        Game racingGame = new RacingGame(player);
+        racingGame.start();
     }
 }
