a63533f17f0b11287ea5c4c28e7454125c8bd42a
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-10-30 10:41:35 +0900

    feat: 사용자에게 몇 번 이동할 것인지 시도할 횟수 입력받는 기능 구현
    - 숫자가 아니거나 0 이하의 숫자를 입력하면 IleegalArgumentException을 발생시킵니다.

--- src/test/java/racingcar/GameService.java
+++ src/test/java/racingcar/GameService.java
@@ -3,8 +3,25 @@ package racingcar;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class GameService {
+    private static final String ErrorMessage = "0보다 큰 숫자를 입력해주세요.";
     public static String getUserInputForCars() {
         return readLine();
+    }
+
+    public static int getUserInputForNumberOfMatches() {
+        try {
+            int number = Integer.parseInt(readLine());
+            return validateNumber(number);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ErrorMessage);
+        }
+    }
+
+    private static int validateNumber(int number) {
+        if (number > 0) {
+            return number;
+        }
+        throw new IllegalArgumentException(ErrorMessage);
     }
 
 
