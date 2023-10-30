9c1dfd9d2b7184ca65ae16efed34402c0b24ffcb
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-30 15:05:50 +0900

    refactor: change Numbers.map initialization

--- src/main/java/racingcar/Numbers.java
+++ src/main/java/racingcar/Numbers.java
@@ -2,20 +2,15 @@ package racingcar;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Numbers {
     Map<Integer, Integer> map = new HashMap<>();
     public Numbers() {
-        map.put(0, 0);
-        map.put(1, 0);
-        map.put(2, 0);
-        map.put(3, 0);
-        map.put(4, 1);
-        map.put(5, 1);
-        map.put(6, 1);
-        map.put(7, 1);
-        map.put(8, 1);
-        map.put(9, 1);
+        IntStream.range(0, 10).forEach(i -> {
+            if (i < 4) map.put(i, 0);
+            else map.put(i, 1);
+        });
     }
 
     public int compareFour(int number) {return this.map.get(number);}
