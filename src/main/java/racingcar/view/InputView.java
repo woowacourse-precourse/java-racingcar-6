32353ddf0635c1d016a76fa57fff3d50cf4943df
newh08 <newh08@gmail.com>
2023-10-30 11:29:20 +0900

    ♻️REFACTOR: InputView 메서드 리턴 타입 변경, 메서드 이름변경
    기존 메서드는 String 을 반환해 InputView 의 역할이 거의 없었지만, 입력받는 인풋 값에 대한 객체를 직접 반환하도록 변경.
    메서드 이름 변경: inputAttemptTies -> inputTotalMoveNumber

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,22 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.car.Cars;
+import racingcar.domain.car.carmove.CarMoveReader;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public String inputCarNames() {
+    public Cars inputCarNames() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
-        return Console.readLine();
+        String inputCarName = Console.readLine();
+        return Cars.makeCars(inputCarName);
     }
 
-    public String inputAttemptTimes() {
+    public CarMoveReader inputTotalMoveNumber() {
         System.out.println(INPUT_ATTEMPT_NUMBER_MESSAGE);
-        return Console.readLine();
+        String totalMoveNumber = Console.readLine();
+        return CarMoveReader.makeCarMoveReader(totalMoveNumber);
     }
 }
