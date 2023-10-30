ca11b7bbe7d73c73c31e1e764585c79d0d8d0bda
sangchu <didi1484@gmail.com>
2023-10-30 14:35:36 +0900

    test : findMaxDistanceCar 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class CarsTest {
     @Test
-    void 입력_값을_콤마로_나누어_리스트에_저장() {
+    void createCarList_메서드로_입력_값을_콤마로_나누어_리스트에_저장() {
         String input = "pobi,woni,jun";
         Cars cars = new Cars();
         List<Car> carList = cars.createCarList(input);
@@ -15,5 +15,21 @@ class CarsTest {
                 .map(x -> x.getName())
                 .toArray(String[]::new);
         assertThat(nameArr).contains("pobi", "woni", "jun");
+    }
+
+    @Test
+    void findMaxDistanceCar_메서드로_가장_많은_거리를_이동한_차량_찾기() {
+        String input = "pobi,woni,jun";
+        Cars cars = new Cars();
+        List<Car> carList = cars.createCarList(input);
+        carList.get(0).moveForward(5);
+        carList.get(1).moveForward(3);
+        carList.get(2).moveForward(2);
+        Car maxDistanceCar = cars.findMaxDistanceCar();
+        assertThat(maxDistanceCar.getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    void findWinner() {
     }
 }
