d96a84a207928276821a738a92a62e2dc6e15fb3
seminchoi <choicco89@gmail.com>
2023-10-30 23:20:27 +0900

    feat(racingcar.RacingCarGameMachineValidator) : RacingCarGameMachineValidator 클래스 생성, public 메소드 정의

---
+++ src/main/java/racingcar/RacingCarGameMachineValidator.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.List;
+
+public class RacingCarGameMachineValidator {
+
+    public void validateNameList(List<String> racingCarNameList) {
+    }
+
+    public void validateRoundCount(int roundCount) {
+    }
+}
