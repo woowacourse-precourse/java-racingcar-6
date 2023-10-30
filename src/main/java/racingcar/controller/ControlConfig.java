61fa3130bf13b48095bf44f45e269770a693b36e
howudong <tjdvy963@naver.com>
2023-10-31 00:18:54 +0900

    refactor : playController와 Resultcontroller 생성함수의 매개변수가 service가 아닌 cars 객체를 받음으로써 좀 더 편하게 작동

--- src/main/java/racingcar/controller/ControlConfig.java
+++ src/main/java/racingcar/controller/ControlConfig.java
@@ -1,5 +1,8 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.domain.LimitScoreMoveRule;
+import racingcar.domain.RandomNumberGenerator;
 import racingcar.service.CarService;
 import racingcar.view.inputview.InputValidator;
 import racingcar.view.inputview.SettingInputView;
@@ -10,19 +13,20 @@ import racingcar.view.outputview.SettingOutputView;
 public class ControlConfig {
     public Controller createSettingController() {
         return new SettingController(new SettingInputView(new InputValidator()), new SettingOutputView());
-
     }
 
-    public Controller createRacingController(Object param) {
-        if (param instanceof CarService service) {
-            return new RacingController(new RacingOutputView(), service);
+    public Controller createPlayController(Object param) {
+        if (param instanceof Cars cars) {
+            return new playController(new PlayOutputView(),
+                    new CarService(cars, new LimitScoreMoveRule(new RandomNumberGenerator())));
         }
         return null;
     }
 
     public Controller createResultController(Object param) {
-        if (param instanceof CarService service) {
-            return new ResultController(new ResultOutputView(), service);
+        if (param instanceof Cars cars) {
+            return new ResultController(new ResultOutputView(),
+                    new CarService(cars, new LimitScoreMoveRule(new RandomNumberGenerator())));
         }
         return null;
     }
