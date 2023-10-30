1a48cb7a31a4bd7a1911fe49c7e731af7bfa2033
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-30 23:29:16 +0900

    자동차의 위치를 "-" 로 출력하는 메서드

--- src/main/java/racingcar/game/CarRaceGame.java
+++ src/main/java/racingcar/game/CarRaceGame.java
@@ -1,5 +1,6 @@
 package racingcar.game;
 
+import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.service.CarRaceService;
@@ -22,6 +23,22 @@ public class CarRaceGame implements Game {
     private void saveRaceRound(String input) {
         Validate.isNumber(input);
         this.gameRound = Integer.parseInt(input);
+    }
+
+//    private void printResult() {
+//        System.out.println("실행 결과");
+//        List<String> names = service.getCarsName();
+//        List<Integer> locations = service.getCarsLocation();
+//        for (int i = 0; i < names.size(); i++) {
+//            System.out.println();
+//        }
+//    }
+
+    private void printLocation() {
+        List<Integer> locations = service.getCarsLocation();
+        for (int i = 0; i < locations.size(); i++) {
+            System.out.print("-");
+        }
     }
 
 
