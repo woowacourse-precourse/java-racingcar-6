2ef31eafab0d606439dfeb358074c0b117ca9be4
hongkikii <hyangmeeh@icloud.com>
2023-10-30 19:39:51 +0900

    feat: 자동차 이름 부여 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
     public Game() {
@@ -8,9 +10,24 @@ public class Game {
 
     public void run() {
         alertEnterCarName();
+        SaveCarName();
     }
 
     private void alertEnterCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    private void SaveCarName() {
+        String names = Console.readLine();
+        String[] nameArray = names.split(",");
+        if (nameArray.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (String name : nameArray) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            Car car = new Car(name);
+        }
     }
 }
