714548a5b3d5a71d332ba941688572a1e4750e2d
HeungY <rkrkrkrk02@naver.com>
2023-10-31 02:52:28 +0900

    feat: 전진 진행 상황 출력하는 PrintStopAndGo 구현

--- src/main/java/racingcar/PrintStopAndGo.java
+++ src/main/java/racingcar/PrintStopAndGo.java
@@ -5,9 +5,10 @@ import java.util.List;
 public class PrintStopAndGo {
 
     void printStopAndGo(List<String> racingCar, List<Integer> score){
+        String hyphen = "-";
 
         for(int i=0;i<racingCar.size();i++){
-            System.out.println(racingCar.get(i)+": "+score.get(i));
+            System.out.println(racingCar.get(i)+": "+hyphen.repeat(score.get(i)));
         }
     }
 }
