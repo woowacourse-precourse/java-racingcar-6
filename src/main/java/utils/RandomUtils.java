17296ec28ac2f54b900ebeafc1f406f0a7588481
dbstn1325 <dbstn1325@naver.com>
2023-10-30 19:59:28 +0900

    feat(RandomUtils): 0~9 사이에서 무작위 값을 생성하는 기능 구현

---
+++ src/main/java/utils/RandomUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtils {
+
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
