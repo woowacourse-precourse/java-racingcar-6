81dc05a8443c740fcd9a588ce88f60d62d3b4691
ay-eonii <naayen@naver.com>
2023-10-31 02:04:29 +0900

    fix: 오타 수정

--- src/main/java/racingcar/domain/Random.java
+++ src/main/java/racingcar/domain/Random.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Random {
     private static final int MIN_RANGE = 0;
-    private static final int MAX_RANGE = 0;
+    private static final int MAX_RANGE = 9;
 
     public int createNumber() {
         return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
