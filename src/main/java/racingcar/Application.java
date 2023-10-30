1257125b7062c74f85a9d6d6a4c1868c741dc03d
SeongJunChun <csj991210@naver.com>
2023-10-31 08:39:30 +0900

    STYLE  : 사용안하는 메서드 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,7 @@
 package racingcar;
 
-import racingcar.View.InputMessage;
-import racingcar.View.SystemMessage;
+
 import racingcar.controller.GameController;
-import racingcar.model.CarAction;
-import racingcar.model.Cars;
-import racingcar.model.Referee;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static java.util.stream.Collectors.joining;
 
 public class Application {
     public static void main(String[] args) {
