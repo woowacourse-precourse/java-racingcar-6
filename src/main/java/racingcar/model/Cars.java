fcc1aaa5f0a3db4fe14e5df984ef9861a6350bfe
dmong <dt2980_hee@naver.com>
2023-10-30 13:33:11 +0900

    feat: 최종 우승자 이름 리스트를 반환하는 메소드 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private static final String DUPLICATED_NAME_ERROR_MESSAGE = "중복된 이름 삽입 불가";
@@ -31,5 +32,18 @@ public class Cars {
         return cars.stream().noneMatch(car -> car.getName().equals(newName));
     }
 
- 
+    private Car getMaxDistanceCar() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException(CARS_RANGE_ERROR_MESSAGE));
+    }
+
+    public List<String> findWinners() {
+        return cars.stream()
+                .filter(getMaxDistanceCar()::isSameDistance)
+                .map(Car::getName)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+
 }
