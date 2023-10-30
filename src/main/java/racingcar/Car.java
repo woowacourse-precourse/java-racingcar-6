7710bb0e13fc49b58521352add0534569b9cb948
juns1s <juns1s@naver.com>
2023-10-30 13:59:27 +0900

    feat: Car 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car implements Comparable<Car> {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        int rand = Randoms.pickNumberInRange(0, 9);
+
+        if (rand >= 4) {
+            this.position++;
+        } else {
+
+        }
+        System.out.println(name + " : " + "-".repeat(position));
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Integer.compare(o.getPosition(), this.position);
+    }
+}
