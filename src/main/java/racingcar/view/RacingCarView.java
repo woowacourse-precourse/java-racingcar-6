d72314a87a0953fe4b94fc1b387a44de275028bc
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 03:49:57 +0900

    feat: 최종 우승자를 보여주는 View 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -3,14 +3,16 @@ package racingcar.view;
 import racingcar.domain.Car;
 import racingcar.domain.CarList;
 
+import java.util.List;
+
 public class RacingCarView {
 
     private final String LOCATION = "-";
 
+
     public void carNameListInputPrint() {
         System.out.println("경주할 자동차 이름을 입력하시오");
     }
-
 
     public void readCountPrint() {
         System.out.println("시도할 회수는 몇회인가요?");
@@ -22,6 +24,11 @@ public class RacingCarView {
             System.out.println(car.getName() + " : " + LOCATION.repeat(location));
         }
         System.out.println();
+    }
+
+    public void printWinner(List<String> winnerList) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winnerList));
     }
 
 }
