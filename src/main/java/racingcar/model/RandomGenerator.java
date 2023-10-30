8dc6d31078d09ae72fabba0a6caee23e47225274
ksw13 <ksw48483@gmail.com>
2023-10-30 12:45:36 +0900

    refactor: 랜덤 숫자 생성시 매직 넘버 제거

--- src/main/java/racingcar/model/RandomGenerator.java
+++ src/main/java/racingcar/model/RandomGenerator.java
@@ -3,7 +3,10 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
     public int createNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
