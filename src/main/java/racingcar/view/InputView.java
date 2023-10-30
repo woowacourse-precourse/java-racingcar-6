3894eeb6c6b5433febf6c7747f2708de104e8319
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:41:35 +0900

    feat(racingcar): InputView에 자동차 이름을 입력받는 메소드, 공백 여부 검증 메소드 추가 자동차 이름을 입력받아 CarNameRequest 리스트로 매핑하여 반환하는 static 메소드를 추가하였습니다. 입력값 공백 여부를 검증하여 예외를 발생시키는 메소드를 추가하였습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,30 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.dto.request.CarNameRequest;
+import racingcar.exception.UserInputException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static racingcar.config.GameConfig.INPUT_NAMES_SEPARATOR;
+import static racingcar.exception.errorcode.UserInputErrorCode.INPUT_VALUE_BLANK;
+
 public final class InputView {
     private InputView() {
 
+    }
+
+    public static List<CarNameRequest> requestCarNames() {
+        final String carNames = Console.readLine();
+        validateBlank(carNames);
+        return Arrays.stream(carNames.split(INPUT_NAMES_SEPARATOR)).map(CarNameRequest::new).collect(Collectors.toList());
+    }
+
+    private static void validateBlank(final String input) {
+        if (input.isBlank()) {
+            throw new UserInputException(INPUT_VALUE_BLANK);
+        }
     }
 }
