9ffde6931ce87a1fa80b943f1d1a39028028d460
belljun3395 <195850@jnu.ac.kr>
2023-10-30 17:14:04 +0900

    refactor(domain/system): RacingGameTest 가독성을 위해 GameTestFactory 추가 및 테스트를 위한 상수 정리

--- src/test/java/racingcar/domain/system/game/RacingGameTest.java
+++ src/test/java/racingcar/domain/system/game/RacingGameTest.java
@@ -1,6 +1,13 @@
 package racingcar.domain.system.game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getCoWinnerTestHost;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getConsoleViewer;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getTestCarManager;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getTestCarNameReader;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getTestCarPositionManager;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getTestHost;
+import static racingcar.domain.system.game.RacingGameTest.GameTestFactory.getTestTryCountReader;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +16,7 @@ import racingcar.domain.core.car.MoveSignal;
 import racingcar.domain.core.car.MovingCar;
 import racingcar.domain.core.car.OnRaceCar;
 import racingcar.domain.system.host.Host;
+import racingcar.domain.system.manager.car.CarManager;
 import racingcar.domain.system.manager.car.InMemoryCarManager;
 import racingcar.domain.system.manager.car.value.SavedCar;
 import racingcar.domain.system.manager.position.CarPositionManager;
@@ -16,34 +24,22 @@ import racingcar.domain.system.manager.position.InMemoryCarPositionManager;
 import racingcar.domain.system.manager.position.value.SavedCarPosition;
 import racingcar.domain.system.reader.CarNameReader;
 import racingcar.domain.system.reader.TryCountReader;
-import racingcar.initializer.ConsoleGameFactory;
-import racingcar.initializer.GameFactory;
+import racingcar.domain.system.viewer.ConsoleViewer;
+import racingcar.domain.system.viewer.Viewer;
 
 class RacingGameTest {
 
+    private static final Long MOVE = 5L;
+    private static final Long STOP = 1L;
     private static final String CAR_A_NAME = "a";
     private static final String CAR_B_NAME = "b";
     private static final String CAR_C_NAME = "c";
     private static final int CAR_A_POSITION = 3;
     private static final int CAR_B_POSITION = 2;
     private static final int CAR_C_POSITION = 1;
-
     private static final int COWINNER_CAR_A_POSITION = 3;
     private static final int COWINNER_CAR_B_POSITION = COWINNER_CAR_A_POSITION;
     private static final int COWINNER_CAR_C_POSITION = 1;
-    private static final Long MOVE = 5L;
-    private static final Long STOP = 1L;
-
-    /**
-     * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(2), C(1)
-     */
-    private static final Long[] MOVE_DATA = {MOVE, STOP, STOP, MOVE, MOVE, STOP, MOVE, MOVE, MOVE};
-
-    /**
-     * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(3), C(1)
-     */
-    private static final Long[] COWINNER_MOVE_DATA = {MOVE, MOVE, STOP, MOVE, MOVE, STOP, MOVE,
-        MOVE, MOVE};
     private static final int[] EXPECTED_POSITIONS = {CAR_A_POSITION, CAR_B_POSITION,
         CAR_C_POSITION};
     private static final int[] COWINNER_EXPECTED_POSITIONS = {COWINNER_CAR_A_POSITION,
@@ -53,15 +49,14 @@ class RacingGameTest {
     @DisplayName("게임을 시작한다.")
     void start() {
         // given
-        TestCarNameReader carNameReader = new TestCarNameReader();
-        TestTryCountReader tryCountReader = new TestTryCountReader();
-        Host host = new TestHost();
-        GameFactory factory = new ConsoleGameFactory();
-        InMemoryCarManager carManager = new InMemoryCarManager();
-        CarPositionManager carPositionManager = new InMemoryCarPositionManager();
-
-        Game game = new RacingGame(carNameReader, tryCountReader, factory.getViewer(), host,
-            carManager, carPositionManager);
+        CarNameReader carNameReader = getTestCarNameReader();
+        TryCountReader tryCountReader = getTestTryCountReader();
+        Viewer viewer = getConsoleViewer();
+        Host host = getTestHost();
+        CarManager carManager = getTestCarManager();
+        CarPositionManager carPositionManager = getTestCarPositionManager();
+        Game game = new RacingGame(carNameReader, tryCountReader, viewer, host, carManager,
+            carPositionManager);
 
         // when
         game.start();
@@ -79,15 +74,14 @@ class RacingGameTest {
     @DisplayName("공동 우승자가 있는 게임을 시작한다.")
     void coWinnerStart() {
         // given
-        TestCarNameReader carNameReader = new TestCarNameReader();
-        TestTryCountReader tryCountReader = new TestTryCountReader();
-        Host host = new CoWinnerTestHost();
-        GameFactory factory = new ConsoleGameFactory();
-        InMemoryCarManager carManager = new InMemoryCarManager();
-        CarPositionManager carPositionManager = new InMemoryCarPositionManager();
-
-        Game game = new RacingGame(carNameReader, tryCountReader, factory.getViewer(), host,
-            carManager, carPositionManager);
+        CarNameReader carNameReader = getTestCarNameReader();
+        TryCountReader tryCountReader = getTestTryCountReader();
+        Viewer viewer = getConsoleViewer();
+        Host host = getCoWinnerTestHost();
+        CarManager carManager = getTestCarManager();
+        CarPositionManager carPositionManager = getTestCarPositionManager();
+        Game game = new RacingGame(carNameReader, tryCountReader, viewer, host, carManager,
+            carPositionManager);
 
         // when
         game.start();
@@ -101,24 +95,44 @@ class RacingGameTest {
         }
     }
 
-    private static class TestCarNameReader implements CarNameReader {
+    static class GameTestFactory {
 
-        @Override
-        public List<String> read() {
-            return List.of(CAR_A_NAME, CAR_B_NAME, CAR_C_NAME);
+        static CarNameReader getTestCarNameReader() {
+            return () -> List.of(CAR_A_NAME, CAR_B_NAME, CAR_C_NAME);
         }
-    }
 
-    private static class TestTryCountReader implements TryCountReader {
+        static TryCountReader getTestTryCountReader() {
+            return () -> 3L;
+        }
 
-        @Override
-        public Long read() {
-            return 3L;
+        static Viewer getConsoleViewer() {
+            return new ConsoleViewer();
+        }
+
+        static Host getTestHost() {
+            return new TestHost();
+        }
+
+        static Host getCoWinnerTestHost() {
+            return new CoWinnerTestHost();
+        }
+
+        static CarManager getTestCarManager() {
+            return new InMemoryCarManager();
+        }
+
+        static CarPositionManager getTestCarPositionManager() {
+            return new InMemoryCarPositionManager();
         }
     }
 
     private static class TestHost extends Host {
 
+        /**
+         * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(2), C(1)
+         */
+        private static final Long[] MOVE_DATA = {MOVE, STOP, STOP, MOVE, MOVE, STOP, MOVE, MOVE,
+            MOVE};
         private static int idx = 0;
 
         @Override
@@ -134,6 +148,11 @@ class RacingGameTest {
 
     private static class CoWinnerTestHost extends Host {
 
+        /**
+         * 행 : 라운드 차수(총 : 3회), 열 : 자동차(A, B, C), 값 : 자동차의 이동 신호, 결과 : A(3), B(3), C(1)
+         */
+        private static final Long[] MOVE_DATA = {MOVE, MOVE, STOP, MOVE, MOVE, STOP, MOVE,
+            MOVE, MOVE};
         private static int idx = 0;
 
         @Override
@@ -143,7 +162,7 @@ class RacingGameTest {
 
         @Override
         protected MoveSignal getSignal() {
-            return MoveSignal.of(COWINNER_MOVE_DATA[idx++]);
+            return MoveSignal.of(MOVE_DATA[idx++]);
         }
     }
 }
