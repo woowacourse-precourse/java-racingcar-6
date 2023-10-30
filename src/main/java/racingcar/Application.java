08e9003c10bc8ddc95164ec056ef7c36401b98bc
yooyoungpark <yoongtopiaa@gmail.com>
2023-10-30 12:08:16 +0900

    feat : 게임 끝난 후 우승자 이름 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         ArrayList<String> inputCarName = new ArrayList<>();
         String input = Console.readLine();
@@ -19,10 +20,9 @@ public class Application {
             }
         }
 
-        System.out.println(inputCarName);
         System.out.println("시도할 회수는 몇회인가요?");
         int tryNumber = inputTryNumber();
-        System.out.println(tryNumber);
+        System.out.println("\n실행 결과");
 
         int[] scores = new int[inputCarName.size()];
         for (int j = 0; j < tryNumber; j++) {
@@ -32,33 +32,43 @@ public class Application {
                     scores[i]++;
                 }
             }
+
             for (int i = 0; i < inputCarName.size(); i++) {
-                System.out.println(inputCarName.get(i)+ " : " + scores[i]);
+                System.out.print(inputCarName.get(i) + " : ");
+                for (int k = 0; k < scores[i]; k++) {
+                    System.out.print('-');
+                }
+                System.out.println();
             }
             System.out.println();
         }
 
         int max = scores[0];
         for (int i = 0; i < scores.length; i++) {
-            if(scores[i] > max) {
+            if (scores[i] > max) {
                 max = scores[i];
             }
         }
-        System.out.println(max);
+        System.out.print("최종 우승자 : ");
+        List<String> winner = new ArrayList<>();
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == max) {
+                winner.add(inputCarName.get(i));
 
-        // TODO : 1max와 같은 점수를 가진 사람이름을 출력해보시오. 2스코어점수를 막대로 표현하기
-
+            }
+        }
+        String winnerNames = String.join(", ", winner);
+        System.out.println(winnerNames);
     }
 
-
-    private static int inputTryNumber(){
-        try{
+    private static int inputTryNumber() {
+        try {
             int tryNumberInt = Integer.parseInt(Console.readLine());
             if (tryNumberInt <= 0) {
                 throw new IllegalArgumentException("양수를 입력하시오");
             }
             return tryNumberInt;
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력하시오");
         }
     }
