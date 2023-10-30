db5175e6cb4bf8420b73f61d1573c2b466cfd7d1
HSBODo <vinsulill@gmaile.com>
2023-10-30 18:41:01 +0900

    feat : 사용자 입력 자동차이름 5자이하 검증 메서드 구현
    구현메서드
    - inputCarNameValidation()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,19 +18,29 @@ public class Application {
       return input;
     }
 
-    public String inputCarNameValidation(String inputCarName){
-
-      return null;
-    }
-
     public static String[] inputCarNameSplit(String inputCarName){
       String[] cars = inputCarName.split(",");
+
       if(cars.length<2){
         throw new IllegalArgumentException("이름은 쉼표(,) 기준으로 구분합니다.");
       }
+
       return cars;
     }
 
+    public static Map<String,Integer> inputCarNameValidation(String[] inputCarNameSplit){
+      Map<String,Integer> cars = new HashMap<>();
+
+      for (String car : inputCarNameSplit){
+        if(car.length()>5){
+          throw new IllegalArgumentException("자동차이름은 5자 이하만 가능합니다.");
+        }
+
+        cars.put(car,0);
+      }
+
+      return cars;
+    }
     public Map<String,Integer> cars(String inputCarName){
       return null;
     }
