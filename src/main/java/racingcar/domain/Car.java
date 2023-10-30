dd96add99b801279204289e51ef6ff2a3b6ec515
jiixon <jr0107380@gmail.com>
2023-10-31 01:23:05 +0900

    feat: 자동차 점수 +1 증가 함수

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,19 @@
 package racingcar.domain;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private String name;
     private int score;
 
-    public void Car(String name, int score){
+    public void Car(String name){
         this.name = name;
-        this.score = score;
+        this.score = 0;
+    }
+    public void moveCar(){
+        int moveNum = Randoms.pickNumberInRange(0, 9);
+        if(moveNum >= 4){
+            score+=1;
+        }
     }
     public String getName(String name){
         return name;
