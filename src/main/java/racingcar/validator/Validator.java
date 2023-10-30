014049e8f3196899b509fa6b0dd480e24231bb03
rjsdn9025 <rjsdn9025@naver.com>
2023-10-30 22:36:36 +0900

    feat : inputData 예외처리 기능 추가

---
+++ src/main/java/racingcar/validator/Validator.java
@@ -0,0 +1,44 @@
+package racingcar.validator;
+
+import java.util.List;
+import racingcar.utils.Parser;
+
+public class Validator {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final int CAR_NAME_MIN_LENGTH = 1;
+    private final Parser parser = new Parser();
+
+    public void checkCarNameInput(String input) {
+        checkEmptyAndThrowException(input);
+        List<String> carNameList = parser.parseInputCarNames(input);
+        for (String carName : carNameList) {
+            checkLengthAndThrowException(carName);
+        }
+    }
+
+    public void checkAttemptNumInput(String input) {
+        checkEmptyAndThrowException(input);
+        checkCharAndThrowException(input);
+    }
+
+    private void checkEmptyAndThrowException(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkCharAndThrowException(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkLengthAndThrowException(String carName) {
+        if (CAR_NAME_MAX_LENGTH < carName.length()
+                || CAR_NAME_MIN_LENGTH > carName.length()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
