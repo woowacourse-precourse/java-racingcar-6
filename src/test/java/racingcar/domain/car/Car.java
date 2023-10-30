1ed3d09f0184ac16203a8fe00fbd4486bca91b26
jae woo Jung <awfjol2008@naver.com>
2023-10-30 16:18:17 +0900

    feat: 0부터 9 사이의 무작위 값을 하나 구하는 기능 구현

--- src/test/java/racingcar/domain/car/Car.java
+++ src/test/java/racingcar/domain/car/Car.java
@@ -1,7 +1,13 @@
 package racingcar.domain.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private Name name;
 
     private Distance distance;
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
