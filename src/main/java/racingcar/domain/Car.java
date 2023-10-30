864a6cd15b1a4b48b3bfca1bd1dc78ee02062dc5
subat27 <ksw1010100@gmail.com>
2023-10-30 20:57:20 +0900

    feat(racingcar/domain) : Car 클래스 추가
    입력받은 자동차 이름에 현재 상태를 포함한 정보를 저장하는 객체를 생성하기 위한 클래스

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String car_name;
+    private int move_count;
+
+    public Car(String car_name){
+        this.car_name = car_name;
+        this.move_count = 0;
+    }
+
+    public int getMove_count() {
+        return move_count;
+    }
+
+    public String getCar_name() {
+        return car_name;
+    }
+
+    public void determine_Move(){
+        if(Randoms.pickNumberInRange(0,9) > 4){
+            this.move_count++;
+        }
+    }
+
+}
