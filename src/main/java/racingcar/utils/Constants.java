824222511e610223e8a23ed9712adc273b538ea3
JiEung2 <https://github.com/JiEung2>
2023-10-30 23:02:06 +0900

    Add: 메시지 추가

--- src/main/java/racingcar/utils/Constants.java
+++ src/main/java/racingcar/utils/Constants.java
@@ -1,7 +1,10 @@
 package racingcar.utils;
 
 public class Constants {
-    private Constants() {}
     public static final int MIN_STRENGTH = 0;
     public static final int MAX_STRENGTH = 9;
+    public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INSERT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_MESSAGE = "실행 결과";
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
 }
