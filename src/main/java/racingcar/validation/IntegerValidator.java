4462d3fdbb122aa2e7d73b4190bad6ad62030d44
inpink <inpink@kakao.com>
2023-10-30 19:14:41 +0900

    feat: Integer가 아닌 경우 예외 발생하는 IntegerValidator
    프로젝트 전역에서 사용할 수 있는 검증 validation package에 관리.
    IntegerUtil과 협력함.

---
+++ src/main/java/racingcar/validation/IntegerValidator.java
@@ -0,0 +1,21 @@
+package racingcar.validation;
+
+import racingcar.util.ExceptionUtil;
+import racingcar.util.IntegerUtil;
+
+public class IntegerValidator {
+    private static final String INVALID_INTEGER_MESSAGE = "정수 값이 필요합니다.";
+
+    public static void validateInteger(Object object) {
+        if (!(object instanceof Integer)) {
+            ExceptionUtil.throwInvalidValueException(INVALID_INTEGER_MESSAGE);
+        }
+    }
+
+    public static void validateInteger(String string) {
+        if (!IntegerUtil.isInteger(string)) {
+            ExceptionUtil.throwInvalidValueException(INVALID_INTEGER_MESSAGE);
+        }
+    }
+
+}
