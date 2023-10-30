ec7ce4638cda3c74afe1a210b5fba1177bee3a77
Hyeon-hak Kim <07ily@naver.com>
2023-10-31 07:59:20 +0900

    feat: create *interface* `Rule` for class `Game`

---
+++ src/main/java/racingcar/model/Rule.java
@@ -0,0 +1,8 @@
+package racingcar.model;
+
+public interface Rule {
+    public String getCommaSeperatedCarNames();
+    public Integer getNumberOfRounds();
+    public void printCurrentPerformance();
+    public void printBestPerformers();
+}
