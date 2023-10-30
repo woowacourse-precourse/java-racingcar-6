f46aa991694a0b5bef967098da49aeb6320a60f9
jang010505 <jang010505@inu.ac.kr>
2023-10-31 03:16:29 +0900

    feat: validateTotalTurnInput 에러 메시지 추가

--- src/main/java/racingcar/constant/MessageConstant.java
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -5,7 +5,9 @@ public enum MessageConstant {
     CAR_NAME_INPUT_DUPLICATE_ERROR("서로 다른 자동차 이름을 입력해주세요."),
 
     CAR_NAME_LENGTH_ERROR("자동차의 이름은 1자 이상 5자 이하야야합니다."),
-    CAR_NAME_CONTAINS_SPACE_ERROR("자동차의 이름은 공백이 포함되지 말아야합니다.");
+    CAR_NAME_CONTAINS_SPACE_ERROR("자동차의 이름은 공백이 포함되지 말아야합니다."),
+
+    TOTAL_ROUND_INPUT_ERROR("1 이상의 양의 정수를 입력해야합니다.");
 
     private final String message;
 
