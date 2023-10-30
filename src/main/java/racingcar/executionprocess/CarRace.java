13e8490b281a307e4a14a14dd6531ea07b232fdf
woong2e <tlswodnd12345@naver.com>
2023-10-30 20:38:33 +0900

    feat : 기본 출력사항, 시도별 결과 출력, 우승자 출력하는 기능 구현

--- src/main/java/racingcar/executionprocess/CarRace.java
+++ src/main/java/racingcar/executionprocess/CarRace.java
@@ -1,33 +1,45 @@
 package racingcar.executionprocess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import racingcar.executionprocess.CarRandomNum;
 import racingcar.input.CarName;
 import racingcar.input.NumberOfAttempts;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class CarRace {
-
+    private static List<String> cars;
+    private static int number;
+//    private static int[] move;
     public static void start() {
         printCarName();
         printNumberOfAttempts();
-
+        printResult();
     }
 
-    public static void printCarName() {
+    private static void printCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> cars = CarName.input();
-        System.out.println(cars);
+        cars = CarName.input();
     }
 
-    public static void printNumberOfAttempts() {
+    private static void printNumberOfAttempts() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int num = NumberOfAttempts.input();
-        System.out.println(num);
+        number = NumberOfAttempts.input();
         System.out.println();
         System.out.println("실행 결과");
+    }
+
+    private static void printResult() {
+        int[] move = new int[cars.size()];
+        Result result = new Result(move);
+        for (int i = 0; i < number; i++) {
+            result.output(cars);
+            System.out.println();
+        }
+
+        System.out.print("최종 우승자 : " + result.findWinner(cars));
+
     }
 }
