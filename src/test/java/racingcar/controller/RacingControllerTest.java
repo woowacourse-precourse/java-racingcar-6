8dd71d11549ed70164343abbe3577616166ac644
junest66 <chlwndks333@gmail.com>
2023-10-31 01:04:03 +0900

    test(RacingControllerTest): 게임플레이 기능 테스트 추가
    - 모든 시도횟수를 진행후 결과출력 테스트 추가

---
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.TestNumberGenerator;
+
+class RacingControllerTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 모든시도횟수_진행후_결과출력_테스트() {
+        Car car1 = new Car("pobi", new TestNumberGenerator(4));
+        Car car2 = new Car("woni", new TestNumberGenerator(1));
+        Car car3 = new Car("jun", new TestNumberGenerator(3));
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+        int attemptCount = 5;
+        RacingController racingController = new RacingController();
+        racingController.playGame(cars,attemptCount);
+
+        String expectedOutput = "실행 결과\n" + "pobi : -\n" + "woni : \n" + "jun : \n" + "\n" +
+                "pobi : --\n" + "woni : \n" + "jun : \n" + "\n" + "pobi : ---\n" + "woni : \n" + "jun : \n" + "\n" +
+                "pobi : ----\n" + "woni : \n" + "jun : \n" + "\n" + "pobi : -----\n" + "woni : \n" + "jun : \n" +
+                "\n" + "최종 우승자 : pobi\n";
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+    }
+}
