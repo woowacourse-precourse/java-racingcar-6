1a350a2591c07fb44264250b6955a29765519e86
zzawang <hobin0116@naver.com>
2023-10-30 21:25:49 +0900

    refactor: RacingGame의 출력 기능 외부 주입
    - 출력 기능을 다루는 클래스를 만들어 외부에서 주입하도록 개선하였다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class Application {
     private static UserInputException userInputException;
     private static InputView inputView;
+    private static OutputView outputView;
     private static List<String> carNames;
     private static int attemptCount;
 
     public static void main(String[] args) {
         userInputException = new UserInputException();
         inputView = new InputView(userInputException);
+        outputView = new OutputView();
         carNames = inputView.getCarName();
         attemptCount = inputView.getAttemptCount();
         List<Car> cars = new ArrayList<>();
@@ -19,7 +21,7 @@ public class Application {
             Car car = new Car(carName);
             cars.add(car);
         }
-        RacingGame racingGame = new RacingGame(cars, attemptCount);
+        RacingGame racingGame = new RacingGame(cars, attemptCount, outputView);
         racingGame.gameStart();
     }
 }
