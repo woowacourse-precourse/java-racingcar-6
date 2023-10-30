510352fb644f782eb776d221412026a58d2cb1fe
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-30 06:12:07 +0000

    [Input] ADD : 입력 기능 및 입력 예외처리 추가

---
+++ src/main/java/racingcar/InputManager.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputManager {
+    public static String[] getCarNames() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("경주할 자동차 이름을 입력하세요 (쉼표로 구분): ");
+        String input = scanner.nextLine();
+        String[] carNames = input.split(",");
+        return carNames;
+    }
+
+    public static int getNumberOfAttempts() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("시도할 회수를 입력하세요: ");
+        try {
+            int numberOfAttempts = Integer.parseInt(scanner.nextLine());
+            if (numberOfAttempts <= 0) {
+                throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+            }
+            return numberOfAttempts;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 숫자를 입력하세요.");
+        }
+    }
+}
