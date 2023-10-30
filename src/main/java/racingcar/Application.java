e941bed25deac22ef58a8252ba60351ad6fb6ec6
Seungjida <esg0107@pusan.ac.kr>
2023-10-30 18:52:36 +0900

    style($location): 출력 형식 보충

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,17 +7,15 @@ public class Application {
     private static int attemptNumber = 0;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         CarRaceManager newRacingCars = new CarRaceManager();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        // 자동차 이름 받아서 쉼표 기준으로 나누고 리스트에 저장(static으로 할 지 말 지 고민되네에베네)
         newRacingCars.createCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
         attemptNumber = getAttemptNumber();
 
-        for(int i = 0; i < attemptNumber; i++) {
+        System.out.println("\n" + "실행 결과");
+        for (int i = 0; i < attemptNumber; i++) {
             newRacingCars.nthAttemptRace();
             newRacingCars.printAttemptResult();
         }
