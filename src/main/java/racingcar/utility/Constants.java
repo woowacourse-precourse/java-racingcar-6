573a65d7d07868b4651cab4df1e885b68b7c0255
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:18:00 +0900

    [feat] Add Constans class fields

--- src/main/java/racingcar/utility/Constants.java
+++ src/main/java/racingcar/utility/Constants.java
@@ -2,5 +2,15 @@ package racingcar.utility;
 
 public class Constants {
     private Constants() {}
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String RESULT = "실행결과";
+    public static final String COLON = " : ";
+    public static final String LINE = "-";
+    public static final String COMMA = ",";
+    public static final String WINNERS = "최종 우승자";
+
+    // 정규식 (문자열이 숫자로만 이루어져 있는지 검사)
+    public static final String ONLYINT = "\\d+";
 
 }
