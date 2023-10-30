75bc046a7990d1591b6c42f85e2b56cae3b921fa
깃wooqqq <sge0225@naver.com>
2023-10-30 15:45:42 +0900

    feat: 사용자가 입력한 이름을 자동차 객체로 저장하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,10 @@ public class Car {
     private String name;
     private int position = 0;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     public void moveForward(int distance) {
         position += distance;
     }
--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -1,8 +1,10 @@
 package racingcar.Controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
 import racingcar.Service.GameService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
@@ -12,10 +14,19 @@ public class GameController {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
         List<String> carList = gameService.parseCarName(carName);
+        saveCars(carList);
 
         System.out.println("시도할 회수는 몇회인가요?");
         String countTrial = Console.readLine();
 
         System.out.println(carList);
+    }
+
+    private void saveCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
     }
 }
