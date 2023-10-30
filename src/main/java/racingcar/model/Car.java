ba2ec9d7385dc2d4a645a9c0ced5f76aae262cbc
hoyeon <highoption@naver.com>
2023-10-31 00:22:31 +0900

    feat(Car) : 정적 메서드 팩토리를 이용한 Car 객체 선언

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,4 +3,13 @@ package racingcar.model;
 public class Car {
     private String name;
     private int distance;
+
+    private Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public static Car makeCarByName(String carName) {
+        return new Car(carName);
+    }
 }
