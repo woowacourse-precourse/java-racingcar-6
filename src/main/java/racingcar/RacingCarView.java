4ceeef17507ee29d01664fefd5476eac3c4a0911
JISUmaker <knightMaker666@gmail.com>
2023-10-30 12:13:48 +0900

    feat: RacingCarView 차 이름 입력 구현

--- src/main/java/racingcar/RacingCarView.java
+++ src/main/java/racingcar/RacingCarView.java
@@ -3,6 +3,11 @@ package racingcar;
 import java.util.ArrayList;
 
 public class RacingCarView {
+	public String inputCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		return Console.readLine();
+	}
+	
 	public void printRacing(ArrayList<Car> cars) {
 		for(Car c : cars) {
 			System.out.println(c.getName() + " : " );
