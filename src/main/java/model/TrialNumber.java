e7679682faffb1e5bc1481efabfb73512356ebf8
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 16:55:04 +0900

    feat : 시도 횟수 1씩 감소하기와 0인지 판별하기

--- src/main/java/model/TrialNumber.java
+++ src/main/java/model/TrialNumber.java
@@ -2,15 +2,25 @@ package model;
 
 public class TrialNumber {
     private int trial;
+    private int INITIAL_TRIAL = 0;
 
     public TrialNumber(String input){
         validateTrialNumber(input);
         this.trial = Integer.parseInt(input);
     }
 
-    public void proceedToNextRound(){
-        trial++;
+    public int getTrialNumber(){
+        return trial;
     }
+
+    public void decreaseTrialNumber(){
+        trial--;
+    }
+
+    public boolean isTrialNumberZero(){
+        return trial == 0;
+    }
+
 
     private void validateTrialNumber(String input){
         validateTrialNumberIsInt(input);
