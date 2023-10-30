a99a6c951509b39ad1bb3dc1a2a3122a209d9785
고동환\eurdr <eurdream98@naver.com>
2023-10-30 23:09:12 +0900

    feat($Winner) : Add findMaxNumberAndToSaveIndexOfMaxNumber method
    -각 car에 대한 전진 횟수를 갖고 있는 goCount의 수를 똑같이 가진
    copyGoCount를 생성
    -goCount(원본)는 오름차순으로 정렬
    -정렬하여 가장 큰 수 찾음 maxNumber
    -goCount(원본)에서 maxNumber를 갖고 있는 인덱스(위치)를 indexOfMaxNumber리스트에 저장

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -9,6 +9,22 @@ public class Winner {
 
         findMaxNumberAndToSaveIndexOfMaxNumber(goCount,carList);
     }
+    public static void findMaxNumberAndToSaveIndexOfMaxNumber(int[] goCount,List<String> carList){
+        int[] copyGoCount = new int[goCount.length];
+        for(int i = 0 ; i < copyGoCount.length ; i++){
+            copyGoCount[i]=goCount[i];
+        }
+        Arrays.sort(goCount);
+        int maxNumber = goCount[goCount.length-1];
+
+        List<Integer> indexOfMaxNumber = new ArrayList<>();
+        for(int i = 0 ; i< copyGoCount.length;i++){
+            if(copyGoCount[i]==maxNumber){
+                indexOfMaxNumber.add(i);
+            }
+        }
+        findWinnerCarByIndexOfMaxNumber(indexOfMaxNumber,carList);
+    }
 
 
 }
