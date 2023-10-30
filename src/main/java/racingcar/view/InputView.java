228d120a8f80663aff27e33a0307360a6087afe0
Minsu Choi <cms102510@naver.com>
2023-10-30 11:10:58 +0900

    feat: 입력 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String carInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String tryCountInput() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryCount = Console.readLine();
+        System.out.println();
+        return tryCount;
+    }
+}
