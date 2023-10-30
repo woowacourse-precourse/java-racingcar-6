6e16f0ac27f0e68690ad1a1b453f27ebbf9d9a57
hee9841 <lee98417716@gmail.com>
2023-10-31 06:01:44 +0900

    feat(racingcar.domain): 출력 기능 구현

--- src/main/java/racingcar/domain/PrintFormat.java
+++ src/main/java/racingcar/domain/PrintFormat.java
@@ -1,6 +1,39 @@
 package racingcar.domain;
 
 
+
+import java.util.List;
+
 public class PrintFormat {
+
+    private final static String PRINT_FORMAT_CAR_LOCATION_BY_NAME = "%s : ";
+    private final static String PRINT_FORMAT_WINNERS = "최종 우승자 : %s";
+
+    private final static String CAR_LOCATION_INDICATOR = "-";
+    private final static String MEMBER_SEPARATOR = ",";
+
+
+    private final static String SHOW_RESULT_MESSAGE = "실행 결과";
+
+
+
+    public PrintFormat() {
+    }
+
+
+
+    public void printExecuteResultMessage() {
+        System.out.println();
+        System.out.println(SHOW_RESULT_MESSAGE);
+    }
+
+    public void printCarLocationByCarName(String carName, int carLocation) {
+        System.out.println(String.format(PRINT_FORMAT_CAR_LOCATION_BY_NAME, carName)
+                + CAR_LOCATION_INDICATOR.repeat(carLocation));
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.printf(String.format(PRINT_FORMAT_WINNERS, String.join(MEMBER_SEPARATOR + "\s", winners)));
+    }
 
 }
