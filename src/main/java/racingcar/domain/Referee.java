4e639a4dc431b0a983205a3e1244eb0d0497bc03
BellWin98 <jongseunghan@naver.com>
2023-10-30 18:25:56 +0900

    Refactor(racingcar): 점수 기록하는 함수 Car 클래스로 이전

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -3,11 +3,8 @@ package racingcar.domain;
 import java.util.List;
 
 public class Referee {
-    public Referee(){}
 
-    public int countMoves(String carTrace){
-        return 0;
-    }
+    public Referee(){}
 
     public List<String> announceWinner(List<Integer> carMoves){
         return null;
