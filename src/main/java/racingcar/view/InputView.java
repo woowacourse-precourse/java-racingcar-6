5781e009931d8ec8265b4d5d2ee84d5d717bf48b
devhyeon0 <devhyeon0@gmail.com>
2023-10-31 01:30:44 +0900

    feat: 입력 메시지 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String RACING_CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUMBER_ATTEMPTS_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static String askRacingCarName() {
+        System.out.println(RACING_CAR_NAME_INPUT_MESSAGE);
+
+        return Console.readLine();
+    }
+
+    public static int askPlayNumber() {
+        System.out.println(NUMBER_ATTEMPTS_INPUT_MESSAGE);
+
+        return Integer.parseInt(Console.readLine());
+    }
+}
