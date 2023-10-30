0e7da94ec1b82cd5240e17255824977a38c4c17c
minw0095 <kmu6898@naver.com>
2023-10-30 20:25:05 +0900

    feat: 전진 or 정지를 정하는 랜덤넘버 생성

---
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    public static Integer  RandomNumber () {
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+}
