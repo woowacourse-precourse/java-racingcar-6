7c731befa941a836bf8c4fa9c00490881297f4f7
junghunim07 <dlawndgns9@naver.com>
2023-10-30 12:17:07 +0900

    feat(ResultPrinter): printCourse 메소드 구현

--- src/main/java/racingcar/domain/ResultPrinter.java
+++ src/main/java/racingcar/domain/ResultPrinter.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.HashMap;
+
 public class ResultPrinter {
     private static String getUserName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static String getTry = "시도할 회수는 몇회인가요?";
@@ -11,5 +13,19 @@ public class ResultPrinter {
 
     public static void getTryMessage() {
         System.out.println(getTry);
+    }
+
+    public static void printCourse(HashMap<String,Integer> race) {
+        for (String key : race.keySet()) {
+            System.out.print(key + " : ");
+
+            for (int i = 0; i < race.get(key); i++) {
+                System.out.print("-");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
