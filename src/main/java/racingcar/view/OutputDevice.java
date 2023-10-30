0a54ed3a3514b98b690c10497917e46c072d5964
BonSik <rnqhstlr2297@gmail.com>
2023-10-31 01:30:17 +0900

    feat: 우승자 콘솔 출력 메서드 추가

--- src/main/java/racingcar/view/OutputDevice.java
+++ src/main/java/racingcar/view/OutputDevice.java
@@ -4,6 +4,7 @@ public final class OutputDevice {
     private final static String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private final static String RESULT_MESSAGE = "실행 결과";
+    private final static String WINNER_MESSAGE = "최종 우승자 : ";
 
     private OutputDevice() {
     }
@@ -19,6 +20,10 @@ public final class OutputDevice {
     public static void printResultMessage(String resultMessage) {
         System.out.println(RESULT_MESSAGE);
         System.out.println(resultMessage);
+    }
+
+    public static void printWinnerMessage(String winnerMessage){
+        System.out.println(WINNER_MESSAGE + winnerMessage);
     }
 
 }
