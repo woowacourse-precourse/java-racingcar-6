63d24be71b6b2a85cb6fd239ce657e7df18334c8
inpink <inpink@kakao.com>
2023-10-30 20:59:34 +0900

    refactor: MemoryCarRepository와 MemoryRacingRepository 상속 구조
    MemoryCarRepository와 구현체는 MemoryRepository와 CarRepository 둘 다를 상속받아야 함.
    CarRepository를 통해 CarRepository가 가져야 하는 모든 메소드를 상속받거나 구현함.
    MemoryRepository<Car>을 통해 내부 저장소(맵)을 쓰는 저장소의 기능을 상속받음.
    이를 통해 Config의 내용은 전혀 수정하지 않아도 되며, 추후 CarRepository를 바꿔끼우고 싶다면 MemoryRepository와 CarRepository를 상속받은 구현체로 바꿔끼워주면 됨.
    기존 테스트도 모두 통과함.

--- src/main/java/racingcar/repository/MemoryCarRepository.java
+++ src/main/java/racingcar/repository/MemoryCarRepository.java
@@ -1,52 +1,14 @@
 package racingcar.repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import racingcar.domain.Car;
 
-public class MemoryCarRepository implements CarRepository {
-
-    private static Map<Long, Car> store = new HashMap<>();
-    private static Long sequence;
-
-    public MemoryCarRepository() {
-        resetSequence();
-    }
-
-    private void resetSequence() {
-        sequence = 0L;
-    }
-
-    @Override
-    public Car save(Car car) {
-        store.put(++sequence, car);
-        car.setId(sequence);
-        return car;
-    }
-
-    @Override
-    public Optional<Car> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
+public class MemoryCarRepository extends MemoryRepository<Car> implements CarRepository {
 
     @Override
     public Optional<Car> findByName(String name) {
         return store.values().stream()
                 .filter(car -> car.getName().equals(name))
                 .findFirst();
-    }
-
-    @Override
-    public List<Car> findAll() {
-        return new ArrayList<>(store.values());
-    }
-
-    @Override
-    public void deleteAll() {
-        store.clear();
-        resetSequence();
     }
 }
--- src/main/java/racingcar/repository/MemoryRacingRepository.java
+++ src/main/java/racingcar/repository/MemoryRacingRepository.java
@@ -1,30 +1,9 @@
 package racingcar.repository;
 
-import java.util.List;
-import java.util.Optional;
 import racingcar.domain.Racing;
 import racingcar.domain.Winners;
 
-public class MemoryRacingRepository implements RacingRepository {
-    @Override
-    public Racing save(Racing object) {
-        return null;
-    }
-
-    @Override
-    public Optional<Racing> findById(Long id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public List<Racing> findAll() {
-        return null;
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
+public class MemoryRacingRepository extends MemoryRepository<Racing> implements RacingRepository {
 
     @Override
     public void updateWinners(Winners winners) {
