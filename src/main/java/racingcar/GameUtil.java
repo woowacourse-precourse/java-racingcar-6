a9269fd4d7703a074c5d51af398c725c2fe2234d
JGM <cnxw4570123@gmail.com>
2023-10-30 20:15:47 +0900

    feat : add GameUtil class to assist Game class GameUtil 클래스 - 우승자 출력하는 printWinner() 메소드 추가 - 입력받은 문자열을 이름 배열로 바꿔주는 inputToNameArr() 메소드 추가 - 각 선수 성적을 출력하는 printScore() 메소드 추가 - 각 문자가 이름으로 적합한지 판별하는 isNameOK() 메소드 추가

---
+++ src/main/java/racingcar/GameUtil.java
@@ -0,0 +1,47 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GameUtil {
+
+    public void printWinner(List<Racer> winners) {
+        System.out.print("최종 우승자 : ");
+        List<String> winnerNames = winners.stream()
+                .map(Racer::getName)
+                .toList();
+        System.out.println(String.join(", ", winnerNames));
+    }
+
+    public void printEachResult(List<Racer> racerList) {
+        StringBuilder result = new StringBuilder();
+        for (Racer racer : racerList) {
+            result.append(String.format("%5s : ", racer.getName()))
+                    .append(racer.getScore())
+                    .append("\n");
+        }
+        System.out.println(result.toString());
+    }
+
+    public String[] inputToNameArr(String input) {
+        String[] nameArr = input.split(",");
+        for (String name : nameArr) {
+            if (isNameOK(name)) {
+                throw new IllegalArgumentException("이름은 한 글자 이상 다섯 글자 이하여야 합니다.");
+            }
+        }
+        return nameArr;
+    }
+
+    /**
+     * 이름이 1자리 이상 5자리 이하인지 확인하는 함수
+     *
+     * @param nameCandidate 전달받은 이름
+     * @return boolean true or false
+     */
+    private boolean isNameOK(String nameCandidate) {
+        return !nameCandidate.isEmpty() && nameCandidate.length() <= 5;
+    }
+
+
+}
