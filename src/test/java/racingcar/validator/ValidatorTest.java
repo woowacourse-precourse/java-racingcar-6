4a79eccfa3e3f72200b623da0fb421605261003a
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 14:05:42 +0900

    test(ValidatorTest) : 시도 횟수 입력에 대한 유효성 검증 테스트 코드 작성

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -11,9 +11,9 @@ class ValidatorTest {
     void 자동차의_이름이_5자를_넘은_경우_예외_처리() {
         String input = "abcdef";
 
-        assertThatThrownBy(() -> Validator.validateUnderFiveLength(input))
+        assertThatThrownBy(() -> Validator.validateAvailableLength(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차의 이름은 5자 이하로 입력해야 합니다.");
+                .hasMessage("5자 이하로 입력해야 합니다.");
     }
 
     @Test
@@ -22,16 +22,33 @@ class ValidatorTest {
 
         assertThatThrownBy(() -> Validator.validateIsBlank(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차의 이름으로 공백은 사용할 수 없습니다.");
+                .hasMessage("공백은 사용할 수 없는 입력입니다.");
     }
 
     @Test
     void 자동차의_이름이_중복되는_경우_예외_처리() {
-        List<String> input = List.of("pobi,pobi,jun");
+        List<String> input = List.of("pobi", "pobi", "jun");
 
         assertThatThrownBy(() -> Validator.validateIsDuplicate(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("서로 다른 이름을 입력해야 합니다.");
+                .hasMessage("중복되지 않게 입력해야 합니다.");
     }
 
+    @Test
+    void 시도할_횟수에_숫자가_아닌_입력시_예외_처리() {
+        String input = "abc";
+
+        assertThatThrownBy(() -> Validator.validateIsNumeric(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력할 수 있습니다.");
+    }
+
+    @Test
+    void 시도할_횟수에_공백_입력시_예외_처리() {
+        String input = "";
+
+        assertThatThrownBy(() -> Validator.validateIsBlank(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공백은 사용할 수 없는 입력입니다.");
+    }
 }
