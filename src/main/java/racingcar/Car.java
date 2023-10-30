b343cda78df20874a6572434a46919a133cb0f98
ParkJinYong <mej1406@gmail.com>
2023-10-31 04:21:48 +0900

    feat: 자동차 클래스 추가, 함수 틀 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,23 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+    private String name;
+    private int moveIndex;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveIndex = 0;
+    }
+
+    public void move() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public int getMoveIndex() {
+        return -1;
+    }
 }
