f05722d1598b338170e83fe69660b9424f883855
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 16:23:39 +0900

    test : 자동차 이름 중복 입력 검증

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,10 +1,11 @@
 package racingcar;
 
-import model.Car;
 import model.Cars;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarsTest {
 
@@ -16,9 +17,18 @@ public class CarsTest {
     }
 
     @Test
-    @DisplayName("자동차 리스트 이름 테스트")
-    void 자동차_리스트_이름_테스트() {
+    @DisplayName("자동차 리스트 이름 출력 테스트")
+    void 자동차_리스트_이름_출력_테스트() {
         Cars cars = new Cars("pobi,woni,hand");
         Assertions.assertThat(cars.toString()).isEqualTo("pobi,woni,hand");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,pobi,hand"})
+    @DisplayName("자동차 리스트 이름 중복 테스트")
+    void 자동차_리스트_이름_중복_테스트(String string) {
+        Assertions.assertThatThrownBy(() -> new Cars(string))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
