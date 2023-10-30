7a0dddcd0c7eea87dec1f13af6e5abd0ab652e77
dong_hy <kimadonghyon99@gmail.com>
2023-10-30 19:30:39 +0900

    refactor : 메인함수 필요없는코드제거 반복문 Car클래스의 메소드로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,5 @@
 package racingcar;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,13 +10,7 @@ public class Application {
         GameView.gameCount();
         cars.moveCount = Input.inputGameCount();
 
-        GameView.gameMonitoring((HashMap<String, Integer>) cars.carMap,cars.moveCount);
-        //차량생성까지 끝 게임 시작 ~
-
-        for(int i=0;i < cars.moveCount;i++){
-            cars.moveCar();
-            GameView.gameCurrent((HashMap<String, Integer>) cars.carMap);
-        }
+        cars.moveCarFor();
 
         GameView.gameResult(cars.referee());
     }
