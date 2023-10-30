cb758c7f1d71b2080b0cf952f2ad290f3236c8b8
minnim1010 <pms1010123@daum.net>
2023-10-31 05:30:59 +0900

    feat: 시도 횟수 입력 화면 구현

--- src/main/java/racingcar/game/RacingGameScreen.java
+++ src/main/java/racingcar/game/RacingGameScreen.java
@@ -6,6 +6,7 @@ import racingcar.common.io.writer.Writer;
 public class RacingGameScreen {
 
     private static final String INPUT_RACER_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUMBER_OF_TURNS = "시도할 횟수는 몇 회인가요?";
 
     private final Reader reader;
     private final Writer writer;
@@ -17,6 +18,11 @@ public class RacingGameScreen {
 
     public String inputRacer(){
         writer.writeLine(INPUT_RACER_MESSAGE);
+        return reader.readLine();
+    }
+
+    public String inputNumberOfTurns(){
+        writer.writeLine(INPUT_NUMBER_OF_TURNS);
         return reader.readLine();
     }
 }
--- src/test/java/racingcar/game/RacingGameScreenTest.java
+++ src/test/java/racingcar/game/RacingGameScreenTest.java
@@ -56,4 +56,20 @@ class RacingGameScreenTest {
         assertThat(output).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         assertThat(racerInput).isEqualTo(input);
     }
+
+    @DisplayName("시도 횟수 입력 화면을 올바르게 구성하였는지 확인한다.")
+    @Test
+    void checkInputNumberOfTurnsScreen(){
+        //given
+        String input = "10";
+        mockReader.setInput(input);
+
+        //when
+        String racerInput = racingGameScreen.inputNumberOfTurns();
+
+        //then
+        String output = mockWriter.getOutput();
+        assertThat(output).isEqualTo("시도할 횟수는 몇 회인가요?\n");
+        assertThat(racerInput).isEqualTo(input);
+    }
 }
