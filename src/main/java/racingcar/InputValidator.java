e80cb6bf88f84cf7c0f059f04143ce8d21dff659
daneng4 <daneng4@naver.com>
2023-10-30 21:25:56 +0900

    feat: 자동차 이름 길이 검사 기능 추가

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class InputValidator {
+
+    public void checkStringLength(String[] carNames){
+        for (String CarName : carNames) {
+            
+        }
+    }
+}
