0681dbe3f25507f3acebdaaf332c24990fa24778
pjaehyun <show6756@gmail.com>
2023-10-30 17:50:26 +0900

    refactor: createCarCollection함수로 분리

--- src/test/java/racingcar/CarCollectionTest.java
+++ src/test/java/racingcar/CarCollectionTest.java
@@ -6,23 +6,29 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarCollectionTest {
 
     @Test
     void addCar_자동차_추가() {
-        CarCollection carCollection = new CarCollection();
-
         Car car1 = new Car("car1");
         Car car2 = new Car("car2");
-
-        carCollection.addCar(car1);
-        carCollection.addCar(car2);
+        CarCollection carCollection = createCarCollection(car1, car2);
 
         List<Car> cars = carCollection.getCars();
 
         assertThat(cars).contains(car1);
         assertThat(cars).contains(car2);
+    }
+
+    private CarCollection createCarCollection(Car... cars) {
+        CarCollection carCollection = new CarCollection();
+
+        for (Car car : cars) {
+            carCollection.addCar(car);
+        }
+        return carCollection;
     }
 }
