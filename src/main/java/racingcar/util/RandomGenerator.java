8b17c19b03c419a9f75359c0e13a88a59923a44f
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 16:04:19 +0900

    refactor(RandomGenerator) : generate 메소드를 static으로 변경

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import racingcar.consts.GameConstant;
 
 public class RandomGenerator {
-    public int generate() {
+    public static int generate() {
         return pickNumberInRange(GameConstant.MIN_NUMBER_RANDOM_RANGE.get(),
                 GameConstant.MAX_NUMBER_RANDOM_RANGE.get());
     }
