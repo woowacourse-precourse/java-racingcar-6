12e677357b2b18cae01e1f7c10043257495287f2
songsunkook <songsunkook@gmail.com>
2023-10-30 10:05:55 +0900

    feat: OutputView 구현

--- src/main/java/constant/Message.java
+++ src/main/java/constant/Message.java
@@ -1,5 +1,10 @@
 package constant;
 
+import dto.CarDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Message {
     INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
     INPUT_ROUND_NUMBER("시도할 회수는 몇회인가요?\n"),
@@ -17,5 +22,29 @@ public enum Message {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getMessage(CarDto car) {
+        if (message.equals(OUTPUT_CAR_POSITION.getMessage())) {
+            return String.format(message, car.getName(), calculatePosition(car));
+        }
+        return message;
+    }
+
+    private String calculatePosition(CarDto car) {
+        return "-".repeat(car.getPosition());
+    }
+
+    public String getMessage(List<CarDto> cars) {
+        if (message.equals(OUTPUT_FINAL_WINNER.getMessage())) {
+            return String.format(message, calculateWinner(cars));
+        }
+        return message;
+    }
+
+    private String calculateWinner(List<CarDto> cars) {
+        return cars.stream()
+                .map(CarDto::getName)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,33 @@
 package view;
 
 import constant.Message;
+import dto.CarDto;
+
+import java.util.List;
 
 public class OutputView {
+    public void inputCarName() {
+        System.out.print(Message.INPUT_CAR_NAMES.getMessage());
+    }
 
+    public void inputRoundNumber() {
+        System.out.println(Message.INPUT_ROUND_NUMBER);
+    }
+
+    public void outputRunResult() {
+        System.out.println(Message.OUTPUT_RUN_RESULT);
+    }
+
+    public void outputCarPosition(List<CarDto> cars) {
+        for (CarDto car : cars) {
+            System.out.println(
+                    Message.OUTPUT_CAR_POSITION.getMessage(car)
+            );
+        }
+        System.out.println(Message.OUTPUT_NEWLINE);
+    }
+
+    public void outputFinalWinner(List<CarDto> cars) {
+        System.out.println(Message.OUTPUT_FINAL_WINNER.getMessage(cars));
+    }
 }
