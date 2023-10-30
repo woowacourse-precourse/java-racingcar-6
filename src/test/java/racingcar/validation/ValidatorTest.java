bb7dfa0a5fc6fe21da64fdbf89219f5f50bc9df0
DaeYoungK <hanguk830@naver.com>
2023-10-30 11:58:07 +0900

    test(Validator): 입력된 시도할 횟수 타입에 대한 검증기 테스트

--- src/test/java/racingcar/validation/ValidatorTest.java
+++ src/test/java/racingcar/validation/ValidatorTest.java
@@ -10,11 +10,19 @@ class ValidatorTest {
 
     Validator validator = new Validator();
 
-    @DisplayName("검증기 정상 동작 여부 테스트")
+    @DisplayName("검증기 정상 동작 여부 테스트 - 자동차 이름")
     @ValueSource(strings = {"Goooood", "baaaaaad", "555555"})
     @ParameterizedTest
     void validatorTest(String value) {
         assertThatThrownBy(() -> validator.validCarName(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("검증기 동작 여부 테스트 - 시도할 횟수")
+    @ValueSource(strings = {"k", "d", "y"})
+    @ParameterizedTest
+    void validatorOfCountTest(String value) {
+        assertThatThrownBy(() -> validator.validCount(value))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
