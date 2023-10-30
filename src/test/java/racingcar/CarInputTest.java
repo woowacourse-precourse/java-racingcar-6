ce1590fcbeded4bea3adefb5e76858ab5502b2eb
SeongIl <biuld1234@gmail.com>
2023-10-30 15:00:07 +0900

    test: add 2.CountInputTest - 시도_횟수_출력문() - error 방지로, Console.close() 추가

--- src/test/java/racingcar/CarInputTest.java
+++ src/test/java/racingcar/CarInputTest.java
@@ -150,6 +150,5 @@ class CarInputTest {
                 .contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         Console.close();
-
     }
 }
--- src/test/java/racingcar/CountInputTest.java
+++ src/test/java/racingcar/CountInputTest.java
@@ -1,45 +1,72 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.Computer.createRoundCount;
+import static racingcar.Computer.getInput;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class CountInputTest {
 
     @Test
-    void 음수_또는_0_입력_시_예외_처리(){
+    void 음수_또는_0_입력_시_예외_처리() {
 
         // given
         String userInputOne = "-1";
         String userInputTwo = "0";
 
         // then
-        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputOne))
+        assertThatThrownBy(() -> Validator.checkPositiveNumber(userInputOne))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1이상의 숫자를 입력하세요.");
 
-        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputTwo))
+        assertThatThrownBy(() -> Validator.checkPositiveNumber(userInputTwo))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1이상의 숫자를 입력하세요.");
     }
 
     @Test
-    void 문자_또는_공백_입력_시_예외_처리(){
+    void 문자_또는_공백_입력_시_예외_처리() {
 
         // given
         String userInputOne = "3pobi";
         String userInputTwo = " ";
 
         // then
-        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputOne))
+        assertThatThrownBy(() -> Validator.checkPositiveNumber(userInputOne))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자만 입력하세요.");
 
-        assertThatThrownBy(()-> Validator.checkPositiveNumber(userInputTwo))
+        assertThatThrownBy(() -> Validator.checkPositiveNumber(userInputTwo))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자만 입력하세요.");
     }
 
+    @Test
+    void 시도_횟수_출력문() {
 
+        // given
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
+        String input = "5";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // when
+        Car car = new Car(Arrays.asList("pobi", "woni", "jun"));
+        car.move(createRoundCount(getInput("시도할 회수는 몇회인가요?")));
+
+        // then
+        assertThat(out.toString())
+                .contains("시도할 회수는 몇회인가요?");
+        Console.close();
+
+    }
 }
