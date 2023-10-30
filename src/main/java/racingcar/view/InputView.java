8b780dadcba33682f0ea47f4527113adb2c1b263
yoonjuho <dbswn990@naver.com>
2023-10-30 13:49:00 +0900

    feat(InputView): 몇 번의 이동을 할것인지 입력받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("^[0-9]*$");
     private static final String SEPARATOR = ",";
     public List<String> inputCarNames(){
         String carNames = Console.readLine();
@@ -15,9 +17,23 @@ public class InputView {
                 .toList();
     }
 
-    private void validateNullAndBlank(String carNames){
-        if(carNames == null || carNames.isBlank()){
+    private void validateNullAndBlank(String input){
+        if(input == null || input.isBlank()){
             throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
         }
     }
+
+    public Integer inputGameCount(){
+        String gameCount = Console.readLine();
+        validateNullAndBlank(gameCount);
+        validateNumeric(gameCount);
+        return Integer.parseInt(gameCount);
+    }
+
+    private void validateNumeric(String input){
+        if (!NUMERIC_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+    }
+
 }
