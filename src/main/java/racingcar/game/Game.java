7ba8cca0618ac16317f9be1753fe656d1c8f75bd
kang jihyeong <her4386@gmail.com>
2023-10-31 00:01:35 +0900

    [feat] 시도할 횟수 입력

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -4,10 +4,13 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Game {
     private String carNames;
+    private String numberOfTry;
 
     public Game() {
         printPhrasesCarName();
         inputCarName();
+        printPhraseNumberOfTry();
+        inputNumberOfTry();
     }
 
     private void printPhrasesCarName() {
@@ -16,5 +19,13 @@ public class Game {
 
     private void inputCarName() {
         carNames = readLine();
+    }
+
+    private void printPhraseNumberOfTry() {
+        System.out.println(GamePhrases.PHRASES_INPUT_NUMBER_OF_TRY);
+    }
+
+    private void inputNumberOfTry() {
+        numberOfTry = readLine();
     }
 }
