dd90c90532ef653b2e32a7a9a1323651b0bc7db4
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-30 23:37:18 +0900

    feat: add car constructor

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,14 @@
 package racingcar.model;
 
 public class Car {
+
+    private String carName;
+    private int driveCount;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.driveCount = 0;
+    }
+
+
 }
