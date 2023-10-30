9d75d304953d063c883b693a3d628c287ac54a24
KellyKimHyeJin <kelly317kim@naver.com>
2023-10-30 21:39:59 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingGame {
+    List<String> carList = new ArrayList<>();
+    int tryNumber = 0;
+
+    public void getInput(){
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine();
+        carList = Arrays.asList(cars.split(","));
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        tryNumber = Integer.parseInt(Console.readLine());
+
+    }
+}
