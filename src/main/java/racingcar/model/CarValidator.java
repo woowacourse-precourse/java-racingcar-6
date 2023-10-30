d9f4be917e2f5d9a9f709ff5e9b1ac59e389033c
sinminseok12 <sinminseok12@naver.com>
2023-10-30 16:54:37 +0900

    refactor(CarValidator): 검증 메서드 분리

--- src/main/java/racingcar/model/CarValidator.java
+++ src/main/java/racingcar/model/CarValidator.java
@@ -8,11 +8,19 @@ public class CarValidator {
     private static final String NAME_RANGE_ERROR = "자동차 이름은 1글자 이상, 5글자 이하만을 허용합니다.";
 
     public static void validate(String name) {
-        if(name.isBlank()){
-            throw new IllegalArgumentException(NAME_BLANK_ERROR);
-        }
+        validateBlank(name);
+        validateNameRange(name);
+    }
+
+    private static void validateNameRange(String name) {
         if (name.length() < MIN_NAME_RANGE || name.length() > MAX_NAME_RANGE) {
             throw new IllegalArgumentException(NAME_RANGE_ERROR);
+        }
+    }
+
+    private static void validateBlank(String name) {
+        if(name.isBlank()){
+            throw new IllegalArgumentException(NAME_BLANK_ERROR);
         }
     }
 }
