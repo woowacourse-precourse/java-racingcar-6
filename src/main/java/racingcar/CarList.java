aa38b2aacd3471cc338367ad9f3fc24934c3a1a2
kchranger <kchranger@gmail.com>
2023-10-30 22:30:39 +0900

    feature(app) : check blank space in car name

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -6,23 +6,31 @@ import java.util.List;
 public class CarList {
     private List<Car> carList;
 
-    public CarList(String carsName)  throws IllegalArgumentException{
-        this.carList = new ArrayList<>();
+    public CarList(String carsName) throws IllegalArgumentException {
         String[] names = splitNamesByComma(carsName);
-        if (!isValidNameLength(names)) {
+        if (isBlankSpace(carsName) || !isValidNameLength(names)) {
             throw new IllegalArgumentException();
         }
+        this.carList = new ArrayList<>();
+
     }
 
-    private String[] splitNamesByComma(String name){
+    private String[] splitNamesByComma(String name) {
         return name.split(",");
     }
 
-    private boolean isValidNameLength(String[] names){
+    private boolean isValidNameLength(String[] names) {
         for (String name : names) {
             if (name.length() == 0 || name.length() >= 6) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    private boolean isBlankSpace(String names) {
+        if (names.contains(" ")) {
+            return false;
         }
         return true;
     }
