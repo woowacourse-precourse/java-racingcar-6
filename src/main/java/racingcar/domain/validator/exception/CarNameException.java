560bc1f34b5d4f5898c973747aa1c3a69f07829d
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-30 13:00:56 +0900

    fix(CarNameException): 자동차 이름 최대 5자 이하로 수정

--- src/main/java/racingcar/domain/validator/exception/CarNameException.java
+++ src/main/java/racingcar/domain/validator/exception/CarNameException.java
@@ -2,7 +2,7 @@ package racingcar.domain.validator.exception;
 
 public enum CarNameException {
     NO_VALUE_NAME("자동차 이름을 위한 입력값이 입력되지 않았습니다."),
-    INVALID_TOO_LONG_NAME("자동차 이름은 최대 10자 이하로 가능합니다."),
+    INVALID_TOO_LONG_NAME("자동차 이름은 최대 5자 이하로 가능합니다."),
     INVALID_TOO_SHORT_NAME("자동차 이름은 최소 2자 이상으로 가능합니다.");
 
     private final String message;
