2a08770cbda97e85763f79cc8ae8cdebe45eec7f
jaegyeongkim <dongji_11@naver.com>
2023-10-30 19:40:45 +0900

    test(CarsTest): 최종 우승자들 추려내는 기능 테스트

--- src/test/java/model/CarsTest.java
+++ src/test/java/model/CarsTest.java
@@ -19,4 +19,23 @@ public class CarsTest {
         assertThatThrownBy(() -> new Cars(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    public void 최대_전진_거리_반환(){
+        String input = "pobi,jun,woni";
+        Cars cars = new Cars(input);
+        cars.getCars().get(0).tryMove(4);
+        cars.getCars().get(1).tryMove(1);
+        cars.getCars().get(2).tryMove(9);
+        assertThat(cars.getMaxPosition()).isEqualTo(1);
+    }
+    @Test
+    public void 최종_우승자들_반환(){
+        String input = "pobi,jun,woni";
+        Cars cars = new Cars(input);
+        cars.getCars().get(0).tryMove(4);
+        cars.getCars().get(1).tryMove(1);
+        cars.getCars().get(2).tryMove(9);
+        assertThat(cars.getWinner().size()).isEqualTo(2);
+    }
 }
