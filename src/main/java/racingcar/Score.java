665e50424881f6d24ed65c590704cb7b671995f5
yujeong-shin <vouobb@naver.com>
2023-10-30 14:54:25 +0900

    class Score printWinnerResult함수 간결하게 수정 <before> for(Car car : Cars) {     if(car.score == max) count++; }
    winnerNames = new String[count];
        for(Car car : Cars) {
            if(car.score == max) {
                winnerNames[count] = car.name;
            }
        }
    
    <after>
    for(Car car : Cars) {
        if(car.score == max) {
            winnerNames.add(car.name);
        }
    }

--- src/main/java/racingcar/Score.java
+++ src/main/java/racingcar/Score.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Score {
@@ -12,32 +13,26 @@ public class Score {
     }
 
     public void printRoundResult(List<Car> Cars) {
-        String result = "";
+        System.out.println();
         for(Car car : Cars) {
             System.out.println(car.name + " : " + scoreVisualization(car.score));
         }
     }
 
     public void printWinnerResult(List<Car> Cars) {
-        //List<String> members = Arrays.asList("pobi", "jason");
-        //String result = String.join(",", members); // "pobi,jason"
         int max = Integer.MIN_VALUE;
-        int count=0;
-        String[] winnerNames;
+        List<String> winnerNames = new ArrayList<>();
         String result="";
 
         for(Car car : Cars) {
             if(car.score > max) max = car.score;
         }
         for(Car car : Cars) {
-            if(car.score == max) count++;
-        }
-        winnerNames = new String[count];
-        for(Car car : Cars) {
             if(car.score == max) {
-                winnerNames[count] = car.name;
+                winnerNames.add(car.name);
             }
         }
+        System.out.println();
         result = String.join(",", winnerNames);
         System.out.println("최종 우승자 : " + result);
     }
