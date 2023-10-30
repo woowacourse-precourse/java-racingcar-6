61b97ae7a3ceeb07c0ad59c9a5250f4c6b4429ec
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 19:37:14 +0900

    feat: 0~9까지의 무작위 숫자 생성

--- src/main/java/racingcar/model/User.java
+++ src/main/java/racingcar/model/User.java
@@ -1,8 +1,12 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static racingcar.util.Constants.MIN_RANGE;
+import static racingcar.util.Constants.MAX_RANGE;
 import static racingcar.util.Constants.NUMBER_REGEXP;
 import static racingcar.util.VerificationMessage.IS_NOT_NUMBER;
 
@@ -15,6 +19,9 @@ public class User {
     public int moveSize(String size) {
         if(!isNumber(size)) throw new IllegalArgumentException(IS_NOT_NUMBER);
         return Integer.parseInt(size);
+    }
+    private int createRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANGE,MAX_RANGE);
     }
     private boolean isNumber(String size) {
         Pattern pattern = Pattern.compile(NUMBER_REGEXP);
