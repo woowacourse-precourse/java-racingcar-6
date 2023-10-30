c924c981cf422a76e9e2df55ff398576d006c363
sami355-24 <ddung355@gmail.com>
2023-10-30 11:13:00 +0900

    refactor(Contorller): 변수들을 선언한 부분과 실질적인 로직이 담겨있는 메소드사이에 공백을 추가해 가독성 높임

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -14,6 +14,7 @@ public class GameController {
     CarModel carModel;
     RoundModel roundModel;
     GameModel gameModel;
+
     public void initGame() {
         inputView = new InputView();
         outPutView = new OutPutView();
