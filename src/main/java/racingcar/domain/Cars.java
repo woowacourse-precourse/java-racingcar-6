07886c93251ba8288a0c96d7f067d834b87397b0
gundorit <packdev937@gmail.com>
2023-10-30 21:52:12 +0900

    feat (Cars) : 자동차 이름이 중복되지 않는지 검증한다

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ public class Cars {
 
     public Cars(List<String> carNames) {
         validateCarNamesSize(carNames);
+        validateDuplicatedCarNames(carNames);
         cars = carNames.stream()
             .map(name -> new Car(name))
             .collect(Collectors.toList());
@@ -23,6 +25,16 @@ public class Cars {
     public void validateCarNamesSize(List<String> carNames) {
         if (carNames.size() < 2) {
             throw new IllegalArgumentException("자동차는 두 대 이상 입력해야 합니다.");
+        }
+    }
+
+    public void validateDuplicatedCarNames(List<String> carNames) {
+        HashMap<String, Integer> namesMap = new HashMap<>();
+        for (String name : carNames) {
+            namesMap.put(name, namesMap.getOrDefault(name, 0) + 1);
+            if (namesMap.get(name) > 1) {
+                throw new IllegalArgumentException("중복되는 이름이 있으면 안됩니다.");
+            }
         }
     }
 }
