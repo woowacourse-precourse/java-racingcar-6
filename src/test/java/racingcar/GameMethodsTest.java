6457310d5a0d3986400b703ee6b9be045fb07a8b
Jihye Lee <zerom980706@gmail.com>
2023-10-30 17:52:20 +0900

    test(gameMethods): add a test for testing car movement record feature

--- src/test/java/racingcar/GameMethodsTest.java
+++ src/test/java/racingcar/GameMethodsTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Index.atIndex;
+import static racingcar.GameMethods.goOrStop;
 import static racingcar.GameMethods.makeRandNum;
 
 import org.junit.jupiter.api.Test;
@@ -16,4 +18,18 @@ class GameMethodsTest {
             assertThat(n).isBetween(0, 9);
         }
     }
+
+    @Test
+    void goOrStopTest() {
+        int numberOfCars = 3;
+        String[] movedDistance = {"", "", ""};
+        int[] howFar = {1, 5, 9};
+        goOrStop(numberOfCars, movedDistance, howFar);
+
+        assertThat(movedDistance)
+                .contains("", atIndex(0))
+                .contains("-", atIndex(1))
+                .contains("-", atIndex(2));
+    }
+
 }
