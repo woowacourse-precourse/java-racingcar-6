d4a611dd116fc2d9d7ea3d1bc8f6056a28096d0c
jiixon <jr0107380@gmail.com>
2023-10-31 00:44:38 +0900

    feat: Car 도메인 설정

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int score;
+
+    public void Car(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+    public String getName(String name){
+        return name;
+    }
+    public int getScore(int score){
+        return score;
+    }
+
+
+
+}
