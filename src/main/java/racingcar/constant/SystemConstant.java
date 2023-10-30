d2d74887ff9640ec8c5bd0d2591d9cb400360ea4
jang010505 <jang010505@inu.ac.kr>
2023-10-31 03:09:58 +0900

    feat: SystemConstant 객체 생성 및 필드 작성

---
+++ src/main/java/racingcar/constant/SystemConstant.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class SystemConstant {
+    public static final int CAR_NAME_LENGTH_MIN = 1;
+    public static final int CAR_NAME_LENGTH_MAX = 5;
+
+    public static final String SPACE = " ";
+    public static final String COMMA = ",";
+
+}
