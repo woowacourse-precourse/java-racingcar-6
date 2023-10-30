65f07dee46a773a184c32818892316e1de6d6b32
junghunim07 <dlawndgns9@naver.com>
2023-10-30 22:58:42 +0900

    feat(Race): makeCars 메소드 구현

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class Race {
     static List<Car> carRace = new ArrayList<>();
 
-    public Race() {
+    public Race(List<String> list) {
+        makeCars(list);
+    }
 
+    private void makeCars(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Car car = new Car(list.get(i));
+            carRace.add(car);
+        }
     }
 }
