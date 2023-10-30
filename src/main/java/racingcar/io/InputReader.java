7c48dc42f7983571d085292e54813b3e1a2f7efd
Jiihyun <cwlgusc@gmail.com>
2023-10-30 20:16:47 +0900

    feat: 사용자 입력 값 읽어들이기 및 널값 검증

--- src/main/java/racingcar/io/InputReader.java
+++ src/main/java/racingcar/io/InputReader.java
@@ -1,4 +1,18 @@
 package racingcar.io;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.ExceptionMessage;
+
 public class InputReader {
+    public String readLine() {
+        String input = Console.readLine().trim();
+        validateIfIsBlank(input);
+        return input;
+    }
+
+    private void validateIfIsBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw ExceptionMessage.INPUT_BLANK.getException();
+        }
+    }
 }
