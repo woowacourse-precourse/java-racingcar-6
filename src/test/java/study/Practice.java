01b64a13af4ffeb6fecb5187796a1ffb0bd1f865
MyStraw <kikongdosa@gmail.com>
2023-10-30 22:05:11 +0900

    feat(Practice): 로직풀이 코드 연습
    기능구현 목록에 없는 코드 작성
    테스트코드에 미리 연습하면서 어떻게 풀면될지 생각

---
+++ src/test/java/study/Practice.java
@@ -0,0 +1,65 @@
+package study;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Practice {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String carNames = br.readLine();
+        // String[] carName = carNames.split(",");
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        // int n = carName.length;
+        int movement = Integer.parseInt(br.readLine());
+
+        List<ownCar> cars = new ArrayList<>();
+
+        for (String name : carNameList) {
+            cars.add(new ownCar(name));
+        }
+
+        for (int i = 0; i < movement ; i++) {
+            for (ownCar car : cars) {
+                car.move();
+                System.out.println(car.getName() + " : " + car.getPosition());
+            }
+            System.out.println();
+        }
+    }
+}
+
+class ownCar {
+    private String name;
+    private int position;
+    private Random random;
+
+    public ownCar(String name) {
+        this.name = name;
+        this.position = 0;
+        this.random = new Random();
+    }
+
+    public void move() {
+        if (random.nextInt(10) >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+}
+
