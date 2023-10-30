5549a11b1fa329b64f602260647be32909037118
leorb159 <ldgsocial159@naver.com>
2023-10-31 05:30:11 +0900

    feat(Output): 실행결과 문구 출력 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -3,11 +3,16 @@ package racingcar;
 public class Output {
     private String enterNameOfCar = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private String enterTryTime = "시도할 회수는 몇회인가요?";
+    private String executionResult = "실행결과";
     public void printEnterNameOfCar(){
         System.out.println(enterNameOfCar);
     }
 
     public void printEnterTryTime(){
         System.out.println(enterTryTime);
+    }
+
+    public void printExecutionResult(){
+        System.out.println(executionResult);
     }
 }
