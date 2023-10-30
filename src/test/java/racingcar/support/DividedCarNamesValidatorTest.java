4f0549cdd7b04b10060a6309a55f9cb931deac44
kssumin <201566@jnu.ac.kr>
2023-10-31 04:00:08 +0900

    refactor : jupiter Assertions 사용하도록 변경

--- src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
+++ src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
@@ -1,7 +1,8 @@
 package racingcar.support;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,9 +14,8 @@ class DividedCarNamesValidatorTest {
 	@ValueSource(strings = "12345")
 	void validate_divided_count(String input) {
 		// when & then
-		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining("자동차 이름은 2개 이상 입력해야합니다");
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> DividedCarNamesValidator.validate(input.split(",")));
 	}
 
 	@DisplayName("자동차 이름은 좌우 공백을 제외하고 1 ~ 5 글자로 입력해야 한다.")
@@ -23,19 +23,18 @@ class DividedCarNamesValidatorTest {
 	@ValueSource(strings = {"     , "})
 	void validate_car_empty(String input) {
 		// when & then
-		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> DividedCarNamesValidator.validate(input.split(",")));
 	}
 
-	@DisplayName("자동차 이름은 1 ~ 5 글자로 입력해야합니다")
+	@DisplayName("자동차 이름은 1 ~ 5 글자로 입력해야 한다.")
 	@ParameterizedTest
 	@ValueSource(strings = {"12345, 1234567"})
 	void validate_car_length(String input) {
 		// when & then
-		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+		assertThrows(IllegalArgumentException.class,
+				() -> DividedCarNamesValidator.validate(input.split(",")));
+
 	}
 
 	@DisplayName("자동차 이름은 중복될 수 없다.")
@@ -43,8 +42,7 @@ class DividedCarNamesValidatorTest {
 	@ValueSource(strings = {"12345, 12345"})
 	void validate_duplicated_car_name(String input) {
 		// when & then
-		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+		assertThrows(IllegalArgumentException.class,
+				() -> DividedCarNamesValidator.validate(input.split(",")));
 	}
 }
