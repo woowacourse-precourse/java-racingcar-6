f13525494bd0e30898458c8d8f8cca7ac2dc178f
mikekks <thdalsrb79@naver.com>
2023-10-30 20:52:35 +0900

    [feat] Car 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final Integer THRESHOLD = 4;
+    private String name;
+    private Integer moveNum;
+
+
+    public Car(String name, Integer moveNum) {
+        this.name = name;
+        this.moveNum = moveNum;
+    }
+
+    public void tryMove(int randomNum) {
+        if (randomNum >= THRESHOLD) {
+            moveOneSpace();
+        }
+    }
+
+    private void moveOneSpace() {
+        moveNum++;
+    }
+}
