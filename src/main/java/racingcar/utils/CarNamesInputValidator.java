0462c06e588641036468660e61a5154260070e56
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 16:01:28 +0900

    refactor(CarNamesInputValidator): 상수 처리

--- src/main/java/racingcar/utils/CarNamesInputValidator.java
+++ src/main/java/racingcar/utils/CarNamesInputValidator.java
@@ -1,12 +1,18 @@
 package racingcar.utils;
 
 public class CarNamesInputValidator {
+
+    private static final String SPLIT_REGEX = ",";
+    private static final char COMMA = ',';
+    private static final int VALID_MAX_LENGTH = 5;
+
+
     public static void validate(String target) {
         validateIsNotBlank(target);
         validateFirstCharacterIsNotComma(target);
         validateLastCharacterIsNotComma(target);
 
-        String[] elements = target.split(",");
+        String[] elements = target.split(SPLIT_REGEX);
         validateEachElementIsNotBlank(elements);
         validateEachElementCorrectLength(elements);
     }
@@ -18,13 +24,13 @@ public class CarNamesInputValidator {
     }
 
     private static void validateFirstCharacterIsNotComma(String target) {
-        if (target.charAt(0) == ',') {
+        if (target.charAt(0) == COMMA) {
             throw new IllegalArgumentException(ExceptionMessage.FIRST_CHARACTER_COMMA.getError());
         }
     }
 
     private static void validateLastCharacterIsNotComma(String target) {
-        if (target.charAt(target.length() - 1) == ',') {
+        if (target.charAt(target.length() - 1) == COMMA) {
             throw new IllegalArgumentException(ExceptionMessage.LAST_CHARACTER_COMMA.getError());
         }
     }
@@ -39,7 +45,7 @@ public class CarNamesInputValidator {
 
     private static void validateEachElementCorrectLength(String[] targets) {
         for (String target : targets) {
-            if (target.length() > 5) {
+            if (target.length() > VALID_MAX_LENGTH) {
                 throw new IllegalArgumentException(ExceptionMessage.OUT_OF_LENGTH.getError());
             }
         }
