cd3535587a32f96cc20cebeb7abd59a2ac1f195d
muyaaho <skanthsu715@gmail.com>
2023-10-30 22:53:54 +0900

    refactor(racingcar): InputView의 메소드 줄이기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,22 +2,8 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class InputView {
-
-    public List<String> getNames(){
-        String inputString = Console.readLine();
-        List<String> nameList = new ArrayList<>();
-        for (String name: inputString.replaceAll(" ", "").split(",")){
-            nameList.add(name);
-        }
-        return nameList;
-    }
-
-    public String askHowMany(){
-        String inputString = Console.readLine();
-        return inputString;
+    public String getLine(){
+        return Console.readLine();
     }
 }
