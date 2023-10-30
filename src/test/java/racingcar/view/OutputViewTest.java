f1ba5053d6086e85da0cde770cd9bb3730354dab
Choo <choo000407@naver.com>
2023-10-31 07:06:40 +0900

    test(OutputViewTest): `우승 자동차` 출력 테스트 추가

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -21,7 +21,7 @@ public class OutputViewTest {
     }
 
     @Test
-    void 자동차들_위치_출력시_포맷팅해_반환() {
+    void 자동차들_위치_포맷팅해_출력() {
         CarDto winnerCar = new CarDto("pobi",3);
         CarDto nonWinnerCar = new CarDto("woni", 2);
 
@@ -29,5 +29,26 @@ public class OutputViewTest {
 
         assertThat(outputStream.toString())
                 .isEqualTo("pobi : ---\nwoni : --\n\n");
+    }
+
+    @Test
+    void 우승_자동차_한대_일때_출력() {
+        String winnerCarName = "pobi";
+
+        outputView.printWinnerCar(List.of(winnerCarName));
+
+        assertThat(outputStream.toString())
+                .isEqualTo("최종 우승자 : pobi\n");
+    }
+
+    @Test
+    void 우승_자동차_여러대_일때_포맷팅해_출력() {
+        String winnerCarName = "pobi";
+        String nonWinnerCarName = "woni";
+
+        outputView.printWinnerCar(List.of(winnerCarName, nonWinnerCarName));
+
+        assertThat(outputStream.toString())
+                .isEqualTo("최종 우승자 : pobi, woni\n");
     }
 }
