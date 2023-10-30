089e8bc44111ae945986e29c140913d79cefbdfe
kangyuseok <kingyuseok@naver.com>
2023-10-31 01:59:25 +0900

    Feat: startGame()의 기능 추가
    1. 게임 첫 시작 출력문
    2. 자동차 이름들 입력
    3. 입력된 이를들을 parsing할 parkParkingLot() 호출

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,10 +1,21 @@
 package racingcar.controller;
 
+import static racingcar.view.Input.askCarNames;
 import static racingcar.view.Print.suggestCarName;
 import static racingcar.view.constant.ConstPrint.GAME_START;
 
-public class RacingCarGame {
-    public void gameStart(){
+import racingcar.service.CarService;
+
+public class RacingCarGame{
+    private CarService carService = CarService.getInstance();
+    private String cars;
+    public void startGame(){
         suggestCarName(GAME_START);
+        cars = askCarNames();
+        carService.parkParkingLot(cars);
+    }
+
+    private void playGame(){
+
     }
 }
