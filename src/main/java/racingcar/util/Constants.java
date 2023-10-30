0c37d95333cbb1f1c59699bfe7a3a00eba704497
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 16:21:58 +0900

    feat(Constants): 상수 클래스 생성
    - 랜덤으로 생성할 숫자의 최솟값 및 최댓값 상수 생성
    - 이동을 위한 최소 임계값 상수 생성

---
+++ src/main/java/racingcar/util/Constants.java
@@ -0,0 +1,7 @@
+package racingcar.util;
+
+public class Constants {
+    public final static int MIN_INPUT_RANGE = 0;
+    public final static int MAX_INPUT_RANGE = 9;
+    public final static int MIN_MOVE_THRESHOLD = 4;
+}
