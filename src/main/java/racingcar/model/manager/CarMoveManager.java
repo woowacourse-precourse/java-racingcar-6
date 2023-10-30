3ca319031d782cd607ef98697fb75d168f0f3d01
IM-GYURI <kgulr0517@gmail.com>
2023-10-30 20:48:39 +0900

    Feat: 추가 CarMoveManger 클래스

--- src/main/java/racingcar/model/manager/CarMoveManager.java
+++ src/main/java/racingcar/model/manager/CarMoveManager.java
@@ -1,4 +1,13 @@
 package racingcar.model.manager;
 
+import racingcar.util.RandomNumberValidator;
+
 public class CarMoveManager {
+    private static final int CHECK_NUMBER = 4;
+
+    public boolean isMove(int number) {
+        RandomNumberValidator.validate(number);
+        return number >= CHECK_NUMBER;
+    }
+
 }
