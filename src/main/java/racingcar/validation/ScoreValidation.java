de043fa57c34bdb3117b7295dfffa801f5a3ae88
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 20:09:13 +0900

    fix: modify verify logic score value is numeric

--- src/main/java/racingcar/validation/ScoreValidation.java
+++ src/main/java/racingcar/validation/ScoreValidation.java
@@ -5,7 +5,7 @@ import racingcar.property.ErrorProperty;
 public class ScoreValidation extends InputValidation{
 
     public static void verifyForScoreIsNumericValue(String targetScore){
-        if (!targetScore.equals("\\d")){
+        if (!targetScore.matches("\\d")){
             throw new IllegalArgumentException(ErrorProperty.SCORE_VALUE_IS_NOT_NUMERIC);
         }
     }
