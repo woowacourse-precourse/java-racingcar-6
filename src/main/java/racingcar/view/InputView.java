a1a3f7a6ac21b9ddc6650a79e698964143512901
junest66 <chlwndks333@gmail.com>
2023-10-31 02:28:51 +0900

    style(InputView): 시도입력 메세지 오타 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String PROMPT_MESSAGE = "시도할 회수는 몇 회인가요?";
+    private static final String PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public static String getCarNames() {
         System.out.println(GAME_START_MESSAGE);
