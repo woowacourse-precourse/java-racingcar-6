a1ab4786e13110abb7d229c3a7ccb4cf37f5e3ec
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-30 21:19:26 +0900

    test: 자동차 길이 예외처리 테스트 추가

--- src/test/java/domain/InputManagerTest.java
+++ src/test/java/domain/InputManagerTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,5 +37,31 @@ public class InputManagerTest {
         assertTrue(parsedCarList.stream().anyMatch(car -> car.getName().equals("jun")));
     }
 
+    @Test
+    @DisplayName("자동차의 길이가 5 이상에서 에러가 발생하는지 확인")
+    public void 자동차길이_5이상() throws Exception {
+        try {
+            new InputManager(5, "pobi,woni,too long");
+            fail("문자열의 길이가 5이상이 아님");
+        } catch (IllegalArgumentException e) {
 
+        }
+    }
+
+    @Test
+    public void 자동차길이_0이하() throws Exception {
+        try {
+            new InputManager(5, "pobi, ,jun");
+            fail("문자열이 공백이 아님");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        try {
+            new InputManager(5, "pobi,,jun");
+            fail("문자열이 공백이 아님");
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
 }
