334912a25680d02ca39e3f8d4397dc6d62c46f80
wonindev <sangwonparkdev@gmail.com>
2023-10-30 18:52:06 +0900

    feat: 시도 횟수에 공백이 포함되어 있는 경우 예외처리를 기능 추가

--- src/main/java/racingcar/ExceptionHandler.java
+++ src/main/java/racingcar/ExceptionHandler.java
@@ -13,6 +13,10 @@ public class ExceptionHandler {
             throw new IllegalArgumentException("시도 횟수가 공백입니다.");
         }
 
+        if (Validation.isBlankContain(attempt)) {
+            throw new IllegalArgumentException("시도 횟수가 공백을 포함합니다.");
+        }
+
         if (Validation.isNegative(Integer.parseInt(attempt))) {
             throw new IllegalArgumentException("시도 횟수가 0보다 작습니다.");
         }
@@ -25,6 +29,10 @@ public class ExceptionHandler {
     }
 
     public void isMembersException(List<String> members) {
+        if (Validation.isBlank(members)) {
+            throw new IllegalArgumentException("자동차 이름이 공백입니다.");
+        }
+
         if (Validation.isOverFiveWord(members)) {
             throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
         }
