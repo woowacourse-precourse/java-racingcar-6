e1c74ee4e8786eb8dded4d5e85caab0332bda06f
siwol025 <cyk99115695@gmail.com>
2023-10-30 20:23:52 +0900

    feat(racingcar): 가장 먼 위치에 있는 자동차를 찾는 기능 구현

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -3,13 +3,20 @@ package racingcar.domain;
 import java.util.List;
 
 public class Referee {
+    private final List<Car> cars;
+
+    public Referee(List<Car> cars) {
+        this.cars = cars;
+    }
 
     public List<Car> findWinners() {
         return null;
     }
 
     public Car findFarthestPositionCar() {
-        return null;
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("자동차 리스트가 비어있습니다."));
     }
 
     public List<Car> findSamePositionCar() {
