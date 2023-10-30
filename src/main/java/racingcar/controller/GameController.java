c96f4cb26d10e0ae7c90e8db6be9f4d0b7a74d10
donghoonyeom <dua5410773@naver.com>
2023-10-31 00:43:58 +0900

    feat(controller): GameController 클래스 추가
    다음과 같은 기능을 수행하는 GameController 클래스를 추가했습니다:
    - RacingGame 모델과 ConsoleView 뷰를 조합하여 게임 플레이를 관리합니다.
    - 주어진 횟수만큼 게임을 실행하고, 각 단계에서 자동차를 이동시키고 현재 상태를 콘솔에 출력합니다.
    - 게임이 종료된 후 우승자를 결정하고 콘솔에 출력합니다.

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,27 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.RacingGame;
+import racingcar.view.ConsoleView;
+
+import java.util.List;
+
+public class GameController {
+    private final RacingGame racingGame;
+    private final ConsoleView consoleView;
+
+    public GameController(RacingGame racingGame, ConsoleView consoleView) {
+        this.racingGame = racingGame;
+        this.consoleView = consoleView;
+    }
+
+    public void playGame(int numberOfAttempts) {
+        for (int i = 0; i < numberOfAttempts; i++) {
+            racingGame.moveCars();
+            consoleView.printCurrentPositions(racingGame.getCars());
+        }
+
+        List<Car> winners = racingGame.getWinners();
+        consoleView.printWinners(winners);
+    }
+}
