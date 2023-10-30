98f94a10ffb66fffcd99278bdade7ee72ef9dc6a
gmelon <hsh1769@naver.com>
2023-10-30 14:05:55 +0900

    feat: 인자를 받는 CarPosition의 생성 로직 구현

--- src/main/java/racingcar/domain/CarPosition.java
+++ src/main/java/racingcar/domain/CarPosition.java
@@ -14,4 +14,15 @@ public class CarPosition {
         return new CarPosition(MINIMUM_VALUE);
     }
 
+    public static CarPosition from(int value) {
+        validateValue(value);
+        return new CarPosition(value);
+    }
+
+    private static void validateValue(int value) {
+        if (value < MINIMUM_VALUE) {
+            throw new IllegalArgumentException("자동차의 위치는 양수여야 합니다. 주어진 값은 " + value + " 입니다.");
+        }
+    }
+
 }
