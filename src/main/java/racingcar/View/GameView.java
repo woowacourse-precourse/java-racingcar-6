20a7957f1cc7945291f698b0cfee70adf93029a4
jiixon <jr0107380@gmail.com>
2023-10-31 01:39:41 +0900

    refactor: 시도하는 횟수 입력함수 리턴값 수정

--- src/main/java/racingcar/View/GameView.java
+++ src/main/java/racingcar/View/GameView.java
@@ -6,8 +6,10 @@ public class GameView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
-    public String inputTrialGame(){
+    public int inputTrialGame(){
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        String trialNumStr = Console.readLine();
+        int trialNum = Integer.parseInt(trialNumStr);
+        return trialNum;
     }
 }
