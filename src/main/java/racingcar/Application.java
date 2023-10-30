f745074906bbe102e5c6c811959d1d69b43613f6
mj.kim <alswo9853@gmail.com>
2023-10-30 14:08:40 +0900

    feat: Add get the number of game from user input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,9 +11,13 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Referee referee = new Referee();
+
         referee.notifyInsertCars();
         referee.setCars(Console.readLine());
+
+        referee.notifyNumOfGame();
+        referee.setNumOfGame(Integer.parseInt(Console.readLine()));
+
     }
 }
--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -39,4 +39,8 @@ public class Referee {
     public void notifyInsertCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public void notifyNumOfGame(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
