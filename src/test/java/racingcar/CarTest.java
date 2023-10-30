d92601d28a33893a764197df6a60de37f625783d
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-30 12:18:30 +0900

    test: carTest 메소드명 변경

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -21,14 +21,14 @@ public class CarTest {
 
     @Test
     @DisplayName("주어진 자동차의 이름을 올바르게 반환한다.")
-    void carTest2() throws Exception{
+    void carTest() throws Exception{
         //given, when, then
         assertThat(car.getCarName()).isEqualTo(name.getName());
     }
 
     @Test
     @DisplayName("주어진 랜덤 숫자가 4이상인 경우 자동차 전진하므로 1을 반환한다.")
-    void carTest3() throws Exception{
+    void carTest2() throws Exception{
 
         //given
         final int randomNumber = 4;
@@ -42,7 +42,7 @@ public class CarTest {
 
     @Test
     @DisplayName("주어진 랜덤 숫자가 3이하 경우 자동차가 전진하지 못하므로 0을 반환한다.")
-    void carTest4() throws Exception{
+    void carTest3() throws Exception{
 
         //given
         final int randomNumber = 0;
