54eab478df7b6bdf59d1cc96d86099017c3e9cc3
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 17:48:37 +0900

    feat(Constants): 상수 추가
    - 자동차 최소 개수를 의미하는 상수 추가

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public final static int MAX_INPUT_RANGE = 9;
     public final static int MIN_MOVE_THRESHOLD = 4;
     public final static int MAX_INPUT_SIZE = 5;
+    public final static int MIN_INPUT_COUNT = 2;
 }
