aea700c9b274c53b6cb1ba39885e5124481808ba
jjaegii <hn06038@gmail.com>
2023-10-30 20:03:50 +0900

    feat: InputManager 클래스 내 getCarNames 메소드 추가

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -1,4 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class InputManager {
+    public ArrayList<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] nameArray = input.split(",");
+
+        ArrayList<String> names = new ArrayList<>();
+        for (String name : nameArray) {
+            names.add(name);
+        }
+
+        return names;
+    }
 }
