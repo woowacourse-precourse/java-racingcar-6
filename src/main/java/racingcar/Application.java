1418ad4117ac1eb3af81d49d9056ab33084f2e5d
stone <109290247+astrataraxia@users.noreply.github.com>
2023-10-30 17:19:51 +0900

    feat :  Print Game message and user input
    1. 게임 시작 후, 진행 메시지 출력
    2. 유저 입력 기능 구현
    3. 입력받는 carName과 tryCount를 검증하고  파싱할 메서드 네임 구현
    4. CarName을 저장해 놓을 Car Class 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        racingGame();
+    }
+
+    private static void racingGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarNames = Console.readLine();
+        List<Car> cars = parsingCarName(inputCarNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputTryCount = Console.readLine();
+        int tryCount = parsingTryCount(inputTryCount);
+    }
+
+    private static List<Car> parsingCarName(String inputCarNames) {
+        //TODO: Name Parsing 기능 구현 및 validator class 구현
+        return null;
+    }
+
+    private static int parsingTryCount(String inputTryCount) {
+        //TODO: tryCount 기능 구현 및 validator class 구현
+        return 0;
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,5 @@
+package racingcar;
+
+public class Car {
+    //TODO: Car Class 기능 구현
+}
