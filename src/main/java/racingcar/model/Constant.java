2551b7cde0ce52aeaafe5901c28d717af4d08167
DongHyun <kimdh0315@naver.com>
2023-10-30 15:09:40 +0900

    feat(src/main/model/Constant) : define constants for max length of name of car & conditions of car’s moving

---
+++ src/main/java/racingcar/model/Constant.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Constant {
+    /* constants for Car name */
+    public static final Integer MAX_LEN_OF_CAR_NAME = 5;
+
+    /* constants for condition of car's moving forward */
+    public static final Integer FORWARD_BOUNDARY = 4;
+    public static final Integer BOUNDARY_START = 0;
+    public static final Integer BOUNDARY_END = 9;
+}
