1661d72e58f25626bc23b8cdf4049af74a1c4688
sh990920 <sh990920@gmail.com>
2023-10-30 17:40:22 +0900

    test : winner 메소드 테스트

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -186,4 +186,28 @@ public class GameTest {
 
         assertThat(maxCount).isEqualTo(3);
     }
+
+    @Test
+    public void testWinner(){
+        Game game = new Game();
+
+        int randNumber = game.randomNumber(5);
+
+        List<String> people = game.addPerson("user1,user2");
+
+        List<Car> carList = game.addCar(people);
+
+        systemIn("3");
+        int count = game.count();
+
+        game.gameResult(carList, randNumber, count);
+
+        int maxCount = game.maxCount(carList);
+
+        List<String> winnerList = game.winner(carList, maxCount);
+
+        assertThat(winnerList.size()).isEqualTo(2);
+        assertThat(winnerList.get(0)).isEqualTo("user1");
+        assertThat(winnerList.get(1)).isEqualTo("user2");
+    }
 }
