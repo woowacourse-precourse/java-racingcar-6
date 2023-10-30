b02d365616c32b4b9031ae90b7bac0cd0a2031eb
eora21 <eora21@naver.com>
2023-10-30 14:42:58 +0900

    feat: 🎸 추가 콘솔을 통한 레이싱 게임 결과 처리하는 메서드 작성

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -1,10 +1,18 @@
 package racingcar.view;
 
+import racingcar.domain.CarStatus;
+import racingcar.domain.RaceStatus;
+import racingcar.domain.RacingGameStatus;
+
 public class ConsoleOutputView implements OutputView {
 
     private static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.";
     private static final String SEPARATED_BY = "(이름은 %s(%s) 기준으로 구분)";
     private static final String REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String DRIVE_COUNT_DESCRIPTION_TEXT = "-";
+    private static final String CAR_STATUS_MESSAGE = "%s : %s";
+    private static final String WINNERS = "최종 우승자 : %s";
 
     @Override
     public void requestCarNames(String delimiterDescription, String delimiter) {
@@ -19,5 +27,29 @@ public class ConsoleOutputView implements OutputView {
     @Override
     public void requestTryCount() {
         System.out.println(REQUEST_TRY_COUNT);
+    }
+
+    @Override
+    public void printRacingGameStatus(RacingGameStatus racingGameStatus) {
+        printResultMessage();
+        racingGameStatus.getRaceStatuses()
+                .forEach(this::printRaceStatus);
+    }
+
+    private void printResultMessage() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    private void printRaceStatus(RaceStatus raceStatus) {
+        raceStatus.getCarStatuses()
+                .forEach(this::printCarStatus);
+        System.out.println();
+    }
+
+    private void printCarStatus(CarStatus carStatus) {
+        String driveCountText = DRIVE_COUNT_DESCRIPTION_TEXT.repeat(carStatus.getDriveCount());
+        String carStatusMessage = String.format(CAR_STATUS_MESSAGE, carStatus.getName(), driveCountText);
+        System.out.println(carStatusMessage);
     }
 }
