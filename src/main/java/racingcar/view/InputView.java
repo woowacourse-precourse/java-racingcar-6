aa30942841c5cced2ace1333b47d20f431565d73
muyaaho <skanthsu715@gmail.com>
2023-10-30 17:22:03 +0900

    refactor(racingcar): 입력 받는 부분과 출력 받는 부분 옮기기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,25 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.Validate;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    public List<String> askNames(){
-        List<String> names = new ArrayList<>();
-        return names;
+    Validate validate = new Validate();
+
+    public List<String> getNames(){
+        String inputString = Console.readLine();
+        List<String> nameList = new ArrayList<>();
+        for (String name: inputString.replaceAll(" ", "").split(",")){
+            nameList.add(name);
+        }
+        return nameList;
     }
 
-    public int askHowMany(){
-        return 0;
+    public String askHowMany(){
+        String inputString = Console.readLine();
+        return inputString;
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,15 +3,23 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
+    public void askNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
     public void printResult(){
         System.out.println("실행 결과");
     }
 
-    public void intermPrint(String intermprint){
+    public void printInterm(String intermprint){
         System.out.println(intermprint);
     }
 
     public void printWinner(List<String> winners){
         // 결과 리턴
+    }
+
+    public void printError(){
+        System.out.println("잘못된 값을 입력하셨습니다. 프로그램이 종료됩니다.");
     }
 }
