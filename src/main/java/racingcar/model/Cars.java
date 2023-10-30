11e39b20cd35c16fb4da912e9ec8de9cca6ec20f
eom-tae-in <eti0728@daum.net>
2023-10-30 22:01:04 +0900

    refactor: CarRacingGame를 Cars로 변경

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,78 @@
+package racingcar.model;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import racingcar.exception.car_racing_manager.NotExistCarException;
+import racingcar.exception.car_racing_manager.NotUniqueCarNameException;
+import racingcar.util.NumberGenerator;
+
+public class Cars implements Vehicles {
+
+    private final List<Car> cars;
+
+    private Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars createWith(final List<String> carNames) {
+        validateNameDuplicate(carNames);
+        validateCarExistence(carNames);
+        return new Cars(convertToCarsFrom(carNames));
+    }
+
+    private static void validateNameDuplicate(final List<String> carNames) {
+        if (hasDuplicate(carNames)) {
+            throw new NotUniqueCarNameException(carNames);
+        }
+    }
+
+    private static boolean hasDuplicate(final List<String> carNames) {
+        int distinctCount = (int) carNames.stream()
+                .distinct()
+                .count();
+
+        return distinctCount != carNames.size();
+    }
+
+    private static void validateCarExistence(final List<String> carNames) {
+        if (carNames.isEmpty()) {
+            throw new NotExistCarException();
+        }
+    }
+
+    private static List<Car> convertToCarsFrom(final List<String> carNames) {
+        return carNames.stream()
+                .map(Car::createWith)
+                .toList();
+    }
+
+    @Override
+    public void moveAll(final NumberGenerator numberGenerator) {
+        cars.forEach(car -> car.move(numberGenerator));
+    }
+
+    @Override
+    public Map<String, Integer> getResult() {
+        return cars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getPosition, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+    @Override
+    public List<String> getWinner() {
+        int maxPosition = findMaxPosition();
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(NotExistCarException::new);
+    }
+}
