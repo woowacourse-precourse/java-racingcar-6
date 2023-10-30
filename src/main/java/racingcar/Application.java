9f59a198319d8da7e738158c044c75826385d3d3
naayoung <ask5046a@gmail.com>
2023-10-30 22:07:45 +0900

    feat: add RandomDistance

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // 입력값 받기
         InputValue inputValue  = new InputValue();
         InputValue inputTime  = new InputValue();
+        //랜덤 거리
+        RandomDistance randomDis = new RandomDistance();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] value = inputValue.getInputValue();
@@ -12,11 +19,13 @@ public class Application {
         Integer time = inputTime.getInputTime();
         System.out.println("실행 결과");
 
-        for(int i=0; i<time;i++){
-            for(int j=0; j< value.length; j++){
-                System.out.println(value[j] + " :");
-            }
+
+        for(int i=0; i<value.length;i++){
+            String userDis = randomDis.getDistance(time);
+            System.out.println(value[i] + " :" + userDis);
         }
+        System.out.println(" ");
+
         System.out.println("최종 우승자 : ");
     }
 }
