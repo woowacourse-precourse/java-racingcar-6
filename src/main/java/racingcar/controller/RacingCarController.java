c26bdf6906e8cf0980d43d40d68e232465d21d1d
1Bronze <khdkkhdd@gmail.com>
2023-10-31 00:00:55 +0900

    refactor: create car domain

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -5,7 +5,9 @@ import static racingcar.common.Constants.CAR_NAME_REQUEST_MESSAGE;
 import static racingcar.common.Constants.PLAY_COUNT_REQUEST_MESSAGE;
 import static racingcar.common.Constants.PRINT_RESULT_MESSAGE;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.service.RacingCarService;
 import racingcar.util.StringUtil;
 import racingcar.validator.InputValidator;
@@ -16,21 +18,34 @@ public class RacingCarController {
     private final RacingCarService racingCarService = new RacingCarService();
 
     public void start() {
-        List<String> carList = inputCarList();
+        List<Car> carList = inputCarList();
         Integer playCount = inputPlayCount();
     }
 
-    private List<String> inputCarList() {
+    private List<Car> inputCarList() {
         OutputView.printInfo(CAR_NAME_REQUEST_MESSAGE);
-        List<String> carList = StringUtil.splitByDelimiter(InputView.input(), CAR_LIST_DIVIDER);
-        InputValidator.validateCarList(carList);
+
+        List<String> carNames = StringUtil.splitByDelimiter(InputView.input(), CAR_LIST_DIVIDER);
+        InputValidator.validateCarList(carNames);
+
+        List<Car> carList = createCarList(carNames);
+        return carList;
+    }
+
+    private List<Car> createCarList(List<String> carNames) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carNames) {
+            carList.add(new Car(carName));
+        }
         return carList;
     }
 
     private Integer inputPlayCount() {
         OutputView.printInfo(PLAY_COUNT_REQUEST_MESSAGE);
+
         Integer playCount = StringUtil.convertToNumber(InputView.input());
         InputValidator.validatePlayCount(playCount);
+
         return playCount;
     }
 }
