0ac9398bf439f5cd0beb81fa4ab1a8e740438b45
inwoo <korean5078@gmail.com>
2023-10-30 14:28:44 +0900

    fix(Player): inputNames 함수에서 질문 출력 순서 변경

--- src/main/java/racingcar/domain/Player.java
+++ src/main/java/racingcar/domain/Player.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class Player {
     public List<String> inputNames() throws IllegalArgumentException {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] inputs = Console.readLine().split(",");
         List<String> names = new ArrayList<>();
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         for (String name : inputs) {
             validateName(name);
             names.add(name);
