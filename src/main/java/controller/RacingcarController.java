a96cc7fcf1cbe8cf78e12cbf34387fe2f6fb7b3d
songsunkook <songsunkook@gmail.com>
2023-10-30 10:28:14 +0900

    feat: RacingcarController 생성자 수정

--- src/main/java/controller/RacingcarController.java
+++ src/main/java/controller/RacingcarController.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class RacingcarController {
+import view.InputView;
+import view.OutputView;
 
+public class RacingcarController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingcarController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,8 +7,8 @@ import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RacingcarController racingcarController = new RacingcarController();
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        RacingcarController racingcarController = new RacingcarController(
+                new InputView(), new OutputView()
+        );
     }
 }
