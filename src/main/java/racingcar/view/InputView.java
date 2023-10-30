3708f40a1bc8f19b95d60c842511fe08d650a303
yuseok0215 <elwlahstmxjf@naver.com>
2023-10-30 22:10:08 +0900

    refactor: 입력받는 기능을 입력알림을 알리는 기능과 병합
    입력받는 것과 입력을 알리는 기능은 다른 역할이지만 메서드를 분리하기에는 너무 직관적인 코드이다. 그래서 하나로 병합하는 것이 명확한 메서드 네이밍이 가능하고 불필요한 메서드 분리를 줄일 수 있다.
    informInputCarNames -> askCarNames
    informInputTryNumber -> askTryNumber

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public void informInputCarNames() {
+    public String informInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 
-    public void informInputTryNumber() {
+    public int informInputTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
+        String stringNumber = Console.readLine();
+        return Integer.parseInt(stringNumber);
     }
 
     public String inputCarNames() {
