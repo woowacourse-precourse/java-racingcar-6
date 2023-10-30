51e6afa38137fb63f359c12b26d7c841ac5c1801
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-30 14:14:15 +0900

    test(InGameReferee): add InGameReferee
    InGameReferee 테스트 코드 작성
    - isBiggerLowerLimit: 3가지 케이스
    - findWinner: 우승 판별

--- src/test/java/racingcar/domain/referee/InGameRefereeTest.java
+++ src/test/java/racingcar/domain/referee/InGameRefereeTest.java
@@ -2,15 +2,39 @@ package racingcar.domain.referee;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.generator.NumberGenerator;
+import racingcar.domain.car.Car;
 
 class InGameRefereeTest {
     @Test
-    public void createBetween0and9_0부터9사이의_숫자_생성(){
-        NumberGenerator numberGenerator = new NumberGenerator();
-        int result=numberGenerator.createBetween0and9();
+    public void isBiggerLowerLimit_LOWER_LIMIT_보다_큰_수인지() throws Exception{
+        InGameReferee inGameReferee = new InGameReferee();
+        final int LOWER_LIMIT=inGameReferee.getLOWER_LIMIT_Fortest();
+        int 작은수= LOWER_LIMIT-1;
+        int 큰수=LOWER_LIMIT+1;
 
-        assertThat(result).isBetween(0,9);
+        boolean 작은result= inGameReferee.isBiggerLowerLimit(작은수);
+        boolean 큰result= inGameReferee.isBiggerLowerLimit(큰수);
+        boolean 같은result= inGameReferee.isBiggerLowerLimit(LOWER_LIMIT);
+
+        assertThat(작은result).isEqualTo(false);
+        assertThat(큰result).isEqualTo(true);
+        assertThat(같은result).isEqualTo(true);
+    }
+
+    @Test
+    public void findWinner_우승_판별() throws Exception{
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+        List<Car> carList=List.of(car1,car2,car3);
+        car2.forward();
+        car3.forward();
+
+        InGameReferee inGameReferee = new InGameReferee();
+        List<Car> result = inGameReferee.findWinner(carList);
+
+        assertThat(result).isEqualTo(List.of(car2,car3));
     }
 }
