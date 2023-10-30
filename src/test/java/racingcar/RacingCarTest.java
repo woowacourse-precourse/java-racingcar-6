7e1f6d883347cdd77e4a6753988689eba2bc8000
seongha <11pi885@gmail.com>
2023-10-30 17:14:12 +0900

    test : 생성자 성공, 실패 테스트 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -6,6 +6,22 @@ import org.junit.jupiter.api.Test;
 public class RacingCarTest {
 
     @Test
+    void createTest_인스턴스_생성_성공() {
+        String name = "test";
+        RacingCar racingCar = new RacingCar(name);
+
+        Assertions.assertThat(racingCar.getName()).isEqualTo("test");
+    }
+
+    @Test
+    void createTest_인스턴스_생성_실패_5글자_이상() {
+        String name = "test11";
+
+        Assertions.assertThatThrownBy(() -> new RacingCar(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void carMoveTest_1칸_움직이기_테스트() {
         RacingCar racingCar = new RacingCar("test");
         racingCar.moveOneBlock();
