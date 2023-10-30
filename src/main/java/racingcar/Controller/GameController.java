51a9230c036b667cfe78b71999cfd2decb4a4b69
sami355-24 <ddung355@gmail.com>
2023-10-30 11:00:16 +0900

    feat(Controller): 게임 컨트롤러 로직 구현하고 Application에서 이를 호출하도록 코드 추가
    - 게임 컨트롤러가 필요로 하는 로직을 구현하고 있는 메소드들을 호출하도록 기능 구현

---
+++ src/main/java/racingcar/Controller/GameController.java
@@ -0,0 +1,42 @@
+package racingcar.Controller;
+
+import racingcar.Domain.CarGroup;
+import racingcar.Model.CarModel;
+import racingcar.Model.GameModel;
+import racingcar.Model.RoundModel;
+import racingcar.View.InputView;
+import racingcar.View.OutPutView;
+
+public class GameController {
+
+    InputView inputView;
+    OutPutView outPutView;
+    CarModel carModel;
+    RoundModel roundModel;
+    GameModel gameModel;
+    public void initGame() {
+        inputView = new InputView();
+        outPutView = new OutPutView();
+        carModel = new CarModel();
+        roundModel = new RoundModel();
+        gameModel = new GameModel();
+
+        startGame();
+    }
+
+    public void startGame() {
+        String carNames = inputView.getCarName();
+        CarGroup carGroup = carModel.preProcessing(carNames);
+        String numberOfRound = inputView.getNumberOfRound();
+        Integer round = roundModel.preProcessing(numberOfRound);
+
+        outPutView.showPrompt();
+        while (round > 0) {
+            carGroup = gameModel.playGameRound(carGroup);
+            outPutView.showRoundResult(carGroup);
+            round--;
+        }
+
+        outPutView.showFinalWinner(carGroup);
+    }
+}
