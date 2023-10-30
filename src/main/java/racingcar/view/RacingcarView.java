8c22445dde39fceabbbce7307385c05a3c87a4eb
shlihs95 <fhzptvh@gmail.com>
2023-10-31 07:37:06 +0900

    Rename: 클래스 이름이 소문자로 시작한 오류 수정(racingCarView -> RacingCarView)

--- src/main/java/racingcar/view/RacingcarView.java
+++ src/main/java/racingcar/view/RacingcarView.java
@@ -2,7 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class racingcarView {
+public class RacingcarView {
 
     public String inputName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
