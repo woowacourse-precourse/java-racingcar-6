dab67136fac38a96f7bd94deedcf28b1020331ad
tam31 <wxogud3377@naver.com>
2023-10-30 16:22:00 +0900

    Test(WinnerTest) : 이동거리별 우승자 검사테스트

--- src/test/java/racingcar/model/WinnerTest.java
+++ src/test/java/racingcar/model/WinnerTest.java
@@ -42,4 +42,68 @@ class WinnerTest {
         //then (기능 작동 후 결과)
         Assertions.assertThat(result).isEqualTo(2);
     }
+
+    @Test
+    public void 단일_우승자_자동차_이름() throws Exception{
+        //given (주어진 값)
+        cars.get(1).moveForward(9);
+        List<String> equals = new ArrayList<>(List.of("bbb"));
+
+        //when (기능 작동)
+        winner.findWinnerName(cars);
+        List<String> result = winner.getWinnerName();
+
+        //then (기능 작동 후 결과)
+        Assertions.assertThat(result.size()).isEqualTo(1);
+        Assertions.assertThat(result).isEqualTo(equals);
+    }
+
+    @Test
+    public void 동일_우승자_자동차_이름() throws Exception{
+        //given (주어진 값)
+        List<String> equals = new ArrayList<>(List.of("bbb","ccc"));
+
+        //when (기능 작동)
+        winner.findWinnerName(cars);
+        List<String> result = winner.getWinnerName();
+
+        //then (기능 작동 후 결과)
+        Assertions.assertThat(result.size()).isEqualTo(2);
+        Assertions.assertThat(result).isEqualTo(equals);
+    }
+
+    @Test
+    public void 모두_우승자() throws Exception{
+        //given (주어진 값)
+        cars.get(0).moveForward(8);
+        cars.get(0).moveForward(7);
+        List<String> equals = new ArrayList<>(List.of("aaa","bbb","ccc"));
+
+        //when (기능 작동)
+        winner.findWinnerName(cars);
+        List<String> result = winner.getWinnerName();
+
+        //then (기능 작동 후 결과)
+        Assertions.assertThat(result.size()).isEqualTo(3);
+        Assertions.assertThat(result).isEqualTo(equals);
+    }
+
+    @Test
+    public void 플레이횟수가_없을경우() throws Exception{
+        //given (주어진 값)
+        cars = new ArrayList<>();
+        cars.add(new Car("aaa"));
+        cars.add(new Car("bbb"));
+        cars.add(new Car("ccc"));
+        cars.add(new Car("ddd"));
+
+        List<String> equals = new ArrayList<>(List.of("aaa","bbb","ccc","ddd"));
+        //when (기능 작동)
+        winner.findWinnerName(cars);
+        List<String> result = winner.getWinnerName();
+
+        //then (기능 작동 후 결과)
+        Assertions.assertThat(result.size()).isEqualTo(4);
+        Assertions.assertThat(result).isEqualTo(equals);
+    }
 }
