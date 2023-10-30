7d61caebff1a4f28c7137f9399949450909c60fa
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-10-30 20:17:55 +0900

    feat: 사용자 입력값에 따른 car 배열 생성 기능 추가 사용자 입력값을 파싱 후 car 배열 요소 추가

--- src/main/java/racingcar/domain/racing/CarManager.java
+++ src/main/java/racingcar/domain/racing/CarManager.java
@@ -1,4 +1,21 @@
 package racingcar.domain.racing;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.car.Car;
+
 public class CarManager {
+    private final List<Car> cars;
+
+    public CarManager(String carNames) {
+        this.cars = Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
