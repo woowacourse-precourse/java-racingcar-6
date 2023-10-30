67b07b91afa2799b76cad75878f69d0aa61fef77
recreate.kang@gmail.com <zetiburon666@gmail.com>
2023-10-30 19:13:28 +0900

    feat(Application) : system print feature added, Car_Names method Basic structure written

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 import java.util.*;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -9,20 +10,32 @@ public class Application {
     	Try_Numb();
     	//Run(Car_Names(),Try_Numb());
     }
+    	
     
+    //실행 문서//
     public static void Run(List<String> Car_Names, Integer Try_Numb) {
     	
     }
     
-    public static ArrayList<String> Car_Names() {
-    	ArrayList<String> Car_Names = new ArrayList<>();
-    	    	
+    //차량 이름//
+    public static String[] Car_Names() {
+    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은(,쉼표) 기준으로 구");
+    	String[] Car_Names = Console.readLine().split(",");
+    	Names_ErrCheck(Car_Names);
     	return Car_Names;
     }
+    public static void Names_ErrCheck(String[] Car_Names) {
+    	
+    }
     
+    
+    //시도 횟수//
     public static Integer Try_Numb() {
-    	Integer Try_numb = Randoms.pickNumberInRange(1, 9);
-    	System.out.print(Try_numb);
+    	System.out.println("시도할 회수는 몇회인가요?")
+    	//Integer Try_numb = Randoms.pickNumberInRange(1, 9);
+    	//System.out.print(Try_numb);
     	return Try_numb;
     }
+    
+    
 }
