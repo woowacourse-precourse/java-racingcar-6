d42df40b86895742e73aa0bc20791ada5eec552a
pjaehyun <show6756@gmail.com>
2023-10-30 17:54:41 +0900

    feat: 추가 moveCarsForward 함수 기능 테스트

--- src/test/java/racingcar/CarCollectionTest.java
+++ src/test/java/racingcar/CarCollectionTest.java
@@ -23,6 +23,27 @@ public class CarCollectionTest {
         assertThat(cars).contains(car2);
     }
 
+    @Test
+    void moveCarsForward_모든_자동차_앞으로_전진() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car1 = new Car("car1");
+                    Car car2 = new Car("car2");
+                    Car car3 = new Car("car3");
+
+                    CarCollection carCollection = createCarCollection(car1, car2, car3);
+
+                    carCollection.moveCarsForward();
+
+                    List<Car> cars = carCollection.getCars();
+                    for(Car car : cars) {
+                        assertThat(car.getDistance()).isEqualTo(1);
+                    }
+                },
+                4, 4, 4
+        );
+    }
+
     private CarCollection createCarCollection(Car... cars) {
         CarCollection carCollection = new CarCollection();
 
