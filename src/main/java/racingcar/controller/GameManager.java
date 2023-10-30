ea2b07033a50bbf40f8a72a899f2cf0f3f6059f0
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:30:27 +0900

    [feat] Add GameManager class playGame method

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -14,8 +14,28 @@ public class GameManager {
         userView = new UserView();
         user = new User();
     }
+    public void playGame() {
+        CarStorage.init();
 
+        // 사용자 입력 받기
+        userView.printInputNameMessage();
+        user.inputCarNames();
+        userView.printInputCountMessage();
+        tryCount = user.inputTryCount();
 
+        // 실행 결과 출력
+        System.out.println();
+        userView.printResultMessage();
+        for (int i=0; i<tryCount; i++) {
+            for (Car tempCar: CarStorage.cars()) {
+                Integer randInt = user.pickRandomInt();
+                tempCar.addStep(randInt);
+            }
+            userView.printCurrentResult();
+        }
+        choiceWinner();
+        userView.printWinners();
+    }
     private void choiceWinner() {
         Integer tempCarStep;
         Integer maxStep = CarStorage.getMaxSteps();
