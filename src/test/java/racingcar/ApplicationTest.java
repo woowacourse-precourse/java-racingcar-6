7fe2f3bf502a3f289feb32d60c3754d64ec36b4d
unknown <y2kf9a0q@gmail.com>
2023-10-30 16:37:50 +0900

    test: ApplicationTest.java - 시도회수_예외_처리_숫자가아닐때()

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -140,6 +140,12 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 시도회수_예외_처리_숫자가아닐때() {
+        String gameTurnsInput = "...";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("wtc,prcs", gameTurnsInput))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("Not valid input: game turn number"));
     }
 
     @Test
