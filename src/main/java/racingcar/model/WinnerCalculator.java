6f763e62bccc99eaeb28d0d46899bf402c5ab79c
Jiyoung Choi <12201817@inha.edu>
2023-10-30 19:52:43 +0900

    refactor(WinnerCalculator) : static 함수로 수정

--- src/main/java/racingcar/model/WinnerCalculator.java
+++ src/main/java/racingcar/model/WinnerCalculator.java
@@ -5,27 +5,27 @@ import java.util.StringJoiner;
 
 public class WinnerCalculator {
 
-    public String getWinners(List<Car> cars) {
+    public static String getWinners(List<Car> cars) {
         int maxMoveDistance = getMaxMoveDistance(cars);
         List<String> winnerList = findWinners(cars, maxMoveDistance);
         return convertListToString(winnerList);
     }
 
-    private int getMaxMoveDistance(List<Car> cars) {
+    private static int getMaxMoveDistance(List<Car> cars) {
         return cars.stream()
                 .mapToInt(Car::getMoveDistance)
                 .max()
                 .getAsInt();
     }
 
-    private List<String> findWinners(List<Car> cars, int maxMoveDistance) {
+    private static List<String> findWinners(List<Car> cars, int maxMoveDistance) {
         return cars.stream()
                 .filter(car -> car.getMoveDistance() == maxMoveDistance)
                 .map(Car::getName)
                 .toList();
     }
 
-    private String convertListToString(List<String> winnerList) {
+    private static String convertListToString(List<String> winnerList) {
         StringJoiner finalWinnersJoiner = new StringJoiner(", ");
         for (String winner : winnerList) {
             finalWinnersJoiner.add(winner);
