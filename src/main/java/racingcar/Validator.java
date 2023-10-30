4470f169ad69bad8109aed8207e2021ccd668f00
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-30 14:01:20 +0900

    feat: [Validator] 자동차 이름이 중복되면 예외

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
 
     public void validateInput(String input) {
@@ -8,6 +12,21 @@ public class Validator {
         }
         if (input.matches("^[가-힣a-zA-Z,]+$")) {
             throw new IllegalArgumentException("자동차 이름을 영문자,한글로만 구성하고 쉼표로만 구분해주세요.");
+        }
+    }
+
+    public void validateCarNames(List<String> carNames) {
+        checkCarNameDuplicated(carNames);
+    }
+
+    private void checkCarNameDuplicated(List<String> carNames) {
+        Set<String> duplicacySet = new HashSet<>();
+
+        for (String carName : carNames) {
+            duplicacySet.add(carName);
+        }
+        if (carNames.size() != duplicacySet.size()) {
+            throw new IllegalArgumentException("자동차 이름이 중복되면 우승자를 확인하는데 어려움이 있으니 중복 없이 입력하세요.");
         }
     }
 }
