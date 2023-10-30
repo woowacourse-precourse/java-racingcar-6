043f402225755ee2c3171c387eca27ddb5e31d13
고동환\eurdr <eurdream98@naver.com>
2023-10-30 18:21:50 +0900

    feat($Car) : Add makeRacingCarList method
    -Car클래스의 selectCars메소드를 이용하여 나온 cars를
    cars변수에 넣는다.
    -,를기준으로 자동차들을 종류별로 분류하여 리스트에 넣음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Car.makeRacingCarList();
+        Race.numberOfGameRound();
+
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,24 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
     public static String selectCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.");
         String cars = Console.readLine();
         return cars;
     }
-
+    public static List<String> makeRacingCarList(){
+        String cars = selectCars();
+        List<String> carList =new ArrayList<>();
+        String[] carArray = cars.split(",");
+        for(int i = 0 ; i< carArray.length;i++){
+            carList.add(carArray[i]);
+        }
+        System.out.println(carList);
+        return carList;
+    }
 
 }
