688fe89fecd413636766831179fe8fac4793542a
Arachneee <jhg2819@naver.com>
2023-10-30 16:02:39 +0900

    style: 에러 메시지 중복된 [ERROR] 문자 제거

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,9 +1,9 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-    NOT_INTEGER("[ERROR] 숫자만 입력가능합니다."),
-    INVALID_NAME_LENGTH("[ERROR] 잘못된 이름 길이입니다."),
-    CANT_FIND_MAX_VALUE("[ERROR] 최대 위치를 찾을 수 없습니다.");
+    NOT_INTEGER("숫자만 입력가능합니다."),
+    INVALID_NAME_LENGTH("잘못된 이름 길이입니다."),
+    CANT_FIND_MAX_VALUE("최대 위치를 찾을 수 없습니다.");
 
     private final String message;
 
@@ -12,7 +12,7 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return "[ERROR] " + message;
     }
 
 }
