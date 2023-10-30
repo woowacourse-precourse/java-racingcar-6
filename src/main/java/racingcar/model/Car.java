bed27cfd1ccbc834db3484f29e1f9a86195d275c
HoeSeong123 <ghltjd3@gmail.com>
2023-10-30 14:01:21 +0900

    feat(Car) : 자동차 전진하거나 멈추는 기능 구현
    주어진 횟수 동안 전진 또는 멈출 수 있다.
    0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 한 칸 전진한다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private Long movingCount;
@@ -7,5 +9,13 @@ public class Car {
     public Car(String name) {
         this.name = name;
         movingCount = 0L;
+    }
+
+    public void moveOrStay() {
+        int decisionNumber = Randoms.pickNumberInRange(0, 9);
+
+        if (decisionNumber >= 4) {
+            movingCount++;
+        }
     }
 }
