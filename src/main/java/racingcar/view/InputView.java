cc927aaecd304f5ef5da1b0803a6b647e0a4443f
Jaeho Kim <fldzm7905@gmail.com>
2023-10-31 02:03:11 +0900

    feat: 자동차 이름 쉼표로 구분해서 받기, 5자 이상 이름에 대한 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,24 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.validate.Validator;
+
 public class InputView {
 
     public int getNumberOfAttempts() {
         return 0;
     }
-    public String inputName() {
-        return null;
+    public List<String> inputName() {
+        List<String> carName = splitString(Console.readLine());
+        Validator.inputNameSize(carName);
+        return carName;
+    }
+
+    private List<String> splitString(String input) {
+        List<String> str = new ArrayList<>();
+        str = List.of(input.split(","));
+        return str;
     }
 }
