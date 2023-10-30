5dd0de9862769aeb638de39052dd27c0419d1ad7
unknown <y2kf9a0q@gmail.com>
2023-10-30 14:56:33 +0900

    test: ApplicationTest.java - 한명_단독우승();

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -11,21 +11,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+            INPUT_GAME_TURNS = "시도할 회수는 몇회인가요?",
+            RUN_RESULT = "실행 결과";
 
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 한명_단독우승() {
+        String carNameInput = "wtc";
+        String gameTurnInput = "3";
+        Integer[] randoms = {STOP, MOVING_FORWARD};
 
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(carNameInput, gameTurnInput);
+                    assertThat(output()).contains(INPUT_CAR_NAMES, INPUT_GAME_TURNS, RUN_RESULT,
+                            "wtc : -", "wtc : --", "최종 우승자 : wtc");
+                },
+                MOVING_FORWARD, randoms
+        );
     }
 
     @Test
