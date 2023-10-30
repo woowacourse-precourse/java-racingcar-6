bc162c605b1bb7d6c2cb40297ca0235510cfe97a
ahma0 <dsd932@naver.com>
2023-10-31 02:27:40 +0900

    test: 자동차 이름 글자 수 테스트

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -15,33 +15,48 @@ public class ValidatorTest extends NsTest {
 
     @Test
     @DisplayName("자동차 이름에 중복이 있으면 실패한다.")
-    void validateCarNamesTest() {
+    void validateCarNamesDuplicateTest() {
         //given
         List<String> carNames = new ArrayList<>(List.of("Test1", "Test1", "Test2"));
 
         //when
-
-        //then
         Throwable exception = assertThrows(RuntimeException.class, () -> {
             Validator.validateCarNames(carNames);
         });
+
+        //then
         assertEquals(ExceptionMessage.NOT_DUPLICATE_NAME, exception.getMessage());
 
     }
 
     @Test
     @DisplayName("자동차 이름에 빈 칸이 있을 경우 실패한다.")
-    void validateEmptyTest() {
+    void validateCarNamesEmptyTest() {
         //given
         List<String> carNames = new ArrayList<>(List.of("Test1", "", "Test3"));
 
         //when
-
-        //then
         Throwable exception = assertThrows(RuntimeException.class, () -> {
             Validator.validateCarNames(carNames);
         });
+
+        //then
         assertEquals(ExceptionMessage.NOT_EMPTY_AND_BLANK, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자를 초과해서는 안된다.")
+    void validateCarNamesGreaterThanFiveTest() {
+        //given
+        List<String> carNames = new ArrayList<>(List.of("TestTest1", "Test2", "Test3"));
+
+        //when
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            Validator.validateCarNames(carNames);
+        });
+
+        //then
+        assertEquals(ExceptionMessage.NOT_MORE_THAN_FIVE, exception.getMessage());
     }
 
     @Override
