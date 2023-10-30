555774b344cfd6a04a389e5c8279cc558e5e3565
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 07:40:44 +0900

    chore : 하드 코딩 되어 있는 것 수정 상수 이름 지정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,7 +4,7 @@ public class Car {
     private static final int CAN_GO_FORWARD_START = 4;
     private int forward;
     public int goForward(int random){
-        if(random >= 4){
+        if(random >= CAN_GO_FORWARD_START){
             forward++;
         }
         return this.forward;
