aaf46972599e083804436a2e2e083b467be1651c
honi20 <1594cyh@gmail.com>
2023-10-30 13:55:30 +0900

    feat: Car 클래스 세팅
    자동차 이름, 전진 횟수 정보 저장

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int movingCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.movingCount = 0;
+    }
+}
