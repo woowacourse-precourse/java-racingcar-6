8fc77ab964a6de5d4b4a4d3de659def373c2ab19
leorb159 <ldgsocial159@naver.com>
2023-10-31 01:29:45 +0900

    feat(Car): 자동차의 이름과 이동거리를 저장하는 클래스 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car(){
+        name ="";
+        distance = 0;
+    }
+    public Car(String name){
+        this.name = name;
+        distance = 0;
+    }
+
+
+}
