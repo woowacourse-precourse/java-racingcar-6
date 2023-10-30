9274d659172a21aef885e3269771d8eccef1927f
minseo  jo <liging12@naver.com>
2023-10-30 17:42:32 +0900

    refactor: RoundCountValidator 필요없는 코드 제거
    validateIntegerOverflowException 의 기능은
    validateNumericException 에서 Integer.parseInt(input); 가 대신 해준다.
    
    parseInt 는 들어온 문자열을 int 형으로 바꾸지 못한다면 NumberFormatException 예외를 뱉는다.
    NumberFormatException 은 IllegalArgumentException 을 상속 받는다.

--- src/main/java/racingcar/validator/RoundCountValidator.java
+++ src/main/java/racingcar/validator/RoundCountValidator.java
@@ -9,7 +9,6 @@ public class RoundCountValidator {
     private static final String ERROR_BLANK = ERROR + "공백을 허용하지 않습니다.";
     private static final String ERROR_NON_NUMERIC = ERROR + "숫자를 입력해 주세요." + "입력 값은 1부터 "  + ROUND_COUNT_MAX_INT_VALUE + "까지 입니다.";
     private static final String ERROR_NON_ZERO = ERROR + "1 이상인 양수를 입력해 주세요.";
-    private static final String ERROR_INT_OVERFLOW = ERROR + "입력 값은 최대 " + ROUND_COUNT_MAX_INT_VALUE + "입니다.";
 
     private RoundCountValidator() {}
 
@@ -17,7 +16,6 @@ public class RoundCountValidator {
         validateBlankException(input);
         validateNumericException(input);
         validatePositiveNumber(input);
-        validateIntegerOverflowException(input);
     }
 
     private static void validateBlankException(final String input) {
@@ -38,13 +36,6 @@ public class RoundCountValidator {
         int number = Integer.parseInt(input);
         if (number < ROUND_COUNT_MIN_INT_VALUE) {
             throw new IllegalArgumentException(ERROR_NON_ZERO);
-        }
-    }
-
-    private static void validateIntegerOverflowException(final String input) {
-        long number = Long.parseLong(input);
-        if (number > ROUND_COUNT_MAX_INT_VALUE || number < ROUND_COUNT_MIN_INT_VALUE) {
-            throw new IllegalArgumentException(ERROR_INT_OVERFLOW);
         }
     }
 }
