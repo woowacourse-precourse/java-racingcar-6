da3ddd45347514b7cde6563fb3efc285cf057cc8
wonindev <sangwonparkdev@gmail.com>
2023-10-30 18:26:37 +0900

    feat: 리스트로 나누기 전 input 스트링이 공백인 경우 예외 처리 추가

--- src/main/java/racingcar/ExceptionHandler.java
+++ src/main/java/racingcar/ExceptionHandler.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 public class ExceptionHandler {
 
-    public void isAttemptException(String input) {
-        if (!Validation.isNum(input)) {
+    public void isAttemptException(String attempt) {
+        if (!Validation.isNum(attempt)) {
             throw new IllegalArgumentException("시도 횟수가 숫자가 아닙니다.");
         }
 
-        if (Validation.isBlank(input)) {
+        if (Validation.isBlank(attempt)) {
             throw new IllegalArgumentException("시도 횟수가 공백입니다.");
         }
 
-        if (Validation.isNegative(Integer.parseInt(input))) {
+        if (Validation.isNegative(Integer.parseInt(attempt))) {
             throw new IllegalArgumentException("시도 횟수가 0보다 작습니다.");
+        }
+    }
+
+    public void isCarNamePlainTextException(String str) {
+        if (Validation.isBlank(str)) {
+            throw new IllegalArgumentException("자동차 이름이 공백입니다.");
         }
     }
 }
