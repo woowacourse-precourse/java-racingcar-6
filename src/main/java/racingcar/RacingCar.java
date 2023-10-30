ad0fa7b7badb7e6b35ef2863d4e8269a01f62bdd
kang yeon joo <rkdduswndi@naver.com>
2023-10-30 10:41:59 +0900

    feat: 사용자 입력 유효성 검사 메서드와 자동차 생성자 구현

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class RacingCar {
+    private String name;
+    public RacingCar(String input_name) {
+        if(!is_valid_input_name(input_name)){
+            new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+        this.name=input_name;
+    }
+
+    static boolean is_valid_input_name(String inputName) {
+        return 0<inputName.length() && inputName.length()<6;
+    }
+}
