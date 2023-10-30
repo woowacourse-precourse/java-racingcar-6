796f400ad03e13fbe488c51ae301f56c61c46b0f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 15:15:20 +0900

    feat : 움직이는 조건 생성
    - random number 가 4 이상이면 움직이기

---
+++ src/main/java/model/MoveRule.java
@@ -0,0 +1,16 @@
+package model;
+
+import util.RandomNumberGenerator;
+
+public class MoveRule {
+    private final int MOVE_CONDITION = 4;
+    private int randomNumber;
+
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    public boolean shouldMove() {
+        randomNumber = randomNumberGenerator.getRandomNumber();
+        return randomNumber >= MOVE_CONDITION;
+    }
+
+}
