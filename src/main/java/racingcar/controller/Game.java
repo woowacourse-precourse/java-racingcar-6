1e1d37e34347ac76287599f89b8aeef518e85563
lotuxsoo <tndus5383@gmail.com>
2023-10-30 14:46:23 +0900

    feat: Race 클래스에서 차들을 이동시킴

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
 import racingcar.model.Car;
 import racingcar.model.RandomNumber;
 import racingcar.view.InputView;
@@ -9,10 +11,19 @@ import racingcar.view.OutputView;
 public class Game {
     public void start() {
         OutputView.printStartMessage();
-        List<String> racingCarList = InputView.getRacingCars(); // [abc,def,ge]
+        List<String> stringCarList = InputView.getRacingCars(); // [abc,def,ge]
+        List<Car> racingCarList = new ArrayList<>();
+        for (String stringCar : stringCarList) {
+            Car car = new Car(stringCar, 0);
+            racingCarList.add(car);
+        }
 
+        Race race = new Race(racingCarList);
         OutputView.printTryMessage();
         int tryNumber = InputView.getTryNumber(); // 5
+        for (int i = 0; i < tryNumber; i++) {
+            race.run();
+        }
 
         OutputView.printShowResultString();
     }
---
+++ src/main/java/racingcar/controller/Race.java
@@ -0,0 +1,23 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.RandomNumber;
+
+public class Race {
+    private List<Car> racingCars;
+
+    public Race(List<Car> racingCarList) {
+        this.racingCars = racingCarList;
+    }
+
+    public void run() {
+        for (Car car : racingCars) {
+            RandomNumber number = RandomNumber.makeRandomNumber();
+            if (number.isMovable()) {
+                car.move();
+            }
+        }
+    }
+}
