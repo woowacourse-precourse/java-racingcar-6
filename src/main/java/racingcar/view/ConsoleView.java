99dfa0033824bc022ad4607afe39e5b570ff88cb
donghoonyeom <dua5410773@naver.com>
2023-10-31 00:30:18 +0900

    feat(view): printCurrentPositions() 메서드 추가
    다음과 같은 기능을 수행하는 printCurrentPositions() 메서드를 ConsoleView 클래스에 추가했습니다:
    - 주어진 자동차 리스트의 현재 위치를 콘솔에 출력합니다.
    - 각 자동차의 이름과 현재 위치를 '-' 문자로 나타내어 출력합니다.

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -1,9 +1,23 @@
 package racingcar.view;
 
+import racingcar.model.Car;
 
+import java.util.List;
 
 public class ConsoleView {
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printCurrentPositions(List<Car> cars) {
+        StringBuilder output = new StringBuilder();
+        for (Car car : cars) {
+            StringBuilder position = new StringBuilder();
+            for (int i = 0; i < car.getDistance(); i++) {
+                position.append("-");
+            }
+            output.append(car.getName()).append(" : ").append(position).append("\n");
+        }
+        printMessage(output.toString());
     }
 }
