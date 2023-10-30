a799346bd355624faddfe4deae345e06ee38f9d1
SiHyun-Lee113 <82255633+SiHyun-Lee113@users.noreply.github.com>
2023-10-30 17:52:37 +0900

    feat : 입력 메서드 생성
    - 기능 목록 1.1 자동차 이름 입력 구현
      - 입력 검증 메서드 추가
      - List<Car> : 자동차 관리용 리스트
    - 기능 목록 1.2 이동 시도 횟수 입력 구현
      - 입력 검증 메서드 추가

---
+++ src/main/java/racingcar/IO/Input.java
@@ -0,0 +1,34 @@
+package racingcar.IO;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.Model.Car;
+import racingcar.Util.ErrorCollection;
+import racingcar.Util.Validation;
+
+public class Input {
+    public static List<Car> getCars() {
+        String input = Validation.validationEmpty(Console.readLine());
+
+        input = input.replace(" ", "");
+        String[] names = input.split(",");
+
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : names) {
+            String verifiedName = Validation.validationCarName(name);
+            Car car = new Car(verifiedName);
+            cars.add(car);
+        }
+
+        return cars;
+    }
+
+    public static int getAttempts() {
+        String input = Validation.validationEmpty(Console.readLine());
+
+        return Validation.validationAttempts(input);
+    }
+}
