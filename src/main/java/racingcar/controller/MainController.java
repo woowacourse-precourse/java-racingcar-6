40261b16157ca2bbb475fdb74b6a6543902d94a3
IM-GYURI <kgulr0517@gmail.com>
2023-10-30 22:15:58 +0900

    Feat: 추가 MoveCountValidator 클래스

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -1,6 +1,12 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class MainController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Map
 
     public void play() {
 
