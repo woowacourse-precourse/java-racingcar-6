6a18ce29784db347c34244d4770b0c884b380ad2
Yong Ta Park <qkrdydxk80@nate.com>
2023-10-30 23:52:49 +0900

    test : InputGenerateManagerImplTest
    테스트 코드 작성

--- src/test/java/racingcar/game/inputgenerateManager/InputGenerateManagerImplTest.java
+++ src/test/java/racingcar/game/inputgenerateManager/InputGenerateManagerImplTest.java
@@ -14,6 +14,7 @@ class InputGenerateManagerImplTest {
     private final String STRING_NUMBER = "999";
     private final int PURE_NUMBER = 999;
     private final String STRING_NAME = "한놈,두식이,석삼";
+    private final String[] NAME_LIST = {"한놈","두식이","석삼"};
 
     @RepeatedTest(10)
     void 난수_범위() {
@@ -37,6 +38,6 @@ class InputGenerateManagerImplTest {
         List<String> nameList = inputGenerateManager.generateInputStringSplitWithComma(STRING_NAME);
 
         Assertions.assertThat(nameList.size()).isEqualTo(3);
-        Assertions.assertThat(nameList).contains("한놈","두식이","석삼");
+        Assertions.assertThat(nameList).contains(NAME_LIST[0], NAME_LIST[1], NAME_LIST[2]);
     }
 }
