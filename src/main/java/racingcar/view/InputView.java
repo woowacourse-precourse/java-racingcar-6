bd565e2a179fb580f5802afd0732089e04d869b9
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-30 17:28:53 +0900

    style(InputView): 컨벤션 정리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,13 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-    private InputView() {
-    }
-
     private static final String INPUT_CAR_NAME_MESSAGE = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUND_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
+    private InputView() {
+    }
 
     public static String carNamesInput() {
         return input(INPUT_CAR_NAME_MESSAGE);
