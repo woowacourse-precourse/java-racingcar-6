a1041ace8e82a4947027485743d04f64f9f5ccc2
dltjdn <hana40004@naver.com>
2023-10-31 02:41:05 +0900

    feat(InputView): 중복된 자동차 이름 검증 기능

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,10 +1,11 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-    NAME_LENGTH_ERROR ("이름은 5자 이하여야 합니다."),
-    TRY_COUNT_INPUT_NON_NEGATIVE_ERROR ("시도 횟수는 0 이상이어야 합니다."),
-    INVALID_INPUT_ERROR ("잘못된 입력입니다. 유효한 숫자를 입력해주세요."),
-    CAR_NAME_INPUT_SIZE_ERROR ("적어도 한 개 이상의 자동차 이름을 입력해주세요.");
+    NAME_LENGTH_ERROR("이름은 5자 이하여야 합니다."),
+    TRY_COUNT_INPUT_NON_NEGATIVE_ERROR("시도 횟수는 0 이상이어야 합니다."),
+    INVALID_INPUT_ERROR("잘못된 입력입니다. 유효한 숫자를 입력해주세요."),
+    CAR_NAME_INPUT_SIZE_ERROR("적어도 한 개 이상의 자동차 이름을 입력해주세요."),
+    CAR_NAME_DUPLICATE_ERROR("중복된 차 이름이 있습니다. 각 차 이름은 고유해야 합니다.");
 
     public final String message;
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,7 +2,9 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.exception.ErrorMessage;
 
 public class InputView {
@@ -10,44 +12,55 @@ public class InputView {
     private static final String TRY_COUNT_INPUT_PROMPT = "시도할 회수는 몇회인가요?";
 
 
-    public List<String> readCarNames(){
+    public List<String> readCarNames() {
         System.out.println(CAR_NAME_INPUT_PROMPT);
         String input = Console.readLine();
-        List<String> names = Arrays.asList(input.split(","));
+        List<String> names = splitAndTrimInput(input);
+
         validateNotEmpty(names);
-        return trimString(names);
+        validateDuplicate(names);
+        return names;
     }
 
-    private List<String> trimString(List<String> names) {
-        return names.stream()
+    private List<String> splitAndTrimInput(String input) {
+        return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .toList();
     }
 
+    private void validateDuplicate(List<String> names) {
+        Set<String> uniqueNames = new HashSet<>(names);
+
+        if (names.size() != uniqueNames.size()) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATE_ERROR.getMessage());
+        }
+    }
+
+
     private void validateNotEmpty(List<String> names) {
-        if(names.isEmpty()){
+        if (names.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_INPUT_SIZE_ERROR.getMessage());
         }
     }
 
-    public int readTryCount(){
+    public int readTryCount() {
         System.out.println(TRY_COUNT_INPUT_PROMPT);
         int tryCount = readLineAndParseInt();
         validateNonNegative(tryCount);
         return tryCount;
     }
 
-    private int readLineAndParseInt(){
-        try{
+    private int readLineAndParseInt() {
+        try {
             String input = Console.readLine().trim();
             return Integer.parseInt(input);
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_ERROR.getMessage());
         }
     }
 
     private void validateNonNegative(int tryCount) {
-        if(tryCount < 0){
+        if (tryCount < 0) {
             throw new IllegalArgumentException(ErrorMessage.TRY_COUNT_INPUT_NON_NEGATIVE_ERROR.getMessage());
         }
     }
