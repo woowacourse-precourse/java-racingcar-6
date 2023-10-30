99854cf7a07bee7690e5a393cec6f3300a57cec2
Han <yulmam1@naver.com>
2023-10-30 13:51:15 +0900

    feat(Cars) : 이름이 같을 경우 예외처리하는 기능 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,9 +1,6 @@
 package racingcar.model;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Cars {
@@ -26,6 +23,7 @@ public class Cars {
     private void validate(List<String> carsName) {
         validateCarsNameLength(carsName);
         validateCarsNameNotNull(carsName);
+        validateCarsNameNotDuplication(carsName);
     }
 
     private void validateCarsNameLength(List<String> carsName) {
@@ -36,5 +34,13 @@ public class Cars {
     private void validateCarsNameNotNull(List<String> carsName) {
         if(carsName.stream().anyMatch(carName -> carName == null))
             throw new IllegalArgumentException("자동차의 이름이 null 입니다.");
+    }
+
+    private void validateCarsNameNotDuplication(List<String> carsName) {
+        HashSet<String> Set = new HashSet<>(carsName);
+
+        if (Set.size() < carsName.size()) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름이 중복됐습니다.");
+        }
     }
 }
