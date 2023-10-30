df85b6c1e200d4e2f578cd55cf2b6e4e959f8a29
Elliot-KJW <eidechse4@hanmail.net>
2023-10-30 19:46:29 +0900

    Feat(Game.java): Add makeCarList method and Test method
    add src/test/java/racingcar/GameTest.java and makeCarListTest method

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,21 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import validation.StringValidator;
+
 public class Game {
+  public static List<Car> makeCarList(String nameList) {
+    List<Car> carList = new ArrayList<Car>();
+    StringTokenizer nameTokenizer = new StringTokenizer(nameList, ",");
+
+    while(nameTokenizer.hasMoreTokens()) {
+      String carName = StringValidator.validateStringLengthOver(nameTokenizer.nextToken(), 5);
+      Car carListElement = new Car(carName);
+      carList.add(carListElement);
+    }
+
+    return carList;
+  }
 }
---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+  @Test
+  void makeCarList_입력받은_값을_리스트로_반환() {
+    List<Car> resultList = Game.makeCarList("pobi,woni,jun");
+
+    assertThat(resultList).extracting("name")
+                            .contains("pobi", "woni", "jun");
+  }
+}
