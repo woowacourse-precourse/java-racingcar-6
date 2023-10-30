d6fef477823b506a510050ae880f2a17a925213d
pjaehyun <show6756@gmail.com>
2023-10-30 15:37:37 +0900

    feat: 추가 validateInputLengthExceeded 함수 기능 테스트

--- src/test/java/racingcar/ValidateTest.java
+++ src/test/java/racingcar/ValidateTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static validate.InputValidation.validateNumericInput;
+import static validate.InputValidation.validateInputLengthExceeded;
 
 public class ValidateTest {
 
@@ -14,5 +15,14 @@ public class ValidateTest {
         assertThatThrownBy(() -> validateNumericInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력된 값에 숫자가 아닌 값이 포함되어있습니다.");
+     }
+
+     @Test
+    public void validateInputLengthExceeded_입력된_자동차의_이름이_5글자를_초과했을때_예외_발생() {
+        String name = "abcdef";
+
+        assertThatThrownBy(() -> validateInputLengthExceeded(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름은 5글자를 초과할 수 없습니다.");
      }
 }
