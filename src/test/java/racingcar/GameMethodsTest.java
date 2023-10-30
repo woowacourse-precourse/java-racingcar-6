6caf4103ede70ecea7469d20c80a45d4eea8f622
Jihye Lee <zerom980706@gmail.com>
2023-10-30 17:44:54 +0900

    test(gameMethods): make a test for random number making method

--- src/test/java/racingcar/GameMethodsTest.java
+++ src/test/java/racingcar/GameMethodsTest.java
@@ -1,5 +1,19 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.GameMethods.makeRandNum;
+
+import org.junit.jupiter.api.Test;
+
 class GameMethodsTest {
 
+    @Test
+    void makeRandNumTest() {
+        int numberOfCars = 3;
+        int[] howFar = makeRandNum(3);
+
+        for (int n : howFar) {
+            assertThat(n).isBetween(0, 9);
+        }
+    }
 }
