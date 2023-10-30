123ea8e28fe6e2942ab45ed806d407b39f3672a3
2021110806 <2021110806@dgu.ac.kr>
2023-10-30 16:59:12 +0900

    test(OutputTest.java): add printRacingMapTest, printResultTitleTest 시도마다의 레이싱 맵 결과를 출력하는 기능에 대한 테스트 코드를 구현하였습니다. 테스트 코드 실행 환경 차이에서의 오류 방지를 위해 "\r\n"을 System.lineSeparator 로 변경하였습니다.

--- src/test/java/racingcar/OutputTest.java
+++ src/test/java/racingcar/OutputTest.java
@@ -3,9 +3,9 @@ package racingcar;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedHashMap;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static racingcar.Output.*;
@@ -13,6 +13,8 @@ import static racingcar.Output.*;
 public class OutputTest {
 
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    String lineSeparator = System.lineSeparator();
 
     @BeforeEach
     public void setUpStreams() {
@@ -27,12 +29,27 @@ public class OutputTest {
     @Test
      void printCarNameInputRequestTest() {
         printCarNameInputRequest();
-        assertThat(output.toString()).isEqualTo(CAR_NAME_INPUT_REQUEST_MESSAGE+"\r\n");
+        assertThat(output.toString()).isEqualTo(CAR_NAME_INPUT_REQUEST_MESSAGE + lineSeparator);
     }
 
     @Test
     void printTryNumberInputRequestTest() {
         printTryNumberInputRequest();
-        assertThat(output.toString()).isEqualTo(TRY_NUMBER_INPUT_REQUEST_MESSAGE+"\r\n");
+        assertThat(output.toString()).isEqualTo(TRY_NUMBER_INPUT_REQUEST_MESSAGE + lineSeparator);
+    }
+    @Test
+    void printResultTitleTest() {
+        printResultTitle();
+        assertThat(output.toString()).isEqualTo(RESULT_TITLE + lineSeparator);
+    }
+    @Test
+    void printRacingMapTest() {
+        LinkedHashMap racingMap = new LinkedHashMap<String, Integer>();
+        racingMap.put("car1", 3);
+        racingMap.put("car2", 5);
+        racingMap.put("car3", 1);
+        printRacingMap(racingMap);
+        String answer = "car1 : ---" + lineSeparator + "car2 : -----" + lineSeparator + "car3 : -" + lineSeparator + lineSeparator;
+        assertThat(output.toString()).isEqualTo((answer));
     }
 }
