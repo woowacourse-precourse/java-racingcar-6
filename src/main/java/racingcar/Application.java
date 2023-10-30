2cd2eee49a8fd69a93cade00cb3adbf36f1649be
juns1s <juns1s@naver.com>
2023-10-30 14:08:30 +0900

    feat: 전체 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = RacingUtil.getCarsList();
+        int times = RacingUtil.getTimes();
+
+        RacingUtil.raceStart(cars, times);
+
+        List<String> winner = RacingJudge.raceJudge(cars);
+        System.out.println("최종 우승자 : " + String.join(", ", winner));
     }
 }
