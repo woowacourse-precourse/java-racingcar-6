90e0d68cfb2536159758bd53076eab1f4bcd8cc4
happy-nana <nxxxgnim@gmail.com>
2023-10-31 03:17:42 +0900

    feat: Car.java 기능 구현 RacingGame 클래스에 다 넣으려 한 기능들을 따로 Car 클래스에 분리하였음 메서드 기능 절차 : InputSystem에서 받을 이름, 위치 초기화 -> 이름, 위치 리턴받기 -> 전진하기 -> 각 회차 실행 결과 표시 -> 전진 조건 확인

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,47 @@
 package racingcar;
 
 public class Car {
+    private static final String STATUS_BAR = "-";
+    private static final int START_POSITION = 0;
+    private static final int STANDARD_NUM = 3;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this(name, START_POSITION);
+    }
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moving(int random) {
+        if (isBiggerOrEqualStandard(random)) {
+            position++;
+        }
+    }
+
+    public String displayMoving() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" : ");
+
+        for (int i=0; i<position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    private boolean isBiggerOrEqualStandard(int num) {
+        return STANDARD_NUM <= num;
+    }
 }
