f494248eadee51dd6342dee52597495f23a52513
dealim <136411152+dealim@users.noreply.github.com>
2023-10-30 16:06:45 +0900

    [update] skeleton classes

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+public class Car {
+    private Integer cuurentLocation;
+    private String name;
+
+    public Car() {
+
+    }
+
+    public void movingForward(int times) {
+    }
+
+    public boolean judgeForward(int number) {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void printName() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Game {
+    public void start() {
+
+    }
+
+    public void userInput() {
+
+    }
+
+    public void printResult() {
+
+    }
+}
