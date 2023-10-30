64989d037189374df5d37e91da31d06c0033d0ae
JiEung2 <https://github.com/JiEung2>
2023-10-30 23:54:15 +0900

    Feat: 입력된 자동차 이름의 수만큼 차를 만들어주는 기능 구현
    게임 컨트롤러에서 이름만큼 자동차를 생성하게되면 역할이 너무 많아지기 때문에 해당 기능을 자동차 공장이라는 클래스를 만들어 따로 구현

---
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+    public static List<Car> createCars(List<String> names) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+}
