8740bef14da4c93aaf74436f0469aae38c95e70d
miro-22 <rlawlgus96@snu.ac.kr>
2023-10-30 23:55:07 +0900

    feat: receive the user's input for the car name

--- src/main/java/racingcar/IOHandler.java
+++ src/main/java/racingcar/IOHandler.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class IOHandler {
     public void printComment(String comment) {
         System.out.println(comment);
+    }
+
+    public String readConsoleInput() {
+        return Console.readLine();
     }
 }
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,8 +1,25 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Racing {
+    List<RacingCar> racingCars = new ArrayList<>();
+    IOHandler ioHandler = new IOHandler();
+
     public void run() {
-        IOHandler ioHandler = new IOHandler();
         ioHandler.printComment("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        generateRacingCar();
+    }
+
+
+    private void generateRacingCar() {
+        String cars = ioHandler.readConsoleInput();
+        List<String> carsList = Arrays.asList(cars.split(","));
+        for (String car : carsList) {
+            racingCars.add(new RacingCar(car));
+        }
     }
 }
