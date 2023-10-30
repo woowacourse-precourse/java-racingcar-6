76736912871e35e6f7f6f68fdfae95456660e97c
kunsanglee <gosmdochee2@gmail.com>
2023-10-30 13:37:02 +0900

    feat(RacingCarInputValidator): 사용자에게 입력받은 자동차의 이름 중복 검증 기능 구현

--- src/main/java/racingcar/view/RacingCarInputValidator.java
+++ src/main/java/racingcar/view/RacingCarInputValidator.java
@@ -6,16 +6,27 @@ public class RacingCarInputValidator {
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_ATTEMPT_NUMBER = 1;
     private static final int MAX_ATTEMPT_NUMBER = 200_000_000;
-    private static final String MAX_CAR_NAME_EXCEPTION = "자동차 이름은 최대 5자 이하만 가능합니다.";
     private static final String AVAILABLE_CHARACTER_REGEX = "[a-zA-Z0-9가-힣]*";
+    private static final String DUPLICATED_CAR_NAMES_EXCEPTION = "각 자동차의 이름은 고유해야 합니다.";
+    private static final String MAX_CAR_NAME_EXCEPTION = "자동차 이름은 최대 5자 이하만 가능합니다.";
     private static final String AVAILABLE_CHARACTER_EXCEPTION = "자동차 이름은 한글, 영어, 숫자만 가능합니다.";
     private static final String ATTEMPT_NOT_POSITIVE_NUMBER_EXCEPTION = "시도 횟수는 1 이상 2억 이하의 숫자만 입력 가능합니다.";
     private static final String ATTEMPT_NOT_NUMBER_EXCEPTION = "입력값이 숫자가 아닙니다.";
 
     public void validateCarNames(List<String> carNames) {
+        validateDuplicated(carNames);
         for (String carName : carNames) {
             validateLength(carName);
             validateCharacter(carName);
+        }
+    }
+
+    private void validateDuplicated(List<String> carNames) {
+        int distinctCount = (int) carNames.stream()
+                .distinct()
+                .count();
+        if (distinctCount != carNames.size()) {
+            throw new IllegalArgumentException(DUPLICATED_CAR_NAMES_EXCEPTION);
         }
     }
 
