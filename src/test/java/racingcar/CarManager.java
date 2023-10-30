b92a4b2ac89e45000b0cdfd556abc418ae9291ef
Heojamong <wlaud0823@naver.com>
2023-10-30 18:14:55 +0900

    feat: 자동차 이름 입력 받는 메소드 구현 자동차 이름을 Console.readLine 메소드를 통해 입력받은 후 split(",")을 사용하여 String[] carArray에 저장한다.

--- src/test/java/racingcar/CarManager.java
+++ src/test/java/racingcar/CarManager.java
@@ -3,9 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarManager {
-
+    String[] carArray;
     public void printCarNamePrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void inputForCarName() {
+        String carStr = Console.readLine();
+        carArray = carStr.split(",");
     }
 
 }
