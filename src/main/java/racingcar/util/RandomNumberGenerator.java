a34e9bd3ceb7b9d95873eccd0962e0c4cc7d01ae
IM-GYURI <kgulr0517@gmail.com>
2023-10-30 20:18:30 +0900

    Feat: 추가 RandomNumberGenerator 클래스

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    public static int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
