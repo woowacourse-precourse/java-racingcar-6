c95c9da1ec8f3b750d894f58a3d3c346ce246f2e
inpink <inpink@kakao.com>
2023-10-30 21:26:16 +0900

    refactor: Racing class에 Builder 패턴 적용
    테스트 통과.
    save는 MemoryRepository에 구현되어있음.

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,9 +1,54 @@
 package racingcar.domain;
 
 public class Racing extends IndexModel {
-
     private Participations participations;
     private int tryCount;
     private Winners winners;
 
+    public Participations getParticipations() {
+        return participations;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public Winners getWinners() {
+        return winners;
+    }
+
+    private Racing(Builder builder) {
+        this.participations = builder.participations;
+        this.tryCount = builder.tryCount;
+        this.winners = builder.winners;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Participations participations;
+        private int tryCount;
+        private Winners winners;
+
+        public Builder participations(Participations participations) {
+            this.participations = participations;
+            return this;
+        }
+
+        public Builder tryCount(int tryCount) {
+            this.tryCount = tryCount;
+            return this;
+        }
+
+        public Builder winners(Winners winners) {
+            this.winners = winners;
+            return this;
+        }
+
+        public Racing build() {
+            return new Racing(this);
+        }
+    }
 }
