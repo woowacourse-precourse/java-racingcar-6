d370e9c6d8d3b50cf0631d0f1c201d9dc4be4e5f
jum0624 <jum0624@naver.com>
2023-10-30 21:39:06 +0900

    feat: 자동차 움직이기 코드 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,2 +1,44 @@
-package racingcar.domain;public class Cars {
+package racingcar.domain;
+
+import racingcar.util.RandomUtil;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int getSize() {
+        return cars.size();
+    }
+
+    public void moveCars() {
+        List<Car> carList = this.getCars();
+        carList.forEach(car -> {
+            int random = RandomUtil.createRandom();
+            if (isMove(random)) {
+                car.accountDistance();
+            }
+        });
+    }
+
+    public void printMoves() {
+        for (Car car : cars) {
+            car.printMove();
+            System.out.println();
+        }
+    }
+    public boolean isMove(int random) {
+        if (random < 4) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,4 +1,45 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.util.InputUtil;
+import racingcar.util.RandomUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarService {
+    private final InputUtil inputUtil;
+    private final RandomUtil randomUtil;
+    private Cars cars;
+
+    public RacingCarService() {
+        inputUtil = new InputUtil();
+        randomUtil = new RandomUtil();
+    }
+
+    public void start() {
+        initCars();
+        int count = inputUtil.getCount();
+        for (int i = 0; i < count; i++) {
+            move(cars);
+        }
+    }
+
+    public void move(Cars cars) {
+        cars.moveCars();
+        cars.printMoves();
+    }
+
+    public void initCars() {
+        List<String> input = inputUtil.getCarNames();
+        cars = createCars(input);
+    }
+
+    public Cars createCars(List<String> names) {
+        List<Car> carList = new ArrayList<>();
+        names.forEach(name -> carList.add(new Car(name)));
+        cars = new Cars(carList);
+        return cars;
+    }
 }
