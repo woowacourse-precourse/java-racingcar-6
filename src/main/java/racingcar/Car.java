fdd7d5d783c1e91800e5c916078d2792e5b93ec2
Elliot-KJW <eidechse4@hanmail.net>
2023-10-30 21:59:45 +0900

    Style: Apply Java Code Convention

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,33 +1,38 @@
 package racingcar;
 
 public class Car {
-  private Car() {}
+
+  private String name;
+  private int forwardDistance;
+
+  private Car() {
+  }
 
   public Car(String name) {
     this.name = name;
     this.forwardDistance = 0;
   }
 
-  private String name;
-  private int forwardDistance;
-
   public String getName() {
     return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public int getForwardDistance() {
     return forwardDistance;
   }
 
-  public void setForwardDistance(int forwardDistance) {
-    this.forwardDistance = forwardDistance;
-  }
-
   public void addForwardDistance() {
     this.forwardDistance++;
+  }
+
+  public String getStatus() {
+    StringBuilder status = new StringBuilder();
+    status.append(this.getName()).append(" : ");
+
+    for (int i = 0; i < this.forwardDistance; i++) {
+      status.append("-");
+    }
+
+    return status.toString();
   }
 }
