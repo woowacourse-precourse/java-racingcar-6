c29d73498529b6763fadaac074e093d2555e47a2
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-10-30 23:57:34 +0900

    refactor: CarGameRound 객체를 record class로 수정

--- src/main/java/racingcar/domain/CarGameRound.java
+++ src/main/java/racingcar/domain/CarGameRound.java
@@ -1,15 +1,8 @@
 package racingcar.domain;
 
-import java.util.Objects;
-
-public class CarGameRound {
+public record CarGameRound(int gameRound) {
+    
     private static final String GAMR_ROUND_MUST_DIGIT = "시도할 횟수는 숫자이어야 합니다.";
-
-    private final int gameRound;
-
-    private CarGameRound(int gameRound) {
-        this.gameRound = gameRound;
-    }
 
     public static CarGameRound createByString(String gameRound) {
         try {
@@ -21,18 +14,5 @@ public class CarGameRound {
 
     public boolean isRunning(int currentRound) {
         return gameRound > currentRound;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarGameRound that = (CarGameRound) o;
-        return gameRound == that.gameRound;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(gameRound);
     }
 }
