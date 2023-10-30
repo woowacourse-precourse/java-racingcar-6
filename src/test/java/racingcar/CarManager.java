f35114049c0b7d4f6bf7312b96e4102ed36fc058
Heojamong <wlaud0823@naver.com>
2023-10-30 18:41:43 +0900

    feat: 자동차 이동 횟수가 유효한지 확인하는 메소드 구현 조건문을 통해 입력 받은 값이 0보다 작거나 같을 경 throw new IllegalArgumentException을 발생시킨다.

--- src/test/java/racingcar/CarManager.java
+++ src/test/java/racingcar/CarManager.java
@@ -4,24 +4,30 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CarManager {
     String[] carArray;
+    int moveCount;
     int carMoveCount;
     public void printCarNamePrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         inputForCarName();
     }
 
-    public void inputForCarName() {
+    public String[] inputForCarName() {
         String carStr = Console.readLine();
         carArray = carStr.split(",");
         carNameValidationCheck(carArray);
+        return carArray;
     }
 
     public void printCarMovePrompt() {
         System.out.println("시도할 횟수는 몇회인가요?");
+        inputForCarMove();
     }
 
-    public void inputForCarMove() {
+    public int inputForCarMove() {
         String carMove = Console.readLine();
+        carMoveValidationCheck(carMove);
+        moveCount = Integer.parseInt(carMove);
+        return moveCount;
     }
 
     public void carNameValidationCheck(String[] carArray) {
@@ -32,6 +38,13 @@ public class CarManager {
             if (car.length() > 5) {
                 throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
             }
+        }
+    }
+
+    public void carMoveValidationCheck(String carMove) {
+        int number = Integer.parseInt(carMove);
+        if (number <= 0) {
+            throw new IllegalArgumentException("입력하신 값이 0보다 작거나 같습니다.");
         }
     }
 }
