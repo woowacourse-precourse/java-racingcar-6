421001599682d35801040156ddf67faad88ec179
hunko <rhwlgns4386@naver.com>
2023-10-30 18:20:05 +0900

    feat(RacingGameRequest) : controler에서 RacingGame 으로 가는 dto작성

---
+++ src/main/java/racingcar/dto/RacingGameRequest.java
@@ -0,0 +1,47 @@
+package racingcar.dto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.RandomNumberGenerator;
+
+public class RacingGameRequest {
+
+    private final List<Car> cars;
+    private final int count;
+
+
+    public RacingGameRequest(String names, String count) {
+        validateCount(count);
+        this.cars= toList(names);
+        this.count= toInt(count);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    private static int toInt(String count) {
+        return Integer.parseInt(count);
+    }
+
+    private static List<Car> toList(String names) {
+        return Arrays.stream(names.split(","))
+                .map(name -> name.replace(" ", ""))
+                .map(name -> new Car(name, new RandomNumberGenerator()))
+                .collect(Collectors.toList());
+    }
+
+    private void validateCount(String count) {
+        try {
+            Integer.parseInt(count);
+        }catch (NumberFormatException ex){
+            throw new IllegalArgumentException("숫자로 입력해주세요");
+        }
+    }
+}
