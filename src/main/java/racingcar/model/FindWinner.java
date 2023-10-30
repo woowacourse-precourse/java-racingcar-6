1ad24f3adce2f2486d749314b8183830a914bee0
chaniii999 <psc0729@naver.com>
2023-10-30 21:59:38 +0900

    feat: 우승자 검색 기능 구현

---
+++ src/main/java/racingcar/model/FindWinner.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+
+public class FindWinner {
+
+    private StringBuilder winnerList = new StringBuilder();
+
+    public FindWinner(ArrayList<Car> classList) {
+        int threshold = -1;
+        for (Car car : classList) {
+            int position = car.getPosition();
+            if (position > threshold) {
+                winnerList.setLength(0); // 클리어
+                winnerList.append(car.getName());
+                threshold = position;
+            } else if (position == threshold) {
+                winnerList.append(", ").append(car.getName());
+            }
+        }
+    }
+
+    public StringBuilder getWinner() {
+        return winnerList;
+    }
+
+}
