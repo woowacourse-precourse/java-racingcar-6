37adba69542ac6aa5c51ff03dd401e834ea2a3bf
icebear0111 <wltjq0901@naver.com>
2023-10-31 01:30:50 +0900

    feat: method to print result for each move

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -7,7 +7,10 @@ public class GameManager {
         return Randoms.pickNumberInRange(0, 9);
     }
 
-    public void printResult() {
-
+    public void printResult(String[] cars, int[] distance) {
+        System.out.println("실행 결과");
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i] + " : " + distance[i]);
+        }
     }
 }
