fd22cf31e83278e5685273ba7a309f2bcf15266c
pjaehyun <show6756@gmail.com>
2023-10-30 20:18:35 +0900

    feat: validateListIsEmpty에 관한 예외 처리 메세지 상수 추가

--- src/test/java/racingcar/RacingCarTestConstants.java
+++ src/test/java/racingcar/RacingCarTestConstants.java
@@ -11,4 +11,6 @@ public class RacingCarTestConstants {
     public static final String INPUT_IS_EMPTY_ERROR_MESSAGE = "자동차의 이름은 공백일 수 없습니다.";
 
     public static final String INPUT_DOES_NOT_CONTAIN_SPECIAL_CHARACTERS_ERROR_MESSAGE = "자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.";
+
+    public static final String LIST_IS_EMPTY_ERROR_MESSAGE = "입력된 자동차가 없습니다.";
 }
