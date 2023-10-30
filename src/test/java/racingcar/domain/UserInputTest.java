70b9250dc2064743c7e5d93f4a3ca71f082c7b06
HYOAN <aspqll@naver.com>
2023-10-30 19:22:59 +0900

    feat(racingcar): 사용자 입력 기능 테스트 코드 구현

--- src/test/java/racingcar/domain/UserInputTest.java
+++ src/test/java/racingcar/domain/UserInputTest.java
@@ -1,4 +1,25 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserInputTest {
-  
+
+    @Test
+    void carName() {
+        String input = "pobi,woni";
+        assertThat(input).isEqualTo("pobi,woni");
+    }
+
+    @Test
+    void attemptCount() {
+        int input = 3;
+        assertThat(input).isEqualTo(input);
+    }
+
+    @Test
+    void processException() {
+    }
 }
