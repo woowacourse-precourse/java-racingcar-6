d78cd201ee43f079f48b4b07b4545423373d7914
devhoya97 <diajbh123@gmail.com>
2023-10-30 11:39:38 +0900

    test(racingcar) : 테스트 범위 외의 객체들에 의존하지 않도록 테스트코드 수정

--- src/test/java/racingcar/OutputViewTest.java
+++ src/test/java/racingcar/OutputViewTest.java
@@ -1,55 +1,41 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
 import racingcar.domain.OutputView;
-import racingcar.domain.Referee;
 
 class OutputViewTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-
-    public static void main(String[] args) {
-        //given
-        Cars cars = new Cars(Arrays.asList(new Car("pobi"), new Car("woni")));
-        //when
-        for (int index = 0; index < 2; index++) {
-            cars.move();
-            OutputView.printCarsLocation(cars);
-        }
-        //then
-    }
 
     @Test
-    void printResult_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    main(new String[]{});
-                    assertThat(output()).contains("pobi : -", "woni : ", "pobi : --", "woni : -"); // output() 메서드의 trim() 적용됨을 유의
-                },
-                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD
-        );
+    void printCarsLocation_테스트() {
+        // given
+        Map<String, Integer> moveCountMap = new LinkedHashMap<>();
+        moveCountMap.put("pobi", 2);
+        moveCountMap.put("woni", 1);
+        // when
+        OutputView.printCarsLocation(moveCountMap);
+        // then
+        assertThat(output()).contains("pobi : --", "woni : -");
     }
 
     @Test
     void printWinner() {
         //given
-        Cars cars = new Cars(Arrays.asList(new Car("pobi"), new Car("jun")));
-        Referee referee = new Referee(cars.getMoveCountMap());
+        List<String> winnerList = Arrays.asList("pobi", "jun");
         //when
-        OutputView.printWinner(referee.getWinnerList());
+        OutputView.printWinner(winnerList);
         //then
         assertThat(output()).contains("최종 우승자 : pobi, jun");
     }
 
     @Override
     public void runMain() {
-        main(new String[]{});
+        Application.main(new String[]{});
     }
 }
