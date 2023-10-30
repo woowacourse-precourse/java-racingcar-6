7ed78a498e41e7751a2ed5de0e04277d8da2997f
dbstn1325 <dbstn1325@naver.com>
2023-10-30 18:14:28 +0900

    feat:(TryCountValidator): 시도 횟수가 빈 값인 경우 예외처리 기능 구현

--- src/main/java/validator/TryCountValidator.java
+++ src/main/java/validator/TryCountValidator.java
@@ -9,4 +9,10 @@ public class TryCountValidator {
             throw new IllegalArgumentException("시도 횟수는 숫자만 가능합니다. 애플리케이션을 종료합니다.");
         }
     }
+
+    public static void validateEmpty(String tryCount) {
+        if(tryCount.isEmpty()) {
+            throw new IllegalArgumentException("시도 횟수가 빈 값입니다. 애플리케이션을 종료합니다.");
+        }
+    }
 }
