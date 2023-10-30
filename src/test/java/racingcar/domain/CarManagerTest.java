83551e0cf887ebeb0a8040d3c38f0e8698af7a04
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-10-30 20:16:51 +0900

    test: car 배열 생성 기능 테스트 사용자 입력값에 따른 car 배열 객체 생성 기능 테스트

---
+++ src/test/java/racingcar/domain/CarManagerTest.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import racingcar.domain.car.Car;
+import racingcar.domain.racing.CarManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarManagerTest {
+
+    @Test
+    @DisplayName("입력된 자동차 이름을 기반으로 Car 객체 리스트를 생성한다.")
+    void createCars_ValidInput_CreatesCarList() {
+        // Given
+        String carNames = "car1,car2,car3";
+
+        // When
+        CarManager carManager = new CarManager(carNames);
+        List<Car> cars = carManager.getCars();
+
+        // Then
+        assertThat(cars).hasSize(3);
+        assertThat(cars.get(0).getName()).isEqualTo("car1");
+        assertThat(cars.get(1).getName()).isEqualTo("car2");
+        assertThat(cars.get(2).getName()).isEqualTo("car3");
+    }
+
+    @Test
+    @DisplayName("입력된 자동차 이름 중에 길이가 5자를 초과하는 이름이 있으면 예외를 던진다.")
+    void createCars_NameLengthGreaterThanFive_ThrowsException() {
+        // Given
+        String carNames = "car1,tooLongName,car3";
+
+        // When & Then
+        assertThatThrownBy(() -> new CarManager(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 5자 이하여야 합니다.");
+    }
+}
