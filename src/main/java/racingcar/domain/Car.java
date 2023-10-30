a5f26fbcc9bbeb846b771cac0dafc3bb67d57152
chjcoder <chjcoder@gmail.com>
2023-10-30 16:09:11 +0900

    fix: Car클래스가 랜덤값을 4이상 받았을 때 한 칸 전진하도록 수정
    - 랜덤값 4이상 받았을 때
    	- 랜덤값 만큼 전진 -> 한 칸 전진하도록 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,19 +3,17 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private String name;
+    private final String name;  //refactor final
     private String position;
+
     public Car(String name) {
         this.name = name;
         position = "";
     }
 
     public void move(int randomNum) {
-//        int randomNum = Randoms.pickNumberInRange(0, 9);
         if (randomNum >= 4) {
-            for (int count = 0; count < randomNum; count++) {
-                position += "-";
-            }
+            position += "-";
         }
     }
 
