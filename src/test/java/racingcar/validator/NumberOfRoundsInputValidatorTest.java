62c075df2cfd65f87e98de78bf4121e3cf466d73
koomchang <leahpar0401@gmail.com>
2023-10-30 16:36:01 +0900

    test: 라운드 수 입력값에 대한 validator 관련 테스트 작성

--- src/test/java/racingcar/validator/NumberOfRoundsInputValidatorTest.java
+++ src/test/java/racingcar/validator/NumberOfRoundsInputValidatorTest.java
@@ -1,4 +1,41 @@
+package racingcar.validator;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class NumberOfRoundsInputValidatorTest {
-  
+
+    private final BasicValidator<String> numberOfRoundsInputValidator = new NumberOfRoundsInputValidator();
+
+    @Test
+    void validate() {
+        Throwable isInputEmptyException = assertThrows(
+                IllegalArgumentException.class,
+                () -> numberOfRoundsInputValidator.validate("")
+        );
+        assertEquals("입력값이 없습니다.", isInputEmptyException.getMessage());
+
+        Throwable isInputPositiveException = assertThrows(
+                IllegalArgumentException.class,
+                () -> numberOfRoundsInputValidator.validate("-1")
+        );
+        assertEquals("입력값이 0 이하의 숫자입니다.", isInputPositiveException.getMessage());
+
+        Throwable isInputIntegerException = assertThrows(
+                IllegalArgumentException.class,
+                () -> numberOfRoundsInputValidator.validate("1.1")
+        );
+        assertEquals("입력값이 정수가 아닙니다.", isInputIntegerException.getMessage());
+
+        Throwable isInputIntegerException2 = assertThrows(
+                IllegalArgumentException.class,
+                () -> numberOfRoundsInputValidator.validate("a")
+        );
+        assertEquals("입력값이 정수가 아닙니다.", isInputIntegerException2.getMessage());
+
+        assertDoesNotThrow(
+                () -> numberOfRoundsInputValidator.validate("1")
+        );
+    }
 }
