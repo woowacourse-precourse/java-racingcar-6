2b40219171e49f1b5af23dc3992da72fff9d771b
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 06:06:12 +0900

    refactor : 공백 포함 예외처리와 구분자는 쉼표만 예외처리 코드 기능 수정

--- src/main/java/racingcar/exception/UserInputException.java
+++ src/main/java/racingcar/exception/UserInputException.java
@@ -13,7 +13,6 @@ public class UserInputException {
     private static final String BLANK_STRING_MESSAGE = "빈 문자를 입력하셨습니다.";
     private static final String DUPLICATE_MESSAGE = "자동차 이름은 중복되지 않게 입력해주세요";
     private static final String ONLY_COMMA = "구분자는 쉼표로만 작성해주세요";
-    private static final String INPUT_MORE_THAN_TWO_CAR = "차를 두대 이상 입력해주세요";
 
 
     public static void isLength(String carName) {
@@ -37,15 +36,8 @@ public class UserInputException {
 
     public static void isComma(String carName) {
         for (char c : carName.toCharArray())
-        if(!isLetterOrDigit(c) && c != ','){
+        if(!(Character.isLetterOrDigit(c) || c == ',' || Character.isWhitespace(c))){
             throw new IllegalArgumentException(ONLY_COMMA);
-        }
-    }
-
-    public static void isMoreThan(String carName) {
-        List<String> carNameList = Arrays.asList(carName.split(","));
-        if (carNameList.size() < 2) {
-            throw new IllegalArgumentException(INPUT_MORE_THAN_TWO_CAR);
         }
     }
 }
