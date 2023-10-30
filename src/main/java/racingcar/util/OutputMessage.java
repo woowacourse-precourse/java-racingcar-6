b5db7442999e1571a41b3cd4aa91c86066e29d3c
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 17:58:47 +0900

    feat: 기본 출력 메세지 추가

---
+++ src/main/java/racingcar/util/OutputMessage.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+public class OutputMessage {
+    public static final String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String MOVE_SIZE = "시도할 회수는 몇회인가요?";
+    public static final String RESULT = "실행 결과";
+    public static final String WINNER = "최종 우승자 : ";
+
+}
