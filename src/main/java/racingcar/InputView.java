a3c0a4004eb17dbc3a0143aca1321ac412b66c83
wochae <officialwochae@gmail.com>
2023-10-30 17:38:06 +0900

    feat(racingcar): inputView read nameList
    사용자의 입력을 콤마 단위로 분리를 하여 배열로 반환한다.

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readLine() {
+        return Console.readLine();
+    }
+
+    public String[] carNameList() {
+        return readLine().split(",");
+    }
+}
