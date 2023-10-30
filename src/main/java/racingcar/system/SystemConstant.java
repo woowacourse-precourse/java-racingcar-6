e347a68f963e24c7c57389d4a8347bdf10a3175d
yshong1998 <yshong1998@gmail.com>
2023-10-30 22:53:53 +0900

    Feat(SystemConstant): 경주에 필요한 상수를 저장하는 기능 구현

---
+++ src/main/java/racingcar/system/SystemConstant.java
@@ -0,0 +1,8 @@
+package racingcar.system;
+
+public class SystemConstant {
+    public static final int RANDOM_NUMBER_START_INCLUSIVE = 0;
+    public static final int RANDOM_NUMBER_END_INCLUSIVE = 9;
+    public static final int GO_FORWARD_REQUIRE_AT_LEAST = 4;
+    public static final int CAR_NAME_MAX_RANGE = 5;
+}
