b653a971f74314586165960fb2201806a55d85cb
hongyongjae <developsvai5096@gmail.com>
2023-10-31 08:45:24 +0900

    frst update

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("hello world");
+        input();
     }
+
+    private static void input(){
+        String input = Console.readLine();
+
+    }
+
+
+
 }
