8027771401239ecd0c097c58b226e2cafa9cb464
jongmee <ra123221@gmail.com>
2023-10-30 14:40:25 +0900

    feat: 프로그램의 출력을 담당하는 view 생성하기

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,31 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.dto.response.MovementDto;
+import racingcar.dto.response.WinnersDto;
+import racingcar.utility.Writer;
+
+public class OutputView {
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void announceGameResult() {
+        Writer.println(" ");
+        Writer.println(GAME_RESULT_MESSAGE);
+    }
+
+    public void writeMovement(final List<Car> carList) {
+        MovementDto movementDto;
+        for(Car car: carList) {
+            movementDto = new MovementDto(car);
+            Writer.println(movementDto.getMovementMessage());
+        }
+        Writer.println(" ");
+    }
+
+    public void writeFinalWinner(final WinnersDto winnersDto) {
+        Writer.print(WINNER_MESSAGE);
+        Writer.print(winnersDto.createMessage());
+    }
+}
