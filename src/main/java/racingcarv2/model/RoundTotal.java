53944cc50f570250ba4c0c989ea4a79513d890ff
sunghun <nokdonwithkj211@gmail.com>
2023-10-30 21:12:21 +0900

    feat(RoundTotal): 실행 회수를 표현하는 RoundTotal 클래스 형성

---
+++ src/main/java/racingcarv2/model/RoundTotal.java
@@ -0,0 +1,9 @@
+package racingcarv2.model;
+
+public class RoundTotal {
+    private final int roundTotal;
+
+    public RoundTotal(String roundTotal) {
+        this.roundTotal = Integer.parseInt(roundTotal);
+    }
+}
