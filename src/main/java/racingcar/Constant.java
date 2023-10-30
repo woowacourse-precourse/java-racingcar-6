c241e3710199fac24f5f438a31d3fb5a4d94095b
BH kang <rkdqudgjs1127@gmail.com>
2023-10-30 18:44:21 +0900

    feat: 시도 횟수 구현 및 예외처리 구현

--- src/main/java/racingcar/Constant.java
+++ src/main/java/racingcar/Constant.java
@@ -2,4 +2,5 @@ package racingcar;
 
 public class Constant {
     public static final String INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TURNNUM_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -9,15 +9,57 @@ import java.util.List;
 public class Race {
 
     private List<Car> allCars = new ArrayList<>();
+    private int turnNum;
 
-    private void inputCars(){
+    public void start(){
+        inputCars();
+        inputTurnNum();
+    }
 
+    public void inputCars(){
+
+        System.out.println(Constant.INPUT_CARNAME_MESSAGE);
        String s = Console.readLine();
        String[] cars = s.split(",");
 
         for (String carName : cars) {
             Car car = new Car(carName,0);
             allCars.add(car);
+        }
+        validateInputCars();
+    }
+
+    public void validateInputCars(){
+        validatInputCars_length();
+        validateInputCars_type();
+    }
+
+    public void validatInputCars_length() {
+        for (Car car : allCars) {
+            if(car.carName.isEmpty() || car.carName.length()>5){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateInputCars_type(){
+        for (Car car : allCars) {
+            if(car.carName.matches("a-zA-Z")){
+                throw new IllegalArgumentException();
+            }
+        }
+
+    }
+    public void inputTurnNum(){
+        System.out.println(Constant.TURNNUM_INPUT_MESSAGE);
+
+        int number = Integer.parseInt(Console.readLine());
+        validateInputTurnNUm(number);
+    }
+
+    public void validateInputTurnNUm(int number){
+        if(number<1){
+            throw new IllegalArgumentException();
         }
     }
 }
