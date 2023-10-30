1ea3d3d0e5466482a0ecc2e124bc00c934b46ffc
seminchoi <choicco89@gmail.com>
2023-10-30 19:45:21 +0900

    feat(racingcar) : OutputMessage enum 클래스 구현

---
+++ src/main/java/racingcar/OutputMessage.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public enum OutputMessage {
+    RACING_CAR_NAME_INPUT_REQUEST("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ROUND_COUNT_INPUT_REQUEST("시도할 회수는 몇회인가요?"),
+    EXECUTION_RESULT("실행 결과"),
+    ;
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+}
