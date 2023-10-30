e0f0bcf1690e8274aec920ffc0f24765d96e8a58
euniiiii <hee9920@naver.com>
2023-10-30 15:23:13 +0900

    style(utils): 출력 메세지 내용 수정

--- src/main/java/racingcar/domain/PrintMessage.java
+++ src/main/java/racingcar/domain/PrintMessage.java
@@ -4,8 +4,8 @@ public class PrintMessage {
 
     private PrintMessage() { }
 
-    public static final String INPUT_RACING_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+    public static final String INPUT_RACING_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?\n";
     public static final String PRINT_EXECUTION_RESULT = "\n실행 결과";
     public static final String PRINT_FINAL_WINNER = "최종 우승자 : ";
     public static final String ONLY_SPLIT_COMMA = "차 이름은 쉽표로 구분해야합니다.";
