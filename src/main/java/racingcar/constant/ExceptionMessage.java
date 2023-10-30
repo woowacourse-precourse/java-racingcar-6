567e799aec55964cac0be40cff37fe9115bb9128
bamm2 <xxknu88@naver.com>
2023-10-30 21:02:56 +0900

    "feat: 예외 발생 메세지 모음"

---
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -0,0 +1,22 @@
+package racingcar.constant;
+
+public enum ExceptionMessage {
+
+    DUPLICATE_CAR_NAME("입력한 자동차의 이름 중 동일한 이름이 존재합니다."),
+    OVER_SIZE_CAR_NAME("자동차의 이름은 5글자를 초과할 수 없습니다."),
+    EMPTY_CAR_NAME("자동차 이름은 공백을 사용할 수 없습니다."),
+    NOT_NUMBER_MESSAGE("숫자를 제외한 문자는 입력하실 수 없습니다."),
+    ZERO_NUMBER_MESSAGE("게임 횟수는 1보다 크거나 같고 100_000 보다 작거나 같은 양의 정수여야 합니다."),
+    MAX_VALUE_MESSAGE("100_000 보다 작거나 같은 양의 정수값을 입력해야 합니다."),
+    ;
+
+    final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
