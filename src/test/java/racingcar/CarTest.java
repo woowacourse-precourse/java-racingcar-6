2e4eb4d3a4f7ffb8a35c0b70f4c16d88e2443e08
unknown <hakie2kim@gmail.com>
2023-10-30 19:45:10 +0900

    test(src/test/java/racingcar/): test the current status matches with the form of 실행 결과

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void 현재_상태를_실행_결과와_같이_출력() {
+        Car testCar = new Car("jun");
+
+        for (int i=0; i<5; i++) { // 5번 이동
+            testCar.moveForward();
+        }
+
+        for (int i=0; i<2; i++) { // 2번 멈춤
+            testCar.stop();
+        }
+
+        assertThat(testCar.toString()).isEqualTo("jun : -----"); // toString() 생략 불가
+    }
+}
