1f16763b1260a65c4a4dd0db5a783077a49dac53
yohanii <dygks924@gmail.com>
2023-10-30 21:28:36 +0900

    feat: skeleton code 만들기

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.List;
+
+public class Input {
+
+    public static List<Car> inputCarNames() {
+        return null;
+    }
+
+    public static int inputTryCount() {
+        return 0;
+    }
+}
---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    public static void playRacing() {
+    }
+
+    private void trySeveral(List<Car> carList, int tryCount) {
+    }
+
+    private void tryOnce(List<Car> carList) {
+    }
+
+    private void printResult(List<Car> carList) {
+    }
+
+    private void printWinner(List<Car> winnerList) {
+    }
+}
