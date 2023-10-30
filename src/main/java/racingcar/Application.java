62593b1009660d5943402bb724773ca59d150765
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-10-30 22:13:22 +0900

    feat(Application): Application 클래스 main() 메소드 (기능 - 6) 구현
    기능 - 6 구현
    - ApplicationTest 동작 테스트 완료
    - main() 메소드 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,43 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        String nameInput = Console.readLine();
+        List<String> names = Arrays.asList(nameInput.split(","));
+
+        if (Checker.isNameAllCorrect(names) == false) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Car> cars = new ArrayList<>();
+
+        for (Integer nameIndex = 0; nameIndex < names.size(); nameIndex++) {
+            cars.add(new Car(names.get(nameIndex)));
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String countInput = Console.readLine();
+        Integer count = Integer.parseInt(countInput);
+
+        System.out.println();
+
+        System.out.println("실행 결과");
+        for (Integer gameCount = 1; gameCount <= count; gameCount++) {
+            Game.forwardOrStop(cars);
+
+            Printer.cars(cars);
+        }
+
+        List<String> winners = Checker.judgeWinners(cars);
+
+        Printer.winners(winners);
     }
 }
