7605ac7a11405d93cf12c1f24257eb0ca31361bd
soochan <tncksdl05@gmail.com>
2023-10-31 02:24:48 +0900

    refactor(RandomMovementCondition): 전진할 수 있는 조건 경계를 상수화
    전진할 수 있는 조건 경계를 상수화 함으로써 가독성 향상
    움직일 수 있는 상한 경계를 설정함으로써 예상 밖 시나리오 제한

--- src/main/java/racingcar/model/RandomMovementCondition.java
+++ src/main/java/racingcar/model/RandomMovementCondition.java
@@ -3,7 +3,8 @@ package racingcar.model;
 public class RandomMovementCondition implements MovementCondition {
     private static final int MIN_RANDOM_NUMBER = 0;
     private static final int MAX_RANDOM_NUMBER = 9;
-    private static final int MOVE_CONDITION = 4;
+    private static final int MIN_MOVABLE_NUMBER = 4;
+    private static final int MAX_MOVABLE_NUMBER = 9;
 
     private final NumberGenerator numberGenerator;
 
@@ -14,6 +15,6 @@ public class RandomMovementCondition implements MovementCondition {
     @Override
     public boolean isMovable() {
         int randomNumber = numberGenerator.generateBetween(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
-        return randomNumber >= MOVE_CONDITION;
+        return randomNumber >= MIN_MOVABLE_NUMBER && randomNumber <= MAX_MOVABLE_NUMBER;
     }
 }
