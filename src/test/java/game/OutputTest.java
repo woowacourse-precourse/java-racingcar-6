0593dadaaf682d350fae5965e2bb25329ff96ab7
문고은 <>
2023-10-30 19:06:02 +0900

    test : 출력 메서드 테스트
    consoleOutput 클래스 메서드 테스트 코드 추가

--- src/test/java/game/OutputTest.java
+++ src/test/java/game/OutputTest.java
@@ -1,32 +1,71 @@
 package game;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import racingcar.GameInputHandler;
+import racingcar.ConsoleOutput;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OutputTest {
+    ConsoleOutput consoleOutput = new ConsoleOutput();
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
+
     @Test
-    void output_실행_시_자동차_이름_입력_메시지_출력(){
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
+    void 실행_시_자동차_이름_입력_메시지_출력(){
+        consoleOutput.printInputCarName();
+        // 프로그램이 출력한 메시지 확인
+        String expectedMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        assertEquals(expectedMessage, outContent.toString().trim());
+    }
 
-        PrintStream originalOut = System.out;
+    @Test
+    void 시도할_횟수_받을_출력_메시지(){
+        consoleOutput.printInputAttempts();
 
-        try{
-            System.setOut(printStream);
+        String expectedMessage = "시도할 회수는 몇회인가요?";
+        assertEquals(expectedMessage, outContent.toString().trim());
+    }
+    @Test
+    void 실행_결과_출력_메시지(){
+        List<String> cars = new ArrayList<>();
+        List<String> forward = new ArrayList<>();
 
-            GameInputHandler gameInputHandler = new GameInputHandler();
-            gameInputHandler.startGame();
+        cars.add("aaa");
+        cars.add("bbb");
+        cars.add("ccc");
 
-            String printedMessage = outputStream.toString();
+        forward.add("--");
+        forward.add("---");
+        forward.add("----");
 
-            assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", printedMessage.trim());
-        } finally {
-            System.setOut(originalOut);
-        }
+        consoleOutput.printForward(cars, forward);
+
+        String expectedMessage = "aaa : --\nbbb : ---\nccc : ----";
+        assertEquals(expectedMessage, outContent.toString().trim());
+    }
+
+    @Test
+    void 최종_우승자_출력_메시지(){
+        String winners = "aaa, bbb";
+        consoleOutput.printWinners(winners);
+        String expectedMessage = "최종 우승자 : aaa, bbb";
+        assertEquals(expectedMessage, outContent.toString().trim());
     }
 }
