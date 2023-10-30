8dd4318e6afd12ca2605c41e4d8429dedb36f8b6
k9want <zndwlrnjr26@daum.net>
2023-10-30 22:57:34 +0900

    refactor: addCarsToList 생성 - 메소드 분리

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -2,24 +2,26 @@ package racingcar.controller;
 
 import racingcar.model.Car;
 import racingcar.util.Validation;
+import racingcar.view.InputView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarController {
 
-    public static List<Car> cars = new ArrayList<>();
+    public static final List<Car> cars = new ArrayList<>();
 
     public static void makeCar(String inputCarNames) {
-        String[] carNames = splitInputCarNames(inputCarNames);
+        String[] carNames = InputView.splitInputCarNames(inputCarNames);
         Validation.valiEmptySplitInputCarNames(carNames);
+        addCarsToList(carNames);
+    }
+
+    private static void addCarsToList(String[] carNames) {
         for (int i = 0; i < carNames.length; i++) {
             String carName = carNames[i].trim();
             Validation.valiCarName(carName.trim());
             cars.add(new Car(carName));
         }
-    }
-
-    private static String[] splitInputCarNames(String inputCarNames) {
-        return inputCarNames.split(",");
     }
 }
