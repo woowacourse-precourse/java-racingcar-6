e5bbf7fd53e052d91c1d98e9404bae8dd02ee150
baksakcci <pshsh910@gmail.com>
2023-10-31 00:24:56 +0900

    refactor($Application): update main logic for the role of Controller

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,20 @@ import racingcar.view.InputView;
 public class Application {
 
     public static void main(String[] args) {
-        RacingGameController racingGameController = new RacingGameController(new InputView());
+        // setting
+        InputView inputView = new InputView();
+        RacingGameController racingGameController = new RacingGameController();
+
+        // start application
+        // input car name event
+        String carNames = inputView.inputCarNames();
+        racingGameController.setCarName(carNames);
+
+        // input attempt event
+        String attempts = inputView.inputAttempts();
+        racingGameController.setAttempts(attempts);
+
+        // start event
         racingGameController.startGame();
     }
 }
