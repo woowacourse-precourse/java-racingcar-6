80ef7ea09bd1b5dadaf84a52d087700d75bbd743
Handwoong <handwoong@gmail.com>
2023-10-30 22:46:59 +0900

    test(car): 자동차 이름 유효성 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,12 +1,13 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class CarTest {
     @Test
-    void moveForward() throws Exception {
+    void 자동차_전진() throws Exception {
         // given
         final Car car = new Car("test");
 
@@ -18,7 +19,7 @@ class CarTest {
     }
 
     @Test
-    void notMoveForward() throws Exception {
+    void 자동차_멈춤() throws Exception {
         // given
         final Car car = new Car("test");
 
@@ -27,5 +28,26 @@ class CarTest {
 
         // then
         assertThat(car.getPosition()).isZero();
+    }
+
+    @Test
+    void 자동차_이름_NULL_예외() throws Exception {
+        assertThatThrownBy(() -> new Car(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력 문자열은 null일 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름_공백_예외() throws Exception {
+        assertThatThrownBy(() -> new Car("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력 문자열 길이는 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름_최대_길이_예외() throws Exception {
+        assertThatThrownBy(() -> new Car("handwoong"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력 문자열의 길이는 5이하만 가능합니다.");
     }
 }
