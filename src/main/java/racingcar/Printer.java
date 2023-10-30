7d5ef571e8387923a80fb97249b5606796bf48ee
tackyu97 <tack970913@gmail.com>
2023-10-30 19:44:07 +0900

    create Printer class
    안내 문구 출력 메서드 추가

---
+++ src/main/java/racingcar/Printer.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class Printer {
+    private static final String REQUEST_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRIAL = "시도할 회수는 몇회인가요?";
+    private static final String RESULT = "실행 결과";
+
+    public void requestNames() {
+        System.out.println(REQUEST_NAMES);
+    }
+
+    public void requestTrial() {
+        System.out.println(REQUEST_TRIAL);
+    }
+
+    public void result() {
+        System.out.println(RESULT);
+    }
+}
+
