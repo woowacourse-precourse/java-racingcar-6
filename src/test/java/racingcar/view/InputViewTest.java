19d2cca4feaf911fb2eade13de95266a499a99ec
jongheonleee <qwefghnm1212@gmail.com>
2023-10-30 16:47:04 +0900

    [chore] DisplayName 문구 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,23 +1,15 @@
 package racingcar.view;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest extends InTest {
 
 
     @Test
-    @DisplayName("공백만 입력할 경우 예외 발생")
+    @DisplayName("이름 입력시 공백만 입력할 경우 예외 발생")
     void 예외_처리_테스트_1() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -28,7 +20,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("이름 한 개만 입력할 경우 예외 발생")
+    @DisplayName("이름 입력시 이름 한 개만 입력할 경우 예외 발생")
     void 예외_처리_테스트_2() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -39,7 +31,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("글자 이외의 값을 입력한 경우 예외 발생")
+    @DisplayName("이름 입력시 글자 이외의 값을 입력한 경우 예외 발생")
     void 예외_처리_테스트_3() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -50,7 +42,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("입력 형식을 맞추지 못한 경우 예외 발생 - 공백이 포함된 경우")
+    @DisplayName("이름 입력시 입력 형식을 맞추지 못한 경우 예외 발생 - 공백이 포함된 경우")
     void 예외_처리_테스트_4() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -61,7 +53,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("입력 형식을 맞추지 못한 경우 예외 발생 - ',' 이외의 구분자가 사용된 경우")
+    @DisplayName("이름 입력시 입력 형식을 맞추지 못한 경우 예외 발생 - ',' 이외의 구분자가 사용된 경우")
     void 예외_처리_테스트_5() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -72,7 +64,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("여러 이름을 입력하면 리스트로 변환하는 기능 테스트")
+    @DisplayName("이름 입력시 여러 이름을 입력하면 리스트로 변환하는 기능 테스트")
     void inputNames() {
         // given
         InputView inputView = new InputView();
@@ -87,7 +79,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("공백을 입력한 경우 예외 발생")
+    @DisplayName("게임 회수 입력시 공백을 입력한 경우 예외 발생")
     void 예외_처리_테스트_6() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -98,7 +90,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("숫자가 아닌 값을 입력한 경우 예외 발생")
+    @DisplayName("게임 회수 입력시 숫자가 아닌 값을 입력한 경우 예외 발생")
     void 예외_처리_테스트_7() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -109,7 +101,7 @@ class InputViewTest extends InTest {
     }
 
     @Test
-    @DisplayName("1 보다 작은 수를 입력한 경우 예외 발생")
+    @DisplayName("게임 회수 입력시 1 보다 작은 수를 입력한 경우 예외 발생")
     void 예외_처리_테스트_8() {
         InputView inputView = new InputView();
         assertThrows(IllegalArgumentException.class,
@@ -121,7 +113,7 @@ class InputViewTest extends InTest {
 
 
     @Test
-    @DisplayName("게임 회수 입력 받는 기능 테스트")
+    @DisplayName("게임 회수 입력시 게임 회수 입력 받는 기능 테스트")
     void inputCountOfGameRound() {
         // given
         InputView inputView = new InputView();
@@ -134,7 +126,4 @@ class InputViewTest extends InTest {
         assertEquals(7, result);
 
     }
-
-
-
 }
