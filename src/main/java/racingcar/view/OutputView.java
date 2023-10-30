4caf84a88ff46c3352d317558a7ce27b43ad8927
choimung <103022503+choimung@users.noreply.github.com>
2023-10-30 16:28:33 +0900

    style : view 폴더 이동 및 메서드명 수정 InputView, OutputView 파일을 view폴더로 이동 OutputView 내부 메서드 이름을 일부 수정하였습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,12 @@
-package racingcar;
+package racingcar.view;
 
 public class OutputView {
 
-    public void inputCarNames() {
+    public void printInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void inputTryCount() {
+    public void printInputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
@@ -14,11 +14,11 @@ public class OutputView {
         System.out.println("\n" + "실행 결과");
     }
 
-    public void raceOutput(String currentCarNameAndPosition) {
+    public void printCurrentNameAndPosition(String currentCarNameAndPosition) {
         System.out.println(currentCarNameAndPosition + "\n");
     }
 
-    public void winnerOutput(String winnerList) {
+    public void printWinners(String winnerList) {
         System.out.println("최종 우승자 : " + winnerList);
     }
 }
