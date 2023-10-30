9770e8a9ccba725fbe0dc57ede1bc633aace1212
chan-won <clw8679@naver.com>
2023-10-30 23:02:35 +0900

    feat: RacingGame 클래스 구현

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private final List<Car> cars = new ArrayList<>();
+    private final int finalRound;
+    private int currentRound;
+
+    public RacingGame(int finalRound, Car... cars) {
+        this.cars.addAll(List.of(cars));
+        this.finalRound = finalRound;
+        this.currentRound = 0;
+    }
+
+    public void nextRound() {
+        currentRound++;
+    }
+}
