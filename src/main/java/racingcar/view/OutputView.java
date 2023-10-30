cb2a958a1d5530611f213acbf1bb7d1677e4ec02
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-30 23:29:05 +0900

    feat(OutputView): 각 자동차의 이름과 현재 결과를 반환하는 함수 구현
    - 자동차 객체를 받아와서 자동차의 이름과 이동거리를 출력하는 함수 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,28 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 public class OutputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String DASH = "-";
+    private static final String COLON = " : ";
     public static void printNameInputMessage() {
         printlnMessage(NAME_INPUT_MESSAGE);
     }
 
     public static void printRoundInputMessage() {
         printlnMessage(ROUND_INPUT_MESSAGE);
+    }
+
+    public static void printCarNameAndPosition(Car car) {
+        String dashes = createDashes(car.getPosition());
+        String result = car.getName() + COLON + dashes;
+        printlnMessage(result);
+    }
+
+    public static String createDashes(int position) {
+        return DASH.repeat(position);
     }
 
     private static void printlnMessage(String message) {
