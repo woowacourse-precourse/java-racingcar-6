699144d64dff812bb47c74cc89e784b4d9295be8
heogeonho <geonh1016@gmail.com>
2023-10-30 11:11:09 +0900

    feat: 경주할 자동차의 이름 입력 기능 구현
    경주할 자동차의 이름을 입력받을 수 있게 함
    이름값은 , 기준으로 구분, 5자 이하로 제한, 중복된 값 불가능, 공백 불가능에 대한 예외처리 수행

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingGame {
+    public void startGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputName = Console.readLine();
+        validateInputName(inputName);
+    }
+
+    private static void validateInputName(String input) {
+        input = input.trim();
+        if (input.startsWith(",") || input.endsWith(",")) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+        String[] nameArray = input.split(",");
+        for (String name : nameArray) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하로 만들어야 합니다.");
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+            }
+        }
+        List<String> nameList = Arrays.asList(nameArray);
+        if (nameList.size() != nameList.stream().distinct().count()) {
+            throw new IllegalArgumentException("자동차의 이름은 중복된 값이 있을 수 없습니다.");
+        }
+    }
+}
