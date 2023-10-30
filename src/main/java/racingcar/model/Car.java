130fa6ed959a97c525b032113ce4fe2a749a2ae2
LeeJJ07 <jkj9513@naver.com>
2023-10-30 17:06:31 +0900

    Feat : Car model 추가(view에 생성된 Car 삭제)
    자동차의 이름과 전진한 거리를 알 수 있도록 class 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,15 +1,16 @@
-package racingcar.view;
+package racingcar.model;
 
 public class Car {
 	private String name;
 	private int ForwardCount;
-	
+
 	public Car(String nm) {
 		this.name = nm;
 		this.ForwardCount = 0;
 	}
-	
+
 	public int getForwardCount() {
 		return this.ForwardCount;
 	}
+
 }
