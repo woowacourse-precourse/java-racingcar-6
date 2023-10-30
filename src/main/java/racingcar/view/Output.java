43a00c0ff54b9b29c8fb99155cd2ec869e49ee9e
LeeJJ07 <jkj9513@naver.com>
2023-10-30 17:34:31 +0900

    feat : 라운드 출력 뷰 구현
    각 라운드에 자동차 전진 정도를 출력하는 기능 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,5 +1,17 @@
 package racingcar.view;
 
-public class Output {
+import java.util.List;
 
+import racingcar.model.Car;
+
+public class Output {
+	public final String RESULT_MESSAGE = "실행 결과";
+	public final String WINNER_MESSAGE = "최종 우승자";
+	
+	public void roundResult(List<Car> cars) {
+		for(Car car : cars) {
+			System.out.printf("%s : %s\n", car.getName(), car.getForwardCount());
+		}
+		System.out.println();
+	}
 }
