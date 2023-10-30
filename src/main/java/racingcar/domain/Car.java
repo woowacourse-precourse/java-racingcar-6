4b589d681dfad38a14cb6c3bce07724515f1decf
leemiyeon <lemingul7@gmail.com>
2023-10-30 20:18:53 +0900

    refactor: add MovementHistory, Round 1. Car: 캡슐화를 위해 MovementHistory, Round 객체 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,67 +1,34 @@
 package racingcar.domain;
 
-import static racingcar.domain.RacingConfig.FORWARD_HISTORY_MARK;
-import static racingcar.domain.RacingConfig.FORWARD_NUMBER;
-
-import java.util.List;
-
 public class Car {
     private final String name;
-    private int currentRound;
-    private final int finalRound;
-    private int movedDistance;
-    private final List<String> movedHistory;
+    private final MovementStrategy movementStrategy = new RandomMovementStrategy();
+    private final MovementHistory movementHistory = new MovementHistory();
+    private final Round round;
 
-    public Car(String name, int currentRound, int finalRound, int movedDistance, List<String> movedHistory) {
+    public Car(String name, Round round) {
         this.name = name;
-        this.currentRound = currentRound;
-        this.finalRound = finalRound;
-        this.movedDistance = movedDistance;
-        this.movedHistory = movedHistory;
+        this.round = round;
     }
 
     public String getName() {
         return name;
     }
-    public int getMovedDistance() {
-        return movedDistance;
-    }
-    public List<String> getFormattedMovedHistory(){
-        return this.movedHistory.stream()
-                .map(history -> history.replaceAll("\\s+", ""))
-                .toList();
+
+    public MovementHistory getMovementHistory() {
+        return movementHistory;
     }
 
-    public String generateMoveHistoryMark() {
-        String history = String.join("", getFormattedMovedHistory());
-        return String.format("%s : %s",this.name,history);
-    }
-
-    public void move(int generatedRandomNumber) {
-        if (generatedRandomNumber >= FORWARD_NUMBER) {
-            moveForward();
-            increaseRound();
-            return;
-        }
-        stayInplace();
-        increaseRound();
-    }
-    private void moveForward() {
-        this.movedDistance+= 1;
-        this.movedHistory.add(FORWARD_HISTORY_MARK);
-    }
-    private void stayInplace() {
-        this.movedHistory.add(" ");
-    }
-
-    private void increaseRound() {
-        this.currentRound += 1;
-    }
-    public boolean hasMoreRound() {
-        return this.currentRound < this.finalRound;
+    public void move() {
+        movementHistory.recordMovement(movementStrategy.couldMove());
+        round.proceedToNextRound();
     }
 
     public boolean isWinner(int maxDistance) {
-        return this.movedDistance == maxDistance;
+        return movementHistory.isMaxDistance(maxDistance);
+    }
+
+    public boolean isReachedFinalRound() {
+        return round.hasReachedFinalRound();
     }
 }
---
+++ src/main/java/racingcar/domain/MovementHistory.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import static racingcar.domain.RacingConfig.MOVED_HISTORY_MARK;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MovementHistory {
+    private int movedDistance;
+    private List<String> movedHistories = new ArrayList<>();
+
+    public int getMovedDistance() {
+        return movedDistance;
+    }
+    public List<String> getMovedHistories() {
+        return movedHistories;
+    }
+
+    public void recordMovement(boolean couldMove) {
+        if (couldMove) {
+            movedDistance += 1;
+            movedHistories.add(MOVED_HISTORY_MARK);
+            return;
+        }
+        movedHistories.add(" ");
+    }
+
+    public boolean isMaxDistance(int maxDistance) {
+        return Objects.equals(movedDistance, maxDistance);
+    }
+
+}
