fcd620e98dd83612659df3bf387e6ff4f807b179
Jiyun <alex68566856@gmail.com>
2023-10-30 14:44:30 +0900

    refactor: 경주 결과 문자열 생성 방식 수정 - StringBuilder -> String.join

--- src/main/java/racingcar/view/RaceViewer.java
+++ src/main/java/racingcar/view/RaceViewer.java
@@ -9,6 +9,7 @@ public class RaceViewer {
     private final static String INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String INPUT_STEP_NUMBER = "시도할 회수는 몇회인가요?";
     private final static String RUN_RESULT = "\n실행 결과";
+    private final static String FINAL_WINNERS = "최종 우승자 : ";
 
     public void showCarsNameInput() {
         System.out.println(INPUT_CARS_NAME);
@@ -17,7 +18,6 @@ public class RaceViewer {
     public void showRepeatNumberInput() {
         System.out.println(INPUT_STEP_NUMBER);
     }
-
 
     public void showResultString() {
         System.out.println(RUN_RESULT);
@@ -41,9 +41,7 @@ public class RaceViewer {
 
 
     public void showRaceWinner(List<String> winners) {
-        StringBuilder raceWinners = new StringBuilder("최종 우승자 : ");
-        winners.forEach(winner -> raceWinners.append(winner).append(", "));
-        raceWinners.setLength(raceWinners.length() - 2);
+        String raceWinners = FINAL_WINNERS + String.join(", ", winners);
 
         System.out.println(raceWinners);
     }
