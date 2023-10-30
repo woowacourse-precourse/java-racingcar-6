6a7ee521fb75597c1a2f3b80024cbeff2bfa0351
heogeonho <geonh1016@gmail.com>
2023-10-30 11:26:12 +0900

    feat: 자동차 객체 리스트 생성 코드 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +11,12 @@ public class RacingGame {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
         validateInputName(inputName);
+
+        String[] inputArray = inputName.split(",");
+        List<Car> carList = new ArrayList<>();
+        for (String name : inputArray) {
+            carList.add(generateCar(name));
+        }
     }
 
     private static void validateInputName(String input) {
@@ -30,5 +37,9 @@ public class RacingGame {
         if (nameList.size() != nameList.stream().distinct().count()) {
             throw new IllegalArgumentException("자동차의 이름은 중복된 값이 있을 수 없습니다.");
         }
+    }
+
+    private static Car generateCar(String name) {
+        return new Car(name);
     }
 }
