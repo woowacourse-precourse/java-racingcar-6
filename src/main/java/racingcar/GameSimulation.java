284e82ea2c34f7ced87c62c7170c9a29e4cdd817
kms0946 <gghuly67@gmail.com>
2023-10-30 15:02:38 +0900

    make Car model

--- src/main/java/racingcar/GameSimulation.java
+++ src/main/java/racingcar/GameSimulation.java
@@ -7,22 +7,23 @@ import java.util.List;
 
 public class GameSimulation {
     private List<String> user;
+    private Integer fowardTime;
 
 
-    public GameSimulation(){
+    public GameSimulation() {
     }
 
-    public List<String> userInit(String userInput){
+    public List<String> userInit(String userInput) {
         String[] array = userInput.split(",");
         List<String> userList = new ArrayList<>();
-        for (int i = 0; i < array.length; i++){
-            if (array[i].contains(" ")){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].contains(" ")) {
                 throw new IllegalArgumentException("이름에 공백이 있으면 안됩니다.");
             }
-            if (array[i].length()>5){
+            if (array[i].length() > 5) {
                 throw new IllegalArgumentException("이름이 5자리 이상입니다.");
             }
-            if (userList.contains(array[i])){
+            if (userList.contains(array[i])) {
                 throw new IllegalArgumentException("같은 이름이 존재합니다.");
             }
             userList.add(array[i]);
@@ -30,18 +31,16 @@ public class GameSimulation {
         return userList;
     }
 
-    public void gamePlay(){
+    public void gamePlay() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
         this.user = userInit(userInput);
         System.out.println("시도할 회수는 몇회인가요?");
-        String foward=Console.readLine();
-        Integer fowardTime=Integer.parseInt(foward);
+        String foward = Console.readLine();
+        fowardTime = Integer.parseInt(foward);
         System.out.println(user);
+        System.out.println(fowardTime);
     }
-
-
-
 
 
 }
