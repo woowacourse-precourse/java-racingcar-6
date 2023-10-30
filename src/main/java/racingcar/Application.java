bf2fb39ef40b40d1d1cd2e77d54f53a6f1ae2c94
DESKTOP-UPSCALB\tkddn <thwlsdl0989@gmail.com>
2023-10-30 12:21:58 +0900

    최종 프로그램 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserInput userInput = new UserInput();
+        GameFunction gameFunction = new GameFunction();
+        Print print = new Print();
+
+        List<Car> carList;
+
+        int turn;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNameList = userInput.getCarNames();
+        carList = gameFunction.makingCar(carNameList);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        turn = userInput.getTurn();
+        System.out.println();
+
+        System.out.println("실행 결과");
+        gameFunction.repeatGame(turn, carList);
+        print.Winner(carList);
     }
 }
