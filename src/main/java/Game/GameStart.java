b3c9ad07412c57f13f905427f1ec1798dfa6899a
gunwonpark <parkgunwon1@naver.com>
2023-10-30 13:37:02 +0900

    패키지 분리 및 메세지 기능 구현

--- src/main/java/Game/GameStart.java
+++ src/main/java/Game/GameStart.java
@@ -1,10 +1,13 @@
-package racingcar;
+package Game;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import static Utill.Messages. *;
 
 public class GameStart {
     private String cars;
@@ -12,6 +15,7 @@ public class GameStart {
     private int[] carsScore;
     private String[] carsScoreString;
     public GameStart(){
+        gameStartMessage();
         cars = Console.readLine();
         carsName = cars.split(",");
 
@@ -20,10 +24,14 @@ public class GameStart {
     }
 
     public void Start(){
+        repeatCountRequestMessage();
         int repeatCount = repeatCount();
+        playStartMessage();
         while(repeatCount-- > 0){
             play();
+            playMessage(carsName, carsScoreString);
         }
+        gameEnd();
     }
 
     private void play(){
@@ -31,6 +39,10 @@ public class GameStart {
         calcCarsScore();
         //자동차 점수 시각화
         carsScoreToString();
+    }
+
+    void gameEnd(){
+        gameEndMessage(winner(getMax()));
     }
 
     private int getMax(){
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,9 @@
 package racingcar;
 
+import Game.GameStart;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         GameStart game = new GameStart();
         game.Start();
     }
