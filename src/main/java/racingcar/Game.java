5eedcfcccca326f41a42bf0cef258fded37c151b
coh <och5405@naver.com>
2023-10-31 03:00:09 +0900

    Make Message Class

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,13 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+public class Game {
+    Car[] Cars;
+    String randomNumber;
+    String nameLine;
+    String[] names;
+
+    void getNames(){
+        nameLine = Console.readLine();
+    }
+}
--- src/main/java/racingcar/Message.java
+++ src/main/java/racingcar/Message.java
@@ -1,4 +1,12 @@
 package racingcar;
 
 public class Message {
+    private static String startMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String attemptNumbersMessage = "시도할 회수는 몇회인가요?";
+    private static String resultMessage = "실행 결과";
+    private static String nameLengthError = "5글자 초과입니다";
+    private static String nullError = "공백입니다";
+    private static String digitError = "숫자가 아닙니다";
+    private static String digitRangeError = "음수입니다";
+    private String makeMessage = "";
 }
