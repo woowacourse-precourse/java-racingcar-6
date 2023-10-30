08f6b7d6a163ea5435e3529e29d02d09a75974db
jeondui <jeondui@gmail.com>
2023-10-30 17:43:44 +0900

    refactor(InputView): 시도 횟수 반환형을 Integer형에서 String형으로 수정. 유효성 검증 절차를 위해 수정하였다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,8 +8,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static int inputTryCount() {
+    public static String inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
