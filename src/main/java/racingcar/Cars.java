d7cf9e05a6a4144dc1f975e054466226b2bdf511
Jonghyeok Lee <endb1228@gmail.com>
2023-10-30 21:04:01 +0900

    Cars 생성자 및 Car 내부 클래스 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,4 +1,24 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(List<String> names) {
+        cars = names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private class Car {
+        private String name;
+        private int count = 0;
+
+        private Car(String name) {
+            this.name = name;
+        }
+    }
 }
