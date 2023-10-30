3d32529e949d56a582c22c12934276dd42f4ee13
minoooo119 <minoth0119@naver.com>
2023-10-30 22:42:53 +0900

    [FIX] reflect Race on Main App

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,8 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.*;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,10 +17,26 @@ public class Application {
         String userInputCarsNames;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        //1. 자동차 이름 입력, 확인
+        //1. 자동차 이름 입력, 확인, 초기에 이동 횟수 0으로
         userInputCarsNames = Console.readLine();
         UserCar userCar = new UserCar(userInputCarsNames);
-        userCar.PrintAllCars();
+
+        //2. 진행 횟수 입력
+        Integer userAttemptNums;
+        System.out.println("시도할 회수는 몇회인가요?");
+        userAttemptNums = Integer.parseInt(Console.readLine());
+        System.out.println(userAttemptNums);
+
+        //3. race 진행
+        Race carRace=new Race(userCar.Cars);
+        for(int i=0;i<userAttemptNums;i++){
+            carRace.forwardInRace();
+            carRace.printAllCarAndAdvance();
+        }
+        carRace.findWinner();
+        carRace.printWinner();
+
+
 
     }
 }
