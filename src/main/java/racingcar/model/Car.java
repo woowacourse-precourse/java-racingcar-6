0e6c0bda9292f359030b838928dc20cf4dba8707
donghoonyeom <dua5410773@naver.com>
2023-10-31 02:51:58 +0900

    refactor(model): Car 클래스 유효성 검사 및 중복 검사 리팩토링
    다음과 같은 변경사항을 포함한 Car 클래스의 유효성 검사 및 중복 검사 로직을 리팩토링했습니다:
    - 자동차 이름의 유효성을 검사하는 로직을 별도의 private 메서드로 분리하고, 중복된 이름 검사 로직을 추가하여 중복을 방지했습니다.
    - createCars 메서드 파라미터를 List<String>으로 변경하고, 중복된 이름이 있는지 검사하는 로직을 추가했습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,16 +2,17 @@ package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int distance;
 
     public Car(String name) {
-        if (name == null || name.trim().isEmpty() || name.trim().length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 1~5자 이어야 합니다.");
-        }
+        validateName(name);
         this.name = name.trim();
         this.distance = 0;
     }
@@ -31,11 +32,28 @@ public class Car {
         }
     }
 
-    public static List<Car> createCars(String[] carNames) {
+    public static List<Car> createCars(List<String> carNames) {
+        validateCarNames(carNames);
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
             cars.add(new Car(name));
         }
         return cars;
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.trim().isEmpty() || name.trim().length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 이어야 합니다.");
+        }
+    }
+
+    private static void validateCarNames(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        if (uniqueNames.size() != carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+        for (String name : carNames) {
+            validateName(name);
+        }
     }
 }
