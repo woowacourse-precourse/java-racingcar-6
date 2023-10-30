4099b36572ba91333ead6f943b9395c7369d64b4
dh1010a <dh1010a@naver.com>
2023-10-30 15:43:37 +0900

    feat: add OutputView

---
+++ src/main/java/racingcar/view/OutPutView.java
@@ -0,0 +1,50 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class OutPutView {
+
+    private static final int SOLO = 1;
+
+    private OutPutView() {
+    }
+
+    public static void printResultMessage() {
+        System.out.println(ViewMessage.OUTPUT_RESULT);
+    }
+
+    public static void printScore(List<Car> carList) {
+        for (Car car : carList) {
+            printCarScore(car);
+        }
+        System.out.println();
+    }
+
+    public static void printFinalWinner(List<String> winners) {
+        System.out.println(ViewMessage.OUTPUT_FINAL_WINNER + winners.get(0));
+        if (isSoloWin(winners)) {
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i < winners.size(); i++) {
+            System.out.print(", " + winners.get(i));
+        }
+        System.out.println();
+    }
+
+    private static boolean isSoloWin(List<String> winner) {
+        return winner.size() == SOLO;
+    }
+
+    private static void printCarScore(Car car) {
+        System.out.println(car.getName() + ViewMessage.OUTPUT_COLON);
+        printCarPosition(car);
+    }
+
+    private static void printCarPosition(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+    }
+
+}
