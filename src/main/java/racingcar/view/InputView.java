deda40848090a56bd92941e9c36fffe530e0588b
ksw13 <ksw48483@gmail.com>
2023-10-30 10:25:30 +0900

    feat: 검증 기능 적용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,15 +2,20 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.util.MessageConst;
+import racingcar.util.Validator;
 
 public class InputView {
     public String inputCarNames() {
         System.out.println(MessageConst.INPUT_NAME_MSG);
-        return Console.readLine();
+        String carNames = Console.readLine();
+        Validator.getValidatedCarName(carNames);
+        return carNames;
     }
 
     public int inputAttemptCount() {
         System.out.println(MessageConst.INPUT_ATTEMPT_MSG);
-        return Integer.parseInt(Console.readLine());
+        String attemptCount = Console.readLine();
+        Validator.getValidatedAttemptCount(attemptCount);
+        return Integer.parseInt(attemptCount);
     }
 }
