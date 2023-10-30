7fbda5f503792786f4444d7586001733510063cf
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-30 16:56:49 +0900

    test: 자동차에 이름을 부여할 수 있는지 테스트

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,21 +1,14 @@
 package model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    private final Car car = new Car();
-
-    @Nested
     @ParameterizedTest
-    @DisplayName("난수가 4이상이면 자동차는 전진한다. 4 미만이면 정지한다.")
     @ValueSource(ints = {0,1,2,3,4,5,6,7,8,9})
-    void isCarGoFoward(int randomNumber){
+    void 난수가_4이상이면_전진_4미만이면_정지(int randomNumber) {
+        Car car = new Car();
         int distanceBeforeMove = car.getDistance();
         car.move(randomNumber);
         if(randomNumber>=4) {
@@ -25,4 +18,10 @@ class CarTest {
         }
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"car1", "car2", "car3"})
+    void 자동차에_이름을_부여할_수_있다(String name) {
+        Car car = new Car(name);
+        assertEquals(name,car.getName());
+    }
 }
