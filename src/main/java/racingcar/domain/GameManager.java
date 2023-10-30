b44d4f7061f9729d1173ab25a96ac6917a073a2b
khsrla9806 <kimhunsope@naver.com>
2023-10-30 21:01:34 +0900

    feat: 게임을 진행하며, 게임 로그를 관리하고, 게임 승자를 계산하는 메서드 구현

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -1,11 +1,15 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.View;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
     private static final String CAR_NAME_DELIMIT = ",";
+    private static final String SPACE = " ";
+
     private List<Car> cars;
     private StringBuilder gameLog;
     private StringBuilder gameWinner;
@@ -30,6 +34,33 @@ public class GameManager {
         int enteredAttemptCount = Integer.parseInt(Console.readLine());
 
         this.attemptCount = enteredAttemptCount;
+    }
+
+    public void startGame() {
+        for (int count = 0; count < attemptCount; count++) {
+            for (Car car : cars) {
+                car.decideMoveOrNot();
+                gameLog.append(car.getCurrentStatus()).append(View.NEW_LINE);
+            }
+            gameLog.append(View.NEW_LINE);
+        }
+    }
+
+    public void calculateWinner() {
+        cars.sort((car1, car2) -> car2.getScore() - car1.getScore());
+        Car winner = cars.get(0);
+        gameWinner.append(winner);
+        findCoWinner(winner);
+    }
+
+    private void findCoWinner(Car winner) {
+        for (int index = 1; index < cars.size(); index++) {
+            Car car = cars.get(index);
+            if (winner.getScore() != car.getScore()) {
+                break;
+            }
+            gameWinner.append(CAR_NAME_DELIMIT).append(SPACE).append(car);
+        }
     }
 
 }
