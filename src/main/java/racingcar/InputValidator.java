6392efdf5ffbfe417020eb2b558bb3fc5f6a7638
Jiyoung Choi <12201817@inha.edu>
2023-10-30 15:55:22 +0900

    refactor(InputValidator) : 입력되는 자동차 이름 유효성 검사 기능 분리

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -9,19 +9,22 @@ public class InputValidator {
     private final static int MAX_LENGTH_OF_CAR_NAME = 5;
     private final static int MIN_SIZE_OF_COUNT = 0;
 
-    public static void validate(List<String> carsName) {
+    public static void validateCarsName(List<String> carsName) {
         if (isEmpty(carsName)) {
             ExceptionMessage.INPUT_NO_CAR_NAME_MESSAGE.throwException();
         }
         if (isNotDuplicated(carsName)) {
             ExceptionMessage.INPUT_DUPLICATE_CAR_NAME_MESSAGE.throwException();
         }
-        if (isOutOfRange(carsName)) {
+    }
+
+    public static void validateCarName(String carName){
+        if (isOutOfRange(carName)) {
             ExceptionMessage.INPUT_WRONG_CAR_NAME_LENGTH_MESSAGE.throwException();
         }
     }
 
-    public static void validate(int count) {
+    public static void validateCount(int count) {
         if (!isOverZero(count)) {
             ExceptionMessage.INPUT_WRONG_RANGE_OF_COUNT_MESSAGE.throwException();
         }
@@ -35,10 +38,9 @@ public class InputValidator {
         return racingCarsName.stream().distinct().count() != racingCarsName.size();
     }
 
-    private static boolean isOutOfRange(List<String> racingCarsName) {
-        return racingCarsName.stream().
-                anyMatch(racingCarName -> racingCarName.length() > MAX_LENGTH_OF_CAR_NAME
-                        || racingCarName.length() < MIN_LENGTH_OF_CAR_NAME);
+    private static boolean isOutOfRange(String racingCarName) {
+        return (racingCarName.length() > MAX_LENGTH_OF_CAR_NAME)
+                        || (racingCarName.length() < MIN_LENGTH_OF_CAR_NAME);
     }
 
     private static boolean isOverZero(int count) {
