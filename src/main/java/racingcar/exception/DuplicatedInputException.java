16e6eab86cb1bafb4a62f1d8e91b1b069e52ec1c
Kite_Z <duswl339@ajou.ac.kr>
2023-10-30 22:57:35 +0900

    remove: remove Exception class

--- src/main/java/racingcar/exception/DuplicatedInputException.java
+++
@@ -1,5 +0,0 @@
-package racingcar.exception;
-
-// 사용자 입력(Car name)의 중복이 발생한 경우
-public class DuplicatedInputException extends IllegalArgumentException{
-}
--- src/main/java/racingcar/exception/OutOfRangeIntegerException.java
+++
@@ -1,5 +0,0 @@
-package racingcar.exception;
-
-// 정수 입력 범위를 넘은 경우
-public class OutOfRangeIntegerException extends IllegalArgumentException{
-}
