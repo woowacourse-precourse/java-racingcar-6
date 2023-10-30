7bbd878afff34888acf883bd401133207d4b959e
honi20 <1594cyh@gmail.com>
2023-10-30 14:17:03 +0900

    feat: 자동차 이름 입력받기
    자동차 이름들을 입력받고 Car 객체로 이루어진 List로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startRacing();
+    }
+
+    private static void startRacing() {
+        List<Car> carList = inputAndGetCarList();
+    }
+
+    private static List<Car> inputAndGetCarList() {
+        String carNames = inputCarNames();
+        return getCarList(carNames);
+    }
+
+    private static String inputCarNames() {
+        System.out.println("\"경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static List<Car> getCarList(String carNames) {
+        List<Car> carList = new ArrayList<>();
+        String[] carNameArray = carNames.split(",");
+
+        for (String carName : carNameArray) {
+            Car car = new Car(carName);
+            carList.add(car);
+        }
+        return carList;
     }
 }
