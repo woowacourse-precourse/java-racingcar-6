76b2e5eca999c8b52a2d8e9b71edc05f7b5b569d
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-30 23:35:03 +0900

    [FEAT] Game 클래스 분류, 랜덤 숫자 뽑아주는 함수 구현
    docs/README.md 파일에 따라 Game 클래스를 따로 만들고, 0부터 9까지 randomNumber를 뽑아주는 함수를 구현

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Game {
+    private int randomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
