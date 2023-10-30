65a748fe061a92dba6681117109bd8e926f6601b
gyeongmann <gyeongmann@github.com>
2023-10-30 17:37:03 +0900

    feat(racingcar): implement input car list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String readLine = readLine();
+        List<String> list = Arrays.asList(readLine.split(","));
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String name = list.get(i);
+            cars.add(new Car(name));
+        }
+
+        System.out.println(cars);
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     public String name;
     public int distance = 0;
-    Car(String name) {
+    public Car(String name) {
         this.name = name;
     }
 
@@ -17,5 +17,10 @@ public class Car {
         int pickNum = pickNumber();
         if (pickNum >= 4) return true;
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
     }
 }
