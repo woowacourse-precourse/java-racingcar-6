1116024e8878eca86170767bceeb7c388de653b3
You Taein <ytn1574@gmail.com>
2023-10-30 17:32:02 +0900

    feat: 기본 베이스 코드에 기능 변경 및 추가
    기존 참가자 별 진행 거리를 정수로 출력하던것을 요구 사항에 맞게 '---'의 형태로 출력하도록 변경
    
    최종 우승자 출력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,8 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -13,18 +15,38 @@ public class Application {
 
         String[] participants = input.split(",");
         int[] runDistance = new int[participants.length];
+        String[] runDistanceByHyphen = new String[participants.length];
+        Arrays.fill(runDistanceByHyphen, "");
+
+        int winnerDistance = 0;
 
         for (int i = 0; i < tryCount; i++) {
             for (int j = 0; j < participants.length; j++) {
                 if (goOrStop()) {
                     runDistance[j] = runDistance[j] + 1;
+                    runDistanceByHyphen[j] += "-";
                 }
             }
             for (int k = 0; k < participants.length; k++) {
-                System.out.println(participants[k] + " : " + runDistance[k]);
+                winnerDistance = Math.max(winnerDistance, runDistance[k]);
+                System.out.println(participants[k] + " : " + runDistanceByHyphen[k]);
             }
             System.out.println();
         }
+
+        String winners = "";
+
+        for (int l = 0; l < participants.length; l++) {
+            if (runDistance[l] == winnerDistance && winners.equals("")) {
+                winners += participants[l];
+            } else if (runDistance[l] == winnerDistance && !winners.equals("")) {
+                winners += ", ";
+                winners += participants[l];
+            }
+        }
+
+        System.out.println("최종 우승자 : " + winners);
+
     }
 
     public static boolean goOrStop() {
