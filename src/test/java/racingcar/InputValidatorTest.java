37465c5b8ceee0d94c8fa38f18e6f9d30bb6c25f
CodingApe9 <tlqkf6974@>
2023-10-30 18:06:19 +0900

    test(*) : 테스트 코드 리펙토링
    테스트에 사용하는 메소드 변경
    하나의 자동차 이름 검증하는 케이스 추가

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -1,7 +1,7 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import racingcar.validator.InputValidator;
@@ -10,166 +10,139 @@ public class InputValidatorTest {
 
     @Test
     void 자동자_전체_이름_검증_null_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carNames = null;
-                    InputValidator.validateCarNames(carNames);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateCarNames(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름 목록이 없습니다.");
     }
 
     @Test
     void 자동자_전체_이름_검증_빈_문자열_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carNames = "";
-                    InputValidator.validateCarNames(carNames);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateCarNames(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름 목록이 없습니다.");
     }
 
     @Test
     void 자동자_전체_이름_검증_공백_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carNames = " ";
-                    InputValidator.validateCarNames(carNames);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateCarNames(" "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름 목록이 없습니다.");
     }
 
     @Test
     void 자동자_전체_이름_검증_개행_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carNames = "\n";
-                    InputValidator.validateCarNames(carNames);
-                }
-        );
+        assertThatThrownBy(() -> InputValidator.validateCarNames("\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름 목록이 없습니다.");
+    }
 
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    @Test
+    void 자동자_전체_이름_검증_부적절한_콤마_사용1_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames(","))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 ','사용입니다.");
+    }
+
+    @Test
+    void 자동자_전체_이름_검증_부적절한_콤마_사용2_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames("aa,bb,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 ','사용입니다.");
+    }
+
+    @Test
+    void 자동자_전체_이름_검증_부적절한_콤마_사용3_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames(",aa,bb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 ','사용입니다.");
+    }
+
+    @Test
+    void 자동자_전체_이름_검증_부적절한_콤마_사용4_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames(",aa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 ','사용입니다.");
+    }
+
+    @Test
+    void 자동자_전체_이름_검증_부적절한_콤마_사용5_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames("aa,,bb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 ','사용입니다.");
+    }
+
+    @Test
+    void 자동자_전체_이름_검증_이름중복_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarNames("aa,aa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름이 같은 자동차들이 있습니다.");
     }
 
     @Test
     void 자동자_전체_이름_검증_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carNames = "홍길동,test,a맨";
-                    InputValidator.validateCarNames(carNames);
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+        assertThatCode(() -> InputValidator.validateCarNames("홍길동,test,a맨"))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 하나의_자동차_이름_검증_한글_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "홍길동";
-                    InputValidator.validateCarName(carName);
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+        assertThatCode(() -> InputValidator.validateCarName("홍길동"))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 하나의_자동차_이름_검증_영어_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "Asus";
-                    InputValidator.validateCarName(carName);
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+        assertThatCode(() -> InputValidator.validateCarName("Asus"))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 하나의_자동차_이름_검증_빈_문자열_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "";
-                    InputValidator.validateCarName(carName);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 하나의_자동차_이름_검증_5자_초과_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "superMan";
-                    InputValidator.validateCarName(carName);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateCarName(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름이 없습니다.");
     }
 
     @Test
     void 하나의_자동차_이름_검증_개행_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "\n";
-                    InputValidator.validateCarName(carName);
-                }
-        );
+        assertThatThrownBy(() -> InputValidator.validateCarName("\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검증할 자동차 이름이 없습니다.");
+    }
 
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    @Test
+    void 하나의_자동차_이름_검증_5자_초과_예외처리() {
+        assertThatThrownBy(() -> InputValidator.validateCarName("superMan"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 1~5자의 한글, 영어 대소문자로 구성됩니다. " +
+                        "한글은 자음, 모음만으로 구성할 수 없습니다.");
     }
 
     @Test
     void 하나의_자동차_이름_검증_숫자_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String carName = "666";
-                    InputValidator.validateCarName(carName);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateCarName("666"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 1~5자의 한글, 영어 대소문자로 구성됩니다. " +
+                        "한글은 자음, 모음만으로 구성할 수 없습니다.");
     }
 
     @Test
     void 시도_회수_검증_양수_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    InputValidator.validateIterationNumBer(5);
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+        assertThatCode(() -> InputValidator.validateIterationNumBer(13))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 시도_회수_검증_0_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    InputValidator.validateIterationNumBer(0);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateIterationNumBer(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 회수는 양수입니다.");
     }
 
     @Test
     void 시도_회수_검증_음수_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    InputValidator.validateIterationNumBer(-12);
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateIterationNumBer(-12))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 회수는 양수입니다.");
     }
 }
--- src/test/java/racingcar/RacingCarInputConsoleTest.java
+++ src/test/java/racingcar/RacingCarInputConsoleTest.java
@@ -1,7 +1,7 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
@@ -25,92 +25,72 @@ public class RacingCarInputConsoleTest {
 
     @Test
     void 자동차_이름_입력받기_빈문자열_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatThrownBy(() -> {
                     String input = "";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readCarNames();
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+                })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름을 정해주세요.");
     }
 
     @Test
     void 자동차_이름_입력받기_개행_예외_발생하지_않음() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatCode(() -> {
                     String input = "\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readCarNames();
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+                })
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 자동차_이름_입력받기_정상_처리() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatCode(() -> {
                     String input = "홍길동,test,a맨\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readCarNames();
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+                })
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 시도_회수_입력받기_양수_입력_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatCode(() -> {
                     String input = "3\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readIterationNumBer();
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+                })
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 시도_회수_입력받기_0_입력_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatCode(() -> {
                     String input = "0\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readIterationNumBer();
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+                })
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 시도_회수_입력받기_음수_입력_정상처리() {
-        Throwable result = catchThrowable(
-                () -> {
+        assertThatCode(() -> {
                     String input = "-12\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readIterationNumBer();
-                }
-        );
-
-        assertThat(result).doesNotThrowAnyException();
+                })
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 시도_회수_입력받기_문자열_입력_예외처리() {
-        Throwable result = catchThrowable(
-                () -> {
-                    String input = "-11abc#\n";
+        assertThatCode(() -> {
+                    String input = "11abc!\n";
                     System.setIn(new ByteArrayInputStream(input.getBytes()));
                     racingCarConsole.readIterationNumBer();
-                }
-        );
-
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+                })
+                .doesNotThrowAnyException();
     }
 }
