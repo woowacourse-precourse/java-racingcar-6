f73522b5ec25f53d9372b06ba9d183cf611053bc
“EvelynKimm” <evekimmy09@gmail.com>
2023-10-31 01:24:51 +0900

    refactor: divide one method into two methods
    raceProcess의 이중 for문을 각각 하나씩 나눠 두 개의 메서드로 분리

--- src/main/java/racingcar/control/Control.java
+++ src/main/java/racingcar/control/Control.java
@@ -9,39 +9,42 @@ import racingcar.view.OutputView;
 
 public class Control {
     private static int rounds;
-    // 여기 Control 클래스는 private으로 최대한 써보자! -> 어차피 Model, View로 못 나가니까!
-    // main 메서드인 playGame() 먼저 만들고 후에 이 클래스 내에서 나누기
-    // Cars 입력받고 List에 넣어주는 메서드도 여기서 구현할 것
-    // Winner 판별하는 메서드도 여기
-    public void playGame() {
 
+    public void playGame() {
         startGame();
         raceProcess();
         OutputView.printWinner();
     }
 
     private static void startGame() {
+        readyCarName();
+        readyRound();
+    }
+
+    private static void readyCarName() {
         String input = InputView.requestCarName();
         Validation.validateNameInput(input);
         Cars.carNames = Cars.makeCarList(input);
-        // 라운드 수 입력 받기
+    }
+
+    private static void readyRound() {
         String roundInput = InputView.requestRoundNumber();
         Validation.validateNumberInput(roundInput);
         rounds = Integer.parseInt(roundInput);
     }
 
-
     private static void raceProcess() {
         Cars.putObject();
-
-        System.out.print("실행 결과");
+        System.out.print("\n실행 결과");
         for (int i = 1; i <= rounds; i++) {
             System.out.println();
-
-            for (Car car : Cars.cars) {
-                car.move();
-                OutputView.printRacingProcess(car.getName(), car.getPosition());
-            }
+            movePosition();
+        }
+    }
+    private static void movePosition() {
+        for (Car car : Cars.cars) {
+            car.move();
+            OutputView.printRacingProcess(car.getName(), car.getPosition());
         }
     }
 }
