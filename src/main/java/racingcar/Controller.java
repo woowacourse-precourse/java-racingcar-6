67ece7f8e848ab70f5227774ae71a8e4fa294e1e
LeeJuWon <woen1016@gmail.com>
2023-10-30 21:26:39 +0900

    controller 생성

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+class Game {
+
+    static final int FOUR = 4;
+    Game() {
+
+    }
+
+    public static void start(){
+
+    }
+}
