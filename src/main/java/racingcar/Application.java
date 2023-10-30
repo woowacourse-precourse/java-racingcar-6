7c4dd467cc655100e27655d3aa383669fa06c077
junest66 <chlwndks333@gmail.com>
2023-10-31 00:38:03 +0900

    feat(RacingController): play 함수에 attemptCount 매개변수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
-import racingcar.utils.CarListCreator;
+import racingcar.utils.AttemptCountReader;
+import racingcar.utils.CarNamesReader;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         RacingController racingController = new RacingController();
-        racingController.play(CarListCreator.generateCarListFromInput());
+        racingController.play(CarNamesReader.get(), AttemptCountReader.get());
     }
 }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -3,17 +3,14 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Game;
-import racingcar.validator.AttemptCountValidator;
-import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
 
     private Game game;
 
-    public void play(List<Car> cars) {
+    public void play(List<Car> cars, int attemptCount) {
         this.game = new Game(cars);
-        int attemptCount = AttemptCountValidator.validateAttemptCount(InputView.getAttemptCount());
         OutputView.printResultMessage();
         playAllRounds(attemptCount);
         OutputView.printWinnerMessage(game.getWinners());
