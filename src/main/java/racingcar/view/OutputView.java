bc765db711f032344838550b08ab49c0e7d7d132
ycis5013@naver.com <ycis5013@naver.com>
2023-10-31 01:00:49 +0900

    feat(OutputView) : 각 라운드가 끝난 후 결과 출력하는 함수 printRoundResult 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,14 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+
 public class OutputView {
     private static OutputView outputView = new OutputView();
 
-    private OutputView() {}
+    private OutputView() {
+    }
 
     public static OutputView getInstance() {
         return outputView;
@@ -11,5 +16,17 @@ public class OutputView {
 
     public void printResult() {
         System.out.println("\n실행 결과");
+    }
+
+    public void printRoundResult(List<Car> carList) {
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            System.out.print(car.name + " : ");
+            for (int j = 0; j < car.moveCount; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
