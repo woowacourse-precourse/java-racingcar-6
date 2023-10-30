9d5189ac9688ab85abb0256dc608861eae1db51b
jihyuk <kwlgur0416@gmail.com>
2023-10-30 16:54:30 +0900

    feat : 우승자 선정을 위한 구조 작성

---
+++ src/main/java/racingcar/model/Judgment.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Judgment {
+
+    public List<String> judge(List<Car> cars) {
+        return new ArrayList<>();
+    }
+
+}
