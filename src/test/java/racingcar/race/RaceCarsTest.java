78b2d2cf542b7cdc638e30457b4c940738fa7d86
1000zoo <cjswldn99@gmail.com>
2023-10-31 01:31:21 +0900

    test(RaceCars): 수정된 생성자에 맞게 테스트 수정
    - RaceCars의 String을 받는 생성자가 사라졌기에 테스트코드 수정
    - Input을 상속받는 TestInput을 구현하는 방식으로 수정

--- src/test/java/racingcar/race/RaceCarsTest.java
+++ src/test/java/racingcar/race/RaceCarsTest.java
@@ -3,6 +3,7 @@ package racingcar.race;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.view.Input;
 
 class RaceCarsTest {
 
@@ -10,7 +11,16 @@ class RaceCarsTest {
     @DisplayName("String 기반 생성자 테스트")
     @ValueSource(strings = {"pobi,woni,jun"})
     void newConstructorTest(String names) {
-        RaceCars raceCars = new RaceCars(names);
+        Input testInput = new TestInput(names);
+        RaceCars raceCars = new RaceCars(testInput);
         System.out.println("raceCars = " + raceCars);
+    }
+
+    private record TestInput(String input) implements Input {
+
+        @Override
+        public String readLine() {
+            return input;
+        }
     }
 }
