3005c469eda490be11c2bb2c83a6ea1221bb266a
YongHyeon <pyh4616@naver.com>
2023-10-30 16:14:12 +0900

    Feat: (Application 클래스), 프로그램을 실행하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+import racingcar.service.RacingCarGameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarGameService racingCarGameService = new RacingCarGameService();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RandomNumberGenerator racingNumberGenerator = new RandomNumberGenerator();
+        RacingCarGameController racingCarGameController = new RacingCarGameController(racingCarGameService, inputView,
+                outputView);
+        racingCarGameController.playRacingCarGame(racingNumberGenerator);
     }
 }
