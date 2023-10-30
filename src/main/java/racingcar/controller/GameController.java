e2c0aeaf4965a799b884bcfbca4b3ae5fcd06a56
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:14:39 +0900

    feat(GameController) : InputView 클래스에서가 유저가 입력받은 자동차와 시도횟수를 통해 자동차 경주를 구현하는 playGame 메소드 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,2 +1,22 @@
-package racingcar.controller;public class GameController {
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.util.GoStopCar;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+
+    public void playGame(String carNamesInput, int numberOfAttempts) {
+        List<Car> cars = createCars(carNamesInput);
+        for (int attempt = 0; attempt < numberOfAttempts; attempt++) {
+            moveCars(cars);
+        }
+        List<String> winners = determineWinners(cars);
+        OutputView.printGameResult(cars, numberOfAttempts);
+        OutputView.printWinners(winners);
+    }
 }
