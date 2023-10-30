29b01425a16c036e5e4a2a6dd2ca23acbc87001a
sami355-24 <ddung355@gmail.com>
2023-10-30 10:26:07 +0900

    feat(Model): 자동차 이동 여부 판단을 통해 게임 한판을 진행해주는 메서드 추가

--- src/main/java/racingcar/Model/GameModel.java
+++ src/main/java/racingcar/Model/GameModel.java
@@ -1,14 +1,21 @@
 package racingcar.Model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Domain.Car;
 import racingcar.Domain.CarGroup;
 
 public class GameModel {
     public CarGroup playGameRound(CarGroup carGroup) {
-        return null;
+        for (int order = 1; order <= carGroup.findSize(); order++) {
+            Car findCar = carGroup.findCarByOrder(order);
+            if (Boolean.TRUE.equals(determineMove())) {
+                findCar.updatePosition();
+            }
+        }
+        return carGroup;
     }
 
-    public Boolean determineMove(){
+    public Boolean determineMove() {
         int number = Randoms.pickNumberInRange(0, 9);
         return number >= 4;
     }
