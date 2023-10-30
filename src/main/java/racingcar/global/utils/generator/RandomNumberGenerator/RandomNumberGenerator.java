d7259fb7ed58ce8750df81a7abf65ab7e78a9763
kariskan <zkfzpf56@naver.com>
2023-10-30 18:48:34 +0900

    feat: 0에서 9사이의 난수 생성기 작성

---
+++ src/main/java/racingcar/global/utils/generator/RandomNumberGenerator/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.global.utils.generator.RandomNumberGenerator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+	private static final int MIN_NUMBER_OF_RANGE = 0;
+	private static final int MAX_NUMBER_OF_RANGE = 9;
+
+	public static int generateRandomNumber() {
+		return Randoms.pickNumberInRange(MIN_NUMBER_OF_RANGE, MAX_NUMBER_OF_RANGE);
+	}
+}
