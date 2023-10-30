f3fd42ee99ca66c7f254aa5c4054f6a529555ec6
황중섭 <jshwang9709@gmail.com>
2023-10-31 02:32:12 +0900

    fix(car): 자동차 이름이 모두 서로 다른지 검증
    자동차 이름 전체를 보고 검증해야하는 사항이기 때문에 CarValidator에
    추가하지 못하고 StringUtil에 해당 로직을 추가했다.

--- src/main/java/racingcar/game/StringUtil.java
+++ src/main/java/racingcar/game/StringUtil.java
@@ -1,13 +1,28 @@
 package racingcar.game;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StringUtil {
 
     public static List<String> separate(String carNames) {
         String[] carNameArray = carNames.split(",");
         List<String> carNameList = Arrays.asList(carNameArray);
+        validateNamesDifferent(carNameList);
         return carNameList;
     }
+
+    private static void validateNamesDifferent(List<String> carNameList) {
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (String carName : carNameList) {
+            if (uniqueNames.contains(carName)) {
+                throw new IllegalArgumentException("모든 자동차 이름은 서로 달라야 합니다.");
+            }
+            uniqueNames.add(carName);
+        }
+    }
+
 }
