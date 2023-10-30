3c113814bc63567dc98f0c809d53ddd22587e029
zzawang <hobin0116@naver.com>
2023-10-30 15:03:16 +0900

    feat: Car 클래스 생성
    - 자동차의 이름과 이동한 거리를 멤버 변수로 가진다.
    - 자동차의 이름과 이동한 거리의 getter 메소드를 추가하였다.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    private String name; // 자동차의 이름
+
+    private int distance; // 자동차가 이동한 거리
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
