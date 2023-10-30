59ba5cae30e00612d6a96ae58ad9dfbf0d31d472
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-10-30 15:32:15 +0900

    test: 시도횟수 변환 메서드 검증

--- src/test/java/racingcar/utils/InputConvertorTest.java
+++ src/test/java/racingcar/utils/InputConvertorTest.java
@@ -32,4 +32,30 @@ class InputConvertorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이름의 제한 길이를 초과했습니다.");
     }
+
+    @Test
+    void parseTryCount_입력횟수를_int로_변환() {
+        String inputTry = "12";
+        int result = InputConvertor.parseTryCount(inputTry);
+
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    void parseTryCount_메서드_사용시_소수_변환_예외_발생() {
+        String inputTry = "12.5";
+
+        assertThatThrownBy(() -> InputConvertor.parseTryCount(inputTry))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효한 숫자를 입력해주세요.");
+    }
+
+    @Test
+    void validateTryCount_메서드_사용시_음수_또는_0_변환_예외_발생() {
+        int inputTry = 0;
+
+        assertThatThrownBy(() -> InputConvertor.validateTryCount(inputTry))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 1 이상이어야 합니다.");
+    }
 }
