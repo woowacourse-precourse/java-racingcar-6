f1d33e496ec522dc49d2ba74a691fc28be2ddd41
nhlee98 <e5slnh9876@gmail.com>
2023-10-30 17:22:24 +0900

    feat: 입력이 비어있다면 예외를 발생시키는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,9 @@ import java.util.function.Supplier;
 
 public class InputView {
     private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String EMPTY_EXCEPTION_MESSAGE = "입력은 빈 문자열이 될 수 없습니다.";
     private static final String CAR_NAMES_DELIMITER = ",";
+    private static final String EMPTY_STRING = "";
 
     private final Supplier<String> reader;
 
@@ -16,6 +18,18 @@ public class InputView {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
         String carNames = reader.get();
 
+        validateCarNames(carNames);
+
         return carNames;
+    }
+
+    private void validateCarNames(String carNames) {
+        validateIsEmpty(carNames);
+    }
+
+    private void validateIsEmpty(String carNames) {
+        if (carNames == null || carNames.equals(EMPTY_STRING)) {
+            throw new IllegalArgumentException(EMPTY_EXCEPTION_MESSAGE);
+        }
     }
 }
