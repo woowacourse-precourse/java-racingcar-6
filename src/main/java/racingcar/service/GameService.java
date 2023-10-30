82e447fada68c0db6e0685e04faa8b5c4fdab6a9
chan-won <clw8679@naver.com>
2023-10-31 00:19:45 +0900

    feat: GameService 우승자를 결정하는 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,5 +1,8 @@
 package racingcar.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 
 public class GameService {
@@ -9,5 +12,16 @@ public class GameService {
             racingGame.nextRound();
             racingGame.getCars().forEach(CarService::goByPickRandomNumber);
         }
+    }
+
+    public static List<Car> getWinners(List<Car> cars) {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
     }
 }
