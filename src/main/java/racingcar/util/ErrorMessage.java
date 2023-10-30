232106670be7a317fbac365c43c774dffd04f961
박성춘 <songchon100@gmail.com>
2023-10-30 14:38:28 +0900

    feat : 차 이름이 중복된 경우의 에러메시지 추가

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     NOT_COMMA_ERROR("이름은 쉽표(,)로 구분해주세요."),
     TRY_NUM_NOT_NUMBER_ERROR("이동 횟수는 숫자를 입력해주세요"),
     EMPTY_INPUT_ERROR("공백을 입력하지 마세요."),
-    CAR_NUM_ERROR("차는 두대 이상 입력해주세요.");
+    CAR_NUM_ERROR("차는 두대 이상 입력해주세요."),
+    DUPLE_CAR_NAME_ERROR("차 이름이 중복되었습니다.");
 
     private String errorMessage;
 
