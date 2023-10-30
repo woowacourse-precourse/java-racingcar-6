9f099e1f9898fb480029733cf979e938f3ef534b
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-31 01:58:41 +0900

    ServiceOutputManager의 printState를 CarManager로 이전

---
+++ src/main/java/racingcar/utils/ServiceOutputManager.java
@@ -0,0 +1,14 @@
+package racingcar.utils;
+
+public class ServiceOutputManager {
+
+    private static final String WINNER_PRINT = "최종 우승자 : ";
+
+    public void winnerPrint(String winners) {
+        System.out.println(WINNER_PRINT + winners);
+    }
+
+
+
+
+}
