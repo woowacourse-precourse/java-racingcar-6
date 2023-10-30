cdd18d19fba3e4c3d83a5b63d48e374454bf0599
inmyownway <wjdals05070@gmail.com>
2023-10-30 12:07:33 +0900

    Feat: 유저 입력기능 추가

---
+++ src/main/java/racingcar/view/UserInput.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+
+    public String readUserInput() {
+        return Console.readLine();
+    }
+
+    public String readAttemptsCount() {
+        return Console.readLine();
+    }
+
+
+}
