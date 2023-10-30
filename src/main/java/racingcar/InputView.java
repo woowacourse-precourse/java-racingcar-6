36d4612b7c95acd63cf410f5b85abc8a4e8f8f5e
dokkisan <dokkisan.wang@gmail.com>
2023-10-30 18:39:37 +0900

    feat(input-view): 뼈대 코드 생성

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	public String askTryCount() {
+		return Console.readLine();
+	}
+
+	public String askCarName() {
+		return Console.readLine();
+	}
+}
