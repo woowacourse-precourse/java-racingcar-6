f0166cadcf700631dcc1d2b5597521edbef5446d
Hoo-sung <gntjd135@gmail.com>
2023-10-30 10:45:10 +0900

    refactor(RefereeTest) : @ParameterizedTest와 구분자 변경을 통해 공동 우승자 테스트 케이스 리펙토링

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -30,15 +29,16 @@ class RefereeTest {
 
     @Nested
     class MultipleWinnersTests {
-        @Test
-        void 공동우승자인경우() {
+        @ParameterizedTest
+        @CsvSource(value = {"4:4:4:pobi,woni,jun","4:2:4:pobi,jun","0:1:1:woni,jun"}, delimiter = ':')
+        void 공동우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
             List<Car> cars = new ArrayList<>();
-            cars.add(new Car("pobi", 4));
-            cars.add(new Car("woni", 4));
-            cars.add(new Car("jun", 4));
+            cars.add(new Car("pobi", pobiDistance));
+            cars.add(new Car("woni", woniDistance));
+            cars.add(new Car("jun", junDistance));
 
             List<String> answer = Referee.determineWinner(cars);
-            List<String> expected = List.of("pobi", "woni", "jun");
+            List<String> expected = List.of(expectedWinners.split(","));
             assertThat(answer).isEqualTo(expected);
         }
     }
