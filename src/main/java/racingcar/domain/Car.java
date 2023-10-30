ad68660c7bbc30d0364cd9368bdf148d654d5143
gusgh075 <gusgh075@naver.com>
2023-10-30 20:16:04 +0900

    자동차의 이름과 현재위치를 출력시키는 기능구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,9 +2,13 @@ package racingcar.domain;
 
 public class Car {
     String name;
+    int location=0;
     public Car(String name){
         if(name.length()>5)
             throw new IllegalArgumentException();
         this.name=name;
+    }
+    public String showLocation(){
+        return name+" : "+"-".repeat(location);
     }
 }
