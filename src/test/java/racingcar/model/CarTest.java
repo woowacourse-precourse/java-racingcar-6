609b17e4e21d4895a0273aa1b45810965538ed36
Flash-KIM142 <topqr123q@gmail.com>
2023-10-30 20:51:40 +0900

    test: 자동차 객체 테스트 추가
    전진 여부 테스트

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setup() {
+        Name name = new Name("name");
+        car = new Car(name);
+    }
+
+    @Test
+    void 전진_성공_테스트() {
+        car.run(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 전진_실패_테스트() {
+        car.run(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
