12e410316f13d23b0c743d9310b0492c2f80ba9a
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-10-30 16:42:15 +0900

    ✨ feat: <레이싱을 진행할 횟수 입력받기> - inputView를 통해 race를 진행할 횟수 정수형으로 입력받기

--- src/main/java/racingcar/controller/racingCarGameController.java
+++ src/main/java/racingcar/controller/racingCarGameController.java
@@ -5,8 +5,12 @@ import racingcar.domain.racingCars;
 import racingcar.view.inputView;
 
 public class racingCarGameController {
+    private static int raceCount;
     public static void play() {
         racingCars racingCars = new racingCars(inputView.requireRacingCarName());
+        raceCount = inputView.requireRacingCount();
 
+        for (int i = 0; i < raceCount; i++) {
+        }
     }
 }
