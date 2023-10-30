b667d1c433a523b077285f4d9abdcb158eef7aa2
eunjin3481 <eunjin3481@gmail.com>
2023-10-30 23:31:07 +0900

    test(Race): 자동차 위치 갱신 기능 테스트 구현

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RaceTest {
     @Test
@@ -21,6 +23,30 @@ public class RaceTest {
         for (int i = 0; i < carNames.length; i++) {
             assertEquals(carNames[i], cars.get(i).getName());
         }
+    }
+
+    @Test
+    void updateCarsPositionTest() {
+        // Given
+        Race race = new Race();
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+        Car car3 = new Car("Car3");
+        race.cars.add(car1);
+        race.cars.add(car2);
+        race.cars.add(car3);
+
+        NumberGenerator numberGenerator = mock(NumberGenerator.class);
+        when(numberGenerator.createRandomNumber()).thenReturn(1,4,5);
+        race.numberGenerator = numberGenerator;
+
+        // When
+        race.updateCarsPosition();
+
+        // Then
+        assertEquals(0, car1.getPosition());
+        assertEquals(1, car2.getPosition());
+        assertEquals(1, car3.getPosition());
     }
 
 
