ba31163828441e2f5faca93c899b5015c341fa88
박승찬 <seungchan141414@gmail.com>
2023-10-30 16:46:01 +0900

    [Test(racing)] 현제 레이싱을 하는 자동차의 움직인 횟수중에 최고로 많은 횟수를 알려준다._5

--- src/test/java/racingcar/domain/GameHostTest.java
+++ src/test/java/racingcar/domain/GameHostTest.java
@@ -203,4 +203,19 @@ class GameHostTest {
         // than
         assertThat(result).isEqualTo(3);
     }
+
+    @DisplayName("현제 레이싱을 하는 자동차의 움직인 횟수중에 최고로 많은 횟수를 알려준다._5")
+    @Test
+    void getCntMaxMoveTest_5() {
+        // given
+        moveRaceCar(raceCar_3);
+        moveRaceCar(raceCar_3);
+        moveRaceCar(raceCar_3);
+
+        // when
+        Integer result = gameHost.getCntMaxMove(progressRaceCarList);
+
+        // than
+        assertThat(result).isEqualTo(3);
+    }
 }
