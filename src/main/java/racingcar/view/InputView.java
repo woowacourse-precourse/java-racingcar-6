c1f4b9b17daed08a64b06a04978a4f86ac5b877c
Isaac Han <isaachan233@gmail.com>
2023-10-30 20:46:31 +0900

    refactor: InputView도 독립적인 객체로 존재할 필요가 없을거 같아 static 으로 처리하여 공통적으로 유지하게 하고 인스턴스 생성에서 자유롭게 만듦

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,22 +1,12 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.utils.Validator;
 
 public class InputView {
-    private final Validator validator;
-    public InputView(Validator validator) {
-        this.validator = validator;
+    public static String carNamesInput() {
+        return Console.readLine();
     }
-
-    public String carNamesInput() {
-        String carNames = Console.readLine();
-        validator.validateCarNameInput(carNames);
-        return carNames;
-    }
-    public String numberOfAttemptsInput() {
-        String numberOfAttempts = Console.readLine();
-        validator.validateNumberOfAttemptsInput(numberOfAttempts);
-        return numberOfAttempts;
+    public static String numberOfAttemptsInput() {
+        return Console.readLine();
     }
 }
