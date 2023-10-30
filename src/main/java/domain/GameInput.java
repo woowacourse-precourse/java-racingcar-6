cabf7ef8f8ebecf5a742e193c203296e4a96b3fe
Sur0n <skyzephyr333@gmail.com>
2023-10-31 04:42:24 +0900

    feat : Write down GameInput

--- src/main/java/domain/GameInput.java
+++ src/main/java/domain/GameInput.java
@@ -1,17 +1,34 @@
 package domain;
 import camp.nextstep.edu.missionutils.Console;
+import exceptional.ExceptionalTry;
+import java.util.Arrays;
+import java.util.List;
+import validate.ValidateNameInRange;
 
 public class GameInput {
 
 
 
-    public static void userGameTries(){
-//        exceptionalTry();
+    public static int userGameTries(){
+        String Tries = Console.readLine();
+        //정규식 0-9로만 이루어져있는지
+        if(Tries.matches("[0-9]+")){
+            ExceptionalTry.exception();
+        }
+        Console.close();
+        return 5;
     }
 
     public static void userCarNameInput(){
-//        validateNameInRange();
-//        NameSplit();
+        String strCarName = Console.readLine();
+        List<String> CarNames = NameSplit(strCarName);
+        ValidateNameInRange.NameInRange(CarNames);
+        Console.close();
+    }
+    private static List<String> NameSplit(String strCarName){
+        List<String> CarNames = Arrays.stream(strCarName.split(",")).toList();
+        return CarNames;
+
     }
 
 }
