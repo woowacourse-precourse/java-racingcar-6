792bd52f9f552eeec383dfb0e9020dba5238ecc3
Arachneee <jhg2819@naver.com>
2023-10-30 15:37:36 +0900

    style: 매직넘버 static 추가

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
-    private final int MAX_VALUE = 9;
-    private final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+    private static final int MIN_VALUE = 0;
 
     @Override
     public int generate() {
