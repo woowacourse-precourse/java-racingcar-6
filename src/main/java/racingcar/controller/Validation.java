48eda6aecfc9598e6ed78ffb524e3779c8df9254
shlihs95 <fhzptvh@gmail.com>
2023-10-30 17:17:31 +0900

    feat: 2. 입력 받은 데이터 검증 - 입력받은 자동차 이름 검증

---
+++ src/main/java/racingcar/controller/Validation.java
@@ -0,0 +1,42 @@
+package racingcar.controller;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    private static final Pattern LOWERCASE_AND_COMMA = Pattern.compile("^[a-z,]*$[a-z]");
+
+    public boolean validationNames(String inputCarName) {
+        String[] carNames = inputCarName.split(",");
+
+        if (!checkCharacter(inputCarName)) {
+            return false;
+        }
+        if (!checkCharacterNumber(carNames)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkCharacter(String inputCarName) {
+        Matcher matcher = LOWERCASE_AND_COMMA.matcher(inputCarName);
+
+        if (!matcher.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkCharacterNumber(String[] carNames) {
+        for (String carName : carNames) {
+            int nameLength = carName.length();
+
+            if (nameLength > 5 || nameLength <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
