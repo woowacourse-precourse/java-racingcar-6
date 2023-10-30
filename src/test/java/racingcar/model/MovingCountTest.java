7d54d60c0ed865005e2d0a171720d20605112728
kimwanyoung <dhks2869@gmail.com>
2023-10-30 14:04:48 +0900

    test : MovingCount getter테스트

---
+++ src/test/java/racingcar/model/MovingCountTest.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovingCountTest {
+
+    @Test
+    public void MovingCount_getter_테스트() {
+        //given
+        MovingCount movingCount = new MovingCount();
+        int expectResult = 0;
+
+        //when
+        int currentMovingCount = movingCount.getMovingCount();
+
+        //then
+        Assertions.assertThat(currentMovingCount).isEqualTo(expectResult);
+
+    }
+}
