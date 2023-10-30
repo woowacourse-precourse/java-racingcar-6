0af98201447cbcc10581a5b855db76838e806574
Genius-PAK <pyj789123@gmail.com>
2023-10-30 18:02:39 +0900

    feat: implement methods for initial settings and add method signatures for others in RacingCarView

--- src/main/java/racingcar/RacingCarView.java
+++ src/main/java/racingcar/RacingCarView.java
@@ -1,4 +1,21 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class RacingCarView {
+
+    public String displayCarNameSetupMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    public String displayRoundCountSetupMessage(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return readLine();
+    }
+
+    public void displayRoundResults(){}
+
+    public void displayWinnerList(){}
+
 }
