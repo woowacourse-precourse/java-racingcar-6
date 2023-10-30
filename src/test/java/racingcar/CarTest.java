65e0603a1e5e092145ead0fa70833c1757662802
Jin409 <sualng123@gmail.com>
2023-10-31 01:44:37 +0900

    [TEST] 개선된 for 문으로 리팩토링

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -13,9 +13,7 @@ public class CarTest {
     public void 이름이_5글자가_넘어가는_경우_예외_발생() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 1; i <= MAX_NAME_LEGNTH + 1; i++) {
-            stringBuilder.append("A");
-        }
+        stringBuilder.append("A".repeat(MAX_NAME_LEGNTH + 1));
 
         String invalidName = stringBuilder.toString();
         assertThrows(IllegalArgumentException.class, () -> Car.getInstance(invalidName));
@@ -25,9 +23,7 @@ public class CarTest {
     public void 이름이_5글자_이하인_경우_정상적으로_자동차가_등록() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 1; i <= MAX_NAME_LEGNTH; i++) {
-            stringBuilder.append("A");
-        }
+        stringBuilder.append("A".repeat(MAX_NAME_LEGNTH));
 
         String validName = stringBuilder.toString();
         assertDoesNotThrow(() -> Car.getInstance(validName));
