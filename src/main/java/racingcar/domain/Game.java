2b1a5bf0d9e9d6c2debf6a1f12391bd6786150e0
ddongjunn <ddongjunn@gmail.com>
2023-10-30 11:19:32 +0900

    feat : 경주할 자동차 이름 입력 받기 기능 완료

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -3,12 +3,12 @@ package racingcar.domain;
 import java.util.*;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Game {
 
     private List<Car> participant = new ArrayList<>();
 
     public void play(){
+        System.out.println(GameMessage.INPUT_CAR_NAMES.message);
         List<String> cars = parseCarNames(readLine());
     }
 
