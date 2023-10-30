271420c9fb8f826fd7a4516a228a7fbfe47420bb
Jaewon <choijawon99@gmail.com>
2023-10-30 16:24:36 +0900

    [FEAT] Application.java 기본 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,10 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager GM = new GameManager();
+
+        Game game = new Game(GM.inputCarsName(), GM.inputTrial());
+
+
     }
 }
