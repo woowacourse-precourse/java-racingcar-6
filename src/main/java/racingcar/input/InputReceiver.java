d7ab9ffca876ec051ffd9eb18a6d436696dd78eb
pgd <rudeh1253@gmail.com>
2023-10-30 15:15:31 +0900

    refactor: 클래스 이름 변경

--- src/main/java/racingcar/input/InputReceiver.java
+++ src/main/java/racingcar/input/InputReceiver.java
@@ -2,7 +2,7 @@ package racingcar.input;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputAdapter {
+public class InputReceiver {
     private static final String QUERY_FOR_CAR_NAMES_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String QUERY_FOR_NUM_OF_TRIES_INPUT = "시도할 회수는 몇회인가요?";
 
