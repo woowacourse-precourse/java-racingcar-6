a4a768324b98527813a30db223839eca4003acdd
dong_hy <kimadonghyon99@gmail.com>
2023-10-30 19:45:43 +0900

    feat : 캐릭터가 하나밖에없는경우 에러발생,에러 메서드 종합

--- src/main/java/racingcar/ErrorExcept.java
+++ src/main/java/racingcar/ErrorExcept.java
@@ -6,11 +6,11 @@ import java.util.Set;
 public class ErrorExcept {
     //
     public static void errorRedundant(String carName){
-        String[] carArr = carName.split(",");
+        String[] carArr = carName.split(Common.seperator);
         Set<String> set = new HashSet<>();
         for (String element : carArr) {
             if (!set.add(element)) {
-                throw new IllegalArgumentException("중복된 차량이름입니다.");
+                throw new IllegalArgumentException(Common.strErrorRedundant);
             }
         }
     }
@@ -19,9 +19,32 @@ public class ErrorExcept {
         String[] carArr = carName.split(",");
         for(String name:carArr){
             if(name.length() > 5){
-                throw new IllegalArgumentException("차량의 이름이 5글자를 초과했습니다.");
+                throw new IllegalArgumentException(Common.strErrorExcessLength);
             }
         }
+    }
+
+    public static void errorLessParameter(String carName){
+        String[] carArr = carName.split(",");
+        if(carArr.length <= 1){
+            throw new IllegalArgumentException(Common.strErrorExcessLength);
+        }
+    }
+
+    public static void errorInputCarName(String carName){
+        errorLessParameter(carName);
+        errorExcessLength(carName);
+        errorRedundant(carName);
+    }
+
+    public static int errorNaN(String strNum){
+        int num;
+        try{
+            num = Integer.parseInt(strNum);
+        }catch (NumberFormatException ex){
+            throw new IllegalArgumentException(Common.strErrorExcessLength);
+        }
+        return num;
     }
 
 
