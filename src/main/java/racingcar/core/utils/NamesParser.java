e906977fd19e984a4f8e7f5abad1665c0609f9db
sonjiseokk <thswltjr11@gmail.com>
2023-10-30 17:20:20 +0900

    :refactoring 조건문을 메소드로 분리

--- src/main/java/racingcar/core/utils/NamesParser.java
+++ src/main/java/racingcar/core/utils/NamesParser.java
@@ -3,18 +3,26 @@ package racingcar.core.utils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NamesParser {
     public List<String> parsing(String stringNames){
-        if (stringNames.startsWith(" ") || stringNames.startsWith(",")) { // 시작이 이상한 경우
+        if (startCheck(stringNames) || endCheck(stringNames)
+                || commaCheck(stringNames)) {
             throw new IllegalArgumentException();
         }
-        if (stringNames.endsWith(",") || stringNames.endsWith(", ")) {// 끝이 이상한 경우
-            throw new IllegalArgumentException();
-        }
-        if (stringNames.contains(",,") || stringNames.contains(", ,")) { // 쉼표가 이상한 경우
-            throw new IllegalArgumentException();
-        }
-        return Arrays.asList(stringNames.split(","));
+        return Arrays.stream(stringNames.split(","))
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
+                .collect(Collectors.toList());
+    }
+    private static boolean commaCheck(final String stringNames) {
+        return stringNames.contains(",,") || stringNames.contains(", ,");
+    }
+    private static boolean endCheck(final String stringNames) {
+        return stringNames.endsWith(",") || stringNames.endsWith(", ");
+    }
+    private static boolean startCheck(final String stringNames) {
+        return stringNames.startsWith(" ") || stringNames.startsWith(",");
     }
 }
