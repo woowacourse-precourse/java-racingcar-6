f1ffbd3c78a630081555826de40701894dd087e2
yshong1998 <yshong1998@gmail.com>
2023-10-31 02:15:36 +0900

    Feat(SystemConstant): 전진을 표현하는 상수 추가

--- src/main/java/racingcar/system/SystemConstant.java
+++ src/main/java/racingcar/system/SystemConstant.java
@@ -6,4 +6,5 @@ public class SystemConstant {
     public static final int GO_FORWARD_REQUIRE_AT_LEAST = 4;
     public static final int WAIT_LINE_POSITION = 0;
     public static final int CAR_NAME_MAX_RANGE = 5;
+    public static final String POSITION_EXPRESSION = "-";
 }
