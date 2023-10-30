7cef6adb4bde0f2a57dabc739bc994ef6ef4ffbd
sh990920 <sh990920@gmail.com>
2023-10-30 15:50:50 +0900

    feat: addCar 메소드 추가

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,7 @@
 package game;
 
+import car.Car;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +18,16 @@ public class Game {
             people.add(personArray[i]);
         }
         return people;
+    }
+
+    // 자동차 클래스 추가
+    public List<Car> addCar(List<String> people){
+        List<Car> carList = new ArrayList<>();
+        for(int i = 0; i < people.size(); i++){
+            Car car = new Car(people.get(i));
+            carList.add(car);
+        }
+        return carList;
     }
 
 }
