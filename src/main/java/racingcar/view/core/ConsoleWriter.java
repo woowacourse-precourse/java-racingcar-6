5d38e278157485b629c2025004ffb8e446ba0e65
JaehongDev <println@kakao.com>
2023-10-30 22:10:43 +0900

    refactor: ConsoleWriter 접근제어자 protected로 변경

--- src/main/java/racingcar/view/core/ConsoleWriter.java
+++ src/main/java/racingcar/view/core/ConsoleWriter.java
@@ -2,15 +2,15 @@ package racingcar.view.core;
 
 public sealed class ConsoleWriter permits InputView, OutputView {
 
-    public void print(final Object data) {
+    protected void print(final Object data) {
         System.out.print(data);
     }
 
-    public void println(final Object data) {
+    protected void println(final Object data) {
         System.out.println(data);
     }
 
-    public void newLine() {
+    protected void newLine() {
         System.out.println();
     }
 
