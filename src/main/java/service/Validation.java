332d26145c4cc22c6eda3c9a7a270a7d1e0cfe74
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 01:04:49 +0900

    feat (Validation): 반복 횟수 입력 시 검증 추가
    반복 횟수를 입력 시 검증을 추가하였습니다:
    - 첫번째 숫자는 1~9사이
    - 두번째 이후 숫자는 0~9사이
    - 모두 숫자로 이루어져야 함
    - 잘못된 값을 입력받으면 IllegalArgumentException 반환

--- src/main/java/service/Validation.java
+++ src/main/java/service/Validation.java
@@ -1,8 +1,17 @@
 package service;
 
+import java.util.regex.Pattern;
+
 public class Validation {
+    public static final Pattern timesPattern = Pattern.compile("^[1-9][0-9]*$");
     public static void CarNames(String carName){
         if(carName.length()>5){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void Times(String times){
+        if(!timesPattern.matcher(times).matches()){
             throw new IllegalArgumentException();
         }
     }
