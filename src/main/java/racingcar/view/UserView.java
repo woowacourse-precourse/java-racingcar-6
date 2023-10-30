d8d8688debf483722f83c5ceb20ea7bb1187a854
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:19:42 +0900

    [feat] Add UserView class printInputNameMessage method

--- src/main/java/racingcar/view/UserView.java
+++ src/main/java/racingcar/view/UserView.java
@@ -1,6 +1,10 @@
 package racingcar.view;
 
+import static racingcar.utility.Constants.*;
+
 public class UserView {
     public UserView() {}
-
+    public void printInputNameMessage() {
+        System.out.println(INPUT_CAR_NAME);
+    }
 }
