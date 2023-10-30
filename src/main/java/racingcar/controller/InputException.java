35f7eba28add611af6be4cdd8023836db3529706
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:25:43 +0900

    [feat] Add InputException class checkOnlyInt method

--- src/main/java/racingcar/controller/InputException.java
+++ src/main/java/racingcar/controller/InputException.java
@@ -1,9 +1,16 @@
 package racingcar.controller;
 
+import static racingcar.utility.Constants.ONLYINT;
+
 public class InputException {
     private InputException() {}
     public static void checkNameLength(String newCarName) { // 이름 길이 5자 초과 예외처리
         if (newCarName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public static void checkOnlyInt(String newCarName) { // 숫자로만 이루어진 이름 예외처리
+        if (newCarName.matches(ONLYINT)) {
             throw new IllegalArgumentException();
         }
     }
