e60bd4cc185971a670d6abbfffab7e6c0f27fba6
bbbig5360 <bbbig5360>
2023-10-30 15:36:19 +0900

    자동차 경주 게임 이동횟수 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,17 @@ public class Application {
             throw new IllegalArgumentException();
     }
 
+    public static int Input2Integer(){
+        String inputNumber = Console.readLine();
+        int returnInt;
+        try{
+            returnInt = Integer.parseInt(inputNumber);
+        }catch(Exception e){
+            throw new IllegalArgumentException();
+        }
+        return returnInt;
+    }
+
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameInput = Console.readLine();
@@ -18,6 +29,8 @@ public class Application {
         for(int i=0; i<carNameList.length; i++){
             NameCheck(carNameList[i]);
         }
-        
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int repeatNum = Input2Integer();        
     }
 }
