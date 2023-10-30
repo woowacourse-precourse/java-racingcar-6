f17b75548cc7080e76d17c69df84bfcf79b1dd27
KangDeok <rkdejr2321@naver.com>
2023-10-30 15:33:36 +0900

    [feat] RacingCar 객체

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,2 +1,19 @@
-package racingcar.domain;public class RacingCar {
+package racingcar.domain;
+
+public class RacingCar {
+
+    private String carName;
+    private int forwardCount;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getForwardCount() {
+        return forwardCount;
+    }
+
+    public void increaseForwardCount() {
+        this.forwardCount++;
+    }
 }
