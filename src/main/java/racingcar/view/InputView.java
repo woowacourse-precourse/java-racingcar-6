064b96dd688d72b94a84e10494d0f7ef7b1ecb3e
yshong1998 <yshong1998@gmail.com>
2023-10-31 02:47:58 +0900

    Refactor(InputView): 조건에 맞게 입력받도록 예외처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,18 +3,39 @@ package racingcar.view;
 import static racingcar.system.SystemMessage.*;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.assist.InputValidator;
 
 public class InputView {
 
-    public String requestCarNamesInput(){
+    private final InputValidator inputValidator;
+
+    InputView(){
+        inputValidator = new InputValidator();
+    }
+
+    public List<String> requestCarNamesInput(){
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String carNames = Console.readLine();
-        return carNames;
+        List<String> carNameList = generateCarNameListByCarNames(carNames);
+        inputValidator.validateCarNamesInput(carNameList);
+        return carNameList;
     }
 
     public String requestRepeatTimesInput(){
         System.out.println(INPUT_REPEAT_TIMES_MESSAGE);
         String repeatTimes = Console.readLine();
+        inputValidator.validateRepeatTimesInput(repeatTimes);
         return repeatTimes;
+    }
+
+    private static List<String> generateCarNameListByCarNames(String carNames) {
+        List<String> carNameList = Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+        return carNameList;
     }
 }
