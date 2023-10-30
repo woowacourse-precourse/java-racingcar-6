182278b4072fc91486db0d87d174669ad58e3145
hongseokhodev <hongseokhodev@gmail.com>
2023-10-30 23:29:57 +0900

    fix: 자동차가 한 개일 경우 예외를 반환하도록 수정

--- src/main/java/view/inputView/ConsoleInputView.java
+++ src/main/java/view/inputView/ConsoleInputView.java
@@ -7,14 +7,22 @@ import java.util.List;
 
 public class ConsoleInputView implements InputView {
     private static final String NOT_NUMBER_MESSAGE = "숫자를 입력하세요.";
+    private static final String NEED_MORE_NAMES_MESSAGE = "두 개 이상의 이름을 입력하세요.";
 
     @Override
     public List<String> readCarNames() {
         String input = Console.readLine();
         String[] carNames = input.replace(" ", "")
                 .split(",");
+        validateSize(carNames);
         return Arrays.stream(carNames)
                 .toList();
+    }
+
+    private void validateSize(String[] carNames) {
+        if (carNames.length <= 1) {
+            throw new IllegalArgumentException(NEED_MORE_NAMES_MESSAGE);
+        }
     }
 
     @Override
