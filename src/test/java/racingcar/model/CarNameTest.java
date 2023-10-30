2bfe187038c15283029e29a629f2e4325447a29c
soochan <tncksdl05@gmail.com>
2023-10-30 21:22:12 +0900

    test(CarName): 자동차 이름에 null 값 검증 테스트 추가
    자동차 이름이 null인 경우 검증 테스트 추가
    똑같은 hashcode을 가지는지 검증 테스트 추가

--- src/test/java/racingcar/model/CarNameTest.java
+++ src/test/java/racingcar/model/CarNameTest.java
@@ -9,24 +9,30 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarNameTest {
+    
+    @Test
+    void 자동차_이름이_null이라면_유효하지_않다() {
+        assertThatThrownBy(() -> CarName.from(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "  "})
-    void 자동차_이름은_공백이_될_수_없다(String carName) {
+    void 자동차_이름에_공백이_들어가면_유효하지_않다(String carName) {
         assertThatThrownBy(() -> CarName.from(carName))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"sochan", "sochan ", " sochan"})
-    void 자동차_이름은_허용가능한_길이를_벗어나면_안된다(String carName) {
+    @ValueSource(strings = {"crong ", " crong"})
+    void 자동차_이름은_공백을_포함한_허용가능한_길이를_초과하면_유효하지_않다(String carName) {
         assertThatThrownBy(() -> CarName.from(carName))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"pobi", "crong", "a"})
-    void 자동차_이름은_공백이_아니고_허용가능한_길이를_가져야한다(String carName) {
+    @ValueSource(strings = {"a", "crong"})
+    void 자동차_이름이_허용_길이_이내면_생성된다(String carName) {
         assertDoesNotThrow(() -> CarName.from(carName));
     }
 
@@ -36,5 +42,21 @@ class CarNameTest {
         CarName expectedCarName = CarName.from("jack");
 
         assertThat(actualCarName).isEqualTo(expectedCarName);
+    }
+
+    @Test
+    void 자동차_이름이_서로_다르다면_다른_객체로_판단한다() {
+        CarName actualCarName = CarName.from("jack");
+        CarName expectedCarName = CarName.from("crong");
+
+        assertThat(actualCarName).isNotEqualTo(expectedCarName);
+    }
+
+    @Test
+    void 동일한_자동차_이름은_동일한_hashcode를_가진다() {
+        CarName actualCarName = CarName.from("jack");
+        CarName expectedCarName = CarName.from("jack");
+
+        assertThat(actualCarName.hashCode()).isEqualTo(expectedCarName.hashCode());
     }
 }
