e3ebf90cef25c282d32782db6557c114da452bb6
Gyeongho Yang <chchch1213@naver.com>
2023-10-30 11:37:22 +0900

    feat(view): 우승자 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import racingcar.global.GeneralMessage;
 
 import static racingcar.global.GeneralMessage.EXECUTION_RESULT;
+import static racingcar.global.GeneralMessage.WINNER;
 
 public class OutputView {
     public static void printGeneralMessage(GeneralMessage generalMessage) {
@@ -10,11 +11,15 @@ public class OutputView {
     }
 
     public static void printResultTitle() {
-        System.out.println();
-        printGeneralMessage(EXECUTION_RESULT);
+        System.out.println("\n" + EXECUTION_RESULT.getMessage());
     }
 
     public static void printStatus(String status) {
         System.out.println(status + "\n");
+    }
+
+    public static void printWinner(String winner) {
+        System.out.printf("%s : %s\n", WINNER.getMessage(), winner);
+
     }
 }
