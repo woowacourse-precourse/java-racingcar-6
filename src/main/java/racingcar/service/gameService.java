03a2021c673559df4be19c871f5ad3623d825546
Seyeon <wjdtpdus828@naver.com>
2023-10-30 15:10:52 +0900

    feat(gameService): 자동차의 이름을 쉼표(,)로 구분하여 list로 return 하는 메소드인 splitCarNames 구현

--- src/main/java/racingcar/service/gameService.java
+++ src/main/java/racingcar/service/gameService.java
@@ -5,5 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class gameService {
-
+    public List<String> splitCarNames(String input){
+        String [] carString = input.split(",");
+        List<String> carList = new ArrayList<String>(Arrays.asList(carString));
+        return carList;
+    }
 }
