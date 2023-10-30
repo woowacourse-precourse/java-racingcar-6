40f084e89ea94c9579a0cbb1624c66637e314ec2
김효준 <khyojun1104@gmail.com>
2023-10-30 16:34:10 +0900

    refactor(CarRace): 파라미터들 순서 수정!

--- src/main/java/racingcar/service/WinnerService.java
+++ src/main/java/racingcar/service/WinnerService.java
@@ -4,23 +4,22 @@ import static racingcar.constant.Delimeter.WINNER_OUTPUT_DELIMETER;
 
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.constant.Delimeter;
 import racingcar.domain.Car;
 
 public class WinnerService {
 
 
     public String findWinners(List<Car> cars) {
-        int max=0;
+        int max = 0;
         List<String> winners = new ArrayList<>();
         max = getMaxMove(cars, max);
-        makeWinnersOutput(cars, max, winners);
+        makeWinnersOutput(cars, winners, max);
         return String.join(WINNER_OUTPUT_DELIMETER.getDelimeter(), winners);
     }
 
-    private void makeWinnersOutput(List<Car> cars, int max, List<String> winners) {
+    private void makeWinnersOutput(List<Car> cars, List<String> winners, int max) {
         for (Car car : cars) {
-            if(max == car.getMoveCount()){
+            if (max == car.getMoveCount()) {
                 winners.add(car.getName());
             }
         }
@@ -28,13 +27,13 @@ public class WinnerService {
 
     private int getMaxMove(List<Car> cars, int max) {
         for (Car car : cars) {
-            max = getMax(max, car);
+            max = getMax(car, max);
         }
         return max;
     }
 
-    private int getMax(int max, Car car) {
-        if(max < car.getMoveCount()){
+    private int getMax(Car car, int max) {
+        if (max < car.getMoveCount()) {
             max = car.getMoveCount();
         }
         return max;
