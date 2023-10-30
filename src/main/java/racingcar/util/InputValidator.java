5d7cbd11806588c7d8d5f7035910a434f578a1f6
siwol025 <cyk99115695@gmail.com>
2023-10-30 15:23:50 +0900

    feat(racingcar): 자동차 이름의 글자수에 따른 예외처리 기능 구현

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class InputValidator {
     private static final int MIN_CAR_NAMES_SIZE = 2;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
     public void validateCarNamesSize(List<String> carNames) {
         if (carNames.size() < MIN_CAR_NAMES_SIZE) {
@@ -11,8 +12,14 @@ public class InputValidator {
         }
     }
 
-    public void validateCarNameLength() {
+    public void validateCarNameLength(List<String> carNames) {
+        boolean isOverMaxCarNameLength = carNames.stream().map(carName -> carName.length())
+                .filter(nameLength -> nameLength > MAX_CAR_NAME_LENGTH)
+                .count() > 0;
 
+        if (isOverMaxCarNameLength) {
+            throw new IllegalArgumentException("자동차의 이름이 5글자를 초과 했습니다. 5글자 이하의 이름으로 입력해주세요.");
+        }
     }
 
     public void validateMoveCountRange() {
