62e303ddd911fd74d479a514bf9a63dab07f5493
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-30 22:33:27 +0900

    refactor(input): remove magic numbers in Validator class for clarity

--- src/main/java/racingcar/input/Validator.java
+++ src/main/java/racingcar/input/Validator.java
@@ -1,17 +1,20 @@
 package racingcar.input;
 
 class Validator {
+    private static final int MINIMUM_CAR_NAME_LENGTH = 5;
+    private static final String NUMBERS_ONLY_REGEX = "[0-9]+";
+
     static void validateCarNames(String input) {
         String[] carNames = input.split(",");
         for (String carName : carNames) {
-            if (carName.length() > 5) {
+            if (carName.length() > MINIMUM_CAR_NAME_LENGTH) {
                 throw new IllegalArgumentException("Car name '" + carName + "' should not exceed 5 characters.");
             }
         }
     }
 
     static void validateNumberOfAttempts(String input) {
-        if (!input.matches("[0-9]+")) {
+        if (!input.matches(NUMBERS_ONLY_REGEX)) {
             throw new IllegalArgumentException("The number of attempts '" + input + "' should be an integer.");
         }
         if (Integer.parseInt(input) < 0) {
