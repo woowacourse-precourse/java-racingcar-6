2f7de2d67f7d20563b7009638d88685dc6db5cbe
Hoo-sung <gntjd135@gmail.com>
2023-10-30 09:53:09 +0900

    refactor(RefereeTest): @Nested를 사용하여 테스트 그룹화

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,27 +10,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RefereeTest {
 
-    @Test
-    void 단독우승자인경우() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("pobi",4));
-        cars.add(new Car("woni",3));
-        cars.add(new Car("jun",2));
+    @Nested
+    class SingleWinnerTests {
+        @Test
+        void 단독우승자인경우() {
+            List<Car> cars = new ArrayList<>();
+            cars.add(new Car("pobi", 4));
+            cars.add(new Car("woni", 3));
+            cars.add(new Car("jun", 2));
 
-        List<String> answer = Referee.determineWinner(cars);
-        List<String> expected = List.of("pobi");
-        assertThat(answer).isEqualTo(expected);
+            List<String> answer = Referee.determineWinner(cars);
+            List<String> expected = List.of("pobi");
+            assertThat(answer).isEqualTo(expected);
+        }
     }
 
-    @Test
-    void 공동우승자인경우() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("pobi",4));
-        cars.add(new Car("woni",4));
-        cars.add(new Car("jun",4));
+    @Nested
+    class MultipleWinnersTests {
+        @Test
+        void 공동우승자인경우() {
+            List<Car> cars = new ArrayList<>();
+            cars.add(new Car("pobi", 4));
+            cars.add(new Car("woni", 4));
+            cars.add(new Car("jun", 4));
 
-        List<String> answer = Referee.determineWinner(cars);
-        List<String> expected = List.of("pobi","woni","jun");
-        assertThat(answer).isEqualTo(expected);
+            List<String> answer = Referee.determineWinner(cars);
+            List<String> expected = List.of("pobi", "woni", "jun");
+            assertThat(answer).isEqualTo(expected);
+        }
     }
+
 }
