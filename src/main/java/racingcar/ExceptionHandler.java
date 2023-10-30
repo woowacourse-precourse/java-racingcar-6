6778aaa2faf84dcb3088b29e583c06408e72ab66
wonindev <sangwonparkdev@gmail.com>
2023-10-30 18:20:38 +0900

    feat: 시도 횟수가 0보다 작은 경우 예외 처리 추가

--- src/main/java/racingcar/ExceptionHandler.java
+++ src/main/java/racingcar/ExceptionHandler.java
@@ -12,5 +12,9 @@ public class ExceptionHandler {
         if (Validation.isBlank(input)) {
             throw new IllegalArgumentException("시도 횟수가 공백입니다.");
         }
+
+        if (Validation.isNegative(Integer.parseInt(input))) {
+            throw new IllegalArgumentException("시도 횟수가 0보다 작습니다.");
+        }
     }
 }
