c25ab69118d1eeebbf4508e30b7c0e0d3e59ce95
seokmyungham <hellotjraud@gmail.com>
2023-10-30 09:11:19 +0900

    feat(Race): Race 클래스 생성 및 생성자 추가
    Race 클래스는 Car 객체를 저장하는 ArrayList를 포함하고 있습니다

---
+++ src/main/java/racingcar/domain/Race.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+
+public class Race {
+    private ArrayList<Car> cars;
+
+    public Race() {
+        this.cars = new ArrayList<>();
+    }
+}
