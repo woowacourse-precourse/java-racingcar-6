258b2729bfcef702a5cd2f52c433aab40963e546
ro-el <chung3286@naver.com>
2023-10-31 00:39:05 +0900

    feat: 게임 시작 메서드 생성 및 Application 실행시 해당 메서드 호출

--- src/main/java/racingcar/controller/CarRacingGame.java
+++ src/main/java/racingcar/controller/CarRacingGame.java
@@ -1,4 +1,18 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.service.GameService;
+
+import java.util.List;
+
 public class CarRacingGame {
+    private List<Car> cars;
+    private GameService gameService;
+
+    public CarRacingGame() {
+        this.gameService = new GameService();
+    }
+
+    public void gameStart() {
+    }
 }
