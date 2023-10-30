277d11d886accc06553ba4975ec920f63866daf9
kangyuseok <kingyuseok@naver.com>
2023-10-31 02:04:36 +0900

    Feat: 처음 자동차 이름들을 입력하는 메서드 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,4 +1,9 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Input {
+    public static String askCarNames(){
+        return readLine();
+    }
 }
