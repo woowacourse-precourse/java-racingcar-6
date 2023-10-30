ed6f49c3cfca15570ec2f46aeca4fc59c619a81e
kihwn <kihwn98@gmail.com>
2023-10-30 13:38:50 +0900

    Feat: 자동차 이름 입력에 대한 예외처리 추가
    자동차 이름이 5자를 초과할 경우 에외 처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -18,8 +18,14 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carInput = Console.readLine();
         String[] carArray = carInput.split(",");
+        for (String carName: carArray){
+            if (carName.length()>5){
+                throw new IllegalArgumentException("5자를 넘는 이름을 입력하셨습니다. 오류 발생");
+            }
+        }
         System.out.println("시도할 회수는 몇회인가요?");
         int tryNumber = Integer.parseInt(Console.readLine());
+
         int[]raceAccumulations = new int[carArray.length];
         return new Race(carArray,tryNumber,raceAccumulations);
     }
