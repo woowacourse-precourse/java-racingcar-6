35582feebd2e5be9e40fb2f9f6c202bce8401d73
yang <hjyang@epience.com>
2023-10-30 16:17:15 +0900

    🎯feat(service/RandomGenerator) : random 결과가 4이상일때 true 반환하는 함수 구현

--- src/main/java/racingcar/service/RadomGenerator.java
+++ src/main/java/racingcar/service/RadomGenerator.java
@@ -15,4 +15,12 @@ public class RadomGenerator {
     public boolean generateRandom() {
         return randomResult(Randoms.pickNumberInRange(0,9));
     }
+
+    // random 결과가 4이상이면 true
+    public boolean randomResult(int random) {
+        if (random >= 4) {
+            return true;
+        }
+        return false;
+    }
 }
