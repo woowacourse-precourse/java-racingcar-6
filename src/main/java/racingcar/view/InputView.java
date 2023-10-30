5237a2c7a2a670ad98640b6cacfd52d4228be9d2
elyudwo <dudqk9696@naver.com>
2023-10-30 13:32:48 +0900

    [Refactor] : InputView -> Utils 의존성 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,27 +1,17 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.util.Utils;
-
-import java.util.List;
 
 public class InputView {
-    private final Utils utils;
 
-    public InputView() {
-        utils = new Utils();
-    }
-
-    public List<String> insertCarName() {
+    public String insertCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String names = Console.readLine();
-        return utils.convertStringToList(names);
+        return Console.readLine();
     }
 
-    public int insertTryTimes() {
+    public String insertTryTimes() {
         System.out.println("시도할 회수는 몇회인가요?");
-        String tryNumber = Console.readLine();
-        return utils.convertStringToNumber(tryNumber);
+        return Console.readLine();
     }
 
 }
