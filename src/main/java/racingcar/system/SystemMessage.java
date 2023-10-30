818e4386021ca1b36d2f42d6ef7ccac0ac394ac1
SeongUk52 <rkfka7849@gmail.com>
2023-10-30 13:02:15 +0900

    fix(SystemMessage): 오타 수정

--- src/main/java/racingcar/system/SystemMessage.java
+++ src/main/java/racingcar/system/SystemMessage.java
@@ -2,7 +2,7 @@ package racingcar.system;
 
 public enum SystemMessage {
     INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
-    INPUT_NUMBER_OF_TRY("시도할 회수는 몇회인가요?\n"),
+    INPUT_NUMBER_OF_TRY("시도할 횟수는 몇회인가요?\n"),
     RACE_RESULT("\n실행 결과\n"),
     FINAL_WINNER("최종 우승자 : ");
 
