ff3d5913e47ed9e3cfc494e5b3f510edbdb9a689
lynjun97 <lynjun97@naver.com>
2023-10-30 15:54:12 +0900

    Feat: OutputView 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import java.util.Set;
+
+public class OutputView {
+
+    public void winnerOutView(Set<String> list){
+        System.out.println("최종 우승자 : " + String.join(",", list));
+    }
+
+    public void carNameOutView(String car){
+        System.out.print(car + " : ");
+    }
+
+    public void score(){
+        System.out.print("-");
+    }
+
+    public void newLine(){
+        System.out.println();
+    }
+}
