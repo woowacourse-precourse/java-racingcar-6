768a06394f85b00429f29b6fb23593b4363ba2bd
Jaeho Kim <fldzm7905@gmail.com>
2023-10-31 01:25:23 +0900

    feat: 자동차 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
-
-    private String[] generate() {
-        return null;
+    public List<String> generate(String carName) {
+        List<String> cars = new ArrayList<>();
+        cars = List.of(carName.split(","));
+        return cars;
     }
 }
