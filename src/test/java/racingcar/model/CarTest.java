6326279a72fb7341d7dc44c8f8a6e9a5f5b5d41d
jmj005 <jmj00005@naver.com>
2023-10-31 03:13:48 +0900

    test: 자동차 이동 성공 테스트 추가

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CarTest {
+    private static Car car;
+
+    @BeforeAll
+    static void setUp() {
+        car = new Car("TestCar", 0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 1",
+            "6, 2",
+            "9, 3"
+    })
+    void 자동차_이동_성공_테스트(int randomNumber, int expectedDistance) {
+        car.move(randomNumber);
+        assertEquals(expectedDistance, car.getDistance());
+    }
+}
