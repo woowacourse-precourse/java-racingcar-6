8f920b8bcffa0ca214d118b3dcfbbece6edea683
sequaa <asl06194@gmail.com>
2023-10-31 01:32:36 +0900

    feat: Application 입력 구현 사용자로부터 필요한 자동차 이름과 시도 횟수를 입력 받는 기능을 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String inputNames = Console.readLine();
+        List<String> carNames = Arrays.asList(inputNames.split(","));
+//      List<String> carNames = Arrays.asList(Console.readLine().split(","));
+
+        int tryCount = Integer.parseInt(Console.readLine());
+
+
     }
 }
