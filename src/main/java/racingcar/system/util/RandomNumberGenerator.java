44072b7ec998856e458306d213f89af12c9181d5
Yeriimii <seojs0511@naver.com>
2023-10-31 01:49:51 +0900

    refactor(RandomNumberGenerator): static method 에서 일반 메서드로 변경

--- src/main/java/racingcar/system/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/system/util/RandomNumberGenerator.java
@@ -6,8 +6,7 @@ public class RandomNumberGenerator {
     public static final int START_NUMBER = 0;
     public static final int END_NUMBER = 9;
 
-    public static int generate() {
+    public int generate() {
         return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
-
 }
