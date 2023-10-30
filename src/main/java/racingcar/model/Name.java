a75ffccddbfbd2f14349e72862e52b49a0441357
HYUNJUN\sajun <sajun28@naver.com>
2023-10-31 01:12:31 +0900

    Refactor: "google java code 스타일로 수정"

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,18 +1,14 @@
 package racingcar.model;
 
-import org.mockito.internal.configuration.FieldAnnotationProcessor;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class Name {
 
     public static final int CAR_NAME_LENGTH = 5;
+
     public void isValid(List<String> playerNames) {
-        for (Object playerName : playerNames){
-            if (playerName.toString().length() > CAR_NAME_LENGTH){
+        for (String playerName : playerNames) {
+            if (playerName.length() > CAR_NAME_LENGTH) {
                 throw new IllegalArgumentException();
             }
         }
