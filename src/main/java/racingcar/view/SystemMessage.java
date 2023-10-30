66557c5be2ff09e48635023f539d277a5434bf0d
KangDeok <rkdejr2321@naver.com>
2023-10-30 15:32:05 +0900

    [feat] 메세지 출력 기능

---
+++ src/main/java/racingcar/view/SystemMessage.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.RacingCar;
+
+public class SystemMessage {
+
+    public void printRequestInputMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printRequestTryCountMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printWinnerMessage(List<String> winners) {
+        String winnerMessage = String.join(",", winners);
+
+        System.out.println("최종 우승자 : " + winnerMessage);
+    }
+}
