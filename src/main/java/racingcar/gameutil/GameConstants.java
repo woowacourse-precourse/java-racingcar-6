44186de7477e7ee4f8d9624657221bfba0319dc1
leedohyun <lucky77795@naver.com>
2023-10-30 22:16:45 +0900

    refactor(racingCar) : 매직넘버 포장 추가.

--- src/main/java/racingcar/gameutil/GameConstants.java
+++ src/main/java/racingcar/gameutil/GameConstants.java
@@ -6,4 +6,6 @@ public class GameConstants {
     public static final int RANDOM_NUMBER_MAX_RANGE = 9;
     public static final int MOVE_DISTANCE = 1;
     public static final int CAN_MOVE_MIN_VALUE = 4;
+    public static final int CAR_SIZE_MIN = 2;
+    public static final int CAR_NAME_MAX_LEN = 5;
 }
