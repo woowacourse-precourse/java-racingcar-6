0170f74ef2dcecf0ca3afd7ca42e2e8a44bc25bf
pwrwpw <dlgustjr9912@gmail.com>
2023-10-30 23:32:31 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/test/java/racingcar/TryCountTest.java
+++ src/test/java/racingcar/TryCountTest.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
