ac5e19e9036c913a22269e667b85263906561ef9
sangchu <didi1484@gmail.com>
2023-10-30 18:05:27 +0900

    feat: 잘못된 횟수 입력시 예외 발생시키는 validateNumber 메서드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,8 @@ import static racingcar.view.constants.Notice.ASK_ATTEMPT_NUMBER;
 import static racingcar.view.constants.Notice.ASK_CAR_NAME;
 
 public class InputView {
+    private static int attemptNumber;
+
     public static String askCarName() {
         System.out.println(ASK_CAR_NAME);
         return readLine();
@@ -12,6 +14,14 @@ public class InputView {
 
     public static int askAttemptNumber() {
         System.out.println(ASK_ATTEMPT_NUMBER);
-        return Integer.parseInt(readLine());
+        attemptNumber = Integer.parseInt(readLine());
+        validateNumber();
+        return attemptNumber;
+    }
+
+    private static void validateNumber() {
+        if (attemptNumber < 1) {
+            throw new IllegalArgumentException("입력하신 횟수가 올바르지 않습니다.");
+        }
     }
 }
