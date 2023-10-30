3e42f34dfbef28b5a27346969c5b6b376d3285db
kimwanyoung <dhks2869@gmail.com>
2023-10-30 16:08:39 +0900

    test : Attempts 문자 입력 예외 테스트

--- src/test/java/model/AttemptsTest.java
+++ src/test/java/model/AttemptsTest.java
@@ -19,4 +19,17 @@ public class AttemptsTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("공백 혹은 빈");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b", "c"})
+    @DisplayName("시도 횟수 문자 들어왔을 때 예외 테스트")
+    public void 시도_횟수_문자_입력_예외_테스트(String input) {
+        //given
+        String attempts = input;
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> new Attempts(attempts))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만");
+    }
 }
