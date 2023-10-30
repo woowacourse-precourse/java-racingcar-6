1a70ab59b98b2b02e721e7c4d38d32793c2c0a5a
maark1106 <mark1106@naver.com>
2023-10-30 20:21:07 +0900

    test : 자동차 전진 숫자중 최댓값 찾는 테스트 코드 작성

--- src/test/java/racingcar/controller/RacingCarGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarGameControllerTest.java
@@ -3,7 +3,10 @@ package racingcar.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 
 class RacingCarGameControllerTest {
@@ -16,5 +19,19 @@ class RacingCarGameControllerTest {
         assertThat(carNameList).contains("벤츠", "제네시스", "소나타");
     }
 
+    @Test
+    void 자동차들_전진_숫자_중_최댓값_찾는_기능() {
+        Cars cars = new Cars();
+        Map<String, Car> carMap = cars.getCars();
+        Car car1 = new Car("벤츠");
+        car1.setAdvanceCount(3);
+        Car car2 = new Car("제네시스");
+        car2.setAdvanceCount(5);
 
+        carMap.put("벤츠", car1);
+        carMap.put("제네시스", car2);
+        int maxAdvanceCount = racingCarGameController.getMaxAdvanceCount(carMap);
+
+        assertThat(maxAdvanceCount).isEqualTo(5);
+    }
 }
