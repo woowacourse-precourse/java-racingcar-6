36578bf251b191290828f7a23efc41e79fb46a0b
shkisme <shkisme0130@gmail.com>
2023-10-30 20:26:05 +0900

    feat: 자동차 리스트 예외처리 기능 구현
    자동차 리스트가 비어있을 경우, 자동차의 이름이 중복인 경우를 예외처리하는 기능을 구현했습니다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,12 +1,16 @@
 package racingcar.model;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.model.car.Car;
 import racingcar.model.car.Name;
 import racingcar.model.car.Position;
 
 public class Cars {
+    private static final String DUPLICATE_CAR_MESSAGE = "중복된 이름의 자동차가 존재합니다.";
+    private static final String EMPTY_CAR_MESSAGE = "자동차 리스트가 비었습니다.";
 
     private final List<Car> cars;
 
@@ -16,7 +20,25 @@ public class Cars {
 
     public static Cars from(String carNames) {
         List<Car> cars = generateCars(carNames);
+        validateEmpty(cars);
+        validateDuplicate(cars);
         return new Cars(cars);
+    }
+
+    private static void validateEmpty(List<Car> cars) {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_CAR_MESSAGE);
+        }
+    }
+
+    private static void validateDuplicate(List<Car> cars) {
+        List<Name> carNames = cars.stream()
+                .map(Car::name)
+                .toList();
+        Set<Name> nonDuplicateCarNames = new HashSet<>(carNames);
+        if (nonDuplicateCarNames.size() != carNames.size()) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_MESSAGE + carNames);
+        }
     }
 
     private static List<Car> generateCars(String carNames) {
