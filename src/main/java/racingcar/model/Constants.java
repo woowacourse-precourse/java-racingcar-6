83260208d894933ba5ccc9348e7f9a99ffc59384
geunsu <geunsu23@naver.com>
2023-10-30 14:10:52 +0900

    add first_repeat constant

--- src/main/java/racingcar/model/Constants.java
+++ src/main/java/racingcar/model/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int IS_GO_FORWARD = 4;
     public static final int MAX_RANDOM_NUMBER = 9;
     public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int FIRST_REPEAT = 0;
 }
