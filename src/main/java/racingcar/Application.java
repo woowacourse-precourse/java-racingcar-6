541e21fe980ec47fbc7a8e24d0991e1e351541c9
Isaac Han <isaachan233@gmail.com>
2023-10-30 10:55:11 +0900

    refactor: 레이싱 게임이 심판한테 의존하게 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,11 @@
 package racingcar;
 
 import racingcar.controller.RacingGame;
+import racingcar.service.Referee;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
+        RacingGame racingGame = new RacingGame(new Referee());
         racingGame.run();
     }
 }
