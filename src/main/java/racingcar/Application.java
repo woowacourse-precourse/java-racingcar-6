03924f66e16b5f9e5784ec65853efefd0e26923e
Shin Changyu <scv1702@naver.com>
2023-10-30 17:09:32 +0900

    Feat: 애플리케이션 설정 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controllers.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        RaceController raceController = applicationConfig.raceController();
+        raceController.progress();
     }
 }
