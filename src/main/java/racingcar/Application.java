2044cedb83bf32f1dbf3d8ef650b6f0e9f5e0d35
unknown <yschoi0119@naver.com>
2023-10-30 19:34:08 +0900

    사용자 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
+    private static final int MIN_CAR_NAME_LENGTH = 1;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application game = new Application();
+        game.start();
+    }
+
+    private void start() {
+        String inputNames = getUserInput("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)로 구분):");
+        String[] carNames = validateCarNames(inputNames);
+
+        int tryCount = Integer.parseInt(getUserInput("시도할 회수는 몇 회인가요?"));
+        validateTryCount(tryCount);
+
+        Race race = new Race(carNames, tryCount);
+        race.startRace();
+    }
+
+    private String getUserInput(String prompt) {
+        System.out.println(prompt);
+        return Console.readLine();
     }
 }
