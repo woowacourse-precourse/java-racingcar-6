1329887e0f8706a2571dbe621fd29643e39750d4
aaa <cnj991279@gmail.com>
2023-10-30 19:56:22 +0900

    feat: run 메서드 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -10,6 +10,14 @@ import java.util.List;
 public class Race {
     Core core = new Core();
 
+    public void run() {
+        List<String> carNames = getCarNames();
+        int count = core.getCount();
+        List<Car> cars = core.getCarListByCarNames(carNames);
+        showResult(cars, count);
+        printWinner(core.getWinner(cars));
+    }
+
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
@@ -29,6 +37,5 @@ public class Race {
 
     public void printWinner(List<String> winner) {
         System.out.println("최종 우승자 : " + String.join(", ", winner));
-        
     }
 }
