e71278044d0213764a3d2d7a168327efc5a14529
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-30 20:50:25 +0900

    feat: 특수 기호를 가진 Enum 추가

---
+++ src/main/java/racingcar/model/SpecialSign.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+public enum SpecialSign {
+    SEPARATOR(","),
+    COLON(":"),
+    HYPHEN("-"),
+    NEW_LIEN("\n"),
+    SPACE(" ");
+
+    private final String sign;
+
+    SpecialSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getSign() {
+        return this.sign;
+    }
+
+    public static String drawHyphen(int distance) {
+        String drawDistance = "";
+        for (int i = 0; i < distance; i++) {
+            drawDistance += HYPHEN.getSign();
+        }
+        return drawDistance + NEW_LIEN.getSign();
+    }
+}
