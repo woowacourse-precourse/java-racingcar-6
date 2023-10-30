ebb53c100b253276b013704dd3ea9b9e7bf362c5
jjaegii <hn06038@gmail.com>
2023-10-30 21:31:54 +0900

    feat: RaceManager 클래스 내 getWinners 메소드 추가

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RaceManager {
@@ -18,5 +19,23 @@ public class RaceManager {
             }
             // 각 라운드 결과 출력 예정
         }
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = -1;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
     }
 }
