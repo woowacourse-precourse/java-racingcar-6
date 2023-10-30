61b2e72d4c94aeb873b072afa2d1087f7b26853b
rjsdn9025 <rjsdn9025@naver.com>
2023-10-30 22:41:20 +0900

    refactor : 정적 메소드로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,7 +8,7 @@ public class OutputView {
     public static void printExecutionResult() {
         System.out.println("실행 결과");
     }
-    public void printCarScore(List<Car> cars) {
+    public static void printCarScore(List<Car> cars) {
         for (Car car : cars) {
             System.out.print(car.getName() + " : ");
             printPosition(car);
@@ -16,7 +16,7 @@ public class OutputView {
         }
     }
 
-    public void printWinners(List<String> carNames) {
+    public static void printWinners(List<String> carNames) {
         System.out.print("최종 우승자 : ");
         for (int i = 0; i < carNames.size(); i++) {
             if (i >= 1) {
@@ -27,7 +27,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printPosition(Car car) {
+    public static void printPosition(Car car) {
         for(int i = 0; i < car.getPosition(); i++) {
             System.out.print("-");
         }
