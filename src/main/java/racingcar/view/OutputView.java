2b12d8fec5ea9e5fe69161a20ad99261c1ac3799
Dreaming-J <kimjjong321@naver.com>
2023-10-31 01:59:03 +0900

    :sparkles: feat. 최종 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,17 @@
 package racingcar.view;
 
+import racingcar.model.RaceResult;
+
 public class OutputView {
 
     private static final String ROUND_PROGRESS = "실행 결과\n";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
 
     public void printRoundResult(String roundResult) {
         System.out.println(ROUND_PROGRESS + roundResult);
+    }
+
+    public void printResult(RaceResult result) {
+        System.out.println(FINAL_WINNER + result.toString());
     }
 }
