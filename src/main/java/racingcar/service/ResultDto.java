a10936b935aeac0ac939fde1e1341ebc9fcc26eb
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 10:49:11 +0900

    feat(ResultDto): 움직임 횟수 getter

--- src/main/java/racingcar/service/ResultDto.java
+++ src/main/java/racingcar/service/ResultDto.java
@@ -3,12 +3,19 @@ package racingcar.service;
 // TODO: record로 변경 가능한 듯 함. 뭔지 자료조사하고, 적용가능하면 적용해볼 것
 public class ResultDto {
     private final String name;
+    private final Long totalMoveCount;
 
     public ResultDto(String name, Long totalMoveCount) {
         this.name = name;
+        this.totalMoveCount = totalMoveCount;
     }
 
     public String getName() {
         return name;
     }
+
+    public Long getTotalMoveCount() {
+        return totalMoveCount;
+    }
+
 }
