b8741dfcf694c9059bbeca1f8e71cc3b270c1e6a
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 16:19:53 +0900

    refactor(CarNamesInputValidator): 자동차 이름 입력과 관련한 예외 메시지는 클래스 내부에서 관리하도록 변경

--- src/main/java/racingcar/utils/CarNamesInputValidator.java
+++ src/main/java/racingcar/utils/CarNamesInputValidator.java
@@ -19,26 +19,26 @@ public class CarNamesInputValidator {
 
     private static void validateIsNotBlank(String target) {
         if (target.isBlank()) {
-            throw new IllegalArgumentException(ExceptionMessage.BLANK.getError());
+            throw new IllegalArgumentException(CarNamesInputExceptionMessage.BLANK.getError());
         }
     }
 
     private static void validateFirstCharacterIsNotComma(String target) {
         if (target.charAt(0) == COMMA) {
-            throw new IllegalArgumentException(ExceptionMessage.FIRST_CHARACTER_COMMA.getError());
+            throw new IllegalArgumentException(CarNamesInputExceptionMessage.FIRST_CHARACTER_COMMA.getError());
         }
     }
 
     private static void validateLastCharacterIsNotComma(String target) {
         if (target.charAt(target.length() - 1) == COMMA) {
-            throw new IllegalArgumentException(ExceptionMessage.LAST_CHARACTER_COMMA.getError());
+            throw new IllegalArgumentException(CarNamesInputExceptionMessage.LAST_CHARACTER_COMMA.getError());
         }
     }
 
     private static void validateEachElementIsNotBlank(String[] targets) {
         for (String target : targets) {
             if (target.isBlank()) {
-                throw new IllegalArgumentException(ExceptionMessage.ELEMENT_BLANK.getError());
+                throw new IllegalArgumentException(CarNamesInputExceptionMessage.ELEMENT_BLANK.getError());
             }
         }
     }
@@ -46,8 +46,26 @@ public class CarNamesInputValidator {
     private static void validateEachElementCorrectLength(String[] targets) {
         for (String target : targets) {
             if (target.length() > VALID_MAX_LENGTH) {
-                throw new IllegalArgumentException(ExceptionMessage.OUT_OF_LENGTH.getError());
+                throw new IllegalArgumentException(CarNamesInputExceptionMessage.OUT_OF_LENGTH.getError());
             }
+        }
+    }
+
+    enum CarNamesInputExceptionMessage {
+        BLANK("공백 입력을 해선 안됩니다."),
+        FIRST_CHARACTER_COMMA("첫 번째 문자로 콤마(,)를 입력하면 안됩니다."),
+        LAST_CHARACTER_COMMA("마지막 문자로 콤마(,)를 입력하면 안됩니다."),
+        ELEMENT_BLANK("자동차 이름은 공백 문자로만 이루어져선 안됩니다."),
+        OUT_OF_LENGTH("자동차 이름은 5글자 이하여야 합니다.");
+
+        private final String error;
+
+        CarNamesInputExceptionMessage(String error) {
+            this.error = error;
+        }
+
+        String getError() {
+            return error;
         }
     }
 }
