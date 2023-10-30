c1d6e3bf711117c55f86f68655b692e5d7163154
Jay <injaesong0@gmail.com>
2023-10-30 18:13:41 +0900

    feat: implement proceeding logic of cars

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
-    private final int displacement;
+    private int displacement;
 
     public Car(String name, int displacement) {
         this.name = name;
         this.displacement = displacement;
+    }
+
+    public void conductAction() {
+        if (isAbleToProceed()) {
+            proceed();
+        }
+    }
+
+    private boolean isAbleToProceed() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber > 3;
+    }
+
+    private void proceed() {
+        displacement++;
     }
 }
