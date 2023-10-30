52ffb5b8aa4b5e71f931b1a09424c856e7872252
pgd <rudeh1253@gmail.com>
2023-10-30 15:24:33 +0900

    feat: 입력과 관련된 기능을 모아주는 클래스 구현

---
+++ src/main/java/racingcar/input/InputAdaptor.java
@@ -0,0 +1,28 @@
+package racingcar.input;
+
+import racingcar.validation.Validation;
+
+public class InputAdaptor {
+    private final InputReceiver inputReceiver;
+    private final Parser parser;
+
+    public InputAdaptor(InputReceiver inputReceiver, Parser parser) {
+        this.inputReceiver = inputReceiver;
+        this.parser = parser;
+    }
+
+    public String[] readCarNames() {
+        String carNamesInRaw = inputReceiver.readCarNames();
+        String[] carNames = parser.parseCarNames(carNamesInRaw);
+        Validation.checkCarNames(carNames);
+        return carNames;
+    }
+
+    public int readNumOfTries() {
+        String numOfTriesInRaw = inputReceiver.readNumOfTries();
+        Validation.checkIfInteger(numOfTriesInRaw);
+        int numOfTries = parser.parseInt(numOfTriesInRaw);
+        Validation.checkIfNonNegative(numOfTries);
+        return numOfTries;
+    }
+}
