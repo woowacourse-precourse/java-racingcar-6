8c3a3a83c3ce1290b3765bbdd648b89e972ea3ac
jyajoo <jyajoo1020@gmail.com>
2023-10-30 15:29:08 +0900

    style: 필드명 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,20 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> carList;
 
     public Cars(List<String> carNames) {
-        cars = carNames.stream().map(Car::new).toList();
+        carList = carNames.stream().map(Car::new).toList();
     }
 
     public void run() {
-        for (Car car : cars) {
+        for (Car car : carList) {
             int randomNumber = Randoms.pickNumberInRange(0, 9);
             car.run(randomNumber);
         }
     }
 
     public List<Car> getCars() {
-        return cars;
+        return carList;
     }
 }
