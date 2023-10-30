a2ce77646f5e9a8082e8c024ffc6aa9e0919fa82
혜민 <alddlv81@gmail.com>
2023-10-30 13:36:50 +0900

    feat: 자동차 최대 이동 횟수의 이름 파악

--- src/main/java/racingcar/game/ResultGame.java
+++ src/main/java/racingcar/game/ResultGame.java
@@ -6,10 +6,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ResultGame {
+    public static List<String> checkWinner(List<CarInfo> carList) {
+        int maxMoveCount = getMaxMoveCount(carList);
+        return getWinners(carList, maxMoveCount);
+    }
+
     private static int getMaxMoveCount(List<CarInfo> carList) {
         return carList.stream()
                 .mapToInt(CarInfo::getMoveCount)
                 .max()
                 .orElse(0);
+    }
+
+    private static List<String> getWinners(List<CarInfo> carList, int maxMoveCount) {
+        return carList.stream()
+                .filter(car -> car.getMoveCount() == maxMoveCount)
+                .map(CarInfo::getName)
+                .collect(Collectors.toList());
     }
 }
