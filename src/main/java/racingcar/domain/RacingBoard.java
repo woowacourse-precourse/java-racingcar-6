6e31aa9fece76093da16806366d24d5adad6ba0f
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-31 02:55:12 +0900

    feat: RacingBoard 승자 기록 기능 추가
    - RacingBoard에 recordWinner(), viewWinner() 기능 추가
    - 기능 추가에 따른 일부 수정

--- src/main/java/racingcar/domain/RacingBoard.java
+++ src/main/java/racingcar/domain/RacingBoard.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class RacingBoard {
     private ArrayList<Car> board = new ArrayList<>();
+
+    private ArrayList<String> winnerBoard = new ArrayList<>();
 
     public void join(int num, Car car) {
         board.set(num, car);
@@ -15,5 +18,22 @@ public class RacingBoard {
 
     public ArrayList<Car> view() {
         return board;
+    }
+
+    public void recordWinner(ArrayList<String> winnerBoard) {
+        this.winnerBoard = winnerBoard;
+    }
+
+    public String viewWinner() {
+        String result;
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i < winnerBoard.size(); i++) {
+            temp.append(winnerBoard.get(i));
+            if (i < (winnerBoard.size() - 1)) {
+                temp.append(", ");
+            }
+        }
+        result = temp.toString();
+        return result;
     }
 }
