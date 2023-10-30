7bfacc0d424c51cfe8d29bbf90f0f333f71e6387
junest66 <chlwndks333@gmail.com>
2023-10-31 01:12:08 +0900

    refactor(OutputViewTest): 게임 객체 생성 방법 변경으로 인한 테스트 수정

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.domain.Game;
 import racingcar.domain.TestNumberGenerator;
 
@@ -30,8 +31,12 @@ class OutputViewTest {
 
     @Test
     void 자동차_모두_전진_결과_출력테스트() {
-        String names = "pobi,woni,jun";
-        Game game = new Game(names, new TestNumberGenerator(5));
+        List<Car> carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(7)),
+                new Car("woni", new TestNumberGenerator(8)),
+                new Car("jun", new TestNumberGenerator(9))
+        );
+        Game game = new Game(carList);
         game.playOneRound();
         OutputView.printResult(game.getCars());
 
@@ -41,8 +46,12 @@ class OutputViewTest {
 
     @Test
     void 자동차_모두_멈춤_결과_출력테스트() {
-        String names = "pobi,woni,jun";
-        Game game = new Game(names, new TestNumberGenerator(3));
+        List<Car> carList = Arrays.asList(
+                new Car("pobi", new TestNumberGenerator(1)),
+                new Car("woni", new TestNumberGenerator(2)),
+                new Car("jun", new TestNumberGenerator(3))
+        );
+        Game game = new Game(carList);
         game.playOneRound();
         OutputView.printResult(game.getCars());
 
