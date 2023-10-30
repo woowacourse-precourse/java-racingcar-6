b5adfcb24d0b4390aa34f8b3ab61c8779ba5ddd3
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-30 17:36:12 +0900

    style: RacingcarController 클래스의 함수들 접근 제한자를 public 으로 변경

--- src/main/java/racingcar/RacingcarController.java
+++ src/main/java/racingcar/RacingcarController.java
@@ -9,15 +9,15 @@ public class RacingcarController {
         addCars(splitCarsName(carsName));
     }
 
-    private String inputCarsName() {
+    public String inputCarsName() {
         return input.inputCarName();
     }
 
-    private String[] splitCarsName(String carsName) {
+    public String[] splitCarsName(String carsName) {
         return carsName.split(",");
     }
 
-    private void addCars(String[] CarsName) {
+    public void addCars(String[] CarsName) {
         for (String name: CarsName) {
             Car car = new Car(name.trim(), 0);
             cars.addCarList(car);
