89ef46b3409b2ee109a12820e24ba6d7967e4691
zangsu <zangsu_@naver.com>
2023-10-30 20:57:37 +0900

    feat : create first-class-collection PositionMap

---
+++ src/main/java/racingcar/firstclasscollection/PositionMap.java
@@ -0,0 +1,19 @@
+package racingcar.firstclasscollection;
+
+import java.util.HashMap;
+import java.util.Map;
+import racingcar.model.Car;
+
+public class PositionMap {
+    private Map<Car, Integer> positionMap;
+
+    public PositionMap() {
+        positionMap = new HashMap<>();
+    }
+
+    // TODO : void addCar(Car car)
+
+    // TODO : void moveCar(Car car)
+
+    // TODO : List<Car> getWinner()
+}
