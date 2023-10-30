69e025508c853eea51fdd49bed3cc8e9e0e40a88
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 17:44:16 +0900

    [feat] : add carName class with validate car name method

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class CarName {
+    private String name;
+
+    public CarName(String carName) {
+        validateCarName(carName);
+    }
+
+    public void validateCarName(String carName) {
+
+    }
+}
