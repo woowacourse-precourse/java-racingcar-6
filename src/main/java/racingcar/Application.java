eb7aa1ad9fc84ec036442dd03b45ed8e92ef4bd8
jongmin Lee <rjsqktod1@gmail.com>
2023-10-30 18:10:40 +0900

    refactor(main): remove unnecessary try-catch block
    delete the try-catch block in application main
    this block redundant with test
    
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,12 +4,8 @@ import racingcar.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            // TODO: 프로그램 구현
-            GameController gameController = new GameController();
-            gameController.Init();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.Init();
     }
 }
