b42264040f76390246d2c82d8f24180c40dc908f
june-777 <wlwhswnsrl96@gmail.com>
2023-10-30 20:11:58 +0900

    refactor(AttemptCountsInputValidator): 시도 횟수 검증 기능 수정

--- src/main/java/racingcar/utils/AttemptCountsInputValidator.java
+++ src/main/java/racingcar/utils/AttemptCountsInputValidator.java
@@ -5,10 +5,13 @@ import java.util.regex.Pattern;
 public class AttemptCountsInputValidator {
 
     private static final Pattern NUMERIC_REGEX = Pattern.compile("^(0|[-]?[1-9]\\d*)$");
+    private static final int MIN_ATTEMPT_COUNT = 1;
+    private static final int MAX_ATTEMPT_COUNT = 10;
+
 
     public static void validate(String target) {
         validateNumeric(target);
-        validateIntegerRange(target);
+        validateInteger(target);
         validateRange(target);
     }
 
@@ -18,25 +21,25 @@ public class AttemptCountsInputValidator {
         }
     }
 
-    private static void validateIntegerRange(String target) {
+    private static void validateInteger(String target) {
         try {
             Integer.parseInt(target);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.OUT_OF_INTEGER_RANGE.getError());
+            throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.NOT_INTEGER.getError());
         }
     }
 
     private static void validateRange(String target) {
         int attemptCounts = Integer.parseInt(target);
-        if (attemptCounts < 1) {
+        if (attemptCounts < MIN_ATTEMPT_COUNT || attemptCounts > MAX_ATTEMPT_COUNT) {
             throw new IllegalArgumentException(AttemptCountsInputExceptionMessage.OUT_OF_RANGE.getError());
         }
     }
 
     enum AttemptCountsInputExceptionMessage {
         NOT_NUMERIC("숫자만 입력해야 합니다."),
-        OUT_OF_INTEGER_RANGE("int 범위를 벗어났습니다."),
-        OUT_OF_RANGE("1회 ~ 2147483647회 만 입력 가능합니다.");
+        NOT_INTEGER("정수 범위 숫자만 입력해야 합니다."),
+        OUT_OF_RANGE(String.format("%d ~ %d 사이의 수만 입력 가능합니다.", MIN_ATTEMPT_COUNT, MAX_ATTEMPT_COUNT));
 
         private final String error;
 
