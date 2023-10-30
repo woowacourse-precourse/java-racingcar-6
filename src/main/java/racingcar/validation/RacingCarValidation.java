40aea1f2179124d1e1bce17a676598f25866ce0f
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-31 04:12:11 +0900

    refactor: 검증이 완료되면 count를 반환하도록 수정

--- src/main/java/racingcar/validation/RacingCarValidation.java
+++ src/main/java/racingcar/validation/RacingCarValidation.java
@@ -9,9 +9,11 @@ public class RacingCarValidation {
             throw new IllegalArgumentException("해당 이름은 크키가 5 초과입니다.");
     }
 
-    public void illegalValidation(int count){
+    public int illegalValidation(int count){
         if(count < 1 || count > 10000)
             throw new IllegalArgumentException("해당 값은 범위 내에서 실행할 수 없는 값입니다. (1 ~ 10000)");
+
+        return count;
     }
 
     public void checkDuplicateName(CarList carList, String name){
