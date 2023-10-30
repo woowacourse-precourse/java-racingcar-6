39cf839d9b54ada8112dbc81bcda0199cba45208
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 22:31:36 +0900

    feat: 중복된 이름 검증

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -7,6 +7,7 @@ import static racingcar.util.Constants.CAR_NAME_MAX_LENGTH;
 import static racingcar.util.Constants.SPLIT;
 import static racingcar.util.Constants.INIT;
 import static racingcar.util.VerificationMessage.OVER_SIZE;
+import static racingcar.util.VerificationMessage.DUPLICATE_NAME;
 
 public class Car {
     private final Map<String,Integer> carMap;
@@ -18,12 +19,16 @@ public class Car {
         Map<String,Integer> carMap = new HashMap<>();
         for (String car : carArr) {
             if(!checkNameLength(car)) throw new IllegalArgumentException(OVER_SIZE);
+            if(duplicateName(car,carMap)) throw new IllegalArgumentException(DUPLICATE_NAME);
             carMap.put(car,INIT);
         }
         return carMap;
     }
     private boolean checkNameLength(String car){
         return car.length() <= CAR_NAME_MAX_LENGTH;
+    }
+    private boolean duplicateName(String car, Map<String,Integer> carMap) {
+        return carMap.containsKey(car);
     }
     public Map<String,Integer> getCarMap() {
         return this.carMap;
