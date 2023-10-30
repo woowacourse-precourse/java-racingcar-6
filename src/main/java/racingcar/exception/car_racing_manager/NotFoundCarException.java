162e49e163c15d9846de3502300992dd88af3d38
eom-tae-in <eti0728@daum.net>
2023-10-30 22:04:57 +0900

    remove: 불필요해진 클래스 제거

--- src/main/java/racingcar/exception/car_racing_manager/NotFoundCarException.java
+++
@@ -1,8 +0,0 @@
-package racingcar.exception.car_racing_manager;
-
-public class NotFoundCarException extends IllegalArgumentException {
-
-    public NotFoundCarException(final String carName) {
-        super(carName + "을(를) 찾을 수 없습니다.");
-    }
-}
--- src/main/java/racingcar/model/RacingTrackGenerator.java
+++
@@ -1,8 +0,0 @@
-package racingcar.model;
-
-import java.util.List;
-
-public interface RacingTrackGenerator<T extends Vehicle> {
-
-    RacingGame create(List<T> vehicles, int playCount);
-}
