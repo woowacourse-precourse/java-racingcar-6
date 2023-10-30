106b9cbbde8c440bddeb6ebebe2c247f7623c374
oxdjww <oxdjww@gmail.com>
2023-10-30 22:45:47 +0900

    feat : 게임 승리조건 상수화하여 추가 (기본값 4)

--- src/main/java/racingcar/model/RacingCarGameConfig.java
+++ src/main/java/racingcar/model/RacingCarGameConfig.java
@@ -6,4 +6,5 @@ public class RacingCarGameConfig {
   public static final Integer RANDOM_MINIMUM_VALUE = 0;
   public static final Integer RANDOM_MAXIMUM_VALUE = 9;
 
+  public static final Integer GAME_WIN_CONDITION = 4;
 }
