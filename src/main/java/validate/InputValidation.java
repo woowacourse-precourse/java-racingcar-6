f38725ba6228c84ab85a304a765030400dea0faa
pjaehyun <show6756@gmail.com>
2023-10-30 20:10:44 +0900

    feat: 추가 split 후 빈배열일 때 예외 처리 함수

--- src/main/java/validate/InputValidation.java
+++ src/main/java/validate/InputValidation.java
@@ -1,5 +1,7 @@
 package validate;
 
+import java.util.List;
+
 public class InputValidation {
 
     public static void validateNumericInput(String input) {
@@ -23,6 +25,12 @@ public class InputValidation {
     public static void validateInputDoesNotContainSpecialCharacters(String name) {
         if (name.matches(".*[ !@#$%^&*(),.?\":{}|<>].*")) {
             throw new IllegalArgumentException("자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.");
+        }
+    }
+
+    public static void validateListIsEmpty(String[] list) {
+        if (list.length == 0) {
+            throw new IllegalArgumentException("입력된 자동차가 없습니다.");
         }
     }
 }
