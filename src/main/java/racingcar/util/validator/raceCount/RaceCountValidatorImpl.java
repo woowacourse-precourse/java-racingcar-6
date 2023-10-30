86dedd1e76bb69bc3ebf4705028d21e096272b7f
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-30 23:34:04 +0900

    feat(RaceCountValidatorImpl): validate() 로직 변경
    - 정규표현식을 이용한 유효성 검사로 로직 수정

--- src/main/java/racingcar/util/validator/raceCount/RaceCountValidatorImpl.java
+++ src/main/java/racingcar/util/validator/raceCount/RaceCountValidatorImpl.java
@@ -3,18 +3,11 @@ package racingcar.util.validator.raceCount;
 public class RaceCountValidatorImpl implements RaceCountValidator {
     @Override
     public boolean validateRaceCount(String input) {
-        return isNonNegative( parseInt(input) );
+        return isMatch(input);
     }
 
-    private int parseInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch(NumberFormatException e) {
-            return -1;
-        }
-    }
-
-    private boolean isNonNegative(int number) {
-        return number >= 0;
+    private boolean isMatch(String input) {
+        String regex = "^[0-9]+$";
+        return input.matches(regex);
     }
 }
