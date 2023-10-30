8b8e9225ed64f59f6325f6a1e749efbf979251c3
hjeongb0320 <hjeongb0320@naver.com>
2023-10-30 16:26:22 +0900

    fix(src/main/java/service) : setWinnersList 함수 기능 분리
    한 메소드에서 여러 기능을 가지지 않게하고,
    어떤 기능을 수행하는 지 직관적으로 나타내기 위해 분리

--- src/main/java/service/GameResult.java
+++ src/main/java/service/GameResult.java
@@ -16,22 +16,33 @@ public class GameResult {
     }
 
     public void setWinnersList() {
-        List<Map.Entry<String, String>> carIdxNamePosList = Controller.gameManager.getCarIdxNamePosList();
-        carIdxNamePosList.sort(Comparator.comparing(e -> e.getValue().length(),
-                Comparator.reverseOrder()));
+        List<Map.Entry<String, String>> carIdxNamePosList = sortCarIdxNamePosListByPosLen();
 
-        int longestMove = carIdxNamePosList.get(0).getValue().length();
+        int longestPos = setLongestPos(carIdxNamePosList);
 
+        // 길이가 가장 긴 pos와 같은 car들이 winners에 해당한다. (winner가 여려명일 수 있기 때문이다.)
         for (Map.Entry<String, String> car : carIdxNamePosList) {
             String name = car.getKey();
             String position = car.getValue();
 
-            if (position.length() != longestMove) {
+            if (position.length() != longestPos) {
                 break;
             }
 
             winnersList.add(name);
         }
+    }
+
+    private List<Map.Entry<String, String>> sortCarIdxNamePosListByPosLen() {
+        List<Map.Entry<String, String>> carIdxNamePosList = Controller.gameManager.getCarIdxNamePosList();
+        carIdxNamePosList.sort(Comparator.comparing(e -> e.getValue().length(),
+                Comparator.reverseOrder()));
+
+        return carIdxNamePosList;
+    }
+
+    private int setLongestPos(List<Map.Entry<String, String>> carIdxNamePosList) {
+        return carIdxNamePosList.get(0).getValue().length();
     }
 
     public void cleanup() {
