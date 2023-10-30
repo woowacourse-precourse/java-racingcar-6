bdeb681a3d4437f87a125887117be6ce791c8b23
jae woo Jung <awfjol2008@naver.com>
2023-10-30 21:19:37 +0900

    feat: Distance 클래스 생성자 추가 및 protected 키워드로 접근 제한

--- src/test/java/racingcar/domain/car/Distance.java
+++ src/test/java/racingcar/domain/car/Distance.java
@@ -3,8 +3,8 @@ package racingcar.domain.car;
 public class Distance {
     private int distance = 0;
 
-    public Distance(int distance) {
-        this.distance = distance;
+    protected Distance() {
+
     }
 
     public void add(int number) {
@@ -19,3 +19,9 @@ public class Distance {
         return "-".repeat(distance);
     }
 }
+
+/*
+
+부분부분 trouble shooting이나 설계하면서 고민했던 부분들을 짧게 노션에 메모해서 나중에 소감문이나 블로그에 포스팅해보자... 허허
+
+ */
