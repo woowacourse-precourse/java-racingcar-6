091fb0ce34e55a5f395a1a5c3f47eca286579ab5
dltmddus <tmddus4671@gmail.com>
2023-10-31 01:10:54 +0900

    fix: Map 오류 수정

--- src/main/java/racingcarGame/Game.java
+++ src/main/java/racingcarGame/Game.java
@@ -8,13 +8,8 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
-    List<String> carNames = new ArrayList<>();
-    Set<String> carNameSet = new HashSet<>();
     private Map<String, Integer> carData = new HashMap<>();
     int carNumber;
-    public Game(){
-
-    }
 
     public static void run(){
         Game game;
@@ -40,17 +35,8 @@ public class Game {
                 throw new IllegalArgumentException(WRONG_INPUT_DUPLICATION);
             }
             carData.put(inputStringArr[i], 0);
-//            carNames.add(inputStringArr[i]);
         }
-//        for(String carName : carNames){
-//            if(carNameSet.contains(carName)){
-//                throw new IllegalArgumentException(WRONG_INPUT_DUPLICATION);
-//            }
-//            carNameSet.add(carName);
-//        }
     }
-
-//    public void addCar(String )
 
     private void inputNumber(){
         System.out.println(INPUT_NUMBER);
@@ -65,19 +51,18 @@ public class Game {
     }
 
     private void racing(int inputNum){
-        int[] tryNumber = new int[3];
-
+        System.out.println(RESULT);
         for(int i = 0; i < inputNum; i++){ // 총 5번 반복
-            racingOnce(tryNumber);
-            printRanking(tryNumber);
+            racingOnce();
+            printRanking();
 
             System.out.println();
         }
-        printWinner(tryNumber);
+        printWinner();
 
     }
 
-    private void printWinner(int[] tryNumber){
+    private void printWinner(){
         List<String> maxCarNames = new ArrayList<>();
         int maxValue = 0;
         for(Map.Entry<String, Integer> entry : carData.entrySet()) {
@@ -98,28 +83,18 @@ public class Game {
                 System.out.print(", ");
             }
         }
-//        for(String carName : maxCarNames) {
-//            System.out.print(carName + ", ");
-//        }
     }
 
-    private void printRanking(int[] tryNumber){
-//        for(int i = 0; i < carNumber; i++){
-//            System.out.print(carNameSet[i]);
-//        }
-        System.out.println(RESULT);
+    private void printRanking(){
         int curCarNum = 0;
         for(Map.Entry<String, Integer> entry : carData.entrySet()) {
             System.out.print(entry.getKey() + " : ");
             System.out.print("-".repeat(entry.getValue()));
             System.out.println();
-
-//            System.out.println(tryNumber[curCarNum++]);
         }
     }
 
-    private void racingOnce(int[] tryNumber){
-//        System.out.println("racingOnce함수");
+    private void racingOnce(){
         for(Map.Entry<String, Integer> entry : carData.entrySet()){
             String carName = entry.getKey();
             int value = entry.getValue();
@@ -129,19 +104,9 @@ public class Game {
             }
             carData.put(carName, value);
         }
-
-//        for(int i = 0; i < carNumber; i++){
-//            int randomNum = getRandomNum();
-////            System.out.println(randomNum);
-//            if(randomNum >= 4){
-//                tryNumber[i]++;
-//            }
-////            System.out.print("".repeat(tryNumber[i]));
-//        }
-
     }
+
     private int getRandomNum(){
-//        System.out.println("랜덤넘버 실행");
         int randomNum = Randoms.pickNumberInRange(0,9);
         return randomNum;
     }
