7632a0b46773b3146fa19e526bd1d86d76993039
oiler831 <qudwls831@gmail.com>
2023-10-30 15:31:58 +0900

    이름이 5자가 넘는지 판별
    차 이름이 5자가 넘는지 판별하는 isCarNameOverFive 메소드와 잘못된 입력값이면 error를 띄우는 isWrongInput 메소드를 만들었다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -15,8 +15,22 @@ public class RacingGame {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameLine = Console.readLine();
         divideCarNameLine(carNameLine);
+        isWrongInput(isCarNameOverFive());
     }
     private static void divideCarNameLine(String carNameLine){
         carName = carNameLine.split(",");
+    }
+    private static boolean isCarNameOverFive(){
+        for (String name : carName) {
+            if (name.length() > 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static void isWrongInput(boolean inputResult){
+        if (inputResult) {
+            throw new IllegalArgumentException();
+        }
     }
 }
