5b9df42e0634990e9d577f8c8f31c47a05c91a34
Bosub Kim <grapeflavoredapple@gmail.com>
2023-10-31 00:25:36 +0900

    feat: 도메인 모델 구현
    - 자동차는 이름과 점수를 가짐
    - 자동차의 점수는 1씩 상승만 가능함
    - 자동차의 점수는 문자열로 확인 가능함

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,4 +1,24 @@
 package racingcar.model;
 
 public class RacingCar {
+
+    String name;
+    int score;
+
+    private RacingCar(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public static RacingCar nameOf(String name) {
+        return new RacingCar(name, 0);
+    }
+
+    public void addScore() {
+        this.score++;
+    }
+
+    public String getScoreBar() {
+        return "-".repeat(this.score);
+    }
 }
