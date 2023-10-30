b0cc7e7991c2a34f2796819cc60c7bf2d7ef26b1
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 18:11:04 +0900

    feat: 입력받은 진행 횟수 검증
    입력값이 숫자가 아니면 예외를 발생시킨다.

--- src/main/java/racingcar/model/User.java
+++ src/main/java/racingcar/model/User.java
@@ -1,9 +1,25 @@
 package racingcar.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static racingcar.util.Constants.NUMBER_REGEXP;
+import static racingcar.util.VerificationMessage.IS_NOT_NUMBER;
+
 public class User {
     private final int moveSize;
 
     public User(String size) {
         this.moveSize = moveSize(size);
     }
+    public int moveSize(String size) {
+        if(!isNumber(size)) throw new IllegalArgumentException(IS_NOT_NUMBER);
+        return Integer.parseInt(size);
+    }
+    private boolean isNumber(String size) {
+        Pattern pattern = Pattern.compile(NUMBER_REGEXP);
+        Matcher matcher = pattern.matcher(size);
+        return matcher.matches();
+    }
+
 }
