be0d9eddf8a6394d3930cd634d159db9f37c3f76
wonindev <sangwonparkdev@gmail.com>
2023-10-30 19:32:04 +0900

    feat: 입력에 대한 예외처리 코드 추가 기능

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -11,13 +11,19 @@ public class GameManager {
     public static void GameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
+        ExceptionHandler eh = new ExceptionHandler();
+        eh.isCarNamePlainTextException(input);
 
         List<String> members = Arrays.asList(input.split(","));
+        eh.isMembersException(members);
+
         HashMap<String, String> membersHM = new HashMap<>();
         CarMovement.putHM(membersHM, members, "");
 
         System.out.println("시도할 회수는 몇회인가요?");
-        int attemptNum = Integer.parseInt(readLine());
+        String attempt = readLine();
+        eh.isAttemptException(attempt);
+        int attemptNum = Integer.parseInt(attempt);
 
         Print.progress(attemptNum, members, membersHM);
 
