b9db4d6f05c33b4578b8930bf51f3c332ff007d2
donghyun <kdhdh001@naver.com>
2023-10-31 02:02:25 +0900

    fix: 자동차 입력하지 않는 경우 예외처리 안되는것 수정

--- src/main/java/racingcar/validation/CarNameValidator.java
+++ src/main/java/racingcar/validation/CarNameValidator.java
@@ -11,6 +11,11 @@ public class CarNameValidator {
     }
 
     private static void validateNotEmpty(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.trim().isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+            }
+        }
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
         }
