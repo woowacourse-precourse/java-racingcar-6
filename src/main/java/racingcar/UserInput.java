3931ee6e36a642fb3773c48fd0fb1ef36bda5af0
K-KY <kyuyoungk@naver.com>
2023-10-31 04:29:27 +0900

    Feat(UserInput) : 자동차 이름을 입력받는 메소드

---
+++ src/main/java/racingcar/UserInput.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class UserInput {
+
+    public static List<String> carName() {
+        String names = Console.readLine();
+        List<String> carNames = List.of(names.split(""));
+        return carNames;
+    }
+
+}
