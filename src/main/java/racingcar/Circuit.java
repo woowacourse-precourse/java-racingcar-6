461d1d9b0c6a74d49592b625e27a9868a163409a
최휘용 <chy2581@naver.com>
2023-10-30 16:01:33 +0900

    feat: 필수 클래스와 필드 정의
    자동차 클래스와 레이싱 서킷을 추가하고 필드와 생성자를 정의했습니다

---
+++ src/main/java/racingcar/Circuit.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Circuit {
+    private ArrayList<Car> carList;
+    private int repeat;
+
+    public Circuit() {
+        this.carList = new ArrayList<Car>();
+        this.repeat = 0;
+    }
+}
