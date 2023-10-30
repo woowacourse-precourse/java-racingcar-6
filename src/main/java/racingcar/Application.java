9afec3947d6a62f1cbec6a7c957956cf1d985814
02ggang9 <02ggang9@gmail.com>
2023-10-30 15:48:43 +0900

    chore(Application): 불필요한 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,6 @@ import racingcar.domain.car.service.CarService;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         AppConfig.getController().process();
     }
 }
