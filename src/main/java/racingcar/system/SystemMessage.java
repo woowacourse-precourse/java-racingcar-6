f1c5e8064568bc07c4f1379514fb1e1569f44bc2
yshong1998 <yshong1998@gmail.com>
2023-10-30 23:01:16 +0900

    Feat(SystemMessage): 레이서에게 보여줄 메세지를 저장하는 기능 구현

---
+++ src/main/java/racingcar/system/SystemMessage.java
@@ -0,0 +1,16 @@
+package racingcar.system;
+
+public enum SystemMessage {
+
+    GAME_START_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_REPEAT_TIMES_MESSAGE("시도할 회수는 몇회인가요?"),
+    GAME_WINNER("최종 우승자 : "),
+    INPUT_OUT_OF_RANGE("입력이 범위를 초과했습니다."),
+    ONLY_NUMBER_INPUT_AVAILABLE("숫자 이외에는 입력할 수 없습니다.");
+
+    private final String message;
+
+    SystemMessage(String message){
+        this.message = message;
+    }
+}
