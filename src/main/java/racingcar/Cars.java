6488db29d0914a78a6a6d1dcbceaf6df8fe38394
giljin <68088970+giljin@users.noreply.github.com>
2023-10-30 15:00:36 +0900

    refactor: 멤버변수 변경 안되도록 final 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
     Cars(String carNames) {
         validateNullOrEmpty(carNames);
