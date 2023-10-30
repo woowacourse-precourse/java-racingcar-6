5f01e6ab5f17371e47b4aad8d2d5b61fc29d491d
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-30 11:37:43 +0900

    feat(Car): 무작위 값이 4 이상인 경우만 전진하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,18 +1,27 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.message.ErrorMessage;
 
 public class Car {
     private final String name;
+    private int position;
 
     public Car(String name) {
         validateNameLength(name);
         this.name = name;
+        this.position = 0;
     }
 
     private void validateNameLength(String name) {
         if(name.length() > 5) {
             throw new IllegalArgumentException(ErrorMessage.OVER_FIVE_LENGTH.toString());
+        }
+    }
+
+    public void moveForward() {
+        if (Randoms.pickNumberInRange(0,9) >= 4) {
+            position++;
         }
     }
 }
