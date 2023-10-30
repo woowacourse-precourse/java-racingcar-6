c31fddbaea482b5e4dee07270ebe1f7ff2143572
seminchoi <choicco89@gmail.com>
2023-10-30 16:44:31 +0900

    feat(racingcar.RacingCar) : 레이싱카가 전진할 지 결정하는 기능 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 public class RacingCar {
+    private final int MINIMUM_NUMBER_TO_MOVING_FORWARD = 4;
     private final RandomNumberGenerator randomNumberGenerator;
     private final String name;
     private int distance = 0;
@@ -9,5 +10,10 @@ public class RacingCar {
     public RacingCar(RandomNumberGenerator randomNumberGenerator, String name) {
         this.randomNumberGenerator = randomNumberGenerator;
         this.name = name;
+    }
+
+
+    private boolean canMovingForward() {
+        return randomNumberGenerator.generateRandomNumber() >= MINIMUM_NUMBER_TO_MOVING_FORWARD;
     }
 }
