8e72d3fd15330ba6c7a510f1f44747880025b81e
bin <gimgyubin1225@gmail.com>
2023-10-30 17:55:07 +0900

    Test: 실행회수 입력이 잘못된 경우 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 실행회수에_대한_예외_처리_문자인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
