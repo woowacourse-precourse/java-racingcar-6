8e5ee27ba15b8ae683ec45bf454e85849136164f
Hanbee Lee <bee1162@naver.com>
2023-10-31 03:36:18 +0900

    feat: inputView에서 사용자에게 자동차이름과 회수 입력받는 함수 클래스 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,18 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_RACE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public String inputCarName() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputRaceCount() {
+        System.out.println(INPUT_RACE_COUNT_MESSAGE);
+        return Console.readLine();
+    }
 }
