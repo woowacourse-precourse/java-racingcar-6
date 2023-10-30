f08d8ff85035d4ed7b49c76e4c95edb218de9ec8
한다현 <handahyeon@handahyeon-ui-MacBookAir.local>
2023-10-30 19:29:23 +0900

    refactor: move the methods from Car class to Application class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static String inputCarName(){
+        return Console.readLine();
+    }
+
+    static String[] splitCarName(String carString){
+        return carString.split(",");
+    }
+
+    static boolean isValidate(String[] cars){
+        for (String car: cars){
+            if (car.length() > 5){
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,22 +2,5 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Car {
-    String inputCarName(){
-        return Console.readLine();
-    }
-
-    String[] splitCarName(String cars){
-        return cars.split(",");
-    }
-
-    boolean isValidate(String[] cars){
-        for (String car: cars){
-            if (car.length() > 5){
-                throw new IllegalArgumentException();
-            }
-        }
-        return true;
-    }
-
 
 }
