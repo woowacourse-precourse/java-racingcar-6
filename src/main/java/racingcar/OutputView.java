fb74213da5b7f5f7894ea71061ce97cd7a557613
zzawang <hobin0116@naver.com>
2023-10-30 21:11:59 +0900

    feat: 출력 클래스 생성
    - RacingGame으로 부터 출력을 분리하기 위해 출력 클래스를 생성하였다.
    - print(): 메시지를 개행 문자('\n')없이 출력하는 메소드
    - println(): 메시지를 개행 문자('\n')를 붙여서 출력하는 메소드
    - lineBreak: 개행 문자('\n')만을 출력하는 메소드

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class OutputView {
+    public void print(String msg) {
+        System.out.print(msg);
+    }
+
+    public void println(String msg) {
+        System.out.println(msg);
+    }
+
+    public void lineBreak() {
+        System.out.println();
+    }
+}
