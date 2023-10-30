75f782aa054d95356fdd2e8c651770ef2cb36851
zzawang <hobin0116@naver.com>
2023-10-30 18:49:58 +0900

    feat: 랜덤값에 따라 자동차가 움직이는 메소드 구현
    - 랜덤값을 생성한다.
    - 생성한 랜덤값이 4이상이면 자동차가 전진한다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import static racingcar.Constant.MAX_RANDOM_NUM;
 import static racingcar.Constant.MIN_ATTEMPT_COUNT;
+import static racingcar.Constant.MIN_RANDOM_NUM;
+import static racingcar.Constant.MOVE_FORWARD_NUM;
 import static racingcar.Constant.PRINT_RESULT_MESSAGE;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RacingGame {
@@ -24,7 +28,12 @@ public class RacingGame {
     }
 
     private void moveCars() {
-
+        for (Car car : cars) {
+            int randomNum = Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
+            if (randomNum >= MOVE_FORWARD_NUM) {
+                car.moveForward();
+            }
+        }
     }
 
     private void printResult() {
