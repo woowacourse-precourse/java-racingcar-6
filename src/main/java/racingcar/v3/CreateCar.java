e5a60aa9ef004d072889761b3b471ef693196a85
권장순 <jsoonworld@gmail.com>
2023-10-30 20:53:58 +0900

    feat(racingcar) : CreateCar#validateCarCount()를 구현함.

--- src/main/java/racingcar/v3/CreateCar.java
+++ src/main/java/racingcar/v3/CreateCar.java
@@ -17,4 +17,10 @@ public class CreateCar {
         return carNames;
     }
 
+    public void validateCarCount(String[] carNames) throws IllegalArgumentException {
+        if (carNames.length < 2 || carNames.length > 10) {
+            throw new IllegalArgumentException("자동차의 수는 2대 이상 10대 이하만 가능합니다.");
+        }
+    }
+
 }
