6fd495d2bfdac7caad43f38e82a8bafaad0869e9
yummygyudon <bang2brew@gmail.com>
2023-10-31 02:53:39 +0900

    FIX : 잘못된 이름 형식에 대한 예외 처리 추가
    - 정상적인 구분자를 사용했지만 이름이 빈 값일 경우

--- src/main/java/racingcar/util/channel/ReaderForRacingCars.java
+++ src/main/java/racingcar/util/channel/ReaderForRacingCars.java
@@ -19,16 +19,19 @@ public class ReaderForRacingCars extends Reader {
 
     private void validateSeparator(String carsInput) {
         for (String str : carsInput.split(SPLIT_SEPARATOR)) {
-            if (ConstStandard.INVALID_SEPARATES.contains(str)) {
+            if (ConstStandard.INVALID_NAMES_INPUT_SEPARATORS.contains(str)) {
                 throw new IllegalArgumentException();
             }
         }
     }
     private void validateNameLength(String carsInput) {
-        for (String carName : carsInput.split(ConstStandard.CRITERIA_SEPARATE)) {
-            if (carName.trim().length() > ConstStandard.CRITERIA_NAME_UPPER_LIMIT) {
+        for (String carName : carsInput.split(ConstStandard.CRITERIA_NAMES_INPUT_SEPARATOR)) {
+            // 순수 이름값에 대해서만 길이를 재기 위해 String.trim() 을 수행합니다.
+            carName = carName.trim();
+            if (carName.length() > ConstStandard.CRITERIA_NAME_LENGTH_UPPER_LIMIT) {
                 throw new IllegalArgumentException();
             }
+            validateBlank(carName);
         }
     }
 
