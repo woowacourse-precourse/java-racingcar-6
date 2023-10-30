f53a85b34c495c1ded49d40ebff76498c1a6723a
silverwest8 <silverwest8@gmail.com>
2023-10-31 01:46:08 +0900

    refactor: change method order and format

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,10 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Car implements Comparable<Car>{
+public class Car implements Comparable<Car> {
 
     public String name;
     public Integer distance;
+
     @Override
     public int compareTo(Car o) {
         return o.distance - distance;
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -18,6 +18,30 @@ public class RacingGame {
         printWinners();
     }
 
+    private void getInputCarName() {
+        // 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        // 몇대까지 가능하게 할 것인가?
+        String[] nameList = input.split(",");
+        for (String s : nameList) {
+            if (s.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하입니다.");
+            }
+            carList.add(new Car(s, 0));
+        }
+    }
+
+    private void getInputCount() {
+        // 시도할 횟수 입력
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        // 몇대까지 가능하게 할 것인가?
+        this.count = Integer.parseInt(input);
+        System.out.println();
+    }
+
     private void race() {
         for (int i = 0; i < this.count; i++) {
             executeOneTurn();
@@ -50,31 +74,6 @@ public class RacingGame {
 
     private void printWinners() {
         System.out.println("최종 우승자 : " + String.join(", ", winnerList));
-    }
-
-    private void getInputCount() {
-        // 시도할 횟수 입력
-        System.out.println("시도할 회수는 몇회인가요?");
-        String input = Console.readLine();
-        // 몇대까지 가능하게 할 것인가?
-        this.count = Integer.parseInt(input);
-        System.out.println();
-
-    }
-
-    private void getInputCarName() {
-        // 자동차 이름 입력
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-
-        // 몇대까지 가능하게 할 것인가?
-        String[] nameList = input.split(",");
-        for (String s : nameList) {
-            if (s.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하입니다.");
-            }
-            carList.add(new Car(s, 0));
-        }
     }
 }
 
