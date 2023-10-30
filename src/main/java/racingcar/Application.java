e0baf9f9ae14550717301463574298b0a8ec4bb6
sonjeongdong <sjd0219@gmail.com>
2023-10-30 18:28:45 +0900

    Feat: 자동차 이름을 저장하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,18 +2,27 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(saveCarName(getCarNames()));
 
     }
 
     public static void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
-    public static String getCarName() {
+    public static String getCarNames() {
         printInputCarName();
         String input = Console.readLine();
         return input;
+    }
+    public static List<String> saveCarName(String names) {
+        List<String> carNames = Arrays.asList(names.split(","));
+        return carNames;
     }
 }
