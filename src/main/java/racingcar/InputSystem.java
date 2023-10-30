c5eb634ae8491f21e62835cd3baf04c8fd6be73b
happy-nana <nxxxgnim@gmail.com>
2023-10-31 04:47:01 +0900

    feat: InputSystem.java 기능 구현 사용자로부터 입력 받고 예외처리를 하는 클래스. 상수(메세지,구분자,공백,최대길이)지정 -> 이름받기(+유효성검사&예외처리) -> 시도횟수받기(+유효성검사&예외처리) -> 메세지 출력 및 답변 받는 공통부분 메서드 처리(새로 추가한 개선사항임) -> 예외처리의 조건들 각 메서드로 생성 테스트 이후 예외처리 추가될 수 있음

--- src/main/java/racingcar/InputSystem.java
+++ src/main/java/racingcar/InputSystem.java
@@ -1,4 +1,77 @@
 package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class InputSystem {
+
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_CNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String SEPARATOR = ",";
+    private static final String GAP = "";
+    private static final int MAX_LENGTH = 5;
+
+    public InputSystem() {
+
+    }
+
+    public void inputCarName(List<Car> cars) {
+        String input = answer(INPUT_NAME_MESSAGE);
+        String[] names = input.split(SEPARATOR);
+
+        for (String name : names) {
+            if (!isNotValidNames(name, cars)) {
+                cars.add(new Car(name));
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public int inputTryCNT() {
+        String input = answer(INPUT_CNT_MESSAGE);
+        if (!isNotNumber(input)) {
+            return Integer.parseInt(input);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String answer(String str) {
+        System.out.println(str);
+        return Console.readLine();
+    }
+
+    private boolean isNotValidNames(String name, List<Car> cars) {
+        return  isEmptyName(name)
+                || isBiggerThanMax(name)
+                || isDuplicatedName(name, cars);
+    }
+
+    private boolean isEmptyName(String name) {
+        return name == null || GAP.equals(name);
+    }
+
+    private boolean isDuplicatedName(String name, List<Car> cars) {
+        for (Car car : cars) {
+            if (name.equals(car.getName())) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+
+    private boolean isBiggerThanMax(String name) {
+        return name.length() > MAX_LENGTH;
+    }
+
+    private boolean isNotNumber(String str) {
+        Pattern pattern = Pattern.compile("^[0-9]*$");
+        return pattern.matcher(str).matches();
+    }
 
 }
