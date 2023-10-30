b1fb9863ce6f7276e2dd0b90b273d3c85e80189a
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 17:36:17 +0900

    test(CarManager): 저장된 레이싱카를 규칙에 맞게 이동시키는 함수의 테스트 코드 작성

--- src/test/java/racingcar/CarManagerTest.java
+++ src/test/java/racingcar/CarManagerTest.java
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarManagerTest {
-    CarManager carManager = new CarManager();
+    CarManager carManager = new CarManager(new RandomNumberGenerator());
+    CarManager carManagerWithFixedNumberGeneratorValueFive = new CarManager(new RandomNumberGeneratorMock(5));
+    CarManager carManagerWithFixedNumberGeneratorValueThree = new CarManager(new RandomNumberGeneratorMock(3));
 
     @Test
     void 입력받은_이름_목록의_레이싱카_목록을_생성한다() {
@@ -16,5 +18,44 @@ class CarManagerTest {
 
         // then
         Assertions.assertThat(carManager.getCars().getCarList().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 저장된_레이싱카들을_규칙에_맞게_이동시킨다_성공_케이스() {
+        // given
+        String[] names = {"성겸"};
+        carManagerWithFixedNumberGeneratorValueFive.createCars(names);
+
+        // when
+        carManagerWithFixedNumberGeneratorValueFive.moveCars();
+
+        // then
+        Assertions.assertThat(carManagerWithFixedNumberGeneratorValueFive.getCars().getCarList().get(0).getDistance()).isEqualTo(1);
+    }
+
+    @Test
+    void 저장된_레이싱카들을_규칙에_맞게_이동시킨다_실패_케이스() {
+        // given
+        String[] names = {"성겸"};
+        carManagerWithFixedNumberGeneratorValueThree.createCars(names);
+
+        // when
+        carManagerWithFixedNumberGeneratorValueThree.moveCars();
+
+        // then
+        Assertions.assertThat(carManagerWithFixedNumberGeneratorValueThree.getCars().getCarList().get(0).getDistance()).isEqualTo(0);
+    }
+}
+
+class RandomNumberGeneratorMock extends RandomNumberGenerator {
+    private int fixedValue;
+
+    public RandomNumberGeneratorMock(int fixedValue) {
+        this.fixedValue = fixedValue;
+    }
+
+    @Override
+    public Integer getRandomInteger() {
+        return fixedValue;
     }
 }
