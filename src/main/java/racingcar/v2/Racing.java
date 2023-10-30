b1ce40d9e46a3d8317bb27df971fb192e516c021
권장순 <jsoonworld@gmail.com>
2023-10-30 09:04:56 +0900

    feat(racingcar) : Racing#Round()를 구현했고 예외처리는 TODO 로 남겨둠.

--- src/main/java/racingcar/v2/Racing.java
+++ src/main/java/racingcar/v2/Racing.java
@@ -1,10 +1,14 @@
 package racingcar.v2;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Map;
 
 public class Racing {
-    public Map<String, Integer> state() {
-
-        return null;
+    // TODO: 시도 횟수 예외 처리. 숫자가 아닌 값, 음수 값, 0 값, 제한 횟수 초과.
+    public int Round() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }
