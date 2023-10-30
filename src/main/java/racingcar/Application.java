46c0dbdbe42d1831d572a4764277a1e7726030eb
ryugeumjung <fbgjung@gmail.com>
2023-10-30 21:36:25 +0900

    feat: 자동차 경주 게임 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,8 @@ import java.util.Set;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Race race = new Race();
+        SetRace set = new SetRace();
+        System.out.println(race.raceCar(set.createCar(),set.inputMovement()));
     }
 }
