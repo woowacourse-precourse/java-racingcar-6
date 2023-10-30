92f0abd294c637feb7c810139f03fb7d49dc308a
JW0321 <digneotn123@gmail.com>
2023-10-31 05:41:13 +0900

    Refactor : 변수 이름 변경 및 반복문 이동
    변수명을 의도를 잘 나타낼 수 있게 변경
    반복문을 Car 클래스로 옮겨 Application 클래스에서는 메소드 호출만 하도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //여기를 출력 및 인터페이스로 만들자
         // TODO: 프로그램 구현
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
-        Car cars = Input.initRacingCarLineup(names);
+        Car racing = Input.setRacingCarLineup(names);
 
         System.out.println("시도할 횟수는 몇회인가요?");
         String number = Console.readLine();
@@ -17,11 +17,9 @@ public class Application {
 
         System.out.println();
         System.out.println("실행 결과");
-        for (int i=0; i<repeatTimes; i++){
-            cars.runRace();
-        }
+        racing.runRace(repeatTimes);
 
-        ArrayList<String> winnerList = cars.makeWinnerList();
+        ArrayList<String> winnerList = racing.makeWinnerList();
         System.out.print("최종 우승자 : ");
         String winner = String.join(", ", winnerList);
         System.out.println(winner);
