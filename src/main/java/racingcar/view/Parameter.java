8723fece0caaed4ef87933ad9e6fe9d88ffe188c
howudong <tjdvy963@naver.com>
2023-10-31 01:12:41 +0900

    feat : View의 파라미터 상수화를 위해 Parameter 클래스 생성

---
+++ src/main/java/racingcar/view/Parameter.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+public class Parameter {
+    private Parameter() {
+    }
+
+    public static final String GAME_COUNT = "gameCount";
+
+    public static class Input {
+        private Input() {
+        }
+
+        public static final String CAR_NAMES = "carNames";
+    }
+
+    public static class Output {
+        private Output() {
+        }
+
+        public static final String CARS = "cars";
+    }
+}
