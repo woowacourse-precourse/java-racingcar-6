d5f8d509680b1b1228fa7cc815875403e18992d7
ycis5013@naver.com <ycis5013@naver.com>
2023-10-31 00:53:23 +0900

    feat(InputView) : 시도할 횟수 입력 받는 함수 inputRoundNum 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,15 +3,21 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static InputView inputView= new InputView();
+    private static InputView inputView = new InputView();
 
-    private InputView(){}
-    public static InputView getInstance(){
+    private InputView() {}
+
+    public static InputView getInstance() {
         return inputView;
     }
 
-    public String inputCarList(){
+    public String inputCarList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public String inputRoundNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
     }
 }
