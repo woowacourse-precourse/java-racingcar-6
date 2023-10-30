23cf488e10f535b1c3a5053b5c4c944d89519a32
songyi <song21677@naver.com>
2023-10-31 03:46:35 +0900

    refactor: 앞 뒤 공백 제거하는 stripCarName 함수 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,8 +12,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        List<String> carNameList = splitCarName(input);
-        carNameList.replaceAll(String::strip);
+        List<String> carNameList = stripCarName(splitCarName(input));
 
         List<Car> carList = new ArrayList<>();
         for (int i = 0; i < carNameList.size(); i++) {
@@ -96,5 +95,10 @@ public class Application {
 
     private static List<String> splitCarName(String input) {
         return Arrays.asList(input.split(","));
+    }
+
+    private static List<String> stripCarName(List<String> carNameList) {
+        carNameList.replaceAll(String::strip);
+        return carNameList;
     }
 }
