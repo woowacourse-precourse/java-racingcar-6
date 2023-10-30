98bbea1a5cd6ce4fd78eb6f604f2562ea1b7a2d9
jae woo Jung <awfjol2008@naver.com>
2023-10-30 18:12:44 +0900

    feat: 이동 거리가 최대인 플레이어를 구하는 기능 구현

--- src/test/java/racingcar/domain/Race.java
+++ src/test/java/racingcar/domain/Race.java
@@ -7,4 +7,14 @@ import java.util.List;
 public class Race {
 
     private List<Car> cars;
+
+    private Car getCarWithMaxDistance() {
+        Car maxDistanceCar = cars.get(0);
+        for (Car car : cars) {
+            if (car.isEqualOrGreaterThan(maxDistanceCar)) {
+                maxDistanceCar = car;
+            }
+        }
+        return maxDistanceCar;
+    }
 }
