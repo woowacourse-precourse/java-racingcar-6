d2e6ce28ec6e1cb9148c6d37d14f520b0ee926ef
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-30 14:32:13 +0900

    refactor(InputParser) : parserCarName 메소드가 List<Car>를 리턴하도록 리팩터링
    리팩터링 내용에 따라 test코드도 수정

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -1,13 +1,20 @@
 package racingcar.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.domain.Car;
 
 public class InputParser {
     private static final String DELIMITER = ",";
 
-    public List<String> parseCarName(String playerInput) {
-        return Arrays.stream(playerInput.split(DELIMITER)).toList();
+    public List<Car> parseCarName(String playerInput) {
+        List<Car> result = new ArrayList<>();
+        List<String> carNames = Arrays.stream(playerInput.split(DELIMITER)).toList();
+        for (String carName : carNames) {
+            result.add(new Car(carName));
+        }
+        return result;
     }
 
     public int parseNumberOfAttempt(String playerInput) {
--- src/test/java/racingcar/InputParserTest.java
+++ src/test/java/racingcar/InputParserTest.java
@@ -2,14 +2,37 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.util.InputParser;
 
 public class InputParserTest {
     InputParser inputParser = new InputParser();
+
+    private boolean deepEquals(List<Car> cars1, List<Car> cars2) {
+        List<String> carNames1 = new ArrayList<>();
+        List<String> carNames2 = new ArrayList<>();
+
+        for (Car car : cars1) {
+            carNames1.add(car.toString());
+        }
+        for (Car car : cars2) {
+            carNames2.add(car.toString());
+        }
+
+        if (carNames1.size() != carNames2.size()) {
+            return false;
+        }
+        for (int i = 0; i < carNames1.size(); i++) {
+            if (carNames1.get(i).equals(carNames2)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     @Test
     @DisplayName("자동차 이름 입력을 List형태로 파싱하는지를 테스트")
@@ -17,13 +40,18 @@ public class InputParserTest {
         String input1 = "car1,car2,car3";
         String input2 = "car1.car2,car3";
 
-        List<String> result1 = inputParser.parseCarName(input1);
-        List<String> result2 = inputParser.parseCarName(input2);
-        List<String> expected1 = Arrays.asList("car1", "car2", "car3");
-        List<String> expected2 = Arrays.asList("car1.car2", "car3");
+        List<Car> result1 = inputParser.parseCarName(input1);
+        List<Car> result2 = inputParser.parseCarName(input2);
+        List<Car> expected1 = new ArrayList<>();
+        expected1.add(new Car("car1"));
+        expected1.add(new Car("car2"));
+        expected1.add(new Car("car3"));
+        List<Car> expected2 = new ArrayList<>();
+        expected2.add(new Car("car1.car2"));
+        expected2.add(new Car("car3"));
 
-        assertThat(result1).isEqualTo(expected1);
-        assertThat(result2).isEqualTo(expected2);
+        assertThat(deepEquals(result1, expected1)).isTrue();
+        assertThat(deepEquals(result2, expected2)).isTrue();
     }
 
     @Test
