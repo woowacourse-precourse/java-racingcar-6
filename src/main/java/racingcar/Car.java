aa87670da30bb0b5541902c646a15ab61405ef81
dudxo <father8475@naver.com>
2023-10-31 02:58:20 +0900

    [Feat] 자동차 이름 입력 및 부여 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,13 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private String move;
+
+    Car(String name, String move){
+        this.name = name;
+        this.move = move;
+    }
 
 
 }
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,35 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Scanner;
+
 public class Game {
+    private int count;
+    private Car[] cars;
+    private String winner;
+
+    Game(){}
+
+    public void init(){
+        carsNameInput();
+        System.out.println("시도할 횟수는 몇회인가요?");
+        this.count = Integer.parseInt(Console.readLine());
+
+
+    }
+
+    public Car[] carsNameInput(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+        String[] carNames = carName.split(",");
+
+        return generateCars(carNames);
+    }
+
+    public Car[] generateCars(String[] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i], "");
+        }
+        return cars;
+    }
 }
