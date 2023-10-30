88b6a7b0db5ace04a2187893cbc2036c22994ca5
donglee99 <jhnj741@naver.com>
2023-10-30 11:45:37 +0900

    feat : input, output 유틸 구현

---
+++ src/main/java/racingcar/util/InputUtil.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+    private InputUtil() {};
+
+    public static final String stringInput() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/util/OutputUtil.java
@@ -0,0 +1,14 @@
+package racingcar.util;
+
+public class OutputUtil {
+
+    private OutputUtil() {};
+
+    public static final void println(String message) {
+        System.out.println(message);
+    }
+
+    public static final void print(String message) {
+        System.out.print(message);
+    }
+}
