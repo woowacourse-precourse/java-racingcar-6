e9a3da770dd7dbdcc02ea0d99f4ed0f6c01e0ce9
jeondui <jeondui@gmail.com>
2023-10-30 14:50:41 +0900

    feat(InputValueValidator): 게임 시도 횟수를 검증하는 기능 추가 및 에러메시지 활용

--- src/main/java/racingcar/util/InputValueValidator.java
+++ src/main/java/racingcar/util/InputValueValidator.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import racingcar.exception.ErrorMessage;
 
 public class InputValueValidator {
 
@@ -11,13 +12,30 @@ public class InputValueValidator {
         List<String> carNames = converStringToList(inputCarNames);
 
         if (!isDuplicateName(carNames)) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 이름은 중복을 허용하지 않습니다.");
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NAME_DUPLICATE.getMessage());
         } else if (!isCarNameLength(carNames)) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 이름 길이는 5자 이하만 가능합니다.");
-        } else if(!isMinimumTwoCarNames(carNames)) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 최소 2개 이상의 이름을 입력해주세요.");
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NAME_LENGTH.getMessage());
+        } else if (inputCarNames.contains(" ")) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NAME_NO_SPACES.getMessage());
+        } else if (!isMinimumTwoCarNames(carNames)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MINIMUM_NAMES.getMessage());
         }
         return carNames;
+    }
+
+    public int validateTryCount(int tryCount) {
+        if (!isMinimumOneTryCounts(tryCount)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MINIMUM_TRY_COUNT.getMessage());
+        }
+        return tryCount;
+    }
+
+    private boolean isMinimumOneTryCounts(int tryCount) {
+        return tryCount > 0;
+    }
+
+    private boolean isMinimumTwoCarNames(List<String> carNames) {
+        return carNames.size() > 1;
     }
 
     private boolean isDuplicateName(List<String> carNames) {
@@ -27,17 +45,6 @@ public class InputValueValidator {
 
     private boolean isCarNameLength(List<String> carNames) {
         return carNames.stream().allMatch(name -> name.length() <= 5);
-    }
-
-    private boolean isMinimumTwoCarNames(List<String> carNames) {
-        return carNames.size() > 1;
-    }
-
-    public int validateTryNumber(int tryNumber) {
-        if (tryNumber < 1) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 시도 횟수는 최소 1회 이상이어야 합니다.");
-        }
-        return tryNumber;
     }
 
     private List<String> converStringToList(String inputCarNames) {
