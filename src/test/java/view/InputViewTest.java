aef362a4a7f8c680234185d91d1af28d5191b8f1
yeahdy <yejine98@naver.com>
2023-10-31 00:43:01 +0900

    test(validateCarNames):자동차 유효성 검사 테스트코드 작성

---
+++ src/test/java/view/InputViewTest.java
@@ -0,0 +1,26 @@
+package view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputViewTest {
+
+    InputView inputView;
+
+    @BeforeEach
+    void init(){
+        inputView = new InputView();
+    }
+
+    @DisplayName("자동차 이름 입력값 유효성 검사")
+    @ParameterizedTest
+    @ValueSource(strings = {"잠온다,자고싶다", ",","라라", " ","   ","라라, ","여섯글자인데, 붕붕이"})
+    void validateCarNamesTest(String carNames){
+        assertThat(inputView.validateCarNames(carNames)).isTrue();
+    }
+
+}
