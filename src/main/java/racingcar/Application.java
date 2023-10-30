0e528cb87f9a7260d1adbf501f866c55c040290e
HJLee <goodforx0147@naver.com>
2023-10-30 11:15:51 +0900

    feat: car racing based on random number

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,7 @@ public class Application {
         List<String> inputCars = inputCars();
         Map<String, Integer> cars = storeCars(inputCars);
         int N = inputN();
+        cars = goOrStop(cars, N);
     }
     public static List<String> inputCars() {
         String input = Console.readLine();
@@ -32,5 +34,26 @@ public class Application {
     }
     public static Integer inputN() {
         return Integer.parseInt(Console.readLine());
+    }
+    public static Integer randomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+    public static Boolean checkRandomNumber(int randomNumber) {
+        return randomNumber > 3;
+    }
+    public static int addStep(int random_number) {
+        if (checkRandomNumber(random_number)) {
+            return 1;
+        }
+        return 0;
+    }
+    public static Map<String, Integer> goOrStop(Map<String, Integer> cars,int N) {
+        for (int i = 0; i < N; i++) {
+            for (String car : cars.keySet()) {
+                cars.put(car, cars.get(car) + addStep(randomNumber()));
+                System.out.println(car + " " + cars.get(car));
+            }
+        }
+        return cars;
     }
 }
