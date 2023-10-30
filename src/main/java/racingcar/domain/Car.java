b81250197b865247491a37a7d46ada6dd8393d14
jinsu4755 <parkjinsu4755@gmail.com>
2023-10-30 20:16:33 +0900

    feat: 자동차 자체가 할 수 있는 정보를 담는 역할 기능 작성
    feat: 자동차가 조건에 따라 움직이는 행위 기능 작성
    
    이동 거리를 불변하도록 작성
    이동 거리는 조건에 따라서만 변해야하는 요소이므로,
    별도에 setter를 두지 않고 side effect를 최소화 하고자 불변 요소로 사용함.
    또한 자동차 객체는 여러곳에서 참조하게 되기 때문에 이 참조에서 항상 동일한 값이 나오도록 고려했습니다.
    
    feat:  자동차 생성시 이름 유효성 검사 기능 추가
    
    feat: 자동차의 책임을 가지는 객체 생성
    
    feat: 자동차가 움직일 수 있는 조건의 전략 인터페이스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,50 @@
+package racingcar.domain;
+
+public final class Car {
+
+    private final int MAX_CAR_NAME_LENGTH = 5;
+    private final String MAX_CAR_NAME_ERROR_MESSAGE = String.format(
+            "Car name length must be less than %d characters",
+            MAX_CAR_NAME_LENGTH
+    );
+    private final String name;
+    private final MovableStrategy movableStrategy;
+
+    private final int movementDistance;
+
+    public Car(String name, MovableStrategy movableStrategy) {
+        validateCarName(name);
+        this.name = name;
+        this.movableStrategy = movableStrategy;
+        this.movementDistance = 0;
+    }
+
+    private Car(String name, MovableStrategy movableStrategy, int movementDistance) {
+        validateCarName(name);
+        this.name = name;
+        this.movableStrategy = movableStrategy;
+        this.movementDistance = movementDistance;
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(MAX_CAR_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMovementDistance() {
+        return movementDistance;
+    }
+
+    public Car move() {
+        int currentMovementDistance = movementDistance;
+        if (movableStrategy.isMovable()) {
+            currentMovementDistance++;
+        }
+        return new Car(name, movableStrategy, currentMovementDistance);
+    }
+}
