ce7f2e74123d063698e1f62428b16375f1600d88
zangsu <zangsu_@naver.com>
2023-10-30 21:48:47 +0900

    refactor : change project struct

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.game.Game;
+import racingcar.game.GameBuilder;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/racingcar/game/GameBuilder.java
+++ src/main/java/racingcar/game/GameBuilder.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.game;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.constant.ExceptionMessage;
