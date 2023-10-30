b89df77901b92c57a0a60a534ec23d053cb56b0f
DH Jeong <wjdehgns97@gmail.com>
2023-10-30 10:22:36 +0900

    refactor: 변수명 변경 maxValue -> winnerRecord

--- src/main/java/racingcar/domain/broadcast/Broadcast.java
+++ src/main/java/racingcar/domain/broadcast/Broadcast.java
@@ -20,14 +20,14 @@ public class Broadcast {
     }
 
     public static void prizeCeremony(HashMap<String, Integer> lineUp) {
-        int maxValue = 0;
+        int winnerRecord = 0;
         for (int value : lineUp.values()) {
-            if (value > maxValue) maxValue = value;
+            if (value > winnerRecord) winnerRecord = value;
         }
 
         List<String> winnerList = new ArrayList<>();
         for (Map.Entry<String, Integer> carRecord : lineUp.entrySet()) {
-            if (carRecord.getValue() == maxValue) {
+            if (carRecord.getValue() == winnerRecord) {
                 winnerList.add(carRecord.getKey());
             }
         }
