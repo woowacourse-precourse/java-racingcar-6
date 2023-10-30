a5d131506d1bf2d7c69d00085d818d8e221b4081
BDlhj <akk808@naver.com>
2023-10-30 17:27:28 +0900

    rename: Change printGameStartMessage() to printAskingForCarNamesMessage()

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RacingCarGame {
-    static void printGameStartMessage() {
+    static void printAskingForCarNamesMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
