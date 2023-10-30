882827ea5756f9a0c9770ab29ed8b87de7b64618
Elliot-KJW <eidechse4@hanmail.net>
2023-10-30 19:59:45 +0900

    Feat(StringValidator.java): Add checkIntegerFormat method
    rename validateStringValidatorOver to checkLengthOver because of readability
    - StringValidator : add checkIntegerFormatTest method

--- src/main/java/validation/StringValidator.java
+++ src/main/java/validation/StringValidator.java
@@ -3,9 +3,19 @@ package validation;
 public class StringValidator {
   private StringValidator() {}
 
-  public static String validateStringLengthOver(String checkString, int comparisonNumber) {
+  public static String checkLengthOver(String checkString, int comparisonNumber) {
     if(checkString.length() > comparisonNumber) {
       throw new IllegalArgumentException("자동차 이름은 " + comparisonNumber + "자 이하로 해주세요.");
+    }
+
+    return checkString;
+  }
+
+  public static String checkIntegerFormat(String checkString) {
+    try {
+      Integer.parseInt(checkString);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("숫자를 입력해주세요");
     }
 
     return checkString;
--- src/test/java/validation/StringValidatorTest.java
+++ src/test/java/validation/StringValidatorTest.java
@@ -11,7 +11,7 @@ public class StringValidatorTest {
     String inputString = "pobi";
     int inputInteger = 5;
 
-    String result = StringValidator.validateStringLengthOver(inputString, inputInteger);
+    String result = StringValidator.checkLengthOver(inputString, inputInteger);
 
     assertThat(result).isEqualTo(inputString);
   }
@@ -21,8 +21,26 @@ public class StringValidatorTest {
     String inputString = "pobi";
     int inputInteger = 3;
 
-    assertThatThrownBy(() -> StringValidator.validateStringLengthOver(inputString, inputInteger))
+    assertThatThrownBy(() -> StringValidator.checkLengthOver(inputString, inputInteger))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("자동차 이름은 " + inputInteger + "자 이하로 해주세요.");
+  }
+
+  @Test
+  void checkNumberFormat_메서드_사용시_정수면_그대로_반환() {
+    String inputString = "4";
+
+    String result = StringValidator.checkIntegerFormat(inputString);
+
+    assertThat(result).isEqualTo(inputString);
+  }
+
+  @Test
+  void checkNumberFormat_메서드_사용시_정수가_아니면_에러_발생() {
+    String inputString = "pobi";
+
+    assertThatThrownBy(() -> StringValidator.checkIntegerFormat(inputString))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("숫자를 입력해주세요");
   }
 }
