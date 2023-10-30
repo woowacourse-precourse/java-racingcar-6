a62d094072cd321383fac78a1a7470693e898f18
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-30 18:03:19 +0900

    feat: 기본 실행 메세지 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import static racingcar.util.OutputMessage.*;
+public class OutputView {
+    public void start() {
+        System.out.println(START);
+    }
+    public void moveSize() {
+        System.out.println(MOVE_SIZE);
+    }
+    public void result() {
+        System.out.println(RESULT);
+    }
+    public void winner() {
+        System.out.print(WINNER);
+    }
+}
