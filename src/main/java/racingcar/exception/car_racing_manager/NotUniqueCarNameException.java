31a211563018164b50f806a5852b0267b655e0ff
eom-tae-in <eti0728@daum.net>
2023-10-30 22:06:43 +0900

    refactor: 예외 메세지 수정

--- src/main/java/racingcar/exception/car_racing_manager/NotUniqueCarNameException.java
+++ src/main/java/racingcar/exception/car_racing_manager/NotUniqueCarNameException.java
@@ -1,8 +1,10 @@
 package racingcar.exception.car_racing_manager;
 
+import java.util.List;
+
 public class NotUniqueCarNameException extends IllegalArgumentException {
 
-    public NotUniqueCarNameException(final String carName) {
-        super(carName + "은(는) 중복된 이름입니다.");
+    public NotUniqueCarNameException(final List<String> carNames) {
+        super("입력하신 자동차 이름중에 중복된 이름이 있습니다. 입력하신 자동차 이름: " + carNames);
     }
 }
