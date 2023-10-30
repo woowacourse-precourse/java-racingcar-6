05075d11657658aff37898bf4ea0f244f78ae6b8
JuSeong1130 <jsmoon1130@naver.com>
2023-10-30 13:09:45 +0900

    comment(Application) : todo 주석제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,6 @@ import racingcar.domain.generator.RandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         GameController gameController = new GameController(new RacingGame(new RandomNumberGenerator()));
         gameController.run();
     }
