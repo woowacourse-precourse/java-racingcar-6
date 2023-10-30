2a34705b1ecaedb8cd6e227e1a3185f83d3dba2a
DevFrog92 <yongsu1481@gmail.com>
2023-10-30 16:47:05 +0900

    fix: application main method for refactoring RacingGame class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,14 +2,18 @@ package racingcar;
 
 import racingcar.domain.game.Game;
 import racingcar.domain.game.RacingGame;
+import racingcar.domain.judgement.Judgement;
+import racingcar.domain.judgement.RacingJudgement;
 import racingcar.domain.player.RacingGameParticipant;
 import racingcar.domain.player.RacingPlayer;
 import racingcar.util.GameConsoleUtil;
+import racingcar.util.RandomNumberUtil;
 
 public class Application {
-    public static void main(String[] args) {
-        RacingPlayer player = new RacingGameParticipant(GameConsoleUtil.getInstance());
-        Game racingGame = new RacingGame(player);
-        racingGame.start();
-    }
+        public static void main(String[] args) {
+                Judgement judgement = new RacingJudgement();
+                RacingPlayer player = new RacingGameParticipant(GameConsoleUtil.getInstance());
+                Game racingGame = new RacingGame(judgement, player, RandomNumberUtil.getInstance());
+                racingGame.start();
+        }
 }
