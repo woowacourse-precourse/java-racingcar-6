4cc8007b908fe2e2cdd2e923cdd2753f99694a0c
BlueArcticFox <jominhyeok0103@gmail.com>
2023-10-30 23:05:57 +0900

    Modify typo

--- src/main/java/racingcar/constant/Message.java
+++ src/main/java/racingcar/constant/Message.java
@@ -2,7 +2,7 @@ package racingcar.constant;
 
 public enum Message {
     INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_NUMBER_EXCUTION("시도할 회수는 몇회인가요?"),
+    INPUT_NUMBER_EXECUTION("시도할 회수는 몇회인가요?"),
     RESULT("실행 결과"),
     WINNER("최종 우승자 : ");
 
