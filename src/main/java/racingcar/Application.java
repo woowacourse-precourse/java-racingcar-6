998883239be0647e4196f0f3270747f3be6676b2
seonghoo1217 <seonghoo1217@naver.com>
2023-10-30 17:28:55 +0900

    feat: add input value for racer name in application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,33 @@
 package racingcar;
 
+import racingcar.core.generate.GenerateRacer;
+import racingcar.entity.Racer;
+import racingcar.entity.TargetScore;
+import racingcar.property.ValidateType;
+import racingcar.tool.InputTool;
+import racingcar.validation.ValidateForm;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.outputForRaceStart();
+        race();
+    }
+
+    private static void race(){
+        String inputValue = InputTool.readLineByConsole();
+        validateRacerNameList(inputValue);
+    }
+
+    private static void validateRacerNameList(String inputValue){
+        ValidateForm.validateForValidateType(ValidateType.NAME_LIST,inputValue);
+        List<String> racerNameList = Arrays.asList(inputValue.split(" "));
+        racerNameList.stream()
+                            .forEach(racerName->ValidateForm.validateForValidateType(
+                                    ValidateType.NAME,racerName
+                            ));
     }
 }
