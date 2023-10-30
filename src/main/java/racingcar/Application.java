abb882f63b042ba7942ea8bf057acc0d899b8a00
706com <ehdgusjdbswl@gmail.com>
2023-10-30 11:16:55 +0900

    feat : controller 생성 및 실행함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.run();
+        Console.close();
     }
 }
