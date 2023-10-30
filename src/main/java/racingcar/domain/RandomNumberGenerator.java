97c38f7a6827337fb8313e55823618c7dfdb57bf
hunko <rhwlgns4386@naver.com>
2023-10-30 19:37:04 +0900

    fix(RandomNumberGenerator) : 잘못된 함수 사용 변경

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,18 +1,15 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
 
 public class RandomNumberGenerator implements Generator {
 
     private static final int MIN = 0;
     private static final int MAX = 9;
-    private static final int SIZE = 1;
 
     @Override
     public Integer generate() {
-        List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(MIN, MAX, SIZE);
-        Integer choose = randomNumber.get(0);
+        Integer choose = Randoms.pickNumberInRange(MIN, MAX);
 
         assert inRange(choose);
         return choose;
