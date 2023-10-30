a9510d85e0f90ada7c28cbc6e4bfd894097b674c
icebear0111 <wltjq0901@naver.com>
2023-10-31 01:21:32 +0900

    feat: random number generating method

---
+++ src/main/java/racingcar/GameManager.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class GameManager {
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void printResult() {
+
+    }
+}
