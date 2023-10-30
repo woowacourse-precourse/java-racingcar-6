d90bd18c8e67973bbb2efa9af839a2ce3dc6ef91
706com <ehdgusjdbswl@gmail.com>
2023-10-30 11:17:26 +0900

    feat : Car (Player) 도메인 객체 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+
+    private int moveCount;
+
+    private int score;
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+    private static final int POSSIBLE_TO_MOVE_NUMBER = 4;
+
+    public Car(String name, int chanceCount) {
+        this.name = name;
+        this.moveCount = chanceCount;
+        this.score = 0;
+    }
+
+    public void getMoveChance(){
+        if(pickRandomNumberToMove()>=POSSIBLE_TO_MOVE_NUMBER) {
+            this.score++;
+        }
+        this.moveCount--;
+    }
+
+    private int pickRandomNumberToMove(){
+        return Randoms.pickNumberInRange(START_NUMBER,END_NUMBER);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+}
