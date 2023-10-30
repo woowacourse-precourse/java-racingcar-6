390df336e6d7ee5768b6aee6db127b667282e959
donghyun <kdhdh001@naver.com>
2023-10-31 01:13:59 +0900

    feat: 자동차 이름이 5자 초과하는 경우 예외처리 구현

--- src/main/java/racingcar/validation/CarNameValidator.java
+++ src/main/java/racingcar/validation/CarNameValidator.java
@@ -3,14 +3,24 @@ package racingcar.validation;
 import java.util.List;
 
 public class CarNameValidator {
+    private static final int MAX_NAME_LENGTH = 5;
 
     public static void validateCarNames(List<String> carNames) {
         validateNotEmpty(carNames);
+        validateLength(carNames);
     }
 
     private static void validateNotEmpty(List<String> carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private static void validateLength(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
         }
     }
 }
