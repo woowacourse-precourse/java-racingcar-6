5720cce4d69696bdb3ba027f0e3d9ca41e5359b3
kimwanyoung <dhks2869@gmail.com>
2023-10-30 14:28:03 +0900

    test : Car객체 생성시 공백, 빈 문자열 예외 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -21,4 +21,17 @@ public class CarTest {
                 .hasMessageContaining("5글자 미만");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("공백 혹은 빈 문자열 이름 입력시 Car객체 생성 예외 발생 테스트")
+    public void Car_객체_이름_입력_예외_테스트_공백_빈문자열(String input) {
+        //given
+        String name = input;
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백 혹은 빈");
+    }
+
 }
