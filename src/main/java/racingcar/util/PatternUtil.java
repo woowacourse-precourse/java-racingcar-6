56af568306649f93361c193913a51457fa7f901d
DevFrog92 <yongsu1481@gmail.com>
2023-10-30 14:20:20 +0900

    feat: implement PatternUtil class

---
+++ src/main/java/racingcar/util/PatternUtil.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+public class PatternUtil {
+        public static final String ONLY_DIGIT_PATTERN = "\\d+";
+        
+        public static String getOnlyDigitNumberPattern() {
+                return ONLY_DIGIT_PATTERN;
+        }
+}
