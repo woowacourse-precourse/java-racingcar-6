6d25e3392dbee9636cafb0531a38a618da7c36af
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-30 17:19:13 +0900

    Test: 우승자 가리는 기능 테스트 코드 추가

---
+++ src/test/java/mytest/ResultTest.java
@@ -0,0 +1,27 @@
+package mytest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.model.Car;
+import racingcar.utils.GetWinner;
+
+public class ResultTest {
+    @DisplayName("우승자 여부 확인 테스트")
+    @Test
+    void 우승자_구하는_태스트() {
+        //given
+        List<racingcar.model.Car> Users = new ArrayList<>();
+        Users.add(new Car("jeong"));
+        Users.add(new Car("min"));
+        //when
+        Users.get(1).forward();
+        //then
+        List<String> result = GetWinner.winnerList(Users);
+        assertThat(result).contains("min");
+    }
+}
