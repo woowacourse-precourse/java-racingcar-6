887f87377ec5385ff07b7b6ff79161f966f63aa2
Handwoong <handwoong@gmail.com>
2023-10-31 00:49:03 +0900

    refactor(util): 랜덤 숫자 생성 싱글톤 패턴 적용

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -3,6 +3,15 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final RandomNumberGenerator instance = new RandomNumberGenerator();
+
+    private RandomNumberGenerator() {
+    }
+
+    public static RandomNumberGenerator getInstance() {
+        return instance;
+    }
+
     public int createRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
