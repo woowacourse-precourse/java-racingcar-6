af2190c820b2d96487fd747368415379b3d5eeeb
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-10-30 23:27:46 +0900

    잘못된 수 입력시 NumberFormatException 대신 IllegalArgumentException이 발생하도록 변경

--- src/main/java/racingcar/utils/InputManager.java
+++ src/main/java/racingcar/utils/InputManager.java
@@ -4,8 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputManager {
 
     public int inputNumber(String numString){
-        int number = Integer.parseInt(numString);
-
+        Integer number;
+        try {
+             number = Integer.parseInt(numString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 수 입력");
+        }
         return number;
     }
 
