dd38317c3c500153d865eb074a245869578e9383
혜민 <alddlv81@gmail.com>
2023-10-30 14:08:44 +0900

    refactor: main 클래스 -> Race.run()에서 실행으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,7 @@
 package racingcar;
 
-import racingcar.car.CarInfo;
-import racingcar.car.UserInputCarName;
-import racingcar.game.ProcessGame;
-import racingcar.game.ResultGame;
-import racingcar.game.UserInputGameRound;
-
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        List<CarInfo> carList = UserInputCarName.getCarList();
-        int gameRound = UserInputGameRound.getGameRound();
-        ProcessGame.playGame(carList, gameRound);
-        ResultGame.printWinner(ResultGame.connectWinners(ResultGame.checkWinner(carList)));
-
+        Race.run();
     }
 }
