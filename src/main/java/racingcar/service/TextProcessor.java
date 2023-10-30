a4452f237765d0f7dcacd34016c80b275716603a
InJun2 <os2864@naver.com>
2023-10-31 02:42:40 +0900

    :sparkles: 문자열 조작 서비스 클래스 구분

--- src/main/java/racingcar/service/TextProcessor.java
+++ src/main/java/racingcar/service/TextProcessor.java
@@ -1,4 +1,17 @@
 package racingcar.service;
 
+import racingcar.exception.ExceptionMessage;
+
 public class TextProcessor {
+    private StringBuilder builder;
+
+    public int parseTryNumber(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INTEGER_FORMAT
+                    .getErrorMessage());
+        }
+    }
+
 }
