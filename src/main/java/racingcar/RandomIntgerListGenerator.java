f06b7f6eefdf45f209d66fb5ce2a5964fd7af9c1
robinjoon <robin980108@naver.com>
2023-10-31 02:52:36 +0900

    feat(랜덤 숫자 목록 생성 - 생성 범위) : 랜덤 숫자 목록 기능의 생섬 범위 구현

---
+++ src/main/java/racingcar/RandomIntgerListGenerator.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class RandomIntgerListGenerator {
+    static List<Integer> generate(final int startInclusive, final int endInclusive, int generateCount){
+        return IntStream.range(0, generateCount)
+                .map(operand -> Randoms.pickNumberInRange(startInclusive, endInclusive))
+                .boxed()
+                .toList();
+    }
+}
