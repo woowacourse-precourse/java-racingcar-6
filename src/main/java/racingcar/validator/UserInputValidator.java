7f9f8c70c326599a0ec39ef034cfa7350a349b73
cmong0516 <cmong0516@gmail.com>
2023-10-30 10:22:25 +0900

    feat(UserInputValidator): add validCarNameIsDuplicated() 중복된 이름이 입력되었는지 검증한다.

--- src/main/java/racingcar/validator/UserInputValidator.java
+++ src/main/java/racingcar/validator/UserInputValidator.java
@@ -1,5 +1,6 @@
 package racingcar.validator;
 
+import java.util.Objects;
 import racingcar.ErrorMessage;
 
 public class UserInputValidator {
@@ -10,6 +11,15 @@ public class UserInputValidator {
     public static void validCarNameLength(String userInput) {
         if (userInput.length() > 5) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_EXCEPTION.getMessage());
+        }
+    }
+
+    public static void validCarNameIsDuplicated(String[] userInput) {
+
+        for (int i = 1; i < userInput.length; i++) {
+            if (Objects.equals(userInput[i], userInput[i - 1])) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAME.getMessage());
+            }
         }
     }
 }
