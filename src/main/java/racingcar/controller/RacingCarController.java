7df4de245fed83b1d637f53253ecf5a292a39637
jhsseonn <hs125bt@gmail.com>
2023-10-30 21:04:09 +0900

    refactor: 입력받은 자동차들의 객체 생성해 저장

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.domain.Racing;
+import racingcar.service.RacingCarService;
 import racingcar.util.GlobalExceptionHandler;
 import racingcar.view.RacingCarView;
 
@@ -17,6 +18,7 @@ public class RacingCarController {
 
     private final static GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
     private final static RacingCarView racingCarView = new RacingCarView();
+    private final static RacingCarService racingCarService = new RacingCarService();
 
     private int getRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
@@ -33,25 +35,12 @@ public class RacingCarController {
     public void startRacingCar(){
 
         racingCarView.printStartRacingCar();
-
         final String cars = racingCarView.inputRacingCars();
-        List<String> carArray = new ArrayList<>();
-        if (globalExceptionHandler.isValidCarArray(cars)) {
-            carArray = List.of(cars.split(","));
-        }
 
-        // 입력받은 자동차 이름의 자동차 클래스들 생성하기
-        final List<Car> carList = new ArrayList<>();
-
-        for (int i=0; i<carArray.size(); i++){
-            if (globalExceptionHandler.isValidCarName(carArray.get(i))) {
-                final Car car = new Car(carArray.get(i));
-                carList.add(car);
-            }
-        }
+        List<Car> racingCarList = racingCarService.getRacingCarsList(cars);
 
         // 자동차 경주 게임 객체 생성해 자동차 리스트 저장하기
-        final Racing racingGame = new Racing(carList);
+        final Racing racingGame = new Racing(racingCarList);
 
         // 자동차 경주 시도 횟수 입력받아 저장하기
         System.out.println("시도할 회수는 몇회인가요?");
--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,5 +1,36 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.util.GlobalExceptionHandler;
+import racingcar.view.RacingCarView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarService {
+
+    private final static RacingCarView racingCarView = new RacingCarView();
+    private final static GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+    private static List<String> getCarsNameList(String cars) {
+        List<String> carArray = new ArrayList<>();
+        if (globalExceptionHandler.isValidCarArray(cars)) {
+            carArray = List.of(cars.split(","));
+        }
+        return carArray;
+    }
+
+    public List<Car> getRacingCarsList(String cars) {
+        List<String> racingCars = getCarsNameList(cars);
+        final List<Car> carList = new ArrayList<>();
+
+        for (int i=0; i<racingCars.size(); i++){
+            if (globalExceptionHandler.isValidCarName(racingCars.get(i))) {
+                final Car car = new Car(racingCars.get(i));
+                carList.add(car);
+            }
+        }
+        return carList;
+    }
 
 }
