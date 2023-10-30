790425dbc45c678faa644923d1a967d31c2e8db2
박상현 <shawnpark98@naver.com>
2023-10-30 15:18:52 +0900

    feat: Car#move() 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,15 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    int position = 0;
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+        if(randomNumber >= 4) {
+            position += 1;
+        }
+    }
 }
