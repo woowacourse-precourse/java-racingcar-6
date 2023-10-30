53cc80052211c3eca6ed47f04d422179ee0e2ee4
dbstn1325 <dbstn1325@naver.com>
2023-10-30 18:48:46 +0900

    refactor(CommonValidator): validateIncludeEmpty 메서드 내 변수와 메시지를 공통의 역할 수행하기 위해 수정

--- src/main/java/validator/CommonValidator.java
+++ src/main/java/validator/CommonValidator.java
@@ -2,9 +2,9 @@ package validator;
 
 public class CommonValidator {
 
-    public static void validateIncludeEmpty(String carName) {
-        if (carName.contains(" ")) {
-            throw new IllegalArgumentException("자동차 이름이 공백을 포함하고 있습니다. 애플리케이션을 종료합니다.");
+    public static void validateIncludeEmpty(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException("입력값이 공백을 포함하고 있습니다. 애플리케이션을 종료합니다.");
         }
     }
 }
