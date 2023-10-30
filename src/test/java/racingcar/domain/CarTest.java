dc29540e7e4b3ae08f99eda70bfa87f4af830b66
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-10-30 22:47:06 +0900

    test: Car isEqualPosition 메서드 테스트 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarTest {
 
@@ -39,5 +40,41 @@ class CarTest {
 
         car.goForward(movableSymbol);
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @DisplayName("Car goForward 실패 테스트")
+    @ValueSource(ints = {-1, 10, 100, 5000, -100})
+    @ParameterizedTest
+    void carGoForwardFailTest(int movableSymbol) {
+        CarName carName = new CarName("김씨차");
+        Car car = new Car(carName);
+
+        assertThrows(IllegalArgumentException.class, () -> car.goForward(movableSymbol));
+    }
+
+    @DisplayName("Car isEqualPosition 테스트 - 같은 위치")
+    @Test
+    void carIsEqualPositionSuccessTest_1() {
+        CarName carName1 = new CarName("김씨차");
+        CarName carName2 = new CarName("최씨차");
+
+        Car car1 = new Car(carName1);
+        Car car2 = new Car(carName2);
+
+        assertThat(car1.isEqualPosition(car2)).isTrue();
+    }
+
+    @DisplayName("Car isEqualPosition 테스트 - 같은 위치")
+    @Test
+    void carIsEqualPositionSuccessTest_2() {
+        CarName carName1 = new CarName("김씨차");
+        CarName carName2 = new CarName("최씨차");
+
+        Car car1 = new Car(carName1);
+        Car car2 = new Car(carName2);
+
+        car2.goForward(5);
+
+        assertThat(car1.isEqualPosition(car2)).isFalse();
     }
 }
