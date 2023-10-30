3123e0188cf5e7afb614920b790cbbb696dc1858
emost22 <emoney96@naver.com>
2023-10-31 00:47:35 +0900

    Feat: Add move car

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.data.Car;
@@ -12,6 +13,33 @@ public class CarService {
     public void start() {
         carList = getCarList();
         round = getRound();
+
+        System.out.println("실행 결과");
+        while (round-- > 0) {
+            carList.forEach(res -> {
+                res.move(isPossibleMove());
+            });
+            System.out.println();
+        }
+
+        int max = -1;
+        List<String> winnerList = new ArrayList<>();
+        for (Car car : carList) {
+            if (max < car.getMoveDistance()) {
+                max = car.getMoveDistance();
+                winnerList.clear();
+                winnerList.add(car.getName());
+            } else if (max == car.getMoveDistance()) {
+                winnerList.add(car.getName());
+            }
+        }
+
+        String winnerString = winnerList.get(0);
+        for (int i = 1; i < winnerList.size(); i++) {
+            winnerString += ", " + winnerList.get(i);
+        }
+
+        System.out.println("최종 우승자 : " + winnerString);
     }
 
     private List<Car> getCarList() {
@@ -37,5 +65,9 @@ public class CarService {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isPossibleMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
