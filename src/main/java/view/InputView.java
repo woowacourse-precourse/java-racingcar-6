4373721e3925ab238b07210b8021a528668ac193
kimwanyoung <dhks2869@gmail.com>
2023-10-31 07:58:17 +0900

    feat : 시도 횟수 입력 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     private static final String COMMA = ",";
     private static final String NAMES_FROM_USER_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPTS_FROM_USER_INPUT = "시도할 회수는 몇회인가요?";
 
     private InputView() {
     }
@@ -16,5 +17,10 @@ public class InputView {
         System.out.println(NAMES_FROM_USER_INPUT);
         String names = Console.readLine();
         return Arrays.asList(names.split(COMMA));
+    }
+
+    public static String getAttemptsFromUserInput() {
+        System.out.println(ATTEMPTS_FROM_USER_INPUT);
+        return Console.readLine();
     }
 }
