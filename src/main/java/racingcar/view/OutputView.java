de7c6bb3f85029cb4771f651db21b976340c8b9b
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-10-30 15:06:41 +0900

    feat (OutputView) : 각 차수별 자동차 위치 현황 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,17 @@
 package racingcar.view;
 
 import java.util.Objects;
+import racingcar.dto.CarDto;
+import racingcar.dto.RacingResultDto;
 import racingcar.view.printer.ConsolePrinter;
 import racingcar.view.printer.Printer;
 
 public class OutputView {
 
+    private static final String NEW_LINE = "\n";
     private static final String LOCATION_RESULT_TITLE = String.format("%n실행 결과");
+    private static final String CAR_RESULT_FORMAT = "%s : %s".concat(NEW_LINE);
+    private static final String POSITION_CHARACTER = "-";
 
     private final Printer printer;
 
@@ -20,6 +25,20 @@ public class OutputView {
 
     public void printRacingResultTitle() {
         print(LOCATION_RESULT_TITLE);
+    }
+
+    public void printRacingResult(RacingResultDto resultDto) {
+        resultDto.cars().forEach(this::printCarResult);
+        print("\n");
+    }
+
+    private void printCarResult(CarDto carDto) {
+        String carResult = CAR_RESULT_FORMAT.formatted(carDto.name(), makePositionFormat(carDto.position()));
+        print(carResult);
+    }
+
+    private String makePositionFormat(int position) {
+        return POSITION_CHARACTER.repeat(position);
     }
 
     private void print(String message) {
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -2,8 +2,11 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.CarDto;
+import racingcar.dto.RacingResultDto;
 import racingcar.view.printer.MockPrinter;
 
 class OutputViewTest {
@@ -16,6 +19,24 @@ class OutputViewTest {
         String expectedMessage = String.format("%n실행 결과");
 
         outputView.printRacingResultTitle();
+
+        assertThat(printer.getPrintedMessage()).isEqualTo(expectedMessage);
+    }
+
+    @Test
+    @DisplayName("각 차수별 자동차 위치 현황 출력 기능 테스트")
+    void printRacingResultTest() {
+        MockPrinter printer = new MockPrinter();
+        OutputView outputView = new OutputView(printer);
+        RacingResultDto resultDto = new RacingResultDto(
+                List.of(new CarDto("pobi", 3), new CarDto("woni", 2), new CarDto("jun", 3)));
+        String expectedMessage = """
+                pobi : ---
+                woni : --
+                jun : ---
+                \n""";
+
+        outputView.printRacingResult(resultDto);
 
         assertThat(printer.getPrintedMessage()).isEqualTo(expectedMessage);
     }
