73bca4d1a2d9d4434a86adc3ed30e27bd9cc1d35
lkdcode <dlrlejr1@naver.com>
2023-10-30 18:17:17 +0900

    Add : ConsoleWriter

---
+++ src/main/java/racingcar/domain/console/output/ConsoleWriter.java
@@ -0,0 +1,18 @@
+package racingcar.domain.console.output;
+
+public class ConsoleWriter {
+    private static final String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public ConsoleWriter() {
+    }
+
+    public void inputNamesMessagePrint() {
+        System.out.println(INPUT_NAMES_MESSAGE);
+    }
+
+    public void inputCountMessagePrint() {
+        System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
+    }
+
+}
