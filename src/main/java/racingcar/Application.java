9a0027cc9befa7c4b396d7acdd8af06386dd196c
SeungHo0422 <songseungho9258@gmail.com>
2023-10-31 03:34:47 +0900

    feat : askRacingCars 사용자 입력함수 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static List<String> askRacingCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+        List<String> racingCars = List.of(userInput.split(","));
+
+        //TODO : 쉼표가 아닌 다른 값으로 구분짓는다면 예외처리
+        //TODO : 이름이 5자 초과일 시 예외처리
+        return racingCars;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(askRacingCars());
+
+
     }
 }
