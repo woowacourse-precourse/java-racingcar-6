9404bd7b1dda25bde19209882ac845ea5f24cea6
Dreaming-J <kimjjong321@naver.com>
2023-10-31 02:17:06 +0900

    :sparkles: feat. 자동차 전진 회수 입력

--- src/main/java/racingcar/util/TypeConverter.java
+++ src/main/java/racingcar/util/TypeConverter.java
@@ -6,9 +6,18 @@ import java.util.stream.Stream;
 public class TypeConverter {
 
     private static final String DELIMiTER = ",";
+    private static final String WRONG_INPUT_MSG = "숫자만 입력해주세요.";
 
     public static List<String> toList(String input) {
         return Stream.of(input.split(DELIMiTER))
                 .toList();
+    }
+
+    public static int toInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(WRONG_INPUT_MSG);
+        }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,9 +8,15 @@ import racingcar.util.TypeConverter;
 public class InputView {
 
     private static final String INPUT_ENTRY_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND_MSG = "시도할 회수는 몇회인가요?";
 
     public List<String> readEntryNames() {
         System.out.println(INPUT_ENTRY_NAME_MSG);
         return TypeConverter.toList(readLine());
+    }
+
+    public int readRound() {
+        System.out.println(INPUT_ROUND_MSG);
+        return TypeConverter.toInt(readLine());
     }
 }
