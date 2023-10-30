8f21320a489447efc705bf423db2fd7781be6223
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-30 15:47:48 +0900

    test : 자동차 이름의 비즈니스 검증 클래스에 대한 테스트 코드 원래 로직으로 재수정

--- src/test/java/racingcar/exception/CarNameValidatorTest.java
+++ src/test/java/racingcar/exception/CarNameValidatorTest.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 class CarNameValidatorTest {
 
     private static final Integer CAR_NAME_MAX_LENGTH = 5;
+    private static final String INVALID_CAR_NAME_LENGTH_ERROR_MESSAGE =
+            "자동차 이름은 " + CAR_NAME_MAX_LENGTH + "자 이하만 가능합니다.";
 
     @DisplayName("자동차 이름이 최대 길이 이하인 경우 예외가 발생하지 않는다.")
     @Test
@@ -23,8 +25,12 @@ class CarNameValidatorTest {
     @DisplayName("자동차 이름이 최대 길이보다 긴 경우 예외가 발생해야 한다.")
     @Test
     void testInvalidCarName() {
-        String invalidCarName = "C".repeat(CAR_NAME_MAX_LENGTH + 1);
+        String invalidCarName = "C".repeat(CAR_NAME_MAX_LENGTH);
 
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateCarName(invalidCarName));
+        try {
+            CarNameValidator.validateCarName(invalidCarName);
+        } catch (IllegalArgumentException e) {
+            assertEquals(INVALID_CAR_NAME_LENGTH_ERROR_MESSAGE, e.getMessage());
+        }
     }
 }
