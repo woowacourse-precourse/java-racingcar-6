36b6d3f82f9a173fb5ea012de46bf89e5dad75d1
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-30 23:15:32 +0900

    🎯feat(view / OutputValidator) : 결과 출력 함수 구현

---
+++ src/main/java/racingcar/view/Output/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view.Output;
+
+public class OutputView {
+    private final String RACE_REULT= "실행 결과";
+
+    public void printResult() {
+        System.out.println(RACE_REULT);
+    }
+}
