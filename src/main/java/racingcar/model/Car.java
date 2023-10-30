cf0bf71f33a5e58340c703ed5627126533d2db4e
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:08:37 +0900

    [feat] Add Car class constructor

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,4 +3,8 @@ package racingcar.model;
 public class Car {
     private String name;
     private Integer steps;
+    public Car (String name) {
+        this.name = name;
+        this.steps = 0;
+    }
 }
