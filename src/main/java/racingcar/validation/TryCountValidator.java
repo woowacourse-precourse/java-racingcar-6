ee8e8260652889b814e06295ebd029243bc0348e
donghyun <kdhdh001@naver.com>
2023-10-31 02:31:42 +0900

    feat: 아무것도 입력하지 않았을 때 예외처리 구현

--- src/main/java/racingcar/validation/TryCountValidator.java
+++ src/main/java/racingcar/validation/TryCountValidator.java
@@ -3,6 +3,7 @@ package racingcar.validation;
 public class TryCountValidator {
 
     public static int validateTryCount(String input) {
+        validateInputEmpty(input);
         int tryCount = parseInputToInteger(input);
         return tryCount;
     }
@@ -12,6 +13,12 @@ public class TryCountValidator {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닌 다른 문자를 입력하였습니다. 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void validateInputEmpty(String input) {
+        if (input == null || input.length() == 0) {
+            throw new IllegalArgumentException("시도할 횟수를 입력하지 않았습니다. 숫자를 입력해주세요.");
         }
     }
 
