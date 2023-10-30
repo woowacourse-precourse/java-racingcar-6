a49b73b36dfbff595bbbca297852f38536c4c8bd
Han <yulmam1@naver.com>
2023-10-30 14:14:36 +0900

    feat(Attempt) : 시도횟수가 정수가 아닌경우 예외처리하는 기능 추가

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -4,8 +4,21 @@ public class AttemptCount {
     private final int totalAttemptCount;
 
     public AttemptCount(String totalAttemptCount) {
+        validate(totalAttemptCount);
         this.totalAttemptCount = Integer.parseInt(totalAttemptCount);
     }
 
+    private void validate(String totalAttemptCount) {
+        validateInteger(totalAttemptCount);
+    }
+
+    private void validateInteger(String totalAttemptCount) {
+        try{
+            Integer.parseInt(totalAttemptCount);
+        }
+        catch (NumberFormatException ex){
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
