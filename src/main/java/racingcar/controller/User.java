ad604667a5fe39e1865888511465f2fc3595c472
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:23:21 +0900

    [feat] Add User class inputCarNames method

--- src/main/java/racingcar/controller/User.java
+++ src/main/java/racingcar/controller/User.java
@@ -1,6 +1,21 @@
 package racingcar.controller;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import racingcar.model.Car;
+import racingcar.model.CarStorage;
+
 public class User {
     public User() {}
+    public void inputCarNames() throws IllegalArgumentException {
+        String carNames = readLine();
+        String[] splitCars = carNames.split(",");
 
+        for (String car: splitCars) {
+            car = car.replace(" ", "");
+
+
+            CarStorage.addCar(new Car(car));
+        }
+    }
 }
