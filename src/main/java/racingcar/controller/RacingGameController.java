0e59f560fd7323043fec4d86e8450533104394e7
hunko <rhwlgns4386@naver.com>
2023-10-30 18:58:50 +0900

    feat(RacingGameController) : run에 사태 출력 로직 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,7 +1,10 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 import racingcar.dto.RacingGameRequest;
+import racingcar.dto.StatusResponse;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -11,12 +14,14 @@ public class RacingGameController {
         String names = InputView.readCarNames();
         String count = InputView.readCount();
 
-        RacingGameRequest request=new RacingGameRequest(names,count);
+        RacingGameRequest request = new RacingGameRequest(names, count);
 
         RacingGame racingGame = new RacingGame(request);
         OutputView.printPlayResultMessage();
-         while (!racingGame.isEnd()){
-             racingGame.doRace();
-         }
+        while (!racingGame.isEnd()) {
+            racingGame.doRace();
+            List<Car> status=racingGame.getStatus();
+            OutputView.printStatus(new StatusResponse(status));
+        }
     }
 }
