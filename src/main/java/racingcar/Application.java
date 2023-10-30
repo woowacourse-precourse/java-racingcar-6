ba68f6c3b9aa88c5be4f522366eef2ae172f8a32
Yerinnnn <shinyerin0216@gmail.com>
2023-10-30 15:17:33 +0900

    Feat: "main() 수정"
    Race 객체 생성 및 startGame() 메소드 실행.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Race race = new Race();
+        race.startGame();
+
     }
 }
