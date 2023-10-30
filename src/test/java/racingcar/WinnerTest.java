9f19432129b1aec511b7c70b15c5f1fa9585a96e
suwhan2 <jerry5230@naver.com>
2023-10-30 23:44:53 +0900

    test(racingcar): create WinnerTest code

--- src/test/java/racingcar/WinnerTest.java
+++ src/test/java/racingcar/WinnerTest.java
@@ -1,20 +1,33 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinnerTest {
     @Test
     void decisionWinner_단독우승자_결정() {
-
+        Winner winner = new Winner();
+        List<Integer> inputTrack = Arrays.asList(5,3,3);
+        List<Integer> result = winner.decisionWinner(inputTrack);
+        assertThat(result).containsExactly(0);
     }
     @Test
     void decisionWinner_공동우승자_결정() {
-
+        Winner winner = new Winner();
+        List<Integer> inputTrack = Arrays.asList(5,5,3);
+        List<Integer> result = winner.decisionWinner(inputTrack);
+        assertThat(result).containsExactly(0,1);
     }
 
     @Test
     void decisonWinner_아무도_앞으로_가지_못한_경우(){
-
+        Winner winner = new Winner();
+        List<Integer> inputTrack = Arrays.asList(0,0,0);
+        List<Integer> result = winner.decisionWinner(inputTrack);
+        assertThat(result).containsExactly(0,1,2);
     }
 }
