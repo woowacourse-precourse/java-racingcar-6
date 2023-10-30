7fa8d7d063813222ca27b54098f5b99246352a81
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-30 19:17:49 +0900

    feat(InputView): implement 'requestRepetitionNumber' method to prompt users for repetition number

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String REQUEST_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_REPETITION = "시도할 회수는 몇회인가요?";
 
     public static String requestRacingCarNames() {
         System.out.println(REQUEST_CAR_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String requestRepetitionNumber() {
+        System.out.println(REQUEST_REPETITION);
         return Console.readLine();
     }
 }
