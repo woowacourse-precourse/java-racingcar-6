4bb8fd16234c6ace89fed12d32d0a9cf76ce4255
Jiwon Na <jiwonna52@gmail.com>
2023-10-30 21:01:17 +0900

    Test : TEST WRONG INPUT FOR DUBLICATE NAMES, NAME LENGTH AND WRONG NUMBER

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -6,19 +6,29 @@ import racingcar.controller.Input;
 import java.util.ArrayList;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static racingcar.controller.Input.checkDuplicateName;
+import static racingcar.controller.CheckInput.*;
 
 public class InputTest {
 
     @Test
-    void 중복된_입력() {
+    void 중복된_이름_입력() {
         List<String> names = new ArrayList<>();
         names.add("poobee");
         names.add("poobee");
         assertThatThrownBy(() -> checkDuplicateName(names)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 잘못된_숫자_입력() {
+        String wrongNum = "a";
+        assertThatThrownBy(() -> checkIfNum(wrongNum)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 잘못된_이름_길이_입력() {
+        List<String> names = new ArrayList<>();
+        names.add("aaaaaa");
+        assertThatThrownBy(() -> checkNameLength(names)).isInstanceOf(IllegalArgumentException.class);
     }
 }
