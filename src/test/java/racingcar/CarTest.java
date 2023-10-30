1d6be92fbde53750e29a08e44dbc34854eed2144
Gukin Han <gukin.dev@gmail.com>
2023-10-30 20:30:15 +0900

    test: createCarByNameTest를 에러 상황별로 분리

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,38 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
 import static racingcar.Car.createCarByName;
 
 class CarTest {
 
     @Test
-    void createCarByNameTest() {
-        String longName = "abcdef";
-        String emptyName = "";
-        String name = "abc";
-        String nameWithNull = null;
-
-        assertThatThrownBy(() -> createCarByName(longName))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("5자 이하의 이름만 입력해주세요");
-
-        assertThatThrownBy(() -> createCarByName(emptyName))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1자 이상의 이름만 입력해주세요");
-
-        assertThatThrownBy(() -> createCarByName(nameWithNull))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("null은 이름이 될 수 없습니다");
-    }
-
-    @Test
-    void 유효한_이름으로_Car_오브젝트_생성() {
+    void createCarByName_메서드에서_유효한_이름으로_Car_오브젝트_생성() {
         String validName = "abcd";
         Car car = createCarByName(validName);
         assertThat(car.getName()).isEqualTo(validName);
     }
 
+    @Test
+    void createCarByName_메서드에서_긴_이름에_대한_예외_처리() {
+        String longName = "abcdef";
+        assertThatThrownBy(() -> createCarByName(longName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자 이하의 이름만 입력해주세요");
+    }
 }
