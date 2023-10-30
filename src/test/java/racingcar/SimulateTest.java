ccfc30dd3631a282a1ed17d8ce56b55f91334abb
suwhan2 <jerry5230@naver.com>
2023-10-30 16:29:29 +0900

    test(racingcar): create test skeleton code

---
+++ src/test/java/racingcar/SimulateTest.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimulateTest {
+    @Test
+    void goSimulate_상황에_따라_트랙에_기록() {
+
+    }
+}
---
+++ src/test/java/racingcar/WinnerTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinnerTest {
+    @Test
+    void decisionWinner_공동우승자_결정() {
+
+    }
+
+    @Test
+    void 아무도_앞으로_가지_못한_경우(){
+
+    }
+}
