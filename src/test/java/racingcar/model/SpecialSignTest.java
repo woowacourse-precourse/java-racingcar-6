95396489c5289d3cf8e5323ca4dd77244a5f40e4
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-30 20:59:40 +0900

    SpecialSign Enum test 추가

---
+++ src/test/java/racingcar/model/SpecialSignTest.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
+
+class SpecialSignTest {
+
+    @DisplayName("Enum 인자를 확인한다.")
+    @ParameterizedTest
+    @EnumSource(value = SpecialSign.class)
+    void testEnumSourceValue(SpecialSign specialSign) {
+    }
+
+    @DisplayName("값이 스페이스만 확인한다.")
+    @ParameterizedTest
+    @EnumSource(mode = Mode.INCLUDE, names = {"SPACE"})
+    void testSpaceValueTest(SpecialSign specialSign) {
+        assertThat(specialSign.getSign()).isEqualTo(" ");
+    }
+}
