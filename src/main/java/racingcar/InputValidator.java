4f7841984cf8856b4600522e5f57a7a4bbacecde
eunjin3481 <eunjin3481@gmail.com>
2023-10-31 00:41:30 +0900

    feat(InputValidator): 입력값이 없는 경우 예외처리 구현

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class InputValidator {
     public static void nameLength(String name) {
+    public static void validateNamesIsEmpty(String[] input) {
+        for (String name : input) {
+            if (name.trim().isEmpty()) {
+                throw new IllegalArgumentException("입력 값이 없습니다. 프로그램 종료");
+            }
+        }
+    }
+
 
     }
 
