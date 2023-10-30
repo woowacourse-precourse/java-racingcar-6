fd1355466083b09f68995b84afbc18455bd7a6b8
gunwoong <gwj0421@gmail.com>
2023-10-30 16:42:32 +0900

    feat : 시나리오에 맞는 raceStart 메서드 추가
    - 차를 이동시키고 결과를 보여줌
    1. 차가 한대일 경우
    2. 여러 차일 경우

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Cars;
 import racingcar.util.StringUtil;
 
@@ -29,6 +30,37 @@ public class RaceService {
         StringUtil.checkNumber(cnt);
 
         return Integer.parseInt(cnt);
+    }
+
+    public void raceStart(Cars cars, int cnt) {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < cnt; i++) {
+            moveCars(cars);
+            showAllCars(cars);
+        }
+
+    }
+
+    private void moveCars(Cars cars) {
+        for (Car car : cars.getCars()) {
+            car.moveCar(getRandomMoveDistance());
+        }
+
+    }
+
+    protected int getRandomMoveDistance() {
+        int distance = Randoms.pickNumberInRange(0, 9);
+        if (distance > 3) {
+            return distance;
+        }
+        return 0;
+    }
+
+    private void showAllCars(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(String.format("%s : ", car.getName()) + StringUtil.repeatString(car.getDistance()));
+        }
+        System.out.println();
     }
 
 
