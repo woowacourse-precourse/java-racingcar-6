ddb91e8c56cbfdd3a46a26e6070a13d3311a07f9
jongheonleee <qwefghnm1212@gmail.com>
2023-10-30 19:35:45 +0900

    [test] outputWinner() 메서드 기능 테스트 및 테스트 문구 작성

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OutputViewTest extends IOTest {
 
     @Test
+    @DisplayName("'실행 결과' 문구 출력 테스트")
     void outputGameResultMessage() {
         // given
         OutputView outputView = new OutputView();
@@ -25,6 +27,7 @@ class OutputViewTest extends IOTest {
     }
 
     @Test
+    @DisplayName("자동차 위치 정보 그래프로 출력하는 기능 테스트")
     void outputCarsPosition() {
         // given
         OutputView outputView = new OutputView();
@@ -37,27 +40,35 @@ class OutputViewTest extends IOTest {
         crong.goForward(); // crong : -
         // nimo :
 
-        List<Car> cars = List.of(pobi, crong, nimo);
-        /** 기대 값
-         * pobi : --
-         * crong : -
-         * nimo :
-         *
-         */
+        List<Car> carsList = List.of(pobi, crong, nimo);
         String expected = "pobi : --\ncrong : -\nnimo : \n\n";
 
 
         // when
-        outputView.outputCarsPosition(cars);
+        outputView.outputCarsPosition(carsList);
 
         // then
         assertThat(getOutput()).isEqualTo(expected);
     }
 
     @Test
+    @DisplayName("최종 우승자 문구 출력 기능 테스트")
     void outputWinner() {
         // given
+        OutputView outputView = new OutputView();
+        MovingStrategy moveStrategy = new MovingStrategy((a, b) -> 4);
+
+        Car pobi = new Car(moveStrategy, "pobi", 0);
+        Car crong = new Car(moveStrategy, "crong", 0);
+
+        List<Car> winners = List.of(pobi, crong);
+        String expected = "최종 우승자 : pobi, crong\n";
+
         // when
+        outputView.outputWinner(winners);
+
         // then
+        assertThat(getOutput()).isEqualTo(expected.toString());
+
     }
 }
