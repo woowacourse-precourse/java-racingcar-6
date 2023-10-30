80fc54a3eb59f6ada7e8f157858aec368c5bd38a
yeeuniii <anna862700@gmail.com>
2023-10-31 01:34:54 +0900

    Feat: Car 클래스 생성 및 전진함수 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int numberOfMove;
+
+    public Car(String name) {
+        this.name = name;
+        numberOfMove = 0;
+    }
+
+    public void moveFoward() {
+        if (getRandomNumber() >= 4)
+            numberOfMove++;
+    }
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public int getNumberOfMove() {
+        return numberOfMove;
+    }
+}
