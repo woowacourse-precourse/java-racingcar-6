19821aff4700adf3da82cfcfa26a3d5cf38bc67c
PUNGZI <ashyoo1367@gmail.com>
2023-10-31 00:01:28 +0900

    test: 우승자가 여러 명일 때 쉼표를 이용하여 출력하는지에 대한 테스트 코드 작성

--- src/test/java/racingcar/FunctionTest.java
+++ src/test/java/racingcar/FunctionTest.java
@@ -9,4 +9,23 @@ import racingcar.domain.RacingValue;
 import racingcar.domain.Referee;
 
 public class FunctionTest {
+    @Test
+    void 우승자가_여러_명일_때_쉼표를_이용하여_구분_후_출력하는가() {
+
+        // given (이런 값이 들어왔을 때)
+        RacingValue racingValue = new RacingValue();
+        Referee referee = new Referee();
+        Map<String, Integer> racingResult = new HashMap<>() {{
+            put("qwer", 3);
+            put("asdf", 2);
+            put("zxcv", 3);
+        }};
+
+        // when (이렇게 실행되면)
+        racingValue.racingStatus = racingResult;
+        String winnerCars = referee.callWinnerCar();
+
+        // then (이렇게 나와야 함)
+        assertThat(winnerCars).contains("qwer, zxcv");
+    }
 }
