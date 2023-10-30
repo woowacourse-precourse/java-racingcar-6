83336495691e0b2688a1b93d6bd02e04776062bd
hoyun06 <hoyun06@gmail.com>
2023-10-30 18:29:31 +0900

    feat: Car 클래스에 생성자 추가

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -9,9 +9,13 @@ public class Car {
     private final Name name;
     private final Position position;
 
-    public Car(String name) {
+    public Car(String name, int position) {
         this.name = new Name(name);
-        this.position = new Position(0);
+        this.position = new Position(position);
+    }
+
+    public Car(String name) {
+        this(name, 0);
     }
 
     public void attemptMovingForward() {
