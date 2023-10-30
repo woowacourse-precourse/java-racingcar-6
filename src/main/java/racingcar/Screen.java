eebeb7a124839ceae84c2fd1e71d74dfb6ae1050
yeeuniii <anna862700@gmail.com>
2023-10-31 00:58:31 +0900

    Feat: 자동차 이름 길이 유효성 확인

--- src/main/java/racingcar/Screen.java
+++ src/main/java/racingcar/Screen.java
@@ -4,14 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Screen {
     static public String[] inputRaceInformation() {
-        String[] names = inputCarNames();
+        String[] names = askCarNames();
         return names;
     }
 
-    static public String[] inputCarNames() {
+    static public String[] askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String names = Console.readLine();
-        return names.split(",");
+        String[] names = Console.readLine().split(",");
+        for (String name : names) {
+            checkValidName(name);
+        }
+        return names;
     }
 
+    static private void checkValidName(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw (new IllegalArgumentException("Invalid Argument"));
+        }
+    }
 }
--- src/test/java/racingcar/ScreenTest.java
+++ src/test/java/racingcar/ScreenTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +15,30 @@ public class ScreenTest {
         String input = "pobi,yen,joy,red";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        String[] result = Screen.inputCarNames();
+        String[] result = Screen.askCarNames();
 
         assertThat(result).hasSize(4);
         assertThat(result).containsExactly("pobi", "yen", "joy", "red");
     }
+
+    @Test
+    void 길이_5이상의_자동차_이름_입력() {
+        String input = "computer,yen,joy,red";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(Screen::askCarNames)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 길이_0인_자동차_이름_입력() {
+        String input = ",yen,joy,red";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(Screen::askCarNames)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
