2309888df98b7f71d4046d6959a0ea5feb886f33
kyu-wooon <catdog0831@gmail.com>
2023-10-30 13:22:54 +0900

    * 테스트 코드 추가 작성 (10.30) 1. 이름에_대한_예외_처리() 2. 자동차_이름_중복_입력_예외_처리() 3. 횟수_숫자_이외_글자_예외_처리() 4. 입력값_확인_테스트()

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,10 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.Input_car.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -29,6 +33,31 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 자동차_이름_중복_입력_예외_처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("car1,car2,car2", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 횟수_숫자_이외_글자_예외_처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("car1,car2,car3", "two"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력값_확인_테스트() {
+                run("car1,car2,car3", "3");
+                assertThat(car_map.containsKey("car1")).isTrue();
+                assertThat(car_map.containsKey("car2")).isTrue();
+                assertThat(car_map.containsKey("car3")).isTrue();
+                assertThat(excute_count.equals("3"));
     }
 
     @Override
