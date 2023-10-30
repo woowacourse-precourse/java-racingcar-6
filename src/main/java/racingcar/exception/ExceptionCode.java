1cb748e3c7076a798a203b1ea5caafc81c82718a
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-31 08:56:15 +0900

    feat: 예외 코드 목록 열거형 생성

---
+++ src/main/java/racingcar/exception/ExceptionCode.java
@@ -0,0 +1,32 @@
+package racingcar.exception;
+
+public enum ExceptionCode {
+    NO_RESULT("MODEL-001", "게임 결과가 존재하지 않습니다."),
+    NO_CAR_LIST("MODEL-002", "자동차 이름 목록이 존재하지 않습니다."),
+    NO_ATTEMPT_NUMBER("MODEL-003", "시도 횟수가 존재하지 않습니다."),
+    ;
+
+    private final String code;
+    private final String description;
+
+    ExceptionCode(final String code, final String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "ExceptionCode{" +
+                "code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
