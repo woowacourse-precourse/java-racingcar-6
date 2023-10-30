47e40a1f4ff3aab4b1266f9e1f8aaf01374d1ef8
ysy56 <ysy561356@naver.com>
2023-10-30 15:47:26 +0900

    feat : 경주할 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static void printCarNamePrompt() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static String getUserInputForCarNames() {
+        return Console.readLine();
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printCarNamePrompt();
+        String inputCarNames = getUserInputForCarNames();
+        String[] splitCarNames = inputCarNames.split(",");
+        System.out.println(splitCarNames[0]);
+
     }
 }
