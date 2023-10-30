f771d1967ef664353fa0f8f3435283f9680078aa
xogns4909 <xogns4909@naver.com>
2023-10-30 13:18:29 +0900

    Refactor : WinnerFomatter 포맷 형식 변환

--- src/main/java/util/formatter/WinnerFormatter.java
+++ src/main/java/util/formatter/WinnerFormatter.java
@@ -5,9 +5,15 @@ import java.util.stream.Collectors;
 
 public class WinnerFormatter {
 
+    private static final String WINNER_PREFIX = "최종 우승자: ";
+
     public String format(WinnerDto winnerDto) {
-        return winnerDto.winner().stream()
-                .collect(Collectors.joining(", "));
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(WINNER_PREFIX)
+                .append(winnerDto.winner().stream().collect(Collectors.joining(", ")));
+
+        return builder.toString();
     }
 
 }
