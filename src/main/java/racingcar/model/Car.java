6823680f5d5b365a7502e2ddabfef7d3726ec23b
cookingTorch <jhw7181@naver.com>
2023-10-30 19:13:42 +0900

    feat(car): make class and add basic fields and constructor
    Added the Car class with basic fields such as 'name', 'moveCount'.
    Also included is a constructor for initializing these fields.
    
    BREAKING CHANGE: Introduces new Car class into the codebase.

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    String name;
+    int moveCount;
+
+    Car(String name) {
+        validateName(name);
+        this.name = name;
+        this.moveCount = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw (new IllegalArgumentException());
+        }
+    }
+}
