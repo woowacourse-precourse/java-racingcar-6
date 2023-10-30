1d9d3a9c300707b6bcc314dda823d8e90bd8ed61
shining013 <nevertheless113@naver.com>
2023-10-30 22:55:15 +0900

    자동차 클래스 정의

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,26 @@
 package racingcar;
 
 public class Application {
+    public class Car {
+        String name;
+        int length;
+
+        public Car(String name) {
+            setName(name);
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setLength(int length) {
+            this.length += length;
+        }
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
