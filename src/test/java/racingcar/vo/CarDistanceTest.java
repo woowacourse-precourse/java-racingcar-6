4647737da3be79d6c8e778a811712956678130a0
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-30 15:21:10 +0900

    test : 자동차가 이동한 거리에 대한 레코드의 테스트 코드 작성

---
+++ src/test/java/racingcar/vo/CarDistanceTest.java
@@ -0,0 +1,44 @@
+package racingcar.vo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarDistanceTest {
+
+    @Test
+    public void carDistance_constructor_test() {
+        CarDistance carDistance = new CarDistance(100);
+
+        assertEquals(100, carDistance.distance());
+    }
+
+    @DisplayName("필드값이 동일하면 객체는 동일해야 한다.")
+    @Test
+    public void carDistance_equality_test() {
+        CarDistance carDistance1 = new CarDistance(200);
+        CarDistance carDistance2 = new CarDistance(200);
+
+        assertEquals(carDistance1, carDistance2);
+    }
+
+
+    @DisplayName("필드값이 동일하지 않으면 객체는 동일하지 않다.")
+    @Test
+    public void carDistance_not_equal_test() {
+        CarDistance carDistance1 = new CarDistance(200);
+        CarDistance carDistance2 = new CarDistance(300);
+
+        assertNotEquals(carDistance1, carDistance2);
+    }
+
+    @DisplayName("레코드의 이름과 각 필드의 이름 및 대괄호 안의 해당 값이 포함된 문자열을 반환한다.")
+    @Test
+    public void carDistance_toString_test() {
+        CarDistance carDistance = new CarDistance(150);
+
+        assertEquals("CarDistance[distance=150]", carDistance.toString());
+    }
+}
