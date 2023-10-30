71fcb3bef06817e7899e5514952c56560af0ca7f
jae woo Jung <awfjol2008@naver.com>
2023-10-30 21:26:25 +0900

    refector: 자동차 이름 목록 입력값 검증 메소드명 변경

--- src/test/java/racingcar/validator/CarNameValidator.java
+++ src/test/java/racingcar/validator/CarNameValidator.java
@@ -2,18 +2,18 @@ package racingcar.validator;
 
 public class CarNameValidator {
 
-    public static void validateForString(String carNames) {
-        validateEmptyForString(carNames);
-        validateContainsCommaForString(carNames);
+    public static void validateForInputString(String carNames) {
+        validateEmptyForInputString(carNames);
+        validateContainsCommaForInputString(carNames);
     }
 
-    private static void validateEmptyForString(String carNames) {
+    private static void validateEmptyForInputString(String carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
         }
     }
 
-    private static void validateContainsCommaForString(String carNames) {
+    private static void validateContainsCommaForInputString(String carNames) {
         if (!carNames.contains(",")) {
             throw new IllegalArgumentException("입력값은 쉼표(,)를 기준으로 구분되어야 합니다.");
         }
