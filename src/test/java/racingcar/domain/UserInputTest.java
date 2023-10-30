110788ec4f6aece060d805f09e834d8b3890c829
HYOAN <aspqll@naver.com>
2023-10-31 02:01:35 +0900

    fix(racingcar): 사용자 입력 기능 테스트 코드 실수 수정

--- src/test/java/racingcar/domain/UserInputTest.java
+++ src/test/java/racingcar/domain/UserInputTest.java
@@ -1,22 +1,30 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputTest {
+    private UserInput userInput;
 
+    @BeforeEach
+    void setUp() {
+        userInput = new UserInput();
+    }
     @Test
     void carName() {
-        String input = "pobi,woni";
-        assertThat(input).isEqualTo("pobi,woni");
+        List<String> testInput = userInput.carName("pobi,woni");
+        assertThat(testInput).containsExactly("pobi", "woni");
     }
 
     @Test
     void attemptCount() {
-        int input = 3;
-        assertThat(input).isEqualTo(input);
+        int testInput = userInput.attemptCount("3");
+        assertThat(testInput).isEqualTo(3);
     }
 
     @Test
