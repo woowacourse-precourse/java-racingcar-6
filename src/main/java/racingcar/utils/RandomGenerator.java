2eed237168cfe8d60ce6bf0acef779550ce1fec3
J0YERIM <chosarah70@gmail.com>
2023-10-30 21:19:13 +0900

    test: Add test to verify random number generation between 0 and 9

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -6,7 +6,7 @@ public class RandomGenerator {
     private static final int MIN_BOUND = 0;
     private static final int MAX_BOUND = 9;
 
-    public int generateRandomNumber() {
+    public static int generateRandomNumber() {
         return Randoms.pickNumberInRange(MIN_BOUND, MAX_BOUND);
     }
 }
