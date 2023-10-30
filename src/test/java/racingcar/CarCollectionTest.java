477d5044b025c7e55240a5edef0b0ea55e20ec60
pjaehyun <show6756@gmail.com>
2023-10-30 17:38:19 +0900

    feat: 추가 addCar 함수 기능 테스트

--- src/test/java/racingcar/CarCollectionTest.java
+++ src/test/java/racingcar/CarCollectionTest.java
@@ -1,4 +1,28 @@
 package racingcar;
 
+import car.Car;
+import car.CarCollection;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarCollectionTest {
+
+    @Test
+    void addCar_자동차_추가() {
+        CarCollection carCollection = new CarCollection();
+
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+
+        carCollection.addCar(car1);
+        carCollection.addCar(car2);
+
+        List<Car> cars = carCollection.getCars();
+
+        assertThat(cars).contains(car1);
+        assertThat(cars).contains(car2);
+    }
 }
