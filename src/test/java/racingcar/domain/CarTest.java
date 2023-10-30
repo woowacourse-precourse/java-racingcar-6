70126c96f422758d7af72bdaff4a76b0b72b3ca0
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 07:39:54 +0900

    test : 랜덤 수가 4 이상일 때 직진하는 기능 테스트 코드 완성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,11 +2,18 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
+    private Car car;
+    @Test
+    void 수가_랜덤_중_4_이상_일_때_직진(){
+        int random = 4;
+        Car car = new Car();
+        int result = car.goForward(random);
+        assertThat(result).isEqualTo(1);
+
+    }
+
 
 }
