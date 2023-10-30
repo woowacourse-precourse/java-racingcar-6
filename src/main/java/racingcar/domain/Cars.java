364b5a8cda5452365bcd15ceb7c3d62e5d9f8e0b
euniiiii <hee9920@naver.com>
2023-10-30 14:53:41 +0900

    feat(domain): 최종 우승자를 보여주는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,7 +3,9 @@ package racingcar.domain;
 import racingcar.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
@@ -15,6 +17,24 @@ public class Cars {
 
     public void showResult() {
         OutputView.printResult(cars);
+    }
+
+    public void showFinalWinner() {
+        OutputView.printFinalWinner(getFinalWinner());
+    }
+
+    private List<Car> getFinalWinner() {
+        int maxMovement = getMaxMovement();
+        return cars.stream()
+                .filter(car -> car.getMovement() == maxMovement)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxMovement() {
+        return cars.stream()
+                .max(Comparator.comparingInt(Car::getMovement))
+                .get()
+                .getMovement();
     }
 
     public List<Car> getCars() {
