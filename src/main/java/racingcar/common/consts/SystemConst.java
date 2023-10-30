04f47eed88dfe8b3c7321e99f8a85e92d7c6d395
jifrozen0110 <answldjs1836@gmail.com>
2023-10-30 13:53:47 +0900

    refactor(NumberGenerator) : 매직 넘버 상수 생성

---
+++ src/main/java/racingcar/common/consts/SystemConst.java
@@ -0,0 +1,7 @@
+package racingcar.common.consts;
+
+public class SystemConst {
+    public static final int MAX_DIGIT_SIZE=9;
+    public static final int MIN_DIGIT_SIZE=0;
+    public static final int MOVE_FORWARD_NUMBER=4;
+}
--- src/main/java/racingcar/common/utils/NumberGenerator.java
+++ src/main/java/racingcar/common/utils/NumberGenerator.java
@@ -1,9 +1,10 @@
 package racingcar.common.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.common.consts.SystemConst;
 
 public class NumberGenerator {
     public static int makeRandomNumber(){
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(SystemConst.MIN_DIGIT_SIZE,SystemConst.MAX_DIGIT_SIZE);
     }
 }
