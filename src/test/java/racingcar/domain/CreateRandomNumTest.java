04ed69bf085eb9231c81a28862218c665a833ef1
박승찬 <seungchan141414@gmail.com>
2023-10-30 16:34:02 +0900

    [Test(racing)] 접근 제어자 변경

--- src/test/java/racingcar/domain/CreateRandomNumTest.java
+++ src/test/java/racingcar/domain/CreateRandomNumTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateRandomNumTest {
-    CreateRandomNum createRandomNum;
+    private CreateRandomNum createRandomNum;
 
     @BeforeEach
     void setUp() {
