b9c44d6646c81d9eec6b4626217243517b365d7a
hgh1472 <hgh1472@naver.com>
2023-10-31 03:23:47 +0900

    Test : View 테스트 코드 추가

---
+++ src/test/java/racingcar/view/ViewTest.java
@@ -0,0 +1,97 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ViewTest {
+    private static ByteArrayOutputStream outputMessage;
+
+    @BeforeEach
+    void setupStreams() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 질문 테스트")
+    void askCarNameTest() {
+        OutputView.askCarName();
+        assertThat(outputMessage.toString().trim())
+                .isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    @Test
+    @DisplayName("카운트 입력 질문 테스트")
+    void askCountTest() {
+        OutputView.askCount();
+        assertThat(outputMessage.toString().trim())
+                .isEqualTo("시도할 회수는 몇회인가요?");
+    }
+
+    @Test
+    @DisplayName("레이스 실행 문장 테스트")
+    void startRaceTest() {
+        OutputView.nextLine();
+        assertThat(outputMessage.toString())
+                .isEqualTo(System.lineSeparator());
+        OutputView.startRace();
+        assertThat(outputMessage.toString().trim())
+                .isEqualTo("실행 결과");
+    }
+
+    @Test
+    @DisplayName("각 회수에 따른 출력 결과 테스트")
+    void printResultTest() {
+        List<String> userName = new ArrayList<>();
+        List<Integer> distance = new ArrayList<>();
+        String result = "pobi : ---" + System.lineSeparator() + "woni : ----"
+                + System.lineSeparator() + System.lineSeparator();
+        userName.add("pobi");
+        userName.add("woni");
+        distance.add(0, 3);
+        distance.add(1, 4);
+        OutputView.printResult(userName, distance);
+        assertThat(outputMessage.toString())
+                .isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("우승자 출력 테스트")
+    void plusWinnerTest() {
+        assertThat(OutputView.plusWinner("", "pobi"))
+                .isEqualTo("pobi");
+        assertThat(OutputView.plusWinner("pobi", "woni"))
+                .isEqualTo("pobi, woni");
+    }
+
+    @Test
+    @DisplayName("우승자 계산 테스트")
+    void printWinnerTest() {
+        List<String> userName = new ArrayList<>();
+        List<Integer> distance = new ArrayList<>();
+        Integer winnerDistance = 4;
+
+        userName.add("pobi");
+        userName.add("woni");
+        userName.add("jun");
+        distance.add(0, 3);
+        distance.add(1, 4);
+        distance.add(2, 4);
+        OutputView.printWinner(userName, distance, winnerDistance);
+        assertThat(outputMessage.toString().trim())
+                .isEqualTo("최종 우승자 : woni, jun");
+    }
+}
