c38f49748e0e4b231c865486b11547309fdb91af
MyStraw <kikongdosa@gmail.com>
2023-10-30 20:05:29 +0900

    feat: CarTest 클래스

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("Car 테스트");
+    }
+
+    @Test
+    void 자동차_전진_테스트() {
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_멈춤_테스트() {
+        car.move(3);
+        assertThat(car.getPosition()).isEqalto(0);
+    }
+
+}
