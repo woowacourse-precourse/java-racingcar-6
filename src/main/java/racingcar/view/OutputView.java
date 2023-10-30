697671508828d2c7ab7a42eacc67bf5d8216453d
ay-eonii <naayen@naver.com>
2023-10-31 02:37:20 +0900

    feat: `실행 결과` 문구 출력 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 public class OutputView {
     private final static String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final static String RESULT_GUIDE_MESSAGE = "\n" + "실행 결과";
 
     public void displayCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -13,6 +14,7 @@ public class OutputView {
     }
 
     public void displayResultGuideMessage() {
+        System.out.println(RESULT_GUIDE_MESSAGE);
     }
 
     public void displayLocation() {
