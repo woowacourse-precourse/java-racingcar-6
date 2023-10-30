7cab5fa746cec7dbb73eb8473169e7ac9bd10e2b
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-10-30 21:46:59 +0900

    refactor: validate 함수 분리
    하나로 합쳐놨던 함수를 분리

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -10,18 +10,22 @@ public class CarName {
     private final String name;
 
     public CarName(String name) {
-        validate(name);
+
+        validateBlack(name);
+        validateLength(name);
+
         this.name = name;
     }
 
-    private void validate(String name) {
-
-        if (name.equals("")) {
-            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
-        }
-
+    private void validateLength(String name) {
         if (!(MIN_LENGTH <= name.length() && name.length() <= MAX_LENGTH)) {
             throw new IllegalArgumentException("자동차 이름 길이가 올바르지 않습니다");
+        }
+    }
+
+    private void validateBlack(String name) {
+        if (name.equals("")) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
         }
     }
 
