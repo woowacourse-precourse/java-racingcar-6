b02016767c5100d86342e087e4b35fc13e1c5e8d
acycoco <pink10257@naver.com>
2023-10-31 00:29:13 +0900

    refactor(RacingCars): 자동차 이름리스트로 RacingCars를 생성하게 변경

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -2,13 +2,16 @@ package racingcar.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCars {
 
     private final List<RacingCar> racingCars;
 
-    public RacingCars(final List<RacingCar> racingCars) {
-        this.racingCars = racingCars;
+    public RacingCars(final List<String> racingCarNames) {
+        this.racingCars = racingCarNames.stream()
+                .map(RacingCar::new)
+                .collect(Collectors.toList());
     }
 
     public Integer getMaxForwardDistance() {
@@ -18,4 +21,5 @@ public class RacingCars {
     public List<RacingCar> getRacingCars() {
         return Collections.unmodifiableList(racingCars);
     }
+
 }
