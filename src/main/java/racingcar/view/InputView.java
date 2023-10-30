8c14b9f422114a618fa2f78716b4f6cc32f52a94
Leehyoju97 <dlgywn2420@gmail.com>
2023-10-30 17:21:39 +0900

    refactor: 차이름 받을 때 String[] -> String으로 반환형 변환

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,9 @@ public class InputView {
     private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public static String[] inputCarName() {
+    public static String inputCarName() {
         System.out.println(START_MESSAGE);
-        return Input.carSplit(Input.inputString());
+        return Input.inputString();
     }
 
     public static String inputNumberOfAttempts() {
