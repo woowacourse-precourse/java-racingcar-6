be01edaa24fc70232f69423b5509d30a75470f6a
hee9841 <lee98417716@gmail.com>
2023-10-31 02:54:44 +0900

    feat(racingcar): 게임 조건상수를 가져오는 기능 구현

--- src/main/java/racingcar/domain/GameCondition.java
+++ src/main/java/racingcar/domain/GameCondition.java
@@ -1,5 +1,19 @@
 package racingcar.domain;
 
+public enum GameCondition {
+    NAME_SIZE(5),
+    MOVING_FORWARD(4),
+    MIN_RANDOM_NUM(0),
+    MAX_RANDOM_NUM(9);
 
-public class GameCondition {
+
+    private final int value;
+
+    GameCondition(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
