b74f53f4b7d1cce508e6ec939675b0e0324fcebf
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 17:35:35 +0900

    feat(CarManager): 저장된 Cars 를 규칙에 맞게 이동시킨다.

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -3,17 +3,33 @@ package racingcar;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CarManager {
     private Cars cars;
+    private RandomNumberGenerator randomNumberGenerator;
+
+    public CarManager(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    private static final Integer MOVEMENT_CONDITION = 4;
 
     public void createCars(String[] names) {
         List<Car> carList = Arrays.stream(names).map(Car::new).toList();
 
         cars = new Cars(carList);
+    }
+
+    public void moveCars() {
+        cars.getCarList().forEach(car -> {
+            Integer randomInteger = randomNumberGenerator.getRandomInteger();
+
+            if (randomInteger >= MOVEMENT_CONDITION) {
+                car.incrementDistance();
+            }
+        });
     }
 
     public Cars getCars() {
