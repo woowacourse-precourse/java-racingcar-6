529c9cdc50bd4d9106872884f39eeb05d9148009
yeonsu <jack0810@kookmin.ac.kr>
2023-10-30 17:50:38 +0900

    feat: valid input below 5 words?

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,19 +8,36 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        inputCarName();
+        try{
+            inputCarName();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static void inputCarName(){
+    public static void inputCarName() {
         String tm = Console.readLine();
-
-        System.out.println(Arrays.toString(parseCarName(tm)));
+        String[] carNames = parseCarName(tm);
+        
     }
 
-    public static String[] parseCarName(String input){
+    public static String[] parseCarName(String input) {
 
         String[] names = input.split(",");
 
+        for (String name : names) {
+            validateCarName(name);
+        }
         return names;
+    }
+
+    public static void validateCarName(String name) {
+        isBelowFiveWord(name);
+    }
+
+    static void isBelowFiveWord(String name) {
+        if (5 < name.length()){
+            throw new IllegalArgumentException("자동차의 이름이 5글자 이상입니다.");
+        }
     }
 }
