e64fb2650033a9c0711a9f82696fe333df4fb69a
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-31 00:38:44 +0900

    docs : 레이싱을 하기위한 메소드

--- src/main/java/racingcar/Controller/RacingController.java
+++ src/main/java/racingcar/Controller/RacingController.java
@@ -1,13 +1,8 @@
 package racingcar.Controller;
 
-import racingcar.View.InputView;
 
 public class RacingController {
     public void playRacing(){
-        InputView.printAskCarName();
-        //MoveRandomNumber.generateRandomGameNumber();
-
         WinnerController.judgeWin();
-
     }
 }
