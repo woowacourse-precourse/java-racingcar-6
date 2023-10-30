999df4d984977a4e00a3f3d6e7455f5ee14b3c28
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 03:54:08 +0900

    test : 구분자가 쉼표 외에 다른 것이 들어왔을 경우 및 차를 2대 이상 입력하지 않았을 경우 테스트 코드 작성
    구분자를 구별하기 위해 입력값에 문자,숫자,쉼표 외에 다른 것이 있으면 예외처리, 경주 이므로 차를 0대 혹은 1대만 입력하면 예외처리

--- src/test/java/racingcar/exception/UserInputExceptionTest.java
+++ src/test/java/racingcar/exception/UserInputExceptionTest.java
@@ -2,6 +2,11 @@ package racingcar.exception;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -9,6 +14,9 @@ class UserInputExceptionTest {
     private UserInputException userInputException;
     private static final String LENGTH_ERROR_MESSAGE = "자동차 이름은 1 이상, 5 이하로 입력해 주세요";
     private static final String BLANK_STRING_MESSAGE = "빈 문자를 입력하셨습니다.";
+    private static final String DUPLICATE_MESSAGE = "자동차 이름은 중복되지 않게 입력해주세요";
+    private static final String ONLY_COMMA = "구분자는 쉼표로만 작성해주세요";
+    private static final String INPUT_MORE_THAN_TWO_CAR = "차를 두대 이상 입력해주세요";
 
     @Test
     void 이름_길이가_유효할_때() {
@@ -33,5 +41,41 @@ class UserInputExceptionTest {
         assertDoesNotThrow(() -> UserInputException.isBlank(input));
     }
 
+    @Test
+    void 중복된_값이_존재_할_때(){
+        List<String> input = Arrays.asList("pobi","pobi","woni");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> UserInputException.isDuplicate(input));
+        assertEquals(DUPLICATE_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void 중복된_값이_존재하지_않을_때() {
+        List<String> input = Arrays.asList("pobi","jun","woni");
+        assertDoesNotThrow(() -> UserInputException.isDuplicate(input));
+    }
+
+    @Test
+    void 컴마_숫자_문자_이외의_것_일_때() {
+        String input = "pobi.woni,jun";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> UserInputException.isComma(input));
+        assertEquals(ONLY_COMMA, exception.getMessage());
+    }
+    @Test
+    void 컴마_숫자_문자만_있을_때(){
+        String input = "pobi,woni,jun";
+        assertDoesNotThrow(() -> UserInputException.isComma(input));
+    }
+
+    @Test
+    void 자동차_두_대_이상_입력_안_했을_때(){
+        List<String> input = Arrays.asList("pobi");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> UserInputException.isMoreThan(input));
+        assertEquals(INPUT_MORE_THAN_TWO_CAR, exception.getMessage());
+    }
+    @Test
+    void 자동차_두_대_이상_입력_했을_때(){
+        List<String> input = Arrays.asList("pobi","woni");
+        assertDoesNotThrow(() -> UserInputException.isMoreThan(input));
+    }
 
 }
