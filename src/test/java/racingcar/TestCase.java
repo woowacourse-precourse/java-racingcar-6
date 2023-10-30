f521d3e95e2cb61491ff508572257f8237884192
SeongJunChun <csj991210@naver.com>
2023-10-31 07:15:51 +0900

    TEST  :  자동차 전진에 관련된 테스트 케이스 작성

--- src/test/java/racingcar/TestCase.java
+++ src/test/java/racingcar/TestCase.java
@@ -1,19 +1,28 @@
 package racingcar;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import racingcar.View.SystemMessage;
+import racingcar.util.ExceptionHandling;
+import racingcar.model.CarAction;
 import racingcar.model.Cars;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TestCase {
+    @AfterEach
+    public void afterEach(){
+        Cars.carList = new ArrayList<>();
+    }
     @Test
     void 주어진_값을_구분() {
         String input = "ab,cd,df";
         Cars cars = new Cars(input);
-        List<String> result = cars.carList;
+        List<String> result = Cars.carList;
         assertThat(result).contains("ab","cd","df");
     }
 
@@ -41,6 +50,24 @@ public class TestCase {
                 .hasMessageContaining("자동차 이름은 중복이 될수 없습니다.");
 
     }
+
+    @Test
+    void 랜덤_숫자에_따라_이동(){
+        String input = "ab,cd,df";
+        Cars cars = new Cars(input);
+        CarAction carAction = new CarAction();
+        carAction.carMoveCount();
+        List<Integer> result = Cars.countList;
+        assertThat(result).contains(1);
+    }
+    @Test
+    void 숫자0_입력시_예외발생(){
+        String input = "0";
+        assertThatThrownBy(() -> ExceptionHandling.isNumberZero(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("0은 입력할 수 없습니다");
+    }
+
 
 
 
