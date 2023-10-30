16b379b0ec0be88d9d191ab76ae83412f67dc6e0
jeondui <jeondui@gmail.com>
2023-10-30 17:48:19 +0900

    feat(RacingGame): 시도 횟수 검증 기능

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.util.InputValueValidator;
 
 public class RacingGame {
 
     private List<String> winners;
+    private final InputValueValidator inputValueValidator;
 
     public RacingGame() {
         this.winners = new ArrayList<>();
+        this.inputValueValidator = new InputValueValidator();
     }
 
     public List<String> getWinners(List<Car> carList) {
@@ -26,6 +29,10 @@ public class RacingGame {
         return carList.stream()
                 .max(Comparator.comparingInt(Car::getLocation))
                 .get().getLocation();
+    }
+
+    public int getTryCount(String tryCount) {
+        return inputValueValidator.validateTryCount(tryCount);
     }
 
 }
