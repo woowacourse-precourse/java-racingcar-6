cb2d5d8b09f28ad748215ac0c344f01d862a76ad
dsa04156 <dsa04156@naver.com>
2023-10-31 01:10:42 +0900

    feat : 우승자가 여러명일 경우 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -16,10 +16,24 @@ class ApplicationTest extends NsTest {
     void 전진_정지() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                    run("pobi,woni", "5");
+                    assertThat(output()).contains("pobi : -----", "woni : ", "최종 우승자 : pobi");
                 },
-                MOVING_FORWARD, STOP
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD,
+                STOP
+        );
+    }
+
+    void 우승자가_여러명일_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,java", "5");
+                    assertThat(output()).contains("pobi : -----", "woni : -----", "java : -----",
+                            "최종 우승자 : pobi,woni,java");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
         );
     }
 
@@ -43,6 +57,14 @@ class ApplicationTest extends NsTest {
     void 이름이_중복일_때_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도_할_횟수가_숫자가_아닌_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobia", "a"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
