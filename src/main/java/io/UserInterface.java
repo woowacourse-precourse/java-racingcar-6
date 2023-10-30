2f95e1eabb76fb93064ab41dd343aebe052ff10b
yunseung <yseungun.21@gmail.com>
2023-10-30 22:03:00 +0900

    feat(UserInterface): getAttemptsNumber 메서드 추가

--- src/main/java/io/UserInterface.java
+++ src/main/java/io/UserInterface.java
@@ -1,5 +1,6 @@
 package io;
 
+import static constant.Constant.ATTEMPTS_NUMBER_REQUEST_MESSAGE;
 import static constant.Constant.CAR_NAME_REQUEST_MESSAGE;
 
 import basis.Converter;
@@ -8,6 +9,12 @@ import java.util.LinkedHashMap;
 
 public class UserInterface {
     Converter converter = new Converter();
+
+    public int getAttemptsNumber() {
+        System.out.println(ATTEMPTS_NUMBER_REQUEST_MESSAGE);
+        String attemptsString = Console.readLine();
+        return converter.wordToInt(attemptsString);
+    }
 
     public LinkedHashMap<String, String> getCarNameHashMap() {
         System.out.println(CAR_NAME_REQUEST_MESSAGE);
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,27 +1,21 @@
 package racingcar;
 
-import static constant.Constant.ATTEMPTS_NUMBER_REQUEST_MESSAGE;
 import static constant.Constant.RUN_RESULT_START_MESSAGE;
 import static constant.Constant.WINNER_RESULT_MESSAGE;
 
-import basis.Converter;
 import basis.MovingStatus;
 import basis.WinnerResult;
-import camp.nextstep.edu.missionutils.Console;
 import io.UserInterface;
 import java.util.LinkedHashMap;
 
 public class Application {
-    static Converter converter = new Converter();
     static MovingStatus movingStatus = new MovingStatus();
     static WinnerResult winnerResult = new WinnerResult();
     static UserInterface userInterface = new UserInterface();
 
     public static void main(String[] args) {
-        LinkedHashMap<String,String> carNameHashMap = userInterface.getCarNameHashMap();
-        System.out.println(ATTEMPTS_NUMBER_REQUEST_MESSAGE);
-        String attemptsString = Console.readLine();
-        int attemptsNumber = converter.wordToInt(attemptsString);
+        LinkedHashMap<String, String> carNameHashMap = userInterface.getCarNameHashMap();
+        int attemptsNumber = userInterface.getAttemptsNumber();
         System.out.println();
         System.out.println(RUN_RESULT_START_MESSAGE);
         movingStatus.printRaceResult(attemptsNumber, carNameHashMap);
