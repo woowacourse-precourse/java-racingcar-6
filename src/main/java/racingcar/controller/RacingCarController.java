5e281a19faea84d33a1394e970b37becf3094c44
ChaeAg <dydkglel123@gmail.com>
2023-10-31 01:23:53 +0900

    refactor(RacingCarController): 주석 삭제

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -17,35 +17,20 @@ public class RacingCarController {
     private static final Cars cars = new Cars();
 
     public void run() {
-        // 자동차 입력 안내 메세지 출력
         outputView.carNameInputGuide();
-
-        // 자동차 이름 입력
         String inputCarNames = carNamesInput();
-
-        // 자동차 객체 리스트 생성
         createCars(inputCarNames);
-
-        // 시도할 횟수 입력 안내 메세지 출력
         outputView.timesInputGuide();
-
-        // 시도할 횟수 입력
         int times = timesInput();
-
-        // '실행 결과' 메세지 출력
         outputView.resultGuide();
-
-        // 경주 시작
         for (int i = 0; i < times; i++) {
             startRacing();
         }
-
         outputView.printFinalWinner(formatWinnerNames(cars.getWinner()));
     }
 
     private void createCars(String inputCarNames) {
         List<Car> inputCarList = Arrays.stream(inputCarNames.split(",")).map(Car::new).collect(Collectors.toList());
-
         cars.addCars(inputCarList);
     }
 
@@ -55,7 +40,7 @@ public class RacingCarController {
 
     private String carNamesInput() {
         String input = inputView.carNamesInput();
-        invalidInputException.checkInput(input);
+        invalidInputException.checkCarNames(input);
         return input;
     }
 
