4db01b4a333a9fd3f370a0185fd7fda65f2fdcc7
lsj5657 <lsj56575@naver.com>
2023-10-30 13:13:48 +0900

    우승자 목록을 출력하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,6 +14,6 @@ public class Application {
             playGame.simulation();
             playGame.printResult();
         }
-
+       playGame.printWinnerList();
     }
 }
--- src/main/java/racingcar/PlayGame.java
+++ src/main/java/racingcar/PlayGame.java
@@ -3,15 +3,16 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class PlayGame{
+public class PlayGame {
     List<Integer> moveCount;
-    List<String> nameList ;
+    List<String> nameList;
 
 
     PlayGame(List<String> argumentNameList) {
-        nameList=argumentNameList;
+        nameList = argumentNameList;
         moveCount = new ArrayList<>(Collections.nCopies(nameList.size(), 0));
 
     }
@@ -21,28 +22,47 @@ public class PlayGame{
     }
 
     public void simulation() {
-        int go=4;
+        int go = 4;
         for (int i = 0; i < moveCount.size(); i++) {
             if (getRandomNumber() >= go) moveCount.set(i, moveCount.get(i) + 1);
         }
     }
 
-    public void printCount(int index){
-        for (int i=0; i<moveCount.get(index); i++)
+    public void printCount(int index) {
+        for (int i = 0; i < moveCount.get(index); i++)
             System.out.print("-");
     }
 
 
-    public void printResult(){
+    public void printResult() {
         System.out.println("실행 결과");
-        for (int i=0; i<nameList.size(); i++){
-            System.out.print(nameList.get(i)+" : ");
+        for (int i = 0; i < nameList.size(); i++) {
+            System.out.print(nameList.get(i) + " : ");
             printCount(i);
             System.out.println();
         }
         System.out.println();
     }
 
+    public int maxScore() {
+        return Collections.max(moveCount);
+    }
 
+    public List<String> getWinnerList() {
+        List<String> winnerList = new ArrayList<>();
+        for (int i = 0; i < moveCount.size(); i++) {
+            if (moveCount.get(i) == maxScore()) winnerList.add(nameList.get(i));
+        }
+        return winnerList;
+    }
 
+    public void printWinnerList() {
+        System.out.print("최종 우승자 : ");
+
+       for (int i = 0; i < getWinnerList().size(); i++) {
+            System.out.print(getWinnerList().get(i));
+            if (i < getWinnerList().size() - 1) System.out.print(",");
+        }
+        System.out.println();
+    }
 }
