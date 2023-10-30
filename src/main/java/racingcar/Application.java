d36728672ef802ee6f518c0805b7b8f59d86a49c
grax <grax@172.29.112.89>
2023-10-30 15:45:11 +0900

    comment: "bring project completed"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	// Fork&Clone -> create new branch -> bring file in my eclipse 
     }
 }
