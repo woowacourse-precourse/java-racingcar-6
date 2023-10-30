c43fcd5b5951890c243027f7a46c8d91346e78c4
ohjuyoungg <ejrm12@naver.com>
2023-10-30 18:55:19 +0900

    feat(Application) : 메인 메소드 내에 게임 시작하는 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.view.Inputview;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Inputview inputview = new Inputview();
+        inputview.startGame();
     }
 }
