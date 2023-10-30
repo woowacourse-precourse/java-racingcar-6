210866bb2b5e25e57d44bc35adc0909364b33a6f
seokmyungham <hellotjraud@gmail.com>
2023-10-30 16:18:05 +0900

    feat(Race): validateAttempt, validateNullOrBlank, validatePositiveInteger 메서드 추가
    validateAttempt: validateNullOrBlank, validatePositiveInteger를 호출합니다
    validateNullOrBlank: 시도 회수가 공백인지 검증하고 예외를 발생시킵니다
    validatePositiveInteger: 시도 회수가 양의 정수인지 검증하고 예외를 발생시킵니다

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -6,7 +6,8 @@ public class Race {
     private ArrayList<Car> cars;
     private int attempt;
 
-    public Race(int attempt) {
+    public Race(String input) {
+        validateAttempt(input);
         this.cars = new ArrayList<>();
     }
 
@@ -21,6 +22,23 @@ public class Race {
         for (Car car : cars) {
             car.moveForward();
             car.print();
+        }
+    }
+
+    private void validateAttempt(String input) {
+        validateNullOrBlank(input);
+        validatePositiveInteger(input);
+    }
+
+    private void validateNullOrBlank(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("시도 회수는 공백일 수 없습니다.");
+        }
+    }
+
+    private void validatePositiveInteger(String input) {
+        if (!(input.matches("^[1-9]\\d*$"))) {
+            throw new IllegalArgumentException("시도 회수는 양의 정수만 가능합니다.");
         }
     }
 }
