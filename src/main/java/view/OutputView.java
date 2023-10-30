01e2ff36916632a53731495f11ec4538b364c3ec
jaegyeongkim <dongji_11@naver.com>
2023-10-30 20:19:55 +0900

    feat(OutputView): 최종 우승자를 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,14 +1,37 @@
 package view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import model.Car;
 import model.Cars;
 
 public class OutputView {
     static final String EXECUTION_RESULT = "실행 결과";
+    static final String FINAL_WINNER = "최종 우승자 : ";
+
     public void printCarsPosition(Cars cars) {
         System.out.println(EXECUTION_RESULT);
         for (Car car : cars.getCars()) {
             System.out.println(car.getName() + " : " + car.getNowPlace());
         }
+    }
+
+    public void printWinner(Cars cars) {
+        List<Car> winners = cars.getWinner();
+        String winnersAndDelimiter = "";
+        if (getMultipleWinners(winners)) {
+            winnersAndDelimiter = winners.stream().map(Car::getName)
+                    .collect(Collectors.joining(", "));
+            System.out.print(FINAL_WINNER + winnersAndDelimiter);
+        }
+
+    }
+
+    public boolean getMultipleWinners(List<Car> winners) {
+        if (winners.size()==1) {
+            System.out.print(FINAL_WINNER + winners.get(0).getName());
+            return false;
+        }
+        return true;
     }
 }
