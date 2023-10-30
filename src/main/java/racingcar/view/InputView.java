27ccb92c578143027bb8d25263b4bb6c422e024c
kmw2378 <rlarla677@gmail.com>
2023-10-30 23:43:23 +0900

    feat(racingcar): InputView에 게임 진행횟수를 입력받는 메소드, 숫자 여부 검증 메소드 추가 게임 진행횟수를 입력받아 TotalRoundNumberRequest 객체를 반환하는 메소드를 추가하였습니다. 추가로, 숫자 여부를 검증하여 예외를 발생시키는 메소드를 추가하였습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.dto.request.CarNameRequest;
+import racingcar.dto.request.TotalRoundNumberRequest;
 import racingcar.exception.UserInputException;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import java.util.stream.Collectors;
 
 import static racingcar.config.GameConfig.INPUT_NAMES_SEPARATOR;
 import static racingcar.exception.errorcode.UserInputErrorCode.INPUT_VALUE_BLANK;
+import static racingcar.exception.errorcode.UserInputErrorCode.INPUT_VALUE_NOT_NUMERIC;
 
 public final class InputView {
     private InputView() {
@@ -22,9 +24,24 @@ public final class InputView {
         return Arrays.stream(carNames.split(INPUT_NAMES_SEPARATOR)).map(CarNameRequest::new).collect(Collectors.toList());
     }
 
+    public static TotalRoundNumberRequest requestTotalRoundNumber() {
+        final String totalRound = Console.readLine();
+        validateBlank(totalRound);
+        validateNumeric(totalRound);
+        return new TotalRoundNumberRequest(Integer.parseInt(totalRound));
+    }
+
     private static void validateBlank(final String input) {
         if (input.isBlank()) {
             throw new UserInputException(INPUT_VALUE_BLANK);
+        }
+    }
+
+    private static void validateNumeric(final String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new UserInputException(INPUT_VALUE_NOT_NUMERIC);
         }
     }
 }
