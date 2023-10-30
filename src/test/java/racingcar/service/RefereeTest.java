3b2d3ceb16f78c3dcff77e133048701ca6aa3d27
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-30 21:10:28 +0900

    test(RefereeTest): 최종 우승자 명단 생성 기능 테스트 구현

--- src/test/java/racingcar/service/RefereeTest.java
+++ src/test/java/racingcar/service/RefereeTest.java
@@ -30,4 +30,26 @@ class RefereeTest {
         assertThat(maxDistance).isNotEqualTo(0);
     }
 
+    @Test
+    void generateRaceWinnersList_자동차_전진하기_전_테스트() {
+        String[] carNames = {"test", "car", "name"};
+        List<RacingCar> racingCars = RacingCarList.generateRacingCarList(carNames);
+
+        String winnersList = Referee.generateRaceWinnersList(racingCars);
+        assertThat(winnersList).isEqualTo("test, car, name");
+    }
+
+    @Test
+    void generateRaceWinnersList_자동차_한대만_전진_후_테스트() {
+        String[] carNames = {"test", "car", "name"};
+        List<RacingCar> racingCars = RacingCarList.generateRacingCarList(carNames);
+
+        for (int i = 0; i < 100; i++) {
+            racingCars.get(0).moveForward();
+        }
+
+        String winnersList = Referee.generateRaceWinnersList(racingCars);
+        assertThat(winnersList).isEqualTo("test");
+    }
+
 }
