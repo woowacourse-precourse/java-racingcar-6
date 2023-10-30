fb6fcdfed11069d1029132022340da4b12af5868
kang yeon joo <rkdduswndi@naver.com>
2023-10-30 11:31:50 +0900

    feat: RacingCar에 무작위 숫자 받아 전진 여부 판단하는 기능 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
     private String name;
+    private int count=0;
+
     public RacingCar(String input_name) {
         if(!is_valid_input_name(input_name)){
             new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
@@ -11,6 +15,23 @@ public class RacingCar {
 
     static boolean is_valid_input_name(String inputName) {
         return 0<inputName.length() && inputName.length()<6;
+    }
+
+    public void move() {
+        if(getRandomNum()>=4)
+            this.count++;
+    }
+
+    private int getRandomNum() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    public void printNowCount(){
+        System.out.print(this.name + " : ");
+        for(int i=0;i<this.count;i++){
+            System.out.print('-');
+        }
+        System.out.println();
     }
 
     @Override
