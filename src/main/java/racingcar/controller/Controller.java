c546a09f644c88bf135cb625813b7c937740c093
Minju Ji <hinote444@gmail.com>
2023-10-30 19:21:52 +0900

    feat(Controller): 이동 횟수를 저장하는 객체 생성 기능 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -2,10 +2,16 @@ package racingcar.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.domain.Attempt;
 import racingcar.domain.Car;
 import racingcar.view.InputView;
 
 public class Controller {
+
+    public void createAttempt() {
+        Attempt attempt = new Attempt(InputView.inputAttemptCount());
+    }
+
     public List<Car> addCarToList() {
         List<Car> cars = InputView.inputCarName()
                                   .stream()
