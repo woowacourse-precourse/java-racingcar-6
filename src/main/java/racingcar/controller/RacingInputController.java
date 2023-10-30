4a9b41c5e5a7788cd35806230e5ac73dfa3ae1fc
Sully <2dcoder@naver.com>
2023-10-30 11:30:22 +0900

    feat(RacingInputController): 검증 후 RacingCars를 생성하는 컨트롤러 로직 추가

---
+++ src/main/java/racingcar/controller/RacingInputController.java
@@ -0,0 +1,28 @@
+package racingcar.controller;
+
+import static racingcar.util.Parser.parseCarName;
+
+import java.util.List;
+import racingcar.domain.RacingCars;
+import racingcar.domain.validator.CarNameValidator;
+import racingcar.view.input.RacingInputView;
+
+public class RacingInputController {
+
+    private final RacingInputView racingInputView = new RacingInputView();
+
+    public RacingCars createRacingCars() {
+        try {
+            List<String> carNames = parseCarName(racingInputView.inputCarName());
+
+            CarNameValidator carNameValidator = new CarNameValidator(carNames);
+            carNameValidator.validateAll();
+
+            return new RacingCars(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return this.createRacingCars();
+        }
+    }
+
+}
