48fd5bda9b17ccee869a7bf3e06fc36b8a409657
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-10-31 01:51:35 +0900

    이름 입력 및 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,78 +2,71 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import net.bytebuddy.implementation.bytecode.ShiftRight;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Character.getName;
-
 public class Application {
 
-        // TODO: 프로그램 구현
-        // TODO: 들여쓰기 3이하
         public static void main(String[] args) {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-//                startGame();                  //TODO: 가시성을 위해 추후에 시작점이 될 함수
-
-                List<String> participants = new ArrayList<>();  //TODO: (임시)입력된 이름이 저장
-                participantsInput(participants);
+                String participantInputName = Console.readLine();
+                participantsInput(participantInputName);        // 이름 저장
         }
 
-        public static void participantsInput(List<String> partInput) {      //명단 메소드
+        public static void participantsInput(String participantInputName) {      //명단 메소드
                 List<Car> participants = new ArrayList<>();
-                String participantInputName = Console.readLine();
-//                try {     //todo: try catch문 삭제
-                for (String name : participantInputName.split(",")) {
+                String[] names = participantInputName.split(",");
+                for (int i=0;i<names.length;i++){
+                        String name = names[i];
                         if (!participants.contains(name)) {
                                 participants.add(new Car(name, 0));
                         } else {
-                                System.out.println("중복된 이름이 있습니다. 입력입니다.");
+                                System.out.println("중복된 이름이 있습니다.");
                                 throw new IllegalArgumentException();
                         }
+                        validateName(name);
+                }
+                String participant = String.join(",",names);
+                        System.out.print(participant);
+        }
+        public static Boolean validateName(String name){
                         if (name.length() > 5 || name.length() == 0) {
                                 System.out.println("5글자이상의 이름이 있습니다.");
                                 System.out.println(name.length());
                                 throw new IllegalArgumentException();
                         }       //                                유효성 검사
-                        System.out.println(name);
-                }
-
-//                } catch (IllegalArgumentException e) {
-//                        return;
-//                }
-                System.out.println("시도할 회수는 몇회인가요?");
-                String inputMoves = Console.readLine();
-                int intMove = Integer.parseInt(inputMoves);
-                // 사람 수만큼 돌게 해야함
-
-                for (int i = 0; i < intMove; i++) {
-                        System.out.println(i+1 + "회차");
-                        for (Car car : participants) {
-                                car.move();
-                                System.out.println(car.getName() + " : " + car.getMoveStatus());
-                        }
-                        System.out.println();
-                }
-                int maxMove = 0;
-                List<String> winner = new ArrayList<>();
-                for (Car car : participants) {
-//                        maxMove = Math.max(maxMove, car.getMove());   //TODO 질문 왜 이건 안되고
-                        int carMove = car.getMove();
-                        if (carMove > maxMove) {
-                                maxMove = carMove;                      //TODO 얘를 추가하는게 무슨 상관?
-                                winner.clear();
-                                winner.add(car.getName());
-                        } else if (carMove == maxMove) {
-                                winner.add(car.getName());
-                        }
-                }
-                System.out.print("최종 우승자 : " + String.join(",",winner));          // 최종우승자 출력
-
-
+                return true;
         }
+//        public static void attemptInput(String inputMoves) {
+//                int move = Integer.parseInt(inputMoves);
+//                System.out.println(move + "회차");
+//                System.out.println("시도할 회수는 몇회인가요?");
+//                String inputMoves = Console.readLine();
+//                int intMove = Integer.parseInt(inputMoves);
+//                // 사람 수만큼 돌게 해야함
+//
+//                for (int i = 0; i < intMove; i++) {
+//                        System.out.println(i+1 + "회차");
+//                        for (Car car : participants) {
+//                                car.move();
+//                                System.out.println(car.getName() + " : " + car.getMoveStatus());
+//                        }
+//                        System.out.println();
+//                }
+//                int maxMove = 0;
+//                List<String> winner = new ArrayList<>();
+//                for (Car car : participants) {
+//                        int carMove = car.getMove();
+//                        if (carMove > maxMove) {
+//                                maxMove = carMove;
+//                                winner.clear();
+//                                winner.add(car.getName());
+//                        } else if (carMove == maxMove) {
+//                                winner.add(car.getName());
+//                        }
+//                }
+//                System.out.print("최종 우승자 : " + String.join(", ",winner));          // 최종우승자 출력
+//        }
 }
 
 // Ming,Seo,Park,Bin,Seong
@@ -99,7 +92,7 @@ class Car {
         }
 
         public void move() {
-                int random = Randoms.pickNumberInRange(1, 9);
+                int random = Randoms.pickNumberInRange(0, 9);
                 if (random >= 4) {
                         move += 1;
                 }
