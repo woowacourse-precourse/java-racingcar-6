bcc24f342b46d8c82c496927dc750f9b7a66818d
qudcks8084 <https://github.com/qudcks8084>
2023-10-30 19:53:23 +0900

    테스트를 위한 파일 생성 및 시험

---
+++ src/test/java/racingcar/MyTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+public class MyTest extends NsTest {
+
+    void Duplicate_CarName_Test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("Park,Park", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
