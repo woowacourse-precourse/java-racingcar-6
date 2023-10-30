199cc50caba87047328504429b15860954a8aa7b
recreate.kang@gmail.com <zetiburon666@gmail.com>
2023-10-30 18:51:28 +0900

    feat(Application) : Basic methods and its rolls created

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	Run(Car_Names(),Try_Numb());
+    }
+    
+    public static void Run(List<String> Car_Names, Integer Try_Numb) {
+    	
+    }
+    
+    public static ArrayList<String> Car_Names() {
+    	ArrayList<String> Car_Names = new ArrayList<>();
+    	    	
+    	return Car_Names;
+    }
+    
+    public static Integer Try_Numb() {
+    	Integer Try_numb = null;
+    	
+    	return Try_numb;
     }
 }
