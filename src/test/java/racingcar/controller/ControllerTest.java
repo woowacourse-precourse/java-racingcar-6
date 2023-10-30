2ed2d965d00e61606ac284e52c6396ed5135e513
hgh1472 <hgh1472@naver.com>
2023-10-31 02:09:17 +0900

    Test : Controller 테스트 코드 추가

--- src/test/java/racingcar/controller/ControllerTest.java
+++ src/test/java/racingcar/controller/ControllerTest.java
@@ -1,7 +1,26 @@
 package racingcar.controller;
 
-public class ControllerTest {
-    void 이름_쉼표로_구분() {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+
+    @Test
+    @DisplayName("구분자로 이름이 나누어지는지 테스트")
+    void splitNameTest() {
+        List<String> result = Controller.splitName("Steph,curry");
+        assertThat(result).contains("steph", "curry");
     }
+
+    @Test
+    @DisplayName("count를 횟수로 읽을 수 있는지 테스트")
+    void readCountTest() {
+        int result = Controller.readCount("6");
+        assertThat(result).isEqualTo(6);
+    }
+
+
 }
