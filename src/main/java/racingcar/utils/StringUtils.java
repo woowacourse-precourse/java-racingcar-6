990d1b8081d5baa62a5772d29caa62f174aaa020
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-30 21:26:33 +0900

    [refactor] : split car names method logic change

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -1,7 +1,10 @@
 package racingcar.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringUtils {
-    public static String[] splitCarNames(String carNames) {
-        return carNames.split(",");
+    public static List<String> splitCarNames(String carNames) {
+        return Arrays.asList(carNames.split(",")); // 한줄에 점하나만 찍는다 검증.
     }
 }
