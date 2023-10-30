0b0077e25f74e89576ad4a8f05ad42edcdc8103e
aaa <cnj991279@gmail.com>
2023-10-30 19:20:27 +0900

    feat: class 이름 변경 및 getCarNames 메서드 구현

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.core.Core;
+import racingcar.utils.Utils;
+
+import java.util.List;
+
+public class Race {
+    Core core = new Core();
+
+    public List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+        Utils.validateStringSeparateWithComma(carName);
+        List<String> carNames = core.stringToStringList(carName);
+        Utils.validateStringLength(carNames);
+        return carNames;
+    }
+}
