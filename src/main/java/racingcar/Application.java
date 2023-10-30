ec58e0b4a5c3cd7fd448512e2c8758532570fcfc
juns1s <juns1s@naver.com>
2023-10-31 00:56:36 +0900

    feat: Application MVC패턴에 맞게 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,10 @@
 package racingcar;
 
-import java.util.List;
+import racingcar.controller.RacingCarController;
 
 public class Application {
     public static void main(String[] args) {
-        List<Car> cars = RacingUtil.getCarsList();
-        int times = RacingUtil.getTimes();
-
-        RacingUtil.raceStart(cars, times);
-
-        List<String> winner = RacingJudge.raceJudge(cars);
-        System.out.println("최종 우승자 : " + String.join(", ", winner));
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.run();
     }
 }
