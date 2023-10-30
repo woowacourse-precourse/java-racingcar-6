4bbd8c25b312840aca62c19aa064584b7f031d13
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-30 13:25:15 +0900

    fix: static 추가
    carNames 메서드에 static 추가
    - 유틸리티 클래스에서 사용할 메서드이므로 static 추가

--- src/main/java/racingcar/Reader.java
+++ src/main/java/racingcar/Reader.java
@@ -8,7 +8,7 @@ public final class Reader {
         throw new UnsupportedOperationException();
     }
 
-    public String[] carNames() {
+    public static String[] carNames() {
         return Console.readLine().split(",");
     }
 
