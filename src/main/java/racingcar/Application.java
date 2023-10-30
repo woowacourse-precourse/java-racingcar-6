2fb7530792a61b5a1c9b04cad97cc8a50beeba48
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-31 02:37:22 +0900

    feat : 메인클래스 메인메소드에서 Controller 안에 gameStart 메소드만 실행하면 모든 클래스가 동작하도록 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.GameStartController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameStartController gameStartController = new GameStartController();
+
+        gameStartController.gameStart();
     }
 }
