7284c824ca2f66f852395df0fb92eefd89e90530
junghunim07 <dlawndgns9@naver.com>
2023-10-30 16:11:43 +0900

    feat(GameManager): playGame 메소드 및 Car객체 생성 메소드 구현

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -8,5 +8,17 @@ public class GameManager {
 
     public GameManager() {
         InputValue inputValue = new InputValue();
+        playGame();
+    }
+
+    private void playGame() {
+        makeCars(InputValue.carName);
+    }
+
+    private void makeCars(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Car car = new Car(list.get(i));
+            carRace.add(car);
+        }
     }
 }
