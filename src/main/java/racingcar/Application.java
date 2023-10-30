6270102a164e96bce7f8e04c66aa9c54b583999e
EHCTH <lees90896@gmail.com>
2023-10-30 16:09:41 +0900

    re

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,6 @@ import racingcar.controller.RacingGame;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingGame racingGame = new RacingGame();
+        RacingGame racing = new RacingGame();
     }
 }
