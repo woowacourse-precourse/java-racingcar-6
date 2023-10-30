c012412197a850117866d1b9eea0ae3f82eaf345
minseo  jo <liging12@naver.com>
2023-10-30 15:08:28 +0900

    test: CarId 테스트 코드 추가

---
+++ src/test/java/racingcar/vo/CarIdTest.java
@@ -0,0 +1,27 @@
+package racingcar.vo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CarIdTest {
+
+    @Test
+    void CarName_생성() {
+        CarId carId = new CarId(1);
+        Assertions.assertThat(carId.getValue()).isEqualTo(1);
+    }
+
+    @Test
+    void 같은_VO_동등성_검사() {
+        CarId carId1 = new CarId(1);
+        CarId carId2 = new CarId(1);
+        Assertions.assertThat(carId1.getValue()).isEqualTo(carId2.getValue());
+    }
+
+    @Test
+    void 같은_VO_동일성_검사() {
+        CarId carName1 = new CarId(1);
+        CarId carName2 = new CarId(1);
+        Assertions.assertThat(carName1).hasSameHashCodeAs(carName2);
+    }
+}
