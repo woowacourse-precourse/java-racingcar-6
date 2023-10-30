6eb108cb3ff508b230109d2be111b03ef00c4f4d
choiejae <ejae8319@gmail.com>
2023-10-30 23:05:25 +0900

    feat(RacingCar): 이름과 위치를 가진 RacingCar 클래스 구현
    최초 position 의 위치를 0 으로 초기화
    move의 책임을 가지도록 구현(FowardMethod 를 외부에서 받아 움직임 적용)

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+public record RacingCar(Name name, Position position) implements Comparable<RacingCar> {
+
+    private static final int INITIAL_POSITION = 0;
+
+    public RacingCar(String name) {
+        this(new Name(name), new Position(INITIAL_POSITION));
+    }
+
+    public RacingCar(RacingCar racingCar) {
+        this(racingCar.name, racingCar.position);
+    }
+
+    public RacingCar move(ForwardMethod forwardMethod) {
+
+        if (forwardMethod.isMovable()) {
+            return new RacingCar(name, new Position(position.currentPosition() + 1));
+        }
+        return this;
+    }
+
+    public String getName() {
+        return name.name();
+    }
+
+    public int getPosition() {
+        return position.currentPosition();
+    }
+
+    @Override
+    public int compareTo(RacingCar racingCar) {
+        return Integer.compare(getPosition(), racingCar.getPosition());
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + position;
+    }
+}
---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,70 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class RacingCarTest {
+
+    @Test
+    void 문자열로_자동차를_생성할_수_있다() {
+        // given
+        String name = "ejae";
+
+        // when
+        RacingCar racingCar = new RacingCar(name);
+
+        // then
+        assertEquals(name, racingCar.getName());
+    }
+
+    @Test
+    void 생성된_자동차의_위치는_0이다() {
+        // given
+        String name = "ejae";
+
+        // when
+        RacingCar racingCar = new RacingCar(name);
+
+        // then
+        assertEquals(0, racingCar.getPosition());
+    }
+
+    @Test
+    void 이름이_같으면_같은_자동차다() {
+        // given
+        String name = "ejae";
+
+        // when
+        RacingCar racingCarA = new RacingCar(name);
+        RacingCar racingCarB = new RacingCar(name);
+
+        // then
+        assertEquals(racingCarA, racingCarB);
+    }
+
+    @Test
+    void 전진_기능이_true를_반환하면_자동차는_움직일_수_있다() {
+        // given
+        String name = "ejae";
+        RacingCar racingCar = new RacingCar(name);
+
+        // when
+        RacingCar relocatedracingCar = racingCar.move(() -> true);
+
+        // then
+        assertEquals(1, relocatedracingCar.getPosition());
+    }
+
+    @Test
+    void 전진_기능이_false를_반환하면_자동차는_움직일_수_없다() {
+        // given
+        RacingCar racingCar = new RacingCar("ejae");
+
+        // when
+        RacingCar relocatedracingCar = racingCar.move(() -> false);
+
+        // then
+        assertEquals(0, relocatedracingCar.getPosition());
+    }
+}
