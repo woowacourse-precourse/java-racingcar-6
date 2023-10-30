8e66bf08bd0952056202e2945eec8ca3706bbea9
beomsic <qjatjr29@khu.ac.kr>
2023-10-30 13:02:00 +0900

    :sparkles: Feat: Add controller class for racing game
    - 경주를 위한 input 정보를 받는 기능 추가
    - 입력받은 값들을 통해 레이싱 게임을 진행
    - 진행중, 진행 후 결과를 출력하는 기능 추가

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import static racingcar.view.IOMessage.PRINT_RESULT;
+
+import java.util.List;
+import racingcar.model.RacingGame;
+import racingcar.model.Result;
+import racingcar.view.InputView;
+import racingcar.view.OutPutView;
+
+public class RacingGameController {
+    public void run() {
+
+        String inputCarNames = InputView.inputCarNames();
+        RacingGame racingGame = RacingGame.of(inputCarNames);
+        int tryCount = InputView.inputTryCount();
+
+        playRacingGame(racingGame, tryCount);
+    }
+    private void printResults(List<Result> results) {
+        results.forEach(OutPutView::printResult);
+        OutPutView.println();
+    }
+
+    private void printWinnerResults(RacingGame racingGame) {
+        List<String> winnerNameList = racingGame.determineRaceWinners();
+        OutPutView.printWinner(winnerNameList);
+    }
+
+    private void playRacingGame(RacingGame racingGame, int tryCount) {
+        OutPutView.println();
+        OutPutView.printIOMessage(PRINT_RESULT);
+        for (int i = 0; i < tryCount; i++) {
+            List<Result> results = racingGame.play();
+            printResults(results);
+            if(i == tryCount - 1) printWinnerResults(racingGame);
+        }
+    }
+
+}
