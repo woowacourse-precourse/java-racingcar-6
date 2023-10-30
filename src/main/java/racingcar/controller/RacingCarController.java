b2476a76df7ec15230278d1c07303244080bcf97
kimdohyung <pdohyung@gmail.com>
2023-10-30 17:34:43 +0900

    feat(RacingCarController): 각 자동차가 랜덤 숫자를 뽑고 4 이상인지(전진하는지) 체크

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.view.InputView;
@@ -9,10 +10,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingCarController {
+    private static final int FORWARD_RULE = 4;
     private final InputView inputView;
 
     public RacingCarController() {
         this.inputView = new InputView();
+    }
+
+    private void checkEachCarForward(List<Car> cars) {
+        cars.forEach(this::checkAtLeastFour);
+    }
+
+    private void checkAtLeastFour(Car car) {
+        if (randomNumber() >= FORWARD_RULE) {
+            car.forward();
+        }
+    }
+
+    private int randomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     private List<Car> createCarList() {
