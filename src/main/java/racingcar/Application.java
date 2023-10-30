2632655b6cff3fd713751c303318d3d4909d9bc8
totocm <typesapk8445@gmail.com>
2023-10-30 20:05:25 +0900

    새로운 리드미..구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,11 @@
 package racingcar;
 
-import java.util.List;
 
 public class Application {
-  private static IoService io = new IoService();
   public static void main(String[] args) {
-    String input = io.gameStartInputMsg();
-    List<String> isNmaes = io.nameChangeNames(input);
-    System.out.println(isNmaes);
-    //경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)
-    //pibo,tototot,boblkd
-    //[pibo, tototot, boblkd]
+
+
+
 
 
   }
