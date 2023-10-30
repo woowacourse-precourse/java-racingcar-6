27f6b90b2374f0adb2c8f2cc8326dde248699652
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:12:51 +0900

    [feat] Add CarStorage class init method

--- src/main/java/racingcar/model/CarStorage.java
+++ src/main/java/racingcar/model/CarStorage.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarStorage {
@@ -7,4 +8,9 @@ public class CarStorage {
     private static List<Car> cars;
     private static List<Integer> stepsList;
     public CarStorage() {}
+    public static void init() { // GameManager에서 1번만 호출
+        cars = new ArrayList<>();
+        winners = new ArrayList<>();
+        stepsList = new ArrayList<>();
+    }
 }
