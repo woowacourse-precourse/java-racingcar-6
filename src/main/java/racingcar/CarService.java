1fcb41ec61b884ef781f521bf1b4539ad8febdf2
ddrongy <ddrongy@gmail.com>
2023-10-30 17:05:44 +0900

    feat: 자동차 이름 기능 구현

--- src/main/java/racingcar/CarService.java
+++ src/main/java/racingcar/CarService.java
@@ -6,6 +6,15 @@ import static racingcar.Number.*;
 
 public class CarService {
 
+    public Car create(String name){
+        checkName(name);
+        return new Car(name);
+    }
+    private void checkName(String name){
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5글자 이하여야 합니다.");
+        }
+    }
     public void move(Car car){
         Integer power = makeRandomPower();
         car.move(power);
--- src/main/java/racingcar/Number.java
+++ src/main/java/racingcar/Number.java
@@ -3,5 +3,5 @@ package racingcar;
 public class Number {
     public static final Integer NUMBER_RANGE_START = 0;
     public static final Integer NUMBER_RANGE_END = 9;
-
+    public static final Integer MAX_NAME_LENGTH = 5;
 }
