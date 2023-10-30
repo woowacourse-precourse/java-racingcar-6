3cbeba0755f43972e28ea62077e64f7f5bb3160c
hjhan <hjhan.0621@gmail.com>
2023-10-31 06:42:16 +0900

    feat(move()): 자동차의 랜덤값이 4이상이면 앞으로 나아가는 메서드 구현
    - Car 클래스에서 distance 변수 선언 및 0으로 초기화
    - move() 메서드를 통해 randomNumber가 4이상이면 distance값 1 증가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private String name;
     private int randomNumber;
+    private int distance = 0;
 
     public Car(String name) {
         this.name = name;
@@ -16,6 +17,12 @@ public class Car {
 
     private void setRandomNumber() {
         this.randomNumber = Randoms.pickNumberInRange(0, 9);
+    }
+
+    private void move() {
+        if (randomNumber >= 4) {
+            this.distance++;
+        }
     }
 
 }
