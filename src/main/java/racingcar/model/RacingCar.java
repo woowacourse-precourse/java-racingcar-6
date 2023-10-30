181262803cb59449d28b61e4f1792394293d7178
Dreaming-J <kimjjong321@naver.com>
2023-10-30 23:35:52 +0900

    :sparkles: feat. 자동차 이름

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -2,12 +2,21 @@ package racingcar.model;
 
 public class RacingCar {
     private static final int FORWARD_THRESHOLD = 4;
+    private static final int NAME_LIMIT = 5;
+    private static final String WRONG_NAME_LENGTH = "이름은 5자 이하만 입력됩니다.";
     private final String name;
     private int position;
 
     public RacingCar(String name) {
+        validate(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validate(String name) {
+        if (name.length() > NAME_LIMIT) {
+            throw new IllegalArgumentException(WRONG_NAME_LENGTH);
+        }
     }
 
     public boolean move(int speed) {
