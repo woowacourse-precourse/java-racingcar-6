9d494619c3beace06c843529a3b6ceda125ff19c
Sully <2dcoder@naver.com>
2023-10-30 14:02:38 +0900

    feat(RacingCar): 이동 거리 생성 로직 구현
    0에서 9의 난수를 생성하고 이 난수가 4 이상일 때 position(이동 거리)에 1을 더하도록 했다.

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
 
     private final String carName;
+    private int position = 0;
 
     public RacingCar(String carName) {
         this.carName = carName;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position += 1;
+        }
     }
 
 }
