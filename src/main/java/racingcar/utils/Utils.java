96265531be2ad2dd168252250ace4fe05a4d404b
aaa <cnj991279@gmail.com>
2023-10-30 19:11:02 +0900

    feat: validateStringLength 메서드 구현
    상수 MAXIMUM_CAR_NAME_LENGTH 선언하여 비교하는 수가 무엇인지 쉽게 알아볼 수 있게함

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,9 +1,21 @@
 package racingcar.utils;
 
+import java.util.List;
+
 public class Utils {
+    private final static int MAXIMUM_CAR_NAME_LENGTH = 5;
+    
     public static void validateStringSeparateWithComma(String input) {
         if (!input.contains(",")) {
             throw new IllegalArgumentException("자동차의 이름은 쉼표(,) 기준으로 구분되어야 합니다.");
+        }
+    }
+
+    public static void validateStringLength(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > MAXIMUM_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
+            }
         }
     }
 }
