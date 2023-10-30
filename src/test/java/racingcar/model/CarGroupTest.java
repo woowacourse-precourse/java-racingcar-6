2dfc7e41e991ef086d6b316ea594361dfb09a024
sinminseok12 <sinminseok12@naver.com>
2023-10-30 10:38:10 +0900

    refactor(CarGroupTest): 우승 자동차 이름을 찾는 기능 테스트 수정

--- src/test/java/racingcar/model/CarGroupTest.java
+++ src/test/java/racingcar/model/CarGroupTest.java
@@ -1,9 +1,10 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import racingcar.utils.NumberGenerator;
+import racingcar.utils.Movement;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,34 +15,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarGroupTest {
 
     @ParameterizedTest
-    @MethodSource("provideCarGroupTestArgument")
-    void 우승_자동차들을_찾는다(List<Integer> positions, List<String> results) {
-        CarGroup carGroup = new CarGroup(List.of("자동차1", "자동차2", "자동차3"), new TestRandomNumberFactory(positions));
+    @MethodSource("provideFindWinnersInformation")
+    void findWinnerNames_는_우승_자동차_이름을_반환한다(List<Boolean> moves, List<String> results) {
+        CarGroup carGroup = new CarGroup(List.of("자동차1", "자동차2", "자동차3"), new TestMovement(moves));
         carGroup.race();
         List<String> winners = carGroup.findWinnerNames();
         assertThat(winners).isEqualTo(results);
     }
 
-
-    static List<Arguments> provideCarGroupTestArgument() {
+    static List<Arguments> provideFindWinnersInformation() {
         return Arrays.asList(
-                Arguments.of(List.of(9, 9, 9), List.of("자동차1", "자동차2", "자동차3")),
-                Arguments.of(List.of(1, 1, 9), List.of("자동차3")),
-                Arguments.of(List.of(1, 9, 9), List.of("자동차2", "자동차3"))
+                Arguments.of(List.of(true, true, true), List.of("자동차1", "자동차2", "자동차3")),
+                Arguments.of(List.of(false, false, true), List.of("자동차3")),
+                Arguments.of(List.of(false, true, true), List.of("자동차2", "자동차3"))
         );
     }
 
-    private class TestRandomNumberFactory implements NumberGenerator {
+    class TestMovement implements Movement {
+        List<Boolean> values;
 
-        private List<Integer> numbers;
-
-        public TestRandomNumberFactory(List<Integer> numbers) {
-            this.numbers = new ArrayList<>(numbers);
+        TestMovement(List<Boolean> values) {
+            this.values = new ArrayList<>(values);
         }
 
         @Override
-        public int generate() {
-            return numbers.remove(0);
+        public boolean canMove() {
+            return values.remove(0);
         }
     }
+
 }
