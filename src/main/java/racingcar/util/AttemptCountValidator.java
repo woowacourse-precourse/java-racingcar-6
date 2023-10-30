bd8c5e5f03ff5b658d981c549e07f66663a57631
Somin Park <ps4708@naver.com>
2023-10-30 23:20:47 +0900

    refactor: 게임 횟수 입력 관련 메서드 반환값 수정

--- src/main/java/racingcar/util/AttemptCountValidator.java
+++ src/main/java/racingcar/util/AttemptCountValidator.java
@@ -4,13 +4,14 @@ public class AttemptCountValidator {
 
     private final static String INVALID_ATTEMPT_COUNT_MESSAGE = "시도 횟수는 숫자로만 입력해야 합니다";
 
-    public void validateAttemptCount(String input) {
+    public static int validateAttemptCount(String input) {
         if (!isNumeric(input)) {
             throw new IllegalArgumentException(INVALID_ATTEMPT_COUNT_MESSAGE);
         }
+        return Integer.parseInt(input);
     }
 
-    private boolean isNumeric(String input) {
+    private static boolean isNumeric(String input) {
         return input.matches("\\d+");
     }
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,13 +6,13 @@ import racingcar.util.CarNameValidator;
 
 public class InputView {
 
-    public static String readAttemptCount(){
+    public static int readAttemptCount() {
         OutputView.printAttemptCountPrompt();
         String input = Console.readLine();
-        return input;
+        return AttemptCountValidator.validateAttemptCount(input);
     }
 
-    public static String[] readCarNames(){
+    public static String[] readCarNames() {
         OutputView.printCarNamePrompt();
         String input = Console.readLine();
         return CarNameValidator.validateCarNames(input);
