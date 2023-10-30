7b316e522311eb39a75b77477dd042b76e2d5527
wonindev <sangwonparkdev@gmail.com>
2023-10-30 15:30:25 +0900

    fix: 메소드에서 바로 예외를 터트려서 생기는 버그 때문에 return을 boolean으로 설정

--- src/main/java/racingcar/ExceptionManager.java
+++ src/main/java/racingcar/ExceptionManager.java
@@ -1,24 +1,27 @@
 package racingcar;
 
+import java.util.List;
+
 public class ExceptionManager {
-    public static void isNegative(int attemptNum) {
+    public static boolean isNegative(int attemptNum) {
         if (attemptNum < 1) {
-            throw new IllegalArgumentException("시도 회수는 1 이상만 가능합니다.");
+            return true;
         }
+        return false;
     }
 
     public static boolean isNum(String input) {
         if (input.matches("\\d+")) {
             return true;
         } else {
-            throw new IllegalArgumentException("시도 회수는 숫자만 가능합니다.");
+            return false;
         }
     }
 
     public static boolean isBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("시도 회수는 공백이 될 수 없습니다.");
+            return true;
         }
-        return true;
+        return false;
     }
 }
