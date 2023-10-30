44300acc0e0e1bc980b780fabf97e12a99fce434
shkisme <shkisme0130@gmail.com>
2023-10-30 20:47:26 +0900

    test: NameTest 작성
    이름 길이에 대한 유효성 검사 진행하는지 테스트

---
+++ src/test/java/racingcar/model/NameTest.java
@@ -0,0 +1,49 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static racingcar.model.TestHelper.generateRandomString;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.car.Name;
+
+public class NameTest {
+    static final int MIN_NAME_LENGTH = 1;
+    static final int MAX_NAME_LENGTH = 5;
+
+    @Test
+    @DisplayName("유효한 이름 길이일 경우 Name은 생성된다.")
+    public void 유효한_이름_길이일_경우_Name은_생성된다() {
+        //given
+        String randomName = generateRandomString(MIN_NAME_LENGTH);
+
+        //when
+        Name name = Name.from(randomName);
+
+        //then
+        assertThat(name.name()).isEqualTo(randomName);
+    }
+
+    @Test
+    @DisplayName("이름 길이가 최소 길이 미만일 경우 IllegalArgumentException 발생한다.")
+    public void 이름_길이가_최소_길이_미만일_경우_IllegalArgumentException_발생한다() {
+        //given
+        String randomName = generateRandomString(MIN_NAME_LENGTH - 1);
+
+        //when & then
+        assertThatThrownBy(() -> Name.from(randomName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름 길이가 최대 길이 초과일 경우 IllegalArgumentException 발생한다.")
+    public void 이름_길이가_최대_길이_초과일_경우_IllegalArgumentException_발생한다() {
+        //given
+        String name = generateRandomString(MAX_NAME_LENGTH + 1);
+
+        //when & then
+        assertThatThrownBy(() -> Name.from(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
---
+++ src/test/java/racingcar/model/TestHelper.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import static racingcar.model.NameTest.MIN_NAME_LENGTH;
+
+import java.util.UUID;
+import racingcar.model.car.Car;
+import racingcar.model.car.Name;
+import racingcar.model.car.Position;
+
+public class TestHelper {
+
+    static String generateRandomString(int length) {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().substring(0, length);
+    }
+
+
+    /**
+     * @return 랜덤 Name을 가진 Car 반환합니다.
+     */
+    static Car genearteCar() {
+        return new Car(Name.from(generateRandomCarName()), new Position(0));
+    }
+
+    private static String generateRandomCarName() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().substring(0, MIN_NAME_LENGTH);
+    }
+}
