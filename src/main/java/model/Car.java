2d5a6e29f10dc01b1827413e9401cca2f4152da2
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-30 15:24:17 +0900

    feat : 자동차는 조건에 따라 움직이기

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -2,9 +2,26 @@ package model;
 
 public class Car {
     private final CarName carName;
+    private final CarPosition carPosition;
+    private final MoveRule moveRule = new MoveRule();
 
-    public Car(String name){
+    public Car(String name) {
         this.carName = new CarName(name);
+        this.carPosition = new CarPosition();
+    }
+
+    public void moveByCondition() {
+        if (moveRule.shouldMove()) {
+            carPosition.advancePosition();
+        }
+    }
+
+    public String getName() {
+        return this.carName.toString();
+    }
+
+    public int getPosition() {
+        return this.carPosition.toInt();
     }
 
 }
