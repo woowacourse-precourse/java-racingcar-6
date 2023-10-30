3eaab9da4ab09ecb9af8667ce5bc2062fc549f57
Han <yulmam1@naver.com>
2023-10-30 10:18:20 +0900

    feat(InputView) : 사용자에게 경주할 자동차의 이름을 물어보는 문자열 출력 기능 추가

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,20 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class RacingController {
+    private final InputView inputView;
+
+    public RacingController() {
+        inputView = new InputView();
+    }
+
+    public void start() {
+        inputView.printRequestCarName();
+    }
+}
