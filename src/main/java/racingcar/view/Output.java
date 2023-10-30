6fda41a789953dfd61c8c2dabbc7469428816ea7
bin <gimgyubin1225@gmail.com>
2023-10-30 17:19:21 +0900

    Feat: 실행 결과 출력 기능 추가

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class Output {
 
     public static void printInputCarNameInstruction(){
@@ -7,6 +10,18 @@ public class Output {
     }
 
     public static void printExecutionCountQuestion(){
-        System.out.println("시도할 회수는 몇회인가요?\n");
+        System.out.println("시도할 회수는 몇회인가요?");
     }
+
+    public static void printResultOfExecution(List<Car> carList){
+        System.out.println("실행 결과");
+        for(Car car: carList){
+            String driveTrace = "";
+            for(int i=0; i<car.getLocation(); i++){
+                driveTrace+="-";
+            }
+            System.out.printf("%s : %s\n",car.getName(), driveTrace);
+        }
+    }
+
 }
