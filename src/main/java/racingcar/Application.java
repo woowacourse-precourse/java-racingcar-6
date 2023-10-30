9aefb4d19bd8a37a64d5a817f04a1a416aef93a3
inpink <inpink@kakao.com>
2023-10-30 19:12:58 +0900

    feat: main에서 racingController.playGame()를 통해 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.configuration.AppConfig;
+import racingcar.configuration.Config;
+import racingcar.controller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Config config = generateConfig();
+        RacingController racingController = generateRacingController(config);
+        racingController.playGame();
+    }
+
+    private static Config generateConfig() {
+        return AppConfig.getInstance();
+    }
+
+    private static RacingController generateRacingController(Config config) {
+        return config.racingController();
     }
 }
