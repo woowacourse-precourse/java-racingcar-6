17fe26f1d0df3fa6e76c4966abbf35350185b5e2
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-30 12:14:33 +0900

    feat(generator): add NumberGenerator
    0~9 사이에서 무작위 값 생성하는 createBetween0and9 작성

---
+++ src/main/java/racingcar/domain/generator/NumberGenerator.java
@@ -0,0 +1,9 @@
+package racingcar.domain.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberGenerator {
+    public int createBetween0and9(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
