6311b495a1b0f888737e4d0a23d5936f4eb5c2b7
juns1s <juns1s@naver.com>
2023-10-31 00:54:44 +0900

    chore: 메시지 불변적용

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
     INVALID_CAR_NAME("차의 이름은 5자 이하만 가능"),
     INVALID_TIMES("레이스 진행 횟수는 자연수만 가능");
 
-    private String message;
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
