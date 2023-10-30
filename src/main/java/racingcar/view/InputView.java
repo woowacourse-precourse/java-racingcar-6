1fc58a86c2574ecf92068b6815c691dc9d930c1a
박준영 <wnsdud6969@naver.com>
2023-10-30 18:44:49 +0900

    feat(InputView): add new feature
    Add new feature to InputView
    - validate empty user input
    - add error messages

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,10 @@ import java.util.List;
 public class InputView {
 
     private static final String SEPARATOR = ",";
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+    private static final String ERROR_EMPTY_NAME = "올바르지 않은 입력입니다.";
+    private static final String ERROR_NON_DIGIT_NUMBER = "숫자만 입력 가능합니다.";
+
 
     private static InputView inputView;
 
@@ -22,7 +26,9 @@ public class InputView {
 
     public List<String> inputCarNames() {
         String userInput = Console.readLine();
-        return Arrays.asList(userInput.split(SEPARATOR));
+        List<String> carNames = Arrays.asList(userInput.split(SEPARATOR));
+        validateEmptyCarNames(carNames);
+        return carNames;
     }
 
     public int inputNumberOfGameAttempts() {
@@ -31,11 +37,17 @@ public class InputView {
         return Integer.parseInt(userInput);
     }
 
+    private void validateEmptyCarNames(List<String> carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_EMPTY_NAME);
+        }
+    }
+
     private void validateUserInputIsNumber(String userInput) {
         try {
             Integer.parseInt(userInput);
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_NON_DIGIT_NUMBER);
         }
     }
 }
