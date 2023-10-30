e1feeb31f337ea95cc826b44aacefa4ff4599ea3
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 07:39:26 +0900

    feat : 랜덤 수가 4 이상일 때 직진하는 기능 완성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int CAN_GO_FORWARD_START = 4;
+    private int forward;
+    public int goForward(int random){
+        if(random >= 4){
+            forward++;
+        }
+        return this.forward;
+    }
 }
