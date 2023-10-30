b06f9fa33d28fdc32dcd10bcd4a0d38ef12a9101
linirini <2001yerin@naver.com>
2023-10-30 14:19:01 +0900

    feat : implement creating random number

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,9 +1,11 @@
 package racingcar.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class NumberGenerator{
 
     public int createRandomNumber(){
-        return -1;
+        return pickNumberInRange(0,9);
     }
 
 }
--- src/test/java/racingcar/domain/NumberGeneratorTest.java
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ public class NumberGeneratorTest{
 
     private NumberGenerator generator;
 
-    @BeforeAll
+    @BeforeEach
     void init(){
         generator = new NumberGenerator();
     }
