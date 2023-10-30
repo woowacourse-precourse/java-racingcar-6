7f8746d767a21e1c560c024a586442acb189c533
hongseokhodev <hongseokhodev@gmail.com>
2023-10-30 23:50:32 +0900

    test: Generator가 기준보다 큰 수를 생성하면 Decider의 isSucceed 메소드가 true를 반환한다

--- src/test/java/racingcar/domain/car/DeciderTest.java
+++ src/test/java/racingcar/domain/car/DeciderTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain.car;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ class DeciderTest {
             return 1;
         }
     }
+
     @Test
     void 기준보다_작은_수를_뽑으면_isSucceed_메소드가_false를_반환한다() {
         //given
@@ -23,5 +25,26 @@ class DeciderTest {
 
         //then
         assertFalse(result);
+    }
+
+    static class nineGenerator implements Generator {
+        @Override
+        public int generate(int startInclusive, int endInclusive) {
+            return 9;
+        }
+    }
+
+    @Test
+    void 기준보다_큰_수를_뽑으면_isSucceed_메소드가_true를_반환한다() {
+        //given
+        Rule rule = new DecideRule();
+        Generator testGenerator = new nineGenerator();
+        Decider decider = Decider.of(rule, testGenerator);
+
+        //when
+        boolean result = decider.isSucceed();
+
+        //then
+        assertTrue(result);
     }
 }
