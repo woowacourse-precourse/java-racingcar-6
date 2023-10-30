d8aa37070000f49ff3baf27ab0659882fc862ac1
JoonKyo <joonkyolee@gmail.com>
2023-10-30 21:10:25 +0900

    feat(CarNameValidator): 예외 처리 메시지 상수 추가

---
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class CarNameValidator {
+    private static final String INPUT_HAS_SPACE = "입력 값에 스페이스 값이 존재합니다.";
+    private static final String INPUT_HAS_ZERO_LENGTH = "입력 값에 길이가 0인 자동차가 존재합니다.";
+    private static final String INPUT_HAS_OVER_FIVE_LENGTH = "입력 값에 길이가 5가 넘는 자동차가 존재합니다.";
+    private static final String INPUT_HAS_DUPLICATED_NAME = "입력 값에 중복 값이 존재합니다.";
+}
