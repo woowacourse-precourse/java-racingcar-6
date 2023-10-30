bbf6b45d9828f0d8716aa89d88752807809f3b66
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-30 15:21:06 +0900

    refactor(NumberGenerator): modify NumberGenerator & NumberGeneratorTest
    createListNumber로 변경: 0~9 사이 숫자를 랜덤으로 생성하여 리스트에 담기
    NumberGeneratorTest 코드도 변경

--- src/main/java/racingcar/domain/generator/NumberGenerator.java
+++ src/main/java/racingcar/domain/generator/NumberGenerator.java
@@ -1,9 +1,16 @@
 package racingcar.domain.generator;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberGenerator {
-    public int createBetween0and9() {
-        return Randoms.pickNumberInRange(0, 9);
+    public List<Integer> createListNumber(int listNumber) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < listNumber; i++) {
+            int between0and9 = Randoms.pickNumberInRange(0, 9);
+            result.add(between0and9);
+        }
+        return result;
     }
 }
--- src/test/java/racingcar/domain/generator/NumberGeneratorTest.java
+++ src/test/java/racingcar/domain/generator/NumberGeneratorTest.java
@@ -2,14 +2,17 @@ package racingcar.domain.generator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class NumberGeneratorTest {
     @Test
-    public void createBetween0and9_0부터9사이의_숫자_생성(){
+    public void createListNumber_0부터9사이의_숫자_생성() {
         NumberGenerator numberGenerator = new NumberGenerator();
-        int result=numberGenerator.createBetween0and9();
+        List<Integer> result = numberGenerator.createListNumber(3);
 
-        assertThat(result).isBetween(0,9);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(3);
+        assertThat(result).allMatch(number -> number >= 0 && number <= 9);
     }
 }
