23ec0a78d600468be87bd8ac84fe32daece1789a
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-30 17:24:42 +0900

    feat: 입력받은 자동차 리스트를 파싱하는 기능 추가

--- src/main/java/racingcar/domain/InputManager.java
+++ src/main/java/racingcar/domain/InputManager.java
@@ -1,21 +1,41 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.car.Car;
 
 public class InputManager {
     private final int gameProcessCount;
     private final String inputCarList;
+    private final List<Car> parsedCarList;
 
     public InputManager(int gameProcessCount, String inputCarList) {
         this.gameProcessCount = gameProcessCount;
         this.inputCarList = inputCarList;
+        this.parsedCarList = parseCarList(inputCarList);
     }
 
     public int getGameProcessCount() {
         return gameProcessCount;
     }
 
-    public String getInputCarList() {
-        return inputCarList;
+    public List<Car> getParsedCarList() {
+        return parsedCarList;
+    }
+
+    private List<Car> parseCarList(String carList) {
+        List<Car> cars = new ArrayList<>();
+        String[] carNames = carList.split(",");
+        for (String carName : carNames) {
+            carNameLengthValidate(carName);
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+    private void carNameLengthValidate(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다.");
+        }
     }
 }
