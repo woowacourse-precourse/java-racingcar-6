cbf9ae79195364b97f6170cf1de3fd49a540e576
Gukin Han <gukin.dev@gmail.com>
2023-10-30 22:01:16 +0900

    refactor: 유저로 부터 값 입력, 검증, 처리에 대한 메서드 분할 및 리팩토링

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,68 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class InputHandler {
+    private static final String MESSAGE_TO_INQUIRE_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MESSAGE_TO_INQUIRE_ATTEMPTS = "시도할 회수는 몇회인가요?";
+
+    private static final String ERROR_NOT_INTEGER = "정수를 입력해주세요.";
+    private static final String ERROR_LESS_THAN_ONE = "0보다 큰 값을 입력해야합니다.";
+    private static final String ERROR_NOT_ENOUGH_NUMBER_OF_NAME = "충분한 수가 아닙니다. 최소 2개 이상 입력해주세요.";
+
+    private static final Pattern INTEGER_PATTERN = Pattern.compile("^-?\\d+$");
+    private static final int MINIMUM_NUMBER_OF_NAME = 2;
+
+    public List<String> getValidCarNamesFromUser() throws IllegalArgumentException{
+        String userInput = getUserInputForCarNames();
+        List<String> carNames = splitByCommaAndTrim(userInput);
+        validateCarNames(carNames);
+        return carNames;
+    }
+
+    private String getUserInputForCarNames() {
+        System.out.println(MESSAGE_TO_INQUIRE_CARS_NAME);
+        return Console.readLine();
+    }
+
+    private List<String> splitByCommaAndTrim(String str) {
+        return Arrays
+                .stream(str.split(","))
+                .map(String::strip)
+                .filter(name -> !name.isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        checkIfEnoughNumberOfName(carNames);
+    }
+
+    private void checkIfEnoughNumberOfName(List<String> carNames) {
+        if (carNames.size() < MINIMUM_NUMBER_OF_NAME) {
+            throw new IllegalArgumentException(ERROR_NOT_ENOUGH_NUMBER_OF_NAME);
+        }
+    }
+
+    public int getValidAttemptsFromUser() throws IllegalArgumentException{
+        System.out.println(MESSAGE_TO_INQUIRE_ATTEMPTS);
+
+        String userInput = Console.readLine();
+        if (!isInteger(userInput)) throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+
+        int attempts = Integer.parseInt(userInput);
+        if (attempts < 0) throw new IllegalArgumentException(ERROR_LESS_THAN_ONE);
+
+        return attempts;
+    }
+
+    public boolean isInteger(String str) {
+        Matcher matcher = INTEGER_PATTERN.matcher(str);
+        return matcher.matches();
+    }
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static racingcar.Utils.*;
-
 public class RacingGame {
-    private final int MINIMUM_NUMBER_OF_NAME = 2;
-    private final String ERROR_NOT_ENOUGH_NUMBER_OF_NAME = "충분한 수가 아닙니다. 최소 2개 이상 입력해주세요.";
 
-    private int howManyTries;
     private final Referee referee = new Referee();
+    private final InputHandler inputHandler = new InputHandler();
 
     public void run() {
-        String carNames = getCarNamesWithCommaSeparated();
-        List<String> carNameList = mapToListAndValidateName(carNames);
-        List<Car> cars = createCars(carNameList);
-        howManyTries = getHowManyTries();
-        startRacing(cars);
+        List<String> carNames = inputHandler.getValidCarNamesFromUser();
+        List<Car> cars = createCars(carNames);
+        int attempts = inputHandler.getValidAttemptsFromUser();
+        startRacing(cars, attempts);
         List<Car> winners = referee.getWinners(cars);
         printWinners(winners);
     }
@@ -29,9 +24,9 @@ public class RacingGame {
     }
 
 
-    private void startRacing(List<Car> cars) {
+    private void startRacing(List<Car> cars, int attempts) {
         StringBuilder racingResult = new StringBuilder("\n실행 결과\n");
-        while (howManyTries > 0) {
+        while (attempts > 0) {
             for (Car car : cars) {
                 car.moveForwardOrStop();
                 racingResult
@@ -39,30 +34,19 @@ public class RacingGame {
                         .append("\n");
             }
             racingResult.append("\n");
-            howManyTries--;
+            attempts--;
         }
         System.out.print(racingResult.toString());
     }
 
-    private List<Car> createCars(List<String> carNameList) {
+    private List<Car> createCars(List<String> carNames) {
         List<Car> cars = new ArrayList<>();
-        for (String name : carNameList) {
+        for (String name : carNames) {
             cars.add(Car.createCarByName(name));
         }
         return cars;
     }
 
-    private List<String> mapToListAndValidateName(String carNames) {
-        List<String> carNameList = splitToListBySeparator(carNames);
-        checkIfEnoughNumberOfName(carNameList);
-        return carNameList;
-    }
-
-    private void checkIfEnoughNumberOfName(List<String> names) {
-        if (names.size() < MINIMUM_NUMBER_OF_NAME) {
-            throw new IllegalArgumentException(ERROR_NOT_ENOUGH_NUMBER_OF_NAME);
-        }
-    }
 
 
 }
