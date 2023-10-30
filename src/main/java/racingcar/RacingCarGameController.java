519578f05d0bf1ec6569fc614961804c01559a4b
seminchoi <choicco89@gmail.com>
2023-10-30 20:06:14 +0900

    refactor(racingcar.RacingCarGameController) : 멤버변수 추가 정의, 생성자 수정

--- src/main/java/racingcar/RacingCarGameController.java
+++ src/main/java/racingcar/RacingCarGameController.java
@@ -2,8 +2,12 @@ package racingcar;
 
 public class RacingCarGameController {
     private final RacingCarGameMachine racingCarGameMachine;
+    private final InputView inputView;
+    private final OutputView outputView;
 
-    public RacingCarGameController(RacingCarGameMachine racingCarGameMachine) {
+    public RacingCarGameController(RacingCarGameMachine racingCarGameMachine, InputView inputView, OutputView outputView) {
         this.racingCarGameMachine = racingCarGameMachine;
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 }
