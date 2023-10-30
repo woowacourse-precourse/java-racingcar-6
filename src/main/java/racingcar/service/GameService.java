fd615d7e0ed134120463b09e79e15ee8879c7284
haruday97 <starharu97@naver.com>
2023-10-30 20:27:21 +0900

    feat : 숫자 4이상 확인 기능 추가
    그 외 상수명 네이밍컨벤션 맞게 수정

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameService {
 
-    private final int startInclusive = 1;
-    private final int endInclusive = 9;
+    private final int START_INCLUSIVE = 1;
+    private final int END_INCLUSIVE = 9;
+    private final int STANDARD_NUMBER = 4;
 
     public int getRandomNumber() {
-        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    }
+
+    public boolean isBiggerThanStandard(int number) {
+        return number <= STANDARD_NUMBER;
     }
 }
