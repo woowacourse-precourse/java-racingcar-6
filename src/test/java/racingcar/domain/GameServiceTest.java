7825a3ca74a4e29b80d76683ee506089aaa3aa3a
hunko <rhwlgns4386@naver.com>
2023-10-30 14:05:41 +0900

    test(GameServiceTest) : 이동관련 테스트 추가

---
+++ src/test/java/racingcar/domain/GameServiceTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class GameServiceTest {
+
+    @Test
+    public void 레이싱을_한다() {
+        GameService gameService = new GameService();
+        ArrayList<Car> carList = new ArrayList<>();
+        carList.add(new Car(new TestNumberGenerator(5)));
+        carList.add(new Car(new TestNumberGenerator(3)));
+        carList.add(new Car(new TestNumberGenerator(6)));
+        gameService.init(carList);
+
+        gameService.doRacing();
+
+        List<Car> status = gameService.getStatus();
+        assertThat(status).filteredOn(car->car.getLocation()!=0).hasSize(2);
+    }
+
+}
