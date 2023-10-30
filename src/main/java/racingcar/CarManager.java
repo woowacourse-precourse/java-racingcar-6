1ce3d161a109611683e35f7c3f2c7448e37e254e
SungKyum Kim <sungkyum1@naver.com>
2023-10-30 16:36:59 +0900

    feat(CarManager): 입력받은 이름 목록의 Car를 생성하고 Cars에 저장한다.

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -1,2 +1,22 @@
-package racingcar;public class CarManager {
+package racingcar;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CarManager {
+    private Cars cars;
+
+    public void createCars(String[] names) {
+        List<Car> carList = Arrays.stream(names).map(Car::new).toList();
+
+        cars = new Cars(carList);
+    }
+
+    public Cars getCars() {
+        return cars;
+    }
 }
