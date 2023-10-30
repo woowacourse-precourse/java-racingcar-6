f93069b93ae664f77b43d09c7329febe9cc36b85
강승민 <rkdtmdals999@naver.com>
2023-10-30 16:25:09 +0900

    [feat] Add InputException class checkNameLength method

--- src/main/java/racingcar/controller/InputException.java
+++ src/main/java/racingcar/controller/InputException.java
@@ -2,5 +2,9 @@ package racingcar.controller;
 
 public class InputException {
     private InputException() {}
-
+    public static void checkNameLength(String newCarName) { // 이름 길이 5자 초과 예외처리
+        if (newCarName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
