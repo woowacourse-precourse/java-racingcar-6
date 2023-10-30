4620263911da23ab07eee077e81ff852f3064384
kssumin <201566@jnu.ac.kr>
2023-10-31 03:54:10 +0900

    feat : 자동차 이름이 중복되지 않은지 검증

--- src/main/java/racingcar/support/DividedCarNamesValidator.java
+++ src/main/java/racingcar/support/DividedCarNamesValidator.java
@@ -1,5 +1,8 @@
 package racingcar.support;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class DividedCarNamesValidator {
 	private static final int MIN_NAME_LENGTH = 1;
 	private static final int MAX_NAME_LENGTH = 5;
@@ -12,6 +15,7 @@ public class DividedCarNamesValidator {
 	public static void validate(String[] carsName) {
 		validateCount(carsName);
 		validateLength(carsName);
+		validateDuplicated(carsName);
 	}
 
 	private static void validateCount(String[] carsName) {
@@ -24,6 +28,13 @@ public class DividedCarNamesValidator {
 			if (carName.length() > MAX_NAME_LENGTH || carName.trim().length() < MIN_NAME_LENGTH) {
 				throw new IllegalArgumentException("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
 			}
+		}
+	}
+	private static void validateDuplicated(String[] carsName) {
+		HashSet<String> cars = new HashSet<>(List.of(carsName));
+
+		if (cars.size() != carsName.length) {
+			throw new IllegalArgumentException("자동차 이름이 중복되었습니다.");
 		}
 	}
 }
--- src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
+++ src/test/java/racingcar/support/DividedCarNamesValidatorTest.java
@@ -1,9 +1,7 @@
 package racingcar.support;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -34,6 +32,16 @@ class DividedCarNamesValidatorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"12345, 1234567"})
 	void validate_car_length(String input) {
+		// when & then
+		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("자동차 이름은 1 ~ 5 글자로 입력해야합니다");
+	}
+
+	@DisplayName("자동차 이름은 중복될 수 없다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"12345, 12345"})
+	void validate_duplicated_car_name(String input) {
 		// when & then
 		assertThatThrownBy(() -> DividedCarNamesValidator.validate(input.split(",")))
 				.isInstanceOf(IllegalArgumentException.class)
