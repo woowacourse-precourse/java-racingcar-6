cab92b1b612e7e23c33f66554730e8d6aac4794f
Isaac Han <isaachan233@gmail.com>
2023-10-30 10:56:28 +0900

    refactor: 시도할 회수를 생성자에서 받아서 설정하도록 수정

--- src/main/java/racingcar/model/Attempts.java
+++ src/main/java/racingcar/model/Attempts.java
@@ -3,8 +3,12 @@ package racingcar.model;
 public class Attempts {
     private int number;
 
-    public void setNumber(int number) {
+    public Attempts(int number) {
         this.number = number;
+    }
+
+    public Attempts(String value) {
+        this.number = Integer.valueOf(value);
     }
 
     public int getNumber() {
