6de1868962036f260cc9b0ddeb9b604009f7e1c7
Hwang In Tae <sjobs66@gmail.com>
2023-10-31 01:06:31 +0900

    feat: RacingCarGame validateAttempts 구현
    잘못된 값을 입력했을 때 IllegalArgumentException을 발생시킨다.

--- src/main/java/racingcar/model/RacingCarGame.java
+++ src/main/java/racingcar/model/RacingCarGame.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 public class RacingCarGame implements Game {
     private static final int criterion = 4;
-    private final int attempts;
+    private int attempts;
     private final List<Car> carList;
 
     public void goAhead(Car car, int randomNumber) {
         if (randomNumber >= criterion) {
             car.move();
+        }
+    }
+
+    public void validateAttempts(String attempts) {
+        try {
+            this.attempts = Integer.parseInt(attempts);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("횟수를 입력해주세요.", e);
         }
     }
 }
