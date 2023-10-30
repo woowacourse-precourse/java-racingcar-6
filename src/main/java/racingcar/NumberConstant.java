79ae7d9192aa65a0bebd5614cefcaa1906c2d725
kkw2758 <dtc3229@gmail.com>
2023-10-30 13:07:51 +0900

    refactor : 상수 값 관리 클래스 생성
    - NumberConstant 클래스 생성
      - 숫자 상수 관리를 위한 클래스
    
    - SystemMessage 클래스 생성
      - 프로그램 내에서 출력하는 문자열을 관리하는 클래스
    
    - Error 클래스 변수 값 수정
      - CAR_NAME_LENGTH 값에 NumberConstant에서 선언한 내용 반영

---
+++ src/main/java/racingcar/NumberConstant.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class NumberConstant {
+    public static int MIN_CAR_NAME_LENGTH = 0;
+    public static int MAX_CAR_NAME_LENGTH = 5;
+    public static int TARGET_NUMBER = 4;
+}
