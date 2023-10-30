24cd109c8b699b6fbbaac7f83ab24114235bec1b
donglee99 <jhnj741@naver.com>
2023-10-30 11:45:09 +0900

    feat : 자동차 전진 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,9 +2,18 @@ package racingcar.domain;
 
 public class Car {
     private final Name name;
+    private int position = 0;
 
     private Car(Name name) {
         this.name = name;
+    }
+
+    public void changePosition() {
+        this.position++;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 
     public static Car of(Object name) {
