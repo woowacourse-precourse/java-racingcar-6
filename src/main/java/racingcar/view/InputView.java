f2c01243bc8fff242488b47ec551b5115a2c80f4
sgo722 <sgo722@naver.com>
2023-10-31 16:11:37 +0900

    feat : 자동차 이름 공백 제거 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.Validator;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -13,7 +15,9 @@ public class InputView {
     }
 
     public static List<String> parseCarNames(String names){
-        List<String> parseCarNames = List.of(names.split(","));
+        List<String> parseCarNames = Arrays.stream(names.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
         Validator.checkCarNameSize(parseCarNames);
         Validator.checkCarNameDuplication(parseCarNames);
         Validator.checkCarNameIsEnglish(parseCarNames);
