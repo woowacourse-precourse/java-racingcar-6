02ba17c3fe726e92f82e848251faa4e4faa46e7b
qocksdlf123 <qocksdlf123@naver.com>
2023-11-01 00:38:36 +0900

    feat(GameData): car들의 이름들, 최종 라운드를 관리하는 GameData Class 구현

--- src/main/java/racingcar/GameData.java
+++ src/main/java/racingcar/GameData.java
@@ -1,4 +1,21 @@
 package racingcar;
 
 public class GameData {
+    private final String[] carNames;
+    private final int tryRoundCount;
+
+    public GameData(String[] carNames, int tryCount) {
+        this.carNames = carNames;
+        this.tryRoundCount = tryCount;
+    }
+    //입력 받은 차들의 이름들 반환
+    public String[] getCarNames() {
+        return carNames;
+    }
+    // 최종 라운드 수 반환
+    public int getTryCount() {
+        return tryRoundCount;
+    }
+
+
 }
