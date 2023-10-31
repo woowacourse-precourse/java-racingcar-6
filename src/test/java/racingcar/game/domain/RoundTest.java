69a1d00d674d76cd1eb2648946c7aa918da2da6f
황중섭 <jshwang9709@gmail.com>
2023-10-31 14:49:35 +0900

    test(round): 이동횟수가 정수인지 여부 테스트

--- src/test/java/racingcar/game/domain/RoundTest.java
+++ src/test/java/racingcar/game/domain/RoundTest.java
@@ -2,6 +2,7 @@ package racingcar.game.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.game.constant.RoundExceptionMessage;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,6 +13,14 @@ class RoundTest {
     void 라운드_객체_생성() {
         Round round = Round.create("5");
         assertThat(round).isNotNull();
+    }
+
+    @Test
+    void 입력값이_정수가_아니면_예외_발생() {
+        assertThatThrownBy(() -> Round.create("asdf"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasCauseInstanceOf(NumberFormatException.class)
+                .hasMessageContaining(RoundExceptionMessage.NOT_INTEGER);
     }
 
 }
