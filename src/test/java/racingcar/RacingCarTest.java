378a9921f84972958aaa7a13b7b2df327d859846
hjhyun98 <hyunjo722@naver.com>
2023-11-01 06:01:44 +0900

    TEST: 최종 우승자가 여러명일 경우 테스트 코드 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,13 +3,17 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarTest extends NsTest {
-
+    private static final int MOVING_FORWARD = 8;
     Computer computer = new Computer();
+    Output output = new Output();
     @Test
     void 이름_공백_무효_처리() {
         String testName = "   pobi";
@@ -37,16 +41,18 @@ class RacingCarTest extends NsTest {
     @Test
     void 시도_횟수_입력값이_0일_경우_예외_처리(){
         assertThatThrownBy(() -> runException("pobi,woni","0")).isInstanceOf(IllegalArgumentException.class);
-
     }
 
-//    @Test
-//    void 최종_우승자가_여러_명일_경우(){
-//        assertSimpleTest(() ->
-//                run("pobi,woni,jini", "3")
-//
-//                assertThat());
-// }
+    @Test
+    void 최종_우승자가_여러_명일_경우(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi,woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
 
     @Override
     public void runMain() {
