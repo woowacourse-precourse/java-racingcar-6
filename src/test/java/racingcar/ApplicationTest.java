373ebdce1be21d0e5d9b707edcb65dd18fb0cf98
Sunhee Kim <sunni372@icloud.com>
2023-11-01 02:00:55 +0900

    test: 자동차 전진 및 우승자 선정 후 출력 기능 테스트
    - 조건에 따른 자동차 전진 기능을 테스트했습니다.
    - 각 차수별 실행 결과 출력 기능을 테스트했습니다.
    - 우승자 선정 및 출력 기능을 테스트했습니다. (공동 우승자 가능)

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,6 +24,32 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void N번_전진_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "3");
+                    assertThat(output()).contains(
+                            "pobi : -", "woni : ",
+                            "pobi : -", "woni : -",
+                            "pobi : -", "woni : --",
+                            "최종 우승자 : woni");
+                },
+                MOVING_FORWARD, STOP, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 공동_우승자() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,java", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "java : -", "최종 우승자 : pobi, java");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
