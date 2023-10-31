573a0cfdbc3094fb418a852c3de25537bfe7ea04
edgar6bf <edgar6bf@gmail.com>
2023-10-31 14:59:28 +0900

    refactor: InputView 콘솔 입력 객체를 외부로부터 주입받도록 로직 수정
    Test 편의를 위해 InputView 내부에 들어있던 Console 객체를 외부로 분리하여 생성자를 통해 의존성 주입을 받도록 로직을 수정함.
    외부로부터 주입받은 객체를 사용하는 static 메서드를 모두 인스턴스 메서드로 변경함.

---
+++ src/main/java/racingcar/util/ConsoleInputUtil.java
@@ -0,0 +1,6 @@
+package racingcar.util;
+
+public interface ConsoleInputUtil {
+
+    String input();
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,6 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.ConsoleInputUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,25 +8,35 @@ import java.util.List;
 public class InputView {
 
     private static final String INPUT_DELIMITER = ",";
-    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표("+ INPUT_DELIMITER + ") 기준으로 구분)";
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(" + INPUT_DELIMITER + ") 기준으로 구분)";
     private static final String INPUT_RACE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public static List<String> inputStringsByDelimiter() {
+    private final ConsoleInputUtil consoleInputUtil;
+
+    public InputView(ConsoleInputUtil consoleInputUtil) {
+        this.consoleInputUtil = consoleInputUtil;
+    }
+
+    public List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
-        return Arrays.stream(Console.readLine().split(INPUT_DELIMITER))
+        return splitString(consoleInputUtil.input());
+    }
+
+    private static List<String> splitString(String input) {
+        return Arrays.stream(input.split(INPUT_DELIMITER))
                 .toList();
     }
 
-    public static int inputNumber() {
+    public int inputRaceCount() {
         System.out.println(INPUT_RACE_COUNT_MESSAGE);
-        return convertStringToInt(Console.readLine());
+        return convertStringToInt(consoleInputUtil.input());
     }
 
     private static int convertStringToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("정수만 입력할 수 있습니다.");
+            throw new IllegalArgumentException("경주 진행 횟수는 정수만 입력할 수 있습니다.");
         }
     }
 }
