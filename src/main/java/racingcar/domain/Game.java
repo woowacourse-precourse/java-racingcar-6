8f3066b691749dab842f64defc9c6704f69d1dde
leeminjun <sohwi0305@gmail.com>
2023-10-31 23:44:34 +0900

    feat: 횟수 변수 작성

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -3,12 +3,12 @@ package racingcar.domain;
 import java.util.List;
 
 public class Game {
-    private final int playCount;
+    private final int playCountEnd;
     private final List<Car> cars;
-
+    private int playCount = 0;
     public Game(List<Car> cars, int playCount) {
         this.cars = cars;
-        this.playCount = playCount;
+        this.playCountEnd = playCount;
     }
 
     public List<Car> getCars() {
