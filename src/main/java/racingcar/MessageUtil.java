858973b9d1d655c28df54ec2e34ccd7a54bd3c45
minw0095 <kmu6898@naver.com>
2023-11-01 03:13:02 +0900

    feat: 입력시 에러 메세지, 우승자 출력 유틸

--- src/main/java/racingcar/MessageUtil.java
+++ src/main/java/racingcar/MessageUtil.java
@@ -3,6 +3,12 @@ package racingcar;
 public class MessageUtil {
     final static String Carname_Message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    final static String Try_Message = "시도할 회수는 몇회인가요?";
+    final static String Try_Message = "시도할 회수는 몇 회인가요?";
+
+    final static String Empty_Error_Message = "빈 공간이 없어야 합니다";
+
+    final static String Same_Name_Message = "중복되는 이름은 안됩니다";
+
+    final static String Winners_Message = "최종 우승자";
 
 }
