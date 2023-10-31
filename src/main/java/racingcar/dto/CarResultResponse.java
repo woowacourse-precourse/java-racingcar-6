cdcf905d3bec838183ea1f95bb4323bf89bc06b5
박승민 <tmddudals369@naver.com>
2023-11-01 01:49:39 +0900

    feat(carResultResponse): 게임 진행 상황을 출력하기 위해 toString 재정의

--- src/main/java/racingcar/dto/CarResultResponse.java
+++ src/main/java/racingcar/dto/CarResultResponse.java
@@ -1,4 +1,9 @@
 package racingcar.dto;
 
 public record CarResultResponse(String carName, int movingCount) {
+
+    @Override
+    public String toString() {
+        return String.format("%s : %s", carName, "-".repeat(movingCount));
+    }
 }
