68246206c2d7c10888d373660fc39132f59485fa
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 10:46:13 +0900

    [Fix](Exception) - 매직 넘버 상수 변경

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -2,10 +2,11 @@ package racingcar;
 
 public class Exception {
 
+    private static final int CAR_MAX_LENGTH = 5;
     public void ErrorCheck(String carName) {
         String[] carNameArray = carName.split(",");
         for (String name: carNameArray) {
-            if (name.length() > 5) {
+            if (name.length() > CAR_MAX_LENGTH) {
                 throw new IllegalArgumentException("5글자 넘어감");
             }
         }
