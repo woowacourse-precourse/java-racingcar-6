9a749f82539b8833210342890c2bf42900d8798f
jsj <tkdlek4186@naver.com>
2023-10-31 14:29:29 +0900

    feat : 중복입력 판별함수 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,13 +1,25 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Race {
     private final List<Car> cars;
 
     public Race(List<String> carNames) {
+        checkDuplicateNames(carNames);
         this.cars = carNames.stream().map(Car::new).collect(Collectors.toList());
+    }
+
+    public static void checkDuplicateNames(List<String> names) {
+        Set<String> nameSet = new HashSet<>();
+        for (String name : names) {
+            if (!nameSet.add(name)) {
+                throw new IllegalArgumentException("Duplicate name");
+            }
+        }
     }
 
     public void playRound() {
