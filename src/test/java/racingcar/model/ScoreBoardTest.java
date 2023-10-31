327f934962b4ac577d3437fea63033243ca27cbf
insu3160 <is23193160@gmail.com>
2023-10-31 15:51:39 +0900

    test: 스코어보드의 라운드에 따른 스코어 리스트를 잘 반환하는지 확인

--- src/test/java/racingcar/model/ScoreBoardTest.java
+++ src/test/java/racingcar/model/ScoreBoardTest.java
@@ -16,7 +16,22 @@ class ScoreBoardTest {
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.recordScore(1, Arrays.asList(pobiScore, dobiScore, robiScore));
         scoreBoard.recordScore(2, Arrays.asList(pobiScore, dobiScore, robiScore));
+        
         assertThat(scoreBoard.getSize()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("스코어보드의 라운드에 따른 스코어 리스트를 잘 반환하는지 확인")
+    public void getScoreForRound() {
+        ScoreDto pobiScore = new ScoreDto("pobi", 3);
+        ScoreDto dobiScore = new ScoreDto("dobi", 4);
+        ScoreDto robiScore = new ScoreDto("dobi", 4);
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.recordScore(1, Arrays.asList(pobiScore, dobiScore));
+        scoreBoard.recordScore(2, Arrays.asList(pobiScore, dobiScore, robiScore));
+
+        assertThat(scoreBoard.getScoreForRound(1)).containsExactly(pobiScore, dobiScore);
+        assertThat(scoreBoard.getScoreForRound(2)).containsExactly(pobiScore, dobiScore, robiScore);
     }
 
 }
