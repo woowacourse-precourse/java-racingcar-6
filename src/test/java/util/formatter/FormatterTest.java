e6eb0221dec1c408d93e3ca7761c0f0ce129911f
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-01 00:09:24 +0900

    feat: Formatter 테스트 코드 작성 - List<String>을 String으로 정상적으로 변환하는지 테스트 - String을 List<String>으로 정상적으로 변환하는지 테스트

---
+++ src/test/java/util/formatter/FormatterTest.java
@@ -0,0 +1,51 @@
+package util.formatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static resource.TestData.CAR_NAME_1;
+import static resource.TestData.CAR_NAME_2;
+import static resource.TestData.CAR_NAME_3;
+import static resource.TestData.COMMA;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class FormatterTest {
+    @Test
+    void formatListStringToString_메서드_정상적인_입력() {
+        Formatter formatter = new Formatter();
+        List<String> inputList = Arrays.asList(CAR_NAME_1, CAR_NAME_2, CAR_NAME_3);
+        StringBuilder sb = new StringBuilder();
+        sb.append(CAR_NAME_1);
+        sb.append(",");
+        sb.append(CAR_NAME_2);
+        sb.append(",");
+        sb.append(CAR_NAME_3);
+        String result = formatter.formatListStringToString(inputList);
+        assertEquals(sb.toString(), result);
+    }
+
+    @Test
+    void formatListStringToString_메서드_null_입력() {
+        Formatter formatter = new Formatter();
+        List<String> inputList = null;
+        assertThrows(NullPointerException.class, () -> formatter.formatListStringToString(inputList));
+    }
+
+    @Test
+    void formatStringToListString_메서드_정상적인_입력() {
+        Formatter formatter = new Formatter();
+        String inputString = CAR_NAME_1 + COMMA + CAR_NAME_2 + COMMA + CAR_NAME_3;
+        List<String> expectedResult = Arrays.asList(CAR_NAME_1, CAR_NAME_2, CAR_NAME_3);
+        List<String> result = formatter.formatStringToListString(inputString);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void formatStringToListString_메서드_null_입력() {
+        Formatter formatter = new Formatter();
+        String inputString = null;
+        assertThrows(NullPointerException.class, () -> formatter.formatStringToListString(inputString));
+    }
+}
