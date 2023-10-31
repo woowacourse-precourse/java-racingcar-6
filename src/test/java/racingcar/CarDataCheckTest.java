453df2404ae9c18e8ebe0c753ec5a4530de01bc4
Jung ui heon <ky7662@naver.com>
2023-10-31 22:13:20 +0900

    test : 이동거리와 시도 회수가 동일한지 확인

--- src/test/java/racingcar/CarDataCheckTest.java
+++ src/test/java/racingcar/CarDataCheckTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarDataCheckTest {
     @Test
     void 자동차_이름이_6자_이상이면_예외_발생(){
@@ -16,5 +18,19 @@ public class CarDataCheckTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             carDataCheck.checkCarNameIsValid(CarData);
         });
+    }
+
+    @Test
+    void 이동거리와_시도한_회수가_동일한지_확인(){
+        int tryNumber = 5;
+
+        LinkedHashMap<String, Integer> carData = new LinkedHashMap<>();
+        carData.put("pobi", 4);
+        carData.put("woni", 5);
+        carData.put("jun", 2);
+
+        CarDataCheck carDataCheck = new CarDataCheck();
+
+        assertThat(carDataCheck.checkFinishedTime(carData, tryNumber)).isTrue();
     }
 }
