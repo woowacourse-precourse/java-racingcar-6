1f92370673506d1d649bdbce6e2050ef8ac1960a
Hoo-sung <gntjd135@gmail.com>
2023-10-31 10:52:14 +0900

    refactor(AttemptsNumVerifierTest): NsTest 상속, assertSimpleTest를 사용하여  Verifier 의존성 제거

--- src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
+++ src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
@@ -1,39 +1,43 @@
 package racingcar.verifier;
 
 
-import org.junit.jupiter.api.BeforeEach;
+import camp.nextstep.edu.missionutils.test.Assertions;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 import racingcar.system.ExceptionMessage;
 
 import static org.assertj.core.api.Assertions.*;
 
-class AttemptsNumVerifierTest {
-
-    private Verifier attemptsNumVerifier;
-
-
-    @BeforeEach
-    void setUp() {
-        attemptsNumVerifier = new AttemptsNumVerifier();
-    }
-
+class AttemptsNumVerifierTest extends NsTest {
     @Test
     void 정상입력이_주어진경우() {
-        assertThatCode(() -> attemptsNumVerifier.check("56"))
-                .doesNotThrowAnyException();
+        Assertions.assertSimpleTest(()->{
+            assertThatCode(() -> run("pobi,woni,jun","23"))
+                    .doesNotThrowAnyException();
+        });
     }
 
     @Test
     void 숫자가아닌입력이_주어진경우() {
-        assertThatThrownBy(() -> attemptsNumVerifier.check("35k"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ExceptionMessage.NOT_NUMERIC);
+        Assertions.assertSimpleTest(()->
+                assertThatThrownBy(() -> run("pobi,woni,jun","35k"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ExceptionMessage.NOT_NUMERIC)
+                );
     }
 
     @Test
     void 유효한범위의_숫자가_아닌_입력이_아닌경우() {
-        assertThatThrownBy(() -> attemptsNumVerifier.check("-1"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ExceptionMessage.INVALID_NUM_OF_ATTEMPTS);
+        Assertions.assertSimpleTest(() -> {
+            assertThatThrownBy(() -> run("pobi,woni,jun","-1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ExceptionMessage.INVALID_NUM_OF_ATTEMPTS);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
