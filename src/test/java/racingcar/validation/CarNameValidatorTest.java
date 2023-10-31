0945285666ac7f46e305ad6ea713a76e8c8260ae
donghyun <kdhdh001@naver.com>
2023-11-01 01:44:00 +0900

    refactor: CarNameValidatorTest public 제거

--- src/test/java/racingcar/validation/CarNameValidatorTest.java
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -17,7 +17,7 @@ public class CarNameValidatorTest {
     }
 
     @Test
-    public void 자동차_이름이_공백만_있는_경우_검증() {
+    void 자동차_이름이_공백만_있는_경우_검증() {
         String input = "    ";
         List<String> carNames = Arrays.asList(input.split(","));
         assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
@@ -26,7 +26,7 @@ public class CarNameValidatorTest {
     }
 
     @Test
-    public void 자동차_이름이_5자_이상인_경우_검증() {
+    void 자동차_이름이_5자_이상인_경우_검증() {
         String input = "car1,carrrrrr2222,car3";
         List<String> carNames = Arrays.asList(input.split(","));
         assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
@@ -35,7 +35,7 @@ public class CarNameValidatorTest {
     }
 
     @Test
-    public void 자동차_이름이_중복되는_경우_검증() {
+    void 자동차_이름이_중복되는_경우_검증() {
         String input = "car1,car1,car3";
         List<String> carNames = Arrays.asList(input.split(","));
         assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
