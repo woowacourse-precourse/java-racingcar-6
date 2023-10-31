0820a85f672caa2a4f2b024477b2b6250c0b1075
ghdeo <goldencorn7@naver.com>
2023-10-31 23:38:55 +0900

    feat: Add RacingService startRace

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.service.RacingService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingService racingService = new RacingService();
+        racingService.startRace();
     }
 }
