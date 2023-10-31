8ee4e22e12ada72a473f11889d898b8772e78938
JeongHunHui <deca3248@gmail.com>
2023-11-01 06:44:04 +0900

    feat: 우승자 이름 출력 시 사용할 DTO 클래스 생성

---
+++ src/main/java/racingcar/dto/WinnerNames.java
@@ -0,0 +1,21 @@
+package racingcar.dto;
+
+import java.util.List;
+import racingcar.model.Racing;
+
+public class WinnerNames {
+
+    private final List<String> winnerNames;
+
+    private WinnerNames(List<String> winnerNames) {
+        this.winnerNames = winnerNames;
+    }
+
+    public static WinnerNames of(Racing racing) {
+        return new WinnerNames(racing.getWinningCarNames());
+    }
+
+    public List<String> getWinnerNames() {
+        return winnerNames;
+    }
+}
