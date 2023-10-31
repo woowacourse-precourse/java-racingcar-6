ea4440197c1afa17169704d9046f43a201dab08c
Hanbee Lee <bee1162@naver.com>
2023-10-31 13:10:57 +0900

    feat : 실행결과와 우승자 출력 view작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.CarContainer;
 import racingcar.dto.CarPositionDto;
 
 import java.util.List;
@@ -8,28 +9,32 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String COLON = " : ";
     private static final String CAR_POSITION = "-";
-    private String output;
-
-    public OutputView(String output) {
-        this.output = output;
-    }
+    private static final String WINNER_MESSAGE = "최종 우승자";
 
     public void printStatusGuide() {
+        System.out.println();
         System.out.println(RESULT_MESSAGE);
     }
 
     public void printStatus(List<CarPositionDto> racingStatus) {
         racingStatus.forEach(this::printOneStatus);
+        System.out.println();
     }
 
     private void printOneStatus(CarPositionDto racingStatus) {
-        System.out.println(racingStatus.getCarName());
-        System.out.println(COLON);
+        System.out.print(racingStatus.getCarName());
+        System.out.print(COLON);
 
         for (int i = 0; i < racingStatus.getStatus(); i++) {
-            System.out.println(CAR_POSITION);
+            System.out.print(CAR_POSITION);
         }
         System.out.println();
+    }
+    public void printWinner(List<String> winners) {
+        System.out.print(WINNER_MESSAGE);
+        System.out.print(COLON);
+        System.out.print(String.join(",", winners));
+        System.out.print("\n");
     }
 
 }
