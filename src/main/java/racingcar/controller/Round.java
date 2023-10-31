d42cacf2d4b183ccfd73c703b68e18dbf628b4d3
gahyeon <1000ji1@naver.com>
2023-11-01 02:12:06 +0900

    feat : print current position
    모든 차에 대한 현재 position 출력

--- src/main/java/racingcar/controller/Round.java
+++ src/main/java/racingcar/controller/Round.java
@@ -3,20 +3,22 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.RandomNumber;
+import racingcar.view.OutputView;
 
 public class Round {
 
     public void play(List<Car> carList) {
         createRandom(carList);
+        OutputView.resultGame(carList);
     }
 
     public void createRandom(List<Car> carList) {
         for (Car car : carList) {
             RandomNumber.make();
             if (RandomNumber.isForward()) {
-                car.forward(); // 랜덤 수에 따라 차의 위치를 증가
+                car.forward();
             }
         }
     }
-
+    
 }
