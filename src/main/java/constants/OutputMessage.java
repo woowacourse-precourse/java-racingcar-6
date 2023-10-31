91a516f304a0b398e4b9830c6b0530c05e2ffc90
nayonsoso <panda0329@naver.com>
2023-11-01 03:31:04 +0900

    refactor: 안쓰는 import문 삭제

---
+++ src/main/java/constants/OutputMessage.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class OutputMessage {
+    private final static String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String ATTEMPT_TIMES_INPUT_PROMPT = "시도할 회수는 몇회인가요?";
+    private final static String RESULT_PRINT_START_STATEMENT = "실행 결과";
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,6 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import validator.AttemptTimesValidator;
-import validator.NameListValidator;
 
 public class InputView {
     public String getUserInput(){
