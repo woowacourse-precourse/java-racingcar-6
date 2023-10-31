e8f04092e3a70f21fb0c5346599ac6b5ad70ccba
minsu20 <syt07100@gmail.com>
2023-11-01 02:48:46 +0900

    feat(view): 자동차 이름을 입력받는 메서드 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static racingcar.constants.ViewMessages.REQUEST_CAR_NAMES;
+import static racingcar.constants.ViewMessages.REQUEST_RACE_ROUNDS;
+
+public class InputView{
+
+
+    public static String getUserInput() {
+        return Console.readLine();
+    }
+
+    public static String getCarNames() {
+        System.out.println((REQUEST_CAR_NAMES.getMessage()));
+        return getUserInput();
+    }
+
+}
