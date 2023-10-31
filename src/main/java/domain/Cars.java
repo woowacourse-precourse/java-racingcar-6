03844485736f8cd956376e550518d18e90d56d05
hanriverkitty <danieltls@naver.com>
2023-10-31 23:49:52 +0900

    setCars, move 미완성

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -5,7 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+    public void setCars(List<String>cars){
+        List<String> initCar = new ArrayList<String>();
+        for (int i=0;i<cars.size();i++){
+            initCar.add("");
+        }
 
+    }
+    public void move(List<String> cars){
+        for (int i=0;i<cars.size();i++){
+            if(randomNum()){
+
+            }else {
+
+            }
+        }
+
+    }
     public boolean randomNum(){
         int num = Randoms.pickNumberInRange(0,9);
         if (num>=4){
