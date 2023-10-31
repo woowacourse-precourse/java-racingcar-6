800025b6599164131726e992560df0d9f7925cb2
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 15:32:16 +0900

    refactor(domain): 해당 크래스에 getter를 추가 설정

--- src/main/java/racingcar/domain/GamePlayer.java
+++ src/main/java/racingcar/domain/GamePlayer.java
@@ -6,28 +6,27 @@ import static racingcar.constant.CarConstant.MIN_MOVE_CONDITION_NUMBER;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.dto.CarNames;
-import racingcar.view.OutputView;
+import java.util.NoSuchElementException;
 
 /*
  *   플레이어의 정보를 담당
  * */
 
 public class GamePlayer {
+
     private final List<Car> cars = new ArrayList<>();
 
     private GamePlayer(CarNames carNames) {
-        for (String carName : carNames.getCarNames()) {
-            cars.add(new Car(carName));
-        }
+        carNames.createCars(cars);
     }
 
     public static GamePlayer from(CarNames carNames) {
         return new GamePlayer(carNames);
     }
 
-    public GameRank createGameRank() {
-        return new GameRank(cars);
+    public List<Car> getCars() {
+
+        return cars;
     }
 
     public void moveCars() {
@@ -39,7 +38,12 @@ public class GamePlayer {
     private void moveCar(Car car) {
         int number = Randoms.pickNumberInRange(MIN_MOVE_CONDITION_NUMBER, MAX_MOVE_CONDITION_NUMBER);
         car.moveOrStop(number);
-        OutputView.moveResult(car.recordToMoveSign());
     }
 
+    public int getMaxMoveDistance() {
+        return cars.stream()
+                .mapToInt(Car::getMoveDistance)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+    }
 }
