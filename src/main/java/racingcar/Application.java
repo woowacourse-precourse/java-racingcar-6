c22d092be55912f199aa4267c59f842c2928d969
sung-yeop <jjabc9459@naver.com>
2023-10-31 16:12:43 +0900

    feat: start 기능 추가
    - 메인 로직들이 동작할 순서를 정의하여 최종 결과물을 출력함
    - 전진_정지() 테스트 통과 실패
    - 출력은 정상적으로 진행되나 테스트는 실패함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -14,23 +13,47 @@ public class Application {
         // TODO: 프로그램 구현
 
         Application application = new Application();
-        String input = application.input_name();
-        String[] split = application.devide_name(input);
-        application.exceptionOfNameLength(split);
+        application.Start();
 
     }
 
-    public String input_name(){
+    public void Start() {
+        Application application = new Application();
+        Print print = new Print();
+
+        print.printInitName();
+        String input = application.input_name();
+        String[] split = application.devide_name(input);
+        application.exceptionOfNameLength(split);
+        Car[] carlist = application.createCar(split);
+
+        print.printInitCount();
+        int cnt = Integer.parseInt(Console.readLine());
+        for (int i = 0; i < cnt; i++) {
+            for (Car car : carlist) {
+                application.move(car, go(Randoms.pickNumberInRange(0, 9)));
+            }
+        }
+
+        print.printResult(carlist);
+
+        List<String> victorylist = compare(carlist);
+
+        print.printVictoryCar(victorylist);
+
+    }
+
+    public String input_name() {
         String input = Console.readLine();
         return input;
     }
 
-    public String[] devide_name(String input){
+    public String[] devide_name(String input) {
         String[] split = input.split(",");
         return split;
     }
 
-    public void exceptionOfNameLength(String[] split){
+    public void exceptionOfNameLength(String[] split) {
         for (String s : split) {
             if (s.length() > 5) {
                 throw new IllegalArgumentException();
@@ -39,25 +62,25 @@ public class Application {
     }
 
     // 파라미터의 random은 외부에서 랜덤 값을 생성하여 집어넣어주는 방식으로 구현 필요
-    public boolean go(int random){
+    public boolean go(int random) {
         if (random >= 4) {
             return false;
         }
         return true;
     }
 
-    public Car[] createCar(String[] split){
+    public Car[] createCar(String[] split) {
         Car[] carList = new Car[split.length];
-        for(int i=0; i< split.length; i++){
+        for (int i = 0; i < split.length; i++) {
             carList[i] = new Car(split[i], 0);
         }
 
         return carList;
     }
 
-    public void move(Car car, Boolean go){
-        if(go == true){
-            car.setPos(car.getPos()+1);
+    public void move(Car car, Boolean go) {
+        if (go == true) {
+            car.setPos(car.getPos() + 1);
         }
     }
 
@@ -65,8 +88,6 @@ public class Application {
 
         List<Car> collect = Arrays.stream(cars).sorted(Comparator.comparing(Car::getPos).reversed())
                 .collect(Collectors.toList());
-
-        List<String> result = null;
 
         List<String> victorylist = collect.stream().filter((Car car) -> car.getPos() == collect.get(0).getPos())
                 .map(v -> v.getName()).collect(Collectors.toList());
