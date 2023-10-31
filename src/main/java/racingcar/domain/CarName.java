825365301284da206bae076197fd1c23f95ecbee
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-31 16:06:20 +0900

    [refactor] : change location validate method

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,31 +1,16 @@
 package racingcar.domain;
 
+import racingcar.validate.CarNameValidate;
+
 public class CarName {
     private final String name;
 
     public CarName(String carName) {
+        CarNameValidate.validateCarName(carName);
         this.name = carName;
-        validateCarName();
     }
 
     public String getName() {
         return name;
-    }
-
-    private void validateCarName() {
-        isCarNameLength();
-        isCommaPresent();
-    }
-
-    private void isCarNameLength() {
-        if (name.length() >= 5) {
-            throw new IllegalArgumentException("자동차 이름은 5글자 이하로만 입력해주세요.");
-        }
-    }
-
-    private void isCommaPresent() {
-        if (name.contains(" ")) {
-            throw new IllegalArgumentException("자동차 이름에 공백을 제외해주세요.");
-        }
     }
 }
