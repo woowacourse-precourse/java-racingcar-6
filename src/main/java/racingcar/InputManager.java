8e8b45d1cd88d6f526419457fefa9c982e9efc7c
dk7648 <dk7648@korea.ac.kr>
2023-10-31 15:09:36 +0900

    feat(InputManager): readNames메소드 추가

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -1,8 +1,15 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputManager {
     public String readNames() {
-        return null;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String enteredNames = readLine();
+        if (!Validation.validNames(enteredNames))
+            throw new IllegalArgumentException("영어와 쉼표로만 입력해주세요.");
+
+        return enteredNames;
     }
 
     public int readNumberOfTimes() {
