22b7da58d63297901af82699c9c6c0e51aacbd0c
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 06:39:36 +0900

    feat : 게임 진행 상황과 최종 결과를 출력하는 기능 구현
    - outputGameProgress() 메소드 구현
    - outputFinalSoloWinner() 메소드 구현
    - outputFinalJointWinners() 메소드 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
-public class Output {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Output {
+
+    public static void outputGameProgress(HashMap<String,String> hashMap) {
+        for (Map.Entry<String,String> m : hashMap.entrySet()) {
+            System.out.println(m.getKey() + " : " + m.getValue());
+        }
+        System.out.println();
+    }
+
+    public static void outputFinalSoloWinner(ArrayList<String> soloWinner) {
+        OutputMessage.outputFinalWinner();
+        System.out.println(soloWinner.get(0));
+    }
+
+    public static void outputFinalJointWinners(ArrayList<String> jointWinners) {
+        OutputMessage.outputFinalWinner();
+        System.out.println(String.join(", ", jointWinners));
+    }
 }
