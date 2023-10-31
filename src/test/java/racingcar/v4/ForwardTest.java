21aafc9322638311b2a66d2858a4ba123cfb8c70
권장순 <jsoonworld@gmail.com>
2023-11-01 01:29:17 +0900

    Test(racingcar) : canForward는 randomNum이 4 이상일 때 true, 4 미만일 떈 false를 반환하는 단위 테스트 코드 작성 및 통과. 구현 및 통과.

--- src/test/java/racingcar/v4/ForwardTest.java
+++ src/test/java/racingcar/v4/ForwardTest.java
@@ -7,10 +7,29 @@ class ForwardTest {
     @Test
     void randomNums는_0에서_9사이의_값을_반환한다() {
         Forward forward = new Forward();
-
         for (int i = 0; i < 1000; i++) {
             int randomNum = forward.randomNums();
             assertThat(randomNum).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
         }
     }
+
+    @Test
+    void canForward는_randomsNum이_4이상일_때_true를_반환한다() {
+        Forward forward = new Forward();
+
+        assertThat(forward.canForward(4)).isTrue();
+        assertThat(forward.canForward(5)).isTrue();
+        assertThat(forward.canForward(9)).isTrue();
+    }
+
+    @Test
+    void canForward는_randomsNum이_4미만일_때_false를_반환한다() {
+        Forward forward = new Forward();
+
+        assertThat(forward.canForward(0)).isFalse();
+        assertThat(forward.canForward(1)).isFalse();
+        assertThat(forward.canForward(3)).isFalse();
+    }
+
+
 }
