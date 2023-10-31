12094d5740c6e98c2cd8d4ce4efff2da87a9c760
NAM GIBEOM <rlqja0523@naver.com>
2023-10-31 18:39:52 +0900

    refactor(Car): 메서드명 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,12 +7,12 @@ public class Car {
     private int movedCount;
 
     public Car(String name) {
-        validateName(name);
+        validateNameLength(name);
         this.name = name;
         this.movedCount = 0;
     }
 
-    private void validateName(String name) {
+    private void validateNameLength(String name) {
         if (name.isBlank() || name.length() > CAR_NAME_MAX_LENGTH.getValue()) {
             throw new IllegalArgumentException(
                     String.format("rejected value: %s\n자동차 이름은 1자 이상 %d자 이하만 가능합니다.",
