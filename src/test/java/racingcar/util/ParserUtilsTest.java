65df6c3fc6fddb9190086ff0d98549c7fad6becc
seokmyungham <hellotjraud@gmail.com>
2023-11-01 08:46:31 +0900

    test(ParserUtilsTest): 가독성을 위한 코드라인 변경

--- src/test/java/racingcar/util/ParserUtilsTest.java
+++ src/test/java/racingcar/util/ParserUtilsTest.java
@@ -12,13 +12,13 @@ class ParserUtilsTest {
     @DisplayName("Parsing user input, separating it by commas")
     void 사용자의_자동차_이름_입력을_파싱해서_List_Collection_으로_반환() {
         String carNames1 = "ham,seok,myung";
-        List<String> expectedValue1 = Arrays.asList("ham", "seok", "myung");
         List<String> carNameArray1 = ParserUtils.namesParser(carNames1);
+        List<String> expectedValue1 = Arrays.asList("ham", "seok", "myung");
         Assertions.assertThat(carNameArray1).isEqualTo(expectedValue1);
 
         String carNames2 = "ham";
-        List<String> expectedValue2 = Arrays.asList("ham");
         List<String> carNameArray2 = ParserUtils.namesParser(carNames2);
+        List<String> expectedValue2 = Arrays.asList("ham");
         Assertions.assertThat(carNameArray2).isEqualTo(expectedValue2);
     }
 }
