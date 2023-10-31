33e8ead7fa285fdc44a71355c5326b5c01b7d6b8
yhcho0405 <yhcho0405@kakao.com>
2023-10-31 23:34:58 +0900

    feat: 중복된 이름의 자동차 입력에 대한 예외처리

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -40,12 +40,16 @@ public class Race {
     }
 
     private void validateInputs(List<Car> cars, int rounds) {
-        if (rounds <= 0) {
-            throw new IllegalArgumentException("라운드 수는 양수 여야만 합니다.");
+        if (cars == null || cars.isEmpty()) {
+            throw new IllegalArgumentException("경주에 참여하는 자동차 리스트를 입력해야 합니다.");
         }
 
-        if (cars == null || cars.isEmpty()) {
-            throw new IllegalArgumentException("경주에 참여하는 자동차는 한 대 이상 이여 야만 합니다");
+        long uniqueNamesCount = cars.stream()
+                .map(Car::getName)
+                .distinct()
+                .count();
+        if (uniqueNamesCount != cars.size()) {
+            throw new IllegalArgumentException("중복된 이름의 자동차가 있습니다.");
         }
     }
 
