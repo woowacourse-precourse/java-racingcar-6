dbf109e328e175cd25f3948dfce62c79d017fbbc
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-10-31 18:05:08 +0900

    feat(racingCar): 모든 입력받는 기능을 Input 클래스 상속받아서 구현

--- src/main/java/racingcar/domain/InputName.java
+++ src/main/java/racingcar/domain/InputName.java
@@ -2,24 +2,12 @@ package racingcar.domain;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-public class InputName {
+public class InputName extends Input{
     private final static String message= "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private String input;
 
     public InputName(){
-        print();
+        print(message);
         input();
-    }
-
-    private void print(){
-        System.out.println(message);
-    }
-
-    private void input(){
-        this.input = readLine();
-    }
-
-    public String getInput(){
-        return input;
     }
 }
