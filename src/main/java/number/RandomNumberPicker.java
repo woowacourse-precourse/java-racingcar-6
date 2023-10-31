fa89f92437191b85f14832466e32c8e77b37aa22
mainfn <_@mainfn.dev>
2023-10-31 13:11:08 +0900

    refactor: `RandomNumberPicker` 수정

--- src/main/java/number/RandomNumberPicker.java
+++ src/main/java/number/RandomNumberPicker.java
@@ -5,14 +5,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 public final class RandomNumberPicker implements NumberPicker {
     private static final int DEFAULT_START_INCLUSIVE = 0;
     private static final int DEFAULT_END_INCLUSIVE = 9;
-    private final int startInclusive = DEFAULT_START_INCLUSIVE;
-    private final int endInclusive = DEFAULT_END_INCLUSIVE;
 
     public RandomNumberPicker() {
     }
 
+    // 추후 필요성이 생길 경우 외부에서 startInclusive, endInclusive 주입할 수 있도록 변경
+
     @Override
     public int pick() {
-        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+        return Randoms.pickNumberInRange(DEFAULT_START_INCLUSIVE, DEFAULT_END_INCLUSIVE);
     }
 }
