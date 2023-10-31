ce641261ca7893d6f1509a80eeaf724237d3cf0b
Jaewon <choijawon99@gmail.com>
2023-10-31 17:13:11 +0900

    [DONE] Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         GameManager GM = new GameManager();
+
         Game game = new Game(GM.inputListCarsName(), GM.inputTrial());
 
         game.playGame();
