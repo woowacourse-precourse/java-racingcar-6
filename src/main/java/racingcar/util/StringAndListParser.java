c60062f631b409a9a9a3f8f31bd865668e3cbb36
Gyeonghun Min <riverpower6@naver.com>
2023-11-01 01:49:55 +0900

    Refactor: 사용되지 않는 코드 삭제

--- src/main/java/racingcar/util/StringAndListParser.java
+++ src/main/java/racingcar/util/StringAndListParser.java
@@ -1,11 +1,8 @@
 package racingcar.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class StringAndListParser {
-	private List<String> cars = new ArrayList<>();
 	private String[] carArray;
 	private String winner;
 
