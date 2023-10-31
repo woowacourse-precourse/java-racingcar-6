f915ad4fe5097d6e47c5902966127110b351403f
Seungjida <esg0107@pusan.ac.kr>
2023-10-31 12:15:05 +0900

    test(generateRandomNumber): 랜덤 숫자를 범위에 맞게 생성하는지 테스트하는 메서드 추가

--- src/test/java/racingcar/UtilsTest.java
+++ src/test/java/racingcar/UtilsTest.java
@@ -8,25 +8,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class UtilsTest {
+    @Test
+    void generateRandomNumberTest() {
+        int randomNumber = Utils.generateRandomNumber();
+
+        assertTrue(randomNumber >= 0 && randomNumber <= 9);
+    }
+
     @ParameterizedTest
     @CsvSource({"4", "5", "6", "7", "8", "9"})
     void isMoveTest_True(int input) {
-        // given
-
-        // when
-
-        // then
         assertTrue(Utils.isMove(input));
     }
 
     @ParameterizedTest
     @CsvSource({"0", "1", "2", "3"})
     void isMoveTest_False(int input) {
-        // given
-
-        // when
-
-        // then
         assertFalse(Utils.isMove(input));
     }
 }
