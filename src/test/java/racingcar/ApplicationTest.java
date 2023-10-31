f9d1c6caedb71832ade4c2ed71c7db125809f507
Identityless <rlandwnd555@naver.com>
2023-10-31 23:18:45 +0900

    test : 기존 테스트와 충돌을 일으키는 이름 입력 테스트 메서드 삭제

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -8,14 +8,12 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.component.Car;
 import racingcar.component.Inputter;
 import racingcar.component.Printer;
-import racingcar.component.RaceSimulator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -27,13 +25,11 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
     private Inputter inputter;
     private Printer printer;
-    private RaceSimulator simulator;
 
     @BeforeEach
     void initAll() {
         this.inputter = Inputter.getInputter();
         this.printer = Printer.getPrinter();
-        this.simulator = RaceSimulator.getRaceSimulator(this.printer);
     }
 
 
@@ -57,27 +53,13 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    @DisplayName("입력된 자동차 이름이 올바르게 리스트로 변환되는지에 대한 테스트")
-    void 자동차_이름_입력() {
-        String input = "wj,pobi,jun,nick";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        List<String> returns = inputter.getNameInput();
-        assertThat(returns)
-                .isNotNull()
-                .contains("wj")
-                .contains("pobi")
-                .contains("jun")
-                .contains("nick");
-    }
-
-    @Test
     @DisplayName("시도할 횟수 입력에 대한 예외 테스트")
     void 시도할_회수_예외_처리() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "two"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+        System.out.println();
     }
 
     @Test
