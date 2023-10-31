79cf097ab58a613838cde22c9f750d9630723012
inpink <inpink@kakao.com>
2023-11-01 02:06:43 +0900

    refactor: Constants를 enum에서 일반 class로 변경
    단순히 값만 사용하기 때문에 enum으로 관리하는 비용이 더 크기 때문임.

--- src/main/java/racingcar/domain/Constants.java
+++ src/main/java/racingcar/domain/Constants.java
@@ -1,17 +1,13 @@
 package racingcar.domain;
 
-public enum Constants {
-    MAX_CAR_LENGTH(5);
+public class Constants { //단순히 값만 사용하기 때문에 enum으로 관리하는 비용이 더 큼
+    public static final int MAX_CAR_LENGTH = 5;
+    public static final int MIN_MOVE_NUMBER = 4;
+    public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int MAX_RANDOM_NUMBER = 9;
 
-    private final int value;
+    private Constants() {
 
-    Constants(int value) {
-        this.value = value;
     }
-
-    public int getValue() {
-        return value;
-    }
-
 }
 
