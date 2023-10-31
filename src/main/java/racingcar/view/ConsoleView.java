62c947fda097cfd0095340bcc91303f562381950
donghoonyeom <dua5410773@naver.com>
2023-10-31 18:37:45 +0900

    refactor(ConsoleView): 사용자 입력을 받는 메서드를 수정
    ConsoleView 클래스에서 사용자 입력을 받는 메서드를 통합하여 일관된 인터페이스를 제공하도록 수정했습니다. 입력값을 리스트로 반환하도록 변경하여 의미 있는 데이터 구조를 반환하고, 불필요한 배열 처리를 최소화했습니다.

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -1,10 +1,22 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.Car;
 
 import java.util.List;
 
 public class ConsoleView {
+    public List<String> getInputCarNames() {
+        printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNamesArray = Console.readLine().split(",");
+        return List.of(carNamesArray);
+    }
+
+    public int getInputNumberOfAttempts() {
+        printMessage("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
     public void printMessage(String message) {
         System.out.println(message);
     }
