a0b91b6d525541da459ee528fe77439da0f46238
seunghye218 <seunghye@student.42seoul.kr>
2023-10-31 11:47:28 +0900

    feat: add forward method
    Get a random value between 0 and 9 and move if the random value is 4 or greater.
    
    The new feature checks for the following cases:
    
    - Don't move when a randomly generated integer is 1
    - Move when a randomly generated integer is 4
    - Move when a randomly generated integer is 9

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,16 @@
 package racingcar;
 
+import java.util.function.BiFunction;
+
 public class Car {
 
     private int distance;
     private final String name;
     private static final int NAME_MAX_LENGTH = 5;
     private static final int DISTANCE_MIN = 0;
+    private static final int RANDOM_LOWER_BOUND = 0;
+    private static final int RANDOM_UPPER_BOUND = 9;
+    private static final int FORWARD_CONDITION = 4;
 
 
     public Car(String name, int distance) {
@@ -23,5 +28,12 @@ public class Car {
     @Override
     public String toString() {
         return this.name + " : " + "-".repeat(this.distance);
+    }
+
+    public void forward(BiFunction<Integer, Integer, Integer> randomFunction) {
+        int randomNumber = randomFunction.apply(RANDOM_LOWER_BOUND, RANDOM_UPPER_BOUND);
+        if (randomNumber >= FORWARD_CONDITION) {
+            ++this.distance;
+        }
     }
 }
