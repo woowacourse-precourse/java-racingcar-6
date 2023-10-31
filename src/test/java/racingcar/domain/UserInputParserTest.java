06247da21be1208ede3b91d6795657ed6e388178
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 06:34:54 +0900

    test(domain.UserInputParser) : 기능1. 사용자 입력값을 받는다(숫자)
    사용자 입력값을 숫자로 변환하는지와 숫자가 아닌 값이 입력되었을 때 IllegalArgumentException을 발생시키는지 확인하는 테스트 코드를 작성함.

--- src/test/java/racingcar/domain/UserInputParserTest.java
+++ src/test/java/racingcar/domain/UserInputParserTest.java
@@ -1,13 +1,38 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputParserTest {
+    private UserInputParser userInputParser;
+
+    @BeforeEach
+    void setUp() {
+        userInputParser = new UserInputParser();
+    }
 
     @Test
-    void parseInt() {
+    void parseInt_메서드로_유저_입력_값을_숫자로_변환() {
+        // given
+        String userInput = "1";
+        // when
+        int result = userInputParser.parseInt(userInput);
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void parseInt_메서드에_숫자가_아닌_값을_입력하면_에러_발생() {
+        // given
+        String userInput = "a";
+        // when, then
+        assertThatThrownBy(() -> userInputParser.parseInt(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력 가능합니다.");
     }
 
     @Test
