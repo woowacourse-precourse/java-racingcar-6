13dfb761cac13c6ac1c774f6c008857e0c6a330a
Jung ui heon <ky7662@naver.com>
2023-10-31 21:58:58 +0900

    feat : 시도할 회수와 이동거리가 동일한 레이싱카가 있는지 확인하는 메서드 추가

--- src/main/java/racingcar/CarDataCheck.java
+++ src/main/java/racingcar/CarDataCheck.java
@@ -11,4 +11,14 @@ public class CarDataCheck {
             }
         }
     }
+
+    // 시도한 회수와 동일한 거리를 이동한 자동차가 있는지 확인하는 checkFinishedTime 메서드
+    public boolean checkFinishedTime(LinkedHashMap<String, Integer> racingCarNames, int tryNumber){
+        for(Integer carValue : racingCarNames.values()){
+            if(carValue == tryNumber){ // 이동한 거리 == 시도한 회수 인 자동차가 있다면 return true
+                return true;
+            }
+        }
+        return false; // 모든 자동차가 시도한 회수에 못 미치는 거리를 이동했다면 return false
+    }
 }
