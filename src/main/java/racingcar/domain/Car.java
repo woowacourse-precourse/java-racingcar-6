39da136bed6fa4b73616b8bba35bb1d4eb2bbcaf
dj <fjzl12@naver.com>
2023-10-31 16:15:45 +0900

    feat(car): 자동차 객체 및 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,29 @@
 package racingcar.domain;
 
 public class Car {
-    public String name;
-    public String position = "";
+    private final String name;
+    private String state = "";
 
     Car(String name) {
         this.name = name;
+    }
+
+    public void accelerate() {
+        state += "-";
+    }
+
+    public boolean canAccelerate(int power) {
+        if (power >= 4) {
+            return true;
+        }
+        return false;
+    }
+    
+    public String getState() {
+        return state;
+    }
+
+    public String getName() {
+        return name;
     }
 }
