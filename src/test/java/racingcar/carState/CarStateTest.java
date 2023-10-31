217be545a38df6d796a1ca55044f72e4469e4625
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-31 22:50:58 +0900

    test: 랜덤 값에 따른 차 직진 여부 테스트 추가
    랜덤 값이 0~3일 땐 제동, 4~9일 땐 직진

--- src/test/java/racingcar/carState/CarStateTest.java
+++ src/test/java/racingcar/carState/CarStateTest.java
@@ -2,13 +2,10 @@ package racingcar.carState;
 
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
-import racingcar.util.GameUtil;
 import racingcar.validator.CarNameValidator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class CarStateTest {
     @Test
@@ -16,5 +13,23 @@ public class CarStateTest {
         CarNameValidator validator = new CarNameValidator("1,2");
         assertThat(validator.NAMES).contains("2", "1");
         assertThat(validator.NAMES).containsExactly("1", "2");
+    }
+
+    @Test
+    void 차_직진_X_테스트(){
+        for (int i = 0; i <= 3; i++) {
+            Car car = new Car("Hyun");
+            car.move(i);
+            assertThat(car.getCarPosition()).isEqualTo(0);
+        }
+    }
+
+    @Test
+    void 차_직진_O_테스트(){
+        for (int i = 4; i <= 9; i++) {
+            Car car = new Car("Hyun");
+            car.move(i);
+            assertThat(car.getCarPosition()).isEqualTo(1);
+        }
     }
 }
