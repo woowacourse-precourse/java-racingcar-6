5995cc1b07e6c54a4f032b151257fdb74316a74c
nayonsoso <panda0329@naver.com>
2023-11-01 02:01:28 +0900

    feat: 양수인지 검증하는 함수 구현

--- src/main/java/view/AttemptTimesValidator.java
+++ src/main/java/view/AttemptTimesValidator.java
@@ -1,10 +1,17 @@
 package view;
 
 public class AttemptTimesValidator {
-    private void validateInteger(String input){
+
+    private void validateInteger(String input) {
         try {
             Integer.parseInt(input);
-        } catch (Exception e){
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validatePlus(int input) {
+        if (input < 0) {
             throw new IllegalArgumentException();
         }
     }
