43b35b08a305e8b7f9fc86fc604209df1603dd9e
saera <yook.saera@gmail.com>
2023-11-01 06:35:11 +0900

    feat(racingcar): 게임에 참가하는 자동차들 각각의 이름과 위치값을 가진 Car 타입 객체의 List를 생성하라

--- src/main/java/racingcar/Registration.java
+++ src/main/java/racingcar/Registration.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registration {
     public String[] getCarNames(String input) {
         return input.split(",");
+    }
+
+    public List<Car> createCarList(String[] carNames) {
+        List<Car> carList = new ArrayList<>();
+        for (String name : carNames) {
+            carList.add(new Car(name, 0));
+        }
+        return carList;
     }
 }
--- src/test/java/racingcar/RegistrationTest.java
+++ src/test/java/racingcar/RegistrationTest.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationTest {
@@ -14,6 +18,26 @@ public class RegistrationTest {
         String[] actual = registration.getCarNames(input);
 
         assertThat(actual).containsExactly("po bi", "woni", " jun");
+    }
+
+    @Test
+    void createCarList_메서드가_문자열_배열을_받아_Car_타입_객체의_배열을_생성() {
+        String[] carNames = {"pobi", "woni", "jun"};
+        Registration registration = new Registration();
+
+        List<Car> carList = registration.createCarList(carNames);
+        List<String> actualName = new ArrayList<>();
+        for (Car car : carList) {
+            actualName.add(car.getName());
+        }
+        Set<Integer> actualPosition = new HashSet<>();
+        for (Car car : carList) {
+            actualPosition.add(car.getPosition());
+        }
+
+        assertThat(carList.size()).isEqualTo(3);
+        assertThat(actualName).containsExactly("pobi", "woni", "jun");
+        assertThat(actualPosition).containsExactly(0);
     }
 
 }
