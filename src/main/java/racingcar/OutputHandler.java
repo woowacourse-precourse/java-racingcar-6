2e20f4dc3554bfd891f2dbdac81fa275d71abd7c
Park Eunhyeong <silbroeh@gmail.com>
2023-11-01 00:30:54 +0900

    feat : OutputHandler클래스 - printFinalResult() 메소드 구현

--- src/main/java/racingcar/OutputHandler.java
+++ src/main/java/racingcar/OutputHandler.java
@@ -3,9 +3,23 @@ package racingcar;
 import java.util.List;
 
 public class OutputHandler {
+    private final static int ZERO = 0;
+    private final static String LINK = " : ";
+    private final static String DISTANCE_UNIT = "-";
     public static void printEachResult(List<Car> carList) {
         for (Car car : carList) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+            System.out.println(car.getName() + LINK + DISTANCE_UNIT.repeat(car.getDistance()));
+        }
+        System.out.println();
+    }
+
+    public static void printFinalResult(List<String> nameList) {
+        System.out.print("최종 우승자 : ");
+        for (int index = ZERO; index < nameList.size(); index++) {
+            System.out.print(nameList.get(index));
+            if (index + 1 == nameList.size()) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
     }
