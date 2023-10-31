570b529a56a6a5032840765a2b665318a968681f
hyunjin <guswlsdl0121@gmail.com>
2023-11-01 00:29:18 +0900

    test: Name에 대한 단위 테스트 진행

---
+++ src/test/java/racingcar/common/type/NameTest.java
@@ -0,0 +1,24 @@
+package racingcar.common.type;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+    @Test
+    public void 이름_생성() {
+        Name name = Name.of("hyunjin");
+        Assertions.assertEquals("hyunjin", name.getName());
+    }
+
+    @Test
+    public void 이름_조회() {
+        Name name = Name.of("hyunjin");
+        Assertions.assertEquals("hyunjin", name.getName());
+    }
+
+    @Test
+    public void 이름_앞뒤_공백() {
+        Name name = Name.of(" hyunjin ");
+        Assertions.assertEquals("hyunjin", name.getName());
+    }
+}
