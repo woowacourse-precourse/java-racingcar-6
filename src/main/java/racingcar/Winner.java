e4112e34495e2d0c12d07226599cebc0c4bedaa9
이희수 <hslee9234@naver.com>
2023-11-01 01:05:48 +0900

    feat: Winner 객체 구현

---
+++ src/main/java/racingcar/Winner.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.sql.Array;
+import java.util.*;
+public class Winner {
+    public static List<Car> getWinners(List<Car> cars){
+        int maxPosition=getMaxPosition(cars);
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : cars){
+            if (car.getPosition() == maxPosition){
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private static int getMaxPosition(List<Car> cars){
+        int maxPosition=0;
+
+        for(Car car : cars){
+            if(car.getPosition() > maxPosition){
+                maxPosition = car.getPosition();
+            }
+        }
+        return maxPosition;
+    }
+
+}
+
