c8ca9de94967f34fd0ef26636364f2c9bda8a3b2
alreadysons <alreadysons@gmail.com>
2023-10-31 18:32:37 +0900

    기존에 하나의 함수에 있던 기능을 각각의 racing,racer 클래스에 옮겨 구현을 시켰고, 우승자 출력하는 기능을 racing클래스 안에 구현하였다ㅏ.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,56 +1,39 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
-    private static String[] RacerInput(){ //사용자의 입력을 받는 함수
+    private static List<Racer> RacerInput(){ //사용자의 입력을 받는 함수
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String RacerNameInput = Console.readLine();
         return RacerCheck(RacerNameInput);
     }
-    private static String[] RacerCheck(String RacerNameChk){ //사용자에 입력값에 따른 유효성 검사
+    private static List<Racer> RacerCheck(String RacerNameChk){ //사용자에 입력값에 따른 유효성 검사
         String[] RacerNames = RacerNameChk.split(","); //사용자의 ","에 따라 값을 나눠 리스트에 저장
-        if (!RacerNameChk.contains(",")) return RacerNames; //사용자가 1개의 값을 입력한 경우 바로 출력
+        List<Racer> racers = new ArrayList<>();
         for (String racerName : RacerNames) {
-            if (racerName.length() > 5) throw new IllegalArgumentException("유효하지않은 수 입니다."); //사용자의 입력값 유효성검사
+            if (racerName.length() > 5) throw new IllegalArgumentException("유효하지않은 수 입니다.");
+            racers.add(new Racer(racerName));
         }
-        return RacerNames; //유효성 검사 후 리스트 return
+        return racers; //racer 클래스에 정보 전달
     }
     private static int RacingCount(){
         System.out.println("시도할 회수는 몇회인가요?");
         String RacingCountInput = Console.readLine(); //시도할 횟수를 입력받음
-        try{return Integer.parseInt(RacingCountInput);} //시도할 횟수를 Int 형식으로 return
-        catch (NumberFormatException ex) {throw new IllegalArgumentException("유효하지 않은 수 입니다.");} //수가 아닌 경우 에러
-    }
-    private static void Racing_setting(){ //Racer에 대한 정보와, Racing횟수에 대한 정보를 Racing 함수에 전달
-        String[] RacerNames = RacerInput();
-        int RacingCount = RacingCount();
-        Racing(RacerNames,RacingCount);
-    }
-    private static void Racing(String[] RacerName,int RacingCount){
-        ArrayList<Integer> RacerList = new ArrayList<>();
-        for(int i=0;i<RacingCount;i++){
-            for(int j=0;j<RacerName.length;j++) {
-                RacerList.add(Randoms.pickNumberInRange(1, 9));
-            }
-            RacingResult(RacerList,RacerName);
-            RacerList.clear();
+        try{
+            return Integer.parseInt(RacingCountInput);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("유효하지 않은 수 입니다.");
         }
     }
-    private static void RacingResult(ArrayList<Integer>RacerRanking, String[] RacerName){
-        ArrayList<Integer> CounterList = new ArrayList<>();
-        for(int i=0;i<RacerRanking.size();i++){
-            if (RacerRanking.get(i)>=4){
 
-            }
-        }
-    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Racer racer = new Racer(RacerInput());
+        List<Racer> racers = RacerInput();
         Racing racing = new Racing(RacingCount());
-        racing.Race(racer);
+        racing.Race(racers);
     }
 }
--- src/main/java/racingcar/Racer.java
+++ src/main/java/racingcar/Racer.java
@@ -1,18 +1,20 @@
 package racingcar;
 
-import java.util.Arrays;
-
-public class Racer {
-    public final  String[] name;
+class Racer {
+    public final String name;
     public final StringBuilder position;
-    public Racer(String[] name) {
+
+    public Racer(String name) {
         this.name = name;
         this.position = new StringBuilder();
     }
-    public void advance(){
+
+    public void advance(){ //각 레이서 별의 position을 '-'로 변환
         position.append("-");
     }
-    public String toString(){
-        return Arrays.toString(name) + " : " + position.toString();
+
+    @Override
+    public String toString(){ //racer별로 position출력
+        return name + " : " + position.toString();
     }
 }
