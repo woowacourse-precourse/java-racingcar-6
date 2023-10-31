3b5fb5e6500c3146a36cd33ef75bce6fea9bc097
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-31 15:52:01 +0900

    refactor: 게임 결과 관련 DTO 출력 책임 이전

--- src/main/java/racingcar/io/OutputManager.java
+++ src/main/java/racingcar/io/OutputManager.java
@@ -1,21 +1,39 @@
 package racingcar.io;
 
 import racingcar.config.GameMessages;
+import racingcar.dto.CarStateDto;
+import racingcar.dto.GameResultDto;
+import racingcar.dto.RoundResultDto;
+
+import java.util.List;
 
 public class OutputManager {
+    private static final String JOIN_DELIMITER = ", ";
+
     public void printCarNameInputMessage() {
         printMessage(GameMessages.INPUT_CAR_NAME);
     }
     public void printTryCountInputMessage() {
         printMessage(GameMessages.INPUT_TRY_COUNT);
     }
-
     public void printExecutionResultMessage() {
         printMessage(GameMessages.EXECUTION_RESULT);
     }
 
-    public void printFinalWinnerMessage(String winnerNames) {
+    public void printRoundResult(RoundResultDto roundResultDto) {
+        List<CarStateDto> carStates = roundResultDto.getCarStates();
+        for (CarStateDto carState : carStates) {
+            printMessage(carState.getCarStatus());
+        }
+    }
+
+    public void printFinalWinner(GameResultDto gameResult) {
+        String winnerNames = String.join(JOIN_DELIMITER, gameResult.getWinnerNames());
         printMessage(GameMessages.FINAL_WINNER + winnerNames);
+    }
+
+    public void printLine() {
+        printMessage("");
     }
 
     public static void printMessage(String message) {
