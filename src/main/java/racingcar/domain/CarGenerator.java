1b75da625aee9cb320c381ef49f9694eff6b8b5f
Junobee25 <wnsdhqo@naver.com>
2023-10-31 19:24:13 +0900

    feat(domain): splitStringByCommas 메서드 구현

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -1,12 +1,17 @@
 package racingcar.domain;
 
 import racingcar.car.Car;
-
+import camp.nextstep.edu.missionutils.Console;
 public class CarGenerator {
 
     Car car = new Car();
 
     public void createCarList() {
 
+    }
+
+    public String[] splitStringByCommas() {
+        String[] carList = Console.readLine().split(",");
+        return carList;
     }
 }
