347b18e4f9aadc8534e810d9e4a89416649118c5
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:31:22 +0900

    feat(NumberValidator): Number 양수 여부 검증 구현

--- src/main/java/racingcar/validator/AttemptNumberValidator.java
+++ src/main/java/racingcar/validator/AttemptNumberValidator.java
@@ -6,6 +6,9 @@ public class AttemptNumberValidator {
     public static int validateNumber(String number) {
         try {
             int attempts = Integer.parseInt(number);
+            if (attempts <= 0) {
+                throw new IllegalArgumentException(INVALID_ATTEMPTS_ERROR);
+            }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ATTEMPTS_FORMAT_ERROR);
         }
