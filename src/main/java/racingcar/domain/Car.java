45d23598c55b2c32356df44695cddd73232ffff8
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:14:34 +0900

    Feat: 필드값과 메서드 추가
    - 현재 자동차의 전진 거리를 보여주는 distance 필드 추가
    - 각 필드값들의 getter, setter 추가
    - 생성자 안에 StringBuilder 객체를 초기화하는 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,11 +3,44 @@ package racingcar.domain;
 public class Car {
     private String carName;
     private int randomNumber;
-    private int forwarDistance;
+    private int forwardDistance;
 
-    public Car(){}
+    private StringBuilder distance;
+
+
+    public Car(){
+        distance = new StringBuilder();
+    }
 
     public void setCarName(String carName) {
         this.carName = carName;
+    }
+
+    public void setRandomNumber(int randomNumber){
+        this.randomNumber = randomNumber;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public int getForwardDistance() {
+        return distance.length();
+    }
+
+    public void setForwardDistance(int forwarDistance) {
+        this.forwardDistance = forwarDistance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance.append(distance);
+    }
+
+    public StringBuilder getDistance() {
+        return distance;
     }
 }
