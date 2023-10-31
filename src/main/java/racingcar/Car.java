636f97707cb8f5002549c764b14a9787621b2694
jiwon1217 <koagjw@gmail.com>
2023-10-31 11:57:07 +0900

    :recycle: refactor: Car 생성자 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,11 @@ public class Car {
     private String name;
     private int position;
 
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
     public String getName() {
         return name;
     }
