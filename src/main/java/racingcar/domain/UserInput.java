51012b52a5c3ed99aa63007e8c549037ccb49a5e
dj <fjzl12@naver.com>
2023-10-31 16:18:49 +0900

    feat(UserInput): 자동차 이름 및 진행 횟수 입력 기능 구현

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -2,17 +2,30 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import javax.naming.Name;
 import java.util.List;
 
 public class UserInput {
     public static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        List<String> CarNameList = NameValidator.splitComma(input);
-        return CarNameList;
-        // validate가 되면 List<Car> 에 객체로 넣는다. (CarGenerator - createCarList 에서)
+        List<String> nameList = NameValidator.splitComma(input);
+        if (!NameValidator.correctNumberOfChar(nameList)) {
+            throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다.");
+        }
+        if (!NameValidator.hasOnlyAlphabets(nameList)) {
+            throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다.");
+        }
+        if (!NameValidator.isNotRedundant(nameList)) {
+            throw new IllegalArgumentException("중복된 값을 입력하여 게임을 종료합니다.");
+        }
+        return nameList;
     }
 
-    public void setRoundOfGame() {
-
+    public static int setRoundOfGame() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        int round = RoundValidator.checkNaturalNumber(input);
+        return round;
     }
 }
