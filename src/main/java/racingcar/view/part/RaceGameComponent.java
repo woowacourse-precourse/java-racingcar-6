3612b4a90ed1f19bfe19531ecec0883b4f53b812
JaehongDev <println@kakao.com>
2023-10-31 12:57:34 +0900

    feat: 레이싱 진행 컴포넌트 구현

--- src/main/java/racingcar/view/part/RaceGameComponent.java
+++ src/main/java/racingcar/view/part/RaceGameComponent.java
@@ -1,19 +1,26 @@
 package racingcar.view.part;
 
 import java.util.List;
+import java.util.stream.Stream;
 import racingcar.event.core.EventListener;
+import racingcar.event.part.RaceGameEvent;
 import racingcar.view.core.OutputView;
 import racingcar.view.core.ScreenComponent;
-import racingcar.view.part.RaceGameComponent.RaceGameResult.TrackPerResult;
 
 public record RaceGameComponent(OutputView outputView, EventListener eventListener) implements ScreenComponent {
     @Override
     public void render() {
-        final List<TrackPerResult> trackPerResult = List.of(new TrackPerResult("pobi", 5), new TrackPerResult("jaehong", 3));
-        outputView.printTrackPerResult(trackPerResult);
+        Stream.generate(() -> eventListener.listenWithResult(RaceGameEvent::new))
+                .takeWhile(RaceGameResult::isNotCompleted)
+                .map(RaceGameResult::trackPerResults)
+                .forEach(outputView::printTrackPerResult);
     }
 
-    public record RaceGameResult(List<TrackPerResult> trackPerResults) {
+    public record RaceGameResult(boolean isCompleted, List<TrackPerResult> trackPerResults) {
+        public boolean isNotCompleted() {
+            return !isCompleted;
+        }
+
         public record TrackPerResult(String name, int distance) {
 
         }
