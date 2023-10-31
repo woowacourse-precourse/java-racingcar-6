aac8a7606580cdf1aefba957bd8a464dd8380b52
saera <yook.saera@gmail.com>
2023-11-01 05:00:27 +0900

    refactor(MessageContainerTest): showCarResult()를 테스트하는 함수 이름을 수정하라

--- src/test/java/racingcar/MessageContainerTest.java
+++ src/test/java/racingcar/MessageContainerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class MessageContainerTest {
 
     @Test
-    void printCarResult_메서드로_자동차의_이름과_현재_위치를_출력() {
+    void showCarResult_메서드로_자동차의_이름과_현재_위치를_나타내는_문자열_생성() {
         Car car1 = new Car("pobi", 0);
         Car car2 = new Car("woni", 2);
         Car car3 = new Car("jun", 5);
