e39204ed167bddaa6eda360b978ca5b2ac801de6
HYOAN <aspqll@naver.com>
2023-10-31 20:02:26 +0900

    fix(racingcar):사용자 입력 기능 수정

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -9,20 +9,24 @@ import java.util.List;
 public class UserInput {
     public UserInput() {
     }
+
     public String input() {
         String input = Console.readLine();
         return input;
     }
+
     public List<String> carName(String input) {
         String[] names = input.split(",");
         List<String> carNames = new ArrayList<>(Arrays.asList(names));
         return carNames;
     }
+
     public int attemptCount(String input) {
         int attemptCount;
         attemptCount = Integer.parseInt(input);
         return attemptCount;
     }
+
     public void processException() {
     }
 }
