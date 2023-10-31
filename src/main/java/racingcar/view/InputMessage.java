1043c4207b47da375068c239a40049f051de8864
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 05:41:19 +0900

    feat : 입력 안내 메세지 출력 기능 구현
    각 상황에 맞는 메세지(문자열)를 출력할 수 있도록 메소드들을 구현

--- src/main/java/racingcar/view/InputMessage.java
+++ src/main/java/racingcar/view/InputMessage.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
-public class InputMessage {
+public final class InputMessage {
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public static void inputCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+    }
+
+    public static void inputTryNumber() {
+        System.out.println(INPUT_TRY_NUMBER_MESSAGE);
+    }
 }
