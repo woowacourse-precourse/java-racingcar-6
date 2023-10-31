9091a9745f426c4ad2ec960ab68d94ac70e82c2f
yang <hc1489@naver.com>
2023-10-31 18:13:13 +0900

    determineCanGo 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@
    - 사용자에게 자동차 이름들을 입력 받아 각 요소로 나누어 반환 
 2. tryCountInput()
     - 사용자에게 시도 횟수 입력 받기
-3. nameInputValidation()
+3. nameValidation()
     - 사용자에게 자동차 이름을 입력 받는 경우, 이름의 길이에 대해 유효성 검사
 4. determineCanGo()
    - 무작위 숫자를 생성하여 4이상인지 판단 후 전진 가능 여부 결정
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private String name;
     private String distance = "";
     private boolean canGo;
+
+    private void determineCanGo() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+
+        this.canGo = randomNumber >= 4;
+    }
 }
