d48bf213e58e16be0711cae5ffb58850d9d314c5
scarlet0o0 <ghd7673@naver.com>
2023-10-31 13:10:26 +0900

    Refactor : view 모든 문자열 상수화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,9 +3,10 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String COMMA = ",";
 
     public static String[] promptForCarNames() {
-        return Console.readLine().split(",");
+        return Console.readLine().split(COMMA);
     }
 
     public static int promptNumberOfAttempts() {
--- src/main/java/racingcar/view/OutView.java
+++ src/main/java/racingcar/view/OutView.java
@@ -6,15 +6,21 @@ import racingcar.dto.CarDto;
 import racingcar.dto.RaceConditionDto;
 
 public class OutView {
-
+    private static final String RACE_CAR_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUM_TRIES_PROMPT = "시도할 회수는 몇회인가요?";
+    private static final String EXECUTION_RESULT_PROMPT = "실행 결과";
+    private static final String WINNER_PROMPT = "최종 우승자 : %s";
     private final static String DEFAULT_TRAVEL_LENGTH = "-";
+    private static final String RACING_STATUS_PROMPT = "%s : %s \n";
+    private static final String COMMA = ", ";
+
 
     public static void printCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(RACE_CAR_PROMPT);
     }
 
     public static void printNumberOfAttempts(){
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(NUM_TRIES_PROMPT);
     }
 
     public static void printRaceResults(RaceConditionDto raceConditionDto) {
@@ -27,17 +33,17 @@ public class OutView {
     public static void printFinalWinner(List<CarDto> carDtoList){
         String winners = carDtoList.stream()
                 .map(CarDto::name)
-                .collect(Collectors.joining(", "));
-        System.out.printf("최종 우승자 : %s", winners);
+                .collect(Collectors.joining(COMMA));
+        System.out.printf(WINNER_PROMPT, winners);
     }
 
     public static void printResults() {
-        System.out.println("실행 결과");
+        System.out.println(EXECUTION_RESULT_PROMPT);
     }
 
     private static void printCarDtoList(List<CarDto> carDtoList) {
         for(CarDto carDto : carDtoList){
-            System.out.printf("%s : %s \n", carDto.name(), DEFAULT_TRAVEL_LENGTH.repeat(carDto.travelDistance()));
+            System.out.printf(RACING_STATUS_PROMPT, carDto.name(), DEFAULT_TRAVEL_LENGTH.repeat(carDto.travelDistance()));
         }
         System.out.println();
     }
