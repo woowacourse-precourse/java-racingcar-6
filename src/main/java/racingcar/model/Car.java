1dc1187eca9ae01b69531d676859a203313d0b57
Flash-KIM142 <topqr123q@gmail.com>
2023-11-01 00:56:05 +0900

    style: 사용하지 않는 변수 삭제

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,8 +2,6 @@ package racingcar.model;
 
 public class Car implements Comparable<Car> {
 
-    private static final String BETWEEN_NAME_AND_POSITION = " : ";
-    private static final String POSITION_UNIT = "-";
     private static final int GOOD_TO_RUN = 4;
 
     private final Name name;
