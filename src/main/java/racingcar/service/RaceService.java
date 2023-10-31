1dd17f2af4bf0a66236fba5b94b719b4545da99c
seokmyungham <hellotjraud@gmail.com>
2023-10-31 09:27:54 +0900

    feat(RaceService): 클래스 생성 및 initializeRace 메서드 추가
    initializeRace: Race 클래스의 initializeRace 를 호출하는 기능을 합니다

---
+++ src/main/java/racingcar/service/RaceService.java
@@ -0,0 +1,15 @@
+package racingcar.service;
+
+import racingcar.domain.Race;
+
+public class RaceService {
+    private final Race race;
+
+    public RaceService() {
+        this.race = new Race();
+    }
+
+    public void initializeRace(String attemptCountInput, String carNamesInput) {
+        race.initializeRace(attemptCountInput, carNamesInput);
+    }
+}
