b5e4da6539b966502777c508cf9de92fd795c0c6
park_yeongyu <sjjs7771@naver.com>
2023-10-31 18:53:56 +0900

    refactor : Car클래스 model 패키지로 리펙터링

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,10 +1,8 @@
-package racingcar;
+package model;
 
 public class Car {
     private String name;
     private int runCount = 0;
-
-    int moveCount;
 
     public void setCarName(String name) {
         this.name = name;
