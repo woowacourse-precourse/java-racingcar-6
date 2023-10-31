94d57e939f4be2feb792d4fb2d386964b347d978
JongMinCh0i <wonderland314592@gmail.com>
2023-10-31 16:32:41 +0900

    refactor(Car): 매직 리터럴 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,6 +4,7 @@ public class Car {
 
     private final static int LIMIT_NAME_LENGTH = 6;
     private final static String CAR_NAME_LENGTH_FIVE_UNDER = "자동차의 이름은 5글자 이하만 가능합니다.";
+    private final static String CAR_NAME_NO_BLANK = "자동차의 이름은 공백이 불가능합니다.";
 
     private final String name;
     private Integer distance = 0;
@@ -21,7 +22,7 @@ public class Car {
         }
 
         if (name.isEmpty() || name.isBlank()) {
-            throw new IllegalArgumentException("자동차의 이름은 공백이 불가능합니다.");
+            throw new IllegalArgumentException(CAR_NAME_NO_BLANK);
         }
     }
 
