27c852425dc538df4399fa5038676c59bd0120ab
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-01 02:11:07 +0900

    chore: List<Car> 변수명 변경

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -3,12 +3,12 @@ package racingcar.domain;
 import java.util.List;
 
 public class RacingGame {
-    private List<Car> carList;
+    private List<Car> cars;
     private final Referee referee;
     private final int round;
 
     public RacingGame(List<Car> carList, Referee referee, int round) {
-        this.carList = carList;
+        this.cars = carList;
         this.referee = referee;
         this.round = round;
     }
