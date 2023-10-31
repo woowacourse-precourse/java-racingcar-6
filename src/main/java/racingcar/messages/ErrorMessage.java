db5584371754dc083dee2f83ae19d2c23d8347a7
조은별 <joeunbyeol98@gmail.com>
2023-11-01 00:43:26 +0900

    feat(ErrorMessage): 시도 횟수 입력 예외 문자열 수정

--- src/main/java/racingcar/messages/ErrorMessage.java
+++ src/main/java/racingcar/messages/ErrorMessage.java
@@ -6,6 +6,6 @@ public class ErrorMessage {
     public static String CAR_NAMES_NOT_UNIQUE = ERROR_TAG + "자동차 이름은 중복될 수 없습니다.";
     public static String CAR_NAME_LENGTH_OUT_OF_RANGE = ERROR_TAG + "자동차 이름은 1~5자만 가능합니다.";
 
-    public static String TRY_COUNT_NOT_POSITIVE_INTEGER = ERROR_TAG + "시도 회수는 0보다 큰 정수만 가능합니다.";
-    public static String TRY_COUNT_OVERFLOW = ERROR_TAG + "시도 회수는 2147483647를 넘을 수 없습니다.";
+    public static String TRY_COUNT_NOT_INTEGER = ERROR_TAG + "시도 회수는 21474836470이하 정수만 가능합니다.";
+    public static String TRY_COUNT_NOT_POSITIVE = ERROR_TAG + "시도 횟수는 1이상만 가능합니다.";
 }
