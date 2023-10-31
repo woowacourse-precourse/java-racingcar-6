910f7addf279b9353eae9a5cabb860d7e72316db
dongho <dons97@naver.com>
2023-11-01 00:46:25 +0900

    feat: Implement Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,44 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import racingcar.controller.InputProcessor;
+import racingcar.controller.JudgeMovement;
+import racingcar.controller.JudgeWinner;
+import racingcar.model.RaceGameStateModel;
+import racingcar.view.RaceGameUserInterfaceView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RaceGameUserInterfaceView.displayUserCarInput();
+        String userCarInput = RaceGameUserInterfaceView.getInput();
+        RaceGameUserInterfaceView.displayTotalRoundInput();
+        String totalRoundInput = RaceGameUserInterfaceView.getInput();
+
+        List<String> userCars = InputProcessor.userCarInputProcessor(userCarInput);
+        int totalRound = InputProcessor.totalRoundInputProcessor(totalRoundInput);
+
+        RaceGameStateModel raceGameStateModel = new RaceGameStateModel(totalRound, userCars);
+
+        RaceGameUserInterfaceView.raceGameStartMessage();
+
+        for (int i = 0; i < raceGameStateModel.getTotalRound(); i++) {
+            for (String userCar : userCars) {
+                int randomNumber = JudgeMovement.getRandomNumber(0, 9);
+                boolean goFlag = JudgeMovement.goFlag(randomNumber, 4);
+
+                if (goFlag){
+                    int curDistance = raceGameStateModel.getUserCarDistance(userCar);
+                    raceGameStateModel.updateUserDistance(userCar, curDistance+1);
+                }
+            }
+            LinkedHashMap<String, Integer> userCarDistance = raceGameStateModel.getUserCarDistance();
+            RaceGameUserInterfaceView.displayUserCarDistance(userCarDistance);
+        }
+        LinkedHashMap<String, Integer> userCarDistance = raceGameStateModel.getUserCarDistance();
+        List<String> winners = JudgeWinner.judgeWinner(userCarDistance);
+        RaceGameUserInterfaceView.displayWinner(winners);
+
     }
 }
