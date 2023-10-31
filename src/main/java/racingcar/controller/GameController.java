7f95fc23cfd8b3b95c012b0a558b5df5abf48c33
KJBig <kjgi73k@gmail.com>
2023-10-31 16:50:32 +0900

    feat : 게임 시작 메세지 출력 기능
    - GameInputView의 static 메소드인 printStartMessage()를 GameController에서 출력.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,14 +1,17 @@
 package racingcar.controller;
 
 import racingcar.service.GameService;
+import racingcar.view.GameInputView;
+import racingcar.view.GameOutputView;
 
 public class GameController {
     private final GameService gameService;
+
     public GameController() {
         this.gameService = new GameService();
     }
 
     public void start(){
-
+        GameInputView.printStartMessage();
     }
 }
---
+++ src/main/java/racingcar/view/GameInputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class GameInputView {
+
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public GameInputView() {
+    }
+    public static void printStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+}
