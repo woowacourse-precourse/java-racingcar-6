914a97fb8449f528c34ca1307cab9dba93ac5c17
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-10-31 23:13:53 +0900

    Test: ParameterizedTest로 테스트 단순화

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -4,15 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
     private final InputView inputView = new InputView();
 
+    @BeforeEach
+    void setUp() {
+        Console.close();
+    }
+
     @Test
     void 시도횟수_입력이_양의_정수일경우_정상적으로_진행() {
-        Console.close();
-
         String input = "5";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
@@ -20,35 +26,10 @@ class InputViewTest {
         inputView.getTrials();
     }
 
-    @Test
-    void 시도횟수_입력에_문자가_포함된경우_예외발생() {
-        Console.close();
-
-        String input = "5aa";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
-
-    }
-
-    @Test
-    void 시도횟수_입력이_음수일경우_예외발생() {
-        Console.close();
-
-        String input = "-5";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 시도횟수_입력이_0일경우_예외발생() {
-        Console.close();
-
-        String input = "0";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+    @ParameterizedTest
+    @ValueSource(strings = {"5aa", "-5", "0"})
+    void 시도횟수_입력_예외발생_테스트_문자포함_음수_0(String userInput) {
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
         Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
