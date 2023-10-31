30afa4b8d9af93c79fcca0ef31783ce2ff189d2a
Gyeonghun Min <riverpower6@naver.com>
2023-11-01 04:35:22 +0900

    Refactor: 메서드 순서 정리

--- src/main/java/racingcar/service/DecideWinnerService.java
+++ src/main/java/racingcar/service/DecideWinnerService.java
@@ -12,29 +12,28 @@ public class DecideWinnerService {
 	private String winner;
 	private List<String> winnerList = new ArrayList<>();
 
-	public void getMax(List<Car> cars) {
-		for (int i=0; i<cars.size(); i++) {
-			compareMax(cars.get(i).getDistance().length());
+	public void compareMax(int length) {
+		if (isMax(length)) {
+			maxLength = length;
 		}
 	}
 
-	public void compareMax(int length) {
-		if (maxLength < length) {
-			maxLength = length;
+	public void addWinner(List<Car> cars, int index) {
+		if (isWinner(cars, index)) {
+			winnerList.add(cars.get(index).getName());
 		}
 	}
 
 	public void setWinnerList(List<Car> cars) {
 		getMax(cars);
-
-		for (int i=0; i<cars.size(); i++) {
+		for (int i = 0; i < cars.size(); i++) {
 			addWinner(cars, i);
 		}
 	}
 
-	public void addWinner(List<Car> cars, int index) {
-		if (cars.get(index).getDistance().length() == maxLength) {
-			winnerList.add(cars.get(index).getName());
+	public void getMax(List<Car> cars) {
+		for (int i = 0; i < cars.size(); i++) {
+			compareMax(cars.get(i).getDistance().length());
 		}
 	}
 
@@ -46,5 +45,13 @@ public class DecideWinnerService {
 	public String getResult(List<Car> cars) {
 		winner = parser.parseToString(getWinner(cars));
 		return winner;
+	}
+
+	private boolean isMax(int length) {
+		return maxLength < length;
+	}
+
+	public boolean isWinner(List<Car> cars, int index) {
+		return cars.get(index).getDistance().length() == maxLength;
 	}
 }
