fedcdd2f42738853181d4476f1b79428f4522d60
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-31 22:13:54 +0900

    refactor ($InputView) : 함수 합함 startGame 함수 (차 이름 입력받는 함수) + inputCarNameToList 함수(입력값을 리스트로 변환하는 함수) = startGame 함수에 .split(",") 추가해서 합침 - 반환값 String -> String[] 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,15 +2,10 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class InputView {
-    public String startGame(){
+    public String[] startGame(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
-    }
-    public List<String> inputCarNameToList(String inputCarName){
-        return List.of(inputCarName.split(","));
+        return Console.readLine().split(",");
     }
     public Integer inputTryCount(){
         System.out.println("시도할 회수는 몇회인가요?");
