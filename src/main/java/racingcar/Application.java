6e422214eeef60b0cd29ab816315612addf52a58
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-31 06:43:46 +0000

    [Main] Refactoring : 인텐트 depth 2 되도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,24 +6,36 @@ import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
         try {
-            String[] carNames = InputManager.getCarNames();
-            int numberOfAttempts = InputManager.getNumberOfAttempts();
-
-            List<Car> cars = new ArrayList<>();
-            for (String name : carNames) {
-                cars.add(new Car(name));
-            }
-
-            for (int round = 1; round <= numberOfAttempts; round++) {
-                Race.startRace(cars, 1);
-                OutputManager.printRoundResult(round, cars);
-            }
-
-            List<Car> winners = RaceGame.getWinners(cars);
-            OutputManager.printWinners(winners);
-
+            playRacingGame();
         } catch (IllegalArgumentException e) {
-            System.out.println("오류: " + e.getMessage());
+            handleException(e);
         }
+    }
+
+    private static void playRacingGame() {
+        String[] carNames = InputManager.getCarNames();
+        int numberOfAttempts = InputManager.getNumberOfAttempts();
+
+        List<Car> cars = initializeCars(carNames);
+
+        for (int round = 1; round <= numberOfAttempts; round++) {
+            Race.startRace(cars, 1);
+            OutputManager.printRoundResult(round, cars);
+        }
+
+        List<Car> winners = RaceGame.getWinners(cars);
+        OutputManager.printWinners(winners);
+    }
+
+    private static List<Car> initializeCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    private static void handleException(IllegalArgumentException e) {
+        System.out.println("오류: " + e.getMessage());
     }
 }
