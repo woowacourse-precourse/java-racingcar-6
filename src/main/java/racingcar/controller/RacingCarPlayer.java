bc65978f46cb18950adcde655f869f3906db55c7
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-01 03:26:18 +0900

    style(RacingCarPlayer) : fix newlines and spaces

--- src/main/java/racingcar/controller/RacingCarPlayer.java
+++ src/main/java/racingcar/controller/RacingCarPlayer.java
@@ -19,24 +19,28 @@ public class RacingCarPlayer implements GamePlayer {
         judgeMachine = new JudgeMachine();
         carsGenerator = new CarsGenerator();
     }
+
     @Override
     public void run() {
         List<Car> carList = generateCars(inputCarNames());
         Integer numberOfRounds = inputNumberOfRounds();
-        race(numberOfRounds,carList);
+        race(numberOfRounds, carList);
         List<Car> winningCars = judgeMachine.getWinningCars(carList);
         printer.printResult(winningCars);
     }
+
     private List<Car> generateCars(List<String> carNames) {
         List<Car> carList;
         carList = carsGenerator.generateCars(carNames);
         return carList;
     }
+
     private Integer inputNumberOfRounds() {
         printer.printInputNumberOfRoundsMessage();
         Integer numberOfRounds = scanner.inputNumberOfRound();
         return numberOfRounds;
     }
+
     private void race(Integer numberOfRounds, List<Car> carList) {
         printer.printRoundStateMessage();
         for (int round = 1; round <= numberOfRounds; round++) {
@@ -44,9 +48,10 @@ public class RacingCarPlayer implements GamePlayer {
             printer.printRoundState(carList);
         }
     }
+
     private void playARound(List<Car> carList) {
         for (Car car : carList) {
-            car.advance();
+            car.move();
         }
     }
 
