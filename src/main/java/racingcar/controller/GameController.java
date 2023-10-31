13c7d202bfe6815eab8356d57eec0ab994a34b73
grax <grax@192.168.0.103>
2023-10-31 18:17:05 +0900

    feat: "add codes in playGame() method"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -4,11 +4,13 @@ import racingcar.view.Input;
 import racingcar.view.Printer;
 
 public class GameController {
-	private Printer printer;
-	private Input input;
+	private Printer printer = new Printer();
+	private Input input = new Input();
 	
 	public void playGame() {
-		printer.displayMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		
+		printer.displayMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+		input.getCarName();
+		printer.displayMessage("시도할 회수는 몇회인가요?\n");
+		input.getTrialNumber();
 	}
 }
