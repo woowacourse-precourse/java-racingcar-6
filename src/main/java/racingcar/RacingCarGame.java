f01608a509139c05e0acb8abf6ba1c0c60aa5c7c
JunRain <sa2313@tukorea.ac.kr>
2023-10-31 21:33:06 +0900

    사용자에게 게임 진행 횟수 입력받기 구현

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,16 +1,33 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingCarGame {
 	void start(){
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String userInput = Console.readLine();
 
 		List<String> users = List.of(userInput.split(","));
 		checkUserName(users);
+
+		System.out.println("시도할 회수는 몇회인가요?");
+		String InputLoop = Console.readLine();
+
+		int loop = checkLoop(InputLoop);
+	}
+
+	private int checkLoop(String  InputLoop) {
+		int loop;
+
+		try {
+			loop = Integer.parseInt(InputLoop);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException();
+		}
+
+		return loop;
 	}
 
 	private void checkUserName(List<String> users) {
