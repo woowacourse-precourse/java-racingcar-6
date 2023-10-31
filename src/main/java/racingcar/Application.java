7ac52d664fadf14feda8976ed7db28f64018bc75
donghyun <kdhdh001@naver.com>
2023-10-31 22:22:16 +0900

    feat: 메인 메서드에서 InputView 클래스 확인

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputView.inputCarNames();
+        int tryCount = InputView.inputTryCount();
+        System.out.println(carNames);
+        System.out.println(tryCount);
     }
 }
