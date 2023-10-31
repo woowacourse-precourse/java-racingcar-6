b64b20145adebe427e8cf7fd86032f49f9de6752
ykmxxi <ykmxxi97@gmail.com>
2023-10-31 14:35:29 +0900

    refactor(CarDtoTest): given_when_then 구조에 맞게 변경

--- src/test/java/racingcar/dto/CarDtoTest.java
+++ src/test/java/racingcar/dto/CarDtoTest.java
@@ -11,9 +11,13 @@ class CarDtoTest {
     @DisplayName("라운드 결과 출력을 위한 레코드")
     void givenNameAndPosition_whenCreateRoundResult_thenReturnCorrectValue() {
         // given
-        CarDto carDto = new CarDto("pobi", 3);
+        String name = "pobi";
+        int position = 3;
 
-        // when & then
+        // when
+        CarDto carDto = new CarDto(name, position);
+
+        // then
         assertThat(carDto.name()).isEqualTo("pobi");
         assertThat(carDto.position()).isEqualTo(3);
     }
