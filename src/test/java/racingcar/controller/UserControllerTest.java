f982415b57619aafcf4056f025030bdd8b8bb73b
icecoff22 <wjdtmd987@gmail.com>
2023-11-01 02:43:52 +0900

    test(user) 컨트롤러 검증 테스트

--- src/test/java/racingcar/controller/UserControllerTest.java
+++ src/test/java/racingcar/controller/UserControllerTest.java
@@ -1,2 +1,31 @@
-package racingcar.controller;public class UserControllerTest {
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import racingcar.manager.controller.UserController;
+
+public class UserControllerTest {
+
+    private final UserController userController = new UserController();
+
+    @AfterEach
+    void closeConsole(){
+        Console.close();
+    }
+
+    @Test
+    void 유저_시도_횟수_반환_검증_테스트(){
+        String tryCount = "5";
+
+        InputStream in = new ByteArrayInputStream(tryCount.getBytes());
+        System.setIn(in);
+
+        Integer expectedCount = 5;
+        assertThat(userController.getUserTryCount()).isEqualTo(expectedCount);
+    }
 }
