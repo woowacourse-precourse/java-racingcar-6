f8f2f51d743f578a14685a361e1eb9c74f429978
eom-tae-in <eti0728@daum.net>
2023-11-01 00:45:34 +0900

    refactor: DTO 매개변수 수정

--- src/main/java/racingcar/controller/dto/GameResultResponse.java
+++ src/main/java/racingcar/controller/dto/GameResultResponse.java
@@ -1,17 +1,22 @@
 package racingcar.controller.dto;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import racingcar.model.Vehicle;
 
 public class GameResultResponse {
 
-    private final Map<String , Integer> result;
+    private final Map<String, Integer> result;
 
     private GameResultResponse(final Map<String, Integer> result) {
         this.result = result;
     }
 
-    public static GameResultResponse from(final Map<String , Integer> map) {
-        return new GameResultResponse(map);
+    public static GameResultResponse from(final List<Vehicle> vehicles) {
+        Map<String, Integer> result = new LinkedHashMap<>();
+        vehicles.forEach(vehicle -> result.put(vehicle.getName(), vehicle.getPosition()));
+        return new GameResultResponse(result);
     }
 
     public Map<String, Integer> getResult() {
