338e3f5f02aff9039c6160dbf0ff738d0ef66f4f
Yoonseol Lee <thisyoon97@gmail.com>
2023-10-31 20:57:21 +0900

    feat: 레이싱 게임 진행 결과를 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.Car;
+
 public class OutputView {
+    public static final String CAR_FORMAT = "%s : %s%n";
+
+    public void printCars(List<Car> participatingCars) {
+        String carsOutput = participatingCars.stream()
+                .map(car -> String.format(CAR_FORMAT, car.getName(), getCarMovement(car.getLocation())))
+                .collect(Collectors.joining());
+
+        System.out.println(carsOutput);
+    }
+
+    private String getCarMovement(int location) {
+        return "-".repeat(Math.max(0, location));
+    }
 }
