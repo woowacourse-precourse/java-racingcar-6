d668b33a26718082b16723e0c923ab5857b969c2
Infikei <hckhong@naver.com>
2023-11-01 02:11:03 +0900

    refactor: 우승자 출력 메시지 상수로 변경
    - RESULT_START_MESSAGE 상수 이름에 PRINT 키워드가 들어가도록 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,11 +3,12 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-    private static final String RESULT_START_MESSAGE = "실행 결과";
+    private static final String RESULT_PRINT_START_MESSAGE = "실행 결과";
+    private static final String WINNER_PRINT_START_MESSAGE = "최종 우승자 : ";
 
     public void printMessageStartResult() {
         System.out.println();
-        System.out.println(RESULT_START_MESSAGE);
+        System.out.println(RESULT_PRINT_START_MESSAGE);
     }
 
     public void printResultsMoving(final List<String> results) {
@@ -18,7 +19,7 @@ public class OutputView {
     }
 
     public void printWinners(final List<String> winners) {
-        System.out.print("최종 우승자 : ");
+        System.out.print(WINNER_PRINT_START_MESSAGE);
         System.out.println(String.join(", ", winners));
     }
 }
