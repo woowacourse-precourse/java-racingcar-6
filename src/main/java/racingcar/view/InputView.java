a080571994a59b27e51152f6691909ab5e3fe46e
sinminseok12 <sinminseok12@naver.com>
2023-10-31 20:37:38 +0900

    refactor(InputView): InputView 메서드 static 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,14 +11,14 @@ public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
-    public List<String> inputCarNames() {
+    public static List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateList(input);
         return parseStrings(input);
     }
 
-    public Integer inputTryCount() {
+    public static Integer inputTryCount() {
         System.out.println(INPUT_TRY_COUNT);
         String input = Console.readLine();
         InputValidator.validatePositiveIntegerType(input);
