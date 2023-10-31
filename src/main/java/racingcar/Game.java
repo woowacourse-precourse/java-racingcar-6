6aa85712f29da43b66d19bf59ade6b75c6157b06
SeungGwa123 <skwan123@naver.com>
2023-10-31 18:40:49 +0900

    Fix: Fix opening
    Input객체의 이름을 ip에서 input으로 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -4,14 +4,18 @@ import java.util.Map;
 
 public class Game {
     public void opening() {
-        Input ip = new Input();
-        String[] car_name = ip.carName();
-        Map<String, Integer> parkinglot = ip.carDistance(car_name);
-        int runtime = ip.runtime();
+        Input input = new Input();
+        String[] car_name = input.carName();
+        Map<String, Integer> parkinglot = input.carDistance(car_name);
+        int runtime = input.runtime();
         System.out.println();
         gameStart(parkinglot, car_name, runtime);
     }
     public void gameStart(Map<String,Integer> parkinglot, String[] car_name, int runtime) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < runtime; i++) {
 
+        }
     }
+
 }
