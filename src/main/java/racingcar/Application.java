781e14b25a4c1a943889cdfb17c7ff25d97b33ad
KongSim4369 <inging5839@naver.com>
2023-10-31 16:56:21 +0900

    feat : 시도 회수 입력 기능 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,9 +6,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     static List<String> inputCarNameList = new ArrayList<>();
+    static String inputTryNum;
 
     public static void main(String[] args) {
         inputCarInformation();
+        inputTryCount();
     }
     // 자동차 이름 입력 받기
     static void inputCarInformation() {
@@ -28,6 +30,24 @@ public class Application {
             throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
         }
     }
+
+    //시도 회수 입력 받기
+    static void inputTryCount() {
+        System.out.printf("시도할 회수는 몇회인가요?%n");
+        inputTryNum = Console.readLine();
+        for (int i = 0; i < inputTryNum.length(); i++) {
+            char checkInputTryNum = inputTryNum.charAt(i);
+        // 예외처리 1 : 입력 값 0으로 시작 불가 (+ 시도값 0 입력 불가)
+            if (inputTryNum.charAt(0) == '0') {
+                throw new IllegalArgumentException("입력 값이 0으로 시작할 수 없습니다.");
+            }
+        // 예외처리 2 : 숫자 형식만 입력 가능
+            if (checkInputTryNum < '0' || checkInputTryNum > '9') {
+                throw new IllegalArgumentException("숫자형식만 입력 가능합니다.");
+            }
+        }
+    }
 }
+
 
 
