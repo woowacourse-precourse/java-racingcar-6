cb7372d587877e534a9d6c93434090d4b778e3dc
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-31 18:08:31 +0900

    TEST: RacingInfo 클래스 테스트 코드 생성 - setNameAndNum: 차량이름과 시도횟수를 입력 받을 때 잘못된 값이 들어올 경우 예외처리 - getNum: 입력된 시도횟수가 옳바르게 반환 되는지 확인 - getNames: 입력된 차량이름이 옳바르게 반환 되는지 확인

--- src/test/java/mytest/RacingInfoTest.java
+++ src/test/java/mytest/RacingInfoTest.java
@@ -2,18 +2,19 @@ package mytest;
 
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 import racingcar.RacingInfo;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class RacingTest extends NsTest {
-
+class RacingInfoTest extends NsTest {
     @Test
-    void 차량이름_시도횟수_예외처리(){
+    void setNamesAndNum_차량이름_시도횟수_예외처리(){
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("jiji,min,minjung"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -22,6 +23,27 @@ class RacingTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("jiji,min,minji", "-5"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void getNum_입력된_시도횟수_반환(){
+        assertSimpleTest(
+                () -> {
+                    run("jiji,minji", "2");
+                    assertThat(RacingInfo.getNum()).isEqualTo(2);
+                }
+        );
+    }
+
+    @Test
+    void getNames_입력된_차량이름_반환(){
+        String[] result = {"jiji", "minji"};
+        assertSimpleTest(
+                () -> {
+                    run("jiji,minji", "2");
+                    assertThat(RacingInfo.getNames()).isEqualTo(result);
+                }
         );
     }
 
