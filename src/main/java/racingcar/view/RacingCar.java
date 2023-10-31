d489356238ed02b171d14dde61e3ce969290f286
aristo <dkfud2121@naver.com>
2023-11-01 01:59:02 +0900

    REFACTOR: 변수값 및 함수 구성 세부 조정

--- src/main/java/racingcar/view/RacingCar.java
+++ src/main/java/racingcar/view/RacingCar.java
@@ -11,23 +11,23 @@ import static racingcar.constant.Limits.MAX_NAME_SIZE;
 public class RacingCar {
 
     public List<String> gameStart() throws IOException {
+        List<String> resultList = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         try {
 //            입력값 받기: 자동차 이름, 플레이 횟수
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             String nameLine = bufferedReader.readLine();
             String[] userCar = nameLine.split(",");
             String tryNumber = bufferedReader.readLine();
 
 //            Error Handler
-            errorName(userCar);
-            errorTryNum(tryNumber);
+            nameHandler(userCar);
+            trialHandler(tryNumber);
 
 //            controller에 넘길 반환값 설정하기
-            List<String> resultList = new ArrayList<>();
             resultList.add(tryNumber);
-
-            for (String car : userCar) {
-                resultList.add(car);
+            for (String carName : userCar) {
+                resultList.add(carName);
             }
 
 //            index-0: 플레이 횟수, index-1,2,...:자동차 이름
@@ -38,22 +38,22 @@ public class RacingCar {
         }
     }
 
-    public void showLoad(List<String> name, List<String> location, int carNum) {
+    public void showLoad(List<String> name, List<String> location, int theNumberOfCar) {
 //        자동차 별 진척 사항 시각화
-        for (int i = 0; i < carNum; i++) {
+        for (int i = 0; i < theNumberOfCar; i++) {
             System.out.println(name.get(i) + " : " + location.get(i));
         }
     }
 
-    public void errorName(String[] car){
-        for(String name:car){
-            if (name.length()>MAX_NAME_SIZE) {
+    public void nameHandler(String[] car) {
+        for (String name : car) {
+            if (name.length() > MAX_NAME_SIZE) {
                 throw new IllegalArgumentException("사용 가능한 최대 글자수는 5 입니다.");
             }
         }
     }
 
-    public void errorTryNum(String numString){
+    public void trialHandler(String numString) {
         if (numString.equals("")) {
             throw new IllegalArgumentException("플레이 횟수를 기입하지 않았습니다.");
         }
