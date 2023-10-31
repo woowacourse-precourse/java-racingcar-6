d8ba3eeaf9b0d3f042f403415e59eb6fc194a3d3
jacomyou1026 <jacomyou1026@naver.com>
2023-11-01 01:38:28 +0900

    feat : 시도 회수 구하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,10 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" );
         String[] carNames = getCarNames(sc.nextLine());
+        int[] carPositions = new int[carNames.length];
+        int numAttempts = getNumAttempts(sc.nextLine());
+
+
     }
     public static String[] getCarNames(String names) {
         String[] nameSplit = names.split(",");
@@ -18,4 +22,15 @@ public class Application {
         }
         return nameSplit;
     }
+
+    public static int getNumAttempts(String numAttempt ) {
+        System.out.println("시도할 회수는 몇회인가요?");
+        if (!numAttempt.matches("[0-9]+")) {
+            throw new IllegalStateException("숫자로 입력해주세요");
+        }
+        int numAttempts = Integer.parseInt(numAttempt);
+        System.out.println(numAttempts);
+        return numAttempts;
+    }
+
 }
