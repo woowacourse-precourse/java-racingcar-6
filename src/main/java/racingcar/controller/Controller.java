90f7b42401dac774373b32f860d66e1b35a39220
bohyeon <980926a@gmail.com>
2023-10-31 17:58:52 +0900

    feat: Game 관련 Controller

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -18,11 +18,50 @@ public class Controller {
   private final validator validator = new validator();
   private final CarDao carRepository = new CarDao();
 
-  public void run() {
+  public void run() throws IllegalArgumentException{
     // 입력을 받고, 입력을 저장한다.
     saveCars(parser.parseCarNames(getCarNamesByUserInput()));
+    // 게임 생성
+    play(createGame());
+  }
+  // 게임 생성
+  private Game createGame() {
+    Game game = new Game(getCarList(), getNumberOfTrial()); // 게임 객체 생성
+    gameService.save(game);
+    return game;
   }
 
+  private void play(Game game) throws IllegalArgumentException{
+    outputView.printHead();
+    while (!gameService.isEnd(game)) {
+      gameService.play(game);
+      outputView.printScore(game.getCars());
+    }
+    outputView.printWinners(gameService.getWinners(game));
+  }
+
+  private List<Car> getCarList() {
+    return carRepository.findAll();
+  }
+
+  private int getNumberOfTrial() {
+    return parser.parseNumberOfTrial(getTrialByUserInput());
+  }
+
+  private String getTrialByUserInput() {
+    inputView.requestNumberOfTrialMessage(); // print
+    return checkValidationNumberOfTrialInput(Console.readLine()); // 입력
+  }
+
+  private String checkValidationNumberOfTrialInput(String input) {
+    try {
+      validator.checkNumberOfTrialInput(input);
+      return input;
+    } catch (IllegalArgumentException e) {
+      outputView.printException(e.getMessage());
+      return getTrialByUserInput();
+    }
+  }
   // 입력을 위한 함수
   private void saveCars(List<String> carNames) {
     for (String carName : carNames) {
@@ -30,20 +69,20 @@ public class Controller {
     }
   }
 
-  private String getCarNamesByUserInput() {
+  private String getCarNamesByUserInput() throws IllegalArgumentException{
     // 사용자의 입력으로 자동차의 이름을 입력받는다.
     inputView.requestCarNamesMessage(); // view
     String input = Console.readLine(); // 입력
     return checkValidationCarNamesInput(input); // vaildation check
   }
 
-  private String checkValidationCarNamesInput(String input) {
+  private String checkValidationCarNamesInput(String input) throws IllegalArgumentException{
     try {
       validator.checkCarNamesInput(input);
       return input;
     } catch (IllegalArgumentException e) {
       outputView.printException(e.getMessage()); // view
-      return getCarNamesByUserInput(); //?
+      throw e;
     }
   }
 }
