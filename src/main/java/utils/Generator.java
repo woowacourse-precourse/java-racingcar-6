87e82c3758f32dbe9e129f3a46271271a221a333
kimwanyoung <dhks2869@gmail.com>
2023-10-31 13:47:32 +0900

    refactor : 요구사항에 맞춰 최소값 0으로 수정

--- src/main/java/utils/Generator.java
+++ src/main/java/utils/Generator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Generator {
 
-    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MIN_RANDOM_NUMBER = 0;
     private static final int MAX_RANDOM_NUMBER = 9;
 
     private Generator() {
