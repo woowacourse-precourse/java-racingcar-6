190e9c75cc6cb6e6f2d6015e61eaf2f70aa2aa98
jiwon1217 <koagjw@gmail.com>
2023-10-31 11:57:50 +0900

    :sparkles: feat: 구현한 기능 조립

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -8,13 +8,23 @@ import java.util.List;
 import java.util.Set;
 
 public class RacingGame {
-
-    private void inputCarsInfo() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        getCarNames();
+    public void startGame() {
+        List<Car> cars = inputCarsInfo();
+        int round = inputRound();
+        System.out.println("실행 결과");
+        for (int count = 1; count <= round; count++) {
+            proceedRound(cars);
+            System.out.println();
+        }
+        printWinner(cars);
     }
 
-    private void getCarNames() {
+    private List<Car> inputCarsInfo() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return getCarNames();
+    }
+
+    private List<Car> getCarNames() {
         String input = Console.readLine();
         String[] carNames = input.split(",");
 
@@ -22,6 +32,9 @@ public class RacingGame {
             validateCarName(carName);
         }
         validateDuplicate(carNames);
+
+        List<Car> cars = convertStringArrayToCarList(carNames);
+        return cars;
     }
 
     private void validateCarName(String carName) {
@@ -53,19 +66,28 @@ public class RacingGame {
         }
     }
 
-    private void inputRound() {
+    private List<Car> convertStringArrayToCarList(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName, 0));
+        }
+        return cars;
+    }
+
+    private int inputRound() {
         System.out.println("시도할 회수는 몇회인가요?");
-        getRound();
+        return getRound();
     }
 
-    private void getRound() {
+    private int getRound() {
         String input = Console.readLine();
-        validateRound(input);
+        return validateRound(input);
     }
 
-    private void validateRound(String input) {
+    private int validateRound(String input) {
         int round = validateRoundType(input);
         isValidRoundRange(round);
+        return round;
     }
 
     private int validateRoundType(String input) {
@@ -87,6 +109,13 @@ public class RacingGame {
         result.append(String.join(",", getWinnerNames(cars)));
         result.append("가 최종 우승했습니다.");
         System.out.println(result);
+    }
+
+    private void proceedRound(List<Car> cars) {
+        for (Car car : cars) {
+            car.move();
+            System.out.println(car.makeMoveResult());
+        }
     }
 
     private List<String> getWinnerNames(List<Car> cars) {
