07cb035b7d842dc8af0dbf52c170faf33b33064a
hyocheol An <anhyocheol@naver.com>
2023-11-01 06:10:48 +0900

    test : GameController 테스트

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,13 +1,27 @@
 package racingcar.controller;
 
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.util.InputValidation;
+
 public class GameController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    InputValidation inputValidation = new InputValidation();
     public void startGame() {
         // 자동차 이름 입력 요청
-        // 입력값 검증
+        String inputCarName = inputView.CarName();
+        inputValidation.isVaildCarName(inputCarName);
         // 시도할 회수 입력 요청
+        String inputGameRound = inputView.GameRound();
         // 입력값 검증
+        inputValidation.isVaildGameRound(inputGameRound);
+
+        System.out.println("게임종료");
         // playGame();
     }
+
     public void playGame() {
         // 실행 결과 메세지 출력
         // 회차별 게임 진행
