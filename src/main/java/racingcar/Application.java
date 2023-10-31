7e63437658f42c9151f52a23bedd6b0a6d2b0e49
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-10-31 22:40:35 +0900

    refactor (racingcar - Application): main() 안에서의 과정들 각각 개별 함수화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,12 +6,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> carNamesArrayList = UserInput.getCarNames();
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        int trialNumber = UserInput.getTrialNumber();
-        System.out.println();
+        List<String> carNamesArrayList = enterCarNames();
+
+
+        int trialNumber = enterTrialNumber();
+
 
         System.out.println("실행 결과");
         Map<String, String> raceResultsHMap = new LinkedHashMap<>();
@@ -22,10 +22,29 @@ public class Application {
             Race.showRace(raceResultsHMap);
         }
 
-//        List<String> winnersArrayList;
+
         List<String> winnersArrayList = Winner.pickWinners(raceResultsHMap);
         Winner.showWinners(winnersArrayList);
 
-
     }
+
+    private static List<String> enterCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        List<String> carNamesArrayList = UserInput.getCarNames();
+
+        return carNamesArrayList;
+    }
+
+
+    private static int enterTrialNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        int trialNumber = UserInput.getTrialNumber();
+
+        System.out.println();
+
+        return trialNumber;
+    }
+
 }
