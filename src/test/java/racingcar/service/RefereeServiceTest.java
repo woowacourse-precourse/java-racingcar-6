0403c3d4fc145477c41e58a20e6139a3240915c9
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-01 05:18:30 +0900

    test: 테스트 수정

--- src/test/java/racingcar/service/RefereeServiceTest.java
+++ src/test/java/racingcar/service/RefereeServiceTest.java
@@ -9,7 +9,9 @@ import racingcar.domain.car.Car;
 import racingcar.domain.car.CarName;
 import racingcar.domain.position.Position;
 import racingcar.domain.power.DefaultPowerGenerator;
+import racingcar.dto.CarStatusDto;
 import racingcar.dto.GameResultDto;
+import racingcar.dto.RoundResultDto;
 
 @DisplayName("심판 테스트")
 class RefereeServiceTest {
@@ -17,24 +19,45 @@ class RefereeServiceTest {
     @DisplayName("단일 우승자를 올바르게 가려낸다")
     @Test
     void testSelectWinnersWithSingleWinner() {
+
         Car car1 = new Car(new CarName("car1"), new Position(3), new DefaultPowerGenerator());
         Car car2 = new Car(new CarName("car2"), new Position(5), new DefaultPowerGenerator());
         Car car3 = new Car(new CarName("car3"), new Position(2), new DefaultPowerGenerator());
+
         List<Car> cars = Arrays.asList(car1, car2, car3);
+        RoundResultDto round1 = RoundResultDto.createFrom(cars);
+        List<RoundResultDto> roundResultDtos = Arrays.asList(round1);
+
+        CarStatusDto dto1 = CarStatusDto.createFrom(car1);
+        CarStatusDto dto2 = CarStatusDto.createFrom(car2);
+        CarStatusDto dto3 = CarStatusDto.createFrom(car3);
+        List<CarStatusDto> carDtos = Arrays.asList(dto1, dto2, dto3);
+
         RefereeService refereeService = new RefereeService();
-        GameResultDto result = refereeService.publishGameResult(cars);
-        Assertions.assertThat(result.getWinners()).containsExactly(car2);
+        GameResultDto result = refereeService.publishGameResult(roundResultDtos, carDtos);
+
+        Assertions.assertThat(result.getWinners().size()).isEqualTo(1);
     }
 
     @DisplayName("복수 우승자를 올바르게 가려낸다")
     @Test
     void testSelectWinnersWithMultipleWinner() {
         Car car1 = new Car(new CarName("car1"), new Position(3), new DefaultPowerGenerator());
-        Car car2 = new Car(new CarName("car2"), new Position(5), new DefaultPowerGenerator());
-        Car car3 = new Car(new CarName("car3"), new Position(5), new DefaultPowerGenerator());
+        Car car2 = new Car(new CarName("car2"), new Position(3), new DefaultPowerGenerator());
+        Car car3 = new Car(new CarName("car3"), new Position(2), new DefaultPowerGenerator());
+
         List<Car> cars = Arrays.asList(car1, car2, car3);
+        RoundResultDto round1 = RoundResultDto.createFrom(cars);
+        List<RoundResultDto> roundResultDtos = Arrays.asList(round1);
+
+        CarStatusDto dto1 = CarStatusDto.createFrom(car1);
+        CarStatusDto dto2 = CarStatusDto.createFrom(car2);
+        CarStatusDto dto3 = CarStatusDto.createFrom(car3);
+        List<CarStatusDto> carDtos = Arrays.asList(dto1, dto2, dto3);
+
         RefereeService refereeService = new RefereeService();
-        GameResultDto result = refereeService.publishGameResult(cars);
-        Assertions.assertThat(result.getWinners()).containsExactly(car2, car3);
+        GameResultDto result = refereeService.publishGameResult(roundResultDtos, carDtos);
+
+        Assertions.assertThat(result.getWinners().size()).isEqualTo(2);
     }
 }
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +14,8 @@ import racingcar.domain.car.Car;
 import racingcar.domain.car.CarName;
 import racingcar.domain.position.Position;
 import racingcar.domain.power.DefaultPowerGenerator;
+import racingcar.dto.CarStatusDto;
 import racingcar.dto.RoundResultDto;
-import racingcar.service.RefereeService;
 
 @DisplayName("뷰 로직 테스트")
 class OutputViewTest {
@@ -47,30 +48,29 @@ class OutputViewTest {
     @DisplayName("복수 우승자일 경우 올바르게 출력한다")
     @Test
     void testShowGameResult() {
-        List<Car> cars = Arrays.asList(
-                new Car(new CarName("car1"), new Position(3), new DefaultPowerGenerator()),
-                new Car(new CarName("car2"), new Position(5), new DefaultPowerGenerator()),
-                new Car(new CarName("car3"), new Position(5), new DefaultPowerGenerator())
-        );
 
-        String expectedOutput = "최종 우승자 : car2,car3";
-        RefereeService refereeService = new RefereeService();
-        outputView.showGameResult(refereeService.publishGameResult(cars));
-        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
+        List<CarStatusDto> winners = new ArrayList<>();
+        Car car1 = new Car(new CarName("car1"), new Position(5), new DefaultPowerGenerator());
+        Car car2 = new Car(new CarName("car2"), new Position(5), new DefaultPowerGenerator());
+        winners.add(CarStatusDto.createFrom(car1));
+        winners.add(CarStatusDto.createFrom(car2));
+
+        MessageResolver messageResolver = new MessageResolver();
+        String generated = messageResolver.resolveWinnerMessage(winners);
+        String expectedOutput = "최종 우승자 : car1,car2";
+        assertEquals(expectedOutput.trim(), generated.trim());
     }
 
     @DisplayName("단일 우승자일 경우 올바르게 출력한다")
     @Test
     void testShowGameResultWithSingleWinner() {
-        List<Car> cars = Arrays.asList(
-                new Car(new CarName("car1"), new Position(3), new DefaultPowerGenerator()),
-                new Car(new CarName("car2"), new Position(5), new DefaultPowerGenerator()),
-                new Car(new CarName("car3"), new Position(2), new DefaultPowerGenerator())
-        );
+        List<CarStatusDto> winners = new ArrayList<>();
+        Car car1 = new Car(new CarName("car1"), new Position(5), new DefaultPowerGenerator());
+        winners.add(CarStatusDto.createFrom(car1));
 
-        String expectedOutput = "최종 우승자 : car2";
-        RefereeService refereeService = new RefereeService();
-        outputView.showGameResult(refereeService.publishGameResult(cars));
-        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
+        MessageResolver messageResolver = new MessageResolver();
+        String generated = messageResolver.resolveWinnerMessage(winners);
+        String expectedOutput = "최종 우승자 : car1";
+        assertEquals(expectedOutput.trim(), generated.trim());
     }
 }
