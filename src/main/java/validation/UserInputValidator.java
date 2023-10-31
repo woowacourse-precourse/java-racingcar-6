ca4d18243bbe4875d3b993d2eed974c26a556d75
yunseung <yseungun.21@gmail.com>
2023-10-31 20:14:18 +0900

    refactor(UserInputValidator): 메서드명 'validate~'로 변경

--- src/main/java/validation/UserInputValidator.java
+++ src/main/java/validation/UserInputValidator.java
@@ -4,13 +4,13 @@ import static constant.Constant.NAME_LENGTH_LIMIT;
 import static constant.ErrorMessage.EMPTY_NAME_CASE_MESSAGE;
 
 public class UserInputValidator {
-    public static void checkNameLength(String name) {
+    public static void validateNameLength(String name) {
         if (name.length() > NAME_LENGTH_LIMIT) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void checkEmptyInput(String part) {
+    public static void validateEmptyInput(String part) {
         if (part.isBlank()) {
             throw new IllegalArgumentException(EMPTY_NAME_CASE_MESSAGE);
         }
