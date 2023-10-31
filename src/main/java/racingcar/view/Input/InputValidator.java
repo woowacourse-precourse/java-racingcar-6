17853f059304625b0a41f9e60492fd04d2b6715e
yang <hjyang@epience.com>
2023-10-31 17:33:46 +0900

    🎯feat(view/InputValidator) : 자동차 숫자 입력 예외 처리 구현

--- src/main/java/racingcar/view/Input/InputValidator.java
+++ src/main/java/racingcar/view/Input/InputValidator.java
@@ -1,11 +1,24 @@
 package racingcar.view.Input;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class InputValidator {
-    private final String REGULAR_EXPRESSION = "^[0-9]+$";
+    private final String NUMBER_REGX = "^[0-9]+$";
+    private final String CAR_REGEX = "^[a-zA-Z가-힣]+$";
 
         // [x] 쉼표가 연속해서 있을 경우 >> 정규 표현식 split(",+")
         // [x] 쉼표 사이에 공백이 있을 경우 >> replace(" ", "");
         // [x] 공백일 경우 >> exception
+    public void validateIsString(List<String> inputCars) {
+        boolean isValid = inputCars.stream().noneMatch(input -> input.matches(CAR_REGEX));
+
+        if (isValid) {
+            throw new IllegalArgumentException("문자만 입력 가능합니다.");
+        }
+
+    }
+
     public void validateCarString(String input) {
         if (input.trim() == null) {
             throw new IllegalArgumentException("");
@@ -13,7 +26,7 @@ public class InputValidator {
     }
 
     public void isNumberOnly(String number) {
-        if (!number.matches(REGULAR_EXPRESSION)) {
+        if (!number.matches(NUMBER_REGX)) {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
     }
