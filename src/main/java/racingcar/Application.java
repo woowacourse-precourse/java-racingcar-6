fc916b7794c4ad4f5993eda377be4e8259803f0f
jun02160 <jun020216@naver.com>
2023-11-01 01:35:24 +0900

    feat : main 함수에서 게임 시작 메서드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.app.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame game = new RacingGame();
+        game.start();
     }
 }
