bd087fa8b5924342c31059f6800261d5f2215531
sheepseung <qkffldlfdjsk12@naver.com>
2023-10-31 13:27:19 +0900

    feat(UserController): 횟수(코인) 입력 받기 기능 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -7,21 +7,31 @@ import java.util.ArrayList;
 
 public class UserController {
     private ArrayList<Car> racingCars;
+    private int coin = 0;
 
     public UserController() {
         this.racingCars = new ArrayList<Car>();
     }
+
     public ArrayList<Car> getRacingCars() {
         return racingCars;
     }
 
-    public void intputUserValue() {
-        String userInput = Console.readLine();
-
-        registerCar(userInput);
+    public int getCoin() {
+        return coin;
     }
 
-    public void registerCar(String userInput){
+    public void intputUserValue() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String userInput = Console.readLine();
+        registerCar(userInput);
+
+        System.out.println("시도할 회수는 몇회인가요?\n");
+        String coinStr = Console.readLine();
+        registerCoin(coinStr);
+    }
+
+    public void registerCar(String userInput) {
         int startNameIndex = 0;
         int size = userInput.length();
 
@@ -38,12 +48,16 @@ public class UserController {
         }
     }
 
-    public void inputCarToList(String carName){
+    public void inputCarToList(String carName) {
         validateNameLength(carName);
         racingCars.add(new Car(carName));
     }
 
     public void validateNameLength(String carName) {
         if (carName.length() > 5) throw new IllegalArgumentException("이름은 5글자 이하로 입력해 주십시오.");
+    }
+
+    public void registerCoin(String coinStr) {
+        this.coin = Integer.parseInt(coinStr);
     }
 }
