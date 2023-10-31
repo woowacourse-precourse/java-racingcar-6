02d94326043b0906cbde15e25877f6195e213d88
SeongIl <biuld1234@gmail.com>
2023-10-31 14:11:47 +0900

    refactor: use Error Enum 열거형

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,6 +3,12 @@ package racingcar;
 import static racingcar.BoundaryValue.MAX_NAME_LENGTH;
 import static racingcar.BoundaryValue.MIN_CAR_COUNT;
 import static racingcar.BoundaryValue.MIN_ROUND;
+import static racingcar.Error.BLANK_ERROR;
+import static racingcar.Error.CAR_COUNT_ERROR;
+import static racingcar.Error.DUPLICATION_ERROR;
+import static racingcar.Error.LENGTH_ERROR;
+import static racingcar.Error.NOT_NUMBER_ERROR;
+import static racingcar.Error.ROUND_COUNT_ERROR;
 
 import java.util.HashSet;
 import java.util.List;
@@ -12,7 +18,7 @@ public class Validator {
     public static void checkCount(List<String> carNames) {
 
         if (carNames.size() < MIN_CAR_COUNT.get()) {
-            throw new IllegalArgumentException("2대 이상의 자동차 이름을 입력해 주세요.");
+            throw new IllegalArgumentException(CAR_COUNT_ERROR.getMessage());
         }
 
     }
@@ -22,7 +28,7 @@ public class Validator {
         for (String carName : carNames) {
 
             if (carName.isBlank()) {
-                throw new IllegalArgumentException("이름을 공백으로 입력하지 마세요.");
+                throw new IllegalArgumentException(BLANK_ERROR.getMessage());
             }
 
         }
@@ -34,7 +40,7 @@ public class Validator {
         for (String carName : carNames) {
 
             if (carName.length() > MAX_NAME_LENGTH.get()) {
-                throw new IllegalArgumentException("자동차 이름을 5자 이하로 입력해 주세요.");
+                throw new IllegalArgumentException( LENGTH_ERROR.getMessage());
             }
 
         }
@@ -46,7 +52,7 @@ public class Validator {
         CarManager.trimNames(carNames);
 
         if (carNames.size() > new HashSet<>(carNames).size()) {
-            throw new IllegalArgumentException("중복되지 않은 이름을 입력해 주세요.");
+            throw new IllegalArgumentException(DUPLICATION_ERROR.getMessage());
         }
 
     }
@@ -56,11 +62,11 @@ public class Validator {
         try {
 
             if (Integer.parseInt(userInput) < MIN_ROUND.get()) {
-                throw new IllegalArgumentException("1이상의 숫자를 입력하세요.");
+                throw new IllegalArgumentException(ROUND_COUNT_ERROR.getMessage());
             }
 
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력하세요.");
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR.getMessage());
         }
 
     }
