30b3f94d9f9d1fc347bd6b074fcfb56342982761
jongmyeongbak <withpleasure@gmail.com>
2023-11-01 02:16:42 +0900

    feat(ResultView): add printWinners method

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,10 +1,12 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class ResultView {
     private static final String POSITION_INDICATOR = "-";
+    private static final String MESSAGE_WINNER = "최종 우승자 : ";
 
     public void printRoundResult(final List<Car> cars) {
         cars.forEach(this::printCarPosition);
@@ -17,5 +19,15 @@ public class ResultView {
 
     private String getPositionLine(final int position) {
         return POSITION_INDICATOR.repeat(position);
+    }
+
+    public void printWinners(final List<Car> winners) {
+        System.out.print(MESSAGE_WINNER + joinWinnerNames(winners));
+    }
+
+    private String joinWinnerNames(final List<Car> winners) {
+        return winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
     }
 }
