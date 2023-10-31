a057ea6646a6840057b32af6eaf9859cc6a68f3a
eunjin3481 <eunjin3481@gmail.com>
2023-10-31 23:56:07 +0900

    test(Race): Race 테스트코드 리팩토링

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,12 +1,14 @@
 package racingcar;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,16 +16,48 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class RaceTest {
+
+    private Race race;
+    private static ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        race = new Race();
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+        Car car3 = new Car("Car3");
+        race.cars.add(car1);
+        race.cars.add(car2);
+        race.cars.add(car3);
+    }
+
+    @AfterEach
+    void restoresStreams() {
+        System.setOut(System.out);
+    }
+
+    private void setUpStreams() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    private void setupTestData() {
+        Car car1 = race.cars.get(0);
+        Car car2 = race.cars.get(1);
+        Car car3 = race.cars.get(2);
+
+        car1.setPosition(3);
+        car2.setPosition(1);
+        car3.setPosition(3);
+    }
+
     @Test
     void createCarObjectsTest() {
-        // Given
-        Race race = new Race();
+        race.cars.clear();
+
         String[] carNames = {"Car1", "Car2", "Car3"};
-
-        // When
         race.createCarObjects(carNames);
-
-        // Then
         List<Car> cars = race.cars;
         assertEquals(carNames.length, cars.size());
         for (int i = 0; i < carNames.length; i++) {
@@ -33,48 +67,25 @@ public class RaceTest {
 
     @Test
     void updateCarsPositionTest() {
-        // Given
-        Race race = new Race();
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        Car car3 = new Car("Car3");
-        race.cars.add(car1);
-        race.cars.add(car2);
-        race.cars.add(car3);
-
         NumberGenerator numberGenerator = mock(NumberGenerator.class);
         when(numberGenerator.createRandomNumber()).thenReturn(1, 4, 5);
         race.numberGenerator = numberGenerator;
 
-        // When
         race.updateCarsPosition();
 
-        // Then
-        assertEquals(0, car1.getPosition());
-        assertEquals(1, car2.getPosition());
-        assertEquals(1, car3.getPosition());
+        List<Car> cars = race.cars;
+        assertEquals(0, cars.get(0).getPosition());
+        assertEquals(1, cars.get(1).getPosition());
+        assertEquals(1, cars.get(2).getPosition());
     }
 
     @Test
     void displayCarMoving() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        setUpStreams();
 
-        Race race = new Race();
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        Car car3 = new Car("Car3");
-        race.cars.add(car1);
-        race.cars.add(car2);
-        race.cars.add(car3);
-
-        car1.setPosition(3);
-        car2.setPosition(1);
-        car3.setPosition(2);
+        setupTestData();
 
         race.displayCarMoving();
-
-        System.setOut(System.out);
 
         assertThat(outputStream.toString()).contains("Car1 : ---");
         assertThat(outputStream.toString()).contains("Car2 : -");
@@ -83,63 +94,30 @@ public class RaceTest {
 
     @Test
     void findMaxPositionTest() {
-        Race race = new Race();
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        Car car3 = new Car("Car3");
-        race.cars.add(car1);
-        race.cars.add(car2);
-        race.cars.add(car3);
-
-        car1.setPosition(3);
-        car2.setPosition(4);
-        car3.setPosition(2);
+        setupTestData();
 
         int maxPosition = race.findMaxPosition();
-        assertEquals(4, maxPosition);
+        assertEquals(3, maxPosition);
     }
 
     @Test
     void findWinnersTest() {
-        Race race = new Race();
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        Car car3 = new Car("Car3");
-        race.cars.add(car1);
-        race.cars.add(car2);
-        race.cars.add(car3);
-
-        car1.setPosition(3);
-        car2.setPosition(1);
-        car3.setPosition(3);
+        setupTestData();
 
         List<String> winners = race.findWinners();
         List<String> expectedWinners = Arrays.asList("Car1", "Car3");
-        assertEquals(expectedWinners, winners);
+        assertThat(winners).isEqualTo(expectedWinners);
     }
 
     @Test
     void displayWinner() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        setUpStreams();
 
-        Race race = new Race();
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        Car car3 = new Car("Car3");
-        race.cars.add(car1);
-        race.cars.add(car2);
-        race.cars.add(car3);
-
-        car1.setPosition(3);
-        car2.setPosition(3);
-        car3.setPosition(2);
+        setupTestData();
 
         race.displayWinner();
 
-        System.setOut(System.out);
-
-        String expectedOutput = "최종 우승자 : Car1, Car2";
+        String expectedOutput = "최종 우승자 : Car1, Car3";
         assertThat(outputStream.toString().trim()).isEqualTo(expectedOutput);
     }
 }
