ccd181426b526b01263f30dbf3e20b97d787b752
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 10:21:26 +0900

    Test: CarTest생성 moveByRandomNumber테스트 코드 추가
    생성자를 통해 항상 일정한 숫자 발생시키는 NumberGenerator구현 객체 주입
    4이상일 경우 position 필드 1 증가
    4미만일 경우 position 필드 증가하지 않음

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    @Test
+    @DisplayName("3를 발생시키는 NumberGenerator 주입")
+    void 랜덤값_4미만이면_움직이지_않음() {
+        Car car = new Car("abc", () -> 3);
+        car.moveByRandomNumber();
+        Assertions.assertEquals(0, car.getPosition());
+    }
+
+    @Test
+    @DisplayName("4를 발생시키는 NumberGenerator 주입")
+    void 랜덤값_4이상이면_움직임() {
+        Car car = new Car("abc", () -> 4);
+        car.moveByRandomNumber();
+        Assertions.assertEquals(1, car.getPosition());
+    }
+}
