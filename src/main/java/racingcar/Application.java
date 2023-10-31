2f72baade10be6936aedf3c8f14a22a2f45ff43d
KyungsooLee <jmtkd9196@gmail.com>
2023-11-01 04:15:57 +0900

    feat: 입력되는 자동차의 이름에 대한 예외처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -42,10 +42,19 @@ public class Application {
         return maxDistanceIndics;
     }
 
+    public static void checkNameValidation(String car) {
+        if (car.length() > 5) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 프로그램을 종료합니다.");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> cars = Arrays.asList(camp.nextstep.edu.missionutils.Console.readLine().split(","));
+        for (int i = 0; i < cars.size(); i++) {
+            checkNameValidation(cars.get(i));
+        }
         List<Integer> carsMovedDistance = new ArrayList<>(Collections.nCopies(cars.size(), 0));
         System.out.println("시도할 회수는 몇회인가요?");
         int testNumber = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
