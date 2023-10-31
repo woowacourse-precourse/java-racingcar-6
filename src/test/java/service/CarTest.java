73a5311d6e21025c6c9d5140d94e6b42d5372277
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 18:07:39 +0900

    refactor (Cartest): 테스트 변경
    go_랜덤값이_기준값_이상이면_거리_증가:
    - 2가지 경계값(4,9)에 대해 테스트를 시행하도록 변경
    
    go_랜덤값이_기준값_미만이면_거리_변경_없음:
    - 2가지 경계값(0,3)에 대해 테스트를 시행하도록 변경

--- src/test/java/service/CarTest.java
+++ src/test/java/service/CarTest.java
@@ -2,27 +2,30 @@ package service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
-    @Test
-    void go_랜덤값이_기준값_이상이면_거리_증가() {
+    @ParameterizedTest
+    @ValueSource(ints = {4,9})
+    void go_랜덤값이_기준값_이상이면_거리_증가(int randomNumber) {
         Car car = new Car("test");
         int initialDistance = car.getDistance();
 
-        car.go(4);
+        car.go(randomNumber);
         int updatedDistance = car.getDistance();
 
         assertEquals(updatedDistance, initialDistance + 1);
     }
 
-    @Test
-    void go_랜덤값이_기준값_미만이면_거리_변경_없음() {
+    @ParameterizedTest
+    @ValueSource(ints = {0,3})
+    void go_랜덤값이_기준값_미만이면_거리_변경_없음(int randomNumber) {
         Car car = new Car("test");
         int initialDistance = car.getDistance();
 
-        car.go(3);
+        car.go(randomNumber);
         int updatedDistance = car.getDistance();
 
         assertEquals(updatedDistance, initialDistance);
