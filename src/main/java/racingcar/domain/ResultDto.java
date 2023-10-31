638eae33e62e9d96a3bcf85a8c3bc8c0513cea04
kyeoungchan <wkc1002@naver.com>
2023-10-31 18:43:52 +0900

    feat(racingcar): ResultDto 기능 구현

--- src/main/java/racingcar/domain/ResultDto.java
+++ src/main/java/racingcar/domain/ResultDto.java
@@ -1,2 +1,26 @@
-package racingcar.domain;public class ResultDto {
+package racingcar.domain;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultDto {
+    private final Map<BigInteger, SingleRoundResultDto> resultDtoByRound = new HashMap<>();
+    private final BigInteger finalRound;
+
+    public ResultDto(BigInteger finalRound) {
+        this.finalRound = finalRound;
+    }
+
+    public void addSingleRoundResult(BigInteger round, SingleRoundResultDto singleRoundResultDto) {
+        resultDtoByRound.put(round, singleRoundResultDto);
+    }
+
+    public BigInteger getFinalRound() {
+        return finalRound;
+    }
+
+    public SingleRoundResultDto getSingleRoundResultDto(BigInteger round) {
+        return resultDtoByRound.get(round);
+    }
 }
--- src/test/java/racingcar/domain/ResultDtoTest.java
+++ src/test/java/racingcar/domain/ResultDtoTest.java
@@ -1,4 +1,94 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class ResultDtoTest {
-  
+
+    public static final long TOTAL_ROUND = 3L;
+    public static final int FIRST_ROUND = 1;
+    public static final int NUMBER_OF_CARS = 3;
+    private static BigInteger idProvider = BigInteger.ZERO;
+    private ResultDto resultDto;
+
+    @BeforeEach
+    @DisplayName("각 테스트 실행 전 데이터 저장")
+    void addData() {
+        // given
+        resultDto = new ResultDto(BigInteger.valueOf(TOTAL_ROUND));
+        Car pobiCar = Car.createCar(generateId(), "pobi");
+        Car woniCar = Car.createCar(generateId(), "woni");
+        Car junCar = Car.createCar(generateId(), "jun");
+
+        // when
+        for (int i = FIRST_ROUND; i < TOTAL_ROUND + 1; i++) {
+            List<Car.CarResultDto> carResultDtos = new ArrayList<>(NUMBER_OF_CARS);
+            carResultDtos.add(pobiCar.createCarResultDto());
+            carResultDtos.add(woniCar.createCarResultDto());
+            carResultDtos.add(junCar.createCarResultDto());
+            SingleRoundResultDto singleRoundResult = new SingleRoundResultDto(carResultDtos);
+            resultDto.addSingleRoundResult(BigInteger.valueOf(i), singleRoundResult);
+        }
+    }
+
+    private static BigInteger generateId() {
+        return idProvider = idProvider.add(BigInteger.ONE);
+    }
+
+    @Test
+    @DisplayName("저장한 순서대로 자동차들이 SingleRoundResultDto에 저장되어 있다.")
+    void getCarResultFromSingleResultDto() {
+        // then
+        SingleRoundResultDto findSingleRoundDto = resultDto
+                .getSingleRoundResultDto(BigInteger.ONE);
+        assertThat(findSingleRoundDto)
+                .isExactlyInstanceOf(SingleRoundResultDto.class);
+
+        Car.CarResultDto carResult = findSingleRoundDto.getCarResultDto(0);
+        assertThat(carResult.getName()).isEqualTo("pobi");
+
+        Car.CarResultDto carResult2 = findSingleRoundDto.getCarResultDto(1);
+        assertThat(carResult2.getName()).isEqualTo("woni");
+
+        Car.CarResultDto carResult3 = findSingleRoundDto.getCarResultDto(2);
+        assertThat(carResult3.getName()).isEqualTo("jun");
+    }
+
+    @Test
+    @DisplayName("저장한 순서대로 SingleRoundResultDto들이 저장되어 있다.")
+    void getSingleRoundResultDto() {
+        // then
+        assertThat(resultDto.getSingleRoundResultDto(BigInteger.valueOf(1)))
+                .isNotNull();
+        assertThat(resultDto.getSingleRoundResultDto(BigInteger.valueOf(2)))
+                .isNotNull();
+        assertThat(resultDto.getSingleRoundResultDto(BigInteger.valueOf(3)))
+                .isNotNull();
+    }
+
+    @Test
+    @DisplayName("저장되지 않은 라운드의 SingleRoundResultDto를 조회하면 null이 반환된다.")
+    void ifTryGetNullResult() {
+        assertThat(resultDto.getSingleRoundResultDto(BigInteger.valueOf(4)))
+                .isNull();
+        assertThat(resultDto.getSingleRoundResultDto(BigInteger.valueOf(100)))
+                .isNull();
+
+    }
+
+    @Test
+    @DisplayName("자동차 경주 결과가 아무리 저장되든 최종 라운드에 대한 데이터는 그대로 저장돼있다.")
+    void getFinalRound() {
+        assertThat(resultDto.getFinalRound())
+                .isEqualTo(TOTAL_ROUND);
+    }
 }
