e109fec017a463a026b295c866f1f84ec3c817ab
Jaeho Kim <fldzm7905@gmail.com>
2023-10-31 23:36:40 +0900

    feat: 숫자 입력 후 메시지 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
     private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_NUM_OF_ATTEMPTS_MESSGE = "시도할 회수는 몇회인가요?";
+
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
@@ -13,9 +14,20 @@ public class OutputView {
         System.out.println(ASK_NUM_OF_ATTEMPTS_MESSGE);
     }
 
-    public void printProgress() {
+    public void printProgress(String name, int position) {
+        System.out.println(name + " : " + convertPositionString(position));
     }
 
+    private String convertPositionString(int position) {
+        String stringPosition = "";
+        for (int i = 0; i < position; i++) {
+            stringPosition += "-";
+        }
+        return stringPosition;
+    }
+    public void printAfterAskNumber() {
+        System.out.println('\n' + "실행결과");
+    }
     public void printWinners(List<String> winners) {
         String winCars = "";
         System.out.print("최종 우승자 : ");
