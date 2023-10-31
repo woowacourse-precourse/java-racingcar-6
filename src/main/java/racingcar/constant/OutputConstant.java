952646f971a6da8a2b02d794255ad30460e4ba79
wonjun <wonjon0930@daum.net>
2023-11-01 01:00:51 +0900

    fix: fix problem that output format is not appropriate when many people win game

--- src/main/java/racingcar/constant/OutputConstant.java
+++ src/main/java/racingcar/constant/OutputConstant.java
@@ -2,5 +2,5 @@ package racingcar.constant;
 
 public class OutputConstant {
     public static final String CAR_DISTANCE_VALUE_SYMBOL = "-";
-    public static final String FINAL_WINNER_DUPLICATE_DELIMITER = ",";
+    public static final String FINAL_WINNER_DUPLICATE_DELIMITER = ", ";
 }
