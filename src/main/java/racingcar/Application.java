2854b11427853ff4fb38165ba0d29753d9ac6252
PgmJun <chltmdwns96@gmail.com>
2023-11-01 01:06:52 +0900

    refactor: CarRacingManager 객체를 생성하여 의존성 주입 및 주요 로직 실행을 담당하도록 수정
    프로그램의 사이즈가 더 커지게 되면 main 메서드의 길이가 너무 길어질 것을 우려하여, Manager 객체를 정의하여 애플리케이션 실행의 전반적인 로직을 담당하도록 변경하고, main함수에선 간단하게 CarRacingManager 객체의 execute 메서드를 호출함으로서 애플리케이션의 의존성 주입과 주요 로직 실행을 담당하도록 개선

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,36 +1,10 @@
 package racingcar;
 
-import racingcar.domain.CarRacing;
-import racingcar.domain.Cars;
-import racingcar.domain.Referee;
-import racingcar.domain.Turn;
-import racingcar.io.CarRacingInputManager;
-import racingcar.io.CarRacingOutputManager;
+import racingcar.domain.CarRacingManager;
 
 public class Application {
     public static void main(String[] args) {
-        CarRacingOutputManager outputManager = new CarRacingOutputManager();
-        CarRacingInputManager inputManager = new CarRacingInputManager(outputManager);
-
-        CarRacing carRacing = createCarRacing(inputManager, outputManager);
-        carRacing.start();
-    }
-
-
-    private static CarRacing createCarRacing(CarRacingInputManager inputManager,
-                                             CarRacingOutputManager outputManager) {
-        Cars cars = readCarsInfo(inputManager);
-        Turn turn = readTurn(inputManager);
-        Referee referee = new Referee();
-
-        return new CarRacing(cars, turn, referee, outputManager);
-    }
-
-    private static Turn readTurn(CarRacingInputManager inputManager) {
-        return Turn.fromTryCount(inputManager.readTryToMoveTurnCount());
-    }
-
-    private static Cars readCarsInfo(CarRacingInputManager inputManager) {
-        return Cars.fromCarNames(inputManager.readCarNames());
+        CarRacingManager carRacingManager = new CarRacingManager();
+        carRacingManager.execute();
     }
 }
