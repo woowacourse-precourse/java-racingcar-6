d468bda4e245887438532cfd90bec0689f26bd3e
hyuuuun <pam4522@gmail.com>
2023-11-01 01:53:35 +0900

    feat($exception): Validator.java에 예외처리 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,2 +1,26 @@
-package racingcar;public class Validator {
+package racingcar;
+
+public class Validator {
+        private String carNames;
+        private String tryCount;
+
+        public void exception(){
+                nameException();
+                tryCountException();
+        }
+        private void nameException(){
+                if (carNames.length() > 5) {
+                        throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+                }
+        }
+        private void tryCountException(){
+
+                if(!tryCount.matches("\\d")){
+                        throw new IllegalArgumentException("시도 횟수는 숫자만 가능합니다.");
+                }
+                if (Integer.parseInt(tryCount) < 1) {
+                        throw new IllegalArgumentException("시도 횟수는 1이상만 가능합니다.");
+                }
+        }
+
 }
