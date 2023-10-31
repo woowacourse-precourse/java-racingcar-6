df53422e4c18cac6189f1e255ab2a76fa0183b18
honi20 <1594cyh@gmail.com>
2023-10-31 23:29:55 +0900

    style: fix 출력 메시지 오타

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -30,7 +30,7 @@ public class Application {
     }
 
     private static String inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
