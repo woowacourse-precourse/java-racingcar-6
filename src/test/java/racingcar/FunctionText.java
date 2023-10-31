bb0d996bd5e8370dce7a6c4010c77dd057aa5c4c
jsj <tkdlek4186@naver.com>
2023-10-31 15:18:19 +0900

    feat : 기능 테스트 추가

--- src/test/java/racingcar/FunctionText.java
+++ src/test/java/racingcar/FunctionText.java
@@ -2,14 +2,50 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FunctionText {
     @Test
-    void split_메서드로_주어진_값을_구분() {
-        String input = "pobi,java";
-        //List<String> result = Racingcar_Game.userInput(input);
+    void 자동차이름_입력_테스트() {
+        String input = "pobi";
+        Car name= new Car(input);
+        String result = name.getName();
+        assertThat(result).contains("pobi");
+    }
 
-        //assertThat(result).contains("java", "pobi");
-        //assertThat(result).containsExactly("pobi", "java");
+    @Test
+    void 전진_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    String input = "pobi";
+                    Car name= new Car(input);
+                    name.move();
+                    String result = name.getPositionInString();
+                    assertThat(result).contains("-");
+                },
+                5
+        );
+    }
+
+    @Test
+    void 우승자_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    String input = "pobi,java";
+                    List<String> carNames = Arrays.asList(input.split(","));
+
+                    Race race = new Race(carNames);
+
+                    race.playRound();
+                    List<String> result=race.getWinners();
+                    assertThat(result).contains("pobi");
+                },
+                5,1
+        );
     }
 
 }
