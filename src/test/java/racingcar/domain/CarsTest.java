03c9b6653cac9e54580f1d06d302c6d2139e7c27
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-31 09:12:03 +0900

    test - CarsTest 테스트코드 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.service.Rule;
+
+public class CarsTest {
+
+    @Mock
+    private RandomNumberGenerator randomNumberGenerator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("자동차의 개수만큼 RandomNumber가 생성된다")
+    @Test
+    void generateRandomNumberByCarsNumber() {
+        List<Car> carList = Arrays.asList(mock(Car.class), // Mock Car 객체 생성
+                mock(Car.class),
+                mock(Car.class));
+        Cars cars = new Cars(carList);
+        Rule rule = new Rule();
+        cars.moveAll(rule, randomNumberGenerator);
+
+        when(randomNumberGenerator.generateRandomNumber()).thenReturn(5);
+
+        for (Car car : carList) {
+            verify(car, times(1)).move(eq(rule), anyInt());
+        }
+    }
+}
