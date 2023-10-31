d16f25eed7d126c02f48315974ee5d6ea04316fa
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:08:06 +0900

    Feat: 실행횟수를 묻는 메서드 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,9 +1,16 @@
 package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.service.ValidateService.isNumber;
 
 public class Input {
     public static String askCarNames(){
         return readLine();
+    }
+
+    public static int askNumberOfGames(){
+        String Number = readLine();
+        isNumber(Number);
+        return Integer.parseInt(Number);
     }
 }
