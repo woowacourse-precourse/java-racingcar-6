7515f90b8e1da3daea9c5855c40947f731bd1e2f
j2noo <wlsdndml213@naver.com>
2023-11-01 00:59:44 +0900

    feat : 게임 종료 확인 메서드

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -7,20 +7,26 @@ public class GameManager {
 
   private List<Car> cars = new ArrayList<Car>();
   private int trialCount;
+  private int currentCount;
 
-   public void init() {
+  public void init() {
     System.out.println("초기화");
-     View.printGameStart();
-     List<String> inputedCars = View.inputCars();
-     inputedCars.forEach(car -> cars.add(new Car(car)));
-     
-     View.printGameTrialCount();
-     trialCount = View.inputGameTrialCount();
-  }
-   public void start(){
-    System.out.println("시작");
-   
+    View.printGameStart();
+    List<String> inputedCars = View.inputCars();
+    inputedCars.forEach(car -> cars.add(new Car(car)));
 
-   
+    View.printGameTrialCount();
+    trialCount = View.inputGameTrialCount();
+    currentCount = 0;
+  }
+
+  public void start() {
+    System.out.println("시작");
+
+
+  }
+
+  public boolean isGameEnd() {
+    return currentCount >= trialCount;
   }
 }
