4461a1909383649342e16ad2a48a705a8d5e0962
devhyeon0 <devhyeon0@gmail.com>
2023-10-31 23:55:00 +0900

    Revert "feat: 랜덤값이 4 이상일 경우 전진하는 기능 구현"
    This reverts commit 85a236de65bc8f85e9942db4cbd53b80b9ef0cac.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,27 +1,2 @@
-package racingcar.domain;
-
-public class Car {
-
-    private static final int BASE_NUMBER = 4;
-    private final String name;
-    private int position;
-
-    public Car(String name) {
-        this.name = name;
-        this.position = 0;
-    }
-
-    public void move(int randomNumber) {
-        if (randomNumber >= BASE_NUMBER) {
-            position++;
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPosition() {
-        return position;
-    }
+package racingcar.domain;public class Car {
 }
