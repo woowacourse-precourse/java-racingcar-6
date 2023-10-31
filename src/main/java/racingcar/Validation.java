6ee571ca4518fc64afe25322968b7732a33e2bfd
hjhyun98 <hyunjo722@naver.com>
2023-11-01 04:14:47 +0900

    REFACTOR: 에러메시지 상수처리

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -2,9 +2,11 @@ package racingcar;
 
 public class Validation {
     static final int CAR_NAME_LENGTH = 5;
+    static final String NAME_LENGTH_ERROR_MESSAGE = "[System] 자동차 이름은 5자 이하만 가능합니다.";
+    static final String ONLY_NUMBER_ERROR_MESSAGE = "[System] 숫자만 입력 가능합니다.";
     void carNameValidation(String name){
         if(name.length() > CAR_NAME_LENGTH){
-            throw new IllegalArgumentException("[System] 자동차 이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
         }
     }
 
@@ -12,7 +14,7 @@ public class Validation {
         try{
             Integer.parseInt(roundString);
         } catch (IllegalArgumentException e){
-            System.out.println("[System] 숫자만 입력 가능합니다.");
+            System.out.println(ONLY_NUMBER_ERROR_MESSAGE);
         }
     }
 }
