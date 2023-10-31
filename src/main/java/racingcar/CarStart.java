7e7f363a6c62fcad9828e14484c922a9c4a7f4d3
LUKR7Q <ybkim0628@naver.com>
2023-10-31 12:40:49 +0900

    기능(carstart): CarStart 클래스에 lineUp() 메서드 추가

--- src/main/java/racingcar/CarStart.java
+++ src/main/java/racingcar/CarStart.java
@@ -7,4 +7,28 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CarStart {
 
+    private final List<Car> cars;
+
+    public CarStart() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void racingGame() {
+        lineUp();
+
+    }
+
+    public void lineUp() {
+        String userInput;
+        String[] names;
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            userInput = Console.readLine();
+            names = userInput.split(",");
+        } while (!Valid.checkNames(names));
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+    }
+
 }
