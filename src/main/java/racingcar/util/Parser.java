00c3d0b4b7a6c2e5d0e6b229d0bdc9f0e807a87a
nari <avakray@hansung.ac.kr>
2023-10-31 16:14:19 +0800

    feat(racingcar): 자동차 이름 5자 이하만 가능하게 예외처리

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -6,6 +6,19 @@ import java.util.Arrays;
 public class Parser {
     public List<String> getSplitString(String str) {
         String[] splitStrings = str.split(",");
-        return Arrays.asList(splitStrings);
+        return checkNamingCondition(Arrays.asList(splitStrings));
+    }
+
+    private List<String> checkNamingCondition(List<String> carNames) {
+        for (String carName : carNames) {
+            checkNamingException(carName);
+        }
+        return carNames;
+    }
+
+    private void checkNamingException(String str) throws IllegalArgumentException{
+        if (str.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 }
