2f5a391a2e5a9dbdb83245a5f6bcfe506ea80197
kdomo <dongho5088@naver.com>
2023-11-01 00:57:03 +0900

    fix: 숫자 정규 표현식 수정

--- src/main/java/racingcar/constants/GameConstants.java
+++ src/main/java/racingcar/constants/GameConstants.java
@@ -3,7 +3,7 @@ package racingcar.constants;
 public class GameConstants {
     public static final String COMMA_SEPARATOR = ",";
     public static final String JOIN_COMMA_SEPARATOR = ", ";
-    public static final String ONLY_NUMBER_REGEX_PATTERN = "-?\\d+";
+    public static final String ONLY_NUMBER_REGEX_PATTERN = "\\d+";
 
 
     public static final int GAME_RANDOM_RANGE_MIN_VALUE  = 0;
