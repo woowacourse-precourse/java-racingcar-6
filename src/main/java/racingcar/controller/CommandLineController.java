b53c428b785a60bb0f1051224749a6e1f468041e
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-31 16:15:49 +0900

    외부에서 Game을 주입받는 멤버변수 및 생성자

--- src/main/java/racingcar/controller/CommandLineController.java
+++ src/main/java/racingcar/controller/CommandLineController.java
@@ -1,5 +1,11 @@
 package racingcar.controller;
 
-public class CommandLineController {
+import racingcar.game.Game;
 
+public class CommandLineController {
+    private Game game;
+
+    public CommandLineController(Game game) {
+        this.game = game;
+    }
 }
