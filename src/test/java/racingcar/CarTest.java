9a20c86eb3c69cf9e69a6908d9a24f14ba11a968
haruday97 <starharu97@naver.com>
2023-10-31 21:17:44 +0900

    test: Car 테스트케이스 작성
    - 4 미만의 수를 받았을 경우 이동하지 않는 테스트케이스
    - 4 이상의 수를 받았을 경우 이동하는 테스트케이스
    - 같은 위치에 있는지 확인하는 테스트케이스
    - 다른 위치에 있는지 확인하는 테스트케이스

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Car;
+import racingcar.domain.CarName;
+import racingcar.domain.CarPosition;
+
+public class CarTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("4 미만의 수가 나오면 이동하지 않는다.")
+    void should_NotMove_When_LessThan4(int number) {
+        Car car = new Car(new CarName("보라돌이"), new CarPosition(0));
+        Car carAfter = car.move(number);
+        assertThat(carAfter.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("4이상의 수가 나오면 이동한다.")
+    void should_Move_When_GreaterThanOrEqualTo4(int number) {
+        Car car = new Car(new CarName("뚜비"), new CarPosition(0));
+        Car carAfter = car.move(number);
+        assertThat(carAfter.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("같은 위치에 있는지 확인한다.")
+    void should_True_When_SamePosition() {
+        Car nana = new Car(new CarName("나나"), new CarPosition(4));
+        Car ppo = new Car(new CarName("뽀"), new CarPosition(4));
+        assertThat(nana.isSamePosition(ppo)).isTrue();
+    }
+
+    @Test
+    @DisplayName("다른 위치에 있는지 확인한다.")
+    void should_False_When_DifferentPosition() {
+        Car nana = new Car(new CarName("나나"), new CarPosition(4));
+        Car ppo = new Car(new CarName("뽀"), new CarPosition(5));
+        assertThat(nana.isSamePosition(ppo)).isFalse();
+    }
+
+}
