7a2240764999fa4c4b315fd909e36ddf0b72e309
hyocheol An <anhyocheol@naver.com>
2023-11-01 04:55:21 +0900

    style : GameController의 클래스 생성 및 기능 구현 진행 상황 주석 입력

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,23 @@
 package racingcar.controller;
 
 public class GameController {
+    public void startGame() {
+        // 자동차 이름 입력 요청
+        // 입력값 검증
+        // 시도할 회수 입력 요청
+        // 입력값 검증
+        // playGame();
+    }
+    public void playGame() {
+        // 실행 결과 메세지 출력
+        // 회차별 게임 진행
+        // 1.난수 발생
+        // 2.자동차 이동
+        // 3.회차 결과 출력
+        // endGame();
+    }
+    public void endGame() {
+        // 최종 우승자 선발
+        // 최종 우승자 출력
+    }
 }
