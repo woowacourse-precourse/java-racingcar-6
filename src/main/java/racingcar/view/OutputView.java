1709d735c0da19088be524a844418327bcbeaa3e
kmw2378 <rlarla677@gmail.com>
2023-10-31 15:38:36 +0900

    refactor(racingcar): OutputView에 상수 필드 추가 이전 GameMessage, GameConfig에서 있던 상수값을 내부에 정의하여 사용하도록 수정하였습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,16 +3,24 @@ package racingcar.view;
 import racingcar.domain.Cars;
 
 public final class OutputView {
+    private static final String CAR_NAMES_INPUT_QUESTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TOTAL_ROUND_INPUT_QUESTION = "시도할 횟수는 몇회인가요?";
+    private static final String GAME_RESULT_PREFIX_MESSAGE = "실행 결과";
+    private static final String GAME_RESULT_FORMAT = "%s : %s";
+    private static final String MOVE_VALUE = "-";
+    private static final String WINNER_NAMES_MESSAGE_FORMAT = "최종 우승자 : %s";
+    private static final String WINNER_NAMES_RESULT_SEPARATOR = ", ";
+
     private OutputView() {
 
     }
 
     public static void printCarNameInputQuestion() {
-        System.out.println(CAR_NAME_INPUT_QUESTION_MESSAGE);
+        System.out.println(CAR_NAMES_INPUT_QUESTION);
     }
 
     public static void printTotalRoundInputQuestion() {
-        System.out.println(TOTAL_ROUND_INPUT_QUESTION_MESSAGE);
+        System.out.println(TOTAL_ROUND_INPUT_QUESTION);
     }
 
     public static void printGameStartMessage() {
@@ -24,7 +32,7 @@ public final class OutputView {
         for (int i = 0; i < cars.getSize(); i++) {
             final String name = cars.getNameFromIndex(i);
             final String currentDistanceValue = getCurrentDistanceValue(cars, i);
-            System.out.println(String.format(GAME_PROGRESS_RESULT_FORMAT, name, currentDistanceValue));
+            System.out.println(String.format(GAME_RESULT_FORMAT, name, currentDistanceValue));
         }
         System.out.println();
     }
@@ -36,6 +44,6 @@ public final class OutputView {
 
     public static void printWinnerNames(final Cars cars) {
         final String winnerNames = String.join(WINNER_NAMES_RESULT_SEPARATOR, cars.getWinnerNames());
-        System.out.println(String.format(WINNER_NAME_MESSAGE_FORMAT, winnerNames));
+        System.out.println(String.format(WINNER_NAMES_MESSAGE_FORMAT, winnerNames));
     }
 }
