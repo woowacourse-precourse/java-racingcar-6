c758cfdf18378f325b6e8ca5a75ce1fdc5e7c3f1
Roista57 <Roista57@gmail.com>
2023-10-31 14:24:31 +0900

    test: 우승자가_여럿인_경우에_대한_출력_확인 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -79,6 +79,23 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 우승자가_여럿인_경우에_대한_출력_확인(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("Ryze,Lux,Leona,Lulu", "3");
+                    assertThat(output()).contains(
+                            "Ryze : -", "Lux : ", "Leona : ", "Lulu : ",
+                            "Ryze : --", "Lux : -", "Leona : -", "Lulu : -",
+                            "Ryze : --", "Lux : --", "Leona : -", "Lulu : -",
+                            "최종 우승자 : Ryze, Lux");
+                },
+                MOVING_FORWARD, STOP, STOP, STOP,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                STOP, MOVING_FORWARD, STOP, STOP
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
