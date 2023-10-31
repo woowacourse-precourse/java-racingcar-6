753d0dedb5be7d5e7d6667dad6e08c6c12d08cb8
jacomyou1026 <jacomyou1026@naver.com>
2023-11-01 01:37:50 +0900

    feat : 차들 이름 구하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" );
+        String[] carNames = getCarNames(sc.nextLine());
+    }
+    public static String[] getCarNames(String names) {
+        String[] nameSplit = names.split(",");
+        for (int i = 0; i < nameSplit.length; i++) {
+            if (nameSplit[i].length() > 5) {
+                throw new IllegalArgumentException("5자 이하만 가능합니다.");
+            }
+        }
+        return nameSplit;
     }
 }
