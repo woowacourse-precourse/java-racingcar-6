0d66fb85de8654a0d18c27e8a3537c927dfc7c1c
Yeop <syt05342@naver.com>
2023-11-01 00:01:24 +0900

    feat: 차 이름 입력 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static racingcar.view.OutputView.printInputCarName;
+
+public class InputView {
+
+    private final static String SPLIT_REGEX = "\\s*,\\s*";
+    private final static int SPLIT_LIMIT = -1;
+
+    public List<String> getNames() {
+        printInputCarName();
+        String input = Console.readLine();
+        String[] names = input.split(SPLIT_REGEX, SPLIT_LIMIT);
+        return new ArrayList<>(Arrays.asList(names));
+    }
+}
