edc592a29c321045b05aa63e00fa6ad5e9e46c4c
leedabin2 <12193152@inha.edu>
2023-11-01 02:24:36 +0900

    refactor(ConfirmationTest): Directly call UserInputHandler methods in test code

--- src/test/java/racingcar/ConfirmationTest.java
+++ src/test/java/racingcar/ConfirmationTest.java
@@ -11,14 +11,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ConfirmationTest extends NsTest {
-    UserInputHandler userInputHandler = new UserInputHandler();
     GameController gameController = new GameController();
 
     @Test
     void 자동차이름_5자초과_예외_처리() {
         String nameList = "iamdabin";
 
-        assertThatThrownBy(() -> userInputHandler.validateName(nameList))
+        assertThatThrownBy(() -> UserInputHandler.validateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
     }
@@ -27,7 +26,7 @@ public class ConfirmationTest extends NsTest {
     void 자동차이름_중복_예외_처리() {
         List<String> nameList = Arrays.asList("dabin","dabin");
 
-        assertThatThrownBy(() -> userInputHandler.checkDuplicateName(nameList))
+        assertThatThrownBy(() -> UserInputHandler.checkDuplicateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
     }
@@ -36,7 +35,7 @@ public class ConfirmationTest extends NsTest {
     void 자동차이름_공백_예외_처리() {
         String nameList = "";
 
-        assertThatThrownBy(() -> userInputHandler.validateUserInput(nameList))
+        assertThatThrownBy(() -> UserInputHandler.validateUserInput(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름을 입력해주세요.");
     }
@@ -48,22 +47,23 @@ public class ConfirmationTest extends NsTest {
         String numFormatCount = "dabin";
         int attemptCount = Integer.parseInt(numCount);
 
-        assertThatThrownBy(() -> userInputHandler.validateAttemptCount(String.valueOf(attemptCount)))
+        assertThatThrownBy(() -> UserInputHandler.validateAttemptCount(String.valueOf(attemptCount)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도 횟수는 0보다 커야 합니다.");
 
-        assertThatThrownBy(() -> userInputHandler.validateAttemptCount(count))
+        assertThatThrownBy(() -> UserInputHandler.validateAttemptCount(count))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도 횟수를 입력해주세요.");
 
-        assertThatThrownBy(() -> userInputHandler.validateAttemptCount(numFormatCount))
+        assertThatThrownBy(() -> UserInputHandler.validateAttemptCount(numFormatCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도 횟수는 숫자만 입력할 수 있습니다.");
     }
 
     @Test
     void setUp_자동차_객체_생성_테스트() {
-        gameController.setUp("pobi,dabin,jun");
+        List<String> names = UserInputHandler.validateUserInput("pobi,dabin,jun");
+        gameController.setUp(names.toString());
         gameController.run(1);
 
         List<String> winners = gameController.getWinners();
@@ -71,7 +71,7 @@ public class ConfirmationTest extends NsTest {
     }
 
     @Test
-    public void getWinners_우승자_자동차_이름_반환_테스트() {
+    void getWinners_우승자_자동차_이름_반환_테스트() {
         gameController.setUp("pobi,dabin,jun");
         gameController.run(1);
 
@@ -82,7 +82,7 @@ public class ConfirmationTest extends NsTest {
     }
 
     @Test
-    public void run_메서드_테스트() {
+    void run_메서드_테스트() {
         gameController.setUp("pobi,dabin");
         gameController.run(5);
 
