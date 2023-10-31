264fd530877f0c75a2d47229f0e6c325542fdb29
minisyu <iswater.kim@gmail.com>
2023-10-31 11:44:02 +0900

    refactor(InputValidator): InputValidator를 지니도록 변경, 메소드명 변경

--- src/main/java/racingcar/converter/InputConverter.java
+++ src/main/java/racingcar/converter/InputConverter.java
@@ -5,9 +5,22 @@ import java.util.List;
 import racingcar.domain.game.car.Car;
 import racingcar.domain.game.car.CarName;
 import racingcar.domain.game.car.Cars;
+import racingcar.domain.game.car.TrialCount;
+import racingcar.validator.InputValidator;
 
 public class InputConverter {
-    public Cars convertStringToCars(String input) {
+    private final InputValidator inputValidator;
+
+    public InputConverter(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public Cars toCars(String input) {
+
+        inputValidator.validateInputFormat(input);
+        inputValidator.validateNonEmpty(input);
+        inputValidator.validateInputCarCount(input);
+
         List<Car> cars = Arrays.stream(input.split(","))
                 .map(CarName::from)
                 .map(Car::from)
@@ -16,7 +29,13 @@ public class InputConverter {
         return Cars.from(cars);
     }
 
-    public int covertStringToInteger(String input) {
+    public int toTrial(String input) {
+        inputValidator.validateNumericString(input);
         return Integer.parseInt(input);
+    }
+
+    public TrialCount converStringToTrialCount(String input) {
+        int trialCount = Integer.parseInt(input);
+        return TrialCount.from(trialCount);
     }
 }
