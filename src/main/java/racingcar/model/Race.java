847a8835ffa1f1e513e3d3cbff2331233c8b9f10
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:14:25 +0900

    feat(Race): Race 모델 초기화 메서드 추가

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,4 +1,15 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
