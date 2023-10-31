946f9458c78706ff998c3405295aea98aed1597f
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-01 03:59:34 +0900

    feat: 차량 이름 정보를 받기 위한 안내 문구 CarNamesGuideOutputView 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import view.CarNamesGuideOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarNamesGuideOutputView carNamesGuideOutputView = new CarNamesGuideOutputView();
+        carNamesGuideOutputView.printGameStart();
     }
 }
