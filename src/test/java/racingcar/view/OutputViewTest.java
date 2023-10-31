e7995d246f89190999f78dd3457e57a89c676ad7
Dawin <dawin002@naver.com>
2023-10-31 22:09:50 +0900

    test(OutputViewTest): 라운드 결과 출력 기능 테스트 이름 변경
    라운드 결과를 출력하는 기능의 이름 일부 수정(반환한다 -> 반환)
    - buildResultString_자동차별_문자열을_합쳐_결과_문자열로_만들어_반환()
    - makeResultString_자동차의_이름과_이동_거리를_문자열로_만들어_반환()

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class OutputViewTest {
 
     @Test
-    void buildResultString_자동차별_문자열을_합쳐_결과_문자열로_만들어_반환한다() {
+    void buildResultString_자동차별_문자열을_합쳐_결과_문자열로_만들어_반환() {
         //given
         java.util.List<String> carNames = List.of("dawin", "woowa", "han", "tech", "cours");
         List<Integer> roundResult = List.of(1, 2, 3, 4, 5);
@@ -26,7 +26,7 @@ class OutputViewTest {
     }
 
     @Test
-    void makeResultString_자동차의_이름과_이동_거리를_문자열로_만들어_반환한다() {
+    void makeResultString_자동차의_이름과_이동_거리를_문자열로_만들어_반환() {
         //given
         String carName = "dawin";
         int distance = 7;
