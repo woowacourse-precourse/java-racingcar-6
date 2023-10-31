333a5444a013151634d9cf763835947c02d76b44
Yujin <tkfkd7038@naver.com>
2023-11-01 01:42:07 +0900

    refactor(racingcar): Referee클래스를 WinnerManager와 RaceGameManager로 분리

---
+++ src/main/java/racingcar/RaceGameManager.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class RaceGameManager {
+    RaceGameManager(Car[] car){
+
+    }
+    public void noticeRacingResult(String[] carNameList, NumberGenerator Generator) { // 레이싱 실행 결과 출력
+
+
+
+    }
+
+    public void noticeWinner() { // 최종 우승자 출력
+
+    }
+}
