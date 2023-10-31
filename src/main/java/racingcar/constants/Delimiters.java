5ebe572009573c6a0e2ff7650859abf553b7667c
minsu20 <syt07100@gmail.com>
2023-10-31 23:47:03 +0900

    feat(constants): 구분자 관련 상수 클래스 추가

---
+++ src/main/java/racingcar/constants/Delimiters.java
@@ -0,0 +1,19 @@
+package racingcar.constants;
+
+public enum Delimiters {
+
+    CAR_NAME_DELIMITER(","),
+    RACE_WINNER_DELIMITER(","),
+    CAR_NAME_DELIMITER_DESCRIPTION("쉼표(,)");
+
+    private final String message;
+
+    Delimiters(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
