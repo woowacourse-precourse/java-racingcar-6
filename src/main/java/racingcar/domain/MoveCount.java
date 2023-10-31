bf3879fc0234a05c3d71c09d5abab5ddb824bdd0
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 23:35:27 +0900

    refactor(domain): 역할에 맞는 기능 이동

--- src/main/java/racingcar/domain/MoveCount.java
+++ src/main/java/racingcar/domain/MoveCount.java
@@ -1,11 +1,31 @@
 package racingcar.domain;
 
+import static racingcar.constant.CarConstant.MAX_MOVE_COUNT;
+import static racingcar.constant.CarConstant.MIN_MOVE_COUNT;
+
+import racingcar.validator.Validator;
+
+/*
+ *  시도할 횟수를 담당
+ * */
+
 public class MoveCount {
     private final int count;
 
-    public MoveCount(int count) {
-        this.count = count;
+    private MoveCount(String input) {
+        validate(input);
+        this.count = Integer.parseInt(input);
     }
+
+    public static MoveCount fromInput(String input) {
+        return new MoveCount(input);
+    }
+
+    private void validate(String input) {
+        Validator.commandToInteger(input);
+        Validator.commandValueInRange(Integer.parseInt(input), MIN_MOVE_COUNT, MAX_MOVE_COUNT);
+    }
+
 
     public int getCount() {
         return count;
