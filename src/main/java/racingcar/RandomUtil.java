4b68688a099fd39cc3d03962083a924155574cba
jaea-kim <jaeakim98@gmail.com>
2023-10-31 11:34:57 +0900

    FIX: 랜덤 함수 범위 수정

--- src/main/java/racingcar/RandomUtil.java
+++ src/main/java/racingcar/RandomUtil.java
@@ -5,6 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomUtil {
     public static int getRandomNumber() {
 
-        return Randoms.pickNumberInRange(GameConfig.MIN_RANDOM_NUMBER.getValue(), GameConfig.MIN_RANDOM_NUMBER.getValue());
+        return Randoms.pickNumberInRange(GameConfig.MIN_RANDOM_NUMBER.getValue(), GameConfig.MAX_RANDOM_NUMBER.getValue());
     }
 }
