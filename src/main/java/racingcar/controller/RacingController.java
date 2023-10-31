14d53cb3d57e4453adbe6dfbc74c9f18880ebb4f
kimky0320 <kim3268418@naver.com>
2023-10-31 14:33:26 +0900

    feat: main 시작점 설정
    controller run()에서 핵심 로직 실행

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,14 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingController {
+    private InputView inputView;
+
+    public RacingController() {
+        this.inputView = new InputView();
+    }
+
+    public void run() {
+    }
+}
