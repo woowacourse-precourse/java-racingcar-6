f77dac1f5a3dbf5af23da53993a340c8d9e2eacb
JoonKyo <joonkyolee@gmail.com>
2023-10-31 22:14:41 +0900

    feat(WinnerCar): 이동 거리 중 가장 많이 이동한 거리를 저장하는 생성자 추가

--- src/main/java/racingcar/model/WinnerCar.java
+++ src/main/java/racingcar/model/WinnerCar.java
@@ -1,2 +1,17 @@
-package racingcar.model;public class Winner {
+package racingcar.model;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class WinnerCar {
+    private List<String> winnerCarNames;
+    private final int winnerStateValue;
+
+    public WinnerCar(List<Car> cars){
+        winnerStateValue = cars.stream()
+                .sorted(Comparator.comparing(Car::getCarState).reversed())
+                .toList()
+                .get(0)
+                .getCarState();
+    }
 }
