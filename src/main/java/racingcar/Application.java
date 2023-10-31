1bb6ba808919938c1ad171a931547fdf8b81c2f5
yh-skku <112400744+yh-skku@users.noreply.github.com>
2023-10-31 16:16:43 +0900

    feat: 사용자 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,40 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.*;
+class Car{
+    String name="";
+
+    Car(String name) {
+
+        this.name = name;
+
+    }
+
+    void move(){
+
+    }
+
+
+}
+
+class User{
+    static int count;
+
+
+}
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String inputName="";
+        String inputCount="";
+        int count;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputName = Console.readLine();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        inputCount=Console.readLine();
+        count=Integer.parseInt(inputCount);
     }
 }
