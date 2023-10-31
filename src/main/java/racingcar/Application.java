e2f43427541d943378edaf4f423dc5f7b46f2b7b
hongsunghak <qmffndnjzm@naver.com>
2023-10-31 23:02:12 +0900

    feat: 실행 파일 및 테스트 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GamePlay gamePlay = new GamePlay();
+        gamePlay.start();
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,29 +1,36 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.validation.RaceValidation;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-
+    private RaceValidation raceValidation = new RaceValidation();
     @Test
-    void 전진_정지() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
+    @DisplayName("자동차 이름 입력에 쉼포가 없으면 예외 발생")
+    void validateSeparator() {
+        String includeComma = "자동차1,자동차2";
+        String unIncludeComma = "자동차1자동차2";
+        assertThatNoException().isThrownBy(() -> raceValidation.validationSeparator(includeComma));
+        assertThatThrownBy(() -> raceValidation.validationSeparator(unIncludeComma)).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("자동차 이름 5글자 넘길 시 예외 발생")
+    void validateNameLength() {
+        String carName1 = "마이바흐";
+        String carName2 = "롤스로이스 고스트";
+        assertThatNoException().isThrownBy(() -> raceValidation.validationNameLength(carName1));
+        assertThatThrownBy(() -> raceValidation.validationNameLength(carName2)).isInstanceOf(IllegalArgumentException.class);
     }
 
+
     @Test
+
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
