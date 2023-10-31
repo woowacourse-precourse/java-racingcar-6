f76c99be953f86eb6155e98c003eb21462683a34
노정완 <jeongwan.noh@gmail.com>
2023-10-31 19:15:01 +0900

    feat: 뷰어 클래스 생성

---
+++ src/main/java/racingcar/view/RacingViewer.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+public class RacingViewer {
+    private final static String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final static String RESULT_MESSAGE = "실행 결과";
+    private final static String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void showCarNameMessage() {
+        System.out.println(CAR_NAME_MESSAGE);
+    }
+
+    public void showTryCountMessage() {
+        System.out.println(TRY_COUNT_MESSAGE);
+    }
+
+    public void showResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void showWinnerMessage() {
+        System.out.println(WINNER_MESSAGE);
+    }
+}
