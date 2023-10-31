bdd7f0b3965c1c8b6d2a4dcd28b299a7b13135cb
win9 <krrr883@gmail.com>
2023-11-01 06:00:37 +0900

    [Test]: 통합테스트 오류 수정
    DB초기화가 되지 않아서 통합테스트가 실패하는 오류 수정

--- src/test/java/racingcar/repository/CarRepositoryTest.java
+++ src/test/java/racingcar/repository/CarRepositoryTest.java
@@ -1,16 +1,29 @@
 package racingcar.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.service.RacingService;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarRepositoryTest {
-    private final CarRepository carRepository = CarRepository.getInstance();
+    private CarRepository carRepository;
 
+    @BeforeEach
+    void setUp() {
+        carRepository = CarRepository.getInstance();
+        carRepository.clean();
+    }
+
+    @AfterEach
+    void end() {
+        carRepository.clean();
+    }
     @Test
     public void 저장소_삽입_성공_테스트() {
         //given
--- src/test/java/racingcar/repository/ResultRepositoryTest.java
+++ src/test/java/racingcar/repository/ResultRepositoryTest.java
@@ -1,14 +1,26 @@
 package racingcar.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResultRepositoryTest {
-    private final ResultRepository resultRepository = ResultRepository.getInstance();
+    private ResultRepository resultRepository;
 
+    @BeforeEach
+    void setUp() {
+        resultRepository = ResultRepository.getInstance();
+        resultRepository.clean();
+    }
+
+    @AfterEach
+    void end() {
+        resultRepository.clean();
+    }
     @Test
     public void 저장소_삽입_성공_테스트() {
         //given
