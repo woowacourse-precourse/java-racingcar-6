3205dc9766e5fc950140485b5513ee6c0fc285bb
zjacom <eephus625@icloud.com>
2023-10-31 19:53:34 +0900

    style(racingCar): User 클래스 주석 수정

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -7,9 +7,7 @@ public class User {
     private List<Car> carList = new ArrayList<>();
 
     public void makeCars(String inputStr) {
-        // 콤마로 구분한 자동차를 담아두는 임시 배열
         String[] carNameTempArr = splitByComma(inputStr);
-        // 나눈 이름의 길이가 5 이하라면 cars에 Car 인스턴스 추가
         for (int i = 0; i < carNameTempArr.length; i++) {
             String carName = carNameTempArr[i];
             appendCar(carName);
@@ -21,8 +19,7 @@ public class User {
         return carNameTempArr;
     }
 
-
-    // 함수 이름 다시 생각. carList에 추가하는 내용 안들어감
+    // 자동차 이름의 길이가 5와 같거나 작으면 carList에 추가
     private void appendCar(String carName) {
         if (carName.length() <= 5) {
             carList.add(new Car(carName));
@@ -32,10 +29,9 @@ public class User {
     }
 
     public void tryDriving(int attemptNum) {
-        // 입력받은 횟수만큼 각각의 자동차를 주행한다.
         for (int i = 0; i < attemptNum; i++) {
             drivingEachCar();
-            // 각각의 자동차를 한 번씩 print하면 줄바꿈
+            // 각각의 자동차를 한 번씩 출력하면 줄바꿈
             System.out.println();
         }
     }
@@ -43,7 +39,7 @@ public class User {
     private void drivingEachCar() {
         for (Car car : carList) {
             car.driving();
-            // 자동차의 누적 주행거리 print
+            // 자동차의 누적 주행거리 출력
             System.out.println(car.getCarName() + " : " + car.getDistanceDriven());
         }
     }
