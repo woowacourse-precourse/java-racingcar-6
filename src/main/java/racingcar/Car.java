afea88f6ccac40ab87626c3821815553a795fb0f
Yoonseol Lee <thisyoon97@gmail.com>
2023-10-31 15:36:07 +0900

    feat: Car 클래스의 생성자 초기화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,4 +3,9 @@ package racingcar;
 public class Car {
     private String name;
     private int location = 0;
+
+    public Car(String name) {
+        // validateName(name);
+        this.name = name;
+    }
 }
