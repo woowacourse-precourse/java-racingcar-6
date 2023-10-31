8868b80d80311f73e09a19181f960c35dd66417b
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-01 01:17:02 +0900

    test(CarsTest): findWinners 함수 테스트 코드 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,11 +1,14 @@
 package racingcar.domain;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CarsTest {
+public class CarsTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
     private static final String carNames1 = "bora,dori,nana";
@@ -18,5 +21,26 @@ public class CarsTest {
         Cars cars = Cars.from(carNames2);
         //then
         assertEquals(cars.getCarList().size(), 3);
+    }
+
+    @Test
+    @DisplayName("findWinners 함수 테스트")
+    void 우승자_반환_기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Cars cars = Cars.from(carNames1);
+                    cars.moveCars();
+                    List<String> winners = cars.findWinners();
+                    assertEquals(winners.size(), 2);
+                    assertEquals(winners.get(0), "bora");
+                    assertEquals(winners.get(1), "nana");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Override
+    protected void runMain() {
+
     }
 }
