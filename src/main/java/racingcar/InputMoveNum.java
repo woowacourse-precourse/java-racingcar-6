9b2d05b6b057833b8bbfdbed989dfa2e9caf294c
gyahn <pop2413@gmail.com>
2023-10-31 15:27:52 +0900

    fix: 숫자 입력 -> 사용자 입력 수정

--- src/main/java/racingcar/InputMoveNum.java
+++
@@ -1,6 +0,0 @@
-package racingcar;
-
-public class InputMoveNum {
-    //이동 횟수 입력 클래스
-
-}
---
+++ src/main/java/racingcar/UserInput.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class UserInput {
+    //사용자 입력 클래스
+    public String[] InputCar(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine();
+        String[] carNames = cars.split(",");
+        return carNames;
+    }
+    public int InputNum(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+
+
+}
