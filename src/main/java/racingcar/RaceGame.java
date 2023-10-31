ea547236c66941fe48c742ac18b75294eb154832
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-31 06:51:18 +0000

    [RaceGame] Refactoring : intent depth 2 수준으로 변경

--- src/main/java/racingcar/RaceGame.java
+++ src/main/java/racingcar/RaceGame.java
@@ -1,30 +1,41 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class RaceGame {
     public static void main(String[] args) {
         try {
-            String[] carNames = InputManager.getCarNames();
-            int numberOfAttempts = InputManager.getNumberOfAttempts();
-
-            List<Car> cars = new ArrayList<>();
-            for (String name : carNames) {
-                cars.add(new Car(name));
-            }
-
-            for (int round = 1; round <= numberOfAttempts; round++) {
-                Race.startRace(cars, 1);
-                OutputManager.printRoundResult(round, cars);
-            }
-
-            List<Car> winners = getWinners(cars);
-            OutputManager.printWinners(winners);
-
+            playRacingGame();
         } catch (IllegalArgumentException e) {
-            System.out.println("오류: " + e.getMessage());
+            handleException(e);
         }
+    }
+
+    private static void playRacingGame() {
+        String[] carNames = InputManager.getCarNames();
+        int numberOfAttempts = InputManager.getNumberOfAttempts();
+        List<Car> cars = initializeCars(carNames);
+
+        for (int round = 1; round <= numberOfAttempts; round++) {
+            Race.startRace(cars, 1);
+            OutputManager.printRoundResult(round, cars);
+        }
+
+        List<Car> winners = getWinners(cars);
+        OutputManager.printWinners(winners);
+    }
+
+    private static List<Car> initializeCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    private static void handleException(IllegalArgumentException e) {
+        System.out.println("오류: " + e.getMessage());
     }
 
     public static List<Car> getWinners(List<Car> cars) {
@@ -32,11 +43,12 @@ public class RaceGame {
         List<Car> winners = new ArrayList<>();
 
         for (Car car : cars) {
-            if (car.getPosition() > maxPosition) {
-                maxPosition = car.getPosition();
+            int carPosition = car.getPosition();
+            if (carPosition > maxPosition) {
+                maxPosition = carPosition;
                 winners.clear();
                 winners.add(car);
-            } else if (car.getPosition() == maxPosition) {
+            } else if (carPosition == maxPosition) {
                 winners.add(car);
             }
         }
