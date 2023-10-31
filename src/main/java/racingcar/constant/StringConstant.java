5dc2f361e28db599444bc2bed057ee626de26a2a
jum0624 <jum0624@naver.com>
2023-11-01 02:26:09 +0900

    refactor: 문자열 상수명 변경

--- src/main/java/racingcar/constant/StringConstant.java
+++ src/main/java/racingcar/constant/StringConstant.java
@@ -4,7 +4,7 @@ public enum StringConstant {
 
     PATTERN("^[a-zA-Z]*$"),
     SPACE(" "),
-    REGEX(","),
+    COMMA(","),
     INPUT_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
     INPUT_ROUND_NUMBER_MESSAGE("시도할 회수는 몇회인가요?\n"),
     OUTPUT_RESULT_MESSAGE("실행 결과\n"),
