607510d4cdee2a5bd34f2a8d35b1646ed4ca91f7
flame-possible <werq2003@naver.com>
2023-11-01 00:36:32 +0900

    refactor : Race.java 리팩토링
    indent가 너무 깊은 것 같아 RacingEachCars 메소드를 추가

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -8,12 +8,18 @@ public class Race {
     public void Racing(List<Car> cars, int GameNum){
 
         for(int i = 0; i < GameNum; i++){
-            for(Car car : cars){
-                WinCar(car);
-            }
+            RacingEachCars(cars);
         }
 
     }
+
+    private void RacingEachCars(List<Car> cars){
+        for(Car car : cars){
+            WinCar(car);
+        }
+
+    }
+
 
     private void WinCar(Car car){
         int randnum = Randoms.pickNumberInRange(0,9);
