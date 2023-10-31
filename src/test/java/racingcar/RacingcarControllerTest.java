7e71d8efb3ff8b573d5b5c4206d59b745e0b5e74
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-31 22:56:52 +0900

    test: BeforeEach 추가 (RacingcarController 클래스의 인스턴스 생성하여 racingcarController 변수에 할당)

--- src/test/java/racingcar/RacingcarControllerTest.java
+++ src/test/java/racingcar/RacingcarControllerTest.java
@@ -1,16 +1,26 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingcarControllerTest {
-    RacingcarController racingcarController = new RacingcarController();
+    private RacingcarController racingcarController;
+    @BeforeEach
+    void beforeEach() {
+        racingcarController = new RacingcarController();
+    }
 
     @Test
     void 자동차이름_입력값_구분() {
         String inputCarsName = "abc,def,gh";
         String[] result = racingcarController.splitCarsName(inputCarsName);
         assertThat(result).contains("abc", "def", "gh");
+    }
+
+    @Test
+    void 자동차_리스트_저장() {
+
     }
 }
