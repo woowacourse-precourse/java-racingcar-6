94173a036e6b5ccb2721ad373b7d8e99876194db
ZZ3n <ndsrhkd@naver.com>
2023-10-31 15:48:10 +0900

    fix(racer): 리터럴 "-"에서 상수로 변경
    "-"가 리터럴로 쓰여 있었는데, 상수로 변경하였다

--- src/main/java/racingcar/Racer.java
+++ src/main/java/racingcar/Racer.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.text.MessageFormat;
 
 public class Racer {
+    private static final int MOVING_FORWARD = 4;
+    private static final String RACING_DISPLAY = "-";
+
     private final String name;
     private String distance = "";
-    private static final int MOVING_FORWARD = 4;
 
     public Racer(String name) {
         if (name == null) {
@@ -18,7 +20,7 @@ public class Racer {
     public void proceedTurn() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= MOVING_FORWARD) {
-            this.distance = this.distance + "-";
+            this.distance = this.distance + RACING_DISPLAY;
         }
         String printString = MessageFormat.format("{0} : {1}", name, distance);
         System.out.println(printString);
