f3cf31d5b6e6d5847b104ef8646696c83d9e459c
jae woo Jung <awfjol2008@naver.com>
2023-10-31 09:57:52 +0900

    feat: OutputView 클래스의 접근 제한을 위해 private 생성자 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,10 @@ package racingcar.view;
 
 public class OutputView {
 
+    private OutputView() {
+
+    }
+
     public static void printMessage(String message) {
         System.out.println(message);
     }
