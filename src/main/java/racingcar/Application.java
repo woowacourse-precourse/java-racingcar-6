49a544e1166c3dceae193eb25a5fbb6b8fa0e7eb
Tiger <skdlghwns@gmail.com>
2023-11-01 00:04:16 +0900

    feat(racingcar): 프로그램 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarGenerator;
+import racingcar.domain.Racing;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarGenerator carGenerator = new CarGenerator();
+        List<Car> racingCarList = carGenerator.createCars(carGenerator.askCarName());
+        Racing racingOne = new Racing(racingCarList);
+        racingOne.doOneGame();
     }
 }
