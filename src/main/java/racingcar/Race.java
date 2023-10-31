7f9ac0fa717cd9386116c298f39693d51208f5f2
Yoonseol Lee <thisyoon97@gmail.com>
2023-10-31 15:48:02 +0900

    feat: 사용자로부터 입력 받은 차량을 등록하는 기능

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,8 +1,20 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
-    private List<Car> carsList;
+    private List<Car> participatingCars;
     private int moveCounts;
+
+    public Race() {
+        participatingCars = new ArrayList<>();
+    }
+
+    public void registerCar(List<String> participatingCars) {
+        // 검증 추가 필요
+        for (String carName : participatingCars) {
+            this.participatingCars.add(new Car(carName));
+        }
+    }
 }
