854e7a7e8fda2eb96c8c1f9bdd0e6715d073d7dd
kchranger <kchranger@gmail.com>
2023-10-31 23:06:13 +0900

    feature(app) : race guide message

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -12,7 +12,10 @@ public class RacingGame {
     }
 
     public void startRacingProcess() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsName = GameInput.inputCarsName();
+
+        System.out.println("시도할 회수는 몇회인가요?");
         int maxRaceTime = GameInput.raceTime();
         CarList racingCarList = new CarList(carsName);
 
