ad88d9e3a597f09aee20a42f583033e4696636d6
ssunnykku <ssunnykku@gmail.com>
2023-10-31 20:54:46 +0900

    feat(Referee): numberGenerator에게 무작위 값을 받는 기능

---
+++ src/main/java/racingcar/game/Referee.java
@@ -0,0 +1,16 @@
+package racingcar.game;
+
+import racingcar.numberGenerator.NumberGenerator;
+import racingcar.user.UserInput;
+
+public class Referee {
+    UserInput user = new UserInput();
+    NumberGenerator numberGenerator = new NumberGenerator();
+    private int userAttemptCount() {
+      return user.attemptCountInput();
+    }
+    private int randomValueForMove() {
+        return numberGenerator.createRandomNumber();
+    }
+
+}
