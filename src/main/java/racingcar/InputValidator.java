c39cae07cd900f71118f39135ab52a806c5efd4c
myoungjin-kim <kaudwls58@naver.com>
2023-10-31 23:10:49 +0900

    feat: InputValidator 클래스 추가
    차량의 이름에 대한 예외처리
    알파벳으로만 이뤄져야하고, 길이가 1~5사이 이어야 한다.

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    private final static int MAX_CAR_NAME_LENGTH = 5;
+
+    public static boolean carName(String[] inputs) {
+        for (String input : inputs)
+            if (!Pattern.matches("^[a-zA-Z]*$", input) && input.length()<=MAX_CAR_NAME_LENGTH && !input.isEmpty()) {
+                return false;
+            }
+        return true;
+    }
+}
