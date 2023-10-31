2bffdb845d18a1151d5a7c66c4c4ba9c4a6ecb91
K-KY <kyuyoungk@naver.com>
2023-11-01 06:08:28 +0900

    Refactor(NumberGenerator) : 상수화

--- src/main/java/racingcar/NumberGenerator.java
+++ src/main/java/racingcar/NumberGenerator.java
@@ -3,8 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+
     public static int generateNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 
 }
