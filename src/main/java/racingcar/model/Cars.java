e62aa35a4f52a8cb12ffb746b1a2f64029c789d3
hoyeon <highoption@naver.com>
2023-10-31 20:49:11 +0900

    feat(Cars) : 우승자를 정해주는 winner메서드 생성
    게임이 끝난 후 가장 멀리 간 자동차를 찾아서 반환

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,11 +1,26 @@
 package racingcar.model;
 
+import static racingcar.model.CarNameValidator.carNameValidation;
+
+import java.util.Comparator;
 import java.util.List;
 
 public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
+        carNameValidation(cars);
         this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public Car winner() {
+        Car win = cars.stream()
+                .max(Comparator.comparingInt(Car::getDistance))
+                .orElse(null);
+        return win;
     }
 }
