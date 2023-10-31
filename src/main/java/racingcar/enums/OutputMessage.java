d3c717ae20a6c2d947bacb3b8799672a34e740aa
Jae-Young98 <ygy0102@gmail.com>
2023-11-01 00:10:39 +0900

    fix(OutputMessage): 출력 오류 수정
    - 실행 결과 출력시 개행이 아닌
    NEW_LINE으로 표시되던 오류 수정

--- src/main/java/racingcar/enums/OutputMessage.java
+++ src/main/java/racingcar/enums/OutputMessage.java
@@ -3,7 +3,7 @@ package racingcar.enums;
 public enum OutputMessage {
     NEW_LINE(System.lineSeparator()),
     WINNER_MESSAGE("최종 우승자 : "),
-    RESULT_MESSAGE(NEW_LINE + "실행 결과"),
+    RESULT_MESSAGE(NEW_LINE.getMessage() + "실행 결과"),
     JOIN_DELIMITER(", "),
     COLON(" : "),
     DASH("-");
