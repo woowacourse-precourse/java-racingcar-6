9836104be5be9d399efcaa659374a37dc7042508
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-01 05:20:15 +0900

    test : 최소 자동차 개수에 대한 예외 발생 테스트 작성

--- src/test/java/racingcar/util/validation/CarNamesValidatorTest.java
+++ src/test/java/racingcar/util/validation/CarNamesValidatorTest.java
@@ -12,8 +12,10 @@ import racingcar.vo.CarName;
 
 class CarNamesValidatorTest {
 
-    private static final String EMPTY_CAR_NAME_MESSAGE = "자동차 이름에 빈 값은 허용되지 않습니다.";
-    private static final String DUPLICATE_CAR_NAME_MESSAGE = "중복된 자동차 이름이 존재합니다.";
+    private static final Integer MIN_CAR_NAME_SIZE = 2;
+    private static final String EMPTY_CAR_NAME_ERROR_MESSAGE = "자동차 이름에 빈 값은 허용되지 않습니다.";
+    private static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 존재합니다.";
+    private static final String MIN_CAR_NAME_SIZE_ERROR_MESSAGE = "최소 %s개 이상의 자동차 이름을 입력하셔야 합니다.";
 
     @DisplayName("유효한 자동차 이름 목록이 주어진 경우 예외가 발생하지 않는다.")
     @Test
@@ -35,7 +37,7 @@ class CarNamesValidatorTest {
 
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
                 () -> CarNamesValidator.validateCarNames(carNames));
-        assertEquals(EMPTY_CAR_NAME_MESSAGE, e.getMessage());
+        assertEquals(EMPTY_CAR_NAME_ERROR_MESSAGE, e.getMessage());
     }
 
     @DisplayName("자동차 이름에 중복된 이름이 있을 경우 예외가 발생한다.")
@@ -45,6 +47,20 @@ class CarNamesValidatorTest {
 
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
                 () -> CarNamesValidator.validateCarNames(carNames));
-        assertEquals(DUPLICATE_CAR_NAME_MESSAGE, e.getMessage());
+        assertEquals(DUPLICATE_CAR_NAME_ERROR_MESSAGE, e.getMessage());
+    }
+
+
+    @DisplayName("자동차 이름의 개수가 최소 개수보다 작은 경우 예외가 발생한다.")
+    @Test
+    void carNameListSmallerThanMin_test() {
+        List<CarName> carNames = Arrays.asList(
+                new CarName("Car1"));
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> CarNamesValidator.validateCarNames(carNames));
+
+        String errorMessage = String.format(MIN_CAR_NAME_SIZE_ERROR_MESSAGE, MIN_CAR_NAME_SIZE);
+        assertEquals(errorMessage, e.getMessage());
     }
 }
