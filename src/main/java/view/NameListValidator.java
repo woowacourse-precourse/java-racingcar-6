a59ac8f9b98e0e256b3bcb8d6c06841e02b4edf5
nayonsoso <panda0329@naver.com>
2023-10-31 23:23:13 +0900

    feat: 이름에 대한 검증 함수를 모두 호출하는 함수 생성

--- src/main/java/view/NameListValidator.java
+++ src/main/java/view/NameListValidator.java
@@ -1,9 +1,22 @@
 package view;
 
+import constants.Constants;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NameListValidator {
+    public List<String> validate(String nameStr){
+        validateNotEmpty(nameStr);
+        validateNotEndsWithComma(nameStr);
+        validateContainsRepeatingCommas(nameStr);
+        List<String> nameList = splitAndTrim(nameStr, ",");
+        validateNameLength(nameList);
+        validateNameDuplication(nameList);
+        return nameList;
+    }
+
     private void validateNotEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("입력된 값이 없습니다.");
@@ -29,10 +42,10 @@ public class NameListValidator {
         return input.replace(" ", "");
     }
 
-    private void validateNameLength(List<String> names, int maxLength) {
+    private void validateNameLength(List<String> names) {
         for (String name : names) {
-            if (name.length() > maxLength) {
-                String message = "이름의 길이가 " + maxLength + "자가 넘습니다.";
+            if (name.length() > Constants.MAX_LENGTH) {
+                String message = "이름의 길이가 " + Constants.MAX_LENGTH + "자가 넘습니다.";
                 throw new IllegalArgumentException(message);
             }
         }
@@ -45,5 +58,11 @@ public class NameListValidator {
                 throw new IllegalArgumentException("중복되는 이름이 있습니다.");
             }
         }
+    }
+
+    private List<String> splitAndTrim(String input, String delimiter){
+        return Arrays.stream(input.split(delimiter))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
