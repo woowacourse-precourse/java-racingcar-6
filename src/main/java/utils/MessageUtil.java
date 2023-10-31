5afedb6b451652faa80276fc925d7dbe424ceecb
Dowoon <woodw920@gmail.com>
2023-10-31 18:26:13 +0900

    간단한 메세지 메소드빼기

---
+++ src/main/java/utils/MessageUtil.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.util.Map;
+
+public class MessageUtil {
+    public void gameStart() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void tryGame() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void runGame() {
+        System.out.println("실행 결과");
+    }
+}
