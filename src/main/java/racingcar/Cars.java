ec22a02a018bce9fb861d6c0762a203c0678a30f
hamings <wjdgpal9630@gmail.com>
2023-10-31 19:53:02 +0900

    Fix: 이름 중복 예외 기능 수정

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -14,17 +14,18 @@ public class Cars {
             for (int i = 0; i < cars.length; i++) {
                 cars[i] = new Car(carNames[i]);
             }
+        }else {
+            throw new IllegalArgumentException("이름이 중복되었습니다.");
         }
-        throw new IllegalArgumentException("이름이 중복되었습니다.");
     }
 
     public boolean existSameName(String[] carNames) {
         Set<String> names = new HashSet<>();
         for (int i = 0; i < carNames.length; i++) {
-            if (names.contains(carNames[i])) return false;
+            if (names.contains(carNames[i])) return true;
             names.add(carNames[i]);
         }
-        return true;
+        return false;
     }
 
     public void moveResult(int count) {
