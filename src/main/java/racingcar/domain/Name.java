2801950c47875da069255781e7288c8fd15013ea
elyudwo <dudqk9696@naver.com>
2023-10-31 23:14:47 +0900

    [Feat] : Name 객체 유효성 검사 기능 구현
    1. 자동차 이름 길이 1~5 사이가 아닐 경우 예외 발생
    2. 자동차 이름 비어있을 경우 예외 발생

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,12 +1,33 @@
 package racingcar.domain;
 
 
+import static racingcar.util.MagicNumber.MAX_NAME_LENGTH;
+import static racingcar.util.MagicNumber.MIN_NAME_LENGTH;
+
 public class Name {
 
     private final String name;
 
     public Name(String name) {
+        validateCarName(name);
         this.name = name;
+    }
+
+    private void validateCarName(String name) {
+        validateCarNameEmptyOrNull(name);
+        validateCarNameLength(name);
+    }
+
+    public void validateCarNameEmptyOrNull(String carName) {
+        if (carName == null || carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있으면 안됩니다.");
+        }
+    }
+
+    public void validateCarNameLength(String carName) {
+        if (name.length() < MIN_NAME_LENGTH.getNumber() || name.length() > MAX_NAME_LENGTH.getNumber()) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 1에서 5 사이여야 합니다.");
+        }
     }
 
     public String getValue() {
