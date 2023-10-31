9e6a84a017d03eb29517c2b02739f5d6843c0c40
donghyun <kdhdh001@naver.com>
2023-10-31 23:45:48 +0900

    feat: printWinners 메서드 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -12,4 +12,17 @@ public class ResultView {
         }
         System.out.println();
     }
+
+    public static void printWinners(List<Car> winners) {
+        StringBuilder winnerNames = new StringBuilder();
+        for (int i = 0; i < winners.size(); i++) {
+            Car winner = winners.get(i);
+            String winnerName = winner.getName();
+            winnerNames.append(winnerName);
+            if (i != winners.size() - 1) {
+                winnerNames.append(", ");
+            }
+        }
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
