19e1e66dd96fd03ac40f240ebe09bde9c8ad191f
oiler831 <qudwls831@gmail.com>
2023-10-31 15:09:35 +0900

    InputValue의 클래스 분리
    값을 입력하는 메서드와 게임을 하는 메소드는 분리할 필요있다 생각하여 InputValue 클래스로 분리, 추가로 checkCar 메소드가 이름이 명확하지 않다고 생각하여 checkCarStatus로 변경.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -12,80 +12,32 @@ public class RacingGame {
         throw new IllegalArgumentException();
     }
     public static void startGame() {
-        inputCarName();
-        inputTryNumber();
+        InputValue.writeInputValue();
+        carName = InputValue.carName;
+        tryNumber = InputValue.tryNumber;
         playGame();
-    }
-    private static void inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNameLine = Console.readLine();
-        isWrongInput(isNull(carNameLine));
-        divideCarNameLine(carNameLine);
-        isWrongInput(isCarNameOverFive());
-    }
-    private static void divideCarNameLine(String carNameLine) {
-        carName = carNameLine.split(",");
-    }
-    private static boolean isCarNameOverFive(){
-        for (String name : carName) {
-            if (name.length() > 5) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private static boolean isNull(String input) {
-        return input.isEmpty();
-    }
-    private static void isWrongInput(boolean inputResult) {
-        if (inputResult) {
-            throw new IllegalArgumentException();
-        }
-    }
-    private static void inputTryNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String stringNumber = Console.readLine();
-        System.out.println();
-        isCorrectNumber(stringNumber);
-        tryNumber = Integer.parseInt(stringNumber);
-    }
-    private static void isCorrectNumber(String stringNumber) {
-        isWrongInput(isNotNumber(stringNumber));
-        isWrongInput(isNotInRange(stringNumber));
-    }
-    private static boolean isNotNumber(String stringNumber) {
-        try {
-            Integer.parseInt(stringNumber);
-            return false;
-        }
-        catch (NumberFormatException e) {
-            return true;
-        }
-    }
-    private static boolean isNotInRange(String stringNumber) {
-        return (Integer.parseInt(stringNumber) <= 0);
+        findWinner();
     }
     private static void playGame() {
         distance = new int[carName.length];
         System.out.println("실행 결과");
         while (tryNumber > 0) {
-            checkCar();
+            checkCarStatus();
             tryNumber--;
         }
-        findWinner();
     }
-    private static void checkCar() {
+    private static void checkCarStatus() {
         for (int i = 0; i < carName.length; i++) {
             runCar(i);
             printRacingSequence(i);
         }
         System.out.println();
     }
-    private static boolean isRandomValueOverFour() {
-        return Randoms.pickNumberInRange(0,9) >= 4;
-    }
     private static void runCar(int number) {
         distance[number] += Boolean.compare(isRandomValueOverFour(), false);
+    }
+    private static boolean isRandomValueOverFour() {
+        return Randoms.pickNumberInRange(0,9) >= 4;
     }
     private static void printRacingSequence(int number) {
         String sequence = carName[number] + " : " + "-".repeat(distance[number]);
@@ -117,5 +69,68 @@ public class RacingGame {
         if (winner.charAt(winner.length() - 1) != ' '){
             winner.append(", ");
         }
+    }
+}
+
+class InputValue {
+    public static String[] carName;
+    public static int tryNumber;
+
+    private InputValue() {
+        throw new IllegalArgumentException();
+    }
+    public static void writeInputValue() {
+        inputCarName();
+        inputTryNumber();
+    }
+
+    private static void inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameLine = Console.readLine();
+        isWrongInput(isNull(carNameLine));
+        divideCarNameLine(carNameLine);
+        isWrongInput(isCarNameOverFive());
+    }
+    private static void isWrongInput(boolean inputResult) {
+        if (inputResult) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private static boolean isNull(String input) {
+        return input.isEmpty();
+    }
+    private static void divideCarNameLine(String carNameLine) {
+        carName = carNameLine.split(",");
+    }
+    private static boolean isCarNameOverFive() {
+        for (String name : carName) {
+            if (name.length() > 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static void inputTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String stringNumber = Console.readLine();
+        System.out.println();
+        isCorrectNumber(stringNumber);
+        tryNumber = Integer.parseInt(stringNumber);
+    }
+    private static void isCorrectNumber(String stringNumber) {
+        isWrongInput(isNotNumber(stringNumber));
+        isWrongInput(isNotInRange(stringNumber));
+    }
+    private static boolean isNotNumber(String stringNumber) {
+        try {
+            Integer.parseInt(stringNumber);
+            return false;
+        }
+        catch (NumberFormatException e) {
+            return true;
+        }
+    }
+    private static boolean isNotInRange(String stringNumber) {
+        return (Integer.parseInt(stringNumber) <= 0);
     }
 }
