1b20abc86a1e81a2ea947bcbd58025ffd9439aef
tam31 <wxogud3377@naver.com>
2023-10-31 22:10:08 +0900

    Feat(InputValidation) : 자동차 이름 중복 검사 기능 구현 Docs : 자동차 이름 중복 검사 완료체크

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -1,8 +1,12 @@
 package racingcar.validation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidation {
     private static final String IS_NULL_CAR_NAME_ERROR_MESSAGE = "입력하지 않았습니다.";
     private static final String IS_NOT_RULE_NAME_ERROR_MESSAGE = "규칙에 맞지 않습니다";
+    private static final String IS_DUPLICATE_NAME = "중복이 발생했습니다";
     private static final String IS_NOT_RANGE_NAME_ERROR_MESSAGE = "5자리 이하가 아닙니다.";
     private static final String IS_NOT_NUMBER_ERROR_MESSAGE = "숫자가 아닙니다.";
     private static final String IS_NOT_NUMBER_RANGE_ERROR_MESSAGE = "0보다 작을 수는 없습니다";
@@ -12,6 +16,7 @@ public class InputValidation {
     public String[] checkedInputValidation(String carNames) {
         isNullText(carNames);
         isNotRule(carNames);
+        isDuplicationNames(carNames);
         return collectCarNames(carNames);
     }
 
@@ -32,6 +37,23 @@ public class InputValidation {
         if (carNames.charAt(0) == ' ' || carNames.charAt(0) == ',') {
             throw new IllegalArgumentException(IS_NOT_RULE_NAME_ERROR_MESSAGE);
         }
+    }
+
+    private void isDuplicationNames(String carNames) {
+        String[] cars = carNames.split(",");
+        Set<String> collectNames = new HashSet<>();
+        collectNames = putCollectNames(cars, collectNames);
+
+        if (collectNames.size() != cars.length) {
+            throw new IllegalArgumentException(IS_DUPLICATE_NAME);
+        }
+    }
+
+    private Set<String> putCollectNames(String[] cars, Set<String> collectNames) {
+        for (String car : cars) {
+            collectNames.add(car);
+        }
+        return collectNames;
     }
 
     private String[] collectCarNames(String carNames) {
