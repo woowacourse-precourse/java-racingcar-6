0282e4437620404df96747aba278fcf91708c704
alscjf1329 <alscjf1329@gmail.com>
2023-10-31 21:24:34 +0900

    refactor(AlarmMessage): AlarmMessage enum 클래스 작성
    상수들을 따로 분류하여 가독성을 높이고 유지 보수에 좋게 하기 위해 enum 클래스로 변경했습니다.

--- src/main/java/racingcar/utils/RacingCarGameOutput.java
+++ src/main/java/racingcar/utils/RacingCarGameOutput.java
@@ -4,22 +4,6 @@ import java.util.List;
 import racingcar.dto.RacingCar;
 
 public class RacingCarGameOutput {
-    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    public static final String MOVE_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-    public static final String BEFORE_GAME_MESSAGE = "실행 결과";
-
-    public static void printCarNameInputMessage(String message) {
-        System.out.println(message);
-    }
-
-    public static void printMoveCountInputMessage(String message) {
-        System.out.println(message);
-    }
-
-    public static void printBeforeGameProgress(String message) {
-        System.out.println(message);
-    }
-
     public static void printGameProgress(List<RacingCar> carList) {
         StringBuilder sb = new StringBuilder();
         for (RacingCar car : carList) {
