05aeda589f1584165f9783861e4ec5e65b3fbdc5
i3kae <snoger1234@gmail.com>
2023-10-31 11:30:38 +0900

    test : 우승자 1명일 경우, 전체가 우승일 경우 테스트 추가

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 public class GameTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
     @Test
     void 자동차_객체_생성_테스트(){
         Racingcar car = new Racingcar("TEST");
@@ -19,8 +22,25 @@ public class GameTest extends NsTest {
     }
 
     @Test
-    void 정수_문자열_무결성_검사_테스트(){
+    void 우승자_1명일_경우_테스트(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123,abc,ABC", "1");
+                    assertThat(output()).contains("123 : -", "abc : ", "ABC : ", "최종 우승자 : 123");
+                },
+                MOVING_FORWARD, STOP, STOP
+        );
+    }
 
+    @Test
+    void 우승자가_전체일_경우_테스트(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123,abc,ABC", "1");
+                    assertThat(output()).contains("123 : -", "abc : -", "ABC : -", "최종 우승자 : 123, abc, ABC");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
     }
     @Override
     public void runMain() {
