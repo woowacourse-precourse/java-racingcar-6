ab1d0d8cf3366c0c1be984461f70556fbc54527a
gillyongs <khs20010327@gmail.com>
2023-11-01 08:59:10 +0900

    feat: input view 생성

--- src/main/java/racingcar/Service.java
+++ src/main/java/racingcar/Service.java
@@ -1,6 +1,15 @@
 package racingcar;
-
+import racingcar.view.Input;
 public class Service {
+    Input inputView = new Input();
     public void userInput() {
+        getCarNames();
+        //getAttemptNumber();
     }
+
+    private void getCarNames(){
+        String carNames = inputView.getCarInput();
+        System.out.println(carNames);
+    }
+
 }
