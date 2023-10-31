18ca0e5efdcf0b19b52417aa56c640d2fc52b835
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-10-31 13:21:58 +0900

    fix: 시도 횟수 입력 문구 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,7 +6,7 @@ import racingcar.utils.InputConvertor;
 public class InputView {
     private static final String START_COMMAND = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    private static final String NUMBER_OF_TRIES_COMMAND = "시도할 회수는 몇회인가요?";
+    private static final String NUMBER_OF_TRIES_COMMAND = "시도할 횟수는 몇회인가요?";
 
     public static String[] startGameCommand() {
         System.out.println(START_COMMAND);
