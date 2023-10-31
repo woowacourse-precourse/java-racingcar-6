7f1a2ae7f87361d31b8503b59589dc45a910e1c8
Seyeon <wjdtpdus828@naver.com>
2023-10-31 21:55:48 +0900

    feat(Validator): 자동차 이름이 중복될 경우 오류를 발생시키는 validateNameDuplication 메소드 구현

--- src/main/java/racingcar/service/Validator.java
+++ src/main/java/racingcar/service/Validator.java
@@ -8,6 +8,7 @@ public class Validator {
     TypeConverter typeConverter = new TypeConverter();
     private static final String NAME_LENGTH_ERROR_MESSAGE = "이름의 길이가 5보다 큽니다.";
     private static final String NAME_SPACE_ERROR_MESSAGE = "이름에 공백이 존재합니다.";
+    private static final String NAME_DUPLICATE_ERROR_MESSAGE = "중복된 이름이 존재합니다.";
     public void validateCarNames(List<String> input){
         validateNameLength(input);
         validateNameSpace(input);
@@ -25,6 +26,15 @@ public class Validator {
             if(carName.contains(" ")){
                 throw new IllegalArgumentException(NAME_SPACE_ERROR_MESSAGE);
             }
+        }
+    }
+    public void validateNameDuplication(List<String> input){
+        Set<String> set = new HashSet<>();
+        for(String carName : input){
+            set.add(carName);
+        }
+        if(set.size() != input.size()){
+            throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
