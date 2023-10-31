b2fb47216fec1a64ef219fa3b1a85e2ac028b566
junest66 <chlwndks333@gmail.com>
2023-10-31 19:26:58 +0900

    feat(AttemptCountValidator): 시도 횟수 최소 입력 검증 기능 추가

--- src/main/java/racingcar/validator/AttemptCountValidator.java
+++ src/main/java/racingcar/validator/AttemptCountValidator.java
@@ -2,13 +2,22 @@ package racingcar.validator;
 
 public class AttemptCountValidator {
 
-    private static final String ATTEMPT_COUNT_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private static final String INVALID_INPUT_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private static final String MIN_ATTEMPT_COUNT_ERROR_MESSAGE = "1이상을 입력해주세요";
 
     public static int validateAttemptCount(String prompt) {
         try {
-            return Integer.parseInt(prompt);
+            int attemptCount = Integer.parseInt(prompt);
+            checkRange(attemptCount);
+            return attemptCount;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ATTEMPT_COUNT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    private static void checkRange(int attemptCount) {
+        if (attemptCount < 1) {
+            throw new IllegalArgumentException(MIN_ATTEMPT_COUNT_ERROR_MESSAGE);
         }
     }
 }
