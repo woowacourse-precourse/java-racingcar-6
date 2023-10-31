735f467649934845e6f56a31f891c1cf33dfa9a8
jun <ggprgrkjh@naver.com>
2023-10-31 17:51:22 +0900

    feat(cargamecontroller): GAME_END_NUMBER 상수 추가

--- src/main/java/racingcar/controller/CarGameController.java
+++ src/main/java/racingcar/controller/CarGameController.java
@@ -11,7 +11,7 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class CarGameController {
-
+    private final int GAME_END_NUMBER = 0;
     private Game game;
     private Cars cars;
     private CarNames carNames;
@@ -38,7 +38,7 @@ public class CarGameController {
     public void printResult(int tryNumber) {
         List<Car> movedCars = new ArrayList<>();
         OutputView.printResultStartMessage();
-        while (tryNumber-- > 0) {
+        while (tryNumber-- > GAME_END_NUMBER) {
             movedCars = cars.moveAll();
             OutputView.printGameResultMessage(movedCars);
         }
