b52bfb59627aab74d552a69806c0e72ac8b16517
jaeyoungjang2 <lion85488@gmail.com>
2023-10-31 21:16:34 +0900

    test: 랜덤 숫자에 의한 자동차 이동 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
@@ -11,7 +12,23 @@ class CarTest {
     @Test
     @DisplayName("자동차의 이름이 5글자 이상이면 에러가 발생합니다.")
     public void CarNameTest() {
-        Assertions.assertThatThrownBy(() -> new Car("abcde"))
+        assertThatThrownBy(() -> new Car("abcde"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자가 4이상일 때는 자동차가 한 칸 앞으로 전진한다.")
+    public void randomMoveTest() {
+        Car car = new Car("jay");
+        car.go(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자가 3이하일 때는 자동차가 이동하지 않는다.")
+    public void randomMoveTest2() {
+        Car car = new Car("jay");
+        car.go(3);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
