7aaaedea43737d9b6ac4dc3e9ec5a5328357f142
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-31 19:26:45 +0900

    TEST: PlayResult 테스트 - getResult: 실행 결과 출력하는 메소드로 랜덤 숫자를 고정하여 그 숫자에 따라 출력이 옳바르게 되는지 테스트

---
+++ src/test/java/mytest/PlayResultTest.java
@@ -0,0 +1,33 @@
+package mytest;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+import racingcar.RacingInfo;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayResultTest extends NsTest  {
+    final int GO = 4;
+    final int STOP = 3;
+
+    @Test
+    void getResult_실행결과_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("minji,jiji", "2");
+                    assertThat(output()).contains("minji : -", "jiji : ", "minji : --", "jiji : -");
+                },
+                GO, STOP, GO, GO
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
