9c3014c1dd8945c1c24a96ddf808980b68b9005d
SeYoE <onetuks@naver.com>
2023-11-01 00:50:46 +0900

    test: Car 전진 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -4,10 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.error.ErrorMessage.LONG_CAR_NAME;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+
+    private Car danaka;
+    private int expectForwardCount;
+
+    @BeforeEach
+    void setUp() {
+        expectForwardCount = 3;
+        danaka = new Car("다나카", expectForwardCount);
+    }
 
     @Test
     @DisplayName("5글자 이하인 이름으로 자동차 객체를 생성한다.")
@@ -38,16 +48,30 @@ class CarTest {
     @DisplayName("4 이상인 값일 때 자동차가 전진한다.")
     void forward_GreaterThanFour_Forward() {
         // Given
-        Car car = new Car("다나카");
         int value = 4;
 
         // When
-        car.forward(value);
+        danaka.forward(value);
 
         // Then
-        int result = car.getForwardCount();
+        int result = danaka.getForwardCount();
 
-        assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo(expectForwardCount + 1);
+    }
+
+    @Test
+    @DisplayName("4 미만인 값일 때 자동차는 멈춘다.")
+    void forward_LessThanFour_Stop() {
+        // Given
+        int value = 3;
+
+        // When
+        danaka.forward(value);
+
+        // Then
+        int result = danaka.getForwardCount();
+
+        assertThat(result).isEqualTo(expectForwardCount);
     }
 
 }
