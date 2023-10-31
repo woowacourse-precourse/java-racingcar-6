3f3427739222d70b7a34f6a4c7b0d2bba26ce760
morenow <jjang9877@gmail.com>
2023-11-01 03:23:17 +0900

    fix: Car 클래스의 생성자에 위치값 필요없도록 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -8,9 +8,9 @@ public class Car {
     private final String name;
     private int position;
 
-    public Car(String name, int position) {
+    public Car(String name) {
         this.name = name;
-        this.position = position;
+        this.position = 0;
         this.id = carNum++;
     }
 }
