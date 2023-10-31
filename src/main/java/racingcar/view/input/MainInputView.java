4d15138f427aa32cd19537d001fa12e118f9a540
gmelon <hsh1769@naver.com>
2023-10-31 20:38:48 +0900

    feat: MainInputView에 시도 횟수를 입력받는 로직 구현

--- src/main/java/racingcar/view/input/MainInputView.java
+++ src/main/java/racingcar/view/input/MainInputView.java
@@ -3,14 +3,38 @@ package racingcar.view.input;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class MainInputView {
     private static final String CARNAME_SPLIT_DELIMETER = ",";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+    private static final String WHITESPACES_REGEX = "\\s+";
+    private static final String BLANK_STRING = "";
 
     private MainInputView() {
     }
 
     public static List<String> readCarNames() {
-        return Arrays.asList(Console.readLine().split(CARNAME_SPLIT_DELIMETER));
+        String input = readTrimmedLine();
+
+        return Arrays.asList(input.split(CARNAME_SPLIT_DELIMETER));
+    }
+
+    public static int readPlayCount() {
+        String input = readTrimmedLine();
+
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다.");
+        }
+
+        return Integer.parseInt(input);
+    }
+
+    private static String readTrimmedLine() {
+        return trimmedString(Console.readLine());
+    }
+
+    private static String trimmedString(String rawString) {
+        return rawString.replaceAll(WHITESPACES_REGEX, BLANK_STRING);
     }
 }
