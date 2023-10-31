68306c6b7c9cf5704676cd9f305d46baf3a87bdf
sora0319 <sojeoung536985@gmail.com>
2023-10-31 20:52:22 +0900

    feat (Car.getName) : 차 이름을 전달하는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -65,6 +65,17 @@ class TotalCar {
 }
 
 class Car {
+    private String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 
 }
 
