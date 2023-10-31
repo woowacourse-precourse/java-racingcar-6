49bc084c649a84bfc0b191e8eac02e06ba1df531
sinyebin <dpqls0625@naver.com>
2023-11-01 00:12:56 +0900

    test:레이싱카 최대거리 구하기

--- src/test/java/racingcar/service/RacingLogicTest.java
+++ src/test/java/racingcar/service/RacingLogicTest.java
@@ -2,8 +2,10 @@ package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.vo.RacingCar;
+import racingcar.vo.RacingCars;
 
 class RacingLogicTest {
 
@@ -25,6 +27,28 @@ class RacingLogicTest {
         RacingCar racingCar = new RacingCar("car");
         RacingLogic.isPlusDistance(0, racingCar);
         assertThat(racingCar.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    void 레이싱카_최대_거리_여러명일때() {
+        RacingCars racingCars = new RacingCars(List.of("car1", "car2"));
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        for (RacingCar car : racingCarList) {
+            car.setForwardDistance(5);
+        }
+        int maxDistance = RacingLogic.getMaxDistance(racingCars);
+        assertThat(maxDistance).isEqualTo(5);
+
+    }
+
+    @Test
+    void 레이싱카_최대_거리_한명일때() {
+        RacingCars racingCars = new RacingCars(List.of("car1", "car2"));
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        racingCarList.get(0).setForwardDistance(3);
+        racingCarList.get(1).setForwardDistance(5);
+        int maxDistance = RacingLogic.getMaxDistance(racingCars);
+        assertThat(maxDistance).isEqualTo(5);
     }
 
 }
