3a854d21bf34b3cc0af0a0dc6451d84e83f6bc5e
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-31 23:22:18 +0900

    feat ($Validation) : 차 이름이 5자 이하인지 확인하는 함수 생성

--- src/main/java/racingcar/utils/Validation.java
+++ src/main/java/racingcar/utils/Validation.java
@@ -1,5 +1,7 @@
 package racingcar.utils;
 
+import java.util.List;
+
 public class Validation {
     public Integer validTryCount(Integer tryCount){
         if (tryCount < 1){
@@ -7,4 +9,14 @@ public class Validation {
         }
         return tryCount;
     }
+    public List<String> validateCarNames(List<String> carNames) {
+        boolean fiveUnder = carNames.stream()
+                .anyMatch(name ->name.length() > 6);
+
+        if (fiveUnder) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+        return carNames;
+    }
+
 }
