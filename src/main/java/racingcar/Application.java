4e469730564ee7fabdca4ccf08ff139cea766fa8
foreverfall123 <foreverfall@naver.com>
2023-10-31 23:22:51 +0900

    feat : create setting function
    - create car name list function
    - create game round function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static Map<String, Integer> getCarName(){
+        Map<String, Integer> result = new HashMap<>();
+
+        String carInput = Console.readLine();
+        for(String name : carInput.split(",")) {
+            result.put(name, 0);
+        }
+
+        return result;
+    }
+
+    public static int getGameRound(){
+        String input = Console.readLine();
+
+        return Integer.parseInt(input);
     }
 }
