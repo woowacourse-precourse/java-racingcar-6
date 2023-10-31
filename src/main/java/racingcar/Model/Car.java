a1c90a1da113cf897d86ad7ddbd19c27120cfdb7
SSUHYUNKIM <yellosuhyun@naver.com>
2023-10-31 11:47:10 +0900

    feat: Car 모델 클래스 생성

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,4 +1,19 @@
 package racingcar.Model;
 
 public class Car {
+    String name;
+    String position;
+
+    public Car(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
 }
