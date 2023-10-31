052f1e64d0342be8a82414153ac0e300b9b1b505
InJun2 <os2864@naver.com>
2023-10-31 14:05:46 +0900

    :sparkles: 문자열 조작 로직 분리

--- src/main/java/racingcar/service/TextProcessor.java
+++ src/main/java/racingcar/service/TextProcessor.java
@@ -1,9 +1,21 @@
 package racingcar.service;
 
 import racingcar.exception.ExceptionMessage;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.List;
 
 public class TextProcessor {
-    private StringBuilder builder;
+    private final static String SPLIT = ",";
+    private final static String SEPARATOR = " : ";
+    private final static char CAR_DISTANCE = '-';
+    private final static String DELIMITER = ", ";
+    private final static String NEW_LINE = "\n";
+
+    public String[] splitCarNames(String str) {
+        return str.split(SPLIT);
+    }
 
     public int parseTryNumber(String str) {
         try {
@@ -12,6 +24,27 @@ public class TextProcessor {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_INTEGER_FORMAT
                     .getErrorMessage());
         }
+    }
+
+    public String carsDistanceAsString(Cars cars) {
+        StringBuilder builder = new StringBuilder();
+        for (Car car : cars.getCars()) {
+            builder.append(car.getName())
+                    .append(SEPARATOR)
+                    .append(displayDistance(car.getDistance()))
+                    .append(NEW_LINE);
+        }
+
+        return builder.toString();
+    }
+
+    public String displayDistance(int distance) {
+        return String.valueOf(CAR_DISTANCE)
+                .repeat(Math.max(0, distance));
+    }
+
+    public String joinText(List<String> winnerNames) {
+        return String.join(DELIMITER, winnerNames);
     }
 
 }
