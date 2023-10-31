21a377a48cdf2df409b8c29d24dfccd6ed9dc1bc
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-01 00:10:36 +0900

    feat(RacingGame): 이동횟수 n만큼 자동차를 전진하거나 멈추는 기능
    이동횟수만큼 이동하는 기능
    - 4이상이면 전진
    - 4미만이면 멈춤

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -8,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingGame {
+    PrintStream printStream = System.out;
     List<Car> cars;
     int moveCount;
 
@@ -44,7 +46,35 @@ public class RacingGame {
         this.moveCount = Integer.parseInt(moveCount);
     }
 
+    public void start() {
+        for (int i = 0; i < this.moveCount; i++) {
+            this.run();
+        }
+    }
     private int createRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    private String getMovingCommand() {
+        int randomNumber = this.createRandomNumber();
+
+        if (randomNumber >= 4) {
+            return "FORWARD";
+        }
+
+        return "STOP";
+    }
+
+    private void run() {
+        for (int i = 0; i < this.cars.size(); i++) {
+            String movingCommand = getMovingCommand();
+            Car car = this.cars.get(i);
+
+            if (movingCommand == "FORWARD") {
+                int movingDistance = car.getMovingDistance();
+                
+                car.setMovingDistance(movingDistance + 1);
+            }
+        }
     }
 }
