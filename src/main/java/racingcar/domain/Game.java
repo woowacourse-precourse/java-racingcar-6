175951fbccc0a8b4b1a56626408840631e958e7c
ddangme <ssilver121@nav>
2023-10-31 20:14:53 +0900

    refactor: 자동차 이름 유효성 체크 코드 CarNameValidationService.java 파일로 분리

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,20 +1,16 @@
 package racingcar.domain;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Game {
-    private static int finalRaceCount;
-    private List<Car> carList;
+    private int finalRaceCount;
     private int currentRaceCount;
-
-    public Game() {}
+    private List<Car> carList;
 
     public void setCar(String carNameListString) {
-        List<String> carNameList = validationCarName(carNameListString);
-        setCarList(carNameList);
+        setCarList(Arrays.stream(carNameListString.split(",")).toList());
     }
 
     public void setFinalRaceCount(String finalRaceCount) {
@@ -30,13 +26,6 @@ public class Game {
         if (finalRaceCount < 1) {
             throw new IllegalArgumentException("1~2,147,483,647 사이의 값으로 입력해주세요.");
         }
-    }
-
-    private List<String> validationCarName(String carNameListString) {
-        checkCarNameListIsEmpty(carNameListString);
-        checkCarNameIsNull(carNameListString);
-        checkCarNameListSize(carNameListString);
-        return checkCarNameDuplicate(carNameListString);
     }
 
     private void setCarList(List<String> carNameList) {
@@ -57,47 +46,4 @@ public class Game {
         return finalRaceCount != currentRaceCount;
     }
 
-    private void checkCarNameListIsEmpty(String carNameListString) {
-        if (carNameListString.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력하지 않았습니다.");
-        }
-    }
-
-    private void checkCarNameListSize(String carNameListString) {
-        String[] carNames = carNameListString.split(",");
-
-        if (carNames.length <= 1) {
-            throw new IllegalArgumentException("경주할 자동차의 이름은 2개 이상 입력해야 합니다.");
-        }
-
-    }
-
-    private List<String> checkCarNameDuplicate(String carNameListString) {
-        String[] carNames = carNameListString.split(",");
-        List<String> carNameList = new ArrayList<>(Arrays.asList(carNames));
-        Set<String> carNameSet = new HashSet<>(carNameList);
-
-        if (carNameList.size() != carNameSet.size()) {
-            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
-        }
-
-        return carNameList;
-    }
-
-    private void checkCarNameIsNull(String carNameListString) {
-        String[] carNames = carNameListString.split(",");
-        int commaCount = 1;
-
-        Pattern pattern = Pattern.compile(",");
-        Matcher matcher = pattern.matcher(carNameListString);
-
-        while (matcher.find()) {
-            commaCount++;
-        }
-
-        if (carNames.length != commaCount) {
-            throw new IllegalArgumentException("이름이 없는 자동차가 존재합니다.");
-        }
-
-    }
 }
---
+++ src/main/java/racingcar/service/CarNameValidationService.java
@@ -0,0 +1,49 @@
+package racingcar.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CarNameValidationService {
+
+    public void carNameValidation(List<String> carNameList) {
+        listSizeCheck(carNameList);
+
+        for (String name : carNameList) {
+            textLengthCheck(name);
+            regexCheck(name);
+        }
+
+        duplicateCheck(carNameList);
+    }
+
+    private void listSizeCheck(List<String> carNameList) {
+        if (carNameList.size() < 2) {
+            throw new IllegalArgumentException("경주할 자동차의 이름은 2개 이상 입력해야 합니다.");
+        }
+    }
+
+    private void textLengthCheck(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름 글자수는 1~5자 사이로 입력해야합니다.");
+        }
+    }
+
+    private void regexCheck(String name) {
+        String pattern = "^[가-힣A-Za-z0-9]*$";
+
+        if (!name.matches(pattern)) {
+            throw new IllegalArgumentException("사용할 수 없는 자동차 이름이 존재합니다.");
+        }
+    }
+
+    private void duplicateCheck(List<String> carNameList) {
+        Set<String> carNameSet = new HashSet<>(carNameList);
+
+        if (carNameSet.size() != carNameList.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+        }
+    }
+
+
+}
