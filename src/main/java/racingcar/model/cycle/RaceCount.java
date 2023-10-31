db745a2000d602048a3bf5674d8b25b5578f6441
상원 최 <swchoi1@swm.ai>
2023-10-31 23:26:48 +0900

    refactor(자료형 변경) : 레이싱을 진행할 수 있는 숫자 범위 제한
    결과 출력 시 메모리 문제가 있기 때문에 최대 레이싱 진행 횟수를 Integer형의 Max값으로 제한

--- src/main/java/racingcar/model/cycle/RaceCount.java
+++ src/main/java/racingcar/model/cycle/RaceCount.java
@@ -1,20 +1,19 @@
 package racingcar.model.cycle;
 
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.LongStream;
+import java.util.stream.IntStream;
 import racingcar.model.car.Car;
-import racingcar.model.car.CarManager;
 import racingcar.utils.validator.RaceCountValidator;
 import racingcar.utils.validator.Validator;
 
 public class RaceCount {
 
-    private final static Long START_COUNT = 0L;
+    private final static Integer START_COUNT = 0;
 
-    private final Long count;
+    private final Integer count;
 
     public RaceCount(final String count) {
         this(count, new RaceCountValidator());
@@ -22,19 +21,43 @@ public class RaceCount {
 
     public RaceCount(final String count, final Validator<String> validator) {
         validator.validate(count);
-        this.count = Long.parseLong(count);
+        this.count = Integer.parseInt(count);
     }
 
-    public RaceCount(final Long count) {
+    public RaceCount(final Integer count) {
         this.count = count;
     }
 
-    public void runActionInLoop(final Consumer<List<Car>> printSingleRaceResult , final Supplier<List<Car>> action) {
-        LongStream.range(START_COUNT, this.count)
+    public void runActionInLoop(final Consumer<List<Car>> printSingleRaceResult, final Supplier<List<Car>> action) {
+        IntStream.range(START_COUNT, this.count)
                 .forEach(i -> printSingleRaceResult.accept(action.get()));
     }
 
-    public Long getCount() {
+    public Integer getCount() {
         return count;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RaceCount raceCount = (RaceCount) o;
+        return Objects.equals(count, raceCount.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
+    }
+
+    @Override
+    public String toString() {
+        return "RaceCount{" +
+                "count=" + count +
+                '}';
     }
 }
