955e80f53059f1468f6aa37c719d613db07110d4
seondays <agseon9@gmail.com>
2023-10-31 20:09:11 +0900

    feat: 게임 진행, 게임 실행 결과 출력을 위한 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -6,6 +6,7 @@ import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.model.GameManager;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class GameController {
 
@@ -14,7 +15,9 @@ public class GameController {
     Cars cars;
 
     public void gameStart() {
+        OutputView.printInputCarNameMessage();
         cars = createCars(inputView.getCarsName());
+        OutputView.printInputMovingTryCountMessage();
         runRacing(inputView.getMovingTryCount(), cars);
     }
 
@@ -25,8 +28,10 @@ public class GameController {
     }
 
     public void runRacing(int round, Cars cars) {
+        OutputView.printGameRoundResultMessage();
         for (int i = 0; i < round; i++) {
             runRound(cars);
+            OutputView.printInfomation(cars.toString());
         }
     }
 
