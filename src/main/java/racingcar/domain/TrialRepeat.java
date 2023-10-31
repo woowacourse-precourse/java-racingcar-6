77066f497aaf2937ccf5b1adfba3faa65fc73bbe
lss0809 <ippnss@gmail.com>
2023-10-31 17:47:39 +0900

    refactor(TrialRepeat): 점수판을 새 해시테이블로 저장, 개별 스코어 출력하는 메서드 분리

--- src/main/java/racingcar/domain/TrialRepeat.java
+++ src/main/java/racingcar/domain/TrialRepeat.java
@@ -5,15 +5,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class TrialRepeat {
     public static final String RESULT_PRINT_TEXT = "실행 결과";
+    public static Hashtable<String, Integer> sb;
 
     public void trialExecution(Hashtable<String, Integer> scoreboard) {
-        for (String name : scoreboard.keySet()) {
-            if (goOrNot()) {
-                scoreboard.put(name, scoreboard.get(name) + 1);
-            }
-            System.out.println(name + " : " + "-".repeat(scoreboard.get(name)));
+        sb = scoreboard;
+        for (String name : sb.keySet()) {
+            System.out.println(personalScore(name, goOrNot()));
         }
         System.out.println();
+    }
+
+    private String personalScore(String name, boolean go) {
+        if (go) {
+            sb.put(name, sb.get(name) + 1);
+        }
+        return name + " : " + "-".repeat(sb.get(name));
     }
 
     private boolean goOrNot() {
