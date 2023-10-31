eef79c23081020cb1fd89c90503adab7d160e01e
gunsookim <ccpc4896@naver.com>
2023-10-31 17:37:52 +0900

    Fix: 상수 내용 수정
    잘못된 초기화로 인한 출력 오류 수정.
    NAME_CLASSIFICATION_CRITERIA 내용 수정.
    REQUEST_INPUT_NUMBER_OF_ROUNDS 내용 수정

--- src/main/java/racingcar/Vars.java
+++ src/main/java/racingcar/Vars.java
@@ -7,6 +7,6 @@ public class Vars {
     public static final int MOVE_CONDITION = 4;
 
     public static final String REQUEST_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String NAME_CLASSIFICATION_CRITERIA = "시도할 회수는 몇회인가요?";
-    public static final String REQUEST_INPUT_NUMBER_OF_ROUNDS = ",";
+    public static final String NAME_CLASSIFICATION_CRITERIA = ",";
+    public static final String REQUEST_INPUT_NUMBER_OF_ROUNDS = "시도할 회수는 몇회인가요?";
 }
