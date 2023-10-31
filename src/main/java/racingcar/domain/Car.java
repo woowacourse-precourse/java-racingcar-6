44c4f86ca30a7b57bd76b708e5b5c4f3a2558383
ghdeo <goldencorn7@naver.com>
2023-10-31 23:34:17 +0900

    feat: Add Car methods

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position;
@@ -9,4 +11,28 @@ public class Car {
         this.position = 0;
     }
 
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
+        }
+    }
+
+    public void printPosition() {
+        StringBuilder positionBuilder = new StringBuilder();
+
+        positionBuilder.append(name + " : ");
+        for (int i = 0; i < position; i++) {
+            positionBuilder.append("-");
+        }
+
+        System.out.println(positionBuilder.toString());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getPosition() {
+        return this.position;
+    }
 }
