6cb9bccc6e625eadfbc5dfad95afd79757370b35
yyeennyy <kye3314@gmail.com>
2023-10-31 14:27:26 +0900

    test: 스냅샷 콘솔출력 테스트 방법 변경

--- src/test/java/racingcar/Entity/SnapshotTest.java
+++ src/test/java/racingcar/Entity/SnapshotTest.java
@@ -1,7 +1,11 @@
 package racingcar.Entity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +13,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class SnapshotTest {
+
+    private static ByteArrayOutputStream outputMessage;
+
+    @BeforeEach
+    void setUpStreams() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
     @Test
     @DisplayName("스냅샷__정상출력")
     void printSnapshot_메서드_정상출력_테스트() {
@@ -21,7 +34,9 @@ class SnapshotTest {
         Snapshot snapshot = new Snapshot(tryCount);
         snapshot.setSnapshot(3, cars);
 
-        assertThat(snapshot.printSnapshot(3))
+        // 출력값 검증
+        snapshot.printSnapshot(3);
+        assertThat(outputMessage.toString())
                 .isEqualTo("car1 : -\ncar2 : \ncar3 : ---\n\n");
     }
 
@@ -30,6 +45,7 @@ class SnapshotTest {
     void printSnapshot_메서드_수행시_존재하지_않는_인덱스_파라미터_예외발생() {
         int tryCount = 1;
         Snapshot snapshot = new Snapshot(tryCount);
+
         assertThatThrownBy(() -> snapshot.printSnapshot(3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -39,7 +55,10 @@ class SnapshotTest {
     void printSnapshot_메서드_수행시_인덱스범위_내에서_스냅샷_미존재() {
         int tryCount = 1;
         Snapshot snapshot = new Snapshot(tryCount);
-        assertThat(snapshot.printSnapshot(1))
+
+        // 출력값 검증
+        snapshot.printSnapshot(1);
+        assertThat(outputMessage.toString())
                 .isEqualTo("no snapshot\n");
     }
 
@@ -55,7 +74,9 @@ class SnapshotTest {
         int raceNumber = 4;
         snapshot.setSnapshot(raceNumber, cars);
 
-        assertThat(snapshot.printWinner(raceNumber))
+        // 출력값 검증
+        snapshot.printWinner(raceNumber);
+        assertThat(outputMessage.toString())
                 .isEqualTo("최종 우승자 : car1, car2");
     }
 }
