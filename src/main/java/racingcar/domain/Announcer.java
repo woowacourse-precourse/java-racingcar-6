8388caeb2faafc3cb798097de34b1e248489e71f
mmalfoy <stpmalfoy@gmail.com>
2023-11-01 03:13:49 +0900

    rename : Outputs 클래스 to Announcer 클래스

--- src/main/java/racingcar/domain/Announcer.java
+++ src/main/java/racingcar/domain/Announcer.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 public class Outputs {
 
-    public static void resultMessage() {
+    public void resultMessage() {
         System.out.println();
         System.out.println("실행 결과");
     }
 
-    public static void result(List resultList) {
-        List<String> cars = InputDesk.getCars();
+    public void result(List resultList, List<String> cars) {
+
         for (int i = 0 ; i < cars.size() ; i++) {
             System.out.println(cars.get(i)+" : "+resultList.get(i));
         }
         System.out.println();
     }
 
-    public static void winner(List winners){
+    public void winner(List winners){
         System.out.print("최종 우승자 : ");
         System.out.println(String.join(", ",winners));
     }
