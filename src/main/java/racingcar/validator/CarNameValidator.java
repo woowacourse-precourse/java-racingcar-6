606ff94fb343db164eb81c94ed67dba9a176079a
노정완 <jeongwan.noh@gmail.com>
2023-10-31 20:49:08 +0900

    feat: 자동차 이름 길이 예외처리 로직 구현

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -2,8 +2,10 @@ package racingcar.validator;
 
 public class CarNameValidator {
 
-    public void checkCarNameLength() {
-
+    public void checkCarNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 5 이하로 설정 해주세요.");
+        }
     }
 
     public void checkDuplicatedCarName() {
