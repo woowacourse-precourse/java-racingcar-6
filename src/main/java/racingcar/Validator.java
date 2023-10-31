851b8f72e3fbbe9160a14be59091f856b06aec7e
K-KY <kyuyoungk@naver.com>
2023-11-01 06:10:32 +0900

    Refactor(Validator) : 상수화

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,17 +3,22 @@ package racingcar;
 import java.util.List;
 
 public class Validator {
+
+    private static final String CAR_NAME_BLANK_EXCEPTION = "[ERROR] 자동차의 이름은 공백을 입력 할 수 없습니다.";
+    private static final String CAR_NAME_LENGTH_EXCEPTION = "[ERROR] 자동차의 이름은 최대 5자까지 입력 가능합니다.";
+    private static final String NOT_NUMBER_EXCEPTION = "[ERROR] 숫자만 입력해주세요.";
+
     public static void validateBlank(String carName) {
         String ridBlank = carName.replace(" ", "");
         if (ridBlank.length() == 0) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 공백을 입력 할 수 없습니다.");
+            throw new IllegalArgumentException(CAR_NAME_BLANK_EXCEPTION);
         }
 
     }
 
     public static void validateLength(String carName) {
         if (carName.length() > 5) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 최대 5자까지 입력 가능합니다.");
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION);
         }
     }
 
@@ -28,7 +33,7 @@ public class Validator {
         if (trialNumber.matches("\\d+")) {
             return;
         }
-        throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
     }
 
 }
