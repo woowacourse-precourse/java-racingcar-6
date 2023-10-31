e24daf9873373faba890633137569da96a6fd23a
ikjo39 <chi4321@naver.com>
2023-10-31 14:01:17 +0900

    test: 자동차 위치 테스트
    - CarsPosition 생성
    - 각 경주별 결과 반환
    - 우승자 결과 요청(단독 우승자, 공동 우승자)

---
+++ src/test/java/racingcar/model/CarsPositionTest.java
@@ -0,0 +1,100 @@
+package racingcar.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.constant.StepConstant.MOVING_FORWARD;
+import static racingcar.constant.StepConstant.STOP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.util.StringFormatter;
+
+class CarsPositionTest {
+    @DisplayName("자동차 위치 생성 시 경주 자동차를 이용한다.")
+    @Test
+    void success_Create_Cars_Position() {
+        // given
+        RacingCars racingCars = createRacingCars();
+        Map<String, Integer> expected = racingCars.createInitPosition();
+
+        // when
+        CarsPosition result = CarsPosition.createPositionWithRacingCars(racingCars);
+
+        // then
+        assertThat(result).hasFieldOrPropertyWithValue("carsPosition", expected);
+    }
+
+    @DisplayName("각 자동차 별 경주 결과를 요청하면 경주를 진행 후 경주 결과를 반환한다.")
+    @Test
+    void success_Get_Each_Race_Result() {
+        // given
+        RacingCars racingCars = createRacingCars();
+        CarsPosition carsPosition = CarsPosition.createPositionWithRacingCars(racingCars);
+        RaceResultTexts expected = createRaceResults();
+
+        // when // then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    RaceResultTexts result = carsPosition.getEachRaceResult();
+                    assertThat(result).isEqualTo(expected);
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @DisplayName("우승자를 안내 문구를 요청한다.")
+    @Test
+    void success_Get_Winner_Sole() {
+        // given
+        RacingCars racingCars = createRacingCars();
+        CarsPosition carsPosition = CarsPosition.createPositionWithRacingCars(racingCars);
+        String expectedWinner = "apple";
+        String expected = StringFormatter.winnerFormat(expectedWinner);
+
+        // when // then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    carsPosition.getEachRaceResult();
+                    String result = carsPosition.getWinner();
+                    assertThat(result).isEqualTo(expected);
+                },
+                MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @DisplayName("우승자를 안내 문구를 요청한다. 여러 명일 경우 쉼표를 이용하여 구분한다.")
+    @Test
+    void success_Get_Winner_Multi() {
+        // given
+        RacingCars racingCars = createRacingCars();
+        CarsPosition carsPosition = CarsPosition.createPositionWithRacingCars(racingCars);
+        String expectedWinners = "apple, bear";
+        String expected = StringFormatter.winnerFormat(expectedWinners);
+
+        // when // then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    carsPosition.getEachRaceResult();
+                    String result = carsPosition.getWinner();
+                    assertThat(result).isEqualTo(expected);
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    private RaceResultTexts createRaceResults() {
+        List<String> list = new ArrayList<>();
+        list.add(StringFormatter.raceResultFormat("apple", "-"));
+        list.add(StringFormatter.raceResultFormat("bear", ""));
+        list.add(StringFormatter.raceResultFormat("cake", "-"));
+        return new RaceResultTexts(list);
+    }
+
+    private RacingCars createRacingCars() {
+        String input = "apple,bear,cake";
+        return RacingCars.createRacingCars(input);
+    }
+}
