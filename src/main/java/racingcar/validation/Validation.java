190cff447347c142f090fbfd69a30d86b53289c6
dustjq1004 <dustjq1005@naver.com>
2023-10-31 20:41:45 +0900

    yeonsup/feat : Validation 기능 추가 1. validateApplicantCars : 자동차 이름 검증  - hasSpace : 공백 검증  - hasSpace : 영문 검증 2. validateRaceCount : 경주 횟수 검증  - isNumberType : 정수 검증  - isNumberInRange : 범위 검증

--- src/main/java/racingcar/validation/Validation.java
+++ src/main/java/racingcar/validation/Validation.java
@@ -1,9 +1,46 @@
 package racingcar.validation;
 
 public class Validation {
+
+    public static boolean validateApplicantCars(String carNames) {
+        if (hasSpace(carNames)) {
+            throw new IllegalArgumentException("입력 값에 공백 존재");
+        }
+
+        if (!isEnglish(carNames)) {
+            throw new IllegalArgumentException("영문 외에 글자 존재");
+        }
+
+        return true;
+    }
+
+    private static boolean hasSpace(String carNames) {
+        return carNames.matches("\"^[\\\\s]*$\"");
+    }
+
+    private static boolean isEnglish(String carNames) {
+        return carNames.matches("^[a-zA-Z]*$");
+    }
+
+    public static boolean validateRaceCount(String raceCount) {
+        if (!isNumberType(raceCount)) {
+            throw new IllegalArgumentException("정수 아닌 다른 값 잘못 입력");
+        }
+
+        if (!isNumberInRange(Integer.parseInt(raceCount))) {
+            throw new IllegalArgumentException("random 값 범위 초과, 범위 : 0 ~ 9");
+        }
+
+        return true;
+    }
+
+    private static boolean isNumberType(String raceCount) {
+        return raceCount.matches("^[\\d]*$");
+    }
+
     public static boolean isNumberInRange(int random) {
         if (random < 0 || 9 < random) {
-            throw new IllegalArgumentException("random 값 범위 초과, 범위 : 0 ~ 9");
+            return false;
         }
         return true;
     }
