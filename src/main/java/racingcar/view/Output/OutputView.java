460172fc19c4767f9f959cc3efc888fcc9f2492e
yang <hjyang@epience.com>
2023-10-31 12:51:17 +0900

    🎯feat(view/OutputView) : 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/view/Output/OutputView.java
+++ src/main/java/racingcar/view/Output/OutputView.java
@@ -2,6 +2,7 @@ package racingcar.view.Output;
 
 public class OutputView {
     private final String RACE_REULT= "실행 결과";
+    private final String FINAL_WINNER = "최종 우승자 : ";
 
     // 결과 출력
     public void printResult() {
@@ -16,6 +17,11 @@ public class OutputView {
     // 시도 횟수 입력 값 출력 --> String 으로 받아도 상관 없긴 함.
     public void printTrialNumber(int number) {
         System.out.println(number);
+    }
+
+    // 최종 우승자 출력
+    public void printWinner(String winner) {
+        System.out.println(String.format("%s%s",FINAL_WINNER ,winner));
     }
 
 }
