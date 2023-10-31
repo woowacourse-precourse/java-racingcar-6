7e520ff4f4c153bcd23c22c1d7aeda69f9e03dca
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-10-31 17:01:06 +0900

    feat: 각 시도마다 실행 결과 출력 및 게임 종료 후 우승자 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class OutputView {
+
+	public static void printResultsMessage() {
+		System.out.println();
+		System.out.println("실행 결과");
+	}
+
+	public static void printCurrentRacingResults(List<String> players, List<Integer> results) {
+		for (int i = 0; i < players.size(); i++) {
+			System.out.printf("%s : ", players.get(i));
+			for(int j = 0 ; j < results.get(i); j ++){
+				System.out.print('-');
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printFinalRacingResults(List<String> winners) {
+		System.out.print("최종 우승자 : ");
+		for(int i = 0 ; i < winners.size(); i ++){
+			if(i != 0) System.out.print(", ");
+			System.out.print(winners.get(i));
+		}
+	}
+}
