470a8b54f70ee2c78a13fcff3c45610826cc30ff
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 11:06:42 +0900

    Feat: validataBlankForCarNameInput 구현
    주어진 문자열이 빈 문자열인경우(" ") IllegalArgumentException 발생

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -9,13 +9,16 @@ public class Validator {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final String WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE = "이름은 5자 이하여야 합니다";
     private static final String CAR_NAME_DUPLICATION_ERROR_MESSAGE = "이름이 중복되어선 안됩니다";
-
+    private static final String CAR_NAME_BLANK_ERROR_MESSAGE = "이름이 비어있으면 안 됩니다";
+    
     Parser parser = new Parser();
 
     public String validateCarNameInput(String userInput) {
         List<String> carNameList = parser.parseCarNameInput(userInput);
         validateLengthForCarNameInput(carNameList);
         validateDuplicateForCarNameInput(carNameList);
+        validateBlankForCarNameInput(carNameList);
+
         return userInput;
     }
 
@@ -32,6 +35,14 @@ public class Validator {
         set.addAll(carNameList);
         if (set.size() != carNameList.size()) {
             throw new IllegalArgumentException(CAR_NAME_DUPLICATION_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateBlankForCarNameInput(List<String> carNameList) {
+        for (String carName : carNameList) {
+            if (carName.isBlank()) {
+                throw new IllegalArgumentException(CAR_NAME_BLANK_ERROR_MESSAGE);
+            }
         }
     }
 }
