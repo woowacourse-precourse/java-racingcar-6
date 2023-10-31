d9bcfe637b83593fdf2b04d01ebf7d58deaa7694
KimSehoon <shggm2000@gmail.com>
2023-10-31 16:35:17 +0900

    fix: InputTest class 전체 실행 시 오류 해결
    - 전체 실행 시 오류 발생
    - 한 테스트 메소드를 실행 후 Console이 닫기지 않아 후에 실행하는 테스트 메소드에서 오류가 발생하는 것으로 판별
    - try-finally 문으로 Console.close해 해결

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -3,21 +3,27 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.Input;
 
-public class InputTest extends NsTest {
+public class InputTest {
     Input input = new Input();
 
     @Test
     @DisplayName("경주 게임에 참여할 자동차 이름 입력 테스트")
     void getCarNameList_test() {
-        command("pobi,woni,jun");
-        List<String> carNameListForTest = input.getCarNameList();
+        List<String> carNameListForTest;
+
+        try {
+            command("pobi,woni,jun");
+            carNameListForTest = input.getCarNameList();
+        } finally {
+            Console.close();
+        }
 
         assertEquals(3, carNameListForTest.size());
         assertThat(carNameListForTest).containsExactly("pobi", "woni", "jun");
@@ -25,9 +31,15 @@ public class InputTest extends NsTest {
 
     @Test
     @DisplayName("시도할 횟수 입력 테스트")
-    void getTryCount_test(){
-        command(String.valueOf(4));
-        int tryCountForTest = input.getTryCount();
+    void getTryCount_test() {
+        int tryCountForTest;
+
+        try {
+            command(String.valueOf(4));
+            tryCountForTest = input.getTryCount();
+        } finally {
+            Console.close();
+        }
 
         assertThat(tryCountForTest).isEqualTo(4);
     }
@@ -35,10 +47,5 @@ public class InputTest extends NsTest {
     private void command(final String... args) {
         final byte[] buf = String.join("\n", args).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
-    }
-
-    @Override
-    protected void runMain() {
-
     }
 }
