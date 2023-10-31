d7ce17080397475605b70eee8b2f47cdea25b27c
jinchiim <nellroll@syuin.ac.kr>
2023-10-31 21:02:29 +0900

    refactor: Cars List<Car>만 가지도록 수정
    Cars List<Car>만 가지도록 수정
    - 일급 컬렉션이 되도록 수정
    - 일부 validate 로직 Cars로 이동

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,22 +1,62 @@
 package racingcar.domain.car;
 
+import static racingcar.util.Validator.Validator.validateNameLength;
+import static racingcar.view.output.Output.printNewLine;
+import static racingcar.view.output.Output.printResultMessage;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.view.input.error.InputError;
+import racingcar.view.input.error.InputException;
+import racingcar.view.output.OutputMessage;
 
 public class Cars {
 
-    private final Car car;
+    private static final String CARS_ARE_EMPTY = "차가 존재하지 않습니다.";
+    private static final int MAXIMUM_FORWARD_NUMBER = 4;
 
-    private final List<Car> cars;
+    private final List<Car> cars = new ArrayList<>();
 
-    public Cars(Car car, List<Car> cars) {
-        this.car = car;
-        this.cars = cars;
+    public Cars(List<String> carNames) {
+        validateNameEmpty(carNames);
+        validateNameLength(carNames);
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
     }
 
-    public List<Car> changeToCar(List<String> carNames) {
-        for (String name : carNames) {
-            cars.add(car.of(name));
+    private void validateNameEmpty(List<String> names) {
+        if (names.size() <= 1) {
+            throw new InputException(InputError.NOT_EXCEED_MINIMUM_AMOUNT);
         }
-        return cars;
+    }
+
+    public List<String> findWinners() {
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException(CARS_ARE_EMPTY));
+
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .map(Car::getCarName)
+                .collect(Collectors.toList());
+    }
+
+    public void randomCarMove() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        for (Car car : cars) {
+            if (isCanForward(randomNumberGenerator.generateRandomNumber())) {
+                car.moveCar();
+            }
+            printResultMessage(OutputMessage.PROGRESS_RESULT, car.getCarName(), car.getDistance());
+        }
+        printNewLine();
+    }
+
+    private boolean isCanForward(int forwardNumber) {
+        return MAXIMUM_FORWARD_NUMBER <= forwardNumber;
     }
 }
