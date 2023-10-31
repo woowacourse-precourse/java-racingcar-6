c0e679defc8ca09a7f9e04aca87b881f544d6953
choiejae <ejae8319@gmail.com>
2023-10-31 21:59:16 +0900

    feat(RacingCars): RacingCar 를 저장하는 RacingCars 클래스 구현
    [예외처리]: 두 대 미만의 RacingCar 가 있는 경우 IllegalArgumentException
    [예외처리]: 같은 이름을 가진 RacingCar 가 있는 경우 IllegalArgumentException
    - RacingCars(RacingCars racingCarList): 각 라운드마다 레이싱 결과를 저장하기 위해 복사생성자 생성 기능
    - move(): Service에서 ForwardMethod 를 주입받아 각 RacingCar들간의 race 가 진행되는 기능
    - findWinners(): Race 의 우승자들을 찾는 기능

---
+++ src/main/java/racingcar/RacingCars.java
@@ -0,0 +1,95 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class RacingCars implements Iterable<RacingCar> {
+
+    private static final int REQUIRED_NUMBER_RACINGCAR = 2;
+    private final List<RacingCar> racingCars;
+
+    public RacingCars(List<RacingCar> racingCars) {
+        if (isNotEnoughCars(racingCars)) {
+            throw new IllegalArgumentException("경주를 위해 최소 두 대의 RacingCar 가 필요합니다.");
+        }
+        if (isDuplicateName(racingCars)) {
+            throw new IllegalArgumentException("중복된 이름을 가진 RacingCar 가 함께 경주할 수 없습니다.");
+        }
+        this.racingCars = racingCars;
+    }
+
+    public RacingCars(RacingCars racingCarList) {
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (RacingCar racingCar : racingCarList.racingCars) {
+            racingCars.add(new RacingCar(racingCar));
+        }
+        this.racingCars = racingCars;
+    }
+
+    public RacingCars move(ForwardMethod forwardMethod) {
+        List<RacingCar> relocatedRacingCars = new ArrayList<>();
+
+        for (RacingCar racingCar : racingCars) {
+            RacingCar relocatedRacingCar = new RacingCar(racingCar.move(forwardMethod));
+            relocatedRacingCars.add(relocatedRacingCar);
+        }
+        return new RacingCars(relocatedRacingCars);
+    }
+
+    public RacingCarWinners findWinners() {
+        int maxPosition = findMaxPosition();
+
+        removeLosers(maxPosition);
+        return new RacingCarWinners(racingCars);
+    }
+
+    private void removeLosers(int maxPosition) {
+        racingCars.removeIf(racingCar -> racingCar.getPosition() < maxPosition);
+    }
+
+    public int findMaxPosition() {
+        racingCars.sort(Collections.reverseOrder());
+
+        return racingCars.get(0)
+                .getPosition();
+    }
+
+    private boolean isNotEnoughCars(List<RacingCar> racingCars) {
+        return racingCars.size() < REQUIRED_NUMBER_RACINGCAR;
+    }
+
+    private boolean isDuplicateName(List<RacingCar> racingCars) {
+        Set<RacingCar> racingCarSet = new HashSet<>(racingCars);
+
+        return racingCarSet.size() != racingCars.size();
+    }
+
+    public void add(RacingCar racingCar) {
+        if (racingCars.contains(racingCar)) {
+            throw new IllegalArgumentException();
+        }
+        racingCars.add(racingCar);
+    }
+
+    public boolean contains(RacingCar racingCar) {
+        return racingCars.contains(racingCar);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        racingCars.forEach(racingCar -> stringBuilder.append(racingCar.toString())
+                .append("\n"));
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public Iterator<RacingCar> iterator() {
+        return racingCars.iterator();
+    }
+}
---
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -0,0 +1,124 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RacingCarsTest {
+
+    RacingCars racingCars;
+
+    @BeforeEach
+    void setUp() {
+        List<RacingCar> racingCarList = List.of(new RacingCar("one"), new RacingCar("two"), new RacingCar("three"));
+
+        racingCars = new RacingCars(racingCarList);
+    }
+
+    @Test
+    void RacingCars_리스트에_생성된_RacingCar_들이_존재함() {
+        // given
+
+        // when, then
+        assertTrue(racingCars.contains(new RacingCar("one")));
+        assertTrue(racingCars.contains(new RacingCar("two")));
+        assertTrue(racingCars.contains(new RacingCar("three")));
+    }
+
+    @Test
+    void 최소_두_대의_RacingCar_가_없으면_경주가_진행되지_않는다() {
+        // given
+        List<RacingCar> racingCarList = new ArrayList<>();
+
+        // when
+        racingCarList.add(new RacingCar("four"));
+
+        // then
+        Assertions.assertThatThrownBy(() -> new RacingCars(racingCarList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("경주를 위해 최소 두 대의 RacingCar 가 필요합니다.");
+    }
+
+    @Test
+    void 한_경기에_이름이_같은_RacingCar끼리_경주할_수_없다() {
+        // given
+        List<RacingCar> racingCarList = new ArrayList<>();
+
+        // when
+        racingCarList.add(new RacingCar("same"));
+        racingCarList.add(new RacingCar("same"));
+
+        // then
+        Assertions.assertThatThrownBy(() -> new RacingCars(racingCarList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름을 가진 RacingCar 가 함께 경주할 수 없습니다.");
+    }
+
+    @Test
+    void 깊은_복사된_객체는_서로_다르다() {
+        // given
+
+        // when
+        RacingCars deepCopyRacingCar = new RacingCars(racingCars);
+
+        // then
+        assertNotSame(racingCars, deepCopyRacingCar);
+    }
+
+    @Test
+    void RacingCars들의_초기_위치는_모두_0이다() {
+        // given
+
+        // when, then
+        for (RacingCar racingCar : racingCars) {
+            assertEquals(racingCar.getPosition(), 0);
+        }
+    }
+
+    @Test
+    void RacingCar들은_모두_move_될_수_있다() {
+        // given
+
+        // when
+        RacingCars relocatedRacingCars = racingCars.move(() -> true);
+
+        // then
+        for (RacingCar racingCar : relocatedRacingCars) {
+            assertEquals(racingCar.getPosition(), 1);
+        }
+    }
+
+    @Test
+    void 레이스의_결과로_우승한_RacingCar를_모두_찾을_수_있어야_한다() {
+        // given
+        List<RacingCar> racingCarList = moveOddRacingCars();
+
+        RacingCars relocatedRacingCars = new RacingCars(racingCarList);
+
+        // when
+        RacingCarWinners winners = relocatedRacingCars.findWinners();
+
+        // then
+        assertEquals(winners.get(0).name(), new Name("one"));
+        assertEquals(winners.get(1).name(), new Name("three"));
+    }
+
+    private List<RacingCar> moveOddRacingCars() {
+        boolean isMovable = true;
+        List<RacingCar> racingCarList = new ArrayList<>();
+
+        for (RacingCar racingCar : racingCars) {
+            boolean movableFlag = isMovable;
+
+            racingCarList.add(racingCar.move(() -> movableFlag));
+            isMovable = !movableFlag;
+        }
+        return racingCarList;
+    }
+}
