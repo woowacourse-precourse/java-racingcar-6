cc0e889be069d129fd2f148e51c7374db14aae3c
kimdeokyoung <dudejrss@naver.com>
2023-10-31 12:45:59 +0900

    * refactor: GameExecutor.getNames()와 getTrial() 책임을 InputInterface로 위임하기 (#6)
    * refactor: GameExecutor.getNames()와 InputInterface.getName()로 위임하기
    * refactor: GameExecutor.getTrials()를 InputInterface.getTrial()로 위임하기

--- src/main/java/racingcar/GameExecutor.java
+++ src/main/java/racingcar/GameExecutor.java
@@ -10,19 +10,9 @@ public class GameExecutor {
         this.out = new OutputInterface();
     }
 
-    private String[] getNames() {
-        out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return in.getNames();
-    }
-
-    private int getTrial(){
-        out.println("시도할 회수는 몇회인가요?");
-        return in.getTrial();
-    }
-
     public void run(GameRandom random) {
         try {
-            game = new RacingCarGame(getNames(), getTrial(), out, random);
+            game = new RacingCarGame(in.getNames(), in.getTrial(), out, random);
             game.run();
         } finally {
             in.close();
--- src/main/java/racingcar/InputInterface.java
+++ src/main/java/racingcar/InputInterface.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputInterface {
 
     public String[] getNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         if ("".equals(input)) {
             throw new IllegalArgumentException("자동차 이름을 입력하지 않았습니다.");
@@ -13,6 +14,7 @@ public class InputInterface {
     }
 
     public int getTrial() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String input = Console.readLine();
         try {
             return Integer.parseInt(input);
