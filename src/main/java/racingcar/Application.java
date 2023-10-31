31983b4c49d2f0519b36df21170be40e652b6be9
ppdoli123 <ppdoli123@naver.com>
2023-10-31 13:27:40 +0900

    create_car

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,58 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 생성
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        // 자동차 경주 게임 시작
+        RacingGame racingGame = new RacingGame(carNames);
+
     }
+}
+
+class RacingGame {
+    private final List<Car> cars;
+
+    public RacingGame(String[] carNames) {
+        validateCarNames(carNames);
+        cars = createCars(carNames);
+    }
+
+    private void validateCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
+            }
+        }
+    }
+
+    private List<Car> createCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+
+
+}
+
+class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
 }
