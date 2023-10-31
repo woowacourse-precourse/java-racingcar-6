37e0cc6d7a092fdffa908ce3a151e26ed7d6cac1
PARK9898 <wjddjs21@naver.com>
2023-10-31 19:15:27 +0900

    "feat: 우승자 출력 메서드 구현"

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,23 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.util.StringHandler;
+
 public class OutputView {
+
+    private static final String RESULT_PHRASE = "실행 결과";
+    private static final String WINNER_PHRASE = "최종 우승자 : ";
+
     public void printResultPhrase() {
-        System.out.println("실행 결과");
+        System.out.println(RESULT_PHRASE);
     }
+
+    public void printWinner(List<Car> cars) {
+        String winner = StringHandler.listToString(cars);
+        String result = String.format("%s%s",WINNER_PHRASE,winner);
+
+        System.out.println(result);
+    }
+
 }
