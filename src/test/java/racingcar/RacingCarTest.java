cb50643a34b79796d03c81101059dffbb2a0f03a
yim hyunwoo <hwlim@citylabs.co.kr>
2023-10-31 17:40:46 +0900

    test : 자동차 이름은 빈 문자열 일 수 없다 테스트 코드

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
@@ -23,6 +25,16 @@ public class RacingCarTest {
         assertThatThrownBy(()->{
             String [] carList = {"1234","1234"};
             new Cars(carList);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름은 빈 문자열 일 수 없다")
+    @ParameterizedTest
+    @ValueSource(strings ={""," ","   ","    "})
+    @Test
+    public void checkCarNameBlank(){
+        assertThatThrownBy(()->{
+            new Car(" ");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
