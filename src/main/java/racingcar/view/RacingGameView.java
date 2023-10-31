2b3f3989474f7476282e7ce5e94c1b06e1331bf8
Kim mingyu <minq0302@naver.com>
2023-10-31 21:28:56 +0900

    feat: add car name input prompt for the race

---
+++ src/main/java/racingcar/view/RacingGameView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+public class RacingGameView {
+
+    // 4. 자동차 이름을 입력해달라는 문구를 출력하는 기능
+    public void showCarNameInputMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+}
