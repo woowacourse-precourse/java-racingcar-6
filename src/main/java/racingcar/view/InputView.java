d7fb8a14e64161391602c92e82c6f7293fb302c6
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 14:59:19 +0900

    feat: add get trial num to input view

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class InputView {
 
     private final static String GET_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String GET_TRIAL_NUM = "시도할 회수는 몇회인가요?";
 
     public List<String> getCarName() {
         System.out.println(GET_CAR_NAME);
@@ -13,6 +14,12 @@ public class InputView {
         List<String> carNameList = stringToList(carNames);
 
         return carNameList;
+    }
+
+    public int getTrialNum() {
+        System.out.println(GET_TRIAL_NUM);
+        String trialNum = Console.readLine();
+        return Integer.parseInt(trialNum);
     }
 
     public List<String> stringToList(String str) {
