d6bc0d30828673f2f96346e593671fc0c4e6b660
jongmee <ra123221@gmail.com>
2023-10-31 13:56:57 +0900

    improve: 우승자를 구하는 메서드 성능 향상시키기
    - 기존에는 sort 메서드를 사용하여 정렬 후 정렬된 리스트의 첫 번째 값과 동일한 경주차를 구했다. 시간복잡도는 nlogn + n이므로 O(nlogn)이다.
    - 전체 경주차를 순회하며 최대 전진 횟수를 찾고 그 횟수를 가진 경주차를 찾도록 변경하였다. 시간복잡도는 n + n이므로 O(n)이다.

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,13 +2,11 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.CarRepository;
 import racingcar.dto.request.CarNamesDto;
 import racingcar.dto.response.WinnersDto;
-import racingcar.utility.MovementComparator;
 
 public class RacingService {
 
@@ -40,17 +38,26 @@ public class RacingService {
 
     public WinnersDto findWinners() {
         List<Car> carList = carRepository.findAll();
-        Collections.sort(carList, new MovementComparator());
+        Integer maximumScore = findMaximumScore(carList);
 
         List<Car> winners = new ArrayList<>();
-        Integer score = carList.get(0).getForwardCount();
-        for (int i = 0; i < carList.size(); i++) {
-            if (carList.get(i).getForwardCount() == score) {
-                winners.add(carList.get(i));
+        for (Car car: carList) {
+            if (car.getForwardCount() == maximumScore) {
+                winners.add(car);
             }
         }
 
         return new WinnersDto(winners);
+    }
+
+    private Integer findMaximumScore(List<Car> carList) {
+        Integer max = 0;
+        for(Car car: carList) {
+            if(max < car.getForwardCount()) {
+                max = car.getForwardCount();
+            }
+        }
+        return max;
     }
 
     public List<Car> findAllCars() {
