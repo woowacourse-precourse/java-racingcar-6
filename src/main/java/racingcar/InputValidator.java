868220b5d32d3e5b7e20fb45c2b1a96060a2fdc4
daneng4 <daneng4@naver.com>
2023-11-01 00:16:11 +0900

    refactor: 예외처리 출력 메시지 상수화

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class InputValidator {
 
+    private static final String ILLEGAL_NAME_SIZE_EXCEPTION_MESSAGE = "자동차 이름은 5자리 이하로 만들어야 합니다";
+    private static final String VOID_NAME_EXCEPTION_MESSAGE = "자동차 이름을 입력해주세요.";
+    private static final String DUPLICATE_NAME_EXCEPTION_MESSAGE = "자동차 이름이 중복됩니다.";
+    private static final String ZERO_ATTEMPT_NUMBER_EXCEPTION_MESSAGE = "시도 횟수는 1이상이어야 합니다.";
+    private static final String ILLEGAL_ATTEMPT_NUMBER_EXCEPTION_MESSAGE = "시도 횟수는 숫자여야 합니다.";
+
     char temp;
 
     public void checkString(String[] input) {
@@ -20,23 +26,23 @@ public class InputValidator {
     public void checkStringLength(String[] input){
         for (String str : input) {
             if (str.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자리 이하로 만들어야 합니다");
+                throw new IllegalArgumentException(ILLEGAL_NAME_SIZE_EXCEPTION_MESSAGE);
             }
             if (str.isEmpty()){
-                throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+                throw new IllegalArgumentException(VOID_NAME_EXCEPTION_MESSAGE);
             }
         }
     }
 
     public void checkDuplicateString(String[] input){
         if(Arrays.stream(input).distinct().count() != input.length){
-            throw new IllegalArgumentException("자동차 이름이 중복됩니다.");
+            throw new IllegalArgumentException(DUPLICATE_NAME_EXCEPTION_MESSAGE);
         }
     }
 
     public void isNumberZero(String number){
         if(number.equals("0")){
-            throw new IllegalArgumentException("시도 횟수는 1이상이어야 합니다.");
+            throw new IllegalArgumentException(ZERO_ATTEMPT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
@@ -44,7 +50,7 @@ public class InputValidator {
         for(int i = 0; i<number.length(); i++){
             temp = number.charAt(i);
             if (!Character.isDigit(temp)){
-                throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+                throw new IllegalArgumentException(ILLEGAL_ATTEMPT_NUMBER_EXCEPTION_MESSAGE);
             }
         }
     }
