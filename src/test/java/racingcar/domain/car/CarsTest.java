f6a998f299d96f27a9a8d17df8c357f724792b50
mainfn <_@mainfn.dev>
2023-10-31 13:13:28 +0900

    test: `CarsTest` 테스트 메소드 명 양식에 맞도록 변경

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain.car;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -11,14 +10,12 @@ import racingcar.domain.car.dto.output.CarRacingDto;
 import racingcar.domain.car.dto.output.CarsRacingDto;
 import racingcar.domain.move.MoveCommand;
 import racingcar.domain.move.MoveCommander;
-import racingcar.exception.DuplicateCarNameException;
-import racingcar.exception.NoCarsException;
 
 final class CarsTest {
 
     @DisplayName("moveAllBy 호출 시, MoveCommand.GO가 생성되면 모든 Car의 coodrinate가 1씩 증가한다.")
     @Test
-    void moveAllByGoCommand() {
+    void moveAllBy_withGoCommand_shouldIncreaseCoordinateOfAllCar() {
         // given
         final CreateCarsRacing createCarsRacing = new CreateCarsRacing(List.of("a", "b", "c"));
         final CarsRacing cars = CarsRacing.from(createCarsRacing);
@@ -50,7 +47,7 @@ final class CarsTest {
 
     @DisplayName("moveAllBy 호출 시, MoveCommand.STAY가 생성되면 모든 Car의 coodrinate가 그대로 유지된다")
     @Test
-    void moveAllByStayCommand() {
+    void moveAllBy_withStayCommand_shouldNotIncreaseCoordinateOfAllCar() {
         // given
         final CreateCarsRacing createCarsRacing = new CreateCarsRacing(List.of("a", "b", "c"));
         final CarsRacing cars = CarsRacing.from(createCarsRacing);
@@ -80,57 +77,4 @@ final class CarsTest {
                 .isEqualTo(0);
     }
 
-    @DisplayName("중복 없는 차 이름으로 Cars 생성 가능")
-    @Test
-    void createCarsSuccess() {
-        // given
-        final String carName1 = "a";
-        final String carName2 = "b";
-        final String carName3 = "c";
-        final List<String> carNames = List.of(carName1, carName2, carName3);
-        final CreateCarsRacing createCarsRacing = new CreateCarsRacing(carNames);
-
-        // when
-        final CarsRacing cars = CarsRacing.from(createCarsRacing);
-
-        // then
-        assertThat(createCarsRacing.carNames().size()).isEqualTo(3);
-        assertThat(createCarsRacing.carNames().get(0)).isEqualTo(carName1);
-        assertThat(createCarsRacing.carNames().get(1)).isEqualTo(carName2);
-        assertThat(createCarsRacing.carNames().get(2)).isEqualTo(carName3);
-        assertThat(cars).isNotNull();
-    }
-
-    @DisplayName("입력된 차 이름이 하나도 없으면 생성하면 예외 발생")
-    @Test
-    void createCarsFailure2() {
-        // given
-        final List<String> carNames = List.of();
-
-        // when
-        // then
-        assertThatThrownBy(
-                () -> new CreateCarsRacing(carNames)
-        )
-                .isInstanceOf(NoCarsException.class)
-                .hasMessage(NoCarsException.NO_CARS_EXCEPTION_MESSAGE);
-    }
-
-    @DisplayName("중복된 이름을 가진 CreateCars를 생성하면 예외 발생")
-    @Test
-    void createCarsDtoFailure() {
-        // given
-        final String carName1 = "a";
-        final String carName2 = "a";
-        final String carName3 = "b";
-        final List<String> carNames = List.of(carName1, carName2, carName3);
-
-        // when
-        // then
-        assertThatThrownBy(
-                () -> new CreateCarsRacing(carNames)
-        )
-                .isInstanceOf(DuplicateCarNameException.class)
-                .hasMessage(DuplicateCarNameException.DUPLICATE_CAR_NAME_EXCEPTION_MESSAGE);
-    }
 }
