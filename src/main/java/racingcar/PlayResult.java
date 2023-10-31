fffd313f70c920b11d70e9a359559b3a19fd6f31
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-31 16:46:43 +0900

    REFACTOR: 메소드 분리 및 주석 추가 - 메소드가 최대한 하나의 역할을 할 수 있도록 Race 클래스의 race 메소드에 있는 한 기능을 메소드로 뺐다. --> 실행 결과에 뜨는 "이름 : " 이 부분을 만들어 getResult 메소드에 넣어 "-"를 추가하며 실행결과 출력 --> 메소드 이름 : getNameListofResults - 함수 설명 주석 추가

--- src/main/java/racingcar/PlayResult.java
+++ src/main/java/racingcar/PlayResult.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class PlayResult {
+    // 실행 결과 출력 메소드
     public void getResult(String[] names, String[] results){
         for (int i = 0; i < names.length; i++) {
             int r = Randoms.pickNumberInRange(0, 9);
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 public class Race {
+    // 자동차 경주 플레이하는 함수
     public void race(){
         RacingInfo racingInfo = new RacingInfo();
         PlayResult playResult = new PlayResult();
@@ -9,13 +10,21 @@ public class Race {
         int num = racingInfo.getNum();
         String[] names = racingInfo.getNames();
 
+        // 실행 결과 출력을 위한 "차량이름 : "이 담긴 배열 생성
+        String[] results = getNameListofResults(names);
+
+        // 실행 횟수 만큼 실행 결과 출력
+        for (int i = 0; i < num; i++) {
+            playResult.getResult(racingInfo.getNames(), results);
+        }
+    }
+
+    // 실행 결과 출력을 위한 "차량이름 : "이 담긴 배열 생성 메소드
+    public String[] getNameListofResults(String[] names){
         String[] results = new String[names.length];
         for (int i = 0; i < names.length; i++) {
             results[i] = names[i] + " : ";
         }
-
-        for (int i = 0; i < num; i++) {
-            playResult.getResult(racingInfo.getNames(), results);
-        }
+        return results;
     }
 }
