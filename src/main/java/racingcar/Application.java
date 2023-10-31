b2ee70ab51556bdc4d6e588e6cc6eb67b63d1cde
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-01 03:08:54 +0900

    feat(racingCar): 게임실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.domain.GameService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameService gameService = new GameService();
     }
 }
