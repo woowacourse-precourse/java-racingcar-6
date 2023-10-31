d1a89984f5f4cf1b7509ed3fe9b1e9ce4cde07ae
Junobee25 <wnsdhqo@naver.com>
2023-10-31 19:54:10 +0900

    feat(service): racingGameStart 메서드 구현

--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -1,4 +1,26 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import racingcar.domain.CarGenerator;
+import racingcar.domain.RandomNumber;
+import racingcar.domain.RandomNumberGenerator;
+
 public class RacingGame {
+
+    CarGenerator carGenerator = new CarGenerator();
+
+    public void racingGameStart() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carGenerator.setCarListToCar();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numberOfAttempts = Integer.parseInt(Console.readLine());
+        int tryValue = 0;
+
+        System.out.println("실행 결과");
+        while (tryValue < numberOfAttempts) {
+
+        }
+    }
 }
