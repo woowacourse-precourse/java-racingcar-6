496b598203c0d3644c27a6ec6c6609b174df2530
ohjuyoungg <ejrm12@naver.com>
2023-10-31 20:13:45 +0900

    refactor(RandomNumbers) : 클래스명 메소드 명 변경

--- src/main/java/racingcar/util/RandomNumbers.java
+++ src/main/java/racingcar/util/RandomNumbers.java
@@ -2,10 +2,10 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class GoStopCar {
+public class RandomNumbers {
     private static final int RANDOM_START_NUMBER = 0;
     private static final int RANDOM_END_NUMBER = 9;
-    public static int pickNumberInRange() {
+    public static int pick() {
         int random = Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER);
         return random;
     }
