e2d4ee47deb4e5d845bd1ecd8ea976e3f2ed9624
shAn-kor <kjn0406@naver.com>
2023-10-31 14:22:12 +0900

    car 객체 삭제 후 단순히 이름 리스트 저장하는 CarNameList 객체로 변환

---
+++ src/main/java/racingcar/model/CarNameList.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class CarNameList {
+    private List<String> carNameList = null;
+
+    private CarNameList(List<String> carNameList) {
+        this.carNameList = carNameList;
+    }
+
+    public static CarNameList createCar (List<String> carNameList) {
+        return new CarNameList(carNameList);
+    }
+
+    public Integer countCar() {
+        return carNameList.size();
+    }
+
+    public String toString(Integer index) {
+        return carNameList.get(index);
+    }
+}
