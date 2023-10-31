24921495ae58bd46c19bc5fdd4db99821dfee099
ddohyeong <dohyeong7026@gmail.com>
2023-10-31 14:57:31 +0900

    refactor(CarNamesInputTest) : CarNamesInput 지역변수를 전역 변수로 선언
    CarNamesInput 객체 생성시 검증되는 코드가 있기 때문에 미리 검증된 전역변수로 테스트를 실행

--- src/test/java/domain/CarNamesInputTest.java
+++ src/test/java/domain/CarNamesInputTest.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CarNamesInputTest {
+	private static final CarNamesInput validatedCarNamesInput = new CarNamesInput("abc");
+
 	@Test
 	public void 이름은_쉼표로_구분한다() {
 		//given
@@ -21,10 +24,9 @@ public class CarNamesInputTest {
 	void 이름은_5글자_이하가_아니라면_예외발생() {
 		//given
 		String name = "abcedf";
-		CarNamesInput carNamesInput = new CarNamesInput(name);
 
 		// when & then
-		Assertions.assertThatThrownBy(() -> carNamesInput.validateNameLength(name))
+		Assertions.assertThatThrownBy(() -> validatedCarNamesInput.validateNameLength(name))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
@@ -32,9 +34,8 @@ public class CarNamesInputTest {
 	public void 이름이_공백이면_예외_발생() {
 		//given
 		String name = " ";
-		CarNamesInput carNamesInput = new CarNamesInput(name);
 		// when & then
-		Assertions.assertThatThrownBy(() -> carNamesInput.validateNameBlank(name))
+		Assertions.assertThatThrownBy(() -> validatedCarNamesInput.validateNameBlank(name))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
@@ -42,20 +43,18 @@ public class CarNamesInputTest {
 	public void 이름이_빈_값이면_예외_발생() {
 		//given
 		String name = "";
-		CarNamesInput carNamesInput = new CarNamesInput(name);
 		// when & then
-		Assertions.assertThatThrownBy(() -> carNamesInput.validateNameEmpty(name))
+		Assertions.assertThatThrownBy(() -> validatedCarNamesInput.validateNameEmpty(name))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	public void 이름들에_중복이_있으면_에외_발생() {
 		//given
-		String inputNames = "pobi,pobi";
-		CarNamesInput carNamesInput = new CarNamesInput(inputNames);
-		List<String> names = carNamesInput.getNames();
+		List<String> names = Arrays.asList("pobi","pobi");
+
 		// when & then
-		Assertions.assertThatThrownBy(() -> carNamesInput.validateNamesDuplicate(names))
+		Assertions.assertThatThrownBy(() -> validatedCarNamesInput.validateNamesDuplicate(names))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
@@ -63,7 +62,7 @@ public class CarNamesInputTest {
 	public void 이름_앞뒤로_공백시_없애기() {
 		//given
 		String name = " asdf ";
-		CarNamesInput carNamesInput = new CarNamesInput(name);
+		CarNamesInput carNamesInput = new CarNamesInput("abc");
 		// when & then
 		String trimmedName = carNamesInput.trimName(name);
 		Assertions.assertThat(trimmedName).isEqualTo("asdf");
