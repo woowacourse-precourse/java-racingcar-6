b52d9aed057667829b1efe7cab2df0a1969667f4
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 13:01:17 +0900

    refactor (Cars): record로 변환
    Cars 클래스를 record 타입으로 변환하였습니다

--- src/main/java/service/Cars.java
+++ src/main/java/service/Cars.java
@@ -3,31 +3,22 @@ package service;
 import java.util.Collections;
 import java.util.List;
 
-public class Cars{
-    private List<Car> car;
+public record Cars(List<Car> car) {
 
-    public Cars(List<Car> car) {
-        this.car = car;
-    }
-
-    public List<Car> getCar() {
-        return car;
-    }
-
-    public void go(){
+    public void go() {
         car.forEach(Car::go);
     }
 
-    public List<String> determineWinner(){
+    public List<String> determineWinner() {
         List<String> name;
         name = car.stream()
-                .filter(c -> c.getDistance()==getMaxDistance())
-                        .map(Car::getName)
-                                .toList();
+                .filter(c -> c.getDistance() == getMaxDistance())
+                .map(Car::getName)
+                .toList();
         return name;
     }
 
-    public int getMaxDistance(){
+    public int getMaxDistance() {
         List<Integer> distance;
         distance = car.stream().map(Car::getDistance).toList();
         return Collections.max(distance);
