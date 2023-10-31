422f1b30f2677eec65b409514e9f3e84c929b368
seonghoo1217 <seonghoo1217@naver.com>
2023-10-31 16:17:07 +0900

    test: generate racer test on interface `GenerateRacer`

--- src/test/java/study/CoreTest.java
+++ src/test/java/study/CoreTest.java
@@ -2,7 +2,10 @@ package study;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
+import racingcar.core.GenerateRacer;
 import racingcar.core.PickNumber;
+import racingcar.entity.Racer;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class CoreTest {
@@ -19,5 +22,21 @@ public class CoreTest {
 
     private static PickNumber pickRandomNumber(){
         return ()-> Randoms.pickNumberInRange(1,9);
+    }
+
+    @Test
+    void GenerateRacer_인터페이스_레이서_생성_기능_테스트(){
+        //given
+        String target = "test";
+
+        //when
+        Racer generateRacer = generateRacer().generateRacerByInputValue(target);
+
+        //then
+        assertThat(Racer.getInstance(target).hashCode()).isEqualTo(generateRacer.hashCode());
+    }
+
+    private static GenerateRacer generateRacer(){
+        return (name)-> Racer.getInstance(name);
     }
 }
