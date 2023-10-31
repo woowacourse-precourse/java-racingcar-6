2b89ffa3c8286b087e7b7e5eb2a65cefdb244a17
seastamp <seastamp10@gmail.com>
2023-10-31 19:28:10 +0900

    feat: 자동차 경주 실행결과 출력 구현

--- src/main/java/racingcar/view/RaceView.java
+++ src/main/java/racingcar/view/RaceView.java
@@ -1,14 +1,23 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class RaceView {
     private RaceView() {
     }
 
-    public static void displayRoundResults() {
-
+    public static void displayRoundResults(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getForwardDistance(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
     }
 
-    public static void displayWinner() {
+    public static void displayWinner(List<Car> winner) {
 
     }
 }
