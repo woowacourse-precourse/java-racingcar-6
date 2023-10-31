601aaa9d5439fb3cf31a485522326e33bcfe2b3d
Jaeho Kim <fldzm7905@gmail.com>
2023-11-01 00:50:47 +0900

    feat: 우승자 판별 기능 구현

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,8 +1,29 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Referee {
 
-    private boolean isWinner() {
-        return false;
+    public static List<Car> separationWinners(List<Car> carList) {
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = checkMaxPosition(carList);
+        for (Car car : carList) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
     }
+
+    public static int checkMaxPosition(List<Car> carList) {
+        int maxPosition = 0;
+        for (Car car : carList) {
+            if (maxPosition < car.getPosition()) {
+                maxPosition = car.getPosition();
+            }
+        }
+        return maxPosition;
+    }
+
 }
