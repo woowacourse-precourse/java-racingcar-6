49c73d7cc51e1ec0e0f20d08949b1bc6a5a75441
Cycrypto <jh01love@naver.com>
2023-10-31 12:10:49 +0900

    refactor:최종 결과 출력 형식 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,24 +1,33 @@
 package racingcar.view;
 
-import java.util.List;
+
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import racingcar.constant.GameMessage;
 import racingcar.model.RacingCar;
+import racingcar.model.RacingCarList;
+import racingcar.model.RacingCarResult;
 
 import static racingcar.constant.GameMessage.*;
 public class OutputView {
     private static final String SEPARATE_CHAR = ", ";
     private static final String SEPARATE_COLON = " : ";
-
+    private static final String GAME_RESULT_MESSAGE = GameMessage.GAME_RESULT_MESSAGE.getValue();
+    private static final String GAME_WINNER_MESSAGE = GameMessage.GAME_WINNER_MESSAGE.getValue();
     public static void gameResultMessge() {
-        System.out.println(GAME_RESULT_MESSAGE.getValue());
+        System.out.println(GAME_RESULT_MESSAGE);
     }
 
     public static void moveDistanceMessage(RacingCar racingCar) {
         System.out.println(racingCar.getName() + SEPARATE_COLON + racingCar.getDistanceAsBar());
     }
 
-    public static void printFinalWinner(List<String> winners) {
-        String result = String.join(SEPARATE_CHAR, winners);
-        System.out.println(GAME_WINNER_MESSAGE.getValue() + SEPARATE_COLON + result);
+    public static void printFinalWinner(RacingCarList winners) {
+        String result = winners.stream()
+                .map(RacingCar::getName)
+                .collect(Collectors.joining(SEPARATE_CHAR));
+
+        System.out.println(GAME_WINNER_MESSAGE+SEPARATE_COLON+result);
     }
 
     public static void newLine() {
