311075eb2d3021bf90a69256f666f6d8e3a1b120
fly-like-you <qkrwnsgh71w@gmail.com>
2023-11-01 03:00:09 +0900

    fix: "변경 InputController를 Service로 변경"
    입력을 받고 다른 객체로 변환해준다는 점에서 Service의 개념이 더 맞다고 생각하여 변경

--- src/main/java/racingcar/controller/InputController.java
+++
@@ -1,5 +0,0 @@
-package racingcar.controller;
-
-public class InputController {
-    // 사용자의 Input을 받는 컨트롤러
-}
---
+++ src/main/java/racingcar/service/InputService.java
@@ -0,0 +1,5 @@
+package racingcar.service;
+
+public class InputService {
+    // 사용자의 Input을 받는 컨트롤러
+}
