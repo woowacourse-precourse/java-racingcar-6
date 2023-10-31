9a3b281b5a889571eececab4a39a073bd1042b03
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 22:46:46 +0900

    refactor(GameController) : start()메서드 코드 수정 과 패키지 이동
    1. start() 메서드 코드를 askCarNames() 메서드를 만들어서 메서드화 하였습니다.
    2. start() 메서드 코드를 askTryCount() 메서드를 만들어 메서드화 하였습니다.
    
    GameController의 역할이 View와 domain의 클래스들읠 모두 사용하여 구성된다고 생각하여 다른 패키지로 구분하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import racingcar.domain.GameController;
+import racingcar.Controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
