f57b111a7a309a5c01d69887ff9272866f4bcf00
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-31 19:46:25 +0900

    test: Car attempt 테스트 추가
    랜덤값에 따라 포지션 증가, 유지 테스트
    - 지정 값(4)이상 값이 주어지면 car 포지션 증가
    - 지정 값(4)미만 값이 주어지면 car 포지션 유지

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,11 +1,16 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
     private static final String NAME_UNDER_LENGTH_LIMIT = "car";
     private static final String NAME_EXCEEDS_LENGTH_LIMIT = "longCar";
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
 
     @Test
@@ -17,9 +22,31 @@ class CarTest {
     @Test
     void 지정한_길이를_만족하는_이름의_자동차_정상_생성() {
         Car car = new Car(NAME_UNDER_LENGTH_LIMIT);
-        Assertions.assertThat(car.getName())
+        assertThat(car.getName())
                 .isEqualTo(NAME_UNDER_LENGTH_LIMIT);
     }
 
+    @Test
+    void 지정한_값을_넘는_랜덤_값이_주어지면_포지션_증가() {
+        Car car = new Car(NAME_UNDER_LENGTH_LIMIT);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.attempt();
+                    assertThat(car.getPosition()).isEqualTo(1);
+                },
+                MOVING_FORWARD
+        );
+    }
 
+    @Test
+    void 지정한_값을_미만_랜덤_값이_주어지면_포지션_증가() {
+        Car car = new Car(NAME_UNDER_LENGTH_LIMIT);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.attempt();
+                    assertThat(car.getPosition()).isEqualTo(0);
+                },
+                STOP
+        );
+    }
 }
