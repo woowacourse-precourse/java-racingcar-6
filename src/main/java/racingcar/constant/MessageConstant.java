dcd2679fe5dc7eaab03c1a3c255628166520c6a9
bonooy <choiboyuna@gmail.com>
2023-10-31 12:30:49 +0900

    Add: input/error 발생 메시지 저장하는 Message Constant 추가

---
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -0,0 +1,15 @@
+package racingcar.constant;
+
+public class MessageConstant {
+
+    public static final String USER_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String USER_INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
+
+    public static final String ERROR_MESSAGE_NAME_INPUT = "잘못된 입력입니다. 자동차 이름을 입력해주세요.";
+    public static final String ERROR_MESSAGE_NAME_DUPLICATE = "동일한 자동차 이름이 이미 존재합니다.";
+    public static final String ERROR_MESSAGE_NAME_BLANK = "자동차 이름 안에 공백이 있지 않아야 합니다.";
+    public static final String ERROR_MESSAGE_NAME_MAX = "자동차 이름은 5글자를 초과해서는 안됩니다.";
+    public static final String ERROR_MESSAGE_NAME_MIN = "자동차 이름은 1글자 이상이어야 합니다.";
+    public static final String ERROR_MESSAGE_NUMBER_INPUT = "잘못된 입력입니다. 시도할 횟수를 입력해주세요.";
+    public static final String ERROR_MESSAGE_NUMBER_TYPE = "시도할 횟수는 정수만 입력 가능합니다.";
+}
