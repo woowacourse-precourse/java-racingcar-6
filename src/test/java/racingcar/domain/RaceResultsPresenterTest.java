2d555d4ae67d81dbdeca141d87eb6840219c5a38
namhoon-kim97 <kskcmh5723@gmail.com>
2023-10-31 23:08:35 +0900

    test: printWinner 메서드 test 구현

--- src/test/java/racingcar/domain/RaceResultsPresenterTest.java
+++ src/test/java/racingcar/domain/RaceResultsPresenterTest.java
@@ -6,25 +6,43 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RaceResultsPresenterTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setSystemOut() {
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Test
     public void testPrintRaceProcess() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
         RaceResultsPresenter racePrinter = new RaceResultsPresenter();
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("pobi", new RandomNumberGenerator(), 1));
-        cars.add(new Car("woni", new RandomNumberGenerator(), 2));
-        cars.add(new Car("jun", new RandomNumberGenerator(), 1));
-
+        List<Car> cars = createTestCars();
         racePrinter.printRaceProcess(cars);
 
         String expectedOutput = "pobi : -\r\nwoni : --\r\njun : -\r\n\r\n";
         assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void testPrintWinner() {
+        RaceResultsPresenter racePrinter = new RaceResultsPresenter();
+        List<Car> cars = createTestCars();
+        racePrinter.printWinner(cars);
+
+        String expectedOutput = "최종 우승자 : woni\r\n";
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    private List<Car> createTestCars() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("pobi", new RandomNumberGenerator(), 1));
+        cars.add(new Car("woni", new RandomNumberGenerator(), 2));
+        cars.add(new Car("jun", new RandomNumberGenerator(), 1));
+        return cars;
     }
 }
