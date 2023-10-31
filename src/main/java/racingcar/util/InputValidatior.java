071c98b58441cc15a10ce68fb9b87d2d81b912a1
whereami0404 <whereami2048@gmail.com>
2023-10-31 19:24:01 +0900

    feat: 입력 예외처리 기능 구현(누락 추가)

---
+++ src/main/java/racingcar/util/InputValidatior.java
@@ -0,0 +1,16 @@
+package racingcar.util;
+
+import java.util.regex.Pattern;
+
+public class InputValidatior {
+
+    private static final String REGEXP_PATTERN_NUMBER = "^[\\d]*$";
+    private static final int CAR_MIN_NUMBER = 0;
+    public boolean isCarNamesUnderLength(String[] names) {
+        return names.length <= CAR_MIN_NUMBER;
+    }
+
+    public boolean isNaturalNumber(String number) {
+        return Pattern.matches(REGEXP_PATTERN_NUMBER, number);
+    }
+}
