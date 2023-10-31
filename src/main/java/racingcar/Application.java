79b77ca78f58c2ba8d3c33f88a1b4da82d9e9cf9
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-31 14:54:07 +0900

    feat: 게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		final CarList carList = new CarList();
+		final InputView inputView = new InputView();
+		final OutputView outputView = new OutputView();
+
+		inputView.printCarNameInputMessage();
+		String carNames = Console.readLine();
+		carList.splitCarNames(carNames);
+
+		inputView.printAttemptNumberInputMessage();
+		int attempt = Integer.parseInt(Console.readLine());
+
+		for (int i = 0; i < attempt; i++) {
+			outputView.printResult(carList);
+		}
+
+		outputView.printWinners(carList.getWinners());
+
+	}
 }
