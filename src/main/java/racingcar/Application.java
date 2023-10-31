367f092bc793eb16d92e58a2d2c3756e2d74ec21
= <=>
2023-10-31 22:59:57 +0900

    Revert "feat(Application): 메인 어플리케이션 설정"
    This reverts commit 6923e43dd549f00f112dcd840b7f6098517e9c88.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,6 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        Input input = new Input();
-
-        String carNames = input.inputCarName();
-        String tryCount = input.inputTryCount();
-
-        RacingCar game = new RacingCar(carNames, Integer.parseInt(tryCount));
-        game.startGame();
+        // TODO: 프로그램 구현
     }
 }
