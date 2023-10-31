5d506494a1444cd49fce805c2d3d93ad79a9a312
simjaeyoun <spqjfgogo@gmail.com>
2023-11-01 02:59:11 +0900

    feat: '입력 메세지 추가'

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
+    public static final String INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBEROFTRY_MESSAGE = "시도할 회수는 몇회인가요?";
     public static String[] splitNames(String namesWithComma){
         checingNullOrBlankException(namesWithComma);
 
