9c36f3b970701331a5c176b56249d9a4eab8f9ca
MingyeomKim <mingyum119@naver.com>
2023-10-31 12:38:33 +0900

    refator(Cars): List<String>을 검증하는 로직으로 수정
    기존에 Raw 문자열의 파라미터를 List<String> 형식으로 바꿈에 따라 검증 로직을 수정한다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,50 +1,40 @@
 package racingcar.domain;
 
 import static racingcar.global.constants.NumberType.MIN_CAR_SIZE;
-import static racingcar.global.constants.SymbolType.DELIMITER;
 
-import java.util.Arrays;
 import java.util.List;
-import racingcar.global.constants.NumberType;
 
 public class Cars {
     private List<Car> carList;
 
-    private Cars(List<Car> carList) {
-        this.carList = carList;
+    private Cars(List<String> nameList) {
+        validateNameList(nameList);
+        this.carList = convertNameListToCarList(nameList);
     }
 
-    public static Cars of(String names) {
-        return new Cars(parseStringToCarList(names));
+    public static Cars of(List<String> nameList) {
+        return new Cars(nameList);
     }
 
-    private void validateCarList(List<Car> carList) {
-        validateListSize(carList);
+    private List<Car> convertNameListToCarList(List<String> nameList) {
+        return nameList.stream()
+                .map(Car::of)
+                .toList();
     }
 
-    private void validateListSize(List<Car> carList) {
-        if (hasSingleCar(carList)) {
+    private void validateNameList(List<String> nameList) {
+        validateListSize(nameList);
+    }
+
+    private void validateListSize(List<String> nameList) {
+        if (hasSingleCar(nameList)) {
             throw new IllegalArgumentException(
                     String.format("%d대 이상의 자동차의 이름을 입력해주세요.", MIN_CAR_SIZE.getValue())
             );
         }
     }
 
-    private boolean hasSingleCar(List<Car> carList) {
-        return carList.size() < MIN_CAR_SIZE.getValue();
-    }
-
-    private static List<Car> parseStringToCarList(String names) {
-        return createCarList(splitNames(names));
-    }
-
-    private static List<Car> createCarList(String[] splitNames) {
-        return Arrays.stream(splitNames)
-                .map(Car::of)
-                .toList();
-    }
-
-    private static String[] splitNames(String names) {
-        return names.split(DELIMITER.getSymbol());
+    private boolean hasSingleCar(List<String> nameList) {
+        return nameList.size() < MIN_CAR_SIZE.getValue();
     }
 }
