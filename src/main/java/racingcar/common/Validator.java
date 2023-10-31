d3fc595eca9d05137c2502e4cea90196c27f0a6a
Joohui <joohuii96@gmail.com>
2023-10-31 17:34:20 +0900

    feat: 중복된 이름 검증 기능 구현

--- src/main/java/racingcar/common/Validator.java
+++ src/main/java/racingcar/common/Validator.java
@@ -2,7 +2,9 @@ package racingcar.common;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
@@ -10,6 +12,7 @@ public class Validator {
     public static final String CAR_NAME_BLANK_MESSAGE = "빈 문자열은 입력이 불가능합니다.";
     public static final String CAR_NAME_WRONG_LENGTH_MESSAGE = "5자 이하만 가능합니다.";
     public static final int CAR_NAME_LENGTH = 5;
+    public static final String CAR_NAME_DUPLICATED_MESSAGE = "중복된 이름은 사용이 불가능합니다.";
 
     public static void validateCarNames(List<String> carNameList) {
         if(isContainsEmptyName(carNameList)) {
@@ -21,6 +24,18 @@ public class Validator {
         if(isOverLength(carNameList)) {
             throw new IllegalArgumentException(CAR_NAME_WRONG_LENGTH_MESSAGE);
         }
+        if(isDuplicated(carNameList)) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATED_MESSAGE);
+        }
+    }
+
+    private static boolean isDuplicated(List<String> carNameList) {
+        Set<String> carNameSet = new HashSet<>();
+        for(String carName : carNameList) {
+            if(carNameSet.contains(carName)) return true;
+            carNameSet.add(carName);
+        }
+        return false;
     }
 
     private static boolean isContainsEmptyName(List<String> carNameList) {
