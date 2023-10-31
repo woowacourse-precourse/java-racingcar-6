52383369f612bd6d4d5b0d774c29e5e8ea820e0f
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 19:10:47 +0900

    feat : OutputView 경주 1회 실행 결과를 출력하는 기능 추가

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -4,4 +4,7 @@ public class Constant {
 
     public static final String COMMA = ",";
     public static final String EXECUTION_RESULT_MENT = "실행 결과";
+    public static final String COLON = " : ";
+    public static final String MINUS_SIGN = "-";
+    public static final char ENTER = '\n';
 }
--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -1,10 +1,29 @@
 package racingcar.view.output;
 
+import static racingcar.constant.Constant.COLON;
+import static racingcar.constant.Constant.ENTER;
 import static racingcar.constant.Constant.EXECUTION_RESULT_MENT;
+import static racingcar.constant.Constant.MINUS_SIGN;
+
+import java.util.List;
+import racingcar.dto.RacingCarDto;
 
 public class OutputView {
 
     public void printExecutionResultMent() {
         System.out.println(EXECUTION_RESULT_MENT);
+    }
+
+    public void printExecutionResult(List<RacingCarDto> executionResult) {
+        System.out.println(renderResult(executionResult));
+    }
+
+    private String renderResult(List<RacingCarDto> executionResult) {
+        StringBuilder renderer = new StringBuilder();
+        executionResult.forEach(result -> {
+            renderer.append(result.getCarName()).append(COLON);
+            renderer.append(MINUS_SIGN.repeat(Math.max(0, result.getPosition()))).append(ENTER);
+        });
+        return renderer.toString();
     }
 }
