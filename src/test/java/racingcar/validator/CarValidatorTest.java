81237107342975c802ddea93958ab2a9cc30813a
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-31 21:00:31 +0900

    refactor: 예외 처리 비교 메세지 수정
    기존의 [ERROR]이 뜨는 지 확인하는 테스트에서
    완전히 예외 처리 메세지랑 일치하는 지 비교하는 테스트로 수정

--- src/test/java/racingcar/validator/CarValidatorTest.java
+++ src/test/java/racingcar/validator/CarValidatorTest.java
@@ -1,63 +1,49 @@
-package racingcar;
+package racingcar.validator;
 
 import org.junit.jupiter.api.Test;
-import racingcar.validator.CarNameValidator;
-import racingcar.validator.RoundValidator;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ValidatorTest  {
+public class CarValidatorTest {
 
     private CarNameValidator carNameValidator;
     private RoundValidator roundValidator;
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private final static String NAME_ERROR_MESSAGE = "차 이름은 문자로만 구성되어야 합니다.";
+    private final static String NAME_NULL_ERROR_MESSAGE = "차 이름을 적어주세요.";
+    private final static String NAME_RANGE_ERROR_MESSAGE = "차 이름은 1자 이상 5자 이하로 적어주세요.";
+    private final static String NAME_DUPLICATE_ERROR_MESSAGE = "중복된 이름을 적으면 안 됩니다.";
 
     @Test()
     void 차_이름_안_적음_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(NAME_NULL_ERROR_MESSAGE);
     }
 
     @Test()
     void 문자_외_입력_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hy un,pobi"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(NAME_ERROR_MESSAGE);
     }
     @Test()
     void 차_이름_1자_미만_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator(",pobi,woote"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(NAME_RANGE_ERROR_MESSAGE);
     }
 
     @Test()
     void 차_이름_5자_초과_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hyun,pobi,wooteco"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(NAME_RANGE_ERROR_MESSAGE);
     }
 
     @Test()
     void 중복_이름_예외_처리() {
         assertThatThrownBy(() -> carNameValidator = new CarNameValidator("hyun,hyun,pobi"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @Test()
-    void 진행_라운드_숫자_외_문자_입력_예외_처리() {
-        assertThatThrownBy(() -> roundValidator = new RoundValidator("asd"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @Test()
-    void 진행_라운드_미_입력_예외_처리() {
-        assertThatThrownBy(() -> roundValidator = new RoundValidator(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(NAME_DUPLICATE_ERROR_MESSAGE);
     }
 }
