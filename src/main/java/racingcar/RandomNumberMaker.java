e04ee2ec4f148b6f1fb6f8793dd98791e2e26521
gdakate <gdakate1215@gmail.com>
2023-10-31 19:07:33 +0900

    Feat: 랜덤 숫자 생성 클래스
    - 0~9사이의 난수 생성

--- src/main/java/racingcar/RandomNumberMaker.java
+++ src/main/java/racingcar/RandomNumberMaker.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberMaker {
     private RandomNumberMaker() {
     }
+
     public static int makeRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
