410e093a515f0c39f56d72fa5a67e28a040cbffe
yohanii <dygks924@gmail.com>
2023-10-31 21:14:31 +0900

    feat: 경주 할 자동차 이름 입력받기

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -2,12 +2,29 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
 
     public static List<Car> inputCarNames(String input) {
-        return null;
+        List<Car> carList = Arrays.stream(input.split(","))
+                .filter(name -> validateCarName(name))
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
+        return carList;
+    }
+
+    private static boolean validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        } else if (name.length() <= 0) {
+            throw new IllegalArgumentException("자동차 이름을 1자 이상 적어주세요.");
+        } else if (name.trim().length() == 0) {
+            throw new IllegalArgumentException("공백만으로 구성된 이름은 안됩니다.");
+        }
+        return true;
     }
 
     public static int inputTryCount(String input) {
