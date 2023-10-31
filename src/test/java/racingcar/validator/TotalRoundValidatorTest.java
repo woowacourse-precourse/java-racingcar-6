642548c58c07f038e6842b2c7e42282fe17c1ffe
liarreez <liarreez@gmail.com>
2023-10-31 17:00:34 +0900

    test : 부적절한 전체 라운드 입력시 에러 발생 테스트 작성

---
+++ src/test/java/racingcar/validator/TotalRoundValidatorTest.java
@@ -0,0 +1,19 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TotalRoundValidatorTest {
+    Validator totalRoundValidator = new TotalRoundValidator();
+
+    @ParameterizedTest
+    @DisplayName("부적절한 전체 라운드 입력 시 에러 발생 테스트")
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "\t", "\n", "다섯", "five", "@"})
+    public void testCarNameGreaterThanFive(String totalRound) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> totalRoundValidator.validate(totalRound));
+    }
+}
