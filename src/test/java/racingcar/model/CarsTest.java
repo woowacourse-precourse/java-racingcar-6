7c001902338090fd84c8367243e77e8abe8fbac1
siyeonSon <siyeon0209@naver.com>
2023-10-31 23:08:06 +0900

    test(cars): 전진한다

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
 import racingcar.util.NumberGenerator;
@@ -13,6 +14,25 @@ class CarsTest {
         String carName = "pobi,woni,jun";
         Cars cars = new Cars(carName);
         assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 전진한다() {
+        String carNames = "pobi,woni,jun";
+        Cars cars = new Cars(carNames);
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        assertRandomNumberInRangeTest(
+                () -> {
+                    cars.forward(numberGenerator);
+                    assertAll(
+                            () -> assertThat(cars.getCars().get(0).getPoint()).isEqualTo(1),
+                            () -> assertThat(cars.getCars().get(1).getPoint()).isEqualTo(1),
+                            () -> assertThat(cars.getCars().get(2).getPoint()).isEqualTo(0)
+                    );
+
+                },
+                5, 4, 3
+        );
     }
 
     @Test
