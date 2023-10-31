d1c6b5afe8b7282d922bbfd480c0d57bc0b8758f
emost22 <emoney96@naver.com>
2023-11-01 04:54:28 +0900

    Fix: number regex zero check

--- src/main/java/racingcar/validator/RoundInputValidator.java
+++ src/main/java/racingcar/validator/RoundInputValidator.java
@@ -1,7 +1,7 @@
 package racingcar.validator;
 
 public class RoundInputValidator {
-    private static String REGEX_NUMBER = "^[0-9]+$";
+    private static String REGEX_NUMBER = "^[1-9][0-9]*$";
 
     public static void checkRoundNumberFormat(String round) {
         if (!round.matches(REGEX_NUMBER)) {
