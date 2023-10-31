d2cc2658ec28a0b5541fa4e54bea26d856f8ca5b
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-31 07:00:43 +0000

    [Test] ADD : 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -4,31 +4,37 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-
     @Test
-    void 전진_정지() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
+    void 테스트_전체_실행() {
+        assertRandomNumberInRangeTest(() -> {
+            run("pobi,woni", "1");
+            assertThat(output()).contains("pobi : -", "woni : ", "우승자: pobi");
+        }, 4, 3);
     }
 
     @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 자동차_이름_예외_테스트() {
+        assertThatThrownBy(() -> run("pobi,javaji", "1"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("자동차 이름은 5글자 이하여야 합니다.");
+    }
+
+    @Test
+    void 시도_횟수_예외_테스트() {
+        assertThatThrownBy(() -> run("pobi,woni", "0"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("시도 횟수는 1 이상이어야 합니다.");
+    }
+
+    @Test
+    void 숫자_입력_예외_테스트() {
+        assertThatThrownBy(() -> run("pobi,woni", "not_a_number"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("올바른 숫자를 입력하세요.");
     }
 
     @Override
