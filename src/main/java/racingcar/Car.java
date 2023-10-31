1c5a951edcb2a49ba4dfdf0f41059d090fe293f5
baejeong-eun <lovelyeun0914@gmail.com>
2023-11-01 02:24:54 +0900

    feat: 입력값 확인 후 예외발생

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,7 +5,39 @@ public class Car {
     String name;
 
     public Car(String name) {
+        checkInputValue(name);
         this.name = name;
+    }
+    public void checkInputValue(String input) {
+        input = input.trim();
+        if (isEmptyName(input)
+                || !isCheckLength(input)
+                || isNumber(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean isEmptyName(String input) {
+        if (input.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isCheckLength(String input){
+        if (input.length() > 5) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isNumber(String input){
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
     }
 
 }
