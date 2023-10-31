f462f7e748d3a155d9ee237cd2e34b24fc3b3f52
Isaac Han <isaachan233@gmail.com>
2023-10-31 15:06:37 +0900

    refactor: 생성시 results가 할당되게함

--- src/main/java/racingcar/model/PlayResults.java
+++ src/main/java/racingcar/model/PlayResults.java
@@ -10,14 +10,14 @@ public class PlayResults {
     private final List<PlayResult> results = new ArrayList<>();
 
     public PlayResults(Cars cars) {
-        this.cars = cars;
-    }
-
-    public List<PlayResult> getResults() {
         for(Car car : cars.getCarList()) {
             PlayResult result = new PlayResult(car);
             results.add(result);
         }
+        this.cars = cars;
+    }
+
+    public List<PlayResult> getResults() {
         return results;
     }
 }
