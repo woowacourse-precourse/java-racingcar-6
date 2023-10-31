83ca458be72f9ad64c71d9e8c2f03ca60b881dbd
SuRiimm <surimii45@gmail.com>
2023-11-01 03:44:31 +0900

    feat(racingcar): Race(경주) Model 무작위 값 구하기 기능 구현

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,4 +1,18 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Race {
+  private List<Car> cars;
+
+  public Race(List<Car> cars){
+    this.cars = cars;
+  }
+
+  public int getRandomValue(){
+    int randomValue = Randoms.pickNumberInRange(0,9);
+    return randomValue;
+  }
+
 }
