63a451813d90d42bc2b7a467e7a3145240cc9ce1
SeungGwa123 <skwan123@naver.com>
2023-10-31 16:49:04 +0900

    Feat: Fix totalPrint
    전체 자동차 현재 거리 출력하는 함수에서 routePrint를 사용하는 라인을 추가

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -11,8 +11,9 @@ public class Output {
     }
     public void totalPrint(Map<String,Integer> parkingLot,String[] carlist) {
         for(int i=0;i<parkingLot.size();i++){
-            //하나씩 출력
+            routePrint(carlist[i],parkingLot.get(carlist[i]));
         }
+        System.out.println();
     }
 
     public void routePrint(String carName,int carDistance) {
