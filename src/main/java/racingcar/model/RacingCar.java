694806a1542450238c7c7c3bfba4929dddd9254b
snaag <snaag.dev@gmail.com>
2023-11-01 00:46:21 +0900

    refactor: (RacingCarRace) 불필요한 주석, 코드 제거 및 정답 출력 로직 개선 - 이동 경로 출력 함수(getScore) 개선   - Arrays.fill() 후에 join -> repeat 함수 사용

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,9 +1,7 @@
 package racingcar.model;
 
-import java.util.Arrays;
-import camp.nextstep.edu.missionutils.Randoms;
-
 import static racingcar.utils.Constants.*;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
     String carName;
@@ -23,7 +21,6 @@ public class RacingCar {
         return carName;
     }
 
-    // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
     public void randomStepForward() {
         int random = Randoms.pickNumberInRange(ZERO, NINE);
         if(random >= RANDOM_MOVE_STANDARD) {
@@ -32,8 +29,7 @@ public class RacingCar {
     }
 
     public String getScore() {
-        char[] paths = new char[distanceTraveled];
-        Arrays.fill(paths, DASH);
-        return this.carName + " : " + String.join("", new String(paths));
+        String path = DASH.repeat(distanceTraveled);
+        return this.carName + COLON + String.join("", path);
     }
 }
