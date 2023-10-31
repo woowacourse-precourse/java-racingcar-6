578ce714a16e7bc831491d53d93da845075df0a7
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-31 13:24:23 +0900

    feat: 자동차 리스트 세팅 메서드 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private List<Car> cars = new ArrayList<>();
+
     private boolean isMoveForward() {
         return CreateRandomNumber.getNumber() >= 4;
+    }
+
+    private void setCars(String[] names) {
+        for (String name : names) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
     }
 }
