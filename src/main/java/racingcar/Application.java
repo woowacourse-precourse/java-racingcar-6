2c9d448003e7c1f5997f7fdea5a8d618e94f024b
Jaeho Kim <fldzm7905@gmail.com>
2023-11-01 00:52:13 +0900

    refactor: 경기 시작 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,11 @@
 package racingcar;
 
-import java.util.Arrays;
-import racingcar.view.OutputView;
+import racingcar.domain.Race;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        OutputView outputView = new OutputView();
-        outputView.printWinners(Arrays.asList("pobi", "jun"));
+        Race race = new Race();
+        race.initialRace();
     }
 }
