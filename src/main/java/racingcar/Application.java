c7b7bc6fa851f2e8497a509bdab636fbf385770b
kang jihyeong <her4386@gmail.com>
2023-10-31 22:32:13 +0900

    [feat] 자동차 게임 동작시키기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.car.Car;
+import racingcar.game.Game;
+import racingcar.round.Round;
+
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IllegalArgumentException {
+        Game game = new Game();
+        game.startGame(game.makeCarEntity());
     }
 }
