e52b41891849e18bbe85a7fe18cbb2a5775deb52
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-10-31 09:55:31 +0900

    Feat : "HashMap 생성과 Print 메서드 구현"
    Input된 값을 HashMap으로 만들어주는 메서드와 디버깅 및 상태 표현에 필요한 print 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,22 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
+    
+
+
+    /** 입력 받은 List를 HashMap으로 초기화 하는 메서드입니다.
+     * 
+     * @param userlist 사용자로부터 입력받은 User 목록
+     * @return User 목록을 key로 갖는 빈 문자열을 갖는 HashMap 입니다.
+     */
+    public static HashMap<String, String> makeHashMap(ArrayList<String> userlist) {
+        HashMap<String, String> userStatusMap = new HashMap<>();
+
+        for (String user : userlist) {
+           userStatusMap.put(user, "");
+        }
+        return userStatusMap;
+   }
 
     /** 입력과 관련된 메서드 입니다.
      * 
@@ -36,7 +52,7 @@ public class Application {
         return inputlist;
     }
 
-        /** 입력과 관련된 메서드 입니다.
+    /** 입력과 관련된 메서드 입니다.
      * 
      * @return input은 사용자가 입력한 숫자 입니다.
      */
@@ -46,5 +62,18 @@ public class Application {
             throw new IllegalArgumentException("음수가 입력되었습니다.");
         
         return input;
+    }
+
+    /** 현재 user들의 레이싱 상태를 표시합니다.
+     * 
+     * @param userStatushMap 현재 레이싱 중인 HashMap입니다.
+     */
+    public static void printUsers(HashMap<String, String> userStatushMap) {
+        Set<String> keySet = userStatushMap.keySet();
+
+        for (String key : keySet) {
+            System.out.println(key + " : " + userStatushMap.get(key));
+        }
+        System.out.println();
     }
 }
