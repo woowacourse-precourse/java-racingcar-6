61b1e77eeb96b2e95fffd6b4144455c9025aff14
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-01 00:30:58 +0900

    feat: 몇번 시도할 것인지 입력에 대한 유효성 검사 개발 및 rename

--- src/main/java/racingcar/utils/Validation.java
+++ src/main/java/racingcar/utils/Validation.java
@@ -5,22 +5,29 @@ import java.util.regex.Pattern;
 public class Validation {
     private static final int NAME_LENGTH_MIN = 1;
     private static final int NAME_LENGTH_MAX = 5;
-    private static final Pattern regex = Pattern.compile("\\W");
+    private static final Pattern specialChar = Pattern.compile("\\W");
+    private static final Pattern exceptNumber = Pattern.compile("\\D");
 
     public static void validateName(String name) {
         isValidNameLength(name);
         hasSpecialCharacter(name);
     }
 
-    public static void isValidNameLength(String name) throws IllegalArgumentException {
+    private static void isValidNameLength(String name) throws IllegalArgumentException {
         if (name.length() < NAME_LENGTH_MIN
                 || name.length() > NAME_LENGTH_MAX) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void hasSpecialCharacter(String name) throws IllegalArgumentException {
-        if (regex.matcher(name).find()) {
+    private static void hasSpecialCharacter(String name) throws IllegalArgumentException {
+        if (specialChar.matcher(name).find()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateTrial(String trial) throws IllegalArgumentException {
+        if (exceptNumber.matcher(trial).find()) {
             throw new IllegalArgumentException();
         }
     }
