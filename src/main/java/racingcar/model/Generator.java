36bdeabc581f5ffdb09b23626129b67345cf9b54
Haeun Jeong <gkdms4248@gmail.com>
2023-10-31 11:20:43 +0900

    fix(model): generator static 으로 변경

--- src/main/java/racingcar/model/Generator.java
+++ src/main/java/racingcar/model/Generator.java
@@ -6,7 +6,7 @@ public class Generator {
     private final static int MIN_NUMBER = 0;
     private final static int MAX_NUMBER = 9;
 
-    public int generateRandomNumber() {
+    public static int generateRandomNumber() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
