461a385555807f0e247bc285c9c5f17c1fb2e283
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-01 08:35:01 +0900

    refactor: "라운드" 용어를 직관적인 "시도 횟수" 로 변경, `round` 의 경우 `attemptNumber` 로 변경

--- src/test/java/racingcar/controller/AttemptNumberValidatorTest.java
+++ src/test/java/racingcar/controller/AttemptNumberValidatorTest.java
@@ -4,19 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class RoundNumberValidatorTest {
+public class AttemptNumberValidatorTest {
     private static final String ROUND_NUMBER_ERROR_MESSAGE = "라운드는 1 이상이어야 합니다.";
 
     @Test
     public void 유효한_라운드_수_입력() {
         String validRoundNumber = "5";
-        RoundNumberValidator.validate(validRoundNumber);
+        AttemptNumberValidator.validate(validRoundNumber);
     }
 
     @Test
     public void 라운드_수가_0인_경우() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            RoundNumberValidator.validate("0");
+            AttemptNumberValidator.validate("0");
         });
         String actualErrorMessage = exception.getMessage();
         assert (actualErrorMessage.contains(ROUND_NUMBER_ERROR_MESSAGE));
@@ -25,13 +25,13 @@ public class RoundNumberValidatorTest {
     @Test
     public void 라운드_수가_1인_경우() {
         String validRoundNumber = "1";
-        RoundNumberValidator.validate(validRoundNumber);
+        AttemptNumberValidator.validate(validRoundNumber);
     }
 
     @Test
     public void 라운드_수가_음수인_경우() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            RoundNumberValidator.validate("-1");
+            AttemptNumberValidator.validate("-1");
         });
         String actualErrorMessage = exception.getMessage();
         assert (actualErrorMessage.contains(ROUND_NUMBER_ERROR_MESSAGE));
@@ -40,7 +40,7 @@ public class RoundNumberValidatorTest {
     @Test
     public void 라운드_수가_숫자가_아닌_경우() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            RoundNumberValidator.validate("a");
+            AttemptNumberValidator.validate("a");
         });
         String actualErrorMessage = exception.getMessage();
         assert (actualErrorMessage.contains(ROUND_NUMBER_ERROR_MESSAGE));
