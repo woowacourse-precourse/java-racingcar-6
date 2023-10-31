de0de81b0be2702afb2ce224617494d5009e1f5e
davemins <davemins4125@naver.com>
2023-10-31 14:34:44 +0900

    test: 반복 횟수 입력 메시지 출력 기능 테스트

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class OutputViewTest {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MOVE_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     @Test
     void 출력_메시지_확인() {
@@ -22,5 +24,20 @@ class OutputViewTest {
 
         // Then
         assertThat(out.toString().trim()).isEqualTo(INPUT_CAR_NAME_MESSAGE);
+    }
+
+    @Test
+    void 반복_횟수_출력_메시지_확인() {
+        // Given
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        OutputView outputView = new OutputView();
+
+        // When
+        outputView.printInputMoveNumberMessage();
+
+        // Then
+        assertThat(out.toString().trim()).isEqualTo(INPUT_MOVE_NUMBER_MESSAGE);
     }
 }
