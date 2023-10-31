7fed1a79b5dc640541746a484a8f3651d5288c4f
k-kmw <kimmw2_@naver.com>
2023-10-31 12:15:32 +0900

    fix: RacingService 출력 수정 및 method depth 조절

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -17,32 +17,52 @@ public class RacingService {
     }
 
     public void race() {
+
         getUserInput();
+
+        System.out.println();
+        System.out.println("실행 결과");
+
         play();
+        
         System.out.println("최종 우승자 : " + getResult());
     }
 
     private String getResult() {
+
         Referee referee = new Referee();
+
         return referee.selectWinner(cars);
     }
 
     private void play() {
-        for(int i=0; i<tryNum; i++) {
-            for (Car car : cars) {
-                boolean isMoveCondition = car.isMoveCondition();
-                car.move(isMoveCondition);
-            }
+        for (int i = 0; i < tryNum; i++) {
+            carsTryMove();
         }
     }
 
+    private void carsTryMove() {
+
+        for (Car car : cars) {
+            boolean isMoveCondition = car.isMoveCondition();
+            car.move(isMoveCondition);
+        }
+
+        System.out.println();
+    }
+
     private void getUserInput() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
         String tryNum = Console.readLine();
+
         List<String> carNameList = List.of(carNames.split(","));
         for (String carName : carNameList) {
             cars.add(new Car(carName));
         }
+
         this.tryNum = Integer.parseInt(tryNum);
     }
 }
