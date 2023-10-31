e2e175441cac2cfaf5f9e63d0422839de3f39d13
ChanUi <hcu55@naver.com>
2023-10-31 17:10:45 +0900

    feat: 사용자 입력에 대한 검증 서비스 구현

---
+++ src/main/java/racingcar/validator/InputValidateService.java
@@ -0,0 +1,20 @@
+package racingcar.validator;
+
+public class InputValidateService {
+    private final InputCarNameValidator inputCarNameValidator = new InputCarNameValidator();
+    private final InputTryCountValidator inputTryCountValidator = new InputTryCountValidator();
+
+    public void validateInputCarName(String carName) {
+        inputCarNameValidator.validateNonInputCarName(carName);
+        inputCarNameValidator.validateOverFiveCarName(carName);
+        inputCarNameValidator.validateNonCommaCarName(carName);
+        inputCarNameValidator.validateWrongType(carName);
+    }
+
+    public void validateInputTryCount(String tryCount) {
+        inputTryCountValidator.validateNonInputTryCount(tryCount);
+        inputTryCountValidator.validateNonIntegerTryCount(tryCount);
+        inputTryCountValidator.validateNonOverOneTryCount(tryCount);
+        inputTryCountValidator.validateMinusTryCount(tryCount);
+    }
+}
