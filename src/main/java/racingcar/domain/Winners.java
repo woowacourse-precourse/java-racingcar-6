b06010b7a80131076e3da8d2b8bb5e896aa7f676
MingyeomKim <mingyum119@naver.com>
2023-11-01 04:15:25 +0900

    feat(Winners): 우승자를 저장하는 도메인 생성
    Cars와 별개로, toString 오버라이딩, 코드 가독성 등을 위해 우승자를 저장하는 클래스를 생성한다.

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import static racingcar.global.constants.MessageType.FINAL_RESULT_MESSAGE;
+import static racingcar.global.constants.SymbolType.FINAL_RESULT_TOKEN;
+
+import java.util.List;
+
+public class Winners {
+    private List<Car> winners;
+
+    private Winners(Cars cars) {
+        this.winners = findWinners(cars);
+    }
+
+    public static Winners of(Cars cars) {
+        return new Winners(cars);
+    }
+
+    private List<Car> findWinners(Cars cars) {
+        return cars.findMaxPositionCars();
+    }
+
+    @Override
+    public String toString() {
+        List<String> names = convertCarListToNames(winners);
+        String winners = String.join(FINAL_RESULT_TOKEN.getSymbol(), names);
+        return String.format(FINAL_RESULT_MESSAGE.getMessage(), winners);
+    }
+
+    private List<String> convertCarListToNames(List<Car> winners) {
+        return winners.stream().map(Car::getName).toList();
+    }
+}
