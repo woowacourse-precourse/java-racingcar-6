03245174f481ce4fdcd11ad2a1151b907587b9ee
IE-MangChi <hsm983278@gmail.com>
2023-10-31 16:54:49 +0900

    refactor: 객체 -> 배열 로 파라매터 변경
    출력 역할만 하도록 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,28 +3,24 @@ package racingcar.view;
 import racingcar.model.Car;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
-    public static void outputCarMoveResult(Car car) {
-        System.out.println("실행 결과");
-        List<String> carNames = car.getCarName();
-        List<Integer> carPositions = car.getCarPosition();
 
-        for (int i=0; i<carNames.size(); i++) {
-            System.out.println(carNames.get(i) + " : " + "-".repeat(carPositions.get(i)));
-        }
+    public static void outputCarMoveResult(Map<String, Integer> carData) {
+        System.out.print("실행 결과\n");
+
+        carData.forEach((key, value) -> {
+            int intValue = value;
+            System.out.print(key + " : " + "-".repeat(intValue)+"\n");
+        });
+
         System.out.println();
     }
 
-    public static void outputWinnerResult(Car car, List<Integer> winnerIndex) {
+    public static void outputWinnerResult(List<String> winners) {
         System.out.print("최종 우승자 : ");
 
-        System.out.print(car.getCarName().get(winnerIndex.get(0)));
-        for (int i=1; i<winnerIndex.size(); i++) {
-            System.out.print(", " + car.getCarName().get(i));
+        System.out.print(String.join(",", winners));
         }
-
-
-
-    }
 }
