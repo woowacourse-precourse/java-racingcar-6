5b385c09e7ef1b6d70e876bba90daac39a9cd149
sora0319 <sojeoung536985@gmail.com>
2023-10-31 19:53:07 +0900

    feat(NameValidation.checkWord) : 차 이름 유효성 검사 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,61 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView read = new InputView();
+        OutputView write = new OutputView();
+
+        GameSystem system = new GameSystem();
+        system.start(read, write);
+
     }
 }
+
+
+class GameSystem {
+    public void start(InputView read, OutputView write) {
+
+    }
+
+}
+
+class InputView {
+
+
+}
+
+class OutputView {
+
+}
+
+class NameValidation {
+    public void checkWord(String input) {
+        if (!input.matches("^([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*$")) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
+
+class TryValidation {
+
+}
+
+class InputManufacture {
+
+}
+
+class TotalCar {
+
+}
+
+class Car {
+
+}
+
+class CarCondition {
+
+}
+
+class Winner {
+
+}
+
