662002b86840177f1fa6000f372370f2743630e6
Chun Hyunwoo <1000chw@gmail.com>
2023-11-01 08:21:50 +0900

    feat: OutputTest Update
    출력이 정상적으로 나오는지 테스트

--- src/test/java/racingcar/view/OutputTest.java
+++ src/test/java/racingcar/view/OutputTest.java
@@ -1,4 +1,44 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OutputTest {
+    private final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setOs() {
+        System.setOut(new PrintStream(os));
+    }
+
+    @AfterEach
+    void resetOs() {
+        System.setOut(System.out);
+        os.reset();
+    }
+
+    @Test
+    void printCarResult_차가_간_거리_출력(){
+        String expectedResult = "harry : ---";
+        Output.printCarResult("harry", 3);
+        assertThat(os.toString().strip()).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void printRaceResult_최종_우승자_출력(){
+        String expectedResult = "최종 우승자 : harry, ron";
+        List<String> names = new ArrayList<>();
+        names.add("harry");
+        names.add("ron");
+        Output.printRaceResult(names);
+        assertThat(os.toString().strip()).isEqualTo(expectedResult);
+    }
 }
