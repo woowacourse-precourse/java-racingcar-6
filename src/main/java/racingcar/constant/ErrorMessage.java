f75b02acfc34dbd274ddadd574e0018c2700d8b0
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:21:32 +0900

    feat(ErrorMessage): ErrorMessage 상수 클래스 구현

---
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -0,0 +1,12 @@
+package racingcar.constant;
+
+public class ErrorMessage {
+    public static final String CAR_NAME_LENGTH_ERROR = "자동차 이름은 1~5자 이하만 가능합니다.";
+    public static final String CAR_NAME_DUPLICATE_ERROR = "자동차 이름은 중복될 수 없습니다.";
+    public static final String CAR_NAME_EMPTY_ERROR = "자동차 이름은 공백일 수 없습니다.";
+    public static final String CAR_NAME_FORMAT_ERROR = "자동차 이름은 알파벳만 가능합니다.";
+
+    public static final String ATTEMPTS_FORMAT_ERROR = "시도 횟수는 숫자만 가능합니다.";
+    public static final String ATTEMPTS_EMPTY_ERROR = "시도 횟수는 공백일 수 없습니다.";
+    public static final String INVALID_ATTEMPTS_ERROR = "유효하지 않은 시도 횟수입니다.";
+}
