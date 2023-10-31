105710a0915692a36ba7a08140283ad2d82aac69
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 23:03:01 +0900

    [Test](ExceptionText) - TestCode 추가

--- src/test/java/racingcar/ExceptionTest.java
+++ src/test/java/racingcar/ExceptionTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ExceptionTest {
-  
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest {
+
+    @Test
+    public void testLengthCheck() {
+        Exception exception = new Exception();
+
+        exception.lengthCheck("tjwls,MJ"); // 옳은 입력
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            exception.lengthCheck("audwls"); // 틀린 입력
+        });
+    }
+
+    @Test
+    public void testStringCompile() {
+        Exception exception = new Exception();
+
+        exception.stringCompile("tjwls,MJ");
+        
+        assertThatThrownBy(() -> exception.stringCompile("Invalid#,MJ!"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 입력이 아닙니다.");
+    }
 }
