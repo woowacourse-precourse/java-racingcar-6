27338a0d4fa59d2dc42d140e7f6ab7e45f72d2fc
Cycrypto <jh01love@naver.com>
2023-10-31 12:10:01 +0900

    refactor:게임 결과 콜렉션 변경으로 인한 RacingCar콜랙션 매서드 생성 및 변경

--- src/main/java/racingcar/model/RacingCarList.java
+++ src/main/java/racingcar/model/RacingCarList.java
@@ -2,19 +2,23 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static racingcar.validator.InputValidation.*;
 
-public class RacingCarList {
+public class RacingCarList{
     private List<RacingCar> car;
     private final static String SEPARATE_REGEX = ",";
 
     public RacingCarList(String inputs) {
+        car = new ArrayList<>();
         splitByComma(inputs);
+    }
+    public RacingCarList(){
+        car = new ArrayList<>();
     }
 
     private void splitByComma(String inputs) {
-        car = new ArrayList<>();
         String[] names = inputs.split(SEPARATE_REGEX);
         for (String name : names) {
             validNameFormat(name);
@@ -27,6 +31,17 @@ public class RacingCarList {
         return this.car.size();
     }
 
+    public void add(RacingCar car){
+        this.car.add(car);
+    }
+
+    public void clear(){
+        this.car.clear();
+    }
+
+    public Stream<RacingCar> stream(){
+        return this.car.stream();
+    }
     public RacingCar getCar(int index) {
         return this.car.get(index);
     }
