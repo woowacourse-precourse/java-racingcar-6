73531d60038c399a3029e45a10161937bdfb6ea9
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-01 00:53:23 +0900

    feat : Car 생성자 구현

---
+++ src/main/java/Object/Car.java
@@ -0,0 +1,12 @@
+package Object;
+
+public class Car {
+    private String name;
+    private int postion = 0;
+    private final int MOVE_THRESHOLD = 4;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+}
