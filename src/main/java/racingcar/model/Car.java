d466339d8f487ed373702dc867d5a6da3671ec10
leedabin2 <12193152@inha.edu>
2023-10-31 12:44:59 +0900

    feat(Car): add getName and getPosition methods
    Add getName method to return the name of the car.
    Add getPosition method to return the current position of the car.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private static final int MIN_NUMBER_FOR_MOVE = 4;
@@ -8,6 +9,17 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (isMovable(randomNumber)) {
+            position++;
+        }
+    }
+
+    private boolean isMovable(int randomNumber) {
+        return randomNumber >= MIN_NUMBER_FOR_MOVE;
     }
 
     public String getName() {
