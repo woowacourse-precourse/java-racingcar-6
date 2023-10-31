95e471b15339af31f043e39d0dfdfdd23a3f45f4
Jihyeon Park <mmmszip@gmail.com>
2023-11-01 05:02:51 +0900

    feat: 입력값에 따른 예외 처리 구현

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -1,5 +1,8 @@
 package racingcar.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidator {
     private InputValidator() {
     }
@@ -10,11 +13,36 @@ public class InputValidator {
                 throw new IllegalArgumentException(Constants.NAME_LENGTH_EXCEED);
             }
         }
+        if (containsDuplicate(carNames)) {
+            throw new IllegalArgumentException(Constants.DUPLICATE_CAR_NAMES);
+        }
+        if (isEmpty(carNames)) {
+            throw new IllegalArgumentException(Constants.EMPTY_CAR_NAMES);
+        }
     }
 
     public static void validateTryCount(int tryCount) {
         if (tryCount <= 0) {
             throw new IllegalArgumentException(Constants.INVALID_TRY_COUNT);
         }
+    }
+
+    private static boolean containsDuplicate(String[] carNames) {
+        Set<String> uniqueNames = new HashSet<>();
+        for (String carName : carNames) {
+            if (!uniqueNames.add(carName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isEmpty(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName == null || (carName.trim().isEmpty() || carName.isBlank())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
