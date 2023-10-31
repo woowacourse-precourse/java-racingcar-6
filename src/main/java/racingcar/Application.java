c85c7c713c6d19860664b223a7d0157ceba548e1
lkdcode <dlrlejr1@naver.com>
2023-10-31 15:49:39 +0900

    Add : Application 1차 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.config.RacingCarGameFactory;
+import racingcar.domain.RacingCarGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final RacingCarGame racingCarGame = RacingCarGameFactory.createSingletonRacingCarGame();
+        racingCarGame.start();
     }
+
 }
