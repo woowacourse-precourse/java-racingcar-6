b7a2ab013e17991719e72e12c9d2606bad3eb027
jihun <pjh2996@naver.com>
2023-10-31 14:23:36 +0900

    test : 이름 입력 시 공백만 들어올 경우 오류 테스트

--- src/test/java/racingcar/model/CarGameTest.java
+++ src/test/java/racingcar/model/CarGameTest.java
@@ -1,11 +1,13 @@
 package racingcar.model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarGameTest {
 
-    private final CarGame car = new CarGame();
+    private final CarGame carGame = new CarGame();
 
     @Test
     public void 이름_입력시_양쪽_공백은_문자_길이에_미포함() {
@@ -13,11 +15,19 @@ class CarGameTest {
         String input = " JiHun ";
 
         //when
-        car.setCarsName(input);
-        int length = car.getCarsName().get(0).getName().length();
+        carGame.setCarsName(input);
+        int length = carGame.getCarsName().get(0).getName().length();
 
         //then
         Assertions.assertThat(5).isEqualTo(length);
+    }
 
+    @Test
+    public void 자동차_이름_한개_이상_참여_공백만_입력은_금지() {
+        String input = " ";
+
+        assertThatThrownBy(() -> carGame.setCarsName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("한 대 이상의 자동차가 있어야 합니다.");
     }
 }
