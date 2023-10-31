0ee3b2ba29f223027b9d026878ea8c57145e5c9f
MeoungHum <emmabark84@gmail.com>
2023-10-31 21:12:52 +0900

    자동차 전진 속성 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
+    private int distance;
 
     public Car(String name) {
         this.name = name;
+        this.distance = 0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0,9) >= 4) {
+            distance++;
+        }
     }
 }
 
