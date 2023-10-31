9d68408c7e32962f82b76fd1812689bc8572b4b1
aeyongdodam <kikititi0510@gmail.com>
2023-11-01 04:15:12 +0900

    refactor(RacingService): 캡슐화 원칙 적용

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,29 +1,18 @@
 package racingcar.service;
 
-import java.util.List;
-import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.utils.RandomNumberGenerator;
 
 public class RacingService {
     private static final int FORWARD_THRESHOLD = 4;
-    private final List<Car> cars;
     private final RandomNumberGenerator randomNumberGenerator;
 
-    public RacingService(List<Car> cars) {
-        this.cars = cars;
+    public RacingService() {
         this.randomNumberGenerator = new RandomNumberGenerator();
     }
 
-    private void playRacing() {
-        for (Car car : cars) {
-            if (canMoveForward()) {
-                car.moveForward();
-            }
-        }
-    }
-
-    private boolean canMoveForward() {
-        return randomNumberGenerator.createRandomNumber() >= FORWARD_THRESHOLD;
+    public void playRacing(Cars cars) {
+        cars.moveCars(randomNumberGenerator, FORWARD_THRESHOLD);
     }
 
 }
