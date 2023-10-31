440464f81eb5faac39c08d9196820abd9c316e9b
iceprins <junghankyul@gmail.com>
2023-10-31 14:23:17 +0900

    refactor: isIncludeBlank 메소드의 출력 메시지 수정

--- src/main/java/racingcar/service/CarNameCheck.java
+++ src/main/java/racingcar/service/CarNameCheck.java
@@ -4,13 +4,13 @@ public class CarNameCheck {
 
     public void isNothing(String carName) {
         if (carName.length() == 0) {
-            throw new IllegalArgumentException("입력값이 없습니다.");
+            throw new IllegalArgumentException("입력값이 없습니다. 자동차 이름을 입력해주세요.");
         }
     }
 
     public void isIncludeBlank(String carName) {
         if (carName.contains(" ")) {
-            throw new IllegalArgumentException("자동차 이름에 공백이 포함됩니다.");
+            throw new IllegalArgumentException("자동차 이름에 공백이 포함됩니다. 자동차 이름은 쉼표(,)로 구분되오니 공백 없이 올바르게 입력해주세요.");
         }
     }
 
