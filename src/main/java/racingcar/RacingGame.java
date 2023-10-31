1d6feada246c19921165c1c07a9c4f1e94f2cd1e
minjong kim <godminjong@naver.com>
2023-10-31 17:36:34 +0900

    feat: 게임을 진행 전 정보 입력 받는 기능 추가
    게임을 시작하고 참여자의 수와 라운드 수에 대한 정보를 입력받고 해당 입력을 알려주는 문구 출력하는 기능 추가 작성

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,8 +1,46 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.services.WinnerDetector.printAllWinners;
+
+import racingcar.constant.Constant;
 import racingcar.services.RaceGameState;
+import racingcar.services.RaceRoundSimulator;
+import racingcar.validate.Validator;
+import racingcar.validate.impls.NumberOfParticipantValidator;
 
 public class RacingGame {
     private RaceGameState state;
 
+    public RacingGame(String listOfParticipants) {
+        this.state = new RaceGameState(listOfParticipants);
+    }
+
+    public void startRacing(){
+        getParticipantNamesFromConsole();
+        int numberOfRounds = getNumberOfRoundsFromConsole();
+        printEachRoundResult(numberOfRounds);
+        printAllWinners(state);
+    }
+
+    private void printEachRoundResult(int numberOfRounds){
+        System.out.println(Constant.EXECUTE_RESULT);
+        for (int i =0 ; i< numberOfRounds;i++){
+            RaceRoundSimulator.execute(state);
+        }
+    }
+    private void getParticipantNamesFromConsole(){
+        System.out.println(Constant.START_MESSAGE);
+        String participantsList = readLine();
+        this.state = new RaceGameState(participantsList);
+    }
+
+    private int getNumberOfRoundsFromConsole(){
+        System.out.println(Constant.ROUND_NUMBER_MESSAGE);
+        String numberOfRounds = readLine();
+        Validator validator = new NumberOfParticipantValidator();
+        validator.validate(numberOfRounds);
+        System.out.println();
+        return Integer.parseInt(numberOfRounds);
+    }
 }
