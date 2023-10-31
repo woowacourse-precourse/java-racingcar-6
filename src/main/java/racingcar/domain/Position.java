ad5a7cf4d135eabc21e747ba8bc75f91115b381f
Arachneee <jhg2819@naver.com>
2023-10-31 16:35:54 +0900

    refactor: compareTo 메소드 추가
    바로 비교할 수 있게 Comparable을 구현하도록 변경했습니다.

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,33 +1,35 @@
 package racingcar.domain;
 
 
+import static racingcar.constant.CarConstant.CAR_ONE_STEP;
+import static racingcar.constant.CarConstant.CAR_START_POSITION;
 import static racingcar.exception.ErrorMessage.NEGATIVE_POSITION;
 
 import java.util.Objects;
 import racingcar.exception.RacingGameException;
 
-public class Position {
-    private static final int CAR_ONE_STEP = 1;
-    private int position;
+public class Position implements Comparable<Position> {
 
-    public Position(int position) {
-        validate(position);
-        this.position = position;
+    private int value;
+
+    public Position(int value) {
+        validate(value);
+        this.value = value;
     }
 
     private void validate(int position) {
-        if (position < 0) {
+        if (position < CAR_START_POSITION) {
             throw RacingGameException.of(NEGATIVE_POSITION);
         }
 
     }
 
     public void plus() {
-        this.position += CAR_ONE_STEP;
+        this.value += CAR_ONE_STEP;
     }
 
     public int getValue() {
-        return position;
+        return value;
     }
 
     @Override
@@ -41,12 +43,24 @@ public class Position {
         }
 
         Position position1 = (Position) o;
-        return position == position1.position;
+        return value == position1.value;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(position);
+        return Objects.hash(value);
     }
 
+    @Override
+    public int compareTo(Position o) {
+        if (this.equals(o)) {
+            return 0;
+        }
+
+        if (this.value > o.value) {
+            return 1;
+        }
+
+        return -1;
+    }
 }
