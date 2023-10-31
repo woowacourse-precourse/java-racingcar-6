8e0d33df92912d6c8a5c1c30c955461a6472019f
minisyu <iswater.kim@gmail.com>
2023-10-31 11:48:44 +0900

    feat(Configuration): GameController 및 자원 설정 구성 추가

---
+++ src/main/java/racingcar/config/Configuration.java
@@ -0,0 +1,34 @@
+package racingcar.config;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.GameController;
+import racingcar.converter.InputConverter;
+import racingcar.io.reader.InputReader;
+import racingcar.io.reader.StdReader;
+import racingcar.io.writer.OutputWriter;
+import racingcar.io.writer.StdWriter;
+import racingcar.move.MoveStrategy;
+import racingcar.move.NumberGenerator;
+import racingcar.move.RandomMoveStrategy;
+import racingcar.move.RandomNumberGenerator;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class Configuration {
+    public static GameController getGameController() {
+        StdReader stdReader = new InputReader();
+        StdWriter stdWriter = new OutputWriter();
+        OutputView outputView = new OutputView(stdWriter);
+        InputValidator inputValidator = new InputValidator();
+        InputConverter inputConverter = new InputConverter(inputValidator);
+        InputView inputView = new InputView(stdReader, stdWriter, inputConverter);
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator(0, 9);
+        MoveStrategy randomMoveStrategy = new RandomMoveStrategy(randomNumberGenerator);
+        return new GameController(inputView, outputView, randomMoveStrategy);
+    }
+
+    public static void closeResources() {
+        Console.close();
+    }
+}
