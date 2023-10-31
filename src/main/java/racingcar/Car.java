d8c2b8125d938d68019d8ad83f5712af8d5fc675
heemin2728 <idkhm0728@gmail.com>
2023-11-01 01:45:34 +0900

    feat : 자동차 객체 생성 및 전진 기능

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int currentStep = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public boolean isMovingForward() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= 4;
+    }
+
+    public void startGameTurn() {
+        if (isMovingForward()) {
+            currentStep++;
+        }
+    }
+}
