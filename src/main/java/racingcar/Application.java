6e4492723f06d42b69a0d62afdde9d2a9d895c09
hajungIm <ihj_ewha@ewhain.net>
2023-10-31 18:03:45 +0900

    Feat(CarRacing): 레이싱 경기 기능 구현
    `CarRacing` 생성자
    - 입력받은 cars 리스트 필드 주입
    - `carPos` ArrayList 생성 후 자동차 수 만큼 0으로 초기화
    
    `race`
    - turn 수 만큼 주행
    
    `oneTurnRace`
    - 모든 차들이 랜덤값에 따라 전진 or 멈춤 수행
    - carPos 필드에 위치 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,15 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.domain.CarRacing;
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         List<String> cars = InputView.inputCars();
         int turn = InputView.inputTurn();
+
+        CarRacing carRacing = new CarRacing(cars);
+        carRacing.race(turn);
     }
 }
--- src/main/java/racingcar/domain/CarRacing.java
+++ src/main/java/racingcar/domain/CarRacing.java
@@ -1,5 +1,35 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.view.OutputView;
+
 public class CarRacing {
 
+    List<String> cars;
+    List<Integer> carPos;
+
+    public CarRacing(List<String> cars) {
+        this.cars = cars;
+        this.carPos = new ArrayList<>();
+        for (int i = 0; i < cars.size(); i++) {
+            carPos.add(0);
+        }
+    }
+
+    public void race(int turn) {
+        while (turn-- > 0) {
+            oneTurnRace();
+        }
+    }
+
+    private void oneTurnRace() {
+        for (int i = 0; i < cars.size(); i++) {
+            int r = Randoms.pickNumberInRange(0, 9);
+            if (r >= 4) {
+                carPos.set(i, carPos.get(i) + 1);
+            }
+        }
+    }
 }
