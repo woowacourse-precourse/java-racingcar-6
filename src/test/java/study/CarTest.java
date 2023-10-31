96a6f3bfad27cb5ab6473d2ca8d5e0c949dc1972
yyubin <hazing120@gmail.com>
2023-11-01 03:40:17 +0900

    test: Car test 수정

--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -3,19 +3,32 @@ package study;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
+
     @Test
-    void testValidCarName() {
-        assertDoesNotThrow(() -> new Car("Valid"));
+    void 생성_유효한_자동차() {
+        // 유효한 자동차 이름인 경우
+        Car car = new Car("pobi");
+        assertThat(car.getName()).isEqualTo("pobi");
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Test
-    void testInvalidCarName() {
-        assertThrows(IllegalArgumentException.class, () -> new Car("ThisNameIsTooLong"));
-        assertThrows(IllegalArgumentException.class, () -> new Car(""));
+    void 생성_무효한_자동차_이름() {
+        // 무효한 자동차 이름인 경우
+        assertThatThrownBy(() -> new Car("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Car("123456")).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 이동_결과_문자열_확인() {
+        Car car = new Car("pobi");
+        assertThat(car.getMoveResult()).isEqualTo("");
+        car.move();
+        assertThat(car.getMoveResult()).isEqualTo("-");
+    }
 }
