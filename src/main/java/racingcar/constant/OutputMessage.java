0e1aa45059962d003018e9e3841ce05f6d087c6c
limzzum <chfhddl1234@naver.com>
2023-11-01 01:10:42 +0900

    :sparkles: feat : 출력 메시지 클래스 상수 정의

---
+++ src/main/java/racingcar/constant/OutputMessage.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class OutputMessage {
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String RUN_RESULT = "실행 결과";
+    public static final String WINNER_RESULT = "최종 우승자 : ";
+}
