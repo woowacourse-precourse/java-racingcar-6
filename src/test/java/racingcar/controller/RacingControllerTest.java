3163f60b897e30a9b13c3a980197ca07b88e1b08
hong <kevin0928@naver.com>
2023-11-01 01:06:32 +0900

    Test: RacingControllerTest 구현
    - Controller에서는 비즈니스 로직을 사용하는 코드 밖에 없음. 그로인해 메서드 호출 횟수로 테스트 구현

--- src/test/java/racingcar/controller/RacingControllerTest.java
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -1,8 +1,16 @@
 package racingcar.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import racingcar.service.RacingService;
@@ -25,5 +33,19 @@ class RacingControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         racingController = new RacingController(inputView, racingService, outputView);
+    }
+    @Test
+    void runShouldInteractWithInputViewRacingServiceAndOutputView() {
+        List<String> carNames = Arrays.asList("용준", "용준1", "용준2");
+        when(inputView.getCarNames()).thenReturn("용준,용준1,용준2");
+        when(racingService.stringToList(anyString())).thenReturn(carNames);
+        when(inputView.getTries()).thenReturn(3);
+
+        racingController.run();
+
+        verify(inputView, times(1)).getCarNames();
+        verify(inputView, times(1)).getTries();
+        verify(racingService, times(1)).stringToList(anyString());
+        verify(outputView, times(1)).printGameResult(any());
     }
 }
