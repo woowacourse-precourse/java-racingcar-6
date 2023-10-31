1395e3a80d53e23992107c08b540889ba9340f80
dustjq1004 <dustjq1005@naver.com>
2023-10-31 18:29:10 +0900

    yeonsup/feat : Track 클래스 추가

---
+++ src/main/java/racingcar/Track.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.List;
+
+public class Track {
+
+    private final int raceCount;
+    private final List<RacingCar> acingCars;
+
+    public Track(int raceCount, List<RacingCar> acingCars) {
+        this.raceCount = raceCount;
+        this.acingCars = acingCars;
+    }
+
+    public void race() {
+
+    }
+}
