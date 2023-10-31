f2a13a9e314287075896be705ca555ffdaee8ed0
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 00:48:52 +0900

    feat : 전진 기호 출력하는 기능 로직 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,23 +1,34 @@
 package racingcar.view;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String FORWARD_SYMBOL = "-";
     private static final String GAME_RESULT = "실행결과";
     private static final String MVP = "최종 우승자 : ";
-    private static final String Comma = " ,";
+    private static final String Comma = ", ";
 
-    public void printForward(String carName, int forward){
-        String forwardSymbol = FORWARD_SYMBOL.repeat(forward);
-        System.out.println(carName + " : " + forwardSymbol);
-    }
-    public void printResult(){
+    public static void printResult(){
         System.out.println("");
         System.out.println(GAME_RESULT);
     }
 
-    public void printMVP(List<String> mvpList) {
+    public static void printMVP(List<String> mvpList) {
         System.out.println(MVP + String.join(Comma, mvpList));
+    }
+
+    public static void carPosition(int forward){
+        for(int i = 0; i < forward; i++){
+            System.out.print(FORWARD_SYMBOL);
+        }
+        System.out.println();
+    }
+    public static void printForward(LinkedHashMap<String, Integer> forwardStateList){
+        for(Map.Entry<String, Integer> entry : forwardStateList.entrySet()){
+            System.out.print(entry.getKey()+ " : ");
+            carPosition(entry.getValue());
+        }
     }
 }
