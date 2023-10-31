6f37ab922a62328dd38b95d0176d46e27d471803
chaeshee0908 <chaeshee0908@naver.com>
2023-10-31 20:01:44 +0900

    [feat] 차 이름 null 입력시 예외처리 테스트 구현

---
+++ src/test/java/racingcar/util/io/InputUtilsTest.java
@@ -0,0 +1,15 @@
+package racingcar.util.io;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class InputUtilsTest {
+
+    @Test
+    void 차_이름_null_입력_예외_처리() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputUtils.validateCarNameInput(null);
+        });
+    }
+}
