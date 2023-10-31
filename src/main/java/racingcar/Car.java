47d0c008abf9ec33ec1fb62e313367593d9713c1
hyuuuun <pam4522@gmail.com>
2023-10-31 23:42:20 +0900

    feat(Car):Car.java 모델 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,31 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+        private String name;
+        private int position;
+
+        public Car(String name) {
+                this.name = name;
+                this.position = 0;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getPosition() {
+                return position;
+        }
+
+        public void move() {
+                position++;
+        }
+
+        public void printCar() {
+                System.out.print(name + " : ");
+                for (int i = 0; i < position; i++) {
+                        System.out.print("-");
+                }
+                System.out.println();
+        }
 }
