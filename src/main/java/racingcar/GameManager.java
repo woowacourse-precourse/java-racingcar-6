88a9293fc12ee847eeb35c8b81657925912dfcc7
j2noo <wlsdndml213@naver.com>
2023-11-01 01:19:43 +0900

    feat : 라운드 진행 메서드

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class GameManager {
 
   private List<Car> cars = new ArrayList<Car>();
+  private List<Integer> progressState = new ArrayList<Integer>();
   private int trialCount;
   private int currentCount;
 
@@ -15,9 +16,16 @@ public class GameManager {
     List<String> inputedCars = View.inputCars();
     inputedCars.forEach(car -> cars.add(new Car(car)));
 
+    for(int i=0;i<inputedCars.size();i++){
+      progressState.add(0);
+    }
+
     View.printGameTrialCount();
     trialCount = View.inputGameTrialCount();
     currentCount = 0;
+
+    
+
   }
 
   public void start() {
@@ -35,5 +43,12 @@ public class GameManager {
     currentCount++;
     System.out.println(currentCount+"라운드 진행");
     
+    for(int i=0;i<cars.size();i++){
+      if(cars.get(i).isProgress()){
+        progressState.set(i,progressState.get(i)+1);
+      }
+    }
+
+    System.out.println(progressState);
   }
 }
