2c132b1813fddec489011bc22ed2cb2bcab82f9e
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-10-31 14:06:10 +0900

    Feature : 예외 처리 구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -2,10 +2,23 @@ package racingcar;
 
 public class Validator {
     void validateGivenCarNames(String carNames) {
+        String[] names = carNames.split(",");
+        if (names.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
 
     }
 
     void validateCount(String count) {
-
+        try {
+            int parsed = Integer.parseInt(count);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
