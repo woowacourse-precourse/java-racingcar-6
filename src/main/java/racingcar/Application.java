b8520e3cbce4f7df737ff6a20721dc0073db7cdd
Cuzz <rygus9@naver.com>
2023-10-31 22:42:31 +0900

    feat: 전체 흐름 로직 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingGame;
+import racingcar.dto.RacingCarDto;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.inputCars();
+        List<String> racingCarNames = InputView.racingCars();
+        OutputView.inputTurn();
+        int gameTurn = InputView.gameTurn();
+
+        RacingGame racingGame = new RacingGame(racingCarNames);
+
+        OutputView.progressGameIntro();
+
+        for(int i = 0; i < gameTurn; i++){
+            List<RacingCarDto> racingCarDtoList = racingGame.progress();
+            OutputView.gameProgress(racingCarDtoList);
+        }
+
+        OutputView.gameResult(racingGame.result());
     }
 }
