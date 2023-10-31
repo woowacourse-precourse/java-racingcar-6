845b0447152026fb907bb649c7f7ec8e4e780ae1
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-01 03:56:53 +0900

    feat: 입력 값 예외처리 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -18,12 +18,12 @@ public class Application {
 
     }
 
-    private static int UserEnterRepeatNumber() {
+    private static int UserEnterRepeatNumber() throws IllegalArgumentException{
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(readLine());
     }
 
-    private static String UserEnterCarName() {
+    private static String UserEnterCarName() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return readLine();
     }
