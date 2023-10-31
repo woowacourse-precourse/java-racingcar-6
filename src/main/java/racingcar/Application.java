7c1fb17f3034db389a4c8acb426a53f1fa1d4b4b
Jaeyoung Lim <limjyjustin@gmail.com>
2023-10-31 12:52:31 +0900

    feat(Application): 시도할 회수 입력 기능 구현
    Application 클래스 내부 main 함수 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceController race = new RaceController();
+
+        String carInput = Console.readLine();
+        String[] racingCarList = race.ready(carInput);
+
+        String roundInput = Console.readLine();
+        race.setRound(Integer.parseInt(roundInput));
     }
 }
