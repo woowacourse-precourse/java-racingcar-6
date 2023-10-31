de0a781438bd0bb13f05bb3c8c50c0e3e8fbe2e7
박승민 <tmddudals369@naver.com>
2023-11-01 01:56:33 +0900

    feat(outputView): 게임 진행 상황 안내 메세지를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_GAME_PROGRESS_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String GAME_PROCESS_MESSAGE = "\n실행 결과";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -15,6 +16,10 @@ public class OutputView {
 
         System.out.println(INPUT_GAME_PROGRESS_COUNT_MESSAGE);
 
+    }
+
+    public void printGameProcessMessage() {
+        System.out.println(GAME_PROCESS_MESSAGE);
     }
 
     public void printGameProcessResult(CarResultResponse carResultResponse) {
