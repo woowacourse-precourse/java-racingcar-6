cb9e7fe1f8d2216e2f25a8c04b1c12a647b5dd6f
jsj <tkdlek4186@naver.com>
2023-10-31 14:42:23 +0900

    feat : 공백 및 중복입력 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -41,7 +41,7 @@ class ApplicationTest extends NsTest {
     @Test
     void 중복에_대한_예외_처리() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,java,pubi","1"))
+                assertThatThrownBy(() -> runException("pobi,java,pobi","1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
