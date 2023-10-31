34721366afa0c43d3e1fa16378c7f21c707efcd6
jiu6525 <jiu6525@daum.net>
2023-10-31 16:42:27 +0900

    style : ResultView 클래스의 매직넘버 상수화

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -7,8 +7,15 @@ import racingcar.vo.Car;
 
 public class ResultView {
 
+    private static final String RACING_RESULT = "실행결과";
+    private static final String RACING_RESULT_WINNERS = "최종 우승자 : ";
+    private static final String CAR_POSITION_DISPLAY = "-";
+    private static final String USER_INPUT_SEPARATOR = ",";
+    private static final String CAR_NAME_BORDER = " : ";
+    private static final String LINE_BREAK = "";
+
     public ResultView() {
-        System.out.println("실행결과");
+        System.out.println(RACING_RESULT);
     }
 
     public void RacingResult(Map<Car, List<Integer>> positionHistory, int attempt) {
@@ -18,21 +25,21 @@ public class ResultView {
                 List<Integer> position = racing.getValue();
                 showRacingResult(car, position.get(i));
             }
-            System.out.println("");
+            System.out.println(LINE_BREAK);
         }
     }
 
     private void showRacingResult(Car car, int position) {
-        System.out.print(car.getName() + " : ");
+        System.out.print(car.getName() + CAR_NAME_BORDER);
         for (int i = 0; i < position; i++) {
-            System.out.print("-");
+            System.out.print(CAR_POSITION_DISPLAY);
         }
-        System.out.println("");
+        System.out.println(LINE_BREAK);
     }
 
     public void showRacingWinners(List<String> winners) {
-        String names = String.join(",", winners);
-        System.out.println("최종 우승자 : " + names);
+        String names = String.join(USER_INPUT_SEPARATOR, winners);
+        System.out.println(RACING_RESULT_WINNERS + names);
     }
 
 }
