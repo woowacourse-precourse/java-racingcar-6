d99747a8e9f860379589817c2cf6ce3225bf3cba
dvlprjw <dvlprjw@gmail.com>
2023-10-31 22:42:15 +0900

    feat: Test 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,10 +9,8 @@ public class Application {
     private static List<Car> cars;
 
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        System.out.println("시도할 회수는 몇회인가요?");
-        int tryCount = Integer.parseInt(Console.readLine());
+        String carNames = inputCarNames();
+        int tryCount = inputTryCount();
 
         cars = CarUtils.createCars(carNames);
         System.out.println();
@@ -22,7 +20,21 @@ public class Application {
             play();
         }
 
-        System.out.println("최종 우승자: " + CarUtils.getWinner(cars));
+        System.out.println("최종 우승자 : " + CarUtils.getWinner(cars));
+    }
+
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int inputTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+        return tryCount;
     }
 
     private static void play() {
