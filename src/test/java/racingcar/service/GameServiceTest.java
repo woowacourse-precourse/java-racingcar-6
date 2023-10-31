535ad3c6ca0ac6c2d2485f4725330b6a48deafe8
WooJJam <jaemin5548@naver.com>
2023-10-31 17:21:14 +0900

    [Test] GameService 테스트

---
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -0,0 +1,21 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import racingcar.validator.RoundCountValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    @DisplayName("게임 횟수 입력")
+    void 게임_횟수_입력(){
+
+        GameService gameService = new GameService();
+        int round = gameService.initGameRound("3");
+
+        assertEquals(3,round);
+    }
+}
