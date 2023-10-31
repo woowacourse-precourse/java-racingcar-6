0df662375e34f4de41055b62f5e68c38471927a6
lkdcode <dlrlejr1@naver.com>
2023-10-31 15:52:24 +0900

    Refactor : Engine
    정적 팩터리 메서드 추가
    매개변수 오타 수정

--- src/main/java/racingcar/domain/console/input/InputParser.java
+++ src/main/java/racingcar/domain/console/input/InputParser.java
@@ -2,8 +2,15 @@ package racingcar.domain.console.input;
 
 public class InputParser {
 
-    public String[] parseUserNames(final String input, final String delimter) {
-        return input.split(delimter);
+    private InputParser() {
+    }
+
+    public static InputParser newInstance() {
+        return new InputParser();
+    }
+
+    public String[] parseUserNames(final String input, final String delimiter) {
+        return input.split(delimiter);
     }
 
     public int parseInt(final String input) {
