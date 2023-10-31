17a0aa181a77c48564823d8ce01b4650db708cae
jyajoo <jyajoo1020@gmail.com>
2023-10-31 23:26:00 +0900

    fix: 자동차 이름 제한 숫자 변경

--- src/main/java/racingcar/constant/RaceSetting.java
+++ src/main/java/racingcar/constant/RaceSetting.java
@@ -3,7 +3,7 @@ package racingcar.constant;
 public enum RaceSetting {
     MIN_MOVE_NUMBER(4),
     MOVE_NUMBER(1),
-    MAX_CAR_NAME_LENGTH(4),
+    MAX_CAR_NAME_LENGTH(5),
     MIN_RANDOM_NUMBER(0),
     MAX_RANDOM_NUMBER(9);
 
