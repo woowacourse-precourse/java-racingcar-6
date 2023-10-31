843be9fb68422975275c69a5c70324b4d8977b08
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-01 01:07:12 +0900

    [Feat](Game) - Game 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,33 @@
 package racingcar;
 
 public class Game {
+    public static final String RESULT_MESSAGE = "실행 결과";
+    private String inputRacerName;
+    private String[] racerNameArray;
+    private int round;
+    private User user;
+    private Racing racing;
+
+    public Game() {
+        user = new User();
+        inputRacingCarName();
+        inputRacingRound();
+        racing = new Racing(racerNameArray, round);
+    }
+
+    private void inputRacingCarName() {
+        this.inputRacerName = user.inputCarName();
+        this.racerNameArray = inputRacerName.split(",");
+    }
+
+    private void inputRacingRound() {
+        this.round = user.inputRound();
+    }
+
+    public void startRacingGame() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+        racing.carRacingGameStart();
+        racing.carRacingGameResult();
+    }
 }
