df321ada0743e2ea5014753b6f601984141817a3
jiohh <dkswlgh99@naver.com>
2023-10-31 16:49:04 +0900

    feat(RoundNumberValidator): create function validator for roundNumber
    create function validator for roundNumber
    
    1. validate roundNumber is digit
    2. validate roundNumber is black

---
+++ src/main/java/racingcar/RoundNumberValidator.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class RoundNumberValidator {
+
+    public static void checkRoundNumber(String readLine) {
+        checkBlank(readLine);
+        checkDigit(readLine);
+    }
+
+    private static void checkBlank(String readLine) {
+        if (readLine.isBlank()) {
+            throw new IllegalArgumentException("빈칸입니다.");
+        }
+    }
+
+    private static void checkDigit(String readLine) {
+        for (int i = 0; i < readLine.length(); i++) {
+            if (!Character.isDigit(readLine.charAt(i))) {
+                throw new IllegalArgumentException("문자를 입력하였습니다.");
+            }
+        }
+    }
+}
---
+++ src/test/java/racingcar/implementtest/RoundNumberValidatorTest.java
@@ -0,0 +1,25 @@
+package racingcar.implementtest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.RoundNumberValidator;
+
+public class RoundNumberValidatorTest {
+
+    @ParameterizedTest
+    @CsvSource({"'-1234'", "'가나다'", "//////"})
+    void 횟수에_문자가_있을때_예외_처리(String testInput) {
+        Assertions.assertThatThrownBy(() -> RoundNumberValidator.checkRoundNumber(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("문자를 입력하였습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"''", "' '", "'  '"})
+    void 횟수에_공백이_있을때_예외_처리(String testInput) {
+        Assertions.assertThatThrownBy(() -> RoundNumberValidator.checkRoundNumber(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("빈칸입니다.");
+    }
+}
