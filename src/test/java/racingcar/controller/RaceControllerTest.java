8d8261402c3583b04b0e7c460cfb38754d03229e
Chan Mi <rkdcksal02@gmail.com>
2023-11-01 05:13:28 +0900

    우승자 결정 테스트

--- src/test/java/racingcar/controller/RaceControllerTest.java
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -22,4 +22,27 @@ public class RaceControllerTest {
             assertThat(car.getPosition()).isGreaterThanOrEqualTo(0);
         }
     }
+    @Test
+    void 자동차_경주_컨트롤러_우승자_확인() {
+        List<Car> cars = Arrays.asList(new Car("pobi"), new Car("woni"), new Car("jun"));
+        RaceController raceController = new RaceController(cars);
+
+        cars.get(0).move();
+        cars.get(0).move();
+        cars.get(1).move();
+        cars.get(2).move();
+
+        List<Car> winners = raceController.getWinners();
+
+        int maxPosition = winners.stream()
+                .map(Car::getPosition)
+                .max(Integer::compare)
+                .orElse(0);
+
+        List<Car> expectedWinners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+
+        assertThat(winners).containsExactlyElementsOf(expectedWinners);
+    }
 }
