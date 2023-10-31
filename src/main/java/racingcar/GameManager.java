e533b7361f43239aab1923d1afae81c1271824cc
j2noo <wlsdndml213@naver.com>
2023-10-31 17:27:36 +0900

    feat : 게임 시작 문구 출력 메서드 작성 완료

---
+++ src/main/java/racingcar/GameManager.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class GameManager {
+  static public void init() {
+    System.out.println("초기화");
+  }
+  static  public void start(){
+    System.out.println("시작");
+  }
+}
