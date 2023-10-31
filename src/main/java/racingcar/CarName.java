e7d030206b29511be6eac7dda7b8641bd591b7d3
noeyodeel <sko8044@gmail.com>
2023-10-31 23:42:14 +0900

    fix : 입력 예외 수정
    자동차 이름 중복 입력 예외처리 오류로 수정함.

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -38,18 +40,22 @@ public class CarName {
     String[] nameArray = input.split(",");
     List<String> names = new ArrayList<>();
     for (String name : nameArray) {
-      names.add(name.trim());
+      name = name.trim(); // 앞뒤 공백 제거
+      name = name.replace(" ", ""); // 중간 공백 제거
+      names.add(name);
     }
     return names;
   }
 
   private void exception_Length(List<String> names) {
+    Set<String> uniqeNames = new HashSet<>();
+
     for (String name : names) {
       if (name.length() > 5 | name.length() < 1) {
-        throw new IllegalArgumentException("1자 이상 5자 이하로 입력하세요.");
+        throw new IllegalArgumentException("1자 이상 5자 이하로 입력하세요." + name);
       }
-      if(name.contains(name)) {
-        throw new IllegalArgumentException("동일한 이름은 입력할 수 없습니다.");        
+      if (!uniqeNames.add(name)) {
+        throw new IllegalArgumentException("동일한 이름은 입력할 수 없습니다." + name);
       }
     }
   }
