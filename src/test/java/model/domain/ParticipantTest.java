407b461e354462fbfbc29d1e39508427d6eb8a22
nayeon Lee <skdus4190@gmail.com>
2023-11-01 02:09:32 +0900

    Test: 테스트 코드 추가(각 이름의 앞뒤 공백 제거)
    ParticipantTest.java에 입력된 각각의 자동차 이름에서 앞뒤 공백 제거 수행에 대한 테스트 코드 작성

--- src/test/java/model/domain/ParticipantTest.java
+++ src/test/java/model/domain/ParticipantTest.java
@@ -15,4 +15,14 @@ class ParticipantTest {
         assertEquals("na", test.getParticipant().get(1).getNameValue());
         assertEquals("yeon", test.getParticipant().get(2).getNameValue());
     }
+
+    @Test
+    void 각_이름의_앞뒤_공백_제거(){
+        String carNames = "lee, na ,  yeon";
+        Participant test = new Participant(carNames);
+
+        assertEquals("lee", test.getParticipant().get(0).getNameValue());
+        assertEquals("na", test.getParticipant().get(1).getNameValue());
+        assertEquals("yeon", test.getParticipant().get(2).getNameValue());
+    }
 }
