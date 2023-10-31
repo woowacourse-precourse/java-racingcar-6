fb1b5a6c161f48f0759bc5c96e329bfcd76b9a86
Yoon <kohyc97@gmail.com>
2023-10-31 16:09:28 +0900

    feat: makeCar(자동차 객체 생성) method  작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -28,5 +28,12 @@ public class Car implements Comparable<Car> {
         return true;
     }
 
-
+    public void makeCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        for (String name : Console.readLine().split(",")) {
+            if (checkLength(name)) {
+                cars.add(new Car(name));
+            }
+        }
+    }
 }
