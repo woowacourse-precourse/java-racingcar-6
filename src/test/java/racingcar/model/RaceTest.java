5777f64608f6c1d984a833aec6a36bdff5f8f34c
Dawin <dawin002@naver.com>
2023-10-31 18:44:51 +0900

    test(RaceTest): 자동차 리스트 반환 관련 기능 테스트 추가
    자동차 리스트 반환 관련 기능 테스트 추가:
    - getCarNames_자동차_객체의_이름_리스트로_반환()
    - getCarListSize_자동차_객체_리스트의_크기_반환()

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -9,68 +9,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RaceTest {
-    private Race race;
-    @BeforeEach
-    void setUp() {
-        race = new Race();
-    }
+//    private Race race;
+//    @BeforeEach
+//    void setUp() {
+//        race = new Race();
+//    }
 
     @Test
     void setCarList_여러_개의_이름으로_초기화된_자동차_객체_리스트_생성() {
         //given
-        Race race1 = new Race();
-        String nameInput = "aaa,bbb,ccc,ddd";
+        Race race = new Race();
+        String nameInput = "dawin,suji,son,blue";
 
         //when
-        race1.setCarList(nameInput);
+        race.setCarList(nameInput);
 
         //then
-        assertThat(race1.getCarNames()).isEqualTo(List.of("aaa", "bbb", "ccc", "ddd"));
-        assertThat(race1.getRoundResult()).isEqualTo(List.of(0, 0, 0, 0));
+        assertThat(race.getCarNames()).isEqualTo(List.of("dawin", "suji", "son", "blue"));
+        assertThat(race.getRoundResult()).isEqualTo(List.of(0, 0, 0, 0));
     }
 
     @Test
-    void setCarList_한_개의_이름으로_초기화된_자동차_객체_리스트_생성() {
+    void getCarNames_자동차_객체의_이름_리스트로_반환() {
         //given
-        Race race1 = new Race();
-        String nameInput = "aaa";
+        String nameInput = "dawin,suji,blue";
+        Race race = new Race();
+        race.setCarList(nameInput);
 
         //when
-        race1.setCarList(nameInput);
+        List<String> actualCarNames = race.getCarNames();
 
         //then
-        assertThat(race1.getCarNames()).isEqualTo(List.of("aaa"));
-        assertThat(race1.getRoundResult()).isEqualTo(List.of(0));
+        assertThat(actualCarNames).isEqualTo(List.of("dawin", "suji", "blue"));
     }
 
     @Test
-    void getCarNames() {
+    void getCarListSize_자동차_객체_리스트의_크기_반환() {
         //given
-        String nameInput = "aaa,bbb,ccc,ddd";
+        String nameInput = "dawin,suji,blue";
+        Race race = new Race();
+        race.setCarList(nameInput);
 
         //when
+        int actualListSize = race.getCarListSize();
 
         //then
-
+        assertThat(actualListSize).isEqualTo(3);
     }
 
     @Test
-    void getCarListSize() {
-        //given
-
-        //when
-
-        //then
-
-    }
-
-    @Test
-    void moveCars() {
-        //given
-
-        //when
-
-        //then
+    void moveCars_입력된_랜덤_숫자가_4_미만일_경우_전진하지_않음() {
 
     }
 
