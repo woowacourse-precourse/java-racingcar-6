01f50d25ef0a16b547de2b347b49e32c03d5e398
GS_song <20003204@sju.ac.kr>
2023-10-31 21:26:54 +0900

    fix: abstract와 관련이 없는 코드이기 때문에 삭제

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -2,7 +2,7 @@ package racingcar.validation;
 
 import static racingcar.constant.IllegalArgumentMessage.EMPTY_INPUT;
 
-public abstract class InputValidation {
+public class InputValidation {
 
     public static void isEmptyInput(String input) {
         if (input.isBlank()) {
