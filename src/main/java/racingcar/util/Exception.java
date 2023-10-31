b35d9c7c506e638fc8bfe39aef44a2a3de889532
leeminjun <sohwi0305@gmail.com>
2023-10-31 13:32:28 +0900

    feat: 공백값 예외처리 문구 날리는 함수

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -4,4 +4,8 @@ public class Exception {
     private static final int CAR_NAME_LIMIT_MIN = 1;
     private static final int CAR_NAME_LIMIT_MAX = 5;
     private static final String SPACE = " ";
+
+    private void throwSpaceException() {
+        throw new IllegalArgumentException("자동차 이름안에 공백을 제거하세요.");
+    }
 }
