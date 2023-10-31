58eadac61dd505c1ba5d4e29f305f5b2f72bbc46
chanhoim <cksgh201@naver.com>
2023-11-01 00:18:05 +0900

    test(input): 자동차 이동 시도 횟수 입력 테스트

--- src/test/java/racingcar/InputConsoleViewTest.java
+++ src/test/java/racingcar/InputConsoleViewTest.java
@@ -4,12 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputConsoleView;
 
 public class InputConsoleViewTest {
 
     private final InputConsoleView inputConsoleView = new InputConsoleView();
+
+    @AfterEach
+    void close() {
+        Console.close();
+    }
 
     @Test
     void 자동차_이름_입력_성공() {
@@ -46,6 +53,55 @@ public class InputConsoleViewTest {
         assertThatThrownBy(inputConsoleView::readCarName)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차들의 이름은 ','으로 구분해주세요.");
+    }
+
+    @Test
+    void 시도_횟수_입력_성공() {
+        //given
+        String input = "3";
+        setIn(input);
+
+        //when
+        String result = inputConsoleView.readNumberOfTrial();
+
+        //then
+        assertThat(result).isEqualTo(input);
+    }
+
+    @Test
+    void 시도_횟수_빈칸_입력() {
+        //given
+        String input = " ";
+        setIn(input);
+
+        //when,then
+        assertThatThrownBy(inputConsoleView::readNumberOfTrial)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수를 입력해주세요.");
+    }
+
+    @Test
+    void 시도_횟수_숫자가_아닌_입력() {
+        //given
+        String input = "two";
+        setIn(input);
+
+        //when,then
+        assertThatThrownBy(inputConsoleView::readNumberOfTrial)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력해주세요.");
+    }
+
+    @Test
+    void 시도_횟수_1보다_작은_값_입력() {
+        //given
+        String input = "0";
+        setIn(input);
+
+        //when,then
+        assertThatThrownBy(inputConsoleView::readNumberOfTrial)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수는 1보다 커야됩니다.");
     }
 
 
