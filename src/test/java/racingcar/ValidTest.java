24fcd0e9c76064d7f4534d7c2d16d069d6c5cc96
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-11-01 03:40:35 +0900

    test: 6자 이상 이름을 입력할 경우에 대한 테스트 코드 추가

---
+++ src/test/java/racingcar/ValidTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.validator.CarNameValidator.*;
+
+public class ValidTest {
+
+    @Test
+    void 이름_6자_이상에_대한_예외_처리(){
+        List<String> inputList = new ArrayList<>(Arrays.asList("pobi", "woonni", "jun"));
+
+        assertThatThrownBy(() -> checkLength(inputList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
