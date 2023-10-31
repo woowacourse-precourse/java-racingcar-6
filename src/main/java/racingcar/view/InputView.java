7db73db00a9574831d67d4839414a56b6ff8eb3c
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-31 22:38:31 +0900

    2023.10.31_feat : InputView inputCarName 메서드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,12 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Constants;
+
 public class InputView {
 
     public static String inputCarName() {
-        System.out.println();
+        System.out.println(Constants.START_MESSAGE);
+        return Console.readLine();
     }
 }
