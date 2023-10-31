b63447e4857d8b689bb38e41698dfa95f9dd9e8c
inpink <inpink@kakao.com>
2023-10-31 16:18:04 +0900

    test: RacingServiceImpl의 carNames input 정제, 검증 test

--- src/test/java/racingcar/service/RacingServiceImplTest.java
+++ src/test/java/racingcar/service/RacingServiceImplTest.java
@@ -1,12 +1,16 @@
 package racingcar.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.configuration.AppConfig;
 import racingcar.configuration.Config;
+import racingcar.domain.Car;
 import racingcar.domain.Racing;
 import racingcar.repository.CarRepository;
 import racingcar.repository.RacingRepository;
@@ -52,58 +56,46 @@ public class RacingServiceImplTest {
         assertThat(result).isSameAs(racing);
     }
 
-
-    /*@Test
-    public void Car_Name들_한줄에_입력받아_저장하기() {
-        // given
-        String input = "pobi,woni,jun";
-
-        // when
-        racingService.processCarNamesInput(input);
-
-        // then
-        assertThat(memoryCarRepository.findAll()).hasSize(3);
-    }
-
     @Test
     public void Car_Name_미입력시_예외발생() {
         // given
         String input = "";
+        String tryCount = "5";
 
-        // when
-        final RuntimeException result = assertThrows(IllegalArgumentException.class,
-                () -> racingService.processCarNamesInput(input));
-
-        // then
-        assertThat(result).isNotNull();
+        // when && then
+        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
     @Test
     public void Car_Name_공백입력시_예외발생() {
         // given
         String input = " ";
+        String tryCount = "5";
 
-        // when
-        final RuntimeException result = assertThrows(IllegalArgumentException.class,
-                () -> racingService.processCarNamesInput(input));
-
-        // then
-        assertThat(result).isNotNull();
+        // when && then
+        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
     @Test
     public void Car_Name_콤마만_입력시_예외발생() {
         // given
         String input = ", , ,,";
+        String tryCount = "5";
 
-        // when
-        final RuntimeException result = assertThrows(IllegalArgumentException.class,
-                () -> racingService.processCarNamesInput(input));
+        System.out.println(Arrays.toString(input.split(",")));
+        System.out.println((input.split(",").length));
 
-        // then
-        assertThat(result).isNotNull();
+        // when && then
+        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
+/*
     @Test
     public void 시도횟수_정수하나_입력받아_저장하기() {
         // given
