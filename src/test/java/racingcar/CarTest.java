5ce547543f22b6e5c16084d1e5200fc3d2eb799b
zjacom <eephus625@icloud.com>
2023-10-31 19:35:31 +0900

    test(racingCar): CarTest 수정
    함수 추출하기 리팩토링을 통해 private으로 변한 메서드 테스트 삭제

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,24 +7,14 @@ import org.junit.jupiter.api.RepeatedTest;
 public class CarTest {
     Car car = new Car("name");
 
-    /*
-    @Test
-    void 랜덤숫자_생성기_테스트() {
-        int randomNumber = car.createRandomNumber();
-        // 랜덤 숫자가 0에서 9 사이에 있는 숫자인지 확인
-        assertThat(randomNumber).isLessThan(10);
-        assertThat(randomNumber).isGreaterThan(-1);
-    }
-     */
-
     @RepeatedTest(10)
     void 자동차_주행_테스트() {
         for (int i = 0; i < 5; i++) {
             car.driving();
         }
-        // 주행을 5번 시도했을 때 "-"를 포함하는지 확인
-        // 주행거리 문자열의 길이가 5보다 작은지 확인
+        // 주행을 5번 시도했을 때 "-"를 포함하는지 확인 -> 약 1프로의 확률로 포함되지 않을 수 있음
         assertThat(car.getDistanceDriven()).as("운이 나빠서 '-'가 포함되지 않았어요!").contains("-");
+        // 주행거리 문자열의 길이가 5보다 작은지 확인
         assertThat(car.getDistanceDriven()).hasSizeLessThanOrEqualTo(5);
     }
 }
