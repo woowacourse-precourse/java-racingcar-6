8cb6eb00d2ed919153f6c2a823c7822e5222dff0
leejuno <zhqkfxm0127@naver.com>
2023-10-31 14:08:23 +0900

    feat(racingcar): 입력받은 이름으로 자동차를 생성하는 기능 구현

--- src/main/java/racingcar/domain/InputController.java
+++ src/main/java/racingcar/domain/InputController.java
@@ -2,9 +2,21 @@ package racingcar.domain;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputController {
-    public void insertCarNames() {
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    public String insertCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String carNames = readLine();
+        return carNames;
+    }
+
+    public List<String> setCar(String carNames) {
+        List<String> carList = new ArrayList<>();
+        for (String car : carNames.split(",")) {
+            carList.add(car);
+        }
+        return carList;
     }
 }
