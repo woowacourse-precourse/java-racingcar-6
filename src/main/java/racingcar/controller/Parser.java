d776cfca576804ca7e17a35a6ff65818561eb50f
HongYeseul <ghd8119@naver.com>
2023-11-01 01:34:56 +0900

    :recycle: refactor(Parser.parsingByComma): 로직이 많은 것 같아 메서드로 빼기

--- src/main/java/racingcar/controller/Parser.java
+++ src/main/java/racingcar/controller/Parser.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 import static racingcar.controller.constants.DelimiterConstants.*;
 
 public class Parser {
-    // TODO: input.split()은 parser 역할이지만, 앞 부분은 StringToArrayList()로 분리할 필요가 있어보임
     public static ArrayList<String> parsingByComma(String input){
-        return new ArrayList<>(Arrays.asList(input.split(DELIMITER.getValue())));
+        return stringToArrayList(input.split(DELIMITER.getValue()));
+    }
+
+    public static ArrayList<String> stringToArrayList(String[] input){
+        return new ArrayList<>(Arrays.asList(input));
     }
 
     public static Integer parsingInteger(String input){
