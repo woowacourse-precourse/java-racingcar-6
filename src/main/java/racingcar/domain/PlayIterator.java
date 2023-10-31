7f6957643eef07dfb188537caa8126cffcd5767c
Gyeongho Yang <chchch1213@naver.com>
2023-10-31 20:56:09 +0900

    feat(domain): 시도할 횟수가 음수일 때 예외 발생 검증 추가

--- src/main/java/racingcar/domain/PlayIterator.java
+++ src/main/java/racingcar/domain/PlayIterator.java
@@ -14,12 +14,20 @@ public class PlayIterator {
 
     public static PlayIterator from(String playCountStr) {
         validateNumericString(playCountStr.trim());
-        return new PlayIterator(Integer.parseInt(playCountStr.trim()));
+        int playCount = Integer.parseInt(playCountStr.trim());
+        validateNotNegative(playCount);
+        return new PlayIterator(playCount);
     }
 
     private static void validateNumericString(String playCountStr) {
-        if (!Pattern.compile("\\d+").matcher(playCountStr).matches()) {
+        if (!Pattern.compile("-?\\d+").matcher(playCountStr).matches()) {
             throw RaceException.of(ErrorMessage.NOT_NUMERIC_PLAY_COUNT_STRING);
+        }
+    }
+
+    private static void validateNotNegative(int playCount) {
+        if (playCount < 0) {
+            throw RaceException.of(ErrorMessage.NEGATIVE_PLAY_COUNT);
         }
     }
 
