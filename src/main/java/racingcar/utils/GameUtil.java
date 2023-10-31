dab8f3b4e0e6b1ff67f751c17b93f47edfb02df1
이정민 <jmlee2705@gmail.com>
2023-11-01 05:11:10 +0900

    feat: GameUtil 스켈레톤 코드 작성

---
+++ src/main/java/racingcar/utils/GameUtil.java
@@ -0,0 +1,21 @@
+package racingcar.utils;
+
+public class GameUtil {
+
+    public void splitNamesByComma() {
+
+    }
+
+    public void generateRandomNumber() {
+
+    }
+
+    public void formatWinners() {
+
+    }
+
+    public void judgeGo() {
+        
+    }
+
+}
