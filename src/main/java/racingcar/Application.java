02f16c6606063eeac8e90dda2e50aad1e09fac7b
ae-GYEONGWON <tjsxor50@gmail.com>
2023-10-31 20:23:58 +0900

    feat(racingcar): 자동차들을 경주 시키는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,31 @@
 package racingcar;
 
 import racingcar.domain.Checker;
+import racingcar.domain.Input;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-//        Input input = new Input();
-//        System.out.println(input.carsName());
-//        System.out.println(input.numberOfAttempts());
+        Input input = new Input();
+        List<String> carsName = input.carsName();
+        int numberOfAttempts = input.numberOfAttempts();
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < carsName.size(); i++) {
+            result.add(0);
+        }
         Checker checker = new Checker();
-        System.out.println(checker.advance());
+
+        for (int i = 0; i<numberOfAttempts; i++) {
+            for (int j = 0; j < carsName.size(); j++) {
+                if (checker.advance()) {
+                    result.set(j, result.get(j)+1);
+                }
+            }
+        }
+
+
     }
 }
