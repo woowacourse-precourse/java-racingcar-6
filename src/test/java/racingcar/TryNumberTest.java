531b19dde971af7bb378cff393fb2d000bfaeb8b
lotuxsoo <tndus5383@gmail.com>
2023-10-31 20:38:02 +0900

    test: 자동차 여러대 리스트 생성, 한대 생성 테스트 추가

--- src/test/java/racingcar/TryNumberTest.java
+++ src/test/java/racingcar/TryNumberTest.java
@@ -3,9 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.validator.InputValidator;
