9d92732b92806b3892f56d9ec074d52b43148531
twoosky <lhjae3@naver.com>
2023-10-31 19:19:10 +0900

    feat: 자동차 이름 길이 검증 로직 구현

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import static racingcar.utils.ErrorMessages.CAR_NAME_BLANK;
+import static racingcar.utils.ErrorMessages.CAR_NAME_CONTAIN_BLANK;
+import static racingcar.utils.ErrorMessages.CAR_NAME_EMPTY;
+import static racingcar.utils.ErrorMessages.OUT_OF_LENGTH_CAR_NAME;
+
+import racingcar.utils.ErrorMessages;
+
+
+public class Name {
+    public static final int MIN_NAME_SIZE = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validateSize(name);
+        this.name = name;
+    }
+
+    private void validateSize(String name) {
+        if (name.length() > MIN_NAME_SIZE) {
+            throw new IllegalArgumentException(OUT_OF_LENGTH_CAR_NAME);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
