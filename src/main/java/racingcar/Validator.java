5f65643d89eca9250274d3fa052ba9f33c3102ff
깃wooqqq <sge0225@naver.com>
2023-10-31 18:25:24 +0900

    feat: 자동차 이름에 공백이 포함되어있으면 예외처리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class Validator {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final int CAR_NAME_MIN_LENGTH = 1;
+    private static final String BLANK = " ";
 
     public void checkCarName(List<String> carNames) {
         for (String carName : carNames) {
             checkCarNameMaxLength(carName);
             checkCarNameMinLength(carName);
+            checkCarNameBlank(carName);
         }
     }
 
@@ -22,6 +24,12 @@ public class Validator {
     private void checkCarNameMinLength(String carName) {
         if (carName.length() < CAR_NAME_MIN_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 1자 이상만 가능합니다.");
+        }
+    }
+
+    private void checkCarNameBlank(String carName) {
+        if (carName.contains(BLANK)) {
+            throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수 없습니다.");
         }
     }
 }
