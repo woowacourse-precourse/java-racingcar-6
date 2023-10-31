39ea53dc01d6ab99e5f8250320d9745e659f0125
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:17:13 +0900

    feat(InputView): 사용자 입력 뷰 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static racingcar.constant.RaceMessage.INPUT_CAR_NAMES;
+import static racingcar.constant.RaceMessage.INPUT_NUMBER_OF_ATTEMPTS;
+
 public class InputView {
+    public static List<String> inputCarNames() {
+        System.out.println(INPUT_CAR_NAMES);
+        String input = Console.readLine();
+        return Arrays.asList(input.split(","));
+    }
+
+    public static String inputNumberOfAttempts() {
+        System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
+        return Console.readLine();
+    }
 }
