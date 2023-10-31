0d83b2573a226a2e49609996fab8e8d56010cbc3
황준혁 <hyug0819@gmail.com>
2023-11-01 00:09:48 +0900

    feat : 자동차 이름 입력 구현

---
+++ src/main/java/racingcar/play/car/Car.java
@@ -0,0 +1,14 @@
+package racingcar.play.car;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.CarException;
+
+public class Car {
+    public static String[] input(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String s = Console.readLine();
+        String[] names = s.split(",");
+
+        return names;
+    }
+}
---
+++ src/main/java/racingcar/play/game/Game.java
@@ -0,0 +1,11 @@
+package racingcar.play.game;
+
+import racingcar.play.car.Car;
+
+public class Game {
+
+    public static void start(){
+        String[] names = Car.input();
+    }
+
+}
