eb204bdfd9379ec1daf65108daf316acc5a7aa74
jae woo Jung <awfjol2008@naver.com>
2023-10-31 09:17:55 +0900

    feat: 경주할 자동차 이름들을 입력받는 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,10 +1,16 @@
 package racingcar.controller;
 
+import racingcar.validator.CarNameValidator;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 
     private static String readCarNames() {
         OutputView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = InputView.read();
+        CarNameValidator.validateForInputString(carNames);
+
+        return carNames;
     }
 }
