4c0a4ddffa467ef9116ef733bc5ab0e76c7784b6
twoosky <lhjae3@naver.com>
2023-10-31 19:48:58 +0900

    feat: error 메시지 상수 정의

---
+++ src/main/java/racingcar/utils/ErrorMessages.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+
+public class ErrorMessages {
+    public static final String NOT_INTEGER_TYPE = "[ERROR] 숫자가 아닙니다.";
+    public static final String REPEAT_COUNT_NOT_ZERO = "[ERROR] 시도 횟수는 0보다 커야 합니다.";
+    public static final String OUT_OF_LENGTH_CAR_NAME = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+    public static final String CAR_COUNT_UNDER_MINIMUM = "[ERROR] 자동차 이름은 2개 이상 입력해야 합니다.";
+    public static final String NO_SUCH_WINNER = "[ERROR] 우승자를 찾을 수 없습니다.";
+    public static final String CAR_NAME_EMPTY = "[ERROR] 자동차 이름은 필수로 지정해야 합니다.";
+    public static final String CAR_NAME_BLANK = "[ERROR] 자동차 이름은 최소 1자 이상이여야 합니다.";
+    public static final String CAR_NAME_CONTAIN_BLANK = "[ERROR] 자동차 이름에는 공백을 포함할 수 없습니다.";
+}
