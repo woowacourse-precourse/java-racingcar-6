80bb83d307dcaf4ca34b9c305c4ddfc731df2e4a
inpink <inpink@kakao.com>
2023-10-31 16:31:52 +0900

    test: RacingServiceImpl의 tryCount input 정제, 검증 test

--- src/test/java/racingcar/service/RacingServiceImplTest.java
+++ src/test/java/racingcar/service/RacingServiceImplTest.java
@@ -45,9 +45,9 @@ public class RacingServiceImplTest {
     @Test
     public void Racing객체_저장하기() {
         // given
-        String input = "pobi,woni,jun";
+        String carNames = "pobi,woni,jun";
         String tryCount = "5";
-        final Racing racing = racingService.generateRacing(input, tryCount);
+        final Racing racing = racingService.generateRacing(carNames, tryCount);
 
         // when
         final Racing result = racingService.save(racing);
@@ -57,56 +57,63 @@ public class RacingServiceImplTest {
     }
 
     @Test
-    public void Car_Name_미입력시_예외발생() {
+    public void 자동차이름들_미입력시_예외발생() {
         // given
-        String input = "";
+        String carNames = "";
         String tryCount = "5";
 
         // when && then
-        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+        assertThatThrownBy(() -> racingService.generateRacing(carNames, tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
     @Test
-    public void Car_Name_공백입력시_예외발생() {
+    public void 자동차이름들_공백입력시_예외발생() {
         // given
-        String input = " ";
+        String carNames = " ";
         String tryCount = "5";
 
         // when && then
-        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+        assertThatThrownBy(() -> racingService.generateRacing(carNames, tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
     @Test
-    public void Car_Name_콤마만_입력시_예외발생() {
+    public void 자동차이름들_콤마만_입력시_예외발생() {
         // given
-        String input = ", , ,,";
+        String carNames = ", , ,,";
         String tryCount = "5";
 
-        System.out.println(Arrays.toString(input.split(",")));
-        System.out.println((input.split(",").length));
-
         // when && then
-        assertThatThrownBy(() -> racingService.generateRacing(input, tryCount))
+        assertThatThrownBy(() -> racingService.generateRacing(carNames, tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름을 하나 이상 입력하세요.");
     }
 
-/*
     @Test
-    public void 시도횟수_정수하나_입력받아_저장하기() {
+    public void 시도횟수_정수가아닌_문자일시_예외발생() {
         // given
-        String input = "3";
+        String carNames = "pobi,woni,jun";
+        String tryCount = "k";
 
-        // when
-        racingService.processTryCountInput(input);
+        // when && then
+        assertThatThrownBy(() -> racingService.generateRacing(carNames, tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상 2,147,483,647 이하의 정수 값이 필요합니다.");
+    }
 
-        // then
+    @Test
+    public void 시도횟수_정수범위_벗어난_숫자일시_예외발생() {
+        // given
+        String carNames = "pobi,woni,jun";
+        String tryCount = "2200000000";
 
-    }*/
-
+        // when && then
+        assertThatThrownBy(() -> racingService.generateRacing(carNames, tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상 2,147,483,647 이하의 정수 값이 필요합니다.");
+    }
 
 }
