19b1e62f677c4480f7801c3d02895e80c1024f63
ParkKyuIl <hakim@nex.market>
2023-11-01 08:26:17 +0900

    feat: 추가 예외 케이스 및 예외 메세지 추가

--- src/main/java/racingcar/view/userInput.java
+++ src/main/java/racingcar/view/userInput.java
@@ -20,18 +20,28 @@ public class userInput {
         Set<String> setNames = new HashSet<>(names);
 
         if(setNames.size()!= names.size()){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("차량이름이 중복되었습니다.");
         }
 
         for (String name : names) {
             if (name.length() > 5) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("이름의 길이는 5자를 넘지 않아야 합니다.");
             }
         }
 
     }
+
+    public static void validateTryNumber(int tryNum){
+        if(tryNum < 0 ){
+            throw new IllegalArgumentException("시도 횟수는 0을 넘어야 합니다.");
+        }
+
+    }
     public static int getTryNumber(){
-        return Integer.parseInt(readLine());
+        int tryNum = Integer.parseInt(readLine());
+        validateTryNumber(tryNum);
+        return tryNum;
+
     }
     private static String readLine(){
         return Console.readLine();
