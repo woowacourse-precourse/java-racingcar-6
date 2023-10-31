1c15e1aa88821e35e49e8367fed309d5e364515a
Jiyoung Choi <12201817@inha.edu>
2023-10-31 14:37:46 +0900

    refactor(OutputView) : 필요없는 함수 삭제

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,17 +1,11 @@
 package racingcar.view;
 
 public class OutputView {
-    private final static String resultMessage = "\n실행 결과";
-
-    public static void printResultMessage() {
-        System.out.println(resultMessage);
-    }
-
-    public static void printMove(String resultMap) {
-        System.out.println(resultMap);
+    public static void printResult(String result) {
+        System.out.print(result);
     }
 
     public static void printFinalWinners(String finalWinners) {
-        System.out.printf(finalWinners);
+        System.out.print(finalWinners);
     }
 }
