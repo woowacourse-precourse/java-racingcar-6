abb37d9ebb38ec86a6d91c535d9f80eb187a6464
깃wooqqq <sge0225@naver.com>
2023-10-31 17:59:52 +0900

    refactor: 중복되는 코드 하나의 메서드로 묶어서 처리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -6,24 +6,22 @@ public class Validator {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final int CAR_NAME_MIN_LENGTH = 1;
 
-    public void checkCarName(List<String> input) {
-        checkCarNameMaxLength(input);
-        checkCarNameMinLength(input);
-    }
-
-    private void checkCarNameMaxLength(List<String> carNames) {
+    public void checkCarName(List<String> carNames) {
         for (String carName : carNames) {
-            if (carName.length() > CAR_NAME_MAX_LENGTH) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
+            checkCarNameMaxLength(carName);
+            checkCarNameMinLength(carName);
         }
     }
 
-    private void checkCarNameMinLength(List<String> carNames) {
-        for (String carName : carNames) {
-            if (carName.length() < CAR_NAME_MIN_LENGTH) {
-                throw new IllegalArgumentException("자동차 이름은 1자 이상만 가능합니다.");
-            }
+    private void checkCarNameMaxLength(String carName) {
+        if (carName.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    private void checkCarNameMinLength(String carName) {
+        if (carName.length() < CAR_NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상만 가능합니다.");
         }
     }
 }
