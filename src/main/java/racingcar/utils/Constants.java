177b3fd5cdf9f5af556b1bd22b5034beda05769a
Jihyeon Park <mmmszip@gmail.com>
2023-11-01 06:02:58 +0900

    refactor: 예외 출력 상수 추가

--- src/main/java/racingcar/utils/Constants.java
+++ src/main/java/racingcar/utils/Constants.java
@@ -4,9 +4,10 @@ public class Constants {
 
     public static final int RANDOM_NUMBER_MINIMUM = 1;
     public static final int RANDOM_NUMBER_MAXIMUM = 9;
-    public static final int MAXIMUM_NAME_LENGTH = 5;
+    public static final int NAME_LENGTH_MAXIMUM = 5;
+    public static final String INVALID_CAR_NAMES = "자동차 이름은 ','로 구분되게 입력해주세요.";
     public static final String INVALID_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
-    public static final String NAME_LENGTH_EXCEED = "이름의 제한 길이를 초과했습니다.";
+    public static final String EXCEED_NAME_LENGTH = "이름의 제한 길이를 초과했습니다.";
     public static final String DUPLICATE_CAR_NAMES = "중복된 자동차 이름이 존재합니다.";
     public static final String EMPTY_CAR_NAMES = "1개 이상의 자동차 이름을 입력해주세요.";
 }
