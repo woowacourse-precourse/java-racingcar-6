0bf277c9698b8e92ee3b764ac6219422cdcd886c
alenjb <alenjb@naver.com>
2023-11-01 02:42:17 +0900

    :sparkles:feat(racingcar): 시도할 회수를 입력받는 기능 구현
    Inputs.java에 시도할 회수를 입력받는 메서드 askRaceNumbers를 구현함.

--- src/main/java/racingcar/Inputs.java
+++ src/main/java/racingcar/Inputs.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Inputs {
     List<String> carNamesList = new ArrayList<>();
+    int raceNumbers; // 레이스를 진행할 횟수
 
     public String askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -19,11 +20,22 @@ public class Inputs {
         if (carNamesArray.length > 0) {
             for (String carName : carNamesArray) {
                 // 자동차 이름이 5글자가 넘으면 에러를 발생시키고 종료
-                if(carName.length() > 5) throw new IllegalArgumentException("비정상적인 값을 입력하셨습니다. 경주를 자동으로 종료합니다.");
+                if (carName.length() > 5) throw new IllegalArgumentException("비정상적인 값을 입력하셨습니다. 경주를 자동으로 종료합니다.");
                 this.carNamesList.add(carName);
             }
         } else { // 쉼표가 없는 경우: 문자열을 자동차 이름 리스트에 추가
             this.carNamesList.add(carNames.trim());
         }
+    }
+
+    public void askRaceNumbers() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        try {
+            raceNumbers = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        }catch (NumberFormatException e) { // 입력값이 정수가 아니면 에러를 발생시키고 종료
+            raceNumbers = -1; // 에러 테스트를 위해 해당 값을 -1로 변경
+            throw new IllegalArgumentException("비정상적인 값을 입력하셨습니다. 경주를 자동으로 종료합니다.");
+        }
+
     }
 }
