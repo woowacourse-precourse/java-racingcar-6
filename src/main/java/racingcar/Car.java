6df6811bf5b33938b5be7971b57ccb3177353c43
Hyunjun KIM <hihj070914@icloud.com>
2023-11-01 00:03:03 +0900

    feat: Car Class Structure - Car 클래스에 대한 구조를 구현 기능 명세에 맞게 정리하였습니다. - 테스트 코드 작성 후 로직 완성 예정

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+public class Car {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    private String name;
+    private int currentLocation;
+
+    private Car() {
+    }
+
+    Car(String name) {
+        this.name = name;
+        this.currentLocation = 0;
+    }
+
+    public String getCurrentStatus() {
+
+    }
+
+    public boolean determineWhetherToMove(int randomValue) {
+
+    }
+
+    public void move() {
+
+    }
+}
