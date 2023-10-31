7505f5bafe032bece4ce531271a69cd517385941
“sangjun121” <juncho12011201@gmail.com>
2023-11-01 02:30:42 +0900

    fix: 컴마 위치 적합성 판단하는 정규표현식 변경

--- src/main/java/racingcar/utility/Constants.java
+++ src/main/java/racingcar/utility/Constants.java
@@ -7,6 +7,6 @@ public final class Constants {
     public static final int NAME_LENGTH_LIMIT = 5;
 
     //예외처리 정규표현식 패턴
-    public static final String IMPROPER_COMMA_POSITION_PATTERN = "^,|,$|,,"; //문자열 처음 또는 끝에서 comma가 나오거나 문자열 내에서 2번 연속 나오는 경우
+    public static final String IMPROPER_COMMA_POSITION_PATTERN = "^(,.*|.*,,.*|.*,)$"; //문자열 처음 또는 끝에서 comma가 나오거나 문자열 내에서 2번 연속 나오는 경우
     public static final String VAILD_RACING_COUNT_PATTERN = "^[1-9]\\d*$";
 }
