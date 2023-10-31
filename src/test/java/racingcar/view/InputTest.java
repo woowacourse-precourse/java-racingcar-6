b968ce6b2b9898f2aecf2657eb1f554f0384c25a
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-31 16:14:42 +0900

    test: 입력 테스트

---
+++ src/test/java/racingcar/view/InputTest.java
@@ -0,0 +1,39 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class InputTest {
+
+    @Test
+    void carName_입력() {
+        assertThat(MockInput.inputCarName("pobi1,pobi2,pobi3"))
+                .isEqualTo(List.of("pobi1", "pobi2", "pobi3"));
+
+    }
+
+    @Test
+    void moveChance_입력() {
+        assertThat(MockInput.inputMoveChance("1")).isEqualTo(1);
+    }
+
+    public class MockInput {
+        public static List<String> inputCarName(String name) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            List<String> carNames = Mapper.changeToListString(name);
+            Validator.carName(carNames);
+            return carNames;
+        }
+
+        public static int inputMoveChance(String number) {
+            System.out.println("시도할 회수는 몇회인가요?");
+            int moveNumber = Mapper.changeToInteger(number);
+            Validator.moveNumber(moveNumber);
+            return moveNumber;
+        }
+
+    }
+
+}
