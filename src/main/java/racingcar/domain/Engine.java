8e5ea94e88d18de52e09064aa91c9dcdc42c0dba
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-31 17:52:27 +0900

    feat: 무작위값 생성 메서드 및 테스트 구현

--- src/main/java/racingcar/domain/Engine.java
+++ src/main/java/racingcar/domain/Engine.java
@@ -1,11 +1,13 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Engine {
 	public Boolean canMoveForward() {
 		return true;
 	}
 
-	public int createRandomnumber() {
-		return 0;
+	public Integer createRandomnumber() {
+		return Randoms.pickNumberInRange(0, 9);
 	}
 }
