fb2aa1d5cf17c8149a89896b61062f0ab68a2c29
ohjuyoungg <ejrm12@naver.com>
2023-10-31 19:44:31 +0900

    test(GameController) : 중복코드 제거

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -11,17 +12,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class GameControllerTest {
 
+    private GameController gameController;
+
+    @BeforeEach
+    void setUp() {
+        gameController = new GameController();
+    }
 
     @Test
     void playGame_자동차_경주_게임_성공() {
-        GameController gameController = new GameController();
         assertDoesNotThrow(() -> gameController.playGame("pobi", 1));
     }
 
     @ParameterizedTest
     @MethodSource("playGame_자동차_경주_게임_검증시도_실패한다")
     void playGame_자동차_경주_게임_검증시도_실패한다(String carNamesInput, String numberOfAttempts) {
-        GameController gameController = new GameController();
         assertThatThrownBy(() -> gameController.playGame(carNamesInput, Integer.parseInt(numberOfAttempts)))
             .isInstanceOf(IllegalArgumentException.class);
     }
