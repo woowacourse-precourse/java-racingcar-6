023a027055af205b35fc55552a3fd994aec1062f
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-01 01:56:25 +0900

    feat: 자동차 모델의 전진 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,7 +1,9 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private String name;
+    private final String name;
     private int position;
 
     public Car(String name) {
@@ -9,6 +11,25 @@ public class Car {
         this.position = 0;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
     public void move() {
+        if (isMovable(getRandomNumber())) {
+            this.position++;
+        }
+    }
+
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    private boolean isMovable(int number) {
+        return number >= 4;
     }
 }
