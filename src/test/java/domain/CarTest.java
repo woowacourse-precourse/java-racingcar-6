12e9b0dd6dd12ba2db4ae122dd971cf6c1a916b6
nohheeyeon <heeyeon1213@gmail.com>
2023-11-01 01:26:08 +0900

    refactor : 중복되는 테스트 코드 제거

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,8 +2,6 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 public class CarTest {
 
@@ -26,19 +24,5 @@ public class CarTest {
         Car car = new Car("TestCar");
         car.moveForward(3);
         assertEquals(0, car.getPosition());
-    }
-
-    @Test
-    void printPositionTest() {
-        Car car = new Car("TestCar");
-        car.moveForward(5);
-        car.moveForward(3);
-        car.moveForward(2);
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        car.printPosition();
-        System.setOut(System.out);
-        assertEquals("TestCar : -----\n", outContent.toString());
     }
 }
