a31a4cf9cf96662af05d6ac6e3fe32e4120398ad
GO-TE <alsrudxo9810@naver.com>
2023-10-31 13:48:41 +0900

    chore: 불필요한 라이브러리 제거 - OuputGame Test 제거

--- src/main/java/racingcar/OutputGame.java
+++ src/main/java/racingcar/OutputGame.java
@@ -1,2 +1,41 @@
-package racingcar;public class OutputGame {
+package racingcar;
+
+import java.util.ArrayList;
+
+public class OutputGame {
+    private enum Message {
+        EXECUTION_RESULT("실행 결과"),
+        RESULT_FORMAT("%s : %s%n"),
+        DASH("-"),
+        WINNER("최종 우승자 : ");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+    }
+
+    private final int START = 0;
+
+    public void printGame() {
+        System.out.println(Message.EXECUTION_RESULT.message);
+
+    }
+
+    public void printRacing(ArrayList<String> carName, ArrayList<Integer> position) {
+        for (int order = START; order < carName.size(); order++) {
+            System.out.printf(Message.RESULT_FORMAT.message, carName.get(order),
+                    Message.DASH.message.
+                            repeat(position.get(order)));
+        }
+        System.out.println();
+    }
+
+    public void printWinner(ArrayList<String> winner) {
+        System.out.print(Message.WINNER.message);
+        System.out.println(String.join(", ", winner));
+    }
+
+
 }
