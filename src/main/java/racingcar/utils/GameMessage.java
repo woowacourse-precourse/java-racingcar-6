9d474eee0454eea26f6ed97b1f76dcd7dfcbdd1d
honeydanji <tjdwls3680@naver.com>
2023-10-31 23:21:12 +0900

    FEAT : 게임 메시지 추가
    - 상수형 문자열로 선언

---
+++ src/main/java/racingcar/utils/GameMessage.java
@@ -0,0 +1,10 @@
+package racingcar.utils;
+
+public class GameMessage {
+
+    static String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static String TRY = "시도할 횟수는 몇회인가요?";
+    static String RESULT = "실행 결과";
+    static String WINNER = "최종 우승자 : ";
+
+}
