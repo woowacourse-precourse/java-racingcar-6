8e2cdabe0f44cc0b9297247c00867a11173be142
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-10-31 21:26:20 +0900

    Test: 시도횟수 입력 테스트 작성

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,47 @@
+package racingcar.view;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    private final InputView inputView = new InputView();
+
+    @Test
+    void 시도횟수_입력이_양의_정수일경우_정상적으로_진행() {
+        String input = "5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        inputView.getTrials();
+    }
+
+    @Test
+    void 시도횟수_입력에_문자가_포함된경우_예외발생() {
+        String input = "5aa";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도횟수_입력이_음수일경우_예외발생() {
+        String input = "-5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도횟수_입력이_0일경우_예외발생() {
+        String input = "0";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(inputView::getTrials).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
