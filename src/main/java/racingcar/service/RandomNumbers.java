4f3815d802b6f33c5ec4099d84837e0de09641a4
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-10-31 17:27:56 +0900

    feat: 입력한 자동차 이름의 수와 동일하게 0~9 사이 난수를 생성하여 저장

---
+++ src/main/java/racingcar/service/RandomNumbers.java
@@ -0,0 +1,31 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumbers {
+    private final List<Integer> randomValues;
+
+    public RandomNumbers(int size) {
+        this.randomValues = generateRandomValues(size);
+    }
+
+    public int size() {
+        return randomValues.size();
+    }
+
+    private List<Integer> generateRandomValues(int size) {
+        int RANDOM_RANGE_START = 0;
+        int RANDOM_RANGE_END = 9;
+        List<Integer> randomValues = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            randomValues.add(Randoms.pickNumberInRange(RANDOM_RANGE_START, RANDOM_RANGE_END));
+        }
+        return randomValues;
+    }
+
+    public Boolean isGreaterEqualThan(int index, int threshold) {
+        return randomValues.get(index) >= threshold;
+    }
+}
