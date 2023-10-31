72ac5cb5acc5b8af80d7d85b2fa177d4cb1d8849
songjayhyun <thewinner1st@naver.com>
2023-10-31 17:07:33 +0900

    feat: 자동차가 전진 혹은 멈추는 기능 구현
    - 0과 9사이에 무작위 값을 생성하는 함수(generateRandomNumbers) 구현
    - 4 이상이면 true 아니면 false를 반환하는 함수(canMove) 구현
    - 자동차가 전진하는 함수(move) 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
+
+    private final int MIN_NUMBER = 0;
+    private final int MAX_NUMBER = 9;
+    private final int MOVE_MIN_NUMBER = 4;
 
     int position = 0;
 
@@ -11,15 +16,15 @@ public class Car {
     }
     
     public boolean canMove() {
-        List<Integer> numbers = generateRandomNumbers();
-        return true;
+        return generateRandomNumber() > MOVE_MIN_NUMBER;
     }
 
-    private List<Integer> generateRandomNumbers() {
-        return new ArrayList<>();
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
     }
 
     public void move() {
+        position++;
     }
 
     public int getPosition() {
