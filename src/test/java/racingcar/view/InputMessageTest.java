e3091f3cbcd76f4aefc5adb130fc253de7f12237
win9 <krrr883@gmail.com>
2023-11-01 05:32:11 +0900

    [Test]: 자동차 이름 예외 입력 테스트 추가
    자동차 이름의 개행이나 공백에 대한 입력 테스트 추가

---
+++ src/test/java/racingcar/view/InputMessageTest.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Application;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("입력 기능 테스트")
+class InputMessageTest extends NsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\n", "\r", "\r\n"})
+    void 자동차_이름_공백_입력_예외_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
