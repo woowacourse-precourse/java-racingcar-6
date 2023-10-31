f588793a2c7ca89c933d1e7f4f1e7ce1ece7606b
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-01 04:06:48 +0900

    style(racingCar): 불필요한 코드 삭제

--- src/main/java/racingcar/domain/InputMoveNumber.java
+++ src/main/java/racingcar/domain/InputMoveNumber.java
@@ -2,8 +2,6 @@ package racingcar.domain;
 
 public class InputMoveNumber extends Input {
     private final static String message = "시도할 회수는 몇회인가요?";
-    private String input;
-
     public InputMoveNumber() {
         print(message);
         input();
--- src/main/java/racingcar/domain/InputName.java
+++ src/main/java/racingcar/domain/InputName.java
@@ -4,7 +4,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputName extends Input {
     private final static String message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private String input;
 
     public InputName() {
         print(message);
