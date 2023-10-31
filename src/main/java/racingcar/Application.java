81e74a1a241a2e25ddea408d6a520d9179f75edd
Yonghyun Yoon <dragonappear1@gmail.com>
2023-10-31 16:52:11 +0900

    feat: 사용자로부터 자동차 이름, 시도 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,38 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carInput = Console.readLine();
+        List<String> cars = Arrays.stream(carInput.split(",")).toList();
+        validateCarName(cars);
+
+        String attemptInput = Console.readLine();
+        validateAttemptInput(attemptInput);
+        int attempt = Integer.parseInt(attemptInput);
+    }
+
+    private static void validateAttemptInput(String attempt) {
+        try {
+            Integer.parseInt(attempt);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("시도 횟수를 입력해주세요.");
+        }
+    }
+
+    public static void validateCarName(List<String> cars) {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+
+        for (String carName : cars) {
+            if (carName.isBlank() || carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+            }
+        }
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,33 +1,51 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
-    void 이름에_대한_예외_처리() {
+    void 자동차_이름이_올바르지_않으면_예외가_발생한다() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도_횟수가_올바르지_않으면_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "null"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름과_시도할_횟수를_입력받는다() {
+        assertSimpleTest(
+                () -> {
+                    run("pobi,woni,goood", "5");
+                }
         );
     }
 
