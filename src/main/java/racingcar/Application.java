d9c71a16dff6b2a89ba8dca7f7d939f01a2a662b
yang <ywj9811@gmail.com>
2023-11-01 02:09:10 +0900

    refactor : 추상 객체를 통한 참조

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,17 +3,23 @@ package racingcar;
 import racingcar.controller.ProgressController;
 import racingcar.controller.InitController;
 import racingcar.dto.GameSettingDto;
-import racingcar.repository.CarRepository;
+import racingcar.repository.CarRepositoryImpl;
+import racingcar.repository.Repository;
 import racingcar.service.InputService;
+import racingcar.service.InputServiceImpl;
 import racingcar.service.RacingService;
+import racingcar.service.RacingServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
-        CarRepository carRepository = new CarRepository();
-        InputService inputService = new InputService(carRepository);
+        Repository repository = new CarRepositoryImpl();
+        InputService inputService = new InputServiceImpl(repository);
+
         InitController initController = new InitController(inputService);
         GameSettingDto gameSettingDto = initController.gameInit();
-        RacingService racingService = new RacingService(gameSettingDto.getCars(), gameSettingDto.getTimes(), carRepository);
+
+        RacingService racingService = new RacingServiceImpl(gameSettingDto.getCars(), gameSettingDto.getTimes(), repository);
+
         ProgressController progressController = new ProgressController(racingService);
         progressController.gameProgress();
     }
