76cb45b21f016ed9bf48fcec20fb4b66827f65e9
bnfkim <kgr3819@gmail.com>
2023-10-31 14:50:14 +0900

    Refactor: view input car list & add valid method

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,25 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
 
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class InputView {
 
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_GAME_ROUND = "시도할 회수는 몇회인가요?";
+    private static final String BLANK_STRING = "입력 문자열이 비어 있습니다.";
+    private static final String REQUIRE_LENGTH_UNDER_FIVE = "자동차 이름을 5자 이하로 입력해주세요.";
+    private static final String REQUIRE_INTEGER_TYPE = "자동차 이름을 정수 값을 입력해주세요.";
+    private static final String REQUIRE_NOT_BLANK = "자동차 이름을 공백 없이 입력해주세요.";
+    private static final String REQUIRE_NOT_DUPLICATE = "자동차 이름을 중복 없이 입력해주세요.";
+    private static final String GAME_ROUND_OVER_ONE = "1회 이상 게임이 진행 되어야 합니다.";
 
     public static String inputCarName() {
         System.out.println(INPUT_CAR_NAME);
         String input = Console.readLine();
-        validInputCarName(input);
+        validInputBlank(input);
         return input;
     }
     public static String inputGameRound(){
@@ -21,23 +28,49 @@ public class InputView {
         validInputGameRound(input);
         return input;
     }
+    public static ArrayList<Car> inputCars(ArrayList<String> carNames) {
+        validCarNameBlank(carNames);
+        validCarNameLength(carNames);
+        validCarsDuplicate(carNames);
 
-    public static void validInputCarName(String input){
-        if(input.isBlank()) throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
-        StringTokenizer st = new StringTokenizer(input, ",");
-        while(st.hasMoreTokens()){
-            if(st.nextToken().length() > 5) throw new IllegalArgumentException("자동차 이름을 5자 이하로 입력해주세요.");
+        ArrayList<Car> cars = new ArrayList<>();
+        for(String car : carNames) cars.add(new Car(car));
+        return cars;
+    }
+
+    public static void validInputBlank(String input){
+        if(input.isBlank()) throw new IllegalArgumentException(BLANK_STRING);
+    }
+
+    public static void validCarNameLength(ArrayList<String> cars) {
+        for(String car : cars){
+            if(car.length() > 5) throw new IllegalArgumentException(REQUIRE_LENGTH_UNDER_FIVE);
         }
     }
 
-    public static void validInputGameRound(String input) {
-        if(input.isBlank()) throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
-
-        for(int i=0; i<input.length(); i++) {
-            if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException("정수 값을 입력해주세요.");
+    public static void validCarNameBlank(ArrayList<String> cars) {
+        for(String car : cars){
+            if (car.contains(" ")) throw new IllegalArgumentException(REQUIRE_NOT_BLANK);
         }
+    }
+
+    public static void validInputInteger(String input){
+        for(int i=0; i<input.length(); i++) {
+            if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException(REQUIRE_INTEGER_TYPE);
+        }
+    }
+    public static void validCarsDuplicate(ArrayList<String> cars){
+        HashSet<String> set = new HashSet<>(cars);
+        if(set.size() != cars.size()) throw new IllegalArgumentException(REQUIRE_NOT_DUPLICATE);
+    }
+    public static void validInputGameRound(String input) {
+        validInputBlank(input);
+
+        validInputInteger(input);
 
         int num = Integer.parseInt(input);
-        if(num<=0) throw new IllegalArgumentException("1회 이상 게임이 진행 되어야 합니다.");
+
+        if(num<=0) throw new IllegalArgumentException(GAME_ROUND_OVER_ONE);
     }
+
 }
