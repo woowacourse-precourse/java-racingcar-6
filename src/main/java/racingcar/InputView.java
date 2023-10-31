839074898e5e12eac154b5141846a5dc9b29f88d
HyungHoKim <hkim1109@naver.com>
2023-10-31 12:38:18 +0900

    refactor(main): 정규형 변경
    가독성이 더 좋은 정규형 형태로 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,7 +6,7 @@ import static racingcar.ExceptionCase.INPUT_TYPE_MISMATCH;
 public class InputView {
     private static final String CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TIMES_TO_TRY_REQUEST = "시도할 횟수는 몇회인가요?";
-    private static final String INT_TYPE = "-?\\d+";
+    private static final String INT_TYPE = "[0-9]*$";
 
     public static String carNames() {
         System.out.println(CAR_NAME_REQUEST);
