2e7f5f44ba6cb9e1b6ef0aa05f48834790d6f387
BellWin98 <jongseunghan@naver.com>
2023-11-01 00:15:41 +0900

    Refactor(InputValidation): 사용자의 입력값을 검증하는 기능 구현

--- src/main/java/racingcar/domain/InputValidation.java
+++ src/main/java/racingcar/domain/InputValidation.java
@@ -1,12 +1,30 @@
 package racingcar.domain;
 
-public class InputValidation {
-    public InputValidation(){}
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-    public boolean isValidName(String carName){
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException();
+public class InputValidation {
+
+    private static final int CONDITION_DIGITS = 5;
+
+    public InputValidation() {
+    }
+
+    public boolean checkName(String name) {
+        if (name.length() > CONDITION_DIGITS) {
+            throw new IllegalArgumentException("5자리 이하의 이름을 입력해주세요.");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("입력된 값이 없습니다. 값을 입력해주세요.");
         }
         return true;
+    }
+
+    public void checkDuplicate(List<String> carNames) {
+        Set<String> nameSet = new HashSet<>(carNames);
+        if (nameSet.size() != carNames.size()) {
+            throw new IllegalArgumentException("중복된 자동차가 있습니다.");
+        }
     }
 }
