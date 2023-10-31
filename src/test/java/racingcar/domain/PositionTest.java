c820b0c2b6c5d01e6333b408ff487ed5b57cddcf
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-31 09:14:41 +0900

    test - Position 테스트코드 작성

---
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+
+    private Position position;
+
+    @BeforeEach()
+    void setUp() {
+        position = new Position(0);
+    }
+
+    @DisplayName("Car Position을 1만큼 증가시킨다.")
+    @Test
+    void increasePosition() {
+        position.increase();
+
+        Assertions.assertEquals(1, position.getCarPosition());
+    }
+}
