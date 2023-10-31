8450749f25bbe3400cee300b0c4bcd5d38963c94
jooda00 <dragon2000a@gmail.com>
2023-11-01 02:35:56 +0900

    test(CarValidatorTest): 입력값 관련 예외 처리 테스트

---
+++ src/test/java/racingcar/validator/CarValidatorTest.java
@@ -0,0 +1,37 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarValidatorTest {
+    private CarValidator carValidator;
+
+    @BeforeEach
+    void setUp() {
+        carValidator = new CarValidator();
+    }
+
+    @Test
+    void 자동차_이름_길이_제한() {
+        List<String> carList = new ArrayList<>(Arrays.asList("apple", "melon", "watermelon"));
+
+        assertThatThrownBy(() -> carValidator.validateCarNameLength(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하여야 합니다.");
+    }
+
+    @Test
+    void 자동차_이름_중복() {
+        List<String> carList = new ArrayList<>(Arrays.asList("apple", "melon", "grape", "melon"));
+
+        assertThatThrownBy(() -> carValidator.validateCarNameDuplication(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 겹치지 않아야 합니다.");
+    }
+}
