093c5a02af400ff2bcca811feda6e24217199015
jalddak <jalddak98@gmail.com>
2023-11-01 01:01:48 +0900

    fix - 출력관련 테스트코드 모두 수정

--- src/test/java/jalddak/view/OutputViewTest.java
+++ src/test/java/jalddak/view/OutputViewTest.java
@@ -6,58 +6,108 @@ import static racingcar.view.OutputView.printSystemMessage;
 import static racingcar.view.OutputView.printWinner;
 import static racingcar.view.SystemMessage.ASK_CAR_LIST;
 import static racingcar.view.SystemMessage.ASK_NUM_OF_MATCHES;
+import static racingcar.view.SystemMessage.TESTING;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.view.OutputView;
 
 public class OutputViewTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+        output.reset();
+    }
+
     @Test
     void 출력확인_printSystemMessage() {
+        //given
+        String expect = ASK_CAR_LIST.getMessage() + "\n"
+                + ASK_NUM_OF_MATCHES.getMessage() + "\n"
+                + TESTING.getMessage() + "\n";
+
+        //when
         printSystemMessage(ASK_CAR_LIST);
         printSystemMessage(ASK_NUM_OF_MATCHES);
+        printSystemMessage(TESTING);
+
+        //then
+        assertThat(output.toString()).isEqualTo(expect);
     }
 
     @Test
     void 출력확인_printAllMatchesResult() {
-        List<Map<String, Integer>> givenList = new ArrayList<>();
-        Map<String, Integer> start = new LinkedHashMap<>();
-        start.put("HJ", 0);
-        start.put("HS", 0);
-        start.put("SJ", 0);
-        Map<String, Integer> second = new LinkedHashMap<>();
-        second.put("HJ", 4);
-        second.put("HS", 2);
-        second.put("SJ", 7);
-        givenList.add(start);
-        givenList.add(second);
-        for (Map<String, Integer> given : givenList) {
-            printPerMatchResult(given);
-        }
+        //given
+        Map<String, Integer> testData = new LinkedHashMap<>();
+        testData.put("HJ", 4);
+        testData.put("HS", 2);
+        testData.put("SJ", 7);
+
+        String expect = "HJ : ----\nHS : --\nSJ : -------\n\n";
+
+        //when
+        printPerMatchResult(testData);
+
+        //then
+        assertThat(output.toString()).isEqualTo(expect);
     }
 
     @Test
     void drawDistance() {
+        // given
         String expect = "----";
-        assertThat(expect).isEqualTo(OutputView.drawDistance(4));
+
+        //when
+        String result = OutputView.drawDistance(4);
+
+        //then
+        assertThat(result).isEqualTo(expect);
     }
 
     @Test
     void 우승자_한명_printWinner() {
+        //given
         List<String> one = new ArrayList<>();
         one.add("HJ");
+
+        String expect = "최종 우승자 : HJ\n";
+
+        //when
         printWinner(one);
+
+        //then
+        assertThat(output.toString()).isEqualTo(expect);
+
     }
 
     @Test
     void 우승자_여러명_printWinner() {
+        //given
         List<String> several = new ArrayList<>();
         several.add("HJ");
         several.add("HS");
         several.add("RK");
+
+        String expect = "최종 우승자 : HJ, HS, RK\n";
+
+        //when
         printWinner(several);
+
+        //then
+        assertThat(output.toString()).isEqualTo(expect);
     }
 }
