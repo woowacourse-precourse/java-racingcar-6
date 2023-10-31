e9772e2b3485f35ac3ec73eb78145aa1b74e51cf
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 14:54:38 +0900

    refactor (Validation): 검증 조건 추가
    공백에 대한 검증과 컴마에 대한 검증을 분리하였습니다:
    before:
    - CheckSpaceOrCommas
    after:
    - CheckSpace
    - CheckComma

--- src/main/java/service/Validation.java
+++ src/main/java/service/Validation.java
@@ -1,43 +1,57 @@
 package service;
 
+import static util.ErrorMessage.CAR_COUNT_ERROR;
+import static util.ErrorMessage.CAR_NAME_COMMA_ERROR;
+import static util.ErrorMessage.CAR_NAME_DUPLICATE_ERROR;
+import static util.ErrorMessage.CAR_NAME_SIZE_ERROR;
+import static util.ErrorMessage.CAR_NAME_SPACE_ERROR;
+
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validation {
     private static final int MAX_NAME_SIZE = 5;
     private static final Pattern timesPattern = Pattern.compile("^[1-9][0-9]*$");
-    private static final Pattern spaceOrCommasPattern = Pattern.compile("^[,\\s]*$");
+    private static final Pattern spacePattern = Pattern.compile("^\\s*$");
+    private static final Pattern commaPattern = Pattern.compile("^,*$");
     public static void CheckCarNames(List<String> carNames){
         CheckDuplicate(carNames);
         CheckNameCount(carNames);
         carNames.forEach(car -> {
             CheckLength(car);
-            CheckSpaceOrCommas(car);
+            CheckSpace(car);
+            CheckComma(car);
         });
 
     }
 
     private static void CheckNameCount(List<String> carNames) {
         if(carNames.size() == 1){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_COUNT_ERROR);
         }
     }
 
     public static void CheckDuplicate(List<String> carNames){
         if(carNames.size() != carNames.stream().distinct().count()){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATE_ERROR);
         };
     }
 
     public static void CheckLength(String carName){
         if(carName.isEmpty() || carName.length()>MAX_NAME_SIZE){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_SIZE_ERROR);
         }
     }
 
-    public static void CheckSpaceOrCommas(String carName){
-        if(spaceOrCommasPattern.matcher(carName).matches()){
-            throw new IllegalArgumentException();
+    public static void CheckSpace(String carName){
+        if(spacePattern.matcher(carName).matches()){
+            throw new IllegalArgumentException(CAR_NAME_SPACE_ERROR);
+        }
+    }
+
+    public static void CheckComma(String carName){
+        if(commaPattern.matcher(carName).matches()){
+            throw new IllegalArgumentException(CAR_NAME_COMMA_ERROR);
         }
     }
 
