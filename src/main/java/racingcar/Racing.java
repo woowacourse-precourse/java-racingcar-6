6e805d3405b6a3e0f25580df67aa5600f76d60de
yohanii <dygks924@gmail.com>
2023-11-01 00:10:44 +0900

    feat: 경주하기 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,10 +15,32 @@ public class Racing {
     public Racing(List<Car> playerList, Integer tryCount) {
         this.tryCount = tryCount;
         this.playerList = playerList;
-        this.winnerList = new ArrayList<>();
     }
 
     public void playRacing() {
+        System.out.println("");
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryCount; i++) {
+            tryOnce();
+            printResult();
+        }
+
+        Integer maxValue = getMaxPosition();
+
+        winnerList = selectWinner(maxValue);
+    }
+
+    private Integer getMaxPosition() {
+        return playerList.stream()
+                .map(Car::getPosition)
+                .max(Integer::compare)
+                .orElse(null);
+    }
+
+    private List<Car> selectWinner(Integer maxValue) {
+        return playerList.stream()
+                .filter(car -> car.getPosition() == maxValue)
+                .toList();
     }
 
     public void tryOnce() {
