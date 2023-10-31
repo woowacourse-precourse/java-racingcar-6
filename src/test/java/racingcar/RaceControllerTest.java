811b6cf2337d714995c4af4870b7091246f8c1f0
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-31 18:24:33 +0900

    doc : CODEREVIEW -> README 로 이전

---
+++ src/test/java/racingcar/RaceControllerTest.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+class RaceControllerTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void startRaceGame() {
+    }
+}
