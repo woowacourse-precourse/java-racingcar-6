5f61cbf602fd376ced8119be1a27c5146131f0c8
isyoo1213 <moethns@gmail.com>
2023-10-31 14:18:43 +0900

    test: CarsTest @BeforeAll/setCarsInfo() 메서드 추가 기존 테스트 케이스마다 Cars 객체를 생성/초기화하는 로직이 불필요하게 반복 -> @BeforeAll  사용으로 테스트 케이스마다 사용될 Cars 객체를 정의 -> @BeforeEach 를 통해 Cars 인스턴스의 필드를 초기화 시키는 방향으로 변경 CarsTest - setCarsInfo() 추가   - @BeforeAll - 기존 initCars()를 resetCars()로 변경   - @BeforeEach - addCars() 삭제   -> 기존 테스트 케이스에서 Cars 인스턴스를 생성/초기화 로직 삭제

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,58 +13,59 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CarsTest {
 
-    List<Car> carList = new ArrayList<>();
-    List<List<Object>> carsStatus = Arrays.asList(
-            Arrays.asList("pobi", 5),
-            Arrays.asList("inss", 2),
-            Arrays.asList("R8", 4)
-    );
+    private static List<Car> carList;
+    private static Cars racingCars;
+    private static List<List<Object>> carsStatus;
+
+    @BeforeAll
+    static void setCarsInfo() {
+        carList = new ArrayList<>();
+        racingCars = new Cars();
+        carsStatus = Arrays.asList(
+                Arrays.asList("pobi", 5),
+                Arrays.asList("inss", 2),
+                Arrays.asList("R8", 4)
+        );
+    }
 
     @BeforeEach
-    void initCars() {
+    void resetCars() {
+        carList.clear();
+        racingCars.provideRacingCars().clear();
         for (List<Object> carStatus : carsStatus) {
             Car car = new Car((String) carStatus.get(0));
             for (int moveTimes = 0; moveTimes < (Integer) carStatus.get(1); moveTimes++) {
                 car.calculatePosition(true);
             }
             carList.add(car);
+            racingCars.addCar(car);
         }
     }
 
     @DisplayName("각 자동차 저장/반환 테스트 - 정상 데이터 - 성공")
     @Test
     void addCarsTest() {
-        Cars cars = new Cars();
+        racingCars.provideRacingCars().clear();
         for (Car car : carList) {
-            cars.addCar(car);
+            racingCars.addCar(car);
         }
-        assertThat(cars.provideRacingCars()).isEqualTo(carList);
+        assertThat(racingCars.provideRacingCars()).isEqualTo(carList);
     }
 
     @DisplayName("자동차 총 대수 반환 테스트 - 정상 데이터 - 성공")
     @Test
     void getNumberOfCarsTest() {
-        Cars cars = new Cars();
-        addCar(cars);
-        assertThat(cars.getNumberOfCars()).isEqualTo(carList.size());
+        assertThat(racingCars.getNumberOfCars()).isEqualTo(carList.size());
     }
 
     @DisplayName("각 자동차 위치정보 적용 테스트 - 정상 데이터 - 성공")
     @Test
     void updateCarsPositionTest() {
-        Cars cars = new Cars();
-        addCar(cars);
-        cars.updateCarsPosition(Arrays.asList(true, true, true));
+        racingCars.updateCarsPosition(Arrays.asList(true, true, true));
 
-        for (int i = 0; i < cars.getNumberOfCars(); i++) {
-            assertThat(cars.provideRacingCars().get(i).provideCarStatus().get("position"))
+        for (int i = 0; i < racingCars.getNumberOfCars(); i++) {
+            assertThat(racingCars.provideRacingCars().get(i).provideCarStatus().get("position"))
                     .isEqualTo((Integer)carsStatus.get(i).get(1) + 1);
-        }
-    }
-
-    private void addCar(Cars cars) {
-        for (Car car : carList) {
-            cars.addCar(car);
         }
     }
 }
