bb51f96760908c05293080850d1293a17da50448
“sangjun121” <juncho12011201@gmail.com>
2023-10-31 16:31:58 +0900

    feat: 문자열이 공백 일때 예외처리 구현
    문자열 입력시 엔터입력 등으로 인한 문자열의 길이가 0으로 비워져 있으면 예외 발생시키도록 메소드 isCarsNameEmpty() 구현 완료

--- src/main/java/racingcar/controller/InputException.java
+++ src/main/java/racingcar/controller/InputException.java
@@ -8,9 +8,24 @@ public class InputException {
 
     public InputException(){}
 
-    public void carsNameException(){}
+    public void carsNameException(String carsName){
+        if(isCarsNameEmpty(carsName)){ //문자열에 비었을 때 (엔터 입력시)
+            throw new IllegalArgumentException();
+        }
+
+        if(isCommaAtImproperPosition(carsName)){
+            throw new IllegalArgumentException();
+        }
+    }
 
     private boolean isCommaAtImproperPosition(String carsName){
         return(Pattern.matches(IMPROPER_COMMA_POSITION,carsName));
+    }
+
+    private boolean isCarsNameEmpty(String carsName){
+        if(carsName.length() == 0){
+            return true;
+        }
+        return false;
     }
 }
