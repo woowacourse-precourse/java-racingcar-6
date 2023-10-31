2a0ccaaf4427c3de54928dc957620cf7245c2c07
SeungGwa123 <skwan123@naver.com>
2023-10-31 18:33:30 +0900

    Feat: Add opening
    게임에서 사용자 입력을 받는 오프닝 작성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,2 +1,17 @@
-package racingcar;public class Game {
+package racingcar;
+
+import java.util.Map;
+
+public class Game {
+    public void opening() {
+        Input ip = new Input();
+        String[] car_name = ip.carName();
+        Map<String, Integer> parkinglot = ip.carDistance(car_name);
+        int runtime = ip.runtime();
+        System.out.println();
+        gameStart(parkinglot, car_name, runtime);
+    }
+    public void gameStart(Map<String,Integer> parkinglot, String[] car_name, int runtime) {
+
+    }
 }
