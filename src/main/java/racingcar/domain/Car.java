b393c41555973781a41369eccc9b8d27d7d150be
NyaongKAI <yukihusiru18@gmail.com>
2023-11-01 03:37:41 +0900

    Car 객체 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,18 @@
 package racingcar.domain;
 
 public class Car {
+    private final String carName;
+    private int position = 0;
 
+    public Car(String carName){
+        this.carName = carName;
+    }
+
+    public String getCarName(){
+        return carName;
+    }
+
+    public int getPosition(){
+        return position;
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,5 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class InputView {
     public static void requestCarNamesText(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
