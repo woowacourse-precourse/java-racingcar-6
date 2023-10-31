e4f3bacdc24356b6213e103f8a4922178c19fc32
InJun2 <os2864@naver.com>
2023-10-31 14:07:57 +0900

    :sparkles: 레이싱카 실행 로직 구현

--- src/main/java/racingcar/resolver/RacingCarRun.java
+++ src/main/java/racingcar/resolver/RacingCarRun.java
@@ -1,16 +1,55 @@
 package racingcar.resolver;
 
-import racingcar.controller.RacingCarController;
+import racingcar.model.Cars;
+import racingcar.service.RacingCarService;
+import racingcar.service.TextProcessor;
 import racingcar.view.RacingCarView;
 
+import java.util.List;
+
 public class RacingCarRun {
-    private final RacingCarController controller;
+    private final RacingCarService service;
+    private final TextProcessor textProcessor;
     private final RacingCarView view;
 
     public RacingCarRun() {
-        this.controller = new RacingCarController();
+        this.service = new RacingCarService();
+        this.textProcessor = new TextProcessor();
         this.view = new RacingCarView();
     }
 
+    public void run() {
+        var cars = createCarsByInput(view.getCarNameInput());
+        int count = getTryCountByInput(view.getTryCountInput());
+        view.racingResultOutput();
+        repeatMoveCar(cars, count);
+        view.racingWinnerOutput(getRacingWinners(cars));
+    }
 
+    private Cars createCarsByInput(String carNames) {
+        var separatorCarNames = textProcessor.splitCarNames(carNames);
+        return service.createCars(separatorCarNames);
+    }
+
+    private int getTryCountByInput(String tryCount) {
+        return textProcessor.parseTryNumber(tryCount);
+    }
+
+    private void repeatMoveCar(Cars cars, int count) {
+        for (int i = 0; i < count; i++) {
+            service.moveCars(cars);
+            String output = textProcessor.carsDistanceAsString(cars);
+            view.racingCarsDistanceOutput(output);
+        }
+    }
+
+    private String getRacingWinners(Cars cars) {
+        var winners = choiceRacingWinner(cars);
+        return textProcessor.joinText(winners);
+    }
+
+    private List<String> choiceRacingWinner(Cars cars) {
+        int maxDistance = service.maxCarDistance(cars);
+        return service.getWinnerCarNames(cars, maxDistance);
+    }
 }
