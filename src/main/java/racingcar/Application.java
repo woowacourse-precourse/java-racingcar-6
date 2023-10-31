753f0c767659884fc4be449eb7cf3f86d92237cf
hoduulmu <timewarp96@naver.com>
2023-11-01 01:32:58 +0900

    feat: 사용자는 몇 번의 이동을 할 것인지 입력한다 입력값은 숫자만 가능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         List<String> carNames = inputView.askCarNames();
+        int roundNumber = inputView.askRoundNumber();
         for (String carName : carNames) {
             Car car = new Car(carName);
         }
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -8,7 +8,9 @@ import java.util.function.Supplier;
 public class InputView {
 
     private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_ROUND_NUMBER = "시도할 회수는 몇회인가요?";
     private static final String COMMA = ",";
+    private static final String ERROR_INPUT_MUST_NUMBER = "입력값은 숫자여야 합니다.";
     private final Supplier<String> reader;
 
     public InputView() {
@@ -24,6 +26,20 @@ public class InputView {
         String carNamesInput = reader.get();
         String[] carNames = carNamesInput.split(COMMA);
         return List.of(carNames);
+    }
+
+    public int askRoundNumber() {
+        println(ASK_ROUND_NUMBER);
+        String roundNumberInput = reader.get();
+        return toInteger(roundNumberInput);
+    }
+
+    private int toInteger(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_INPUT_MUST_NUMBER);
+        }
     }
 
     private void println(String message) {
