d815efe42ff28ef8d9ad10ee83cbf6b30726987e
LUKR7Q <ybkim0628@naver.com>
2023-10-31 12:31:41 +0900

    기능(car): Car 클래스의 기본 구현 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private final String name;
+    private final StringBuffer report;
+
+    public Car(String name) {
+        this.name = name;
+        this.report = new StringBuffer();
+        this.report.append(name);
+        this.report.append(" : ");
+    }
+
 }
