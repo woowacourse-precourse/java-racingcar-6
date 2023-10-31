2fe479897af335c9d6314f5cd118e85aa6b9397c
rdyjun <rdyjun00@gmail.com>
2023-10-31 13:09:50 +0900

    feat: Add comparing all car and get winner

--- src/main/java/racingcar/CarRacingManagement.java
+++ src/main/java/racingcar/CarRacingManagement.java
@@ -3,10 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class CarRacingManagement {
-    public static void startRacing() {
+    public static void startRacing () {
         Commentator.printEnterCarName();
         String inputValue = Console.readLine();
         inputValue = inputValue.replaceAll(" ", "");
@@ -25,11 +26,31 @@ public class CarRacingManagement {
             updateAllCarPosition(cars);
             Commentator.printNowProgress(cars);
         }
+        String winCarList = winnerCar(cars);
+        Commentator.printWinner(winCarList);
     }
-    private static void updateAllCarPosition(Car[] cars) {
+    private static void updateAllCarPosition (Car[] cars) {
         for (Car car : cars) {
             int randomDigit = Randoms.pickNumberInRange(0, 9);
             car.updatePosition(randomDigit);
         }
+    }
+    private static String winnerCar (Car[] cars) {
+        String[] winnerList = getWinnerList(cars);
+        String result = "";
+        for (int winnerIndex = 0; winnerIndex < winnerList.length - 1; winnerIndex++)
+            result += winnerList[winnerIndex] + ", ";
+        result +=winnerList[winnerList.length - 1];
+        return result;
+    }
+    private static String[] getWinnerList (Car[] cars) {
+        int winnerPosition = Arrays.stream(cars)
+                .mapToInt(car -> car.getCarPosition())
+                .max()
+                .orElse(1);
+        return Arrays.stream(cars)
+                .filter(car -> car.getCarPosition() == winnerPosition)
+                .map(car -> car.getCarName())
+                .toArray(String[]::new);
     }
 }
