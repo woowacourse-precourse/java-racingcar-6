1ab5dc314eb1940c569199ab585c1bb32160a6d7
Sinorang <aodkrak@naver.com>
2023-11-01 04:09:46 +0900

    chore. 자동차(경주) 이름 추가
    - 경주할 자동차 이름을 입력받기 위해 변수 name 선언

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,7 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
 
+    private final String name;
     private int successCount;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     int getSuccessCount() {
         return successCount;
