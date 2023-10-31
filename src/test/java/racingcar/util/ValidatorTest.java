8840ab9898e2538ebfd048cd4ff9b0c8ddf2fd85
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-01 06:35:02 +0900

    style: 미사용 import 제거

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
