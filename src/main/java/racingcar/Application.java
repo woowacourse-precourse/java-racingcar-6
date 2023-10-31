35df14730cc86a136b9f997f89896b0f6e21a98a
Subi Hwang <ghkdtnql@gmail.com>
2023-10-31 10:09:52 +0900

    feat: 사용자 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,41 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Application {
+
+    static ArrayList<String> user = new ArrayList<>();
+    static int TESTCASE = 0;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        try {
+            st = new StringTokenizer(br.readLine(),",");
+            user.add(st.nextToken());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        try {
+            TESTCASE = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("\n" + "실행결과" + "\n");
     }
 }
