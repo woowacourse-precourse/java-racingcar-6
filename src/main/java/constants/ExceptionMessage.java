5ac49c2983450cc4e1a391bcf5b78c2142f7b176
nayonsoso <panda0329@naver.com>
2023-11-01 00:41:01 +0900

    feat(ExceptionMessage): 이름 목록 예외 메세지 추가

--- src/main/java/constants/ExceptionMessage.java
+++ src/main/java/constants/ExceptionMessage.java
@@ -1,5 +1,10 @@
 package constants;
 
 public class ExceptionMessage {
-
+    // 이름 목록 예외 메세지
+    private final static String EMPTY_INPUT = "입력된 값이 없습니다.";
+    private final static String ENDS_WITH_COMMA = "마지막이 쉼표로 끝납니다.";
+    private final static String CONTAINS_REPEATING_COMMAS = "쉼표 사이에 값이 없습니다.";
+    private final static String EXCEEDED_NAME_LENGTH = "이름의 길이가 최대 길이를 넘습니다.";
+    private final static String DUPLICATED_NAME = "중복되는 이름이 있습니다.";
 }
