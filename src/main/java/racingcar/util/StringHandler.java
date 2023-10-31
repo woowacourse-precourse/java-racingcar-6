f41f46e6d688f2660b5faa80f369bdf2b1e09b07
PARK9898 <wjddjs21@naver.com>
2023-10-31 15:54:45 +0900

    "refactor: 숫자가 아닌 문자에 대한 예외처리"

--- src/main/java/racingcar/util/StringHandler.java
+++ src/main/java/racingcar/util/StringHandler.java
@@ -18,6 +18,11 @@ public class StringHandler {
     }
 
     public static int stringToInt(String input) {
-        return Integer.parseInt(input);
+        try {
+            int output = Integer.parseInt(input);
+            return output;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("문자가 아닌 숫자를 입력해야합니다");
+        }
     }
 }
