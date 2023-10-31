7588967fb96d1176acaabde7ab4b9fbf7649c4d5
hjeongb0320 <hjeongb0320@naver.com>
2023-10-31 12:31:29 +0900

    feat(src/main/java) : test 파일 구현
    각 입력값에 대하여 test 파일 구현

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,8 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,29 +12,150 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    void 차_두대_승자_한_명() {
+    void ㄱ_자동차_입력받기_일반() {
+        String inputCars = "pobi,woni,hjb,java,spri";
+        String inputPlayTime = "1";
+
+        run(inputCars, inputPlayTime);
+        assertThat(output()).contains("pobi, woni, hjb, java, spri");
+        assertThat(output()).contains("1");
+    }
+
+    @Test
+    void ㄴ1_자동차_입력받기_이름이_5자_이상() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄴ2_자동차_입력받기_이름이_없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄴ3_자동차_입력받기_이름이_공백문자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("     ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄴ4_자동차_입력받기_이름이_공백문자_미포함_5자_이상() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("t, 3  5  ㅇ  ㅇ ㅏ   k", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄷ1_자동차_입력받기_실행회수_숫자아님() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1ab8d"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄷ3_자동차_입력받기_실행회수_0() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄷ4_자동차_입력받기_실행회수_음수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void ㄹ1_전진내용_갱신_전진() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "2");
+                    assertThat(output()).contains("-", "--");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void ㄹ2_전진내용_갱신_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "1");
+                    assertThat(output()).contains("pobi : \n");
+                },
+                STOP
+        );
+    }
+
+    @Test
+    void ㅁ_전진내용_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : -\nwoni : -", "pobi : -\nwoni : --");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void ㅂ1_단일승자_출력() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                    assertThat(output()).contains("최종 우승자 : pobi");
                 },
                 MOVING_FORWARD, STOP
         );
     }
 
     @Test
-    void 차_두대_승자_두명() {
+    void ㅂ2_단일승자_reverse_출력() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("pobi,woni", "2");
-                    assertThat(output()).contains("pobi : --", "woni : --", "최종 우승자 : pobi, woni");
+                    run("woni,pobi", "1");
+                    assertThat(output()).contains("최종 우승자 : pobi");
                 },
-                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+                STOP, MOVING_FORWARD
         );
     }
 
     @Test
-    void 차_세대_승자_세명() {
+    void ㅅ1_다수승자_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void ㅅ2_다수승자_reverse_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("woni,pobi", "1");
+                    assertThat(output()).contains("최종 우승자 : woni, pobi");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void ㅇ1_실행결과_차_세대_승자_세명() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni,hjb", "2");
@@ -46,79 +166,13 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 차_세대_승자_두명() {
+    void ㅇ2_실행결과_차_세대_승자_두명() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni,hjb", "2");
                     assertThat(output()).contains("pobi : --", "woni : --", "hjb : -", "최종 우승자 : pobi, woni");
                 },
                 MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, STOP
-        );
-    }
-
-    @Test
-    void 이름이_white_space_포함() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("po b  i,w   o   n  i", "2");
-                    assertThat(output()).contains("pobi : --", "woni : --", "최종 우승자 : pobi, woni");
-                },
-                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
-        );
-    }
-
-    @Test
-    void 유효성_검사_이름이_5자_이상() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 유효성_검사_실행회수_숫자아님() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1ab8d"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 유효성_검사_실행회수_0() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "0"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-    @Test
-    void 유효성_검사_실행회수_음수() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "-1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 유효성_검사_이름이_없음() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 유효성_검사_이름이_공백문자() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("     ", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 유효성_검사_이름이_공백문자_포함_5자_이상() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("t, 3  5  ㅇ  ㅇ ㅏ   k", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
