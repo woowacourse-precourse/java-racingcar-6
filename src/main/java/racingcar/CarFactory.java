ff728b4cb516760b7c8acf3d34edd769f27f0842
ehtjsv2 <ehtjsv2@naver.com>
2023-11-01 07:54:08 +0900

    Feat: 중복된 이름 확인 기능 구현

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -15,7 +16,14 @@ public class CarFactory {
                 .collect(Collectors.toList());
     }
 
-    public void hasDuplicatedName(){
+    public boolean hasDuplicatedName(List<String> names){
+        if(countDistinctListSize(names)!=names.size()){
+            return true;
+        }
+        return false;
+    }
 
+    public Long countDistinctListSize(List<String> list){
+        return list.stream().distinct().count();
     }
 }
--- src/test/java/developtest/CarFactoryTest.java
+++ src/test/java/developtest/CarFactoryTest.java
@@ -2,10 +2,12 @@ package developtest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,6 +27,13 @@ public class CarFactoryTest {
     void stringtoListByComma_콤마를_기준으로_문자열을_리스트로_변환(String string){
         List<String> result = carFactory.stringtoListByComma(string);
         assertThat(result.toString()).isEqualTo(Arrays.asList("pobi","crong").toString());
+    }
+
+    @Test
+    void hasDuplicatedName_중복된_이름_가지고_있는지_확인(){
+        List<String> names = new ArrayList<String>(Arrays.asList("pobi","crong","pobi"));
+        boolean result = carFactory.hasDuplicatedName(names);
+        assertThat(result).isEqualTo(true);
     }
 
 }
