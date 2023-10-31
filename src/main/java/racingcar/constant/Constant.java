6c29ee30050958020dda7d1ef5a8d913de886821
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 19:02:57 +0900

    feat : OutputView 경주 시작 멘트 출력 기능 추가

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -3,4 +3,5 @@ package racingcar.constant;
 public class Constant {
 
     public static final String COMMA = ",";
+    public static final String EXECUTION_RESULT_MENT = "실행 결과";
 }
---
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view.output;
+
+import static racingcar.constant.Constant.EXECUTION_RESULT_MENT;
+
+public class OutputView {
+
+    public void printExecutionResultMent() {
+        System.out.println(EXECUTION_RESULT_MENT);
+    }
+}
