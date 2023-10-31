fe8a96dcde561d8509edb8d03cd9fe78af39d488
ohhamma <judyham10634@naver.com>
2023-10-31 19:13:52 +0900

    :art: refactor(CarPosition): 자동차 위치 유효 기준 변경
    - validateCarPosition : 자동차 위치 0보다 작아지면 예외처리

--- src/main/java/racingcar/data/CarPosition.java
+++ src/main/java/racingcar/data/CarPosition.java
@@ -2,8 +2,16 @@ package racingcar.data;
 
 public class CarPosition {
     private int pos;
+    private static final String NEGATIVE_INTEGER = "CarPosition cannot be a negative integer.";
 
     public CarPosition(int pos) {
         this.pos = pos;
+        validateCarPosition(pos);
+    }
+
+    public static void validateCarPosition(int pos) {
+        if (pos < 0) {
+            throw new IllegalArgumentException(NEGATIVE_INTEGER);
+        }
     }
 }
