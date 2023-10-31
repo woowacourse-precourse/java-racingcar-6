d9662d9e58df335ef8aef2bce9263ac020a527c8
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 10:43:16 +0900

    Test: carRaceOnce 테스트 코드 추가
    움직일 수 있는 차 2대와 움직일 수 없는 차 1대를 이용해 game객체를 생성한다
    레이스를 한 번 시행한 후 이들의 positiondl (1,1,0)과 같은지 확인한다

---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    Car movableCar1;
+    Car movableCar2;
+    Car immovableCar;
+    Game game;
+    static final int givenAttemptCount = 5;
+
+    @BeforeEach
+    void setUp() {
+        movableCar1 = new Car("win1", () -> 4);
+        movableCar2 = new Car("win2", () -> 4);
+        immovableCar = new Car("loser", () -> 3);
+
+        game = new Game(Arrays.asList(movableCar1, movableCar2, immovableCar), givenAttemptCount);
+    }
+
+    @Test
+    @DisplayName("한 번 시행 후 position리스트 (1,1,0)")
+    void 레이스_한번_시행() {
+        game.carRaceOnce();
+        Assertions.assertEquals(Arrays.asList(1,1,0),
+                game.getCars().stream().map(Car::getPosition).toList());
+    }
+}
