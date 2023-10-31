9a926e88f5f3b5e8c73dbe3143514d9ead6d84ce
J0YERIM <chosarah70@gmail.com>
2023-10-31 13:59:41 +0900

    refactor: Add constant and adjust minimum range

--- src/main/java/racingcar/exception/InvalidAttemptCountException.java
+++ src/main/java/racingcar/exception/InvalidAttemptCountException.java
@@ -4,6 +4,8 @@ public class InvalidAttemptCountException extends IllegalArgumentException {
 
     private static final String DEFAULT_MESSAGE = "시도할 회수는 양의 정수여야 합니다.";
 
+    private static final int MIN_BOUND = 0;
+
     public InvalidAttemptCountException() {
         super(DEFAULT_MESSAGE);
     }
@@ -16,13 +18,13 @@ public class InvalidAttemptCountException extends IllegalArgumentException {
         try {
             return Integer.parseInt(attemptCount);
         } catch (NumberFormatException e) {
-            throw new InvalidAttemptCountException("");
+            throw new InvalidAttemptCountException();
         }
     }
 
     public static void validate(int attemptCount) {
-        if (attemptCount <= 0) {
-            throw new InvalidAttemptCountException(DEFAULT_MESSAGE);
+        if (attemptCount < MIN_BOUND) {
+            throw new InvalidAttemptCountException();
         }
     }
 }
