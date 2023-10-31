dc9e0f4baf3795c25947e680c00ee5fe5c61688f
sinminseok12 <sinminseok12@naver.com>
2023-10-31 15:25:44 +0900

    test(TryCountTest): TryCount 생성자에 대한 테스트 기능 구현

--- src/test/java/racingcar/model/TryCountTest.java
+++ src/test/java/racingcar/model/TryCountTest.java
@@ -9,6 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TryCountTest {
 
     @Test
+    void 생성자는_전달받은_횟수_로_TryCount_객체를_생성한다() {
+        TryCount tryCount = new TryCount(5);
+        assertThat(tryCount.getCount()).isEqualTo(5);
+    }
+
+    @Test
     void decrease_메서드는_시도회수를_1_감소시킨다() {
         TryCount count = new TryCount(2);
         count.decrease();
