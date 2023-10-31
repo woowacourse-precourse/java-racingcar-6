626f8e50bf3804ea05ce855a542a9431f7d41c69
gourderased <gourderased@gmail.com>
2023-10-31 10:31:02 +0900

    "feat(racingcar): 이동 횟수 양수값 검증 로직 추가"

--- src/main/java/racingcar/utils/Validater.java
+++ src/main/java/racingcar/utils/Validater.java
@@ -11,4 +11,9 @@ public class Validater {
             throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
         }
     }
+    public static void validateNumberOfMove(int move) {
+        if(move <= 0) {
+            throw new IllegalArgumentException("이동 횟수는 양수여야 합니다.");
+        }
+    }
 }
