a1f5bdcb79c3aa06fd10f8f2342b2f9e3e9a3bba
황준혁 <hyug0819@gmail.com>
2023-11-01 00:52:59 +0900

    feat : 레이싱 게임 진행 구현

--- src/main/java/racingcar/play/game/Game.java
+++ src/main/java/racingcar/play/game/Game.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import racingcar.play.car.Car;
 import racingcar.play.user.User;
+import racingcar.util.Random;
 
 public class Game {
 
@@ -14,7 +15,31 @@ public class Game {
 
         //사용자로부터 시도 회수 입력
         int count = User.input();
+        play(names, count);
     }
+
+    public static void play(String[] names, int count){
+        System.out.println("실행 결과");
+        Map<String, String> score = new LinkedHashMap<>();
+
+        //초기화
+        for(String name : names){
+            score.put(name, "");
+        }
+
+        //Map의 Key모임
+        String[] keys = score.keySet().toArray(new String[0]);
+
+        //시도 회수만큼 반복
+        for(int i=0;i<count;i++){
+            racing(keys, score);
+        }
+
+        //게임 종료로 이동
+
+
+    }
+
     private static void racing(String[] keys, Map<String, String> score) {
         //점수 증가
         for (String key : keys) {
