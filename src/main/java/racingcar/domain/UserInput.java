321ab9626b2a0488d10efcad385794262b6239ba
JeonJW24 <tyui153@naver.com>
2023-10-31 20:32:13 +0900

    fix(racingcar): 자동차 이름 입력 기능 로직 수정

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -8,7 +8,10 @@ public class UserInput {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         List<String> carName = List.of(input.split(","));
-
+        InvalidValue invalidValue = new InvalidValue();
+        if(invalidValue.isExceedFiveLetters(carName)) {
+            throw new IllegalArgumentException();
+        }
         return carName;
     }
 
