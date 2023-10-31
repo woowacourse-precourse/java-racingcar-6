e2d9e1cc92256e4f29c572380d71f601d9ba64d8
Jae-Young98 <ygy0102@gmail.com>
2023-10-31 23:43:25 +0900

    refactor(InputValidator): 부정형 네이밍 변경

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -2,13 +2,13 @@ package racingcar.util;
 
 import java.util.List;
 
-import static racingcar.constant.NumberConstant.MAX_NAME_LENGTH;
-import static racingcar.constant.NumberConstant.MIN_ATTEMPT_NUMBER;
-import static racingcar.constant.NumberConstant.MIN_NAME_LENGTH;
-import static racingcar.constant.StringConstant.DIGIT;
+import static racingcar.enums.CarConstant.MAX_NAME_LENGTH;
+import static racingcar.enums.CarConstant.MIN_NAME_LENGTH;
 import static racingcar.enums.ExceptionMessage.INVALID_ATTEMPT_NUMBER_RANGE_EXCEPTION_MESSAGE;
 import static racingcar.enums.ExceptionMessage.INVALID_NAME_LENGTH_EXCEPTION_MESSAGE;
 import static racingcar.enums.ExceptionMessage.INVALID_NUMBER_EXCEPTION_MESSAGE;
+import static racingcar.enums.RaceConstant.MIN_ATTEMPT_NUMBER;
+import static racingcar.enums.RegexConstant.DIGIT_REGEX;
 
 public class InputValidator {
     public static void validateCarNames(List<String> carNames) {
@@ -16,28 +16,34 @@ public class InputValidator {
     }
 
     private static void validateNameLength(List<String> carNames) {
-        boolean invalidLength = carNames.stream()
-                .anyMatch(name -> name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH);
+        boolean validLength = carNames.stream()
+                .allMatch(InputValidator::isNameLengthValid);
 
-        if (invalidLength) {
+        if (!validLength) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH_EXCEPTION_MESSAGE.getMessage());
         }
     }
 
+    private static boolean isNameLengthValid(String name) {
+        int nameLength = name.length();
+
+        return nameLength >= MIN_NAME_LENGTH.getNumber() && nameLength <= MAX_NAME_LENGTH.getNumber();
+    }
+
     public static void validateAttemptNumber(String input) {
-        if (isNotNumber(input)) {
+        if (!isNumber(input)) {
             throw new IllegalArgumentException(INVALID_NUMBER_EXCEPTION_MESSAGE.getMessage());
         }
-        if (isInvalidRange(input)) {
+        if (!isRange(input)) {
             throw new IllegalArgumentException(INVALID_ATTEMPT_NUMBER_RANGE_EXCEPTION_MESSAGE.getMessage());
         }
     }
 
-    private static boolean isNotNumber(String number) {
-        return !number.matches(DIGIT);
+    private static boolean isNumber(String number) {
+        return number.matches(DIGIT_REGEX.getRegex());
     }
 
-    private static boolean isInvalidRange(String number) {
-        return Integer.parseInt(number) < MIN_ATTEMPT_NUMBER;
+    private static boolean isRange(String number) {
+        return Integer.parseInt(number) >= MIN_ATTEMPT_NUMBER.getNumber();
     }
 }
