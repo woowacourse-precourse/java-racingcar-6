3e440fc7b14d175e41f8a9723dd9b4f0a12c6dbf
minsu20 <syt07100@gmail.com>
2023-11-01 02:43:44 +0900

    fix(constants): 구분자 수정

--- src/main/java/racingcar/constants/Delimiters.java
+++ src/main/java/racingcar/constants/Delimiters.java
@@ -3,7 +3,7 @@ package racingcar.constants;
 public enum Delimiters {
 
     CAR_NAME_DELIMITER(","),
-    RACE_WINNER_DELIMITER(","),
+    RACE_WINNER_DELIMITER(", "),
     CAR_NAME_DELIMITER_DESCRIPTION("쉼표(,)");
 
     private final String message;
