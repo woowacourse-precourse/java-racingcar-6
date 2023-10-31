02473d0e49879ae5f295d4cfdb335dae2b7464ed
win9 <krrr883@gmail.com>
2023-10-31 12:11:14 +0900

    [Feat]: 오류발생 로직 구현
    이름이 5자리가 넘고, 숫자 입력시 양의 정수가 아니면 오류를 발생하도록 변경

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -6,10 +6,19 @@ import java.util.stream.Collectors;
 
 public class Parser {
     public static List<String> parseCarNameInput (String input) {
-        return Arrays.stream(input.split(",")).collect(Collectors.toList());
+        List<String> nameList = Arrays.stream(input.split(",")).collect(Collectors.toList());
+        for (String s : nameList) {
+            if (s.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return nameList;
     }
 
     public static int parseTryTimesInput(String input) {
+        if (!input.matches("^\\d+$")) {
+            throw new IllegalArgumentException();
+        }
         return Integer.parseInt(input);
     }
 }
