da47b7b01a581c431d7f16356d07d29ca834b2cd
youngduck98 <tommykimyd98@gmail.com>
2023-10-31 20:20:47 +0900

    model-car 구현

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,4 +1,28 @@
 package racingcar.Model;
 
-public class Car {
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car implements Comparable<Car>{
+    private String name;
+    private Integer process;
+
+    public Car(String name){
+        this.name = name;
+        process = 0;
+    }
+
+    public int getProcess(){
+        return this.process;
+    }
+
+    public void trial(){
+        int n = Randoms.pickNumberInRange(0, 9);
+        if(n >=4)
+            this.process++;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Integer.compare(this.process, o.process);
+    }
 }
