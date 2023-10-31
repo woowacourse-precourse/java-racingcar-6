da33fecee25292564b5bac75cbad85b7576bbe02
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 20:35:40 +0900

    Refactor: CheckValidation 메소드 추출
    랜덤 숫자 발생기의 숫자의 유효성을 확인하는 역할 분리
    유효하지 않을 경우 예외 발생

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -5,16 +5,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator implements NumberGenerator {
 
-    private final static int START_INCLUSIVE = 1;
+    private final static int START_INCLUSIVE = 0;
     private final static int END_INCLUSIVE = 9;
 
     @Override
     public int generate() {
-        int randomNumber =  Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-        if(randomNumber >= START_INCLUSIVE && randomNumber <= END_INCLUSIVE){
-            return randomNumber;
-        }
+        int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+        checkValidation(randomNumber);
+        return randomNumber;
 
-        throw new IllegalArgumentException();
+    }
+
+    private void checkValidation(int randomNumber) {
+        if (randomNumber < START_INCLUSIVE || randomNumber > END_INCLUSIVE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
