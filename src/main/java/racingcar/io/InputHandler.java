2da86ca30e363a43e5759a22f9801552a5f47247
tfer2442 <tfer2442@gmail.com>
2023-11-01 03:50:43 +0900

    refactor(Validator, InputHandler, StringManipulator: 입력 검증 메서드 Validator로 이동

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -1,14 +1,11 @@
 package racingcar.io;
 
 import static racingcar.constant.Constants.INPUT_SEPARATOR;
-import static racingcar.constant.ExceptionMessage.DUPLICATED_CAR_NAME;
-import static racingcar.constant.ExceptionMessage.INVALID_TRIAL_NUMBER;
-import static racingcar.constant.ExceptionMessage.LENGTH_OUT_OF_RANGE_CAR_NAME;
-import static racingcar.constant.ExceptionMessage.NOT_ALLOWED_WORDS_CAR_NAME;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.util.StringManipulator;
+import racingcar.util.Validator;
 
 public class InputHandler {
     public static String readLine() {
@@ -16,34 +13,14 @@ public class InputHandler {
     }
     public static List<String> convertCarNames(String carNamesWithSeparator) {
         String[] carNameGroup = StringManipulator.splitSeparator(carNamesWithSeparator, INPUT_SEPARATOR);
-        checkValidCarNames(carNameGroup);
+        Validator.checkValidCarNames(carNameGroup);
         List<String> carNames = StringManipulator.toList(carNameGroup);
 
         return carNames;
     }
 
     public static int convertTrialNumber(String trialNum) {
-        checkValidTryNumber(trialNum);
+        Validator.checkValidTrialNumber(trialNum);
         return Integer.parseInt(trialNum);
-    }
-
-    private static void checkValidCarNames(String[] carNameGroup) {
-        if (!StringManipulator.isValidLength(carNameGroup)) {
-            throw new IllegalArgumentException(LENGTH_OUT_OF_RANGE_CAR_NAME);
-        }
-
-        if (StringManipulator.hasDuplicates(carNameGroup)) {
-            throw new IllegalArgumentException(DUPLICATED_CAR_NAME);
-        }
-
-        if (!StringManipulator.isValidNames(carNameGroup)) {
-            throw new IllegalArgumentException(NOT_ALLOWED_WORDS_CAR_NAME);
-        }
-    }
-
-    private static void checkValidTryNumber(String trialNum) {
-        if (!trialNum.matches("^[1-9][0-9]*$")) {
-            throw new IllegalArgumentException(INVALID_TRIAL_NUMBER);
-        }
     }
 }
--- src/main/java/racingcar/util/StringManipulator.java
+++ src/main/java/racingcar/util/StringManipulator.java
@@ -1,12 +1,7 @@
 package racingcar.util;
 
-import static racingcar.constant.Constants.MAX_CAR_NAME_LEN;
-import static racingcar.constant.Constants.MIN_CAR_NAME_LEN;
-
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class StringManipulator {
     public static String[] splitSeparator(String sentence, String separator) {
@@ -14,35 +9,5 @@ public class StringManipulator {
     }
     public static List<String> toList(String[] tokens) {
         return Arrays.asList(tokens);
-    }
-
-    public static boolean isValidLength(String[] strings) {
-        for (String element : strings) {
-            if (element.length() < MIN_CAR_NAME_LEN || element.length() > MAX_CAR_NAME_LEN) {
-                return false;
-            }
-        }
-        return true; 
-    }
-
-    public static boolean hasDuplicates(String[] strings) {
-        Set<String> set = new HashSet<>();
-
-        for (String element : strings) {
-            if (!set.add(element)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public static boolean isValidNames(String[] strings) {
-        for (String element : strings) {
-            if (!element.matches("^[0-9a-zA-Z가-힣_]+$")) {
-                return false;
-            }
-        }
-        return true;
     }
 }
