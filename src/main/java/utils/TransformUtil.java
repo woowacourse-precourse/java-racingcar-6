2db8e4652d2c16f9efbbca06928e6ffe28999689
Dowoon <woodw920@gmail.com>
2023-10-31 22:44:34 +0900

    String -> List, List -> String 쉼표 기준으로 변환 메소드 기능 추가

--- src/main/java/utils/TransformUtil.java
+++ src/main/java/utils/TransformUtil.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TransformUtil {
     public String dashes(Integer carRunCnt) {
         StringBuilder dashes = new StringBuilder();
@@ -9,6 +12,16 @@ public class TransformUtil {
         }
 
         return dashes.toString();
+    }
+
+    public String commasToString(List<String> winnerData) {
+        return String.join(",", winnerData);
+    }
+
+    public List<String> commasToList(String carData) {
+        String[] dataArray = carData.split("\\s*,\\s*");
+
+        return Arrays.asList(dataArray);
     }
 
 }
