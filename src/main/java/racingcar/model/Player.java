16d0479f1cf43a5dd9f81f50e287dfe00d34d6c3
김관진 <cozyschooll@gmail.com>
2023-10-31 20:36:31 +0900

    Feat: Player 클래스 생성

---
+++ src/main/java/racingcar/model/Player.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Player {
+    private String name;
+    private int location;
+
+    public Player(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+}
