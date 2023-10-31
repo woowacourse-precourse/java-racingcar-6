859490a4a4718f193f8e5eebc02a2eff4e299c86
Hyunstone <hyun104467@naver.com>
2023-10-31 16:14:00 +0900

    [Feat] 차의 이동 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,7 +6,11 @@ public class Car {
     private String name;
     private int moveAmount;
 
-    public void move() {}
+    public void move() {
+        if (isMoveCondition()) {
+            this.moveAmount++;
+        }
+    }
     public boolean isMoveCondition() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
     }
