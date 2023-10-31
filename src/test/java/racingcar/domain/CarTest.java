3b56d5c8e8a3e94a3678760827e2818d865e9911
chaeshee0908 <chaeshee0908@naver.com>
2023-10-31 21:08:03 +0900

    [fix] 등록된 mock 재등록 에러 해결

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,13 +2,30 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.framework;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import racingcar.util.RandomNumber;
 
 public class CarTest {
+
+    private MockedStatic<RandomNumber> mockedRandomNumber;
+
+    @BeforeEach
+    void setUp() {
+        mockedRandomNumber = mockStatic(RandomNumber.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        mockedRandomNumber.close();
+    }
 
     @Test
     void car_생성자() {
@@ -20,8 +37,7 @@ public class CarTest {
     @Test
     void 랜덤숫자_4미만_수에서_움직이지_않음_확인() {
         // 모의 객체(Mock)을 사용하여 RandomNumber.createRandomNumber()의 결과 조작
-        mockStatic(RandomNumber.class);
-        when(RandomNumber.createRandomNumber()).thenReturn(2);
+        mockedRandomNumber.when(RandomNumber::createRandomNumber).thenReturn(2);
 
         Car car = new Car("pobi");
         car.move();
@@ -32,8 +48,7 @@ public class CarTest {
     @Test
     void 랜덤숫자_4이상_수에서_전진_확인() {
         // 모의 객체(Mock)을 사용하여 RandomNumber.createRandomNumber()의 결과 조작
-        mockStatic(RandomNumber.class);
-        when(RandomNumber.createRandomNumber()).thenReturn(7);
+        mockedRandomNumber.when(RandomNumber::createRandomNumber).thenReturn(7);
 
         Car car = new Car("pobi");
         car.move();
