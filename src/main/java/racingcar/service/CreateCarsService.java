5c0181791481270c9296abc24774adf6981b9390
'sun1234' <'sdfjikl8787@gmail.com'>
2023-10-31 13:12:53 +0900

    refactor(CreateCarsService): 자동차생성기능 스트림을 이용

--- src/main/java/racingcar/service/CreateCarsService.java
+++ src/main/java/racingcar/service/CreateCarsService.java
@@ -1,7 +1,8 @@
 package racingcar.service;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
@@ -10,15 +11,16 @@ public class CreateCarsService {
 
 	public Cars createCars(String inputValue) {
 		String[] carNames = inputValue.split(",");
-		List<Car> carList = new ArrayList<>();
-
-		for (String carName : carNames) {
-			carList.add(createCar(carName));
-		}
-
-		return new Cars(carList);
+		
+		return new Cars(convertStringArrToCarList(carNames));
 	}
-
+	
+	private List<Car> convertStringArrToCarList(String[] strArr){
+		return Arrays.stream(strArr)
+						.map(this::createCar)
+						.collect(Collectors.toList());
+	}
+	
 	private Car createCar(String carName) {
 		return new Car(carName);
 	}
