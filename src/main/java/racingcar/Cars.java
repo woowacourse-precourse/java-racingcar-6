93962149f47e2978f61065976db63c357e682aaf
gdakate <gdakate1215@gmail.com>
2023-10-31 16:34:40 +0900

    Feat: Cars 클래스 개발
    Car들 관리하는 클래스
    - 전체 자동차들 이동시킴
    - 전체 자동차들 중 우승자 추려냄

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    public Cars(List<String> names) {
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public List<CarStatusDto> moveAllCars() {
+        return this.cars.stream()
+                .map(car -> car.move())
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = 0;
+        List<String> winners = new Stack<>();
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                winners.clear();
+                winners.add(car.getName());
+                maxPosition = car.getPosition();
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+
+}
+
+
