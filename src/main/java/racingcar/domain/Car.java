476683f14fc0019f0a8facfec1460d4a7709e227
JIKYUNGHEE <wlrudgml517@gmail.com>
2023-10-31 21:17:07 +0900

    feat: 자동차 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int moveCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
