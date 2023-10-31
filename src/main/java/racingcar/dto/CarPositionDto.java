db7b325800cf92304cb9e2b67c86181e6f5bcdbc
Hanbee Lee <bee1162@naver.com>
2023-10-31 09:12:40 +0900

    feat: Name Output과 Result Output view class 작성

---
+++ src/main/java/racingcar/dto/CarPositionDto.java
@@ -0,0 +1,2 @@
+package racingcar.dto;public class CarPositionDto {
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,35 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import racingcar.dto.CarPositionDto;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String COLON = " : ";
+    private static final String CAR_POSITION = "-";
+    private String output;
+
+    public OutputView(String output) {
+        this.output = output;
+    }
+
+    public void printStatusGuide() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printStatus(List<CarPositionDto> racingStatus) {
+        racingStatus.forEach(this::printOneStatus);
+    }
+
+    private void printOneStatus(CarPositionDto racingStatus) {
+        System.out.println(racingStatus.getCarName());
+        System.out.println(COLON);
+
+        for (int i = 0; i < racingStatus.getStatus(); i++) {
+            System.out.println(CAR_POSITION);
+        }
+        System.out.println();
+    }
+
 }
