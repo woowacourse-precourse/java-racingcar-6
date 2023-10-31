7e1fd6417c5bf66c16c3a027aabb1a6c991931d9
songjayhyun <thewinner1st@naver.com>
2023-10-31 17:21:30 +0900

    feat: 우승자를 가려낼 수 있는 기능 구현
    - judge의 decideWinner 메소드 구현

--- src/main/java/racingcar/Judge.java
+++ src/main/java/racingcar/Judge.java
@@ -2,19 +2,26 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Judge {
-    
+
     private List<Car> cars;
-    
+
     public Judge(List<Car> cars) {
         this.cars = cars;
     }
 
     public List<Car> decideWinner() {
-        for (Car car : cars) {
-            int position = car.getPosition();
-        }
-        return new ArrayList<>();
+        Optional<Integer> maxPoistion = cars.stream()
+                .map(Car::getPosition)
+                .max(Integer::compareTo);
+
+        List<Car> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPoistion.get())
+                .toList();
+
+        return winners;
     }
 }
