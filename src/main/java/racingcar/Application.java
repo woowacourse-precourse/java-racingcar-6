467032219ebc27008797b69a2dad082b386d05ab
jifrozen0110 <answldjs1836@gmail.com>
2023-11-01 00:46:22 +0900

    feat(Application) : 레이싱 게임 컨트롤러 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.RacingCars;
+import racingcar.domain.RandomNumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCars racingCars = new RacingCars(numberGenerator());
+        RacingGameController racingGameController = new RacingGameController(racingCars);
+        racingGameController.start();
+    }
+
+    private static NumberGenerator numberGenerator() {
+        return new RandomNumberGenerator();
     }
 }
