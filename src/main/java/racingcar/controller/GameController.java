9afc7935dfb4281df206f9015d51d7f45424daf6
Infikei <hckhong@naver.com>
2023-11-01 01:41:38 +0900

    refactor: 게임 진행 단계별로 메서드 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -8,21 +8,34 @@ import racingcar.view.OutputView;
 import java.util.List;
 
 public class GameController {
-    public void playGame() {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        RacingCarsValidator racingCarsValidator = new RacingCarsValidator();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    RacingCarsValidator racingCarsValidator = new RacingCarsValidator();
+    RacingCars racingCars;
+    MovingNumber movingNumber;
 
+    public void playGame() {
+        initGame();
+        proceedGame();
+        terminateGame();
+    }
+
+    private void initGame() {
         List<String> racingCarNames = racingCarsValidator.validateNames(inputView.inputRacingCarNames());
-        RacingCars racingCars = RacingCars.createRacingCars(racingCarNames);
-        MovingNumber movingNumber = MovingNumber.createMovingNumber(inputView.inputMovingNumber());
+        racingCars = RacingCars.createRacingCars(racingCarNames);
+        movingNumber = MovingNumber.createMovingNumber(inputView.inputMovingNumber());
+    }
+
+    private void proceedGame() {
         outputView.printMessageStartResult();
 
         for (int i = 0; i < movingNumber.getNumber(); i++) {
             racingCars.tryMoveCars();
             outputView.printResultsMoving(racingCars);
         }
+    }
 
+    private void terminateGame() {
         outputView.printWinners(racingCars.getWinners());
     }
 }
