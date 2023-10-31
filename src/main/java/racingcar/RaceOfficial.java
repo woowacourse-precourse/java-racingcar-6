1757e659283e23346b2a8719b26d1d04a7d981a3
ParkJinYong <mej1406@gmail.com>
2023-10-31 21:43:57 +0900

    feat: 최대거리 탐색, 우승자 목록 탐색 기능 구현

--- src/main/java/racingcar/RaceOfficial.java
+++ src/main/java/racingcar/RaceOfficial.java
@@ -1,21 +1,23 @@
 package racingcar;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RaceOfficial {
-    private List<String> winners = new ArrayList<>();
-    private int longestDistance = 0;
-
-    public int findLongestDistance() {
-        return -1;
+    public int findLongestDistance(List<Car> cars) {
+        return (int) cars.stream()
+                .map(Car::getDistance)
+                .sorted(Comparator.reverseOrder())
+                .findFirst()
+                .orElse(0);
     }
 
-    public void findWinners(List<Car> cars) {
+    public List<Car> findWinners(List<Car> cars) {
+        int longestDistance = findLongestDistance(cars);
 
-    }
-
-    public List<String> getWinners() {
-        return winners;
+        return cars.stream()
+                .filter(car -> car.getDistance() == longestDistance)
+                .collect(Collectors.toList());
     }
 }
