6da66d3ff7a5fe2b4cd3097815588dc767aaecce
JeonJW24 <tyui153@naver.com>
2023-11-01 06:02:24 +0900

    fix(racingcar): RaceCarTest 클래스 오류 수정

--- src/test/java/racingcar/domain/RaceCarTest.java
+++ src/test/java/racingcar/domain/RaceCarTest.java
@@ -3,21 +3,20 @@ package racingcar.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.Arrays;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RaceCarTest {
-    private static RaceCar raceCar;
+    private RaceCar raceCar;
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void setUp() {
         raceCar = new RaceCar(Arrays.asList("Jeon", "Jeong", "Won"));
     }
 
     @Test
     void 전진() {
+        System.out.println("raceCar.names = " + raceCar.names);
         raceCar.forward(Arrays.asList(1, 2, 4));
         assertThat(raceCar.moveDistance).isEqualTo(Arrays.asList("", "", "-"));
     }
@@ -32,10 +31,5 @@ class RaceCarTest {
     void 공동_우승() {
         raceCar.moveDistance = Arrays.asList("-", "---", "---");
         assertThat(raceCar.winner()).isEqualTo("Jeong, Won");
-    }
-
-    @AfterEach
-    void tearDown() {
-        raceCar.moveDistance = Arrays.asList("", "", "");
     }
 }
