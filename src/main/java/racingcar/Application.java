ee2660e1dfe4723793445f771c167c2776db68b0
hajungIm <ihj_ewha@ewhain.net>
2023-10-31 17:48:15 +0900

    Feat(InputView): 시도 횟수 입력 기능 구현
    시도 횟수를 입력받는 `inputTurn` 메소드 작성
    
    Application.main() 에서 해당 메소드를 이용하여
    int turn 입력 받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,6 @@ import racingcar.view.InputView;
 public class Application {
     public static void main(String[] args) {
         List<String> cars = InputView.inputCars();
+        int turn = InputView.inputTurn();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,3 +9,10 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Arrays.stream(Console.readLine().split(",")).toList();
     }
+
+    public static int inputTurn() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int turn = Integer.parseInt(Console.readLine());
+        return turn;
+    }
+}
