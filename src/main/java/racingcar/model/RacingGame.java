53daef020061fcea5265391fbd2eaa3c3a1fd473
Jiyoung Choi <12201817@inha.edu>
2023-10-31 14:13:46 +0900

    refactor(RacingGame) : 테스트를 위한 함수 삭제

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -26,10 +26,6 @@ public class RacingGame {
         return WinnerCalculator.getWinners(cars);
     }
 
-    private List<Car> getCars() {
-        return cars;
-    }
-
     private void initCars(List<String> carNames) {
         for (String carName : carNames) {
             cars.add(new Car(carName));
