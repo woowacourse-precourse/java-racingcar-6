d150cba3d0c9a8a33b43afd172f816f7e56001da
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-11-01 03:12:29 +0900

    fix: winners리스트 파라미터로 넘기던 문제 해결

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class RacingGame {
     private final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -26,19 +27,35 @@ public class RacingGame {
         validateCarName();
 
         System.out.println(INPUT_TRIALS_MESSAGE);
-        printResultMessage(readInputByInt());
+        String trial = readInput();
 
-        findWinners(winners);
+        validateTrial(trial);
 
-        printWinners(winners);
+        printResultMessage(Integer.parseInt(trial));
+
+        findWinners();
+
+        printWinners();
     }
 
     private String readInput() {
         return Console.readLine().trim();
     }
 
-    private int readInputByInt() {
-        return Integer.parseInt(readInput());
+    private void validateCarName() {
+        if (cars.isEmpty())
+            throw new IllegalArgumentException();
+
+        for (Car car : cars) {
+            int lengthOfCarName = car.getLengthOfName();
+            if (lengthOfCarName > MAX_CAR_NAME_LENGTH || lengthOfCarName < MIN_CAR_NAME_LENGTH)
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateTrial(String trial) {
+        if (!Pattern.matches("^[0-9]*$", trial))
+            throw new IllegalArgumentException();
     }
 
     private void splitInputToCarNames(String input) {
@@ -51,17 +68,6 @@ public class RacingGame {
 
                     cars.add(new Car(car));
                 });
-    }
-
-    private void validateCarName() {
-            if (cars.isEmpty())
-                throw new IllegalArgumentException();
-
-        for (Car car : cars) {
-            int lengthOfCarName = car.getLengthOfName();
-            if (lengthOfCarName > MAX_CAR_NAME_LENGTH || lengthOfCarName < MIN_CAR_NAME_LENGTH)
-                throw new IllegalArgumentException();
-        }
     }
 
     private void printResultMessage(int trial) {
@@ -81,7 +87,7 @@ public class RacingGame {
         System.out.printf("%s : %s\n", car.getName(), car.getPosition());
     }
 
-    private void findWinners(List<Car> winners) {
+    private void findWinners() {
         int maxDistance = 0;
         for (int i = 0; i < cars.size(); i++) {
             Car car = cars.get(i);
@@ -94,12 +100,13 @@ public class RacingGame {
         }
     }
 
-    private void printWinners(List<Car> winners) {
+    private void printWinners() {
         StringBuilder result = new StringBuilder();
         winners.stream()
                 .iterator()
                 .forEachRemaining(car -> result.append(car.getName()).append(", "));
-        result.delete(result.length() - 2, result.length());
+        if (winners.size() > 1)
+            result.delete(result.length() - 2, result.length());
         System.out.printf("%s%s", PRINT_FINAL_WINNER, result);
     }
 }
