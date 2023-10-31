dba88f3246d902d5a25ac726988058c69364dd1a
yhcho0405 <yhcho0405@kakao.com>
2023-10-31 23:21:58 +0900

    feat: 자동차 이름에 대한 예외처리(공백, 특수문자)

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Car {
     private final String name;
@@ -32,6 +34,11 @@ public class Car {
         if (name.trim().length() >= 5) {
             throw new IllegalArgumentException("자동차 이름은 5글자 이하 여야 합니다.");
         }
+
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9가-힣]*");
+        Matcher matcher = pattern.matcher(name);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("자동차 이름은 영문자, 숫자, 한글만 포함할 수 있습니다.");
     }
 
     private boolean shouldMove() {
