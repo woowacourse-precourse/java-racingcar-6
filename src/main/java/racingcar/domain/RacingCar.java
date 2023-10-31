27a235a7aec49f74a63c9903bced5e417ab9e53a
Jaeyoung Lim <limjyjustin@gmail.com>
2023-10-31 11:17:51 +0900

    feat(RacingCar): 기능 메서드 구현
    move, display 메서드 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,15 +1,27 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
 
     private String name;
     private int place;
 
-    public RacingCar(String name, int place) {
+    public RacingCar(String name) {
         this.name = name;
         this.place = 0;
     }
 
-    // move 메서드
-    // display 메서드
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4)
+            place += 1;
+    }
+
+    public void display() {
+        System.out.print(name + " : ");
+
+        for(int i = 0; i < place; i++) {
+            System.out.print("-");
+        }
+    }
 }
---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarTest {
+
+    private RacingCar car;
+
+    @BeforeEach
+    void setUp() {
+        car = new RacingCar("justin");
+    }
+
+    @Test
+    void 무작위_값에_따라_이동하는지_확인() {
+        car.move();
+    }
+
+    @Test
+    void 실행_결과_제대로_출력되는지_확인() {
+        car.move();
+        car.move();
+        car.move();
+        car.move();
+        car.move();
+        car.display();
+    }
+}
