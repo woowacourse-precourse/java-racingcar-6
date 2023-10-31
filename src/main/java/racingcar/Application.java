8e0c4a0f8da3cdf2dff38fbd85fca14a0cf1adc3
mango606 <yuj08184@kangwon.ac.kr>
2023-10-31 22:05:47 +0900

    feat(Car): add method to check car name length

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -87,7 +87,14 @@ class Car {
     private int position;
 
     public Car(String name) {
+            validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public void moveForward() {
