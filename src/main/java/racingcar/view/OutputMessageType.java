6206fb62042286a09c4fef04abbecb82166ff87a
상원 최 <swchoi1@swm.ai>
2023-10-31 23:29:38 +0900

    feat(객체 생성) : OutputView 메세지를 담은 Enum 생성
    Output시 출력되는 메세지를 담은 Enum 생성

---
+++ src/main/java/racingcar/view/OutputMessageType.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+public enum OutputMessageType implements MessageType {
+    RESULT_MESSAGE("실행 결과"),
+    FINAL_RESULT_MESSAGE("최종 우승자"),
+    PROGRESS_EMOJI("-"),
+    SEPARATOR(" : "),
+    COMMAS(", ")
+    ;
+
+
+    private final String message;
+
+    OutputMessageType(final String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
