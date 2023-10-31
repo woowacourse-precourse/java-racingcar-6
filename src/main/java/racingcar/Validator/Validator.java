cfc1683ecd1887b4bbbcc7c1994202bb96838725
jookbooin <3670lsh@naver.com>
2023-11-01 04:50:55 +0900

    refactor: validateNotEndsWithComma() inputView 내부로 이동

--- src/main/java/racingcar/Validator/Validator.java
+++ src/main/java/racingcar/Validator/Validator.java
@@ -8,14 +8,4 @@ public class Validator {
         }
     }
 
-    public static void validateNotEndsWithComma(String input) {
-        int namesLength = input.length();
-        if (isEndWithComma(input, namesLength)) {
-            throw new IllegalArgumentException(",로 끝나면 안됩니다.");
-        }
-    }
-
-    private static boolean isEndWithComma(String input, int inputLength) {
-        return input.charAt(inputLength - 1) == ',';
-    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,6 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import static racingcar.Validator.Validator.validateNotBlank;
-import static racingcar.Validator.Validator.validateNotEndsWithComma;
-
 
 public class InputView {
 
@@ -16,6 +14,17 @@ public class InputView {
         validateNotBlank(input);
         validateNotEndsWithComma(input);
         return input;
+    }
+
+    private void validateNotEndsWithComma(String input) {
+        int namesLength = input.length();
+        if (isEndWithComma(input, namesLength)) {
+            throw new IllegalArgumentException(",로 끝나면 안됩니다.");
+        }
+    }
+
+    private boolean isEndWithComma(String input, int inputLength) {
+        return input.charAt(inputLength - 1) == ',';
     }
 
     public int getNumberOfRounds() {
