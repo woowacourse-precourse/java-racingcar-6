907537239fd8388d51035909b4450b02d94a1136
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-01 01:27:18 +0900

    fix(Constants) : 자동차 이름에 숫자도 넣을 수 있게 수정

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final int MAX_RANDOM_NUM = 9;
     public static final int START_NUM = 4;
     public static final int NAME_SIZE = 5;
-    public static final String NAME_PATTERN = "[a-zA-Z]+";
+    public static final String NAME_PATTERN = "[a-zA-Z0-9]+";
 
     public static final String NAME_SIZE_ERROR = "자동차 이름은 1자 이상 5자 이하의 알파벳 문자여야 합니다.";
     public static final String ROUND_SIZE_ERROR = "시도 횟수는 1 이상 이어야 합니다.";
