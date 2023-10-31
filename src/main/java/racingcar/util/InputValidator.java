cc8c70ee51da4c459cbb51af45fd56a028941a44
pwrwpw <dlgustjr9912@gmail.com>
2023-10-31 22:26:08 +0900

    feat: Integer로 변환이 가능한 입력인지 확인하는 메서드 추가

---
+++ src/main/java/racingcar/util/InputValidator.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+public class InputValidator {
+
+    public static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
