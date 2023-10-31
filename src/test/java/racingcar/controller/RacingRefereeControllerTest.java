4fe49cce662233968e7996d6827e674959bf8f23
youngkyunIm <yee0230@naver.com>
2023-10-31 21:09:07 +0900

    test(controller) : 정상 작동 테스트

--- src/test/java/racingcar/controller/RacingRefereeControllerTest.java
+++ src/test/java/racingcar/controller/RacingRefereeControllerTest.java
@@ -1,10 +1,42 @@
 package racingcar.controller;
 
-import org.junit.jupiter.api.Test;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-class RacingRefereeControllerTest {
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+class RacingRefereeControllerTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
-    void normal_test() {
+    void 전진_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "5");
+                    assertThat(output()).contains(
+                            "pobi : -", "woni : ", "jun : ",
+                            "pobi : --", "woni : -", "jun : --",
+                            "pobi : ---", "woni : --", "jun : ---",
+                            "pobi : ----", "woni : ---", "jun : ----",
+                            "pobi : -----", "woni : ----", "jun : -----",
+                            "최종 우승자 : pobi, jun");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+                );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
