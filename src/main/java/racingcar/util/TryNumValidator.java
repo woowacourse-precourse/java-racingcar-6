1400c9ce4a2da32cfa23b0722b13abdfaa806189
박성춘 <songchon100@gmail.com>
2023-11-01 01:27:42 +0900

    feat : 이동 횟수가 10인 경우 유효성 검사 구현

--- src/main/java/racingcar/util/TryNumValidator.java
+++ src/main/java/racingcar/util/TryNumValidator.java
@@ -6,6 +6,7 @@ public class TryNumValidator extends Validator{
     public void validate(String tryNum) {
         validateEmptyInput(tryNum);
         validateTryNumIsNum(tryNum);
+        validateTryNumIsZero(tryNum);
     }
 
     // 이동 횟수가 수가 아닌 문자인 경우
@@ -15,5 +16,10 @@ public class TryNumValidator extends Validator{
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ErrorMessage.TRY_NUM_NOT_NUMBER_ERROR.getErrorMessage());
         }
+    }
+
+    // 이동 횟수가 0이 입력되었을 경우
+    private void validateTryNumIsZero(String tryNum) {
+        if(tryNum.equals("0")) throw new IllegalArgumentException(ErrorMessage.TRY_NUM_IS_ZERO_ERROR.getErrorMessage());
     }
 }
