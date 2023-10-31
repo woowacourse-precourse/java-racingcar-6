e08bc03bed10d78ab6cafc50fa0ba620e76e5d8f
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-31 13:56:12 +0900

    refactor: 예외 안내 메세지 변경

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Validator {
     public static void nameLengthCheck(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("잘못된 입력");
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
         }
     }
 
@@ -15,19 +15,19 @@ public class Validator {
         int initLength = names.size();
         HashSet<String> deduplicationSet = new HashSet<>(names);
         if (initLength != deduplicationSet.size()) {
-            throw new IllegalArgumentException("중복되지 않은 이름 입력");
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 
     public static void blankCheck(String input) {
         if (Objects.equals(input, "")) {
-            throw new IllegalArgumentException("공백 입력");
+            throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
     public static void countZeroCheck(String input) {
         if (input.length() == 1 && input.charAt(0) == '0') {
-            throw new IllegalArgumentException("0이 아닌 정수 입력");
+            throw new IllegalArgumentException("0이 아닌 양의 정수 입력해주세요.");
         }
     }
 
@@ -35,7 +35,7 @@ public class Validator {
         for (int i = 0; i < input.length(); i++) {
             char inputChar = input.charAt(i);
             if (!('0' <= inputChar && inputChar <= '9')) {
-                throw new IllegalArgumentException("잘못된 값 입력");
+                throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
             }
         }
     }
