b63421383ce9faba57d1e28bd3bfd1be0089ce61
Kyoungtae <rudxo0525@gmail.com>
2023-10-31 18:03:07 +0900

    feat: car 객체 생성 메소드 makeCar() 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,30 @@
 package racingcar;
+
 import java.util.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    public static Car[] car;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         start();
+
     }
 
     public static void start(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void makeCar(){
+        ArrayList<String> names = getName();
+        int count = getCount();
+        car = new Car[names.size()];
+
+        for(int i = 0; i < names.size(); i++){
+            car[i] = new Car(names.get(i), count);
+        }
+
     }
 
     public static ArrayList<String> getName(){
