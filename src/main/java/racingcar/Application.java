a026b8fdce972dedb5217c86a1d416cc2afc4c65
jinsoo Kim <jinsoo335@naver.com>
2023-10-31 14:53:23 +0900

    [추가/수정] 자동차 이름 입력 기능 생성, 기능 목록 수정
    - 입력 시 쉼표 이후에 들어오는 공백과 같이 이름의 앞 뒤에 존재하는 공백은 제거해서 이름으로 간주라도록 기능 목록의 내용 수정
    
    - 입력 기능은 이름을 입력 받고, 검증하는 과정으로 이뤄진다.
    - 정상적인 이름이라면, Map에 이름을 저장한다.
    - 중복, 형식 오류의 문제가 있다면, IllegalArgumentException 예외 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,49 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Map<String, Integer> racer = new LinkedHashMap<>();
+
+
+        inputRacerName(racer);
+
+
+
+
+    }
+
+
+    private static void inputRacerName(Map<String, Integer> racer) {
+
+        String input;
+        input = Console.readLine();
+
+        for (String name : input.split(",")) {
+            // 이름의 앞 뒤에 들어오는 공백은 지운다. -> 쉼표 이후 공백일 가능성이 존재하기 때문
+            nameValidetor(name.trim(), racer);
+            racer.put(name.trim(), 0);
+        }
+    }
+
+
+    /**
+     * 이름이 양식에 맞게 들어왔는지, 중복된 이름이 들어왔는지 검사
+     */
+    private static void nameValidetor(String name, Map<String, Integer> racer) {
+
+        if(name.length() >= 5 || name.length() == 0){
+            throw new IllegalArgumentException("이름은 5자 이하만 가능, 0자는 불가");
+        }
+
+        if(racer.get(name) != null){
+            throw new IllegalArgumentException("중복된 이름이 들어옴");
+        }
     }
 }
