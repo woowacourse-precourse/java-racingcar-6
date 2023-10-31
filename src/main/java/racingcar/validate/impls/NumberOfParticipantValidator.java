f1794b223db30f20331f9ce5fcf525173b7b6ce8
minjong kim <godminjong@naver.com>
2023-10-31 17:22:18 +0900

    feat: 입력받는 라운드 수에 대한 검증을 하는 검증기능 Class 추가

---
+++ src/main/java/racingcar/validate/impls/NumberOfParticipantValidator.java
@@ -0,0 +1,13 @@
+package racingcar.validate.impls;
+
+import racingcar.validate.Validator;
+
+public class NumberOfParticipantValidator implements Validator {
+
+    @Override
+    public void validate(Object obj) {
+    }
+
+
+    
+}
