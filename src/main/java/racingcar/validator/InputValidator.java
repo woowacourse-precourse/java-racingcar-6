59b254c46de138e7089b52d179344fc1ea190df0
Kim mingyu <minq0302@naver.com>
2023-10-31 21:44:46 +0900

    feat: add trial number validator
    A validator that judges whether input number consists of only digits.

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -9,5 +9,17 @@ public class InputValidator {
         }
     }
 
+    //12. 시도할 횟수를 입력할 때 입력한 값이 숫자로만 이루어져 있는지 검사하는 메서드
+    public static void validateTrialNumber(String inputNum) {
+        for(int index=0; index<inputNum.length(); index++) {
+            if (isInputNotDigit(inputNum, index)) {
+                throw new IllegalArgumentException("시도할 횟수는 숫자만 입력해야 합니다.");
+            }
+        }
+    }
+
+    private static boolean isInputNotDigit(String inputNum, int index) {
+        return !Character.isDigit(inputNum.charAt(index));
+    }
 
 }
