36b1bc4cd09fef44800181d1de22df7050904b16
ghdeo <goldencorn7@naver.com>
2023-10-31 23:36:50 +0900

    feat: Add Cars methods

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Cars {
     List<Car> cars = new ArrayList<>();
@@ -11,4 +12,28 @@ public class Cars {
         }
     }
 
+    public void race() {
+        cars.forEach(car -> {
+            car.move();
+            car.printPosition();
+        });
+        System.out.println();
+    }
+
+    public List<Car> getWinners() {
+        List<Car> winners = cars.stream()
+                .collect(Collectors.groupingBy(Car::getPosition, Collectors.toList()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingInt(entry -> entry.getKey()))
+                .map(entry -> entry.getValue())
+                .orElseThrow();
+        return winners;
+    }
+
+    public void printWinners() {
+        List<Car> winners = getWinners();
+        String collect = winners.stream().map(Car::getName).collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + collect);
+    }
 }
