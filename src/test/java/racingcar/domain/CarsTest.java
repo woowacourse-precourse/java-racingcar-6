23d20555158a3d71d4fddcaf5a057968d39e8ba1
ohjuyoungg <ejrm12@naver.com>
2023-10-31 15:01:41 +0900

    test(CarsTest) : 우승 자동차에 대한 테스트 추가
    우승자 검증에 실패한다 변경 필요

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,2 +1,33 @@
-package racingcar.domain;public class CarsTest {
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CarsTest {
+
+    private List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = List.of(new Car("Car1"), new Car("Car2"), new Car("Car3"));
+    }
+
+    @Test
+    void getWinners_우승자_검증에_성공한다() {
+        assertDoesNotThrow(() -> Cars.getWinners(cars, 3));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1, 2", "3, -1", "-1, -1"})
+    void testSomeMethod(int maxPosition, int numCars) {
+        assertThrows(IllegalArgumentException.class, () -> Cars.getWinners(cars, numCars));
+    }
 }
+
+
