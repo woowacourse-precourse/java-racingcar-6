1f267372fc6d2e6ac232a93f49f2256839b070d7
HYOAN <aspqll@naver.com>
2023-10-31 19:55:48 +0900

    feat(racingcar): 각 자동차마다 무작위 값을 생성하고 그 값으로 전진 여부를 판단 후 결과를 해당 자동차 객체에 저장하는 기능 구현

--- src/main/java/racingcar/domain/CarMovement.java
+++ src/main/java/racingcar/domain/CarMovement.java
@@ -2,10 +2,26 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
 public class CarMovement {
     public CarMovement() {
     }
+
     public boolean getRandomNumber() {
-       return false;
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber > 4;
+    }
+
+    public void movement(List<Car> carList) {
+        for (int carNumber = 0; carNumber < carList.size(); carNumber++) {
+            if (getRandomNumber()) forward(carList, carNumber);
+        }
+    }
+
+    public void forward(List<Car> carList, int carNumber) {
+        String location = carList.get(carNumber).getLocation();
+        location += "-";
+        carList.get(carNumber).setLocation(location);
     }
 }
--- src/main/java/racingcar/domain/GameController.java
+++ src/main/java/racingcar/domain/GameController.java
@@ -7,6 +7,7 @@ public class GameController {
     private List<Car> carList = new ArrayList<>();
     private UserInput userInput = new UserInput();
     private List<String> carNames = new ArrayList<>();
+    private int attemptCount;
 
     public GameController() {
     }
@@ -15,16 +16,24 @@ public class GameController {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = userInput.input();
         updateName(names);
+        System.out.println("시도할 회수는 몇회인가요?");
+        String count = userInput.input();
+        updatePosition(count);
     }
 
     public void updateName(String names) {
         carNames = userInput.carName(names);
-        for (int i = 0; i < carNames.size(); i++) {
-            Car car = new Car(carNames.get(i));
+        for (int carNumber = 0; carNumber < carNames.size(); carNumber++) {
+            Car car = new Car(carNames.get(carNumber));
             carList.add(car);
         }
     }
 
-    public void updatePosition() {
+    public void updatePosition(String strCount) {
+        int attemptCount = userInput.attemptCount(strCount);
+        CarMovement carMovement = new CarMovement();
+        for (int count = 0; count < attemptCount; count++) {
+            carMovement.movement(carList);
+        }
     }
 }
