4abe83fca1a5971eed0c796d51c2f4e8fbc254c5
IE-MangChi <hsm983278@gmail.com>
2023-10-31 16:53:41 +0900

    feat: 전진가능여부 판단기능 추가

--- src/main/java/racingcar/util/CarUtil.java
+++ src/main/java/racingcar/util/CarUtil.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarUtil {
 
+    private static final int MORE_THAN_NUMBER = 4;
     private static final int START_RANDOM_NUMBER_RANGE = 0;
     private static final int FINISH_RANDOM_NUMBER_RANGE = 9;
 
-    public static int getRandomValue() {
+    public static boolean isCanMove() {
+        return CarUtil.getRandomValue() >= MORE_THAN_NUMBER;
+    }
+
+    private static int getRandomValue() {
         return Randoms.pickNumberInRange(START_RANDOM_NUMBER_RANGE, FINISH_RANDOM_NUMBER_RANGE);
     }
 }
