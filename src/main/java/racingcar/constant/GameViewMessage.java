4ec879cc56607cfa0dde778364407e5d82c2a88f
DDing77 <devseo2021@gmail.com>
2023-10-31 21:35:08 +0900

    feat: 게임 메세지 통합 관리 클래스 구현

---
+++ src/main/java/racingcar/constant/GameViewMessage.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class GameViewMessage {
+    public static final String MESSAGE_INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String MESSAGE_INPUT_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String MESSAGE_PROGRESS_DISTANCE_CHAR = "-";
+    public static final String MESSAGE_SPLIT_CHAR = " : ";
+    public static final String MESSAGE_RESULT = "\n실행 결과";
+    public static final String MESSAGE_RESULT_CHAMPION = "최종 우승자";
+}
