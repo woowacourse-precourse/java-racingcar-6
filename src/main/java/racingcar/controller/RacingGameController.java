8bd3c11e69207646cda5ae3228ef4fa7ec01752e
ChoiseU <csw980928@gmail.com>
2023-11-01 00:43:00 +0900

    feat : 입력 받은 값을 RacingDto로 convert 하는 메서드 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -17,6 +17,7 @@ public class RacingGameController {
     private String inputCarListMsg = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private String inputGameCntMsg = "시도할 회수는 몇회인가요?";
     private int checkLen = 5;
+    private RacingDto racingDto;
 
 
     public void game() {
@@ -26,6 +27,7 @@ public class RacingGameController {
     public void ready() {
         List<String> carList = getInputCarListAndCheckName();
         int gameCnt = getInputGameCntAndCheckGameCnt();
+        racingDto = convertRacingDto(carList, gameCnt);
 
     }
 
@@ -46,5 +48,20 @@ public class RacingGameController {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(gameCnt);
+    }
+
+    public RacingDto convertRacingDto(List<String> carList, int gameCnt) {
+        RacingDto racingDto = new RacingDto();
+        Map<String, String> carMap = new HashMap<>();
+        AtomicInteger dupleIndex = new AtomicInteger(1);
+
+        carList.forEach(car -> {
+            carMap.put(car + dupleIndex.getAndIncrement(), "");
+        });
+
+        racingDto.setRacingCar(carMap);
+        racingDto.setGameCnt(gameCnt);
+
+        return racingDto;
     }
 }
