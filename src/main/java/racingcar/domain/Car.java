c2f99016b2cf5e7f9f2fab2da77e4e8797b0f065
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-31 21:53:03 +0900

    Feat[domain] 자동차 도메인 이름 형식 검증

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,13 @@
 package racingcar.domain;
 
+import racingcar.validation.CarNameValidator;
+
 public class Car {
     private final String name;
     private int position;
 
     public Car(String name){
+        CarNameValidator.checkValidLength(name);
         this.name = name;
         this.position = 0;
     }
