72591c1fd0bdbe4b769a1c45b4e70a47b363c31d
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 13:06:37 +0900

    refactor (Car): 매직 넘버 상수 변환
    Car 클래스의 매직 넘버를 상수로 변환하였습니다:
    - START_NUMBER = 0
    - END_NUMBER = 9
    - GO_NUMBER = 4

--- src/main/java/service/Car.java
+++ src/main/java/service/Car.java
@@ -3,6 +3,10 @@ package service;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car{
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+    private static final int GO_NUMBER = 4;
     private final String name;
     private int distance = 0;
 
@@ -11,7 +15,7 @@ public class Car{
     }
 
     public void go(){
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(START_NUMBER, END_NUMBER) >= GO_NUMBER) {
             distance++;
         }
     }
