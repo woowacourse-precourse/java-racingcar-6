7445f0a661944a0b671763ab9c566fee346f96a4
InJun2 <os2864@naver.com>
2023-10-31 14:46:42 +0900

    :sparkles: 입력값이 없을 경우 예외처리 추가

--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -1,6 +1,8 @@
 package racingcar.exception;
 
 public enum ExceptionMessage {
+    EMPTY_NAME("이름이 존재하지 않는 레이싱카가 존재합니다"),
+    EMPTY_TRY_COUNT("시도 횟수가 입력되지 않았습니다"),
     CAR_NAME_MAX_LENGTH_EXCEEDED("레이싱카의 이름 입력의 최대 길이를 초과하셨습니다"),
     INVALID_INTEGER_FORMAT("입력된 문자가 정수로 변환되지 않습니다");
 
