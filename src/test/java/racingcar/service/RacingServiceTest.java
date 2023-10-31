24183c46f93befe60eec37836c6e00fc37f96872
Hyun Woo <hwsa10041@gmail.com>
2023-10-31 16:29:18 +0900

    test : findByVictoryPlayers() 테스트 작성

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -55,4 +55,26 @@ class RacingServiceTest {
         cars.stream().filter(car -> result.get(car.getName()) == maxScore)
                 .forEach(car -> assertThat(car.getWinCount()).isEqualTo(1));
     }
+
+    @DisplayName("자동차의 우승 횟수를 판단해 최종 우승자 List를 생성한다.")
+    @Test
+    void findByVictoryPlayers() {
+        // given
+        Car pobi = Car.createCar("pobi");
+        Car woni = Car.createCar("woni");
+        Car jun = Car.createCar("jun");
+
+        List<Car> cars = new ArrayList<>(List.of(pobi, woni, jun));
+        cars.get(0).plusWinCount();
+        cars.get(2).plusWinCount();
+
+        // when
+        List<Car> victoryPlayers =
+                racingService.findByVictoryPlayers(cars);
+
+        // then
+        assertThat(victoryPlayers)
+                .extracting("name")
+                .contains("pobi", "jun");
+    }
 }
