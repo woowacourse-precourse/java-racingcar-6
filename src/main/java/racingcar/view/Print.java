52208db1219cd195d312893e7262190d49dfda72
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:10:01 +0900

    Feat: 게임 결과에 따른 출력문 추가
    - 게임 결과를 보여주는 출력 메서드
    - 최종 우승자를 보여주는 출력 메서드

--- src/main/java/racingcar/view/Print.java
+++ src/main/java/racingcar/view/Print.java
@@ -1,9 +1,24 @@
 package racingcar.view;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import racingcar.domain.Car;
 import racingcar.view.constant.ConstPrint;
 
 public class Print {
-    public static void suggestCarName(ConstPrint constPrint){
+    public static void showUser(ConstPrint constPrint){
         System.out.println(constPrint.getConstPrint());
+    }
+
+    public static void showGameResult(List<Car>parkingLot){
+        for(int i = 0; i< parkingLot.size();i++){
+            System.out.println(parkingLot.get(i).getCarName() + " : " + parkingLot.get(i).getDistance());
+        }
+        System.out.println();
+    }
+
+    public static void showGameWinner(ConstPrint constPrint, List<String>winners){
+        System.out.println(constPrint.getConstPrint() + " : " + String.join(", ",winners));
     }
 }
