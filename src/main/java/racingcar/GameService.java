598bb163f913696e327063927b9c27907c9d8f71
Sinorang <aodkrak@naver.com>
2023-11-01 04:49:19 +0900

    feat. 자동차 이름을 이용하여 자동차 객체 생성
    - 사용자로부터 입력받은 자동차 이름을 이용하여 자동차 객체를 생성하는 기능 구현

--- src/main/java/racingcar/GameService.java
+++ src/main/java/racingcar/GameService.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 public class GameService {
 
-    private List<String> racingCarList = new ArrayList<>();
+    private List<String> racingCarNames = new ArrayList<>();
+    private ArrayList<RacingCar> racingCarList = new ArrayList<>();
     private int tryCount;
     private ResultView view = new ResultView();
 
     private void setUpGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        racingCarList = splitCarNames(Console.readLine());
+        racingCarNames = splitCarNames(Console.readLine());
         System.out.print("시도할 회수는 몇회인가요?");
         tryCount = Integer.parseInt(Console.readLine());
         System.out.println("");
@@ -23,4 +24,12 @@ public class GameService {
     public List<String> splitCarNames(String input) {
         return new ArrayList<>(Arrays.asList(input.split(",")));
     }
+
+    private void addRacingCar() {
+        for(int i=0; i<racingCarNames.size(); i++) {
+            racingCarList.add(new RacingCar(racingCarNames.get(i)));
+        }
+    }
+
+
 }
