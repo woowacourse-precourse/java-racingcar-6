cc5e9cb234f73089c053451e39b702833cb2f019
GaHee99 <u1879@sookmyung.ac.kr>
2023-10-31 10:47:29 +0900

    refactor(OutputView): roundResult 리팩토링

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,6 +8,7 @@ import racingcar.util.Constants;
 public class OutputView {
     public static void printCarResult(List<String> result) {
         result.forEach(System.out::println);
+        System.out.println();
     }
 
     public static void printResultConstant() {
