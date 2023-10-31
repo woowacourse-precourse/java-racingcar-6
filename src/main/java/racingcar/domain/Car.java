22c4551e1fde5b6fd69a1e1b5b1c2da31caf886c
Hyoseop Song <crad_on25@naver.com>
2023-11-01 04:54:19 +0900

    feature(Car): move, Random 반환 메소드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MOVE_CONDITION = 4;
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     public Car(String name) {
         if (name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH) {
@@ -13,11 +15,21 @@ public class Car {
         this.name = name;
     }
 
+    public void move() {
+        if (makeRandomNumber() >= MOVE_CONDITION) {
+            position++;
+        }
+    }
+
     public String getName() {
         return name;
     }
 
     public int getPosition() {
         return position;
+    }
+
+    private int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
