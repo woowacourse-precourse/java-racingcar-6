8ffed5cf373c09bff6e4488dd51097932c419b92
jinchiim <nellroll@syuin.ac.kr>
2023-11-01 04:38:25 +0900

    refactor: Application 수정
    Application 수정
    - 의존성 추가
    - 처음부터 게임을 출력하고 시작하도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,20 @@
 package racingcar;
 
-import racingcar.controller.GameController;
+import static racingcar.view.input.Input.inputNames;
+import static racingcar.view.output.Output.printMessage;
+
+import racingcar.controller.CarController;
+import racingcar.domain.car.Cars;
+import racingcar.view.output.OutputMessage;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        GameController gameController = new GameController();
-        gameController.startCarGame();
+        printMessage(OutputMessage.PLEASE_ENTER_CAR_NAME);
+        Cars cars = new Cars(inputNames());
+
+        CarController carController = new CarController(cars);
+        carController.run();
     }
 }
