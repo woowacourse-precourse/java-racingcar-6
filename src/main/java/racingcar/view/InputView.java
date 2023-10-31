f83ddb1315960e48fd297ba3f8b24c1277f0daa6
twoosky <lhjae3@naver.com>
2023-10-31 19:48:26 +0900

    fix: 숫자 변환기 객체명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,7 @@ import static racingcar.utils.ViewMessages.INPUT_CAR_NAMES;
 import static racingcar.utils.ViewMessages.INPUT_REPEAT_COUNT;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.utils.IntegerConvertor;
+import racingcar.utils.IntegerConverter;
 
 public class InputView {
 
@@ -15,6 +15,6 @@ public class InputView {
 
     public static int inputRepeatCount() {
         System.out.println(INPUT_REPEAT_COUNT);
-        return IntegerConvertor.convert(Console.readLine());
+        return IntegerConverter.convert(Console.readLine());
     }
 }
