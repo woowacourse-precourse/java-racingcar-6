53e83e58b47911afd15b4414ecd4a622f4966b42
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-01 00:06:38 +0900

    refactor(PrintRaceResult): if 구문에 중괄호 추가

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -10,23 +10,27 @@ public class InputValidator {
     private static final int MAX_LENGTH_OF_CAR_NAME = 5;
 
     public static void isInputValid(String userInput){
-        if (userInput == null || userInput.equals(" ") || userInput.isEmpty())
+        if (userInput == null || userInput.equals(" ") || userInput.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_IS_INVALID.getMessage());
+        }
     }
 
     public static void isFiveLettersOver(String userInput){
-        if (userInput.length() > MAX_LENGTH_OF_CAR_NAME)
+        if (userInput.length() > MAX_LENGTH_OF_CAR_NAME) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_OVER_MAX_LEN.getMessage());
+        }
     }
 
     public static void isInputDigit(String userInput){
-        if(!Pattern.matches("^[0-9]*$",userInput))
+        if(!Pattern.matches("^[0-9]*$",userInput)) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_IS_NOT_NUMBER.getMessage());
+        }
     }
 
 
     public static <E> void isListEmpty(List<E> list){
-        if(list.isEmpty())
+        if(list.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_IS_INVALID.getMessage());
+        }
     }
 }
