2c9624f2f1a8964eb519415abddd1813ab950498
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 15:37:19 +0900

    feat: add constructor to game controller

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,16 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
 }
