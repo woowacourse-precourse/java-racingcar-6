1fecf7ac1f4b74fec4cd6cc05ffe621012bc3243
Yoonseol Lee <thisyoon97@gmail.com>
2023-10-31 12:28:39 +0900

    refactor: carNamesInput 변수명 수정 가독성을 위해 변수 carNamesInput의 이름을 carInput으로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class InputView {
     public List<String> getCarNamesInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNamesInput = Console.readLine();
-        // 검증
-        String[] carsList = carNamesInput.split(",");
+        String carInput = Console.readLine();
+        // validateCarInput(carInput);
+        String[] carsList = carInput.split(",");
         return Arrays.asList(carsList);
     }
 }
