c8cff3226d43e8548247d83e3101e5f6a915b115
ddingmin <ddingminyt@gmail.com>
2023-10-31 15:49:28 +0900

    구현: 자동차 클래스를 구현한다.
    1. 자동차는 이름과 거리에 대한 정보를 가지고 있다.
    2. 자동차의 거리의 초기값은 0이다.
    3. 자동차는 이름을 입력받아 생성한다.
        - 자동차의 이름은 5자리 이하로 구성하며, 올바르지 않은 입력에는 `IllegalArgumentException`를 발생시킨다.
    4. 자동차는 전진할 수 있다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+public class Car {
+	public static int INIT_DISTANCE = 0;
+	private final String name;
+	private int distance;
+
+	private Car(String name) {
+		validateName(name);
+
+		this.name = name;
+		this.distance = INIT_DISTANCE;
+	}
+
+	public static Car from(String name) {
+		return new Car(name);
+	}
+
+	public void move() {
+		distance += 1;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void validateName(String name){
+		if (name.length() > 5 || name.length() < 1) {
+			throw new IllegalArgumentException("자동차 이름이 올바르지 않습니다. (5자 이하의 이름을 입력해주세요.)");
+		}
+	}
+}
