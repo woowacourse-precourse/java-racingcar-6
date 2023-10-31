b5d3e2edae207ef7978e47945efc0634d78a0907
sotogito <108535526+sotogito@users.noreply.github.com>
2023-10-31 20:00:40 +0900

    chore : Validator의 isVaildRange if문 없애고 return으로 바로 반환되게 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -7,19 +7,20 @@ public class Validator {
     private static final String WORNG_CAR_LENGTH = "이름은 5자 이하만 가능해요.";
     private static final String WORNG_COUNT_RANDE = "최소 시도 횟수는 1번이에요.";
 
-    //false일때 오류
-    public void validatorForCar(List<String> cars) { //자동차 이름이 5자 이하인가?
+    public void validatorForCar(List<String> cars) {
         if (!isMaxSizeName(cars)) {
             throw new IllegalArgumentException(WORNG_CAR_LENGTH);
         }
     }
 
-    public void validatorForAttemptCount(int count) { //시도 횟수가 1 이상인가?
+    public void validatorForAttemptCount(int count) {
         if (!isVaildRange(count)) {
             throw new IllegalArgumentException(WORNG_COUNT_RANDE);
         }
     }
 
+
+    //region 자동자 유효 검사 Section
     private boolean isMaxSizeName(List<String> cars) {
         for (String car : cars) {
             if (car.length() > 5) {
@@ -29,12 +30,19 @@ public class Validator {
         return true;
     }
 
+    //private boolean isDuplicateName
+    //endregion
+
+
+
+
+    //region 시도 횟수 유효 검사 Section
     private boolean isVaildRange(int count) {
-        if (count < 1) {
-            return false;
-        }
-        return true;
+        return count >= 1;
     }
+    //endregion
+
+
 
     //중복된 이름을 입력했을 때
     //,이 2개가 연달아서 있을 때
