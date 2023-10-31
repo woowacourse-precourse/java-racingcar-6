7f2a55b34f260f58318c222c22acaaf1a07b3b64
yang <hc1489@naver.com>
2023-10-31 18:07:17 +0900

    validation ->> nameValidation 으로 수정

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Controller {
     private ArrayList<Car> racingCars = new ArrayList<>();
 
-    private String[] validation(String[] names) {
+    private String[] nameValidation(String[] names) {
         for (String name : names) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("Too long name");
@@ -18,7 +18,9 @@ public class Controller {
     public String[] nameInput() {
         String[] names = Console.readLine().split(",");
 
-        return validation(names);
+        return nameValidation(names);
     }
+
+
 
 }
