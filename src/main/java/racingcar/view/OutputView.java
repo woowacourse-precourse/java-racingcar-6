516013457a247c4faaf74274fb75b3e4299bdac3
yoonjuho <dbswn990@naver.com>
2023-10-31 18:11:19 +0900

    feat(OutputView): 우승자를 출력한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printGameScore(GameScore gameScore){
         System.out.println(gameScore);
     }
+
+    public void printWinner(String winner){
+        System.out.println("최종 우승자 : " + winner);
+    }
 }
