9b80d2a32eea9ddce4123b3963f02ff08b44758f
uwoobeat <desktop3711@gmail.com>
2023-10-31 20:23:46 +0900

    refactor: 문자열 상수를 사용하도록 개선

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -1,17 +1,21 @@
 package racingcar;
 
 public class OutputManager {
-    // TODO: 문자열 상수로 리팩토링
+
+    public static final String CAR_NAMES_REQ_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String RACE_COUNT_REQ_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String GAME_RESULT_INFO_MESSAGE = "실행 결과";
+
     public void printCarNameRequestMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAMES_REQ_MESSAGE);
     }
 
     public void printRaceCountRequestMessage() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(RACE_COUNT_REQ_MESSAGE);
     }
 
     public void printGameResult(GameResult gameResult) {
-        System.out.println("실행 결과");
+        System.out.println(GAME_RESULT_INFO_MESSAGE);
         gameResult.history().forEach(this::printGameSnapshot);
     }
 
