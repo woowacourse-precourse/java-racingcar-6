743700fb2c06e58585e89b66f16e4479a24f9c40
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-01 05:10:49 +0900

    feat: 우승자를 선발하는 기능 추가
    자동차 게임 우승자를 선발해 출력하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -25,6 +25,23 @@ public class Application {
         PrintResult(cars);
         }
 
+        WhoIsWinner(cars);
+
+    }
+
+    private static void WhoIsWinner(Car[] cars) {
+        int max = 0;
+        StringBuilder sb = new StringBuilder();
+        for(Car car : cars){
+            if(car.meter > max){
+                sb.setLength(0);
+                max = car.meter;
+                sb.append(car.name).append(", ");
+            }
+            else if(car.meter == max) sb.append(car.name).append(", ");
+        }
+        sb.setLength(sb.length() -2);
+        System.out.print("최종 우승자 : " + sb);
     }
 
     private static void PrintResult(Car[] cars) {
