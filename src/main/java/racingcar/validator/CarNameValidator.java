fe0c6c1c2793c477a6bdbc7793e12344ff84648d
junest66 <chlwndks333@gmail.com>
2023-10-31 19:24:20 +0900

    feat(CarNameValidator): 자동차의 최소 입력 대수 검증 기능 추가

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -5,16 +5,24 @@ import java.util.Set;
 
 public class CarNameValidator {
 
+    private static final String CAR_NAME_MINIMUM_ERROR_MESSAGE = "최소 두 대 이상의 자동차 이름을 입력해야 합니다.";
     private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차의 이름은 5자 이하만 가능합니다.";
     private static final String CAR_NAME_DUPLICATE_ERROR_MESSAGE = "자동차의 이름은 중복될 수 없습니다.";
 
 
     public static void validateCarNames(String[] splitNames) {
+        checkCarNamesNum(splitNames);
         Set<String> uniqueNames = new HashSet<>();
         for (String name : splitNames) {
             checkNameLength(name);
             checkNameDuplicate(name, uniqueNames);
             uniqueNames.add(name);
+        }
+    }
+
+    private static void checkCarNamesNum(String[] splitNames) {
+        if (splitNames.length <= 1) {
+            throw new IllegalArgumentException(CAR_NAME_MINIMUM_ERROR_MESSAGE);
         }
     }
 
