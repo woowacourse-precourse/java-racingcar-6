9e2f398189947eadd2acc47842daca74a79d85bd
dmdrk1414 <seungchan141414@gmail.com>
2023-10-31 20:41:15 +0900

    [Style(racing)] 주석 수정

--- src/main/java/racingcar/domain/CreateRandomNum.java
+++ src/main/java/racingcar/domain/CreateRandomNum.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class CreateRandomNum {
+    private static final int START_NUM = 0;
+    private static final int END_NUM = 9;
 
     /**
      * 0에서 9까지의 정수 중 한 개의 정수 반환.
@@ -10,7 +12,6 @@ public class CreateRandomNum {
      * @return
      */
     public Integer createRanOneNum() {
-        // 0에서 9까지의 정수 중 한 개의 정수 반환.
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(START_NUM, END_NUM);
     }
 }
