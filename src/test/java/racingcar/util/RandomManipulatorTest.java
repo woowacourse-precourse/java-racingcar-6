0c9a9da2f1c4de42d839dfa04c8290e83eaa8fbf
tfer2442 <tfer2442@gmail.com>
2023-11-01 03:28:38 +0900

    test(RandomManipulatorTest): pickNumber 메서드 반복 테스트 생성

---
+++ src/test/java/racingcar/util/RandomManipulatorTest.java
@@ -0,0 +1,25 @@
+package racingcar.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.constant.Constants.MAX_RANDOM_VALUE;
+import static racingcar.constant.Constants.MIN_RANDOM_VALUE;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+
+class RandomManipulatorTest {
+    private RandomManipulator randomManipulator;
+    private final int REPEAT_NUMBER = (MAX_RANDOM_VALUE - MIN_RANDOM_VALUE) * 10;
+
+    @BeforeEach
+    void beforeEach() {
+        randomManipulator = new RandomManipulator();
+    }
+
+    @RepeatedTest(REPEAT_NUMBER)
+    void pickNumber() {
+        int result = randomManipulator.pickNumber();
+
+        assertTrue((result >= MIN_RANDOM_VALUE) && (result <= MAX_RANDOM_VALUE));
+    }
+}
