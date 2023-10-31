4bb6534c7e5c22cad7f26d21ad617bd2eca8656c
zeroung13 <nwj6535@gmail.com>
2023-10-31 17:06:02 +0900

    feat: CarView 클래스 추가
    자동차의 진행도와 우승자를 출력한다.

---
+++ src/main/java/racingcar/CarView.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.List;
+
+public class CarView {
+  public void printStatus(List<Car> cars) {
+    for (Car car : cars) {
+      System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+    }
+  }
+
+  public void printWinners(List<String> winners) {
+    System.out.println("최종 우승자: " + String.join(", ", winners));
+  }
+}
