fdd070f9e83309580a72f2d56549056c8b30491e
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-01 01:49:59 +0900

    test: 위치에 대한 테스트코드 추가

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import racingcar.model.Position;
 
 public class PositionTest {
     @Test
-    void 최대_거리_테스트() {
+    void 최대_위치_테스트() {
         Position maxPosition = new Position();
         List<Position> positions = new ArrayList<>();
         positions.add(new Position(7));
@@ -23,6 +24,36 @@ public class PositionTest {
             }
         }
         assertEquals(new Position(9), maxPosition);
+    }
+
+    @Test
+    void 위치_객체_동등성_테스트() {
+        Position position1 = new Position(5);
+        Position position2 = new Position(5);
+        assertTrue(position1.equals(position2));
+    }
+
+    @Test
+    void 위치_비교_테스트() {
+        Position position1 = new Position(1);
+        Position position2 = new Position(2);
+        assertTrue(position2.isGreaterThan(position1));
+    }
+
+    @Test
+    void 이동한_후_위치_비교_테스트() {
+        Position position1 = new Position();
+        Position position2 = new Position();
+        position1.move();
+        assertTrue(position1.isGreaterThan(position2));
+    }
+
+    @Test
+    void 위치를_문자열로_변환하는_테스트() {
+        Position position = new Position();
+        position.move();
+        position.move();
+        assertEquals("--", position.format());
     }
 
 }
