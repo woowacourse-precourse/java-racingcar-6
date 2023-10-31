cc26244da1d424fc81c15fb1f929fe6544acc40a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-31 21:06:43 +0900

    test(inputView): 이름 사이에 공백이 있는 경우 제거하는지, 이름 안에 공백이 있는 경우 예외 발생하는지 테스트

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 public class InputView {
     private final String CAR_NAME_DELIMITER = ",";
     private final String INVALID_INPUT_TYPE = "입력은 자동차 이름과 구분자(,)만 가능합니다.";
+    private final String NAME_CANNOT_CONTAINS_SPACE = "이름에는 공백이 포함될 수 없습니다.";
     private final Integer MIN_TRY_COUNT = 1;
     private final String TRY_COUNT_ONLY_NUMBER = "시도 횟수는 숫자여야 합니다.";
     private final String MINIMUM_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
@@ -20,7 +21,10 @@ public class InputView {
     }
 
     public List<String> inputCarNames() {
-        return Arrays.asList(input().split(CAR_NAME_DELIMITER));
+        return Arrays.stream(input().split(CAR_NAME_DELIMITER))
+                .map(String::trim)
+                .peek(this::validateNameContainSpace)
+                .toList();
     }
 
     public int inputTryCount() {
@@ -34,8 +38,14 @@ public class InputView {
     }
 
     private void validateInputType(String carName) {
-        if (!Pattern.matches("^[a-zA-Z,]*$", carName)) {
+        if (!Pattern.matches("^[a-zA-Z,\s]*$", carName)) {
             throw new IllegalArgumentException(INVALID_INPUT_TYPE);
+        }
+    }
+
+    private void validateNameContainSpace(String carName) {
+        if (carName.contains(" ")) {
+            throw new IllegalArgumentException(NAME_CANNOT_CONTAINS_SPACE);
         }
     }
 
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,15 +1,17 @@
 package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class ViewerTest {
+public class InputViewTest {
     private final InputView inputView = new InputView();
 
     @Nested
@@ -23,9 +25,29 @@ public class ViewerTest {
                             .hasMessage("입력은 자동차 이름과 구분자(,)만 가능합니다."));
         }
 
+        @Test
+        void 이름에_공백이_있을_경우_예외() {
+            final String input = "pob  i,woni";
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("이름에는 공백이 포함될 수 없습니다.");
+            });
+        }
+
+        @Test
+        void 입력에_공백을_제거하여_이름_목록을_생성하는지() {
+            final String input = " pobi,    woni ";
+            assertSimpleTest(() -> {
+                run(input);
+                assertThat(output()).contains("pobi", "woni");
+            });
+        }
+
         @Override
         protected void runMain() {
-            inputView.inputCarNames();
+            List<String> carNames = inputView.inputCarNames();
+            System.out.println(carNames);
         }
     }
 
