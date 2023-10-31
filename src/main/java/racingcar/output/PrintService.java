566fcc86c8203a95b1f5c6456c2e06cb5c6e97f5
cousim46 <wjdghldns24@naver.com>
2023-10-31 22:49:23 +0900

    refactor : 중간 발표 및 메서드 명 변경

--- src/main/java/racingcar/output/PrintService.java
+++ src/main/java/racingcar/output/PrintService.java
@@ -9,12 +9,15 @@ public class PrintService {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void askAttemtCount() {
+    public static void askAttemptCount() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void show(Car car) {
-        System.out.printf("%s : %s\n", car.getName(), "-".repeat(car.getStraightCount()));
+    public static void show(List<Car> cars) {
+        cars.forEach(car -> {
+            System.out.printf("%s : %s\n", car.getName(), "-".repeat(car.getStraightCount()));
+        });
+        PrintService.nextLine();
     }
 
     public static void result(List<String> racings) {
