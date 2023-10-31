cf09fd8e08379b85f2ff6bed0b3fa910f74095b0
Yujin <tkfkd7038@naver.com>
2023-10-31 16:50:33 +0900

    feat(racingcar): 랜덤값이 4이상인 경우 전진하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,16 @@
 package racingcar;
 
 public class Car {
-    public Car() {
+    int randomNumber;
+    int forwardCount;
+    String carName;
+
+    public Car(String carName) {
+        this.carName = carName;
     }
 
-    public int moveForward(){
-        return 0;
+    public void moveForward(int randomNum) {
+        if (randomNum >= 4)
+            forwardCount++;
     }
 }
