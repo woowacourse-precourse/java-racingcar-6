55d2002a342c7103d29328fff103ac50e1dce2df
hyocheol An <anhyocheol@naver.com>
2023-11-01 08:20:13 +0900

    stlye : 출력 문구 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
 public class OutputView {
+    public void lineBreak () {
+        System.out.println();
+    }
+
+    public void result() {
+        System.out.println("\n실행 결과");
+    }
+    public void resultByRound(String name, String printDistance) {
+        System.out.printf("%s : %s\n", name, printDistance);
+    }
 }
