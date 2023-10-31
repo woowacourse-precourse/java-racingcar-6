9faf5a9e4b95cc0c0a9723b825bd367938944c32
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 23:56:06 +0900

    [Fix](Exception) - 예외 처리 메서드 추가

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -4,13 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Exception {
-
     private static final int CAR_MAX_LENGTH = 5;
+    private static final int ZERO_VALUE = 0;
     private static final String NAME_LENGTH_ERROR = "5글자가 넘어 갑니다.";
     private static final String INPUT_FORMAT_ERROR = "올바른 입력이 아닙니다.";
+    public static final String NEGATIVE_ERROR = "음수 입력 입니다.";
     private static final String REGEX = "^[a-zA-Z가-힣,]*$";
 
-    public void lengthCheck(String carName) {
+    public void checkLength(String carName) {
         String[] carNameArray = carName.split(",");
         for (String name : carNameArray) {
             if (name.length() > CAR_MAX_LENGTH) {
@@ -27,6 +28,10 @@ public class Exception {
         }
     }
 
-
+    public void checkNegativeValue(int round) {
+        if (round < ZERO_VALUE) {
+            throw new IllegalArgumentException(NEGATIVE_ERROR);
+        }
+    }
 }
 
