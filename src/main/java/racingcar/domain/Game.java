ef71818de331c94076555102e98b485b934fb73e
이정민 <jmlee2705@gmail.com>
2023-11-01 07:20:02 +0900

    feat(Game): generateCarsByCarNames 구현

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,18 +1,31 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.utils.GameUtil;
 
 public class Game {
 
-    private List<Car> cars;
+    private List<Car> cars = new ArrayList<>();
     private List<Car> winners;
     private int totalTrialCount;
     private int curTrialCount = 0;
 
+    private final GameUtil gameUtil = new GameUtil();
+
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
+    }
+
+    public void generateCarsByCarNames(String carNames) {
+        List<String> carNamesList = gameUtil.splitNamesByComma(carNames);
+
+        for (String carName : carNamesList) {
+            Car newCar = new Car(carName);
+            cars.add(newCar);
+        }
     }
 
     public void inputTrialCount() {
