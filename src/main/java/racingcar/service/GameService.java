f9cd90e453622436368040d7cbe7c708ada57066
suihub <qkdcrt@gmail.com>
2023-11-01 00:37:55 +0900

    <test>: "자동차 이동 판정 테스트 구현"
    변경된 로직에 맞추어 자동차가 이동하기 위한 판정 테스트 구현.
    다음의 2가지를 검증.
    
    1. 자동차 이동이 가능한 경우.
    2. 자동차 이동이 불가능한 경우.

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,7 +1,5 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.Arrays;
 import java.util.OptionalInt;
 
--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -5,10 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class GameServiceTest {
+    private final GameService service = new GameService();
+
+    @Test
+    void 이동_판정_허가() {
+        Integer number = 8;
+
+        boolean checked = service.checkMovePossible(number);
+
+        assertThat(checked).isTrue();
+    }
+
+    @Test
+    void 이동_판정_실패() {
+        Integer number = 2;
+
+        boolean checked = service.checkMovePossible(number);
+
+        assertThat(checked).isFalse();
+    }
 
     @Test
     void 우승자_위치_반환() {
-        GameService service = new GameService();
         Integer[] positions = new Integer[10];
 
         for (int i = 0; i < 10; i++) {
