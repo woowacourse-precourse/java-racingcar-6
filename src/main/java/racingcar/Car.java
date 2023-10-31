85550416e98f0288762ed9a912263c90cf76cfcd
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-01 01:06:09 +0900

    [Feat](Car) - Car 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,31 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private String distance = "";
+    private int count = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance += distance;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount() {
+        this.count++;
+    }
 }
