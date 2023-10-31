9758c42d792099542bd0c5965585f50de8d20d0f
devhyeon0 <devhyeon0@gmail.com>
2023-10-31 23:56:51 +0900

    feat: 랜덤값이 4 이상일 경우 전진하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,2 +1,27 @@
-package racingcar.domain;public class Car {
+package racingcar.domain;
+
+public class Car {
+
+    private static final int BASE_NUMBER = 4;
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= BASE_NUMBER) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,2 +1,28 @@
-package racingcar.domain;public class Cars {
+package racingcar.domain;
+
+import racingcar.service.GameService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<String> carList) {
+        this.cars = createCars(carList);
+    }
+
+    private List<Car> createCars(List<String> carList) {
+        return carList.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            car.move(GameService.generateRandomNumber());
+        }
+    }
 }
