494d52525e1d67f16bbdc8c84d52325cd2809760
happy-nana <nxxxgnim@gmail.com>
2023-10-31 15:54:58 +0900

    feat: Application.java 기능 구현 자동차 경주 게임의 엔트리 main 메서드 실행 -> 경주 게임 시작 -> 유저 입력 받기 -> 게임 실행 -> 결과 출력의 흐름을 가지고 있음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,8 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         RacingGame game = new RacingGame();
+        game.userInput();
+        game.gameSystem();
+        game.printWinners();
     }
 }
