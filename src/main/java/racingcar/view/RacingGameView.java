94a5bd03a5940d9926b93c845371b3ab4fd64eba
Kim mingyu <minq0302@naver.com>
2023-11-01 01:05:05 +0900

    feat: add printing final winners function

--- src/main/java/racingcar/view/RacingGameView.java
+++ src/main/java/racingcar/view/RacingGameView.java
@@ -1,11 +1,18 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class RacingGameView {
 
     // 4. 자동차 이름을 입력해달라는 문구를 출력하는 기능
-    public void showCarNameInputMessage(){
+    public void showCarNameInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+//    3. 최종 우승자(들)을 출력하는 기능
+    public void printFinalWinners(List<String> winnerList) {
+        String winners = "최종 우승자 : ";
+        winners += String.join(", ", winnerList);
+        System.out.print(winners);
+    }
 }
