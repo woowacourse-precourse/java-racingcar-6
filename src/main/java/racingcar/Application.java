5088262f84ad7f928c4c692765fa905dce5e8b22
Nbowow <nbo5548@naver.com>
2023-10-31 19:33:36 +0900

    Refactor: Application 2023.10.31 사용하지 않는 라이브러리 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,5 @@
 package racingcar;
-
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.controller.Controller;
-import racingcar.service.RaceService;
-import racingcar.util.Validation;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
