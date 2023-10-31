2f1b81f112d713d452596cabd1c22bee27f4c125
jun02160 <jun020216@naver.com>
2023-10-31 10:06:02 +0900

    feat : 사용자 입력을 위한 메서드 및 예외처리

---
+++ src/main/java/racingcar/global/util/GameInput.java
@@ -0,0 +1,31 @@
+package racingcar.global.util;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public final class GameInput {
+
+    public static int inputRoundCnt() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = readLine();
+        return validateCnt(input);
+    }
+
+    private static int validateCnt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자 형식이 올바르지 않습니다.");
+        }
+    }
+
+    public static String[] inputCarList() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carList = readLine().split(",\\s*");
+        validateCarList(carList);
+        return carList;
+    }
+
+    private static void validateCarList(String[] carList) {
+
+    }
+}
