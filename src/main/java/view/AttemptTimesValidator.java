0b0e2f4bf3d941e757a08aa8b0f648e791003c87
nayonsoso <panda0329@naver.com>
2023-11-01 02:00:19 +0900

    feat: 정수인지 검증하는 함수 구현

--- src/main/java/view/AttemptTimesValidator.java
+++ src/main/java/view/AttemptTimesValidator.java
@@ -1,5 +1,11 @@
 package view;
 
 public class AttemptTimesValidator {
-
+    private void validateInteger(String input){
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
 }
