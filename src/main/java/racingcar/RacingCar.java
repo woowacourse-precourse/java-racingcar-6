2afd4ed8f04e0e7fd0f8a9941fada1136532f4d7
Seunghyun-Jeong <95638057+Seunghyun-Jeong@users.noreply.github.com>
2023-10-31 13:54:35 +0900

    feat: 주석 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -22,7 +22,9 @@ public class RacingCar {
         System.out.println("최종 우승자 : " + getWinnerString(winners));
     }
 
-
+    // 입력한 자동차 이름 쉼표(,)로 구분
+    // 5자 이하가 아니면, 에러 발생
+    // 띄워쓰기가 포함되어 있으면, 에러 발생
     public String[] getCarName() {
         String[] carNameArr = Console.readLine().split(",");
         for (String carName : carNameArr) {
@@ -37,6 +39,9 @@ public class RacingCar {
         return carNameArr;
     }
 
+    // 게임 횟수 입력
+    // 숫자가 아니면, 에러 발생
+    // 0이면, 에러 발생
     public int readTryCount() {
         int tryCount = 0;
         try {
@@ -51,6 +56,7 @@ public class RacingCar {
         return tryCount;
     }
 
+    // 입력한 반복 횟수만큼 게임 반복 실행
     public int[] executeRacingGame(int tryCount, String[] carNameArr) {
         int[] carResult = new int[carNameArr.length];
         for (int i = 0; i < tryCount; i++) {
@@ -61,6 +67,7 @@ public class RacingCar {
         return carResult;
     }
 
+    // 랜덤 숫자 4이상 이면, 한칸 전진
     public void executeRacing(int[] carResult) {
         for (int idx = 0; idx < carResult.length; idx++) {
             int randomNumber = Randoms.pickNumberInRange(0, 9);
@@ -70,12 +77,14 @@ public class RacingCar {
         }
     }
 
+    // "자동차 이름 : 전진 칸 수(-)" 형태로 출력
     public void printRacingResult(String[] carNameArr, int[] carResult) {
         for (int idx = 0; idx < carNameArr.length; idx++) {
             System.out.println(carNameArr[idx] + " : " + getResultString(carResult[idx]));
         }
     }
 
+    // 한칸 전진을 "-"로 표현
     public String getResultString(int result) {
         String resultString = "";
         for (int i = 0; i < result; i++) {
@@ -84,6 +93,7 @@ public class RacingCar {
         return resultString;
     }
 
+    // 전진한 칸 수 비교해서, winners에 추가
     public List<String> getWinnersResult(int[] carResult, String[] carNameArr) {
         int maxResult = -1;
         List<String> winners = new ArrayList<>();
@@ -103,7 +113,8 @@ public class RacingCar {
         }
         return winners;
     }
-    
+
+    // 공동 우승자 출력 시, 쉼표(,)로 구분
     public String getWinnerString(List<String> winners) {
         return String.join(", ", winners);
     }
