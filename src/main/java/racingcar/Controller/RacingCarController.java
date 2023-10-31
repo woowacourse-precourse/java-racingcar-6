954acb70ee01dcc5f0a4940e8ee65add9b339bcf
HurDong <gjehdals456@gmail.com>
2023-11-01 02:45:21 +0900

    feat : Controller

---
+++ src/main/java/racingcar/Controller/RacingCarController.java
@@ -0,0 +1,36 @@
+package racingcar.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.Model.Race;
+import racingcar.Model.RacingCar;
+import racingcar.View.RacingCarView;
+
+public class RacingCarController {
+
+    private Race race;
+    private RacingCarView view;
+
+    public RacingCarController() {
+        view = new RacingCarView();
+    }
+
+    public void startGame() {
+        String input = view.getCarNames();
+        String[] carNames = input.split(",");
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (String carName : carNames) {
+            RacingCar racingCar = new RacingCar(carName);
+            racingCars.add(racingCar);
+        }
+        int rounds = view.getRoundsInput();
+
+        race = new Race(racingCars, rounds, view);
+
+        race.run();
+
+        view.printRaceResult(race.getCars());
+        view.printWinners(race.getWinners());
+    }
+}
