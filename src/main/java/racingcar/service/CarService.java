bb436f83a7c3c130347f2b2990976d0b26a26365
emost22 <emoney96@naver.com>
2023-11-01 01:31:08 +0900

    Refactor: separate getWinner method

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -16,28 +16,8 @@ public class CarService {
         round = getRound();
 
         playRound();
-
-        int max = carList.get(0).getMoveDistance();
-        List<String> winnerList = new ArrayList<>(List.of(carList.get(0).getName()));
-        int size = carList.size();
-        for (int i = 1; i < size; i++) {
-            Car car = carList.get(i);
-
-            if (max < car.getMoveDistance()) {
-                winnerList.clear();
-                max = car.getMoveDistance();
-                winnerList.add(car.getName());
-            } else if (max == car.getMoveDistance()) {
-                winnerList.add(car.getName());
-            }
-        }
-
-        StringBuilder winnerNames = new StringBuilder(winnerList.get(0));
-        for (int i = 1; i < winnerList.size(); i++) {
-            winnerNames.append(CarUtil.WINNER_SEPARATOR).append(winnerList.get(i));
-        }
-
-        CarUtil.printWinner(winnerNames.toString());
+        String winnerNames = getWinnerNames();
+        CarUtil.printWinner(winnerNames);
     }
 
     private List<Car> getCarList() {
@@ -73,6 +53,36 @@ public class CarService {
             });
             CarUtil.printNewLine();
         }
+    }
+
+    private String getWinnerNames() {
+        List<String> winnerList = getWinnerList();
+        StringBuilder winnerNames = new StringBuilder(winnerList.get(0));
+        for (int i = 1; i < winnerList.size(); i++) {
+            winnerNames.append(CarUtil.WINNER_SEPARATOR).append(winnerList.get(i));
+        }
+
+        return winnerNames.toString();
+    }
+
+    private List<String> getWinnerList() {
+        int maxDistance = carList.get(0).getMoveDistance();
+        List<String> winnerList = new ArrayList<>(List.of(carList.get(0).getName()));
+        
+        int carListSize = carList.size();
+        for (int i = 1; i < carListSize; i++) {
+            Car car = carList.get(i);
+
+            if (maxDistance < car.getMoveDistance()) {
+                winnerList.clear();
+                maxDistance = car.getMoveDistance();
+                winnerList.add(car.getName());
+            } else if (maxDistance == car.getMoveDistance()) {
+                winnerList.add(car.getName());
+            }
+        }
+
+        return winnerList;
     }
 
     private boolean isPossibleMove() {
