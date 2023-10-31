f065d9b12add92aacb0d04f06ff49f94ec86be18
baksakcci <pshsh910@gmail.com>
2023-10-31 22:22:46 +0900

    fix($AttemptValidator): fix isNumeric logic

--- src/main/java/racingcar/validation/AttemptValidator.java
+++ src/main/java/racingcar/validation/AttemptValidator.java
@@ -5,7 +5,7 @@ public class AttemptValidator {
         if (attempt.isBlank()) {
             throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
-        if (attempt.matches("[0-9]+")) {
+        if (!attempt.matches("\\d")) {
             throw new IllegalArgumentException("숫자 이외에 입력할 수 없습니다.");
         }
     }
