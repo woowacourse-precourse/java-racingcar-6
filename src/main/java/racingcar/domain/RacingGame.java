5c3f086267f500c3e908fba81a9851eb84dc5070
cyPark95 <parkcy1007@gmail.com>
2023-10-31 16:01:07 +0900

    refactor: 사용자로부터 자동차 이름 목록 처리 로직 InputView 로 분리

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -13,8 +13,6 @@ import racingcar.dto.CarInfo;
 
 public class RacingGame {
 
-    private static final String SEPARATOR = ",";
-
     private final Cars cars;
     private final RaceCount raceCount;
 
@@ -56,16 +54,14 @@ public class RacingGame {
     }
 
     private Cars createCars() {
-        String input = inputCarNames();
-        String[] carNames = input.split(SEPARATOR);
-        List<String> carNameList = List.of(carNames);
-        return new Cars(carNameList);
+        List<String> carNames = inputCarNames();
+        return new Cars(carNames);
     }
 
     private RaceCount createRaceCount() {
-        String input = inputRaceCount();
+        String count = inputRaceCount();
         try {
-            int raceCount = Integer.parseInt(input);
+            int raceCount = Integer.parseInt(count);
             return new RaceCount(raceCount);
         } catch (NumberFormatException e) {
             throw new CustomErrorException(ErrorCode.NOT_NUMBER_STRING);
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputView {
+
+    private static final String SEPARATOR = ",";
 
     private InputView(){
     }
 
-    public static String inputCarNames() {
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        String[] carNames = input.split(SEPARATOR);
+        return List.of(carNames);
     }
 
     public static String inputRaceCount() {
