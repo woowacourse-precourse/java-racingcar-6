a9a3dd07557c14d587be9a40dd34951cb0db6d98
kimdohyung <pdohyung@gmail.com>
2023-10-31 18:43:24 +0900

    test(RacingCarControllerTest): 쉼표(,)로 이름을 분리하는 함수 테스트 코드 추가

--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -3,8 +3,10 @@ package racingcar.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RacingCarControllerTest {
     private RacingCarController racingCarController;
@@ -29,6 +31,37 @@ public class RacingCarControllerTest {
 
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
             racingCarController.inputCarNames(testInput);
+        });
+
+        assertThat(e.getMessage()).isEqualTo("빈칸은 이름이 될 수 없습니다.");
+    }
+
+    @Test
+    void splitCarNamesByComma_올바른_이름_목록일_때_목록_반환() {
+        String testInput = "pobi,woni,jun";
+        List<String> answerList = List.of("pobi", "woni", "jun");
+
+        List<String> result = racingCarController.splitCarNamesByComma(testInput);
+
+        assertThat(result).isEqualTo(answerList);
+    }
+
+    @Test
+    void splitCarNamesByComma_구분자가_없을_때_목록_반환() {
+        String testInput = "pobi";
+        List<String> answerList = List.of("pobi");
+
+        List<String> result = racingCarController.splitCarNamesByComma(testInput);
+
+        assertThat(result).isEqualTo(answerList);
+    }
+
+    @Test
+    void splitCarNamesByComma_빈칸_이름이_있을_때_예외_발생() {
+        String testInput = "pobi,woni,,jun";
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            racingCarController.splitCarNamesByComma(testInput);
         });
 
         assertThat(e.getMessage()).isEqualTo("빈칸은 이름이 될 수 없습니다.");
