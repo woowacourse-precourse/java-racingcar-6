e1acbbd45aec03ac8b5354b59aa543892772f9bb
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-31 17:14:59 +0900

    feat: GameResultDto 구현

---
+++ src/main/java/racingcar/dto/GameResultDto.java
@@ -0,0 +1,23 @@
+package racingcar.dto;
+
+import java.util.List;
+import racingcar.domain.car.Car;
+
+public class GameResultDto {
+
+    private boolean isSingleWinner;
+    private List<Car> winners;
+
+    public GameResultDto(List<Car> winners) {
+        isSingleWinner = winners.size() == 1;
+        this.winners = winners;
+    }
+
+    public boolean isSingleWinner() {
+        return isSingleWinner;
+    }
+
+    public List<Car> getWinners() {
+        return winners;
+    }
+}
