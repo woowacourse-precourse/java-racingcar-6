1ef05a6c6bc8708a26d1bfab6db2e166845da12f
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-31 12:28:11 +0900

    refactor: [Car] 함수마다 하나의 기능을 하도록 함수 분리 및 상수화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static racingcar.Output.RACING_RESULT;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -14,9 +16,9 @@ public class Car {
 
     public void move(List<String> carNames, int movingNumber) {
         Map<String, Integer> carNameAndMoving = listToMap(carNames);
-        System.out.println("실행 결과");
+        System.out.println(RACING_RESULT);
         while (movingNumber > 0) {
-            moveCars(carNames, carNameAndMoving);
+            moveCars(carNameAndMoving);
             output.printRacingProcess(carNameAndMoving);
             movingNumber--;
         }
@@ -25,26 +27,31 @@ public class Car {
 
     private Map<String, Integer> listToMap(List<String> carNames) {
         Map<String, Integer> carNameAndMoving = new LinkedHashMap<>();
+        return putZeroToValues(carNames, carNameAndMoving);
+    }
 
+    private Map<String, Integer> putZeroToValues(List<String> carNames, Map<String, Integer> carNameAndMoving) {
         for (String carName : carNames) {
             carNameAndMoving.put(carName, 0);
         }
         return carNameAndMoving;
     }
 
-    private void moveCars(List<String> carNames, Map<String, Integer> carNameAndMoving) {
-        for (String carName : carNames) {
-            if (isMovable()) {
-                carNameAndMoving.put(carName, carNameAndMoving.getOrDefault(carName, 0) + 1);
-            }
+    private void moveCars(Map<String, Integer> carNameAndMoving) {
+        for (String carName : carNameAndMoving.keySet()) {
+            move(carName, carNameAndMoving);
         }
     }
 
+    private void move(String carName, Map<String, Integer> carNameAndMoving) {
+        if (isMovable()) {
+            carNameAndMoving.put(carName, carNameAndMoving.get(carName) + 1);
+        }
+    }
+
+
     private boolean isMovable() {
         int random = Randoms.pickNumberInRange(0, 9);
-        if (random >= 4) {
-            return true;
-        }
-        return false;
+        return random >= 4;
     }
 }
