49e112788583799317b5855727004e4edb85a257
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 11:12:34 +0900

    Feat: Controller클래스 getCars 구현
    InputView의 정적 메소드로 입력 요청 메시지를 출력한다.
    Validator 객체로 입력된 문자열을 검사한다.
    Parser객체로 입력된 문자열을 쉼표 기준으로 파싱한다.
    getCarNameList를 통해 List<String>을 얻고 stream을 통해 각각의 이름을 carName필드로 갖는 Car객체 생성 후 List<Car>로 리턴한다.

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,10 +1,40 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
 import racingcar.domain.Game;
+import racingcar.utils.Parser;
+import racingcar.utils.RandomGenerator;
+import racingcar.utils.Validator;
+import racingcar.view.InputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
 
     private final Validator validator = new Validator();
     private final Parser parser = new Parser();
     private Game game;
+
+
+    private List<Car> getCars() {
+        InputView.printCarNameRequest();
+        return getCarNameList().stream()
+                .map(carName -> new Car(carName, new RandomGenerator()))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getCarNameList() {
+        return parseCarNameInput(checkValidationCarNameInput(Console.readLine()));
+    }
+
+    private List<String> parseCarNameInput(String userInput) {
+        return parser.parseCarNameInput(userInput);
+    }
+
+    private String checkValidationCarNameInput(String userInput) {
+        return validator.validateCarNameInput(userInput);
+    }
+
 }
