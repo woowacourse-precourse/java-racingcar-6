35c3f4f08a3aeee1007f994f49af093f1dbf9565
YongHyeon <pyh4616@naver.com>
2023-10-31 17:17:56 +0900

    Refactor: (RacingCarGameController 클래스), 가장 상위 메서드의 이름을 run으로 변경하고 메서드 분리 진행, 메서드 인자들을 다시 한줄로 정렬

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -13,27 +13,40 @@ public class RacingCarGameController {
     private final InputView inputView;
     private final OutputView outputView;
 
-    public RacingCarGameController(
-            RacingCarGameService racingCarGameService,
-            InputView inputView,
-            OutputView outputView
-    ) {
+    public RacingCarGameController(RacingCarGameService racingCarGameService, InputView inputView, OutputView outputView) {
         this.racingCarGameService = racingCarGameService;
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
-    public void playRacingCarGame() {
+    public void run() {
+        List<String> carNames = getCarNames();
+        LinkedHashMap<String, Integer> racingProgressStatus = racingCarGameService.initializeRacingProgressStatus(carNames);
+        int attemptCount = getAttemptCount();
+        playRacingCarGame(attemptCount, carNames, racingProgressStatus);
+        announceWinners(racingProgressStatus);
+    }
+
+    private List<String> getCarNames() {
         outputView.printInputRacingCarNamesMessage();
-        List<String> carNames = inputView.readCarNames();
-        LinkedHashMap<String, Integer> racingProgressStatus = racingCarGameService.racingProgressStatusInitialize(carNames);
+        return inputView.readCarNames();
+    }
+
+    private int getAttemptCount() {
         outputView.printInputAttemptCountMessage();
-        int attemptCount = inputView.readAttemptCount();
+        return inputView.readAttemptCount();
+    }
+
+    private void playRacingCarGame(int attemptCount, List<String> carNames, LinkedHashMap<String, Integer> racingProgressStatus) {
+        System.out.println();
         outputView.printExecutionResultMessage();
         for (int i = 0; i < attemptCount; i++) {
             racingCarGameService.playSingleGame(carNames, racingProgressStatus);
             outputView.printExecutionResult(racingProgressStatus);
         }
+    }
+
+    private void announceWinners(LinkedHashMap<String, Integer> racingProgressStatus) {
         List<String> winners = racingCarGameService.getWinners(racingProgressStatus);
         outputView.printWinners(winners);
     }
