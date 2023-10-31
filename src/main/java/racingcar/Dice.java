a38a626d5c9ddcbce461a6da5956a2d715761f59
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 10:48:10 +0900

    [Fix](Dice) - 매직 넘버 상수 변경

--- src/main/java/racingcar/Dice.java
+++ src/main/java/racingcar/Dice.java
@@ -1,11 +1,14 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Dice {
     private int dice;
+    private static final int ZERO = 0;
+    private static final int NINE = 9;
 
     public Dice() {
-        this.dice = Randoms.pickNumberInRange(0, 9);
+        this.dice = Randoms.pickNumberInRange(ZERO, NINE);
     }
 
     public int getDice() {
