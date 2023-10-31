ef6bb4ad3e57f2fe1630db71638a4f74f57d3134
gilik154 <okk080200@gmail.com>
2023-10-31 18:53:47 +0900

    Feat : 애플리케이션 로직 작성
    - Application.java 에 로직 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.application.RandomDriveRule;
+import racingcar.controller.ConsoleInputOutput;
+import racingcar.domain.game.Game;
+import racingcar.domain.game.GameOperator;
+import racingcar.domain.policy.DriveRule;
+
 public class Application {
+    private static final DriveRule driveRule = new RandomDriveRule();
+    private static final GameOperator gameOperator = new ConsoleInputOutput();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(driveRule, gameOperator);
+
+        String result = game.run();
+
+        gameOperator.print(result);
     }
 }
