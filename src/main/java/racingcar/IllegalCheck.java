5271bc97ee51a10da52c5bb54d07b636b7a5b3ce
tkdtn4657 <tkdtn101226@gmail.com>
2023-10-31 16:48:55 +0900

    fix(IllegalCheck): 횟수의 범위 재정의, 문자열길이 범위 변경
    splittedName.length() >= STRING_MAX_LENGTH : >= -> >
    INPUT_RANGE_MAX : Integer.MIN_VALUE -> Integer.MAX_VALUE

--- src/main/java/racingcar/IllegalCheck.java
+++ src/main/java/racingcar/IllegalCheck.java
@@ -3,11 +3,11 @@ package racingcar;
 public class IllegalCheck {
 
     private static final int INPUT_RANGE_MIN = 0;
-    private static final int INPUT_RANGE_MAX = Integer.MIN_VALUE;
+    private static final int INPUT_RANGE_MAX = Integer.MAX_VALUE;
     private static final int STRING_MAX_LENGTH = 5;
 
-    public boolean integerRangeOverCheck(Long checkIntegerLength) {
-        if (INPUT_RANGE_MIN < checkIntegerLength && checkIntegerLength <= INPUT_RANGE_MAX) {
+    public boolean integerRangeOverCheck(Long checkIntegerValue) {
+        if (INPUT_RANGE_MIN < checkIntegerValue && checkIntegerValue <= INPUT_RANGE_MAX) {
             return true;
         }
         return false;
@@ -15,7 +15,7 @@ public class IllegalCheck {
 
 
     public boolean stringLengthOverCheck(String splittedName) {
-        return splittedName.length() >= STRING_MAX_LENGTH;
+        return splittedName.length() > STRING_MAX_LENGTH;
     }
 
     public boolean availableConvertInteger(int thisChar) {
