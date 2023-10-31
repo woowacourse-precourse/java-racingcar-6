55f4b229893c6d74c67e84072deeb3f2f2527161
hlmg <devhlmg@gmail.com>
2023-10-31 19:56:10 +0900

    test(Validator(CarName, Cars)): 검증 메서드 테스트 작성

---
+++ src/test/java/racingcar/domain/CarNameValidatorTest.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CarNameValidatorTest {
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            '   ', 자동차 이름은 비어있을 수 없습니다
+            ' a', 자동차 이름 양 옆에 공백이 올 수 없습니다
+            'a ', 자동차 이름 양 옆에 공백이 올 수 없습니다
+            123456, 자동차 이름은 5자 이하여야 합니다
+            """)
+    void 검증이_실패하면_예외가_발생한다(String name, String message) {
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> carNameValidator.accept(name))
+                .withMessage(message);
+    }
+}
---
+++ src/test/java/racingcar/domain/CarsValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CarsValidatorTest {
+    private static final String DUPLICATED_NAME = "경주에 참가하는 자동차 이름은 중복될 수 없습니다";
+    private static final String MIN_PARTICIPANTS = "경주에 참가하는 자동차는 최소 두 대 이상이어야 합니다";
+
+    public static Stream<Arguments> 검증이_실패하면_예외가_발생한다() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(new Car("1", 0, null), new Car("1", 0, null)),
+                        DUPLICATED_NAME),
+                Arguments.of(List.of(new Car("1", 0, null)), MIN_PARTICIPANTS),
+                Arguments.of(List.of(), MIN_PARTICIPANTS)
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 검증이_실패하면_예외가_발생한다(List<Car> cars, String message) {
+        CarsValidator carsValidator = new CarsValidator();
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> carsValidator.accept(cars))
+                .withMessage(message);
+    }
+}
