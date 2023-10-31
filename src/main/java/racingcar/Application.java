9959dee2e24bbf487fbfb52134f119974840cfca
hajungIm <ihj_ewha@ewhain.net>
2023-10-31 17:46:53 +0900

    Feat(InputView): 자동차 이름 입력 기능 구현
    자동차 이름을 입력받는 메소드 `inputCars` 작성
    
    Application.main() 에서 해당 메소드를 이용하여
    List<String> cars 입력 받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> cars = InputView.inputCars();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,11 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
-}
+    public static List<String> inputCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Arrays.stream(Console.readLine().split(",")).toList();
+    }
