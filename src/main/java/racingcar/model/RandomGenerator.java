2afd2cc5adbbe5bc4124607d5c7d098a6291a16f
Dawin <dawin002@naver.com>
2023-10-31 17:52:33 +0900

    refactor(RandomGenerator): 랜덤 숫자 관련 기능 변경
    랜덤 숫자 관련 메서드 2개를 non-static으로 변경:
    - getNumbers()
    - createNumber()

--- src/main/java/racingcar/model/RandomGenerator.java
+++ src/main/java/racingcar/model/RandomGenerator.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomGenerator {
-    public static List<Integer> getNumbers(int size) {
+    public List<Integer> getNumbers(int size) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             numbers.add(createNumber());
         }
         return numbers;
     }
-    private static int createNumber() {
+    private int createNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
 }
