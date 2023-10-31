5aaf9f831f4f9057b71fc02b334f771af7bfa2e3
KangDeok <rkdejr2321@naver.com>
2023-11-01 02:20:43 +0900

    [feat] 경주 결과 출력 기능

--- src/main/java/racingcar/view/SystemMessage.java
+++ src/main/java/racingcar/view/SystemMessage.java
@@ -5,12 +5,27 @@ import racingcar.domain.RacingCar;
 
 public class SystemMessage {
 
+    StringBuilder stringBuilder = new StringBuilder();
+
     public void printRequestInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printRequestTryCountMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printRacingResult(List<RacingCar> racingCars) {
+        System.out.println("실행 결과");
+
+        for (RacingCar racingCar : racingCars) {
+            String carName = racingCar.getCarName();
+            String forward = "-".repeat(racingCar.getForwardCount());
+
+            String result = stringBuilder.append(carName).append(" : ").append(forward).toString();
+
+            System.out.println(result);
+        }
     }
 
     public void printWinnerMessage(List<String> winners) {
