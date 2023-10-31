08a5159336a257aa245fa6600928efe68daa087a
bboodd <92453279+bboodd@users.noreply.github.com>
2023-11-01 00:59:48 +0900

    refactor: inputCarsName change private to public

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -10,16 +10,15 @@ public class RacingGame {
     List<String> cars;
     public void start(){
 
-        cars = inputCarsName();
+        cars = inputCarsName(Console.readLine());
         System.out.println(cars);
     }
 
-    private List<String> inputCarsName(){
+    public List<String> inputCarsName(String names){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> cars = new ArrayList<String>();
-        String line = Console.readLine();
 
-        for(String car : line.split(",")){
+        for(String car : names.split(",")){
             if(car.length() > 5){
                 throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
             }
