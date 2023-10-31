dbbb4fa43d537b85c3245ca5a4130e279d0e8a27
sangjun <sangjun1389@ajou.ac.kr>
2023-10-31 17:20:32 +0900

    feat(OutputHandler): 출력 관련 메소드 기능 구현
    - Judge에서 진행하던 printResult, printWinners 등의 메소드를 이동

--- src/main/java/racingcar/domain/Judge.java
+++ src/main/java/racingcar/domain/Judge.java
@@ -24,4 +24,40 @@ public class Judge {
     protected List<Car> getCars() {
         return cars;
     }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            int randomNumber = NumericUtility.getConvertedNumber();
+            car.move(randomNumber);
+        }
+    }
+
+    public void run() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        setCars(InputHandler.makeCarNamesArray());
+        System.out.println("시도할 회수는 몇회인가요?");
+        numberOfAttempts = InputHandler.makeNumberOfAttempts();
+        System.out.println("\n실행결과");
+        for (int i = 0; i < numberOfAttempts; i++) {
+            moveCars();
+            OutputHandler.printResult(this.cars);
+        }
+        selectWinner();
+    }
+
+    public void selectWinner() {
+        int maxPosition = NumericUtility.findMaxPosition(this.cars);
+        List<String> winners = getWinners(maxPosition);
+        OutputHandler.printWinners(winners);
+    }
+
+    private List<String> getWinners(int maxPosition) {
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
 }
