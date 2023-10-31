a5b25ee0335872092585cb1d533e0dd2a6544ac9
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-01 02:11:44 +0900

    test(CarsValidatorTest): validateDuplicateName 함수 테스트 코드 추가

---
+++ src/test/java/racingcar/util/CarsValidatorTest.java
@@ -0,0 +1,25 @@
+package racingcar.util;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+public class CarsValidatorTest extends NsTest {
+    @Test
+    @DisplayName("validateDuplicateName 함수 기능 테스트")
+    void 중복된_자동차_이름에_대한_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bora,bora", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
