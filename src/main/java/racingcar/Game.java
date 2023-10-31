acb303566415d622831354a0c83e430b7a016ea7
Lee9Bin <imagen33@daum.net>
2023-11-01 00:06:04 +0900

    feat : 경주를 시작하고 진행하는 기능 완성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,11 +7,35 @@ import java.util.List;
 
 public class Game {
     List<Car> cars = new ArrayList<>();
+    NumberGenerator numberGenerator = new NumberGenerator();
 
     public void save(String[] carNames){
         for (String name : carNames){
             Car car = new Car(name);
             cars.add(car);
         }
+    }
+
+    public void giveRandomNumber(){
+        for(Car car : cars){
+            int randomNumber = numberGenerator.makeRandomNumber();
+            car.go(randomNumber);
+        }
+    }
+
+    public void play(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        save(readLine().split(","));
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = Integer.parseInt(readLine());
+        Narrator narrator = new Narrator();
+        for(int i = 0; i<count; i++){
+            giveRandomNumber();
+            narrator.showEachRound(cars);
+        }
+        Referee referee = new Referee();
+        System.out.print("최종 우승자 : ");
+        referee.showWinners(cars);
+
     }
 }
