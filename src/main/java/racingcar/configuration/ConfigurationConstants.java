5f6a552bf583fa6d3737b8200a75ba22c157beee
june-777 <wlwhswnsrl96@gmail.com>
2023-10-31 16:31:00 +0900

    feat(ConfigurationConstants): 클래스 추가

---
+++ src/main/java/racingcar/configuration/ConfigurationConstants.java
@@ -0,0 +1,11 @@
+package racingcar.configuration;
+
+public class ConfigurationConstants {
+    public static final int TOTAL_CAR_MIN_LIMIT = 2;
+    public static final int TOTAL_CAR_MAX_LIMIT = 10;
+
+    public static final int TOTAL_ROUND_MIN_LIMIT = 1;
+    public static final int TOTAL_ROUND_MAX_LIMIT = 10;
+
+    public static final int CAR_NAME_LENGTH_MAX_LIMIT = 5;
+}
