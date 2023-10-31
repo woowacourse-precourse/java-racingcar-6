31b510a243e90e5c7ddfdfa32101bebd2e33d1ed
ChoiseU <csw980928@gmail.com>
2023-11-01 00:48:34 +0900

    refactor : 메서드명 변경으로 인한 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         RacingGameController racingGame = new RacingGameController();
-        racingGame.gameStart();
+        racingGame.game();
     }
 }
