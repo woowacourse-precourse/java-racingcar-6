d85c623acce5c09eae6152de6dc6f8803f5459f8
devholic22 <hyunjoon.tech@gmail.com>
2023-10-31 12:21:25 +0900

    refactor: CarsTest에 Nested 적용
    Nested 어노테이션을 적용함으로써 같은 맥락의 테스트를 구조화

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -6,17 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static racingcar.model.exception.ExceptionMessage.DUPLICATE_CAR_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class CarsTest {
-
-    @Test
-    @DisplayName("자동차 이름이 모두 독립적이면 정상 생성된다.")
-    void createCarsTest() {
-        // when & then
-        assertDoesNotThrow(() -> Cars.from("john,bob,alice"));
-    }
 
     @Test
     @DisplayName("쉼표를 기준으로 자동차가 생성된다.")
@@ -29,11 +23,23 @@ public class CarsTest {
         assertThat(playCars.size()).isEqualTo(3);
     }
 
-    @Test
-    @DisplayName("같은 이름을 가진 자동차가 있으면 예외가 발생한다.")
-    void duplicateNameCarExceptionTest() {
-        // when & then
-        assertThatThrownBy(() -> Cars.from("john,john,bob")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(DUPLICATE_CAR_EXCEPTION.getMessage());
+    @Nested
+    @DisplayName("자동차 이름 독립/중복 테스트")
+    class duplicateCarsTest {
+
+        @Test
+        @DisplayName("자동차 이름이 모두 독립적이면 정상 생성된다.")
+        void createCarsTest() {
+            // when & then
+            assertDoesNotThrow(() -> Cars.from("john,bob,alice"));
+        }
+
+        @Test
+        @DisplayName("같은 이름을 가진 자동차가 있으면 예외가 발생한다.")
+        void duplicateNameCarsExceptionTest() {
+            // when & then
+            assertThatThrownBy(() -> Cars.from("john,john,bob")).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(DUPLICATE_CAR_EXCEPTION.getMessage());
+        }
     }
 }
