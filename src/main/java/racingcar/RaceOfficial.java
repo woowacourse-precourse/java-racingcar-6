24a2b2aeaf8b59f0d93cafc39b4e6f31e05b16e7
ParkJinYong <mej1406@gmail.com>
2023-10-31 20:09:10 +0900

    feat: 심판 클래스 추가, 함수 틀 작성

--- src/main/java/racingcar/RaceOfficial.java
+++ src/main/java/racingcar/RaceOfficial.java
@@ -1,2 +1,21 @@
-package racingcar;public class RaceOfficial {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceOfficial {
+    private List<String> winners = new ArrayList<>();
+    private int longestDistance = 0;
+
+    public int findLongestDistance() {
+        return -1;
+    }
+
+    public void findWinners(List<Car> cars) {
+
+    }
+
+    public List<String> getWinners() {
+        return winners;
+    }
 }
