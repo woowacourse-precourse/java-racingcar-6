a4fb6525ff5d5742bacbbf3c6d979d167a713a51
DK <laancer4@gmail.com>
2023-10-31 15:37:08 +0900

    feat : Cars 객체로부터 책임을 분리, 유저 입력 검증을 위한 유틸 클래스 작성

--- src/main/java/car/Cars.java
+++ src/main/java/car/Cars.java
@@ -1,6 +1,6 @@
 package car;
 
-import org.junit.platform.commons.util.StringUtils;
+import console.UserInputValidator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,11 +8,7 @@ import java.util.List;
 
 public class Cars {
     private final List<Car> carList;
-    private static final int CAR_NAME_LIMIT = 5;
-    private static final String INVALID_NAME_LENGTH = "이름은 5자 이하만 가능하다.";
     private static final String SPLIT_DELIMITER = ",";
-    private static final String MUST_NOT_BLANK = "공백 입력은 불가능하다.";
-
     public Cars(String userInput) {
         this.carList = this.initByStringInput(userInput);
     }
@@ -22,25 +18,10 @@ public class Cars {
         return Arrays.stream(carNames)
                 .map(String::trim)
                 .map(carName -> {
-                    validateInput(carName);
+                    UserInputValidator.validate(carName);
                     return new Car(carName);
                 })
                 .toList();
-    }
-
-    private void validateInput(String userInput){
-        this.validateNotBlank(userInput);
-        this.validateLength(userInput);
-    }
-    private void validateNotBlank(String userInput){
-        if (StringUtils.isBlank(userInput)) {
-            throw new IllegalArgumentException(MUST_NOT_BLANK);
-        }
-    }
-    private void validateLength(String userInput) {
-        if (userInput.length() > CAR_NAME_LIMIT) {
-            throw new IllegalArgumentException(INVALID_NAME_LENGTH);
-        }
     }
 
     public List<Car> getCarList() {
