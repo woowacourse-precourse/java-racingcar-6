92362afe6069d7ba5c666dac98aafe01231a67cb
DrivenByCode <jaykey1004@gmail.com>
2023-11-01 01:42:15 +0900

    chore: fix typo error

--- src/main/java/racingcar/constant/ServiceMessage.java
+++ src/main/java/racingcar/constant/ServiceMessage.java
@@ -2,7 +2,7 @@ package racingcar.constant;
 
 public enum ServiceMessage {
     START("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
-    INPUT("시도할 회수는 몇회인가요?\n"),
+    INPUT("시도할 횟수는 몇회인가요?\n"),
     OUTPUT("실행 결과\n"),
     WINNER("최종 우승자 :");
     public String serviceMessage;
