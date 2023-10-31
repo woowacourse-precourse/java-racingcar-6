6b62e0a3ab587b075e428f9cc1710476af543bfa
Montep95 <sewd1224@gmail.com>
2023-10-31 20:05:39 +0900

    [refactor] Game Start (Stadium), InputView
    Add GAME_START_MESSAGE, INPUT_ATTEMPT
    Add start() - Manage Game Process
    moving() - move or stop Decision
    Connect domain and view

--- src/main/java/racingcar/controller/Stadium.java
+++ src/main/java/racingcar/controller/Stadium.java
@@ -1,9 +1,39 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Winner;
+import racingcar.dto.WinnerDto;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Stadium {
+
+    private final Cars cars = new Cars();
+
+    private int attempt;
     public void start(){
-        //game();
-        //OutputView.result(winners);
+        cars.join(InputView.getInputCar());
+        attempt = InputView.getInputAttempt();
+        game();
+        WinnerDto winners = Winner.decision(cars);
+
+        OutputView.result(winners);
+
+    }
+
+    private void game(){
+        OutputView.printGameStartMessage();
+        for(int round = 0; round < attempt; round++){
+            moving();
+            OutputView.currentRacingPosition(cars);
+        }
+    }
+
+    private void moving(){
+        for(Car driver : cars.getCars()){
+            driver.choice();
+        }
     }
 
 }
