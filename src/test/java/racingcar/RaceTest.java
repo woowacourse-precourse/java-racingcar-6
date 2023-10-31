f4f624ff33a4db964728a3d5c040a43f27afd04b
kihwn <kihwn98@gmail.com>
2023-10-31 21:25:36 +0900

    Test: Race클래스를 테스트할 RaceTest 생성
    레이스에 참가하는 자동차 중 현재 위치가 가장 큰 값을 가지는 자동차가 최종 winner로 출력되는지 확인하는 테스트 & 최종 winner가 여러 개일 경우 winner 모두가 출력되는 지 확인하는 테스트 진행

---
+++ src/test/java/racingcar/RaceTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RaceTest {
+
+    @Test
+    void findFinalWinner_메서드로_raceAccumulations가_가장_큰_car가_출력되어야_한다() {
+        Race race = Race.initializeRaceInformation("1,2,3", 0, Arrays.asList(10,2,3));
+        List<String> finalWinners = race.findFinalWinner();
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("1");
+
+        assertEquals(expectedList, finalWinners);
+    }
+
+
+    @Test
+    void 가장_큰_car가_여러_개라면_여러_개_전부_출력되어야_한다() {
+        Race race = Race.initializeRaceInformation("1,2,3", 0, Arrays.asList(10,10,3));
+        List<String> finalWinners = race.findFinalWinner();
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("1");
+        expectedList.add("2");
+
+        assertEquals(expectedList, finalWinners);
+    }
+
+
+}
