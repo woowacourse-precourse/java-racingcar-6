83411edf6735b13ffed5be9eace090915d1c0145
jinkshower <askthem@naver.com>
2023-10-31 14:43:16 +0900

    feat(InputView): 중복된 이름 예외처리 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.util.Utils.splitString;
+import static racingcar.util.Validator.validateDuplication;
 import static racingcar.view.OutputView.printGameNotice;
 import static racingcar.view.constants.GameNotice.PROMPT_CAR_NAMES;
 import static racingcar.view.constants.GameNotice.PROMPT_ROUNDS;
@@ -11,7 +12,9 @@ import java.util.List;
 public class InputView {
     public static List<String> askCarNames() {
         printGameNotice(PROMPT_CAR_NAMES);
-        return splitString(readLine());
+        List<String> splitUserInput = splitString(readLine());
+        validateDuplication(splitUserInput);
+        return splitUserInput;
     }
 
     public static String askHowManyRounds() {
--- src/main/java/racingcar/view/constants/GameNotice.java
+++ src/main/java/racingcar/view/constants/GameNotice.java
@@ -1,9 +1,9 @@
 package racingcar.view.constants;
 
 public enum GameNotice {
-    PROMPT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    PROMPT_ROUNDS("시도할 회수는 몇회인가요?"),
-    EXECUTION_RESULT("실행 결과"),
+    PROMPT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
+    PROMPT_ROUNDS("시도할 회수는 몇회인가요?\n"),
+    EXECUTION_RESULT("실행 결과\n"),
     FINAL_WINNER("최종 우승자 : ");
 
     private final String message;
