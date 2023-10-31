de9bcf893884d9b28f59ee7ab5a9cd26f40ccf05
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-31 19:09:36 +0900

    test: 출력 관련 객체 테스트

---
+++ src/test/java/racingcar/io/OutputManagerTest.java
@@ -0,0 +1,68 @@
+package racingcar.io;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.config.GameMessages;
+import racingcar.dto.CarStateDto;
+import racingcar.dto.GameResultDto;
+import racingcar.dto.RoundResultDto;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OutputManagerTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private OutputManager outputManager = new OutputManager();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testPrintCarNameInputMessage() {
+        outputManager.printCarNameInputMessage();
+        assertEquals(GameMessages.INPUT_CAR_NAME + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void testPrintTryCountInputMessage() {
+        outputManager.printTryCountInputMessage();
+        assertEquals(GameMessages.INPUT_TRY_COUNT + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void testPrintExecutionResultMessage() {
+        outputManager.printExecutionResultMessage();
+        assertEquals(GameMessages.EXECUTION_RESULT + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void testPrintRoundResult() {
+        RoundResultDto roundResultDto = new RoundResultDto(Arrays.asList(
+                new CarStateDto("car1 : ----"),
+                new CarStateDto("car2 : --")
+        ));
+        outputManager.printRoundResult(roundResultDto);
+        String expectedOutput = "car1 : ----" + System.lineSeparator() + "car2 : --" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void testPrintFinalWinner() {
+        GameResultDto gameResultDto = new GameResultDto(Collections.singletonList("car1"));
+        outputManager.printFinalWinner(gameResultDto);
+        assertEquals(GameMessages.FINAL_WINNER + "car1" + System.lineSeparator(), outContent.toString());
+    }
+}
