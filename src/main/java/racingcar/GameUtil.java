18817e781cf34af040ae84bec1b1a1366138157c
JGM <cnxw4570123@gmail.com>
2023-11-01 00:04:07 +0900

    add : add getCarName(), getMoveCount() to GameUtil class GameUtil 클래스 - 차량 이름을 입력받는 getCarName() 메소드와 움직일 회수를 입력받는 getMoveCount() 메소드 추가

--- src/main/java/racingcar/GameUtil.java
+++ src/main/java/racingcar/GameUtil.java
@@ -1,10 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameUtil {
+
+    public String getCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return Console.readLine();
+    }
+
+    public int getMoveCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        int res;
+        try{
+            String input = Console.readLine();
+            res = Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+        return res;
+    }
 
     public void printWinner(List<Racer> winners) {
         System.out.print("최종 우승자 : ");
