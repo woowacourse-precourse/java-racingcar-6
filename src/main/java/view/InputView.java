b467c31388b23a243cae067afbb903c901d09017
nayonsoso <panda0329@naver.com>
2023-10-31 22:38:46 +0900

    feat: 자동차 이름이 5자 이하인지 검증하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,8 +12,8 @@ public class InputView {
         }
     }
 
-    private void checkNotEndsWithComma(String userInput){
-        if(userInput.endsWith(",")){
+    private void checkNotEndsWithComma(String userInput) {
+        if (userInput.endsWith(",")) {
             throw new IllegalArgumentException("마지막이 쉼표로 끝납니다.");
         }
     }
@@ -21,13 +21,22 @@ public class InputView {
     private void checkContainsRepeatingCommas(String userInput) {
         String ignoreSpace = ignoreSpace(userInput);
         for (int i = 0; i < ignoreSpace.length() - 1; i++) {
-            if (ignoreSpace.charAt(i) == ',' && ignoreSpace.charAt(i+1) == ',') {
+            if (ignoreSpace.charAt(i) == ',' && ignoreSpace.charAt(i + 1) == ',') {
                 throw new IllegalArgumentException("쉼표 사이에 값이 없습니다.");
             }
         }
     }
 
-    private String ignoreSpace(String userInput){
+    private String ignoreSpace(String userInput) {
         return userInput.replace(" ", "");
+    }
+
+    private void checkNameLength(String[] names, int maxLength) {
+        for (String name : names) {
+            if (name.length() > maxLength) {
+                String message = "이름의 길이가 " + maxLength + "자가 넘습니다.";
+                throw new IllegalArgumentException(message);
+            }
+        }
     }
 }
