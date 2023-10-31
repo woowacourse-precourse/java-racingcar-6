7a1aaf2de7f6dfc9b19c4d039eb70c661060b11d
bonooy <choiboyuna@gmail.com>
2023-10-31 12:47:52 +0900

    Feat: 자동차 이름과 위치를 위한 Car domain 기능 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+}
