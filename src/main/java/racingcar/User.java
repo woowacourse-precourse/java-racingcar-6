754a2fa95e6c5fb4333e72e1e4167a3e038883ff
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-31 16:17:28 +0900

    refactor: [User] 함수 이름에 맞는 기능만을 할 수 있게 함수 내부를 분리

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -13,19 +13,26 @@ public class User {
     }
 
     public List<String> createCarNames(String input) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        validator.validateInput(input);
         String[] inputCarNames = split(input);
         return toList(inputCarNames);
     }
 
     public int createMovingNumber(String input) {
-        System.out.println("시도할 회수는 몇회인가요?");
         return toInt(input);
     }
 
-    public String input() {
-        return Console.readLine();
+    public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarNames = Console.readLine();
+        validator.validateInput(inputCarNames);
+        return inputCarNames;
+    }
+
+    public String inputNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputMovings = Console.readLine();
+        validator.validateMovingNumber(inputMovings);
+        return inputMovings;
     }
 
     private String[] split(String carName) {
@@ -40,7 +47,6 @@ public class User {
 
     private int toInt(String input) {
         System.out.println();
-        validator.validateMovingNumber(input);
         return Integer.parseInt(input);
     }
 }
