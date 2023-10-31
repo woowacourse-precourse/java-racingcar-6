3e351e4ee70e8b21ad38fc39896f7069b18a335f
chaeshee0908 <chaeshee0908@naver.com>
2023-10-31 18:35:38 +0900

    [feat] 랜덤숫자 4미만 수에서 움직이지 않음 테스트 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,8 +1,13 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import racingcar.util.RandomNumber;
+import org.mockito.Mockito;
 
 public class CarTest {
 
@@ -13,4 +18,15 @@ public class CarTest {
         assertThat(car.getName()).isEqualTo(name);
     }
 
+    @Test
+    void 랜덤숫자_4미만_수에서_움직이지_않음_확인() {
+        // 모의 객체(Mock)을 사용하여 RandomNumber.createRandomNumber()의 결과 조작
+        mockStatic(RandomNumber.class);
+        when(RandomNumber.createRandomNumber()).thenReturn(2);
+
+        Car car = new Car("pobi");
+        car.move();
+
+        assertEquals(0, car.getMoveDistance());
+    }
 }
