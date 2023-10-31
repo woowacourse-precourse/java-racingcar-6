9a9f3a7d0230af123632b7c74fb3cc26bc9fb45d
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 17:13:40 +0900

    feat: List 구조를 Map구조로 변환하는 기능 구현

--- src/main/java/racingcar/domain/Splitter.java
+++ src/main/java/racingcar/domain/Splitter.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Splitter {
     //자동차 이름은 쉼표(,)를 기준으로 구분한다.
@@ -9,5 +11,15 @@ public class Splitter {
         List<String> list = Arrays.asList(carNameLine.split(","));
 
         return list;
+    }
+
+    public Map<String, Integer> listToMap(List<String> list) {
+        Map<String, Integer> resultMap = new HashMap<>();
+
+        for (String element : list) {
+            resultMap.put(element, 0);
+        }
+
+        return resultMap;
     }
 }
