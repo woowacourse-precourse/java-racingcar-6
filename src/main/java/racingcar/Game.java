13e147d10066ed57f4c9d790b293fecefc457137
Lee9Bin <imagen33@daum.net>
2023-11-01 00:36:39 +0900

    feat : 자동차 이름에 대한 예회처리 기능 완성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -10,9 +10,25 @@ public class Game {
     NumberGenerator numberGenerator = new NumberGenerator();
 
     public void save(String[] carNames){
-        for (String name : carNames){
-            Car car = new Car(name);
+        for (String carName : carNames){
+            checkNameLength(carName);
+            isCharacter(carName);
+            Car car = new Car(carName);
             cars.add(car);
+        }
+    }
+
+    public void checkNameLength(String carName){
+        if (carName.length() > 5){
+            throw new IllegalArgumentException("이름이 5자 이하만 가능합니다.");
+        }
+    }
+
+    public void isCharacter(String carName){
+        for (int i = 0; i<carName.length(); i++){
+            if(Character.isDigit(carName.charAt(i))){
+                throw new IllegalArgumentException("이름에는 숫자가 들어가면 안됩니다.");
+            }
         }
     }
 
@@ -26,8 +42,10 @@ public class Game {
     public void play(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         save(readLine().split(","));
+
         System.out.println("시도할 회수는 몇회인가요?");
         int count = Integer.parseInt(readLine());
+
         Narrator narrator = new Narrator();
         for(int i = 0; i<count; i++){
             giveRandomNumber();
