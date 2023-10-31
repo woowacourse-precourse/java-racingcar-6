2ac3113af8c33d044af607789d75319e1b66b5c9
dmdrk1414 <seungchan141414@gmail.com>
2023-10-31 20:46:46 +0900

    [Style(racing)] 주석 수정

--- src/main/java/racingcar/domain/GameHost.java
+++ src/main/java/racingcar/domain/GameHost.java
@@ -10,12 +10,6 @@ public class GameHost {
         this.judgeStandard = new JudgeStandard();
     }
 
-    /**
-     * 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다.
-     *
-     * @param raceCarList 현제 진행중인 RaceCarList
-     * @return
-     */
     public List<RaceCar> giveWinnerList(List<RaceCar> raceCarList) {
         List<RaceCar> winnerList = new ArrayList<>();
         int cntOfMaxMove = getMaxCntMove(raceCarList);
@@ -28,17 +22,10 @@ public class GameHost {
                 winnerList.add(raceCar);
             }
         }
-        
+
         return winnerList;
     }
 
-    /**
-     * 현제 레이싱을 하는 자동차의 움직인 횟수중에
-     * 최고로 많은 횟수를 알려준다.
-     *
-     * @param progressCarList
-     * @return
-     */
     public Integer getMaxCntMove(List<RaceCar> progressCarList) {
         int max = progressCarList.stream()
                 .mapToInt(RaceCar::getCntMovementOfCar)
