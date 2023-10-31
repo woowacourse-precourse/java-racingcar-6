e1e51f55c9becfcdcfa3f8fdf89b2860000e6b6e
styughjvbn <3338761@naver.com>
2023-11-01 01:24:20 +0900

    <chore> : 필요없는 import문 제거 ----------------------- 1. 필요없는 import문 제거

--- src/test/java/racingcar/NumTest.java
+++ src/test/java/racingcar/NumTest.java
@@ -1,10 +1,8 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import game.NumOfAttempt;
-import game.car.CarName;
 import org.junit.jupiter.api.Test;
 
 public class NumTest {
@@ -28,6 +26,7 @@ public class NumTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("회수는 숫자만 입력해야합니다.");
     }
+
     @Test
     void NumOfAttempt_숫자가_아닌_값_입력시_예외_발생() {
         assertThatThrownBy(() -> new NumOfAttempt("56d"))
