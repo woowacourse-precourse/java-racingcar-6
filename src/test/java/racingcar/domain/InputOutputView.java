c1d2a2fa6a6133a6dac539d12edfcb38c8d23d03
Choijake <shameless8@naver.com>
2023-10-31 16:25:16 +0900

    Feat : 입출력 관련 메시지 뷰 클래스 작성

---
+++ src/test/java/racingcar/domain/InputOutputView.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import racingcar.Constants;
+
+public class InputOutputView {
+    public static String carInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String roundInput() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
+
+    public static void printPlayMessage() {
+        System.out.println(Constants.ROUND_RESULT);
+    }
+
+    public static void printRoundResult(ArrayList<String> roundResults) {
+        roundResults.forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printWinner(ArrayList<String> winners) {
+        System.out.println(Constants.WINNER_RESULT + String.join(Constants.DELIMITER_WINNER, winners));
+    }
+
+}
