b85c218e4e7baf8aed5e471e1c2e311e63ac1d4b
donghyun <kdhdh001@naver.com>
2023-11-01 01:34:26 +0900

    feat: 자동차 이름이 공백만있는 경우 테스트 구현

--- src/test/java/racingcar/validation/CarNameValidatorTest.java
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -15,4 +15,13 @@ public class CarNameValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름이 공백이면 안됩니다.");
     }
+
+    @Test
+    public void 자동차_이름이_공백만_있는_경우_검증() {
+        String input = "    ";
+        List<String> carNames = Arrays.asList(input.split(","));
+        assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름이 공백이면 안됩니다.");
+    }
 }
