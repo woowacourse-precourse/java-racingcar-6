2ca2556aed88b455165c7ddbf3f8ad86913eacc6
leeminjun <sohwi0305@gmail.com>
2023-10-31 13:29:29 +0900

    feat: 공백값 추가

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -3,5 +3,5 @@ package racingcar.util;
 public class Exception {
     private static final int CAR_NAME_LIMIT_MIN = 1;
     private static final int CAR_NAME_LIMIT_MAX = 5;
-
+    private static final String SPACE = " ";
 }
