4b5680f5cfed8b23269f361f7e06de120ef38af0
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-10-31 15:33:08 +0900

    feat: 사용자 입력, 결과 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String inputcars = Console.readLine();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int number = Integer.parseInt(Console.readLine());
+
+        List<String> racingcar = Arrays.asList(inputcars.split(","));
+        Racing.createCars(racingcar);
+
+        System.out.println("실행결과");
+        for (int i = 0; i < number; i++) {
+            Racing.racing();
+        }
+
+        System.out.print("최종 우승자 : " + Racing.result());
     }
 }
