b460ba62df2ff54f4568cdee07c628fbf61020fd
Joohui <joohuii96@gmail.com>
2023-10-31 17:28:55 +0900

    test: StringToList 타입 컨버터 테스트

---
+++ src/test/java/racingcar/common/ConverterTest.java
@@ -0,0 +1,23 @@
+package racingcar.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ConverterTest {
+
+    @ParameterizedTest(name = "carNames=\"{0}\"")
+    @ValueSource(strings = {"pobi,jun", " pobi , jun"})
+    void stringToList(String carNames) {
+        StringToListConverter converter = new StringToListConverter();
+        List<String> carNameList = converter.convert(carNames);
+        assertAll(
+                () -> assertEquals(2, carNameList.size()),
+                () -> assertEquals(true, carNameList.contains("pobi")),
+                () -> assertEquals(true, carNameList.contains("jun"))
+        );
+    }
+}
