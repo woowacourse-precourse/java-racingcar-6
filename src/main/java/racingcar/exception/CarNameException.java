7ee3fb646fb1c3d9a521e746d0698847061ca5b8
dragon0622 <iamdragon.dev@gmail.com>
2023-10-31 17:31:37 +0900

    feat(racingCar): making skeleton code for exception

---
+++ src/main/java/racingcar/exception/CarNameException.java
@@ -0,0 +1,20 @@
+package racingcar.exception;
+
+public class CarNameException {
+
+    public static void carNumberLengthException(){
+
+    }
+
+    public static void carNumberSameException(){
+
+    }
+
+    public static void lotsOfCommaException(){
+
+    }
+
+    public static void blankException(){
+
+    }
+}
---
+++ src/main/java/racingcar/exception/RoundException.java
@@ -0,0 +1,8 @@
+package racingcar.exception;
+
+public class RoundException {
+
+    public static void noIntRoundException(){
+
+    }
+}
