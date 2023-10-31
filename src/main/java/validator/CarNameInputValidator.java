1467c661e062036ff561e8c72c31c5cf421aee58
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-01 03:42:33 +0900

    feat: 자동차 이름의 중복 확인을 위한 vaildCarNameDuplication 함수 생성

--- src/main/java/validator/CarNameInputValidator.java
+++ src/main/java/validator/CarNameInputValidator.java
@@ -1,11 +1,24 @@
 package validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CarNameInputValidator {
-    void vaildCarNameLength(String[] carNames) {
+    public void vaildCarNameLength(String[] carNames) {
         for (String name: carNames) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능해요.");
             }
+        }
+    }
+
+    public void vaildCarNameDuplication(String[] carNames) {
+        Set<String> nameSet = new HashSet<>();
+        for (String name: carNames) {
+            if (nameSet.contains(name)) {
+                throw new IllegalArgumentException("자동차 이름은 중복이 되면 안돼요.");
+            }
+            nameSet.add(name);
         }
     }
 }
