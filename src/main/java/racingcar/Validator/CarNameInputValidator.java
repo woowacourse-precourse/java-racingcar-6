412609f65c5f70cb9c2f492c9e65cda10a79c669
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-31 18:06:00 +0900

    feat: 공백 입력, 특수문자 및 숫자 입력 검증 로직 추가

--- src/main/java/racingcar/Validator/CarNameInputValidator.java
+++ src/main/java/racingcar/Validator/CarNameInputValidator.java
@@ -8,15 +8,19 @@ public class CarNameInputValidator {
     private final String ASK_MORE_THAN_TWO = "2개 이상의 차 이름을 입력하세요";
     private final String ASK_NO_DUPLICATION = "중복된 이름을 입력 불가합니다";
     private final String ASK_VALID_NAME_LENGTH = "차 이름은 5자 이하로 입력해주세요";
+    private final String ASK_ONLY_CHARACTER_INPUT = "쉼표로 구분된 차 이름은 문자(알파벳, 한극)로만 입력 가능합니다";
+    private final String ASK_ONE_SPACE_BETWEEN_CHAR = "문자와 문자 사이이 공백은 한 칸까지만 가능합니다";
+    private final String ASK_NO_SPACE_IN_START_OR_END_POSITION = "이름의 앞 뒤에 공백 없이 쉼표(,)로 구분해서 입력해주세요";
 
     private int MINIMUM_PARTICIPANT = 2;
     private int MAXIMUM_NAME_LENGTH = 5;
 
-
-
     public void validate(String carNameInput) {
-        List<String> carNameInputList = Arrays.asList(carNameInput.split(","))
+        List<String> carNameInputList = Arrays.asList(carNameInput.split(","));
         areNamesMoreThanTwo(carNameInputList);
+        isNameStartOrEndWithSpace(carNameInputList);
+        isNameOnlyWithChar(carNameInputList);
+        isNameWithConSequenceSpaces(carNameInputList);
         isNameLengthBelowFive(carNameInputList);
         isNameNotDuplicated(carNameInputList);
     }
@@ -32,6 +36,32 @@ public class CarNameInputValidator {
             if (carName.length() > MAXIMUM_NAME_LENGTH) {
                 throw new IllegalArgumentException(ASK_VALID_NAME_LENGTH);
             }
+        }
+    }
+
+    public void isNameOnlyWithChar(List<String> carNameInputList) throws IllegalArgumentException {
+        for (String carName : carNameInputList) {
+            if (carName.matches("^[a-zA-Z가-힣 ]*$")) {
+                return;
+            }
+            throw new IllegalArgumentException(ASK_ONLY_CHARACTER_INPUT);
+        }
+    }
+
+    public void isNameStartOrEndWithSpace(List<String> carNameInputList) throws IllegalArgumentException {
+        for (String carName : carNameInputList) {
+            if (carName.startsWith(" ") || carName.endsWith(" ")) {
+                throw new IllegalArgumentException(ASK_NO_SPACE_IN_START_OR_END_POSITION);
+            }
+        }
+    }
+
+    public void isNameWithConSequenceSpaces(List<String> carNameInputList) throws IllegalArgumentException {
+        for (String carName : carNameInputList) {
+            if (carName.matches("^(?!.*  )[\\s\\S]*$")) {
+                return;
+            }
+            throw new IllegalArgumentException(ASK_ONE_SPACE_BETWEEN_CHAR);
         }
     }
 
