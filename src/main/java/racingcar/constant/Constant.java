83c34f52c5d967d3f4caea329f6546f87005b09c
minjong kim <godminjong@naver.com>
2023-10-31 15:11:39 +0900

    refact: 콘솔 출력에 대한 상수 문자열 정의함.
    콘술 출력에 있어 고정적으로 등장하는 문구를 상수로 정의하여 사용함.

---
+++ src/main/java/racingcar/constant/Constant.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public abstract class Constant {
+    public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    public static final String ROUND_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String EXECUTE_RESULT = "실행 결과";
+    public static final String INITIAL_LIST = "init";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+}
