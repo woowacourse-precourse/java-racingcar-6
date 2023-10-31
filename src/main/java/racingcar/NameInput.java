8abc675dd9b989ad961931425ced4eb48ef4e8ff
parkheeddong <phj7214go@gmail.com>
2023-10-31 20:14:16 +0900

    feat : 이름 입력의 유효성 체크하는 기능 구현

--- src/main/java/racingcar/NameInput.java
+++ src/main/java/racingcar/NameInput.java
@@ -11,6 +11,12 @@ public class NameInput {
         return nameArr;
     }
 
-
+    static void checkNameInput(String[] nameArr) throws IllegalArgumentException {     // 이름을 체크하는 함수
+        for (String name: nameArr) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
 
 }
