2920bf96c687ff4b49fb2c7f350b02ff346dea01
SangHee Lee <lsh277604@gmail.com>
2023-10-31 14:11:20 +0900

    Feat: Add basic functionality to Game class

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private List<Car> playingCar = new ArrayList<Car>();
+    private int playtime;
+
+    public void startGame() {
+
+    }
+    public void runGame(){
+
+    }
+
+    public List<String> parsingInput(String input){
+        return null;
+    }
+    public void validateInput(List<String> parsedInput){
+
+    }
+    public void validateInput(int inputPlaytime){
+
+    }
+
+    public List<Integer> compareCarPosition(){
+        return null;
+    }
+
+}
