274705435f4b7ac7b820f89d7595afe589a6633e
bohyeon <980926a@gmail.com>
2023-10-31 16:01:46 +0900

    feat: MVC 구조로 프로젝트 디렉토리 구성

---
+++ src/main/java/racingcar/controller/controller.java
@@ -0,0 +1,8 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class controller {
+
+}
---
+++ src/main/java/racingcar/util/parser.java
@@ -0,0 +1,8 @@
+package racingcar.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class parser {
+}
