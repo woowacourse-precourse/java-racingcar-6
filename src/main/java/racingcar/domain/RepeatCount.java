727b1f99fabfc6733a23a95a11fe0ae55f4fb115
twoosky <lhjae3@naver.com>
2023-10-31 19:23:32 +0900

    feat: 시도 횟수 검증 로직 구현

---
+++ src/main/java/racingcar/domain/RepeatCount.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import static racingcar.utils.ErrorMessages.REPEAT_COUNT_NOT_ZERO;
+
+public class RepeatCount {
+    private int count;
+
+    public RepeatCount(int count) {
+        validateZero(count);
+        this.count = count;
+    }
+
+    private void validateZero(int count) {
+        if (count == 0) {
+            throw new IllegalArgumentException(REPEAT_COUNT_NOT_ZERO);
+        }
+    }
+}
