493f8ea2840a380c1f3708468fe898739c05e216
june-777 <wlwhswnsrl96@gmail.com>
2023-10-31 18:38:34 +0900

    refactor(RacingGameServiceTest): 테스트코드 가독성 개선

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -1,10 +1,13 @@
 package racingcar.service;
 
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.dto.OneGameResultsDto;
 import racingcar.service.domain.Engine;
 import racingcar.service.domain.RacingCars;
@@ -13,25 +16,28 @@ class RacingGameServiceTest {
 
     static final Integer MOVE = 1;
     static final Integer STOP = 0;
+    static final String racingCarNames = "L!!O,new,j2AnS,H,yp,b 0y";
     static final int CANT_MOVE_EDGE_CONDITION = 3;
     RacingGameService racingGameService = new RacingGameService();
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"june,jayZ,White"})
     @DisplayName("[성공 테스트] 자동차 3대 중 2대만 전진하는 테스트")
-    void playOneTimeCarRaceTest() {
+    void playOneTimeCarRaceTest(String name1, String name2, String name3) {
         // given
-        RacingCars racingCars = new RacingCars(List.of("june", "jayZ", "White"), new CustomEngine());
+        RacingCars racingCars = new RacingCars(List.of(name1, name2, name3), new CustomEngine());
         // when
         OneGameResultsDto resultDto = racingGameService.playOneTimeCarRace(racingCars);
         // then
-        Assertions.assertThat(resultDto.getTotalMoveCounts()).containsExactly(MOVE, STOP, MOVE);
+        Assertions.assertThat(resultDto.getTotalPositions()).containsExactly(MOVE, STOP, MOVE);
     }
 
     @Test
     @DisplayName("최종 우승자 판별 기능 테스트. 경주 1회 진행")
     void calculateFinalWinnersTest1() {
         // given
-        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"), new CustomEngine2());
+        List<String> carNameList = Arrays.asList(racingCarNames.split(","));
+        RacingCars racingCars = new RacingCars(carNameList, new CustomEngine2());
         // when
         racingGameService.playOneTimeCarRace(racingCars);
         List<String> winners = racingGameService.calculateFinalWinners(racingCars);
@@ -43,7 +49,8 @@ class RacingGameServiceTest {
     @DisplayName("최종 우승자 판별 기능 테스트. 경주 3회 진행")
     void calculateFinalWinnersTest2() {
         // given
-        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"), new CustomEngine2());
+        List<String> carNameList = Arrays.asList(racingCarNames.split(","));
+        RacingCars racingCars = new RacingCars(carNameList, new CustomEngine2());
         int totalRound = 3;
         // when
         for (int round = 0; round < totalRound; round++) {
@@ -58,8 +65,8 @@ class RacingGameServiceTest {
     @DisplayName("최종 우승자 판별 기능 테스트. 모든 자동차가 움직이지 못하는 경우")
     void calculateFinalWinnersTest3() {
         // given
-        RacingCars racingCars = new RacingCars(List.of("L!!O", "new", "j2AnS", "H", "yp", "b 0y"),
-                () -> CANT_MOVE_EDGE_CONDITION);
+        List<String> carNameList = Arrays.asList(racingCarNames.split(","));
+        RacingCars racingCars = new RacingCars(carNameList, () -> CANT_MOVE_EDGE_CONDITION);
         // when
         racingGameService.playOneTimeCarRace(racingCars);
         List<String> winners = racingGameService.calculateFinalWinners(racingCars);
@@ -68,7 +75,6 @@ class RacingGameServiceTest {
     }
 
     static class CustomEngine implements Engine {
-
         List<Integer> enginePowers = Lists.newArrayList(4, 3, 8);
 
         @Override
@@ -78,7 +84,6 @@ class RacingGameServiceTest {
     }
 
     static class CustomEngine2 implements Engine {
-
         List<Integer> enginePowers = Lists.newArrayList(1, 2, 3, 4, 5, 6);
         List<Integer> enginePowers2 = Lists.newArrayList(0, 0, 9, 6, 3, 4);
         List<Integer> enginePowers3 = Lists.newArrayList(4, 3, 7, 9, 1, 8);
