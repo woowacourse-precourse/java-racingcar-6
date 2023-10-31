b49923cd543037a0151dc41e11d55528be4ad22e
akskflwn <akskflwn@gmail.com>
2023-10-31 14:01:29 +0900

    feat: 시도할 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,9 @@ public class InputView {
     private static final String REQUEST_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)를 기준으로 구분).";
     private static final String INVALID_NAME_ERROR_MESSAGE = "이름은 쉼표(,)로 구분되는 최소 한 개 이상의 문자열이어야 합니다.\";";
     private static final String NAME_DELIMITER = ",";
+    private static final String TRIAL_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String NUMBER_REGEX ="^[1-9]\\d*$";
+    private static final String INVALID_TRIAL_COUNT_MESSAGE = "시도 횟수는 1 이상의 자연수만 가능합니다.";
 
     public static List<String> getNames() {
         System.out.println(REQUEST_NAMES_MESSAGE);
@@ -26,5 +29,17 @@ public class InputView {
         return List.of(input.split(NAME_DELIMITER));
     }
 
+    public static int getTrialCount() {
+        System.out.println(TRIAL_COUNT_MESSAGE);
+        String input = Console.readLine();
+        return isValidTrialCount(input);
+    }
+
+    private static int isValidTrialCount(String input) {
+        if(!input.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException(INVALID_TRIAL_COUNT_MESSAGE);
+        }
+        return Integer.parseInt(input);
+    }
 
 }
