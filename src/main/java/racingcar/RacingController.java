59ebc5422fdf5aa6bf351dffaca2f5e7f0ba1c4b
jihyeok <kidjh@naver.com>
2023-11-01 00:09:03 +0900

    feat : 우승자 출력 수정 및 컨트롤러 메서드 정리

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -9,19 +9,31 @@ public class RacingController {
 
     List<Car> cars = new ArrayList<>();
     int tryCount;
+    int highScore = 0;
+    List<Car> highestCars = new ArrayList<>();
 
     public void gameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputName();
+        inputTryCount();
 
+        System.out.println("실행 결과");
+        resultOutput();
+        findWinner();
+
+    }
+
+    public void inputName() {
         List<String> carNames = List.of(Console.readLine().split(","));
-        carNames.forEach(name -> cars.add(new Car(name)));
-
         // 자동차 이름 검사 로직
         for (String name : carNames) {
             if (name == null || name.isBlank()) throw new IllegalArgumentException();
             if (name.length() > 5) throw new IllegalArgumentException();
         }
+        carNames.forEach(name -> cars.add(new Car(name)));
+    }
 
+    public void inputTryCount() {
         // 시도 회수 검사 로직
         System.out.println("시도할 회수는 몇회인가요?");
         try {
@@ -31,9 +43,10 @@ public class RacingController {
         }
 
         if (tryCount == 0) throw new IllegalArgumentException();
+    }
 
-        System.out.println("실행 결과");
-
+    public void resultOutput() {
+        // 실행 결과 출력 로직
         for (int i = 0; i < tryCount; i++) {
             for (Car car : cars) {
                 car.move();
@@ -45,25 +58,26 @@ public class RacingController {
             }
             System.out.println();
         }
+    }
 
-        List<Car> highestCars = new ArrayList<>();
-        int highScore = 0;
-
+    public void findWinner() {
         for (Car car : cars) {
             if (car.getScore() > highScore) {
                 highScore = car.getScore();
                 highestCars.clear();
                 highestCars.add(car);
-            } else if(car.getScore() == highScore){
+            } else if (car.getScore() == highScore) {
                 highestCars.add(car);
             }
         }
-
+        // 다수의 우승자일 경우 , 추가 로직
         System.out.print("최종 우승자 : ");
-        for (Car car : highestCars) {
-            System.out.print(car.getName());
+        for (int i = 0; i < highestCars.size(); i++) {
+            System.out.print(highestCars.get(i).getName());
+            if (i < highestCars.size() - 1) {
+                System.out.print(",");
+            }
         }
     }
-
 
 }
