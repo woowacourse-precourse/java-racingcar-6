f3c98273da7d65b381fd46eded444b4ad5b22d5f
gahyeon <1000ji1@naver.com>
2023-10-31 16:45:31 +0900

    feat : functions that return location and name

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,4 +4,11 @@ public class Car {
     private String name;
     private int position;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
