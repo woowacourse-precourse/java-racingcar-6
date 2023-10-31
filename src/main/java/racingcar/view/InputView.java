231ea4a571850abd739905ff56db429524cdfbdb
kssumin <201566@jnu.ac.kr>
2023-10-31 11:19:34 +0900

    feat : 이동할 횟수를 입력받도록 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,15 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.support.DividedCarNamesValidator;
+import racingcar.support.TrialValidator;
 
 public class InputView {
 
+	private InputView() {
+		throw new IllegalStateException();
+	}
+
 	private static final String DELIMITER = ",";
 
-	public List<String> readCarNames() {
+	public static List<String> readCarNames() {
 		String names = Console.readLine();
 		String[] dividedCarNames = names.split(DELIMITER);
 		DividedCarNamesValidator.validate(dividedCarNames);
 		return Arrays.asList(dividedCarNames);
+	}
+
+	public static int readTrial() {
+		String trial = Console.readLine();
+		TrialValidator.validate(trial);
+		return Integer.parseInt(trial);
 	}
 }
