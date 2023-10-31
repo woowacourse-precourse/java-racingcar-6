9c13156b6e14bf2982070a0f2ea63a5df31787ab
DaehoSon <thseogh135@gmail.com>
2023-10-31 22:55:15 +0900

    refact: Exception 발생 시 message를 enum(ErrorCode.java)으로 분리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,20 +1,21 @@
 package racingcar;
 
+import racingcar.common.ErrorCode;
+
 public class Validator {
 
-    private static final String INPUT_WRONG_VALUE = "잘못된 입력값입니다.";
     private static final int MAX_CAR_NAME_LENGTH = 5;
 
     public static void validateCarNameArray(String[] carNameArray) throws IllegalArgumentException {
         for (String s : carNameArray) {
             if (s.length() > MAX_CAR_NAME_LENGTH) {
-                throw new IllegalArgumentException(INPUT_WRONG_VALUE);
+                throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_CAR_NAME_LENGTH.getMessage());
             }
             if (s.isEmpty()) {
-                throw new IllegalArgumentException(INPUT_WRONG_VALUE);
+                throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_CAR_NAME_EMPTY.getMessage());
             }
             if (s.equals("null")) {
-                throw new IllegalArgumentException(INPUT_WRONG_VALUE);
+                throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_CAR_NAME_NULL.getMessage());
             }
         }
     }
@@ -23,7 +24,7 @@ public class Validator {
         try {
             Integer.parseInt(moveCountString);
         } catch (Exception e) {
-            throw new IllegalArgumentException(INPUT_WRONG_VALUE);
+            throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_NUMBER.getMessage());
         }
 
     }
