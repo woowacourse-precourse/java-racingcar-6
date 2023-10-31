0902092d8514d697b6b34e758ae3f88cb11e4c68
june-777 <wlwhswnsrl96@gmail.com>
2023-10-31 15:56:33 +0900

    test(Car): 유효하지 않은 이름으로 자동차 생성 테스트

--- src/test/java/racingcar/service/domain/CarTest.java
+++ src/test/java/racingcar/service/domain/CarTest.java
@@ -4,59 +4,94 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
     final static int CAN_MOVE_EDGE_CONDITION = 4;
     final static int CANT_MOVE_EDGE_CONDITION = 3;
 
-    @Test
-    @DisplayName("[성공 테스트] 8번의 움직임 중 4번만 4이상의 수를 생성하여 자동차 4번 움직임")
-    void moveTest() {
-        // given
-        Car car = new Car("junGi", new CustomEngine());
-        int totalMoveOrder = 8;
-        int expectedMoveCount = 4;
-        // when
-        for (int move = 0; move < totalMoveOrder; move++) {
-            car.moveCar();
+    @Nested
+    @DisplayName("자동차 움직임 테스트")
+    class MoveCarTest {
+        @Test
+        @DisplayName("[성공 테스트] 8번의 움직임 중 4번만 4이상의 수를 생성하여 자동차 4번 움직임")
+        void moveTest() {
+            // given
+            Car car = new Car("junGi", new CustomEngine());
+            int totalMoveOrder = 8;
+            int expectedMoveCount = 4;
+            // when
+            for (int move = 0; move < totalMoveOrder; move++) {
+                car.moveCar();
+            }
+            // then
+            Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
         }
-        // then
-        Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
+
+        @Test
+        @DisplayName("[엣지 테스트] 움직임 가능한 최소값 4일 때 자동차 움직임")
+        void moveSuccessEdgeTest() {
+            // given
+            Car car = new Car("junGi", () -> CAN_MOVE_EDGE_CONDITION);
+            int expectedMoveCount = 1;
+            // when
+            car.moveCar();
+            // then
+            Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
+        }
+
+        @Test
+        @DisplayName("[엣지 테스트] 움직임 불가한 최댓값 3일 때 자동차 움직임 불가")
+        void moveFailEdgeTest() {
+            // given
+            Car car = new Car("p", () -> CANT_MOVE_EDGE_CONDITION);
+            int expectedMoveCount = 0;
+            // when
+            car.moveCar();
+            // then
+            Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
+        }
+
+        static class CustomEngine implements Engine {
+
+            List<Integer> enginePowers = Lists.newArrayList(1, 4, 0, 9, 2, 3, 4, 8);
+
+            @Override
+            public int generateNumber() {
+                return enginePowers.remove(0);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("[엣지 테스트] 움직임 가능한 최소값 4일 때 자동차 움직임")
-    void moveSuccessEdgeTest() {
-        // given
-        Car car = new Car("junGi", () -> CAN_MOVE_EDGE_CONDITION);
-        int expectedMoveCount = 1;
-        // when
-        car.moveCar();
-        // then
-        Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
-    }
+    @Nested
+    @DisplayName("자동차 생성 테스트")
+    class CreateCarTest {
+        @ParameterizedTest
+        @ValueSource(strings = {"1,", "j,ung", "JA,Z", "JQA,!", "K,IG!"})
+        @DisplayName("[실패 테스트] 자동차 이름에 콤마가 존재")
+        void commaExistsTest(String name) {
+            Assertions.assertThatThrownBy(() -> new Car(name, new WootecoEngine()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Test
-    @DisplayName("[엣지 테스트] 움직임 불가한 최댓값 3일 때 자동차 움직임 불가")
-    void moveFailEdgeTest() {
-        // given
-        Car car = new Car("p", () -> CANT_MOVE_EDGE_CONDITION);
-        int expectedMoveCount = 0;
-        // when
-        car.moveCar();
-        // then
-        Assertions.assertThat(car.getTotalMoveCount()).isEqualTo(expectedMoveCount);
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {"", " ", "  ", "   ", "    ", "     "})
+        @DisplayName("[실패 테스트] 자동차 이름에 공백만 존재")
+        void blankTest(String name) {
+            Assertions.assertThatThrownBy(() -> new Car(name, new WootecoEngine()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    static class CustomEngine implements Engine {
-
-        List<Integer> enginePowers = Lists.newArrayList(1, 4, 0, 9, 2, 3, 4, 8);
-
-        @Override
-        public int generateNumber() {
-            return enginePowers.remove(0);
+        @ParameterizedTest
+        @ValueSource(strings = {"jungiK", "     !", "K     ", "       Kim", "myNameIsJAZ"})
+        @DisplayName("[실패 테스트] 유효하지 않은 자동차 길이")
+        void outOfLengthTest(String name) {
+            Assertions.assertThatThrownBy(() -> new Car(name, new WootecoEngine()))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
