627a8ba177eb4ca6f6d1d3ab9f2c4ef20f29c0f6
daeun Kong <daeun113@ajou.ac.kr>
2023-10-31 21:58:37 +0900

    feat(setPlayers): 자동차 이름 입력

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,4 +1,11 @@
 package racingcar;
 
 public class RacingCar {
+    private String name;
+    public RacingCar(String name) {
+        this.setName(name);
+    }
+
+    private void setName(String name) {
+    }
 }
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import java.util.ArrayList;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class RacingCarGame {
     int gameCount = 0;
     ArrayList<RacingCar> racingCars = new ArrayList<>();
@@ -17,6 +19,11 @@ public class RacingCarGame {
     }
 
     void setPlayers() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] items = readLine().split(",");
+        for (String item : items) {
+            racingCars.add(new RacingCar(item));
+        }
     }
 
     void setGameCount() {
