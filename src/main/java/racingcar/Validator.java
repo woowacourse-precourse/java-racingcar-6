76558a01427ba025f23eb2844cf8d60f67f2b955
깃wooqqq <sge0225@naver.com>
2023-10-31 17:56:51 +0900

    style: 메서드명 및 오타 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -7,14 +7,14 @@ public class Validator {
     private static final int CAR_NAME_MIN_LENGTH = 1;
 
     public void checkCarName(List<String> input) {
-        checkCarNameLength(input);
+        checkCarNameMaxLength(input);
         checkCarNameMinLength(input);
     }
 
-    private void checkCarNameLength(List<String> carNames) {
+    private void checkCarNameMaxLength(List<String> carNames) {
         for (String carName : carNames) {
             if (carName.length() > CAR_NAME_MAX_LENGTH) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다,");
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
         }
     }
