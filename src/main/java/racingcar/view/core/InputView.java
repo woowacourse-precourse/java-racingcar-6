5c9b094b22e4525ac4903adecb5eb1bcdbc9ae32
JaehongDev <println@kakao.com>
2023-10-31 21:51:18 +0900

    refactor: IO View 메서드 분리

--- src/main/java/racingcar/view/core/InputView.java
+++ src/main/java/racingcar/view/core/InputView.java
@@ -5,17 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class InputView extends ConsoleWriter {
+
     public List<String> readParticipantCarNames() {
         this.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         final var input = Console.readLine();
-        return Arrays.stream(input.split(","))
-                .toList();
+
+        return Arrays.stream(input.split(",")).toList();
     }
 
     public int readExtractLapCount() {
         this.println("시도할 회수는 몇회인가요?");
+
+        return convertStringToInt(Console.readLine());
+    }
+
+    private Integer convertStringToInt(String input) {
         try {
-            return Integer.parseInt(Console.readLine());
+            return Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException("정수만 입력 가능합니다.");
         }
--- src/main/java/racingcar/view/core/OutputView.java
+++ src/main/java/racingcar/view/core/OutputView.java
@@ -6,12 +6,13 @@ import racingcar.view.part.RaceGameComponent.RaceGameResult.TrackPerResult;
 
 public final class OutputView extends ConsoleWriter {
     public void printRaceStartMessage() {
+        this.newLine();
         this.println("실행 결과");
     }
 
     public void printTrackPerResult(List<TrackPerResult> trackPerResults) {
         this.println(trackPerResults.stream()
-                .map((trackPerResult -> String.format("%s : %s", trackPerResult.name(), "-".repeat(trackPerResult.distance()))))
+                .map(TrackPerResult::toResult)
                 .collect(Collectors.joining("\n"))
         );
         this.newLine();
@@ -20,6 +21,6 @@ public final class OutputView extends ConsoleWriter {
 
     public void printWinner(List<String> winners) {
         this.print("최종 우승자 : ");
-        this.print(String.join(",", winners));
+        this.print(String.join(", ", winners));
     }
 }
