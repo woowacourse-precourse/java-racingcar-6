30c77b55563596a36457663eb5ef4c97fda9402f
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-31 22:26:13 +0900

    [FEAT] Car 객체 생성
    car의 이름과 count를 getName(), getCount(), setCount()와 함께 꺼낼 수 있도록 함수 분리

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int count;
+
+    public Car(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
