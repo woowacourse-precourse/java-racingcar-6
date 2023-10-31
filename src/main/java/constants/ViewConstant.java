27260d6281e8c446057b4ac5879e154795cb9d8a
liarreez <liarreez@gmail.com>
2023-10-31 17:59:43 +0900

    refactor : 상수를 따로 클래스로 만들어서 관리하도록 리팩토링
    상수값을 따로 클래스로 만들어서 리팩토링하고 이에 맞춰 각각의 클래스코드 수정

---
+++ src/main/java/constants/ViewConstant.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class ViewConstant {
+    public static final String ONE_MOVE_SYMBOL = "-";
+    public static final String ROUND_SEPARATOR = "\n";
+    public static final String WINNERS_SEPARATOR = ", ";
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,9 @@
 package racingcar.view;
 
+import constants.ViewConstant;
 import java.util.List;
 
 public class OutputView {
-    private static final String ONE_MOVE_SYMBOL = "-";
-    private static final String ROUND_SEPARATOR = "\n";
-    private static final String WINNERS_SEPARATOR = ", ";
-
     public void printInputCarsNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
@@ -20,14 +17,14 @@ public class OutputView {
     }
 
     public void printMoveRecord(String carName, int moveRecord) {
-        System.out.printf("%s : %s\n", carName, ONE_MOVE_SYMBOL.repeat(moveRecord));
+        System.out.printf("%s : %s\n", carName, ViewConstant.ONE_MOVE_SYMBOL.repeat(moveRecord));
     }
 
     public void printRoundSeparator() {
-        System.out.print(ROUND_SEPARATOR);
+        System.out.print(ViewConstant.ROUND_SEPARATOR);
     }
 
     public void printWinners(List<String> winnersName) {
-        System.out.println("최종 우승자 : " + String.join(WINNERS_SEPARATOR, winnersName));
+        System.out.println("최종 우승자 : " + String.join(ViewConstant.WINNERS_SEPARATOR, winnersName));
     }
 }
