bf7154b93724c20090879666a2d82c5c19018f3a
Junobee25 <wnsdhqo@naver.com>
2023-10-31 20:06:21 +0900

    feat(service): racingGameStart 메서드 추가 구현

--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -6,21 +6,31 @@ import racingcar.domain.CarGenerator;
 import racingcar.domain.RandomNumber;
 import racingcar.domain.RandomNumberGenerator;
 
+import java.util.LinkedHashMap;
+
 public class RacingGame {
 
+    Car car = new Car();
     CarGenerator carGenerator = new CarGenerator();
+    RandomNumber randomNumber = new RandomNumber();
+    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    Move move = new Move();
 
     public void racingGameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carGenerator.setCarListToCar();
+        car.setCarList(carGenerator.createCarList());
+        LinkedHashMap<String, String> carList = car.getCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println();
         int numberOfAttempts = Integer.parseInt(Console.readLine());
         int tryValue = 0;
 
         System.out.println("실행 결과");
         while (tryValue < numberOfAttempts) {
-
+            randomNumber.setRandomNumberList(randomNumberGenerator.createRandomNumberList(carList));
+            move.moveTheCar(car.getCarList(), randomNumber.getRandomNumberList());
+            move.stopTheCar(car.getCarList(), randomNumber.getRandomNumberList());
         }
     }
 }
