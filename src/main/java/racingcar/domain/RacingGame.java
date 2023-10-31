61866833adce5235f193a726f4c2f7728807bfa6
Cuzz <rygus9@naver.com>
2023-10-31 22:08:05 +0900

    feat: RacingGame 도메인 개발

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.Optional;
+import racingcar.dto.RacingCarDto;
+
+public class RacingGame {
+    private final List<RacingCar> racingCars;
+
+    public RacingGame(List<String> racingCarNames) {
+        this.racingCars = racingCarNames.stream()
+                .map(RacingCar::new).toList();
+    }
+
+    public List<RacingCarDto> progress() {
+        this.racingCars.forEach(RacingCar::go);
+        return racingCars.stream().map(racingCar ->
+                new RacingCarDto(racingCar.getName(), racingCar.getDistance())
+        ).toList();
+    }
+
+    public List<String> result() {
+        int maxDistance = this.racingCars.stream()
+                .map(RacingCar::getDistance)
+                .max(Integer::compare).orElse(0);
+
+        return this.racingCars.stream()
+                .filter(racingCar -> racingCar.getDistance() == maxDistance)
+                .map(RacingCar::getName).toList();
+    }
+}
