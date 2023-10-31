0da381eeb5b174b6ec801337b4a8636633e596ba
Nara Park <skfk6352@naver.com>
2023-11-01 06:15:47 +0900

    <test>: check winner's position is same max position
    게임 실행 결과를 확인하고 position이 가장 높은 사람이 우승자인지 확인

--- src/test/java/racingcar/domain/CarListTest.java
+++ src/test/java/racingcar/domain/CarListTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,14 @@ class CarListTest {
     }
 
     @Test
-    void 게임_횟수_입력값만큼_실행되는지_확인() {
+    void 게임_횟수_입력만큼_실행되는지_확인() {
         carList.printGameProceed(2);
+    }
+
+    @Test
+    void 게임_실행_결과를_확인하고_우승자와_결과가_동일한지_확인() {
+        carList.printGameProceed(2);
+        List<String> winners = carList.findWinner();
+        System.out.println(winners);
     }
 }
