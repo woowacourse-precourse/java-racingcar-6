72d44d1ecd623413fa35f0e87cf52875e6bc8a3b
realcold0 <realcold0@gmail.com>
2023-11-01 04:35:30 +0900

    feat : Car가 움직여야하는지 안움직여야하는지 랜덤 수를 받아 처리하는 기능 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,22 +1,33 @@
 package racingcar.domain;
 
+import racingcar.util.GenerateRandomNumber;
+
 public class Car {
     private String name;
     private int distance;
 
-    public Car(String name){
-        this.name =name;
+    public Car(String name) {
+        this.name = name;
         distance = 0;
     }
 
-    public void move(){
+    public void move() {
         distance++;
     }
+
     public String getName() {
         return name;
     }
 
     public int getDistance() {
         return distance;
+    }
+
+    public boolean isCanMove() {
+        int random = GenerateRandomNumber.getRandomNumber();
+        if (random >= 4) {
+            return true;  //이동
+        }
+        return false; //이동하지 않기
     }
 }
