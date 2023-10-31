36e78e42e5ce663889691c6c02b3792eef73645f
aka-nick <bechalrang@gmail.com>
2023-10-31 15:14:49 +0900

    추가(RaceView) : RaceView#askRoundNumber() 예외처리
    이름 입력 시 길이가 1~5자리를 벗어나면 예외를 일으키도록 코드 추가함
    
    RaceView

--- src/main/java/racingcar/io/views/RaceView.java
+++ src/main/java/racingcar/io/views/RaceView.java
@@ -7,19 +7,46 @@ import racingcar.io.Output;
 
 public class RaceView {
 
+    // TODO : 리터럴 문자열 제거할 것
     public List<Racer> askRacersNames() {
         Output.consoleLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        // TODO : 이름에 대한 예외처리
+
         return Input.consoleStrings(",").stream()
-                .map(Racer::new)
+                .map(String::strip)
+                .map(RaceView::toRacer)
                 .toList();
+    }
+
+    private static Racer toRacer(String name) {
+        validateWrongLength(name);
+        return new Racer(name);
+    }
+
+    private static void validateWrongLength(String name) {
+        int maxLength = 5;
+        int minLength = 1;
+        int nameLength = name.length();
+
+        if (nameLength < minLength || maxLength < nameLength) {
+            throw new IllegalArgumentException("이름의 길이는 1~5자리여야 합니다.");
+        }
     }
 
     public Integer askRoundNumber() {
         Output.consoleLine("시도할 회수는 몇회인가요?");
-        // TODO : 회수에 대한 예외처리
-        Integer numberOfRound = Input.consoleNumber();
+        Integer numberOfRound = getInputNumberOfRound();
         Output.consoleLine(); // 입력 후 공백 한 줄이 들어가게 되어있음
+        return numberOfRound;
+    }
+
+    private static Integer getInputNumberOfRound() {
+        int minRound = 1;
+        int maxRound = 100;
+        Integer numberOfRound = Input.consoleNumber();
+
+        if (numberOfRound < minRound || maxRound < numberOfRound) {
+            throw new IllegalArgumentException("진행회수는 1~100회여야 합니다.");
+        }
         return numberOfRound;
     }
 
