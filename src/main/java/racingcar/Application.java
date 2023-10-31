e2905819563f5c07632c94e090a9a6dfb49f5312
baejeong-eun <lovelyeun0914@gmail.com>
2023-11-01 02:18:41 +0900

    feat: 자동차 이름받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+        List<Car> carNameList = getCarNames();
+
     }
+
+    public static List<Car> getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = readLine();
+
+        return makeCarNameList(inputNames);
+
+    }
+
+
 }
