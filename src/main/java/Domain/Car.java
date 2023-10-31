a9c47591592ad8d432ba014ba46edc41f5fd9c5e
c1oud-dev <gksmf0187@gmail.com>
2023-10-31 19:53:28 +0900

    feat(car): add car domain

---
+++ src/main/java/Domain/Car.java
@@ -0,0 +1,40 @@
+package Domain;
+
+
+public class Car implements Comparable<Car> {
+    private static final int MIN_FORWARD_NUMBER = 4;
+    private static final int POSITION_DEFAULT = 0;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = POSITION_DEFAULT;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPosition(int randomNumber) {
+        if (randomNumber >= MIN_FORWARD_NUMBER) {
+            this.position++;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        if (car.position < position) {
+            return 1;
+        }
+        if (car.position > position) {
+            return -1;
+        }
+        return 0;
+    }
+}
