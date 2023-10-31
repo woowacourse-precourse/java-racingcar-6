d29c9224f31ef07e9ba1d495461d7b4fe605d3c4
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 02:57:11 +0900

    feat: 중복된 이름 입력에 대한 검증

--- src/main/java/racingcar/util/vaildator/carNameValidator.java
+++ src/main/java/racingcar/util/vaildator/carNameValidator.java
@@ -1,6 +1,8 @@
 package racingcar.util.vaildator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class carNameValidator {
     private static final int MAX_INPUT_NAME_SIZE = 5;
@@ -8,6 +10,7 @@ public class carNameValidator {
     private static final String INPUT_NAME_SIZE_EXCEPTION = "이름이 5자를 초과하였습니다.";
     private static final String INPUT_NAME_EXCEPTION = "이름을 입력해주세요.";
     private static final String INPUT_NAME_MINIMUM_EXCEPTION = "2명 이상 입력해주세요.";
+    private static final String INPUT_NAME_DUPLICATE_EXCEPTION = "중복된 이름이 존재합니다.";
 
     public static void validateInputNameSize(String name) {
         if (name.length() > MAX_INPUT_NAME_SIZE) {
@@ -24,6 +27,13 @@ public class carNameValidator {
     public static void validateMinimumCarNameCount(List<String> carList) {
         if (carList.size() < MININUM_CARLIST_SIZE) {
             throw new IllegalArgumentException(INPUT_NAME_MINIMUM_EXCEPTION);
+        }
+    }
+
+    public static void validateInputNameDupication(List<String> carList) {
+        Set<String> duplicateCarList = new HashSet<>(carList);
+        if (carList.size() != duplicateCarList.size()) {
+            throw new IllegalArgumentException(INPUT_NAME_DUPLICATE_EXCEPTION);
         }
     }
 
