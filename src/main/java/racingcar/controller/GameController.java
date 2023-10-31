ea6489af65223dd5b1e45a7d63974a32962a1b9d
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-31 23:03:21 +0900

    feat ($GameController) : 컨트롤러 생성 - view 와 service를 통해 게임 컨트롤 - 시도할 회수 만큼 자동차 실행

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,35 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.service.GameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+    GameService service = new GameService();
+    private final Integer tryCount;
+    private final List<Car> cars;
+    public GameController(){
+        InputView inputView = new InputView();
+
+        String[] carNames = inputView.startGame();
+
+        tryCount = inputView.inputTryCount();
+        cars = service.makeCars(carNames);
+    }
+    public void game(){
+        OutputView outputView = new OutputView();
+
+        for(int i=0; i<tryCount; i++){
+            for(Car car: cars){
+                car.forward();
+                outputView.printEachFoward(car.getName(), car.getPlace());
+            }
+            System.out.println();
+        }
+        List<String> winnerNames = service.findEqualsMaxPlace(cars);
+        outputView.printWinner(winnerNames);
+    }
+}
