177cdec1fb4d2d869dec4e59b3d9d6661d7fad3c
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 17:49:11 +0900

    feat : 자동차 객체 생성 - 자동차가 랜덤으로 생성된 값에 따라 움직이도록 한다. - 자동차 전진 기능 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import racingcar.enums.Constants;
+
+public class Car {
+    Name carName;
+    Distance carDistance;
+
+    public Car(Name carName, Distance carDistance) {
+        this.carName = carName;
+        this.carDistance = carDistance;
+    }
+
+    public void move(Integer randomNumber) {
+        if (determineMove(randomNumber)) moveForward();
+    }
+
+    private boolean determineMove(Integer number) {
+        if (number >= Constants.MOVE_NUMBER.getNumber()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void moveForward() {
+        this.carDistance.setDistance(carDistance.getDistance() + 1);
+    }
+}
