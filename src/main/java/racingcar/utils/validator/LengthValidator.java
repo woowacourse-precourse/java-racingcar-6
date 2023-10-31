dbbe2f63e75947f52e5c44606feaa831bcab2d07
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-31 17:26:54 +0900

    fix: 문자열 -> 리스트 내 문자열 검증으로 수정

--- src/main/java/racingcar/utils/validator/LengthValidator.java
+++ src/main/java/racingcar/utils/validator/LengthValidator.java
@@ -1,7 +1,10 @@
 package racingcar.utils.validator;
 
+import java.util.List;
+
 public class LengthValidator {
-    public static boolean validate(String input, int maxLength) {
-        return input.length() <= maxLength;
+    public static boolean validate(List<String> inputs, int maxLength) {
+        return inputs.stream()
+                .allMatch(input -> input.length() <= maxLength);
     }
 }
