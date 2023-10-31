bc2f70f55d3ceb2e604ac49e69cff5fde070e988
moomint8 <office05053@gmail.com>
2023-10-31 19:11:38 +0900

    2023/10/31 19:10 ver 1.1.0 feat : 사용자 입력 값을 받아 쉼표(,)로 나누는 기능 구현

---
+++ src/main/java/racingcar/GameManager.java
@@ -0,0 +1,41 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameManager {
+    protected static class RacingCar {
+        String name;
+        int distance;
+    }
+
+    public static void newGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        List<RacingCar> player = new ArrayList<>();
+
+        splitPlayer(input, player);
+
+        for (RacingCar racingCar : player) {
+            System.out.println(racingCar.name + " : ");
+        }
+        System.out.println("이름 쉼표로 나누기");
+    }
+
+    private static void splitPlayer(String input, List<RacingCar> player) {
+        String[] splitInput = input.split(",");
+        for (int i = 0; i < splitInput.length; i++) {
+            addPlayer(splitInput[i], player);
+        }
+    }
+
+    private static void addPlayer(String name, List<RacingCar> player) {
+        RacingCar racingCar = new RacingCar();
+        racingCar.name = name;
+        racingCar.distance = 0;
+        player.add(racingCar);
+    }
+}
