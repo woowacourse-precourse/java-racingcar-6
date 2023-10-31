dfbfb99e5051e819bcd14ccebb3e48f0fa86e50c
Jiwon Na <jiwonna52@gmail.com>
2023-10-31 20:48:41 +0900

    Test : 추가 우승자가 여러 명 일 때 Test

--- src/test/java/racingcar/FindWinTest.java
+++ src/test/java/racingcar/FindWinTest.java
@@ -26,7 +26,7 @@ public class FindWinTest {
     }
 
     @Test
-    void 우승자가_존재할_때() {
+    void 우승자가_한_명() {
         Car car1 = new Car("a", 1);
         Car car2 = new Car("b", 0);
         Car car3 = new Car("c", 0);
@@ -39,5 +39,22 @@ public class FindWinTest {
 
         assertThat(1).isEqualTo(winCars.size());
         assertThat("a").isEqualTo(winCars.get(0));
+    }
+
+    @Test
+    void 우승자가_여러_명() {
+        Car car1 = new Car("a", 1);
+        Car car2 = new Car("b", 1);
+        Car car3 = new Car("c", 0);
+        List<Car> testList = new ArrayList<>();
+
+        testList.add(car1);
+        testList.add(car2);
+        testList.add(car3);
+        List<String> winCars = findWinCars(testList);
+
+        assertThat(2).isEqualTo(winCars.size());
+        assertThat("a").isEqualTo(winCars.get(0));
+        assertThat("b").isEqualTo(winCars.get(1));
     }
 }
