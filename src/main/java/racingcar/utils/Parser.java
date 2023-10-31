3de432cec9124828406db500d45cd68c35b1b08d
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 10:59:30 +0900

    Feat: Parser 클래스 추가, validateCarNameInput구현
    parseCarNameInput를 이용해 쉼표(.)를 통해 구분하여 List<String>을 얻는다
    validateCarNameInput를 통해 유효한 길이의 carName인지 검사한다.

---
+++ src/main/java/racingcar/utils/Parser.java
@@ -0,0 +1,13 @@
+package racingcar.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+
+    private static final String SPLIT_REGEX = ",";
+
+    public List<String> parseCarNameInput(String userInput) {
+        return Arrays.asList(userInput.split(SPLIT_REGEX));
+    }
+}
--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -9,7 +9,7 @@ class ValidatorTest {
     Validator validator = new Validator();
 
     private static final String WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE = "이름은 5자 이하여야 합니다";
-   
+
     @Test
     void 차_이름_길이_예외테스트() {
         IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
