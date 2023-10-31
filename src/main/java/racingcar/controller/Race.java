1477a62ac897955a566c17a4ba382a5da9ed277e
lotuxsoo <tndus5383@gmail.com>
2023-10-31 10:30:22 +0900

    feat: 전진한 자동차가 없는 경우 '최종 우승자가 없습니다!' 문구 출력

--- src/main/java/racingcar/controller/Race.java
+++ src/main/java/racingcar/controller/Race.java
@@ -3,8 +3,10 @@ package racingcar.controller;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.model.RandomNumber;
@@ -33,11 +35,15 @@ public class Race {
     public List<String> getWinners() {
         List<String> winnerNames = new ArrayList<>();
         for (Car car : racingCars) {
-            if (car.getStatus() == getMaxScore()) {
+            if (IsWinnerCondition(car)) {
                 winnerNames.add(car.getName());
             }
         }
         return winnerNames;
+    }
+
+    private boolean IsWinnerCondition(Car car) {
+        return car.getStatus() == getMaxScore() && car.getStatus() > 0;
     }
 
     private Integer getMaxScore() {
