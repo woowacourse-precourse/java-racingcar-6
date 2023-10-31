8fbbabc33be44bbc278deda31d96a2e50b6dc06b
KimKyuHwan <vkdls754@gmail.com>
2023-10-31 22:04:05 +0900

    feat(RacingService): 레이싱기능 구현
    레이싱 차량 등록 기능
    레이싱 차량 드라이브 기능
    레이싱 결과 기록 기능

---
+++ src/main/java/racingcar/service/RacingServiceImpl.java
@@ -0,0 +1,53 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.repository.CarRepository;
+import racingcar.repository.MemoryCarRepository;
+
+public class RacingServiceImpl implements RacingService{
+    CarRepository carRepository = new MemoryCarRepository();
+    List<String> record = new ArrayList<>();
+    @Override
+    public void race(List<String> carList, String count) {
+        int tryCount = Integer.parseInt(count);
+        registerCars(carList);
+
+        for (int i = 0; i < tryCount ; i++) {
+            driveAllCars(carRepository.getCarList());
+            writeRecord(carRepository.getCarList());
+        }
+    }
+
+    @Override
+    public void registerCars(List<String> carList) {
+        for (String car : carList) {
+            carRepository.save(car);
+        }
+    }
+
+    @Override
+    public void driveAllCars(List<Car> carList) {
+        for (int i = 0; i < carList.size(); i++) {
+            carList.get(i).drive();
+        }
+    }
+
+    @Override
+    public void writeRecord(List<Car> carList) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            sb.append(car.getName() + " : " + car.showCarPosition() + "\n");
+        }
+
+        record.add(sb.toString());
+    }
+
+    @Override
+    public List<String> getRecord() {
+        return this.record;
+    }
+}
