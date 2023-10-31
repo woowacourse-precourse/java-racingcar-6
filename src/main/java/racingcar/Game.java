674a05afce17f315c70029ac412b67fecbebce6e
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-31 11:26:43 +0900

    [REFACTOR] printWinner 함수 depth 수정
    들여쓰기 depth를 2로 수정

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -63,11 +63,9 @@ public class Game {
     private static void printWinner(List<String> winner) {
         System.out.print("최종 우승자 : ");
         if (!winner.isEmpty()) {
-            for (int i = 0; i < winner.size(); i++) {
-                System.out.print(winner.get(i));
-                if (i < winner.size() - 1) {
-                    System.out.print(", ");
-                }
+            System.out.print(winner.get(0));
+            for (int i = 1; i < winner.size(); i++) {
+                System.out.print(", " + winner.get(i));
             }
         }
         System.out.println();
