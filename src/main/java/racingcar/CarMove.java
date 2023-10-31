bf26a914fdc6af15edd0afa7b8db102601aedaec
keongmiiin99 <smartmina7@naver.com>
2023-10-31 21:33:29 +0900

    refactor: 전진/멈춤 기능

--- src/main/java/racingcar/CarMove.java
+++ src/main/java/racingcar/CarMove.java
@@ -1,14 +1,34 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class CarMove {
+    private RaceInfo raceInfo;
+
+
+    public CarMove(RaceInfo raceInfo) {
+        this.raceInfo = raceInfo;
+        carRace();
+    }
+
+    public void carRace() {
+        System.out.println("실행 결과");
+        int i = 1;
+        while (i <= raceInfo.getAttemptCount()) {
+            for (Car car : raceInfo.getCarList()) {
+                if (decideCarMovement()){
+                    car.increaseCarDistance();
+                }
+                RaceResultView.raceResultDisplay(car);
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
     public static boolean decideCarMovement() {
         int randomNum = Randoms.pickNumberInRange(0, 9);
-        if (randomNum >= 4) {
-            return true;
-        } else {
-            return false;
-        }
+        return randomNum >= 4;
     }
 }
