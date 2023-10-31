96dfd23c3a0524f4f755d39ccc1158362e539e59
watermelon3737 <pshrina1@naver.com>
2023-10-31 16:56:43 +0900

    refactor(RacingCar) : 불필요한 list<boolean> move 값을 삭제 후 변경 점 적용

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -5,41 +5,33 @@ import racingcar.view.RacingGameMessage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RacingCar {
     private final int MOVE_LIMITS = 3;
     private final String racingCarName;
-    private final List<Boolean> move;
     private int moveCount;
 
     public RacingCar(final String carName) {
         this.racingCarName = carName;
-        this.move = new ArrayList<>();
         this.moveCount = 0;
     }
 
     public void move(final int movableNumber) {
         if (movableNumber > MOVE_LIMITS) {
-            this.move.add(true);
             moveCount += 1;
         }
     }
 
     public String getRaceResult() {
-        final String gameResult = this.move.stream()
-                .filter(move -> move)
-                .map(move -> RacingGameMessage.MOVE.getMoveMessage())
-                .collect(Collectors.joining());
+        StringBuilder dash = new StringBuilder();
+        IntStream.range(0, moveCount).forEach(i -> dash.append(RacingGameMessage.MOVE.getMoveMessage()));
 
-        return gameResult;
+        return dash.toString();
     }
 
     public String getRacingCarName() {
         return this.racingCarName;
-    }
-
-    public List<Boolean> getMove() {
-        return this.move;
     }
 
     public int getMoveCount() {
