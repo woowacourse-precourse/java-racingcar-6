3253bba830ca4560330994a840118ac85d238d71
jae woo Jung <awfjol2008@naver.com>
2023-10-31 09:57:34 +0900

    feat: InputView 클래스의 접근 제한을 위해 private 생성자 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private InputView() {
+
+    }
+
     public static String read() {
         return Console.readLine();
     }
