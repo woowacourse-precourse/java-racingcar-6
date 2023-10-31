f62133c005c2d3a9ef7c66ba08d4a78f97cb6492
cousim46 <wjdghldns24@naver.com>
2023-10-31 23:05:41 +0900

    refactor : 기능별 메서드 분리

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -9,18 +9,35 @@ import racingcar.output.PrintService;
 public class Race {
 
     public static void execute() {
-        PrintService.inputCarNameRequest();
-        String[] carNames = InputService.writeCarName().split(",");
+        String[] carNames = startRacing();
         List<Car> cars = Car.create(carNames);
         PrintService.askAttemptCount();
         int currentAttemtCount = 0;
         int attemptCount = InputService.attemptCount();
+        race(cars, currentAttemtCount, attemptCount);
+        endRacing(cars);
+    }
+
+    private static void race(List<Car> cars, int currentAttemtCount, int attemptCount) {
         while (currentAttemtCount < attemptCount) {
-            List<Car> reportCar = Referee.report(cars);
-            PrintService.show(reportCar);
+            resultReport(cars);
             currentAttemtCount++;
         }
+    }
+
+    private static String[] startRacing() {
+        PrintService.inputCarNameRequest();
+        String[] carNames = InputService.writeCarName().split(",");
+        return carNames;
+    }
+
+    private static void endRacing(List<Car> cars) {
         PrintService.result(Referee.determineWinner(cars));
         InputService.end();
+    }
+
+    private static void resultReport(List<Car> cars) {
+        List<Car> reportCar = Referee.report(cars);
+        PrintService.show(reportCar);
     }
 }
