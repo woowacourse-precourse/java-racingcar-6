3b44cba14dca0c688ce5d6f85628240f75cf45bf
YongJOoOo <we072200@naver.com>
2023-10-31 14:41:34 +0900

    test: UserInputTestConsole에서 콘솔창에 입력될 다양한 사용자 입력값에 대하여 테스트 메소드 구현

--- src/test/java/racingcar/view/UserInputTestConsole.java
+++ src/test/java/racingcar/view/UserInputTestConsole.java
@@ -3,21 +3,15 @@ package racingcar.view;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class UserInputTest {
-
-    private void setSystemIn(String input) {
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-    }
-
+class UserInputTestConsole extends NsConsoleIOTest {
     @Test
     void readCarNames_정상적인_입력() {
         // given
         String input = "pobi,wooni";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when
         UserInput userInput = new UserInput();
@@ -28,10 +22,52 @@ class UserInputTest {
     }
 
     @Test
+    void readCarNames_쉼표로_시작하는_입력() {
+        // given
+        String input = ",pobi";
+        setConsoleInput(input);
+
+        // when & then
+        UserInput userInput = new UserInput();
+
+        assertThatThrownBy(() -> userInput.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름 입력이 (,) 쉼표로 시작되면 안됩니다.");
+    }
+
+    @Test
+    void readCarNames_쉼표_중첩된_입력() {
+        // given
+        String input = "pobi,,woni";
+        setConsoleInput(input);
+
+        // when & then
+        UserInput userInput = new UserInput();
+
+        assertThatThrownBy(() -> userInput.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름에는 (,) 연속된 쉼표를 입력해서는 안됩니다.");
+    }
+
+    @Test
+    void readCarNames_쉼표로_끝나는_입력() {
+        // given
+        String input = "pobi,";
+        setConsoleInput(input);
+
+        // when & then
+        UserInput userInput = new UserInput();
+
+        assertThatThrownBy(() -> userInput.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름 입력 끝에는 (,) 쉼표가 있으면 안됩니다.");
+    }
+
+    @Test
     void readCarNames_5글자가_넘는_입력() {
         // given
         String input = "pobi,woooni";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when & then
         UserInput userInput = new UserInput();
@@ -45,7 +81,7 @@ class UserInputTest {
     void readCarNames_이름_중복() {
         // given
         String input = "pobi,pobi,abc";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when & then
         UserInput userInput = new UserInput();
@@ -59,7 +95,7 @@ class UserInputTest {
     void readCarNames_이름_부분_중복_정상작동() {
         // given
         String input = "abcd,abc,ab";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when
         UserInput userInput = new UserInput();
@@ -72,8 +108,8 @@ class UserInputTest {
     @Test
     void readCarNames_공백_비정상_입력() {
         // given
-        String input = "pobi ,woni, abc";
-        setSystemIn(input);
+        String input = "pobi ,woni, a bc";
+        setConsoleInput(input);
 
         // when & then
         UserInput userInput = new UserInput();
@@ -82,12 +118,13 @@ class UserInputTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("경주할 자동차 이름에 공백이 포함되면 안됩니다.");
     }
+
 
     @Test
     void readCarNames_공백인경우() {
         // given
         String input = " ";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when & then
         UserInput userInput = new UserInput();
@@ -97,11 +134,12 @@ class UserInputTest {
                 .hasMessageContaining("경주할 자동차 이름에 공백이 포함되면 안됩니다.");
     }
 
+
     @Test
     void readGameCount_정상적인_입력() {
         // given
         String input = "12";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when
         UserInput userInput = new UserInput();
@@ -115,7 +153,7 @@ class UserInputTest {
     void readGameCount_숫자가_아닌_입력() {
         // given
         String input = "a12b";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when and then
         UserInput userInput = new UserInput();
@@ -129,7 +167,7 @@ class UserInputTest {
     void readGameCount_0_입력() {
         // given
         String input = "0";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when and then
         UserInput userInput = new UserInput();
@@ -140,10 +178,24 @@ class UserInputTest {
     }
 
     @Test
+    void readGameCount_0_으로_시작하는_입력() {
+        // given
+        String input = "07";
+        setConsoleInput(input);
+
+        // when and then
+        UserInput userInput = new UserInput();
+
+        assertThatThrownBy(() -> userInput.readGameCount())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임 실행 횟수 입력은 0으로 시작하면 안됩니다.");
+    }
+
+    @Test
     void readGameCount_음수_입력() {
         // given
         String input = "-123";
-        setSystemIn(input);
+        setConsoleInput(input);
 
         // when and then
         UserInput userInput = new UserInput();
