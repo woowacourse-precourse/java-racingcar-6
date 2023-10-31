3a12b1080c472af776d1dfb44620e892729b5083
ryugeumjung <fbgjung@gmail.com>
2023-11-01 00:02:15 +0900

    feat: 사용자입력 오류처리 - 하나 이상의 자동차 이름을 쉼표로 구분하여 입력하도록 입력 유효성 검사 추가 - 이동 횟수 숫자만 입력하도록 입력 유효성 검사 추가

--- src/main/java/racingcar/SetRace.java
+++ src/main/java/racingcar/SetRace.java
@@ -9,22 +9,28 @@ public class SetRace {
     private final List<Car> carsList = new ArrayList<>();
     public List<Car> createCar() {
         System.out.println("경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         String input = Console.readLine();
-        String[] carNames = input.split(",");
 
-        // 각 자동차 이름에 대해 Car 객체 생성
-        for (String carName : carNames) {
-            Car car = new Car(carName);
-            carsList.add(car);
+        if (input.contains(",")) {
+            String[] carNames = input.split(",");
+            for (String carName : carNames) {
+                Car car = new Car(carName);
+                carsList.add(car);
+            }
+        } else {
+            throw new IllegalArgumentException("적어도 하나 이상의 자동차 이름을 입력해야 합니다.");
         }
         return carsList;
     }
 
     public int inputMovement() {
-        System.out.println("이동할 횟수를 입력하세요");
-        String time = Console.readLine();
-        int number = Integer.parseInt(time);
-        return number;
+        try{
+            System.out.println("이동할 횟수를 입력하세요");
+            String time = Console.readLine();
+            int number = Integer.parseInt(time);
+            return number;
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
     }
 }
