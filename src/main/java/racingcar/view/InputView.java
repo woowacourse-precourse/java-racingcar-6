9a5e2d0658ab2f166624a1901174da145bad2927
박승민 <tmddudals369@naver.com>
2023-11-01 00:49:37 +0900

    feat(inputView): 자동차 이름을 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.utils.CarNameValidator;
 import racingcar.utils.GameCountValidator;
 
 public class InputView {
@@ -9,5 +12,24 @@ public class InputView {
         String gameCount = Console.readLine();
         GameCountValidator.validatePositiveDigitAnInRangeGameCount(gameCount);
         return gameCount;
+    }
+
+    public List<String> inputCarNames() {
+        String carNames = Console.readLine();
+        CarNameValidator.validateMultipleCarName(carNames);
+
+        List<String> convertedCarNames = convertToList(carNames);
+        for (String carName : convertedCarNames) {
+            CarNameValidator.validateCarName(carName);
+        }
+
+        CarNameValidator.validateDuplicateCarName(convertedCarNames);
+
+        return convertedCarNames;
+    }
+
+    private List<String> convertToList(String carNames) {
+        return Arrays.stream(carNames.split(CarNameValidator.MULTIPLE_CAR_NAME_DELIMITER))
+            .toList();
     }
 }
