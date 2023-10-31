75c155d801593966c2f680b7fc19355dc701d49d
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-31 06:48:52 +0000

    [Output] Refactoring - intent2 수준으로 변경

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -1,31 +1,21 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputManager {
     public static void printRoundResult(int round, List<Car> cars) {
         System.out.println("실행 결과");
-        for (Car car : cars) {
-            System.out.println(car.getName() + " : " + createPositionString(car.getPosition()));
-        }
+        cars.forEach(car -> System.out.println(car.getName() + " : " + createPositionString(car.getPosition())));
         System.out.println();
     }
 
     private static String createPositionString(int position) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < position; i++) {
-            result.append("-");
-        }
-        return result.toString();
+        return "-".repeat(position);
     }
 
     public static void printWinners(List<Car> winners) {
         System.out.print("우승자: ");
-        for (int i = 0; i < winners.size(); i++) {
-            System.out.print(winners.get(i).getName());
-            if (i < winners.size() - 1) {
-                System.out.print(", ");
-            }
-        }
+        System.out.print(winners.stream().map(Car::getName).collect(Collectors.joining(", ")));
     }
 }
