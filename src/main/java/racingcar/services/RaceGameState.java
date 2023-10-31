7eba1df74179954549c92bda0766fa842b016fa4
minjong kim <godminjong@naver.com>
2023-10-31 15:37:28 +0900

    feat:생성자를 통해 String 타입의 문자열을 Map 타입의 필드로 변환해주는 기능 추가.
    필드의 Map을 통해 키값인 String 은  자동차의 이름을 의미하고 Value값은 해당 자동차의 전진 진행정도를 나타내도록함. 또한 생성자들 통해 String 타입의 문자열을 List<String>타입으로 목록화 을 진행함.

--- src/main/java/racingcar/services/RaceGameState.java
+++ src/main/java/racingcar/services/RaceGameState.java
@@ -2,10 +2,17 @@ package racingcar.services;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import racingcar.validate.Validator;
 import racingcar.validate.impls.RaceGameStateValidator;
 
 public class RaceGameState {
+    private final Map<String, Integer> stateOfEachCar;
+
+    public RaceGameState(String inputStringOfParticipant) {
+        List<String> listOfCarNames = enumerateCarNames(inputStringOfParticipant);
+    }
     private List<String> enumerateCarNames(String inputStringOfParticipants){
         Validator validator = new RaceGameStateValidator();
         String [] parsed = inputStringOfParticipants.replaceAll(" ", "").split(",");
