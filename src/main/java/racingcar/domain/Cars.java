77fb5da718ae6e9166aafb126c8d7e5f372fbd4c
YuJeongYun <yunyj99@naver.com>
2023-11-01 03:54:45 +0900

    refactor: Cars 일급컬렉션 순회 가능하도록 코드 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,19 +2,12 @@ package racingcar.domain;
 
 import racingcar.constant.ErrorMessage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Cars {
+public class Cars implements Iterable<Car> {
     List<Car> cars;
 
     public Cars() {
-    }
-
-    public List<Car> getCars() {
-        return Collections.unmodifiableList(this.cars);
     }
 
     public Cars(String userInputCarName) {
@@ -22,13 +15,23 @@ public class Cars {
 
         splitUserInputCarName(userInputCarName);
     }
+
     public Cars(List<Car> cars) {
         this.cars = new ArrayList<>(cars);
     }
 
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(this.cars);
+    }
+
+    @Override
+    public Iterator<Car> iterator() {
+        return cars.iterator();
+    }
+
     // 사용자가 입력한 자동차 이름을 쉼표(,)로 구분하여 리스트에 저장
     private void splitUserInputCarName(String userInput) {
-        userInput.replaceAll(" ","");
+        userInput.replaceAll(" ", "");
         String[] names = userInput.split(",");
         validateDuplicate(names);
 
@@ -39,7 +42,7 @@ public class Cars {
 
     private void validateDuplicate(String[] names) {
         // 중복된 자동차 이름 있으면 예외 발생
-        if(names.length != Arrays.stream(names).distinct().count()) {
+        if (names.length != Arrays.stream(names).distinct().count()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATE);
         }
     }
