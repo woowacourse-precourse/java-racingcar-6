c5114850fa75d6a14f41048dcb18f77a77752b4d
hwangseonheon <dolphin33@jbnu.ac.kr>
2023-10-31 19:50:43 +0900

    feat: 전진 또는 멈춤 기능
    - Create function that moves forward when the random value exceeds 3

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,10 +1,19 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
   private final String name;
+  private int position = 0;
 
   public RacingCar(String name) {
     this.name = name;
+  }
+
+  public void decideMovePosition() {
+    if(Randoms.pickNumberInRange(0, 9) > 3) {
+      position++;
+    }
   }
 
 }
