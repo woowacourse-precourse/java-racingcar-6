1726fb0d5ca4f22df1bf6e407dc96b0820dac00b
minsu20 <syt07100@gmail.com>
2023-11-01 02:43:26 +0900

    fix(constants):  출력 문구 수정

--- src/main/java/racingcar/constants/ViewMessages.java
+++ src/main/java/racingcar/constants/ViewMessages.java
@@ -4,10 +4,12 @@ import static racingcar.constants.Delimiters.CAR_NAME_DELIMITER_DESCRIPTION;
 
 public enum ViewMessages {
 
-    REQUEST_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(" + CAR_NAME_DELIMITER_DESCRIPTION + ") 기준으로 구분)"),
+    REQUEST_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 " + CAR_NAME_DELIMITER_DESCRIPTION.getMessage() + " 기준으로 구분)"),
     REQUEST_RACE_ROUNDS("시도할 회수는 몇회인가요?"),
     RACE_WINNERS("최종 우승자"),
-    RACE_RESULT("실행 결과");
+    RACE_RESULT("실행 결과"),
+    EQUAL(" : "),
+    MOVE_FORWARD_SYMBOL("-");
 
     private final String message;
 
