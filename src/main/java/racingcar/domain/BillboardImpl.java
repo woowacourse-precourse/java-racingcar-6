70a63e275e05bf9e330904ea927136601f4c4db8
khabh1 <kshababe1@gmail.com>
2023-10-31 11:21:34 +0900

    refactor(Billboard): 문자열 생성 로직 수정
    Collectors -> String.join으로 수정

--- src/main/java/racingcar/domain/BillboardImpl.java
+++ src/main/java/racingcar/domain/BillboardImpl.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import java.util.stream.Collectors;
 import racingcar.config.GameMessage;
 import racingcar.config.Message;
 import racingcar.dto.RaceProgressDTO;
@@ -10,10 +9,8 @@ public class BillboardImpl implements Billboard {
     @Override
     public void displayraceprogress(RaceProgressDTO raceProgressDTO) {
         display(Message.NEW_LINE);
-        String raceProgress = raceProgressDTO.nameAndDistanceMessages()
-                .stream()
-                .collect(Collectors.joining(Message.NEW_LINE));
-        display(GameMessage.RACE_PROGRESS_PREFIX);
+        String raceProgress = String.join(Message.NEW_LINE, raceProgressDTO.nameAndDistanceMessages());
+        display(GameMessage.RACE_PROGRESS_PREFIX.getMessage());
         display(raceProgress);
     }
 
@@ -21,10 +18,6 @@ public class BillboardImpl implements Billboard {
     public void displayWinners(WinnerStand winnerStand) {
         String winners = GameMessage.WINNER_STAND_PREFIX.getMessage() + winnerStand.getWinners();
         display(winners);
-    }
-
-    private void display(GameMessage gameMessage) {
-        System.out.println(gameMessage.getMessage());
     }
 
     private void display(String message) {
