6d4676fc383569478230733764ffb07989e34c50
slowcloud <sju9417@gmail.com>
2023-10-31 14:47:03 +0900

    refactor(OutputViewTest): NsTest를 확장해서 코드가 간결하게 보이도록 수정

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,35 +1,14 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 import racingcar.domain.CarDistanceMessage;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class OutputViewTest {
-
-    private final static PrintStream stdout = System.out;
-    private final static ByteArrayOutputStream captor = new ByteArrayOutputStream();
-
-    private String output() {
-        return captor.toString().trim();
-    }
-
-    @BeforeAll
-    public static void beforeAll() {
-        System.setOut(new PrintStream(captor));
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        System.setOut(stdout);
-    }
+class OutputViewTest extends NsTest {
 
     @Test
     void printCarDistanceMessages_출력_확인() {
@@ -42,6 +21,11 @@ class OutputViewTest {
         String log = output();
         Assertions.assertThat(log).contains("foo : -");
 
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 
 }
