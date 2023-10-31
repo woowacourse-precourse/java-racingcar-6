1074e1beea45c30b9c4c26a95dfe1c948735a83a
waveofmymind <sjun0913@gmail.com>
2023-10-31 09:08:09 +0900

    refactor: 총 라운드와 현재 라운드를 변수로 가지며, 현재 라운드의 변경을 NumberOfRound가 직접 처리한다.

--- src/main/java/racingcar/domain/NumberOfRound.java
+++ src/main/java/racingcar/domain/NumberOfRound.java
@@ -1,19 +1,37 @@
 package racingcar.domain;
 
-public record NumberOfRound(
-        int value
-) {
-    public NumberOfRound {
-        validate(value);
+public class NumberOfRound {
+    private int currentRound = 0;
+    private final int totalRounds;
+
+    public NumberOfRound(int totalRounds) {
+        validate(totalRounds);
+        this.totalRounds = totalRounds;
     }
 
-    public NumberOfRound consumeRound() {
-        return new NumberOfRound(value - 1);
+    public void nextRound() {
+        if (isLastRound()) {
+            throw new IllegalStateException("마지막 라운드입니다.");
+        }
+        currentRound++;
+    }
+
+    public boolean isLastRound() {
+        return currentRound >= totalRounds;
+    }
+
+    public int getCurrentRound() {
+        return currentRound;
+    }
+
+    public int getTotalRounds() {
+        return totalRounds;
     }
 
     private void validate(int value) {
-        if (value < 0) {
-            throw new IllegalArgumentException("0 이상의 숫자만 입력 가능합니다.");
+        if (value <= 0) {
+            throw new IllegalArgumentException("0 초과의 숫자만 입력 가능합니다.");
         }
     }
 }
+
--- src/test/java/racingcar/domain/NumberOfRoundTest.java
+++ src/test/java/racingcar/domain/NumberOfRoundTest.java
@@ -16,7 +16,8 @@ public class NumberOfRoundTest {
         NumberOfRound numberOfRound = new NumberOfRound(value);
 
         // then
-        Assertions.assertThat(numberOfRound.value()).isEqualTo(value);
+        Assertions.assertThat(numberOfRound.getCurrentRound()).isEqualTo(0);
+        Assertions.assertThat(numberOfRound.getTotalRounds()).isEqualTo(value);
     }
 
     @DisplayName("시도 횟수가 음수이면 예외가 발생한다.")
@@ -28,20 +29,20 @@ public class NumberOfRoundTest {
         // when & then
         Assertions.assertThatThrownBy(() -> new NumberOfRound(value))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("0 이상의 숫자만 입력 가능합니다.");
+                .hasMessage("0 초과의 숫자만 입력 가능합니다.");
     }
 
     @DisplayName("남은 시도 횟수가 없을 경우 hasRoundsLeft()는 false를 반환한다.")
     @Test
     void hasRoundsLeftFalseTest() {
         // given
-        int currentRound = 1;
-        NumberOfRound numberOfRound = new NumberOfRound(currentRound);
+        int totalRound = 1;
+        NumberOfRound numberOfRound = new NumberOfRound(totalRound);
 
         // when
-        NumberOfRound nextNumberOfRound = numberOfRound.consumeRound();
+        numberOfRound.nextRound();
 
         // then
-        Assertions.assertThat(nextNumberOfRound.value()).isEqualTo(currentRound - 1);
+        Assertions.assertThat(numberOfRound.isLastRound()).isTrue();
     }
 }
