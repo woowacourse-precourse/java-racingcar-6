0d812b74f56abf60d0efdf08131d56d8b47629a9
Lee Yeon Su <yslee0540@gmail.com>
2023-10-31 15:07:31 +0900

    feat(Car): 자동차 생성 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
