5f0b3a3607c9613e3ad54d2525d7c0d09c049bdf
JaehongDev <println@kakao.com>
2023-10-31 21:49:53 +0900

    refactor: DataStore -> RacingCarRepository로 이름 변경

--- src/main/java/racingcar/event/part/CollectWinnerEvent.java
+++ src/main/java/racingcar/event/part/CollectWinnerEvent.java
@@ -1,13 +1,13 @@
 package racingcar.event.part;
 
 import java.util.List;
-import racingcar.data.DataStore;
+import racingcar.data.RacingCarRepository;
 import racingcar.event.core.EventListener.ReturnEvent;
 
-public record CollectWinnerEvent(DataStore dataStore) implements ReturnEvent<List<String>> {
+public record CollectWinnerEvent(RacingCarRepository racingCarRepository) implements ReturnEvent<List<String>> {
 
     @Override
     public List<String> execute() {
-        return dataStore.findParticipants().collectWinnerNames();
+        return racingCarRepository.findParticipants().collectWinnerNames();
     }
 }
