dfed72c518336cb885dd9fc72cd5a3f1dfb6b11e
scarlet0o0 <ghd7673@naver.com>
2023-10-31 13:13:09 +0900

    Comment : 로직 순서에 대한 주석 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -9,15 +9,19 @@ public class RacingCarController {
     public static void run() {
         CarRacing carRacing = new CarRacingImp();
 
+        // 1. 자동차 이름 입력 받기
         OutView.printCarNames();
         carRacing.addCar(InputView.promptForCarNames());
 
+        // 2. 경기 횟수 입력 받기
         OutView.printNumberOfAttempts();
         carRacing.addNumberOfRace(InputView.promptNumberOfAttempts());
 
+        // 3. 자동차 레이싱 시작
         OutView.printResults();
         OutView.printRaceResults(carRacing.start());
 
+        // 4. 우승자 출력
         OutView.printFinalWinner(carRacing.getWinner());
 
     }
