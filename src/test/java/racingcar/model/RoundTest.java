07f01001c6631c39f7bf4e6a2ef56bc1f356403f
Dawin <dawin002@naver.com>
2023-10-31 21:18:25 +0900

    test(RoundTest): 사용되지 않는 import 문 삭제
    삭제된 import 문:
    - import org.junit.jupiter.api.BeforeEach;
    - import static org.junit.jupiter.api.Assertions.*;

--- src/test/java/racingcar/model/RoundTest.java
+++ src/test/java/racingcar/model/RoundTest.java
@@ -1,10 +1,8 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RoundTest {
 
