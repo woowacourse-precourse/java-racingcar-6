528f30fa7bc507b72bf9c4087f0fcec4fef57ee9
yechop <channetattoo@gmail.com>
2023-11-01 04:25:58 +0900

    test: InputTest 테스트코드 추가

--- src/test/java/racingcar/domain/InputTest.java
+++ src/test/java/racingcar/domain/InputTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest {
@@ -20,9 +21,20 @@ public class InputTest {
     }
 
     @Test
-    @DisplayName("입력값이 1 미만이면 예외 발생")
-    void getGameCountTest() {
+    @DisplayName("중복된 이름을 입력시 예외 발생")
+    void getCarNamesDuplicateTest() {
+        //given
+        System.setIn(makeUserInput("aa,bb,cc,aa"));
 
+        //when
+
+        //then
+        assertThatThrownBy(() -> input.getCarNames()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("게임 카운트 입력값이 1 미만이면 예외 발생")
+    void getGameCountValidTest() {
         //given
         System.setIn(makeUserInput("0"));
 
@@ -30,7 +42,19 @@ public class InputTest {
 
         //then
         assertThatThrownBy(() -> input.getGameCount()).isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    @DisplayName("유저 입력값을 게임 카운트로 반환")
+    void getGameCountTest() {
+        //given
+        System.setIn(makeUserInput("5"));
+
+        //when
+        GameCount gameCount = input.getGameCount();
+
+        //then
+        assertThat(gameCount).extracting("gameCount").isEqualTo(5);
     }
 
     @DisplayName("테스트코드용 유저입력 메소드")
