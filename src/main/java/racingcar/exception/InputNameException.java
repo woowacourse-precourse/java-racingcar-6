6a961c4a1c1b75566d5144040251d61779eff4cb
Gyeonghun Min <riverpower6@naver.com>
2023-11-01 04:19:33 +0900

    Feat: 입력값 중복 시 예외 처리 추가

--- src/main/java/racingcar/exception/InputNameException.java
+++ src/main/java/racingcar/exception/InputNameException.java
@@ -1,5 +1,8 @@
 package racingcar.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputNameException {
 	static final int NAME_LENGTH = 5;
 
@@ -9,9 +12,18 @@ public class InputNameException {
 		}
 	}
 
+	public void checkDuplication(List<String> name, String[] input, int index) {
+		if (name.contains(input[index])) {
+			throw new IllegalArgumentException("자동차의 이름은 중복될 수 없습니다.");
+		}
+		name.add(input[index]);
+	}
+
 	public void checkException(String[] input) {
+		List<String> name = new ArrayList<>();
 		for (int i=0; i<input.length; i++) {
 			checkLength(input[i]);
+			checkDuplication(name, input, i);
 		}
 	}
 }
