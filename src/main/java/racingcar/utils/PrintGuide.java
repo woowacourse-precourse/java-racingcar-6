a691cf3fa5a64601e822c49802dab5a7b97c7618
olive-su <1466su@naver.com>
2023-11-01 03:12:45 +0900

    feat: 입력 및 출력 모듈 추가

--- src/main/java/racingcar/utils/PrintGuide.java
+++ src/main/java/racingcar/utils/PrintGuide.java
@@ -1,7 +1,9 @@
 package racingcar.utils;
 
-public class Print {
-    private final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+import racingcar.domain.Car;
+
+public class PrintGuide {
+    private final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String INPUT_COUNT_MSG = "시도할 회수는 몇회인가요?";
     private final String RESULT_MSG = "\n실행 결과";
     private final String WINNER_MSG = "최종 우승자 : ";
@@ -11,6 +13,14 @@ public class Print {
     }
     public void printInputCount () {
         System.out.println(INPUT_COUNT_MSG);
+    }
+    public void printRaceRound(Car car) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(car.getName());
+        sb.append(" : ");
+        sb.append("-".repeat(car.getScore()));
+
+        System.out.println(sb);
     }
     public void printResult () {
         System.out.println(RESULT_MSG);
