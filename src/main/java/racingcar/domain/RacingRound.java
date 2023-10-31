c099a120069b4cd0f14913d6f9bc0e2d75b37975
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-31 20:58:51 +0900

    refactor: 레이싱 라운드 클래스 이름 변경 및 검증 클래스 분리
    - 레이싱 라운드를 입력받아 저장하는 Round 클래스의 이름을 Round에서 RacingRound로 변경
    - 입력값에 대한 검증을 진행하는 클래스를 분리
    - 기존의 public 이였던 round를 private로 변경하고 getRound를 통해 값을 반환하도록 변경

---
+++ src/main/java/racingcar/domain/RacingRound.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import racingcar.util.RacingRoundValidator;
+
+public class RacingRound {
+    private final int round;
+
+    public RacingRound(String input) {
+        RacingRoundValidator.validateInputNotEmpty(input);
+        RacingRoundValidator.validateInteger(input);
+        int roundNumber = Integer.parseInt(input);
+        RacingRoundValidator.validateInputRange(roundNumber);
+        this.round = roundNumber;
+    }
+
+    public int getRound() {
+        return round;
+    }
+}
--- src/main/java/racingcar/util/RacingRoundValidator.java
+++ src/main/java/racingcar/util/RacingRoundValidator.java
@@ -1,27 +1,18 @@
-package racingcar.domain;
+package racingcar.util;
 
-public class Round {
-    public final int round;
+public class RacingRoundValidator {
     private static final String INVALID_INPUT_IS_EMPTY = "입력값이 공백입니다.";
     private final static String INVALID_INPUT_TYPE = "숫자가 아닌 값이 포함되어있습니다.";
     private final static String INVALID_INPUT_RANGE = "입력값은 자연수입니다.";
     private final static int MIN_RANGE = 1;
 
-    public Round(String input) {
-        validateInputNotEmpty(input);
-        validateInteger(input);
-        int roundNumber = Integer.parseInt(input);
-        validateInputRange(roundNumber);
-        this.round = roundNumber;
-    }
-
-    private void validateInputNotEmpty(String input) {
+    public static void validateInputNotEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(INVALID_INPUT_IS_EMPTY);
         }
     }
 
-    private static void validateInteger(String input) {
+    public static void validateInteger(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -29,7 +20,7 @@ public class Round {
         }
     }
 
-    private static void validateInputRange(int round) {
+    public static void validateInputRange(int round) {
         if (round < MIN_RANGE) {
             throw new IllegalArgumentException(INVALID_INPUT_RANGE);
         }
