5b484b5cb4370211199b954036efba53b9f6e16a
mmalfoy <stpmalfoy@gmail.com>
2023-11-01 04:43:08 +0900

    test(InputDesk) : 자동차 이름 길이 테스트코드작성

--- src/test/java/racingcar/domain/InputDeskTest.java
+++ src/test/java/racingcar/domain/InputDeskTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class InputDeskTest {
 
@@ -23,30 +25,22 @@ class InputDeskTest {
                 .hasMessageContaining("사용자 입력값 null");
     }
 
-    @Test
-    void checkIllegalArgumentException_파싱된_경주_자동차_이름_길이_0() {
-        String input = "";
+    @ParameterizedTest
+    @CsvSource({"\0,자동차 이름 길이 0", "abcdef,자동차 이름 길이 5 초과"})
+    void checkIllegalArgumentException_파싱된_경주_자동차_이름_길이(String input, String expected) {
         assertThatThrownBy(() -> inputdesk.checkIllegalArgumentException(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름 길이 0");
+                .hasMessageContaining(expected);
     }
 
-    @Test
-    void checkIllegalArgumentException_파싱된_경주_자동차_이름_길이_5초과() {
-        String input = "abcdef";
-        assertThatThrownBy(() -> inputdesk.checkIllegalArgumentException(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름 길이 5 초과");
-    }
 
     @Test
     void checkIllegalArgumentException_시도할_회수_입력_0이하() {
-        int input = (int)(Math.random()*100*-1);
+        int input = (int) (Math.random() * 100 * -1);
         assertThatThrownBy(() -> inputdesk.checkIllegalArgumentException(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도할 회수 입력값 0 이하");
     }
-
 
 
 }
