df79fe2f2bb052fa605c4853301ec389ac5d4601
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-01 03:39:50 +0900

    feat: 자동차 객체 생성기능 추가
    사용자에게 입력받은 값을 바탕으로 자동차 객체들을 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,45 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
     public static void main(String[] args) {
-        UserEnterCarName();
+        StartGame();
     }
 
-    private static void UserEnterCarName() {
-        String carName = readLine();
+    private static void StartGame() {
+        String carNameInput = UserEnterCarName();
+        Car[] cars = MakeCarObject(carNameInput);
+        for(int i=0; i<cars.length; i++){
+            System.out.println(cars[i].name+" "+cars[i].meter);
+        }
     }
+
+    private static String UserEnterCarName() {
+        return readLine();
+    }
+
+    private static Car[] MakeCarObject(String carNameInput) {
+        String[] carNames = carNameInput.split(",");
+        Car[] cars = new Car[carNames.length];
+        for(int i=0; i<carNames.length; i++){
+            cars[i] = new Car(carNames[i]);
+        }
+
+        return cars;
+    }
+
+    static class Car {
+        String name;
+        int meter;
+
+        public Car(String name) {
+            this.name = name;
+            int meter = 0;
+        }
+    }
+
 }
-
-
 
 
 //    pickNumberInRange(1,9);
