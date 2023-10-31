4090173e9c69b0b4e3889e1757a816eba2c672c6
NahyeChoi <skgp0628@naver.com>
2023-10-31 15:52:13 +0900

    feat(racingcar):자동차 경주게임 결과 출력 기능 구현

--- src/main/java/racingcar/PrintResult.java
+++ src/main/java/racingcar/PrintResult.java
@@ -1,13 +1,18 @@
 package racingcar;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class PrintResult {
-//	- 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다. viewRaceCar()
 	public void viewRaceCar(int totalCar, List<Integer> racingRecord, List<String> carName) {
 		int i = 0;
 		while(i < totalCar) {
@@ -18,26 +23,34 @@ public class PrintResult {
 			System.out.println();
 			i++;
 		}
-		
 	}
 	
-//	- 시도 횟수가 끝나면 경주 게임에서 우승자를 알려준다. (우승자는 한 명 이상일 수 있다, 쉼표로 구분) racingWinner()
-	public void racingWinner(List<Integer> racingResult, List<String> name) {
-		int win = Collections.max(racingResult);
+	public void racingWinner(List<Integer> carRecord, List<String> carName) {
 		
-//		while(computer.size() < 3) {
-//    		int randomNumber = Randoms.pickNumberInRange(1, 9);
-//    		if(!computer.contains(randomNumber)) {
-//    			computer.add(randomNumber);
-//    		}
-//    	}
-		for(int i=0; i<totalCar) {
-			if(racingResult.contains(win)) {
-				
-			}
+		List<Integer> winnerList = new ArrayList<Integer>();
+		int maxScore = maxScore(carRecord);
+		List<String> winner = gameList(carRecord, carName, maxScore);
+		
+		System.out.print("최종 우승자 : "+String.join(", ", winner));
+	}
+	
+	public int maxScore(List<Integer> carRecord) {
+		int maxScore = Collections.max(carRecord);
+		return maxScore;
+	}
+	
+	public List<String> gameList(List<Integer> carRecord, List<String> carName, int maxScore){
+		List<String> winner = new ArrayList<String>();
+		Map<String, Integer> racingList = new HashMap<String, Integer>();
+		
+		for(int i = 0; i < carName.size(); i++) {
+			racingList.put(carName.get(i), carRecord.get(i));
 		}
 		
-		System.out.print("최종 우승자 : "+winner);
+		winner = racingList.entrySet().stream()
+				.filter(entry-> Objects.equals(entry.getValue(), maxScore))
+				.map(Map.Entry::getKey)
+				.collect(Collectors.toList());
+		return winner;
 	}
-	
 } //end of class
