4ca5ef36e6d098bbcf164398c87d3ea5ccfb9631
“mpfo0106” <mpfo0106@gmail.com>
2023-10-31 10:34:58 +0900

    feat(racingcar): create inputCarNames TestCode

--- src/main/java/racingcar/domain/UserInputManager.java
+++ src/main/java/racingcar/domain/UserInputManager.java
@@ -2,5 +2,4 @@ package racingcar.domain;
 
 public class UserInputManager {
 
-
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -12,6 +12,41 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+
+    @Test
+    void split_메서드로_주어진_값을_구분() {
+        String input = "pobi,woni,jun";
+        String[] result = input.split(",");
+
+        assertThat(result).contains("woni", "pobi", "jun");
+        assertThat(result).containsExactly("pobi", "woni" , "jun");
+    }
+
+    @Test
+    void split_메서드_사용시_구분자가_포함되지_않은_경우_값을_그대로_반환() {
+        String input = "pobi";
+        String[] result = input.split(",");
+
+        assertThat(result).contains("pobi");
+    }
+
+    @Test
+    void 입력값이_5글자를_넘지_않는지() {
+        String[] carNames = {"pobi","woni","junho"};
+
+        for (String carName : carNames) {
+            assertThat(carName.length()).isLessThanOrEqualTo(5);
+        }
+    }
+
+    @Test
+    void 이름에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
@@ -23,13 +58,6 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
 
     @Override
     public void runMain() {
