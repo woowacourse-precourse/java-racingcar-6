a5fe95ce5025c0b473979e8d32700c3c2565292b
Jinnie Kim <jinniek48@gmail.com>
2023-10-31 20:33:36 +0900

    feat(racingcar): 우승자 안내 문구를 만드는 기능 구현

--- src/main/java/racingcar/domain/Leaderboard.java
+++ src/main/java/racingcar/domain/Leaderboard.java
@@ -7,45 +7,52 @@ public class Leaderboard {
     public Map<String, Integer> board = new LinkedHashMap<String, Integer>();
 
     public Leaderboard(List<String> names) {
-        for (String name : names){
+        for (String name : names) {
             board.put(name, 0);
         }
     }
 
-    public void status(){
-        for (String name : board.keySet()){
-            System.out.printf(name+" : ");
+    public void status() {
+        for (String name : board.keySet()) {
+            System.out.printf(name + " : ");
             distance(board.get(name));
             System.out.println();
         }
         System.out.println();
     }
 
-    public void distance(int n){
-        for (int i=0; i<n; i++){
+    public void distance(int n) {
+        for (int i = 0; i < n; i++) {
             System.out.print("-");
         }
     }
+
     public void winner() {
-        String result="최종 우승자 : ";
+        List<String> winnerList = new ArrayList<String>();
         int maxDistance = Collections.max(board.values());
-        int numberOfWinners = 0;
 
-        for (String name : board.keySet()){
-
-            if (maxDistance==board.get(name)) {
-
-                if (numberOfWinners == 0) {
-                    result = result + name;
-                } else {
-                    result = result + (", " + name);
-                }
-
-                numberOfWinners++;
+        for (String name : board.keySet()) {
+            if (maxDistance == board.get(name)) {
+                winnerList.add(name);
             }
         }
 
-        System.out.print(result);
+        System.out.print( winnerFormat(winnerList) );
+    }
+
+    public String winnerFormat(List<String> winnerList){
+        String format = "";
+
+        for (String name : winnerList) {
+            if (format.isEmpty()) {
+                format = format + name;
+            }
+            else {
+                format = format + (", " + name);
+            }
+        }
+
+        return "최종 우승자 : "+format;
     }
 
 }
