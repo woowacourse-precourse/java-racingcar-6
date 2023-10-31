0aa3d0a969497d63ee77e1c70e6fc0294c67c79a
JeongHunHui <deca3248@gmail.com>
2023-11-01 06:05:01 +0900

    refactor: 필요없는 클래스 제거

--- src/main/java/racingcar/constant/RacingMessage.java
+++
@@ -1,9 +0,0 @@
-package racingcar.constant;
-
-public class RacingMessage {
-
-    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
-    public static final String TRY_RESULT = "실행 결과";
-    public static final String RACING_WINNER = "최종 우승자 : ";
-}
