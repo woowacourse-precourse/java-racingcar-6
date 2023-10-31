4775bad129552d3d4d0306c41b06017bdb86402c
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-31 17:59:12 +0900

    test : toString 변환 테스트에 대한 DisplayName 추가

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,9 +1,8 @@
 package racingcar.domain.car;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,11 +19,11 @@ class CarTest {
     @DisplayName("차가 앞으로 전진합니다.")
     void moveForward() {
         // GIVEN
-        car = Car.createCar("pobi",0);
+        car = Car.createCar("pobi", 0);
         int moveTrial = 3;
 
         // WHEN
-        for (int i = 0; i < moveTrial ; i++) {
+        for (int i = 0; i < moveTrial; i++) {
             car.moveForward();
         }
 
@@ -33,11 +32,12 @@ class CarTest {
     }
 
     @Test
+    @DisplayName("Car가  전진 후 toString을 호출 시, 본인의 상태를 문자열로 반환합니다.")
     void testToString() {
         // GIVEN
-        car = Car.createCar("pobi",0);
+        car = Car.createCar("pobi", 0);
         int moveTrial = 3;
-        for (int i = 0; i < moveTrial ; i++) {
+        for (int i = 0; i < moveTrial; i++) {
             car.moveForward();
         }
         String expectedStatus = "pobi : ---";
@@ -46,6 +46,6 @@ class CarTest {
         String actualStatus = car.toString();
 
         // THEN
-        assertEquals(expectedStatus,actualStatus);
+        assertEquals(expectedStatus, actualStatus);
     }
 }
