da73863c23336f8d1feac4d68ce06f38a3d9408b
Donggeon Moon <nullsafer@naver.com>
2023-11-01 00:35:30 +0900

    feat: 예외 메시지 추가

--- src/main/java/racingcar/enums/ExceptionMessage.java
+++ src/main/java/racingcar/enums/ExceptionMessage.java
@@ -1,11 +1,12 @@
 package racingcar.enums;
 
 public enum ExceptionMessage {
-    OVER_FIVE_LETTERS("자동차 이름은 5자 이하로 입력해주세요."),
-    CONTAINING_MORE_THAN_ONE_CAR("자동차 이름을 2개 이상 입력해주세요."),
-    NOT_BLANK_NAME("이름은 빈 칸일 수 없습니다."),
-    NOT_TOO_LONG_INPUT("너무 긴 값을 입력했습니다."),
-    SHOULD_BE_NUMBER("숫자를 입력해주세요.");
+    SHOULD_BE_FIVE_LETTERS_OR_BELOW("자동차 이름은 5자 이하로 입력해주세요."),
+    SHOULD_INPUT_MORE_THAN_ONE_NAME("자동차 이름을 2개 이상 입력해주세요."),
+    NAME_CANNOT_BE_BLANK("이름은 빈 칸일 수 없습니다."),
+    TOO_LONG_INPUT("너무 긴 값을 입력했습니다."),
+    SHOULD_HAVE_UNIQUE_NAMES("이름이 중복되어서는 안 됩니다."),
+    SHOULD_INPUT_ONLY_NUMBER("숫자를 입력해주세요.");
 
     private final String message;
 
