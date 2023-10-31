e47f9a72c40d4dd4edfee6c752591b7b0a9a5cc8
myonjin <kjinabc@gmail.com>
2023-11-01 04:46:19 +0900

    Test : CarList와 Num 입력값 테스트 코드 작성

---
+++ src/test/java/racingcar/model/CarListTest.java
@@ -0,0 +1,30 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.CarList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarListTest {
+
+    private CarList carList;
+
+    @BeforeEach
+    public void setUp() {
+        carList = new CarList("test1,test2");
+    }
+
+    @Test
+    public void 중복없는이름() {
+        Car car = new Car("test3");
+        assertDoesNotThrow(() -> carList.isNameDupe(car));
+    }
+
+    @Test
+    public void 중복있는이름() {
+        Car car = new Car("test1");
+        assertThrows(IllegalArgumentException.class, () -> carList.isNameDupe(car));
+    }
+}
