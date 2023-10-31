3d75229b20ca69a1211f671d8097b36bf4dd18ed
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-10-31 23:48:25 +0900

    Test: Car 클래스 테스트 코드

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,52 +1,41 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     private Car car;
-    private InputStream standardIn; // inputCarName을 standardIn으로 변경
 
     @BeforeEach
     void setUp() {
         car = new Car();
-        standardIn = System.in;
     }
 
-    @AfterEach
-    public void cleanUp() {
-        System.setIn(standardIn); // restoreStreams을 cleanUp으로 변경
-    }
     @Test
-    @DisplayName("입력된 자동차 이름을 정상적으로 반환")
-    void successGetNames() {
+    @DisplayName("입력된 자동차 이름을 쉼표(,)를 기준으로 구분")
+    void testSuccessSplitInputNames() {
         String input = "pobi,woni,jun";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
 
         List<String> expected = List.of("pobi", "woni", "jun");
-        List<String> result = car.getNames();
+        List<String> result = car.splitInputNames(input);
 
         assertEquals(expected, result);
     }
 
     @Test
-    @DisplayName("입력된 자동차 이름을 쉼표(,)를 기준으로 구분하지 못하기")
-    void failGetNames() {
+    @DisplayName("입력된 자동차 이름을 쉼표(,)를 기준으로 구분")
+    void testFailSplitInputNames() {
         String input = "pobi,woni,jun";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
 
         List<String> expected = List.of("pobi,woni,jun");
-        List<String> result = car.getNames();
+        List<String> result = car.splitInputNames(input);
 
         assertNotEquals(expected, result);
     }
@@ -54,50 +43,45 @@ class CarTest {
     @Test
     @DisplayName("입력된 자동차 이름에 빈값 존재")
     void testGetNamesWithEmptyInput() {
-        String input = "\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        List<String> carNames = Arrays.asList("");
 
-        assertThrows(IllegalArgumentException.class, () -> car.getNames());
+        assertThatThrownBy(() -> car.checkEmpty(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력된 자동차 이름에 빈값 존재")
     void testGetNamesWithEmptyInputContainingEmptyValues() {
-        String input = "pobi,,woni\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        List<String> carNames = Arrays.asList("pobi", "", "jun");
 
-        assertThrows(IllegalArgumentException.class, () -> car.getNames());
+        assertThatThrownBy(() -> car.checkEmpty(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력된 자동차 이름의 마지막에 빈값 존재")
     void testGetNamesWithInvalidInput() {
-        String input = "pobi,woni,jun,\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        List<String> carNames = Arrays.asList("pobi", "woni", "jun", "");
 
-        assertThrows(IllegalArgumentException.class, () -> car.getNames());
+        assertThatThrownBy(() -> car.checkEmpty(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력된 자동차 이름에 중복 값 존재")
     void testGetNamesWithDuplicateInput() {
-        String input = "pobi,woni,jun,pobi\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        List<String> carNames = Arrays.asList("pobi", "woni", "jun", "pobi");
 
-        assertThrows(IllegalArgumentException.class, () -> car.getNames());
+        assertThatThrownBy(() -> car.checkDuplicate(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력된 자동차 이름에 5자 초과된 값 존재")
     void testGetNamesWithLongInput() {
-        String input = "pobi,woni,jun,longname\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        List<String> carNames = Arrays.asList("pobi", "woni", "jun", "longname");
 
-        assertThrows(IllegalArgumentException.class, () -> car.getNames());
+        assertThatThrownBy(() -> car.checkLength(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
