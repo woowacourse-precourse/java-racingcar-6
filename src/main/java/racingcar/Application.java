827c2f803162938418fa2bc5df97fafeff90af34
준써크 <kjs8960@inha.edu>
2023-10-31 15:53:02 +0900

    feat: 메인 함수 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = View.initGameView();
+        List<Car> cars = Controller.inputToCarList(input);
+
+        Integer cycle = View.getCycleView();
+
+        for (int i = 0; i < cycle; i++) {
+            Controller.isForward(cars);
+            View.resultView(i, cars);
+        }
+
+        View.winnerView(Controller.findWinner(cars));
     }
 }
