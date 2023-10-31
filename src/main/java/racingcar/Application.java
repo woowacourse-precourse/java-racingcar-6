c722c3e58ff26f53c1eb71ea29ea277bbdc031a6
donghoonyeom <dua5410773@naver.com>
2023-11-01 03:49:27 +0900

    style(Application): 코드 형식 및 일관성 향상
    Application 클래스의 코드 형식을 향상시켰습니다. 주요 변경사항은 다음과 같습니다:
    - 들여쓰기 일관성을 유지하도록 수정했습니다.
    - 불필요한 빈 줄을 제거했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,14 +9,14 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-            ConsoleView consoleView = new ConsoleView();
-            List<String> carNames = consoleView.getInputCarNames();
-            List<Car> cars = Car.createCars(carNames);
-            int numberOfAttempts = consoleView.readValidNumberOfAttempts();
-            consoleView.printMessage("");
+        ConsoleView consoleView = new ConsoleView();
+        List<String> carNames = consoleView.getInputCarNames();
+        List<Car> cars = Car.createCars(carNames);
+        int numberOfAttempts = consoleView.readValidNumberOfAttempts();
+        consoleView.printMessage("");
 
-            RacingGame racingGame = new RacingGame(cars);
-            GameController gameController = new GameController(racingGame, consoleView);
-            gameController.playGame(numberOfAttempts);
+        RacingGame racingGame = new RacingGame(cars);
+        GameController gameController = new GameController(racingGame, consoleView);
+        gameController.playGame(numberOfAttempts);
     }
 }
