d668a5685bfdbf9366d5fc27df8ce25d1200227b
Infikei <hckhong@naver.com>
2023-10-31 17:54:31 +0900

    test: 게임 결과를 검증하는 테스트 추가

--- src/test/java/racingcar/ApplicationSecondTest.java
+++ src/test/java/racingcar/ApplicationSecondTest.java
@@ -3,12 +3,62 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationSecondTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void 게임_결과에_대한_검증() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("aa,bb,cc", "1");
+                    assertThat(output()).contains("aa : ", "bb : -", "cc : ", "최종 우승자 : bb");
+                },
+                STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 게임_결과에_대한_검증_2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("aa,bb,cc", "1");
+                    assertThat(output()).contains("aa : ", "bb : -", "cc : -", "최종 우승자 : bb, cc");
+                },
+                STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 게임_결과에_대한_검증_3() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("aa,bb", "00003");
+                    assertThat(output()).contains("aa : ", "bb : -", "aa : -", "bb : --", "aa : --", "bb : ---",
+                                    "최종 우승자 : bb")
+                            .doesNotContain("aa : ---");
+                },
+                STOP, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 게임_결과에_대한_검증_4() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("aa,bb", "0000000003");
+                    assertThat(output()).contains("aa : ", "aa : -", "aa : --", "bb : -",
+                                    "최종 우승자 : aa")
+                            .doesNotContain("bb : --");
+                },
+                STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
+        );
+    }
 
     @Test
     void 긴_문자열_이름에_대한_예외_처리() {
