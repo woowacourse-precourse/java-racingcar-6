1928f845e58b8024c41d2420c8ec046eacd087b3
siwol025 <cyk99115695@gmail.com>
2023-11-01 00:49:20 +0900

    refactor(CarTest): Car에서 이동 거리 출력기능을 맡게됨에 따른 테스트 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,22 +3,27 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-    @DisplayName("이동한 거리 표현 테스트")
+    @DisplayName("이동한 거리 출력 테스트")
     @Test
-    void expressMoveDistanceTest() {
+    void showMoveDistanceTest() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
         Car car = new Car("test");
-        String expected = "test : --";
+        String expected = "test : --\n";
+
         car.moveForward(6);
         car.moveForward(7);
+        car.showMoveDistance();
 
-        String carDistance = car.expressMoveDistance();
-
-        assertThat(carDistance).isEqualTo(expected);
+        assertThat(out.toString()).isEqualTo(expected);
     }
 }
