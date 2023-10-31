c95e09e92155b48e024877114ce2f2837958e58e
KangDeok <rkdejr2321@naver.com>
2023-10-31 23:13:50 +0900

    [test] 우승자 선정 테스트

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -3,6 +3,7 @@ package racingcar.service;
 import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -34,6 +35,38 @@ class RacingGameServiceTest {
         assertThat("Car1").isEqualTo(racingCars.get(0).getCarName());
         assertThat("Car2").isEqualTo(racingCars.get(1).getCarName());
         assertThat("Car3").isEqualTo(racingCars.get(2).getCarName());
+    }
+
+    @Test
+    public void 우승자_선정(){
+        //given
+        RacingCar racingCar1 = new RacingCar();
+        racingCar1.initCar("Car1");
+        racingCar1.increaseForwardCount();
+
+        RacingCar racingCar2 = new RacingCar();
+        racingCar2.initCar("Car2");
+        racingCar2.increaseForwardCount();
+        racingCar2.increaseForwardCount();
+
+        RacingCar racingCar3 = new RacingCar();
+        racingCar3.initCar("Car3");
+        racingCar3.increaseForwardCount();
+        racingCar3.increaseForwardCount();
+
+        List<RacingCar> racingCars = new ArrayList<>();
+        racingCars.add(racingCar1);
+        racingCars.add(racingCar2);
+        racingCars.add(racingCar3);
+
+        //when
+        List<String> winners = racingGameService.getWinner(racingCars);
+
+        //then
+        assertThat(2).isEqualTo(winners.size());
+        assertThat("Car2").isEqualTo(winners.get(0));
+        assertThat("Car3").isEqualTo(winners.get(1));
+
     }
 
     @Test
