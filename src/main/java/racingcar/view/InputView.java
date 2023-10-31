7bee068ec779d083b85f9a4860622c14dc53cc93
PARK9898 <wjddjs21@naver.com>
2023-10-31 20:55:19 +0900

    "refactor: 변수 인라인화"

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.util.StringHandler;
 
 public class InputView {
 
-    public String InputCarNames() {
+    public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        return StringHandler.stringToList(input);
     }
 
-    public String InputTrialNumber() {
+    public int inputTrialNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        String input = Console.readLine();
+        return StringHandler.stringToInt(input);
     }
-
 }
