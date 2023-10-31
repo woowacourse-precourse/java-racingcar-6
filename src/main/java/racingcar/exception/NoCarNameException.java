de373da1d30ae66ad6e797f0f99f99ca83de7eb7
khabh1 <kshababe1@gmail.com>
2023-10-31 15:37:01 +0900

    fix(NoCarNameException): 누락된 생성자 추가

--- src/main/java/racingcar/exception/NoCarNameException.java
+++ src/main/java/racingcar/exception/NoCarNameException.java
@@ -2,4 +2,8 @@ package racingcar.exception;
 
 public class NoCarNameException extends IllegalArgumentException {
     private static final String message = "자동차 이름이 입력되지 않았습니다.";
+
+    public NoCarNameException() {
+        super(message);
+    }
 }
