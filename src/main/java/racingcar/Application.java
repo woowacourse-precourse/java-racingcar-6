cc4c64666b5bc1d61957d66b52a2e375e663b7ee
JeonJW24 <tyui153@naver.com>
2023-10-31 22:18:57 +0900

    feat(racingcar): 메인 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.Generator;
+import racingcar.domain.InvalidValue;
+import racingcar.domain.RaceCar;
+import racingcar.domain.UserInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserInput userInput = new UserInput();
+        RaceCar raceCar = new RaceCar(userInput.carName());
+        int moveNumber = userInput.moveNumber();
+        System.out.println();
+        System.out.println("실행 결과");
+        Generator generator = new Generator();
+        while (moveNumber != 0) {
+            List<Integer> randomNumbers = generator.createRandomNumbers(raceCar.cars.size());
+            raceCar.forward(randomNumbers);
+            for (int i = 0; i < raceCar.cars.size(); i++) {
+                System.out.println(raceCar.cars.get(i) + " : " + raceCar.moveDistance.get(i));
+            }
+            System.out.println();
+            moveNumber--;
+        }
+        System.out.println("최종 우승자 : " + raceCar.winner());
     }
 }
