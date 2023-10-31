c1ef233c25f7252f992f42334b9ba131feb94636
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-31 16:21:29 +0900

    프로그램을 시작하는 Application 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.CommandLineController;
+import racingcar.controller.Controller;
+import racingcar.game.CarRaceGame;
+import racingcar.game.Game;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new CarRaceGame();
+        Controller controller = new CommandLineController(game);
+        controller.start();
     }
 }
