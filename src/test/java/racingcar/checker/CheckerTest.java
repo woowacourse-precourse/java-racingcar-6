74399f8e93bb2be201f33ad7d36f44bef8919560
ImWonYong <ssassb123@daum.net>
2023-11-01 03:04:25 +0900

    test(Checker): 승리한 사람들 체크 기능 테스트

--- src/test/java/racingcar/checker/CheckerTest.java
+++ src/test/java/racingcar/checker/CheckerTest.java
@@ -3,10 +3,37 @@ package racingcar.checker;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.Checker;
 
 public class CheckerTest {
+
+    @Test
+    void 승리한_사람() {
+        List<Car> cars = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+
+        cars.get(0).goStraight();
+
+        List<Car> winners = Checker.checkWinners(cars);
+        assertThat(winners).hasSize(1);
+
+        List<String> winnerNames = winners.stream().map(car -> car.getName()).collect(Collectors.toList());
+        assertThat(winnerNames).containsExactly("pobi");
+    }
+
+    @Test
+    void 승리한_사람들() {
+        List<Car> cars = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+
+        List<Car> winners = Checker.checkWinners(cars);
+        assertThat(winners).hasSize(3);
+
+        List<String> winnerNames = winners.stream().map(car -> car.getName()).collect(Collectors.toList());
+        assertThat(winnerNames).containsExactly("pobi", "woni", "jun");
+    }
 
     @Test
     void 값이_4_이상일_때_확인() {
