0f70da54e2b9db6ca79654f4553f4043872c33d5
Yujin <tkfkd7038@naver.com>
2023-10-31 16:47:47 +0900

    feat(racingcar): 사용자에게 n대의 자동차 이름과 시도할 횟수 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        NumberGenerator Generator = new NumberGenerator();
-        System.out.println(Generator.createRandomNumber());
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carsInput = Console.readLine();
+        String[] carNameList = carsInput.split(",");
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int attempts = Integer.parseInt(Console.readLine());
+
     }
 }
