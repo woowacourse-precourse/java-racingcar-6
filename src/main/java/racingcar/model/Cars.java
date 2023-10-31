4c061578568cf288eb0ed5763138991c97c35e27
seona-moon <fhflwhwl5@gmail.com>
2023-10-31 10:45:46 +0900

    feat: Car 객체들을 관리하기 위한 Cars 클래스 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,17 +1,26 @@
-package racingcar.service;
+package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.model.Car;
 
-public class GenerateCar {
-  private List<Car> carList = new ArrayList<>();
-  public GenerateCar(List<String> nameList) {
+public class Cars {
+
+  private List<Car> carList;
+
+  public Cars(List<String> nameList) {
+    this.carList = new ArrayList<>();
     for (String name : nameList) {
       carList.add(new Car(name));
     }
   }
+
   public List<Car> getCarList() {
     return carList;
+  }
+
+  public void moveCars() {
+    for (Car car : carList) {
+      car.setMovement();
+    }
   }
 }
