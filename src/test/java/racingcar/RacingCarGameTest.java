82c81eb705b1c3ddb3d7cc011318f903fd38ac91
Lee ChanHui <ing3616062@naver.com>
2023-10-31 12:12:47 +0900

    docs: 예외 사항 추가
    중복된 이름 입력 시 발생하는 예외 추가

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -1,11 +1,15 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Player;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarGameTest {
 
+    RacingCarGame racingCarGame = new RacingCarGame();
+
+    //구분자로 String 나누기 확인
     @Test
     void inputCarName() {
          String input = "1,2";
@@ -14,5 +18,5 @@ class RacingCarGameTest {
          assertThat(result).contains("2", "1");
          assertThat(result).containsExactly("1", "2");
     }
-
+    
 }
