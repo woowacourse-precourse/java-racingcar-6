2e4c2cc53029f3c0049b8aa85ef603eb1a50cfe3
gonixxxx <akdntm946@gmail.com>
2023-11-01 02:44:01 +0900

    style: code formatting

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -13,6 +13,7 @@ public class InputTest {
     @Test
     void 이름_길이_예외_처리() {
         List<String> input = List.of("pobiiiii", "pobi", "goni");
+
         assertThatThrownBy(() -> InputValidator.checkNameLength(input)).isInstanceOf(
                 IllegalArgumentException.class);
     }
@@ -20,6 +21,7 @@ public class InputTest {
     @Test
     void 쉼표_이외_구분자_예외_처리() {
         String input = "pobi.goni!godoli,jun";
+
         assertThatThrownBy(() -> InputValidator.checkSeparator(input)).isInstanceOf(
                 IllegalArgumentException.class);
     }
@@ -27,6 +29,7 @@ public class InputTest {
     @Test
     void 이름_중복_예외_처리() {
         List<String> input = List.of("pobi", "gorani", "goni", "goni");
+
         assertThatThrownBy(() -> InputValidator.checkNameIsDuplicated(input)).isInstanceOf(
                 IllegalArgumentException.class);
     }
@@ -42,6 +45,7 @@ public class InputTest {
     @Test
     void 이름_미입력_예외_처리() {
         String input = "  ,goni,jun, ";
+
         assertThatThrownBy(() -> InputValidator.checkNameIsExist(
                 StringConvertor.convertStringToList(input))).isInstanceOf(
                 IllegalArgumentException.class);
