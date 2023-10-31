99e544bca3590bff8d2e03c4bce996b55fe044c4
이정민 <jmlee2705@gmail.com>
2023-11-01 05:09:21 +0900

    feat: Game 스켈레톤 코드 작성

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Game {
+
+    private List<Car> cars;
+    private List<Car> winners;
+    private int totalTrialCount;
+    private int curTrialCount = 0;
+
+    public void inputCarNames() {
+
+    }
+
+    public void inputTrialCount() {
+
+    }
+
+    public void judgeWinner() {
+
+    }
+
+    public void printWinner() {
+
+    }
+
+}
