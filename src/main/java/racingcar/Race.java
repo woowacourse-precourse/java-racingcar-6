b7130741c22fc70d5d1d9285d76b1075a322eb9f
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-11-01 01:05:14 +0900

    feat(car-input): implement name input and validation methods
    - Implement inputRaceCars() for splitting car names and storing in a list.
    - Implement nameValidation() to throw IllegalArgumentException for invalid car names.
    - Implement makeRaceCars() to create RaceCar object lists from name string list.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,9 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Race {
@@ -12,4 +13,26 @@ public class Race {
     public Race(){
      raceCarList = new ArrayList<>();
     }
+
+    private List<String> inputRaceCars(){
+        String input = Console.readLine();
+        List<String> names = Arrays.asList(input.split(","));
+        nameValidation(names);
+        return names;
+    }
+    private void nameValidation(List<String> names){
+        for(String str : names){
+            if( str.length() >5 || str.length() ==0)
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    public void makeRaceCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> names = inputRaceCars();
+        for(String str : names){
+            raceCarList.add(new RaceCar(str));
+        }
+    }
+
 }
