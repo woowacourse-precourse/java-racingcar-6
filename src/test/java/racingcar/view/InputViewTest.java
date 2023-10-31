e0877559f744b49bc2459e5d354bd46092e9c5c9
xogus3492 <wkdxogus3492@gmail.com>
2023-10-31 16:14:49 +0900

    Test(InputViewTest): 시도 횟수 입력 테스트 추가

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,8 +1,9 @@
 package racingcar.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.ByteArrayInputStream;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,21 @@ public class InputViewTest {
         final List<String> list = inputView.inputCarName();
 
         //then
-        Assertions.assertThat(list).contains("pobi", "java");
+        assertThat(list).contains("pobi", "java");
+    }
+
+    @Test
+    @DisplayName("시도 횟수 입력 테스트")
+    void trialCountInputTest() {
+        //given
+        InputView inputView = new InputView();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("10".getBytes());
+        System.setIn(byteArrayInputStream);
+
+        //when
+        final int trialCount = inputView.inputTrialCount();
+
+        //then
+        assertThat(trialCount).isEqualTo(10);
     }
 }
