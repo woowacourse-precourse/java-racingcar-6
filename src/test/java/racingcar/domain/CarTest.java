279055e0d184ebf3fc692e71dbe83e37b483d95c
m5s3 <devmyoungsoo2@gmail.com>
2023-10-31 20:56:01 +0900

    test(racingcar): 자동차 이름 예외 테스트는 `CarsGenerator` 보다 `Car`에서 하는것이 타당해 보여서 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,19 +2,21 @@ package racingcar.domain;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+
     @Test
     void 전진() {
         Car car = Car.of("test");
         assertRandomNumberInRangeTest(
-            () -> {
-                car.move();
-                assertThat(car.getPosition()).isEqualTo(1);
-            },
-            4
+                () -> {
+                    car.move();
+                    assertThat(car.getPosition()).isEqualTo(1);
+                },
+                4
         );
     }
 
@@ -28,5 +30,14 @@ class CarTest {
                 },
                 2
         );
+    }
+
+    @Test
+    void 주어진_값으로_자동차_생성시_이름이_6자_글자_이상이면_예외발생() {
+        String input = "pobibibi";
+
+        assertThatThrownBy(() -> Car.of(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
 }
--- src/test/java/racingcar/domain/CarsGeneratorTest.java
+++ src/test/java/racingcar/domain/CarsGeneratorTest.java
@@ -1,9 +1,8 @@
 package racingcar.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarsGeneratorTest {
@@ -14,14 +13,5 @@ class CarsGeneratorTest {
         List<Car> cars = CarsGenerator.generateCars(input);
         assertThat(cars).contains(Car.of("pobi"), Car.of("woni"), Car.of("jun"));
         assertThat(cars.size()).isEqualTo(3);
-    }
-
-    @Test
-    void 주어진_값으로_자동차_생성시_이름이_6자_글자_이상이면_예외발생() {
-        String input = "pobibibi,woni,jun";
-
-        assertThatThrownBy(() -> CarsGenerator.generateCars(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
 }
