36b5a475a7adcf6c45c1d0e590c5c42a21081c04
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 04:50:42 +0900

    feat : Car 클래스 랜덤 수에 따라 전진하는 기능 테스트 코드 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,17 +1,30 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    private static final int DEPART_NUM = 4;
     private Car car;
+    private static final String name = "jun";
+    @BeforeEach
+    void setUp(){
+        car = new Car(name);
+    }
+    private static final int DEPART_NUM = 4;
+    private static final int CANNOT_GO = 3;
+
     @Test
     void 수가_랜덤_중_4_이상_일_때_직진(){
-        Car car = new Car("pobi");
         car.goForward(DEPART_NUM);
         assertThat(car.getForward()).isEqualTo(1);
+    }
+
+    @Test
+    void 수가_랜덤_중_4가_안_될_때(){
+        car.goForward(CANNOT_GO);
+        assertThat(car.getForward()).isEqualTo(0);
     }
 
 
