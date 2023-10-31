7c9e13ca31b35231a9feea11ff02380c310cec9a
i3kae <snoger1234@gmail.com>
2023-10-31 11:11:04 +0900

    feat : 자동차 게임 진행 기능 추가 1. 자동차 게임을 진행하는 기능 추가 2. Random 값을 만들어서 해당 값이 4 이상이면 차를 한칸 앞으로 이동

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingGame {
+        public void game(List<Racingcar> cars, int roundNum){
+                for (int i = 0; i < roundNum; i++){
+                        for (int j = 0; j < cars.size(); j++){
+                                carMove(cars.get(j));
+                        }
+                        currentStat(cars);
+                }
+        }
+
+        private void carMove(Racingcar car){
+                if (4 <= makeRandomNum()){
+                        car.upMoveDistance();
+                }
+        }
+
+        private int makeRandomNum(){
+                return Randoms.pickNumberInRange(0, 9);
+        }
+
+        private void currentStat(List<Racingcar> cars){
+                for (int i = 0; i < cars.size(); i++){
+                        System.out.print(cars.get(i).getName());
+                        System.out.print(" : ");
+                        for (int j = 0; j < cars.get(i).getMoveDistance(); j++){
+                                System.out.print("-");
+                        }
+                        System.out.println();
+                }
+                System.out.println();
+        }
+}
