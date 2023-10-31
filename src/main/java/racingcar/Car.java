650169d036a69146273ef02b79883b337007c8d2
yang <hc1489@naver.com>
2023-10-31 18:14:17 +0900

    Car 클래스의 생성자 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,6 +6,10 @@ public class Car {
     private String distance = "";
     private boolean canGo;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     private void determineCanGo() {
         int randomNumber = Randoms.pickNumberInRange(0,9);
 
