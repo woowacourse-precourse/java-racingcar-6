bb770a81367e5ad674a7b19f6efa9a7fe50d5cae
dlrjs2360 <lhg3670@naver.com>
2023-10-31 15:07:46 +0900

    :sparkles: feat: 우승자 출력 로직 개발
    - 단독과 복수 우승자를 구분하지 않고 우승자를 출력하는 기능을 개발했습니다.
    - maxPosition 변수를 따로 생성하여 메모리 낭비를 최소화했습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,30 +1,39 @@
 package racingcar.model;
 
+import java.util.stream.IntStream;
 import racingcar.constants.GameInfo;
+import racingcar.controller.RacingGameController;
 import racingcar.util.GameUtil;
 
 public class Car {
 
-    private final String car;
+    private final String carName;
     private int position;
 
-    public Car(String car) {
-        this.car = car;
+    public Car(String carName) {
+        this.carName = carName;
         this.position = 0;
     }
 
     public void move() {
         if (GameUtil.getMoveOrStop()) {
             position++;
+            RacingGameController.updateMaxPosition(position);
         }
     }
 
     public void print() {
         StringBuilder sb = new StringBuilder();
-        sb.append(car).append(" : ");
-        for (int i = 0; i < position; i++) {
-            sb.append(GameInfo.CAR_MOVE_SYMBOL);
-        }
+        sb.append(carName).append(" : ");
+        IntStream.range(0, position).forEach(i -> sb.append(GameInfo.CAR_MOVE_SYMBOL));
         System.out.println(sb);
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getCarName() {
+        return carName;
     }
 }
--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -2,6 +2,8 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.controller.RacingGameController;
 
 public class CarList {
     private final List<Car> carList = new ArrayList<>();
@@ -18,5 +20,15 @@ public class CarList {
         carList.forEach(Car::print);
     }
 
+    public List<String> getWinners() {
+        int maxPosition = RacingGameController.getMaxPosition();
+        return carList.stream()
+            .filter(car -> isWinner(car.getPosition(), maxPosition))
+            .map(Car::getCarName).collect(Collectors.toList());
+    }
+
+    public boolean isWinner(int position, int maxPosition) {
+        return position == maxPosition;
+    }
 
 }
