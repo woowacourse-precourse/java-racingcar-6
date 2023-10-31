92aac881f2d86511e07495abdf001c7fa483d21b
Nara Park <skfk6352@naver.com>
2023-11-01 06:42:04 +0900

    <test>: check appropriate message is output when exception occurs
    예외가 발생할 경우 적절한 예외가 발생하는지 확인하는 테스트 메서드로 리팩토링

--- src/test/java/racingcar/validation/CarNameValidatorTest.java
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -1,9 +1,11 @@
 package racingcar.validation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.constant.ExceptionMessage;
 
 class CarNameValidatorTest {
 
@@ -11,14 +13,16 @@ class CarNameValidatorTest {
 
     @Test
     void 자동차_이름이_5자를_초과할경우_예외발생() {
-        assertThrows(IllegalArgumentException.class,
-                () -> carNameValidator.validateCarName(List.of("lovejava")));
+        assertThatThrownBy(() -> carNameValidator.validateCarName(List.of("nara", "lovenara")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.CAR_NAME_LENGTH.getMessage());
     }
 
     @Test
     void 자동차_이름이_중복일경우_예외발생() {
-        assertThrows(IllegalArgumentException.class,
-                () -> carNameValidator.validateCarName(List.of("nara", "nara")));
+        assertThatThrownBy(() -> carNameValidator.validateCarName(List.of("nara", "nara")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.CAR_NAME_DUPLICATION.getMessage());
     }
 
     @Test
@@ -29,13 +33,15 @@ class CarNameValidatorTest {
 
     @Test
     void 자동차_이름에_특수문자가있으면_예외발생() {
-        assertThrows(IllegalArgumentException.class,
-                () -> carNameValidator.validateCarName(List.of("nara", "na^^")));
+        assertThatThrownBy(() -> carNameValidator.validateCarName(List.of("nara", "n^^a")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.CAR_NAME_TYPE.getMessage());
     }
 
     @Test
     void 자동차_이름에_빈칸이_있을경우_예외발생() {
-        assertThrows(IllegalArgumentException.class,
-                () -> carNameValidator.validateCarName(List.of("nara", "na ra")));
+        assertThatThrownBy(() -> carNameValidator.validateCarName(List.of("nara", "n ra")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.CAR_NAME_TYPE.getMessage());
     }
 }
