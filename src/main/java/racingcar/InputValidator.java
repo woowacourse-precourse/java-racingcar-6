69b0b498694a98d0d90ed5697dbb7a62b0311c9b
daneng4 <daneng4@naver.com>
2023-11-01 00:18:23 +0900

    fix: 자동차 이름 미 입력 예외 처리 문구 수정

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InputValidator {
 
     private static final String ILLEGAL_NAME_SIZE_EXCEPTION_MESSAGE = "자동차 이름은 5자리 이하로 만들어야 합니다";
-    private static final String VOID_NAME_EXCEPTION_MESSAGE = "자동차 이름을 입력해주세요.";
+    private static final String VOID_NAME_EXCEPTION_MESSAGE = "자동차 이름은 공백으로 설정할 수 없습니다.";
     private static final String DUPLICATE_NAME_EXCEPTION_MESSAGE = "자동차 이름이 중복됩니다.";
     private static final String ZERO_ATTEMPT_NUMBER_EXCEPTION_MESSAGE = "시도 횟수는 1이상이어야 합니다.";
     private static final String ILLEGAL_ATTEMPT_NUMBER_EXCEPTION_MESSAGE = "시도 횟수는 숫자여야 합니다.";
