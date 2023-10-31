489f054eb69a5e14bfb5378bc718eb9d45d9dd01
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 10:33:06 +0900

    Test: move테스트 코드 추가
    움직일 수 있는 Car와 움직일 수 없는 Car객체를 Cars컬렉션에 추가한 후 move를 시행한다.
    움직이는 차는 시행 횟수(5)만큼 position이 증가하고(5), 움직일 수 없는 차는 증가하지 않는다(0)

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+class CarsTest {
+    
+    Car movableCar1;
+    Car movableCar2;
+    Car immovableCar;
+    int raceRound = 5;
+    Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        movableCar1 = new Car("ABC", () -> 4);
+        movableCar2 = new Car("DEF", () -> 4);
+        immovableCar = new Car("GHI", () -> 3);
+        cars = new Cars(Arrays.asList(movableCar1, movableCar2, immovableCar));
+    }
+
+    @Test
+    @DisplayName("차들의 position을 얻으면 (5,5,0)과 같다")
+    void move테스트() {
+
+        for (int i = 0; i < raceRound; i++) {
+            cars.move();
+        }
+        List<Integer> positions = cars.getCars().stream()
+                .map(Car::getPosition)
+                .toList();
+
+        Assertions.assertEquals(Arrays.asList(5, 5, 0), positions);
+    }
+
+}
