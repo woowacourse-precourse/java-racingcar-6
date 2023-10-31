4e1268b4fb38c54e50b443633d78280bafa165af
seokmyungham <hellotjraud@gmail.com>
2023-11-01 04:32:55 +0900

    feat(GameView): 최종 우승자 출력을 위한 상수 추가 및 printFinalWinners 메서드 추가
    printFinalWinners: 최종 우승자 문자열을 매개변수로 받아서 출력합니다.

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -6,6 +6,7 @@ public class GameView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RACE_RESULT = "실행 결과";
+    private static final String FINAL_WINNERS = "최종 우승자 : ";
 
     public void printCarNamesMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -27,5 +28,9 @@ public class GameView {
 
     public void printRaceResult() {
         System.out.println(RACE_RESULT);
+    }
+
+    public void printFinalWinners(String winners) {
+        System.out.print(FINAL_WINNERS + winners);
     }
 }
