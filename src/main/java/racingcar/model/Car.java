be77d6d53d946b73f0d3deb1c371a996f3033a4d
SuRiimm <surimii45@gmail.com>
2023-11-01 03:31:08 +0900

    feat(racingcar): Car(자동차) Model 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,23 @@
 package racingcar.model;
 
 public class Car {
+  private String name;
+  private int position;
+
+  public Car(String name){
+    this.name = name;
+    this.position = 0;
+  }
+
+  public String getName(){
+    return name;
+  }
+  public  int getPosition(){
+    return position;
+  }
+  public void move(int randomValue){
+    if(randomValue >= 4){
+      position++;
+    }
+  }
 }
