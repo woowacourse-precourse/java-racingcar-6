150e8274ae580290f6b4420c793d8b0eaf67080c
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-31 15:50:31 +0900

    feat: add game start function to game controller

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.model.Race;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -7,10 +9,18 @@ public class GameController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private Race race;
 
     public GameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+    }
+
+    public void start() {
+        List<String> carNameList = inputView.getCarName();
+        int trialNum = inputView.getTrialNum();
+        race = new Race(trialNum, carNameList);
+
     }
 
 }
