edc32934d242035dc4c04c9e3b3a677c1ce49efa
itsmo1031 <itsmo1031@gmail.com>
2023-10-31 23:16:41 +0900

    feat(car): 자동차 클래스 작성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import static racingcar.constant.Console.PROGRESS;
+import static racingcar.constant.Console.SEPARATOR;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void move() {
+        this.distance += 1;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + SEPARATOR.message() + PROGRESS.message().repeat(distance);
+    }
+}
