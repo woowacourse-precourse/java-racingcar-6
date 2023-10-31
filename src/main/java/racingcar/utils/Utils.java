fca8ba6e7946e8cf1496e3e6636c48831d9fb480
bbbang105 <2018111366@dgu.ac.kr>
2023-10-31 23:12:20 +0900

    feat : HashMap의 Values 중 최대값을 반환하는 기능 구현
    measureMaxNumInHashMapValues() 메소드 구현

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -2,9 +2,17 @@ package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
 public final class Utils {
 
     public static int generateRandomNumber() {
         return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0,9);
+    }
+
+    public static int measureMaxNumInHashMapValues(HashMap<String,String> hashMap) {
+        return Collections.max(hashMap.values()).length();
     }
 }
