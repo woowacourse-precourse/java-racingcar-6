7de74f77a65d098d53d04bbb70efc969d5f732af
unknown <winternrabbit@gmail.com>
2023-10-31 19:01:38 +0900

    무작위 문자/숫자 입력하는 테스트코드 작성

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 class GameTest extends NsTest {
@@ -55,6 +57,30 @@ class GameTest extends NsTest {
                 assertThatThrownBy(() -> runException("moi,rei", "ab가"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 무작위_값_입력_테스트() {
+        Random random = new Random();
+        List<Integer> randomAC = random.ints(33, 123)
+                .limit(25)
+                .boxed().toList();
+        List<String> randomCarNames = Arrays.asList("","","","","");
+        String randomNumber = "" + random.nextInt(1, 1001);
+
+        for (int i = 0; i < randomAC.size(); i++) {
+            int listIndex = i / 5;
+            int ascii = randomAC.get(i);
+            String carName = randomCarNames.get(listIndex);
+            randomCarNames.set(listIndex, carName + (char) ascii);
+        }
+
+        String randomCarNamesString = String.join(",", randomCarNames);
+
+        System.out.println("자동차 이름: " + randomCarNamesString);
+        System.out.println("횟수: " + randomNumber);
+
+        assertSimpleTest(() -> run(randomCarNamesString, randomNumber));
     }
 
     @Override
