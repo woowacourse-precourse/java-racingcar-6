fd18e8236b1f2ed2da7178de050bcf5011a0940c
Montep95 <sewd1224@gmail.com>
2023-11-01 00:38:25 +0900

    [feat] Using readLine(), Print message()
    Connect to Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,22 @@
 package racingcar;
 
 import racingcar.controller.Stadium;
+import racingcar.policy.RacingCarGameMovingPolicy;
+import racingcar.policy.RandomNumberGeneratePolicy;
+import racingcar.view.Console;
+import racingcar.view.MissionUtilsInput;
+import racingcar.view.PrintStreamOutput;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        Stadium race = new Stadium();
-        race.start();
+        new Stadium(
+                new Console(
+                        new MissionUtilsInput(), new PrintStreamOutput()
+                ),
+                new RacingCarGameMovingPolicy(),
+                new RandomNumberGeneratePolicy()
+        ).start();
     }
 }
