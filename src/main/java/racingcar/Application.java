af9a7d3d9ae985c31fbac4ba8c5caf2a049f5f06
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-31 22:20:02 +0900

    [REFACTOR] 사용하지 않는 imports 삭제
    .

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,5 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
