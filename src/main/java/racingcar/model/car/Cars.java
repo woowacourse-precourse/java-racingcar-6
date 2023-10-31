5abf251717faf1b81b03ce32d95a27c95aa0d97c
gimdongha <d11210920@gmail.com>
2023-10-31 14:12:44 +0900

    feat(Cars) : cars 클래스 정의
    - car를 list형태로 저장하는 cars attribute
    - string형태로 전달받은 매개변수로부터 쉼표를 구분으로 car를 구분하여 저장하는 생성자
    - getter

---
+++ src/main/java/racingcar/model/car/Cars.java
@@ -0,0 +1,23 @@
+package racingcar.model.car;
+
+import racingcar.constants.Texts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars(String names) {
+        cars = new ArrayList<>();
+        String[] name = names.split(Texts.COMMAS.toString());
+        for (String s : name) {
+            Car car = new Car(s);
+            cars.add(car);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
