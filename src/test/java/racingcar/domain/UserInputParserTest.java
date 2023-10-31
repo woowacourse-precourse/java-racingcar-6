00ceb4f5476624d6c6943775953947c110fed45a
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 06:46:37 +0900

    test(domain.UserInputParser) : 기능2. 사용자 입력값을 쉼표기준으로 분할하여 carList를 생성
    차량이 1대, 2대, 3대일 경우 carList가 잘 생성 되는지 확인하는 테스트 코드와 차량의 이름이 5자 초과일 때 예외를 발생하는 지 확인하는 테스트 코드를 작성하였습니다.

--- src/test/java/racingcar/domain/UserInputParserTest.java
+++ src/test/java/racingcar/domain/UserInputParserTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 
 class UserInputParserTest {
     private UserInputParser userInputParser;
@@ -36,6 +38,55 @@ class UserInputParserTest {
     }
 
     @Test
-    void generateCarList() {
+    void generateCarList_메서드로_사용자의_입력값을_carList로_변형_차량_1대() {
+        // given
+        String userInput = "A";
+
+        // when
+        List<Car> carList = userInputParser.generateCarList(userInput);
+
+        // then
+        assertThat(carList).hasSize(1);
+        assertThat(carList.get(0).getName()).isEqualTo("A");
+    }
+
+    @Test
+    void generateCarList_메서드로_사용자의_입력값을_carList로_변형_차량_2대() {
+        // given
+        String userInput = "A,B";
+
+        // when
+        List<Car> carList = userInputParser.generateCarList(userInput);
+
+        // then
+        assertThat(carList).hasSize(2);
+        assertThat(carList.get(0).getName()).isEqualTo("A");
+        assertThat(carList.get(1).getName()).isEqualTo("B");
+    }
+
+    @Test
+    void generateCarList_메서드로_사용자의_입력값을_carList로_변형_차량_3대() {
+        // given
+        String userInput = "A,B,C";
+
+        // when
+        List<Car> carList = userInputParser.generateCarList(userInput);
+
+        // then
+        assertThat(carList).hasSize(3);
+        assertThat(carList.get(0).getName()).isEqualTo("A");
+        assertThat(carList.get(1).getName()).isEqualTo("B");
+        assertThat(carList.get(2).getName()).isEqualTo("C");
+    }
+
+    @Test
+    void generateCarList_메서드로_사용자의_입력값을_carList로_변형_중_차량의_이름이_5글자_초과면_에러발생() {
+        // given
+        String userInput = "A,B,CDEFGH";
+
+        // when, then
+        assertThatThrownBy(() -> userInputParser.generateCarList(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("차량 이름은 5글자 이하이어야 합니다.");
     }
 }
