bd2aa7a7a33cc030e6132dd9895441d4f48f2976
seona-moon <fhflwhwl5@gmail.com>
2023-10-31 10:48:10 +0900

    feat: 게임 시스템을 관리하는 GameContorller 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,2 +1,47 @@
-package racingcar.controller;public class GameController {
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+
+  private final OutputView outputView;
+  private final InputView inputView;
+  private Cars racingCars;
+  private int gameCount;
+
+  public GameController() {
+    this.outputView = new OutputView();
+    this.inputView = new InputView();
+  }
+
+  public void run() {
+    initializeGame();
+    startGame();
+    endGame();
+  }
+
+  private void initializeGame() {
+    outputView.displayCarName();
+    List<String> nameList = inputView.getCarName();
+    outputView.displayGameCount();
+    gameCount = inputView.getCount();
+    racingCars = new Cars(nameList);
+  }
+
+  private void startGame() {
+    outputView.displayStart();
+    while (gameCount != 0) {
+      gameCount--;
+      racingCars.moveCars();
+      outputView.displayMovement(racingCars.getCarList());
+    }
+  }
+
+  private void endGame() {
+    List<String> winnerList = racingCars.getWinnerList();
+    outputView.displayWinner(winnerList);
+  }
 }
