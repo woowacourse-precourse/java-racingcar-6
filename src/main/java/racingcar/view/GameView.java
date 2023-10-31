fbeb3f44931360668fbf37e91601678b4e0d4c5f
mseo39 <a01063294905@gmail.com>
2023-11-01 04:47:16 +0900

    Feat: View 멤버변수

---
+++ src/main/java/racingcar/view/GameView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+import racingcar.model.Game;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GameView {
+    static final String CAR_NAME_INPUT_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String TRY_NUM_PHRASE = "시도할 회수는 몇회인가요?";
+    static final String MOVE_FORWARD_MARK = "-";
+    static final String CLONE = " : ";
+    static final String DELIMITER = ", ";
+    static final String RESULT_PHRASE = "\n실행 결과";
+    static final String WINNER_PHRASE = "최종 우승자";
+}
