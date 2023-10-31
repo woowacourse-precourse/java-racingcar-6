c2efb5bd4c1fe6746806079f1e857c98a4007c01
Mr.Kim <joajy98@gmail.com>
2023-10-31 16:25:19 +0900

    feat: controller의 기능을 고려하기 위해 service와 controller로 역할 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,66 +1,10 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.domain.Car;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
-import static racingcar.validator.CarValidator.*;
-import static racingcar.view.InputView.*;
-import static racingcar.view.OutputView.*;
+import racingcar.service.RacingService;
 
 public class RacingController {
 
-    private static final String DELIMITER = ",";
-    private static final int MINIMUM_MOVE_CONDITION = 4;
-
     public static void start() {
-        List<Car> carList = new ArrayList<>();
-        getCarInput(carList);
-        int tryCount = getTryCount();
-        for (int i = 0; i < tryCount; ++i) {
-            moveOrNot(carList);
-            printResult(carList);
-        }
-        printWinnerList(carList);
-    }
-
-    public static void getCarInput(List<Car> carList){
-        String carName = getCarName();
-        splitCarList(carList, carName);
-        checkAllCarNameException(carList);
-    }
-
-    public static String getCarName() {
-        getCarNameNotification();
-        return readLine();
-    }
-
-    public static int getTryCount() {
-        getNumberOfTry();
-        String tryCount = readLine();
-        checkAllTryCountException(tryCount);
-        return Integer.parseInt(tryCount);
-    }
-
-    public static void splitCarList(List<Car> carList, String carNames) {
-        String[] carNameList = carNames.split(DELIMITER);
-        for (String carName : carNameList) {
-            carList.add(new Car(carName));
-        }
-    }
-
-    public static void moveOrNot(List<Car> carList) {
-        for (Car car : carList) {
-            determineRandomNumber(car);
-        }
-    }
-
-    public static void determineRandomNumber(Car car) {
-        if (Randoms.pickNumberInRange(0, 9) >= MINIMUM_MOVE_CONDITION) {
-            car.move();
-        }
+        RacingService.doRacing();
     }
 }
