d563ce566cb5c51388c5e19e992cd18ae1ca0872
= <=>
2023-10-31 23:16:44 +0900

    feat(Const): Const 클래스 구현
    - 랜덤수의 범위를 지정하는 상수, 차량이 전진할 수 있는 가장 작은 랜덤 수, 차량이 가질 수 있는 이름의 길이와 예외 처리될 때 출력되는 에러 메세지를 저장하여 다른 클래스에서 사용할 수 있도록 한다.

---
+++ src/main/java/racingcar/Const.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Const {
+
+    public static int START_RANDOM_NUMBER = 0;
+    public static int END_RANDOM_NUMBER = 9;
+    public static int MIN_NUMBER_REQUIRED_TO_MOVE_A_CAR = 4;
+    public static int MAX_CARNAMES_LENGTH=5;
+
+    public static String MAX_LENGTH_OVER_EXECPTION_MESSAGE = "자동차의 이름은 5자를 초과할 수 없습니다.";
+    public static String NAME_INCLUDE_GAP_EXCEPTION_MESSAGE = "자동차의 이름에는 공백이 존재할 수 없습니다.";
+
+
+}
