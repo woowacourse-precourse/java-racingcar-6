9c8dc70c97c57975602cc7525edd5609e7ac4020
Dawin <dawin002@naver.com>
2023-10-31 18:55:30 +0900

    test(CarTest): 자동차 전진 기능 테스트 추가
    자동차 전진 기능 테스트 추가:
    - moveForward_메서드_5회_호출시_자동차의_이동_거리가_5_증가()

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,22 +1,16 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    private String name;
-    private Car car;
-    @BeforeEach
-    void setUp() {
-        name = "dawin";
-        car = new Car(name);
-    }
+    private String name = "dawin";
 
     @Test
     void moveForward_자동차의_이동_거리가_1_증가() {
         // given
+        Car car = new Car(name);
         int nowDistance = car.getDistance();
         int movedDistance = nowDistance + 1;
 
@@ -28,8 +22,25 @@ class CarTest {
     }
 
     @Test
+    void moveForward_메서드_5회_호출시_자동차의_이동_거리가_5_증가() {
+        // given
+        Car car = new Car(name);
+        int nowDistance = car.getDistance();
+        int movedDistance = nowDistance + 5;
+
+        // when
+        for(int i = 0; i < 5; i++) {
+            car.moveForward();
+        }
+
+        // then
+        assertThat(car.getDistance()).isEqualTo(movedDistance);
+    }
+
+    @Test
     void getName_자동차_이름_반환() {
         //given
+        Car car = new Car(name);
 
         //when
         String carName = car.getName();
@@ -41,6 +52,7 @@ class CarTest {
     @Test
     void getDistance_자동차_이동_거리_반환() {
         //given
+        Car car = new Car(name);
         int defaultDistance = 0;
 
         //when
