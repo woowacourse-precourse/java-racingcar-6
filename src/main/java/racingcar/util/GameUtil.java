6a6981dc50fc2f2a9cc2032d63ef9bf8eebafa15
jang010505 <jang010505@inu.ac.kr>
2023-10-31 14:52:50 +0900

    feat: GameUtil 객체 및 내부 메소드 개형 작성

---
+++ src/main/java/racingcar/util/GameUtil.java
@@ -0,0 +1,52 @@
+package racingcar.util;
+
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameUtil {
+    private final List<Car> carList;
+    private int totalRound;
+    private int nowRound;
+
+    public GameUtil() {
+        this.carList = new ArrayList<>();
+    }
+
+    public void initGame() {
+
+    }
+
+    private void initRound() {
+
+    }
+
+    private void initCar() {
+
+    }
+
+    public void checkGameOver() {
+
+    }
+
+    public void startRound() {
+
+    }
+
+    public void showResults() {
+
+    }
+
+    public void showWinners() {
+
+    }
+
+    private int findMax() {
+        return 0;
+    }
+
+    public List<String> getNowRound() {
+        return null;
+    }
+}
