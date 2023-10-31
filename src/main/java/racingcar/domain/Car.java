557d1d0777e0a86bab475cb4a69f5c4df29f98be
ImWonYong <ssassb123@daum.net>
2023-10-31 22:09:42 +0900

    feat(Car): 자동차 정보를 얻기 위한 getter 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -13,4 +13,12 @@ public class Car {
     public void goStraight() {
         this.straightCount++;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStraightCount() {
+        return straightCount;
+    }
 }
