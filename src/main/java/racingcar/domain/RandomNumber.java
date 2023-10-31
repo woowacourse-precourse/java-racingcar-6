7ae9ff29a5f6769c6bdc25c6cc683d7f0713844b
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-31 21:08:08 +0900

    [refactor] : add constants logic

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,15 +1,16 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.utils.Constants;
 
 public class RandomNumber {
     public static int createRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(Constants.MIN_RANDOM_NUMBER,Constants.MAX_RANDOM_NUMBER);
     }
 
     public static boolean isMove(int randomNumber) {
 
-        if (randomNumber <= 4) {
+        if (randomNumber <= Constants.MOVE_NUMBER) {
             return true;
         }
         return false;
