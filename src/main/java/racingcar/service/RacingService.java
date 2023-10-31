56602b0f7e0ee65195d2cd6259b155cb73c804cc
josephuk77 <josephuk77@gmail.com>
2023-11-01 02:06:07 +0900

    feat(validateNames):한대만 있을 경우 예외처리 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -13,6 +13,10 @@ public class RacingService {
     public void validateNames(List<String> racerList) {
         Set<String> nameSet = new HashSet<>();
 
+        if (racerList.size() == 1) {
+            throw new InvalidNameException("혼자서 게임을 진행할 수 없습니다.");
+        }
+
         for (String name : racerList) {
             if (name.isEmpty() || name.length() > 5) {
                 throw new InvalidNameException("이름은 공백일 수 없으며, 5자를 초과할 수 없습니다.");
