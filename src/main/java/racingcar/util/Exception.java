f88c9e085b4d03aa0c2a78fa8902aece4bbacade
leeminjun <sohwi0305@gmail.com>
2023-10-31 13:34:42 +0900

    feat: 자동차 이름 최소,최대값 오류 문구 구현

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -14,4 +14,12 @@ public class Exception {
     private void throwSpaceException() {
         throw new IllegalArgumentException("자동차 이름안에 공백을 제거하세요.");
     }
+
+    private void throwMinLengthException() {
+        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MIN + "글자 이상이어야 합니다.");
+    }
+
+    private void throwMaxLengthException() {
+        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MAX + "글자 이하여야 합니다.");
+    }
 }
