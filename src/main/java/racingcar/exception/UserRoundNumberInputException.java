baf142de5a4f7af5deb9926d2a349cf7cedcc379
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 04:18:42 +0900

    feat : 사용자 입력 라운드 반복 횟수 예외 처리 기능 구현

--- src/main/java/racingcar/exception/UserRoundNumberInputException.java
+++ src/main/java/racingcar/exception/UserRoundNumberInputException.java
@@ -2,4 +2,27 @@ package racingcar.exception;
 
 public class UserRoundNumberInputException {
     // 숫자가 아닐경우, 공백일 경우, 0이나 음수 일 경우
+    private static final String IS_NOT_NUMBER_MESSAGE = "숫자만 입력해주세요";
+    private static final String IS_BLANK_MESSAGE = "공백을 입력하시면 안됩니다.";
+    private static final String IS_NOT_ZERO_OR_MINUS = "0이나 음수값을 입력하시면 안됩니다.";
+    private static final int Zero = 0;
+    public static void isNumber(String userInput) {
+        try{
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(IS_NOT_NUMBER_MESSAGE);
+        }
+    }
+
+    public static void isBlank(String userInput) {
+        if(userInput.isEmpty()){
+            throw new IllegalArgumentException(IS_BLANK_MESSAGE);
+        }
+    }
+
+    public static void isZeroMinus(String userInput) {
+        if(Integer.parseInt(userInput) <= Zero){
+            throw new IllegalArgumentException(IS_NOT_ZERO_OR_MINUS);
+        }
+    }
 }
