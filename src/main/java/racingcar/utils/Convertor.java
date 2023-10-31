c54a49026bbabdc3f2d9f540e168ea5b496820f5
KimChanJin97 <cjkimhello97@gmail.com>
2023-10-31 22:30:38 +0900

    feat : implement Convertor

---
+++ src/main/java/racingcar/utils/Convertor.java
@@ -0,0 +1,10 @@
+package racingcar.utils;
+
+public class Convertor {
+
+    private Convertor() {}
+
+    public static int convertStringIntoInteger(String gameTry) {
+        return Integer.parseInt(gameTry);
+    }
+}
