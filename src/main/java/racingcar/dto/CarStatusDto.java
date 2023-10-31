f68574ef7480c077f3183543183a067626ff7c65
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-01 00:16:02 +0900

    refactor: DTO 생성 로직 정적 팩토리 메서드로 변경

--- src/main/java/racingcar/dto/CarStatusDto.java
+++ src/main/java/racingcar/dto/CarStatusDto.java
@@ -1,5 +1,6 @@
 package racingcar.dto;
 
+import racingcar.domain.car.Car;
 import racingcar.domain.car.CarName;
 import racingcar.domain.position.Position;
 
@@ -7,9 +8,13 @@ public class CarStatusDto {
     private final CarName carName;
     private final Position carPosition;
 
-    public CarStatusDto(CarName carName, Position carPosition) {
+    private CarStatusDto(CarName carName, Position carPosition) {
         this.carName = carName;
         this.carPosition = carPosition;
+    }
+
+    public static CarStatusDto createFrom(Car car) {
+        return new CarStatusDto(new CarName(car.getCarName()), car.getPosition());
     }
 
     public CarName getCarName() {
--- src/main/java/racingcar/dto/RoundResultDto.java
+++ src/main/java/racingcar/dto/RoundResultDto.java
@@ -1,16 +1,27 @@
 package racingcar.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.car.Car;
 
 public class RoundResultDto {
 
     private final List<CarStatusDto> roundResult;
 
-    public RoundResultDto(List<CarStatusDto> roundResult) {
+    private RoundResultDto(List<CarStatusDto> roundResult) {
         this.roundResult = roundResult;
+    }
+
+    public static RoundResultDto createFrom(List<Car> cars) {
+        List<CarStatusDto> carStatusDtos = cars.stream()
+                .map(CarStatusDto::createFrom)
+                .collect(Collectors.toList());
+        return new RoundResultDto(carStatusDtos);
     }
 
     public List<CarStatusDto> getRoundResult() {
         return roundResult;
     }
+
+
 }
