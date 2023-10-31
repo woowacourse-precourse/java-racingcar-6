b14de8caf18b221f831e388236039f93df68aa88
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-31 09:24:37 +0900

    test - 예외상황 추가에 대한 테스트코드 작성

--- src/test/java/racingcar/service/DataValidatorTest.java
+++ src/test/java/racingcar/service/DataValidatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.exception.CarNameDuplicateException;
 import racingcar.exception.CarNameInvalidException;
+import racingcar.exception.NumberOfCarInvalidException;
 import racingcar.exception.TrialNumberInvalidException;
 
 public class DataValidatorTest {
@@ -20,20 +21,28 @@ public class DataValidatorTest {
     @DisplayName("시도 횟수가 입력을 잘못하면 TrialNumberInvalidException을 발생시킨다.")
     @Test
     void validateTrialNumber() {
-        Assertions.assertThrows(TrialNumberInvalidException.class,
-                () -> dataValidator.validateTrialNumber("하이"));
+        Assertions.assertAll(
+                () -> Assertions.assertThrows(TrialNumberInvalidException.class,
+                        () -> dataValidator.validateTrialNumber("하이")),
+                () -> Assertions.assertThrows(TrialNumberInvalidException.class,
+                        () -> dataValidator.validateTrialNumber("0"))
+
+        );
     }
 
-    @DisplayName("자동차 이름이 잘못(중복,5글자 초과)되면 예외을 발생시킨다.")
+    @DisplayName("자동차 이름이 잘못(중복,5글자 초과,자동차 1대)되면 예외을 발생시킨다.")
     @Test
     void validateCarName() {
         String[] overLength = {"123", "faasa", "aisjdiadai"};
         String[] duplication = {"k", "k", "kos"};
+        String[] oneCar = {"pobi"};
         Assertions.assertAll(
                 () -> Assertions.assertThrows(CarNameInvalidException.class,
                         () -> dataValidator.validateCarName(overLength)),
                 () -> Assertions.assertThrows(CarNameDuplicateException.class,
-                        () -> dataValidator.validateCarName(duplication))
-        );
+                        () -> dataValidator.validateCarName(duplication)),
+                () -> Assertions.assertThrows(NumberOfCarInvalidException.class,
+                        () -> dataValidator.validateCarName(oneCar)
+                ));
     }
 }
