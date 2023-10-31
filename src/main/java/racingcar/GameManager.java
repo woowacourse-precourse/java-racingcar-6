6be08fac207943fb6cc9dd57e6248afe3d6e0767
nayooniverse <kangna99@naver.com>
2023-10-31 11:10:01 +0900

    refactor: GameManager -> Validator로 유효성 검증 책임 전가

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,6 +1,8 @@
 package racingcar;
 
-import static racingcar.Validator.isNotValidInput;
+import static racingcar.Validator.validateInputFormat;
+import static racingcar.Validator.validateInputInRange;
+import static racingcar.Validator.validateInputIsNumeric;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,24 +10,15 @@ public class GameManager {
     public String askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
-        if (isNotValidInput(carNames, Constant.CAR_NAMES_REGEX)) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하의 영어 또는 한글만 가능합니다. 쉼표(,)로 구분하여 두 대 이상 입력해주세요.");
-        }
+        validateInputFormat(carNames);
         return carNames;
     }
 
     public int askTryCount() {
         System.out.println("시도할 횟수는 몇회인가요?");
-
-        int tryCount = 0;
-        try {
-            tryCount = Integer.parseInt(Console.readLine());
-            if (tryCount < 1) {
-                throw new IllegalArgumentException("시도할 횟수는 1회 이상이어야 합니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 횟수로 숫자를 입력해주세요.");
-        }
-        return tryCount;
+        String tryCount = Console.readLine();
+        validateInputIsNumeric(tryCount);
+        validateInputInRange(tryCount);
+        return Integer.parseInt(tryCount);
     }
 }
--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -4,9 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-    public static boolean isNotValidInput(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
+    public static void validateInputFormat(String input) {
+        Pattern pattern = Pattern.compile(Constant.CAR_NAMES_REGEX);
         Matcher matcher = pattern.matcher(input);
-        return !matcher.matches();
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하의 영어 또는 한글만 가능합니다. 쉼표(,)로 구분하여 두 대 이상 입력해주세요.");
+        }
+    }
+
+    public static void validateInputIsNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수로 숫자를 입력해주세요.");
+        }
+    }
+
+    public static void validateInputInRange(String input) {
+        int tryCount = Integer.parseInt(input);
+        if (tryCount < 1) {
+            throw new IllegalArgumentException("시도할 횟수는 1회 이상이어야 합니다.");
+        }
     }
 }
