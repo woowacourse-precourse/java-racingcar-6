c64c1f06e0cda43e55d63b88a04e5ab81a945512
liarreez <liarreez@gmail.com>
2023-10-31 17:00:10 +0900

    test : 부적절한 자동차 이름 입력 시 에러 발생하는 테스트 코드 작성

---
+++ src/test/java/racingcar/validator/CarValidatorTest.java
@@ -0,0 +1,19 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarValidatorTest {
+    Validator validator = new CarValidator();
+
+    @ParameterizedTest
+    @DisplayName("부적절한 자동차 이름 입력 시 에러 발생 테스트")
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "\t", "\n", "leopard"})
+    public void testCarNameGreaterThanFive(String name) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.validate(name));
+    }
+}
