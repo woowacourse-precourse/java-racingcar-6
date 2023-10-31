b0090b628aa6beb739f23018b6d7ed9e149ee6d0
nayonsoso <panda0329@naver.com>
2023-11-01 01:01:22 +0900

    refactor(NameListValidator): 상수 예외 메세지을 출력하게 수정

--- src/main/java/view/NameListValidator.java
+++ src/main/java/view/NameListValidator.java
@@ -1,6 +1,7 @@
 package view;
 
 import constants.Constants;
+import constants.ExceptionMessage;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -19,13 +20,13 @@ public class NameListValidator {
 
     private void validateNotEmpty(String input) {
         if (input.isEmpty()) {
-            throw new IllegalArgumentException("입력된 값이 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_INPUT);
         }
     }
 
     private void validateNotEndsWithComma(String input) {
         if (input.endsWith(",")) {
-            throw new IllegalArgumentException("마지막이 쉼표로 끝납니다.");
+            throw new IllegalArgumentException(ExceptionMessage.ENDS_WITH_COMMA);
         }
     }
 
@@ -33,7 +34,7 @@ public class NameListValidator {
         String inputWithoutSpace = ignoreSpace(input);
         for (int i = 0; i < inputWithoutSpace.length() - 1; i++) {
             if (inputWithoutSpace.charAt(i) == ',' && inputWithoutSpace.charAt(i + 1) == ',') {
-                throw new IllegalArgumentException("쉼표 사이에 값이 없습니다.");
+                throw new IllegalArgumentException(ExceptionMessage.CONTAINS_REPEATING_COMMAS);
             }
         }
     }
@@ -45,8 +46,7 @@ public class NameListValidator {
     private void validateNameLength(List<String> names, int maxLength) {
         for (String name : names) {
             if (name.length() > maxLength) {
-                String message = "이름의 길이가 " + maxLength + "자가 넘습니다.";
-                throw new IllegalArgumentException(message);
+                throw new IllegalArgumentException(ExceptionMessage.EXCEEDED_NAME_LENGTH);
             }
         }
     }
@@ -55,7 +55,7 @@ public class NameListValidator {
         HashSet<String> nameSet = new HashSet<>();
         for(String name : names){
             if(!nameSet.add(name)){
-                throw new IllegalArgumentException("중복되는 이름이 있습니다.");
+                throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_NAME);
             }
         }
     }
