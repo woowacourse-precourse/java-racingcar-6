8c3ed519a4d1421759e5280bd653dc7805c74a16
COBI-98 <tkdgus968@naver.com>
2023-11-01 05:59:16 +0900

    feat: 경주할 자동차 이름 입력 출력뷰 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public void printRequestCarNames(){
+        System.out.println(INPUT_CAR_NAMES);
+    }
+}
