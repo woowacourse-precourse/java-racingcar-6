af077db0e901a4b8a58a8f59c11b3c3b02867332
MyStraw <kikongdosa@gmail.com>
2023-11-01 03:13:13 +0900

    feat(RaceTest): 횟수만큼 자동차 이동하는지 파악하는 메소드 수정하고 승자결정 테스트 작성

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -2,16 +2,30 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class RaceTest {
 
     @Test
     void 주어진_횟수동안_자동차는_전진하거나_멈춘다() {
-        Car car = new Car("테스트");
-        Race race = new Race(5, new Car[]{car});
+        List<Car> cars = Arrays.asList(new Car("car1"), new Car("car2"));
+        Race race = new Race(cars);
+        race.run(5); // 5번 움직임
+        for (Car car : cars) {
+            assertThat(car.getPosition()).isBetween(0, 5);
+        }
+    }
 
-        race.startRace();
-
-        assertThat(car.getPosition()).isBetween(0,5);
+    @Test
+    void 승자_결정() {
+        List<Car> cars = Arrays.asList(new Car("car1"), new Car("car2"));
+        Race race = new Race(cars);
+        race.run(5); // 5번 움직임
+        List<String> winners = race.getWinners();
+        assertThat(winners).isNotEmpty();
     }
 
 }
