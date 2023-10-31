4e19cf14bb3a872d04769587d798cf1501bddaad
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:36:33 +0900

    feat(CarRaceController): Car 객체를 초기화

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -1,10 +1,12 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.validator.AttemptNumberValidator;
 import racingcar.validator.CarNameValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarRaceController {
@@ -16,5 +18,15 @@ public class CarRaceController {
         CarNameValidator.validateCarNames(carNames);
 
         int numberOfAttempts = AttemptNumberValidator.validateNumber(inputView.inputNumberOfAttempts());
+
+        List<Car> cars = initializeCars(carNames);
+    }
+
+    private static List<Car> initializeCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
