dcb75178b78603a8c584bc3623878d79bf1e4922
Nara Park <skfk6352@naver.com>
2023-11-01 01:33:02 +0900

    <test>: add method to create car object before test
    테스트 코드 내에서 중복을 제거하기 위해 @BeforeEach를 활용하여 테스트 코드 실행 전 car 객체를 생성하는 메서드 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,29 +3,27 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-    @Test
-    void 정적_팩토리_객체가_생성되는지_확인() {
-        Car car = Car.from("nara");
-        assertThat(car.getPosition() == 0);
+    private Car car;
+
+    @BeforeEach
+    void init() {
+        car = Car.from("nara");
     }
 
     @Test
-    void 랜덤값이_4이상일경우_전진() {
-        Car car = Car.from("nara");
-        car.play();
-        System.out.println(car.getPosition());
+    void 정적_팩토리_객체가_생성되는지_확인() {
+        assertThat(car.getName() == "nara");
     }
 
     @Test
     void 자동차객체의_position에_따른_전진값_출력_확인() {
-        Car car = Car.from("nara");
         car.play();
         car.play();
-        System.out.println(car.getPosition());
         car.printGameProceed();
     }
 }
