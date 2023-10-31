02aae473a820e67e6d44b242409081cf2c58bbbe
inpink <inpink@kakao.com>
2023-11-01 01:24:47 +0900

    fix: racing 객체 이름 변경 적용되지 않은 곳 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,21 +2,29 @@ package racingcar;
 
 import racingcar.configuration.AppConfig;
 import racingcar.configuration.Config;
-import racingcar.controller.RacingController;
+import racingcar.controller.RacingGameController;
 
 public class Application {
     public static void main(String[] args) {
         Config config = generateConfig();
-        RacingController racingController = generateRacingController(config);
+        RacingGameController racingGameController = generateRacingController(config);
 
-        racingController.playGame();
+        racingGameController.playGame();
     }
 
     private static Config generateConfig() {
         return AppConfig.getInstance();
     }
 
-    private static RacingController generateRacingController(Config config) {
+    private static RacingGameController generateRacingController(Config config) {
         return config.racingController();
     }
 }
+
+/*
+
+TODO:
+ final 넣어야할곳 찾아서 다 넣기
+ MemoryRepository 쓸떄도 <>써줘야하나?
+ 테스트코드에서 중복내용 줄이기
+ */
