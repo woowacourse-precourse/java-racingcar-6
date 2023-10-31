ca86c44d5bc2829680bb6d52098a3e1a1745a933
chanhoim <cksgh201@naver.com>
2023-11-01 00:17:23 +0900

    feat(input): 자동차 이동 시도 횟수 입력 구현

--- src/main/java/racingcar/view/InputConsoleView.java
+++ src/main/java/racingcar/view/InputConsoleView.java
@@ -11,12 +11,30 @@ public class InputConsoleView {
         return input;
     }
 
+    public String readNumberOfTrial() {
+        String input = Console.readLine();
+        validateNumberOfTrial(input);
+        return input;
+    }
+
     private void validateCarNameInput(String input) {
         if(isInputEmpty(input)) {
             throw new IllegalArgumentException("자동차들의 이름을 입력해주세요.");
         }
         else if(isNotSeparateWithComma(input)) {
             throw new IllegalArgumentException("자동차들의 이름은 ','으로 구분해주세요.");
+        }
+    }
+
+    private void validateNumberOfTrial(String input) {
+        if(isInputEmpty(input)) {
+            throw new IllegalArgumentException("시도할 횟수를 입력해주세요.");
+        }
+        else if(isNotNumber(input)) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+        else if (isNotBiggerThanZero(input)) {
+            throw new IllegalArgumentException("시도할 횟수는 1보다 커야됩니다.");
         }
     }
 
@@ -28,4 +46,11 @@ public class InputConsoleView {
         return (input.split(",").length == 1);
     }
 
+    private boolean isNotNumber(String input) {
+        return !Pattern.matches("^[0-9]+$",input);
+    }
+
+    private boolean isNotBiggerThanZero(String input) {
+        return (Integer.parseInt(input) <= 0);
+    }
 }
