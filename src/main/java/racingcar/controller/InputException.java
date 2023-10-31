7c83fbe2147e52d18c0731cc4b0317111e0f3b26
“sangjun121” <juncho12011201@gmail.com>
2023-10-31 14:32:27 +0900

    feat: 자동 차 이름 문자열 입력시 쉼표 관리 예외처리하는 함수 선언
    문자열의 맨 앞이나 맨 뒤에 쉼표가 오는 경우 그리고 문자열 전체적으로 연속된 쉼표가 오는 경우 false를 반환하는 메소드 isCommaAtProperPosition() 구현 완료

---
+++ src/main/java/racingcar/controller/InputException.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import java.util.regex.Pattern;
+
+import static racingcar.utility.Constants.IMPROPER_COMMA_POSITION;
+
+public class InputException {
+
+    public InputException(){}
+
+    public void carsNameException(){
+
+    }
+    private boolean isCommaAtProperPosition(String carsName){
+        return(!Pattern.matches(IMPROPER_COMMA_POSITION,carsName));
+    }
+}
