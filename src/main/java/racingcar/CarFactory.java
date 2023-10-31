40f1a5134b9067cacb2b036d9b2630e5bf28f23b
ehtjsv2 <ehtjsv2@naver.com>
2023-11-01 00:30:58 +0900

    Feat: 문자열 콤마 기준 리스트로 변환 기능 구현

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -1,13 +1,18 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class CarFactory {
     
     public void createCar(){
 
     }
 
-    public List<String> receiveCarName(){
-        return null;
+    public List<String> stringtoListByComma(String name){
+        return Arrays.stream(name.split(","))
+                .collect(Collectors.toList());
     }
 
     public void hasDuplicatedName(){
--- src/test/java/developtest/CarFactoryTest.java
+++ src/test/java/developtest/CarFactoryTest.java
@@ -3,14 +3,13 @@ package developtest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import racingcar.CarFactory;
-import racingcar.List;
 
 public class CarFactoryTest {
     private CarFactory carFactory;
@@ -21,5 +20,11 @@ public class CarFactoryTest {
         carFactory = new CarFactory();
     }
 
+    @ParameterizedTest
+    @ValueSource(strings={"pobi,crong"})
+    void stringtoListByComma_콤마를_기준으로_문자열을_리스트로_변환(String string){
+        List<String> result = carFactory.stringtoListByComma(string);
+        assertThat(result.toString()).isEqualTo(Arrays.asList("pobi","crong").toString());
+    }
 
 }
