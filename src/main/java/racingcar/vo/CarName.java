a962f6115ce1fadd318ffc7781d16929506c6f61
Lieslot <rlaxodud1999@gmail.com>
2023-10-31 18:46:40 +0900

    fix: 입력값들에 대한 테스트 작성

--- src/main/java/racingcar/vo/CarName.java
+++ src/main/java/racingcar/vo/CarName.java
@@ -11,15 +11,16 @@ public class CarName {
     private final String carName;
 
     public CarName(String carName) {
-        checkNameFormat(carName);
+        checkNameLength(carName);
         this.carName = carName;
     }
 
-    private void checkNameFormat(String name) {
+    private void checkNameLength(String name) {
         if (name.length() > MAX_CAR_NAME_LENGTH.show()) {
             throw new CarNameLengthExceedException();
         }
     }
+
 
 
     public String show () {
