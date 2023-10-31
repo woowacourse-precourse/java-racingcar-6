27a33387d5739e189e3617d4c9d40f93a7ccfb9a
Yerinnnn <shinyerin0216@gmail.com>
2023-10-31 17:35:07 +0900

    Refactor: "InputView 클래스 내에 메소드 생성"
    -GameController 클래스에 있던 입력값 관련 메소드들을 InputView 클래스로 이동.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+
+import java.util.List;
+
 public class InputView {
+
+    public void setCarName(List<Car> cars) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        List<String> car_name = List.of(Console.readLine().split(","));
+        car_name.stream().forEach(name -> cars.add(new Car(name)));
+    }
+
+    public int setPlayCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int play_count = Integer.parseInt(Console.readLine());
+        return play_count;
+    }
 }
