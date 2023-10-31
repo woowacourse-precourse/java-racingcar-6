33c775eec42dc9f6a5853846cb48525b8be078aa
SeungGwa123 <skwan123@naver.com>
2023-10-31 19:07:02 +0900

    Feat: Add winner
    경기의 우승자를 알려주는 함수 작성

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -9,18 +9,39 @@ public class Output {
         int randNum = Randoms.pickNumberInRange(0, 9);
         return randNum;
     }
-    public void totalPrint(Map<String,Integer> parkingLot,String[] carlist) {
-        for(int i=0;i<parkingLot.size();i++){
-            routePrint(carlist[i],parkingLot.get(carlist[i]));
+
+    public void totalPrint(Map<String, Integer> parkingLot, String[] carlist) {
+        for (int i = 0; i < parkingLot.size(); i++) {
+            routePrint(carlist[i], parkingLot.get(carlist[i]));
         }
         System.out.println();
     }
 
-    public void routePrint(String carName,int carDistance) {
+    public void routePrint(String carName, int carDistance) {
         String distance = "";
         for (int i = 0; i < carDistance; i++) {
             distance += "-";
         }
-        System.out.println(carName+" : "+distance);
+        System.out.println(carName + " : " + distance);
+    }
+
+    public String winner(Map<String, Integer> parkinglot, String[] car_name) {
+        String result = "최종 우승자 : ";
+        String champion = "";
+        int champion_distance = 0;
+        for (int i = 0; i < car_name.length; i++) {
+            if (champion_distance < parkinglot.get(car_name[i])) {
+                champion_distance = parkinglot.get(car_name[i]);
+                champion = car_name[i];
+            }
+            else if (champion_distance == parkinglot.get(car_name[i]) && champion.equals("")) {
+                champion += car_name[i];
+            }
+            else if (champion_distance == parkinglot.get(car_name[i])) {
+                champion = champion + ", " + car_name[i];
+            }
+        }
+        result += champion;
+        return result;
     }
 }
