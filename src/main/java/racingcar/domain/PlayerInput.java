183ec701b01650c50a5b4d62c43efd81fdaffda8
mini1115 <kmini9611@naver.com>
2023-10-31 19:33:16 +0900

    feat : 사용자로부터 시도할 게임 횟수 입력 기능 추가

--- src/main/java/racingcar/domain/PlayerInput.java
+++ src/main/java/racingcar/domain/PlayerInput.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerInput {
-    public static Map<String, Integer> NameInput() {
+    public static Map<String, Integer> nameInput() {
         Map<String, Integer> racers = new HashMap<>();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
@@ -14,5 +14,9 @@ public class PlayerInput {
             racers.put(name, 0);
         }
         return racers;
+    }
+    public int playTimeInput(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
