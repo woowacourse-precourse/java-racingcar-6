1f5ad3d9169d5e277d321206da028b4aa1320129
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-01 02:22:20 +0900

    test(RacingRoundValidatorTest): validateInputNotEmpty 함수 테스트 코드 추가

---
+++ src/test/java/racingcar/util/RacingRoundValidatorTest.java
@@ -0,0 +1,25 @@
+package racingcar.util;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+public class RacingRoundValidatorTest extends NsTest {
+    @Test
+    @DisplayName("validateInputNotEmpty 함수 기능 테스트")
+    void 라운드_횟수가_공백일_경우_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("bora,dori", " "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
