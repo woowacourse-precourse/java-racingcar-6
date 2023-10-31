2e6e30f432982a188e06cb3d3a9b50dbc1edab88
KimSehoon <shggm2000@gmail.com>
2023-10-31 16:32:17 +0900

    test: getTryCount_test method 구현
    - getTryCount method를 테스트하는 getTryCount_test method 구현

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -3,13 +3,14 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.Input;
 
-public class InputTest {
+public class InputTest extends NsTest {
     Input input = new Input();
 
     @Test
@@ -22,8 +23,22 @@ public class InputTest {
         assertThat(carNameListForTest).containsExactly("pobi", "woni", "jun");
     }
 
+    @Test
+    @DisplayName("시도할 횟수 입력 테스트")
+    void getTryCount_test(){
+        command(String.valueOf(4));
+        int tryCountForTest = input.getTryCount();
+
+        assertThat(tryCountForTest).isEqualTo(4);
+    }
+
     private void command(final String... args) {
         final byte[] buf = String.join("\n", args).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    @Override
+    protected void runMain() {
+
     }
 }
