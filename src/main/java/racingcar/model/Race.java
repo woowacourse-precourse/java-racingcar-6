1edbc31386f29815776e91f9526cd93e694fdb1c
Chun Hyunwoo <1000chw@gmail.com>
2023-11-01 01:29:42 +0900

    feat: Race model init

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,7 +1,28 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Race {
+    private final List<Car> carList;
+    private int raceRounds;
     public Race() {
+        carList = new ArrayList<>();
+    }
+
+    public void setRaceRounds(int rounds){
+        raceRounds = rounds;
+    }
+
+    public void putCarList(List<String> carlist){
+
+    }
+
+    public void playRace(){
+
+    }
+
+    public List<String> getRaceResult(){
 
     }
 }
