5e7c3e06dfbceb48cb10dc914049abae2c5e1841
KyungsooLee <jmtkd9196@gmail.com>
2023-11-01 03:52:38 +0900

    feat: 차 이동거리 출력 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,32 +3,40 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
 
-    public static void isMove(List<Integer> carsMovingDistance, int index) {
+    public static void isMove(List<Integer> carsMovedDistance, int index) {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            carsMovingDistance.set(index, carsMovingDistance.get(index) + 1);
+            carsMovedDistance.set(index, carsMovedDistance.get(index) + 1);
         }
+    }
+
+    public static String printCarMovedDistance(List<Integer> carsMovedDistance, int index) {
+        String MovedDistance = "-".repeat(carsMovedDistance.get(index));
+        return MovedDistance;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> cars = Arrays.asList(camp.nextstep.edu.missionutils.Console.readLine().split(","));
-        List<Integer> carsMovingDistance = new ArrayList<>();
+        List<Integer> carsMovedDistance = new ArrayList<>(Collections.nCopies(cars.size(), 0));
         System.out.println("시도할 회수는 몇회인가요?");
         int testNumber = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
         System.out.println("\n실행결과");
 
         while (testNumber > 0) {
             for (int i = 0; i < cars.size(); i++) {
-                isMove(carsMovingDistance, i);
-                System.out.print(cars.get(i) + " : ");
-                System.out.println();
+                isMove(carsMovedDistance, i);
+                System.out.println(cars.get(i) + " : " + printCarMovedDistance(carsMovedDistance, i));
+
             }
+            System.out.println();
             testNumber--;
         }
     }
+
 }
