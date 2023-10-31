c6d2731b381524316610ba1de8e0b765de392f06
leeminjun <sohwi0305@gmail.com>
2023-10-31 10:21:15 +0900

    feat: 자동차 준비
    자동차 이름 받기 기능 구현

---
+++ src/main/java/racingcar/RacingcarReady.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingcarReady {
+
+    public  String InputCarName(){
+        return Console.readLine();
+    }
+
+
+}
