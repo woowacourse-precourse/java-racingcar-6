89fab152aaa2c41c97fdcbdc1f80287526104c69
kunsanglee <gosmdochee2@gmail.com>
2023-10-31 18:49:16 +0900

    refactor(Cars): 입력받은 자동차 이름의 중복을 검증하는 로직 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,26 +4,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
+    private static final String DUPLICATED_CAR_NAMES_EXCEPTION = "각 자동차의 이름은 고유해야 합니다.";
     private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
+    private Cars(List<Car> cars) {
         this.cars = cars;
     }
 
-    public static Cars assemble(List<String> inputCarNames, CarEngine carEngine) {
+    public static Cars from(List<String> inputCarNames, CarEngine carEngine) {
+        validateDuplicated(inputCarNames);
         List<Car> carList = inputCarNames.stream()
-                .map(carName -> new Car(new CarName(carName), carEngine))
+                .map(carName -> new Car(new CarName(carName), carEngine, new Position()))
                 .collect(Collectors.toList());
         return new Cars(carList);
     }
 
-    public void move() {
-        for (Car car : cars) {
-            car.startEngine();
+    private static void validateDuplicated(List<String> carNames) {
+        int distinctCount = (int) carNames.stream()
+                .distinct()
+                .count();
+        if (distinctCount != carNames.size()) {
+            throw new IllegalArgumentException(DUPLICATED_CAR_NAMES_EXCEPTION);
         }
     }
 
-    public List<Car> getReadOnlyCarList() {
+    public void move() {
+        cars.forEach(Car::startEngine);
+    }
+
+    public List<Car> getCars() {
         return cars.stream()
                 .map(car -> new Car(new CarName(car.getCarName()), car.getCarEngine(), new Position(car.getPosition())))
                 .collect(Collectors.toUnmodifiableList());
