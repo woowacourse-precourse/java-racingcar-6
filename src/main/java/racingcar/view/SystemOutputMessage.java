955c6444b36220287d80cdaa299468cdf81eac37
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-10-31 22:43:54 +0900

    fix : select winner 최종 우승자 출력 문구 수정

--- src/main/java/racingcar/view/SystemOutputMessage.java
+++ src/main/java/racingcar/view/SystemOutputMessage.java
@@ -15,7 +15,7 @@ public class SystemOutputMessage {
 
     public void showWinnerMessage(List<Car> winner) {
         StringBuilder str = new StringBuilder();
-        System.out.print("최종우승자 : ");
+        System.out.print("최종 우승자 : ");
         for (int i = 0; i < winner.size(); i++) {
             if(i == winner.size()-1) str.append(winner.get(i).getName());
             else str.append(winner.get(i).getName()).append(", ");
