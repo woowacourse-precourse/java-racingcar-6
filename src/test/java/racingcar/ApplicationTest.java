2317b2a310b91225b3eb43a604b082d7c494c356
nylonmusk <2912rla@gmail.com>
2023-11-01 01:16:36 +0900

    test : 이름이 한글일때 예외처리, 실행 횟수가 영문, 공백 일때 예외처리 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -32,7 +32,47 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 실행_횟수가_0_이하일때_예외_발생() {
+    void 이름이_한글일_때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("한글,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_숫자일_때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름의_길이가_5자리_초과일때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("abcdef,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_공백일_때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름에_다른_특수문자가_포함될_때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi@,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 실행_횟수가_0_이하일_때_예외_발생() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,jun", "0"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -47,6 +87,21 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 실행_횟수가_영문_일때_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", "pobi"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 실행_횟수가_공백일_때_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", " "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     @Override
     public void runMain() {
