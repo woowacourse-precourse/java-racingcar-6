1e6c4afbaef2bbb99733b331eb3ab1d69a201678
seminchoi <choicco89@gmail.com>
2023-10-31 13:32:16 +0900

    test(racingcar.RacingCarGameControllerTest) : RacingCarGameController 클래스에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/RacingCarGameControllerTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarGameControllerTest {
+    private static final RacingCarGameFactory racingCarGameFactory = new MockRacingCarGameFactory();
+
+    private static final String RACING_CAR_NAME_INPUT = "semin,woowa,pre";
+    private static final String ROUND_COUNT_INPUT = "5";
+
+    private MockInputView mockInputView;
+    private MockOutputView mockOutputView;
+    private RacingCarGameController racingCarGameController;
+
+    @BeforeEach
+    void init() {
+        mockInputView = (MockInputView) racingCarGameFactory.inputView();
+        mockOutputView = (MockOutputView) racingCarGameFactory.outputView();
+        racingCarGameController = racingCarGameFactory.racingCarController();
+    }
+
+    @Test
+    void 게임_실행() {
+        mockInputView.init(RACING_CAR_NAME_INPUT, ROUND_COUNT_INPUT);
+
+        racingCarGameController.play();
+        String printedMessage = mockOutputView.getPrintedMessage();
+
+        Assertions.assertThat(printedMessage)
+                .contains("실행 결과", "semin", "woowa", "pre", "최종 우승자");
+    }
+}
