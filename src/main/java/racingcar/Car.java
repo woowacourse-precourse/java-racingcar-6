f6fd3be841a20825415e86cf7366ed5d59c27091
ljkhyeong <jolri24@gmail.com>
2023-10-31 22:34:43 +0900

    feat : Car 클래스 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+	private String name;
+	private int position;
+
+	public Car(String name) {
+		this.name = name;
+		this.position = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public boolean move() {
+		return Randoms.pickNumberInRange(0, 9)>=4;
+	}
+}
