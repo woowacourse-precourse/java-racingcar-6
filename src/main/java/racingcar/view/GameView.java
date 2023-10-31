9db5a35b0552cb3ff42441d957923da9ab058dd5
yooonwodyd <dbswodyd00@naver.com>
2023-11-01 02:20:41 +0900

    GameView 작성. String은 Enum으로 관리한다. 입력을 받는 메서드들은 메서드명을 리팩토링할 필요가 있어보인다.

---
+++ src/main/java/racingcar/view/GameView.java
@@ -0,0 +1,46 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameView {
+    public String printCarInputRequest() {
+        System.out.println(OuputMessage.CAR_INPUT_REQUEST.getMessage());
+        return Console.readLine();
+    }
+
+    public String printAttemptsNumInputRequest() {
+        System.out.println(OuputMessage.ATTEMPTS_NUM_INPUT_REQUEST.getMessage());
+        return Console.readLine();
+    }
+    public void printPreResultMessage() {
+        System.out.println(OuputMessage.PRE_RESULT_MESSAGE.getMessage());
+        return;
+    }
+
+    public void printResult(String result) {
+        System.out.println(result);
+        return;
+    }
+
+    public void printWinner(String winner) {
+        System.out.println(OuputMessage.WINNER.getMessage() + winner);
+        return;
+    }
+
+    enum OuputMessage {
+        CAR_INPUT_REQUEST("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        ATTEMPTS_NUM_INPUT_REQUEST("시도할 회수는 몇회인가요?"),
+        WINNER("최종 우승자 : "),
+        PRE_RESULT_MESSAGE("실행 결과");
+
+        private String message;
+
+        OuputMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
