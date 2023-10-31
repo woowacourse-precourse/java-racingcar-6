dc50c475678dc924c43a3afec6017ab0eb3c6849
inpink <inpink@kakao.com>
2023-10-31 16:32:40 +0900

    refactor: 정수의 범위를 메세지에 함께 나타내어 더욱 명확한 에러 메세지 표시

--- src/main/java/racingcar/validation/IntegerValidator.java
+++ src/main/java/racingcar/validation/IntegerValidator.java
@@ -4,7 +4,7 @@ import racingcar.util.ExceptionUtil;
 import racingcar.util.IntegerUtil;
 
 public class IntegerValidator {
-    private static final String INVALID_INTEGER_MESSAGE = "정수 값이 필요합니다.";
+    private static final String INVALID_INTEGER_MESSAGE = "1 이상 2,147,483,647 이하의 정수 값이 필요합니다.";
 
     public static void validateInteger(Object object) {
         if (!(object instanceof Integer)) {
