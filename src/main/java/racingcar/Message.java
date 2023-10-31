5f51d0605e506dcf2e224cf14bfd1973ef30abff
SeongIl <biuld1234@gmail.com>
2023-10-31 12:41:14 +0900

    refactor: change 게임 메시지 -> Enum - 게임 메시지들을 Enum 열거형 타입으로 묶어서 관리 - 긴 게임 메시지를 다 읽지 않아도 짧은 상수명으로 무엇을 의미하는지 전달   할 수 있어 가독성 향상

---
+++ src/main/java/racingcar/Message.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public enum Message {
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_RACING_ROUND("시도할 회수는 몇회인가요?"),
+    START_RACE("실행 결과"),
+    RESULT("최종 우승자 : ");
+
+    public String getMessage() {
+        return message;
+    }
+
+    private String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+}
