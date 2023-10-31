829a2baebd92bdc34ef965581878892580676e7a
happyhun <younghun.ai@gmail.com>
2023-10-31 14:54:55 +0900

    feat: Car 리스트를 생성하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     private final String name;
@@ -22,4 +25,12 @@ public class Car {
         forwardCount++;
     }
 
+    public static List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
+    }
 }
