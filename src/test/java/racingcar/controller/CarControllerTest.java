ec898d06d7455d3dbeb4430e5ec3f11746fa72af
shAn-kor <kjn0406@naver.com>
2023-10-31 15:15:45 +0900

    ifForwardCars 수정으로 인한 테스트 코드 변경

--- src/test/java/racingcar/controller/CarControllerTest.java
+++ src/test/java/racingcar/controller/CarControllerTest.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -10,6 +11,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarControllerTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
     private static final List<String> CAR_LIST = new ArrayList<>(Arrays.asList("pobi", "woni", "blur"));
     CarController carController;
 
@@ -21,32 +25,30 @@ class CarControllerTest {
     @Test
     @DisplayName("setCarsName 후 toString 출력 확인")
     void testSetCarsName() {
-        assertThat(carController.toString()).isEqualTo("pobi : \nwoni : \nblur : \n");
+        assertThat(carController.toString()).contains("pobi : ", "woni : ", "blur : ");
     }
 
     @Test
     @DisplayName("우승자 한명 일 경우 chooseWinner() 반환값 확인")
     void testChooseWinner() {
-        //given
-
-        carController.ifForwardCars();
-
-        //when
-        String winner = carController.chooseWinner();
-
-        assertThat(winner).isEqualTo("woni");
+        assertRandomNumberInRangeTest(
+                () -> {
+                    carController.ifForwardCars();
+                    assertThat(carController.chooseWinner()).isEqualTo("woni");
+                },
+                STOP, MOVING_FORWARD, STOP
+        );
     }
 
     @Test
     @DisplayName("우승자 세 명 일 경우 chooseWinner() 반환값 확인")
     void testChooseThreeWinner() {
-        //given
-
-        carController.ifForwardCars();
-
-        //when
-        String winner = carController.chooseWinner();
-
-        assertThat(winner).isEqualTo("pobi, woni, blur");
+        assertRandomNumberInRangeTest(
+                () -> {
+                    carController.ifForwardCars();
+                    assertThat(carController.chooseWinner()).isEqualTo("pobi, woni, blur");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
     }
 }
