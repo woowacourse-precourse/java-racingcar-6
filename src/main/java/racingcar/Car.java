b725c7eda2f9b4f42a9fc37197bed188868068ff
yewonahn <yewonahn@naver.com>
2023-11-01 03:54:25 +0900

    feat: 차의 이름, 거리를 담는 Car 객체 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,26 @@
 package racingcar;
 
 class Car implements Comparable{
+    private String name;
+    private int distance;
 
+    public Car() {}
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+    public int getDistance() {
+        return distance;
+    }
 }
