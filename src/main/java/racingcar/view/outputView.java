666c7aee54c62e7824e5c2340f14e4ac9574d956
김관진 <cozyschooll@gmail.com>
2023-10-31 21:34:32 +0900

    Feat: outputView 생성, 일부 기능 구현 자동차 이름 입력 view, 시도할 회수 입력 view 구현 완료

---
+++ src/main/java/racingcar/view/outputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class outputView {
+
+    private static final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRY_NUMBER = "시도할 회수는 몇회인가요?";
+
+    public void requestCarName() {
+        System.out.print(REQUEST_CAR_NAME);
+    }
+
+    public void requestTryNumber() {
+        System.out.print(REQUEST_TRY_NUMBER);
+    }
+}
