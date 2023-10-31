51f520d4cdaa1cf32492a2801e10ffbe686e5559
Hyun Woo <hwsa10041@gmail.com>
2023-10-31 18:52:02 +0900

    refactor : printVictoryPlayers 메서드에서 불필요한 StringBuilder 사용 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -18,17 +18,6 @@ public class OutputView {
     }
 
     public void printVictoryPlayers(List<String> victoryPlayers) {
-        StringBuilder sb = new StringBuilder("최종 우승자 : ");
-        for (int i = 0; i < victoryPlayers.size(); i++) {
-            sb.append(victoryPlayers.get(i));
-            appendComma(victoryPlayers, sb, i);
-        }
-        System.out.println(sb);
-    }
-
-    private static void appendComma(List<String> victoryPlayers, StringBuilder sb, int i) {
-        if (i != victoryPlayers.size() - 1) {
-            sb.append(", ");
-        }
+        System.out.printf("최종 우승자 : %s", String.join(", ", victoryPlayers));
     }
 }
