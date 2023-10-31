9c743bccf4595e79b57ad4e1f30eeeec326fa68a
Isaac Han <isaachan233@gmail.com>
2023-10-31 13:33:28 +0900

    refactor: 우승자들의 이름리스트가 아닌 우승자의 자동차 정보 리스트를 모아놓고 getWinnersNames 시 이름만 가져오도록함

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -4,24 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Winners {
-    private final Cars cars;
-    private final Attempts attempts;
-    private final List<String> winnerNames = new ArrayList<>();
+    private final List<Car> winners = new ArrayList<>();
 
     public Winners(Cars cars, Attempts attempts) {
-        this.cars = cars;
-        this.attempts = attempts;
-    }
-
-    private void countWinners() {
         for (Car car : cars.getCarList()) {
             if (car.getDistance().length() == attempts.getNumber()) {
-                winnerNames.add(car.getName());
+                winners.add(car);
             }
         }
     }
-    public List<String> getWinnerNames() {
-        countWinners();
-        return this.winnerNames;
+
+    public List<String> getWinnersNames() {
+        List<String> winnersNames = new ArrayList<>();
+        for (Car car : winners) {
+            winnersNames.add(car.getName());
+        }
+        return winnersNames;
     }
 }
