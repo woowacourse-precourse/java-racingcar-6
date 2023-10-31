1b3eceac98e56ec38c41bbbef087b8d312cce094
bora <youonl5@naver.com>
2023-11-01 01:43:36 +0900

    Feat: GameView 클래스 inputNameofCars() 구현

--- src/main/java/racingcar/GameView.java
+++ src/main/java/racingcar/GameView.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameView {
+
+    ExceptionHandler exceptionHandler = new ExceptionHandler();
+
+    public String inputNameOfCars() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = Console.readLine();
+        exceptionHandler.validateUserInput(userInput);
+        return userInput;
+    }
+
+
 }
