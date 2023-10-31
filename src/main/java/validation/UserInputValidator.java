f878b9bc882e83bf141e0c0d8f31bcdf34e96f83
yunseung <yseungun.21@gmail.com>
2023-11-01 00:36:54 +0900

    feat(UserInputValidator): 시도 횟수 추가 검증 기능 작성

--- src/main/java/validation/UserInputValidator.java
+++ src/main/java/validation/UserInputValidator.java
@@ -5,8 +5,13 @@ import static constant.ErrorMessage.EMPTY_NAME_CASE_MESSAGE;
 import static constant.ErrorMessage.EXCEED_NAME_CASE_MESSAGE;
 
 public class UserInputValidator {
+    public void checkPositiveNumber(Integer integer) {
+        if (integer < 1) {
+            throw new NumberFormatException();
+        }
+    }
 
-    public static void validateInput(String name) {
+    public void validateInputCarName(String name) {
         if (name.contains(" ")) {
             throw new IllegalArgumentException(EMPTY_NAME_CASE_MESSAGE);
         } else if (name.length() > NAME_LENGTH_LIMIT) {
