e5ecac183be79f873090104dbf5f4fe2201f147a
Seungjida <esg0107@pusan.ac.kr>
2023-10-31 11:56:57 +0900

    refactor(createCarList): 메서드 분리 createCarList() 메서드 리팩토링:
    - inputCarName()라는 새 메서드를 통해 사용자에게 입력 받도록 수정

--- src/main/java/racingcar/CarRaceManager.java
+++ src/main/java/racingcar/CarRaceManager.java
@@ -19,9 +19,13 @@ public class CarRaceManager {
         carCount = racingCars.size();
     }
 
+    private String[] inputCarName() {
+        return Console.readLine().split(","); // array말고 바로 리스트에 저장 못 하나...
+    }
+
     public void createCarList() {
         // 입력 받은 값 쉼표 기준으로 나눠서 리스트에 저장
-        String[] carNameArray = Console.readLine().split(","); // array말고 바로 리스트에 저장 못 하나...
+        String[] carNameArray = inputCarName();
         for (String carName : carNameArray) {
             Car newCar = new Car(carName);
             racingCars.add(newCar);
