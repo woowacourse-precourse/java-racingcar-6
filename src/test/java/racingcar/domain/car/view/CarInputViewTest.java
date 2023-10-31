f13672c7ca589002fc4ea6441983cf42a89b70f9
02ggang9 <02ggang9@gmail.com>
2023-10-31 23:57:03 +0900

    test(CarInputViewTest): 중복되는 코드를 @BeforeEach와 @AfterEach를 이용해 제거

--- src/test/java/racingcar/domain/car/view/CarInputViewTest.java
+++ src/test/java/racingcar/domain/car/view/CarInputViewTest.java
@@ -2,9 +2,7 @@ package racingcar.domain.car.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
@@ -22,6 +20,21 @@ import static racingcar.domain.util.ErrorMessage.*;
 
 class CarInputViewTest {
 
+
+    private CarInputView carInputView;
+    private MockedStatic<Console> consoleMockedStatic;
+
+    @BeforeEach
+    public void setUp() {
+        carInputView = new CarInputView();
+        consoleMockedStatic = mockStatic(Console.class);
+    }
+
+    @AfterEach
+    public void clear() {
+        consoleMockedStatic.close();
+    }
+
     @Nested
     @DisplayName("자동차 이름 입력 테스트")
     class CarNameInputTest {
@@ -29,8 +42,6 @@ class CarInputViewTest {
         @Test
         @DisplayName("자동차 입력을 성공적으로 받아야 한다")
         public void 자동차_입력을_성공적으로_받아야_한다() {
-            CarInputView carInputView = new CarInputView();
-            MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
             when(Console.readLine()).thenReturn("pobi,jun,woni");
 
             List<String> carNames = carInputView.getCarNames();
@@ -39,8 +50,6 @@ class CarInputViewTest {
             assertThat(carNames.get(0)).isEqualTo("pobi");
             assertThat(carNames.get(1)).isEqualTo("jun");
             assertThat(carNames.get(2)).isEqualTo("woni");
-
-            consoleMockedStatic.close();
         }
     }
 
@@ -51,29 +60,34 @@ class CarInputViewTest {
         @Test
         @DisplayName("자동차 이름이 6자 이상일 경우 예외를 발생시켜야 한다")
         public void 자동차_이름이_6자_이상일_경우_예외를_발생시켜야_한다() {
-            CarInputView carInputView = new CarInputView();
-            MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
             when(Console.readLine()).thenReturn("pooooooobi,juuuuuuuun,wooooooooni");
 
+            // jupiter
             assertThrows(IllegalArgumentException.class,
                     carInputView::getCarNames,
                     CAR_NAME_LENGTH_OVER.getErrorMessage());
 
-            consoleMockedStatic.close();
+            // assertJ.core
+            assertThatThrownBy(() -> carInputView.getCarNames())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(CAR_NAME_LENGTH_OVER.getErrorMessage());
         }
 
         @Test
         @DisplayName("시도 횟수 입력이 숫자가 아닌 경우 예외를 발생시켜야 한다")
         public void 시도_횟수_입력이_숫자가_아닌_경우_예외를_발생시켜야_한다() {
-            CarInputView carInputView = new CarInputView();
-            MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
             when(Console.readLine()).thenReturn("NotIntInput");
 
+            // jupiter
             assertThrows(IllegalArgumentException.class,
                     carInputView::getTryCount,
                     TRY_COUNT_IS_NOT_DIGIT.getErrorMessage());
 
-            consoleMockedStatic.close();
+            // assertJ.core
+            assertThatThrownBy(() -> carInputView.getTryCount())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(TRY_COUNT_IS_NOT_DIGIT.getErrorMessage());
+
         }
 
         @Test
