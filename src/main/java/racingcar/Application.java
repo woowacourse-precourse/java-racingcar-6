fc5c2420f460bef41c655ce65e8ed451c68bfc89
Junho Choi <tig9659@ajou.ac.kr>
2023-11-01 04:33:28 +0900

    feat : 경주 진행 및 경주 결과

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+    static List<String> carNames = new ArrayList<>();
+    static List<Integer> carProgress = new ArrayList<>();
+    static int bestScore = 0;
+
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
@@ -13,7 +17,7 @@ public class Application {
         int gameCnt = Integer.parseInt(Console.readLine());
 
         String[] carNameArray  = input.split(",");
-        List<String> carNames = new ArrayList<>();
+
 
         for (String name : carNameArray) {
             if (name.length() > 5) {
@@ -22,10 +26,43 @@ public class Application {
             carNames.add(name);
         }
 
-        List<Integer> carProgress = new ArrayList<>();
+
         for (int i=0; i<carNames.size(); i++){
             carProgress.add(0);
         }
 
+        System.out.println();
+        System.out.println("실행 결과");
+
+        while(gameCnt > 0){
+            race();
+            gameCnt--;
+        }
+
+        List<String> winner = new ArrayList<>();
+        for (int i=0; i<carNames.size(); i++){
+            if (carProgress.get(i) == bestScore){
+                winner.add(carNames.get(i));
+            }
+        }
+        String winners = String.join(", ", winner);
+        System.out.print("최종 우승자 : " + winners);
+    }
+
+    protected static void race(){
+        for (int i=0; i<carNames.size(); i++){
+            int randomNum = Randoms.pickNumberInRange(0,9);
+            if (randomNum >= 4) {
+                carProgress.set(i, carProgress.get(i) + 1);
+                bestScore = updateBestScore(carProgress.get(i), bestScore);
+            }
+            System.out.println(carNames.get(i)+" : "+ "-".repeat(carProgress.get(i)));
+        }
+        System.out.println();
+    }
+
+    protected static Integer updateBestScore(Integer carProgress, Integer bestScore){
+        if (bestScore < carProgress) return carProgress;
+        return bestScore;
     }
 }
