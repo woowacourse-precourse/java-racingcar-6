304bb9c01592ee6f9b36b33b1f554487e6742b06
reddevilmidzy <midzy0228@gmail.com>
2023-10-31 21:02:12 +0900

    style: 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import racingcar.application.RacingCarApplication;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         RacingCarApplication application = new RacingCarApplication();
         application.run();
     }
