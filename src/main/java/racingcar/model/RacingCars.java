cc46b168369f2241773d63f7dc91f2c329103f6d
Donggeon Moon <nullsafer@naver.com>
2023-11-01 00:42:10 +0900

    refactor: 레코드로 변경, 널 타입 검사, 메서드 이름 변경

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -2,36 +2,31 @@ package racingcar.model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 import racingcar.enums.Symbol;
 
-public class RacingCars {
-    private final List<RacingCar> racingCars;
-
-    private RacingCars(List<RacingCar> racingCars) {
-        this.racingCars = racingCars;
-    }
-
+public record RacingCars(List<RacingCar> racingCars) {
     public static RacingCars from(String inputCarNames) {
+        Objects.requireNonNull(inputCarNames);
         String[] carNames = inputCarNames.split(Symbol.CAR_NAME_DELIMITER.getValue());
         return new RacingCars(
                 Arrays.stream(carNames)
-                        .map(RacingCar::new)
+                        .map(RacingCar::from)
                         .toList()
         );
     }
 
     public static RacingCars from(List<RacingCar> racingCars) {
-        return new RacingCars(
-                racingCars
-        );
+        Objects.requireNonNull(racingCars);
+        return new RacingCars(racingCars);
     }
 
     public List<RacingCar> getRacingCars() {
         return racingCars;
     }
 
-    public String toNames() {
+    public String toNameString() {
         StringBuilder stringBuilder = new StringBuilder();
         IntStream.range(0, racingCars.size())
                 .forEach(i -> {
