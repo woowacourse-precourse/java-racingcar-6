6056cdeb55caf5e91423e21e0a35b3a20cea14a8
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-31 22:57:26 +0900

    fix: 예외처리추가 split함수를(,)로 받을 경우, 마지막 ,는 지워지고 값이 받아져서, 사용자가 마지막에 쉼표를 넣을 경우도 고려해 예외처리를 해주었습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,7 +13,10 @@ public class InputView {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        return Console.readLine();
+        String input = Console.readLine();
+        isLastIndexComma(input);
+
+        return input;
     }
 
     public static int startGameCount() {
@@ -21,5 +24,11 @@ public class InputView {
         System.out.println("시도할 회수는 몇회인가요?");
 
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static void isLastIndexComma(String input){
+        if (input.endsWith(",")) {
+            throw new IllegalArgumentException();
+        }
     }
 }
