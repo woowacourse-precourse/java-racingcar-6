f1675e188fde21a7d0c76b6df78211679331f479
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-31 21:15:06 +0900

    FEAT: 승자 결정 및 출력 기능 추가 - getWinner 메소드 - : 를 기준으로 나눔 --> "minji : --" 이면 " --" 이 부분의 길이를 비교하여 가장 긴 것을 찾음 --> winner 리스트에 저장 - 승자는 다수일 수 있으므로 가장 많이 움직인 차량과 이동 수가 같은 것을 찾는다. --> 같은 차량 이름을 winner 리스트에 저장하여 최종 우승자로 출력한다.

--- src/main/java/racingcar/PlayResult.java
+++ src/main/java/racingcar/PlayResult.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayResult {
     // 실행 결과 출력 메소드
     public void getResult(String[] names, String[] results){
@@ -10,6 +13,35 @@ public class PlayResult {
             if (r >= 4)
                 results[i] += "-";
             System.out.println(results[i]);
+        }
+    }
+
+    public void getWinner(String[] results){
+        String max = results[0];
+        List<String> winner = new ArrayList<>();
+
+        for (int i = 1; i < results.length; i++) {
+            if (max.split(":")[1].length() < results[i].split(":")[1].length()){
+                max = results[i];
+            }
+        }
+        String maxname = max.split(" ")[0];
+        winner.add(maxname);
+
+        for (int i = 0; i < results.length; i++) {
+            String name = results[i].split(" ")[0];
+            if (max.split(":")[1].length() == results[i].split(":")[1].length() && !maxname.equals(name)){
+                winner.add(name);
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winner.size(); i++) {
+            if (i == winner.size() - 1){
+                System.out.print(winner.get(i));
+                break;
+            }
+            System.out.print(winner.get(i) + ", ");
         }
     }
 }
