4e67c659f61aaa9e1fa98495a4c6908c748f15fc
Nbowow <nbo5548@naver.com>
2023-10-31 14:41:49 +0900

    Add: Car 2023.10.31 Car도메인 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
