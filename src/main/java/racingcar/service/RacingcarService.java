737f9d263a65b717f017247aee81908eae335b0e
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-31 23:27:50 +0900

    Feat[service] 자동차 경주 서비스 생성

--- src/main/java/racingcar/service/RacingcarService.java
+++ src/main/java/racingcar/service/RacingcarService.java
@@ -1,4 +1,35 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingcarService {
+
+    private Race race;
+
+    public RacingcarService(Race race) {
+        this.race = race;
+    }
+
+    public void raceRound() {
+        race.raceRound();
+    }
+
+    public boolean isRaceFinished() {
+        return race.isFinished();
+    }
+
+    public List<String> getWinner() {
+        return race.getWinner().stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    public String getRaceStatus() {
+        return race.getRaceStatus();
+    }
+
 }
