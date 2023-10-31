3d03deb126bd8e801fdf6b890ef28751621735e2
YeaChan05 <qkenrdl05@gmail.com>
2023-10-31 21:20:36 +0900

    [REFACTOR] 테스트케이스 수정

--- src/test/java/racingcar/domain/StadiumTest.java
+++ src/test/java/racingcar/domain/StadiumTest.java
@@ -16,40 +16,34 @@ class StadiumTest {
     private Stadium stadium;
     private int gameCount;
 
-
     @Test
     void 경주_진행_테스트() {
         gameCount = 5;
-        List<User> users = List.of(User.of("김희겸"), User.of("엄성준"), User.of("김현진"));
-        stadium = new Stadium(gameCount, users);
+        List<String> names = List.of("김희겸","엄성준","김현진");
+        stadium = Stadium.of(gameCount, names);
         try (MockedStatic<Randoms> randoms = mockStatic(Randoms.class)) {
             given(Randoms.pickNumberInRange(0, 9)).willReturn(5);
             stadium.startRace();
-            users.forEach(user -> {
-                assertThat(user.getProceedCount()).isEqualTo(gameCount);
-                assertThat(user.visualizeProgress()).contains(user.getName() + " : ");
-            });
+            stadium.getUsers().stream()
+                    .forEach(user -> {
+                        assertThat(user.getProceedCount()).isEqualTo(gameCount);
+                        assertThat(user.visualizeProgress()).contains(user.getName() + " : ");
+                    });
         }
     }
 
     @Test
     void 우승자_출력_테스트() {
-        User user1 = User.of("김현진");
-        User user2 = User.of("김희겸");
-        User user3 = User.of("엄성준");
-
-        user1.setProceedCount(2);
-        user2.setProceedCount(2);
-        user3.setProceedCount(3);
-
+        List<String> names = List.of("김희겸","엄성준","김현진");
         gameCount = 5;
-        stadium = spy(new Stadium(gameCount, List.of(user1,user2,user3)));
+
+        stadium = spy(Stadium.of(gameCount, names));
 
         ByteArrayOutputStream outputMessage = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputMessage));
 
-        when(stadium.chooseWinner()).thenReturn(List.of(user3));
-        Stadium.displayResult(List.of(user3));
+        when(stadium.getWinner()).thenReturn(List.of(User.of("엄성준")));
+        Stadium.displayResult(List.of(User.of("엄성준")));
         assertThat("최종 우승자 : 엄성준\r\n").isEqualTo(outputMessage.toString());
     }
 
