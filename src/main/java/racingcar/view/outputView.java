c10c07427295565f76966432cdf15a60b5aee348
김관진 <cozyschooll@gmail.com>
2023-10-31 22:20:22 +0900

    Style: outputView 클래스내 문단 수정

--- src/main/java/racingcar/view/outputView.java
+++ src/main/java/racingcar/view/outputView.java
@@ -7,9 +7,7 @@ public class outputView {
 
     private static final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_NUMBER = "시도할 회수는 몇회인가요?";
-
     private static final String GAME_RESULT = "실행 결과";
-
     private static final String GAME_WINNER = "최종 우승자 : ";
 
     public void requestCarName() {
