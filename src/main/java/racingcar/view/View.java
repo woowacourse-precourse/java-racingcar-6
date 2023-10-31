deb528b8e884b0724237bd30d69e9ac936b3b7fd
dustjq1004 <dustjq1005@naver.com>
2023-10-31 19:54:19 +0900

    yeonsup/feat : View 메서드 추가 1. showRaceResultHeader 추가

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,11 +1,14 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 
 public class View {
 
     private final String APPLICANT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String RACE_COUNT_VIEW = "시도할 회수는 몇회인가요?";
+    private final String RACE_RESULT_HEADER = "실행 결과";
+    private final String ROUND = "ROUND";
 
     public String showApplicantView() {
         System.out.println(APPLICANT_VIEW);
@@ -21,5 +24,35 @@ public class View {
         return Integer.parseInt(raceCount);
     }
 
+    public int showRaceResultHeader() {
+        System.out.println(RACE_RESULT_HEADER);
+    }
 
+    public void showRoundResultView(Map<String, Integer> roundResult) {
+        StringBuffer result = new StringBuffer();
+
+        System.out.println(result.toString());
+    }
 }
+/*
+실행 결과
+pobi : -
+woni :
+jun : -
+
+pobi : --
+woni : -
+jun : --
+
+pobi : ---
+woni : --
+jun : ---
+
+pobi : ----
+woni : ---
+jun : ----
+
+pobi : -----
+woni : ----
+jun : -----
+* */
