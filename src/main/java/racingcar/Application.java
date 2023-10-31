6d438194c838dec77b9c2b81c3d17d75baaadf46
장세진 <jsejin95@gmail.com>
2023-10-31 23:38:43 +0900

    feat: 자동차 경주 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import racingcar.domain.Judgement;
+import racingcar.domain.Race;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Race race = new Race();
+        race.play();
     }
+
+
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,8 +2,6 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
-import java.util.List;
 
 public class Car implements Comparable<Car> {
     private String name;
@@ -11,6 +9,7 @@ public class Car implements Comparable<Car> {
 
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
     }
 
@@ -19,6 +18,7 @@ public class Car implements Comparable<Car> {
         if (randomNum >= 4) {
             position++;
         }
+        System.out.println(getNowPositionToString());
     }
 
     public int getPosition() {
@@ -28,10 +28,21 @@ public class Car implements Comparable<Car> {
     public String getName() {
         return name;
     }
+    public String getNowPositionToString() {
+        return name + " : " +  "-".repeat(position);
+    }
+    public void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("이름의 길이가 5를 초과합니다.");
+        }
+    }
+
 
     public void setPosition(int moveCount) {
         this.position = moveCount;
     }
+
+
     @Override
     public int compareTo(Car c) {
         return c.position - this.position;
