a923f5e5b98c28cde046565bfa7cfaabfaff8c27
jeongjihun <wlgns2234@naver.com>
2023-10-31 12:14:21 +0900

    test: isCountZero() 메서드에 대한 테스트 추가

--- src/test/java/racingcar/model/CountTest.java
+++ src/test/java/racingcar/model/CountTest.java
@@ -19,6 +19,26 @@ class CountTest {
         assertThat(result).isEqualTo(new Count(3));
     }
 
+    @DisplayName("isCountZero() 테스트 : 카운트가 0이라면 true를 반환한다.")
+    @Test
+    void isCountZero_0이면_true() {
+        Count count = new Count(0);
+
+        boolean result = count.isCountZero();
+
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("isCountZero() 테스트 : 카운트가 0이 아니라면 false를 반환한다.")
+    @Test
+    void isCountZero_0이_아니라면_false() {
+        Count count = new Count(1);
+
+        boolean result = count.isCountZero();
+
+        assertThat(result).isFalse();
+    }
+
     @DisplayName("down() 예외 테스트 : 0일 때 감소시키려고 하면 IllegalArgumentException가 발생한다.")
     @Test
     void 예외발생_카운트가_0일때_감소시키려는_경우() {
