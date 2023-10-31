ed6a6edd2ed3cdb33365d8795e3e6918006e1a78
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-31 23:18:16 +0900

    fix(InputParserTest) : 테스트 코드 수정

--- src/test/java/racingcar/InputParserTest.java
+++ src/test/java/racingcar/InputParserTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,26 +13,12 @@ import racingcar.util.InputParser;
 public class InputParserTest {
     InputParser inputParser = new InputParser();
 
-    private boolean deepEquals(List<Car> cars1, List<Car> cars2) {
-        List<String> carNames1 = new ArrayList<>();
-        List<String> carNames2 = new ArrayList<>();
-
-        for (Car car : cars1) {
-            carNames1.add(car.toString());
+    private List<String> getNames(List<Car> cars) {
+        List<String> names = new ArrayList<>();
+        for (Car car : cars) {
+            names.add(car.toString());
         }
-        for (Car car : cars2) {
-            carNames2.add(car.toString());
-        }
-
-        if (carNames1.size() != carNames2.size()) {
-            return false;
-        }
-        for (int i = 0; i < carNames1.size(); i++) {
-            if (carNames1.get(i).equals(carNames2)) {
-                return false;
-            }
-        }
-        return true;
+        return names;
     }
 
     @Test
@@ -40,18 +27,12 @@ public class InputParserTest {
         String input1 = "car1,car2,car3";
         String input2 = "q.w,car3";
 
-        List<Car> result1 = inputParser.parseCarName(input1);
-        List<Car> result2 = inputParser.parseCarName(input2);
-        List<Car> expected1 = new ArrayList<>();
-        expected1.add(new Car("car1"));
-        expected1.add(new Car("car2"));
-        expected1.add(new Car("car3"));
-        List<Car> expected2 = new ArrayList<>();
-        expected2.add(new Car("q.w"));
-        expected2.add(new Car("car3"));
-
-        assertThat(deepEquals(result1, expected1)).isTrue();
-        assertThat(deepEquals(result2, expected2)).isTrue();
+        List<String> result1 = getNames(inputParser.parseCarName(input1));
+        List<String> result2 = getNames(inputParser.parseCarName(input2));
+        List<String> expected1 = Arrays.asList("car1", "car2", "car3");
+        List<String> expected2 = Arrays.asList("q.w", "car3");
+        assertThat(result1).containsAll(expected1);
+        assertThat(result2).containsAll(expected2);
     }
 
     @Test
