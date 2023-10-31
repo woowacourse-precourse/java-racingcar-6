d5b1746f1b3bfc8db154e1637dca726af175210e
Yoongsyu <dkdk33555@gmail.com>
2023-11-01 00:49:11 +0900

    feat: Print 클래스에 최종 우승자 출력 기능 구현
    최종 우승자가 몇인지 확인하고 그에 따른 우승자의 이름을 출력하는 기능을 구현하였다

--- src/main/java/racingcar/Print.java
+++ src/main/java/racingcar/Print.java
@@ -22,6 +22,14 @@ public class Print {
         System.out.println("");
     }
 
-
+    public static void PrintWinner(List<String> mostDashesCar) {
+        // 최종적으로 가장 많은 "-" 값을 가진 key(자동차) 출력
+        if (!mostDashesCar.isEmpty()) {
+            String winnerCars = String.join(", ", mostDashesCar);
+            System.out.println("최종 우승자 : " + winnerCars);
+        } else {
+            System.out.println("우승자는 없습니다.");
+        }
+    }
 
 }
