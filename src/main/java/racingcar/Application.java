6108e02672f97c219adf7ecdd954b2bfa385b53c
NahyeChoi <skgp0628@naver.com>
2023-10-31 22:50:32 +0900

    feat(racingcar): 최종 출력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,31 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	
+    	int attemptNum;
+    	List<Integer> carRecord;
+    	List<String> carName;
+    	CheckExceptions checkException = new CheckExceptions();
     	Player player = new Player();
     	player.inputCarName();
-    	player.inputAttemptNumbers();
-    }
-}
+    	carName = player.carName;
+    	checkException.checkCarNameLength(carName);
+    	checkException.checkNotNull(carName);
+    	player.inputAttemptNumber();
+    	attemptNum = player.attemptNumber;
+    	checkException.checkNumber(attemptNum);
+    	carRecord = player.carRecord();
+    	
+    	System.out.println("\n실행 결과");
+    	PrintResult racingGame = new PrintResult();
+    	for(int i = 0; i < attemptNum; i++) {
+    		Car driveStop = new Car();
+    		carRecord = driveStop.driveCar(player.totalCar, carRecord); //랜덤값 판단하여 전진
+    		racingGame.viewRaceCar(player.totalCar, carRecord, carName);
+    		System.out.println();
+    	}
+    	racingGame.racingWinner(carRecord, carName);
+    } // end of main
+} // end of class
