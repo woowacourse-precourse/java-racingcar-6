ce4006b9820daa28271f243ba77d6c81711ccdf3
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-31 19:01:12 +0900

    FEAT / add ONE_CAR_RESULT

--- src/main/java/racingcar/util/Constant.java
+++ src/main/java/racingcar/util/Constant.java
@@ -3,5 +3,10 @@ package racingcar.util;
 public class Constant {
     public static final int MAX_CAR_NAME = 5;
     public static final int MIN_CAR_NAME = 1;
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MIN_CAN_MOVE_VALUE = 4;
+
     public static final String SPLIT_UNIT = ",";
+    public static final String ONE_CAR_RESULT = "%s : %s";
 }
