536945b553b605f067b5f112665a0d5941e808de
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-31 10:41:13 +0900

    [FEAT] updateCarStats 함수 구현
    전진 혹은 멈추는 기능을 구현. randomNumber가 4 이상인지 체크한 다음에 value에 +1을 해주는 기능

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,12 +1,24 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashMap;
 
 public class Game {
+    User user = new User();
+    LinkedHashMap<String, Integer> racingCar = user.carReset();
     private int randomNumber(){
         return Randoms.pickNumberInRange(0, 9);
     }
     private boolean checkNumber(Integer randomNumber){
         return randomNumber >= 4;
     }
+     private LinkedHashMap<String, Integer> updateCarStats(LinkedHashMap<String, Integer> racingCar) {
+         for (String car : racingCar.keySet()) {
+             if (checkNumber(randomNumber())) {
+                 int count = racingCar.get(car);
+                 racingCar.put(car, count + 1);
+             }
+         }
+         return racingCar;
+     }
 }
