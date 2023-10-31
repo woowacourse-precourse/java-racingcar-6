c12554fd61c6bef863dffe1aec83891cb73b1488
k-kmw <kimmw2_@naver.com>
2023-10-31 11:31:11 +0900

    test: car test 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
@@ -10,6 +11,12 @@ class CarTest {
     @Test
     void moveTest() {
         Car car = new Car("myCar");
+        boolean isMoveCondition = car.isMoveCondition();
+        car.move(isMoveCondition);
+        if (car.isMoveCondition()) {
+            assertThat(car.getStatus().equals("-"));
+        } else {
+            assertThat(car.getStatus().isEmpty());
+        }
     }
-
 }
