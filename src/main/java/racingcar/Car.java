b6c04492de2f4b1820c0206c46a2d9f7f9196adc
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-31 06:39:39 +0000

    [Car] Update : Random값 추출 방식 pickNumberInRange로 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position;
@@ -18,7 +20,7 @@ public class Car {
     }
 
     public void move() {
-        int randomValue = (int) (Math.random() * 10);
+        int randomValue = (int) (Randoms.pickNumberInRange(1, 9));
         if (randomValue >= 4) {
             position++;
         }
