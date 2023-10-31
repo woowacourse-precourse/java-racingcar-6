612aa6066ae012d305d1d38b39f1cdf55aabb100
iceprins <junghankyul@gmail.com>
2023-10-31 13:49:29 +0900

    style: 메소드명 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,11 @@ package racingcar.view;
 
 public class OutputView {
 
-    public void showResult() {
+    public void showResultMessage() {
         System.out.println("실행 결과");
     }
 
-    public void showWinner() {
+    public void showWinnerMessage() {
         System.out.print("최종 우승자 : ");
     }
 }
