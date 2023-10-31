89717a8818a3e6fd35166de9bae90431b73c6ded
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-31 23:16:44 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,6 +7,7 @@ import racingcar.util.RandomUtil;
 public class CarTest {
 
     private final Car car = new Car("july");
+    private int beforePosition = this.car.getPosition();
 
     @Test
     void 자동자_이름_확인() {
@@ -22,19 +23,15 @@ public class CarTest {
 
     @Test
     void move_3이하면_이동_안함_테스트() {
-        int randomNumber = RandomUtil.getRandomNumberRange(0, 3);
+        this.car.move(RandomUtil.getRandomNumberRange(0, 3));
 
-        car.move(randomNumber);
-
-        Assertions.assertThat(this.car.getPosition()).isEqualTo(0);
+        Assertions.assertThat(this.car.getPosition()).isEqualTo(this.beforePosition);
     }
 
     @Test
     void move_4이상이면_이동_테스트() {
-        int randomNumber = RandomUtil.getRandomNumberRange(4, 9);
+        car.move(RandomUtil.getRandomNumberRange(4, 9));
 
-        car.move(randomNumber);
-
-        Assertions.assertThat(this.car.getPosition()).isEqualTo(1);
+        Assertions.assertThat(this.car.getPosition()).isEqualTo(this.beforePosition + 1);
     }
 }
