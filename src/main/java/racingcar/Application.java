d040b0f8d11652e30a1c72f1f98c0148f04f3731
mksystem98 <mksystem98@gmail.com>
2023-11-01 00:36:13 +0900

    feat: main함수에서의 게임 로직 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        RacingController controller = new RacingController(inputView);
+        controller.playRacingGame();
     }
 }
