31f41ae9806d45b27e4431d2a41d74ef19e6899f
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-31 13:42:30 +0900

    refactor: 자동차 이름 입력 메서드 반환 타입 리스트로 변경

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class UserInput {
 
-    public static String[] inputNames() {
+    public static List<String> inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         Validator.blankCheck(input);
         List<String> names = List.of(input.split(","));
         Validator.nameDuplicatenCheck(names);
 
-        return input.split(",");
+        return List.of(input.split(","));
     }
 
     public static int inputNumberOfGames() {
