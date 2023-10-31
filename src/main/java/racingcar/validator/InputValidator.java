6bc208384ddde3b3f9b8e8d7ade04b8fec9feb80
minisyu <iswater.kim@gmail.com>
2023-10-31 11:27:32 +0900

    refactor: 패키지 분리

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -3,12 +3,20 @@ package racingcar.util;
 public final class InputValidator {
     private static final String CAR_NAMES_REGEX = "^(\\w+,)+\\w+$";
     private static final String NUMERIC_REGEX = "\\d+";
+    private static final String COMMA = ",";
 
     public void validateInputFormat(String input) {
         if (!input.matches(CAR_NAMES_REGEX)) {
             throw new IllegalArgumentException(Err.INVALID_CAR_NAMES_FORMAT.getMessage());
         }
     }
+
+    public void validateInputCarCount(String input) {
+        if (input.split(COMMA).length == 1) {
+            throw new IllegalArgumentException("자동차는 2대 이상 있어야 경주를 시작할 수 있습니다. 다시 입력해 주세요.");
+        }
+    }
+
 
     public void validateNonEmpty(String input) {
         if (input.isBlank()) {
