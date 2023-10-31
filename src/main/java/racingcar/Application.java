7de2513133349559974637ed52a3e86f7d17d90e
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-31 14:45:22 +0900

    refactor: Application에서 controller start() method 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.validation.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingController racingController = new RacingController(
+                new InputView(new InputValidator()),
+                new OutputView()
+        );
+        racingController.start();
     }
 }
