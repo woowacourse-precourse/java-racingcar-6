5ead0eaac2b4e92f669b15622e21a79778dd3d85
ghdeo <goldencorn7@naver.com>
2023-10-31 23:37:53 +0900

    feat: Add RacingService method

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,28 @@
 package racingcar.service;
 
+import racingcar.domain.Cars;
+
+import java.util.Scanner;
+
 public class RacingService {
+    private Cars cars;
+
+    public void startRace() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Scanner sc = new Scanner(System.in);
+        String playerInput = sc.nextLine();
+        String[] names = playerInput.split(",");
+        cars = new Cars(names);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int trial = sc.nextInt();
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < trial; i++) {
+            cars.race();
+        }
+
+        cars.printWinners();
+    }
 
 }
