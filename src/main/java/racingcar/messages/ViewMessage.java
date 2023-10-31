f8196283cfcb37a7cac306316ac3739e025df03e
조은별 <joeunbyeol98@gmail.com>
2023-10-31 10:15:49 +0900

    feat(ViewMessage): 입력 메시지 정의

---
+++ src/main/java/racingcar/messages/ViewMessage.java
@@ -0,0 +1,10 @@
+package racingcar.messages;
+
+public class ViewMessage {
+    public static String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    public static String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?\n";
+
+    public static String OUTPUT_RESULT = "실행 결과\n";
+    public static String OUTPUT_CAR_RESULT = "%s : %s\n";
+    public static String MOVEMENT = "-";
+}
