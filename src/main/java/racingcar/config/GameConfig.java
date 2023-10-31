6909b9dd4d114644271d9320010202dcdb833b05
jae woo Jung <awfjol2008@naver.com>
2023-10-31 11:40:39 +0900

    feat: GameConfig enum에 이름 검증 조건 추가

--- src/main/java/racingcar/config/GameConfig.java
+++ src/main/java/racingcar/config/GameConfig.java
@@ -5,7 +5,8 @@ public enum GameConfig {
     MOVEMENT_CONDITION_NUMBER(4),
     MOVEMENT_DISTANCE(1),
     RANDOM_NUMBER_RANGE_START(0),
-    RANDOM_NUMBER_RANGE_END(9);
+    RANDOM_NUMBER_RANGE_END(9),
+    NAME_LENGTH_CONDITION(5);
 
 
     private final int number;
