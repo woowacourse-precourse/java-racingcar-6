94c7f4330b7c8e216dd1bdd0046c04d3f20acf08
Woo Joo Chae <enaenen@naver.com>
2023-11-01 00:08:27 +0900

    REFACTOR: Deprecated 추가

--- src/main/java/racingcar/CarNameRegistration.java
+++ src/main/java/racingcar/CarNameRegistration.java
@@ -2,45 +2,42 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.platform.commons.util.StringUtils;
+import racingcar.utils.StringValidator;
 
+@Deprecated
 public class CarNameRegistration {
-    private static final int MIN_CAR_NAME_LENGTH = 2;
-    private static final String splitChar = ",";
 
-    private List<CarName> carNameList;
+	private static final int MIN_CAR_NAME_LENGTH = 2;
+	private static final String DELIMITER = ",";
 
-//    public CarNameRegistration() {
-//        carNameList = new ArrayList<>();
-//    }
-    public CarNameRegistration(String inputCarNames) throws IllegalArgumentException {
-        carNameList = new ArrayList<>();
-        // static import 가능?
-        if (StringUtils.isBlank(inputCarNames)) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_BLANK);
-        }
-        String[] splitCarName = inputCarNames.split(splitChar);
-        validateInputCarName(splitCarName);
-        addToCarNameList(splitCarName);
-    }
+	private List<CarName> carNameList;
 
-    private void validateInputCarName(String[] splitCarName) throws IllegalArgumentException {
-        if (splitCarName.length < MIN_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.MINIMUM_CAR_NEEDED);
-        }
-    }
+	public CarNameRegistration(String inputCarNames) throws IllegalArgumentException {
+		if (StringValidator.isBlank(inputCarNames)) {
+			throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_BLANK);
+		}
+		String[] splitCarName = inputCarNames.split(DELIMITER);
+		validateCarListLength(splitCarName);
+		addCarToNameList(splitCarName);
+	}
 
-    private void addToCarNameList(String[] splitCarName) throws IllegalArgumentException {
-        carNameList = new ArrayList<>();
-        for (String carName : splitCarName) {
-            carNameList.add(new CarName(carName));
-        }
-    }
+	private void validateCarListLength(String[] splitCarName) throws IllegalArgumentException {
+		if (splitCarName.length < MIN_CAR_NAME_LENGTH) {
+			throw new IllegalArgumentException(ErrorMessage.MINIMUM_CAR_NEEDED);
+		}
+	}
+
+	private void addCarToNameList(String[] splitCarName) throws IllegalArgumentException {
+		carNameList = new ArrayList<>();
+		for (String carName : splitCarName) {
+			carNameList.add(new CarName(carName));
+		}
+	}
 
 
-    public List<CarName> getCarNameList() {
-        return carNameList;
-    }
+	public List<CarName> getCarNameList() {
+		return carNameList;
+	}
 
 
 }
