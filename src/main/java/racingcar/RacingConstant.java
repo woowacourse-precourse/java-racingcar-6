c64ae83a9cc575db3340224ed33aba92e4e650e3
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-10-31 21:19:21 +0900

    feat(RacingConstant): 게임 관련 상수 구현
     - 최소 이름 길이, 최대 이름 길이
     - 최소 반복 횟수, 최대 반복 횟수
     - 차량 이동 조건 기준 숫자

---
+++ src/main/java/racingcar/RacingConstant.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class RacingConstant {
+
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_NAME_LENGTH = 5;
+
+    public static final int MIN_PLAY_COUNT = 1;
+    public static final int MAX_PLAY_COUNT = 10;
+
+    public static final int CAR_MOVE_CONDITION_NUM = 4;
+
+}
