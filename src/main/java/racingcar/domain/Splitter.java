e38b20ae4f57670ea3586225d768c17df088bdf0
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 22:00:54 +0900

    refactor(Splitter): 주석 제거
    메서드 명으로도 충분히 알아 볼 수 있는 불필요한 주석은 제거함

--- src/main/java/racingcar/domain/Splitter.java
+++ src/main/java/racingcar/domain/Splitter.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Splitter {
-    //자동차 이름은 쉼표(,)를 기준으로 구분한다.
+
     public List<String> stringToList(String carNameLine) {
         List<String> list = Arrays.asList(carNameLine.split(","));
 
@@ -20,6 +20,6 @@ public class Splitter {
             carList.add(car);
         }
 
-        return carList; // 리스트 반환
+        return carList;
     }
 }
