dcddaa040b141024ec95fbd5b51ee5ee00134159
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-10-31 22:50:58 +0900

    refactor (racingcar - Application): main() 안에서의 과정들 각각 개별 함수화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,27 +6,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-
         List<String> carNamesArrayList = enterCarNames();
-
 
         int trialNumber = enterTrialNumber();
 
+        Map<String, String> raceResultsHMap = runRace(carNamesArrayList, trialNumber);
 
-        System.out.println("실행 결과");
-        Map<String, String> raceResultsHMap = new LinkedHashMap<>();
-
-        Map<String, String> readyForRaceHMap = Race.registerCars(carNamesArrayList);
-        for (int i = 0; i < trialNumber; i++) {
-            raceResultsHMap = Race.doRace(readyForRaceHMap);
-            Race.showRace(raceResultsHMap);
-        }
-
-
-        List<String> winnersArrayList = Winner.pickWinners(raceResultsHMap);
-        Winner.showWinners(winnersArrayList);
-
+        showRaceResult(raceResultsHMap);
     }
+
 
     private static List<String> enterCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -46,5 +34,27 @@ public class Application {
 
         return trialNumber;
     }
+
+
+    private static Map<String, String> runRace(List<String> carNamesArrayList, int trialNumber) {
+        System.out.println("실행 결과");
+
+        Map<String, String> raceResultsHMap = new LinkedHashMap<>();
+
+        Map<String, String> readyForRaceHMap = Race.registerCars(carNamesArrayList);
+        for (int i = 0; i < trialNumber; i++) {
+            raceResultsHMap = Race.doRace(readyForRaceHMap);
+            Race.showRace(raceResultsHMap);
+        }
+
+        return raceResultsHMap;
+    }
+
+
+    private static void showRaceResult(Map<String, String> raceResultsHMap){
+        List<String> winnersArrayList = Winner.pickWinners(raceResultsHMap);
+        Winner.showWinners(winnersArrayList);
+    }
+
 
 }
