9c88829ac48b5e1670ad06af00a5f1efb5382478
Joohui <joohuii96@gmail.com>
2023-11-01 03:50:50 +0900

    style: 테스트에 필요없는 public 제거

--- src/test/java/racingcar/common/ValidatorTest.java
+++ src/test/java/racingcar/common/ValidatorTest.java
@@ -16,7 +16,7 @@ class ValidatorTest {
         @DisplayName("빈 문자열 입력 시 예외 발생")
         @ParameterizedTest(name = "carName=\"{0}\"")
         @ValueSource(strings = {" ", " ,", "pobi, ,jun"})
-        public void empty(String carNames) {
+        void empty(String carNames) {
             StringToListConverter converter = new StringToListConverter();
             List<String> carNameList = converter.convert(carNames);
 
@@ -29,7 +29,7 @@ class ValidatorTest {
         @DisplayName("이름 입력 안할 시 예외 발생")
         @ParameterizedTest(name = "carName=\"{0}\"")
         @ValueSource(strings = {"", ","})
-        public void nullTest(String carNames) {
+        void nullTest(String carNames) {
             StringToListConverter converter = new StringToListConverter();
             List<String> carNameList = converter.convert(carNames);
 
@@ -40,7 +40,7 @@ class ValidatorTest {
 
         @DisplayName("중복된 이름 입력 시 예외 발생")
         @Test
-        public void duplicated() {
+        void duplicated() {
             //given
             String carNames = "pobi, pobi";
             StringToListConverter converter = new StringToListConverter();
@@ -54,7 +54,7 @@ class ValidatorTest {
 
         @DisplayName("길이가 6이상일 경우 예외 발생")
         @Test
-        public void overLength() {
+        void overLength() {
             //given
             String carNames = "longlongname";
             StringToListConverter converter = new StringToListConverter();
@@ -72,7 +72,7 @@ class ValidatorTest {
     class RoundNumberTest {
         @DisplayName("문자 입력 시 예외 발생")
         @Test
-        public void notInteger() {
+        void notInteger() {
             //given
             String roundNumber = "aaa";
 
@@ -85,7 +85,7 @@ class ValidatorTest {
         @DisplayName("양의 정수가 아닌 숫자 입력 시 예외 발생")
         @ParameterizedTest(name = "횟수={0}")
         @ValueSource(strings = {"-1", "0"})
-        public void notPositiveInteger(String roundNumber) {
+        void notPositiveInteger(String roundNumber) {
             Assertions.assertThatThrownBy(() -> Validator.validateRoundNumber(roundNumber))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorMessageConstants.ROUND_NUMBER_WRONG_RANGE_MESSAGE);
