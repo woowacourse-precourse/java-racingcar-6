c76f2bc3422278d948b50b2695d43faa7d4dc786
kihwn <kihwn98@gmail.com>
2023-10-31 17:20:14 +0900

    Refactor: 응집도를 낮추기 위해 Application 클래스 내의 메서드를 Race 클래스로 이동
    모든 메서드를 race로 이동

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,17 +1,29 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 final class Race {
-    public String[] carArray;
-    public int tryNumber;
-    public int[] raceAccumulations;
+    private String[] carArray;
+    private int tryNumber;
+    private int[] raceAccumulations;
 
-    public Race(String[] carArray, int tryNumber, int[] raceAccumulations) {
+    private Race(String[] carArray, int tryNumber, int[] raceAccumulations) {
         this.carArray = carArray;
         this.tryNumber = tryNumber;
         this.raceAccumulations = raceAccumulations;
+    }
 
+    public String[] getCarArray() {
+        return carArray;
+    }
+
+
+    public int[] getRaceAccumulations() {
+        return raceAccumulations;
     }
 
     public static Race initializeRaceInformation() {
@@ -32,6 +44,71 @@ final class Race {
             }
             if (carName.isEmpty()) {
                 throw new IllegalArgumentException("자동차 이름으로 공백 값을 입력하셨습니다. 오류 발생");
+            }
+        }
+    }
+
+    public void playGame() {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < tryNumber; i++) {
+            for (int changeScore = 0; changeScore < carArray.length; changeScore++) {
+                giveScore(changeScore);
+            }
+            printEachRace();
+        }
+    }
+
+    private void giveScore(int chanceScore) {
+        int randomNum = makeRandomNum();
+        if (randomNum >= 4) {
+            raceAccumulations[chanceScore] += 1;
+        }
+    }
+
+    private void printEachRace() {
+        for (int index = 0; index < carArray.length; index++) {
+            System.out.print(carArray[index] + " : ");
+            printEachCarScore(index);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private int makeRandomNum() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        return randomNum;
+    }
+
+    private void printEachCarScore(int index) {
+        int substitutionNum = raceAccumulations[index];
+        for (int printNum = 0; printNum < substitutionNum; printNum++) {
+            System.out.print("-");
+        }
+    }
+
+    private ArrayList<String> findFinalWinner() {
+        int maxScore = 0;
+        ArrayList<String> WinnerNames = new ArrayList<>();
+        for (int index = 0; index < carArray.length; index++) {
+            if (raceAccumulations[index] > maxScore) {
+                maxScore = raceAccumulations[index];
+                WinnerNames.clear();
+                WinnerNames.add(carArray[index]);
+            } else if (raceAccumulations[index] == maxScore) {
+                WinnerNames.add(carArray[index]);
+            }
+        }
+        return WinnerNames;
+    }
+
+    public void printFinalResult() {
+        ArrayList<String> WinnerNames = findFinalWinner();
+        System.out.print("최종 우승자 :");
+        for (int winnerName = 0; winnerName < WinnerNames.size(); winnerName++) {
+            if (winnerName == 0) {
+                System.out.print(" " + WinnerNames.get(winnerName));
+            } else {
+                System.out.print(", " + WinnerNames.get(winnerName));
             }
         }
     }
