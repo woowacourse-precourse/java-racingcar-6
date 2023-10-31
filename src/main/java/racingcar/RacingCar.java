993059fb53df40bcc9d4d5d7398e14f374081017
dustjq1004 <dustjq1005@naver.com>
2023-10-31 19:15:08 +0900

    yeonsup/feat : RaceingCar 기능 추가 1. getRaceLogBy 구현 2. getPosition 메서드 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,14 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.validation.Validation;
 
 public class RacingCar {
 
     private String name;
     private int position;
+    private List<Integer> raceLog;
 
     public RacingCar(String name) {
         this.name = name;
+        this.raceLog = new ArrayList<>();
         this.position = 0;
     }
 
@@ -18,6 +22,14 @@ public class RacingCar {
             return true;
         }
         return false;
+    }
+
+    public Integer getRaceLogBy(int round) {
+        return raceLog.get(round);
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     // 테스트를 위해 추가
