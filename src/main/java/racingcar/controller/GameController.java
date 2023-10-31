19669ef9417e0beccfc8be4f19a64b3ab927875b
chan-won <clw8679@naver.com>
2023-10-31 11:41:13 +0900

    feat: GameController 기능 구현
    게임을 진행하는 playGame 구현
    게임을 종료하는 endGame 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,8 +1,10 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 import racingcar.io.Input;
+import racingcar.service.GameService;
 
 public class GameController {
     private RacingGame racingGame;
@@ -13,5 +15,15 @@ public class GameController {
         this.racingGame = new RacingGame(finalRound, names);
     }
 
+    public void playGame() {
+        while (!racingGame.isFinalRound()) {
+            GameService.playSingleRound(racingGame);
+            //각 라운드 종료후 게임 결과 출력 기능 추가
+        }
+    }
 
+    public void endGame() {
+        List<Car> winners = GameService.getWinners(racingGame.getCars());
+        //우승자 출력 기능 추가
+    }
 }
