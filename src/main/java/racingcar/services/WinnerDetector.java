08643ae6f95d2ba4d6f3ed6a4343313580fc2d15
minjong kim <godminjong@naver.com>
2023-10-31 17:13:47 +0900

    feat: 우승자의 목록화된 리스트를 통해 포멧에 맞에 출력하는 기능 추가.
    detect를 통해 구해린 우승자 명단을 포멧-
    (예시: '최종 우승자 : pobi, jun')에 맞게 출력하는 기능 작성

--- src/main/java/racingcar/services/WinnerDetector.java
+++ src/main/java/racingcar/services/WinnerDetector.java
@@ -1,8 +1,29 @@
 package racingcar.services;
 
+import java.util.List;
 import java.util.Map;
+import racingcar.constant.Constant;
+import racingcar.validate.Validator;
+import racingcar.validate.impls.WinnerDetectorValidator;
 
 public class WinnerDetector {
+    public static void printAllWinners(RaceGameState state) {
+        List<String> winners = detect(state);
+        Validator validator = new WinnerDetectorValidator();
+        validator.validate(winners);
+        String printWinnersString = String.join(", ", winners);
+        System.out.println(Constant.FINAL_WINNER + printWinnersString);
+    }
+
+    private static List<String> detect(RaceGameState state) {
+        int longestDistance = findLongestDistance(state);
+        Map<String, Integer> stateOfEachCar = state.getStateOfEachCar();
+        return state.getStateOfEachCar()
+                .keySet()
+                .stream()
+                .filter(element -> isLongest(stateOfEachCar, element, longestDistance))
+                .toList();
+    }
 
     private static boolean isLongest(Map<String, Integer> state, String target, int distance) {
         return state.get(target) == distance;
