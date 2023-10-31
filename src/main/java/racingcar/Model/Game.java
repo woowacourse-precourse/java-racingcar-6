180b76af8728226bf164b3a19c768ab634dd8403
minjumost <95904729+minjumost@users.noreply.github.com>
2023-11-01 01:05:39 +0900

    feat: 예외처리 추가

--- src/main/java/racingcar/Model/Game.java
+++ src/main/java/racingcar/Model/Game.java
@@ -1,9 +1,15 @@
 package racingcar.Model;
 
+import static racingcar.Util.Constants.MAX_LENGTH_NAME;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Game {
     private Comparator<Car> comparator = new Comparator<Car>() {
@@ -16,15 +22,44 @@ public class Game {
     private int repetition;
 
     public Game(List<String> carNames){
+        isDuplicated(carNames);
         for(String carName: carNames){
+            isLessThanMaxLength(carName);
+            isBlank(carName);
             Car car = new Car(carName);
             carList.add(car);
         }
         repetition = 0;
     }
 
+    private void isLessThanMaxLength(String name){
+        if(name.length() > MAX_LENGTH_NAME){
+            throw new IllegalArgumentException("최대 길이 보다 긴 이름을 입력했습니다.");
+        }
+    }
+
+    private void isBlank(String name){
+        if(name.isBlank()){
+            throw new IllegalArgumentException("이름을 제대로 작성해주세요.");
+        }
+    }
+
+    private void isDuplicated(List<String> nameList){
+        Set<String> nameSet = new HashSet<String>(nameList);
+        if (nameSet.size() != nameList.size()){
+            throw new IllegalArgumentException("중복된 이름을 사용하였습니다.");
+        }
+    }
+
     public void setRepetition(int repetition){
+        isPositiveNumber(repetition);
         this.repetition = repetition;
+    }
+
+    public void isPositiveNumber(int num){
+        if(num < 0){
+            throw new IllegalArgumentException("입력한 숫자가 잘못 됐습니다.");
+        }
     }
 
     public void forward(){
--- src/main/java/racingcar/Util/Constants.java
+++ src/main/java/racingcar/Util/Constants.java
@@ -3,4 +3,6 @@ package racingcar.Util;
 public final class Constants {
     public static final String ENTER_USER = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ENTER_REPETITION = "시도할 회수는 몇회인가요?";
+
+    public static final int MAX_LENGTH_NAME = 5;
 }
