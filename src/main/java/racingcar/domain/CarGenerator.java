29568fcf0134624af9315d29c368e7f437e5250a
dj <fjzl12@naver.com>
2023-10-31 16:17:13 +0900

    feat(CarGenerator): 자동차 객체 리스트 생성 기능 구현

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -6,13 +6,11 @@ import java.util.List;
 public class CarGenerator {
 
     public static List<Car> createCarList() {
-        // 여기서 setcarname으로 String List로 가져온 후 Car List 만들어 반환
-        List<Car> CarList = new ArrayList<Car>();
-        List<String> CarNames = UserInput.getCarNames();
-        for (String name : CarNames) {
-            CarList.add(new Car(name));
+        List<Car> carList = new ArrayList<Car>();
+        List<String> nameList = UserInput.getCarNames();
+        for (String name : nameList) {
+            carList.add(new Car(name));
         }
-        // CarNames 리스트에서 하나씩 꺼내 Car 객체 생성 및 리스트화
-        return CarList;
+        return carList;
     }
 }
