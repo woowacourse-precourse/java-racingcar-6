49c76f9c31b3ca2c4cffde8fbd1ca502aa090837
dustjq1004 <dustjq1005@naver.com>
2023-10-31 20:16:44 +0900

    yeonsup/feat : Track 기능 추가 1. round 기능 구현 2. selectWinners 기능 구현

--- src/main/java/racingcar/Track.java
+++ src/main/java/racingcar/Track.java
@@ -1,17 +1,43 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Track {
-    private final List<RacingCar> acingCars;
+    private final List<RacingCar> racingCars;
+    private final int RANDOM_START = 0;
+    private final int RANDOM_END = 9;
 
-    public Track(List<RacingCar> acingCars) {
-        this.acingCars = acingCars;
+    public Track(List<RacingCar> racingCars) {
+        this.racingCars = racingCars;
     }
 
-    public List<Map<String, Integer>> race() {
+    public Map<String, Integer> round(int number) {
+        Map<String, Integer> roundResult = new HashMap<>();
+        racingCars.forEach(racingCar -> {
+            int random = Randoms.pickNumberInRange(RANDOM_START, RANDOM_END);
+            racingCar.moveBy(random);
+            roundResult.put(racingCar.getName(), racingCar.getPosition());
+        });
+        return roundResult;
+    }
 
-        return null;
+    public List<String> selectWinners() {
+        List<String> winners = new ArrayList<>();
+        int maxPositionTemp = 0;
+
+        for (RacingCar racingCar : racingCars) {
+            maxPositionTemp = Math.max(maxPositionTemp, racingCar.getPosition());
+        }
+
+        int maxPosition = maxPositionTemp;
+        racingCars.stream().filter(racingCar -> racingCar.getPosition() == maxPosition)
+                .forEach(winner -> {
+                    winners.add(winner.getName());
+                });
+        return winners;
     }
 }
