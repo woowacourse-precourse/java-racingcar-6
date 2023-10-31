b88faaa34d745b91b386b2869bf91fbc2dba34c7
SuRiimm <surimii45@gmail.com>
2023-11-01 04:20:25 +0900

    feat(racingcar): 위치 출력 및 우승자 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
 public class OutputView {
+  public void printCarStatus(String name, int position){
+    System.out.println(name + ": "+ new String(new char[position]).replace("\0", "-"));
+  }
+  public void printWinners(String winners){
+    System.out.println("최종 우승자 : " + winners);
+  }
 }
