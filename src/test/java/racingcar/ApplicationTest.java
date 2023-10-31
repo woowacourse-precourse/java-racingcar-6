14b882ac728ceb22d26e7025522dffe0b953b2fc
aaa <cnj991279@gmail.com>
2023-10-31 11:15:48 +0900

    test: stringToStringList 메서드 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.core.Core;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,6 +14,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void stringToStringList_테스트() {
+        // given
+        final Core core = new Core();
+        String case1 = "pobi,woni,jun";
+        String case2 = "kim,lee,park";
+        String case3 = "james,tom,sam";
+
+        // when
+        final List<String> result1 =  core.stringToStringList(case1);
+        final List<String> result2 =  core.stringToStringList(case2);
+        final List<String> result3 =  core.stringToStringList(case3);
+
+        // then
+        assertThat(result1).isEqualTo(List.of("pobi", "woni", "jun"));
+        assertThat(result2).isEqualTo(List.of("kim", "lee", "park"));
+        assertThat(result3).isEqualTo(List.of("james", "tom", "sam"));
+    }
 
     @Test
     void 전진_정지() {
