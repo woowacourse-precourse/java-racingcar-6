77c130b35f787e95ffb73d8ca417e35ae90b89f9
Lee-Minju <mju7513@gmail.com>
2023-11-01 02:33:18 +0900

    구현한 기능들의 테스트 코드 구현과 입력 예외처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,21 +2,26 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.awt.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import org.mockito.internal.matchers.Null;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 자동차들 입력받기
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] cars = makingCarList(Console.readLine());
+        String[] cars = makingCarList();
+
         // 실시횟수 입력받기
-        System.out.println("시도할 회수는 몇회인가요?");
-        int rounds = Integer.parseInt(Console.readLine());
+        int rounds = askRounds();
+
         // 레이싱 진행
         System.out.println("실행 결과");
         Integer[] progress = new Integer[cars.length];
+        for(int i=0; i< cars.length; i++){
+            progress[i] = 0;
+        }
         for(int i=0; i<rounds; i++){
             for(int j=0; j<cars.length; j++){
                 randomForward(cars, progress, j);
@@ -24,6 +29,7 @@ public class Application {
             }
             System.out.println(" ");
         }
+
         // 우승자 판독 출력
         whoisWinner(cars, progress);
     }
@@ -39,17 +45,39 @@ public class Application {
         for(int i=0; i<cars.length; i++){
             maxPoint = Math.max(maxPoint, progress[i]);
         }
-        String winners = "";
+        ArrayList<String> winner = new ArrayList<String>();
         for(int j=0; j< progress.length; j++){
             if(progress[j] == maxPoint){
-                winners += (" " + cars[j]);
+                winner.add(cars[j]);
             }
         }
-        System.out.println("최종 우승자 :" + winners);
+        if(winner.size() == 1){
+            System.out.println("최종 우승자 : " + winner.get(0));
+        }
+        else{
+            String winnerString = winner.get(0);
+            for(int i=1; i<winner.size(); i++){
+                winnerString += ", " + winner.get(i);
+            }
+            System.out.println("최종 우승자 : " + winnerString);
+        }
 
     }
-    public static String[] makingCarList(String userinput){
-        String[] cars = userinput.split(",");
+    public static String[] makingCarList(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carname = Console.readLine();
+        if(carname.equals("")){
+            throw new IllegalArgumentException();
+        }
+        String[] cars = carname.split(",");
+        if(cars.length < 1){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0; i<cars.length; i++){
+            if(cars[i].length() > 5){
+                throw new IllegalArgumentException();
+            }
+        }
         return cars;
     }
     public static void printRacingStatus(int carindex, Integer[] progress, String[] cars){
@@ -58,6 +86,13 @@ public class Application {
             progressbar += "-";
         }
         System.out.println(cars[carindex] + " : " + progressbar);
+    }
+
+    public  static  int askRounds(){
+            System.out.println("시도할 횟수는 몇회인가요?");
+            String input = Console.readLine();
+            int rounds = Integer.parseInt(input);
+            return rounds;
     }
 
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,49 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 이름입력_받기와_처리() {
+        assertSimpleTest(() -> {
+            run("pobi,minju", "1");
+            assertThat(output()).contains("minju :");
+            assertThat(output()).contains("pobi : ");
+        });
+
+    }
+
+    @Test
+    void 실시횟수_입력받고_저장() {
+        assertSimpleTest(() -> {
+            run("pobi,minju", "1");
+
+            String result = output();
+            int roundsCount = 0;
+            roundsCount += (result.length() - result.replace("pobi","").length()) / 4;
+
+            assertThat(roundsCount < 3).isTrue();
+            assertThat(roundsCount > 0).isTrue();
+        });
+    }
+
+    @Test
+    void 우승자를_판단해서_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,minju", "1");
+                    assertThat(output()).contains("최종 우승자 : pobi, minju");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
