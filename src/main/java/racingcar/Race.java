e0ebb9dccc6215447321add910ca1031613c3857
ryugeumjung <fbgjung@gmail.com>
2023-10-31 17:17:22 +0900

    feat: 마지막 위치 결과 반환 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,21 +1,34 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 import model.Car;
 
 public class Race {
+    private final List<String> positionList = new ArrayList<>();
+    private final List<String> currentPositions = new ArrayList<>();
 
     public String raceCar(List<Car> cars, int times){
+
         while(times > 0){
             System.out.print("\n");
-
             for (Car car : cars) {
                 int move = car.moveForward();
                 System.out.println(car.getName() + " : " + move + car.getPosition());
+                currentPositions.add(car.getPosition());
             }
             times--;
-
         }
+        System.out.println(currentPositions);
+        for(int i = currentPositions.size()-1; i >= currentPositions.size() - cars.size(); i--){
+            positionList.add(currentPositions.get(i));
+        }
+        System.out.println(positionList);
+
         return "게임종료";
+    }
+
+    public void raceResult(){
+
     }
 }
