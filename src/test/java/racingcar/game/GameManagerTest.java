0d74c69f8b7b9e79e29df0a367749eb187c8eea2
황중섭 <jshwang9709@gmail.com>
2023-10-31 22:28:07 +0900

    test(round): 이동 횟수의 유효성 검증 테스트

--- src/test/java/racingcar/game/GameManagerTest.java
+++ src/test/java/racingcar/game/GameManagerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static racingcar.game.constant.CarExceptionMessage.*;
+import static racingcar.game.constant.RoundExceptionMessage.*;
 
 class GameManagerTest {
 
@@ -31,8 +32,25 @@ class GameManagerTest {
         assertCarExceptionCase("a,b,c,d,e,f,g,h,i,j,k", OUT_OF_COUNT_RANGE);
     }
 
+    @Test
+    void 이동_횟수가_정수가_아니면_예외_발생() {
+        assertRoundExceptionCase("bbb", NOT_INTEGER);
+    }
+
+    @Test
+    void 이동_횟수가_1이상_10이하가_아니면_예외_발생() {
+        assertRoundExceptionCase("0", OUT_OF_RANGE);
+        assertRoundExceptionCase("101", OUT_OF_RANGE);
+    }
+
     private void assertCarExceptionCase(String carNames, String errorMessage) {
         Assertions.assertThatThrownBy(() -> manager.setCars(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(errorMessage);
+    }
+
+    private void assertRoundExceptionCase(String movingCount, String errorMessage) {
+        Assertions.assertThatThrownBy(() -> manager.setRound(movingCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(errorMessage);
     }
