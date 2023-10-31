08c0c798cafefda156fb9d843324775ce3ed3ee0
parkheeddong <phj7214go@gmail.com>
2023-10-31 20:16:41 +0900

    feat : 회차 입력값의 유효성 검사 기능 구현

--- src/main/java/racingcar/CountInput.java
+++ src/main/java/racingcar/CountInput.java
@@ -12,6 +12,15 @@ public class CountInput {
         return input;
     }
 
-
+    static void checkCountInput(String input) throws IllegalArgumentException { // 입력값을 검사하는 메서드
+        try {
+            count = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+        if (count < 0 | count != (int)count) {
+            throw new IllegalArgumentException("자연수가 아닙니다.");
+        }
+    }
 
 }
