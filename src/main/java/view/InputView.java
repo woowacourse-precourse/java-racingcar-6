63b0c8a612fda89996601a5cb047dbfd63be8628
kitewater <97283971+kitewater@users.noreply.github.com>
2023-10-31 21:09:07 +0900

    fix: 자동차 생성하는 코드 추가
    자동차를 생성하는 코드를 stream을 사용함.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,18 +1,26 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import model.Car;
+import model.RacingCar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
-    public ArrayList createCar(){
-        System.out.printf("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        return new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
+    public ArrayList<Car> createCar(){
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        ArrayList<Car> carArrayList = Stream
+                .of(Console.readLine().split(","))
+                .map(name -> new RacingCar(name))
+                .collect(Collectors.toCollection(() -> new ArrayList<>()));
+        return carArrayList;
     }
 
     public int setLaps(){
-        System.out.printf("시도할 회수는 몇회인가요?\n");
+        System.out.print("시도할 회수는 몇회인가요?\n");
         int count = Integer.parseInt(Console.readLine());
         return count;
     }
