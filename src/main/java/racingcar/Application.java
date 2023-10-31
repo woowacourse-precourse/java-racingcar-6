a069a27a4297b4028ff7586815b33a6c7b8adbf3
jimin <grapes612@naver.com>
2023-11-01 03:20:01 +0900

    Created Utils class and added parseInput method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Utils {
+    public static List<String> parseInput(String input) {
+        String[] splitInput = input.split(",");
+        List<String> carNames = new ArrayList<>();
+        for (String name : splitInput) {
+            if(name.length() > 5)
+                throw new IllegalArgumentException();
+            carNames.add(name);
+        }
+        return carNames;
+    }  
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCar = Console.readLine();
+        List<String> carNames = Utils.parseInput(inputCar);
     }
 }
