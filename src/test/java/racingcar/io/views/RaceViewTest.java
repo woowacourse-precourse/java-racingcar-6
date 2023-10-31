9fc03a5c15e6bba8198aaca3d14069d5631d41d6
aka-nick <bechalrang@gmail.com>
2023-10-31 21:39:52 +0900

    추가(RaceViewTest) : RaceView 테스트 클래스
    RaceView에 대한 테스트 케이스를 추가
    
    RaceViewTest

---
+++ src/test/java/racingcar/io/views/RaceViewTest.java
@@ -0,0 +1,61 @@
+package racingcar.io.views;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import racingcar.collaborator.race.Racer;
+import racingcar.io.racing.RaceRandoms;
+
+class RaceViewTest {
+
+    @Test
+    void askRacersNames로_레이서리스트를_입력할수있음() {
+        String testInput = "김길동,   김홍삼, 김말미잘";
+        List<Racer> expected = Arrays.stream(testInput.split(","))
+                .map(String::strip)
+                .map(name -> new Racer(name, new RaceRandoms()))
+                .toList();
+
+        String input = enterInput(testInput);
+        customSetIn(input);
+        List<Racer> actual = new RaceView().askRacersNames();
+
+        assertThat(actual.size()).isEqualTo(expected.size());
+        String actualCollect = actual.stream()
+                .map(Racer::getName)
+                .collect(Collectors.joining(","));
+        String expectedCollect = expected.stream()
+                .map(Racer::getName)
+                .collect(Collectors.joining(","));
+        assertThat(actualCollect).isEqualTo(expectedCollect);
+    }
+
+    @Test
+    void askRoundNumber로_회수를입력할수있음() {
+        String testInput = "5";
+        int expected = 5;
+
+        String input = enterInput(testInput);
+        customSetIn(input);
+        Integer actual = new RaceView().askRoundNumber();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static String enterInput(String testString) {
+        return testString + "\n";
+    }
+
+    private static void customSetIn(String input) {
+        InputStream customInput =
+                new BufferedInputStream(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(customInput);
+    }
+
+}
