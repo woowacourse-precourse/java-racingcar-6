e486d1c389cceeec964b8c2fb19d908199c57f93
Chan Mi <rkdcksal02@gmail.com>
2023-10-31 20:39:25 +0900

    Race 클래스 - 여러 자동차 간의 경주를 처리하며 최종 우승자를 결정하는 로직을 구현. - 레이스를 시작하고 여러 차수마다 자동차의 이동 상태를 출력하는 부분 구현.

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void run(int numOfMoves) {
+        for (int i = 0; i < numOfMoves; i++) {
+            // 1. 각 차량을 순회하며 이동합니다.
+            for (Car car : cars) {
+                car.move();
+            }
+            // 2. 각 차수별 실행 결과 출력
+            ConsoleWrapper.printRaceStatus(cars);
+        }
+    }
+
+    public List<Car> getWinners() {
+        // 3. 최종 우승자를 결정하여 반환합니다.
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+}
