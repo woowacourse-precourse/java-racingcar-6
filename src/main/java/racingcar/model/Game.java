cc1bf9fd99d95e994faf8b45acfde0fc2efa0f17
besky <onlybesky@gmail.com>
2023-10-31 16:39:26 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -3,12 +3,7 @@ package racingcar.model;
 import java.util.HashMap;
 
 public class Game {
-    //    private int stageTimes;
     private HashMap<String, Integer> cars = new HashMap<>();
-
-//    public int getStageTimes() {
-//        return stageTimes;
-//    }
 
     public HashMap<String, Integer> getCars() {
         return cars;
@@ -22,31 +17,4 @@ public class Game {
         Integer originalStatus = cars.get(key);
         cars.replace(key, originalStatus + 1);
     }
-
-//    public class GameBuilder {
-//        private int stageTimes;
-//        private HashMap<String, Integer> cars;
-//
-//        public GameBuilder() {
-//        }
-//
-//        public GameBuilder(int stageTimes, HashMap<String, Integer> cars) {
-//            this.stageTimes = stageTimes;
-//            this.cars = cars;
-//        }
-//
-//        public GameBuilder stageTimes(int stageTimes) {
-//            this.stageTimes = stageTimes;
-//            return this;
-//        }
-//
-//        public GameBuilder cars(HashMap<String, Integer> cars) {
-//            this.cars = cars;
-//            return this;
-//        }
-//
-//        public Game build() {
-//            return new Game(this.stageTimes, this.cars);
-//        }
-//    }
 }
