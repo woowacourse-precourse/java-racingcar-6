2f37eb505c5cb4b5c08e9b0fca63322143484e8c
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 19:26:57 +0900

    feat : 상수 관리 enum 객체 생성

--- src/main/java/racingcar/enums/Constants.java
+++ src/main/java/racingcar/enums/Constants.java
@@ -2,16 +2,31 @@ package racingcar.enums;
 
 public enum Constants {
     NAME_LENGTH(5),
-    BLANK_LENGTH(0)
+    BLANK_LENGTH(0),
+    MIN_NUMBER(0),
+    MAX_NUMBER(9),
+    MOVE_NUMBER(4),
+    NAME_SPLITTER(","),
+    FORWARD(1),
+    ZERO(0),
     ;
 
     private int number;
+    private String message;
 
     Constants(int number) {
         this.number = number;
     }
 
+    Constants(String message) {
+        this.message = message;
+    }
+
     public int getNumber() {
         return number;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
