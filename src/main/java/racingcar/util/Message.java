5d8f7a493cd0dc17c6c00e9f790156925e666a49
sudhdkso <waga1009@gmail.com>
2023-10-31 20:22:12 +0900

    refactor: -상수처리해서 출력할 수 있도록 수정

--- src/main/java/racingcar/util/Message.java
+++ src/main/java/racingcar/util/Message.java
@@ -4,9 +4,9 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class Message {
+    private static final String DASH = "-";
     private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void printStartMessage() {
@@ -27,7 +27,7 @@ public class Message {
 
     public static void printMoveResultMessage(List<Car> carList) {
         carList.forEach(x ->
-                System.out.println(x.getName() + " : " + "-".repeat(x.getMoveCount()))
+                System.out.println(x.getName() + " : " + DASH.repeat(x.getPosition()))
         );
         System.out.println();
     }
