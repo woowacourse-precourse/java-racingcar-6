0ed29e94d98361e8e2a7b5d213654865d636304e
watermelon3737 <pshrina1@naver.com>
2023-10-31 19:47:29 +0900

    refactor(ErrorMessage) : 클린 코드 지향하기 위한 줄 바꿈 추가

--- src/main/java/racingcar/utils/ErrorMessage.java
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -2,11 +2,17 @@ package racingcar.utils;
 
 public enum ErrorMessage {
     DELIMITER_ERROR("자동차 이름 구분은 ','로 하셔야합니다"),
+
     NO_NAME_ERROR("레이싱 자동차 이름이 없습니다"),
+
     ALPHA_NAME_ERROR("레이싱 자동차 이름은 영어만 허용됩니다"),
+
     NAME_LENGTH_ERROR("자동차 이름 길이는 5이하 입니다"),
+
     DUPLICATED_NAME_ERROR("중복된 자동차 이름이 있습니다"),
+
     DIGIT_RACE_COUNT_ERROR("숫자만 입력해주시길 바랍니다"),
+
     EMPTY_RACE_COUNT_ERROR("레이스 횟수를 입력하시길 바랍니다");
 
     private String error;
