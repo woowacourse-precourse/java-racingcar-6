d86d817cf5c6e4f441876f34726f32c6c2c55d74
권장순 <jsoonworld@gmail.com>
2023-11-01 00:44:57 +0900

    feat(racingcar) : 0 ~ 9 사이의 값을 반환하는 Forward#randomNums()를 구현함.

---
+++ src/main/java/racingcar/v4/Forward.java
@@ -0,0 +1,9 @@
+package racingcar.v4;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Forward {
+    public int randomNums() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
