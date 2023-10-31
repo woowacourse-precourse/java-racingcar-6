c5e4eb2fcef7064af8abbcbb7dcc8d79ecbf57c8
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 04:17:42 +0900

    style(domain.CarTest) : import문 위치 수정
    static import문은 일반 import문 보다 더 위에 있어야 한다는 코드 컨벤션에 따라 import문 위치 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,8 +1,9 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
