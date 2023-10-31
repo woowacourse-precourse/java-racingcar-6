774beffcf3d4868216d7ace0af3a8bc57978274b
Yoon Jiho <zlhh3842@nate.com>
2023-11-01 00:25:14 +0900

    feat : 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static void checkCarNames(String[] inputNames){
+        for(String name: inputNames) {
+            if(name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] inputs = Console.readLine().split(",");
+        checkCarNames(inputs);      // 예외 처리
     }
 }
