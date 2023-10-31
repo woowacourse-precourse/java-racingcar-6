1b900faba02f48bbec75ab31e1ec935f2834d0e0
ohhyeon2 <dhgus1235@gmail.com>
2023-10-31 11:37:18 +0900

    feat: 특수 문자 예외 처리 및 차량의 이름을 입력 하지 않은 경우 예외 처리

--- src/main/java/racingcar/controller/ExceptionController.java
+++ src/main/java/racingcar/controller/ExceptionController.java
@@ -13,6 +13,14 @@ public class ExceptionController {
         }
     }
 
+    public static void multiCommaException(String[] carNameArr) {
+        for (String userCar : carNameArr) {
+            if (userCar.length() < Constant.MIN_CAR_NAME_SIZE) {
+                throw new IllegalArgumentException("한 글자 이상의 자동차 이름을 입력해주세요. 연속된 ','를 입력할 수 없습니다.");
+            }
+        }
+    }
+
     public static void carNameDuplicateException(HashMap<String, String> carName, String[] carNameArr) {
         if (carName.size() != carNameArr.length) {
             throw new IllegalArgumentException("중복된 이름의 차량이 존재합니다.");
@@ -26,8 +34,8 @@ public class ExceptionController {
     }
 
     public static void tryCountLengthException(String tryCount) {
-        if (tryCount.length() >= 2) {
-            throw new IllegalArgumentException("한 자리의 숫자만 입력 바랍니다.");
+        if (tryCount.length() > Constant.MAX_CRY_COUNT_SIZE) {
+            throw new IllegalArgumentException("한 자리의 숫자만 입력해주세요.");
         }
     }
 
@@ -36,6 +44,13 @@ public class ExceptionController {
             Integer.parseInt(tryCount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 외에 다른 문자는 사용할 수 없습니다.");
+        }
+    }
+
+    public static void specialCharactersException(String carName) {
+        String checkSpecialChar = carName.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9,]", "");
+        if (!checkSpecialChar.equals(carName)) {
+            throw new IllegalArgumentException("한글, 영어, 숫자 외에 다른 특수 문자는 허용하지 않습니다.");
         }
     }
 }
