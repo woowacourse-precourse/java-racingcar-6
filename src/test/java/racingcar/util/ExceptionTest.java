3647e57f272d9964546d85ef686a76f88752dd22
leeminjun <sohwi0305@gmail.com>
2023-10-31 14:35:18 +0900

    Test: 자동차 이름 중복 오류테스트

--- src/test/java/racingcar/util/ExceptionTest.java
+++ src/test/java/racingcar/util/ExceptionTest.java
@@ -18,4 +18,10 @@ public class ExceptionTest {
         assertSimpleTest(() -> assertThatThrownBy(() -> exception.checkCarNamesInput(""))
                 .isInstanceOf(IllegalArgumentException.class));
     }
+
+    @Test
+    void 자동차이름_중복() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> exception.checkCarNamesInput("a,a"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
 }
