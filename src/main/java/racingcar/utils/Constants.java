184cce0b46c6c459908322a7dd93d412fe17cb5e
Jihyeon Park <mmmszip@gmail.com>
2023-11-01 05:08:50 +0900

    feat: Constants 상수 정의

--- src/main/java/racingcar/utils/Constants.java
+++ src/main/java/racingcar/utils/Constants.java
@@ -4,8 +4,10 @@ public class Constants {
 
     public static final int RANDOM_NUMBER_MINIMUM = 1;
     public static final int RANDOM_NUMBER_MAXIMUM = 9;
-    //public static final int MINIMUM_FORWARD_COUNT = 4;
     public static final int MAXIMUM_NAME_LENGTH = 5;
-    public static final String PLAYER_NAME_INPUT_SEPARATOR = ",";
+    public static final String INVALID_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
+    public static final String NAME_LENGTH_EXCEED = "이름의 제한 길이를 초과했습니다.";
+    public static final String DUPLICATE_CAR_NAMES = "중복된 자동차 이름이 존재합니다.";
+    public static final String EMPTY_CAR_NAMES = "1개 이상의 자동차 이름을 입력해주세요.";
 }
 
