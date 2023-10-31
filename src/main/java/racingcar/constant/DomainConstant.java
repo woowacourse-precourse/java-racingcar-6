29b83ef2d4c69cfb21a1ee2b9d967e6cc68ac042
JaehongDev <println@kakao.com>
2023-10-31 21:51:38 +0900

    refactor: DomainConstant 사용하지 않는 메서드 제거

--- src/main/java/racingcar/constant/DomainConstant.java
+++ src/main/java/racingcar/constant/DomainConstant.java
@@ -1,37 +1,16 @@
 package racingcar.constant;
 
-import java.util.function.Consumer;
-import racingcar.exception.DomainExceptionCode;
-
 public enum DomainConstant {
     LAP_MIN_SIZE(0),
     LAP_INCREASE_COUNT(1),
 
     EXTRACT_TRACK_MIN_SIZE(0),
-    EXTRACT_TRACK_DECREASE(1);
-
-
+    EXTRACT_TRACK_DECREASE(1),
+    CAR_NAME_SIZE(5);
     private final int value;
 
     DomainConstant(int value) {
         this.value = value;
-    }
-
-
-    public Consumer<DomainExceptionCode> isLessThan(int other) {
-        return (domainExceptionCode) -> {
-            if (this.value < other) {
-                throw domainExceptionCode.create();
-            }
-        };
-    }
-
-    public Consumer<DomainExceptionCode> isGreaterThan(int other) {
-        return (domainExceptionCode) -> {
-            if (this.value > other) {
-                throw domainExceptionCode.create();
-            }
-        };
     }
 
     public int getValue() {
