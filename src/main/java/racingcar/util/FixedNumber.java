1dcb3a7379bdf37bf3c927b871a3c19517887f05
이병수 <qudtn0315@naver.com>
2023-10-31 13:58:55 +0900

    상수값을 담는 클래스 생성

---
+++ src/main/java/racingcar/util/FixedNumber.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+public class FixedNumber {
+    public static String input = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String trial = "시도할 회수는 몇회인가요?";
+    public static String output = "실행 결과";
+    public static String result = "최종 우승자 :";
+    public static int MAX_NUMBER = 9;
+    public static int MIN_NUMBER = 1;
+    public static int AVAILABLE_MOVE = 4;
+    public static int ASCII_ZERO = 48;
+    public static int ASCII_NINE = 57;
+}
