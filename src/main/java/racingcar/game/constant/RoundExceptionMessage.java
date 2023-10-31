6cffb101d3b5468a1d8f87839fbfb223fdf9de91
황중섭 <jshwang9709@gmail.com>
2023-10-31 15:04:15 +0900

    fix(round): 이동 횟수의 범위 검증

--- src/main/java/racingcar/game/constant/RoundExceptionMessage.java
+++ src/main/java/racingcar/game/constant/RoundExceptionMessage.java
@@ -3,4 +3,6 @@ package racingcar.game.constant;
 public abstract class RoundExceptionMessage {
 
     public static final String NOT_INTEGER = "입력값이 정수가 아닙니다.";
+    public static final String OUT_OF_RANGE = "이동 횟수는 1 이상 100 이하만 가능합니다.";
+
 }
--- src/main/java/racingcar/game/validation/RoundValidator.java
+++ src/main/java/racingcar/game/validation/RoundValidator.java
@@ -1,12 +1,14 @@
 package racingcar.game.validation;
 
 import static racingcar.game.constant.RoundExceptionMessage.NOT_INTEGER;
+import static racingcar.game.constant.RoundExceptionMessage.OUT_OF_RANGE;
 
 public class RoundValidator implements Validator {
 
     @Override
     public void execute(String name) {
         int movingCount = validateInteger(name);
+        validateRange(movingCount);
     }
 
     private int validateInteger(String name) {
@@ -14,6 +16,12 @@ public class RoundValidator implements Validator {
             return Integer.parseInt(name);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_INTEGER, e);
+        }
+    }
+
+    private void validateRange(int movingCount) {
+        if (movingCount < 1 || movingCount > 100) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
         }
     }
 }
