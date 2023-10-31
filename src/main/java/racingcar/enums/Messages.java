d2b1b275645fd693a87c233a5683904abd8703f7
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 19:25:28 +0900

    feat : Messages 생성

--- src/main/java/racingcar/enums/Messages.java
+++ src/main/java/racingcar/enums/Messages.java
@@ -1,4 +1,17 @@
 package racingcar.enums;
 
 public enum Messages {
+    INPUT_RACING_CAR_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_TRY_NUMBER_MESSAGE("시도할 회수는 몇회인가요?"),
+    ;
+
+    private String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
