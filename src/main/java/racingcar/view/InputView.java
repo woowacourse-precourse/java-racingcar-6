9a35ea8ccab0dba4ba05ebbd8d12048e2ceb8b9e
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-01 01:49:01 +0900

    refactor: InputView 내 메소드 기능을 분리 메서드가 한가지 일만 하도록 리팩터링하였음

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,17 +12,42 @@ public class InputView {
     MoveCountsValidation moveCountsValidation = new MoveCountsValidation();
 
     public List<String> getCarNamesInput() {
+        String carInput = receiveCarInput();
+        validateCarInput(carInput);
+        return splitCarInput(carInput);
+    }
+
+    private String receiveCarInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carInput = Console.readLine();
+        return Console.readLine();
+    }
+
+    private void validateCarInput(String carInput) {
         carInputValidation.validateCarInput(carInput);
+    }
+
+    private List<String> splitCarInput(String carInput) {
         String[] carsList = carInput.split(",");
         return Arrays.asList(carsList);
     }
 
     public int getMoveCountsInput() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String MoveCountsInput = Console.readLine();
-        moveCountsValidation.validateMoveCountsInput(MoveCountsInput);
-        return Integer.parseInt(MoveCountsInput);
+        String moveCountsInput = receiveMoveCountsInput();
+        validateMoveCountsInput(moveCountsInput);
+        return convertToInteger(moveCountsInput);
     }
+
+    private String receiveMoveCountsInput() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
+
+    private void validateMoveCountsInput(String moveCountsInput) {
+        moveCountsValidation.validateMoveCountsInput(moveCountsInput);
+    }
+
+    private int convertToInteger(String moveCountsInput) {
+        return Integer.parseInt(moveCountsInput);
+    }
+
 }
