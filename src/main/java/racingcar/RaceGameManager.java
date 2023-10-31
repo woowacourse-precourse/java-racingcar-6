4d6d6fdd7002468f818cbc0f002a7e27bb10d836
Yujin <tkfkd7038@naver.com>
2023-11-01 01:54:29 +0900

    feat(racingcar): 각 차수별 레이싱 실행 결과 출력하는 함수 구현

--- src/main/java/racingcar/RaceGameManager.java
+++ src/main/java/racingcar/RaceGameManager.java
@@ -3,11 +3,22 @@ package racingcar;
 import java.util.ArrayList;
 
 public class RaceGameManager {
+    Car[] car;
     RaceGameManager(Car[] car){
-
+        this.car = car;
     }
     public void noticeRacingResult(String[] carNameList, NumberGenerator Generator) { // 레이싱 실행 결과 출력
-
+        for (int j = 0; j < carNameList.length; j++) {
+            car[j].randomNumber = Generator.createRandomNumber();
+            //System.out.println(car[j].carName + "'s randomNumber : " + car[j].randomNumber);
+            car[j].moveForward(car[j].randomNumber);
+            System.out.print(car[j].carName + " : ");
+            for (int i = 0; i < car[j].forwardCount; i++) {
+                System.out.print('-');
+            }
+            System.out.println();
+        }
+        System.out.println();
 
 
     }
