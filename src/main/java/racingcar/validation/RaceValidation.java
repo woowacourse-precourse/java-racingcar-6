bd12c2ce4cb5a8af7d5dabe5dbb1aca11cb1a5b6
hongsunghak <qmffndnjzm@naver.com>
2023-10-31 16:11:59 +0900

    refactor: validation 기능 구분 및 수정

---
+++ src/main/java/racingcar/validation/RaceValidation.java
@@ -0,0 +1,27 @@
+package racingcar.validation;
+
+public class RaceValidation {
+    public void validationSeparator(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("자동차 이름은 쉼표(,)로 구분해 주세요.");
+        }
+    }
+    public void validationNameLength(String input) {
+        if (input.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하민 가능합니다.");
+        }
+    }
+
+    public void validationMoveTrailCount(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            findDigit(input, i);
+        }
+    }
+
+    private void findDigit(String input, int i) {
+        if (!Character.isDigit(input.charAt(i))) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+}
