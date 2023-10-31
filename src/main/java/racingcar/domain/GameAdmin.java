c4e80c35af43cc8851c62906c2851215cd361e68
정영동 <63975200+Yeongdong@users.noreply.github.com>
2023-10-31 16:24:55 +0900

    refactor: ResultView 멤버 삭제, Round 멤버 추가

--- src/main/java/racingcar/domain/GameAdmin.java
+++ src/main/java/racingcar/domain/GameAdmin.java
@@ -1,30 +1,28 @@
 package racingcar.domain;
 
-import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
 public class GameAdmin {
-    private final ResultView resultView;
+    private final Round round;
 
-    private GameAdmin(ResultView resultView) {
-        this.resultView = resultView;
+    private GameAdmin(Round round) {
+        this.round = round;
     }
 
-    public static GameAdmin from(ResultView resultView) {
-        return new GameAdmin(resultView);
+    public static GameAdmin from(Round round) {
+        return new GameAdmin(round);
     }
 
     public void startGame(Cars cars, MovableStrategy movableStrategy) {
-        Round round = Round.getRound(InputView.inputRound());
-        resultView.printShowResult();
+        ResultView.printShowResult();
         for (int i = 0; i < round.getRound(); i++) {
             cars.startRace(movableStrategy);
-            resultView.showRoundResult(cars);
+            ResultView.showRoundResult(cars);
         }
     }
 
     public void presentWinner(Cars cars) {
         Winners winners = Winners.from(cars);
-        resultView.callWinner(winners.getWinnerName());
+        ResultView.callWinner(winners.getWinnerName());
     }
 }
