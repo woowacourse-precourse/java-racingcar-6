b1a5218f20049f3540cf9b689b07d7fcc8870b11
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-01 01:10:35 +0900

    Feat : 사용자가 입력한 값을 ','로 구분하여 자동차의 목록을 만든다
    - List<String> 타입으로 변환한 후, stream을 이용하여 List<Car> 객체로 변환한다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 입력 받기
+        String userInput = InputView.requestUserInputNames();
+
+        // 이름 리스트 만들기 [name, name, name, ...]
+        List<String> names = List.of(userInput.split(","));
+
+        // 돌아가면서 Car 객체 생성하여 List<Car>로 변환
+        List<Car> cars = names.stream()
+                .map(name -> new Car(name, 0))
+                .collect(Collectors.toList());
     }
 }
