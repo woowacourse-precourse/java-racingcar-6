ec5f605f6f9509bfdd658ef5c937c6ba06035904
Juho <jeuslee99@naver.com>
2023-11-01 04:02:13 +0900

    feat:add StartRacing, and error handling by try_catch

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.lang.IllegalArgumentException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Application {
@@ -14,43 +13,77 @@ public class Application {
         String[] CarArray = carNum.split(",");
 
         //CarArray의 입력 check
-        CheckCarArray(CarArray);
+        try {
+            CheckCarArray(CarArray);
+        }
+        catch(IllegalArgumentException e) {
+            System.exit(0);
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
         String racingStr = Console.readLine();
         int racingNum = Integer.parseInt(racingStr);
 
         //레이싱 시작
+        System.out.println();
         StartRacing(CarArray, racingNum);
     }
+
     public static void CheckCarArray(String[] CarArray) {
         for(String s: CarArray) {
             if(s.length() > 5) {
-                throw new IllegalArgumentException("유효하지 않은 입력합니다.");
+                throw new IllegalArgumentException("유효하지 않은 입력입니다.");
             }
         }
     }
 
     public static void StartRacing(String[] CarArray, int racingNum) {
-        ArrayList<Integer> forwardDistance = new ArrayList<>();
+        ArrayList<Integer> ForwardDistance = new ArrayList<>();
 
         //차의 개수만큼 진행도 배열에 개수 추가
         for(String s: CarArray) {
-            forwardDistance.add(0);
+            ForwardDistance.add(0);
         }
 
         System.out.println("실행 결과");
         for(int i=0; i< racingNum; i++) {
             //각 사람마다 랜덤함수 돌리고 조건에 따라 전진
-            GoOrStop(forwardDistance);
+            GoOrStop(ForwardDistance);
 
             //차수별 실행 결과 출력
-            PrintIntermediateResult(CarArray, forwardDistance);
+            PrintIntermediateResult(CarArray, ForwardDistance);
             System.out.println();
         }
 
+        //우승자 출력
+        FindWinner(CarArray, ForwardDistance);
+    }
 
-        //다 돌았으니까 우승자 파악
+    public static void FindWinner(String[] CarName, ArrayList<Integer> distanceArray) {
+        //최댓값 판별
+        int max = -1;
+        for(int d: distanceArray) {
+            if(d > max) {
+                max = d;
+            }
+        }
+
+        //우승자 출력
+        ArrayList<String> Winner = new ArrayList<>();
+        System.out.print("최종 우승자 : ");
+
+        for(int i=0; i<CarName.length; i++) {
+            if(distanceArray.get(i) == max) {
+                Winner.add(CarName[i]);
+            }
+        }
+
+        for(int i=0; i<Winner.size(); i++) {
+            if(i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(Winner.get(i));
+        }
     }
 
     public static void PrintIntermediateResult(String[] CarName,ArrayList<Integer> distanceArray) {
@@ -74,6 +107,4 @@ public class Application {
             }
         }
     }
-
-
 }
