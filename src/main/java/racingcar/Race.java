9d820933f42ddf145ae2d148de962f7bbec1bdc6
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-31 23:57:08 +0900

    feat(Race): Implement Race class for racing managenets with basic attributes and constructor

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    List<RaceCar> raceCarList;
+
+    public Race(){
+     raceCarList = new ArrayList<>();
+    }
+}
