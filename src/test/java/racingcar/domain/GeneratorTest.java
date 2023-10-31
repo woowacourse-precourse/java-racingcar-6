4967cb1bc2715e41ee67490d05057d9161af9ce3
JeonJW24 <tyui153@naver.com>
2023-11-01 02:05:15 +0900

    test(racingcar): 난수 개수 테스트

---
+++ src/test/java/racingcar/domain/GeneratorTest.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class GeneratorTest {
+    private static final int CAR_NUMBER = 3;
+
+    @Test
+    void 난수_개수() {
+        Generator generator = new Generator();
+        List<Integer> result = generator.createRandomNumbers(CAR_NUMBER);
+        assertThat(result.size()).isEqualTo(CAR_NUMBER);
+    }
+}
