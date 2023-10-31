70f85b3adcd383c439f7f7f07d12ea7d0a4ece88
FutureCircuit <137764369+FutureCircuit@users.noreply.github.com>
2023-10-31 18:49:53 +0900

    feat: 자동차 이름과 이동 거리를 저장하는 기능 추가 경주할 자동차의 이름과 이동 거리 저장 및 예외 처리 기능을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,11 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] temp = Console.readLine().split(",");
         carNames = new ArrayList<>(Arrays.asList(temp));
+
+        for(String str : carNames) {
+            if(str.length() >= 6) throw new IllegalArgumentException();
+            cars.put(str, 0);
+        }
     }
 
 
