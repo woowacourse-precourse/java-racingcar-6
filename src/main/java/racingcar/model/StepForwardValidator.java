8922cf949df451025a2aea0c181a33de1d29698c
ikjo39 <chi4321@naver.com>
2023-10-31 16:03:18 +0900

    feat: 경주 자동차의 전진을 검증하는 로직 추가
    - 무작위 숫자를 생성하는 로직 추가
    - 무작위 숫자의 전진 여부를 결정

---
+++ src/main/java/racingcar/model/StepForwardValidator.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+import static racingcar.constant.RaceConfig.LEAST_VALUE_TO_STEP_FORWARD;
+import static racingcar.constant.RaceConfig.MAXIMUM_RANDOM_NUMBER;
+import static racingcar.constant.RaceConfig.MINIMUM_RANDOM_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class StepForwardValidator {
+
+    private StepForwardValidator() {
+    }
+
+    public static boolean isCarStepForward() {
+        return generateRandomNumbers() >= LEAST_VALUE_TO_STEP_FORWARD;
+    }
+
+    private static int generateRandomNumbers() {
+        return Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+    }
+}
