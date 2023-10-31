6fb2b9d53be28d05a960f383535db20aa847ea4b
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-31 16:20:10 +0900

    fix: 길이초과 메시지 오류 수정
    - 5글자 보다 작아야 -> 5글자 이하 여야

--- src/main/java/racingcar/utils/enums/ErrorMessage.java
+++ src/main/java/racingcar/utils/enums/ErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.utils.enums;
 
 public enum ErrorMessage {
     EMPTY_NAME_ERROR("빈 문자열을 입력할 수 없습니다."),
-    OVER_THRESHOLD_ERROR("자동차의 이름은" + Constraints.MAX_SIZE.getValue() + "보다 작아야 합니다."),
+    OVER_THRESHOLD_ERROR("자동차의 이름은 " + Constraints.MAX_SIZE.getValue() + "글자 이하 여야 합니다."),
     NAME_DUPLICATE_ERROR("중복된 자동차 이름이 존재 합니다."),
     NOT_INTEGER_MESSAGE("정수 값을 입력 해야 합니다."),
     NOT_POSITIVE_NUMBER_MESSAGE("시도 회수는 1 이상 이어야 합니다.");
