6c882a150e84559c8c1f0c317a58a44290ed2135
2uwls <2uwls1126@gmail.com>
2023-10-31 13:32:09 +0900

    feat: create saveTrialNumber

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,13 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        createCars(Console.readLine());
+        createCars(Console.readLine().trim());
+        System.out.println("시도할 회수는 몇회인가요?");
+        saveTrialNumber(Console.readLine().trim());
+
+
+
+
 
     }
-
     private static void createCars(String carNames) {
         String[] carList = carNames.split(",");
         Integer carListLength = carList.length;
     }
+    private static void saveTrialNumber(String trialNumberInput) {
+        Integer trialNumber = Integer.valueOf(trialNumberInput);
+    }
+
 
 }
