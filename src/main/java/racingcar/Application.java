f98ee43cdc96d01193f6e9e68476859bf3ea6a2f
KwangHo <skh990427@naver.com>
2023-10-31 21:46:16 +0900

    feat: 자동차 이름 입력 및 리스트에 자동차 객체 저장 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Car> carList = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        StringTokenizer token = new StringTokenizer(Console.readLine(), ",");
+
+        while (token.hasMoreTokens()) {
+            carList.add(new Car(token.nextToken()));
+        }
+
     }
 }
