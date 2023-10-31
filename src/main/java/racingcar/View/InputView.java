83eb27d222c575949b66270482f013795bac9053
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 22:16:34 +0900

    refactor(InputView): input기능만 수행하게 수정
    input기능에 질문과 validate 메서드를 추가하여 입력 외에 다른 기능을 수행하지 못하게 수정하였습니다.

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -8,20 +8,15 @@ public class InputView {
     Validator validator = new Validator();
 
     public String inputCarString() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
 
         return userInput;
     }
 
-    public int inputTryCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+    public String inputTryCount() {
         String userInput = Console.readLine();
 
-        validator.validateIsNumber(userInput);
-        int number = validator.parseStringToInteger(userInput);
-
-        return number;
+        return userInput;
     }
 
 
