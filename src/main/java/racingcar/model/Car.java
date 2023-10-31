769b9ba4ae30341cac0fe9eb49d748ceccb201b7
IE-MangChi <hsm983278@gmail.com>
2023-10-31 16:56:30 +0900

    feat: 최대전진찾기 기능, 우승자 찾기 기능 추가 refactor: 전진기능 리팩토링

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,58 +1,45 @@
 package racingcar.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import racingcar.util.CarUtil;
+
+import java.util.*;
 
 public class Car {
 
-    private static final int CAR_START_POSITION = 0;
     private static final int CAR_MOVE_STEP = 1;
 
-    private List<String> carName = new ArrayList<>();
-    private List<Integer> carPosition = new ArrayList<>();
+    private Map<String, Integer> carData = new LinkedHashMap<>();
+
+    public Map<String, Integer> getCarData() {
+        return this.carData;
+    }
 
     public void initCar(List<String> carlist) {
         for (String element : carlist) {
-            this.carName.add(element);
-            this.carPosition.add(CAR_START_POSITION);
+            this.carData.put(element, 0);
         }
     }
 
-    public List<String> getCarName() {
-        return this.carName;
-    }
-
-    public List<Integer> getCarPosition() {
-        return this.carPosition;
-    }
-
-    public void carMove(int move) {
-        this.carPosition.set(move, this.carPosition.get(move) + CAR_MOVE_STEP);
+    public void move() {
+        carData.forEach((key, value) -> {
+            if (CarUtil.isCanMove()) {
+                carData.replace(key, value + CAR_MOVE_STEP);
+            }
+        });
     }
 
     public int findMaxPosition() {
-        int maxPosition = 0;
-
-        for (Integer carPosition : carPosition) {
-            if (carPosition > maxPosition) {
-                maxPosition = carPosition;
-            }
-        }
-        return maxPosition;
+        return carData.values()
+                .stream().mapToInt(v -> v).max()
+                .orElseThrow(NoSuchElementException::new);
     }
 
-    public List<Integer> findWinnerIndex(int maxPosition) {
-        List<Integer> result = new ArrayList<>();
+    public List<String> findWinner(int maxPosition) {
+        List<String> result = new ArrayList<>();
 
-        for (int i=0; i<this.carPosition.size(); i++) {
-            if (this.carPosition.get(i) == maxPosition) {
-                result.add(i);
-            }
-        }
+        carData.entrySet().stream()
+                .filter(car -> car.getValue() == maxPosition)
+                .forEach(car -> result.add(car.getKey()));
         return result;
-    }
-
-    public int getCarSize() {
-        return this.carPosition.size();
     }
 }
