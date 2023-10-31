44012f71d98a27eb7b35014e3a37688f756b3bae
ay-eonii <naayen@naver.com>
2023-11-01 00:31:40 +0900

    test: 우승자를 판별하는 단위테스트 작성

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -1,8 +1,14 @@
 package racingcar.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RefereeTest {
 
@@ -14,5 +20,31 @@ class RefereeTest {
         assertThat(referee.isGreaterThanEqual(8)).isTrue();
 
         assertThat(referee.isGreaterThanEqual(2)).isFalse();
+    }
+
+    @Test
+    public void 우승자를_판별한다() {
+        List<Car> carList = Arrays.asList(
+                mock(Car.class),
+                mock(Car.class),
+                mock(Car.class)
+        );
+
+        when(carList.get(0).getName()).thenReturn("Car1");
+        when(carList.get(0).getLocation()).thenReturn(3);
+
+        when(carList.get(1).getName()).thenReturn("Car2");
+        when(carList.get(1).getLocation()).thenReturn(3);
+
+        when(carList.get(2).getName()).thenReturn("Car3");
+        when(carList.get(2).getLocation()).thenReturn(2);
+
+        Referee referee = mock(Referee.class);
+        when(referee.judgeWinner(carList)).thenCallRealMethod();
+
+        List<String> result = referee.judgeWinner(carList);
+        List<String> expected = List.of("Car1", "Car2");
+
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
