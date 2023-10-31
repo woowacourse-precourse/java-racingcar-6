c04b790d608184cf024f9cdfa314d0d677d147cc
gilik154 <okk080200@gmail.com>
2023-10-31 15:05:31 +0900

    Fix : Conveter의 숫자 변경 테스트 코드 작성

--- src/test/java/racingcar/controller/ConverterTest.java
+++ src/test/java/racingcar/controller/ConverterTest.java
@@ -2,6 +2,8 @@ package racingcar.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.car.Cars;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,5 +55,21 @@ class ConverterTest {
         String names = "";
 
         assertThrows(IllegalArgumentException.class, () -> Converter.stringToCar(names));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "9", "100"})
+    @DisplayName("문자열을 숫자로 정상적적으로 변경해주어야 한다.")
+    void String을_Integer로_변환(String value) {
+        int trial = Converter.stringToTrial(value);
+
+        assertEquals(Integer.parseInt(value), trial);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "a", "cde"})
+    @DisplayName("숫자가 아니면 오류 발생")
+    void String이_숫자가_아니면_익셉션_발생(String value) {
+        assertThrows(IllegalArgumentException.class, () -> Converter.stringToTrial(value));
     }
 }
