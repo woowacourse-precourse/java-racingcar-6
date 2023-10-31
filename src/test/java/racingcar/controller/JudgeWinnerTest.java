2962a567a6cadfcc2a18b71ec20f92039b1f9ed7
dongho <dons97@naver.com>
2023-10-31 16:32:59 +0900

    test: Add unit test for JudgeWinner()

---
+++ src/test/java/racingcar/controller/JudgeWinnerTest.java
@@ -0,0 +1,57 @@
+package racingcar.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JudgeWinnerTest {
+
+    @Test
+    void judge1WinnerTest() {
+        LinkedHashMap<String, Integer> userCarDistance = new LinkedHashMap<String, Integer>();
+        userCarDistance.put("poby", 20);
+        userCarDistance.put("woni", 10);
+        userCarDistance.put("jun", 10);
+
+        List<String> expectedWinners = new ArrayList<>();
+        expectedWinners.add("poby");
+
+        List<String> winners = JudgeWinner.judgeWinner(userCarDistance);
+        Assertions.assertThat(expectedWinners).isEqualTo(winners);
+    }
+
+    @Test
+    void judge2WinnerTest() {
+        LinkedHashMap<String, Integer> userCarDistance = new LinkedHashMap<String, Integer>();
+        userCarDistance.put("poby", 20);
+        userCarDistance.put("woni", 10);
+        userCarDistance.put("jun", 20);
+
+        List<String> expectedWinners = new ArrayList<>();
+        expectedWinners.add("poby");
+        expectedWinners.add("jun");
+
+        List<String> winners = JudgeWinner.judgeWinner(userCarDistance);
+        Assertions.assertThat(expectedWinners).isEqualTo(winners);
+    }
+
+    @Test
+    void judge3WinnerTest() {
+        LinkedHashMap<String, Integer> userCarDistance = new LinkedHashMap<String, Integer>();
+        userCarDistance.put("poby", 20);
+        userCarDistance.put("woni", 20);
+        userCarDistance.put("jun", 20);
+
+        List<String> expectedWinners = new ArrayList<>();
+        expectedWinners.add("poby");
+        expectedWinners.add("woni");
+        expectedWinners.add("jun");
+
+        List<String> winners = JudgeWinner.judgeWinner(userCarDistance);
+        Assertions.assertThat(expectedWinners).isEqualTo(winners);
+    }
+}
