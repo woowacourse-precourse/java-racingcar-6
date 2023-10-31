19cc6261d7558f03a31abffb630d58af8b4efcb1
InJun2 <os2864@naver.com>
2023-10-31 21:51:31 +0900

    :art: 생성자를 통한 Car 객체들 생성으로 변경

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,12 +1,20 @@
 package racingcar.model;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
-    List<Car> cars;
+    private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
-        this.cars = cars;
+    public Cars(String[] carNames) {
+        this.cars = createCars(carNames);
+    }
+
+    private List<Car> createCars(String[] carNames) {
+        return Arrays.stream(carNames)
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
