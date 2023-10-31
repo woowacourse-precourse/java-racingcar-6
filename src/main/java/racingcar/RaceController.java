8f1d4153e2a6fe9af63b0610e699c595786fb018
K-KY <kyuyoungk@naver.com>
2023-11-01 05:42:18 +0900

    Feat(RaceController) : 컨트롤러 추가

---
+++ src/main/java/racingcar/RaceController.java
@@ -0,0 +1,53 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class RaceController {
+    private final LinkedHashMap<Integer, List<String>> ranks = new LinkedHashMap<>();
+
+    private Cars cars;
+
+    public void start() {
+        List<String> names = inputCarNames();
+        List<Car> carNames = DataTypeChanger.stringToCar(names);
+        cars = new Cars(carNames);
+
+        tryMove(cars);
+
+        calculateRanking(names, carNames);
+        SystemOutput.printWinner(ranks.get(0));
+    }
+
+    private List<String> inputCarNames() {
+        SystemOutput.printInputCarNames();
+        List<String> names = UserInput.carName();
+        return names;
+    }
+
+    private void tryMove(Cars cars) {
+        int trial = inputTrialNumber();
+        SystemOutput.printThisIsResult();
+
+        for (int i = 0; i < trial; i++) {
+            cars.move();
+            SystemOutput.printResult(cars.indicate());
+        }
+    }
+
+    private int inputTrialNumber() {
+        SystemOutput.printInputTrialCount();
+        int trial = UserInput.trialCount();
+        return trial;
+    }
+
+    private void calculateRanking(List<String> names, List<Car> carNames) {
+        for (int i = 0; i < carNames.size(); i++) {
+            int compare = cars.compare(carNames.get(i));
+            List<String> rank = ranks.getOrDefault(compare, new ArrayList<>());
+            rank.add(names.get(i));
+            ranks.put(compare, rank);
+        }
+    }
+}
