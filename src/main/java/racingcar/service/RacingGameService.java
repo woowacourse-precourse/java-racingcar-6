bbfe749ee7ba77e18ab744e420ce3af0936f3291
june-777 <wlwhswnsrl96@gmail.com>
2023-10-31 18:37:37 +0900

    refactor(RacingGameService): final 키워드 추가

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -8,7 +8,7 @@ import racingcar.service.domain.RacingCars;
 public class RacingGameService {
 
     public OneGameResultsDto playOneTimeCarRace(RacingCars cars) {
-        List<Car> endCars = cars.doRace();
+        final List<Car> endCars = cars.doRace();
         return new OneGameResultsDto(endCars);
     }
 
