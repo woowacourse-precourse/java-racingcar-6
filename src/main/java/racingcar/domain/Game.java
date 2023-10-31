6fff8aa4e4c64bdab0e5bdbd8aa284659d033e1b
bonooy <choiboyuna@gmail.com>
2023-10-31 12:53:42 +0900

    Feat: Game domain 추가 - 자동차 리스트 반환, 시도 횟수 증가

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Game {
+    private final List<Car> cars;
+    private final int trial;
+    private int trialNumber = 0;
+
+    public Game(List<Car> cars, int trial) {
+        this.cars = cars;
+        this.trial = trial;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void increaseTrialNumber() {
+        trialNumber += 1;
+    }
+
+}
