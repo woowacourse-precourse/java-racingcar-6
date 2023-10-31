5e898ef1a68f65a7fd0d0232168823e0f30dbb62
zjacom <eephus625@icloud.com>
2023-10-31 22:49:22 +0900

    style(racingCar): 사용하지 않는 라이브러리 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Application {
     public static void main(String[] args) {
         GameUI gameUI = new GameUI();
