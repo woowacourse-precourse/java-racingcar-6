e9818e92295d66b033b2454e688e158a2d1d649e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-31 13:04:01 +0900

    refactor: isCarDuplicated 내부 로직 한줄로 변경

--- src/main/java/racingcar/utils/CarListValidator.java
+++ src/main/java/racingcar/utils/CarListValidator.java
@@ -2,23 +2,21 @@ package racingcar.utils;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import racingcar.model.Car;
 
 public class CarListValidator {
 
-    public static void validate(List<Car> carList){
-        if(isCarAlone(carList) || isCarDuplicated(carList)){
+    public static void validate(List<Car> carList) {
+        if (isCarAlone(carList) || isCarDuplicated(carList)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static boolean isCarAlone(List<Car> carList){
+    private static boolean isCarAlone(List<Car> carList) {
         return carList.size() <= 1;
     }
 
-    private static boolean isCarDuplicated(List<Car> carList){
-        Set<Car> cars = new HashSet<>(carList);
-        return cars.size() == carList.size();
+    private static boolean isCarDuplicated(List<Car> carList) {
+        return new HashSet<>(carList).size() == carList.size();
     }
 }
