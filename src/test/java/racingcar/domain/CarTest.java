307d00fc4906d5e9524c1211b51bd893a244d6b3
liarreez <liarreez@gmail.com>
2023-10-31 16:45:22 +0900

    refactor : 중복을 제거하고 자동차 이름을 검증하는 테스트를 CarValidatorTest로 이동
    tiger 차량을 생성하는 생성자가 중복되어 BeforeEach를 사용하여 중복을 제거하고 CarValidator 클래스가 생김에 따라 자동차 이름 검증 테스트는 CarValidatorTest로 이동

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,24 +2,27 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
-    @Test
-    @DisplayName("5글자 초과의 이름의 자동차 생성 시 에러 발생 테스트")
-    public void testCarNameGreaterThanFive() {
-        String name = "superman";
+    Car car;
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car(name));
+    @BeforeEach
+    void setUp() {
+        car = new Car("tiger");
+    }
+
+    @Test
+    @DisplayName("자동차 이름 반환 테스트")
+    void testGetCarName() {
+        assertThat(car.getName()).isEqualTo("tiger");
     }
 
     @Test
     @DisplayName("파라미터가 4미만이면 이동하지 않는지 테스트")
-    public void testCarDontMoveParameterIsLessThanFour() {
-        Car car = new Car("leon");
-
+    void testCarDontMoveParameterIsLessThanFour() {
         car.move(3);
 
         assertThat(car.getPosition()).isEqualTo(0);
@@ -27,9 +30,7 @@ public class CarTest {
 
     @Test
     @DisplayName("파라미터가 4이상이면 한칸 이동 테스트")
-    public void testCarMoveOneDistanceParameterIsGREATERThanOrEqualFour() {
-        Car car = new Car("leon");
-
+    void testCarMoveOneDistanceParameterIsGREATERThanOrEqualFour() {
         car.move(6);
 
         assertThat(car.getPosition()).isEqualTo(1);
