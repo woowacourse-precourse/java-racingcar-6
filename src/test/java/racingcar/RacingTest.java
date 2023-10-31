38b9adaeddb4f4786b4d9d223734daa4dfc39802
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 22:51:09 +0900

    [Test](RacingTest) - TestCode 변경

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,4 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
-class RacingTest {
-  
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RacingTest {
+
+    private Racing racing;
+    private int round;
+
+    @BeforeEach
+    void setUp() {
+        String[] racerNameArray = {"pobi", "MJ", "tjwls"};
+        round = 3;
+        racing = new Racing(racerNameArray, round);
+    }
+
+    @Test
+    void testCarRacingGameStart() {
+        racing.carRacingGameStart();
+    }
 }
+
+
+
+
