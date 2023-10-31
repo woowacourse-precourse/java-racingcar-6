510b4ace3681d3e4e5c68839880569d2d40fbfa7
donghoonyeom <dua5410773@naver.com>
2023-10-31 18:38:41 +0900

    refactor(Application): 입력값 처리 메서드를 ConsoleView에 위임
    Application 클래스에서 자동차 이름과 시도 횟수를 받아오는 부분을 ConsoleView 클래스의 새로운 메서드에 위임하도록 수정했습니다. 이렇게 함으로써 Application 클래스는 더 간결해지고, 코드의 역할 분리가 더 명확해졌습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,20 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
 import racingcar.controller.GameController;
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
 import racingcar.view.ConsoleView;
 
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
         try {
             ConsoleView consoleView = new ConsoleView();
-            List<String> carNames = getCarNames(consoleView);
+            List<String> carNames = consoleView.getInputCarNames();
             List<Car> cars = Car.createCars(carNames);
-            int numberOfAttempts = getNumberOfAttempts(consoleView);
+            int numberOfAttempts = consoleView.getInputNumberOfAttempts();
             consoleView.printMessage("");
 
             RacingGame racingGame = new RacingGame(cars);
@@ -24,16 +24,5 @@ public class Application {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
         }
-    }
-
-    private static List<String> getCarNames(ConsoleView consoleView) {
-        consoleView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNamesArray = Console.readLine().split(",");
-        return Arrays.asList(carNamesArray);
-    }
-
-    private static int getNumberOfAttempts(ConsoleView consoleView) {
-        consoleView.printMessage("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
     }
 }
