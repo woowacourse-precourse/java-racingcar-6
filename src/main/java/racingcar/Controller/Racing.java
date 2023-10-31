08fd4b3b7020054b31f79fe6ca333e859cd5f73f
hahah-a-ha <smha01@hanyang.ac.kr>
2023-10-31 23:23:34 +0900

    refactor: racing 함수 분리

--- src/main/java/racingcar/Controller/Racing.java
+++ src/main/java/racingcar/Controller/Racing.java
@@ -11,16 +11,26 @@ public class Racing {
         Map<String,String> result=new HashMap<>();
         Output output=new Output();
         for(int i=0; i<repeatTime; i++){
-            for(String car: carLists){
-                //addAccumulateGame(result,player);
-                String forwardValue = output.saveForward(output.canForward());
-                result.put(car, result.getOrDefault(car, "") + forwardValue);
-            }
+//            for(String car: carLists){
+//                //addAccumulateGame(result,player);
+//                String forwardValue = output.saveForward(output.canForward());
+//                result.put(car, result.getOrDefault(car, "") + forwardValue);
+//            }
+            result=playGame(result,carLists);
             output.printRaceRound(result);
 
         }
         return result;
     }
+    public Map<String,String> playGame(Map<String,String> result,List<String> carLists){
+        Output output = new Output();
+        for(String car:carLists){
+            String forwardValue=output.saveForward(output.canForward());
+            result.put(car,result.getOrDefault(car,"")+forwardValue);
+        }
+        return result;
+    }
+
     public void addAccumulateGame(Map<String,String> result, String car){
         Output output=new Output();
         if (result.containsKey(car)) {
