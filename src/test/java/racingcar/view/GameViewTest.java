dcccd7fdd9b5557f1d27f8cca626e0fe072f5f2e
jihwankim128 <jihwan1535@nate.com>
2023-11-01 05:02:39 +0900

    test : 사용자 시도횟수 입력 검증 테스트

--- src/test/java/racingcar/view/GameViewTest.java
+++ src/test/java/racingcar/view/GameViewTest.java
@@ -1,15 +1,24 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
 
-public class GameViewTest extends NsTest {
+public class GameViewTest {
+    private InputStream inputStream = System.in;
+
+    @BeforeEach
+    void init() {
+        Console.close();
+    }
+
     GameView gameView = new GameView();
     @DisplayName("자동차 이름 입력 검증 테스트")
     @Test
@@ -19,8 +28,19 @@ public class GameViewTest extends NsTest {
         assertThat(carNames).contains("ab", "cd", "ef");
     }
 
-    @Override
-    protected void runMain() {
-        Application.main(new String[]{});
+    @DisplayName("시도할 횟수 검증 테스트1")
+    @Test
+    void 시도횟수_입력_검증1() {
+        System.setIn(new ByteArrayInputStream("0".getBytes()));
+        assertThatThrownBy(() -> gameView.getTotalRounds())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("시도할 횟수 검증 테스트2")
+    @Test
+    void 시도횟수_입력_검증2() {
+        System.setIn(new ByteArrayInputStream("ab".getBytes()));
+        assertThatThrownBy(() -> gameView.getTotalRounds())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
