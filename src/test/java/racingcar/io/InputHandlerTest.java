0ee47c7464436bde960e764251089575ddefef32
tfer2442 <tfer2442@gmail.com>
2023-11-01 04:19:54 +0900

    test(InputHandlerTest): 테스트 코드 분리

--- src/test/java/racingcar/io/InputHandlerTest.java
+++ src/test/java/racingcar/io/InputHandlerTest.java
@@ -7,7 +7,6 @@ import static racingcar.constant.ExceptionMessage.INVALID_TRIAL_NUMBER;
 import static racingcar.constant.ExceptionMessage.LENGTH_OUT_OF_RANGE_CAR_NAME;
 import static racingcar.constant.ExceptionMessage.NOT_ALLOWED_WORDS_CAR_NAME;
 
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -48,39 +47,65 @@ class InputHandlerTest {
     }
 
     @Test
-    void convertCarNames_이름_형식_오류_예외처리() {
+    void convertCarNames_특수문자_이름_형식_오류_예외처리() {
         String carNamesWithAt = "pobi,wo@ni,jun";
-        String carNamesWithSpace = "pobi, woni, jun";
 
-        for (String carNames : Arrays.asList(carNamesWithAt, carNamesWithSpace)) {
-            assertThatThrownBy(() -> InputHandler.convertCarNames(carNames))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(NOT_ALLOWED_WORDS_CAR_NAME);
-        }
+        assertThatThrownBy(() -> InputHandler.convertCarNames(carNamesWithAt))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_ALLOWED_WORDS_CAR_NAME);
     }
 
     @Test
-    void convertTrialNumber_정상_입력() {
+    void convertCarNames_공백_이름_형식_오류_예외처리() {
+        String carNamesWithSpace = "pobi, woni, jun";
+
+        assertThatThrownBy(() -> InputHandler.convertCarNames(carNamesWithSpace))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_ALLOWED_WORDS_CAR_NAME);
+    }
+
+    @Test
+    void convertTrialNumber_한자리수_정상_입력() {
         String trialNumOfSingleDigit = "5";
-        String trialNumOfTwoDigit = "10";
 
         int resultOfSingleDigit = InputHandler.convertTrialNumber(trialNumOfSingleDigit);
-        int resultOfTwoDigit =  InputHandler.convertTrialNumber(trialNumOfTwoDigit);
 
         assertThat(resultOfSingleDigit).isEqualTo(5);
+    }
+
+    @Test
+    void convertTrialNumber_두자리수_정상_입력() {
+        String trialNumOfTwoDigit = "10";
+
+        int resultOfTwoDigit =  InputHandler.convertTrialNumber(trialNumOfTwoDigit);
+
         assertThat(resultOfTwoDigit).isEqualTo(10);
     }
 
     @Test
-    void convertTrialNumber_입력_예외처리() {
+    void convertTrialNumber_0_입력_예외처리() {
         String trialNumZero = "0";
+
+        assertThatThrownBy(() -> InputHandler.convertTrialNumber(trialNumZero))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_TRIAL_NUMBER);
+    }
+
+    @Test
+    void convertTrialNumber_음수_입력_예외처리() {
         String trialNumNegative = "-1";
+
+        assertThatThrownBy(() -> InputHandler.convertTrialNumber(trialNumNegative))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_TRIAL_NUMBER);
+    }
+
+    @Test
+    void convertTrialNumber_문자_입력_예외처리() {
         String trialNumChar = "a";
 
-        for (String trialNum : Arrays.asList(trialNumZero, trialNumNegative, trialNumChar)) {
-            assertThatThrownBy(() -> InputHandler.convertTrialNumber(trialNum))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(INVALID_TRIAL_NUMBER);
-        }
+        assertThatThrownBy(() -> InputHandler.convertTrialNumber(trialNumChar))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_TRIAL_NUMBER);
     }
 }
