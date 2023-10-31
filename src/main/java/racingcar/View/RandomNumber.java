fbe969d2458e65fbf6bc333b7dd798b28a1f3eed
hahah-a-ha <smha01@hanyang.ac.kr>
2023-10-31 23:49:21 +0900

    refactor: static 제거

--- src/main/java/racingcar/View/RandomNumber.java
+++ src/main/java/racingcar/View/RandomNumber.java
@@ -3,7 +3,7 @@ package racingcar.View;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomNumber {
-    public static int makeRandom() {
+    public int makeRandom() {
         return pickNumberInRange(0, 9);
     }
 }
