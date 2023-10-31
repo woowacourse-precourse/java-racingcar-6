f6e46c082a168e8b71039003b56a922b730a42fc
jinsu868 <mjinsu098@gmail.com>
2023-10-31 13:39:38 +0900

    feat: errorCode, 메시지 추가

---
+++ src/main/java/racingcar/error/ErrorCode.java
@@ -0,0 +1,15 @@
+package racingcar.error;
+
+public enum ErrorCode {
+    EXCEED_LENGTH("이름 길이를 초과했습니다."),
+    INVALID_TRY_COUNT("시도 횟수에 문자가 들어왔습니다");
+
+    private String message;
+    private ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
