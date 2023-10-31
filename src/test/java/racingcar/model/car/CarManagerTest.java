5b933717640ca3d6d37e1963868e4cbb07ac844b
상원 최 <swchoi1@swm.ai>
2023-11-01 00:11:26 +0900

    test(테스트 추가) : 최종 우승 차량 정보 테스트

--- src/test/java/racingcar/model/car/CarManagerTest.java
+++ src/test/java/racingcar/model/car/CarManagerTest.java
@@ -1,4 +1,4 @@
-package racingcar.model;
+package racingcar.model.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -6,8 +6,6 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.model.car.CarManager;
-import racingcar.model.car.RacingCar;
 
 @DisplayName("[Car Manager]")
 class CarManagerTest {
@@ -32,9 +30,31 @@ class CarManagerTest {
         Assertions.assertDoesNotThrow(
                 () -> new CarManager(carNames)
         );
-        org.assertj.core.api.Assertions.assertThat(
+        assertThat(
                 new CarManager("abc,def,자동차1,자동차2,자동차3").getCars().size()).isEqualTo(5);
     }
+
+    @Test
+    @DisplayName("최종 우승자 가져오기")
+    public void getWinner() {
+        final RacingCar test1Car = new RacingCar("test1", 10);
+        final RacingCar sangwonCar = new RacingCar("sang", 4);
+        final RacingCar dddddCar = new RacingCar("ddddd", 10);
+
+
+        final CarManager carManager =
+                new CarManager(List.of(test1Car,sangwonCar,dddddCar));
+
+        List<Car> winner = carManager.getWinner();
+
+        assertThat(winner.size()).isEqualTo(2);
+        Assertions.assertTrue(winner.contains(test1Car));
+        Assertions.assertFalse(winner.contains(sangwonCar));
+        Assertions.assertTrue(winner.contains(dddddCar));
+        assertThat(winner.get(0)).isEqualTo(test1Car);
+        assertThat(winner.get(1)).isEqualTo(dddddCar);
+    }
+
 
 
 }
