13d98c020b85674a767107ac5f1e7c34975e3bc8
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 18:50:06 +0900

    refactor : MoveCondition 값 상수화

--- src/main/java/racingcar/constant/RacingGameConstant.java
+++ src/main/java/racingcar/constant/RacingGameConstant.java
@@ -2,4 +2,5 @@ package racingcar.constant;
 
 public class RacingGameConstant {
     public static final int ATTEMPT_NUMBER = 1;
+    public static final int MOVE_CONDITION_NUMBER = 4;
 }
--- src/main/java/racingcar/domain/condition/MoveCondition.java
+++ src/main/java/racingcar/domain/condition/MoveCondition.java
@@ -1,7 +1,10 @@
 package racingcar.domain.condition;
 
+import static racingcar.constant.RacingGameConstant.MOVE_CONDITION_NUMBER;
+
 public class MoveCondition {
+
     public boolean check(int number) {
-        return number >= 4;
+        return number >= MOVE_CONDITION_NUMBER;
     }
 }
