5996a7ec5b2cd73c906669ca820516723106319c
elizabethgim <yujingim@naver.com>
2023-10-31 10:43:37 +0900

    feat: 최종 우승자 구하기 및 출력

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,11 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Race {
     private Map<String, Integer> cars;
@@ -31,6 +28,27 @@ public class Race {
         count = Integer.parseInt(Console.readLine());
     }
 
+    private List<String> getWinner(){
+        List<String> winners = new ArrayList<>();
+        List<String> scores = new ArrayList<>(cars.keySet());
+        scores.sort((o1, o2)-> cars.get(o2).compareTo(cars.get(o1)));
+        int maxRun = cars.get(scores.get(0));
+
+        for(int i=0;i<cars.size()-1;i++){
+            if(cars.get(scores.get(i)) == maxRun){
+                winners.add(scores.get(i));
+            }
+        }
+        return winners;
+    }
+    private void showResults(){
+        List<String> winners = getWinner();
+        System.out.print("최종 우승자 : ");
+        for(String winner: winners){
+            System.out.print(winner+" ");
+        }
+
+    }
     public void start(){
         getCarNames();
 
@@ -39,5 +57,6 @@ public class Race {
         Car car = new Car();
         car.run(count, names, cars);
 
+        showResults();
    }
 }
