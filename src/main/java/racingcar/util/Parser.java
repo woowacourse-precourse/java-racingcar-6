1145a7d794839bffc306dbb20088549b2c05108e
leeminjun <sohwi0305@gmail.com>
2023-10-31 14:32:13 +0900

    feat: 자동차이름 쉼표로 구분

---
+++ src/main/java/racingcar/util/Parser.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+
+    public List<String> parseCarNames(String input) {
+        return new ArrayList<>(Arrays.asList(input.split(",")));
+    }
+
+}
