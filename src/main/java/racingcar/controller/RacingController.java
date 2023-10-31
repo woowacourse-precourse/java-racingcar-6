945beb0c988bf534de13be845e0ad468add5e933
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-31 14:44:48 +0900

    feat: RacingController 추가

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,45 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.model.PlayRacing;
+import racingcar.model.TryCount;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private  PlayRacing playRacing;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        Cars cars = initCars();
+        TryCount tryCount = initTryCount();
+
+        playRacing = new PlayRacing(new RandomNumberGenerator(), cars);
+        outputView.printRacingResult();
+        inProgress(tryCount);
+        outputView.printWinner(playRacing.end());
+    }
+
+    private void inProgress(TryCount tryCount) {
+        for (int i = 0; i < tryCount.getTryCount(); i++) {;
+            outputView.printProcess(playRacing.process());
+        }
+    }
+
+    private TryCount initTryCount() {
+        System.out.println();
+        outputView.printTryCountMessage();
+        return new TryCount(inputView.inputTryCount());
+    }
+    private Cars initCars() {
+        outputView.printStartMessage();
+        return new Cars(inputView.inputCarNames());
+    }
+}
