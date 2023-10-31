f6f6c9d0f6b7ff23f54718bf26d3a37ee2da8f6f
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-10-31 22:46:12 +0900

    refactor: 한 대만 입력 시 예외 처리
    한 대만 입력했을 경우 "두 대 이상의 차를 입력하세요."
    메세지 출력 후 종료

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -7,11 +7,11 @@ public class CarNameValidator {
 
     private final String NAME;
     public final ArrayList<String> NAMES;
-    private final static String ERROR = "[ERROR]";
     private final static String NAME_ERROR_MESSAGE = "차 이름은 문자로만 구성되어야 합니다.";
     private final static String NAME_NULL_ERROR_MESSAGE = "차 이름을 적어주세요.";
     private final static String NAME_RANGE_ERROR_MESSAGE = "차 이름은 1자 이상 5자 이하로 적어주세요.";
     private final static String NAME_DUPLICATE_ERROR_MESSAGE = "중복된 이름을 적으면 안 됩니다.";
+    private final static String ONE_CAR_ERROR_MESSAGE = "두 대 이상의 차를 입력하세요.";
     private final static int MAX_LENGTH = 5;
     private final static int MIN_LENGTH = 1;
 
@@ -19,34 +19,41 @@ public class CarNameValidator {
         NAME = input;
         NAMES = new ArrayList<>(Arrays.asList(input.split(",")));
         isNull();
-        isRangeOut();
         isNameError();
+        isRangeOut();
+        isOneCar();
         isDuplicate();
-    }
-
-    public void isNameError(){
-        if (NAME.contains(" ")){
-            throw new IllegalArgumentException(ERROR+NAME_ERROR_MESSAGE);
-        }
     }
 
     public void isNull(){
         if (NAME.equals("")){
-            throw new IllegalArgumentException(ERROR+NAME_NULL_ERROR_MESSAGE);
+            throw new IllegalArgumentException(NAME_NULL_ERROR_MESSAGE);
+        }
+    }
+
+    public void isNameError(){
+        if (NAME.contains(" ")){
+            throw new IllegalArgumentException(NAME_ERROR_MESSAGE);
         }
     }
 
     public void isRangeOut(){
         for (String name : NAMES) {
             if (name.length() > MAX_LENGTH || name.length() < MIN_LENGTH){
-                throw new IllegalArgumentException(ERROR+NAME_RANGE_ERROR_MESSAGE);
+                throw new IllegalArgumentException(NAME_RANGE_ERROR_MESSAGE);
             }
+        }
+    }
+
+    public void isOneCar(){
+        if (NAMES.size()<2){
+            throw new IllegalArgumentException(ONE_CAR_ERROR_MESSAGE);
         }
     }
 
     public void isDuplicate(){
         if (NAMES.stream().distinct().count() != NAMES.size()){
-            throw new IllegalArgumentException(ERROR+NAME_DUPLICATE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
