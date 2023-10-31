2cd37b3e62237b942f3eaf639a1fcfd17097f177
hakdang113 <zapper15@naver.com>
2023-10-31 20:47:43 +0900

    시도횟수를 사용자로부터 입력받는 부분추가

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarService {
 
 	private RegistCarInfo registCarInfo;
@@ -8,7 +10,8 @@ public class RacingCarService {
 		System.out.println("경주할 자동차 이름을 입력하세요. " +
 				"(이름은 쉼표(,) 기준으로 구분)");
 		registCarInfo.registCarName(Console.readLine());
-
+		System.out.println("시도할 회수는 몇회인가요?");
+		registCarInfo.registTryCount(Console.readLine());
 
 		return null;
 	}
