16c07c7e345075ea7fa713ccd48f2277bad1d3b9
yewonahn <yewonahn@naver.com>
2023-11-01 04:05:28 +0900

    feat: 자동차 이름을 입력받는 메서드와 시도할 횟수를 입력받는 메서드를 모아둔 InputController

--- src/main/java/racingcar/InputController.java
+++ src/main/java/racingcar/InputController.java
@@ -7,21 +7,31 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputController {
 
-    public static String[] carNamesArray;
-    public static ArrayList<Car> carArrList;
+    public static String[]carNamesArray;
+    public static ArrayList<Car>carArrList;
     public static int cycleNum;
 
     public InputController() {
-        carNamesArray = new String[0];
-        carArrList = new ArrayList<>();
-        cycleNum = 0;
+        carNamesArray= new String[0];
+        carArrList= new ArrayList<>();
+        cycleNum= 0;
     }
 
     void inputCarName() {
+        PrintController.printCarNameDirection();
+        String carNames =readLine();
+        carNamesArray= carNames.split(",", -1);
 
+        ArrayList<String> carNamesList = new ArrayList<>(Arrays.asList(carNamesArray));
+
+        for (String carName :carNamesArray) {
+            carArrList.add(new Car(carName));
+        }
     }
 
     void inputCycleNum() {
-
+        PrintController.printCycleNumDirection();
+        String stringCycleNum =readLine();
+        cycleNum= Integer.parseInt(stringCycleNum);
     }
 }
