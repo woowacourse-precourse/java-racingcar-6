52bd9d49349ed6ba6c6e783fc52dd5b51a5e9adc
dbstn1325 <dbstn1325@naver.com>
2023-10-31 18:07:33 +0900

    refactor(TryCountValidator): 시도 횟수가 숫자가 아닌 문자열인 경우 예외처리 기능 내 숫자 변환 메서드를 분리

--- src/main/java/validator/TryCountValidator.java
+++ src/main/java/validator/TryCountValidator.java
@@ -4,7 +4,7 @@ public class TryCountValidator extends CommonValidator {
 
     public static void validateString(String tryCount) {
         try {
-            Integer.parseInt(tryCount);
+            convertToInteger(tryCount);
         } catch (NumberFormatException ex) {
             throw new IllegalArgumentException("시도 횟수는 숫자만 가능합니다. 애플리케이션을 종료합니다.");
         }
@@ -24,5 +24,9 @@ public class TryCountValidator extends CommonValidator {
         if (tryCount < 1) {
             throw new IllegalArgumentException("시도 횟수가 양의 정수가 아닙니다. 애플리케이션을 종료합니다.");
         }
+    }
+
+    private static void convertToInteger(String tryCount) {
+        Integer.parseInt(tryCount);
     }
 }
