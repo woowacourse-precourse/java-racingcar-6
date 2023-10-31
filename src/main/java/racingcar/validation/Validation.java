ffdf34957906e337db29f7dcb1b82185736634e5
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 06:05:13 +0900

    feat : 검증 결과 에러 발생시, 에러 메세지를 출력하는 기능 추가

--- src/main/java/racingcar/validation/Validation.java
+++ src/main/java/racingcar/validation/Validation.java
@@ -1,5 +1,7 @@
 package racingcar.validation;
 
+import racingcar.view.ErrorMessage;
+
 import java.util.HashMap;
 
 import static racingcar.utils.Utils.*;
@@ -9,30 +11,35 @@ public final class Validation {
     public static void validateIsNotNull(String inputString){
         if (inputString == null
                 || replaceCommaToBlank(inputString).isBlank()) {
+            ErrorMessage.isNotNullError();
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateSeparator(String inputString){
         if (!isEqualArray(splitByComma(inputString), splitByCommaAndBlank(inputString))) {
+            ErrorMessage.separatorError();
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateCarNumber(String[] arr){
         if (arr.length < 1) {
+            ErrorMessage.carNumberError();
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateCarNameLength(String carName){
         if (carName.length() > 5) {
+            ErrorMessage.carNameLengthError();
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateCarNamesNotDuplicate(HashMap<String,String> hashMap, String carName){
         if (hashMap.keySet().contains(carName)) {
+            ErrorMessage.carNamesNotDuplicateError();
             throw new IllegalArgumentException();
         }
     }
@@ -41,12 +48,14 @@ public final class Validation {
         try {
             Integer.parseInt(string);
         } catch (NumberFormatException e) {
+            ErrorMessage.isDigitError();
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateTryNumber(String inputString){
         if (stringToInt(inputString) < 0) {
+            ErrorMessage.tryNumberError();
             throw new IllegalArgumentException();
         }
     }
