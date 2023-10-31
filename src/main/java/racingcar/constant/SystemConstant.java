71fc2b34e6d8015dc810ef8d03cb4c7b70cd5dc8
bonooy <choiboyuna@gmail.com>
2023-10-31 12:36:48 +0900

    Add: 설정에 필요한 상수 지정하는 System Constant 추가

---
+++ src/main/java/racingcar/constant/SystemConstant.java
@@ -0,0 +1,12 @@
+package racingcar.constant;
+
+public class SystemConstant {
+    public static final int LENGTH_OF_CAR_NAME_MAX = 5;
+    public static final int LENGTH_OF_CAR_NAME_MIN = 1;
+    public static final String BLANK = " ";
+
+    public static final int RANGE_OF_START = 0;
+    public static final int RANGE_OF_END = 9;
+    public static final int FORWARD_CONDITION = 4;
+
+}
