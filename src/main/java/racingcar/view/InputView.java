67255f8d6205ba07d7a66c404e5bf5d1df96506e
aeyongdodam <kikititi0510@gmail.com>
2023-11-01 03:27:43 +0900

    feat(InputView): 시도 회수 입력 함수 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         return Console.readLine();
     }
+
+    public String getTrial() {
+        System.out.print("시도할 회수는 몇회인가요?\n");
+        return Console.readLine();
+    }
 }
