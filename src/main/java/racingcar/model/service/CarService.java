dde70b6f27e15850ba09b4465b21956bf6732784
조은별 <joeunbyeol98@gmail.com>
2023-11-01 03:08:07 +0900

    feat(CarServiceImpl): 무작위 값이 4이상일 때 전진하는 기능 추가

--- src/main/java/racingcar/model/service/CarService.java
+++ src/main/java/racingcar/model/service/CarService.java
@@ -3,4 +3,6 @@ package racingcar.model.service;
 public interface CarService {
     void registerCarLineUp(String carNames);
     void registerTryCount(String tryCount);
+
+    void goOrStop();
 }
--- src/main/java/racingcar/model/service/CarServiceImpl.java
+++ src/main/java/racingcar/model/service/CarServiceImpl.java
@@ -2,15 +2,22 @@ package racingcar.model.service;
 
 import static racingcar.messages.ViewMessage.INPUT_SEPARATOR;
 
+import java.util.List;
 import racingcar.dto.Car;
 import racingcar.model.dao.CarDao;
 import racingcar.model.dao.CarDaoImpl;
+import racingcar.utils.RandomNumberUtil;
 import racingcar.validator.InputValidator;
 
 public class CarServiceImpl implements CarService {
     private static CarService instance = new CarServiceImpl();
     private InputValidator validator = InputValidator.getInstance();
     private CarDao carDao = CarDaoImpl.getInstance();
+    private RandomNumberUtil randomNumberUtil = RandomNumberUtil.getInstance();
+    private final int MIN_NUMBER = 0;
+    private final int MAX_NUMBER = 9;
+    private final int MOVING_CONDITION = 4;
+
 
     private CarServiceImpl() {}
     public static CarService getInstance() {
@@ -35,5 +42,16 @@ public class CarServiceImpl implements CarService {
         validator.validatePositiveNumber(tryNumber);
 
         carDao.insertTryCount(tryNumber);
+    }
+
+    @Override
+    public void goOrStop() {
+        List<Car> carLineup = carDao.selectAllCars();
+
+        for (Car car : carLineup) {
+            if (randomNumberUtil.pickRandomNumber(MIN_NUMBER, MAX_NUMBER) >= MOVING_CONDITION) {
+                carDao.increaseMoveCount(car.getName());
+            }
+        }
     }
 }
