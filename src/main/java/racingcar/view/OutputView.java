3db169c4713e96caedc6d6042af5ded936e75458
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-31 20:51:42 +0900

    refactor(OutputView): 함수 이름 변경 및 우승자 출력, 이동거리 출력 함수 변경 및 공백 출력 함수 추가
    - 메시지 출력 의미에 맞도록 printlnMessage에서 printMessage로 함수명 변경
    - 공백 출력 함수를 따로 분리
    - printCarNameAndPosition 함수에서 이동거리를 나타내도록 변경
    - printWinners 함수의 인자값을 List<Car>에서 List<String>으로 변경하고, join을 통해 문자열을 합쳐서 출

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,49 +1,47 @@
 package racingcar.view;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class OutputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇회인가요?";
-    private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
     private static final String DASH = "-";
     private static final String COLON = " : ";
     private static final String RESULT_MESSAGE = "최종 우승자 : ";
     private static final String JOINING_COMMA = ", ";
+
     public static void printNameInputMessage() {
-        printlnMessage(NAME_INPUT_MESSAGE);
+        printMessage(NAME_INPUT_MESSAGE);
     }
 
     public static void printRoundInputMessage() {
-        printlnMessage(ROUND_INPUT_MESSAGE);
+        printMessage(ROUND_INPUT_MESSAGE);
+    }
+
+    public static void printBlankLine() {
+        System.out.println();
     }
 
     public static void printExecutionResultMessage() {
-        System.out.println();
-        printlnMessage(EXECUTION_RESULT);
+        printBlankLine();
+        printMessage(EXECUTION_RESULT_MESSAGE);
     }
 
     public static void printCarNameAndPosition(Car car) {
-        String dashes = createDashes(car.getPosition());
+        String dashes = DASH.repeat(car.getPosition());
         String result = car.getName() + COLON + dashes;
-        printlnMessage(result);
+        printMessage(result);
     }
 
-    public static String createDashes(int position) {
-        return DASH.repeat(position);
-    }
-
-    public static void printWinners(List<Car> cars) {
-        String joinCommaWinners = cars.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(JOINING_COMMA));
+    public static void printWinners(List<String> carNames) {
+        String joinCommaWinners = String.join(JOINING_COMMA, carNames);
         String result = RESULT_MESSAGE + joinCommaWinners;
-        printlnMessage(result);
+        printMessage(result);
     }
 
-    private static void printlnMessage(String message) {
+    private static void printMessage(String message) {
         System.out.println(message);
     }
 }
