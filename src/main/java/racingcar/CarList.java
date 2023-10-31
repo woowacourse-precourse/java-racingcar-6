e36caf23ead82fdacbd1777697c4cfe3bc97f80d
hyun-hyang <jimin3060@naver.com>
2023-11-01 00:11:08 +0900

    Feat : class CarList created

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarList {
-    final List<Car> CarList;
+    final List<Car> carList = new ArrayList<>();
 
-    public CarList(List<Car> carList) {
-        this.CarList = carList;
+    public CarList(List<String> carNameSplit) {
+        for(String carName : carNameSplit){
+            this.carList.add(new Car(carName));
+        }
     }
 }
