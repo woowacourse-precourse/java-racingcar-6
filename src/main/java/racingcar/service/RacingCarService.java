6933289b937185d1684d36ccf95134cc2b5ee3a4
InJun2 <os2864@naver.com>
2023-10-31 14:01:48 +0900

    :sparkles: Car 객체 로직 구현

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,4 +1,41 @@
 package racingcar.service;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingCarService {
+    private final CarsFactory carsFactory;
+
+    public RacingCarService() {
+        carsFactory = new CarsFactory();
+    }
+
+    public Cars createCars(String[] strArr) {
+        return carsFactory.createCars(strArr);
+    }
+
+
+    public void moveCars(Cars cars) {
+        cars.getCars()
+                .forEach(Car::move);
+    }
+
+    public int maxCarDistance(Cars cars) {
+        return cars.getCars()
+                .stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+    }
+
+    public List<String> getWinnerCarNames(Cars cars, int maxDistance) {
+        return cars.getCars()
+                .stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
