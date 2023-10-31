c7f85162d1c5588386d31916de47880280c7eff6
ykmxxi <ykmxxi97@gmail.com>
2023-10-31 12:41:44 +0900

    refactor(TrialCountValidator): 메소드가 어떤 동작을 하는지 구체적으로 변경

--- src/main/java/racingcar/util/TrialCountValidator.java
+++ src/main/java/racingcar/util/TrialCountValidator.java
@@ -7,14 +7,14 @@ public final class TrialCountValidator {
 
     private static final String POSITIVE_NUMBER_REGEX = "^[1-9]+[0-9]*$";
     private static final String POSITIVE_NUMBER_ERROR_MESSAGE = "양수가 아닙니다.";
-    private static final String RANGE_ERROR_MESSAGE = "int 범위 숫자가 아닙니다.";
+    private static final String INTEGER_RANGE_ERROR_MESSAGE = "int 범위 숫자가 아닙니다.";
 
     private TrialCountValidator() {
     }
 
     public static void validate(final String trialCount) {
         validatePositiveNumber(trialCount);
-        validateRange(trialCount);
+        validateIntegerRange(trialCount);
     }
 
     private static void validatePositiveNumber(final String trialCount) {
@@ -25,10 +25,10 @@ public final class TrialCountValidator {
         }
     }
 
-    private static void validateRange(final String trialCount) {
+    private static void validateIntegerRange(final String trialCount) {
         long parseLong = Long.parseLong(trialCount);
         if (parseLong > Integer.MAX_VALUE) {
-            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(INTEGER_RANGE_ERROR_MESSAGE);
         }
     }
 
