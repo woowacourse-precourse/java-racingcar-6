2b6edefe2117b307c612f50b65a2b72c05fdf42e
moong2 <www.castlehi@gmail.com>
2023-10-31 20:19:06 +0900

    docs: remove unused variable

--- src/main/java/racingcar/util/GameConstants.java
+++ src/main/java/racingcar/util/GameConstants.java
@@ -1,9 +1,6 @@
 package racingcar.util;
 
 public class GameConstants {
-    // 자동차 개수
-    public static final int COUNT_OF_CAR = 3;
-
     // 자동차 이름 입력
     public static final int MINIMUM_CAR_NAME_LENGTH = 1;
     public static final int MAXIMUM_CAR_NAME_LENGTH = 5;
