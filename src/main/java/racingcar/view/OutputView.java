381185bb087f8957186a19ce9ee468c7199cacea
JoonKyo <joonkyolee@gmail.com>
2023-10-31 21:28:52 +0900

    feat(OutputView): 실행 결과 메시지 출력 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     private static final String RESULT_INTRODUCTION = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자";
     private static final String COLON = " : ";
+
+    public void printResultIntroduction(){
+        System.out.println(RESULT_INTRODUCTION);
+    }
 }
