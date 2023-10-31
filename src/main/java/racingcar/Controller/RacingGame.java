ee28370d6b8ebbc2e072208b23177cee8a8f45f7
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-31 16:35:06 +0900

    feat(RacingGame): add tryMove function
    Add tryMove function to create random number and repeat updateCarStatus function

--- src/main/java/racingcar/Controller/RacingGame.java
+++ src/main/java/racingcar/Controller/RacingGame.java
@@ -1,12 +1,30 @@
 package racingcar.Controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.CarInfo;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 import racingcar.util.Validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
     private final CarInfo carInfo = new CarInfo();
+
+    private int convertStringToInt(String num) {
+        Validation.checkTryNumber(num);
+        return Integer.parseInt(num);
+    }
+
+    private void tryMove() {
+        int carNum = carInfo.getCarName().size();
+        for(int i = 0; i < carNum; i++) {
+            int randomNum = Randoms.pickNumberInRange(0,9);
+            carInfo.updateCarStatus(carInfo.isMove(randomNum), i);
+        }
+    }
+
     public void racingStart() {
         int tryNum;
 
@@ -16,14 +34,12 @@ public class RacingGame {
 
         OutputView.printExecResult();
         for(int i = 0; i < tryNum; i++) {
-            carInfo.updateCarStatus();
+            tryMove();
             OutputView.printCarStatus(carInfo.getCarName(), carInfo.getCarStatus());
         }
         OutputView.printWinner(carInfo.getWinnerName());
     }
-    public int convertStringToInt(String num) {
-        Validation.checkTryNumber(num);
-        return Integer.parseInt(num);
-    }
+
+
 
 }
