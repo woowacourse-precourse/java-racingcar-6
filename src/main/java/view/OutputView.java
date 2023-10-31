5558db49cdfd0fe042dca6e6ceac643e47dfcc52
nayonsoso <panda0329@naver.com>
2023-11-01 03:36:37 +0900

    feat: 상수화된 메세지 출력하는 함수 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,17 @@
 package view;
 
-public class OutputView {
+import constants.OutputMessage;
 
+public class OutputView {
+    public void requestCarNames(){
+        System.out.println(OutputMessage.CAR_NAME_INPUT_PROMPT);
+    }
+
+    public void requestAttemptTimes(){
+        System.out.println(OutputMessage.ATTEMPT_TIMES_INPUT_PROMPT);
+    }
+
+    public void noticeRaceStart(){
+        System.out.println(OutputMessage.RESULT_PRINT_START_STATEMENT);
+    }
 }
