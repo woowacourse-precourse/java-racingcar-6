40c0419e4241a513f3913007c7caac45ded2b4e2
Youth <boysoeng@naver.com>
2023-11-01 03:06:12 +0900

    feat: 게임 세팅 기능 추가

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.utils.Converter;
+
+public class Game {
+
+    private List<Car> carList = new ArrayList<>();
+    private List<Car> winner = new ArrayList<>();
+
+    private Player player;
+    private Long gameCount;
+
+    public void raceSetting() {
+        player = new Player();
+        setCarList();
+        setGameCount();
+    }
+
+    private void setCarList() {
+        player.inputCarNames();
+        List<String> carNames = player.getCarNames();
+        carList = Converter.convertStringListToCarList(carNames);
+    }
+
+    private void setGameCount() {
+        player.inputGameCount();
+        gameCount = player.getGameCount();
+    }
+
+}
