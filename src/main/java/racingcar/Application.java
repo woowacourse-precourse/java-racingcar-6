3b78809a65568df5240ecbc3a29dca60f418c4d4
tsulocalize <tsulocalizing@gmail.com>
2023-10-31 17:46:54 +0900

    feat: 자동차 경주 메인 동작 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,15 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager();
+        CarManager carManager = new CarManager(inputManager);
+
+        int times = inputManager.getTimes();
+        for (int i = 0; i < times; i++) {
+            carManager.runCars();
+            carManager.printResult();
+        }
+
+        carManager.printWinner();
     }
 }
