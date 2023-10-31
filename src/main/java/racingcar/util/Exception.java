086a53a5a24dfc6033617c44642742b525de4338
leeminjun <sohwi0305@gmail.com>
2023-10-31 14:25:45 +0900

    Refactor: 글자를 입력 안했을때로 수정

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -1,7 +1,6 @@
 package racingcar.util;
 
 public class Exception {
-    private static final int CAR_NAME_LIMIT_MIN = 1;
     private static final int CAR_NAME_LIMIT_MAX = 5;
     private static final String SPACE = " ";
 
@@ -31,14 +30,8 @@ public class Exception {
         if (CAR_NAME_LIMIT_MAX < carName.length()) {
             throwMaxLengthException();
         }
-        if (carName.length() < CAR_NAME_LIMIT_MIN) {
-            throwMinLengthException();
-        }
     }
 
-    private void throwMinLengthException() {
-        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MIN + "글자 이상이어야 합니다.");
-    }
 
     private void throwMaxLengthException() {
         throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MAX + "글자 이하여야 합니다.");
