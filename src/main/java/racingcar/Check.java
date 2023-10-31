4173dd56fe81bc675c6d607feb78dbb479d07d33
leorb159 <ldgsocial159@naver.com>
2023-11-01 04:10:40 +0900

    feat(Check): 자동차 이름 길이 예외 함수 구현

--- src/main/java/racingcar/Check.java
+++ src/main/java/racingcar/Check.java
@@ -1,2 +1,15 @@
-package racingcar;public class Check {
+package racingcar;
+
+public class Check {
+
+    public void checkNameSize(String input){
+        String[] strArr = input.split(",");
+        for(int i=0; i<strArr.length; i++){
+            if(strArr[i].length() > 5){
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
+            }
+        }
+        return;
+    }
+
 }
