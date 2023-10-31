e5d6cb3f10a9996403bbc46c004ad5d362fa3d2e
leorb159 <ldgsocial159@naver.com>
2023-11-01 04:22:05 +0900

    feat(Input): 자동차 이름 길이를 확인하도록 적용

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 public class Input {
 
     private ArrayList<String> nameList;
+    private Check validation;
     public Input(){
         nameList = new ArrayList<>();
+        validation = new Check();
     }
 
     public ArrayList<String> getNameList(String input){
+        validation.checkNameSize(input);
         String[] strArr = input.split(",");
         for(int i=0; i<strArr.length; i++){
             nameList.add(strArr[i]);
