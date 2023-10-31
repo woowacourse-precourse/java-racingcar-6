8d5d0e1bacf1f2fcbc7e2a9ed5d324f9d46e5ed3
limzzum <chfhddl1234@naver.com>
2023-10-31 21:41:48 +0900

    :sparkles: feat(View) : 차수별 게임 결과 출력 메서드 추가

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,9 +1,20 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.dto.PerGameCarResultDto;
+
 public class View {
 
     public void println(String input) {
         System.out.println(input);
+    }
+
+    public void printResult(List<PerGameCarResultDto> resultDtos) {
+        for (PerGameCarResultDto resultDto : resultDtos) {
+            String moveResult = new String(new char[resultDto.getMoveCnt()]).replace('\0', '-');
+            System.out.println(resultDto.getCarName() + " : " + moveResult);
+        }
+        System.out.println();
     }
 
 }
