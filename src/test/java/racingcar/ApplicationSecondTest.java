25816f3aeb8fc46239a3d4912d7e387f80887998
Infikei <hckhong@naver.com>
2023-10-31 17:49:26 +0900

    test: 자동차 이름으로 빈 문자열이 들어오는 경우에 대한 테스트 추가

---
+++ src/test/java/racingcar/ApplicationSecondTest.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationSecondTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void 빈_문자열_이름에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_문자열_이름에_대한_예외_처리_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("infi,,kei", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_문자열_이름에_대한_예외_처리_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",infi,kei", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_문자열_이름에_대한_예외_처리_4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("infi,kei,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_문자열_이름에_대한_예외_처리_5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,,,,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
