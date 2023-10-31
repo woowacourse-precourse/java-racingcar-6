18168be9c78a29218e66aaedb05bd0613519c719
kwon <kckc0608@naver.com>
2023-11-01 00:48:32 +0900

    feat: 자동차 이름 입력 데이터를 쉼표 기준으로 나누기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static void main(String[] args) {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         String carNameInput = camp.nextstep.edu.missionutils.Console.readLine();
+        List<String> carNames = Arrays.stream(carNameInput.split(",")).toList();
     }
 }
