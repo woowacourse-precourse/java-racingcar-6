8a9186f1fac91a873c97eaccce9322c20d179ba7
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 23:32:21 +0900

    refactor: 기능에 맞는 위치로 코드 이동

--- src/main/java/racingcar/domain/CarNames.java
+++ src/main/java/racingcar/domain/CarNames.java
@@ -1,14 +1,16 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import racingcar.validator.Validator;
 
 /*
- *   자동차 이름 정보를 담당
+ *   여러개의 이름 입력을 담당(일급컬렉션)
  * */
 
-
 public class CarNames {
+    private static final String SPLIT_SIGN = ",";
     private final List<CarName> names;
 
     private CarNames(List<CarName> names) {
@@ -16,24 +18,32 @@ public class CarNames {
         this.names = names;
     }
 
-    public static CarNames from(List<CarName> carNames) {
+    public static CarNames fromInput(String input) {
+        List<String> splitInput = inputSplitBySign(input);
+        List<CarName> carNames = splitInput.stream()
+                .map(CarName::new)
+                .toList();
         return new CarNames(carNames);
+    }
+
+    private static List<String> inputSplitBySign(String input) {
+        return Arrays.stream(input.split(SPLIT_SIGN)).toList();
     }
 
     private void validateCarNames(List<CarName> carNames) {
         List<String> namesToString = carNames.stream()
                 .map(CarName::getValue)
                 .toList();
-        // Validator.commandsSizeBiggerThan(0, carNames); 입력받은 이름임 없을때는
+        Validator.commandsSizeZero(namesToString);
         Validator.commandsNotDuplicated(namesToString);
     }
     //일급컬렉션의 불변성을 지키기위해 getter 제거
 
-    public void createCars(List<Car> cars) {
+    public List<Car> createCars() {
+        List<Car> cars = new ArrayList<>();
         for (CarName name : names) {
-            cars.add(new Car(name));
+            cars.add(Car.from(name));
         }
+        return cars;
     }
-    
-
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,36 +1,21 @@
 package racingcar.view;
 
-import static racingcar.constant.CarConstant.MAX_MOVE_COUNT;
-import static racingcar.constant.CarConstant.MIN_MOVE_COUNT;
-
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
-import racingcar.domain.CarName;
 import racingcar.domain.CarNames;
 import racingcar.domain.MoveCount;
-import racingcar.validator.Validator;
 
 /*
  *   사용자의 입력을 담당
  * */
 public class InputView {
-    private static final int SIZE_ZERO = 0;
-
     public CarNames carNames() {
         String input = Console.readLine();
-        //가독성 괜찮나?
-        List<CarName> carNames = Arrays.stream(input.split(",")).map((carName) -> new CarName(carName)).toList();
-        return CarNames.from(carNames);
+        return CarNames.fromInput(input);
     }
-
 
     public MoveCount moveCount() {
         String input = Console.readLine();
-        Validator.commandToInteger(input);
-        int moveCount = Integer.parseInt(input);
-        Validator.commandValueInRange(moveCount, MIN_MOVE_COUNT, MAX_MOVE_COUNT);
         System.out.println();
-        return new MoveCount(moveCount);
+        return MoveCount.fromInput(input);
     }
 }
