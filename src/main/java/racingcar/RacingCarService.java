95a10f297951f1fc055e5807adec6508d6004dfe
hakdang113 <zapper15@naver.com>
2023-11-01 03:09:24 +0900

    자동차 경주 서비스 클래스 수정

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -2,17 +2,22 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class RacingCarService {
 
-	private RegistCarInfo registCarInfo;
-
-	public String playRacing() {
+	public void playRacing() {
 		System.out.println("경주할 자동차 이름을 입력하세요. " +
 				"(이름은 쉼표(,) 기준으로 구분)");
-		registCarInfo.registCarName(Console.readLine());
+		String carNames = Console.readLine();
+		System.out.println(carNames);
+		RegistCarInfo registCarInfo = new RegistCarInfo();
+		List<String> racingCarNames = registCarInfo.registCarName(carNames);
 		System.out.println("시도할 회수는 몇회인가요?");
-		registCarInfo.registTryCount(Console.readLine());
+		Integer tryCount = registCarInfo.registTryCount(Console.readLine());
 
-		return null;
+		System.out.println("실행 결과");
+		RacingResult racingResult = new RacingResult();
+		racingResult.racingGame(racingCarNames, tryCount);
 	}
 }
