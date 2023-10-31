0b9e81e9238ec335819a23fa4bbccf11876b202f
songjayhyun <thewinner1st@naver.com>
2023-10-31 21:59:57 +0900

    refactor: startGame 메서드 다섯 줄 규칙 적용 리팩토링
    - 메서드 추출로 다섯 줄 규칙 리팩토링

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -4,36 +4,54 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingCarGame {
+    private final InputView inputView = new InputView();
+    private final User user = new User();
+    private final OutputView outputView = new OutputView();
+
     public void startGame() {
-        InputView inputView = new InputView();
-        User user = new User();
 
-        inputView.printCarNameInputMsg();
-        user.inputCarName();
-        List<String> carNames = user.getCarNames();
-        List<Car> cars = carNames.stream()
-                .map(Car::new)
-                .collect(Collectors.toList());
+        List<Car> cars = createCars();
 
-        inputView.printTryCountInputMsg();
-        user.inputTryCount();
+        inputTryCount();
 
-        OutputView outputView = new OutputView();
         outputView.printGameResultMsg();
 
-        int tryCount = user.getTryCount();
-        while (tryCount > 0) {
-            playRound(cars);
-            outputView.printCarsPosition(cars);
-            tryCount--;
-        }
+        playRound(cars);
 
+        announceWinners(cars);
+    }
+
+    private void announceWinners(List<Car> cars) {
         Judge judge = new Judge(cars);
         List<Car> winners = judge.decideWinner();
         outputView.printWinnersName(winners);
     }
 
     private void playRound(List<Car> cars) {
+        int tryCount = user.getTryCount();
+        while (tryCount > 0) {
+            moveCars(cars);
+            outputView.printCarsPosition(cars);
+            tryCount--;
+        }
+    }
+
+    private void inputTryCount() {
+        inputView.printTryCountInputMsg();
+        user.inputTryCount();
+    }
+
+    private List<Car> createCars() {
+        inputView.printCarNameInputMsg();
+        user.inputCarName();
+
+        List<String> carNames = user.getCarNames();
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private void moveCars(List<Car> cars) {
         for (Car car : cars) {
             if (car.canMove()) {
                 car.move();
