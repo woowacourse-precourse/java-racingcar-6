bfa6efef47b0f34fad56edc19d2e39fada1f87cd
elyudwo <dudqk9696@naver.com>
2023-11-01 00:18:10 +0900

    [Feat] : Distance 객체 유효성 검사 기능 구현
    1. 자동차 위치 0 미만일 경우 예외 발생

--- src/main/java/racingcar/domain/Distance.java
+++ src/main/java/racingcar/domain/Distance.java
@@ -5,7 +5,18 @@ public class Distance {
     private int distance;
 
     public Distance(int distance) {
+        validateCarDistance(distance);
         this.distance = distance;
+    }
+
+    private void validateCarDistance(int carDistance) {
+        validateCarDistanceIsMinus(carDistance);
+    }
+
+    public void validateCarDistanceIsMinus(int carDistance) {
+        if (carDistance < 0) {
+            throw new IllegalArgumentException("자동차 위치는 0 미만이 될 수 없습니다.");
+        }
     }
 
     public int getValue() {
