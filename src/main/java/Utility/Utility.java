749c4f8dfdd599779a981734e96de5a3bc29e566
ebang <dskhe8@gmail.com>
2023-10-31 17:21:07 +0900

    feat: [Utility] 문자열- 주어진 문자열을 ','로 쪼개는 기능 구현

---
+++ src/main/java/Utility/Utility.java
@@ -0,0 +1,7 @@
+package Utility;
+
+public class Utility {
+    public static String[] separateCarNamesBySeparator(String input, String delimeter) {
+        return input.split(delimeter);
+    }
+}
