aa26f185304822eb195911168dcf20554284cd78
Hanbee Lee <bee1162@naver.com>
2023-10-31 13:09:59 +0900

    feat: controller사용 시 car에서 필요한 값들만 가져오기 위한 dto

--- src/main/java/racingcar/dto/CarPositionDto.java
+++ src/main/java/racingcar/dto/CarPositionDto.java
@@ -1,2 +1,19 @@
-package racingcar.dto;public class CarPositionDto {
+package racingcar.dto;
+
+public class CarPositionDto {
+    private final int status;
+    private final String carName;
+
+    public CarPositionDto(int status, String carName) {
+        this.status = status;
+        this.carName = carName;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
 }
