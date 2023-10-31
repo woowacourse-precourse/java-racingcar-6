8361be890bc1673b067f156b6b991b8b174b486e
ksngh <chzhqk98@naver.com>
2023-11-01 00:01:54 +0900

    feat : 사용자 입력값 정의

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 class car {
 	String name;
 	String GoCount = "";
@@ -14,5 +16,13 @@ class car {
 
 public class Application {
     public static void main(String[] args) {
+		System.out.println("자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String Input_Name = Console.readLine();
+		System.out.println("시도할 회수는 몇회인가요?");
+		String Input_Trynum = Console.readLine();
+		String[] arr_Name = Input_Name.split(",");
+		int TryNum = Integer.parseInt(Input_Trynum);
+		car[] members = new car[arr_Name.length];
+		System.out.println("실행 결과");
     }
 }
