ac6115ad315818d0396dd15f8346144dc021c188
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 22:56:30 +0900

    [Style](User) - 매직 넘버 상수 처리

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -3,20 +3,23 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
+    public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     Exception errorHandling = new Exception();
 
     private void exceptionManager(String carName) {
-        errorHandling.ErrorCheck(carName);
+        errorHandling.lengthCheck(carName);
+        errorHandling.stringCompile(carName);
     }
     public String inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(START_MESSAGE);
         String carName = Console.readLine();
         exceptionManager(carName);
         return carName;
     }
 
     public int inputRound() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(ROUND_MESSAGE);
         int round = Integer.parseInt(Console.readLine());
         return round;
     }
