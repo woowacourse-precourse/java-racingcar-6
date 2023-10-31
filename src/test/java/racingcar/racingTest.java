548e642a732c3e3d39643d25128712519eea3aa4
BH946 <kobh2895@gmail.com>
2023-10-31 15:25:14 +0900

    test: 추가 테스트코드 작성

--- src/test/java/racingcar/racingTest.java
+++ src/test/java/racingcar/racingTest.java
@@ -3,8 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.CarGenerator;
+import racingcar.domain.NumberGenerator;
 import racingcar.domain.Referee;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -13,6 +15,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class racingTest {
 
+    /**
+     * CarGenerator
+     */
     @Test
     public void CarGenerator_생성() throws Exception {
         String input = "ab,cd,ef";
@@ -25,6 +30,9 @@ public class racingTest {
         }
     }
 
+    /**
+     * 입력 예외처리
+     */
     @Test
     public void 문자열길이_예외처리() {
         String input = "abcdef";
@@ -34,6 +42,18 @@ public class racingTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    /**
+     * NumberGenerator
+     */
+    @Test
+    public void 랜덤숫자_생성() {
+        Integer[] result = NumberGenerator.createRandomNumbers(3);
+        assertThat(Arrays.stream(result).allMatch(v->v>=0 && v<=9)).isTrue();
+    }
+
+    /**
+     * Referee
+     */
     @Test
     public void 우승자() {
         String input = "ab,cd,ef";
