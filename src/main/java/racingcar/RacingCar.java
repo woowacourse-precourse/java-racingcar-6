61426d2e5dfd8f15ab91f4f0f04385b89f6e96fb
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-31 19:28:11 +0900

    feat: 무작위 값 구하는 기능
    무작위 값이 될 수 있는 범위인 0,9를 상수로 지정
    Randoms 사용해 무작위값 생성 후 반환

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,6 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
+
+    private static final Integer PICK_NUMBER_START_RANGE = 0;
+    private static final Integer PICK_NUMBER_END_RANGE = 9;
 
     private String name;
     private Integer location;
@@ -8,5 +13,9 @@ public class RacingCar {
     public RacingCar(String name) {
         this.name = name;
         location = 0;
+    }
+
+    private Integer createRandomNumber() {
+        return Randoms.pickNumberInRange(PICK_NUMBER_START_RANGE, PICK_NUMBER_END_RANGE);
     }
 }
