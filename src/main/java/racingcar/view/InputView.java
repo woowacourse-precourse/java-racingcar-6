b724d265f9b9c9107e7f6e4ac43d12ebf2e59e55
NyaongKAI <yukihusiru18@gmail.com>
2023-11-01 03:31:25 +0900

    View 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,4 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    public static void requestCarNamesText(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void requestTryNumText(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
+    public void printGameResult(){
+        System.out.println("실행결과");
+    }
+
+    public void printCarMovement(String string, int number){
+        String result = "";
+
+        for (int i = 0; i < number; i++){
+            result = result.concat("-");
+        }
+
+        System.out.println(string + " : " + result);
+    }
+
+    public void printStringListJoinComma(List<String> listString){
+        System.out.println("최종 우승자 : " + String.join(", ", listString));
+    }
 }
