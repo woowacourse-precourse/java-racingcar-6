66297f2fba92aaf9a8676c1097e184ee138b127d
kyungwon Kim <ddwu20190942@gmail.com>
2023-10-31 20:36:15 +0900

    feat: 기본적인 입력문 추가
    추가된 입력문:
    - 자동차 이름 입력문
    - 경주 회수 입력문
    
    location: Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
 
         System.out.println("시도할 회수는 몇회인가요?");
+        String inputCount = Console.readLine();
 
         System.out.println("실행 결과");
 
