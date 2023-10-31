799798ba9559f0da1addd27de0e55154fc9e79ad
Daseul <daseull.rep@gmail.com>
2023-10-31 15:45:15 +0900

    feat: 게임 진행 기능 구현

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,10 +1,52 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final InputValidator validator = new InputValidator();
     private Cars cars;
+
+    public void run() {
+
+        cars = new Cars(createCarList());
+        int times = readTimes();
+
+        outputView.printResultMessage();
+        for (int i = 0; i < times; i++) {
+            cars.roundLevelMove();
+            outputView.printResult(cars.getCarList());
+        }
+
+        outputView.printWinner(cars.findWinner());
+
+    }
+
+    public List<Car> createCarList() {
+
+        List<String> carNames = inputView.readCarNames();
+
+        carNames.forEach(validator::validateName);
+
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public int readTimes() {
+        int times = inputView.readTimes();
+
+        validator.validateTimes(times);
+
+        return times;
+    }
 
 
 }
--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -8,7 +8,7 @@ public class RandomNumberGenerator {
     private static final int END_INCLUSIVE = 9;
 
 
-    public int createRandomNumber() {
+    public static int createRandomNumber() {
         return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 
