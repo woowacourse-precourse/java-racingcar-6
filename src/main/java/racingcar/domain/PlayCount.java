28c2edb5eb87d551ec5b0b5630559f7be63242ad
gmelon <hsh1769@naver.com>
2023-10-31 15:50:21 +0900

    feat: PlayCount의 값을 1만큼 감소시킨 객체를 반환하는 로직 구현

--- src/main/java/racingcar/domain/PlayCount.java
+++ src/main/java/racingcar/domain/PlayCount.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class PlayCount {
     private static final int MINIMUM_VALUE = 1;
 
@@ -14,5 +16,26 @@ public class PlayCount {
             throw new IllegalArgumentException("시도 횟수는 " + MINIMUM_VALUE + "이상이어야 합니다. 주어진 값은 " + value + "입니다.");
         }
         return new PlayCount(value);
+    }
+
+    public PlayCount play() {
+        return new PlayCount(value - 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PlayCount playCount = (PlayCount) o;
+        return value == playCount.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/racingcar/domain/PlayCountTest.java
+++ src/test/java/racingcar/domain/PlayCountTest.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,5 +13,12 @@ class PlayCountTest {
     void 시도_횟수가_0이하면_예외가_발생한다(int value) {
         assertThatThrownBy(() -> PlayCount.from(value))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 게임을_진행하면_값이_1만큼_줄어든_객체가_반환된다() {
+        PlayCount playCount = PlayCount.from(2);
+
+        assertThat(playCount.play()).isEqualTo(PlayCount.from(1));
     }
 }
