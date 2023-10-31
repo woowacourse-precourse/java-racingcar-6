c671d3e4e7750cd73a107ec733d2273eafecb3f6
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 21:34:52 +0900

    feat : User에게 Input받는 기능 추가

--- src/main/java/racingcar/domain/UserInputer.java
+++ src/main/java/racingcar/domain/UserInputer.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInputer {
 
+    Validator validator = new Validator();
+
     public String inputCarString() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
@@ -11,12 +13,15 @@ public class UserInputer {
         return userInput;
     }
 
-    public String inputTryCount() {
+    public int inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String userInput = Console.readLine();
 
-        return userInput;
+        validator.validateIsNumber(userInput);
+        int number = validator.parseStringToInteger(userInput);
+
+        return number;
     }
 
-    
+
 }
