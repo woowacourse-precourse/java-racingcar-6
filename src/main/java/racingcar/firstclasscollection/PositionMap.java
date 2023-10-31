c165fa896af31c5347468f336cc2ea258f178eae
zangsu <zangsu_@naver.com>
2023-10-31 14:33:53 +0900

    refactor : remove all variable except only one Collection

--- src/main/java/racingcar/firstclasscollection/PositionMap.java
+++ src/main/java/racingcar/firstclasscollection/PositionMap.java
@@ -9,13 +9,10 @@ import racingcar.constant.ExceptionMessage;
 import racingcar.model.Car;
 
 public class PositionMap {
-    private static final int INIT_POSITION = 0;
     private Map<Car, Integer> positionMap;
-    private int winnerPosition;
 
     public PositionMap(Supplier<Map> mapSupplier) {
         positionMap = mapSupplier.get();
-        winnerPosition = INIT_POSITION;
     }
 
     /**
@@ -23,6 +20,7 @@ public class PositionMap {
      * @throws IllegalArgumentException - 중복된 Car 객체를 저장 시도시
      */
     public void putCar(Car car) {
+        final int INIT_POSITION = 0;
         if (positionMap.containsKey(car)) {
             throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_DUPLICATED);
         }
@@ -37,19 +35,28 @@ public class PositionMap {
     public void moveCar(Car car) {
         int nextPosition = positionMap.get(car) + 1;
         positionMap.put(car, nextPosition);
-        if (winnerPosition < nextPosition) {
-            winnerPosition = nextPosition;
-        }
     }
 
     /**
      * @return List of Winner Car instance
      */
     public List<Car> getWinners() {
+        int winnerPosition = getWinnerPosition();
+        return getCarsInPosition(winnerPosition);
+    }
+
+    private List<Car> getCarsInPosition(int position) {
         return positionMap.entrySet()
                 .stream()
-                .filter(entry -> entry.getValue() == winnerPosition)
+                .filter(entry -> entry.getValue() == position)
                 .map(Entry::getKey)
                 .collect(Collectors.toList());
+    }
+
+    private int getWinnerPosition() {
+        return positionMap.values()
+                .stream()
+                .max(Integer::compareTo)
+                .get();
     }
 }
