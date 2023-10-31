fa19f2c33c9864354850bc93f75642d5945857d4
pwrwpw <dlgustjr9912@gmail.com>
2023-10-31 18:02:49 +0900

    fix(ExceptionMessages): 사용하지 않는 예외 삭제

--- src/main/java/racingcar/constants/ExceptionMessages.java
+++ src/main/java/racingcar/constants/ExceptionMessages.java
@@ -5,7 +5,6 @@ public enum ExceptionMessages {
     CAR_NAME_LIMIT_EXCEEDED("자동차 이름은 1자 이상 5자 이하만 가능합니다."),
     NOT_INPUT_CARS_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
     CARS_LESS_THAN_TWO("자동차 이름은 2개 이상이어야 합니다."),
-    INVALID_CHARACTER_INPUT_ERROR("문자,쉼표만 입력 가능 합니다."),
     INVALID_INPUT_ERROR_MESSAGE("유효하지 않은 입력입니다. 0보다 큰 숫자만 입력 가능합니다.");
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
