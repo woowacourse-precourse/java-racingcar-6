a19b97fdcab64c524331124a962d5cc7b0ed1cb1
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-31 16:18:38 +0900

    feat : 통합 테스트 케이스 추가
    - 공동 우승 케이스
    - 이름 중복 입력 예외 처리 케이스
    - 횟수 문자열 입력 에외 처리 케이스
    - 횟수 음수값 입력 예외 처리 케이스

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -22,6 +22,16 @@ class ApplicationTest extends NsTest {
             MOVING_FORWARD, STOP
         );
     }
+    @Test
+    void 포비_크롱_공동_우승(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,crong", "3");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi, crong");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
 
     @Test
     void 이름에_대한_예외_처리() {
@@ -30,6 +40,32 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void 이름_중복에_대한_예외_처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,crong,pobi","2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("중복된 이름이 있습니다!")
+        );
+    }
+    @Test
+    void 횟수_입력중_정수가_아닌값에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,crong", "pobi"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("양의 정수를 입력해주세요!")
+        );
+    }
+    @Test
+    void 횟수_입력중_양수가_아닌값에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaj", "-3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("양의 정수를 입력해주세요!")
+        );
+    }
+
+
 
     @Override
     public void runMain() {
