729ed2d7df3bed8e2b0810ae2bb4bb9a8f49651b
jaegyeongkim <dongji_11@naver.com>
2023-10-31 12:13:19 +0900

    fix(InputView): 출력 메시지 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import utils.Utils;
 
 public class InputView {
-    static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.";
+    static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     static final String INPUT_TRY_COUNT = "시도할 횟수는 몇회인가요?";
     Utils utils = new Utils();
 
@@ -17,6 +17,7 @@ public class InputView {
     public int requestTryCount() {
         System.out.println(INPUT_TRY_COUNT);
         String inputTryCount = Console.readLine();
+        System.out.println();
         int tryCount = utils.convertStringToInt(inputTryCount);
         utils.isPositiveNumber(tryCount);
         return tryCount;
