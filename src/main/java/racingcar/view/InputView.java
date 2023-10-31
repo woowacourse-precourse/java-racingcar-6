38e0be4c74a659ee60f8f0414a4d3c84729b3fda
minisyu <iswater.kim@gmail.com>
2023-10-31 11:45:14 +0900

    refactor(InputView): StdReader와 StdWriter 추가, InputValidator 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,37 +1,32 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.converter.InputConverter;
 import racingcar.domain.game.car.Cars;
-import racingcar.util.InputValidator;
+import racingcar.io.reader.StdReader;
+import racingcar.io.writer.StdWriter;
 
 public class InputView {
     private final String INPUT_CAR_NAMES_MESSSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String INPUT_ALL_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
-    private final InputValidator inputValidator;
+    private final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final StdReader reader;
+    private final StdWriter writer;
     private final InputConverter inputConverter;
 
-    public InputView(InputValidator inputValidator, InputConverter inputConverter) {
-        this.inputValidator = inputValidator;
+    public InputView(StdReader reader, StdWriter writer, InputConverter inputConverter) {
+        this.reader = reader;
+        this.writer = writer;
         this.inputConverter = inputConverter;
     }
 
     public Cars getCarsFromUserInput() {
-        System.out.println(INPUT_CAR_NAMES_MESSSAGE);
-        String input = readLine();
-        inputValidator.validateInputFormat(input);
-        inputValidator.validateNonEmpty(input);
-        return inputConverter.convertStringToCars(input);
+        writer.writeLine(INPUT_CAR_NAMES_MESSSAGE);
+        String input = reader.readLine();
+        return inputConverter.toCars(input);
     }
 
     public int getTrialFromUserInput() {
-        System.out.println(INPUT_ALL_ROUND_MESSAGE);
-        String input = readLine();
-        inputValidator.validateNumericString(input);
-        return inputConverter.covertStringToInteger(input);
-    }
-
-    private String readLine() {
-        return Console.readLine().trim();
+        writer.writeLine(INPUT_TRIAL_MESSAGE);
+        String input = reader.readLine();
+        return inputConverter.toTrial(input);
     }
 }
