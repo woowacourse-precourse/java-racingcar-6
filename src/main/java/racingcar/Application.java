3ee696ee6af49f91ec2229fbe7569495037c9681
ddangme <ssilver121@nav>
2023-10-31 22:22:33 +0900

    refactor: 분류된 service에 맞게 controller 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,9 @@
 package racingcar;
 
 import racingcar.controller.Controller;
-
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.start();
+        controller.run();
     }
 }
--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,16 +1,40 @@
 package racingcar.controller;
 
-
+import racingcar.domain.Car;
 import racingcar.domain.Game;
+import racingcar.service.CarNameValidationService;
 import racingcar.service.GameService;
+import racingcar.service.InputService;
+import racingcar.service.RaceCountValidationService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
 
+    InputService inputService = new InputService();
     GameService gameService = new GameService();
+    CarNameValidationService carNameValidationService = new CarNameValidationService();
+    RaceCountValidationService raceCountValidationService = new RaceCountValidationService();
 
-    public void start() {
-        gameService.initCarList();
-        gameService.runGame();
-        gameService.choiceWinner();
+    public void run() {
+        initCar();
+        initGame();
+        gameService.playGame();
+        gameService.winnerChoice();
     }
+
+    public void initCar() {
+        String carNames = inputService.inputCarNameList();
+        carNameValidationService.carNameValidation(carNames);
+        gameService.setCarList(carNames);
+    }
+
+    public void initGame() {
+        String raceCountInString = inputService.inputRaceCount();
+        raceCountValidationService.raceCountValidation(raceCountInString);
+        gameService.setGame(raceCountInString);
+    }
+
 }
