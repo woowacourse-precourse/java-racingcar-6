1bd145740d07df18974b3169b6a3aa42e97e03c6
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:17:33 +0900

    Feat: 전체적인 게임 프로세스 기능 추가
    playGame()에 게임이 실행되고 필요한 메서드를 추가
    startGame()에서 실행횟수 만큼 playGame() 실행

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,8 +1,13 @@
 package racingcar.controller;
 
 import static racingcar.view.Input.askCarNames;
-import static racingcar.view.Print.suggestCarName;
+import static racingcar.view.Input.askNumberOfGames;
+import static racingcar.view.Print.showGameWinner;
+import static racingcar.view.Print.showUser;
+import static racingcar.view.constant.ConstPrint.ASK_ATTEMPT;
 import static racingcar.view.constant.ConstPrint.GAME_START;
+import static racingcar.view.constant.ConstPrint.PRINT_RESULT;
+import static racingcar.view.constant.ConstPrint.PRINT_WINNER;
 
 import racingcar.service.CarService;
 
@@ -10,12 +15,22 @@ public class RacingCarGame{
     private CarService carService = CarService.getInstance();
     private String cars;
     public void startGame(){
-        suggestCarName(GAME_START);
+        showUser(GAME_START);
         cars = askCarNames();
         carService.parkParkingLot(cars);
+        showUser(ASK_ATTEMPT);
+        int Count = askNumberOfGames();
+        showUser(PRINT_RESULT);
+        while(Count >0){
+            playGame();
+            Count--;
+        }
+        showGameWinner(PRINT_WINNER, carService.getWinner());
     }
 
     private void playGame(){
-
+        carService.generateRandomNumbers();
+        carService.confirmRandomNumbers();
+        carService.callResultMethod();
     }
 }
