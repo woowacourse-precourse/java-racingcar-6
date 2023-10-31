6f6ef74ec252fcbe341663d68183265465f23a99
flame-possible <werq2003@naver.com>
2023-11-01 00:27:03 +0900

    feat : 경기 진행, 비교 기능 완료

---
+++ src/main/java/racingcar/domain/Comp.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comp {
+
+    public void CompCars(List<Car> cars, List<Car> winner){
+
+        int max = FindMaxValue(cars);
+
+        for(Car car : cars){
+            if(max == car.cnt){
+                winner.add(car);
+            }
+        }
+
+    }
+
+    private int FindMaxValue(List<Car> cars){
+
+        int max = 0;
+
+        for(Car car : cars){
+            if(max < car.cnt){
+                max = car.cnt;
+            }
+        }
+
+        return max;
+    }
+
+
+}
---
+++ src/main/java/racingcar/domain/Race.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class Race {
+
+    public void Racing(List<Car> cars, int GameNum){
+
+        for(int i = 0; i < GameNum; i++){
+            for(Car car : cars){
+                WinCar(car);
+            }
+        }
+
+    }
+
+    private void WinCar(Car car){
+        int randnum = Randoms.pickNumberInRange(0,9);
+
+        if(randnum >= 4){
+            car.cnt++;
+        }
+
+    }
+
+}
