f4689a2a629f8a85c676b33ac64a8b2ee766b112
lzlkolo <lzlko1209@gmail.com>
2023-10-31 21:12:45 +0900

    refactor: Remove unused files

--- src/main/java/racingcar/CarGenerator.java
+++
@@ -1,7 +0,0 @@
-package racingcar;
-
-public class CarGenerator {
-    public static Car createCar(String name) {
-        return new Car(name);
-    }
-}
--- src/main/java/racingcar/Judgment.java
+++
@@ -1,7 +0,0 @@
-package racingcar;
-
-public class Judgment {
-    public boolean isMoveOrStop(int randomNumber) {
-        return randomNumber >= 4;
-    }
-}
