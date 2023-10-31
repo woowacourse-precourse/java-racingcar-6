89c77dc6c52823bac9ba8e787160b7a1c8b421ed
Jun Hyeok Choi <chrisgold@naver.com>
2023-10-31 18:16:54 +0900

    입력부 기능구현 : 잘못된 입력 예외처리

--- docs/README.md
+++ docs/README.md
@@ -3,19 +3,19 @@
 ![VIEW MAP](Function_Implementation_Map(RacingCar_Main).jpg)
 
 1. RacingCar 클래스 구현
-- 속성 : 이름(name),  현재위치(loc)
-- 함수
-    - 생성자 : name을 입력받아서 객체를 생성할 수 있게함
-    - getLoc : 현재 위치를 반환하는 함수 구현
-    - reset : 현재의 위치를 0으로 리셋하는 함수 구현
-    - accel : 확률을 통해 랜덤으로 이동하는 함수 구현
+   - 속성 : 이름(name),  현재위치(loc)
+   - 함수
+       - 생성자 : name을 입력받아서 객체를 생성할 수 있게함
+       - getLoc : 현재 위치를 반환하는 함수 구현
+       - reset : 현재의 위치를 0으로 리셋하는 함수 구현
+       - accel : 확률을 통해 랜덤으로 이동하는 함수 구현
 
 2. RacingCenter클래스 구현
-- 속성 : List<RacingCar> Cars
-- 함수
-    - all_accel : 카센터 등록 차 모두 악셀(1회 시도)
-    - print_locs : 카센터 등록차로 부터 현재 위치를 받아 실행결과처럼 출력
-    - compare : 위치 비교 후 우승자 List<RacingCar> 반환
+   - 속성 : List<RacingCar> Cars
+   - 함수
+       - all_accel : 카센터 등록 차 모두 악셀(1회 시도)
+       - print_locs : 카센터 등록차로 부터 현재 위치를 받아 실행결과처럼 출력
+       - compare : 위치 비교 후 우승자 List<RacingCar> 반환
 
 3. Main 클래스
     - RacingCenter 객체 생성
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,8 +2,47 @@ package racingcar;
 import  camp.nextstep.edu.missionutils.Randoms;
 import  camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // main
+
+        int i=0, cnt; // idx용 변수 i, 경주횟수 cnt변수
+
+        /* 입력부 구현 */
+        // 자동차 이름 입력
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String temp = Console.readLine();
+        List<String> names = Arrays.asList(temp.split(",")); // pobi,woni,jun
+
+        // 자동차 이름 잘못 입력 시 처리
+        for (String name : names) {  // 이름 입력 예외 처리
+            name = name.trim();      // 좌우 공백 제거, 중간의 공백은 글자수로 포함.
+            names.set(i++, name);    // 공백 제거 글자로 원본 변경
+            if (name.length() > 5 || name.length() < 1 ) {
+                throw new IllegalArgumentException("자동차이름은 1자 이상, 5자 이하로 적어주십시오\n");
+            }
+        }
+
+        // 시도 횟수 입력
+        System.out.print("시도할 회수는 몇회인가요?\n"); // 5
+        temp = Console.readLine();
+
+        // 시도 횟수 잘못 입력 시 처리
+        try{
+            cnt = Integer.parseInt(temp);  // 정수변환이 안되면 예외발생
+            if(cnt < 1)                    // 1이상의 양의 정수만 입력 받음
+                throw new IllegalArgumentException();
+        }catch(Throwable t){
+            throw new IllegalArgumentException("양의 정수를 입력하십시오\n");
+        }
+
+        // 테스트용 간단 출력문
+        for (String name : names){System.out.print(name + "\n");}
+        System.out.print(cnt);
     }
 }
+
+
