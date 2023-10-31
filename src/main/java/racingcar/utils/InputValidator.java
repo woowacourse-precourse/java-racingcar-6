720b4dd83308c4bd8ac85556d54793a7ad151477
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-01 05:32:45 +0900

    Refactor: InputValidator 메서드 반환타입 수정

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -6,16 +6,17 @@ import java.util.Arrays;
 
 public class InputValidator {
     private final String error = "올바르지 않은 입력값입니다.";
-    public void validateTimes(String timesInput) {
-        if(timesInput.matches("^[0-9]$")) return;
-        throw new IllegalArgumentException(error);
+    public boolean validateTimes(String timesInput) {
+        if(timesInput.matches(".*\\D.*")) throw new IllegalArgumentException(error);
+        return true;
     }
 
-    public void validateCars(String carsInput){
+    public boolean validateCars(String carsInput){
         String[] carsArr = carsInput.split(",");
         long max_length = Arrays.stream(carsArr)
                 .filter(car->car.length()>5 || car.startsWith(" ") || car.endsWith(" "))
                 .count();
         if(max_length>0) throw new IllegalArgumentException(error);
+        return true;
     }
 }
