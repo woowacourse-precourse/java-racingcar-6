f24ce66c9de148cf5178216dac66ffbb39646e34
seastamp <seastamp10@gmail.com>
2023-10-31 16:09:06 +0900

    feat: 0~9사이의 랜덤한 상수 생성기능 구현

--- src/main/java/racingcar/controller/NumberGenerator.java
+++ src/main/java/racingcar/controller/NumberGenerator.java
@@ -1,7 +1,14 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
-    public static int generateRandomNumber() {
-        return 1;
+    private NumberGenerator() {
+    }
+
+    public static int generateRandom() {
+        int startInclusive = 0;
+        int endInclusive = 9;
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
     }
 }
