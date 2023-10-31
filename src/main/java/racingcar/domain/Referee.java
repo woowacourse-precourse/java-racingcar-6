b909c28a5af480328c662d16ee904fa800db66bb
carol <carol072400@gmail.com>
2023-10-31 21:07:26 +0900

    feat(racingcar): 우승자를 구하는 기능 구현

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,12 @@ public class Referee {
     public List<String> winner(Map<String, Integer> raceResult) {
 
         List<String> winnerNames = new ArrayList<String>();
+        int max = (Collections.max(raceResult.values()));
+        for (Map.Entry<String, Integer> m : raceResult.entrySet()) {
+            if(m.getValue() == max) {
+                winnerNames.add(m.getKey());
+            }
+        }
         return winnerNames;
     }
 }
