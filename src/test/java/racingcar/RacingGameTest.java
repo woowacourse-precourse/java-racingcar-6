b03d76a1cd11a4ad8365be56062f9e670d004177
2021110806 <2021110806@dgu.ac.kr>
2023-10-31 10:36:26 +0900

    test(RacingGameTest.java):add getCurrentStatus test 차수별 실행 결과를 만드는 기능을 테스트하는 코드를 구현하였습니다.

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -3,16 +3,18 @@ package racingcar;
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RacingGameTest {
 
     final int ENOUGH_BIG_NUMBER = 10000;
-    RacingGame racingGame = new RacingGame();
     RandomNumberMaker randomNumberMaker = new RandomNumberMaker();
     @Test
     void canCreateCars() throws NoSuchFieldException, IllegalAccessException {
+        RacingGame racingGame = new RacingGame();
         String[] answer = {"car1", "car2", "car3"};
         String[] result = new String[3];
         racingGame.createCars(answer);
@@ -38,10 +40,35 @@ public class RacingGameTest {
 
     @Test
     void canMoveCars() throws NoSuchFieldException {
+        RacingGame racingGame = new RacingGame();
         Car car = new Car("car1");
         racingGame.moveCar(car, 8);
         racingGame.moveCar(car, 2);
         racingGame.moveCar(car, 7);
         assertEquals(2,car.getLocation());
+    }
+
+    @Test
+    void canGetCurrentStatus() throws NoSuchFieldException, IllegalAccessException {
+        RacingGame racingGame = new RacingGame();
+        LinkedHashMap<String, Integer> answer = new LinkedHashMap<String, Integer>();
+        String[] carNames = {"car1", "car2", "car3"};
+        racingGame.createCars(carNames);
+        Field cars = racingGame.getClass().getDeclaredField("cars");
+        cars.setAccessible(true);
+        answer.put("car1", 3);
+        answer.put("car2", 5);
+        answer.put("car3", 2);
+        ArrayList<Car> carSet = (ArrayList<Car>) cars.get(racingGame);
+        for(int moveTimes = 0; moveTimes < 3; moveTimes++) {
+            racingGame.moveCar(carSet.get(0), 9);
+        }
+        for(int moveTimes = 0; moveTimes < 5; moveTimes++) {
+            racingGame.moveCar((Car) carSet.get(1), 9);
+        }
+        for(int moveTimes = 0; moveTimes < 2; moveTimes++) {
+            racingGame.moveCar((Car) carSet.get(2), 9);
+        }
+        assertEquals(answer, racingGame.getCurrentStatus());
     }
 }
