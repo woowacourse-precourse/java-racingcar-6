09ad88c448cdef825038b450eb655a3ce29bdaaa
PARKHWANYEONG <welcome0828@naver.com>
2023-10-31 23:43:27 +0900

    refactor(validator): 사용자에게 입력받는 메시지 수정
    - 회수를 -> 횟수로 오타 정정하였습니다.

--- src/main/java/racingcar/constants/InputViewConstants.java
+++ src/main/java/racingcar/constants/InputViewConstants.java
@@ -2,5 +2,5 @@ package racingcar.constants;
 
 public class InputViewConstants {
     public static final String ENTER_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    public static final String ENTER_ATTEMPT_COUNT_MESSAGE  = "시도할 회수는 몇회인가요?";
+    public static final String ENTER_ATTEMPT_COUNT_MESSAGE  = "시도할 횟수는 몇회인가요?";
 }
