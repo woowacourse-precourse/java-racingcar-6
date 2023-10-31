91c5343f41ba5b36278c77b7f73e18928d283f39
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-31 19:16:01 +0900

    refactor(NumberGenerator): 자동차 갯수만큼 무작위 숫자 생성하는 기능 NumberGenerator로 이동

--- src/main/java/racingcar/utils/NumberGenerator.java
+++ src/main/java/racingcar/utils/NumberGenerator.java
@@ -1,5 +1,8 @@
 package racingcar.utils;
 
+import java.util.List;
+
 public interface NumberGenerator {
     int generate();
+    List<Integer> generateNumbers(int size);
 }
--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,6 +1,9 @@
 package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomNumberGenerator implements NumberGenerator{
     private static final int MINIMUM_NUMBER_RANGE = 0;
@@ -18,5 +21,12 @@ public class RandomNumberGenerator implements NumberGenerator{
 
     public int generate() {
         return Randoms.pickNumberInRange(MINIMUM_NUMBER_RANGE, MAXIMUM_NUMBER_RANGE);
+    }
+
+    public List<Integer> generateNumbers(int size) {
+        return IntStream.generate(() -> this.generate())
+                .limit(size)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
