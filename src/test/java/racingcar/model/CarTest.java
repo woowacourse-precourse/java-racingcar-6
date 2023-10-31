1fd21bcf992d47ed2e025034dec560affdd895f3
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-31 20:23:49 +0900

    2023.10.31_test : CarTest 자동차 생성 실패 테스트 코드 작성

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,7 +1,11 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
@@ -13,5 +17,11 @@ public class CarTest {
         Car car = new Car(name);
         // then
         assertThat(car).isNotNull();
+    }
+
+    @ParameterizedTest // 여러 입력 값을 받아서 여러 번 실행
+    @ValueSource(strings = {"" , "123456", ""})
+    public void 자동차_생성_실패(String name) {
+        assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
     }
 }
