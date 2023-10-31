e4071383c9978caf2413c43a874935500944f7f2
Juho <jeuslee99@naver.com>
2023-11-01 04:14:19 +0900

    feat:add README.md

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+1. 입력받은 자동차의 이름이 5자를 초과한다면 try-catch문을 통하여 에러를 판별하고, catch했다면 시스템을 종료시킨다.
+2. GoOrStop 함수를 통해, 0~9 사이의 Random 숫자를 받고 4 이상이라면 전진하도록 한다.
+3. PrintIntermediateResult 함수를 통해, 경주의 중간 결과를 출력 형식에 맞추어 출력한다.
+4. 마지막으로 FindWinner 함수를 통해 우승자를 판별하여 출력한다.
+5. StartRacing 함수 내부에서 GoOrStop, PrintIntermediateResult, FindWinner가 각각 실행된다.
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,8 +15,7 @@ public class Application {
         //CarArray의 입력 check
         try {
             CheckCarArray(CarArray);
-        }
-        catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.exit(0);
         }
 
@@ -30,8 +29,8 @@ public class Application {
     }
 
     public static void CheckCarArray(String[] CarArray) {
-        for(String s: CarArray) {
-            if(s.length() > 5) {
+        for (String s : CarArray) {
+            if (s.length() > 5) {
                 throw new IllegalArgumentException("유효하지 않은 입력입니다.");
             }
         }
@@ -41,12 +40,12 @@ public class Application {
         ArrayList<Integer> ForwardDistance = new ArrayList<>();
 
         //차의 개수만큼 진행도 배열에 개수 추가
-        for(String s: CarArray) {
+        for (String s : CarArray) {
             ForwardDistance.add(0);
         }
 
         System.out.println("실행 결과");
-        for(int i=0; i< racingNum; i++) {
+        for (int i = 0; i < racingNum; i++) {
             //각 사람마다 랜덤함수 돌리고 조건에 따라 전진
             GoOrStop(ForwardDistance);
 
@@ -61,36 +60,42 @@ public class Application {
 
     public static void FindWinner(String[] CarName, ArrayList<Integer> distanceArray) {
         //최댓값 판별
-        int max = -1;
-        for(int d: distanceArray) {
-            if(d > max) {
-                max = d;
-            }
-        }
+        int maxDistance = getMax(distanceArray);
 
         //우승자 출력
         ArrayList<String> Winner = new ArrayList<>();
         System.out.print("최종 우승자 : ");
 
-        for(int i=0; i<CarName.length; i++) {
-            if(distanceArray.get(i) == max) {
+        for (int i = 0; i < CarName.length; i++) {
+            if (distanceArray.get(i) == maxDistance) {
                 Winner.add(CarName[i]);
             }
         }
 
-        for(int i=0; i<Winner.size(); i++) {
-            if(i > 0) {
+        for (int i = 0; i < Winner.size(); i++) {
+            if (i > 0) {
                 System.out.print(", ");
             }
             System.out.print(Winner.get(i));
         }
     }
 
-    public static void PrintIntermediateResult(String[] CarName,ArrayList<Integer> distanceArray) {
-        for(int c = 0; c < CarName.length; c++) {
+    public static int getMax(ArrayList<Integer> distanceArray) {
+        int max = -1;
+        for (int d : distanceArray) {
+            if (d > max) {
+                max = d;
+            }
+        }
+
+        return max;
+    }
+
+    public static void PrintIntermediateResult(String[] CarName, ArrayList<Integer> distanceArray) {
+        for (int c = 0; c < CarName.length; c++) {
             System.out.print(CarName[c] + " : ");
 
-            for(int d=0; d< distanceArray.get(c); d++) {
+            for (int d = 0; d < distanceArray.get(c); d++) {
                 System.out.print("-");
             }
             System.out.println();
@@ -99,11 +104,11 @@ public class Application {
 
     public static void GoOrStop(ArrayList<Integer> distanceArray) {
         //각 진척도를 순회하며 랜덤 값이 4 이상이면 전진
-        for(int j=0; j< distanceArray.size(); j++) {
-            if(Randoms.pickNumberInRange(0,9) >= 4) {
+        for (int j = 0; j < distanceArray.size(); j++) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
                 int distanceValue = distanceArray.get(j);
-                distanceValue +=1;
-                distanceArray.set(j,distanceValue);
+                distanceValue += 1;
+                distanceArray.set(j, distanceValue);
             }
         }
     }
