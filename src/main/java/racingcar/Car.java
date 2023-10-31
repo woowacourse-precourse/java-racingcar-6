1efedc27cd9c525f50d1835acde62692b42f9683
rbgusdlza <rbgusdlza@gmail.com>
2023-10-31 19:25:58 +0900

    feat: Car class 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,18 @@
 package racingcar;
 
 public class Car {
+    final String carName;
+    int moveState;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    String getCarName() {
+        return carName;
+    }
+
+    int getMoveState(){
+        return moveState;
+    }
 }
