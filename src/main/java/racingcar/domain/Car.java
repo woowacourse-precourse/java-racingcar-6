60f0a6b37e2b746b355a7757f2de548e691335d7
정영동 <63975200+Yeongdong@users.noreply.github.com>
2023-10-31 16:26:41 +0900

    refactor: MovableStrategy 멤버 삭제

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,18 +7,16 @@ import java.util.Objects;
 public class Car {
     private final String name;
     private int position;
-    private final MovableStrategy movableStrategy;
 
-    private Car(String name, MovableStrategy movableStrategy) {
+    private Car(String name) {
         this.name = name;
         this.position = GameRules.START_POSITION.getValue();
-        this.movableStrategy = movableStrategy;
     }
 
     public static Car createCar(String name) {
         isNotNull(name);
         isOverSize(name);
-        return new Car(name, RandomMoveStrategy.createStrategy());
+        return new Car(name);
     }
 
     private static void isNotNull(final String carName) {
@@ -42,23 +40,12 @@ public class Car {
     public boolean isWinner(int position) {
         return this.position == position;
     }
+
     public String getName() {
         return name;
     }
+
     public int getPosition() {
         return position;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return position == car.position && Objects.equals(name, car.name) && Objects.equals(movableStrategy, car.movableStrategy);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, position, movableStrategy);
     }
 }
