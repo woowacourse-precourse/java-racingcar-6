3aec5b334db9972d3b1e1c4028ad7dd6befa1f76
Chun Hyunwoo <1000chw@gmail.com>
2023-11-01 05:04:21 +0900

    feat: Race Test code Update

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -1,4 +1,45 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RaceTest {
+    private List<String> input;
+
+    @BeforeAll
+    void setInput() {
+        input = new ArrayList<>();
+        input.add("harry");
+        input.add("ron");
+    }
+
+    @Test
+    void setCarList_차_리스트_생성() {
+        Race race = new Race();
+        List<String> cars = new ArrayList<>();
+
+        race.setCarList(input);
+        for (Car car : race.getCarList()){
+            cars.add(car.getCarName());
+        }
+
+        assertThat(cars).isEqualTo(input);
+    }
+
+    @Test
+    void getRaceResult_레이스_결과_반환() {
+        Race race = new Race();
+        race.setCarList(input);
+        race.setRaceRounds(1);
+        race.playOneRound();
+
+        assertThat(race.getRaceResult()).containsAnyElementsOf(input);
+    }
+
 }
