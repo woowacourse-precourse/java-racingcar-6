783ae5574a44f84860d2a1bc13f16dad5ce82204
K-KY <kyuyoungk@naver.com>
2023-11-01 06:33:52 +0900

    Test(CarsTest) : 객체비교 테스트

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -6,6 +6,10 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.DataTypeChanger;
+import racingcar.view.SystemOutput;
 
 public class CarsTest {
     @Test
@@ -27,5 +31,25 @@ public class CarsTest {
         cars.move();
 
         assertThatCode(() -> SystemOutput.printResult(cars.indicate())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 객체_비교_테스트() {
+        List<String> names = DataTypeChanger.stringToList("123,234");
+        List<Car> carNames = DataTypeChanger.stringToCar(names);
+        carNames.get(0).movable(4);
+        Cars cars = new Cars(carNames);
+        assertThat(cars.indicate().toString()).isEqualTo("[123 : -, 234 : ]");
+        assertThat(cars.compare(carNames.get(0))).isEqualTo(0);
+    }
+    @Test
+    void 객체_비교_테스트_무승부() {
+        List<String> names = DataTypeChanger.stringToList("123,234");
+        List<Car> carNames = DataTypeChanger.stringToCar(names);
+        carNames.get(0).movable(4);
+        carNames.get(1).movable(4);
+        Cars cars = new Cars(carNames);
+        assertThat(cars.indicate().toString()).isEqualTo("[123 : -, 234 : -]");
+        assertThat(cars.compare(carNames.get(0))).isEqualTo(0);
     }
 }
