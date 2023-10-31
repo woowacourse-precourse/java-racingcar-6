9fef7d6ee200cf8ed0df9d1ae254c591905996a2
leeminjun <sohwi0305@gmail.com>
2023-10-31 14:34:03 +0900

    feat: 리스트추가 및 자동차 이름 중복 오류

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -1,13 +1,17 @@
 package racingcar.util;
 
+import java.util.List;
+
 public class Exception {
     private static final int CAR_NAME_LIMIT_MAX = 5;
     private static final String SPACE = " ";
-
+    private final Parser parser = new Parser();
     public void checkCarNamesInput(String input) throws IllegalArgumentException {
+        List<String> carNameList = parser.parseCarNames(input);
         checkLengthAndThrowException(input);
         checkSpaceAndThrowException(input);
         checkEmptyAndThrowException(input);
+        checkCarNameListDuplicationAndThrowException(carNameList);
     }
 
     private void checkSpaceAndThrowException(String carName) {
@@ -35,5 +39,19 @@ public class Exception {
 
     private void throwMaxLengthException() {
         throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MAX + "글자 이하여야 합니다.");
+    }
+
+    private void checkCarNameListDuplicationAndThrowException(List<String> carNameList) {
+        for (int i = 0; i < carNameList.size(); i++) {
+            for (int j = i + 1; j < carNameList.size(); j++) {
+                checkEqualsAndThrowException(carNameList.get(i), carNameList.get(j));
+            }
+        }
+    }
+
+    private void checkEqualsAndThrowException(String carName1, String carName2) {
+        if (carName1.equals(carName2)) {
+            throw new IllegalArgumentException("자동차 이름이 중복입니다.");
+        }
     }
 }
