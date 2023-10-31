37626de6e8a8643ab76284775511deee8cc5930f
Dawin <dawin002@naver.com>
2023-10-31 19:33:28 +0900

    test(RaceTest): 자동차 리스트 생성 기능 테스트 추가
    자동차 리스트 생성 기능 setCarList() 관련 테스트 추가:
    - setCarList_더_많은_이름으로_초기화된_자동차_객체_리스트_생성()

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -6,68 +6,73 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RaceTest {
-//    private Race race;
-//    @BeforeEach
-//    void setUp() {
-//        race = new Race();
-//    }
+    private Race race;
+    private String nameInput = "dawin,woowa,han,tech";
+
+    @BeforeEach
+    void setUp() {
+        race = new Race();
+        race.setCarList(nameInput);
+    }
 
     @Test
     void setCarList_여러_개의_이름으로_초기화된_자동차_객체_리스트_생성() {
         //given
-        String nameInput = "dawin,suji,son,blue";
-
-        Race race = new Race();
+        Race race1 = new Race();
+        String nameInput1 = "dawin,woowa,han,tech";
 
         //when
-        race.setCarList(nameInput);
+        race1.setCarList(nameInput1);
 
         //then
-        assertThat(race.getCarNames()).isEqualTo(List.of("dawin", "suji", "son", "blue"));
-        assertThat(race.getRoundResult()).isEqualTo(List.of(0, 0, 0, 0));
+        assertThat(race1.getCarNames()).isEqualTo(List.of("dawin", "woowa", "han", "tech"));
+        assertThat(race1.getRoundResult()).isEqualTo(List.of(0, 0, 0, 0));
+    }
+
+    @Test
+    void setCarList_더_많은_이름으로_초기화된_자동차_객체_리스트_생성() {
+        //given
+        Race race1 = new Race();
+        String nameInput1 = "dawin,woowa,han,tech,pre,cours";
+
+        //when
+        race1.setCarList(nameInput);
+
+        //then
+        assertThat(race1.getCarNames()).isEqualTo(List.of("dawin", "woowa", "han", "tech", "pre", "cours"));
+        assertThat(race1.getRoundResult()).isEqualTo(List.of(0, 0, 0, 0, 0, 0));
     }
 
     @Test
     void getCarNames_자동차_객체의_이름_리스트로_반환() {
         //given
-        String nameInput = "dawin,suji,blue";
-
-        Race race = new Race();
-        race.setCarList(nameInput);
+        // setUp()의 코드 실행
 
         //when
         List<String> actualCarNames = race.getCarNames();
 
         //then
-        assertThat(actualCarNames).isEqualTo(List.of("dawin", "suji", "blue"));
+        assertThat(actualCarNames).isEqualTo(List.of("dawin", "woowa", "han", "tech"));
     }
 
     @Test
     void getCarListSize_자동차_객체_리스트의_크기_반환() {
         //given
-        String nameInput = "dawin,suji,blue";
-
-        Race race = new Race();
-        race.setCarList(nameInput);
+        // setUp()의 코드 실행
 
         //when
         int actualListSize = race.getCarListSize();
 
         //then
-        assertThat(actualListSize).isEqualTo(3);
+        assertThat(actualListSize).isEqualTo(4);
     }
 
     @Test
     void moveCars_입력된_랜덤_숫자가_4_미만일_경우_자동차_전진하지_않음() {
         //given
-        String nameInput = "a,b,c,d";
         List<Integer> randomNumbers = List.of(0, 1, 2, 3);
-
-        Race race = new Race();
-        race.setCarList(nameInput);
 
         //when
         race.moveCars(randomNumbers);
@@ -79,49 +84,47 @@ class RaceTest {
     @Test
     void moveCars_입력된_랜덤_숫자가_4_이상일_경우_자동차_전진() {
         //given
-        String nameInput = "a,b,c,d,e,f";
-        List<Integer> randomNumbers = List.of(4, 5, 6, 7, 8, 9);
-
-        Race race = new Race();
-        race.setCarList(nameInput);
+        List<Integer> randomNumbers = List.of(4, 5, 6, 9);
 
         //when
         race.moveCars(randomNumbers);
 
         //then
-        assertThat(race.getRoundResult()).isEqualTo(List.of(1, 1, 1, 1, 1, 1));
+        assertThat(race.getRoundResult()).isEqualTo(List.of(1, 1, 1, 1));
     }
 
     @Test
     void getRoundResult_각_자동차의_이동_결과를_리스트로_반환() {
         //given
-        String nameInput = "a,b,c,d";
-
-        Race race = new Race();
-        race.setCarList(nameInput);
-
-        race.moveCars(List.of(3,3,3,4));
-        race.moveCars(List.of(3,3,4,4));
-        race.moveCars(List.of(3,4,4,4));
+        race.moveCars(List.of(3, 3, 3, 4));
+        race.moveCars(List.of(3, 3, 4, 4));
+        race.moveCars(List.of(3, 4, 4, 4));
 
         //when
         List<Integer> roundResult = race.getRoundResult();
 
         //then
-        assertThat(roundResult).isEqualTo(List.of(0,1,2,3));
+        assertThat(roundResult).isEqualTo(List.of(0, 1, 2, 3));
     }
 
     @Test
     void getWinners_이동_거리가_가장_큰_자동차의_이름_반환() {
         //given
-        String nameInput = "dawin,hyein,woowa";
+        race.moveCars(List.of(0, 0, 0, 4));
+        race.moveCars(List.of(0, 0, 4, 4));
+        race.moveCars(List.of(0, 4, 4, 4));
 
-        Race race = new Race();
-        race.setCarList(nameInput);
+        //when
+        List<String> winner = race.getWinners();
 
-        race.moveCars(List.of(0,0,4));
-        race.moveCars(List.of(0,4,4));
-        race.moveCars(List.of(4,4,4));
+        //then
+        assertThat(winner).isEqualTo(List.of("tech"));
+    }
+
+    @Test
+    void getWinners_우승자가_1명인_경우_우승자의_이름만_반환() {
+        //given
+        race.moveCars(List.of(0, 4, 0, 0));
 
         //when
         List<String> winner = race.getWinners();
@@ -131,36 +134,14 @@ class RaceTest {
     }
 
     @Test
-    void getWinners_우승자가_1명인_경우_우승자의_이름만_반환() {
-        //given
-        String nameInput = "lose1,lose2,lose3,win";
-
-        Race race = new Race();
-        race.setCarList(nameInput);
-
-        race.moveCars(List.of(0,0,0,4));
-
-        //when
-        List<String> winner = race.getWinners();
-
-        //then
-        assertThat(winner).isEqualTo(List.of("win"));
-    }
-
-    @Test
     void getWinners_우승자가_여러명인_경우_여러명의_우승자_이름을_반환() {
         //given
-        String nameInput = "win1,lose1,win2,win3,lose2";
-
-        Race race = new Race();
-        race.setCarList(nameInput);
-
-        race.moveCars(List.of(4,0,4,4,0));
+        race.moveCars(List.of(4, 0, 4, 4));
 
         //when
         List<String> winner = race.getWinners();
 
         //then
-        assertThat(winner).isEqualTo(List.of("win1", "win2", "win3"));
+        assertThat(winner).isEqualTo(List.of("dawin", "han", "tech"));
     }
 }
