b7daadf8e8ae8da5bbcb9900a4f3c2214da6e89e
Minju Ji <hinote444@gmail.com>
2023-11-01 04:05:08 +0900

    test(ValidatorTest): 자동차 이름의 길이가 5자 이하인지 검사하는 테스트 구현

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.validator.Validator;
@@ -21,6 +22,28 @@ public class ValidatorTest {
         });
         Throwable result2 = catchThrowable(() -> {
             Validator.validateSeparatedByComma(case2);
+        });
+
+        // then
+        assertThat(result1).isInstanceOf(IllegalArgumentException.class);
+        assertThat(result2).doesNotThrowAnyException();
+    }
+
+    @DisplayName("자동차 이름의 길이가 5자 이하인지 검사")
+    @Test
+    void 자동차_이름_길이_테스트() {
+        // given
+        final String NAME_LENGTH_SIX = "minjun";
+        final String NAME_LENGTH_FIVE = "minju";
+        List<String> case1 = List.of("pobi", "woni", "jun", NAME_LENGTH_SIX);
+        List<String> case2 = List.of("pobi", "woni", "jun", NAME_LENGTH_FIVE);
+
+        // when
+        Throwable result1 = catchThrowable(() -> {
+            Validator.validateCarNameLength(case1);
+        });
+        Throwable result2 = catchThrowable(() -> {
+            Validator.validateCarNameLength(case2);
         });
 
         // then
