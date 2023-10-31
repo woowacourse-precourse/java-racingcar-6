10c42a0b5123ad6e6f2c4d127956e5ca976e08ff
haruday97 <starharu97@naver.com>
2023-10-31 20:55:12 +0900

    test: Cars 테스트케이스 추가
    우승자가 한명일 경우와 여러명일 경우의 테스트케이스 작성

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -16,17 +16,15 @@ public class CarsTest {
     @Test
     @DisplayName("5가 나올경우 이동한다.")
     void move_test() {
-        //G
         NumberGenerator generator = new NumberGenerator(5);
-
         Cars cars = new Cars(List.of(
                 new Car(new CarName("호빵맨"), new CarPosition(5)),
                 new Car(new CarName("식빵맨"), new CarPosition(3)),
                 new Car(new CarName("카레빵맨"), new CarPosition(4))
         ));
-        //W
+
         Cars carsAfterRound = cars.simulateNextRound(generator);
-        //T
+
         assertThat(carsAfterRound).usingRecursiveComparison().isEqualTo(new Cars(List.of(
                 new Car(new CarName("호빵맨"), new CarPosition(6)),
                 new Car(new CarName("식빵맨"), new CarPosition(4)),
@@ -37,17 +35,15 @@ public class CarsTest {
     @Test
     @DisplayName("3이 나올경우 이동하지 않는다.")
     void move_test2() {
-        //G
         NumberGenerator generator = new NumberGenerator(3);
-
         Cars cars = new Cars(List.of(
                 new Car(new CarName("호빵맨"), new CarPosition(5)),
                 new Car(new CarName("식빵맨"), new CarPosition(3)),
                 new Car(new CarName("카레빵맨"), new CarPosition(4))
         ));
-        //W
+
         Cars carsAfterRound = cars.simulateNextRound(generator);
-        //T
+
         assertThat(carsAfterRound).usingRecursiveComparison().isEqualTo(new Cars(List.of(
                 new Car(new CarName("호빵맨"), new CarPosition(5)),
                 new Car(new CarName("식빵맨"), new CarPosition(3)),
@@ -55,4 +51,30 @@ public class CarsTest {
         )));
     }
 
+    @Test
+    @DisplayName("우승자가 한명일 경우 한명이 담긴 리스트를 반환한다.")
+    void single_winner_test() {
+        Cars cars = new Cars(List.of(
+                new Car(new CarName("피카츄"), new CarPosition(24)),
+                new Car(new CarName("꼬부기"), new CarPosition(41)),
+                new Car(new CarName("파이리"), new CarPosition(37))
+        ));
+
+        List<String> winner = cars.getWinners();
+        assertThat(winner.get(0)).isEqualTo("꼬부기");
+    }
+
+    @Test
+    @DisplayName("우승자가 여러명일 경우 여러명이 담긴 리스트를 반환한다.")
+    void multiple_winners_test() {
+        Cars cars = new Cars(List.of(
+                new Car(new CarName("피카츄"), new CarPosition(44)),
+                new Car(new CarName("꼬부기"), new CarPosition(32)),
+                new Car(new CarName("파이리"), new CarPosition(44))
+        ));
+
+        List<String> winner = cars.getWinners();
+        assertThat(winner.get(0)).isEqualTo("피카츄");
+        assertThat(winner.get(1)).isEqualTo("파이리");
+    }
 }
