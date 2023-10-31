4afc48dc25d9632c77acadc3e4f4d70644f60b75
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 06:57:09 +0900

    test : 자동차 전진 테스트 예외코드

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,31 +1,31 @@
-//package racingcar.domain;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class CarTest {
-//    private Car car;
-//    private static final String name = "jun";
-//    @BeforeEach
-//    void setUp(){
-//        car = new Car(name);
-//    }
-//    private static final int DEPART_NUM = 4;
-//    private static final int CANNOT_GO = 3;
-//
-//    @Test
-//    void 수가_랜덤_중_4_이상_일_때_직진(){
-//        car.goForward(DEPART_NUM);
-//        assertThat(car.getForward()).isEqualTo(1);
-//    }
-//
-//    @Test
-//    void 수가_랜덤_중_4가_안_될_때(){
-//        car.goForward(CANNOT_GO);
-//        assertThat(car.getForward()).isEqualTo(0);
-//    }
-//
-//
-//}
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+    private Car car;
+    private static final String name = "jun";
+    @BeforeEach
+    void setUp(){
+        car = new Car(name);
+    }
+    private static final int DEPART_NUM = 4;
+    private static final int CANNOT_GO = 3;
+
+    @Test
+    void 수가_랜덤_중_4_이상_일_때_직진(){
+        car.goForward(DEPART_NUM);
+        assertThat(car.getForward()).isEqualTo(1);
+    }
+
+    @Test
+    void 수가_랜덤_중_4가_안_될_때(){
+        car.goForward(CANNOT_GO);
+        assertThat(car.getForward()).isEqualTo(0);
+    }
+
+
+}
