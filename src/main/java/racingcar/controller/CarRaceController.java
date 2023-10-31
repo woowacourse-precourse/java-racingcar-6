26fc84fcdd9b4be3d08d694a18760353e128493d
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:34:45 +0900

    feat(CarRaceController): 게임 시작 및 입력 구현

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -1,4 +1,18 @@
 package racingcar.controller;
 
+import racingcar.validator.AttemptNumberValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class CarRaceController {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public static void startRace() {
+        List<String> carNames = inputView.inputCarNames();
+
+        int numberOfAttempts = AttemptNumberValidator.validateNumber(inputView.inputNumberOfAttempts());
+    }
 }
