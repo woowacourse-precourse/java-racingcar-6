49727ac9271ee7c5107861be6ab8d809c63a1edd
hoyeon <highoption@naver.com>
2023-10-31 20:29:29 +0900

    feat(OutputView) : 하나의 게임 진행후 결과 값을 출력해줄 printOneGameResult 생성
    하나의 게임을 진행한 후 각각의 나아간 거리를 출력해주는 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,19 @@
 package racingcar.view;
 
+import java.util.stream.IntStream;
+import racingcar.model.Cars;
+
 public class OutputView {
     public static void gameStartMessage() {
         System.out.println("실행 결과");
+    }
+
+    public static void printOneGameResult(Cars cars) {
+        cars.getCars().forEach(car -> {
+            System.out.print(car.getName() + " : ");
+            IntStream.range(0, car.getDistance()).forEach(i -> System.out.print("-"));
+            System.out.println();
+        });
+        System.out.println();
     }
 }
