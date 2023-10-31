3de8251029507990a9404a845ac70a592c005a76
dokkisan <dokkisan.wang@gmail.com>
2023-10-31 22:17:48 +0900

    feat(controller): 입력된 이름의 개수 유효성 검사 구현

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -25,6 +25,7 @@ public class GameController {
 
 	private void setupGame() {
 		String input = inputView.askCarName();
+		validateCarCount(input);
 		names = List.of(input.split(","));
 		cars = setupCars(names);
 		tryCount = Integer.parseInt(inputView.askTryCount());
@@ -72,5 +73,16 @@ public class GameController {
 		}
 
 		return moveResult;
+	}
+
+	private void validateCarCount(String input) {
+		final String ERROR_MESSAGE = "[ERROR] 자동차의 이름을 쉼표(,)로 구분해 2개 이상 입력해 주세요. 예) pobi,woni";
+		final String DELIMITER = ",";
+		final int MIN_DELIMITER_COUNT = 1;
+
+		int delimiterCount = input.length() - (input.replace(DELIMITER, "")).length();
+		if (delimiterCount < MIN_DELIMITER_COUNT) {
+			throw new IllegalArgumentException(ERROR_MESSAGE);
+		}
 	}
 }
