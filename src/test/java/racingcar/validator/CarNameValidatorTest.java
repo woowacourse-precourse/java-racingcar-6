2fddd8c6aa622ed2a65e00ae6f82e764a515b3e0
gomudayya <gomudayya@gmail.com>
2023-10-31 22:36:14 +0900

    Refactor,Test : 자동차 이름 검증 테스트 메서드별로 분할

--- src/test/java/racingcar/validator/CarNameValidatorTest.java
+++ src/test/java/racingcar/validator/CarNameValidatorTest.java
@@ -11,22 +11,31 @@ import static racingcar.constant.ErrorMessage.CAR_NAME_IS_LONGER;
 class CarNameValidatorTest {
 
     @Test
-    void 자동차_이름_검증_테스트() {
+    void 긴_자동차_이름_검증() {
         String longNames = "fobiiiii,geonwoo,park,carr";
         assertThatThrownBy(() -> CarNameValidator.validate(longNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CAR_NAME_IS_LONGER);
+    }
 
+    @Test
+    void 비어있는_자동차_이름_검증() {
         String emptyNames = "fobi,me,,,,,you";
         assertThatThrownBy(() -> CarNameValidator.validate(emptyNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CAR_NAME_IS_EMPTY);
+    }
 
+    @Test
+    void 중복된_자동차_이름_검증() {
         String duplicatedNames = " fobi  ,   fobi    , park";
         assertThatThrownBy(() -> CarNameValidator.validate(duplicatedNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CAR_NAME_IS_DUPLICATED);
+    }
 
+    @Test
+    void 올바른_자동차_이름_확인() {
         String properNames = "fobi,me, you  ";
         CarNameValidator.validate(properNames);
     }
