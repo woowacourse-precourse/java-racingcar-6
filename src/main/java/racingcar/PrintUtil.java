238b9fc5225b1fee1269de433cf94272c6e60650
Chenowoo <cjsgusdn14@naver.com>
2023-11-01 01:49:36 +0900

    refactor(PrintUtil): 실행 결과 출력과 최종 우승자 출력 StringBuilder 분리하고 실행 결과를 출력해주는 기능 분리  - 실행 결과와 최종 우승자 출력에 사용되는 StringBuilder를 분리  - 실행 결과를 출력하는 printFinalResultMsg 기능 분리

--- src/main/java/racingcar/PrintUtil.java
+++ src/main/java/racingcar/PrintUtil.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import static racingcar.MessageConst.*;
 
 public class PrintUtil {
-    private static StringBuilder sb = new StringBuilder();
+    private static StringBuilder finalWinner, gameResult;
 
     public static void printNameOfCarInputMsg() {
         System.out.println(NAMES_OF_CARS_INPUT_MESSAGE);
@@ -18,30 +18,36 @@ public class PrintUtil {
     }
 
     public static void printResultMsg() {
-        sb.append(LINE_BREAK).append(RESULT_MESSAGE).append(LINE_BREAK);
+        gameResult = new StringBuilder();
+        gameResult.append(LINE_BREAK).append(RESULT_MESSAGE).append(LINE_BREAK);
     }
 
     public static void printGameResultMsg(Map<String, String> raceScore) {
 
-        raceScore.forEach((Key, Value) -> sb.append(Key).append(" : ").append(Value).append(LINE_BREAK));
-        sb.append("\n");
+        raceScore.forEach((Key, Value) -> gameResult.append(Key).append(" : ").append(Value).append(LINE_BREAK));
+        gameResult.append("\n");
+    }
+
+    public static void printFinalResultMsg() {
+        System.out.print(gameResult);
     }
 
     public static void printWinnerMsg(Map<String, String> raceScore, List<String> winner) {
-        sb.append(WINNER_MESSAGE);
+        finalWinner = new StringBuilder();
+        finalWinner.append(WINNER_MESSAGE);
 
         String winnerScore = raceScore.get(winner.get(0));
-        sb.append(winner.get(0));
+        finalWinner.append(winner.get(0));
 
         String winnerList = winner.stream()
                 .skip(1)
                 .filter(name -> winnerScore.equals(raceScore.get(name)))
                 .collect(Collectors.joining(", "));
 
-        if(!winnerList.isEmpty()) {
-            sb.append(", ").append(winnerList);
+        if (!winnerList.isEmpty()) {
+            finalWinner.append(", ").append(winnerList);
         }
 
-        System.out.print(sb);
+        System.out.print(finalWinner);
     }
 }
