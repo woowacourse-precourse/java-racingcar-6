ee590a56b588c19d0e5c4e163efa7f40225d58d0
jinkshower <askthem@naver.com>
2023-10-31 14:28:32 +0900

    test(RaceTest): 입력된 회수에 맞게 진행되는지 테스트 추가

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -6,15 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import racingcar.controller.GameController;
 import racingcar.domain.Car;
 
 public class RaceTest extends NsTest{
     private static final int MOVE_FORWARD = 4;
+    private static final int INCLUDE_WINNER_OUTPUT = 5;
     @Test
     void 사용자_입력으로_자동차_생성() {
         assertSimpleTest(() -> {
@@ -31,6 +36,23 @@ public class RaceTest extends NsTest{
                     assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
                 },
                 MOVE_FORWARD,MOVE_FORWARD
+        );
+    }
+
+    @Test
+    void 입력된_회수에_맞게_진행되는지_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "4");
+                    int count = 0;
+                    Pattern pattern = Pattern.compile("pobi");
+                    Matcher matcher = pattern.matcher(output());
+                    while(matcher.find()) {
+                        count++;
+                    }
+                    assertThat(count).isEqualTo(INCLUDE_WINNER_OUTPUT);
+                },
+                MOVE_FORWARD
         );
     }
 
