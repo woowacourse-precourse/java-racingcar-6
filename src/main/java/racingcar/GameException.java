9725a95120832827df07c348320c82719e7ba888
J-petto <hminji4565@gmail.com>
2023-10-31 23:57:26 +0900

    feat: 자동차 갯수 체크 입셉션

--- src/main/java/racingcar/GameException.java
+++ src/main/java/racingcar/GameException.java
@@ -1,9 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class GameException {
     public void NameCount(String carName){
         if(carName.length() > 5){
             throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
+        }
+    }
+
+    public void NameNull(ArrayList<String> allCar){
+        if (allCar.isEmpty()){
+            throw new IllegalArgumentException("1개 이상의 자동차 이름을 입력해주세요.");
         }
     }
 }
