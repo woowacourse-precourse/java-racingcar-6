364ef50016b213b2fb0b86d454162713ad829733
bonooy <choiboyuna@gmail.com>
2023-10-31 12:58:12 +0900

    Feat: 난수 생성/조건에 따른 전진/자동차 위치에 따른 우승자 반환 service 기능 추가

---
+++ src/main/java/racingcar/service/GameService.java
@@ -0,0 +1,56 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.domain.Car;
+import racingcar.domain.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.constant.SystemConstant.*;
+
+public class GameService {
+
+    public void play(Game game) {
+        moveForwardByRandomNumber(game);
+        game.increaseTrialNumber();
+    }
+
+    public List<String> getWinners(Game game) {
+        List<Car> cars = game.getCars();
+        int maxPosition = getMaxPosition(cars);
+        return getMaxPositionCars(cars, maxPosition);
+    }
+
+    private List<String> getMaxPositionCars(List<Car> cars, int maxPosition) {
+        List<String> carNames = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                carNames.add(car.getName());
+            }
+        }
+        return carNames;
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        for (Car car : cars) {
+            if (MAX < car.getPosition()) {
+                MAX = car.getPosition();
+            }
+        }
+        return MAX;
+    }
+
+    private void moveForwardByRandomNumber(Game game) {
+        for (Car car : game.getCars()) {
+            if (FORWARD_CONDITION <= getRandomNumbers()) {
+                car.moveForward(1);
+            }
+        }
+    }
+
+    private int getRandomNumbers() { // 범위 내 난수 생성
+        return Randoms.pickNumberInRange(RANGE_OF_START, RANGE_OF_END);
+    }
+
+}
