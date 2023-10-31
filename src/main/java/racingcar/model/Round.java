25847107e8e17749b5e9173a1102aab194bbdb96
Yeriimii <seojs0511@naver.com>
2023-10-31 15:24:13 +0900

    feat(Round): 라운드가 종료될 때 진행된 라운드 수를 알 수 있다. feat(Round): 라운드 진행

--- src/main/java/racingcar/model/Round.java
+++ src/main/java/racingcar/model/Round.java
@@ -6,10 +6,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Round {
+    private static final int INITIAL_ROUND = 0;
+    private static int currentRoundCount = INITIAL_ROUND;
     private List<CarDTO> roundResult;
 
     public void startRace(List<Car> cars) {
         cars.forEach(Car::startEngine);
+    }
+
+    public void endRace() {
+        currentRoundCount++;
     }
 
     public List<CarDTO> generateResult(List<Car> cars) {
@@ -18,5 +24,9 @@ public class Round {
                 .collect(Collectors.toList());
 
         return roundResult;
+    }
+
+    public static int getCurrentRoundCount() {
+        return currentRoundCount;
     }
 }
