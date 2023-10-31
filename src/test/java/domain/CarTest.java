87bb06f72996cccccc318aca9abef519583ea175
ddohyeong <dohyeong7026@gmail.com>
2023-10-31 14:24:08 +0900

    refactor(CarTest) : 자바 컨벤션에 맞게 공백 리팩토링

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -9,9 +9,11 @@ public class CarTest {
 	@ValueSource(ints = {4, 5, 6, 7, 8, 9})
 	public void 숫자가_4이상_9이하라면_전진한다(int randomNumber) {
 		//given
-		// when
 		Car car = new Car();
+
+		// when
 		car.moveByNumber(randomNumber);
+
 		//then
 		Assertions.assertThat(car.getLocation()).isEqualTo(1);
 	}
