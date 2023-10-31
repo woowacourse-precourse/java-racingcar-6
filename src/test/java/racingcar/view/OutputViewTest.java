49a267620de5f1dd98259dc9b7fdbe91a9eff5b5
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 18:35:12 +0900

    Test: OutputView클래스 printRaceStatus테스트 코드 추가
    Game생성시 움직일 수 있는 Car("Tom", "Jin")객체와 움직일 수 없는 Car("Ive")객체가 주어진 경우
    올바른 레이스 상황을 출력하는지 확인한다.
    OuterNested에서 테스트 시작과 이후 출력스트림을 설정하고,
    InnerNested에서 @BeforeEach를 활용해 게임의 상황을 성정한다

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -3,40 +3,68 @@ package racingcar.view;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Game;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static racingcar.view.OutputView.*;
 
 class OutputViewTest {
+    @Nested
+    class OuterNested {
 
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-    @BeforeEach
-    void setUpStreams() {
-        System.setOut(new PrintStream(outputStream));
-    }
+        @BeforeEach
+        void setUpStreams() {
+            System.setOut(new PrintStream(outputStream));
+        }
 
-    @AfterEach
-    void restoreStreams() {
-        System.setOut(System.out);
-        outputStream.reset();
-    }
+        @AfterEach
+        void restoreStreams() {
+            System.setOut(System.out);
+            outputStream.reset();
+        }
 
-    @Test
-    void 차_이름_입력요청_메시지_출력_테스트() {
-        OutputView.printCarNameRequest();
-        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(CAR_NAME_REQUEST_MESSAGE);
-    }
+        @Test
+        void 차_이름_입력요청_메시지_출력_테스트() {
+            OutputView.printCarNameRequest();
+            Assertions.assertThat(outputStream.toString().trim()).isEqualTo(CAR_NAME_REQUEST_MESSAGE);
+        }
 
 
-    @Test
-    void 시도횟수_입력요청_메시지_출력_테스트() {
-        OutputView.printAttemptCountRequest();
-        Assertions.assertThat(outputStream.toString().trim()).contains(ATTEMPT_COUNT_REQUEST_MESSAGE);
+        @Test
+        void 시도횟수_입력요청_메시지_출력_테스트() {
+            OutputView.printAttemptCountRequest();
+            Assertions.assertThat(outputStream.toString().trim()).contains(ATTEMPT_COUNT_REQUEST_MESSAGE);
+        }
+
+        @Nested
+        class InnerNested {
+            Game game;
+
+            @BeforeEach
+            void setUp() {
+                game = new Game(Arrays.asList(
+                        new Car("Tom", () -> 4),
+                        new Car("Jin", () -> 4),
+                        new Car("Ive", () -> 3)),
+                        1);
+            }
+
+            @Test
+            void 레이스_상황_출력_테스트() {
+                game.carRaceOnce();
+                OutputView.printRaceStatus(game.getCars());
+                Assertions.assertThat(outputStream.toString()).contains("실행 결과", "Tom : -", "Jin : -", "Ive : ");
+            }
+        }
     }
 
 }
