570d1c30aecb3e1c36b2118f85c5091bf8c371fd
emost22 <emoney96@naver.com>
2023-11-01 04:12:13 +0900

    Refactor: Add CarRepository for data

---
+++ src/main/java/racingcar/data/CarRepository.java
@@ -0,0 +1,70 @@
+package racingcar.data;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.util.CarUtil;
+import racingcar.validator.CarInputValidator;
+
+public class CarRepository {
+    private List<Car> carList = new ArrayList<>();
+    private int round;
+
+    public CarRepository() {
+    }
+
+    public void setCarList(String[] carNames) {
+        for (String carName : carNames) {
+            CarInputValidator.checkCarNameLength(carName);
+            carList.add(new Car(carName, CarUtil.FIRST_SCORE));
+        }
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+    public void playRound() {
+        for (int i = 0; i < round; i++) {
+            carList.forEach(car -> {
+                car.move(isPossibleMove());
+            });
+            CarUtil.printNewLine();
+        }
+    }
+
+    public String getWinnerNames() {
+        List<String> winnerList = getWinnerList();
+        StringBuilder winnerNames = new StringBuilder(winnerList.get(0));
+        for (int i = 1; i < winnerList.size(); i++) {
+            winnerNames.append(CarUtil.WINNER_SEPARATOR).append(winnerList.get(i));
+        }
+
+        return winnerNames.toString();
+    }
+
+    private List<String> getWinnerList() {
+        int maxDistance = carList.get(0).getMoveDistance();
+        List<String> winnerList = new ArrayList<>(List.of(carList.get(0).getName()));
+
+        int carListSize = carList.size();
+        for (int i = 1; i < carListSize; i++) {
+            Car car = carList.get(i);
+
+            if (maxDistance < car.getMoveDistance()) {
+                winnerList.clear();
+                maxDistance = car.getMoveDistance();
+                winnerList.add(car.getName());
+            } else if (maxDistance == car.getMoveDistance()) {
+                winnerList.add(car.getName());
+            }
+        }
+
+        return winnerList;
+    }
+
+    private boolean isPossibleMove() {
+        return Randoms.pickNumberInRange(CarUtil.MIN_RANDOM_NUMBER, CarUtil.MAX_RANDOM_NUMBER)
+                >= CarUtil.MIN_POSSIBLE_MOVE_NUMBER;
+    }
+}
--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,86 +1,41 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.data.Car;
+import racingcar.data.CarRepository;
 import racingcar.util.CarUtil;
 import racingcar.validator.CarInputValidator;
 import racingcar.validator.RoundInputValidator;
 
 public class CarService {
-    private List<Car> carList = new ArrayList<>();
-    private int round;
+    private final CarRepository carRepository;
+
+    public CarService() {
+        carRepository = new CarRepository();
+    }
 
     public void start() {
         getCarList();
         getRound();
         playRound();
-        CarUtil.printWinner(getWinnerNames());
+        CarUtil.printWinner(carRepository.getWinnerNames());
     }
 
     private void getCarList() {
         CarUtil.printGetCarList();
         String[] carNames = Console.readLine().split(CarUtil.CAR_NAME_SPLIT);
         CarInputValidator.checkCarNameDuplicate(carNames);
-
-        for (String carName : carNames) {
-            CarInputValidator.checkCarNameLength(carName);
-            carList.add(new Car(carName, CarUtil.FIRST_SCORE));
-        }
+        carRepository.setCarList(carNames);
     }
 
     private void getRound() {
         CarUtil.printGetRound();
         String roundString = Console.readLine();
         RoundInputValidator.checkRoundNumberFormat(roundString);
-
-        round = Integer.parseInt(roundString);
+        carRepository.setRound(Integer.parseInt(roundString));
     }
 
     private void playRound() {
         CarUtil.printResult();
-        while (round-- > 0) {
-            carList.forEach(car -> {
-                car.move(isPossibleMove());
-            });
-            CarUtil.printNewLine();
-        }
-    }
-
-    private String getWinnerNames() {
-        List<String> winnerList = getWinnerList();
-        StringBuilder winnerNames = new StringBuilder(winnerList.get(0));
-        for (int i = 1; i < winnerList.size(); i++) {
-            winnerNames.append(CarUtil.WINNER_SEPARATOR).append(winnerList.get(i));
-        }
-
-        return winnerNames.toString();
-    }
-
-    private List<String> getWinnerList() {
-        int maxDistance = carList.get(0).getMoveDistance();
-        List<String> winnerList = new ArrayList<>(List.of(carList.get(0).getName()));
-
-        int carListSize = carList.size();
-        for (int i = 1; i < carListSize; i++) {
-            Car car = carList.get(i);
-
-            if (maxDistance < car.getMoveDistance()) {
-                winnerList.clear();
-                maxDistance = car.getMoveDistance();
-                winnerList.add(car.getName());
-            } else if (maxDistance == car.getMoveDistance()) {
-                winnerList.add(car.getName());
-            }
-        }
-
-        return winnerList;
-    }
-
-    private boolean isPossibleMove() {
-        return Randoms.pickNumberInRange(CarUtil.MIN_RANDOM_NUMBER, CarUtil.MAX_RANDOM_NUMBER)
-                >= CarUtil.MIN_POSSIBLE_MOVE_NUMBER;
+        carRepository.playRound();
     }
 }
