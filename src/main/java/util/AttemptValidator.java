87aea497d4ae2893d2d47dc42094f7528ddcb20a
leesangjun <hubls@naver.com>
2023-10-31 22:10:47 +0900

    refactor: 오류 메시지 추가

--- src/main/java/util/AttemptValidator.java
+++ src/main/java/util/AttemptValidator.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 
 public class AttemptValidator {
     private static final Pattern ATTEMPT_NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
+    private static final String INVALID_VALUE_MESSAGE = "0이상의 숫자 값이 아닙니다.";
 
     public static void checkValidAttemptNumber(String number) {
         Matcher numberMatcher = ATTEMPT_NUMBER_PATTERN.matcher(number);
 
         if (!numberMatcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
         }
     }
 }
