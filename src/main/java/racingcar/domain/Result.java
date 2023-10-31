a497f86b2ff7dcb2f32efff01fa1e6269c37bbbb
dj <fjzl12@naver.com>
2023-10-31 22:57:44 +0900

    feat(Result): 라운드 별 선두의 거리 구하는 기능 구현

--- src/main/java/racingcar/domain/Result.java
+++ src/main/java/racingcar/domain/Result.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
@@ -13,6 +14,10 @@ public class Result {
 
     public void printWinner() {
 
+    public int getLeaderDistance(List<Car> carList) {
+        int leaderDistance = carList.stream().map(Car::getState).mapToInt(String::length).max().getAsInt();
+        // car 하나씩 빼서 car.getState 가장 긴 것 반환
+        return leaderDistance;
     }
 
 }
