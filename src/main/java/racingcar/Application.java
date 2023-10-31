0c38b1569ede16aaf29d636fd7643ad68c9eaf28
Jiwon Na <jiwonna52@gmail.com>
2023-10-31 20:54:39 +0900

    Refactor : 변경 Application 엔터

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,10 +4,8 @@ import static racingcar.controller.Play.start;
 
 public class Application {
 
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         start();
-
     }
 }
