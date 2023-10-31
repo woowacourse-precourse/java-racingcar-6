d365ffff186a92720784ca31b49a450cbf9cad64
SeungGwa123 <skwan123@naver.com>
2023-10-31 13:18:08 +0900

    Feat: Fix Check.java
    자바 코딩 컨벤션에 맞게 코드를 수정

--- src/main/java/racingcar/Check.java
+++ src/main/java/racingcar/Check.java
@@ -1,33 +1,33 @@
 package racingcar;
 
 public class Check {
-    public static boolean checkCarName(String carlist){
+    public static boolean checkCarName(String carlist) {
         boolean result = true;
         //이름 중간에 공백이 있는 입력값 체크하기 위해서 작성 ( 중간에 공백이 있다면 요구사항에 부합하지 않는 입력값이라 판단)
-        String noBlank = carlist.replace(" ","");
-        if(noBlank.length()!=carlist.trim().length()) result = false;
+        String noBlank = carlist.replace(" ", "");
+        if (noBlank.length() != carlist.trim().length()) result = false;
 
         String[] car_list = noBlank.split(",");
         int comma = countComma(noBlank);
         // 컴마 갯수 +1 == car_list.length() 인지 체크
-        if(comma+1!=car_list.length) result = false;
-        if(!measureNameLength(car_list)) result = false;
+        if (comma + 1 != car_list.length) result = false;
+        if (!measureNameLength(car_list)) result = false;
         return result;
     }
-    public static boolean measureNameLength(String[] car_list){
+    public static boolean measureNameLength(String[] car_list) {
         boolean result = true;
-        for(int i=0; i<car_list.length; i++){
-            if(car_list[i].length()>5||car_list[i].length()<0){
+        for (int i = 0; i < car_list.length; i++) {
+            if (car_list[i].length() > 5 || car_list[i].length() < 0) {
                 result = false;
                 break;
             }
         }
         return result;
     }
-    public static int countComma(String carlist){
-        int result=0;
-        for(int i=0; i<carlist.length(); i++){
-            if(carlist.charAt(i)==',')result++;
+    public static int countComma(String carlist) {
+        int result = 0;
+        for (int i = 0; i < carlist.length(); i++) {
+            if (carlist.charAt(i) == ',') result++;
         }
         return result;
     }
