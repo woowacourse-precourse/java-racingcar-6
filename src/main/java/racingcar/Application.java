d59ca84aa77270da6d761a9ef3bf27f14428d1b3
leedabin2 <12193152@inha.edu>
2023-10-31 13:13:14 +0900

    feat(Application): Implement main application
    Implement main application that sets up the game controller with user input.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.handler.UserInputHandler;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> nameList = UserInputHandler.getNamesFromUser();
+        String names = String.join(", ", nameList);
+
+        GameController gameController = new GameController();
+        gameController.setUp(names);
     }
 }
