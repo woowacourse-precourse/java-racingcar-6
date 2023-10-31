feaaa588cffa35393255b864a0a93fafa4d63555
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 10:51:03 +0900

    [Feat](Result) - 기능 구현

--- src/main/java/racingcar/Result.java
+++ src/main/java/racingcar/Result.java
@@ -1,7 +1,35 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Result {
-    ArrayList<Racer> userArray = new ArrayList<>();
+    ArrayList<Car> userArray = new ArrayList<>();
+
+    Result(ArrayList<Car> userArray) {
+        this.userArray = userArray;
+    }
+
+    public void finalWinner() {
+        List<Integer> countArray = userArray.stream()
+                .map(Car::getCount)
+                .toList();  // [3,2,3] // 꿀팁 감사합니다.
+
+        int maxNumber = Collections.max(countArray); // 가장 큰 값
+
+        List<Integer> result = IntStream.range(0, countArray.size())
+                .filter(i -> countArray.get(i) == maxNumber)
+                .boxed()
+                .toList();
+
+        List<String> finalWinner = result.stream()
+                .map(i -> userArray.get(i).getName())
+                .toList(); // 가장 큰 값이 있는 자동차(들) 출력
+
+        String lastString = String.join(", ", finalWinner);
+
+        System.out.println("최종 우승자 : " + lastString);
+    }
 }
