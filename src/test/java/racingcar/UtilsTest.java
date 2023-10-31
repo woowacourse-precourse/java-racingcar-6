720114d572c4fb488e700a50c16de1cd78e5ac32
bbbang105 <2018111366@dgu.ac.kr>
2023-10-31 23:12:43 +0900

    test : HashMap의 Values 중 최대값을 반환하는 기능 테스트 코드 작성

--- src/test/java/racingcar/UtilsTest.java
+++ src/test/java/racingcar/UtilsTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 import racingcar.utils.Utils;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class UtilsTest {
     @Test
     void 랜덤으로_생성한_숫자가_0과_9사이면_통과() {
@@ -20,5 +22,26 @@ public class UtilsTest {
         //then
         assertThat(result1 > case1 && result1 < case2);
         assertThat(result2 > case1 && result2 < case2);
+    }
+
+    @Test
+    void 해시맵의_밸류들중에서_최댓값이면_통과() {
+        //given
+        HashMap<String,String> case1 = new HashMap<>();
+        HashMap<String,String> case2 = new HashMap<>();
+        HashMap<String,String> case3 = new HashMap<>();
+        case1.put("one","-----"); case1.put("two","----");
+        case2.put("one","-----"); case1.put("two","-----");
+        case3.put("one",""); case3.put("two","");
+
+        //when
+        int result1 = Utils.measureMaxNumInHashMapValues(case1);
+        int result2 = Utils.measureMaxNumInHashMapValues(case2);
+        int result3 = Utils.measureMaxNumInHashMapValues(case3);
+
+        //then
+        assertThat(result1 == 5);
+        assertThat(result2 == 5);
+        assertThat(result3 == 0);
     }
 }
