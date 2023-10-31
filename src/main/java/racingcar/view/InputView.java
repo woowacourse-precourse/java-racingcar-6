5b65bfcf13c91bde0e7bd7119fe6d36467a3baef
realcold0 <realcold0@gmail.com>
2023-11-01 05:50:54 +0900

    feat : 사용자의 입력에 대하여 오류를 처리하는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,13 +11,45 @@ public class InputView {
     public List<String> inputNames() {
         System.out.println(STRING_INPUT_CARNAMES);
         String input = Console.readLine();
+        input = inputStringException(input);
         List<String> names = Arrays.asList(input.split("[,]"));
+        inputNamesException(names);
         return names;
     }
 
     public int inputAttempCount() {
         System.out.println(STRING_INPUT_ATTEMPCOUNT);
         int input = Integer.parseInt(Console.readLine());
+        return input;
+    }
+
+
+    /**
+     * 사용자가 입력한 이름들을 ","로 나눈뒤 5자이하인지 확인
+     *
+     * @param names 입력한 이름 배열
+     */
+    public void inputNamesException(List<String> names) {
+        names.stream().forEach(name -> {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자리 이하만 가능합니다.");
+            }
+        });
+    }
+
+    /**
+     * 사용자가 입력한 이름을 나누기전에 확인하는 메서드
+     *
+     * @param input 사용자가 입력했던 문자열
+     * @return 만약 하나만 입력했다면 ,를 붙여서 반환, 그렇지 않다면 그냥 그대로 반환
+     */
+    public String inputStringException(String input) {
+        if (input.isEmpty()) { //입력없이 엔터만 쳤을때
+            throw new IllegalArgumentException("이름을 입력하세요");
+        }
+        if (!input.contains(",")) { //하나만 입력했을때
+            return input + ",";
+        }
         return input;
     }
 }
