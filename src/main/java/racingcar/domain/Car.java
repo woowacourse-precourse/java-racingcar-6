4f04d6cdbcdce795c59ed23cc8b470e21f480d37
junseoks-99 <angec5645@naver.com>
2023-11-01 03:18:07 +0900

    refactor: 매직넘버 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,21 +2,14 @@ package racingcar.domain;
 
 public class Car extends Vehicle {
 
+    private static final int SAME_RANK = 0;
+
     private final Integer INPUT_RANK;
 
     public Car(String name, Integer INPUT_RANK) {
         this.name = name;
         this.distance = new Distance();
         this.INPUT_RANK = INPUT_RANK;
-    }
-
-    public void printName() {
-        String nameString = this.name + " : ";
-        System.out.print(nameString);
-    }
-
-    public void printDistance() {
-        this.distance.printDistance();
     }
 
     @Override
@@ -28,7 +21,7 @@ public class Car extends Vehicle {
     public int compareTo(Vehicle otherVehicle) {
         Car otherCar = (Car)otherVehicle;
         int compareResult = this.distance.compareTo(otherVehicle.distance);
-        if (compareResult == 0) {
+        if (compareResult == SAME_RANK) {
             return this.INPUT_RANK - otherCar.INPUT_RANK;
         }
         return compareResult;
