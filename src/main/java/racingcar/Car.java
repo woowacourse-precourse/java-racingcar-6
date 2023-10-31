159fe0c6d12eee037bd19e68c904b16ea18ef58c
jiwon1217 <koagjw@gmail.com>
2023-10-31 10:56:15 +0900

    :sparkles: feat: 각 Car가 움직인 결과를 가공하는 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,14 @@ public class Car {
             this.position++;
         }
     }
+
+    public String makeMoveResult() {
+        StringBuilder result = new StringBuilder();
+
+        result.append(this.name + " : ");
+        for (int round = 0; round < this.position; round++) {
+            result.append("-");
+        }
+        return result.toString();
+    }
 }
