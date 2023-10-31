b0cfefce45f5da821fe4f348bbf2568ddee8d745
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-31 17:31:52 +0900

    feat: 자동차 상태 담는 클래스 생성
    자동차의 이름과 전진 상태를 나타내는 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int step;
+
+    public Car(String name, int step) {
+        this.name = name;
+        this.step = step;
+    }
+}
