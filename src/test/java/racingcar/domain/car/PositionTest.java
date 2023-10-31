7b5316bdafa4fafdd47122be28c9cb07eea2ebed
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-31 18:27:05 +0900

    test(PositionTest): 위치 이동 테스트 구현

--- src/test/java/racingcar/domain/car/PositionTest.java
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -13,4 +13,16 @@ public class PositionTest {
         //then
         assertThat(position.getPosition()).isEqualTo(0);
     }
+
+    @Test
+    void 위치_이동_테스트() {
+        //given
+        Position position = new Position();
+
+        //when
+        position.moveForward(7);
+
+        //then
+        assertThat(position.getPosition()).isEqualTo(1);
+    }
 }
