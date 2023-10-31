7bd39b2c6828e7ea6cddb39996923d96b9604e24
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-31 18:06:28 +0900

    refactor(Car): 생성자 private로 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -9,7 +9,7 @@ public class Car {
     private final String name;
     private int position;
 
-    public Car(String name) {
+    private Car(String name) {
         this.name = name;
         this.position = INITIAL_POSITION;
     }
