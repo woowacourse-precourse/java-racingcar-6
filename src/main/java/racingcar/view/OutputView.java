553339116cb0343313d37b4589ad05536a23fe30
Cuzz <rygus9@naver.com>
2023-10-31 22:34:53 +0900

    feat: OutputView에서 인풋 안내 함수 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,19 @@
 package racingcar.view;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.dto.RacingCarDto;
 
 public class OutputView {
+    public static void inputCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static  void  inputTurn() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+    public static void progressGameIntro() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
     public static void gameProgress(List<RacingCarDto> racingCarDtoList) {
         StringBuilder sb = new StringBuilder();
         racingCarDtoList.forEach(racingCarDto -> {
@@ -12,8 +21,9 @@ public class OutputView {
                     + " : "
                     + "-".repeat(Math.max(0, racingCarDto.getDistance())));
         });
+        System.out.println();
     }
     public static void gameResult(List<String> racingCarNames) {
-        System.out.println(String.join(", ", racingCarNames));
+        System.out.println("최종 우승자 : " + String.join(", ", racingCarNames));
     }
 }
