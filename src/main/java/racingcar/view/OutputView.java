20e82981d34d6e77b572fc4208bbd7ad5c2669ec
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-31 22:18:04 +0900

    2023.10.31_feat : OutputView printRoundResult 메서드 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public static void printResultMessage() {
         System.out.println(Constants.ROUND_RESULT);
     }
+    public static void printRoundResult(ArrayList<String> roundResults) {
+        roundResults.forEach(System.out::println);
+        System.out.println();
+    }
 }
