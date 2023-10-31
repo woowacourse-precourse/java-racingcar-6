4782443cb7e4a2894fce474e76ae112e15656a33
nayonsoso <panda0329@naver.com>
2023-11-01 04:00:45 +0900

    feat: 경기를 준비하는 함수 생성

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import constants.Constants;
+import java.util.HashMap;
+import java.util.List;
 import validator.AttemptTimesValidator;
 import validator.NameListValidator;
 import view.InputView;
@@ -11,20 +14,31 @@ public class RaceController {
     private final NameListValidator nameListValidator;
     private final AttemptTimesValidator attemptTimesValidator;
 
+    private HashMap<String, Integer> cars;
+    private int attemptTimes;
+
     RaceController(){
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.nameListValidator = new NameListValidator();
         this.attemptTimesValidator = new AttemptTimesValidator();
+
+        this.cars = new HashMap<>();
+        this.attemptTimes = 0;
     }
 
     private void PrepareRace(){
         outputView.requestCarNames();
-        String carNames = inputView.getUserInput();
-        nameListValidator.validate(carNames);
+        String carNamesInput = inputView.getUserInput();
+        nameListValidator.validate(carNamesInput);
 
         outputView.requestAttemptTimes();
-        String attemptTimes = inputView.getUserInput();
-        attemptTimesValidator.validate(attemptTimes);
+        String attemptTimesInput = inputView.getUserInput();
+        attemptTimesValidator.validate(attemptTimesInput);
+
+        List<String> validatedNames
+                = nameListValidator.splitAndTrim(carNamesInput, Constants.NAME_DELIMITER);
+        validatedNames.forEach( e -> this.cars.put(e, 0));
+        this.attemptTimes = Integer.parseInt(attemptTimesInput);
     }
 }
