2a0a784ab8bf259d2223e0d1cf8a548258f619e6
happyhun <younghun.ai@gmail.com>
2023-10-31 15:09:38 +0900

    feat: 우승자를 찾기 위해 가장 큰 전진 횟수를 가진 Car 리스트를 구하는 기능구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class RacingGame {
@@ -11,9 +12,26 @@ public class RacingGame {
         int attemptCount = ioHandler.inputAttemptCount();
 
         List<Car> cars = Car.createCars(carNames);
+        List<String> winners = getWinners(cars, attemptCount);
 
-        // TODO: List<String> winners = getWinners(cars, attemptCount);
         // TODO: ioHandler.printWinners(winners);
     }
+
+    private List<String> getWinners(List<Car> cars, int attemptCount) {
+        for (int i = 0; i < attemptCount; i++) {
+            // TODO: attemptForward(cars);
+        }
+
+        int maxForwardCount = cars.stream()
+                .max(Comparator.comparingInt(Car::getForwardCount))
+                .map(Car::getForwardCount)
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getForwardCount() == maxForwardCount)
+                .map(Car::getName)
+                .toList();
+    }
+
 
 }
