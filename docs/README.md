e60b581326a11791a8d517705fc67542d2d03d82
dlswns <dlswns8679@naver.com>
2023-10-31 23:56:11 +0900

    feat - 레이싱 함수 기능 개발

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+Car 클래스와 Main 함수를 분리하여 기능을 개발했습니다.
+1) Car 클래스 내의 move 함수: 랜덤으로 0 ~ 9 까지의 숫자를 뽑아서 4 이상이면 앞으로 전진
+2) name과 position에 대한 getter 함수
+3) 메인 함수 내의 validateCarNames: 자동차 이름이 5자보다 긴지 체크
+4) printDash 함수: 전진한 만큼 "-"를 출력하는 함수
+5) getWinners 함수: 자동차의 최종 위치를 보고 우승자를 뽑는 함수
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,103 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
+
+
+
 public class Application {
+    private static void validateCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+    public static String printDash(int num){
+        String dash = "";
+        for(int i = 0; i<num; i++)
+        {
+            dash += "-";
+        }
+        return dash;
+    }
+
+    private static List<String> getWinners(List<Car> cars) {
+        int maxPosition = 0;
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+                winners.clear();
+                winners.add(car.getName());
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(" 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputLine = readLine();
+        String[] carNames = inputLine.split(",");
+        validateCarNames(carNames);
+
+        List<Car> carList = new ArrayList<>();
+        for(String carname : carNames) {
+            carList.add(new Car(carname.trim()));
+        }
+        System.out.println("시도할 회수는 몇회인가요?");
+        Integer tryNumber = Integer.valueOf(readLine());
+
+
+        System.out.println("\n실행 결과");
+        for (int attempt = 0; attempt < tryNumber; attempt++) {
+            for (Car car : carList) {
+                car.move();
+                System.out.println(car.getName() + " : " + printDash(car.getPosition()));
+            }
+            System.out.println();
+        }
+
+        int maxPosition = 0;
+        for (Car car : carList) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+
+        // 우승자 확인
+        List<String> winners = getWinners(carList);
+        System.out.print("최종 우승자 : " + String.join(", ", winners));
     }
 }
