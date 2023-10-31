ad439d6f14ad463a6babc289c990e0e214258797
lcqff <yeonnu82g@gmail.com>
2023-10-31 21:09:23 +0900

    refactor: controller 메소드 분리

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -8,18 +8,34 @@ import racingcar.view.OutputView;
 
 public class RacingCarController {
     public void startGame() {
+        List<Car> cars = inputCarNames();
+        int attempNumber = inputAttempNumber();
+        printResult(attempNumber,cars);
+        printWinners(cars);
+    }
+
+    private List<Car> inputCarNames() {
         OutputView.printStartGameMessage();
         List<String> carstr = InputView.inputCarNames();
         List<Car> cars = RacingCarService.convertCarNames(carstr);
+        return cars;
+    }
+
+    private int inputAttempNumber() {
         OutputView.printAttempNumMessage();
         int attempNum = InputView.inputAttempNum();
+        return attempNum;
+    }
 
+    private void printResult(int attempNumber, List<Car> cars) {
         OutputView.printResultMessage();
-        for (int i = 0; i < attempNum; i++) {
+        for (int i = 0; i < attempNumber; i++) {
             RacingCarService.raceCar(cars);
             OutputView.printAttempResult(cars);
         }
+    }
 
+    private void printWinners(List<Car> cars) {
         List<Car> winners = RacingCarService.calculatWinner(cars);
         OutputView.printWinners(winners);
     }
