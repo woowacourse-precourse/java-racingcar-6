fbb37976985ae93bf9bec3b80de28f03bfd580dc
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-10-31 15:42:31 +0900

    test: CarGame race 메서드 테스트 구현

--- src/test/java/racingcar/domain/CarGameTest.java
+++ src/test/java/racingcar/domain/CarGameTest.java
@@ -2,8 +2,11 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.CarDTO;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -26,5 +29,34 @@ class CarGameTest {
 
         // then
         assertThat(carGame).isNotNull();
+    }
+
+    @DisplayName("CarGame race 테스트")
+    @Test
+    void carGameRaceTest() {
+
+        // given
+        List<String> names = List.of("a", "b", "c");
+        int carSize = names.size();
+        int gameSize = 10;
+        List<CarName> carNames = names.stream()
+                .map(CarName::new)
+                .toList();
+        Cars cars = Cars.createByNames(carNames);
+        CarGameRound carGameRound = CarGameRound.createByString(Integer.toString(gameSize));
+        CarGame carGame = CarGame.create(cars, carGameRound);
+
+        // when
+        CarDTO.ProgressResult status = carGame.race();
+        List<Map<String, Integer>> result = status.getResult();
+
+        // then
+        assertThat(status).isInstanceOf(CarDTO.ProgressResult.class);
+        assertThat(result.size()).isEqualTo(gameSize);
+        for (Map<String, Integer> roundStatus : result) {
+            Set<String> keys = roundStatus.keySet();
+            assertThat(keys.size()).isEqualTo(carSize);
+            assertThat(keys.containsAll(names)).isTrue();
+        }
     }
 }
