119954751a022f7aec64e702a3bc053d39865f76
wonjun <wonjon0930@daum.net>
2023-10-31 21:04:55 +0900

    refactor: wrap Long type numberOfGames variable to new class NumberOfGames

---
+++ src/main/java/racingcar/domain/NumberOfGames.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class NumberOfGames {
+    private final Long numberOfGames;
+
+    public NumberOfGames(Long numberOfGames) {
+        this.numberOfGames = numberOfGames;
+    }
+
+    public Long getNumberOfGames() {
+        return this.numberOfGames;
+    }
+}
