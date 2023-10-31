53e37ee94e9c761bd23962b145b91f941502c39e
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 18:11:34 +0900

    feat : 시도 횟수가 0보다 작은지 확인한다.

--- src/main/java/racingcar/enums/Exceptions.java
+++ src/main/java/racingcar/enums/Exceptions.java
@@ -3,6 +3,7 @@ package racingcar.enums;
 public enum Exceptions {
     CAR_NAME_IS_OVER_5("자동차 이름은 5글자 이하여야 합니다."),
     CAR_NAME_IS_BLANK("빈 자동차 이름은 입력할 수 없습니다."),
+    TRY_NUMBER_IS_UNDER_0("시도 횟수는 0 이상이어야 합니다.")
     ;
 
     private String message;
