4790acda006db4837e2de8b8fae7518361d55317
ay-eonii <naayen@naver.com>
2023-10-31 17:25:12 +0900

    refactor: 자동차 이름 구분 기준 확장성 부여

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final static String SPLIT_SYMBOL = ",";
     public String[] inputCarNames() {
-        return Console.readLine().split(",");
+        return Console.readLine().split(SPLIT_SYMBOL);
     }
 
     public String inputTryCount() {
