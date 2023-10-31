c58926a690c790f9f9b0883d71db1dff13c869d6
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-31 19:19:44 +0900

    refactor: 상수를 enum이 아닌 class로 관리하도록 변경
    enum을 사용할 경우 그냥 class를 사용할 때 보다 부가적인 생성자, getter가 필요하기 때문에 별다른 로직이 필요없는 경우 enum이 아닌 class로 관리하도록 변경

--- src/main/java/racingcar/PrintMessage.java
+++ src/main/java/racingcar/PrintMessage.java
@@ -1,12 +1,8 @@
 package racingcar;
 
-public enum PrintMessage {
-    INPUT_RACING_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_NUMBER_OF_ATTEMPTS("시도할 회수는 몇회인가요?");
+public class PrintMessage {
 
-    private final String message;
+    public static final String INPUT_RACING_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
 
-    PrintMessage(String message) {
-        this.message = message;
-    }
 }
