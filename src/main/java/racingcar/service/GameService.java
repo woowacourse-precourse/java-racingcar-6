f6e0d5c06643bf6512978757bd1b52f556252d17
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-31 19:03:51 +0900

    FEAT / add toCarString()

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -30,4 +30,22 @@ public class GameService {
     public Boolean canMove(int value) {
         return value >= Constant.MIN_CAN_MOVE_VALUE;
     }
+
+    public List<String> toCarString(List<Car> cars) {
+        List<String> oneCarResults = new ArrayList<>();
+
+        for (Car car : cars) {
+            String name = car.getName();
+            String moving = toMovingString(car.getMoving());
+            String result = String.format(Constant.ONE_CAR_RESULT, name, moving);
+
+            oneCarResults.add(result);
+        }
+
+        return oneCarResults;
+    }
+
+    public String toMovingString(int moving_int) {
+        return Constant.MOVING_UNIT.repeat(moving_int);
+    }
 }
