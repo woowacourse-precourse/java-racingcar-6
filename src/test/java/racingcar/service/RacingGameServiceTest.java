00b5f9e9c98fc0336d7e31e034bf5e45b5473ec8
PARKHWANYEONG <welcome0828@naver.com>
2023-10-31 18:41:29 +0900

    docs(test): 코드 주석 추가 및 수정
    - 테스트 숫자 동작 방식을 설명하는 주석을 추가했습니다.

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -1,6 +1,5 @@
 package racingcar.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
@@ -9,7 +8,6 @@ import racingcar.util.TestNumberGenerator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingGameServiceTest {
 
@@ -36,7 +34,7 @@ class RacingGameServiceTest {
     }
 
     /**
-     *  테스트 숫자가 1,6,4,7 이므로
+     *  임의로 생성한 테스트 숫자가 1,6,4,7 이었을 때 (임의의 숫자를 순서대로 생성한다)
      *  자동차가 2개 기준 게임을 2번 반복했을때
      *  1번 자동차는 1 -> 정지 , 4 -> 전진
      *  2번 자동차는 6 -> 전진 , 7 -> 전진 이다.
@@ -59,7 +57,8 @@ class RacingGameServiceTest {
     }
 
     /**
-     *  임의로 생성한 테스트 숫자가 1,6,4,7 이었을 때
+     *  임의로 생성한 테스트 숫자가 1,6,4,7 이었을 때 (임의의 숫자를 순서대로 생성한다)
+     *  자동차가 4개 기준 게임을 1번 반복했을때
      *  첫번째 숫자 1 빼고 나머지는 모두 숫자 4 이상으로
      *  2,3,4번째 자동차만 전진 조건(숫자 4 이상일 경우)에 해당하여
      *  position이 1씩 증가하여 2,3,4번째 자동차가 공동으로 최종 우승하게 된다.
@@ -78,12 +77,19 @@ class RacingGameServiceTest {
         assertThat(winningCarStr).isEqualTo("looloo, woo, hee");
     }
 
+    /**
+     *  임의로 생성한 테스트 숫자가 1,6,4,7 이었을 때 (임의의 숫자를 순서대로 생성한다)
+     *  자동차가 2개 기준 게임을 2번 반복했을때
+     *  첫번째 자동차 hwan은 1 -> stop , 4 -> 전진 결과는 position = 1
+     *  두번째 자동차 looloo은 6 -> 전진 , 7 -> 전진 결과는 position = 2 이므로
+     *  두번째 자동차 looloo가 승리하게 된다.
+     */
     @Test
     void 최종_우승자를_찾는_테스트_우승자가_한_명일_경우() {
         //given
         String[] carNames = {"hwan","looloo"};
         racingGameService.init(carNames);
-        racingGameService.playGame(1);
+        racingGameService.playGame(2);
 
         //when
         String winningCarStr = racingGameService.getWinningCar();
@@ -91,5 +97,4 @@ class RacingGameServiceTest {
         //then
         assertThat(winningCarStr).isEqualTo("looloo");
     }
-
 }
