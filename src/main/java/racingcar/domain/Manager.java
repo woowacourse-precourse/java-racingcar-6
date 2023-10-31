8621517d057bbdabb413aaeb30532669041fb184
706com <ehdgusjdbswl@gmail.com>
2023-10-31 23:09:54 +0900

    refactor : 메서드 분할

--- src/main/java/racingcar/domain/Manager.java
+++ src/main/java/racingcar/domain/Manager.java
@@ -7,59 +7,69 @@ public class Manager {
 
     private List<Car> carList;
 
-    private int maxLine;
+    private List<String> winnerList;
 
-    private int moveCount;
+    private int maxMovingScore;
+
+    private int RemainingCycle;
 
     public Manager(int moveCount) {
         this.carList = new ArrayList<>();
-        this.moveCount = moveCount;
-        this.maxLine = 0;
+        this.RemainingCycle = moveCount;
+        this.maxMovingScore = 0;
     }
 
     public void registerCar(Car car){
         this.carList.add(car);
     }
 
-    public void manageMoveOneCycle(){
+    public void manageOneCycle(){
         for(Car car : carList){
             if(possibleToMove(car)) {
-                car.getChanceToMove();
+                car.tryToGetMovementScore();
             }
-            increaseMaxLine(car.getScore());
+            increaseMaxMovingScore(car.getMovementScore());
         }
         endOneCycle();
     }
 
-    public List<String> resultWinner(){
-        List<String> winner = new ArrayList<>();
+    public List<String> announceWinner(){
+        checkWinner();
+        return winnerList;
+    }
+
+    private void checkWinner(){
+        winnerList = new ArrayList<>();
         for(Car car : carList){
-            if(car.getScore()==maxLine){
-                winner.add(car.getName());
+            if(car.getMovementScore()== maxMovingScore){
+                winnerList.add(car.getName());
             }
         }
-        return winner;
     }
 
     private boolean possibleToMove(Car car){
-        if(car.getRemainMoveCount()>0) {
+        if(car.getRemainingOpportunity()>0) {
             return true;
         }
         return false;
     }
 
-    private void increaseMaxLine(int score){
-        if (maxLine < score) {
-            this.maxLine = score;
+    private void increaseMaxMovingScore(int score){
+        if (maxMovingScore < score) {
+            this.maxMovingScore = score;
         }
     }
 
     private void endOneCycle(){
-        moveCount--;
+        RemainingCycle--;
     }
 
-    public int getMoveCount(){
-        return moveCount;
+    public int getRemainingCycle(){
+        return RemainingCycle;
+    }
+
+    public int getMaxMovingScore(){
+        return maxMovingScore;
     }
 
     public List<Car> getCarList() {
