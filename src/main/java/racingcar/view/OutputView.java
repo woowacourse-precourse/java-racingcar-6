40de69d38bead8756554702ed845abaaa094f2ae
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-31 21:07:27 +0900

    [refactor] : change show car name and position with final winner message method logic

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,17 +2,18 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 import racingcar.domain.Winner;
+import racingcar.utils.Constants;
 
 import java.util.List;
 
 public class OutputView {
 
     public static void showCarNameAndPosition(List<Car> cars) {
-        cars.forEach(car -> System.out.println(car.getName() + " : " + car.resultPosition()));
-        System.out.println("\n");
+        cars.forEach(car -> System.out.println(car.getName() + Constants.SHOW_CAR_RESULT_DELIMITER + car.resultPosition()));
+        System.out.println(Constants.NEXT_LINE);
     }
 
     public static void finalWinnerMessage(Winner winner) {
-        System.out.println("최종 우승자 : " + winner.getWinnerCarNames());
+        System.out.println(Constants.SHOW_WINNER_MESSAGE + winner.getWinnerCarNames());
     }
 }
