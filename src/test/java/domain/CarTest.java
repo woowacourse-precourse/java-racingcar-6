1f634443cb13fd27d78339103fe3b673002b69ca
nohheeyeon <heeyeon1213@gmail.com>
2023-10-31 18:36:52 +0900

    test : printPosition이 정상적으로 출력되는 지 확인하는 테스트 코드 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class CarTest {
 
@@ -24,5 +26,19 @@ public class CarTest {
         Car car = new Car("TestCar");
         car.moveForward(3);
         assertEquals(0, car.getPosition());
+    }
+
+    @Test
+    void printPositionTest() {
+        Car car = new Car("TestCar");
+        car.moveForward(5);
+        car.moveForward(3);
+        car.moveForward(2);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        car.printPosition();
+        System.setOut(System.out);
+        assertEquals("TestCar : -----\n", outContent.toString());
     }
 }
