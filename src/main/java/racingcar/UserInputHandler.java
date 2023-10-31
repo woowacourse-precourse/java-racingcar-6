964bc68fd1e54b2d637f3c7f8e33f99f738e831d
RESD\dkanf <dkanfjgwls@naver.com>
2023-10-31 20:39:03 +0900

    refactor: UserInputHandler 클래스의 getUserCarNames() 안의 검증 함수를 따로 배치, 함수의 역할을 주석으로 표시

--- src/main/java/racingcar/UserInputHandler.java
+++ src/main/java/racingcar/UserInputHandler.java
@@ -12,16 +12,23 @@ public class UserInputHandler {
     public List<String> getUserCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
-        validateInputNotEmpty(userInput);
-        String[] carNames = userInput.split(",");
-        duplicateCarNames(carNames);
-        validateCarNames(carNames);
+        userInputVerification(userInput);
 
+        String[] carNames = userInput.split(",");
         cars.addAll(Arrays.asList(carNames));
 
         return cars;
     }
 
+    // 자동차 이름 검증 함수들
+    private void userInputVerification(String userInput) {
+        validateInputNotEmpty(userInput);
+        String[] carNames = userInput.split(",");
+        duplicateCarNames(carNames);
+        validateCarNames(carNames);
+    }
+
+    // 중복된 입력 검사
     public void duplicateCarNames(String[] userInput) {
         for (int i = 0; i < userInput.length - 1; i++) {
             if (userInput[i].contains(userInput[i + 1])) {
@@ -30,12 +37,14 @@ public class UserInputHandler {
         }
     }
 
+    // 공백 입력 검사
     public void validateInputNotEmpty(String userInput) {
         if (userInput.isEmpty()) {
             throw new IllegalArgumentException("입력이 없습니다.");
         }
     }
 
+    // 5글자 이하 검사
     public void validateCarNames(String[] carNames) {
         for (String carName : carNames) {
             if (carName.length() > 5) {
@@ -44,6 +53,7 @@ public class UserInputHandler {
         }
     }
 
+    // 시도할 횟수 입력
     public long getUserCount() {
         System.out.println("시도할 횟수는 몇회인가요?");
         String userCount = Console.readLine();
@@ -53,6 +63,7 @@ public class UserInputHandler {
         return Long.parseLong(userCount);
     }
 
+    // 횟수가 숫자로 입력되었는지 확인
     private void validateNotLongNumber(String userCount) {
         try {
             Long.parseLong(userCount);
