9e3c168e61d74203b9f9d653cf441fd3954c9d4c
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 23:34:37 +0900

    refactor(domain): 사용할 필요없는 기능 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,18 +5,20 @@ package racingcar.domain;
  * */
 
 import static racingcar.constant.CarConstant.MOVE_COUNT_INITIAL;
-import static racingcar.constant.CarConstant.STANDARD_MOVE_CONDITION_NUMBER;
 
-public class Car implements Comparable<Car> {
-
+public class Car {
 
     private CarName name;
     //변수명 수정 해보기 클래스도 (distance)
     private int moveDistance;
 
-    public Car(CarName carName) {
+    private Car(CarName carName) {
         this.name = carName;
         this.moveDistance = MOVE_COUNT_INITIAL;
+    }
+
+    public static Car from(CarName carName) {
+        return new Car(carName);
     }
 
     public String getName() {
@@ -27,29 +29,7 @@ public class Car implements Comparable<Car> {
         return moveDistance;
     }
 
-    public void moveOrStop(int number) {
-        if (number >= STANDARD_MOVE_CONDITION_NUMBER) {
-            moveDistance++;
-        }
+    public void moveForward() {
+        moveDistance++;
     }
-
-
-    //함수명 네이밍 처리 잘 되었나
-    public int getBiggerRecord(int maxRecord) {
-        return Integer.max(this.moveDistance, maxRecord);
-    }
-
-
-    @Override
-    public int compareTo(Car compareCar) {
-        return -Integer.compare(this.moveDistance, compareCar.moveDistance);
-    }
-
-    public boolean isEqualRecord(int record) {
-        if (this.moveDistance == record) {
-            return true;
-        }
-        return false;
-    }
-
 }
