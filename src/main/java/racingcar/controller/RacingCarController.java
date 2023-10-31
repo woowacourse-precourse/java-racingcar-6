b51604d9d9b0ea9f9328a107d13f0cfb725182b2
Dowon <3120kang@gmail.com>
2023-10-31 23:44:10 +0900

    Refactor(RacingCarController): 접근자 수정

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -9,38 +9,38 @@ import java.util.List;
 
 public class RacingCarController {
 
-    User user=new User();
-    InputView inputView=new InputView();
-    OutputView outputView=new OutputView();
-    Judgment judgment=new Judgment();
-    Random random=new Random();
-    Referee referee=new Referee();
+    private final static int FIRST_CAR = 0;
+    private Judgment judgment = new Judgment();
+    private Random random = new Random();
+    private Referee referee = new Referee();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
 
-    private List<Car> cars=new ArrayList<>();
-    public void start(){
-        cars=user.nameForCar(inputView.inputCars());
-        int count=inputView.inputCount();
+    private List<Car> cars = new ArrayList<>();
+
+    public void start() {
+        User user = new User();
+        cars = user.nameForCar(inputView.inputCars());
+        int count = inputView.inputCount();
         repeat(count);
         outputView.printWinner(referee.announceWinner(referee.decideWinner(cars)));
     }
 
-    private void repeat(int count){
+    private void repeat(int count) {
         System.out.println("실행 결과");
-        for(int num=0;num<count;num++){
+        for (int num = 0; num < count; num++) {
             startForEach();
             System.out.println();
         }
     }
-    
-    private void startForEach(){
-        for(int num=0;num<cars.size();num++){
-            if(judgment.isNumberFourOrMore(random.createNumber())){
 
-                outputView.printForEach(cars.get(num),cars.get(num).moveForward());
+    private void startForEach() {
+        for (int car = FIRST_CAR; car < cars.size(); car++) {
+            if (judgment.isNumberFourOrMore(random.createNumber())) {
+                outputView.printForEach(cars.get(car), cars.get(car).moveForward());
                 continue;
             }
-
-            outputView.printForEach(cars.get(num),cars.get(num).moveStop());
+            outputView.printForEach(cars.get(car), cars.get(car).moveStop());
         }
     }
 }
