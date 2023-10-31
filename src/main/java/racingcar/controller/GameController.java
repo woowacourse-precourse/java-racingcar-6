540bf12fe6ccf9ae492a99cf94b03e5fe6796155
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-31 19:49:13 +0900

    feat: GameController 클래스 생성

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.model.move.RandomMove;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomMove randomMove;
+
+    public GameController(InputView inputView, OutputView outputView, RandomMove randomMove) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.randomMove = randomMove;
+    }
+
+
+}
