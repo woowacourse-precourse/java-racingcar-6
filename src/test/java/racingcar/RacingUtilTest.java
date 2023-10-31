5bf341820a9082c53366188522cfa7fe5d18b07e
Oseuseu <lakingsung@gmail.com>
2023-10-31 16:03:27 +0900

    test: generateRandomPower 테스트 코드 수정

--- src/test/java/racingcar/RacingUtilTest.java
+++ src/test/java/racingcar/RacingUtilTest.java
@@ -2,17 +2,12 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingUtilTest {
     @Test
     void generateRandomPower_테스트() {
-        int result = RacingUtil.generateRandomPower();
-
-        assertThat(result).isBetween(0, 9);
+        assertRandomNumberInRangeTest(() -> assertEquals(5, RacingUtil.generateRandomPower()), 5);
     }
 }
