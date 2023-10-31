de22d56d4fea1a7c4a632da790d4937935346d09
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-01 03:05:39 +0900

    feat(racingCar): 경주차 이름 입력시 예외처리 추가

--- src/main/java/racingcar/domain/CarNameService.java
+++ src/main/java/racingcar/domain/CarNameService.java
@@ -7,25 +7,29 @@ public class CarNameService {
     private final static String partition = ",";
     private final static String exceptionMessageOfNull = "이름이 입력되지 않았습니다.";
     private final static String exceptionMessageOfBlank = "공백을 포함할 수 없습니다.";
-    private final static String exceptionMessageOfLength = "이름의 길이는 5자 이하만 가능합니다.";
+    private final static String exceptionMessageOfLength = "이름의 길이는 1~5자 사이만 가능합니다.";
+    private final static String exceptionMessageOfLastCharacter = "마지막 이름이 입력되지 않았습니다.";
+    private final static String exceptionMessageOfSameName = "이름 중복을 허용하지 않습니다.";
 
     private ArrayList<Car> carList;
+
+    private List<String> nameList;
 
 
     public void input() {
         InputName inputName = new InputName();
         String input = inputName.getInput();
 
-        List<String> nameList = convertStrToList(input);
+        convertStrToList(input);
         makeCarList(nameList);
     }
 
     public List<String> convertStrToList(String str) {
-        List<String> nameList = new ArrayList<>();
+        nameList = new ArrayList<>();
 
         String[] splited = str.split(partition);
         for (String s : splited) {
-            if (checkException(s)) nameList.add(s);
+            if (checkException(s) && (!nameList.contains(s))) nameList.add(s);
         }
 
         return nameList;
@@ -34,7 +38,9 @@ public class CarNameService {
     public boolean checkException(String s) {
         if (s == null) throw new IllegalArgumentException(exceptionMessageOfNull);
         if (s.contains(" ")) throw new IllegalArgumentException(exceptionMessageOfBlank);
-        if (s.length() > 5) throw new IllegalArgumentException(exceptionMessageOfLength);
+        if (s.length() > 5 || s.length() == 0) throw new IllegalArgumentException(exceptionMessageOfLength);
+        if (s.charAt(s.length() - 1) == ',') throw new IllegalArgumentException(exceptionMessageOfLastCharacter);
+        if (nameList.contains(s)) throw new IllegalArgumentException(exceptionMessageOfSameName);
 
         return true;
     }
