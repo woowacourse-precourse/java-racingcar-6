59295a34508736f457f0f08d3769f1a6163f72fa
hajungIm <ihj_ewha@ewhain.net>
2023-11-01 03:10:58 +0900

    Feat(Validator): 입력된 자동차 이름 검증 기능 구현
    쉼표로 구분된 입력에서 최대 글자 수인 5를 넘지 않도록 확인

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,5 +1,14 @@
 package racingcar.validation;
 
+import java.util.List;
+
 public class Validator {
+    public static void validateCarsInput(List<String> cars) {
+        for (String car : cars) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,11 +3,15 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.validation.Validator;
 
 public class InputView {
     public static List<String> inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Arrays.stream(Console.readLine().split(",")).toList();
+        List<String> cars = Arrays.stream(Console.readLine().split(",")).toList();
+
+        Validator.validateCarsInput(cars);
+        return cars;
     }
 
     public static int inputTurn() {
