86b4513bc8f390228175d2a981a9c74a980d77a6
dabin <db021129@naver.com>
2023-10-31 18:58:49 +0900

    feat: Car 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class Car {
 
     private List<String> carName = new ArrayList<String>();
+    private int[] carDistance;
 
     Car(List<String> carName) {
+        int length = carName.size();
+
         this.carName = carName;
+        carDistance = new int[length];
     }
 }
