b1635abd37f560f36182fa7c0c6d197e8962ce27
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 02:45:21 +0900

    feat: 이동 횟수 입력값 숫자 검증 기능 추가

--- src/main/java/racingcar/validation/MoveCountInputValidation.java
+++ src/main/java/racingcar/validation/MoveCountInputValidation.java
@@ -3,6 +3,10 @@ package racingcar.validation;
 public class MoveCountInputValidation {
 
     public void isNumber(String input) {
-        return;
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("움직일 횟수의 입력값이 숫자가 아닙니다.");
+        }
     }
 }
