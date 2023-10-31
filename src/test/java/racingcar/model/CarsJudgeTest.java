aa82a0e4d1db126a9531359bfc10e2da06d8a14d
jihyuk <kwlgur0416@gmail.com>
2023-10-31 16:11:29 +0900

    test : 우승자가 한 명인 경우와 여러 명인 경우를 나눠서 테스트 작성

--- src/test/java/racingcar/model/CarsJudgeTest.java
+++ src/test/java/racingcar/model/CarsJudgeTest.java
@@ -6,37 +6,54 @@ import static racingcar.CarFixtures.JUN;
 import static racingcar.CarFixtures.POBY;
 import static racingcar.CarFixtures.WONY;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarsJudgeTest {
 
-    private Cars cars;
+    private List<Car> carList;
 
     @BeforeEach
     void setUp() {
-        final List<Car> cars = Arrays.asList(
+        carList = List.of(
             POBY.움직인_횟수와_생성(3),
             WONY.움직인_횟수와_생성(2),
-            JUN.움직인_횟수와_생성(1),
-            HYUK.움직인_횟수와_생성(3));
-        this.cars = new Cars(cars);
+            JUN.움직인_횟수와_생성(1));
     }
+
+    @Test
+    void 판정결과_우승자를_반환한다() {
+        // given
+        final Cars cars = new Cars(carList);
+        List<String> expected = List.of("poby");
+
+        // when
+        List<String> result = cars.judge();
+
+        // then
+        expected.forEach(name ->
+            assertThat(result).isEqualTo(expected)
+        );
+    }
+
 
     @Test
     void 판정결과_공동우승자가_있다면_모두_반환한다() {
         // given
+        final ArrayList<Car> updatedCarList = new ArrayList<>(carList);
+        updatedCarList.add(HYUK.움직인_횟수와_생성(3));
+        final Cars cars = new Cars(updatedCarList);
+
         List<String> expected = List.of("poby", "hyuk");
 
         // when
         List<String> result = cars.judge();
 
         // then
-        assertThat(result.size()).isEqualTo(expected.size());
         expected.forEach(name ->
-            assertThat(result.contains(name)).isTrue()
+            assertThat(result).isEqualTo(expected)
         );
     }
 
