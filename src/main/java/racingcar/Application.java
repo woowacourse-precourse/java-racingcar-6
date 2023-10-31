42fc1957a3c4098430ad7d84955a4a46fd647309
yh-skku <112400744+yh-skku@users.noreply.github.com>
2023-10-31 19:20:07 +0900

    feat: 사용자 입력 기능 추가
    이름 쉼표 구분 기능
    이름 수만큼 Car 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,8 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 class Car{
     String name="";
 
@@ -23,18 +26,27 @@ class User{
 }
 
 
+
 public class Application {
     public static void main(String[] args) {
 
-        String inputName="";
-        String inputCount="";
-        int count;
+
+        ArrayList<Car> carList= new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        inputName = Console.readLine();
+        String inputName = Console.readLine();
+        String[] nameArray= inputName.split(",");
+        ArrayList<String> nameList= new ArrayList<>(Arrays.asList(nameArray));
 
         System.out.println("시도할 회수는 몇회인가요?");
-        inputCount=Console.readLine();
-        count=Integer.parseInt(inputCount);
+        String inputCount=Console.readLine();
+        int count=Integer.parseInt(inputCount);
+
+
+        for(int i=0;i<nameList.size();i++){
+            String getName=nameList.get(i);
+            carList.add(new Car(getName));
+            System.out.println("자동차 이름 출력 : "+carList.get(i).name);
+        }
     }
 }
