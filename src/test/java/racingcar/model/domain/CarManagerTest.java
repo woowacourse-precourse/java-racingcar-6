e3742e47a23f78837d0a1e308ee7a39280fc0b0d
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-31 22:54:55 +0900

    CarManagerTest 작성

--- src/test/java/racingcar/model/domain/CarManagerTest.java
+++ src/test/java/racingcar/model/domain/CarManagerTest.java
@@ -2,36 +2,48 @@ package racingcar.model.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class CarManagerTest {
-    private static final CarManager CAR_MANAGER = new CarManager();
+    private static final CarManager cm = new CarManager();
 
     @Test
     void saveCars() {
-        CAR_MANAGER.saveCars("pobi,woni");
-        assertthat
-    }
-
-    @Test
-    void size() {
+        cm.saveCars("pobi,woni");
+        assertThat(cm.size()).isEqualTo(2);
     }
 
     @Test
     void moveCar() {
+        cm.saveCars("pobi");
+        cm.moveCar(0);
+        assertThat(cm.getLocations().get(0)).isEqualTo(1);
     }
 
     @Test
     void getNames() {
+        cm.saveCars("pobi");
+        assertThat(cm.getNames().get(0)).isEqualTo("pobi");
     }
 
     @Test
     void getLocations() {
+        cm.saveCars("pobi");
+        assertThat(cm.getLocations().get(0)).isEqualTo(0);
     }
 
     @Test
     void getMaxLocation() {
+        cm.saveCars("pobi");
+        cm.moveCar(0);
+        assertThat(cm.getMaxLocation()).isEqualTo(1);
     }
 
     @Test
     void getCarsByLocation() {
+        cm.saveCars("pobi");
+        assertThat(cm.getCarsByLocation(0).get(0).getName()).isEqualTo("pobi");
     }
 }
