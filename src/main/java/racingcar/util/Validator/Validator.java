200186ddd4f4c2512e5513fc28b326e9fa5b39ef
jinchiim <nellroll@syuin.ac.kr>
2023-10-31 21:09:28 +0900

    refactor: Validator 수정
    Validator 수정
    - 일부 검증 로직 도메인으로 이동
    - 공통으로 사용하는 비교 로직만 그대로

--- src/main/java/racingcar/util/Validator/Validator.java
+++ src/main/java/racingcar/util/Validator/Validator.java
@@ -1,37 +1,21 @@
 package racingcar.util.Validator;
 
 import java.util.List;
-import racingcar.domain.input.error.InputError;
-import racingcar.domain.input.error.InputException;
+import racingcar.view.input.error.InputError;
+import racingcar.view.input.error.InputException;
 
 public class Validator {
 
-    private static final int MAXIMUM_FORWARD_NUMBER = 4;
-
-    public static final int MAXIMUM_NAME_LENGTH = 5;
-    public static final int MINIMUM_NAME_LENGTH = 1;
-
-
-    private static void validateNameEmpty(List<String> names) {
-        if (names.size() <= 1) {
-            throw new InputException(InputError.NOT_EXCEED_MINIMUM_AMOUNT);
-        }
-    }
+    public static final int MAXIMUM_NAME_LENGTH = 6;
+    public static final int MINIMUM_NAME_LENGTH = 0;
 
     public static void validateNameLength(List<String> names) {
-
-        validateNameEmpty(names);
-
-        long countNames = names.stream()
+        int countNames = (int) names.stream()
                 .filter(name -> name.length() < MAXIMUM_NAME_LENGTH && name.length() != MINIMUM_NAME_LENGTH)
                 .count();
 
         if (countNames != names.size()) {
             throw new InputException(InputError.EXCEEDED_MAXIMUM_ERROR);
         }
-    }
-
-    public static boolean isCanForward(int forwardNumber) {
-        return MAXIMUM_FORWARD_NUMBER <= forwardNumber;
     }
 }
