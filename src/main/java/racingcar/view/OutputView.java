ea8043d42debc1891db01cad06978b7fcbca6fce
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-01 01:36:36 +0900

    refactor: 메소드 명 변경 및 stream을 사용해 indent 줄이기

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,11 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class OutputView {
 
-	public static void printFirstResultsMessage() {
+	public static void printFirstMessage() {
 		System.out.println();
 		System.out.println("실행 결과");
 	}
@@ -12,17 +13,19 @@ public class OutputView {
 	public static void printCurrentRacingResults(List<String> players, List<Integer> results) {
 		for (int i = 0; i < players.size(); i++) {
 			System.out.printf("%s : ", players.get(i));
-			for(int j = 0 ; j < results.get(i); j ++){
-				System.out.print('-');
-			}
+			IntStream.range(0, results.get(i))
+					.map(j -> '-')
+					.forEach(System.out::print);
 			System.out.println();
 		}
 	}
 
 	public static void printFinalRacingResults(List<String> winners) {
 		System.out.print("최종 우승자 : ");
-		for(int i = 0 ; i < winners.size(); i ++){
-			if(i != 0) System.out.print(", ");
+		for (int i = 0; i < winners.size(); i++) {
+			if (i != 0) {
+				System.out.print(", ");
+			}
 			System.out.print(winners.get(i));
 		}
 	}
