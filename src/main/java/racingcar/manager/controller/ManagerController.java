b1146f23c9af341e1202beb54be6e4de18d1c9b2
icecoff22 <wjdtmd987@gmail.com>
2023-10-31 17:31:54 +0900

    feat(ManagerController) : 각 시도마다 차량들이 얼마나 움직였는지 확인 기능

--- src/main/java/racingcar/manager/controller/ManagerController.java
+++ src/main/java/racingcar/manager/controller/ManagerController.java
@@ -1,14 +1,27 @@
 package racingcar.manager.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import racingcar.car.controller.CarController;
+import racingcar.manager.view.ManagerView;
 import racingcar.user.controller.UserController;
 
 public class ManagerController {
 
-
+    ManagerView managerView = new ManagerView();
 
     public void run(){
         CarController carController = new CarController();
         UserController userController = new UserController();
+    }
+
+    public void printTryResult(HashMap<String, Integer> cars){
+        for(Map.Entry<String, Integer> entry : cars.entrySet()){
+            String carName = entry.getKey();
+            Integer moveCount = entry.getValue();
+
+            String totalMoveCount = managerView.getTotalMoveCount(moveCount);
+            managerView.printMovingMessage(carName,totalMoveCount);
+        }
     }
 }
--- src/main/java/racingcar/manager/view/ManagerView.java
+++ src/main/java/racingcar/manager/view/ManagerView.java
@@ -1,4 +1,17 @@
 package racingcar.manager.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ManagerView {
+    private static String MOVING_CHARACTER = "-";
+
+
+    public String getTotalMoveCount(int moveCount){
+        return MOVING_CHARACTER.repeat(moveCount);
+    }
+
+    public void printMovingMessage(String carName, String totalMoveCount){
+        System.out.print(carName + " : " + totalMoveCount);
+    }
 }
