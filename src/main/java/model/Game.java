dd005f6d09bcf0707107fa81fe60e7dd711db414
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-31 18:18:02 +0900

    feat : Game 객체 생성
    - 모든 자동차들 움직이기
    - 다음 라운드로 진행
    - 게임 끝난 여부 판별

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Game {
+
+    private final Cars cars;
+    private final TrialNumber trialNumber;
+
+    public Game(Cars cars, TrialNumber trialNumber) {
+        this.cars = cars;
+        this.trialNumber = trialNumber;
+    }
+
+    public void playGame(Cars cars) {
+        cars.moveCars();
+    }
+
+    public void nextRound(TrialNumber trialNumber) {
+        trialNumber.decreaseTrialNumber();
+    }
+
+    public boolean isGameEnd(TrialNumber trialNumber) {
+        return trialNumber.isTrialNumberZero();
+    }
+
+}
