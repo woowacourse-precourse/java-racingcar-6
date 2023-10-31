7e5aebd0dc19c54529dba10874ec5e33ea96f835
깃wooqqq <sge0225@naver.com>
2023-11-01 03:34:52 +0900

    feat: 중복된 자동차 이름 입력 시 예외처리 기능 구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,17 +1,21 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final int CAR_NAME_MIN_LENGTH = 1;
     private static final String BLANK = " ";
+    private Set<String> existCarNames = new HashSet<>();
 
     public void checkCarName(List<String> carNames) {
         for (String carName : carNames) {
             checkCarNameMaxLength(carName);
             checkCarNameMinLength(carName);
             checkCarNameBlank(carName);
+            checkCarNameDuplicates(carName);
         }
     }
 
@@ -34,6 +38,12 @@ public class Validator {
     private void checkCarNameBlank(String carName) {
         if (carName.contains(BLANK)) {
             throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수 없습니다.");
+        }
+    }
+
+    private void checkCarNameDuplicates(String carName) {
+        if (!existCarNames.add(carName)) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
         }
     }
 
