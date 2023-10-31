390d851583b375fe2b45c8589b2e1e43a0971bb3
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-01 00:09:04 +0900

    feat: 테스트에 사용할 상수 정의

---
+++ src/test/java/resource/TestData.java
@@ -0,0 +1,13 @@
+package resource;
+
+public class TestData {
+    public static final String CAR_NAME_1 = "test1";
+    public static final String CAR_NAME_2 = "test2";
+    public static final String CAR_NAME_3 = "test3";
+    public static final String CAR_NAME_4 = "test4";
+    public static final String CAR_NAME_5 = "test5";
+    public static final String LONG_CAR_NAME = "testtest";
+    public static final int ONE = 1;
+    public static final int ZERO = 0;
+    public static final int NINE = 9;
+}
