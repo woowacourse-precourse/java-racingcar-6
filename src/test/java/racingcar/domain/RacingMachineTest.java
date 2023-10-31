0b27bb07f98a719b093380d628fb7129533e3cfa
yuseonjun <seonjun0906@gmail.com>
2023-10-31 17:30:12 +0900

    test: 올바른 tryCount가 들어오면 RacingMachine 객체가 생성된다.

--- src/test/java/racingcar/domain/RacingMachineTest.java
+++ src/test/java/racingcar/domain/RacingMachineTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,9 +20,17 @@ class RacingMachineTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1, -2})
-    void RacingMachine_생성자는_올바른_tryCount가_들어오지_않으면_IllegalArgumentException기_발생한다(int number) {
+    void RacingMachine_생성자는_올바른_tryCount가_들어오지_않으면_IllegalArgumentException가_발생한다(int number) {
         assertThatThrownBy(() -> new RacingMachine(cars, number))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void RacingMachine_생성자는_올바른_tryCount가_들어오면_RacingMachine_객체를_생성한다(int number) {
+        RacingMachine racingMachine = new RacingMachine(cars, number);
+
+        assertThat(racingMachine).isInstanceOf(RacingMachine.class);
     }
 }
 
