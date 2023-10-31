7de3862b67826aeea97443fc9fc56207c2950fdf
Seyeon <wjdtpdus828@naver.com>
2023-10-31 19:04:25 +0900

    modify(Application): 게임 시작을 관리하는 메소드인 makeGame의 구현으로 시작 메소드 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Controller racingcarController = new Controller();
-        racingcarController.startGame();
+        racingcarController.makeGame();
     }
 }
