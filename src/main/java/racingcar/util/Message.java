8a27f01d62cab37aaf0542c9d7e6457711633581
cherBlair <cher.blair.h@gmail.com>
2023-10-31 19:05:49 +0900

    [FEAT] util - Message class 생성

---
+++ src/main/java/racingcar/util/Message.java
@@ -0,0 +1,8 @@
+package racingcar.util;
+
+public class Message {
+
+    public final static String INSERT_CAR_NAME = "경주할 자동차 이름을 임력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String ASK_TRY_COUNT = "시도할 회수는 몇회인가요?";
+
+}
