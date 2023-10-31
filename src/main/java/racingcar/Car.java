436c89f4378138a7b5623993897122fa9f6c6655
2uwls <2uwls1126@gmail.com>
2023-10-31 17:31:17 +0900

    fix: modify Car class

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,5 +3,17 @@ package racingcar;
 public class Car {
     private String name;
     private int driveNum;
-
+    Car(String name, int driveNum) {
+        this.name = name;
+        this.driveNum = driveNum;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getDriveNum() {
+        return driveNum;
+    }
+    private void setDriveNum(int driveNum) {
+        this.driveNum = driveNum;
+    }
 }
