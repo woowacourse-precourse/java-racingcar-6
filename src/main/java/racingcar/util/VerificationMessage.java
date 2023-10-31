bb47b19547d618e5cb55668d82e8e2d014f82443
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-31 22:28:23 +0900

    feat: 시도 횟수가 음수일 때 예외 메세지 추가

--- src/main/java/racingcar/util/VerificationMessage.java
+++ src/main/java/racingcar/util/VerificationMessage.java
@@ -5,4 +5,5 @@ public class VerificationMessage {
     public static final String OVER_SIZE = "이름은 5자 이하로 입력해 주세요.";
     public static final String DUPLICATE_NAME = "이미 입력한 자동차 이름 입니다.";
     public static final String NOT_ZERO = "시도 횟수는 1 이상이어야 합니다.";
+    public static final String NOT_NEGATIVE_NUMBER = "시도 횟수는 양수여야 합니다.";
 }
