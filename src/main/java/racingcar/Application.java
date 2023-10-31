fbbd7f2b45ed627021454b4255eb422056eff38d
Seungjida <esg0107@pusan.ac.kr>
2023-10-31 11:04:50 +0900

    style(CarRaceManager): 인스턴스 이름 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,27 +1,26 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
 
 public class Application {
     private static int attemptNumber = 0;
 
     public static void main(String[] args) {
-        CarRaceManager newRacingCars = new CarRaceManager();
+        CarRaceManager newRaceManager = new CarRaceManager();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        newRacingCars.createCarList();
+        newRaceManager.createCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
         attemptNumber = getAttemptNumber();
 
         System.out.println("\n" + "실행 결과");
         for (int i = 0; i < attemptNumber; i++) {
-            newRacingCars.nthAttemptRace();
-            newRacingCars.printAttemptResult();
+            newRaceManager.nthAttemptRace();
+            newRaceManager.printAttemptResult();
         }
 
-        newRacingCars.winnerJudgment();
-        newRacingCars.printWinner();
+        newRaceManager.winnerJudgment();
+        newRaceManager.printWinner();
         Console.close();
     }
 
