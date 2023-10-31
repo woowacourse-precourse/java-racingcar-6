b2b687ed03be8507a730ef68326386419bf8d2c2
JoonKyo <joonkyolee@gmail.com>
2023-10-31 22:20:42 +0900

    feat(OutputView): 우승 자동차를 출력하는 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
     private static final String RESULT_INTRODUCTION = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자";
     private static final String COLON = " : ";
+    private static final String COMMA = ", ";
 
     public void printResultIntroduction(){
         System.out.println(RESULT_INTRODUCTION);
@@ -16,5 +17,9 @@ public class OutputView {
         for(Car car : cars){
             System.out.println(car.getName() + COLON + car.getCarMovedResult());
         }
+    }
+
+    public void printWinnerCars(List<String> winnerCars){
+        System.out.println(WINNER_MESSAGE + COLON + String.join(COMMA, winnerCars));
     }
 }
