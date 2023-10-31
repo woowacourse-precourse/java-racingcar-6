fa27830f3799e6161f78aa7fc524e7cb46510238
Yeop <syt05342@naver.com>
2023-11-01 02:03:12 +0900

    feat: 애플리케이션 실행 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new InputView(), new OutputView());
+        gameController.runRace();
     }
 }
