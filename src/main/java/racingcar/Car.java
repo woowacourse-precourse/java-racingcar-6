65f64ab73bb42f621616d28035fd805412d2e247
YoonSub Lim <yslim37@naver.com>
2023-11-01 03:50:13 +0900

    feat: 자동차 전진 시도 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,21 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
 
     public String name;
+    public Integer location;
 
     public Car(String carName) {
         name = carName;
+        location = 0;
+    }
+
+    public void tryMove() {
+
+        if (pickNumberInRange(0, 9) >= 4) {
+            location += 1;
+        }
     }
 }
