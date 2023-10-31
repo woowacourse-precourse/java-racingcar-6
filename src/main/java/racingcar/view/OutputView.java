2f3666931ce92e07df50fafe9bace3ebac4c54e5
hong <kevin0928@naver.com>
2023-10-31 22:34:59 +0900

    refactor: " : "을 COLON으로 정의
    - 재사용성을 높이기 위해 변수로 선언

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,11 +7,12 @@ import racingcar.dto.response.RoundResultDto;
 public class OutputView {
     private static final String ONE_STEP = "-";
     private static final String RESULT_MESSAGE = "최종 우승자";
+    private static final String COLON = " : ";
 
     public void printRoundResult(RoundResultDto roundResultDto) {
         String carName = roundResultDto.name();
         int carLocation = roundResultDto.location();
-        System.out.println(carName + " : " + printOneStepPosition(carLocation));
+        System.out.println(carName + COLON + printOneStepPosition(carLocation));
     }
 
     private String printOneStepPosition(int location) {
@@ -19,7 +20,7 @@ public class OutputView {
     }
 
     public void printGameResult(GameResultDto gameResultDto) {
-        System.out.println(RESULT_MESSAGE + " : " + joinNames(gameResultDto.winners()));
+        System.out.println(RESULT_MESSAGE + COLON + joinNames(gameResultDto.winners()));
     }
 
     private String joinNames(List<String> nameList) {
