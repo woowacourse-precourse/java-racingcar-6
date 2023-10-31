3f03df2004d7a31a00aac3dc8ada486ecd2a589c
yshong1998 <yshong1998@gmail.com>
2023-10-31 15:04:12 +0900

    Refactor(SystemMessage): 누락된 메세지 추가

--- src/main/java/racingcar/system/SystemMessage.java
+++ src/main/java/racingcar/system/SystemMessage.java
@@ -4,6 +4,7 @@ public enum SystemMessage {
 
     INPUT_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_REPEAT_TIMES_MESSAGE("시도할 회수는 몇회인가요?"),
+    GAME_RESULT("실행 결과\n"),
     GAME_WINNER("최종 우승자 : "),
     INPUT_OUT_OF_RANGE("입력이 범위를 초과했습니다."),
     INPUT_POSITIVE_INTEGER_ONLY("양의 정수만 입력할 수 있습니다.");
