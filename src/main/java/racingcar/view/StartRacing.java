a2c50a0ec59e1630e5d006641dbf0fcd53acf15c
OctoHun <97264011+OctoHun@users.noreply.github.com>
2023-10-31 21:05:11 +0900

    레이싱 게임 시작 기능

---
+++ src/main/java/racingcar/view/StartRacing.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+public class StartRacing {
+
+    public void run() {
+        // 게임 시작 전 준비단계
+        beforeStartGame();
+
+        // 게임 중
+        playGame();
+
+        //게임 마무리
+        endGame();
+    }
+
+    private void beforeStartGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    private void playGame() {
+        System.out.println("실행 결과");
+
+    }
+
+    private void endGame() {
+        System.out.print("최종 우승자 : ");
+    }
+}
