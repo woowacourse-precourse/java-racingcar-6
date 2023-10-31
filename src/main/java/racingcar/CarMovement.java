f5551a5e71639f91cf8017cd948c92ef7170b98c
wonindev <sangwonparkdev@gmail.com>
2023-11-01 03:33:24 +0900

    refactor(CarMovement): 해쉬맵에 "-"인 자동차 움직임 상황을 추가할 때 코드 수정

--- src/main/java/racingcar/CarMovement.java
+++ src/main/java/racingcar/CarMovement.java
@@ -10,7 +10,13 @@ public class CarMovement {
 
     public static void putHM(HashMap<String, String> membersHM, List<String> members, String str) {
         for (int i = 0; i < members.size(); i++) {
-            membersHM.put(members.get(i), str);
+            int randomNum = PickNum.pickNum();
+
+            if (isMove(randomNum) && membersHM.get(members.get(i)).equals("")) {
+                membersHM.put(members.get(i), "-");
+            } else if (isMove(randomNum)) {
+                membersHM.put(members.get(i), membersHM.get(members.get(i)) + str);
+            }
         }
     }
 }
