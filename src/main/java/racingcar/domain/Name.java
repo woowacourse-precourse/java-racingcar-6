f827a98f574dbeb7edebb983bafaa5f46c7c2f48
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-01 02:05:39 +0900

    feat(Name): 자동차 이름 공백일 경우 검증 구현

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -11,11 +11,18 @@ public class Name {
     }
 
     private void validate(String name){
+        validateIsNotEmpty(name);
         validateLength(name);
     }
 
     private void validateLength(String name){
         if (name.length() > MAX_LENGTH){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateIsNotEmpty(String name){
+        if(name == null || name.trim().length() == 0){
             throw new IllegalArgumentException();
         }
     }
