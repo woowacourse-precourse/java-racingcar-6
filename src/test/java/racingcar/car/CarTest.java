199f3c8e3b68e1fa19bd975f94dbada837ea75f0
이병수 <qudtn0315@naver.com>
2023-10-31 15:39:53 +0900

    필요하지 않은 import문 삭제

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -1,7 +1,5 @@
 package racingcar.car;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
