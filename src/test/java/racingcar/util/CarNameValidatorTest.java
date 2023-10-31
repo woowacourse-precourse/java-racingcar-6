3f85ee2d83e554a2a41408d8a324b45bad0f7203
Somin Park <ps4708@naver.com>
2023-10-31 20:33:05 +0900

    test: CarNameValidator에 대한 테스트 코드 작성

--- src/test/java/racingcar/util/CarNameValidatorTest.java
+++ src/test/java/racingcar/util/CarNameValidatorTest.java
@@ -1,19 +1,33 @@
 package racingcar.util;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CarNameValidatorTest {
 
-class CarNameValidatorTest {
+    private CarNameValidator carNameValidator;
+
+    @BeforeEach
+    void init(){
+        carNameValidator = new CarNameValidator();
+    }
 
     @Test
-    void validateCarName() {
-        String input ="one,two,three1";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidator.validateCarNames(input);
-        });
-        String[] carName = CarNameValidator.validateCarNames(input);
-
+    @DisplayName("입력한 자동차 이름의 길이가 5자보다 클 경우 예외가 발생한다.")
+    void TestCarNameLength(){
+        String carNames = "car1,car2,car345";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> CarNameValidator.validateCarNames(carNames));
     }
+
+    @Test
+    @DisplayName("입력한 자동차 이름이 중복될 경우 예외가 발생한다.")
+    void TestCarNameDuplicate(){
+        String carNames = "car1,car2,car2";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> CarNameValidator.validateCarNames(carNames));
+    }
+
 }
