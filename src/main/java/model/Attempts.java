3a994d3b4739dd98db6d62e10be1bc9ba40350d7
kimwanyoung <dhks2869@gmail.com>
2023-10-31 10:49:24 +0900

    refactor : Attempts클래스 멤버 변수 제어자 final 추가

--- src/main/java/model/Attempts.java
+++ src/main/java/model/Attempts.java
@@ -2,7 +2,7 @@ package model;
 
 public class Attempts {
 
-    private int attempts;
+    private final int attempts;
     private static final String EMPTY_MESSAGE = "공백 혹은 빈 문자열은 입력 할 수 없습니다.";
     private static final String ONLY_NUMBER_INPUT_MESSAGE = "숫자만 입력 가능합니다.";
 
