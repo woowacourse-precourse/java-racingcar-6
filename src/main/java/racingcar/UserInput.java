971289a61f446863a23f201525a1514504eada1b
jun23314 <dlwnsfml@naver.com>
2023-11-01 03:22:06 +0900

    사용자로부터값을 입력받는 기능 - 예외 처리

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -4,8 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 public class UserInput {
     public String[] inputCarName(){
         String input = Console.readLine();
-        String[] carName = input.split(", ");
+        String[] carName = input.split(",");
+        checkLength(carName);
+        checkNull(carName);
         return carName;
+    }
+
+    private void checkLength(String[] strings){
+        for(int i = 0; i < strings.length; i++){
+            if(strings[i].length() > 5){
+                throw new IllegalArgumentException("이름은 5글자 이하여야 합니다.");
+            }
+        }
+    }
+
+    private void checkNull(String[] strings){
+        if(strings.length == 1 && strings[0] == ""){
+            throw new IllegalArgumentException("하나 이상의 이름을 입력해주세요");
+        }
     }
 
     public int inputCount(){
