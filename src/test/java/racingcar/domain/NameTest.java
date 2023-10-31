f132c6024117abe4b6a9c7769962ad0b1f63a859
seokmyungham <hellotjraud@gmail.com>
2023-11-01 08:47:55 +0900

    test(NameTest): Name 클래스 테스트 추가

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("If Name Exceeds 5 Characters, Throw Exception")
+    void 이름의_길이가_5글자를_넘을_경우_에러_발생() {
+        Assertions.assertThatThrownBy(() -> new Name("abcedf"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5글자 이하만 가능합니다");
+    }
+
+    @Test
+    @DisplayName("If Car Name Is Blank, Throw Exception")
+    void 이름이_공백일_경우_에러_발생() {
+        Assertions.assertThatThrownBy(() -> new Name(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 공백일 수 없습니다");
+
+        Assertions.assertThatThrownBy(() -> new Name("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 공백일 수 없습니다");
+    }
+}
