2b16354126318362d2d28c0d3e475af092ffe766
siyeonSon <siyeon0209@naver.com>
2023-10-31 22:45:09 +0900

    test(car): 자동차는 무작위 값이 4 미만일 경우 전진하지 않는다

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -18,6 +18,19 @@ class CarTest {
     }
 
     @Test
+    void 자동차는_무작위_값이_4_미만일_경우_전진하지_않는다() {
+        Car car = new Car("pobi");
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.forward(numberGenerator);
+                    assertThat(car.getPoint()).isEqualTo(0);
+                },
+                2
+        );
+    }
+
+    @Test
     void 자동차는_무작위_값이_4_이상일_경우_전진한다() {
         Car car = new Car("pobi");
         NumberGenerator numberGenerator = new RandomNumberGenerator();
