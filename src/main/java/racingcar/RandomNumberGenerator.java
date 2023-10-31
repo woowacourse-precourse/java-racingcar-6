1e902e5d3ee67c6005b01e2857132fa10864050f
이희수 <hslee9234@naver.com>
2023-11-01 00:58:20 +0900

    feat: randomnumbergenerator 객체 생성

---
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class RandomNumberGenerator {
+    public static int generateRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
