38f45137856cc9638f921b4b001afa13bc745f49
carol <carol072400@gmail.com>
2023-10-31 21:21:49 +0900

    feat(racingcar): 사용자에게서 몇 번의 이동을 할 것인지를 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +12,26 @@ public class Application {
 
     public static List<String> askCarNames() {
         List<String> carNames = new ArrayList<String>();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input[] = Console.readLine().replaceAll("\\p{Z}", "").split(",");
+
+        for (String x : input) {
+            if(x.length() <=5) {
+                carNames.add(x);
+            }
+            else throw new IllegalArgumentException();
+        }
         return carNames;
     }
 
     public static int askMoveCount() {
-        int moveCount = 0;
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String input = Console.readLine();
+        try {
+            int d = Integer.parseInt(input);
+        } catch(NumberFormatException e) { throw new IllegalArgumentException(); }
+        int moveCount = Integer.parseInt(input);
+        System.out.print("\n");
         return moveCount;
     }
 }
