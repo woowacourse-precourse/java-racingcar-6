430688382d3e621985045bcbf0fdd68ee3b631d0
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 01:41:04 +0900

    feat: 이름을 아무것도 입력하지 않았을 때를 검증한다.

--- src/main/java/racingcar/util/vaildator/carNameValidator.java
+++ src/main/java/racingcar/util/vaildator/carNameValidator.java
@@ -3,10 +3,18 @@ package racingcar.util.vaildator;
 public class carNameValidator {
     private static final int MAX_INPUT_NAME_SIZE = 5;
     private static final String INPUT_NAME_SIZE_EXCEPTION = "이름이 5자를 초과하였습니다.";
+    private static final String INPUT_NAME_EXCEPTION = "이름을 입력해주세요.";
 
     public static void validateInputNameSize(String name) {
         if (name.length() > MAX_INPUT_NAME_SIZE) {
             throw new IllegalArgumentException(INPUT_NAME_SIZE_EXCEPTION);
         }
     }
+
+    public static void validateInputName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_NAME_EXCEPTION);
+        }
+    }
+
 }
