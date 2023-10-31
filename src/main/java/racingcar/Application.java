88563fa31a04ca737ca56d775ba4cd154c82d2fc
DH Jeong <wjdehgns97@gmail.com>
2023-10-31 21:23:57 +0900

    style: Service 생성을 print() 후로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,9 +12,8 @@ import java.util.HashMap;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        final ReceptionDeskService receptionDeskService = new ReceptionDeskServiceImpl();
-
         Script.REGISTER.print();
+        final ReceptionDeskService receptionDeskService = new ReceptionDeskServiceImpl();
         HashMap<String, Integer> lineUp = receptionDeskService.registering(Console.readLine());
         // hi,my,name,is,hello,world
 
