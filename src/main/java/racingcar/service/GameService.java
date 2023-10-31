fad26ebb289c15f822b91fd4a3f00314dee10eae
leeminjun <sohwi0305@gmail.com>
2023-11-01 00:13:59 +0900

    feat: 선두에 달리는 우승후보 위치 로직

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -11,10 +11,21 @@ import racingcar.repository.GameRepository;
 public class GameService {
     private static final int START_POSITION = 0;
     private static final int END_POSITION = 9;
+    private static final int MIN_VALUE = -1;
     private final GameRepository gameRepository = new GameRepository();
     public void Fowardstart(Game game){
         moveForwardByRandomNumber(game);
         game.incresePlayCount();
+    }
+
+    private int getWinnerPosition(List<Car> cars) {
+        int max = MIN_VALUE;
+        for (Car car : cars) {
+            if (max < car.getPosition()) {
+                max = car.getPosition();
+            }
+        }
+        return max;
     }
 
     private void moveForwardByRandomNumber(Game game) {
