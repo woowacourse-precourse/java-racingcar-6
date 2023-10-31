58eef0d4b03f652d8203d96ec4648e79f01c2462
yang <hjyang@epience.com>
2023-10-31 09:14:08 +0900

    🎯feat(view/Output) : 입력받은 자동차 출력 기능 구현

--- src/main/java/racingcar/view/Output/OutputView.java
+++ src/main/java/racingcar/view/Output/OutputView.java
@@ -3,7 +3,14 @@ package racingcar.view.Output;
 public class OutputView {
     private final String RACE_REULT= "실행 결과";
 
+    // 결과 출력
     public void printResult() {
         System.out.println(RACE_REULT);
     }
+
+    // 입력받은 자동차 List 출력
+    public void printCars(String cars) {
+        System.out.println(cars);
+    }
+
 }
