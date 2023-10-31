effbd187bfeb0bf74608ddbb8e39a31adc57774b
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 23:22:06 +0900

    feat : InputView 사용자가 값을 입력하기 앞서 무엇을 입력해야하는지에 대한 멘트를 출력하는 기능 추가

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -9,4 +9,6 @@ public class Constant {
     public static final char ENTER = '\n';
     public static final String WINNER_NAMES = "최종 우승자 : ";
     public static final String COMMA_AND_BLANK = ", ";
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
 }
--- src/main/java/racingcar/view/input/InputView.java
+++ src/main/java/racingcar/view/input/InputView.java
@@ -1,6 +1,9 @@
 package racingcar.view.input;
 
 import static racingcar.constant.Constant.COMMA;
+import static racingcar.constant.Constant.ENTER;
+import static racingcar.constant.Constant.INPUT_CAR_NAMES;
+import static racingcar.constant.Constant.INPUT_NUMBER_OF_ATTEMPTS;
 import static racingcar.exception.ErrorMessage.NUMBER_OF_ATTEMPTS_ONLY_ALLOW_NUMBER_EXCEPTION;
 
 import java.util.List;
@@ -13,12 +16,16 @@ public class InputView {
     }
 
     public List<String> names() {
+        System.out.println(INPUT_CAR_NAMES);
         return List.of(consoleService.readLine().split(COMMA));
     }
 
     public long number() {
         try {
-            return Long.parseLong(consoleService.readLine());
+            System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
+            long number = Long.parseLong(consoleService.readLine());
+            System.out.print(ENTER);
+            return number;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_OF_ATTEMPTS_ONLY_ALLOW_NUMBER_EXCEPTION);
         }
