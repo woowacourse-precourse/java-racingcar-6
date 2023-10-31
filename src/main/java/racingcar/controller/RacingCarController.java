0ef6fcf0fe7f0096d8ca992b9f31d9c3b5e1ce1a
yuseonjun <seonjun0906@gmail.com>
2023-10-31 17:14:40 +0900

    refactor: RacingMachine으로 주요 로직 이동

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import racingcar.domain.Cars;
 import racingcar.domain.NumberGenerator;
 import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class RacingCarController {
 
@@ -12,17 +11,6 @@ public class RacingCarController {
 
     public RacingCarController(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
-    }
-
-    public void start() {
-        Cars cars = generateCar();
-        int tryCount = InputView.readTryCount();
-        OutputView.printPlayResultMessage();
-        for (int i = 0; i < tryCount; i++) {
-            cars.moveCars(numberGenerator);
-            OutputView.printResult(cars);
-        }
-        OutputView.printWinner(cars);
     }
 
     private Cars generateCar() {
