afa3f79687db32212b42ce0cc4c56124eef3fd0c
siho-song <thdtlgh234@naver.com>
2023-11-01 00:00:39 +0900

    [Feat] 게임 출력관련 함수 구현

---
+++ src/main/java/racingcar/GameOutput.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GameOutput {
+
+    public void printRoundResult(List<Car> carList) {
+        String roundResult = carList.stream().
+                map(car -> car + " : " + GameUtils.numToHyphen(car.getMovingCount())).
+                collect(Collectors.joining("\n"));
+
+        System.out.println(roundResult + "\n");
+    }
+
+    public void printWinners(List<Car> winnerList) {
+
+        String winners = winnerList.stream()
+                .map(Car::toString)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + winners);
+    }
+
+}
