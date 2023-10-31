9a60139e3fb5db66357c1bc66e91fc9456e97017
Hyoseop Song <crad_on25@naver.com>
2023-11-01 04:52:54 +0900

    feature(Car): Car 필드 및 생성자 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MOVE_CONDITION = 4;
+    private final String name;
+    private final int position = 0;
+
+    public Car(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 이내만 가능합니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
