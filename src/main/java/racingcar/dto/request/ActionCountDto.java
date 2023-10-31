f46313c43c6d0a0e8af2a8bb5b381fbe521848c5
jongmee <ra123221@gmail.com>
2023-10-31 14:21:54 +0900

    feat: 입력된 시도 횟수가 음인 경우 예외 처리하기

--- src/main/java/racingcar/dto/request/ActionCountDto.java
+++ src/main/java/racingcar/dto/request/ActionCountDto.java
@@ -5,7 +5,10 @@ public class ActionCountDto {
     private final Integer actionCount;
 
     public ActionCountDto(final String actionCount) {
-        this.actionCount = convertToInteger(actionCount);
+        Integer integerCount =  convertToInteger(actionCount);
+        validateRange(integerCount);
+
+        this.actionCount = integerCount;
     }
 
     private Integer convertToInteger(final String actionCount) {
@@ -13,6 +16,12 @@ public class ActionCountDto {
             return Integer.valueOf(actionCount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("please enter an integer");
+        }
+    }
+
+    private void validateRange(final Integer actionCount) {
+        if(actionCount < 0) {
+            throw new IllegalArgumentException("please enter a non-negative integer");
         }
     }
 
