32b74b2521c0687fc4b64fa8fb1e9a141b7e83ba
mingtorr <mingtorr@nave.com>
2023-11-01 01:30:37 +0900

    입출력 view class

---
+++ src/main/java/racingcar/view/GameOutput.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+import racingcar.constants.AppConstants;
+
+public class GameOutput {
+
+    public static void printInputCarName() {
+        System.out.println(AppConstants.MSG_INPUT_CAR_NAME);
+    }
+
+    public static void printInputGameRound() {
+        System.out.println(AppConstants.MSG_INPUT_GAME_ROUND);
+    }
+}
