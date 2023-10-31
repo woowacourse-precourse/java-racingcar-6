9d7f8bfc7b3a6a91372f1cc99fab27e5099fc3bb
Hyso <hsk7953@gmail.com>
2023-10-31 22:27:01 +0900

    feat ì자동차이름과 실행회수를 입력받는 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,15 @@
-package racingcar.View;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String requestCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String requestNumberOfTry(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
 }
