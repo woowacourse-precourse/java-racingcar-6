2eee0b61d9a750b77442693ef2babde94a329496
HurDong <gjehdals456@gmail.com>
2023-11-01 02:46:34 +0900

    feat : Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.Controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.startGame();
     }
+
 }
