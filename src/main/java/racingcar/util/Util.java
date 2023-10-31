04ed3e013d641f25f47da96cbba5c98c829d3a6e
JaeHunJang <wuddlaa@gmail.com>
2023-10-31 16:58:06 +0900

    refactor(Contant,Util): 이동거리 함수 구현 및 상수 추가

--- src/main/java/racingcar/util/Util.java
+++ src/main/java/racingcar/util/Util.java
@@ -6,4 +6,8 @@ public class Util {
     public static int getRandomNumber() {
         return Randoms.pickNumberInRange(Constant.MIN_RANDOM_NUM, Constant.MAX_RANDOM_NUM);
     }
+
+    public static String createMarkString(int distance) {
+        return Constant.OUTPUT_CAR_MOVE_MARK.repeat(distance);
+    }
 }
