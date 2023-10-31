351efef9510202d876be6e6884e6df185a760230
Junobee25 <wnsdhqo@naver.com>
2023-10-31 20:01:31 +0900

    refactor(service): moveTheCar 메서드 수정

--- src/main/java/racingcar/service/Move.java
+++ src/main/java/racingcar/service/Move.java
@@ -1,23 +1,22 @@
 package racingcar.service;
 
 import racingcar.domain.Car;
-import racingcar.domain.CarGenerator;
-import racingcar.domain.RandomNumber;
-import racingcar.domain.RandomNumberGenerator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Move {
     Car car = new Car();
 
     public void moveTheCar(LinkedHashMap<String, String> carList, List<Integer> randomNumberList) {
         int randomNumberListIndex = 0;
-        carList.forEach((key, value) -> {
+        for (Map.Entry<String, String> entry : carList.entrySet()) {
+            String key = entry.getKey();
             if (randomNumberList.get(randomNumberListIndex) >= 4) {
-                String moveValue = car.getCarList().get(key);
-                car.getCarList().put(key, moveValue + "-");
+                String moveValue = carList.get(key);
+                carList.put(key, moveValue + "-");
             }
-        });
+        randomNumberListIndex++;
     }
 
     public void stopTheCar(String carname) {
