eddeafdc6ed4a7dd3c1e5e4230e55beb57caf7c8
hil679 <hil679922@gmail.com>
2023-11-01 02:32:03 +0900

    Test(Car): 자동차 이름 조회 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,21 +1,31 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+    private Car car1;
+    private Car car2;
+    @BeforeEach
+    void setUp() {
+        car1 = new Car("robin");
+        car2 = new Car("hood");
+    }
+    @Test
+    void 자동차_이름_조회() {
+        assertThat(car1.getName()).isEqualTo("robin");
+    }
+
     @Test
     void 자동차_객체_전진_증가_테스트() {
-        Car car = new Car("robin");
-        Car car2 = new Car("nope");
         assertSimpleTest(() -> {
-            assertThat(car.getForwardNum()).isEqualTo(0);
-            car.increaseForwardNum();
+            assertThat(car1.getForwardNum()).isEqualTo(0);
+            car1.increaseForwardNum();
             car2.increaseForwardNum();
-            assertThat(car.getForwardNum()).isEqualTo(1);
+            assertThat(car2.getForwardNum()).isEqualTo(1);
         });
-
     }
 }
