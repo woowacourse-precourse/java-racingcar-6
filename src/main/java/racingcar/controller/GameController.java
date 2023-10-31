4d503140e07683f6fa8d5e28c4a13df092d8ecad
PARK9898 <wjddjs21@naver.com>
2023-10-31 21:34:54 +0900

    "feat: Cars 초기화 메서드 구현"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,11 +1,15 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Car;
 import racingcar.domain.NumberGenerator;
 import racingcar.domain.RandomNumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
+    private static final int INITIALIZE_POSITION = 0;
+
     private final InputView inputView;
     private final OutputView outputView;
     private final NumberGenerator numberGenerator = new RandomNumberGenerator();
@@ -14,4 +18,11 @@ public class GameController {
         this.inputView = inputView;
         this.outputView = outputView;
     }
+
+    public List<Car> InitializeCars(List<String> carNames) {
+        return carNames.stream()
+                .map(carName -> new Car(carName,INITIALIZE_POSITION))
+                .toList();
+    }
+
 }
