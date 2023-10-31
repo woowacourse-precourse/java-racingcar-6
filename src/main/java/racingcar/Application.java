1d30fd62e76ac386b0f9ab1a6b2d775456f23453
aeyongdodam <kikititi0510@gmail.com>
2023-11-01 04:37:13 +0900

    feat(RacingCar): racingcar 생성 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RacingCar;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCar racingCar = new RacingCar();
+        racingCar.startRacing();
     }
 }
