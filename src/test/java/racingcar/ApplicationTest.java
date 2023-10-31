5911cdfd4cf2c42c314d60a95ab21efdc675a209
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-31 19:24:58 +0900

    test: rewrite test code in default

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,14 +31,6 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    @Test
-    void 시행_횟수에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaj", "abc"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
