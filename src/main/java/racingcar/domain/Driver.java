88c2d89bd2fad9807fad068d57e52f66c034bae2
JiEung2 <https://github.com/JiEung2>
2023-10-31 21:27:54 +0900

    Refactor: Driver 클래스 변경
    인터페이스를 받아와 인터페이스의 메서드를 구현하는 코드로 변경

--- src/main/java/racingcar/domain/Driver.java
+++ src/main/java/racingcar/domain/Driver.java
@@ -4,9 +4,10 @@ import static racingcar.utils.Constants.MAX_STRENGTH;
 import static racingcar.utils.Constants.MIN_STRENGTH;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.utils.NumberGenerator;
 
-public class Driver {
-    public static int pressAccelerator() {
+public class Driver implements NumberGenerator {
+    public int press() {
         int pressStrength = Randoms.pickNumberInRange(MIN_STRENGTH, MAX_STRENGTH);
         return pressStrength;
     }
