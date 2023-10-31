b64b88c8d1e2eb455dbd0f7cfdd7843c6eaaee32
Park, Sojeong <sojeong2101@gmail.com>
2023-10-31 18:16:58 +0900

    refactor: 최댓값 연산 재조정2

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,22 +4,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Car implements Comparable<Car> {
+public class Car {
     String carName;
     List<String> movement = new ArrayList<>();
     int forwardMovementNumber = movement.size();
     private int movementRandomNumber = Randoms.pickNumberInRange(0, 9);
 
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getForwardMovementNumber() {
+        return forwardMovementNumber;
+    }
+
     public void decideAction() {
         if (movementRandomNumber >= 4) {
             movement.add("-");
         }
-    }
-
-    @Override
-    public int compareTo(Car car) {
-        if (this.forwardMovementNumber > car.forwardMovementNumber) {
-            return 1;
-        } return -1;
     }
 }
--- src/main/java/racingcar/Judgment.java
+++ src/main/java/racingcar/Judgment.java
@@ -1,12 +1,11 @@
 package racingcar;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Judgment {
     // 차 이름, 전진 모양, 횟수
     List<Car> cars = new ArrayList<>();
-    Car maxMovement = Collections.max(cars);
-
+    Car maxMovement = cars.stream().max(Comparator.comparing(Car::getForwardMovementNumber)).get();
 }
