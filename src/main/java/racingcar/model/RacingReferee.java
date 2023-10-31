445e97d557acb0e14fcaa29f0f02870f9beb0696
youngkyunIm <yee0230@naver.com>
2023-10-31 16:54:12 +0900

    feat(RacingReferee) : 기능 구현
    플레이어 이동, 승자 구하는 기능

---
+++ src/main/java/racingcar/model/RacingReferee.java
@@ -0,0 +1,54 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RacingReferee {
+    private final Players players;
+    private final Map<Player, Integer> result;
+    private final Dice dice;
+    public RacingReferee(Players players, Dice dice) {
+        result = new LinkedHashMap<>();
+        initResult(players);
+        this.players = players;
+        this.dice = dice;
+    }
+
+    private void initResult(Players players) {
+        for (Player player : players.getPlayers()) {
+            result.put(player, 0);
+        }
+    }
+
+    public Map<Player, Integer> carMove() {
+        for (Player player : players.getPlayers()) {
+            int v = dice.roll();
+            System.out.println("valeu =" + v);
+            if(isMove(v)) {
+                result.put(player, result.get(player) + 1);
+            }
+        }
+        return result;
+    }
+
+    public List<String> getWinner() {
+        int maxMove = Collections.max(result.values());
+
+        List<String> winner = new ArrayList<>();
+
+        for (Player player : players.getPlayers()) {
+            if (result.getOrDefault(player, 0) == maxMove) {
+                winner.add(player.getName());
+            }
+        }
+
+        return winner;
+    }
+
+    private boolean isMove(int value) {
+        return value >= 4;
+    }
+}
