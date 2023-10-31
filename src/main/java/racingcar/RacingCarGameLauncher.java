a8113e0fa8ed740024c668262f8b6e8e9f36e54e
alscjf1329 <alscjf1329@gmail.com>
2023-10-31 17:37:30 +0900

    style: 의미에 따라 공백을 작성
    의미에 따라 공백을 작성했습니다.

--- src/main/java/racingcar/RacingCarGameLauncher.java
+++ src/main/java/racingcar/RacingCarGameLauncher.java
@@ -1,10 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.dto.RacingCar;
 import racingcar.utils.RacingCarGame;
 import racingcar.utils.RacingCarGameInput;
 import racingcar.utils.RacingCarGameOutput;
+import racingcar.utils.RandomUtil;
 
 public class RacingCarGameLauncher {
     private RacingCarGame racingCarGame;
@@ -12,11 +14,15 @@ public class RacingCarGameLauncher {
     public void launch() {
         RacingCarGameOutput.printCarNameInputMessage(RacingCarGameOutput.CAR_NAME_INPUT_MESSAGE);
         List<RacingCar> racingCarList = RacingCarGameInput.readCarNames();
+
         RacingCarGameOutput.printMoveCountInputMessage(RacingCarGameOutput.MOVE_COUNT_INPUT_MESSAGE);
         int moveCount = RacingCarGameInput.readMoveCount();
+
         racingCarGame = new RacingCarGame(racingCarList, moveCount);
+
         RacingCarGameOutput.printBeforeGameProgress(RacingCarGameOutput.BEFORE_GAME_MESSAGE);
         racingCarGame.run(() -> Randoms.pickNumberInRange(0, 9));
+
         RacingCarGameOutput.printWinnersMessage(racingCarGame.getWinners());
     }
 }
