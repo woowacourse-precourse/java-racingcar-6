cd61e580ada80f4a330f696b9f609082e274ac9a
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-31 18:55:58 +0900

    test(InputViewTest) : 테스트 displayname 추가

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputViewTest {
@@ -15,12 +16,14 @@ public class InputViewTest {
 
     private InputView inputView;
 
+
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outContent));
         inputView = new InputView();
     }
 
+    @DisplayName("자동차 이름 입력 요청을 올바르게 출력하는지 확인")
     @Test
     public void testPrintCarNameRequest() {
         try {
@@ -30,6 +33,7 @@ public class InputViewTest {
         assertThat(outContent.toString().trim()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+    @DisplayName("시도 횟수 입력 요청을 올바르게 출력하는지 확인")
     @Test
     public void testPrintAttemptCountRequest() {
         try {
