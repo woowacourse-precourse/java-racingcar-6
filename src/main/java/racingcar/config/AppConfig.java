6353b7b2a75529798329be7c91b1b897cf7902f7
khabh1 <kshababe1@gmail.com>
2023-10-31 11:18:01 +0900

    feat(Application): AppConfig에 의존성 연결 및 게임 실행 로직 추가

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -4,11 +4,21 @@ import racingcar.domain.Billboard;
 import racingcar.domain.BillboardImpl;
 import racingcar.domain.CarRegistration;
 import racingcar.domain.CarRegistrationImpl;
+import racingcar.domain.GasolineProvider;
+import racingcar.domain.RandomGasolineProvider;
+import racingcar.util.InputHandler;
+import racingcar.util.InputHandlerImpl;
 
 public class AppConfig {
 
+    private static final InputHandler inputHandler = new InputHandlerImpl();
     private static final CarRegistration carRegistration = new CarRegistrationImpl();
     private static final Billboard billboard = new BillboardImpl();
+    private static final GasolineProvider gasolineProvider = new RandomGasolineProvider();
+
+    public static InputHandler inputHandler() {
+        return inputHandler;
+    }
 
     public static CarRegistration carRegistration() {
         return carRegistration;
@@ -16,5 +26,9 @@ public class AppConfig {
 
     public static Billboard billboard() {
         return billboard;
+    }
+
+    public static GasolineProvider gasolineProvider() {
+        return gasolineProvider;
     }
 }
