4446e73d49397556bfdf4815f264c854a7514372
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-31 22:24:32 +0900

    2023.10.31_feat : OutputView printWinner 메서드 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -13,4 +13,8 @@ public class OutputView {
         roundResults.forEach(System.out::println);
         System.out.println();
     }
+    public static void printWinner(ArrayList<String> winners) {
+        System.out.println(Constants.FINAL_WINNER + String.join(Constants.DELIMITER_WINNER, winners));
+        // String.join() : 리스트 or 배열의 모든 요소를 하나의 문자열로 결합하고, 각 요소 사이에 구분자 삽입
+    }
 }
