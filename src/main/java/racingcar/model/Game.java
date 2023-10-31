b5a2481c0817d5ef092a9bf41abb27423fce9180
poow810 <poow810@naver.com>
2023-10-31 22:37:38 +0900

    feat($Game): generate random number Add new event to $Game: - generate random number Resolves : #1

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,7 +1,10 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game {
 
-
-
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
