e1fecfd2be66eb8e64295f8839d799d544b1b549
DaehoSon <thseogh135@gmail.com>
2023-10-31 23:22:45 +0900

    feat: 자동차가 한 대도 없을 경우 예외처리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -6,6 +6,9 @@ import racingcar.common.Rule;
 public class Validator {
 
     public static void validateCarNameArray(String[] carNameArray) throws IllegalArgumentException {
+        if (carNameArray.length == 0) {
+            throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_CAR_NAME_EMPTY.getMessage());
+        }
         for (String s : carNameArray) {
             if (s.length() > Rule.MAX_CAR_NAME_LENGTH.getValue()) {
                 throw new IllegalArgumentException(ErrorCode.INPUT_WRONG_CAR_NAME_LENGTH.getMessage());
--- src/main/java/racingcar/common/ErrorCode.java
+++ src/main/java/racingcar/common/ErrorCode.java
@@ -1,6 +1,7 @@
 package racingcar.common;
 
 public enum ErrorCode {
+    INPUT_WRONG_CAR_NOTHING("자동차가 비어있습니다."),
     INPUT_WRONG_CAR_NAME_LENGTH("길이가 잘못된 자동차 이름입니다."),
     INPUT_WRONG_CAR_NAME_EMPTY("자동차 이름이 비어있습니다."),
     INPUT_WRONG_NUMBER("잘못된 숫자를 입력하셨습니다. (숫자 범위는 0~9)");
