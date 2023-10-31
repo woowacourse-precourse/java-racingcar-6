6667e156a8e3f0592543719bba21da3909814680
ParkDH0809 <znxn1237@gmail.com>
2023-10-31 18:46:45 +0900

    fix: 테스트 실행 순서에 따른 오류 발생 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,38 +1,38 @@
 package racingcar.view;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class InputViewTest {
     private static InputView inputView;
     @BeforeAll
     static void initInputView() {
         inputView = new InputView();
+        String testString = "pobi,java,test\n10\n";
+        System.setIn(new ByteArrayInputStream(testString.getBytes()));
     }
 
     @Test
+    @Order(1)
     @DisplayName("입력한 이름 리스트 반환 테스트: 정상 반환")
     void testCarNameInputView() {
-        String testString = "pobi,java,test";
         List<String> testList = List.of("pobi","java","test");
-
-        System.setIn(new ByteArrayInputStream(testString.getBytes()));
         assertThat(inputView.inputCarName()).isEqualTo(testList);
     }
 
     @Test
+    @Order(2)
     @DisplayName("입력한 시도 횟수 정수형 반환 테스트: 정상 반환")
     void testTryNumberInputView() {
-        String testString = "10";
+        String testString = "10\n";
         int returnNumber = 10;
 
-        System.setIn(new ByteArrayInputStream(testString.getBytes()));
+//        System.setIn(new ByteArrayInputStream(testString.getBytes()));
         assertThat(inputView.inputTryNumber()).isEqualTo(returnNumber);
     }
 }
