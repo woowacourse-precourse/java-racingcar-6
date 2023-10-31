1c29fbd98b020177865427acd6de2034262fb371
HongChaa <jun761_1@naver.com>
2023-10-31 18:20:43 +0900

    feat(Cars) : 자동차 이름 저장 기능 구현

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> carList = new ArrayList<>();
+
+    public List<Car> setPlayer(String rawCarList) {
+        String[] carNameArray = spiltPlayer(rawCarList);
+
+        addCarNametoCarList(carNameArray);
+
+        return carList;
+    }
+
+    private String[] spiltPlayer(String rawCarList) {
+        return rawCarList.split(",");
+    }
+
+    private void addCarNametoCarList(String[] carNameArray) {
+        for (int i = 0; i < carNameArray.length; i++) {
+            carList.add(new Car(carNameArray[i], 0));
+        }
+    }
+
+}
