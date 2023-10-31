d2673f321fb387cc21b08ef369494dbe4c36f643
U-keun Song <sukeun319@gmail.com>
2023-10-31 13:21:01 +0900

    Refactor : 중복된 이름이 입력되는 예외 케이스에 에러 메시지 추가

--- src/main/java/racingcar/util/exception/ErrorMessage.java
+++ src/main/java/racingcar/util/exception/ErrorMessage.java
@@ -2,6 +2,7 @@ package racingcar.util.exception;
 
 public enum ErrorMessage {
     INVALID_NAME("이름은 1 ~ 5자만 가능합니다."),
+    DUPLICATE_NAME("같은 이름이 있습니다."),
     NOT_INTEGER("정수가 아닙니다."),
     NOT_POSITIVE("양의 정수가 아닙니다.");
 
--- src/main/java/racingcar/util/validator/InputValidator.java
+++ src/main/java/racingcar/util/validator/InputValidator.java
@@ -1,8 +1,9 @@
-package racingcar.util;
+package racingcar.util.validator;
 
 import static racingcar.util.ConstantNumbers.MAXIMUM_NAME_LENGTH;
 import static racingcar.util.ConstantNumbers.MINIMUM_TRY_COUNT;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.util.exception.ErrorMessage;
 import racingcar.util.exception.RacingCarException;
@@ -10,10 +11,12 @@ import racingcar.util.exception.RacingCarException;
 public class InputValidator implements Validator {
     InputValidator() {}
     public void isValidList(final List<String> names) {
-        for (String name:names) {
-            if (!isValidName(name)) {
-                throw RacingCarException.of(ErrorMessage.INVALID_NAME);
-            }
+        if (!hasValidNames(names)) {
+            throw RacingCarException.of(ErrorMessage.INVALID_NAME);
+        }
+
+        if (!hasDistinctName(names)) {
+            throw RacingCarException.of(ErrorMessage.DUPLICATE_NAME);
         }
     }
 
@@ -31,8 +34,26 @@ public class InputValidator implements Validator {
 
         return number;
     }
-    private boolean isValidName(final String name) {
-        return !name.isBlank() && name.length() <= MAXIMUM_NAME_LENGTH.number();
+    private boolean hasValidNames(final List<String> names) {
+        for (String name:names) {
+            if (name.isBlank() || name.length() > MAXIMUM_NAME_LENGTH.number()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasDistinctName(final List<String> names) {
+        List<String> checkList = new ArrayList<>();
+        for (String name:names) {
+            if (checkList.contains(name)) {
+                return false;
+            }
+            checkList.add(name);
+        }
+
+        return true;
     }
 
     private boolean isNegative(final int number) {
