972d9a655f48010d7f209e6e9724c807d214dcbc
hosung-222 <gik11kr@naver.com>
2023-10-31 15:37:31 +0900

    feat : 게임 라운드 입력시 예외 처리 기능

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -18,6 +18,20 @@ public class InputValidator {
         }
     }
 
+    public static void checkRoundNumber(String gameRound){
+        try{
+            Integer.parseInt(gameRound);
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("1 이상의 자연수만 입력 가능합니다.");
+        }
+    }
+
+    public static void checkNotNegative(int gameRound){
+        if (gameRound < 1){
+            throw new IllegalArgumentException("1 보다 커야 합니다.");
+        }
+    }
+
     public static String[] splitCarName(String name){
         return name.endsWith(",") ? name.split(",", -1) : name.split(",");
     }
