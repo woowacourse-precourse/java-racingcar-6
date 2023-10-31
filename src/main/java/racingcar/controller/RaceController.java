5b6237d904868ce339cd3c62ee0cc92199523d1b
seokmyungham <hellotjraud@gmail.com>
2023-11-01 04:51:21 +0900

    feat(RaceController): 경주를 시작하는 raceStart 메서드 추가 및 메서드 분리
    메시지를 출력하고 사용자의 입력을받아 경주를 시작하고, 적절히 메서드들을 분리하였습니다

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -11,4 +11,35 @@ public class RaceController {
         this.gameView = new GameView();
         this.raceService = new RaceService();
     }
+
+    public void raceStart() {
+        String carNames = printAndInputCarName();
+        String attemptCount = printAndInputAttemptCount();
+        advanceRace(carNames, attemptCount);
+        printWinners();
+    }
+
+    private String printAndInputCarName() {
+        gameView.printCarNamesMessage();
+        String carNames = gameView.inputCarNames();
+        return carNames;
+    }
+
+    private String printAndInputAttemptCount() {
+        gameView.printAttemptCountMessage();
+        String attemptCount = gameView.inputAttemptCount();
+        System.out.println();
+        return attemptCount;
+    }
+
+    private void advanceRace(String carNames, String attemptCount) {
+        gameView.printRaceResult();
+        raceService.initializeRace(attemptCount, carNames);
+        raceService.advanceRace(attemptCount);
+    }
+
+    private void printWinners() {
+        String winners = raceService.selectWinners();
+        gameView.printFinalWinners(winners);
+    }
 }
