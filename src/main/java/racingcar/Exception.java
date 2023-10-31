82dffe024f535226e5f0296142b7c1ba102697c1
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 22:55:14 +0900

    [Fix](Exception) - 예외 메서드 추가

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -1,14 +1,27 @@
 package racingcar;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exception {
 
     private static final int CAR_MAX_LENGTH = 5;
     public void ErrorCheck(String carName) {
+
+    public void lengthCheck(String carName) {
         String[] carNameArray = carName.split(",");
         for (String name: carNameArray) {
             if (name.length() > CAR_MAX_LENGTH) {
                 throw new IllegalArgumentException("5글자 넘어감");
             }
+        }
+    }
+
+    public void stringCompile(String carName) {
+        final Pattern compile = Pattern.compile(REGEX);
+        final Matcher matcher = compile.matcher(carName);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INPUT_FORMAT_ERROR);
         }
     }
 
