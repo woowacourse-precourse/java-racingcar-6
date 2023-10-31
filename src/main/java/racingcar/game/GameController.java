cc9ccb4b5c969dc47239ead3392994591b1aa533
gmu1026 <curiouscomponent@gmail.com>
2023-11-01 03:41:08 +0900

    FEAT: 게임 진행 기능 구현

---
+++ src/main/java/racingcar/game/GameController.java
@@ -0,0 +1,36 @@
+package racingcar.game;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.car.Car;
+import racingcar.car.CarFactory;
+import racingcar.view.CarView;
+import racingcar.view.ResultView;
+
+public class GameController {
+    private GameController() {}
+
+    private static List<Car> cars;
+
+    public static void start() {
+        CarView.printCarCreateMessage();
+        cars = CarFactory.createCar(Console.readLine());
+
+        CarView.printCarMoveMessage();
+        int inputMoveCount = Integer.parseInt(Console.readLine());
+        moveCar(inputMoveCount);
+    }
+
+    private static void moveCar(int inputMoveCount) {
+        for (int tryCount = 0; tryCount < inputMoveCount; tryCount++) {
+            for (Car car:cars) {
+                car.move();
+            }
+
+            ResultView.printResultCoverMessage();
+            ResultView.printMoveResultMessage(cars);
+        }
+
+        ResultView.printWinnerMessage(cars);
+    }
+}
