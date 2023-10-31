da90f9445a060ccd6f65099857376379379fd7ee
SeokEui Hong <hongs7701@naver.com>
2023-10-31 21:05:39 +0900

    refactor: [#1] RacingCarController 리팩터링
    1. 프로그래밍을 작동시키는 가장 상위의 메소드 start를 run으로 수정함. start보다 run이 더 적합하다고 생각했기 때문
    
    2. run의 하위 메소드로 racingcar를 입력하고 List에 저장저장하는 inputracingCarName ,시도 횟수를 입력하고 저장하는 inputTotalTurn의 접근 제한자를 private에서 public으로 바꿈. 자료를 보호하는 역할이 중요하지 않고 테스트 코드를 작성하는 이점이 더 컸다고 생각했기 대문

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class RacingCarController {
     RacingCarService racingCarService = new RacingCarService();
 
-    public void start(){
+    public void run(){
         try{
             List<String> racingCars = inputRacingCarName();
             int totalTurn = inputTotalTurn();
@@ -19,7 +19,7 @@ public class RacingCarController {
         racingCarService.play();
     }
 
-    private List<String> inputRacingCarName() throws IllegalArgumentException{
+    public List<String> inputRacingCarName() throws IllegalArgumentException{
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         List<String> inputRacingCars = new ArrayList<>();
@@ -33,11 +33,13 @@ public class RacingCarController {
 
             inputRacingCars.add(racingCar);
         }
-
+        for(int i = 0; i < racingCarInput.length; i++){
+            System.out.println(racingCarInput[i]);
+        }
         return inputRacingCars;
     }
 
-    private int inputTotalTurn() throws IllegalArgumentException{
+    public int inputTotalTurn() throws IllegalArgumentException{
         System.out.println("시도할 회수는 몇회인가요?");
 
         int inputTotalTurn = Integer.parseInt(Console.readLine());
