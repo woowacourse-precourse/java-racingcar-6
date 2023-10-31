fa65c43cf6c64542242d0ba02cfe17007c6cc961
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 07:18:07 +0900

    test : 사용자 입력 라운드 횟수 예외 처리 테스트 코드 작성

--- src/test/java/racingcar/exception/UserRoundNumberInputExceptionTest.java
+++ src/test/java/racingcar/exception/UserRoundNumberInputExceptionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,15 +16,23 @@ class UserRoundNumberInputExceptionTest {
     private static final String IS_NOT_NUMBER_MESSAGE = "숫자만 입력해주세요";
     private static final String IS_BLANK_MESSAGE = "공백을 입력하시면 안됩니다.";
     private static final String IS_NOT_ZERO_OR_MINUS = "0이나 음수값을 입력하시면 안됩니다.";
-    private String input;
+    private String input = "abc";
     @BeforeEach
     void setUp(){
         userRoundNumberInputException = new UserRoundNumberInputException(input);
     }
-
     @Test
     void 라운드_입력이_숫자가_아닐_경우(){
-        assertThrows(IllegalArgumentException.class, () -> new UserRoundNumberInputException("1"));
+        assertThrows(IllegalArgumentException.class, () -> new UserRoundNumberInputException(input));
     }
 
+    @Test
+    void 라운드_입력이_공백일_경우(){
+        assertThrows(IllegalArgumentException.class, () -> new UserRoundNumberInputException(input));
+    }
+
+    @Test
+    void 라운드_입력이_음수_혹은_0_(){
+        assertThrows(IllegalArgumentException.class, () -> new UserRoundNumberInputException(input));
+    }
 }
