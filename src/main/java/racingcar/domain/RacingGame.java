e231ffb24dcbd32c3e64ebff12dffc27f73400c1
mmalfoy <stpmalfoy@gmail.com>
2023-11-01 01:09:43 +0900

    feat(RacingGame) : 게임 실행 결과 리스트 초기화 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,7 +1,27 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RacingGame {
+
+    private List<String> resultList = new ArrayList<>();
+    private List<String> winner = new ArrayList<>();
+
+    private RacingGame() {
+
+    }
+
+    public void initResultList(List resultList) {
+        List<String> cars = Inputs.getCars();
+        for (int i = 0 ; i < cars.size() ; i++) {
+            resultList.add("");
+        }
+    }
+
 
 
 }
