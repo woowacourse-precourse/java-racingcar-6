204ba8031fa4a3093267d317d3f567a3e23c5268
Seyeon <wjdtpdus828@naver.com>
2023-10-31 19:12:25 +0900

    feat(View): "최종 우승자" 메세지를 출력하는 printLastWinnerMessage 메소드 구현

--- src/main/java/racingcar/view/Views.java
+++ src/main/java/racingcar/view/Views.java
@@ -2,10 +2,13 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.List;
+
 public class Views {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
     private static final String RESULT_NESSAGE = "실행 결과";
+    private static final String LAST_WINNER_MESSAGE = "최종 우승자 : ";
     public void printInputCarMessage(){
         System.out.println(INPUT_CAR_NAME_MESSAGE);
     }
@@ -14,6 +17,7 @@ public class Views {
         System.out.println(INPUT_TRIAL_NUMBER);
     }
     public void printResultMessage(){
+        printLineBreak();
         System.out.println(RESULT_NESSAGE);
     }
     public void printCarName(Car car){
@@ -27,4 +31,10 @@ public class Views {
     public void printLineBreak(){
         System.out.println();
     }
+    public void printLastWinnerMessage(){
+        System.out.print(LAST_WINNER_MESSAGE);
+    }
+
+
+
 }
