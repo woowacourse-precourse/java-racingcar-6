e854fe569177771fd7aac012eb8a0ec2fc9d0e66
KDT live <dnjsxo0616@gmail.com>
2023-10-31 16:34:25 +0900

    refactor(Computer): Randoms 범위 상수 추가
    - `Computer` 클래스의 `CreateRandomNumber` 메서드 내에서 사용되는 랜덤 숫자 범위 상수화
    - `MIN_NUMBER`: 최소값, `MAX_NUMBER`: 최대값

--- src/main/java/racingcar/Computer.java
+++ src/main/java/racingcar/Computer.java
@@ -2,7 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 public class Computer {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     public static Integer CreateRandomNumber(){
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
