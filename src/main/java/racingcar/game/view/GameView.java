f4adb704f07914511fb6dc3ec29bca77c78396a5
nijesmik <bkybb@duck.com>
2023-10-31 11:38:49 +0900

    feat(view): GameView 및 Message 구현

---
+++ src/main/java/racingcar/game/view/GameView.java
@@ -0,0 +1,22 @@
+package racingcar.game.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameView {
+    static public String requestInput(Message requestMessage) {
+        printlnMessage(requestMessage);
+        return Console.readLine();
+    }
+
+    static public void printMessage(Message message) {
+        message.print();
+    }
+
+    static public void printlnMessage(Message message) {
+        message.println();
+    }
+
+    static public void printlnMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/racingcar/game/view/Message.java
@@ -0,0 +1,22 @@
+package racingcar.game.view;
+
+public enum Message {
+    INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_RACE_COUNT("시도할 회수는 몇회인가요?"),
+    RACE_START("\n실행 결과"),
+    WINNER("최종 우승자 : ");
+
+    String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    protected void println() {
+        System.out.println(message);
+    }
+
+    protected void print() {
+        System.out.print(message);
+    }
+}
