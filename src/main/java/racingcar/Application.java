526ad4635f6c490a6b0c4bd12ab6990e5c0a00dc
Jinnie Kim <jinniek48@gmail.com>
2023-10-31 21:48:29 +0900

    refactor(racingcar): 최종 리팩터링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.domain.Car;
 import racingcar.domain.Leaderboard;
 import racingcar.domain.NamesManager;
+import racingcar.domain.RoundNumberManager;
 
 import java.util.List;
 
@@ -11,9 +11,9 @@ public class Application {
 
     public static void main(String[] args) {
 
+        NamesManager namesManager = new NamesManager();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        NamesManager namesManager = new NamesManager();
         List<String> namesList = namesManager.separateNamesString(Console.readLine());
         if (namesManager.checkNamesException(namesList)) {
             throw new IllegalArgumentException("중복되는 이름이나 5 글자 이상의 이름이 있는지 확인해주세요.");
@@ -21,60 +21,25 @@ public class Application {
 
         Leaderboard leaderboard = new Leaderboard(namesList);
 
+        RoundNumberManager roundNumberManager = new RoundNumberManager();
         System.out.println("시도할 회수는 몇회인가요?");
-        int roundNumber;
+
         String roundNumberString = Console.readLine();
-        if (notNumber(roundNumberString)) {
-            throw new IllegalArgumentException("입력하신 값이 숫자가 맞는지 확인해주세요.");
-        }
-        else {
-            roundNumber = Integer.parseInt(roundNumberString);
-
-            if (negativeNumber(roundNumber)) {
-                throw new IllegalArgumentException("입력하신 값이 0 이상의 수가 맞는지 확인해주세요.");
-            }
+        if (roundNumberManager.checkRoundNumberExceptions(roundNumberString)) {
+            throw new IllegalArgumentException("시도할 횟수가 0 이상의 정수가 맞는지 확인해주세요.");
         }
 
-        System.out.println();
-        System.out.println("실행 결과");
+        int roundNumber = Integer.parseInt(roundNumberString);
 
-        while (roundNumber>0){
-            round(leaderboard);
+        System.out.print("\n실행 결과\n");
 
+        while (roundNumber > 0) {
+            leaderboard.round();
             leaderboard.status();
-
             roundNumber--;
         }
 
         leaderboard.winner();
-    }
-
-    public static boolean notNumber(String number){
-        try {
-            Integer.parseInt(number);
-        } catch(NumberFormatException e) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean negativeNumber(Integer number){
-        if (number<0) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static void round(Leaderboard leaderboard){
-        for (String name : leaderboard.board.keySet()){
-            int currentPlace = leaderboard.board.get(name);
-
-            if (new Car().engine()){
-                leaderboard.board.put(name, currentPlace+1);
-            }
-        }
     }
 
 }
