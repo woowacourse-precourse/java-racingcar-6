0faedf2bd4f28c20c1343e5bef22bcc87e592592
“mpfo0106” <mpfo0106@gmail.com>
2023-11-01 01:36:49 +0900

    feat(racingcar): Judgement 구현

--- src/main/java/racingcar/controller/Judgement.java
+++ src/main/java/racingcar/controller/Judgement.java
@@ -1,6 +1,18 @@
 package racingcar.controller;
 
 
+import java.util.List;
+
 public class Judgement {
+    private List<Integer> carRandomNumbers;
+
+    List<Integer> isNumberMoreThan4(List<Integer> carRandomNumbers) {
+        for (int carRandomNumber : carRandomNumbers) {
+            if (carRandomNumber < 4) {
+                carRandomNumber = 0;
+            }
+        }
+        return carRandomNumbers;
+    }
 
 }
