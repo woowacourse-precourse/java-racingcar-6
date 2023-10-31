49d07adcb43e07c6995be574c7b3f451e6b1037c
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-31 15:07:51 +0900

    feat: 게임 실행을 담당하는 RacingCar 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,28 +1,9 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import view.InputView;
-import view.OutputView;
-
 public class Application {
 
 	public static void main(String[] args) {
-		final CarList carList = new CarList();
-		final InputView inputView = new InputView();
-		final OutputView outputView = new OutputView();
-
-		inputView.printCarNameInputMessage();
-		String carNames = Console.readLine();
-		carList.splitCarNames(carNames);
-
-		inputView.printAttemptNumberInputMessage();
-		int attempt = Integer.parseInt(Console.readLine());
-
-		for (int i = 0; i < attempt; i++) {
-			outputView.printResult(carList);
-		}
-
-		outputView.printWinners(carList.getWinners());
-
+		RacingCar racing = new RacingCar();
+		racing.gameStart();
 	}
 }
