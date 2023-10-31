0b1f5c2f0bcd8d281a60a3767f75fa71dbbaf31e
JisooPyo <audwl326@gmail.com>
2023-11-01 01:40:18 +0900

    test(car): 자동자 전진 테스트 완료

--- src/test/java/racingcar/CarNameValidatorTest.java
+++ src/test/java/racingcar/CarNameValidatorTest.java
@@ -3,15 +3,11 @@ package racingcar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarNameValidatorTest {
 	private CarNameValidator carNameValidator;
