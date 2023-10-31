66404289701ccbf3aff567a3d552ce0651b882dd
minseo  jo <liging12@naver.com>
2023-10-31 13:08:14 +0900

    test: vo 관련 테스트 매소드명 변경 + 테스트 추가

--- src/test/java/racingcar/vo/CarIdTest.java
+++ src/test/java/racingcar/vo/CarIdTest.java
@@ -12,16 +12,31 @@ class CarIdTest {
     }
 
     @Test
-    void 같은_VO_동등성_검사() {
+    void 값이_같은_vo는_동등하다() {
         CarId carId1 = new CarId(1);
         CarId carId2 = new CarId(1);
         Assertions.assertThat(carId1.value()).isEqualTo(carId2.value());
     }
 
     @Test
-    void 같은_VO_동일성_검사() {
+    void 값이_같은_vo는_동하다() {
         CarId carName1 = new CarId(1);
         CarId carName2 = new CarId(1);
         Assertions.assertThat(carName1).hasSameHashCodeAs(carName2);
     }
+
+    @Test
+    void 값이_다른_vo는_동등하지않일() {
+        CarId carId1 = new CarId(1);
+        CarId carId2 = new CarId(2);
+        Assertions.assertThat(carId1.value()).isNotEqualTo(carId2.value());
+    }
+
+    @Test
+    void 값이_다른_vo는_동일하지않다() {
+        CarId carName1 = new CarId(1);
+        CarId carName2 = new CarId(2);
+        Assertions.assertThat(carName1).doesNotHaveSameHashCodeAs(carName2);
+    }
+
 }
