b1ca6ed1dfee927a06ce18844d1f74aebe406462
kimky0320 <kim3268418@naver.com>
2023-10-31 14:14:53 +0900

    feat: 시작시 자동차이름 입력 및 시도횟수 입력 요구하는 메시지 출력

---
+++ src/main/java/racingcar/view/Message.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public enum Message {
+    START_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ASK_ATTEMPT("시도할 회수는 몇회인가요?");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public static void printStart() {
+        System.out.println(Message.START_MESSAGE.getMessage());
+    }
+
+    public static void printAsk() {
+        System.out.println(Message.ASK_ATTEMPT.getMessage());
+    }
+
+}
