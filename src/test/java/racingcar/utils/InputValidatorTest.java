710d7c9260409fde12d532e7c92d6f79ed6ef2ef
kim <hwiyul_g@daum.net>
2023-10-31 17:01:30 +0900

    Test(InputValidator) : assertThat 사용과 오류 메시지 확인 테스트 포함

--- src/test/java/racingcar/utils/InputValidatorTest.java
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -1,98 +1,84 @@
 package racingcar.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
 
 class InputValidatorTest {
 
     @Test
     void 다섯_글자_넘는_경우() {
-        List<String> carNames = Arrays.asList("다섯자이상의이름");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+        assertInvalidCarNames(Arrays.asList("다섯자이상의이름"), "자동차 이름은 5자 이하만 가능합니다.");
     }
 
     @Test
     void 중간에_다섯글자_넘는_경우() {
-        List<String> carNames = Arrays.asList("pobi", "다섯글자넘는이름", "jun");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+        assertInvalidCarNames(Arrays.asList("pobi", "다섯글자넘는이름", "jun"), "자동차 이름은 5자 이하만 가능합니다.");
     }
 
     @Test
     void 컴마_연속_두개() {
-        List<String> carNames = Arrays.asList("pobi", "", "jun", "aa");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+        assertInvalidCarNames(Arrays.asList("pobi", "", "jun", "aa"),
+            "자동차 이름은 null 또는 비어 있을 수 없습니다.");
     }
 
     @Test
-    void NULL_테스트(){
-        List<String> carNames = Arrays.asList(null, "", "jun", "aa");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+    void NULL_테스트() {
+        assertInvalidCarNames(Arrays.asList(null, "", "jun", "aa"),
+            "자동차 이름은 null 또는 비어 있을 수 없습니다.");
     }
 
     @Test
-    void 중복_이름이_들어오는_경우(){
-        List<String> carNames = Arrays.asList("a", "a", "b", "c");
-        assertThrows(IllegalArgumentException.class, ()->{
-            InputValidator.validateCarNames(carNames);
-        });
+    void 중복_이름이_들어오는_경우() {
+        assertInvalidCarNames(Arrays.asList("a", "a", "b", "c"), "자동차 이름은 중복될 수 없습니다.");
     }
 
     @Test
     void 빈_문자열() {
-        List<String> carNames = Arrays.asList("");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+        assertInvalidCarNames(Arrays.asList(""), "자동차 이름은 null 또는 비어 있을 수 없습니다.");
     }
 
     @Test
     void 컴마_빈칸_컴마() {
-        List<String> carNames = Arrays.asList("pobi", " ", "jun");
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateCarNames(carNames);
-        });
+        assertInvalidCarNames(Arrays.asList("pobi", " ", "jun"), "자동차 이름은 null 또는 비어 있을 수 없습니다.");
     }
 
     @Test
     void 올바른_입력() {
         List<String> carNames = Arrays.asList("pobi", "jun", "aa");
-        assertDoesNotThrow(()->InputValidator.validateCarNames(carNames));
+        assertThatCode(() -> {
+            InputValidator.validateCarNames(carNames);
+        }).doesNotThrowAnyException();
     }
-
 
     @Test
     void 시도횟수_정수가_아닌경우() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            InputValidator.validateAttemptCount("1.1");
-        });
+        assertInvalidAttemptCount("1.1", "시도 횟수는 정수여야 합니다.");
     }
 
     @Test
     void 시도횟수_0이하_인_경우() {
-        assertThrows(IllegalArgumentException.class, ()->{
-            InputValidator.validateAttemptCount("-1");
-        });
+        assertInvalidAttemptCount("-1", "시도 횟수는 0보다 커야 합니다.");
     }
 
     @Test
     void 시도횟수_숫자가_아닌_경우() {
-        assertThrows(IllegalArgumentException.class, ()->{
-            InputValidator.validateAttemptCount("다섯");
-        });
+        assertInvalidAttemptCount("다섯", "시도 횟수는 정수여야 합니다.");
     }
 
+    private void assertInvalidCarNames(List<String> carNames, String expectedErrorMessage) {
+        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(expectedErrorMessage);
+    }
+
+    private void assertInvalidAttemptCount(String attemptCount, String expectedErrorMessage) {
+        assertThatThrownBy(() -> InputValidator.validateAttemptCount(attemptCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(expectedErrorMessage);
+    }
 
 }
