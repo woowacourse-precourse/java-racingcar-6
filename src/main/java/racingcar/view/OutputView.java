a764b56d157caa7d46caed74b60131447d47adaf
gahyeon <1000ji1@naver.com>
2023-11-01 01:54:10 +0900

    feat : print Car position
    한 판마다 차에 대한 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,24 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
     public static void exeutionResult() {
         System.out.println("실행 결과");
+    }
+
+    public static void resultGame(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.print(car.getName() + " : ");
+            printPosition(car);
+            System.out.println();
+        }
+    }
+
+    private static void printPosition(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
     }
 }
