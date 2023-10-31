5a067a286bd96c19dbe3421a5a73b6949f38b707
HeoManBo <iovesssr__@naver.com>
2023-10-31 21:48:25 +0900

    style: 불필요한 코드 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,7 +12,6 @@ public class InputView {
 
     private final static String CARS_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String READ_ROUND = "시도할 회수는 몇회인가요?";
-    private final static String NUMBER_OF_ATTEMPT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String start(){
         System.out.println(CARS_NAMES_INPUT_MESSAGE);
