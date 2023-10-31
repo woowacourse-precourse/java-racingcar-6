a771d0557ccd5f11d7bdce01c106f833a097d322
hee9841 <lee98417716@gmail.com>
2023-10-31 16:56:30 +0900

    feat(racingcar): 게임 데이터 셋팅 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,5 +1,32 @@
 package racingcar.service;
 
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.view.GameConsoleIO;
+
 public class GameService {
+    private static List<Car> cars;
+    private static GameConsoleIO gameConsoleIO;
+
+    private static int movingCount;
+
+
+    public GameService() {
+        gameConsoleIO = new GameConsoleIO();
+        setCars();
+        setMoveCount();
+    }
+
+
+    private void setCars() {
+        List<String> carNames = gameConsoleIO.getCarNames();
+        carNames.forEach(name -> cars.add(new Car(name)));
+    }
+
+    private void setMoveCount() {
+        movingCount = gameConsoleIO.getMovingCount();
+    }
+
+
 }
