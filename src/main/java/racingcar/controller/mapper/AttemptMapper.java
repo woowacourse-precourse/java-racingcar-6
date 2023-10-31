719f0a24e54d1838c5f2f7324262b1ae52c57a7d
baksakcci <pshsh910@gmail.com>
2023-10-31 22:24:25 +0900

    style($AttemptValidator, AttemptMapper): add newline

--- src/main/java/racingcar/controller/mapper/AttemptMapper.java
+++ src/main/java/racingcar/controller/mapper/AttemptMapper.java
@@ -3,6 +3,7 @@ package racingcar.controller.mapper;
 import racingcar.model.vo.Attempt;
 
 public class AttemptMapper {
+
     public Attempt toAttempt(String attempt) {
         return new Attempt(Integer.parseInt(attempt));
     }
--- src/main/java/racingcar/validation/AttemptValidator.java
+++ src/main/java/racingcar/validation/AttemptValidator.java
@@ -1,6 +1,7 @@
 package racingcar.validation;
 
 public class AttemptValidator {
+
     public void validate(String attempt) {
         if (attempt.isBlank()) {
             throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
