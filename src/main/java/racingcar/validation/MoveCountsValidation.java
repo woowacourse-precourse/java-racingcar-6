251956f15ac46d878e002558469d756651281a88
Yoonseol Lee <thisyoon97@gmail.com>
2023-10-31 19:25:19 +0900

    feat: MoveCounts에 대한 검증기능 수정 MoveCountsValidation 내 검증 항목 및 메서드 일부 수정

--- src/main/java/racingcar/validation/MoveCountsValidation.java
+++ src/main/java/racingcar/validation/MoveCountsValidation.java
@@ -2,37 +2,21 @@ package racingcar.validation;
 
 public class MoveCountsValidation {
     public void validateMoveCountsInput(String input) {
-        validateNumber(input);
-        validateRange(input);
-        validateInteger(input);
-    }
-
-    private void validateNumber(String input) {
-        if (!input.matches("^[0-9]+$")) {
-            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        if (!isInteger(input)) {
+            throw new IllegalArgumentException("정수만 입력이 가능합니다.");
         }
-    }
-
-    private void validateRange(String input) {
-        int moveCounts = parseMoveCounts(input);
-        if (moveCounts <= 0) {
+        int moveCounts = Integer.parseInt(input);
+        if (moveCounts < 1) {
             throw new IllegalArgumentException("1 이상의 정수를 입력해야 합니다.");
         }
     }
 
-    private int parseMoveCounts(String input) {
+    private boolean isInteger(String input) {
         try {
-            return Integer.parseInt(input);
+            Integer.parseInt(input);
+            return true;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("유효한 숫자 형식이 아닙니다.");
-        }
-    }
-
-    private void validateInteger(String input) {
-        try {
-            int moveCounts = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("정수만 입력이 가능합니다.");
+            return false;
         }
     }
 }
