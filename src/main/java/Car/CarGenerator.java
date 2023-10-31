40b0bcfc8677150f93cf16c25a33b28936ecc232
transfer_kk <kseysh123@naver.com>
2023-10-31 21:43:18 +0900

    feat : 사용자에게 값을 받아 Car 인스턴스를 생성하는 기능 구현

--- src/main/java/Car/CarGenerator.java
+++ src/main/java/Car/CarGenerator.java
@@ -1,13 +1,18 @@
+package Car;
+
+import Car.Car;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.Validator;
 
 public class CarGenerator {
     public static List<Car> generateCar(String userInput){
+        Validator.validateCarName(userInput);
         String[] carNames = userInput.split(",");
 
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
-            cars.add(new Car(carName));
+            cars.add(new Car(carName.trim()));
         }
 
         return cars;
