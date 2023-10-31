a02af2403d813e00b0058efed21a175ad8416820
huhdy32 <mong3268@gmail.com>
2023-10-31 14:34:10 +0900

    feat(main/RacingGame): 게임 종료시 플레이어 목록 초기화 기능추가

--- src/main/java/racingcar/logic/RacingGame.java
+++ src/main/java/racingcar/logic/RacingGame.java
@@ -42,6 +42,7 @@ public class RacingGame implements Game {
             view.printRoundProgress(playerList);
         }
         view.printWinner(getWinners());
+        controller.clearPlayer();
     }
 
     private List getWinners() {
