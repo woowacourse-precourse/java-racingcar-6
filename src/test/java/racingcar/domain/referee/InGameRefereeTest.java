73ce2d7cebe371fbcd86d387a3a832ff7b59fee5
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-31 19:38:19 +0900

    style(all): coding convention
    코딩 컨벤션 적용

--- src/test/java/racingcar/domain/referee/InGameRefereeTest.java
+++ src/test/java/racingcar/domain/referee/InGameRefereeTest.java
@@ -8,15 +8,15 @@ import racingcar.domain.car.Car;
 
 class InGameRefereeTest {
     @Test
-    public void isBiggerLowerLimit_LOWER_LIMIT_보다_큰_수인지() throws Exception{
+    public void isBiggerLowerLimit_LOWER_LIMIT_보다_큰_수인지() throws Exception {
         InGameReferee inGameReferee = new InGameReferee();
-        final int LOWER_LIMIT=inGameReferee.getLOWER_LIMIT_Fortest();
-        int 작은수= LOWER_LIMIT-1;
-        int 큰수=LOWER_LIMIT+1;
+        final int LOWER_LIMIT = inGameReferee.getLOWER_LIMIT_Fortest();
+        int 작은수 = LOWER_LIMIT - 1;
+        int 큰수 = LOWER_LIMIT + 1;
 
-        boolean 작은result= inGameReferee.isBiggerLowerLimit(작은수);
-        boolean 큰result= inGameReferee.isBiggerLowerLimit(큰수);
-        boolean 같은result= inGameReferee.isBiggerLowerLimit(LOWER_LIMIT);
+        boolean 작은result = inGameReferee.isBiggerLowerLimit(작은수);
+        boolean 큰result = inGameReferee.isBiggerLowerLimit(큰수);
+        boolean 같은result = inGameReferee.isBiggerLowerLimit(LOWER_LIMIT);
 
         assertThat(작은result).isEqualTo(false);
         assertThat(큰result).isEqualTo(true);
@@ -24,17 +24,17 @@ class InGameRefereeTest {
     }
 
     @Test
-    public void findWinner_우승_판별() throws Exception{
+    public void findWinner_우승_판별() throws Exception {
         Car car1 = new Car("car1");
         Car car2 = new Car("car2");
         Car car3 = new Car("car3");
-        List<Car> carList=List.of(car1,car2,car3);
+        List<Car> carList = List.of(car1, car2, car3);
         car2.forward();
         car3.forward();
 
         InGameReferee inGameReferee = new InGameReferee();
         List<Car> result = inGameReferee.findWinner(carList);
 
-        assertThat(result).isEqualTo(List.of(car2,car3));
+        assertThat(result).isEqualTo(List.of(car2, car3));
     }
 }
