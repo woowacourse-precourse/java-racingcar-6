30c1329e28d4b62a3560c175b1bb164f3192326f
HeungY <rkrkrkrk02@naver.com>
2023-10-31 10:35:48 +0900

    feat: 우승자 판별하여 출력하는 JudgeStopAndGo 구현

--- src/main/java/racingcar/StopAndGo.java
+++ src/main/java/racingcar/StopAndGo.java
@@ -8,25 +8,27 @@ public class StopAndGo {
     InputRacingCar racingCar = new InputRacingCar();
     InputRacingNumber racingNumber = new InputRacingNumber();
 
-    void stopAndGo(){
+    void stopAndGo() {
         List<String> racingCarList = racingCar.inputRacingCar();
         int number = racingNumber.inputRacingNumber();
 
         int racingCarSize = racingCarList.size();
         List<Integer> racingCarScore = new ArrayList<>();
 
-        for(int i = 0; i<racingCarSize; i++){
+        for (int i = 0; i < racingCarSize; i++) {
             racingCarScore.add(0);
         }
 
         SaveStopAndGo save = new SaveStopAndGo();
         PrintStopAndGo print = new PrintStopAndGo();
 
-        for (int i=0;i<number;i++){
+        for (int i = 0; i < number; i++) {
             save.saveStopAndGo(racingCarScore);
-            print.printStopAndGo(racingCarList,racingCarScore);
+            print.printStopAndGo(racingCarList, racingCarScore);
             System.out.println();
         }
 
+        JudgeWinner winner = new JudgeWinner();
+        winner.judgeWinner(racingCarList, racingCarScore);
     }
 }
