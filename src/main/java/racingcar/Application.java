47203bea81e2ea5771b225f339c31c8288686e76
codms85 <lce0805@naver.com>
2023-11-01 00:04:55 +0900

    Update Print

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,20 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 	public static String[] get_user() { // 사용자 이름 입력받기
 		String input = Console.readLine();
-		System.out.println(input);
 		String[] user = input.split(",");
-		for(int i=0; i<user.length;i++) {
-			if(user[i].length()>5) {
-				 throw new IllegalArgumentException();
+		for (int i = 0; i < user.length; i++) {
+			if (user[i].length() > 5) {
+				throw new IllegalArgumentException();
 			}
 		}
 		return user;
 	}
-	
+
 	public static int get_move() { // 움직일 값 입력받기
 		int result; // 승패 여부
 
@@ -26,7 +26,7 @@ public class Application {
 		}
 		return result;
 	}
-	
+
 	public static void print_result(String name, int num) { // 결과 출력하기
 		System.out.print(name + " : ");
 		for (int i = 0; i < num; i++) {
@@ -34,7 +34,7 @@ public class Application {
 		}
 		System.out.println();
 	}
-	
+
 	public static void print_winner(String[] name, int[] num, int usernum) {
 		int max = 0; // 최댓값
 		int[] result_name = {}; // 우승자 이름 목록
@@ -54,16 +54,14 @@ public class Application {
 			}
 		}
 	}
-	
-	
-	
-    public static void main(String[] args) {
-    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+	public static void main(String[] args) {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
 		// 사용자 이름 입력받기
 		String[] user = get_user();
 		int usernum = user.length; // 사용자 인원
-		
+
 		int[] result = new int[usernum]; // 결과값
 
 		System.out.println("시도할 회수는 몇회인가요?");
@@ -71,7 +69,7 @@ public class Application {
 		// 시도할 횟수 입력받기
 		String s = Console.readLine();
 		int num = Integer.parseInt(s);
-		
+
 		// 실행결과 출력하기
 		System.out.println("\n실행 결과");
 		while (num != 0) {
@@ -82,10 +80,10 @@ public class Application {
 			System.out.println();
 			num--;
 		}
-		
+
 		// 최종 우승자 출력하기
 		System.out.print("최종 우승자 : ");
 
 		print_winner(user, result, usernum);
-    }
+	}
 }
