e05aa966db8e8998b52798544d0fc8e44a4cf06a
hyun-hyang <jimin3060@naver.com>
2023-11-01 03:48:06 +0900

    fix: typo

--- src/main/java/racingcar/ExceptionMessage.java
+++ src/main/java/racingcar/ExceptionMessage.java
@@ -3,7 +3,7 @@ package racingcar;
 public enum ExceptionMessage {
     OUT_OF_CAR_NAME_LENGTH("자동차의 이름은 6글자를 넘길 수 없습니다."),
     BLANK_CAR_NAME("자동차의 이름이 공백이 될 수 없습니다."),
-    DUPLICATE_CAR_NAME("자동차의 이름이 중복됩니다."),
+    DUPLICATE_CAR_NAME("자동차의 이름이 중복됩니다.");
 
     private final static String EXCEPTION_HEADER = "[error] ";
     private final String message;
