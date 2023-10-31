38e571a6e4489442bfa77bc98d833d10b435508c
Yujin <tkfkd7038@naver.com>
2023-11-01 01:48:44 +0900

    feat(racingcar): 가장 많이 전진한 자동차의 인덱스를 구하는 기능 구현

--- src/main/java/racingcar/WinnerManager.java
+++ src/main/java/racingcar/WinnerManager.java
@@ -16,7 +16,11 @@ public class WinnerManager {
 
 
     public void getIndexOfMaxForwardCount() { // 가장 많이 전진한 자동차의 인덱스 구하기
-
+        for (int i = 0; i < car.length - 1; i++) {
+            if (car[maxIndex].forwardCount < car[i + 1].forwardCount) {
+                maxIndex = i + 1;
+            }
+        }
     }
 
     public void findValuesEqualToMax() { // 동점인 우승자가 있는지 구하기
