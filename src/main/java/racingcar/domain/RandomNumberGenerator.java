1bedf05056bea4e3627e53a04fefe8b4d3d492f0
yerimya <syl1602@naver.com>
2023-10-31 16:31:36 +0900

    [feat]randomnumber 생성 도메인 추가

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,2 +1,12 @@
-package racingcar.domain;public class RandomNumberGenerator {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int MIN_NUMBER_SIZE = 0;
+    private static final int MAX_NUMBER_SIZE = 9;
+
+    public int generate() {
+        return Randoms.pickNumberInRange(MIN_NUMBER_SIZE, MAX_NUMBER_SIZE);
+    }
 }
