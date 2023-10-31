7a52d3d58a35bb7e10940c0fc6f6ae44f3720c8a
MyStraw <kikongdosa@gmail.com>
2023-11-01 03:10:29 +0900

    refactor(Car): Practice로 연습한 코드로 다시 재정비
    random부분은 Race에 넘기기

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,21 +2,32 @@ package racingcar.domain;
 
 public class Car {
     private String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
-
         this.name = name;
+        this.position = 0;
     }
 
-    public void move(int randomValue) {
-        if (randomValue >= 4){
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
             position++;
         }
     }
 
     public int getPosition() {
-
         return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMoveHyphen() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position ; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
     }
 }
