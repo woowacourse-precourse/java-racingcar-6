c0bbd6b1826106063f3ff83b8ac368440f0cc946
HaLin Kim <halin130@tukorea.ac.kr>
2023-10-31 22:15:43 +0900

    test: 경주할 자동차 이름 입력 요청 메시지 출력 테스트 추가

--- src/main/java/racingcar/view/OutputManager.java
+++ src/main/java/racingcar/view/OutputManager.java
@@ -3,11 +3,11 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputManager {
-    private static final String REQUEST_CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String REQUEST_GAME_COUNT_INPUT = "시도할 회수는 몇회인가요?";
-    private static final String RESULT = "실행 결과";
-    private static final String WINNER = "최종 우승자 : ";
-    private static final String GAME_OVER = "게임을 종료합니다...";
+    public static final String REQUEST_CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String REQUEST_GAME_COUNT_INPUT = "시도할 회수는 몇회인가요?";
+    public static final String RESULT = "실행 결과";
+    public static final String WINNER = "최종 우승자 : ";
+    public static final String GAME_OVER = "게임을 종료합니다...";
 
     public void printWithNewLine(String s) {
         System.out.println(s);
---
+++ src/test/java/racingcar/OutputManagerTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.view.OutputManager;
+
+class OutputManagerTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 경주할_자동차_입력요청_메시지_출력() {
+        // given
+        OutputManager outputManager = new OutputManager();
+
+        // when
+        outputManager.printRequestCarNameInputMessage();
+
+        // then
+        assertThat(outputStreamCaptor.toString().trim()).contains(OutputManager.REQUEST_CAR_NAME_INPUT);
+    }
+}
