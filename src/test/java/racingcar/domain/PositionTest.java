a5491c28d97ad5afa8a10c7dc82829cfa2c05aab
GaHee99 <u1879@sookmyung.ac.kr>
2023-10-31 16:33:44 +0900

    feat(PositionTest): Postion validation 테스트코드 작성

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -1,5 +1,29 @@
 package racingcar.domain;
 
-public class PositionTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
 
+import org.junit.jupiter.api.Test;
+import racingcar.model.Position;
+import racingcar.util.Constants;
+
+public class PositionTest {
+    private final int distance = Integer.parseInt(Constants.MINIMUM_DISTANCE_SIZE.constant);
+    @Test
+    void 기준_미달_랜덤값에_따른_정지() {
+        Position position = new Position();
+        int notGoDistance = distance - 1;
+
+        position.validatePosition(notGoDistance);
+        assertThat(position.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 기준_이상_랜덤값에_따른_이동() {
+        Position position = new Position();
+        int goDistance = distance;
+
+        position.validatePosition(goDistance);
+        assertThat(position.getPosition()).isEqualTo(1);
+    }
 }
