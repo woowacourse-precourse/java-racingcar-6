d3bdfdeb0c1dc86dec95fb828e72126caffeea14
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-01 05:20:59 +0900

    test: 진행 라운드 1 미만 입력 예외 처리 테스트
    0, -1, -100 넣어봐서 테스트함

--- src/test/java/racingcar/validator/RoundValidatorTest.java
+++ src/test/java/racingcar/validator/RoundValidatorTest.java
@@ -1,6 +1,9 @@
 package racingcar.validator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -8,6 +11,7 @@ public class RoundValidatorTest {
     private RoundValidator roundValidator;
 
     private final static String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요.";
+    private final static String NUMBER_WILL_MORE_THAN_ONE = "1이상의 라운드를 숫자로 입력해주세요";
 
     @Test()
     void 진행_라운드_숫자_외_문자_입력_예외_처리() {
@@ -21,5 +25,13 @@ public class RoundValidatorTest {
         assertThatThrownBy(() -> roundValidator = new RoundValidator(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NOT_NUMBER_ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "0", "-1", "-100" }) // 여러 입력값
+    void 진행_라운드_1_미만_입력_예외_처리(String input) {
+        assertThatThrownBy(() -> new RoundValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NUMBER_WILL_MORE_THAN_ONE);
     }
 }
