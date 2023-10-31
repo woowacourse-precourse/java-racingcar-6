a3011cd2fb22db281080e3188106c65b4473b6ed
gahyeon <1000ji1@naver.com>
2023-10-31 16:35:10 +0900

    feat : get the number of attempts
    시도할 횟수 입력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,15 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     public String carNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return read();
+    }
+
+    public String attemptCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return read();
+    }
+
+    public String read() {
         return readLine();
     }
 }
