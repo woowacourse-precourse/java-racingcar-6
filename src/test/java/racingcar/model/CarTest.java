3c2b75e82d095b034ba22418e45c80e34e030904
kim <hwiyul_g@daum.net>
2023-10-31 15:58:47 +0900

    Test(Car) : assertEquals를 assertThat을 이용함으로써 어떤 오류가 났는지 확인 가능하게 변경

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,7 +1,7 @@
 package racingcar.model;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import racingcar.service.StubMovementStrategy;
@@ -10,17 +10,19 @@ class CarTest {
 
 
     @Test
-    void 자동차_멈춤_테스트(){
-        Car car = new Car("a",0, new StubMovementStrategy(1));
+    void 자동차_멈춤_테스트() {
+        Car car = new Car("a", 0, new StubMovementStrategy(1));
         Car movedCar = car.attemptMove();
-        assertEquals(0,movedCar.distance());
+
+        assertThat(movedCar.distance()).isEqualTo(0);
     }
 
     @Test
-    void 자동차_이동_테스트(){
-        Car car = new Car("a", 0, new StubMovementStrategy((4)));
+    void 자동차_이동_테스트() {
+        Car car = new Car("a", 0, new StubMovementStrategy(4));
         Car movedCar = car.attemptMove();
-        assertEquals(1, movedCar.distance());
+
+        assertThat(movedCar.distance()).isEqualTo(1);
     }
 
 }
