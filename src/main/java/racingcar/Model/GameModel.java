cdd334b8f9e4cfe165ac8cba6090814230a13df0
twinson37 <twinson37@hanyang.ac.kr>
2023-10-31 22:11:05 +0900

    Style: 게임 모델 코드 간격 변경

--- src/main/java/racingcar/Model/GameModel.java
+++ src/main/java/racingcar/Model/GameModel.java
@@ -6,30 +6,36 @@ public class GameModel {
 
     int carNumber;
     int coinNumber;
+    ArrayList<CarModel> winnerCar = new ArrayList<>();
+
 
     public void setCarNumber(int carNumber) {
+
         this.carNumber = carNumber;
     }
 
     public void setCoinNumber(int coinNumber) {
+
         this.coinNumber = coinNumber;
     }
 
     public int getCarNumber() {
+
         return carNumber;
     }
 
     public int getCoinNumber() {
+
         return coinNumber;
     }
 
     public ArrayList<CarModel> getWinnerCar() {
+
         return winnerCar;
     }
 
-    ArrayList<CarModel> winnerCar = new ArrayList<>();
-
     public void setWinnerCar(CarModel winnerCar) {
+
         this.winnerCar.add(winnerCar);
     }
 }
