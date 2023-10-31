72bff7135f0e336e61d6eeb1eed89e155a16c95d
Chun Hyunwoo <1000chw@gmail.com>
2023-11-01 01:33:13 +0900

    feat: Car model init

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,6 +1,22 @@
 package racingcar.model;
 
 public class Car {
-    public Car() {
+    private String carName;
+    private int moveLength;
+    public Car(String name) {
+        carName = name;
+        moveLength = 0;
+    }
+
+    public int getMoveLength() {
+        return moveLength;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void moveForward(){
+        moveLength++;
     }
 }
