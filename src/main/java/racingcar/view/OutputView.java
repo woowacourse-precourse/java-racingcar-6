a9ad749c125c61581404c14e6ec5978cd2a46802
HongYeseul <ghd8119@naver.com>
2023-10-31 10:12:10 +0900

    :sparkles: feat: InputView, OutputView 기본 기능 만들기

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
 public class OutputView {
+    // TODO: Message enum으로 빼기
+    private final static String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String ASK_GAME_ROUNDS = "시도할 횟수는 몇회인가요?";
+    private final static String GAME_RESULT = "실행 결과";
+    private final static String DISPLAY_USER_SCORE_SIGN = ":";
+    private final static String SCORE_SIGN = "-";
+
+    public void startGame(){
+        System.out.println(START_MESSAGE);
+    }
+
+    public void askGameRounds(){
+        System.out.println(ASK_GAME_ROUNDS);
+    }
+
+    public void gameResult(){
+        System.out.println(GAME_RESULT);
+    }
 }
