987d1004286acce1162e67030f608a12aa55a992
dhl1924 <dhl1924@naver.com>
2023-11-01 00:32:40 +0900

    feat: Implemented MessageConstants class

---
+++ src/main/java/racingcar/constants/MessageConstants.java
@@ -0,0 +1,10 @@
+package racingcar.constants;
+
+public class MessageConstants {
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String INVALID_CAR_NAME = "5자리 이하로만 입력해주세요. 공백은 포함할 수 없습니다.";
+    public static final String INVALID_TRY_COUNT = "1회 이상 입력해주세요.";
+    public static final String INVALID_NUMBER_FORMAT = "숫자를 입력해 주세요";
+    public static final String FINAL_WINNERS = "최종 우승자 : ";
+}
