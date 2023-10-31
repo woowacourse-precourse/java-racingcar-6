b511599b2d36f710745c8820f297b4666eb198a9
hil679 <hil679922@gmail.com>
2023-11-01 02:34:17 +0900

    Test(Car): 자동차 전진 횟수 조회 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +19,15 @@ public class CarTest {
     @Test
     void 자동차_이름_조회() {
         assertThat(car1.getName()).isEqualTo("robin");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    void 자동차_전진_횟수_조회(int forwardNum) {
+        for(int i = 0; i < forwardNum; i++){
+            car1.increaseForwardNum();
+        }
+        assertThat(car1.getForwardNum()).isEqualTo(forwardNum);
     }
 
     @Test
