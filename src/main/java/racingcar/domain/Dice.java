e8b023b77b8085e9fdf16d579b1f762898d9c204
minjun7410 <minjun7410@gmail.com>
2023-10-31 22:56:55 +0900

    refactor(Dice): Dice 클래스 상수 추가
    * 성공 판정을 위한 기준점, 랜덤 수를 생성할 때 최소값, 최대값을 위한 상수를 생성했다.

--- src/main/java/racingcar/domain/Dice.java
+++ src/main/java/racingcar/domain/Dice.java
@@ -3,14 +3,18 @@ package racingcar.domain;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Dice {
+    private final static int BASE_LINE_FOR_JUDGE_SUCCESS = 4;
+    private final static int MINIMUM_NUMBER_FOR_PICK_NUMBER = 0;
+    private final static int MAXIMUM_NUMBER_FOR_PICK_NUMBER = 9;
+
     private Dice() {
     }
 
     public static boolean isSuccess() {
-        return (getRandomNumber() >= 4);
+        return (getRandomNumber() >= BASE_LINE_FOR_JUDGE_SUCCESS);
     }
 
     private static int getRandomNumber() {
-        return pickNumberInRange(0, 9);
+        return pickNumberInRange(MINIMUM_NUMBER_FOR_PICK_NUMBER, MAXIMUM_NUMBER_FOR_PICK_NUMBER);
     }
 }
