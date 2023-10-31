6a025be2c89fe2821d9655ed8d04c7324f4e0d53
realcold0 <realcold0@gmail.com>
2023-11-01 05:49:16 +0900

    feat : OutputView에서 현재 회차의 이동을 출력, 우승자를 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Cars;
 
 public class OutputView {
     static private String STRING_START_GAME = "실행 결과";
+    static private String STRING_RESULT = "최종 우승자 : ";
 
     public void printStartGame() {
         System.out.println();
@@ -11,8 +14,25 @@ public class OutputView {
     }
 
     public void printCurrentGame(Cars cars) {
+        StringBuilder sb = new StringBuilder();
         cars.getCars().stream().forEach(car -> {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+            sb.append(car.getName());
+            sb.append(" : ");
+            sb.append("-".repeat(car.getDistance()));
+            sb.append("\n");
         });
+        System.out.println(sb);
+    }
+
+    public void printResult(List<String> winners) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(STRING_RESULT);
+        if (winners.size() == 1) {
+            sb.append(winners.get(0));
+        }
+        if (winners.size() > 1) {
+            sb.append(winners.stream().collect(Collectors.joining(", ")));
+        }
+        System.out.println(sb);
     }
 }
