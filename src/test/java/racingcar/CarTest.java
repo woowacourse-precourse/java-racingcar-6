f836b0299da1f463b8d515a9ceb261e6652d1410
voyager003 <miraclex000@gmail.com>
2023-10-31 15:53:18 +0900

    test: Car 클래스 테스트 코드  및 테스트 주석 추가
    Car의 increaseMovingCount() 메서드의 동작을 확인하는 테스트 코드 작성 및 성공
    테스트 코드 가독성을 위한 given/when/then절을 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,17 +1,37 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
+
+    @Test
+    @DisplayName("Car의 movingCount를 증가시키는 메서드 동작을 확인한다.")
+    void increaseMovingCount_successful() {
+        /**
+         * given : Car 객체를 생성한다.
+         * when : increaseMovingCount 메서드를 호출한다.
+         * then : Car의 count 예상값은 1이다.
+         */
+        Car car = new Car("rome", 0);
+        car.increaseMovingCount();
+
+        assertThat(car.getMovingCount()).isEqualTo(1);
+    }
 
     @Test
     @DisplayName("최종 우승자를 판별하는 메서드의 동작을 확인한다")
     void getRacingGameWinner_winnerIsRome() {
+        /**
+         * given : 3대의 Car 객체를 생성하여 List에 담는다.
+         * when : getRacingGameWinner 메서드를 호출한다.
+         * then : count가 가장 높은 이름인 "rome"이 gameWinner의 유일한 값이다.
+         */
 
         List<Car> carNames = List.of(
                 new Car("rome", 3),
@@ -20,12 +40,17 @@ class CarTest {
         );
 
         List<String> gameWinner = Car.getRacingGameWinner(carNames);
-        Assertions.assertThat(gameWinner).containsExactly("rome");
+        assertThat(gameWinner).containsExactly("rome");
     }
 
     @Test
     @DisplayName("공동 우승자를 판별하는 메서드의 동작을 확인한다")
     void getRacingGameWinner_winnerIsCommon() {
+        /**
+         * given : 3대의 Car 객체를 생성하여 List에 담는다.
+         * when : getRacingGameWinner 메서드를 호출한다.
+         * then : count가 가장 높으면서 동률인 "rome"과 "seoul"이 gameWinner의 값이다.
+         */
 
         List<Car> carNames = List.of(
                 new Car("rome", 3),
@@ -34,7 +59,6 @@ class CarTest {
         );
 
         List<String> gameWinner = Car.getRacingGameWinner(carNames);
-        Assertions.assertThat(gameWinner).contains("rome");
-        Assertions.assertThat(gameWinner).contains("seoul");
+        assertThat(gameWinner).contains("rome", "seoul");
     }
 }
