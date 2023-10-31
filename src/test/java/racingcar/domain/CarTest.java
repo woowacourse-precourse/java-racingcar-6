d089aa5325671695c5cfb2420e8fc4d0a872e945
Chan Mi <rkdcksal02@gmail.com>
2023-11-01 05:09:18 +0900

    무작위 값 4 미만 정지 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CarTest {
@@ -13,5 +12,19 @@ public class CarTest {
             car.move();
         }
         assertTrue(car.getPosition() > 0);
+    }
+    @Test
+    void 자동차_전진_테스트_4미만이면_정지() {
+        Car car = new Car("car");
+
+        while (true) {
+            int initialPosition = car.getPosition(); // 초기 위치 저장
+            car.move();
+            int finalPosition = car.getPosition(); // 최종 위치 저장
+
+            if (initialPosition == finalPosition) {
+                break; // 만약 초기 위치와 최종 위치가 같다면 루프를 나옴
+            }
+        }
     }
 }
