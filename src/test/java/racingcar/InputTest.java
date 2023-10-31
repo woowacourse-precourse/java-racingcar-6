3641123ac446ad83e059b2c179f26c84bb3088ab
KimSehoon <shggm2000@gmail.com>
2023-10-31 17:20:54 +0900

    test: validateCarNameLength_test method 구현
    - getCarNameList_test method가 인자 값을 받도록 변경
    - 자동차 이름 입력 시 길이가 5 초과이면 IllegalArgumentException 예외 발생을 테스트 하는 validateCarNameLength_test method 구현

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -8,25 +9,28 @@ import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.view.Input;
 
 public class InputTest {
     Input input = new Input();
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = "pobi,woni,jun")
     @DisplayName("경주 게임에 참여할 자동차 이름 입력 테스트")
-    void getCarNameList_test() {
+    void getCarNameList_test(String carNames) {
         List<String> carNameListForTest;
 
         try {
-            command("pobi,woni,jun");
+            command(carNames);
             carNameListForTest = input.getCarNameList();
+
+            assertEquals(3, carNameListForTest.size());
+            assertThat(carNameListForTest).containsExactly("pobi", "woni", "jun");
         } finally {
             Console.close();
         }
-
-        assertEquals(3, carNameListForTest.size());
-        assertThat(carNameListForTest).containsExactly("pobi", "woni", "jun");
     }
 
     @Test
@@ -42,6 +46,14 @@ public class InputTest {
         }
 
         assertThat(tryCountForTest).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 입력 시 길이가 5 초과이면 IllegalArgumentException 예외 발생 테스트")
+    void validateCarNameLength_test() {
+        assertThatThrownBy(() -> getCarNameList_test("pobi,javaji,jun"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력이 잘못되었습니다.");
     }
 
     private void command(final String... args) {
