6096ea3d5a3fab6ede98f6126393bf786d17b18d
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-31 14:39:42 +0900

    경주할 자동차이름 입력 받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String[] inputName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] userInput = Console.readLine().split(",");
+
+        return userInput;
+    }
 }
