d6330486aee96d728fd137065a7faa69dfee6532
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-31 16:03:46 +0900

    test : 자동차 위치에 대한 필드명의 변경에 따라 테스트 코드의 필드명도 동일하게 변경

--- src/test/java/racingcar/vo/CarPositionTest.java
+++ src/test/java/racingcar/vo/CarPositionTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class carPositionTest {
+class CarPositionTest {
 
     @DisplayName("필드값이 동일하면 레코드는 동일하다.")
     @Test
