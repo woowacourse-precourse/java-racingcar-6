d12891515b4316a8ccc6ce63e9993f8804e390d2
jinchiim <nellroll@syuin.ac.kr>
2023-10-31 21:01:33 +0900

    refactor: Car 도메인 수정
    Car 도메인 수정
    - 조건식은 따로 두어, Car이 호출할때 움직이도록 수정

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,46 +1,26 @@
 package racingcar.domain.car;
 
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.util.RandomUtil;
-import racingcar.util.Validator.Validator;
-
 public class Car {
 
-    private String name;
+    private final String name;
 
-    private int distance;
+    private int distance = 0;
 
-    public Car() {
-    }
-
-    public Car(String name, int distance) {
+    public Car(String name) {
         this.name = name;
-        this.distance = distance;
     }
 
-    public Car of(String name){
-        return new Car(name, 0);
-    }
-
-    public String getCarName(){
+    public String getCarName() {
         return this.name;
     }
 
-    public int getDistance(){
+    public int getDistance() {
         return this.distance;
     }
 
-    public void updateCarDistance(){
-        int randomNumber = RandomUtil.generateRandomNumber();
-        boolean isCanForward = Validator.isCanForward(randomNumber);
-        if (isCanForward){
-            distance += 1;
-        }
+    public void moveCar() {
+        distance++;
     }
 
-    public List<Car> createCars(){
-        return new ArrayList<Car>();
-    }
 
 }
