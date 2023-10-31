689ddebfe10737bb00a1443161c08fe7795b3a13
CHAHANS <herodev0129@gmail.com>
2023-10-31 14:30:13 +0900

    Test(Game): play테스트 코드 작성

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,29 +1,33 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
+    static String[] cars;
+    static Game game;
+
+    @BeforeEach
+    void setup() {
+        cars = new String[]{"hans", "jins"};
+        game = new Game(cars, 3);
+    }
 
     @Test
-    public void Game_생성(){
-        String[] cars = {"hans", "jins"};
-        Game gm = new Game(cars, 1);
-        for (int i = 0; i < cars.length; i++){
-            assertThat(gm.players[i].name).isEqualTo(cars[i]);
-            assertThat(gm.players[i].location).isEqualTo(0);
+    public void Game_생성() {
+        for (int i = 0; i < cars.length; i++) {
+            assertThat(game.players[i].name).isEqualTo(cars[i]);
+            assertThat(game.players[i].location).isEqualTo(0);
         }
     }
 
     @Test
-    public void Game_play의_테스트(){
-        String[] cars = {"hans", "jins"};
-        Game gm = new Game(cars, 1);
-        gm.play(gm);
-
-        for (int i=0; i<cars.length; i++){
-            System.out.println(i+"번 자동차의 이름 : "+gm.players[i].name);
-            System.out.println(i+"번 자동차의 위치 : "+gm.players[i].location);
+    public void Game_play의_테스트() {
+        game.play(game);
+        for (int i = 0; i < cars.length; i++) {
+            assertThat(game.players[i].location<=game.playNum).isTrue();
         }
     }
 }
