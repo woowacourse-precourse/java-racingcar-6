4f599196fc601f60e947363d8389d9ad01429e4e
rbgusdlza <rbgusdlza@gmail.com>
2023-10-31 19:49:27 +0900

    feat: CarRacing class에 문자열 상수 추가

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -6,5 +6,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarRacing {
+    private static final String PROMPT_ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_ENTER_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String ERROR_NO_INPUT = "[error] 값을 입력하세요.";
+    private static final String ERROR_WRONG_INPUT = "[error] 숫자나 영어로 이루어진 자동차 이름을 입력하세요.";
+    private static final String ERROR_WRONG_FORMAT = "[error] 잘못된 형식의 입력입니다.";
+    private static final String ERROR_DUPLICATE_CAR_NAME = "[error] 자동차 이름이 중복되었습니다.";
+    private static final String ERROR_CAR_NAME_LENGTH = "[error] 자동차 이름의 길이는 1이상 5이하 이어야 합니다.";
+    private static final String ERROR_WRONG_COUNT = "[error] 시도 횟수는 1이상의 정수 이어야 합니다.";
 
+    
 }
