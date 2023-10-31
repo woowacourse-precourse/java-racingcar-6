cc9cf52e69630a3e3d1f5e079e39dbb847ed2e7c
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-31 23:21:09 +0900

    test : RaceController 에서 분리된 메서드에 대한 테스트 추가

--- src/test/java/racingcar/RaceControllerTest.java
+++ src/test/java/racingcar/RaceControllerTest.java
@@ -9,10 +9,14 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.CarDto;
 
 class RaceControllerTest {
     private static RaceController raceController;
@@ -30,13 +34,24 @@ class RaceControllerTest {
     @AfterEach
     void tearDown() {
         System.setOut(standardOut);
-        System.out.println(output());
         Console.close();
         raceController = null;
     }
 
+    /*
+     * 공백제거
+     * */
     final String output() {
         return captor.toString().trim();
+    }
+
+
+    /*
+     * 입력문자열에 대한 입력스트림 생성
+     * */
+    final void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 
     @Test
@@ -62,24 +77,74 @@ class RaceControllerTest {
         final int STOP = 3;
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("pobi,woni", "1");
+                    // GIVEN
+                    command("pobi,woni", "1");
+
+                    // WHEN
+                    raceController.startRaceGame();
+
+                    // THEN
                     assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
                 },
                 MOVE_FORWARD, STOP
         );
     }
 
-    void run(final String... args) {
-        try {
-            command(args);
-            raceController.startRaceGame();
-        } finally {
-            tearDown();
-        }
+    @Test
+    @DisplayName("시도 횟수만큼 게인 진행 후, 우승자를 구합니다.")
+    public void getWinnersAfterMultipleRounds() {
+        final int MOVE_FORWARD = 4;
+        final int STOP = 3;
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // GIVEN
+                    List<Car> cars = new ArrayList<>();
+                    cars.add(Car.createCar("pobi", 0));
+                    cars.add(Car.createCar("woni", 0));
+                    int round = 4;
+
+                    // WHEN
+                    CarDto actualDto = raceController.getWinnerDto(cars, round);
+
+                    // THEN
+                    CarDto expectedDto = new CarDto("pobi");
+                    assertEquals(actualDto.carNames(), expectedDto.carNames());
+                },
+                MOVE_FORWARD, STOP
+        );
     }
 
-    void command(final String... args) {
-        final byte[] buf = String.join("\n", args).getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+    @Test
+    @DisplayName("시도 횟수를 입력받습니다.")
+    public void getRoundUserInput() {
+        // GIVEN
+        command("3");
+
+        // WHEN
+        int actualRound = raceController.getRound();
+
+        // THEN
+        int expectedRound = 3;
+        assertEquals(expectedRound, actualRound);
+    }
+
+    @Test
+    @DisplayName("자동차들을 입력받습니다.")
+    public void getCarsFromUserInput() {
+        // GIVEN
+        command("pobi,jiwon,tony,jihoo");
+
+        // WHEN
+        List<Car> actualCars = raceController.getCars();
+        List<String> actualCarNames = actualCars.stream()
+                .map(car -> car.getName()).toList();
+        List<Integer> actualCarLocations = actualCars.stream()
+                .map(car -> car.getCurrentLocation()).toList();
+
+        // THEN
+        assertThat(actualCarNames).containsExactly(
+                "pobi", "jiwon", "tony", "jihoo");
+        assertThat(actualCarLocations).containsExactly(
+                0, 0, 0, 0);
     }
 }
