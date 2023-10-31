4b301058636d88902a7f6b8f22f48ba1d3181291
minjumost <95904729+minjumost@users.noreply.github.com>
2023-11-01 01:10:45 +0900

    refactor: 코드스타일 반영 및 no usage file 삭제

--- src/main/java/racingcar/Model/Game.java
+++ src/main/java/racingcar/Model/Game.java
@@ -4,10 +4,8 @@ import static racingcar.Util.Constants.MAX_LENGTH_NAME;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -21,9 +19,9 @@ public class Game {
     private List<Car> carList = new ArrayList<>();
     private int repetition;
 
-    public Game(List<String> carNames){
+    public Game(List<String> carNames) {
         isDuplicated(carNames);
-        for(String carName: carNames){
+        for (String carName : carNames) {
             isLessThanMaxLength(carName);
             isBlank(carName);
             Car car = new Car(carName);
@@ -32,53 +30,53 @@ public class Game {
         repetition = 0;
     }
 
-    private void isLessThanMaxLength(String name){
-        if(name.length() > MAX_LENGTH_NAME){
+    private void isLessThanMaxLength(String name) {
+        if (name.length() > MAX_LENGTH_NAME) {
             throw new IllegalArgumentException("최대 길이 보다 긴 이름을 입력했습니다.");
         }
     }
 
-    private void isBlank(String name){
-        if(name.isBlank()){
+    private void isBlank(String name) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("이름을 제대로 작성해주세요.");
         }
     }
 
-    private void isDuplicated(List<String> nameList){
+    private void isDuplicated(List<String> nameList) {
         Set<String> nameSet = new HashSet<String>(nameList);
-        if (nameSet.size() != nameList.size()){
+        if (nameSet.size() != nameList.size()) {
             throw new IllegalArgumentException("중복된 이름을 사용하였습니다.");
         }
     }
 
-    public void setRepetition(int repetition){
+    public void setRepetition(int repetition) {
         isPositiveNumber(repetition);
         this.repetition = repetition;
     }
 
-    public void isPositiveNumber(int num){
-        if(num < 0){
+    public void isPositiveNumber(int num) {
+        if (num < 0) {
             throw new IllegalArgumentException("입력한 숫자가 잘못 됐습니다.");
         }
     }
 
-    public void forward(){
+    public void forward() {
         System.out.println("\n실행 결과");
-        for(int i=0; i<repetition; i++){
-            for(Car car: carList){
-                    car.go(Randoms.pickNumberInRange(1,9));
+        for (int i = 0; i < repetition; i++) {
+            for (Car car : carList) {
+                car.go(Randoms.pickNumberInRange(1, 9));
             }
             System.out.println();
         }
     }
 
-    public void result(){
+    public void result() {
         carList.sort(comparator);
         Car winner = carList.get(0);
         winner.win();
-        for(int i =1; i< carList.size(); i++){
+        for (int i = 1; i < carList.size(); i++) {
             Car car = carList.get(i);
-            if(car.getDistance() == winner.getDistance()){
+            if (car.getDistance() == winner.getDistance()) {
                 car.jointWin();
             }
         }
--- src/main/java/racingcar/View/TextInterface.java
+++ src/main/java/racingcar/View/TextInterface.java
@@ -6,29 +6,29 @@ import static racingcar.Util.Constants.ENTER_USER;
 import camp.nextstep.edu.missionutils.Console;
 
 public class TextInterface {
-    public String enterPlayer(){
+    public String enterPlayer() {
         System.out.println(ENTER_USER);
         return Console.readLine();
     }
 
-    public String enterRepetition(){
+    public String enterRepetition() {
         System.out.println(ENTER_REPETITION);
         return Console.readLine();
     }
 
-    public void printDistance(String name, int distance){
-        System.out.print(name+ " : ");
-        for(int i=0; i<distance; i++){
+    public void printDistance(String name, int distance) {
+        System.out.print(name + " : ");
+        for (int i = 0; i < distance; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public void printWinner(String name){
-        System.out.print("최종 우승자 : "+ name);
+    public void printWinner(String name) {
+        System.out.print("최종 우승자 : " + name);
     }
 
-    public void printJointWinner(String name){
-        System.out.print(", "+name);
+    public void printJointWinner(String name) {
+        System.out.print(", " + name);
     }
 }
