c8055a0f38b6d3fa95a981f7a41e1cbf32606fe1
Eunicekk <kate4234@gmail.com>
2023-10-31 23:50:57 +0900

    feat(main) : 나동차 이름, 시도할 회수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("잘못 입력하셨습니다.");
+            }
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = Integer.parseInt(Console.readLine());
+
+        if(count <= 0){
+            throw new IllegalArgumentException("잘못 입력하셨습니다.");
+        }
 
     }
-    
+
 }
