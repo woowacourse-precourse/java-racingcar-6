7f261501b7a0d93022bac87a3cc444aad54c9e60
mseo39 <a01063294905@gmail.com>
2023-11-01 04:45:21 +0900

    Feat: 회수입력 정수인지 확인하는 기능

--- src/main/java/racingcar/exception/GameException.java
+++ src/main/java/racingcar/exception/GameException.java
@@ -6,4 +6,13 @@ public class GameException {
             throw new IllegalArgumentException();
         }
     }
+
+    public int checkInput(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit((input.charAt(i)))) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return Integer.parseInt(input);
+    }
 }
