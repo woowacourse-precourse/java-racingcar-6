3275f1665f144f1530c00975411d1be7231325af
DoYoung Kim <rlaehdud159@gmail.com>
2023-10-31 13:36:28 +0900

    완료

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,2 +1,67 @@
-package racingcar.domain;public class Car {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    public static List<Car> carList = new ArrayList<>();
+    private final String name;
+    private int position = 0;
+    public static int maxPosition = 0;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void addCarList(Car car) {
+        carList.add(car);
+    }
+
+    private void move() {
+        position++;
+    }
+
+    public static void canMove(Car car) {
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        if (randomNum >= 4) {
+            car.move();
+        }
+    }
+
+    private static int getMaxPosition() {
+        for(Car car : carList) {
+            if(car.position > maxPosition) {
+                maxPosition = car.position;
+            }
+        }
+        return maxPosition;
+    }
+
+    private static List<String> winnerList() {
+        List<String> winnerList = new ArrayList<>();
+        for(Car car : carList) {
+            if(car.position == maxPosition) {
+                winnerList.add(car.getName());
+            }
+        }
+        return winnerList;
+    }
+
+    public static void getWinner() {
+        getMaxPosition();
+        winnerList();
+        OutputView.printWinner(winnerList());
+    }
 }
--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,2 +1,60 @@
-package racingcar.domain;public class RacingGame {
+package racingcar.domain;
+
+import racingcar.ValidChecker;
+import racingcar.View.OutputView;
+import racingcar.View.InputView;
+
+import java.util.List;
+
+public class RacingGame {
+
+    private final String SEPARATOR = ",";
+    private final String ESCAPE_GAME = "[ERROR] 잘못된 입력으로 프로그램을 종료합니다.";
+
+    private ValidChecker validChecker = new ValidChecker();
+    private static List<Car> carList = Car.carList;
+
+    public void start() {
+        String[] cars = askCarName();
+        int trialNum = askTrialNumber();
+        makeCarList(cars);
+        printDistance(carList, trialNum);
+        Car.getWinner();
+    }
+
+    public void printDistance(List<Car> carList, int trialNum) {
+        OutputView.printGameResult();
+        for (int i = 0; i < trialNum; i++) {
+            for (Car car : carList) {
+                Car.canMove(car);
+                OutputView.printPlayerAndPosition(car);
+            }
+            System.out.println();
+        }
+    }
+
+    private void makeCarList(String[] cars) {
+        for (String name : cars) {
+            Car car = new Car(name, 0);
+            car.addCarList(car);
+        }
+    }
+
+    private String[] askCarName() {
+        String inputCars = InputView.getCarNameList();
+        boolean result = validChecker.isValidCar(inputCars);
+        if (!result) {
+            throw new IllegalArgumentException(ESCAPE_GAME);
+        }
+        return inputCars.split(SEPARATOR);
+    }
+
+    private int askTrialNumber() {
+        String inputTrialNum = InputView.getAskNumberOfAttempts();
+        boolean result = validChecker.isValidNum(inputTrialNum);
+        if (!result) {
+            throw new IllegalArgumentException(ESCAPE_GAME);
+        }
+        return Integer.parseInt(inputTrialNum);
+    }
 }
