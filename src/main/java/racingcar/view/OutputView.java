d63e610696f4d3883aa4de6deeccfa3b060a4d86
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-31 18:53:41 +0900

    refactor(OutputView): 문자열 상수 추출

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,7 @@ import java.util.List;
 import racingcar.model.dto.CarDto;
 
 public class OutputView {
+    private static final String EXECUTION_RESULT = "실행 결과";
     private static final String ROUND_REG_EX = " : ";
     private static final String POSITION_EXPRESSION = "-";
     private static final String WINNER_EXPRESSION = "최종 우승자 : ";
@@ -13,7 +14,7 @@ public class OutputView {
     }
 
     public static void printResultMessage() {
-        System.out.println("실행 결과");
+        System.out.println(EXECUTION_RESULT);
     }
 
     public static void printRoundResult(List<CarDto> cars) {
