f98055e75f50d9c2c9c6cdd43a00a0e98dd98403
dhlrwu <yso7414@sookmyung.ac.kr>
2023-10-31 16:18:47 +0900

    feat(racingcar): 사용자로부터 자동차 등록

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        String[] array = carNames.split(",");
+        List<Car> carList = new ArrayList<>();
+        for (String name : array) {
+            carList.add(new Car(name));
+        }
     }
 }
