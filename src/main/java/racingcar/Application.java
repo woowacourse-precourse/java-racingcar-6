d3aef76085948b4d55f9ef5b082ef71a8aef00d5
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-10-31 15:54:40 +0900

    Feat(Application): 이동 시도할 횟수 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    public static int askTimes() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        int moveTime = 0;
+        try {
+            moveTime = Integer.parseInt(input);
+        }
+        catch (NumberFormatException ex) {
+            throw new IllegalArgumentException();
+        }
+        return moveTime;
+    }
+
 }
