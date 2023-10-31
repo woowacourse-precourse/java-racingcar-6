b566197779e670d1d70849a51b1e9bb317333309
bora <youonl5@naver.com>
2023-11-01 02:56:28 +0900

    Fix: GameView 클래스 inputNameOfCars 출력 함수 println으로 변경

--- src/main/java/racingcar/GameView.java
+++ src/main/java/racingcar/GameView.java
@@ -8,7 +8,7 @@ public class GameView {
     ExceptionHandler exceptionHandler = new ExceptionHandler();
 
     public String inputNameOfCars() {
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
         exceptionHandler.validateUserInput(userInput);
         return userInput;
