4a9ab33d74ae42bf7572ec60417b22618a491977
alenjb <alenjb@naver.com>
2023-11-01 03:29:07 +0900

    :sparkles:feat(racingcar): 전진 또는 멈춤을 선택하는 기능 구현
    Car.java에 전진 또는 멈춤을 선택하는 메서드 goOrStop을 구현함.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,9 +2,17 @@ package racingcar;
 
 public class Car {
     int randomNumber;
+    String[] result; // 각 회차의 결과를 저장하는 배열
+
 
     void createRandomNumber() {
         // 0~9 사이의 무작위 값을 추출
         this.randomNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
     }
+
+    void goOrStop() {
+        // 무작위 값이 4 이상이면 전진
+        if (randomNumber >= 4) go();
+    }
+
 }
