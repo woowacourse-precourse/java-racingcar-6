7167248835ba17d61c0d1577fef2dfbe77059933
alscjf1329 <alscjf1329@gmail.com>
2023-10-31 17:54:38 +0900

    test: 전진하는_조건일시_정상_작동 테스트 작성
    전진하는 조건일시 정상 작동하는지 확인하는 테스트를 작성했습니다.

---
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.dto.RacingCar;
+import racingcar.utils.RacingCarGame;
+
+public class RacingCarGameTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    private RacingCarGame racingCarGame;
+    private ByteArrayOutputStream outputStreamCaptor;
+
+    @BeforeEach
+    private void setUp() {
+        outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    private String getOutputString() {
+        return outputStreamCaptor.toString().trim();
+    }
+
+    @Test
+    void 전진하는_조건일시_정상_작동() {
+        racingCarGame = new RacingCarGame(List.of(
+                new RacingCar("yang")
+        ), 1);
+        racingCarGame.run(() -> MOVING_FORWARD);
+
+        assertThat(getOutputString()).isEqualTo("yang : -");
+    }
+}
