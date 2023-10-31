2db18e7a32d58ddf6d69fbb08786a052a40680b9
Jae-Young98 <ygy0102@gmail.com>
2023-10-31 23:30:52 +0900

    refactor(enums): 레이스에 사용되는 상수 enum으로 분리
    - 최소 시도 횟수, 무작위 숫자 범위 0, 9

--- src/main/java/racingcar/constant/NumberConstant.java
+++
@@ -1,7 +0,0 @@
-package racingcar.constant;
-
-public class NumberConstant {
-    public static final int MIN_ATTEMPT_NUMBER = 1;
-    public static final int MIN_RANDOM_NUMBER = 0;
-    public static final int MAX_RANDOM_NUMBER = 9;
-}
--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,11 +2,11 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import static racingcar.constant.NumberConstant.MAX_RANDOM_NUMBER;
-import static racingcar.constant.NumberConstant.MIN_RANDOM_NUMBER;
+import static racingcar.enums.RaceConstant.MAX_RANDOM_NUMBER;
+import static racingcar.enums.RaceConstant.MIN_RANDOM_NUMBER;
 
 public class RandomNumberGenerator {
     public static int makeRandomNumber() {
-        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER.getNumber(), MAX_RANDOM_NUMBER.getNumber());
     }
 }
