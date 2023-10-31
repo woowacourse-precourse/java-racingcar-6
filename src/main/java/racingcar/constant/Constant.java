087637b2d82516dbca86f0d99bf628ef4c3722d6
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 19:23:02 +0900

    feat : OutputView 최종 우승자를 출력하는 기능 추가

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -7,4 +7,6 @@ public class Constant {
     public static final String COLON = " : ";
     public static final String MINUS_SIGN = "-";
     public static final char ENTER = '\n';
+    public static final String WINNER_NAMES = "최종 우승자 : ";
+    public static final String COMMA_AND_BLANK = ", ";
 }
--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -1,11 +1,14 @@
 package racingcar.view.output;
 
 import static racingcar.constant.Constant.COLON;
+import static racingcar.constant.Constant.COMMA_AND_BLANK;
 import static racingcar.constant.Constant.ENTER;
 import static racingcar.constant.Constant.EXECUTION_RESULT_MENT;
 import static racingcar.constant.Constant.MINUS_SIGN;
+import static racingcar.constant.Constant.WINNER_NAMES;
 
 import java.util.List;
+import java.util.StringJoiner;
 import racingcar.dto.RacingCarDto;
 
 public class OutputView {
@@ -25,5 +28,34 @@ public class OutputView {
             renderer.append(MINUS_SIGN.repeat(Math.max(0, result.getPosition()))).append(ENTER);
         });
         return renderer.toString();
+    }
+
+    public void printWinners(List<RacingCarDto> racingCarDtos) {
+        List<RacingCarDto> winnerDtos = getWinnerDtos(racingCarDtos);
+        List<String> winnerNames = createWinnerNames(winnerDtos);
+        System.out.println(WINNER_NAMES + renderWinner(winnerNames));
+    }
+
+    private String renderWinner(List<String> winnerNames) {
+        StringJoiner renderer = new StringJoiner(COMMA_AND_BLANK);
+        winnerNames.forEach(renderer::add);
+        return renderer.toString();
+    }
+
+    private List<RacingCarDto> getWinnerDtos(List<RacingCarDto> racingCarDtos) {
+        int finishLine = findFinishLine(racingCarDtos);
+        return racingCarDtos.stream().filter(racingCarDto -> racingCarDto.getPosition() == finishLine).toList();
+    }
+
+    private List<String> createWinnerNames(List<RacingCarDto> racingCarDtos) {
+        return racingCarDtos.stream().map(RacingCarDto::getCarName).toList();
+    }
+
+    private int findFinishLine(List<RacingCarDto> racingCarDtos) {
+        int finishLine = 0;
+        for (RacingCarDto racingCarDto : racingCarDtos) {
+            finishLine = Math.max(racingCarDto.getPosition(), finishLine);
+        }
+        return finishLine;
     }
 }
