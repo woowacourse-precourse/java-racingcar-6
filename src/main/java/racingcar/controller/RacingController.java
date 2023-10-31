ec31e5c4c7cda976d83f29923f8e7939e55e3783
hong <kevin0928@naver.com>
2023-10-31 19:25:12 +0900

    refactor: RacingController 메소드 분리
    - 메소드는 한가지 일만 하며, 최소한 작게 유지

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.service.RacingService;
@@ -22,23 +23,37 @@ public class RacingController {
     }
 
     public void run() {
+        racingGameStart();
+    }
+
+    private void racingGameStart() {
+        List<String> carNames = getCarNames();
+        Cars cars = new Cars(carNames);
+
+        int tries = getTries();
+        racingGame(cars, tries);
+    }
+
+    private List<String> getCarNames() {
         ConsoleOutput.displayMessage(ConsoleOutput.CAR_NAME_MESSAGE);
         String input = inputView.getCarNames();
         List<String> carNames = racingService.stringToList(input);
         InputValidator.isLengthGreaterThanFive(carNames);
+        return carNames;
+    }
 
-        Cars cars = new Cars(carNames);
-
+    private int getTries() {
         ConsoleOutput.displayMessage(ConsoleOutput.TRY_MESSAGE);
         int tries = inputView.getTries();
         InputValidator.isGreaterThanZero(tries);
-
         ConsoleOutput.printNewLine();
+        return tries;
+    }
 
+    private void racingGame(Cars cars, int tries) {
         ConsoleOutput.displayMessage(ConsoleOutput.ROUND_RESULT_MESSAGE);
-        for (int num = 0; num < tries; num++) {
-            cars.playGame();
-        }
+        IntStream.range(0,tries)
+                .forEach(i -> cars.playGame());
         outputView.printGameResult(cars.getWinner());
     }
 }
