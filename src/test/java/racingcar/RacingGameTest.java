d21334845b985c86dfb6eb6b8aa7fe28ea95668a
ZZ3n <ndsrhkd@naver.com>
2023-10-31 14:56:54 +0900

    test(game): 참가자 명단 입력 테스트 추가
    참가자 명단 입력받기의 테스트 케이스 3개 추가
    - 중복 참가자 입력
    - 공백 참가자 입력
    - 정상 참가자 입력

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,55 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.Test;
+
+class RacingGameTest {
+
+    @Test
+    void instance_생성_테스트() {
+        //given
+        final byte[] buf = "Aa,BB,C\n".getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+
+        //when
+        RacingGame racingGame = new RacingGame();
+        Console.close();
+
+        //then
+        assertThat(racingGame).isNotNull();
+        assertThat(racingGame).isInstanceOf(RacingGame.class);
+    }
+
+    @Test
+    void 참가자_명단_중복_테스트() {
+        //given
+        final byte[] buf = "AA,BB,CC,BB\n".getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> {
+            RacingGame racingGame = new RacingGame();
+        });
+
+        Console.close();
+    }
+
+    @Test
+    void 참가자_명단_공백_테스트() {
+        //given
+        final byte[] buf = "AA,BB,,BB\n".getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> {
+            RacingGame racingGame = new RacingGame();
+        });
+
+        Console.close();
+    }
+
+}
