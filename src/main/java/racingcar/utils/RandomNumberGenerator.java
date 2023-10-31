b1f7d4330cb599306938ddd656f2126f1c599e3a
gourderased <gourderased@gmail.com>
2023-11-01 00:11:00 +0900

    "feat(racingcar): 0부터 9사리 랜덤 숫자 반환하는 'RandomNumberGenerator 클래스 추가"

---
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
