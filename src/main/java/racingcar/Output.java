a26c1d128243f19d9df3aab82795837156abd9d3
SeungGwa123 <skwan123@naver.com>
2023-10-31 16:42:49 +0900

    Feat: Add totalPrint
    자동차의 현재 거리를 출력하는 함수 작성
    전체 자동차를 출력해야하는 것이라 routePrint를 통해서 한대씩 출력하는 함수를 만들면 여기서 사용할 예정으로 주석으로 위치표시 했음

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -2,9 +2,16 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Map;
+
 public class Output {
     public int randomNumber() {
         int randNum = Randoms.pickNumberInRange(0, 9);
         return randNum;
+    }
+    public void totalPrint(Map<String,Integer> parkingLot,String[] carlist) {
+        for(int i=0;i<parkingLot.size();i++){
+            //하나씩 출력
+        }
     }
 }
