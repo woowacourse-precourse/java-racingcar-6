dbb75bb539eb8ca4e9844e9f9ee524aa3be6bcad
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-31 22:56:23 +0900

    feat: testCar클래스 Car클래스에 정의된 예외처리 메서드들이 잘 작동하는지에 대한 테스트 클래스 입니다.

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,52 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+public class CarTest {
+
+    @Test
+    void isCarNameLengthOver_자동차_이름_5글자_이상() {
+
+        String input = "aasdaa";
+
+        assertThatThrownBy(() -> new Car(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isCarNameNull_자동차_이름_null() {
+
+        String input = null;
+
+        assertThatThrownBy(() -> new Car(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isCarNameBlank_자동차_이름_빈문자열() {
+
+        String input = "";
+
+        assertThatThrownBy(() -> new Car(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isNotAlphaBetAndComma_자동차_이름_공백으로_이루어짐() {
+
+        String input = "          ";
+
+        assertThatThrownBy(() -> new Car(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isNotAlphaBetAndComma_자동차_이름_문자가_아닌_경우() {
+
+        String input = "123";
+
+        assertThatThrownBy(() -> new Car(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
