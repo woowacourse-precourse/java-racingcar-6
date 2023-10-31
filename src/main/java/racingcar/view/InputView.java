f55f2ab8cac81ee454905419b447f253882afa5a
seona-moon <fhflwhwl5@gmail.com>
2023-10-31 10:41:21 +0900

    refactor: 가독성을 위해 validatior 코드를 외부 메소드로 추출

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,19 +7,35 @@ import java.util.List;
 import racingcar.validation.Validator;
 
 public class InputView {
-  Validator validator = new Validator();
+
+  private static final String NAME_DELIMITER = ",";
+  private Validator inputValidator;
+
+  public InputView() {
+    this.inputValidator = new Validator();
+  }
+
   public List<String> getCarName() {
     String names = readLine();
-    validator.validateNullValue(names);
-    List<String> nameList = List.of(names.split(","));
-    validator.validateNameLength(nameList);
-    validator.validateDuplicatedName(nameList);
+    return validateCarName(names);
+  }
+
+  private List<String> validateCarName(String names) {
+    inputValidator.validateNullValue(names);
+    List<String> nameList = List.of(names.split(NAME_DELIMITER));
+    inputValidator.validateNameLength(nameList);
+    inputValidator.validateDuplicatedName(nameList);
     return nameList;
   }
+
   public int getCount() {
     String count = readLine();
-    validator.validateNullValue(count);
-    validator.validateNum(count);
+    validateCount(count);
     return Integer.parseInt(count);
+  }
+
+  private void validateCount(String count) {
+    inputValidator.validateNullValue(count);
+    inputValidator.validateNum(count);
   }
 }
