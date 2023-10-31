202e2818a9d287bcab8e564aaa4da2d615698186
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-10-31 13:40:25 +0900

    feat: 시도 횟수 입력 및 시행 결과 출력
    1. 메서드를 직관적으로 이해하기 위해 List<> car, List<> presentDistance 전역 변수로 변경
    
    Ref: Docs/README.md
    Resolve: README.md #3

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,37 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static List<String> cars;
+    static List<StringBuilder> presentDistance = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<String> cars;
-        List<StringBuilder> presentDistance = new ArrayList<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
         cars = List.of(input.split(","));
 
-        validateCarName(cars, presentDistance);
+        validateCarName();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int trial = Integer.parseInt(Console.readLine());
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < trial; i++) {
+            for (int j = 0; j < cars.size(); j++) {
+                pickRandomNumberAndPrintResult(j);
+            }
+            System.out.println();
+        }
     }
 
-    private static void validateCarName(List<String> cars, List<StringBuilder> presentDistance) {
+    private static void validateCarName() {
         for (int i = 0; i < cars.size(); i++) {
             cars.set(i, cars.get(i).trim());
 
@@ -27,5 +40,12 @@ public class Application {
 
             presentDistance.add(new StringBuilder());
         }
+    }
+
+    private static void pickRandomNumberAndPrintResult(int j) {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4)
+            presentDistance.get(j).append("-");
+        System.out.printf("%s : %s\n", cars.get(j), presentDistance.get(j));
     }
 }
