f7ee93304153afb15b06278a142a48eaf508fdb8
minsu20 <syt07100@gmail.com>
2023-11-01 03:05:40 +0900

    feat: main함수에서 게임을 시작하는 함수 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.service.RaceService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RaceController raceController=new RaceController();
+        raceController.startGame();
+
     }
 }
