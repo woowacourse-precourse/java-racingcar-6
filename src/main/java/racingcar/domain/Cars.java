abf7ece53c3537578d33840140e9fdbf4f613e81
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-31 17:02:23 +0900

    feat(Cars) : 각 car를 전진 및 멈추는 기능과 각 총 거리,이름을 알아내는 기능 우승자를 알아내는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,11 +1,54 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.util.InputParser;
+import racingcar.util.InputValidator;
 
 public class Cars {
     private List<Car> cars;
 
-    public Cars(List<Car> cars) {
-        this.cars = cars;
+    public Cars(String playerInput) {
+        InputValidator inputValidator = new InputValidator();
+        InputParser inputParser = new InputParser();
+        inputValidator.validateCarNames(playerInput);
+        this.cars = inputParser.parseCarName(playerInput);
+    }
+
+    public void driveAll() {
+        cars.forEach(Car::drive);
+    }
+
+    public List<String> getWinners() {
+        List<String> winners = new ArrayList<>();
+        for (Car car : this.cars) {
+            if (car.getTotalDistance() == this.getMaxDistance()) {
+                winners.add(car.toString());
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxDistance() {
+        int maxDistance = 0;
+        for (Car car : this.cars) {
+            maxDistance = Math.max(maxDistance, car.getTotalDistance());
+        }
+        return maxDistance;
+    }
+
+    public List<Integer> getTotalDistances() {
+        List<Integer> totalDistances = cars.stream()
+                .map(Car::getTotalDistance)
+                .collect(Collectors.toList());
+        return totalDistances;
+    }
+
+    public List<String> getNames() {
+        List<String> names = cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.toList());
+        return names;
     }
 }
