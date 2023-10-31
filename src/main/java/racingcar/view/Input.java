c56a82e4a18824a3a16f714e3578f5bb42015e46
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 06:11:10 +0900

    feat : 사용자로부터 값을 입력 받는 기능 구현
    - getCarNames() 메소드 구현
    - getTryNumber() 메소드 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
-public class Input {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.Validation;
+
+public final class Input {
+
+    public static String getCarNames() {
+        String carNames = Console.readLine();
+        Validation.validateIsNotNull(carNames);
+        Validation.validateSeparator(carNames);
+        return carNames;
+    }
+
+    public static String getTryNumber() {
+        String tryNumber = Console.readLine();
+        Validation.validateIsDigit(tryNumber);
+        Validation.validateTryNumber(tryNumber);
+        return tryNumber;
+    }
 }
