d6e1f3222e14b7f1cc64087517116de8aeeea250
jongmee <ra123221@gmail.com>
2023-11-01 03:41:40 +0900

    test: 경주차 이름 길이 제한의 예외 처리 테스트하기

--- src/test/java/racingcar/dto/request/CarNamesDtoTest.java
+++ src/test/java/racingcar/dto/request/CarNamesDtoTest.java
@@ -1,4 +1,21 @@
+package racingcar.dto.request;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.constants.Condition.NAME_SIZE;
+import static racingcar.constants.ErrorMessage.LENGTH_LIMIT;
+
+import org.junit.jupiter.api.Test;
+
 class CarNamesDtoTest {
-  
+    @Test
+    void 경주차이름의_길이제한을_넘길때_예외발생시키기() {
+        // given
+        String request = "jongmee,meoru,gari";
+
+        // when & then
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            new CarNamesDto(request);
+        });
+        assertEquals(LENGTH_LIMIT + NAME_SIZE, exception.getMessage());
+    }
 }
