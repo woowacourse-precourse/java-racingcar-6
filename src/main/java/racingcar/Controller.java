379139181ab410fc372b106b77fadaa31f1a8698
yang <hc1489@naver.com>
2023-10-31 18:01:59 +0900

    nameInput 기능, validation 기능 구현

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -6,15 +6,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class Controller {
     private ArrayList<Car> racingCars = new ArrayList<>();
 
-    public String[] nameInput() {
-        String[] names = Console.readLine().split(",");
-
+    private String[] validation(String[] names) {
         for (String name : names) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("Too long name");
             }
         }
-
         return names;
     }
+
+    public String[] nameInput() {
+        String[] names = Console.readLine().split(",");
+
+        return validation(names);
+    }
+
 }
