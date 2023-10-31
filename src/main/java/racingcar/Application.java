f391dc8a818534f8b7bf69723c670ec450659da1
MIN SEON IK <alstjsdlr0321@korea.ac.kr>
2023-10-31 16:27:08 +0900

    refactor : indent depth 3 이하, 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Application {
@@ -9,8 +11,8 @@ public class Application {
     //Car 클래스 정의
     public static class Car {
         public String name;
-        public int RandomNumber; // 랜덤으로 받을 정수
-        public int AdvanceCount = 0; // 전진 횟수
+        public int randomNumber; // 랜덤으로 받을 정수
+        public int advanceCount = 0; // 전진 횟수
 
         public Car(String name) {
             this.name = name;
@@ -20,82 +22,106 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // 자동차 이름 입력 받기
+        String[] carNames = inputCarNames(); // 자동차 이름 입력 받기
+        int tries = inputTries(); // 시도 횟수 입력 받기
+        Car[] cars = createCars(carNames); //자동차 객체 생성
+
+        System.out.println("실행 결과");
+        StartRacing(tries, cars); // 경주 시작
+        printWinners(findWinners(cars, carNames));
+
+
+
+    }
+
+    private static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String Input = Console.readLine();
-        String[] carNames = Input.split(","); // 쉼표로 자르기
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
 
         for (String carName : carNames) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             }
         }
+        return carNames;
+    }
 
-        // 시도 횟수 입력 받기
+    private static int inputTries() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int tries = Integer.parseInt(Console.readLine());
+        int tries = Integer.parseInt(Console.readLine());  // 시도 횟수 입력 받기
 
         if (tries < 0) {
             throw new IllegalArgumentException("시도 횟수는 음수일 수 없습니다.");
         }
 
-        //자동차 객체 생성
+        return tries;
+    }
+
+    private static Car[] createCars(String[] carNames) {
         Car[] cars = new Car[carNames.length];
         for (int i = 0; i < carNames.length; i++) {
             cars[i] = new Car(carNames[i] + " : ");
-            //System.out.println(cars[i].getName());
         }
-
-        System.out.println("실행 결과");
-        StartRacing(tries, cars);
-        FinalWinner(cars, carNames);
-
+        return cars;
     }
 
-    // 경주 시작
-    public static void StartRacing(int tries, Car[] cars) {
+    private static void StartRacing(int tries, Car[] cars) {
         for (int i = 0; i < tries; i++) { // 시도 횟수 만큼 반복
-            for (int j = 0; j < cars.length; j++) { // 차 개수 만큼 반복
-                cars[j].RandomNumber = Randoms.pickNumberInRange(0, 9); // 랜덤한 숫자 할당
-                if (cars[j].RandomNumber > 3) { // 4 이상시
-                    cars[j].AdvanceCount++; // 전진
-                    cars[j].name = cars[j].name.concat("-");
-                }
-            }
-            for (Car car : cars) {
-                System.out.println(car.name);
-            }
-            System.out.println();
+           randomAssignment(cars);
         }
     }
 
-    public static void FinalWinner(Car[] cars, String[] carNames) {
-
-        // AdvanceCount 최댓값 찾기
-        int max = cars[0].AdvanceCount; // 초기값
-
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i].AdvanceCount >= max) {
-                max = cars[i].AdvanceCount;
+    private static void randomAssignment(Car[] cars) {
+        for (Car car : cars) {
+            car.randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (car.randomNumber > 3) {
+                car.advanceCount++;
+                car.name = car.name.concat("-");
             }
         }
+        printCars(cars);
+    }
 
-        System.out.print("최종 우승자 :"); // 1등 출력
+    private static void printCars(Car[] cars) {
+        for (Car car : cars) {
+            System.out.println(car.name);
+        }
+        System.out.println();
+    }
 
-        boolean isFirstWinner = true; // 첫 번쨰 우승자인지 확인하기 위한 변수
 
-        // max와 값이 같은 자동차를 공동 우승자로 포함시키기
+    private static List<String> findWinners(Car[] cars, String[] carNames) {
+        int maxAdvanceCount = findMaxAdvanceCount(cars);
+        List<String> winners = new ArrayList<>();
+
         for (int i = 0; i < cars.length; i++) {
-            if (cars[i].AdvanceCount == max) {
-                if (!isFirstWinner) {
-                    System.out.print(", "); // 첫 번쨰 우승자가 아닐 경우 콤마 출력
-                }
-                System.out.print(" " + carNames[i]);
-                isFirstWinner = false; // 첫 번쨰 우승자가 아니라고 표ㅕ시
-
+            if (cars[i].advanceCount == maxAdvanceCount) {
+                winners.add(carNames[i]);
             }
         }
+        return winners;
+    }
 
+    private static int findMaxAdvanceCount(Car[] cars) {
+        int max = cars[0].advanceCount;
+
+        for (Car car : cars) {
+            max = Math.max(max, car.advanceCount);
+        }
+        return max;
+    }
+
+    private static void printWinners(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+
+        for (int i = 0; i < winners.size(); i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(winners.get(i));
+        }
+        System.out.println();
     }
 }
 
