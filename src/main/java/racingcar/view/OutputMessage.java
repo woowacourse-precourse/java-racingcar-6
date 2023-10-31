cdf667a8f58aa3ef71b5b7d6fdea78a52b7b5684
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 06:41:21 +0900

    feat : 최종 우승자 출력 메세지 추가
    - outputFinalWinner() 메소드 구현

--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -2,7 +2,11 @@ package racingcar.view;
 
 public final class OutputMessage {
     private static final String OUTPUT_RESULT_TEXT_MESSAGE = "실행 결과";
+    private static final String OUTPUT_FINAL_WINNER_MESSAGE = "최종 우승자 : ";
     public static void outputResultText() {
         System.out.println(OUTPUT_RESULT_TEXT_MESSAGE);
+    }
+    public static void outputFinalWinner() {
+        System.out.print(OUTPUT_FINAL_WINNER_MESSAGE);
     }
 }
