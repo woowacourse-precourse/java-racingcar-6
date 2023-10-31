7d2dfe8dd6cf52a96abd5aa20e6d7b8042898be8
sotogito <108535526+sotogito@users.noreply.github.com>
2023-10-31 19:50:34 +0900

    refactor : String -> List<String 변환 코드 MainCarGame클래스에서 분리 DataConverter 클래스 생성

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
-import java.util.ArrayList;
 
 public class InputManager {
     static final String MSG_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -20,18 +19,7 @@ public class InputManager {
         System.out.println(MSG_INPUT_CAR_NAME);
         String answer = Console.readLine();
 
-        return toListString(answer);
-    }
-
-    public List<String> toListString(String str) {
-        String[] arry = str.split(",");
-
-        List<String> list = new ArrayList<>();
-        for (String sample : arry) {
-            list.add(sample);
-        }
-
-        return list;
+        return DataConverter.toListString(answer);
     }
 
 }
