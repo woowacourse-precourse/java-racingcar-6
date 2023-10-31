e0577870f6433774519ecf24fc347fc9ade90f31
조은별 <joeunbyeol98@gmail.com>
2023-10-31 16:00:03 +0900

    feat(InputValidator): 자동차 이름 길이 조건 기능 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,14 +1,16 @@
 package racingcar.validator;
 
 import static racingcar.messages.ErrorMessage.CAR_NAMES_NOT_UNIQUE;
+import static racingcar.messages.ErrorMessage.CAR_NAME_LENGTH_OUT_OF_RANGE;
 import static racingcar.messages.ViewMessage.INPUT_SEPARATOR;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class InputValidator {
     private static InputValidator instance;
+    private static int NAME_MIN_LENGTH = 1;
+    private static int NAME_MAX_LENGTH = 5;
 
     private InputValidator() {}
 
@@ -29,6 +31,12 @@ public class InputValidator {
 
         if (nameArray.length != uniqueNameSet.size()) {
             throw new IllegalArgumentException(CAR_NAMES_NOT_UNIQUE);
+        }
+    }
+
+    public void validateNameLengthRange(String name) {
+        if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_OUT_OF_RANGE);
         }
     }
 }
