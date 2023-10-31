0f35f3461045ac3a812a5bd9196f8feeaa43485a
haruday97 <starharu97@naver.com>
2023-11-01 06:17:12 +0900

    test: CarPositionTest 오타 수정
    Next함수명을 수정했었는데 테스트클래스에선 반영이 안됐었음

--- src/test/java/racingcar/domain/CarPositionTest.java
+++ src/test/java/racingcar/domain/CarPositionTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 
 public class CarPositionTest {
     @Test
-    @DisplayName("next함수를 실행시 CarPosition을 1 증가시킨다.")
-    void should_IncreaseByOne_When_ExecutedNext() {
+    @DisplayName("goForward 함수를 실행시 CarPosition을 1 증가시킨다.")
+    void should_IncreaseByOne_When_ExecutedGoForward() {
         CarPosition carPosition = new CarPosition(4);
         assertThat(carPosition.goForward()).isEqualTo(new CarPosition(5));
     }
