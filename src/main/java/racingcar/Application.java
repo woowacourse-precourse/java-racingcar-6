75ec17f02efa93ea4d33819016dfdb882afd8a6b
cyberjam <jamsince1994@naver.com>
2023-11-01 08:42:47 +0900

    refactor: remove unused import statement

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,5 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Console.*;
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
