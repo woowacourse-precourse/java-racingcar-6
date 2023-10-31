4d28758b4d2f5daa5ed1c75c947187ca06b6b008
Son JunHyung <kingjh1125@gmail.com>
2023-10-31 13:08:50 +0900

    refactor (Validation): 매직넘버 상수 변경
    Validation 클래스의 매직 넘버를 상수로 변환하였습니다:
    - MAX_NAME_SIZE = 5

--- src/main/java/service/Validation.java
+++ src/main/java/service/Validation.java
@@ -3,9 +3,10 @@ package service;
 import java.util.regex.Pattern;
 
 public class Validation {
-    public static final Pattern timesPattern = Pattern.compile("^[1-9][0-9]*$");
+    private static final int MAX_NAME_SIZE = 5;
+    private static final Pattern timesPattern = Pattern.compile("^[1-9][0-9]*$");
     public static void CarNames(String carName){
-        if(carName.length()>5){
+        if(carName.length()>MAX_NAME_SIZE){
             throw new IllegalArgumentException();
         }
     }
