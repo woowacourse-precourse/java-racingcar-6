d82dd92b7b279e1047aea224e90b3efbdca03981
HiiWee <wpdlzhf159@gmail.com>
2023-10-31 21:23:52 +0900

    feat: 자동차의 이름이 공백일 경우의 검증 추가

--- src/main/java/racingcar/validator/RaceRuleValidator.java
+++ src/main/java/racingcar/validator/RaceRuleValidator.java
@@ -10,8 +10,15 @@ public class RaceRuleValidator {
     }
 
     public static void validate(final List<String> carNames) {
+        validateEmptyName(carNames);
         validateDuplicate(carNames);
         validateCarCount(carNames);
+    }
+
+    private static void validateEmptyName(final List<String> carNames) {
+        if (isBlankName(carNames)) {
+            throw new IllegalArgumentException("자동차의 이름은 반드시 존재해야 합니다.");
+        }
     }
 
     private static void validateDuplicate(final List<String> carNames) {
@@ -24,6 +31,11 @@ public class RaceRuleValidator {
         if (carNames.size() < MIN_ADDITION_NUMBER) {
             throw new IllegalArgumentException("자동차는 2대 이상 등록해야 합니다");
         }
+    }
+
+    private static boolean isBlankName(final List<String> carNames) {
+        return carNames.stream()
+                .anyMatch(String::isBlank);
     }
 
     private static boolean isDuplicates(final List<String> carNames) {
