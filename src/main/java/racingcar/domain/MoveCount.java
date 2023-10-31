26a676d3aa1416da9a82ed0b8fb3987f65baeaae
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 15:33:49 +0900

    refactor(domain): 입력값을 받는 역할의 객체 생성

---
+++ src/main/java/racingcar/domain/MoveCount.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class MoveCount {
+    private final int count;
+
+    public MoveCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
