025a5af9a7e864733b8ccd32928a0e6275a4792d
qocksdlf123 <qocksdlf123@naver.com>
2023-11-01 00:44:37 +0900

    fix(GameData): GameData 현재 라운트 변수 수정

--- src/main/java/racingcar/GameData.java
+++ src/main/java/racingcar/GameData.java
@@ -3,6 +3,7 @@ package racingcar;
 public class GameData {
     private final String[] carNames;
     private final int tryRoundCount;
+    private int currentRound;
 
     public GameData(String[] carNames, int tryCount) {
         this.carNames = carNames;
@@ -17,5 +18,20 @@ public class GameData {
         return tryRoundCount;
     }
 
+    // 현재 라운드 수를 반환
+    public int getCurrentRound() {
+        return currentRound;
+    }
 
+    // 다음 라운드로 진행.
+    public void nextRound() {
+        if (currentRound < tryRoundCount) {
+            currentRound++;
+        }
+    }
+
+    // 모든 라운드가 완료되었는지 확인
+    public boolean isRacesFinished() {
+        return currentRound >= tryRoundCount;
+    }
 }
