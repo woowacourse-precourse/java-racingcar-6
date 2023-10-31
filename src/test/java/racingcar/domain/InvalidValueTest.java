245d3acece96706e58e99b83006f7f2c005cded2
JeonJW24 <tyui153@naver.com>
2023-11-01 04:58:07 +0900

    test(racingcar): 입력값 5자 초과 테스트

---
+++ src/test/java/racingcar/domain/InvalidValueTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InvalidValueTest {
+    private InvalidValue invalidValue;
+
+    @BeforeEach
+    void setUp() {
+        invalidValue = new InvalidValue();
+    }
+
+    @Test
+    void 입력값_5자_초과() {
+        boolean result = invalidValue.isExceedFiveLetters(Arrays.asList("123456"));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 입력값_5자_이하() {
+        boolean result = invalidValue.isExceedFiveLetters(Arrays.asList("12345"));
+        assertThat(result).isFalse();
+    }
+}
