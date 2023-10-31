0ddb3aeb37514a8c49de01bfec3ff19fd9fc8e84
Junobee25 <wnsdhqo@naver.com>
2023-11-01 01:27:33 +0900

    refactor(service): entry 객체 삽입 시 매개변수 수정

--- src/main/java/racingcar/service/Move.java
+++ src/main/java/racingcar/service/Move.java
@@ -1,21 +1,16 @@
 package racingcar.service;
 
-import racingcar.domain.Car;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Move {
-    Car car = new Car();
 
     public void moveTheCar(LinkedHashMap<String, String> carList, List<Integer> randomNumberList) {
         int randomNumberListIndex = 0;
         for (Map.Entry<String, String> entry : carList.entrySet()) {
-            String key = entry.getKey();
             if (randomNumberList.get(randomNumberListIndex) >= 4) {
-                String moveValue = carList.get(key);
-                carList.put(key, moveValue + "-");
+                carList.put(entry.getKey(), entry.getValue() + "-");
             }
             randomNumberListIndex++;
         }
@@ -24,10 +19,8 @@ public class Move {
     public void stopTheCar(LinkedHashMap<String, String> carList, List<Integer> randomNumberList) {
         int randomNumberListIndex = 0;
         for (Map.Entry<String, String> entry : carList.entrySet()) {
-            String key = entry.getKey();
             if (randomNumberList.get(randomNumberListIndex) < 4) {
-                String moveValue = carList.get(key);
-                carList.put(key, moveValue + "");
+                carList.put(entry.getKey(), entry.getValue() + "-");
             }
             randomNumberListIndex++;
         }
