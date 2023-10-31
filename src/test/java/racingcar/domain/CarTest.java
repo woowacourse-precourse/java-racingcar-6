42940cfe39c4f6eae69d5e64b56a3ae5eb6e8640
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 04:07:01 +0900

    test(domain.Car) : 기능3. 랜덤값에 따라 특정 자동차를 한 칸 전진
    move 메서드의 동작확인을 위해서 random값이 4일 때 이동하는지와 random값이 3일 때 이동하지 않는지 확인하는 테스트 코드를 작성하였습니다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,12 +2,30 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
     @Test
-    void move() {
+    void move_랜덤숫자가_4_이상_나왔을_때_차량_이동() {
+        // given
+        Car car = new Car("A", 0);
+        // when
+        car.move(4);
+        int movingDistance = car.getMovingDistance();
+        // then
+        assertThat(movingDistance).isEqualTo(1);
+    }
+
+    @Test
+    void move_랜덤숫자가_4_미만_나왔을_때_차량_이동_없음() {
+        // given
+        Car car = new Car("A", 0);
+        // when
+        car.move(3);
+        int movingDistance = car.getMovingDistance();
+        // then
+        assertThat(movingDistance).isEqualTo(0);
     }
 
     @Test
