9a0e9c338c380c6de84ebfc9323ed1ebbbfe20e9
권장순 <jsoonworld@gmail.com>
2023-11-01 01:54:26 +0900

    feat(racingcar) : 시도할 회수를 입력 받는 RacingGame#inputTryCount() 구현함.

---
+++ src/main/java/racingcar/v4/RacingGame.java
@@ -0,0 +1,12 @@
+package racingcar.v4;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingGame {
+    public String inputTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputTryCount = Console.readLine();
+        return inputTryCount;
+    }
+
+}
