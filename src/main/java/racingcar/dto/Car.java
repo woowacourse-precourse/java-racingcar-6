6cf0e0e163b37e0c63a3988ce78e5a9072759a62
조은별 <joeunbyeol98@gmail.com>
2023-10-31 19:51:05 +0900

    feat(Car): validate 분리, dto로 패키지 변경

--- src/main/java/racingcar/dto/Car.java
+++ src/main/java/racingcar/dto/Car.java
@@ -5,10 +5,8 @@ import racingcar.validator.InputValidator;
 public class Car {
     private String name;
     private int moveCount;
-    private InputValidator validator = InputValidator.getInstance();
 
     public Car(String name) {
-        validator.validateNameLengthRange(name);
         this.name = name;
         this.moveCount = 0;
     }
@@ -21,7 +19,7 @@ public class Car {
         return moveCount;
     }
 
-    public void increaseMoveCount(int moveCount) {
-        this.moveCount += moveCount;
+    public void setMoveCount(int moveCount) {
+        this.moveCount = moveCount;
     }
 }
