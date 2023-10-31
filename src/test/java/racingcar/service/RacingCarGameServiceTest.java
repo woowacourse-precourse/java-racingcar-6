93e89dafb3b71d6b3dd9491a9834df09d682ada7
YongHyeon <pyh4616@naver.com>
2023-10-31 21:21:27 +0900

    Test: (RacingCarGameServiceTest 클래스), 한 회차의 게임을 진행하는 기능을 테스트

--- src/test/java/racingcar/service/RacingCarGameServiceTest.java
+++ src/test/java/racingcar/service/RacingCarGameServiceTest.java
@@ -3,13 +3,15 @@ package racingcar.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.util.RandomNumberGenerator;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RacingCarGameServiceTest {
 
@@ -20,13 +22,19 @@ public class RacingCarGameServiceTest {
         racingCarGameService = new RacingCarGameService();
     }
 
-    @ParameterizedTest(name = "[테스트 케이스 {index}] 랜덤 숫자 = {0}, 전진 결과 = {1}")
-    @CsvSource({"3,false", "4,true", "5,true"})
-    @DisplayName("4이상은 전진하고, 3이하는 정지상태를 유지한다.")
-    public void should_MoveForward_When_FourOrMore(int randomNumber, boolean expected) {
-        boolean actual = racingCarGameService.isMoveForward(randomNumber);
+    @Test
+    @DisplayName("한 회차의 게임 진행에서 랜덤 숫자가 4 이상이면 전진하고, 3 이하이면 정지 상태를 유지한다. ")
+    void should_MoveForward_When_FourOrMore() {
+        RandomNumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
+        when(randomNumberGenerator.getRandomNumber()).thenReturn(3, 4, 5);
+        List<String> carNames = List.of("carName1", "carName2", "carName3");
+        LinkedHashMap<String, Integer> racingProgressStatus = new LinkedHashMap<>(Map.of("carName1", 0, "carName2", 1, "carName3", 2));
 
-        assertThat(actual).isEqualTo(expected);
+        racingCarGameService.playSingleGame(randomNumberGenerator, carNames, racingProgressStatus);
+
+        assertThat(racingProgressStatus.get("carName1")).isEqualTo(0);
+        assertThat(racingProgressStatus.get("carName2")).isEqualTo(2);
+        assertThat(racingProgressStatus.get("carName3")).isEqualTo(3);
     }
 
     @Test
