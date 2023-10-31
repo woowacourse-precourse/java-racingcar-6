5916520c42a9f8a72b9922bc3f793e8f1133c818
whereami2048 <whereami2048@gmail.com>
2023-10-31 15:21:32 +0900

    feat: model toString 함수 오버라이딩

--- src/main/java/racingcar/model/CarModel.java
+++ src/main/java/racingcar/model/CarModel.java
@@ -9,16 +9,26 @@ public class CarModel {
         this.forward = 0;
     }
 
+    public CarModel(String name, int forward) {
+        this.name = name;
+        this.forward = forward;
+    }
+
+    public int increaseForward() {
+        this.forward += 1;
+        return this.forward;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : " + "-".repeat(this.forward);
+    }
+
     public String getName() {
         return name;
     }
 
     public int getForward() {
         return forward;
-    }
-
-    public int increaseForward() {
-        this.forward += 1;
-        return this.forward;
     }
 }
