789309c5648b866d60678232d41e996f684e0914
whereami0404 <whereami2048@gmail.com>
2023-10-31 20:24:19 +0900

    test: 난수 생성 기능, 4 이상 숫자 판단 기능 테스트 코드 추가

---
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import racingcar.model.CarModel;
+
+class RacingGameControllerTest {
+    RacingGameController controller;
+
+    @BeforeAll
+    void beforeAll() {
+        this.controller = new RacingGameController(new ArrayList<CarModel>());
+    }
+    @Test
+    void 랜덤_숫자_생성기능_테스트() {
+        //given
+        //when
+        int randomNumber = controller.generateRandomNumber();
+        //then
+        assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @Test
+    void 랜덤_숫자가_4_이상인지_판단기능_테스트() {
+        //given
+        int canForwardNumber = 5;
+        //when
+        boolean result = controller.isUpperThanCanForwardNumber();
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 랜덤_숫자가_4_미만인지_판단기능_테스트() {
+        //given
+        int canForwardNumber = 2;
+        //when
+        boolean result = controller.isUpperThanCanForwardNumber();
+        //then
+        assertThat(result).isFalse();
+    }
+}
