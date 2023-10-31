335da9b02c4f31ada8c246f01820209b902bf7c4
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-31 16:16:20 +0900

    [feat] : add is input null method

--- src/main/java/racingcar/validate/TryCountValidate.java
+++ src/main/java/racingcar/validate/TryCountValidate.java
@@ -2,12 +2,18 @@ package racingcar.validate;
 
 public class TryCountValidate {
     public static void validateTryCount(String tryCount) {
+        isInputNull(tryCount);
         isInputNumber(tryCount);
         isInputNegative(tryCount);
     }
 
+    private static void isInputNull(String tryCount) {
+        if (tryCount.length() == 0) {
+            throw new IllegalArgumentException("시도 횟수를 입력해주세요.");
+        }
+    }
     private static void isInputNumber(String tryCount) {
-        if (tryCount.matches("[^0-9]")) {
+        if (tryCount.matches(".*[^0-9].*")) {
             throw new IllegalArgumentException("숫자만 입력해주세요.");
         }
     }
