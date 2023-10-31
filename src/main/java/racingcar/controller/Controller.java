83214debff487171797e7e43d845d6883395a791
Nbowow <nbo5548@naver.com>
2023-10-31 19:29:56 +0900

    Refactor: Controller 2023.10.31 Application에 있는 메소드->  Controller클래스로 리팩토링

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -2,40 +2,45 @@ package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Car;
+import racingcar.domain.Race;
 import racingcar.repository.CarRepository;
+import racingcar.repository.RaceRepository;
+import racingcar.service.RaceService;
 import racingcar.util.Parsing;
 import racingcar.util.Validation;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-import java.util.List;
-
 public class Controller {
-
-    private final Parsing parsing = new Parsing();
     private final Validation validation = new Validation();
-    private final CarRepository carRepository = new CarRepository();
+    private final RaceService raceService = new RaceService();
+    private final OutputView outputView = new OutputView();
 
     public void run() {
-        saveCars(parsing.parseCarNames(getCarNamesByInput()));
-    }
-
-    private void saveCars(List<String> carNames) {
-        for (String carName : carNames) {
-            carRepository.save(new Car(carName));
-        }
-    }
-
-    private String getCarNamesByInput() {
+        //자동차 이름
         InputView.requestInputMessage();
-        String input = Console.readLine();
 
-        try {
-            validation.checkCarNamesInput(input);
-            return input;
-        } catch (IllegalArgumentException e) {
-            OutputView.printException(e.getMessage());
-            return getCarNamesByInput();
-        }
+        String carNameInput = Console.readLine();
+        
+        validation.checkCarNamesInput(carNameInput);
+
+        String[] carName = carNameInput.split(",");
+        int[] carPresentPoint = new int[carName.length];
+
+        //시도 횟수
+        InputView.requestTrialMessage();
+
+        String trialInput = Console.readLine();
+
+        validation.checkTrialInput(trialInput);
+
+        int count = Integer.parseInt(trialInput);
+
+        OutputView.responseExecution();
+        raceService.resultOfExecution(count, carPresentPoint, carName);
+
+        outputView.responseWinner(raceService.getWinner(carName, carPresentPoint));
+
     }
+
 }
