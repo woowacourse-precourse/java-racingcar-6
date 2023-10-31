0dc9523dfff147cba1b2ce76790da560cf871269
kimwanyoung <dhks2869@gmail.com>
2023-10-31 13:49:50 +0900

    refactor : 요구사항에따라 이름 제한 5자 이하로 수정

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -3,7 +3,7 @@ package model;
 public class Name {
 
     private final String name;
-    private static final int MAX_LENGTH = 4;
+    private static final int MAX_LENGTH = 5;
     private static final String OVER_LENGTH_MESSAGE = "5글자 미만 이름만 입력 가능합니다.";
     private static final String BELOW_LENGTH_MESSAGE = "공백 혹은 빈 문자열은 입력 할 수 없습니다.";
 
