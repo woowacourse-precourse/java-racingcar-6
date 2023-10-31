b9435a801fd1965287fe327627e3f1c07454f886
= <=>
2023-10-31 23:19:16 +0900

    feat(Input): Input 클래스 구현
    - 레이싱 게임 시작 시 사용자에게 입력을 받아야 하는 차량의 이름과 시행 횟수 처리를 위한 Input 클래스 정의.
    - 사용자의 입력은 프리코스에서 정한 camp.nextstep.edu.missionutils.Console 라이브러리 사용

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String inputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return Console.readLine();
+    }
+    public String inputTryCount(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return Console.readLine();
+    }
+
+}
