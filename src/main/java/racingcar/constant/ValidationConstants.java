3308a4e0fc7c455bb6193b524f2a3375d745e627
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 00:27:35 +0900

    feat: 이름 5자 이하 검증 기능 구현

---
+++ src/main/java/racingcar/constant/ValidationConstants.java
@@ -0,0 +1,5 @@
+package racingcar.constant;
+
+public class ValidationConstants {
+    public static final int MAX_LENGTH_OF_NAME = 5;
+}
--- src/main/java/racingcar/validation/NameInputValidation.java
+++ src/main/java/racingcar/validation/NameInputValidation.java
@@ -1,8 +1,12 @@
 package racingcar.validation;
 
+import static racingcar.constant.ValidationConstants.MAX_LENGTH_OF_NAME;
+
 public class NameInputValidation {
 
     public void isUnderFiveLetters(String input) {
-
+        if (input.length() > MAX_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
