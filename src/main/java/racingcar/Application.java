3218a05d22969b4a9c70d3b50202dc1d0a510f1e
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-01 03:22:14 +0900

    feat: 차 이름 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserEnterCarName();
+    }
+
+    private static void UserEnterCarName() {
+        String carName = readLine();
     }
 }
+
+
+
+
+//    pickNumberInRange(1,9);
+//    readLine();
