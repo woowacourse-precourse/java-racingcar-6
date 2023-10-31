4eb06a1098a7dd103e50fcf711606829af1138f4
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-01 01:03:37 +0900

    refactor: Car클래스를 domain패키지로 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,20 +5,5 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        String input = "조나단,김치,라면";
-        boolean isValidInput = isValidInputString(input);
-
-        if (isValidInput) {
-            System.out.println("입력이 유효합니다.");
-        } else {
-            System.out.println("입력이 유효하지 않습니다.");
-        }
-    }
-
-    public static boolean isValidInputString(String input) {
-        String regex = "^[\\w]+[\\w,]*[\\w]$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher.matches();
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
