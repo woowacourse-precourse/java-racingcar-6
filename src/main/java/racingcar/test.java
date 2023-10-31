573cd4f4fda76b1b7c2ea036c48aa594bd07334c
Lee Hyunju <guswnee00@gmail.com>
2023-10-31 20:49:08 +0900

    AssertJ 테스트  코드 작성(2)

--- src/main/java/racingcar/test.java
+++ src/main/java/racingcar/test.java
@@ -9,11 +9,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class test {
     @Test
     void test_input_car_name() {
-        String input = "kiki, min, honey";
+        String input = "kiki, min, hone";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         String[] car_name_list = Application.input_car_name();
         System.setIn(System.in);
-        String[] result = {"kiki", "min", "honey"};
+        String[] result = {"kiki", "min", "hone"};
 
         assertThat(result).isEqualTo(car_name_list);
     }
@@ -21,28 +21,36 @@ public class test {
     @Test
     void test_check_name_validation_empty() {
         String input = "";
+
         assertThatThrownBy(() -> Application.check_name_validation(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void test_check_name_validation_longname() {
         String input = "kihyun";
-        assertThatThrownBy(() -> Application.check_name_validation(input)).isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @Test
-    void test_input_repeat_count() {
-        String input = "count";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        System.setIn(System.in);
-        assertThatThrownBy(() -> Application.input_repeat_count()).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Application.check_name_validation(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void test_check_count(){
         int input = 0;
+
         assertThatThrownBy(() -> Application.check_count(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void test_racing_winner() {
+        String[] car_name_list = {"kiki", "min", "hone"};
+
+        String[] result1 = {"--", "----", "---"};
+        assertThat(Application.racing_winner(car_name_list, result1)).contains("min");
+
+        String[] result2 = {"----", "--", "---"};
+        assertThat(Application.racing_winner(car_name_list, result2)).contains("kiki");
+
+        String[] result3 = {"--", "---", "---"};
+        assertThat(Application.racing_winner(car_name_list, result3)).contains("min", "hone");
+    }
 
 }
