55d74d7f13e3fc16b95002b70527ffc99918875b
jongheonleee <qwefghnm1212@gmail.com>
2023-10-31 09:25:58 +0900

    [refactor] GameController 클래스의 필드 재정의

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -17,71 +17,28 @@ public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
     private final MovingStrategy movingStrategy;
+    private final Cars cars;
+    private RaceRound raceRound;
 
 
-    public GameController(final InputView inputView , final OutputView outputView, final MovingStrategy movingStrategy) {
+    public GameController(
+            final InputView inputView,
+            final OutputView outputView,
+            final MovingStrategy movingStrategy,
+            final Cars cars) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.movingStrategy = movingStrategy;
+        this.cars = cars;
     }
 
-
+    // 프로그램 전체 흐름을 파악할 수 있는 형식으로 작성하기
     public void play() {
-        MovingStrategy movingStrategy = createMovingStrategy(new RandomNumberUtilImp());
-        Cars cars = createCars(movingStrategy);
-        RaceRound raceRound = createGameRound();
-        playAllGameRound(raceRound, cars);
-        List<Car> winner = findWinner(cars);
-        showWinner(winner);
+        // Cars 생성
+        // 게임 라운드 받기
+        // 게임 라운드 만큼 실행
+        // 결과 찾기 및 출력
     }
 
-    private MovingStrategy createMovingStrategy(final RandomNumberUtil randomNumberUtil) {
-        return new MovingStrategy(randomNumberUtil);
-    }
-
-    private Cars createCars(final MovingStrategy movingStrategy) {
-        List<Car> carList = convertNamesCarList(movingStrategy);
-        return new Cars(carList);
-    }
-
-    private List<Car> convertNamesCarList(final MovingStrategy movingStrategy) {
-        List<String> names = inputView.inputNames();
-        List<Car> carList = new ArrayList<>();
-
-        for (String name : names) {
-            carList.add(new Car(movingStrategy, name));
-        }
-
-        return carList;
-    }
-
-    private RaceRound createGameRound() {
-        // 게임 라운드 회수 입력 받기
-        Integer numberOfGameRound = inputView.inputCountOfGameRound();
-        RaceRound raceRound = new RaceRound(numberOfGameRound, 0);
-
-        return raceRound;
-    }
-
-    private void playAllGameRound(final RaceRound raceRound, final Cars cars) {
-        // 각 라운드 별 게임 진행 및 결과 출력
-        outputView.outputGameResultMessage();
-        while (raceRound.isLeftRound()) {
-            raceRound.increaseCurrentRound();
-            cars.raceAllCars();
-            outputView.outputCarsPosition(cars.getCars());
-        }
-    }
-
-    private List<Car> findWinner(final Cars cars) {
-        Integer winnerPosition = cars.findWinnerPosition();
-        List<Car> winner = cars.findWinner(winnerPosition);
-
-        return winner;
-    }
-
-    private void showWinner(final List<Car> winner) {
-        outputView.outputWinner(winner);
-    }
 
 }
