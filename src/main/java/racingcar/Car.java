06d3a827678d970da3a617707063ba88d59d8286
Chan Mi <rkdcksal02@gmail.com>
2023-10-31 20:35:04 +0900

    Car 클래스 - 자동차의 이름, 위치, 이동 등 자동차의 핵심 로직을 구현.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        // 1. 자동차 이름이 5자 이하인지 확인합니다.
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        // 2. 랜덤 값에 따라 자동차의 위치를 변경합니다.
+        int randomNumber = calculateRandomNumber();
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    private int calculateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
