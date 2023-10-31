6ca25231257c7587d57b6f35127769a758a9e680
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-31 11:05:03 +0900

    refactor : 자동차 요구사항 2번 테스트 코드 리팩토링
    기존 : Car Class 를 Mocking 하여 함수 값 조작 ->
    Randoms.pickNumberInRange 함수를 Mocking 하여 통과하게 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,40 +1,55 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-import org.mockito.Mockito;
+import org.mockito.MockedStatic;
+import org.mockito.MockedStatic.Verification;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+
 
 
 public class CarTest {
     private Car car;
-
+    private Verification verificationPickNumberInRange =
+            () -> Randoms.pickNumberInRange(anyInt(),anyInt());
+    final MockedStatic<Randoms> mockingRandomClass = mockStatic(Randoms.class);
 
     @BeforeEach
     void setUp(){
-        car = Mockito.mock(Car.class);
+        car = new Car();
     }
+    @AfterEach
+    void setDown(){mockingRandomClass.close();}
+
     @Test
     void 최소값과_최대값사이_랜덤한_숫자를_생성한다(){
-        Mockito.when(car.generateRandomSpeed()).thenCallRealMethod();
+
         int speed = car.generateRandomSpeed();
+
         Assertions.assertTrue(speed>= Config.MINIMUM_VALUE && speed<=Config.MAXIMUM_VALUE);
     }
 
     @Test
     void 숫자가_일정값을_넘으면_전진할수있다(){
-        Mockito.when(car.generateRandomSpeed()).thenReturn(Config.FORWARD_THRESHOLD+1);
-        Mockito.when(car.checkCanMoveForwardIfSpeedIsExceedThreshold()).thenCallRealMethod();
+
+        mockingRandomClass.when(verificationPickNumberInRange)
+                .thenReturn(Config.FORWARD_THRESHOLD+1);
+
         Assertions.assertEquals(car.checkCanMoveForwardIfSpeedIsExceedThreshold(),true);
     }
     @Test
     void 숫자가_일정값을_넘지못하면_전진할수없다(){
-        Mockito.when(car.generateRandomSpeed()).thenReturn(Config.FORWARD_THRESHOLD-1);
-        Mockito.when(car.checkCanMoveForwardIfSpeedIsExceedThreshold()).thenCallRealMethod();
+
+        mockingRandomClass.when(verificationPickNumberInRange)
+                .thenReturn(Config.FORWARD_THRESHOLD-1);
+
         Assertions.assertEquals(car.checkCanMoveForwardIfSpeedIsExceedThreshold(),false);
     }
-
 }
