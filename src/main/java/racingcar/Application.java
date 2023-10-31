0838379380b5f868651295136991653e55f88728
Jihyeon Park <mmmszip@gmail.com>
2023-11-01 05:09:16 +0900

    refactor: 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,12 +6,9 @@ import racingcar.view.ConsoleView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         ConsoleView view = new ConsoleView();
         GameModel model = null;
         GameController gameController = new GameController(view, model);
-
         gameController.start();
     }
 }
-
