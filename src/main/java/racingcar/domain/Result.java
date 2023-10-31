ddb1e0d1dabef842631441d9380d61f7959615af
dj <fjzl12@naver.com>
2023-10-31 22:58:28 +0900

    feat(Result): 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/domain/Result.java
+++ src/main/java/racingcar/domain/Result.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
-    private List<Car> winnerList;
-    public static void printRaceResult(List<Car> carList) {
+    public Result() {
+    }
+
+    private List<String> winnerList;
+    private String winner;
+
+    public void printRaceResult(List<Car> carList) {
         for (Car car : carList) {
             System.out.printf("%s : %s\n", car.getName(), car.getState());
         }
@@ -13,6 +18,8 @@ public class Result {
     }
 
     public void printWinner() {
+        System.out.println("최종 우승자 : " + winner);
+    }
 
     public int getLeaderDistance(List<Car> carList) {
         int leaderDistance = carList.stream().map(Car::getState).mapToInt(String::length).max().getAsInt();
