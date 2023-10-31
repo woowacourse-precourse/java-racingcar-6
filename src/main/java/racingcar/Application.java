a341d08c4e5d708cc94a57fd30d5077266ac248f
2uwls <2uwls1126@gmail.com>
2023-10-31 14:28:07 +0900

    feat: create drive number

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,33 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         createCars(Console.readLine().trim());
         System.out.println("시도할 회수는 몇회인가요?");
         saveTrialNumber(Console.readLine().trim());
-
-
-
-
     }
+
+    private static void driveCar () {
+        Integer driveNumber = isMoreThanFour(createRandomNum());
+    }
+
+    private static Integer createRandomNum() {
+        Integer randomNum = Randoms.pickNumberInRange(0,9);;
+        return randomNum;
+    }
+    private static Integer isMoreThanFour (Integer randomNum) {
+        if (randomNum >= 4) {
+            return randomNum;
+        } else {
+            return 0;
+        }
+    }
+
+
     private static void createCars(String carNames) {
         String[] carList = carNames.split(",");
         Integer carListLength = carList.length;
@@ -32,7 +48,6 @@ public class Application {
                 throw new IllegalArgumentException("이름의 길이가 5자가 넘어갑니다. ");
             }
         }
-        //이름이 문자열이 아닐 경우?<- 조건에 없는 듯..
         //구분문자가 쉼표가 아닌 경우 한 명만 들어오면 쉼표는 없는데..
 
     }
