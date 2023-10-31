b5476ca519e3c8d08fc4c13952e9a421dbe8178e
Ozwong <owongs99@gmail.com>
2023-11-01 04:29:38 +0900

    feat($RacingCarController): 새로운 기능 구현
    $RacingCarController에 새로운 기능 구현
    - 레이싱카 리스트를 클래스 외부에서 삽입할 수 있도록 수정
    - 레이싱카의 위치를 업데이트하는 함수 구현

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -1,15 +1,27 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCarController {
 
-    private static List<RacingCar> racingCarList = new ArrayList<>();
+    private static List<RacingCar> racingCarList;
+
+    public static void setRacingCarList(List<RacingCar> _racingCarList) {
+        racingCarList = _racingCarList;
+    }
 
     public static RacingCar registerRacingCar(String name) {
         RacingCar newRacingCar = new RacingCar(name, 0);
         racingCarList.add(newRacingCar);
         return newRacingCar;
+    }
+
+    public static void updateRacingCar(RacingCar racingCar) {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            racingCar.updateLocation();
+        }
     }
 }
