a83dd8c7c9631586555e0e4c24385b92936a0069
ddangme <ssilver121@nav>
2023-10-31 21:29:27 +0900

    refactor: 게임 횟수 유효성 체크 클래스 분리

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -5,37 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Game {
-    private int finalRaceCount;
+    private final int finalRaceCount;
     private int currentRaceCount;
-    private List<Car> carList;
 
-    public void setCar(String carNameListString) {
-        setCarList(Arrays.stream(carNameListString.split(",")).toList());
-    }
-
-    public void setFinalRaceCount(String finalRaceCount) {
-        try {
-            this.finalRaceCount = Integer.parseInt(finalRaceCount);
-            validationFinalRaceCount();
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("1~2,147,483,647 사이의 값으로 입력해주세요.");
-        }
-    }
-
-    private void validationFinalRaceCount() {
-        if (finalRaceCount < 1) {
-            throw new IllegalArgumentException("1~2,147,483,647 사이의 값으로 입력해주세요.");
-        }
-    }
-
-    private void setCarList(List<String> carNameList) {
-        this.carList = carNameList.stream()
-                .map(Car::new)
-                .collect(Collectors.toList());
-    }
-
-    public List<Car> getCarList() {
-        return carList;
+    public Game(int finalRaceCount) {
+        this.finalRaceCount = finalRaceCount;
     }
 
     public void setCurrentRaceCount() {
--- src/main/java/racingcar/service/RaceCountValidationService.java
+++ src/main/java/racingcar/service/RaceCountValidationService.java
@@ -1,2 +1,24 @@
-package racingcar.service;public class RaceCountValidationService {
+package racingcar.service;
+
+public class RaceCountValidationService {
+
+    public void raceCountValidation(String raceCountInString) {
+        int raceCount = stringToInt(raceCountInString);
+        validationFinalRaceCount(raceCount);
+    }
+
+    private int stringToInt(String raceCountInString) {
+        try {
+            return Integer.parseInt(raceCountInString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1~2,147,483,647 사이의 숫자로 입력해주세요.");
+        }
+    }
+
+    private void validationFinalRaceCount(int raceCount) {
+        if (raceCount < 1) {
+            throw new IllegalArgumentException("1~2,147,483,647 사이의 숫자로 입력해주세요.");
+        }
+    }
+
 }
