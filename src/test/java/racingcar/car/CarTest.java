b9b42b565be2fa84e0e55d8c1e348393353bbf22
이병수 <qudtn0315@naver.com>
2023-10-31 15:36:41 +0900

    테스트 코드

---
+++ src/test/java/racingcar/car/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar.car;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @DisplayName("자동차 객체 테스트")
+    @Test
+    void 자동차_객체_테스트() {
+        String name = "pobi";
+        Car car = new Car(name);
+        Assertions.assertThat(car.carName()).isEqualTo(name);
+        Assertions.assertThat(car.carPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 자동차들_테스트() {
+        String[] carName = {"pobi", "zambo"};
+        CarList carList = new CarList(carName);
+
+        for (int i = 0; i < 5; i++) {
+            carList.printExecuteCarsMove();
+        }
+
+        Assertions.assertThat(carList.printWinnerList()).isNotEmpty();
+    }
+}
---
+++ src/test/java/racingcar/controller/ControllerTest.java
@@ -0,0 +1,49 @@
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+    private static Controller controller;
+
+    @BeforeEach
+    void 초기화() {
+        controller = new Controller();
+    }
+
+    @Test
+    void 자동차_이름_공백_예외처리() {
+        String input = "jaavi, ,jon";
+        String[] result = input.split(",");
+
+        assertThatThrownBy(() -> controller.checkInvalidate(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름에 공백이 들어갑니다.");
+    }
+
+    @Test
+    void 자동차_이름_글자수_예외처리() {
+        String input = "longName,hon,javvvi";
+        String[] result = input.split(",");
+
+        assertThatThrownBy(() -> controller.checkInvalidate(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름의 길이가 다섯 글자가 넘습니다.");
+    }
+
+    @Test
+    void 자동차_이름_중복_예외처리() {
+        String input = "java,java";
+        String[] result = input.split(",");
+
+        assertThatThrownBy(() -> controller.checkInvalidate(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 이름이 있습니다.");
+    }
+
+}
