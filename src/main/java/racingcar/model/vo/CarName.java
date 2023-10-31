05d9271d5c3a2a00864faaa18fbbbdd9bfa7e2b6
baksakcci <pshsh910@gmail.com>
2023-10-31 21:44:21 +0900

    test($CarNameMapperTest): add test `should_true_when_convertSuccessful()`
    - add `equals()` and `hashCode()` to ensure equivalence of `CarName` objects.

--- src/main/java/racingcar/model/vo/CarName.java
+++ src/main/java/racingcar/model/vo/CarName.java
@@ -1,5 +1,7 @@
 package racingcar.model.vo;
 
+import java.util.Objects;
+
 public class CarName {
 
     private final String name;
@@ -11,5 +13,22 @@ public class CarName {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
