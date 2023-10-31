9deece73ded90f45c822ae5fac1906182aeb935a
ppusda <ppusda1234@gmail.com>
2023-10-31 12:14:50 +0900

    fix: Message와 Util 분리

---
+++ src/main/java/racingcar/GameMessage.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class GameMessage {
+
+    public final String inputNameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final String inputTimeMessage = "시도할 횟수는 몇회인가요?";
+    public final String executeMessage = "실행 결과";
+    public final String finalWinnerMessage = "최종 우승자";
+
+}
--- src/main/java/racingcar/GameUtil.java
+++ src/main/java/racingcar/GameUtil.java
@@ -1,28 +1,43 @@
 package racingcar;
 
-import java.util.List;
-
 public class GameUtil {
+    private final CarUtil carUtil = new CarUtil();
 
-    public String getWinner(List<Car> cars) {
+    public void gameProceed(Game game) {
+        for(Car car : game.getCars()) {
+            carUtil.process(car);
+        }
+    }
+
+    public String getWinner(Game game) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for(Car car : cars) {
+        for(Car car : game.getCars()) {
             if(stringBuilder.isEmpty()) {
-                stringBuilder.append(winnerCheck(car));
+                stringBuilder.append(getWinner(car, game.goal));
             } else {
-                stringBuilder.append(", ").append(winnerCheck(car));
+                stringBuilder.append(", ").append(getWinner(car, game.goal));
             }
         }
 
         return stringBuilder.toString();
     }
 
-    public String winnerCheck(Car car) {
-        if(car.getProgress().length() >= 5) {
+    public String getWinner(Car car, int goal) {
+        if(car.getProgress().length() >= goal) {
             return car.getRacingCar();
         }
         return "";
+    }
+
+    public void show(String message) {
+        System.out.println(message);
+    }
+
+    public void ProgressShow(Game game) {
+        for(Car car: game.getCars()) {
+            System.out.println(car.getRacingCar() + " : " + car.getProgress());
+        }
     }
 
 }
