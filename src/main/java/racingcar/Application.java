0f290bca004a7f5ab6901ea17df4adf2a4bf1829
zunione <xtwish1@ajou.ac.kr>
2023-10-31 10:25:41 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args)
+    {
+        /* Get car names */
+        System.out.print("Name of the racing cars (distinguish by ','): ");
+        String input = Console.readLine();
     }
 }
