406cd88533a7113b7228c27d57c8e7eca80458e1
JGM <cnxw4570123@gmail.com>
2023-11-01 00:54:40 +0900

    test : add test code for Game class 3차수만큼 게임을 진행하는 테스트 메소드 추가

--- src/test/java/racingcar/MyTest.java
+++ src/test/java/racingcar/MyTest.java
@@ -3,14 +3,19 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 class MyTest extends NsTest {
-
     private final GameUtil gameUtil = new GameUtil();
     @Test
     void 한_칸_움직이면_한_줄_추가된다() {
@@ -46,6 +51,19 @@ class MyTest extends NsTest {
         racerList.get(2).move();
         gameUtil.printEachResult(racerList);
         assertThat(output()).contains("첫 번째 : ", "김 : ", "더벤티선수 : -");
+    }
+
+    @Test
+    void 방장_사기_맵_세_차수_실행한다(){
+        try(MockedStatic<Console> console = mockStatic(Console.class);
+            MockedStatic<Randoms> random = mockStatic(Randoms.class)) {
+            when(Console.readLine()).thenReturn("방장,유저1,유저2", "3");
+            when(Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(4, 4, 3, 4, 4, 3, 4, 3, 4);
+            Game game = new Game();
+            game.gameStart();
+        }
+        assertThat(output()).contains("방장 : -", "유저1 : -", "유저2 : ", "방장 : --", "유저1 : --", "유저2 : ", "방장 : ---", "유저1 : ",
+                "유저2 : -", "최종 우승자 : 방장");
     }
 
     @Override
