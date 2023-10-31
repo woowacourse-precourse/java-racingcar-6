c7ee6c4dffba25a74894985e5cf04ed77c6008a0
koomchang <leahpar0401@gmail.com>
2023-10-31 14:51:14 +0900

    refactor: Location 테스트를 assertThat 메소드를 이용하여 리팩토링

--- src/test/java/racingcar/model/LocationTest.java
+++ src/test/java/racingcar/model/LocationTest.java
@@ -1,22 +1,17 @@
 package racingcar.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LocationTest {
 
-    private Location location;
-
-    @BeforeEach
-    public void setUp() {
-        location = Location.init();
-    }
+    private final Location location = Location.init();
 
     @Test
     public void 위치_이동시_랜덤한_값에_따라_0_혹은_1씩_움직인다() {
         Location moved = location.move();
-        assertTrue(moved.location() == 0 || moved.location() == 1);
+        int currentLocationValue = moved.location();
+        assertThat(currentLocationValue).isBetween(0, 1);
     }
 }
