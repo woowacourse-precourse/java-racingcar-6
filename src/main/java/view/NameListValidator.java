984cd736261baee93da25c2f549ea6cfbbdac1aa
nayonsoso <panda0329@naver.com>
2023-10-31 23:12:38 +0900

    refactor(NameListValidator): 함수와 매개변수명이 더 의미가 드러나게 수정

--- src/main/java/view/NameListValidator.java
+++ src/main/java/view/NameListValidator.java
@@ -1,34 +1,35 @@
 package view;
 
 import java.util.HashSet;
+import java.util.List;
 
 public class NameListValidator {
-    private void checkNotEmpty(String userInput) {
-        if (userInput.isEmpty()) {
+    private void validateNotEmpty(String input) {
+        if (input.isEmpty()) {
             throw new IllegalArgumentException("입력된 값이 없습니다.");
         }
     }
 
-    private void checkNotEndsWithComma(String userInput) {
-        if (userInput.endsWith(",")) {
+    private void validateNotEndsWithComma(String input) {
+        if (input.endsWith(",")) {
             throw new IllegalArgumentException("마지막이 쉼표로 끝납니다.");
         }
     }
 
-    private void checkContainsRepeatingCommas(String userInput) {
-        String ignoreSpace = ignoreSpace(userInput);
-        for (int i = 0; i < ignoreSpace.length() - 1; i++) {
-            if (ignoreSpace.charAt(i) == ',' && ignoreSpace.charAt(i + 1) == ',') {
+    private void validateContainsRepeatingCommas(String input) {
+        String inputWithoutSpace = ignoreSpace(input);
+        for (int i = 0; i < inputWithoutSpace.length() - 1; i++) {
+            if (inputWithoutSpace.charAt(i) == ',' && inputWithoutSpace.charAt(i + 1) == ',') {
                 throw new IllegalArgumentException("쉼표 사이에 값이 없습니다.");
             }
         }
     }
 
-    private String ignoreSpace(String userInput) {
-        return userInput.replace(" ", "");
+    private String ignoreSpace(String input) {
+        return input.replace(" ", "");
     }
 
-    private void checkNameLength(String[] names, int maxLength) {
+    private void validateNameLength(List<String> names, int maxLength) {
         for (String name : names) {
             if (name.length() > maxLength) {
                 String message = "이름의 길이가 " + maxLength + "자가 넘습니다.";
@@ -37,7 +38,7 @@ public class NameListValidator {
         }
     }
 
-    private void checkNameDuplication(String[] names){
+    private void validateNameDuplication(List<String> names){
         HashSet<String> nameSet = new HashSet<>();
         for(String name : names){
             if(!nameSet.add(name)){
