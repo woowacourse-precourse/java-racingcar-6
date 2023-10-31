dee554bae0d5e56a2c49b3329654c8357787fb9c
zunione <xtwish1@ajou.ac.kr>
2023-10-31 10:26:52 +0900

    feat: 움직일 횟수 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,14 @@ public class Application {
         /* Get car names */
         System.out.print("Name of the racing cars (distinguish by ','): ");
         String input = Console.readLine();
+
+        /* Get number of moves */
+        System.out.print("How many times do the cars move?: ");
+        input = Console.readLine();
+        if (!input.matches("\\d"))
+        {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+        int moves = Integer.parseInt(input);
     }
 }
