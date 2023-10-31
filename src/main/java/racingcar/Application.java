fab6c4406657f1233e8b2230b61a891c3fb22b62
donghoonyeom <dua5410773@naver.com>
2023-10-31 19:57:13 +0900

    refactor(Application): 사용자 입력 로직 업데이트
    Application 클래스에서 ConsoleView를 사용하여 사용자 입력을 받는 부분을 업데이트했습니다. 'getInputNumberOfAttempts()' 메서드를 'readValidNumberOfAttempts()' 메서드로 변경하여 유효성 검사를 수행하고 예외를 처리합니다. 이로써 사용자로부터 올바른 시도 횟수를 입력받을 수 있게 되었습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,22 +7,16 @@ import racingcar.view.ConsoleView;
 
 import java.util.List;
 
-
 public class Application {
     public static void main(String[] args) {
-        try {
             ConsoleView consoleView = new ConsoleView();
             List<String> carNames = consoleView.getInputCarNames();
             List<Car> cars = Car.createCars(carNames);
-            int numberOfAttempts = consoleView.getInputNumberOfAttempts();
+            int numberOfAttempts = consoleView.readValidNumberOfAttempts();
             consoleView.printMessage("");
 
             RacingGame racingGame = new RacingGame(cars);
             GameController gameController = new GameController(racingGame, consoleView);
             gameController.playGame(numberOfAttempts);
-
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
-        }
     }
 }
