35af8008026dc46afb49d28c96cc2793e186126b
myonjin <kjinabc@gmail.com>
2023-11-01 05:10:38 +0900

    Feat : race 기능

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import racingcar.domain.Car;
+import racingcar.domain.CarList;
+import racingcar.view.outputView;
+
+public class Game {
+    public static void run(CarList carList,Integer tryNum){
+
+        for(int i = 0; i<tryNum;i++){
+            race(carList);
+            outputView.printMessage(carList);
+            System.out.println();
+        }
+        outputView.winner(carList);
+
+    }
+
+    private static void race(CarList carList) {
+        for (Car car : carList.getCarList()) {
+            car.move();
+        }
+    }
+
+
+}
