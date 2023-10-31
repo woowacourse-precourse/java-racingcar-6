770bdd1365be49d7ea402850bb32b9242e181e77
nature1216 <annekim1216@naver.com>
2023-11-01 02:53:54 +0900

    test: 예외처리 테스트 추가 자동차 이름 입력값이 쉼표로 시작하거나 끝날 때 예외발생

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,23 +1,40 @@
 package racingcar.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import racingcar.controller.RacingController;
 import racingcar.service.RacingService;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.*;
 
 public class ValidatorTest {
 
     RacingService racingService = new RacingService();
     RacingController racingController = new RacingController();
+
     @Test
     void 자동차_이름에_공백만_있는_경우() {
         String input = "pobi,woni, ";
 
         assertThatThrownBy(() ->
                 racingService.stringToList(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름_입력값이_쉼표로_끝나는_경우() {
+        String input = "pobi,woni,jun,";
+
+        assertThatThrownBy(() ->
+                Validator.validateNameEndsWithComma(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름_입력값이_쉼표로_시작하는_경우() {
+        String input = ",pobi,woni,jun";
+
+        assertThatThrownBy(() ->
+                Validator.validateNameStartsWithComma(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
