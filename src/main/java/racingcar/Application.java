104bf7bb9ec0ba5807b78223eab26de40c4629fc
chanhoim <cksgh201@naver.com>
2023-11-01 02:09:07 +0900

    feat(main): 메인 함수에서 게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.CarRacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarRacingGame carRacingGame =  new CarRacingGame();
+        carRacingGame.run();
     }
 }
