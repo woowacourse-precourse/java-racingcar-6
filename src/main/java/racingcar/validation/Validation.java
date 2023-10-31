ac1dc6f91b6d25f36c9a9c01886c9de8d9e15a21
meluvrose <tanggu3910@gmail.com>
2023-10-31 17:38:16 +0900

    fix: edit Exception that fail to requirements

--- src/main/java/racingcar/validation/Validation.java
+++ src/main/java/racingcar/validation/Validation.java
@@ -2,15 +2,13 @@ package racingcar.validation;
 
 public class Validation {
 
-    public static void validNames(String names){
+    private static boolean isConsecutiveComma(String names){
         int i = 0;
         boolean prevComma = false;
 
         while (i < names.length()){
             if (names.charAt(i) == ','){
-                if (prevComma) {
-                    throw new IllegalArgumentException("잘못된 입력입니다. (연속된 ',')");
-                }
+                if (prevComma) return true;
                 prevComma = true;
                 i++;
                 continue;
@@ -18,17 +16,40 @@ public class Validation {
             prevComma = false;
             i++;
         }
+        return false;
     }
 
-    public static void validCount(String count){
+    private static boolean isLengthen(String names){
+        String[] nameList = names.split(",");
+        int i = 0;
+
+        while (i < nameList.length){
+            if (nameList[i].length() > 5) return true;
+            i++;
+        }
+        return false;
+    }
+
+    private static boolean isNumberString(String count){
         int i = 0;
 
         while (i < count.length()){
             char c = count.charAt(i);
-            if (c < '0' || c > '9') {
-                throw new IllegalArgumentException("숫자를 입력해주세요.");
-            }
+            if (c < '0' || c > '9') return false;
             i++;
+        }
+        return true;
+    }
+
+    public static void validNames(String names){
+        if (isConsecutiveComma(names) || isLengthen(names)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    public static void validCount(String count){
+        if (!isNumberString(count)) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
     }
 }
