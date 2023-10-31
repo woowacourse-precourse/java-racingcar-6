1edcf6edc5c81adc32894b167e2ee1b93a95c067
kunsanglee <gosmdochee2@gmail.com>
2023-10-31 10:46:43 +0900

    refactor(RacingRoundResult): 자동차 정보를 담고있는 변수명 변경 carInfoList라고 되어있던 변수명을 추후 리스트가 아닌 다른 자료구조로 변경될 수 있으므로 변수명에 컬렉션 타입을 제거하여 변경시에 타입과 변수명의 불일치 가능성을 제거했다.

--- src/main/java/racingcar/domain/RacingRoundResult.java
+++ src/main/java/racingcar/domain/RacingRoundResult.java
@@ -6,14 +6,14 @@ import racingcar.dto.CarInfo;
 
 public class RacingRoundResult {
     private static final String NOT_AVAILABLE_CAR_LIST_EXCEPTION = "올바르지 않은 자동차 리스트입니다.";
-    private final List<CarInfo> carInfoList;
+    private final List<CarInfo> carInfos;
 
     public RacingRoundResult(List<CarInfo> carInfoList) {
-        this.carInfoList = carInfoList;
+        this.carInfos = carInfoList;
     }
 
-    public List<CarInfo> getCarInfoList() {
-        return this.carInfoList.stream()
+    public List<CarInfo> getCarInfos() {
+        return this.carInfos.stream()
                 .map(carInfo -> new CarInfo(carInfo.name(), carInfo.position()))
                 .collect(Collectors.toUnmodifiableList());
     }
@@ -24,14 +24,14 @@ public class RacingRoundResult {
     }
 
     private List<String> getSamePosition(int maxPosition) {
-        return this.carInfoList.stream()
+        return this.carInfos.stream()
                 .filter(carInfo -> carInfo.position() == maxPosition)
                 .map(CarInfo::name)
                 .collect(Collectors.toList());
     }
 
     private int getMaxPosition() {
-        return this.carInfoList.stream()
+        return this.carInfos.stream()
                 .mapToInt(CarInfo::position)
                 .max()
                 .orElseThrow(() -> new IllegalArgumentException(NOT_AVAILABLE_CAR_LIST_EXCEPTION));
