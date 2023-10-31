a70ebce40ff2bc539f08719dc5e985c84f9e3bc6
Choo <choo000407@naver.com>
2023-11-01 01:18:02 +0900

    test(Car): `전진 시도` 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,16 +1,30 @@
 package racingcar.model;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.util.CustomIntGenerator;
+import racingcar.util.RandomIntGenerator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobipo", "po bip"})
     void 글자수_제한_넘긴_이름으로_자동차_생성시_예외_발생(String name) {
-        assertThatThrownBy(() -> new Car(name))
+        assertThatThrownBy(() -> new Car(name, new RandomIntGenerator()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자 이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,0", "3,0", "5,1", "9,1"}, delimiter = ',')
+    void 전진_시도_시_기준점을_넘을때만_위치_1만큼_증가(int pickedNumber, int expectedMoveCount) {
+        Car car = new Car("test", new CustomIntGenerator(pickedNumber));
+
+        car.tryMove();
+
+        assertThat(car.getMoveCount()).isEqualTo(expectedMoveCount);
     }
 }
