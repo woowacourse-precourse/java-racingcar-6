224bdd4e13a123d066a06942b69900f4592ffa43
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-31 20:03:49 +0900

    feat : input으로 공백이 입력되면 예외발생 기능 추가

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -3,16 +3,29 @@ package racingcar.domain;
 import java.util.List;
 
 public class Validator {
-    public boolean checkCarNameLength(List<String> carNameList) {
+    public void validateCarNameLength(List<String> carNameList) {
         for (String carName : carNameList) {
             if (carName.length() > 5) {
-                return false;
+                throw new IllegalArgumentException("유효하지 않은 값을 입력하여 게임을 종료하겠습니다.");
             }
         }
-        return true;
     }
 
-    public boolean isForward(int number) {
-        return number >= 4;
+    public int parseStringToInteger(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public void validateIsNumber(String input) {
+        try {
+            parseStringToInteger(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력 값이 유효한 숫자가 아닙니다.", e);
+        }
+    }
+
+    public static void valideteIsEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
     }
 }
