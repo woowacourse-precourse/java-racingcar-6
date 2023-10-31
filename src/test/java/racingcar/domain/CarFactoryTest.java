2c570889dc54ec87bd43586937a209e5ce36ce6d
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-31 16:13:54 +0900

    test: 자동차 생산 기능 테스트

---
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarFactoryTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi1", "pobi2", "pobi3"})
+    void 자동차_생산(String carName) {
+        //given
+        List<String> carNameList = List.of(carName);
+
+        //when
+        CarFactory carFactory = new CarFactory();
+        Cars cars = carFactory.produceCars(carNameList);
+
+        //then
+        assertThat(cars.getCarList()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.toList())).isEqualTo(List.of(carName));
+    }
+}
