179fff5769b9e7ed56d2432d57974cf52cd4401b
GaHee99 <u1879@sookmyung.ac.kr>
2023-10-31 16:20:46 +0900

    feat(NameTest): 이름 validation 테스트코드 작성

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Name;
+import racingcar.util.Constants;
+
+public class NameTest {
+
+    Name name;
+
+    @Test
+    void 이름객체의_예외_처리() {
+        assertThatThrownBy(() -> new Name("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Constants.EXCEED_NAME_SIZE.constant);
+    }
+
+}
