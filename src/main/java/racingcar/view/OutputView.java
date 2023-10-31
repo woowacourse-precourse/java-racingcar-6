dd351e52623b8b10561117b9e81eabd91c96a87e
Yerinnnn <shinyerin0216@gmail.com>
2023-10-31 17:36:12 +0900

    Refactor: "OutputView 클래스 내에 메소드 생성"
    -GameController 클래스에 있던 출력값 관련 메소드들을 OutputView 클래스로 이동.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,29 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class OutputView {
+
+    public void printEachCarResult(String name, int score) {
+        StringBuilder result = new StringBuilder(name + " : ");
+        Optional<String> formattedPosition = Stream.generate(() -> "-").limit(score).reduce((a, b) -> a + b);
+        formattedPosition.ifPresent(result::append);
+        System.out.println(result);
+    }
+
+    public void result(List<Car> cars) {
+        StringBuilder result = new StringBuilder();
+        result.append("최종 우승자 : ");
+
+        int max_score = cars.stream().max(Comparator.comparingInt(Car::getScore)).get().getScore();
+        result.append(cars.stream().filter(Car -> Car.getScore() == max_score).map(Car::getName).collect(Collectors.joining(", ")));
+
+        System.out.println(result);
+    }
 }
