fba22ff89e0fbf6570367524d947209a421ba6d8
H-server <hani103201@naver.com>
2023-10-31 21:23:02 +0900

    test: 콘솔 입력값 테스트 추가

--- src/test/java/racingcar/domain/UserTest.java
+++ src/test/java/racingcar/domain/UserTest.java
@@ -6,16 +6,37 @@ import static racingcar.domain.User.ATTEMPTS_INT_ERROR_MESSAGE;
 import static racingcar.domain.User.ATTEMPTS_NEGATIVE_ERROR_MESSAGE;
 import static racingcar.domain.User.CAR_NAME_ERROR_MESSAGE;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class UserTest {
     private User user;
+    private InputStream originalSystemIn;
     @BeforeEach
     void setUp() {
         user = new User();
+        originalSystemIn = System.in;
+    }
+
+    @AfterEach
+    void restoreSystemIn() {
+        System.setIn(originalSystemIn);
+    }
+// mock으로 대체하는 방법?
+    @Test
+    void 자동차_이름_입력() {
+        final String input = "pobi,woni,jun";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(user.inputCarsName()).isEqualTo(input);
     }
 
     @Test
@@ -31,9 +52,21 @@ class UserTest {
     void 예외_자동차_이름_글자수_초과() {
         final String input = "pobi,woni,goodjun";
 
-        assertThatThrownBy(() -> user.splitCarsName(input))
+        user.splitCarsName(input);
+
+        assertThatThrownBy(() -> user.checkCarNameUnderFive())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(CAR_NAME_ERROR_MESSAGE);
+    }
+
+    @Test
+    @Disabled("자동차_이름_입력과 함께 존재하면 java.util.NoSuchElementException: No line found 에러가 생김.")
+    void 시행_횟수_입력() {
+        final String input = "4";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(user.inputAttempts()).isEqualTo(input);
     }
 
     @ParameterizedTest
