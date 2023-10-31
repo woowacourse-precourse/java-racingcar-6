26269479521d1aa2c0f52cc68dd5670de3864fab
mksystem98 <mksystem98@gmail.com>
2023-11-01 00:12:33 +0900

    feat: 게임을 실행하는 로직 구현

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,44 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.RacingGame;
+import racingcar.model.AttemptCountValidator;
+import racingcar.model.CarNameValidator;
+import racingcar.model.RealRandomGenerator;
+import racingcar.view.InputView;
+import racingcar.view.RacingAttemptInput;
+
+public class RacingController {
+
+    private InputView inputView;
+    private RacingGame racingGame;
+
+    public RacingController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void playRacingGame() {
+        // 사용자로부터 자동차 이름을 받기
+        inputView.requestCarNames();
+        String carNamesInput = inputView.getUserCarNames();
+        List<String> carNames = inputView.parseCarNames(carNamesInput);
+        CarNameValidator.validateCarNames(carNames);
+
+        // 사용자로부터 시도할 횟수 받기
+        RacingAttemptInput attemptInput = new RacingAttemptInput();
+        attemptInput.requestAttemptCount();
+        int attemptCount = attemptInput.getAttemptCount();
+        AttemptCountValidator.validateIsPositive(attemptCount);
+
+        // 게임 초기화
+        racingGame = new RacingGame(carNames, new RealRandomGenerator());
+
+        // 게임 실행 로직
+        for (int i = 0; i < attemptCount; i++) {
+            racingGame.race();
+        }
+    }
+
+}
+
+
