d7661ead19b6834dc6fae76e3d488642b10232d3
hoyeon <highoption@naver.com>
2023-10-31 21:33:30 +0900

    feat(RacingGameController) : 랜덤 넘버를 생성 하여 게임 진행
    - 입력 받은 게임 횟수만큼 게임을 진행
    - 랜덤 넘버가 4이상이면 앞으로 전진
    - 각 라운드 마다 앞으로 간 거리 표시
    - 게임 진행후 최종 우승자 출력

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -4,11 +4,14 @@ import static racingcar.model.GameCountValidator.gameCountValidation;
 import static racingcar.utils.Utils.convertStringToList;
 import static racingcar.view.InputView.inputCarsName;
 import static racingcar.view.InputView.inputGameTryNumber;
+import static racingcar.view.OutputView.gameStartMessage;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.model.Car;
 import racingcar.model.Cars;
+import racingcar.model.RandomNumberGenerator;
+import racingcar.view.OutputView;
 
 public class RacingGameController {
     private int gameCount;
@@ -23,6 +26,16 @@ public class RacingGameController {
         String gameTryNumber = inputGameTryNumber();
         gameCountValidation(gameTryNumber);
         gameCount = Integer.parseInt(gameTryNumber);
-
+        gameStartMessage();
+        for (int i = 0; i < gameCount; i++) {
+            for (Car car : cars.getCars()) {
+                int randomNumber = RandomNumberGenerator.makeNumber();
+                if (randomNumber >= 4) {
+                    car.go();
+                }
+            }
+            OutputView.printOneGameResult(cars);
+        }
+        OutputView.printWinners(cars.winner());
     }
 }
