c939d5729e2666fb806215896b8be5c2338a53e1
권장순 <jsoonworld@gmail.com>
2023-10-31 23:05:41 +0900

    Test(racingcar) : 자동차 이름을 쉼표로 구분하여 리스트로 변환하는 단위 테스트 코드 구현 및 통과

--- src/test/java/racingcar/v4/CreateCarTest.java
+++ src/test/java/racingcar/v4/CreateCarTest.java
@@ -3,7 +3,10 @@ package racingcar.v4;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 class CreateCarTest {
@@ -33,11 +36,18 @@ class CreateCarTest {
     }
 
     @Test
-    void 자동차_이름에_쉼표가_없는_경우_예외가_발생한다() {
+    void 입력값에_쉼표가_없는_경우_예외가_발생한다() {
         CreateCar createCar = new CreateCar();
         assertThatThrownBy(() -> createCar.splitCarNames("페라리 벤츠"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름을 쉼표로 구분해주세요.");
+    }
+
+    @Test
+    void 자동차_이름을_쉼표로_구분하여_리스트로_변환한다() {
+        CreateCar createCar = new CreateCar();
+        List<String> carNames = createCar.splitCarNames("벤츠,페라리,레드불");
+        assertThat(carNames).containsExactly("벤츠", "페라리", "레드불");
     }
 
 }
