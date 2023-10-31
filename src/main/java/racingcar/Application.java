bb4d4c3c73ba5a5ffe76ca43e977e84058e8d927
Haerang <jbu418@naver.com>
2023-11-01 01:40:10 +0900

    Application main함수 코딩
    기능들이 잘 구현되도록 메인함수 코딩

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,16 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        game game = new game();
+
+        game.store_name();
+        game.input_trynumber();
+        System.out.println("\n실행 결과");
+        while(true) {
+            game.car_position();
+            game.print_simul();
+            if(game.try_number == 0) break;
+        }
+        game.print_max();
     }
 }
