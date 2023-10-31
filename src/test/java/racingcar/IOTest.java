b372b853b8b2b33a5bcb3802aacc57c8021cb76e
jun02160 <jun020216@naver.com>
2023-10-31 10:46:27 +0900

    test : 입력에 대한 예외처리 테스트 추가

---
+++ src/test/java/racingcar/IOTest.java
@@ -0,0 +1,53 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.global.util.GameInput.validateCarList;
+import static racingcar.global.util.GameInput.validateCnt;
+
+import org.junit.jupiter.api.Test;
+
+public class IOTest {
+
+    @Test
+    public void 여러_차량명_입력의_구분자가_쉼표가_아닌_경우_예외처리() {
+        String input = "jun|    seo";
+        assertThat(input).isNotEqualTo(new String[] {"jun", "seo"});
+    }
+
+    @Test
+    public void 여러_차량명_입력에_공백이_포함된_경우_예외처리() {
+        String input = "      jun,     seo ";
+        assertThat(input.trim().split(",\\s*")).isEqualTo(new String[] {"jun", "seo"});
+    }
+
+    @Test
+    public void 차량명이_5자를_초과하는_경우_예외처리() {
+        String[] input = new String[]{"jun", "seodse"};
+        assertThatThrownBy(() -> validateCarList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    @Test
+    public void 입력값이_비어_있는_경우_예외처리() {
+        String input1 = "";
+        String[] input2 = new String[]{""};
+        assertThatThrownBy(() -> validateCnt(input1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 비어 있습니다.");
+        assertThatThrownBy(() -> validateCarList(input2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 비어 있습니다.");
+    }
+
+    @Test
+    public void 입력된_회차_형식이_숫자가_아닌_경우_예외처리() {
+        String input = "!";
+
+        assertThatThrownBy(() -> validateCnt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자 형식이 올바르지 않습니다.");
+    }
+
+}
