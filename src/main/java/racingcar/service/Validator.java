b96632238752ec5bd711030aec4e51d243b6158c
Seyeon <wjdtpdus828@naver.com>
2023-10-31 21:52:27 +0900

    feat(Validator): 자동차 이름의 길이가 5 초과일 경우 예외처리를 발생시키는 validateNameLength 메소드 구현

--- src/main/java/racingcar/service/Validator.java
+++ src/main/java/racingcar/service/Validator.java
@@ -6,10 +6,18 @@ import java.util.Set;
 
 public class Validator {
     TypeConverter typeConverter = new TypeConverter();
+    private static final String NAME_ERROR_MESSAGE = "이름의 길이가 5보다 큽니다.";
     public void validateCarNames(List<String> input){
-        validateLength(input);
-        validateSpace(input);
-        validateDuplication(input);
+        validateNameLength(input);
+        validateNameSpace(input);
+        validateNameDuplication(input);
+    }
+    public void validateNameLength(List<String> input){
+        for(String carName : input){
+            if(carName.length() > 5){
+                throw new IllegalArgumentException(NAME_ERROR_MESSAGE);
+            }
+        }
     }
 
 
