72a53c5619e6c37d171b3aefd762c114416976ed
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-31 13:38:42 +0900

    feat: implement exception handling when round number is not a number
    create isNumeric method to check whether input is number

--- src/main/java/racingcar/validator/RoundNumberValidation.java
+++ src/main/java/racingcar/validator/RoundNumberValidation.java
@@ -12,11 +12,18 @@ public class RoundNumberValidation {
 
     private void validate() {
         isEmpty();
+        isNumeric();
     }
 
     private void isEmpty() {
         if (roundNumber.isEmpty()) {
             throw new IllegalArgumentException("라운드 회수를 입력해주셔야 합니다.");
+        }
+    }
+
+    private void isNumeric() {
+        if (!roundNumber.matches("[1-9][0-9]*")) {
+            throw new IllegalArgumentException("시도 회수는 1 이상의 정수를 입력해주셔야 합니다.");
         }
     }
 }
