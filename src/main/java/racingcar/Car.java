bbe9b9c1a504b9c2d757865156bdd8119135f78a
Hyunstone <hyun104467@naver.com>
2023-10-31 16:11:33 +0900

    [Feat] 움직이는 조건 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int moveAmount;
 
     public void move() {}
-//    public boolean isMoveCondition() {}
+    public boolean isMoveCondition() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
 }
