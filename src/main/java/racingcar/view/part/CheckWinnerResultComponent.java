c9bd4fa60698a52d32e59195de27a5996e41964e
JaehongDev <println@kakao.com>
2023-10-31 12:58:04 +0900

    feat: 우승자 출력 컴포넌트 구현

--- src/main/java/racingcar/view/part/CheckWinnerResultComponent.java
+++ src/main/java/racingcar/view/part/CheckWinnerResultComponent.java
@@ -1,13 +1,13 @@
 package racingcar.view.part;
 
-import java.util.List;
 import racingcar.event.core.EventListener;
+import racingcar.event.part.CollectWinnerEvent;
 import racingcar.view.core.OutputView;
 import racingcar.view.core.ScreenComponent;
 
 public record CheckWinnerResultComponent(OutputView outputView, EventListener eventListener) implements ScreenComponent {
     @Override
     public void render() {
-        this.outputView.printWinner(List.of("1", "2"));
+        this.outputView.printWinner(eventListener.listenWithResult(CollectWinnerEvent::new));
     }
 }
