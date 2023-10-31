438e455da8735bea820a7e1f5f76a4e7a573b3c6
giljin <68088970+giljin@users.noreply.github.com>
2023-10-31 11:08:17 +0900

    refactor: 시작 LAP 사이즈 상수로 변경

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 public class RacingGame {
+    private static final int START_LAP_SIZE = 1;
+
     private final Cars cars;
     private final Lap lap;
 
@@ -12,7 +14,7 @@ public class RacingGame {
     public void play() {
         GameIO.printResultMessage();
 
-        int currentLapSize = 1;
+        int currentLapSize = START_LAP_SIZE;
         Lap currentLap = new Lap(currentLapSize);
         while (!lap.isFinish(currentLap)) {
             cars.race();
