a8c591c4700f473842e2351b17e72dbb2ddebd7c
yj kim <yjee0215@gmail.com>
2023-11-01 01:42:49 +0900

    style: try 변수 -> tries로 변경

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -5,7 +5,7 @@ public class GameView {
     private static GameView INSTANCE;
 
     private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
-    private final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
+    private final String TRIES_INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
     private final String RESULT_MESSAGE = "\n실행 결과\n";
     private final String WINNER_MESSAGE = "최종 우승자 : ";
 
@@ -30,7 +30,7 @@ public class GameView {
     }
 
     public void printTryInputMessage() {
-        System.out.print(TRY_INPUT_MESSAGE);
+        System.out.print(TRIES_INPUT_MESSAGE);
     }
 
     public void printResultMessage(String gameResult) {
