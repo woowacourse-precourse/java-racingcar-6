c607e398b3c84134f8561a565a4eed294184e3a4
Yeop <syt05342@naver.com>
2023-10-31 23:20:03 +0900

    fix: RandomNumberGenerator에서 인터페이스 구현 및 메서드 오버라이드 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -2,11 +2,12 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.*;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator{
     private static final int MIN_VAL = 0;
     private static final int MAX_VAL = 9;
 
-    private int randomNumber() {
+    @Override
+    public int generate() {
         return Randoms.pickNumberInRange(MIN_VAL, MAX_VAL);
     }
 
