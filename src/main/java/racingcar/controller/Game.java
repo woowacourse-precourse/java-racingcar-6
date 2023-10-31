2b32701bedff4ac261a9ac824d87853f07d0fd8c
JoonKyo <joonkyolee@gmail.com>
2023-10-31 23:38:54 +0900

    feat(Game): 랜덤으로 추출한 값이 4 이상일 경우 true를 반환시켜 이동하게 만드는 메서드 추가

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,14 +1,25 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import racingcar.model.Car;
 import racingcar.model.Player;
 import racingcar.view.InputView;
 
 public class Game {
+    private static final int FIRST_BOUND = 0;
+    private static final int LAST_BOUND = 9;
+    private static final int MOVE_LEAST_BOUND = 4;
     private final Player player = new Player();
     private final InputView inputView = new InputView();
+    private List<Car> cars;
 
     public Game(){
        player.setCarName(inputView.askCarName());
        player.setAttemptNumber(inputView.askAttemptNumber());
+    }
+
+    public boolean canMove(){
+        return Randoms.pickNumberInRange(FIRST_BOUND, LAST_BOUND) >= MOVE_LEAST_BOUND;
     }
 }
