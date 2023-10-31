47735f3c1ec474e2230ccbf31ab51ee069da9c5e
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-31 23:53:59 +0900

    feat: Count 입력 메소드 정의

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(getCar().toString());
     }
 
     public static String[] getCar() {
@@ -13,5 +14,16 @@ public class Application {
         String[] carNames = answer.split(",");
         return carNames;
     }
- 
+
+    public static int getCount() {
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String answer = Console.readLine();
+        //TODO: Max값 확인도 해야하지 않을까?
+        if (!answer.matches("\\d+")) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+        return Integer.parseInt(answer);
+    }
+
+    
 }
