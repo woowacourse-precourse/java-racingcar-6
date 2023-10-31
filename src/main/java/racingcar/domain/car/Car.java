fb951797de20086a73d740e057bcb556f2fc8356
penrose15 <seongju99@naver.com>
2023-10-31 22:49:53 +0900

    Fix: Car name 5글자 이상 작성 시 오류 나게 수정

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,11 +1,14 @@
 package racingcar.domain.car;
 
+import java.nio.charset.StandardCharsets;
+
 public class Car {
     private final String name;
     private int moveCount;
     private RandomUtil randomUtil;
 
     public Car(String name, RandomUtil randomUtil) {
+        validateCarNameLengthLessThanFive(name);
         this.name = name;
         this.moveCount = 0;
         this.randomUtil = randomUtil;
@@ -27,6 +30,12 @@ public class Car {
 
     public int compareTo(Car otherCar) {
         return this.moveCount - otherCar.getMoveCount();
+    }
+
+    private void validateCarNameLengthLessThanFive(String carName) {
+        if(carName.length() >= 5) {
+            throw new IllegalArgumentException("자동차 이름 길이는 5글자 이내");
+        }
     }
 
     public String getName() {
