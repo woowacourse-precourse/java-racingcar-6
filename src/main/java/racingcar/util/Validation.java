f5577f8a5fcdbcba713902facccb3e12a88996ea
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-31 15:28:49 +0900

    refactor(Validation): add descrption IllegalArgumentException
    IllegalArgumentException() -> IllegalArgumentException('이름은 5자 이하로 작성해주세요.') in checkCarName function
    IllegalArgumentException() -> IllegalArgumentException('숫자가 아닌 다른 문자를 입력하셨습니다.') in checkTryNumber function

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -2,9 +2,10 @@ package racingcar.util;
 
 public class Validation {
     private static final int MAX_NAME_LENGTH = 5;
+
     public static void checkCarName(String carName) throws IllegalArgumentException {
         if(carName.length() > MAX_NAME_LENGTH){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("이름은 5자 이하로 작성해주세요.");
         }
     }
 
@@ -12,7 +13,7 @@ public class Validation {
         try {
             Integer.parseInt(tryNum);
         }catch (NumberFormatException ex) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자가 아닌 다른 문자를 입력하셨습니다.");
         }
     }
 }
