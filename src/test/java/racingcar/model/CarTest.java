7b540db96124a51bb4cd2da9cbdc450f5b6f552c
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 17:45:52 +0900

    test : 숫자가 4 이상일 경우 전진 테스트

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,35 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @Nested
+   class MoveCatTest {
+        // given
+        Name testName = new Name("car");
+        Distance testDistance = new Distance(0);
+        Car testCar = new Car(testName, testDistance);
+
+        @Test
+        void 숫자가_4_이상일_경우_전진한다() {
+            // given
+            Integer input = 4;
+            Integer carPosition = testCar.carDistance.getDistance();
+
+            // when
+            testCar.move(input);
+
+            // then
+            assertThat(testCar.carDistance.getDistance()).isEqualTo(carPosition + 1);
+        }
+
+        @Test
+        void 숫자가_4_미만이면_전진하지_않는다() {
+
+        }
+    }
+}
