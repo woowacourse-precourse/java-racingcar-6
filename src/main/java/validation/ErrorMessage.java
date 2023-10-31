0e8aec3b35829dcac29322781cffe6246422d83c
chan-won <clw8679@naver.com>
2023-10-31 10:37:13 +0900

    feat: 게임 시도 횟수 검증 기능 추가

--- src/main/java/validation/ErrorMessage.java
+++ src/main/java/validation/ErrorMessage.java
@@ -2,9 +2,11 @@ package validation;
 
 public enum ErrorMessage {
     NAME_LENGTH_ERROR("자동차 이름은 1이상 5이하만 가능합니다."),
-    NAME_DUPLICATE_ERROR("자동차 이름은 중복될 수 없습니다.");
+    NAME_DUPLICATE_ERROR("자동차 이름은 중복될 수 없습니다."),
+    ROUND_INPUT_ERROR("정수를 입력해 주세요"),
+    ROUND_LENGTH_ERROR("라운드는 1이상 20이하만 가능합니다");
 
-    private String message;
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
--- src/main/java/validation/InputValidate.java
+++ src/main/java/validation/InputValidate.java
@@ -9,6 +9,12 @@ public class InputValidate {
         validateNameDuplication(names);
     }
 
+    public static void gameRound(String input) {
+        validateInteger(input);
+        int round = Integer.parseInt(input);
+        validateRoundLength(round);
+    }
+
     private static void validateCarNameLength(String name) {
         if(name.isEmpty() || name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH_ERROR.getMessage());
@@ -18,6 +24,20 @@ public class InputValidate {
     private static void validateNameDuplication(List<String> names) {
         if(names.size() != names.stream().distinct().count()) {
             throw new IllegalArgumentException(ErrorMessage.NAME_DUPLICATE_ERROR.getMessage());
+        }
+    }
+
+    private static void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.ROUND_INPUT_ERROR.getMessage());
+        }
+    }
+
+    private static void validateRoundLength(int round) {
+        if(round < 1 || round > 20) {
+            throw new IllegalArgumentException(ErrorMessage.ROUND_LENGTH_ERROR.getMessage());
         }
     }
 }
