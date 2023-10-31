db4d7af8e48b674a0530e81353c8616a6b41b1bf
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-01 06:31:53 +0900

    test: 쉼표로 문자열 구분 테스트

--- src/test/java/racingcar/util/UtilTest.java
+++ src/test/java/racingcar/util/UtilTest.java
@@ -2,6 +2,7 @@ package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class UtilTest {
@@ -10,5 +11,12 @@ class UtilTest {
         String input = "띄 어 쓰 기";
         String result = Util.removeSpaceAll(input);
         assertThat(result).isEqualTo("띄어쓰기");
+    }
+
+    @Test
+    void 쉼표로_문자열_구분_테스트() {
+        String input = "안녕하세요, one, two 2, 띄 어 쓰 기";
+        List<String> results = Util.splitNameByComma(input);
+        assertThat(results).contains("안녕하세요", "one", "two2", "띄어쓰기");
     }
 }
