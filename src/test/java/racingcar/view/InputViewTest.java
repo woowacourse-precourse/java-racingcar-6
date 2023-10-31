8c0a281872759d6ec824bbeb17ceac2011a467c7
uknow-mac <yoon6763@naver.com>
2023-10-31 21:02:11 +0900

    test: 입력 테스트 추가

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,56 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputViewTest extends NsTest {
+
+    @Test
+    public void 입력_테스트() {
+        String input = "pobi,crong,honux";
+        run(input, "1");
+    }
+
+    @Test
+    public void 이름_입력_5자_초과_테스트() {
+        String input = "pobi,crong,honux,java,python";
+
+        try {
+            run(input, "1");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    @Test
+    public void 시도_횟수_음수_테스트() {
+        String input = "pobi,crong,honux";
+        String tryCount = "-1";
+
+        try {
+            run(input, tryCount);
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("시도 횟수는 1 이상만 가능합니다.");
+        }
+    }
+
+    @Test
+    public void 시도_횟수_0회_테스트() {
+        String input = "pobi,crong,honux";
+        String tryCount = "0";
+
+        try {
+            run(input, tryCount);
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("시도 횟수는 1 이상만 가능합니다.");
+        }
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
