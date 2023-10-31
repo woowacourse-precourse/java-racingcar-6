682553070aa8da2b4aa42ae063578316df427fd4
Park Eunhyeong <silbroeh@gmail.com>
2023-11-01 08:47:57 +0900

    feat : CarGroup클래스 - 생성자 수정

--- src/main/java/racingcar/CarGroup.java
+++ src/main/java/racingcar/CarGroup.java
@@ -9,11 +9,8 @@ public class CarGroup {
     private int maxDistance = Integer.MIN_VALUE;
 
 
-    public CarGroup(String[] names) {
-        carList = new ArrayList<>();
-        for (String name : names) {
-            carList.add(new Car(name));
-        }
+    public CarGroup(List<Car> carList) {
+        this.carList = carList;
     }
 
     public List<Car> getCarList() {
