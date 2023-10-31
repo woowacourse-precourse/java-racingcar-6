f65303053a6d3ae46fd82a6aeb6c9b26a24aa7ab
IE-MangChi <hsm983278@gmail.com>
2023-10-31 21:13:22 +0900

    style: 회수 -> 횟수 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -15,7 +15,7 @@ public class InputView {
     }
 
     public String userInputGameCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println("시도할 횟수는 몇회인가요?");
         return InputValidation.gameCountValidate(Console.readLine());
     }
 }
