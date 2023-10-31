495af0eab2389f6de5b2517be661b319a4b7554b
kyungwon Kim <ddwu20190942@gmail.com>
2023-10-31 20:32:47 +0900

    feat: 기본적인 출력문 추가
    추가된 출력문:
    - 자동차 이름 입력문
    - 시도할 회수 입력문
    - 실행 결과 출력문
    - 최종 우승자 출력문
    
    location: Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,13 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        System.out.println("실행 결과");
+
+        System.out.print("최종 우승자 : ");
     }
 }
