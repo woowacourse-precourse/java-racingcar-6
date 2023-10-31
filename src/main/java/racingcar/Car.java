7bb439d90660beba4b12563da119d41108f611c0
zzawang <hobin0116@naver.com>
2023-11-01 03:02:47 +0900

    refactor: 상수를 final로 수정 및 코드 개선
    - name의 불변성을 위해 final로 수정하였다.
    - 결과에 변화는 없지만 직관성을 위해 생성자에 distance가 0으로 초기화되는 기능을 추가하였다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 public class Car {
-    private String name; // 자동차의 이름
+    private final String name; // 자동차의 이름
 
     private int distance; // 자동차가 이동한 거리
 
     public Car(String name) {
         this.name = name;
+        this.distance = 0;
     }
 
     public void moveForward() {
