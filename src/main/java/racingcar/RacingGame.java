650408f99d82d6bf7c2fbf24e3e911e1112d3945
Hyunstone <hyun104467@naver.com>
2023-10-31 16:29:22 +0900

    [Feat] 인게임 초기 세팅 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,16 +1,34 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
     private InputHandler inputHandler = new InputHandler();
-    private static int count;
+    private List<Car> carList = new ArrayList<>();
+    private int count;
     public void play() {
-        inputHandler.inputCarName();
-        inputHandler.inputMatchCount();
+        initializeGame();
+        for (Car car :carList) {
+            
+        }
+    }
+    private void initializeGame() {
+        carList = registCarList(inputHandler.inputCarName());
+        count = inputHandler.inputMatchCount();
     }
 
-    private static void registerCar() {}
+    private List<Car> registCarList(List<String> carNameList) {
+        List<Car> cars = new ArrayList<>();
+        for (String name: carNameList) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
 
-    private static void moveCar() {}
+    private static void moveCar() {
+
+    }
 
     public static void announceWinner() {}
 
