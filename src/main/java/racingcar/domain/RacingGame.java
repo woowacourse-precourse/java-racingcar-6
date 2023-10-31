00d36b344926e5fd75dca6e31249692b2983a182
heogeonho <geonh1016@gmail.com>
2023-10-31 13:22:25 +0900

    refactor: 유지 보수성 향상을 위한 메서드 명칭 수정 및 코드 개선
    View 관련 메서드 명칭 통일성 있게 수정
    Validator 관련 메서드 명칭 생략된 의미 보완 및 수정
    RacingGame 관련 메서드 의미 보완 및 코드 로직 개선

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -8,43 +8,44 @@ import java.util.List;
 
 public class RacingGame {
     public void startGame() {
-        String inputNames = InputView.getCarNames();
-        Validator.name(inputNames);
-        String inputRound = InputView.getRoundNumber();
-        Validator.round(inputRound);
+        String inputNames = InputView.requestCarNames();
+        Validator.validateCarNames(inputNames);
 
-        OutputView.startRepeat();
-        int repeatNumber = Integer.parseInt(inputRound);
+        String inputRound = InputView.requestRoundNumber();
+        Validator.validateRoundValue(inputRound);
+
+        OutputView.printExecutionMessage();
+
         List<Car> carList = generateCarList(inputNames);
-        List<Car> resultList = repeatRound(repeatNumber, carList);
-        OutputView.gameResult(determineWinners(resultList));
+        int repeatNumber = Integer.parseInt(inputRound);
+        List<Car> resultList = repeatRounds(repeatNumber, carList);
+
+        List<String> winners = determineWinners(resultList);
+        OutputView.printWinners(winners);
     }
 
     private static List<Car> generateCarList(String inputNames) {
         String[] inputArray = inputNames.split(",");
         List<Car> carList = new ArrayList<>();
         for (String name : inputArray) {
-            carList.add(generateCar(name));
+            carList.add(new Car(name));
         }
         return carList;
     }
-    private static Car generateCar(String name) {
-        return new Car(name);
-    }
-    private static List<Car> repeatRound(int repeatNumber, List<Car> carList) {
+    private static List<Car> repeatRounds(int repeatNumber, List<Car> carList) {
         for (int i = 0; i < repeatNumber; i++) {
             for (Car car : carList) {
                 car.moveForward();
-                OutputView.positionResult(car);
+                OutputView.printPosition(car);
             }
             System.out.println();
         }
         return carList;
     }
     private static List<String> determineWinners(List<Car> resultList) {
+        int maxPosition = getMaxPosition(resultList);
         List<String> winnerList = new ArrayList<>();
         for (Car car : resultList) {
-            int maxPosition = getMaxPosition(resultList);
             if (maxPosition == car.position) {
                 winnerList.add(car.name);
             }
@@ -54,9 +55,7 @@ public class RacingGame {
     private static int getMaxPosition(List<Car> carList) {
         int maxPosition = 0;
         for (Car car : carList) {
-            if (maxPosition < car.position) {
-                maxPosition = car.position;
-            }
+            maxPosition = Math.max(maxPosition, car.position);
         }
         return maxPosition;
     }
--- src/main/java/racingcar/veiw/OutputView.java
+++ src/main/java/racingcar/veiw/OutputView.java
@@ -7,13 +7,13 @@ import java.util.StringJoiner;
 
 public class OutputView {
 
-    public static void startRepeat() {
+    public static void printExecutionMessage() {
         System.out.println("실행 결과");
     }
-    public static void positionResult(Car car) {
-        System.out.printf("%s : %s%n", car.name, printPosition(car.position));
+    public static void printPosition(Car car) {
+        System.out.printf("%s : %s%n", car.name, createPositionString(car.position));
     }
-    public static void gameResult(List<String> winnerList) {
+    public static void printWinners(List<String> winnerList) {
         System.out.print("최종 우승자 : ");
         StringJoiner winnerPrint = new StringJoiner(", ");
         for (String winner : winnerList) {
@@ -21,7 +21,7 @@ public class OutputView {
         }
         System.out.print(winnerPrint);
     }
-    private static String printPosition(int position) {
+    private static String createPositionString(int position) {
         return "-".repeat(position);
     }
 }
