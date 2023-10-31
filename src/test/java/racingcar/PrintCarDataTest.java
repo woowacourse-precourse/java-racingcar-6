1264714b316bcecee4d9d86f1b371189ae66a854
Jung ui heon <ky7662@naver.com>
2023-10-31 22:14:11 +0900

    test : 우승자가 제대로 출력되는지 확인

--- src/test/java/racingcar/PrintCarDataTest.java
+++ src/test/java/racingcar/PrintCarDataTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+
 public class PrintCarDataTest {
     @Test
     void 이동거리에_대한_출력이_제대로_이루어지는지_확인(){
@@ -10,5 +12,20 @@ public class PrintCarDataTest {
         PrintCarData printCarData = new PrintCarData();
         String printCarDistance = printCarData.printRacingCarDistance(movingDistance);
         Assertions.assertThat(printCarDistance.equals("-----"));
+    }
+
+    @Test
+    void 우승자가_제대로_출력되는지_확인(){
+        int tryNumber = 5;
+        LinkedHashMap<String, Integer> racingCarNames = new LinkedHashMap<>();
+        racingCarNames.put("pobi", 5);
+        racingCarNames.put("woni", 3);
+        racingCarNames.put("jun", 5);
+
+        PrintCarData printCarData = new PrintCarData();
+        String winCarName = printCarData.printWinRacingCar(racingCarNames, tryNumber);
+
+        Assertions.assertThat(winCarName.contains("pobi"));
+        Assertions.assertThat(winCarName.contains("jun"));
     }
 }
