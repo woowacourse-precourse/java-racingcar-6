496a2d938bdded01315418d9c524a1601792079b
Jaeho Kim <fldzm7905@gmail.com>
2023-10-31 21:32:30 +0900

    feat: 자동차 이름 리스트를 받아서 자동차 리스트 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,10 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
+    private int position = 0;
+    private String name = "";
 
-    public List<String> generate(String carName) {
-        List<String> cars = new ArrayList<>();
-        cars = List.of(carName.split(","));
-        return cars;
+
+
+    private Car(String name) {
+        this.name = name;
+    }
+
+    public static List<Car> generate(List<String> cars) {
+        List<Car> player = new ArrayList<>();
+        for (int i = 0; i < cars.size(); i++) {
+            player.add(new Car(cars.get(i)));
+        }
+        return player;
+    }
+
+    private boolean ableMoveForward() {
+
+        return false;
+    }
+
+    private int rollDice() {
+
+        return 0;
     }
 }
