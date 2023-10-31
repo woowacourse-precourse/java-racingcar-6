f88224962f3c6791c98d646c0670a15e7f1280d2
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-31 16:07:39 +0900

    [feat] : add validate logic

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
+import racingcar.validate.TryCountValidate;
+
 public class TryCount {
     private final int tryCount;
 
     public TryCount(String tryCount) {
+        TryCountValidate.validateTryCount(tryCount);
         this.tryCount = Integer.parseInt(tryCount);
     }
 
