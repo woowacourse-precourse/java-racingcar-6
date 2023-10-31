b901d76d7eeb54bb726bf19072f22e435b419c22
songjayhyun <thewinner1st@naver.com>
2023-10-31 19:53:11 +0900

    refactor: exception 상수 메시지 수정
    - name과 value 수정

--- src/main/java/racingcar/RacingCarException.java
+++ src/main/java/racingcar/RacingCarException.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 public class RacingCarException {
-    public static final String EXCEED_LENGTH_MSG = "이름을 5자 이내로 입력해주세요.";
-    public static final String NOT_SEPARATED_BY_COMMAS_MSG = "이름은 쉼표(,)로 구분해주세요.";
-    public static final String NOT_NUMBER_MSG = "숫자만 입력해주세요.";
+    public static final String INVALID_CAR_NAME_LENGTH_MSG = "자동차 이름을 5자 이하로 입력해주세요.";
+    public static final String INVALID_BLANK_NAME_MSG = "이름이 단순 공백이 안되게 입력해주세요.";
+    public static final String INVALID_NUMBER_MSG = "숫자만 입력해주세요.";
+    public static final String INVALID_TRY_COUNT_LENGTH_MSG = "시도 횟수를 입력해주세요.";
 }
