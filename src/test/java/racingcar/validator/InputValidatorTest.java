4defcb9ba786284f1316642df5a9acac2e7abf5d
Leehyoju97 <dlgywn2420@gmail.com>
2023-10-31 19:07:50 +0900

    refactor: 메소드명 수정

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -11,31 +11,31 @@ class InputValidatorTest {
 
     @DisplayName("자동차 이름 5자 초과시 오류")
     @Test
-    void FilterCarNameOutOfRange() {
+    void filterCarNameOutOfRange() {
         assertThrows(IllegalArgumentException.class, () -> InputValidator.filterCarName(List.of("abcdef")));
     }
 
     @DisplayName("자동차 이름 중복 입력시 오류")
     @Test
-    void FilterCarNameDuplicate() {
+    void filterCarNameDuplicate() {
         assertThrows(IllegalArgumentException.class, () -> InputValidator.filterCarName(List.of("pobi", "woni", "jun", "pobi")));
     }
 
     @DisplayName("자동차 이름 입력 안 한 경우 오류")
     @Test
-    void FilterCarNameEmpty() {
+    void filterCarNameEmpty() {
         assertThrows(IllegalArgumentException.class, () -> InputValidator.filterCarName(List.of("")));
     }
 
     @DisplayName("시도 횟수 입력값이 정수가 아닌 경우 오류")
     @Test
-    void FilterNumberOfAttempNotNumeric() {
+    void filterNumberOfAttempNotNumeric() {
         assertThrows(IllegalArgumentException.class, () -> InputValidator.filterNumberOfAttempt("abc"));
     }
 
     @DisplayName("시도 횟수 입력값이 음수일 경우 오류")
     @Test
-    void FilterNumberOfAttemptNegative() {
+    void filterNumberOfAttemptNegative() {
         assertThrows(IllegalArgumentException.class, () -> InputValidator.filterNumberOfAttempt("-1"));
     }
 }
