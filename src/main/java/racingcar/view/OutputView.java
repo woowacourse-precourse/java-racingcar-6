10a5c30c2e12423ee10fd24f0fdbca3d20e57ddd
JongMinCh0i <wonderland314592@gmail.com>
2023-11-01 03:00:44 +0900

    feat(OutputView): 게임의 결과를 저장하는 GameResult 객체를 활용한 결과창 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,10 @@
 package racingcar.view;
 
-import static racingcar.controller.RaceController.gameResult;
 
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Referee;
+import racingcar.util.GameResult;
 
 public class OutputView {
 
@@ -12,9 +12,17 @@ public class OutputView {
     private final static String FINAL_WINNER = "최종 우승자 : ";
 
     public void printGameResult(List<Car> list, Referee referee) {
-        gameResult.append(GAME_RESULT).append('\n');
-        gameResult.append(winnerName(referee.selectWinnerList(list))).append('\n');
-        System.out.println(gameResult);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(GAME_RESULT).append('\n');
+        GameResult instance = GameResult.getInstance();
+        StringBuilder gameResult = instance.getGameResult();
+        sb.append(gameResult);
+
+        sb.append(winnerName(referee.selectWinnerList(list))).append('\n');
+
+        System.out.println(sb);
     }
 
     // 우승자 이름을 반환한다.
@@ -23,6 +31,7 @@ public class OutputView {
         Integer baseScore = sortedCarList.get(0).getDistance();
 
         for (int i = 1; i < sortedCarList.size(); i++) {
+
             if (baseScore.equals(sortedCarList.get(i).getDistance())) {
                 baseName += ", " + sortedCarList.get(i).getName();
             }
