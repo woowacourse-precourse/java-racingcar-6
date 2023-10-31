6f14e9cbde0079bc8efc00f221d6b0973b319d10
gmelon <hsh1769@naver.com>
2023-10-31 22:16:42 +0900

    feat: MainOutputView에 입력을 위한 스크립트 출력 메서드 추가

--- src/main/java/racingcar/view/output/MainOutputView.java
+++ src/main/java/racingcar/view/output/MainOutputView.java
@@ -5,6 +5,18 @@ import java.util.stream.Collectors;
 import racingcar.dto.CarDto;
 
 public class MainOutputView {
+    public static void printNewLine() {
+        System.out.println();
+    }
+
+    public static void printReadCarNamesScript() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printReadPlayCountScript() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
     public static void printWinningCars(List<CarDto> carDtos) {
         System.out.println("최종 우승자 : " + joinedNames(carDtos));
     }
