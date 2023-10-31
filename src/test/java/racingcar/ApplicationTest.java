14353f3ee0295b2890398505ff4d07962de2329e
BOYOUNG-KANG <28287@naver.com>
2023-11-01 03:06:25 +0900

    test: 테스트 코드 위치 변경

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,15 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.message.ValidateErrorMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.internal.bytebuddy.implementation.MethodCall.run;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -23,21 +20,6 @@ class ApplicationTest extends NsTest {
                 assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
             },
             MOVING_FORWARD, STOP
-        );
-    }
-
-    @Test
-    @DisplayName("이동 결과만큼 실행 결과가 출력된다.")
-    void 실행결과_테스트(){
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("runa,crong,boo", "4");
-                    assertThat(output()).contains("runa : ---", "crong : -", "boo : ----", "최종 우승자 : boo");
-                },
-                MOVING_FORWARD, STOP, MOVING_FORWARD,
-                STOP, STOP, MOVING_FORWARD,
-                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
-                MOVING_FORWARD, STOP, MOVING_FORWARD
         );
     }
 
