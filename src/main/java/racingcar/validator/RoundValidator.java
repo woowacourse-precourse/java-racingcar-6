75e437a4036d75384bdc19bd76bcfb8c4394a8e4
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-01 05:20:21 +0900

    feat: 반복 횟수 1 미만 예외 처리 추가
    1미만 입력 시 "1이상의 라운드를 숫자로 입력해주세요" 뜬 뒤 종료

--- src/main/java/racingcar/validator/RoundValidator.java
+++ src/main/java/racingcar/validator/RoundValidator.java
@@ -2,19 +2,27 @@ package racingcar.validator;
 
 public class RoundValidator {
 
-    private final static String ERROR = "[ERROR]";
     private final static String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요.";
+    private final static String NUMBER_WILL_MORE_THAN_ONE = "1이상의 라운드를 숫자로 입력해주세요";
     public static String repeatNum;
 
     public RoundValidator(String input){
         repeatNum = input;
         isNumError();
+        isNumMoreThanOne();
     }
+
     public void isNumError(){
         try {
             Integer.parseInt(repeatNum);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR+NOT_NUMBER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public void isNumMoreThanOne(){
+        if(Integer.parseInt(repeatNum) < 1){
+            throw new IllegalArgumentException(NUMBER_WILL_MORE_THAN_ONE);
         }
     }
 }
