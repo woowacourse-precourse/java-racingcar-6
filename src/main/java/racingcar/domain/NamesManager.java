e78fc3ea27d652f9a707221539b2cafd416ce39b
Jinnie Kim <jinniek48@gmail.com>
2023-10-31 19:08:28 +0900

    feat(racingcar): 자동차 이름이 모두 unique한지 확인하는 기능 구현

--- src/main/java/racingcar/domain/NamesManager.java
+++ src/main/java/racingcar/domain/NamesManager.java
@@ -1,19 +1,25 @@
 package racingcar.domain;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NamesManager {
 
-    public List<String> separateNamesString(String namesString){
+    public List<String> separateNamesString(String namesString) {
         return Arrays.asList(namesString.split(","));
     }
 
-    public boolean checkNamesException(List<String> names){
-        for (String name : names){
-            if (wrongNameLength(name)){
+    public boolean checkNamesException(List<String> names) {
+        for (String name : names) {
+            if (wrongNameLength(name)) {
                 return true;
             }
+        }
+
+        if (duplicateName(names)) {
+            return true;
         }
 
         return false;
@@ -22,9 +28,18 @@ public class NamesManager {
     public boolean wrongNameLength(String name) {
         if (name.length() > 5) {
             return true;
-        } else {
-            return false;
         }
+
+        return false;
+    }
+
+    public boolean duplicateName(List<String> names){
+        Set<String> nameSet = new HashSet<>(names);
+        if (nameSet.size()!=names.size()){
+            return true;
+        }
+
+        return false;
     }
 
 }
