8b7c593326acb4a0bd5f39507a2fec8f1b3532f2
yookyung Choi <ykc131719@gmail.com>
2023-11-01 01:25:22 +0900

    feat: 입력받은 이름 쉼표 기준으로 구분하는 클래스 작성 완료

--- src/main/java/racingcar/util/StringToList.java
+++ src/main/java/racingcar/util/StringToList.java
@@ -1,9 +1,17 @@
 package racingcar.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Model.Car;
+
 public class StringToList {
 
-    public static String[] stringToList(String input){
+    public static List<Car> stringToList(String input){
         String[] names = input.split(",");
-        return names;
+        List<Car> carList = new ArrayList<>();
+        for(int i=0; i<names.length;i++){
+            carList.add(new Car(names[i]));
+        }
+        return carList;
     }
 }
--- src/test/java/racingcar/View/inputViewTest.java
+++ src/test/java/racingcar/View/inputViewTest.java
@@ -1,10 +1,12 @@
 package racingcar.View;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static racingcar.util.StringToList.stringToList;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
+import racingcar.Model.Car;
 
 public class inputViewTest {
 
@@ -12,9 +14,12 @@ public class inputViewTest {
     void split_메서드로_주어진_값을_구분() {
         String input = "1,2";
 
-        String[] result = stringToList(input);
-        assertThat(result).contains("2", "1");
-        assertThat(result).containsExactly("1", "2");
+        List<Car> ans = new ArrayList<>();
+        ans.add(new Car("1"));
+        ans.add(new Car("2"));
+
+        List<Car> result = stringToList(input);
+        assertThat(result).hasSameClassAs(ans);
     }
 
 }
