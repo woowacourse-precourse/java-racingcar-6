61564835ccfbb21f54bf85ab5c09e12d3ae08a1a
transfer_kk <kseysh123@naver.com>
2023-10-31 21:42:20 +0900

    feat : Car class 구현

--- src/main/java/Car/Car.java
+++ src/main/java/Car/Car.java
@@ -1,10 +1,39 @@
+package Car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
+
+    public int getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     private int currentLocation;
 
-    Car(String name){
+    Car(String name) {
         this.name = name;
         this.currentLocation = 0;
+    }
+
+    public void moveForward() {
+        int randomNumber = Randoms.pickNumberInRange(0, 10);
+
+        if (randomNumber >= 4) {
+            currentLocation++;
+        }
+    }
+
+    public void printCurrentLocation() {
+        System.out.print(this.name + " : ");
+        for (int i = 0; i < this.currentLocation; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
 }
