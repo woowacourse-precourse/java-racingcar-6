ef9fe77af297d7090929cc7e601639b57aaf122c
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:07:17 +0900

    Feat: 입력값 유효성을 검사하는 기능 추가
    - 자동차 이름을 입력받을 때 공백이 있는지 검사
    - 실행횟수를 입력받을 때 올바른 숫자인지 검사

--- src/main/java/racingcar/service/ValidateService.java
+++ src/main/java/racingcar/service/ValidateService.java
@@ -1,5 +1,8 @@
 package racingcar.service;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class ValidateService {
     public ValidateService(){}
 
@@ -7,10 +10,23 @@ public class ValidateService {
         if(carName.isEmpty())throw new IllegalArgumentException();
     }
 
+    public static void hasBlank(final String carName){
+        if(carName.contains(" "))throw new IllegalArgumentException();
+    }
+
 
     public static void isValidLength(final String carName){ //자동차 이름이 1이상 5이하가 아닐 경우
         int carNameLength = carName.length();
         if(carNameLength <= 0 || carNameLength > 5)
+            throw new IllegalArgumentException();
+    }
+
+    public static void isDuplicateName(List<Car> parkingLot, final String carName){
+
+    }
+
+    public static void isNumber(final String count){ //숫자 아닌 경우 예외 발생
+        if(count.matches("^[1-9]\\d*$") == false)
             throw new IllegalArgumentException();
     }
 }
