3c6c827bcdfce7e23f067327d159f6ae6a865394
JuSeong1130 <jsmoon1130@naver.com>
2023-10-31 23:06:46 +0900

    refactor(Application) : GameController 생성자 바뀜으로 인한 코드 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,10 @@
 package racingcar;
 
 import racingcar.controller.GameController;
-import racingcar.domain.game.RacingGame;
-import racingcar.domain.generator.RandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController(new RacingGame(new RandomNumberGenerator()));
+        GameController gameController = new GameController();
         gameController.run();
     }
 }
