58523194f63095f49fd19335e30845308605b135
seona-moon <fhflwhwl5@gmail.com>
2023-10-31 10:34:53 +0900

    feat: 출력 메시지를 Enum으로 관리

--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -1,2 +1,19 @@
-package racingcar.view;public enum OutputMessage {
+package racingcar.view;
+
+public enum OutputMessage {
+  CAR_NAME_PROMPT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+  GAME_COUNT_PROMPT("시도할 회수는 몇회인가요?"),
+  RESULT_HEADER("실행 결과"),
+  WINNER_PREFIX("최종 우승자 : ");
+
+  private final String message;
+
+  OutputMessage(String message) {
+    this.message = message;
+  }
+
+
+  public String getMessage() {
+    return message;
+  }
 }
