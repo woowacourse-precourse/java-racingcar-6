e205efe01bc98a56066327b1ff16b8db2df31d8b
siho-song <thdtlgh234@naver.com>
2023-11-01 02:03:15 +0900

    [Refactor] 구분자 상수화

--- src/main/java/racingcar/GameInput.java
+++ src/main/java/racingcar/GameInput.java
@@ -8,6 +8,7 @@ public class GameInput {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String MOVING_NUMBER_PATTERN = "^[1-9][0-9]*$";
+    private static final String DELIMITER = ",";
 
 
     public String readInput() {
@@ -24,7 +25,7 @@ public class GameInput {
     }
 
     public List<String> convertToCarNameList(String input) {
-        List<String> names = splitInput(input, ",");
+        List<String> names = splitInput(input, DELIMITER);
         validateCarName(names);
         return names;
     }
@@ -38,7 +39,7 @@ public class GameInput {
     public void validateCarName(List<String> names) {
         for (String name : names) {
             if (name.length() > MAX_CAR_NAME_LENGTH) {
-                throw new IllegalArgumentException("Carname length must be <= " + MAX_CAR_NAME_LENGTH);
+                throw new IllegalArgumentException("Car name length must be <= " + MAX_CAR_NAME_LENGTH);
             }
         }
     }
