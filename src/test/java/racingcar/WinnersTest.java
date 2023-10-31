21800f403c4da104330f51facab1a184382e7128
dbstn1325 <dbstn1325@naver.com>
2023-10-31 11:43:14 +0900

    test(WinnersTest): 전진도가 가장 큰 자동차를 공동 우승자로 판단하는 기능 테스트

--- src/test/java/racingcar/WinnersTest.java
+++ src/test/java/racingcar/WinnersTest.java
@@ -28,4 +28,26 @@ public class WinnersTest {
         //then
         Assertions.assertThat(winners.winners().size()).isEqualTo(1);
     }
+
+    @Test
+    void 전진도가_가장_큰_자동차를_공동_우승자로_판단하는_기능_테스트() {
+        //given
+        String userInput = "yoon,su";
+        String[] carNames = userInput.split(",");
+        Cars cars = new Cars(carNames);
+        List<Car> getCars = cars.getCars();
+
+        Car yoonCar = getCars.get(0);
+        yoonCar.moveAhead();
+        yoonCar.moveAhead();
+        Car suCar = getCars.get(1);
+        suCar.moveAhead();
+        suCar.moveAhead();
+
+        //when
+        Winners winners = cars.generateWinners();
+
+        //then
+        Assertions.assertThat(winners.winners().size()).isEqualTo(2);
+    }
 }
