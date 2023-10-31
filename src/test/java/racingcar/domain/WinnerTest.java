fae9b5ddc1cb891b9a35833d1475f6c88b72f745
JINU-CHANG <jenny010925@naver.com>
2023-10-31 11:57:21 +0900

    Test : 우승자 구하는 테스트

--- src/test/java/racingcar/domain/WinnerTest.java
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -1,2 +1,35 @@
-package racingcar.domain;public class WinnerTest {
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.util.RacingCarUtilsForTest;
+
+public class WinnerTest {
+	@Test
+	void 우승자가_1명일때_우승자를_구한다() {
+		List<Car> carList = List.of(
+			new Car("pobi"), new Car("woni"), new Car("jun")
+		);
+		carList = RacingCarUtilsForTest.getRacingCar(carList, List.of(3, 3, 5));
+		Winner winner = new Winner();
+		List<String> winnerList = winner.get(carList);
+
+		assertThat(winnerList).hasSize(1)
+			.containsOnly("jun");
+	}
+
+	@Test
+	void 우승자가_2명일때_우승자를_구한다() {
+		List<Car> carList = List.of(
+			new Car("pobi"), new Car("woni"), new Car("jun")
+		);
+		carList = RacingCarUtilsForTest.getRacingCar(carList, List.of(3, 5, 5));
+		Winner winner = new Winner();
+		List<String> winnerList = winner.get(carList);
+
+		assertThat(winnerList).hasSize(2)
+			.contains("jun", "woni");
+	}
 }
