46be7241c4d3650b3483631457e14b5be06648fa
MingyeomKim <mingyum119@naver.com>
2023-10-31 10:51:29 +0900

    feat(Cars): 2대 이상의 자동차가 입력되었는 지 검증
    Cars 일급 클렉션 객채가 생성될 때 자동차의 개수가 최솟값을 만족하는 지 검증한다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,9 +1,11 @@
 package racingcar.domain;
 
+import static racingcar.global.constants.NumberType.MIN_CAR_SIZE;
 import static racingcar.global.constants.SymbolType.DELIMITER;
 
 import java.util.Arrays;
 import java.util.List;
+import racingcar.global.constants.NumberType;
 
 public class Cars {
     private List<Car> carList;
@@ -14,6 +16,22 @@ public class Cars {
 
     public static Cars of(String names) {
         return new Cars(parseStringToCarList(names));
+    }
+
+    private void validateCarList(List<Car> carList) {
+        validateListSize(carList);
+    }
+
+    private void validateListSize(List<Car> carList) {
+        if (hasSingleCar(carList)) {
+            throw new IllegalArgumentException(
+                    String.format("%d대 이상의 자동차의 이름을 입력해주세요.", MIN_CAR_SIZE.getValue())
+            );
+        }
+    }
+
+    private boolean hasSingleCar(List<Car> carList) {
+        return carList.size() < MIN_CAR_SIZE.getValue();
     }
 
     private static List<Car> parseStringToCarList(String names) {
