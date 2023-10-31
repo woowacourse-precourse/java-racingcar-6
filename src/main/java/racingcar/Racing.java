1aad28a2d2ab5dced628f3737b99c1eb9feb90fb
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 10:45:59 +0900

    [Fix](Racing) - 매직 넘버 상수 변경

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,23 +2,25 @@ package racingcar;
 
 import java.util.ArrayList;
 
-public class CarRacingGame {
+public class Racing {
 
-    private static String[] racerNameArray;
+    private static final int RACE_CONDITION = 3;
+    private static String[] raceCarNameArray;
     private int round;
-    ArrayList<Racer> userArray = new ArrayList<>();
-    CarRacingGame(String[] racerNameArray, int round) {
-        this.racerNameArray = racerNameArray;
+    private ArrayList<Car> carArray = new ArrayList<>();
+
+    Racing(String[] raceCarNameArray, int round) {
+        this.raceCarNameArray = raceCarNameArray;
         this.round = round;
+        for (int i = 0; i < raceCarNameArray.length; i++) {
+            Car car = new Car(raceCarNameArray[i]);
+            carArray.add(car);
+        }
     }
 
     public void CarRacingGameStart() {
-        for (int i = 0; i < racerNameArray.length; i++) {
-            Racer racer = new Racer(racerNameArray[i]);
-            userArray.add(racer);
-        }
-        raceStart(racerNameArray, round);
-        Result result = new Result(userArray);
+        raceStart(raceCarNameArray, round);
+        Result result = new Result(carArray);
         result.finalWinner();
     }
 
@@ -38,11 +40,11 @@ public class CarRacingGame {
     private void resultPrint(int i) {
         Dice dice = new Dice();
         int randomNumber = dice.getDice();
-        if (randomNumber > 3) {
-            userArray.get(i).setDistance("-");
-            userArray.get(i).setCount();
+        if (randomNumber > RACE_CONDITION) {
+            carArray.get(i).setDistance("-");
+            carArray.get(i).setCount();
         }
-        System.out.println(userArray.get(i).getName() + " : " + userArray.get(i).getDistance());
+        System.out.println(carArray.get(i).getName() + " : " + carArray.get(i).getDistance());
     }
 
 }
