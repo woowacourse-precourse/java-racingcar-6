956d0d6592905df0ca2bcdded7fbdf50f0dfb7d6
K-KY <kyuyoungk@naver.com>
2023-11-01 06:09:55 +0900

    Refactor(SystemOutput) : 상수화

--- src/main/java/racingcar/SystemOutput.java
+++ src/main/java/racingcar/SystemOutput.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 public class SystemOutput {
 
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRIAL_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String THIS_IS_RESULT = "\n실행 결과";
+    private static final String WINNER = "최종 우승자 : ";
+
     public static void printInputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAMES);
     }
 
     public static void printInputTrialCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(INPUT_TRIAL_COUNT);
     }
 
     public static void printThisIsResult() {
-        System.out.println("\n실행 결과");
+        System.out.println(THIS_IS_RESULT);
     }
 
 
@@ -23,6 +28,6 @@ public class SystemOutput {
     }
 
     public static void printWinner(List<String> winner) {
-        System.out.println("최종 우승자 : " + String.join(", ", winner));
+        System.out.println(WINNER + String.join(", ", winner));
     }
 }
