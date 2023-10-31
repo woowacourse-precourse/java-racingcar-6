c908f5b8328ea806f0329f2d83ddc744558862c2
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:18:30 +0900

    feat(NumberConstant): NumberConstant 상수 클래스 구현

---
+++ src/main/java/racingcar/constant/NumberConstant.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class NumberConstant {
+    public static final int MIN_RANDOM_BOUND = 0;
+    public static final int MAX_RANDOM_BOUND = 9;
+    public static final int MOVING_POSITION = 4;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+}
