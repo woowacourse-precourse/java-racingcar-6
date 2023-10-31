4f13aff8ae9cfd7d07355f03db6ae65896b8eb69
Hyun Woo <hwsa10041@gmail.com>
2023-10-31 16:07:32 +0900

    test : playRacingCar() 테스트 작성

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
@@ -16,7 +19,7 @@ class RacingServiceTest {
     @Test
     void createCarList() {
         // given
-        String[] names = new String[]{"pobi","woni","jun"};
+        String[] names = new String[]{"pobi", "woni", "jun"};
 
         // when
         List<Car> carList = racingService.createCarList(names);
@@ -29,5 +32,27 @@ class RacingServiceTest {
                         tuple("woni", 0),
                         tuple("jun", 0)
                 );
+    }
+
+    @DisplayName("각각의 자동차가 랜덤한 수로 움직인다.")
+    @Test
+    void playRacingCar() {
+        // given
+        Car pobi = Car.createCar("pobi");
+        Car woni = Car.createCar("woni");
+        Car jun = Car.createCar("jun");
+        List<Car> cars = List.of(pobi, woni, jun);
+
+        // when
+        Map<String, Integer> result = racingService.playRacingCar(cars);
+
+        // then
+        result.values().forEach(score ->
+                assertThat(score).isBetween(0, 9)
+        );
+
+        int maxScore = Collections.max(result.values());
+        cars.stream().filter(car -> result.get(car.getName()) == maxScore)
+                .forEach(car -> assertThat(car.getWinCount()).isEqualTo(1));
     }
 }
