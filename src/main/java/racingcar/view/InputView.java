a6061c748fa7822a8cf700fceaf89190075c0db2
readable-ko <21700026@handong.edu>
2023-10-31 22:25:17 +0900

    refactor(InputView): updated method
    구현된 메소드
    - checkTurnOver 함수구현
    
    Before:
    단순 값 받아오기
    After:
    값의 비교 결과값을 받아오기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,17 +6,26 @@ import racingcar.error.Error;
 import java.util.List;
 
 public class InputView {
+    private int turns = Integer.MIN_VALUE;
+    private static final int ZERO = 0;
+
+    public void getInputTurns() {
+        OutputView.printTurnHelp();
+        String turn = Console.readLine().strip();
+        Error.checkIsNumber(turn);
+
+        turns = Integer.parseInt(turn);
+    }
+
     public List<String> getInputNameList() {
+        OutputView.printNameHelp();
         List<String> nameList = Arrays.asList(Console.readLine().replace(" ", "").split(","));
         Error.checkIllegalString(nameList);
 
         return nameList;
     }
 
-    public int getGameTurns() {
-        String turn = Console.readLine().strip();
-        Error.checkIsNumber(turn);
-
-        return Integer.parseInt(turn);
+    public boolean checkTurnOver() {
+        return (turns--) != ZERO;
     }
 }
