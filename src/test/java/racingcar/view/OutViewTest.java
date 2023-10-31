adea71c847a4e5e2ba80857123552d3535b58c70
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-01 03:04:45 +0900

    test(OutputViewTest): printExecutionResultMessage 함수 테스트 코드 추가

--- src/test/java/racingcar/view/OutViewTest.java
+++ src/test/java/racingcar/view/OutViewTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 public class OutViewTest extends NsTest {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
     @Test
     @DisplayName("printNameInputMessage 함수 기능 테스트")
@@ -22,6 +23,14 @@ public class OutViewTest extends NsTest {
     void 시도_횟수_입력_메시지_출력_기능_테스트() {
         OutputView.printRoundInputMessage();
         assertThat(output()).contains(ROUND_INPUT_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("printExecutionResultMessage 함수 기능 테스트")
+    void 실행_결과_메시지_출력_기능_테스트() {
+        OutputView.printExecutionResultMessage();
+        assertThat(output()).contains(
+                EXECUTION_RESULT_MESSAGE);
     }
 
     @Override
