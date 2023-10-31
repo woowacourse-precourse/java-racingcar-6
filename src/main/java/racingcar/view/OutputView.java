d41b5b14f0e2a80848d9ea7e5da949f0c77dc606
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-31 17:11:01 +0900

    feat : 우승자 출력 기능 완료

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String FORWARD_SYMBOL = "-";
     private static final String GAME_RESULT = "실행결과";
+    private static final String MVP = "최종 우승자 : ";
+    private static final String Comma = " ,";
 
     public void printForward(String carName, int forward){
         String forwardSymbol = FORWARD_SYMBOL.repeat(forward);
@@ -11,5 +15,9 @@ public class OutputView {
     public void printResult(){
         System.out.println("");
         System.out.println(GAME_RESULT);
+    }
+
+    public void printMVP(List<String> mvpList) {
+        System.out.println(MVP + String.join(Comma, mvpList));
     }
 }
