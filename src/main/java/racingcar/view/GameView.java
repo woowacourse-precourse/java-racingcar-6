f181bd3357c74ee93035c36601ce5c1f42a122f9
mseo39 <a01063294905@gmail.com>
2023-11-01 04:50:05 +0900

    Feat: 실행결과 문구 출력하는 기능

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -22,4 +22,8 @@ public class GameView {
     public static void printTryNumPhrase() {
         System.out.println(TRY_NUM_PHRASE);
     }
+
+    public static void printResultPhrase() {
+        System.out.println(RESULT_PHRASE);
+    }
 }
