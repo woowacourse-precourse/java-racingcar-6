6c4b382ef34c498cf5704a98cdbdd0e24558949b
ldw <leehan02@hanyang.ac.kr>
2023-11-01 04:11:03 +0900

    fix(racingcar): 경주 종료 후 최대 전진 횟수 계산 하는 로직을 기존의 forEach에서 stream 사용으로 변경

--- src/main/java/racingcar/Calculator.java
+++ src/main/java/racingcar/Calculator.java
@@ -3,13 +3,12 @@ package racingcar;
 import java.util.List;
 
 public class Calculator {
+    private static final int THERE_IS_NO_MAX = -1;
 
-    public static int calculateMax(List<Integer> totalMoveList) {
-        int max = totalMoveList.stream()
-                .mapToInt(Integer::intValue)
-                .max()
-                .orElse(-1);
-
-        return max;
+    public static int MaxMove(List<Car> cars) {
+        int max = cars.stream()
+                .map(Car::getTotalMoveLen)
+                .max(Integer::compareTo)
+                .orElse(THERE_IS_NO_MAX);
     }
 }
