10ca0cbf3ba13d9b382be53aa5e207958fdeb730
K-KY <kyuyoungk@naver.com>
2023-10-31 23:17:14 +0900

    Feat(SystemOutput) : 출력 할 메세지

---
+++ src/main/java/racingcar/SystemOutput.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import java.util.List;
+
+public class SystemOutput {
+
+    public static void printCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printTrialCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printThisIsResult() {
+        System.out.println("실행 결과");
+    }
+
+
+    public static void printResult(List<Car> cars) {
+        cars.forEach(car -> System.out.println(car.indicate()));
+        System.out.println();
+    }
+}
