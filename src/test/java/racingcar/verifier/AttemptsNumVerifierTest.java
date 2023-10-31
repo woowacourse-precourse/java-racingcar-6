ccdea6fa8da89af8f44e24fe1a2f609280b08d9a
Hoo-sung <gntjd135@gmail.com>
2023-10-31 10:54:20 +0900

    refactor(AttemptsNumVerifierTest): 정상 입력과, 예외 입력에 따른 테스트 그룹화

--- src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
+++ src/test/java/racingcar/verifier/AttemptsNumVerifierTest.java
@@ -3,6 +3,7 @@ package racingcar.verifier;
 
 import camp.nextstep.edu.missionutils.test.Assertions;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 import racingcar.system.ExceptionMessage;
@@ -10,30 +11,37 @@ import racingcar.system.ExceptionMessage;
 import static org.assertj.core.api.Assertions.*;
 
 class AttemptsNumVerifierTest extends NsTest {
-    @Test
-    void 정상입력이_주어진경우() {
-        Assertions.assertSimpleTest(()->{
-            assertThatCode(() -> run("pobi,woni,jun","23"))
-                    .doesNotThrowAnyException();
-        });
+
+    @Nested
+    class ValidInputTest {
+        @Test
+        void 정상입력이_주어진경우() {
+            Assertions.assertSimpleTest(() -> {
+                assertThatCode(() -> run("pobi,woni,jun", "23"))
+                        .doesNotThrowAnyException();
+            });
+        }
     }
 
-    @Test
-    void 숫자가아닌입력이_주어진경우() {
-        Assertions.assertSimpleTest(()->
-                assertThatThrownBy(() -> run("pobi,woni,jun","35k"))
+    @Nested
+    class ExceptionTest {
+        @Test
+        void 숫자가아닌입력이_주어진경우() {
+            Assertions.assertSimpleTest(() ->
+                    assertThatThrownBy(() -> run("pobi,woni,jun", "35k"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining(ExceptionMessage.NOT_NUMERIC)
+            );
+        }
+
+        @Test
+        void 유효한범위의_숫자가_아닌_입력이_아닌경우() {
+            Assertions.assertSimpleTest(() -> {
+                assertThatThrownBy(() -> run("pobi,woni,jun", "-1"))
                         .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(ExceptionMessage.NOT_NUMERIC)
-                );
-    }
-
-    @Test
-    void 유효한범위의_숫자가_아닌_입력이_아닌경우() {
-        Assertions.assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("pobi,woni,jun","-1"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ExceptionMessage.INVALID_NUM_OF_ATTEMPTS);
-        });
+                        .hasMessageContaining(ExceptionMessage.INVALID_NUM_OF_ATTEMPTS);
+            });
+        }
     }
 
     @Override
