def504c9594886ba158b19f32d8fe43b074c4e3b
ddohyeong <dohyeong7026@gmail.com>
2023-10-31 15:59:24 +0900

    refactor(OutputView) : 문자열을 가독성을 위해 상수로 리팩토링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,16 +1,17 @@
 package view;
 
-import java.util.List;
-
 import domain.Car;
 import domain.Winners;
 
 public class OutputView {
+	private final String COLON = " : ";
+	private final String COMMA = ", ";
+
 	public void printRacingCarStatus(Car car) {
-		System.out.println(car.getName() + " : " + car.createHyphenByLocation());
+		System.out.println(car.getName() + COLON + car.createHyphenByLocation());
 	}
 
 	public void printAwardWinner(Winners winners) {
-		System.out.println(String.join(", ", winners.getCarNames()));
+		System.out.println(String.join(COMMA, winners.getCarNames()));
 	}
 }
