2fe0fd48fa04901159f503cd07b78ba3bc4381df
taewan625 <taewan1997625@gmail.com>
2023-11-01 01:36:46 +0900

    !BREAKING CHANGE: "Fix Application"
    mvc 패턴에 맞체 프로젝트 구현 후 동작만 수행하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,25 +1,10 @@
 package racingcar;
 
-import java.util.Map;
+import racingcar.controller.RacingCarController;
 
 public class Application {
     public static void main(String[] args) {
-        Print print = new Print();
-        Input input = new Input();
-        RacingGame racingGame = new RacingGame();
-
-        print.carName();
-        Map<String, Integer> carsData = input.carNames(); // todo tdd
-        print.tryCount();
-        int tryCount = input.tryCount(); // todo tdd
-
-        print.executeInit();
-        for (int i = 0; i < tryCount; i++) {
-            // todo 게임 수행
-            racingGame.executeCondition(carsData);
-            print.executeProgress(carsData);
-        }
-        int maxValue = racingGame.maxValue(tryCount, carsData);
-        print.result(maxValue, carsData);
+        RacingCarController controller = new RacingCarController();
+        controller.run();
     }
 }
