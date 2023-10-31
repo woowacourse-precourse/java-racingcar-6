0932a1a86234a133889dac78932e76cd8de4adc7
Woongbae Yun <devbelly@naver.com>
2023-10-31 13:52:58 +0900

    feat : 게임에 필요한 정보를 초기화 하는 기능 구현

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -4,11 +4,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import racingcar.view.InputView;
 
 public class Game {
 
     private Cars cars;
     private TryCount tryCount;
+
+    public void initialize() {
+        List<String> carNames = InputView.inputCarNames();
+        cars = Cars.createdByName(carNames);
+        tryCount = new TryCount(InputView.inputTryCount());
+    }
 
     public void play() {
         for (int i = 0; i < tryCount.getTryCount(); ++i) {
