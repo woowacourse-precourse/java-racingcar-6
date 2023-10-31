636427d478f3d8e69fc48f9ee4071906f99a7ae5
“mpfo0106” <mpfo0106@gmail.com>
2023-11-01 01:40:55 +0900

    feat(racingcar): GameController 골격 및 일부 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,6 @@ public class Application {
 
     public static void main(String[] args) {
         gameController = new GameController();
-
+        gameController.startGame();
     }
 }
--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,54 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.NumberGenerator;
+import racingcar.model.Race;
+import racingcar.utils.UserInputManager;
+import racingcar.view.RaceView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameController {
+
+    private List<Car> cars;
+    private Race race;
+    private RaceView raceView;
+    private final NumberGenerator numberGenerator;
+    private final Judgement judgement;
+    private final UserInputManager userInputManager;
+
+    List<String> carNames;
+    List<Integer> randomNumbers;
+    List<Integer> carBoost;
+    int attempt;
+
+
+    public GameController() {   //생성자
+        this.userInputManager = new UserInputManager();
+        this.numberGenerator = new NumberGenerator();
+        this.judgement = new Judgement();
+    }
+
+    private void initializeGame() {
+        carNames = userInputManager.getCarNames();
+        cars = carNames.stream().map(Car::new).collect(Collectors.toList());
+        attempt = userInputManager.getAttempts();
+    }
+
+    public void startGame() {
+        initializeGame();
+        conductRaces();
+        determineWinner();
+    }
+
+    public void conductRaces() {
+
+    }
+
+    public void determineWinner() {
+
+    }
 
 
 }
