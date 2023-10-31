7a9ab719405762222f470445a155f11eb3dd218a
2018009107 <2018009107@hanyang.ac.kr>
2023-10-31 23:51:46 +0900

    feat: 경주 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,6 @@
 package racingcar;
 
 import java.util.List;
-
-import org.mockito.internal.matchers.Or;
-
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
@@ -14,33 +11,31 @@ public class Application {
         String Name = "";
         int Location = 0;
 
-        public void Move(int moveDistance) {
-            Location = Location + moveDistance;
+        public void move(int moveDistance) {
+            this.Location = this.Location + moveDistance;
         }
     }
 
     public static Car[] isCarNameValid(String inp) {
-        Car[] cars;
-
         // 입력에 컴마가 없는 경우
-        if (inp.contains(",")) {
+        if (!inp.contains(",")) {
             throw new IllegalArgumentException();
-        } else {
-            String CarName;
-            String[] ArraysInp = inp.split(",");
-            cars = new Car[ArraysInp.length];
+        }
 
-            for (int i = 0; i < ArraysInp.length; i++) {
-                CarName = ArraysInp[i];
+        String CarName;
+        String[] ArraysInp = inp.split(",");
+        Car[] cars = new Car[ArraysInp.length];
 
-                // 입력이 빈칸인 경우
-                if ((CarName.length() > 5) || (CarName == "" )) {
-                    throw new IllegalArgumentException();
-                }
+        for (int i = 0; i < ArraysInp.length; i++) {
+            CarName = ArraysInp[i];
 
-                // 입력이 유효하면 cars 클래스에 자동차 이름 입력
-                cars[i].Name = CarName;
+            // 입력이 빈칸인 경우
+            if ((CarName.length() > 5) || (CarName == "" )) {
+                throw new IllegalArgumentException();
             }
+
+            // 입력이 유효하면 cars 클래스에 자동차 이름 입력
+            cars[i].Name = CarName;
         }
 
         return cars;
@@ -65,10 +60,40 @@ public class Application {
         return racingCount;
     }
 
+    public static int getRandomNumber(){
+        int randomNumber;
+        do {
+            randomNumber = Randoms.pickNumberInRange(0, 9);
+        } while (randomNumber < 4);
+            
+        return randomNumber;
+    }
+
+    public static void printCar(Car car) {
+        System.out.printf("%s : ", car.Name);
+        for (int i = 0; i < car.Location; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public static Car[] racing(Car[] cars, int racingCount) {
+        while (racingCount > 0) {
+            for (int i = 0; i < cars.length; i++) {
+                cars[i].move(getRandomNumber());
+                printCar(cars[i]);
+            }
+            System.out.println();
+            
+            racingCount = racingCount - 1;
+        }
+
+        return cars;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String inp;
-        
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         inp = camp.nextstep.edu.missionutils.Console.readLine();
         Car[] cars = isCarNameValid(inp);
@@ -79,5 +104,6 @@ public class Application {
 
         System.out.println("\n실행 결과");
         
+        cars = racing(cars, racingCount);
     }
 }
