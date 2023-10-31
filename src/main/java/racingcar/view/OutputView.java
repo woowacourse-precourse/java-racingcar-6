446a5fac77a3d06c480cbbc6c0a1c3c206aa3d16
hunko <rhwlgns4386@naver.com>
2023-10-31 14:25:26 +0900

    refactor : 중복을 상수로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,8 +6,10 @@ import racingcar.dto.StatusResponse;
 
 public class OutputView {
 
-    private static final String PLAY_RESULT_MESSAGE = "실행 결과";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자";
     private static final String HYPHEN = "-";
+    private static final String PLAY_RESULT_MESSAGE = "실행 결과";
+    public static final String PATTERN = "%s : %s";
 
     public static void printPlayResultMessage() {
         print(PLAY_RESULT_MESSAGE);
@@ -16,13 +18,17 @@ public class OutputView {
     public static void printStatus(StatusResponse response) {
         LinkedHashMap<String, Integer> status = response.getStatus();
         for (String name : status.keySet()) {
-            print(String.format("%s : %s", name, HYPHEN.repeat(status.get(name))));
+            print(String.format(PATTERN, name, HYPHEN.repeat(status.get(name))));
         }
-        print("");
+        lineBreak();
     }
 
     public static void printWinner(List<String> winner) {
-        print(String.format("%s : %s", "최종 우승자", String.join(",", winner)));
+        print(String.format(PATTERN, FINAL_WINNER_MESSAGE, String.join(",", winner)));
+    }
+
+    private static void lineBreak() {
+        print("");
     }
 
     private static void print(String message) {
