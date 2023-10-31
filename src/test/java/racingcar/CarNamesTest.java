380f11abac1d17080fcfa9be6c6af337e535e3fb
jun <ggprgrkjh@naver.com>
2023-10-31 22:18:35 +0900

    test(carnames): 자동차 이름 길이 검증 테스트

--- src/test/java/racingcar/CarNamesTest.java
+++ src/test/java/racingcar/CarNamesTest.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.model.CarNames;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
 
 public class CarNamesTest {
 
@@ -16,5 +19,12 @@ public class CarNamesTest {
         assertThat(carNames.getNames()).containsExactly("pobi", "jun", "king");
     }
 
+    @Test
+    void 자동차_이름_길이_검증_테스트() {
+        List<String> testCarNames = List.of("overlengthname", "jun", "king");
+        assertThatThrownBy(() -> new CarNames(testCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 5자리를 초과한 자동차 이름이 입력되었습니다.");
+    }
 
 }
