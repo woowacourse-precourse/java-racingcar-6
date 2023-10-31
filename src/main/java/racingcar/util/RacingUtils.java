9e834c4e8aed7bac6633ee8e00cd6d51637587af
ChoiseU <csw980928@gmail.com>
2023-11-01 00:32:34 +0900

    refactor : 메시지 출력하는 메서드 출력 및 받도록 수정, 길이 체크하는 메서드 수정 및 문자열 잘라서 리스트로 만드는 메서드 수정

--- src/main/java/racingcar/util/RacingUtils.java
+++ src/main/java/racingcar/util/RacingUtils.java
@@ -1,20 +1,30 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RacingUtils {
 
-    public void showEnterMessage(String message) {
+    final String NUM_REG = "[0-9]+";
+
+    public String getInputAnswerAndShowMessage(String message) {
         System.out.println(message);
+        return Console.readLine();
     }
 
-    public boolean isRacingCarNameLength(String[] racingCars, int length) {
-        for(String racingCar : racingCars) {
-            if(racingCar.length() > length) {
-                return false;
+    public List<String> splitStringToList(String inputStr) {
+        return Arrays.stream(inputStr.split(",")).toList();
+    }
+
+    public boolean isCheckStringLength(List<String> strList, int length) {
+        for(String str : strList) {
+            if(str.length() > length) {
+                return true;
             }
         }
-
-        return true;
+        return false;
     }
 }
