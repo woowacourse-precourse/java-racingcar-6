1274bd6e6c05b2811429d8b58a826916981ac2a1
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-11-01 00:47:35 +0900

    feat: 실행 결과를 출력하는 기능 추가

---
+++ src/main/java/racingcar/ui/OutputView.java
@@ -0,0 +1,23 @@
+package racingcar.ui;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void outputStep(List<String> moveResultList) {
+        System.out.println(RESULT_MESSAGE);
+
+        moveResultList.stream().forEach(result -> {
+            System.out.println(result);
+            System.out.println();
+        });
+    }
+
+    public void outputWinner(String winners) {
+        System.out.printf(WINNER_MESSAGE);
+        System.out.println(winners);
+    }
+}
