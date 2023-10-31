f0afd800e11e877f33f05ed4b36f028829b14171
flame-possible <werq2003@naver.com>
2023-11-01 00:27:35 +0900

    feat : 모든 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Comp;
+import racingcar.domain.Input;
+import racingcar.domain.Output;
+import racingcar.domain.Race;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = new ArrayList<>();
+        List<Car> winner = new ArrayList<>();
+        int gamenum = 0;
+
+        Race race = new Race();
+        Comp comp = new Comp();
+        Output output = new Output();
+        Input input = new Input();
+
+        output.InputCarDes();
+        input.InputCarName(cars);
+        output.InputGameNumDes();
+        gamenum = input.InputGameNum();
+
+        race.Racing(cars, gamenum);
+
+        comp.CompCars(cars, winner);
+
+        output.CarsResult(cars, winner);
+
+
     }
 }
