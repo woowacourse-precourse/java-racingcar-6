8d7319f4aa1129b84939ededc54226d7e06c36a0
zjacom <eephus625@icloud.com>
2023-10-31 17:00:38 +0900

    test(racingCar): UserTest 수정
    User 클래스 변수명 변경으로 인한 자동 수정

--- src/test/java/racingcar/UserTest.java
+++ src/test/java/racingcar/UserTest.java
@@ -13,7 +13,7 @@ public class UserTest {
         // 리스트 cars에 Car 인스턴스가 3개 있는지 확인
         String inputStr = "pobi,woni,jun";
         user.makeCars(inputStr);
-        assertThat(user.getCars()).hasSize(3);
+        assertThat(user.getCarList()).hasSize(3);
         // 5 글자를 초과하는 자동차 이름이 있으면 예외 발생이 되는지 확인
         String inputStrForException = "pobipobi,woni,jun";
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -26,7 +26,7 @@ public class UserTest {
         int attemptNumber = 5;
         user.tryDriving(attemptNumber);
         // 리스트 cars에 있는 각각의 Car 인스턴스가 길이가 5 이하인 주행거리를 가지고 있는지 확인
-        for (Car car : user.getCars()) {
+        for (Car car : user.getCarList()) {
             assertThat(car.getDistanceDriven()).as("운이 나빠서 '-'가 포함되지 않았어요!").contains("-");
             assertThat(car.getDistanceDriven()).hasSizeLessThanOrEqualTo(5);
         }
