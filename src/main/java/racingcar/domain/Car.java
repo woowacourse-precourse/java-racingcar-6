675da2d279da2cd3cc991ac38bc4d530da2a59e5
MingyeomKim <mingyum119@naver.com>
2023-11-01 05:19:13 +0900

    feat(Name): 이름의 입력에 대한 검증 로직을 Name 클래스로 이동
    기존에 Car 도메인에서 이름 입력에 대해 수행한 검증을 Name 도메인에서 수행하도록 수정한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,9 @@
 package racingcar.domain;
 
-import static racingcar.global.constants.NumberType.MAX_LENGTH_OF_NUMBER;
 import static racingcar.global.constants.NumberType.MIN_FORWARD_THRESHOLD;
 import static racingcar.global.constants.SymbolType.RESULT_POSITION;
 import static racingcar.global.constants.SymbolType.RESULT_TOKEN;
-import static racingcar.global.exception.ErrorMessage.INVALID_NAME_CHARACTERS;
-import static racingcar.global.exception.ErrorMessage.INVALID_NAME_LENGTH;
 
-import racingcar.global.exception.RaceException;
 import racingcar.utils.RandomUtils;
 
 public class Car {
@@ -15,7 +11,6 @@ public class Car {
     private Integer position;
 
     private Car(final String name) {
-        Validator.validateName(name);
         this.name = Name.of(name);
         this.position = 0;
     }
@@ -54,32 +49,5 @@ public class Car {
     @Override
     public String toString() {
         return name + RESULT_TOKEN.getSymbol() + RESULT_POSITION.getSymbol().repeat(position);
-    }
-
-    private static class Validator {
-        private static void validateName(final String name) {
-            validateLength(name);
-            validateCharacters(name);
-        }
-
-        private static void validateLength(final String name) {
-            if (isLengthExceeded(name)) {
-                throw RaceException.of(INVALID_NAME_LENGTH);
-            }
-        }
-
-        private static boolean isLengthExceeded(final String name) {
-            return name.length() > MAX_LENGTH_OF_NUMBER.getValue();
-        }
-
-        private static void validateCharacters(final String name) {
-            if (containsInvalidCharacters(name)) {
-                throw RaceException.of(INVALID_NAME_CHARACTERS);
-            }
-        }
-
-        private static boolean containsInvalidCharacters(final String name) {
-            return !name.matches("[a-zA-Z]+");
-        }
     }
 }
--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,13 +1,47 @@
 package racingcar.domain;
 
+import static racingcar.global.constants.NumberType.MAX_LENGTH_OF_NUMBER;
+import static racingcar.global.exception.ErrorMessage.INVALID_NAME_CHARACTERS;
+import static racingcar.global.exception.ErrorMessage.INVALID_NAME_LENGTH;
+
+import racingcar.global.exception.RaceException;
+
 public class Name {
     private String value;
 
-    private Name(String value) {
+    private Name(final String value) {
+        Validator.validateName(value);
         this.value = value;
     }
 
-    public static Name of(String value) {
+    public static Name of(final String value) {
         return new Name(value);
+    }
+
+    private static class Validator {
+        private static void validateName(final String name) {
+            validateLength(name);
+            validateCharacters(name);
+        }
+
+        private static void validateLength(final String name) {
+            if (isLengthExceeded(name)) {
+                throw RaceException.of(INVALID_NAME_LENGTH);
+            }
+        }
+
+        private static boolean isLengthExceeded(final String name) {
+            return name.length() > MAX_LENGTH_OF_NUMBER.getValue();
+        }
+
+        private static void validateCharacters(final String name) {
+            if (containsInvalidCharacters(name)) {
+                throw RaceException.of(INVALID_NAME_CHARACTERS);
+            }
+        }
+
+        private static boolean containsInvalidCharacters(final String name) {
+            return !name.matches("[a-zA-Z]+");
+        }
     }
 }
