8f6c6b19bfb34cfaea297d2baacea2ce62216275
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-01 05:17:09 +0900

    feat : 최소 자동차 개수에 대한 검증 기능 구현

--- src/main/java/racingcar/util/validation/CarNamesValidator.java
+++ src/main/java/racingcar/util/validation/CarNamesValidator.java
@@ -7,19 +7,29 @@ import racingcar.vo.CarName;
 
 public class CarNamesValidator {
 
-    private static final String EMPTY_CAR_NAME_MESSAGE = "자동차 이름에 빈 값은 허용되지 않습니다.";
-    private static final String DUPLICATE_CAR_NAME_MESSAGE = "중복된 자동차 이름이 존재합니다.";
+    private static final Integer MIN_CAR_NAME_SIZE = 2;
+    private static final String EMPTY_CAR_NAME_ERROR_MESSAGE = "자동차 이름에 빈 값은 허용되지 않습니다.";
+    private static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 존재합니다.";
+    private static final String MIN_CAR_NAME_SIZE_ERROR_MESSAGE = "최소 %s개 이상의 자동차 이름을 입력하셔야 합니다.";
 
     private CarNamesValidator() {
     }
 
     public static void validateCarNames(List<CarName> carNames) {
         if (hasEmptyName(carNames)) {
-            throw new IllegalArgumentException(EMPTY_CAR_NAME_MESSAGE);
+            throw new IllegalArgumentException(EMPTY_CAR_NAME_ERROR_MESSAGE);
         }
         if (hasDuplicate(carNames)) {
-            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_MESSAGE);
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR_MESSAGE);
         }
+        if (isCarNameListSmallerThanMin(carNames)) {
+            String errorMessage = String.format(MIN_CAR_NAME_SIZE_ERROR_MESSAGE, MIN_CAR_NAME_SIZE);
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private static boolean isCarNameListSmallerThanMin(List<CarName> carNames) {
+        return carNames.size() < MIN_CAR_NAME_SIZE;
     }
 
     private static boolean hasEmptyName(List<CarName> carNames) {
