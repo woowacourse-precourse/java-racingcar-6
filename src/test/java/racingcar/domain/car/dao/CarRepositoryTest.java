e22d93ca5d566e9c2e7e25758c7b081264b09c3d
02ggang9 <02ggang9@gmail.com>
2023-10-31 23:46:30 +0900

    test(CarRepositoryTest): 중복되는 코드를 @BeforeEach와 @AfterEach를 이용해 제거

--- src/test/java/racingcar/domain/car/dao/CarRepositoryTest.java
+++ src/test/java/racingcar/domain/car/dao/CarRepositoryTest.java
@@ -1,9 +1,7 @@
 package racingcar.domain.car.dao;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import racingcar.domain.car.Car;
 import racingcar.domain.util.ErrorMessage;
@@ -20,24 +18,35 @@ import static racingcar.domain.util.ErrorMessage.*;
 
 class CarRepositoryTest {
 
-    // 1. save
-    // 2. findAll
-    // 3. findAllCarsWithMaxPosition
+    private CarRepository carRepository;
+    private Car firstCar;
+    private Car secondCar;
+    private Car thirdCar;
+
+    @BeforeEach
+    public void setUp() {
+        carRepository = new CarRepository();
+        firstCar = mock(Car.class);
+        secondCar = mock(Car.class);
+        thirdCar = mock(Car.class);
+    }
+
+    @AfterEach
+    public void clear() {
+        carRepository.clear();
+    }
 
     @Nested
     @DisplayName("Car 저장 테스트")
+
     class SaveCarRepositoryTest {
 
         @Test
         @DisplayName("Car 객체가 성공적으로 저장되어야 한다.")
         public void Car_객체가_성공적으로_저장되어야_한다() {
-            CarRepository carRepository = new CarRepository();
-            Car firstCar = new Car("FirstCarName");
-            Car secondCar = new Car("SecondCarName");
-
             carRepository.clear();
-            carRepository.save(firstCar);
-            carRepository.save(secondCar);
+            carRepository.save(new Car("FirstCarName"));
+            carRepository.save(new Car("SecondCarName"));
 
             List<Car> cars = carRepository.findAll();
 
@@ -57,10 +66,6 @@ class CarRepositoryTest {
         @Test
         @DisplayName("가장 많이 움직인 Car 객체를 읽어와야 한다")
         public void 가장_많이_움직인_Car_객체를_읽어와야_한다() {
-            CarRepository carRepository = new CarRepository();
-            Car firstCar = Mockito.mock(Car.class);
-            Car secondCar = Mockito.mock(Car.class);
-
             when(firstCar.getName()).thenReturn("firstCar");
             when(secondCar.getName()).thenReturn("secondCar");
 
@@ -81,11 +86,6 @@ class CarRepositoryTest {
         @Test
         @DisplayName("가장 많이 움직인 Car 객체가 여럿일 경우 함께 읽어와야 한다")
         public void 가장_많이_움직인_Car_객체가_여럿일_경우_함께_읽어와야_한다() {
-            CarRepository carRepository = new CarRepository();
-            Car firstCar = Mockito.mock(Car.class);
-            Car secondCar = Mockito.mock(Car.class);
-            Car thirdCar = Mockito.mock(Car.class);
-
             when(firstCar.getName()).thenReturn("firstCar");
             when(secondCar.getName()).thenReturn("secondCar");
             when(thirdCar.getName()).thenReturn("thirdCar");
@@ -117,13 +117,10 @@ class CarRepositoryTest {
         @Test
         @DisplayName("데이터베이스를 성공적으로 초기화 해야 한다")
         public void 데이터베이스를_성공적으로_초기화_해야_한다() {
-            CarRepository carRepository = new CarRepository();
-            Car car = mock(Car.class);
             carRepository.clear();
-
-            carRepository.save(car);
-            carRepository.save(car);
-            carRepository.save(car);
+            carRepository.save(firstCar);
+            carRepository.save(secondCar);
+            carRepository.save(thirdCar);
 
             assertThat(carRepository.findAll()).hasSize(3);
 
@@ -138,8 +135,6 @@ class CarRepositoryTest {
         @Test
         @DisplayName("DB에 Car 객체가 없는 경우 예외를 발생시켜야 한다")
         public void DB에_Car_객체가_없는_경우_예외를_발생시켜야_한다() {
-            CarRepository carRepository = new CarRepository();
-
             carRepository.clear();
 
             // junit.jupiter
