c469a3323b2ab7ac832275e7fce892471ecfe3fb
hlmg <devhlmg@gmail.com>
2023-10-31 19:34:10 +0900

    test(CarTest): 테스트 코드 분리

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,16 +1,12 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 class CarTest {
-
-    private static final CarFactory carFactory = new CarFactory(new RandomMoveEngine(), new CarNameValidator());
 
     @ParameterizedTest
     @CsvSource(textBlock = """
@@ -24,33 +20,5 @@ class CarTest {
         car.moveForward();
 
         assertThat(car.getPosition()).isEqualTo(expected);
-    }
-
-    // TODO: test 분리하기
-    @Nested
-    class 생성_테스트 {
-        @ParameterizedTest
-        @CsvSource(textBlock = """
-                a,
-                12345
-                """)
-        void 유효한_이름이면_정상_생성(String validName) {
-            Car car = carFactory.create(validName);
-
-            assertThat(car.getName()).isEqualTo(validName);
-        }
-
-        @ParameterizedTest
-        @CsvSource(textBlock = """
-                '',
-                ' ',
-                '1 ',
-                ' 1',
-                123456
-                """)
-        void 이름이_유효하지_않으면_예외가_발생한다(String invalidName) {
-            assertThatIllegalArgumentException()
-                    .isThrownBy(() -> carFactory.create(invalidName));
-        }
     }
 }
