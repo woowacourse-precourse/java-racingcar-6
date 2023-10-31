8c745ecc36a7025211f61f9f0046871097842c70
JayMon0327 <kioplm0211@naver.com>
2023-10-31 23:50:56 +0900

    사용자가 몇 번의 이동을 할 것인지를 입력할 수 있게 numAttempts 변수에 이동 횟수 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,9 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNames = splitCarNames(Console.readLine());
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numAttempts = Integer.parseInt(Console.readLine());
+
     }
 
     private static List<String> splitCarNames(String input) {
