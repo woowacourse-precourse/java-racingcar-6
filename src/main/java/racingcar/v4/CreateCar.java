846293585cd05e117bf25030161e58f30183d95a
권장순 <jsoonworld@gmail.com>
2023-10-31 22:26:16 +0900

    feat(racingcar) : CreateCar#validateCarNameLength()를 구현함.

--- src/main/java/racingcar/v4/CreateCar.java
+++ src/main/java/racingcar/v4/CreateCar.java
@@ -29,6 +29,14 @@ public class CreateCar {
         return trimmedCarNames;
     }
 
+    public void validateCarNameLength(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
 
 
 
