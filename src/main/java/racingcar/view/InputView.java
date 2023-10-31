d4ee36fdde2f5f8dc906a99bd7ec60b81bb127e8
gourderased <gourderased@gmail.com>
2023-10-31 10:23:35 +0900

    "feat(racingcar): 이동 횟수를 입력받는 기능 구현"

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.");
         return Console.readLine().split(",");
     }
+
+    public static int getNumberOfMovesFromUser() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
