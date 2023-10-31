9b1288cb230e8586bb21ac1c087935ed230765e9
InJun2 <os2864@naver.com>
2023-10-31 14:02:18 +0900

    :art: 변수명 수정 및 공백 문자 추가

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -4,22 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class RacingCarView {
     private final static String START_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final static String TRY_NUMBER_INPUT = "시도할 회수는 몇회인가요?";
+    private final static String TRY_COUNT_INPUT = "시도할 회수는 몇회인가요?";
     private final static String RACING_RESULT_OUTPUT = "실행 결과";
     private final static String RACING_WINNER_OUTPUT = "최종 우승자 : ";
+    private final static String NEW_LINE = "\n";
 
     public String getCarNameInput() {
         System.out.println(START_INPUT);
         return Console.readLine();
     }
 
-    public String getTryNumberInput() {
-        System.out.println(TRY_NUMBER_INPUT);
+    public String getTryCountInput() {
+        System.out.println(TRY_COUNT_INPUT);
         return Console.readLine();
     }
 
     public void racingResultOutput() {
-        System.out.println(RACING_RESULT_OUTPUT);
+        System.out.println(NEW_LINE + RACING_RESULT_OUTPUT);
     }
 
     public void racingCarsDistanceOutput(String str) {
@@ -27,6 +28,6 @@ public class RacingCarView {
     }
 
     public void racingWinnerOutput(String str) {
-        System.out.println(RACING_WINNER_OUTPUT + str);
+        System.out.print(RACING_WINNER_OUTPUT + str);
     }
 }
