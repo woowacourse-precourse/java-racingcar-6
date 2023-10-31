d2b91952e387cf5a9ed4ff9344f43abcadd47a97
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-31 22:33:19 +0900

    refactor ($OutputView) : - 출력 방식 생성 - "---"을 받아와서 출력만하는 거에서 "-" 만들어서 출력하는 걸로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,7 @@ public class OutputView {
     public void printWinner(List<String> winner){
         System.out.println("최종 우승자 : " + String.join(",", winner));
     }
-    public void printEachFoward(String carName,String carForward){
-        System.out.println(carName + " : "+ carForward);
+    public void printEachFoward(String carName,Integer carPlace){
+        System.out.println(carName + " : "+ "-".repeat(carPlace));
     }
 }
