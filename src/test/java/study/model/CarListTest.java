80356649bf4e710944bee038833fa59572b48add
haiseong <wjdgotjd9908@gmail.com>
2023-10-31 18:42:15 +0900

    test: 리스트_사이즈_테스트 추가

--- src/test/java/study/model/CarListTest.java
+++ src/test/java/study/model/CarListTest.java
@@ -2,11 +2,33 @@ package study.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Iterator;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.model.CarList;
+import racingcar.model.RacingCar;
 
 class CarListTest {
+
+    @Test
+    void test_리스트_사이즈_테스트() {
+        // given
+        CarList carList = new CarList();
+        carList.add("pobi");
+        carList.add("woni");
+        carList.add("jun");
+
+        // when
+        int listSize = 0;
+        Iterator<RacingCar> carIterator = carList.getIterator();
+        while (carIterator.hasNext()) {
+            carIterator.next();
+            listSize++;
+        }
+
+        // then
+        assertThat(listSize).isEqualTo(3);
+    }
 
     @Test
     void test_정상적인경우() {
@@ -26,5 +48,4 @@ class CarListTest {
         System.out.println("winners = " + winners);
         assertThat(winners.size()).isGreaterThan(0);
     }
-
 }
