79224d0440a8874334c0625af92bdb4d165ade34
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-31 21:54:44 +0900

    style(application): reorder code for readability

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,10 +8,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.printf("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String carNames = InputManager.readCarNames();
-        Racing racing = new Racing(carNames);
         System.out.printf("시도할 회수는 몇회인가요?\n");
         int numberOfAttempts = InputManager.readNumberOfAttempts();
-
+        
+        Racing racing = new Racing(carNames);
         System.out.printf("\n실행 결과\n");
         for (int i = 0; i < numberOfAttempts; i++) {
             racing.moveAllCarsForward();
