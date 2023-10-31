94cc1b4aa1326901a6d76e0f1523396186f7ceae
uwoobeat <desktop3711@gmail.com>
2023-10-31 21:19:48 +0900

    feat: 게임의 경주 횟수는 1회 이상이어야 한다

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,14 +2,17 @@ package racingcar;
 
 import java.util.List;
 import racingcar.exception.InsufficientCarCountException;
+import racingcar.exception.InsufficientRaceCountException;
 
 public class Game {
     public static final int MIN_CARS_REQUIRED = 2;
+    public static final int MIN_RACE_REQUIRED = 1;
     private final List<Car> cars;
     private final int raceCount;
 
     public Game(List<Car> cars, int raceCount) {
         validateCars(cars);
+        validateRaceCount(raceCount);
 
         this.cars = cars;
         this.raceCount = raceCount;
@@ -18,6 +21,12 @@ public class Game {
     private void validateCars(List<Car> cars) {
         if (cars.size() < MIN_CARS_REQUIRED) {
             throw new InsufficientCarCountException();
+        }
+    }
+
+    private void validateRaceCount(int raceCount) {
+        if (raceCount < MIN_RACE_REQUIRED) {
+            throw new InsufficientRaceCountException();
         }
     }
 
