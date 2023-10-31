a1886e28c5fdfac47cbcfd4bbf478c08c764fb13
uiop9900 <jia9510@naver.com>
2023-10-31 21:58:34 +0900

    readLine() 에러 해결.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+
         System.out.println("경주할 자동 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = readLine().trim();
 
@@ -18,9 +19,6 @@ public class Application {
 
         // repeat
         System.out.println("시도할 회수는 몇회인가요?");
-        String repeatNumber = readLine();
-        validateNumber(repeatNumber);
-
         int repeat = Integer.parseInt(readLine());
         Map<String, Integer> carsWithResult = carListToMap(carNames);
 
