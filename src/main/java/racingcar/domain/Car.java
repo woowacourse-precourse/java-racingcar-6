03a5a4a4283d86cb5030221372eff9926976a4c3
ghdeo <goldencorn7@naver.com>
2023-10-31 23:33:46 +0900

    feat: Add Car instance variables

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,12 @@
 package racingcar.domain;
 
 public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
 
 }
