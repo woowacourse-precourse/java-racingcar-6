403ba8db9adb7ab48a08405470ff29392f8b5c80
박성춘 <songchon100@gmail.com>
2023-11-01 01:27:17 +0900

    feat : 이동 횟수가 1인 경우 예외 메시지 추가

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     TRY_NUM_NOT_NUMBER_ERROR("이동 횟수는 숫자를 입력해주세요"),
     EMPTY_INPUT_ERROR("공백을 입력하지 마세요."),
     CAR_NUM_ERROR("차는 두대 이상 입력해주세요."),
+    TRY_NUM_IS_ZERO_ERROR("이동 횟수는 1이상으로 입력해주세요."),
     DUPLE_CAR_NAME_ERROR("차 이름이 중복되었습니다.");
 
     private String errorMessage;
