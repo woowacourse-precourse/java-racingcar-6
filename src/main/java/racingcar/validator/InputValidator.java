82f2889f8d1c58527277d823522e41f776c0cf8f
HongYeseul <ghd8119@naver.com>
2023-10-31 20:41:01 +0900

    :recycle: refactor(InputValidator): 클래스 내 상수 사용 - ValidatorConstants enum형 클래스 생성

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,11 +1,11 @@
 package racingcar.validator;
 
 import static racingcar.validator.ExceptionMessage.*;
+import static racingcar.validator.constants.ValidatorConstants.*;
 
 public class InputValidator {
     public static void lessThenFiveLetters(String input){
-        // TODO: 상수 교체
-        if (input.length() > 5) {
+        if (input.length() > MAX_LENGTH_OF_CAR_NAME.getValue()) {
             Exception.illegalArgument(LESS_THEN_FIVE_LETTERS.getMessage());
         }
     }
---
+++ src/main/java/racingcar/validator/constants/ValidatorConstants.java
@@ -0,0 +1,15 @@
+package racingcar.validator.constants;
+
+public enum ValidatorConstants {
+    MAX_LENGTH_OF_CAR_NAME(5);
+
+    final int value;
+
+    ValidatorConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
