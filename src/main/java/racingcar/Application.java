344330bdedade214de93daa2bb0d3b95e5e8120d
yerim <yerimm99@github.com>
2023-10-31 15:10:51 +0900

    Feat: 시도 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,7 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         carNameInput();
+        int moveNum = Integer.parseInt(readLine());
     }
 
     private static void carNameInput() {
