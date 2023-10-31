3c9ba7a61d8f2aa17975023173058f18e7a3d9d9
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 11:01:08 +0900

    Test: validateCarNameInput 중복 검사 테스트 추가
    중복된 이름이 있을 경우("abc,abc,def")  IllegalArgumentException 발생, 발생한 예외메시지 체크

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public class InputView {
-    
+
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -3,12 +3,14 @@ package racingcar.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class ValidatorTest {
     Validator validator = new Validator();
 
     private static final String WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE = "이름은 5자 이하여야 합니다";
+    private static final String CAR_NAME_DUPLICATION_ERROR_MESSAGE = "이름이 중복되어선 안됩니다";
 
     @Test
     void 차_이름_길이_예외테스트() {
@@ -16,5 +18,13 @@ class ValidatorTest {
             validator.validateCarNameInput("wonjun,abc,def");
         });
         assertEquals(WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    void 차_이름_중복_예외테스트() {
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validator.validateCarNameInput("abc,abc,def");
+        });
+        assertEquals(CAR_NAME_DUPLICATION_ERROR_MESSAGE, e.getMessage());
     }
 }
