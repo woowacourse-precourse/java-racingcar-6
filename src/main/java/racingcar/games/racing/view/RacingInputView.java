c287a2c37ac283a661559e963d8c914b184ea88f
haebyun <bhk4392@gmail.com>
2023-11-01 07:29:01 +0900

    feat(RacingInputView): 자동차들이 몇 번 이동할 지 입력 받는 기능 구현

--- src/main/java/racingcar/games/racing/view/RacingInputView.java
+++ src/main/java/racingcar/games/racing/view/RacingInputView.java
@@ -5,10 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class RacingInputView {
 
     private static final String CAR_NAMES_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_REQUEST = "실행할 횟수를 입력하세요.";
 
     public String readCarNames() {
         System.out.println(CAR_NAMES_REQUEST);
         String carNames = Console.readLine();
         return carNames;
+    }
+
+    public String readAttempt() {
+        System.out.println(ATTEMPT_REQUEST);
+        String attempt = Console.readLine();
+        return attempt;
     }
 }
