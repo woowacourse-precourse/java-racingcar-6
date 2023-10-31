de63e3b19324bd321c6ecfcd3c4114d332e9cd9d
ay-eonii <naayen@naver.com>
2023-11-01 00:12:55 +0900

    test: 자동차 위치 안내 문구 생성 단위테스트 작성

--- src/test/java/racingcar/domain/AnnouncerTest.java
+++ src/test/java/racingcar/domain/AnnouncerTest.java
@@ -23,4 +23,30 @@ class AnnouncerTest {
 
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void 위치_안내_문구를_작성한다() {
+        List<Car> carList = Arrays.asList(
+                mock(Car.class),
+                mock(Car.class),
+                mock(Car.class)
+        );
+
+        when(carList.get(0).getName()).thenReturn("Car1");
+        when(carList.get(0).getLocation()).thenReturn(3);
+
+        when(carList.get(1).getName()).thenReturn("Car2");
+        when(carList.get(1).getLocation()).thenReturn(5);
+
+        when(carList.get(2).getName()).thenReturn("Car3");
+        when(carList.get(2).getLocation()).thenReturn(2);
+
+        Announcer announcer = mock(Announcer.class);
+        when(announcer.generateLocationAnnouncement(carList)).thenCallRealMethod();
+
+        String result = announcer.generateLocationAnnouncement(carList);
+        String expected = "Car1 : ---\nCar2 : -----\nCar3 : --\n";
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
