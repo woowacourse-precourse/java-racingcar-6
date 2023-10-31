e1acd54aeab0e33adfdd7699b357ad52fc638fea
chnbroa <chnbroa011@naver.com>
2023-10-31 19:18:31 +0900

    feat: 무작위 값이 4 이상일 경우 이동거리 1증가 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,15 +1,26 @@
 package racingcar.model;
 
+import static racingcar.util.Util.getRandomNumber;
 import static racingcar.util.validator.InputValidator.isStringLengthValid;
 import static racingcar.util.validator.InputValidator.checkBlank;
 
 public class Car {
     private final String NAME;
-
+    private int position = 0;
 
     public Car(String name) {
         isStringLengthValid(name);
         checkBlank(name);
         this.NAME = name;
+    }
+    public void move(){
+        if(getRandomNumber() >= 4){
+            position++;
+        }
+    }
+
+    public int getPosition()
+    {
+        return position;
     }
 }
