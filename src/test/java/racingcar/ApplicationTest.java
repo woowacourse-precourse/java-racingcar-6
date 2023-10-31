b95b5cd905fd41243b0b9aeca64a816a9408ca64
kgy1008 <gayeon1008@g.skku.edu>
2023-10-31 14:17:13 +0900

    test: 시도 횟수 처리 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -7,6 +7,10 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import static racingcar.CheckException.checkPositiveNumberOfMove;
+import static racingcar.CheckException.checkRightNumberOfMove;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -28,6 +32,21 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도_횟수에_대한_예외_처리() {
+        int input1 = -2;
+        String input2 = "SAE";
+
+        assertAll(
+                () -> assertThatThrownBy(() -> checkPositiveNumberOfMove(input1))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Input should be a positive number"),
+                () -> assertThatThrownBy(() -> checkRightNumberOfMove(input2))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Input should be convert to number")
         );
     }
 
