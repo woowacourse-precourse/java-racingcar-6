95787acf906752a37088972e90514cda3600f2d2
sudhdkso <waga1009@gmail.com>
2023-10-31 21:33:21 +0900

    feat: 최소 움직임에 대한 횟수 추가

--- src/main/java/racingcar/domain/enums/Constants.java
+++ src/main/java/racingcar/domain/enums/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int RANDOM_END_RANGE = 9;
     public static final int MAX_NAME_LENGTH = 5;
     public static final int MIN_MOVE_THRESHOLD = 4;
+    public static final int MIN_RACING_COUNT = 1;
 }
