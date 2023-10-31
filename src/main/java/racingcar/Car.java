f29c07482f1f1f2c99f828141f877bfe5bd792b2
Park Eunhyeong <silbroeh@gmail.com>
2023-10-31 17:51:49 +0900

    feat : Car클래스 - move 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,17 @@
 package racingcar;
 
 public class Car {
-
+    private static final int MOVE_CONDITION = 4;
     private final String name;
     private int distance = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private void move(int number) {
+        if (number >= MOVE_CONDITION) {
+            addDistance();
+        }
     }
 }
