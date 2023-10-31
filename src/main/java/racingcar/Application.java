653b9ea1bdffd0066d7a35eed21860d2649d26c1
ekeprl <ykeon26@naver.com>
2023-10-31 20:53:56 +0900

    보기 좋도록 배치 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,9 @@
 package racingcar;
 
-import racingcar.Controller.Controller;
+import racingcar.Controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        new Controller();
+        new GameController();
     }
 }
