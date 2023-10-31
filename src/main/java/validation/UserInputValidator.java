3bffd3de41392db08c78feea4c662ebe0fc11266
yunseung <yseungun.21@gmail.com>
2023-11-01 01:21:18 +0900

    feat(UserInputValidator): 차 이름을 한개만 입력했을 때 케이스 에러 처리 추가

--- src/main/java/validation/UserInputValidator.java
+++ src/main/java/validation/UserInputValidator.java
@@ -3,6 +3,7 @@ package validation;
 import static constant.Constant.NAME_LENGTH_LIMIT;
 import static constant.ErrorMessage.EMPTY_NAME_CASE_MESSAGE;
 import static constant.ErrorMessage.EXCEED_NAME_CASE_MESSAGE;
+import static constant.ErrorMessage.UNCOMPETITIVE_CASE_MESSAGE;
 
 public class UserInputValidator {
     public void checkPositiveNumber(Integer integer) {
@@ -16,6 +17,12 @@ public class UserInputValidator {
             throw new IllegalArgumentException(EMPTY_NAME_CASE_MESSAGE);
         } else if (name.length() > NAME_LENGTH_LIMIT) {
             throw new IllegalArgumentException(EXCEED_NAME_CASE_MESSAGE);
+        }
+    }
+
+    public void validateInputCarNameCount(String[] names) {
+        if (names.length < 2) {
+            throw new IllegalArgumentException(UNCOMPETITIVE_CASE_MESSAGE);
         }
     }
 }
