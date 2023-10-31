5a27d1cdf1ff0475b2ccb3fc96e5b89fb7b60aa8
bbbang105 <2018111366@dgu.ac.kr>
2023-10-31 22:55:13 +0900

    feat : 0~9 사이의 랜덤한 값을 생성하는 기능 구현
    generateRandomNumber() 메소드 구현

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,4 +1,10 @@
 package racingcar.utils;
 
-public class Utils {
+import camp.nextstep.edu.missionutils.Randoms;
+
+public final class Utils {
+
+    public static int generateRandomNumber() {
+        return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0,9);
+    }
 }
