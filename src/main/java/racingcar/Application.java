063f7fbd871141476f74a0766ab32bf47b1c019f
m5s3 <devmyoungsoo2@gmail.com>
2023-10-31 20:22:03 +0900

    feat(racingcar): 자동차 게임을 진행하는 애플리케이션 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.CarsGenerator;
+import racingcar.domain.Race;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = requestCarNames();
+        List<Car> cars = CarsGenerator.generateCars(carNames);
+        Race race = new Race(cars);
+        int raceCount = requestRaceCount();
+
+        while (raceCount > 0) {
+            race.play();
+            raceCount--;
+            System.out.println();
+        }
+        race.printWinner();
+    }
+
+    private static String requestCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int requestRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
