de30c39fa40589fef2fb93a55791f81af579df09
Junobee25 <wnsdhqo@naver.com>
2023-10-31 19:34:53 +0900

    refactor(domain): createCarList 메서드 리턴 값 수정

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -2,18 +2,20 @@ package racingcar.domain;
 
 import racingcar.car.Car;
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.HashMap;
+
 public class CarGenerator {
 
     Car car = new Car();
 
-    public void createCarList() {
-        HashMap<String,String> carList = new HashMap<>();
-        String [] cars = splitStringByCommas();
-        for(String car : cars) {
-            carList.put(car,"");
+    public HashMap<String, String> createCarList() {
+        HashMap<String, String> carList = new HashMap<>();
+        String[] cars = splitStringByCommas();
+        for (String car : cars) {
+            carList.put(car, "");
         }
-        car.setCarList(carList);
+        return carList;
     }
 
     public String[] splitStringByCommas() {
