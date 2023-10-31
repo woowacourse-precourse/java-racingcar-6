fd53cc4df09cf7835465a61b973998a6fe932add
unknown <winternrabbit@gmail.com>
2023-10-31 13:13:15 +0900

    자동차 이름 공백 시의 테스트코드 생성

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class GameTest extends NsTest {
+    @Test
+    void 이름_공백_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
