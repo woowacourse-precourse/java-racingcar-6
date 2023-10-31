61f19bb076fffdc945487658d84820bd2611ca83
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-10-31 17:49:17 +0900

    feat(racingCar): 경주차 정보를 담을 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name){
+        this.name = name;
+        this.distance = 0;
+    }
+}
