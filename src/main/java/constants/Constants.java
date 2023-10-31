5ac2dec5ffa89d7784db85edeb18f9f9613edbab
nayonsoso <panda0329@naver.com>
2023-11-01 04:08:59 +0900

    feat: 랜덤 시작과 끝, 이동하기 위한 최솟값을 상수화

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -3,4 +3,7 @@ package constants;
 public class Constants {
     public static final int MAX_NAME_LENGTH = 5;
     public static final String NAME_DELIMITER = ",";
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MIN_VALUE_TO_MOVE = 4;
 }
