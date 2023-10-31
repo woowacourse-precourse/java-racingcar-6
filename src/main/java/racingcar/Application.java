ce7af70fa7b6c600bacd9c0d371f73827d67c99e
minisyu <iswater.kim@gmail.com>
2023-10-31 11:49:09 +0900

    refactor: Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,10 @@
 package racingcar;
 
-import racingcar.controller.GameController;
-import racingcar.converter.InputConverter;
-import racingcar.domain.game.move.RandomMoveStrategy;
-import racingcar.domain.game.move.RandomNumberGenerator;
-import racingcar.util.InputValidator;
-import racingcar.util.MoveStrategy;
-import racingcar.util.NumberGenerator;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import racingcar.config.Configuration;
 
 public class Application {
     public static void main(String[] args) {
-        InputValidator inputValidator = new InputValidator();
-        InputConverter inputConverter = new InputConverter();
-        InputView inputView = new InputView(inputValidator, inputConverter);
-        OutputView outputView = new OutputView();
-        NumberGenerator randomNumberGenerator = new RandomNumberGenerator(0, 9);
-        MoveStrategy randomMoveStrategy = new RandomMoveStrategy(randomNumberGenerator);
-        GameController gameController = new GameController(inputView, outputView, randomMoveStrategy);
-        gameController.run();
+        Configuration.getGameController().run();
+        Configuration.closeResources();
     }
 }
