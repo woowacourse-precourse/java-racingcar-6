b82823bd5ca60f565928b51adb8de8816c9eb2cd
transfer_kk <kseysh123@naver.com>
2023-10-31 21:46:14 +0900

    feat : 레이싱 게임 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import Car.CarGenerator;
+import Car.Car;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        List<Car> carList = CarGenerator.generateCar(carNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String userInputRepeatCount = Console.readLine();
+        Validator.validateRepeatCount(userInputRepeatCount);
+        int repeatCount = Integer.parseInt(userInputRepeatCount);
+
+        for (int i = 0; i < repeatCount; i++) {
+            for (Car car : carList) {
+                car.moveForward();
+                car.printCurrentLocation();
+            }
+            System.out.println();
+        }
+
+        RacingGame.printWinner(carList);
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,2 +1,30 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import Car.Car;
+
 public class RacingGame {
+    public static void printWinner(List<Car> carList) {
+        List<String> winnerNameList = new ArrayList<>();
+
+        int maxCurrentLocation = 0;
+
+        for (Car car : carList) {
+            if (car.getCurrentLocation() > maxCurrentLocation) {
+                maxCurrentLocation = car.getCurrentLocation();
+            }
+        }
+
+
+        for (Car car : carList) {
+            if (car.getCurrentLocation() == maxCurrentLocation) {
+                winnerNameList.add(car.getName());
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        String winnerNames = String.join(",", winnerNameList);
+        System.out.println(winnerNames);
+    }
 }
