3d0b7bcbc343cc08801d1d51beef16dc6df2ef83
ikjo39 <chi4321@naver.com>
2023-10-31 17:08:01 +0900

    test: 무작위 숫자에 따른 전진 판단 로직 테스트 추가
    - 무작위 숫자가 전진 조건을 만족하는 경우 true를 반환
    - 무작위 숫자가 멈춤 조건을 만족하는 경우 false를 반환

---
+++ src/test/java/racingcar/model/StepForwardValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static racingcar.constant.StepConstant.MOVING_FORWARD;
+import static racingcar.constant.StepConstant.STOP;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StepForwardValidatorTest {
+
+    @DisplayName("무작위 숫자가 전진 해야하는 값일 경우 true를 반환한다.")
+    @Test
+    void success_isCarStepForward_true() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    boolean result = StepForwardValidator.isCarStepForward();
+                    Assertions.assertThat(result).isTrue();
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @DisplayName("무작위 숫자가 멈춤 하는 값일 경우 false를 반환한다.")
+    @Test
+    void success_isCarStepForward_false() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    boolean result = StepForwardValidator.isCarStepForward();
+                    Assertions.assertThat(result).isFalse();
+                },
+                STOP
+        );
+    }
+}
