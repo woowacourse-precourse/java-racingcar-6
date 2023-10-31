80ee44d54bb2aa53cc63a97563edc7bc080c54c3
ks0927 <brian_als@naver.com>
2023-11-01 00:16:35 +0900

    feat: Application클래스에 run() 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        GameController gameController = new GameController(new InputView(), new OutputView(),
+                new RandomNumberGenerator());
+        gameController.run();
     }
 }
