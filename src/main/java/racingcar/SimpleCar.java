efe743c0682f94779a21d78b977b5f425c57006c
san <soh7174@gmail.com>
2023-10-31 10:16:21 +0900

    feat(SimpleCar) : SimpleCar 클래스 구현

--- src/main/java/racingcar/SimpleCar.java
+++ src/main/java/racingcar/SimpleCar.java
@@ -1,14 +1,30 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.RandomNumberGenerator;
 
 public class SimpleCar {
     private String carName;
+    private String carMovement;
 
-    public SimpleCar(String name) {
+    private SimpleCar(String name) {
         this.carName = name;
+        this.carMovement = "";
     }
-    public void createCar(List<String> playerNamesList) {
-        ;
+    public static void createCar(List<String> playerNamesList, List<SimpleCar> players) {
+        for(String playerName : playerNamesList) {
+            SimpleCar car = new SimpleCar(playerName);
+            players.add(car);
+        }
+    }
+    public void goFoward() {
+        int randomNumber = RandomNumberGenerator.generateRandomNumber();
+        if(randomNumber >= 4) this.carMovement += "-";
+    }
+    public String getCarName() {
+        return carName;
+    }
+    public String getCarMovement() {
+        return carMovement;
     }
 }
