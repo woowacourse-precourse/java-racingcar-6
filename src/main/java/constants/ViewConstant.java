4c2982f559d70a11470e6e96b2db7469cfedd768
liarreez <liarreez@gmail.com>
2023-10-31 18:01:38 +0900

    feat : 상수를 관리하는 클래스 생성을 방지하는 생성자 코드 구현

--- src/main/java/constants/ViewConstant.java
+++ src/main/java/constants/ViewConstant.java
@@ -4,4 +4,8 @@ public class ViewConstant {
     public static final String ONE_MOVE_SYMBOL = "-";
     public static final String ROUND_SEPARATOR = "\n";
     public static final String WINNERS_SEPARATOR = ", ";
+
+    private ViewConstant() {
+        throw new AssertionError();
+    }
 }
