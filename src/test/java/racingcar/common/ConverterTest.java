e14bb0744651420c201c299be1b9c62e2a494b30
Joohui <joohuii96@gmail.com>
2023-11-01 03:12:45 +0900

    style: 테스트 @DisplayName 적용

--- src/test/java/racingcar/common/ConverterTest.java
+++ src/test/java/racingcar/common/ConverterTest.java
@@ -3,11 +3,13 @@ package racingcar.common;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ConverterTest {
 
+    @DisplayName("String 타입의 자동차 이름을 List<String> 타입으로 변경")
     @ParameterizedTest(name = "carNames=\"{0}\"")
     @ValueSource(strings = {"pobi,jun", " pobi , jun"})
     void stringToList(String carNames) {
