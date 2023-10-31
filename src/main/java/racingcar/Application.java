032ed7c83470cbdafcd6c8a3801191100d2c6e8e
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-10-31 13:25:39 +0900

    feat: 경주할 자동차 이름 입력 and 자동차 이름들 저장
    입력을 ','으로 split한 자동차 이름들을 List에 저장
    
    Ref: Docs/README.md
    Resolve: README.md #1

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> cars;
+        List<StringBuilder> presentDistance = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        cars = List.of(input.split(","));
+
+
     }
 }
