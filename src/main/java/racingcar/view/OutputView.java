5f9cb29fc3880ce4910c3b29f2e5fffda9ffe8ad
HongYeseul <ghd8119@naver.com>
2023-10-31 17:40:43 +0900

    :recycle: refactor(OutputView): OutputMessage라는 enum 클래스로 메시지 상수 선언

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,33 +1,26 @@
 package racingcar.view;
 
 import racingcar.game.Car;
+import static racingcar.view.constants.OutputMessage.*;
 
 public class OutputView {
-    // TODO: Message enum으로 빼기
-    private final static String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final static String ASK_GAME_ROUNDS = "시도할 횟수는 몇회인가요?";
-    private final static String GAME_RESULT = "실행 결과";
-    private final static String DISPLAY_USER_SCORE_SIGN = ":";
-    private final static String SCORE_SIGN = "-";
-    private final static String FINAL_WINNER = "최종 우승자 :";
-
     public static void startGame(){
-        System.out.println(START_MESSAGE);
+        System.out.println(START_MESSAGE.getMessage());
     }
 
-    public static void askGameRounds(){
-        System.out.println(ASK_GAME_ROUNDS);
+    public static void askGameRounds() {
+        System.out.println(ASK_GAME_ROUNDS.getMessage());
     }
 
     public static void startGameResult(){
-        System.out.println(GAME_RESULT);
+        System.out.println(GAME_RESULT.getMessage());
     }
 
-    public static void broadcastUserBoardOfRound(Car car){
+    public static void broadcastUserBoardOfRound(Car car) {
         // TODO: getter 없이 쓸 수 있는 방법 고민 해보기
-        System.out.printf("%s %s ", car.getCarName(), DISPLAY_USER_SCORE_SIGN);
+        System.out.printf("%s %s ", car.getCarName(), DISPLAY_USER_SCORE_SIGN.getMessage());
         for(int i=0; i<car.getScore(); i++){
-            System.out.printf("%s", SCORE_SIGN);
+            System.out.printf("%s", SCORE_SIGN.getMessage());
         }
         System.out.println();
     }
@@ -37,6 +30,6 @@ public class OutputView {
     }
 
     public static void finalGameResult(String cars){
-        System.out.printf("%s %s",FINAL_WINNER, cars);
+        System.out.printf("%s %s",FINAL_WINNER.getMessage(), cars);
     }
 }
