651f3f2f192a2b4558f3d2d9f51db241ecb3b8ce
0Chord <seaweed.0chord@gmail.com>
2023-10-31 16:05:59 +0900

    test : 테스트 예외 처리 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -8,12 +13,6 @@ import racingcar.domain.CarPark;
 import racingcar.service.impl.InputConvertServiceImpl;
 import racingcar.service.impl.ValidateServiceImpl;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
-
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
@@ -21,11 +20,33 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 이름이_숫자일때_전진_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("1,2", "1");
+                    assertThat(output()).contains("1 : -", "2 : ", "최종 우승자 : 1");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 전진_전진() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
         );
     }
 
@@ -38,12 +59,12 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 마지막_글자가_쉼표_일때_예외_처리(){
+    void 마지막_글자가_쉼표_일때_예외_처리() {
         String input = "kyh,";
         ValidateServiceImpl validateService = new ValidateServiceImpl();
         InputConvertServiceImpl inputConvertService = new InputConvertServiceImpl(validateService);
 
-        Assertions.assertThatThrownBy(()->inputConvertService.inputConvertCarPark(input))
+        Assertions.assertThatThrownBy(() -> inputConvertService.inputConvertCarPark(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -55,18 +76,17 @@ class ApplicationTest extends NsTest {
         CarPark carPark = inputConvertService.inputConvertCarPark(input);
 
         List<String> nameList = carPark.makeNameList();
-        System.out.println("nameList = " + nameList);
-        assertThat(nameList).contains("cbnu", "math","eng");
-        assertThat(nameList).containsExactly("cbnu", "math","eng");
+        assertThat(nameList).contains("cbnu", "math", "eng");
+        assertThat(nameList).containsExactly("cbnu", "math", "eng");
     }
 
     @Test
-    void 횟수_숫자가_아닌_문자가_입력될떄_예외_처리(){
+    void 횟수_숫자가_아닌_문자가_입력될떄_예외_처리() {
         String input = "s";
         ValidateServiceImpl validateService = new ValidateServiceImpl();
         InputConvertServiceImpl inputConvertService = new InputConvertServiceImpl(validateService);
 
-        Assertions.assertThatThrownBy(()->inputConvertService.inputConvertRoundCount(input))
+        Assertions.assertThatThrownBy(() -> inputConvertService.inputConvertRoundCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
