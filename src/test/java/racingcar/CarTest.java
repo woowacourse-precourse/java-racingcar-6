ed45b5affcb2a331673f30a7ca30ade075d7891f
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 12:01:17 +0900

    [Test](CarTest) - TestCode 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,36 @@
+package racingcar;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class CarTest {
-  
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("TestCar");
+    }
+
+    @Test
+    void getName() {
+        assertEquals("TestCar", car.getName());
+    }
+
+
+    @Test
+    void distanceTest() {
+        car.setDistance("-");
+        car.setDistance("+");
+        assertEquals("-+", car.getDistance());
+    }
+
+    @Test
+    void countTest() {
+        car.setCount();
+        car.setCount();
+        assertEquals(2, car.getCount());
+    }
 }
