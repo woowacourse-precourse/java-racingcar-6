d2ebdccdb4338092268cb8391df3cf706686b130
노정완 <jeongwan.noh@gmail.com>
2023-10-31 22:21:23 +0900

    fix: 시도 횟수 범위 예외처리 로직 삭제

--- src/main/java/racingcar/validator/TryCountValidator.java
+++ src/main/java/racingcar/validator/TryCountValidator.java
@@ -6,14 +6,7 @@ public class TryCountValidator {
         try {
             Integer.parseInt(tryCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다 : 0에서 9사이의 숫자를 입력해주세요");
-        }
-    }
-
-    public void isNumberInRange(String tryCount) {
-        int tmpCount = Integer.parseInt(tryCount);
-        if (tmpCount < 0 || tmpCount > 9) {
-            throw new IllegalArgumentException("범위 내의 숫자가 아닙니다 : 0에서 9사이의 숫자를 입력해주세요");
+            throw new IllegalArgumentException("숫자가 아닙니다");
         }
     }
 }
