aa2bdb464a5c788f58e8b940dbdb96c61e58698d
oiler831 <qudwls831@gmail.com>
2023-10-31 14:26:09 +0900

    자바 코딩 스타일 및 결과랑 일치하게 수정
    자바 코딩 스타일에 맞게 띄어쓰기 등의 변경 및 '시도할 회수는 몇회인가요?' 후 입력값 입력 후 띄어쓰기 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -8,21 +8,21 @@ public class RacingGame {
     public static int tryNumber;
     public static int[] distance;
 
-    private RacingGame(){
+    private RacingGame() {
         throw new IllegalArgumentException();
     }
-    public static void startGame(){
+    public static void startGame() {
         inputCarName();
         inputTryNumber();
         playGame();
     }
-    private static void inputCarName(){
+    private static void inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameLine = Console.readLine();
         divideCarNameLine(carNameLine);
         isWrongInput(isCarNameOverFive());
     }
-    private static void divideCarNameLine(String carNameLine){
+    private static void divideCarNameLine(String carNameLine) {
         carName = carNameLine.split(",");
     }
     private static boolean isCarNameOverFive(){
@@ -41,27 +41,27 @@ public class RacingGame {
     private static void inputTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         String stringNumber = Console.readLine();
+        System.out.println();
         isCorrectNumber(stringNumber);
         tryNumber = Integer.parseInt(stringNumber);
     }
-    private static void isCorrectNumber(String stringNumber){
+    private static void isCorrectNumber(String stringNumber) {
         isWrongInput(isNotNumber(stringNumber));
         isWrongInput(isNotInRange(stringNumber));
     }
-    private static boolean isNotNumber(String stringNumber){
-        try{
+    private static boolean isNotNumber(String stringNumber) {
+        try {
             Integer.parseInt(stringNumber);
             return false;
         }
-        catch(NumberFormatException e){
+        catch (NumberFormatException e) {
             return true;
         }
     }
     private static boolean isNotInRange(String stringNumber){
-        int number = Integer.parseInt(stringNumber);
-        return (number <= 0);
+        return (Integer.parseInt(stringNumber) <= 0);
     }
-    private static void playGame(){
+    private static void playGame() {
         distance = new int[carName.length];
         System.out.println("실행 결과");
         while (tryNumber > 0) {
@@ -70,32 +70,30 @@ public class RacingGame {
         }
         findWinner();
     }
-    private static void checkCar(){
-        for (int i=0;i<carName.length;i++){
+    private static void checkCar() {
+        for (int i = 0; i < carName.length; i++) {
             runCar(i);
             printRacingSequence(i);
         }
         System.out.println();
     }
-    private static boolean isRandomValueOverFour(){
+    private static boolean isRandomValueOverFour() {
         return Randoms.pickNumberInRange(0,9) >= 4;
     }
     private static void runCar(int number){
         distance[number] += Boolean.compare(isRandomValueOverFour(), false);
     }
     private static void printRacingSequence(int number){
-        String sequence = carName[number]+" : ";
-        sequence += "-".repeat(distance[number]);
+        String sequence = carName[number] + " : " + "-".repeat(distance[number]);
         System.out.println(sequence);
     }
-    private static void findWinner(){
-        int maxValue = findMax();
-        printWinner(maxValue);
+    private static void findWinner() {
+        printWinner(findMax());
     }
-    private static int findMax(){
+    private static int findMax() {
         int maxValue = distance[0];
-        for(int value : distance){
-            if (maxValue < value){
+        for (int value : distance) {
+            if (maxValue < value) {
                 maxValue = value;
             }
         }
@@ -112,7 +110,7 @@ public class RacingGame {
         System.out.println(winner);
     }
     private static void appendCommas(StringBuilder winner) {
-        if(winner.charAt(winner.length()-1) != ' '){
+        if (winner.charAt(winner.length() - 1) != ' '){
             winner.append(", ");
         }
     }
