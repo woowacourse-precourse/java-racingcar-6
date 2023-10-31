2d284d9d1796abd2f68b91402cc09288138cbe8e
wnsgh12s <junho1273@gmail.com>
2023-10-31 22:42:10 +0900

    refactor: AttemptsNumber의 attempts 필드를 int로 변경

--- src/main/java/racingcar/domain/model/AttemptsNumber.java
+++ src/main/java/racingcar/domain/model/AttemptsNumber.java
@@ -1,4 +1,8 @@
 package racingcar.domain.model;
 
-public record AttemptsNumber(String attempts) {
+public record AttemptsNumber(int attempts) {
+
+    public AttemptsNumber(String attempts){
+        this(Integer.parseInt(attempts));
+    }
 }
