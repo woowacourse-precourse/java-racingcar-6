86ba433297528a459016c442819ac7e248a0b776
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 07:00:47 +0900

    test : 사용자 입력 라운드 숫자 테스트 코드

--- src/test/java/racingcar/domain/UserInputRoundNumberTest.java
+++ src/test/java/racingcar/domain/UserInputRoundNumberTest.java
@@ -1,22 +1,22 @@
-//package racingcar.domain;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//
-//class UserInputRoundNumberTest {
-//    private UserInputRoundNumber userInputRoundNumber;
-//    @BeforeEach
-//    void setUp(){
-//        userInputRoundNumber = new UserInputRoundNumber();
-//    }
-//    @Test
-//    void 사용자_입력_라운드_횟수_출력() {
-//        String userInput = "1";
-//        int result = userInputRoundNumber.createRoundNumber(userInput);
-//        assertThat(result).isEqualTo(1);
-//    }
-//
-//}
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class UserInputRoundNumberTest {
+    private UserInputRoundNumber userInputRoundNumber;
+    @BeforeEach
+    void setUp(){
+        userInputRoundNumber = new UserInputRoundNumber();
+    }
+    @Test
+    void 사용자_입력_라운드_횟수_출력() {
+        String userInput = "1";
+        int result = userInputRoundNumber.createRoundNumber(userInput);
+        assertThat(result).isEqualTo(1);
+    }
+
+}
