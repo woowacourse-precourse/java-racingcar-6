4df44d806554e17752cd7274025a80b31170dcb1
ehdwoKIM <kinbell19@gmail.com>
2023-11-01 01:44:58 +0900

    [feat] ValidatorTest 추가&메소드 분리

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
+import racingcar.util.Validator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -34,5 +36,62 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    public static class ValidatorTest {
+        private final Validator validator = new Validator();
+        private static final String ERROR_MESSAGE = "[ERROR]";
+
+        @Test
+        void 자동차이름_빈문자_입력() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput(""))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 시도횟수이름_빈문자_입력() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkNumberOfTrialInput(""))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 자동차이름_1글자미만() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,b,,c"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 자동차이름_5글자초과() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,b,avsdfe"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 자동차이름_공백() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a, ,c"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 자동차이름_중복() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,a"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
+
+        @Test
+        void 시도횟수_문자() {
+            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkNumberOfTrialInput("a"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE));
+        }
     }
 }
