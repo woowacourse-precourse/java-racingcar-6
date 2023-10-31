30bc6ba025e12a8d882165770ee732f13e02ba38
saebyeok2 <tomato8269@naver.com>
2023-10-31 17:23:42 +0900

    feat: 자동차 생성 기능 구현
    - 사용자가 입력한 이름을 기준으로 자동차를 생성하고 이름을 부여한다.
    - 생성된 자동차를 List에 저장하여 관리한다.

--- src/main/java/racingcar/controller/CarRaceGame.java
+++ src/main/java/racingcar/controller/CarRaceGame.java
@@ -1,5 +1,34 @@
 package racingcar.controller;
 
-public class CarRaceGame {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import racingcar.domain.Car;
+import racingcar.view.InputView;
 
+public class CarRaceGame {
+    private static CarRaceGame instance;
+    private final List<Car> cars;
+
+    private CarRaceGame() {
+        cars = new ArrayList<>();
+    }
+
+    public static CarRaceGame getInstance() {
+        if (instance == null) {
+            instance = new CarRaceGame();
+        }
+        return instance;
+    }
+
+    public void play() {
+        createCars(InputView.getCarNames());
+        
+    }
+
+    private void createCars(Set<String> carNames) {
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public Set<String> getCarNames() {
+    public static Set<String> getCarNames() {
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
         String rawString = Console.readLine();
 
