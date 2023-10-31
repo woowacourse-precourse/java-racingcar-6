f90c19c140a6bca51d7f0e1c649992a38ecb7925
Jung Yunho <a01049048063@gmail.com>
2023-10-31 18:38:16 +0900

    refactor(InputValidator): if문 내 조건문 함수처리

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -7,15 +7,23 @@ public class InputValidator {
     private static final String DELIMITER = ",";
     private static final String NUMBER_REGEX = "^[0-9]+$";
 
-    public void validateDelimiter(String input) {
-        if (!input.contains(DELIMITER)) {
+    public void validateDelimiter(final String input) {
+        if (isOneCar(input)) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_HAVE_DELIMITER.getMessage());
         }
     }
 
-    public void validateNumber(String input) {
-        if (!input.matches(NUMBER_REGEX)) {
+    public void validateNumber(final String input) {
+        if (isNotInputNumber(input)) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_AVAILABLE_STRING.getMessage());
         }
+    }
+
+    private static boolean isOneCar(String input) {
+        return !input.contains(DELIMITER);
+    }
+
+    private static boolean isNotInputNumber(String input) {
+        return !input.matches(NUMBER_REGEX);
     }
 }
