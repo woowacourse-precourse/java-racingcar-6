a0ff68d88949e954c64dba46349cf8e00ca5465d
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-10-31 20:51:13 +0900

    refactor(racingcar): clean out the unnecessary code and apply necessary access modifier

--- src/main/java/racingcar/domain/GameService.java
+++ src/main/java/racingcar/domain/GameService.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import racingcar.domain.Constants;
-
 public class GameService {
     UserInput userInput = new UserInput();
     MoveContainer moveContainer = new MoveContainer();
@@ -23,22 +21,10 @@ public class GameService {
 
         List<String> winnerList = getWinner(numMoveMap);
         systemOutput.printOverallResult(winnerList);
-//        System.out.println(Constants.CAR_NAME_INPUT_MESSAGE);
-//        List<String> carList = userInput.askCarList();
-//
-//        System.out.println(Constants.ATTEMP_INPUT_MESSAGE);
-//        int gameNumber = userInput.askNumberGame();
-//
-//        System.out.println(Constants.RESULT_OF_GAME);
-//        Map<String, Integer> numMoveMap = moveContainer.initMap(carList);
-//
-//        printMoveGameNumber(gameNumber, carList, numMoveMap);
-//
-//        List<String> winnerList = getWinner(numMoveMap);
-//        systemOutput.printOverallResult(winnerList);
+
     }
 
-    public void printMoveGameNumber(int gameNumber, List<String> carList, Map<String, Integer> numMoveMap){
+    private void printMoveGameNumber(int gameNumber, List<String> carList, Map<String, Integer> numMoveMap){
         for (int i = 0; i < gameNumber; i++) {
             numMoveMap = moveContainer.decideIncMove(carList, numMoveMap);
             systemOutput.printMoveResult(numMoveMap);
