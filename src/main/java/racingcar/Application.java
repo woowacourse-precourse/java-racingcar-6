6923e43dd549f00f112dcd840b7f6098517e9c88
= <=>
2023-10-31 22:58:27 +0900

    feat(Application): 메인 어플리케이션 설정
    - Application 클래스 내 메인 메서드 구현
    - Input, RacingCar, Output, CarState 클래스를 결합하여 레이싱 게임 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,12 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+
+        String carNames = input.inputCarName();
+        String tryCount = input.inputTryCount();
+
+        RacingCar game = new RacingCar(carNames, Integer.parseInt(tryCount));
+        game.startGame();
     }
 }
