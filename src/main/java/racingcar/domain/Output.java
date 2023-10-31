e95b190db810fdb4c13eec6156cb4ddac664fc6b
flame-possible <werq2003@naver.com>
2023-11-01 00:25:40 +0900

    feat: 출력 기능 완료

---
+++ src/main/java/racingcar/domain/Output.java
@@ -0,0 +1,47 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Output {
+
+    public void InputCarDes(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void InputGameNumDes(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+
+    public void CarsResult(List<Car> cars, List<Car> winner){
+        System.out.println("실행 결과");
+        for(Car c : cars){
+            CarResult(c);
+        }
+
+        WinnerResult(winner);
+    }
+
+    private void CarResult(Car car){
+        System.out.print(car.name + " : ");
+        for(int i = 0; i < car.cnt; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    private void WinnerResult(List<Car> cars){
+        System.out.print("최종 우승자 : ");
+
+        String result = "";
+
+        for(Car c : cars){
+            result += c.name;
+            result += ", ";
+        }
+        result.substring(0, result.length()-2);
+
+        System.out.println(result);
+    }
+
+}
