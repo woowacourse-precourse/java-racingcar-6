081544abd1affc6c2a44518b8f60229cf383d12d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-31 19:22:11 +0900

    feat: 입력 받은 시도 횟수 숫자로 변환하는 기능 및 숫자인지 검증하는 기능
    Integer로 반환받기 위해 Integer.parseInt가 아닌 Integer.valueOf 사용
    정수인지 판별하기 위해 NumberFormatException 사용
    시도 횟수 만큼 경주를 진행해야하므로 숫자로 변환

--- src/main/java/racingcar/CarRacingGame.java
+++ src/main/java/racingcar/CarRacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static racingcar.ErrorMessage.NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER;
 import static racingcar.PrintMessage.INPUT_NUMBER_OF_ATTEMPTS;
 import static racingcar.PrintMessage.INPUT_RACING_CAR_NAME;
 
@@ -14,7 +15,15 @@ public class CarRacingGame {
         String[] carNames = Console.readLine().split(RACING_CAR_NAME_SEPARATOR);
         RacingCars racingCars = RacingCars.from(carNames);
         System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
-        String numberOfAttempts = Console.readLine();
+        Integer numberOfAttempts = convertInputToInteger(Console.readLine());
+    }
+
+    private Integer convertInputToInteger(String input) {
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER);
+        }
     }
 
 }
---
+++ src/main/java/racingcar/ErrorMessage.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class ErrorMessage {
+
+    public static final String NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER = "시도 횟수는 숫자 형태여야 합니다. ";
+
+}
