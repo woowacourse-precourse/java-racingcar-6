fb2fb1f69eaefe22fdb36badac7edad286446fb3
JeonJW24 <tyui153@naver.com>
2023-11-01 01:26:30 +0900

    refactor(racingcar): 안 쓰는 import문 삭제, cars -> names 변수 이름 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,7 @@
 package racingcar;
 
-import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.Generator;
-import racingcar.domain.InvalidValue;
 import racingcar.domain.RaceCar;
 import racingcar.domain.UserInput;
 
@@ -16,10 +14,10 @@ public class Application {
         System.out.println("실행 결과");
         Generator generator = new Generator();
         while (moveNumber != 0) {
-            List<Integer> randomNumbers = generator.createRandomNumbers(raceCar.cars.size());
+            List<Integer> randomNumbers = generator.createRandomNumbers(raceCar.names.size());
             raceCar.forward(randomNumbers);
-            for (int i = 0; i < raceCar.cars.size(); i++) {
-                System.out.println(raceCar.cars.get(i) + " : " + raceCar.moveDistance.get(i));
+            for (int i = 0; i < raceCar.names.size(); i++) {
+                System.out.println(raceCar.names.get(i) + " : " + raceCar.moveDistance.get(i));
             }
             System.out.println();
             moveNumber--;
--- src/main/java/racingcar/domain/RaceCar.java
+++ src/main/java/racingcar/domain/RaceCar.java
@@ -2,21 +2,20 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class RaceCar {
-    public static List<String> cars = new ArrayList<>();
+    public static List<String> names = new ArrayList<>();
     public static List<String> moveDistance = new ArrayList<>();
 
     public RaceCar(List<String> carName) {
         for (int i = 0; i < carName.size(); i++) {
-            cars.add(carName.get(i));
+            names.add(carName.get(i));
             moveDistance.add("");
         }
     }
 
     public void forward(List<Integer> randomNumber) {
-        for (int i = 0; i < cars.size(); i++) {
+        for (int i = 0; i < names.size(); i++) {
             if (randomNumber.get(i) >= 4) {
                 moveDistance.set(i, moveDistance.get(i) + "-");
             }
@@ -33,9 +32,9 @@ public class RaceCar {
         }
         for (int i = 0; i < moveDistance.size(); i++) {
             if (winner.isEmpty() && max == moveDistance.get(i).length()) {
-                winner = cars.get(i);
+                winner = names.get(i);
             } else if (!winner.isEmpty() && max == moveDistance.get(i).length()) {
-                winner = winner + ", " + cars.get(i);
+                winner = winner + ", " + names.get(i);
             }
         }
         return winner;
