138f1bbe3559f899abcf95e6f964332f13c00f16
yhcho0405 <yhcho0405@kakao.com>
2023-11-01 00:19:36 +0900

    refactor: InputView validate매서드 반환값 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -29,14 +29,16 @@ public class Car {
 
     private void validateCarName(String name) {
         if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다..");
+            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
         }
+        
         if (name.trim().length() >= 5) {
             throw new IllegalArgumentException("자동차 이름은 5글자 이하 여야 합니다.");
         }
 
         Pattern pattern = Pattern.compile("[a-zA-Z0-9가-힣]*");
         Matcher matcher = pattern.matcher(name);
+
         if (!matcher.matches()) {
             throw new IllegalArgumentException("자동차 이름은 영문자, 숫자, 한글만 포함할 수 있습니다.");
         }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,28 +6,34 @@ public class InputView {
 
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return validateCarNamesInput(Console.readLine());
+
+        String carNames = Console.readLine();
+        validateCarNamesInput(carNames);
+
+        return carNames;
     }
 
     public static int getRounds() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return validateRoundsInput(Console.readLine());
+
+        String rounds = Console.readLine();
+        validateRoundsInput(rounds);
+
+        return Integer.parseInt(rounds);
     }
 
-    private static String validateCarNamesInput(String carNamesInput) {
+    private static void validateCarNamesInput(String carNamesInput) {
         if (carNamesInput == null || carNamesInput.trim().isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 하나 이상 입력해야 합니다.");
+            throw new IllegalArgumentException("경주에 참가할 자동차들을 입력해야 합니다.");
         }
-        return carNamesInput;
     }
 
-    private static int validateRoundsInput(String roundsInput) {
+    private static void validateRoundsInput(String roundsInput) {
         try {
             int rounds = Integer.parseInt(roundsInput);
             if (rounds <= 0) {
                 throw new IllegalArgumentException("라운드 수는 1 이상의 정수여야 합니다.");
             }
-            return rounds;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("라운드 수는 정수여야 합니다.");
         }
