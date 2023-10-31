04ce85dec2407167991dcaca00895a5d820573bd
jmj005 <jmj00005@naver.com>
2023-11-01 02:31:01 +0900

    refactor: ValidatorTest 코드를 CarsTest로 이동

--- src/test/java/racingcar/controller/ValidatorTest.java
+++ src/test/java/racingcar/controller/ValidatorTest.java
@@ -3,45 +3,11 @@ package racingcar.controller;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 public class ValidatorTest {
-    @ParameterizedTest
-    @MethodSource("generateInvalidCarNames")
-    void 이름_유효성_실패_테스트(List<String> carNames) {
-        assertThrows(IllegalArgumentException.class,
-                () -> Validator.validateCarNames(carNames));
-    }
-
-    @ParameterizedTest
-    @MethodSource("generateValidCarNames")
-    void 이름_유효성_성공_테스트(List<String> carNames) {
-        assertDoesNotThrow(() -> Validator.validateCarNames(carNames));
-    }
-
-    static Stream<Arguments> generateInvalidCarNames() {
-        return Stream.of(
-                Arguments.of(Arrays.asList("pobi", "pobi", "woni")),
-                Arguments.of(Arrays.asList("pobi", "jjjjun")),
-                Arguments.of(Arrays.asList("", ""))
-        );
-    }
-
-    static Stream<Arguments> generateValidCarNames() {
-        return Stream.of(
-                Arguments.of(Arrays.asList("pobi", "woni", "jun")),
-                Arguments.of(Arrays.asList("pobi", "shu")),
-                Arguments.of(Arrays.asList("jjjun", "pobi"))
-        );
-    }
-
     @ParameterizedTest
     @CsvSource({
             "d",
--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,2 +1,41 @@
-package racingcar.model;public class CarsTest {
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CarsTest {
+    @ParameterizedTest
+    @MethodSource("generateInvalidCarNames")
+    void 이름_유효성_실패_테스트(List<String> carNames) {
+        assertThrows(IllegalArgumentException.class,
+                () -> Cars.createCars(carNames));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateValidCarNames")
+    void 이름_유효성_성공_테스트(List<String> carNames) {
+        assertDoesNotThrow(
+                () -> Cars.createCars(carNames));
+    }
+
+    static Stream<Arguments> generateInvalidCarNames() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("pobi", "pobi", "woni")),
+                Arguments.of(Arrays.asList("pobi", "pobi", "pobi"))
+        );
+    }
+
+    static Stream<Arguments> generateValidCarNames() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("pobi", "woni", "jun")),
+                Arguments.of(Arrays.asList("pobi", "shu"))
+        );
+    }
 }
