e89f9fd354a91cb6185b023746ef57bb6e326ae8
ohhyeon2 <dhgus1235@gmail.com>
2023-10-31 11:39:20 +0900

    fix: 차량의 이름을 입력할 시 시도 회수 입력 전 문자열 바로 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,12 +16,12 @@ public class Application {
 class Game {
     public void init() {
         String carName = RacingGameView.inputCarName();
-        int tryCount = RacingGameView.inputTryCount();
 
         HashMap<String, String> car = RacingGameController.carNameAndForwardStatus(carName);
 
-        RacingGameView.gameResultMessage();
+        int tryCount = RacingGameView.inputTryCount();
 
+        RacingGameView.gameResultMessage();
         for (int i = 0; i < tryCount; i++) {
             RacingGameController.moveForward(car);
             RacingGameView.forwardResult(car);
