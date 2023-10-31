03d618acc74a62c16a83f66ca743f2fc39cec84f
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:15:46 +0900

    Feat: Car중에서 최대전진거리를 계산하는 메서드 추가

--- src/main/java/racingcar/util/Result.java
+++ src/main/java/racingcar/util/Result.java
@@ -1,2 +1,13 @@
-package racingcar.util;public class Result {
+package racingcar.util;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public class Result {
+    public static int checkMaxDistance(List<Car> parkingLot){
+        int maxDistance = -1;
+        for(int i=0;i<parkingLot.size();i++)
+            maxDistance = Math.max(parkingLot.get(i).getForwardDistance(), maxDistance);
+        return maxDistance;
+    }
 }
