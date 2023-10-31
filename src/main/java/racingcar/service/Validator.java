7bf11a03fd980a3ab1865374f2b4c7d3d175115b
Seyeon <wjdtpdus828@naver.com>
2023-10-31 21:49:54 +0900

    feat(Validator): 자동차 이름에 대한 오류 처리를 진행하는 validateCarNames 메소드 구현

--- src/main/java/racingcar/service/Validator.java
+++ src/main/java/racingcar/service/Validator.java
@@ -1,6 +1,16 @@
 package racingcar.service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
+    TypeConverter typeConverter = new TypeConverter();
+    public void validateCarNames(List<String> input){
+        validateLength(input);
+        validateSpace(input);
+        validateDuplication(input);
+    }
 
 
 }
