99078df70cc9e9dd2cc01db68b6ba53e487fe8bd
hoyeon <highoption@naver.com>
2023-10-31 15:58:32 +0900

    feat(CarNameValidator) : 중복 이름을 검증하는 isDuplicateName 생성
    자동차의 이름이 중복되면 안되기 때문에 예외를 생성

--- src/main/java/racingcar/model/CarNameValidator.java
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarNameValidator {
     private final List<Car> carNameList;
@@ -15,6 +17,17 @@ public class CarNameValidator {
                 .findAny()
                 .ifPresent(car -> {
                     throw new IllegalArgumentException("자동차 이름은 5글자가 넘으면 안됩니다.");
+                });
+    }
+
+    private void isDuplicateName() {
+        Set<String> checkName = new HashSet<>();
+        carNameList.stream()
+                .map(Car::getName)
+                .filter(name -> !checkName.add(name))
+                .findAny()
+                .ifPresent(name -> {
+                    throw new IllegalArgumentException("자동차의 이름이 중복되면 안됩니다.");
                 });
     }
 }
