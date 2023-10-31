8d5a15aea37652addecd0880cd168399a1e0648c
shAn-kor <kjn0406@naver.com>
2023-10-31 14:21:23 +0900

    WINNER_MESSAGE, EACH_RACER_RESULT 스트링 포맷 사용하도록 변경
    가독성 향상

--- src/main/java/racingcar/common/Config.java
+++ src/main/java/racingcar/common/Config.java
@@ -4,11 +4,15 @@ public class Config {
     public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String START_RACING_MESSAGE = "실행 결과";
-    public static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public static final String WINNER_MESSAGE = "최종 우승자 : %s";
+    public static final String EACH_RACER_RESULT = "%s : %s \n";
+
     public static final String SPLIT_REGEXP = ",";
     public static final String RACING_COUNT_CHARACTER = "-";
+
     public static final Integer MAX_NAME_SIZE = 5;
     public static final Integer CAN_FORWARD_NUMBER = 4;
     public static final Integer SMALL_RANDOM_RANGE_NUMBER = 0;
-    public static final Integer BIG_RANDOM_RANGE_NUMBER = 0;
+    public static final Integer BIG_RANDOM_RANGE_NUMBER = 9;
 }
