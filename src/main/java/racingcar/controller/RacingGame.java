cdc05b0a4770bfd370708963761313401976f7f9
NyaongKAI <yukihusiru18@gmail.com>
2023-11-01 08:56:15 +0900

    최종 우승자 메서드 생성 및 구현

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -2,32 +2,31 @@ package racingcar.controller;
 
 import racingcar.domain.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static racingcar.domain.CarList.getCarList;
 import static racingcar.domain.TryNum.getTryNum;
 import static racingcar.view.InputView.requestCarNamesText;
 import static racingcar.view.InputView.requestTryNumText;
-import static racingcar.view.OutputView.printGameResult;
-import static racingcar.view.OutputView.printCarMovement;
+import static racingcar.view.OutputView.*;
 
 public class RacingGame {
     public void RunGame(){
         requestCarNamesText();
-
         List<Car> carList = getCarList();
 
         requestTryNumText();
-
         int count = getTryNum();
 
         System.out.println();
-
         printGameResult();
 
         for(int i = 0; i < count; i++){
             carMovementOfGame(carList);
         }
+
+        printStringListJoinComma(getWinnerList(carList));
     }
 
     private void carMovementOfGame(List<Car> carList){
@@ -36,5 +35,31 @@ public class RacingGame {
             printCarMovement(car.getCarName(), car.getPosition());
         }
         System.out.println();
+    }
+
+    private List<String> getWinnerList(List<Car> carList) {
+        List<String> winner = new ArrayList<>();
+
+        for (Car car : carList){
+            if (car.getPosition() == getGreatPosition(carList)){
+                winner.add(car.getCarName());
+            }
+        }
+
+        return winner;
+    }
+
+    private int getGreatPosition(List<Car> carList) {
+        int greatPosition = 0;
+
+        for (Car car : carList){
+            int position = car.getPosition();
+
+            if (position >= greatPosition){
+                greatPosition = position;
+            }
+        }
+
+        return greatPosition;
     }
 }
