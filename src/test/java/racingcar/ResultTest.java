a9ae5f86f5a8e89b8b8cc4258bee2df4c19ed205
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 22:50:58 +0900

    [Test](ResultTest) - TestCode 추가

--- src/test/java/racingcar/ResultTest.java
+++ src/test/java/racingcar/ResultTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ResultTest {
-  
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+public class ResultTest {
+
+    private Result result;
+    private ArrayList<Car> carArrayList;
+    private String winner;
+
+    @Test
+    void testFinalWinner() {
+
+        carArrayList = new ArrayList<>();
+        carArrayList.add(new Car("pobi"));
+        carArrayList.add(new Car("MJ"));
+        carArrayList.add(new Car("tjwls"));
+
+        for (Car car : carArrayList) {
+            car.setCount();
+            car.setDistance("-");
+        }
+        carArrayList.get(1).setCount();
+        carArrayList.get(2).setCount();
+
+        result = new Result(carArrayList);
+        winner = result.finalWinner();
+
+        assertThat(winner).isEqualTo("MJ, tjwls");
+    }
+
+
 }
