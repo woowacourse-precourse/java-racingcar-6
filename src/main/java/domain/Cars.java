56c8e4ed5483f59f7d98fa6a9699df69ed29d9aa
dd <12202340@inha.edu>
2023-10-31 19:34:29 +0900

    Style: 불필요한 import 삭제

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
