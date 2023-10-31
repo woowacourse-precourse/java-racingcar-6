44ee80c1bd20105d94ebc7b8b57e7dd540b0f8f5
JiEung2 <https://github.com/JiEung2>
2023-10-31 20:10:20 +0900

    Add: CarFactory for문 제거
    for문 stream으로 변경

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -1,14 +1,12 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarFactory {
     public static List<Car> createCars(List<String> names) {
-        List<Car> cars = new ArrayList<>();
-        for (String name : names) {
-            cars.add(new Car(name));
-        }
-        return cars;
+        return names.stream()
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
     }
 }
