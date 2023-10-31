fcb2d19c82e92a978b91730f7869143e7f834e59
Hyun Woo <hwsa10041@gmail.com>
2023-10-31 18:07:47 +0900

    feat : main() 메서드 자동차 생성 부분 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.model.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        List<Car> cars = createCars(racingController, inputView);
     }
+
+    private static List<Car> createCars(RacingController racingController, InputView inputView) {
+        String[] carNames = inputView.inputRacingCar();
+        return racingController.createCarList(carNames);
+    }
+
 }
