9a1635d146b8bc9f29e049d47ffbe5d5cd9cd050
idingg <idingg@gmail.com>
2023-10-31 11:04:19 +0900

    test: 수정 전진_정지_예제_동일_조건
    시스템 간 개행문자 차이로 인한 테스트 오류 방지

--- src/test/java/racingcar/ApplicationTest_add.java
+++ src/test/java/racingcar/ApplicationTest_add.java
@@ -8,42 +8,44 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+
 class ApplicationTest_add extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
     void 전진_정지_예제_동일_조건() {
-        String exampleOutput = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-                + "시도할 회수는 몇회인가요?\n"
-                + "\n"
-                + "실행 결과\n"
-                + "pobi : -\n"
-                + "woni : \n"
-                + "jun : -\n"
-                + "\n"
-                + "pobi : --\n"
-                + "woni : -\n"
-                + "jun : --\n"
-                + "\n"
-                + "pobi : ---\n"
-                + "woni : --\n"
-                + "jun : ---\n"
-                + "\n"
-                + "pobi : ----\n"
-                + "woni : ---\n"
-                + "jun : ----\n"
-                + "\n"
-                + "pobi : -----\n"
-                + "woni : ----\n"
-                + "jun : -----\n"
-                + "\n"
-                + "최종 우승자 : pobi, jun";
+        List<String> exampleOutput = List.of(
+                "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                "시도할 회수는 몇회인가요?",
+                "실행 결과",
+                "pobi : -",
+                "woni : ",
+                "jun : -",
+                "",
+                "pobi : --",
+                "woni : -",
+                "jun : --",
+                "",
+                "pobi : ---",
+                "woni : --",
+                "jun : ---",
+                "",
+                "pobi : ----",
+                "woni : ---",
+                "jun : ----",
+                "",
+                "pobi : -----",
+                "woni : ----",
+                "jun : -----",
+                "",
+                "최종 우승자 : pobi, jun");
 
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni,jun", "5");
-                    assertThat(output()).isEqualTo(exampleOutput);
+                    assertThat(output()).containsSubsequence(exampleOutput);
                 },
                 MOVING_FORWARD, STOP, MOVING_FORWARD,
                 MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
