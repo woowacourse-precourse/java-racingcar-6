dadd36a341f31e7f4c98f0fc4b82709b6b31c1a4
JongMinCh0i <wonderland314592@gmail.com>
2023-11-01 03:08:33 +0900

    refactor(InputToCarList): 매직 리터럴 제거

--- src/main/java/racingcar/domain/InputToCarList.java
+++ src/main/java/racingcar/domain/InputToCarList.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class InputToCarList {
     private static final String NO_PLAYERS_PARTICIPATE = "모집 된 선수가 없습니다.";
+    private static final String USER_DEFAULT_DELIMITER = ",";
 
     private static class SingletonHelper {
         private static final InputToCarList INSTANCE = new InputToCarList();
@@ -19,7 +20,7 @@ public class InputToCarList {
     }
 
     public List<Car> askCarName(final String input) {
-        List<Car> cars = Arrays.stream(input.split(","))
+        List<Car> cars = Arrays.stream(input.split(USER_DEFAULT_DELIMITER))
                 .map(Car::new)
                 .collect(Collectors.toList());
         if (cars.isEmpty()) {
