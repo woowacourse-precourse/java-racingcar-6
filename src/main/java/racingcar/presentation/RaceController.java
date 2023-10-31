bfbaa276486c4bcd22c53297cb3c53d1122c12f4
MingyeomKim <mingyum119@naver.com>
2023-10-31 22:57:20 +0900

    feat(RaceController): 총 시도 횟수만큼 자동차들을 이동하는 함수를 호출
    totalCount만큼 순회하며 RaceService에서 구현한 playRace를 호출한다.

--- src/main/java/racingcar/presentation/RaceController.java
+++ src/main/java/racingcar/presentation/RaceController.java
@@ -7,6 +7,8 @@ import racingcar.view.OutputView;
 
 public class RaceController {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TOTAL_COUNT_REQUEST_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
     private final OutputView outputView;
     private final InputView inputView;
     private final RaceService raceService;
@@ -20,7 +22,16 @@ public class RaceController {
     public void startRace() {
         outputView.printlnMessage(CAR_NAME_REQUEST_MESSAGE);
         Cars cars = raceService.generateCars(inputView.readCarNames());
+        outputView.printlnMessage(TOTAL_COUNT_REQUEST_MESSAGE);
         Integer count = raceService.generateCount(inputView.readInput());
-        raceService.startRace(cars, count);
+        play(cars, count);
+    }
+
+    private void play(Cars cars, Integer totalCount) {
+        outputView.printlnMessage(RESULT_MESSAGE);
+        for (int count = 0; count < totalCount; count++) {
+            raceService.playRace(cars);
+            outputView.printlnMessage(cars.toString());
+        }
     }
 }
