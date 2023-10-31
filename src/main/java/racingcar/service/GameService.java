1bb81e0e77dc13cfadd44b23536cc7aeaba51fe8
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 15:35:17 +0900

    refactor(service): getter의 사용으로 인한 코드 수정

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,12 +1,11 @@
 package racingcar.service;
 
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.CarNames;
 import racingcar.domain.GamePlayer;
-import racingcar.domain.GameRank;
 import racingcar.domain.GameWinner;
-import racingcar.dto.CarNames;
-import racingcar.dto.MoveCount;
-import racingcar.view.OutputView;
 
 /*
  *   racingCar게임의 기능들을 담당
@@ -14,46 +13,22 @@ import racingcar.view.OutputView;
 
 public class GameService {
 
-    //    public GamePlayer createGamePlayer(List<String> carNames) {
-//        GamePlayer gamePlayer = new GamePlayer();
-////        GamePlayer gamePlayer = GamePlayer.create(initializeCars());
-//        addCarsToGamePlayer(gamePlayer, initializeCars(carNames));
-//        return gamePlayer;
-//    }
-//
-//    private void addCarsToGamePlayer(GamePlayer gamePlayer, List<Car> cars) {
-//        for (Car car : cars) {
-//            gamePlayer.addCar(car);
-//        }
-//    }
-//
-//    private List<Car> initializeCars(List<String> carNames) {
-//        List<Car> cars = new ArrayList<>();
-//        for (String carName : carNames) {
-//            cars.add(new Car(carName));
-//        }
-//        return cars;
-//    }
     public GamePlayer createGamePlayer(CarNames carNames) {
         return GamePlayer.from(carNames);
     }
 
-    public void moveCarsByCount(GamePlayer gamePlayer, MoveCount moveCount) {
+    public GameWinner createGameWinner(GamePlayer gamePlayer) {
+        List<Car> cars = gamePlayer.getCars();
+        int maxMoveDistance = gamePlayer.getMaxMoveDistance();
+        List<Car> winningCars = cars.stream()
+                .filter(car -> car.getMoveDistance() == maxMoveDistance)
+                .toList();
+        return GameWinner.from(winningCars);
+    }
+
+
+    public void moveCars(GamePlayer gamePlayer) {
         //OutputView.beforeMoveMessage() 로 하는 것이 더 가독성이 좋은가
-        OutputView.beforeMove();
-        for (int i = 0; i < moveCount.getMoveCount(); i++) {
-            gamePlayer.moveCars();
-        }
+        gamePlayer.moveCars();
     }
-
-
-    public GameRank createGameRank(GamePlayer gamePlayer) {
-        return gamePlayer.createGameRank();
-    }
-
-
-    public GameWinner createGameWinner(GameRank gameRank) {
-        return gameRank.createGameWinner();
-    }
-
 }
