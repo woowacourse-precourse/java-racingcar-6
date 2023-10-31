07263c1f953ea2bea86967beaee33a25e3d914cd
yyh <woyunkr@gmail.com>
2023-10-31 22:35:56 +0900

    사용자 입력받기 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
+        String names = Console.readLine();
+        moreThanFiveLetter(names);
+        divideNames(names);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+
+    }
+
+    static String [] divideNames(String names) {
+        String [] carNames = names.split(",");
+        return carNames;
+    }
+
+    static void moreThanFiveLetter(String names) {
+        String [] carNames = names.split(",");
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
