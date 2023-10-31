a677b1e853e9516fcb096123b5e3b735676a7064
donghyun <kdhdh001@naver.com>
2023-11-01 01:27:42 +0900

    feat: 자동차 이름이 비어있는 경우 테스트 구현

---
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -0,0 +1,18 @@
+package racingcar.validation;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class CarNameValidatorTest {
+    @Test
+    void 자동차_이름이_비어있는_경우_검증() {
+        String input = ",car2,car3";
+        List<String> carNames = Arrays.asList(input.split(","));
+        assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름이 공백이면 안됩니다.");
+    }
+}
