a67d691d03bd6e88297918238271cfb48c3d8b99
ddohyeong <dohyeong7026@gmail.com>
2023-10-31 14:31:19 +0900

    feat(controller) : controller 패키지 이동
    프로젝트 구조에 맞게 domain.controller 에서 java.controller 로 변경

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,4 +1,4 @@
-package domain.controller;
+package controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- src/test/java/domain/RacingGameTest.java
+++ src/test/java/domain/RacingGameTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import domain.controller.RacingGameController;
+import controller.RacingGameController;
 
 public class RacingGameTest {
 	@Test
