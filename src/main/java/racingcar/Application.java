f390c9ecd40cd7a9727c6b959ab43ba8874bc421
pgd <rudeh1253@gmail.com>
2023-10-31 16:50:18 +0900

    feat: main 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.game.Game;
+import racingcar.io.InputAdaptor;
+import racingcar.io.InputReceiver;
+import racingcar.io.Parser;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(new InputAdaptor(new InputReceiver(), new Parser()));
+        game.inputCarNames();
+        game.inputNumOfTries();
+        game.execute();
     }
 }
