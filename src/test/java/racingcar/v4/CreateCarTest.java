cbde1f845af1f0568517d67c81c0534074483935
권장순 <jsoonworld@gmail.com>
2023-10-31 23:03:31 +0900

    Test(racingcar) : 입력 값에 쉼표가 없는 경우 예외가 발생하는 단위 테스트 코드 구현 및 통과

--- src/test/java/racingcar/v4/CreateCarTest.java
+++ src/test/java/racingcar/v4/CreateCarTest.java
@@ -32,4 +32,12 @@ class CreateCarTest {
                 .hasMessage("자동차 이름은 1자 이상이어야 합니다.");
     }
 
+    @Test
+    void 자동차_이름에_쉼표가_없는_경우_예외가_발생한다() {
+        CreateCar createCar = new CreateCar();
+        assertThatThrownBy(() -> createCar.splitCarNames("페라리 벤츠"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름을 쉼표로 구분해주세요.");
+    }
+
 }
