8d7951cf800f940d750404f80a0a2e81210ecb65
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2023-10-31 21:52:23 +0900

    feat: 입력받은 이름으로 Car 객체를 생성하고 저장하는 기능

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -5,25 +5,38 @@ import static racingcar.view.GameView.inputCarName;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.model.Game;
 import racingcar.view.GameView;
 
 public class GameController {
     private Game model;
     private GameView view;
-    public GameController(Game model, GameView view){
+
+    public GameController(Game model, GameView view) {
         this.model = model;
         this.view = view;
     }
 
-    public void startGame(){
+    public void startGame() {
         String input = inputCarName();
         List<String> carNames = splitCarNames(input);
         System.out.println(carNames);
+
+        List<Car> cars = createCarObject(carNames);
+        model.setCars(cars);
     }
 
-    private List<String> splitCarNames(String input){
+    private List<String> splitCarNames(String input) {
         return new ArrayList<>(Arrays.asList(input.split(",")));
     }
 
+    private List<Car> createCarObject(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            Car car = new Car(carName, 0);
+            cars.add(car);
+        }
+        return cars;
+    }
 }
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,11 @@
 package racingcar.model;
 
 public class Car {
+    private String name;
+    private int currentLocation;
+
+    public Car(String name, int currentLocation){
+        this.name=name;
+        this.currentLocation=currentLocation;
+    }
 }
