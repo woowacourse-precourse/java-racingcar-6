1527158a4ebaf0755a6cc1e447b7b5d9e4618e1c
dmdrk1414 <seungchan141414@gmail.com>
2023-10-31 22:38:31 +0900

    [Refactor(racing)] 상수 변경

--- src/main/java/racingcar/domain/CreateRandomNum.java
+++ src/main/java/racingcar/domain/CreateRandomNum.java
@@ -3,8 +3,8 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class CreateRandomNum {
-    private static final int START_NUM = 0;
-    private static final int END_NUM = 9;
+    private static final int START_RAN_NUM = 0;
+    private static final int END_RAN_NUM = 9;
 
     /**
      * 0에서 9까지의 정수 중 한 개의 정수 반환.
@@ -12,6 +12,6 @@ public class CreateRandomNum {
      * @return
      */
     public Integer createRanOneNum() {
-        return Randoms.pickNumberInRange(START_NUM, END_NUM);
+        return Randoms.pickNumberInRange(START_RAN_NUM, END_RAN_NUM);
     }
 }
