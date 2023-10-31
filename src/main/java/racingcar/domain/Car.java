37f2a5e75eed0d78f50cc12b909b6c6115c9bc32
이정민 <jmlee2705@gmail.com>
2023-11-01 05:05:29 +0900

    feat: Car 스켈레톤 코드 작성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+public class Car {
+
+    private int position = 0;
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void printName() {
+        
+    }
+
+}
