d6076fc1d92adf4e2d7daa2d1506005c96831c28
Youth <boysoeng@naver.com>
2023-11-01 04:41:20 +0900

    feat: 메인 게임 진행 기능 추가

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,13 @@
+package racingcar.controller;
+
+import racingcar.domain.Game;
+
+public class GameController {
+
+    private static final Game game = new Game();
+    public static void startGame(){
+        game.raceSetting();
+        game.race();
+        game.declareWinner();
+    }
+}
