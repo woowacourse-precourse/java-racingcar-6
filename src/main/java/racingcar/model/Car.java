c5f147486df3fb76e868ea4d21d55c057521b9d1
dabin <db021129@naver.com>
2023-10-31 16:47:02 +0900

    feat: Car 작성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,13 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
+
+    private List<String> carName = new ArrayList<String>();
+
+    Car(List<String> carName) {
+        this.carName = carName;
+    }
 }
