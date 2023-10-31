76a76f052a4d5b9fe7575552985edefc9727f01a
jy3026 <jy_3026@naver.com>
2023-10-31 21:41:02 +0900

    fix : 배열 대신 컬렉션 사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,26 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        String[] cars = carNameInput();// 자동차 이름 입력 받기
-        int retryCount = getRetryCount();// 시도 횟수 입력 받기
-        String[] results = resultArray(cars); // 결과값 배열 생성
+        List<String> cars = carNameInput();
+        int retryCount = getRetryCount();
+        List<String> results = resultArray(cars); // 결과값 배열 생성
 
         System.out.println();
         System.out.println("실행 결과");
 
-        playGame(cars, retryCount, results); // 게임 시작
+        playGame(cars, retryCount, results);
 
-        determineWinners(cars, results); // 우승자 출력
+        determineWinners(cars, results);
     }
 
-    private static String[] carNameInput() {
+    private static List<String> carNameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        String[] cars = input.split(",");
+        List<String> cars = Arrays.asList(input.split(","));
 
         for (String car : cars) {
             String carName = car.trim();
@@ -47,42 +48,41 @@ public class Application {
         }
     }
 
-    private static String[] resultArray(String[] cars) {
-        String[] result = new String[cars.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = "";
+    private static List<String> resultArray(List<String> cars) {
+        List<String> results = new ArrayList<>(cars.size());
+        for (int i = 0; i < cars.size(); i++) {
+            results.add("");
         }
-
-        return result;
+        return results;
     }
 
-    private static void playGame(String[] cars, int retryCount, String[] results) {
+    private static void playGame(List<String> cars, int retryCount, List<String> results) {
         for (int i = 0; i < retryCount; i++) {
-            updateCarResults(cars, results); // 이동 결과 출력
+            updateCarResults(cars, results);
             System.out.println();
         }
     }
 
-    private static void updateCarResults(String[] cars, String[] results) {
-        for (int i = 0; i < cars.length; i++) {
+    private static void updateCarResults(List<String> cars, List<String> results) {
+        for (int i = 0; i < cars.size(); i++) {
             int randomNumber = Randoms.pickNumberInRange(0, 9);
-            if (randomNumber >= 4) results[i] += "-";
-            System.out.println(cars[i] + " : " + results[i]);
+            if (randomNumber >= 4) results.set(i, results.get(i) + "-");
+            System.out.println(cars.get(i) + " : " + results.get(i));
         }
     }
 
-    private static void determineWinners(String[] cars, String[] results) {
+    private static void determineWinners(List<String> cars, List<String> results) {
         int maxDistance = 0;
         List<String> winners = new ArrayList<>();
 
-        for (int i = 0; i < cars.length; i++) {
-            int distance = results[i].length();
+        for (int i = 0; i < cars.size(); i++) {
+            int distance = results.get(i).length();
             if (distance > maxDistance) {
                 maxDistance = distance;
                 winners.clear();
-                winners.add(cars[i]);
+                winners.add(cars.get(i));
             } else if (distance == maxDistance) {
-                winners.add(cars[i]);
+                winners.add(cars.get(i));
             }
         }
 
