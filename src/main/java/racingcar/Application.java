de327ad48f98486722738173eacf1072b9e8926b
hanriverkitty <danieltls@naver.com>
2023-11-01 00:59:38 +0900

    기능목록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         InputUser inputUser = new InputUser();
-        System.out.println(inputUser.getNumber());
+
 
         // TODO: 프로그램 구현
     }
