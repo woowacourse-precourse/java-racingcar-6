fcff630fbeb231dda0f40064a2d57cdd96908fc3
cousim46 <wjdghldns24@naver.com>
2023-10-31 22:48:20 +0900

    fix : PrintService와 의존성 분리를 위해 메서드 반환타입 설정

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -2,24 +2,22 @@ package racingcar.domain;
 
 import java.util.List;
 import racingcar.RandomMovementLimitGenerator;
-import racingcar.output.PrintService;
 
 public class Referee {
 
-    public static void announceWinner(List<Car> cars) {
+    public static List<String> determineWinner(List<Car> cars) {
         int max = cars.stream().mapToInt(Car::getStraightCount).max()
             .orElseThrow(IllegalArgumentException::new);
-        List<String> list = cars.stream().filter(car -> max == car.getStraightCount()).map(Car::getName)
+        return cars.stream().filter(car -> max == car.getStraightCount()).map(Car::getName)
             .toList();
-        PrintService.result(list);
     }
 
-    public static void report(List<Car> cars) {
+    public static List<Car> report(List<Car> cars) {
         cars.forEach(car -> {
             int value = RandomMovementLimitGenerator.value();
             car.moveStraight(value);
-            PrintService.show(car);
         });
+        return cars;
     }
 
 }
