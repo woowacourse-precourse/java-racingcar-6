bacee873a0606f629c152fcdc0cac30e87978e04
동원 <ai06021@gmail.com>
2023-11-01 03:13:46 +0900

    feat : 이름 길이 확인

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,12 +1,27 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
-    public List<String> Cars() {
+    public List<String> Name() {
         String console = Console.readLine();
         List<String> list = List.of(console.split(","));
+
+        for (String s : list) {
+            NameLengthCheck(s);
+        }
         return list;
+    }
+
+    private void NameLengthCheck(String name) {
+        try {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
