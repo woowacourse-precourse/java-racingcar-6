c4e944d4dca8015fa5ad305e14c69220e142f6c9
noeyodeel <sko8044@gmail.com>
2023-10-31 15:59:17 +0900

    style : 코드 스타일 수정
    코드 스타일 수정함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,23 +3,21 @@ package racingcar;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-//    자동차 이름 입력
-      CarName car = new CarName();
-      car.inputCarNames();
+    // 자동차 이름 입력
+    CarName car = new CarName();
+    car.inputCarNames();
+
+    // 실행할 횟수 입력
+    AttemptNumber attempt = new AttemptNumber();
+    int number = attempt.Attempt();
+
+    List<String> savedCarNames = car.getCarNames();
+    MoveResult move = new MoveResult();
+    move.move(savedCarNames, number);
 
 
-      
-//      실행할 횟수 입력
-      AttemptNumber attempt = new AttemptNumber();
-      int number = attempt.Attempt();
-      
-      List<String> savedCarNames = car.getCarNames();
-      MoveResult move = new MoveResult();
-      move.move(savedCarNames,number);
-      
-      
-      
-    }
+
+  }
 }
--- src/main/java/racingcar/MoveResult.java
+++ src/main/java/racingcar/MoveResult.java
@@ -7,62 +7,62 @@ import java.util.Map;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class MoveResult {
-    public void move(List<String> carNames, int attemptNumber) {
-        System.out.println("실행 결과");
+  public void move(List<String> carNames, int attemptNumber) {
+    System.out.println("실행 결과");
 
-        Map<String, String> results = new HashMap<>();
-        for (String name : carNames) {
-            results.put(name, "");
-        }
-
-        for (int i = 0; i < attemptNumber; i++) {
-            for (String name : carNames) {
-                String currentResult = results.get(name);
-                results.put(name, currentResult + generateResult());
-            }
-            printResults(results);
-        }
-        List<String> winners = determineWinners(results);
-        printWinners(winners);
+    Map<String, String> results = new HashMap<>();
+    for (String name : carNames) {
+      results.put(name, "");
     }
 
-    private String generateResult() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            return "-";
-        }
-        return "";
-    }
-
-    private void printResults(Map<String, String> results) {
-        for (Map.Entry<String, String> entry : results.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-        System.out.println();
-    }
-    
-    private List<String> determineWinners(Map<String, String> results) {
-      int maxDistance = 0;
-      List<String> winners = new ArrayList<>();
-      for (Map.Entry<String, String> entry : results.entrySet()) {
-          int distance = entry.getValue().length();
-          if (distance > maxDistance) {
-              maxDistance = distance;
-              winners.clear();
-              winners.add(entry.getKey());
-          } else if (distance == maxDistance) {
-              winners.add(entry.getKey());
-          }
+    for (int i = 0; i < attemptNumber; i++) {
+      for (String name : carNames) {
+        String currentResult = results.get(name);
+        results.put(name, currentResult + generateResult());
       }
-      return winners;
+      printResults(results);
+    }
+    List<String> winners = determineWinners(results);
+    printWinners(winners);
+  }
+
+  private String generateResult() {
+    if (Randoms.pickNumberInRange(0, 9) >= 4) {
+      return "-";
+    }
+    return "";
+  }
+
+  private void printResults(Map<String, String> results) {
+    for (Map.Entry<String, String> entry : results.entrySet()) {
+      System.out.println(entry.getKey() + " : " + entry.getValue());
+    }
+    System.out.println();
+  }
+
+  private List<String> determineWinners(Map<String, String> results) {
+    int maxDistance = 0;
+    List<String> winners = new ArrayList<>();
+    for (Map.Entry<String, String> entry : results.entrySet()) {
+      int distance = entry.getValue().length();
+      if (distance > maxDistance) {
+        maxDistance = distance;
+        winners.clear();
+        winners.add(entry.getKey());
+      } else if (distance == maxDistance) {
+        winners.add(entry.getKey());
+      }
+    }
+    return winners;
   }
 
   private void printWinners(List<String> winners) {
-      System.out.print("최종 우승자 : ");
-      for (int i = 0; i < winners.size(); i++) {
-          System.out.print(winners.get(i));
-          if (i < winners.size() - 1) {
-              System.out.print(", ");
-          }
+    System.out.print("최종 우승자 : ");
+    for (int i = 0; i < winners.size(); i++) {
+      System.out.print(winners.get(i));
+      if (i < winners.size() - 1) {
+        System.out.print(", ");
       }
+    }
   }
 }
