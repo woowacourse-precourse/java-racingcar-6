284da06de75c4d878fe76c253d10cc1a4d897a42
eunjin <12191804@inha.edu>
2023-11-01 00:23:49 +0900

    feat: 자동차 객체 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
