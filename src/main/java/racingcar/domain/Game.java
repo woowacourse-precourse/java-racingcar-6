fe8b8197558de0d730b01c0142a4f2ab1943480e
YoungJun-L <dudwns328@naver.com>
2023-10-31 19:33:37 +0900

    feat: 게임 시작 기능 구현

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+public class Game {
+    public Game() {
+    }
+
+    public void start() {
+    }
+}
---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+    @Test
+    void start() {
+    }
+}
