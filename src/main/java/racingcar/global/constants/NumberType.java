663706eaadda262e4e049025eb84c78af7f189c7
MingyeomKim <mingyum119@naver.com>
2023-10-31 14:28:26 +0900

    feat(NumberType): 무작위 숫자를 사용하기 위해 필요한 상수 정의
    무작위 숫자의 범위와 전진할 수 있는 최솟값에 대한 상수를 Enum 클래스에 정의한다.

--- src/main/java/racingcar/global/constants/NumberType.java
+++ src/main/java/racingcar/global/constants/NumberType.java
@@ -3,7 +3,10 @@ package racingcar.global.constants;
 public enum NumberType {
     MAX_LENGTH_OF_NUMBER(5),
     MIN_CAR_SIZE(2),
-    MIN_RACE_COUNT(1);
+    MIN_RACE_COUNT(1),
+    MIN_RANDOM_NUMBER(0),
+    MAX_RANDOM_NUMBER(9),
+    MIN_FORWARD_THRESHOLD(4);
 
     private final Integer value;
 
