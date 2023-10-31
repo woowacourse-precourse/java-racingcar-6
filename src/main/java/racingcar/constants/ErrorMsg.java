5f9370cdc60069c35b5185aaa0c9f975a07c4837
Kohyeonjeong <toyu7870@naver.com>
2023-10-31 12:34:13 +0900

    Refactor: 생성 constants/errormsg,promptmsg 클래스 생성
    에러메시지,프롬프트 메시지 상수 패키지 분리

---
+++ src/main/java/racingcar/constants/ErrorMsg.java
@@ -0,0 +1,7 @@
+package racingcar.constants;
+
+public class ErrorMsg {
+    public static String INITIALIZED_ERROR ="Game objects not initialized.";
+    public static String NO_REFEREE_ERROR = "Game not started. No round results to display.";
+
+}
