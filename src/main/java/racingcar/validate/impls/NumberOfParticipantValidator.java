c9e6f9dd8c91a82970adf562e74c9f73b5055aac
minjong kim <godminjong@naver.com>
2023-10-31 17:26:47 +0900

    feat: 입력 받는 진행 라운드 수가 음수가 아닌지, 숫자가 맞는지를 확인하는 기능 추가
    입력받은 문자가 숫자가 맞는지 그리고 음수가 아닌지에 대해 종합적으로 검증하는 기능을 추가 작성함.

--- src/main/java/racingcar/validate/impls/NumberOfParticipantValidator.java
+++ src/main/java/racingcar/validate/impls/NumberOfParticipantValidator.java
@@ -6,8 +6,15 @@ public class NumberOfParticipantValidator implements Validator {
 
     @Override
     public void validate(Object obj) {
+        String numberOfParticipants = (String) obj;
+        if (isInvalidNumber(numberOfParticipants)){
+            throw new IllegalArgumentException("0 이상의 정수만 입력해주세요.");
+        }
     }
 
+    private boolean isInvalidNumber(String number){
+        return isInvalidInteger(number) || isNegativeNumber(number);
+    }
 
     private boolean isNegativeNumber(String number){
         int convertedNumber = Integer.parseInt(number);
