e7427c55f375c4dbd367476c1d209aa1c58d234b
HungKungE <jonsun59@gmail.com>
2023-10-31 13:45:35 +0900

    [main/utils] 이름 사이 들어가는 공백 제거

--- src/main/java/racingcar/utils/Converter.java
+++ src/main/java/racingcar/utils/Converter.java
@@ -1,8 +1,17 @@
 package racingcar.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Converter {
     public static String[] convertCarsName(String newCarsName){
-        return newCarsName.split(",");
+        String[] newCarsNameArray = newCarsName.split(",");
+
+        for(int i=0;i< Arrays.stream(newCarsNameArray).count();i++){
+            newCarsNameArray[i] = newCarsNameArray[i].replace(" ","");
+        }
+
+        return newCarsNameArray;
     }
 
     public static int convertTurn(String newTurn){
