fbcd989de456b36376e6a08bf594c9818fffe8e8
kunsanglee <gosmdochee2@gmail.com>
2023-10-31 19:33:11 +0900

    test(RacingCarOutputViewTest): DTO를 받아서 출력하도록 테스트코드 수정

--- src/test/java/racingcar/view/RacingCarOutputViewTest.java
+++ src/test/java/racingcar/view/RacingCarOutputViewTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,57 +14,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import racingcar.domain.RacingResult;
-import racingcar.domain.RacingRoundResult;
-import racingcar.dto.CarInfo;
+import racingcar.dto.CarDto;
+import racingcar.dto.RacingResultDto;
+import racingcar.dto.RacingRoundResultDto;
 
 public class RacingCarOutputViewTest {
     private static final ByteArrayOutputStream OUTPUT = new ByteArrayOutputStream();
 
-    private static CarInfo setCarInfo(String carName, int position) {
-        return new CarInfo(carName, position);
-    }
-
-    static Stream<Arguments> providePrintEachRacingResultTestArguments() {
-        return Stream.of(
-                arguments(new RacingRoundResult(List.of(setCarInfo("pobi", 1),
-                                setCarInfo("woni", 0))),
-                        """
-                                pobi : -
-                                woni :\s
-                                                                
-                                """),
-                arguments(new RacingRoundResult(List.of(setCarInfo("pobi", 2),
-                                setCarInfo("woni", 4),
-                                setCarInfo("jun", 3))),
-                        """
-                                pobi : --
-                                woni : ----
-                                jun : ---
-                                                                
-                                """)
-        );
-    }
-
-    static Stream<Arguments> providePrintWinnerTestArguments() {
-        return Stream.of(
-                arguments(new RacingRoundResult(List.of(
-                                setCarInfo("pobi", 2),
-                                setCarInfo("woni", 4),
-                                setCarInfo("jun", 3))),
-                        "최종 우승자 : woni\n",
-                        new RacingRoundResult(List.of(
-                                setCarInfo("pobi", 4),
-                                setCarInfo("woni", 4),
-                                setCarInfo("jun", 3))),
-                        "최종 우승자 : pobi, woni\n",
-                        new RacingRoundResult(List.of(
-                                setCarInfo("pobi", 1),
-                                setCarInfo("woni", 1),
-                                setCarInfo("jun", 1))),
-                        "최종 우승자 : pobi, woni, jun\n"
-                )
-        );
+    private static CarDto setCarInfo(String carName, int position) {
+        return new CarDto(carName, position);
     }
 
     @BeforeEach
@@ -83,22 +42,42 @@ public class RacingCarOutputViewTest {
     @DisplayName("매 회차마다 자동차 경주의 결과를 출력한다.")
     @ParameterizedTest
     @MethodSource("providePrintEachRacingResultTestArguments")
-    void printEachRacingResult(RacingRoundResult racingRoundResult, String expectedMessage) {
+    void printEachRacingResult(RacingRoundResultDto racingRoundResultDto, String expectedMessage) {
         RacingCarOutputView racingCarOutputView = new RacingCarOutputView();
-        RacingResult racingResult = new RacingResult();
-        racingResult.addResult(racingRoundResult);
-        racingCarOutputView.printEachRacingResult(racingResult);
+        racingCarOutputView.printEachRacingResult(racingRoundResultDto);
         assertThat(OUTPUT.toString()).isEqualTo(expectedMessage);
     }
 
+    static Stream<Arguments> providePrintEachRacingResultTestArguments() {
+        return Stream.of(
+                arguments(new RacingRoundResultDto(List.of(setCarInfo("pobi", 1),
+                                setCarInfo("woni", 0))),
+                        """
+                                pobi : -
+                                woni :\s
+                                                                
+                                """),
+                arguments(new RacingRoundResultDto(List.of(setCarInfo("pobi", 2),
+                                setCarInfo("woni", 4),
+                                setCarInfo("jun", 3))),
+                        """
+                                pobi : --
+                                woni : ----
+                                jun : ---
+                                                                
+                                """)
+        );
+    }
+
     @DisplayName("최종 우승자를 출력한다.")
-    @ParameterizedTest
-    @MethodSource("providePrintWinnerTestArguments")
-    void printWinnerTest(RacingRoundResult racingRoundResult, String expectedMessage) {
+    @Test
+    void printWinnerTest() {
         RacingCarOutputView racingCarOutputView = new RacingCarOutputView();
-        RacingResult racingResult = new RacingResult();
-        racingResult.addResult(racingRoundResult);
-        racingCarOutputView.printWinner(racingResult);
-        assertThat(OUTPUT.toString()).isEqualTo(expectedMessage);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("pobi");
+        strings.add("woni");
+        RacingResultDto racingResultDto = RacingResultDto.from(strings);
+        racingCarOutputView.printWinner(racingResultDto);
+        assertThat(OUTPUT.toString()).isEqualTo("최종 우승자 : pobi, woni\n");
     }
 }
