5df3416fa482e4566bebf1d6b14918be01aa36c8
SCY <helenason@naver.com>
2023-10-31 18:36:53 +0900

    refactor: 사용하지 않는 속성 클래스에서 제거

--- src/main/java/racingcar/userinput/UserInput.java
+++ src/main/java/racingcar/userinput/UserInput.java
@@ -3,18 +3,14 @@ package racingcar.userinput;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.Race;
 
-import java.util.List;
-
 public class UserInput {
 
     private static final String CAR_NAME_INPUT_DELIMITER = ",";
     private static final String ASK_GAME_COUNT = "시도할 회수는 몇회인가요?";
 
-    private final Race race;
     private final UserInputValidator inputValidator;
 
     public UserInput(Race race) {
-        this.race = race;
         this.inputValidator = new UserInputValidator(race);
     }
 
