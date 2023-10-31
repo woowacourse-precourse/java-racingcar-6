9cf8d0025febf9574392026e4de60f0cd34769c1
hunko <rhwlgns4386@naver.com>
2023-10-31 14:41:33 +0900

    refactor(RacingGameController)  : inline 적용

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,7 +1,5 @@
 package racingcar.controller;
 
-import java.util.List;
-import racingcar.domain.car.Car;
 import racingcar.domain.game.RacingGame;
 import racingcar.dto.RacingGameRequest;
 import racingcar.dto.StatusResponse;
@@ -11,30 +9,24 @@ import racingcar.view.OutputView;
 public class RacingGameController {
 
     public void run() {
-        RacingGame racingGame = init();
+        RacingGame racingGame = initRacingGame();
         playGame(racingGame);
         endGame(racingGame);
     }
 
     private void endGame(RacingGame racingGame) {
-        List<String> winner = racingGame.getWinner();
-        OutputView.printWinner(winner);
+        OutputView.printWinner(racingGame.getWinner());
     }
 
     private static void playGame(RacingGame racingGame) {
         OutputView.printPlayResultMessage();
         while (!racingGame.isEnd()) {
             racingGame.doRace();
-            List<Car> status = racingGame.getStatus();
-            OutputView.printStatus(new StatusResponse(status));
+            OutputView.printStatus(new StatusResponse(racingGame.getStatus()));
         }
     }
 
-    private static RacingGame init() {
-        String names = InputView.readCarNames();
-        String count = InputView.readCount();
-
-        RacingGameRequest request = new RacingGameRequest(names, count);
-        return new RacingGame(request);
+    private static RacingGame initRacingGame() {
+        return new RacingGame(new RacingGameRequest(InputView.readCarNames(), InputView.readCount()));
     }
 }
