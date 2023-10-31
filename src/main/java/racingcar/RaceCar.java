f27ef8ec5399a9a572456bce60dc413967d36ba2
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-31 23:31:33 +0900

    feat(RaceCar): implemented class with basic attributes and constructor

---
+++ src/main/java/racingcar/RaceCar.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+public class RaceCar {
+    int position;
+    String name;
+    public RaceCar(){
+        name = " ";
+        position =0;
+    }
+    public RaceCar(String name){
+        this.name = name;
+        position =0;
+    }
+
+
+}
