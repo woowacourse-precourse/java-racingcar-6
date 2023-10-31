e0324900dc09210988de2daa730b0dacadc08c24
ddohyeong <dohyeong7026@gmail.com>
2023-10-31 14:35:10 +0900

    feat(RacingCameController) : 게임 시작 자동차 이름 입력 문구 출력 메시지 기능 구현
    docs : 자동차 이름 입력 문구 메시지 기능 구현 완료 체크

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,5 +1,15 @@
 package controller;
 
-public class RacingGameController {
+import view.MessageView;
 
+public class RacingGameController {
+	MessageView messageView = new MessageView();
+
+	public void startGame() {
+		initCarData();
+	}
+
+	public void initCarData() {
+		messageView.printCarNameMessage();
+	}
 }
