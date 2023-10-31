4bea81937e6bf70b82dcfcbf4d96da88f17b71e9
JoonKyo <joonkyolee@gmail.com>
2023-10-31 09:47:07 +0900

    feat(OutputView): 결과 출력 메시지 상수 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,7 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String RESULT_INTRODUCTION = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자";
+    private static final String COLON = " : ";
+}
