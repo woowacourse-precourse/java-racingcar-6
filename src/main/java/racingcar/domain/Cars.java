4aca52dc017453c4c3a7975cd742ae89ce42b8eb
ks0927 <brian_als@naver.com>
2023-10-31 22:23:09 +0900

    feat: List<Car>을 가지고있는 일급 컬렉션 Cars 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,52 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.NumberGenerator;
+
+public class Cars {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    public Cars(List<String> carList) {
+        for (String carName : carList) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+    }
+
+    public void racing(NumberGenerator numberGenerator) {
+        cars.stream()
+                .filter(car -> car.canMove(numberGenerator.generate()) == true)
+                .forEach(car -> car.move(true));
+    }
+
+    //포지션이 가장 큰 자동차 이름 리스트를 반환하는 메서드
+    public List<String> getFurthestCarNameList() {
+        int maxPosition = getMaxPosition();
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition() {
+        int maxMove = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .getAsInt();
+        return maxMove;
+    }
+
+    public void printCarsStatus() {
+        cars.stream().forEach(Car::printCarStatus);
+    }
+
+    // 현재 List<Car> cars를 깊은 복사로 복사한 값을 반환하는 메서드
+    public List<Car> getCurrentCarList() {
+        return cars.stream().map(car -> new Car(car.getName(), car.getPosition()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
