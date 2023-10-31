2f47a1dfdd844fa187f6c375cbf94dd5d8db850b
coli-geonwoo <kkwoo001021@naver.com>
2023-11-01 03:51:35 +0900

    feat(NameValidiator) : 이름입력 적법성 검사기능 구현

---
+++ src/main/java/domain/InputValues/NameValidiator.java
@@ -0,0 +1,33 @@
+package domain.InputValues;
+
+import java.util.Arrays;
+
+public class NameValidiator {
+    public static final int NAME_MAX_SIZE =5;
+    NameValidiator(String inputCarNames)
+    {
+        checkEmpty(inputCarNames);
+    }
+
+    private void checkEmpty(String inputCarNames){
+        if(inputCarNames.isEmpty()){
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+    public void checkDuplicate(String []  inputCarNamesArray){
+        if(inputCarNamesArray.length != Arrays.stream(inputCarNamesArray).distinct().count()){
+            throw new IllegalArgumentException("중복되는 이름이 존재합니다.");
+        }
+    }
+
+    public void checkEmptyorLongNames(String [] inputCarNamesArray){
+        for(String carNames: inputCarNamesArray){
+            if(carNames.isEmpty() || carNames.length()>NAME_MAX_SIZE){
+                throw new IllegalArgumentException("이름이 너무 길거나 없습니다.");
+            }
+        }
+
+
+    }
+
+}
