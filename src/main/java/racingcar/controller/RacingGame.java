7d695feaa9b7616bffac5985f2e7a33a555ac303
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 00:50:19 +0900

    feat : 컨트롤러 코드 수정

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,60 +1,52 @@
 package racingcar.controller;
 
 
-import racingcar.domain.Car;
 import racingcar.domain.Cars;
-import racingcar.domain.RandomNumber;
+import racingcar.util.RandomNumber;
 import racingcar.domain.UserInputRoundNumber;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RacingGame {
-    private InputView inputView;
-    private OutputView outputView;
     private Cars cars;
     private UserInputRoundNumber userInputRoundNumber;
-    private Map<String, Integer> forwardState = new HashMap<>();
-
+    private LinkedHashMap<String, Integer> forwardState = new LinkedHashMap<>();
+    private String userRoundNum;
+    private static int roundRepeatNum;
 
     public void game(){
         setPlay();
         play();
         rewardMVP();
     }
-    private int setPlay(){
-        String userInput = InputView.createCarName();
-        Cars cars = new Cars(userInput);
-        String userRoundNum = InputView.createRoundNumber();
-        int roundRepeatNum = UserInputRoundNumber.stringToInt(userRoundNum); // 라운드 반복 횟수
-        return roundRepeatNum;
+    private void setPlay(){
+        cars = new Cars(InputView.createCarName());
+        userRoundNum = InputView.createRoundNumber();
+        roundRepeatNum = UserInputRoundNumber.stringToInt(userRoundNum); // 라운드 반복 횟수
     }
 
     private void play(){
-        RandomNumber randomNumber = new RandomNumber();
-        int random = randomNumber.createRandomNumber();
-        int repeatNum = setPlay();
-        for(int i = 0; i < repeatNum; i++) {
-            cars.depart(random);
+        OutputView.printResult();
+        for(int i = 0; i < roundRepeatNum; i++) {
+            cars.depart();
             getForwardState();
         }
     }
 
     private void rewardMVP(){
         List<String> mvp = cars.getMVP();
-        outputView.printMVP(mvp);
+        OutputView.printMVP(mvp);
     }
 
     private void getForwardState() {
         forwardState = cars.getForwardState();
-        for (Map.Entry<String, Integer> entry : forwardState.entrySet()) {
-            outputView.printForward(entry.getKey(), entry.getValue());
+        OutputView.printForward(forwardState);
         }
     }
 
 
-
-}
