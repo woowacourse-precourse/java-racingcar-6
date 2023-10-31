0b6e4a049d66021fac0d6f4996d3e5d9608238fc
“sangjun121” <juncho12011201@gmail.com>
2023-10-31 21:36:27 +0900

    feat: 레이싱 진행 횟수 유효성 판단할 정규표현식 패턴 선언

--- src/main/java/racingcar/utility/Constants.java
+++ src/main/java/racingcar/utility/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
 
     //예외처리 정규표현식 패턴
     public static final String IMPROPER_COMMA_POSITION = "^,|,$|,,"; //문자열 처음 또는 끝에서 comma가 나오거나 문자열 내에서 2번 연속 나오는 경우
+    public static final String VAILD_RACING_COUNT_PATTERN = "^[1-9]\\d*$";
 }
