b524c1cf55e6680179508bd55a8fc1b38589c103
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-31 10:44:35 +0900

    feat: implement exception handling for empty name
    update isNameTooLong method to isNameWithinRange method with adding empty name validation

--- src/main/java/racingcar/util/Utils.java
+++ src/main/java/racingcar/util/Utils.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Utils {
     public static List<String> toList(String carNames) {
-        return Arrays.asList(carNames.split("\\s*,\\s*"));
+        return Arrays.asList(carNames.split("\\s*,\\s*", -1));
     }
 }
--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class CarNameValidator {
     private final String carNames;
     public final List<String> carNameList;
+
     public CarNameValidator(String carNames) {
         this.carNames = carNames;
         this.carNameList = Utils.toList(carNames);
@@ -17,13 +18,15 @@ public class CarNameValidator {
 
     public void validate() {
         isNameValid();
-        isNameTooLong();
+        isNameWithinRange();
     }
-    public void isNameTooLong() {
-        if(carNameList.stream().anyMatch(name -> name.length() > 5)) {
-            throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+
+    public void isNameWithinRange() {
+        if(carNameList.stream().anyMatch(name -> name.length() > 5 || name.isEmpty())) {
+            throw new IllegalArgumentException("자동차의 이름은 1~5자이어야 합니다.");
         }
     }
+
     public void isNameValid() {
         Pattern pattern = Pattern.compile("[^a-zA-Z0-9,]");
         if(carNames.matches(".*[^a-zA-Z0-9 ,].*")) {
