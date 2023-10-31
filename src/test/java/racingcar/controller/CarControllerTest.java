aa6b7f38506a11e78b492d9818df5d4179d4c7f7
jaehee <y2hjjh@naver.com>
2023-11-01 01:16:31 +0900

    test: CarController 사용자 입력 유효성 검사 테스트
    정상적인 입력일 경우, 자동차 이름이 1개일 경우, 글자수 범위를 벗어날 경우, 중복일 경우 검사

--- src/test/java/racingcar/controller/CarControllerTest.java
+++ src/test/java/racingcar/controller/CarControllerTest.java
@@ -1,4 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.constant.ExceptionMessage;
+import racingcar.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarControllerTest {
-  
+    private CarController carController;
+
+    @BeforeEach
+    void setUp() {
+        carController = new CarController();
+    }
+
+    @Test
+    void 자동차_이름_입력_유효성_검사_정상() {
+        String playerInput = "dog,cat,bear";
+        List<Car> expectedCarNames = Arrays.asList(new Car("dog"), new Car("cat"), new Car("bear"));
+        assertThat(carController.getRacingCars(playerInput))
+                .usingRecursiveComparison()
+                .ignoringFields("record")
+                .isEqualTo(expectedCarNames);
+    }
+
+    @Test
+    void 자동차_이름_입력_유효성_검사_1대일_경우() {
+        String playerInput = "dog";
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> carController.getRacingCars(playerInput));
+        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NAMES_COUNT_EXCEPTION_MESSAGE.get());
+    }
+
+    @Test
+    void 자동차_이름_입력_유효성_검사_글자수_범위를_벗어날_경우() {
+        String playerInput = "dog,elephant,cat";
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> carController.getRacingCars(playerInput));
+        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NAME_LENGTH_EXCEPTION_MESSAGE.get());
+    }
+
+    @Test
+    void 자동차_이름_입력_유효성_검사_중복일_경우() {
+        String playerInput = "dog,dog";
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> carController.getRacingCars(playerInput));
+        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NAME_DUPLICATION_EXCEPTION_MESSAGE.get());
+    }
 }
