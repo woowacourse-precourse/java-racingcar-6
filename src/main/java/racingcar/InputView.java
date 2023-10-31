8334791556e9399981b43a923c9a712f866371f6
songjayhyun <thewinner1st@naver.com>
2023-10-31 13:35:24 +0900

    feat: create input view skeleton code
    - 시작 문구 출력 함수 추가
    - 자동차 이름 입력 문구 출력 함수 추가
    - 시도 횟수 입력 문구 출력 함수 추가

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class InputView {
+    public void pirntStartMsg() {
+    }
+
+    public void printCarNameInputMsg() {
+    }
+
+    public void printTryCountInputMsg() {
+    }
+}
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -2,5 +2,11 @@ package racingcar;
 
 public class RacingCarGame {
     public void startGame() {
+        InputView inputView = new InputView();
+        inputView.pirntStartMsg();
+
+        inputView.printCarNameInputMsg();
+
+        inputView.printTryCountInputMsg();
     }
 }
