fc31f0add9e0ef706643dd1985d9db3f3bac8282
saebyeok2 <tomato8269@naver.com>
2023-10-31 15:58:33 +0900

    refactor: 사용자 입력 예외 처리
    - 사용자로부터 자동차 이름을 입력받을 때 예외사항을 처리한다.
    - 공백 문자를 입럭해 자동차 이름이 존재하지 않는 경우
    - 중복된 자동차 이름이 존재하는 경우
    - 자동차 이름이 5자를 초과하는 경우

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,23 +1,40 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputView {
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public List<String> getCarNames() {
+    public Set<String> getCarNames() {
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
         String rawString = Console.readLine();
 
         return parseString(rawString);
     }
 
-    private static List<String> parseString(String rawString) {
-        return new ArrayList<>(Arrays.asList(rawString.split(",")));
-    }
+    private static Set<String> parseString(String rawString) {
+        String[] carNameArray = rawString.split(",");
+        Set<String> carNames = new HashSet<>();
 
+        for (String carName : carNameArray) {
+            carName = carName.trim();
+
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름에 공백이 포함되어 있습니다.");
+            }
+
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로만 입력 가능합니다.");
+            }
+
+            if (!carNames.add(carName)) {
+                throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+            }
+        }
+
+        return carNames;
+    }
 }
