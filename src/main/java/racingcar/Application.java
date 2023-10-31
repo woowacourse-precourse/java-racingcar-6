faa166a5ebc27a3eb874e87636604a4b21a91870
junho <junho1185@gmail.com>
2023-10-31 16:54:53 +0900

    User input implementation

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,26 @@
 package racingcar;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int player_n, i, n;
+        String input;
+        String[] names;
+
+        TException te = new TException();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        input = Console.readLine();
+        names = input.split(",");
+        player_n = names.length;
+        Racer[] racers = new Racer[player_n];
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        n = Integer.parseInt(Console.readLine());
+        
     }
 }
 
@@ -19,5 +37,11 @@ class Racer{
 
     public int getDistance(){
         return this.distance;
+    }
+}
+
+class TException{
+    public void shoot() throws IllegalArgumentException{
+        throw new IllegalArgumentException();
     }
 }
