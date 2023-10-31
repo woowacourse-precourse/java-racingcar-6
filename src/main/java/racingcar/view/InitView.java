e11fb53921b06bfe5a4782fd2c05f7c30ec10711
whereami2048 <whereami2048@gmail.com>
2023-10-31 15:58:39 +0900

    feat: InitView 예외 처리 구현
    inputCarNames() : 아무 입력도 하지 않은 경우 예외 발생
    inputTryNumber() : 자연수가 아닌 경우 예외 발생

--- src/main/java/racingcar/view/InitView.java
+++ src/main/java/racingcar/view/InitView.java
@@ -1,19 +1,36 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.InputValidatior;
 
 public class InitView {
 
+    private final InputValidatior validator;
+
+    public InitView(InputValidatior validator) {
+        this.validator = validator;
+    }
+
     public String[] inputCarNames() {
         inputCarNameMessagePrint();
-        String names = Console.readLine();
 
-        return names.split(",");
+        String[] names = Console.readLine().split(",");
+
+        if (validator.isCarNamesUnderLength(names)) {
+            throw new IllegalArgumentException("최소 한 개 이상의 자동차 이름이 필요합니다.");
+        }
+
+        return names;
     }
 
     public int inputTryNumber() {
         inputTryNumberMessagePrint();
+
         String number = Console.readLine();
+
+        if (!validator.isNaturalNumber(number)) {
+            throw new IllegalArgumentException("자연수가 아닙니다.\n자연수를 입력해주세요");
+        }
 
         return Integer.parseInt(number);
     }
