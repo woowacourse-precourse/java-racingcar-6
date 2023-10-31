8ebfc7b89b1df27530d8b7ad823025b42e372248
gmelon <hsh1769@naver.com>
2023-10-31 22:44:43 +0900

    feat: Input의 값에서 공백 문자를 제거한 값을 반환하는 메서드 구현

--- src/main/java/racingcar/view/input/Input.java
+++ src/main/java/racingcar/view/input/Input.java
@@ -3,6 +3,9 @@ package racingcar.view.input;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
+    private static final String WHITESPACES_REGEX = "\\s+";
+    private static final String BLANK_STRING = "";
+
     private final String value;
 
     Input(String value) {
@@ -11,5 +14,9 @@ public class Input {
 
     public static Input readLine() {
         return new Input(Console.readLine());
+    }
+
+    public String trimmed() {
+        return value.replaceAll(WHITESPACES_REGEX, BLANK_STRING);
     }
 }
--- src/main/java/racingcar/view/input/MainInputView.java
+++ src/main/java/racingcar/view/input/MainInputView.java
@@ -1,6 +1,5 @@
 package racingcar.view.input;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -8,33 +7,25 @@ import java.util.regex.Pattern;
 public class MainInputView {
     private static final String CARNAME_SPLIT_DELIMETER = ",";
     private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
-    private static final String WHITESPACES_REGEX = "\\s+";
-    private static final String BLANK_STRING = "";
 
     private MainInputView() {
     }
 
     public static List<String> readCarNames() {
-        String input = readTrimmedLine();
+        Input input = Input.readLine();
+        String trimmedInput = input.trimmed();
 
-        return Arrays.asList(input.split(CARNAME_SPLIT_DELIMETER));
+        return Arrays.asList(trimmedInput.split(CARNAME_SPLIT_DELIMETER));
     }
 
     public static int readPlayCount() {
-        String input = readTrimmedLine();
+        Input input = Input.readLine();
+        String trimmedInput = input.trimmed();
 
-        if (!NUMBER_PATTERN.matcher(input).matches()) {
+        if (!NUMBER_PATTERN.matcher(trimmedInput).matches()) {
             throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다.");
         }
 
-        return Integer.parseInt(input);
-    }
-
-    private static String readTrimmedLine() {
-        return trimmedString(Console.readLine());
-    }
-
-    private static String trimmedString(String rawString) {
-        return rawString.replaceAll(WHITESPACES_REGEX, BLANK_STRING);
+        return Integer.parseInt(trimmedInput);
     }
 }
