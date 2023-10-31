eb858692d5056fbdc173b2fed2372aad6a51d6e5
Junobee25 <wnsdhqo@naver.com>
2023-10-31 20:25:31 +0900

    refactor(service): racingGameStart 메서드 수정

--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -18,7 +18,13 @@ public class RacingGame {
 
     public void racingGameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carGenerator.inputCarList();
+        car.checkContainsConsecutiveCommas();
         car.setCarList(carGenerator.createCarList());
+        car.checkCarNameLength();
+        car.checkCarNameDuplicate();
+        car.checkCarNameFirstLetterIsSpace();
+        car.checkCarNameLastLetterIsSpace();
         LinkedHashMap<String, String> carList = car.getCarList();
 
         System.out.println("시도할 회수는 몇회인가요?");
