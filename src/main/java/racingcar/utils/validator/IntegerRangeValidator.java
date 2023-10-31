251406fa86046c2fcaecbcce3089748881dacf0f
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-31 16:49:20 +0900

    fix: 범위 오류 수정

--- src/main/java/racingcar/utils/validator/IntegerRangeValidator.java
+++ src/main/java/racingcar/utils/validator/IntegerRangeValidator.java
@@ -3,6 +3,6 @@ package racingcar.utils.validator;
 public class IntegerRangeValidator {
     public static boolean validate(String input, int min, int max) {
         int value = Integer.parseInt(input);
-        return value > min && value <= max;
+        return value >= min && value <= max;
     }
 }
