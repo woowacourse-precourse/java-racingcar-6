e617e5c1d22126e47c31598030d5bb7251614278
yewonahn <yewonahn@naver.com>
2023-11-01 04:34:58 +0900

    feat: Application 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,14 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
 
+        InputController inputController = new InputController();
+        inputController.inputCarName();
+        inputController.inputCycleNum();
+        PrintController printController = new PrintController();
+        printController.printDirection();
+        FindWinnerController findWinnercontroller = new FindWinnerController();
+        findWinnercontroller.totalCycle();
+        findWinnercontroller.findWinner();
+        printController.printWinner();
     }
 }
