0a6aca24eb3ab6c827b1bb70369aeab4408dad0b
jinny-l <jinny.l.dev@gmail.com>
2023-10-31 22:42:45 +0900

    ♻️refactor: 출력 메시지 포맷을 `String.format()`으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,18 +6,18 @@ import racingcar.util.InputUtil;
 
 public final class InputView {
 
-    private static final String INPUT_CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 %s(%s) 기준으로 구분)%n";
+    private static final String INPUT_CAR_NAMES_REQUEST_MESSAGE = String.format(
+            "경주할 자동차 이름을 입력하세요.(이름은 %s(%s) 기준으로 구분)",
+            GameConfig.CAR_NAME_DELIMITER_KR,
+            GameConfig.CAR_NAME_DELIMITER
+    );
     private static final String INPUT_NUMBER_OF_ATTEMPTS_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private InputView() {
     }
 
     public static List<String> readCarNames() {
-        System.out.printf(
-                INPUT_CAR_NAMES_REQUEST_MESSAGE,
-                GameConfig.CAR_NAME_DELIMITER_KR,
-                GameConfig.CAR_NAME_DELIMITER
-        );
+        System.out.println(INPUT_CAR_NAMES_REQUEST_MESSAGE);
         return InputUtil.readStringList();
     }
 
