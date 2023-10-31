e8a78ebfd422a1e53cca9bf8b7b3efea64526e0e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-31 18:54:18 +0900

    refactor : RacingCar 값의 상수화

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import static racingcar.constant.RacingGameConstant.RACING_CAR_LIMIT_NAME_LENGTH;
+import static racingcar.constant.RacingGameConstant.START_POSITION;
 import static racingcar.exception.ErrorMessage.CAR_NAME_LENGTH_OVER_FIVE_EXCEPTION;
 
 import java.util.Objects;
@@ -11,11 +13,11 @@ public class RacingCar {
     private int position;
 
     public RacingCar(String carName, MoveCondition moveCondition) {
-        this(carName, 0, moveCondition);
+        this(carName, START_POSITION, moveCondition);
     }
 
     public RacingCar(String carName, int position, MoveCondition moveCondition) {
-        if (carName.length() <= 5) {
+        if (carName.length() <= RACING_CAR_LIMIT_NAME_LENGTH) {
             this.carName = carName;
             this.moveCondition = moveCondition;
             this.position = position;
--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.constant.RacingGameConstant.START_POSITION;
 import static racingcar.exception.ErrorMessage.CAR_NAME_LENGTH_OVER_FIVE_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
@@ -71,6 +72,6 @@ public class RacingCarTest {
         racingCar.race(randomNumber);
 
         // then
-        assertThat(racingCar).isEqualTo(new RacingCar("tobi", 0, moveCondition));
+        assertThat(racingCar).isEqualTo(new RacingCar("tobi", START_POSITION, moveCondition));
     }
 }
