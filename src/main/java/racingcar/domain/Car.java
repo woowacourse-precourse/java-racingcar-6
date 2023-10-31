882659a6fa2321890690fbd33c0307de5c7537bb
jhsong76 <20190976@sungshin.ac.kr>
2023-10-31 17:02:25 +0900

    feat: Car 도메인 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        int random = Randoms.pickNumberInRange(0,9);
+
+        if (random >= 4) {
+            position++;
+        }
+    }
+}
