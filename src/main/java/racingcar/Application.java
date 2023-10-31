59640463d374e522c509acb840ae237234385ba4
aka-nick <bechalrang@gmail.com>
2023-10-31 16:43:13 +0900

    수정(Application) : 객체 초기화 부분 메서드로 추출
    복잡한 객체 초기화 코드를 메서드로 추출했다.
    
    Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,14 +11,21 @@ public class Application {
 
     public static void main(String[] args) {
         try {
-            // TODO : 연관관계 초기화를 어플리케이션으로 몰았더니.. 몰은 것 자체는 좋은데 너무 보기 싫다..
-            RacingGame racingGame = new RacingGame(new RacingGameView(new RaceView(), new RaceTotalProgressView()),
-                    new Race());
+            RacingGame racingGame = getRacingGame();
             racingGame.play();
         } catch (IllegalArgumentException e) {
             Output.consoleLine("잘못된 동작으로 인해 프로그램을 종료합니다.");
             throw e;
         }
+    }
+
+    private static RacingGame getRacingGame() {
+        // COMMENT : 연관관계 초기화를 어플리케이션으로 몰았더니.. 몰은 것 자체는 좋은데 너무 보기 싫다..
+        return new RacingGame(
+                new RacingGameView(
+                        new RaceView(), new RaceTotalProgressView()
+                ),
+                new Race());
     }
 
 }
