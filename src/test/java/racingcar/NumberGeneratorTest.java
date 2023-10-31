eb4756be0ad32ff9e68f76dcd3bc8fd8f2c0606f
K-KY <kyuyoungk@naver.com>
2023-11-01 06:38:30 +0900

    Test(NumberGeneratorTest) : 임포트 패키지 변경

--- src/test/java/racingcar/NumberGeneratorTest.java
+++ src/test/java/racingcar/NumberGeneratorTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.NumberGenerator;
 
 public class NumberGeneratorTest {
     @Test
