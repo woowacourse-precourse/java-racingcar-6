f33be9377ae02236bd01e7531e743ff1857839fa
seminchoi <choicco89@gmail.com>
2023-10-31 13:01:28 +0900

    test(racingcar.MockRandomNumberGenerator) : 초기화 하지 않아도 객체를 사용할 수 있도록 수정

--- src/test/java/racingcar/MockRandomNumberGenerator.java
+++ src/test/java/racingcar/MockRandomNumberGenerator.java
@@ -1,10 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MockRandomNumberGenerator implements RandomNumberGenerator {
     private static final MockRandomNumberGenerator INSTANCE = new MockRandomNumberGenerator();
+
+    private final int MIN_RANDOM_NUMBER = 0;
+    private final int MAX_RANDOM_NUMBER = 9;
+
     private List<Integer> numberList;
     private int index = 0;
 
@@ -25,6 +31,9 @@ public class MockRandomNumberGenerator implements RandomNumberGenerator {
 
     @Override
     public int generateRandomNumber() {
+        if(numberList.isEmpty()) {
+            return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+        }
         int generatedNumber = numberList.get(index);
         changeIndex();
         return generatedNumber;
