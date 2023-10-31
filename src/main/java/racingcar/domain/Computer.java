6977261e8b178e2db29a254caf3ebcaeefe719bf
노정완 <jeongwan.noh@gmail.com>
2023-10-31 21:59:34 +0900

    feat: 자동차 이동 여부 판단 로직 구현

--- src/main/java/racingcar/domain/Computer.java
+++ src/main/java/racingcar/domain/Computer.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
 public class Computer {
-
-    public void judgeCarMovement() {
-
+    private final static int MOVEMENT_FLAG = 4;
+    public boolean judgeCarMovement(int random) {
+        if (random >= 4) {
+            return true;
+        }
+        return false;
     }
 
     public void judgeWinner() {
