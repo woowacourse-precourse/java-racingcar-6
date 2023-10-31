1b87f026d5d56a02c2050da76be1e2087e7da41a
lee jihyeon <lejinhy@gmail.com>
2023-10-31 19:39:55 +0900

    test: 경주 진행 테스트 완료

---
+++ src/test/java/racingcar/RacingControllerTest.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class RacingControllerTest  extends NsTest {
+
+    private static final int MOVE = 4;
+    private static final int STOP = 3;
+
+    @Test()
+    void 경주_세번_진행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("one,two", "3");
+                    assertThat(output()).contains("one : -", "two : ", "one : -", "two : ", "one : --", "two : ", "최종 우승자 : one");
+                },
+                MOVE, STOP, STOP, STOP, MOVE, STOP
+        );
+
+    }
+
+
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+}
