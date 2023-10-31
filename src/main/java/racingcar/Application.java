0652443bdd684e0468c207d056fcb1612e775bc8
FutureCircuit <137764369+FutureCircuit@users.noreply.github.com>
2023-10-31 19:11:14 +0900

    refactor: 구현한 각 기능을 메서드로 분리 코드 가독성을 위해 메서드 분리를 진행하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,28 +8,41 @@ public class Application {
         // TODO: 프로그램 구현
         Map<String, Integer> cars = new HashMap<>();
         List<String> carNames;
+        carNames = getCarNames();
+
+        setCarInfo(carNames, cars);
+
+        int attempt = getAttempt();
+        printRaceResult(attempt, carNames, cars);
+        printWinner(cars, carNames);
+    }
+
+    private static List<String> getCarNames() {
+        List<String> carNames;
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] temp = Console.readLine().split(",");
         carNames = new ArrayList<>(Arrays.asList(temp));
+        return carNames;
+    }
 
+    private static void setCarInfo(List<String> carNames, Map<String, Integer> cars) {
         for(String str : carNames) {
             if(str.length() >= 6) throw new IllegalArgumentException();
             cars.put(str, 0);
         }
+    }
 
+    private static int getAttempt() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int attempt = Integer.parseInt(Console.readLine());
+        return Integer.parseInt(Console.readLine());
+    }
 
+    private static void printRaceResult(int attempt, List<String> carNames, Map<String, Integer> cars) {
         System.out.println("\n실행 결과");
 
         for(int i = 0; i < attempt; i++) {
             race(carNames, cars);
         }
-
-        System.out.print("최종 우승자 : ");
-        Integer maxVal = Collections.max(cars.values());
-        carNames.removeIf(str -> !(Objects.equals(cars.get(str), maxVal)));
-        System.out.println(String.join(",", carNames));
     }
 
     private static void race(List<String> carNames, Map<String, Integer> cars) {
@@ -42,5 +55,12 @@ public class Application {
         }
 
         System.out.println();
+    }
+
+    private static void printWinner(Map<String, Integer> cars, List<String> carNames) {
+        System.out.print("최종 우승자 : ");
+        Integer maxVal = Collections.max(cars.values());
+        carNames.removeIf(str -> !(Objects.equals(cars.get(str), maxVal)));
+        System.out.println(String.join(",", carNames));
     }
 }
