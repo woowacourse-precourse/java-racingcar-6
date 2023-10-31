d9331571650ae807f5d604549d880336f09be269
nayonsoso <panda0329@naver.com>
2023-10-31 23:04:12 +0900

    refactor: 이름 입력을 검증하는 함수들을 InputView 에서 분리

---
+++ src/main/java/view/NameListValidator.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.util.HashSet;
+
+public class NameListValidator {
+    private void checkNotEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException("입력된 값이 없습니다.");
+        }
+    }
+
+    private void checkNotEndsWithComma(String userInput) {
+        if (userInput.endsWith(",")) {
+            throw new IllegalArgumentException("마지막이 쉼표로 끝납니다.");
+        }
+    }
+
+    private void checkContainsRepeatingCommas(String userInput) {
+        String ignoreSpace = ignoreSpace(userInput);
+        for (int i = 0; i < ignoreSpace.length() - 1; i++) {
+            if (ignoreSpace.charAt(i) == ',' && ignoreSpace.charAt(i + 1) == ',') {
+                throw new IllegalArgumentException("쉼표 사이에 값이 없습니다.");
+            }
+        }
+    }
+
+    private String ignoreSpace(String userInput) {
+        return userInput.replace(" ", "");
+    }
+
+    private void checkNameLength(String[] names, int maxLength) {
+        for (String name : names) {
+            if (name.length() > maxLength) {
+                String message = "이름의 길이가 " + maxLength + "자가 넘습니다.";
+                throw new IllegalArgumentException(message);
+            }
+        }
+    }
+
+    private void checkNameDuplication(String[] names){
+        HashSet<String> nameSet = new HashSet<>();
+        for(String name : names){
+            if(!nameSet.add(name)){
+                throw new IllegalArgumentException("중복되는 이름이 있습니다.");
+            }
+        }
+    }
+}
