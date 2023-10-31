baf1e0d2a782a4b8521ad3a787ab0d2cbca405b7
MingyeomKim <mingyum119@naver.com>
2023-11-01 03:34:37 +0900

    feat(Parser): Cars에서 문자열을 List<Car>로 변환하기 위한 Parser 정의
    사용자로부터 입력된 문자열을 구분자로 구분하고 CarList로 만들어 반환하는 로직을 작성한다.

---
+++ src/main/java/racingcar/utils/Parser.java
@@ -0,0 +1,19 @@
+package racingcar.utils;
+
+import static racingcar.global.constants.SymbolType.DELIMITER;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.Car;
+
+public class Parser {
+    public static List<Car> parseStringToCarList(String userInput) {
+        return Arrays.stream(split(userInput))
+                .map(name -> Car.of(name))
+                .toList();
+    }
+
+    private static String[] split(String userInput) {
+        return userInput.split(DELIMITER.getSymbol());
+    }
+}
