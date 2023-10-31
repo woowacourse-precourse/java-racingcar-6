ea681f557d28a79c7d6901b7ae2f12e4bf43155c
박승민 <tmddudals369@naver.com>
2023-11-01 01:54:38 +0900

    feat(outputView): 게임 진행 상황을 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import racingcar.dto.CarResultResponse;
+
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -13,5 +15,9 @@ public class OutputView {
 
         System.out.println(INPUT_GAME_PROGRESS_COUNT_MESSAGE);
 
+    }
+
+    public void printGameProcessResult(CarResultResponse carResultResponse) {
+        System.out.println(carResultResponse.toString());
     }
 }
