2f241fc04ce10fb6ade50f1d5dc6237050153dd1
youngkyunIm <yee0230@naver.com>
2023-10-31 20:52:16 +0900

    feat(controller): 게임 횟수만큼 플레이어 이동 반복

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,11 +2,14 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.model.Players;
+import racingcar.model.RacingReferee;
+import racingcar.model.RandomDice;
 import racingcar.view.View;
 
 public class RacingController {
 
     private final View view;
+    private RacingReferee racingReferee;
 
     public RacingController(View view) {
         this.view = view;
@@ -17,6 +20,16 @@ public class RacingController {
         Players players = new Players(getStringListFromString(view.printInputCarsNames()));
         int racingTime = getIntFromString(view.printInputRacingTime());
 
+        racingReferee = new RacingReferee(players, new RandomDice());
+
+        view.printResultMessage();
+        race(racingTime);
+    }
+
+    private void race(int racingTime) {
+        for (int i = 0; i < racingTime; i++) {
+            view.printMovedLog(racingReferee.carMove());
+        }
     }
 
     private List<String> getStringListFromString(String string) {
