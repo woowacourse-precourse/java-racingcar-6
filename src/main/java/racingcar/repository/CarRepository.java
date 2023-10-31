7cd54d44a91d38d9ca81a3a47311fd36e5b21b72
bch0520 <bch410953@gmail.com>
2023-10-31 11:28:31 +0900

    feat: implement CarRepository/GameRepository Class

--- src/main/java/racingcar/repository/CarRepository.java
+++ src/main/java/racingcar/repository/CarRepository.java
@@ -1,4 +1,27 @@
 package racingcar.repository;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CarRepository {
+    private Long id = 0L;
+    private final Map<Long, Car> cars = new HashMap<>();
+
+    public Long save(Car car) {
+        cars.put(id, car);
+        id += 1;
+        return id;
+    }
+
+    public Car findById(Long id) {
+        return cars.get(id);
+    }
+
+    public List<Car> findAll() {
+        return new ArrayList<>(cars.values()); //cars 맵에 저장된 객체들을 map의 values(자동차) 값만 반환
+    }
 }
--- src/main/java/racingcar/repository/GameRepository.java
+++ src/main/java/racingcar/repository/GameRepository.java
@@ -1,4 +1,32 @@
 package racingcar.repository;
 
+import racingcar.domain.Game;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GameRepository {
+    private Long id = 0L;
+    private final Map<Long, Game> games = new HashMap<>();
+
+    public Long save(Game game) {
+        Long returnId = id;
+        games.put(id,game);
+        id += 1;
+        return returnId;
+    }
+
+    public Boolean isEnd(Game game) {
+        return game.checkReachTrialNumberToTrial();
+    }
+
+    public Game findById(Long id) {
+        return games.get(id);
+    }
+
+    public List<Game> findAll() {
+        return new ArrayList<>(games.values());
+    }
 }
