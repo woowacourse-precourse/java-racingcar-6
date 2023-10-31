adacfd441dfc38751fd99ad94a5bebb32cb108be
Chan Mi <rkdcksal02@gmail.com>
2023-10-31 22:55:05 +0900

    Car 클래스 - 자동차 이름이 빈 문자열인 경우와 5자를 초과하는 경우 모두 예외 처리로 변경.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,8 +8,8 @@ public class Car {
 
     public Car(String name) {
         // 1. 자동차 이름이 5자 이하인지 확인합니다.
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1자에서 5자 사이여야 합니다.");
         }
         this.name = name;
         this.position = 0;
