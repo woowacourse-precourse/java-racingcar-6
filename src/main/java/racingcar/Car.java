ed7e7a9027cb8570812c969df2ef2abee6201504
LUKR7Q <ybkim0628@naver.com>
2023-10-31 12:35:08 +0900

    기능(car): Car 클래스에 move() 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,8 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private static final int MAX_NUMBER_OF_RANDOM = 9;
+    private static final int MIN_NUMBER_OF_RANDOM = 0;
+    private static final int MIN_NUMBER_OF_MOVE = 4;
 
     private final String name;
+    private int position = 0;
     private final StringBuffer report;
 
     public Car(String name) {
@@ -12,6 +16,13 @@ public class Car {
         this.report = new StringBuffer();
         this.report.append(name);
         this.report.append(" : ");
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(MIN_NUMBER_OF_RANDOM, MAX_NUMBER_OF_RANDOM) >= MIN_NUMBER_OF_MOVE) {
+            this.position++;
+            this.report.append("-");
+        }
     }
 
 }
