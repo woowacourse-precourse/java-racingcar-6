fa5269bbc6eb1589a20743529189307a6684e1ae
kwon <kckc0608@naver.com>
2023-11-01 01:42:10 +0900

    feat: 게임 진행 및 랜덤 숫자 생성 구현
    1. processGame 메서드로 기능 분리
    2. processGame 메서드 내에서 랜덤 숫자 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_TITLE_MESSAGE = "\n" + "실행 결과";
+
+    private static final List<Car> cars = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         String carNameInput = camp.nextstep.edu.missionutils.Console.readLine();
 
         List<String> carNames = Arrays.stream(carNameInput.split(",")).toList();
-        List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
             Car car = new Car(carName);
             cars.add(car);
         }
 
         System.out.println(TRY_COUNT_INPUT_MESSAGE);
-        String tryCount = camp.nextstep.edu.missionutils.Console.readLine();
+        String tryCountInput = camp.nextstep.edu.missionutils.Console.readLine();
+        int tryCount = Integer.parseInt(tryCountInput);
 
         System.out.println(RESULT_TITLE_MESSAGE);
+        for (int nowCount = 1; nowCount <= tryCount; nowCount++) {
+            processGame();
+        }
+    }
+
+    static void processGame() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+        }
     }
 }
