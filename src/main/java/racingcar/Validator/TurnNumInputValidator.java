73fa7b15d3b1ac44d47cc1a0a4812a56ac256ac9
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-31 18:06:48 +0900

    style: 예외 처리 출력 문구 수정

--- src/main/java/racingcar/Validator/TurnNumInputValidator.java
+++ src/main/java/racingcar/Validator/TurnNumInputValidator.java
@@ -2,10 +2,10 @@ package racingcar.Validator;
 
 public class TurnNumInputValidator {
     private int turnNumInputInt;
-    private final String ASK_NUMBER_INPUT = "잘못된 입력입니다. 숫자만 입력해야 합니다";
+    private final String ASK_INT_UNDER_PROPER_AMOUNT = "잘못된 입력입니다. 적정한 수를 정수 형태로 입력해주세요";
     private final String ASK_POSITIVE_NUM_INPUT = "잘못된 입력입니다. 0이상의 수를 입력해주세요";
     private final int MIN_TURN_NUM = 1;
-    
+
 
     public void validate(String turnNumInputString) {
         isInputDigit(turnNumInputString);
@@ -16,7 +16,7 @@ public class TurnNumInputValidator {
         try {
             this.turnNumInputInt = Integer.parseInt(turnNumInputString);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ASK_NUMBER_INPUT);
+            throw new IllegalArgumentException(ASK_INT_UNDER_PROPER_AMOUNT);
         }
     }
 
