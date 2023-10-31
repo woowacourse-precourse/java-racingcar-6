1a5e2c6c9d9544cdfaf2096b4418fa74706ac8eb
조은별 <joeunbyeol98@gmail.com>
2023-10-31 20:07:49 +0900

    feat(CarServiceImpl): CarService 구현 클래스 생성, InputValidator로 자동차 이름 검증

---
+++ src/main/java/racingcar/model/service/CarServiceImpl.java
@@ -0,0 +1,35 @@
+package racingcar.model.service;
+
+import static racingcar.messages.ViewMessage.INPUT_SEPARATOR;
+
+import racingcar.dto.Car;
+import racingcar.model.dao.CarDao;
+import racingcar.model.dao.CarDaoImpl;
+import racingcar.validator.InputValidator;
+
+public class CarServiceImpl implements CarService {
+    private static CarService instance = new CarServiceImpl();
+    private InputValidator validator = InputValidator.getInstance();
+    private CarDao carDao = CarDaoImpl.getInstance();
+
+    private CarServiceImpl() {}
+    public static CarService getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void registerCarLineUp(String carNames) {
+        validator.validateNameDuplication(carNames);
+        validator.validateNameLengthRange(carNames);
+
+        String[] names = carNames.split(INPUT_SEPARATOR);
+        for (String name : names) {
+            carDao.insertCar(new Car(name.strip()));
+        }
+    }
+
+    @Override
+    public void registerTryCount(String tryCount) {
+
+    }
+}
