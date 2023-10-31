b15e9aa85c4105d1838189bfc52bd7dbc973ab67
yerimya <syl1602@naver.com>
2023-10-31 16:18:39 +0900

    [feat] inputview, outputview 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,19 @@
-package racingcar.view.InputView;public class OutputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import java.util.List;
+
+public class OutputView {
+    public static void printRoundResults(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + ": " + car.getPositionBar());
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        String message = "최종 우승자: " + String.join(", ", winners);
+        System.out.println(message);
+    }
 }
