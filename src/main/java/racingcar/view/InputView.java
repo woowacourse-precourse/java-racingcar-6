57a2e31336001cd9e62ac11b02026cb91faf47dc
Yang woosung <ngxk1217@gmail.com>
2023-10-31 21:35:33 +0900

    오류 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,6 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private InputView() {
-    }
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final String CAR_NAME_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
@@ -12,11 +10,30 @@ public class InputView {
 
     public static String carNames() {
         System.out.println(CAR_NAME_GUIDE);
-        return scanner.nextLine();
+        String carNames = scanner.nextLine();
+
+        //자동차 이름이 비어 있는 경우
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+        }
+        //자동차 이름이 5자 이하인지 확인
+        String[] carNameList = carNames.split(",");
+        if (carNameList.length > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+        return carNames;
     }
 
     public static int trialNumber() {
         System.out.println(TRIAL_NUMBER_GUIDE);
-        return scanner.nextInt();
+        int trialNumber = scanner.nextInt();
+
+        //시도할 회수가 0보다 작은 경우
+        if (trialNumber < 0) {
+            throw new IllegalArgumentException("시도할 회수는 0보다 커야 합니다.");
+        }
+        return trialNumber;
     }
+
+
 }
