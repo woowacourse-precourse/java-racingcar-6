b07c77095ff64d1c02db28f685de57686fe8bd0c
edgar6bf <edgar6bf@gmail.com>
2023-10-31 15:04:29 +0900

    refactor: OutputView 내부 클래스 메서드를 인스턴스 메서드로 변경
    OutputView를 GameController 내부로 의존 주입을 하기 위해서 OutputView 내부 클래스 메서드를 모두 인스턴스 메서드로 변경함.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -11,13 +11,13 @@ public class OutputView {
     private static final String PRINT_WINNERS_MESSAGE = "최종 우승자 : ";
     private static final String WINNER_LIST_DELIMITER = ", ";
 
-    public static void printRaceResult(List<RaceResult> raceResults) {
+    public void printRaceResult(List<RaceResult> raceResults) {
         System.out.println();
         System.out.println(PRINT_RACE_RESULT_MESSAGE);
-        raceResults.forEach(OutputView::printRaceStatus);
+        raceResults.forEach(this::printRaceStatus);
     }
 
-    private static void printRaceStatus(RaceResult raceResult) {
+    private void printRaceStatus(RaceResult raceResult) {
         raceResult.carStatuses().forEach(carStatus -> {
             String carMovementVisualString = createCarMovementVisualString(carStatus.position());
             System.out.println(carStatus.carName() + " : " + carMovementVisualString);
@@ -25,11 +25,11 @@ public class OutputView {
         System.out.println();
     }
 
-    private static String createCarMovementVisualString(int carPosition) {
+    private String createCarMovementVisualString(int carPosition) {
         return ONE_STEP.repeat(carPosition);
     }
 
-    public static void printWinners(List<String> winners) {
+    public void printWinners(List<String> winners) {
         String winnerList = String.join(WINNER_LIST_DELIMITER, winners);
         System.out.println(PRINT_WINNERS_MESSAGE + winnerList);
     }
