1998f6d20f90061837f3bc0668d46e82cc673ad1
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-31 16:28:30 +0900

    refactor(CarInfo): add parameter in isMove, updateCarStatus function
    Add parameter isMove function: isMove() -> isMove(int randomNum)
    Add parameter updateCarStatus function: updateCarStatus() -> updateCarStatus(boolean isMove, int carIndex)
    Modify logic updateCarStatus function to use parameter: no more repeat to set carStatus

--- src/main/java/racingcar/model/CarInfo.java
+++ src/main/java/racingcar/model/CarInfo.java
@@ -9,25 +9,7 @@ import racingcar.util.Validation;
 public class CarInfo {
     private final List<String> carName = new ArrayList<>();
     private final List<Integer> carStatus = new ArrayList<>();
-    public void setCarName(String carName) {
-        String[] splitStr = carName.split(",");
-        Collections.addAll(this.carName, splitStr);
-        for (String s : this.carName) {
-            Validation.checkCarName(s);
-        }
-    }
-    public boolean isMove() {
-        int randomNum = Randoms.pickNumberInRange(0,9);
-        return randomNum >= 4;
-    }
-    public void updateCarStatus() {
-        for (int i = 0; i < carName.size(); i++) {
-            if(isMove()){
-                int newValue = carStatus.get(i) + 1;
-                carStatus.set(i, newValue);
-            }
-        }
-    }
+
     public List<Integer> getCarStatus() {
         return carStatus;
     }
@@ -47,6 +29,26 @@ public class CarInfo {
         }
         return maxMoveList;
     }
+
+    public void setCarName(String carName) {
+        String[] splitStr = carName.split(",");
+        Collections.addAll(this.carName, splitStr);
+        for (String s : this.carName) {
+            Validation.checkCarName(s);
+        }
+    }
+
+    public boolean isMove(int randomNum) {
+        return randomNum >= 4;
+    }
+
+    public void updateCarStatus(boolean isMove, int carIndex) {
+        if(isMove){
+            int newValue = carStatus.get(carIndex) + 1;
+            carStatus.set(carIndex, newValue);
+        }
+    }
+
     public void initCarStatus() {
         for(int i = 0; i < carName.size(); i++){
             carStatus.add(0);
