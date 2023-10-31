fa7ca84af0e53657b0bf5eaa4275a3de33bff347
jun02160 <jun020216@naver.com>
2023-10-31 10:07:26 +0900

    test : 레이싱 라운드 진행 상황 및 결과 확인 테스트

--- src/test/java/racingcar/CarFeatureTest.java
+++ src/test/java/racingcar/CarFeatureTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.Round;
 
 public class CarFeatureTest {
 
@@ -29,6 +31,33 @@ public class CarFeatureTest {
         assertThat(advancedScore2).isEqualTo("-");   // 전진 X
         assertThat(advancedScore3).isEqualTo("--");   // 전진 O
 
+    }
+
+    @Test
+    public void 레이싱_1라운드_결과_저장() {
+        Round round = new Round();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+
+        round.init(Arrays.asList(car1, car2, car3), 3);
+        round.play();
+
+        assertThat(round.saveResult()).containsKey("car1");
+        assertThat(round.saveResult()).containsKey("car2");
+        assertThat(round.saveResult()).containsKey("car3");
+    }
+
+    @Test
+    public void 레이싱_종료_회차_미달_시_상황() {
+        Round round = new Round();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+
+        round.init(Arrays.asList(car1, car2, car3), 3);
+        assertThat(round.victory()).isEmpty();
+        assertThat(round.saveResult()).containsValues("");  // 초기값으로 Map에 넣어둔 ""값이 저장된 상태
     }
 
 }
