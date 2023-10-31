39a37c3cf195f098774c2026f4a748d2a6c11713
leedabin2 <12193152@inha.edu>
2023-10-31 22:14:53 +0900

    test: Add test for car name duplication validation
    Introduce a new test case to verify if an error is correctly thrown when the car names are duplicated.

--- src/test/java/racingcar/ConfirmationTest.java
+++ src/test/java/racingcar/ConfirmationTest.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.handler.UserInputHandler;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ConfirmationTest {
@@ -16,6 +20,15 @@ public class ConfirmationTest {
         assertThatThrownBy(() -> userInputHandler.validateName(nameList))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+    }
+
+    @Test
+    void 자동차이름_중복_예외_처리() {
+        List<String> nameList = Arrays.asList("dabin","dabin");
+
+        assertThatThrownBy(() -> userInputHandler.checkDuplicateName(nameList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
     }
 }
 
