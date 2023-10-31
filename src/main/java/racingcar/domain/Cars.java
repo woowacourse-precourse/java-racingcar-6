8178998d00a48a27d2d517e2c9432f6fd62ad00e
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-31 14:07:38 +0900

    refactor: 자동차 입력 유효성 검사를 Validator 클래스로 분리

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,11 +1,11 @@
 package racingcar.domain;
 
-
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import racingcar.validator.Validator;
 
 public class Cars {
 
@@ -13,33 +13,17 @@ public class Cars {
 
     public Cars(String carsInput) {
         List<String> carsList = convertToList(carsInput);
-        validateSize(carsList);
-        validateName(carsList);
-        validateDuplicate(carsList);
+        Validator.validateSize(carsList);
+        Validator.validateName(carsList);
+        Validator.validateDuplicate(carsList);
         this.maps = generateCarsMap(carsList);
     }
 
-    private void validateDuplicate(List<String> carsList) {
-        if (carsList.stream().distinct().count() != carsList.size()) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 중복이 되면 안됩니다.");
-        }
-    }
 
     private List<String> convertToList(String carsInput) {
         return Arrays.stream(carsInput.split(",")).collect(Collectors.toList());
     }
 
-    private void validateName(List<String> carsList) {
-        if (carsList.stream().anyMatch(car -> car.length() > 5 || car.isEmpty())) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 1글자 이상 5글자 이하여야합니다.");
-        }
-    }
-
-    private void validateSize(List<String> carsList) {
-        if (carsList.size() < 2) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 대수는 2대 이상이여야 합니다.");
-        }
-    }
 
     private Map<String, Integer> generateCarsMap(List<String> carsList) {
         Map<String, Integer> carsMap = new LinkedHashMap<>();
