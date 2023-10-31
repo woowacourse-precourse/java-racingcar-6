48034da19a6f4c3f09600a4bf34370929260c2ce
alenjb <alenjb@naver.com>
2023-11-01 02:18:51 +0900

    :white_check_mark:test(racingcar): 자동차 이름들을 쉼표로 분리하는 기능 테스트 추가
    Inputs.java에 경주 할 자동차 이름들을 쉼표(,) 기준으로 분리하는 메서드 splitCarNames를 테스트하는
    '자동차_이름들을_쉼표로_분리' 메서드를 ApplicationTest.java에 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,25 +1,73 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    Inputs inputs = new Inputs();
+
+    @Test
+    void 자동차_이름들을_쉼표로_분리() {
+        //자동차 이름이 1개인 경우
+        String carName = "자동차1";
+        //자동차 이름이 여러개인 경우
+        String carNames = "자동차1,자동차2,자동차3";
+        //자동차 이름이 5글자를 넘는 경우
+        String tooLongCarName = "자동차이름5글자이상";
+        //자동차 이름이 5글자를 넘고 쉼표로 구분된 경우
+        String tooLongCarNames = "자동차이름5글자이상,자동차1";
+        //콤마 이후에 공백이 있는 경우
+        String carNameWithSpace = "자동차1, 자동차2";
+
+        //자동차 이름이 1개인 경우
+        inputs.carNamesList.clear();
+        inputs.splitCarNames(carName);
+        assertThat(inputs.carNamesList).containsExactly(carName);
+
+        //자동차 이름이 여러개인 경우
+        inputs.carNamesList.clear();
+        inputs.splitCarNames(carNames);
+        assertThat(inputs.carNamesList).containsExactly("자동차1", "자동차2", "자동차3");
+
+        //자동차 이름이 5글자를 넘는 경우
+        inputs.carNamesList.clear();
+        Throwable exception1 = catchThrowable(() -> {
+            inputs.splitCarNames(tooLongCarName);
+        });
+        assertThat(exception1).isInstanceOf(IllegalArgumentException.class).hasMessage("비정상적인 값을 입력하셨습니다. 경주를 자동으로 종료합니다.");
+
+        //자동차 이름이 5글자를 넘고 쉼표로 구분된 경우
+        inputs.carNamesList.clear();
+        Throwable exception2 = catchThrowable(() -> {
+            inputs.splitCarNames(tooLongCarNames);
+        });
+        assertThat(exception2).isInstanceOf(IllegalArgumentException.class).hasMessage("비정상적인 값을 입력하셨습니다. 경주를 자동으로 종료합니다.");
+        //콤마 이후에 공백이 있는 경우
+        inputs.carNamesList.clear();
+        inputs.splitCarNames(carNameWithSpace);
+        assertThat(inputs.carNamesList).containsExactly("자동차1", " 자동차2");
+
+    }
+
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
