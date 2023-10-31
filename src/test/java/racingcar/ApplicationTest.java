1a676a833cefc5e8189d390c875d7c8ed56f9498
kymin <park48664866@gmail.com>
2023-10-31 15:46:47 +0900

    test: 테스트코드 추가
    테스트코드 추가:
    - 사용자로부터 입력받은 시도횟수에 대해 예외처리를 검증하는 테스트코드 추가
    - 레이스 결과를 출력할 때 요구되는 문자열 형식으로의 변환과 정상 출력 여부를 검증하는 테스트코드 추가
    - 최종우승자를 출력할 때 요구되는 문자열 형식으로의 변환과 정상 출력 여부를 검증하는 테스트코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.ListOfCar;
+import racingcar.view.Output;
+import racingcar.domain.Race;
+import racingcar.model.Car;
+import java.util.Arrays;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,24 +20,49 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP);
     }
 
     @Test
     void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 시도횟수에_대한_예외_처리() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobi,jun", "-3"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 레이스결과_출력에_대한_문자열_처리() {
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("jun");
+        Car car3 = new Car("woni");
+        Race.advance(car1);
+        Race.advance(car3);
+        Race.advance(car3);
+        ListOfCar cars = new ListOfCar(Arrays.asList(car1, car2, car3));
+        Output.printResult(cars);
+        assertThat(output()).contains("pobi : -", "jun :", "woni : --");
+    }
+
+    @Test
+    void 최종우승자_출력에_대한_문자열_처리() {
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("jun");
+        ListOfCar cars = new ListOfCar(Arrays.asList(car1, car2));
+        Output.printWinner(cars);
+        assertThat(output()).isEqualTo("최종 우승자 : pobi, jun");
     }
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }
