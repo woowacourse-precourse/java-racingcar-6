adfb20b7eca619b7df75ff4ad4534daea3d908f4
acycoco <pink10257@naver.com>
2023-11-01 03:27:44 +0900

    feat(InputValidator): 문자열을 "," 구분하는 기능 추가 - parsing하고 각 문자열의 앞뒤 공백 제거

--- src/main/java/racingcar/input/InputParser.java
+++ src/main/java/racingcar/input/InputParser.java
@@ -1,9 +1,13 @@
 package racingcar.input;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputParser {
     public static List<String> parseCarNames(String input) {
-        return List.of(input.split(","));
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
