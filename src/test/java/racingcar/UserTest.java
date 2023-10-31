ccefa5c027eee69fb5fbef3957139a9f5709d1bf
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-31 22:50:51 +0900

    [Test](UserTest) - TestCode 추가

--- src/test/java/racingcar/UserTest.java
+++ src/test/java/racingcar/UserTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
-class UserTest {
-  
+package racingcar;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+    private final InputStream originalSystemIn = System.in;
+
+    @BeforeEach
+    public void setUpInput() {
+        // 테스트 전에 원래의 System.in을 저장하고 사용자 입력을 재지정
+        String input = "pobi,MJ,tjwls";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    @AfterEach
+    public void restoreSystemIn() {
+        // 테스트 후에 원래의 System.in을 복원합니다.
+        System.setIn(originalSystemIn);
+    }
+
+    @Test
+    public void testInputCarName() {
+        User user = new User();
+        String carName = user.inputCarName();
+        assertEquals("pobi,MJ,tjwls", carName);
+    }
 }
