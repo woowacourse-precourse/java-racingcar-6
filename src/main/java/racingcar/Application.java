c3c0af2e1af2aad6e67e2db3e3ef37f208e5a386
Leegeonwoo7 <workgeon07@naver.com>
2023-11-01 05:25:42 +0900

    실행 클래스

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	ArrayList<Car> carList = new ArrayList<>();
+    	String[] nameArr = Input.setCarName();
+    	for(int i=0; i<nameArr.length; i++) {
+    		Car car = new Car(nameArr[i]);
+    		carList.add(car); 
+    	}
+    	
+    	int attemptNum = Input.setAttemptNum();
+    	ScoreBoard.moveCar(carList, attemptNum);
+    	ArrayList<String> winner = Winner.resultWinner(carList, Move.carLocation);
+    	System.out.println("최종 우승자 : " + String.join(", ", winner));
     }
 }
