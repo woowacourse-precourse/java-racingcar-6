dc2d40363a1104c0b00ecd1050a7ffa0de39669c
sonjeongdong <sjd0219@gmail.com>
2023-10-31 17:00:31 +0900

    Refactor: 람다식과 stream()을 사용하여 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         playRacingGame();
     }
+
     private static void playRacingGame() {
-        printAskForInputCarNames();
         List<Car> cars = createCars(getCarNames(getInputLineOfCarNames()));
-        printAskForInputNumberOfAttempts();
         int numberOfAttempts = getNumberOfAttempts();
         printHeadResult();
-
         for (int i = 0; i < numberOfAttempts; i++) {
             attemptOneRound(cars);
             printOneRoundResult(cars);
@@ -23,11 +23,8 @@ public class Application {
         printWinner(cars);
     }
 
-    private static void printAskForInputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    }
-
     private static String getInputLineOfCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return validateInputLineOfCarNames(Console.readLine());
     }
 
@@ -53,15 +50,15 @@ public class Application {
     }
 
     private static void checkCarNameForm(List<String> carNames) {
-        for (String carName : carNames) {
+        carNames.forEach(carName -> {
             validateCarNameRegex(carName);
             validateMaxLengthOfCarName(carName);
-        }
+        });
     }
 
     private static void validateCarNameRegex(String carName) {
         String regex = "^[a-zA-Z]+[a-zA-Z0-9]*$";
-        if (!carName.matches(regex)) {
+        if (!Pattern.matches(regex, carName)) {
             throw new IllegalArgumentException("이름 형식이 맞지 않습니다.");
         }
     }
@@ -74,27 +71,21 @@ public class Application {
 
     private static void validateDuplicateOfCarName(List<String> carNames) {
         Set<String> uniqueNames = new HashSet<String>();
-        for (String carName : carNames) {
+        carNames.forEach(carName -> {
             if (!uniqueNames.add(carName)) {
                 throw new IllegalStateException("이름이 중복됩니다.");
             }
-        }
+        });
     }
 
     private static List<Car> createCars(List<String> carNames) {
-        List<Car> cars = new ArrayList<Car>();
-        for (int i = 0; i < carNames.size(); i++) {
-            Car car = new Car(carNames.get(i));
-            cars.add(car);
-        }
-        return cars;
-    }
-
-    private static void printAskForInputNumberOfAttempts() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     private static int getNumberOfAttempts() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(checkNumberOfAttemptsForm(Console.readLine()));
     }
 
@@ -106,13 +97,13 @@ public class Application {
 
     private static void validateIsNumber(String numberOfAttempts) {
         String numberRegex = "^[0-9]*$";
-        if (!numberOfAttempts.matches(numberRegex)) {
+        if (!Pattern.matches(numberRegex,numberOfAttempts)) {
             throw new IllegalArgumentException("숫자 형식이 아닙니다.");
         }
     }
 
     private static void validateIsZero(String numberOfAttempts) {
-        if (numberOfAttempts.matches("0")) {
+        if (Pattern.matches("0",numberOfAttempts)) {
             throw new IllegalArgumentException("0은 입력할 수 없습니다.");
         }
     }
@@ -122,32 +113,28 @@ public class Application {
     }
 
     private static void attemptOneRound(List<Car> cars) {
-        for (Car car : cars) {
+        cars.forEach(car -> {
             if (isMove()) {
                 car.moveForward();
             }
-        }
+        });
     }
 
     private static boolean isMove() {
-        return Randoms.pickNumberInRange(0,9) >= 4;
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 
     private static void printOneRoundResult(List<Car> cars) {
-        for (Car car : cars) {
-            System.out.println(car.toString());
-        }
+        cars.forEach(car -> System.out.println(car.toString()));
         System.out.println();
     }
 
     private static List<String> findWinner(List<Car> cars) {
         int maxPosition = findMaxPosition(cars);
-        List<String> winner = new ArrayList<String>();
-        for (Car car : cars) {
-            if (car.getPosition() == maxPosition) {
-                winner.add(car.getName());
-            }
-        }
+        List<String> winner = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
         return winner;
     }
 
