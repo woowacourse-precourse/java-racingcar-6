e64b4e442e8198401ba46b416e693e6a708da43d
limzzum <chfhddl1234@naver.com>
2023-10-31 21:10:53 +0900

    :sparkles: feat(Car) : 0,9 사이 무작위 값 추출 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,17 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int moveCnt;
 
     public Car(String name){
         this.name = name;
+    }
+
+    public int getRandomNum(){
+        return Randoms.pickNumberInRange(0, 9);
     }
 
 }
