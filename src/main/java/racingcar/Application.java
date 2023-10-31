b9be8c4871d90fba201704bb48b1d229c683e967
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-31 15:52:47 +0900

    feat: 게임 시작 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.io.InputManager;
+import racingcar.io.OutputManager;
+import racingcar.manager.GameManager;
+import racingcar.utils.generator.NumberGenerator;
+import racingcar.utils.generator.RandomNumberGenerator;
+import racingcar.utils.provider.ConsoleInputProvider;
+import racingcar.utils.provider.InputProvider;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputProvider inputProvider = new ConsoleInputProvider();
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        OutputManager outputManager = new OutputManager();
+        InputManager inputManager = new InputManager(outputManager, inputProvider);
+        GameManager gameManager = new GameManager(inputManager, outputManager, numberGenerator);
+        gameManager.startGame();
     }
 }
