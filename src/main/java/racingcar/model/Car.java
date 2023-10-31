788922d1cef54ba506cf98b6ccd33bcbf8fed698
Choo <choo000407@naver.com>
2023-10-31 23:18:36 +0900

    feat(InputView): 자동차 이름들 입력 시 리스트 속 `빈 문자열` 예외 처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,19 +5,8 @@ public class Car {
     private final String name;
 
     public Car(String name) {
-        validateName(name);
-        this.name = name;
-    }
-
-    private void validateName(String name) {
-        checkNameNotEmpty(name);
         checkNameLength(name);
-    }
-
-    private void checkNameNotEmpty(String name) {
-        if(name.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름은 1자 이상이어야 합니다.");
-        }
+        this.name = name;
     }
 
     private void checkNameLength(String name) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -21,18 +21,31 @@ public class InputView {
     }
 
     private List<String> convertInputCarNamesToList(String inputCarNames) {
-        List<String> carNames = Arrays.stream(inputCarNames.split(",")).toList();
+        List<String> carNames = Arrays.stream(inputCarNames.split(","))
+                .map(String::trim)
+                .peek(this::checkCarNameNotEmpty)
+                .toList();
+        checkCarNamesNotEmpty(carNames);
+        return carNames;
+    }
+
+    private void checkCarNameNotEmpty(String carName) {
+        if(carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 앞뒤 공백을 제외한 1자 이상으로 입력해야 합니다.");
+        }
+    }
+
+    private void checkCarNamesNotEmpty(List<String> carNames) {
         if(carNames.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름을 하나 이상 입력해야 합니다.");
         }
-        return carNames;
     }
 
     private int convertInputTryCountToInt(String inputTryCount) {
         try {
             return Integer.parseInt(inputTryCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 횟수는 정수여야 합니다.");
+            throw new IllegalArgumentException("시도할 횟수는 정수로 입력해야 합니다.");
         }
     }
 }
