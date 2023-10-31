34e7610eff84d1e3b968b425034f488fb0939245
minjong kim <godminjong@naver.com>
2023-10-31 17:29:51 +0900

    test: 콘솔로 입력받은 라운드 수가 음수 일 경우의 테스트
    입력받은 숫자가 -1인 경우를 가정하여 검증 기능이 성공으로 작동하여 IllegalArgumentException 예외를 던지는가에 대해 확인하는 테스트 작성.

--- src/test/java/racingcar/validate/impls/NumberOfParticipantValidatorTest.java
+++ src/test/java/racingcar/validate/impls/NumberOfParticipantValidatorTest.java
@@ -10,6 +10,16 @@ import racingcar.validate.Validator;
 class NumberOfParticipantValidatorTest {
 
     Validator validator = new NumberOfParticipantValidator();
+    @Test
+    @DisplayName("콘솔로 음수가 입력되었을때")
+    void negativePlayerTest() throws Exception{
+        //given
+        String sample = "-1";
+        //then
+        Assertions.assertThatThrownBy(() -> validator.validate(sample))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("0 이상의 정수만 입력해주세요.");
+     }
 
      @Test
      @DisplayName("정상적인 검증 시")
