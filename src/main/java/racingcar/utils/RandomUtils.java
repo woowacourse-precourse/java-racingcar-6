b30c1e370b5c18453d0556d0fe2eac7424f8f81f
ChaeAg <dydkglel123@gmail.com>
2023-10-31 14:54:52 +0900

    refactor(RandomUtils): 랜덤 수 생성 예외 처리 구현

--- src/main/java/racingcar/utils/RandomUtils.java
+++ src/main/java/racingcar/utils/RandomUtils.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomUtils {
     public static int getRandomNumber(int randomStartNumber, int randomLastNumber) {
+        if (randomStartNumber > randomLastNumber) {
+            throw new IllegalArgumentException();
+        }
+        if (randomStartNumber < 0) {
+            throw new IllegalArgumentException();
+        }
         if (randomStartNumber == randomLastNumber) {
             return randomStartNumber;
         }
+
         return Randoms.pickNumberInRange(randomStartNumber, randomLastNumber);
     }
 }
