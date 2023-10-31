f17e70aaeadf1dc54b1a46233e603b3285eecf96
yyubin <hazing120@gmail.com>
2023-11-01 02:55:17 +0900

    feat: game 시작 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.service.GameService;
+import racingcar.util.MessageProcessor;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MessageProcessor messageProcessor = new MessageProcessor();
+        GameController gameController = new GameController(messageProcessor, new GameService(messageProcessor));
+        gameController.runGame();
     }
 }
