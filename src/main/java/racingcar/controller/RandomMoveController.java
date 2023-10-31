b0830ec0d2c0cd3ba268859d7c49724cdaba4185
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-10-31 17:31:49 +0900

    feat: RandomNumbers 에서 threshold 이상인 경우 자동차가 전진하는 기능 개발

---
+++ src/main/java/racingcar/controller/RandomMoveController.java
@@ -0,0 +1,26 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.service.RandomNumbers;
+
+public class RandomMoveController {
+    private final RandomNumbers randomNumbers;
+
+    public RandomMoveController(RandomNumbers randomNumbers) {
+        this.randomNumbers = randomNumbers;
+    }
+
+    public void moveForward(Cars cars) {
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            if (isForward(i)) {
+                cars.moveForwardValueOf(i);
+            }
+        }
+    }
+
+    private Boolean isForward(int index) {
+        int MOVEMENT_THRESHOLD = 4;
+        return randomNumbers.isGreaterEqualThan(index, MOVEMENT_THRESHOLD);
+    }
+
+}
