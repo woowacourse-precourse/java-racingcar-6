23de62a9cc971756e2164ffae75d3d0ccfc5d5ef
flown222 <jangu3384@gmail.com>
2023-10-31 18:30:38 +0900

    test: 기존 GameTimeValidator 테스트를 GameTimeTest로 변경

--- src/test/java/racingcar/model/GameTimeTest.java
+++ src/test/java/racingcar/model/GameTimeTest.java
@@ -1,22 +1,20 @@
-package racingcar.util;
+package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-class GameTimeValidatorTest {
+class GameTimeTest {
     @Test
     void 이동횟수제한_1에서_100사이의_숫자만_허용_실패() {
-        int failGameTime = 101;
-        assertThatThrownBy(() -> GameTimeValidator.validateGameTime(failGameTime))
+        assertThatThrownBy(() -> new GameTime(101))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이동 횟수는 1부터 100까지 가능합니다.");
     }
 
     @Test
     void 이동횟수제한_1에서_100사이의_숫자만_허용_성공() {
-        int successGameTime = 5;
-        assertThat(GameTimeValidator.validateGameTime(successGameTime)).isEqualTo(successGameTime);
+        assertThat(new GameTime(5)).isInstanceOf(GameTime.class);
     }
 }
