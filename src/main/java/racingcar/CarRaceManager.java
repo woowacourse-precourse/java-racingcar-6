f1e0cfa6b4c7a800eec0a0f3753745511950d21c
Seungjida <esg0107@pusan.ac.kr>
2023-10-31 11:36:48 +0900

    feat(winner): 경주 우승자를 판단하고 출력하는 기능 구현 winner와 관련된 새로운 기능들 구현:
    - winnerJudgment(): 전진한 거리값이 가장 큰 차의 이름을 리스트에 저장
    - printWinner(): 정해진 출력 양식에 맞게 우승자 리스트의 값을 출력

--- src/main/java/racingcar/CarRaceManager.java
+++ src/main/java/racingcar/CarRaceManager.java
@@ -8,11 +8,13 @@ import java.util.Map;
 
 public class CarRaceManager {
     private List<Car> racingCars = new ArrayList<>();
-    private final List<String> winnerList= new ArrayList<>();
+    private final List<String> winnerList = new ArrayList<>();
     private int carCount = 0;
 
-    public CarRaceManager () {}
-    public CarRaceManager (final List<Car> newRacingCars) {
+    public CarRaceManager() {
+    }
+
+    public CarRaceManager(final List<Car> newRacingCars) {
         racingCars = newRacingCars;
         carCount = racingCars.size();
     }
@@ -44,10 +46,30 @@ public class CarRaceManager {
     }
 
     public void winnerJudgment() {
+        int max = 0;
+        for (Car car : racingCars) {
+            if (car.getMovingCount() < max) {
+                continue;
+            } else if (car.getMovingCount() > max) {
+                winnerList.clear(); // 안에 뭐 있는지 확인  안해도 오류 안 나려나?
+                max = car.getMovingCount();
+            }
+            winnerList.add(car.getCarName());
+        }
     }
+
     public List<String> getWinnerList() {
         return winnerList;
     }
+
     public void printWinner() {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winnerList.size(); i++) {
+            if (i == winnerList.size() - 1) {
+                System.out.println(winnerList.get(i));
+            } else {
+                System.out.print(winnerList.get(i) + ", ");
+            }
+        }
     }
 }
