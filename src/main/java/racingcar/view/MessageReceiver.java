0684a8637900469756d75c7ee6f7c994050cd7c8
Jung-YouHwan <yhwjd99@gmail.com>
2023-10-31 12:13:21 +0900

    refactor : 자동차 이름 검증 세분화

--- src/main/java/racingcar/view/MessageReceiver.java
+++ src/main/java/racingcar/view/MessageReceiver.java
@@ -3,7 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static racingcar.constant.NumberConstant.*;
 import static racingcar.constant.TextConstant.COMMA;
@@ -13,7 +15,7 @@ public class MessageReceiver {
     public List<String> receiveCarNames() {
         String inputText = Console.readLine();
         List<String> carNames = Arrays.asList(inputText.split(COMMA));
-        validateCarName(carNames);
+        validateCarNames(carNames);
 
         return carNames;
     }
@@ -26,13 +28,27 @@ public class MessageReceiver {
         return attemptCount;
     }
 
-    private void validateCarName(final List<String> carNames) {
+    private void validateCarNames(final List<String> carNames) {
+        validateLength(carNames);
+        validateDuplication(carNames);
+    }
+
+    private void validateLength(final List<String> carNames) {
         for (String carName : carNames) {
             if (carName.length() > MAX_CAR_NAME_SIZE) {
                 throw new IllegalArgumentException();
             }
         }
     }
+
+    private void validateDuplication(final List<String> carNames) {
+        Set<String> carNameSet = new HashSet<>(carNames);
+
+        if (carNames.size() != carNameSet.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     private void validateAttemptCount(final int attemptCount) {
         if (attemptCount < MIN_ATTEMPT_COUNT) {
