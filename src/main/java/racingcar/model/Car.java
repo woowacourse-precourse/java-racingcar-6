bf467e37cff6e9af3e18232a5c90bb8ccbc9876b
Isaac Han <isaachan233@gmail.com>
2023-11-01 01:24:58 +0900

    refactor: distance를 Integer 형으로 변화  및 네이밍 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -7,29 +7,29 @@ import racingcar.utils.NumberGenerator;
 import racingcar.utils.RandomGenerator;
 
 public class Car {
-    private static final String DISTANCE_MARK = "-";
+    private static final int MAX_MOVE_DISTANCE = 1;
     private final String name;
-    private final StringBuilder distance;
+    private Integer distance;
 
     public Car(String name) {
         validateCarNameInput(name);
         this.name = name;
-        this.distance = new StringBuilder();
+        this.distance = 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDistance() {
-        return distance.toString();
+    public int getDistance() {
+        return distance;
     }
 
-    public void moveForward() {
+    public void moveForwardByRandom() { // TODO:
         NumberGenerator randomGenerator = new RandomGenerator();
         int randomNumber = randomGenerator.generateNumber(Common.MIN_NUMBER.getIntValue(), Common.MAX_NUMBER.getIntValue());
         if (randomNumber >= Common.STANDARD_NUMBER.getIntValue())
-            distance.append(DISTANCE_MARK);
+            distance += MAX_MOVE_DISTANCE;
     }
 
     private void validateCarNameInput(String carName) {
@@ -37,7 +37,7 @@ public class Car {
             throw new IllegalArgumentException(ErrorMessages.NULL_ERROR.getMessage());
         if (carName.isBlank())
             throw new IllegalArgumentException(ErrorMessages.EMPTY_ERROR.getMessage());
-        if (CommonValidator.isOverMaxLength(carName))
+        if (CommonValidator.isStringOverLength(carName, Common.STANDARD_NUMBER.getIntValue()))
             throw new IllegalArgumentException(ErrorMessages.OVER_LENGTH_ERROR.getMessage());
         if (carName.contains(Common.SEPARATOR.getStringValue()))
             throw new IllegalArgumentException(ErrorMessages.SEPARATOR_ERROR.getMessage());
