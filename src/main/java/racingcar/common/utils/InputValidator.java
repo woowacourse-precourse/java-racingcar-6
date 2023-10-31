af4f1ba324aa9177e43a773092662f2aaafc315a
jifrozen0110 <answldjs1836@gmail.com>
2023-10-31 18:06:54 +0900

    style(utils) : reformat code

--- src/main/java/racingcar/common/utils/InputValidator.java
+++ src/main/java/racingcar/common/utils/InputValidator.java
@@ -1,15 +1,14 @@
 package racingcar.common.utils;
 
-import racingcar.common.consts.SystemConst;
 import racingcar.common.consts.SystemMessage;
 import racingcar.view.OutputView;
 
 public class InputValidator {
-    public static int validateInputNumber(String number){
-        try{
-            int n=Integer.parseInt(number);
+    public static int validateInputNumber(String number) {
+        try {
+            int n = Integer.parseInt(number);
             return n;
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             OutputView.printErrMessage(SystemMessage.INVALID_INPUT_NUMBER_ERROR);
             throw new IllegalArgumentException(SystemMessage.INVALID_INPUT_NUMBER_ERROR);
         }
--- src/main/java/racingcar/common/utils/NumberGenerator.java
+++ src/main/java/racingcar/common/utils/NumberGenerator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.common.consts.SystemConst;
 
 public class NumberGenerator {
-    public static int makeRandomNumber(){
-        return Randoms.pickNumberInRange(SystemConst.MIN_DIGIT_SIZE,SystemConst.MAX_DIGIT_SIZE);
+    public static int makeRandomNumber() {
+        return Randoms.pickNumberInRange(SystemConst.MIN_DIGIT_SIZE, SystemConst.MAX_DIGIT_SIZE);
     }
 }
