10a9314157646b7fa4f58954b25fa87d12bf37b7
sequaa <asl06194@gmail.com>
2023-10-31 17:15:21 +0900

    feat: Application 기능 추가 입력 값의 유효성 검사를 하는 기능을 추가했습니다. 입력 되는 자동차의 이름은 5자를 넘지 않게 했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,7 +11,11 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = Console.readLine();
         List<String> carNames = Arrays.asList(inputNames.split(","));
-//      List<String> carNames = Arrays.asList(Console.readLine().split(","));
+
+        for (String name: carNames) {
+            if (name.length() > 5) throw new IllegalArgumentException();
+        }
+
 
         System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = Integer.parseInt(Console.readLine());
