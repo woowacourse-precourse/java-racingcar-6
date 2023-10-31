e33f6d9dc0857563cb71a1081023a963b726e609
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-31 17:43:15 +0900

    feat(OutputView) : 출력에 관한 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.consts.ViewConstant;
+
+public class OutputView {
+    public void printCarNamePrompt() {
+        System.out.println(ViewConstant.RACING_CAR_PROMPT_MESSAGE.get());
+    }
+
+    public void printNumberOfAttemptPrompt() {
+        System.out.println(ViewConstant.NUMBER_OF_ATTEMPT_PROMPT_MESSAGE.get());
+        ;
+    }
+
+    public void printResultMessage() {
+        System.out.println(ViewConstant.RUN_RESULT_PRINT_MESSAGE.get());
+    }
+
+    public void printResultRound(List<String> result) {
+        result.forEach(System.out::println);
+        System.out.println();
+    }
+
+    public void printWinner(List<String> winners) {
+        System.out.println(ViewConstant.WINNER_PRINT_MESSAGE + String.join(ViewConstant.DELIMITER.get(), winners));
+    }
+}
