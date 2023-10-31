239262eb9a8941fa5f4347bed57725b984b7d211
gahyeon <1000ji1@naver.com>
2023-11-01 01:14:52 +0900

    feat : register Car
    사용자로부터 차에 대한 이름을 입력 받고, 차를 등록

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,17 +1,36 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
+import racingcar.model.Exception;
+import racingcar.view.InputView;
 
 public class RacingGame {
-    private List<Car> carList;
+    private final List<Car> carList = new ArrayList<>();
     private int attemptCount;
 
     RacingGame() {
-
     }
 
     public void start() {
-        
+        registerCars();
     }
+
+    public void registerCars() {
+        String carNames = InputView.carNames();
+        String[] names = carNames.split(",");
+
+        Exception.isSpace(names);
+
+        for (String name : names) {
+            addCar(name);
+        }
+    }
+
+    private void addCar(String name) {
+        Car car = new Car(name);
+        carList.add(car);
+    }
+
 }
