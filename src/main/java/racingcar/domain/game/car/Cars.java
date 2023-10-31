01d14dc0cb6de4274e2e0eeb0af5e61aca1adc27
minisyu <iswater.kim@gmail.com>
2023-10-31 20:35:25 +0900

    refactor(Cars): 최종 우승자를 가려내는 메소드의 이름과 로직 변경

--- src/main/java/racingcar/domain/game/car/Cars.java
+++ src/main/java/racingcar/domain/game/car/Cars.java
@@ -1,14 +1,14 @@
 package racingcar.domain.game.car;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.domain.dto.CarMovementDto;
 import racingcar.domain.dto.CarsMovementDto;
 import racingcar.domain.dto.WinnersDto;
-import racingcar.util.MoveStrategy;
+import racingcar.move.MoveStrategy;
 
 public class Cars {
-    private static final int MIN_POSITION = 0;
     private static final String WINNER_NAME_DELIMITER = ", ";
     private final List<Car> cars;
 
@@ -36,22 +36,21 @@ public class Cars {
     }
 
     public WinnersDto toWinnerDto() {
-        String winners = getWinners();
+        String winners = findWinners();
         return new WinnersDto(winners);
     }
 
-    private String getWinners() {
+    private String findWinners() {
         return cars.stream()
-                .filter(car -> car.isSameAs(getMaxPosition()))
+                .filter(car -> car.isSameAs(findCarWithMaxPosition()))
                 .map(Car::getCarName)
                 .collect(Collectors.joining(WINNER_NAME_DELIMITER));
     }
 
-    private int getMaxPosition() {
-        return cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(MIN_POSITION);
+    private Car findCarWithMaxPosition() {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException("자동차 목록이 비어 있습니다.");
+        }
+        return Collections.max(cars);
     }
-
 }
