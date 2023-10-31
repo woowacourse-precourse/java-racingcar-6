171a50c806ea256bc78f7ac275df84580d2b1cf4
junest66 <chlwndks333@gmail.com>
2023-10-31 19:28:47 +0900

    feat(AttemptCountValidatorTest): 시도 횟수 최소 입력 검증 기능 테스트 추가 및 에러메시지 검증 추가

--- src/test/java/racingcar/validator/AttemptCountValidatorTest.java
+++ src/test/java/racingcar/validator/AttemptCountValidatorTest.java
@@ -4,20 +4,34 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttemptCountValidatorTest {
 
-    @Test
-    void 시도횟수_문자입력_예외발생() {
-        String input = "abc";
-        assertThatThrownBy(() -> AttemptCountValidator.validateAttemptCount(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    private static final String INVALID_INPUT_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private static final String MIN_ATTEMPT_COUNT_ERROR_MESSAGE = "1이상을 입력해주세요";
 
     @Test
     void 시도횟수_정상입력시_숫자변환() {
         String input = "5";
         int attemptCount = AttemptCountValidator.validateAttemptCount(input);
         assertThat(attemptCount).isEqualTo(5);
+    }
+
+    @Test
+    void 시도횟수_문자입력_예외발생() {
+        String input = "abc";
+        assertThatThrownBy(() -> AttemptCountValidator.validateAttemptCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_INPUT_ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "-2"})
+    void 시도횟수_1미만입력_예외발생(String input) {
+        assertThatThrownBy(() -> AttemptCountValidator.validateAttemptCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(MIN_ATTEMPT_COUNT_ERROR_MESSAGE);
     }
 }
