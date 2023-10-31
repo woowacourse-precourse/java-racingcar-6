55f673b70900539f8e9f0324f787b8180a527cd2
Nara Park <skfk6352@naver.com>
2023-11-01 06:44:26 +0900

    <test>: check appropriate message is output when exception occurs
    예외가 발생할 경우 적절한 예외가 발생하는지 확인하는 테스트 메서드로 리팩토링

--- src/test/java/racingcar/validation/GameCountValidatorTest.java
+++ src/test/java/racingcar/validation/GameCountValidatorTest.java
@@ -1,9 +1,11 @@
 package racingcar.validation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.constant.ExceptionMessage;
 
 class GameCountValidatorTest {
 
@@ -16,8 +18,9 @@ class GameCountValidatorTest {
     }
 
     @Test
-    void 입력값이_자연수가_아니라면_예외발생() {
-        assertThrows(IllegalArgumentException.class,
-                () -> gameCountValidator.validateGameCount("0"));
+    void 자동차_이름이_중복일경우_예외발생() {
+        assertThatThrownBy(() -> gameCountValidator.validateGameCount("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.GAME_COUNT_TYPE.getMessage());
     }
 }
