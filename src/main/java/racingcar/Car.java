f238f918f725d955fdc307ee82bc3affb942dd41
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-31 17:09:40 +0900

    refactor: merge number checking function

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,15 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Optional;
+
 public class Car {
     private String name;
     private int position = 0;
-    private NumberCondition numberCondition = new NumberCondition();
-    public Car(String name) { this.name = name; }
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void printMoves() {
         System.out.println(this.name + " : " + "-".repeat(this.position));
@@ -14,10 +18,17 @@ public class Car {
 
     public void trial() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
-        this.position += this.numberCondition.compareFour(randomNumber);
+        this.position += Optional.of(randomNumber)
+                .filter(n -> n >= 4)
+                .map(n -> 1)
+                .orElse(0);
     }
 
-    public String getName() { return this.name; }
+    public String getName() {
+        return this.name;
+    }
 
-    public int getPosition() { return this.position; }
+    public int getPosition() {
+        return this.position;
+    }
 }
