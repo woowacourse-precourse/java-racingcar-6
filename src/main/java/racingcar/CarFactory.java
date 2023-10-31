14bdce7a379e19025bc3e876ca19f0ad7a2aba4e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-31 16:46:44 +0900

    feat(carFactory): 자동차 여러 개 생성 시 중복 예외 처리

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class CarFactory {
     private final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이하, 1자 이상만 가능합니다.";
+    private final String DUPLICATE_CAR_NAME = "자동차 이름은 중복될 수 없습니다.";
 
     public Car generate(String carName) {
         validateCarNameLength(carName);
@@ -11,14 +12,24 @@ public class CarFactory {
     }
 
     public List<Car> generate(List<String> names) {
-        return names.stream()
+        List<Car> carList = names.stream()
+                .distinct()
                 .map(this::generate)
                 .toList();
+
+        validateCarDuplication(names.size(), carList.size());
+        return carList;
     }
 
     private void validateCarNameLength(String carName) {
         if (carName.length() > 5 || carName.length() < 1) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
+        }
+    }
+
+    private void validateCarDuplication(int nameListSize, int carListSize) {
+        if (nameListSize != carListSize) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME);
         }
     }
 }
