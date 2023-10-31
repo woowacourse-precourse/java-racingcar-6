469bb621b99d782722cc114f16a2b7cfc9511f5e
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 03:07:58 +0900

    test: 이동 횟수 입력 받기 테스트 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,14 +2,17 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import racingcar.validation.MoveCountInputValidation;
 import racingcar.validation.NameInputValidation;
 
 public class InputView {
 
     private final NameInputValidation nameInputValidation;
+    private final MoveCountInputValidation moveCountInputValidation;
 
-    public InputView(NameInputValidation nameInputValidation) {
+    public InputView(NameInputValidation nameInputValidation, MoveCountInputValidation moveCountInputValidation) {
         this.nameInputValidation = nameInputValidation;
+        this.moveCountInputValidation = moveCountInputValidation;
     }
 
     public String[] askCarNames() {
@@ -17,5 +20,9 @@ public class InputView {
         String[] carNames = input.split(",");
         Arrays.stream(carNames).forEach(nameInputValidation::isUnderFiveLetters);
         return carNames;
+    }
+
+    public int askMoveCount() {
+        return -1;
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -6,12 +6,14 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.validation.MoveCountInputValidation;
 import racingcar.validation.NameInputValidation;
 
 public class InputViewTest {
     private final InputStream systemInBackUp = System.in; // 원래 System.in 백업
     private final NameInputValidation nameInputValidation = new NameInputValidation();
-    private final InputView inputView = new InputView(nameInputValidation);
+    private final MoveCountInputValidation moveCountInputValidation = new MoveCountInputValidation();
+    private final InputView inputView = new InputView(nameInputValidation, moveCountInputValidation);
 
     @Test
     @DisplayName("자동차 이름 입력받기 테스트")
@@ -23,6 +25,20 @@ public class InputViewTest {
         // test
         assertThat(inputView.askCarNames())
                 .contains("pobi", "woni", "jun");
+        // System.in 복원
+        System.setIn(systemInBackUp);
+    }
+
+    @Test
+    @DisplayName("이동할 횟수 입력받기 테스트")
+    public void askMoveCountTest() {
+        // 원하는 입력값 InputStream 에 추가
+        String input = "10";
+        ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
+        System.setIn(testIn);
+        // test
+        assertThat(inputView.askMoveCount())
+                .isEqualTo(10);
         // System.in 복원
         System.setIn(systemInBackUp);
     }
