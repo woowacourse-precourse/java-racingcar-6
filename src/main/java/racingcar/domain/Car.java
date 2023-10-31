6d292097bd1c643a9b505578b5fd655d2e62ff7b
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 00:46:55 +0900

    refactor : forward가 생성자 안에서 유효하도록 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,29 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Car {
     private static final int CAN_GO_FORWARD_START = 4;
     private int forward;
-    public int goForward(int random){
-        if(random >= CAN_GO_FORWARD_START){
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+        forward = 0;
+    }
+
+    public void goForward(int randomNum){
+        if(randomNum >= CAN_GO_FORWARD_START){
             forward++;
         }
-        return this.forward;
     }
+
+    public int getForward() {
+        return forward;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
