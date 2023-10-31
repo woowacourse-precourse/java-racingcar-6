f3e2c02cd0141ec158da9d35cfe148d5202e2e10
yy8305 <yy8305@kakao.com>
2023-10-31 17:06:56 +0900

    feat(InputHandler): 사용자 입력 처리 모듈 기능 구현

---
+++ src/main/java/racingcar/util/InputHandler.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public static String getUserInput() {
+        return Console.readLine().trim();
+    }
+}
