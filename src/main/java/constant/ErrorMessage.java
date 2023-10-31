6d5865b685881b033fc24f065a766acb54ef9fc1
yunseung <yseungun.21@gmail.com>
2023-10-31 22:25:01 +0900

    refactor(UserInputValidator): 이름 길이 기준 초과 시 발생 에러메시지 설정

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public final static String NOT_NUMBER_INPUT_CASE_MESSAGE = "[ERROR] 숫자가 아닌 것을 입력했습니다. 게임을 종료합니다.";
     public final static String EMPTY_NAME_CASE_MESSAGE = "[ERROR] 이름이 존재하지 않습니다. 게임을 종료합니다.";
     public final static String INVALID_INPUT_CASE_MESSAGE = "[ERROR] 입력이 잘못되었습니다. 게임을 종료합니다.";
+    public final static String EXCEED_NAME_CASE_MESSAGE = "[ERROR] 입력 길이가 초과되었습니다. 게임을 종료합니다.";
 }
--- src/main/java/validation/UserInputValidator.java
+++ src/main/java/validation/UserInputValidator.java
@@ -2,11 +2,13 @@ package validation;
 
 import static constant.Constant.NAME_LENGTH_LIMIT;
 import static constant.ErrorMessage.EMPTY_NAME_CASE_MESSAGE;
+import static constant.ErrorMessage.EXCEED_NAME_CASE_MESSAGE;
 
 public class UserInputValidator {
+
     public static void validateNameLength(String name) {
         if (name.length() > NAME_LENGTH_LIMIT) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXCEED_NAME_CASE_MESSAGE);
         }
     }
 
