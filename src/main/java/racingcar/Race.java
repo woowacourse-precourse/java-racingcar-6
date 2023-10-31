57129f73582ee811a1cc7a0c393a6d40fbd48b54
elizabethgim <yujingim@naver.com>
2023-10-31 10:20:11 +0900

    refactor: 사용자 입력 받기 - 자동차 이름, 시도 횟수

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -10,6 +10,8 @@ import java.util.Map;
 public class Race {
     private Map<String, Integer> cars;
     private List<String> names;
+    private int count;
+
     private void mapCarNames(){
         cars = new HashMap<>();
         for(String name: names){
@@ -17,38 +19,24 @@ public class Race {
         }
     }
 
-    public boolean run(){
-        int dice = Randoms.pickNumberInRange(0, 9);
-
-        if(dice >= 4){
-            return true;
-        }
-
-        return false;
-    }
-
-    public void start(){
+    private void getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         names = List.of(input.split(","));
         mapCarNames();
+    }
 
+    private void getNumberOfAttempts(){
         System.out.println("시도할 회수는 몇회인가요?");
-        int count = Integer.parseInt(Console.readLine());
+        count = Integer.parseInt(Console.readLine());
+    }
 
-        for(int i =0;i<count;i++){
-            for(String name: names) {
-                if (run()) {
-                    cars.put(name, cars.get(name) + 1);
-                }
-            }
+    public void start(){
+        getCarNames();
 
-            System.out.println("실행 결과");
-            for(String name:names){
-                System.out.println(name+" : "+"-".repeat(cars.get(name)));
-            }
-            System.out.println();
-        }
+        getNumberOfAttempts();
+
+
 
    }
 }
