f84cebcda1c71e43103365d3e67848f0978d5711
mango606 <yuj08184@kangwon.ac.kr>
2023-10-31 20:46:45 +0900

    feat: add car object

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,10 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
 
     public static void main(String[] args) {
@@ -15,5 +19,36 @@ public class Application {
 
         System.out.println("시도할 회수는 몇회인가요?");
         int rounds = Integer.parseInt(Console.readLine());
+
+        List<Car> cars = initializeCars(carNames);
+    }
+
+    private List<Car> initializeCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+}
+
+class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void moveForward() {
+        position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
