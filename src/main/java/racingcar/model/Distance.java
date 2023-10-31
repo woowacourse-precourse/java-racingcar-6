402cdaf1ed95340d833d5ab141ecb80bcecac7a9
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-31 17:29:58 +0900

    feat : distance 객체 생성, get, set 추가

---
+++ src/main/java/racingcar/model/Distance.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+public class Distance {
+    private Integer distance;
+
+    public Distance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+}
