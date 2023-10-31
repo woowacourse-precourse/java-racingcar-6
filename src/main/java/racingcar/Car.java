9b6eb735acdbbbe438e0eedaf5a07ac6bc9e2669
MeoungHum <emmabark84@gmail.com>
2023-10-31 21:09:32 +0900

    자동차 이름 속성 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,15 @@
 package racingcar;
 
 public class Car {
+    private String name;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 
 
