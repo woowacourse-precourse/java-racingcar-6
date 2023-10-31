1ba74fd8be4a0bc57231f719df54b364ff34acda
kunsanglee <gosmdochee2@gmail.com>
2023-10-31 19:00:03 +0900

    refactor(CarDto): CarInfo에서 CarDto로 클래스명 변경

--- src/main/java/racingcar/dto/CarDto.java
+++ src/main/java/racingcar/dto/CarDto.java
@@ -2,11 +2,11 @@ package racingcar.dto;
 
 import racingcar.domain.Car;
 
-public record CarInfo(String name, int position) {
+public record CarDto(String name, int position) {
     public static final String POSITION_PROGRESS = "-";
 
-    public static CarInfo from(Car car) {
-        return new CarInfo(car.getCarName(), car.getPosition());
+    public static CarDto from(Car car) {
+        return new CarDto(car.getCarName(), car.getPosition());
     }
 
     public String convertPositionValue() {
