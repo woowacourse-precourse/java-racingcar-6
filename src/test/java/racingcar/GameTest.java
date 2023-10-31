a0c8fbcad8501da155706fd1ba5993bdb200148e
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-01 01:12:00 +0900

    [Test](GameTest) - TestCode 추가

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+class GameTest {
+
+    @Test
+    void 배열로_잘_쪼개지는지() {
+        String carName = "pobi,MJ,tjwls";
+        String[] racerNameArrayExpected = {"pobi", "MJ", "tjwls"}; // 원하는 결과 배열
+        String[] racerNameArraySplit = carName.split(",");
+
+        // 공백을 제거하여 원하는 결과와 비교
+        for (int i = 0; i < racerNameArraySplit.length; i++) {
+            racerNameArraySplit[i] = racerNameArraySplit[i].trim();
+        }
+
+        assertArrayEquals(racerNameArrayExpected, racerNameArraySplit);
+    }
+
+}
