bf12870ce554339407fa1e64c2238800ba46ece6
muyaaho <skanthsu715@gmail.com>
2023-10-31 16:33:54 +0900

    refactor(racingcar): 객체를 전달해주도록 controller, application 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,22 @@
 package racingcar;
 
-import racingcar.controller.Controller;
-import racingcar.view.OutputView;
-
 import java.util.List;
+import racingcar.controller.Controller;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-//        InputView inputView = new InputView();
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         Controller controller = new Controller();
 
-
         outputView.printAskName();
-        List<String> nameList = controller.getNames();
+        List<String> nameList = controller.getNames(inputView.getLine());
         outputView.printAskNumber();
-        int howMany = controller.getHowMany();
-        controller.putCarList(nameList);
+        int howMany = controller.getHowMany(inputView.getLine());
+        controller.makeCarList(nameList);
         outputView.printResult();
         controller.play(howMany);
         List<String> winners = controller.whoWinner();
--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,67 +1,65 @@
 package racingcar.controller;
 
-import racingcar.model.CarList;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import static java.lang.Integer.parseInt;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.lang.Integer.parseInt;
+import racingcar.model.CarList;
+import racingcar.view.OutputView;
 
 public class Controller {
     Validate validate = new Validate();
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
     CarList carList;
-    public void putCarList(List<String> nameList){
-        // CarList 생성자 만들기 -> 여기에 저장을 하든지 해야지
+
+    public void makeCarList(List<String> nameList) {
         this.carList = new CarList(nameList);
     }
 
-    public int getHowMany(){
-        String strNumber = inputView.getLine();
+    public int getHowMany(String strNumber) {
         int number = 0;
-        if (validate.validateNumber(strNumber)){
+        if (validate.validateNumber(strNumber)) {
             number = parseInt(strNumber);
         }
         return number;
     }
-    public List<String> getNames(){
-        String inputLine = inputView.getLine();
+
+    public List<String> getNames(String inputLine) {
         List<String> inputNames = seperateNames(inputLine);
         validate.validateOverNameorZero(inputNames);
         validate.validateDuplicate(inputNames);
         return inputNames;
     }
 
-    public List<String> seperateNames(String line){
+    public List<String> seperateNames(String line) {
         List<String> nameList = new ArrayList<>();
-        for (String name: line.split(",")){
+        for (String name : line.split(",")) {
             nameList.add(name);
         }
         return nameList;
     }
 
+    public List<Integer> makeRandomList() {
+        List<Integer> randomNums = new ArrayList<>();
+        for (int i = 0; i < carList.getSize(); i++) {
+            randomNums.add(new Generator().generateRandomNumber());
+        }
+        return randomNums;
+    }
 
     /* 입력한 수 만큼 반복 진행
-    * 자동차 수 만큼 랜덤한 숫자 만들어서
-    * 이걸 carList에 넘겨줌*/
-    public void play(int howMany){
+     * 자동차 수 만큼 랜덤한 숫자 만들어서
+     * 이걸 carList에 넘겨줌*/
+    public void play(int howMany) {
         OutputView outputView = new OutputView();
-        while (howMany > 0){
-            List<Integer> randomNums = new ArrayList<>();
-            for (int i = 0; i< carList.getSize(); i++){
-                randomNums.add(new Generator().generateRandomNumber());
-            }
-            List<Integer> zeroOne = validate.replaceZeroAndOne(randomNums);
-            carList.upgradeDistance(zeroOne);
+        while (howMany > 0) {
+            List<Integer> randomNums = makeRandomList();
+            carList.upgradeDistance(validate.replaceZeroAndOne(randomNums));
             outputView.printInterm(carList.toString());
             howMany--;
         }
     }
 
-    public List<String> whoWinner(){
+    public List<String> whoWinner() {
         return carList.getWinner();
     }
 
