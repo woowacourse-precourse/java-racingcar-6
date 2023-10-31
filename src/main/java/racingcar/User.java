aa7fe2710dca2d08a73e044a7e146d27430276ef
minwoo <mark42son@gmail.com>
2023-11-01 02:23:37 +0900

    feat : 자동차 이름 길이 조건 상수처리

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class User {
 
     private static final String OPERATOR = ",";
-    private static final int CAR_NAME_LENGTH = 5;
+    private static final int CAR_NAME_LENGTH_MAX = 5;
+    private static final int CAR_NAME_LENGTH_MIN = 1;
     private static final int MINUMUM_RANGE_OF_NUMBER = 0;
     private static final int MAXIMUM_RANGE_OF_NUMBER = 9;
     private static final int DRIVE_FORWARD_NUMBER = 4;
@@ -14,7 +15,7 @@ public class User {
     private static final String EXCEPTION_SEPARATOR_MESSAGE = "자동차의 이름을 쉼표(,)로 구분해서 입력해주세요.";
     private static final String EXCEPTION_DUPLICATION_MESSAGE = "[ERROR] 자동차들 중에 같은 이름의 자동차가 있습니다.";
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static int trialNumber;
 
@@ -27,13 +28,13 @@ public class User {
     private static String inputCarName() { return Console.readLine(); }
 
     public void setTrialNumber() {
-        System.out.println(INPUT_TRIAL_MESSAGE);
+        System.out.println(TRIAL_MESSAGE);
         String input = inputTrialNumber();
         this.trialNumber = Integer.parseInt(input);
 
     }
 
-    private void vaildateStringLength(String input) {
+    private void vaildateCarNameLength(String input) {
         if (input.length() > CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(EXCEPTION_NAME_LENGTH_MESSAGE);
         }
