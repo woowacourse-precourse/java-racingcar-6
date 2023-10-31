c4bea4d177bb136faa40651cd2f3d1761a0ba80f
Park Eunhyeong <silbroeh@gmail.com>
2023-10-31 23:25:19 +0900

    feat : InputHandler클래스 - validateCarNames() 메소드 구현

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -2,7 +2,13 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class InputHandler {
+    private static final String COMMA = ",";
+    private static final String BLANK = " ";
+    private static int MAX_LENGTH = 5;
     public String userInput() {
         return Console.readLine();
     }
@@ -17,5 +23,24 @@ public class InputHandler {
         return Integer.parseInt(input);
     }
 
-
+    public void validateCarNames(String target) {
+        if (target.isEmpty()) { // 입력값 부재
+            throw new IllegalArgumentException();
+        }
+        else if (target.contains(BLANK)) {  // 공백 포함
+            throw new IllegalArgumentException();
+        }
+        else if (target.contains(COMMA.repeat(2))) {   // 쉼표(,)가 2개이상 연속
+            throw new IllegalArgumentException();
+        }
+        else if (isBothEndPattern(target)) {    // 쉼표(,)가 맨 앞/뒤에 위치
+            throw new IllegalArgumentException();
+        }
+        else if (hasLengthExcess(target)) {   // 길이가 5 초과
+            throw new IllegalArgumentException();
+        }
+        else if (hasDuplicates(target)) {   // 중복 입력
+            throw new IllegalArgumentException();
+        }
+    }
 }
