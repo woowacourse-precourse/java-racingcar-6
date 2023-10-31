2d26113b7e6718621da771828cbd94b9020a466d
hyocheol An <anhyocheol@naver.com>
2023-11-01 08:18:33 +0900

    style : 생성자 매개변수 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,24 @@
 package racingcar.domain;
 
 public class Car {
+    private String name;
+    private Integer moveDistance;
+
+    public Car(){
+    }
+    public Car(String name){
+        this.name = name;
+        this.moveDistance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Integer getMoveDistance() {
+        return moveDistance;
+    }
+    public void moveForward() {
+        moveDistance ++;
+    }
+
 }
