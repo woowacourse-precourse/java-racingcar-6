a4e930be00ddc6659b75560f0a5f1f006bfb7725
jaegyeongkim <dongji_11@naver.com>
2023-10-31 18:37:57 +0900

    refactor: 이름 입력 검증하는 클래스 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,24 +1,17 @@
 package model;
 
 public class Car {
-    final static int MAX_INPUT_LENGTH = 5;
-    final static String INPUT_IS_EMPTY = "입력은 공백일 수 없습니다.";
-    final static String INPUT_HAS_SPACE = "입력은 공백이 포함될 수 없습니다.";
-    final static String INPUT_OVER_LENGTH = "입력은 5자 이하여야 합니다.";
     final static int FORWARDING_THRESHOLD = 4;
     final static String ONE_PLACE = "-";
-    String name;
+    Name name;
     String nowPlace = "";
 
     public Car(String name) {
-        isOverLength(name);
-        isEmpty(name);
-        hasSpace(name);
-        this.name = name;
+        this.name= new Name(name);
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 
     public void moveForward() {
@@ -34,24 +27,4 @@ public class Car {
             moveForward();
         }
     }
-
-    public void hasSpace(String input) {
-        if (input.contains(" ")) {
-            throw new IllegalArgumentException(INPUT_HAS_SPACE);
-        }
-    }
-
-    public void isEmpty(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException(INPUT_IS_EMPTY);
-        }
-    }
-
-    public void isOverLength(String input) {
-        if (input.length() > MAX_INPUT_LENGTH) {
-            throw new IllegalArgumentException(INPUT_OVER_LENGTH);
-        }
-    }
-
-
 }
