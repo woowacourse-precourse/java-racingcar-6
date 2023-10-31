00ff5ba9715cc3d4bf8db5c267af0bb075f45c11
edgar6bf <edgar6bf@gmail.com>
2023-10-31 15:17:36 +0900

    refactor: InputView와 OutputView를 외부에서 주입받도록 GameController 로직 수정
    InputView와 OutputView가 외부 주입을 하도록 로직을 수정하였기에 이에 맞춰 GameController 로직을 수정함.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -9,9 +9,13 @@ import java.util.List;
 
 public class GameController {
 
+    private final InputView inputView;
+    private final OutputView outputView;
     private final GameService gameService;
 
-    public GameController(GameService gameService) {
+    public GameController(InputView inputView, OutputView outputView, GameService gameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
         this.gameService = gameService;
     }
 
@@ -22,18 +26,18 @@ public class GameController {
     }
 
     private void initGame() {
-        List<String> carNames = InputView.inputStringsByDelimiter();
+        List<String> carNames = inputView.inputCarNames();
         gameService.initCars(carNames);
     }
 
     private void executeRacing() {
-        int raceCount = InputView.inputNumber();
+        int raceCount = inputView.inputRaceCount();
         List<RaceResult> raceResults = gameService.race(raceCount);
-        OutputView.printRaceResult(raceResults);
+        outputView.printRaceResult(raceResults);
     }
 
     private void announceWinner() {
         List<String> winners = gameService.getWinners();
-        OutputView.printWinners(winners);
+        outputView.printWinners(winners);
     }
 }
