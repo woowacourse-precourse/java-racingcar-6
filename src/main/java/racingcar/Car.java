a7bf3cdfeb277afc4be88d58f00941b35bbd9a08
choiyewon <choiyewon1997@gmail.com>
2023-10-31 10:27:02 +0900

    feat: make Car class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    String name;
+    Integer go;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGo() {
+        return go;
+    }
+    public void setGo(Integer go) {
+        this.go = go;
+    }
+
+
+}
