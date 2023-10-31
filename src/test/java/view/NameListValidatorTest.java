c1bd2ffb8019c256db3859c0a5847bbcbb51281a
nayonsoso <panda0329@naver.com>
2023-11-01 03:11:18 +0900

    test(NameListValidatorTest): 함수명을 직관적이게 수정

--- src/test/java/view/NameListValidatorTest.java
+++ src/test/java/view/NameListValidatorTest.java
@@ -12,7 +12,19 @@ class NameListValidatorTest {
     static final NameListValidator nameListValidator = new NameListValidator();
 
     @Test
-    public void 빈_값을_입력하면_예외발생(){
+    public void validate_제대로된_형식이면_예외없음(){
+        String input = "aa,bb,ss";
+        boolean isExceptionExist = false;
+        try{
+            nameListValidator.validate(input);
+        } catch (Exception e){
+            isExceptionExist = true;
+        }
+        assertThat(isExceptionExist).isFalse();
+    }
+
+    @Test
+    public void validate_빈_값을_입력하면_예외발생(){
         String input = "";
         assertThatThrownBy(() -> nameListValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -20,7 +32,7 @@ class NameListValidatorTest {
     }
 
     @Test
-    public void 쉼표로_끝나면_예외발생(){
+    public void validate_쉼표로_끝나면_예외발생(){
         String input = "aa,bb,";
         assertThatThrownBy(() -> nameListValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -28,7 +40,7 @@ class NameListValidatorTest {
     }
 
     @Test
-    public void 쉼표가_연달아_있으면_예외발생(){
+    public void validate_쉼표가_연달아_있으면_예외발생(){
         String input = "aa,,bb";
         assertThatThrownBy(() -> nameListValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -36,7 +48,7 @@ class NameListValidatorTest {
     }
 
     @Test
-    public void 이름이_최대_길이를_넘어가면_예외발생(){
+    public void validate_이름이_최대_길이를_넘어가면_예외발생(){
         String input = "aaaaa,bbbbbb";
         assertThatThrownBy(() -> nameListValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -44,10 +56,40 @@ class NameListValidatorTest {
     }
 
     @Test
-    public void 중복되는_이름을_입력하면_예외발생(){
+    public void validate_중복되는_이름을_입력하면_예외발생(){
         String input = "aa,bb,aa";
         assertThatThrownBy(() -> nameListValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.DUPLICATED_NAME);
+    }
+
+    @Test
+    public void splitAndTrim_하나의_이름만_입력할_때_하나만_담긴_리스트_반환(){
+        String input = "aa";
+        nameListValidator.validate(input);
+        List<String> carName = nameListValidator.splitAndTrim(input, ",");
+        assertThat(carName.size()).isEqualTo(1);
+        assertThat(carName).contains(input);
+    }
+
+    @Test
+    public void splitAndTrim_쉼표_사이에_띄어쓰기가_포함되었을_때_양옆_띄어쓰기_제외하고_이름_추출(){
+        String input = "aa, bbbb, ccccc ";
+        nameListValidator.validate(input);
+        List<String> carName = nameListValidator.splitAndTrim(input, ",");
+        assertThat(carName.size()).isEqualTo(3);
+        assertThat(carName).contains("aa");
+        assertThat(carName).contains("bbbb");
+        assertThat(carName).contains("ccccc");
+    }
+
+    @Test
+    public void splitAndTrim_이름_안에_띄어쓰기가_포함되었을_때_띄어쓰기_포함하고_이름_추출(){
+        String input = "a  a, b b";
+        nameListValidator.validate(input);
+        List<String> carName = nameListValidator.splitAndTrim(input, ",");
+        assertThat(carName.size()).isEqualTo(2);
+        assertThat(carName).contains("a  a");
+        assertThat(carName).contains("b b");
     }
 }
