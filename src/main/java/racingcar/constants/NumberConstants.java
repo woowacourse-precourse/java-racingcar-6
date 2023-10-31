426526a8c27b4e605c03f01437f5a21e962f8bf0
dhl1924 <dhl1924@naver.com>
2023-11-01 00:33:52 +0900

    feat: Implemented NumberConstants class

---
+++ src/main/java/racingcar/constants/NumberConstants.java
@@ -0,0 +1,7 @@
+package racingcar.constants;
+
+public class NumberConstants {
+    public static final int MIN_RANGE = 0;
+    public static final int MAX_RANGE = 9;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+}
