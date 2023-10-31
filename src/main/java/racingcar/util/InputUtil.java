e785de22d83ec4a14841a7f08f7ebb734be6b743
jae woo Jung <awfjol2008@naver.com>
2023-10-31 09:52:22 +0900

    feat: InputUtil 클래스의 접근 제한을 위해 private 생성자 추가

--- src/main/java/racingcar/util/InputUtil.java
+++ src/main/java/racingcar/util/InputUtil.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class InputUtil {
 
+    private InputUtil() {
+
+    }
+
     public static List<String> convertInputStringToList(String input) {
         return Arrays.stream(input.split(","))
                 .toList();
