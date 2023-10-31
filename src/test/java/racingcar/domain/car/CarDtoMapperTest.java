655ad00f89528b760035a278ef1a31e95913d325
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-31 17:13:37 +0900

    test : CarMapping을 테스트 합니다.

--- src/test/java/racingcar/domain/car/CarDtoMapperTest.java
+++ src/test/java/racingcar/domain/car/CarDtoMapperTest.java
@@ -2,25 +2,27 @@ package racingcar.domain.car;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarDtoMapperTest {
 
     @Test
+    @DisplayName("CarDto를 List<Car>에 매핑합니다.")
     void mapToCar() {
         // GIVEN
         CarDto carDto = new CarDto("pobi,jiwoo,jiwon");
+        List<Car> expected = new ArrayList<>();
+        expected.add(Car.createCar("pobi", 0));
+        expected.add(Car.createCar("jiwoo", 0));
+        expected.add(Car.createCar("jiwon", 0));
 
         // WHEN
-        List<Car> cars = CarDtoMapper.mapToCar(carDto);
+        List<Car> actual = CarDtoMapper.mapToCar(carDto);
 
         // THEN
-        assertEquals(cars.get(1).getName(), "jiwoo");
-        assertEquals(cars.get(2).getName(), "jiwon");
-
-        assertEquals(cars.get(0).getCurrentLocation(), 0);
-        assertEquals(cars.get(1).getCurrentLocation(), 0);
-        assertEquals(cars.get(2).getCurrentLocation(), 0);
+        assertEquals(actual, expected);
     }
 }
