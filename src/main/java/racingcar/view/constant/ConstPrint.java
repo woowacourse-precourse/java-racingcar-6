1d561a42a4f7de6e17711125157d1b31eeeb482f
kangyuseok <kingyuseok@naver.com>
2023-11-01 00:11:01 +0900

    Feat: 최송 우승자를 출력하는 enum 추가

--- src/main/java/racingcar/view/constant/ConstPrint.java
+++ src/main/java/racingcar/view/constant/ConstPrint.java
@@ -2,8 +2,9 @@ package racingcar.view.constant;
 
 public enum ConstPrint {
     GAME_START("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    ASK_ATTEMPT("시도할 회수는 몇회인가요?\n"),
-    PRINT_RESULT("실행 결과\n");
+    ASK_ATTEMPT("시도할 회수는 몇회인가요?"),
+    PRINT_RESULT("\n실행 결과"),
+    PRINT_WINNER("최종 우승자");
 
     private final String constPrint;
 
