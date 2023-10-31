11c9e30fab35f36f7a49459f015359ae49312616
jooys130 <wndustn130@gmail.com>
2023-11-01 04:08:13 +0900

    refactor: InputView 함수 및 변수 명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String CAR_NAMES_QUESTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String NUMBER_ATTEMPTED_QUESTION= "시도할 회수는 몇회인가요?";
+    private static final String ROUNDS_NUMBER_QUESTION= "시도할 회수는 몇회인가요?";
 
     public InputView() {
     }
@@ -15,8 +15,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static String getNumberAttempted(){
-        System.out.println(NUMBER_ATTEMPTED_QUESTION);
+    public static String getRounds(){
+        System.out.println(ROUNDS_NUMBER_QUESTION);
         return Console.readLine();
     }
 
