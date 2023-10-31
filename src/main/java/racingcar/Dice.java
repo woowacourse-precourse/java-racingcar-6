056323dccab205136dc6bb8730d1014bee2d7f87
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-01 01:06:24 +0900

    [Feat](Dice) - Random 기능 구현

--- src/main/java/racingcar/Dice.java
+++ src/main/java/racingcar/Dice.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Dice {
+    private static final int ZERO = 0;
+    private static final int NINE = 9;
+    private int dice;
+
+    public Dice() {
+        this.dice = Randoms.pickNumberInRange(ZERO, NINE);
+    }
+
+    public int getDice() {
+        return dice;
+    }
 }
