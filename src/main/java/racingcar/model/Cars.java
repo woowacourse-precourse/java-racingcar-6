d77eed28bddd2dbbc14c7a18fa0ec7660ebc90f9
gourderased <gourderased@gmail.com>
2023-11-01 01:10:52 +0900

    "feat(racingcar): Cars클래스에 getCars 메소드 추가"

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,16 +1,21 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.List;
 import racingcar.utils.Validator;
 
 public class Cars {
 
-    private final ArrayList<Car> cars = new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
     public Cars(String[] carNames) {
         for(String carName : carNames) {
             Validator.validateName(carName);
             Car car = new Car(carName);
             cars.add(car);
         }
+    }
+
+    public List<Car> getCars() {
+        return cars;
     }
 }
