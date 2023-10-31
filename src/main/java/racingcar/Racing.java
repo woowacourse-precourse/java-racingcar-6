9baeffad9b6af2613a902e92e99106e39801f3eb
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-31 16:14:51 +0900

    feat: change input forms, printing status

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -4,19 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Racing {
     private List<Car> cars = new ArrayList<>();
     private int trials;
 
     public void getInfo() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         NameInput.inputNames(cars);
+        System.out.println("시도할 회수는 몇회인가요?");
         trials = TrialInput.inputTrial();
+        System.out.println();
     }
 
     public void start() {
-        IntStream.range(0, trials).forEach(i -> cars.forEach(Car::trial));
+        System.out.println("실행 결과");
+        IntStream.range(0, trials)
+                .peek(i -> cars.forEach(Car::trial))
+                .peek(i -> printMoves())
+                .forEach(i -> System.out.println());
     }
 
     public void printMoves() {
@@ -26,7 +31,7 @@ public class Racing {
     public void printWinner() {
         int maxPosition = getMaxPosition();
         List<String> winningCarNames = getWinningCarNames(maxPosition);
-        System.out.println("최종 우승자 : " + String.join(", ", winningCarNames));
+        System.out.println("\n최종 우승자 : " + String.join(", ", winningCarNames));
     }
 
     private List<String> getWinningCarNames(int maxPosition) {
