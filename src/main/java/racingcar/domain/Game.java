861644403d8496a34800aa214f498e8419c91953
Jin409 <sualng123@gmail.com>
2023-10-31 15:48:45 +0900

    [REFACTOR] OutputView 에서 출력 시 인자로 문자열을 받아 출력하도록 리팩토링

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
+import static racingcar.view.Messages.REGISTER_CAR_MESSAGE;
+
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.utils.Parser;
 import racingcar.utils.RandomNumberPicker;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -10,33 +13,30 @@ public class Game {
 
     private final List<Car> cars;
 
-    private final OutputView outputView;
-
-    private final InputView inputView;
-
     private int trialNumber;
 
     private static final int MOVE_FORWARD_CONDITION_OF_CAR = 4;
     private static final int LOWER_BOUND_OF_RANDOM_NUMBER = 1;
     private static final int UPPER_BOUND_OF_RANODM_NUMBER = 9;
 
-    public Game(OutputView outputView, InputView inputView) {
+    public Game() {
         cars = new ArrayList<>();
-        this.outputView = outputView;
-        this.inputView = inputView;
     }
 
     public void run() {
-        outputView.printStartMessage();
         registerCars();
-        this.trialNumber = inputView.readTrialNumber();
+
+        this.trialNumber = Parser.stringToInteger(InputView.readValue());
         for (int i = 0; i < trialNumber; i++) {
             moveCars();
         }
     }
 
     private void registerCars() {
-        List<String> namesOfCars = inputView.readNamesOfCars();
+        OutputView.println(REGISTER_CAR_MESSAGE);
+
+        List<String> namesOfCars = Parser.stringToStringList(InputView.readValue(), ",");
+
         for (String name : namesOfCars) {
             cars.add(Car.getInstance(name));
         }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,10 +2,12 @@ package racingcar.view;
 
 public class OutputView {
 
-    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static void print(final String message) {
+        System.out.print(message);
+    }
 
-    public void printStartMessage() {
-        System.out.println(START_MESSAGE);
+    public static void println(final String message) {
+        System.out.println(message);
     }
 
 }
