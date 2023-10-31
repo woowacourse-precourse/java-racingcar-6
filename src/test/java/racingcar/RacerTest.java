dbcdb370e7aba6baad13c5d29bc15459c17e0891
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-31 12:10:23 +0900

    feat : RandomsMocking Class 추가
    Randoms 함수를 Mocking 하는 Class
    pickNumberInRange 의 함수 결과가 원하는 값이 나오게 수정
    기존 , pickNumberInRange 사용하는 테스트 수정

--- src/test/java/racingcar/RacerTest.java
+++ src/test/java/racingcar/RacerTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import racingcar.mocking.RandomsMocking;
+
+
 public class RacerTest {
     Racer racer;
     @BeforeEach
@@ -16,15 +19,16 @@ public class RacerTest {
         Assertions.assertEquals(Car.class,raceCar.getClass());
     }
     @Test
-    void 자동차_전진여부에_따라_현재_위치에서_일정_칸_전진할_수_있다(){
-        int pastPos = racer.getCurrentPos();
-        while(true){
-            racer.runRaceEachStep();
-            if (racer.getCurrentPos()!=pastPos){
-                break;
-            }
-        }
-        int currentPos = racer.getCurrentPos();
-        Assertions.assertEquals(pastPos+Config.FORWARD_DISTANCE,currentPos);
+    void 자동차_전진여부에_따라_현재_위치에서_일정_칸_전진할_수_있다() {
+
+        RandomsMocking.fixPickNumberInRangeOverThreshold(
+            () -> {
+                int pastPos = racer.getCurrentPos();
+                racer.runRaceEachStep();
+                int currentPos = racer.getCurrentPos();
+
+                Assertions.assertEquals
+                    (pastPos + Config.FORWARD_DISTANCE, currentPos);
+            });
     }
 }
---
+++ src/test/java/racingcar/mocking/RandomsMocking.java
@@ -0,0 +1,40 @@
+package racingcar.mocking;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.function.Executable;
+
+import org.mockito.Mockito;
+import org.mockito.MockedStatic;
+import org.mockito.ArgumentMatchers;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Config;
+
+
+public class RandomsMocking {
+    private static MockedStatic.Verification verificationPickNumberInRange =
+            () -> Randoms.pickNumberInRange(ArgumentMatchers.anyInt(),ArgumentMatchers.anyInt());
+
+    public static void fixPickNumberInRangeUnderThreshold(Executable executable){
+
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(10L), () -> {
+            try (final MockedStatic<Randoms> mock = Mockito.mockStatic(Randoms.class)) {
+                mock.when(verificationPickNumberInRange)
+                        .thenReturn(Config.FORWARD_THRESHOLD -1);
+                executable.execute();
+            }
+        });
+    }
+    public static void fixPickNumberInRangeOverThreshold(Executable executable){
+
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(10L), () -> {
+            try (final MockedStatic<Randoms> mock = Mockito.mockStatic(Randoms.class)) {
+                mock.when(verificationPickNumberInRange)
+                        .thenReturn(Config.FORWARD_THRESHOLD + 1);
+                executable.execute();
+            }
+        });
+    }
+}
