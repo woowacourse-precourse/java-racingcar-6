13a220f0b9a5ee6809dcd54ebc80d6469bea6e1f
jae woo Jung <awfjol2008@naver.com>
2023-10-31 21:50:01 +0900

    refector: CarNameValidator 클래스의 개별 이름 검증 메소드 Name 클래스로 이동

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,13 +1,32 @@
 package racingcar.domain.car;
 
-import racingcar.validator.CarNameValidator;
+import static racingcar.config.GameConfig.NAME_LENGTH_CONDITION;
+import static racingcar.exception.ErrorMessage.VALIDATE_EMPTY_FOR_EACH_NAME;
+import static racingcar.exception.ErrorMessage.VALIDATE_LENGTH_FOR_EACH_NAME;
 
 public class Name {
     protected final String name;
 
     protected Name(String name) {
-        CarNameValidator.validateForEachName(name);
+        validate(name);
         this.name = name;
+    }
+
+    public static void validate(String name) {
+        validateEmpty(name);
+        validateLength(name);
+    }
+
+    private static void validateEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(VALIDATE_EMPTY_FOR_EACH_NAME.getMessage());
+        }
+    }
+
+    private static void validateLength(String name) {
+        if (name.length() > NAME_LENGTH_CONDITION.getNumber()) {
+            throw new IllegalArgumentException(String.format(VALIDATE_LENGTH_FOR_EACH_NAME.getMessage(), NAME_LENGTH_CONDITION.getNumber()));
+        }
     }
 
     public String toString() {
--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -37,21 +37,4 @@ public class CarNameValidator {
             throw new IllegalArgumentException(VALIDATE_DUPLICATE_FOR_LIST.getMessage());
         }
     }
-
-    public static void validateForEachName(String carName) {
-        validateEmptyForEachName(carName);
-        validateLengthForEachName(carName);
-    }
-
-    private static void validateEmptyForEachName(String carName) {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException(VALIDATE_EMPTY_FOR_EACH_NAME.getMessage());
-        }
-    }
-
-    private static void validateLengthForEachName(String carName) {
-        if (carName.length() > NAME_LENGTH_CONDITION.getNumber()) {
-            throw new IllegalArgumentException(String.format(VALIDATE_LENGTH_FOR_EACH_NAME.getMessage(), NAME_LENGTH_CONDITION.getNumber()));
-        }
-    }
 }
