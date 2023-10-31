0ad24d88f94eb771ae5493a2f3ec8bcc6ebca5c7
hil679 <hil679922@gmail.com>
2023-11-01 00:49:43 +0900

    Refactor(RacingTest): BeforeEach로 공통 사전 작업 수행하기 위해 변경

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,10 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingTest {
     private static final int MOVING_FORWARD = 4;
     private static final Integer STOP = 3;
+    private Racing racing;
+    private List<Car> carList;
+
+    @BeforeEach
+    void setUp() {
+        racing = new Racing();
+
+        Car car1 = new Car("robin");
+        Car car2 = new Car("hood");
+        Car car3 = new Car("kyle");
+
+        carList = new ArrayList<>(List.of(car1, car2, car3));
+    }
+
 
     @Test
     void 자동차_객체_리스트_추가_테스트() {
-        Racing racing = new Racing();
         List<String> carNameList = new ArrayList<>(List.of("robin", "hood", "kyle"));
         List<Car> carList = racing.toCarList(carNameList);
 
@@ -25,21 +39,15 @@ public class RacingTest {
 
     @Test
     void 자동차_객체_전진_테스트() {
-        Racing racing = new Racing();
-        Car car1 = new Car("robin");
-        Car car2 = new Car("hood");
-        Car car3 = new Car("kyle");
-
-        List<Car> carList = new ArrayList<>(List.of(car1, car2, car3));
         assertRandomNumberInRangeTest(
                 () -> {
                     for (int i = 0; i < 3; i++) {
                         racing.initCarsRandomNum(carList);
                         racing.modifyForwardNum(carList);
                     }
-                    assertThat(car1.getForwardNum()).isEqualTo(1);
-                    assertThat(car2.getForwardNum()).isEqualTo(3);
-                    assertThat(car3.getForwardNum()).isEqualTo(1);
+                    assertThat(carList.get(0).getForwardNum()).isEqualTo(1);
+                    assertThat(carList.get(1).getForwardNum()).isEqualTo(3);
+                    assertThat(carList.get(2).getForwardNum()).isEqualTo(1);
                 },
                 STOP, MOVING_FORWARD, STOP,
                 MOVING_FORWARD, MOVING_FORWARD, STOP,
@@ -49,13 +57,6 @@ public class RacingTest {
 
     @Test
     void 우승자_찾기_복수_테스트() {
-        Racing racing = new Racing();
-        Car car1 = new Car("robin");
-        Car car2 = new Car("hood");
-        Car car3 = new Car("kyle");
-
-        List<Car> carList = new ArrayList<>(List.of(car1, car2, car3));
-
 
         assertRandomNumberInRangeTest(
                 () -> {
@@ -74,13 +75,6 @@ public class RacingTest {
 
     @Test
     void 단독_우승자_찾기_테스트() {
-        Racing racing = new Racing();
-        Car car1 = new Car("robin");
-        Car car2 = new Car("hood");
-        Car car3 = new Car("kyle");
-
-        List<Car> carList = new ArrayList<>(List.of(car1, car2, car3));
-
 
         assertRandomNumberInRangeTest(
                 () -> {
