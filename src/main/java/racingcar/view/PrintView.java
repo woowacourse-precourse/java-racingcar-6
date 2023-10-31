31876b15230678413293e4897b3977665c7e5b66
besky <onlybesky@gmail.com>
2023-10-31 15:54:09 +0900

    fix: 메세지 대신 변수 이름이 출력되는 문제 수정

--- src/main/java/racingcar/view/PrintView.java
+++ src/main/java/racingcar/view/PrintView.java
@@ -6,15 +6,15 @@ import racingcar.util.ReturnUtil;
 
 public class PrintView {
     public static void inputCarNamesMessage() {
-        System.out.println(Message.INPUT_CAR_NAME);
+        System.out.println(Message.INPUT_CAR_NAME.getMessage());
     }
 
     public static void inputStageTimesMessage() {
-        System.out.println(Message.INPUT_STAGE_TIME);
+        System.out.println(Message.INPUT_STAGE_TIME.getMessage());
     }
 
     public static void stageResultMessage(ArrayList<String> list) {
-        System.out.println(Message.PRINT_STAGE_RESULT);
+        System.out.println(Message.PRINT_STAGE_RESULT.getMessage());
         for (String result : list) {
             System.out.println(result);
         }
@@ -22,7 +22,7 @@ public class PrintView {
 
     public static void winnerResultMessage(ArrayList<String> winners) {
         StringBuilder sb = new StringBuilder();
-        sb.append(Message.PRINT_WINNER);
+        sb.append(Message.PRINT_WINNER.getMessage());
         sb.append(ReturnUtil.winnerResultFormat(winners));
         System.out.println(sb);
     }
