9a9b8e7a9085768d04fbe08a71cc8aabe6f7e9d6
Nara Park <skfk6352@naver.com>
2023-11-01 02:25:27 +0900

    <test>: add test methods for checking position
    랜덤값에 따라 자동차가 전진하거나 정지하는 테스트 메서드 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     private Car car;
+    private Car otherCar;
 
     @BeforeEach
     void init() {
         car = Car.from("nara");
+        otherCar = Car.from("otherCar");
     }
 
     @Test
@@ -21,15 +23,23 @@ class CarTest {
     }
 
     @Test
-    void 자동차객체의_position에_따른_전진값_출력_확인() {
-        car.play();
-        car.play();
-        car.printGameProceed();
+    void 랜덤값이_4이상이면_자동차_전진() {
+        car.movePosition(4);
+        int compareNumber = car.compareTo(otherCar);
+
+        assertThat(compareNumber).isEqualTo(1);
+    }
+
+    @Test
+    void 랜덤값이_3이하면_자동차_정지() {
+        car.movePosition(3);
+        int compareNumber = car.compareTo(otherCar);
+
+        assertThat(compareNumber).isEqualTo(0);
     }
 
     @Test
     void 자동차의_위치가_같으면_true_반환_확인() {
-        Car otherCar = Car.from("other");
         boolean isSame = car.isSamePosition(otherCar);
 
         assertThat(isSame).isTrue();
@@ -37,12 +47,9 @@ class CarTest {
 
     @Test
     void 자동차의_위치가_다르면_false_반환_확인() {
-        Car otherCar = Car.from("other");
-        otherCar.play();
-        otherCar.play();
-
+        car.movePosition(7);
         boolean isSame = car.isSamePosition(otherCar);
 
-        assertThat(isSame).isTrue();
+        assertThat(isSame).isFalse();
     }
 }
