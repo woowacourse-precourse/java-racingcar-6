a4bec663a15f46f0bf61c5309c313de77225940f
slowcloud <sju9417@gmail.com>
2023-10-31 13:34:43 +0900

    test(InputViewTest): getAttempt에 대한 테스트 코드 추가

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -3,16 +3,18 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.BDDMockito;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import racingcar.domain.Attempt;
 import racingcar.domain.Car;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
 
@@ -39,6 +41,30 @@ class InputViewTest {
         List<Car> expect = List.of(new Car("foo"), new Car("bar"));
 
         assertIterableEquals(cars, expect);
+
+    }
+
+    @Test
+    void getAttempt_시도_횟수_0_입력_확인() {
+
+        BDDMockito.given(Console.readLine()).willReturn("0");
+        InputView inputView = new InputView();
+
+        Attempt attempt = inputView.getAttempt();
+
+        assertFalse(attempt.isExist());
+
+    }
+
+    @Test
+    void getAttempt_시도_횟수_1_입력_확인() {
+
+        BDDMockito.given(Console.readLine()).willReturn("1");
+        InputView inputView = new InputView();
+
+        Attempt attempt = inputView.getAttempt();
+
+        assertTrue(attempt.isExist());
 
     }
 
