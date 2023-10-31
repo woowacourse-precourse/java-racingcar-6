990e87fd8e66d7ba77a5a32192db7eda591fadfd
yang <hjyang@epience.com>
2023-10-31 14:37:07 +0900

    🎯feat(domain/Winner) : 가장 높은 위치값을 가진 자동차 이름 반환 기능 구현

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -12,6 +12,14 @@ public class Winner {
         this.cars = cars;
     }
 
+
+    public String maxLocationCarName() {
+        return this.cars.cars.stream() // Cars.cars --- > this.cars.cars  (인스턴스 필드이기 때문)
+                .filter(car -> car.location == maxLocation())
+                .map(car -> car.carName)
+                .collect(Collectors.joining(","));
+    }
+
     public int maxLocation() {
         return this.cars.cars.stream()
                 .mapToInt(car -> car.location)
