fb672184360ec00c7d9e042b153dfd65cdb13941
yhcho0405 <yhcho0405@kakao.com>
2023-11-01 01:58:43 +0900

    test: 동일한 이름의 자동차가 있을 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -63,6 +63,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 동일한_이름의_자동차가_있을_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
