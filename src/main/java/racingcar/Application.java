9e34987c320fef7245099449a1613cdd364d515a
Yoongsyu <dkdk33555@gmail.com>
2023-11-01 00:58:54 +0900

    feat: Application 클래스에 입력, 저장 기능 구현
    사용자에게 입력받는 기능을 구현하고 그 값에 따라 예외 처리, 입력값 저장을 하는 기능을 구현하였다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarsName = Console.readLine();
+
+        Validate.validateInputCars(inputCarsName);
+
+        Map<String, Integer> carsList = GameUtil.Savecars(inputCarsName);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int inputTryNumber = Integer.parseInt(Console.readLine());
+
+        Validate.validateInputNumber(inputTryNumber);
+
+
     }
 }
