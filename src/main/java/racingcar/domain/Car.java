96cb607869256c15f0f2258d6dde4818803d0fb4
aeyongdodam <kikititi0510@gmail.com>
2023-11-01 01:01:04 +0900

    docs(Car): Car 도메인 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,46 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int START_POSITION = 0;
+    private static final int NAME_MAX_LENGTH = 5;
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+
+        this.name = name;
+        this.position = START_POSITION;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void isValid(String name) {
+        checkNull(name);
+        checkBlank(name);
+        checkNameLength(name);
+    }
+
+    private void checkNull(String name) {
+        if (name == null) {
+            throw new NullPointerException("자동차 이름 null 불가");
+        }
+    }
+
+    private void checkBlank(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름 공백 불가");
+        }
+    }
+
+    private void checkNameLength(String name) {
+        if (name.trim().length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름 글자 범위 초과");
+        }
+    }
+}
