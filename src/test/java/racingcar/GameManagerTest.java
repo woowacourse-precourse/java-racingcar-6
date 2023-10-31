5395857afe2d31e64091fc36e28e7a1c66ce456d
KimSehoon <shggm2000@gmail.com>
2023-11-01 02:09:15 +0900

    test: checkWinner_test method 구현
    - 가장 많이 전진한 자동차를 판별하는 checkWinner_test method 구현

--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -25,8 +26,24 @@ public class GameManagerTest {
         when(input.getCarNameList()).thenReturn(Arrays.asList("pobi", "jun", "woni"));
         List<Car> carListForTest = gameManager.carSetting();
 
-        assertTrue(carListForTest.contains(new Car("pobi")));
-        assertTrue(carListForTest.contains(new Car("jun")));
-        assertTrue(carListForTest.contains(new Car("woni")));
+        assertTrue(carListForTest.contains(new Car("pobi", 0)));
+        assertTrue(carListForTest.contains(new Car("jun", 0)));
+        assertTrue(carListForTest.contains(new Car("woni", 0)));
     }
+
+    @Test
+    @DisplayName("가장 많이 전진한 자동차를 판별 테스트")
+    void checkWinner_test() {
+        Car car1 = new Car("pobi", 4);
+        Car car2 = new Car("jun", 3);
+        Car car3 = new Car("woni", 4);
+        List<Car> carListForTest = List.of(car1, car2, car3);
+
+        List<String> winnerListForTest = gameManager.checkWinner(carListForTest);
+
+        assertEquals(2, winnerListForTest.size());
+        assertEquals("pobi", winnerListForTest.get(0));
+        assertEquals("woni", winnerListForTest.get(1));
+    }
+
 }
