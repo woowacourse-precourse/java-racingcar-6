87e0bea2de3e5143d7c0c33baa25bfba1b1afa9c
Isaac Han <isaachan233@gmail.com>
2023-11-01 01:29:03 +0900

    refactor: 우승자 목록 로직 변경

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -1,17 +1,23 @@
 package racingcar.model;
 
+import racingcar.enums.ErrorMessages;
+import racingcar.utils.CommonValidator;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Winners {
-    private final List<Car> winners = new ArrayList<>();
+    private final List<Car> winners;
 
-    public Winners(Cars cars, Attempts attempts) {
-        for (Car car : cars.getCarList()) {
-            if (car.getDistance().length() == attempts.getNumber()) {
-                winners.add(car);
-            }
-        }
+    public Winners(Cars cars) {
+        validateWinners(cars);
+        int max = cars.getCars().stream()
+                .mapToInt(car -> car.getDistance())
+                .boxed()
+                .max(Integer::compare)
+                .get();
+        winners = cars.getCars().stream().filter(car -> car.getDistance() == max).collect(Collectors.toList());
     }
 
     public List<String> getWinnersNames() {
@@ -20,5 +26,12 @@ public class Winners {
             winnersNames.add(car.getName());
         }
         return winnersNames;
+    }
+
+    private void validateWinners(Cars cars) {
+        if(cars.getCars() == null)
+            throw new IllegalArgumentException(ErrorMessages.NULL_ERROR.getMessage());
+        if(cars.getCars().size() < 1)
+            throw new IllegalArgumentException(ErrorMessages.NULL_ERROR.getMessage());
     }
 }
