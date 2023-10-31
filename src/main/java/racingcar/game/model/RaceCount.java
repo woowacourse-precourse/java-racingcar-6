456ab488a82d0a3a9b3d58b5aab9d7a47ce8e239
nijesmik <bkybb@duck.com>
2023-10-31 11:48:26 +0900

    feat(model): RaceCount 구현

---
+++ src/main/java/racingcar/game/model/RaceCount.java
@@ -0,0 +1,30 @@
+package racingcar.game.model;
+
+public class RaceCount {
+    static final String NUMBER_SIGN_ERROR = "[ERROR] 시도 횟수는 양수여야 합니다.";
+    static private int totalCount;
+    static private int currentCount;
+
+    private RaceCount() {
+    }
+
+    static public void initializeByInput(String input) {
+        totalCount = Integer.parseInt(input);
+        validateTotalCount();
+        currentCount = 0;
+    }
+
+    static private void validateTotalCount() {
+        if (totalCount <= 0) {
+            throw new IllegalArgumentException(NUMBER_SIGN_ERROR);
+        }
+    }
+
+    static public boolean equalsTotal() {
+        return (totalCount == currentCount);
+    }
+
+    static public void up() {
+        currentCount++;
+    }
+}
