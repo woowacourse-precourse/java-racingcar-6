4fb349bf9e0dc12c8f85491986ab4f4b334bab82
k-kmw <kimmw2_@naver.com>
2023-10-31 22:45:03 +0900

    refactor: 입력 예외처리 리팩토링

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -18,22 +18,25 @@ public class RacingGameController {
         racingGameService.race(userRequestDTO.getCarNames(), userRequestDTO.getTryNum());
     }
 
-    private void checkIllegalInput(String carNames, String tryNumStr) {
+    private void checkIllegalInput(String carNames, String tryNum) {
         try {
-            int tryNum = Integer.parseInt(tryNumStr);
-            if (tryNum < 1) {
-                throw new IllegalArgumentException();
-            }
+            checkTryNum(tryNum);
+            checkCarNames(carNames);
         } catch (RuntimeException e) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void checkCarNames(String carNames) {
         List<String> carNameList = List.of(carNames.split(","));
-        if (carNameList.size() < 2) throw new IllegalArgumentException();
-        for (String carName : carNameList) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException();
-            }
+        if (carNameList.size() < 2 || carNameList.stream().anyMatch(carName -> carName.length() > 5))
+            throw new IllegalArgumentException();
+    }
+
+    private void checkTryNum(String tryNumStr) {
+        int tryNum = Integer.parseInt(tryNumStr);
+        if (tryNum < 1) {
+            throw new IllegalArgumentException();
         }
     }
 
