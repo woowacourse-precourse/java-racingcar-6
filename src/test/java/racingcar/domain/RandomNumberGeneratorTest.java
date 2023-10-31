804d4305bf4b329fc0b19586d5b99d5ba02fe8bf
sotogito <108535526+sotogito@users.noreply.github.com>
2023-10-31 22:56:55 +0900

    test : 지정된 범위에서의 랜덤 숫자 생성

--- src/test/java/racingcar/domain/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/domain/RandomNumberGeneratorTest.java
@@ -9,12 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class RandomNumberGeneratorTest {
 
     @Test
-    void 랜덤값이_4이상일때_true반환(){
-
-
-    }
-
-    @Test
     void 지정된_범위의_랜덤(){
         int number = Randoms.pickNumberInRange(0,9);
         assertThat(number).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
