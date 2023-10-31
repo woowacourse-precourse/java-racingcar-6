d4c5e498ba0dc89db1c9b111cad4b5273127a899
K-KY <kyuyoungk@naver.com>
2023-10-31 19:07:29 +0900

    Feat(DataTypeChanger) : String 타입의 리스트를 Car타입의 리스트로 변환

--- src/main/java/racingcar/DataTypeChanger.java
+++ src/main/java/racingcar/DataTypeChanger.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypeChanger {
     public static List<String> stringToList(String names) {
         names = names.replace(" ", "");
         return List.of(names.split(","));
+    }
+
+    public static List<Car> stringToCar(List<String> names) {
+        List<Car> cars = new ArrayList<>();
+        names.forEach(name -> cars.add(new Car(name)));
+        return cars;
     }
 }
