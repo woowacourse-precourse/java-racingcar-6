900db76480111cacc56a8a100279f504d64c1daf
alscjf1329 <alscjf1329@gmail.com>
2023-10-31 21:42:36 +0900

    refactor(RacingCarUtil): RacingCarUtil 작성
    RacingCarGameInput에 있던 검증하는 메소드들을 RacingCarUtil로 분류해습니다.

--- src/main/java/racingcar/utils/RacingCarGameInput.java
+++ src/main/java/racingcar/utils/RacingCarGameInput.java
@@ -1,14 +1,12 @@
 package racingcar.utils;
 
-import static racingcar.Message.ErrorMessage.CAR_NAME_HAVE_WHITE_CHAR;
-import static racingcar.Message.ErrorMessage.CAR_NAME_IS_BIGGER_THAN_5CHAR;
-import static racingcar.Message.ErrorMessage.CAR_NAME_IS_DUPLICATED;
-import static racingcar.Message.ErrorMessage.CAR_NAME_IS_EMPTY;
-import static racingcar.Message.ErrorMessage.MOVE_COUNT_IS_NOT_NUM;
-import static racingcar.Message.ErrorMessage.MOVE_COUNT_IS_NOT_POSITIVE_NUM;
+import static racingcar.utils.RacingCarUtil.validateCarList;
+import static racingcar.utils.RacingCarUtil.validateCarNamesInput;
+import static racingcar.utils.RacingCarUtil.validateDuplication;
+import static racingcar.utils.RacingCarUtil.validateMoveCount;
+import static racingcar.utils.RacingCarUtil.validateNumber;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.dto.RacingCar;
@@ -18,8 +16,10 @@ public class RacingCarGameInput {
     public static List<RacingCar> readCarNames() {
         String input = Console.readLine();
         validateCarNamesInput(input);
+
         String[] arr = input.split(",");
         validateDuplication(arr);
+
         List<RacingCar> carList = Arrays.stream(arr)
                 .map(RacingCar::new)
                 .toList();
@@ -27,59 +27,14 @@ public class RacingCarGameInput {
         return carList;
     }
 
-    private static boolean validateCarNamesInput(String input) {
-        String noWhiteSpaceInput = input.replace(" ", "");
-        if (noWhiteSpaceInput.length() != input.length()) {
-            throw new IllegalArgumentException(CAR_NAME_HAVE_WHITE_CHAR.getMessage());
-        }
-        return true;
-    }
-
-    private static boolean validateDuplication(String[] arr) {
-        List<String> tempArr = new ArrayList<>();
-        for (String e : arr) {
-            if (tempArr.contains(e)) {
-                throw new IllegalArgumentException(CAR_NAME_IS_DUPLICATED.getMessage());
-            }
-            tempArr.add(e);
-        }
-        return true;
-    }
-
-    private static boolean validateCarList(List<RacingCar> carList) {
-        for (RacingCar car : carList) {
-            if (car.getName().length() > 5) {
-                throw new IllegalArgumentException(CAR_NAME_IS_BIGGER_THAN_5CHAR.getMessage());
-            }
-            if (car.getName().isEmpty()) {
-                throw new IllegalArgumentException(CAR_NAME_IS_EMPTY.getMessage());
-            }
-        }
-        return true;
-    }
-
     public static int readMoveCount() {
         String input = Console.readLine();
         validateNumber(input);
+
         int moveCount = Integer.parseInt(input);
         validateMoveCount(moveCount);
         return moveCount;
     }
 
-    private static boolean validateNumber(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(MOVE_COUNT_IS_NOT_NUM.getMessage());
-        }
-        return true;
-    }
-
-    private static boolean validateMoveCount(int moveCount) {
-        if (moveCount < 1) {
-            throw new IllegalArgumentException(MOVE_COUNT_IS_NOT_POSITIVE_NUM.getMessage());
-        }
-        return true;
-    }
 
 }
