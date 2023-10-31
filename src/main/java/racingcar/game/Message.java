4beffff96bd40cd3f0de8346949c3349f5103511
DooDuZ <sin9158@naver.com>
2023-10-31 16:56:31 +0900

    fix: requestPlayCount 수정
    횟수 제한 문구 추가

--- src/main/java/racingcar/game/Message.java
+++ src/main/java/racingcar/game/Message.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Message {
     public final String requestPlayerName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public final String requestPlayCount = "시도할 회수는 몇회인가요?";
+    public final String requestPlayCount = "시도할 회수는 몇회인가요? (1~100회 제한)";
     public final String result = "실행 결과";
     private final String champion = "최종 우승자 : ";
     private final String spaceColon = " : ";
