e741611f78d74235f95af66235fc879dce2c8312
Jae-Young98 <ygy0102@gmail.com>
2023-10-31 19:47:16 +0900

    style(test): 메소드 라인 정렬

--- src/test/java/racingcar/util/ConverterTest.java
+++ src/test/java/racingcar/util/ConverterTest.java
@@ -1,22 +1,19 @@
-package racingcar;
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import racingcar.util.Converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConverterTest {
-
-    private static final Converter converter = new Converter();
-
     @Test
     void 문자열_리스트_변환() {
         // given
         String names = "pobi,woni,test";
 
         // when
-        List<String> carNames = converter.getCarNames(names);
+        List<String> carNames = Converter.getCarNames(names);
 
         // then
         assertThat(carNames.size()).isEqualTo(3);
--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -1,14 +1,13 @@
-package racingcar;
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import racingcar.util.InputValidator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
-    private static final InputValidator validator = new InputValidator();
 
     @Test
     void 자동차_이름_길이_예외() {
@@ -16,7 +15,7 @@ public class InputValidatorTest {
         List<String> names = Arrays.asList("woni,pobiii".split(","));
 
         // when, then
-        assertThatThrownBy(() -> validator.validateCarNames(names))
+        assertThatThrownBy(() -> InputValidator.validateCarNames(names))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차의 이름 길이는 1이상 5이하입니다.");
     }
@@ -27,7 +26,7 @@ public class InputValidatorTest {
         String input = "a";
 
         // when, then
-        assertThatThrownBy(() -> validator.validateAttemptNumber(input))
+        assertThatThrownBy(() -> InputValidator.validateAttemptNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자만 입력 가능합니다.");
     }
@@ -38,7 +37,7 @@ public class InputValidatorTest {
         String input = "0";
 
         // when, then
-        assertThatThrownBy(() -> validator.validateAttemptNumber(input))
+        assertThatThrownBy(() -> InputValidator.validateAttemptNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도 횟수는 1이상입니다.");
     }
