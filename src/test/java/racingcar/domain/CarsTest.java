b517a1855ef1b836deb213723ae9ceec2ae9d3be
chaeshee0908 <chaeshee0908@naver.com>
2023-10-31 19:22:17 +0900

    [feat] 최종 우승자 테스트 구현

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,5 +21,30 @@ public class CarsTest {
 
         List<String> carNames = cars.getCars().stream().map(Car::getName).collect(toList());
         assertEquals(names, carNames);
+    }
+
+    @Test
+    void 최종_우승자_테스트() {
+        Car car1 = mock(Car.class);
+        Car car2 = mock(Car.class);
+        Car car3 = mock(Car.class);
+
+        when(car1.getMoveDistance()).thenReturn(5);
+        when(car2.getMoveDistance()).thenReturn(3);
+        when(car3.getMoveDistance()).thenReturn(5);
+
+        when(car1.getName()).thenReturn("pobi");
+        when(car2.getName()).thenReturn("woni");
+        when(car3.getName()).thenReturn("jun");
+
+        Cars cars = new Cars(Arrays.asList("pobi", "woni", "jun"));
+
+        List<Car> carList = cars.getCars();
+        carList.set(0, car1);
+        carList.set(1, car2);
+        carList.set(2, car3);
+
+        String result = cars.award();
+        assertEquals("pobi, jun", result);
     }
 }
