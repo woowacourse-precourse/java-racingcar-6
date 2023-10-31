a6e387fbb994326a07040991cc9dbe4f288308b0
kdomo <dongho5088@naver.com>
2023-11-01 00:30:36 +0900

    test: Car Test 수정

---
+++ src/test/java/racingcar/domain/GameResultTest.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameResultTest {
+
+    @Test
+    void 게임_결과_생성() {
+
+    }
+
+    @Test
+    void gameResultPrint() {
+    }
+}
