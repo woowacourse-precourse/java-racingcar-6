9e603a0dba4d2e3e2e77f43631ab157de4946ef1
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-31 20:05:20 +0900

    2023.10.31_feat : Car Car생성자 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+
+    private final String NAME;
+    private int position = 0;
+
+    public Car(String name) {
+        this.NAME = name;
+    }
+}
