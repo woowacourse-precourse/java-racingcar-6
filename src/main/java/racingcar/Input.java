cb9b3446bdc7a86713b6e8dde1e8dd2a349086cd
Leegeonwoo7 <workgeon07@naver.com>
2023-11-01 05:19:09 +0900

    입력을 위한 클래스

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+	public static String[] setCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String[] nameArr = Console.readLine().split(",");
+		for(int i=0; i<nameArr.length; i++) {
+			ValidateInputName.validateNameLenth(nameArr[i]);
+		}
+		return nameArr;
+	}
+	
+	public static int setAttemptNum() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		int attemptNum = Integer.parseInt(Console.readLine());
+		return attemptNum;
+	}
+}
