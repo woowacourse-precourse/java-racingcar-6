22de4647338440260cbcd04d444baaaa1c6d75e3
dmdrk1414 <seungchan141414@gmail.com>
2023-10-31 20:52:32 +0900

    [Refactor(racing)] 변수 이름 변경

--- src/main/java/racingcar/domain/JudgeStandard.java
+++ src/main/java/racingcar/domain/JudgeStandard.java
@@ -4,16 +4,9 @@ import racingcar.utill.Utill;
 
 public class JudgeStandard {
 
-    /**
-     * 자동차중에 전진을 한 숫작 numOfWinCondition 이면 승리를 한것이다.
-     *
-     * @param raceCar
-     * @param numOfWinCondition
-     * @return
-     */
-    public Boolean isVictoryCondition(RaceCar raceCar, Integer numOfWinCondition) {
+    public Boolean isVictoryCondition(RaceCar raceCar, Integer numOfCondition) {
         // 자동차중에 전진을 한 숫자가 정해진 조건 숫자와 같으면 승리를 한것이다.
-        Integer cntMovementOfCurrentRaceCar = raceCar.getCntMovementOfCar();
-        return Utill.isSameNum(cntMovementOfCurrentRaceCar, numOfWinCondition);
+        Integer cntMoveOfRaceCar = raceCar.getCntMovementOfCar();
+        return Utill.isSameNum(cntMoveOfRaceCar, numOfCondition);
     }
 }
