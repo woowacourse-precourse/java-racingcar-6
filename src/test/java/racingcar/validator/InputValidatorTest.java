4bf070a852d84b1e350093cd987aa097c886c577
heesane <tees3359@gmail.com>
2023-10-31 16:39:25 +0900

    test : test code about validator

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar.validator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,26 +8,47 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputValidatorTest {
 
     @Test
+    @DisplayName("이름 길이가 5자를 초과하는 경우")
     void validateNameLength() {
         //given
         String carName = "123456";
-        //when
 
         //then
         assertThatThrownBy(()->
                 InputValidator.validateNameLength(carName)).isInstanceOf(IllegalArgumentException.class);
-
     }
 
     @Test
+    @DisplayName("자동차 이름이 없는 경우")
     void validateCarNamesIsEmpty() {
+        //given
+        String[] carNames = {};
+        //when
+        //then
+        assertThatThrownBy(()->
+                InputValidator.validateCarNamesIsEmpty(carNames)).isInstanceOf(IllegalArgumentException.class);
+
     }
 
     @Test
+    @DisplayName("반복 횟수가 숫자가 아닌 경우")
     void validateRepeatNumberIsNumber() {
+        //given
+        String repeatNumber = "a";
+
+        //then
+        assertThatThrownBy(()->
+                InputValidator.validateRepeatNumberIsNumber(repeatNumber)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
+    @DisplayName("반복 횟수가 0 이하인 경우")
     void validateRepeatNumberSign() {
+        //given
+        Integer repeatNumber = -1;
+
+        //then
+        assertThatThrownBy(()->
+                InputValidator.validateRepeatNumberSign(repeatNumber)).isInstanceOf(IllegalArgumentException.class);
     }
 }
