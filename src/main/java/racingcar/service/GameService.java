48e043ba6e8ec10ecabf981c18a9b0bdd2003af2
leeminjun <sohwi0305@gmail.com>
2023-11-01 00:10:00 +0900

    feat: 랜덤값 받아서 4이상이면 앞으로 가는 로직 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -6,10 +6,23 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.domain.Game;
+import racingcar.repository.GameRepository;
 
 public class GameService {
     private static final int START_POSITION = 0;
     private static final int END_POSITION = 9;
+    private final GameRepository gameRepository = new GameRepository();
+    public void Fowardstart(Game game){
+
+    }
+
+    private void moveForwardByRandomNumber(Game game) {
+        for (Car car : game.getCars()) {
+            if (START_POSITION <= getRandomDistance()) {
+                car.moveFoward(1);
+            }
+        }
+    }
 
     private int getRandomDistance() {
         return Randoms.pickNumberInRange(START_POSITION, END_POSITION);
