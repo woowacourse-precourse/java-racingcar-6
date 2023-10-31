a665b9e5dc999c659cd76254d9de7d12e6424828
kdomo <dongho5088@naver.com>
2023-10-31 22:41:47 +0900

    feat: Car domain 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int distance = 0;
+
+    private Car(String name) {
+        this.name = name;
+    }
+
+    private Car(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public static Car registerCar(String name) {
+        return new Car(name);
+    }
+
+    public void moveForwardCar() {
+        distance++;
+    }
+}
