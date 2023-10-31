4c8e32ac6cfe04904a267eaddeb58754fc552ae3
wisenick1 <51376082+wisenick1@users.noreply.github.com>
2023-10-31 20:25:47 +0900

    make racing game view

--- src/main/java/racingcar/view/ViewRacingGame.java
+++ src/main/java/racingcar/view/ViewRacingGame.java
@@ -1,2 +1,19 @@
-package racingcar.view;public class ViewRacingGame {
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class ViewRacingGame {
+    public void printCarStatus(Car car) {
+        System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+    }
+
+    public void printWinners(List<String> winners) {
+        if (winners.size() == 1) {
+            System.out.println("최종 우승자 : " + winners.get(0));
+        } else {
+            System.out.println("최종 우승자 : " + String.join(", ", winners));
+        }
+    }
 }
