30e5e8945f803eed3625feb911a13f2fbcfef623
ChoiseU <csw980928@gmail.com>
2023-11-01 00:41:23 +0900

    feat : 사용자로부터 입력 받고 입력 값이 에러가 있는지 확인 하는 메서드 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,14 +1,50 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Console;
+import racingcar.application.racing.dto.RacingDto;
 import racingcar.util.RacingUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class RacingGameController {
 
     RacingUtils racingUtils = new RacingUtils();
 
-    public void gameStart() {
-        racingUtils.showEnterMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Console.readLine();
+    private String inputCarListMsg = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private String inputGameCntMsg = "시도할 회수는 몇회인가요?";
+    private int checkLen = 5;
+
+
+    public void game() {
+        ready();
+    }
+
+    public void ready() {
+        List<String> carList = getInputCarListAndCheckName();
+        int gameCnt = getInputGameCntAndCheckGameCnt();
+
+    }
+
+    public List<String> getInputCarListAndCheckName() {
+        String cars = racingUtils.getInputAnswerAndShowMessage(inputCarListMsg);
+        List<String> carList = racingUtils.splitStringToList(cars);
+        boolean isOverLen = racingUtils.isCheckStringLength(carList, checkLen);
+        if(isOverLen) {
+            throw new IllegalArgumentException();
+        }
+        return carList;
+    }
+
+    public int getInputGameCntAndCheckGameCnt() {
+        String gameCnt = racingUtils.getInputAnswerAndShowMessage(inputGameCntMsg);
+        boolean isGameCountValid = racingUtils.isNumericOrNonNegative(gameCnt);
+        if(isGameCountValid) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(gameCnt);
     }
 }
