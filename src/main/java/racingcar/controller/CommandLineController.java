bdc98be43466e7618ae9044397800465af9da8be
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-10-31 16:16:38 +0900

    프로그램을 시작하는 start메서드 작성

--- src/main/java/racingcar/controller/CommandLineController.java
+++ src/main/java/racingcar/controller/CommandLineController.java
@@ -3,9 +3,14 @@ package racingcar.controller;
 import racingcar.game.Game;
 
 public class CommandLineController {
+
     private Game game;
 
     public CommandLineController(Game game) {
         this.game = game;
+    }
+
+    public void start() {
+        game.startGame();
     }
 }
