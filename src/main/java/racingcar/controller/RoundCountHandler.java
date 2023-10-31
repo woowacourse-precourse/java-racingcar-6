95ec9174cc1d77f4709f95b7115292267f8373f7
Genius-PAK <pyj789123@gmail.com>
2023-10-31 19:09:48 +0900

    feat: add a method to check whether the roundCount is zero or not in RoundcountHandler class

--- src/main/java/racingcar/controller/RoundCountHandler.java
+++ src/main/java/racingcar/controller/RoundCountHandler.java
@@ -7,11 +7,17 @@ public class RoundCountHandler implements UserInputHandler<Integer>{
     public void handle(String stringRoundCount){
         try{
             roundCount = Integer.parseInt(stringRoundCount);
+            isZero(roundCount);
         } catch (NumberFormatException e){
             throw new IllegalArgumentException();
         }
     }
 
+    private void isZero(int roundCount){
+        if (roundCount == 0){
+            throw new IllegalArgumentException();
+        }
+    }
     @Override
     public Integer getHandledResult(){
         return roundCount;
