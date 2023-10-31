6ba55f02b0c996ce978fa0f8743d7706e27e4d74
LeeJiWon <dlwldnjs1009@gmail.com>
2023-10-31 22:39:52 +0900

    feat: 사용자 안내 메세지 출력 메소드 추가

--- src/main/java/racingcar/io/OutputPrinter.java
+++ src/main/java/racingcar/io/OutputPrinter.java
@@ -4,6 +4,10 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class OutputPrinter {
+    public static void printExecutionResult() {
+        System.out.println("실행 결과");
+    }
+
     public static void printCarsPosition(List<Car> cars) {
         for (Car car : cars) {
             System.out.print(car.getName() + " : ");
@@ -16,5 +20,13 @@ public class OutputPrinter {
 
     public static void printWinners(List<String> winners) {
         System.out.print("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public void printCarNamePrompt() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printTriesPrompt() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
