5068c1e9aadcd9cec386c87821bdabbe2631ce11
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-10-31 18:41:20 +0900

    refactor(input): cache Pattern object for improved performance in Validator class

--- src/main/java/racingcar/input/Validator.java
+++ src/main/java/racingcar/input/Validator.java
@@ -1,8 +1,11 @@
 package racingcar.input;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 class Validator {
     private static final int MINIMUM_CAR_NAME_LENGTH = 5;
-    private static final String NUMBERS_ONLY_REGEX = "[0-9]+";
+    private static final Pattern NUMBERS_OLNY_PATTERN = Pattern.compile("[0-9]+");
 
     static void validateCarNames(String userInput) {
         String[] carNames = userInput.split(",");
@@ -15,7 +18,8 @@ class Validator {
     }
 
     static void validateNumberOfAttempts(String userInput) {
-        if (!userInput.matches(NUMBERS_ONLY_REGEX)) {
+        Matcher matcher = NUMBERS_OLNY_PATTERN.matcher(userInput);
+        if (!matcher.matches()) {
             throw new IllegalArgumentException(
                     "The number of attempts '" + userInput + "' should be a positive integer.");
         }
