06defd0c4d4aaeb8f882e2ab78a59754195aeaea
nove1080 <skwpqjq@gmail.com>
2023-11-01 01:59:29 +0900

    feat: Car 클래스의 비교 메서드 및 getter 추가

--- src/main/java/racingcar/game/components/Car.java
+++ src/main/java/racingcar/game/components/Car.java
@@ -1,8 +1,11 @@
 package racingcar.game.components;
 
+import java.util.Objects;
+
 public class Car {
     private String name;
     private int distance;
+
     public Car(String name) {
         this.name = name;
         distance = 0;
@@ -12,7 +15,24 @@ public class Car {
         return name;
     }
 
+    public int getDistance() {
+        return distance;
+    }
+
     public void move() {
         distance++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
     }
 }
