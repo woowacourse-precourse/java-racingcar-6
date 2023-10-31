94b523bdcaad882b05696fa6db002934cf0bf25d
dj <fjzl12@naver.com>
2023-10-31 16:20:21 +0900

    feat(RoundValidator): 입력된 진행 횟수 검증 기능 구현

---
+++ src/main/java/racingcar/domain/RoundValidator.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+public class RoundValidator {
+    public static int checkNaturalNumber(String number) {
+        int naturalNumber;
+        try {
+            naturalNumber = Integer.parseInt(number);
+            if (naturalNumber < 1 || naturalNumber > 10) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다.");
+        }
+        return naturalNumber;
+    }
+}
