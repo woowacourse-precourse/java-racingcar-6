2e4ce7567f94bc2e97e211cc681ed78771022af7
2018009107 <2018009107@hanyang.ac.kr>
2023-10-31 22:54:29 +0900

    feat: car class 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public class Car {
+        String Name;
+        int Location;
+
+        public void Move(int moveDistance) {
+            Location = Location + moveDistance;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+                
     }
 }
