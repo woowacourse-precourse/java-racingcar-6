791702ae9ad419c80367d0eb91092d1e29d4bc8c
Junobee25 <wnsdhqo@naver.com>
2023-10-31 19:16:58 +0900

    feat(car): Car 클래스 생성

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,15 @@
+package racingcar.car;
+
+import java.util.HashMap;
+
+public class Car {
+    private HashMap<String,String> carList;
+
+    public HashMap<String, String> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(HashMap<String, String> carList) {
+        this.carList = carList;
+    }
+}
