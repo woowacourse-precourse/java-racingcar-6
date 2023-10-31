5cbb92109e5eab65d5c512fc8caf2ca830ad2a3a
jeondui <jeondui@gmail.com>
2023-10-31 23:52:23 +0900

    feat(viewTest): 화면에 차수별 게임 실행 결과를 출력하는 기능 테스트

--- src/test/java/racingcar/view/viewTest.java
+++ src/test/java/racingcar/view/viewTest.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 
 public class viewTest extends NsTest {
 
@@ -33,6 +34,24 @@ public class viewTest extends NsTest {
 
         //then
         assertThat(output()).isEqualTo("최종 우승자 : pobi,dui");
+    }
+
+    @DisplayName("차수별 실행 결과를 출력하는 기능 테스트")
+    @Test
+    void printResult() {
+        //given
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("dui");
+
+        car1.move(2);
+        car2.move(4);
+        List<Car> carList = List.of(car1, car2);
+
+        //when
+        OutputView.printResult(carList);
+
+        //then
+        assertThat(output()).isEqualTo("pobi : --\ndui : ----");
     }
 
     @Override
