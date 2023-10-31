159df4b14edafec2a045b75502b13e4dee4bae9c
kdomo <dongho5088@naver.com>
2023-11-01 00:27:31 +0900

    refoctor: ErrorMessage enum 객체 생성하여 공통으로 관리

--- src/main/java/racingcar/validator/GameValidator.java
+++ src/main/java/racingcar/validator/GameValidator.java
@@ -5,13 +5,13 @@ import racingcar.constants.GameConstants;
 public class GameValidator {
     public static void validationCarNameLength(String carName) {
         if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차의 이름은 5자 이하만 입력 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_EXECESS.getErrorMessage());
         }
     }
 
     public static void validationMoveCntIsNumber(String moveCnt) {
         if (!moveCnt.matches(GameConstants.ONLY_NUMBER_REGEX_PATTERN)) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER_POSSIBLE.getErrorMessage());
         }
     }
 }
