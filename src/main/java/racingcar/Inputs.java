d2e2d63b67a0b425b943ebbc72283036a9ec58cd
alenjb <alenjb@naver.com>
2023-10-31 23:50:47 +0900

    :sparkles:feat(racingcar): 경주 할 자동차 이름들을 쉼표(,) 기준으로 분리하는 기능 구현
    Inputs.java에 경주 할 자동차 이름들을 쉼표(,) 기준으로 분리하는 메서드 splitCarNames를 구현함.

--- src/main/java/racingcar/Inputs.java
+++ src/main/java/racingcar/Inputs.java
@@ -10,4 +10,18 @@ public class Inputs {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return camp.nextstep.edu.missionutils.Console.readLine(); // 사용자로부터 입력받은 자동차 이름들이 쉼표로 구분되어 있는 문자열
     }
+
+    public void splitCarNames(String carNames) {
+        // 문자열을 쉼표(,)로 나누어 자동차 이름 배열에 저장
+        String[] carNamesArray = carNames.split(",");
+
+        // 쉼표로 이름들이 구분되어 있는 경우: 각 요소를 자동차 이름 리스트에 추가
+        if (carNamesArray.length > 0) {
+            for (String carName : carNamesArray) {
+                this.carNamesList.add(carName.trim()); // 공백 제거
+            }
+        } else { // 쉼표가 없는 경우: 문자열을 자동차 이름 리스트에 추가
+            this.carNamesList.add(carNames.trim());
+        }
+    }
 }
