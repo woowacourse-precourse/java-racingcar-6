da79ae64d03919eb2d1e111f11a5ca2c6f44ad84
choheejin <hjcho9510@gmail.com>
2023-10-31 17:34:14 +0900

    test(input): rename the test function

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -15,7 +15,7 @@ import racingcar.utils.Validator;
 public class InputTest {
     @Test
     @DisplayName("사용자 입력이 콤마(,)를 기준으로 나뉘어지는가?")
-    void testStoreCarNames() {
+    void input_자동차이름_구분() {
         String input = "pobi,jun";
         List<String> names = Converter.convertStringToList(input);
 
@@ -24,7 +24,7 @@ public class InputTest {
 
     @Test
     @DisplayName("자동차 명이 5글자 초과일 경우, 에러 문구를 발생하는가?")
-    void testErrorNot5CharLess() {
+    void input_자동차이름_5글자초과() {
         String input = "pobi,javaji";
 
         List<String> names = Converter.convertStringToList(input);
@@ -37,16 +37,16 @@ public class InputTest {
 
 
     @Test
-    @DisplayName("사용자의 입력이 숫자로 변경 되는가?")
-    void testConvertToInt() {
+    @DisplayName("사용자의 입력(횟수)이 정수로 변경 되는가?")
+    void input_횟수_정수로_변경() {
         String input = "4";
 
         assertThat(Converter.convertStringToInt(input)).isEqualTo(4);
     }
 
     @Test
-    @DisplayName("사용자의 입력이 숫자가 아닐때, 에러를 출력하는가?")
-    void testErrorConvertToInt() {
+    @DisplayName("사용자의 입력(횟수)이 숫자가 아닐때, 에러를 출력하는가?")
+    void input_횟수_정수가_아님() {
         String input = "다섯번";
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
@@ -56,8 +56,8 @@ public class InputTest {
     }
 
     @Test
-    @DisplayName("사죵자의 입력이 0일때, 에러를 출력하는가?")
-    void testErrorZero() {
+    @DisplayName("사용자의 입력(횟수)이 0일때, 에러를 출력하는가?")
+    void input_횟수_0() {
         String input = "0";
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
@@ -67,8 +67,8 @@ public class InputTest {
     }
 
     @Test
-    @DisplayName("사죵자의 입력이 숫자가 아닐때, 에러를 출력하는가? - 공백")
-    void input_숫자가_아닌_공백() {
+    @DisplayName("사용자의 입력(횟수)이 숫자가 아닐때, 에러를 출력하는가? - 공백")
+    void input_횟수_숫자가_아닌_공백() {
         String input = "";
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
@@ -78,8 +78,8 @@ public class InputTest {
     }
 
     @Test
-    @DisplayName("사용자의 입력이 중복이 된 경우, 에러를 출력하는가?")
-    void testErrorNameDuplicated() {
+    @DisplayName("사용자의 입력(횟수)이 중복이 된 경우, 에러를 출력하는가?")
+    void input_자동차이름_중복발생() {
         String input = "pobi,pobi";
 
         List<String> names = Converter.convertStringToList(input);
@@ -93,7 +93,7 @@ public class InputTest {
 
     @ParameterizedTest
     @ValueSource(strings = {",java,jigi", "java,,jigi", "java, ,jigi", " ,java,jigi"})
-    @DisplayName("사용자의 입력에 공백이 있는 경우, 에러를 출력하는가?")
+    @DisplayName("자동차 이름에 공백이 있는 경우, 에러를 출력하는가?")
     void input_자동차_이름_공백_테스트(String input) {
         List<String> names = Converter.convertStringToList(input);
 
