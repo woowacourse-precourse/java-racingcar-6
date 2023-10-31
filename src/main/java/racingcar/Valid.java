1f1f829ef584b1b7c8dad215a341d40e211e0848
LUKR7Q <ybkim0628@naver.com>
2023-10-31 12:41:12 +0900

    기능(validator): Validator 클래스에 checkNames() 메서드 추가

--- src/main/java/racingcar/Valid.java
+++ src/main/java/racingcar/Valid.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Valid {
 
+    private static final int MAX_LENGTH_OF_NAME = 5;
     private static final String ERROR_MSG = "[ERROR] ";
 
     public static void isNull(String input) {
@@ -18,5 +19,21 @@ public class Valid {
         } catch (Exception e) {
             throw new IllegalArgumentException(ERROR_MSG + "시도 횟수는 숫자여야 합니다.");
         }
+    }
+
+    public static boolean checkNames(String[] names) {
+        List<String> nameList = Arrays.asList(names);
+
+        for (String name : nameList) {
+            if (name.trim().length() > MAX_LENGTH_OF_NAME) {
+                throw new IllegalArgumentException(ERROR_MSG + "자동차 이름은 최대 5글자입니다.");
+            }
+        }
+
+        if (new HashSet<>(nameList).size() != nameList.size()) {
+            throw new IllegalArgumentException(ERROR_MSG + "중복된 이름을 입력했습니다.");
+        }
+
+        return true;
     }
 }
