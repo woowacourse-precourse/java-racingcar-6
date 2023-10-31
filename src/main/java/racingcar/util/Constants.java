d3cb810505b2f74234916b14184747b37af6ddda
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-31 22:08:21 +0900

    test(controller): RacingGameConsole 클래스 테스트 코드 추가
    움직임 전략 여부에 따라 레이싱 게임 생성 가능 여부를 테스트합니다.
    - 움직임 전략이 null일 경우에는 실패합니다.

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
     public static final String INVALID_ROUND_EMPTY = "시도할 회수를 입력해주세요.";
     public static final String INVALID_ROUND_ONE_OR_MORE = "시도할 회수는 1 이상의 숫자만 가능합니다.";
     public static final String REGEX_NUMBER = "[0-9]+";
+    public static final String INVALID_MOVE_STRATEGY_NULL = "움직임 전략이 null입니다.";
 
     /** Input, Output **/
     public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
