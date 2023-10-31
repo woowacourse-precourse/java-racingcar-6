3d0083e8a38c2dc068ee4192b256171a51629cd9
dhlrwu <yso7414@sookmyung.ac.kr>
2023-10-31 23:23:07 +0900

    refactor(racingcar): 예외 처리 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,64 +8,65 @@ import racingcar.domain.Judgment;
 
 public class Application {
     public static void main(String[] args) {
-        List<Car> carList = registerEntry();
+        List<Car> entry = registerEntry();
         int lapCount = askRaceLaps();
 
         Judgment judgment = new Judgment();
         System.out.println("실행 결과");
-        for(int i = 0; i < lapCount; i++) {
-            judgment.decideMovement(carList);
-            printLapDetails(carList);
+        for (int i = 0; i < lapCount; i++) {
+            judgment.decideMovement(entry);
+            printLapDetails(entry);
         }
 
-        announceWinner(carList);
+        announceWinner(entry);
     }
 
     public static List<Car> registerEntry() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        String[] array = carNames.split(",");
-        List<Car> carList = new ArrayList<>();
-        for (String name : array) {
-            carList.add(new Car(name));
+        String nameString = Console.readLine();
+        String[] nameArray = nameString.split(",");
+        List<Car> entry = new ArrayList<>();
+        for (String name : nameArray) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+            entry.add(new Car(name));
         }
-        return carList;
+        return entry;
     }
 
     public static int askRaceLaps() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
+        try {
+            int laps = Integer.parseInt(Console.readLine());
+            if (laps <= 0) {
+                throw new IllegalArgumentException("0보다 큰 값을 입력하세요.");
+            }
+            return laps;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효한 입력값이 아닙니다.");
+        }
     }
 
-    public static void printLapDetails(List<Car> carList) {
-        for(Car car : carList) {
+    public static void printLapDetails(List<Car> entry) {
+        for (Car car : entry) {
             car.printDistance();
         }
-        System.out.println();
+        System.out.println(); // 공백
     }
 
-    public static void announceWinner(List<Car> carList) {
+    public static void announceWinner(List<Car> entry) {
         List<String> winnerList = new ArrayList<>();
-        int max = carList.get(0).distanceMoved;
-        for (Car car : carList) {
+        int max = entry.get(0).distanceMoved;
+        for (Car car : entry) {
             if (car.distanceMoved > max) {
                 max = car.distanceMoved;
                 winnerList.clear();
                 winnerList.add(car.getName());
-            }
-            else if (car.distanceMoved == max) {
+            } else if (car.distanceMoved == max) {
                 winnerList.add(car.getName());
             }
         }
-
-        StringBuilder nameOfWinners = new StringBuilder();
-        for (String name : winnerList) {
-            nameOfWinners.append(name).append(", ");
-        }
-        // 마지막 ", " 제거
-        if (nameOfWinners.length() > 0) {
-            nameOfWinners.setLength(nameOfWinners.length() - 2);
-        }
-        System.out.println("최종 우승자 : " + nameOfWinners);
+        System.out.println("최종 우승자 : " + String.join(", ", winnerList));
     }
 }
