b05a8c2abd89e193bca79f335cefa0268da38408
D0ri123 <lsh81550@gmail.com>
2023-10-31 14:34:00 +0900

    test: Game 클래스가 하는 역할 테스트 구현 - 4차 구현
    시도 횟수 입력을 검증하는 테스트를 구현했습니다.
    그리고 게임을 진행했을 때 라운드의 결과가 Map으로 저장되는지
    검증하는 테스트를 구현했습니다.

---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,63 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class GameTest {
+
+    Cars cars;
+    Game game;
+
+    @BeforeEach
+    void beforeGame() {
+        cars = new Cars();
+        cars.settingNames("seori,snow,joy");
+        game = new Game(cars);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "1",
+        "1 ",
+        " 1",
+        " 1 "
+    })
+    void 시도횟수를_올바르게_입력한_경우(String input) {
+        game.settingChance(input);
+        assertEquals(1, game.tryChance);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "0,0보다 큰 숫자를 입력해야 합니다.",
+        "r,숫자를 입력해야 합니다.",
+    })
+    void 시도횟수를_잘못_입력한_경우(String input, String exceptionMessage) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+            () -> game.settingChance(input));
+        assertEquals(e.getMessage(), exceptionMessage);
+    }
+
+    @Test
+    void 게임_1라운드_결과가_Map으로_저장된다() {
+        game.running();
+        Map<String, Integer> gameResult = game.getGameResult();
+        ArrayList<String> carNames = new ArrayList<>();
+
+        for(Car car : game.getCars().cars) {
+            carNames.add(car.name);
+        }
+        Set<String> resultKey = gameResult.keySet();
+
+        assertEquals(resultKey.size(), carNames.size());
+        assertTrue(resultKey.containsAll(carNames));
+    }
+
+}
