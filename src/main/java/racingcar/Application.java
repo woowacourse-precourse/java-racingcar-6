a66312d84af792d190383ba666fba9e2cf38bd46
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-11-01 01:39:20 +0900

    최종 Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import setting.Information;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		Information inf = new Information();
+		inf.inputname();
+
+		System.out.println("시도할 회수는 몇회인가요?");
+		inf.inputNum();
+
+		System.out.print("최종 우승자 : ");
+		inf.winner();
+	}
 }
