c752cbd4c44a7c1409b9c83aa673cfe76097844e
yooonwodyd <dbswodyd00@naver.com>
2023-11-01 02:22:47 +0900

    컨트롤러 작성. init 과 run의 분리를 위해 생성자 추가

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,35 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.model.RaceNumber;
+import racingcar.view.GameView;
+
+import java.util.Arrays;
+
+public class Controller {
+    GameView gameView;
+    Race race;
+    public static Controller init(GameView gameView){
+        return new Controller(gameView,new Race());
+    }
+
+    public void RunGame(){
+        RaceNumber raceNumber = RaceNumber.of(gameView.printAttemptsNumInputRequest());
+        gameView.printPreResultMessage();
+        for(int i = 0; i < raceNumber.count(); i++){
+            race.run();
+            gameView.printResult(race.toResult());
+        }
+        gameView.printWinner(race.getWinner());
+    }
+
+    private Controller(GameView gameView, Race race) {
+        this.gameView = gameView;
+        this.race = race;
+        String names = gameView.printCarInputRequest();
+        Arrays.stream(names.split(","))
+                .map(Car::of)
+                .forEach(race::addCar);
+    }
+}
