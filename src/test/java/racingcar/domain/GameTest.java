483b39048099276a911fc06c35cece87c12afebb
WooJJam <jaemin5548@naver.com>
2023-10-31 16:24:32 +0900

    [Test] Game 테스트

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,4 +1,22 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameTest {
-  
+
+    @Test
+    void 게임_객체를_생성() {
+        Game game = new Game(3,5);
+
+        int carCount = game.getCarsCount();
+        int roundCount = game.getRoundCount();
+
+        assertAll(
+                ()-> assertEquals(3,carCount),
+                () -> assertEquals(5,roundCount)
+        );
+    }
+
 }
