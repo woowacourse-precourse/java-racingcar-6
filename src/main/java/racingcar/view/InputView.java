306c94a11c8f23fc42ff53950a3ffb23f3419784
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-31 15:36:17 +0900

    refactor(view): 검증 부분을 입력과정에서 객체 생성 부분으로 이동

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,14 @@
 package racingcar.view;
 
 import static racingcar.constant.CarConstant.MAX_MOVE_COUNT;
-import static racingcar.constant.CarConstant.MAX_NAME_SIZE;
 import static racingcar.constant.CarConstant.MIN_MOVE_COUNT;
-import static racingcar.constant.CarConstant.MIN_NAME_SIZE;
-import static racingcar.constant.CarConstant.NAME_REGEX;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import racingcar.dto.CarNames;
-import racingcar.dto.MoveCount;
+import racingcar.domain.CarName;
+import racingcar.domain.CarNames;
+import racingcar.domain.MoveCount;
 import racingcar.validator.Validator;
 
 /*
@@ -21,17 +19,11 @@ public class InputView {
 
     public CarNames carNames() {
         String input = Console.readLine();
-        List<String> carNames = Arrays.stream(input.split(",")).toList();
-        validateCarNames(carNames);
+        //가독성 괜찮나?
+        List<CarName> carNames = Arrays.stream(input.split(",")).map((carName) -> new CarName(carName)).toList();
         return CarNames.from(carNames);
     }
 
-    private void validateCarNames(List<String> carNames) {
-        Validator.commandsSizeBiggerThan(SIZE_ZERO, carNames);
-        Validator.commandsLengthInRange(carNames, MIN_NAME_SIZE, MAX_NAME_SIZE);
-        Validator.commandsFollowRegex(carNames, NAME_REGEX);
-        Validator.commandsNotDuplicated(carNames);
-    }
 
     public MoveCount moveCount() {
         String input = Console.readLine();
