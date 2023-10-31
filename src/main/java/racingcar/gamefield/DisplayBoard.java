44f539d064ddffde9777013a56d7cc561c8c5796
yshong1998 <yshong1998@gmail.com>
2023-10-31 15:11:10 +0900

    Feat(DisplayBoard): 최종 우승자를 반환하는 기능 추가, currentCarPosition을 LinkedHashMap으로 변경

--- src/main/java/racingcar/gamefield/DisplayBoard.java
+++ src/main/java/racingcar/gamefield/DisplayBoard.java
@@ -1,36 +1,65 @@
 package racingcar.gamefield;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import racingcar.car.Car;
 import racingcar.car.CarNameDto;
-import racingcar.system.SystemConstant;
+import racingcar.car.CarPositionDto;
 
 public class DisplayBoard {
 
-    private Map<CarNameDto, String> currentCarPosition;
+    private Map<CarNameDto, CarPositionDto> currentCarPosition;
 
-    DisplayBoard(List<String> carNameList){
-        this.currentCarPosition = new HashMap<>(carNameList.size());
-        for (String carName : carNameList) {
-            currentCarPosition.put(new CarNameDto(carName),"");
-        }
+    public DisplayBoard() {
+        this.currentCarPosition = new LinkedHashMap<>();
     }
 
-    public StringBuilder displayCurrentCarPosition(){
+    public StringBuilder displayCurrentCarPosition() {
         StringBuilder displayBoard = new StringBuilder();
         for (CarNameDto carNameDto : currentCarPosition.keySet()) {
-            displayBoard.append(carNameDto.getCarName()).append(" : ").append(currentCarPosition.get(carNameDto)).append("\n");
+            displayBoard.append(carNameDto.getCarName())
+                    .append(" : ")
+                    .append(currentCarPosition.get(carNameDto).getPosition())
+                    .append("\n");
         }
         return displayBoard;
     }
 
-    public void updateCurrentCarPosition(Car car){
+    public void initCarPosition(List<String> carNameList) {
+        for (String carName : carNameList) {
+            currentCarPosition.put(new CarNameDto(carName), new CarPositionDto());
+        }
+    }
+
+    public void updateCurrentCarPosition(Car car) {
         for (CarNameDto carNameDto : currentCarPosition.keySet()) {
-            if(car.isNameEquals(carNameDto)){
-                currentCarPosition.replace(carNameDto, currentCarPosition.get(carNameDto) + SystemConstant.POSITION_EXPRESSION);
+            if (car.isNameEquals(carNameDto)) {
+                currentCarPosition.replace(carNameDto, car.wrapCarPosition());
             }
         }
     }
+
+    public List<String> getWinnerList() {
+        List<String> winnerList = new ArrayList<>();
+        int maxPosition = getMaxPosition();
+        for (CarNameDto carNameDto : currentCarPosition.keySet()) {
+            if(currentCarPosition.get(carNameDto).getPositionLength() == maxPosition){
+                winnerList.add(carNameDto.getCarName());
+            }
+        }
+        return winnerList;
+    }
+
+    private int getMaxPosition(){
+        CarPositionDto maxCarPositionDto = currentCarPosition.values().stream()
+                .max(Comparator.comparingInt(CarPositionDto::getPositionLength))
+                .get();
+        return maxCarPositionDto.getPositionLength();
+    }
+
 }
