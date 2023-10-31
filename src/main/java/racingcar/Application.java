d447546862a1eea31496ae51eb855206f0b2b163
kwon <kckc0608@naver.com>
2023-11-01 00:28:09 +0900

    feat: 자동차 이름 입력 안내 문구 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 public class Application {
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
     }
 }
