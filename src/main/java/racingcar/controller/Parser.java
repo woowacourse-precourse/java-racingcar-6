c730d647bfaeec94b4397ef1e1a6d0fc4ffc37c6
HongYeseul <ghd8119@naver.com>
2023-10-31 10:42:18 +0900

    🚚  rename(parser): 파서 클래스 명 수정 - 소문자로 이뤄진 파서 클래스를 첫 글자 대문자로 수정

--- src/main/java/racingcar/controller/Parser.java
+++ src/main/java/racingcar/controller/Parser.java
@@ -3,7 +3,7 @@ package racingcar.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class parser {
+public class Parser {
     // TODO: input.split()은 parser 역할이지만, 앞 부분은 StringToArrayList()로 분리할 필요가 있어보임
     public ArrayList<String> parsingByComma(String input){
         return new ArrayList<>(Arrays.asList(input.split(",")));
