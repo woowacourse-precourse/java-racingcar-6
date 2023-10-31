7ed9e27a02172bcda5fd51cf6267deb5fb3e37d9
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-10-31 20:02:06 +0900

    feat: 입력을 받는 InputView 기능 구현

---
+++ src/main/java/racingcar/ui/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public String carNameInput() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+
+        return Console.readLine();
+    }
+
+    public String tryInputNumber() {
+        System.out.println(TRY_INPUT_MESSAGE);
+
+        return Console.readLine();
+    }
+
+}
