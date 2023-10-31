b499d3369f04ce900306925ba1868dbeb439c628
lkdcode <dlrlejr1@naver.com>
2023-10-31 15:52:38 +0900

    Refactor : InputValidator
    정젝 팩터리 메서드 추가

--- src/main/java/racingcar/domain/console/input/InputValidator.java
+++ src/main/java/racingcar/domain/console/input/InputValidator.java
@@ -6,6 +6,13 @@ public class InputValidator {
     private final static int MAX_LENGTH = 6;
     private final static int MIN_ATTEMPT_COUNT = 1;
 
+    private InputValidator() {
+    }
+
+    public static InputValidator newInstance() {
+        return new InputValidator();
+    }
+
     public void validateNameLength(final String name) throws IllegalArgumentException {
         if (name.length() >= MAX_LENGTH) {
             RacingCarException.NAME_LENGTH_EXCEPTION.getException();
