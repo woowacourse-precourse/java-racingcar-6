7c45b747dc88b6944ba798807a8c763e90bfee4c
happy-nana <nxxxgnim@gmail.com>
2023-10-31 15:21:50 +0900

    feat: Winners.java 기능 구현 우승자들의 정보를 저장&제공 클래스 우승자들의 목록 저장 List -> (우승자가 없는(null) 동안 게임이 돌아갈 것) winners 목록의 크기 반환(RacingGame.java에서 while문에 적용하기 위해 필요) -> 우승자의 이름을 받아서 목록에 추가 -> list에 있는 우승자들의 이름 문자열로 반환(리스트 -> 스트림 -> 문자열 순으로 반환하는 짧은 코드를 만드는 방법이 있었음) RacingGame.java 코드 작성하면서 변동이 있을 수 있음

--- src/main/java/racingcar/Winners.java
+++ src/main/java/racingcar/Winners.java
@@ -1,4 +1,25 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Winners {
+    private List<String> winners = new ArrayList<>();
+
+    public int countWinners() {
+        if (winners == null) {
+            return 0;
+        } else {
+            return winners.size();
+        }
+    }
+
+    public void addWinner(String winner) {
+        winners.add(winner);
+    }
+
+    public String winnersResult() {
+        return winners.stream().collect(Collectors.joining(", "));
+    }
 }
