368225f9399d51b0bdd16ba11461a206ad3ad38a
haebyun <bhk4392@gmail.com>
2023-11-01 08:06:41 +0900

    feat(RacingOutputView): 우승한 자동차 이름들을 문자열 List로 받아와서 형식에 맞춰 출력하는 기능 구현

--- src/main/java/racingcar/games/racing/view/RacingOutputView.java
+++ src/main/java/racingcar/games/racing/view/RacingOutputView.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class RacingOutputView {
 
+    private static final String DELIMITER = ", ";
+
+    public void printWinners(List<String> winners) {
+        String result = String.join(DELIMITER, winners);
+        System.out.println("최종 우승자 : " + result);
+    }
+
     public void printPlayResult(List<String> results) {
         results.forEach(System.out::println);
         System.out.println();
     }
+
 }
