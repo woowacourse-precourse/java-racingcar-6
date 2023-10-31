7a5f2eb4602538cb60dbbd78dca5b2df7de9390b
youjae <youjae0803@gmail.com>
2023-10-31 17:12:17 +0900

    chore: fix typo

--- src/main/java/racingcar/GameService.java
+++ src/main/java/racingcar/GameService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public class GameService {
     private final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String COUNT_QUERY_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String COUNT_QUERY_MESSAGE = "시도할 횟수는 몇회인가요?";
 
     private IOService ioService;
 
