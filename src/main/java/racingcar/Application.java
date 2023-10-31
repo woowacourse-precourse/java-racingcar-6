59743b26f4c8b46c6179e35b2fd385f386b7fac8
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-31 23:46:00 +0900

    feat: 자동차 이름 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static String[] getCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String answer = Console.readLine();
+        String[] carNames = answer.split(",");
+        return carNames;
+    }
+ 
 }
