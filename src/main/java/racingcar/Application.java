02190036d07cd7eeebf2613e90501eb907ed7a58
bbungjin <leejintwo12@naver.com>
2023-10-31 13:44:35 +0900

    주요 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,4 @@
 package racingcar;
-import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,18 +9,23 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         ArrayList<String> car = new ArrayList<String>(List.of(input.split(",")));
         ArrayList<Integer> goTime = new ArrayList<Integer>();
+        // 자동차의 전진 횟수
         for(int i=0; i< car.size(); i++){
             goTime.add(0);
         }
+        System.out.println("시도할 회수는 몇회인가요?");
         Integer runTime = Integer.parseInt(readLine());
-
+        System.out.println();
+        System.out.println("실행 결과");
         for(int i=0; i<runTime; i++){
             for(int j=0; j< car.size(); j++){
                 goTime.set(i,randGo(goTime.get(i)));
             }
+            printCar(car,goTime);
         }
     }
     public static Integer randGo(Integer go){
@@ -31,7 +35,15 @@ public class Application {
         }
         return go;
     }
-    public static void printCar(){
-
+    public static void printCar(ArrayList<String> car, ArrayList<Integer> goTime){
+        //모든 종류 한번만 출력하면 됨
+        for(int i=0; i<car.size(); i++){
+            System.out.print(car.get(i)+": ");
+            for(int j=0; j<goTime.get(i); j++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
