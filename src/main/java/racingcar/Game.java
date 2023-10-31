38f784be5579cae40f4b84c59d26abbeb466367a
mung96 <mung5079@gmail.com>
2023-10-31 12:11:17 +0900

    refactor:println문을 추가

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Game {
     public String[] createName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
+
         String[] cars = camp.nextstep.edu.missionutils.Console.readLine().split(",");
 
         for (String name : cars) {
