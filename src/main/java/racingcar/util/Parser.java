5de3bcd3493c3845b629a6307cf9449091ad265a
1015s <1015_s@naver.com>
2023-10-31 13:25:27 -0700

    feat(Parser) : Stirng을 적절한 type으로 파싱하는 기능 구현

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -1,2 +1,20 @@
-package racingcar.util;public class Parser {
+package racingcar.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    private static final String COMMA = ",";
+    public static List<String> carNames(String input){
+        return new ArrayList<>(Arrays.asList(input.split(COMMA)));
+    }
+
+    public static int tryCount(String input){
+        try{
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
 }
