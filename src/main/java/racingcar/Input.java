9567084cbbfdd4887e584f813f291e358cdca7a5
yohanii <dygks924@gmail.com>
2023-10-31 20:50:11 +0900

    feat: 시도할 횟수 입력받기

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Input {
@@ -9,6 +11,10 @@ public class Input {
     }
 
     public static int inputTryCount(String input) {
-        return 0;
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 input 형식입니다.");
+        }
     }
 }
