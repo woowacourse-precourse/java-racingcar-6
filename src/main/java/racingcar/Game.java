e5f368f08c857983126b50566fdc6da751c06d6b
Lee9Bin <imagen33@daum.net>
2023-11-01 00:44:09 +0900

    style : 코드 정리

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -9,8 +9,8 @@ public class Game {
     List<Car> cars = new ArrayList<>();
     NumberGenerator numberGenerator = new NumberGenerator();
 
-    public void save(String[] carNames){
-        for (String carName : carNames){
+    public void save(String[] carNames) {
+        for (String carName : carNames) {
             checkNameLength(carName);
             isCharacter(carName);
             Car car = new Car(carName);
@@ -18,28 +18,28 @@ public class Game {
         }
     }
 
-    public void checkNameLength(String carName){
-        if (carName.length() > 5){
+    public void checkNameLength(String carName) {
+        if (carName.length() > 5) {
             throw new IllegalArgumentException("이름이 5자 이하만 가능합니다.");
         }
     }
 
-    public void isCharacter(String carName){
-        for (int i = 0; i<carName.length(); i++){
-            if(Character.isDigit(carName.charAt(i))){
+    public void isCharacter(String carName) {
+        for (int i = 0; i < carName.length(); i++) {
+            if (Character.isDigit(carName.charAt(i))) {
                 throw new IllegalArgumentException("이름에는 숫자가 들어가면 안됩니다.");
             }
         }
     }
 
-    public void giveRandomNumber(){
-        for(Car car : cars){
+    public void giveRandomNumber() {
+        for (Car car : cars) {
             int randomNumber = numberGenerator.makeRandomNumber();
             car.go(randomNumber);
         }
     }
 
-    public void play(){
+    public void play() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         save(readLine().split(","));
 
@@ -47,7 +47,7 @@ public class Game {
         int count = Integer.parseInt(readLine());
 
         Narrator narrator = new Narrator();
-        for(int i = 0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             giveRandomNumber();
             narrator.showEachRound(cars);
         }
--- src/main/java/racingcar/Narrator.java
+++ src/main/java/racingcar/Narrator.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Narrator {
 
-    public void showEachRound(List<Car> cars){
-        for (Car car : cars){
-            System.out.println(car.name+" : "+String.join("", car.distance));
+    public void showEachRound(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.name + " : " + String.join("", car.distance));
         }
         System.out.println();
     }
