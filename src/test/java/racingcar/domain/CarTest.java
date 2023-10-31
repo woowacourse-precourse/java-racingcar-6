54b46054b2c2f7d93e0f46ca1d546f7865c0c686
Mr.Kim <joajy98@gmail.com>
2023-10-31 16:09:28 +0900

    refactor: 다른 테스트 코드를 위한 필드 중복 최소화

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,7 +1,11 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,16 +13,25 @@ public class CarTest {
 
     private static final int INITIAL_POSITION = 0;
 
+    List<Car> carList = new ArrayList<>();
+
+    @AfterEach
+    void clearCarList(){
+        carList.clear();
+    }
+
     @Test
     @DisplayName("자동차의 처음 위치가 0임을 검증")
     void initialCarPositionIsZeroTest() {
         //given
-        Car car = new Car("자동차");
+        carList.add(new Car("pobi"));
+        carList.add(new Car("crong"));
 
         //when
-        int position = car.getCarPosition();
 
         //then
-        assertThat(position).isEqualTo(INITIAL_POSITION);
+        for(Car car : carList){
+            assertThat(car.getCarPosition()).isEqualTo(INITIAL_POSITION);
+        }
     }
 }
