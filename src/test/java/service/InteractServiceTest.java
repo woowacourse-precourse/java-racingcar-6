3bc1266b6c17bdf3deb8aa90658a756d33757f72
dong2ast <kdh01132006@naver.com>
2023-10-31 11:33:50 +0900

    ✅ test: 이름에 공백이 들어오는 경우 테스트 추가

--- src/test/java/service/InteractServiceTest.java
+++ src/test/java/service/InteractServiceTest.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
@@ -18,6 +19,23 @@ public class InteractServiceTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,pobi", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @DisplayName("이름에 공백이 들어올 경우")
+    @Test
+    public void Name_Blank() {
+        // when & given
+        List<String> inputs = List.of(
+                " pobi", " ", "\n"
+        );
+
+        // then
+        for (String input : inputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input, "1"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
     }
 
     @Override
