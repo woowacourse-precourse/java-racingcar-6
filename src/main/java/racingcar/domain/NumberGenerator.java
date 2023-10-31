205b06d3091b649ed05fdbc46412012d57e99a2b
Zho <chlwlgh1011@naver.com>
2023-11-01 03:10:28 +0900

    feat(racingcar): 컴퓨터 수 생성 구현

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,9 +1,17 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
-    public List<Integer> createRandomNumber() {
-        return null;
+    public static List<Integer> createRandomNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            computerNumber.add(randomNumber);
+        }
+        return computerNumber;
     }
 }
