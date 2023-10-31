e928f820a59a60d0a414aef90d784d8e462d8083
alenjb <alenjb@naver.com>
2023-11-01 06:18:55 +0900

    :sparkles:feat(racingcar): Game 클래스에 우승자가 여러명인지 확인하는 기능 구현
    Game.java에 우승자가 여러명인지 확인하는 메서드 areWinners를 추가함.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -5,17 +5,11 @@ import java.util.List;
 
 public class Game {
     Inputs inputs;
+    boolean hasWinners; // 우승자가 여러명인지 판별하는 변수 : true이면 우승자가 여러명
 
-    List<Car> selectWinner(List<Car> cars) {
-        List<Car> winners = new ArrayList<>();
-        int maxGoNumber = 0;
-        for (Car car : cars) {
-            // 경주 자동차들의 최대 전진 회수를 체크
-            maxGoNumber = checkMaxGoNumber(car, maxGoNumber, winners);
-        }
-        return winners;
-    }
 
+
+    // 최대 전진 회수를 체크하는 함수
     private int checkMaxGoNumber(Car car, int maxGoNumber, List<Car> winners) {
         if (car.totalGoNumber > maxGoNumber) { // 현재 자동차의 전진 회수 > 기존 최고 전진 회수
             maxGoNumber = car.totalGoNumber; // 기존 최고 전진 회수 업데이트
@@ -27,17 +21,36 @@ public class Game {
         return maxGoNumber;
     }
 
-    public void printResult(List<Car> cars) {
+    //우승자가 여러명인지 확인하는 메서드
+    public boolean areWinners(List<Car> winners) {
+        return winners.size() > 1;
+    }
+
+    List<Car> selectWinner(List<Car> cars) {
+        List<Car> winners = new ArrayList<>();
+        int maxGoNumber = 0;
+        for (Car car : cars) {
+            // 경주 자동차들의 최대 전진 회수를 체크
+            maxGoNumber = checkMaxGoNumber(car, maxGoNumber, winners);
+        }
+        return winners;
+    }
+
+    public void printResult(List<Car> cars, List<Car> winners) {
+        System.out.println("\n실행 결과");
         for (int i = 0; i < inputs.raceNumbers; i++) {
             printResultByRound(cars, i);
+            System.out.println();
         }
+        if (hasWinners) {
+            printWinners(winners);
+        } else printWinner(winners);
     }
 
     private void printResultByRound(List<Car> cars, int turn) {
         for (Car car : cars) {
-            System.out.printf("%s : ", car.carName);
-            System.out.print(car.result[turn]);
+            System.out.printf("%s : %s\n", car.carName, arrayToStringByTurn(car.result, turn));
         }
-    }
 
+    }
 }
