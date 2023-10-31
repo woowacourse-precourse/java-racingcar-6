c7b52d90fd38f64653f8754cff56ee46d054e9e6
nayeon Lee <skdus4190@gmail.com>
2023-11-01 02:15:41 +0900

    Test: 테스트 코드 추가(중복된 이름 불가)
    ParticipantTest.java에 입력된 이름이 중복일 경우 예외 처리 하는 테스트 코드 작성

--- src/test/java/model/domain/ParticipantTest.java
+++ src/test/java/model/domain/ParticipantTest.java
@@ -1,5 +1,6 @@
 package model.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,21 @@ class ParticipantTest {
     }
 
     @Test
-    void 각_이름의_앞뒤_공백_제거(){
+    void 각_이름의_앞뒤_공백_제거() {
         String carNames = "lee, na ,  yeon";
         Participant test = new Participant(carNames);
 
         assertEquals("lee", test.getParticipant().get(0).getNameValue());
         assertEquals("na", test.getParticipant().get(1).getNameValue());
         assertEquals("yeon", test.getParticipant().get(2).getNameValue());
+    }
+
+    @Test
+    void 자동차_이름은_중복_불가() {
+        String carNames = "lee,na,na";
+        assertThatThrownBy(() -> {
+            new Participant(carNames);
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
