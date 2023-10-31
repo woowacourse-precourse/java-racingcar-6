cc7ddaee6a466bb07f5d650ed4555fef9d4baee3
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-31 11:07:15 +0900

    [FEAT] Main함수 기능 구현
    .

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Game.racingGame();
     }
 }
