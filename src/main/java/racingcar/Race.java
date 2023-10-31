68ca78f2ad887be5e0046f92651499db5d180161
dyeongkim <dyeongkim98@gmail.com>
2023-10-31 15:05:35 +0900

    feat(Race): Add isGo
    isGo 메서드 추가
    - 메서드 실행 시 전진할지 여부를 반환
    - 랜덤 숫자를 생성해서 4이상이면 true 반환 아니면 false

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,13 +1,24 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Race {
     private int inputMove;
+
+    private int randomNum;
 
     public void inputMove(){
         System.out.println("시도할 회수는 몇회인가요?");
         inputMove = Integer.parseInt(Console.readLine());
         System.out.println(inputMove);
+    }
+
+    private boolean isGo(){
+        randomNum = Randoms.pickNumberInRange(0,9);
+        if (randomNum >= 4){
+            return true;
+        }
+        return false;
     }
 }
