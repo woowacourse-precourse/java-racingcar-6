181c54e9a0bbb41abac7408cd90786c9bab0ea25
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-10-31 20:49:46 +0900

    feat: Car 클래스 테스트 작성 1. Car의 이름이 5글자를 초과할 시 IllegalArgumentException을 반환하는지 검증 2. 랜덤 숫자 생성을 통해 4 이상의 숫자가 나오면 한 칸 이동하는지를 검증 3. 현재 몇 칸 이동했는지 return하는 매서드를 검증 4. 객체의 이름을 return 하는 매서드를 검증

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class CarTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+    @Test
+    void carNameShouldBeAssigned(){
+        Car car = new Car("tmp");
+        assertThat(car.getName()).isEqualTo("tmp");
+    }
+    @Test
+    void carNameShouldBeLessThanFiveCharacters(){
+        assertThatThrownBy(()-> new Car("tester"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void carInitialPositionShouldBeZero(){
+        Car car = new Car("test");
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
