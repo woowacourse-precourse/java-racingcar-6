c3bcd81d84478ac40947c93d5778469fbdd67f09
koomchang <leahpar0401@gmail.com>
2023-11-01 01:19:24 +0900

    refactor: 의미 있는 숫자들 상수 처리

--- src/main/java/racingcar/model/Location.java
+++ src/main/java/racingcar/model/Location.java
@@ -6,6 +6,10 @@ import racingcar.validator.LocationValidator;
 
 public record Location(int location) {
 
+    private static final int MOVE_CONDITION = 4;
+    private static final int START_LOCATION = 0;
+    private static final int MOVE_VALUE = 1;
+
     private static final BasicValidator<Integer> locationValidator = new LocationValidator();
 
     public Location {
@@ -13,18 +17,18 @@ public record Location(int location) {
     }
 
     public static Location init() {
-        return new Location(0);
+        return new Location(START_LOCATION);
     }
 
     public Location move() {
-        if (moveCondition()) {
-            return new Location(location + 1);
+        if (isMovable()) {
+            return new Location(location + MOVE_VALUE);
         }
         return new Location(location);
     }
 
-    private boolean moveCondition() {
+    private boolean isMovable() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
-        return randomNumber >= 4;
+        return randomNumber >= MOVE_CONDITION;
     }
 }
