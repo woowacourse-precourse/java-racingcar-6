f2f77d6efab6f81b2069a691b9210ecd7e60101c
BaeYoungSuk <bysuk01@gmail.com>
2023-10-31 22:09:02 +0900

    feat(getCarNamesFromInput): 입력받은 자동차 이름을 쉼표 기준으로 구분
    입력받은 자동차 이름을 쉼표 기준으로 구분하는 기능을 추가하였습니다.
    - getCarNamesFromInput 함수 추가
    - 입력받은_자동차_이름_구분_테스트 추가
    - README 구현 기능 및 테스트 체크 표시

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> cars = getCarNamesFromInput(input);
+    }
+
+    public static List<String> getCarNamesFromInput(String input) {
+        return List.of(input.split(","));
     }
 }
