d06d3f7aff0bdab2a76f764fd22a62cd1bb45935
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-10-31 21:29:20 +0900

    feat : start the game
    게임을 시작하는 함수

---
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.Car;
+
+public class RacingGame {
+    private List<Car> carList;
+    private int attemptCount;
+
+    RacingGame() {
+
+    }
+
+    public void start() {
+        
+    }
+}
