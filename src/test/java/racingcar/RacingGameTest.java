a946b914e7e17283f36350ae5511bec079714a08
ZZ3n <ndsrhkd@naver.com>
2023-10-31 17:28:55 +0900

    test(game): 게임 우승자 중복 케이스 추가
    게임에서 중복 우승자인 경우의 테스트 케이스가 없어 추가하였습니다.

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,13 +1,17 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
-class RacingGameTest {
+class RacingGameTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     void instance_생성_테스트() {
@@ -60,4 +64,19 @@ class RacingGameTest {
         Console.close();
     }
 
+    @Test
+    void 우승자_중복_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("ZZ3n,park", "1");
+                    assertThat(output()).contains("ZZ3n : -", "park : -", "최종 우승자 : ZZ3n, park");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }
