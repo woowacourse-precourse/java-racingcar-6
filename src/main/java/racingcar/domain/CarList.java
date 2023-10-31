5c281cda53f266b15ea1e788fde44f9781632118
SongJwans <yhh5916@naver.com>
2023-10-31 19:00:33 +0900

    feat: car.stopOrMove()를 한번에 처리하는 moveAllCars() 추가

--- src/main/java/racingcar/domain/CarList.java
+++ src/main/java/racingcar/domain/CarList.java
@@ -5,26 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarList {
-    private static final String SPLIT_CONDITION = ",";
-
     private final List<Car> carList = new ArrayList<>();
 
     public List<Car> getCarList() {
         return carList;
     }
 
-    public String[] splitCarNames(String carNames) {
-        return splitName(carNames);
-    }
-
-    private String[] splitName(String carNames) {
-        return carNames.split(SPLIT_CONDITION);
-    }
-
     public void addCars(String[] carNameArray) {
         Arrays.stream(carNameArray)
                 .map(Car::new)
                 .forEach(carList::add);
+    }
+    public void moveAllCars() {
+        for (Car car : carList) {
+            car.stopOrMove();
+        }
     }
 
     public Winners getWinners() {
