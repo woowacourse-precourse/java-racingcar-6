15401e6c5ae7b235367bad98baa9ea25b641e561
jang010505 <jang010505@inu.ac.kr>
2023-10-31 13:26:16 +0900

    feat: inputCarName 함수 코드 작성

--- src/main/java/racingcar/constant/MessageConstant.java
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -7,7 +7,9 @@ public enum MessageConstant {
     CAR_NAME_LENGTH_ERROR("자동차의 이름은 1자 이상 5자 이하야야합니다."),
     CAR_NAME_CONTAINS_SPACE_ERROR("자동차의 이름은 공백이 포함되지 말아야합니다."),
 
-    TOTAL_ROUND_INPUT_ERROR("1 이상의 양의 정수를 입력해야합니다.");
+    TOTAL_ROUND_INPUT_ERROR("1 이상의 양의 정수를 입력해야합니다."),
+
+    CAR_NAME_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
 
     private final String message;
 
--- src/main/java/racingcar/input/Input.java
+++ src/main/java/racingcar/input/Input.java
@@ -1,10 +1,25 @@
 package racingcar.input;
 
+import static racingcar.constant.MessageConstant.CAR_NAME_INPUT_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.Validator;
 import java.util.List;
 
-public class Input {
-    public static List<String> inputCarName() {
 
-        return null;
+public class Input {
+    private static String inputString(String input) {
+        System.out.print(input);
+        return Console.readLine();
+    }
+
+    public static List<String> inputCarName() {
+        String input = inputString(CAR_NAME_INPUT_MESSAGE.getMessage());
+
+        List<String> splitInput = Validator.validateCarNameInput(input);
+
+        splitInput.forEach(Validator::validateCheckCarName);
+
+        return splitInput;
     }
 }
