e03d1ed5efa98843e5965bfb5fae63784297a50b
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-31 16:55:08 +0900

    test: RacingBoard 테스트 추가
    - RacingBoard의 기능추가로 추가 테스트 케이스 작성하였음

--- src/test/java/racingcar/domain/RacingBoardTest.java
+++ src/test/java/racingcar/domain/RacingBoardTest.java
@@ -27,4 +27,23 @@ class RacingBoardTest {
         //then
         Assertions.assertThat(result).isEqualTo(compareStr);
     }
+
+    @Test
+    void viewExecution() {
+        //given
+        String resultStr;
+        ArrayList<String> compapreArrayList = new ArrayList<>(Arrays.asList("car1", "car2", "car4"));
+        Car car1 = new Car("car1", 3);
+        Car car2 = new Car("car2", 3);
+        Car car3 = new Car("car3", 2);
+        Car car4 = new Car("car4", 3);
+        racingBoard.join(car1);
+        racingBoard.join(car2);
+        racingBoard.join(car3);
+        racingBoard.join(car4);
+        //when
+        resultStr = racingBoard.viewExecution();
+        //then
+        System.out.println(resultStr);
+    }
 }
