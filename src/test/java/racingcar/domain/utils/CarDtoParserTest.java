df6f16a46a7d713f527ac2af153f86e72af54bed
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-31 15:56:22 +0900

    test : CarDtoParserTest > 테스트 로직 수정

--- src/test/java/racingcar/domain/utils/CarDtoParserTest.java
+++ src/test/java/racingcar/domain/utils/CarDtoParserTest.java
@@ -1,5 +1,8 @@
 package racingcar.domain.utils;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.car.CarDtoParser;
@@ -11,29 +14,27 @@ class CarDtoParserTest {
     void splitNames() {
         // GIVEN
         String carNames = "pobipobi,qwe,ert";
+        String[] cars = {"pobipobi", "qwe", "ert"};
 
         // WHEN
         String[] splittedNames = CarDtoParser.trimAndSplit(carNames);
 
         // THEN
-        splittedNames[0] = "pobipobi";
-        splittedNames[1] = "qwe";
-        splittedNames[2] = "ert";
+        assertTrue(Arrays.equals(cars, splittedNames));
     }
 
     @Test
     @DisplayName("주어진 자동차 이름 내에 공백이 있다면 공백을 제거하고 ,로 구분합니다.")
-    void trimAndSplitNames(){
+    void trimAndSplitNames() {
         // GIVEN
         String carNames = "pobipobi         ,qwe,       ert";
+        String[] cars = {"pobipobi", "qwe", "ert"};
 
         // WHEN
         String[] splittedNames = CarDtoParser.trimAndSplit(carNames);
 
         // THEN
-        splittedNames[0] = "pobipobi";
-        splittedNames[1] = "qwe";
-        splittedNames[2] = "ert";
+        assertTrue(Arrays.equals(cars, splittedNames));
     }
 
 }
