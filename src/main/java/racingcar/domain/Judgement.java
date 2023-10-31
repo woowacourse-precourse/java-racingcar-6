a226a9ff705459e7440d51da0d240038e772e41e
grax <grax@192.168.0.103>
2023-10-31 16:52:50 +0900

    feat: "create goStop() method"
    "추후 보완 필요"

---
+++ src/main/java/racingcar/domain/Judgement.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Judgement {
+	private static final int STANDARD_NUMBER = 4;
+	
+	public void goStop(int randomNumber) {
+		if (randomNumber >= STANDARD_NUMBER) {
+			System.out.println("-");
+			// TODO: 메서드 사용해서 구현하기 addDash()
+		}
+	}
+}
