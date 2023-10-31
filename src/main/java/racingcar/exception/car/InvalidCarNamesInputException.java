3c7cbee5b6dc6183dda3d9efc07cd139fd5fec27
mainfn <_@mainfn.dev>
2023-10-31 13:26:59 +0900

    refactor: `final` 키워드 누락된 `class`에 추가

--- src/main/java/racingcar/exception/car/InvalidCarNamesInputException.java
+++ src/main/java/racingcar/exception/car/InvalidCarNamesInputException.java
@@ -1,6 +1,6 @@
 package racingcar.exception.car;
 
-public class InvalidCarNamesInputException extends IllegalArgumentException {
+public final class InvalidCarNamesInputException extends IllegalArgumentException {
     public static final String INVALID_CAR_NAMES_INPUT = "유효하지 않은 양식입니다.\n차 이름 양식은 ,로 구분해주세요.";
 
     public InvalidCarNamesInputException() {
--- src/main/java/racingcar/exception/round/InvalidMaxRoundInputException.java
+++ src/main/java/racingcar/exception/round/InvalidMaxRoundInputException.java
@@ -1,6 +1,6 @@
 package racingcar.exception.round;
 
-public class InvalidMaxRoundInputException extends IllegalArgumentException {
+public final class InvalidMaxRoundInputException extends IllegalArgumentException {
     public static final String INVALID_MAX_ROUND_INPUT_EXCEPTION_MESSAGE = "유효하지 않은 양식입니다.\n반드시 숫자를 입력해주세요.";
 
     public InvalidMaxRoundInputException() {
