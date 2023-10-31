dcd40c3922b32f164655f78ae1c399d265427a45
Yujin <tkfkd7038@naver.com>
2023-11-01 01:49:49 +0900

    feat(racingcar): 동점인 우승자가 있는지 구하는 기능

--- src/main/java/racingcar/WinnerManager.java
+++ src/main/java/racingcar/WinnerManager.java
@@ -24,7 +24,11 @@ public class WinnerManager {
     }
 
     public void findValuesEqualToMax() { // 동점인 우승자가 있는지 구하기
-
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].forwardCount == car[maxIndex].forwardCount) {
+                maxIndexList.add(i);
+            }
+        }
     }
 
     public ArrayList<String> getWinnersByForwardCount() { // 우승자 명단 List를 반환
