ad270010118ab4d074ff6006a3f28e844aba7d15
ldw <leehan02@hanyang.ac.kr>
2023-11-01 03:45:08 +0900

    feat(racingcar): 자동차 경주를 진행하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
-
-    // 객체 생성 (의존성 주입?)
     private static final Input input = new Input();
     private static final Referee referee = new Referee();
     private static final String RESULT_MESSAGE =  "실행 결과";
@@ -17,29 +16,25 @@ public class Application {
 
         // 사용자 입력 - 입력 객체는 입력을 받는 책임만을 가질 뿐 검증 책임은 다른 곳에서 가진다.
         String[] carsName = input.askCarsName();
-        int tryNum = input.askTryNum();
+        String tryNumString = input.askTryNum();
+        int tryNum = Converter.convertStringToInt(tryNumString);
+        InputValidator.validateTryNum(tryNum);
 
         // 자동차 생성
         for (String name : carsName) {
             cars.add(new Car(name));
         }
 
-        // round 시작
-        System.out.println(RESULT_MESSAGE);
-        for (int i = 0; i < tryNum; i++) {
-            runRound(cars);
-            System.out.println();
-        }
+        startRace(cars, tryNum);
 
         // 각 자동차의 총 전진 횟수 담기
         List<Integer> totalMoveList = new ArrayList<>();
         cars.forEach(car -> {
-            totalMoveList.add(car.getTotalMove());
+            totalMoveList.add(car.getTotalMoveLen());
         });
         int max = Calculator.calculateMaxMove(totalMoveList);
         List<String> winners = referee.judgeWinners(cars, max);
 
-        // 우승자 출력
         printWinners(winners);
 
         // 정상 종료 시 혹은 에러 발생에 의한 종료 시 close (필수적인 부분인지?)
@@ -59,4 +54,13 @@ public class Application {
         String winnerList = String.join(", ", winners);
         System.out.println(winnerList);
     }
+
+    public static void startRace(List<Car> cars, int tryNum) {
+        System.out.println(RESULT_MESSAGE);
+        for (int i = 0; i < tryNum; i++) {
+            runRound(cars);
+            System.out.println();
+        }
+    }
+
 }
