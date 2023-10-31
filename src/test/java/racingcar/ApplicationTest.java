13ccea3fb9bcc3165fe9e5a235a9b6b94e3338bd
jhsong76 <20190976@sungshin.ac.kr>
2023-10-31 18:17:21 +0900

    test: 테스트 코드 삭제

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,26 +1,16 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import racingcar.InputView.InputView;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
-    private InputView inputView;
-    private Validator validator;
-
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
@@ -39,30 +29,6 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
-    @BeforeEach
-    public void setup() {
-        inputView = new InputView();
-        validator = new Validator();
-    }
-
-    @Test
-    public void 게임시도_회수_예외_처리() {
-        String input = "3";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-
-        int result = inputView.getRoundNumber();
-        assertEquals(3, result);
-;    }
-
-    @Test
-    public void 게임시도_회수_공백_예외_처리() {
-        String input = " ";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-
-        assertThrows(IllegalArgumentException.class, () -> inputView.getRoundNumber());
-    }
-
 
     @Override
     public void runMain() {
