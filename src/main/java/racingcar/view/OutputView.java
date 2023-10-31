ee5cceaaf543cd59a4b04ce5a3373826a58d23b7
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 05:17:43 +0900

    feat(OutputView): 사용자 출력 뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,34 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+
+import static racingcar.constant.RaceMessage.EXECUTION_RESULT;
+import static racingcar.constant.RaceMessage.FINAL_WINNER;
+
 public class OutputView {
+    public static void printExecution() {
+        System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printCarStatus(List<Car> cars) {
+        for (Car car : cars) {
+            printCarStatusLine(car);
+        }
+        System.out.println();
+    }
+
+    private void printCarStatusLine(Car car) {
+        StringBuilder status = new StringBuilder(car.getName() + " : ");
+        for (int i = 0; i < car.getPosition(); i++) {
+            status.append("-");
+        }
+        System.out.println(status);
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.print(FINAL_WINNER);
+        System.out.println(String.join(", ", winners));
+    }
 }
