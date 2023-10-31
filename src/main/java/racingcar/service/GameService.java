b32cc78c274ec5bd29355e70e1620423b3f4ed98
devhyeon0 <devhyeon0@gmail.com>
2023-10-31 22:20:46 +0900

    refactor: 클래스 변경(Number -> GameService)

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,2 +1,23 @@
-package racingcar.service;public class GameService {
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GameService {
+
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
+    public List<Integer> generateRandomNumber(int carNumbers) {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        for (int i = 0; i < carNumbers; i++) {
+            randomNumbers.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        }
+
+        return randomNumbers;
+    }
 }
