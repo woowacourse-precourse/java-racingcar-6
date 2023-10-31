dc3d605a0892a8cd01e55ca235660c2b7a23d794
Minsu Choi <cms102510@naver.com>
2023-10-31 17:03:26 +0900

    refactor: 필요 하지 않은 메서드 삭제

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -11,15 +11,12 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
-        validateNameLength(name);
-        this.name = name;
-    }
-
-    private void validateNameLength(String name) {
         if (name.length() > MAX_NAMELENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
+        this.name = name;
     }
+
 
     public void tryMove() {
         if (Randoms.pickNumberInRange(1, 9) >= MOVE_THRESHOLD) {
