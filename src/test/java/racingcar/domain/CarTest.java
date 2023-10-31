ae62fc0d286cf20602842fe842703d94f7952960
hyunjin <guswlsdl0121@gmail.com>
2023-11-01 02:56:33 +0900

    test: CarTest와 RacingCarsTest를 Mock을 쓰지 않도록 구현
    이동 전략의 boolean값을 람다로 설정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,84 +4,54 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import racingcar.common.strategy.RandomMoveStrategy;
 import racingcar.common.type.Names;
 
 public class CarTest {
     private List<Car> cars;
-    private RandomMoveStrategy randomMoveStrategy;
 
     @BeforeEach
     public void setUp() {
+        // given: Car 객체들을 초기화
         Names names = Names.of(new String[]{"hyunjin", "pobi", "lefthand"});
         cars = Car.createCarList(names);
-        randomMoveStrategy = Mockito.mock(RandomMoveStrategy.class);
     }
 
     @Test
     public void 자동차_목록_생성_확인() {
+        // then
         Assertions.assertEquals(3, cars.size());
     }
 
     @Test
     public void 이름_조회() {
-        //given
+        // when
         String name = cars.get(0).getName();
 
-        //when & then
-        Assertions.assertEquals(name, "hyunjin");
+        // then
+        Assertions.assertEquals("hyunjin", name);
     }
 
     @Test
     public void 이동_테스트() {
-        //given
-        Mockito.when(randomMoveStrategy.isMovable()).thenReturn(true);
-
-        //when
+        // given: 첫 번째 자동차를 선택
         Car car = cars.get(0);
-        car.move(randomMoveStrategy);
 
-        //then
+        // when: 자동차를 움직임
+        car.move(() -> true);
+
+        // then: 이동 거리가 1인지 확인
         Assertions.assertEquals(1, car.getDistance());
     }
 
     @Test
     public void 이동_불가능_테스트() {
-        //given
-        Mockito.when(randomMoveStrategy.isMovable()).thenReturn(false);
-
-        //when
+        // given, 첫 번째 자동차를 선택
         Car car = cars.get(0);
-        car.move(randomMoveStrategy);
 
-        //then
+        // when, 자동차를 움직이지 않음
+        car.move(() -> false);
+
+        // then, 이동 거리가 0인지 확인
         Assertions.assertEquals(0, car.getDistance());
-    }
-
-    @Test
-    public void 우승자와_점수가_같은_경우() {
-        //given
-        Car car = cars.get(0);
-        Mockito.when(randomMoveStrategy.isMovable()).thenReturn(true);
-
-        //when
-        car.move(randomMoveStrategy);
-
-        //then
-        Assertions.assertTrue(car.isWinner(1));
-    }
-
-    @Test
-    public void 우승자와_점수보다_점수가_작은_경우() {
-        //given
-        Car car = cars.get(0);
-        Mockito.when(randomMoveStrategy.isMovable()).thenReturn(false);
-
-        //when
-        car.move(randomMoveStrategy);
-
-        //then
-        Assertions.assertFalse(car.isWinner(1));
     }
 }
--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -4,31 +4,27 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import racingcar.common.strategy.MoveStrategy;
 import racingcar.common.type.Names;
 
 public class RacingCarsTest {
     private List<Car> carList;
-    private MoveStrategy moveStrategy;
 
     @BeforeEach
     public void setUp() {
+        // given: Car 객체들을 초기화
         Names names = Names.of(new String[]{"hyunjin", "pobi", "lefthand"});
         carList = Car.createCarList(names);
-        moveStrategy = Mockito.mock(MoveStrategy.class);
     }
 
     @Test
     public void 모든_차들에_대한_이동진행() {
-        //given, 모든 차가 이동할 수 있는 상태로 설정
-        Mockito.when(moveStrategy.isMovable()).thenReturn(true);
+        // given, 모든 차가 이동할 수 없는 상태로 설정,  이동 전략을 true를 반환하는 람다로 설정
+        RacingCars racingCars = RacingCars.of(carList, () -> true);
 
-        //when
-        RacingCars racingCars = RacingCars.of(carList, moveStrategy);
+        // when: 모든 차량을 이동
         racingCars.move();
 
-        //then,  모든 차가 1칸 이동했는지 검증
+        // then: 모든 차가 1칸 이동했는지 검증
         for (Car car : racingCars.cars()) {
             Assertions.assertEquals(1, car.getDistance());
         }
@@ -36,14 +32,13 @@ public class RacingCarsTest {
 
     @Test
     public void 모든_차들의_이동불가() {
-        //given, 모든 차가 이동할 수 없는 상태로 설정
-        Mockito.when(moveStrategy.isMovable()).thenReturn(false);
+        // given, 모든 차가 이동할 수 없는 상태로 설정,  이동 전략을 false를 반환하는 람다로 설정
+        RacingCars racingCars = RacingCars.of(carList, () -> false);
 
-        //when
-        RacingCars racingCars = RacingCars.of(carList, moveStrategy);
+        // when, 모든 차량을 이동
         racingCars.move();
 
-        //then,  모든 차가 이동하지 않았는지 검증
+        // then, 모든 차가 이동하지 않았는지 검증
         for (Car car : racingCars.cars()) {
             Assertions.assertEquals(0, car.getDistance());
         }
