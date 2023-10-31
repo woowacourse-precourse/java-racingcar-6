f2391ed175615ef0a53aa55766fa9858f7a3ed8f
jh <pv104@naver.com>
2023-10-31 15:53:02 +0900

    refactor : User 클래스 삭제, README. md의 기능 목록, 클래스 목록 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -19,13 +19,23 @@ import org.junit.jupiter.api.Test;
    [ ] (7) 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다. -> returnWinner() */
 
 public class Car {
+    private String carName;
     public boolean carMoveOrStop() {
         return false;
     }
-    public String giveNameOfCar() {
-        return "";
+
+    public void giveNameOfCar(String carName) {
+        if(isLengthFive(carName)) {
+            this.carName = carName;
+        }
     }
 /*
+[ ] (2) 각 자동차에 이름을 부여할 수 있다. -> giveNameOfCar()
+    각자의 자동차에 이름을 부여해야함 즉,해당 인스턴스마다 새로운 이름을 부여
+    하지만 자동차의 이름은 ","을 구분자로 하여 한 줄에 쭉 입력되기 때문에
+    해당 자동차의 이름들을 List<String> 형태로 split한다음 각각의 Car에 입력해야 함.
+    -> 그렇다면 해당 메서드는 List<String>의 값들을 읽어서 해당 값을
+    Car 내의 carName이라는 변수에 저장해야 한다. 즉, setter의 역할.
 [ ] (3) 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다. ->
 printMovingCarWithCarName()
 [ ] (4) 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
@@ -35,14 +45,17 @@ printMovingCarWithCarName()
     public String printMovingCarWithCarName() {
         return "";
     }
+
     public boolean isLengthFive(String carName) {
         return carName.length() <= 5;
     }
+
     public boolean isMoveAble() {
         return generateRandomNumber() > 3;
     }
+
     public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(0, 9);
     }
 
 }
