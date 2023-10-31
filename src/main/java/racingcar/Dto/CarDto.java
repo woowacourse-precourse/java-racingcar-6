a17cbf1dfbc1c810fcc516ff975e5220136d2f94
Heejun-0219 <heejun0219@gmail.com>
2023-10-31 22:31:52 +0900

    feat(index): 객체 인덱스 추가

--- src/main/java/racingcar/Dto/CarDto.java
+++ src/main/java/racingcar/Dto/CarDto.java
@@ -2,19 +2,43 @@ package racingcar.Dto;
 
 public class CarDto {
     private String carName;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
     private int position;
+    private boolean won = false;
+
+    public boolean isWon() {
+        return won;
+    }
+
+    public void setWon(boolean won) {
+        this.won = won;
+    }
 
     public CarDto() {
         throw new IllegalArgumentException();
     }
 
-    public CarDto(String carName, int position) {
+    public CarDto(String carName, int position, int index) {
         throw new IllegalArgumentException();
     }
 
     public CarDto(String carName) {
+        throw new IllegalArgumentException();
+    }
+
+    public CarDto(String carName, int index) {
         this.carName = carName;
         this.position = 0;
+        this.index = index;
     }
 
     public void setPosition(int position) {
--- src/test/java/racingcar/Dto/CarDtoTest.java
+++ src/test/java/racingcar/Dto/CarDtoTest.java
@@ -8,31 +8,36 @@ import org.junit.jupiter.api.Test;
 public class CarDtoTest {
     @Test
     void newRacingCar_정상_테스트() {
-        CarDto carDto = new CarDto("A");
+        CarDto carDto = new CarDto("A", 1);
         assertThat(carDto.getCarName()).isEqualTo("A");
     }
 
     @Test
     void newRacingCar_에러_테스트() {
-        assertThatThrownBy(() -> new CarDto("A", 1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new CarDto("A")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void newRacingCar_에러_테스트2() {
+        assertThatThrownBy(() -> new CarDto("A", 1, 1)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void setPosition_정상_테스트() {
-        CarDto carDto = new CarDto("A");
+        CarDto carDto = new CarDto("A", 1);
         carDto.setPosition(1);
         assertThat(carDto.getPosition()).isEqualTo(1);
     }
 
     @Test
     void setPosition_에러_테스트() {
-        CarDto carDto = new CarDto("A");
+        CarDto carDto = new CarDto("A", 1);
         assertThatThrownBy(() -> carDto.setPosition(-1)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void plusPosition_정상_테스트() {
-        CarDto carDto = new CarDto("A");
+        CarDto carDto = new CarDto("A", 1);
         carDto.plusPosition();
         assertThat(carDto.getPosition()).isEqualTo(1);
     }
