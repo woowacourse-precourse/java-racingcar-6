7cbdea5fc2c41ac6cb1508b79afafe31d958c03f
chan-won <clw8679@naver.com>
2023-10-31 11:23:20 +0900

    test: 자동차 게임 시도 횟수 입력 테스트 추가

--- src/test/java/racingcar/io/InputTest.java
+++ src/test/java/racingcar/io/InputTest.java
@@ -46,4 +46,27 @@ class InputTest {
                 .hasMessageContaining("자동차 이름은 1이상 5이하만 가능합니다.");
     }
 
+    @Test
+    void 시도_횟수가_숫자가_아닌_경우_에러가_발생한다() {
+        String input = "abc";
+
+        Assertions.assertThatThrownBy(() -> InputValidate.gameRound(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("정수를 입력해 주세요");
+    }
+
+    @Test
+    void 시도_횟수가_1에서_20_사이의_숫자가_아닌_경우_에러가_발생한다() {
+        String zero = "0";
+
+        Assertions.assertThatThrownBy(() -> InputValidate.gameRound(zero))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("라운드는 1이상 20이하만 가능합니다");
+
+        String overRange = "21";
+
+        Assertions.assertThatThrownBy(() -> InputValidate.gameRound(overRange))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("라운드는 1이상 20이하만 가능합니다");
+    }
 }
