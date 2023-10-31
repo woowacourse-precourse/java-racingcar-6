3d90381850e4e788ca8be3d01705d835912fbd2e
Junobee25 <wnsdhqo@naver.com>
2023-10-31 19:55:29 +0900

    refactor(domain): createRandomNumberList 메서드 반환 값 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,17 +1,19 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 public class RandomNumberGenerator {
 
     RandomNumber randomNumber = new RandomNumber();
 
-    public int createRandomNumber() {
-        int randomNumberValue = Randoms.pickNumberInRange(0,9);
-        return randomNumberValue;
+    public List<Integer> createRandomNumberList(LinkedHashMap<String, String> carList) {
+        List<Integer> randomNumberList = new ArrayList<>();
+        for (int i = 0; i < carList.size(); i++) {
+            randomNumberList.add(Randoms.pickNumberInRange(0, 9));
+        }
+        return randomNumberList;
     }
 
-    public void setRandomNumberValueToRandomNumber() {
-        randomNumber.setRandomNumber(createRandomNumber());
-    }
 }
