052afca4302e800fc1a7e9453051c8281b2f99a4
leeminjun <sohwi0305@gmail.com>
2023-10-31 14:59:12 +0900

    feat: 끝날때 메시지 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class OutputView {
+    public static void printHead() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printException(String message) {
+        System.out.println(message);
+    }
+}
