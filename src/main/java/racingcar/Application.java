4da182985206f4093edcdf984b5902c0647e2f73
kichang_98 <richkc2006@naver.com>
2023-10-31 20:59:11 +0900

    함수 활용 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,16 +5,40 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // 사용자 입력 요구
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         // 자동차 이름 입력
+        String[] carNames = getCarNames();
+
+        // 시도 회수 입력
+        int attempts = getAttempts();
+
+        // 실행 결과 문구 출력
+        System.out.println();
+        System.out.println("실행 결과");
+
+        // 자동차 전진 거리를 저장할 공간
+        int[] carForward = new int[carNames.length];
+
+        // 시도 회수 만큼 진행 하고 결과 출력
+        for (int i = 0; i < attempts; i++) {
+            forwardCar(carNames, carForward);
+            printCar(carNames, carForward);
+        }
+
+        String winner = getWinner(carNames, carForward);
+        System.out.println("최종 우승자: " + winner);
+    }
+
+    // 자동차 이름을 입력 받는 함수
+    private static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
-
-        // 입력된 자동차 이름을 (,)기준으로 split
         String[] carNames = carName.split(",");
+        validCarNames(carNames);
+        return carNames;
+    }
 
-        // 잘못된 입력에 대해 IllegalArgumentException 발생
+    // 입력된 자동차 이름이 유효한지 검사하는 함수
+    private static void validCarNames(String[] carNames) {
         for (int i = 0; i < carNames.length; i++) {
             // 자동차 이름은 5자 이하만 가능
             if (carNames[i].trim().length() > 5) {
@@ -25,7 +49,10 @@ public class Application {
                 throw new IllegalArgumentException();
             }
         }
+    }
 
+    //시도 회수를 입력받고 검사하는 함수
+    private static int getAttempts() {
         // 시도 회수 요구
         System.out.println("시도할 회수는 몇회인가요?");
 
@@ -37,42 +64,39 @@ public class Application {
         if (attempts <= 0) {
             throw new IllegalArgumentException();
         }
+        return attempts;
+    }
 
-        // 실행 결과 문구 출력
-        System.out.println();
-        System.out.println("실행 결과");
-
-        // 전진 결과를 저장
-        int[] carForward = new int[carNames.length];
-
-        // 각 자동차 값 4 이상일 때 전진, 회차별 결과 출력
-        for (int i = 0; i < attempts; i++) {
-            for (int j = 0; j < carNames.length; j++) {
-                int randomNumber = Randoms.pickNumberInRange(0, 9);
-                if (randomNumber >= 4) {
-                    carForward[j]++;
-                }
+    // 각 자동차 값 4 이상일 때 전진하는 함수
+    private static void forwardCar(String[] carNames, int[] carForward) {
+        for (int i = 0; i < carNames.length; i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                carForward[i]++;
             }
-            for (int j = 0; j < carNames.length; j++) {
-                System.out.print(carNames[j] + " : ");
-                for (int k = 0; k < carForward[j]; k++) {
-                    System.out.print("-");
-                }
-                System.out.println();
+        }
+    }
+
+    // 진행 상황 출력 함수
+    private static void printCar(String[] carNames, int[] carForward) {
+        for (int i = 0; i < carNames.length; i++) {
+            System.out.print(carNames[i] + " : ");
+            for (int j = 0; j < carForward[i]; j++) {
+                System.out.print("-");
             }
             System.out.println();
         }
+        System.out.println();
+    }
 
-        // 우승자 전진 거리 구하기
+    // 우승자 결정 함수
+    private static String getWinner(String[] carNames, int[] carForward) {
         int winnerDistance = 0;
         for (int i = 0; i < carNames.length; i++) {
             if (carForward[i] > winnerDistance) {
                 winnerDistance = carForward[i];
             }
         }
-
-        // 우승자 출력
-        System.out.print("최종 우승자 : ");
         String winner = "";
         for (int i = 0; i < carNames.length; i++) {
             if (carForward[i] == winnerDistance) {
@@ -82,6 +106,6 @@ public class Application {
                 winner += carNames[i];
             }
         }
-        System.out.println(winner);
+        return winner;
     }
 }
