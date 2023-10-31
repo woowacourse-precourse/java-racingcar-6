f18cde845ab0ec187a9990675ee1d621e09cbd4a
reddevilmidzy <midzy0228@gmail.com>
2023-10-31 20:27:58 +0900

    style: 테스트 파일 하단 개행 추가

--- src/test/java/racingcar/validation/RacerValidatorTest.java
+++ src/test/java/racingcar/validation/RacerValidatorTest.java
@@ -8,12 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RacerValidatorTest {
 
-    //TODO: 나중에 검증기 모아서 한 클래스에서 테스트 하는 것도 나쁘지 않을 뜻
     @DisplayName("클래스 지원 테스트")
     @Test
     void checkSupport() {
         Validator validator = new RacerValidator();
         assertThat(validator.support(Racer.class)).isTrue();
     }
-
 }
