2763c9f5ca643e12e9da6592c1af1d3266ce49e9
chaehuichan <eqholic1125@gmail.com>
2023-10-31 22:39:23 +0900

    feat: 우승자 판별 기능 구현

--- src/main/java/racingcar/RacingManager.java
+++ src/main/java/racingcar/RacingManager.java
@@ -11,11 +11,14 @@ import java.util.List;
 
 public class RacingManager {
     private static final int LOOP_START_NUMBER = 0;
+    private static final int MINIMUM_TRY_COUNT = 1;
+    private static final int MAX_COUNT_INITIAL_NUMBER = 0;
 
     private final RacingPrinter racingPrinter = new ConsoleRacingPrinter();
     private final RacingReceiver racingReceiver = new ConsoleRacingReceiver();
 
     private List<RacingCar> racingCars = new ArrayList<>();
+    private List<String> winners = new ArrayList<>();
 
     public void run() {
         racingPrinter.requestInputRacingCarName();
@@ -40,6 +43,10 @@ public class RacingManager {
             racingCar.moveOrStop();
             racingPrinter.printResultDetail(racingCar);
         }
+
+        decideWinner();
+
+        racingPrinter.noticeWinner(winners);
     }
 
     private String[] splitByComma(String names) {
@@ -49,13 +56,36 @@ public class RacingManager {
     private int validateTryCount(String tryCount) {
         try {
             int numberedTryCount = Integer.parseInt(tryCount);
-            if (numberedTryCount < 1) {
+            if (numberedTryCount < MINIMUM_TRY_COUNT) {
                 throw new IllegalArgumentException();
             }
             return numberedTryCount;
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void decideWinner() {
+        int maxCount = findMaxCount();
+
+        for (RacingCar racingCar : racingCars) {
+            isWinner(racingCar, maxCount);
+        }
+    }
+
+    private void isWinner(RacingCar racingCar, int maxCount) {
+        if (racingCar.getMoveCount() == maxCount) {
+            winners.add(racingCar.getName());
+        }
+    }
+
+    private int findMaxCount() {
+        int maxCount = MAX_COUNT_INITIAL_NUMBER;
+        for (RacingCar racingCar : racingCars) {
+            maxCount = Math.max(racingCar.getMoveCount(), maxCount);
+        }
+
+        return maxCount;
     }
 
 }
