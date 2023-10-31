ea51a1823e9353ff95a01e3854e523bff29f720b
whereami0404 <whereami2048@gmail.com>
2023-11-01 04:19:56 +0900

    test: InputValidator 테스트 코드 추가

---
+++ src/test/java/util/InputValidatorTest.java
@@ -0,0 +1,35 @@
+package util;
+
+import net.bytebuddy.pool.TypePool.Resolution.NoSuchTypeException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import racingcar.util.InputValidatior;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidatorTest {
+    InputValidatior validatior = new InputValidatior();
+
+    @Test
+    void 자동차_이름_String_배열_중_빈_값_존재_테스트() {
+        String input = "";
+        String[] stringList = input.split(",");
+
+        assertThat(validatior.hasEmptyCarName(stringList)).isTrue();
+    }
+
+    @Test
+    void 자연수_검증_테스트() {
+        String number = "1";
+
+        assertThat(validatior.isNaturalNumber(number)).isTrue();
+    }
+
+    @Test
+    void 자연수_예외_테스트() {
+        String number = "a";
+
+        assertThat(validatior.isNaturalNumber(number)).isFalse();
+    }
+}
