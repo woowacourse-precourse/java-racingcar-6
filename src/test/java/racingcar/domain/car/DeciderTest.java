6dab232011f6e0642893330010068ba541d8ef3f
hongseokhodev <hongseokhodev@gmail.com>
2023-11-01 00:21:59 +0900

    test: Generator를 구현하는 테스트 클래스를 사용하는 것이 아니라 mock을 사용하도록 테스트 수정

--- src/test/java/racingcar/domain/car/DeciderTest.java
+++ src/test/java/racingcar/domain/car/DeciderTest.java
@@ -2,23 +2,21 @@ package racingcar.domain.car;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
 class DeciderTest {
-    static class oneGenerator implements Generator {
-        @Override
-        public int generate(int startInclusive, int endInclusive) {
-            return 1;
-        }
-    }
+    private final Rule rule = new DecideRule();
 
     @Test
     void 기준보다_작은_수를_뽑으면_isSucceed_메소드가_false를_반환한다() {
         //given
-        Rule rule = new DecideRule();
-        Generator testGenerator = new oneGenerator();
-        Decider decider = Decider.of(rule, testGenerator);
+        Generator oneGenerator = mock(Generator.class);
+        when(oneGenerator.generate(anyInt(), anyInt())).thenReturn(1);
+        Decider decider = Decider.of(rule, oneGenerator);
 
         //when
         boolean result = decider.isSucceed();
@@ -27,19 +25,13 @@ class DeciderTest {
         assertFalse(result);
     }
 
-    static class nineGenerator implements Generator {
-        @Override
-        public int generate(int startInclusive, int endInclusive) {
-            return 9;
-        }
-    }
-
     @Test
     void 기준보다_큰_수를_뽑으면_isSucceed_메소드가_true를_반환한다() {
         //given
         Rule rule = new DecideRule();
-        Generator testGenerator = new nineGenerator();
-        Decider decider = Decider.of(rule, testGenerator);
+        Generator nineGenerator = mock(Generator.class);
+        when(nineGenerator.generate(anyInt(), anyInt())).thenReturn(9);
+        Decider decider = Decider.of(rule, nineGenerator);
 
         //when
         boolean result = decider.isSucceed();
