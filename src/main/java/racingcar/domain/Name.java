d4f16f45b16b490adb645f2319f55470790fef76
twoosky <lhjae3@naver.com>
2023-11-01 06:01:26 +0900

    fix: 사용하지 않는 의존성 제거

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -5,9 +5,6 @@ import static racingcar.utils.ErrorMessages.CAR_NAME_CONTAIN_BLANK;
 import static racingcar.utils.ErrorMessages.CAR_NAME_EMPTY;
 import static racingcar.utils.ErrorMessages.OUT_OF_LENGTH_CAR_NAME;
 
-import racingcar.utils.ErrorMessages;
-
-
 public class Name {
     public static final int MIN_NAME_SIZE = 5;
 
