e417e0e9d7822f4df11f6143609b3f9c0ea03401
hyun-hyang <jimin3060@naver.com>
2023-10-31 22:08:31 +0900

    Fix: main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,8 +2,6 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        new InputView();
         new Controller();
-        new OutputView();
     }
 }
