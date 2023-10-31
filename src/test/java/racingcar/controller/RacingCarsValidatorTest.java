6a9e5a777cce5d5c74ac3164114226a310fa596d
Infikei <hckhong@naver.com>
2023-11-01 03:01:02 +0900

    test: RacingCarsValidator에서 빈 문자열에 대한 테스트 추가

--- src/test/java/racingcar/controller/RacingCarsValidatorTest.java
+++ src/test/java/racingcar/controller/RacingCarsValidatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarsValidatorTest {
     RacingCarsValidator racingCarsValidator = new RacingCarsValidator();
@@ -14,5 +15,45 @@ public class RacingCarsValidatorTest {
         String names = "abc,def";
         List<String> namesSplit = racingCarsValidator.validateNames(names);
         assertThat(namesSplit).containsExactly("abc", "def");
+    }
+
+    @Test
+    void validateEmptyNameTest() {
+        assertThatThrownBy(() -> {
+            String names = "";
+            List<String> namesSplit = racingCarsValidator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateEmptyNameTest2() {
+        assertThatThrownBy(() -> {
+            String names = "infi,,kei";
+            List<String> namesSplit = racingCarsValidator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateEmptyNameTest3() {
+        assertThatThrownBy(() -> {
+            String names = ",infi,kei";
+            List<String> namesSplit = racingCarsValidator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateEmptyNameTest4() {
+        assertThatThrownBy(() -> {
+            String names = "infi,kei,";
+            List<String> namesSplit = racingCarsValidator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateEmptyNameTest5() {
+        assertThatThrownBy(() -> {
+            String names = ",,,,,";
+            List<String> namesSplit = racingCarsValidator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
