d02093d9e74863c82217e504c8fa1f75aa099ab7
starcat37 <starcat37@korea.ac.kr>
2023-10-31 23:02:57 +0900

    feat: 입력 횟수 검증 메서드 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,14 +10,16 @@ import java.util.stream.Stream;
 public class Input {
     public static Map<String, Integer> getInitialStatus() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] cars = readLine().split(",");
+        String[] cars = Console.readLine().split(",");
         Validator.validateCar(cars);
         return Stream.of(cars).collect(Collectors.toMap(car -> car, car -> 0));
     }
 
     public static int getCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(readLine());
+        String count = Console.readLine();
+        Validator.validateCount(count);
+        return Integer.parseInt(count);
     }
 
 }
--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,9 +3,20 @@ package racingcar;
 public class Validator {
     public static void validateCar(String[] cars) {
         for (String car : cars) {
-            if (car.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름이 5자를 넘습니다.");
+            if (car.length() > 5 || car.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름이 1~5자여야 합니다.");
             }
+        }
+    }
+
+    public static void validateCount(String count) {
+        try {
+            int parsedCount = Integer.parseInt(count);
+            if (parsedCount <= 0) {
+                throw new IllegalArgumentException("반복 횟수는 자연수여야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("반복 횟수는 자연수여야 합니다.");
         }
     }
 }
