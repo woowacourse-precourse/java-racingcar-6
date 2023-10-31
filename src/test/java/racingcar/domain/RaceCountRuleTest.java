9521a6c5016e51ae73571c46567e1a4b4f8b4c77
HiiWee <wpdlzhf159@gmail.com>
2023-10-31 22:35:11 +0900

    fix: 테스트 코드 예외 메시지 수정

--- src/test/java/racingcar/domain/RaceCountRuleTest.java
+++ src/test/java/racingcar/domain/RaceCountRuleTest.java
@@ -9,23 +9,23 @@ import racingcar.validator.InputMoveCountValidator;
 
 class RaceCountRuleTest {
 
-    @DisplayName("시도 횟수가 1미만, 10초과라면 예외가 발생합니다.")
+    @DisplayName("이동 횟수가 1미만, 10초과라면 예외가 발생합니다.")
     @ParameterizedTest
     @ValueSource(strings = {"0", "11"})
     void validateCount_exception_withInvalidRange(String invalidInputCount) {
         // given & when & then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> InputMoveCountValidator.validateCount(invalidInputCount))
-                .withMessageContaining("시도 횟수는 1 이상 10 이하의 숫자를 입력해야 합니다.");
+                .withMessageContaining("이동 횟수는 1 이상 10 이하의 숫자를 입력해야 합니다.");
     }
 
-    @DisplayName("시도 횟수가 숫자가 아닌 입력이라면 예외가 발생합니다.")
+    @DisplayName("이동 횟수가 숫자가 아닌 입력이라면 예외가 발생합니다.")
     @ParameterizedTest
     @ValueSource(strings = {"0_", "f3"})
     void validateCount_exception_withNumberFormat(String invalidInputCount) {
         // given & when & then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> InputMoveCountValidator.validateCount(invalidInputCount))
-                .withMessageContaining("시도 횟수는 숫자만 입력해야 합니다.");
+                .withMessageContaining("이동 횟수는 숫자만 입력해야 합니다.");
     }
 }
