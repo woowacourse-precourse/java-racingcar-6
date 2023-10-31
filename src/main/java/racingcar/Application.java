af8849aa6845b60ecc0da7bbe1eaaaa111899e1e
ksngh <chzhqk98@naver.com>
2023-10-31 23:50:53 +0900

    feat: car class 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+class car {
+	String name;
+	String GoCount = "";
+	int RanNum;
+
+	public void move() {
+		if (RanNum >= 4) {
+			GoCount += "-";
+		}
+	}
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
