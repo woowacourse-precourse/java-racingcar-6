25435775fcadd81e859857ba80e000b064ecb9e2
kwon <kckc0608@naver.com>
2023-11-01 00:31:24 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,5 +4,6 @@ public class Application {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static void main(String[] args) {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
+        String carNameInput = camp.nextstep.edu.missionutils.Console.readLine();
     }
 }
