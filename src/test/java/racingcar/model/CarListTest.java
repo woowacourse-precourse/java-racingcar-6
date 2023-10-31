e345ad07c75346c41ecca73512636ce99f436d8f
myeongju00 <j51677767@gmail.com>
2023-11-01 01:09:49 +0900

    [test] : 랜덤 값을 기준으로 자동차가 전진하는 기능 테스트

--- src/test/java/racingcar/model/CarListTest.java
+++ src/test/java/racingcar/model/CarListTest.java
@@ -1,12 +1,17 @@
 package racingcar.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 
-class CarListTest {
+class CarListTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     public void 자동차_이름에_중복이_있으면_예외() {
@@ -27,4 +32,19 @@ class CarListTest {
         assertThat(result.getCarList()).containsExactly(new Car("a"), new Car("b"), new Car("c"));
     }
 
+    @Test
+    public void move_메서드_사용시_모든_자동차가_움직인다() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("car1,car2", "1");
+                    assertThat(output()).contains("car1 : ", "car2 : -");
+                },
+                STOP, MOVING_FORWARD
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
