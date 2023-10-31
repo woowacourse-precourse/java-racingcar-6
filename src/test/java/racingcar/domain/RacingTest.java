39da67e566b381060c0fc903aa01a17825261d8b
JayG-5 <jg55552758@gmail.com>
2023-10-31 12:16:36 +0900

    refactor: 테스트 이름에 빈 문자열 기입 리팩토링

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,24 +1,22 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Racing;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("RacingTest")
 public class RacingTest {
 
     @Test
+    @DisplayName("성공 : 이름에 빈 문자열 기입")
     public void 이름에_빈_문자열_기입() {
         String input = "";
 
         final Racing racing = new Racing();
 
-        assertThatThrownBy(() -> {
-            racing.ready();
-        })
-                .isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("String index out of range: 5");
+        assertThatThrownBy(() -> racing.getCarList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1~5자로 입력해주세요.");
     }
 
 }
