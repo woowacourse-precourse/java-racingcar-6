46a6ecb5ee3209371ade0e2ed868d3dd11094ab9
zion0425 <zion0425@naver.com>
2023-10-31 23:06:53 +0900

    feat(inputCarNames) : 입력 받기 전, 표준 출력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 public class Application {
     public static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
         String[] carNames = spilitNames(carName);
 
