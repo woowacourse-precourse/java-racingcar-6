9a48f0ca7762e43d434df9d78c7e489f29182674
davemins <davemins4125@naver.com>
2023-10-31 16:53:53 +0900

    feat: 전체 게임 진행 메소드 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -9,16 +9,17 @@ import java.util.List;
 public class RaceController {
 
     private String carNames;
+    private int moveNumber;
     private List<String> racersNames;
     private List<Racer> racers;
 
     public void play() {
         OutputView.printInputRacersNameMessage();
-        carNames = InputView.readRacersName();
+        this.carNames = InputView.readRacersName();
         racersNames = RacerManager.seperateRacersName(carNames);
+        InputValidator.validateRacersName(racersNames);
         racers = RacerManager.createRacers(racersNames);
-
-
-
+        OutputView.printInputMoveNumberMessage();
+        moveNumber = InputView.readMoveNumber();
     }
 }
