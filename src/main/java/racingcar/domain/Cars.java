628bf6469980856ab69a7d86228771f2ef105bd4
ghdeo <goldencorn7@naver.com>
2023-10-31 23:36:27 +0900

    feat: Add Cars instance variable

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,14 @@
 package racingcar.domain;
 
+import java.util.*;
+
 public class Cars {
+    List<Car> cars = new ArrayList<>();
+
+    public Cars(String[] names) {
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+    }
 
 }
