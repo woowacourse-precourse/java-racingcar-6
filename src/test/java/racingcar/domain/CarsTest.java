83345c708af5e7db082264f66bf97741b23ba990
D0ri123 <lsh81550@gmail.com>
2023-10-31 12:41:08 +0900

    test: 콘솔 입력 검증 테스트 구현  - 4차 구현
    Car의 이름들을 가지고 있는 Cars의 입력 검증 테스트를 구현했습니다.
    안내 문구에 구분자 이외에 입력 예시를 보여주지 않다는걸 고려해서,
    중간 공백, 양끝 공백을 모두 제거하는 로직을 추가했습니다.
    또한 입력 예외 처리에 대해서도 테스트를 구현했습니다.

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,45 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CarsTest {
+
+    Cars cars;
+
+    @BeforeEach
+    public void setUpCars() {
+        cars = new Cars();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "'seori,snow,joy'",
+        "'seori, snow, joy'",
+        "'seori ,snow ,joy '",
+        "'seori, sn ow, jo y'"
+    })
+    void 이름을_올바르게_입력한다(String input) {
+        cars.settingNames(input);
+        assertEquals(cars.getCars().size(), 3);
+        assertEquals(cars.getCars().get(0).name, "seori");
+        assertEquals(cars.getCars().get(1).name, "snow");
+        assertEquals(cars.getCars().get(2).name, "joy");
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "'seori,seori,snow,joy', 동일한 이름으로 경주 게임을 진행할 수 없습니다.",
+        "'snow,sn ow,j oy,joy',동일한 이름으로 경주 게임을 진행할 수 없습니다.",
+        "meow,'이름을 쉼표(,)로 구분해야 합니다. 또한, 최소 2대 이상의 자동차가 필요합니다.'"
+    })
+    void 이름들을_잘못_입력하는_경우(String input, String exceptionMessage) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+            () -> cars.settingNames(input));
+        assertEquals(e.getMessage(), exceptionMessage);
+    }
+}
