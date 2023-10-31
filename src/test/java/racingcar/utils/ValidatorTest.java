4389943fbdf943172dd01e799cd15fd1a0a105d6
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-10-31 10:58:08 +0900

    Test: validateCarNameInput테스트 추가
    5글자 이상시(6글자 문자열 "wonjun"을 입력한 경우)  IllegalArgumentException 발생, 발생한 예외메시지 체크

---
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -0,0 +1,20 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class ValidatorTest {
+    Validator validator = new Validator();
+
+    private static final String WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE = "이름은 5자 이하여야 합니다";
+   
+    @Test
+    void 차_이름_길이_예외테스트() {
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validator.validateCarNameInput("wonjun,abc,def");
+        });
+        assertEquals(WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE, e.getMessage());
+    }
+}
