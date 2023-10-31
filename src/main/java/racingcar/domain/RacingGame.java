35ad3738d2c2170aa365aa7032592b0eb89b04a9
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-31 18:00:21 +0900

    fix: 멤버 변수 이전

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,13 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class RacingGame {
+	private final List<Car> carList;
+	private final Referee referee;
+
+	public RacingGame(List<Car> carList, Referee referee) {
+		this.carList = carList;
+		this.referee = referee;
+	}
 }
--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -3,13 +3,6 @@ package racingcar.domain;
 import java.util.List;
 
 public class Referee {
-	private final List<Car> carList;
-	private final Referee referee;
-
-	public Referee(List<Car> carList, Referee referee) {
-		this.carList = carList;
-		this.referee = referee;
-	}
 
 	public List<Car> getFurthestCar() {
 		return null;
