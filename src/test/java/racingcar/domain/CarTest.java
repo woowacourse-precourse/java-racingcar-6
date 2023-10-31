6a350c2ca451fc76834783bff6d320aaf769e87e
Hyoseop Song <crad_on25@naver.com>
2023-11-01 05:20:22 +0900

    test(CarTest): 전진 함수 입력값 조건 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +14,8 @@ class CarTest {
 //    void setUp() {
 //        Car car = new Car("pobi");
 //    }
-    
+
+
     @DisplayName("자동차 이름이 null, 빈 문자열, 5자 초과인 경우 IllegalArgumentException 발생")
     @ParameterizedTest
     @NullAndEmptySource
@@ -22,5 +24,14 @@ class CarTest {
         assertThatThrownBy(() -> new Car(inputName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 1~5자 이내만 가능합니다.");
+    }
+
+    @DisplayName("랜덤으로 생성된 숫자가 4~9 인 경우 자동차가 전진한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void moveTest(int randomNumber) {
+        Car car = new Car("pobi");
+        car.move(randomNumber);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 }
