3db8bd90307f0d0039abfc9492af7b5ab4545946
AmitaWhite <amitawhite@AmitaWhites-MacBook-Pro-13.local>
2023-11-01 01:33:22 +0900

    TODO: 포멧맞춰서 정리만 더

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Input
+# @ Input
 ** 잘못된 값 입력시 throw IllegalArgumentException **
 1. N개의 자동차 (이름 부여)
    1. Restrictions
@@ -6,16 +6,22 @@
        * 5글자 이하 
 2. 이동 횟 수
 
-# func1 - input 정리
+# @ func1 - input 정리
     syso 1. 경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)
-input 1 -> String usersCarName -> 분할하여 Class 
+
+- input 1 -> String usersCarName -> 분할하여 Class 
 
     syso 2. 시도할 회수는 몇회인가요?
-input 2 -> int tryNum
 
-# func2 - 결과 출력
-    while (tryNum) {
-      tryNum--
+- input 2 -> int loofInt
+
+# @ func2 - 결과 출력
+    while (loofInt) {
+      loofInt--
+        for (list.size){
+            addCount(random)
+            syso(포멧)
+        }
       } 
 
-# output
+# @ Output
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,58 @@
 package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 
 public class Application {
     public static void main(String[] args) {
 
         //input 1
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String userInput = readLines();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = readLine();
 
-        //func1 -> input 정리해서 생성
-        ArrayList<String> carList = userInput.split(",");
+        //func1 -> input 정리
+        List<String> carList = List.of(userInput.split(","));
+        List<RacingCar> carStatus = new ArrayList<>();
+
+        for(int i=0;i<carList.size();i++){
+
+            //carStatus 에 담기
+            RacingCar tmpRacingCar = new RacingCar();
+            tmpRacingCar.setCarName(carList.get(i));
+
+            carStatus.add(tmpRacingCar);
+        }
 
 
-        for (i==0;i<size(carList);i++){
+        //input 2
+        System.out.println("시도할 회수는 몇회인가요?");
+        String loofStr = readLine();
+
+        // int 형으로 바꿔 주긴 할건데, '35T'이런건 except 로 보고.. -35 이런건 그냥 바꿔줘야하나 몰겠다
+        try{
+            int loofInt = Math.abs(Integer.parseInt(loofStr));
+        }catch(NumberFormatException ex){
+            throw new IllegalArgumentException();
+        }
+
+        // func2 -> 결과출력
+        System.out.println("실행 결과");
+
+        while(loofInt){
+            loofInt--;
+
+            for (int i=0;i<carStatus.size();i++){
+                carStatus.get(i).addWinCount(pickNumberInRange(0,9));
+                System.out.println(String.format())
+            }
 
         }
 
-        RacingCar tmp = new RacingCar();
-        tmp.setCarName("hellow");
-        System.out.print(String.format("%s",tmp.carName));
+
     }
 }
 
