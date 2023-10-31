532a41228d0a6317a58cd48e63b9d470a4cf6878
josephuk77 <josephuk77@gmail.com>
2023-10-31 15:41:24 +0900

    Refactor(Exception): 사용하지 않는 함수 삭제

--- src/main/java/racingcar/exception/InvalidCountException.java
+++ src/main/java/racingcar/exception/InvalidCountException.java
@@ -1,10 +1,6 @@
 package racingcar.exception;
 
 public class InvalidCountException extends IllegalArgumentException {
-    public InvalidCountException() {
-        super("숫자를 입력해 주세요.");
-    }
-
     public InvalidCountException(String message) {
         super(message);
     }
--- src/main/java/racingcar/exception/InvalidNameException.java
+++ src/main/java/racingcar/exception/InvalidNameException.java
@@ -1,10 +1,6 @@
 package racingcar.exception;
 
 public class InvalidNameException extends IllegalArgumentException {
-    public InvalidNameException() {
-        super("유효한 이름을 입력해 주세요.");
-    }
-
     public InvalidNameException(String message) {
         super(message);
     }
