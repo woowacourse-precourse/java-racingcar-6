745e5ad56a56ab429fb664715648d4594d3e0bb8
jihun <pjh2996@naver.com>
2023-10-31 21:18:18 +0900

    test : 무작위 값이 4 이상일 경우에만 전진가능 테스트

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -18,4 +18,32 @@ class CarsTest {
         //then
         Assertions.assertThat(2).isEqualTo(car.getDistance());
     }
+
+    @Test
+    public void 숫자_4_미만_전진_불가능() {
+        //given
+        Cars car = new Cars("JiHun", 0);
+
+        //when
+        car.move(2);
+        car.move(3);
+        car.move(1);
+
+        //then
+        Assertions.assertThat(0).isEqualTo(car.getDistance());
+    }
+
+    @Test
+    public void 숫자_4_이상_전진_가능() {
+        //given
+        Cars car = new Cars("JiHun", 0);
+
+        //when
+        car.move(4);
+        car.move(5);
+        car.move(6);
+
+        //then
+        Assertions.assertThat(3).isEqualTo(car.getDistance());
+    }
 }
