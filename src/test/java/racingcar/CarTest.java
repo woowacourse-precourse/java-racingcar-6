0cbe355e1873df89b6e855fe05a89cfcff86f8bb
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2023-11-01 03:25:31 +0900

    refactor: 사용하지 않는 import문 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,7 +3,6 @@ package racingcar;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     @Test
