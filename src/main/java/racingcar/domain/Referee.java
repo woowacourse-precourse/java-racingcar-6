d6c29b696e59c109babc183017b196214b247d1f
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-10-31 15:24:03 +0900

    Feat(Referee): 모든 자동차의 전진횟수를 비교하는 기능 구현

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,9 +1,30 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Referee {
-    public String compare(List<String> cars, List<Integer> scores) {
-        return null;
+    public String compare(List<String> cars, List<Integer> racingResult) {
+        int maxGoCount = Collections.max(racingResult);
+        List<Integer> maxGoCountIndexes = findAllIndexOf(maxGoCount, racingResult);
+
+        List<String> winners = new ArrayList<>();
+        for (int index : maxGoCountIndexes) {
+            winners.add(cars.get(index));
+        }
+        return String.join(",", winners);
+    }
+
+
+    public List<Integer> findAllIndexOf(int maxValue, List<Integer> racingResult) {
+        List<Integer> indexes = new ArrayList<>();
+        for (int index = 0; index < racingResult.size(); index++) {
+            int value = racingResult.get(index);
+            if (value == maxValue) {
+                indexes.add(index);
+            }
+        }
+        return indexes;
     }
 }
