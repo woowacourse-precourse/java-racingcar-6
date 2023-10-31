0f63530f0bba8613f756154249ab0d8a3c89bc1e
깃wooqqq <sge0225@naver.com>
2023-10-31 17:56:20 +0900

    feat: 자동차 이름이 1자 미만일 경우 예외처리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 public class Validator {
     private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final int CAR_NAME_MIN_LENGTH = 1;
 
     public void checkCarName(List<String> input) {
         checkCarNameLength(input);
+        checkCarNameMinLength(input);
     }
 
     private void checkCarNameLength(List<String> carNames) {
         for (String carName : carNames) {
             if (carName.length() > CAR_NAME_MAX_LENGTH) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다,");
+            }
+        }
+    }
+
+    private void checkCarNameMinLength(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() < CAR_NAME_MIN_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상만 가능합니다.");
             }
         }
     }
