5a5f279f830895f58a32d8af946aee58f534c5c5
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-31 21:55:34 +0900

    refactor(GameMessage): 게임 메시지 enum으로 변경

---
+++ src/main/java/racingcar/utils/GameMessage.java
@@ -0,0 +1,21 @@
+package racingcar.utils;
+
+public enum GameMessage {
+    REQUEST_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    REQUEST_GAME_ROUND_MESSAGE("시도할 횟수는 몇회인가요?"),
+    RESULT_MESSAGE("실행 결과"),
+    FINAL_WINNER_MESSAGE("최종 우승자 : "),
+    ROUND_RESULT("%s : %s"),
+    DISTANCE_SYMBOL("-"),
+    NEW_LINE("\n");
+
+    private String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -4,18 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import racingcar.domain.car.CarDto;
+import racingcar.utils.GameMessage;
 
-public class ConsoleOutputView implements OutputView{
-    private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String REQUEST_GAME_ROUND_MESSAGE = "시도할 횟수는 몇회인가요?";
-    private static final String RESULT_MESSAGE = "실행 결과";
-    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
-    private static final String ROUND_RESULT = "%s : %s";
-    private static final String DISTANCE_SYMBOL = "-";
-    private static final String NEW_LINE = "\n";
+public class ConsoleOutputView implements OutputView {
     private static ConsoleOutputView instance;
 
-    private ConsoleOutputView() {}
+    private ConsoleOutputView() {
+    }
 
     public static ConsoleOutputView getInstance() {
         if (instance == null) {
@@ -26,17 +21,18 @@ public class ConsoleOutputView implements OutputView{
 
     @Override
     public void printRequestCarNames() {
-        System.out.println(REQUEST_CAR_NAMES_MESSAGE);
+        System.out.println(GameMessage.REQUEST_CAR_NAMES_MESSAGE.getMessage());
     }
 
     @Override
     public void printRequestGameRound() {
-        System.out.println(REQUEST_GAME_ROUND_MESSAGE);
+        System.out.println(GameMessage.REQUEST_GAME_ROUND_MESSAGE.getMessage());
     }
 
     @Override
     public void printResultMessage() {
-        System.out.println(NEW_LINE + RESULT_MESSAGE);
+        System.out.print(GameMessage.NEW_LINE.getMessage());
+        System.out.println(GameMessage.RESULT_MESSAGE.getMessage());
     }
 
     @Override
@@ -45,18 +41,19 @@ public class ConsoleOutputView implements OutputView{
             CarDto car = result.get(i);
             String name = car.name();
             String position = generatePositionWithSymbol(car.position());
-            System.out.printf(ROUND_RESULT + NEW_LINE, name, position);
+            System.out.printf(GameMessage.ROUND_RESULT.getMessage(), name, position);
+            System.out.print(GameMessage.NEW_LINE.getMessage());
         }
-        System.out.println();
+        System.out.print(GameMessage.NEW_LINE.getMessage());
     }
 
     @Override
     public void printGameWinner(String winner) {
-        System.out.print(FINAL_WINNER_MESSAGE + winner);
+        System.out.print(GameMessage.FINAL_WINNER_MESSAGE.getMessage() + winner);
     }
 
     private String generatePositionWithSymbol(int position) {
-        return Stream.generate(() -> DISTANCE_SYMBOL)
+        return Stream.generate(() -> GameMessage.DISTANCE_SYMBOL.getMessage())
                 .limit(position)
                 .collect(Collectors.joining());
     }
