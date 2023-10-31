764f0b2e3be6afaeacbbd9fd552c36f681dcc68f
twoosky <lhjae3@naver.com>
2023-10-31 19:39:40 +0900

    feat: 자동차 경주 심판 및 중간 결과 반환 기능 구현

---
+++ src/main/java/racingcar/domain/Judgment.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import java.util.List;
+import racingcar.utils.NumberGenerator;
+
+public class Judgment {
+    private final Cars cars;
+    private final NumberGenerator numberGenerator;
+
+    public Judgment(Cars cars, NumberGenerator numberGenerator) {
+        this.cars = cars;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public List<Car> judge() {
+        cars.move(numberGenerator);
+        return cars.getCars();
+    }
+}
