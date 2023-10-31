b9adbbd2a28213ed4a0b7e4b3f546f22bd85fda7
2uwls <2uwls1126@gmail.com>
2023-11-01 01:44:55 +0900

    fix: remove duplicate calls of createRandomNum

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,10 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.ArrayList;
 
-
-
 public class Application {
-
     static List<Car> cars = new ArrayList<>();
     static ArrayList<String> winners = new ArrayList<>();
     public static void main(String[] args) {
@@ -21,13 +18,19 @@ public class Application {
         drive(trialNumber);
         showWinner(decideWinner(getMaxScore()));
     }
-
+    private static void drive(Integer saveTrialNumber) {
+        for (int i = 0; i<saveTrialNumber; i++) {
+            updateDriveNum();
+            driveResult();
+            System.out.println("");
+        }
+    }
     private static Integer createDriveCarNum () {
         Integer driveNumber = isMoreThanFour(createRandomNum());
         return driveNumber;
     }
     private static Integer createRandomNum() {
-        Integer randomNum = Randoms.pickNumberInRange(0,9);;
+        Integer randomNum = Randoms.pickNumberInRange(0,9);
         return randomNum;
     }
     private static Integer isMoreThanFour (Integer randomNum) {
@@ -35,6 +38,12 @@ public class Application {
             return 1;
         } else {
             return 0;
+        }
+    }
+    private static void updateDriveNum() {
+        for (Car car : cars) {
+            Integer newDriveNum = createDriveCarNum();
+            car.setDriveNum(newDriveNum);
         }
     }
     private static void createCars(String carNames) {
@@ -48,19 +57,6 @@ public class Application {
             cars.add(car);
         }
     }
-
-    private static void drive(Integer saveTrialNumber) {
-        for (int i = 0; i<saveTrialNumber; i++) {
-            updateDriveNum(createDriveCarNum());
-            driveResult();
-            System.out.println("");
-        }
-    }
-    private static void updateDriveNum(Integer createDriveCarNum) {
-        for (Car car : cars) {
-            car.setDriveNum(createDriveCarNum());
-        }
-    }
     private static String makeResultSign(int number) {
         String resultSign="";
         for (int i = 0; i<number; i++) {
@@ -68,13 +64,11 @@ public class Application {
         }
         return resultSign;
     }
-
     private static void driveResult() {
         for (Car car : cars ) {
             System.out.println(car.getName()+" : "+makeResultSign(car.getDriveNum()));
         }
     }
-
     private static int getMaxScore() {
         int max = 0;
         int countPerson = 0;
@@ -103,16 +97,12 @@ public class Application {
 
     private static void showWinner(boolean decideWinner) {
         if (decideWinner) {
-            System.out.println("최종우승자 : "+ winners.get(0));
+            System.out.println("최종 우승자 : "+ winners.get(0));
         } else {
             String winnerList = String.join(", ",winners);
             System.out.print("최종 우승자 : "+winnerList);
         }
     }
-
-
-
-
     private static Integer saveTrialNumber(String trialNumberInput) {
         try {
             Integer trialNumber = Integer.valueOf(trialNumberInput);
@@ -121,13 +111,10 @@ public class Application {
             throw new IllegalArgumentException("숫자를 입력해주세요");
         }
     }
-
     private static void isCorrectCarsInput(String carName) {
         if (carName.length() > 5 ) {
             throw new IllegalArgumentException("이름의 길이가 5자가 넘어갑니다. ");
         }
         //구분문자가 쉼표가 아닌 경우 한 명만 들어오면 쉼표는 없는데..
     }
-
-
 }
