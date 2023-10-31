ae5b43eac93933fc603fec5e9f32d445e750bc55
MingyeomKim <mingyum119@naver.com>
2023-11-01 04:16:09 +0900

    refactor(RaceController): Winner 도메인을 활용해 코드 간결화
    Winner 도메인을 생성하고 toString()을 오버라이딩하여 최종 우승자 메시지를 출력한다.

--- src/main/java/racingcar/presentation/RaceController.java
+++ src/main/java/racingcar/presentation/RaceController.java
@@ -1,21 +1,13 @@
 package racingcar.presentation;
 
-import static racingcar.global.constants.MessageType.FINAL_RESULT_MESSAGE;
 import static racingcar.global.constants.MessageType.RESULT_MESSAGE;
 
-import java.util.List;
-import racingcar.application.RaceService;
 import racingcar.domain.Cars;
+import racingcar.domain.Winners;
 import racingcar.utils.Parser;
 import racingcar.view.View;
 
 public class RaceController {
-    private final RaceService raceService;
-
-    public RaceController(RaceService raceService) {
-        this.raceService = raceService;
-    }
-
     public void startRace() {
         Cars cars = Cars.of(View.requestCarNames());
         Integer count = Parser.parseStringToInteger(View.requestCount());
@@ -32,18 +24,7 @@ public class RaceController {
     }
 
     private void finish(Cars cars) {
-        List<String> winners = raceService.getWinners(cars);
-        View.printlnString(generateFinalResultMessage(winners));
-    }
-
-    private String generateFinalResultMessage(List<String> winners) {
-        StringBuilder messageBuilder = new StringBuilder();
-        for (String winner : winners) {
-            messageBuilder.append(winner).append(FINAL_RESULT_TOKEN.getSymbol());
-        }
-        if (messageBuilder.length() > 0) {
-            messageBuilder.setLength(messageBuilder.length() - FINAL_RESULT_TOKEN.getSymbol().length());
-        }
-        return String.format(FINAL_RESULT_MESSAGE.getMessage(), messageBuilder.toString());
+        Winners winners = Winners.of(cars);
+        View.printlnString(winners.toString());
     }
 }
