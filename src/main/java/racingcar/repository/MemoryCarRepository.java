0a480f3a914d93356ec9b6cb3d0c28992eb80c29
KimKyuHwan <vkdls754@gmail.com>
2023-10-31 22:11:15 +0900

    docs(README.md): 요구사항 구현 체크 수정
    MemoryCarRepository : 변수 타입 변경

--- src/main/java/racingcar/repository/MemoryCarRepository.java
+++ src/main/java/racingcar/repository/MemoryCarRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class MemoryCarRepository implements CarRepository {
-    private static ArrayList<Car> carList = new ArrayList<>();
+    private static List<Car> carList = new ArrayList<>();
 
     @Override
     public void save(String name) {
