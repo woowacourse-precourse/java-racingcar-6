7b7812ce0ef09e39e505d9d52f924081fa741b61
Eunjung <fkznsha23@gmail.com>
2023-10-31 21:37:59 +0900

    test commit

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,6 @@
 package racingcar;
 
-import controller.RacingCarComtroller;
-
 public class Application {
     public static void main(String[] args) {
-        RacingCarComtroller.raceStart();
     }
 }
