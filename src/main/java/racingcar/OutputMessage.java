efad0e47206ce97a2791a4d70ac58cb7eb2dbb99
jiho kim <jihokim1024@gmail.com>
2023-11-01 06:41:08 +0900

    결과 출력 시 안내 멘트 출력

---
+++ src/main/java/racingcar/OutputMessage.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class OutputMessage {
+    public void outputMessage1() {
+        System.out.println("실행 결과");
+    }
+
+    public void outputMessage2() {
+        System.out.print("최종 우승자 : ");
+    }
+}
