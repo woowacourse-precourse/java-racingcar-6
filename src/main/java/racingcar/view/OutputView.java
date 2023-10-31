8f26215449d20ca0723b45359d73c121c3060da7
jeondui <jeondui@gmail.com>
2023-11-01 00:08:34 +0900

    refactor(OutputView): 최종 우승자 문자열 연결 부분 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,29 +1,21 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.StringJoiner;
 import racingcar.model.Car;
 
 public class OutputView {
 
     public static void printResult(List<Car> cars) {
+        StringJoiner result = new StringJoiner("\n");
         for (Car car : cars) {
-            System.out.printf("%s : %s\n", car.getName(), car.getStringLocation());
+            result.add(String.format("%s : %s", car.getName(), car.getStringLocation()));
         }
-        System.out.println();
+        System.out.println(result);
     }
 
     public static void printFinalWinner(List<String> winners) {
-        System.out.println("최종 우승자 : " + getWinnersNameString(winners));
-    }
-
-    private static String getWinnersNameString(List<String> winners) {
-        StringBuilder builder = new StringBuilder();
-        for (String winner : winners) {
-            builder.append(winner);
-            if (winners.indexOf(winner) < winners.size() - 1) {
-                builder.append(",");
-            }
-        }
-        return builder.toString();
+        String winnersString = String.join(",", winners);
+        System.out.println("최종 우승자 : " + winnersString);
     }
 }
