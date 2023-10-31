eada0373fb60daace714e50f29a47dd5d4a8cabe
HongChaa <jun761_1@naver.com>
2023-10-31 19:42:39 +0900

    feat(Car) : 무작위 값을 구해 전진할 것인지 정지할 것인지 정하는 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private String carName;
+    private int move;
+
+    public Car(String carName, int move) {
+        this.carName = carName;
+        this.move = move;
+    }
+
+    public void carMoveOrStay() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            move++;
+        }
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+    public int getMove() {
+        return move;
+    }
+
+}
