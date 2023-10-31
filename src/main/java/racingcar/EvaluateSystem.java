45dcf9f497dfc87b2a59a415f2ca7125b9cda7f3
fishman1123 <cho.y.j1997@gmail.com>
2023-10-31 23:39:05 +0900

    차량 입력 및 횟수를 통한 배열 생성 구현 완ㄹ료.

--- src/main/java/racingcar/EvaluateSystem.java
+++ src/main/java/racingcar/EvaluateSystem.java
@@ -1,8 +1,17 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 
 public class EvaluateSystem {
-    public void racingCondition() {
-
+    public static ArrayList<Integer> racingCondition(int inputCount) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int carStatus;
+        for (int i = 0; i < inputCount; i++) {
+            temp.add(Randoms.pickNumberInRange(0,9));
+        }
+        return temp;
     }
     public void winnerDisplay() {
 
--- src/main/java/racingcar/RacingSystem.java
+++ src/main/java/racingcar/RacingSystem.java
@@ -1,8 +1,22 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RacingSystem {
-    public void sequence() {
-
+    public static ArrayList<ArrayList<Integer>> carRecordSheet = new ArrayList<>();
+    public static int sequence(int inputCount, int carAmount) {
+        EvaluateSystem raceResult = new EvaluateSystem();
+        for (int i = 0; i < carAmount; i++) {
+            carRecordSheet.add(EvaluateSystem.racingCondition(inputCount));
+        }
+        for (int j = 0; j < carRecordSheet.size(); j++) {
+            for (int k = 0; k < carRecordSheet.get(j).size(); k++) {
+                System.out.println("car number " + j + ": " + carRecordSheet.get(j).get(k));
+            }
+        }
+        return 0;
     }
     public void display() {
 
