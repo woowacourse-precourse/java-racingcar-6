eb218e5c8ed664c3b836091ca0b0a37489a6b5df
SeungGwa123 <skwan123@naver.com>
2023-10-31 13:40:03 +0900

    Feat: Add carDistance
    각 이름의 자동차를 거리 0으로 초기화 하는 함수 작성

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -8,12 +8,16 @@ public class Input {
         Map<String,Integer> parkingLot = null;
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carlist = readLine();
-        // checkCarName 넣어서 맞는 입력 값인지 확인
+        if(!Check.checkCarName(carlist)) throw new IllegalStateException();
         // carDistance 넣어서 거리 0으로 초기화
         return parkingLot;
     }
 
-    public Map<String,Integer> carDistance(Map<String,Integer> parkingLot,String carlist){
+    public Map<String,Integer> carDistance(Map<String,Integer> parkingLot,String carlist) {
+        String[] car_list = carlist.split(",");
+        for (int i = 0; i < car_list.length; i++) {
+            parkingLot.put(car_list[i],0);
+        }
         return parkingLot;
     }
 
