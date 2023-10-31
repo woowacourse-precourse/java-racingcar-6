7a0186fa5158ccb1a5453451ba95a954c011a0e5
LeeJiWon <dlwldnjs1009@gmail.com>
2023-10-31 22:44:54 +0900

    feat: main 메서드 controller run 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.controller.GameController;
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+import racingcar.io.InputReader;
+import racingcar.io.OutputPrinter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        OutputPrinter outputPrinter = new OutputPrinter();
+        GameController gameController = new GameController(inputReader, outputPrinter);
+        gameController.run();
     }
 }
