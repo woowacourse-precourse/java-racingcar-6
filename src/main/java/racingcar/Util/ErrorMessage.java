c6b543a5d96805a1521587b1fc023b1db71a659c
SiHyun-Lee113 <82255633+SiHyun-Lee113@users.noreply.github.com>
2023-10-31 16:36:54 +0900

    feat : 에러 메시지 Enum 생성

---
+++ src/main/java/racingcar/Util/ErrorMessage.java
@@ -0,0 +1,17 @@
+package racingcar.Util;
+
+public enum ErrorMessage {
+    EMPTY("올바르지 않은 입력입니다."),
+    NAME("올바르지 않은 입력입니다. 자동차 이름은 5자 이하만 가능합니다.\n올바른 예) pobi,woni,jun\n"),
+    ATTEMPT("올바르지 않은 입력입니다. 숫자만 입력해 주세요.");
+
+    private final String label;
+
+    ErrorMessage(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
+}
