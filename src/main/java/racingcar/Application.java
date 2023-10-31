707a0a2208390206d7aa96125d835fe05679d976
yang <hjyang@epience.com>
2023-10-31 17:34:41 +0900

    🚀refector(main) : main 함수 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,16 @@
 package racingcar;
 
 import racingcar.domain.RacingGame;
+import racingcar.domain.Winner;
 import racingcar.service.RandomGenerator;
 import racingcar.view.Input.InputParser;
 import racingcar.view.Input.InputValidator;
 import racingcar.view.Input.InputView;
+import racingcar.view.Output.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame(new InputView(new InputParser(), new InputValidator()), new RandomGenerator());
+        RacingGame racingGame = new RacingGame(new InputView(new InputParser(), new InputValidator()),new OutputView(), new RandomGenerator());
         racingGame.goOrStop();
     }
 }
