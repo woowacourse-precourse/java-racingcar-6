6e6854d0e4130fe42fd72ad59441c904d57cee36
parkheeddong <phj7214go@gmail.com>
2023-10-31 20:16:06 +0900

    feat : 회차 입력을 받는 기능 구현

--- src/main/java/racingcar/CountInput.java
+++ src/main/java/racingcar/CountInput.java
@@ -1,2 +1,17 @@
-package racingcar;public class CountInput {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CountInput {
+
+    static private int count;
+
+    static String getCountInput() { // 회차 입력 받는 메서드
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+
+
+
 }
