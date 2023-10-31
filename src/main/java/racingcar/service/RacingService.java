ed482bdf6fac6839b149360149ce2f7162af12a0
Hyun Woo <hwsa10041@gmail.com>
2023-10-31 16:16:30 +0900

    feat : playRacingCar() 메서드 구현
    - 라운드 하나를 진행하는 기능을 수행

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,9 +1,9 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.Car;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RacingService {
     public List<Car> createCarList(String[] carNames) {
@@ -12,5 +12,33 @@ public class RacingService {
             cars.add(Car.createCar(carName));
         }
         return cars;
+    }
+
+    public Map<String, Integer> playRacingCar(List<Car> cars) {
+        Map<String, Integer> result = new LinkedHashMap<>();
+        putRaceMove(cars, result);
+        checkRoundWinner(cars, result);
+        return result;
+    }
+
+    private static void putRaceMove(List<Car> cars, Map<String, Integer> result) {
+        for (Car car : cars) {
+            int move = Randoms.pickNumberInRange(0, 9);
+            result.put(car.getName(), move);
+        }
+    }
+
+    private static void checkRoundWinner(List<Car> cars, Map<String, Integer> result) {
+        int max = Collections.max(result.values());
+        for (Car car : cars) {
+            Integer score = result.get(car.getName());
+            plusWinnerCount(max, car, score);
+        }
+    }
+
+    private static void plusWinnerCount(int max, Car car, Integer score) {
+        if (max == score) {
+            car.plusWinCount();
+        }
     }
 }
