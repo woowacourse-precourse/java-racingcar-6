1227e2f2ee9f5218c8b53868dc184edd7dafa35d
seonghoo1217 <seonghoo1217@naver.com>
2023-10-31 15:34:35 +0900

    fix: modify error property message

--- src/main/java/racingcar/property/ErrorProperty.java
+++ src/main/java/racingcar/property/ErrorProperty.java
@@ -4,7 +4,7 @@ public class ErrorProperty {
 
     public static final String VALUE_IS_EMPTY="[ERROR] 입력값이 공백일 수는 없습니다.";
     public static final String VALUE_CONTAINS_SPACE="[ERROR] 입력값에 공백이 포함되었습니다.";
-    public static final String NAME_VALUE_IS_NOT_CORRECT_FORM="[ERROR] 이름은 한글 혹은 숫자 값만 들어가야 합니다.";
+    public static final String NAME_VALUE_IS_NOT_CORRECT_FORM="[ERROR] 이름은 한글 혹은 영어 값만 들어가야 합니다.";
     public static final String NAME_VALUE_LENGTH_IS_OVER="[ERROR] 이름의 길이는 %s 자리 이하여야합니다.".formatted(ValidateProperty.NAME_LENGTH_STANDARD);
     public static final String NAME_VALUE_IS_DUPLICATE="[ERROR] 이름이 중복되어선 안됩니다";
     public static final String SCORE_VALUE_IS_NOT_NUMERIC="[ERROR] 시도 횟수는 정수로 작성되어야합니다.";
