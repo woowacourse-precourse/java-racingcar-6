4b44391f4e371c4365fce2497aa3e8d6b18c5413
Dendrog <goodshs335@gmail.com>
2023-11-01 04:40:47 +0900

    Feat: MethodTest 추가 및 Validator 메소드에 대해 테스트 완료

--- src/test/java/racingcar/MethodTest.java
+++ src/test/java/racingcar/MethodTest.java
@@ -12,8 +12,75 @@ public class MethodTest {
     void 자동차_공백_입력() {
         String input = "";
         assertThatThrownBy(() -> Validator.checkCarInput(input))
-                .isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("String index out of range: 5");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
     }
+
+    @Test
+    void 자동차_5자_이상_이름_입력() {
+        String input = "abcdefg";
+        assertThatThrownBy(() -> Validator.checkCarInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 자동차_공백_포함() {
+        String input = "a vcd";
+        assertThatThrownBy(() -> Validator.checkCarInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 자동차_쉼표앞_공백() {
+        String input = " ,vcd";
+        assertThatThrownBy(() -> Validator.checkCarInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 자동차_이름_중복() {
+        String input = "test,test";
+        assertThatThrownBy(() -> Validator.checkCarInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 라운드_실수_입력() {
+        String input = "1.3";
+        assertThatThrownBy(() -> Validator.checkRoundInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 라운드_문자_입력() {
+        String input = "aa";
+        assertThatThrownBy(() -> Validator.checkRoundInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 라운드_음수_입력() {
+        String input = "-1";
+        assertThatThrownBy(() -> Validator.checkRoundInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+    @Test
+    void 라운드_0_입력() {
+        String input = "0";
+        assertThatThrownBy(() -> Validator.checkRoundInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Error:");
+    }
+
+
+
 
 }
