a2fa422805ff8e618422f9b5246a93c2b0d95aa0
Kiltae <loverpleasestayo0@gmail.com>
2023-10-31 19:44:08 +0900

    feat: add car name error checking

---
+++ src/main/java/racingcar/utils/CarNameValidator.java
@@ -0,0 +1,29 @@
+package racingcar.utils;
+import java.util.ArrayList;
+
+public class CarNameValidator {
+    public static final int NAME_SIZE = 5;
+    public final ArrayList<String> NAMES;
+
+    public CarNameValidator(String name) {
+        this.NAMES = Util.toArrayList(name);
+        validate();
+    }
+
+    public void validate() {
+        isNameNoDuplicate();
+        isRightNameSize();
+    }
+
+    public void isNameNoDuplicate() {
+        if (NAMES.size() != NAMES.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isRightNameSize() {
+        if (NAMES.stream().anyMatch(name -> name.isEmpty() || name.length() > NAME_SIZE)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
