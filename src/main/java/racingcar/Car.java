7691cc75abbb6471d1ef7023550224a25f0bd34f
choji <waf268@gmail.com>
2023-10-31 17:10:13 +0900

    total_change

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Car {
+    private String carName;
+    private int carLocation;
+    public Car(String carName){
+        this.carName=carName;
+        this.carLocation=0;
+    }
+    public String getCarName(){
+        return this.carName;
+    }
+    public int getCarLocation(){
+        return this.carLocation;
+    }
+    public void setCarLocation(int number){
+        this.carLocation=number;
+    }
+    public void moveCar(){
+        this.carLocation+=1;
+    }
+}
