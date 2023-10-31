d696131510a72da3d25d885fbb3a7cbdeb7ddd4e
yohanii <dygks924@gmail.com>
2023-10-31 23:57:17 +0900

    feat: 우승자 안내 문구 출력

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Racing {
@@ -33,5 +34,10 @@ public class Racing {
     }
 
     public void printWinner() {
+        System.out.print("최종 우승자 : ");
+        List<String> winnerNameList = winnerList.stream()
+                .map(car -> car.getCarName())
+                .collect(Collectors.toList());
+        System.out.println(String.join(", ", winnerNameList));
     }
 }
