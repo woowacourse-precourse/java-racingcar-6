35cc9959f217e74fa5e659dc9db1c5e2e55807ae
Chenowoo <cjsgusdn14@naver.com>
2023-10-31 18:54:14 +0900

    refactor(InputValidation): 메소드 명 변경 및 예외 발생 시 메시지 출력 문구 삽입, 상수 지역변수 선언  - validateNameOfCarsRange 메소드 명을 validateNameOfCarsLength 로 변경  - 각 메소드 마다 에러 발생 시 출력 문구 나오도록 수정  - 사용된 상수들을 클래스 내 지역변수로 선언  - 다른 클래스에서 사용하는 메서드를 import

--- src/main/java/racingcar/InputValidation.java
+++ src/main/java/racingcar/InputValidation.java
@@ -2,19 +2,25 @@ package racingcar;
 
 import java.util.Arrays;
 
+import static racingcar.MessageConst.STANDARD_SAPARATOR;
+import static racingcar.NumberConst.*;
+
 public class InputValidation {
 
+    private static String replaceRegexChar = "[가-힣a-zA-Z0-9,]";
+    private static String replaceReplacementChar = "";
+
     public static void validateInputCorrectSeparator(String userInputCarName) {
-        String deleteAllWords = userInputCarName.replaceAll("[가-힣a-zA-Z0-9,]", "");
+        String deleteAllWords = userInputCarName.replaceAll(replaceRegexChar, replaceReplacementChar);
         if (deleteAllWords.length() != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("이름은 " + STANDARD_SAPARATOR + "로 구분하여 입력하세요.");
         }
     }
 
-    public static void validateNameOfCarsRange(String[] namesOfCars) {
+    public static void validateNameOfCarsLength(String[] namesOfCars) {
         for (String name : namesOfCars) {
-            if (name.length() < NumberConst.MIN_NAME_OF_CAR_LENGTH || name.length() > NumberConst.MAX_NAME_OF_CAR_LENGTH) {
-                throw new IllegalArgumentException();
+            if (name.length() < MIN_NAME_OF_CAR_LENGTH || name.length() > MAX_NAME_OF_CAR_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_OF_CAR_LENGTH + "자 이하로 입력하세요.");
             }
         }
     }
@@ -22,13 +28,13 @@ public class InputValidation {
     public static void validateDuplicateName(String[] namesOfCars) {
         Object[] deleteDuplicateName = Arrays.stream(namesOfCars).distinct().toArray();
         if (deleteDuplicateName.length != namesOfCars.length) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복되는 자동차 이름이 있습니다.");
         }
     }
 
     public static void validateMoveCountRange(int carMovingCount) {
-        if (carMovingCount < NumberConst.MOVING_MIN_COUNT || carMovingCount > NumberConst.MOVING_MAX_COUNT) {
-            throw new IllegalArgumentException();
+        if (carMovingCount < MOVING_MIN_COUNT || carMovingCount > MOVING_MAX_COUNT) {
+            throw new IllegalArgumentException("이동 횟수는" +  MOVING_MAX_COUNT + "회 이하로 입력하세요.");
         }
     }
 }
