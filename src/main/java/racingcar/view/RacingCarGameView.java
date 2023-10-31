2f55eac489a8528089ed5c7a5ced31cedb897bd0
김관진 <cozyschooll@gmail.com>
2023-10-31 22:34:57 +0900

    Chore: outputView파일 이름 변경 outputView에서 RacingCarGameView로 이름 변경

--- src/main/java/racingcar/view/RacingCarGameView.java
+++ src/main/java/racingcar/view/RacingCarGameView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import java.util.List;
 import racingcar.model.Player;
 
-public class outputView {
+public class RacingCarGameView {
 
     private static final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_NUMBER = "시도할 회수는 몇회인가요?";
