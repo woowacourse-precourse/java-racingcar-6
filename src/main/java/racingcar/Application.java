0a9464937758a170a93ed1861d9e3cca28be76bd
suhyun__ <thebbbb1206@gmail.com>
2023-10-31 22:33:50 +0900

    feat: 게임시작 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.CarGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarGame carGame = new CarGame();
+        carGame.start();
     }
 }
