880869e524a64b384f5c34cb877bf556e56b4335
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-10-31 19:18:15 +0900

    feat : Verify that the random number is 4 or more
    해당 랜덤 수가 4이상인지 확인하는 isForward() 함수 정의

--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -5,10 +5,17 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class RandomNumber {
     private int randomNum;
 
-    RandomNumber(){
+    RandomNumber() {
     }
 
-    public void make(){
-        this.randomNum = pickNumberInRange(0,9);
+    public void make() {
+        this.randomNum = pickNumberInRange(0, 9);
+    }
+
+    public boolean isForward() {
+        if (this.randomNum >= 4) {
+            return true;
+        }
+        return false;
     }
 }
