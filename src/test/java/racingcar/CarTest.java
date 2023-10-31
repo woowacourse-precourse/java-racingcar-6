499223fc4ac4853fc0b9bfadf7e84f7a991abbf7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-31 19:52:20 +0900

    test: 포지션에 맞는 출력값 반환 테스트 추가
    포지션에 맞는 출력값 반환 테스트 추가
    - attempt를 5번 수행
    - Car의 Position 값에 맞는 출력값 생성
    - 실제 toString 값과 맞는지 비교

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -11,6 +11,7 @@ class CarTest {
     private static final String NAME_EXCEEDS_LENGTH_LIMIT = "longCar";
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static final String CAR_FORMAT = "%s : %s";
 
 
     @Test
@@ -48,5 +49,21 @@ class CarTest {
                 },
                 STOP
         );
+    }
+
+    @Test
+    void 포지션에_맞는_출력값_반환() {
+        Car car = new Car(NAME_UNDER_LENGTH_LIMIT);
+        for (int attempt = 1; attempt <= 5; attempt++) {
+            car.attempt();
+        }
+        int position = car.getPosition();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int positionIndex = 1; positionIndex <= position; positionIndex++) {
+            stringBuilder.append("-");
+        }
+        Assertions.assertThat(car.toString())
+                .isEqualTo(String.format(CAR_FORMAT, car.getName(), stringBuilder.toString()));
+
     }
 }
