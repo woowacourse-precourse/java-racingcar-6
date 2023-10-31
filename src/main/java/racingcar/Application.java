5dd954e7e7489b8c1cf9b6d298b78eec1d3d2967
KwangHo <skh990427@naver.com>
2023-10-31 23:46:08 +0900

    refactor: 시도할 회수에 숫자가 아닌 값이 입력된 경우 예외처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -24,9 +24,15 @@ public class Application {
 
         System.out.println("시도할 회수는 몇회인가요?");
 
-        int gameCount = Integer.parseInt(Console.readLine());
-        if (gameCount <= 0) {
-            throw new IllegalArgumentException("시도할 회수는 0 이하일 수 없습니다.");
+        int gameCount;
+
+        try {
+            gameCount = Integer.parseInt(Console.readLine());
+            if (gameCount <= 0) {
+                throw new IllegalArgumentException("시도할 회수는 0 이하일 수 없습니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
         }
 
         System.out.println("\n실행 결과");
