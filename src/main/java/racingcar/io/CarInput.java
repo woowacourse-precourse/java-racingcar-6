7d6cf19bbf659187d3224a147abbf15776aff861
emost22 <emoney96@naver.com>
2023-11-01 04:20:39 +0900

    Refactor: Add io package input

---
+++ src/main/java/racingcar/io/CarInput.java
@@ -0,0 +1,20 @@
+package racingcar.io;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.CarUtil;
+import racingcar.validator.CarInputValidator;
+import racingcar.validator.RoundInputValidator;
+
+public class CarInput {
+    public static String[] getCarNames() {
+        String[] carNames = Console.readLine().split(CarUtil.CAR_NAME_SPLIT);
+        CarInputValidator.checkCarNameDuplicate(carNames);
+        return carNames;
+    }
+
+    public static int getRound() {
+        String round = Console.readLine();
+        RoundInputValidator.checkRoundNumberFormat(round);
+        return Integer.parseInt(round);
+    }
+}
--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,11 +1,8 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.data.CarRepository;
+import racingcar.io.CarInput;
 import racingcar.io.CarOutput;
-import racingcar.utils.CarUtil;
-import racingcar.validator.CarInputValidator;
-import racingcar.validator.RoundInputValidator;
 
 public class CarService {
     private final CarRepository carRepository;
@@ -23,16 +20,12 @@ public class CarService {
 
     private void getCarList() {
         CarOutput.printGetCarList();
-        String[] carNames = Console.readLine().split(CarUtil.CAR_NAME_SPLIT);
-        CarInputValidator.checkCarNameDuplicate(carNames);
-        carRepository.setCarList(carNames);
+        carRepository.setCarList(CarInput.getCarNames());
     }
 
     private void getRound() {
         CarOutput.printGetRound();
-        String roundString = Console.readLine();
-        RoundInputValidator.checkRoundNumberFormat(roundString);
-        carRepository.setRound(Integer.parseInt(roundString));
+        carRepository.setRound(CarInput.getRound());
     }
 
     private void playRound() {
