478e477895d569d4a6b2dac4dee9028a1b540ab5
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-31 16:55:06 +0900

    test(Car): 전진 기능 테스트 구현

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,9 +2,11 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.exception.car.BlankCarNameException;
 import racingcar.exception.car.CarNameRangeException;
+import racingcar.model.number.InputNumberGenerator;
 
 class CarTest {
 
@@ -22,5 +24,15 @@ class CarTest {
         assertThatThrownBy(() -> Car.createCar(" "))
                 .isInstanceOf(BlankCarNameException.class)
                 .hasMessage("자동차 이름을 입력하세요.");
+    }
+
+    @Test
+    void 숫자가_4이상이면_앞으로_이동() {
+        Car car = Car.createCar("car1");
+        InputNumberGenerator generator = new InputNumberGenerator(4);
+        car.moveForward(generator);
+        car.moveForward(generator);
+
+        Assertions.assertThat(car.getPosition()).isEqualTo(2);
     }
 }
