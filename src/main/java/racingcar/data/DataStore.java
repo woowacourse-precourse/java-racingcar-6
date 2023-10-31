230732bc3d798bc03c16bbb6975a0d8bb9007887
JaehongDev <println@kakao.com>
2023-10-31 12:58:13 +0900

    feat: DataStore 구현

--- src/main/java/racingcar/data/DataStore.java
+++ src/main/java/racingcar/data/DataStore.java
@@ -1,4 +1,28 @@
 package racingcar.data;
 
+import racingcar.domain.ExtractTrack;
+import racingcar.domain.Participants;
+
 public class DataStore {
+
+    private ExtractTrack extractTrack;
+    private Participants participants;
+
+
+    public ExtractTrack saveExtractTrack(ExtractTrack extractTrack) {
+        return this.extractTrack = extractTrack;
+    }
+
+    public Participants saveParticipants(Participants participants) {
+        return this.participants = participants;
+    }
+
+
+    public ExtractTrack findExtractTrack() {
+        return extractTrack;
+    }
+
+    public Participants findParticipants() {
+        return participants;
+    }
 }
