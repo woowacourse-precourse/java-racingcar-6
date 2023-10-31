0e93dcdc3bf65a48b44fa89f082b7e98d319b0aa
seona-moon <fhflwhwl5@gmail.com>
2023-10-31 10:46:25 +0900

    feat: 우승자를 선별하는 기능 메서드 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -23,4 +23,24 @@ public class Cars {
       car.setMovement();
     }
   }
+
+  public List<String> getWinnerList() {
+    List<String> winnerList = calculateWinner();
+    return winnerList;
+  }
+
+  private List<String> calculateWinner() {
+    List<String> winnerList = new ArrayList<>();
+    int max = Integer.MIN_VALUE;
+    for (Car car : carList) {
+      if (car.getMovement() > max) {
+        max = car.getMovement();
+        winnerList.clear();
+        winnerList.add(car.getName());
+      } else if (car.getMovement() == max) {
+        winnerList.add(car.getName());
+      }
+    }
+    return winnerList;
+  }
 }
