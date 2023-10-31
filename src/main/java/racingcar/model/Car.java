034321eb3915746ea465a164dce9b07b80b3f968
gourderased <gourderased@gmail.com>
2023-11-01 01:10:16 +0900

    "feat(racingcar): 무작위 값이 4 이상일 경우 전진하는 기능 구현"

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,7 +1,10 @@
 package racingcar.model;
 
+import racingcar.utils.RandomNumberGenerator;
+
 public class Car {
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -9,5 +12,15 @@ public class Car {
 
     public String getName() {
         return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void move() {
+        if(RandomNumberGenerator.generateRandomNumber() >= 4) {
+            this.position++;
+        }
     }
 }
