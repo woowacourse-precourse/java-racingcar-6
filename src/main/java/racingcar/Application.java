93bb6e3dbc082fe1c323b145d55298deeed39595
Jiihyun <cwlgusc@gmail.com>
2023-10-31 17:59:39 +0900

    feat(GameController): 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.domain.MovePossibilityChecker;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.io.InputReader;
+import racingcar.io.OutputWriter;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //TODO: gitignore - .class 파일 어케해?
+        InputReader reader = new InputReader();
+        OutputWriter writer = new OutputWriter();
+        InputView inputView = new InputView(reader, writer);
+        OutputView outputView = new OutputView(writer);
+        MovePossibilityChecker movePossibilityChecker = new MovePossibilityChecker(new RandomNumberGenerator());
+        GameController gameController = new GameController(inputView, outputView, movePossibilityChecker);
+        gameController.start();
     }
 }
--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,37 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.domain.MovePossibilityChecker;
+import racingcar.domain.dto.output.CarsDto;
+import racingcar.domain.dto.output.WinnersDto;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final MovePossibilityChecker movePossibilityChecker;
+
+    public GameController(InputView inputView, OutputView outputView, MovePossibilityChecker movePossibilityChecker) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.movePossibilityChecker = movePossibilityChecker;
+    }
+
+    public void start() {
+        Cars cars = inputView.getCars();
+        int tryTimes = inputView.getTrytimes();
+        WinnersDto winnersDto = printGameResult(cars, tryTimes);
+        outputView.printWinner(winnersDto);
+    }
+
+    private WinnersDto printGameResult(Cars cars, int tryTimes) {
+        outputView.printGameResultMsg();
+        for (int i = 0; i < tryTimes; i++) {
+            cars.moveCarsBy(movePossibilityChecker);
+            CarsDto carsDto = cars.toCarsDto();
+            outputView.printGameResult(carsDto);
+        }
+        return cars.toCarsDto().getWinners();
+    }
 }
