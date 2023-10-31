522eabca2eb198efdf2f9278ba91bea7c4b86cbc
Jin409 <sualng123@gmail.com>
2023-10-31 19:07:13 +0900

    [TEST] 자동차가 전진하는 경우 위치가 1 증가하는지 확인하는 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,14 +1,23 @@
 package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
 public class CarTest {
 
     private final static int MAX_NAME_LEGNTH = 5;
+
+    private Car car;
+
+    @BeforeAll
+    void setUp() {
+        car = new Car("name");
+    }
 
     @Test
     public void 이름이_5글자가_넘어가는_경우_예외_발생() {
@@ -29,4 +38,14 @@ public class CarTest {
         String validName = stringBuilder.toString();
         assertDoesNotThrow(() -> new Car(validName));
     }
+
+    @Test
+    void 자동차가_전진하는_경우_location이_증가() {
+        car.moveForward();
+        int expected = 1;
+
+        assertEquals(expected, car.getLastLocation());
+    }
+
+
 }
