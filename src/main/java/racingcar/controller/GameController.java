59be2d1f2501e30e2254ec9f8a303f3ffe3800a4
ohjuyoungg <ejrm12@naver.com>
2023-10-31 20:12:57 +0900

    refactor(GameController) : 코드 호출 구조 변경하여 리팩토링 진행

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,17 +3,29 @@ package racingcar.controller;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
-import racingcar.domain.Cars;
-import racingcar.util.GoStopCar;
+import racingcar.util.Cars;
+import racingcar.util.RandomNumbers;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 
-    public void playGame(String carNamesInput, int numberOfAttempts) {
+    public void play() {
+        // 1. 게임 시작 시 안내 메시지를 출력한다.
+        OutputView.printGameStartMessage();
+
+        // 2. 게임 진행에 필요한 값들을 사용자로부터 입력 받는다.
+        List<String> carNames = InputView.inputCarName();
+        int numberOfAttempts = InputView.inputTryCount();
+        String carNamesInput = String.join(",", carNames);
+
+        // 3. 입력받은 값을 기반으로 Car 객체 리스트를 생성하고, 게임을 진행한다.
         List<Car> cars = createCars(carNamesInput);
         for (int attempt = 0; attempt < numberOfAttempts; attempt++) {
             moveCars(cars);
         }
+
+        // 4. 우승자를 결정하고 사용자에게 출력한다.
         List<String> winners = determineWinners(cars);
         OutputView.printGameResult(cars, numberOfAttempts);
         OutputView.printWinners(winners);
@@ -29,7 +41,7 @@ public class GameController {
     }
 
     private void moveCar(Car car) {
-        int randomNumber = GoStopCar.pickNumberInRange();
+        int randomNumber = RandomNumbers.pick();
         if (randomNumber >= 4) {
             car.moveForward();
         }
