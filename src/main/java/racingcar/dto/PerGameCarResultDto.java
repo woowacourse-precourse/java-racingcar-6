9d32d8fd2459aeedf59a5cc6ab9457a4d5ca6f4a
limzzum <chfhddl1234@naver.com>
2023-11-01 01:39:02 +0900

    :hammer: refactor : DTO 간편 사용 위한 record 로 변환

--- src/main/java/racingcar/dto/PerGameCarResultDto.java
+++ src/main/java/racingcar/dto/PerGameCarResultDto.java
@@ -1,19 +1,5 @@
 package racingcar.dto;
 
-public class PerGameCarResultDto {
-    private String carName;
-    private int moveCnt;
-    
-    public PerGameCarResultDto(String carName, int moveCnt){
-        this.carName = carName;
-        this.moveCnt = moveCnt;
-    }
+public record PerGameCarResultDto(String carName, int moveCnt) {
 
-    public String getCarName() {
-        return carName;
-    }
-
-    public int getMoveCnt() {
-        return moveCnt;
-    }
 }
