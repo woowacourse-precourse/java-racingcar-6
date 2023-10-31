8f2ac99671032535de166b7d656d2596bf682655
kyeoungchan <wkc1002@naver.com>
2023-10-31 19:11:55 +0900

    feat(racingcar): 각 라운드 별 DTO(SingleRoundResultDto) 기능 구현

--- src/main/java/racingcar/domain/SingleRoundResultDto.java
+++ src/main/java/racingcar/domain/SingleRoundResultDto.java
@@ -1,2 +1,16 @@
-package racingcar.domain;public class SingleRoundResultDto {
+package racingcar.domain;
+
+import java.util.List;
+
+public class SingleRoundResultDto {
+
+    private final List<Car.CarResultDto> carResultDtoList;
+
+    public SingleRoundResultDto(List<Car.CarResultDto> carResultDtoList) {
+        this.carResultDtoList = carResultDtoList;
+    }
+
+    public Car.CarResultDto getCarResultDto(int index) {
+        return carResultDtoList.get(index);
+    }
 }
--- src/test/java/racingcar/domain/SingleRoundResultDtoTest.java
+++ src/test/java/racingcar/domain/SingleRoundResultDtoTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SingleRoundResultDtoTest {
-  
+    private static BigInteger idProvider = BigInteger.ZERO;
+
+    private static BigInteger generateId() {
+        return idProvider = idProvider.add(BigInteger.ONE);
+    }
+
+    @Test
+    @DisplayName("SingleRoundResultDto에는 각 차마다의 CarResultDto를 저장하고 있다.")
+    void getCarResultDto() {
+        // given
+        Car pobiCar = Car.createCar(generateId(), "pobi");
+        Car woniCar = Car.createCar(generateId(), "woni");
+        Car junCar = Car.createCar(generateId(), "jun");
+
+        Car.CarResultDto pobiCarResult = pobiCar.createCarResultDto();
+        Car.CarResultDto woniCarResult = woniCar.createCarResultDto();
+        Car.CarResultDto junCarResult = junCar.createCarResultDto();
+        // when
+        List<Car.CarResultDto> carResults = Arrays.asList(
+                pobiCarResult, woniCarResult, junCarResult
+        );
+        SingleRoundResultDto singleRoundResult = new SingleRoundResultDto(carResults);
+
+        // then
+        assertThat(singleRoundResult.getCarResultDto(0))
+                .isEqualTo(pobiCarResult);
+        assertThat(singleRoundResult.getCarResultDto(1))
+                .isEqualTo(woniCarResult);
+        assertThat(singleRoundResult.getCarResultDto(2))
+                .isEqualTo(junCarResult);
+    }
 }
