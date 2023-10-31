dbc47b0f2de3cefcfcc0c3c4a9969c912bf920c1
gyuoo <yeomky98@naver.com>
2023-11-01 03:10:41 +0900

    feat: set initial position to 0 for each car

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    public String name;
+    public int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+}
