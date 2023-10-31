3ea5b53bdf275eda1e8f651c6a53832ed130e9ae
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-31 21:29:35 +0900

    Feat[domain] 자동차 필드 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,11 @@
 package racingcar.domain;
 
 public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name){
+        this.name = name;
+        this.position = 0;
+    }
 }
