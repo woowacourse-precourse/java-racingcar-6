8ad51f5372aa06d301e9f92bb7cb8d42fcbfbe3b
honeydanji <tjdwls3680@naver.com>
2023-10-31 23:07:00 +0900

    TEST : 자동차 이름 테스트 코드 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -8,11 +8,19 @@ import java.util.List;
 public class CarTest {
 
     @Test
+    void 자동차_이름_Split_리스트_테스트() {
+        Car car = new Car();
+        String carsName = "dokgo,may,cat,uncle,king";
+
+        assertThat(car.carsNameSplit(carsName)).containsExactly("dokgo", "may", "cat", "uncle", "king");
+    }
+
+    @Test
     void 자동차_Set_Get_테스트() {
         Car car = new Car();
 
-        car.setCar(List.of("dokgo", "may", "cat", "uncle", "king"));
+        car.setCarName(List.of("dokgo", "may", "cat", "uncle", "king"));
 
-        assertThat(car.getCar()).isEquals("dokgo", "may", "cat", "uncle", "king");
+        assertThat(car.getCarName()).isEquals("dokgo", "may", "cat", "uncle", "king");
     }
 }
