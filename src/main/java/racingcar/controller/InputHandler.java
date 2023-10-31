3ef07feecef2b4166050fe07197a5b5d85d84b55
seastamp <seastamp10@gmail.com>
2023-10-31 18:20:44 +0900

    feat: 사용자에게 입력받은 값을 숫자로 변환하는 기능 구현

--- src/main/java/racingcar/controller/InputHandler.java
+++ src/main/java/racingcar/controller/InputHandler.java
@@ -2,22 +2,35 @@ package racingcar.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class InputHandler {
+    private static final String NUMBER_REG = "^[0-9]*$";
+
     private InputHandler() {
     }
 
     public static List<String> parseInputToList(String input) throws IllegalArgumentException {
         List<String> parsedList;
+        
         parsedList = splitInputByComma(input);
         if (!validateCarNameLength(parsedList)) {
-            throw new IllegalArgumentException("잘못된 입력 값입니다");
+            throw new IllegalArgumentException("잘못된 입력 값입니다 자동차 이름을 5자리 이하로 입력해주세요.");
         }
         return parsedList;
     }
 
-    public static int parseInputToIntager(String input) {
+    public static int parseInputToIntager(String input) throws IllegalArgumentException {
         int parsedInt = 0;
+
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다. 숫자를 입력해주세요.");
+        }
+        parsedInt = Integer.parseInt(input);
+
+        if (isPositiveInteger(parsedInt)) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다. 자연수를 입력해주세요.");
+        }
         return parsedInt;
     }
 
@@ -26,16 +39,20 @@ public class InputHandler {
     }
 
     private static boolean validateCarNameLength(List<String> carNames) {
-        int endinclusive = 5;
+        int endInclusive = 5;
         for (String carName : carNames) {
-            if (carName.length() <= endinclusive) {
+            if (carName.length() <= endInclusive) {
                 return false;
             }
         }
         return true;
     }
 
+    private static boolean isNumber(String input) {
+        return Pattern.matches(input, NUMBER_REG);
+    }
+
     private static boolean isPositiveInteger(int num) {
-        return false;
+        return num > 0;
     }
 }
