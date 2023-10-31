9a0b9e99be5f5a6eb98f00bbc2708b3d6632c4ac
bbbang105 <2018111366@dgu.ac.kr>
2023-11-01 05:43:34 +0900

    feat : 출력 안내 메세지 출력 기능 구현
    각 상황에 맞는 메세지(문자열)를 출력할 수 있도록 메소드를 구현

--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -1,4 +1,8 @@
 package racingcar.view;
 
-public class OutputMessage {
+public final class OutputMessage {
+    private static final String OUTPUT_RESULT_TEXT_MESSAGE = "실행 결과";
+    public static void outputResultText() {
+        System.out.println(OUTPUT_RESULT_TEXT_MESSAGE);
+    }
 }
