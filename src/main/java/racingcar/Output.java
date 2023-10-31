2e0e94028280ecc0f8a58328158b861fbe004be2
suhoon <jobak4200nim@nate.com>
2023-10-31 18:17:41 +0900

    feat: output 기능 추가

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Output {
+
+
+
+    // 커맨드 출력
+    public String startOutput() {
+        return "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    }
+
+    // 시도할 횟수 출력
+    public String timeOutput() {
+        return "시도할 회수는 몇회인가요?";
+    }
+
+    // 상황 출력 //TODO
+    
+
+
+    // 최종 우승자 출력 //TODO
+
+}
