b7df489eeb2966f0bf4d6fa133c13ac267a0b8cd
noyes5 <noyes5@naver.com>
2023-10-31 16:42:16 +0900

    refactor: carNames 분리를 StringUtils로 이동

--- src/main/java/racingcar/util/StringUtils.java
+++ src/main/java/racingcar/util/StringUtils.java
@@ -2,6 +2,10 @@ package racingcar.util;
 
 public class StringUtils {
     public static boolean isBlank(String name) {
-        return name == null || name.trim().isEmpty();
+        return name == null || name.strip().isEmpty();
+    }
+
+    public static String[] splitByDelimiter(String carNames) {
+        return carNames.split(",");
     }
 }
