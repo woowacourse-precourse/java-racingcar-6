be10289cb47d225ba0ccef9efe04c3e14effbcd0
daeun Kong <daeun113@ajou.ac.kr>
2023-11-01 01:07:32 +0900

    feat(printResult): 차수별 실행 결과 출력

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -2,11 +2,17 @@ package racingcar;
 
 public class RacingCar {
     private String name;
+    private int score;
     public RacingCar(String name) {
         this.setName(name);
     }
-
     private void setName(String name) {
+    }
+    String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
     }
     int move(int randomNumber) {
         return 0;
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -48,6 +48,19 @@ public class RacingCarGame {
     }
 
     void printResult() {
+        for (RacingCar player : racingCars) {
+            printPlayerScore(player);
+        }
+    }
+
+    private void printPlayerScore(RacingCar player) {
+        StringBuffer playerScore = new StringBuffer();
+        playerScore.append(player.getName());
+        playerScore.append(" : ");
+        for(int i=0; i<player.getScore(); i++) {
+            playerScore.append("-");
+        }
+        System.out.println(playerScore);
     }
 
     void printWinners() {
