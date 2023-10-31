90b55ac675030ff99f04c8ea375ff90df120618c
kwon <kckc0608@naver.com>
2023-11-01 00:51:05 +0900

    feat: 시도 횟수 입력 안내 문구 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Application {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static void main(String[] args) {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
         String carNameInput = camp.nextstep.edu.missionutils.Console.readLine();
         List<String> carNames = Arrays.stream(carNameInput.split(",")).toList();
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
     }
 }
