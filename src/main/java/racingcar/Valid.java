2bfc08eda1eec70097091f8cd57709436f2dd5ff
LUKR7Q <ybkim0628@naver.com>
2023-10-31 12:32:50 +0900

    기능(validator): Validator 클래스에 isNull() 메서드 추가

--- src/main/java/racingcar/Valid.java
+++ src/main/java/racingcar/Valid.java
@@ -1,4 +1,15 @@
 package racingcar;
 
+import java.util.*;
+
 public class Valid {
+
+    private static final String ERROR_MSG = "[ERROR] ";
+
+    public static void isNull(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MSG + "입력 값이 공백입니다.");
+        }
+    }
+
 }
