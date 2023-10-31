fe878229b156bdfddbab88362136146ab1df66eb
706com <ehdgusjdbswl@gmail.com>
2023-10-31 23:10:21 +0900

    style : 변수 및 메서드명 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,25 +5,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private String name;
 
-    private int remainMoveCount;
+    private int RemainingOpportunity;
 
-    private int score;
+    private int movementScore;
 
     private static final int START_NUMBER = 0;
     private static final int END_NUMBER = 9;
     private static final int POSSIBLE_TO_MOVE_NUMBER = 4;
 
-    public Car(String name, int moveCount) {
+    public Car(String name, int movementOpportunity) {
         this.name = name;
-        this.remainMoveCount = moveCount;
-        this.score = 0;
+        this.RemainingOpportunity = movementOpportunity;
+        this.movementScore = 0;
     }
 
-    public void getChanceToMove(){
+    public void tryToGetMovementScore(){
         if(pickRandomNumberToMove()>=POSSIBLE_TO_MOVE_NUMBER) {
-            this.score++;
+            this.movementScore++;
         }
-        this.remainMoveCount--;
+        this.RemainingOpportunity--;
     }
 
     protected int pickRandomNumberToMove(){
@@ -33,11 +33,10 @@ public class Car {
     public String getName(){
         return name;
     }
-    public int getScore(){
-        return score;
-    }
 
-    public int getRemainMoveCount(){
-        return remainMoveCount;
+    public int getMovementScore(){ return movementScore; }
+
+    public int getRemainingOpportunity(){
+        return RemainingOpportunity;
     }
 }
