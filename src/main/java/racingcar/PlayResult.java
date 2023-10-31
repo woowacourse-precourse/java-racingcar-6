cd338b37fd0c37812bf08c36eb74e207609f333c
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-31 16:11:20 +0900

    FIX: 전진 기준 오류 수정 - 랜덤숫자가 4 이상일 경우 전진으로 수정

--- src/main/java/racingcar/PlayResult.java
+++ src/main/java/racingcar/PlayResult.java
@@ -6,7 +6,7 @@ public class PlayResult {
     public void getResult(String[] names, String[] results){
         for (int i = 0; i < names.length; i++) {
             int r = Randoms.pickNumberInRange(0, 9);
-            if (r > 4)
+            if (r >= 4)
                 results[i] += "-";
             System.out.println(results[i]);
         }
