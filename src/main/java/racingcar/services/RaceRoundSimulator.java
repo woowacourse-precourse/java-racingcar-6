bfbbacbc939d8c7a8fdeab68f8db9808088c778c
minjong kim <godminjong@naver.com>
2023-10-31 16:51:50 +0900

    feat: 각각의 차가 각 횟수마다 전진하는 정도를 난수를 통해 추가하는 기능 추가
    RaceGameState.class를 통해 Map<자동차 이름, 전진 횟수>을 받아와 4이상의 난수일 경우 전진횟수를 1 추가하고 해당 라운드의 결과를 콘솔로 출력해주는 기능 구현.

--- src/main/java/racingcar/services/RaceRoundSimulator.java
+++ src/main/java/racingcar/services/RaceRoundSimulator.java
@@ -1,11 +1,25 @@
 package racingcar.services;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Map;
 
 public class RaceRoundSimulator {
 
+    public static void execute(RaceGameState state) {
+        Map<String, Integer> stateOfEachCar = state.getStateOfEachCar();
+        stateOfEachCar
+                .keySet()
+                .stream()
+                .filter(targetName -> goForward())
+                .forEach(state::moveForwardByName);
+
+        printCurrentProgress(state);
+    }
 
 
+    private static void printCurrentProgress(RaceGameState currentState) {
+        System.out.println(currentState);
+    }
 
     private static int generateZeroToNineRandomValue() {
         return Randoms.pickNumberInRange(0, 9);
