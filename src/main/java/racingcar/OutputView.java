0e40c081a9eb03f1971c2d0c94b75f22ce5b6270
HyungHoKim <hkim1109@naver.com>
2023-10-31 10:11:34 +0900

    refactor(main): \n 제거
    OutputView에서 가독성 증가를 위해 \n을 println(); 으로 변경

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class OutputView {
     public static void printExecutionResultInKorean() {
-        System.out.println("\n실행 결과");
+        System.out.println();
+        System.out.println("실행 결과");
     }
 
     public static void printCarMovement(String name, int movedLately) {
@@ -12,6 +13,7 @@ public class OutputView {
     }
 
     public static void printResult(List<String> winnerCarNames) {
-        System.out.print("\n최종 우승자 : " + String.join(", ", winnerCarNames));
+        System.out.println();
+        System.out.print("최종 우승자 : " + String.join(", ", winnerCarNames));
     }
 }
