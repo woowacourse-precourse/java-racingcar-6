e74fcbcc904a4c0a1665016db2fb12aa8b14e53a
Jaeho Kim <fldzm7905@gmail.com>
2023-10-31 22:28:03 +0900

    feat: 경기시작 구현

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,16 +1,27 @@
 package racingcar.domain;
 
+import java.util.List;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Race {
+    private List<Car> carList;
+    private int round = 0;
+    private boolean isRacing = true;
+    OutputView outputView = new OutputView();
 
-
-    private boolean ableMoveForward() {
-
-        return false;
+    public void raceStart() {
+        InputView inputView = new InputView();
+        carList = Car.generate(inputView.inputName());
+        outputView.printStartMessage();
+        outputView.askNumOfAttemptsMessage();
+        round = inputView.inputRound();
     }
-    private int rollDice() {
 
-        return 0;
+    private void duringRace() {
+        for (Car car : carList) {
+            car.ableMoveForward();
+        }
     }
-
 
 }
