7e918538974d36d88e13b15b3cca6caa63ae2a74
Nbowow <nbo5548@naver.com>
2023-10-31 19:37:09 +0900

    Refactor: OutputView 2023.10.31 로직이 있는 부분 RaceService클래스로 이동 및 코드 간결화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,28 +1,20 @@
 package racingcar.view;
 
-import java.util.List;
-
 public class OutputView {
 
     public static void responseExecution() {
         System.out.println("실행 결과");
     }
 
-    public static void responseWinner(List<String> carNames, List<Integer> carPresentPoint) {
-        int max = Integer.MIN_VALUE;
-        String answer = "";
-        for (int i = 0; i < carPresentPoint.size(); i++) {
-            max = Math.max(max, carPresentPoint.get(i));
+    public void outputOfExecution(int[] carPresentPoint, String[] carName) {
+        for (int i = 0; i < carName.length; i++) {
+            System.out.println(carName[i] + " : " + "-".repeat(carPresentPoint[i]));
         }
-
-        for (int i = 0; i < carNames.size(); i++) {
-            if (carPresentPoint.get(i) == max) {
-                answer += carNames.get(i);
-                answer += ", ";
-            }
-        }
-
-        System.out.println("최종 우승자 : " + answer);
-
+        System.out.println("");
     }
+
+    public void responseWinner(String answer) {
+        System.out.println("최종 우승자 : " + answer);
+    }
+
 }
