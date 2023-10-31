6c432721ba5e0a6cec72169854f60eeb62f93def
JayMon0327 <kioplm0211@naver.com>
2023-10-31 23:21:44 +0900

    각 자동차에 이름부여, 쉼표를 기준으로 구분한다. - splitCarNames

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = splitCarNames(Console.readLine());
+    }
+
+    private static List<String> splitCarNames(String input) {
+        String[] carNamesArray = input.split(",");
+        return List.of(carNamesArray);
     }
 }
