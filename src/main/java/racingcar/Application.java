3b2be6a54dc65d88f3eecc031cd2c89a555da8bf
Minjeong63 <kmj06030@naver.com>
2023-10-31 20:49:17 +0900

    Chore: GameServiceImpl 클래스명 수정으로 인한 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,10 @@
 package racingcar;
 
 import racingcar.game.GameService;
-import racingcar.game.GameServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
-        GameService gameService = new GameServiceImpl();
+        GameService gameService = new GameService();
         gameService.playGame();
     }
 }
