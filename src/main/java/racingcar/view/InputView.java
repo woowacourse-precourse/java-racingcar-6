c38c18c23345a3a8f12d7e825db9b4ebdec6bc60
ehdwoKIM <kinbell19@gmail.com>
2023-11-01 02:00:40 +0900

    [feat] ValidatorTest 추가&메소드 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,10 +2,10 @@ package racingcar.view;
 
 public class InputView {
     public static void requestCarNamesMessage() {
-        System.out.println("자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public static void requestNumberOfTrialMessage() {
-        System.out.println("시도 횟수는 몇회인가요?");
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,97 +1,41 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
-import racingcar.util.Validator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+        assertSimpleTest(
+                () -> {
+                    runException("pobi,javaji");
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
         );
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
-    }
-
-    public static class ValidatorTest {
-        private final Validator validator = new Validator();
-        private static final String ERROR_MESSAGE = "[ERROR]";
-
-        @Test
-        void 자동차이름_빈문자_입력() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput(""))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 시도횟수이름_빈문자_입력() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkNumberOfTrialInput(""))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 자동차이름_1글자미만() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,b,,c"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 자동차이름_5글자초과() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,b,avsdfe"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 자동차이름_공백() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a, ,c"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 자동차이름_중복() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkCarNamesInput("a,a"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
-
-        @Test
-        void 시도횟수_문자() {
-            assertSimpleTest(() -> AssertionsForClassTypes.assertThatThrownBy(() -> validator.checkNumberOfTrialInput("a"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE));
-        }
     }
 }
