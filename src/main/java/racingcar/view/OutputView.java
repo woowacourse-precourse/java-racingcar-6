0d03bd925a06b241e39ff948df2225a7b91076b8
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 03:31:32 +0900

    feat: 경기 안내문 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printEnterCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printEnterMoveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printExecResult() {
+        System.out.println("실행 결과");
+    }
+}
