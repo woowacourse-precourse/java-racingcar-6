dc025111ead391f93725ad00f67d336bac5f7ff8
soyoungxxx <otherplet@gmail.com>
2023-11-01 18:32:11 +0900

    test(racingcar): 랜덤 숫자 생성 테스트 코드 작성

--- src/test/java/racingcar/domain/NumberGeneratorTest.java
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -6,5 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberGeneratorTest {
+    @DisplayName("랜덤 번호 생성 테스트")
+    @Test
+    void createRandomNumberTest() {
+        // 0~9 사이 번호인지 테스트
+        NumberGenerator num = new NumberGenerator();
+        int randomNumber = num.createRandomNumber();
 
+        assertTrue(randomNumber <= 9 && randomNumber >= 0);
+    }
 }
