919f80887c67501b6f403fd4e3714e0a600d8456
DonghaOh <donghadarwin@naver.com>
2023-11-01 15:52:37 +0900

    feat: 이름 입력 예외 중복, 0글자인 경우 - isNameRightSize 메소드

--- src/main/java/racingcar/model/nameValidator.java
+++ src/main/java/racingcar/model/nameValidator.java
@@ -7,8 +7,16 @@ public class nameValidator {
     public static void namesException(String names){
         isNamesNull(names);
         List<String> nameList = List.of(names.split(","));
+        isNamesRightSize(nameList);
     }
     public static void isNamesNull(String names) {
         if (names.isEmpty()) throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+    }
+
+    public static void isNamesRightSize(List<String> nameList) {
+        for (String name : nameList) {
+            if (name.length()>5) throw new IllegalArgumentException("이름은 5자 이하만 가능합니다");
+            if (name.isEmpty()) throw new IllegalArgumentException("쉼표를 올바르게 사용해주세요");
+        }
     }
 }
