7dc1868937769366348becf8907c9c7a7875a216
MyStraw <kikongdosa@gmail.com>
2023-11-01 14:29:07 +0900

    feat(Application): 게임을 초기화 하고 시작하는 메서드 기능구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleView view = new ConsoleView();
+        GameController controller = new GameController(view);
+        controller.startGame();
     }
 }
