49254b99cc07d8a07abb965093e9a6e823143eeb
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-01 22:55:52 +0900

    [FIX] AfterEach 스캐너 닫기

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -16,6 +18,11 @@ import java.util.List;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @AfterEach
+    void 스캐너_닫기(){
+        Console.close();
+    }
 
     @Test
     void 전진_정지() {
@@ -43,9 +50,9 @@ class ApplicationTest extends NsTest {
 
     @Test
     public void 자동차_이름_분리_테스트() {
-        ByteArrayInputStream input = new ByteArrayInputStream("자동차1,자동차2,자동차3\n".getBytes());
+        ByteArrayInputStream input = new ByteArrayInputStream("Car1,Car2,Car3".getBytes());
         System.setIn(input);
         String[] carNames = Application.getCarNames();
-        assertThat(carNames).containsExactly("자동차1", "자동차2", "자동차3");
+        assertThat(carNames).containsExactly("Car1", "Car2", "Car3");
     }
 }
