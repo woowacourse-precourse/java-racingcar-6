d329797a1662e3b07dfefb6f307681b814536999
kongji <kongji4092@gmail.com>
2023-11-01 13:27:50 +0900

    refactor: 최종 우승자 메시지 상수로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     public static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ASK_GAME_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String START_GAME_ROUND_MESSAGE = "실행결과";
+    public static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void printStartGame(){
         System.out.println(START_GAME_MESSAGE);
@@ -25,6 +26,6 @@ public class OutputView {
     }
 
     public static void printGameWinner(String gameWinner) {
-        System.out.print("최종 우승자 : " + gameWinner);
+        System.out.print(PRINT_WINNER_MESSAGE + gameWinner);
     }
 }
