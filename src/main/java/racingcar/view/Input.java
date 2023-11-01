a422f956e31d2c54fb7477e30b0d11f1f0b4106c
Han Ju Kim <hjkim2902@gmail.com>
2023-11-01 20:11:47 +0900

    feat : InputView 생성

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+public class Input {
+    public static String consoleInput(){
+        return Console.readLine();
+    }
+}
