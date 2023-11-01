7016c07b5608d998f388e10af9c0d610dbe790d2
이재현 <fanta4715@naver.com>
2023-11-01 19:10:30 +0900

    refactor: 변경된 컨트롤러명 사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,10 @@
 package racingcar;
 
+import racingcar.controller.Game;
+
 public class Application {
     public static void main(String[] args) {
-        CarController carController = new CarController();
-        carController.start();
+        Game game = new Game();
+        game.start();
     }
 }
