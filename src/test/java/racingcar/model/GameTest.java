57d72d730f326e49412a0a3a3449617e4c86daf8
ihun-il <hunil9978@gmail.com>
2023-11-01 21:58:50 +0900

    test: 자동차 이름 Car 객체로 변경 로직 테스트

--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -2,6 +2,9 @@ package racingcar.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
@@ -11,5 +14,21 @@ public class GameTest {
         Game game = new Game();
         int randomNumber = game.makeRandomNumber();
         assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @Test
+    public void StringListChangeCarList() {
+        List<String> carNames = new ArrayList<>(){{
+           add("test");
+        }};
+
+        List<Car> cars = new ArrayList<>();
+
+        for(String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+
+        assertThat(cars.get(0)).isInstanceOf(Car.class);
     }
 }
