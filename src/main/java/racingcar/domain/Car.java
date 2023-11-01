6d91417847d79af00d75f798a6215d7e389266a1
seokmyungham <hellotjraud@gmail.com>
2023-11-01 10:46:26 +0900

    feat(Car): 생성자 오버로딩

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -10,6 +10,11 @@ public class Car {
     private Name name;
     private Move move;
 
+    public Car(Name name, Move move) {
+        this.name = name;
+        this.move = move;
+    }
+
     public Car(String carName) {
         this.name = new Name(carName);
         this.move = new Move();
