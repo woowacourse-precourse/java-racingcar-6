fd41d098650ecd30ade16d0a652928fb8be6bdd4
inwoo <korean5078@gmail.com>
2023-11-01 22:08:53 +0900

    refactor(RacingCarGame): 불필요한 코드 삭제 및 Player 클래스의 의존성 주입 방식 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
+import racingcar.domain.Player;
 import racingcar.domain.RacingCarGame;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarGame racingCarGame = new RacingCarGame();
+        Player player = new Player();
+        RacingCarGame racingCarGame = new RacingCarGame(player);
         racingCarGame.init();
     }
 }
--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -5,6 +5,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class RacingCarGame {
+    private final Player player;
+
+    public RacingCarGame(Player player) {
+        this.player = player;
+    }
+
     public List<RacingCar> createRacingCars(List<String> carNames) {
         List<RacingCar> racingCars = new ArrayList<>();
 
@@ -17,7 +23,6 @@ public class RacingCarGame {
 
     public void roundStart(List<RacingCar> cars) {
         for (RacingCar car : cars) {
-            car.makeRandomNumber();
             car.goOrStop();
         }
     }
@@ -64,7 +69,6 @@ public class RacingCarGame {
     }
 
     public void init() {
-        Player player = new Player();
         List<RacingCar> cars = createRacingCars(player.inputNames());
         int numberOfRounds = player.inputNumberOfRounds();
         System.out.println();
