fb070165d530ed3e2c9644140b2f03ba6dfaf750
ebang <dskhe8@gmail.com>
2023-11-01 20:14:06 +0900

    bugfix : 게임횟수, 경주차 이름 입력 시  출력문 변경

--- src/main/java/racingcar/View/carRaceOutputService.java
+++ src/main/java/racingcar/View/carRaceOutputService.java
@@ -18,11 +18,11 @@ public class carRaceOutputService {
     }
 
     public void askGameRound() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 
     public void askCarNames() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     private void printCarName(String carName) {
