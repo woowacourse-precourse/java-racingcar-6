77f978e62ce20262b8862147b3e0fdc5e79054f5
gillyongs <khs20010327@gmail.com>
2023-11-01 09:13:15 +0900

    feat: 레포지토리와 car 도메인 구현

---
+++ src/main/java/racingcar/Repository.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import racingcar.domain.Car;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Repository {
+    private int id = 1;
+    private Map<Integer, Car> cars = new HashMap<>();
+
+    public void add(Car car) {
+        cars.put(id, car);
+        id++;
+    }
+
+
+}
