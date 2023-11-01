2e865d1e3a1e08e27df7243a958bbdd0322769c1
Youth <boysoeng@naver.com>
2023-11-01 23:03:47 +0900

    test: 우승자 선정 테스트

---
+++ src/test/java/racingcar/domain/game/WinnerTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain.game;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Game;
+
+public class WinnerTest extends NsTest {
+    private static final int STOP = 3;
+    private static final int MOVING_FORWARD = 4;
+
+    private Game game = new Game();
+
+    @Test
+    void 우승자_선정() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,youth", "1");
+                    Assertions.assertThat(output()).contains("최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        game.raceSetting();
+        game.race();
+        game.declareWinner();
+    }
+}
