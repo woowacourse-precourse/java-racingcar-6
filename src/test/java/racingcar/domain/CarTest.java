25731b729a2e2abc57aead97fe9f473693bd3ba4
ay-eonii <naayen@naver.com>
2023-11-01 18:01:13 +0900

    test: 자동차 위치 비교하는 단위테스트 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -34,4 +34,21 @@ class CarTest {
         int expected = 2;
         Assertions.assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void Car_객체를_비교한다() {
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+        Car car3 = new Car("Car3");
+
+        car1.go();
+
+        int result1 = car1.compareTo(car2);
+        int result2 = car2.compareTo(car1);
+        int result3 = car2.compareTo(car3);
+
+        Assertions.assertThat(result1).isGreaterThan(0);
+        Assertions.assertThat(result2).isLessThan(0);
+        Assertions.assertThat(result3).isEqualTo(0);
+    }
 }
