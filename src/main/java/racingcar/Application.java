296342feec82bc4c366305c9e8de9937c8fe16a9
YANGSEOKWOO <didtjrdn030@naver.com>
2023-11-01 21:03:44 +0900

    feat: Game객체 구현 및 함수 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        Game game= new Game();
+        game.Start();
+        game.Winner();
+
         // TODO: 프로그램 구현
     }
 }
