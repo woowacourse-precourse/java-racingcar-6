eb1e74f8ff8bda92ea3b7980b4483ccef3cb754f
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-01 14:45:50 +0900

    Feat: 중복된 이름 입력 검사 기능

--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
-    Validator validator = new Validator();
     private static final String WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE = "이름은 5자 이하여야 합니다";
+    private static final String CAR_NAME_DUPLICATION_ERROR_MESSAGE = "이름이 중복되어선 안됩니다";
+
+    Validator validator = new Validator();
 
     @Test
     void 차_이름_길이_예외테스트() {
@@ -16,4 +18,13 @@ class ValidatorTest {
         });
         assertEquals(WRONG_LENGTH_CAR_NAME_ERROR_MESSAGE, e.getMessage());
     }
+
+    @Test
+    void 차_이름_중복_예외테스트() {
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validator.validateCarNameInput("abc,abc,def");
+        });
+        assertEquals(CAR_NAME_DUPLICATION_ERROR_MESSAGE, e.getMessage());
+    }
+
 }
