c0c60de5f5d723fa53d9d052a1dae8dea101835b
yujeonghyeon <dbdb1114@naver.com>
2023-11-01 23:18:54 +0900

    게인 진행을 위한 메인 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameController gameController = new GameController();
+
+        gameController.startGame();
+        gameController.numberOfGame();
+        gameController.proceedingGame();
+        gameController.gameEnding();
     }
 }
