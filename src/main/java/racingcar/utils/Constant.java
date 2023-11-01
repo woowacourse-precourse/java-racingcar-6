8f478d1c60d499cbd83ed6b4551083111181c644
woong <hiidy115@gmail.com>
2023-11-01 19:40:23 +0900

    :sparkles: feat: 상수를 관리하는 기능 구현

---
+++ src/main/java/racingcar/utils/Constant.java
@@ -0,0 +1,16 @@
+package racingcar.utils;
+
+public class Constant {
+    public static final int MIN_NAME_LENGTH = 2;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MIN_RANGE = 1;
+    public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int MAX_RANDOM_NUMBER = 9;
+    public static final int FIRST_INDEX = 0;
+    public static final int MIN_COUNT = 2;
+    public static final int START_POSITION = 0;
+    public static final int STANDARD_NUMBER_MOVING_FORWARD = 4;
+    public static final int START_INCLUSIVE = 0;
+    public static final int LAST_COUNT = 0;
+    public static final String DELIMITER = ",";
+}
