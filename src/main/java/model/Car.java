46926087b41969afad6980d18ddc1abec52191c7
yujeonghyeon <dbdb1114@naver.com>
2023-11-01 20:47:45 +0900

    1. 자동차 객체 위치 최신화하는 메소드 구현

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,28 @@
+package model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import config.GameProperty;
+
+public class Car {
+
+    private final static int MIN_RANDOM_RANGE = GameProperty.getMinMoveForRange();
+    private final static int MAX_RANDOM_RANGE = GameProperty.getMaxRandomRange();
+    private final static int FOR_MOVE_NUMBER = GameProperty.getForMoveNumber();
+    String name;
+    int location = 0;
+
+    private Car() {
+    }
+
+    Car(String name) {
+        this.name = name;
+    }
+
+    public void moveCar() {
+        if (Randoms.pickNumberInRange(MIN_RANDOM_RANGE, MAX_RANDOM_RANGE) > FOR_MOVE_NUMBER) {
+            this.location++;
+        }
+    }
+
+
+}
