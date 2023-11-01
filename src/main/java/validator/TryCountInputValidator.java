9c2396a415b9a358c60e384e794f56c7f51830a4
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-01 16:50:04 +0900

    feat: 양수만 입력받을 수 있는 validPositiveNumber 함수 생성

--- src/main/java/validator/TryCountInputValidator.java
+++ src/main/java/validator/TryCountInputValidator.java
@@ -14,4 +14,11 @@ public class TryCountInputValidator {
             throw new IllegalArgumentException("공백은 입력할 수 없어요. 숫자를 입력해주세요.");
         }
     }
+
+    public void validPositiveNumber(String number) {
+        int intNumber = Integer.parseInt(number);
+        if (intNumber <= 0) {
+            throw new IllegalArgumentException("0회는 입력할 수 없습니다.");
+        }
+    }
 }
