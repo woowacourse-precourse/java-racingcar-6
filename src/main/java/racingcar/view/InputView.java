3fbf33e93077f23e8a811ca34a61bf6d8c0ca236
JiEung2 <https://github.com/JiEung2>
2023-11-01 14:26:22 +0900

    Refactor: InputView 기능 변경
    InputView에서 굳이 검증하는 것을 알아야되나 싶어서 검증 부분을 따로 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.domain.CarNameValidator;
+import racingcar.domain.RoundNumberValidator;
 
 public class InputView {
 
@@ -13,41 +15,15 @@ public class InputView {
     }
 
     public List<String> toStringList(String name) {
+        new CarNameValidator(name);
         List<String> names = Arrays.asList(name.split(","));
-        validateName(names);
         return names;
-    }
-
-    public void validateName(List<String> names) {
-        if (!isAllFiveOrLess(names)) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이내로 입력해주세요.");
-        }
-    }
-
-    public boolean isAllFiveOrLess(List<String> names) {
-        for (String name : names) {
-            if (name.length() > 5) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public int insertRoundNumber() {
         String input = Console.readLine();
-        validateNumber(input);
-
+        new RoundNumberValidator(input);
         return convertToNumber(input);
-    }
-
-    public void validateNumber(String input) {
-        if (!isNumber(input)) {
-            throw new IllegalArgumentException("숫자를 입력하세요");
-        }
-    }
-
-    public boolean isNumber(String input) {
-        return input.matches("^[0-9]*$");
     }
 
     public int convertToNumber(String input) {
