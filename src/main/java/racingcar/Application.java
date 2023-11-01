54d7dbef10a46f45d9e74003229de032c4c417aa
Walkers15 <qortmd7777@gmail.com>
2023-11-01 23:29:05 +0900

    chore: 미사용 import 문 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Application {
