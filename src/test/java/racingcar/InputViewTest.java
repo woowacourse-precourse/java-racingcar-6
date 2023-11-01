f4bbc56f2fca97355e572666502c2461724df1d4
bnfkim <kgr3819@gmail.com>
2023-11-01 17:04:23 +0900

    Refactor: Validator class로 따로 뺌에 따라 테스트 변경

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.util.CarNameValidator;
+import racingcar.util.CommonValidator;
+import racingcar.util.GameRoundValidator;
 import racingcar.view.InputView;
 
 import java.util.ArrayList;
@@ -10,62 +13,61 @@ import java.util.List;
 class InputViewTest {
 
     @Test
-    void inputCarName_빈_입력값일_경우_에러() {
+    void 빈_입력값일_경우_에러() {
         //given
         String input = "";
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.validInputBlank(input);
+            CommonValidator.validInputBlank(input);
         });
     }
 
     @Test
-    void inputCarName_이름이_중복될_경우_에러() {
+    void 자동차_이름이_중복될_경우_에러() {
         //given
         ArrayList<String> carNames = new ArrayList<>(List.of("pobi", "woni", "jun", "jun"));
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.inputCars(carNames);
+            CarNameValidator.validInputCarName(carNames);
         });
     }
 
     @Test
-    void inputCarName_자동차_이름_5글자_이상일_경우_에러() {
+    void 자동차_이름_5글자_이상일_경우_에러() {
         //given
         ArrayList<String> carNames = new ArrayList<>(List.of("pobi", "woni", "junnnn"));
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.inputCars(carNames);
+            CarNameValidator.validInputCarName(carNames);
         });
     }
 
     @Test
-    void inputCarName_자동차_이름에_공백이_포함된_경우_에러() {
+    void 자동차_이름에_공백이_포함된_경우_에러() {
         //given
         ArrayList<String> carNames = new ArrayList<>(List.of("po bi", "woni", "jun"));
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.inputCars(carNames);
+            CarNameValidator.validInputCarName(carNames);
         });
     }
-
     @Test
-    void inputGameRound_정수가_아닐_경우_에러() {
+    void GameRound_정수가_아닐_경우_에러() {
         //given
         String input = "다섯";
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.validInputGameRound(input);
+            GameRoundValidator.validInputGameRound(input);
         });
     }
 
     @Test
-    void inputGameRound_1미만일_경우_에러() {
+    void GameRound_1미만일_경우_에러() {
         //given
         String input = "0";
         //when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.validInputGameRound(input);
+            GameRoundValidator.validInputGameRound(input);
         });
     }
 }
