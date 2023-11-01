55e9788b36bac3a830126fe79d70b18e604fae1c
gomdol <oioifor@naver.com>
2023-11-01 19:52:20 +0900

    feat(GameManager) : 이름 입력받기 및 예외처리

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.Message;
+import racingcar.validation.InputException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,17 +10,21 @@ import java.util.List;
 public class GameManager {
 
     public List<String> inputCarsName(){
-        String[] split = inputStringCarName().split(",");
+//        String[] split = inputStringCarName().split(",");
         ArrayList<String> carName = new ArrayList<>();
-        for(String name : split){
+        for(String name : this.inputStringCarName()){
             carName.add(name);
         }
         return carName;
     }
 
-    private String inputStringCarName() {
+    private String[] inputStringCarName() {
         System.out.println(Message.start);
-        String str = Console.readLine();
-        return str;
+        return InputException.carNameValidation(Console.readLine()).split(",");
     }
+//    private int inputNumber(){
+//        System.out.println(Message.times);
+//        String input = Console.readLine();
+//
+//    }
 }
--- src/main/java/racingcar/validation/InputException.java
+++ src/main/java/racingcar/validation/InputException.java
@@ -1,13 +1,19 @@
 package racingcar.validation;
 
 public class InputException {
+
+    public static String carNameValidation(String input){
+        carNameLengthCheck(input);
+        containBlank(input);
+        black(input);
+        return input;
+    }
     public static void carNameLengthCheck(String input){
         for (String name : input.split(",")) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름 길이를 초과하였습니다");
             }
         }
-
     }
 
     public static void containBlank(String input){
@@ -19,6 +25,6 @@ public class InputException {
     public static void black(String input){
         if(input.isEmpty()){
             throw new IllegalArgumentException("값을 입력해 주세요");
-        }]
+        }
     }
 }
