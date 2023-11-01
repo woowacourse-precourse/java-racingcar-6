d47c0df085c01e01e03383555c465b30750e82e1
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-01 22:08:39 +0900

    feat: read count to move

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -12,7 +12,7 @@ public class RacingCarGame {
 
     public RacingCarGame() {
         this.racingCars = makeCars();
-        this.moveCount = 0;
+        this.moveCount = readMoveCount();
     }
 
     private List<Car> makeCars() {
@@ -29,5 +29,11 @@ public class RacingCarGame {
     // 기능 1) 5자 이하의 자동차 이름 n개를 쉼표(,)를 기준으로 구분하여 입력 받는다.
     private List<String> readCarNames() {
         return Arrays.stream(Console.readLine().split(",")).toList();
+    }
+
+    // 기능 2) 몇 번의 이동을 할 것인지 입력 받는다.
+    private int readMoveCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
