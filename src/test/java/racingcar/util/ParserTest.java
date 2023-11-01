9584a6763191d7e0901062b4c6493e07bfade978
1015s <1015_s@naver.com>
2023-11-01 00:27:16 -0700

    feat(ParserTest) : 이동횟수 문자 입력 테스트

--- src/test/java/racingcar/util/ParserTest.java
+++ src/test/java/racingcar/util/ParserTest.java
@@ -1,2 +1,15 @@
-package racingcar.util;public class ParserTest {
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ParserTest {
+    @Test
+    void 이동횟수_문자_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> Parser.tryCount("a"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
 }
