41137dcdeebad8ce40c80e69d1f84b1d984eecde
Choi-Moori <anflwkd@gmail.com>
2023-11-01 17:48:43 +0900

    Feat(Application) Application을 통한 게임 시작 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.domain.ActionCars;
+
 public class Application {
     public static void main(String[] args) {
-        
+        ActionCars actioncars = new ActionCars();
+        actioncars.gameStart();
     }
 }
