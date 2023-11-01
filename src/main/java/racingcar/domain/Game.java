aa94d0c5f83de93810316f34c975ff5cee5a3e40
Subin <rla0010101@gmail.com>
2023-11-01 23:39:05 +0900

    Feat(예외 처리): 중복된 자동차 이름을 입력했을 경우

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -4,9 +4,10 @@ import racingcar.views.InputView;
 import racingcar.views.OutputView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
-import static racingcar.util.ErrorInstruction.CAR_NAME_LENGTH_ERROR_MESSAGE;
-import static racingcar.util.ErrorInstruction.TRIAL_COUNT_ERROR_MESSAGE;
+import static racingcar.util.ErrorInstruction.*;
 import static racingcar.views.InputView.readUserInput;
 
 public class Game {
@@ -26,6 +27,7 @@ public class Game {
 
     private ArrayList<Car> makeCarList(String[] carNames) {
         ArrayList<Car> cars = new ArrayList<>();
+        Set<String> uniqueCarNames = new HashSet<>();
         for (String carName : carNames) {
             if (carName.length() > 5 || carName.length() < 1) {
                 throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE.getMessage());
@@ -33,6 +35,10 @@ public class Game {
             if (carName.isBlank()) {
                 throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE.getMessage());
             }
+            if (uniqueCarNames.contains(carName)) {
+                throw new IllegalArgumentException(CAR_NAME_DUPLICATION_ERROR_MESSAGE.getMessage());
+            }
+            uniqueCarNames.add(carName);
             Car car = new Car(carName);
             cars.add(car);
         }
