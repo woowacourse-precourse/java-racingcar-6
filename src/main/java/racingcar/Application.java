b6b36ee0f74b64a7ce520834f0545f6392bd2e64
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-01 09:39:00 +0900

    사용자로부터 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,17 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = scanner.nextLine().split(",");
         int numCars = carNames.length;
+
+        // 2. 사용자로부터 시도할 회수 입력받기
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numAttempts = scanner.nextInt();
+
+        if (numAttempts <= 0) {
+            System.out.println("시도 횟수는 1 이상이어야 합니다.");
+            return;
+        }
+
+        for (int attempt = 0; attempt < numAttempts; attempt++) {
+            System.out.println("\n시도 " + (attempt + 1) + ":");
     }
 }
