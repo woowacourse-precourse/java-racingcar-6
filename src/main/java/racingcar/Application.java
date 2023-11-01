5dee6c1a1a0974a1d3da05e67d710432c6c5503c
jiixon <jr0107380@gmail.com>
2023-11-01 16:55:32 +0900

    feat: main에서 controller 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import racingcar.Controller.Controller;
 
+
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
