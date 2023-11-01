3ef2387883ab979fb187bca4f26234c077e5a722
ay-eonii <naayen@naver.com>
2023-11-01 14:15:19 +0900

    test: 자동차 이름 중복 유효성 검사하는 단위테스트 작성

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -1,9 +1,15 @@
 package racingcar.validator;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ValidatorTest {
 
@@ -26,6 +32,26 @@ class ValidatorTest {
                 assertThatThrownBy(() -> validator.checkCarName(inputCarName))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessageContaining("Car name cannot be blank")
+        );
+    }
+
+    @Test
+    void 자동차_이름이_중복일_경우_예외_발생() {
+        List<Car> carList = Arrays.asList(
+                mock(Car.class),
+                mock(Car.class),
+                mock(Car.class)
+        );
+
+        when(carList.get(0).getName()).thenReturn("중복");
+        when(carList.get(1).getName()).thenReturn("중복");
+        when(carList.get(2).getName()).thenReturn("중복아님");
+
+        Validator validator = new Validator();
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.checkDuplicateName(carList))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("Name must not be duplicated")
         );
     }
 }
