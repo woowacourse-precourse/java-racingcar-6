8cb4f49180d69a61993651fc18303713bbc69861
gumgu <bienlee@naver.com>
2023-11-01 13:08:28 +0900

    Fix : 상수 선언 오류 수정

--- src/main/java/racingcar/business/NumberMaker.java
+++ src/main/java/racingcar/business/NumberMaker.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberMaker {
 
-    public static final int MIN_NUMBER_RANGE = 0;
-    public static final int MAX_NUMBER_RANGE = 9;
+    private static final int MIN_NUMBER_RANGE = 0;
+    private static final int MAX_NUMBER_RANGE = 9;
 
     public Integer getRandomNumber() {
         return Randoms.pickNumberInRange(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE);
