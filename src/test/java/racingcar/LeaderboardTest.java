728814e06fe82000a00ee1406c297713f441e36f
wda067 <mg6359@naver.com>
2023-11-01 23:35:26 +0900

    test(LeaderboardTest): 입력한 값들이 Car객체로 생성되었는지 테스트 추가

---
+++ src/test/java/racingcar/LeaderboardTest.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LeaderboardTest {
+    private static Map<Car, String> map;
+
+    @BeforeEach
+    void setUp() {
+        String input = "test1,test2,test3";
+        Player.makeCarList(input);
+        Leaderboard.makeCarInstanceList();
+        map = Leaderboard.getMap();
+    }
+
+    @Test
+    void 입력된_이름이_Car_객체인지_확인() {
+        for (Car car : map.keySet()) {
+            assertThat(car).isInstanceOf(Car.class);
+        }
+    }
+
+    @Test
+    void race() {
+    }
+
+    @Test
+    void printRaceProgress() {
+    }
+
+    @Test
+    void printResult() {
+    }
+}
