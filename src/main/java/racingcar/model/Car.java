e59454652c8d4c7d05fbfa4342c25b2b2d18cbc7
GyungA <GyungA@users.noreply.github.com>
2023-11-01 20:37:41 +0900

    feat: Car 필드 및 메서드 정의

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,14 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private Integer moveStatus;
+
+    public void move() {
+        this.moveStatus += 1;
+    }
+
+    public void stay() {
+
+    }
+}
