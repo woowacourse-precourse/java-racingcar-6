d20852cfbc1a5029784c60e8164c146a94533042
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-01 09:55:35 +0900

    style(RandomGenerator) : 불필요한 줄바꿈 제거

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -7,7 +7,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator {
     public static int generate() {
-        return Randoms.pickNumberInRange(MIN_NUMBER_RANDOM_RANGE.get(),
-                MAX_NUMBER_RANDOM_RANGE.get());
+        return Randoms.pickNumberInRange(MIN_NUMBER_RANDOM_RANGE.get(), MAX_NUMBER_RANDOM_RANGE.get());
     }
 }
