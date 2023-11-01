a39c6245e25266ab71204e035ac21dee58103d4a
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-01 18:43:31 +0900

    feat: 전진 계산을 위한 2차원 배열 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,15 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String[] cars = Console.readLine().split(",");
 
+        // TODO: 2차원 배열에 저장
+        int carsNum = cars.length;
+        String[][] carForward = new String[carsNum][1];
+        for (int s = 0; s < carsNum; s++) {
+            // 자동차 이름 복사
+            carForward[s][1] = cars[s];
+        }
+
+
         // TODO: 시도할 회수 입력 받기
         System.out.println("시도할 회수는 몇회인가요?");
         int gameCount = Integer.parseInt(Console.readLine());
@@ -18,7 +27,6 @@ public class Application {
         for (int i = 0; i < gameCount; i++) {
 
             // TODO: 경주차 개수에 따른 랜덤 정수 생성
-            int carsNum = cars.length;
             int[] randomNumArray = new int[carsNum];
 
             for (int j = 0; j < carsNum; j++) {
