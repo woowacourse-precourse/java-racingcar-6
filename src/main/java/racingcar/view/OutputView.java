9f0a651c1b63ebb7a5464906102a4d3c64e6b0ae
ay-eonii <naayen@naver.com>
2023-11-01 15:51:55 +0900

    refactor: String + 연산자 지양

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 public class OutputView {
     private final static String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private final static String RESULT_GUIDE_MESSAGE = "\n" + "실행 결과";
+    private final static String RESULT_GUIDE_MESSAGE = "\n실행 결과";
     private final static String WINNER_GUIDE_MESSAGE = "최종 우승자 : ";
 
     public void displayCarNameInputMessage() {
@@ -23,7 +23,7 @@ public class OutputView {
     }
 
     public void displayWinner(String winner) {
-        System.out.print(WINNER_GUIDE_MESSAGE + winner);
+        System.out.printf("%s%s", WINNER_GUIDE_MESSAGE, winner);
     }
 
     public void displayNewLine() {
