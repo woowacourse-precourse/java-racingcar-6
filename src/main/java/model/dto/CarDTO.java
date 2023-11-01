d6b142a49e02b670e640b11229408c114034091e
qudwns017 <qudwns017@naver.com>
2023-11-01 19:45:57 +0900

    Feat: Car DTO 선언 및 getter 메소드 추가

---
+++ src/main/java/model/dto/CarDTO.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+public class CarDTO {
+    private String name;
+    private int moveCount;
+
+    public CarDTO(String name, int moveCount) {
+        this.name = name;
+        this.moveCount = moveCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
