794a1172e37cb5b1c804ba3b071f22ec83e0cd9c
mung96 <mung5079@gmail.com>
2023-11-01 14:17:24 +0900

    Refactor: createName() 반환 타입을 List로 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,21 +1,21 @@
 package racingcar;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
 
 public class Game {
-    private static HashMap<String,String> players= new HashMap<>();
-    public Map<String, String> createName() {
+    public static List<Car> players;
+    public List<Car> createName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        String[] cars = camp.nextstep.edu.missionutils.Console.readLine().split(",");
-
-        for (String name : cars) {
+        for (String name : camp.nextstep.edu.missionutils.Console.readLine().split(",")) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
-            players.put(name,"");
+
+            players.add(new Car(name));
         }
+
         return players;
     }
 
