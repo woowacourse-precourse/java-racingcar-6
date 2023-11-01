094483d5149542bf8292a339585beeb08618b722
haein <sodychoe@icloud.com>
2023-11-01 18:01:43 +0900

    Feat: 출력 콘솔의 라운드 별 결과 출력 구현

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,17 +1,30 @@
 package racingcar.View;
 
+
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class OutputView {
     public static final String WINNER_GUIDE_MESSAGE = "최종 우승자 : ";
+    public static final String GAME_RESULT_MESSAGE = "실행 결과";
 
-    public void printRound() {
-
+    public void printRounds(int roundTrialNum) {
+        System.out.println(GAME_RESULT_MESSAGE);
+        for (int i; i < roundTrialNum; i++) {
+            printCarsResult();
+            System.out.println("/n");
+        }
     }
 
-    public void printCarResult() {
+    public void printCarsResult() {
+    }
 
+    public StringBuilder printCarResult(String name, int position) {
+        StringBuilder sb = new StringBuilder(name + " : ");
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+            return sb;
+        }
     }
 
     public void printWinners(ArrayList<String> winners) {
