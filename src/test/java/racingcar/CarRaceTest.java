076624a30c3786f5ea8bdce6b19c65def0fdbcd9
김민주 <69795199+ks00919@users.noreply.github.com>
2023-11-01 22:57:30 +0900

    feat: carRaceTest precondition test

---
+++ src/test/java/racingcar/CarRaceTest.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarRaceTest {
+
+    @Test
+    @DisplayName("precondition 테스트")
+    void constructorTest() {
+        String[] name = {"123456"};
+        assertThrows(IllegalArgumentException.class, () -> new CarRace(name, 5));
+        String[] names = {"1234,5678"};
+        assertThrows(IllegalArgumentException.class, () -> new CarRace(names, -1));
+    }
+
+}
