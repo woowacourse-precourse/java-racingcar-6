e3857c3a911b2dc7507cd40a314f70deeedb04bd
potatomoong <star901210@hanmail.net>
2023-11-01 18:30:31 +0900

    Feat: 메인클래스에 자동차경기실행 구현하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,11 +8,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         StartCondition startCondition = new StartCondition();
+
         String[] carNames = startCondition.inputCarName();
         int tryCount = startCondition.inputAttemptNumber();
 
         int carCount = carNames.length;
         int[] positions = new int[carCount];
 
+        JudgeMovement judgeMovement = new JudgeMovement();
+        Result result = new Result();
+
+        for (int i = 0; i < tryCount; i++) {
+            int[] randomNumbers = judgeMovement.createRandomNumbers(carCount);
+            judgeMovement.moveForward(positions, randomNumbers);
+            result.printRapResult(carNames, positions);
+        }
     }
 }
