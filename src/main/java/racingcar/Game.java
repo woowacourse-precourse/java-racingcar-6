1095601a83908f1bbe9996f2f72c9e7a266c4142
yes-ee <yes0112@naver.com>
2023-11-01 13:01:30 +0900

    feat(game): 게임 각 라운드를 진행하는 기능 추가
    사용하지 않는 import, 클래스 삭제
    실행 결과 메시지 출력
    한 라운드를 진행하는 doOneRound() 함수 추가
    - 라운드 진행 시 carList에 자동차별 결과 저장

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,21 +1,38 @@
 package racingcar;
 
 import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Game {
-    User user = new User();
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     LinkedHashMap<String, String> carList;
     int totalCount;
 
     public void run() {
-        outputView.printMessage(Message.INPUT_CAR_NAME.getMessage());
+        outputView.printMessage(Message.INPUT_CAR_NAME.getMessage() + "\n");
         carList = inputView.getCarNameInput();
 
-        outputView.printMessage(Message.INPUT_REPEAT_NUMBER.getMessage());
+        outputView.printMessage(Message.INPUT_REPEAT_NUMBER.getMessage() + "\n");
         totalCount = inputView.getGameCountInput();
+
+        outputView.printMessage(Message.GAME_RESULT.getMessage() + "\n");
+
+        for (int i = 0; i < totalCount; i++) {
+            doOneRound();
+        }
+    }
+
+    private void doOneRound() {
+        for (Entry<String, String> carResult : carList.entrySet()) {
+            String carName = carResult.getKey();
+            Car car = new Car();
+
+            if (car.checkForward()) {
+                carList.put(carName, carList.get(carName) + "-");
+            }
+        }
     }
 }
