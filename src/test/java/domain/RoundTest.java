4d1f0843b3aa2fa5f6eef3e9aabaae0a0abb1132
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-01 16:31:49 +0900

    test: Round 테스트 추가
    -실행 횟수가 시도 횟수 미만인 경우 false 반환
    -실행 횟수가 시도 횟수와 동일한 경우 true 반환

---
+++ src/test/java/domain/RoundTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Round;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundTest {
+
+    Round round;
+
+    @Test
+    void 실행_횟수가_시도_횟수_미만인_경우() {
+        int maxRound = 3;
+        round = new Round(maxRound);
+
+        for (int i = 0; i < maxRound; i++) {
+            assertFalse(round.isMaxRound());
+        }
+    }
+
+    @Test
+    void 실행_횟수가_시도_횟수와_동일한_경우() {
+        int maxRound = 3;
+        round = new Round(maxRound);
+
+        for (int i = 0; i <= maxRound; i++) {
+            round.isMaxRound();
+            if (i == maxRound) {
+                assertTrue(round.isMaxRound());
+            }
+        }
+    }
+}
