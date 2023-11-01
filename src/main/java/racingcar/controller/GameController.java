a0e3b75a80e22c33cd0a4e05b7d073e26968fba0
grax <grax@192.168.35.119>
2023-11-01 16:31:24 +0900

    feat: "add a code for filling value 0 in positionList"
    "position을 0 값으로 시작하기위한 코드"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,16 +1,34 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.domain.GameData;
+import racingcar.domain.Judgement;
+import racingcar.domain.NumberGenerator;
 import racingcar.view.Input;
 import racingcar.view.Printer;
 
 public class GameController {
 	private Printer printer = new Printer();
 	private Input input = new Input();
-	
+	private NumberGenerator ng = new NumberGenerator();
+	private GameData data = new GameData();
+	private Judgement referee = new Judgement();
+
 	public void playGame() {
 		printer.displayMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-		input.getCarName();
+		input.getCarName(data);
+
 		printer.displayMessage("시도할 회수는 몇회인가요?\n");
-		input.getTrialNumber();
+		int gameCount = input.getTrialNumber();
+
+		printer.displayMessage("실행 결과\n");
+
+		// positionList에 0값 추가하기
+		data.setInitPositionList(data.getCarList());
 	}
+
+
+
 }
