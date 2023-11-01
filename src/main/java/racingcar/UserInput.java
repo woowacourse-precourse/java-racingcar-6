3234ac2faecd51d53f7737407dcceb189f4d0cb2
kijen723 <kij2646@gmail.com>
2023-11-01 19:12:15 +0900

    fix(UserInput): Fix methods data type and add return

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -6,13 +6,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserInput {
-    public static void getCarName() {
+    public static List<String> getCarName() {
         String carsInput = Console.readLine();
         List<String> cars = Arrays.asList(carsInput.split(","));
         ExceptionControl.checkCarNameInput(cars);
+
+        return cars;
     }
 
-    public static void getGameCount() {
+    public static int getGameCount() {
         String gameCount = Console.readLine();
+
+        return Integer.parseInt(gameCount);
     }
 }
