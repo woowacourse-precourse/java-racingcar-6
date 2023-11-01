fa784cb6e9547815e6dbcfac9ab8392c2462b896
haein <sodychoe@icloud.com>
2023-11-01 17:19:57 +0900

    Feat: 자동차 객체의 이름 속성에서 공백을 제거하는 전처리 메소드 구현

--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -7,8 +7,9 @@ public class CarList {
     List<Car> carlist = new ArrayList<Car>();
 
     public void add(String name) {
-        nameValidator(name);
-        carlist.add(new Car(name));
+        String cleanName = nameCleaner(name);
+        nameValidator(cleanName);
+        carlist.add(new Car(cleanName));
     }
 
     public void nameValidator(String name) throws IllegalArgumentException {
@@ -19,5 +20,9 @@ public class CarList {
         if (name.isBlank()) {
             throw new IllegalArgumentException("유효하지 않은 이름입니다. 프로그램을 종료합니다.");
         }
+    }
+
+    public String nameCleaner(String name) {
+        return name.replaceAll("\\s+", "");
     }
 }
