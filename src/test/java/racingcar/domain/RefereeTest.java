7ea86b05b009617389935fe0ed26051ec52d265b
JongMinCh0i <wonderland314592@gmail.com>
2023-11-01 21:47:50 +0900

    test(Referee): 이동할 수 있는 조건값 이상 시 출발하는 기능 단위 테스트

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -56,4 +56,39 @@ class RefereeTest {
         //then
         Assertions.assertThat(winnerName).isEqualTo("최종 우승자 : 홍길동");
     }
+
+    @DisplayName("이동할 수 있는 값이  4 이상이면 자동차는 이동한다.")
+    @Test
+    void canMove() {
+        //given
+        List<Car> list = Arrays.asList(new Car("김민수"), new Car("홍길동"), new Car("최종민"));
+        Referee referee = new Referee();
+        OutputView outputView = new OutputView();
+        Car car = list.get(2);
+
+        //when
+        referee.startEngine(car, 4);
+        String winnerName = outputView.getWinnerNames(referee.selectWinnerList(list));
+
+        //then
+        Assertions.assertThat(winnerName).isEqualTo("최종 우승자 : 최종민");
+    }
+
+    @DisplayName("이동할 수 있는 값 4를 넘지 못하면 자동차는 이동하지 못한다.")
+    @Test
+    void cant() {
+        //given
+        List<Car> list = Arrays.asList(new Car("김민수"), new Car("홍길동"), new Car("최종민"));
+        Referee referee = new Referee();
+        Car car = list.get(2);
+        OutputView outputView = new OutputView();
+
+        //when
+        referee.startEngine(car, 3);
+        String winnerName = outputView.getWinnerNames(referee.selectWinnerList(list));
+
+        //then
+        Assertions.assertThat(winnerName).isEqualTo("최종 우승자 : 김민수, 홍길동, 최종민");
+    }
+
 }
