c4953dcf9f559ac5c6028b7fc528ba949a123bc2
itsmo1031 <itsmo1031@gmail.com>
2023-11-01 21:00:56 +0900

    test: 공동 우승 시 출력 문구 테스트 추가

--- src/test/java/racingcar/controller/RacingGameTest.java
+++ src/test/java/racingcar/controller/RacingGameTest.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -8,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
 public class RacingGameTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     void 최소_인원_예외_처리() {
@@ -17,6 +21,16 @@ public class RacingGameTest extends NsTest {
         );
     }
 
+    @Test
+    void 공동_우승_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
 
     @Override
     public void runMain() {
