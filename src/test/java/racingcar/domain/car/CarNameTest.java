7ef7ec803b2468a6e2a0496770ee9eac2ca4e0c7
minisyu <iswater.kim@gmail.com>
2023-11-01 11:22:25 +0900

    refactor(CarNameTest):  @ParameterizedTest로 변경

--- src/test/java/racingcar/domain/car/CarNameTest.java
+++ src/test/java/racingcar/domain/car/CarNameTest.java
@@ -3,31 +3,25 @@ package racingcar.domain.car;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.util.Err;
 
 class CarNameTest {
 
+    @ParameterizedTest
     @DisplayName("자동차 이름이 null 또는 길이가 0인 경우, CarName 객체 생성 시 예외 발생")
-    @Test
-    void should_Throw_Exception_For_Null_Or_Empty_CarName() {
-        // given
-        String input1 = null;
-        String input2 = "";
-
+    @ValueSource(strings = {"", "\t", "\r", "\n", " "})
+    @NullSource
+    void should_Throw_Exception_For_Null_Or_Empty_CarName(String name) {
         // when
         // then
-        assertThatThrownBy(() -> CarName.from(input1))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(Err.INVALID_CAR_NAME_MIN_LENGTH.getMessage());
-
-        assertThatThrownBy(() -> CarName.from(input2))
+        assertThatThrownBy(() -> CarName.from(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Err.INVALID_CAR_NAME_MIN_LENGTH.getMessage());
     }
-
+    
     @ParameterizedTest
     @DisplayName("자동차 이름의 길이가 5글자를 넘어가면 CarName 객체 생성 시 예외 발생")
     @ValueSource(strings = {"rrrrrrrr", "pobiii", "yehhhhhhhhhhhhhhhh"})
