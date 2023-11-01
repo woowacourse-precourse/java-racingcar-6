4a7adf9742af02a526d5d77a7b7fa79a4d87b153
yj kim <yjee0215@gmail.com>
2023-11-01 21:53:15 +0900

    style: GameController 테스트 코드 BeforeAll 초기화
    - 기존 MockedStatic을 try-with-resources 구문에서 -> BeforeAll에서 같이 초기화 하도록 수정

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -1,5 +1,9 @@
 package racingcar.controller;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.api.Assertions;
@@ -12,15 +16,13 @@ import org.mockito.MockedStatic;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mockStatic;
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GameControllerTest {
     GameController gameController;
     final PrintStream standardOut = System.out;
     final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    static MockedStatic<Randoms> mockedRandoms;
+    static MockedStatic<Console> mockedConsole;
     final int GO = 9;
     final int STOP = 3;
 
@@ -28,38 +30,34 @@ public class GameControllerTest {
     public void init() {
         gameController = GameController.getInstance();
         System.setOut(new PrintStream(outputStreamCaptor));
+        mockedRandoms = mockStatic(Randoms.class);
+        mockedConsole = mockStatic(Console.class);
     }
 
     @AfterAll
     public void afterAll() {
         System.setOut(standardOut);
+        mockedRandoms.close();
+        mockedConsole.close();
     }
 
     @Test
     void 레이스_테스트_1() {
-        try (MockedStatic<Randoms> mockedRandoms = mockStatic(Randoms.class);
-             MockedStatic<Console> mockedConsole = mockStatic(Console.class);) {
+        given(Console.readLine()).willReturn("car1,car2", "2");
+        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(GO, STOP, GO, STOP);
 
-            given(Console.readLine()).willReturn("car1,car2", "2");
-            given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(GO, STOP, GO, STOP);
+        gameController.race();
 
-            gameController.race();
-
-            Assertions.assertThat(outputStreamCaptor.toString()).contains("car1 : -", "car1 : --", "최종 우승자 : car1");
-        }
+        Assertions.assertThat(outputStreamCaptor.toString()).contains("car1 : -", "car1 : --", "최종 우승자 : car1");
     }
 
     @Test
     void 레이스_테스트_2() {
-        try (MockedStatic<Randoms> mockedRandoms = mockStatic(Randoms.class);
-             MockedStatic<Console> mockedConsole = mockStatic(Console.class);) {
+        given(Console.readLine()).willReturn("car1,car2", "2");
+        given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(GO, GO, GO, GO);
 
-            given(Console.readLine()).willReturn("car1,car2", "2");
-            given(Randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(GO, GO, GO, GO);
+        gameController.race();
 
-            gameController.race();
-
-            Assertions.assertThat(outputStreamCaptor.toString()).contains("car1 : -", "car1 : --", "car2 : -", "car2 : --", "최종 우승자 : car1, car2");
-        }
+        Assertions.assertThat(outputStreamCaptor.toString()).contains("car1 : -", "car1 : --", "car2 : -", "car2 : --", "최종 우승자 : car1, car2");
     }
 }
