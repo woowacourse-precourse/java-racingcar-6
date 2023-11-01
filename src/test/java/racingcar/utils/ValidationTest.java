f702e59fc98ebd20ee21ce5fe22de63f5008543e
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-11-01 19:31:39 +0900

    test ($ValidationTest) : 자동차 이름 5자 이하 검증 테스트 케이스 추가

--- src/test/java/racingcar/utils/ValidationTest.java
+++ src/test/java/racingcar/utils/ValidationTest.java
@@ -1,14 +1,35 @@
 package racingcar.utils;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ValidationTest {
+    Validation validation;
+    @BeforeEach
+    void 클래스생성(){
+        validation = new Validation();
+    }
     @Test
     void 숫자가0이하이면에러발생(){
+        Integer number = 0;
+
+        assertThatThrownBy(()->validation.validTryCount(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 이름이6자이상이면에러발생(){
         Validation validation = new Validation();
-        assertThatThrownBy(()->validation.validTryCount(0))
+        List<String> carNames = new ArrayList<>();
+        carNames.add("1234");
+        carNames.add("12345");
+        carNames.add("123456");
+
+        assertThatThrownBy(()->validation.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
