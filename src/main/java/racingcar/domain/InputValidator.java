96e8e70d3156ad3e81d3f3b412733762bda8ed49
godee95 <50854903+godee95@users.noreply.github.com>
2023-11-01 17:13:45 +0900

    feat : 시도 횟수가 올바른지 확인

--- src/main/java/racingcar/domain/InputValidator.java
+++ src/main/java/racingcar/domain/InputValidator.java
@@ -9,5 +9,11 @@ public class InputValidator {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             }
         }
-    }   
+    }
+
+    public void validateNumAttempts(int numAttempts) {
+        if (numAttempts < 1) {
+            throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
+        }
+    }
 }
