32c3a4eca19bb86c23f9778c095f6c69849aa198
haebyun <bhk4392@gmail.com>
2023-11-01 19:02:36 +0900

    feat(RacingController): 검증된 자동차 이름들을 Car 객체로 저장하도록 RacingService에 요청하는 기능 구현

--- src/main/java/racingcar/games/racing/controller/RacingController.java
+++ src/main/java/racingcar/games/racing/controller/RacingController.java
@@ -6,22 +6,24 @@ import racingcar.games.racing.service.RacingService;
 import racingcar.games.racing.util.DefaultProcessor;
 import racingcar.games.racing.util.RacingFormatter;
 import racingcar.games.racing.view.RacingInputView;
-import racingcar.games.racing.view.RacingOutputView;
 
 public class RacingController implements Game {
 
     private final RacingInputView racingInputView;
     private final RacingFormatter racingFormatter;
+    private final RacingService racingService;
 
     public RacingController() {
         this.racingInputView = new RacingInputView();
         this.racingFormatter = new RacingFormatter();
+        this.racingService = new RacingService(new DefaultProcessor());
     }
 
 
     @Override
     public void execute() {
         List<String> carNames = getValidatedCarNames();
+        racingService.registerCars(carNames);
     }
 
     private List<String> getValidatedCarNames() {
