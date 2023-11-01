da6c354fb69b14d5443fbf454a8cae9f1d4acc26
elizabethgim <yujingim@naver.com>
2023-11-01 18:29:56 +0900

    test: CarService, RaceService

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,12 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Service.CarService;
+import racingcar.Service.RaceService;
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,8 +15,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    private static final CarService carService = new CarService();
+    private static final RaceService raceService = new RaceService();
+    private static List<Car> cars;
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+
+    @Test
+    void 자동차_이동(){
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        cars.add(car1);
+        cars.add(car2);
+        // when
+        carService.runOneTurn(cars);
+        carService.showResults(cars);
+
+        assertThat(output()).contains("-");
+    }
+
+    @Test
+    void 아무도_전진하지_않았을_때_최종_우승자_산출(){
+        // given
+        RaceService raceService = new RaceService();
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        cars.add(car1);
+        cars.add(car2);
+
+        // when
+        raceService.showResults(cars);
+
+        // then
+        assertThat(output()).contains("car1", "car2");
+    }
 
     @Test
     void 전진_정지() {
