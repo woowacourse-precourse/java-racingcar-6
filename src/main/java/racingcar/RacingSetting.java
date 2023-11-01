936e217928d2f1e3924e076622f076007c4d7384
minkyole <7719s@nave.com>
2023-11-01 21:18:25 +0900

    refactor(CarNum.java, RacingSetting.java): 사용 이점이 적은 CarNum 파일/Class 삭제
    - Class분리의 이점이 적은 CarNum.java 파일/class 삭제 및 그에따른 RacingSetting.java 코드수정

--- src/main/java/racingcar/RacingSetting.java
+++ src/main/java/racingcar/RacingSetting.java
@@ -3,19 +3,20 @@ package racingcar;
 import java.util.Arrays;
 
 public class RacingSetting {
-    private CarNum carNum;
     private MoveNum moveNum;
+
+    private final static int MAX_CAR_NUM = 10;
 
     String[] getCarName() {
         String inputCarName;
         String[] parsedCarName;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        inputCarName = Util.getUserInput();;
+        inputCarName = Util.getUserInput();
         isVaildInputCarName(inputCarName);
         parsedCarName = inputCarName.split(",");
         isDuplicatedInputCarName(parsedCarName);
-        carNum = new CarNum(parsedCarName);
+        isValidCarNum(parsedCarName);
         return (parsedCarName);
     }
 
@@ -38,6 +39,15 @@ public class RacingSetting {
                 .distinct()
                 .count() != parsedCarName.length) {
             throw new IllegalArgumentException("Error : 중복된 자동차 이름이 있습니다 중복되지않게 입력해 주세요");
+        }
+    }
+
+    private void isValidCarNum(String[] carNameParsing) throws IllegalArgumentException {
+        if (carNameParsing.length > MAX_CAR_NUM) {
+            throw new IllegalArgumentException("Error : 자동차 개수가 너무 많습니다 최대개수 " + MAX_CAR_NUM);
+        }
+        if (carNameParsing.length == 1 && carNameParsing[0].isBlank()) {
+            throw new IllegalArgumentException("Error : 자동차 개수가 너무 적습니다 최소 하나라도 입력해 주세요");
         }
     }
 
