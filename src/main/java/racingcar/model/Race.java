800018442034fe54e4a4cc6cb9f4830e018071bd
동원 <ai06021@gmail.com>
2023-11-01 19:39:00 +0900

    refactor : 차의 이동유무만 확인후 전진

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -3,21 +3,12 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.view.Cars;
+import racingcar.view.ScorePrint;
 
 public class Race {
     final static int MORE = 4;
 
-    public Race(List<String> cars, int maxTry) {
-        int tryCount = 0;
-        int car_numbers = cars.size();
-        int carsLocation[] = new int[car_numbers];
-        while (tryCount < maxTry) {
-            racing(carsLocation);
-        }
-    }
-
-    private static int[] racing(int currentCarLocation[]) {
-        int MaxCarNumber = currentCarLocation.length;
+    public static int[] racing(int currentCarLocation[], int MaxCarNumber) {
         for (int CarNumber = 0; CarNumber < MaxCarNumber; CarNumber++) {
             if (forwardCondition())
                 currentCarLocation[CarNumber]++;
