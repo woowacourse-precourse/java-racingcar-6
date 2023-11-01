7c13c615f5039009a6d8eed6e9459b6943dedb23
leemiyeon <lemingul7@gmail.com>
2023-11-01 18:01:35 +0900

    test: add MovementStrategyTest

---
+++ src/test/java/study/MovementStrategyTest.java
@@ -0,0 +1,24 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static racingcar.utils.RacingConfig.MAX_NUMBER;
+import static racingcar.utils.RacingConfig.MOVE_FORWARD_VALUE;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.movement.TestMovementStrategy;
+
+public class MovementStrategyTest {
+    @Test
+    void could_move() {
+        TestMovementStrategy testMovementStrategy = new TestMovementStrategy(MOVE_FORWARD_VALUE);
+        assertThat(testMovementStrategy.couldMove()).isTrue();
+    }
+
+    @Test
+    void could_not_move() {
+        TestMovementStrategy testMovementStrategy = new TestMovementStrategy(MOVE_FORWARD_VALUE - 1);
+        assertThat(testMovementStrategy.couldMove()).isFalse();
+    }
+}
