7537c3bbf3a0baf21d2b3ff070ef5c8168931754
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-01 18:52:25 +0900

    feat(racingcar): 사용자 이동 횟수 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,18 +11,34 @@ public class Application {
         // TODO: 프로그램 구현
 
         String[] carNames = readLine().split(",");
-        if (!isNameValid(carNames)){
+        if (!isNameValid(carNames)) {
             throw new IllegalArgumentException();
         }
+
+        String turnCnt = readLine();
+        if (!isCntValid(turnCnt)) {
+            throw new IllegalArgumentException();
+        }
+
     }
 
-    public static boolean isNameValid(String[] names){
-        for (String name: names){
-            if (name.length() > 5){
+    public static boolean isNameValid(String[] names) {
+        for (String name : names) {
+            if (name.length() > 5) {
                 return false;
             }
         }
         return true;
+    }
+
+    public static boolean isCntValid(String cnt) {
+        if (cnt.matches("[0-9]+")) {
+            if (cnt.equals("0")){
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
 
