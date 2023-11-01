df8032eb02859486c68610f3b27d7cdf2e7d260a
김현재 <khjmr@naver.com>
2023-11-01 15:38:51 +0900

    feat(playGame): 게임 진행에 필요한 기본정보 입력받기

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,26 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.view.input.Input;
+import racingcar.view.output.Output;
+
+public class GameController {
+    private final Input inputView = new Input();
+    private final Output outputView = new Output();
+
+    public void playGame() {
+        Cars cars = makeCars();
+        Integer tryNumber = inputTryNumber();
+    }
+
+    private Cars makeCars() {
+        outputView.printInputCarName();
+        String[] carNames = inputView.inputCarNames();
+        return Cars.makeCarsByNames(carNames);
+    }
+
+    private Integer inputTryNumber() {
+        outputView.printInputTryNumber();
+        return inputView.inputTryNumber();
+    }
+}
