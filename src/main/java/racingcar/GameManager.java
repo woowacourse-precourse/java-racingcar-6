215555438fa051cbe75b11a0dfeaf850947012ee
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 18:33:08 +0900

    feat: 게임 진행 후 상황 출력 메소드 추가

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
+    public static final String DISTANCE_CHAR = "-";
     private final List<Car> cars = new ArrayList<>();
     private int numOfExecutions;
     private final InputView inputView = new InputView();
@@ -22,5 +23,24 @@ public class GameManager {
 
         System.out.println(Message.GAME_START_COUNT);
         this.numOfExecutions = inputView.readNumOfExecutions();
+    }
+
+    public void play() {
+        System.out.println(Message.GAME_RESULT);
+        for (int i = 0; i < numOfExecutions; i++) {
+            runGame();
+        }
+    }
+    protected void runGame() {
+        for (Car car : cars) {
+            int distance = Randoms.pickNumberInRange(0, 9);
+            if (distance >= 4) {
+                car.move(distance);
+            }
+
+            System.out.printf("%s : %s\n", car.getName(), DISTANCE_CHAR.repeat(car.getDistance()));
+        }
+
+        System.out.print("\n");
     }
 }
