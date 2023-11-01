84b2d19e8e0fa76e00c0d2fc8fff1a54fc5693e1
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-01 21:13:34 +0900

    feat:"magic number를 통해 상수처리 완료"

---
+++ src/main/java/racingcar/constant/ValidatorConstant.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public class ValidatorConstant {
+    public static final int RANDOM_NUMBER=4;
+    public static final int MIN_RANDOM_RANGE = 0;
+    public static final int MAX_RANDOM_RANGE = 9;
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_NAME_LENGTH = 5;
+}
