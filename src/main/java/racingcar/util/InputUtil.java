39fe894baf0f42ec2dea7cd7e317ff7d0dcb09da
yebin <dev.devin.kr@gmail.com>
2023-11-01 19:35:13 +0900

    fix(input-util): handle an exception for car name longer than 5

--- src/main/java/racingcar/util/InputUtil.java
+++ src/main/java/racingcar/util/InputUtil.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public class InputUtil {
     public static String[] splitCarName(String carName) {
-        return carName.split(",");
+        return validateCarName(carName);
+    }
+
+    private static String[] validateCarName(String carName) {
+        String[] carNameStrings = carName.split(",");
+
+        for (String carNameStr : carNameStrings) {
+            if (carNameStr.length() > 5 || carNameStr.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능하다.");
+            }
+        }
+
+        return carNameStrings;
     }
 }
