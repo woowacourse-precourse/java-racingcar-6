93fd6bca6baebd75280576aa2e31135584a95261
seona-moon <fhflwhwl5@gmail.com>
2023-11-01 18:42:12 +0900

    feat: 추가 예외 메세지 작성

--- src/main/java/racingcar/validation/ErrorMessage.java
+++ src/main/java/racingcar/validation/ErrorMessage.java
@@ -1,10 +1,11 @@
 package racingcar.validation;
 
 public enum ErrorMessage {
-  INVALID_LENGTH("자동차의 이름이 6글자를 넘으면 안됩니다."),
-  DUPLICATED_NAME("동명의 자동차가 존재하면 안됩니다."),
-  CONTAIN_LETTER("숫자가 아닌 값이 입력되었습니다."),
-  EMPTY_INPUT("입력 값이 없습니다.");
+  INVALID_LENGTH("[ERROR] 자동차의 이름이 유효하지 않습니다. (5글자 이하여야 합니다)"),
+  DUPLICATED_NAME("[ERROR] 동명의 자동차가 존재하면 안됩니다."),
+  EMPTY_INPUT("[ERROR] 빈 값이 입력되었습니다."),
+  CONTAIN_LETTER("[ERROR} 시행 횟수는 숫자로만 입력해야 합니다."),
+  NOT_NATURAL_NUM("[ERROR] 시행 횟수는 자연수여야 합니다.");
 
   private final String message;
 
