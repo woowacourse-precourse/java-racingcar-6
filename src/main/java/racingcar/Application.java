3563cc61261094fd8ea67051b08e06de1d530dcf
주은 <00-10-21@naver.com>
2023-11-01 22:21:18 +0900

    feat: Implement car names input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,17 @@ package racingcar;
 import java.util.Scanner;
 
 public class Application {
-
     private static final Scanner scanner = new Scanner(System.in);
+
+    private static void validateCarNames(String carNamesInput) {
+        if (carNamesInput == null || carNamesInput.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = scanner.nextLine();
+        validateCarNames(carNamesInput);
     }
 }
