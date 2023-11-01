0127e0592612d9350552f090adb2694b6ab5d040
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 21:28:07 +0900

    fix : getCarNames 리스트로 변경

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 import camp.nextstep.edu.missionutils.Console;
 public class InputHandler {
     private static final Scanner scanner = new Scanner(System.in);
-    public static String getCarNames(String message){
+    public static String[] getCarNames(String message){
         System.out.print(message);
-        return Console.readLine();
+        return Console.readLine().split(",");
     }
     public static int getTryCount(String message){
         System.out.print(message);
