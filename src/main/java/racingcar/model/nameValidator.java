152f12fb4a548ace809177cad7af77bae92099a5
DonghaOh <donghadarwin@naver.com>
2023-11-01 16:04:05 +0900

    feat: 이름 입력 예외: 중복 - isNameUnique 메소드

--- src/main/java/racingcar/model/nameValidator.java
+++ src/main/java/racingcar/model/nameValidator.java
@@ -1,13 +1,15 @@
 package racingcar.model;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class nameValidator {
     public static void namesException(String names){
         isNamesNull(names);
         List<String> nameList = List.of(names.split(","));
         isNamesRightSize(nameList);
+        isNamesUnique(nameList);
     }
     public static void isNamesNull(String names) {
         if (names.isEmpty()) throw new IllegalArgumentException("자동차 이름을 입력해주세요");
@@ -19,5 +21,12 @@ public class nameValidator {
             if (name.length()>5) throw new IllegalArgumentException("이름은 5자 이하만 가능합니다");
             if (name.isEmpty()) throw new IllegalArgumentException("쉼표를 올바르게 사용해주세요");
         }
+    }
+    public static void isNamesUnique(List<String> nameList) {
+        Set<String> uniqueNames = new HashSet<>();
+        for (String name : nameList) {
+            if (!uniqueNames.add(name)) throw new IllegalArgumentException("중복된 이름을 사용하면 안됩니다");
+        }
+
     }
 }
