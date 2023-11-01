93bf33aa01d91e97b1fee215460f80d4d2cfb45d
inpink <inpink@kakao.com>
2023-11-01 22:45:51 +0900

    refactor: 중첩 for문을 메서드 분리하여 indent depth 줄임

--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.domain.racing.car.Car;
 import racingcar.domain.racing.RacingGame;
 
@@ -12,12 +13,15 @@ public class ConsoleOutputView implements OutputView {
         System.out.println(EXECUTION_RESULT_LABEL);
 
         for (int i = 0; i < racingGame.getTryCount(); i++) {
-            for (Car car : racingGame.getParticipationsList()) {
-                System.out.println(car.getName() + " : " + "-".repeat(car.findPositionAt(i))); //TODO: 분리
-            }
+            outputCarNameAndPosition(racingGame.getParticipationsList(), i);
             System.out.println();
         }
+    }
 
+    private void outputCarNameAndPosition(List<Car> carList, int index){
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.findPositionAt(index)));
+        }
     }
 
     @Override
