47d63457b280d7ca9fd84b2743179d216c53bd92
minkyuu <minkyu023@gmail.com>
2023-11-01 16:46:24 +0900

    refactor: 게임 설정/시작 코드 분리 및 리팩토링

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -1,7 +1,11 @@
 package racingcar.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.car.Car;
+import racingcar.message.ErrorMessage;
+import racingcar.message.Message;
 import racingcar.player.Player;
+import racingcar.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,16 +20,42 @@ public class Game {
         this.player = new Player();
     }
 
+    // 게임 시작 전 설정
+    public void setUp() {
+        setMoveCnt();
+        System.out.println();
+    }
+
     // 게임 시작
     public void start() {
-        // 이동 횟수 입력
-        moveCnt = player.inputMoveCnt();
+        System.out.println(Message.GAME_START);
 
         while (moveCnt > 0) {
-            // 전진 조건 확인
-
-            // 자동차 전진 or 멈춤
+            play();
             moveCnt--;
         }
+    }
+
+    private void setMoveCnt() {
+        System.out.println(Message.INPUT_MOVE_COUNT);
+        String inputMoveCnt = player.inputMoveCnt();
+
+        if (!Validator.validateMoveCnt(inputMoveCnt)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MOVE_CNT.getMessage());
+        }
+
+        moveCnt = Integer.parseInt(inputMoveCnt);
+    }
+
+    private void play() {
+        for (Car car : carList) {
+            if (isMovable()) {
+                car.move();
+            }
+        }
+    }
+
+    private boolean isMovable() {
+        return Randoms.pickNumberInRange(0,9) >= 4;
     }
 }
