0654cc9dddc0d7fc50bd5fc32559faa36b9c6492
unknown <hakie2kim@gmail.com>
2023-11-01 20:28:38 +0900

    style(src/test/java/racingcar/): consolidate indentation as 4 spaces

--- src/test/java/racingcar/CarRacingGameTest.java
+++ src/test/java/racingcar/CarRacingGameTest.java
@@ -14,19 +14,19 @@ class CarRacingGameTest extends NsTest {
     @Test
     void 최종_우승자가_여러명일_경우() {
         assertRandomNumberInRangeTest(
-                () -> {
-                    run("jun,camel,poro", "1");
-                    assertThat(output()).contains("jun : -", "camel : -", "poro : -", "최종 우승자 : jun, camel, poro");
-                },
-                5, 6, 8
+            () -> {
+                run("jun,camel,poro", "1");
+                assertThat(output()).contains("jun : -", "camel : -", "poro : -", "최종 우승자 : jun, camel, poro");
+            },
+            5, 6, 8
         );
     }
 
     @Test
     void 경주할_자동차_설정_중_입력값에_쉼표가_없는_경우() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi/jun/java", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("pobi/jun/java", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
 
     }
@@ -34,8 +34,8 @@ class CarRacingGameTest extends NsTest {
     @Test
     void 경주할_자동차_설정_중_중복된_이름이_있는_경우() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("jun,jun,camel", "3"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("jun,jun,camel", "3"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
 
     }
@@ -43,8 +43,8 @@ class CarRacingGameTest extends NsTest {
     @Test
     void 시도_횟수_설정_중_값이_0_이하인_경우() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("jun,pobi,camel", "-3"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("jun,pobi,camel", "-3"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
 
     }
