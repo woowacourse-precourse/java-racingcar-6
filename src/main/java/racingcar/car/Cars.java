edbe8b2a0ffba5634d6d67155e469f168e89c418
sudo-dustle <asdf748596123@gmail.com>
2023-11-01 18:40:03 +0900

    feat: Car 클래스를 담는 Cars 클래스 생성

--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -1,2 +1,48 @@
-package racingcar.car;public class Cars {
+package racingcar.car;
+
+import racingcar.util.RandomUniqueNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void addCars(Car car) {
+        cars.add(car);
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            int randomUniqueNumber = RandomUniqueNumber.pickRandomUniqueNumber();
+            car.canMove(randomUniqueNumber);
+        }
+    }
+
+    public List<String> getGameWinner() {
+        int maxPosition = getMaxPosition();
+
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .map(Car::getDistance)
+                .mapToInt(distance -> distance)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+    }
 }
