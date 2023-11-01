bef3e7a296562a36b4cd10e1f678d4afe3c54582
gunsookim <ccpc4896@naver.com>
2023-11-01 22:36:49 +0900

    test: 위치 테스트 기능 추가
    초기 위치가 0으로 초기화 되는지 테스트 하는 기능 추가.
    랜덤 생성된 숫자가 기준을 넘었을 때 전진 하는지 테스트 하는 기능 추가.
    랜덤 생성된 숫자가 기준을 못 넘었을 때 정지 하는지 테스트 하는 기능 추가.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
     private Car car;
+    private static final int MOVE_CONDITION = 4;
 
     @BeforeEach
     void setUp() {
@@ -16,4 +17,22 @@ public class CarTest {
     void Car의_이름_반환() {
         assertThat(car.getName()).isEqualTo("Test");
     }
+
+    @Test
+    void 생성시_초기_위치_확인() {
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 랜덤_숫자가_4_이상일_때_전진() {
+        car.move(MOVE_CONDITION);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 랜덤_숫자가_4_미만일_때_멈춤() {
+        car.move(MOVE_CONDITION - 1);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
 }
