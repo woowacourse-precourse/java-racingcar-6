49d81908ba524660c95c525d14945e851a01183e
soyoungxxx <otherplet@gmail.com>
2023-11-01 19:17:04 +0900

    style(racingcar): 클래스 통합

--- src/main/java/racingcar/domain/RoundPlaying.java
+++ src/main/java/racingcar/domain/RoundPlaying.java
@@ -1,10 +1,18 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RoundPlaying {
 
     boolean decideToMove (int randomNumber) {
         return randomNumber >= 4;
     } // randomNumber 가 4 이상이면 true 반환
 
-
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
