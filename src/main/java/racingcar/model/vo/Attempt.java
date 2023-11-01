9947f84a4f4574864954dd5a6443be3e01173448
baksakcci <pshsh910@gmail.com>
2023-11-01 15:07:21 +0900

    fix($Attempt): recreate `Attempt`
    - fixed an issue with the IDE not recognizing it.

--- src/main/java/racingcar/model/vo/Attempt.java
+++ src/main/java/racingcar/model/vo/Attempt.java
@@ -4,11 +4,34 @@ public class Attempt {
 
     private Integer count;
 
-    public Attempt(Integer count) {
-        this.count = count;
+    private Attempt(String count) {
+        validate(count);
+        this.count = Integer.parseInt(count);
     }
 
-    public Integer getCount() {
+    public static Attempt from(String count) {
+        return new Attempt(count);
+    }
+
+    private void validate(String attempt) {
+        isBlank(attempt);
+        isNumeric(attempt);
+    }
+
+    private void isBlank(String attempt) {
+        if (attempt.isBlank()) {
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
+        }
+    }
+
+    private void isNumeric(String attempt) {
+        if (!attempt.matches("\\d")) {
+            throw new IllegalArgumentException("숫자 이외에 입력할 수 없습니다.");
+        }
+    }
+
+    public Integer getIterateCount() {
         return count;
     }
 }
+
