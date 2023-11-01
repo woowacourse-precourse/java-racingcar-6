36e0c3fb258f0a2f601bd7d2fbf9a09ac4617287
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 11:17:41 +0900

    [feat] Application 메인 메소드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.Controller.RacingCarGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarGame racingCarGame = new RacingCarGame();
+
+        racingCarGame.play();
     }
 }
