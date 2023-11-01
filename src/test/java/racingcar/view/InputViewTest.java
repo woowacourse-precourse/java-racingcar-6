1c9a3f59bc3c63d0ad49b723aa7e193d42c4bc10
Maljaaa <Maljaaa@github.com>
2023-11-01 23:44:52 +0900

    refactor(InputViewTest): 변수명 변경
    - systemIn -> originalSystemIn
    - input -> inputForTest
    - in -> testInStream
    - expected, actual -> expectedCarNames, actualCarNames, expectedAttemptCount, actualAttemptCount

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -10,44 +10,44 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class InputViewTest {
 
-    private final InputStream systemIn = System.in;
+    private final InputStream originalSystemIn = System.in;
 
     @BeforeEach
     public void setUpInput() {
-        String input = "pobi,woni,jun\n5\n";
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        String inputForTest = "pobi,woni,jun\n5\n";
+        ByteArrayInputStream testInStream = new ByteArrayInputStream(inputForTest.getBytes());
+        System.setIn(testInStream);
     }
 
     @AfterEach
     public void restoreInput() {
-        System.setIn(systemIn);
+        System.setIn(originalSystemIn);
     }
 
     @Order(1)
     @Test
     public void 자동차_이름_입력() throws Exception {
         // given
-        String expected = "pobi,woni,jun";
+        String expectedCarNames = "pobi,woni,jun";
 
         // when
-        String actual = InputView.getCarNamesFromUser();
+        String actualCarNames = InputView.getCarNamesFromUser();
 
         // then
-        assertEquals(expected, actual, "Input does not match expected car names!!!");
+        assertEquals(expectedCarNames, actualCarNames);
     }
 
     @Order(2)
     @Test
     public void 게밍_시도_횟수_입력() throws Exception {
         //given
-        int expected = 5;
+        int expectedAttemptCount = 5;
 
         //when
-        int actual = InputView.getAttemptCountFromUser();
+        int actualAttemptCount = InputView.getAttemptCountFromUser();
 
         //then
-        assertEquals(expected, actual, "Input does not match expected attempts!!!");
+        assertEquals(expectedAttemptCount, actualAttemptCount);
     }
 
 }
