db79cf546f7e6474eee7939a2376cc6259c9e80f
최휘용 <chy2581@naver.com>
2023-11-01 15:20:29 +0900

    test: 결과 및 입력 예외 처리 테스트
    assertRandomNumberInRangeTest를 통해 제대로 된 결과가 잘 나오고 잘 출력하는지 확인하는 케이스와
    이름, 회수의 입력 예외 처리가 잘 됐는지 확인하는 케이스를 ApplicationTest에 추가했습니다

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,9 +24,69 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 전진_정지2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("a,b,c,d,e,f,g,h,i,j", "1");
+                    assertThat(output()).contains("a : ", "b : ", "c : ", "d : ", "e : -", "f : -",
+                            "g : -", "h : -", "i : -", "j : -", "최종 우승자 : e, f, g, h, i, j");
+                },
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("a,b,c,d,e,f,g,h,i,j", "1");
+                    assertThat(output()).contains("a : ", "b : -", "c : ", "d : -", "e : ", "f : -",
+                            "g : ", "h : -", "i : -", "j : -", "최종 우승자 : b, d, f, h, i, j");
+                },
+                0, 9, 1, 8, 2, 7, 3, 6, 4, 5
+        );
+    }
+
+    @Test
+    void 우승자_출력_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "3");
+                    assertThat(output()).contains("pobi : -", "woni : ", "jun : ", "pobi : -", "woni : -",
+                            "jun : ", "pobi : -", "woni : -", "jun : -", "최종 우승자 : pobi, woni, jun");
+                },
+                MOVING_FORWARD, STOP, STOP, STOP, MOVING_FORWARD, STOP, STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,,,,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 회수에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-10"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
