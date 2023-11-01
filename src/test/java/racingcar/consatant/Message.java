34413fa395fdae6404150ea28f01b5a64f162459
Kim Taeyeong <tyeong1102@gmail.com>
2023-11-01 20:23:07 +0900

    feat: Message 추가

---
+++ src/test/java/racingcar/consatant/Message.java
@@ -0,0 +1,18 @@
+package racingcar.consatant;
+
+public class Message {
+
+    public static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static final String INPUT_COUNT_TRY_MSG = "시도할 회수는 몇회인가요?";
+
+    public static final String GAME_RESULT_HEADER = "실행 결과";
+
+    public static final String GAME_WINNER = "최종 우승자";
+
+    public static final String CAR_POSITION_CHECK = "-";
+
+    public static final String DRIVER_NAME_RESULT = " : ";
+
+    public static final String NAME_COMMA = ", ";
+}
