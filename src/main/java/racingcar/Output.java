8f4db4073e80a3167ac511879b072bbb2c4af7a6
leorb159 <ldgsocial159@naver.com>
2023-11-01 22:29:22 +0900

    test(ApplicationTest): 시도 횟수 입력에 대한 예외 테스트

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.util.ArrayList;
 
 public class Output {
+
     private String enterNameOfCar = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private String enterTryTime = "시도할 회수는 몇회인가요?";
     private String executionResult = "실행결과";
