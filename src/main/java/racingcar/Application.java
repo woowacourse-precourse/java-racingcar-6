88058863ba78ed4f2a5afaf18f3bc5c56723e734
jaeyoon0330 <91931755+jaeyoon0330@users.noreply.github.com>
2023-11-01 15:52:58 +0900

    feat: 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //입력 1. 경주할 자동차 이름 2. 시도할 횟수
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = Arrays.asList(Console.readLine().split(","));
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int cycleNumber = Integer.parseInt(Console.readLine());
+
+        //실행 알고리즘 + 출력 결과(cycle별)
+
     }
 }
