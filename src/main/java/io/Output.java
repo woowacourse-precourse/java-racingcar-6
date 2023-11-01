6e9b2bd8d412b11cd875900b1a1dad7656af2b12
daki <daki@pop-os.localdomain>
2023-11-01 17:28:19 +0900

    Feat(Output): 테스트를 통과하는 코드 작성
    테스크 토드를 통과하는 Output 코드를 작성하였습니다.

---
+++ src/main/java/io/Output.java
@@ -0,0 +1,27 @@
+package io;
+
+import domain.Car;
+import domain.Race;
+
+import java.util.ArrayList;
+
+public class Output {
+    public static final String ROUND_MESSAGE = "실행 결과";
+    public static final String RESULT_MESSAGE = "최종 우승자 :";
+
+    void printRoundResult() {
+        System.out.println(ROUND_MESSAGE);
+    }
+
+    void printWinnerList(ArrayList<Car> carList) {
+        StringBuilder result = new StringBuilder();
+        System.out.print(RESULT_MESSAGE);
+
+        for (Car car: carList) {
+            result.append(" " + car.getName() + ",");
+        }
+        result.deleteCharAt(result.length() - 1);
+
+        System.out.println(result.toString());
+    }
+}
