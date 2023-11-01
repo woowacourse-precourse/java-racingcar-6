aea84f6718cdcb1f213aade8add0692c4c92a67e
DaeyeonCho <daecho8@gmail.com>
2023-11-01 12:35:17 +0900

    perf: 스트림을 활용한 게임 클래스 메소드 구현
    기존의 게임 클래스의 메소드를 스트림을 이용하여 메소드를 다시 구현하였습니다.

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -3,47 +3,55 @@ package racingcar.game;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Car;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
 
 public class Game {
 
     public void start() {
-        List<Car> cars = this.giveCars();
-        int count = this.giveCount();
+        List<Car> cars = this.getCars();
+        int count = this.getAttemptCount();
         this.play(cars, count);
         this.result(cars);
     }
 
-    private List<Car> giveCars() {
+    private List<Car> getCars() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
         String nameInput = Console.readLine();
-        String[] carNames = nameInput.split(",");
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
+
+        List<String> carNames = stream(nameInput.split(","))
+                .toList();
+
+        List<Car> cars = carNames.stream()
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
         return cars;
     }
 
-    private int giveCount() {
+    private int getAttemptCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
-    private void play(List<Car> cars, int count) {
-        System.out.print("\n");
+    private void play(List<Car> cars, int attemptsCount) {
+        this.printBlankLine();
         System.out.println("실행 결과");
-        for (int i = 0; i < count; i++) {
-            for (Car car : cars) {
-                car.run();
-            }
-            System.out.print("\n");
+        for (int i = 0; i < attemptsCount; i++) {
+            cars.forEach(car -> car.run());
+            this.printBlankLine();
         }
     }
 
-    private void result(List<Car> cars) {
+    private void printBlankLine() {
+        System.out.print("\n");
+    }
+
+    void result(List<Car> cars) {
         System.out.print("최종 우승자 : ");
-        System.out.println(String.join(", ", Car.giveWinner(cars)));
+        Result result = new Result();
+        System.out.println(String.join(", ", result.showWinners(cars)));
     }
 }
