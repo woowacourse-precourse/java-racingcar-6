428bd147a2f5c2981451a2596001dd5482cb6fa9
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 19:39:04 +0900

    refactor : 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,8 @@ package racingcar;
 import racingcar.controller.RacingCarController;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        new RacingCarController().start();
-    }
+
+	public static void main(String[] args) {
+		new RacingCarController().start();
+	}
 }
