92ce54679d6089127518057b1081a13ed0bc3bc3
HongYeseul <ghd8119@naver.com>
2023-11-01 19:56:12 +0900

    :art: style(GameTest) 메서드 순서 변경

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -35,6 +35,18 @@ public class GameTest extends NsTest {
     }
 
     @Test
+    @DisplayName("입력한 게임 라운드 수에 따른 진행 상황 중계")
+    void broadCastGameBoard() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("ye,seul", "3");
+                    assertThat(output()).contains("ye : -", "ye : --", "ye : ---");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
     @DisplayName("승자가 한 명일 때")
     void makeWinner() {
         assertRandomNumberInRangeTest(
@@ -55,18 +67,6 @@ public class GameTest extends NsTest {
                     assertThat(output()).contains("최종 우승자 : ye, seul");
                 },
                 MOVING_FORWARD, MOVING_FORWARD
-        );
-    }
-
-    @Test
-    @DisplayName("입력한 게임 라운드 수에 따른 진행 상황 중계")
-    void broadCastGameBoard() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("ye,seul", "3");
-                    assertThat(output()).contains("ye : -", "ye : --", "ye : ---");
-                },
-                MOVING_FORWARD, STOP, MOVING_FORWARD
         );
     }
 
