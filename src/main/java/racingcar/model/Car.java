6e83a7cd9cc453c1b7d1915120fcb81a8c50ce57
nhn1030 <shgusska12@gmail.com>
2023-11-01 10:48:31 +0900

    feat: 자동차 데이터 모델링을 위한 Car 클래스 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    private String carName;
+    private int carCurrentLocation;
+
+    public Car(String name) {
+        this.carName = name;
+        this.carCurrentLocation = 0;
+    }
+}
