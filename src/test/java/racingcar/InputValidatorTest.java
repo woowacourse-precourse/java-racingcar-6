df3c1092e7ffda83b359cfa4d3ddc62e33c788d3
HongYeseul <ghd8119@naver.com>
2023-11-01 17:29:35 +0900

    :white_check_mark: test(InputValidatorTest): 5글자 이상 입력할 경우 예외 발생(포맷 수정)

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.game.validator.InputValidator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,11 +11,12 @@ import static racingcar.game.validator.ExceptionMessage.*;
 
 public class InputValidatorTest {
 
+    @ParameterizedTest
     @DisplayName("5글자 이상 입력 할 경우 예외 발생")
-    @Test
-    void lessThenFiveLetters(){
-        assertThatThrownBy(()-> InputValidator.lessThenFiveLetters("letter"))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining(LESS_THEN_FIVE_LETTERS.getMessage());
+    @ValueSource(strings = {"letter"})
+    void lessThenFiveLetters(String input){
+        assertThatThrownBy(()-> InputValidator.lessThenFiveLetters(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(LESS_THEN_FIVE_LETTERS.getMessage());
     }
 }
