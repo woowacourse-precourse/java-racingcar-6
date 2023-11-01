075af219b7e58dcb9052f0d9f5b1a6e25b534f1c
Laterre <laterre39@naver.com>
2023-11-01 18:22:18 +0900

    feat: 입력 한 자동차 이름과 기준을 검증하고 예외처리 stringValidation() 후 데이터를 가공하는 메서드 구현 enterCarName()

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,14 +1,27 @@
 package racingcar.service;
 
 import java.util.HashMap;
+import java.util.regex.Pattern;
+import racingcar.exception.Exception;
 
 public class RacingService {
+
+    private static final String STRING_REGEX = "^([A-Za-z]+(,[A-Za-z]+)+)$";
+
     public static HashMap<String, String> enterCarName(String args) {
+        String car = stringValidation(args);
         HashMap<String, String> cars = new HashMap<>();
-        String[] arr = args.split(",");
+        String[] arr = car.split(",");
         for (String carName : arr) {
             cars.put(carName, "");
         }
         return cars;
+    }
+
+    private static String stringValidation(String args) {
+        if (!Pattern.matches(STRING_REGEX, args)) {
+            Exception.invalidValueEntered();
+        }
+        return args;
     }
 }
