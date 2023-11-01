06881fc8bbe239792d6ee95ca458751422900ca7
JongMinCh0i <wonderland314592@gmail.com>
2023-11-01 17:13:15 +0900

    fix(InputToCarList): 리터럴 변수명 변경

--- src/main/java/racingcar/domain/InputToCarList.java
+++ src/main/java/racingcar/domain/InputToCarList.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 
 public class InputToCarList {
     private static final String NO_UNIQUE_NAME = "중복되는 이름이 있습니다.";
-    private static final String NO_PLAYERS_PARTICIPATE = "문장의 마지막 문자는 컴마(',') 를 허용하지 않습니다.";
-    private static final String END_OF_SENTENCE_CHARACTER = "모집 된 선수가 없습니다.";
+    private static final String END_OF_SENTENCE_CHARACTER = "문장의 마지막 문자는 컴마(',') 를 허용하지 않습니다.";
+    private static final String NO_PLAYERS_PARTICIPATE = "모집 된 선수가 없습니다.";
     private static final String USER_DEFAULT_DELIMITER = ",";
 
     private static class SingletonHelper {
