4a1712f96e3e6856a95e422a6f9bc04c98de755f
Sung Hyobin <hellmir610@gmail.com>
2023-11-01 17:10:07 +0900

    feat: Add informing view to choose cars to race

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,8 +1,11 @@
 package racingcar.controller;
 
+import racingcar.view.Printer;
+
 public class RacingcarController {
 
     public static void startRacingcarGame() {
+        Printer.printCarChoiceMessage();
     }
 
 }
---
+++ src/main/java/racingcar/view/Printer.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import static racingcar.constant.InfoMessage.*;
+
+public class Printer {
+
+    public static void printCarChoiceMessage() {
+        System.out.println(CAR_CHOICE);
+    }
+
+}
