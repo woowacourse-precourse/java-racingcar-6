16a802688e5919550a786e3114cba7eca798c63f
simjaeyoun <spqjfgogo@gmail.com>
2023-11-01 19:01:28 +0900

    feat: '출력화면 생성'

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,32 @@
 package view;
 
+import domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+    public static final String RESULT_START_MESSAGE = "실행 결과";
+    public static final String RESULT_WINNER_MESSAGE = "최종 우승자 :";
+
+    public static String middleEachResult(Car car){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(car.getName() + " : ");
+        for(int i=0; i< car.getForward(); i++){
+            sb.append("-");
+        }
+        sb.append("\n");
+
+        return sb.toString();
+    }
+    public static String middleResult(List<Car> cars){
+        StringBuilder sb = new StringBuilder();
+
+        for(Car c : cars){
+            sb.append(middleEachResult(c));
+        }
+
+        return sb.toString();
+    }
+
 }
