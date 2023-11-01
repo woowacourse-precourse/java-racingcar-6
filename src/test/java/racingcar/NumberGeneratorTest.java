d5816a4a1129ce3ff48102ed779cfeb486a4da4c
psrom <psrom721@gmail.com>
2023-11-01 16:22:20 +0900

    feat(test): 공동 우승 테스트

--- src/test/java/racingcar/NumberGeneratorTest.java
+++ src/test/java/racingcar/NumberGeneratorTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class NumberGeneratorTest {
 
     @Test
-    void testGenerateRandomNumberInRange() {
+    void 랜덤_값_범위() {
         for (int i = 0; i < 100; i++) {
             int randomNumber = NumberGenerator.generateRandomNumber();
             assertTrue(randomNumber >= 0 && randomNumber <= 9, "0부터 9 사이의 숫자가 아닙니다.");
---
+++ src/test/java/racingcar/RaceWinnerTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RaceSimulator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class RaceWinnerTest {
+
+    @Test
+    void 공동_우승() {
+        HashMap<String, String> carResults = new HashMap<>();
+        carResults.put("pobi", "------");
+        carResults.put("woni", "------");
+        carResults.put("jun", "----");
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        RaceSimulator.raceWinner(carResults);
+
+        String output = outputStream.toString().trim();
+        assertEquals("최종 우승자 : pobi, woni", output);
+
+    }
+}
