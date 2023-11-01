a33fea2d9752748395c733c9d7e7551b5b362d2c
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-01 20:56:55 +0900

    style: 출력문 괄호 누락 수정

--- src/main/java/racingcar/utils/GameMessage.java
+++ src/main/java/racingcar/utils/GameMessage.java
@@ -1,7 +1,7 @@
 package racingcar.utils;
 
 public enum GameMessage {
-    GAME_START_COMMENT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"),
+    GAME_START_COMMENT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_TRY_COUNT("시도할 횟수는 몇회인가요?"),
     ONE_TURN_MESSAGE("실행 결과"),
     ANNOUNCE_WINNER("최종 우승자 : ");
