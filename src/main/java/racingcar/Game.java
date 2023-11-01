053171157f3a4ae863cf183ee0b1c28476d7a471
minwoo <mark42son@gmail.com>
2023-11-01 19:57:55 +0900

    feat : 시도할 횟수만큼 게임을 실행 반복하는 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
 
+    private static final String GAME_RESULT_MESSAGE = "실행 결과";
 
+    private final User user;
+    private final List<Car> cars;
 
-}
+    private int trialCounts;
+
+    public Game() {
+        user = new User();
+        cars = new ArrayList<>();
+        trialCounts = 0;
+    }
+
+    public void play() {
+        setRacingCarList();
+        setAttemptCounts();
+        System.out.println();
+        System.out.println(GAME_RESULT_MESSAGE);
+        for (int i = 0; i < trialCounts; i++) {
+            run();
+        }
+        printWinners();
+    }
