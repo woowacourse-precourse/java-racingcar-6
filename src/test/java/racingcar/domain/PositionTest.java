ebfdede155e546d7c323e4421db60903006227ec
GaHee99 <u1879@sookmyung.ac.kr>
2023-11-01 16:47:31 +0900

    refactor(PositionTest): BeforeEach적용

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -3,15 +3,21 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.not;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Position;
 import racingcar.util.Constants;
 
 public class PositionTest {
     private final int distance = Integer.parseInt(Constants.MINIMUM_DISTANCE_SIZE.constant);
+    Position position;
+    @BeforeEach
+    void init() {
+        position = new Position();
+    }
+
     @Test
     void 기준_미달_랜덤값에_따른_정지() {
-        Position position = new Position();
         int notGoDistance = distance - 1;
 
         position.validatePosition(notGoDistance);
@@ -20,7 +26,6 @@ public class PositionTest {
 
     @Test
     void 기준_이상_랜덤값에_따른_이동() {
-        Position position = new Position();
         int goDistance = distance;
 
         position.validatePosition(goDistance);
