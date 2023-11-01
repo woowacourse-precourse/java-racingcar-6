4ea4f0148a46a0ec10ef3cc04fdbc806a6f81a2d
compasstar <woohong24@naver.com>
2023-11-01 10:12:06 +0900

    feat: 추가 리포지토리

--- src/test/java/racingcar/respository/RacingRepository.java
+++ src/test/java/racingcar/respository/RacingRepository.java
@@ -2,8 +2,27 @@ package racingcar.respository;
 
 import racingcar.domain.Car;
 
+import java.util.List;
+
 public class RacingRepository {
 
     private static Integer attempts;
+    private static List<Car> cars;
 
+    public Car saveCar(Car car) {
+        cars.add(car);
+        return car;
+    }
+
+    public void saveAttempts(int num) {
+        attempts = num;
+    }
+
+    public List<Car> findCars() {
+        return cars;
+    }
+
+    public Integer findAttempts() {
+        return attempts;
+    }
 }
