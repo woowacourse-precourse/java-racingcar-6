5d9d0d0289ef84ad904de9aea0a7ab3fa3bf3eb9
leeminjun <sohwi0305@gmail.com>
2023-11-01 15:16:09 +0900

    feat: 자동차 위치 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+
+import racingcar.domain.Car;
+
 public class OutputView {
     public static void printHead() {
         System.out.println("실행 결과");
     }
-
+    private static void printPosition(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+    }
     public static void printException(String message) {
         System.out.println(message);
     }
