1e6585182098443eeb536b98af0464cbd2b93b3e
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-01 20:59:03 +0900

    feat: separate class Car
    Class Car is now separated to Car.java.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,21 +6,6 @@ import java.util.Arrays;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
-class Car{
-    String name;
-    int position = 0;
-
-    Car (String name, int position) {
-        if (name.length() > 5)
-            throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
-        this.name = name;
-        this.position = position;
-    }
-    public void moveCar() {
-        if (pickNumberInRange(0, 9) >= 4)
-            this.position++;
-    }
-}
 
 public class Application {
     public static void main(String[] args) {
