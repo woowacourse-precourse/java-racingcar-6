70e77c92f600fe60231997d458da5d46b30e77f6
GyungA <GyungA@users.noreply.github.com>
2023-11-01 21:19:21 +0900

    feat: 이동할 횟수 입력 기능

--- src/main/java/racingcar/model/User.java
+++ src/main/java/racingcar/model/User.java
@@ -2,6 +2,12 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
+    private Integer tryCount;
+    public Integer getUserInputForTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        this.tryCount = Integer.parseInt(Console.readLine());
+        return tryCount;
+    }
     public String[] getUserInputForCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = Console.readLine().split(",");
