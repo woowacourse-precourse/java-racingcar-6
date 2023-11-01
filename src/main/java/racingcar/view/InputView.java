99754bb3d5312279d50a79c7fd77df413e824179
bnfkim <kgr3819@gmail.com>
2023-11-01 17:01:26 +0900

    Refactor: util - InputView validate Validator class 로 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,18 +13,15 @@ public class InputView {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_GAME_ROUND = "시도할 회수는 몇회인가요?";
 
-    public static String inputCarName() {
+    public static ArrayList<String> inputCarName() {
         System.out.println(INPUT_CAR_NAME);
-        return getInput();
+        String input = getInput();
+        return getCarNames(input);
     }
     public static int inputGameRound(){
         System.out.println(INPUT_GAME_ROUND);
-        return Integer.parseInt(getInput());
-    }
-
-    public static ArrayList<Car> inputCars(ArrayList<String> carNames) {
-        validInputCarName(carNames);
-        return createCarListByCarNames(carNames);
+        String input = getInput();
+        return getGameRound(input);
     }
 
     public static String getInput(){
@@ -33,21 +30,15 @@ public class InputView {
         return input;
     }
 
-    public static void validInputGameRound(String input) {
-        CommonValidator.validInputBlank(input);
-        GameRoundValidator.validInputInteger(input);
-        GameRoundValidator.validNotUnderZero(Integer.parseInt(input));
+    public static ArrayList<String> getCarNames (String input) {
+        ArrayList<String> carNames = new ArrayList<>(Arrays.asList(input.split(",")));
+        CarNameValidator.validInputCarName(carNames);
+        return carNames;
     }
 
-    public static void validInputCarName(ArrayList<String> carNames) {
-        CarNameValidator.validCarNameBlank(carNames);
-        CarNameValidator.validCarNameLength(carNames);
-        CarNameValidator.validCarsDuplicate(carNames);
+    private static int getGameRound (String input) {
+        GameRoundValidator.validInputGameRound(input);
+        return Integer.parseInt(input);
     }
 
-    private static ArrayList<Car> createCarListByCarNames(ArrayList<String> carNames) {
-        ArrayList<Car> cars = new ArrayList<>();
-        for(String car : carNames) cars.add(new Car(car));
-        return cars;
-    }
 }
