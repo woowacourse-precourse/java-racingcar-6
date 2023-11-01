970a62858892ad7117086f716ab90f531fd4b5eb
HanDaWoon <bookpia.han@gmail.com>
2023-11-01 14:05:37 +0900

    refactor: 랜덤 숫자 생성 조건 상수화

--- src/test/java/racingcar/util/RandomNumberTest.java
+++ src/test/java/racingcar/util/RandomNumberTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomNumberTest {
+    private static final int MIN = 0;
+    private static final int MAX = 9;
+
     @Test
     void 랜덤_숫자_생성() {
-        assertThat(RandomNumber.generate()).isBetween(0, 9);
+        assertThat(RandomNumber.generate()).isBetween(MIN, MAX);
     }
 }
