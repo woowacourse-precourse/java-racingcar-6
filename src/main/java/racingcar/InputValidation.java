08e2bac9263556774defd0f3fb3735ecf92b5348
Hyunjun KIM <hihj070914@icloud.com>
2023-11-01 23:09:43 +0900

    refactor: modify who is responsible for input - 사용자 입력에 대한 책임을 Application 클래스에서 InputValidation 클래스로 수정하였습니다.

--- src/main/java/racingcar/InputValidation.java
+++ src/main/java/racingcar/InputValidation.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.HashSet;
 
 public class InputValidation {
@@ -10,14 +11,22 @@ public class InputValidation {
         HashSet<String> checkDuplicate = new HashSet<>(participantsNameList.length);
 
         for (String participantName : participantsNameList) {
-            if (participantName.length() >= 6)
+            if (participantName.length() >= 6) {
                 throw new IllegalArgumentException(participantName + " is too long!");
-            if (checkDuplicate.contains(participantName))
-                throw  new IllegalArgumentException(participantName + " already exists");
+            }
+            if (checkDuplicate.contains(participantName)) {
+                throw new IllegalArgumentException(participantName + " already exists");
+            }
             checkDuplicate.add(participantName);
         }
 
         return participantsNameList;
+    }
+
+    public static String[] askCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String rawCarNames = Console.readLine();
+        return InputValidation.validateInputNames(rawCarNames);
     }
 
     public static int validateInputMoveCount(String userInput) throws IllegalArgumentException {
@@ -28,9 +37,16 @@ public class InputValidation {
             throw new IllegalArgumentException(userInput + " is not an integer");
         }
 
-        if (moveCount < 0)
-            throw  new IllegalArgumentException(userInput + " is negative");
+        if (moveCount < 0) {
+            throw new IllegalArgumentException(userInput + " is negative");
+        }
 
         return moveCount;
+    }
+
+    public static int askTotalMoveCounts() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String rawMoveCount = Console.readLine();
+        return InputValidation.validateInputMoveCount(rawMoveCount);
     }
 }
