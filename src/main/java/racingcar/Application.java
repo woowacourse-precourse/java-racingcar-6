7f1e23fe9a8f6972817b7659724d1868da05e0fc
GilHun <suleemon1@naver.com>
2023-11-01 23:44:29 +0900

    chore: 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.view.Input;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        input.start();
     }
 }
