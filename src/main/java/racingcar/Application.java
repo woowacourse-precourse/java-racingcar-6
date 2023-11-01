d5628109150cecb1890c3dba7666ce7c0de4bcb9
KYBYK <dbqks56231106@gamil.com>
2023-11-01 19:28:12 +0900

    Feat : 레이싱카 게임 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarGameController racingGame = new RacingCarGameController();
+
+        racingGame.start();
+        racingGame.inProgress();
+        racingGame.end();
     }
 }
