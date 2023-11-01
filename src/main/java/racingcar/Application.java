d6941ca04f4239c65c7e57c0ea7ea7eae12b0ec2
unknown <t0paz_2357@naver.com>
2023-11-01 14:46:59 +0900

    테스트 코드 구현 시도

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -20,7 +20,7 @@ public class Application {
 		getWinner(carList);
 	}
 
-	private static List<Car> carListInput() throws IllegalArgumentException {
+	public static List<Car> carListInput() throws IllegalArgumentException {
 		// 자동차 이름 입력
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
@@ -36,7 +36,7 @@ public class Application {
 		return carList;
 	}
 
-	private static int carTryCount() {
+	public static int carTryCount() {
 		// 시도 회수 입력
 		System.out.println("시도할 회수는 몇회인가요?");
 
@@ -45,14 +45,14 @@ public class Application {
 		return carTryCount;
 	}
 
-	private static void diceThrow(List<Car> carList) {
+	public static void diceThrow(List<Car> carList) {
 		// 전진 조건 확인
 		for (Car car : carList)
 			if (Randoms.pickNumberInRange(0, 9) > 3)
 				car.setCarCount(car.getCarCount() + 1);
 	}
 
-	private static void printCarCurrentState(List<Car> carList) {
+	public static void printCarCurrentState(List<Car> carList) {
 		// 자동차 현황 출력
 		for (Car car : carList)
 			System.out.println(car.toString());
@@ -60,7 +60,7 @@ public class Application {
 		System.out.println();
 	}
 
-	private static void getWinner(List<Car> carList) {
+	public static void getWinner(List<Car> carList) {
 		List<String> winnerList = new ArrayList<String>();
 		int max = countMax(carList);
 
@@ -68,12 +68,12 @@ public class Application {
 		System.out.println("최종 우승자 : " + printWinner(winnerList));
 	}
 
-	private static String printWinner(List<String> winnerList) {
+	public static String printWinner(List<String> winnerList) {
 		// 승자 목록 출력
 		return String.join(", ", winnerList);
 	}
 
-	private static int countMax(List<Car> carList) {
+	public static int countMax(List<Car> carList) {
 		// 최고 점수만 가져옴
 		int max = 0;
 
@@ -84,7 +84,7 @@ public class Application {
 		return max;
 	}
 
-	private static List<String> compareMax(List<Car> carList, int max) {
+	public static List<String> compareMax(List<Car> carList, int max) {
 		// 승자 목록 생성
 		List<String> winnerList = new ArrayList<String>();
 
