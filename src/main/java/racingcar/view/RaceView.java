fb4186ed0f4ad5ccea4ef90bf6f72a2391792c3a
hangillee <skfcb10@naver.com>
2023-11-01 17:13:57 +0900

    chore: fix typo in view class

--- src/main/java/racingcar/view/RaceView.java
+++ src/main/java/racingcar/view/RaceView.java
@@ -12,7 +12,7 @@ public class RaceView {
     }
 
     public int inputRaceRound() {
-        System.out.println("시도할 횟수는 몇회인가요?");
+        System.out.println("시도할 회수는 몇회인가요?");
 
         String raceRoundString = Console.readLine();
         InputValidator.validateRaceRoundString(raceRoundString);
