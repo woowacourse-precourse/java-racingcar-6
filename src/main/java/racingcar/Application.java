2a99b1faa9aa2f542c0a5e9b0c84a929aa1e7cd4
Dowon <3120kang@gmail.com>
2023-11-01 20:35:17 +0900

    Refactor: 코드 정렬

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,7 @@ import racingcar.controller.RacingCarController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarController racingCarController=new RacingCarController();
+        RacingCarController racingCarController = new RacingCarController();
         racingCarController.start();
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,11 +4,11 @@ import racingcar.domain.Car;
 
 public class OutputView {
 
-    public void printForEach(Car car,String current){
-        System.out.println(car.getName()+" : "+current);
+    public void printForEach(Car car, String current) {
+        System.out.println(car.getName() + " : " + current);
     }
 
-    public void printWinner(String winners){
-        System.out.println("최종 우승자 : "+winners);
+    public void printWinner(String winners) {
+        System.out.println("최종 우승자 : " + winners);
     }
 }
