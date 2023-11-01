be240349fd5b8dbbbd7d110fb8e9c887718539bc
shortboy7 <thdwldnd7@naver.com>
2023-11-01 23:07:41 +0900

    feat : implement main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.factory.GameFactory;
+import racingcar.factory.impl.NamedCarGameFactory;
+import racingcar.game.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String count = Console.readLine();
+
+        GameFactory factory = new NamedCarGameFactory(names, count);
+        Game game = factory.createGame();
+        game.play();
     }
 }
