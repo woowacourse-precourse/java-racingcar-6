41c76d28c42dbf1785a826ac3872e5d64a785a0b
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-01 18:26:15 +0900

    feat(racingcar): 자동차가 임의의 수를 생성하여 전진하는지 판단

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,24 @@
-package racingcar;public class Car {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    public String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public boolean canMoveForward(int randomNumber) {
+        if (randomNumber <= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
 }
