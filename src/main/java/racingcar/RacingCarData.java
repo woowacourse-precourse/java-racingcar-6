820d4dd8f50d716ec332d95e8a1e6b1748dda252
Jung ui heon <ky7662@naver.com>
2023-11-01 21:38:50 +0900

    refactor : 입력값을 분리하는 메서드 수정

--- src/main/java/racingcar/RacingCarData.java
+++ src/main/java/racingcar/RacingCarData.java
@@ -1,12 +1,22 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.Set;
 
 public class RacingCarData {
     // 입력한 자동차 이름을 쉼표를 기준으로 나누어 해시테이블에 담는 separateRacingCar 메서드
     public LinkedHashMap<String, Integer> separateRacingCar(String racingCarNames){
         LinkedHashMap<String, Integer> racingCarMap = new LinkedHashMap<>(); // 순서를 보장하기 위해 LinkedHashMap 타입 해시테이블 생성
         String[] splitCarNames = racingCarNames.split(","); // 입력값을 쉼표를 기준으로 String 배열에 담는다
+
+        Set<String> carNameSet = new HashSet<>(); // 중복되는 이름이 존재하는지 확인하기 위해 Set 생성
+        for (String carName : splitCarNames) { // 중복되는 이름이 존재하면 예외 발생
+            if (!carNameSet.add(carName)) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         for(String CarName : splitCarNames){ // String 배열에 담는 자동차 이름을 순서대로 해시테이블에 담는다
             if(CarName.equals("")){ // 자동차 이름이 없다면 예외 발생
                 throw new IllegalArgumentException();
