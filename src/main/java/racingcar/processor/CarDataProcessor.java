a45d705e3aefdf2ed44f59db8fd0a34cf9f37e39
박은지 <qkrdmswl1018@naver.com>
2023-11-01 09:39:39 +0900

    refactor: 게임 시작 전 데이터 처리 로직 분리 및 리팩터링

---
+++ src/main/java/racingcar/processor/CarDataProcessor.java
@@ -0,0 +1,32 @@
+package racingcar.processor;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Name;
+
+public class CarDataProcessor {
+    private static final String SEPARATOR = ",";
+    private final Request request;
+
+    public CarDataProcessor(Request request) {
+        this.request = request;
+    }
+
+    public List<Car> process() {
+        String carNames = request.inputCarName();
+        return saveCars(carNames);
+    }
+
+    public int processCount() {
+        return request.inputGameCount();
+    }
+
+    public List<Car> saveCars(String inputCars) {
+        List<String> carNames = Arrays.stream(inputCars.split(SEPARATOR))
+                .toList();
+        return carNames.stream()
+                .map(carName -> new Car(new Name(carName)))
+                .toList();
+    }
+}
---
+++ src/main/java/racingcar/processor/Generator.java
@@ -0,0 +1,13 @@
+package racingcar.processor;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Generator {
+
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    public int randomNumberGenerate() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+}
