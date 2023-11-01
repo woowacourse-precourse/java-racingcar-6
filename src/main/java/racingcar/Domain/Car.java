defc972aaf63d514e3d1fc47cc75ab5001c1ab17
pletain <pletain@dgu.ac.kr>
2023-11-01 23:18:45 +0900

    feat: 다음 기능들을 위한 Car 도메인 생성

---
+++ src/main/java/racingcar/Domain/Car.java
@@ -0,0 +1,49 @@
+package racingcar.Domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Util.OperationStatus;
+
+public class Car {
+    private String name;
+    private String displacement;
+
+    public Car(String name) {
+        this.name = name;
+        this.displacement = "";
+    }
+
+    public void runRace() {
+        OperationStatus nextMovement = decide_movement();
+        if(nextMovement == OperationStatus.MOVEMENT) {
+            move_forward();
+        }
+    }
+
+    public void showDisplacement() {
+        System.out.println(this.name + " : " + this.displacement);
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void move_forward() {
+        this.displacement += "-";
+    }
+
+    private OperationStatus decide_movement() {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if (randomNumber >= 4) {
+            return OperationStatus.MOVEMENT;
+        }
+        return OperationStatus.STOP;
+    }
+
+
+}
+
+
