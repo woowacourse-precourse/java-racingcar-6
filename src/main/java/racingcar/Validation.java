c4b28ffc0da4632f2a658480acd36da260f0f429
S-DPR <thankyouformuch@naver.com>
2023-11-01 16:41:35 +0900

    feat: 문자열이 비어있는지 체크하는 메소드 추가

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -6,4 +6,9 @@ public class Validation {
             throw new IllegalArgumentException("이름의 길이는 " + limit + "글자를 넘으면 안됩니다.");
         }
     }
+    public static void validateNotEmptyName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("빈 이름이 있습니다.");
+        }
+    }
 }
