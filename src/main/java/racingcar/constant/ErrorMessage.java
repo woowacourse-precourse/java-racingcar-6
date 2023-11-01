63ddd2e987386f52554d10a7e07647c9d75cfc8e
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-01 23:59:22 +0900

    Refactor : 에러 메세지의 숫자를 상수화

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -1,12 +1,15 @@
 package racingcar.constant;
 
 public enum ErrorMessage {
+    FIVE("5"),
+    TWO("2"),
+    ZERO("0"),
     NON_NUMERIC_VALUE("숫자 이외의 값은 입력할 수 없습니다."),
     EMPTY_VALUE("공백은 입력할 수 없습니다."),
-    OUT_OF_STANDARD("자동차 이름은 5자 까지만 입력할 수 있습니다."),
+    OUT_OF_STANDARD("자동차 이름은" + FIVE + "자 까지만 입력할 수 있습니다."),
     DUPLICATED_VALUES("서로 중복되는 이름은 입력할 수 없습니다."),
-    SINGLE_COUNT("2개 이상의 이름을 입력해주세요."),
-    NEGATIVE_VALUE("0보다 큰 숫자만 입력할 수 있습니다.");
+    SINGLE_COUNT(TWO + "개 이상의 이름을 입력해주세요."),
+    NEGATIVE_VALUE(ZERO + "보다 큰 숫자만 입력할 수 있습니다.");
 
     private final String message;
 
