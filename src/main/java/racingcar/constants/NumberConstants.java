91f73c0afb02da117571dacff317874e5fef3bbe
dhl1924 <dhl1924@naver.com>
2023-11-01 22:52:08 +0900

    Refactor: Extracted a Constant for MOVE_THRESHOLD

--- src/main/java/racingcar/constants/NumberConstants.java
+++ src/main/java/racingcar/constants/NumberConstants.java
@@ -4,4 +4,6 @@ public class NumberConstants {
     public static final int MIN_RANGE = 0;
     public static final int MAX_RANGE = 9;
     public static final int MAX_CAR_NAME_LENGTH = 5;
+
+    public static final int MOVE_THRESHOLD = 4;
 }
