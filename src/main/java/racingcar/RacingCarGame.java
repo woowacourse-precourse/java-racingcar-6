046ccc542338f6c53768cd35c3eb5a31478be0dd
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-11-01 17:52:07 +0900

    feat/racingCarGame: 예외를 확인하는 checkInputNumValid함수와 checkCarNameValid함수 작성, init()함수 작성완료

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -21,19 +21,26 @@ public class RacingCarGame {
     private void init(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+        generateListOfRacingCars(carNames);
+
         System.out.println("시도할 회수는 몇회인가요?");
         String inputNum = Console.readLine();
-
-        checkInputValid(carNames,inputNum);
-
+        checkInputNumValid(inputNum);
         setNumOfPlaying(Integer.parseInt(inputNum));
-        generateListOfRacingCars(carNames);
+
     }
 
-    private void checkInputValid(String carNames,String inputNum){
-        //TODO: carNames 5자리 이하인지 확인
-
-        //TODO: inputNum이 숫자로 변환할수 있는지 확인
+    private void checkInputNumValid(String inputNum){
+        try{
+            Integer.parseInt(inputNum);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void checkCarNameValid(String carName){
+        if (carName.length()>6){
+            throw new IllegalArgumentException();
+        }
     }
 
     private void generateListOfRacingCars(String carNames){
@@ -41,6 +48,7 @@ public class RacingCarGame {
         List<String> listOfCarNames = stringToListOfString(carNames);
 
         for(String carName:listOfCarNames){
+            checkCarNameValid(carName);
             RacingCar racingCar = generateRacingCar(carName);
             racingCars.add(racingCar);
         }
