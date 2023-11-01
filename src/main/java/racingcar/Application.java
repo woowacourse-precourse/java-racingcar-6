28f1684ff1619b5054a887bcc5360bebcb4bfd78
eora21 <eora21@naver.com>
2023-11-01 14:14:15 +0900

    refactor: 💡 수정 View 인터페이스 삭제, View의 구현체였던 IOView 이름을 View로 변경
    View가 지닌 행동을 IOView가 InputView, OutputView를 이용하여 실행하고 있었으나 이를 오용할 시
    View만 확장, 특정 Input과 Output을 인터페이스를 통하지 않은 채 구현할 가능성이 있어 Application main 내의 클래스를
    변경하여 적용할 수 있도록 강제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,16 +3,15 @@ package racingcar;
 import racingcar.controller.RacingGameController;
 import racingcar.view.ConsoleInputView;
 import racingcar.view.ConsoleOutputView;
-import racingcar.view.IOView;
+import racingcar.view.View;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
-import racingcar.view.View;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new ConsoleInputView();
         OutputView outputView = new ConsoleOutputView();
-        View view = new IOView(inputView, outputView);
+        View view = new View(inputView, outputView);
         RacingGameController racingGameController = new RacingGameController(view);
         racingGameController.play();
     }
--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -4,12 +4,30 @@ import java.util.List;
 import racingcar.dto.RacingGameStatus;
 import racingcar.dto.WinnerNames;
 
-public interface View {
-    List<String> getCarNames();
+public class View {
+    private final InputView inputView;
+    private final OutputView outputView;
 
-    int getRaceCount();
+    public View(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
-    void showRacingGameStatus(RacingGameStatus racingGameStatus);
+    public List<String> getCarNames() {
+        outputView.requestCarNames(inputView.getDelimiterDescription(), inputView.getDelimiter());
+        return inputView.enterCarNames();
+    }
 
-    void showWinnerNames(WinnerNames winnerNames);
+    public int getRaceCount() {
+        outputView.requestRaceCount();
+        return inputView.enterRaceCount();
+    }
+
+    public void showRacingGameStatus(RacingGameStatus racingGameStatus) {
+        outputView.printRacingGameStatus(racingGameStatus);
+    }
+
+    public void showWinnerNames(WinnerNames winnerNames) {
+        outputView.printWinnerNames(winnerNames);
+    }
 }
