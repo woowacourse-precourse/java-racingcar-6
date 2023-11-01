71947d1e3c07be4917cd17c3d5ab6d3a685dafbe
Jason <jasonecy@gmail.com>
2023-11-01 20:25:43 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = readCarNames();
+    }
+
+    private static String[] readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine().split(",");
     }
 }
