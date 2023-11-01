23e6b738c566bec6cf69e90843b83cf809963d19
masonkimseoul <masonkimseoul@gmail.com>
2023-11-01 16:54:01 +0900

    feat : Add RaceController and allocate name for car object

---
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -0,0 +1,31 @@
+package racingcar.Controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.Domain.Car;
+import racingcar.Domain.Cars;
+import racingcar.View.OutputView;
+
+public class RaceController {
+
+    private static final String SEPARATOR = ",";
+    private static final int MINIMUM_TRY_COUNT = 1;
+    private static final int MINIMUM_ENTRY_COUNT = 2;
+
+    private final String carNames;
+    private final int tryCount;
+
+    public RaceController(String carNames, int tryCount) {
+        this.carNames = carNames;
+        this.tryCount = tryCount;
+    }
+
+    private Cars makeCars(String carNames) {
+        String[] entries = carNames.split(SEPARATOR);
+
+        return new Cars(Arrays.stream(entries)
+                .map(Car::new)
+                .collect(Collectors.toList()));
+    }
+}
