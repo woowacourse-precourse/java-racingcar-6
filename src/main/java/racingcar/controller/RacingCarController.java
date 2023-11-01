ef8eb8aa60387967ba00619955cb5ee19ba73900
COBI-98 <tkdgus968@naver.com>
2023-11-01 16:12:22 +0900

    feat: 자동차 경주 컨트롤러 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,12 @@
+package racingcar.controller;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+public class RacingCarController {
+
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    public void startGame(){
+    }
+
+
+}
