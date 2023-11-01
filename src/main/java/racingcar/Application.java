63e510721415ad67a9654f36a3ab1e3ff308ba8f
seonghun <sdzx0719@naver.com>
2023-11-01 18:19:39 +0900

    test: moveTry 테스트 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         Racing racing = new Racing();
-
         racing.start();
     }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,11 +10,29 @@ import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
 public class StringTest extends NsTest {
+    private static final String USER_A = "pobi";
+    private static final String USER_B = "hun";
 
     @Test
     void split_자동차_이름_입력받기_null_값_예외확인() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,,jun"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void split_자동차_이름_입력받기_공백값_예외확인() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, ,jun"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void split_주행_횟수_문자입력_예외확인(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun","열번시도"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
