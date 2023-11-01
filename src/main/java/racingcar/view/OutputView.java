8ea268d859d70b2bfaeb7fefbd4d59e88cc9cd2f
IDpluto <d0013159@gmail.com>
2023-11-01 10:43:16 +0900

    refactor: OutputView와 RaceGameService 간의 결합을 줄이기 위해 printRaceResult 가 차량 목록을 매개변수로 받도록 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,20 +1,28 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.domain.Car;
 import racingcar.service.RaceGameService;
 
-import java.util.List;
-
 public class OutputView {
 
-  public void printRaceResult() {
-    for (Car car : RaceGameService.getCars()) {
-      System.out.println(car.getCarName() + " : " + "-".repeat(car.getProgressCount()));
+    public void printCarNameInputPrompt() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
-    System.out.println();
-  }
 
-  public void printGameWon(String winners) {
-    System.out.println("최종 우승자 : " + winners);
-  }
+    public void printCarProgressCountInputPrompt() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
+
+    public void printRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getProgressCount()));
+        }
+        System.out.println();
+    }
+
+
+    public void printGameWon(String winners) {
+        System.out.println("최종 우승자 : " + winners);
+    }
 }
