979bf4a3a2afcd6cd9495d6283cdc7b8c73575c8
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-01 16:14:15 +0900

    refactor: List<Car>의 변경 가능성 최소화 Collections.unmodifiableList() 사용

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -16,6 +17,6 @@ public class Cars {
     }
 
     public List<Car> getCarList() {
-        return carList;
+        return Collections.unmodifiableList(carList);
     }
 }
