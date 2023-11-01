d24cd9bdc83362d8b5b5e8176dff19dba9c8b35d
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 18:44:09 +0900

    feat(Output): 시도할 회수를 입력하라는 메세지를 출력 기능 구현
    시도할 회수를 입력하라는 메세지를 출력하는 메서드 promptForTryCount()을 추가하였다.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -3,7 +3,8 @@ package racingcar;
 public class Output {
 
     public enum Message {
-        PROMPT_FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        PROMPT_FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        PROMPT_FOR_TRY_COUNT("시도할 회수는 몇회인가요?")
         ;
 
         private final String label;
@@ -23,7 +24,10 @@ public class Output {
         System.out.println(Message.PROMPT_FOR_CAR_NAME.label());
     }
 
-    // TODO : 시도할 회수를 입력하라는 메세지를 출력합니다.
+    // 시도할 회수를 입력하라는 메세지를 출력합니다.
+    public void promptForTryCount() {
+        System.out.println(Message.PROMPT_FOR_TRY_COUNT.label());
+    }
 
     // TODO : 각 차수별 실행 결과를 출력합니다.
 
