da1c9ec87aefae08809c6143cf792aa31dfe8c6a
Yminji <ymg0818@naver.com>
2023-11-01 21:23:35 +0900

    feat(racingcar) : main 메서드 실행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Racing;
+import racingcar.domain.RacingCarName;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        RacingCarName racingCarName = new RacingCarName();
+        List<String> carNames = racingCarName.carNameList();
+
+        Racing racing = new Racing();
+        racing.countMoveRacing(carNames);
     }
 }
