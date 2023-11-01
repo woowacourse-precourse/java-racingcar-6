f83354d8f0deb3ea1fea0b3ee780c4f4d9a71330
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-01 16:29:46 +0900

    Feat[controller] 플레이어 입력 받은 후 게임 초기화

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,4 +1,33 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+import racingcar.service.RacingcarService;
+import racingcar.util.CarNameParser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class RacingcarController {
+
+    private final RacingcarService racingcarService;
+    private final InputView inputView;
+    private final OutputView outputView;
+    public RacingcarController(){
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        Race race = initRace();
+        this.racingcarService = new RacingcarService(race);
+    }
+    private Race initRace() {
+        String carNamesInput = inputView.readCarNames();
+        List<String> carNames = CarNameParser.parse(carNamesInput);
+        List<Car> cars = Car.createCars(carNames);
+
+        String roundInput = inputView.readRound();
+        int rounds = Integer.parseInt(roundInput);
+
+        return new Race(cars, rounds);
+    }
 }
