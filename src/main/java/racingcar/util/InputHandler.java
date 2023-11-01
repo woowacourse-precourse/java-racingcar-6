30d98339915e6d3da4060b04df8a898cf599de2b
yy8305 <yy8305@kakao.com>
2023-11-01 15:49:09 +0900

    feat(InputHandler): 사용자가 입력하는 숫자에 대한 유효성 검사 추가

--- src/main/java/racingcar/util/InputHandler.java
+++ src/main/java/racingcar/util/InputHandler.java
@@ -1,9 +1,41 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputHandler {
+    private static final String NUMBER_REGEX = "[^0-9]";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile(NUMBER_REGEX);
+
     public static String getUserInput() {
         return Console.readLine().trim();
+    }
+
+    public static Integer getUserInputNumber() {
+        String userInput = Console.readLine();
+
+        validateIsNumber(userInput);
+
+        return convertToInt(userInput);
+    }
+
+    private static void validateIsNumber(String userInput) {
+        if (isNotNumber(userInput)) {
+            throw new IllegalArgumentException("user input is not number. user input : " + userInput);
+        }
+    }
+
+    private static boolean isNotNumber(String userInput) {
+        Matcher matcher = NUMBER_PATTERN.matcher(userInput);
+        return matcher.matches();
+    }
+
+    private static Integer convertToInt(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
