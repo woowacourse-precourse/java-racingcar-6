65ff3fd007a622b9ebb65fe12c5b561322f24e05
siyeonSon <siyeon0209@naver.com>
2023-11-01 09:36:26 +0900

    refactor(inputview, cars): 시스템 입력과 비즈니스 로직 분리

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,27 +1,14 @@
 package racingcar.model;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import racingcar.util.NumberGenerator;
 
 public class Cars {
-    private static final String DELIMITER = ",";
 
     private final List<Car> cars;
 
-    public Cars(String carNames) {
-        cars = stringToCarList(carNames);
-    }
-
     public Cars(List<Car> cars) {
         this.cars = cars;
-    }
-
-    private List<Car> stringToCarList(String carNames) {
-        return Stream.of(carNames.split(DELIMITER))
-                .map(Car::new)
-                .toList();
     }
 
     public void forward(NumberGenerator numberGenerator) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,23 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Stream;
+import racingcar.model.Car;
 import racingcar.model.Cars;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String DELIMITER = ",";
 
     public Cars inputCarName() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String input = Console.readLine();
-        return new Cars(input);
+        List<Car> cars = Stream.of(input.split(DELIMITER))
+                .map(Car::new)
+                .toList();
+        return new Cars(cars);
     }
 
     public int inputCount() {
