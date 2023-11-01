27e9e717b5284f1293b7f17a1ea055bd5195b798
imhungry2 <sjnnj@naver.com>
2023-11-01 17:29:57 +0900

    controller - GameController 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import racingcar.model.RacingGame;
+import racingcar.view.ConsoleView;
+
+public class GameController {
+    private RacingGame game;
+    private ConsoleView view;
+
+    public GameController(RacingGame game, ConsoleView view) {
+        // 생성자 로직
+    }
+
+    public void startGame() {
+        // 게임 실행 로직
+    }
+}
