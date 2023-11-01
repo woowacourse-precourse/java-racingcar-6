b6d7d05abb6c0bf5bcdebeadb29e2f4e5e8e9036
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-01 21:29:44 +0900

    feat : car domain 구현

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Game {
+	private final int trial;
+	private final List<Car> cars;
+	private int trialNum = 0;
+
+	public Game(List<Car> cars, int trial) {
+		this.cars = cars;
+		this.trial = trial;
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void increaseTrialNum() {
+		this.trialNum += 1;
+	}
+
+	public Boolean checkReachTrialNumToTrial() {
+		return this.trial == this.trialNum;
+	}
+}
