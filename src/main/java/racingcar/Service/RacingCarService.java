be0e853afd484c7d09828e19b614fb3dceec1768
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 11:10:11 +0900

    [Refactor] Service 메소드 분리

--- src/main/java/racingcar/Service/RacingCarService.java
+++ src/main/java/racingcar/Service/RacingCarService.java
@@ -7,34 +7,47 @@ import racingcar.Domain.Car;
 import racingcar.Domain.Race;
 
 public class RacingCarService {
-    public List<String> playAndSelectWinners(Race race) {
-        List<String> winners = new ArrayList<>();
 
+    public void playRace(Race race) {
+        moveOrStopByRandomNumber(race);
+        race.increaseTrialCount();
+    }
+
+    private void moveOrStopByRandomNumber(Race race) {
         for (Car car : race.getCars()) {
             if (4 <= generateRandomNumber()) {
                 car.carMove(1);
             }
         }
-
-        race.increaseTrialCount();
-
-        int maxPosition = -1;
-        for (Car car : race.getCars()) {
-            if (maxPosition < car.getCarPosition()) {
-                maxPosition = car.getCarPosition();
-            }
-        }
-
-        for (Car car : race.getCars()) {
-            if (car.getCarPosition() == maxPosition) {
-                winners.add(car.getCarName());
-            }
-        }
-
-        return winners;
     }
 
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public List<String> selectWinners(Race race) {
+        List<Car> cars = race.getCars();
+        int maxPosition = decideMaxPosition(cars);
+        return decideMaxPositionCars(cars, maxPosition);
+    }
+
+    private List<String> decideMaxPositionCars(List<Car> cars, int maxPosition) {
+        List<String> carNames = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getCarPosition() == maxPosition) {
+                carNames.add(car.getCarName());
+            }
+        }
+        return carNames;
+    }
+
+    private int decideMaxPosition(List<Car> cars) {
+        int max = -1;
+        for (Car car : cars) {
+            if (max < car.getCarPosition()) {
+                max = car.getCarPosition();
+            }
+        }
+        return max;
     }
 }
