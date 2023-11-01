df85ab9c39fae855de15f7e7dadd2f86c6dcd4e9
inthejim <inthejim24@gmail.com>
2023-11-01 20:21:18 +0900

    fix: 우승자 알림 출력 문구 수정

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -6,7 +6,7 @@ public class View{
     private static final String CAR_NAME_PROMPT="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_PROMPT="시도할 회수는 몇회인가요?";
     private static final String CONDUCT_MESSAGE="실행 결과";
-    private static final String WINNER_MESSAGE="최종 우승자 :";
+    private static final String WINNER_MESSAGE="최종 우승자 : ";
 
     public static String getCarNames(){
         System.out.println(CAR_NAME_PROMPT);
