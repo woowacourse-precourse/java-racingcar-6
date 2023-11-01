831620cfc505511733865d5d01da45e186b7ded4
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-01 23:26:07 +0900

    style(test/view/InputSetting): 필요없는 주석 제거

--- src/test/java/racingcar/view/InputSetting.java
+++ src/test/java/racingcar/view/InputSetting.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 public class InputSetting {
-    // 사용자 입력 테스트
     protected void systemIn(String ... args){
         byte[] bytes = String.join("\n", args).getBytes();
         InputStream inputStream = new ByteArrayInputStream(bytes);
