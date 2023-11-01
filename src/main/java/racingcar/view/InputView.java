550a7b9f44c206e321f5fccc8da3485b8795a5ef
Hugh <lee2317884@gmail.com>
2023-11-01 11:59:24 +0900

    feat: 사용자 이동 횟수 입력 입력값 숫자 확인 예외 처리 기능
    * trim() 사용 으로 공백값 포함된 경우 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class InputView {
 
     private static final String PRINT_READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String PRINT_READ_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String PRINT_READ_TRY_NUMBER_MESSAGE = "시도할 횟수는 몇회인가요?";
     private static final String CAR_NAME_DELIMITER = ",";
 
     private InputView() {
@@ -18,11 +18,6 @@ public class InputView {
         return convertCarName(getInput());
     }
 
-    public static String readGameRound() {
-        System.out.println(PRINT_READ_TRY_NUMBER_MESSAGE);
-        return getInput();
-    }
-
     private List<String> convertCarName(final String input) {
         return Arrays.stream(splitWithComma(input))
                 .map(String::trim)
@@ -31,6 +26,19 @@ public class InputView {
 
     private static String[] splitWithComma(final String input) {
         return input.split(CAR_NAME_DELIMITER);
+    }
+
+    public static int readGameRound() {
+        System.out.println(PRINT_READ_TRY_NUMBER_MESSAGE);
+        return convertGameRound(getInput());
+    }
+
+    private static int convertGameRound(final String input) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw  new IllegalArgumentException();
+        }
     }
 
     private static String getInput() {
