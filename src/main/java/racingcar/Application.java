2fad5a5b266b42d98a26460a9c89a0688241798b
eunjin <12191804@inha.edu>
2023-11-01 20:59:23 +0900

    feat: 게임 실행 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RacingGameView view = new RacingGameView();
+
+        List<String> carNames = view.requestCarNames();
+        int rounds = view.requestRounds();
+
+        RacingGame racingGame = new RacingGame(carNames, rounds);
+        racingGame.start(view);
     }
 }
