165f12cbbfab58283716e78275c223f76e09993f
songyi <song21677@naver.com>
2023-11-01 22:24:36 +0900

    style: 필요 없는 import 삭제

--- src/test/java/racingcar/ResultTest.java
+++ src/test/java/racingcar/ResultTest.java
@@ -1,15 +1,9 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ResultTest extends NsTest {
