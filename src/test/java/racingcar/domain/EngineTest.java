eca851bb00d76057051e367e947be2ccf5fe66c1
kite707 <12201680@inha.edu>
2023-11-01 20:14:47 +0900

    test(racingcar): 현재까지 게임 결과 출력 테스트코드 작성
    현재까지의 게임 결과를 출력하는 getCurrentResult함수의 테스트코드를 작성하였습니다.

--- src/test/java/racingcar/domain/EngineTest.java
+++ src/test/java/racingcar/domain/EngineTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class EngineTest {
@@ -15,6 +16,7 @@ class EngineTest {
     }
 
     @Test
+    @DisplayName("전진 여부 결정 테스트")
     void 전진_여부_결정_테스트() {
         boolean result = engine.isMovingForward();
         assertThat(result).isIn(true, false);
--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -8,9 +8,12 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class GameTest {
     private Game game;
+    private static final List<String> CAR_NAMES = Arrays.asList("aa", "bb", "cc");
 
     @BeforeEach
     void setUp() {
@@ -26,5 +29,13 @@ class GameTest {
         for (int i = 0; i < result.size(); i++) {
             assertThat(result.get(i)).isIn(1, 2);
         }
+    }
+
+    @ParameterizedTest(name = "현재 결과 출력 테스트 : aa : {0}, bb : {1}, cc : {2}")
+    @CsvSource({"1,2,3,aa : -, bb : --, cc : ---", "3,1,1,aa : ---, bb : -, cc : -"})
+    void 현재_결과_출력_테스트(int score1, int score2, int score3, String expected1, String expected2, String expected3) {
+        String expected = expected1 + "\n" + expected2 + "\n" + expected3 + "\n";
+        String result = game.getCurrentResult(CAR_NAMES, Arrays.asList(score1, score2, score3));
+        assertThat(result).isEqualTo(expected);
     }
 }
