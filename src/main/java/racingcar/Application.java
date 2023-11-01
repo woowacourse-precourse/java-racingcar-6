ad81255bb6965fcc2eb154215647eef4522a8909
jh9788 <gkwhdgns2@gmail.com>
2023-11-01 22:38:48 +0900

    Feat: getRandomNumber 함수 구현
    0부터 9까지 랜덤정수 생성함수 개발

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    static LinkedHashMap<String, Integer> carsMap = new LinkedHashMap<>();
+    static int count;
+    private static int getRandomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
