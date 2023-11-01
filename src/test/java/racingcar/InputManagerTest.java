9450dcc188df94015712c8a6314fb761aff9de3d
jjaegii <hn06038@gmail.com>
2023-11-01 18:57:01 +0900

    feat: InputManagerTest 클래스 내 throw_exception_for_invalid_car_name_length 메소드 추가

--- src/test/java/racingcar/InputManagerTest.java
+++ src/test/java/racingcar/InputManagerTest.java
@@ -1,5 +1,18 @@
 package racingcar;
 
-public class InputManagerTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
+
+public class InputManagerTest {
+    @Test
+    void throw_exception_for_invalid_car_name_length() {
+        InputManager inputManager = new InputManager();
+
+        String invalidName = "abcdef";
+
+        assertThatThrownBy(() -> inputManager.isValidName(invalidName))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
 }
