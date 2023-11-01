f3f38398d5362c11aa737a83c61d4293ef440267
hanriverkitty <danieltls@naver.com>
2023-11-01 14:32:02 +0900

    setCars, move 구현완료

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,33 +1,38 @@
 package domain;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    public void setCars(List<String>cars){
-        List<String> initCar = new ArrayList<String>();
-        for (int i=0;i<cars.size();i++){
-            initCar.add("");
+    //쉼표로 나눠진 차들을 기반으로 각각의 차의 위치 리스트 생성
+
+    List<String> carPos;
+
+    public List<String> setCars(List<String> cars) {
+        carPos = new ArrayList<String>();
+        for (int i = 0; i < cars.size(); i++) {
+            carPos.add("");
         }
-
+        return carPos;
     }
-    public void move(List<String> cars){
-        for (int i=0;i<cars.size();i++){
-            if(randomNum()){
 
-            }else {
-
+    public void move(List<String> cars) {
+        for (int i = 0; i < cars.size(); i++) {
+            if (randomNum()) {
+                carPos.set(i, carPos.get(i) + "-");
             }
         }
-
     }
-    public boolean randomNum(){
-        int num = Randoms.pickNumberInRange(0,9);
-        if (num>=4){
+
+    public boolean randomNum() {
+        int num = Randoms.pickNumberInRange(0, 9);
+        if (num >= 4) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
+
 }
