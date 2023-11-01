0ccd839b58d30f8f5dfc0c87ba808f0adbe0c047
bin <gimgyubin1225@gmail.com>
2023-11-01 16:33:35 +0900

    Refactor: 필드 변수명 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,7 +3,7 @@ package racingcar.domain;
 public class Car {
 
     private static final int LIMIT_NAME_LENGTH = 5;
-    private static final int THRESHOLD = 4;
+    private static final int THRESHOLD_TO_FORWARD = 4;
     private String name;
     private int location;
 
@@ -22,8 +22,8 @@ public class Car {
         return location;
     }
 
-    public int moves(int number) {
-        if (number >= THRESHOLD) {
+    public int moves(int randomNumber) {
+        if (randomNumber >= THRESHOLD_TO_FORWARD) {
             location++;
         }
         return location;
