685f085b7e37e210a266a15fc2b8da23d07b05a4
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 18:32:22 +0900

    feat: start시 사용자 입력 받는 코드 추가

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,14 +1,26 @@
 package racingcar;
 
 import racingcar.messages.Message;
+import racingcar.view.InputView;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
     private final List<Car> cars = new ArrayList<>();
+    private int numOfExecutions;
+    private final InputView inputView = new InputView();
 
     public void start() {
         System.out.println(Message.GAME_START_NAME);
+        List<String> cars = inputView.readCars();
+        for (String car : cars) {
+            this.cars.add(new Car(car));
+        }
+
+        System.out.println(Message.GAME_START_COUNT);
+        this.numOfExecutions = inputView.readNumOfExecutions();
     }
 }
