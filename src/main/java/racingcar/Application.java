2086b54a97baea5c94ddf559f0faff7ee62880e7
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2023-11-01 15:36:46 +0900

    method : getTryCountFromUser

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         String[] carNames = Service.getCarNamesFromUser();
+        int tryCount = Service.getTryCountFromUser();
     }
 
 
--- src/main/java/racingcar/Service.java
+++ src/main/java/racingcar/Service.java
@@ -7,4 +7,13 @@ public class Service {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
     }
+
+    static int getTryCountFromUser(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        try{
+            return Integer.parseInt(Console.readLine());
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("잘못된 입력입니다. 회수는 숫자로 입력해주세요.");
+        }
+    }
 }
