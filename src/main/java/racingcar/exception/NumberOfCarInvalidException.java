d4c3a21368b93c0b64888b87ef500491548bb591
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-01 16:42:42 +0900

    refactor - 예외 메세지 필드명 변경

--- src/main/java/racingcar/exception/NumberOfCarInvalidException.java
+++ src/main/java/racingcar/exception/NumberOfCarInvalidException.java
@@ -2,9 +2,9 @@ package racingcar.exception;
 
 public class NumberOfCarInvalidException extends IllegalArgumentException {
 
-    private static final String message = "자동차는 2대 이상 있어야 합니다.";
+    private static final String exceptionMessage = "자동차는 2대 이상 있어야 합니다.";
 
     public NumberOfCarInvalidException() {
-        super(message);
+        super(exceptionMessage);
     }
 }
