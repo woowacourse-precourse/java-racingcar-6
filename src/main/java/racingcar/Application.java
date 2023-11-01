9e266297cbed1353ae3cf77e5516c67aabb2189a
김주형 <wnguddl96@knou.ac.kr>
2023-11-01 17:39:57 +0900

    refactor: 다시 처음부터

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 
 public class Application {
     public static void main(String[] args) {
