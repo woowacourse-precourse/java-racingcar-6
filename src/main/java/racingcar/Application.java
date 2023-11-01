b0d92bf1ca664b159b18b32f56d98f3f7cf3fd51
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-01 23:58:38 +0900

    style: 줄 바꿈 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import racingcar.controller.GameController;
 
 public class Application {
+    
     public static void main(String[] args) {
         GameController gameController = new GameController();
         gameController.play();
