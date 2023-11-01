7b61e72376bd2f78c33ba8bb5ed67462464d1a79
Ansix1207 <choiansik98@naver.com>
2023-11-01 21:03:35 +0900

    feat: 게임의 룰들 상수화 정의

---
+++ src/main/java/racingcar/utils/RuleConstant.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+public class RuleConstant {
+    public static final int MAX_NAME_SIZE = 5;
+    public static final int START_INCLUSIVE = 0;
+    public static final int END_INCLUSIVE = 9;
+    public static final int CAN_DRIVE_CRITERIA = 4;
+    public static final String DELIMITER = ",";
+}
