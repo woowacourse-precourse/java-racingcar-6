10d8d5d0f135e8b8897dcf6b4f25a9e359dcab1f
KongJunWoo <joonoo3@naver.com>
2023-11-01 22:45:31 +0900

    feat : 자동차 이름 입력 통합 메서드 구현
    자동차 이름 입력 값을 받아 검증하여 반환하는 메서드를 구현합니다.

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
 
+import racingcar.util.ValidateCarName;
+import racingcar.view.InputView;
 import racingcar.model.Car;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,12 @@ public class Game {
 
     public static Game create() {
         return new Game();
+    }
+
+    public List<String> InputCarName() {
+        String inputValue = InputView.inputCarName();
+        List<String> CarNameList = ValidateCarName.validateInputValue(inputValue);
+        return CarNameList;
     }
 
     public void makeCarList(List<String> carList) {
