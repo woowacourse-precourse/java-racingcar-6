d626e5bb35f469420843c310085ecaddd478940c
JiEung2 <https://github.com/JiEung2>
2023-11-01 18:17:02 +0900

    Test: 입력에 대한 테스트 코드 구현
    입력에서 예외가 발생할 여러 상황에 대한 테스트 구현

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,17 +1,18 @@
 package racingcar.view;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class InputViewTest {
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.CarNameValidator;
+import racingcar.domain.RoundNumberValidator;
 
+public class InputViewTest {
     InputView inputView = new InputView();
+
     @Test
-    void 입력된_자동차_이름을_list로_변환(){
+    void 입력된_자동차_이름을_list로_변환() {
         //given
         String name = ("pobi,woni,jun");
         //when
@@ -21,23 +22,73 @@ public class InputViewTest {
     }
 
     @Test
+    void 자동차_이름이_하나_입력되었을_때() {
+        //given
+        String name = ("pobi");
+        //when
+        List<String> names = inputView.toStringList(name);
+        //then
+        assertThat(List.of("pobi")).isEqualTo(names);
+    }
+
+    @Test
     void 입력된_자동차_이름이_5자보다_많으면_예외() {
         //given
-        List<String> names = List.of("pobi", "woni", "jun", "jieung");
+        String names = "pobi,woni,jun,jieung";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> inputView.validateName(names));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
         //then
         assertThat(e.getMessage()).isEqualTo("자동차 이름은 5자 이내로 입력해주세요.");
     }
 
     @Test
-    void 입력된_시도_횟수가_숫자가_아니면_예외() {
+    void 입력된_자동차_이름이_쉼표로_시작할_때() {
         //given
-        String input = "다섯번";
+        String names = ",pobi,woni,jun,jieung";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> inputView.validateNumber(input));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
         //then
-        assertThat(e.getMessage()).isEqualTo("숫자를 입력하세요");
+        assertThat(e.getMessage()).isEqualTo("자동차 이름은 '이름,이름' 형식의 패턴을 지켜주세요.");
+    }
+
+    @Test
+    void 입력된_자동차_이름이_쉼표로_끝날_때() {
+        //given
+        String names = "pobi,woni,jun,jieung,";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
+        //then
+        assertThat(e.getMessage()).isEqualTo("자동차 이름은 '이름,이름' 형식의 패턴을 지켜주세요.");
+    }
+
+    @Test
+    void 자동차_이름_입력에서_쉼표가_두_번_나올_때() {
+        //given
+        String names = "pobi,woni,,jun";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
+        //then
+        assertThat(e.getMessage()).isEqualTo("자동차 이름은 5자 이내로 입력해주세요.");
+    }
+
+    @Test
+    void 입력된_자동차_이름이_공백일_때() {
+        //given
+        String names = " ";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
+        //then
+        assertThat(e.getMessage()).isEqualTo("자동차 이름은 '이름,이름' 형식의 패턴을 지켜주세요.");
+    }
+
+    @Test
+    void 입력된_자동차_이름이_중복일_때() {
+        //given
+        String names = "pobi,pobi";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new CarNameValidator(names));
+        //then
+        assertThat(e.getMessage()).isEqualTo("자동차 이름은 모두 다르게 입력해주세요.");
     }
 
     @Test
@@ -49,4 +100,16 @@ public class InputViewTest {
         //then
         assertThat(5).isEqualTo(number);
     }
+
+    @Test
+    void 입력된_시도_횟수가_숫자가_아닐_때() {
+        //given
+        String input = "다섯번";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new RoundNumberValidator(input));
+        //then
+        assertThat(e.getMessage()).isEqualTo("숫자를 입력해주세요.");
+    }
+
 }
