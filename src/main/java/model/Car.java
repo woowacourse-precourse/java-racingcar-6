141ffd7cc0f8df6ae09ba9f7672e57081c819ba1
qudwns017 <qudwns017@naver.com>
2023-11-01 19:38:55 +0900

    Feat: Car Model 선언

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Car {
+    private final String name;
+    private int moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0;
+    }
+}
