474958bcef2a53cfb9f718f6ba11c912ceb8fd26
voyager003 <miraclex000@gmail.com>
2023-11-01 15:27:04 +0900

    feat: OutputView 이름 입력, 시도 횟수입력 출력기능을 추가
    변경 전: InputView에서 System.out.println()
    변경 후: InputView에서 OutputView의 static 메서드 print..()를 사용
    변경 이유: 메서드가 한 가지 일만 하도록 하는 요구 사항을 지키기 위함

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String getCarNamesFromPlayerInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        OutputView.printInputCarName();
         return Console.readLine();
     }
 
     public static String getRaceCountFromPlayerInput() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        OutputView.printInputRaceCount();
         return Console.readLine();
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 public class OutputView {
 
+    public static void printInputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printInputRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
     public static void printConsole() {
         System.out.println("실행 결과");
     }
