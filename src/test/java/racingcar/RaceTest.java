178c2a24b882f3093660b3d92273556bf746a9a5
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-11-01 22:06:29 +0900

    test(Race_test): correct removed unused imports

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,12 +1,8 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
