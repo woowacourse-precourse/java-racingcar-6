f8c1478b0fbc6b61d159dd2b7fe4573789ba8411
HongChaa <jun761_1@naver.com>
2023-11-01 18:59:11 +0900

    feat(Validator) : 사용자가 입력한 것이 숫자인지 및 범위 검증하는 메소드 추가

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -2,21 +2,42 @@ package racingcar.util;
 
 public class Validator {
 
-    public void checkName(String name) {
+    public static String checkName(String name) {
         validateNameLength(name);
         validateNameBlank(name);
+        return name;
     }
 
-    private void validateNameLength(String name) {
+    private static void validateNameLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateNameBlank(String name) {
+    private static void validateNameBlank(String name) {
         if (name.contains(" ")) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static int checkTryCount(String rawTryCount) {
+        int tryCount = validateIsNotNumber(rawTryCount);
+        return validateInRange(tryCount);
+    }
+
+    private static int validateIsNotNumber(String rawTryCount) {
+        try {
+            return Integer.parseInt(rawTryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int validateInRange(int tryCount) {
+        if (tryCount < 1) {
+            throw new IllegalArgumentException();
+        }
+        return tryCount;
     }
 
 }
