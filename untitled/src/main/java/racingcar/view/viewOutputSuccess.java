37bb15a4ebbbcf5e9f370e2c978f7c29cb29d213
seongnam <jseongnam1109@gmail.com>
2023-11-01 18:37:39 +0900

    viewOutputSuccess

--- untitled/src/main/java/racingcar/view/viewOutputSuccess.java
+++ untitled/src/main/java/racingcar/view/viewOutputSuccess.java
@@ -1,6 +1,15 @@
 package racingcar.view;
 
-public class viewOutputSuccess {
-    private final String searchChar = "-";
+import java.util.ArrayList;
 
+public class viewOutputSuccess {
+    public void viewOutputSuccess(ArrayList<String> winArrayList){
+        System.out.print("최종 우승자 : ");
+        for(int i=0; i<winArrayList.size();i++){
+            if(i != 0){
+                System.out.print(", ");
+            }
+            System.out.printf("%s",winArrayList.get(i));
+        }
+    }
 }
