f175848ed8a115d0a1f5ea7b5eb0953690f748d9
plan11plan <tjtfy7028@gmail.com>
2023-11-01 21:28:15 +0900

    feat:[create][OutputView] 자동차 출력 기능 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -16,10 +16,5 @@ public class OutputView {
         System.out.println(car.getName() + " : " + car.getPositionString());
     }
 
-    public static void printWinners(List<Car> winners) {
-        String winnerNames = winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
-        System.out.println("최종 우승자: " + winnerNames);
-    }
+
 }
