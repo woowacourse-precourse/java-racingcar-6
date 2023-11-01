6514ea6640958f9d6042835daa6b325b4f822dff
jiwon1217 <koagjw@gmail.com>
2023-11-01 15:32:28 +0900

    :recycle: refactor: getCarNames() 삭제

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,10 +7,6 @@ public class InputView {
     private static final String CAR_NAMES_DELIMITER = ",";
 
     public static List<Car> inputCarNames() {
-        return getCarNames();
-    }
-
-    public static List<Car> getCarNames() {
         String input = Console.readLine();
         String[] carNames = input.split(CAR_NAMES_DELIMITER);
 
