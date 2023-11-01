35de10b89b2133192bfc33015b733e1c3e1b3fb7
hyocheol An <anhyocheol@naver.com>
2023-11-01 22:55:48 +0900

    feat : 최종 우승자 안내 메세지 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
-    public void lineBreak () {
+    public void divideLine () {
         System.out.println();
     }
-
     public void result() {
         System.out.println("\n실행 결과");
     }
     public void resultByRound(String name, String printDistance) {
         System.out.printf("%s : %s\n", name, printDistance);
+    }
+    public void winner(String winnerNames) {
+        System.out.printf("최종 우승자 : %s", winnerNames);
     }
 }
