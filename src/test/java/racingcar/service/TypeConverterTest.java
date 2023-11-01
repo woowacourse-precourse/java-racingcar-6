7beb4653912cee2d1003d928c0dfb382d4107cc4
Seyeon <wjdtpdus828@naver.com>
2023-11-01 16:00:55 +0900

    fix(TypeConverterTest): 미사용 import문 제거

--- src/test/java/racingcar/service/TypeConverterTest.java
+++ src/test/java/racingcar/service/TypeConverterTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TypeConverterTest {
 
