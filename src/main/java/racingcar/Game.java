daa0eb2f7150f397b82049f4332a777e7fef240d
kuk329 <kuk329@cau.ac.kr>
2023-11-01 16:37:06 +0900

    refactor : 함수 분리와 함수명 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -13,30 +13,36 @@ public class Game { // 게임 진행 클래스
     private static int maxScore = -1;
     private static int gameRepeatNumber = 0;
     private static ArrayList<String> winnerList = new ArrayList<>();// 최종 우승자 저장을 위한 리스트
-    private List<Car> carList = new ArrayList<>();
+    private final List<Car> carList = new ArrayList<>();
 
 
     public void start() {
-        System.out.println(Message.START_MESSAGE);
-        String inputStr = Console.readLine(); // 자동차 이름들을 입력받는다.
-        String[] splitStr = inputStr.split(","); // 입력 받은 문자열을 , 를 기준으로 분리해서 배열에 저장한다.
-        minCarCountCheck(splitStr); // 입력 값에 대한 예외 처리
+        inputCarNames();
+        inputRepeatNumber();
+        repeatGame();
+        pickWinner();
+    }
 
+    public void inputCarNames() {
+        System.out.println(Message.START_MESSAGE);
+        String inputStr = Console.readLine();
+        String[] splitStr = inputStr.split(",");
+        minCarCountExceptionCheck(splitStr);
+        saveCarNames(splitStr);
+    }
+
+    public void saveCarNames(String[] splitStr) {
         for (String str : splitStr) {
-            minNameLengthCheck(str.strip());
+            minNameLengthExceptionCheck(str.strip());
             carList.add(new Car(str.strip()));
         }
+    }
 
+    public void inputRepeatNumber() {
         System.out.println(Message.REPEAT_NUMBER);
-
         String inputRepeatNumber = Console.readLine();
-        repeatNumberCheck(inputRepeatNumber);
+        repeatNumberExceptionCheck(inputRepeatNumber);
         gameRepeatNumber = Integer.parseInt(inputRepeatNumber);
-
-        repeatGame();
-
-        pickWinner();
-
     }
 
     public void repeatGame() {
@@ -54,13 +60,15 @@ public class Game { // 게임 진행 클래스
     }
 
     public void pickWinner() {
-
         for (int i = 0; i < carList.size(); i++) {
             int carScore = carList.get(i).getScore();
             String carName = carList.get(i).getName();
             calculateMaxScore(carScore, carName);
         }
+        printWinner();
+    }
 
+    public void printWinner() {
         String printString = String.join(",", winnerList);
         System.out.printf("최종 우승자 : %s", printString);
 
@@ -74,31 +82,28 @@ public class Game { // 게임 진행 클래스
         } else if (maxScore == score) {
             winnerList.add(name);
         }
-
     }
 
-    public void minNameLengthCheck(String str) {
-
+    /**
+     * 예외 처리
+     */
+    public void minNameLengthExceptionCheck(String str) {
         if (str.length() > 5 || str.length() == 0) {
             throw new IllegalArgumentException(Message.MAX_CAR_NAME_ERROR);
         }
-
     }
 
 
-    public void minCarCountCheck(String[] stringArr) {
-
+    public void minCarCountExceptionCheck(String[] stringArr) {
         if (stringArr.length < 2) { // 자동차 개수가 최소 2개 이상이여야 게임 진행이 가능하다고 가정
             throw new IllegalArgumentException(Message.MIN_CAR_NUMBER_ERROR);
         }
-
     }
 
-    public void repeatNumberCheck(String target) {
+    public void repeatNumberExceptionCheck(String target) {
         if (!isRepeatNumber(target)) {
             throw new IllegalArgumentException(Message.MIN_REPEAT_ERROR);
         }
-
     }
 
 }
