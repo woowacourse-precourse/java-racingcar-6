6360df07cb2963620fa09b19fe9f06baacf1890c
JudyYeon <dev.judykim@gmail.com>
2023-11-01 22:07:17 +0900

    test: 모두 멈춤상태일 경우 공동우승 처리하는 기능 테스트
    - 우승자는 한 명 이상일 수 있다는 요구사항에 따라 우승자가 없는 경우는 고려하지 않았습니다.
    - 모두 멈춤상태일 경우, 공동우승으로 처리하였습니다.

--- src/test/java/racingcar/controller/RacingGameTest.java
+++ src/test/java/racingcar/controller/RacingGameTest.java
@@ -4,19 +4,21 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
 class RacingGameTest extends NsTest {
 
     @Test
+    @DisplayName("모두 멈춤상태로 아무도 전진하지 못할 경우 공동우승 테스트")
     void run() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("bobo,judy,jin", "2");
-                    assertThat(output()).contains("최종 우승자 : judy");
+                    assertThat(output()).contains("최종 우승자 : bobo, judy, jin");
                 },
-                1, 4, 3, 5, 8, 9
+                1, 1, 3, 1, 1, 1
         );
     }
 
