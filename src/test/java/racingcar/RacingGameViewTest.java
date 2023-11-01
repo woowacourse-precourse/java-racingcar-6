7a18981290ba111fc08c0c0c713b8f5494849e39
정용우 <jyw1341@gmail.com>
2023-11-01 14:20:09 +0900

    test(RacingGameView): 경주 라운드 결과 출력 테스트 추가

--- src/test/java/racingcar/RacingGameViewTest.java
+++ src/test/java/racingcar/RacingGameViewTest.java
@@ -1,14 +1,33 @@
 package racingcar;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.RacingGameView.FORMAT_PRINT_ROUND;
 
 class RacingGameViewTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"a,ab,abc,abcd,abcde", "1,12,안녕,abc"})
@@ -55,5 +74,28 @@ class RacingGameViewTest {
         RacingGameView racingGameView = new RacingGameView();
 
         assertThrows(IllegalArgumentException.class, () -> racingGameView.validatePositiveNumber(invalidInput));
+    }
+
+    @Test
+    @DisplayName("경주 라운드 결과 출력을 테스트한다.")
+    void testPrintRoundResult() {
+        //given
+        RacingGameView racingGameView = new RacingGameView();
+        List<RacingCar> cars = new ArrayList<>();
+        cars.add(new RacingCar("페라리"));
+        cars.add(new RacingCar("벤츠"));
+
+        //when
+        for (int i = 0; i < 10; i++) {
+            cars.forEach(RacingCar::move);
+        }
+        String s1 = String.format(FORMAT_PRINT_ROUND, cars.get(0).getName(), cars.get(0).getMovement());
+        String s2 = String.format(FORMAT_PRINT_ROUND, cars.get(1).getName(), cars.get(1).getMovement());
+        String result = s1 + s2 + "\n";
+        racingGameView.printRoundResult(cars);
+        String actualOutput = outContent.toString().replaceAll("\r", "");
+
+        //then
+        assertEquals(result, actualOutput);
     }
 }
