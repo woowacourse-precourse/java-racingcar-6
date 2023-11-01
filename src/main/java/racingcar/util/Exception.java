7d0eb82e0e81143f70c11d81826592cf0b34489b
leeminjun <sohwi0305@gmail.com>
2023-11-01 22:44:09 +0900

    refactor: 코드 위치 보기 쉽게 변경

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -4,20 +4,26 @@ import java.util.List;
 
 public class Exception {
     private static final int CAR_NAME_LIMIT_MAX = 5;
-    private static final int CAR_NAME_LIMIT_MIN = 1;
+    private static final int CAR_NAME_LIMIT_MIN = 0;
     private static final String SPACE = " ";
     private final Parser parser = new Parser();
     public void checkCarNamesInput(String input) throws IllegalArgumentException {
-        List<String> carNameList = parser.parseCarNames(input);
-        checkCarNameLengthAndSpaceThrowException(carNameList);
         checkEmptyAndThrowException(input);
-        checkCarNameListDuplicationAndThrowException(carNameList);
         checkLastIndexAndThrowException(input);
+        List<String> carNameList = parser.parseCarNames(input);
+        checkCarNameListDuplicationAndThrowException(carNameList);
+        checkCarNameLengthAndSpaceThrowException(carNameList);
     }
 
     public void checkNumberOfPlayCountInput(String input) throws IllegalArgumentException {
         checkEmptyAndThrowException(input);
         checkNumberAndThrowException(input);
+    }
+
+    private void checkEmptyAndThrowException(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("값을 입력해주세요.");
+        }
     }
 
     private void checkNumberAndThrowException(String input) {
@@ -27,11 +33,21 @@ public class Exception {
             throw new IllegalArgumentException("시도 횟수는 숫자로 입력하세요.");
         }
     }
+
+    private void checkCarNameListDuplicationAndThrowException(List<String> carNameList) {
+        for (int i = 0; i < carNameList.size(); i++) {
+            for (int j = i + 1; j < carNameList.size(); j++) {
+                checkEqualsAndThrowException(carNameList.get(i), carNameList.get(j));
+            }
+        }
+    }
+
     private void checkLastIndexAndThrowException(String carName) {
         if (carName.lastIndexOf(",") == carName.length() - 1) {
             throwSpaceException();
         }
     }
+
     private void checkCarNameLengthAndSpaceThrowException(List<String> carNameList) {
         for (String carName : carNameList) {
             checkLengthAndThrowException(carName);
@@ -48,15 +64,8 @@ public class Exception {
         throw new IllegalArgumentException("자동차 이름안에 공백을 제거하세요.");
     }
 
-    private void checkEmptyAndThrowException(String carName) {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
-        }
-    }
 
-    private void throwMinLengthException() {
-        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MIN + "글자 이상이어야 합니다.");
-    }
+
 
     private void checkLengthAndThrowException(String carName) {
         if (CAR_NAME_LIMIT_MAX < carName.length()) {
@@ -72,17 +81,16 @@ public class Exception {
         throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MAX + "글자 이하여야 합니다.");
     }
 
-    private void checkCarNameListDuplicationAndThrowException(List<String> carNameList) {
-        for (int i = 0; i < carNameList.size(); i++) {
-            for (int j = i + 1; j < carNameList.size(); j++) {
-                checkEqualsAndThrowException(carNameList.get(i), carNameList.get(j));
-            }
-        }
+    private void throwMinLengthException() {
+        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MIN + "글자 이상여야 합니다.");
     }
+
 
     private void checkEqualsAndThrowException(String carName1, String carName2) {
         if (carName1.equals(carName2)) {
             throw new IllegalArgumentException("자동차 이름이 중복입니다.");
         }
     }
+
+
 }
