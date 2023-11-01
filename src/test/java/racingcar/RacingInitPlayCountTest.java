96496308145732833a2a2c421cd60682f5527c9f
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-11-01 15:35:55 +0900

    fix(RacingInitPlayCountTest): 불 필요한 공백제거
    - 짧은 내용일 땐 맨 위 공백 없애기

--- src/test/java/racingcar/RacingInitPlayCountTest.java
+++ src/test/java/racingcar/RacingInitPlayCountTest.java
@@ -20,7 +20,6 @@ public class RacingInitPlayCountTest {
     @Test
     @DisplayName("initPlayCount 부족 실패 테스트")
     void initPlayCount_lack_fail() {
-
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> racing.initPlayCount(0))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -30,7 +29,6 @@ public class RacingInitPlayCountTest {
     @Test
     @DisplayName("initPlayCount 초과 실패 테스트")
     void initPlayCount_over_fail() {
-
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> racing.initPlayCount(11))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -40,7 +38,6 @@ public class RacingInitPlayCountTest {
     @Test
     @DisplayName("initPlayCount 검증 테스트")
     void initPlayCount_validate() {
-
         racing.initPlayCount(5);
         assertThat(racing.getPlayCount()).isEqualTo(5);
     }
