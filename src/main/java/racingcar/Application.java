b556da5d6cade602d3c1b2ca95564457a932519a
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-11-01 18:44:07 +0900

    기능 구현 1.getCarNames() 이름 부여
    2.getNumberOfMove() 몇 번의 이동을 할 것인지
    
    3.checkRandomNumber() 4기준으로 bool값 반환
    
    4. printMove() 전진 출력
    
    5.startRacingCar() 시작
         이름 3개를 반복하면서 무작위값에 따라 "-" 추가
    
    6.printWinner() 누가우승했는지
         문자열 분리해서 -의 개수와 입력 수 비교 후 출력
    
    7. isRaceFinished() 끝났는지
         문자열 ex) bori : ----- 에서 -의 개수를 비교
    
    8.main
         RacingCar 생성해서 while 조건동안 실행하도록
         --> 생성자에서 입력 받고 while로 조건동안 실행하도록

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,55 +3,84 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         RacingCar racingCar = new RacingCar();
-        while(true){
-            racingCar.startRacingCar();
-        }
     }
 }
 
 class RacingCar {
     private String[] carNames;
-    private Integer numberOfTry;
+    private Integer numberOfFinish;
     private Integer randomNum;
     public RacingCar(){
         getCarNames();
         getNumberOfMove();
+        int i=0;
+        while(isRaceFinished()){
+            startRacingCar();
+        }
+        printWinner();
     }
 
     private void getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
         String names = Console.readLine();
         carNames = names.split(",");
+        for (int i = 0; i < carNames.length; i++) {
+            carNames[i] = carNames[i].trim() + " : ";
+        }
     }
 
     private void getNumberOfMove(){
         System.out.println("시도할 회수는 몇회인가요?");
-        numberOfTry = Integer.parseInt(Console.readLine());
+        numberOfFinish = Integer.parseInt(Console.readLine());
     }
 
     private Boolean checkRandomNumber(){
         randomNum = Randoms.pickNumberInRange(0,9);
-        if(randomNum>=4){
-            return true;
-        }
+        if(randomNum>=4) return true;
         return false;
     }
 
-    private void printWinner(){
-
-    }
-
-    private void printMove(int move){
-
+    private void printMove(){
+        for(String i : carNames){
+            System.out.println(i);
+        }
+        System.out.println();
     }
 
     public void startRacingCar(){
+        for (int i = 0; i < carNames.length; i++){
+            if(checkRandomNumber()){
+                carNames[i] = carNames[i] + "-";
+            }
+        }
+        printMove();
+    }
 
+    private void printWinner() {
+        String[] Winner;
+        System.out.print("최종 우승 : ");
+        for(String i: carNames){
+            Winner = i.split(" ");
+            if(Winner[2].length()==numberOfFinish){
+                System.out.print(Winner[0]);
+            }
+        }
+    }
+
+    private boolean isRaceFinished() {
+        for(String i: carNames){
+            if(i.chars().filter(c -> c == '-').count()==numberOfFinish){
+                return false;
+            };
+        }
+        return true;
     }
 
 
