3eeb3dbb62f7cf7daa246b97255dd72a98f75ae2
Davin <85967571+dav1n9@users.noreply.github.com>
2023-11-01 18:09:48 +0900

    refactor: 자동차 입력과 우승자 출력 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,24 +9,14 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        List<String> carNames;
-        List<Car> cars = new ArrayList<>();
-
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carNames = List.of(Console.readLine().split(","));
-
-        for (String carName : carNames) {
-            if (carName.length() > 5)
-                throw new IllegalArgumentException();
-            cars.add(new Car(carName));
-        }
+        List<Car> cars = setCars();
 
         System.out.println("시도할 회수는 몇회인가요?");
         int forwardCount = Integer.parseInt(Console.readLine());
         System.out.println();
 
         System.out.println("실행 결과");
-
         while (forwardCount-- > 0) {
             for (Car car : cars) {
                 car.tryMoving();
@@ -35,15 +25,39 @@ public class Application {
             System.out.println();
         }
 
-        int maxDistance = Collections.max(cars).getTotalForward();
+        printWinners(cars);
+    }
+
+
+    private static List<Car> setCars() {
+        List<String> carNames = List.of(Console.readLine().split(","));
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            if (carName.length() > 5)
+                throw new IllegalArgumentException();
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+    private static void printWinners(List<Car> carList) {
+        List<String> winners = getWinners(carList);
+        String result = String.join(",", winners);
+        System.out.println("최종 우승자 : " + result);
+    }
+
+    private static List<String> getWinners(List<Car> carList) {
+        int maxDistance = Collections.max(carList).getTotalForward();
 
         List<String> winners = new ArrayList<>();
-        for (Car car : cars) {
+        for (Car car : carList) {
             if (car.getTotalForward() == maxDistance) {
                 winners.add(car.getName());
             }
         }
-        String result = String.join(",", winners);
-        System.out.println("최종 우승자 : " + result);
+        return winners;
     }
+
+
 }
