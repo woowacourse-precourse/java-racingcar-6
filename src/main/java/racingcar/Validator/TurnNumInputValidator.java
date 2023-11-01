3c60d2e06c8c7c976ae541ff6ecc2124ef8584ed
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-01 15:11:45 +0900

    refactor: 클래스 내 멤버 간 의존 관계 개선

--- src/main/java/racingcar/Validator/TurnNumInputValidator.java
+++ src/main/java/racingcar/Validator/TurnNumInputValidator.java
@@ -1,27 +1,27 @@
 package racingcar.Validator;
 
 public class TurnNumInputValidator {
-    private int turnNumInputInt;
-    private final String ASK_INT_UNDER_PROPER_AMOUNT = "잘못된 입력입니다. 적정한 수를 정수 형태로 입력해주세요";
-    private final String ASK_POSITIVE_NUM_INPUT = "잘못된 입력입니다. 0이상의 수를 입력해주세요";
-    private final int MIN_TURN_NUM = 1;
+    private static final String ASK_INT_UNDER_PROPER_AMOUNT = "잘못된 입력입니다. 적정한 수를 정수 형태로 입력해주세요";
+    private static final String ASK_POSITIVE_NUM_INPUT = "잘못된 입력입니다. 0이상의 수를 입력해주세요";
+    private static final int MIN_TURN_NUM = 1;
 
 
     public void validate(String turnNumInputString) {
         isInputDigit(turnNumInputString);
-        isPositive();
+        isPositive(turnNumInputString);
     }
 
     public void isInputDigit(String turnNumInputString) {
         try {
-            this.turnNumInputInt = Integer.parseInt(turnNumInputString);
+            Integer.parseInt(turnNumInputString);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ASK_INT_UNDER_PROPER_AMOUNT);
         }
     }
 
-    public void isPositive() {
-        if (turnNumInputInt < MIN_TURN_NUM) {
+    public void isPositive(String turnNumInputString) {
+        Integer turnNumInputInteger = Integer.parseInt(turnNumInputString);
+        if (turnNumInputInteger < MIN_TURN_NUM) {
             throw new IllegalArgumentException(ASK_POSITIVE_NUM_INPUT);
         }
     }
