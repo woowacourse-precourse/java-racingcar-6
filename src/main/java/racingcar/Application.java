e6c39f9f81b8d1b85723a5f390ff22a7f1326e3b
ebang <dskhe8@gmail.com>
2023-11-01 20:39:00 +0900

    feat: Application 에서 Game 구동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,10 @@
 package racingcar;
 
-import
+
+import racingcar.Model.Game;
 
 public class Application {
     public static void main(String[] args) {
-
+        Game.getInstance().playGame();
     }
 }
