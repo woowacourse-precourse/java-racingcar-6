3b3711dad6323260dca42223e7b31f19480f4e6f
parkheeddong <phj7214go@gmail.com>
2023-11-01 17:39:59 +0900

    feat : 사용자 입력에 대한 유효성 검사 테스트 코드 구현

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,2 +1,31 @@
-package racingcar;public class RacingCarTest {
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    void 자동차_이름_입력_유효성_검사() {
+        String[] invalidNames = {"blackpink", "rose", "lisa", "jenny", "jisoo"};
+        NameInput.setNameArr(invalidNames);
+        assertThrows(IllegalArgumentException.class, () -> NameInput.checkNameInput());
+    }
+    @Test
+    void 회차_입력_유효성_검사_음수인_경우() {
+        String invalidCount = "-1";
+        assertThrows(IllegalArgumentException.class, () -> CountInput.checkCountInput(invalidCount));
+    }
+
+    @Test
+    void 회차_입력_유효성_검사_소숫점있는_경우() {
+        String invalidCount = "5.6";
+        assertThrows(IllegalArgumentException.class, () -> CountInput.checkCountInput(invalidCount));
+    }
+
+    @Test
+    void 회차_입력_유효성_검사_문자열인_경우() {
+        String invalidCount = "notNumber";
+        assertThrows(IllegalArgumentException.class, () -> CountInput.checkCountInput(invalidCount));
+    }
 }
