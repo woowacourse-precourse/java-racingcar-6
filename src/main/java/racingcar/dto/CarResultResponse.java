7672eac965259d1f3d525df33f7d5702de62afee
박승민 <tmddudals369@naver.com>
2023-11-01 17:58:45 +0900

    refactor(carResultResponse): 출력의 사용되는 문자열 원시값 포장

--- src/main/java/racingcar/dto/CarResultResponse.java
+++ src/main/java/racingcar/dto/CarResultResponse.java
@@ -2,8 +2,11 @@ package racingcar.dto;
 
 public record CarResultResponse(String carName, int movingCount) {
 
+    private static final String GAME_RESULT_MESSAGE_FORMAT = "%s : %s";
+    private static final String MOVEMENT_SYMBOL = "-";
+
     @Override
     public String toString() {
-        return String.format("%s : %s", carName, "-".repeat(movingCount));
+        return String.format(GAME_RESULT_MESSAGE_FORMAT, carName, MOVEMENT_SYMBOL.repeat(movingCount));
     }
 }
