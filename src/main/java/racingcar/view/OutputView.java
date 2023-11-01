9b286e5c73da533518393b4072816ad88e9432cf
LEE-sh1673 <lsh901673@gmail.com>
2023-11-01 21:07:38 +0900

    feat: 게임의 결과를 출력하는 기능을 추가한다
    내용:
    - 자동차의 진행 상황을 출력한다.
    - 최종 우승자를 출력한다.
    - 우승자가 여러 명이면 쉼표를 이용하여 구분한다.

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,51 @@
+package racingcar.view;
+
+import racingcar.RaceScore;
+import racingcar.RacingResult;
+import racingcar.Winners;
+
+public class OutputView {
+
+    private static final String RACING_RESULT_TITLE = "실행 결과";
+    private static final String RACING_RESULT_LINE_FORMAT = "%s : %s\n";
+    private static final String RACING_SCORE_FORMAT = "-";
+    private static final String RACING_WINNERS_FORMAT = "최종 우승자 : %s";
+    private static final String RACING_WINNERS_NAME_DELIMITER = ", ";
+
+
+    // 기본 생성자가 만들어지는 것을 막는다. (인스턴스화 방지용).
+    private OutputView() {
+        throw new AssertionError();
+    }
+
+    public static void printRacingResultTitle() {
+        System.out.println();
+        System.out.println(RACING_RESULT_TITLE);
+    }
+
+    public static void printRacingResult(final RacingResult racingResult) {
+        for (RaceScore score : racingResult.getScores()) {
+            printRacingResultLine(score);
+        }
+        System.out.println();
+    }
+
+    private static void printRacingResultLine(final RaceScore score) {
+        System.out.printf(RACING_RESULT_LINE_FORMAT,
+                score.name(),
+                formatRacingScore(score)
+        );
+    }
+
+    private static String formatRacingScore(final RaceScore score) {
+        return RACING_SCORE_FORMAT.repeat(score.score());
+    }
+
+    public static void printWinners(final Winners winners) {
+        System.out.printf(RACING_WINNERS_FORMAT, formatWinners(winners));
+    }
+
+    private static String formatWinners(final Winners winners) {
+        return String.join(RACING_WINNERS_NAME_DELIMITER, winners.getNames());
+    }
+}
