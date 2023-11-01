5c22291e1a230dabd5a0475a944062b34c46ba5c
SangCheol <scpark0698@gmail.com>
2023-11-01 22:18:25 +0900

    feat: Car 클래스 move 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position;
@@ -7,6 +9,13 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void move() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        if (random >= 4) {
+            position++;
+        }
     }
 
     public String getName() {
