166deb1b25eba8f11f85e19db08e532d53639b1e
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-01 14:26:49 +0900

    [FEAT] 자동차 이름 예외처리

--- src/main/java/racingcar/domain/CarInformation.java
+++ src/main/java/racingcar/domain/CarInformation.java
@@ -3,10 +3,14 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarInformation {
+    private final String name;
     private int position = 0;
 
     public CarInformation(String name){
-
+        if (name == null || name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자여야 합니다.");
+        }
+        this.name = name;
     }
 
     public void move(){
@@ -15,4 +19,5 @@ public class CarInformation {
             position++;
         }
     }
+
 }
