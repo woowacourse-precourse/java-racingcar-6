25664429b4153bfe8ef7dcbc5cc3dac34b601394
haebyun <bhk4392@gmail.com>
2023-11-01 16:51:09 +0900

    feat(RacingFormatter): 자동차 이름들이 비었거나 5글자가 넘는 문자열일 경우 예외 처리 기능 구현

--- src/main/java/racingcar/games/racing/util/RacingFormatter.java
+++ src/main/java/racingcar/games/racing/util/RacingFormatter.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RacingFormatter {
+    private static final String SPLIT_POINT = ",";
 
     public List<String> reformatCarNames(String input) {
-        List<String> origin = Arrays.stream(input.split(",")).toList();
-        List<String> carNames = Arrays.stream(input.split(","))
+        List<String> origin = Arrays.stream(input.split(SPLIT_POINT)).toList();
+        List<String> carNames = Arrays.stream(input.split(SPLIT_POINT))
                 .filter(name -> !name.isEmpty() && name.length() <= 5)
                 .toList();
+        validateCarNames(origin.size(), carNames.size());
         return carNames;
     }
 
+    private void validateCarNames(int beforeFilteringSize, int afterFilteringSize) {
+        if (beforeFilteringSize != afterFilteringSize) {
+            throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 이하만 가능합니다.");
+        }
+    }
 }
