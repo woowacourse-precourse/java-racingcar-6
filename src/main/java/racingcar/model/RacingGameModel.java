c88b2b8f7bd1edd1edf67dcc5f735d2d4e6ae154
Kim mingyu <minq0302@naver.com>
2023-11-01 19:48:53 +0900

    feat(model): add changing string into List<Car> method

--- src/main/java/racingcar/model/RacingGameModel.java
+++ src/main/java/racingcar/model/RacingGameModel.java
@@ -1,5 +1,8 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGameModel {
 
     public static final double FOUR = 4;
@@ -12,4 +15,18 @@ public class RacingGameModel {
             return false;
         }
     }
+
+    // 입력받은 자동차의 이름들을 List<Car>로 바꿔서 return하는 메서드
+    public List<Car> stringToList(String carNames) {
+        String[] carNameArray = carNames.split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : carNameArray) {
+            cars.add(new Car(name));
+        }
+
+        return cars;
+    }
+
+
 }
