a689708f90896ea74de2cae471a333d72a6796ee
hongjungin <jungini0000@gmail.com>
2023-11-01 16:28:26 +0900

    feat(InputView) : 사용자가 자동차 이름 입력 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
@@ -7,6 +8,7 @@ public class RacingController {
 	public void start() {
 		// TODO Auto-generated method stub
 		OutputView.printInputCarName();
+		String carNames = InputView.inputCarNames();
 	}
 
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,12 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+	public static String inputCarNames() {
+		//자동차 이름 입력
+		return Console.readLine();
+	}
 
 }
