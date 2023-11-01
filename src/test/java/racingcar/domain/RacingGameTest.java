991e8245e644d008813507eab6a406bd3d0cd0cc
mmalfoy <stpmalfoy@gmail.com>
2023-11-01 09:11:45 +0900

    test(RacingGame) : 실행결과 초기화 테스트코드

---
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class RacingGameTest {
+    private RacingGame racingGame;
+
+    @BeforeEach
+    void setUp() {
+        racingGame = new RacingGame();
+    }
+
+    @Test
+    void initResultList_초기화_여부_확인_테스트() {
+        int numOfCars = 5;
+        List<String> result = racingGame.initResultList(5);
+        assertThat(result).isEqualTo(Arrays.asList("","","","",""));
+    }
+
+
+}
