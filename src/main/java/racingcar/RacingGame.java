af0e889f67c58758873e907ce8ddce793b3a0616
Minji Kim <minji1289@naver.com>
2023-11-01 10:48:50 +0900

    [feat] 사용자로부터 경주할 자동차 이름 입력받기

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,11 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
     private static List<Car> createCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
         List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name.trim()));
+        }
         return cars;
     }
 
