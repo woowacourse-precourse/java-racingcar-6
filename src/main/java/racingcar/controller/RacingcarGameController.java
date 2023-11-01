6e92289d341c890774eba1d484cfd9bd73693824
Sung Hyobin <hellmir610@gmail.com>
2023-11-01 20:42:48 +0900

    refactor: Split playRacingcarGame method for proceed each rounds

--- src/main/java/racingcar/controller/RacingcarGameController.java
+++ src/main/java/racingcar/controller/RacingcarGameController.java
@@ -29,16 +29,22 @@ public class RacingcarGameController {
     }
 
     private static void playRacingcarGame(String carChoiceInputMessage, int roundToRace) {
+        GameStatus gameStatus = PLAYING;
+
         RacingcarGameService racingcarGameService = new RacingcarGameServiceImpl();
         Cars cars = racingcarGameService.generateCarsToRace(carChoiceInputMessage);
 
         Printer.printResultHeadMessage();
 
         while (gameStatus.isGamePlaying()) {
-            RandomNumbers randomNumbers = new RandomNumbers(RandomNumberGenerator.generateRandomNumbers(cars.size()));
-            racingcarGameService.playOneRound(cars, randomNumbers);
-            PrintHandler.printResultOfGame(cars);
+            proceedEachRound(racingcarGameService, cars, gameStatus);
         }
+    }
+
+    private static void proceedEachRound(RacingcarGameService racingcarGameService, Cars cars, GameStatus gameStatus) {
+        RandomNumbers randomNumbers = new RandomNumbers(RandomNumberGenerator.generateRandomNumbers(cars.size()));
+        racingcarGameService.playOneRound(cars, randomNumbers);
+        PrintHandler.printResultOfGame(cars);
     }
 
 }
