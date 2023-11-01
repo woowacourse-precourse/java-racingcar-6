565ed75c90a6f94881e34ca1db63178019b25b60
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-11-01 20:24:03 +0900

    feat: throw IllegalArgumentException from AttemptNumber

--- src/main/java/racingcar/service/AttemptNumber.java
+++ src/main/java/racingcar/service/AttemptNumber.java
@@ -7,9 +7,22 @@ public class AttemptNumber {
     private int number;
 
     public void inputNumber() {
-        System.out.println("시도할 횟수는 몇회인가요?");
-        this.number = Integer.parseInt(Console.readLine());
+        try {
+            System.out.println("시도할 횟수는 몇회인가요?");
+            String input = Console.readLine();
+
+            if (input.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            this.number = Integer.parseInt(input);
+            if (this.number < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
+
 
     public int getNum() {
         return number;
