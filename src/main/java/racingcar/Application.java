0151e31d7cfaac2265e372b2f0a5b4bd2d1c3564
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-11-01 10:54:55 +0900

    feat: 최종 프로그램을 완성했다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.Input.InputContext;
+import racingcar.Input.RaceStartCommand;
+import racingcar.output.OutPutContext;
+import racingcar.race.OutPutCommand;
+import racingcar.race.RaceContext;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceStartCommand raceStartCommand = InputContext.inputRacingCar().startCommand();
+        OutPutCommand outPutCommand = RaceContext.start(raceStartCommand).outPutCommand();
+        OutPutContext.printOutPutContents(outPutCommand);
     }
 }
