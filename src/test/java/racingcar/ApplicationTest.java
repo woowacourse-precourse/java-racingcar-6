2663de2520a0c80c4637e14eff973d015ba2657a
jhsseonn <hs125bt@gmail.com>
2023-11-01 19:06:57 +0900

    test: 이름 리스트 입력받은 문자열에 대한 예외처리 테스트 통과

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,13 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 이름_리스트_입력받은_문자열에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("one two three four", "3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
