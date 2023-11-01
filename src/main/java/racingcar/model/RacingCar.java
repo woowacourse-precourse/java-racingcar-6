69036d759fe02e4176123dff3fdc141157411225
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-01 18:05:38 +0900

    feat : 경주에 참여하는 자동차 목록 생성하는 기능 구현

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingCar {
+    private List<Car> carList = new ArrayList<>();
+
+    public RacingCar(List<String> playerCarNameList){
+        playerCarNameList
+                .forEach( playerCarName-> carList.add(new Car(playerCarName)));
+    }
+
+    
+
+}
