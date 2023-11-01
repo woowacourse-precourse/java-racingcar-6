ecc44b6f7178034776baf3cb8cad45cc55e73a2e
realisshomyang <realisshoman@kakao.com>
2023-11-01 15:48:00 +0900

    feat : GameRule 을 모아놓은 GameRule Class 작성

---
+++ src/main/java/racingcar/model/GameRule.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+public class GameRule {
+
+    public static final int START_NUMBER = 0;
+    public static final int END_NUMBER = 9;
+    public static final int MOVE_CONDITION = 4;
+
+    public static final int CAR_NAME_LENGTH_UPPER_LIMIT = 5;
+    public static final int CAR_NAME_LENGTH_LOWER_LIMIT = 1;
+
+    public static final int GAME_COUNT_LOWER_LIMIT = 1;
+    public static final String SEPARATOR = "-";
+
+}
