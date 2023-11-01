88467acdc886f8c5360b4f0191e007322f2f1fc6
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-01 16:21:22 +0900

    fix(racingcar): fix bug of application test
    - wrap primitives and strings

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,24 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.domain.Constants;
-import racingcar.domain.GameService;
+import racingcar.domain.*;
+
+import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+    private static final UserInput userInput = new UserInput();
+    private static final ContainerSetting containerSetting = new ContainerSetting(new RandomNumberGenerator(Constants.MIN_RANDOM_NUM, Constants.MAX_RANDOM_NUM));
+    private static final SystemOutput systemOutput = new SystemOutput();
+    private static final GameService gameService = new GameService(userInput, containerSetting, systemOutput);
 
+    public static void main(String[] args) {
         System.out.println(Constants.CAR_NAME_INPUT_MESSAGE);
         String input = Console.readLine();
 
         System.out.println(Constants.ATTEMP_INPUT_MESSAGE);
         String gameNum = Console.readLine();
 
-        GameService gameService = new GameService();
         gameService.run(input, gameNum);
     }
-
 }
+
