244b7097105e61e1591eba4b1dafca0f6269237e
노정완 <jeongwan.noh@gmail.com>
2023-11-01 10:21:18 +0900

    refactor: 자동차 이름 구분 기준 확장성 구현

--- src/main/java/racingcar/view/CarNameInput.java
+++ src/main/java/racingcar/view/CarNameInput.java
@@ -3,8 +3,8 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarNameInput {
-
+    private final static String SPLIT_SYMBOL = ",";
     public String[] returnCarName() {
-        return Console.readLine().split(",");
+        return Console.readLine().split(SPLIT_SYMBOL);
     }
 }
