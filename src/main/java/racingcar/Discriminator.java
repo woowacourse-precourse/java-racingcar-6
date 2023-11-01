c29dd3c8e1978399d410336f51e49e0461e92c2c
JayM-Oh <277ipjk53@gmail.com>
2023-11-01 20:24:38 +0900

    chore: Discriminator

--- src/main/java/racingcar/Discriminator.java
+++ src/main/java/racingcar/Discriminator.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 public class Discriminator {
+
     public static void validateCarNumber(int carNumber){
         if (carNumber <= 0 || carNumber > 8) {
             throw new IllegalArgumentException("The # cars should in range (1,8) ");
@@ -12,7 +13,12 @@ public class Discriminator {
             throw new IllegalArgumentException("Invalid car name.");
         }
     }
+    /*
+    시도 회수에 대한 유효성을 검사하는 메소드입니다.
 
+    시도 회수가 0이하일 경우 예외 처리가 발생되며, 정수로 변환할 수 없는 경우(시도 회수 입력이 문자 등 숫자가 아닌 경우)
+    예외를 발생시킵니다.
+     */
     public static void validateTrialCount(String trialCount){
         try{
             int count = Integer.parseInt(trialCount);
