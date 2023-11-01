f9c97fd7c4ac0249ee1652bebb79ebb3daaedf24
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-11-01 14:51:55 +0900

    fix(RacingTest): assertThatThrownBy 패키지 변경
    원래는 인텔리제이에서 자동으로 넣어주던 걸 사용했는데 ApplicationTest 에서 사용하는 거랑 달라서 ApplicationTest 에서 사용하는 걸로 변경

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,7 +1,7 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
