4520fde3bd3158db6102fb10d6f3659589ef4be6
leejuno <zhqkfxm0127@naver.com>
2023-11-01 19:27:48 +0900

    test(GameController): 시도 횟수만큼 전진 혹은 멈춤을 반복수행하는 기능 테스트

--- src/test/java/racingcar/domain/GameControllerTest.java
+++ src/test/java/racingcar/domain/GameControllerTest.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,4 +25,26 @@ class GameControllerTest {
 
         assertThat(car.getForwardDistance().toString()).isEqualTo(expected.toString());
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4,3,1,car1 : -,car2 : -", "4,6,9,car1 : ---,car2 : ---"})
+    @DisplayName("시도 횟수만큼 전진 혹은 멈춤을 반복수행")
+    void play_시도_횟수만큼_전진_혹은_멈춤을_반복수행(
+            int number1, int number2, int number3, String expected1, String expected2
+    ) {
+        GameController gameController = new GameController();
+        List<Car> carList = new ArrayList<>();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        carList.add(car1);
+        carList.add(car2);
+        int attemptCount = 3;
+        int[] randomNumberList = {number1, number2, number3};
+
+        gameController.play(carList, attemptCount, randomNumberList);
+
+        assertThat(car1.status()).isEqualTo(expected1);
+        assertThat(car2.status()).isEqualTo(expected2);
+    }
+
 }
