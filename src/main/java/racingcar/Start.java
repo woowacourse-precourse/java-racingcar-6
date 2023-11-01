c3760a868d1ee9750721a2dd90a682615826c33a
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-01 19:32:39 +0900

    style: 주석 제거

--- src/main/java/racingcar/Start.java
+++ src/main/java/racingcar/Start.java
@@ -12,7 +12,7 @@ public class Start {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String car = Console.readLine();
         carList = new ArrayList<>();
-        String[] inputList = car.split(","); // ok
+        String[] inputList = car.split(",");
         for (String o : inputList) {
             if (o.length() >= 6) {
                 throw new IllegalArgumentException("입력된 자동차 이름이 6자리 이상입니다.");
