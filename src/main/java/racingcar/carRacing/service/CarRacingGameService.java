ec4db6b8da55469a5d48579c2cbe795fe022ecd8
fing9 <djajsl54@naver.com>
2023-11-01 18:10:11 +0900

    feat : 사용자의 입력을 받아 저장하고 시도 회수만큼 게임을 진행하는 기능 추가

---
+++ src/main/java/racingcar/carRacing/service/CarRacingGameService.java
@@ -0,0 +1,37 @@
+package racingcar.carRacing.service;
+
+import java.util.List;
+import racingcar.car.model.Car;
+import racingcar.car.service.CarMoveService;
+import racingcar.carRacing.model.CarRacing;
+
+public class CarRacingGameService {
+
+    private final CarRacing carRacing;
+
+    private final CarRacingInputService carRacingInputService = new CarRacingInputService();
+
+    private final CarMoveService carMoveService = new CarMoveService();
+
+    private Integer attemptCount;
+
+    public CarRacingGameService(CarRacing carRacing) {
+        this.carRacing = carRacing;
+        this.attemptCount = 0;
+    }
+
+    public void waitInput() {
+        List<String> carNames = carRacingInputService.inputCarNameList();
+        carRacing.initCarList(carNames);
+        attemptCount = carRacingInputService.inputAttemptCount();
+    }
+
+    public void doRace() {
+        for (int i = 0; i < attemptCount; i++) {
+            for (Car car : carRacing.getCarList()) {
+                carMoveService.move(car);
+            }
+        }
+    }
+
+}
