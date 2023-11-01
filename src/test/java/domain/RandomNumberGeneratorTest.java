e7bbe0f9b88cfed8a8d0d3c41cab11cffd63d62c
ddohyeong <dohyeong7026@gmail.com>
2023-11-01 20:36:16 +0900

    refactor(RandomNumberGeneratorTest) : 공백 컨벤션 리팩토링

--- src/test/java/domain/RandomNumberGeneratorTest.java
+++ src/test/java/domain/RandomNumberGeneratorTest.java
@@ -9,8 +9,10 @@ public class RandomNumberGeneratorTest {
 	void generateNumber_메서드로_랜덤_숫자는_0_9사이의_숫자_생성() {
 		//given
 		RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
 		// when
 		int randomNumber = randomNumberGenerator.generateNumber();
+
 		//then
 		assertTrue(randomNumber >= 0); // 0 이상
 		assertTrue(randomNumber < 10); // 10 미만
