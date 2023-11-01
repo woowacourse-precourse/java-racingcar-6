089c28351c352bc79d612d4895d7694e9da2d464
Isaac Han <isaachan233@gmail.com>
2023-11-01 11:55:05 +0900

    test: GameSetting 테스트

---
+++ src/test/java/racingcar/model/GameSettingTest.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSettingTest {
+
+    @Test
+    void getCarsTest() {
+        Cars cars = new Cars("isaac,han");
+        assertThat(cars.getCars().get(0).getName()).isEqualTo("isaac");
+        assertThat(cars.getCars().get(1).getName()).isEqualTo("han");
+    }
+
+    @Test
+    void getAttemptsTest() {
+        int input = 5;
+        Attempts attempts = new Attempts(input);
+        assertThat(attempts.getNumber()).isEqualTo(input);
+    }
+}
