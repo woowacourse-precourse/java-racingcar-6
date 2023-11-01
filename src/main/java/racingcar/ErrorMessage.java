0562a751daeba4549399485a52d17af24ba73d3d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-01 13:42:21 +0900

    feat: 입력한 자동차 이름이 5자 이하가 아닌 경우 예외 발생시키는 기능
    이름의 기준은 공백을 포함한 5자 이하
    이를 어길 경우 IllegalArgumentException 예외

--- src/main/java/racingcar/ErrorMessage.java
+++ src/main/java/racingcar/ErrorMessage.java
@@ -3,5 +3,6 @@ package racingcar;
 public class ErrorMessage {
 
     public static final String NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER = "시도 횟수는 숫자 형태여야 합니다. ";
+    public static final String CAR_NAME_OVER_MAX_LENGTH = "자동차의 이름은 공백 포함 5자 이내여야 합니다. ";
 
 }
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static racingcar.ErrorMessage.CAR_NAME_OVER_MAX_LENGTH;
 import static racingcar.PrintMessage.LOCATION_SIGN;
 import static racingcar.PrintMessage.RESULT_OF_RACING_CAR_FORMAT;
 
@@ -10,11 +11,13 @@ public class RacingCar {
     private static final Integer PICK_NUMBER_START_RANGE = 0;
     private static final Integer PICK_NUMBER_END_RANGE = 9;
     private static final Integer CONDITION_OF_MOVING_FORWARD = 4;
+    private static final Integer NAME_MAX_LENGTH = 5;
 
     private String name;
     private Integer location;
 
     public RacingCar(String name) {
+        validateCarNameLength(name);
         this.name = name;
         location = 0;
     }
@@ -31,6 +34,12 @@ public class RacingCar {
 
     private Integer createRandomNumber() {
         return Randoms.pickNumberInRange(PICK_NUMBER_START_RANGE, PICK_NUMBER_END_RANGE);
+    }
+
+    private void validateCarNameLength(String carName) {
+        if(carName.length() > NAME_MAX_LENGTH){
+            throw new IllegalArgumentException(CAR_NAME_OVER_MAX_LENGTH);
+        }
     }
 
     @Override
