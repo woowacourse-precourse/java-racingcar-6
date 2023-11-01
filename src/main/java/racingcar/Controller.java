a730901defe76a6f6fd0129124fcb64d5ebd0e33
jagarchi <relong1026@naver.com>
2023-11-01 20:21:40 +0900

    0이상의 숫자가 아닐시 오류 출력

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -3,7 +3,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class Controller {
     public int tryNumber(){
         String tryNumber = Console.readLine();
+        numbersizeError(tryNumber);
         return Integer.parseInt(tryNumber);
+    }
+
+    public void numbersizeError(String tryNumber) {
+        int inputNumber = Integer.parseInt(tryNumber);
+
+        if (inputNumber <= 0) {
+            throw new IllegalArgumentException(ExceptionError.numbersizeError);
+        }
     }
 }
 
