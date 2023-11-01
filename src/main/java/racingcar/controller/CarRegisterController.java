f7a7954ca7908da9af98d92ac8e5b9e71c92606c
Ukj0ng <xb253q@gmail.com>
2023-11-01 15:30:02 +0900

    feat(CarRegisterController): 경주할 자동차들을 경주에 참여하는 기능
    추가한 기능:
    - 사용자에게 입력받은 자동차들을 경주에 참여시키는 기능

--- src/main/java/racingcar/controller/CarRegisterController.java
+++ src/main/java/racingcar/controller/CarRegisterController.java
@@ -1,6 +1,26 @@
 package racingcar.controller;
 
-public class CarRegistController {
-  
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.RacingCars;
+import racingcar.view.InputView;
+
+public class CarRegisterController {
+    private final RacingCars racingCars;
+    
+    public CarRegisterController(RacingCars racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void registerCars() {
+        List<String> names = InputHandler.parseCarName(InputView.askForCarName());
+        for(String name : names) {
+            enterRacing(name);
+        }
+    }
+
+    private void enterRacing(String name) {
+        racingCars.addCar(new Car(name));
+    }
 
 }
