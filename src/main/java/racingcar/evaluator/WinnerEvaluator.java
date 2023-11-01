daa910a58c62deb57e06f8202612956127deae2c
박은지 <qkrdmswl1018@naver.com>
2023-11-01 14:18:06 +0900

    refactor: 최댓값을 구하는 동작을 메서드로 분리

--- src/main/java/racingcar/evaluator/WinnerEvaluator.java
+++ src/main/java/racingcar/evaluator/WinnerEvaluator.java
@@ -6,14 +6,17 @@ import racingcar.domain.Car;
 public class WinnerEvaluator {
 
     public List<String> getWinners(List<Car> cars) {
-        int maxNumber = cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElseThrow(IllegalArgumentException::new);
-
+        int maxNumber = getMaxNumber(cars);
         return cars.stream()
                 .filter(car -> car.getPosition() == maxNumber)
                 .map(Car::getName)
                 .toList();
+    }
+
+    public int getMaxNumber(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
