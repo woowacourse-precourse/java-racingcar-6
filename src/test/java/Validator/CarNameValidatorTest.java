6d880f88acfccf44dcd02b8a5b926dfa68edca4e
ebang <dskhe8@gmail.com>
2023-11-01 12:15:15 +0900

    refactor: ValidatorTest 클래스 분리

---
+++ src/test/java/Validator/CarNameValidatorTest.java
@@ -0,0 +1,31 @@
+package Validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class CarNameValidatorTest {
+
+
+    @Test
+    void 자동차_이름이_공백_일때_오류() {
+
+        //given
+        String input = "";
+
+        assertThatThrownBy(() -> CarNameValidator.getInstance().validateCarName(input)) //when
+                .isInstanceOf(IllegalArgumentException.class); //then
+
+    }
+
+    @Test
+    void 자동차_이름이_5자_초과_일때_오류() {
+
+        //given
+        String input = "longName";
+
+        assertThatThrownBy(() -> CarNameValidator.getInstance().validateCarName(input)) //when
+                .isInstanceOf(IllegalArgumentException.class); //then
+    }
+
+}
---
+++ src/test/java/Validator/GameRoundValidatorTest.java
@@ -0,0 +1,28 @@
+package Validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class GameRoundValidatorTest {
+
+    @Test
+    void int_max_보다_크면_오류() {
+        //given
+        String gameRound = "2147483648";
+
+        assertThatThrownBy(() -> GameRoundValidator.getInstance().validateGameRoundAndReturn(gameRound)) //when
+                .isInstanceOf(IllegalArgumentException.class); //then
+
+    }
+
+    @Test
+    void 숫자_0보다_작으면_오류() {
+        //given
+        String gameRound = "-1";
+        assertThatThrownBy(() -> GameRoundValidator.getInstance().validateGameRoundAndReturn(gameRound)) //when
+                .isInstanceOf(IllegalArgumentException.class); //then
+    }
+
+
+}
