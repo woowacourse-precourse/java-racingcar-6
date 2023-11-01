6e593ecd246a349339e50f7aad18e7066b731a61
soyoungxxx <otherplet@gmail.com>
2023-11-01 18:31:36 +0900

    test(racingcar): NumberGenerator Test 파일 생성

---
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+}
