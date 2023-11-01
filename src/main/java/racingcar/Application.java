0fffaa67356566bbf4c88cece4c3a6cb4724afc1
74059 <shyeone@naver.com>
2023-11-01 17:36:42 +0900

    refactor: 시도할 횟수 읽어오는 부분 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,8 +10,7 @@ public class Application {
     String[] names_list = names.split(",");
     racingcarModules.checked_car_name(names_list);
 
-    System.out.println("시도할 회수는 몇회인가요?");
-    String move_num = Console.readLine();
+    String move_num = InputViews.read_move_num();
     racingcarModules.checked_move_num(move_num);
 
     Games game = new Games(names_list, Integer.parseInt(move_num));
