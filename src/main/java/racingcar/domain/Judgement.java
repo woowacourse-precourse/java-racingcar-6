56fdf7a5738085b7e9410376ae84440a3f713dd1
kite707 <12201680@inha.edu>
2023-11-01 21:12:15 +0900

    feat(racingcar): 자동차 이름 5글자 초과 여부 검증 기능 구현

--- src/main/java/racingcar/domain/Judgement.java
+++ src/main/java/racingcar/domain/Judgement.java
@@ -1,7 +1,18 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Judgement {
     public boolean isFourOrMore(int number) {
         return number >= 4;
     }
+
+    public void validateCarNames(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 최대 5글자입니다!");
+            }
+        }
+    }
+
 }
