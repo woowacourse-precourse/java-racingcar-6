dc0b90242cfb5b8c7a4b28839ca7739cda49fdb3
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-01 22:29:52 +0900

    RacingCarTest : getLoc

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,21 +8,25 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 class RacingCarTest {
-//    @BeforeEach
-//    void setUp(){
-//        RacingCar car = new RacingCar(name);
-//    }
+    private String test_name = "test_name";
+    private RacingCar car;
+
+    @BeforeEach
+    void setUp(){
+        car = new RacingCar(test_name);
+    }
 
     @Test
-    void getLoc() {
+    public void getLoc() { //시작 위치 + 값 불러오기 테스트
+        int result = car.getLoc();
+        assertThat(result).isEqualTo(0);
     }
 
     @Test
     void getName() {
-        String test_name = "test_name";
-        RacingCar car = new RacingCar(test_name);
         String result = car.getName();
         assertThat(result).isEqualTo(test_name);
     }
