f7916f87b38fc3b6768fd3f97bcffae6d60e00f2
PeterChoi <94peterchoi@gmail.com>
2023-11-01 17:11:25 +0900

    feat: 게임 진행상황 출력용 클래스(GameOutputView) 추가 및 구현
    - printCarPositions: 매 라운드 경주 이후 자동차들의 위치 출력
    - printWinner: 경주 최종 우승자(들)의 이름을 출력

---
+++ src/main/java/racingcar/view/GameOutputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class GameOutputView {
+
+    public static void printCarPositions(String positions) {
+        System.out.println(positions);
+    }
+
+    public static void printWinner(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+}
