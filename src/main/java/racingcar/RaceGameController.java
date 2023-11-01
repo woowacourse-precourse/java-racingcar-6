b0080300110a555b76aa803c06bc7b6d8deaaf24
BangMingGing <privatebmk802@naver.com>
2023-11-01 11:59:01 +0900

    car names and round validation function complete

--- src/main/java/racingcar/RaceGameController.java
+++ src/main/java/racingcar/RaceGameController.java
@@ -1,4 +1,51 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RaceGameController {
+    View view = new View();
+
+    public void start() {
+        view.showInputCars();
+        String carNamesInput = view.getInput();
+        validCarNamesInput(carNamesInput);
+        List<String> carNamesList = splitCarNamesInput(carNamesInput);
+        validCarNamesList(carNamesList);
+
+        view.showInputRound();
+        String roundInput = view.getInput();
+        validRoundInput(roundInput);
+        int roundInt = Integer.parseInt(roundInput);
+
+        System.out.println(carNamesList);
+        System.out.println(roundInt);
+    }
+
+    private void validCarNamesInput(String carNamesInput) {
+        if (!carNamesInput.contains(",")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<String> splitCarNamesInput(String carNamesInput) {
+        String[] carNames = carNamesInput.split(",");
+        return Arrays.asList(carNames);
+    }
+
+    private void validCarNamesList(List<String> carNamesList) {
+        for (String carName : carNamesList) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validRoundInput(String roundInput) {
+        try {
+            Integer.parseInt(roundInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,4 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
+    private final String PLEASE_INPUT_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String PLEASE_INPUT_ROUND = "시도할 회수는 몇회인가요?";
+    private final String RESULT_PREFIX = "\n실행결과";
+    private final String WINNER_PREFIX= "최종 우승자 : ";
+
+    public String getInput() {
+        return Console.readLine();
+    }
+
+    public void showInputCars() {
+        System.out.println(PLEASE_INPUT_CARS);
+    }
+
+    public void showInputRound() {
+        System.out.println(PLEASE_INPUT_ROUND);
+    }
+
+    public void showResult(String result) {
+        System.out.println(RESULT_PREFIX + result);
+    }
+
+    public void showWinner(String winner) {
+        System.out.println(WINNER_PREFIX + winner);
+    }
 }
