c429de65672b29e9a1aa6b76ceca3a2295151208
olsohee <dlthgml0108@daum.net>
2023-11-01 16:34:25 +0900

    refactor(RandomNumberGenerator): 싱글톤 패턴으로 구현

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public final class RandomNumberGenerator {
+public class RandomNumberGenerator {
 
     private static final int MIN = 0;
     private static final int MAX = 9;
@@ -10,7 +10,15 @@ public final class RandomNumberGenerator {
     private RandomNumberGenerator() {
     }
 
-    public static int generateRandomNumber() {
+    private static class RandomNumberGeneratorHolder {
+        private static RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    }
+
+    public static RandomNumberGenerator getInstance() {
+        return RandomNumberGenerator.RandomNumberGeneratorHolder.randomNumberGenerator;
+    }
+
+    public int generateRandomNumber() {
         return Randoms.pickNumberInRange(MIN, MAX);
     }
 }
