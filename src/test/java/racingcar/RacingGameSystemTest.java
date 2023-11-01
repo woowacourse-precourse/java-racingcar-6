ae0d6486ee48d387fe7567fd20dca10f432262bb
leesh <qwqw8019@naver.com>
2023-11-01 16:06:16 +0900

    test: 잘못된 자동차 이름 테스트

--- src/test/java/racingcar/RacingGameSystemTest.java
+++ src/test/java/racingcar/RacingGameSystemTest.java
@@ -1,13 +1,17 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.util.TestConstantsProvider.provideInvalidSystemTestNameConstants;
 import static racingcar.util.TestConstantsProvider.provideValidSystemTestConstants;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -33,6 +37,26 @@ public class RacingGameSystemTest extends NsTest {
                 }, firstRandomNumber, subsequentRandomNumbers
         );
     }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    @DisplayName("예외 테스트")
+    class ExceptionTest {
+        @ParameterizedTest(name = "자동차 이름에 {0}을 입력하면 예외가 발생한다.")
+        @DisplayName("잘못된 자동차 이름 테스트")
+        @MethodSource("provideInvalidSystemTestNameParameter")
+        void nameExceptionTest(final String invalidCarName) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(invalidCarName))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        private Stream<Arguments> provideInvalidSystemTestNameParameter() {
+            return provideInvalidSystemTestNameConstants();
+        }
+    }
+
 
     private Stream<Arguments> provideValidSystemTestParameter() {
         return provideValidSystemTestConstants();
