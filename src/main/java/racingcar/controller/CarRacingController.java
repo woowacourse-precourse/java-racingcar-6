2ae9b7a00bd0c59a89c3ae9cd3289c8e1d9c9f56
chanwoo <mdsoo55828@gmail.com>
2023-11-01 19:26:21 +0900

    Feat: CarRacingController 경주 실행 로직 작성

--- src/main/java/racingcar/controller/CarRacingController.java
+++ src/main/java/racingcar/controller/CarRacingController.java
@@ -4,6 +4,8 @@ import racingcar.model.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.List;
+
 public class CarRacingController {
     private OutputView outputView;
     private InputView inputView;
@@ -13,6 +15,25 @@ public class CarRacingController {
         this.outputView = new OutputView();
         this.inputView = new InputView();
         this.cars = new Cars();
+    }
+
+    public void run() {
+        inputView.printCarNameMessage();
+        List<String> carNames = inputView.inputCarNames();
+        cars.createCars(carNames);
+        inputView.printTimesInputMessage();
+        Integer inputTimes = inputView.inputNumber();
+        outputView.printResultMassage();
+
+        for (int i = 0; i < inputTimes; i++) {
+            startRacing();
+        }
+
+        outputView.printFinalWinner(cars.getFinalWinner());
+    }
+
+    private void startRacing() {
+        outputView.printRoundGameResult(cars.roundCarResult());
     }
 
 }
