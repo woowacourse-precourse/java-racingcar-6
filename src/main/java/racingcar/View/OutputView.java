7837c37a356f9e48809f41e914aafb1325907374
jihye134seo <jihye.seo134@gmail.com>
2023-11-01 14:41:20 +0900

    style(OutputView.java): 빈줄, 공백 수정

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -8,40 +8,33 @@ public class OutputView {
     private static final String WINNERS_GUIDE = "최종 우승자 : ";
     private static final String RESULT_GUIDE = "실행 결과";
 
-    public void printRESULT_GUIDE(){
+    public void printRESULT_GUIDE() {
         System.out.println("\n"+RESULT_GUIDE);
     }
 
-    public void printRacingResult(List<Car> cars){
-
-        for (Car car : cars){
+    public void printRacingResult(List<Car> cars) {
+        for (Car car : cars) {
             printOneCarResult(car);
         }
         System.out.println();
     }
 
-    private void printOneCarResult(Car car){
+    private void printOneCarResult(Car car) {
         System.out.printf("%s : ",car.getName());
-        for (int i=0; i< car.point; i++){
+        for (int i=0; i<car.point; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public void printWinners(List<String> cars){
-
+    public void printWinners(List<String> cars) {
         System.out.print(WINNERS_GUIDE);
-
-        if(cars.size() == 1){
+        if (cars.size() == 1) {
             System.out.println(cars.get(0));
         }
-        else if(cars.size() > 1){
+        else if (cars.size() > 1) {
             System.out.println(String.join(",", cars));
         }
-
     }
-
-
-
 
 }
