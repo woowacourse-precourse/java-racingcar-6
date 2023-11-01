56e865a0e979f9a390863f0efb1ab79c4495f977
yeop0740 <ssucgoo@gmail.com>
2023-11-01 19:01:00 +0900

    fix: add condition in Validator#validateCarName

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Validator {
     public static boolean validateCarName(String splittedName) throws IllegalArgumentException {
-        if (splittedName.length() <= 5) {
+        if (!splittedName.isEmpty() && splittedName.length() <= 5) {
             return true;
         }
         throw new IllegalArgumentException("올바르지 않은 이름입니다.");
