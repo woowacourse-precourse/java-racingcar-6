2426bf9ea1696b598b1b9c07c18a4e2a4a870af1
gyahn <pop2413@gmail.com>
2023-11-01 20:35:56 +0900

    fix: Application 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         UserInput userInput = new UserInput();
         String[] carNames = userInput.InputCar();
         int trying = userInput.InputNum();
