55d0e2a787f9b2fdb59abdbba77b044f4f20be94
inthejim <inthejim24@gmail.com>
2023-11-01 20:36:25 +0900

    refactor: Controller 내 문자열이나 수 상수화

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -3,68 +3,77 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Controller{
+public class Controller {
     private Race race;
-    private List<Car> getInputForCarNames(){
-        String input=View.getCarNames();
-        String[] carNames=input.split(",");
-        return createCarFromCarNames(carNames);
+    private static final String DELIMITER_COMMA = ",";
+    private static final int MAX_LENGTH_CAR_NAME = 5;
+    private static final int MIN_VALUE_TRY_COUNT = 1;
+    private static final String ERROR_BLANK_CAR_NAME = "자동차 이름은 공백은 불가합니다.";
+    private static final String ERROR_MAX_LENGTH_CAR_NAME = "자동차 이름은 5자 이하만 가능합니다.";
+    private static final String ERROR_MIN_TRY_COUNT = "시도 횟수는 1 이상이어야 합니다.";
+    private static final String ERROR_PARSE_TRY_COUNT = "시도 횟수는 숫자 형식을 만족해야 합니다.";
+
+
+    private List<Car> getInputForCarNames() {
+        String inputForCarName = View.getCarNames();
+        String[] carNames = inputForCarName.split(DELIMITER_COMMA);
+        return createCarsFromCarNames(carNames);
     }
 
-    private void validateCarName(String carName){
-        if(carName.isBlank()){
-            throw new IllegalArgumentException();
+    private void validateCarName(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException(ERROR_BLANK_CAR_NAME);
         }
-        if(carName.length()>5){
-            throw new IllegalArgumentException();
+        if (carName.length() > MAX_LENGTH_CAR_NAME) {
+            throw new IllegalArgumentException(ERROR_MAX_LENGTH_CAR_NAME);
         }
     }
 
-    private List<Car> createCarFromCarNames(String[] carNames){
-        List<Car> cars=new ArrayList<>();
-        for (String carName: carNames){
+    private List<Car> createCarsFromCarNames(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
             validateCarName(carName);
             cars.add(new Car(carName));
         }
         return cars;
     }
 
-    private void validateTryCount(int tryCount){
-        if(tryCount<1){
-            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+    private void validateTryCount(int tryCount) {
+        if (tryCount < MIN_VALUE_TRY_COUNT) {
+            throw new IllegalArgumentException(ERROR_MIN_TRY_COUNT);
         }
     }
 
-    private int parseToInteger(String tryCount){
+    private int parseToInteger(String tryCount) {
         try {
             return Integer.parseInt(tryCount);
-        }catch(NumberFormatException e){
-            throw new IllegalArgumentException();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_PARSE_TRY_COUNT);
         }
     }
 
-    private int getInputForTryCount(){
-        String inputForTryCount= View.getTryCount();
+    private int getInputForTryCount() {
+        String inputForTryCount = View.getTryCount();
         int tryCount = parseToInteger(inputForTryCount);
         validateTryCount(tryCount);
 
         return tryCount;
     }
 
-    public void prepareRace(){
-        List<Car> cars=getInputForCarNames();
-        int tryCount=getInputForTryCount();
+    public void prepareRace() {
+        List<Car> cars = getInputForCarNames();
+        int tryCount = getInputForTryCount();
 
         race = new Race(cars, tryCount);
     }
 
-    public void startRace(){
+    public void startRace() {
         View.printResultAlarm();
         race.conductRace();
     }
 
-    public void announceResult(){
-        List<String> winners=race.getWinners();
+    public void announceResult() {
+        List<String> winners = race.getWinners();
         View.printFinalWinner(String.join(", ", winners));
     }
 }
