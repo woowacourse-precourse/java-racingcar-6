fb4dc2a0a5708b1bbb64a2ceb9d37da680a60d43
DavinAn <blueapple1030@gmail.com>
2023-11-01 22:31:43 +0900

    feat(OutputView) : 최종 우승자를 출력하는 메서드 작성

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -11,4 +11,7 @@ public class OutputView {
     System.out.println();
   }
 
+  public void printWinner(List<String> winners) {
+    System.out.println("최종 우승자 : " + String.join(", ", winners));
+  }
 }
