e8347e282f7a47f188068af82b270d6d8ddfa7bf
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-01 18:36:56 +0900

    fix(GameMessage): 요구 사항에 맞게 횟수 -> 회수로 변경

--- src/main/java/racingcar/utils/message/GameMessage.java
+++ src/main/java/racingcar/utils/message/GameMessage.java
@@ -2,7 +2,7 @@ package racingcar.utils.message;
 
 public enum GameMessage {
     REQUEST_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    REQUEST_GAME_ROUND_MESSAGE("시도할 횟수는 몇회인가요?"),
+    REQUEST_GAME_ROUND_MESSAGE("시도할 회수는 몇회인가요?"),
     RESULT_MESSAGE("실행 결과"),
     FINAL_WINNER_MESSAGE("최종 우승자 : "),
     ROUND_RESULT("%s : %s"),
