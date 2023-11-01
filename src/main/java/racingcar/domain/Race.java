ca51607532baa2c2c73aeab5494a18f9d5a16637
sseunghoon <sseunghun99@naver.com>
2023-11-01 17:56:30 +0900

    refactor: Race의 라운드 결과 발표, 최종 우승자 선별 및 발표 기능을 Referee 클래스로 분리

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -2,18 +2,16 @@ package racingcar.domain;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.List;
-import java.util.Stack;
 import racingcar.domain.wrapper.RaceRound;
 
 public class Race {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NUMBER_OF_ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String ROUND_RESULT_MESSAGE = "\n시도할 회수는 몇회인가요?";
-    private static final String RACE_RESULT_MESSAGE = "최종 우승자 : ";
 
     private final Cars cars;
     private final RaceRound numberOfRound;
+
     private Race() {
         this.cars = Cars.create(getCarNameInput());
         this.numberOfRound = RaceRound.create(getNumberOfRoundInput());
@@ -21,15 +19,6 @@ public class Race {
 
     public static Race create() {
         return new Race();
-    }
-
-    public void start() {
-        System.out.println(ROUND_RESULT_MESSAGE);
-        for (int i = 0; i < numberOfRound.getNumberOfRound(); ++i) {
-            roundStart();
-        }
-        Stack<Car> winners = pickWinners(cars.getCars());
-        printWinners(winners);
     }
 
     private static String getCarNameInput() {
@@ -42,31 +31,18 @@ public class Race {
         return readLine();
     }
 
+    public void start() {
+        System.out.println(ROUND_RESULT_MESSAGE);
+        for (int i = 0; i < numberOfRound.getNumberOfRound(); ++i) {
+            roundStart();
+            Referee.announceRoundResult(cars.getCars());
+        }
+        Referee.announceWinners(cars.getCars());
+    }
+
     private void roundStart() {
-        for (Car car: cars.getCars()) {
+        for (Car car : cars.getCars()) {
             car.tryMove();
-            System.out.println(car.getCarInfo());
-        }
-    }
-
-    private Stack<Car> pickWinners(List<Car> cars) {
-        Stack<Car> winners = new Stack<>();
-        for (Car car: cars) {
-            while (!winners.empty() && winners.peek().getMovedDistance() < car.getMovedDistance()) {
-                winners.pop();
-            }
-            if (winners.empty() || winners.peek().getMovedDistance() == car.getMovedDistance()) {
-                winners.push(car);
-            }
-        }
-        return winners;
-    }
-
-    private void printWinners(Stack<Car> cars) {
-        System.out.print(RACE_RESULT_MESSAGE);
-        System.out.print(cars.get(0).getName());
-        for (int i = 1; i < cars.size(); ++i) {
-            System.out.print(", " + cars.get(i).getName());
         }
     }
 }
---
+++ src/main/java/racingcar/domain/Referee.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Referee {
+    private static final String RACE_RESULT_MESSAGE = "최종 우승자 : ";
+
+    public static void announceRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCarInfo());
+        }
+    }
+
+    private static Stack<Car> pickWinners(List<Car> cars) {
+        Stack<Car> winners = new Stack<>();
+        for (Car car : cars) {
+            while (!winners.empty() && winners.peek().getMovedDistance() < car.getMovedDistance()) {
+                winners.pop();
+            }
+            if (winners.empty() || winners.peek().getMovedDistance() == car.getMovedDistance()) {
+                winners.push(car);
+            }
+        }
+        return winners;
+    }
+
+    public static void announceWinners(List<Car> cars) {
+        Stack<Car> winners = pickWinners(cars);
+        System.out.print(RACE_RESULT_MESSAGE);
+        System.out.print(winners.get(0).getName());
+        for (int i = 1; i < winners.size(); ++i) {
+            System.out.print(", " + winners.get(i).getName());
+        }
+    }
+}
