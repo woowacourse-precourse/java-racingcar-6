4b0b1cec5e26f2a040695ce0b5f48e344b7ea4de
COBI-98 <tkdgus968@naver.com>
2023-11-01 16:09:13 +0900

    feat: 실행결과 출력뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,8 @@ public class OutputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String NEW_LINE = "\n";
+    private static final String EXECUTION_RESULT = "실행 결과";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
@@ -11,5 +13,9 @@ public class OutputView {
 
     public void printRequestTryCount(){
         System.out.println(INPUT_TRY_COUNT);
+    }
+
+    public void printExecutionResult(){
+        System.out.println(NEW_LINE + EXECUTION_RESULT);
     }
 }
