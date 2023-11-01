55922e3052fc550da4d3c6bbeb04d1edea25fa37
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-11-01 22:38:16 +0900

    test: 문자열 나누기 메서드 테스트
    문자열을 쉼표를 기준으로 나누는 메서드를 테스트

--- src/test/java/racingcar/racingcarTest.java
+++ src/test/java/racingcar/racingcarTest.java
@@ -3,10 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class racingcarTest extends NsTest {
+    private final racingcarGame racingcarGame = new racingcarGame();
+
     @Test
     void 숫자입력예외처리(){
         assertSimpleTest(() ->
@@ -23,6 +28,14 @@ public class racingcarTest extends NsTest {
         );
     }
 
+    @Test
+    void 이름나누기() {
+        String input = "1,2";
+        List<String> result = racingcarGame.splitNames(input);
+
+        assertThat(result).contains("2", "1");
+        assertThat(result).containsExactly("1", "2");
+    }
 
     @Override
     public void runMain() {
