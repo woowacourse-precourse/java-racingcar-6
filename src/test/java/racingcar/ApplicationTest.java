ad7ccd4db867d3141a4b42d46c2fdfc31b244481
sseunghoon <sseunghun99@naver.com>
2023-11-01 20:27:51 +0900

    test: Test code 추가 (이름, 시도할 횟수 입력 예외처리)
    - 이름이_1글자_미만인경우_예외_발생()
    - 이름_문자열이_구분자로_시작한다면_예외_발생()
    -
    이름_문자열이_구분자로_끝나면_예외_발생()
    - 중복된_이름이_있는_경우_예외_발생()
    -
    이동을_시도할_횟수가_숫자가_아닌_경우_예외_발생()
    - 이동을_시도할_횟수가_양수가_아닌_경우_예외_발생()
    -
    이동을_시도할_횟수가_int범위를_벗어날_경우_예외_발생()

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,11 +15,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -27,6 +27,62 @@ class ApplicationTest extends NsTest {
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_1글자_미만인경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,java", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_문자열이_구분자로_시작한다면_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,java", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_문자열이_구분자로_끝나면_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 중복된_이름이_있는_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동을_시도할_횟수가_숫자가_아닌_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동을_시도할_횟수가_양수가_아닌_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동을_시도할_횟수가_int범위를_벗어날_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "2147483650"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
