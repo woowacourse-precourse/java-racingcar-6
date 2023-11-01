36d65b99b977aecd459efeb7c3429c585328f7b6
psionic0324 <psionic0324@naver.com>
2023-11-01 14:03:10 +0900

    fix: 우승자 출력 시 콤마 위치가 잘못된 부분 수정
    기존에는 우승자 출력 시 일괄적으로 이름 뒤에 ", "가 붙어 출력 양식에 맞지 않게 출력되던 부분을 우승자가 1명일 경우와 2명일 경우로 나누어 콤마가 알맞은 위치에만 추가되도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,28 +10,30 @@ import racingcar.view.OutputView;
  */
 public class Application {
     public static void main(String[] args) {
-        /** 자동차 이름을 입력받고, 자동차 객체 생성(자동차 컨트롤러에서 관리) */
+        /* 자동차 이름을 입력받고, 자동차 객체 생성(자동차 컨트롤러에서 관리) */
         System.out.println(OutputView.pleaseInputCarsName());
         String[] carNames = InputView.inputCarNames();
         CarController carController = new CarController(carNames);
 
-        /** 시도 회차 반복 회수 입력받기 */
+        /* 시도 회차 반복 회수 입력받기 */
         System.out.println(OutputView.pleaseInputTriesCount());
         final int numberOfTries = InputView.inputNumberOfTries();
 
-        /** n회차 반복 실행 시작 */
+        /* n회차 반복 실행 시작 */
         System.out.println(OutputView.triesStartMessage());
         for (int i = 0; i < numberOfTries; i++) {
             carController.tryToMoveForward();
             carController.printCarsMovePoint();
         }
 
-        /** 우승한 자동차를 찾아서 결과 출력 */
+        /* 우승한 자동차를 찾아서 결과 출력 */
         System.out.print(OutputView.finalWinnerMessage());
         final Car maxMovePointCar = carController.getMaxMovePointCar();
         List<String> winners = carController.findHaveSamePointCars(maxMovePointCar);
-        winners.stream()
-                .map(o -> o + ", ")
-                .forEach(System.out::print);
+        if (winners.size() > 1) { // 우승자가 2명 이상일 경우
+            System.out.println(String.join(", ", winners));
+        } else if (winners.size() == 1) { // 우승자가 1명일 경우
+            System.out.println(winners.get(0));
+        }
     }
 }
