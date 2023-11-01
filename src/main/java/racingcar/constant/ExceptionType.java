25efa6281bfb8f3ad1668f691b08fd6e81304a57
itsmo1031 <itsmo1031@gmail.com>
2023-11-01 20:46:46 +0900

    feat(constant): 경기 최소 인원에 대한 Exception 메세지 추가
    - 2명 미만의 인원으로는 경기가 진행될 이유가 없다고 판단하여 IllegalArgumentException을 throw하도록 하였고, 이에 ExceptionType에 인원 관련 Exception Message를 추가함.

--- src/main/java/racingcar/constant/ExceptionType.java
+++ src/main/java/racingcar/constant/ExceptionType.java
@@ -2,7 +2,8 @@ package racingcar.constant;
 
 public enum ExceptionType {
     NAME("이름 형식이 잘못된 것 같아요. 공백이 포함되지 않은 1글자 이상 5글자 이내의 문자열을 입력해 주세요."),
-    NUMBER("횟수 입력이 잘못된 것 같아요. 이동 횟수는 자연수로만 이루어져야 해요.");
+    NUMBER("횟수 입력이 잘못된 것 같아요. 이동 횟수는 자연수로만 이루어져야 해요."),
+    MEMBER("경주에 참여하는 인원이 부족해요. 인원은 2명 이상이어야 해요.");
 
     private final String message;
 
