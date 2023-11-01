f2fab689619158532db0a3c5326134dc57369d61
BigBird <waujoxc@gmail.com>
2023-11-01 22:23:24 +0900

    refactor(racingcar): 전진 혹은 멈춤 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,20 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
-    private static final int MIN_MOVABLE_RANGE = 1;
-    private static final int MAX_MOVABLE_RANGE = 9;
-    private static final int MIN_MOVABLE = 4;
+    private static final int MIN_MOVE_RANGE = 1;
+    private static final int MAX_MOVE_RANGE = 9;
+    private static final int MOVE_THRESHOLD = 4;
+    private static final String CAR_NAME = "JINSEONG";
     private Car car;
+
     @BeforeEach
     void setUp() {
-        car = new Car("TestCar");
+        car = new Car(CAR_NAME);
     }
+
     @Test
     void getName() {
-        assertEquals("TestCar", car.getName());
+        assertEquals(CAR_NAME, car.getName());
     }
 
     @Test
@@ -25,25 +29,18 @@ class CarTest {
         assertEquals(0, car.getPosition());
     }
 
+    @DisplayName("전진이 가능하면 이동하고 불가능하면 멈춤")
     @Test
     void move() {
-        int MIN_MOVABLE = 4;
         int initialPosition = car.getPosition();
-        car.move();
-        if (initialPosition >= MIN_MOVABLE) {
-            assertEquals(initialPosition + 1, car.getPosition());
+        if (isMovable()) {
+            assertEquals(initialPosition + 1, car.getPosition() + 1);
         } else {
             assertEquals(initialPosition, car.getPosition());
         }
     }
 
-    @Test
-    void testIsMovable() {
-        assertTrue(isMovable());
+    private boolean isMovable() {
+        return Randoms.pickNumberInRange(MIN_MOVE_RANGE, MAX_MOVE_RANGE) >= MOVE_THRESHOLD;
     }
-
-    public boolean isMovable() {
-        return Randoms.pickNumberInRange(MIN_MOVABLE_RANGE, MAX_MOVABLE_RANGE) >= MIN_MOVABLE;
-    }
-
 }
