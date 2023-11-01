91a5e5daafdd8c5afe4dbb0da8880c088fa4b4ae
IDpluto <d0013159@gmail.com>
2023-11-01 10:37:01 +0900

    refactor: intent depth 3 넘는 메소드 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,39 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.util.Validator;
+import java.util.Arrays;
 
 
 public class InputView {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
-  public String readCarName() {
-    String input = Console.readLine();
-    return Validator.parseToNames(input);
-  }
+    public String[] readCarName() {
+        String[] carNames = Console.readLine().split(",");
+        return parseToNames(carNames);
+    }
 
-  public int readProgressCount() {
-    String input = Console.readLine();
-    return Validator.parseToInt(input);
-  }
+    public int readProgressCount() {
+        String input = Console.readLine();
+        return parseToInt(input);
+    }
+
+    private String[] parseToNames(String[] carNames) {
+        return Arrays.stream(carNames)
+                .map(String::trim)
+                .peek(carName -> {
+                    if (carName.length() == 0 || carName.length() > MAX_CAR_NAME_LENGTH) {
+                        throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+                    }
+                })
+                .toArray(String[]::new);
+    }
+
+    private static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 값을 입력해주세요.");
+        }
+    }
 
 }
