92add2b9f2fdf6c046d0d346b79f7ca43b2fcc61
changha <mentenseoul@gmail.com>
2023-11-01 21:53:57 +0900

    feat : 오타 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class OutputView {
     public static void printStartCarNameMessage(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public static void printTryNumberMessage(){
