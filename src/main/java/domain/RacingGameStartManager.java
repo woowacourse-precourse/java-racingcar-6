3305a8a4bfd542b1fad9bd2f527743cde26a4fc5
jjongwon7 <jjongwon7@naver.com>
2023-11-01 22:32:15 +0900

    feat: 자동차 이름 검증 기능 추가 입력받은 자동차 이름이 입력 조건을 만족하는지 검증한다.

--- src/main/java/domain/RacingGameStartManager.java
+++ src/main/java/domain/RacingGameStartManager.java
@@ -6,6 +6,16 @@ public class RacingGameStartManager {
     private static final int RACING_CAR_MAX_LENGTH = 5;
     private static final int RACING_CAR_MIN_LENGTH = 1;
 
+    private void validateRacingCarName(String racingCarName, Set<RacingCar> racingCarSet) {
+        if (!isRacingCarNameLengthInRange(racingCarName)) {
+            throw new IllegalArgumentException(
+                    "자동차 이름은 " + RACING_CAR_MAX_LENGTH + "자 이상 " + RACING_CAR_MAX_LENGTH + "자 이하만 입력 가능합니다."
+            );
+        } else if (!isRacingCarNameUnique(racingCarName, racingCarSet)) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+    }
+
     private boolean isRacingCarNameLengthInRange(String racingCarName) {
         return (racingCarName.length() >= RACING_CAR_MIN_LENGTH) && (racingCarName.length() <= RACING_CAR_MAX_LENGTH);
     }
