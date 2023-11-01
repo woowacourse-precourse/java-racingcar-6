09865e2c3811f6699849732fe0bbf81c7fed37dd
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 22:49:49 +0900

    test(EntryTest): 차량 추가 기능에 대한 단위 테스트 작성
    1. join_테스트
    -> 중복이 존재하지 않을 때 정상 동작을 테스트한다.
    
    2. 중복_join_실패
    -> 중복이 존재할 때 IllegalArgumentException이 발생하는지 테스트한다.
    
    3. size_테스트
    -> 3 개의 중복 없는 차량을 가진 엔트리에 대해 size() 함수의 반환 값이 3인지 확인한다.

--- src/test/java/racingcar/model/EntryTest.java
+++ src/test/java/racingcar/model/EntryTest.java
@@ -2,6 +2,51 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class EntryTest {
 
+    @Test
+    void join_테스트() {
+        try {
+            Car c1 = new Car(new Name("a"));
+            Car c2 = new Car(new Name("bc"));
+            Car c3 = new Car(new Name("d e f"));
+
+            Entry entry = new Entry();
+            entry.join(c1);
+            entry.join(c2);
+            entry.join(c3);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+
+    @Test
+    void 중복_join_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Car c1 = new Car(new Name("abc"));
+            Car c2 = new Car(new Name("abc"));
+
+            Entry entry = new Entry();
+            entry.join(c1);
+            entry.join(c2);
+        });
+    }
+
+    @Test
+    void size_테스트() {
+        Car c1 = new Car(new Name("a"));
+        Car c2 = new Car(new Name("b"));
+        Car c3 = new Car(new Name("c"));
+
+        Entry entry = new Entry();
+        entry.join(c1);
+        entry.join(c2);
+        entry.join(c3);
+
+        assertEquals(3, entry.size());
+    }
 }
