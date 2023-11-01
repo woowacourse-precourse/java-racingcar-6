3fa65082b407228a6269a836dfd3a42e18c6b276
senzoo-oh <osj3418@gmail.com>
2023-11-01 18:27:28 +0900

    feat: add 무작위 값 생성 기능
    RacingCar 클래스 내부에 무작위 값을 생성하는 rollDice() 메서드 추가

---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,14 @@
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCar {
+    String name;
+    int moveCount;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+    private int rollDice() {
+        int diceNumber = Randoms.pickNumberInRange(0,9);
+        return diceNumber;
+    }
+}
