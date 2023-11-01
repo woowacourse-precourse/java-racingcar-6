24a66355d4cad212491897754a2888f78d0e1a42
djc06048 <gsafe1213@gmail.com>
2023-11-01 11:31:47 +0900

    style: 불필요한 코드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,9 +2,6 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputView {
     private static final String carNameInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
