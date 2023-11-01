77b72ebbb13313938803ced156cc60a5b067400b
MisaSohee <sohee5951@naver.com>
2023-11-01 20:22:17 +0900

    출력을 담당하는 OutputHandler 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.Service.InputHandler;
+import racingcar.Service.OutputHandler;
 import racingcar.Service.RacingGame;
 
 
@@ -11,10 +12,12 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+
         String[] carNames = inputHandler.getCarNamesFromUser();
         int raceCount = inputHandler.getRaceCountFromUser();
 
-        RacingGame racingGame = new RacingGame(List.of(carNames), raceCount);
+        RacingGame racingGame = new RacingGame(List.of(carNames), raceCount, outputHandler);
         racingGame.start();
         racingGame.printWinners();
     }
--- src/main/java/racingcar/Service/RacingGame.java
+++ src/main/java/racingcar/Service/RacingGame.java
@@ -10,34 +10,32 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RacingGame {
     private final List<Car> cars;
     private final int raceCount;
+    private final OutputHandler outputHandler;
 
-    public RacingGame(List<String> carNames, int raceCount) {
+    public RacingGame(List<String> carNames, int raceCount, OutputHandler outputHandler) {
         this.cars = carNames.stream()
                 .map(Car::new)
                 .collect(Collectors.toList());
         this.raceCount = raceCount;
+        this.outputHandler = outputHandler;
     }
 
     public void start() {
-        System.out.println(" 실행 결과");
         for (int i = 0; i < raceCount; i++) {
             for (Car car : cars) {
                 car.tryMove();
-                System.out.println(car.getName() + " : " + car.getMoveDistance());
             }
-            System.out.println();
+            outputHandler.printRaceResult(cars);
         }
     }
 
     public void printWinners() {
         int maxMove = getMaxMove();
-
         List<String> winners = cars.stream()
                 .filter(car -> car.getMoveCount() == maxMove)
                 .map(Car::getName)
                 .collect(Collectors.toList());
-
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        outputHandler.printWinners(winners);
     }
 
     private int getMaxMove() {
