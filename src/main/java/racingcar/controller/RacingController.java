525dd7bcc7201b98aa7a5da507e2b13066cd370e
unknown <dbslt33@mju.ac.kr>
2023-11-01 23:00:04 +0900

    feat: Add validator

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.service.RacingService;
+import racingcar.util.InputValidator;
 import racingcar.view.OutputHandler;
 
 public class RacingController {
@@ -19,9 +20,16 @@ public class RacingController {
     private void initRacing() {
         OutputHandler.printRequestCarNames();
         List<String> carNames = getCarNames();
+        if (InputValidator.isValidInputLength(carNames) || InputValidator.isExistSameName(
+            carNames)) {
+            throw new IllegalArgumentException();
+        }
 
         OutputHandler.printRequestNumberOfAttempt();
         int numberOfAttempt = getNumberOfAttempt();
+        if (InputValidator.isSmallerThanOne(numberOfAttempt)) {
+            throw new IllegalArgumentException();
+        }
 
         racingService.initRacing(carNames, numberOfAttempt);
     }
@@ -30,16 +38,24 @@ public class RacingController {
         racingService.startRacing();
     }
 
-    private void endRacing(){
+    private void endRacing() {
         racingService.endRacing();
     }
 
     private List<String> getCarNames() {
         String userInput = Console.readLine();
+        if (InputValidator.isEmptyString(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
         return Arrays.asList(userInput.split(","));
     }
 
     private int getNumberOfAttempt() {
-        return Integer.parseInt(Console.readLine());
+        String numberOfAttempt = Console.readLine();
+        if (InputValidator.isNotNumber(numberOfAttempt)) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(numberOfAttempt);
     }
 }
