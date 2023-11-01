22d1f778258a6e1e10ca8c75f12dc38123fcf0c1
Yminji <ymg0818@naver.com>
2023-11-01 21:56:17 +0900

    feat(racingcar) : 사용하지 않은 코드 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,9 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Racing;
 import racingcar.domain.RacingCarName;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
