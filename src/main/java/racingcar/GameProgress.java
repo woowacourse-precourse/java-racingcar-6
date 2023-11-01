cba54873a3f4a1afa0621992d56a4409e6fafe54
kijen723 <kij2646@gmail.com>
2023-11-01 19:16:46 +0900

    feat(GameProgress): Add Game progress and fix methods data type and add return

--- src/main/java/racingcar/GameProgress.java
+++ src/main/java/racingcar/GameProgress.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class GameProgress {
     public static void startGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> cars = UserInput.getCarName();
+
+        printGameCountInputMessage();
+        int gameCount = UserInput.getGameCount();
+
+        printResultMessage();
+        List<Integer> carsPositions = CalculateCarsPositions.initializationCarsPositions(cars);
+
+        carsPositions = printCarsPositionsProgress(gameCount, cars, carsPositions);
+
+        List<String> winners = GetWinners.getWinners(cars, carsPositions);
+        printWinners(winners);
     }
 
     public static void printGameCountInputMessage() {
@@ -32,10 +44,13 @@ public class GameProgress {
         System.out.println();
     }
 
-    public static void printCarsPositionsProgress(int gameCount, List<String> cars, List<Integer> carsPositions) {
+    public static List<Integer> printCarsPositionsProgress(int gameCount, List<String> cars, List<Integer> carsPositions) {
         for (int i = 0; i < gameCount; i++) {
+            carsPositions = CalculateCarsPositions.calculateCarsProgresses(carsPositions);
             printCarsPositions(cars, carsPositions);
         }
+
+        return carsPositions;
     }
 
     public static void printWinners(List<String> winners) {
