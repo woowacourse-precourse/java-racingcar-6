3ffc823a1066429da659aa04f28cb3e114ce721b
강병준 <freudbjk@gmail.com>
2023-11-01 20:25:37 +0900

    feat(실행): 메인 메써드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,20 @@
 package racingcar;
 
 
+import racingcar.domain.Car;
+import racingcar.service.RaceService;
+import racingcar.view.Input;
+
+import java.util.List;
 
 public class  Application {
     public static void main(String[] args) {
-
-
+        String[] carNames = Input.inputCarName();
+        int tryCount = Input.inputTryNumber();
+        RaceService raceService = new RaceService();
+        List<Car> cars = raceService.createCars(carNames);
+        raceService.playGame(cars, tryCount);
+        raceService.printWinners(cars);
 
     }
 }
