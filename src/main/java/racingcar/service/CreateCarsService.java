04951669a7d36a987ad25859b6afe5ff5c6ab5cd
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-01 14:42:34 +0900

    refactor(CreateCarsService): UnmodifiableList를 반환하도록 수정

--- src/main/java/racingcar/service/CreateCarsService.java
+++ src/main/java/racingcar/service/CreateCarsService.java
@@ -2,7 +2,6 @@ package racingcar.service;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
@@ -18,7 +17,7 @@ public class CreateCarsService {
 	private List<Car> convertStringArrToCarList(String[] strArr){
 		return Arrays.stream(strArr)
 						.map(this::createCar)
-						.collect(Collectors.toList());
+						.toList();
 	}
 	
 	private Car createCar(String carName) {
