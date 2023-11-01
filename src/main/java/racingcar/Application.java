e84d6b8323cac48480ad3a44d2f2e79d40d1f5b8
mainuser <inging5839@naver.com>
2023-11-01 20:38:03 +0900

    refactor : 테스트코드 작성을 위한 메서드 추가 분리 및 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,11 @@
 package racingcar;
+
 import java.util.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-
-
     public static void main(String[] args) {
         inputCarInformation();
         inputTryCount();
@@ -18,6 +17,7 @@ public class Application {
     static String inputTryNum;
     static List<Integer> forwardCountsList = new ArrayList<>();
     static String inputCarName;
+
     // 자동차 이름 입력 받기
     static void inputCarInformation() {
         printCarNameInputPrompt();
@@ -41,12 +41,10 @@ public class Application {
 
     static void validateCarNames() {
         for (String checkCarNameLength : inputCarNameList) {
-            // 예외처리 1 : 5글자 이하만 자동차 이름으로 입력 가능
             if (checkCarNameLength.length() > 5) {
                 throw new IllegalArgumentException("5글자 이하의 이름만 입력 가능합니다.");
             }
         }
-        // 예외처리 2 : 자동차 이름이 중복되지 않아야 함
         Set<String> checkDuplication = new HashSet<String>(inputCarNameList);
         if (checkDuplication.size() != inputCarNameList.size()) {
             throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
@@ -71,22 +69,21 @@ public class Application {
     static void validateTryNum() {
         for (int i = 0; i < inputTryNum.length(); i++) {
             char checkInputTryNum = inputTryNum.charAt(i);
-            // 예외처리 1 : 입력 값 0으로 시작 불가 (+ 시도값 0 입력 불가)
             if (inputTryNum.charAt(0) == '0') {
                 throw new IllegalArgumentException("입력 값이 0으로 시작할 수 없습니다.");
             }
-            // 예외처리 2 : 숫자 형식만 입력 가능
             if (checkInputTryNum < '0' || checkInputTryNum > '9') {
                 throw new IllegalArgumentException("숫자형식만 입력 가능합니다.");
             }
         }
     }
 
+    // 전진 회수 결정하기 + 각 자동차의 결과 출력
     static int carNameListIndex;
     static int forwardCounts = 0;
     static String FOR_WARD_MARK = "-";
+    static int repeatPrintDash;
 
-    // 전진 회수 결정하기 + 각 자동차의 결과 출력
     static void determineForwardMovementCount() {
         initializeForwardCountsList();
         System.out.printf("%n실행 결과");
@@ -98,29 +95,43 @@ public class Application {
 
     static void initializeForwardCountsList() {
         while (forwardCountsList.size() < inputCarNameList.size()) {
-            forwardCountsList.add(0); //각 플레이어의 전진 성공 회수를 저장하는 리스트 생성
+            forwardCountsList.add(0);
         }
     }
 
+    static int RandomNum;
+
     static void updateForwardCountsBasedOnRandom() {
         for (carNameListIndex = 0; carNameListIndex < inputCarNameList.size(); carNameListIndex++) {
-            int RandomNum = Randoms.pickNumberInRange(0, 9);
-            if (RandomNum >= 4) {
-                forwardCounts = forwardCountsList.get(carNameListIndex) + 1;
-            }
-            if (RandomNum < 4) {
-                forwardCounts = forwardCountsList.get(carNameListIndex);
-            }
-            System.out.printf("%n%s : ", inputCarNameList.get(carNameListIndex));
+            RandomNum = Randoms.pickNumberInRange(0, 9);
+            determineForwardFromRandomNum();
+            printCarNameInGame();
             displayCarProgress();
+            setForwardCountsListFromRandomNum();
         }
         System.out.printf("%n");
     }
 
+    static void determineForwardFromRandomNum() {
+        if (RandomNum >= 4) {
+            forwardCounts = forwardCountsList.get(carNameListIndex) + 1;
+        }
+        if (RandomNum < 4) {
+            forwardCounts = forwardCountsList.get(carNameListIndex);
+        }
+    }
+
+    static void printCarNameInGame() {
+        System.out.printf("%n%s : ", inputCarNameList.get(carNameListIndex));
+    }
+
     static void displayCarProgress() {
-        for (int k = 0; k < forwardCounts; k++) {
+        for (repeatPrintDash = 0; repeatPrintDash < forwardCounts; repeatPrintDash++) {
             System.out.printf("%s", FOR_WARD_MARK);
         }
+    }
+
+    static void setForwardCountsListFromRandomNum() {
         forwardCountsList.set(carNameListIndex, forwardCounts);
         forwardCounts = 0;
     }
@@ -153,12 +164,15 @@ public class Application {
         }
     }
 
+    static String winnerMessage;
+
     static void printWinnerMessage() {
         String winnerListToMessage = String.join(", ", winnerList);
-        String winnerMessage = "최종 우승자 : " + winnerListToMessage + "%n";
+        winnerMessage = "최종 우승자 : " + winnerListToMessage + "%n";
         System.out.printf(winnerMessage);
     }
 }
+
 
 
 
