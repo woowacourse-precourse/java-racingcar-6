95b32551fef73f678bce95a2e8fb489aedd96028
Jiihyun <cwlgusc@gmail.com>
2023-11-01 17:58:01 +0900

    refactor(TryTimesValidatorTest): 반복코드 제거 및 메서드 이름 변경

--- src/test/java/racingcar/validator/TryTimesValidatorTest.java
+++ src/test/java/racingcar/validator/TryTimesValidatorTest.java
@@ -2,62 +2,21 @@ package racingcar.validator;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TryTimesValidatorTest {
-    @Test
-    @DisplayName("시도 횟수 입력 통과 케이스")
-    void testTryTimes() {
-        //given
-        String tryTimes = "5";
-        //when
-        //then
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "3", "5", "10"})
+    @DisplayName("통과 - 올바른 시도 횟수 입력")
+    void right_TryTimes(String tryTimes) {
         TryTimesValidator.validateNumber(tryTimes);
     }
 
-    @Test
-    @DisplayName("0 입력했을 시 실패")
-    void testIfTryTimesIsZero() {
-        //given
-        String tryTimes = "0";
-        //when
-        //then
-        Assertions.assertThatThrownBy(() -> TryTimesValidator.validateNumber(tryTimes))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1이상의 숫자만 입력해주세요.\n");
-    }
-
-    @Test
-    @DisplayName("음수 값 입력했을 시 실패")
-    void testIfTryTimesIsNegative() {
-        //given
-        String tryTimes = "-5";
-        //when
-        //then
-        Assertions.assertThatThrownBy(() -> TryTimesValidator.validateNumber(tryTimes))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1이상의 숫자만 입력해주세요.\n");
-    }
-
-    @Test
-    @DisplayName("숫자 외의 값 입력 시 실패")
-    void testIfTryTimesHasLetter() {
-        //given
-        String tryTimes = "g5";
-        //when
-        //then
-        Assertions.assertThatThrownBy(() -> TryTimesValidator.validateNumber(tryTimes))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1이상의 숫자만 입력해주세요.\n");
-    }
-
-    @Test
-    @DisplayName("공백만 입력했을 시 실패")
-    void testIfTryTimesIsEmpty() {
-        //given
-        String tryTimes = "";
-        //when
-        //then
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "g3", ""})
+    @DisplayName("실패 - 음수, 공백, 숫자외의 값 입력")
+    void input_Others_For_TryTimes(String tryTimes) {
         Assertions.assertThatThrownBy(() -> TryTimesValidator.validateNumber(tryTimes))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1이상의 숫자만 입력해주세요.\n");
