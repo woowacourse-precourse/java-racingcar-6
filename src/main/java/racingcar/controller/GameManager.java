70ab6f520cd2be1afd1ef578469c3f70500299c0
“sangjun121” <juncho12011201@gmail.com>
2023-11-01 12:24:52 +0900

    feat: Racing 클래스에 초기 사용자 입력값 전달

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -6,13 +6,15 @@ public class GameManager {
     private GameSettingsDto gameSettingsDto;
 
     private UserInput userInput;
+    private RacingGame racingGame;
 
     public GameManager(){
         userInput = new UserInput();
+        racingGame = new RacingGame();
     }
 
     public void startGame(){
         gameSettingsDto = userInput.readUserGameSettingInput();
-
+        racingGame.runRaceIterations(gameSettingsDto);
     }
 }
