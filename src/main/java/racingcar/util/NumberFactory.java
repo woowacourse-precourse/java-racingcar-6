b04ea936bd285562c27c8674196da32e994e23b6
minsub <chrkb1569@naver.com>
2023-11-01 14:12:40 +0900

    refactor : NumberFactory 상수 -> 변수화

--- src/main/java/racingcar/util/NumberFactory.java
+++ src/main/java/racingcar/util/NumberFactory.java
@@ -4,8 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 // 0부터 9까지의 임의의 값을 생성하기 위한 클래스
 public class NumberFactory {
+    private final static int MIN_RANGE = 0;
+    private final static int MAX_RANGE = 9;
+
     public static int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
     }
 
     private NumberFactory() {
