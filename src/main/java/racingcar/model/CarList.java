7cc659281fdac0787312a5106f974013741ed58f
DonghaOh <donghadarwin@naver.com>
2023-11-01 17:01:06 +0900

    feat: 자동차 리스트 각 자동차 전진 - carListMove 메소드

--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -3,10 +3,15 @@ package racingcar.model;
 import java.util.List;
 
 public class CarList {
-    List<Car> carList;
-    CarList(List<String> nameList){
+    public static List<Car> carList;
+    public CarList(List<String> nameList){
         for (String name : nameList) {
             carList.add(new Car(name));
+        }
+    }
+    public static void carListMove() {
+        for (Car car : carList) {
+            car.randomMove();
         }
     }
 
