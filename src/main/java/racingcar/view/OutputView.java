cef0a289cab45b11c8b57387bc8721f8c50b0dcd
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 18:14:44 +0900

    feat: 차수별 경기 결과 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,12 @@
 package racingcar.view;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import racingcar.model.RacingCar;
+
 public class OutputView {
 
     private static OutputView outputView;
@@ -24,5 +31,23 @@ public class OutputView {
 
     public void printExecResult() {
         System.out.println("실행 결과");
+    }
+
+    public void printRacingResultEachCount(List<RacingCar> racingCarList) {
+        Map<String, Integer> namePositionMap = racingCarList.stream()
+                .collect(Collectors.toMap(RacingCar::getCarName, RacingCar::getCurrentPosition,
+                        (oldPosition, newPosition) -> newPosition, LinkedHashMap::new));
+
+        for (String carName : namePositionMap.keySet()) {
+            int currentPosition = namePositionMap.get(carName);
+            printCarCurrentPosition(carName, currentPosition);
+        }
+    }
+
+    private void printCarCurrentPosition(String carName, int currentPosition) {
+        System.out.print(carName + " : ");
+        IntStream.rangeClosed(1, currentPosition)
+                .forEach(count -> System.out.print("-"));
+        System.out.print("\n");
     }
 }
