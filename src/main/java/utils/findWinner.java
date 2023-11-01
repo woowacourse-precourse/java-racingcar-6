30d7a33c4753a70e417241d8111a61b537ab5dc2
minnjung0887921 <minjeong0887921@gmail.com>
2023-11-01 17:06:21 +0900

    feat : 최종 우승자 가리기

---
+++ src/main/java/utils/findWinner.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.util.ArrayList;
+
+public class findWinner {
+    public static ArrayList<String> getMostMovedCar(ArrayList<String> carDistanceList) {
+        String longestDistance = "";
+        ArrayList<String> mostMovedCar = new ArrayList<>();
+
+        for (String carInfo : carDistanceList) {
+            char separator = ':';
+
+            int separatorIndex = carInfo.indexOf(separator);
+            String carNameString = carInfo.substring(0, separatorIndex - 1);
+            String distanceString = carInfo.substring(separatorIndex + 2, carInfo.length());
+
+            if (longestDistance.length() < distanceString.length()) {
+                mostMovedCar.clear();
+            }
+            if (longestDistance.length() <= distanceString.length()) {
+                longestDistance = distanceString;
+                mostMovedCar.add(carNameString);
+            }
+        }
+        return mostMovedCar;
+    }
+
+    public static ArrayList<String> getRaceResultList(ArrayList<String> carDistanceList) {
+        ArrayList<String> mostMovedCarName = getMostMovedCar(carDistanceList);
+
+        String winnerName= "";
+        for (String carName : mostMovedCarName) {
+            if (!winnerName.isEmpty()) {
+                winnerName = winnerName.concat(", " + carName);
+            } else {
+                winnerName = winnerName.concat(carName);
+            }
+        }
+        carDistanceList.add("최종 우승자 : " + winnerName);
+        return carDistanceList;
+    }
+}
