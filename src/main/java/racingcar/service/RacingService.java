4a434f59bd8771c0049a92e9c9c4eb461b3a8e57
kwonjiyun <ziyun1612@ewhain.net>
2023-11-01 16:25:52 +0900

    refactor : random 숫자 발생 클래스 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,7 @@
 package racingcar.service;
 
+import static racingcar.utils.NumberGernator.pickNumber;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.RaceParticipants;
@@ -7,15 +9,8 @@ import racingcar.domain.Round;
 import racingcar.utils.PrintUtils;
 
 public class RacingService {
-    public static final int NUM_RANGE = 10;
-
-
     public void progress(RaceParticipants raceParticipants) {
         raceParticipants.getParticipantCars().forEach(car -> car.goForwardOrStop(pickNumber()));
-    }
-
-    public static int pickNumber(){
-        return Randoms.pickNumberInRange(0,9);
     }
 
     public void startRound(RaceParticipants raceParticipants, Round round) {
