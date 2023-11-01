79bffb59defc33b286b99dfc2d06c9ad64d8ed86
leeminjun <sohwi0305@gmail.com>
2023-11-01 16:07:03 +0900

    feat: 1글자 이상이여야 합니다 추가

--- src/main/java/racingcar/util/Exception.java
+++ src/main/java/racingcar/util/Exception.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Exception {
     private static final int CAR_NAME_LIMIT_MAX = 5;
+    private static final int CAR_NAME_LIMIT_MIN = 1;
     private static final String SPACE = " ";
     private final Parser parser = new Parser();
     public void checkCarNamesInput(String input) throws IllegalArgumentException {
@@ -53,9 +54,16 @@ public class Exception {
         }
     }
 
+    private void throwMinLengthException() {
+        throw new IllegalArgumentException("자동차 이름은 " + CAR_NAME_LIMIT_MIN + "글자 이상이어야 합니다.");
+    }
+
     private void checkLengthAndThrowException(String carName) {
         if (CAR_NAME_LIMIT_MAX < carName.length()) {
             throwMaxLengthException();
+        }
+        if (carName.length() < CAR_NAME_LIMIT_MIN) {
+            throwMinLengthException();
         }
     }
 
