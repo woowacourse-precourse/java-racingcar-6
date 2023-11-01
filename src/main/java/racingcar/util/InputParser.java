cbdb0843be8876b7e2bd360505cff12c5099779b
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-01 09:08:30 +0900

    refactor(InputParser) : 기능을 더욱 세세하게 분류

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -2,28 +2,33 @@ package racingcar.util;
 
 import static racingcar.consts.ViewConstant.DELIMITER;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class InputParser {
     InputValidator inputValidator = new InputValidator();
 
     public List<Car> parseCarName(String playerInput) {
-        playerInput = playerInput.replaceAll(" ", "");
+        playerInput = removeSpaces(playerInput);
         inputValidator.validateCarNames(playerInput);
-        List<Car> result = new ArrayList<>();
-        List<String> carNames = Arrays.stream(playerInput.split(DELIMITER.get())).toList();
-        for (String carName : carNames) {
-            result.add(new Car(carName));
-        }
-        return result;
+        return createCarList(playerInput);
     }
 
     public int parseNumberOfAttempt(String playerInput) {
-        playerInput = playerInput.replaceAll(" ", "");
+        playerInput = removeSpaces(playerInput);
         inputValidator.validateNumberOfAttempt(playerInput);
         return Integer.parseInt(playerInput);
+    }
+
+    private List<Car> createCarList(String carNames) {
+        return Arrays.stream(carNames.split(DELIMITER.get()))
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private String removeSpaces(String playerInput) {
+        return playerInput.replaceAll(" ", "");
     }
 }
