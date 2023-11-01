34968d4ecac237dede241fa30ab6298ada44b33c
pjm2476 <pjm2476@gmail.com>
2023-11-01 17:41:16 +0900

    fix: 입력 유효성 검사 기능 예외 처리

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -3,11 +3,12 @@ package racingcar.validator;
 import java.util.List;
 
 public class InputValidator {
-    public static void isValidCarName(String carName) {
+    public static boolean isValidCarName(String carName) {
 
         if (checkEmptyOrNull(carName) || !checkCarNameLenghth(carName)) {
             throw new IllegalArgumentException();
         }
+        return true;
     }
 
     private static boolean checkEmptyOrNull(String carName) {
@@ -24,7 +25,7 @@ public class InputValidator {
         return true;
     }
 
-    public static void isValidAttempts(String attempts) {
+    public static boolean isValidAttempts(String attempts) {
         try {
             int numberAttempts = Integer.parseInt(attempts);
             if (numberAttempts <= 0) {
@@ -33,5 +34,6 @@ public class InputValidator {
         } catch (NumberFormatException ex) {
             throw new IllegalArgumentException();
         }
+        return true;
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,20 +1,33 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.validator.InputValidator;
 
 public class InputView {
     public static List<String> inputCarNames() {
-        String names = Console.readLine();
-        InputValidator.isValidCarName(names);
-        List<String> namesList = List.of(names.split(","));
+
+        List<String> namesList = new ArrayList<String>();
+        try {
+            String names = Console.readLine();
+            InputValidator.isValidCarName(names);
+            namesList = List.of(names.split(","));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid input");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid input");
+        }
         return namesList;
     }
 
     public static int inputAttempts() {
-        String input = Console.readLine();
-        InputValidator.isValidAttempts(input);
-        return Integer.parseInt(input);
+        try {
+            String attempts = Console.readLine();
+            InputValidator.isValidAttempts(attempts);
+            return Integer.parseInt(attempts);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid input");
+        }
     }
 }
