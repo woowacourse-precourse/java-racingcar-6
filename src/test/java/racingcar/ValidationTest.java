3487f7b85d7652b7bd1148d8c97b9d61ca8e0a39
Yerinnnn <shinyerin0216@gmail.com>
2023-11-01 19:03:10 +0900

    Test: "자동차 이름 검증 테스트 코드"
    -같은_이름이_여러개일_경우_예외_처리()
    -이름이_1개_이하일_경우_예외_처리()

--- src/test/java/racingcar/ValidationTest.java
+++ src/test/java/racingcar/ValidationTest.java
@@ -12,26 +12,17 @@ public class ValidationTest extends NsTest {
     
     @Test
     void 시도횟수_입력시_공백을_입력하면_예외_처리() {
-        String input = " ";
-        assertThatThrownBy(() -> {
-            Validator.attemptCount(input);
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", " ")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 시도횟수_입력시_문자를_입력하면_예외_처리() {
-        String input = "a";
-        assertThatThrownBy(() -> {
-            Validator.attemptCount(input);
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", "a")).isInstanceOf(IllegalArgumentException.class);
     }
     
     @Test
     void 시도횟수_입력시_음수를_입력하면_예외_처리() {
-        String input = "-1";
-        assertThatThrownBy(() -> {
-            Validator.attemptCount(input);
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", "-1")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -40,6 +31,16 @@ public class ValidationTest extends NsTest {
         assertThatThrownBy(() -> {
             Validator.attemptCount(input);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 같은_이름이_여러개일_경우_예외_처리() {
+        assertThatThrownBy(() -> runException("pobi,pobi", "1")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_1개_이하일_경우_예외_처리() {
+        assertThatThrownBy(() -> runException("pobi", "1")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
