c468ab50236674439f89534fb53ad9e5a0c2fcbb
Jmmk <jmk525@gmail.com>
2023-11-01 13:54:24 +0900

    feat : CLI로 출력하는 기능 구현

---
+++ src/main/java/racingcar/io/CommandLineWriter.java
@@ -0,0 +1,14 @@
+package racingcar.io;
+
+public class CommandLineWriter {
+
+    public void write(String message) {
+        System.out.print(message);
+    }
+    public void writeLine(String message) {
+        System.out.println(message);
+    }
+    public void writeSpaceLine() {
+        System.out.println();
+    }
+}
