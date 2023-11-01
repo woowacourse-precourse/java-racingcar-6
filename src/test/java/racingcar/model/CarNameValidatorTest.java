4a7fd32f29b5c85b8213c1509c26e25d3d770dc9
hoyeon <highoption@naver.com>
2023-11-01 13:31:17 +0900

    test(CarNameValidatorTest) : 5글자 이상 이름은 예외 발생 테스트 생성

--- src/test/java/racingcar/model/CarNameValidatorTest.java
+++ src/test/java/racingcar/model/CarNameValidatorTest.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -15,4 +17,15 @@ public class CarNameValidatorTest {
 
         CarNameValidator.validateCarNames(carList);
     }
+
+    @Test
+    void 최대_이름_길이가_5글자를_넘어가면_예외발생() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(Car.makeCarByName("Car1"));
+        carList.add(Car.makeCarByName("Car123456"));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> CarNameValidator.validateCarNames(carList));
+    }
+
 }
