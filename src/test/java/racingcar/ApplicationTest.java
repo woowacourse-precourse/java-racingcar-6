d04e476ca71e7f092b4d9f69d59d6541c4bf2bca
realisshomyang <realisshoman@kakao.com>
2023-11-01 22:12:03 +0900

    test : 전체 애플리케이션 실행 시 우승자 판별 기능 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,17 +1,17 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    @DisplayName("전진이 잘 되는지, 정지가 잘 되는지 확인")
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
@@ -22,35 +22,31 @@ class ApplicationTest extends NsTest {
             MOVING_FORWARD, STOP
         );
     }
+
+    @DisplayName("중복 우승자 출력이 잘 되는지")
     @Test
-    void 숫자입력에_음수_대한_예외_처리(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "-1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void 중복_우승자() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
         );
     }
-
+    @DisplayName("여러번 전진 및 움직임")
     @Test
-    void 숫자입력_정수아닌값에_대한_예외_처리_(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "S"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 중복이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void 여러번_전진_정지_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "5");
+                    assertThat(output()).contains("pobi : ----", "woni : ---", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD,
+                STOP, MOVING_FORWARD,
+                MOVING_FORWARD, STOP,
+                MOVING_FORWARD, STOP
         );
     }
 
