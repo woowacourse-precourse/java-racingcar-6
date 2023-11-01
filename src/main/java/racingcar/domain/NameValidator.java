d295d1fac5a83e2fa585d49672350f12079dc229
dj <fjzl12@naver.com>
2023-11-01 19:25:38 +0900

    refactor(NameValidator): 정적 메서드 - 인스턴스 메서드로 수정

--- src/main/java/racingcar/domain/NameValidator.java
+++ src/main/java/racingcar/domain/NameValidator.java
@@ -7,7 +7,7 @@ public class NameValidator {
     public NameValidator() {
     }
 
-    public static List<String> validate(String names) {
+    public List<String> validate(String names) {
         List<String> nameList = splitComma(names);
         correctNumberOfChar(nameList);
         hasOnlyAlphabets(nameList);
@@ -16,7 +16,7 @@ public class NameValidator {
         return nameList;
     }
 
-    public static List<String> splitComma(String names) {
+    private List<String> splitComma(String names) {
         List<String> nameList = new ArrayList<>();
         for (String name : names.split(",")) {
             if (name.isEmpty()) {
@@ -30,7 +30,7 @@ public class NameValidator {
         return nameList;
     }
 
-    public static void correctNumberOfChar(List<String> carNameList) {
+    private void correctNumberOfChar(List<String> carNameList) {
         for (String name : carNameList) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("글자 수를 초과 입력하여 게임을 종료합니다. (자동차 이름 글자 수는 5자 이하 까지 가능합니다.)");
@@ -38,7 +38,7 @@ public class NameValidator {
         }
     }
 
-    public static void hasOnlyAlphabets(List<String> carNameList) {
+    private void hasOnlyAlphabets(List<String> carNameList) {
         for (String name : carNameList) {
             if (!name.matches("^[a-zA-Z]*$")) {
                 throw new IllegalArgumentException("영문자 외의 값을 입력하여 게임을 종료합니다.");
@@ -46,13 +46,13 @@ public class NameValidator {
         }
     }
 
-    public static void hasUniqueName(List<String> carNameList) {
+    private void hasUniqueName(List<String> carNameList) {
         if (carNameList.size() != carNameList.stream().distinct().count()) {
             throw new IllegalArgumentException("중복된 값을 입력하여 게임을 종료합니다.");
         }
     }
 
-    public static void isUnderTen(List<String> carNameList) {
+    private void isUnderTen(List<String> carNameList) {
         if (carNameList.size() > 10) {
             throw new IllegalArgumentException("자동차의 수가 10대를 초과하여 게임을 종료합니다.");
         }
