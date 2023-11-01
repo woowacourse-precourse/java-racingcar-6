5adbe52e52f8c3f711438920b22fc9a1e7fd4891
lee-JunR <wns2610@icloud.com>
2023-11-01 17:22:10 +0900

    refactor : "사용자 입력 검 클래스 구현" 입력과 검증의 분리: readInput 메소드를 도입하여 사용자 입력과 유효성 검사를 분리 출력 위치 변경: 입력 값 출력을 readInput 메소드 내부로 이동하여, 유효성 검사 이후에 입력값을 출력하도록 변경

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -2,25 +2,25 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.List;
 import racingcar.utils.ValidationUtils;
 
 public class Input {
 
   public List<String> inputCarNames() {
-    System.out.println("자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-    String inputNames = Console.readLine();
+    String inputNames = readInput("자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
     ValidationUtils.validateCarNames(inputNames);
-    System.out.println(inputNames);
     return Arrays.asList(inputNames.split(","));
   }
 
   public int inputTryCount() {
-    System.out.println("시도할 회수를 입력하세요.");
-    String inputNumber = Console.readLine();
+    String inputNumber = readInput("시도할 회수를 입력하세요.");
     ValidationUtils.validateUserInput(inputNumber);
-    System.out.println(inputNumber);
     return Integer.parseInt(inputNumber);
+  }
+
+  private String readInput(String message) {
+    System.out.println(message);
+    return Console.readLine();
   }
 }
