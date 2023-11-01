44b2f0d9c783567b31179e6c875ac0f80274ec0c
jongmee <ra123221@gmail.com>
2023-11-01 15:19:56 +0900

    test: 양옆 공백을 제거하고 경주차 이름을 파싱하는 기능 테스트하기

--- src/test/java/racingcar/dto/request/CarNamesDtoTest.java
+++ src/test/java/racingcar/dto/request/CarNamesDtoTest.java
@@ -1,10 +1,17 @@
 package racingcar.dto.request;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static racingcar.constants.Condition.NAME_SIZE;
 import static racingcar.constants.ErrorMessage.LENGTH_LIMIT;
+import static racingcar.constants.Format.WINNER_DELIMITER;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CarNamesDtoTest {
     @Test
@@ -17,5 +24,21 @@ class CarNamesDtoTest {
             new CarNamesDto(request);
         });
         assertEquals(LENGTH_LIMIT + NAME_SIZE, exception.getMessage());
+    }
+    @ParameterizedTest
+    @MethodSource("경주차이름_설정")
+    void 공백을_제거하고_경주차이름_입력받기(String request, List<String> parsedNames) {
+        // given & when
+        CarNamesDto carNamesDto = new CarNamesDto(request);
+
+        // then
+        assertThat(carNamesDto.getNames()).containsAll(parsedNames);
+    }
+
+    private static Stream<Arguments> 경주차이름_설정() {
+        return Stream.of(
+            Arguments.arguments("zzol, jong, meoru", List.of("zzol", "jong", "meoru")),
+            Arguments.arguments("  zzol,     jong ,  meoru ", List.of("zzol", "jong", "meoru"))
+        );
     }
 }
