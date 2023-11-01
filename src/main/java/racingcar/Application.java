390a7fd67f6e7050782769b107a3a55acc310250
ch1hyun <och2510@gmail.com>
2023-11-01 17:12:48 +0900

    feat : Add a game engine object to main
    - Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,10 @@
 package racingcar;
 
+import racingcar.controller.GameEngine;
+
 public class Application {
     public static void main(String[] args) {
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.proceedGame();
     }
 }
