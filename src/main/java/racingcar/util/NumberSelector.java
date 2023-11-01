92dfb966fd8f4d3b28676f4cac202e385e824847
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-01 16:34:58 +0900

    Feat : util - NumberSelector class 구현

--- src/main/java/racingcar/util/NumberSelector.java
+++ src/main/java/racingcar/util/NumberSelector.java
@@ -1,4 +1,9 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberSelector {
+    public int pickRandomNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
 }
