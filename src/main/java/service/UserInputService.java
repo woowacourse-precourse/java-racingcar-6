fe435ae92f9dff8812de97b18d7c280ae31c32b7
Dowoon <woodw920@gmail.com>
2023-11-01 14:06:47 +0900

    - 자동차 입력받는 메소드에 유효성 검사 코드 추가 - 게임회차 입력받는 메소드 추가 및 유효성 검사 코드 추가

--- src/main/java/service/UserInputService.java
+++ src/main/java/service/UserInputService.java
@@ -2,11 +2,22 @@ package service;
 
 import camp.nextstep.edu.missionutils.Console;
 import utils.TransformUtil;
+import utils.validation.CarValidation;
+import utils.validation.UserInputValidation;
 
 import java.util.List;
 
 public class UserInputService {
     public List<String> carName() {
-        return TransformUtil.commasToList(Console.readLine());
+        List<String> carNames
+                = TransformUtil.commasToList(Console.readLine());
+
+        CarValidation.carName(carNames);
+
+        return carNames;
+    }
+
+    public Integer roundCnt(String round) {
+        return UserInputValidation.checkNumber(round);
     }
 }
