cfc8ca4816ade58d07389fb490f1a44ea8d057fe
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 14:02:22 +0900

    feat(Application) : 메인 메서드 작성
    기능1. ~ 기능8. 까지를 활용하여 메인 메서드를 작성하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,39 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import racingcar.domain.Car;
+import racingcar.domain.ResultGenerator;
+import racingcar.domain.UserInputParser;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String userInput = "";
+        UserInputParser userInputParser = new UserInputParser();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        userInput = readLine();
+
+        List<Car> carList = userInputParser.generateCarList(userInput);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        userInput = readLine();
+
+        int totalRound = userInputParser.parseInt(userInput);
+
+        ResultGenerator resultGenerator = new ResultGenerator();
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for (int round = 0; round < totalRound; round++) {
+            resultGenerator.playRound(carList);
+            System.out.println();
+        }
+
+        List<String> winnerList = resultGenerator.generateWinnerList(carList);
+        resultGenerator.printFinalResult(winnerList);
     }
 }
