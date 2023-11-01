89d37b65dd0b69f186dd634419d0722aae9a488e
Jedo0224 <qkrwotjr0@gmail.com>
2023-11-01 19:30:56 +0900

    [refactor] 인텔리제이에서는 빌드가 되는데 지원플랫폼에서는 빌드가 안됨. 수정 1

--- src/main/java/test/racingcar/ApplicationTest.java
+++ src/main/java/test/racingcar/ApplicationTest.java
@@ -1,7 +1,8 @@
-package racingcar;
+package test.racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
