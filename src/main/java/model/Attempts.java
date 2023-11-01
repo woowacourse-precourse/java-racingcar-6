88f62a497729df15bdc8915521e7787f6eea3571
kimwanyoung <dhks2869@gmail.com>
2023-11-01 14:28:26 +0900

    feat : Attempts클래스 값 음수 검증 메서드 추가

--- src/main/java/model/Attempts.java
+++ src/main/java/model/Attempts.java
@@ -5,11 +5,13 @@ public class Attempts {
     private final int attempts;
     private static final String EMPTY_MESSAGE = "공백 혹은 빈 문자열은 입력 할 수 없습니다.";
     private static final String ONLY_NUMBER_INPUT_MESSAGE = "숫자만 입력 가능합니다.";
+    private static final String ONLY_POSITIVE_NUMBER_MESSAGE = "양수만 입력 가능합니다.";
 
     public Attempts(String attempts) {
         validateEmpty(attempts);
         validateIsNumericInput(attempts);
         this.attempts = Integer.parseInt(attempts);
+        validateNegative();
     }
 
     public int getAttempts() {
@@ -31,6 +33,12 @@ public class Attempts {
             Integer.parseInt(attempts);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ONLY_NUMBER_INPUT_MESSAGE);
+        }
+    }
+
+    private void validateNegative() {
+        if (this.attempts < 0) {
+            throw new IllegalArgumentException(ONLY_POSITIVE_NUMBER_MESSAGE);
         }
     }
 }
