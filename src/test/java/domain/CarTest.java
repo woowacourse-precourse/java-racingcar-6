0f433259ecf01d815139d52a32302bce85316e2e
PARK9898 <wjddjs21@naver.com>
2023-11-01 19:04:56 +0900

    "refactor(test): 생성자 매개변수 변경에 따른 test 코드 수정"

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -9,24 +9,23 @@ import racingcar.domain.Car;
 public class CarTest {
     @Test
     void 자동차_이름이_다섯_글자가_넘으면_예외를_던진다() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car("abcdef",0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car("abcdef"));
     }
 
     @Test
     void 주어진_숫가자_4이상이면_전진한다() {
-        Car car1 = new Car("박정언",0);
-        Car car2 = new Car("홍길동",1);
+        Car car1 = new Car("박정언");
+        Car car2 = new Car("홍길동");
 
         car1.moveOrStop(4);
 
-        assertThat(car1.isSamePosition(car2)).isTrue();
+        assertThat(car1.isSamePosition(car2)).isFalse();
     }
 
     @Test
     void 주어진_숫자가_4미만이면_전진하지_않는다() {
-        Car car1 = new Car("박정언",0);
-        Car car2 = new Car("홍길동",0);
-
+        Car car1 = new Car("박정언");
+        Car car2 = new Car("홍길동");
         car1.moveOrStop(3);
 
         assertThat(car1.isSamePosition(car2)).isTrue();
@@ -34,8 +33,11 @@ public class CarTest {
 
     @Test
     void 같은_포지션의_자동차를_찿는다() {
-        Car car1 = new Car("박정언",1);
-        Car car2 = new Car("홍길동",1);
+        Car car1 = new Car("박정언");
+        Car car2 = new Car("홍길동");
+
+        car1.moveOrStop(4);
+        car2.moveOrStop(4);
 
         assertThat(car1.isSamePosition(car2)).isTrue();
     }
