efd27fec2dc6168e9497b1bfb460862db1dd4f39
hojemoon <147702295+hojemoon@users.noreply.github.com>
2023-11-01 20:21:09 +0900

    컨벤션 수정

--- src/main/java/racingcar/raceWinner.java
+++ src/main/java/racingcar/raceWinner.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class raceWinner {
 
-    public static List<String> getwinner(List<Car> cars) {
-        List<String> winner = new ArrayList<>();
-        int maxPosition = cars.stream()
-                .mapToInt(Car::carPosition)
-                .max()
-                .orElse(0);
+        public static List<String> getwinner(List<Car> cars) {
+                List<String> winner = new ArrayList<>();
+                int maxPosition = cars.stream()
+                        .mapToInt(Car::carPosition)
+                        .max()
+                        .orElse(0);
 
-        for (Car car : cars) {
-            if (car.carPosition() == maxPosition) {
-                winner.add(car.carNamePrint());
-            }
+                for (Car car : cars) {
+                        if (car.carPosition() == maxPosition) {
+                                winner.add(car.carNamePrint());
+                        }        
+                }
+
+                return winner;
         }
-
-        return winner;
-    }
 }
