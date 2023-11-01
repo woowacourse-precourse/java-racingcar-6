63e71d9ad75edddad6c41ff8212bc6e3ee5c9b36
JDUB9823 <jeong98231@gmail.com>
2023-11-01 14:31:12 +0900

    test: 시도횟수 입력 예외처리 테스트

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -3,13 +3,17 @@ package racingcar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.controller.InputController;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
+import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.List;
 
-class CarTest {
+class InputTest {
     @Test
     @DisplayName("자동차 이름 입력 길이 5초과 테스트")
     public void carNameLengthOver5Test() {
@@ -37,5 +41,21 @@ class CarTest {
         //when
         //then
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Cars(carNames));
+    }
+
+    @ParameterizedTest
+    @DisplayName("시도 횟수 숫자 아닌 입력 처리 테스트")
+    @ValueSource(strings = {"test"})
+    public void getAttemptCountFromStringTest(String incorrectAttemptCountInput) {
+        //given
+        InputController inputController = new InputController();
+        ByteArrayInputStream testIn = new ByteArrayInputStream(incorrectAttemptCountInput.getBytes());
+
+        //when
+        System.setIn(testIn);
+
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                inputController::getAttemptCountFromUserInput);
     }
 }
