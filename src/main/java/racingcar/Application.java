8443d583b3dce3c2995bf511cf78a492aa52c0f5
jh9788 <gkwhdgns2@gmail.com>
2023-11-01 22:40:37 +0900

    Feat: isForward 함수 구현
    전진할지 말지 여부 반환 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,14 @@ public class Application {
     private static int getRandomNumber(){
         return Randoms.pickNumberInRange(0, 9);
     }
+
+    private static boolean isForward(){
+        int number = getRandomNumber();
+        if(number>=4)
+            return true;
+        else
+            return false;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
