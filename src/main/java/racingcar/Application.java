f6b1c8d384775f54b1ed2bcebee153e43dfc7ca5
donghoonyeom <dua5410773@naver.com>
2023-11-01 16:19:10 +0900

    refactor(Application): ConsoleView에 ConsoleWrapper를 사용하여 입력 처리 로직 변경
    ConsoleView 클래스에서 사용자 입력을 받는 부분을 개선하기 위해 ConsoleWrapper를 도입했습니다. 이로써 코드는 더 모듈화되고 유연해졌으며, 사용자 입력 관련 로직이 더 잘 분리되었습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,16 +4,19 @@ import racingcar.controller.GameController;
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
 import racingcar.view.ConsoleView;
+import racingcar.util.ConsoleWrapper;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleView consoleView = new ConsoleView();
+        ConsoleWrapper consoleWrapper = new ConsoleWrapper();
+        ConsoleView consoleView = new ConsoleView(consoleWrapper);
+
         List<String> carNames = consoleView.getInputCarNames();
         List<Car> cars = Car.createCars(carNames);
+
         int numberOfAttempts = consoleView.readValidNumberOfAttempts();
-        consoleView.printMessage("");
 
         RacingGame racingGame = new RacingGame(cars);
         GameController gameController = new GameController(racingGame, consoleView);
