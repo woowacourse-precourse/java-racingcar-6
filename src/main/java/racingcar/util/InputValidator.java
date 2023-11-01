49f9b7f5325c1ea29ece726b74d57d65611dc52f
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-01 09:11:32 +0900

    refactor(InputValidator) : 메소드를 기능별로 더 세세하게 분류

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -11,49 +11,58 @@ import static racingcar.consts.ViewConstant.DELIMITER;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.stream.Collectors;
 
 public class InputValidator {
+    private static final Pattern numericPattern = Pattern.compile("^[1-9]\\d*$");
 
     public void validateCarNames(String playerInput) {
-        List<String> carNames = Arrays.stream(playerInput.split(DELIMITER.get())).toList();
-        if (!isCarNameSizeOneBetweenFive(carNames)) {
-            throwIllegalArgumentException(WRONG_CAR_NAME_LENGTH_MESSAGE.get());
-        } else if (hasDuplicateCarName(carNames)) {
-            throwIllegalArgumentException(DUPLICATION_CAR_NAME_MESSAGE.get());
-        } else if (!isMoreThanTwoCarTypes(carNames)) {
-            throwIllegalArgumentException(WRONG_CAR_TYPES_MESSAGE.get());
-        }
-    }
-
-    private boolean isMoreThanTwoCarTypes(List<String> carNames) {
-        return carNames.size() >= MIN_NUMBER_TYPES_CAR.get();
-    }
-
-    private boolean hasDuplicateCarName(List<String> carNames) {
-        return carNames.size() != carNames.stream().distinct().count();
-    }
-
-    private boolean isCarNameSizeOneBetweenFive(List<String> carNames) {
-        return carNames.stream().allMatch(carName -> carName.length() >= MIN_LENGTH_CAR_NAME.get()
-                && carName.length() <= MAX_LENGTH_CAR_NAME.get());
+        List<String> carNames = splitInput(playerInput);
+        validateCarNameLength(carNames);
+        validateDuplicateCarNames(carNames);
+        validateCarTypesCount(carNames);
     }
 
     public void validateNumberOfAttempt(String playerInput) {
         if (!isNumeric(playerInput)) {
-            throwIllegalArgumentException(WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
+            throw new IllegalArgumentException(WRONG_NUMBER_OF_ATTEMPT_MESSAGE.get());
+        }
+    }
+
+    private List<String> splitInput(String playerInput) {
+        return Arrays.stream(playerInput.split(DELIMITER.get()))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private void validateCarNameLength(List<String> carNames) {
+        if (!carNames.stream().allMatch(this::isCarNameLengthValid)) {
+            throwIllegalArgumentException(WRONG_CAR_NAME_LENGTH_MESSAGE.get());
+        }
+    }
+
+    private boolean isCarNameLengthValid(String carName) {
+        int length = carName.length();
+        return length >= MIN_LENGTH_CAR_NAME.get() && length <= MAX_LENGTH_CAR_NAME.get();
+    }
+
+    private void validateDuplicateCarNames(List<String> carNames) {
+        if (carNames.size() != carNames.stream().distinct().count()) {
+            throwIllegalArgumentException(DUPLICATION_CAR_NAME_MESSAGE.get());
+        }
+    }
+
+    private void validateCarTypesCount(List<String> carNames) {
+        if (carNames.size() < MIN_NUMBER_TYPES_CAR.get()) {
+            throwIllegalArgumentException(WRONG_CAR_TYPES_MESSAGE.get());
         }
     }
 
     private boolean isNumeric(String playerInput) {
-        String regex = "^[1-9]\\d*$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(playerInput);
-        return matcher.matches();
+        return numericPattern.matcher(playerInput).matches();
     }
-    
+
     private void throwIllegalArgumentException(String errorMessage) {
         throw new IllegalArgumentException(errorMessage);
     }
