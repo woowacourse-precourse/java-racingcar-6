8fd5055bb80901878b3878d22ee5a50309b198e0
AidenRoh <colohrby@gmail.com>
2023-11-01 10:25:00 +0900

    feat: add function printing winner

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,13 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class OutputView {
+    private static final String DELIMITER = ", ";
+    private static final String PREFIX = IOMessage.FINAL_WINNER.getMessage();
+    private static final String SUFFIX = "";
+
     public static void printMessage(IOMessage ioMessage) {
         System.out.println(ioMessage.getMessage());
     }
@@ -11,5 +16,11 @@ public class OutputView {
         StringBuilder playResult = new StringBuilder();
         executionList.forEach(playResult::append);
         System.out.println(playResult);
+    }
+
+    public static void printWinner(List<String> winnerList) {
+        StringJoiner winner = new StringJoiner(DELIMITER, PREFIX, SUFFIX);
+        winnerList.forEach(winner::add);
+        System.out.println(winner);
     }
 }
