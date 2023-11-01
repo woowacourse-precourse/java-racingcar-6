e0c9d80bb56901590f7f66559bb98d7c0d296121
kmss6905 <kmss69052@naver.com>
2023-11-01 22:42:13 +0900

    Test: InputView, RacingCars 테스트 추가

---
+++ src/test/java/racingcar/IntputViewTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("입력 뷰 테스트")
+class IntputViewTest {
+
+    @Test
+    @DisplayName("출전할 레이싱 이름을 입력받아 RacingCars 로 반환한다. ")
+    void inputNameOfRacingCars() {
+        String userInput = "Car1,Car2,Car3";
+        InputView inputView = new InputView();
+        RacingCars racingCars = inputView.inputNameOfRacingCars(userInput);
+
+        assertThat(racingCars.getNames()).contains("Car2", "Car1", "Car3");
+        assertThat(racingCars.getNames()).containsExactly("Car1", "Car2", "Car3");
+    }
+}
