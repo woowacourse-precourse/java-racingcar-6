4b0f6ae3103ba0811bc0538b83bcb4ecda627186
Sodychoe <sodychoe@icloud.com>
2023-11-01 21:39:25 +0900

    fix : 메소드를 static 로 변경

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -6,19 +6,19 @@ public class InputView {
     public static final String START_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ROUND_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    public void printStartGuideMessage() {
+    public static void printStartGuideMessage() {
         System.out.println(START_GUIDE_MESSAGE);
     }
 
-    public void printRoundGuideMessage() {
+    public static void printRoundGuideMessage() {
         System.out.println(ROUND_GUIDE_MESSAGE);
     }
 
-    public String getCarNames() {
+    public static String getCarNames() {
         return Console.readLine();
     }
 
-    public String getNumRound() {
+    public static String getNumRound() {
         return Console.readLine();
     }
 }
