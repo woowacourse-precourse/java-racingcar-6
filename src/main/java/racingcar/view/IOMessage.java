20388821cdb8c2da979399613edc108e77b13fc8
AidenRoh <colohrby@gmail.com>
2023-11-01 10:29:19 +0900

    refactor: add break line in execution result

--- src/main/java/racingcar/view/IOMessage.java
+++ src/main/java/racingcar/view/IOMessage.java
@@ -1,9 +1,9 @@
-package racingcar.io;
+package racingcar.view;
 
 public enum IOMessage {
     REGISTER_PLAYER("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     ATTEMPT_TIME("시도할 회수는 몇회인가요?"),
-    EXECUTION_RESULT("실행 결과"),
+    EXECUTION_RESULT("\n실행 결과"),
     FINAL_WINNER("최종 우승자 : ");
 
     private final String message;
