58d116076eebb310ef3b73b685e9b05d3ea7eb84
saera <yook.saera@gmail.com>
2023-11-01 15:02:21 +0900

    feat(Application): 게임을 1라운드만 진행하라

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("이름 입력");
+        String input = Console.readLine();
+        Registration registration = new Registration();
+        List<Car> carList = registration.createCarList(registration.getCarNames(input));
+
+        NumberGenerator numberGenerator = new NumberGenerator();
+        MessageContainer messageContainer = new MessageContainer();
+
+        System.out.println("\n실행 결과");
+        for (Car car : carList) {
+            car.moveForward(numberGenerator.pickNumberFrom0To9());
+        }
+        for (Car car : carList) {
+            System.out.println(messageContainer.showCarResult(car));
+        }
+        System.out.println();
+
+        Judgment judgment = new Judgment();
+        List<String> winnerNames = judgment.getWinnerNames(carList, judgment.findMaxPosition(carList));
+        System.out.println(messageContainer.showWinners(winnerNames));
     }
 }
