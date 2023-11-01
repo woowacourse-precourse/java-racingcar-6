7418730b7120e478e208dffe87e12fc3a39221bf
cheolhyeon <tghy4@naver.com>
2023-11-01 16:36:40 +0900

    refactor : 외부로부터 carNames를 받지 않고 내부에서 user 객체를 이용해서 메서드 작성 및 더 간단하게 코드 구조 개선

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,25 +1,24 @@
 package racingcar.validation;
 
-
-import java.lang.invoke.StringConcatFactory;
-import racingcar.User;
+import racingcar.user.User;
 
 public class Validator {
+    private User user;
 
-    public void checkCarNameCount(String[] carNames) {
-        if (carNames.length <= 1) {
-            throw new IllegalArgumentException("경주할 수 있는 최소 자동차는 2대 이상 입니다.");
-        }
-    }
-    public void checkCarNameLength(String[] carNames) {
-        for (String carName : carNames) {
-            carNameMaxLength(carName);
-        }
+    public Validator(User user) {
+        this.user = user;
     }
 
-    private void carNameMaxLength(String carName) {
-        if (carName.trim().length() > 5) {
-            throw new IllegalArgumentException("자동차 이름의 글자 길이는 5글자 이하 입니다.");
+    public void checkCarNamesCount() {
+        if (user.getRacingCarNames().length <= 1) {
+            throw new IllegalArgumentException("?? 1?? ??? ??? ??????.");
+        }
+    }
+    public void checkCarNamesLength() {
+        for (String carName : user.getRacingCarNames()) {
+            if (carName.trim().length() > 5){
+                throw new IllegalArgumentException("??? ??? 5?? ????? ???.");
+            }
         }
     }
 }
