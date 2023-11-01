b1861c27d3fc27654d0036333f4de7e971b3899f
k1mjunyoung <vovrwm@gmail.com>
2023-11-01 11:12:35 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/main/java/racingcar/RacingService.java
+++ src/main/java/racingcar/RacingService.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
