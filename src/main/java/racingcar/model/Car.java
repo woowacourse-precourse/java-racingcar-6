bc4d48d9778ce9d0cfa471bd509554c5e894e175
beomsic <qjatjr29@khu.ac.kr>
2023-11-01 15:13:03 +0900

    :recycle: Refactor: Name VO를 사용하도록 수정
    - 이름으로 들어온 입력값에 대한 검증 로직 삭제
    - String 타입이 아닌 Name을 사용해 더욱 의미가 들어나게 표현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,23 +1,13 @@
 package racingcar.model;
 
-import static java.util.Objects.isNull;
-import static racingcar.common.exception.ErrorMessage.EMPTY_NAME;
-import static racingcar.common.exception.ErrorMessage.NAME_TOO_LONG;
-import static racingcar.common.exception.ErrorMessage.NULL_NAME;
-
-import racingcar.common.exception.CarException;
-
 public class Car implements Comparable<Car> {
 
-    private static final int MAX_LENGTH_OF_NAME = 5;
     private static final int MIN_RANDOM_VALUE_TO_MOVE = 4;
-    private final String name;
+    private final Name name;
     private Integer movedCount;
 
     private Car(final String name) {
-        validateEmptyName(name);
-        validateNameLength(name);
-        this.name = name;
+        this.name = new Name(name);
         this.movedCount = 0;
     }
 
@@ -27,11 +17,11 @@ public class Car implements Comparable<Car> {
 
     public Result moveOrStop(int randomValue) {
         if(canMove(randomValue)) movedCount++;
-        return new Result(name, movedCount);
+        return new Result(getName(), movedCount);
     }
 
     public String getName() {
-        return name;
+        return name.toString();
     }
 
     public Integer getMovedCount() {
@@ -45,14 +35,5 @@ public class Car implements Comparable<Car> {
 
     private boolean canMove(int randomValue) {
         return randomValue >= MIN_RANDOM_VALUE_TO_MOVE;
-    }
-
-    private void validateNameLength(final String name) {
-        if(name.length() > MAX_LENGTH_OF_NAME) throw new CarException(NAME_TOO_LONG);
-    }
-
-    private void validateEmptyName(final String name) {
-        if(isNull(name)) throw new CarException(NULL_NAME);
-        if(name.isBlank()) throw new CarException(EMPTY_NAME);
     }
 }
