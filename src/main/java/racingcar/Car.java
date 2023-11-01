44492cf3d1f219cad5ffe2b7de5b8dde4995ff8e
suin <parksuuuun@gmail.com>
2023-11-01 12:52:38 +0900

    feat: Car 클래스 생성 및 주된 로직에 사용하기 위한 변수, 생성자, 메서드 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private String name = null;
+    private String position = "";
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void moveForward() {
+        if(Randoms.pickNumberInRange(0, 9) >= 4) {
+            this.position += "-";
+        }
+    }
+
+    private void display() {
+        System.out.println(this.name + " : " + this.position);
+    }
+
+    public int getPositionDistance() {
+        return this.position.length();
+    }
+
+    public void run() {
+        this.moveForward();
+        this.display();
+    }
+
+}
