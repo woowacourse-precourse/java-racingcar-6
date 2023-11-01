58cfec24ffd8e44def72150817316f24187cef00
노정완 <jeongwan.noh@gmail.com>
2023-11-01 10:22:47 +0900

    refactor: 자동차 전진 기준 확장성 구현

--- src/main/java/racingcar/domain/Computer.java
+++ src/main/java/racingcar/domain/Computer.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Computer {
     private final static int MOVEMENT_FLAG = 4;
     public boolean judgeCarMovement(int random) {
-        if (random >= 4) {
+        if (random >= MOVEMENT_FLAG) {
             return true;
         }
         return false;
