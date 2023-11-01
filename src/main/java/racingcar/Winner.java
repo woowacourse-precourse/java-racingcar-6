f4a73fff14647541d59cc22348fd728291e15511
jh <pv104@naver.com>
2023-11-01 22:35:52 +0900

    refactor : Winner.class 리팩토링 findWinner의 리턴 타입을 boolean으로 변경 returnWinner의 carResults의 자료형을 List<Car>로 변경

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -3,27 +3,22 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-   [ ] (12) 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다. -> returnWinner()
-   [ ] (13) 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다. -> returnWinner() in if (winner > 1) */
-/* 12 : result의 길이가 max인 우승자를 List<String>.add 한 다음 리턴한다.
- */
-/* 13 : (12)에서 넘겨받은 리스트의 크기가 1이면 "{우승자}"를, 2 이상이면 "{우승자}, "를 추가해서 리턴한다.
 
- */
 public class Winner {
     private List<String> winners = new ArrayList<>();
-    public List<String> returnWinner(int maxLength, List<String> carResults) {
-        for (String s:
-             carResults) {
-            winners.add(findWinner(maxLength, s));
+
+    public List<String> returnWinner(int maxLength, List<Car> carResults) {
+        for (Car car :
+                carResults) {
+            if (findWinner(maxLength, car.getCarName())) {
+                winners.add(car.getCarName());
+            }
         }
         return winners;
     }
-    public String findWinner(int maxLength, String carResult) {
-        if(maxLength == carResult.length()) {
-            return carResult;
-        }
-        return "";
+
+    public boolean findWinner(int maxLength, String carResult) {
+        return maxLength == carResult.length();
     }
+
 }
