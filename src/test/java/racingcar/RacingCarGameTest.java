ffdb2c44a7b2f0e58f92d3c677390264700f5615
daeun Kong <daeun113@ajou.ac.kr>
2023-11-01 21:55:18 +0900

    test(setPlayers): 사용자 1명 설정할 수 있다

---
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RacingCarGameTest {
+    @Test
+    void setPlayers_사용자_1명_설정할_수_있다() {
+        RacingCarGame game = new RacingCarGame();
+        game.setPlayers("dadi");
+        ArrayList<RacingCar> racingCars = game.getRacingCars();
+
+        assertThat(racingCars).hasSize(1);
+        assertThat(racingCars.get(0).getName()).isEqualTo("dadi");
+    }
+}
