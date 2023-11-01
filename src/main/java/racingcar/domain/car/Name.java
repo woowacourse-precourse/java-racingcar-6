be6fb41d5941584be1c5eb11d051ec211fb67312
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 11:07:33 +0900

    feat: Name 클래스에 Validation 로직 추가

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -3,5 +3,33 @@ package racingcar.domain.car;
 public record Name(
         String name
 ) {
+    private static final int MAX_NAME_LENGTH = 5;
 
+    public Name {
+        validateName(name);
+    }
+
+    private void validateName(final String name) {
+        validateNameNullity(name);
+        validateNameBlank(name);
+        validateNameLength(name);
+    }
+
+    private void validateNameNullity(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("자동차 이름은 null이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNameBlank(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 공백만으로는 구성할 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
 }
