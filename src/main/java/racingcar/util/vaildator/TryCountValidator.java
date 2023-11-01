2834d544023b44417f73d12b987eb28f2448980d
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 14:21:31 +0900

    feat: 자연수만 입력하였는지에 대해 검증한다.

--- src/main/java/racingcar/util/vaildator/TryCountValidator.java
+++ src/main/java/racingcar/util/vaildator/TryCountValidator.java
@@ -1,8 +1,12 @@
 package racingcar.util.vaildator;
 
+import java.util.regex.Pattern;
+
 public class TryCountValidator {
+    private static final Pattern REGEX_NUMERIC = Pattern.compile("^[0-9]\\d*$");
     private static final String INPUT_TRYCOUNT_ZERO_EXCEPTION = "1 이상의 숫자를 입력해주세요.";
     private static final String INPUT_TRYCOUNT_NOTHING_EXCEPTION = "아무것도 입력하지 않았습니다. 1 이상의 숫자를 입력해주세요.";
+    private static final String INPUT_TRYCOUNT_NUMERIC_EXCEPTION = "1 이상의 자연수만 입력해주세요";
 
     public static void validateInputTryCount(String tryCount) {
         int inputTryCount = Integer.parseInt(tryCount);
@@ -14,6 +18,12 @@ public class TryCountValidator {
     public static void validateInputTryCountNothing(String tryCount) {
         if (tryCount.isEmpty()) {
             throw new IllegalArgumentException(INPUT_TRYCOUNT_NOTHING_EXCEPTION);
+        }
+    }
+
+    public static void validateInputTryCountNumeric(String tryCount) {
+        if (!REGEX_NUMERIC.matcher(tryCount).matches()) {
+            throw new IllegalArgumentException(INPUT_TRYCOUNT_NUMERIC_EXCEPTION);
         }
     }
 }
