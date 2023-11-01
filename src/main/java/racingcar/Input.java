f135248bf19094703e850a432ca94f5346a057d1
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-01 16:08:48 +0900

    refactor: Input 클래스 메서드를 분리해 단일책임의원칙 지향

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -11,28 +11,41 @@ public class Input {
 
 	public void getUserInput() {
 
+		String carNames = namesInput();
+		String count = countInput();
+
+		processCarNamesInput(carNames);
+		processCountInput(count);
+	}
+
+	private String namesInput() {
+
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-		String input = Console.readLine();
+		return Console.readLine();
+	}
 
-		String[] carnames = input.split(",");
-
-		InputValid.names(carnames);
-		
-		
-		for (int i = 0; i < carnames.length; i++) {
-			String name = carnames[i];
-			cars.add(new Car(name));
-		}
+	private String countInput() {
 
 		System.out.println("시도할 횟수는 몇회인가요?");
 
-		String cntstr = Console.readLine();
+		return Console.readLine();
 
-		count = Integer.parseInt(cntstr);
+	}
 
+	private void processCarNamesInput(String input) {
+		String[] carNames = input.split(",");
+		InputValid.names(carNames);
+
+		for (int i = 0; i < carNames.length; i++) {
+			String name = carNames[i];
+			cars.add(new Car(name));
+		}
+	}
+
+	private void processCountInput(String input) {
+		count = Integer.parseInt(input);
 		InputValid.count(count);
-		
 	}
 
 	public ArrayList<Car> getCars() {
