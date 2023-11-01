9f7886ed87429714bd7a66f686b8943f678e5d3a
yebin <dev.devin.kr@gmail.com>
2023-11-01 16:49:46 +0900

    feat(input-util): implement user console input handling

---
+++ src/main/java/racingcar/model/InputUtil.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class InputUtil {
+    public static String[] splitCarName(String carName) {
+        return carName.split(",");
+    }
+}
