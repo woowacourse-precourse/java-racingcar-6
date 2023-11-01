7637b3a7b8be9cd360550fdaf8f7138f3cec8e91
phmhp <phm6318@sookmyung.ac.kr>
2023-11-01 16:24:00 +0900

    feat : 최종우승자 출력 수정

--- src/main/java/racingcar/service/Winner.java
+++ src/main/java/racingcar/service/Winner.java
@@ -9,26 +9,29 @@ import java.util.List;
 public class Winner {
 
     PrintAll printAll = new PrintAll();
+
     public String finalResult(List<Car> carList) {
         String result = "";
         //가장 큰 값 구하기
         Car largestCar = largest(carList);
         //동점자 있는지 확인.
-        List<String> finalNameList = winnersNumber(largestCar,carList);
+        List<String> finalNameList = winnersNumber(largestCar, carList);
         //최득점자 이름 출력
         printAll.outputFinalPrint(finalNameList);
 
         return result;
     }
 
-    public List<String> winnersNumber(Car largestCar,List<Car> carList){
+    public List<String> winnersNumber(Car largestCar, List<Car> carList) {
         List<String> winnerNameList = new ArrayList<>();
-        winnerNameList.add(largestCar.getName());
+
         for (Car car : carList) {
-            if (car.getProgressbar()==largestCar.getProgressbar()){
+            if (car.getProgressbar() == largestCar.getProgressbar()) {
+                //System.out.println(car.getName());
                 winnerNameList.add(car.getName());
             }
         }
+
 
         return winnerNameList;
     }
@@ -38,7 +41,6 @@ public class Winner {
         for (Car car : carList) {
             if (car.getProgressbar() > maxNumCar.getProgressbar()) maxNumCar = car;
         }
-
         return maxNumCar;
     }
 
--- src/main/java/racingcar/view/PrintAll.java
+++ src/main/java/racingcar/view/PrintAll.java
@@ -2,49 +2,61 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.awt.*;
 import java.util.List;
 
 public class PrintAll {
 
-    public void inputCarPrint(){
+    public void inputCarPrint() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
-    public void inputTryNumberPrint(){
+
+    public void inputTryNumberPrint() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void outputResultPrint(){
+    public void outputResultPrint() {
         System.out.println("실행 결과");
     }
 
-    public void printProgress(List<Car> CarList){
+    public void printProgress(List<Car> CarList) {
 
-        for(Car car : CarList){
+        for (Car car : CarList) {
             System.out.print(car.getName() + " : ");
             printBar(car);
         }
         System.out.println(" ");
     }
-    public void printBar(Car car){
-        for (int i=0;i<car.getProgressbar(); i++){
+
+    public void printBar(Car car) {
+        for (int i = 0; i < car.getProgressbar(); i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public void outputFinalPrint(List<String> nameList){
+    public void outputFinalPrint(List<String> nameList) {
         System.out.print("최종 우승자 : ");
-        System.out.print(nameList.get(0));
-        if (nameList.size()==1){
+        if (nameList.size() == 1) {
+            System.out.print(nameList.get(0));
             System.out.println();
+        } else {
+            multiWinner(nameList);
         }
-        else{multiWinner(nameList);}
 
     }
-    public void multiWinner(List<String> nameList){
-        for (String name : nameList){
+
+    public void multiWinner(List<String> nameList) {
+        for (int i=0;i< nameList.size()-1;i++){
+            System.out.print(nameList.get(i));
             System.out.print(", ");
-            System.out.print(name);
+        }
+        System.out.print(nameList.get(nameList.size()-1));
+        //for (String name : nameList) {
+        //    System.out.print(name);
+            //if (nameList.get(-1) != name) {
+            //    System.out.print(", ");
+            //}
         }
     }
-}
+
