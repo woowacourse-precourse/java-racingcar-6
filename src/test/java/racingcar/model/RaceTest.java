de9b9d13a73efc5942823a5c0a0210c5d5e6fd49
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 12:13:00 +0900

    test(RaceTest): 시도 횟수 숫자가 아닐 때 예외 테스트

---
+++ src/test/java/racingcar/model/RaceTest.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+import racingcar.validator.AttemptNumberValidator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class RaceTest {
+    @Test
+    void validateNumber_should_throw_exception_when_attempts_is_not_a_number() {
+        assertThatThrownBy(() -> AttemptNumberValidator.validateNumber("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수는 숫자만 가능합니다.");
+    }
+}
