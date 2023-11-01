fc4ad2fb85fa4b27d3e2d231aa44ffb3d141e962
mudrhs1997 <mudrhs1997@daum.net>
2023-11-01 13:27:45 +0900

    feat: input & output 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCars = Console.readLine();
+        // 구분 및 저장
+        List<String> carList = Arrays.stream(inputCars.split(",")).filter(x -> Validate.checkLetterNum(x)).toList();
+        System.out.println("시도할 회수는 몇회인가요?");
+        Console.readLine();
     }
 }
