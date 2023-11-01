bf439d40e82eb4379c0d672ce2ec0c6b23c4e63e
조은별 <joeunbyeol98@gmail.com>
2023-11-01 16:43:16 +0900

    test(CarServiceImplTest): CarService 메서드 명 수정으로 인한 변경

--- src/test/java/racingcar/model/service/CarServiceImplTest.java
+++ src/test/java/racingcar/model/service/CarServiceImplTest.java
@@ -1,13 +1,10 @@
 package racingcar.model.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.dto.Car;
 import racingcar.model.dao.CarDao;
 import racingcar.model.dao.CarDaoImpl;
 
@@ -25,7 +22,7 @@ class CarServiceImplTest {
     @DisplayName("우승자 이름 리스트를 반환한다.")
     @Test
     void selectWinnerNames() {
-        assertThat(carService.selectWinnerNames())
+        assertThat(carService.getWinnerNames())
                 .containsExactly("pobi", "jun");
     }
 }
