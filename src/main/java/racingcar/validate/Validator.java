9bc669379062939046174f82a110d444725f27f6
wonndrive <nonyok812@gmail.com>
2023-11-01 23:02:51 +0900

    feat(racingcar): 자동차 이름 리스트 검증하기

--- src/main/java/racingcar/validate/Validator.java
+++ src/main/java/racingcar/validate/Validator.java
@@ -1,5 +1,19 @@
 package racingcar.validate;
 
-public class Validator {
+import java.util.List;
 
+public class Validator {
+    public static void validateLenOfCarNames(List<String> carNameList) {
+        for (String name : carNameList) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    public static void validateNumOfCars(List<String> carNameList) {
+        if (carNameList.size() < 2) {
+            throw new IllegalArgumentException("최소 2대의 자동차 이름이 필요합니다.");
+        }
+    }
 }
