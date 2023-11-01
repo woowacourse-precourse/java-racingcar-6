4c6a2769d922240dcf39f2541054001789d2793c
PARK9898 <wjddjs21@naver.com>
2023-11-01 10:27:32 +0900

    "test: 여러명의 우승자 테스트"

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -43,12 +43,22 @@ public class GameTest {
     void 우승자를_가려낸다() {
         List<Car> cars = List.of(
                 new Car("park",0),
+                new Car("kim",2)
+        );
+        Game game = new Game(cars);
+        List<Car> winner = game.pickWinner();
+
+        Assertions.assertThat(winner.get(0).toString()).isEqualTo("kim");
+    }
+    @Test
+    void 우승자는_여러명일수_있다() {
+        List<Car> cars = List.of(
+                new Car("park",0),
                 new Car("kim",2),
                 new Car("jay",3),
                 new Car("ray",3)
         );
         Game game = new Game(cars);
-
         List<Car> winner = game.pickWinner();
 
         Assertions.assertThat(winner.get(0).toString()).isEqualTo("jay");
