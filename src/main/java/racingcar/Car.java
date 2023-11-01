b8a946560426e481ecaeb5421c01274549c624f0
Hyunjun KIM <hihj070914@icloud.com>
2023-11-01 14:28:33 +0900

    feat: implement Car - Car 클래스의 세부 로직을 구현하였습니다. - 구현 후 유닛테스트를 완료하였습니다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+    private static final int MOVING_THRESHOLD = 4;
+    private static final int MINIMUM_RANDOM_VALUE = 0;
+    private static final int MAXIMUM_RANDOM_VALUE = 0;
 
     private String name;
     private int currentLocation;
@@ -20,14 +23,17 @@ public class Car {
     }
 
     public String getCurrentStatus() {
-
+        return this.name + ": " + "-".repeat(this.currentLocation);
     }
 
-    public boolean determineWhetherToMove(int randomValue) {
-
+    public boolean canMoveForward(int randomValue) {
+        return randomValue >= MOVING_THRESHOLD;
     }
 
     public void move() {
-
+        int randomValue = Randoms.pickNumberInRange(MINIMUM_RANDOM_VALUE, MAXIMUM_RANDOM_VALUE);
+        if (canMoveForward(randomValue)) {
+            this.currentLocation++;
+        }
     }
 }
