637f73060070e5f2700962d4cd09e22e1817ac8d
lkdcode <dlrlejr1@naver.com>
2023-11-01 15:18:04 +0900

    Add : InputValidator. validateNumericInput method

--- src/main/java/racingcar/domain/console/input/InputValidator.java
+++ src/main/java/racingcar/domain/console/input/InputValidator.java
@@ -5,6 +5,7 @@ import racingcar.exception.RacingCarException;
 public class InputValidator {
     private final static int MAX_LENGTH = 6;
     private final static int MIN_ATTEMPT_COUNT = 1;
+    private final static String NUMBER_REGEX = "^[0-9]+$";
 
     private InputValidator() {
     }
@@ -13,15 +14,21 @@ public class InputValidator {
         return new InputValidator();
     }
 
-    public void validateNameLength(final String name) throws IllegalArgumentException {
-        if (name.length() >= MAX_LENGTH) {
+    public void validateNameLength(final String inputName) throws IllegalArgumentException {
+        if (inputName.length() >= MAX_LENGTH) {
             RacingCarException.NAME_LENGTH_EXCEPTION.getException();
         }
     }
 
-    public void validateAttemptSize(final int count) throws IllegalArgumentException {
-        if (count < MIN_ATTEMPT_COUNT) {
+    public void validateAttemptSize(final int inputCount) throws IllegalArgumentException {
+        if (inputCount < MIN_ATTEMPT_COUNT) {
             RacingCarException.ATTEMPT_MINIMUM_COUNT_EXCEPTION.getException();
+        }
+    }
+
+    public void validateNumericInput(final String inputNumber) {
+        if (!inputNumber.matches(NUMBER_REGEX)) {
+            RacingCarException.NUMERIC_INPUT_EXCEPTION.getException();
         }
     }
 
