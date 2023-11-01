6348240c3b89277d9a82c4c15390e4e49566797b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-11-01 10:54:01 +0900

    test: 버그로 누락된 시행 횟수 테스트를 추가했다.

---
+++ src/test/java/racingcar/Input/ValidatedRoundCountTest.java
@@ -0,0 +1,27 @@
+package racingcar.Input;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ValidatedRoundCountTest {
+
+  @DisplayName("레이스 횟수가 0 이하일 때 예외처리")
+  @Test
+  void if_input_zero_throw_exception() {
+    // given
+    String roundCount = "0";
+    // when // then
+    assertThrows(IllegalArgumentException.class, () -> ValidatedRoundCount.of(roundCount));
+  }
+
+  @DisplayName("입력값이 숫자가 아닐 때 예외처리")
+  @Test
+  void if_input_is_not_number_throw_exception() {
+    // given
+    String roundCount = "a";
+    // when // then
+    assertThrows(IllegalArgumentException.class, () -> ValidatedRoundCount.of(roundCount));
+  }
+}
