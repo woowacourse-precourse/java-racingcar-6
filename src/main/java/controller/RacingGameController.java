9a5d9c81d12f039bbe95569d2a72acbd1e9cdf74
jaegyeongkim <dongji_11@naver.com>
2023-11-01 16:14:03 +0900

    refactor(RacingGameController): 메소드 분리

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -2,25 +2,26 @@ package controller;
 
 import model.Cars;
 import model.TryCount;
-import model.Winners;
 import view.InputView;
 import view.OutputView;
 
 public class RacingGameController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    private Cars cars;
+    private TryCount tryCount;
+    public void start() {
+        cars = new Cars(inputView.requestCarNames());
+        tryCount = new TryCount(inputView.requestTryCount());
+        gameRun();
+        outputView.printWinner(cars.getWinners());
+    }
 
-    public void run() {
-        Cars cars = new Cars(inputView.requestCarNames());
-        TryCount tryCount = new TryCount(inputView.requestTryCount());
+    private void gameRun() {
         outputView.printResult();
-
         for (int i = 0; i < tryCount.getCount(); i++) {
             cars.allTryMove();
             outputView.printCarsPosition(cars.getCars());
         }
-
-        outputView.printWinner(cars.getWinners());
-
     }
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,6 @@ import controller.RacingGameController;
 public class Application {
     public static void main(String[] args) {
         RacingGameController racingGameController = new RacingGameController();
-        racingGameController.run();
+        racingGameController.start();
     }
 }
