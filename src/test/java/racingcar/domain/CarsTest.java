31d62898e5fd9bfb1f7d55df4bbe5a04f211cf88
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-01 17:36:15 +0900

    style - Cars 우승자찾기 테스트코드 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -7,13 +7,16 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import racingcar.exception.CarListEmptyException;
 import racingcar.service.RandomNumberGenerator;
 import racingcar.service.BasicMovementRule;
 
@@ -30,7 +33,7 @@ public class CarsTest {
     @DisplayName("자동차의 개수만큼 RandomNumber가 생성된다")
     @Test
     void generateRandomNumberByCarsNumber() {
-        List<Car> carList = Arrays.asList(mock(Car.class), // Mock Car 객체 생성
+        List<Car> carList = Arrays.asList(mock(Car.class),
                 mock(Car.class),
                 mock(Car.class));
         Cars cars = new Cars(carList);
@@ -42,5 +45,33 @@ public class CarsTest {
         for (Car car : carList) {
             verify(car, times(1)).move(eq(basicMovementRule), anyInt());
         }
+    }
+
+    @DisplayName("차의 위치가 제일 높은 것을 반환한다. 차량 리스트가 없을 경우 예외를 발생시킨다")
+    @Test
+    void findMaxCarPosition() {
+        Car expect = new Car("pboi");
+        List<Car> carList = Arrays.asList(expect,
+                new Car("jun"),
+                new Car("seok"));
+
+        List<Car> emptyList = new ArrayList<>();
+
+        Cars emptyCars = new Cars(emptyList);
+        BasicMovementRule basicMovementRule = new BasicMovementRule();
+
+        carList.get(0).move(basicMovementRule, 6);
+        carList.get(0).move(basicMovementRule, 6);
+        carList.get(0).move(basicMovementRule, 6);
+
+        Cars cars = new Cars(carList);
+
+        Car actual = cars.findMaxCarPosition();
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expect, actual),
+                () -> Assertions.assertThrows(CarListEmptyException.class,
+                        emptyCars::findMaxCarPosition)
+        );
     }
 }
