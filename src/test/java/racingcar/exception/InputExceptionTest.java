20a81e3a75f4b91d84b71425e4f4ca6998a2153f
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-01 19:29:54 +0900

    test: add name length when up 5 to input exception test

---
+++ src/test/java/racingcar/exception/InputExceptionTest.java
@@ -0,0 +1,26 @@
+package racingcar.exception;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputExceptionTest extends NsTest {
+
+    @Test
+    void name_length_when_up_5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("aaaaaaa,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
