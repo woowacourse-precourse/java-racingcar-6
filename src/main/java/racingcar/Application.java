a98c6d3625357deb6f2584b630f15355e8477cdf
Hajuuu <hpt0127@naver.com>
2023-11-01 23:20:49 +0900

    refactor(main): 예외 처리 추가 및 userCar 함수 변경
    회수 입력 받을 때 예외 발생하면 IllegalArgumentException()이 발생하도록 변경,
    userCar의 문자열 자르기를 StringTokenizer에서 split으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,7 +12,13 @@ public class Application {
 
         user.inputCheck(users);
         System.out.println("시도할 회수는 몇회인가요?");
-        int gameCount = Integer.parseInt(readLine());
+
+        int gameCount = 0;
+        try {
+            gameCount = Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
 
         Game game = new Game();
         System.out.println();
@@ -28,10 +33,10 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputString = readLine();
 
-        StringTokenizer st = new StringTokenizer(inputString, ",");
         List<User> users = new ArrayList<>();
-        while (st.hasMoreTokens()) {
-            users.add(new User(st.nextToken(), 0));
+        String[] names = inputString.split(",");
+        for (String s : names) {
+            users.add(new User(s, 0));
         }
 
         return users;
