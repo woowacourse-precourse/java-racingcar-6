bc3b096bf6857652c08d30671dff39391623827f
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 22:15:29 +0900

    feat : createRacingCarWithName 함수 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,5 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RacingCarTest {
-  
+    @Test
+    void createRacingCarWithName() {
+        String name = "test";
+        RacingCar car = new RacingCar(name);
+        assertEquals(name, car.getName());
+    }
+
+
 }
