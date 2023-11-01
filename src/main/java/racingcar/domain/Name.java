033029304f1ffd4419afc758fc3e83f98ec46c40
nahyeon99 <nahyeonee99@gmail.com>
2023-11-01 20:04:46 +0900

    fix(domain/Name): 차 이름 길이의 최소값에 대한 검증 추가
    1. validateLength() : 최소값 검증 추가
    2. ExceptionMessage CAR_NAME_SIZE : 최소값에 대한 메시지 추가

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -4,6 +4,7 @@ import racingcar.util.ExceptionMessage;
 
 public class Name {
 
+    private static final int MIN_NAME_SIZE = 1;
     private static final int MAX_NAME_SIZE = 5;
     private final String name;
 
@@ -13,7 +14,7 @@ public class Name {
     }
 
     private void validateLength(int length) {
-        if (length > MAX_NAME_SIZE) {
+        if (length < MIN_NAME_SIZE || length > MAX_NAME_SIZE) {
             throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_SIZE);
         }
     }
--- src/main/java/racingcar/util/ExceptionMessage.java
+++ src/main/java/racingcar/util/ExceptionMessage.java
@@ -2,8 +2,7 @@ package racingcar.util;
 
 public class ExceptionMessage {
 
-    public static final String CAR_NAME_SIZE = "자동차 이름은 5자 이하여야 합니다.";
-    public static final String CAR_NAME_EMPTY = "자동차 이름 입력은 필수 입니다.";
+    public static final String CAR_NAME_SIZE = "자동차 이름은 1~5자 여야 합니다.";
     public static final String CAR_NAME_FORMAT = "자동차 이름은 영어 소문자만 가능하며 쉼표로 구분하여 입력하세요.";
     public static final String CAR_NAME_DUPLICATE = "중복된 자동차 이름이 있습니다.";
     public static final String CARS_SIZE = "자동차 개수는 2~10개 이어야 합니다.";
