21a1101f5185c80d6e687322b146aed027af65b6
jang-namu <ironhee8005@gmail.com>
2023-11-01 19:46:28 +0900

    feat: inputNewCarNames(자동차 이름 입력받는 기능)

--- src/main/java/racingcar/component/Cars.java
+++ src/main/java/racingcar/component/Cars.java
@@ -1,5 +1,33 @@
 package racingcar.component;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Cars {
+    List<Car> cars;
+
+    public Cars() {
+        cars = new LinkedList<>();
+    }
+
+    public List<String> inputNewCarNames() {
+        List<String> nameList = new LinkedList<>();
+
+        String names = Console.readLine();
+        for (String name : names.split(",")) {
+            nameList.add(name);
+        }
+        return nameList;
+    }
+
+    /*
+    public void addCarsByNames(List<String> names) {
+
+    }
+    */
 
 }
