066755cfc42c4188a0fa09fee7a9eb32486ec872
ljkhyeong <jolri24@gmail.com>
2023-11-01 22:12:27 +0900

    refactor: 출력 기능 모듈화
    결합도 하락

--- src/main/java/racingcar/provider/RacingOutputProvider.java
+++ src/main/java/racingcar/provider/RacingOutputProvider.java
@@ -1,2 +1,36 @@
-package racingcar.provider;public class RacingOutputProvider {
+package racingcar.provider;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+public class RacingOutputProvider {
+
+	private final Console console;
+
+	public RacingOutputProvider(Console console) {
+		this.console = console;
+	}
+
+	public void printRacing(List<Car> cars) {
+		for (Car car : cars) {
+			System.out.println(car.getName() + " : " + carProgress(car.getPosition()));
+		}
+		System.out.println();
+	}
+
+	public void printWinners(List<Car> winners) {
+		String winnerNames = winners.stream()
+			.map(Car::getName)
+			.collect(Collectors.joining(","));
+
+		System.out.println("최종 우승자 : " + winnerNames);
+	}
+
+	private String carProgress(int position) {
+		return "-".repeat(position);
+	}
+
 }
