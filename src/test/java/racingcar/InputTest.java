bc741b7cd0f0f21d0c59a3a8dcaab84ec3bf8567
Hyunstone <hyun104467@naver.com>
2023-11-01 21:37:22 +0900

    [Chore] 테스트 코드 정리

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,31 +1,34 @@
 package racingcar;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class InputValidatorTest {
+class InputTest {
+    InputValidator inputValidator = new InputValidator();
+    SoftAssertions softAssertions;
+    @BeforeEach
+    void init() {
+        softAssertions = new SoftAssertions();
+    }
 
     @Test
     void 자동차의_이름은_길이가_5_이하여야_한다() {
         String input1 = "pobi";
         String input2= "pobibibi";
-        InputValidator inputValidator = new InputValidator();
 
-        SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(inputValidator.validateCarName(input1)).contains("pobi");
         softAssertions.assertThatThrownBy(() -> inputValidator.validateCarName(input2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 자동차의_이름은_영어로_이루어져야_한다() {
+    void 자동차의_이름은_알파벳으로_이루어져야_한다() {
         String input1 = "pobi";
         String input2= "po123po";
         String input3= "12345";
         String input4= "po!@#";
-        InputValidator inputValidator = new InputValidator();
 
-        SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(inputValidator.validateCarName(input1)).contains("pobi");
         softAssertions.assertThatThrownBy(() -> inputValidator.validateCarName(input2))
                 .isInstanceOf(IllegalArgumentException.class);
