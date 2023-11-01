a1625f226921324eb8bfdcae33e34aa24eca9611
min050410 <min050410@gmail.com>
2023-11-01 23:30:10 +0900

    fix(RacingCar): position의 디폴트 생성 값을 추가한다.

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,11 +2,16 @@ package racingcar.domain;
 
 public class RacingCar {
 
+    private final static int DEFAULT_POSITION = 0;
+
+    public final static int MAX_RACING_CAR_NAME_LENGTH = 5;
+
     private final String name;
     private int position;
 
     public RacingCar(String name) {
         this.name = name;
+        this.position = DEFAULT_POSITION;
     }
 
     public void updatePosition(int distance) {
