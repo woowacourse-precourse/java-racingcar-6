4c5d5f2a210c44aecdad1bf8a055a561332aebba
ay-eonii <naayen@naver.com>
2023-11-01 14:14:01 +0900

    test: 자동차 이름 공백 유효성 검사하는 단위테스트 작성

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -17,4 +17,15 @@ class ValidatorTest {
                         .hasMessageContaining("Length must be less or equal than")
         );
     }
+
+    @Test
+    void 이름이_비어있거나_공백으로만_이루어져_있는_경우_예외_발생() {
+        String inputCarName = " ";
+        Validator validator = new Validator();
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.checkCarName(inputCarName))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("Car name cannot be blank")
+        );
+    }
 }
