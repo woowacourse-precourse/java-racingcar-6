7d76d4f5b5ac65824f2ed56d9c73c0e175f5d4de
insu3160 <is23193160@gmail.com>
2023-11-01 17:48:47 +0900

    test: 시도 횟수가 숫자가 아닐 때 예외 처리하는 테스트 코드 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final int MIN_ATTEMPTCOUNT_NUM = 1;
-    private static final String INVALID_INPUT_ERROR = "[ERROR] : 숫자를 입력해주세요.";
+    private static final String NON_NUMERIC_INPUT_ERROR = "[ERROR] : 숫자를 입력해주세요.";
     private static final String NEGATIVE_ATTEMPTCOUNT_ERROR = "[ERROR] : 1 이상의 숫자를 입력해주세요.";
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
@@ -21,7 +21,7 @@ public class InputView {
             validateInput(attemptCount);
             return attemptCount;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_INPUT_ERROR);
+            throw new IllegalArgumentException(NON_NUMERIC_INPUT_ERROR);
         }
     }
 
---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,42 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    @BeforeEach
+    void setUp() {
+        System.setIn(System.in);
+    }
+
+    private void provideInput(String input) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자로 잘 변환되는지 확인")
+    void inputAttemptCount() {
+        provideInput("5");
+        int attemptCount = InputView.inputAttemptCount();
+
+        assertThat(attemptCount).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자가 아닐 때 예외 처리")
+    void inputNonNumericAttemptCount() {
+        provideInput("a");
+
+        assertThatThrownBy(() -> {
+            int attemptCount = InputView.inputAttemptCount();
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] : 숫자를 입력해주세요.");
+    }
+
+}
