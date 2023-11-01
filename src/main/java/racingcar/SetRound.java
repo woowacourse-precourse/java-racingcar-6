8f567ba310a99d3ad874c31237c726df9e505469
youngchive <2000mily@naver.com>
2023-11-01 22:05:34 +0900

    fix: add exception

--- src/main/java/racingcar/SetRound.java
+++ src/main/java/racingcar/SetRound.java
@@ -5,8 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class SetRound {
     public static int setRound() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        Integer num = Integer.valueOf(Console.readLine());
-        return num;
+        while (true) {
+            System.out.println("시도할 회수는 몇회인가요?");
+            Integer num = Integer.valueOf(Console.readLine());
+            if (num <= 0) {
+                throw new IllegalArgumentException("양수인 값을 입력해야 합니다.");
+            }
+                return num;
+        }
     }
 }
