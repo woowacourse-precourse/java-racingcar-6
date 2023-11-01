8ce8a64ab5d2156724e3293055dd4b0ea8303680
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-11-01 20:20:03 +0900

    test/racingCarGameTest: 테스트파일 생성, 테스트하는법 공부후 작성예정

---
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarGameTest {
+
+    private final RacingCarGame racingCarGame = new RacingCarGame();
+
+    @Test
+    void checkInputNumValid(){
+
+    }
+
+}
