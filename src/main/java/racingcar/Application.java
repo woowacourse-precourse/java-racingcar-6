943d2bdc7b895329eb75d2816d6a104bf76c9ec3
Heesu Jeong <huissppea@gmail.com>
2023-11-01 22:52:39 +0900

    feat: 경주할 차 이름, 시도할 횟수 입력 추가, 차 이름 예외처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        System.out.println(input);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNum = Console.readLine();
+        int inputNumber = Integer.parseInt(inputNum);
+
+        System.out.println(inputNum);
+
+        String[] carNames = input.split(",");
+
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
+            }
+        }
     }
 }
