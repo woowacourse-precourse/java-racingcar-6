38d591ee521adf0174fb4e79d8051d7f153e9669
leorb159 <ldgsocial159@naver.com>
2023-11-01 20:02:26 +0900

    style(Check): 코드컨벤션 수정

--- src/main/java/racingcar/Check.java
+++ src/main/java/racingcar/Check.java
@@ -2,17 +2,17 @@ package racingcar;
 
 public class Check {
 
-    public void checkNameSize(String input){
+    public void checkNameSize(String input) {
         String[] strArr = input.split(",");
-        for(int i=0; i<strArr.length; i++){
-            if(strArr[i].length() > 5){
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
             }
         }
         return;
     }
 
-    public void checkNumber(String input){
+    public void checkNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
