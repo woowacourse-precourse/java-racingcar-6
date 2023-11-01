99241d7e140354b3d96442b4cd3940a8170a73a4
hololol-101 <shrkddms1010@naver.com>
2023-11-01 23:29:42 +0900

    [feat] 테스트 추가 - 제일 멀리 간 자동차가 승자 자동차와 일치하는지 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,10 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -30,6 +32,33 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test void 승자_최대값_확인(){
+        //input
+        RacingContest racingContest = new RacingContest(3);
+        racingContest.insertCar(new RacingCar("player1"));
+        racingContest.insertCar(new RacingCar("player2"));
+
+        //play contest
+        racingContest.playGame();
+        int max = 0;
+        List<String> maxList = new ArrayList<>();
+        for(RacingCar carInfo : racingContest.racingCars){
+            if(max==carInfo.getAdvanceCount()){
+                maxList.add(carInfo.getName());
+            }
+
+            if(max<carInfo.getAdvanceCount()) {
+                max = carInfo.getAdvanceCount();
+                maxList.clear();
+                maxList.add(carInfo.getName());
+            }
+        }
+        ArrayList<String> winnerList = racingContest.returnWinner();
+        assertThat(maxList).containsExactlyElementsOf(winnerList);
+    }
+
+
 
     @Override
     public void runMain() {
