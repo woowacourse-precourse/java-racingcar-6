9cb998d3d1b4d692a0aeb8f7621070208f2b7288
Carrot_hyeong <rjgjfl90@naver.com>
2023-11-01 16:17:21 +0900

    feat : 자동차 이름 입력 받고 배열로 정리, 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,17 @@
 package racingcar;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carInput = camp.nextstep.edu.missionutils.Console.readLine();
+        String[] carNames = carInput.split(",");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int gameCnt = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+
         
     }
 }
