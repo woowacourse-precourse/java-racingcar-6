651e7e36a17ecc0b56d5a22d16c8c502dd19098a
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-01 22:12:28 +0900

    좀더 적절하게 INPUT, OUTPUT 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,12 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-
     public String getCarName() {
+        OutputView.printInputCarNameMessage();
         return Console.readLine();
     }
-}
 
+    public int getTryNumber() {
+        OutputView.printInputTryNumber();
+        return Integer.parseInt(Console.readLine());
+    }
+}
 
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,7 +1,13 @@
 package racingcar;
-public class OutputView {
-    public static void printInputCarNameMessage(){
-        System.out.println(InputView.INPUT_CAR_NAME_MESSAGE.print());
-    }
 
+public class OutputView {
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
+
+    public static void printInputCarNameMessage(){
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+    }
+    public static void printInputTryNumber() {
+        System.out.println(InputView.INPUT_TRY_NUMBER);
+    }
 }
