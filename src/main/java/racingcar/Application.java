6ea064f6a2d215ec5ea4c040da0da379efd88ccc
WinterHana <haha920@naver.com>
2023-11-01 14:32:17 +0900

    feat(Application) : 횟수에 대한 예외 처리 추가
    - 예외 상황을 보도록 Application에 InputException.exceptionCheckStep 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,15 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = Console.readLine();
         RacingGame racingGame = new RacingGame(cars);
+
         System.out.println("시도할 회수는 몇회인가요?");
         String step = Console.readLine();
-        racingGame.playGame(Integer.valueOf(step));
+        int stepNum = InputException.exceptionCheckStep(step);
+        racingGame.playGame(stepNum);
     }
 }
