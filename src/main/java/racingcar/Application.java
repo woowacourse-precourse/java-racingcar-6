51a697846c220d4e2634c2518f40b38e304cf1c3
jyeun722 <skqhs7276@gamil.com>
2023-11-01 14:11:14 +0900

    feat: Add car names input function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int MAX_NAME_SIZE = 5;
+
+    private static String[] inputNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine().replaceAll("[^a-zA-Z0-9,]", "");
+        System.out.println(carNamesInput);
+
+        String[] carNamesArray = carNamesInput.split(",");
+        for (int i = 0; i < carNamesArray.length; i++) {
+            if (carNamesArray[i].length() > MAX_NAME_SIZE) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return carNamesArray;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNamesArray = inputNames();
     }
 }
