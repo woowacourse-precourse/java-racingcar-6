b15e4771e969785c8634c94935df238ae34d517b
YongHyeon <pyh4616@naver.com>
2023-11-01 15:42:34 +0900

    Etc: (OutputView 클래스), 회수 -> 횟수, 오타수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class OutputView {
 
     private static final String INPUT_RACING_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
     public void printInputRacingCarNamesMessage() {
