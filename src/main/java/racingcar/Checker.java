fc280c1464757c363609a32f84d2c767094d1d8b
Sim-GyuBin <skb0606@gmail.com>
2023-11-01 16:44:24 +0900

    feat: 횟수 검증 문자열 받도록 수정

--- src/main/java/racingcar/Checker.java
+++ src/main/java/racingcar/Checker.java
@@ -3,11 +3,13 @@ import java.util.*;
 
 public class Checker {
 
-    public static void checkValidTurn(int turn) {
+    public static int checkValidTurn(String turn) {
         try {
-            if (turn <= 0) {
+            int numberOfTurns = Integer.parseInt(turn);
+            if (numberOfTurns <= 0) {
                 throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
             }
+            return numberOfTurns;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력된 값이 정수가 아닙니다.");
         }
