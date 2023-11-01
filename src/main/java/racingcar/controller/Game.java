0ee147124431eea9945bb6c9cf74642abdfdb4a7
KongJunWoo <joonoo3@naver.com>
2023-11-01 22:42:53 +0900

    feat :  자동차 객체 생성
    - Car객체의 List를 선언합니다.
    - makeCarList로 Car 객체들을 생성하여 List에 저장합니다.

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,10 +1,28 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private List<Car> carList;
+
+    public Game() {
+        this.carList = new ArrayList<>();
+    }
 
     public static Game create() {
         return new Game();
     }
 
+    public void makeCarList(List<String> carList) {
+
+        for (String carName : carList) {
+            Car car = Car.create(carName);
+            this.carList.add(car);
+        }
+
+    }
 
 }
