120793e17a9f9cdbc23a35e276a67f2859f0e11f
= <=>
2023-11-01 20:10:05 +0900

    feat(Application): 입력 값에 대한 Exception 유효성 검사 로직 추가
    게임을 시작할 때 사용자에게 입력 받는 값인 자동차의 이름과 시행 횟수에 대해 Exception 클래스에 정의된 유효성 검사를 시행합니다.
    기존 코드는 Input 클래스에서 입력값을 받은 수 검사를 하지 않았지만, 변경 후 받아온 carNames와 tryCount에 대한 유효성 검사를 실시합니다.
     변경전 Console로 받아온 String tryCount를 Integer.parseInt(tryCount)로 변경을 하였지만 변경 후 코드는 tryCount를 Int형으로 바로 바꾸지 않습니다. String -> Int 변경 코드는 RacingCar 클래스에 작성되었습니다.
    
     Before:
     RacingCar game = new RacingCar(carNames, Integer.parseInt(tryCount));
    
     After:
     Exception.checkCarNames(carNames);
    
             String tryCount = input.inputTryCount();
             Exception.checkTryCount(tryCount);
    
             RacingCar game = new RacingCar(carNames, tryCount);

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,15 @@
 package racingcar;
-
 public class Application {
     public static void main(String[] args) {
         Input input = new Input();
 
         String carNames = input.inputCarName();
-        String tryCount = input.inputTryCount();
+        Exception.checkCarNames(carNames);
 
-        RacingCar game = new RacingCar(carNames, Integer.parseInt(tryCount));
+        String tryCount = input.inputTryCount();
+        Exception.checkTryCount(tryCount);
+
+        RacingCar game = new RacingCar(carNames, tryCount);
         game.startGame();
     }
 }
