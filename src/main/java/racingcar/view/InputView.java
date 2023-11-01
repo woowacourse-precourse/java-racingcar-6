e3ab5297bd37d3cefa9dd4c685efb75135f0da91
이 다정 <vicpillon0319@gamil.com>
2023-11-01 21:16:54 +0900

    refator: validator class 통일

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.List;
-import racingcar.validator.Validator;
+import racingcar.validator.Vaildator;
 
 
 public class InputView {
@@ -11,16 +11,9 @@ public class InputView {
     public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_MOVE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private final Validator duplicateNameValidator;
-    private final Validator characterLimitValidator;
-    private final Validator emptyNameValidator;
+    private final Vaildator vaildator = new Vaildator();
 
-
-    public InputView(Validator duplicateNameValidator, Validator characterLimitValidator,
-                     Validator emptyNameValidator) {
-        this.duplicateNameValidator = duplicateNameValidator;
-        this.characterLimitValidator = characterLimitValidator;
-        this.emptyNameValidator = emptyNameValidator;
+    public InputView() {
     }
 
     public String inputCarNames() {
@@ -45,28 +38,19 @@ public class InputView {
     }
 
     public void validateMoveCount(String input) {
-        if (!isPositiveInteger(input)) {
+        if (!vaildator.isPositiveInteger(input)) {
             throw new IllegalArgumentException("error: 숫자가 아닌 값이 입력되었습니다.");
         }
     }
 
-    private boolean isPositiveInteger(String input) {
-        try {
-            int num = Integer.parseInt(input);
-            return num > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
     public void validateCarNames(List<String> carNameList) {
-        if (duplicateNameValidator.isValid(carNameList)) {
+        if (vaildator.checkDuplicateName(carNameList)) {
             throw new IllegalArgumentException("error: 중복된 이름이 입력되었습니다.");
         }
-        if (characterLimitValidator.isValid(carNameList)) {
+        if (vaildator.checkCharactorLimit(carNameList)) {
             throw new IllegalArgumentException("error: 글자수가 초과된 이름이 입력되었습니다.");
         }
-        if (emptyNameValidator.isValid(carNameList)) {
+        if (vaildator.checkEmptyName(carNameList)) {
             throw new IllegalArgumentException("error: 구분자 사이에 입력되지 않은 이름이 있습니다.");
         }
     }
