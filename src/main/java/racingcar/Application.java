2c0cce40e92b7d98c960d5dab89be70ffd4b70f1
potatomoong <star901210@hanmail.net>
2023-11-01 16:56:04 +0900

    Feat: 자동차 입력받기, 유효성검사 메인클래스에 적용 및 오타수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.domain.StartCondition;
+import racingcar.domain.JudgeMovement;
+import racingcar.domain.Result;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StartCondition startCondition = new StartCondition();
+        String[] carNames = startCondition.inputCarName();
     }
 }
--- src/main/java/racingcar/domain/StartCondition.java
+++ src/main/java/racingcar/domain/StartCondition.java
@@ -1,16 +1,22 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Console;
 
 public class StartCondition {
-    public String[] ipuntCarName() {
+    public String[] inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
         checkInputCarName(carName);
         return carName.split(",");
     }
 
-    private void checkInputCarName(String carNames) {
+    private void checkInputCarName(String carName) {
+        String[] racerNames = carName.split(",");
+        for (String racerName : racerNames) {
+            if (racerName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public int inputAttemptNumber() {
