21f711322531a5a3bf5bc6f0ec934e7aa0b32194
jaepyo99 <jaepyo99@naver.com>
2023-11-01 15:38:49 +0900

    [Feat] 사용자 입력값 정수인지 판별메서드 생성

--- src/main/java/racingcar/common/util/Validator.java
+++ src/main/java/racingcar/common/util/Validator.java
@@ -3,5 +3,13 @@ package racingcar.common.util;
 import static racingcar.common.message.ErrorConstant.CAR_NAME_OVER_FIVE_ERROR_MESSAGE;
 
 public class Validator {
-    
+    public boolean isInt(String numberString) {
+        for (int i = 0; i < numberString.length(); i++) {
+            char numberChar = numberString.charAt(i);
+            if (!('0' <= numberChar && numberChar <= '9')) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
