73afd71a31a2399197042020c6faf8d086002711
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 20:17:00 +0900

    feat(Output): 각 차수별 실행 결과를 출력하는 기능 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -3,8 +3,9 @@ package racingcar;
 public class Output {
 
     public enum Message {
-        PROMPT_FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-        PROMPT_FOR_TRY_COUNT("시도할 회수는 몇회인가요?")
+        FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        FOR_TRY_COUNT("시도할 회수는 몇회인가요?"),
+        RESULT("\n실행 결과"),
         ;
 
         private final String label;
@@ -19,17 +20,29 @@ public class Output {
     }
 
     // 경주할 자동차 이름을 입력하라는 메세지를 출력합니다.
-    public void promptForCarName()
+    public static void promptForCarName()
     {
-        System.out.println(Message.PROMPT_FOR_CAR_NAME.label());
+        System.out.println(Message.FOR_CAR_NAME.label());
     }
 
     // 시도할 회수를 입력하라는 메세지를 출력합니다.
-    public void promptForTryCount() {
-        System.out.println(Message.PROMPT_FOR_TRY_COUNT.label());
+    public static void promptForTryCount() {
+        System.out.println(Message.FOR_TRY_COUNT.label());
     }
 
-    // TODO : 각 차수별 실행 결과를 출력합니다.
+    // 각 차수별 실행 결과를 출력합니다.
+    public static void printResult() {
+        System.out.println(Message.RESULT.label());
+    }
+
+    public static void printRoundResults(String name, int count) {
+        System.out.println(name + " : " + "-".repeat(count));
+    }
+
+    public static void printLine()
+    {
+        System.out.println();
+    }
 
     // TODO : 우승자를 출력합니다. 공동 우승자일 경우 쉼표를 이용하여 구분합니다.
 
