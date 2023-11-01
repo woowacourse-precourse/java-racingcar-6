e437177574ffd9572be270ff77db195b81505163
Hyso <hsk7953@gmail.com>
2023-11-01 18:17:52 +0900

    refactor, style: 주석제거

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -1,10 +1,9 @@
-package racingcar.controller;
+package racingcar.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// 입력받은 문자열과 숫자를 일단 파싱해주는 메소드
 public class Parser {
     public List<String> parseCar(String input){
         return new ArrayList<>(Arrays.asList(input.split(",")));
