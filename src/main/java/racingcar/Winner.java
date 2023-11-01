6d4b77a8a293f375b7ad87d02f372dee93976155
junghyun <fnsk1256@naver.com>
2023-11-01 22:44:17 +0900

    feat(Winner): 우승자 클래스

---
+++ src/main/java/racingcar/Winner.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.Vector;
+
+public class Winner {
+    Vector<String> winner = new Vector<String>(); //우승자의 이름이 들어가는 벡터를 만들어서
+	
+	public Vector<String> getWinner() {
+		return winner;
+	}
+}
