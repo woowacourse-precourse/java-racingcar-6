9354e40ca4dfdbae550d59a2018f39bcf5c32ca8
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-01 13:44:01 +0900

    feat: receive the user's input for the number of excutions and validate it

---
+++ src/main/java/racingcar/Execution.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+public class Execution {
+    private static Integer count;
+
+    public Execution(String input) {
+        validateFormat(input);
+        validatePositive(input);
+
+        count = Integer.parseInt(input);
+    }
+
+    private void validateFormat(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("올바른 정수를 입력하세요.");
+        }
+    }
+
+    private void validatePositive(String input) {
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 양수여야 합니다.");
+        }
+    }
+}
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -5,21 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Racing {
-    List<RacingCar> racingCars = new ArrayList<>();
     IOHandler ioHandler = new IOHandler();
+    List<RacingCar> racingCars = new ArrayList<>();
+    Execution execution;
 
     public void run() {
-        ioHandler.printComment("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         generateRacingCar();
+        promptForExecutionCount();
+        ioHandler.printComment("");
+        ioHandler.printComment("실행결과");
     }
 
 
     private void generateRacingCar() {
+        ioHandler.printComment("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = ioHandler.readConsoleInput();
+
         List<String> carsList = Arrays.asList(cars.split(","));
         for (String car : carsList) {
             racingCars.add(new RacingCar(car));
         }
+    }
+
+    private void promptForExecutionCount() {
+        ioHandler.printComment("시도할 회수는 몇회인가요?");
+        String executionCount = ioHandler.readConsoleInput();
+        execution = new Execution(executionCount);
     }
 }
