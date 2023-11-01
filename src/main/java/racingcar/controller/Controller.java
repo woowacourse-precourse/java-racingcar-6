9d2df83265af86a6387c6f907e20d3ad85a672d4
phmhp <phm6318@sookmyung.ac.kr>
2023-11-01 15:39:31 +0900

    feat : 프로세스 수정

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -57,19 +57,23 @@ public class Controller {
         //System.out.println(getCarName.get(0).getName());
         //System.out.println(getCarName.get(1).getName());
         //error: System.out.println(CarList.get(0));
-        System.out.println(carListNew.getCarList().get(0).getName());
-        System.out.println(carListNew.getCarList().get(1).getName());
-        while (this.tryNumber > 0) {
-            System.out.println("hereTryNumber");
+
+        //System.out.println(carListNew.getCarList().get(0).getName());
+        //System.out.println(carListNew.getCarList().get(1).getName());
+        while (tryNumber > 0) {
+            //System.out.println("hereTryNumber");
             for (Car car :carListNew.getCarList()) {
 
-                System.out.println("for문 car");
+                //System.out.println("for문 car");
                 boolean trueOrFalse = getRandomNumber.getTrueOrFalse();
                 System.out.println(trueOrFalse);
                 updateProgress.plusOrNot(car, trueOrFalse);
             }
-            printAll.printProgress(carListNew.getCarList());
-            this.tryNumber--;
+            printAll.printProgress(carListNew.getCarList()); // 이부분이 안되고있음(racingcar.domain.Car@61a485d2 : racingcar.domain.Car@39fb3ab6 : - )
+
+            System.out.println("전 tryNumber수는" + tryNumber);
+            tryNumber--;
+            System.out.println("후 tryNumber수는" + tryNumber);
         }
         //Winner;
         winner.finalResult(carListNew.getCarList());
--- src/main/java/racingcar/repository/UpdateProgress.java
+++ src/main/java/racingcar/repository/UpdateProgress.java
@@ -7,5 +7,6 @@ public class UpdateProgress {
     public void plusOrNot(Car car, boolean tOrF) {
         System.out.println("plusOrNot");
         if (tOrF == true) car.progressPlusOne();
+        System.out.println(car.getName() + "의 프로그레스바 개수 : " + car.getProgressbar());
     }
 }
