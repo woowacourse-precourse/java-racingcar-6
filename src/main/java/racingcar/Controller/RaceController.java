8eeb3844d369504085e95810a7156cc57734e157
masonkimseoul <masonkimseoul@gmail.com>
2023-11-01 18:39:09 +0900

    refactor : Update name validating function's location

--- src/main/java/racingcar/Controller/RaceController.java
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -11,8 +11,6 @@ public class RaceController {
 
     private static final String SEPARATOR = ",";
     private static final int MINIMUM_ENTRY_COUNT = 2;
-    private static final int MINIMUM_NAME_LENGTH = 1;
-    private static final int MAXIMUM_NAME_LENGTH = 5;
 
     private final String carNames;
     private final int tryCount;
@@ -20,15 +18,6 @@ public class RaceController {
     public RaceController(String carNames, int tryCount) {
         this.carNames = carNames;
         this.tryCount = tryCount;
-    }
-
-    private void isValidName(String name) {
-        if(name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException(
-                    "이름은 최소 "
-                            + MINIMUM_NAME_LENGTH + "자에서 최대 "
-                            + MAXIMUM_NAME_LENGTH + "자 사이로 입력하셔야 합니다.");
-        }
     }
 
     private void isValidEntryCount(String[] entries) {
@@ -41,9 +30,6 @@ public class RaceController {
         String[] entries = carNames.split(SEPARATOR);
 
         isValidEntryCount(entries);
-        for (String name : entries) {
-            isValidName(name);
-        }
 
         return new Cars(Arrays.stream(entries)
                 .map(Car::new)
--- src/main/java/racingcar/Domain/Car.java
+++ src/main/java/racingcar/Domain/Car.java
@@ -4,12 +4,24 @@ public class Car {
     private static final int MOVE_THRESHOLD = 4;
     private static final int START_POSITION = 0;
     private static final int TICK = 1;
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
     private final String name;
     private int position;
 
     public Car(String name) {
+        isValidName(name);
         this.name = name;
         this.position = START_POSITION;
+    }
+
+    private void isValidName(String name) {
+        if(name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(
+                    "이름은 최소 "
+                            + MINIMUM_NAME_LENGTH + "자에서 최대 "
+                            + MAXIMUM_NAME_LENGTH + "자 사이로 입력하셔야 합니다.");
+        }
     }
 
     public void moveForward() {
