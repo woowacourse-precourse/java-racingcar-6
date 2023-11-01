93ec5191cc323f12fa1d02d0c8cf9595ce05b201
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 16:39:45 +0900

    feat : 자동차 이름의 길이가 1~5를 벗어나는 경우 예외 처리 추가

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -3,6 +3,8 @@ package racingcar;
 import java.util.List;
 
 public class InputValidator {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
     public boolean isSingleCarInput(List<String> carNames) {
         return carNames.size() < 2;
     }
@@ -10,6 +12,14 @@ public class InputValidator {
     public void checkForWhitespace(List<String> carNames) {
         for (String name : carNames) {
             if (name.contains(" ")) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void checkNameLength(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException();
             }
         }
