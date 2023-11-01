d31ea27662655c3367fb226af38ef8a9c8d2c14a
leejmzz <leejmzz@naver.com>
2023-11-01 23:03:31 +0900

    레이싱

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -14,13 +14,12 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 전진_정지() {
-        assertRandomNumberInRange
-    Test(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
