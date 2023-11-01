feeced51fcd8ea8656330fb108b7e0b7686cc380
DavinAn <blueapple1030@gmail.com>
2023-11-01 22:34:13 +0900

    refactor : 무작위 숫자를 리스트 형식으로 리턴하도록 수정

--- src/main/java/racingcar/RacingcarRandomNumber.java
+++ src/main/java/racingcar/RacingcarRandomNumber.java
@@ -2,9 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingcarRandomNumber {
 
-  public int generate() {
-    return Randoms.pickNumberInRange(0,9);
+  public List<Integer> generate(int numOfCars) {
+    List<Integer> randomNumList = new ArrayList<>();
+    while (numOfCars > 0) {
+      randomNumList.add(Randoms.pickNumberInRange(0, 9));
+      numOfCars--;
+    }
+    return randomNumList;
   }
 }
