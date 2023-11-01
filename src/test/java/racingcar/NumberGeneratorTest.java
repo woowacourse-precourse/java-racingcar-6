63f660733aa5b62b08055b2d620c688831d379a6
psrom <psrom721@gmail.com>
2023-11-01 16:03:02 +0900

    feat(test): 랜덤 값 생성 테스트

---
+++ src/test/java/racingcar/NumberGeneratorTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.NumberGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NumberGeneratorTest {
+
+    @Test
+    void testGenerateRandomNumberInRange() {
+        for (int i = 0; i < 100; i++) {
+            int randomNumber = NumberGenerator.generateRandomNumber();
+            assertTrue(randomNumber >= 0 && randomNumber <= 9, "0부터 9 사이의 숫자가 아닙니다.");
+        }
+    }
+}
