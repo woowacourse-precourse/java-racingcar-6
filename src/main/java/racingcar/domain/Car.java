4d18736b7c0fbfba4f670df259aec3236f492bdd
Lee Yeon Su <yslee0540@gmail.com>
2023-11-01 15:13:02 +0900

    refactor(Car): 랜덤 숫자 생성 함수 분리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private final String name;
     private int position = 0;
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+    public static final int FORWARD_NUM = 4;
 
     public Car(String name) {
         this.name = name;
@@ -19,9 +22,12 @@ public class Car {
     }
 
     public void move() {
-        int num = Randoms.pickNumberInRange(0, 9);
-        if (num >= 4) {
+        if (getRandomNum() >= FORWARD_NUM) {
             position++;
         }
+    }
+
+    private int getRandomNum() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
     }
 }
