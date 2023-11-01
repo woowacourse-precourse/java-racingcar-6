ee8f4608aef7cfafa73ff724406a4ee65a447585
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:08:43 +0900

    test(NameTest): Name 객체 생성 단위 테스트 메소드 추가
    1. 유효한_이름으로_Name_객체_생성
    
    길이가 1이상 5이하이고 앞 또는 뒤에 공백 문자를 포함하지 않는 문자열로 Name 객체를 생성합니다.
    
    2. 유효하지_않은_문자열로_Name_객체_생성하면_실패
    
     - 빈 문자열
     - 앞 또는 뒤에 각종 공백 문자 포함
     - 5글자 초과
    
    위의 경우에 객체 생성 시 IllegalArgumentException이 발생해야한다.

--- src/test/java/racingcar/model/NameTest.java
+++ src/test/java/racingcar/model/NameTest.java
@@ -2,6 +2,54 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class NameTest {
 
+    @Test
+    void 유효한_이름으로_Name_객체_생성() {
+        try {
+            Name n1 = new Name("!");
+            Name n2 = new Name("Van");
+            Name n3 = new Name("픽업 트럭");
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void 유효하지_않은_문자열로_Name_객체_생성하면_실패() {
+        // 빈 문자열
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("");
+        });
+        
+        // 앞 또는 뒤에 공백 문자 포함
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name(" ");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("\t");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("\n");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("\r");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name(" a");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("a ");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name(" a a ");
+        });
+        
+        // 5글자 초과
+        assertThrows(IllegalArgumentException.class, () -> {
+            Name n1 = new Name("123456");
+        });
+    }
 }
