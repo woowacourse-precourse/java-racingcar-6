3c44c400757d5d1ba2da8354888f6f20a80b8b24
jongchan0109 <jongchan1009@naver.com>
2023-11-01 13:57:04 +0900

    Feat: 리스트에서 가장 많이 움직인 위치를 찾는 기능을 구현

--- src/main/java/racingcar/model/Judgement.java
+++ src/main/java/racingcar/model/Judgement.java
@@ -1,9 +1,18 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Judgement {
-    public List<Car> findWinner(List<Car> carList){
-        return null;
+
+    public int findMaxLocation(List<Car> carList) {
+        int maxValue = 0;
+        for (Car car : carList) {
+            int carLocation = car.getLocation();
+            if (carLocation > maxValue) {
+                maxValue = carLocation;
+            }
+        }
+        return maxValue;
     }
 }
