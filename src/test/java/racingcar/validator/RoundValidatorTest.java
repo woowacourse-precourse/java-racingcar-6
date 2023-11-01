3402e2f237ba7533ac46dadb879448dbf90caa17
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-01 15:12:38 +0900

    test: 사용자 입력 라운드 반환 테스트
    util의 setRepeatCount가 잘 반환 되는 지 확인

--- src/test/java/racingcar/validator/RoundValidatorTest.java
+++ src/test/java/racingcar/validator/RoundValidatorTest.java
@@ -3,8 +3,11 @@ package racingcar.validator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.util.GameUtil;
+import racingcar.view.InputView;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RoundValidatorTest {
     private RoundValidator roundValidator;
@@ -33,5 +36,12 @@ public class RoundValidatorTest {
         assertThatThrownBy(() -> new RoundValidator(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NUMBER_WILL_MORE_THAN_ONE);
+    }
+
+    @Test
+    void 사용자_입력_라운드_반환_테스트() {
+        String input = "3";
+        int repeatNum = GameUtil.setRepeatCount(input);
+        assertThat(repeatNum).isEqualTo(3);
     }
 }
