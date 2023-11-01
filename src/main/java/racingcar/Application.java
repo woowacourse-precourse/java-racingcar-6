3bed73026e2c3bb88400d4dcaa889464ac5fe81a
Lee Geun Woo <gamebdy@naver.com>
2023-11-01 12:28:43 +0900

    :sparkles: 캡슐화 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,17 +8,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 class Cars {
-    public final List<String> carNames;
-    public final List<Integer> carNumbers;
+    private final List<String> carNames;
+    private final List<Integer> carDistance;
 
 
     public Cars() {
         carNames = new ArrayList<>();
-        carNumbers = new ArrayList<>();
+        carDistance = new ArrayList<>();
 
     }
 
-    public void addCar(String carName) {
+    public void SetCarNames(String carName) {
         String[] InputArray = carName.split(",");
         for (String car : InputArray) {
             if (car.length() > 5)
@@ -27,30 +27,31 @@ class Cars {
         }
     }
 
-    public void resetBar() {
+    public void SetCarDistance() {
         for (int i = 0; i < carNames.size(); i++) {
-            carNumbers.add(0);
+            carDistance.add(0);
         }
     }
 
     public void addBar(int ListNum) {
-        carNumbers.set(ListNum, carNumbers.get(ListNum) + 1);
+        carDistance.set(ListNum, carDistance.get(ListNum) + 1);
     }
 
     public List<String> getCarNames() {
         return carNames;
     }
 
-    public List<Integer> getCarNumbers() {
-        return carNumbers;
+    public List<Integer> getCarDistance() {
+        return carDistance;
     }
+
 }
 
 public class Application {
 
-    public static int moves;
+    public static int Total_Moves;
 
-    public static int max_move;
+    public static int Max_Distance;
 
     private static final Cars cars = new Cars();
 
@@ -69,8 +70,8 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String InputCars = Console.readLine();
-        cars.addCar(InputCars);
-        cars.resetBar();
+        cars.SetCarNames(InputCars);
+        cars.SetCarDistance();
 
     }
 
@@ -78,15 +79,16 @@ public class Application {
         try {
             System.out.println("시도할 회수는 몇회인가요?");
             String InputMoves = Console.readLine();
-            moves = Integer.parseInt((InputMoves));
+            Total_Moves = Integer.parseInt((InputMoves));
         } catch (Exception e) {
             throw new IllegalArgumentException("You should input integer");
         }
     }
 
     public static void MoveCars() {
-        for (int i = 0; i < moves; i++) {
-            for (int j = 0; j < cars.carNames.size(); j++) {
+        List<String> MoveCar = cars.getCarNames();
+        for (int i = 0; i < Total_Moves; i++) {
+            for (int j = 0; j < MoveCar.size(); j++) {
                 GoOrStop(j);
                 PrintResult(j);
             }
@@ -102,8 +104,11 @@ public class Application {
     }
 
     public static void PrintResult(int Listnum) {
-        System.out.print(cars.carNames.get(Listnum) + " : ");
-        for (int i = 0; i < cars.carNumbers.get(Listnum); i++) {
+        List<String> nameResult = cars.getCarNames();
+        List<Integer> distanceResult = cars.getCarDistance();
+
+        System.out.print(nameResult.get(Listnum) + " : ");
+        for (int i = 0; i < distanceResult.get(Listnum); i++) {
             System.out.print("-");
         }
         System.out.println();
@@ -116,19 +121,23 @@ public class Application {
     }
 
     public static void CheckMax() {
-        for (int i = 0; i < cars.carNumbers.size(); i++) {
-            if (max_move < cars.carNumbers.get(i)) {
-                max_move = cars.carNumbers.get(i);
+        List<Integer> Check = cars.getCarDistance();
+        for(int Distance : Check){
+            if (Max_Distance < Check.get(Distance)) {
+                Max_Distance = Check.get(Distance);
             }
         }
     }
 
     public static void PrintWinner() {
         boolean SingleWinner = true;
-        for (int i = 0; i < cars.carNumbers.size(); i++) {
-            if (max_move == cars.carNumbers.get(i)) {
+        List<String> WinnerName = cars.getCarNames();
+        List<Integer> WinnerDistance = cars.getCarDistance();
+
+        for (int i = 0; i < WinnerDistance.size(); i++) {
+            if (Max_Distance == WinnerDistance.get(i)) {
                 if (!SingleWinner) System.out.print(", ");
-                System.out.print(cars.carNames.get(i));
+                System.out.print(WinnerName.get(i));
                 SingleWinner = false;
             }
         }
