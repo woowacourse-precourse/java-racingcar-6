7b4f4ede999e3a2f8b037523261205e26d7348cd
cheolhyeon <tghy4@naver.com>
2023-11-01 17:11:36 +0900

    refactor :

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,14 +1,17 @@
-package racingcar;
+package racingcar.car;
+
+import static racingcar.constants.RacingGameConstants.MIN_VALUE_FOR_MOVE;
 
 public class Car {
     private String name;
     private int currentPosition;
 
+
     public Car(String name) {
         this.name = name;
     }
     public void move(int numberToMove) {
-        if (numberToMove > 3) {
+        if (numberToMove > MIN_VALUE_FOR_MOVE) {
             currentPosition++;
         }
     }
