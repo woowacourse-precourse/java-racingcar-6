d745fb76345f90f17825bee850be96f2130b7f2e
mk3058 <kmk3058@icloud.com>
2023-11-01 10:39:59 +0900

    refactor(OutputView): 불필요한 메서드 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,15 +1,10 @@
 package racingcar.view;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import racingcar.model.dto.GameStatus;
 import racingcar.model.dto.GameResult;
+import racingcar.model.dto.GameStatus;
 
 public class OutputView {
-
-    public static void printStatus(GameStatus status) {
-        OutputView.printPosition(status.name(), status.position());
-    }
 
     public static void printWinner(GameResult result) {
         List<String> winnersList = result.winners();
@@ -19,7 +14,10 @@ public class OutputView {
         System.out.println(winners);
     }
 
-    private static void printPosition(String name, Long position) {
+    public static void printStatus(GameStatus status) {
+        String name = status.name();
+        Long position = status.position();
+
         System.out.print(name + " : ");
         for (int i = 0; i < position; i++) {
             System.out.print('-');
