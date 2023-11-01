95ab678168736c60b02ec259d4ce477a134dad6c
JiEung2 <https://github.com/JiEung2>
2023-11-01 13:11:09 +0900

    Test: Car와 Cars에 대한 테스트 구현
    인터페이스 추가를 통한 테스트 코드 구현
    자동차가 잘움직이는지, 라운드 결과가 잘 출력되는데, 우승자가 잘 출력되는지, 여러명의 우승자를 잘 출력하는지 테스트 코드 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,49 +3,94 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.utils.FixNumberGenerator;
+import racingcar.utils.NumberGenerator;
 
 
 public class CarsTest {
     Car pobi = new Car("pobi");
     Car woni = new Car("woni");
     Car jun = new Car("jun");
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-    @Test
-    void 공동우승자를_찾을_수_있는지_테스트() {
-        //given
-        pobi.move(4);
-        woni.move(5);
+    @BeforeEach
+    void setUpOutputStream() {
+        System.setOut(new PrintStream(output));
+    }
 
-        Cars cars = new Cars(List.of(pobi, woni, jun));
-        //when
-        List<String> winner = cars.findWinners();
-        //then
-        assertThat(winner).isEqualTo(List.of("pobi", "woni"));
+    @AfterEach
+    void restoreOutputStream() {
+        System.setOut(System.out);
+        output.reset();
     }
 
     @Test
-    void 단독우승자를_찾을_수_있는지_테스트() {
+    void 자동차가_잘움직이는지() {
         //given
-        pobi.move(4);
-        woni.move(5);
-        woni.move(5);
-        woni.move(6);
-        jun.move(4);
-        jun.move(2);
-
+        final NumberGenerator numberGenerator = new FixNumberGenerator();
+        //when
+        pobi.move(numberGenerator);
+        pobi.move(numberGenerator);
+        //then
+        assertThat(pobi).extracting("position").isEqualTo(2);
+    }
+    
+    @Test
+    void 라운드_결과가_잘출력되는지() {
+        //given
+        final NumberGenerator numberGenerator = new FixNumberGenerator();
         Cars cars = new Cars(List.of(pobi, woni, jun));
         //when
-        List<String> winner = cars.findWinners();
+        pobi.move(numberGenerator);
+        woni.move(numberGenerator);
+        jun.move(numberGenerator);
+        cars.getRoundResult();
         //then
-        assertThat(winner).isEqualTo(List.of("woni"));
+        assertThat(output.toString().trim()).isEqualTo("pobi : -\nwoni : -\njun : -");
+    }
+
+    @Test
+    void 우승자를_잘출력하는지() {
+        //given
+        final NumberGenerator numberGenerator = new FixNumberGenerator();
+        Cars cars = new Cars(List.of(pobi, woni, jun));
+        //when
+        pobi.move(numberGenerator);
+        pobi.move(numberGenerator);
+        woni.move(numberGenerator);
+        jun.move(numberGenerator);
+        cars.findWinners();
+        //then
+        assertThat(output.toString().trim()).isEqualTo("최종 우승자 : pobi");
+    }
+
+    @Test
+    void 여러명의_우승자를_출력하는지() {
+        //given
+        final NumberGenerator numberGenerator = new FixNumberGenerator();
+        Cars cars = new Cars(List.of(pobi, woni, jun));
+        //when
+        pobi.move(numberGenerator);
+        woni.move(numberGenerator);
+        jun.move(numberGenerator);
+        cars.findWinners();
+        //then
+        assertThat(output.toString().trim()).isEqualTo("최종 우승자 : pobi, woni, jun");
     }
 
     @Test
     void 등록된_자동차가_없을_때_예외() {
+        //given
         Cars cars = new Cars(List.of());
+        //when
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> cars.findWinners());
+        //then
         assertThat(e.getMessage()).isEqualTo("차량 리스트가 비었습니다.");
     }
 
