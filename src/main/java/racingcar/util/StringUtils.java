88c3dbd834e0dafbf395e08774137b4331e3fdd2
minisyu <iswater.kim@gmail.com>
2023-11-01 21:08:38 +0900

    style(StringUtils): COMMMA -> COMMA 오타 수정

--- src/main/java/racingcar/util/StringUtils.java
+++ src/main/java/racingcar/util/StringUtils.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class StringUtils {
     private static final String DISTANCE = "-";
-    private static final String COMMMA = ",";
+    private static final String COMMA = ",";
 
     public static String generateDashString(int count) {
         return DISTANCE.repeat(count);
     }
 
     public static List<String> splitStringToList(String str) {
-        return Arrays.stream(str.split(COMMMA))
+        return Arrays.stream(str.split(COMMA))
                 .toList();
     }
 }
