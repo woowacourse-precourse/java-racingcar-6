d95ad30b90a2f6fb70e02361116893675e40ad67
yeahdy <yejine98@naver.com>
2023-11-01 12:42:15 +0900

    feat(validateTryCount()): 시도 횟수 유효성 검사

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
      * - 5자 이하 인지
      *
      * @param carNames
-     * @return true or false
+     * @return 예외를 발생 시키지 않으면 true 리턴
      */
     public boolean validateCarNames(String carNames){
         //TODO. 숫자만 있으면 안되는건가?
@@ -36,6 +36,25 @@ public class InputView {
         return true;
     }
 
-    //TODO. 시도 횟수 유효성 검사()
+
+    /**
+     * 시도 횟수 유효성 검사
+     * - 숫자 외의 문자가 있을 경우 예외 발생
+     * - int 정수 최대 범위를 초과할 경우 예외 발생
+     *
+     * @param tryCount
+     * @return 예외를 발생 시키지 않으면 true 리턴
+     */
+    public boolean validateTryCount(String tryCount){
+        final String REGEX = "[0-9]+";
+
+        if(!tryCount.matches(REGEX)) {
+            throw new IllegalArgumentException(ErrorCodeConstant.NUMBER_VALIDATION_ERROR);
+        } else if(Integer.parseInt(tryCount) > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(ErrorCodeConstant.MAX_NUMBER_ERROR);
+        }
+
+        return true;
+    }
 
 }
