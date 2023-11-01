9dc18914ee4f3ec52b6815cce17b0f7f2ebfaf85
Jin409 <sualng123@gmail.com>
2023-11-01 14:40:38 +0900

    [TEST] 경기 결과 판단 및 출력 관련 테스트 추가

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -3,10 +3,13 @@ package racingcar;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.Referee;
+import racingcar.domain.Winners;
 import racingcar.utils.Parser;
 
 public class GameTest {
@@ -45,14 +48,14 @@ public class GameTest {
     }
 
     @Test
-    void 시도_횟수가_숫자가_아닌_경우_예외가_던져지는지() {
+    void 시도_횟수가_숫자가_아닌_경우_예외_발생() {
         String invalidInput = "not_number";
 
         assertThrows(IllegalArgumentException.class, () -> Parser.stringToInteger(invalidInput));
     }
 
     @Test
-    void 자동차가_한_칸_전진할_경우_위치가_증가하는지() {
+    void 자동차가_한_칸_전진할_경우_위치_증가() {
         Car car = new Car("name");
         car.moveForward();
         int expected = 1;
@@ -60,4 +63,52 @@ public class GameTest {
         assertEquals(expected, car.getLocation());
 
     }
+
+    @Test
+    void 우승자가_여러명일_경우_여러명_출력() {
+        String nameOfFirstWinner = "one";
+        String nameOfSecondWinner = "two";
+        String nameOfNotWinner = "three";
+
+        //given
+        Car firstWinner = new Car(nameOfFirstWinner);
+        Car secondWinner = new Car(nameOfSecondWinner);
+        Car notWinner = new Car(nameOfNotWinner);
+
+        List<Car> cars = List.of(firstWinner, secondWinner, notWinner);
+
+        firstWinner.moveForward();
+        secondWinner.moveForward();
+
+        //when
+        Referee referee = new Referee();
+        Winners winners = referee.findWinners(cars);
+
+        //then
+        assertTrue(winners.toString().contains(nameOfFirstWinner) && winners.toString().contains(nameOfSecondWinner)
+                && !winners.toString().contains(nameOfNotWinner));
+    }
+
+    @Test
+    void 우승자가_한명일_경우_한명_출력() {
+        String nameOfWinner = "one";
+        String nameOfNotWinner = "two";
+
+        //given
+        Car winner = new Car(nameOfWinner);
+        Car notWinner = new Car(nameOfNotWinner);
+
+        List<Car> cars = List.of(winner, notWinner);
+
+        winner.moveForward();
+
+        //when
+        Referee referee = new Referee();
+        Winners winners = referee.findWinners(cars);
+
+        //then
+        assertTrue(winners.toString().contains(nameOfWinner)
+                && !winners.toString().contains(nameOfNotWinner));
+    }
+
 }
