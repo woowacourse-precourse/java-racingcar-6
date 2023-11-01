fa8fae39549b310082d18c2a8b36313ca2ff7254
Isaac Han <isaachan233@gmail.com>
2023-11-01 15:17:26 +0900

    test: testCode 실습한거

--- src/test/java/study/ArrayListTest.java
+++ src/test/java/study/ArrayListTest.java
@@ -1,5 +1,7 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,24 +10,77 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArrayListTest {
+
+    private List<String> list;
+    private List<String> list2;
+
+    @BeforeEach
     @Test
-    public void arrayList_단위테스트() {
-        List<String> list = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+    void init() {
+        list = new ArrayList<>();
+        list2 = new ArrayList<>();
         list.add("1");
         list.add("2");
         list2.add("new");
         list2.add("word");
-        assertThat(list.add("3")).isEqualTo(true);
-        assertThat(list.size()).isEqualTo(3);
-        assertThat(list.contains("4")).isEqualTo(false);
-        assertThat(list.get(0)).isEqualTo("1");
-        assertThat(list.set(0, "5")).isEqualTo("1");
-        assertThat(list.remove(0)).isEqualTo("5");
-        assertThat(list.size()).isEqualTo(2);
-        assertThat(list.addAll(1, list2)).isEqualTo(true);
-        assertThat(list.hashCode() == list.hashCode()).isEqualTo(true);
-        assertThat(list.hashCode() == list2.hashCode()).isEqualTo(false);
-        assertThat(list.toArray()).isEqualTo(new String[]{"2", "new", "word", "3"});
     }
+
+    @DisplayName("ArrayList 추가")
+    @Test
+    void arrayList_add_test() {
+        assertThat(list.add("3")).isEqualTo(true);
+    }
+
+    @DisplayName("리스트 size 테스트")
+    @Test
+    void arrayList_size_test() {
+        assertThat(list.size()).isEqualTo(2);
+    }
+
+    @DisplayName("리스트 get 테스트")
+    @Test
+    void arrayList_get_test() {
+        assertThat(list.get(0)).isEqualTo("1");
+    }
+
+    @DisplayName("리스트 contains 테스트")
+    @Test
+    void arrayList_contains_test() {
+        assertThat(list.contains("4")).isEqualTo(false);
+    }
+
+
+    @DisplayName("리스트 set 테스트")
+    @Test
+    void arrayList_set_test() {
+        assertThat(list.set(0, "5")).isEqualTo("1");
+    }
+
+    @DisplayName("리스트 remove 테스트")
+    @Test
+    void arrayList_remove_test() {
+        assertThat(list.remove(0)).isEqualTo("1");
+    }
+
+    @DisplayName("리스트 addall 테스트")
+    @Test
+    void arrayList_allAll_test() {
+        assertThat(list.addAll(1, list2)).isEqualTo(true);
+    }
+
+    @DisplayName("리스트 hash 테스트")
+    @Test
+    void arrayList_hash_test() {
+        assertThat(list.hashCode() == list.hashCode()).isTrue();
+        assertThat(list.hashCode() == list2.hashCode()).isFalse();
+    }
+
+    @DisplayName("리스트 toArray 테스트")
+    @Test
+    void arrayList_toArray_test() {
+        list.addAll(1,list2);
+        assertThat(list.toArray()).isEqualTo(new String[]{"1", "new", "word", "2"});
+    }
+
+
 }
