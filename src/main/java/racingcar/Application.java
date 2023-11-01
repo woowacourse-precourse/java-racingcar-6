05b5c673c7dbf8caf3c76701fbcb6797f5c75b41
JCH <ckdgusjang@naver.com>
2023-11-01 19:47:11 +0900

    feat : Car클래스의 random값 계산 메소드를 이용한 실행결과 출력 및 주석 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,19 +14,32 @@ public class Application {
         int Count;
 
         try {
+            //자동차들의 이름 입력
             System.out.println("경주할 자동차 이름을 입력하시오.(이름은 쉽표(,) 기준으로 구분)");
             String CarName = Console.readLine();
+            //미입력 예외처리
             if (CarName.length() == 0) {
                 throw new Exception("아무런값을 입력하지 않으셨습니다.");
             }
+            //쉽표를 기준으로 자동차 객체 생성
             StringTokenizer st = new StringTokenizer(CarName, ",");
             while (st.hasMoreTokens()) {
                 String car_name = st.nextToken();
                 check_carName(car_name);
                 cars.add(new Car(car_name, 0));
             }
+
+            //반복횟수 입력
             System.out.println("시도할 회수는 몇회인가요?");
             Count = Integer.parseInt(Console.readLine());
+
+            //실행결과 출력
+            System.out.println("실행결과");
+            for (int i = 0; i < Count; i++) {
+                //실행결과 함수 호출
+                exec_result(cars);
+            }
+
         } catch (Exception e) {
             throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
@@ -37,5 +50,14 @@ public class Application {
         if (car_name.length() > 5) {
             throw new Exception("자동차의 이름이 5글자를 넘었습니다.");
         }
+    }
+
+    //실행결과 출력함수
+    public static void exec_result(List<Car> cars) {
+        for (Car car : cars) {
+            car.count_Random();
+            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+        }
+        System.out.println();
     }
 }
