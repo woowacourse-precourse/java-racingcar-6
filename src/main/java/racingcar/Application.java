eec4267c4ad3e07e9cd8b36fa3075491cca8a32f
Kim-jihoo <jihoo1106@ewhain.net>
2023-11-01 21:21:38 +0900

    feat: 실행 동작 구현
    - 무작위 값 중 4 이상일 때 전진
    - 전진할 때마다 - 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,39 +1,63 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SplittableRandom;
+
 
 public class Application {
     public static void main(String[] args) {
+        List<String> players =  playerName();
+        int playTimes = playTime();
+        playGame(players, playTimes);
+    }
+    public static void playGame(List<String> players, int playTimes){
+        System.out.println("실행 결과");
+        List<String> results = playerResults(players);
 
-    playeGame();
+        for (int round =0; round<playTimes; round++) {
+           roundResults(players, results);
+           System.out.println();
+        }
 
     }
-    public static void playeGame(){
-        while (true){
-            playerName();
-            getplayTime();
-
-            break;
+    public static List<String> playerResults(List<String> players){
+        List<String> results = new ArrayList<>();
+       for (int i=0; i< players.size(); i++){
+           results.add("");
+       }
+        return results;
+    }
+    public static void roundResults(List<String> players, List<String> results){
+        for (int i=0; i<players.size(); i++) {
+            int move = Randoms.pickNumberInRange(0, 9);
+            String result;
+            if (move >= 4) {
+                result = "-";
+            } else {
+                result = "";
+            }
+            results.set(i, results.get(i) + result);
+            System.out.println(players.get(i) + " : " + results.get(i));
         }
     }
     public static List<String> playerName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String > player = new ArrayList<>();
+        List<String > players = new ArrayList<>();
         String input = Console.readLine();
         String[] names = input.split(",");
-        for (String name : names) {
+        for (int i=0; i<names.length; i++) {
+            String name = names[i];
             if (name.length() > 5) {
                 throw new IllegalArgumentException("5자 이내로 입력해주세요");
             }
-            player.add(name);
+            players.add(name);
         }
-        return player;
+        return players;
     }
-    public static int getplayTime(){
+    public static int playTime(){
         System.out.println("시도할 횟수는 몇회인가요?");
         int playTimes;
         try{
