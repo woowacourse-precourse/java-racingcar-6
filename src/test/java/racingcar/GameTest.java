f9ec08e76bdda296b21edfa3dbda536ed2e2d82b
HongYeseul <ghd8119@naver.com>
2023-11-01 19:21:15 +0900

    :white_check_mark: test(GameTest) 자동차 이름을 5자 이상 입력시 예외 발생

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,4 +1,24 @@
 package racingcar;
 
-public class GameTest {
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameTest extends NsTest {
+    @Test
+    @DisplayName("자동차 이름을 5글자 이상 입력 할 경우 예외 발생")
+    void inputOverThenFiveLetters() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,yeseul", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
