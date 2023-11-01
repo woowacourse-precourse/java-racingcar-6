b58b0120a4ae2dd264085aa8f3be228c8eea4792
민경 <tjalsrud0101@gmail.com>
2023-11-01 15:35:09 +0900

    feat: 우승자 출력

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -51,4 +51,8 @@ public class Car {
     public void addTotalForward() {
         totalForward += forwardValue;
     }
+
+    public int getTotalForward() {
+        return totalForward;
+    }
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
@@ -15,6 +16,47 @@ public class Race {
             setForwardValues();
             printRaceResult();
         }
+        getWinners();
+    }
+
+    private void getWinners() {
+        int max = getMaxTotalForward();
+        List<Car> winners = findWinners(max);
+        printWinners(winners);
+    }
+
+    private void printWinners(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        if(winners.size() == 1) {
+            System.out.println(winners.get(0).getName());
+        }
+        else {
+            String winnerName = "";
+            for(Car winner : winners) {
+                winnerName += winner.getName() + ", ";
+            }
+            System.out.println(winnerName.substring(0, winnerName.length()-2));
+        }
+    }
+
+    private List<Car> findWinners(int max) {
+        List<Car> winners = new ArrayList<>();
+        for(Car car : cars) {
+            if(car.getTotalForward() == max) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxTotalForward() {
+        int max = 0;
+        for(Car car : cars) {
+            if(car.getTotalForward() > max) {
+                max = car.getTotalForward();
+            }
+        }
+        return max;
     }
 
     private void printRaceResult() {
