aaefd6fdf7aca28c0e05beca21f0e9943a1429fe
gyahn <pop2413@gmail.com>
2023-11-01 21:14:18 +0900

    fix: 우승자 출력 변경

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Winner {
 
-    private static String WinnerToString(Set<String> winner){
+    private static String WinnerToString(ArrayList<String> winner){
         String winners = "";
         int size = winner.size();
         int i = 0;
@@ -21,15 +21,16 @@ public class Winner {
         return winners;
     }
     public void PrintWinner(Car[] cars){
-        Set<String> winner = new HashSet<>();
+//        Set<String> winner = new HashSet<>();
 //        String winnerNm = "";
-
+        ArrayList<String> winner = new ArrayList<>();
+//        String[] winner;
         int maxPosition = 0;
 
         for(Car car: cars) {
             if (maxPosition == car.ReturnMove()) {
                 winner.add(car.ReturnCarName());
-                maxPosition += car.ReturnMove();
+//                maxPosition += car.ReturnMove();
             } else if (maxPosition < car.ReturnMove()) {
                 winner.clear();
                 winner.add(car.ReturnCarName());
