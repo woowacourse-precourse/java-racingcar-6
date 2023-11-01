78261a23dc26dfa51bc6220ee938c3929cb5404f
chanwoo <mdsoo55828@gmail.com>
2023-11-01 19:10:56 +0900

    Test: 자동차 이름 길이 예외처리 테스트 수정

--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -13,11 +13,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidatorTest {
 
     @Test
-    void 자동차_이름_길이_예외_처리() {
-        String inputName = "pobiab";
+    void 자동차_이름_길이_예외처리() {
+        List<String> carNameList = new ArrayList<>();
+        carNameList.add("abcd");
+        carNameList.add("abcde");
+        carNameList.add("abcdef");
         assertThatThrownBy(() -> {
-            Validator.InputRaceCount(inputName);
+            Validator.InputCarName(carNameList);
         }).isInstanceOf(IllegalArgumentException.class);
     }
+
+
+
 
 }
