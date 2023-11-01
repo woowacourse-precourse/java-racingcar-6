7a28aa75fafd7aac733e490a0d2afa0a888b7e92
GO-TE <alsrudxo9810@naver.com>
2023-11-01 11:09:06 +0900

    feat(GameController) : 레이싱 시작, 로직 구현 startGame() : - 게임 시작하는 메소드 initializeGame() : -게임의 전체적인 흐름 메소드

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,2 +1,33 @@
-package racingcar;public class GameController {
+package racingcar;
+
+import java.util.ArrayList;
+
+public class GameController {
+    private final int START = 0;
+    private int executionTime;
+    Car car = new Car();
+    RandomNumber randomNumber = new RandomNumber();
+    InputData inputData = new InputData();
+    OutputGame outputGame = new OutputGame();
+
+    public void startGame() {
+        initializeGame();
+    }
+
+    private void initializeGame() {
+        car.initializeCar();;
+        inputData.inputExecutionTime();
+        executionTime = inputData.returnExecutionTime();
+
+        outputGame.printGame();
+        for (int time = START; time < executionTime; time++) {
+            moveCarByTime();
+            outputGame.printRacing(car.getCarName(), car.getCarPosition());
+        }
+        displayResult();
+
+
+    }
+
+
 }
