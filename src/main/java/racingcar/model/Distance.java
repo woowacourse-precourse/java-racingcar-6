d971acf57ca6d2458b0389ca4c6e0076e176dafc
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 20:13:46 +0900

    feat(Distance): 속성 및 생성자 정의
    Distance 클래스는 자동차의 이동 거리를 나타내기 위해 int 값을 감싸는 래퍼 클래스이다.
    
    속성
     - int
    
    생성자
     - ( ): 0으로 초기화한다.
     - (int): 주어진 정수로 초기화한다. 단, 주어진 정수가 음수인 경우
    `IllegalArgumentException`을 발생시킨다.

--- src/main/java/racingcar/model/Distance.java
+++ src/main/java/racingcar/model/Distance.java
@@ -1,5 +1,25 @@
 package racingcar.model;
 
 public class Distance {
+    private int distance;
 
+    /**
+     * 거리는 0으로 설정됨
+     */
+    public Distance() {
+        this.distance = 0;
+    }
+
+    /**
+     * 주어진 음수가 아닌 정수로 거리를 생성한다.
+     * @param nonNegativeInteger 음이 아닌 정수
+     * @throws IllegalArgumentException nonNegativeInteger < 0
+     */
+    public Distance(int nonNegativeInteger) throws IllegalArgumentException {
+        if (nonNegativeInteger < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.distance = nonNegativeInteger;
+    }
 }
