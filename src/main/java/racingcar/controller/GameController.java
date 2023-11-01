09c19bc7adaa8e8acf8dcb30249c9afe0189c3e5
DonghaOh <donghadarwin@naver.com>
2023-11-01 17:35:32 +0900

    feat: 게임 진행 - gameStart 수정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,17 +2,24 @@ package racingcar.controller;
 
 import static racingcar.controller.InputController.nameInput;
 import static racingcar.controller.InputController.roundNumberInput;
+import static racingcar.view.OutputView.gameResultOutputPrint;
+import static racingcar.view.OutputView.gameRoundResultPrint;
 
 import java.util.List;
 import racingcar.model.CarList;
 
 public class GameController {
     public GameController(){
-
+        gameStart();
     }
     public void gameStart(){
         List<String> nameList = nameInput();
         int roundNumber = roundNumberInput();
         CarList carList = new CarList(nameList);
+        gameResultOutputPrint();
+        for (int i = 0; i < roundNumber; i++) {
+            carList.carListMove();
+            gameRoundResultPrint(carList);
+        }
     }
 }
