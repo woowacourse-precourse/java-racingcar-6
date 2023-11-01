7861a1cff9eb2aebea779d2a6de28af9ff470dee
dani <joyjoy312@naver.com>
2023-11-01 16:36:30 +0900

    feat: 자동차 목록과 시도 횟수를 입력받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args)  {
+
+        // 자동차와 시도 횟수를 입력받는 기능
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        StringTokenizer st = new StringTokenizer(Console.readLine(), ",");
+
+        Map<String,String> cars = new HashMap<>();
+        while (st.hasMoreTokens()) {
+            cars.put(st.nextToken(),"");
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int number = Integer.parseInt(Console.readLine());
+
     }
 }
