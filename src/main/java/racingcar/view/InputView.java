5dfd0cdaaa498e50e22ebc8b6840f8a68ecf4a80
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-01 17:43:16 +0900

    refactor : inputview 메소드 static 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputView {
-    public List<String> inputCarName(){
-        Scanner in = new Scanner(System.in); // Scanner 객체 생성
-        String playerString = in.nextLine();
+    public static List<String> inputCarName(){
+        //Scanner in = new Scanner(System.in); // Scanner 객체 생성
+        //String playerString = in.nextLine();
 
-        //String playerString = Console.readLine();
+        String playerString = Console.readLine();
         List<String> inputCarNameList = Stream.of(playerString.split(","))
                 .map(String::trim).collect(Collectors.toList());
 
