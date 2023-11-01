49381f23225d6f49109af25e51c17402925d5bf1
Isaac Han <isaachan233@gmail.com>
2023-11-01 11:47:22 +0900

    test: Cars 객체 테스트

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,78 +1,64 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.enums.Common;
+
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarsTest {
-    @Test
-    void 빈_입력_테스트() {
-        assertThatThrownBy(() -> new Car(",")).isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    void 중복된_입력_테스트() {
-        assertThatThrownBy(() -> new Car("isaac,isaac")).isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @Test
-    void Cars() {
-        new Cars("isaac,han");
-    }
+    private Cars cars;
+    private final String inputNames = "isaac,han";
 
+    private static final int MOVE_FORWARD = Common.STANDARD_NUMBER.getIntValue();
+    private static final int STOP = Common.STANDARD_NUMBER.getIntValue() - 1;
+
+    @BeforeEach
     @Test
-    void everyCarMoveForward_false() {
-        Cars cars = new Cars("isaac,han");
+    void init() {
+        cars = new Cars(inputNames);
+
         assertRandomNumberInRangeTest(() -> {
-            cars.everyCarMoveForward();
-            assertThat(cars.getCarList().stream().allMatch(car -> car.getDistance().equals("-"))).isFalse();
-        }, Common.STANDARD_NUMBER.getIntValue() - 1, Common.STANDARD_NUMBER.getIntValue());
+            cars.everyCarMoveForwardByRandom();
+            cars.everyCarMoveForwardByRandom();
+        }, MOVE_FORWARD, MOVE_FORWARD, MOVE_FORWARD, MOVE_FORWARD);
     }
 
     @Test
-    void everyCarMoveForward() {
-        Cars cars = new Cars("isaac,han");
+    void everyCarMoveForwardByRandomTest() {
+        Cars cars = new Cars(inputNames);
+
         assertRandomNumberInRangeTest(() -> {
-            cars.everyCarMoveForward();
-//            boolean flag = true;
-//            for(Car car : cars.getCarList()) {
-//                if(!car.getDistance().equals("-")) {
-//                    flag = false;
-//                    break;
-//                }
-//            }
-//            assertThat(flag).isTrue();
-            assertThat(cars.getCarList().stream().allMatch(car -> car.getDistance().equals("-"))).isTrue();
-        }, Common.STANDARD_NUMBER.getIntValue(), Common.STANDARD_NUMBER.getIntValue());
+            cars.everyCarMoveForwardByRandom();
+            cars.everyCarMoveForwardByRandom();
+            assertThat(cars.getCars().get(0).getDistance()).isEqualTo(2);
+            assertThat(cars.getCars().get(1).getDistance()).isEqualTo(0);
+        }, MOVE_FORWARD, STOP, MOVE_FORWARD, STOP);
+    }
+
+
+    @Test
+    void getCarsTest() {
+        assertThat(cars.getCars().stream().allMatch(car -> car.getDistance() == 2)).isTrue();
+        assertThat(cars.getCars().stream().map(car -> car.getName()).collect(Collectors.joining(","))).isEqualTo(inputNames);
     }
 
     @Test
-    void getCarList() {
-        String namesInput = "isaac,han";
-        Cars cars = new Cars(namesInput);
-//        boolean flag = true;
-//        for(Car car : cars.getCarList()) {
-//            if(!namesInput.contains(car.getName()))
-//                flag = false;
-//        }
-//        assertThat(flag).isTrue();
-        assertThat(cars.getCarList().stream().anyMatch(car -> namesInput.contains(car.getName()))).isTrue();
+    void getCarTest() {
+        String searchName = "isaac";
+        assertThat(cars.getCar(searchName).getName()).isEqualTo(searchName);
     }
 
     @Test
-    void getCar() {
-        String namesInput = "isaac,han";
-        Cars cars = new Cars(namesInput);
-        assertThat(namesInput.contains(cars.getCar("isaac").getName())).isTrue();
+    void getCarExceptionTest() {
+        String searchName = "isa";
+        assertThatThrownBy(() -> cars.getCar(searchName).getName()).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> cars.getCar(searchName).moveForwardByRandom()).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void getCar_예외() {
-        String namesInput = "isaac,han";
-        Cars cars = new Cars(namesInput);
-        assertThatThrownBy(() -> namesInput.contains(cars.getCar("isa").getName())).isInstanceOf(IllegalArgumentException.class);
-    }
 }
