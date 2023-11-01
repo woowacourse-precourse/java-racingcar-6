0dbc52b2a153e7bddad28289aa7debb0b1199d9e
jiwon1217 <koagjw@gmail.com>
2023-11-01 11:22:01 +0900

    :recycle: refactor: 문자열 상수 분리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,18 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputView {
+    private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_NAMES_DELIMITER = ",";
+    private static final String ASK_ROUND = "시도할 회수는 몇회인가요?";
+
     public static List<Car> inputCarsInfo() {
         printAskCarNames();
         return getCarNames();
     }
 
     private static void printAskCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(ASK_CAR_NAMES);
     }
 
     public static List<Car> getCarNames() {
         String input = Console.readLine();
-        String[] carNames = input.split(",");
+        String[] carNames = input.split(CAR_NAMES_DELIMITER);
 
         for (String carName : carNames) {
             Validator.validateCarName(carName);
@@ -31,7 +35,7 @@ public class InputView {
     }
 
     private static void printAskRound() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(ASK_ROUND);
     }
 
     public static int getRound() {
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -3,18 +3,24 @@ package racingcar;
 import java.util.List;
 
 public class OutputView {
+    private static final String EXECUTION_RESULT_TEXT = "실행 결과";
+    private static final String MOVE = "-";
+    private static final String FINAL_WINNERS = "최종 우승자";
+    private static final String DELIMITER = " : ";
+    private static final String WINNERS_DELIMITER = ", ";
+
     public static void printExecutionResultText() {
-        System.out.println("실행 결과");
+        System.out.println(EXECUTION_RESULT_TEXT);
     }
 
     public static void printRoundResult(Car car) {
-        System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        System.out.println(car.getName() + DELIMITER + MOVE.repeat(car.getPosition()));
         System.out.println();
     }
 
     public static void printWinners(List<Car> cars) {
         List<String> winners = getWinnersName(cars);
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        System.out.println(FINAL_WINNERS + DELIMITER + String.join(WINNERS_DELIMITER, winners));
     }
 
     private static List<String> getWinnersName(List<Car> cars) {
