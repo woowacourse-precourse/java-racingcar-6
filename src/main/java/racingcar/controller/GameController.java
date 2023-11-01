74362d0a4ce16942db19f791dabe95c845c653b2
lee-JunR <wns2610@icloud.com>
2023-11-01 18:11:53 +0900

    style : "주석 추가"

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -31,12 +31,20 @@ public class GameController {
     showWinners();
   }
 
+  /**
+   * 게임 시작 전, 사용자 입력을 받아 자동차 목록 초기화
+   */
   private void initializeGame() {
     List<String> carNames = input.inputCarNames();
     List<Car> cars = createCars(carNames);
     this.racingCars = new Cars(cars);
   }
 
+  /**
+   * 레이스에서 최종 우승자를 결정
+   *
+   * @return 레이스에서 우승한 차량 이름 목록
+   */
   private List<String> determineWinners() {
     List<String> winners = new ArrayList<>();
     int maxPosition = getMaxPosition();
@@ -48,6 +56,11 @@ public class GameController {
     return winners;
   }
 
+  /**
+   * 레이스에서 가장 멀리 이동한 차량의 위치를 반환
+   *
+   * @return 레이스에서 가장 멀리 이동한 차량의 위치
+   */
   private int getMaxPosition() {
     List<Integer> carPositions = racingCars.getCarPositions();
     int max = 0;
@@ -57,16 +70,25 @@ public class GameController {
     return max;
   }
 
+  /**
+   * 레이스에서 우승한 차량 목록을 출력
+   */
+  private void showWinners() {
+    List<String> winners = determineWinners();
+    output.printFinalWinners(winners);
+  }
+
+  /**
+   * 사용자 입력으로부터 받은 자동차 이름 목록을 Car 객체로 생성
+   *
+   * @param carNames 사용자 입력으로 받은 자동차 이름 목록
+   * @return Car 객체로 변환된 자동차 목록
+   */
   private List<Car> createCars(List<String> carNames) {
     List<Car> cars = new ArrayList<>();
     for (String name : carNames) {
       cars.add(new Car(name));
     }
     return cars;
-  }
-
-  private void showWinners() {
-    List<String> winners = determineWinners();
-    output.printFinalWinners(winners);
   }
 }
--- src/main/java/racingcar/controller/Round.java
+++ src/main/java/racingcar/controller/Round.java
@@ -4,6 +4,7 @@ import racingcar.domain.Cars;
 import racingcar.view.Output;
 
 public class Round {
+
   private final Cars racingCars;
   private final Output output;
 
@@ -12,6 +13,9 @@ public class Round {
     this.output = output;
   }
 
+  /**
+   * 라운드를 실행하고 차량의 이동 상황을 출력
+   */
   public void play() {
     racingCars.moveCars();
     output.printCurrentPositions(racingCars.getCarNames(), racingCars.getCarPositions());
