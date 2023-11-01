645ef0838759fca22b5dcdb804d3e2b3786a366e
JungJei <greteljds0251@gmail.com>
2023-11-01 20:24:20 +0900

    feat: Race 객체 생성 및 반환

---
+++ src/main/java/Model/Race.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private List<Car> cars;
+
+    public Race(List<String> names) {
+        cars = new ArrayList<>();
+        for (String name : names) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
