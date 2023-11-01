fd7d0ec165e0810bc8aef72252abc472a6066973
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 23:36:07 +0900

    test(RaceView): 자동차 이름에 대한 사용자 입력 테스트 작성
    성공 테스트1
    입력 "a,bc,def,ghij,klnmo,p q r,st u,v w,x yz" 에 대해
    ["a", "bc", "def", "ghij", "klnmo", "p q r", "st u", "v w", "x yz"] 의
    9개의 요소를 가진 List 타입 객체가 반환되는가?
    
    성공 테스트2
    입력 "abcd" 에 대해 ["abcd"] 의 1개의 요소를 가진 List 타입 객체가 반환되는가?
    
    실패 테스트1
    빈 문자열을 입력한 경우 IllegalArgumentException 예외를 발생시키는가?

--- src/test/java/racingcar/view/RaceViewTest.java
+++ src/test/java/racingcar/view/RaceViewTest.java
@@ -2,6 +2,54 @@ package racingcar.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class RaceViewTest {
 
+    @Test
+    void getCarNames_유효한_입력_테스트1() {
+        InputStream input = System.in;
+        String content = "a,bc,def,ghij,klnmo,p q r,st u,v w,x yz";
+        ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes());
+        System.setIn(in);
+
+        List<String> compare = new ArrayList<>();
+        compare.add("a");
+        compare.add("bc");
+        compare.add("def");
+        compare.add("ghij");
+        compare.add("klnmo");
+        compare.add("p q r");
+        compare.add("st u");
+        compare.add("v w");
+        compare.add("x yz");
+        assertEquals(compare, RaceView.getCarNames());
+    }
+
+    @Test
+    void getCarNames_유효한_입력_테스트2() {
+        InputStream input = System.in;
+        String content = "abcd";
+        ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes());
+        System.setIn(in);
+
+        List<String> compare = new ArrayList<>();
+        compare.add("abcd");
+        assertEquals(compare, RaceView.getCarNames());
+    }
+
+    @Test
+    void getCarNames에_빈_문자열_입력하면_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputStream input = System.in;
+            String content = "";
+            ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes());
+            System.setIn(in);
+            RaceView.getCarNames();
+        });
+    }
 }
