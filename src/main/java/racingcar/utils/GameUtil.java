c4a500dee20d5c5306fbd34c0a84005c96e998d5
이정민 <jmlee2705@gmail.com>
2023-11-01 22:05:14 +0900

    refractor(GameUtil, CarUtil): Car에서 쓰이는 GameUtil 메소드를 CarUtil로 이동

--- src/main/java/racingcar/utils/GameUtil.java
+++ src/main/java/racingcar/utils/GameUtil.java
@@ -1,6 +1,5 @@
 package racingcar.utils;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,17 +7,6 @@ public class GameUtil {
 
     public List<String> splitNamesByComma(String carNames) {
         return Arrays.stream(carNames.split(",")).toList();
-    }
-
-    public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
-    }
-
-    public boolean judgeGo(int number) {
-        if (number >= 4) {
-            return true;
-        }
-        return false;
     }
 
 }
