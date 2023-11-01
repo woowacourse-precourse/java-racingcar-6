5bedd30ea3e84e9024ae6e6f717800205b99dbba
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-01 15:36:34 +0900

    feat: 자동차 경주 게임 실행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,19 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+
+        racingGame.getCarNameInput();
+        int tryNumber = racingGame.getTryNumberInput();
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for(int i=0; i<tryNumber; i++) {
+            racingGame.eachResult();
+            racingGame.printEachResult();
+        }
+
+        racingGame.printFinalResult();
     }
 }
