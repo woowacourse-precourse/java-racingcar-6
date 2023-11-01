b86f3575e80b4536d9949c030d45dd658380905f
jagarchi <relong1026@naver.com>
2023-11-01 20:27:49 +0900

    메세지와 값이 출력되게 작성

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,6 +1,21 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 public class Controller {
+    public void gameStart(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Cars cars = new Cars();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNumber = tryNumber();
+
+        System.out.println("실행결과");
+        moveWhlie(tryNumber, cars);
+
+        System.out.println();   //개행
+        cars.winnerCar();
+        cars.printWinnerCar();
+    }
+
 
     private void moveWhlie(int tryNumber, Cars car){  // 입력받은 숫자만큼 반복
         for(int i = 0; i < tryNumber; i++){
