cb0e3811bfc94db7968753df3888274c7e7c5d81
ldw <leehan02@hanyang.ac.kr>
2023-11-01 17:33:27 +0900

    refactor(racingcar): 자동차를 생성하는 기능을 stream을 사용하는 방식으로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,72 +1,30 @@
 package racingcar;
 
-import Util.Converter;
-import Util.NumberGenerator;
-import camp.nextstep.edu.missionutils.Console;
+import util.Converter;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     private static final Input input = new Input();
-    private static final Referee referee = new Referee();
-    private static final String RESULT_MESSAGE =  "실행 결과";
-    private static final String WINNERS_MESSAGE = "최종 우승자 : ";
+    private static final RaceGame raceGame = new RaceGame();
     private static final String DELIMITER_FOR_INPUT = ",";
-    private static final String DELIMITER_FOR_OUTPUT = ", ";
-    private static final List<Car> cars = new ArrayList<>();
-    private static String carsName;
-    private static int tryNum;
 
     public static void main(String[] args) {
-        init();
-        startRace();
-        raceOver();
-        // 정상 종료 시 혹은 에러 발생에 의한 종료 시 close (필수적인 부분인지?)
-//        Console.close();
-    }
-
-    public static void init() {
-        getInput();
-        makeCars();
-    }
-
-    public static void startRace() {
-        System.out.println(RESULT_MESSAGE);
-        for (int i = 0; i < tryNum; i++) {
-            runRound();
-            System.out.println();
-        }
-    }
-
-    public static void raceOver() {
-        List<String> winners = referee.judgeWinners(cars, Calculator.MaxMove(cars));
-        printWinners(winners);
-    }
-
-    public static void getInput() {
-        carsName = input.askCarsName();
+        String carsName = input.askCarsName();
         String tryNumString = input.askTryNum();
-        tryNum = Converter.StringToInt(tryNumString);
-        InputValidator.TryNum(tryNum);
+        int tryNum = Converter.stringToInt(tryNumString);
+        InputValidator.tryNum(tryNum);
+        /**
+         * raceGameValidator.validateTryNum(tryNum);
+         * raceGameValidator.validateCarNames(carNames);
+         */
+
+        raceGame.run(makeCars(carsName), tryNum);
     }
 
-    public static void makeCars() {
-        for (String name : Converter.StringToStringArray(carsName, DELIMITER_FOR_INPUT)) {
-            cars.add(new Car(name));
-        }
-    }
-
-    public static void runRound() {
-        cars.forEach(car -> {
-            int randomNumber = NumberGenerator.createRandomNumber();
-            boolean isMove = referee.judgeMoveOrStop(randomNumber);
-            car.moveOrStop(isMove);
-        });
-    }
-
-    public static void printWinners(List<String> winners) {
-        System.out.print(WINNERS_MESSAGE);
-        String winnerList = String.join(DELIMITER_FOR_OUTPUT, winners);
-        System.out.println(winnerList);
+    public static List<Car> makeCars(String carsName) {
+        return Arrays.stream(Converter.stringToStringArray(carsName, DELIMITER_FOR_INPUT)).map(Car::new).toList();
     }
 }
