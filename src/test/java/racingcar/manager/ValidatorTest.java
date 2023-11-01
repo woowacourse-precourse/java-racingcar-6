d3ce4db8f2b17fe8f19aa14c48009194fca5988e
gunsookim <ccpc4896@naver.com>
2023-11-01 20:33:59 +0900

    test: ValidatorTest 클래스 중복 이름 예외 처리 테스트 기능 추가
    참가자 목록에 중복된 이름이 존재하는 경우에 대한 예외 처리 테스트 기능 추가.

--- src/test/java/racingcar/manager/ValidatorTest.java
+++ src/test/java/racingcar/manager/ValidatorTest.java
@@ -23,4 +23,12 @@ public class ValidatorTest {
                 .hasMessageContaining("자동차 이름의 길이는 1~" + LENGTH_CAR_NAME + "자만 가능합니다.");
     }
 
+    @Test
+    void 이름_중복_입력_예외_처리() {
+        String[] duplicateNames = {"pobi", "pobi", "javaji"};
+        assertThatThrownBy(() -> Validator.validateNoDuplicateNames(duplicateNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 이름이 있습니다.");
+    }
+
 }
