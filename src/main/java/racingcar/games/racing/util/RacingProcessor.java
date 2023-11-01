a97ee725eb9bc592dd1cb3aaebe96ca423682637
haebyun <bhk4392@gmail.com>
2023-11-01 17:03:20 +0900

    feat(RacingProcessor): 자동차 들의 전진 조건에 대한 process의 느슨한 결합을 위한 인터페이스 생성

---
+++ src/main/java/racingcar/games/racing/util/RacingProcessor.java
@@ -0,0 +1,8 @@
+package racingcar.games.racing.util;
+
+import java.util.List;
+import racingcar.games.racing.domain.Car;
+
+public interface RacingProcessor {
+    void moveProcess(List<Car> cars);
+}
