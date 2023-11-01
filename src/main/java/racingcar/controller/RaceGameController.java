5c8ec16843e2c3de8bc6245b77291e9ecc264364
peeaaccee <wlgp8958@naver.com>
2023-11-01 23:41:00 +0900

    feat(RaceGameContrller): 게임을 시작하는 기능

--- src/main/java/racingcar/controller/RaceGameController.java
+++ src/main/java/racingcar/controller/RaceGameController.java
@@ -1,7 +1,10 @@
 package racingcar.controller;
 
+import racingcar.model.RacingCars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RaceGameController {
     private final InputView inputView;
@@ -10,5 +13,16 @@ public class RaceGameController {
     public RaceGameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void startGame() {
+        RacingCars racingCars = initializeGame();
+
+    }
+
+    private RacingCars initializeGame() {
+        outputView.printStartMessage();
+        List<String> carNames = inputView.inputNamesStringToList();
+        return new RacingCars(carNames);
     }
 }
