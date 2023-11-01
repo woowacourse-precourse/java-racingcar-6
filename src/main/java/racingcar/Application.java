6d377a7933f58b040b716019e71a091cd45225da
한다현 <handahyeon@handahyeon-ui-MacBookAir.local>
2023-11-01 19:36:41 +0900

    feat: create game processing method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,85 +7,96 @@ import java.util.Collections;
 import java.util.List;
 
 public class Application {
-    static String inputCarName(){
-        String input =  Console.readLine();
-        return input.replaceAll("\\s","");
+    static String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return input.replaceAll("\\s", "");
     }
 
-    static String[] splitCarName(String carString){
+    static String[] splitCarName(String carString) {
         return carString.split(",");
     }
 
-    static boolean isValidate(String[] cars){
-        for (String car: cars){
-            if (car.length() > 5){
+    static boolean isValidate(String[] cars) {
+        for (String car : cars) {
+            if (car.length() > 5) {
                 throw new IllegalArgumentException();
             }
         }
         return true;
     }
 
-    static void createCars(String[] cars, List<Car> carArr){
-        for (String car: cars){
+    static void createCars(String[] cars, List<Car> carArr) {
+        for (String car : cars) {
             carArr.add(new Car(car));
         }
     }
 
-    static int inputNumber(){
+    static int inputNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String numString = Console.readLine();
         return Integer.parseInt(numString);
     }
 
     static List<Integer> createDistanceArr(List<Car> carArr) {
         List<Integer> distanceArr = new ArrayList<>();
-        for (Car car: carArr){
+        for (Car car : carArr) {
             distanceArr.add(car.distance);
         }
         return distanceArr;
     }
 
-    static int findMaxValue(List<Integer> distanceArr){
+    static int findMaxValue(List<Integer> distanceArr) {
         return Collections.max(distanceArr);
     }
 
-    static List<Integer> findAllMaxIndex(List<Integer> distanceArr, int max) {
+    static List<Integer> findAllMaxIndex(List<Integer> distanceArr) {
         List<Integer> maxIndex = new ArrayList<>();
-        for (int value: distanceArr) {
-            if(value == max){
-                maxIndex.add(distanceArr.indexOf(value));
+        int max = findMaxValue(distanceArr);
+
+        for (int i = 0; i < distanceArr.size(); i++) {
+            if (distanceArr.get(i) == max) {
+                maxIndex.add(i);
             }
         }
         return maxIndex;
     }
 
-    static List<String> findWinnerIndex(List<Car> carArr, List<Integer> maxIndex){
+    static List<String> findWinnerIndex(List<Car> carArr, List<Integer> maxIndex) {
         List<String> winnerArr = new ArrayList<>();
-        for (int value: maxIndex){
+        for (int value : maxIndex) {
             winnerArr.add(carArr.get(value).name);
         }
         return winnerArr;
     }
 
-    static void printWinner (List<String> winnerArr){
-        System.out.println(String.join(", ", winnerArr));
+    static void printWinner(List<String> winnerArr) {
+        System.out.printf("최종 우승자 : %s", String.join(", ", winnerArr));
+    }
+
+    static void game_process(List<Car> carArr) {
+        int number = inputNumber();
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < number; i++) {
+            for (Car car : carArr) {
+                car.moveForward();
+                car.printState();
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         String[] cars = splitCarName(inputCarName());
         List<Car> carArr = new ArrayList<>();
-        if(isValidate(cars)){
+
+        if (isValidate(cars)) {
             createCars(cars, carArr);
         }
-        int number = inputNumber();
 
-        //게임 진행
-        for (int i = 0; i < number; i++){
-            for (Car car: carArr) {
-                car.moveForward();
-                car.printState();
-            }
-        }
+        game_process(carArr);
 
-        //우승자 출력
+        List<Integer> distanceArr = createDistanceArr(carArr);
+        printWinner(findWinnerIndex(carArr, findAllMaxIndex(distanceArr)));
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,24 +3,24 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    public final String name;
-    public int distance;
+    final String name;
+    int distance;
 
-    Car(String name){
+    Car(String name) {
         this.name = name;
         this.distance = 0;
     }
 
-    void moveForward(){
+    void moveForward() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
             this.distance++;
         }
     }
 
-    void printState(){
+    void printState() {
         System.out.printf("%s : ", this.name);
-        for (int i = 0; i < this.distance; i++){
+        for (int i = 0; i < this.distance; i++) {
             System.out.print("-");
         }
         System.out.println();
