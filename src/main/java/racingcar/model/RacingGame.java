d2e86b8fe4d906136a287b33d8c3e61a70e9dea9
jooda00 <dragon2000a@gmail.com>
2023-11-01 20:28:53 +0900

    refactor(RacingGame): evaluateMoving 메소드 분리 및 게임 결과 출력 화면 outputView로 역할 넘기기

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,18 +11,21 @@ public class RacingGame {
     private static final int START_NUMBER = 0;
     private static final int END_NUMBER = 9;
     private static final int STANDARD_NUMBER = 4;
-    private static final String MOVING_INDICATORS = "-";
     private final MovingCount movingCount;
+    private final OutputView outputView;
 
-    public RacingGame(MovingCount movingCount) {
+    public RacingGame(MovingCount movingCount, OutputView outputView) {
         this.movingCount = movingCount;
+        this.outputView = outputView;
     }
 
     public List<String> playRacingGame(List<String> carList, String attempt) {
         int attemptCount = Integer.parseInt(attempt);
         int size = carList.size();
+
         movingCount.initMovingCount(size);
-        System.out.println("\n실행 결과");
+        outputView.showDefaultResultSentence();
+
         for (int i = 0; i < attemptCount; i++) {
             extractRandomNumber(carList);
         }
@@ -32,30 +36,41 @@ public class RacingGame {
     public List<Integer> extractRandomNumber(List<String> carList) {
         List<Integer> numberList = new ArrayList<>();
         int size = carList.size();
+
         for (int i = 0; i < size; i++) {
             numberList.add(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
         }
+
         moveCars(carList, numberList);
         return numberList;
     }
 
     public void moveCars(List<String> carList, List<Integer> numberList) {
         int size = numberList.size();
+
         for (int i = 0; i < size; i++) {
             int count = movingCount.getEachMovingCount(i);
-            if (numberList.get(i) >= STANDARD_NUMBER) {
-                System.out.println(carList.get(i) + " : " + MOVING_INDICATORS.repeat(movingCount.addCount(i)));
-            } else {
-                System.out.println(carList.get(i) + " : " + MOVING_INDICATORS.repeat(count));
-            }
+            int target = numberList.get(i);
+            String car = carList.get(i);
+
+            int movingCount = evaluateMoving(target, count, i);
+            outputView.showResult(car, movingCount);
         }
         System.out.println();
+    }
+
+    public int evaluateMoving(int target, int count, int index) {
+        if (target >= STANDARD_NUMBER) {
+            count = movingCount.addCount(index);
+        }
+        return count;
     }
 
     public List<String> extractResult(List<String> carList, int size) {
         List<Integer> resultList = extractResultList();
         List<String> winnerList = new ArrayList<>();
         int max = Collections.max(resultList);
+
         for (int i = 0; i < size; i++) {
             if (resultList.get(i) == max) {
                 winnerList.add(carList.get(i));
