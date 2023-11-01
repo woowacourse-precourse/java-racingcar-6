de7b6ab9b700e2332856dd9bdbca6758afc3abcf
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 18:25:10 +0900

    feat(Output): 경주할 자동차 이름을 입력하라는 메세지를 출력 기능 구현
    Output 클래스를 추가하고,
    경주할 자동차 이름을 입력하라는 메세지를 출력하는 메서드 promptForCarName()을 추가하였다.

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+public class Output {
+
+    public enum Message {
+        PROMPT_FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        ;
+
+        private final String label;
+
+        Message(String label) {
+            this.label = label;
+        }
+
+        public String label() {
+            return label;
+        }
+    }
+
+    // 경주할 자동차 이름을 입력하라는 메세지를 출력합니다.
+    public void promptForCarName()
+    {
+        System.out.println(Message.PROMPT_FOR_CAR_NAME.label());
+    }
+
+    // TODO : 시도할 회수를 입력하라는 메세지를 출력합니다.
+
+    // TODO : 각 차수별 실행 결과를 출력합니다.
+
+    // TODO : 우승자를 출력합니다. 공동 우승자일 경우 쉼표를 이용하여 구분합니다.
+
+}
