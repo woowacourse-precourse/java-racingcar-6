de275e0cbd23f1fc4e0bc47416f44018f729792c
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 14:57:06 +0900

    feat: Race 도메인 정의 및 필요한 메소드 정의

---
+++ src/main/java/racingcar/domain/race/Race.java
@@ -0,0 +1,25 @@
+package racingcar.domain.race;
+
+import racingcar.domain.car.Cars;
+
+public class Race {
+    private static final String RACE_RESULT_DELIMITER = "\n";
+
+    private Cars cars;
+    private Count count;
+
+    public Race(Cars cars, Count count) {
+        this.cars = cars;
+        this.count = count;
+    }
+
+    public void runOneCycle() {
+        cars.moveAll();
+        count.decrease();
+    }
+
+    @Override
+    public String toString() {
+        return cars.toString() + RACE_RESULT_DELIMITER;
+    }
+}
