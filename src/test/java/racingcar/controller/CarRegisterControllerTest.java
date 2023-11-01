ea3bb7f31ce5a80b898e740bdac52a88420c07d6
Ukj0ng <xb253q@gmail.com>
2023-11-01 16:16:59 +0900

    fix(CarRegisterController): 경주에 자동차들의 참가 테스트 코드 수정
    수정한 부분:
    - ByteArrayInputStream을 사용해 메서드 테스트를 위한 모의 사용자 입력 주입
    - @BeforeEach 어노테이션을 사용한 setUp 메서드 추가, 필요한 객체 초기화
    - @AfterEach 어노테이션을 사용한 RestoreStreams 메서드 추가, 테스트 후 System.out 리셋

--- src/test/java/racingcar/controller/CarRegisterControllerTest.java
+++ src/test/java/racingcar/controller/CarRegisterControllerTest.java
@@ -1,13 +1,19 @@
 package racingcar.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.RacingCars;
-import java.util.Arrays;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRegisterControllerTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
     private RacingCars racingCars;
     private CarRegisterController carRegisterController;
 
@@ -15,14 +21,25 @@ class CarRegisterControllerTest {
     void setUp() {
         racingCars = new RacingCars();
         carRegisterController = new CarRegisterController(racingCars);
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
     }
 
     @Test
     void registerCars() {
-        List<String> names = Arrays.asList("pobi", "woni", "jun");
-        carRegisterController.registerCars(names);
+        String input = "pobi,woni,jun\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        carRegisterController.registerCars();
 
         List<String> registeredCarNames = racingCars.getCarNames();
-        assertThat(registeredCarNames).containsExactlyElementsOf(names);
+        assertThat(registeredCarNames).containsExactly("pobi", "woni", "jun");
+
+        System.setIn(System.in);
     }
 }
