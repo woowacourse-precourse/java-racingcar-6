c5d6a00e2891ccdd28b4c0fcd64809e1a3304771
godee95 <50854903+godee95@users.noreply.github.com>
2023-11-01 17:04:23 +0900

    feat : 자동차 이름이 올바른지 확인

---
+++ src/main/java/racingcar/domain/InputValidator.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class InputValidator {
+    public void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
+    }   
+}
