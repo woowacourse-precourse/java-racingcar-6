a0556949cfd31603570e12a3be7d808068e1db62
plan11plan <tjtfy7028@gmail.com>
2023-11-01 21:14:24 +0900

    feat:[create][InputView] 자동차 입력받아 필터링해서 리스트로 반환 메서드

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,14 @@ import java.util.stream.Collectors;
 import racingcar.Car;
 
 public class InputView {
-    public static String[] requestCarNames() {
+
+    public static List<Car> inputCars() {
+        String[] arrCarNames = requestCarNames();
+        List<Car> cars = responseCarNames(arrCarNames);
+        return cars;
+    }
+
+        public static String[]  requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분, 5자 이하)");
         String input = readLine();
         String[] carNames = input.split(",");
