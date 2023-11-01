2f9dd13b7f6b7b8160b703a9ce42984933d3ba91
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 18:54:47 +0900

    feat : 반복 실행할 로직 작성

--- src/main/java/racingcar/GameLogic.java
+++ src/main/java/racingcar/GameLogic.java
@@ -1,2 +1,19 @@
-package racingcar;public class GameLogic {
+package racingcar;
+
+import java.util.List;
+import java.util.Random;
+
+public class GameLogic {
+    private int maxPosition = 0;
+
+    public void runGame(List<String> carNames, int raceRounds) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < raceRounds; i++) {
+            for (String carName : carNames) {
+                String progress = generateProgress();
+                System.out.println(carName + " : " + progress);
+            }
+            System.out.println();
+        }
+    }
 }
