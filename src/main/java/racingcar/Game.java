67b704415834f56d3ca1ac044aa758560a1b97fc
jh <pv104@naver.com>
2023-11-01 16:14:33 +0900

    [feat] 게임 클래스 추가

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+/*
+        [ ] (5) 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다. -> inputMoveCountsIntoCar()
+        [v] (6) 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다. -> isMoveAble()
+        [v] (6-1) 무작위 값을 구하는 메서드 generateRandomNumber()
+        [ ] (7) 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다. -> returnWinner()
+        [ ] (8) 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다. -> returnWinner() in if (winner > 1)
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+public class Game {
+    int counts;
+
+}
