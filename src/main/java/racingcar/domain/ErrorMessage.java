f2bcefae1cf29fdfbcef75a14a605c836ec02ba3
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-11-01 13:40:38 +0900

    에러메세지 수정

--- src/main/java/racingcar/domain/ErrorMessage.java
+++ src/main/java/racingcar/domain/ErrorMessage.java
@@ -1,12 +1,10 @@
 package racingcar.domain;
 
 public class ErrorMessage {
-//    public static final String ERROR = "잘못된 입력입니다.";
     public static final String NO_NAME = "입력된 이름이 없습니다.";
     public static final String IS_NUMBER = "입력된 이름이 숫자입니다.";
     public static final String ONE_NAME = "입력된 이름이 한 개입니다.";
     public static final String NAME_RESTRICTION = "5자를 초과한 이름이 존재합니다.";
     public static final String IS_ZERO = "입력값이 0입니다.";
     public static final String IS_NEGATIVE = "입력값이 음수입니다.";
-//    public static final String = ;
 }
