1602f41bfa4418aede9b0db660b02c52bada1cde
qaknc8 <qaknc8@gmail.com>
2023-11-01 20:25:47 +0900

    feat: 시도 회수 입력 기능 구현

---
+++ src/main/java/racingcar/Time.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+public class Time {
+    private int count;
+
+    public void inputTime(String input) {
+        this.count = toInt(input);
+    }
+
+    private int toInt(String input) {
+        int number;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        validNumber(number);
+        return number;
+    }
+
+    private void validNumber(int number) {
+        if (!isNaturalNumber(number)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNaturalNumber(int number) {
+        return number > 0;
+    }
+}
