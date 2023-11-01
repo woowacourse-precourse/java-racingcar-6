d77232ec4e2922da5018f0a11148dc4157325768
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-01 23:03:40 +0900

    refactor (CarName) : 자동차 이름 검증 과정 리팩토링
    - 각 메서드가 한가지 추상화 수준을 가지도록 리팩토링 함
    - isValidate()보다는 isNameSizeOutOfRange()가 해당 역할을 효율적으로 설명해주어 메서드 이름을 변경함

--- src/main/java/racingcar/domain/car/CarName.java
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -20,16 +20,23 @@ class CarName {
     }
 
     private void validate(String name) {
-        Objects.requireNonNull(name);
+        validateNotNull(name);
+        validateSize(name);
+    }
 
-        if (!isValidate(name)) {
+    private void validateNotNull(String name) {
+        Objects.requireNonNull(name);
+    }
+
+    private void validateSize(String name) {
+        if (isNameSizeOutOfRange(name)) {
             String message = String.format("자동차 이름은 빈칸이 아니어야 하고 %d글자 이하이어야 합니다", NAME_MAX_LENGTH);
             throw new IllegalArgumentException(message);
         }
     }
 
-    private boolean isValidate(String name) {
-        return !name.isEmpty() && name.length() <= NAME_MAX_LENGTH;
+    private boolean isNameSizeOutOfRange(String name) {
+        return name.isEmpty() || name.length() > NAME_MAX_LENGTH;
     }
 
     String getName() {
