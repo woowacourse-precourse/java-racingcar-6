b92cf1ae9b79e2f31ca0d0047a8bc0179d3f752b
aka-nick <bechalrang@gmail.com>
2023-11-01 11:09:01 +0900

    추가(RaceTest) : Race 예외 테스트케이스
    경주자 한명만 등록할 시 예외발생하는지 확인하는 테스트 케이스 추가
    
    RaceTest

--- src/test/java/racingcar/collaborator/race/RaceTest.java
+++ src/test/java/racingcar/collaborator/race/RaceTest.java
@@ -4,13 +4,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.assertj.core.api.AbstractLongAssert;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.collaborator.race.enums.RaceMessage;
 import racingcar.generic.RaceTotalProgress;
 import racingcar.generic.Winners;
 import racingcar.io.racing.FailureRaceRandoms;
 import racingcar.io.racing.SuccessRaceRandoms;
 
 class RaceTest {
+
+    @Test
+    void registerRacer로_레이서를한명등록하고_run을_실행하면_예외발생() {
+        Race race = new Race();
+        List<Racer> exceptionRacers = List.of(new Racer("김길동", new SuccessRaceRandoms()));
+
+        Assertions.assertThatThrownBy(() -> race.registerRacer(exceptionRacers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RaceMessage.EXCEPTION_MIN_PARTICIPANTS.get());
+    }
 
     @Test
     void registerRacer로_레이서를등록하고_run을_실행하면_getTotalProgress로_승자를확인할수있다() {
