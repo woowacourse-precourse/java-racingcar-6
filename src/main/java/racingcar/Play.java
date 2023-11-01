7865fb310a49112eef629531958d5359356548a1
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-01 18:14:31 +0900

    feat) Play: show를 통해 실행 결과를 attempts만큼 반복

--- src/main/java/racingcar/Play.java
+++ src/main/java/racingcar/Play.java
@@ -1,2 +1,20 @@
-package racingcar;public class Play {
+package racingcar;
+
+import java.util.LinkedHashMap;
+
+public class Play {
+    public LinkedHashMap<String, Integer> show(Start start) {
+        CarMap carMap = new CarMap();
+        LinkedHashMap<String, Integer> map = carMap.makeInitMap(start);
+        ExecutionResult executionResult = new ExecutionResult();
+        int attempts = start.getNumberAttempts();
+
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < attempts; i++) {
+            map = carMap.updateMap();
+            executionResult.show(map);
+        }
+        return map;
+    }
 }
