161f56fbe212a2db1f847962d54cd376f0f36042
dongwonkwon <ehddnjs0710@gmail.com>
2023-11-01 23:07:36 +0900

    feat: Car클래스 자동차 정보 변수 및 get함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setCarName();
+        System.out.println(car.getCarInfo());
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,28 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Car {
 
+    private Map<String, Integer> carInfo = new HashMap<String, Integer>();
+
+    public Map<String, Integer> getCarInfo() {
+        return carInfo;
+    }
 
     public void setCarName() {
         String str = Console.readLine();
+        String[] names = str.split(",");
 
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("Invalid argument: " + name);
+            }
+            carInfo.put(name,0);
+        }
     }
 }
