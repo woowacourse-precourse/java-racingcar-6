3511a5312e0abe064ee099a7235f1c704e9646f6
Minyongggg <naminyong97@gmail.com>
2023-11-01 18:18:00 +0900

    feat: 자동차 컬렉션 Cars 클래스 추가
    - 기본 생성자
    - add 메소드 : 컬렉션에 인스턴스 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,15 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
+    public List<Car> cars;
+
+    public Cars() {
+    }
+
+    public void add(Car car) {
+        cars.add(car);
+    }
 }
