8b2c0373424fdf43a730ef3ce0e527232ec3b8a5
nayeone <ice1011@naver.com>
2023-11-01 20:27:16 +0900

    feat(OutputView): output View 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,40 @@
+package racingcar.view;
+
+import racingcar.domain.Winners;
+import racingcar.message.Messages;
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String DISTANCE_MARK = "-";
+    private static final String COLON = ":";
+    private static final String DELIMITER = ", ";
+
+    public static void printExecutionResult() {
+        System.out.println(Messages.EXECUTION_RESULT);
+    }
+
+    public static void printWinners(Winners winners) {
+        System.out.println(Messages.PRINT_WINNERS_MESSAGE + String.join(DELIMITER, winners.getWinnerNames()));
+    }
+
+    public static void printCarsPosition(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(printOneCarPosition(car));
+        }
+        System.out.println();
+    }
+
+    private static String printOneCarPosition(Car car) {
+        return car.getName() + COLON + printDistanceMark(car.getPosition());
+    }
+
+    private static String printDistanceMark(int count) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            stringBuilder.append(DISTANCE_MARK);
+        }
+        return stringBuilder.toString();
+    }
+}
