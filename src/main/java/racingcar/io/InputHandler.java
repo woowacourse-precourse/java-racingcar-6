681ac2e376f1382948e06a3cbc43a386ba1d6b28
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 21:54:25 +0900

    feat : validateCarNames 함수 추가

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 import camp.nextstep.edu.missionutils.Console;
 public class InputHandler {
     private static final Scanner scanner = new Scanner(System.in);
-    public static String[] getCarNames(String message){
-        System.out.print(message);
-        return Console.readLine().split(",");
+
+    public static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        validateCarNames(input);
+
+
+        return input.split(",");
     }
-    public static int getTryCount(String message){
-        System.out.print(message);
+
+    private static void validateCarNames(String input) {
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
+    }
+
+    public static int getRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
