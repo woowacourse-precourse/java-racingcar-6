96faa3a034f4338baff981233be94541b2423ed2
wonndrive <nonyok812@gmail.com>
2023-11-01 22:43:00 +0900

    feat(racingcar): 자동차 생성하기

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,30 @@
 package racingcar.domain;
 
-public class Car {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void moveCar() {
+        if (makeRandomValue() >= 4) {
+            position++;
+        }
+    }
+
+    private int makeRandomValue() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
