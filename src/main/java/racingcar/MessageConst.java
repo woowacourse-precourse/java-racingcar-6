efa85794faa844c6c737783618d858236412e473
Chenowoo <cjsgusdn14@naver.com>
2023-11-01 23:49:40 +0900

    refactor(MessageConst): 오타 수정

--- src/main/java/racingcar/MessageConst.java
+++ src/main/java/racingcar/MessageConst.java
@@ -5,7 +5,7 @@ public final class MessageConst {
 
     public static final String LINE_BREAK = "\n";
 
-    public static final String NAMES_OF_CARS_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은" + STANDARD_SAPARATOR + "기준으로 구분)";
+    public static final String NAMES_OF_CARS_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 " + STANDARD_SAPARATOR + "기준으로 구분)";
 
     public static final String MOVE_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇번인가요?";
 
