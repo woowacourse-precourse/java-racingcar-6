0d7c4e44a67e329f7d49c32f2f6226f4c78ef740
tangba484 <tangba484@naver.com>
2023-11-01 18:03:28 +0900

    Feat: 총 라운드 횟수를 입력받는 기능 추가

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -1,9 +1,11 @@
 package racingcar.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Cars;
 
 public class Input {
     public static String CAR_INPUT_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String ROUND_INPUT_MSG = "시도할 회수는 몇회인가요?";
 
     public Cars readCars() {
         System.out.println(CAR_INPUT_MSG);
@@ -12,4 +14,10 @@ public class Input {
         return Cars.generateCars(carNames);
     }
 
+    public Integer readRounds() {
+        System.out.println(ROUND_INPUT_MSG);
+        String userInput = Console.readLine();
+        int rounds = Integer.parseInt(userInput);
+        return rounds;
+    }
 }
