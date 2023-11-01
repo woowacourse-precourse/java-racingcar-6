47a6ffe8e82e86d09549fa21422761c4a56462d7
c101216 <101216.c@gmail.com>
2023-11-01 22:14:10 +0900

    우승자 선정 및 출력 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,7 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -20,6 +23,8 @@ public class Game {
         }
 
         //2. 0에서 9사이에서 무작위 값을 구한 후 무작위 값이 4이상일 경우 해당 자동차를 전진시키는 기능
+        System.out.println("");
+        System.out.println("실행 결과");
         while(tryCount > 0) {
             for(String carName : carNames){
                 carInfo.put(carName,carInfo.get(carName)+checkCarCanGo());
@@ -29,25 +34,34 @@ public class Game {
             System.out.println("");
             tryCount--;
         }
+
+        //3. 우승자 선정 및 출력
+        checkWinner(carInfo);
     }
 
     public static String[] getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = readLine();
+        String[] carNames = inputCarNames.split(",");
 
-        if(!checkCarNames(inputCarNames)){
-            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+        if(!checkCarNames(carNames)){
+            throw new IllegalArgumentException("자동차 이름을 올바르게 입력하세요.(5자 이하)");
         }
 
-        return inputCarNames.split(",");
+        return carNames;
     }
 
-    public static boolean checkCarNames(String carNames){
-        if(carNames.isEmpty()){
-            return false;
-        }else {
-            return true;
+    public static boolean checkCarNames(String[] carNames){
+        for(String carName : carNames){
+            if(carName.isEmpty()){
+                return false;
+            }
+            if(carName.length() > 5){
+                return false;
+            }
         }
+
+        return true;
     }
 
     public static int getTryCount(){
@@ -56,6 +70,9 @@ public class Game {
 
         if(!isNumeric(inputTryCount)){
             throw new IllegalArgumentException("숫자를 입력하세요.");
+        }
+        if(Integer.parseInt(inputTryCount) < 1){
+            throw new IllegalArgumentException("1 이상의 숫자를 입력하세요.");
         }
 
         return Integer.parseInt(inputTryCount);
@@ -87,6 +104,39 @@ public class Game {
             System.out.printf("-");
             currentCarLocation--;
         }
-        System.out.printf("\n");
+        System.out.println("");
+    }
+
+    public static void checkWinner(HashMap<String,Integer> carInfo){
+        ArrayList<String> winnerList = new ArrayList<>();
+
+        List<String> carInfoKeySet = new ArrayList<>(carInfo.keySet());
+
+        carInfoKeySet.sort(((o1, o2) -> carInfo.get(o2).compareTo(carInfo.get(o1))));
+        int winnerLocation = -1;
+
+
+        for(String carName : carInfoKeySet){
+            if(winnerLocation == -1){
+                winnerLocation = carInfo.get(carName);
+                winnerList.add(carName);
+                continue;
+            }
+
+            if(winnerLocation != -1 && winnerLocation == carInfo.get(carName)){
+                winnerList.add(carName);
+            }
+            else{
+                break;
+            }
+        }
+
+        System.out.printf("최종 우승자 :");
+        for(int i=0; i<winnerList.size(); i++){
+            if(i != 0){
+                System.out.printf(",");
+            }
+            System.out.printf(" %s",winnerList.get(i));
+        }
     }
 }
