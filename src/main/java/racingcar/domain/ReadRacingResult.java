6d556f24466bbb0f8ce673ce77ee47ebfa2d15f2
doka <posmane5@gmail.com>
2023-11-01 20:22:15 +0900

    refactor(ReadRacingResult) : readHashMap 메소드에 반복문 내부 문자열을 "+" 기호로 합치는 대신 StringBuilder 사용

--- src/main/java/racingcar/domain/ReadRacingResult.java
+++ src/main/java/racingcar/domain/ReadRacingResult.java
@@ -10,14 +10,17 @@ import java.util.Collections;
 public class ReadRacingResult {
 
     public String readHashMap(LinkedHashMap<Map.Entry<Integer, String>, Integer> racingResult) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
+        //Map.Entry<Map.Entry<Integer, String>, Integer> 형태로 {(인덱스, 차이름) : 이동값} 형태의 해시맵 제작 
+        //차이름의 중복을 허용하기 위해 위와같은 형태로 제작 
         for (Map.Entry<Map.Entry<Integer, String>, Integer> racingRecord : racingResult.entrySet()) {
-            result = racingRecord.getKey().getValue() + " : " + "-".repeat(racingRecord.getValue());
-            System.out.println(result);
+            result.append(racingRecord.getKey().getValue()).append(" : ").append("-".repeat(racingRecord.getValue()));
+            result.append("\n");
         }
-        System.out.println();
+        String output = result.toString();
+        System.out.println(output);
 
-        return result;
+        return output;
 
     }
 
@@ -25,6 +28,7 @@ public class ReadRacingResult {
 
     public String judgmentWinner(LinkedHashMap<Map.Entry<Integer, String>, Integer> racingResult) {
         int maxRecord = Collections.max(racingResult.values());
+        //가장 먼거리로 이동한 자동차들을 찾음
         for (Map.Entry<Map.Entry<Integer, String>, Integer> racingRecord : racingResult.entrySet()) {
             findWinner(maxRecord, racingRecord);
         }
@@ -33,8 +37,8 @@ public class ReadRacingResult {
         return result;
     }
 
-    public void findWinner(int maxRecord, Map.Entry<Map.Entry<Integer, String>, Integer> currentCarRecord) {
-
+    private void findWinner(int maxRecord, Map.Entry<Map.Entry<Integer, String>, Integer> currentCarRecord) {
+        //자동차의 이동거리가 가장 멀리 이동한 거리(maxRecord) 인지 비교
         if (currentCarRecord.getValue() >= maxRecord) {
             winners.add(currentCarRecord.getKey().getValue());
         }
