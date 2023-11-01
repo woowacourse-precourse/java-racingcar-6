7a2c8387866665c0bf9878976e324f32c622d9d6
DonghaOh <donghadarwin@naver.com>
2023-11-01 15:42:17 +0900

    feat: 이름 리스트 만들기 - nameList 생성

--- src/main/java/racingcar/model/nameValidator.java
+++ src/main/java/racingcar/model/nameValidator.java
@@ -1,8 +1,12 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class nameValidator {
     public static void namesException(String names){
         isNamesNull(names);
+        List<String> nameList = List.of(names.split(","));
     }
     public static void isNamesNull(String names) {
         if (names.isEmpty()) throw new IllegalArgumentException("자동차 이름을 입력해주세요");
