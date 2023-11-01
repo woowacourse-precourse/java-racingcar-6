6f278571537bc32ba209e051a5c43ab29f5f03c5
Yerinnnn <shinyerin0216@gmail.com>
2023-11-01 19:10:14 +0900

    Style: "코드 포맷 수정"

--- src/test/java/racingcar/ValidationTest.java
+++ src/test/java/racingcar/ValidationTest.java
@@ -1,28 +1,29 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.utils.Validator;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ValidationTest extends NsTest {
     
     @Test
     void 시도횟수_입력시_공백을_입력하면_예외_처리() {
-        assertThatThrownBy(() -> runException("pobi,javaji", " ")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", " "))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 시도횟수_입력시_문자를_입력하면_예외_처리() {
-        assertThatThrownBy(() -> runException("pobi,javaji", "a")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", "a"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     
     @Test
     void 시도횟수_입력시_음수를_입력하면_예외_처리() {
-        assertThatThrownBy(() -> runException("pobi,javaji", "-1")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,javaji", "-1"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -35,12 +36,14 @@ public class ValidationTest extends NsTest {
 
     @Test
     void 같은_이름이_여러개일_경우_예외_처리() {
-        assertThatThrownBy(() -> runException("pobi,pobi", "1")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 이름이_1개_이하일_경우_예외_처리() {
-        assertThatThrownBy(() -> runException("pobi", "1")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> runException("pobi", "1"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
