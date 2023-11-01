78029a09a542e192e5cafe8953b2d19b4523ceca
yeahdy <yejine98@naver.com>
2023-11-01 12:43:37 +0900

    feat(ErrorCodeConstant): 시도 횟수 유효성 검사 관련 에러코드 추가

--- src/main/java/constants/ErrorCodeConstant.java
+++ src/main/java/constants/ErrorCodeConstant.java
@@ -5,5 +5,7 @@ public class ErrorCodeConstant {
     public static final String NAME_LENGTH_ERROR = "이름은 5자리를 넘을 수 없습니다.";
     public static final String CAR_MIN_LENGTH_ERROR = "자동차는 1명 미만일 수 없습니다.";
     public static final String STRING_BLANK_ERROR = "이름은 공백일 수 없습니다.";
+    public static final String NUMBER_VALIDATION_ERROR = "숫자 외의 문자는 입력할 수 없습니다.";
+    public static final String MAX_NUMBER_ERROR = "숫자 외의 문자는 입력할 수 없습니다.";
 
 }
