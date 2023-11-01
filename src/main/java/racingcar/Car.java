d00ea99861e027e321ccc619715f994d932c167d
Minji Kim <minji1289@naver.com>
2023-11-01 10:46:25 +0900

    [feat] 자동차 객체 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private static List<Car> createCars() {
+        List<Car> cars = new ArrayList<>();
+        return cars;
+    }
+
+    public void startGame() {
+        List<Car> cars = createCars();
+    }
+
+}
