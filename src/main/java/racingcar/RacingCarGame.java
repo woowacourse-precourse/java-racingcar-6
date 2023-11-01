d515e169bd1c560e230ed715e3b60d351ef454c3
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-11-01 20:29:30 +0900

    refactor/racingCarGame: 너무 작은 단위인 checkNum()함수 제거, 함수들의 위치를 실행순서대로 변경

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -28,15 +28,80 @@ public class RacingCarGame {
         printWinner();
     }
 
-    private void printWinner(){
-        List<String> winnerList = whoIsWinner();
-        System.out.print("최종 우승자 :");
-        for(int i=0;i<winnerList.size();i++){
-            if(i>0){
-                System.out.print(",");
-            }
-            System.out.print(" " + winnerList.get(i));
+    private void init(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        generateListOfRacingCars(carNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNum = Console.readLine();
+        checkInputNumValid(inputNum);
+        setNumOfPlaying(Integer.parseInt(inputNum));
+
+    }
+
+    private void generateListOfRacingCars(final String carNames){
+
+        final List<String> listOfCarNames = stringToListOfString(carNames);
+
+        for(String carName:listOfCarNames){
+            checkCarNameValid(carName);
+            RacingCar racingCar = generateRacingCar(carName);
+            racingCars.add(racingCar);
         }
+    }
+
+    private List<String> stringToListOfString(final String carNames){
+        return Arrays.asList(carNames.split(","));
+    }
+
+    private void checkCarNameValid(final String carName){
+        if (carName.length()>5){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private RacingCar generateRacingCar(final String carName){
+        return new RacingCar(carName);
+    }
+
+    private void checkInputNumValid(final String inputNum){
+        try{
+            Integer.parseInt(inputNum);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void setNumOfPlaying(final int numOfPlaying) {
+        this.numOfPlaying = numOfPlaying;
+    }
+
+    private void play(){
+        decideToGoOrStop();
+        printCurrentResult();
+    }
+    private void decideToGoOrStop(){
+        for(RacingCar racingCar:racingCars){
+            go(racingCar);
+        }
+    }
+
+    private void go(RacingCar racingCar){
+        if(randomNumGenerator.generateNum()>=4){
+            racingCar.plusDistance();
+        }
+    }
+
+    private void printCurrentResult(){
+        for(RacingCar racingCar:racingCars){
+            System.out.print(racingCar.getCarName()+" : ");
+            for(int i =0;i<racingCar.getDistance();i++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     private List<String> whoIsWinner(){
@@ -53,85 +118,14 @@ public class RacingCarGame {
         return winnerList;
     }
 
-    private void play(){
-        decideToGoOrStop();
-        printCurrentResult();
-    }
-    private void decideToGoOrStop(){
-        for(RacingCar racingCar:racingCars){
-            boolean goOrNot = checkNum(randomNumGenerator.generateNum());
-            go(racingCar, goOrNot);
-        }
-    }
-
-    private boolean checkNum(int num){
-        if(num<4){
-            return false;
-        }
-        return true;
-    }
-
-    private void go(RacingCar racingCar,boolean goOrNot){
-        if(goOrNot){
-            racingCar.plusDistance();
-        }
-    }
-
-    private void printCurrentResult(){
-        for(RacingCar racingCar:racingCars){
-            System.out.print(racingCar.getCarName()+" : ");
-            for(int i =0;i<racingCar.getDistance();i++){
-                System.out.print("-");
+    private void printWinner(){
+        List<String> winnerList = whoIsWinner();
+        System.out.print("최종 우승자 :");
+        for(int i=0;i<winnerList.size();i++){
+            if(i>0){
+                System.out.print(",");
             }
-            System.out.println();
+            System.out.print(" " + winnerList.get(i));
         }
-        System.out.println();
-    }
-
-
-
-    private void init(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        generateListOfRacingCars(carNames);
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        String inputNum = Console.readLine();
-        checkInputNumValid(inputNum);
-        setNumOfPlaying(Integer.parseInt(inputNum));
-
-    }
-
-    private void checkInputNumValid(String inputNum){
-        try{
-            Integer.parseInt(inputNum);
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException();
-        }
-    }
-    private void checkCarNameValid(String carName){
-        if (carName.length()>5){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void generateListOfRacingCars(String carNames){
-
-        List<String> listOfCarNames = stringToListOfString(carNames);
-
-        for(String carName:listOfCarNames){
-            checkCarNameValid(carName);
-            RacingCar racingCar = generateRacingCar(carName);
-            racingCars.add(racingCar);
-        }
-    }
-    private List<String> stringToListOfString(String carNames){
-        return Arrays.asList(carNames.split(","));
-    }
-    private RacingCar generateRacingCar(String carName){
-        return new RacingCar(carName);
-    }
-    private void setNumOfPlaying(int numOfPlaying) {
-        this.numOfPlaying = numOfPlaying;
     }
 }
