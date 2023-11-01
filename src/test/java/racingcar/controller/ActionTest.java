d1c5517780b3bd8714371f7fa20a6f8bb972dd71
dragon0622 <iamdragon.dev@gmail.com>
2023-11-01 19:01:38 +0900

    test(racingCar): 전진 및 정지 확인 테스트 구현

--- src/test/java/racingcar/controller/ActionTest.java
+++ src/test/java/racingcar/controller/ActionTest.java
@@ -1,14 +1,37 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 import racingcar.model.Car;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ActionTest {
+class ActionTest extends NsTest {
 
+    @BeforeEach
+    void setup() {
+
+    }
     @Test
     void 전진및정지확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("july,april,march","1");
+                    assertThat(output()).contains("july : -", "april : ", "march : -", "최종 우승자 : july, march");
+                },
+                4,3,7
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
