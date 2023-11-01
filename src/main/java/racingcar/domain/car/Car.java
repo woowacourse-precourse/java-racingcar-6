79e930aed8c1da4f94ead6aba27591392af1e64d
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 14:56:24 +0900

    feat: Car 도메인 객체의 move, toString 메소드 및 필요한 메소드 정의

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,11 +1,41 @@
 package racingcar.domain.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MINIMUM_RANDOM_NUMBER = 0;
+    private static final int MAXIMUM_RANDOM_NUMBER = 9;
+    private static final int MOVABLE_RANDOM_NUMBER = 4;
+    private static final String NAME_POSITION_DELIMITER = " : ";
+
     private final Name name;
     private final Position position;
 
     public Car(final Name name) {
         this.name = name;
         this.position = new Position();
+    }
+
+    public void move() {
+        final int randomNumber = generateRandomNumber();
+
+        if (!isMovable(randomNumber)) {
+            return;
+        }
+
+        position.increase();
+    }
+
+    @Override
+    public String toString() {
+        return name + NAME_POSITION_DELIMITER + position;
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+    }
+
+    private boolean isMovable(final int randomNumber) {
+        return randomNumber >= MOVABLE_RANDOM_NUMBER;
     }
 }
