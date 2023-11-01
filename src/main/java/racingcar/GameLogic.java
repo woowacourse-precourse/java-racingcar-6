60e225826868c640a1ef8c65703a0e30ae4d0db4
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 19:08:13 +0900

    feat : 무작위 값을 구하고 자동차의 전진 여부를 결정한다

--- src/main/java/racingcar/GameLogic.java
+++ src/main/java/racingcar/GameLogic.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Random;
 
 public class GameLogic {
+    private final Random random = new Random();
     private int maxPosition = 0;
 
     public void runGame(List<String> carNames, int raceRounds) {
+        System.out.println();
         System.out.println("실행 결과");
         for (int i = 0; i < raceRounds; i++) {
             for (String carName : carNames) {
@@ -15,5 +17,12 @@ public class GameLogic {
             }
             System.out.println();
         }
+    }
+
+    private String generateProgress() {
+        if (random.nextInt(10) >= 4) {
+            return "-";
+        }
+        return "";
     }
 }
