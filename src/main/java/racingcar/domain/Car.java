9b8402368757d722aed0a7436e4112011c268caf
wooni97 <jhw970714@gmail.com>
2023-11-01 17:51:57 +0900

    refactor(Car): Car 도메인 유효성 검증 메서드 non-static으로 변경
    - 이 커밋은 static으로 선언된 유효성 검증 메서드들을 인스턴스 메서드로 변경하였습니다. 객체가 가진 역할이라고 판단하여 객체를 인스턴스화했을때 해당 메소드를 사용할 수 있도록 합니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,15 +1,16 @@
 package racingcar.domain;
 
 public class Car {
+    public static final int INITIAL_POSITION = 0;
+    public static final int FORWARD_CONDITION_NUMBER = 4;
+    public static final int VALID_CAR_NAME_MAX_LENGTH = 5;
     private final String name;
     private int position;
-    private final int INITIAL_POSITION = 0;
-    private final int FORWARD_CONDITION_NUMBER = 4;
-    private static final int VALID_CAR_NAME_MAX_LENGTH = 5;
+
 
     public Car(String name) {
-        checkCarNameLengthValid(name);
         checkCarNameEmpty(name);
+        checkCarNameLengthValid(name);
         checkCarNameContainsWhiteSpace(name);
 
         this.name = name;
@@ -30,20 +31,20 @@ public class Car {
         return position;
     }
 
-    public static void checkCarNameLengthValid(String carName) {
+    public void checkCarNameLengthValid(String carName) {
         if (carName.length() > VALID_CAR_NAME_MAX_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void checkCarNameEmpty(String carName) {
+    public void checkCarNameEmpty(String carName) {
         if (carName == null || carName.trim().isEmpty()) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void checkCarNameContainsWhiteSpace(String carName) {
-        if (carName.length() != carName.trim().length()) {
+    public void checkCarNameContainsWhiteSpace(String carName) {
+        if (carName.length() != carName.replace(" ", "").length()) {
             throw new IllegalArgumentException();
         }
     }
