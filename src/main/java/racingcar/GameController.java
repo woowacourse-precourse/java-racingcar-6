9b2d3f365cdb27c15824187a5138e17577684262
TaeUkChu <hippo2012@naver.com>
2023-11-01 16:46:54 +0900

    GameController 클래스 1차 작성 완료, 게임 시작, 종료 전체 괄략 작성

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,2 +1,40 @@
-package racingcar;public class GameController {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+    private GameView view;
+    private List<Car> cars;
+    private int num;
+    public GameController(){
+        view = new GameView();
+    }
+    public void createGame(){
+        this.cars = view.inputCars();
+        this.num = view.inputNum();
+    }
+//    게임 진행
+    public void gameSet(){
+        int count= 0;
+        System.out.println("실행 결과");
+        while (count < num){
+            printProcessGame();
+            count++;
+        }
+        gameEnd();
+    }
+//    게임 종료
+    public void gameEnd(){
+        System.out.print("최종 우승자 : " );
+        List<String> winner = getWinner();
+        for (int i =0; i<winner.size()-1; i++){
+            System.out.print(winner.get(i)+", ");
+        }
+        System.out.print(winner.get(winner.size()-1));
+    }
+
+
 }
