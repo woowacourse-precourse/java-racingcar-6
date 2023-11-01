d2f5e48489df3f21e5743cdc103f01573b597b33
dsadara <chaehyun.forest@gmail.com>
2023-11-01 22:30:37 +0900

    refactor: 입력검증 메서드 인스턴스 메소드로 변경

--- src/main/java/racingcar/domain/RacingInfo.java
+++ src/main/java/racingcar/domain/RacingInfo.java
@@ -13,6 +13,7 @@ public class RacingInfo {
     private int turn;
 
     public RacingInfo(List<String> carNames, int turn) {
+        validateInputs(turn, carNames);
         this.carNames = carNames;
         this.turn = turn;
     }
@@ -25,24 +26,7 @@ public class RacingInfo {
         return turn;
     }
 
-    public static RacingInfo enter() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carString = Console.readLine();
-        List<String> carNames = parseCarString(carString);
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        int turn = Integer.parseInt(Console.readLine());
-
-        validateInputs(turn, carNames);
-        return new RacingInfo(carNames, turn);
-    }
-
-    private static List<String> parseCarString(String carString) {
-        return Arrays.stream(carString.split(","))
-                .map(String::trim).collect(Collectors.toList());
-    }
-
-    private static void validateInputs(int turn, List<String> carNames) {
+    private void validateInputs(int turn, List<String> carNames) {
         if (turn <= 0) {
             throw new IllegalArgumentException("0보다 큰 회수를 입력해주세요.");
         }
@@ -52,7 +36,7 @@ public class RacingInfo {
         }
     }
 
-    private static boolean validateCarName(List<String> carList) {
+    private boolean validateCarName(List<String> carList) {
         Set<String> set = new HashSet<>();
         for (String carStr : carList) {
             // 자동차 이름 5자 이하 체크
@@ -67,5 +51,21 @@ public class RacingInfo {
             set.add(carStr);
         }
         return true;
+    }
+
+    public static RacingInfo enter() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carString = Console.readLine();
+        List<String> carNames = parseCarString(carString);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int turn = Integer.parseInt(Console.readLine());
+
+        return new RacingInfo(carNames, turn);
+    }
+
+    private static List<String> parseCarString(String carString) {
+        return Arrays.stream(carString.split(","))
+                .map(String::trim).collect(Collectors.toList());
     }
 }
