1c42011e8c356f6f26e476d4d078efe2e3b4df73
kimdohyung <pdohyung@gmail.com>
2023-11-01 18:42:31 +0900

    feat(ErrorMessage): 에러 메세지 클래스로 분리

---
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar.exception;
+
+public class ErrorMessage {
+    public static final String CAR_NAME_NOT_EMPTY = "빈칸은 이름이 될 수 없습니다.";
+    public static final String CAR_NAME_NOT_BLANK = "이름에 공백이 포함될 수 없습니다.";
+    public static final String CAR_NAME_DUPLICATE = "자동차 이름들은 중복될 수 없습니다.";
+    public static final String CAR_NAME_OVER_MAX_LENGTH = "이름의 길이가 5자를 초과할 수 없습니다.";
+    public static final String NUMBER_OF_TIMES_NOT_NUMBER = "빈칸은 이름이 될 수 없습니다.";
+}
