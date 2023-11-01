fbf3ae831fb0501b5b9c00c59e61ae9d745b3e01
TaeUkChu <hippo2012@naver.com>
2023-11-01 17:00:50 +0900

    2차 예외처리 작성 완료 (공백처리)

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -4,8 +4,11 @@ public class Validation {
 //    자동차 이름 검증 메서드
     public static boolean carValid(String name) throws IllegalArgumentException{
         // 이름이 5자 초과이거나 입력하지 않는다면
-        if ((name.length() > 5) | (name.isEmpty()) | (name.equals(" "))){
+        if ((name.length() > 5) | (name.isEmpty())){
             System.out.println("이름 길이가 5자 미만이 되도록 입력해주세요.");
+            throw new IllegalArgumentException();
+        } else if (name.equals(" ")) {
+            System.out.println("이름을 공백으로 지정할 순 없습니다.");
             throw new IllegalArgumentException();
         }
         return true;
