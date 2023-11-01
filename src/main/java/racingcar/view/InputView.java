03cebf186953836fbe25910834f828cfdd45ecda
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-01 17:42:45 +0900

    feat : 플레이어에게 입력받는 기능 : 횟수입력받기 함수 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -18,4 +18,23 @@ public class InputView {
 
         return inputCarNameList;
     }
+
+    public static Integer inputRacingCount(){
+        try{
+            //Scanner in = new Scanner(System.in); // Scanner 객체 생성
+            //String playerString = in.nextLine();
+            String playerString = Console.readLine();
+
+            Integer playerNumber = Integer.valueOf(playerString.trim());
+            if(playerNumber<1){
+                throw new NumberFormatException();
+            }
+            return playerNumber;
+
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("경주 횟수는 양수여야 합니다.");
+        }
+
+    }
+    
 }
