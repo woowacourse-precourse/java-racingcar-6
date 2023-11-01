18b88a64d31f0306fde9e0bc186c97eac0b42938
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 15:24:01 +0900

    feat : 사용자에게 시도할 회수를 입력 받는다.

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -9,10 +9,16 @@ import java.util.List;
 public class InputManager {
     public void startGame() {
         getCarNames();
+        getRaceRounds();
     }
 
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
+    }
+
+    public int getRaceRounds() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
