b7c40b024da038386e1285377edcb86a5aca6164
sunwoong <sunwoong1122@gmail.com>
2023-11-01 20:49:02 +0900

    feat: 자동차 이름 검증 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -8,6 +8,14 @@ public class Validator {
     private final static String SYMBOL = ",";
     private final static int MAXIMUM_NAME_LENGTH = 5;
 
+    public void validateCarsName(String carsName) {
+        validateIsBlank(carsName);
+        validateContainSymbol(carsName);
+        validateSymbolPosition(carsName);
+        validateMaximumLength(carsName);
+        validateDuplicateName(carsName);
+    }
+
     private void validateIsBlank(String carsName) {
         if (carsName.isBlank()) {
             throw new IllegalArgumentException("자동차의 이름은 공백일 수 없습니다.");
