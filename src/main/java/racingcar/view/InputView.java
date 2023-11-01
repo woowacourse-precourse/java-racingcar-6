b47429c7853375617dfc4f40d9ebc9323d01453e
Hayoon <hy97@sju.ac.kr>
2023-11-01 22:41:56 +0900

    feat(InputView): 입력 클래스 분리

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String inputCarNames() {
+        return readLine();
+    }
+
+    public String inputNumberOfAttempts() {
+        return readLine();
+    }
+}
