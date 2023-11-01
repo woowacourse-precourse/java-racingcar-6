402a11bd97d52b7b287a137275940101dd78f5f2
june-777 <wlwhswnsrl96@gmail.com>
2023-11-01 15:35:57 +0900

    refactor(ApplicationConfiguration): Input 인터페이스 추가로 인한 DI 로직 변경

--- src/main/java/racingcar/configuration/ApplicationConfiguration.java
+++ src/main/java/racingcar/configuration/ApplicationConfiguration.java
@@ -2,13 +2,16 @@ package racingcar.configuration;
 
 import racingcar.controller.GameController;
 import racingcar.service.RacingGameService;
+import racingcar.service.domain.CarEngine;
+import racingcar.service.domain.WootecoCarEngine;
+import racingcar.view.ConsoleInput;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class ApplicationConfiguration {
 
     public GameController gameController() {
-        return new GameController(outputView(), inputView(), racingGameService());
+        return new GameController(outputView(), inputView(), racingGameService(), carEngine());
     }
 
     private OutputView outputView() {
@@ -16,10 +19,14 @@ public class ApplicationConfiguration {
     }
 
     private InputView inputView() {
-        return new InputView();
+        return new InputView(new ConsoleInput());
     }
 
     private RacingGameService racingGameService() {
         return new RacingGameService();
+    }
+
+    private CarEngine carEngine() {
+        return new WootecoCarEngine();
     }
 }
