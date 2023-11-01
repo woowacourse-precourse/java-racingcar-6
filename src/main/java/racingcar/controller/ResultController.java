c8ca846b8397a6517084e344d0d6f0cd24db047d
Ukj0ng <xb253q@gmail.com>
2023-11-01 15:45:13 +0900

    feat(ResultController): 우승자 출력 기능 구현
    추가한 기능:
    - 우승자 출력 기능

--- src/main/java/racingcar/controller/ResultController.java
+++ src/main/java/racingcar/controller/ResultController.java
@@ -1,2 +1,22 @@
-package racingcar.controller;public class ResultController {
+package racingcar.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.model.Car;
+import racingcar.model.RacingCars;
+
+public class ResultController {
+    private final RacingCars racingCars;
+
+    public ResultController(RacingCars racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void showWinners() {
+        List<Car> winners = racingCars.findWinners();
+        String winnerNames = winners.stream()
+            .map(Car::getName)
+            .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
