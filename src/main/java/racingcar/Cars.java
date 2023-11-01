7ef5b74edcdaa609857c2075ed16d603d519ab57
jagarchi <relong1026@naver.com>
2023-11-01 19:46:35 +0900

    차가 이동할 수 있는 조건과 이동했을때 -를 추가해서 표시

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
     private final String[] carNames;
     private int firstMagicNumber = 0;
@@ -33,5 +37,14 @@ public class Cars {
 
     public int randomNumber(){
         return Randoms.pickNumberInRange(firstMagicNumber,lastMagicNumber);
+    }
+
+    public void moveCar() {
+        for (int i = 0; i < carNames.length; i++) {
+
+            if (randomNumber() > 3) {     //3보다 크면 -를 추가
+                carNames[i] += "-";     //car의 이름이 하나의 배열을 가지고 있고 그 배열에서 추가
+            }
+        }
     }
 }
