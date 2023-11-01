15762415f7b95c0aa436eeb20a92fac69034382a
Jinnie Kim <jinniek48@gmail.com>
2023-11-01 23:01:17 +0900

    refactor(racingcar): main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,24 +11,24 @@ public class Application {
 
     public static void main(String[] args) {
 
-        NamesManager namesManager = new NamesManager();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
+        NamesManager namesManager = new NamesManager();
         List<String> namesList = namesManager.separateNamesString(Console.readLine());
+
         namesManager.checkNamesException(namesList);
 
         Leaderboard leaderboard = new Leaderboard(namesList);
 
-        RoundNumberManager roundNumberManager = new RoundNumberManager();
         System.out.println("시도할 회수는 몇회인가요?");
-
+        RoundNumberManager roundNumberManager = new RoundNumberManager();
         String roundNumberString = Console.readLine();
+
         roundNumberManager.checkRoundNumberExceptions(roundNumberString);
 
         int roundNumber = Integer.parseInt(roundNumberString);
 
         System.out.print("\n실행 결과\n");
-
         while (roundNumber > 0) {
             leaderboard.round();
             leaderboard.status();
