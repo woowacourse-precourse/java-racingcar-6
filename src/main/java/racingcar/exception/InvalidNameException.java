d6b11e76346b89919f0a3c6cb7fda3510b3d03de
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-01 16:39:09 +0900

    Feat : Exception - InvalidNameException class 구현

--- src/main/java/racingcar/exception/InvalidNameException.java
+++ src/main/java/racingcar/exception/InvalidNameException.java
@@ -1,4 +1,11 @@
 package racingcar.exception;
 
-public class InvalidNameException {
+public class InvalidNameException extends IllegalArgumentException{
+    public InvalidNameException(){
+        super("유효한 이름을 입력해주세요.");
+    }
+
+    public InvalidNameException(String message){
+        super(message);
+    }
 }
