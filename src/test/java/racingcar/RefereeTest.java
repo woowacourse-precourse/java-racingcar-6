4eea215f70efb88a83665c06762e463af49aa5c7
ldw <leehan02@hanyang.ac.kr>
2023-11-01 16:05:36 +0900

    test(racingcar): 심판이 자동차가 전진하도록 판단한 경우에 대한 단위 테스트 구현

--- src/test/java/racingcar/RefereeTest.java
+++ src/test/java/racingcar/RefereeTest.java
@@ -12,4 +12,11 @@ public class RefereeTest {
 
         Assertions.assertThat(referee.judgeMoveOrStop(LOWER_NUMBER_THAN_FOUR)).isEqualTo(false);
     }
+
+    @Test
+    void 심판이_자동차가_전진하도록_판단한_경우() {
+        final int HIGHER_NUMBER_THAN_FOUR = 5;
+
+        Assertions.assertThat(referee.judgeMoveOrStop(HIGHER_NUMBER_THAN_FOUR)).isEqualTo(true);
+    }
 }
