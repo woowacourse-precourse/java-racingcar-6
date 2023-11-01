93285ff96115f9f6bf11e579b70fe0d7f7bd4906
ch1hyun <och2510@gmail.com>
2023-11-01 11:56:40 +0900

    fix : Add a readline function to view
    - InputView.java

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,18 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.enums.InputMessage;
 
 public class InputView {
-    public static void printCarNameMessage() {
+    public static String inputCarName() {
         System.out.println(InputMessage.NAME_OF_THE_CAR.getMessage());
+
+        return Console.readLine();
     }
 
-    public static void printAttemptsNumberMessage() {
+    public static String inputAttemptsNumber() {
         System.out.println(InputMessage.NUMBER_OF_ATTEMPTS.getMessage());
+
+        return Console.readLine();
     }
 }
