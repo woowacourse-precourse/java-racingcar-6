743f937a390a190ed1cf9b0f48c8eeebafe93c44
hansg931 <hansg0509@cau.ac.kr>
2023-11-01 21:35:55 +0900

    feat: 1 바퀴 처리하는 클래스 작성

---
+++ src/main/java/racingcar/model/Lap.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lap {
+	private final Cars cars;
+	private final Round round;
+
+	public Lap(Cars cars, Round round) {
+		this.cars = cars;
+		this.round = round;
+	}
+
+	public RaceStatus getRaceStatus() {
+		cars.moveForward();
+		round.proceedRound();
+
+		return new RaceStatus(cars.getCars());
+	}
+
+	public RaceStatus getWinner() {
+		return new RaceStatus(cars.getFirstPlaceCars());
+	}
+
+	public boolean isGameEnd() {
+		return round.isEndRound();
+	}
+}
