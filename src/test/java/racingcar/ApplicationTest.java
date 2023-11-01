ad8bf38b416625fc22c0946099da7d72704d2a06
Isaac Han <isaachan233@gmail.com>
2023-11-01 13:13:18 +0900

    test: Application 에 대한 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,8 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.enums.Common;
+import racingcar.enums.ErrorMessages;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -13,53 +15,81 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    @DisplayName("전진 정지")
     @Test
-    void 전진_정지() {
+    void moveForwardAndStop() {
         assertRandomNumberInRangeTest(() -> {
             run("pobi,woni", "1");
             assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
         }, MOVING_FORWARD, STOP);
     }
 
+    @DisplayName("전진 정지 2")
     @Test
-    void 전진_정지_2() {
+    void moveForwardAndStop2() {
         assertRandomNumberInRangeTest(() -> {
             run("pobi,woni", "2");
             assertThat(output()).contains("pobi : -", "woni : ", "pobi : -", "woni : ", "최종 우승자 : pobi, woni");
         }, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD);
     }
 
+    @DisplayName("긴 차 이름에 대한 예외 처리")
     @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class));
-    }
-
-
-    @Test
-    void 이름에_대한_예외_처리_3() {
-        assertSimpleTest(() -> assertThatThrownBy(() -> runException(",pobi", "1"))
-                .isInstanceOf(IllegalArgumentException.class));
-    }
-
-    @Test
-    void 빈_이름에_대한_예외_처리() {
-        assertSimpleTest(
-                () -> assertThatThrownBy(() -> runException("", "1")).isInstanceOf(IllegalArgumentException.class));
-    }
-
-    @Test
-    void 회수에_대한_예외_처리_2() {
+    void longLengthCarNameTest() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "영")).isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(String.format("%d글자 이하", Common.CAR_NAME_MAX_LENGTH.getIntValue()))
+                assertThatThrownBy(() -> runException("pobi,javaji", "1")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.OVER_LENGTH_ERROR.getMessage())
         );
     }
 
+    @DisplayName("공백 차 이름에 대한 예외 처리")
     @Test
-    void 회수에_대한_예외_처리_3() {
-        assertSimpleTest(() -> assertThatThrownBy(() -> runException("pobi,java", "qwe"))
-                .isInstanceOf(IllegalArgumentException.class));
+    void blankInputTypeForCarNameTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi", "1")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.EMPTY_ERROR.getMessage())
+        );
+    }
+
+    @DisplayName("공백 이름에 대한 예외 처리 2")
+    @Test
+    void blankInputTypeForCarNameTest2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ,pobi", "1")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.EMPTY_ERROR.getMessage())
+        );
+    }
+
+    @DisplayName("숫자 형식이 아닌 회수에 대한 예외 처리")
+    @Test
+    void isNotNumericTypeInputTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "영")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.NUMERIC_ERROR.getMessage())
+        );
+    }
+
+    @DisplayName("공백 회수에 대한 예외 처리")
+    @Test
+    void blankInputForAttemptsTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", " ")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.EMPTY_ERROR.getMessage()));
+    }
+    @DisplayName("0인 회수에 대한 예외 처리")
+    @Test
+    void zeroInputTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "0")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.UNDER_NUMBER_ERROR.getMessage()));
+    }
+
+    @DisplayName("음수인 회수에 대한 예외 처리")
+    @Test
+    void negativeNumberInputTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "-1")).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessages.UNDER_NUMBER_ERROR.getMessage()));
     }
 
     @Override
