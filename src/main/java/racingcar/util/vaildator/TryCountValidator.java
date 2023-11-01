775dd0849e57218c8e9f8215683e2c628c841d99
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 14:08:21 +0900

    feat: 아무것도 입력하지 않았을 때를 검증한다.

--- src/main/java/racingcar/util/vaildator/TryCountValidator.java
+++ src/main/java/racingcar/util/vaildator/TryCountValidator.java
@@ -1,12 +1,19 @@
 package racingcar.util.vaildator;
 
 public class TryCountValidator {
-    private static final String INPUT_TRYCOUNT_ZERO_EXCEPTION = "0 이상의 숫자를 입력해주세요.";
+    private static final String INPUT_TRYCOUNT_ZERO_EXCEPTION = "1 이상의 숫자를 입력해주세요.";
+    private static final String INPUT_TRYCOUNT_NOTHING_EXCEPTION = "아무것도 입력하지 않았습니다. 1 이상의 숫자를 입력해주세요.";
 
     public static void validateInputTryCount(String tryCount) {
         int inputTryCount = Integer.parseInt(tryCount);
         if (inputTryCount == 0) {
             throw new IllegalArgumentException(INPUT_TRYCOUNT_ZERO_EXCEPTION);
+        }
+    }
+
+    public static void validateInputTryCountNothing(String tryCount) {
+        if (tryCount.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_TRYCOUNT_NOTHING_EXCEPTION);
         }
     }
 }
