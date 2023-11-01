f97f5baac0f9359e4fdf333668f1df7ff4ddd6ab
KimSangHyeok <tkdgur0906@naver.com>
2023-11-01 12:00:33 +0900

    feat : Car 엔티티 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+public class Car {
+
+    private String carName;
+    private int moveCnt;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.moveCnt = 0;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getMoveCnt() {
+        return moveCnt;
+    }
+
+}
