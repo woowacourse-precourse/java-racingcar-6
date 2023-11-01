c237b3dc1a24eb90a2a88a224caca8a93eb676ee
Yeop <syt05342@naver.com>
2023-11-01 11:28:46 +0900

    feat: 최종 승자를 출력하는 로직 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     private final static String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String INPUT_TRY_MSG = "시도할 회수는 몇회인가요?";
     private final static String RESULT_MSG = "실행 결과";
+    private final static String END_MSG = "최종 우승자 : ";
 
     public static void printInputCarName() {
         System.out.println(INPUT_CAR_NAME_MSG);
@@ -19,11 +20,11 @@ public class OutputView {
         System.out.println(RESULT_MSG);
     }
 
-    public void printExceptionMsg(String errorMsg) {
-        System.out.println(errorMsg);
-    }
-
     public void printResult(String curResult) {
         System.out.println(curResult);
+    }
+
+    public void printWinner(String winnerNames) {
+        System.out.println(END_MSG + winnerNames);
     }
 }
