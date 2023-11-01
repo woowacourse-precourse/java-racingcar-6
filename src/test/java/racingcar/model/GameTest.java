5462d9a6d85fb21c597b175b39466ba1047debb1
ihun-il <hunil9978@gmail.com>
2023-11-01 21:55:36 +0900

    fixed: class 이름 변경

--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -1,16 +1,15 @@
 package racingcar.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class ComputerTest {
+public class GameTest {
 
     @Test
     public void makeRandomNumberTest() {
-        Computer computer = new Computer();
-        int randomNumber = computer.makeRandomNumber();
+        Game game = new Game();
+        int randomNumber = game.makeRandomNumber();
         assertThat(randomNumber).isBetween(0, 9);
     }
 }
