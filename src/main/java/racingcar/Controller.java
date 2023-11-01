a4da3fec2cca47d0a44e06c2c278618a1a4c87e9
JH CHO <junh9541@gmail.com>
2023-11-01 20:32:26 +0900

    feat(Controller): 레이스 진행 기능 추가

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -6,16 +6,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
+    int tryTime;
+    Cars cars = new Cars();
     public void startRace(){
-        ArrayList<String> carNames = getCarsNameInput();
+        Message.NameInputMsg();
+        ArrayList<String> carNameList = splitCarNames(getInput());
+        Validation.validateCarNames(carNameList);
+
+        Message.RaceTryTimeInputMsg();
+        String tryTimeInput = getInput();
+        Validation.validateTrytime(tryTimeInput);
+        tryTime = convertStrToInt(tryTimeInput);
+
+        cars.createCars(carNameList);
+
+        for (int idx=0; idx<tryTime; idx++){
+            String interimResult = cars.doRace();
+            Message.raceInterimResultMsg(interimResult);
+        }
+
+        Message.finalWinnerMsg(cars.CheckFinalWinner());
+
     }
-    public ArrayList<String> getCarsNameInput(){
+    public String getInput(){
         String nameInput = Console.readLine();
 
         if (nameInput == null) {
             throw new IllegalArgumentException("Input is null");
         }
-        ArrayList<String> names = new ArrayList<>(Arrays.asList(nameInput.split(",", -1))) ;;
+        return nameInput;
+    }
+
+    private ArrayList<String> splitCarNames(String carNames){
+
+        ArrayList<String> names = new ArrayList<>(Arrays.asList(carNames.split(",", -1))) ;;
         return names;
     }
+
+    private int convertStrToInt(String time){
+        return Integer.parseInt(time);
+    }
+
 }
