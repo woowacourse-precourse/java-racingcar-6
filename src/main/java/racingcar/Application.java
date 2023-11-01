8ff5a10c7d7a399902a3ddea6b4eb2a565debf49
SuhyungK <dook4966@gmail.com>
2023-11-01 17:36:22 +0900

    feat: 입력받은 자동차의 이름 저장 Cars 클래스의 인스턴스 변수 → 클래스 변수로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,19 +6,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 class Cars {
-    public Map<String, Integer> raceCars = new HashMap<>();
+    public static Map<String, Integer> raceCars = new HashMap<>();
 
-    public Cars(String[] inputCars) {
-        for (String car: inputCars) {
-            this.raceCars.put(car, 0);
+    public static void addCars(String[] inputCarNames) {
+        for (String carName: inputCarNames) {
+            raceCars.put(carName, 0);
         }
     }
 }
 
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Cars cars = new Cars(Console.readLine().split(","));
-        
+        String[] inputCarNames = Console.readLine().split(",");
+        Cars.addCars(inputCarNames);
+
+
     }
 }
