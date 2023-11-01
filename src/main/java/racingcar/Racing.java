3f20af02470cc8cb247763b0c474e7a92daadf3d
minnjung0887921 <minjeong0887921@gmail.com>
2023-11-01 22:39:36 +0900

    refactor : 기능으로 따로 분리했던 파일 삭제

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,14 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import utils.CarRaceLog;
-import utils.RandomMovement;
 
 import java.util.ArrayList;
 
 public class Racing {
     public static void runRace(ArrayList<String> carDistanceList) {
         for (int i = 0 ; i < carDistanceList.size(); i++) {
-            if (RandomMovement.moveForwardOrNot()) {
+            if (Randoms.pickNumberInRange(0, 9) > 3) {
                 CarRaceLog.updateCarDistanceList(i, carDistanceList);
             }
             System.out.println(carDistanceList.get(i));
--- src/main/java/utils/RandomMovement.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class RandomMovement {
-    public static boolean moveForwardOrNot() {
-        return Randoms.pickNumberInRange(0, 9) > 3;
-    }
-}
