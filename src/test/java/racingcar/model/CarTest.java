d1ef4eb215bd6117db4640c79904732738f1efd5
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 12:32:02 +0900

    2023.11.01_test : CarTest 전진_정지 테스트 코드 작성

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,11 +1,15 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import static org.mockito.Mockito.*;
 
 public class CarTest {
 
@@ -28,5 +32,16 @@ public class CarTest {
     @Test
     void 전진_정지() {
 
+        String name = "test1";
+
+        // Utils 클래스의 getRandom 메서드를 목(Mock) 객체로 대체
+        Utils utils = mock(Utils.class);
+        when(utils.getRandomNumber()).thenReturn(6); // 원하는 반환 값을 설정
+
+        Car car = new Car(name);
+        car.moveForward();
+
+        // position 값이 1 증가 했는지 확인
+        Assertions.assertEquals(1, car.getPosition());
     }
 }
