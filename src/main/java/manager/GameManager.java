bc35b8e43364220695b3f3ba6a39b3b222e7f0be
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-01 10:47:32 +0900

    feat : GameManager 생성자 구현

---
+++ src/main/java/manager/GameManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import custom_object.Car;
+
+import java.util.List;
+
+public class GameManager {
+    private List<Car> carList;
+
+    public GameManager(List<Car> carList) {
+        this.carList = carList;
+    }
+}
