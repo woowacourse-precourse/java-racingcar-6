56bbc05665016f992c561fdd1ec1933822b2183e
용가리 <phoenixgiha@gmail.com>
2023-11-01 16:52:29 +0900

    f

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.game.RacingCarGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarGame newgame = new RacingCarGame();
+        newgame.run();
     }
 }
