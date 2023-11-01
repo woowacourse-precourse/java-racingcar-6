205b81b27bb559c804eb4656d0d6765e25fdc6f9
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-01 22:19:51 +0900

    feat(Round) : creat Round

---
+++ src/main/java/racingcar/model/Round.java
@@ -0,0 +1,16 @@
+package racingcar.model;
+
+public class Round {
+    Integer numberOfRound;
+
+    public Round(Integer numberOfRound) {
+        this.numberOfRound = numberOfRound;
+    }
+
+    public Boolean IsNotEnd(){
+        return (numberOfRound >= 0);
+    }
+    public void finishARound(){
+        numberOfRound--;
+    }
+}
