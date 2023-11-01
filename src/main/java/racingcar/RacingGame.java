969e99271b1154e823da93292b99c2ff9d8edda4
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 21:29:15 +0900

    feat : RacingCar 리스트 변경 및 init 함수 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,20 +3,27 @@ package racingcar;
 import racingcar.io.InputHandler;
 import racingcar.io.OutputHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingGame extends Game{
-    private String[] carNames;
+    private final List<RacingCar> car = new ArrayList<>();
     private int tryCount;
 
     @Override
     public void initGame() {
-        carNames = InputHandler.getCarNames("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)").split(",");
-        tryCount = InputHandler.getTryCount("시도할 회수는 몇회인가요?");
+        String[] carNames = InputHandler.getCarNames("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        
     }
 
     @Override
     public void playGame(){
-       
+
+    }
+
+    public void printCarPosition(RacingCar car) {
+        OutputHandler(RacingCar.getName() + " : ");
+        for(int i = 0; i < car.get)
     }
 }
