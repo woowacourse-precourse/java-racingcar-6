efe1838f21bc64ffb73fd2f2a6f3088a32337adb
inthejim <inthejim24@gmail.com>
2023-11-01 21:41:38 +0900

    refactor: CarName 클래스 분리

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -6,9 +6,6 @@ import java.util.List;
 public class Controller {
     private Race race;
     private static final String DELIMITER_COMMA = ",";
-    private static final int MAX_LENGTH_CAR_NAME = 5;
-    private static final String ERROR_BLANK_CAR_NAME = "자동차 이름은 공백은 불가합니다.";
-    private static final String ERROR_MAX_LENGTH_CAR_NAME = "자동차 이름은 5자 이하만 가능합니다.";
 
     private List<Car> getInputForCarNames() {
         String inputForCarName = View.getCarNames();
@@ -16,20 +13,11 @@ public class Controller {
         return createCarsFromCarNames(carNames);
     }
 
-    private void validateCarName(String carName) {
-        if (carName.isBlank()) {
-            throw new IllegalArgumentException(ERROR_BLANK_CAR_NAME);
-        }
-        if (carName.length() > MAX_LENGTH_CAR_NAME) {
-            throw new IllegalArgumentException(ERROR_MAX_LENGTH_CAR_NAME);
-        }
-    }
-
     private List<Car> createCarsFromCarNames(String[] carNames) {
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
-            validateCarName(carName);
-            cars.add(new Car(carName));
+            CarName carNameObject = new CarName(carName);
+            cars.add(new Car(carNameObject.getName()));
         }
         return cars;
     }
