d1a0bf7808863246d4e772e88c9ef0f366a801d5
Seyoung Goo <0917sy@gmail.com>
2023-11-01 18:26:12 +0900

    refactor(input): CarNamesInputConverter 수정
    - 변수, 메소드 이름 수정
    - 메소드 분리

--- src/main/java/racingcar/util/CarNamesInputConverter.java
+++ src/main/java/racingcar/util/CarNamesInputConverter.java
@@ -5,31 +5,37 @@ import java.util.List;
 import java.util.Set;
 
 public class CarNamesInputConverter {
-    private static final String DELIMITER = ",";
-    private static final int NAME_MIN_LENGTH = 1;
-    private static final int NAME_MAX_LENGTH = 5;
-    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "유효하지 않은 입력값 오류. %s. %s";
-    private static final String INVALID_LENGTH_MESSAGE = "이름 길이는 %d 이상 %d 이하만 가능"
-            .formatted(NAME_MIN_LENGTH, NAME_MAX_LENGTH);
-    private static final String DUPLICATED_MESSAGE = "중복된 이름은 존재할 수 없음";
+    private static final String NAME_DELIMITER = ",";
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "유효하지 않은 입력값 입니다. %s. 입력값: %s";
+    private static final String INVALID_LENGTH_MESSAGE = "자동차 이름 길이는 %d 이상 %d 이하만 가능합니다"
+            .formatted(MIN_NAME_LENGTH, MAX_NAME_LENGTH);
+    private static final String DUPLICATED_MESSAGE = "중복된 이름은 존재할 수 없습니다";
+
+    private CarNamesInputConverter() {
+    }
 
     public static List<String> convert(String input) {
-        List<String> names = List.of(input.split(DELIMITER));
-        validateLength(names);
-        validateUnique(names);
+        List<String> names = List.of(input.split(NAME_DELIMITER));
+        validateNameLength(names);
+        validateNameUniqueness(names);
         return names;
     }
 
-    private static void validateLength(List<String> names) {
-        boolean anyInvalidLength = names.stream()
-                .anyMatch(v -> v.length() < NAME_MIN_LENGTH || v.length() > NAME_MAX_LENGTH);
-        if (anyInvalidLength) {
+    private static void validateNameLength(List<String> names) {
+        boolean hasInvalidLength = names.stream().anyMatch(name -> !isValidNameLength(name));
+        if (hasInvalidLength) {
             throw new IllegalArgumentException(
                     ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE.formatted(INVALID_LENGTH_MESSAGE, names));
         }
     }
 
-    private static void validateUnique(List<String> names) {
+    private static boolean isValidNameLength(String name) {
+        return name.length() >= MIN_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH;
+    }
+
+    private static void validateNameUniqueness(List<String> names) {
         Set<String> uniqueNames = new HashSet<>(names);
         if (uniqueNames.size() != names.size()) {
             throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE.formatted(DUPLICATED_MESSAGE, names));
