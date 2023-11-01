e77878ff74a1700407736ad0a446eadd2ba2b71c
dj <fjzl12@naver.com>
2023-11-01 18:41:01 +0900

    refactor(RoundValidator): 진행 횟수 검증 함수 세분화

--- src/main/java/racingcar/domain/RoundValidator.java
+++ src/main/java/racingcar/domain/RoundValidator.java
@@ -4,16 +4,24 @@ public class RoundValidator {
     public RoundValidator() {
     }
 
+    public static int validate(String number) {
+        int naturalNumber = checkNaturalNumber(number);
+        isUnderTwenty(naturalNumber);
+        return naturalNumber;
+    }
+
     public static int checkNaturalNumber(String number) {
-        int naturalNumber;
         try {
-            naturalNumber = Integer.parseInt(number);
-            if (naturalNumber < 1 || naturalNumber > 20) {
-                throw new Exception();
-            }
+            int naturalNumber = Integer.parseInt(number);
+            return naturalNumber;
         } catch (Exception e) {
             throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다.");
         }
-        return naturalNumber;
+    }
+
+    public static void isUnderTwenty(int number) {
+        if (number < 1 || number > 20) {
+            throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다. (최대 20회까지 가능합니다.)");
+        }
     }
 }
