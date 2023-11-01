5d31918dd9dddf898060d90dd0b5dc07df2b073c
Dawin <dawin002@naver.com>
2023-11-01 19:06:22 +0900

    test(InputViewTest): 사용자 입력 기능 테스트 수정
    사용자 입력 기능 테스트 수정:
    - assertThat().isEqualTo()에 전달되는 기대값 문자열 일부 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -46,7 +46,7 @@ class InputViewTest {
         String carNamesInput = InputView.readCarNames();
 
         //then
-        assertThat(getSystemOutput()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        assertThat(getSystemOutput()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" + "\n");
         assertThat(carNamesInput).isEqualTo("dawin,woowa,han");
     }
 
@@ -59,7 +59,7 @@ class InputViewTest {
         String carNamesInput = InputView.readRoundNumber();
 
         //then
-        assertThat(getSystemOutput()).isEqualTo("시도할 회수는 몇회인가요?\n");
+        assertThat(getSystemOutput()).isEqualTo("시도할 회수는 몇회인가요?" + "\n");
         assertThat(carNamesInput).isEqualTo("15");
     }
 }
