79430c8a386d401b6cf48538350c2f1c5b48a0fb
WOONBE <kepull2918@naver.com>
2023-11-01 13:45:34 +0900

    [refactor] 불필요한 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import racingcar.controller.PlayRacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         PlayRacingGame playRacingGame = new PlayRacingGame();
         playRacingGame.racingGame();
     }
