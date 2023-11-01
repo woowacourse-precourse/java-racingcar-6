bfbe311675f081083dfe59c00cc5abb574c96150
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-01 21:09:30 +0900

    feat(InputView): 자동차 이름 입력 뷰 예외 처리 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,17 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import racingcar.validator.InputValidator;
 
 public class InputView {
 
     public List<String> getCarNameList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        return List.of(input.split(","));
+        List<String> carNameList = Arrays.asList(input.split(","));
+        InputValidator.verifyCarNames(carNameList);
+        return carNameList;
     }
 }
