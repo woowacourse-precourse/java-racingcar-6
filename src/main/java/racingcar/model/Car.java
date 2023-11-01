1619de18b7bdea50f5b2ad56b58421d8e93805ea
DonghaOh <donghadarwin@naver.com>
2023-11-01 16:44:46 +0900

    feat: 자동차 이름으로 자동차 생성 - Car 생성자

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,5 +3,8 @@ package racingcar.model;
 public class Car {
     public String name;
     public int move;
-
+    public Car(String name) {
+        this.name = name;
+        this.move = 0;
+    }
 }
