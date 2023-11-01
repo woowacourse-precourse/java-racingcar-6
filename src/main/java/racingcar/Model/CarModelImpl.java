f6e6b29bdf70ca7a49736a46bc4bae25339b6dcf
sami355-24 <ddung355@gmail.com>
2023-11-01 13:01:50 +0900

    refactor(Model): 구체 타입 이름으로 선언되어있던 인스턴스명 수정
    carNameValidatorImpl -> carNameValidator

--- src/main/java/racingcar/Model/CarModelImpl.java
+++ src/main/java/racingcar/Model/CarModelImpl.java
@@ -4,20 +4,19 @@ import java.util.List;
 import racingcar.Domain.CarGroup;
 import racingcar.Factory.Factory;
 import racingcar.Validator.CarNameValidator;
-import racingcar.Validator.CarNameValidatorImpl;
 
 public class CarModelImpl implements CarModel {
 
     public Factory factory = new Factory();
-    private final CarNameValidator carNameValidatorImpl = factory.getCarNameValidator();
+    private final CarNameValidator carNameValidator = factory.getCarNameValidator();
     private static final String DELIMITER = ",";
     public CarGroup preProcessing(String carNames) {
         List<String> carNameList = divideCarNameByDelimiter(carNames);
         for (String carName : carNameList) {
-            carNameValidatorImpl.isValidLength(carName);
-            carNameValidatorImpl.hasWhiteSpace(carName);
+            carNameValidator.isValidLength(carName);
+            carNameValidator.hasWhiteSpace(carName);
         }
-        carNameValidatorImpl.hasDuplicateName(carNameList);
+        carNameValidator.hasDuplicateName(carNameList);
         return CarGroup.of(carNameList);
     }
     public List<String> divideCarNameByDelimiter(String carName) {
