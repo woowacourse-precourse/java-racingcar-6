26eaaedd645898085bd2f22a5a3e2229c2761e9a
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:57:13 +0900

    test(CarTest): Car 객체 생성에 대한 단위 테스트 작성
    1. Car_객체_생성_테스트
    2. Car_객체_생성_실패
    -> 널 인 경우

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,6 +2,23 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class CarTest {
 
+    @Test
+    void Car_객체_생성_테스트() {
+        try {
+            Car car = new Car(new Name("픽업 트럭"));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void Car_객체_생성_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car(null);
+        });
+    }
 }
