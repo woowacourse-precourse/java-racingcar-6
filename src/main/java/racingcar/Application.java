48c90e8f78398d193a3a77e55634efbdd9049643
youngchive <2000mily@naver.com>
2023-11-01 19:25:10 +0900

    feat(SetRound): create setRound

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int n = new RandomNum().RandomNum();
-        System.out.println(n);
         SetName.setCarNames();
+        int r = new SetRound().setRound();
     }
 }
--- src/main/java/racingcar/RandomNum.java
+++ src/main/java/racingcar/RandomNum.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNum {
     public int RandomNum() {
         int num = Randoms.pickNumberInRange(0, 9);
+        System.out.println(num);
         return num;
     }
 
