3d4e60f2909af228f4c7f810f9efc9bf55e62919
jihun <pjh2996@naver.com>
2023-11-01 10:52:36 +0900

    test : 숫자에 따라 전진,스탑 기능 테스트 통합

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,52 +1,23 @@
 package racingcar.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarsTest {
 
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-
-    @Test
-    public void 전진_스탑_전진() {
+    @ParameterizedTest
+    @CsvSource({"9,3,1", "4,2,1", "1,2,0", "2,3,0", "4,4,2", "0,9,1"})
+    public void 전진_또는_스탑_테스트(int move1, int move2, int expected) {
         //given
         Cars car = new Cars("JiHun", 0);
 
         //when
-        car.move(MOVING_FORWARD);
-        car.move(STOP);
-        car.move(MOVING_FORWARD);
+        car.move(move1);
+        car.move(move2);
+        int actual = car.getDistance();
 
         //then
-        Assertions.assertThat(2).isEqualTo(car.getDistance());
-    }
-
-    @Test
-    public void 숫자_4_미만_전진_불가능() {
-        //given
-        Cars car = new Cars("JiHun", 0);
-
-        //when
-        car.move(2);
-        car.move(3);
-        car.move(1);
-
-        //then
-        Assertions.assertThat(0).isEqualTo(car.getDistance());
-    }
-
-    @Test
-    public void 숫자_4_이상_전진_가능() {
-        //given
-        Cars car = new Cars("JiHun", 0);
-
-        //when
-        car.move(4);
-        car.move(5);
-        car.move(6);
-
-        //then
-        Assertions.assertThat(3).isEqualTo(car.getDistance());
+        Assertions.assertThat(expected).isEqualTo(actual);
     }
 }
