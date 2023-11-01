d83723104d204c5c5d2f137e766f4d0251512252
Minsu Choi <cms102510@naver.com>
2023-11-01 17:18:13 +0900

    test: InputViewTest 주석 제거

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -12,32 +12,26 @@ public class InputViewTest {
 
     @BeforeEach
     void setUp() {
-        Console.close(); // Scanner 초기화
+        Console.close();
     }
 
     @Test
     void 자동차_이름_입력값_확인() {
-        // Given
         String input = "pobi,crong,honux";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        // When
         String carNames = InputView.carInput();
 
-        // Then
         assertThat(carNames).isEqualTo(input);
     }
 
     @Test
     void 시도_회수_입력값_확인() {
-        // Given
         String input = "5";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        // When
         int retryCount = Integer.parseInt(InputView.tryCountInput());
 
-        // Then
         assertThat(retryCount).isEqualTo(5);
     }
 }
