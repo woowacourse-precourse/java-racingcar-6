2bfcd363619dde5a0e9d5ba2d678b73a0bdbc0db
JGM <cnxw4570123@gmail.com>
2023-11-01 16:30:23 +0900

    test : add test code for Game class Game 클래스에 대한 테스트 코드 추가 - printWinner()메소드가 공동 우승의 경우 여러 명을 잘 출력하는지 확인함. - getMoveCount() 메소드가 숫자가 아닌 경우 예외를 발생시키는지 확인함. - inputToNameList() 메소드가 중복의 경우 처리를 못하는 것을 확인함.   - 중복된 이름이 불가능하도록 수정할 예정

--- src/test/java/racingcar/MyTest.java
+++ src/test/java/racingcar/MyTest.java
@@ -17,6 +17,7 @@ import org.mockito.MockedStatic;
 
 class MyTest extends NsTest {
     private final GameUtil gameUtil = new GameUtil();
+
     @Test
     void 한_칸_움직이면_한_줄_추가된다() {
         Racer racer = new Racer("이름");
@@ -26,7 +27,7 @@ class MyTest extends NsTest {
     }
 
     @Test
-    void 이름이_너무_깁니다(){
+    void 이름이_너무_깁니다() {
         String input = "엄청긴이름입니다";
         assertThatThrownBy(() -> gameUtil.inputToNameList(input))
                 .hasMessageContaining("이름은 한 글자 이상 다섯 글자 이하여야 합니다.")
@@ -34,7 +35,7 @@ class MyTest extends NsTest {
     }
 
     @Test
-    void 이름이_너무_짧습니다(){
+    void 이름이_너무_짧습니다() {
         assertThatThrownBy(() -> gameUtil.inputToNameList(" , "))
                 .hasMessageContaining("이름은 한 글자 이상 다섯 글자 이하여야 합니다.")
                 .isInstanceOf(IllegalArgumentException.class);
@@ -42,7 +43,7 @@ class MyTest extends NsTest {
 
 
     @Test
-    void 차수_결과를_출력합니다(){
+    void 차수_결과를_출력합니다() {
         List<Racer> racerList = new ArrayList<>();
         racerList.add(new Racer("첫 번째"));
         racerList.add(new Racer("김"));
@@ -54,16 +55,54 @@ class MyTest extends NsTest {
     }
 
     @Test
-    void 방장_사기_맵_세_차수_실행한다(){
-        try(MockedStatic<Console> console = mockStatic(Console.class);
-            MockedStatic<Randoms> random = mockStatic(Randoms.class)) {
+    void 방장_사기_맵_세_차수_실행한다() {
+        try (MockedStatic<Console> console = mockStatic(Console.class);
+             MockedStatic<Randoms> random = mockStatic(Randoms.class)) {
             when(Console.readLine()).thenReturn("방장,유저1,유저2", "3");
             when(Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(4, 4, 3, 4, 4, 3, 4, 3, 4);
             Game game = new Game();
             game.gameStart();
         }
-        assertThat(output()).contains("방장 : -", "유저1 : -", "유저2 : ", "방장 : --", "유저1 : --", "유저2 : ", "방장 : ---", "유저1 : ",
+        assertThat(output()).contains("방장 : -", "유저1 : -", "유저2 : ", "방장 : --", "유저1 : --", "유저2 : ", "방장 : ---",
+                "유저1 : ",
                 "유저2 : -", "최종 우승자 : 방장");
+    }
+
+    @Test
+    void 세_명_모두_우승입니다() {
+        try (MockedStatic<Console> console = mockStatic(Console.class);
+             MockedStatic<Randoms> random = mockStatic(Randoms.class)) {
+            when(Console.readLine()).thenReturn("유저1,유저2,유저3", "3");
+            when(Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(4);
+            Game game = new Game();
+            game.gameStart();
+        }
+        assertThat(output()).contains("유저1 : -", "유저2 : -", "유저3 : -", "유저1 : --", "유저2 : --", "유저3 : --", "유저1 : ---",
+                "유저2 : ---",
+                "유저3 : ---", "최종 우승자 : 유저1, 유저2, 유저3");
+    }
+
+    @Test
+    void 올바른_횟수를_입력해주세요() {
+        assertThatThrownBy(() -> {
+            try (MockedStatic<Console> console = mockStatic(Console.class)) {
+                when(Console.readLine()).thenReturn("이름1,이름2,이름3", "숫자가아닙니다.");
+                Game game = new Game();
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닙니다.");
+    }
+
+    @Test
+    void 이름이_중복될_수_없습니다(){
+        assertThatThrownBy(() -> {
+            try (MockedStatic<Console> console = mockStatic(Console.class)) {
+                when(Console.readLine()).thenReturn("중복,중복,중복", "3");
+                Game game = new Game();
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
