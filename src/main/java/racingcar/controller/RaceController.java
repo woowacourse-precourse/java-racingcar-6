d4ee941e8c556ca4eb41df69667529436cdda750
kangaroo0 <ljw42871@gmail.com>
2023-11-01 19:48:44 +0900

    Feat: 유저의 첫 input 받아서 List로 처리

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,6 +1,20 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
 public class RaceController {
+
+
+    public RaceController() {
+    }
+
+    public void gameStart(){
+
+        InputView.getInput();
+
+
+
+    }
 
 
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,40 @@
 package racingcar.view;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private static void getStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void getInput(){
+
+        getStartMessage();
+
+        try {
+            String playersInput = Console.readLine();
+            //exception
+
+
+            List<String> playersList = makePlayersList(playersInput);
+
+        } catch (IllegalArgumentException e){
+
+            throw new IllegalArgumentException(e.getMessage());
+
+        }
+    }
+
+    private static List<String> makePlayersList(String playersInput){
+
+
+        return Arrays.stream(playersInput.split(",")).toList();
+
+    }
 }
