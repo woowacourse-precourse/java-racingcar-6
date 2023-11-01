ba624f8de0b89858f2c9f3bd79eb1d61065ae6c1
Minyongggg <naminyong97@gmail.com>
2023-11-01 18:03:51 +0900

    feat: Car 클래스 생성자 및 getter 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,18 @@
 package racingcar.domain;
 
 public class Car {
+    private final String name;
+    private Integer position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
 }
