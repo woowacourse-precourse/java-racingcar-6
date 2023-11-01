a0eaa7638d24dbf9edbb9950e7285381b9e255f5
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 10:47:15 +0900

    [feat] Service - 우승자 선정 메소드 로직 추가

--- src/main/java/racingcar/Service/RacingCarService.java
+++ src/main/java/racingcar/Service/RacingCarService.java
@@ -1,17 +1,37 @@
 package racingcar.Service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.Domain.Car;
 import racingcar.Domain.Race;
 
 public class RacingCarService {
-    public void playRaceAndIncreaseTrialCount(Race race) {
+    public List<String> playAndSelectWinners(Race race) {
+        List<String> winners = new ArrayList<>();
+
         for (Car car : race.getCars()) {
             if (4 <= generateRandomNumber()) {
                 car.carMove(1);
             }
         }
+
         race.increaseTrialCount();
+
+        int maxPosition = -1;
+        for (Car car : race.getCars()) {
+            if (maxPosition < car.getCarPosition()) {
+                maxPosition = car.getCarPosition();
+            }
+        }
+
+        for (Car car : race.getCars()) {
+            if (car.getCarPosition() == maxPosition) {
+                winners.add(car.getCarName());
+            }
+        }
+
+        return winners;
     }
 
     private int generateRandomNumber() {
