e4c241b665da2bc820c98f57564241ad945ffb5b
qudwns017 <qudwns017@naver.com>
2023-11-01 22:15:31 +0900

    Test: 자동차 정상 입력 테스트

---
+++ src/test/java/racingcar/model/RacingControllerTest.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingControllerTest {
+
+    @Test
+    @DisplayName("자동차 입력 테스트")
+    void createCar() {
+        String name = "pobi";
+        Car car = new Car(name);
+
+        assertThat(car).isNotNull();
+    }
+}
