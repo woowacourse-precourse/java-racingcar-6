3a0e50dce5f447dcc409b9add79c06ce5e11c3f0
minkyeong <stl9955@naver.com>
2023-11-01 21:28:13 +0900

    Feat(GameInterface): play 메서드 구현

--- src/main/java/racingcar/GameInterface.java
+++ src/main/java/racingcar/GameInterface.java
@@ -1,12 +1,17 @@
 package racingcar;
 
 import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+
 import java.util.ArrayList;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class GameInterface {
-    public void play(){}
+    public void play(){
+        RacingGame racingGame =  new RacingGame(initializeCars(), getNumberOfRepetition());
+        racingGame.startRace();
+    }
 
     private static String[] getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
