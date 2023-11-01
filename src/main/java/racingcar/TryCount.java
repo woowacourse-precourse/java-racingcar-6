be5b97b90bfff7701d2907a8abd1f98bdf0033ee
songyi <song21677@naver.com>
2023-11-01 21:30:03 +0900

    refactor: 시도 횟수 자릿수 확인 기능 목록 수정 테스트코드 작성 이름 변경 및 최대 길이 상수화

--- src/main/java/racingcar/TryCount.java
+++ src/main/java/racingcar/TryCount.java
@@ -5,25 +5,46 @@ import java.util.regex.Pattern;
 
 public class TryCount {
 
-	private static final String ZERO = "0";
-	private static final String ZEROS = "^0+$";
-	private static final String ZERO_HOLDER = "^0+";
-	Pattern pattern = Pattern.compile(ZEROS);
-	private int tryCount;
+    private static final String ZERO = "0";
+    private static final String ZEROS = "^0+$";
+    private static final String ZERO_HOLDER = "^0+";
+    Pattern pattern = Pattern.compile(ZEROS);
+    private static final int MAX_LENGTH = 9;
+    private int tryCount;
 
-	public TryCount(String input) {
-		tryCount = Integer.parseInt(refine(input));
-	}
+    public TryCount(String input) {
+        String refinedInput = refine(input);
+        validate(refinedInput);
+        tryCount = Integer.parseInt(refinedInput);
+    }
 
-	private String refine(String input) {
-		Matcher matcher = pattern.matcher(input);
-		if (matcher.matches()) {
-			return ZERO;
-		}
-		return input.replaceFirst(ZERO_HOLDER, "");
-	}
+    private void validate(String refinedInput) {
+        checkBlank(refinedInput);
+        checkLength(refinedInput);
+    }
 
-	public int getTryCount() {
-		return tryCount;
-	}
+    private String refine(String input) {
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            return ZERO;
+        }
+        return input.replaceFirst(ZERO_HOLDER, "");
+    }
+
+    private void checkBlank(String refinedInput) {
+        if (refinedInput.isBlank()) {
+            throw new IllegalArgumentException("시도 횟수를 입력해주세요.");
+        }
+    }
+
+    private void checkLength(String answer) {
+        if (answer.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("시도 횟수는 " + MAX_LENGTH +"자리 이하의 수여야 합니다.");
+        }
+    }
+
+
+    public int getTryCount() {
+        return tryCount;
+    }
 }
--- src/test/java/racingcar/TryCountTest.java
+++ src/test/java/racingcar/TryCountTest.java
@@ -1,14 +1,41 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TryCountTest {
 
-	@Test
-	void 시도_횟수_앞에_있는_0_제거_테스트() {
-		assertThat(new TryCount("000123456789").getTryCount())
-				.isEqualTo(123456789);
-	}
+    @Test
+    void 시도_횟수_앞에_있는_0_제거_테스트() {
+        assertThat(new TryCount("000123456789").getTryCount())
+                .isEqualTo(123456789);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 시도_횟수_비어있을_경우_테스트(String tryCount) {
+        assertThatThrownBy(() -> new TryCount(tryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"000123456789", "123456789"})
+    void 시도_횟수_자릿수_성공_테스트(String tryCount) {
+        assertThatCode(() -> new TryCount(tryCount))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 시도_횟수_자릿수_실패_테스트() {
+        assertThatThrownBy(() -> new TryCount("1234567890"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이하의 수여야 합니다.");
+    }
+
 }
