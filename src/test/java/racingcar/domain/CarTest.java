84a45188c68314f6b3947b4b2fa6fc7cc458eab2
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-01 16:30:44 +0900

    test(CarTest): 자동차의 현재 위치를 출력하는 기능 검증 로직 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,17 +2,27 @@ package racingcar.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
     private Car car;
 
     @BeforeEach
     void setUp() {
+        System.setOut(new PrintStream(outContent));
         car = new Car("TestCar");
     }
 
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
 
     @Test
     void Car_자동차가_움직인경우_필드멤버_갱신을_확인() {
@@ -27,4 +37,16 @@ class CarTest {
         car.canForward(Car.MIN_NUMBER_TO_MOVE - 1);
         assertEquals(initalLocation, car.getLocation());
     }
+
+    @Test
+    void Car_현재_위치만큼의_막대바를_출력한다() {
+        car.canForward(Car.MIN_NUMBER_TO_MOVE);
+        car.canForward(Car.MIN_NUMBER_TO_MOVE);
+        car.printLocation(car);
+        String expectedOutput = "TestCar : " + "-".repeat(2) + "\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    
 }
