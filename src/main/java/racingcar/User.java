9a0865f0d3d685a1f2adb90d1ff1efc9d7a87021
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-01 15:14:30 +0900

    [FEAT] 입력 값에 대한 예외 처리 구현
    입력 값이 null이면 예외 처리, moveCount에 0이나 String이 있을 경우에는 예외 처리. 예외처리와 게임 시작 메세지를 합쳐 inputCarNames와 inputMoveCount 함수를 구현해서 가독성이 더 좋게 구현.

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -3,9 +3,31 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class User {
     static final String DELIMITER = ",";
+
+    public static List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String input = input();
+        isNull(input);
+        return splitInput(input);
+    }
+
+    public static int inputMoveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = input();
+        isNull(input);
+        try {
+            int moveCount = stringToInteger(input);
+            isZero(moveCount);
+            return moveCount;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+
 
     public static String input() {
         return Console.readLine();
@@ -15,11 +37,23 @@ public class User {
         return Arrays.asList(input.split(DELIMITER));
     }
 
-    public static int stringToInteger() throws IllegalArgumentException {
+    public static int stringToInteger(String input) {
         try {
-            return Integer.parseInt(input());
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("문자가 아닌 숫자를 입력해 주세요.");
+        }
+    }
+
+    public static void isNull(String input) {
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    public static void isZero(int moveCount) {
+        if (moveCount == 0) {
+            throw new IllegalArgumentException("입력회수는 0이 아닌 수를 입력해 주세요.");
         }
     }
 }
