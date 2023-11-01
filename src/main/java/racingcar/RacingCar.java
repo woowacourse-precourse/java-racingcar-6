066c1abac3ec22438a189528d7a00873b53b24c9
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 20:44:40 +0900

    feat : move 함수 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,8 +4,13 @@ public class RacingCar {
     private String name;
     private int position = 0;
 
-    public RacingCar(String name){
+    public RacingCar(String name) {
         this.name = name;
     }
-    
+
+    public void move(int distance) {
+        position += distance;
+    }
+
+
 }
