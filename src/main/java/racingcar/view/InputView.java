4f7363528589c94b54f2eca55efbf5dd3bd39221
LeeSohyeon <teabag759@gmail.com>
2023-11-01 20:22:08 +0900

    feat: implemented 'InputView' for user input handling Responsible for handling user input and displaying input prompts. 1. 'requestCarNamesMessage': Displays the input format for receiving car names from the user. 2. 'requestNumberOfTrialMessage': Displays the input format for receiving the number of trials from the user.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class InputView {
+    // 경주할 자동차 이름을 입력 요청
+    public static void requestCarNamesInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    // 시도 횟수 입력 요청
+    public static void requestNumberOfTrialsInput() {
+        System.out.println("시도할 회수는 몇 회인가요?");
+    }
+}
