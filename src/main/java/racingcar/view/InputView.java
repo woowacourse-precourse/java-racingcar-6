123dcc636a901c3a42ba22c071154a3ff51e0b56
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-01 23:20:31 +0900

    refactor: 자바 코드 컨벤션에 맞춰서 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,12 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getCarName(){
+    public static String getCarName() {
         OutputView.inputMessage();
         return Console.readLine();
     }
 
-    public static String getRound(){
+    public static String getRound() {
         OutputView.repeatNumMessage();
         return Console.readLine();
     }
