116ddab5b2f8789aa5db88bfcc4aed74f024f8a9
seongnam <jseongnam1109@gmail.com>
2023-11-01 18:15:56 +0900

    viewOutputState

--- untitled/src/main/java/racingcar/view/viewOutputState.java
+++ untitled/src/main/java/racingcar/view/viewOutputState.java
@@ -1,4 +1,9 @@
 package racingcar.view;
 
 public class viewOutputState {
+    public void viewOutputState(String[] simulationStr){
+        for(int i=0; i<simulationStr.length;i++){
+            System.out.println(simulationStr[i]);
+        }
+    }
 }
