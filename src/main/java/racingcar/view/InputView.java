9363cf49831c790ebf4735658e2cc0b30842ff41
BOYOUNG-KANG <28287@naver.com>
2023-11-01 21:17:44 +0900

    refactor: 인풋 데이터 변환 위치 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,26 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.message.RaceMessage;
+import racingcar.message.ValidateErrorMessage;
 
 public class InputView {
 
-    public String getCars(){
+    public List<String> getCars(){
         System.out.println(RaceMessage.CARS_INPUT_MESSAGE);
-        return Console.readLine();
+        return Arrays.stream(Console.readLine().split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
-    public String getRaceCount(){
+    public int getRaceCount(){
         System.out.println(RaceMessage.RACE_COUNT_INPUT_MESSAGE);
-        return Console.readLine();
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ValidateErrorMessage.COUNT_TYPE_ERROR);
+        }
     }
 }
