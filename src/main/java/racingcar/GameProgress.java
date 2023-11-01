e4b67abf28f19a266213ef1270269abb93c9c417
kijen723 <kij2646@gmail.com>
2023-11-01 18:23:53 +0900

    feat(GameProgress): TODO8 done

--- src/main/java/racingcar/GameProgress.java
+++ src/main/java/racingcar/GameProgress.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class GameProgress {
     public static void startGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,5 +13,22 @@ public class GameProgress {
 
     public static void printResultMessage() {
         System.out.println("실행 결과");
+    }
+
+    public static void printCarsPositions(List<String> cars, List<Integer> carsPositions) {
+        for (int i = 0; i < cars.size(); i++) {
+            String car = cars.get(i);
+            int position = carsPositions.get(i);
+
+            System.out.printf("%s : ", car);
+
+            for (int j = 0; j < position; j++) {
+                System.out.print("-");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
