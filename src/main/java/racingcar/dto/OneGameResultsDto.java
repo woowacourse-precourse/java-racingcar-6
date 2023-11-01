eb8a0421b879fe0a9a7bca5fb9013a95e947a81b
june-777 <wlwhswnsrl96@gmail.com>
2023-11-01 10:01:18 +0900

    refactor(OneGameResultsDto): 변수명 변경, 불필요한 메서드 제거

--- src/main/java/racingcar/dto/OneGameResultsDto.java
+++ src/main/java/racingcar/dto/OneGameResultsDto.java
@@ -9,18 +9,14 @@ import racingcar.service.domain.Car;
 public class OneGameResultsDto {
     private final List<ResultDto> oneGameResults;
 
-    public OneGameResultsDto(final List<Car> cars) {
-        this.oneGameResults = convertToOneGameResults(cars);
+    public OneGameResultsDto(final List<Car> endCars) {
+        this.oneGameResults = convertToOneGameResults(endCars);
     }
 
-    private List<ResultDto> convertToOneGameResults(final List<Car> cars) {
-        return cars.stream()
+    private List<ResultDto> convertToOneGameResults(final List<Car> endCars) {
+        return endCars.stream()
                 .map(car -> new ResultDto(car.getName(), car.getPosition()))
                 .collect(Collectors.toList());
-    }
-
-    public int size() {
-        return oneGameResults.size();
     }
 
     public List<ResultDto> getResults() {
