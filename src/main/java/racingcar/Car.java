24d0496c78a219c3dccd5a5c1192cafa60944acb
JH CHO <junh9541@gmail.com>
2023-11-01 18:40:11 +0900

    feat(Car): name, location get 메소드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,16 @@ public class Car {
     private String name;
     private int location=0;
 
+    public Car(String carName){
+        name=carName;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getLocation(){
+        return location;
+    }
+
     public void tryMove(){
         int randomInt = Randoms.pickNumberInRange(0,9);
         if (randomInt>=4){
