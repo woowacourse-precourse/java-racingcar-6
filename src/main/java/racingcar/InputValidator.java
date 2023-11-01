cb8bb30f28ddb9a6ecd75d4c3eb74b5f910b965f
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 16:55:25 +0900

    feat : 중복값이 입력된 경우 예외 처리

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputValidator {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
+
     public boolean isSingleCarInput(List<String> carNames) {
         return carNames.size() < 2;
     }
@@ -21,6 +24,15 @@ public class InputValidator {
         for (String name : carNames) {
             if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void checkForDuplicates(List<String> carNames) {
+        Set<String> set = new HashSet<>();
+        for (String name : carNames) {
+            if (!set.add(name)) {
+                throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
             }
         }
     }
