2873bc1fd30369dd3b36ad34c363d2301490e941
DSRN0000 <dsrn0000@gmail.com>
2023-11-01 19:32:57 +0900

    2주차 프리코스 최종제출

--- src/main/java/racingcar/View/User_Input.java
+++ src/main/java/racingcar/View/User_Input.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class User_Input {
