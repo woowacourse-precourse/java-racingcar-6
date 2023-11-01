d85bb41e9c97862e59310eb0b25d38d6103083ae
김현재 <khjmr@naver.com>
2023-11-01 15:26:02 +0900

    fix(output): 접근 제어자 private 에서 public 으로 변경

--- src/main/java/racingcar/view/output/Output.java
+++ src/main/java/racingcar/view/output/Output.java
@@ -1,11 +1,11 @@
 package racingcar.view.output;
 
 public class Output {
-    private void printInputCarName() {
+    public void printInputCarName() {
         System.out.println(OutputConstants.INPUT_CAR_NAME);
     }
 
-    private void printInputTryNumber() {
+    public void printInputTryNumber() {
         System.out.println(OutputConstants.INPUT_TRY_NUMBER);
     }
 }
