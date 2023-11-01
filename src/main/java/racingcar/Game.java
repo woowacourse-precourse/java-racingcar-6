caf2cc6e5e5f62da0d0b5a4e072eae70b8663e12
yes-ee <yes0112@naver.com>
2023-11-01 13:39:15 +0900

    feat(game): 우승자 선택 기능 추가
    Game 클래스에 announceWinner() 추가
    - checkMaxLen() 에서 가장 멀리 간 자동차 거리 확인
    - getWinnerList() 로 우승자 리스트 반환
    
    getWinnerList() 에서 우승자 리스트 확인 가능

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,6 +1,10 @@
 package racingcar;
 
+import static java.lang.Math.max;
+
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map.Entry;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -23,6 +27,8 @@ public class Game {
         for (int i = 0; i < totalCount; i++) {
             doOneRound();
         }
+
+        announceWinner();
     }
 
     private void doOneRound() {
@@ -38,5 +44,45 @@ public class Game {
         }
 
         System.out.println();
+    }
+
+    private void announceWinner() {
+        int maxLen = checkMaxLen(carList);
+
+        List<String> winnerList = getWinnerList(maxLen, carList);
+
+        for (String winner : winnerList) {
+            System.out.println(winner);
+        }
+
+//        outputView.printWinner(winnerList);
+    }
+
+    private int checkMaxLen(LinkedHashMap<String, String> carList) {
+        int maxLen = 0;
+
+        for (Entry<String, String> carResult : carList.entrySet()) {
+            int len = carResult.getValue().length();
+
+            maxLen = max(maxLen, len);
+        }
+
+        return maxLen;
+    }
+
+    private List<String> getWinnerList(int len, LinkedHashMap<String, String> carList) {
+        List<String> winnerList = new ArrayList<>();
+
+        for (Entry<String, String> carResult : carList.entrySet()) {
+            if (isWinner(len, carResult)) {
+                winnerList.add(carResult.getKey());
+            }
+        }
+
+        return winnerList;
+    }
+
+    private boolean isWinner(int len, Entry<String, String> carResult) {
+        return carResult.getValue().length() == len;
     }
 }
