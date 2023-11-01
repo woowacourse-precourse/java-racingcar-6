4f3bb0a796d0cf0d87cf4d71e4408042b50beb1e
조은별 <joeunbyeol98@gmail.com>
2023-11-01 17:01:27 +0900

    test(CarServiceImplTest): carDao 초기화 코드 추가

--- src/test/java/racingcar/model/service/CarServiceImplTest.java
+++ src/test/java/racingcar/model/service/CarServiceImplTest.java
@@ -14,6 +14,7 @@ class CarServiceImplTest {
 
     @BeforeEach
     void setUp() {
+        carDao.init();
         carService.registerCarLineUp("pobi,woni,jun");
         carDao.increaseMoveCount("pobi");
         carDao.increaseMoveCount("jun");
