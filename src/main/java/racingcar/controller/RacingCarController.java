23671e68cf63c1ad9e2506dc1c072377dd86eb06
COBI-98 <tkdgus968@naver.com>
2023-11-01 16:15:33 +0900

    feat: 게임을 시작하면, 경주할 자동차를 생성할 수 있다.

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -6,6 +6,12 @@ public class RacingCarController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
     public void startGame(){
+        Cars racingCar = createRacingCar();
+    }
+    private Cars createRacingCar() {
+        outputView.printRequestCarNames();
+        String[] carNames = inputView.inputCarNames();
+        return CarFactory.createCars(carNames);
     }
 
 
