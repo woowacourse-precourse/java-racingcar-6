ac961fe149389b1e354dd1d13b7e36dec5bf3f70
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-01 23:18:38 +0900

    fix: 테스트코드 수정

--- src/test/java/racingcar/user/ValidatorTest.java
+++ src/test/java/racingcar/user/ValidatorTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -14,12 +11,10 @@ class ValidatorTest {
     Validator validator = new Validator();
     @Test
     void 이름_길이_체크() {
-        String input = "aaa,bbb,ccccc,dddddd";
+        String input = "123456";
         assertThrows(IllegalArgumentException.class,()->{
             validator.carNameCheck(input);
         });
-
-
     }
     @Test
     void 이름_중복_체크(){
@@ -30,8 +25,22 @@ class ValidatorTest {
         });
     }
     @Test
+    void 이름사이_공백_체크() {
+        String input = "dd dd";
+        assertThrows(IllegalArgumentException.class,()->{
+            validator.carNameCheck(input);
+        });
+    }
+    @Test
+    void 특수문자체크() {
+        String input = "d?ddd";
+        assertThrows(IllegalArgumentException.class,()->{
+            validator.carNameCheck(input);
+        });
+    }
+    @Test
     void 숫자_타입_체크() {
-        String input = "123a";
+        String input = "123?";
         String regex = "^[1-9][0-9]*$";
         assertThrows(IllegalArgumentException.class,()->{
             validator.gameRoundCheck(input);
@@ -40,6 +49,11 @@ class ValidatorTest {
     @Test
     void carNameCheck() {
         String input = "ccccc";
+        assertDoesNotThrow(() -> validator.carNameCheck(input));
+    }
+    @Test
+    void carNameCheck2() {
+        String input = "123as";
         assertDoesNotThrow(() -> validator.carNameCheck(input));
     }
 }
