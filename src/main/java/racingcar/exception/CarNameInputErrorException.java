c5ebf433d303182803b2f903e173c06b60b5a84f
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-01 17:07:57 +0900

    feat(exception) : Error Message 상수 따로 관리하도록 구현

--- src/main/java/racingcar/exception/CarNameInputErrorException.java
+++ src/main/java/racingcar/exception/CarNameInputErrorException.java
@@ -1,5 +1,6 @@
 package racingcar.exception;
 
+import racingcar.exception.constant.ErrorMessageConstant;
 import racingcar.util.TransferUtil;
 
 import java.util.HashSet;
@@ -9,26 +10,22 @@ import java.util.Set;
 public class CarNameInputErrorException implements InputErrorException{
     private final static int CAR_NAME_LENGTH_MIN_RANGE = 1;
     private final static int CAR_NAME_LENGTH_MAX_RANGE = 5;
-    private final static String CAR_NAME_LENGTH_SHORT_MESSAGE = "각 자동차 이름은 1글자 이상 입력해야합니다. 게임을 종료합니다.";
-    private final static String CAR_NAME_LENGTH_LONG_MESSAGE = "각 자동차 이름은 5글자 이하로 입력해야합니다. 게임을 종료합니다.";
-    private final static String CAR_NAME_LIST_EMPTY_MESSAGE = "자동차 이름은 필수로 입력해야합니다. 게임을 종료합니다.";
-    private final static String CAR_NAME_DUPLICATE_MESSAGE = "중복된 자동차 이름이 존재합니다. 게임을 종료합니다.";
 
     @Override
     public void checkUserInputValidate(String checkString) {
         List<String> checkStringList = new TransferUtil().transferStringToListString(checkString);
         if (isStringListEmpty(checkStringList)){
-            throw new IllegalArgumentException(CAR_NAME_LIST_EMPTY_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessageConstant.INPUT_EMPTY_ERROR_MESSAGE.getMessage());
         }
         checkStringList.forEach(checkStrings -> {
             if (isCarNameShorterThanMinLength(checkStrings)) {
-                throw new IllegalArgumentException(CAR_NAME_LENGTH_SHORT_MESSAGE);
+                throw new IllegalArgumentException(ErrorMessageConstant.CAR_NAME_LENGTH_SHORT_MESSAGE.getMessage());
             } else if (isCarNameLongerThanMaxLength(checkStrings)) {
-                throw new IllegalArgumentException(CAR_NAME_LENGTH_LONG_MESSAGE);
+                throw new IllegalArgumentException(ErrorMessageConstant.CAR_NAME_LENGTH_LONG_MESSAGE.getMessage());
             }
         });
         if (isCarNameDuplicate(checkStringList)){
-            throw new IllegalArgumentException(CAR_NAME_DUPLICATE_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessageConstant.CAR_NAME_DUPLICATE_MESSAGE.getMessage());
         }
     }
 
--- src/main/java/racingcar/exception/TryCountInputErrorException.java
+++ src/main/java/racingcar/exception/TryCountInputErrorException.java
@@ -1,18 +1,18 @@
 package racingcar.exception;
 
+import racingcar.exception.constant.ErrorMessageConstant;
+
 import java.util.List;
 
 public class TryCountInputErrorException implements InputErrorException{
-    private final static String TRY_INPUT_IS_EMPTY_MESSAGE = "값을 입력해주세요. 게임을 종료합니다.";
-    private final static String TRY_INPUT_IS_NOT_NUMBER_MESSAGE = "숫자만 입력이 가능합니다. 게임을 종료합니다.";
     private final static String NUMBER_REGULAR_EXPRESSION = "\\d+";
 
     @Override
     public void checkUserInputValidate(String checkString) {
         if (isEmptyString(checkString)){
-            throw new IllegalArgumentException(TRY_INPUT_IS_EMPTY_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessageConstant.INPUT_EMPTY_ERROR_MESSAGE.getMessage());
         }else if (isNumeric(checkString)){
-            throw new IllegalArgumentException(TRY_INPUT_IS_NOT_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessageConstant.TRY_INPUT_IS_NOT_NUMBER_MESSAGE.getMessage());
         }
     }
 
