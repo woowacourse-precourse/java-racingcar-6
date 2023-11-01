592f4f52c4c6e8f9128a3682e084bf9da56be73f
GyungA <GyungA@users.noreply.github.com>
2023-11-01 22:19:15 +0900

    feat: Car의 name, moveStatus에 대한 getter 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,6 +3,14 @@ package racingcar.model;
 public class Car {
     private String name;
     private Integer moveStatus;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getMoveStatus() {
+        return this.moveStatus;
+    }
     public Car(String name) {
         this.name = name;
         this.moveStatus = 0;
