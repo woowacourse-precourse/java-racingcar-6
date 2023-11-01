a5e8d98ebd6a80a94fe6f1d27d555f8c22e406c1
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 23:45:16 +0900

    [style] : code clean up

--- src/test/java/racingcar/utils/RandomUtilsTest.java
+++ src/test/java/racingcar/utils/RandomUtilsTest.java
@@ -11,18 +11,18 @@ public class RandomUtilsTest {
 
     @Test
     void createRandomNumberTest_True() {
-        int output = RandomUtils.createRandomNumber(Constants.MIN_RANDOM_NUMBER,Constants.MAX_RANDOM_NUMBER);
+        int output = RandomUtils.createRandomNumber(Constants.MIN_RANDOM_NUMBER, Constants.MAX_RANDOM_NUMBER);
         assertTrue(output >= 0 && output <= 9);
     }
 
     @ParameterizedTest
-    @CsvSource({"1", "2", "3","4"})
+    @CsvSource({"1", "2", "3", "4"})
     void isMoveTest_True(int input) {
         assertTrue(RandomUtils.isMove(input));
     }
 
     @ParameterizedTest
-    @CsvSource({"5","6","7","8","9"})
+    @CsvSource({"5", "6", "7", "8", "9"})
     void isMoveTest_False(int input) {
         assertFalse(RandomUtils.isMove(input));
     }
