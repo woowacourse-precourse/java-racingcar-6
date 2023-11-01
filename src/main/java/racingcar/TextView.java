33cb7df014928b83760e57fa2372c959b55fc066
kmx227 <rlaalstjq227@gmail.com>
2023-11-01 13:47:17 +0900

    Feat: TextView 문구
    입출력 관련한 문구 출력

---
+++ src/main/java/racingcar/TextView.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class TextView {
+    public static void PrintInputCarNameMessage() {
+        System.out.println(GameConstants.INPUT_CAR_NAME_MESSAGE);
+    }
+
+    public static void PrintInputRaceCountMessage() {
+        System.out.println(GameConstants.INPUT_RACE_COUNT_MESSAGE);
+    }
+
+    public static void PrintInRaceMessage() {
+        System.out.println(GameConstants.RACE_MESSAGE);
+    }
+
+    public static void PrintRaceResultMessage() {
+        System.out.print(GameConstants.WINNER_RESULT_MESSAGE);
+    }
+}
