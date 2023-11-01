c22fbc8b7298ae2f3aa8836e77df468ade121788
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 11:07:16 +0900

    [feat] Controller 작성 - 시도 횟수를 통한 각 자동차 이동 메소드

--- src/main/java/racingcar/Controller/RacingCarGame.java
+++ src/main/java/racingcar/Controller/RacingCarGame.java
@@ -1,11 +1,14 @@
 package racingcar.Controller;
 
 import java.util.List;
+import racingcar.Domain.Car;
 import racingcar.Domain.Cars;
 import racingcar.Domain.Race;
+import racingcar.Service.RacingCarService;
 import racingcar.Validator.CarNameValidator;
 import racingcar.Validator.TrialCountValidator;
 import racingcar.View.InputView;
+import racingcar.View.OutputView;
 
 public class RacingCarController {
 
@@ -20,6 +23,16 @@ public class RacingCarController {
             Cars cars = new Cars(carNames);
             Race race = new Race(cars.getCarList(), trialCount);
 
+            for (int i = 0; i < trialCount; i++) {
+                RacingCarService racingCarService = new RacingCarService();
+                racingCarService.playAndSelectWinners(race);
+
+                // 각 자동차의 이동 출력
+                OutputView.gameResultMessage();
+                for (Car car : race.getCars()) {
+                    OutputView.printMovementIndicator(car.getCarName(), car.getCarPosition());
+                }
+            }
 
         } catch (IllegalArgumentException e) {
             System.out.println("오류: " + e.getMessage());
