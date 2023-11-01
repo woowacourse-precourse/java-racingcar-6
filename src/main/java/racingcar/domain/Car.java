2339de12f9d7661a0f97286fb7f8260640bfa437
jjw0071 <jjw0071@naver.com>
2023-11-01 21:04:20 +0900

    feat(Car) : Car class 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+
+public class Car {
+
+    private String name;
+    private int score;
+    public Car(String carName) {
+        this.name = carName;
+        this.score = 0;
+    }
+
+    private Car() {
+    }
+
+
+}
