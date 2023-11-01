7ab83a1d5b4837d572ab69ecba64ec7b6fd48f1f
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 22:48:19 +0900

    feat(Entry): 엔트리에 차량 추가 및 엔트리 크기 반환 메소드 추가
    1. join
    -> 주어진 차량을 엔트리에 추가한다.
    -> 만약, 동일한 이름을 가진 차량이 이미 엔트리에 존재하는 경우 IllegalArgumentException을 발생시킨다.
    
    2. size
    -> 현재 엔트리에 포함된 차량의 개수를 반환한다.

--- src/main/java/racingcar/model/Entry.java
+++ src/main/java/racingcar/model/Entry.java
@@ -9,4 +9,24 @@ public class Entry {
 
     public Entry() {
     }
+
+    public int size() {
+        return cars.size();
+    }
+
+    /**
+     * 엔트리에 차량(car)을 추가합니다.
+     * @param car 추가할 차량
+     * @throws IllegalArgumentException 동일한 차량이 엔트리에 있는 경우.
+     *     한편, 두 차량의 이름이 같을 경우 동일한 차량으로 간주한다.
+     */
+    public void join(Car car) throws IllegalArgumentException{
+        cars.forEach(c -> {
+            if (c.isNamesake(car)) {
+                throw new IllegalArgumentException("해당 차량이 이미 엔트리에 존재합니다.");
+            }
+        });
+
+        cars.add(car);
+    }
 }
