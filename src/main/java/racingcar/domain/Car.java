a5aa79e4cbaac9cd86adf36cbaa56200901aa15e
prao <pch5521@gmail.com>
2023-11-01 19:36:42 +0900

    refactor: 자동차 이름과 위치 출력의 책임을 출력 뷰로 이관
    - 이전의 리팩토링에서 name, position을 알고 있는 Car 객체에게 printCarResult의 책임을 주는게 맞다고 판단하여 리팩토링을 진행하였으나, printCarResult는 입력값으로 문자열 이름, 정수형 위치만 받아서 출력하므로 출력뷰에 있는게 맞다고 판단하여 재수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,10 @@
 package racingcar.domain;
 
-import static racingcar.utils.Console.println;
-
 import racingcar.enums.GameCondition;
-import racingcar.enums.Symbols;
 
 public class Car {
     private final String name;
     private int position = GameCondition.START_POSITION.getValue();
-    private static final String RESULT_FORMAT = "%s : %s";
 
     public Car(final String name) {
         this.name = name;
@@ -20,11 +16,6 @@ public class Car {
 
     public boolean isMaxPosition(final int maxPosition) {
         return position == maxPosition;
-    }
-
-    public void printCarResult() {
-        String positionStr = new String(new char[position]).replace("\0", Symbols.GAME_SCORE.getSymbol());
-        println(String.format(RESULT_FORMAT, name, positionStr));
     }
 
     public String getName() {
