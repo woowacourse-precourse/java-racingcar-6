829c59c5aa08cfc0c348fdb2b8bf87184eb4b933
fromitive <fromitive@gmail.com>
2023-11-01 11:30:51 +0900

    refactor: 사용하지 않은 에러 문구 제거

--- src/main/java/racingcar/RacingCarGameText.java
+++ src/main/java/racingcar/RacingCarGameText.java
@@ -6,8 +6,6 @@ public interface RacingCarGameText {
     String RACINGCAR_EXECUTE_RESULT = "실행 결과";
     String RACINGCAR_FORMAT_STATUS = "%s : %s";
     String RACINGCAR_FORMAT_FINAL_WINNER = "최종 우승자 : %s";
-    String RACINGCAR_ERROR_INVALID_NUMBER_OF_NAMECHARACTER = "자동차 이름은 [%s]자리 이상 넘을 수 없습니다";
-    String RACINGCAR_ERROR_INVALID_TRY_COUNT_NUMBER = "회수는 0 이상의 정수를 입력해야 합니다";
     String RACINGCAR_DOES_NOT_MOVE = "\u200B"; // zero-width-space trim()에 지워지지 않는 값.
     String RACINGCAR_LOCATION_SYMBOL = "-";
     String RACINGCAR_WINNERS_DELIMITER = ", ";
