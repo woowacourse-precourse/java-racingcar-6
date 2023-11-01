d30684cda200d61754b21186a3b444049209a4b5
Yujin <tkfkd7038@naver.com>
2023-11-01 18:20:11 +0900

    refactor(racingcar): noticResult()에 있었던 실행결과 출력하는 기능을 다른 메서드로 분리

--- src/main/java/racingcar/RaceGameManager.java
+++ src/main/java/racingcar/RaceGameManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class RaceGameManager {
     Car[] car;
     NumberGenerator Generator = new NumberGenerator();
+
     public RaceGameManager(Car[] car) {
         this.car = car;
     }
@@ -14,14 +15,18 @@ public class RaceGameManager {
         for (int j = 0; j < carNameList.length; j++) {
             car[j].randomNumber = Generator.createRandomNumber();
             car[j].moveForward(car[j].randomNumber);
-            System.out.print(car[j].carName + " : ");
-            for (int i = 0; i < car[j].forwardCount; i++) {
-                System.out.print('-');
-            }
-            System.out.println();
+            displayCarStatus(car, j);
         }
         System.out.println();
 
+    }
+
+    public void displayCarStatus(Car[] car, int j){
+        System.out.print(car[j].carName + " : ");
+        for (int i = 0; i < car[j].forwardCount; i++) {
+            System.out.print('-');
+        }
+        System.out.println();
     }
 
     public void noticeWinner() { // 최종 우승자 출력
