49261b5ed350168342bab0d364e168b13214728f
coli-geonwoo <kkwoo001021@naver.com>
2023-11-01 09:27:52 +0900

    feat(RacingController) : 게임 프로그램 시퀀스 기능 구현

---
+++ src/main/java/domain/RacingController.java
@@ -0,0 +1,50 @@
+package domain;
+
+import domain.InputView.InputNames;
+import domain.InputView.InputTurn;
+import domain.OutputView.*;
+
+public class RacingController {
+
+    private CarList  carArray;
+    private int turnNum;
+
+    public RacingController(){
+        setCars();
+        setTurnNum();
+        play();
+        showWinner();
+    }
+    public void setCars(){
+        try{
+            String [] nameArray= InputNames.carInput();
+            carArray = new CarList(nameArray);
+        }catch(IllegalArgumentException e){
+            e.getMessage();
+        }
+
+    }
+
+    public void setTurnNum(){
+        try{
+            this.turnNum = InputTurn.turnInput();
+
+        }catch(IllegalArgumentException e){
+            e.getMessage();
+        }
+    }
+
+    public void play(){
+        System.out.println("실행결과");
+        for(int i=0; i<turnNum; i++)
+        {
+            OutputCars.printTurnResult(carArray.turnGoOrStay());
+        }
+
+    }
+
+    public void showWinner(){
+        OutputWinners.printWinners(carArray.getWinnerNames());
+    }
+
+}
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,7 @@
 package racingcar;
-
+import domain.RacingController;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new RacingController();
     }
 }
