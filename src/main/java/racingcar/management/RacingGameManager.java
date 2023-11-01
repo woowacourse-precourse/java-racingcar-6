5ea80ad6a147d6e0bfd48ddb3ba88948f1b7e98c
cheolhyeon <tghy4@naver.com>
2023-11-01 19:02:25 +0900

    refactor  : 한 라운드가 끝나면 결과를 출력하기 위해 랜덤넘버를 받고 자동차가 움직이는 기능 구현 완료

--- src/main/java/racingcar/management/RacingGameManager.java
+++ src/main/java/racingcar/management/RacingGameManager.java
@@ -4,16 +4,27 @@ import java.util.List;
 import racingcar.car.Car;
 import racingcar.carlist.RacingCarList;
 import racingcar.numbergeneration.MoveNumberGenerator;
+import racingcar.print.EachRoundResultPrinter;
+import racingcar.user.User;
 
 public class RacingGameManager {
-    private RacingCarList racingCarList;
+    private final RacingCarList racingCarList;
+    private final int MAX_ROUND;
 
-    public RacingGameManager(RacingCarList racingCarList) {
+    public RacingGameManager(User user, RacingCarList racingCarList) {
         this.racingCarList = racingCarList;
+        this.MAX_ROUND = user.getInputRacingRound();
     }
     public void runRace() {
         List<Car> racingCarsList = racingCarList.getRacingCarList();
 
+        for (int startRound = 0; startRound < MAX_ROUND; startRound++) {
+            runOneRound(racingCarsList);
+            EachRoundResultPrinter.printEachRoundResult(racingCarsList);
+        }
+    }
+
+    private void runOneRound(List<Car> racingCarsList) {
         racingCarsList.forEach(car -> {
             int numberToMove = MoveNumberGenerator.generateNumber();
             car.move(numberToMove);
