5ba1a86259723b74be77d8bcea28d53cf73d1433
jaepyo99 <jaepyo99@naver.com>
2023-11-01 15:39:33 +0900

    [Feat] 자동차 이름 길이 판별 메서드 생성

--- src/main/java/racingcar/common/util/Validator.java
+++ src/main/java/racingcar/common/util/Validator.java
@@ -3,6 +3,13 @@ package racingcar.common.util;
 import static racingcar.common.message.ErrorConstant.CAR_NAME_OVER_FIVE_ERROR_MESSAGE;
 
 public class Validator {
+    public boolean verifyCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(CAR_NAME_OVER_FIVE_ERROR_MESSAGE);
+        }
+        return true;
+    }
+
     public boolean isInt(String numberString) {
         for (int i = 0; i < numberString.length(); i++) {
             char numberChar = numberString.charAt(i);
