83d56165de8a05bba56de25965806db463691cd3
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 14:00:54 +0900

    2023.11.01_feat : RacingCars 생성자 생성

---
+++ src/main/java/racingcar/model/RacingCars.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+
+public class RacingCars {
+    private final ArrayList<Car> CARS = new ArrayList<>();
+
+    public RacingCars(ArrayList<String> cars) {
+        cars.forEach(car -> CARS.add(new Car(car)));
+    }
+}
