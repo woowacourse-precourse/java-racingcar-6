10b49051385506443cdcd360eaae1ecfb7cc5c2c
hellomatia <shuaigejp80@gmail.com>
2023-11-01 15:01:41 +0900

    refactor(domain): Car클래스 개선.
    - name필드는 한 번 생성후 변경되지 않으므로 final을 추가해주었음.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,7 +2,7 @@ package racingcar.domain;
 
 public class Car {
     private static final int START_POSITION = 0;
-    private String name;
+    private final String name;
     private int position;
 
     Car(String name) {
