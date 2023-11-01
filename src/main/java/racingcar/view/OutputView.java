5621f690e553b3633b0e7b162a347154e0067753
Yerinnnn <shinyerin0216@gmail.com>
2023-11-01 19:23:50 +0900

    Refactor: "OutputView로부터 가장 전진을 많이 한 우승자를 찾는 코드를 분리"

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,29 +1,24 @@
 package racingcar.view;
 
-import racingcar.model.Car;
-
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class OutputView {
+    final static String WINNER_IS = "최종 우승자 : ";
 
     public void printEachCarResult(String name, int score) {
         StringBuilder result = new StringBuilder(name + " : ");
-        Optional<String> formattedPosition = Stream.generate(() -> "-").limit(score).reduce((a, b) -> a + b);
+        Optional<String> formattedPosition = Stream.generate(() -> "-")
+                                                        .limit(score)
+                                                        .reduce((a, b) -> a + b);
         formattedPosition.ifPresent(result::append);
         System.out.println(result);
     }
 
-    public void result(List<Car> cars) {
+    public void result(String winner) {
         StringBuilder result = new StringBuilder();
-        result.append("최종 우승자 : ");
-
-        int max_score = cars.stream().max(Comparator.comparingInt(Car::getPosition)).get().getPosition();
-        result.append(cars.stream().filter(Car -> Car.getPosition() == max_score).map(Car::getName).collect(Collectors.joining(", ")));
-
+        result.append(WINNER_IS);
+        result.append(winner);
         System.out.println(result);
     }
 }
