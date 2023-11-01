b3a31bcf4144feecca21988516ff49f5760a8a69
HongChaa <jun761_1@naver.com>
2023-11-01 17:03:46 +0900

    refactor(GameController) : 경주 진행 메서드와 결과를 보여주는 메서드로 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -16,23 +16,30 @@ public class GameController {
     public void proceedGame() {
         List<Car> carList = cars.setPlayer(InputView.carsName());
         int tryCount = InputView.tryCount();
+
         race(carList, tryCount);
     }
 
     private void race(List<Car> carList, int tryCount) {
-        int countCheck = 0;
-
         OutputView.printResultMessage();
+        carList = proceedRace(carList, tryCount);
+        showResult(carList);
+    }
+
+    private List<Car> proceedRace(List<Car> carList, int tryCount) {
+        int countCheck = 0;
 
         while (countCheck < tryCount) {
             carList = cars.raceCars(carList);
             OutputView.executeResult(carList);
             countCheck++;
         }
+        return carList;
+    }
 
+    private void showResult(List<Car> carList) {
         List<String> winnerCarList = winner.determineWinner(carList);
         OutputView.printWinner(winnerCarList);
-
     }
 
 }
