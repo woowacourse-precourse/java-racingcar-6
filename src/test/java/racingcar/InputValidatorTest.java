7d82014040b0d357c3cf48cb26fe942ab6325a2e
ldw <leehan02@hanyang.ac.kr>
2023-11-01 15:53:51 +0900

    test(racingcar): 입력된 시도 횟수가 최소 시도 횟수 이하인 경우에 대한 단위 테스트 구현

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -33,11 +33,10 @@ public class InputValidatorTest {
         Assertions.assertDoesNotThrow(() -> InputValidator.tryNum(tryNum));
     }
 
-//    @Test
-//    void 시도_횟수가_최소_시도_횟수_이하인_경우_예외_발생() {
-//        int tryNum = 0;
-//
-//        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.tryNum(tryNum));
-//    }
+    @Test
+    void 시도_횟수가_최소_시도_횟수_이하인_경우_예외_발생() {
+        int tryNum = 0;
 
+        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.tryNum(tryNum));
+    }
 }
