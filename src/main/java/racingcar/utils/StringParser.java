7f52004187f7710cb01bccebc5cb537615979002
wooni97 <jhw970714@gmail.com>
2023-11-01 22:03:51 +0900

    refactor(StringParser): 앞 뒤 공백 제거 추가
    - 이 커밋은 String의 앞 뒤 공백을 제거하는것이 사용자에게 더 편리한 정책이라고 판단하여 메서드를 추가합니다.

--- src/main/java/racingcar/utils/StringParser.java
+++ src/main/java/racingcar/utils/StringParser.java
@@ -6,8 +6,16 @@ import java.util.List;
 public class StringParser {
     private StringParser() {} //인스턴스화 방지
 
-    public static List<String> splitCarNames(String carNames) {
-       String[] carNamesArr = carNames.split(",");
-       return Arrays.asList(carNamesArr);
+    public static List<String> splitString(String inputString) {
+       String[] arr = inputString.split(",");
+
+       for (int i = 0; i < arr.length; i++) {
+           arr[i] = arr[i].trim();
+       }
+
+       return Arrays.asList(arr);
     }
+
+
+
 }
