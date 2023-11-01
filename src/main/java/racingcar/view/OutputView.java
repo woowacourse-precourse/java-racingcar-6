f5c1804aafbb03e21462819eca0fc4c802fbd06b
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-01 16:26:28 +0900

    Feat[view] 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
+    public void printWinners(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
