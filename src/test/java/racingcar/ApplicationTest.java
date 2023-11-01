105d9a42ab9ff6770112cafb7e089d25100f4f3f
hongsunghak <qmffndnjzm@naver.com>
2023-11-01 14:09:53 +0900

    refactor : testcode 삭제 된거 수정

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,33 +1,26 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.validation.RaceValidation;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
-    private RaceValidation raceValidation = new RaceValidation();
-    @Test
-    @DisplayName("자동차 이름 입력에 쉼포가 없으면 예외 발생")
-    void validateSeparator() {
-        String includeComma = "자동차1,자동차2";
-        String unIncludeComma = "자동차1자동차2";
-        assertThatNoException().isThrownBy(() -> raceValidation.validationSeparator(includeComma));
-        assertThatThrownBy(() -> raceValidation.validationSeparator(unIncludeComma)).isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    @DisplayName("자동차 이름 5글자 넘길 시 예외 발생")
-    void validateNameLength() {
-        String carName1 = "마이바흐";
-        String carName2 = "롤스로이스 고스트";
-        assertThatNoException().isThrownBy(() -> raceValidation.validationNameLength(carName1));
-        assertThatThrownBy(() -> raceValidation.validationNameLength(carName2)).isInstanceOf(IllegalArgumentException.class);
-    }
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
+    @Test
+    void 전진_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
 
     @Test
 
