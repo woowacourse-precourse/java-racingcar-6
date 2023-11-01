c95d3778b8f651776980b241b0a34293af16f876
swhyeon98 <smreo75@gmail.com>
2023-11-01 15:47:49 +0900

    refactor: 메서드를 최대한 작게 만들기 위해 입력을 요청하고 반환하는 로직을 'getInput' 이름의 메서드로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -89,11 +89,15 @@ public class Application {
     }
 
     private static List<String> inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
+        String input = getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         List<String> carNames = Arrays.asList(input.split(","));
 
         return carNames;
+    }
+
+    private static String getInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
     }
 }
