d8b5ad17a2a38675a24816fda9da4a6f9b781943
SuhyungK <dook4966@gmail.com>
2023-11-01 19:41:00 +0900

    feat: 각 자동차에 대해 이동 횟수만큼 랜덤 숫자를 돌려서 전진 여부 판단하는 race 기능 구현, 사용자가 잘못된 자동차 이름 입력했을 때 에러 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,13 +8,18 @@ import java.util.*;
 class Cars {
     public static String[] carNames;
     public static int[] carDistance;
+    public static int carNumber;
 
     public static void readyCars(String[] inputCarNames) {
-        int carNum = inputCarNames.length;
-        carNames = new String[carNum];
-        carDistance = new int[carNum];
+        carNumber = inputCarNames.length;
+        carNames = new String[carNumber];
+        carDistance = new int[carNumber];
 
-        System.arraycopy(inputCarNames, 0, carNames, 0, carNum);
+        for (String carName: inputCarNames) {
+            if (carName.length() > 5) throw new IllegalArgumentException();
+        }
+
+        System.arraycopy(inputCarNames, 0, carNames, 0, carNumber);
     }
 }
 
@@ -24,30 +29,87 @@ class Round {
     public static int finalRound;
 
     public Round(int inputNum) {
-        this.finalRound = inputNum;
+        finalRound = inputNum;
     }
 
     public static boolean isContinue() {
-        nowRound ++;
+        nowRound++;
         return nowRound <= finalRound;
     }
 }
 
+class Race {
+    private static final int MIN_NUM = 0;
+    private static final int MAX_NUM = 9;
+    private static final int MOVING_FORWARD = 4;
+
+    public static List<String> winnerCarNames = new ArrayList<>();
+
+    private static int getRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+    }
+
+    private static int moveCar(int randomNum) {
+        if (randomNum >= MOVING_FORWARD) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void goRace() {
+        for (int idx = 0; idx < Cars.carNumber; idx++) {
+            Cars.carDistance[idx] += moveCar(getRandomNumber());
+        }
+    }
+
+    public static void showResult() {
+        for (int idx = 0; idx < Cars.carNumber; idx++) {
+            System.out.printf("%s : %s%n", Cars.carNames[idx], "-".repeat(Cars.carDistance[idx]));
+        }
+    }
+
+    public static String[] findWinner() {
+        int maxDistance = 0;
+        for (int idx = 0; idx < Cars.carNumber; idx++) {
+            if (Cars.carDistance[idx] > maxDistance) {
+                maxDistance = Cars.carDistance[idx];
+            }
+        }
+        System.out.println(maxDistance);
+        System.out.println(Arrays.toString(Cars.carDistance));
+        for (int idx = 0; idx < Cars.carNumber; idx++) {
+            if (Cars.carDistance[idx] == maxDistance) {
+                winnerCarNames.add(Cars.carNames[idx]);
+            }
+        }
+        return winnerCarNames.toArray(new String[0]);
+    }
+}
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] inputCarNames = Console.readLine().split(",");
-        Cars.readyCars(inputCarNames);
+        try {
+            Cars.readyCars(inputCarNames);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
         Round.finalRound = Integer.parseInt(Console.readLine());
 
+        System.out.println();
         System.out.println("실행 결과");
         while (Round.isContinue()) {
-
+            Race.goRace();
+            Race.showResult();
+            System.out.println();
         }
 
+        String[] winnerCarNames = Race.findWinner();
+
         System.out.print("최종 우승자 : ");
+        System.out.println(String.join(",", winnerCarNames));
     }
 }
