455b2e55e24c52a1894fa499850fd2f046deced9
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-01 14:02:35 +0900

    feat: 입력한 자동차 이름이 1개 이하인 경우 예외 발생
    입력한 자동차의 이름이 1개 이하일 경우 경주의 의미가 없으므로 2개 이상을 입력으로 받도록 검증

--- src/main/java/racingcar/CarRacingGame.java
+++ src/main/java/racingcar/CarRacingGame.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static racingcar.ErrorMessage.CAR_NAME_DUPLICATED;
 import static racingcar.ErrorMessage.NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER;
+import static racingcar.ErrorMessage.NUMBER_OF_CAR_NAME_IS_ONE_OR_LESS;
 import static racingcar.PrintMessage.INPUT_NUMBER_OF_ATTEMPTS;
 import static racingcar.PrintMessage.INPUT_RACING_CAR_NAME;
 import static racingcar.PrintMessage.PRINT_GAME_RESULT;
@@ -16,16 +17,19 @@ public class CarRacingGame {
 
     private static final String RACING_CAR_NAME_SEPARATOR = ",";
     private static final String WINNER_RACING_CAR_NAME_DELIMITER= ", ";
+    private static final Integer MINIMUM_NUMBER_OF_CAR_NAMES = 2;
 
     public void run(){
         System.out.println(INPUT_RACING_CAR_NAME);
         String[] carNames = Console.readLine().split(RACING_CAR_NAME_SEPARATOR);
         validateCarNameDuplicate(carNames);
+        validateNumberOfCarNames(carNames);
         RacingCars racingCars = RacingCars.from(carNames);
         System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
         Integer numberOfAttempts = convertInputToInteger(Console.readLine());
         play(racingCars, numberOfAttempts);
     }
+
 
     private void play(RacingCars racingCars, Integer numberOfAttempts) {
         System.out.println(PRINT_GAME_RESULT);
@@ -51,8 +55,14 @@ public class CarRacingGame {
     }
 
     private void validateCarNameDuplicate(String[] carNames) {
-        if(Arrays.stream(carNames).distinct().count()!= carNames.length){
+        if (Arrays.stream(carNames).distinct().count() != carNames.length) {
             throw new IllegalArgumentException(CAR_NAME_DUPLICATED);
+        }
+    }
+
+    private void validateNumberOfCarNames(String[] carNames) {
+        if (carNames.length < MINIMUM_NUMBER_OF_CAR_NAMES) {
+            throw new IllegalArgumentException(NUMBER_OF_CAR_NAME_IS_ONE_OR_LESS);
         }
     }
 
