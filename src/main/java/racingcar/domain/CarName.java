d8e9f3b06a6db9412376f6664e651b901b255740
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-01 20:58:17 +0900

    feat: 자동차 이름 길이 검증하는 메서드 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 public class CarName {
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
 
@@ -16,6 +17,9 @@ public class CarName {
     private void validate(final String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름이 빈 문자열입니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름이 5글자를 초과했습니다.");
         }
     }
 }
--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,6 +15,16 @@ class CarNameTest {
     @NullAndEmptySource
     @DisplayName("이름이 null인 경우 예외 발생")
     void carNameBlankExceptionTest(final String name) {
+        // expected
+        assertThrows(IllegalArgumentException.class, () -> CarName.from(name));
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자를 초과한 경우 예외 발생")
+    void carNameLengthOverValidationExceptionTest() {
+        // given
+        final String name = "car name test";
+
         // expected
         assertThrows(IllegalArgumentException.class, () -> CarName.from(name));
     }
