9e2c153c28138d5b7443c69ae5330eacb6887c7e
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 15:08:31 +0900

    2023.11.01_feat : GameController play 메서드 생성

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
 
 import racingcar.model.RacingCars;
+import racingcar.view.OutputView;
+
+import java.util.stream.IntStream;
 
 public class GameController {
     private RacingCars racingCars;
@@ -11,9 +14,12 @@ public class GameController {
         play();
         showWinner();
     }
-
     public void set() {
         racingCars = InputController.setRacingCars();
         roundNum = InputController.setRoundNum();
+    }
+    public void play() {
+        OutputView.printResultMessage();
+        IntStream.range(0, roundNum).forEach(i -> racingCars.playRound());
     }
 }
