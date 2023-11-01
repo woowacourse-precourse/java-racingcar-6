5ffa9b541accf118413a15217f68d47449160314
shortboy7 <thdwldnd7@naver.com>
2023-11-01 22:56:35 +0900

    feat : impl  end method

--- src/main/java/racingcar/game/impl/GameImpl.java
+++ src/main/java/racingcar/game/impl/GameImpl.java
@@ -1,6 +1,9 @@
 package racingcar.game.impl;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.car.Car;
 import racingcar.game.Game;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -29,10 +32,22 @@ public class GameImpl implements Game {
         for (int i = 0 ; i < moveNumber ; i++) {
             tryMove();
         }
+        this.cars = this.cars.stream()
+                .sorted(Comparator.comparingInt(Car::getMovedDistance))
+                .collect(Collectors.toList());
     }
 
     @Override
     public void end() {
-
+        int criteria = this.cars.get(0).getMovedDistance();
+        int length = this.cars.size();
+        System.out.print("최종우승자 : " + cars.get(0).getName());
+        for (int i = 1 ; i < length; i++) {
+            Car car = cars.get(i);
+            if (criteria == car.getMovedDistance()) {
+                System.out.print(", " + car.getName());
+            }
+        }
+        System.out.println();
     }
 }
