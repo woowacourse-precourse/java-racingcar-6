3edf405868336aa7e91188982fb825e89261071b
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 20:36:17 +0900

    feat : getCarNames 함수 생성

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -3,5 +3,12 @@ package racingcar.io;
 import java.util.Scanner;
 
 public class InputHandler {
-   
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getCarNames(String message){
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+
+
 }
