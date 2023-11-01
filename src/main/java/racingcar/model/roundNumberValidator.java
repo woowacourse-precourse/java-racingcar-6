08aaac5d37a19fc29fd4409458436c1c2154a044
DonghaOh <donghadarwin@naver.com>
2023-11-01 16:24:34 +0900

    feat: 횟수 입력 예외 양의 정수가 아닌 경우 - isRoundNumberNumeric 메소드

--- src/main/java/racingcar/model/roundNumberValidator.java
+++ src/main/java/racingcar/model/roundNumberValidator.java
@@ -3,8 +3,12 @@ package racingcar.model;
 public class roundNumberValidator {
     public static void roundNumberException(String roundNumberString){
         isRoundNumberNull(roundNumberString);
+        isRoundNumberNumeric(roundNumberString);
     }
     public static void isRoundNumberNull(String roundNumberString) {
         if (roundNumberString.isEmpty()) throw new IllegalArgumentException("시도할 횟수를 입력해주세요");
+    }
+    public static void isRoundNumberNumeric(String roundNumverString){
+        if(!roundNumverString.matches("[0-9]+")) throw new IllegalArgumentException("양의 정수를 입력해주세요");
     }
 }
