f0b8ec653cce83da312e3ae84ff5f51bd3eb7728
inwoo <korean5078@gmail.com>
2023-11-01 23:56:02 +0900

    test(ApplicationTest): 기능 명세서 기능 별 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Player;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCarGame;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -35,7 +40,51 @@ class ApplicationTest extends NsTest {
                 MOVING_FORWARD, MOVING_FORWARD, STOP
         );
     }
-    
+
+    @Test
+    void 자동차_이름_확인() {
+        String name = "pobi";
+        RacingCar car = new RacingCar(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void 자동차_전진() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    RacingCar racingCar = new RacingCar("test");
+                    racingCar.goOrStop();
+                    assertThat(racingCar.distance).isEqualTo(1);
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 자동차_정지() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    RacingCar racingCar = new RacingCar("test");
+                    racingCar.goOrStop();
+                    assertThat(racingCar.distance).isEqualTo(0);
+                },
+                STOP
+        );
+    }
+
+    @Test
+    void 자동차_이름_거리_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    RacingCar racingCar = new RacingCar("pobi");
+                    racingCar.goOrStop();
+                    racingCar.printDistance();
+                    assertThat(output()).contains("pobi : -");
+                },
+                MOVING_FORWARD
+        );
+    }
+
     @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
@@ -50,6 +99,26 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,java,", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 자동차_객체_생성() {
+        RacingCarGame racingCarGame = new RacingCarGame(new Player());
+        List<String> testNames = new ArrayList<>();
+        testNames.add("pobi");
+        testNames.add("june");
+        List<RacingCar> cars = racingCarGame.createRacingCars(testNames);
+        assertThat(cars.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 자동차_게임_라운드_시작_및_결과_출력() {
+        RacingCarGame racingCarGame = new RacingCarGame(new Player());
+        List<RacingCar> cars = new ArrayList<>();
+        cars.add(new RacingCar("abc"));
+        cars.add(new RacingCar("def"));
+        racingCarGame.startGameRounds(cars, 3);
+        assertThat(output()).contains("abc : ", "def : ");
     }
 
     @Override
