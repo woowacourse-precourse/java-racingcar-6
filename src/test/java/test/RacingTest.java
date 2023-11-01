861cd52fada8f696c1ef344fe4232d79e009dc51
jeonseong-gwon <jeonsg9904@gmail.com>
2023-11-01 16:12:17 +0900

    Test : 포지션이 가장 높은 자동차의 우승여부 test

--- src/test/java/test/RacingTest.java
+++ src/test/java/test/RacingTest.java
@@ -2,10 +2,13 @@ package test;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.validation.InputValidation;
 import racingcar.view.InputView;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 public class RacingTest {
 
@@ -36,6 +39,23 @@ public class RacingTest {
         Assertions.assertThatThrownBy(() -> inputView.readTryGame())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("올바른 숫자를 입력하세요.");
+    }
+
+    @Test
+    public void 포지션이_가장_앞선_자동차가_우승() {
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+        Car car3 = new Car("Car3");
+
+        car1.goToPosition(); // Car1 1칸 전진
+        car2.goToPosition(); // Car2 1칸 전진
+
+        List<Car> carList = List.of(car1, car2, car3);
+        Cars cars = new Cars(carList);
+
+        List<String> winners = cars.findWinners();
+
+        Assertions.assertThat(winners).containsExactly("Car1", "Car2");
     }
 
     private String[] generateRacersFromNames(String input) {
