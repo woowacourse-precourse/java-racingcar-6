2b6c27246df7425217c798c50abe630117a848d9
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-01 17:10:48 +0900

    test(UserViewTest): 입력값 예외처리 테스트 구현

---
+++ src/test/java/racingcar/view/UserViewTest.java
@@ -0,0 +1,40 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserViewTest {
+    private UserView userView;
+
+    @BeforeEach
+    public void setUp() {
+        userView = new UserView();
+    }
+
+    @Test
+    public void testValidateNameLength() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userView.validateNameLength("InvalidNameLength");
+        });
+    }
+
+    @Test
+    public void testValidateNullValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userView.validateNullValue("");
+        });
+    }
+
+    @Test
+    public void testValidCoinValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userView.validCoinValue(0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            userView.validCoinValue(-1);
+        });
+    }
+}
