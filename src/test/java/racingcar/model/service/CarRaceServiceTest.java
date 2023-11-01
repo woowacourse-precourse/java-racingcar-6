73441bb9b75a4d8fff335786bae5892584bbbc73
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-01 15:40:04 +0900

    CarRaceServiceTest 테스트 작성

--- src/test/java/racingcar/model/service/CarRaceServiceTest.java
+++ src/test/java/racingcar/model/service/CarRaceServiceTest.java
@@ -9,25 +9,31 @@ class CarRaceServiceTest {
 
     private static final CarRaceService cs = new CarRaceService();
 
+    static {
+        cs.addCars("pobi");
+    }
     @Test
     void addCars() {
-        cs.addCars("pobi,woni");
-        assertThat(cs.getCarsName().stream().map(() -> to
-    }
-
-    @Test
-    void moveCars() {
+        cs.addCars("woni");
+        assertThat(cs.getCarsName()
+                .get(1))
+                .isEqualTo("woni");
     }
 
     @Test
     void getCarsName() {
+        assertThat(cs.getCarsName()
+                .get(0))
+                .isEqualTo("pobi");
     }
 
     @Test
     void getCarsLocation() {
+        assertThat(cs.getCarsLocation().get(0)).isZero();
     }
 
     @Test
     void getWinnersName() {
+        assertThat(cs.getWinnersName().get(0)).isEqualTo("pobi");
     }
 }
