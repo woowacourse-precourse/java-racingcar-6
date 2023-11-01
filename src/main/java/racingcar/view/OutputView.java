d1c67017034b103f9905313f7ac82bbefb2e8b6f
kg9585 <kg9585@naver.com>
2023-11-01 21:20:26 +0900

    refactor(OutputView): 의존성 주입 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,21 +4,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.controller.CarController;
 import racingcar.model.Car;
+import racingcar.model.CarDto;
 
 public class OutputView {
     public void printStartMessage() {
         System.out.println("실행 결과");
     }
 
-    public void printCarStatus(Car car) {
-        System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+    public void printCarStatus(CarDto dto) {
+        System.out.println(dto.getName() + " : " + "-".repeat(dto.getPosition()));
     }
 
     public void printWinners(List<String> winners) {
         String result = String.join(", ", winners);
         System.out.println("최종 우승자 : " + result);
-    }
-    public void printLineBreak() {
-        System.out.println();
     }
 }
