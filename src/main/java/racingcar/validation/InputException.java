3c2452ed935867bacaf554826abb8e83a8dc8d15
gomdol <oioifor@naver.com>
2023-11-01 19:58:44 +0900

    feat(InputException) : 숫자 입력 예외 처리

--- src/main/java/racingcar/validation/InputException.java
+++ src/main/java/racingcar/validation/InputException.java
@@ -8,6 +8,12 @@ public class InputException {
         black(input);
         return input;
     }
+    public static String numberValidation(String input){
+        numberCheck(input);
+        underZeroNumber(input);
+        black(input);
+        return input;
+    }
     public static void carNameLengthCheck(String input){
         for (String name : input.split(",")) {
             if (name.length() > 5) {
@@ -25,6 +31,20 @@ public class InputException {
     public static void black(String input){
         if(input.isEmpty()){
             throw new IllegalArgumentException("값을 입력해 주세요");
+        }
+    }
+
+    public static void numberCheck(String input){
+        try {
+            Integer.parseInt(input);
+        }catch (NumberFormatException numberFormatException){
+            throw new IllegalArgumentException("숫자값을 입력해주세요");
+        }
+    }
+
+    public static void underZeroNumber(String input){
+        if(Integer.parseInt(input)<= 0){
+            throw new IllegalArgumentException("0보다 큰 값을 입력해주세요");
         }
     }
 }
