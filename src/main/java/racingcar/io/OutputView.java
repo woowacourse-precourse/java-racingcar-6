4aa0d690e3c43bd295a69bf32c91b5715d6d0ec1
AidenRoh <colohrby@gmail.com>
2023-11-01 10:27:50 +0900

    chore: change package name

--- src/main/java/racingcar/io/OutputView.java
+++
@@ -1,7 +0,0 @@
-package racingcar.io;
-
-public class OutputView {
-    public static void printMessage(IOMessage ioMessage) {
-        System.out.println(ioMessage.getMessage());
-    }
-}
--- src/test/java/racingcar/view/IOTest.java
+++ src/test/java/racingcar/view/IOTest.java
@@ -1,4 +1,4 @@
-package racingcar.io;
+package racingcar.view;
 
 import java.io.ByteArrayInputStream;
 
