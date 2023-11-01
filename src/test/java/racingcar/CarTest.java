97301b6598c130b43758e575f6212fb857230421
MinseokGo <rhalstjr1999@naver.com>
2023-11-01 16:27:29 +0900

    test($CarTest): 자동차 객체 값 테스트
    자동차 이름은 5글자 이하여야 하므로 5글자를 초과하는 테스트를 진행합니다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
@@ -15,12 +14,21 @@ class CarTest {
         final String name = "고민석";
         final int position = 0;
 
-
         //when
         final Car car = new Car("고민석");
 
         //then
         assertThat(name).isEqualTo(car.getName());
         assertThat(position).isEqualTo(car.getPosition());
+    }
+
+    @Test
+    @DisplayName("실패 테스트 - 자동차 이름이 5글자를 초과한다.")
+    void test2() {
+        //given
+        final String name = "다섯글자초과";
+
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(name));
     }
 }
