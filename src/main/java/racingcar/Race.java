997895ac4832e0f7b67f6b2ac33404416a3030a7
민경 <tjalsrud0101@gmail.com>
2023-11-01 15:21:48 +0900

    feat: 경주 횟수 입력받기

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Race {
@@ -7,6 +9,25 @@ public class Race {
 
     public void play() {
         cars = getCars();
+        int attempt = getAttemptNumber();
+
+    }
+
+
+    private int getAttemptNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        isValidateInput(input);
+        return Integer.parseInt(input);
+    }
+
+    private void isValidateInput(String input) {
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(c < '0' || c > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
 
