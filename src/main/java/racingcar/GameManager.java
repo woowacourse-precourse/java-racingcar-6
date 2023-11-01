377cc24f01fa8e754f85da2f6dec0df86730344c
CJ <choijun0627@konkuk.ac.kr>
2023-11-01 21:18:01 +0900

    Refactor : 자동차 경주 게임 진행 관련 기능 수정
    기능 목록의 자동차 경주 게임 진행 관련 기능 목록 만든대로
    기존의 start 함수에 있던 것들을 메소드로 빼서 만듬
    자동차 이름과 이동 횟수 입력하는 기능인 inputInformation 메소드 생성
    입력 받은 자동차 이름 가지고 자동차 만드는 기능인 makeCar 메소드 생성
    입력 받은 이동 횟수 저장하는 기능인 setMovingCount 메소드 생성
    자동차 전진 횟수 저장하는 기능인 getCarMovingCount 메소드 생성
    게임 종료 되었는지 확인하는 기능인 boolean checkGameOver 메소드 생성
    그리고 필드로 String carName과 int movingCount 가지도록 했고
    생성자에서 초기화 하도록 수정함

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -7,45 +7,68 @@ public class GameManager {
     private Player player;
     private RacingCarGame racingCarGame;
     private OutputView outputView;
+    private String carName;
+    private int movingCount;
 
     public GameManager() {
         player = new Player();
         racingCarGame = new RacingCarGame();
         outputView = new OutputView();
+        carName = "";
+        movingCount = 0;
     }
 
     public void start() {
-        outputView.printNameMessage();
-        String carName = player.inputCarName();
-
-        outputView.printCountMessage();
-        int movingCount = player.inputMovingCount();
-
-        racingCarGame.splitCarName(carName);
-        racingCarGame.saveMovingCount(movingCount);
-
-        racingCarGame.createCar();
+        inputInformation();
+        makeCar();
+        setMovingCount();
 
         outputView.printResultMessage();
-
         while (true) {
-            List<Car> carList = racingCarGame.getCarList();
-
-            for (int i = 0; i < carList.size(); i++) {
-                Car car = carList.get(i);
-                car.raiseMovingCount();
-            }
-
-            racingCarGame.saveCarMovingCount();
-
+            getCarMovingCount();
             outputView.printRacingCarGameResult(racingCarGame.getCarNameList(), racingCarGame.getCarMovingCountList());
 
-            if (racingCarGame.isGameOver()) {
-                outputView.printWinnerMessage();
-                System.out.println(racingCarGame.getWinnerName());
+            if (checkGameOver()) {
                 break;
             }
         }
-
     }
+
+    public void inputInformation() {
+        outputView.printNameMessage();
+        carName = player.inputCarName();
+
+        outputView.printCountMessage();
+        movingCount = player.inputMovingCount();
+    }
+
+    public void makeCar() {
+        racingCarGame.splitCarName(carName);
+        racingCarGame.createCar();
+    }
+
+    public void setMovingCount() {
+        racingCarGame.saveMovingCount(movingCount);
+    }
+
+    public void getCarMovingCount() {
+        List<Car> carList = racingCarGame.getCarList();
+
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            car.raiseMovingCount();
+        }
+
+        racingCarGame.saveCarMovingCount();
+    }
+
+    public boolean checkGameOver() {
+        if (racingCarGame.isGameOver()) {
+            outputView.printWinnerMessage();
+            System.out.println(racingCarGame.getWinnerName());
+            return true;
+        }
+        return false;
+    }
+
 }
