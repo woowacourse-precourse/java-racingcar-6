918bc973b4282826b5d1651e3a60127dde2d8da6
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-01 12:22:08 +0900

    style: ExceptionMessage 변수명 수정
    - 보다 직관적인 변수명을 위해 value -> message 로 수정

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -10,13 +10,13 @@ public enum ExceptionMessage {
     ROUND_CONTAIN_NOT_ONLY_NUM("자동차 이동은 숫자로 입력해야 합니다."),
     ROUND_MUST_BE_POSITIVE("자동차 이동은 양수로 입력해야 합니다.");
 
-    private final String value;
+    private final String message;
 
-    ExceptionMessage(String vlaue) {
-        this.value = vlaue;
+    ExceptionMessage(String message) {
+        this.message = message;
     }
 
-    public String getValue() {
-        return this.value;
+    public String getMessage() {
+        return this.message;
     }
 }
