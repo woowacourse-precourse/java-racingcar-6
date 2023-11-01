a3aa89e9c51f0a1df42e3781e96aa26dc16c4c28
songyi <song21677@naver.com>
2023-11-01 20:46:19 +0900

    refactor: 시도 횟수 입력 객체 분리를 통해 TryCount 클래스의 생성자 인수로 들어간다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,8 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		Cars cars = new Cars(Console.readLine());
+
+		System.out.println("시도할 회수는 몇회인가요?");
+		TryCount tryCount = new TryCount(Console.readLine());
 	}
 }
