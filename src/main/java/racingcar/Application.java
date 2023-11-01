62559468cfda4567eeac9c4e7a851dc92dfe6be7
tlarbals824 <simkm0824@naver.com>
2023-11-01 20:06:36 +0900

    refactor : RacingCar 의존성 정의 및 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,24 @@
 package racingcar;
 
+import racingcar.input.*;
+import racingcar.input.decorator.CarCollectionPrintDecoratorUserInput;
+import racingcar.input.decorator.RepeatCountDecoratorUserInput;
+import racingcar.race.Handler;
+import racingcar.race.RacingCarHandler;
+import racingcar.race.RacingInfoHandler;
+import racingcar.race.RacingResultHandler;
+import racingcar.race.decorator.RacingPrintDecoratorHandler;
+import racingcar.vo.CarCollection;
+import racingcar.vo.RacingInfo;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        final RacingCar racingCar = new RacingCar();
+        final Handler<RacingInfo,RacingInfo> racingInfoHandler = new RacingPrintDecoratorHandler(new RacingInfoHandler(new RacingCarHandler(), new RacingResultHandler()));
+        final ConsoleUserInput consoleUserInput = new ConsoleUserInput();
+        final UserInput<CarCollection> carCollectionUserInput = new CarCollectionPrintDecoratorUserInput(new CarCollectionUserInput(consoleUserInput));
+        final UserInput<Integer> repeatCountUserInput = new RepeatCountDecoratorUserInput(new RepeatCountUserInput(consoleUserInput));
+        final RacingCar racingCar = new RacingCar(new RacingInfoUserInput(carCollectionUserInput, repeatCountUserInput), racingInfoHandler);
         racingCar.run();
     }
 }
