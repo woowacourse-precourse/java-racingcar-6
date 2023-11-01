01a1fdd4fadaf472f3a95172fc9a614611ddb987
kunsanglee <gosmdochee2@gmail.com>
2023-11-01 16:57:12 +0900

    refactor(CarDto): 선언한 상수의 접근 제어자를 private로 변경

--- src/main/java/racingcar/dto/CarDto.java
+++ src/main/java/racingcar/dto/CarDto.java
@@ -3,7 +3,7 @@ package racingcar.dto;
 import racingcar.domain.Car;
 
 public record CarDto(String name, int position) {
-    public static final String POSITION_PROGRESS = "-";
+    private static final String POSITION_PROGRESS = "-";
 
     public static CarDto from(Car car) {
         return new CarDto(car.getCarName(), car.getPosition());
