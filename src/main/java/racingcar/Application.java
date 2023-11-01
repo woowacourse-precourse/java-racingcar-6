ef620bc8de63514941447b91fd730a1b8d457dec
leedoyeon849 <do83430208@gmail.com>
2023-11-01 17:21:31 +0900

    docs: 기능 요구 사항 / 예외 처리 요구사항 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        ArrayList<String> cars = Arrays
+                .stream(Console.readLine()
+                .split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+//        for (String car : cars) {
+//            System.out.println(car);
+//        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryCount = Console.readLine();
+
+
+
+
+
     }
+
+
 }
