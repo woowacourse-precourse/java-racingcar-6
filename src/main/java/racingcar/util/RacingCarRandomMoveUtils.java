dcc08d66665cee347c23927e821fa0d153025b90
COBI-98 <tkdgus968@naver.com>
2023-11-01 09:32:15 +0900

    feat: 0에서 9 사이에서 무작위 값을 구하는 기능 구현

---
+++ src/main/java/racingcar/util/RacingCarRandomMoveUtils.java
@@ -0,0 +1,14 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCarRandomMoveUtils implements RandomUtils{
+
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    @Override
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER,MAX_RANDOM_NUMBER);
+    }
+}
