3551d14e0d470ef3640c210f8ed73959b6ec748e
jyoooooung <jyooooounh@naver.com>
2023-11-01 17:07:37 +0900

    Car  클래스 : 전진 기능 구현

--- src/main/java/racingcar/User/Car/Car.java
+++ src/main/java/racingcar/User/Car/Car.java
@@ -1,4 +1,29 @@
 package racingcar.User.Car;
 
 public class Car {
+
+    private String name;
+
+    private int moveDistance;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveDistance = 0;
+    }
+
+    public void move() {
+        moveDistance += 1;
+    }
+
+    public boolean judgeRandNum(int randNum) {
+        if (randNum >= 4) {
+            return true;
+        } return false;
+    }
+
+    public void moveByRandNum(int randNum) {
+        if (judgeRandNum(randNum)) {
+            move();
+        }
+    }
 }
