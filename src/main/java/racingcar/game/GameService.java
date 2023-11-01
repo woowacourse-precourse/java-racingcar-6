849607435ab60ca61f95c44ea981a97c3164c94c
tangba484 <tangba484@naver.com>
2023-11-01 18:14:25 +0900

    Feat: 라운드에서 승리한 자동차 점수 올리는 기능 구현

--- src/main/java/racingcar/game/GameService.java
+++ src/main/java/racingcar/game/GameService.java
@@ -1,9 +1,22 @@
 package racingcar.game;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Cars;
+
+import java.util.Map;
 
 public class GameService {
     public static int Num_For_Win = 4;
+
+    public void playRound(Cars cars) {
+        Map<String, Integer> state = cars.getState();
+        state.replaceAll((name, score) -> {
+            if (isWin()) {
+                score += 1;
+            }
+            return score;
+        });
+    }
 
     public boolean isWin() {
         int number = Randoms.pickNumberInRange(0, 9);
