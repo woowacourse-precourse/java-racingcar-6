1009c02317ab784287ac1f13c62f3b0d7045a504
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-01 14:12:23 +0900

    Feat : 입력한 자동차 이름이 5자를 초과할 경우 예외를 발생시킨다

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -4,10 +4,21 @@ public class CarName {
     private final String name;
 
     public CarName(String name) {
+        validateLength(name);
         this.name = name;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    private void validateLength(String name) {
+        if (isOverLength(name)) {
+            throw new IllegalArgumentException("자동차 이름은 5자 까지만 입력할 수 있습니다.");
+        }
+    }
+
+    private boolean isOverLength(String name) {
+        return 5 < name.length();
     }
 }
