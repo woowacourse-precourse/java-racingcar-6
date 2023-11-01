2a207aba3ae975075006d07f2144e3d2e21b1f02
suin <parksuuuun@gmail.com>
2023-11-01 13:12:31 +0900

    feat: PlayGame 클래스 통한 자동차 경주 게임의 순차적 실행 - 사용자가 자동차 이름 및 게임 실행 횟수를 실행할 수 있도록 입력창 생성됨. - 각 라운드별 실행 결과가 화면에 출력됨. - 최종 우승자 및 전진 결과가 화면에 출력됨.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        PlayGame playGame = new PlayGame();
+        playGame.startGame();
+
     }
 }
