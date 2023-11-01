aad3920ddcfca8906eeaf36485e8c2ffe08cf5b6
leesh <qwqw8019@naver.com>
2023-11-01 17:02:19 +0900

    refactor: RacingGame 로직 변경
    - RacingGameManager로 책임 위임

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,44 +1,21 @@
 package racingcar.controller;
 
-import static racingcar.view.ConsoleInput.inputCarNames;
-import static racingcar.view.ConsoleInput.inputPlayNumber;
-import static racingcar.view.ConsoleOutput.informInputCarNames;
-import static racingcar.view.ConsoleOutput.informInputPlayNumber;
-import static racingcar.view.ConsoleOutput.informPlayResult;
-import static racingcar.view.ConsoleOutput.printFinalWinner;
-import static racingcar.view.ConsoleOutput.printNewLine;
-import static racingcar.view.ConsoleOutput.printProgressState;
-
-import java.util.List;
-import racingcar.domain.CarManager;
-import racingcar.domain.dto.FinalResult;
-import racingcar.domain.dto.ProgressState;
+import static racingcar.domain.RacingGameManager.proceedAwardStage;
+import static racingcar.domain.RacingGameManager.proceedCarNameInputStage;
+import static racingcar.domain.RacingGameManager.proceedPlayNumberInputStage;
+import static racingcar.domain.RacingGameManager.proceedPlayStage;
 
 
 public class RacingGame {
 
     public static void run() {
-        informInputCarNames();
-        List<String> carNames = inputCarNames();
-        CarManager.makeCarList(carNames);
 
-        informInputPlayNumber();
-        int playNumber = inputPlayNumber();
-        printNewLine();
+        proceedCarNameInputStage();
 
-        informPlayResult();
-        play(playNumber);
+        proceedPlayNumberInputStage();
 
-        FinalResult finalResult = CarManager.getFinalWinner();
-        printFinalWinner(finalResult);
-    }
+        proceedPlayStage();
 
-    private static void play(int playNumber) {
-        for (int i : new int[playNumber]) {
-            CarManager.proceed();
-            ProgressState progressState = CarManager.getState();
-            printProgressState(progressState);
-            printNewLine();
-        }
+        proceedAwardStage();
     }
 }
---
+++ src/main/java/racingcar/domain/RacingGameManager.java
@@ -0,0 +1,52 @@
+package racingcar.domain;
+
+import static racingcar.domain.CarManager.getFinalWinner;
+import static racingcar.domain.CarManager.makeCarList;
+import static racingcar.view.ConsoleInput.inputCarNames;
+import static racingcar.view.ConsoleInput.inputPlayNumber;
+import static racingcar.view.ConsoleOutput.informInputCarNames;
+import static racingcar.view.ConsoleOutput.informInputPlayNumber;
+import static racingcar.view.ConsoleOutput.informPlayResult;
+import static racingcar.view.ConsoleOutput.printFinalWinner;
+import static racingcar.view.ConsoleOutput.printNewLine;
+import static racingcar.view.ConsoleOutput.printProgressState;
+
+import java.util.List;
+import racingcar.domain.dto.FinalResult;
+import racingcar.domain.dto.ProgressState;
+
+public class RacingGameManager {
+    private static int playNumber = 0;
+
+    public static void proceedCarNameInputStage() {
+        informInputCarNames();
+        List<String> carNames = inputCarNames();
+        makeCarList(carNames);
+    }
+
+    public static void proceedPlayNumberInputStage() {
+        informInputPlayNumber();
+        playNumber = inputPlayNumber();
+        printNewLine();
+    }
+
+    public static void proceedPlayStage() {
+        informPlayResult();
+        play();
+    }
+
+    public static void proceedAwardStage() {
+        FinalResult finalResult = getFinalWinner();
+        printFinalWinner(finalResult);
+    }
+
+    private static void play() {
+        for (int i : new int[playNumber]) {
+            CarManager.proceed();
+            ProgressState progressState = CarManager.getState();
+            printProgressState(progressState);
+            printNewLine();
+        }
+    }
+
+}
