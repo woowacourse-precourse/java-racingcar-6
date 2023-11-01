53eba249447129dad14b4384cd1fee82c984caf8
haebyun <bhk4392@gmail.com>
2023-11-01 17:27:59 +0900

    test(RacingFormatter): 자동차 이름들 중 5글자가 넘는 입력에 대한 예외 발생 테스트

--- src/test/java/racingcar/games/racing/util/RacingFormatterTest.java
+++ src/test/java/racingcar/games/racing/util/RacingFormatterTest.java
@@ -2,6 +2,7 @@ package racingcar.games.racing.util;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -21,5 +22,14 @@ class RacingFormatterTest {
         //then
         assertThat(carNames.size()).isEqualTo(3);
         assertThat(carNames).containsExactly("poby", "고죠", "해광");
+    }
+
+    @Test
+    @DisplayName("자동차 이름들 중 5글자가 넘는 입력에 대한 예외 발생 검증")
+    public void reformatOverFiveCarNames() {
+        RacingFormatter formatter = new RacingFormatter();
+
+        assertThatThrownBy(() -> formatter.reformatCarNames("해광isTheBest,poby"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
