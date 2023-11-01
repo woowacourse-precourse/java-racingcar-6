f72124a96f1d5abefd61d25e4fa50e81889f8f34
mung96 <mung5079@gmail.com>
2023-11-01 16:19:53 +0900

    feat:Application.java에 기능구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Game game = new Game();
+        game.createName();
+
+        game.letsGo(game.tryNumber());
+
+        System.out.print("최종 우승자 : "+game.printWinners());
     }
 }
