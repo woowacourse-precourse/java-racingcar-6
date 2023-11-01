c795ae03a786db85ced4403e5dfbbcf312e0ae53
hellomatia <shuaigejp80@gmail.com>
2023-11-01 15:47:59 +0900

    test(domain): CarTest클래스 변경.
    - 기존 Car클래스 이름을 CarRacing으로 변경하여 해당 테스트를 삭제.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,39 +13,5 @@ class CarTest {
 
     private Car car;
 
-    @Test
-    void 경주_자동차_이름_저장() {
-        var carNames = List.of("Car1", "Car2", "Car3");
-        car = new Car(carNames);
-
-        RaceResultDTO raceResultDTO = car.oneCarRace();
-
-        assertEquals(raceResultDTO.names(), carNames);
-    }
-
-    @Test
-    void 경주_자동차_우승자_반환() {
-        var carNames = List.of("Car1", "Car2", "Car3");
-        car = new Car(carNames);
-
-        car.oneCarRace();
-        car.oneCarRace();
-        car.oneCarRace();
-
-        AtomicInteger farthestPosition = new AtomicInteger();
-        RaceResultDTO raceResultDTO = car.oneCarRace();
-        raceResultDTO.names()
-                .stream()
-                .filter(name -> raceResultDTO.positions().get(name) > farthestPosition.get())
-                .forEach(name -> farthestPosition.set(raceResultDTO.positions().get(name)));
-
-        var winners = raceResultDTO.names()
-                        .stream()
-                        .filter(name -> raceResultDTO.positions().get(name) == farthestPosition.get())
-                        .collect(Collectors.toList());
-
-
-        assertEquals(winners, car.getWinners());
-    }
 }
 
