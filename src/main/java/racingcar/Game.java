d621972b3fb86391b0a7b4c94489fa35fb7a2130
DahyeY <dhuniv@naver.com>
2023-11-01 13:08:31 +0900

    feat: 자동차 이름 입력에 대한  예외 처리 추가

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -11,6 +11,7 @@ public class Game {
     public int outputCount = 0;
     public int MOVES;
     public static int MAX_LENGTH_OF_NAME = 5;
+    public static int MIN_LENGTH_OF_NAME = 1;
     public static final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
     public static final String MOVES_MESSAGE = "시도할 회수는 몇회인가요?\n";
     public static final String OUTPUT_MESSAGE = "\n실행 결과\n";
@@ -22,9 +23,18 @@ public class Game {
     public void getCarNames() {
         String input = Console.readLine();
         nameList = Arrays.asList(input.split(","));
+        if (input.charAt(input.length() - 1) == ',') {
+            throw new IllegalArgumentException("이름 입력은 쉼표로 끝낼 수 없습니다.");
+        }
+        if (nameList.size() == 1) {
+            throw new IllegalArgumentException("자동차 이름은 1개 이상 입력해야 합니다.");
+        }
         for (String name : nameList) {
             if (name.length() > MAX_LENGTH_OF_NAME) {
                 throw new IllegalArgumentException("자동차 이름은 " + MAX_LENGTH_OF_NAME + "자 이하여야 합니다.");
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 " + MIN_LENGTH_OF_NAME + "자 이상이어야 합니다.");
             }
         }
     }
