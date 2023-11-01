41c67be4fd8539b041b28ebd6dcfe4f16457e285
“sangjun121” <juncho12011201@gmail.com>
2023-11-01 16:32:58 +0900

    feat: 우승자 판독하는 클래스 생성 및 우승자 리스트 반환 구현
    Judgement.judgeWinner()를 호출하여 우승자 리스트를 WinnerDto로 전달

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -2,23 +2,28 @@ package racingcar.controller;
 
 import racingcar.model.CarDto;
 import racingcar.model.GameSettingsDto;
+import racingcar.model.WinnerDto;
 
 import java.util.List;
 
 public class GameManager {
     private GameSettingsDto gameSettingsDto;
     private List<CarDto> carInformationList;
+    private WinnerDto winnerDto;
 
     private UserInput userInput;
     private RacingGame racingGame;
+    private Judgement judgement;
 
     public GameManager(){
         userInput = new UserInput();
         racingGame = new RacingGame();
+        judgement = new Judgement();
     }
 
     public void startGame(){
         gameSettingsDto = userInput.readUserGameSettingInput();
         carInformationList = racingGame.runRaceIterations(gameSettingsDto);
+        winnerDto = judgement.judgeWinner(carInformationList);
     }
 }
