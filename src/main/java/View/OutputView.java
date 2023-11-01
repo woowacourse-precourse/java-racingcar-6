a6838e58cb2bebc61da28a8442466267616c09cd
JungJei <greteljds0251@gmail.com>
2023-11-01 22:10:29 +0900

    feat: 우승자 출력

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,10 +1,23 @@
 package View;
 
+import java.util.List;
+
 public class OutputView {
     public void roundResult(int position, String name) {
         System.out.printf("%s : ", name);
         for (int i = 0; i < position; i++) {
             System.out.print("-");
+        }
+    }
+
+    public void oneWinner(List<String> name) {
+        System.out.printf("%s", name.get(0));
+    }
+
+    public void someWinner(List<String> name) {
+        System.out.printf("%s", name.get(0));
+        for (int i = 1; i < name.size(); i++) {
+            System.out.printf(", %s", name.get(i));
         }
     }
 }
