0980028402ca4db0cc68a3f33dae2124bc1d8e33
권태현 <kygkth2011@gmail.com>
2023-11-01 20:48:45 +0900

    feat(Car) : Car 클래스 생성 및 생성자 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private Integer score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+}
