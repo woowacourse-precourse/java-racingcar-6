a75936f999afdc5e26d61ea57f1914960b3e1d93
rhdqors <rhdqors@gmail.com>
2023-11-01 15:16:23 +0900

    feat: 경주 실행 및 우승자 출력
    입력받은 게임 횟수와 자동차별 이동 횟수를 확인하여 최종 우승자 출력

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Racing implements RacingBehavior{
 
@@ -26,6 +27,31 @@ public class Racing implements RacingBehavior{
             System.out.println(carNames[k] + " : " + "-".repeat(map.get(carNames[k])));
         }
         System.out.println();
+    }
+
+    @Override
+    public void race() {
+        for (int i = 0; i < user.getTryNum(); i++) {
+            checkStopAndGo();
+        }
+        showWinner();
+    }
+
+    @Override
+    public void showWinner() {
+        map = user.getMap();
+        int max = 0;
+        String winner = "";
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > max) {
+                max = entry.getValue();
+                winner = entry.getKey();
+            } else if(entry.getValue() == max) {
+                winner += ", " + entry.getKey();
+            }
+        }
+        System.out.println("최종 우승자 : " + winner);
     }
 
 }
--- src/main/java/racingcar/RacingBehavior.java
+++ src/main/java/racingcar/RacingBehavior.java
@@ -3,4 +3,6 @@ package racingcar;
 public interface RacingBehavior {
 
     void checkStopAndGo();
+    void race();
+    void showWinner();
 }
