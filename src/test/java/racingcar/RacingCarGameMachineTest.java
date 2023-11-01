2c598f3d510efbd7543c11065ad889d189105007
seminchoi <choicco89@gmail.com>
2023-11-01 11:44:47 +0900

    test(racingcar.RacingCarGameMachineTest) : 초기화 하지 않았을 때 예외처리 테스트 케이스 추가

--- src/test/java/racingcar/RacingCarGameMachineTest.java
+++ src/test/java/racingcar/RacingCarGameMachineTest.java
@@ -22,36 +22,48 @@ public class RacingCarGameMachineTest {
     void init() {
         mockRandomNumberGenerator = (MockRandomNumberGenerator) racingCarGameFactory.randomNumberGenerator();
         racingCarGameMachine = racingCarGameFactory.racingCarGameMachine();
+    }
 
-        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+    @Test
+    void 초기화_하지_않고_게임_실행_예외_처리() {
+        assertThatThrownBy(() -> racingCarGameMachine.getRoundResult())
+                .isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> racingCarGameMachine.getGameResult())
+                .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void 레이싱카_생성() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         String roundResult = racingCarGameMachine.getRoundResult();
 
         assertThat(roundResult).contains("semin", "woowa", "pre");
     }
 
     @Test
-    void 게임_진행_중() {
-        boolean gameInProgress = racingCarGameMachine.isGameInProgress();
+    void 라운드_진행_가능() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
+        boolean gameInProgress = racingCarGameMachine.canPlayRound();
 
         assertThat(gameInProgress).isEqualTo(true);
     }
 
     @Test
-    void 게임_종료() {
-        for (int i = 0; i < ROUND_COUNT; i++) {
-            racingCarGameMachine.getRoundResult();
-        }
-        boolean gameInProgress = racingCarGameMachine.isGameInProgress();
+    void 라운드_진행_불가능() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
+        playRoundUntilGameOver();
+        boolean gameInProgress = racingCarGameMachine.canPlayRound();
 
         assertThat(gameInProgress).isEqualTo(false);
     }
 
     @Test
     void 라운드_지정_횟수_초과_예외처리() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         playRoundUntilGameOver();
 
         assertThatThrownBy(() -> racingCarGameMachine.getRoundResult())
@@ -60,6 +72,8 @@ public class RacingCarGameMachineTest {
 
     @Test
     void 라운드_결과() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         mockRandomNumberGenerator.init(MOVING_FORWARD, STOP, STOP);
 
         String roundResult = racingCarGameMachine.getRoundResult();
@@ -70,12 +84,16 @@ public class RacingCarGameMachineTest {
 
     @Test
     void 게임_진행_중_결과_호출_예외_처리() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         assertThatThrownBy(() -> racingCarGameMachine.getGameResult())
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void 단독_우승() {
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         mockRandomNumberGenerator.init(MOVING_FORWARD, STOP, STOP);
         playRoundUntilGameOver();
         String gameResult = racingCarGameMachine.getGameResult();
@@ -87,6 +105,8 @@ public class RacingCarGameMachineTest {
     @Test
     void 동점_우승() {
         mockRandomNumberGenerator.init(MOVING_FORWARD, MOVING_FORWARD, STOP);
+        racingCarGameMachine.init(RACING_CAR_NAME_INPUT, ROUND_COUNT);
+
         playRoundUntilGameOver();
 
         String gameResult = racingCarGameMachine.getGameResult();
@@ -96,7 +116,7 @@ public class RacingCarGameMachineTest {
     }
 
     void playRoundUntilGameOver() {
-        while (racingCarGameMachine.isGameInProgress()) {
+        while (racingCarGameMachine.canPlayRound()) {
             racingCarGameMachine.getRoundResult();
         }
     }
