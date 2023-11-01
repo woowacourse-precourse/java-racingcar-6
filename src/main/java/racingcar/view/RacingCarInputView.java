013ccd2a4c91c3ec8bbd45dabb30a0e6304c1f94
kunsanglee <gosmdochee2@gmail.com>
2023-11-01 18:41:35 +0900

    refactor(RacingCarInputView): 입력받은 자동차 이름을 UserCarNameDto로 반환하도록 수정

--- src/main/java/racingcar/view/RacingCarInputView.java
+++ src/main/java/racingcar/view/RacingCarInputView.java
@@ -3,17 +3,19 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.dto.UserCarNameDto;
 
 public class RacingCarInputView {
     private static final String DELIMITER = ",";
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNTS_MESSAGE = "시도할 횟수는 몇회인가요?";
 
-    public List<String> inputCarNames() {
+    public UserCarNameDto inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String names = Console.readLine();
-        return Arrays.stream(names.split(DELIMITER))
+        List<String> carNames = Arrays.stream(names.split(DELIMITER))
                 .toList();
+        return new UserCarNameDto(carNames);
     }
 
     public String inputAttemptCounts() {
