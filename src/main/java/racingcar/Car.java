d091abd6cd2d8725575031bed77f4b9b4c818f8a
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 15:51:05 +0900

    feat: Car 클래스 기본 멤버 변수 및 멤버 메소드 정의

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
