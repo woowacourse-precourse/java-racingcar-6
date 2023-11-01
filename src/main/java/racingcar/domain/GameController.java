71d0e0aebd0adeb831e94f2d05c9b64ac9d9ca42
leejuno <zhqkfxm0127@naver.com>
2023-11-01 18:54:36 +0900

    refactor(GameController): 테스트 수행을 위한 인터페이스 메소드 재정의

--- src/main/java/racingcar/domain/GameController.java
+++ src/main/java/racingcar/domain/GameController.java
@@ -3,7 +3,7 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class GameController {
+public class GameController implements GameControllable {
     public void moveOrNot(Car car) {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
@@ -14,7 +14,24 @@ public class GameController {
     public void play(List<Car> carList, int attemptCount) {
         for (int i = 0; i < attemptCount; i++) {
             for (Car car : carList) {
-                car.forward();
+                moveOrNot(car);
+                System.out.println(car.status());
+            }
+        }
+    }
+
+    @Override
+    public void moveOrNot(Car car, int randomNumber) {
+        if (randomNumber >= 4) {
+            car.forward();
+        }
+    }
+
+    @Override
+    public void play(List<Car> carList, int attemptCount, int[] randomNumberList) {
+        for (int i = 0; i < attemptCount; i++) {
+            for (Car car : carList) {
+                moveOrNot(car, randomNumberList[i]);
                 System.out.println(car.status());
             }
         }
