41a434e5722c4c1472ff679f887efc4d6bf318e8
soyoungxxx <otherplet@gmail.com>
2023-11-01 20:28:31 +0900

    fix(racingcar): 변수 형식에 맞춰 Test Code 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
+import java.util.HashMap;
+
 class CarTest {
-  
+    Car carName;
+    final static String NAME_ERROR_MSG = "5글자가 넘는 이름은 입력하실 수 없습니다!";
+
+    @Test
+    void enterName_예외_테스트() {
+        Map<String, Integer> names = new HashMap<>();
+        names.put("Integer", 0);
+
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> carName = new Car(names));
+
+        assertThat(e.getMessage()).isEqualTo(NAME_ERROR_MSG);
+    }
+
 }
