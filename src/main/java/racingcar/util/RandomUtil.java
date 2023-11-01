4a40a628aca5dc568b80fb609669f133cefe42fb
nari <avakray@hansung.ac.kr>
2023-11-01 17:22:57 +0900

    refactor(racingcar): Game 클래스에 기능 통합

--- src/main/java/racingcar/util/RandomUtil.java
+++
@@ -1,9 +0,0 @@
-package racingcar.util;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class RandomUtil {
-    public int getRandomNumber(int start, int end){
-        return Randoms.pickNumberInRange(start, end);
-    }
-}
