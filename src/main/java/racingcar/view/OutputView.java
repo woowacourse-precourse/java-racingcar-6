94c0178700c35b96cf35d95ce70e90c8790d6a36
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-01 16:13:09 +0900

    feat(OutputView): 게임 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
 
     private static final String EXECUTION_RESULT = "\n실행 결과";
+    private static final String GAME_WINNER = "최종 우승자 : ";
 
     public void printExecutionResult() {
         System.out.println(EXECUTION_RESULT);
@@ -15,6 +16,10 @@ public class OutputView {
             System.out.println(status);
         }
         System.out.println();
+    }
+
+    public void printGameWinner(String winners) {
+        System.out.println(GAME_WINNER + winners);
     }
 
 }
