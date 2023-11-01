061377ab87190901c5314617625ed7b7374a540d
joo-sang-ah <jsa@interiorteacher.com>
2023-11-01 18:54:51 +0900

    test: 테스트 코드 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -60,6 +60,9 @@ public class RacingService {
     private Integer readTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String tryCountInput = readLine();
+        if (tryCountInput.equals("0")) {
+            throw new IllegalArgumentException();
+        }
         return validateTryCount(tryCountInput);
     }
 
