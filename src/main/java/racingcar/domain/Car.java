4bc045d8675d1a1e014715b3b8d4eaf8e80b242c
wochae <officialwochae@gmail.com>
2023-11-01 14:57:32 +0900

    feat(entity): car class duplicate validation
    생성자를 생성하는 조건 중에 이름 길이 수를 제한합니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import static racingcar.constant.Constants.NAME_MAX_LENGTH;
+
 public class Car {
     private final int id;
     private final String name;
@@ -18,8 +20,18 @@ public class Car {
     }
 
     public Car(int id, String name, int position) {
+        validator(name);
         this.id = id;
         this.name = name;
         this.position = position;
+    }
+
+    private void validator(String name) {
+        validationNamingRule(name);
+    }
+    private void validationNamingRule(String name) {
+        if (name.length() < NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 }
