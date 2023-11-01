0fbfab6ff24e94c42a2f79ff10ec28ce7c6be79c
kmss6905 <kmss69052@naver.com>
2023-11-01 23:01:39 +0900

    Feature: 시도 횟수 입력 기능 테스트

--- src/test/java/racingcar/IntputViewTest.java
+++ src/test/java/racingcar/IntputViewTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 @DisplayName("입력 뷰 테스트")
 class IntputViewTest {
 
+    private final InputView inputView = new InputView();
+
     @Test
     @DisplayName("출전할 레이싱 이름을 입력받아 RacingCars 로 반환한다. ")
     void inputNameOfRacingCars() {
         String userInput = "Car1,Car2,Car3";
-        InputView inputView = new InputView();
         RacingCars racingCars = inputView.inputNameOfRacingCars(userInput);
 
         assertThat(racingCars.getNames()).contains("Car2", "Car1", "Car3");
