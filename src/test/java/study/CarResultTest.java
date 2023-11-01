804b9eafa3d4df479643cfbe1b201883f5ee02d9
leemiyeon <lemingul7@gmail.com>
2023-11-01 23:17:35 +0900

    test: add CarResultTest

---
+++ src/test/java/study/CarResultTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.result.CarResult;
+
+public class CarResultTest {
+    @Test
+    void getNameTest() {
+
+        CarResult carResult = new CarResult("pobi", 5);
+
+        String retrievedName = carResult.getName();
+
+        assertThat(retrievedName).isEqualTo("pobi");
+    }
+
+    @Test
+    void getDistanceMarkTest() {
+
+        CarResult carResult = new CarResult("pobi", 5);
+
+        String expectedDistanceMark = "-----";
+        String retrievedDistanceMark = carResult.getDistanceMark();
+
+        assertThat(retrievedDistanceMark).isEqualTo(expectedDistanceMark);
+    }
+}
