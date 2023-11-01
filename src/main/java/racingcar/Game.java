15ba1241c68752b7b3eabd97b0a5dce643d3d031
Dendrog <goodshs335@gmail.com>
2023-11-01 18:28:02 +0900

    Feat: 차수별 실행결과 출력기능 작성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,41 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    List<String> carNames;
+    int round;
+
+    public Game() {
+        carNames = Input.inputCarNames();
+        round = Input.inputRound();
+    }
+
+    public void run() {
+        System.out.println("실행 결과");
+
+    }
+
+    private List<Car> setCars() {
+        List<Car> carEntry = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            carEntry.add(new Car(carNames.get(i)));
+        }
+        return carEntry;
+    }
+
+    private void raceStart(List<Car> carEntry) {
+        for (int i = 0; i < round; i++) {
+            for(int j = 0;j<carEntry.size();j++){
+                carEntry.get(i).onceRoundStart();
+                carEntry.get(i).printCarState();
+            }
+            System.out.println();
+        }
+    }
+
+    private void checkWinners(){
+
+    }
 }
