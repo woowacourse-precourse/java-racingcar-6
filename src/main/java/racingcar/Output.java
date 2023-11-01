1765360283bd642be90c668d3b8619b280635cef
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 20:25:41 +0900

    feat(Output): 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,11 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Output {
 
     public enum Message {
         FOR_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
         FOR_TRY_COUNT("시도할 회수는 몇회인가요?"),
-        RESULT("\n실행 결과"),
+        RESULT("실행 결과"),
+        WINNER("최종 우승자 : "),
+        DELIMITER(", ")
         ;
 
         private final String label;
@@ -20,8 +24,7 @@ public class Output {
     }
 
     // 경주할 자동차 이름을 입력하라는 메세지를 출력합니다.
-    public static void promptForCarName()
-    {
+    public static void promptForCarName() {
         System.out.println(Message.FOR_CAR_NAME.label());
     }
 
@@ -32,6 +35,7 @@ public class Output {
 
     // 각 차수별 실행 결과를 출력합니다.
     public static void printResult() {
+        printLine();
         System.out.println(Message.RESULT.label());
     }
 
@@ -39,11 +43,12 @@ public class Output {
         System.out.println(name + " : " + "-".repeat(count));
     }
 
-    public static void printLine()
-    {
+    public static void printLine() {
         System.out.println();
     }
 
-    // TODO : 우승자를 출력합니다. 공동 우승자일 경우 쉼표를 이용하여 구분합니다.
-
+    // 우승자를 출력합니다.
+    public static void printWinner(List<String> winners) {
+        System.out.print(Message.WINNER.label() + String.join(Message.DELIMITER.label(), winners));
+    }
 }
