9d39d4fa0f66504bde66c2f2667608e9a6922f6b
HanDaWoon <bookpia.han@gmail.com>
2023-11-01 12:01:58 +0900

    test: 출력 테스트 작성

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,55 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.CarModel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OutputViewTest {
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void printResult() {
+        OutputView.printResult();
+        assertEquals("\n실행 결과\n", outputStreamCaptor.toString());
+    }
+
+    @Test
+    void printGameResult() {
+        CarModel[] testCars = new CarModel[2];
+
+        testCars[0] = new CarModel("pobi");
+        testCars[0].move();
+        testCars[0].move();
+        testCars[1] = new CarModel("woni");
+        testCars[1].move();
+
+        OutputView.printGameResult(testCars);
+
+        assertEquals("pobi : --\nwoni : -\n\n", outputStreamCaptor.toString());
+    }
+
+    @Test
+    void printWinners() {
+        String[] winnerTestCase1 = {"pobi", "woni"};
+
+        OutputView.printWinners(winnerTestCase1);
+        assertEquals("최종 우승자 : pobi, woni\n", outputStreamCaptor.toString());
+    }
+}
