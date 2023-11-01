48880a5e51eb33691df97ba97eed81aa5fe75c6c
kite707 <12201680@inha.edu>
2023-11-01 19:23:08 +0900

    test(racingcar): 전진 여부 테스트 코드 작성
    생성한 랜덤 숫자가 4 이상인지 여부를 판단하여 전진 여부를 결정하는 함수의 테스트코드를 작성하였습니다.

---
+++ src/test/java/racingcar/domain/EngineTest.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EngineTest {
+    private Engine engine;
+
+
+    @BeforeEach
+    void setUp() {
+        engine = new Engine();
+    }
+
+    @Test
+    void 전진_여부_결정_테스트() {
+        boolean result = engine.isMovingFoward();
+        assertThat(result).isIn(true, false);
+    }
+
+}
