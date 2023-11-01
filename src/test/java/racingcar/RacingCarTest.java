2bd73d8f2dfe8abff3ef34be754f1479abb9b651
Sinorang <aodkrak@naver.com>
2023-11-01 23:43:36 +0900

    feat. 테스트 코드 추가
    - 자동차 전진 상황 출력 테스트 코드 작성
    - 우승자 찾기 테스트 코드 작성
    - 우승자가 여러 명일 경우 최종 우승자 출력 테스트 코드 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,26 +3,31 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.Game;
 import racingcar.service.Forward;
+import racingcar.service.GameService;
 import racingcar.util.Parser;
+import racingcar.view.OutputView;
 
-class RacingCarTest {
+class RacingCarTest extends NsTest {
 
     Parser parser;
     Forward forward;
+    GameService gameService;
 
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+    private static final int NUMBER_OF_TRIAL = 2;
 
     @BeforeEach
     void setUp() {
         parser = new Parser();
         forward = new Forward();
+        gameService = new GameService();
     }
 
     @Test
@@ -50,5 +55,50 @@ class RacingCarTest {
     void 무작위_값_생성_테스트() {
         int randomNumber = forward.generateRandomNumber();
         assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @Test
+    void 자동차_전진_상황_출력_테스트() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("car1"));
+        cars.add(new Car("car2"));
+        cars.get(0).moveForward(1);
+        cars.get(1).moveForward(2);
+        OutputView.showScore(cars);
+
+        assertThat(output()).contains("car1 : -", "car2 : --");
+    }
+
+    @Test
+    void 우승자_찾기() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("car1"));
+        cars.add(new Car("car2"));
+        cars.get(0).moveForward(1);
+        cars.get(1).moveForward(2);
+
+        Game game = new Game(cars, NUMBER_OF_TRIAL);
+        OutputView.showWinners(gameService.findWinners(game));
+
+        assertThat(output()).contains("최종 우승자 : car2");
+    }
+
+    @Test
+    void 우승자가_여러명일_경우_최종_우승자_출력() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("car1"));
+        cars.add(new Car("car2"));
+        cars.get(0).moveForward(2);
+        cars.get(1).moveForward(2);
+
+        Game game = new Game(cars, NUMBER_OF_TRIAL);
+        OutputView.showWinners(gameService.findWinners(game));
+
+        assertThat(output()).contains("최종 우승자 : car1, car2");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
