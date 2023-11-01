6508d8f21d015db22f8bf51d4d2c08512013b89c
donghoonyeom <dua5410773@naver.com>
2023-11-01 16:33:39 +0900

    test(CarTest): 자동차 모델 및 이동 기능에 대한 테스트 추가
    Car 클래스의 생성과 이동 기능을 검증하기 위한 테스트를 작성했습니다. 유효한 자동차 이름을 가지고 올바르게 생성되는지, 잘못된 이름으로 생성할 때 예외가 발생하는지, 자동차가 정상적으로 이동하는지 등을 확인했습니다. 각 테스트 케이스는 예외 상황 및 정상 동작을 포함하여 다양한 시나리오를 다루었습니다.

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,74 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+class CarTest {
+
+    @Test
+    @DisplayName("올바른 자동차 이름 생성 테스트")
+    void createCarWithValidName() {
+        Car car = new Car("car1");
+        assertThat(car.getName()).isEqualTo("car1");
+        assertThat(car.getDistance()).isZero();
+    }
+
+    @Test
+    @DisplayName("잘못된 자동차 이름으로 생성할 때 예외 발생 테스트")
+    void createCarWithInvalidName() {
+        assertThatThrownBy(() -> new Car(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1~5자이고 공백이 없으며 특수 기호를 포함할 수 없습니다.");
+
+        assertThatThrownBy(() -> new Car("  "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1~5자이고 공백이 없으며 특수 기호를 포함할 수 없습니다.");
+
+        assertThatThrownBy(() -> new Car("car with space"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1~5자이고 공백이 없으며 특수 기호를 포함할 수 없습니다.");
+
+        assertThatThrownBy(() -> new Car("car#"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1~5자이고 공백이 없으며 특수 기호를 포함할 수 없습니다.");
+
+        assertThatThrownBy(() -> new Car("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1~5자이고 공백이 없으며 특수 기호를 포함할 수 없습니다.");
+
+        assertThatThrownBy(() -> Car.createCars(Arrays.asList("car1", "car1")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("자동차 이동 테스트")
+    void moveCar() {
+        Car car = new Car("car1");
+        int initialDistance = car.getDistance();
+
+        car.move();
+
+        assertThat(car.getDistance()).isGreaterThanOrEqualTo(initialDistance);
+    }
+
+    @Test
+    @DisplayName("여러 자동차 생성 및 이동 테스트")
+    void createAndMoveCars() {
+        List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        List<Car> cars = Car.createCars(carNames);
+
+        for (Car car : cars) {
+            int initialDistance = car.getDistance();
+            car.move();
+            assertThat(car.getDistance()).isGreaterThanOrEqualTo(initialDistance);
+        }
+    }
+}
