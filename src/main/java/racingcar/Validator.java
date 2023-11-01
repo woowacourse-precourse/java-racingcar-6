6b99093a5b47ddf695c23e5285751f24b8451a3e
leedoyeon849 <do83430208@gmail.com>
2023-11-01 19:31:49 +0900

    fix: 테스트에 따른 Validator 로직 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -6,8 +6,12 @@ public class Validator {
      * 콤마 사이의 공백이 있는지 확인한다.
      */
     public static void validateNoSpacesBetweenCommas(String input) {
-        if (input.matches(".*,\\s.*,.*")) {
-            throw new IllegalArgumentException("쉼표 간에 공백은 허용되지 않습니다.");
+        String[] splited = input.split(",");
+
+        for (String s : splited) {
+            if (s.matches("^\\s+.*|.*\\s+$")) {
+                throw new IllegalArgumentException("쉼표 간에 공백은 허용되지 않습니다.");
+            }
         }
     }
 
@@ -15,7 +19,7 @@ public class Validator {
      * 사용자 입력은 한글자 이상이어야한다.
      */
     public static void validateHasAtLeastOneCharacter(String input) {
-        if (input.length() == 0 || input == null || input.isEmpty()) {
+        if (input == null || input.isEmpty() || input.length() == 0  ) {
             throw new IllegalArgumentException("최소 한 글자 이상 입력해야 합니다.");
         }
     }
@@ -28,8 +32,8 @@ public class Validator {
 
     public static void validateWithinRange(Integer input) {
 
-        if (input < 0 || input > 2_147_483_647) {
-            throw new IllegalArgumentException("입력값은 0 이상, 2,147,483,647 이하이어야 합니다.");
+        if (input < 0 || input > 2_147_483_646) {
+            throw new IllegalArgumentException("입력값은 0 초과, 2,147,483,646 미ㅑ이어야 합니다.");
         }
     }
 }
