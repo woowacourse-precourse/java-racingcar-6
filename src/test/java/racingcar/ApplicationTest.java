93862a968c2e3cd0c9cd94644f7f340a0f0a0936
kyu-wooon <catdog0831@gmail.com>
2023-11-01 14:17:20 +0900

    * 테스트케이스 줄 정렬

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,9 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,17 +10,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.Input_car.*;
 
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -36,7 +34,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 자동차_이름_중복_입력_예외_처리(){
+    void 자동차_이름_중복_입력_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("car1,car2,car2", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -44,7 +42,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 횟수_숫자_이외_글자_예외_처리(){
+    void 횟수_숫자_이외_글자_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("car1,car2,car3", "two"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -53,11 +51,11 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 입력값_확인_테스트() {
-                run("car1,car2,car3", "3");
-                assertThat(car_map.containsKey("car1")).isTrue();
-                assertThat(car_map.containsKey("car2")).isTrue();
-                assertThat(car_map.containsKey("car3")).isTrue();
-                assertThat(excute_count.equals("3"));
+        run("car1,car2,car3", "3");
+        assertThat(car_map.containsKey("car1")).isTrue();
+        assertThat(car_map.containsKey("car2")).isTrue();
+        assertThat(car_map.containsKey("car3")).isTrue();
+        assertThat(excute_count.equals("3"));
     }
 
     @Override
