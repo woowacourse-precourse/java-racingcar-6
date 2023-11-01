bb96a4c8ccff79bccdbe7e812a3abe8d545c0db3
ghdeo <goldencorn7@naver.com>
2023-11-01 21:56:02 +0900

    refactor: Segregate methods

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -5,24 +5,34 @@ import racingcar.domain.Cars;
 import java.util.Scanner;
 
 public class RacingService {
+    private final Scanner sc = new Scanner(System.in);
+    private int trial = 0;
     private Cars cars;
 
-    public void startRace() {
+    public void play() {
+        setCars();
+        setTrial();
+        race();
+        cars.printWinners();
+    }
+
+    private void setCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Scanner sc = new Scanner(System.in);
+
         String playerInput = sc.nextLine();
         String[] names = playerInput.split(",");
         cars = new Cars(names);
+    }
 
+    private void setTrial() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int trial = sc.nextInt();
+        this.trial = sc.nextInt();
+    }
 
+    private void race() {
         System.out.println("\n실행 결과");
         for (int i = 0; i < trial; i++) {
             cars.race();
         }
-
-        cars.printWinners();
     }
-
 }
