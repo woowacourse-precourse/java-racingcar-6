c10e2b310e8b89887c8f7d13c9e388fbf09836ce
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 20:27:11 +0900

    feat : 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,10 @@
 package racingcar.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Splitter;
 
+public class InputView {
+    public static String[] inputCarNames() {
+        return Splitter.splitName(Console.readLine());
+    }
 }
