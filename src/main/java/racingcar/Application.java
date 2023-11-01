5c2c9dc5348bfe4a7c4520a8a512fa6f4db5b3fb
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-01 18:09:36 +0900

    docs: main()에 TODO 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 자동차 이름 입력 받기
+
+        // TODO: 시도할 회수 입력 받기
+
+        // TODO: 전진 및 멈춤 로직
+
+        // TODO: 실행 결과 출력
+
+        // TODO: 우승자 출력
     }
 }
