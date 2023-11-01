cf9d689340b1a8a9c11038a09a7d52286c7578c7
grax <grax@192.168.35.119>
2023-11-01 17:14:08 +0900

    feat: "create displayResult() method"
    "positionList의 값에 따라 -를 붙여 출력한다"

--- src/main/java/racingcar/view/Printer.java
+++ src/main/java/racingcar/view/Printer.java
@@ -1,8 +1,27 @@
 package racingcar.view;
 
-public class Printer {
+import java.util.List;
 
+import racingcar.domain.GameData;
+
+public class Printer {
 	public void displayMessage(String message) {
 		System.out.print(message);
 	}
+	
+	public void displayResult(GameData data) {
+		for (int i = 0; i < data.getCarList().size(); i++) {
+			String carName = data.getCarList().get(i);
+            int position = data.getPositionList().get(i);
+            
+            StringBuilder result = new StringBuilder(carName + " : ");
+            for (int j = 0; j < position; j++) {
+                result.append("-");
+            }
+            System.out.println(result.toString());
+        }
+		System.out.println();
+    }
+	
+
 }
