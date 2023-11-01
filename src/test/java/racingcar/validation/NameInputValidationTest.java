36bab494a2a800399660b615dc9b4415f40db4c3
hyeonwoo <ziczin7176@gmail.com>
2023-11-01 21:32:52 +0900

    test: 자동차 이름 중복 테스트

--- src/test/java/racingcar/validation/NameInputValidationTest.java
+++ src/test/java/racingcar/validation/NameInputValidationTest.java
@@ -16,4 +16,13 @@ public class NameInputValidationTest {
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
     }
+
+    @Test
+    @DisplayName("중복인 자동차 이름이 있는가?")
+    public void isExistNameTest() {
+        String[] nameList = {"pobi", "woni", "woni"};
+        assertThatThrownBy(() -> nameInputValidation.isExistName(nameList))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름을 입력할 수 없습니다.");
+    }
 }
