05376a62a5cc49e4bdf50c54d2417c5c36414c9e
Kim mingyu <minq0302@naver.com>
2023-11-01 20:04:45 +0900

    feat(controller): add running game method

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,14 +1,20 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.RacingGameModel;
 import racingcar.view.RacingGameView;
 
 public class RacingGameController {
 
-    private RacingGameView racingGameView;
+    private final RacingGameView racingGameView;
+    private final RacingGameModel racingGameModel;
+    private List<Car> cars;
 
-    public RacingGameController(RacingGameView racingGameView){
+    public RacingGameController(RacingGameView racingGameView, RacingGameModel racingGameModel){
         this.racingGameView = racingGameView;
+        this.racingGameModel = racingGameModel;
     }
 
     // 1. 자동차 이름 입력받는 기능
@@ -23,5 +29,24 @@ public class RacingGameController {
         String inputValue = Console.readLine();
 
         return Integer.valueOf(inputValue);
+    }
+
+    // 게임을 실행하는 메서드
+    public void playGame() {
+        racingGameView.showCarNameInputMessage();
+        String carNames = inputCarName();
+        cars = racingGameModel.stringToList(carNames);
+
+        racingGameView.showTrialNumberInputMessage();
+        int trialNumber = inputTrialNumber();
+
+        racingGameView.printResultMessage();
+        for(int index=0;index<trialNumber;index++){
+            racingGameModel.playOneRound(cars);
+            racingGameView.printCarsLocation(cars);
+        }
+
+        List<Car> winners = racingGameModel.pickWinners(cars);
+        racingGameView.printFinalWinners(winners);
     }
 }
