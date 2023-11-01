9c715ad76a11acd907503f22c37a0533195b7901
gourderased <gourderased@gmail.com>
2023-11-01 15:43:48 +0900

    "Refactor(racingcar): generateRandomNumber 메소드 범위를 변수값으로 변경"

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -3,7 +3,9 @@ package racingcar.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+    private static final int MIN = 0;
+    private static final int MAX = 9;
     public static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(MIN, MAX);
     }
 }
