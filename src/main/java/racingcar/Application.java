4a6939b63806e4d2956ae59897ba9c22f3566a37
Subi Hwang <ghkdtnql@gmail.com>
2023-11-01 23:12:00 +0900

    feat: 우승자 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,48 +1,63 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
-    static ArrayList<String> playerCar = new ArrayList<>();
-    static ArrayList<Integer> playerLine = new ArrayList<>();
+  static ArrayList<String> playerCar = new ArrayList<>();
+  static ArrayList<Integer> playerLine = new ArrayList<>();
 
-    static ArrayList<String> winner = new ArrayList<>();
-    public static void main(String[] args) {
+  static int maxValue = 0;
+  static ArrayList<String> winner = new ArrayList<>();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = readLine().trim();
-        for(String carName: input.split(",")){
-            if(carName.length() == 0 || carName.length() > 5){
-                throw new IllegalArgumentException("자동차 이름을 다시 입력해주세요.");
-            }
-            playerCar.add(carName);
-            playerLine.add(0);
-        }
+  public static void main(String[] args) {
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        String testCase = readLine();
-
-        try {
-            Integer.parseInt(testCase);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
-        }
-
-        System.out.println("\n" + "실행결과" + "\n");
-
-        for(int i=0;i < Integer.parseInt(testCase);i++){
-           for(int j=0;j<playerCar.size();j++){
-               int randomNum = Randoms.pickNumberInRange(0,9);
-               if(randomNum >=4){
-                  playerLine.set(j,playerLine.get(j)+1);
-               }
-               System.out.println(playerCar.get(j).concat(" : ").concat("-".repeat(playerLine.get(j))));
-           }
-        }
-
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String input = readLine().trim();
+    for (String carName : input.split(",")) {
+      if (carName.length() == 0 || carName.length() > 5) {
+        throw new IllegalArgumentException("자동차 이름을 다시 입력해주세요.");
+      }
+      playerCar.add(carName);
+      playerLine.add(0);
     }
+
+    System.out.println("시도할 회수는 몇회인가요?");
+    String testCase = readLine();
+
+    try {
+      Integer.parseInt(testCase);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("숫자만 입력해주세요.");
+    }
+
+    System.out.println("\n" + "실행결과" + "\n");
+
+    for (int i = 0; i < Integer.parseInt(testCase); i++) {
+      for (int j = 0; j < playerCar.size(); j++) {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+          playerLine.set(j, playerLine.get(j) + 1);
+          if (maxValue < playerLine.get(j)) {
+            maxValue = playerLine.get(j);
+          }
+        }
+        System.out.println(playerCar.get(j).concat(" : ").concat("-".repeat(playerLine.get(j))));
+      }
+      System.out.println();
+    }
+
+    String result = "";
+    for (int i = 0; i < playerCar.size(); i++) {
+      if (playerLine.get(i) == maxValue) {
+        result += playerCar.get(i) + ", ";
+      }
+    }
+
+    System.out.println("최종 우승자 : " + result.substring(0, result.length() - 2));
+  }
 }
