c64f81e30d906f0ed32039e329963fd4bb905fea
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-01 23:55:32 +0900

    refactor(InputViewTest): 시도 횟수 입력 실패 테스트 삭제

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,7 +1,6 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -10,7 +9,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.view.InputView;
 
 public class InputViewTest {
 
@@ -32,16 +30,4 @@ public class InputViewTest {
 
         assertThat(result).isEqualTo(Arrays.asList("pobi", "woni", "jun"));
     }
-
-    @DisplayName("getRoundTime must be fail when input is not Integer type")
-    @Test
-    void getRoundTimeMustBeFail() {
-        String input = "A";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        assertThatThrownBy(() -> inputView.getRoundTime())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
 }
