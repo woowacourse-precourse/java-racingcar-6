45970d1914b65c68e66b022cde69b66cb5de00ad
myoungjin-kim <kaudwls58@naver.com>
2023-11-01 13:08:41 +0900

    feat: Application 클래스 작성
    전체적인 코드의 컨트롤을 담당하는 Application 클래스를 작성하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
 public class Application {
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPTS_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE ="\n실행 결과";
+
+    public void run(){
+        String[] carNames;
+        int attemptsNumber;
+
+        UserInput userInput = new UserInput();
+
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        carNames = userInput.carName();
+
+        System.out.println(INPUT_ATTEMPTS_NUMBER_MESSAGE);
+        attemptsNumber = userInput.attemptsNumber();
+
+        System.out.println(RESULT_MESSAGE);
+        PlayGame playGame = new PlayGame(carNames, attemptsNumber);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.run();
     }
 }
