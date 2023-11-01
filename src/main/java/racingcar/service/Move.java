f8ec04e0d7601e085d712a5f36c4d3239db58e92
dabin <db021129@naver.com>
2023-11-01 09:59:23 +0900

    feat: Move의 moveOrNot메소드 수

---
+++ src/main/java/racingcar/service/Move.java
@@ -0,0 +1,22 @@
+package racingcar.service;
+
+public class Move {
+
+    private static final int MOVE = 1;
+    private static final int NOT_MOVE = 0;
+
+    Random random = new Random();
+
+    private int moveOrNot() {
+        int isOverFour = random.randomNumber();
+        int result;
+
+        if (isOverFour < 4) {
+            result = NOT_MOVE;
+        } else {
+            result = MOVE;
+        }
+
+        return result;
+    }
+}
