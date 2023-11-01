a52fe0254fa4e294993c342108df7e0d335ef45f
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-01 15:22:22 +0900

    feat: Car Class 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,23 +2,34 @@ package racingcar;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class RacingGame {
-    List<String> carList = new ArrayList<>();
-    int tryNumber = 0;
+    List<Car> carList = new ArrayList<>();
 
-    public void getInput(){
+    public void getCarNameInput(){
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = Console.readLine();
-        carList = Arrays.asList(cars.split(","));
+        List<String> carNameList = new ArrayList<>();
+        carNameList = Arrays.asList(cars.split(","));
+        for (String carName: carNameList) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름의 길이가 5자가 넘습니다.");
+            } else {
+                carList.add(new Car(carName, 0));
+            }
+        }
+    }
+
+    public int getTryNumberInput(){
 
         System.out.println("시도할 회수는 몇회인가요?");
-        tryNumber = Integer.parseInt(Console.readLine());
+        return Integer.parseInt(Console.readLine());
 
     }
 }
