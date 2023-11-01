83a7bc5648488be637bd53b893c5748985f64603
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-01 17:32:26 +0900

    test: 입력 받은 시도 횟수가 숫자형태가 아닐때 예외 발생 테스트

--- src/test/java/racingcar/NumberOfAttemptsTest.java
+++ src/test/java/racingcar/NumberOfAttemptsTest.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.constant.ErrorMessage.NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +12,13 @@ class NumberOfAttemptsTest {
     void 입력_받은_시도_횟수_숫자로_변환() {
         NumberOfAttempts numberOfAttempts = new NumberOfAttempts("5");
         Assertions.assertThat(numberOfAttempts.getNumberOfAttempts()).isEqualTo(5);
+    }
+
+    @Test
+    void 입력_받은_시도_횟수는_숫자_형태여야_한다() {
+        assertThatThrownBy(() -> new NumberOfAttempts("숫자"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NUMBER_OF_ATTEMPTS_IS_NOT_NUMBER);
     }
 
 }
