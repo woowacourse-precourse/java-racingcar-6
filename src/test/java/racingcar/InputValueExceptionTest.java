7281cc416d280675f86c3e674c130182118a56a4
Lieslot <rlaxodud1999@gmail.com>
2023-11-01 22:20:43 +0900

    test: 자동차 이름 중복 처리 테스트 작성

--- src/test/java/racingcar/InputValueExceptionTest.java
+++ src/test/java/racingcar/InputValueExceptionTest.java
@@ -1,10 +1,15 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Car;
 import racingcar.domain.ForwardTryCount;
+import racingcar.domain.RacingCars;
 import racingcar.vo.CarName;
 
 public class InputValueExceptionTest {
@@ -32,6 +37,14 @@ public class InputValueExceptionTest {
     @ValueSource(strings = {"wk2", "le2", "eeee"})
     void nonNumbersTest(String input) {
         Assertions.assertThatThrownBy(() -> new ForwardTryCount(input))
+                  .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차의 이름들 중 중복된 것이 있으면 예외처리")
+    @Test
+    void CarNameDuplicationExceptionTest() {
+        List<Car> cars = Arrays.asList(new Car(new CarName("pobi")), new Car(new CarName("pobi")));
+        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
                   .isInstanceOf(IllegalArgumentException.class);
     }
 }
