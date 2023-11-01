1fbb2f8c8b5242d319882f986ca0f71203888074
plan11plan <tjtfy7028@gmail.com>
2023-11-01 21:06:42 +0900

    refact:[update][InputView] requestCarNames() 반환 타입 변환

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,10 +9,11 @@ import java.util.stream.Collectors;
 import racingcar.Car;
 
 public class InputView {
-    public static void requestCarNames() {
+    public static String[] requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분, 5자 이하)");
         String input = readLine();
         String[] carNames = input.split(",");
+        return carNames;
     }
 
     public static List<Car> responseCarNames(String[] carNames) {
