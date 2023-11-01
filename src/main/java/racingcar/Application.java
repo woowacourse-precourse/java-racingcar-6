2bafc09c45c1fc4f493332ace8f2ea97787cea36
sangxxjin <sj_0368@naver.com>
2023-11-01 14:09:45 +0900

    feat: Controller 따로 구현이 필요하지 않다고 생각하여 main에 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Cars cars = new Cars(InputView.getCarsName().split(","));
+        int loopCount = Integer.parseInt(InputView.getLoopCount());
+        OutputView.showResultMessage();
+        for (int i = 0; i < loopCount; i++) {
+            cars.moveAllCars();
+            for (Car car : cars.getCars()) {
+                OutputView.showCarDistanceResults(car.getName(),
+                        OutputView.createDistanceString(car.getDistance()));
+            }
+            System.out.println();
+        }
+        OutputView.winnerResult(cars.compareCarDistances());
     }
 }
