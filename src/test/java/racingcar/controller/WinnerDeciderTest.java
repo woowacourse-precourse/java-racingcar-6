534d1cac212b0cf946105584cd9bcf538b2399c4
davemins <davemins4125@naver.com>
2023-11-01 17:32:39 +0900

    test: 우승자를 제대로 판단하는지 확인하는 테스트 작성 getWinner 메서드가 제대로 된 값을 리턴하는지 확인하는 테스트 코드를 작성했습니다.

---
+++ src/test/java/racingcar/controller/WinnerDeciderTest.java
@@ -0,0 +1,36 @@
+package racingcar.controller;
+
+import org.junit.jupiter.api.Test;
+import racingcar.model.Racer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinnerDeciderTest {
+
+    @Test
+    void 우승자_제대로_리턴하는지_확인() {
+        // Given
+        List<Racer> racers = new ArrayList<>();
+        Racer racer1 = new Racer("racer1");
+        Racer racer2 = new Racer("racer2");
+        Racer racer3 = new Racer("racer3");
+
+        // When
+        racer1.goState();
+        racer1.goState();
+        racer2.goState();
+        racer3.goState();
+        racer3.goState();
+        racer3.goState();
+        racers.add(racer1);
+        racers.add(racer2);
+        racers.add(racer3);
+
+        // Then
+        assertThat(WinnerDecider.getWinner(racers)).isEqualTo("racer3");
+    }
+}
