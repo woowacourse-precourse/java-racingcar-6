b27b89fdf7f3aaf10829cc3b3283459c2d3dc4db
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 10:45:59 +0900

    [feat] Service - 게임 진행 메소드 작성

--- src/main/java/racingcar/Service/RacingCarService.java
+++ src/main/java/racingcar/Service/RacingCarService.java
@@ -1,4 +1,20 @@
 package racingcar.Service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Domain.Car;
+import racingcar.Domain.Race;
+
 public class RacingCarService {
+    public void playRaceAndIncreaseTrialCount(Race race) {
+        for (Car car : race.getCars()) {
+            if (4 <= generateRandomNumber()) {
+                car.carMove(1);
+            }
+        }
+        race.increaseTrialCount();
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
