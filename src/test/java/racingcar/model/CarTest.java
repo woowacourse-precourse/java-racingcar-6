46ebcdcb25477f31f7b863c9a0bebfbb956822b6
seona-moon <fhflwhwl5@gmail.com>
2023-11-01 21:26:21 +0900

    test: Car와 Cars 객체 생성 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,4 +1,29 @@
+package racingcar.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CarTest {
-  
+    @Test
+    @DisplayName("Car가 제대로 생성되었는지 확인한다 (정확한 이름 반환)")
+    void 객체_생성_이름확인() {
+        //given
+        Car car = new Car("seona");
+        //then
+        assertThat(car.getName()).isEqualTo("seona");
+    }
+
+    @Test
+    @DisplayName("Car가 제대로 생성되었는지 확인한다 (생성시 거리는 0)")
+    void 객체_생성_거리확인() {
+        //given
+        Car car = new Car("seona");
+        //then
+        assertThat(car.getMovement()).isEqualTo(0);
+    }
 }
--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,4 +1,53 @@
+package racingcar.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CarsTest {
-  
+    private Cars cars;
+
+    @Test
+    @DisplayName("Cars가 제대로 생성되었는지 확인한다.")
+    void 객체_생성_확인() {
+        // Given
+        String validNames = "Car1,Car2,Car3";
+
+        // When
+        cars = new Cars(validNames);
+
+        // Then
+        assertEquals(3, cars.getCars().size());
+    }
+
+    @Test
+    @DisplayName("빈 String은 예외 처리")
+    void 객체_생성시_공백_예외() {
+        // Given
+        String emptyNames = "";
+
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> new Cars(emptyNames));
+    }
+
+    @Test
+    @DisplayName("중복된 이름은 예외처리")
+    void 객체_생성시_중복_예외() {
+        // Given
+        String duplicateNames = "Car1,Car2,Car1";
+
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> new Cars(duplicateNames));
+    }
+
+    @Test
+    @DisplayName("6글자를 넘는 이름은 예외처리")
+    void 객체_생성시_유효한_길이() {
+        // Given
+        String longName = "ThisNameIsTooLongToBeValid";
+
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> new Cars(longName));
+    }
 }
