502e0887b387fb03961a40c8aeeeedf9542f8e08
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-11-01 17:48:50 +0900

    refactor(): 랜덤값 생성 코드 수정 랜덤값 범위 상수 선언 - 코드의 명확성을 위해 랜덤값의 범위를 상수로 선언하여 변경하였습니다?

--- src/main/java/racingcar/domain/util/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/util/RandomMoveStrategy.java
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
     public static final int MIN_NUM = 4;
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
 
     @Override
     public boolean move() {
-        int randomValue = Randoms.pickNumberInRange(0, 9);
+        int randomValue = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
         return randomValue >= MIN_NUM;
     }
 }
