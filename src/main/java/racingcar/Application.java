4389ef6b2d4227bbd88634213688a4631b96deaf
ddshpp <dshp753@gmail.com>
2023-11-01 22:30:13 +0900

    feat: 각 차수별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,10 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import view.InputView;
+import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -19,6 +21,15 @@ public class Application {
 
         if (testNumber < 0) {
             throw new IllegalArgumentException("숫자 입력은 양수만 가능합니다.");
+        }
+
+        OutputView.printResult();
+        for (int i = 0; i < testNumber; i++) {
+            for (Car car : cars) {
+                car.move(Randoms.pickNumberInRange(0, 9));
+                OutputView.printCarInfo(car);
+            }
+            OutputView.printNewLine();
         }
     }
 }
