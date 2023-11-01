3a74870871be3f84e002e2c400b3ddf4963e8929
hoosa <janghoosa@gmail.com>
2023-11-01 14:25:36 +0900

    feat: Output 클래스 구현

---
+++ src/main/java/racingcar/RacingCarOutput.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class RacingCarOutput {
+    public void printCarNameInput(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printUserInput(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printStep(){
+
+    }
+
+    public void printWinner(){
+
+    }
+}
