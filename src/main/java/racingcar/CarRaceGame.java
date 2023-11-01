54e9c949cf57227117d57ee605342ad8c096157a
sungmin306 <another0306@naver.com>
2023-11-01 22:47:10 +0900

    Refactor: 코드 리펙토링 우승자 출력 코드 리펙토링

--- src/main/java/racingcar/CarRaceGame.java
+++ src/main/java/racingcar/CarRaceGame.java
@@ -20,12 +20,7 @@ public class CarRaceGame {
         }
         System.out.print("최종 우승자 : ");
         List<String> winners = getWinner(players);
-        for(int i = 0; i < winners.size(); i++){
-            if(i == winners.size()-1)
-                System.out.print(winners.get(i));
-            else
-                System.out.print(winners.get(i) + ", ");
-        }
+        System.out.println(String.join(", ",winners));
     }
 
     public static void generateRandomNumber(Player[] players) {
