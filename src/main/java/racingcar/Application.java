266c368f0e10637e8a9adfbe36c1c08a3068d43e
yh-skku <112400744+yh-skku@users.noreply.github.com>
2023-11-01 21:17:18 +0900

    test: 여러 메서드 테스트 확인
    testMakeRandomNumberInRange()로 0~9사이의 랜덤 값이 추출되는지 테스트
    testInputException() 6글자 이상 입력시 예외가 발생하는지 확인
    testMakeWinnerList() 우승자 출력 기능 정상 작동 테스트
    자동차가_4_이상일때_전진하는지_확인()과 random_값을_제어했을때_올바른_값이_나오는지_확인() 테스트 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,15 +3,21 @@ package racingcar;
 import camp.nextstep.edu.missionutils.*;
 
 import java.io.ByteArrayInputStream;
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.test.*;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class Car{
     String name;
@@ -148,7 +154,7 @@ class UserNameInput{
 
 }
 
-class InputTest extends NsTest{
+class InputTest {
 
     @Test
     @DisplayName("사용자 입력이 콤마 단위로 List에 구현 되는지 확인")
@@ -162,6 +168,88 @@ class InputTest extends NsTest{
 
 
         assertThat(nameList).containsExactly("car1", "car2", "car3", "car4", "car5");
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("0~9 사이의 무작위 값이 추출 되는지 확인")
+    public void testMakeRandomNumberInRange(){
+        int min = 0;
+        int max = 9;
+
+        int random = Car.makeRandomNumberInRange();
+
+        assertTrue(random >= min && random <= max);
+
+    }
+
+    @Test
+    @DisplayName("5글자 이상 입력시 예외가 발생하는지 확인")
+    public void testInputException() {
+
+        String input = "car1,car12345"; // 입력 데이터
+        System.setIn(new ByteArrayInputStream(input.getBytes())); // 입력
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserNameInput.InputNameToList();
+
+        });
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("우승자 출력 기능이 작동하는지 테스트")
+    public void testMakeWinnerList(){
+        ArrayList<Car> carList = new ArrayList<>();
+        carList.add(new Car("car1"));
+        carList.add(new Car("car2"));
+        carList.add(new Car("car3"));
+
+
+        carList.get(0).moveCount = 3;  // car1의 이동 횟수는 3
+        carList.get(1).moveCount = 5;  // car2의 이동 횟수는 5
+        carList.get(2).moveCount = 5;  // car3의 이동 횟수도 5
+        Car.maxMoveCount=5;
+
+        ArrayList<String> winnerList = Car.makeWinnerList(carList);
+
+        assertThat(winnerList).containsExactly("car2", "car3");
+
+    }
+}
+
+
+class RacingCarTest extends NsTest{
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    @DisplayName("자동차가 4 이상일때 전진하는지 확인")
+    void 자동차가_4_이상일때_전진하는지_확인(){
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("car1,car2,car3", "5");
+                    assertThat(output()).contains("car1 : -----", "car2 : -----", "car3 : -----", "최종 우승자 : car1, car2, car3");
+                },
+                4, 6, 9
+        );
+        Console.close();
+
+    }
+
+    @Test
+    @DisplayName("random값을 제어했을 때 올바른 결과가 나오는지 확인")
+    void random_값을_제어했을때_올바른_값이_나오는지_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("car1,car2,car3", "5");
+                    assertThat(output()).contains("car1 : ", "car2 : -----","car3 : -----", "최종 우승자 : car2, car3");
+                },
+                STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
+        Console.close();
     }
     @Override
     protected void runMain(){
@@ -170,10 +258,10 @@ class InputTest extends NsTest{
 
     }
 
+
+
+
 }
-
-
-
 
 
 
