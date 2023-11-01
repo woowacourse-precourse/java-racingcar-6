0fa55c474073d4dc9306dcd6c4a735757f98741b
“sangjun121” <juncho12011201@gmail.com>
2023-11-01 12:38:19 +0900

    feat: 게임 결과값 출력해주는 함수 구현
    printExecutionResult()에서 데이터를 바탕으로 출력을 진행하고, 이중 반복문을 피하기 위해 printAdvancementCount()를 호출하여 전진 횟수를 출력하였다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,12 @@
 package racingcar.view;
 
+import racingcar.model.CarDto;
+
+import java.util.List;
+
 import static racingcar.utility.Constants.EXECUTION_RESULT;
+import static racingcar.utility.Constants.COLON;
+import static racingcar.utility.Constants.ADVANCEMENT;
 
 public class OutputView {
     private static final OutputView outputView = new OutputView();
@@ -13,5 +19,24 @@ public class OutputView {
 
     public void printExecutionResultMessage(){
         System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printExecutionResult(List<CarDto> carInformationList){
+        for(int i = 0; i < carInformationList.size(); i++){
+            System.out.print(carInformationList.get(i).getCarName() + COLON);
+            printAdvancementCount(carInformationList.get(i).getAdvancementCount());
+            printNewLine();
+        }
+        printNewLine();
+    }
+
+    private void printAdvancementCount(int advancementCount){
+        for(int i = 0; i < advancementCount; i++){
+            System.out.print(ADVANCEMENT);
+        }
+    }
+
+    private void printNewLine(){
+        System.out.println();
     }
 }
