021b3ec95daf907f1e0358ade9ad265bdcd1b3fa
MingyeomKim <mingyum119@naver.com>
2023-11-01 16:10:47 +0900

    feat(Application): Console.close() 호출
    생성된 스캐너를 메모리에서 정상적으로 해제하기 위해 close함수를 호출한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.RaceController;
 
 public class Application {
     public static void main(String[] args) {
         RaceController raceController = new RaceController();
         raceController.startRace();
+        Console.close();
     }
 }
