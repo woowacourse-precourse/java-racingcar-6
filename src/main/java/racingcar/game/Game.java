a1689ca8eb97c723754c5576ee31aad4231aef2a
sudo-dustle <asdf748596123@gmail.com>
2023-11-01 18:41:00 +0900

    feat: 레이싱 게임 실행을 위한 Game 클래스 생성

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -1,2 +1,57 @@
-package racingcar.game;public class Game {
+package racingcar.game;
+
+import racingcar.car.Car;
+import racingcar.car.Cars;
+import racingcar.util.Input;
+import racingcar.util.Output;
+
+import java.util.List;
+
+public class Game {
+    private Cars cars;
+
+    public Game() {
+        this.cars = new Cars();
+    }
+
+    public void doGame() {
+        List<String> carNames = getCarNames();
+
+        cars = makeCars(carNames);
+        moveCars();
+        endGame();
+    }
+
+    private static List<String> getCarNames() {
+        Output.printWaitCarNames();
+        return Input.readCarName();
+    }
+
+    private Cars makeCars(List<String> carNames) {
+        Output.printWaitTryCount();
+
+        Cars cars = new Cars();
+
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.addCars(car);
+        }
+
+        return cars;
+    }
+
+    private void moveCars() {
+        int tryCount = Input.readTryCount();
+        Output.printAnnounce();
+
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveCars();
+            Output.printResult(cars);
+            Output.printSpace();
+        }
+    }
+
+    private void endGame() {
+        Output.printGameWinner(cars.getGameWinner());
+    }
 }
