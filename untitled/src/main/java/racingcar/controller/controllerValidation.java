8d39cfc18359bacbda7d58307e84a03d456ce6ee
seongnam <jseongnam1109@gmail.com>
2023-11-01 16:25:44 +0900

    vewInputCar

---
+++ untitled/src/main/java/racingcar/controller/controllerValidation.java
@@ -0,0 +1,7 @@
+package racingcar.controller;
+
+// ,을 기준으로 모두 5글자 이하인지 체크
+// 중복된 차 이름이 없는지 체크
+
+public class controllerValidation {
+}
---
+++ untitled/src/main/java/racingcar/view/viewInputCar.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class viewInputCar {
+
+    public String viewInputCar() {
+        String carNameData = Console.readline();
+        return carNameData;
+    }
+}
