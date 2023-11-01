7048cd749d79cd01ec92500559a49f18bdf53193
euni <kongeuni1004@gmail.com>
2023-11-01 10:33:41 +0900

    refactor : java 컨벤션에 따라 수정

--- src/main/java/racingcar/CarNameInput.java
+++ src/main/java/racingcar/CarNameInput.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class CarNameInput {
 
-    public HashMap<String,Integer> inputCarName() {
+    public HashMap<String, Integer> inputCarName() {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userAnswer = Console.readLine();
@@ -19,18 +19,18 @@ public class CarNameInput {
 
         String[] user = userAnswer.split(",");
         for (String userName : user) {
-            if(userName.length()>5){
+            if (userName.length() > 5) {
                 throw new IllegalArgumentException();
             }
         }
         return user;
     }
 
-    private HashMap<String,Integer> arrayToHashMap(String[] user){
+    private HashMap<String, Integer> arrayToHashMap(String[] user) {
         HashMap<String, Integer> userResult = new HashMap<>();
 
-        for(String key : user){
-            userResult.put(key,0);
+        for (String key : user) {
+            userResult.put(key, 0);
         }
 
         return userResult;
--- src/test/java/study/StudyTest.java
+++ src/test/java/study/StudyTest.java
@@ -3,32 +3,30 @@ package study;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import racingcar.AttemptsInput;
 import racingcar.CarNameInput;
 
 public class StudyTest {
 
     @Test
-    public void 유저이름_입력시_오류(){
+    public void 유저이름_입력시_오류() {
         CarNameInput carNameInput = new CarNameInput();
         String userNameInput = "apple,happycat,wind";
         assertThrows(IllegalArgumentException.class,
-            ()->carNameInput.detectErrorInUserInput(userNameInput));
+            () -> carNameInput.detectErrorInUserInput(userNameInput));
     }
 
     @Test
-    public  void 시도횟수_변환시_오류(){
+    public void 시도횟수_변환시_오류() {
         AttemptsInput attemptsInput = new AttemptsInput();
         String number = "숫자";
         assertThrows(IllegalArgumentException.class,
-            ()->attemptsInput.isNumber(number));
+            () -> attemptsInput.isNumber(number));
     }
 
     @Test
-    public void 시도횟수_변환_확인(){
+    public void 시도횟수_변환_확인() {
         AttemptsInput attemptsInput = new AttemptsInput();
         String number = "3";
         assertThat(attemptsInput.isNumber(number)).isEqualTo(3);
@@ -45,12 +43,11 @@ public class StudyTest {
     @Test
     public void testInvalidNumber() {
         AttemptsInput parser = new AttemptsInput();
-        String userInput = "abc"; // Invalid input
+        String userInput = "abc";
         try {
             parser.isNumber(userInput);
             fail("예외 발생 안함");
         } catch (IllegalArgumentException e) {
-            // Expected IllegalArgumentException
         }
     }
 }
