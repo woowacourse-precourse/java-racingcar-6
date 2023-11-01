c64da38cc8781bc17719ac2e809077cba838398a
주은 <00-10-21@naver.com>
2023-11-01 22:25:49 +0900

    feat: Validate attempts input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,12 @@ public class Application {
         }
     }
 
+    private static void validateAttempts(int attempts) {
+        if (attempts < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = scanner.nextLine();
@@ -18,5 +24,6 @@ public class Application {
 
         System.out.println("시도할 회수는 몇회인가요?");
         int attempts = scanner.nextInt();
+        validateAttempts(attempts);
     }
 }
