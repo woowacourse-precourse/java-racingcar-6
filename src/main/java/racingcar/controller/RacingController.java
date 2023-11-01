32d4ed6a0bd3dd1614ca333f7406aada54a9f571
Laterre <laterre39@naver.com>
2023-11-01 18:16:17 +0900

    feat: 경주 할 자동차의 입력을 담당할 enterCarName() 메서드 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,9 +1,13 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import racingcar.service.RacingService;
 import racingcar.view.RacingView;
 
 public class RacingController {
     public static void run() {
         RacingView.startGameMessage();
+        HashMap<String, String> car = RacingService.enterCarName(Console.readLine());
     }
 }
--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,4 +1,14 @@
 package racingcar.service;
 
+import java.util.HashMap;
+
 public class RacingService {
+    public static HashMap<String, String> enterCarName(String args) {
+        HashMap<String, String> cars = new HashMap<>();
+        String[] arr = args.split(",");
+        for (String carName : arr) {
+            cars.put(carName, "");
+        }
+        return cars;
+    }
 }
