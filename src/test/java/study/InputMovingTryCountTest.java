451b69eaec9aabf52121bb10ff319b0c480d4903
seondays <agseon9@gmail.com>
2023-11-01 23:36:27 +0900

    test: 오입력된 부분 제거

--- src/test/java/study/InputMovingTryCountTest.java
+++ src/test/java/study/InputMovingTryCountTest.java
@@ -27,10 +27,9 @@ public class InputMovingTryCountTest {
 
     @ParameterizedTest
     @DisplayName("입력받은 시도할 횟수가 1에서 10 사이가 아닌 경우에 예외 발생")
-    @ValueSource(strings = {"0", "11성"})
+    @ValueSource(strings = {"0", "11"})
     void 범위를_벗어나면_예외_발생(String input) {
         assertThatThrownBy(() -> validator.validateInputMovingTryCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
