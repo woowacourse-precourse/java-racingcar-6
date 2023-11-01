cc1fc1a8d42bd3dc3a18a2464fdabec76ef0eaa3
Hepheir <hepheir@gmail.com>
2023-11-01 18:52:14 +0900

    feat: Application.main() 에서 사용할 Game 구현체로 RacingCarGame을 사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = null; // TODO: Replace null with an actual game
+        Game game = new RacingCarGame();
         game.start();
     }
 }
