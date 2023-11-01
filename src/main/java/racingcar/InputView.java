ba799d0cb77bb93ea91bfcd880a38f1aaf611bf4
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-01 21:27:01 +0900

    view 파일에 대한 문제로 삭제하고 밖으로 나 내보냈다.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,4 +1,4 @@
-package racingcar.view;
+package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,3 +1,4 @@
+package racingcar;
 public class OutputView {
     public static void printInputCarNameMessage(){
         System.out.println(InputView.INPUT_CAR_NAME_MESSAGE.print());
