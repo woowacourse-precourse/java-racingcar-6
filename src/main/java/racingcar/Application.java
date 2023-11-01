422139dd53f6b9a17c25ea80c032d22460cc6e8c
hololol-101 <shrkddms1010@naver.com>
2023-11-01 21:43:23 +0900

    [feat] 메인 함수 구현 - 자동차 이름 받기 - 횟수 받기 - 레이싱 경기 실행하기 - 경기로부터 승자를 받아와 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingContest racingContest;
+        List<String> winners;
+        String result = "죄종 우승자 : ";
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String players = Console.readLine();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int round = Integer.parseInt(Console.readLine());
+
+        racingContest = new RacingContest(round);
+        for(String racingCar :players.split(",")){
+            racingContest.insertCar(new RacingCar(racingCar));
+        }
+
+        racingContest.playGame();
+        winners = racingContest.returnWinner();
+        for(String winner:winners){
+            result = result.concat(winner)
+                    .concat(", ");
+        }
+        result = result.substring(result.length()-2);
+        System.out.println(result);
     }
 }
