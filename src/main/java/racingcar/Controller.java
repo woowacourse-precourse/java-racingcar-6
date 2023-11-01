d2631224c497883dac2b598ad35e99affdb97bad
LeeJuWon <woen1016@gmail.com>
2023-11-01 13:40:26 +0900

    method 생성 - 2

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -2,13 +2,14 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 class Game {
-    private static final int NUMBER_START = 0;
-    private static final int NUMBER_END = 9;
+    private static final int MAX_RANDOM_NUMBER = 0;
+    private static final int MIN_RANDOM_NUMBER = 9;
     private static final int MORE_THAN_FOUR = 4;
     static ArrayList<String> carsNames = new ArrayList<String>();
     private static final String GET_CARS_NAMES_MESSAGE = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
@@ -23,33 +24,68 @@ class Game {
     public static void init(){
         String carsNamesStr = getCarsNames();
         int playNumber = getPlayNumber();
-        divideString(carsNamesStr);
+        stringToArrayList(carsNamesStr);
 
         gamePlay(carsNames, playNumber);
     }
 
     private static void gamePlay(ArrayList<String> carsNames, int playNumber) {
         System.out.println(PLAY_RESULT_MESSAGE);
-        ArrayList<Integer> countCarsMove = new ArrayList<Integer>(carsNames.size());
+        ArrayList<Integer> countCarsMove = new ArrayList<Integer>(Collections.nCopies(carsNames.size(), 0));
+        int endGameNumber = playNumber;
+        while(endGameNumber != 0) {
+            for(String car : carsNames){
+                int randomNumber = getRandomNumber();
+                if(randomNumber >= MORE_THAN_FOUR){
+                    int index = carsNames.indexOf(car);
+                    countCarsMove.set(index, countCarsMove.get(index) + 1);
+                    endGameNumber--;
+                    System.out.print(car + " : ");
+                    System.out.println("-".repeat(countCarsMove.get(index)));
+                    System.out.println();
+                }
+            }
+        }
+        printWinner(countCarsMove, playNumber);
+    }
 
-       for(String car : carsNames){
-           int randomNumber = getRandomNumber();
-           if()
-       }
+    private static void printWinner(ArrayList<Integer> countCarsMove, int playNumber) {
+        int winnerCount = Collections.frequency(countCarsMove, playNumber);
+        if(winnerCount == 1){
+            printWinnerAlone(countCarsMove.indexOf(playNumber));
+        }
+        else if(winnerCount > 1){
+            ArrayList<Integer> winnersIndexes = new ArrayList<>();
+            for(int i = 0; i < countCarsMove.size(); i++){
+                if(countCarsMove.get(i) == playNumber){
+                    winnersIndexes.add(i);
+                }
+            }
+            printWinnerSeveral(winnersIndexes);
+        }
+    }
 
-
+    private static void printWinnerAlone(int index) {
+        System.out.println(FINAL_WINNERS_MESSAGE + carsNames.get(index));
+    }
+    private static void printWinnerSeveral(ArrayList<Integer> winnersIndexes) {
+        ArrayList<String> winners = new ArrayList<>();
+        for(int index : winnersIndexes){
+            winners.add(carsNames.get(index));
+        }
+        System.out.print(FINAL_WINNERS_MESSAGE + String.join(", ", winners));
 
     }
 
     private static int getRandomNumber() {
-        return pickNumberInRange(NUMBER_START, NUMBER_END);
+        return pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 
     private static String getCarsNames() {
         System.out.println(GET_CARS_NAMES_MESSAGE);
         String inputCarsNames = readLine();
         //checkException(inputCarsNames);
-        carsNames = divideString(inputCarsNames);
+        carsNames = stringToArrayList(inputCarsNames);
 
         return inputCarsNames;
     }
@@ -62,8 +98,7 @@ class Game {
         return Integer.parseInt(inputPlayNumber);
     }
 
-    private static ArrayList<String> divideString(String carStr) {
-
+    private static ArrayList<String> stringToArrayList(String carStr) {
         return new ArrayList<>(Arrays.asList(carStr.split(",")));
     }
 
