b7a324d2c33c9db2f316a866db39e3edadcf0056
Min <mijien0179@gmail.com>
2023-11-01 12:41:48 +0900

    refactor : input 모델 사용하도록 전환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,55 +1,9 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.HashMap;
-import java.util.Map;
-import racingcar.controller.Input;
-
 public class Application {
     public static void main(String[] args) {
-        var gameStartData = Input.inputGameInfo();
+        Game.run();
 
-        Map<String, StringBuilder> racer = new HashMap<>();
-        for(String name: gameStartData.carName){
-            racer.put(name, new StringBuilder());
-        }
-
-        final int LEAST_POWER = 4;
-
-        for(int i = 0 ; i < gameStartData.loopCount;++i){
-            for(StringBuilder sb : racer.values()){
-                int forwardPower = Randoms.pickNumberInRange(0, 9);
-                if(forwardPower < LEAST_POWER) continue;
-                sb.append('-');
-            }
-
-            // print result after step
-            for(String key: racer.keySet()){
-                System.out.println(key + " : " + racer.get(key).toString());
-            }
-            System.out.println();
-        }
-
-        int maxLength = 0;
-        for(var sb : racer.values()){
-            if(maxLength < sb.length()){
-                maxLength = sb.length();
-            }
-        }
-
-
-        int finalMaxLength = maxLength;
-        var winner = racer.keySet()
-                .stream()
-                .filter(key -> finalMaxLength == racer.get(key).length())
-                .toList();
-
-        var sb = new StringBuilder();
-
-        sb.append("최종 우승자 : ");
-        sb.append(String.join(", ", winner));
-
-        System.out.println(sb);
     }
 
 
--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,7 +1,23 @@
 package racingcar.view;
 
+import java.util.Map;
+
 public class View {
     public static void show(Description desc){
-        System.out.println(desc.str);
+        show(desc.str);
     }
+
+    public static void show(String str){
+        System.out.println(str);
+    }
+
+    public static void showCurrentState(Map<String, StringBuilder> racer){
+        for(String key: racer.keySet()){
+            System.out.println(key + " : " + racer.get(key).toString());
+        }
+        System.out.println();
+
+    }
+
+
 }
