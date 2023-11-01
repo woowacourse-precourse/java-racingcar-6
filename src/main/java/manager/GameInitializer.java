503411742839e5a343e094a91681ae307cf513dc
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-01 10:26:58 +0900

    feat : inputCarName 구현
    GameInitializer.inputCarName 구현

---
+++ src/main/java/manager/GameInitializer.java
@@ -0,0 +1,39 @@
+package manager;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import custom_object.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameInitializer {
+    private final int CAR_NAME_MAXIMUM_LENGTH = 5;
+
+    public List<Car> inputCarName() {
+        List<Car> carList = new ArrayList<Car>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarName = Console.readLine();
+
+        String[] inputCarNameSplit;
+
+        try {
+            inputCarNameSplit = inputCarName.split(",");
+        } catch(Exception exception) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < inputCarNameSplit.length; i++) {
+            if (inputCarNameSplit[i].length() > CAR_NAME_MAXIMUM_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        for (int i = 0; i < inputCarNameSplit.length; i++) {
+            carList.add(new Car(inputCarNameSplit[i]));
+        }
+
+        return carList;
+    }
+}
