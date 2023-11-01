cd48bab08223ffbfa257579e7628474d5782d785
sgo722 <sgo722@naver.com>
2023-11-01 17:07:24 +0900

    refactor : 난수 생성과정 매직넘버 제거

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -5,8 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
 
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+
 
     public static int getRandomNumber(){
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM,MAX_RANDOM_NUM);
     }
 }
