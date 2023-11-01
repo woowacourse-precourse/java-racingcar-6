6be04acd2db4b0f0b2aefc459ab8647aba6f04fc
plan11plan <tjtfy7028@gmail.com>
2023-11-01 18:51:17 +0900

    test:[create][Car]-생성자

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+    @Test
+    @DisplayName("자동차 생성하기")
+    void newCar(){
+        Car car = new Car("car1");
+        assertThat(car.getName()).isEqualTo("car1");
+
+    }
+}
