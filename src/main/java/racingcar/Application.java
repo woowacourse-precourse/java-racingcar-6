fdfe0976cbc338b2e1d48924c657ab0c4ab50897
chooh <chocomilk66@naver.com>
2023-11-01 19:37:27 +0900

    feat: 자동차의 이름을 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> names = Arrays.asList(Console.readLine().split(","));
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class Car {
+
+    //이름
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
