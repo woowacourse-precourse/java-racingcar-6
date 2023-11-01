fbaa4cfc2da0755dae94147111c854a30e0f9c4d
Sim-GyuBin <skb0606@gmail.com>
2023-11-01 21:04:07 +0900

    fix: 각 멤버 변수 getter 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,7 +8,17 @@ public class Car {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
     public void move() {
         position++;
     }
+
+
 }
