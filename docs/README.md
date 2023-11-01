ee69d0c8f1783e1713ccf2b211c479c3eb03bc96
Choi-Moori <anflwkd@gmail.com>
2023-11-01 17:38:06 +0900

    Feat(goCar) goCar 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -9,7 +9,7 @@
 - [O] 몇 회 이동할 것인지 입력한다. - NumberGenerator#goChance()
     - [O]숫자인가? 예외처리 진행.   - NumberGenerator#validateGameChanceNumbers()
 - [O] 0에서 9 사이의 임의의 수를 생성한다. - NumberGenerator#createRandomNumbers()
-    - [ ] 숫자가 4 이상인 경우 전진 - ActionCars#goCar()
+    - [O] 숫자가 4 이상인 경우 전진 - ActionCars#goCar()
 
 - [ ] 입력에 대한 출력.
     - 차 이름 입력 안내문 - InputView#printCarNamesInformation()
--- src/main/java/racingcar/domain/ActionCars.java
+++ src/main/java/racingcar/domain/ActionCars.java
@@ -1,5 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.model.RacingGame;
+
 public class ActionCars {
-    
+
+    RacingGame racinggame = new RacingGame();
+
+    public void goCar(int carNum){
+        List<String> countResult = new ArrayList<String>();
+        countResult = racinggame.getCountResult();
+        String change = countResult.get(carNum);
+        change += "-";
+        countResult.set(carNum, change); 
+    }   
 }
