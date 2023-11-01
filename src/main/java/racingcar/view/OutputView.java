2760bb023b83188987a4723f713ce1ac9064bf35
이두현 <ldh9411@hanmail.net>
2023-11-01 21:30:43 +0900

    Feat: view 추가
    - input & output을 위한 view 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,42 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public class OutputView {
+
+    public static void printResultMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printCarScore(List<Car> cars) {
+
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            printCarPosition(car);
+        }
+
+    }
+
+    public static void printCarPosition(Car car) {
+        System.out.println("car name&position : " + car.getName() + car.getPosition());
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+
+    public static void printSoloWin(String winner) {
+        System.out.println("최종 우승자 : " + winner);
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+        for (String winner : winners) {
+            System.out.print(winner);
+            System.out.println(", ");
+        }
+    }
+
 }
