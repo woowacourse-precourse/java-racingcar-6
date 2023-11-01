f14fcae3266ec559a38dde362467e800ba87daa1
soyoungxxx <otherplet@gmail.com>
2023-11-01 20:09:43 +0900

    feat(racingcar): 일급컬렉션 Number 생성

--- src/main/java/racingcar/domain/Number.java
+++ src/main/java/racingcar/domain/Number.java
@@ -1,5 +1,24 @@
 package racingcar.domain;
 
-public class InformationStorage {
+public class Number {
+    int number;
+    public Number(String num) {
+        validateSize(num);
+        validateNumber(num);
+        this.number = Integer.parseInt(num);
+    }
 
+    private void validateSize(String num) {
+        if (num.length() > 2) {
+            throw new IllegalArgumentException("0~9 사이 숫자를 입력해주세요!");
+        }
+    }
+
+    private void validateNumber(String num) {
+        try {
+            int number = Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다!");
+        }
+    }
 }
