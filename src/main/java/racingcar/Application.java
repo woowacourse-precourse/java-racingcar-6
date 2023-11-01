7e07e5028c4e3b35341787c94e6199d5854249fb
Bellwin98 <jongseunghan@naver.com>
2023-11-01 13:08:02 +0900

    Remove(Application): 불필요 import문 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,6 @@
 package racingcar;
 
-import racingcar.domain.Car;
-import camp.nextstep.edu.missionutils.Console;
-import racingcar.domain.InputValidation;
 import racingcar.domain.Race;
-
-import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
