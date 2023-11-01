806a42f563d3c0844666b77066027d198e1677da
plan11plan <tjtfy7028@gmail.com>
2023-11-01 22:56:40 +0900

    style:[update][main]: 띄어쓰기 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,8 +9,10 @@ public class Application {
         // TODO: 프로그램 구현
         List<Car> cars = InputView.inputCars();
         int rounds = InputView.inputRounds();
+
         RacingGame racingGame = new RacingGame(cars, rounds);
         racingGame.start();
+
         List<Car> winners = racingGame.findWinners();
         OutputView.printWinners(winners);
 
