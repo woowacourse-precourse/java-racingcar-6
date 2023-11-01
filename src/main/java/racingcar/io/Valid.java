4b9acbe0e1c8425f76a352866ddeffeaba441c11
tangba484 <tangba484@naver.com>
2023-11-01 18:59:11 +0900

    Feat: Valid 클래스 작성 및 자동차 이름 유효성 검증 메서드 추가

--- src/main/java/racingcar/io/Valid.java
+++ src/main/java/racingcar/io/Valid.java
@@ -1,2 +1,17 @@
-package racingcar.io;public class Valid {
+package racingcar.io;
+
+import java.util.*;
+
+public class Valid {
+    public static void nameInput(String input) {
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            if (carName.trim().isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 공백으로 입력 할 수 없습니다.");
+            }
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("입력이 유효하지 않습니다. 자동차 이름은 5자 이하이어야 합니다.");
+            }
+        }
+    }
 }
