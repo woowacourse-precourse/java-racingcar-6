42949f7da56527fbdc109208181356c39d46dc03
potatomoong <star901210@hanmail.net>
2023-11-01 18:05:53 +0900

    Feat: 경기결과화면-자동차이름출력하기

--- docs/README.md
+++ docs/README.md
@@ -8,5 +8,6 @@
     - [V]생성된 숫자를 각 자동차에 배치하고 배치된 숫자가 4이상이면 전진 JudgeMovement#moveForward()
 4. []결과 출력하기 Result
     - []경기 결과 출력하기 Result#printRapResult()
+    - []전진한 칸수 출력하기 Result#printCarPosition()
     - []우승자 선정하기 Result#pickWinners()
     - []우승자 출력하기 Result#printWinner()
--- src/main/java/racingcar/domain/Result.java
+++ src/main/java/racingcar/domain/Result.java
@@ -1,8 +1,15 @@
 package racingcar.domain;
 
 public class Result {
-    public void printRapResult() {
+    public void printRapResult(String[] carNames, int[] positions) {
+        for (int i = 0; i < carNames.length; i++) {
+            System.out.print(carNames[i] + " : ");
+            printCarPosition(positions[i]);
+        }
+        System.out.println();
+    }
 
+    private void printCarPosition(int position) {
     }
 
     public void pickWinners() {
