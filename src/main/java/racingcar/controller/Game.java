14e8a89585c6a011852b062b4a73907213df538f
이재현 <fanta4715@naver.com>
2023-11-01 20:52:42 +0900

    style: 불필요 로직 제거
    불필요 로직 제거 및 public 임시 변환(테스트위함)

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.util.Validator;
-import racingcar.dto.CarDto;
-import racingcar.util.CarDtoConverter;
 import racingcar.model.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -20,7 +18,6 @@ public class Game {
         inputView = new InputView();
     }
 
-    //start() 함수 구현
     public void start() {
         String carListString = inputView.inputCarNames();
         cars = new Cars(carListString);
@@ -31,12 +28,11 @@ public class Game {
         Race race = new Race(cars, outputView);
         race.playRaceAllRounds(roundNumber);
 
-        List<CarDto> carDtoList = CarDtoConverter.convertCarsToCarDtoList(cars);
         List<String> winners = findWinners(cars);
         outputView.printWinner(winners);
     }
 
-    private List<String> findWinners(Cars cars) {
+    public List<String> findWinners(Cars cars) {
         List<String> winners = new ArrayList<>();
         int max = findTopDistance(cars);
         for (int i = 0; i < cars.size(); i++) {
