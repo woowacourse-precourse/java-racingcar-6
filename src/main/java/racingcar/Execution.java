1097f1522861b4d1417e2960aa879b468e86d02a
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-01 13:45:25 +0900

    fix: move generating input logic for testing of the execution class

--- src/main/java/racingcar/Execution.java
+++ src/main/java/racingcar/Execution.java
@@ -2,19 +2,12 @@ package racingcar;
 
 public class Execution {
     public static int count;
-    IOHandler ioHandler = new IOHandler();
 
-    public Execution() {
-        String input = generate();
-        
+    public Execution(String input) {
         validateFormat(input);
         validatePositive(input);
 
         count = Integer.parseInt(input);
-    }
-
-    private String generate() {
-        return ioHandler.readConsoleInputWithMessage("시도할 회수는 몇회인가요?");
     }
 
     private void validateFormat(String input) {
