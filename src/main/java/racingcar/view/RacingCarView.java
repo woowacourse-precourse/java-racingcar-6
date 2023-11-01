5ff4d1540408fa8a61bb44f7fa9cbae3a13ed629
Jmmk <jmk525@gmail.com>
2023-11-01 14:25:59 +0900

    feat : 시도횟수 입력 메서드 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import racingcar.constants.GameMessage;
+import racingcar.dto.input.NumberOfAttemptsDto;
 import racingcar.dto.input.RacingCarsDto;
 import racingcar.io.CommandLineReader;
 import racingcar.io.CommandLineWriter;
@@ -18,5 +19,10 @@ public class RacingCarView {
         commandLineWriter.writeLine(GameMessage.INPUT_CAR_NAME);
         String input = commandLineReader.readLine();
         return new RacingCarsDto(input);
+    }
+    public NumberOfAttemptsDto inputNumberOfAttempts() {
+        commandLineWriter.writeLine(GameMessage.INPUT_NUMBER_OF_ATTEMPTS);
+        String input = commandLineReader.readLine();
+        return new NumberOfAttemptsDto(input);
     }
 }
