1bf7d69654f9606dbbe6606923d00168f3db0d78
Hyyena <sjtxm0320@gmail.com>
2023-11-01 22:11:15 +0900

    ✨ feat: 출력 기능

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Output {
+
+    public static void printRaceState(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winner = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자: " + winner);
+    }
+}
