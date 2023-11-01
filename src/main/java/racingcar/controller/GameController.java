4a4cd5b84a68798e8bb134c983c4bed8178c9092
yebin <dev.devin.kr@gmail.com>
2023-11-01 17:07:11 +0900

    feat(game-controller): create car object with input value as car name

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,9 +1,14 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
 import racingcar.model.InputUtil;
 import racingcar.view.RaceStartView;
 
 public class GameController {
+
+    private List<Car> carObjects;
 
     public void run() {
         RaceStartView raceStartView = new RaceStartView();
@@ -12,5 +17,10 @@ public class GameController {
         String[] carNameStrings = InputUtil.splitCarName(carName);
 
         String raceLap = raceStartView.getRaceLap();
+
+        carObjects = new ArrayList<>();
+        for (String carNameStr : carNameStrings) {
+            carObjects.add(new Car(carNameStr));
+        }
     }
 }
