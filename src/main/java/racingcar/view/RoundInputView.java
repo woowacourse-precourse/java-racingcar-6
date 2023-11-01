61db2a244e6eb31d8ebd459d290078526b419ad8
Chaeros <chpark6851@koreatech.ac.kr>
2023-11-01 19:51:26 +0900

    test: 게임 횟수 입력 View 생성
    사용자로부터 게임 진행 횟수를 입력받고 검증 후, 올바른 입력이면 정수를
    반환한다.

--- src/main/java/racingcar/view/RoundInputView.java
+++ src/main/java/racingcar/view/RoundInputView.java
@@ -5,19 +5,19 @@ import racingcar.errormessage.InputError;
 
 public class RoundInputView {
 
-    private static final String INPUT_MESSAGE = "시도할 회수를 몇회인가요?\n";
+    private static final String INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
 
-    public int generateInputList() {
+    public int generateRoundNumber() {
         System.out.print(INPUT_MESSAGE);
         String number = Console.readLine();
-        validateInputIsNumber(number);
         validateEmptyString(number);
+        validateInputIsNumber(number);
         return Integer.parseInt(number);
     }
 
-    private void validateInputIsNumber(String numbers) {
-        for (int i = 0; i < numbers.length(); ++i) {
-            if (numbers.charAt(i) < 48 || numbers.charAt(i) > 57) {
+    private void validateInputIsNumber(String number) {
+        for (int i = 0; i < number.length(); ++i) {
+            if (number.charAt(i) < 48 || number.charAt(i) > 57) {
                 throw new IllegalArgumentException(InputError.INPUT_NOT_NUMBER_ERROR_MESSAGE);
             }
         }
