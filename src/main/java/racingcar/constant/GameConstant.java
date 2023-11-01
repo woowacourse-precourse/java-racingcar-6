6c3a62c6819db906e45951abd8f3fd609cdffd1d
soddong <aledjdygus9300@gmail.com>
2023-11-01 17:11:56 +0900

    style : 전진 조건 경계갑 상수화

--- src/main/java/racingcar/constant/GameConstant.java
+++ src/main/java/racingcar/constant/GameConstant.java
@@ -3,6 +3,7 @@ package racingcar.constant;
 public class GameConstant {
     public static final int START_OF_RANDOM_GENERATOR_RANGE = 0;
     public static final int END_OF_RANDOM_GENERATOR_RANGE = 9;
+    public static final int MOVING_FORWARD_THRESHOLD = 4;
     public static final int INITIAL_START_ROUND = 0;
     public static final int INITIAL_CAR_INDEX = 0;
     public static final int UNIT_OF_MOVEMENT = 1;
--- src/main/java/racingcar/domain/game/GameCondition.java
+++ src/main/java/racingcar/domain/game/GameCondition.java
@@ -1,6 +1,7 @@
 package racingcar.domain.game;
 
 import static racingcar.constant.GameConstant.END_OF_RANDOM_GENERATOR_RANGE;
+import static racingcar.constant.GameConstant.MOVING_FORWARD_THRESHOLD;
 import static racingcar.constant.GameConstant.START_OF_RANDOM_GENERATOR_RANGE;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -8,6 +9,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class GameCondition {
 
     public static boolean hasDriveCondition() {
-        return Randoms.pickNumberInRange(START_OF_RANDOM_GENERATOR_RANGE, END_OF_RANDOM_GENERATOR_RANGE) >= 4;
+        return Randoms.pickNumberInRange(START_OF_RANDOM_GENERATOR_RANGE, END_OF_RANDOM_GENERATOR_RANGE)
+                >= MOVING_FORWARD_THRESHOLD;
     }
 }
