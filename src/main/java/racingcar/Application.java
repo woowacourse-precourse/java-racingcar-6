aed644abeae81d47f39b7d641dd64bdf3c9f2021
Carrot_hyeong <rjgjfl90@naver.com>
2023-11-01 16:50:04 +0900

    refactor : 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,17 @@
 package racingcar;
 
-import java.util.Arrays;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carInput = camp.nextstep.edu.missionutils.Console.readLine();
+        String carInput = Console.readLine();
         String[] carNames = carInput.split(",");
 
         System.out.println("시도할 회수는 몇회인가요?");
         int gameCnt = Integer.parseInt(Console.readLine());
 
-
+        Game game = new Game(carNames, gameCnt);
+        game.race();
     }
 }
