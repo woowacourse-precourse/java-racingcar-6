fa41709cf84f131c87fc7f5f9fe3afc11bb11f1c
wschoe <chldntjr0425@naver.com>
2023-11-01 22:57:10 +0900

    feat: add car move test code

---
+++ src/test/java/racingcar/car/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar.car;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    public void Car는_난수_값에_따라_전진한다() {
+        // given
+        RandomNumberGenerator forwardGenerator = ()->4;
+        RandomNumberGenerator stayGenerator = ()->3;
+        Car forwardCar = new Car("test1", forwardGenerator);
+        Car stayCar = new Car("test2", stayGenerator);
+
+        // when
+        forwardCar.move();
+        stayCar.move();
+
+        // then
+        assertThat(forwardCar.getLocation()).isEqualTo(1);
+        assertThat(stayCar.getLocation()).isEqualTo(0);
+    }
+}
