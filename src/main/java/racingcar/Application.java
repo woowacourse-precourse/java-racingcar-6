99200bfe84553ae3654b2372aa82af5f5a8dc711
ljkhyeong <jolri24@gmail.com>
2023-11-01 22:32:31 +0900

    refactor: 모듈화에 따른 Main, RacingGame 클래스 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,18 @@
 package racingcar;
 
+import racingcar.game.RacingGame;
+import racingcar.provider.RacingInputProvider;
+import racingcar.provider.RacingOutputProvider;
+import racingcar.service.RacingService;
+
 public class Application {
-    public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
-        racingGame.start();
-    }
+	public static void main(String[] args) {
+
+		RacingInputProvider racingInputProvider = new RacingInputProvider();
+		RacingOutputProvider racingOutputProvider = new RacingOutputProvider();
+		RacingService racingService = new RacingService();
+
+		RacingGame racingGame = new RacingGame(racingInputProvider, racingOutputProvider, racingService);
+		racingGame.start();
+	}
 }
--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -1,89 +1,40 @@
-package racingcar;
+package racingcar.game;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Car;
+import racingcar.provider.RacingInputProvider;
+import racingcar.provider.RacingOutputProvider;
+import racingcar.service.RacingService;
 
 public class RacingGame {
 
 	private static final int MAX_CAR_NAME_LENGTH = 5;
 
+	private final RacingInputProvider racingInputProvider;
+	private final RacingOutputProvider racingOutputProvider;
+	private final RacingService racingService;
+
+	public RacingGame(RacingInputProvider racingInputProvider,
+		RacingOutputProvider racingOutputProvider,
+		RacingService racingService) {
+		this.racingInputProvider = racingInputProvider;
+		this.racingOutputProvider = racingOutputProvider;
+		this.racingService = racingService;
+	}
+
 	public void start() {
-		List<Car> racingCars = getRacingCars();
-		int tryCount = getInputTryCount();
+		List<Car> racingCars = racingInputProvider.getRacingCars();
+		int tryCount = racingInputProvider.getInputTryCount();
 
-		System.out.println("실행 결과");
+		racingOutputProvider.printRacingStart();
 		for (int i = 0; i < tryCount; i++) {
-			for (Car racingCar : racingCars) {
-				racingCar.move();
-			}
-			printRacing(racingCars);
+			racingService.racing(racingCars);
+			racingOutputProvider.printRacing(racingCars);
 		}
 
-		List<Car> winners = getWinners(racingCars);
-		printWinners(winners);
-	}
-
-	private List<Car> getRacingCars() {
-		String carNames = getInputCarNames();
-
-		return Arrays.stream(carNames.split(","))
-			.map(Car::new)
-			.collect(Collectors.toList());
-	}
-
-	private String getInputCarNames() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		String carNames = Console.readLine();
-
-		return carNames;
-	}
-
-	private int getInputTryCount() {
-		System.out.println("시도할 회수는 몇회인가요?");
-		int tryCount = Integer.parseInt(Console.readLine());
-		System.out.println();
-
-		return tryCount;
-	}
-
-	private void printRacing(List<Car> cars) {
-		StringBuffer stringBuffer = new StringBuffer();
-
-		for (Car car : cars) {
-			System.out.println(car.getName() + " : " + carProgress(car.getPosition()));
-		}
-		System.out.println();
-	}
-
-	private String carProgress(int position) {
-		return "-".repeat(position);
-	}
-
-	private List<Car> getWinners(List<Car> cars) {
-		List<Car> winners = new ArrayList<>();
-
-		int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
-
-		for (Car car : cars) {
-			if (car.getPosition() == maxPosition) {
-				winners.add(car);
-			}
-		}
-
-		return winners;
-	}
-
-	private void printWinners(List<Car> winners) {
-		String winnerNames = winners.stream()
-			.map(Car::getName)
-			.collect(Collectors.joining(","));
-
-		System.out.println("최종 우승자 : " + winnerNames);
+		List<Car> winners = racingService.getWinners(racingCars);
+		racingOutputProvider.printWinners(winners);
 	}
 
 }
