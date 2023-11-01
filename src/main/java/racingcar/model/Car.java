4c41971f0848fa77fa6d52be72ded29424715df6
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:55:48 +0900

    feat(Car): 속성 및 생성자 정의
    Car 클래스는 차량을 표현하기 위해 Name 과 Distance 객체를 가진다.
    
    속성
     - Name name
     - Distance distance
    
    생성자
     - (Name): 주어진 이름으로 Car를 생성합니다. 거리는 0으로 초기화한다.
    만약 주어진 이름이 널인 경우 IllegalArgumentException을 발생시킨다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,17 @@
 package racingcar.model;
 
 public class Car {
+
+    private Name name;
+    private Distance distance;
+
+    public Car(Name name) {
+
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null일 수 없습니다.");
+        }
+
+        this.name = name;
+        this.distance = new Distance();
+    }
 }
