598256ffe2207357004e1d5830d5e7de74edc688
kijen723 <kij2646@gmail.com>
2023-11-01 18:14:00 +0900

    feat(CalculateCarsPositions): TODO7 done

--- src/main/java/racingcar/CalculateCarsPositions.java
+++ src/main/java/racingcar/CalculateCarsPositions.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +10,16 @@ public class CalculateCarsPositions {
         List<Integer> carsPositions = new ArrayList<>();
         for (int i = 0; i < cars.size(); i++) {
             carsPositions.add(0);
+        }
+    }
+
+    public static void calculateCarsProgresses(List<Integer> carsPositions) {
+        for (int i = 0; i < carsPositions.size(); i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            if (randomNumber >= 4) {
+                carsPositions.set(i, carsPositions.get(i) + 1);
+            }
         }
     }
 }
