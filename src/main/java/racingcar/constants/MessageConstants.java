a111e335a76def54b16ab5c1d0b44ca834bf8578
dhl1924 <dhl1924@naver.com>
2023-11-01 16:49:54 +0900

    add: MessageConstants for CarNameValidator class

--- src/main/java/racingcar/constants/MessageConstants.java
+++ src/main/java/racingcar/constants/MessageConstants.java
@@ -5,6 +5,7 @@ public class MessageConstants {
     public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
     public static final String INVALID_CAR_NAME_FOR_LENGTH = "5자리 이하로만 입력해주세요.";
     public static final String INVALID_CAR_NAME_FOR_BLANK = "공백은 포함할 수 없습니다.";
+    public static final String INVALID_CAR_NAME_FOR_NULL = "널(null)은 포함할 수 없습니다";
     public static final String INVALID_CAR_NAME_FOR_DUPLICATE_NAME = "이름은 중복될 수 없습니다.";
     public static final String INVALID_TRY_COUNT = "1회 이상 입력해주세요.";
     public static final String INVALID_NUMBER_FORMAT = "숫자를 입력해 주세요";
