92c763a9457a344e5df9b1521760ea7a685b7311
songyi <song21677@naver.com>
2023-11-01 20:43:19 +0900

    refactor: 자동차 이름 개수 검사 Cars 클래스에서 수행하도록 변경 기능 목록 수정 테스트 코드 작성 이름 변경 및 최소 개수 상수화

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -3,8 +3,18 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarsTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = {"pobi", "woni,"})
+	public void 이름_개수_테스트(String carName) {
+		assertThatThrownBy(() ->  new Cars(carName))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("이상이어야 합니다.");
+	}
 
 	@Test
 	public void 이름_중복_테스트() {
