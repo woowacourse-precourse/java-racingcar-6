7ff072df4ee0c78d8d59ac54fadcda2b34b7fcb4
simjaeyoun <spqjfgogo@gmail.com>
2023-11-01 18:19:23 +0900

    test: '경주 차 전진 테스트'

--- src/test/java/service/CarServiceTest.java
+++ src/test/java/service/CarServiceTest.java
@@ -25,6 +25,37 @@ class CarServiceTest {
 
         Assertions.assertThat(cars).containsExactly(new Car("json"),new Car("jade"),new Car("lily"));
     }
+    @Test
+    void 랜덤값_입력받으면_move수행(){
+        String carNamesWithComma ="json,jade,lily";
+        carService.saveCars(InputView.splitNames(carNamesWithComma));
+
+        List<Integer> list= new ArrayList<>(3);
+        list.add(5);
+        list.add(8);
+        list.add(2);
+
+        carService.moveAllCars(list);
+
+        List<Car> cars = carService.findCars();
+
+        Assertions.assertThat(cars.get(0).getForward()).isEqualTo(1);
+        Assertions.assertThat(cars.get(1).getForward()).isEqualTo(1);
+        Assertions.assertThat(cars.get(2).getForward()).isEqualTo(0);
+
+        List<Integer> list2= new ArrayList<>(3);
+        list2.add(3);
+        list2.add(8);
+        list2.add(5);
+
+        carService.moveAllCars(list2);
+
+        Assertions.assertThat(cars.get(0).getForward()).isEqualTo(1);
+        Assertions.assertThat(cars.get(1).getForward()).isEqualTo(2);
+        Assertions.assertThat(cars.get(2).getForward()).isEqualTo(1);
+
+
+    }
 
 
 }
