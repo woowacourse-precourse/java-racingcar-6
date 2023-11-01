fc72755729310f5e7c6822f79227f72b0f8eafe9
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 14:24:41 +0900

    2023.11.01_feat : RacingCars playRound 메서드 생성

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,12 +1,22 @@
 package racingcar.model;
 
+import racingcar.view.OutputView;
+
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class RacingCars {
     private final ArrayList<Car> CARS = new ArrayList<>();
 
     public RacingCars(ArrayList<String> cars) {
         cars.forEach(car -> CARS.add(new Car(car)));
+    }
+
+    public void playRound() {
+        go();
+        OutputView.printRoundResult(new ArrayList<>(CARS.stream()
+                                                        .map(Car::getRoundResult)
+                                                        .collect(Collectors.toList())));
     }
 
     private void go() {
