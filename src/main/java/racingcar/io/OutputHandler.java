2d2eb7c6a103f25b89e32b753900936e2ac56952
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 21:22:38 +0900

    feat : OuputHandler 내 printCarStatus 추가

--- src/main/java/racingcar/io/OutputHandler.java
+++ src/main/java/racingcar/io/OutputHandler.java
@@ -1,8 +1,12 @@
 package racingcar.io;
 
 public class OutputHandler {
-    public static void printMessage(String message){
-        System.out.println(message);
+    public static void printCarStatus(String name, int position) {
+        System.out.print(name + " : ");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     public static void printErrorMessage(String message){
