551f4283422fe24752868a2db9ab6e8c4d41c74a
djc06048 <gsafe1213@gmail.com>
2023-11-01 15:08:49 +0900

    test: 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,33 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 입력값_빈_값에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("입력 값이 없습니다.")
+
+        );
+    }
+
+    @Test
+    void 입력값_형식_오류에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("입력 값의 형식이 잘못되었습니다.")
+
+        );
+    }
+
+    @Test
+    void 시도할_횟수_타입_정수인지_확인() {
+        assertThatThrownBy(() -> runException("pobi,woni", "ss"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 정수가 아닙니다.");
+
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
