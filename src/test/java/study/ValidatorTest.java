c035fbda7f6d1ba6188a80d5581302c17be5e841
leemiyeon <lemingul7@gmail.com>
2023-11-01 23:18:49 +0900

    test: fix ValidatorTest

--- src/test/java/study/ValidatorTest.java
+++ src/test/java/study/ValidatorTest.java
@@ -12,49 +12,49 @@ import racingcar.utils.Validator;
 public class ValidatorTest {
     @ParameterizedTest
     @EmptySource
-    void _빈_문자열_또는_Null_이름_입력(String nullAndEmpty) {
+    void input_null_car_name(String nullAndEmpty) {
         assertThatThrownBy(() -> Validator.validNameFormat(nullAndEmpty)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @EmptySource
-    void _빈_문자열_또는_Null_시도_횟수_입력(String nullAndEmpty) {
+    void input_null_round_number(String nullAndEmpty) {
         assertThatThrownBy(() -> Validator.validIsNumber(nullAndEmpty)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"자동차1", "car11","*(&&",","})
-    void _허용되지_않은_문자를_포함한_이름_입력(String testString) {
+    void input_not_allowed_car_name(String testString) {
         assertThatThrownBy(() -> Validator.validNameFormat(testString)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"name"})
-    void _하나의_이름만_입력(String singleName) {
+    void input_only_single_car_name(String singleName) {
         assertThatThrownBy(() -> Validator.validSingleName(singleName)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"0"})
-    void _0_시도_횟수_입력(String zero) {
+    void input_zero_round_number(String zero) {
         assertThatThrownBy(() -> Validator.validNotZero(zero)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"일", "two", "??"})
-    void _숫자가_아닌_시도_횟수_입력(String testString) {
+    void input_not_allowed_round_number(String testString) {
         assertThatThrownBy(() -> Validator.validIsNumber(testString)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"차차차차차차", "carcarcar", "ccccccccccc"})
-    void _허용되지_않은_길이의_이름_입력(String testString) {
+    void input_not_allowed_length_car_name(String testString) {
         assertThatThrownBy(() -> Validator.validLength(testString)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi,woni,jun,포비,우니,준"})
-    void _허용되는_이름들_입력(String testString) {
+    void input_allowed_car_name(String testString) {
         assertThatCode(() ->{
             Validator.validNameFormat(testString);
             Validator.validSingleName(testString);}).doesNotThrowAnyException();
@@ -62,7 +62,7 @@ public class ValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"123", "1", "1000"})
-    void _허용되는_시도_횟수_입력(String testString) {
+    void input_allowed_round_number(String testString) {
         assertThatCode(() -> {Validator.validIsNumber(testString);
             Validator.validNotZero(testString);}).doesNotThrowAnyException();
     }
