52b81c614b440d9c08b6e747a359d5aca4018608
Suyoung Oh <ohsusu515@gmail.com>
2023-11-01 13:17:27 +0900

    feat: (이름, 전진횟수)갖는 Car 객체 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constant.GameRule;
+
 public class Car {
+    private String name;
+    private int moveCnt;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCnt = 0;
+    }
 }
