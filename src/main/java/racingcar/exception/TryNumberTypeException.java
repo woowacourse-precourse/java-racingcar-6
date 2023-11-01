c95091f869ee62a7f620086bda90751b7b17d692
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 22:56:02 +0900

    feat : 시도 횟수 타입 검사 커스텀 예외 추가

---
+++ src/main/java/racingcar/exception/TryNumberTypeException.java
@@ -0,0 +1,7 @@
+package racingcar.exception;
+
+public class TryNumberTypeException extends IllegalArgumentException{
+    public TryNumberTypeException(String message) {
+        super(message);
+    }
+}
