29167ec9a5f0f2414c8c05be61e3bdc617102ac0
gundorit <packdev937@gmail.com>
2023-11-01 13:47:49 +0900

    test (Race) : 레이스 후 시도 횟수가 1 차감되는지 테스트 한다

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -5,19 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Race 클래스")
 class RaceTest {
 
+    private Cars cars;
+    private Try tryCount;
+    private Race race;
+
+    @BeforeEach
+    void setup() {
+        cars = new Cars(Arrays.asList("pobi", "crong", "jun"));
+        tryCount = new Try(5);
+        race = new Race(cars, tryCount);
+
+    }
+
     @Test
     void 레이스_종료_시_우승자가_존재() {
-        // given
-        Cars cars = new Cars(Arrays.asList("pobi", "crong", "jun"));
-        Try tryCount = new Try(5);
-        Race race = new Race(cars, tryCount);
-
         // when
         while (!race.isOver()) {
             race.doRace();
@@ -27,6 +35,12 @@ class RaceTest {
         List<String> winners = race.findWinners();
         assertThat(winners).isNotNull();
         assertThat(winners.size()).isGreaterThan(0);
+    }
+
+    @Test
+    void 레이스_후_시도_횟수가_1_차감() {
+        race.doRace();
+        assertThat(tryCount.getCount()).isEqualTo(4);
     }
 
 }
