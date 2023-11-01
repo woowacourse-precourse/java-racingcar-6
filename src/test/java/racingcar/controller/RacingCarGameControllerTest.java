2af915bf32c1af1730e25007620b2059baac3473
김관진 <cozyschooll@gmail.com>
2023-11-01 16:54:12 +0900

    Test: run 메소드 단위 테스트 구현

--- src/test/java/racingcar/controller/RacingCarGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarGameControllerTest.java
@@ -2,8 +2,14 @@ package racingcar.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Player;
@@ -13,9 +19,20 @@ class RacingCarGameControllerTest {
 
     private RacingCarGameController racingCarGameController;
     private List<String> playerNameList;
+    private ByteArrayOutputStream outputStream;
+
+    protected InputStream createInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    private String getOutput() {
+        return outputStream.toString();
+    }
 
     @BeforeEach
     void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         RacingCarGameView racingCarGameView = new RacingCarGameView();
         racingCarGameController = new RacingCarGameController(racingCarGameView);
 
@@ -25,8 +42,21 @@ class RacingCarGameControllerTest {
         playerNameList.add("c");
     }
 
+    @AfterEach
+    void coleTest() {
+        Console.close();
+        System.setOut(System.out);
+    }
+
     @Test
     void run() {
+        System.setIn(createInput("a,b,c\n0"));
+
+        racingCarGameController.run();
+        assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+                + "시도할 회수는 몇회인가요?\n\n"
+                + "실행 결과\n"
+                + "최종 우승자 : a, b, c\n", outputStream.toString());
     }
 
     @Test
