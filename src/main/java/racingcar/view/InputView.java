0873506e5b35a12aebbd22812f49f6d7ab3e17dc
kangaroo0 <ljw42871@gmail.com>
2023-11-01 19:55:10 +0900

    Feat: 유저의 두번째 input 받아서 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,15 +7,19 @@ import java.util.List;
 
 public class InputView {
 
-    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String START_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String START_ATTEMPT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private static void getStartMessage(){
-        System.out.println(START_MESSAGE);
+    private static void getStartNameMessage(){
+        System.out.println(START_NAME_MESSAGE);
+    }
+    private static void getStartAttemptMessage(){
+        System.out.println(START_ATTEMPT_MESSAGE);
     }
 
     public static void getInput(){
 
-        getStartMessage();
+        getStartNameMessage();
 
         try {
             String playersInput = Console.readLine();
@@ -23,6 +27,11 @@ public class InputView {
 
 
             List<String> playersList = makePlayersList(playersInput);
+
+
+            getStartAttemptMessage();
+
+            int attemptCount = Integer.parseInt(Console.readLine());
 
         } catch (IllegalArgumentException e){
 
