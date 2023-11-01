d88ab7d63163768c1402ba03754226c45a1778ce
Wangguri <anordinarykorean@gmail.com>
2023-11-01 18:09:24 +0900

    refactor(racingcar): Application 추가 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,39 @@
 package racingcar;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-	
-    public static void main(String[] args) {
-    	List<String> carNames = CarName.inputCarName();
-    	int tryTimes = TryTimes.askTimes();
-		
+	static List<String> carNames;
+	static int tryTimes;
+	static List<Integer> raceResult;
+
+	public static void main(String[] args) {
+		makeCarNames();
+		askTryTimes();
+		startRace();
+		showResult();
+	}
+
+	public static void makeCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String carName = Console.readLine();
+		carNames = CarName.inputCarName(carName);
+	}
+
+	public static void askTryTimes() {
+		System.out.println("시도할 횟수는 몇 회인가요?");
+		String inputTimes = Console.readLine();
+		tryTimes = TryTimes.checkTryTimes(inputTimes);
+	}
+
+	public static void startRace() {
 		System.out.println("실행 결과");
-		CarRacing carRacing = new CarRacing();
-		List<Integer> racingResult = carRacing.playRace(carNames, tryTimes);
-				
-		Winner lastResult = new Winner();
-		String result = lastResult.compareCount(racingResult);		
-		System.out.println("최종 우승자 : " + result);	
-    }
+		raceResult = CarRacing.playRace(carNames, tryTimes);
+	}
+
+	public static void showResult() {
+		String result = Winner.compareCount(raceResult);
+		System.out.println("최종 우승자 : " + result);
+	}
 }
--- src/main/java/racingcar/racingcarUtil.java
+++ src/main/java/racingcar/racingcarUtil.java
@@ -1,103 +1,98 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-
-
 class CarName {
 	public static List<String> carNames = new ArrayList<>();
-	public static List<String> inputCarName() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		String carName = Console.readLine();
-		String[] result = carName.split("[,]",0);
+
+	public static List<String> inputCarName(String carName) {
+		String[] result = carName.split("[,]");
 		carNameCheck(result);
 		carNames = Arrays.asList(result);
 		return carNames;
 	}
-	
+
 	public static void carNameCheck(String[] str) {
 		for (int i = 0; i < str.length; i++) {
-			if(str[i].length() > 5) {
+			if (str[i].length() > 5) {
 				throw new IllegalArgumentException();
 			}
 		}
 	}
-	
 }
 
 class TryTimes {
-	public static int askTimes() {
-		System.out.println("시도할 횟수는 몇 회인가요?");
-		String inputTimes = Console.readLine();
-		int tryTimes = Integer.parseInt(inputTimes);
-		
+	public static int checkTryTimes(String inputTimes) {
+		int tryTimes = 0;
+		try {
+			tryTimes = Integer.parseInt(inputTimes);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException();
+		}
 		return tryTimes;
 	}
-
 }
 
 class CarRacing {
 	public int moveCount = 0;
 	public String moveShow = "";
-	
-	public List<Integer> playRace(List<String> carNames, int tryTimes) {
-		
-		CarRacing[] car = new CarRacing[carNames.size()]; //입력된 자동차 갯수 만큼 객체 생성
+
+	public static List<Integer> playRace(List<String> carNames, int tryTimes) {
+
+		CarRacing[] carObject = new CarRacing[carNames.size()]; // 입력된 자동차 갯수 만큼 객체 생성
 		for (int j = 0; j < carNames.size(); j++) {
-        	car[j] = new CarRacing();
+			carObject[j] = new CarRacing();
 		}
-		
-		for (int i = 0; i < tryTimes ; i++) {
+
+		for (int i = 0; i < tryTimes; i++) {
 			for (int j = 0; j < carNames.size(); j++) {
-				car[j].carMove(carNames.get(j));
+				carObject[j].carMove(carNames.get(j));
 			}
 			System.out.println("");
 		}
-		
+
 		List<Integer> raceResult = new ArrayList<>();
 		for (int j = 0; j < carNames.size(); j++) {
-			raceResult.add(j, car[j].moveCount);
+			raceResult.add(j, carObject[j].moveCount);
 		}
-		
 		return raceResult;
 	}
-	
+
 	public int carMove(String carNames) {
-			String carName = carNames + " : ";
-			int randomNumber = Randoms.pickNumberInRange(0,9);
-			if(randomNumber >= 4) {
-				moveCount++;
-				moveShow += "-";	
-			}
-			System.out.println(carName + moveShow);
-			return moveCount;
+		String carName = carNames + " : ";
+		int randomNumber = Randoms.pickNumberInRange(0, 9);
+		if (randomNumber >= 4) {
+			moveCount++;
+			moveShow += "-";
+		}
+		System.out.println(carName + moveShow);
+		return moveCount;
 	}
 }
 
 class Winner extends CarName {
-	public String compareCount(List<Integer> racingResult) {
-		int winnerPoint = Collections.max(racingResult);
-		
+	public static String compareCount(List<Integer> raceResult) {
+		int winnerPoint = Collections.max(raceResult);
+
 		List<Integer> winnerIndexNumber = new ArrayList<>();
-		for (int i = 0; i < racingResult.size(); i++) {
-			if (racingResult.get(i).equals(winnerPoint)) {
+		for (int i = 0; i < raceResult.size(); i++) {
+			if (raceResult.get(i).equals(winnerPoint)) {
 				winnerIndexNumber.add(i);
 			}
 		}
 		return lastResult(winnerIndexNumber);
 	}
-	
+
 	public static String lastResult(List<Integer> winnerIndexNumber) {
 		List<String> winnerList = new ArrayList<>();
-		for(int i = 0; i < winnerIndexNumber.size(); i++) {
+		for (int i = 0; i < winnerIndexNumber.size(); i++) {
 			winnerList.add(carNames.get((winnerIndexNumber.get(i))));
 		}
-		
+
 		String result = String.join(", ", winnerList);
 		return result;
 	}
