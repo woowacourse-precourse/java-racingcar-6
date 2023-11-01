040775087369c60771d6ad010d5e75c91fca7220
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-01 21:56:23 +0900

    feat: 자동차 이름 입력 시 중복 이름 방지 및 시행 횟수 입력 시 입력하지 않은 경우에 대해 exception 발생

--- src/main/java/racingcar/utils/Validation.java
+++ src/main/java/racingcar/utils/Validation.java
@@ -1,12 +1,15 @@
 package racingcar.utils;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validation {
     private static final int NAME_LENGTH_MIN = 1;
     private static final int NAME_LENGTH_MAX = 5;
     private static final Pattern specialChar = Pattern.compile("\\W");
-    private static final Pattern exceptNumber = Pattern.compile("\\D");
+    private static final Pattern allCharExceptNumber = Pattern.compile("\\D");
+    private static final int TRIAL_LENGTH_MIN = 1;
 
     public static void validateName(String name) {
         isValidNameLength(name);
@@ -26,8 +29,20 @@ public class Validation {
         }
     }
 
+    public static void isDistinctNames(String[] names) throws IllegalArgumentException {
+        Set<String> distinctNames = new HashSet<>();
+        boolean isDistinctName;
+        for (String name : names) {
+            isDistinctName = distinctNames.add(name);
+            if (!isDistinctName) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public static void validateTrial(String trial) throws IllegalArgumentException {
-        if (exceptNumber.matcher(trial).find()) {
+        if (trial.length() < TRIAL_LENGTH_MIN
+                || allCharExceptNumber.matcher(trial).find()) {
             throw new IllegalArgumentException();
         }
     }
