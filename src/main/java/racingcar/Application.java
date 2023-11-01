be88068c59e2ad6f46729c3c1b86de56c4fa10ba
kwon <kckc0608@naver.com>
2023-11-01 09:36:42 +0900

    feat: 우승자 판별 및 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,7 @@ public class Application {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_TITLE_MESSAGE = "\n" + "실행 결과";
+    private static final String FINAL_WINNERS_MESSAGE = "최종 우승자 : ";
 
 
     private static List<Car> cars = new ArrayList<>();
@@ -68,5 +69,11 @@ public class Application {
             winners.add(car.getName());
         }
         return winners;
+    }
+
+    static void printGameResult() {
+        List<String> winners = getWinners();
+        System.out.print(FINAL_WINNERS_MESSAGE);
+        System.out.println(String.join(", ", winners));
     }
 }
