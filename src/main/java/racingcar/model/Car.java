d9b6e2883f4659373ea1d009bcc4acabbc8f51c4
hongjungin <jungini0000@gmail.com>
2023-11-01 17:13:07 +0900

    fear(Car) : 자동차 이름이 5자 이하 되도록 예외처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,7 +3,18 @@ package racingcar.model;
 public class Car {
 	private final String name;
 	
+	private static final int MAX_CARNAME_LENGTH = 5;
+	private static final int MIN_CARNAME_LENGTH = 1;
+		
 	public Car(String name) {
+		vallidateNameLength(name);
 		this.name = name;
+	}
+	
+	private void vallidateNameLength(String name) {
+		if(name.length() > MAX_CARNAME_LENGTH || 
+				name.length() < MIN_CARNAME_LENGTH) {
+			throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하여야 합니다.");
+		}
 	}
 }
