7444ff1456d775e1cd4f49024e2d67b7f08c3989
Zho <chlwlgh1011@naver.com>
2023-11-01 22:42:28 +0900

    fix(racingcar): 예외처리 해결 및 기능 구현완료

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -2,4 +2,5 @@ package racingcar.constant;
 
 public class Constant {
     static final public int BASE_NUMBER = 4;
+    static final public int INPUT_LIMIT_NUMBER = 4;
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.Constant;
 
 import java.util.List;
 
@@ -9,12 +10,10 @@ public class InputView {
     private static final String CHECK_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
     private static int numberOfCars;
     private static int attemtsNumber;
-
     private static List<String> allUserCarName;
     public static void printStartMessage() {
         System.out.println(START_RACING_GAME_INPUT_CAR_NAME);
     }
-
     public static void printAttemptsNumber () {
         System.out.println(CHECK_NUMBER_OF_ATTEMPTS);
     }
@@ -30,16 +29,16 @@ public class InputView {
         if(input == null) {
             throw new IllegalArgumentException();
         }
-        if(input.length() > 5) {
-            throw new IllegalArgumentException();
-        }
-        if(input.length() == 0) {
+        if(input.isEmpty()) {
             throw new IllegalArgumentException();
         }
     }
 
     public static int checkAttemptsNumber () {
         String attemtsNumberStr = getInput();
+        if(attemtsNumberStr.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         attemtsNumber = Integer.parseInt(attemtsNumberStr);
         return attemtsNumber;
     }
@@ -47,6 +46,11 @@ public class InputView {
     public static void diviedInputRacingCarName (String input) {
         allUserCarName = List.of(input.split(","));
         numberOfCars = allUserCarName.size();
+        for(int i = 0; i < numberOfCars; i++) {
+            if(allUserCarName.get(i).length() > Constant.INPUT_LIMIT_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
     public static List<String> getNameOfCars() {
         return allUserCarName;
