20a123253679fa8ca053577a2ef3df5e931ccc26
minwoo <mark42son@gmail.com>
2023-11-01 21:27:46 +0900

    style: error 메세지 자연스럽게 수정

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -3,12 +3,12 @@ package racingcar;
 import java.util.List;
 
 public class Validation {
-    private static final String COUNTS_NUMERIC_ERROR_MESSAGE = "시도 횟수는 자연수로 입력해주세요.";
+    private static final String COUNTS_NUMERIC_ERROR_MESSAGE = "시도 횟수는 자연수로 입력해주세요. 게임을 다시 실행해주세요.";
     private static final String ATTEMPT_COUNTS_VALIDATE_REGEX = "^[1-9][0-9]*$";
     private static final int CAR_NAME_MIN_SIZE = 1;
     private static final int CAR_NAME_MAX_SIZE = 5;
-    private static final String CAR_NAME_SIZE_ERROR_MESSAGE = "[ERROR] 이름은 1자 이상 5자 이하여야 한다.";
-    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 이름은 중복될 수 없다.";
+    private static final String CAR_NAME_SIZE_ERROR_MESSAGE = "이름은 5자 이하만 가능합니다. 게임을 다시 실행해주세요.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "이름이 중복되었습니다. 게임을 다시 실행해주세요.";
     public static void checkException(List<String> carNameList) {
         if (!checkCarNameLength(carNameList)) {
             throw new IllegalArgumentException(CAR_NAME_SIZE_ERROR_MESSAGE);
