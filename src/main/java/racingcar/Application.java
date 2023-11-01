29c7372e0197ddb8356043d6156583041401c147
eonpark <65460400+eonpark@users.noreply.github.com>
2023-11-01 17:51:53 +0900

    readLine으로 input 구현 Game 객체 생성 후 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+        String[] carNames = carNamesInput.split(",");
+
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tries = Integer.parseInt(Console.readLine());
+
+        Game game = new Game(carNames);
+        game.play(tries);
+        game.determineWinners();
     }
 }
+
+
+
