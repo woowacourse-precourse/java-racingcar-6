965b7547b776c3686ebaefc452fb2f8e341850b1
ehdtndla123 <ehdtndla123@naver.com>
2023-11-01 20:52:01 +0900

    refactor : main  테스트에 용이하기 위해 책임 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import game.Game;
+import game.GameInput;
+import util.GamePrinter;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Game game = new Game();
+
+        GamePrinter.inputCarNames();
+        String inputCarNames = Console.readLine();
+
+        GamePrinter.inputCount();
+        String inputCount = Console.readLine();
+
+        GameInput gameInput = new GameInput();
+
+        Game game = new Game(gameInput.inputCarNames(inputCarNames), gameInput.inputCount(inputCount));
         game.run();
     }
 }
