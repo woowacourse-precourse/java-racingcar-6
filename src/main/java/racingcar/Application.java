fa17d2b41d207a5297e82260fa4be748c2deb128
PaikMyeongGyu <qoraudrb123@gmail.com>
2023-11-01 15:23:56 +0900

    first commit

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,8 +5,8 @@ import racingcar.RacingGame.RacingGame;
 public class Application {
     public static void main(String[] args) {
 
+        // 변경하였음.
         RacingGame racingGame = new RacingGame();
-
         racingGame.GameManager();
     }
 }
