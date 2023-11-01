d2ba987ed917041b4bbc3614de6fd512ff6ea23b
jun23314 <dlwnsfml@naver.com>
2023-11-01 16:15:06 +0900

    자동차 이름 입력시 예외 추가 - 공백은 이름이 될 수 없다

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -6,8 +6,7 @@ public class UserInput {
         String input = Console.readLine();
         String[] carName = input.split(",");
         carName = removeBlank(carName);
-        checkLength(carName);
-        checkNull(carName);
+        checkName(carName);
         return carName;
     }
 
@@ -30,6 +29,20 @@ public class UserInput {
         if(strings.length == 1 && strings[0] == ""){
             throw new IllegalArgumentException("하나 이상의 이름을 입력해주세요");
         }
+    }
+
+    private void checkBlank(String[] strings){
+        for(int i = 0; i < strings.length; i++){
+            if(strings[i] == ""){
+                throw new IllegalArgumentException("공백은 이름이 될 수 없습니다.");
+            }
+        }
+    }
+
+    private void checkName(String[] str){
+        checkLength(str);
+        checkNull(str);
+        checkBlank(str);
     }
 
     public int inputCount(){
