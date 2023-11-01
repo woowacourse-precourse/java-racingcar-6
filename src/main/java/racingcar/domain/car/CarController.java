af7d9c885229207092871d6b59dbaac4e28eb304
leesc0839 <leesc0839@naver.com>
2023-11-01 18:08:47 +0900

    refactor(racingcar): CarController 내부에 car 움직이게 하는 로직 모으기
    - CarController
    	- 자동차를 움직이게 하기 위한 랜덤숫자 뽑기
    	- 랜덤 숫자에 따라서 숫자 움직이게 하기

--- src/main/java/racingcar/domain/car/CarController.java
+++ src/main/java/racingcar/domain/car/CarController.java
@@ -1,13 +1,18 @@
 package racingcar.domain.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static racingcar.Constant.*;
+
 public class CarController {
 
-    private static boolean canForward(int randomNumber, int forwardNumber) {
-        return randomNumber >= forwardNumber;
+    private static boolean canForward() {
+        int randomNumber = Randoms.pickNumberInRange(START, END);
+        return randomNumber >= MOVING_FORWARD;
     }
 
-    public static void move(Car car, int randomNumber, int forwardNumber) {
-        if (canForward(randomNumber, forwardNumber))
+    public static void move(Car car) {
+        if (canForward())
             car.forward();
     }
 }
