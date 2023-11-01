8ec58a392d46a99b0ac7b56cdec67a6469b51aa6
hann0079 <haeun8701@gmail.com>
2023-11-01 20:35:38 +0900

    feat: main loop 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,18 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String[] carNames = InputHandler.getCarNames();
+            int raceRounds = InputHandler.getRaceRounds();
+
+            RacingGame racingGame = new RacingGame(carNames);
+            for (int i = 0; i < raceRounds; i++) {
+                racingGame.race(1);
+                OutputDisplay.showRaceResult(racingGame.getCars());
+            }
+            OutputDisplay.showWinners(racingGame.getCars());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
