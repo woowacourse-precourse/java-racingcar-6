925dd5cb3ea54730e4b6ef8c920f92c626c86932
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 22:42:02 +0900

    feat(Entry): 속성 및 생성자 정의
    Entry는 경주에 참여할 일련의 차량들로 구성된 List 타입 객체를 감싸는 일급 컬렉션입니다.

--- src/main/java/racingcar/model/Entry.java
+++ src/main/java/racingcar/model/Entry.java
@@ -1,4 +1,12 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Entry {
+
+    private final List<Car> cars = new ArrayList<>();;
+
+    public Entry() {
+    }
 }
