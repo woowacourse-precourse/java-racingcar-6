4c13c77191904b6455d244e579533eeecae4144b
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-01 13:23:04 +0900

    refactor(OutputView): Output접근제어자 public으로 변경

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,27 +4,27 @@ import java.util.List;
 
 public class OutputView {
 
-    static void printRoundSet() {
+    public static void printRoundSet() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    static void printInitCars() {
+    public static void printInitCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    static void printCarField(Car car) {
+    public static void printCarField(Car car) {
         System.out.println(car.myCurrentStatus());
     }
 
-    static void newLinePrint() {
+    public static void newLinePrint() {
         System.out.println();
     }
 
-    static void gameStartPrint() {
+    public static void gameStartPrint() {
         System.out.println("\n실행 결과");
     }
 
-    static void winnersPrint(List<String> winners) {
+    public static void winnersPrint(List<String> winners) {
         StringBuilder winnersStringBuilder = new StringBuilder();
         winnersStringBuilder.append("최종 우승자 : ");
         winners.stream()
