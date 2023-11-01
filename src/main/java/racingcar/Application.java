0f61b8dc03f9c6ac4038acaa43a247c3ecbaa222
yang <hjyang@epience.com>
2023-11-01 16:05:55 +0900

    🚀refector(maini): main함수 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import racingcar.domain.RacingGame;
-import racingcar.domain.Winner;
 import racingcar.service.RandomGenerator;
 import racingcar.view.Input.InputParser;
 import racingcar.view.Input.InputValidator;
@@ -10,7 +9,12 @@ import racingcar.view.Output.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame(new InputView(new InputParser(), new InputValidator()),new OutputView(), new RandomGenerator());
-        racingGame.goOrStop();
+        RacingGame racingGame = new RacingGame(inputView(),new OutputView(), new RandomGenerator());
+
+        racingGame.race();
+    }
+    private static InputView inputView() {
+        return new InputView(
+                new InputParser(), new InputValidator());
     }
 }
