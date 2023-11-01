400bb923861d550a291afc4aab0576fec3c0c5de
Pearl <rkdwlswn227@gmail.com>
2023-11-01 19:41:34 +0900

    refactor: RacingGame에 게임 결과 출력 메서드 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,16 +1,50 @@
 package racingcar;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingGame implements Game{
-    public void startGame(){
-        List<Car> cars = InputCarInfo.inputCarName();
-        int playCnt = InputTryNumber.inputTryNumber();
 
+    List<Car> cars;
+    int playCnt;
+
+    public void startGame(){
+        cars = InputCarInfo.inputCarName();
+        playCnt = InputTryNumber.inputTryNumber();
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for(int i = 0; i < playCnt; i++){
+            playGame();
+            printResultOfRounds();
+            System.out.println();
+        }
     }
 
     public void playGame(){
-
+        for (Car car : cars){
+            boolean shouldMove = determineMove();
+            if (shouldMove){
+                car.goForward();
+            }
+        }
     }
 
+    private boolean determineMove(){
+        int check = Randoms.pickNumberInRange(0, 9);
+
+        if (check >= 4){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void printResultOfRounds(){
+        for (Car car : cars) {
+            System.out.println(car.toString());
+        }
+    }
 }
