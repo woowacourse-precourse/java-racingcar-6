9b6d372491136c1ffcf4deee52bb34c8d353baee
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 14:33:26 +0900

    feat : 자동차 이름 리스트들을 만들 CarListMaker.java 와 메소드 생성

---
+++ src/main/java/racingcar/car/CarListMaker.java
@@ -0,0 +1,9 @@
+package racingcar.car;
+
+public class CarListMaker {
+    InputController inputController = new InputController();
+    public List<String> makeCarList() {
+        String carNamesString = inputController.scanCarNames();
+        carNamesString
+    }
+}
