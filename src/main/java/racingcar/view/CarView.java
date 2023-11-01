8fc056b6a7e9f2b9d57fc5136c1342bc6b3c86bf
gmu1026 <curiouscomponent@gmail.com>
2023-11-01 12:44:25 +0900

    REFACTOR: 차 관련 상수 이름 변경

--- src/main/java/racingcar/view/CarView.java
+++ src/main/java/racingcar/view/CarView.java
@@ -2,14 +2,14 @@ package racingcar.view;
 
 public class CarView {
     private CarView() {}
-    private static final String CAR_CREATE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String CAR_MOVE_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String CAR_CREATE_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_MOVE_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static void printCarCreateMessage() {
-        System.out.println(CAR_CREATE_MESSAGE);
+        System.out.println(CAR_CREATE_INPUT_MESSAGE);
     }
 
     public static void printCarMoveMessage() {
-        System.out.println(CAR_MOVE_MESSAGE);
+        System.out.println(CAR_MOVE_INPUT_MESSAGE);
     }
 }
