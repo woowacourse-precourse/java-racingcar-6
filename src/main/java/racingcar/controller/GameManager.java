6f6dcdb9ea66f9b5c3449f1ce21a49d93ce20371
mingtorr <mingtorr@nave.com>
2023-11-01 23:06:48 +0900

    feat: 자동차 객체 생성 기능 추가
    GameManager
    - 사용자 입력을 바탕으로 자동차 객체 생성 기능 추가 createCarList()
    
    README.md
    - 기능 구현 체크

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -1,13 +1,30 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameManager {
+    private final UserInputHandler userInputHandler = new UserInputHandler();
 
     public void startGame() {
         try {
-            UserInputHandler inputHandler = new UserInputHandler();
+            List<String> nameList = userInputHandler.getCarNameList();
+            int round = userInputHandler.getGameRound();
+            List<Car> carList = createCarList(nameList);
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public List<Car> createCarList (List<String> nameList) {
+        List<Car> carList = new ArrayList<>();
+        for(String name: nameList) {
+            Car car = new Car(name);
+            carList.add(car);
+        }
+        return carList;
     }
 }
