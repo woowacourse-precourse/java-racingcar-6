9bc67cba8b6def4f9b852d1e48c4f1edc113ff71
chooh <chocomilk66@naver.com>
2023-11-01 21:44:12 +0900

    refactor: 자동차 이름 길이에 대한 코드 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,12 +3,13 @@ package racingcar;
 public class Car {
 
     private static final int BOUND = 4;
+    private static final int maxNameLength = 5;
     private final String name;
 
     private int position = 0;
 
     public Car(String name) {
-        if (name.length() > 5) {
+        if (name.length() > maxNameLength) {
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다.");
         }
         if (name.isEmpty()) {
