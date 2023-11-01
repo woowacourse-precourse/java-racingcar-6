d05d99e4d589279217af8e0a4a8e7184f9ec3a3e
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-01 13:45:33 +0900

    feat: write test code for the execution class

---
+++ src/test/java/racingcar/ExecutionTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ExecutionTest {
+    @DisplayName("시도 회수가 양의 정수인 경우 정상적으로 작동합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "100"})
+    void 시도_회수_생성_테스트(String count) {
+        Assertions.assertDoesNotThrow(() -> {
+            new RacingCar(count);
+        });
+    }
+
+    @DisplayName("시도 회수가 양의 정수가 아니면 예외가 발생합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "exception", "-1", "0.01"})
+    void 시도_회수_생성_예외_테스트(String count) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Execution(count);
+        });
+    }
+}
