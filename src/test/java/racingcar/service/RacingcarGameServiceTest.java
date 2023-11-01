701bc4b84a52f5c279fe0223189284a29a498a40
Sung Hyobin <hellmir610@gmail.com>
2023-11-01 19:51:48 +0900

    test: Add test of function that proceed rounds of racing game

--- src/test/java/racingcar/service/RacingcarGameServiceTest.java
+++ src/test/java/racingcar/service/RacingcarGameServiceTest.java
@@ -1,10 +1,14 @@
 package racingcar.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.RandomNumbers;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static racingcar.testutil.TestConstant.*;
 
@@ -23,12 +27,44 @@ class RacingcarGameServiceTest {
         Cars cars = racingcarGameService.generateCarsToRace(inputMessage);
 
         // then
-        Assertions.assertThat(cars.getCars()).hasSize(3)
+        assertThat(cars.getCars()).hasSize(3)
                 .extracting("name", "position")
                 .containsExactlyInAnyOrder(
                         tuple(CAR_NAME1, 0),
                         tuple(CAR_NAME2, 0),
                         tuple(CAR_NAME3, 0)
+                );
+    }
+
+    @DisplayName("각 라운드마다 임의의 숫자 값을 토대로 자동차를 전진하거나 전진하지 않는다.")
+    @Test
+    void playOneRound() {
+        // given
+        Car car1 = Car.of(CAR_NAME1);
+        Car car2 = Car.of(CAR_NAME2);
+        Car car3 = Car.of(CAR_NAME3);
+        Car car4 = Car.of(CAR_NAME4);
+
+        Cars cars = new Cars();
+        cars.addCar(car1);
+        cars.addCar(car2);
+        cars.addCar(car3);
+        cars.addCar(car4);
+
+        RandomNumbers randomNumbers
+                = new RandomNumbers(List.of(RANDOM_NUMBER1, RANDOM_NUMBER2, RANDOM_NUMBER3, RANDOM_NUMBER4));
+
+        // when
+        racingcarGameService.playOneRound(cars, randomNumbers);
+
+        // then
+        assertThat(cars.getCars()).hasSize(4)
+                .extracting("name", "position")
+                .containsExactlyInAnyOrder(
+                        tuple(CAR_NAME1, 0),
+                        tuple(CAR_NAME2, 0),
+                        tuple(CAR_NAME3, 1),
+                        tuple(CAR_NAME4, 1)
                 );
     }
 
