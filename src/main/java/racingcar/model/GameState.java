9f19ef33bad775eea504a02a456efcd345ad974c
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-01 09:31:52 +0900

    feat: 게임 진행 중 쓰일 상태 값들 추가

---
+++ src/main/java/racingcar/model/GameState.java
@@ -0,0 +1,14 @@
+package racingcar.model;
+
+public class GameState {
+    private int maxDistance;
+    private final int roundLength;
+
+
+    public GameState(){
+        this.roundLength = 0;
+        this.maxDistance=0;
+    }
+
+
+}
