ca4e9e110fd81b67d5a577c15b0f96cab2fcaf39
이두현 <ldh9411@hanmail.net>
2023-11-01 21:29:32 +0900

    Feat: domain 추가
    - Car class 추가
    - Game class 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -21,4 +21,6 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+
 }
--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,12 +1,17 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
 
     private List<Car> cars;
     private int currentAttemptCnt = 0;
-    private int finalAttemptCnt;
+    private int finalAttemptCnt = 0;
+
+
+    public Game() {
+    }
 
     public Game(List<Car> cars, int finalAttemptCnt) {
         this.cars = cars;
@@ -19,5 +24,17 @@ public class Game {
 
     public boolean checkAttemptCountFinal() {
         return this.currentAttemptCnt == this.finalAttemptCnt;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int getCurrentAttemptCnt() {
+        return currentAttemptCnt;
+    }
+
+    public int getFinalAttemptCnt() {
+        return finalAttemptCnt;
     }
 }
