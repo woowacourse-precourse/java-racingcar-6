65b73d8d6f6b61c713bcd51fcb743905f7b3d1d8
동원 <ai06021@gmail.com>
2023-11-01 19:36:47 +0900

    style : 차 이름 확인

--- src/main/java/racingcar/view/Cars.java
+++ src/main/java/racingcar/view/Cars.java
@@ -1,27 +1,10 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Cars {
     private static final int maxNameLength = 5;
-    private static int location = 0;
-    private String name;
-
-    Cars(String name, int location) {
-        this.name = name;
-        this.location = location;
-    }
-
-    public static List<Cars> make() {
-        List<String> names = name();
-        List<Cars> cars = new LinkedList<>();
-        for (String name : names) {
-            cars.add(new Cars(name, location));
-        }
-        return cars;
-    }
     public static List<String> name() {
         String inputNames = Console.readLine();
         List<String> names = List.of(inputNames.split(","));
