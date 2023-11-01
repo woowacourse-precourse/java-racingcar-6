da826a42604863d8a7d212c4ef05eb9bdfefdfe7
Isaac Han <isaachan233@gmail.com>
2023-11-01 12:01:48 +0900

    test: WinnersTest 테스트

--- src/test/java/racingcar/model/WinnersTest.java
+++ src/test/java/racingcar/model/WinnersTest.java
@@ -1,25 +1,15 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Test;
-import racingcar.enums.Common;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 class WinnersTest {
     @Test
-    void getWinnersNames() {
-        Cars cars = new Cars("isaac,pobi,woni,jun,so");
-        Attempts attempts = new Attempts(1);
-        assertRandomNumberInRangeTest(() -> {
-                    cars.everyCarMoveForward();
-                    Winners winners = new Winners(cars, attempts);
-                    assertThat(winners.getWinnersNames()).isEqualTo("isaac");
-                }, Common.STANDARD_NUMBER.getIntValue(),
-                Common.STANDARD_NUMBER.getIntValue()-1,
-                Common.STANDARD_NUMBER.getIntValue()-1,
-                Common.STANDARD_NUMBER.getIntValue()-1,
-                Common.STANDARD_NUMBER.getIntValue()-1);
+    void getWinnersNamesTest() {
+        String carNames = "isaac,han";
+        Winners winners = new Winners(new Cars(carNames));
+        assertThat(winners.getWinnersNames()).contains("isaac","han");
     }
-
 }
