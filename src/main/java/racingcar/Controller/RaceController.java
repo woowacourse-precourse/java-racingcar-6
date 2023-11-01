130f5b42fea0ca93345b5848b20b7ea9d568da00
elizabethgim <yujingim@naver.com>
2023-11-01 18:10:54 +0900

    feat: 게임 진행 및 진행 사항을 보여주는 Race Controller

---
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -0,0 +1,39 @@
+package racingcar.Controller;
+
+import racingcar.Service.CarService;
+import racingcar.Service.RaceService;
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceController {
+    private final CarService carService = new CarService();
+    private final RaceService raceService = new RaceService();
+
+    public void gamePlay(int numberOfGames, List<Car> cars){
+        System.out.println("실행 결과");
+
+        for(int i =0;i<numberOfGames;i++){
+            carService.runOneTurn(cars);
+            carService.showResults(cars);
+        }
+    }
+
+    public void start(){
+        Race race = new Race();
+        List<Car> cars = new ArrayList<>();
+
+        cars = race.setCarNames(cars);
+        int gameCount = race.setNumberOfAttempts();
+
+        System.out.println("실행 결과");
+        for(int i=0;i<gameCount;i++){
+            carService.runOneTurn(cars);
+            carService.showResults(cars);
+        }
+
+        raceService.showResults(cars);
+    }
+}
