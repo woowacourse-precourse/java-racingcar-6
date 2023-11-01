3c6b1d3c41c7eb0292a8d3029b07da9c927eb83d
leeseobin00 <leeseobin000709@gmail.com>
2023-11-01 19:39:13 +0900

    refactor: ATTEMPTS_FORMAT_ERROR 메세지 변경

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -6,7 +6,7 @@ public class ErrorMessage {
     public static final String CAR_NAME_EMPTY_ERROR = "자동차 이름은 공백일 수 없습니다.";
     public static final String CAR_NAME_FORMAT_ERROR = "자동차 이름은 알파벳만 가능합니다.";
 
-    public static final String ATTEMPTS_FORMAT_ERROR = "시도 횟수는 숫자만 가능합니다.";
+    public static final String ATTEMPTS_FORMAT_ERROR = "시도 횟수는 Integer 값만 가능합니다.";
     public static final String ATTEMPTS_EMPTY_ERROR = "시도 횟수는 공백일 수 없습니다.";
     public static final String INVALID_ATTEMPTS_ERROR = "유효하지 않은 시도 횟수입니다.";
 }
