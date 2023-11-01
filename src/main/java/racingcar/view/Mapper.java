ad9731a6da719183a2d1a069694719a1425c9070
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-01 22:30:58 +0900

    refactor: 매직넘버 제거 및 값 수정

--- src/main/java/racingcar/view/Mapper.java
+++ src/main/java/racingcar/view/Mapper.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Mapper {
+    private static final int INT_NUMBER_LENGTH = 11;
+
     public static List<String> changeToListString(String carName) {
         return Arrays.stream(carName.split(","))
                 .toList();
     }
 
     public static int changeToInteger(String number) {
-        if (number.length() >= 10) {
+        if (number.length() >= INT_NUMBER_LENGTH) {
             throw new IllegalArgumentException("너무 큰 숫자를 입력했습니다.");
         }
 
