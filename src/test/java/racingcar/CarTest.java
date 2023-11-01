93afdf038b7f9b12942501df011dbac531251f86
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-11-01 14:19:35 +0900

    test(CarTest): Car Move 메서드 전진 테스트 코드 구현
    move_go 단위 테스트 작성
    - pickNumberInRange 를 어떻게 해야할지 몰라서 그냥 ApplicationTest 에 있는 방식으로 하기로 결정
    - move 메서드 작동 후 변화된 위치 검증

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest extends NsTest {
+
+    private final Car testCar = new Car("test");
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    @DisplayName("move 메서드 전진 테스트")
+    void move_go() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run();
+                    assertThat(testCar.toString())
+                            .isEqualTo("test : -");
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        testCar.move();
+    }
+}
