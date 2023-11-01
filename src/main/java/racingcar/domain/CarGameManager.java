6ec1d8cfb5607d07c8012066c84d498346bca513
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-01 21:49:09 +0900

    fix: 실행 결과 코드 수정 시도 횟수를 5번 하면 실행 결과에 대한 전진 상황이 5번 나와야하는데 1번만 나오는 현상이 있어 코드를 수정했습니다.

--- src/main/java/racingcar/domain/CarGameManager.java
+++ src/main/java/racingcar/domain/CarGameManager.java
@@ -17,8 +17,10 @@ public class CarGameManager {
 
     public void carGameStart(Cars cars, final int roundNumber) {
         OutPutView.executionResultView();
-        IntStream.range(0, roundNumber).forEach(i -> cars.carsMove());
-        OutPutView.resultNameAndAdvanceView(cars.carsResult());
+        IntStream.range(0, roundNumber).forEach(i -> {
+            cars.carsMove();
+            OutPutView.resultNameAndAdvanceView(cars.carsResult());
+        });
     }
 
     public void resultWinner(Cars cars) {
