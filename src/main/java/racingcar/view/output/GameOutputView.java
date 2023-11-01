e29606b0d71fa393814ccb89b846877ee1a8a8bd
gmelon <hsh1769@naver.com>
2023-11-01 13:54:40 +0900

    feat: 게임 진행 결과에 대한 출력을 생성하는 PlayResultOutput 구현

--- src/main/java/racingcar/view/output/GameOutputView.java
+++ src/main/java/racingcar/view/output/GameOutputView.java
@@ -4,21 +4,15 @@ import java.util.List;
 import racingcar.dto.CarDto;
 
 public class GameOutputView {
-    private static final String POSITION_INDICATOR = "-";
-
     public static void printPlayResultTitle() {
         System.out.println("실행 결과");
     }
 
     public static void printPlayResults(List<CarDto> carDtos) {
-        for (CarDto carDto : carDtos) {
-            System.out.println(carDto.name() + " : " + positionString(carDto));
-        }
+        carDtos.stream()
+                .map(PlayResultOutput::from)
+                .map(PlayResultOutput::output)
+                .forEach(System.out::println);
         System.out.println();
-    }
-
-    private static String positionString(CarDto carDto) {
-        // TODO 클래스 분리 및 테스트
-        return POSITION_INDICATOR.repeat(carDto.position());
     }
 }
