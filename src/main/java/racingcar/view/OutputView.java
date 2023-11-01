2ae04fb43dd7ff2b3f40ee42e27280b035e812de
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 22:32:48 +0900

    [refactor] : add result position method

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,15 +5,24 @@ import racingcar.domain.Winner;
 import racingcar.domain.Constants;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OutputView {
 
     public static void showCarNameAndPosition(List<Car> cars) {
-        cars.forEach(car -> System.out.println(car.getName() + Constants.SHOW_CAR_RESULT_DELIMITER + car.resultPosition()));
+        cars.forEach(car -> System.out.println(car.getName() + Constants.SHOW_CAR_RESULT_DELIMITER + resultPosition(car)));
         System.out.println(Constants.NEXT_LINE);
     }
 
     public static void finalWinnerMessage(Winner winner) {
         System.out.println(Constants.SHOW_WINNER_MESSAGE + winner.getWinnerCarNames());
+    }
+
+    public static String resultPosition(Car car) {
+
+        return IntStream.range(0,car.getPosition())
+                .mapToObj(i -> Constants.DASH)
+                .collect(Collectors.joining());
     }
 }
