538594cf106f40eab0b1936e9672fce041970a6e
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-01 20:19:08 +0900

    test: 레이싱 게임 예외 테스트 코드 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -17,4 +17,18 @@ class RacingGameTest {
         // then
         Assertions.assertThatCode(racingGame::run).doesNotThrowAnyException();
     }
+
+    @Test
+    void 레이싱_게임_예외_테스트() {
+        // given
+        final String input = "pobi,woni,ju n\na";
+        RacingGame racingGame = new RacingGame();
+
+        // when
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // then
+        Assertions.assertThatThrownBy(racingGame::run)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
