52271c9e9aebdf7c14ce58cdf3e8365dcf2019bc
ASak1104 <khw56184@gmail.com>
2023-11-01 14:03:40 +0900

    feat : 자동차 이름에 대한 검증 코드 추가

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,13 +1,32 @@
 package racingcar.model;
 
+import java.text.MessageFormat;
+
 public class RacingCar {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+
     private final String name;
 
+    private int lap;
+
     public RacingCar(String name) {
+        validateName(name);
+
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 빈 문자열이 될 수 없습니다.");
+        }
+
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(MessageFormat.format(
+                    "자동차 이름은 {0}글자 이하만 가능합니다.", MAXIMUM_NAME_LENGTH));
+        }
     }
 }
