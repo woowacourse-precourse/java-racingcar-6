6f3e4a4a16cb2d4d35955810ea3850d4929c91c3
donghoonyeom <dua5410773@naver.com>
2023-11-01 17:59:42 +0900

    test(RacingGameTest): 자동차 이동, 우승자 계산 및 예외 처리 관련 테스트 수정
    - 자동차 이동 및 우승자 계산 관련 테스트를 보다 포괄적으로 수정하였습니다.
    - 단일 자동차 게임에서 우승자가 자기 자신인지 확인하는 테스트를 추가하였습니다.
    - 자동차 이동 결과가 유지되는지 확인하는 테스트를 작성하였습니다.
    - 게임에 참여한 자동차가 없을 때 예외 처리하는 테스트를 중복 정의된 부분을 정리하고자 수정하였습니다.

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -39,6 +39,30 @@ class RacingGameTest {
     }
 
     @Test
+    @DisplayName("자동차가 한 대인 경우 우승자가 자기 자신인지 확인")
+    void singleCarGameWinner() {
+        Car car = new Car("car1");
+        RacingGame racingGame = new RacingGame(Arrays.asList(car));
+
+        racingGame.moveCars();
+
+        assertThat(racingGame.getWinners()).containsExactly(car);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 결과 유지 테스트")
+    void keepCarDistanceAfterMultipleMoves() {
+        Car car = new Car("car1");
+        RacingGame racingGame = new RacingGame(Arrays.asList(car));
+
+        int initialDistance = car.getDistance();
+        racingGame.moveCars();
+        racingGame.moveCars();
+
+        assertThat(car.getDistance()).isEqualTo(initialDistance + 1);
+    }
+
+    @Test
     @DisplayName("게임에 참여한 자동차가 없을 때 예외 발생 테스트")
     void createGameWithNoCars() {
         List<Car> cars = Arrays.asList();
