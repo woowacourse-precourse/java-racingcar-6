613ee1d8e9558c2f7a9246ea3c03d97a38bdafec
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-01 14:09:45 +0900

    [FEAT] create skeleton code

---
+++ src/main/java/racingcar/domain/CarInformation.java
@@ -0,0 +1,7 @@
+package racingcar.domain;
+
+public class CarInformation {
+    public void getRandomNumber(){
+
+    }
+}
---
+++ src/main/java/racingcar/domain/GameManagement.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class GameManagement {
+    public void playRound(){
+
+    }
+
+    public List<CarInformation> getWinners(){
+        return null;
+    }
+
+}
