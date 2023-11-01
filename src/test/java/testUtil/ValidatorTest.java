04165ff6d194737fbed55aa4ff0ede78ac924e8f
HungKungE <jonsun59@gmail.com>
2023-11-01 23:50:33 +0900

    [test]test 코드 작성

--- src/test/java/testUtil/ValidatorTest.java
+++ src/test/java/testUtil/ValidatorTest.java
@@ -1,8 +1,7 @@
 package testUtil;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +12,25 @@ class ValidatorTest {
     private final Validator validator = new Validator();
 
     @Test
-    void 차_이름_예외_테스트(){
+    void 차_이름_예외_테스트_NULL(){
+        String input = "";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.validateCarNames(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 차_이름_예외_테스트_정상과비정상(){
+        String input = "ppp,dddddd";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.validateCarNames(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 차_이름_예외_테스트_6글자(){
         String input = "pppppp";
 
         assertSimpleTest(() ->
@@ -21,10 +38,27 @@ class ValidatorTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void 차_이름_예외_테스트_정상(){
+        String input = "qqq,www";
+
+        assertSimpleTest(() ->
+                assertThatCode(() -> validator.validateCarNames(input)).doesNotThrowAnyException()
+        );
+    }
+    @Test
+    void 턴_예외_테스트_숫자와문자(){
+        String input = "123A";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.validateTurns(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     @Test
-    void 턴_예외_테스트(){
-        String input = "123A";
+    void 턴_예외_테스트_문자만(){
+        String input = "ABC";
 
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> validator.validateTurns(input))
