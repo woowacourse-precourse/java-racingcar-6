dabf7e8446a45187a3a8b026ebc5cfe63bc3859a
강승민 <rkdtmdals999@naver.com>
2023-11-01 16:07:38 +0900

    [refactor] 자동차 이름에 대한 예외를 하나의 예외 처리 메소드로 묶음

--- src/main/java/racingcar/controller/InputException.java
+++ src/main/java/racingcar/controller/InputException.java
@@ -8,17 +8,22 @@ import racingcar.model.CarStorage;
 
 public class InputException {
     private InputException() {}
-    public static void checkNameLength(String newCarName) { // 이름 길이 5자 초과 예외처리
+    public static void checkAll(String newCarName) {
+        checkNameLength(newCarName);
+        checkOnlyInt(newCarName);
+        checkExistCar(newCarName);
+    }
+    private static void checkNameLength(String newCarName) { // 이름 길이 5자 초과 예외처리
         if (newCarName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
-    public static void checkOnlyInt(String newCarName) { // 숫자로만 이루어진 이름 예외처리
+    private static void checkOnlyInt(String newCarName) { // 숫자로만 이루어진 이름 예외처리
         if (newCarName.matches(ONLYINT)) {
             throw new IllegalArgumentException();
         }
     }
-    public static void checkExistCar(String newCarName) { // 같은 이름의 자동차 사용 예외처리
+    private static void checkExistCar(String newCarName) { // 같은 이름의 자동차 사용 예외처리
         String carName;
         for (Car car : CarStorage.cars()) {
             carName = car.name();
--- src/main/java/racingcar/controller/User.java
+++ src/main/java/racingcar/controller/User.java
@@ -15,11 +15,7 @@ public class User {
 
         for (String car: splitCars) {
             car = car.replace(" ", "");
-
-            InputException.checkExistCar(car); // 이미 존재하는 차 이름인지 확인
-            InputException.checkNameLength(car); // 이름의 길이가 5 이하인지 확인
-            InputException.checkOnlyInt(car); // 이름이 정수로만 되어있지 않은지 확인
-
+            InputException.checkAll(car);
             CarStorage.addCar(new Car(car));
         }
     }
