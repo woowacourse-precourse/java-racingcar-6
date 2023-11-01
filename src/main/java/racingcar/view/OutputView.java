8e779a18e5ecf31ed7113b862d161641f1676229
yunju-m <dbswnfnal@naver.com>
2023-11-01 15:54:08 +0900

    Feat(OutputView): ✨ 자동차 경주를 시작하는 문구 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,12 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String PRINT_REPEAT_GAME_RESULT_RESULT_MESSAGE = "실행 결과";
     private static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void printGameResultMessage() {
+        System.out.println(PRINT_REPEAT_GAME_RESULT_RESULT_MESSAGE);
+    }
 
     public void printRaceResult(String carName, String ForwardStick) {
         System.out.println(carName + " : " + ForwardStick);
