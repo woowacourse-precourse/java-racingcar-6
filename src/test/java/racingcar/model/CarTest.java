6d1208dd2596450a77a65754d1f42a33be394e99
a3ure <cosyyme@gmail.com>
2023-11-01 21:46:22 +0900

    style: 코드 컨벤션 적용

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,21 +2,22 @@ package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    private final Car testCar = new Car("name",0);
+    private final Car testCar = new Car("name", 0);
 
     @Test
     @DisplayName("자동차 초기화 테스트")
-    void initCarTest(){
+    void initCarTest() {
         assertThat(testCar.name).isEqualTo("name");
     }
 
     @Test
     @DisplayName("자동차 전진 테스트")
-    void goForwardTest(){
+    void goForwardTest() {
         testCar.goForward();
         assertThat(testCar.forwardCount).isEqualTo(1);
     }
