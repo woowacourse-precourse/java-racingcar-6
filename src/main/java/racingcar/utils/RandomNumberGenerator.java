8c616b2b938d8006310db9a9d3d68d90e540372a
BBOXEEEE <noeyhesx@naver.com>
2023-11-01 21:00:55 +0900

    refactor: 하드코딩 대신 상수를 이용하도록 수정

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,9 +1,12 @@
 package racingcar.utils;
 
+import static racingcar.constant.PositionConstant.LOW_BOUND;
+import static racingcar.constant.PositionConstant.HIGH_BOUND;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
     public static int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(LOW_BOUND.getValue(), HIGH_BOUND.getValue());
     }
 }
