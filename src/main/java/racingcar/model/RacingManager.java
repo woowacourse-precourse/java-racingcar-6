c22df9e19ce4d8adf77d6bb848c12722f9b8e801
Choo <choo000407@naver.com>
2023-11-01 15:57:51 +0900

    feat(RacingManager): `이동 시도 횟수` 카운트 기능, `이동 가능 여부` 결정 기능 구현

--- src/main/java/racingcar/model/RacingManager.java
+++ src/main/java/racingcar/model/RacingManager.java
@@ -10,6 +10,14 @@ public class RacingManager {
         this.tryCount = tryCount;
     }
 
+    public void countMove() {
+        tryCount++;
+    }
+
+    public boolean canMove() {
+        return tryCount > 0;
+    }
+
     private void validateTryCount(int tryCount) {
         if (tryCount <= 0) {
             throw new IllegalArgumentException(ErrorCode.NON_POSITIVE_TRY_COUNT.getMessage());
