f7a35692ae4973fbc8119a7d451788cb51cb4c61
opixxx <opix0306@naver.com>
2023-11-01 15:47:22 +0900

    feat(config): RacingCarGameConfig 클래스 추가로 어플리케이션 설정 관리
    RacingCarGameConfig 클래스는 어플리케이션에 필요한 객체들을 생성하고 제공하는 역할을 담당하도록 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,15 @@
 package racingcar;
 
-import racingcar.domain.CarManager;
+import racingcar.config.RacingCarGameConfig;
+
 import racingcar.controller.RacingCarGameController;
-import racingcar.util.RandomNumber;
-import racingcar.util.RandomNumberGenerator;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
 
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        RacingCarGameConfig config = new RacingCarGameConfig();
 
-        RandomNumberGenerator randomNumberGenerator = new RandomNumber();
-        CarManager carManager = new CarManager(randomNumberGenerator);
-
-        RacingCarGameController gameController = new RacingCarGameController(inputView, outputView, carManager);
+        RacingCarGameController gameController = config.gameController();
         gameController.play();
     }
 }
