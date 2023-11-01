9bb95d03d660b9a0b344cd8d4503fbeff4b96c4e
beomsic <qjatjr29@khu.ac.kr>
2023-11-01 13:39:17 +0900

    :sparkles: Feat: 문자열에 있는 공백을 없애는 기능 추가
    - 입력받은 문자열에 공백이 있는 경우 이를 삭제해주는 기능을 추가해 사용자가 잘못 입력한 경우에 대해 처리해준다.

--- src/main/java/racingcar/util/StringUtil.java
+++ src/main/java/racingcar/util/StringUtil.java
@@ -7,4 +7,8 @@ public class StringUtil {
     public static String[] splitByDelimiter(String inputString, String delimiter) {
         return inputString.split(delimiter);
     }
+
+    public static String removeWhitespace(String inputString) {
+        return inputString.replaceAll("\\s", "");
+    }
 }
