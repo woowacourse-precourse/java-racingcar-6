c630797ac5d3853f8abc8bd9b51d5e7a73a19821
dltjdn <hana40004@naver.com>
2023-11-01 20:25:10 +0900

    test(CarTest): 여러 그룹으로 나누어 테스트 구조화
    * 테스트 클래스 내에서 내부 클래스를 만들어 그룹화

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,81 +3,92 @@ package racingcar.model;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import racingcar.exception.ErrorMessage;
 import racingcar.mock.TestNumberGenerator;
 import racingcar.model.car.Car;
 import racingcar.model.car.CarDTO;
 
 class CarTest {
+    @Nested
+    class moveOneStepTest {
+        @Test
+        void 전진_테스트() {
+            // given
+            Car car = new Car("name", new TestNumberGenerator(5));
 
-    @Test
-    void 전진_테스트() {
-        // given
-        Car car = new Car("name", new TestNumberGenerator(5));
+            // when
+            car.moveOneStep();
 
-        // when
-        car.moveOneStep();
+            // then
+            assertThat(car.getStep()).isEqualTo(1);
+        }
 
-        // then
-        assertThat(car.getStep()).isEqualTo(1);
+        @Test
+        void 전진_안함_테스트() {
+            // given
+            Car car = new Car("name", new TestNumberGenerator(3));
+
+            // when
+            car.moveOneStep();
+
+            // then
+            assertThat(car.getStep()).isEqualTo(0);
+        }
     }
 
-    @Test
-    void 전진_안함_테스트() {
-        // given
-        Car car = new Car("name", new TestNumberGenerator(3));
+    @Nested
+    class toCarDTOTest {
+        @Test
+        void CarDTO_변환_테스트() {
+            // given
+            Car car = new Car("name", new TestNumberGenerator(5));
 
-        // when
-        car.moveOneStep();
+            // when
+            CarDTO carDTO = car.toCarDTO();
 
-        // then
-        assertThat(car.getStep()).isEqualTo(0);
+            // then
+            assertThat(carDTO.name()).isEqualTo("name");
+            assertThat(carDTO.step()).isEqualTo(0);
+        }
     }
 
-    @Test
-    void CarDTO_변환_테스트() {
-        // Arrange
-        Car car = new Car("name", new TestNumberGenerator(5));
 
-        // Act
-        CarDTO carDTO = car.toCarDTO();
+    @Nested
+    class compareTest {
+        @Test
+        void 전진_횟수_비교_참_테스트() {
+            // given
+            Car car = new Car("name", new TestNumberGenerator(5));
 
-        // Assert
-        assertThat(carDTO.name()).isEqualTo("name");
-        assertThat(carDTO.step()).isEqualTo(0);
+            // when
+            boolean isSame = car.compare(0);
+
+            // then
+            assertThat(isSame).isTrue();
+        }
+
+        @Test
+        void 전진_횟수_비교_거짓_테스트() {
+            // given
+            Car car = new Car("테스트차", new TestNumberGenerator(5));
+
+            // when
+            boolean isSame = car.compare(1);
+
+            // then
+            assertThat(isSame).isFalse();
+        }
     }
 
-    @Test
-    void 전진_횟수_비교_참_테스트() {
-        // given
-        Car car = new Car("name", new TestNumberGenerator(5));
-
-        // when
-        boolean isSame = car.compare(0);
-
-        // then
-        assertThat(isSame).isTrue();
-    }
-
-    @Test
-    void 전진_횟수_비교_거짓_테스트() {
-        // given
-        Car car = new Car("테스트차", new TestNumberGenerator(5));
-
-        // when
-        boolean isSame = car.compare(1);
-
-        // then
-        assertThat(isSame).isFalse();
-    }
-
-    @Test
-    void 이름_길이가_5_초과할때_에러_발생() {
-        assertThatThrownBy(() -> new Car("LongName", new TestNumberGenerator(5)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름은 5자 이하여야 합니다.");
+    @Nested
+    class createCarTest {
+        @Test
+        void 이름_길이가_5_초과할때_에러_발생() {
+            assertThatThrownBy(() -> new Car("LongName", new TestNumberGenerator(5)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("이름은 5자 이하여야 합니다.");
+        }
     }
 
 
