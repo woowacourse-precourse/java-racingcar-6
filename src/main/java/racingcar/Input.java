5f7a05935f6b039d17f9bc669c0be54fe91c0715
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 20:43:24 +0900

    feat(Input): 경주할 자동차 이름을 입력받고 유효한지 검사하는 기능 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
 
     private final Output output = new Output();
@@ -13,15 +17,46 @@ public class Input {
     }
 
     // 경주할 자동차 이름을 입력받습니다.
-    public String getInputCarNames() {
+    public List<String> getInputCarNames() {
         output.promptForCarName();
-        return getPlayerInput();
+        String playerInput = getPlayerInput();
+        return validateCarNames(playerInput);
     }
 
-    // TODO : 자동차 이름을 유효한지 검사합니다.
+    // 자동차 이름을 유효한지 검사합니다.
+    private List<String> validateCarNames(String input) {
+        String[] carNames = input.split(delimter);
 
+        if (carNames.length < 2) {
+            Exception.invalidCarCount();
+        }
+
+        for (String carName : carNames) {
+            String trimmedCarName = carName.trim();
+            if (trimmedCarName.isEmpty() || trimmedCarName.length() < 1 || trimmedCarName.length() > 5) {
+                Exception.invaildNameLength();
+            }
+        }
+
+        List<String> uniqueCarNames = Arrays.stream(carNames)
+                .map(String::trim)
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (uniqueCarNames.size() != carNames.length) {
+            Exception.notUniqueCar();
+        }
+
+        return uniqueCarNames;
+    }
 
     // TODO : 시도할 회수를 입력받습니다.
+    public int getInputTryCount()
+    {
+        output.promptForTryCount();
+        String playerInput = getPlayerInput();
+        return Integer.parseInt(playerInput);
+    }
 
     // TODO : 시도할 회수가 유효한지 검사합니다.
 }
