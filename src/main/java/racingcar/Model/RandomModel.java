f707629111bdd743ef428d004829c06f8a944da6
yookyung Choi <ykc131719@gmail.com>
2023-11-01 09:06:06 +0900

    refactor: randomModel에서 숫자들 상수로 바꿔주기

--- src/main/java/racingcar/Model/RandomModel.java
+++ src/main/java/racingcar/Model/RandomModel.java
@@ -4,13 +4,17 @@ package racingcar.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomModel {
+    static final Integer NUMBER_RANGE_ZERO = 0;
+    static final Integer NUMBER_RANGE_LAST = 9;
+    static final Integer CONDITION_FOR_TRUE = 4;
+
     public static boolean checkRandomNumber(){
-       if(makeRandomNumber() >= 4)
+       if(makeRandomNumber() >= CONDITION_FOR_TRUE)
            return true;
        return false;
     }
     public static Integer makeRandomNumber(){
-        Integer rands = Randoms.pickNumberInRange(0,9);
+        Integer rands = Randoms.pickNumberInRange(NUMBER_RANGE_ZERO,NUMBER_RANGE_LAST);
         return rands;
     }
 
