2774a736e7d0e7bfab21983de1415f08e70d3623
DonghaOh <donghadarwin@naver.com>
2023-11-01 16:51:44 +0900

    feat: 이름 리스트로 자동차 리스트 생성 - CarList 클래스 생성 - CarList 생성자

---
+++ src/main/java/racingcar/model/CarList.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class CarList {
+    List<Car> carList;
+    CarList(List<String> nameList){
+        for (String name : nameList) {
+            carList.add(new Car(name));
+        }
+    }
+}
