eca838c02729863fb68e85cbaf7f934bf8b15659
kwonjiyun <ziyun1612@ewhain.net>
2023-11-01 14:28:47 +0900

    feat : 게임 실행 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
+import racingcar.utils.InputReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingService racingService = new RacingService();
+        InputReader inputReader = new InputReader();
+        RacingController racingController = new RacingController(racingService, inputReader);
+        racingController.start();
     }
 }
