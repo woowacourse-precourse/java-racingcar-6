d9cb43d717890785f99781270595640fb4e60070
minkyeong <stl9955@naver.com>
2023-11-01 21:22:16 +0900

    Feat(GameInterface): getCarNames 메서드 구현

--- src/main/java/racingcar/GameInterface.java
+++ src/main/java/racingcar/GameInterface.java
@@ -1,5 +1,14 @@
 package racingcar;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class GameInterface {
     public void play(){}
+
+    private static String[] getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarName = readLine();
+        String[] inputCarNames = inputCarName.split(",");
+        checkCarNames(inputCarNames);
+        return inputCarNames;
+    }
 }
