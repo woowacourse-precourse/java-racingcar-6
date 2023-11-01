7c1598068b23f143dfd122d0cbb3746ba24b1afe
DonghaOh <donghadarwin@naver.com>
2023-11-01 15:22:46 +0900

    feat: 이름 입력 문구 출력 불러오기 - InputController 클래스 생성

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,5 +1,11 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import static racingcar.view.InputView.nameInputPrint;
+
 public class InputController {
+    public static void nameInput(){
+        nameInputPrint();
+    }
 
 }
