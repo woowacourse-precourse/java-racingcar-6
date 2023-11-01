4bfe364844f2d66e7693b8423b575d46d1f30683
seungheyon <fhzktm@naver.com>
2023-11-01 22:01:52 +0900

    테스트 주석작성 및 테스트메서드 이름 변경

--- src/test/java/racingcar/RacingCarMethodTest.java
+++ src/test/java/racingcar/RacingCarMethodTest.java
@@ -15,43 +15,53 @@ public class RacingCarMethodTest {
     @Test
     void isCarnameTooLong_메서드에_길이제한을_초과한_입력이_있을_시_true를_반환(){
 
+        //Arrange
         List<String> inputList = List.of("pobi", "woni", "javajigi");
         int inputMaxLength = 5;
 
+        //Act
         var result = Application.isCarnameTooLong(inputList, inputMaxLength);
 
+        //Assert
         assertThat(result).isEqualTo(true);
     }
 
     @Test
     void determineGoOrStop_메서드에_4_이상의_값이_입력될_경우_key값의_value를_1만큼_증가(){
 
+        //Arrange
         Map<String, Integer> carMap = new HashMap<>();
         String testCar = "testCar";
         carMap.put(testCar, 1);
 
+        //Act
         Application.determineGoOrStop(carMap, testCar, 5);
         var result = carMap.get(testCar);
 
+        //Assert
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     void determineGoOrStop_메서드에_3_이하의_값이_입력될_경우_key값의_value를_유지(){
 
+        //Arrange
         Map<String, Integer> carMap = new HashMap<>();
         String testCar = "testCar";
         carMap.put(testCar, 1);
 
+        //Act
         Application.determineGoOrStop(carMap, testCar, 2);
         var result = carMap.get(testCar);
 
+        //Assert
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void printRacingState_메서드는_전달받은_Map의_key값과_value만큼의_전진을_출력(){
 
+        //Arrange
         Map<String, Integer> carMap = new HashMap<>();
         String testCar = "testCar";
         carMap.put(testCar, 4);
@@ -59,39 +69,47 @@ public class RacingCarMethodTest {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
+        //Act
         Application.printRacingState(carMap, testCar);
 
         String expectedOutput = "testCar : ----";
         String result = outContent.toString().trim();
 
+        //Assert
         assertThat(result).isEqualTo(expectedOutput);
     }
 
     @Test
-    void decideWinners_메서드는_전달받은_Map에서_최대값을_가진_key를_String으로_반환(){
+    void decideWinners_메서드는_전달받은_Map에서_최대값을_가진_key의_리스트를_String으로_변환하여_반환(){
 
+        //Arrange
         Map<String, Integer> carMap = new HashMap<>();
         carMap.put("firstCar", 5);
         carMap.put("secondCar", 4);
         carMap.put("thirdCar", 3);
         carMap.put("fourthCar", 1);
 
+        //Act
         String result = Application.decideWinners(carMap);
 
+        //Assert
         assertThat(result).isEqualTo("firstCar");
     }
 
     @Test
     void decideWinners_메서드는_최대값을_가진_key가_여러개일_경우_알파벳_순서의_리스트를_String으로_변환하여_반환(){
 
+        //Arrange
         Map<String, Integer> carMap = new HashMap<>();
         carMap.put("firstCar", 5);
         carMap.put("anotherFirstCar", 5);
         carMap.put("thirdCar", 3);
         carMap.put("fourthCar", 1);
 
+        //Act
         String result = Application.decideWinners(carMap);
 
+        //Assert
         assertThat(result).isEqualTo("anotherFirstCar, firstCar");
     }
 }
