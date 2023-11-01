4b8a63b709912d349df9806925eee7e2e753ba6d
방준호 <yureka0920@naver.com>
2023-11-01 13:35:14 +0900

    RacingCar_객체_생성 test,

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,21 +1,25 @@
 package racingcar;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
 
 public class RacingCarTest {
     @Test
     void RacingCar_객체_생성() {
         List<RacingCar> result = new ArrayList<>();
-        RacingCar racingCar1 = new RacingCar("junho", 0);
-        RacingCar racingCar2 = new RacingCar("bang", 0);
-        RacingCar racingCar3 = new RacingCar("kim", 0);
+        RacingCar racingCar1 = new RacingCar("junho", "");
+        RacingCar racingCar2 = new RacingCar("bang", "");
+        RacingCar racingCar3 = new RacingCar("kim", "");
         result.add(racingCar1);
         result.add(racingCar2);
         result.add(racingCar3);
@@ -23,4 +27,20 @@ public class RacingCarTest {
         assertThat(result).extracting("name", "count")
                 .contains(tuple("junho", 0));
     }
+
+    @Test
+    void 입력된값이없을경우_예외처리(){
+        String userInput = "";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //NullPointerException Throw하는 작업을 작성
+            List<String> userInputList = new ArrayList<>();
+            if(userInput==""){
+                throw new IllegalArgumentException("입력된 값이 없습니다. 어플리케이션을 종료합니다.");
+            }
+            for (String s : userInput.split(",")) {
+                userInputList.add(s);
+            }
+        });
+    }
+
 }
