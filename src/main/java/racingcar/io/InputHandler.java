93374958df63f4bf7c082b2176ca146d190df651
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 21:21:57 +0900

    fix : console로 입력 변경

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -1,17 +1,16 @@
 package racingcar.io;
 
 import java.util.Scanner;
-
+import camp.nextstep.edu.missionutils.Console;
 public class InputHandler {
     private static final Scanner scanner = new Scanner(System.in);
-
     public static String getCarNames(String message){
         System.out.print(message);
-        return scanner.nextLine();
+        return Console.readLine();
     }
     public static int getTryCount(String message){
         System.out.print(message);
-        return scanner.nextInt();
+        return Integer.parseInt(Console.readLine());
     }
 
 }
