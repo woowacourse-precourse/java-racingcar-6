03fe49984d495ca5d049660235056596022e9da3
howudong <tjdvy963@naver.com>
2023-11-01 13:09:08 +0900

    refactor : parameter로 대체되지 않은 문자열들 대체

--- src/main/java/racingcar/controller/PlayController.java
+++ src/main/java/racingcar/controller/PlayController.java
@@ -5,6 +5,8 @@ import racingcar.view.outputview.PlayOutputView;
 
 import java.util.Map;
 
+import static racingcar.view.Parameter.GAME_COUNT;
+
 final class PlayController implements Controller {
     private final PlayOutputView playOutputView;
     private final CarService carService;
@@ -16,13 +18,13 @@ final class PlayController implements Controller {
 
     @Override
     public void run(Map<String, String> parameter, Map<String, Object> model) {
-        if (model.get("gameCount") instanceof Integer) {
+        if (model.get(GAME_COUNT) instanceof Integer) {
             playRacing(model);
         }
     }
 
     private void playRacing(Map<String, Object> model) {
-        int gameCount = (Integer) model.get("gameCount");
+        int gameCount = (Integer) model.get(GAME_COUNT);
 
         for (int i = 0; i < gameCount; i++) {
             carService.moveAllCarByRandom();
