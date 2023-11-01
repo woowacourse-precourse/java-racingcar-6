6447baeed5a496074bebc35c49adda9edbab210a
coli-geonwoo <kkwoo001021@naver.com>
2023-11-01 10:39:51 +0900

    feat(TurnValidiator) : 턴 숫자 형식검사 메소드 추가

--- src/main/java/domain/InputView/TurnValidiator.java
+++ src/main/java/domain/InputView/TurnValidiator.java
@@ -1,9 +1,12 @@
 package domain.InputView;
 
+import java.util.regex.Pattern;
+
 import static message.ErrorMessages.NEGATIVE_INPUT_ERROR;
+import static message.ErrorMessages.WRONG_INPUT_ERROR;
 
 public class TurnValidiator {
-
+    public static final Pattern turnNumPattern = Pattern.compile("^[1-9][0-9]*$");
     public TurnValidiator(String num){
         int newnum= checkNumber(num);
         if(newnum<0){
@@ -15,6 +18,7 @@ public class TurnValidiator {
     private int checkNumber(String turnNum)
     {
         try {
+            matches(turnNum);
             return Integer.parseInt(turnNum);
 
         }
@@ -22,6 +26,14 @@ public class TurnValidiator {
             System.out.println(e.getMessage());
             return -1;
         }
+    }
+    private void matches(String turnNum)
+    {
+        if(!turnNumPattern.matcher(turnNum).matches())
+        {
+            throw new IllegalArgumentException(WRONG_INPUT_ERROR);
+        }
+
     }
 
 
