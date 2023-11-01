59f8cb2597b8208371abc90195e7cc32f1ccd27f
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 11:04:30 +0900

    [feat] Controller 작성 - Input 값 유효성 검사 후 객체 생성

--- src/main/java/racingcar/Controller/RacingCarGame.java
+++ src/main/java/racingcar/Controller/RacingCarGame.java
@@ -1,4 +1,28 @@
 package racingcar.Controller;
 
-public class RacingCarGame {
+import java.util.List;
+import racingcar.Domain.Cars;
+import racingcar.Domain.Race;
+import racingcar.Validator.CarNameValidator;
+import racingcar.Validator.TrialCountValidator;
+import racingcar.View.InputView;
+
+public class RacingCarController {
+
+    public void play() {
+        String carNamesInput = InputView.inputCarName();
+        String trialCountInput = InputView.inputRaceGameCount();
+
+        try {
+            List<String> carNames = CarNameValidator.validateCarNames(carNamesInput);
+            int trialCount = TrialCountValidator.validateTrialCount(trialCountInput);
+
+            Cars cars = new Cars(carNames);
+            Race race = new Race(cars.getCarList(), trialCount);
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("오류: " + e.getMessage());
+        }
+    }
 }
