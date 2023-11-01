496964b2b768ddab9f3d060d3eb15443fd08ce3d
jungyunmok <jungyunmok@gmail.com>
2023-11-01 14:24:05 +0900

    Test : 이름 입력 예외처리 테스트

---
+++ src/test/java/racingcar/RacingcarTest.java
@@ -0,0 +1,54 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RacingcarTest extends NsTest {
+    @Test
+    void 이름_공백입력_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,  ,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_5자초과_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobiwoni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_처음쉼표_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_중간쉼표_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_마지막쉼표_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
