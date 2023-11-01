33da4b8ee259a01b7508672f640985f060292223
hellomatia <shuaigejp80@gmail.com>
2023-11-01 21:22:04 +0900

    refactor(util): 이름 검증 메서드명 수정
    - 이름의 개수가 유효한지 검증하는 메서드 isOnlyOne을 BelowNumberOfNames로 변경하여 명확한 뜻을 내포할 수 있도록 변경.

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -6,20 +6,21 @@ import java.util.Objects;
 public class Validation {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
-    private static final int MIN_NUMBER_OF_RACES = 1;
     private static final int MIN_NUMBER_OF_NAMES = 2;
+    private static final int MIN_NUMBER_OF_RACES = 1;
 
-    private static final String OVER_RANGE_LENGTH_MESSAGE = "5자리 이하 이름를 입력해주세요.";
+    private static final String OVER_RANGE_LENGTH_NAME_MESSAGE = "5자리 이하 이름를 입력해주세요.";
     private static final String NOT_UNIQUE_NAME_MESSAGE = "중복되지 않는 이름을 입력해주세요.";
     private static final String EMPTY_NAME_MESSAGE = "이름을 입력해주세요.";
-    private static final String ONLY_ONE_MESSAGE = "2개 이상의 이름을 입력해주세요.";
+    private static final String BELOW_NUMBER_OF_NAMES_MESSAGE = "2개 이상의 이름을 입력해주세요.";
+
     private static final String NOT_NUMBER_MESSAGE = "숫자를 입력해주세요.";
     private static final String BELOW_RANGE_NUMBER_MESSAGE = "1이상 숫자를 입력해주세요.";
     private static final String EMPTY_NAME = "";
 
     public static void validateCarNames(List<String> names) {
-        if (isOverRangeLength(names)) {
-            throw new IllegalArgumentException(OVER_RANGE_LENGTH_MESSAGE);
+        if (isOverRangeLengthName(names)) {
+            throw new IllegalArgumentException(OVER_RANGE_LENGTH_NAME_MESSAGE);
         }
         if (isNotUniqueName(names)) {
             throw new IllegalArgumentException(NOT_UNIQUE_NAME_MESSAGE);
@@ -27,12 +28,12 @@ public class Validation {
         if (isEmptyName(names)) {
             throw new IllegalArgumentException(EMPTY_NAME_MESSAGE);
         }
-        if (isOnlyOne(names)) {
-            throw new IllegalArgumentException(ONLY_ONE_MESSAGE);
+        if (isBelowNumberOfNames(names)) {
+            throw new IllegalArgumentException(BELOW_NUMBER_OF_NAMES_MESSAGE);
         }
     }
 
-    private static boolean isOverRangeLength(List<String> names) {
+    private static boolean isOverRangeLengthName(List<String> names) {
         return names.stream()
                 .anyMatch(name -> name.length() > MAX_CAR_NAME_LENGTH);
     }
@@ -48,7 +49,7 @@ public class Validation {
                 .anyMatch(name -> Objects.equals(name, EMPTY_NAME));
     }
 
-    private static boolean isOnlyOne(List<String> names) {
+    private static boolean isBelowNumberOfNames(List<String> names) {
         return names.size() < MIN_NUMBER_OF_NAMES;
     }
 
