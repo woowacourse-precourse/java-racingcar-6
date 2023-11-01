3bf08c04651cf26c4c42742b3839319b9f7ac8a6
lee-JunR <wns2610@icloud.com>
2023-11-01 17:30:50 +0900

    refactor : "output 클래스 매직넘버 추가 및 static 키워드 제거"
    코드의 가독성을 생각하여 매직넘버 사용
    객체의 인스턴스 통해 메소드를 호출는 것이 맞다고 생각하여 static 제거

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -3,16 +3,18 @@ package racingcar.view;
 import java.util.List;
 
 public class Output {
+  private static final String POSITION_SYMBOL = "-";
+  private static final String FINAL_WINNER_LABEL = "최종 우승자 : ";
 
   public void printCurrentPositions(List<String> carNames, List<Integer> carPositions) {
     for (int i = 0; i < carNames.size(); i++) {
-      System.out.println(carNames.get(i) + " : " + "-".repeat(carPositions.get(i)));
+      System.out.println(carNames.get(i) + " : " + POSITION_SYMBOL.repeat(carPositions.get(i)));
     }
     System.out.println();
   }
 
-  public static void printFinalWinners(List<String> winnerNames) {
+  public void printFinalWinners(List<String> winnerNames) {
     String winners = String.join(", ", winnerNames);
-    System.out.println("최종 우승자 : " + winners);
+    System.out.println(FINAL_WINNER_LABEL + winners);
   }
 }
