232f854611e8dd508365c1331db41b5f6ee58d22
prefer52 <tjsgh151@naver.com>
2023-11-01 11:14:40 +0900

    feat(ValidatorTest): 자동차 이름 분리 기능 테스트 추가

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+    private final Validator validator = new Validator();
+
+    @Test
+    @DisplayName("부적절한 commma 포함 문자열 예외 발생 테스트")
+    void isSplitByCommaTest() {
+        List<String> testCaseList = Arrays.asList(new String[]{",,", ",", " ", "a", ",b", "a,b,"});
+        for (String testCase : testCaseList) {
+            Assertions.assertThatThrownBy(() -> validator.validate(testCase, 0));
+        }
+    }
+
+    @Test
+    @DisplayName("부적절한 자동차의 이름 예외 발생 테스트")
+    void isValidLength(String[] string) {
+    }
+
+    @Test
+    @DisplayName("부적절한 시도 회수 예외 발생 테스트")
+    void isValidNumber(String string) {
+    }
+}
