f851ab42fd8da9befd2cc42ff994032b26690294
grax <grax@192.168.35.119>
2023-11-01 16:57:09 +0900

    feat: "create gostop() method"
    "랜덤넘버 4를 기준으로 이동하는 지 멈추는지 판단"

--- src/main/java/racingcar/domain/Judgement.java
+++ src/main/java/racingcar/domain/Judgement.java
@@ -1,12 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Judgement {
 	private static final int STANDARD_NUMBER = 4;
 	
-	public void goStop(int randomNumber) {
-		if (randomNumber >= STANDARD_NUMBER) {
-			System.out.println("-");
-			// TODO: 메서드 사용해서 구현하기 addDash()
+	public static void goStop(GameData data) {
+		for (int i = 0; i < data.getNumberList().size(); i++) {
+			if (data.getNumberList().get(i) >= STANDARD_NUMBER) {
+				data.getPositionList().set(i, (data.getPositionList().get(i) + 1)); // 원래 값에 1 추가   
+			}
 		}
 	}
+	
+
+	
 }
