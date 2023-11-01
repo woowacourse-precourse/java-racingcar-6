8d8609a482a5f3436933cc7d0077b53378bf7fd8
nhn1030 <shgusska12@gmail.com>
2023-11-01 23:13:23 +0900

    feat: testAddCar 정의 addCar 매서드 동작 검증

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 import racingcar.model.GameManager;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -17,32 +18,33 @@ class ApplicationTest extends NsTest {
     GameManager gameManager = GameManager.create();
 
 
-//    @Test
-//    void 전진_정지() {
-//        assertRandomNumberInRangeTest(
-//            () -> {
-//                run("pobi,woni", "1");
-//                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-//            },
-//            MOVING_FORWARD, STOP
-//        );
-//    }
-//
-//    @Test
-//    void 이름에_대한_예외_처리() {
-//        assertSimpleTest(() ->
-//                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-//                        .isInstanceOf(IllegalArgumentException.class)
-//        );
-//    }
+    @Test
+    void 전진_정지() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "1");
+                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+            },
+            MOVING_FORWARD, STOP
+        );
+    }
 
     @Test
-    public void testMoveAllCars() {
-        gameManager.addCar("pobi");
-        gameManager.addCar("woni");
-        gameManager.addCar("jun");
-        gameManager.moveAllCars();
-        assertEquals(3, gameManager.getCarPositions().size());
+    void 이름에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void testAddCar() {
+        GameManager gameManager = GameManager.create();
+        String carName = "pobi";
+
+        gameManager.addCar(carName);
+
+        assertEquals(carName, gameManager.getCarNames().get(0));
     }
 
 
