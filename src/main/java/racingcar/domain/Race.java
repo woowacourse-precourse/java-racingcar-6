752c036ed96865e199574b0b82a5b4f195ef6cd3
Subin <rla0010101@gmail.com>
2023-11-01 18:12:27 +0900

    Feat: 전반적인 게임 로직 구현 완료

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -3,16 +3,37 @@ package racingcar.domain;
 public class Race {
     private final Cars cars;
     private int runCount;
+    private int maxDistance;
+    private Cars winnerCars;
 
     public Race(Cars cars) {
         this.cars = cars;
     }
 
     public void run() {
+        execute();
+        calculateMaxDistance();
+        judgeWinner();
+        showWinner();
+    }
 
+    private void execute() {
+        cars.executeRound(runCount);
     }
 
     public void setRunCount(int runCount) {
         this.runCount = runCount;
+    }
+
+    private void calculateMaxDistance() {
+        maxDistance = cars.determineMaxDistance();
+    }
+
+    private void judgeWinner() {
+        winnerCars = this.cars.determineWinner(maxDistance);
+    }
+
+    private void showWinner() {
+        winnerCars.showBestDriver();
     }
 }
