0209d4ce3a342c6352906d8b5df4457f87306351
davemins <davemins4125@naver.com>
2023-11-01 17:39:21 +0900

    test: 레이스 작동 확인 테스트 작성 RaceController의 play() 메서드가 잘 작동하는지 확인하는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -0,0 +1,34 @@
+package racingcar.controller;
+
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Racer;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RaceControllerTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void play_메서드_작동_확인() {
+        // Given
+        // When
+        // Then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        RaceController raceController = new RaceController();
+        raceController.play();
+    }
+}
