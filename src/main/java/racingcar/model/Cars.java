01c8de4e8257a4935392c7ec68e2bcceb129a17c
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-01 21:54:28 +0900

    refactor: 이름을 separator 에 따라 split 하는 메소드 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import racingcar.utils.Validation;
 
 public class Cars implements Iterable<Car> {
     private final List<Car> carList;
+    private static final String SEPARATOR = ",";
 
     @Override
     public Iterator<Car> iterator() {
@@ -15,11 +17,16 @@ public class Cars implements Iterable<Car> {
 
     public Cars(String input) {
         List<Car> list = new ArrayList<>();
-        String[] splitInput = input.split(",");
+        String[] splitInput = splitBy(input, SEPARATOR);
+        Validation.isDistinctNames(splitInput);
         for (String name : splitInput) {
             list.add(new Car(name));
         }
         this.carList = list;
+    }
+
+    public static String[] splitBy(String string, String sep) {
+        return string.split(sep);
     }
 
     public int size() {
