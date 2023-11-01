7ba8673bb3c06c48b45095da3528703739cceb59
senzoo-oh <osj3418@gmail.com>
2023-11-01 20:45:05 +0900

    refactor: change RacingCar class structure
    add field
    - moveBar of StringBuilder type to show gauge
    change access modifier
    - private to package-private(default)
    add method
    - showMoveBar() method return moveBar field

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -5,18 +5,24 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RacingCar {
     String name;
     int moveCount;
+    StringBuilder moveBar = new StringBuilder();
 
     public RacingCar(String name) {
         this.name = name;
     }
-    private int rollDice() {
+    int rollDice() {
         int diceNumber = Randoms.pickNumberInRange(0,9);
         return diceNumber;
     }
-    private void move(int diceNumber) {
-        if (4 <= diceNumber) moveCount++;
+    void move(int diceNumber) {
+        if (4 <= diceNumber) {
+            moveBar.append("-");
+        }
     }
-    public int showMoveCount() {
+    int showMoveCount() {
         return moveCount;
+    }
+    String showMoveBar() {
+        return moveBar.toString();
     }
 }
