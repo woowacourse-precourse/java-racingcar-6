1a79084c0e3fc92f4498c8e1dd122250f4c04f3b
tangba484 <tangba484@naver.com>
2023-11-01 23:52:22 +0900

    Feat: 예외 상황 처리 코드 추가

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -3,6 +3,8 @@ package racingcar.io;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.Cars;
 
+import java.util.Arrays;
+
 public class Input {
     public static String CAR_INPUT_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static String ROUND_INPUT_MSG = "시도할 회수는 몇회인가요?";
