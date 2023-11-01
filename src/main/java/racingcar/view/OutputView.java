79e132ef54c5ded274c70cfe1fedb40e6ecf456b
jooda00 <dragon2000a@gmail.com>
2023-11-01 20:23:40 +0900

    refactor(OutputView): 자동차 움직임 및 "실행 결과" 문구 OutputView에서 실행

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,9 +3,8 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-    public void showResult() {
-        System.out.println("실행 결과");
-    }
+    private static final int STANDARD_NUMBER = 4;
+    private static final String MOVING_INDICATORS = "-";
 
     public String convertListToString(List<String> winnerList) {
         return String.join(", ", winnerList);
@@ -14,5 +13,13 @@ public class OutputView {
     public void showWinners(List<String> winnerList) {
         String winners = convertListToString(winnerList);
         System.out.print("최종 우승자 : " + winners);
+    }
+
+    public void showResult(String car, int movingCount) {
+        System.out.println(car + " : " + MOVING_INDICATORS.repeat(movingCount));
+    }
+
+    public void showDefaultResultSentence() {
+        System.out.println("\n실행 결과");
     }
 }
