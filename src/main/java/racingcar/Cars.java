02bfe949ebd948279d5475e47db721a69b1a80fb
jagarchi <relong1026@naver.com>
2023-11-01 19:35:26 +0900

    자동차 이름이 5글자 초과 했을시 오류 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -7,7 +7,16 @@ public class Cars {
 
     public Cars() {
         String carNames = Console.readLine();
+        nameLengthError(carNames);
         this.carNames = carNames.split(",");
+    }
+    public void nameLengthError(String carNames){  //길이가 5글자 이상일 때
+        String[] carNamesArray = carNames.split(",");
+        for (String oneCarName : carNamesArray) {
+            if (oneCarName.length() > 5) {
+                throw new IllegalArgumentException(ExceptionError.nameLengthError);
+            }
+        }
     }
     
 }
---
+++ src/main/java/racingcar/ExceptionError.java
@@ -0,0 +1,5 @@
+package racingcar;
+
+public class ExceptionError {
+    final static String nameLengthError = "[ERROR] 5글자를 초과하였습니다.";
+}
