a2f46f05a09988d7d7ce7eaf46d831a648a66d78
imhungry2 <sjnnj@naver.com>
2023-11-01 17:24:56 +0900

    model - car 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        // 생성자 로직
+    }
+
+    public void move(int randomValue) {
+        // 이동 로직
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
