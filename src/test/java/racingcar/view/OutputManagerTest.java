37a1a9bad078c54a7146041f4390b2c9c6852d42
strongcookdas <goodjav141592@gmail.com>
2023-11-01 15:01:17 +0900

    test : 최종 우승자들을 출력하는 메서드인 printWinners 메서드 테스트 코드 작성

--- src/test/java/racingcar/view/OutputManagerTest.java
+++ src/test/java/racingcar/view/OutputManagerTest.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
@@ -37,6 +40,23 @@ public class OutputManagerTest {
                 "woni : ----\n" +
                 "jun : ---";
         expectedOutput = expectedOutput.replace("\n", System.lineSeparator());
+        assertThat(actualOutput).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void printWinners_success() {
+        // Given
+        List<String> winners = new ArrayList<>();
+        winners.add("pobi");
+        winners.add("woni");
+
+        // When
+        outputManager.printWinners(winners);
+        System.setOut(System.out);
+
+        // Then
+        String actualOutput = outputStream.toString().trim();
+        String expectedOutput = "최종 우승자 : pobi, woni";
         assertThat(actualOutput).isEqualTo(expectedOutput);
     }
 }
