2010a9952d8c6781513c3b0482891bae9a20e0fb
ghsyn <swanshawn715@gmail.com>
2023-11-01 20:30:30 +0900

    test: Implement round is integer

--- src/test/java/racingcar/exception/InputRoundExceptionTest.java
+++ src/test/java/racingcar/exception/InputRoundExceptionTest.java
@@ -1,17 +1,34 @@
 package racingcar.exception;
 
-import org.junit.jupiter.api.BeforeEach;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class InputRoundExceptionTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+class InputRoundExceptionTest extends NsTest {
 
     @Test
     void 입력된_횟수가_정수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "not int"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "숫자가 아님"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "String"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
