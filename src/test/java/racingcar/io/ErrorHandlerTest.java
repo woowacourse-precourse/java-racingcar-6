bcf4cbc72426fbfb41cf57a5adb15b500f2a0ae1
Hercules <hyukkyulee@alumni.emory.edu>
2023-11-01 15:48:10 +0900

    test: ErrorHandlerTest
    test for checkUnique()

--- src/test/java/racingcar/io/ErrorHandlerTest.java
+++ src/test/java/racingcar/io/ErrorHandlerTest.java
@@ -2,6 +2,7 @@ package racingcar.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import org.junit.jupiter.api.Test;
 
 class ErrorHandlerTest {
@@ -10,5 +11,14 @@ class ErrorHandlerTest {
     @Test
     void 입력값은_5이하여야_합니다() {
         assertThrows(IllegalArgumentException.class, () -> errorHandlerTest.checkLength("123456"));
+    }
+
+    @Test
+    void 입력값은_고유해야_합니다() {
+        HashMap<String, Integer> playingStatus = new HashMap<>() {{
+            put("scott", 0);
+            put("123", 0);
+        }};
+        assertThrows(IllegalArgumentException.class, () -> errorHandlerTest.checkUnique("123", playingStatus));
     }
 }
