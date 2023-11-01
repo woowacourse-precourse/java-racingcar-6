01fe1723fcef0222ab2689292c88e1cfeb6d469b
randallkk <randall.sw@gmail.com>
2023-11-01 21:06:38 +0900

    test: 이름 중복 테스트 제외

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,17 +3,13 @@ package racingcar.domain;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.Application;
 
 public class CarTest {
 
     @ParameterizedTest
-    @Test
-    @ValueSource(strings = {"pobi,javaji", "pobi,pobi", ""})
+    @ValueSource(strings = {"javaji", ""})
     void 이름에_대한_예외_처리(String name) {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> new Car(name))
