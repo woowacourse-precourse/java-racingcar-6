ab1c4c7f88254a904c0310f276827375e053da4a
주은 <00-10-21@naver.com>
2023-11-01 22:49:11 +0900

    Implement random value generation

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,2 +1,12 @@
-package racingcar;public class Game {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Game {
+
+    public static int generateRandomValue() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+
 }
