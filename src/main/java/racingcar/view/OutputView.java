840ddcb7d5b6939c95fe5f470e32b5b96cedc265
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-01 17:44:34 +0900

    refactor : OutputVeiw 메소드 static변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,27 +4,27 @@ import java.util.List;
 
 public class OutputView {
 
-    public void printCarNameInputMessage(){
+    public static void printCarNameInputMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printRacingCountInputMessage(){
+    public static void printRacingCountInputMessage(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void printRacingResultStartMessage(){
-        System.out.println("실행 결과");
+    public static void printRacingResultStartMessage(){
+        System.out.println("\n실행 결과");
     }
 
-    public void printAllCarNowPosition(List<String> carPositionStringList){
-        for(String carPositionString : carPositionStringList){
-            System.out.println(carPositionString);
+    public static void printAllCarNowDistance(List<String> carDistanceStringList){
+        for(String carDistanceString : carDistanceStringList){
+            System.out.println(carDistanceString);
         }
-        System.out.println();
+        System.out.println("\n");
 
     }
 
-    public void printWinnerMessage(List<String> winnerList){
+    public static void printWinnerMessage(List<String> winnerList){
         String winners = String.join(",", winnerList);
         System.out.printf("최종 우승자 : %s", winners);
     }
