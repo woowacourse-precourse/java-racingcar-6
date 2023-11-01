20e5ba0e5003cd99f48c9d0d0605938971be4f94
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-01 13:57:58 +0900

    test(winnerChecker): 최대 위치가 0인 경우 테스트에서 0을 상수로 추출

--- src/main/java/racingcar/WinnerChecker.java
+++ src/main/java/racingcar/WinnerChecker.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class WinnerChecker {
     private final String NO_MOVEMENT_DURING_RACE = "경주 중 이동한 자동차가 없습니다.";
     private final String CANNOT_FIND_WINNER = "우승자를 찾을 수 없습니다.";
+    private final Integer INITIAL_POSITION = 0;
 
     public List<String> getWinnerList(List<Car> carList) {
-        return carList.stream()
-                .filter(car -> car.getPosition() == getMaxPosition(carList))
-                .map(Car::getName)
-                .toList();
+        return carList.stream().filter(car -> car.getPosition() == getMaxPosition(carList)).map(Car::getName).toList();
     }
 
     private int getMaxPosition(List<Car> carList) {
@@ -19,13 +17,12 @@ public class WinnerChecker {
                 .max()
                 .orElseThrow(() -> new IllegalArgumentException(CANNOT_FIND_WINNER));
 
-        validateMaxPosition(maxPosition);
-
+        validateMaxIsInitialPosition(maxPosition);
         return maxPosition;
     }
 
-    private void validateMaxPosition(int maxPosition) {
-        if (maxPosition == 0) {
+    private void validateMaxIsInitialPosition(int maxPosition) {
+        if (maxPosition == INITIAL_POSITION) {
             throw new IllegalArgumentException(NO_MOVEMENT_DURING_RACE);
         }
     }
