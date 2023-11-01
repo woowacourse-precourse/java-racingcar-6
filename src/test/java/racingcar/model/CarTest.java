2f1a11a5fee2eed4e6b483270734bed7f3d1615c
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 22:39:47 +0900

    test(CarTest): Car 객체 비교 메소드에 대한 단위 테스트 작성
    1. isNotLoseTo_테스트
    2. compareTo_테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -98,4 +98,56 @@ class CarTest {
 
         assertEquals(car.toString(), "픽업 트럭 : ---");
     }
+
+    @Test
+    void isNotLoseTo_테스트() {
+        Car c1 = new Car(new Name("a"));
+        Car c2 = new Car(new Name("b"));
+        Car c3 = new Car(new Name("c"));
+
+        proceedNTimes(c1, 0);
+        proceedNTimes(c2, 1);
+        proceedNTimes(c3, 1);
+
+        assertTrue(c2.isNotLoseTo(c1));
+        assertTrue(c2.isNotLoseTo(c3));
+        assertTrue(c3.isNotLoseTo(c2));
+    }
+
+    @Test
+    void compareTo_테스트() {
+        Car c1 = new Car(new Name("a"));
+        Car c2 = new Car(new Name("b"));
+        Car c3 = new Car(new Name("c"));
+        Car c4 = new Car(new Name("d"));
+
+        proceedNTimes(c1, 0);
+        proceedNTimes(c2, 1);
+        proceedNTimes(c3, 1);
+        proceedNTimes(c4, 2);
+
+        // 자기 자신과 비교
+        assertEquals(0, c1.compareTo(c1));
+
+        // 다른 객체와 동일성 비교
+        assertEquals(0, c2.compareTo(c3));
+
+        // 대칭성
+        assertEquals(c1.compareTo(c2), c2.compareTo(c1) * (-1));
+
+        // 추이성
+        if (
+                (c4.compareTo((c3)) > 0)
+                        && (c3.compareTo(c2) == 0)
+                        && c2.compareTo(c1) > 0
+        ) {
+            assertTrue(c4.compareTo(c2) > 0);
+            assertTrue(c4.compareTo(c1) > 0);
+        }
+
+        // 반사성
+        if (c2.compareTo(c3) == 0) {
+            assertTrue((c2.compareTo(c1) * c3.compareTo(c1)) > 0);
+        }
+    }
 }
