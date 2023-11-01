4e414618f94619174de8a35b8b7f7ca96faeaf5a
suin <parksuuuun@gmail.com>
2023-11-01 12:40:16 +0900

    feat: 사용자로부터 자동차 이름 입력 받기 위한 기능

--- src/main/java/racingcar/UserInputView.java
+++ src/main/java/racingcar/UserInputView.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInputView {
 
     private static final String WELCOME_MESSAGE = "환영합니다! 지금부터 자동차 경주 게임을 시작합니다.";
@@ -7,5 +12,28 @@ public class UserInputView {
     private static final String ALERT_MESSAGE = " (주의사항: 본 게임은 최대 3명까지 이용 가능하며 이름은 쉼표(,)로 구분하고 "
             + "5자 이하의 이름으로 입력할 것)";
     private static final String TRY_TIMES_MESSAGE = "시도할 횟수는 몇 회입니까?";
+
+    private List<String> carNames = new ArrayList<>();
+
+    public List<String> getCarNames() {
+        System.out.println(WELCOME_MESSAGE);
+        System.out.print(INPUT_CAR_NAMES_MESSAGE);
+        System.out.println(ALERT_MESSAGE);
+        String userInput = Console.readLine();
+
+        // 예외 발생 여부 확인
+
+        return splitCarNames(userInput);
+    }
+
+    private List<String> splitCarNames(String userInput) {
+        String[] nameArray = userInput.split(",");
+        List<String> carNames = new ArrayList<>();
+
+        for (String name : nameArray) {
+            carNames.add(name.replaceAll("\\s", "").trim());
+        }
+        return carNames;
+    }
 
 }
