fc32a6f9a882cf4397e3af8b3fb937d53051b345
Minyongggg <naminyong97@gmail.com>
2023-11-01 17:59:09 +0900

    feat: 게임 세부 옵션 상수 값 클래스 추가

--- src/main/java/racingcar/constant/GameOptions.java
+++ src/main/java/racingcar/constant/GameOptions.java
@@ -1,4 +1,11 @@
 package racingcar.constant;
 
 public class GameOptions {
+    public static final String DELIMITER_LABEL = "쉼표";
+    public static final String DELIMITER = ",";
+    public static final Integer MAX_NAME_LENGTH = 5;
+
+    public static final Integer MIN_RANDOM_NUMBER = 0;
+    public static final Integer MAX_RANDOM_NUMBER = 9;
+    public static final Integer CRITERIA = 4;
 }
