009ad4ea838a323a173eecd7b37fcf26ca930fd0
Hyunjun KIM <hihj070914@icloud.com>
2023-11-01 23:03:33 +0900

    feat: Application main - Application의 main 로직을 구현하였습니다. - 구현 후 ApplicationTest를 수행하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String rawCarNames = Console.readLine();
+        String[] participants = InputValidation.validateInputNames(rawCarNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String rawMoveCount = Console.readLine();
+        int totalMoves = InputValidation.validateInputMoveCount(rawMoveCount);
+
+        RacingGame game = new RacingGame(totalMoves);
+        for (String participantName : participants) {
+            game.addParticipant(new Car(participantName));
+        }
+        game.proceed();
+        System.out.println(game.getResult());
     }
 }
