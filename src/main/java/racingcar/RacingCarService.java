e0c88f67fe6f72376d192bab2868492d5ed6cd52
SeokEui Hong <hongs7701@naver.com>
2023-11-01 17:18:27 +0900

    feat: [#4] 자동차 게임의 우승자 찾기 및 인쇄기능 추가
    1. RacingCarService의 findTheLeaders 메소드.
    moveCount가 가장 큰 우승자를 찾아줌.
    
    2. RacingCarService의 printLeaders메소드.
    findTHeLeaders에서 찾은 우승자 리스트를 출력.
    1명일 경우와 복수일 경우로 나누어 출력.

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -2,10 +2,12 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarService {
     static final int MOVE_CRITERIA = 4;
+    static final int START_MOVE_COUNT = -1;
 
     void play(List<RacingCar> racingCars, int totalTurn) {
         System.out.println();
@@ -19,6 +21,9 @@ public class RacingCarService {
                 printRace(racingCars);
             }
         }
+
+        List<String> leaders = findTheLeaders(racingCars);
+        printLeaders(leaders);
     }
 
     public RacingCar operateCar(RacingCar car, int moveNum){
@@ -41,5 +46,40 @@ public class RacingCarService {
         }
 
         System.out.println();
+    }
+
+    public List<String> findTheLeaders(List<RacingCar> racingCars){
+        int largestMove = START_MOVE_COUNT;
+
+        for(RacingCar car : racingCars){
+            largestMove = Math.max(largestMove, car.getMoveCount());
+        }
+
+        List<String> leaders = new ArrayList<>();
+
+        for(RacingCar car : racingCars){
+            if(car.getMoveCount() == largestMove){
+                leaders.add(car.getName());
+            }
+        }
+
+        return leaders;
+    }
+
+    public void printLeaders(List<String> leaders){
+        System.out.print("최종 우승자 : ");
+
+        if(leaders.size() == 1){
+            for(String leader : leaders){
+                System.out.print(leader);
+            }
+        } else{
+            int lastLeaderIndex = leaders.size() - 1;
+
+            for(int i = 0; i < lastLeaderIndex; i++){
+                System.out.print(leaders.get(i) + ", ");
+            }
+            System.out.println(leaders.get(lastLeaderIndex));
+        }
     }
 }
