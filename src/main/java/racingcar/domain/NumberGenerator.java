06b89d2a5ea96334e22d142dc3f93666165a788f
ChanbyeongPark <coldbottle@korea.ac.kr>
2023-11-01 15:59:07 +0900

    style(racingcar): 무작위 수 생성의 리턴 자료형 수정

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
-    public Integer createRandomNumber() {
+    public int createRandomNumber() {
         int number;
         number = Randoms.pickNumberInRange(0, 9);
         return number;
