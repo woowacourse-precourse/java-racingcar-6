e0ee6cd1e13c64d076eda76a2ce7145f7c5faba2
박은지 <qkrdmswl1018@naver.com>
2023-11-01 10:50:52 +0900

    test: 우승자 처리 테스트 코드 작성
    test: 우승자 처리 테스트 코드 작성

--- src/test/java/racingcar/evaluator/PrintWinnerTest.java
+++ src/test/java/racingcar/evaluator/PrintWinnerTest.java
@@ -5,20 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import racingcar.printer.ResultPrinter;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrintWinnerTest {
 
-    private final PrintWinner printWinner = new PrintWinner();
+    private final WinnerEvaluator evaluator = new WinnerEvaluator();
+    private final ResultPrinter printer = new ResultPrinter(evaluator);
 
     @Test
     @DisplayName("우승자 List를 String 형태로 변경할 수 있다.")
     void buildWinner() {
         List<String> winners = new ArrayList<>(Arrays.asList("woo", "wa"));
-        String winnersString = printWinner.buildWinner(winners);
+        String winnersString = printer.buildWinner(winners);
         String[] winner = winnersString.split(", ");
 
         assertAll(
