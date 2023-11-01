92f07c36503211f784055fe32e8bef308ba0cfc0
kimdoha <kkhhjj3393@gmail.com>
2023-11-01 17:40:20 +0900

    chore: 테스트 반복 및 파일 코드 정리
    -

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -3,14 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
-    private static final Integer RANDOM_NUMBER_START_INCLUSIVE = 0;
-    private static final Integer RANDOM_NUMBER_END_INCLUSIVE = 9;
+    private static final Integer RANDOM_NUMBER_START = 0;
+    private static final Integer RANDOM_NUMBER_END = 9;
 
     public static Integer pick() {
-        return Randoms.pickNumberInRange(
-                RANDOM_NUMBER_START_INCLUSIVE,
-                RANDOM_NUMBER_END_INCLUSIVE
-        );
+        return Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END);
     }
 
     public static Integer pinkInRange(Integer range1, Integer range2) {
--- src/test/java/racingcar/util/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/util/RandomNumberGeneratorTest.java
@@ -1,26 +1,20 @@
 package racingcar.util;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 class RandomNumberGeneratorTest {
-    private static final int RANDOM_NUMBER_START_INCLUSIVE = 0;
-    private static final int RANDOM_NUMBER_END_INCLUSIVE = 9;
+    private static final int RANDOM_NUMBER_START = 0;
+    private static final int RANDOM_NUMBER_END = 9;
 
     @RepeatedTest(value = 5)
     @DisplayName("0에서 9사이 무작위의 값 랜덤 생성 테스트")
-    @Test
     void pick() {
         // Arrange
         int randomNumber = RandomNumberGenerator.pick();
 
         // Act & Assert
-        Assertions.assertTrue(
-                RANDOM_NUMBER_START_INCLUSIVE <= randomNumber &&
-                randomNumber <= RANDOM_NUMBER_END_INCLUSIVE);
+        Assertions.assertTrue(RANDOM_NUMBER_START <= randomNumber && randomNumber <= RANDOM_NUMBER_END);
     }
 }
