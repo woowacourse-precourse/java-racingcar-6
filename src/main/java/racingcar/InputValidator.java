7c775b921dfa10b3bd07128aca8686c68b53cf1a
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 16:29:31 +0900

    feat : 입력에 공백이 포함된 경우 예외 처리

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -6,4 +6,12 @@ public class InputValidator {
     public boolean isSingleCarInput(List<String> carNames) {
         return carNames.size() < 2;
     }
+
+    public void checkForWhitespace(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.contains(" ")) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
