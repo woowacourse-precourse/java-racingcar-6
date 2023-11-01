905bc31550174bc611affdac23ad245770db7e36
acycoco <pink10257@naver.com>
2023-11-01 16:42:38 +0900

    refactor(CarTest, WinnerPickerTest): 리팩토링으로 테스트 코드 given 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,28 +8,41 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     private Car car1;
-    private final String CAR1_NAME = "pobi";
+    private static final String CAR1_NAME = "pobi";
+    private static final int FORWARD = 4;
+    private NumberGenerator numberGenerator;
+    private ForwardChecker forwardChecker;
+
     @BeforeEach
     void setUp() {
         car1 = new Car(new CarName(CAR1_NAME));
+        numberGenerator = () -> FORWARD;
+        forwardChecker = new ForwardChecker();
     }
 
     @Test
     void RacingCar_생성시_forwardDistance가_0으로_생성() {
-        assertThat(car1.getForwardDistance().getValue()).isEqualTo(0);
+        //then
+        assertThat(car1.getForwardDistance()).isEqualTo(0);
     }
 
     @Test
-    void forward_메서드_사용시_forwardDistance가_1증가() {
-        car1.forward();
-        assertThat(car1.getForwardDistance().getValue()).isEqualTo(1);
+    void runRound메서드_사용시_forwardDistance가_1증가() {
+        //when
+        car1.runRound(numberGenerator, forwardChecker);
+
+        //then
+        assertThat(car1.getForwardDistance()).isEqualTo(1);
     }
 
     @Test
     void toString_메서드로_name과_forwardDistance를_출력() {
-        car1.forward();
-        car1.forward();
+        //given
+        car1.runRound(numberGenerator, forwardChecker);
+        car1.runRound(numberGenerator, forwardChecker);
         String expected = CAR1_NAME + " : --";
+
+        //when & then
         assertThat(car1.toString()).isEqualTo(expected);
     }
 }
--- src/test/java/racingcar/domain/WinnerPickerTest.java
+++ src/test/java/racingcar/domain/WinnerPickerTest.java
@@ -1,85 +1,76 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
 
 class WinnerPickerTest {
 
     private WinnerPicker winnerPicker;
     private Cars cars;
     private CarNames carNames;
+    private RandomNumberGenerator randomNumberGenerator;
+    private ForwardChecker forwardChecker;
     private static final String CAR1_NAME = "pobi";
     private static final String CAR2_NAME = "woni";
     private static final String CAR3_NAME = "jun";
     private static final String CAR4_NAME = "umi";
+    private static final int STOP = 3;
+    private static final int FORWARD = 4;
 
     @BeforeEach
     void setUp() {
         winnerPicker = new WinnerPicker();
-
+        randomNumberGenerator = new RandomNumberGenerator();
+        forwardChecker = new ForwardChecker();
     }
 
     @Test
     void 우승자가_한명인_경우_pickWinner_메서드로_우승자의_이름리스트를_반환() {
+        //given
         carNames = new CarNames(Arrays.asList(CAR1_NAME, CAR2_NAME, CAR3_NAME));
         cars = new Cars(carNames);
 
-        Car car1 = cars.getCars().get(0);
-        Car car2 = cars.getCars().get(1);
-        Car car3 = cars.getCars().get(2);
+        try (MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(STOP, STOP, FORWARD);
 
-        for (int forward = 0; forward < 5; forward++) {
-            car1.forward();
+            cars.runRound(randomNumberGenerator, forwardChecker);
+
+            //when
+            Winners winners = winnerPicker.pickWinner(cars);
+
+            //then
+            assertThat(winners.getWinnersName().getCarNames().size()).isEqualTo(1);
+            assertThat(winners.getWinnersName().getCarNames()).contains(new CarName(CAR3_NAME));
         }
-
-        for (int forward = 0; forward < 7; forward++) {
-            car2.forward();
-        }
-
-        for (int forward = 0; forward < 3; forward++) {
-            car3.forward();
-        }
-
-        Winners winners = winnerPicker.pickWinner(cars);
-        assertThat(winners.getWinnersName().getCarNames().size()).isEqualTo(1);
-        assertThat(winners.getWinnersName().getCarNames()).contains(new CarName(CAR2_NAME));
     }
 
     @Test
     void 우승자가_두명인_경우_pickWinner_메서드로_우승자의_이름리스트를_반환() {
+        //given
         carNames = new CarNames(Arrays.asList(CAR1_NAME, CAR2_NAME, CAR3_NAME, CAR4_NAME));
         cars = new Cars(carNames);
 
-        Car car1 = cars.getCars().get(0);
-        Car car2 = cars.getCars().get(1);
-        Car car3 = cars.getCars().get(2);
-        Car car4 = cars.getCars().get(3);
+        try (MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(STOP, STOP, FORWARD, FORWARD);
 
-        for (int forward = 0; forward < 5; forward++) {
-            car1.forward();
+            cars.runRound(randomNumberGenerator, forwardChecker);
+
+            //when
+            Winners winners = winnerPicker.pickWinner(cars);
+
+            //then
+            assertThat(winners.getWinnersName().getCarNames().size()).isEqualTo(2);
+            assertThat(winners.getWinnersName().getCarNames()).contains(new CarName(CAR3_NAME), new CarName(CAR4_NAME));
         }
-
-        for (int forward = 0; forward < 7; forward++) {
-            car2.forward();
-        }
-
-        for (int forward = 0; forward < 3; forward++) {
-            car3.forward();
-        }
-
-        for (int forward = 0; forward < 7; forward++) {
-            car4.forward();
-        }
-
-        Winners winners = winnerPicker.pickWinner(cars);
-
-        assertThat(winners.getWinnersName().getCarNames().size()).isEqualTo(2);
-        assertThat(winners.getWinnersName().getCarNames()).contains(new CarName(CAR2_NAME), new CarName(CAR4_NAME));
     }
 }
