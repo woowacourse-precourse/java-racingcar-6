c12c7197348abdbff26ff3512b316599374cb28b
Kim Taeyeong <tyeong1102@gmail.com>
2023-11-01 20:25:52 +0900

    feat: Validation 추가

---
+++ src/main/java/racingcar/consatant/Validation.java
@@ -0,0 +1,18 @@
+package racingcar.consatant;
+
+public class Validation {
+
+    public static final String COMMA_VALIDATION = "이름을 ,(쉼표)로 구분해주세요.";
+
+    public static final String NAME_DUPLICATE_VALIDATION = "중복되는 이름이 없게 입력해주세요.";
+
+    public static final String CARS_COUNT_VALIDATION = "참여하는 자동차는 1개 이상이여야 합니다.";
+
+    public static final String INPUT_NUMBER_VALIDATION = "숫자만 입력해주세요.";
+
+    public static final String INPUT_NATURAL_NUMBER_VALIDATION = "자연수만 입력해주세요.";
+
+    public static final String INPUT_NAME_LENGTH_VALIDATION = "1~5글자 사이로 이름을 입력해주세요.";
+
+
+}
