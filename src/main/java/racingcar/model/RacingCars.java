48870d2e4e0cbf34def5d44107cb4f15c57263d6
SeoYoung <seoyoung7623@naver.com>
2023-11-01 21:55:33 +0900

    refactor: 가독성 코드 수정

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,8 +1,12 @@
 package racingcar.model;
 
+import racingcar.view.OutputView;
+import static racingcar.util.Util.getMax;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class RacingCars {
     private final ArrayList<Car> CARS = new ArrayList<>();
@@ -17,26 +21,28 @@ public class RacingCars {
         }
     }
 
-    public HashMap<String,Integer> getRoundResult(){
-        HashMap<String,Integer> roundResult = new HashMap<>();
-        for(Car car:CARS){
-            roundResult.put(car.getName(),car.getPosition());
-        }
-        return roundResult;
+    public void playRound() {
+        go();
+        OutputView.printRoundResult(new ArrayList<>(CARS.stream()
+                .map(Car::getRoundResult)
+                .collect(Collectors.toList())
+        ));
+    }
+    private void go() {
+        CARS.forEach(Car::go);
     }
 
-    public String[] getWinners(){
-        int maxPosition = findMax(getPositions());
-        return (String[])CARS.stream()
-                .map(car -> car.getPosition() == maxPosition)
-                .toArray();
+    public void getWinners(){
+        OutputView.printWinner(new ArrayList<>(CARS.stream()
+                .filter(car -> car.isWinner(getMax(getPositions())))
+                .map(Car::getName)
+                .collect(Collectors.toList())
+        ));
     }
 
     public ArrayList<Integer> getPositions(){
         ArrayList<Integer> positons = new ArrayList<>();
-        for(Car car: CARS){
-            positons.add(car.getPosition());
-        }
+        CARS.forEach(car -> positons.add(car.getPosition()));
         return positons;
     }
 
