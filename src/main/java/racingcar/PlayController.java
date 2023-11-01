ca2c63f6db4d6b002535a84c9d066a45dd44c11b
Soonrang <clearsbyul30@naver.com>
2023-11-01 22:30:23 +0900

    자동차 이동규칙 등록

--- src/main/java/racingcar/PlayController.java
+++ src/main/java/racingcar/PlayController.java
@@ -2,14 +2,14 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class PlayController {
 
+    List<Integer> positions = new ArrayList<>();
+
     // 자동차 이름 부여
-    public void generateRacingCarName(){
+    public List<String> generateRacingCarName(){
         //System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> players = new ArrayList<>();
 
@@ -18,7 +18,16 @@ public class PlayController {
         String[] playersTemp = inputPlayers.split(",");
 
         players.addAll(Arrays.asList(playersTemp));
+        return players;
     }
-
+    public void movingCar(List<String> players, List<Integer> randomNumbers, List<Integer> positions){
+        List<String> movingPlayersList = new ArrayList<>();
+        for(int i=0; i<players.size(); i++){
+            if(randomNumbers.get(i) >3){
+                positions.set(i, positions.get(i)+1);
+                movingPlayersList.add(players.get(i));
+            }
+        }
+    }
 
 }
