1accbf77d8f4947f5f99d2740bb6ffd8e3615b59
Sim-GyuBin <skb0606@gmail.com>
2023-11-01 19:29:28 +0900

    feat: 0~9 사이에 발생하는 무작위 수 생성 메소드

---
+++ src/main/java/racingcar/NumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class NumberGenerator {
+
+    public static int createRandomNumber() {
+        int number = Randoms.pickNumberInRange(0,9);
+
+        return number;
+    }
+}
