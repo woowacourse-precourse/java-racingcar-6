f936b91a4633b4134caac9e972c0fb1fa65a9f50
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-11-01 21:30:14 +0900

    Comment : remove Application comment

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,6 @@ import racingcar.controller.Start;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         Start start = new Start();
         start.play();
