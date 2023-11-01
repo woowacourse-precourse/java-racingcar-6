fc03d29907e95af7fb1b0577f7dc32fa3ec8d15d
KJBig <kjgi73k@gmail.com>
2023-11-01 21:52:24 +0900

    feat : 가장 높은 값을 가진 자동차가 최종 우승자로 선발되는지 테스트
    - 가장 높은 값을 가진 자동차가 최종 우승자로 선발되는지 테스트 및 중복 최종 우승자가 발생할 경우 모두 List<>에 저장되는지 테스트

--- src/test/java/racingcar/GameServiceTest.java
+++ src/test/java/racingcar/GameServiceTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -11,6 +12,8 @@ import racingcar.domain.Car;
 import racingcar.service.GameService;
 
 public class GameServiceTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
     private GameService gameService;
 
     @BeforeEach
@@ -30,6 +33,31 @@ public class GameServiceTest extends NsTest {
 
         // then
         assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 우승자_판단() {
+        //given
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("jun");
+        Car car3 = new Car("ker");
+        List<Car> cars = List.of(car1, car2, car3);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car1.move();
+                    car2.move();
+                    car3.move();
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+
+        //when
+        List<Car> winners = gameService.getWinners(cars);
+
+        //then
+        assertThat(winners)
+                .extracting(Car::getName)
+                .contains("pobi", "ker");
     }
 
     @Override
