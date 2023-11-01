c7aea912a20b258ed53eef0583addb8c2b1341f0
namaek_2 <science4588@gmail.com>
2023-11-01 17:42:13 +0900

    feat : 시도할 횟수 입력받기 구현
    최초 구현

--- src/main/java/racingcar/Control.java
+++ src/main/java/racingcar/Control.java
@@ -13,11 +13,26 @@ public class Control {
         return carNames;
     }
 
+    public static int inputCount() {
+        String userInputCount = Console.readLine();
+        int count = Integer.parseInt(userInputCount);
+
+        countErrorCheck(count);
+
+        return count;
+    }
+
     public static void nameErrorCheck(String[] carNames){
         for (String carName : carNames) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
+        }
+    }
+
+    public static void countErrorCheck(int count){
+        if (count < 1) {
+            throw new IllegalArgumentException("시도 횟수는 양수인 정수만 입력이 가능합니다.");
         }
     }
 }
