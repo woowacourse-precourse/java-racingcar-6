f082ed8e3995f5d69b647f7e620fa62f403ac6f8
uhanuu <hwryu990927@gmail.com>
2023-11-01 23:28:31 +0900

    test: study test 제거

--- src/test/java/study/random/docs/README.md
+++
@@ -1,4 +0,0 @@
-# 해당 패키지의 모든 학습은 아래 링크에 내용입니다.
-Reference: [unluckyjung][studylink]
-
-[studylink]: https://unluckyjung.github.io/testcode/2021/12/20/Mockito-StaticMethod-Mocking/
--- src/test/java/study/random/name/OtherClass.java
+++
@@ -1,9 +0,0 @@
-package study.random.name;
-
-public class OtherClass {
-
-    public static String getCarName() {
-        System.out.println(CarName.getName());
-        return CarName.getName();
-    }
-}
