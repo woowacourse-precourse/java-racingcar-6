6801fc042f454fa757d4f3ef23b707ce12b0f32a
Davin <85967571+dav1n9@users.noreply.github.com>
2023-11-01 14:11:43 +0900

    feat: 사용자로부터 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<String> carNames;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = List.of(Console.readLine().split(","));
+
+        for (String car : carNames) {
+            if(car.length() > 5)
+                throw new IllegalArgumentException();
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int forwardCount = Integer.parseInt(Console.readLine());
+
     }
 }
