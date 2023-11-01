cf9fd212b6cb2ac282d0ef208928367e1bf53e69
godee95 <50854903+godee95@users.noreply.github.com>
2023-11-01 19:11:22 +0900

    fix : 출력 형식 공백 추가

--- src/main/java/racingcar/domain/ResultPrinter.java
+++ src/main/java/racingcar/domain/ResultPrinter.java
@@ -2,7 +2,7 @@ package racingcar.domain;
 
 public class ResultPrinter {
     public void printResult(String carName, int moves) {
-        StringBuilder result = new StringBuilder(carName + " :");
+        StringBuilder result = new StringBuilder(carName + " : ");
 
         for (int i=0; i<moves; i++) {
             result.append("-");
--- src/main/java/racingcar/domain/WinnerAnnouncer.java
+++ src/main/java/racingcar/domain/WinnerAnnouncer.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class WinnerAnnouncer {
     public void announceSoloWinner(String winner) {
-        System.out.println("최종 우승자: " + winner);
+        System.out.println("최종 우승자 : " + winner);
     }
 
     public void announceJointWinners(List<String> winners) {
-        System.out.println("최종 우승자: " + String.join(", ", winners));
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
