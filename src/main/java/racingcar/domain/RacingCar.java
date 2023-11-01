6d84ab8a7830b40fc4b5d300e335d5b40469b09e
Gyunseo Lee <rbstj0311@naver.com>
2023-11-01 21:30:29 +0900

    feat(RacingCar): move 기능 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,10 +2,15 @@ package racingcar.domain;
 
 public class RacingCar {
     public final String name;
+    public int position;
 
     public RacingCar(String name) {
         this.name = name;
+        this.position = 0;
     }
 
+    public void move() {
+        this.position++;
+    }
 
 }
