dba2e6af6ebfc886487a0911d04108a35600193a
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:29:59 +0900

    test: Validator의 실행범위 입력값 테스트 작성

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.utils.Validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    @Test
+    void 실행횟수가_1미만일_경우_에러() {
+        int numOfExecutions = -1;
+
+        assertThatThrownBy(() -> Validator.validateRangeOfExecutions(numOfExecutions))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 실행횟수가_1000초과일_경우_에러() {
+        int numOfExecutions = 1001;
+
+        assertThatThrownBy(() -> Validator.validateRangeOfExecutions(numOfExecutions))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
