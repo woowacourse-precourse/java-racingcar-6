9a5926df1830b5ca7d860f626f488cb6f22b16da
Kim mingyu <minq0302@naver.com>
2023-11-01 19:45:00 +0900

    feat(model): add Car class
    Add getter and constructor also.

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int forwardCount;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getForwardCount() {
+        return this.forwardCount;
+    }
+
+    public Car(String name) {
+        this.name = name;
+        this.forwardCount = 0;
+    }
+}
