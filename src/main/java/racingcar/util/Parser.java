b02239705ce964c06b1f6c79dd23c7d9d921c610
Minchae0322 <jmcabc0322@gmail.com>
2023-11-01 15:18:47 +0900

    style: 오류 처리 분리, 코드 간소화

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -9,10 +9,6 @@ import java.util.List;
 public final class Parser {
 
     public static List<String> splitName(String userInput, String regex) {
-        String[] cutIntoComma = userInput.split(regex);
-        List<String> nameList = new ArrayList<>(Arrays.asList(cutIntoComma));
-        Validator.validateName(nameList);
-
-        return nameList;
+        return new ArrayList<>(Arrays.asList(userInput.split(regex)));
     }
 }
