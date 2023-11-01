34983acb26c1179e40050903c6f539771d00c82f
minkyeong <stl9955@naver.com>
2023-11-01 20:57:21 +0900

    Feat: Car Class 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
