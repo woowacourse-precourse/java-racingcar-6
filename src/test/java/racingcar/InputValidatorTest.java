30b60b4ef011438885e8c256c72d2e38b4476ae0
ldw <leehan02@hanyang.ac.kr>
2023-11-01 15:44:45 +0900

    test(racingcar): 입력값이 빈 문자열 혹은 공백이 아닌 유효한 경우에 대한 단위 테스트 구현

---
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InputValidatorTest {
+
+    @Test
+    void 입력값이_빈_문자열_혹은_공백이_아닌_경우() {
+        String input = "This string is not blank";
+
+        Assertions.assertDoesNotThrow(() -> InputValidator.blank(input));
+    }
+
+//    @Test
+//    void 입력값이_공백_으로만_이루어진_경우_예외_발생() {
+//        String input = "        ";
+//
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.blank(input));
+//    }
+//
+//    @Test
+//    void 입력값이_빈_문자열인_경우_예외_발생() {
+//        String input = "";
+//
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.blank(input));
+//    }
+//
+//    @Test
+//    void 시도_횟수가_최소_시도_횟수_이상인_경우() {
+//        int tryNum = 1;
+//
+//        Assertions.assertDoesNotThrow(() -> InputValidator.tryNum(tryNum));
+//    }
+//
+//    @Test
+//    void 시도_횟수가_최소_시도_횟수_이하인_경우_예외_발생() {
+//        int tryNum = 0;
+//
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.tryNum(tryNum));
+//    }
+}
