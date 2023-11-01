7913572ed5b453c7bddcc2cf97d558dbe6384fba
chosunghyun18 <krismon@naver.com>
2023-11-01 23:32:36 +0900

    refact :  add message delimeter

--- src/main/java/racingcar/model/rule/RuleErrorMessage.java
+++ src/main/java/racingcar/model/rule/RuleErrorMessage.java
@@ -3,8 +3,9 @@ package racingcar.model.rule;
 public enum RuleErrorMessage {
     EMPTY_NAME("Name is Empty"),
     WRONG_SIZE_OF_NAME("Wrong Size Of Name"),
-    DUPLICATED_NAME("Duplicated Name : "),
+    DUPLICATED_NAME("Duplicated Name"),
     WRONG_VALUE_OF_ROUND("Wrong Value Of Round");
+    private static final String MESSAGE_DELIMITER = " : ";
     private final String message;
 
     RuleErrorMessage(String message) {
@@ -16,6 +17,6 @@ public enum RuleErrorMessage {
     }
 
     public String getFormattedMessage(String detail) {
-        return message + detail;
+        return message + MESSAGE_DELIMITER + detail;
     }
 }
