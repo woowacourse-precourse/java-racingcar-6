d496162a98dd096539cdfe9145abb70df2188a55
psrom <psrom721@gmail.com>
2023-11-01 15:46:30 +0900

    feat(test): 자동차 이름 입력과 관련된 예외 처리 테스트

--- src/test/java/racingcar/UserInputTest.java
+++ src/test/java/racingcar/UserInputTest.java
@@ -15,6 +15,34 @@ class UserInputTest extends NsTest {
         );
     }
 
+
+    @Test
+    void 쉼표_뒤에_공백인_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 띄어쓰기가_포함된_이름인_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,wo ni"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 이름이_중복된_경우_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
