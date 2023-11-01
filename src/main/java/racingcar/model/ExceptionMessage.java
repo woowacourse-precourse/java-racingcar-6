b33934425342a2c99c98a21a73a737a17bf8f22c
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-01 17:18:11 +0900

    refactor: (ExceptionMessage) 변수명을 형식에 맞게 변환

--- src/main/java/racingcar/model/ExceptionMessage.java
+++ src/main/java/racingcar/model/ExceptionMessage.java
@@ -1,13 +1,15 @@
 package racingcar.model;
 
 public enum ExceptionMessage {
-    inputIntegerRoundErrorMessage("시도할 횟수는 1이상이어야 합니다."),
-    inputStringRoundErrorMessage("시도할 횟수는 숫자여야 합니다."),
-    carNameOverLengthErrorMessage("경주할 자동차 이름은 5자 이하만 가능합니다."),
-    duplicateCarNameErrorMessage("경주할 자동차 이름은 유일해야 합니다.");
-    ;
+    INPUT_INTEGER_ROUND_ERROR_MESSAGE("시도할 횟수는 1이상이어야 합니다."),
+    INPUT_STRING_ROUND_ERROR_MESSAGE("시도할 횟수는 숫자여야 합니다."),
+    CAR_NAME_OVER_LENGTH_ERROR_MESSAGE("경주할 자동차 이름은 5자 이하만 가능합니다."),
+    DUPLICATE_CAT_NAME_ERROR_MESSAGE("경주할 자동차 이름은 유일해야 합니다."),
+    BLINK_OR_NULL_CAR_NAME_ERROR_MESSAGE("경주할 자동차 이름은 공백이 아니어야 합니다."),
+    LESS_TWO_CAR_COUNT_ERROR_MESSAGE("경주할 자동차 수는 2대 이상이어야 합니다.");
 
     private final String message;
+
     ExceptionMessage(String message) {
         this.message=message;
     }
