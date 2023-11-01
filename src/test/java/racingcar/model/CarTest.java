e4c8be5db5ef0438b72e937d6b016889f6c9daa8
oxdjww <oxdjww@gmail.com>
2023-11-01 20:32:16 +0900

    style : 필요없는 library import 제거

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import racingcar.constant.ExceptionConstants;
 import racingcar.exception.RacingCarGameException;
 
