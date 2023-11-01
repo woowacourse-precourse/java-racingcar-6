5b5731069f988c1e1a3b82b85500e1829f145c1e
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 19:38:52 +0900

    refactor : Java Code Conventionㅇ로 Refactor

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,22 +1,26 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.car.Car;
 import racingcar.car.CarListMaker;
+import racingcar.car.CarMovementController;
 import racingcar.output.OutputController;
+import racingcar.tryingcounts.TryingCounts;
 
 public class RacingCarController {
-    OutputController outputController = new OutputController();
-    public void start() {
-        outputController.printCarNameInputMessage();
-        CarListMaker carListMaker = new CarListMaker();
-        List<String> carList = carListMaker.makeCarList();
 
-        outputController.printTryingCountsInputMessage();
-        TryingCounts tryingCounts = new TryingCounts();
-        Integer tryingCountsNumber = tryingCounts.getTryingCounts();
+	OutputController outputController = new OutputController();
 
-        CarMovingController carMovingController = new CarMovingController();
-        String racingCarResult = carMovingController.moveCars(carList,tryingCountsNumber);
-
-        outputController.printResult(racingCarResult);
-    }
+	public void start() {
+		outputController.printCarNameInputMessage();
+		CarListMaker carListMaker = new CarListMaker();
+		List<Car> carList = carListMaker.makeCarList();
+		outputController.printTryingCountsInputMessage();
+		TryingCounts tryingCounts = new TryingCounts();
+		Integer tryingCountsNumber = tryingCounts.getTryingCounts();
+		System.out.println(tryingCountsNumber);
+		CarMovementController carMovementController = new CarMovementController();
+		String racingCarResult = carMovementController.moveCars(carList, tryingCountsNumber);
+		outputController.printResult(racingCarResult);
+	}
 }
