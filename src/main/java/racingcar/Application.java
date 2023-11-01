7c316fdf52e4bb61076d84d2d3449d3c48cc986c
JIHO <x8608666@gmail.com>
2023-11-01 23:32:47 +0900

    build_success

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,94 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+        this.name = name;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
+}
+
+class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void raceRound() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = Arrays.stream(Console.readLine().split(","))
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        int rounds = Integer.parseInt(Console.readLine());
+
+        Race race = new Race(cars);
+        for (int i = 0; i < rounds; i++) {
+            race.raceRound();
+            printRaceResult(race.getCars());
+        }
+
+        List<String> winners = race.getWinners();
+        String result = winners.stream().collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + result);
+    }
+
+    private static void printRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+        System.out.println();
     }
 }
