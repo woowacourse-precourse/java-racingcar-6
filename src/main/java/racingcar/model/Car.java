3fada764897ee9e6caf0ad4a1f9f399851095c14
qudwns017 <qudwns017@naver.com>
2023-11-01 21:25:30 +0900

    Fix: Model 디렉토리 위치 수정

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import static racingcar.util.Constants.*;
+
+import racingcar.model.dto.CarDTO;
+
+public class Car {
+    private final String name;
+    private int moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = ZERO;
+    }
+
+    public static Car of(String name) {
+        return new Car(name);
+    }
+
+    public CarDTO toDto() {
+        return new CarDTO(this.name, this.moveCount);
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVING_CONDITION) {
+            moveCount++;
+        }
+    }
+}
