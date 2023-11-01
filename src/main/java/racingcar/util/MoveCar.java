a9c632ece18e0e3701377669f9bd9b1807ecb71f
KongJunWoo <joonoo3@naver.com>
2023-11-01 22:56:15 +0900

    feat : 무작위 수 생성
    pickNumberInRange을 사용해 0 에서 9 까지의 정수를 반환합니다.

---
+++ src/main/java/racingcar/util/MoveCar.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class MoveCar {
+
+    public static int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
