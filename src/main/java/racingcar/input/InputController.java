87a3291412858701b870f0f56cb6d6c7ada3a489
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 19:37:04 +0900

    feat : 입력받는 함수들을 관리하는 InputController.java 생성

---
+++ src/main/java/racingcar/input/InputController.java
@@ -0,0 +1,14 @@
+package racingcar.input;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputController {
+
+	public String scanCarNames() {
+		return readLine();
+	}
+
+	public String scanTryingCountsNumber() {
+		return readLine();
+	}
+}
