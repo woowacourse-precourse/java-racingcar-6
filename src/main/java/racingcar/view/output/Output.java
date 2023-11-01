e88f3f52e23a327e40ad014c8498538f4039c8c1
김현재 <khjmr@naver.com>
2023-11-01 14:57:37 +0900

    feat(output): 게임 진행해 필요한 출력 로직 추가

---
+++ src/main/java/racingcar/view/output/Output.java
@@ -0,0 +1,11 @@
+package racingcar.view.output;
+
+public class Output {
+    private void printInputCarName() {
+        System.out.println(OutputConstants.INPUT_CAR_NAME);
+    }
+
+    private void printInputTryNumber() {
+        System.out.println(OutputConstants.INPUT_TRY_NUMBER);
+    }
+}
