9263e0d3d22a5a4c752a81f1ced7f3eb2d7c9dbb
동원 <ai06021@gmail.com>
2023-11-01 19:39:51 +0900

    feat : Race하면서 마지막 출력문 출력

--- src/main/java/racingcar/controller/RaceMap.java
+++ src/main/java/racingcar/controller/RaceMap.java
@@ -2,18 +2,27 @@ package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import racingcar.model.Race;
 import racingcar.view.Cars;
+import racingcar.view.Finish;
+import racingcar.view.ScorePrint;
 import racingcar.view.Try;
 
 public class RaceMap {
     public RaceMap() {
-        List<Cars> cars = Cars.make();
+        List<String> cars = Cars.name();
         int maxTry = Try.input();
+        int car_numbers = cars.size();
+        int carLocation[] = new int[car_numbers];
 
+        int tryCount = 0;
 
+        while (tryCount < maxTry) {
+            carLocation = Race.racing(carLocation, car_numbers);
+            ScorePrint.resultByOrder(carLocation, cars);
+            tryCount++;
+        }
+        Finish.victoryCars(carLocation, cars);
     }
-    private int forwardCondition() {
-        int random = Randoms.pickNumberInRange(0, 9);
-        return random;
-    }
+
 }
