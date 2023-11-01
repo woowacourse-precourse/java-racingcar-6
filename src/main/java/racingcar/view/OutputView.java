e9f61817aa568251cc703c17a2c6a95eabfdaa0a
Ukj0ng <xb253q@gmail.com>
2023-11-01 15:26:28 +0900

    feat(OutputView): 사용자에게 출력할 문자열 생성
    추가한 기능:
    - <실행 결과>문자열 반환
    - 자동차의 전진상태 문자열 반환

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,17 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+public class OutputView {
+
+    public static String resultText(){
+        return UITexts.RACING_RESULT.getText();
+    }
+
+    public static String showMoveStatus(int position) {
+        StringBuilder positionIndicator = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+           positionIndicator.append("-");
+        }
+        return positionIndicator.toString();
+    }
+
 }
