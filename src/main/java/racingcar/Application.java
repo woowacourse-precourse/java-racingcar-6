76b5dbd3aae4c30188e5645402bba778cb8e49a7
acycoco <pink10257@naver.com>
2023-11-01 20:35:51 +0900

    feat(Application): RacingCarGame을 이용해 play하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.domain.RacingCarGame;
+import racingcar.domain.WinnerPicker;
+import racingcar.input.Input;
+import racingcar.output.Output;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Output output = new Output();
+        Input input = new Input(output);
+        WinnerPicker winnerPicker = new WinnerPicker();
+
+        RacingCarGame racingCarGame = new RacingCarGame(output, input, winnerPicker);
+        racingCarGame.play();
     }
 }
