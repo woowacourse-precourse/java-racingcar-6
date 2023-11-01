46eb3c55bc918413d213e4726637a04bbd45416a
이지은 <jieun7056@naver.com>
2023-11-01 15:07:15 +0900

    feat: 입/출력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+
+import racingcar.util.InputUtil;
+
+public class InputView {
+    public static String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return InputUtil.getString();
+    }
+
+    public static String inputTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return InputUtil.getString();
+    }
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+
+public class OutputView {
+    public static void printResult() {
+        System.out.println("\n실행 결과");
+
+    }
+}
