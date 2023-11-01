00d4fef05145005914347e680c5573fcf2450c23
sunwoong <sunwoong1122@gmail.com>
2023-11-01 18:56:20 +0900

    feat: 자동차 전진, 정지 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -2,8 +2,16 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Map;
+
 public class GameService {
-    public int generateRandomNumber() {
+    private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    private void forwardOrStopAccordingToNumber(int number, String name, Map<String, Integer> distance) {
+        if (number >= 4) {
+            distance.put(name, distance.get(name) + 1);
+        }
     }
 }
