02b37bc84791dde79a5620a73b24a89ff8b61063
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-01 18:58:08 +0900

    feat: 전진 개수 배열로 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,15 +10,6 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String[] cars = Console.readLine().split(",");
 
-        // TODO: 2차원 배열에 저장
-        int carsNum = cars.length;
-        String[][] carForward = new String[carsNum][1];
-        for (int s = 0; s < carsNum; s++) {
-            // 자동차 이름 복사
-            carForward[s][1] = cars[s];
-        }
-
-
         // TODO: 시도할 회수 입력 받기
         System.out.println("시도할 회수는 몇회인가요?");
         int gameCount = Integer.parseInt(Console.readLine());
@@ -27,15 +18,25 @@ public class Application {
         for (int i = 0; i < gameCount; i++) {
 
             // TODO: 경주차 개수에 따른 랜덤 정수 생성
+            int carsNum = cars.length;
             int[] randomNumArray = new int[carsNum];
 
             for (int j = 0; j < carsNum; j++) {
                 int randomNum = Randoms.pickNumberInRange(0,9);
                 randomNumArray[j] = randomNum;
             }
+
+            // TODO: 랜덤값이 4 이상이면 전진
+            String[] forwardCount = new String[carsNum];
+
+            for (int k = 0; k < carsNum; k++) {
+                if (randomNumArray[k] >= 4) {
+                    forwardCount[k] += "-";
+                }
+            }
         }
 
-        // TODO: 실행 결과 출력
+        // TODO: 전진 "-" 개수 계산
 
         // TODO: 우승자 출력
     }
