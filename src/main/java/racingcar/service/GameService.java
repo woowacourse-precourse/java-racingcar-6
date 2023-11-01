2767e980049fda30807072fdef850673585d594d
sunwoong <sunwoong1122@gmail.com>
2023-11-01 18:56:59 +0900

    feat: 자동차 모델 핸들링 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,10 +1,21 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
 
+import java.util.List;
 import java.util.Map;
 
 public class GameService {
+    public void handleCar(Car car) {
+        List<String> names = car.getNames();
+        Map<String, Integer> distance = car.getDistance();
+        names.forEach(name -> {
+            int number = generateRandomNumber();
+            forwardOrStopAccordingToNumber(number, name, distance);
+        });
+    }
+
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
