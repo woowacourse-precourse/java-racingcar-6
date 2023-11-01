67591310adfaa865238f5652ecfe6fbda902d205
Sodychoe <sodychoe@icloud.com>
2023-11-01 23:04:46 +0900

    test: 테스트 코드 추가

--- src/test/java/racingcar/OutputViewTest.java
+++ src/test/java/racingcar/OutputViewTest.java
@@ -9,13 +9,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.View.OutputView;
+import racingcar.model.Cars;
 
 public class OutputViewTest {
+    private OutputView ov;
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+
     @BeforeEach
     public void setUp() {
+        ov = new OutputView();
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
@@ -30,18 +34,25 @@ public class OutputViewTest {
         ArrayList<String> winners = new ArrayList<>();
         winners.add("pobi");
         winners.add("jun");
-        OutputView ov = new OutputView();
         ov.printWinners(winners);
 
         // when
-        String expected = "최종 우승자 : pobi, jun\n";
+        String expected = "최종 우승자 : pobi, jun";
 
         // then
         assertThat(outputStreamCaptor.toString()).isEqualTo(expected);
     }
 
     @Test
-    void 라운드_출력테스트()
+    void 자동차진행상황출력테스트(){
+        String name = "vvi";
+        int position = 9;
+
+        String expected = "vvi : ---------";
+        ov.printCarResult(name, position);
+
+        assertThat(outputStreamCaptor.toString()).isEqualTo(expected);
 
     }
+
 }
