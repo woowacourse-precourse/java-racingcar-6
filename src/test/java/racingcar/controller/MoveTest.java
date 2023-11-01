aec3d5f29e0b506c98c9031481fbcc5f908a804d
ghsyn <swanshawn715@gmail.com>
2023-11-01 19:50:14 +0900

    test: delete move forward or not test

--- src/test/java/racingcar/controller/MoveTest.java
+++ src/test/java/racingcar/controller/MoveTest.java
@@ -1,27 +1,11 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
-class MoveTest extends NsTest {
+class MoveTest {
 
     @Test
     void 정지_혹은_전진() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : ", "woni : -", "최종 우승자 : woni");
-                },
-                3, 4
-        );
-    }
 
-    @Override
-    protected void runMain() {
-        Application.main(new String[]{});
     }
 }
