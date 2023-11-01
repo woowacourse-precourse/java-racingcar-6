6b9550d9c4ca27f7019b93c64f04b5d6c0e0217f
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 22:23:20 +0900

    [feat] : add split car names test method

---
+++ src/test/java/racingcar/utils/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringUtilsTest {
+
+    @Test
+    void splitCarNamesTest() {
+        List<String> strings = new ArrayList<>();
+
+        strings.add("1");
+        strings.add("2");
+        strings.add("3");
+
+        assertEquals(StringUtils.splitCarNames("1,2,3"),strings);
+    }
+}
