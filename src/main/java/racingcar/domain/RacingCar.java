ab1c47306fa106f740a5bec80e497d681f44af83
YoujeongChoi <yj91322@gmail.com>
2023-11-01 23:56:42 +0900

    Fix: 버그 수정

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,12 +1,18 @@
 package racingcar.domain;
 
+import racingcar.util.RacingCarConstant;
+
 public class RacingCar {
 
     private final String name;
     private int position = 0;
 
     public RacingCar(String name) {
-        this.name = name;
+        String trimmedName = name.trim();
+        if (trimmedName.length() > RacingCarConstant.CAR_NAME_MAX_LENGTH || trimmedName.isEmpty()) {
+            throw new IllegalArgumentException(RacingCarConstant.CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
+        this.name = trimmedName;
     }
     public void move() {
         int randomValue = RandomNumGenerator.generate();
