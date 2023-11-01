f1f68d123cc9a89527f455d944b282b23de3908d
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-11-01 22:19:09 +0900

    test: 숫자 입력 예외 처리 테스트
    횟수를 입력할 때 숫자가 아닌 문자를 입력했을 때 예외처리가 되는지 테스트

---
+++ src/test/java/racingcar/racingcarTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class racingcarTest extends NsTest {
+    @Test
+    void 숫자입력예외처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("soft,hard", "e"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
