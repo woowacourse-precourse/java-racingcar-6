9fc2c227586034b1e8885184bf0dc93a9b6b4eed
jjaegii <hn06038@gmail.com>
2023-11-01 19:03:10 +0900

    feat: InputManagerTest 클래스 내 throw_exception_for_invalid_try_count 메소드 추가

--- src/test/java/racingcar/InputManagerTest.java
+++ src/test/java/racingcar/InputManagerTest.java
@@ -3,6 +3,9 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputManagerTest {
     @Test
@@ -14,5 +17,15 @@ public class InputManagerTest {
         assertThatThrownBy(() -> inputManager.isValidName(invalidName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1, 시도 횟수는 음수가 될 수 없습니다.", "abc, 문자는 입력할 수 없습니다."})
+    void throw_exception_for_invalid_try_count(String invalidNum, String expectedMessage) {
+        InputManager inputManager = new InputManager();
+
+        assertThatThrownBy(() -> inputManager.isValidNum(invalidNum))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(expectedMessage);
     }
 }
