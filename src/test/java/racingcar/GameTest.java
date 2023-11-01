7d75b9c29fb4e9ae7dedcd3f629bb1d81650ea6a
DonghaOh <donghadarwin@naver.com>
2023-11-01 18:48:14 +0900

    test: 테스트 클래스 생성 - GameTest 클래스

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+public class GameTest extends NsTest {
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
