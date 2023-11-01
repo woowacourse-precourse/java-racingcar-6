475ba96a850324822f3d4bd95386e67d951fceb5
kg9585 <kg9585@naver.com>
2023-11-01 23:38:11 +0900

    refactor(CarDto): record 적용

--- src/main/java/racingcar/model/CarDto.java
+++ src/main/java/racingcar/model/CarDto.java
@@ -2,25 +2,8 @@ package racingcar.model;
 
 import racingcar.util.validate.CarValidateImpl;
 
-public class CarDto {
-    private String name;
-    private int position;
-
-    public CarDto(String name, int position) {
-        this.name = name;
-        this.position = position;
-    }
-
+public record CarDto(String name, int position) {
     public static CarDto convertToDto(Car car) {
         return new CarDto(car.getName(), car.getPosition());
     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getPosition() {
-        return this.position;
-    }
-
 }
