668fe16cb088e5ecd768ebb127455ffcab24bfb0
동원 <ai06021@gmail.com>
2023-11-01 18:54:56 +0900

    feat : 각 차수별 이동위치

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -7,7 +7,23 @@ import racingcar.view.Cars;
 public class Race {
     final static int MORE = 4;
 
+    public Race(List<String> cars, int maxTry) {
+        int tryCount = 0;
+        int car_numbers = cars.size();
+        int carsLocation[] = new int[car_numbers];
+        while (tryCount < maxTry) {
+            racing(carsLocation);
+        }
+    }
 
+    private static int[] racing(int currentCarLocation[]) {
+        int MaxCarNumber = currentCarLocation.length;
+        for (int CarNumber = 0; CarNumber < MaxCarNumber; CarNumber++) {
+            if (forwardCondition())
+                currentCarLocation[CarNumber]++;
+        }
+        return currentCarLocation;
+    }
 
     private static boolean forwardCondition() {
         int random = Randoms.pickNumberInRange(0, 9);
