6f7e29537fe1cba59d73df726757a3e5f9aa8181
Seungjida <esg0107@pusan.ac.kr>
2023-11-01 15:46:02 +0900

    test(input): 입력값의 공백을 잘 제거해 유효한 값들로만 이뤄지는지 확인하는 코드 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,4 +1,36 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
+    private String inputValue;
+
+    Input() {
+        inputValue = Console.readLine();
+    }
+
+    Input(String inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public List<String> inputCarName() {
+        String[] carNameArray = removeSpace(inputValue).split(",");
+        List<String> inputNames = Arrays.asList(carNameArray);
+
+        Validation.validateCarName(inputNames);
+        return inputNames;
+    }
+
+    public String inputAttemptNumber() {
+        String inputTryNumber = removeSpace(inputValue);
+
+        Validation.validateAttemptNumber(inputTryNumber);
+        return inputTryNumber;
+    }
+
+    private String removeSpace(String inputString) {
+        return inputValue.replaceAll(" ", "");
+    }
 }
