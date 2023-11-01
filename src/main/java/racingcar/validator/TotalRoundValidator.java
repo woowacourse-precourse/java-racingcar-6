2442015b8967f5accef1aca3eeb5582d870a3313
liarreez <liarreez@gmail.com>
2023-11-01 21:57:48 +0900

    style : 가독성 향상을 위해 import문 수정

--- src/main/java/racingcar/validator/TotalRoundValidator.java
+++ src/main/java/racingcar/validator/TotalRoundValidator.java
@@ -1,21 +1,21 @@
 package racingcar.validator;
 
-import racingcar.constants.ValidatorConstant;
+import static racingcar.constants.ValidatorConstant.*;
 
 public class TotalRoundValidator implements Validator {
     @Override
     public void validate(String totalRound) {
         if (isBlank(totalRound) || isNotNumeric(totalRound)) {
-            throw new IllegalArgumentException(ValidatorConstant.TOTAL_ROUND_ERROR_MESSAGE);
+            throw new IllegalArgumentException(TOTAL_ROUND_ERROR_MESSAGE);
         }
+    }
+
+    private boolean isBlank(String totalRound) {
+        return totalRound == null || totalRound.isBlank();
     }
 
     private boolean isNotNumeric(String totalRound) {
         return !totalRound.chars()
                 .allMatch(Character::isDigit);
-    }
-
-    private boolean isBlank(String totalRound) {
-        return totalRound == null || totalRound.isBlank();
     }
 }
