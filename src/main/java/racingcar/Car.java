9e7b798c5752af42164b03cf7ebe4d1c7bc77e3c
이예리 <leeyr0412@naver.com>
2023-11-01 22:31:53 +0900

    feat(Car): 자동차 클래스 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class Car {
+
+    private String name;
+    private int score;
+
+    public Car(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+}
