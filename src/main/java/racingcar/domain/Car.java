59c842628e3ea59b86f1d2a7c488689a75a65017
Jiihyun <cwlgusc@gmail.com>
2023-11-01 13:46:36 +0900

    refactor(Car): 안쓰는 메서드 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -31,10 +31,6 @@ public class Car {
         return new CarDto(name, position);
     }
 
-    public String getName() {
-        return name;
-    }
-
     public int getPosition() {
         return position;
     }
