39e0a3e3329f2fcb336b85a10e83c10322ceae07
AidenRoh <colohrby@gmail.com>
2023-11-01 10:33:45 +0900

    fix: fix validator activating condition

--- src/main/java/racingcar/validation/playtime/MinimumPlayTimeValidator.java
+++ src/main/java/racingcar/validation/playtime/MinimumPlayTimeValidator.java
@@ -6,7 +6,7 @@ import racingcar.exception.RacingCarException;
 public class MinimumPlayTimeValidator implements PlayTimeValidator {
     @Override
     public void validate(String inputValue) {
-        if (Integer.parseInt(inputValue) < 0) {
+        if (Integer.parseInt(inputValue) <= 0) {
             throw RacingCarException.of(ErrorMessage.MINIMUM_PLAY_TIME);
         }
     }
