ec86ff426275bf6c6c8b63595d645ca872074eb3
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-01 18:02:27 +0900

    chore: constants 패키지 생성
    MessageConstants를 위한 constants 패키지를 생성해 위치를 이동시켰다.

--- src/main/java/racingcar/constants/MessageConstants.java
+++ src/main/java/racingcar/constants/MessageConstants.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.constants;
 
 public final class MessageConstants {
     public static final String GAME_START_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.MessageConstants;
+import racingcar.constants.MessageConstants;
 import racingcar.validators.InputCarNameValidator;
 import racingcar.validators.InputTryCountValidator;
 
