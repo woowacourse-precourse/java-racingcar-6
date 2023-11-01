8b7496ede67e7df3850333a0d21613b75d4d7f50
fing9 <djajsl54@naver.com>
2023-11-01 17:18:10 +0900

    test : 자동차 객체 단위 테스트 추가

---
+++ src/test/java/racingcar/car/model/CarTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    void 자동차_이름_반환() {
+        Car car = new Car("자동차");
+
+        assertThat(car.getName()).isEqualTo("자동차");
+    }
+
+    @Test
+    void 자동차_위치_반환() {
+        Car car = new Car("자동차");
+
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+}
