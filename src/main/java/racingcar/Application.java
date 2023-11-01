aa78fe641bc759a4cb3768a1899d8d84e34aaa9e
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-11-01 22:35:32 +0900

    [feat] main부분 순차적 구현부

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.Controller.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame game = new RacingGame();
+        game.input();
+        game.run();
     }
 }
