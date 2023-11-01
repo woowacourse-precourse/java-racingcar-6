8a929b69ea4960f56518cd4e08a85337eca9df8e
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:32:18 +0900

    feat: 실행 횟수 입력 메소드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.Converter;
+import racingcar.utils.Validator;
 
 import java.util.List;
 
@@ -9,5 +10,14 @@ public class InputView {
     public List<String> readCars() {
         String input = Console.readLine();
         return Converter.toList(input);
+    }
+
+    public int readNumOfExecutions() throws IllegalArgumentException {
+        String input = Console.readLine();
+        Validator.validateDigit(input);
+
+        int numOfExecutions = Integer.parseInt(input);
+        Validator.validateRangeOfExecutions(numOfExecutions);
+        return numOfExecutions;
     }
 }
