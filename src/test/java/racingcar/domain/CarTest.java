1352abc65639c3cf26642af51629354c89279e58
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-01 22:19:22 +0900

    test(CarTest): 무작위값에 따라 자동차 전진 여부 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -11,7 +11,7 @@ class CarTest {
     void CarTest1() {
         String input = "abcdef";
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car(input));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car(input, new RandomNumberGenerator()));
     }
 
     @Test
@@ -19,7 +19,27 @@ class CarTest {
     void CarTest2() {
         String input = "abcde";
 
-        Car car = new Car(input);
+        Car car = new Car(input, new RandomNumberGenerator());
         Assertions.assertEquals(car.getName(), input);
+    }
+
+    @Test
+    @DisplayName("무작위 값이 4이상이면 전진한다.")
+    void CarTest3() {
+        String input = "abcde";
+
+        Car car = new Car(input, new FixedNumberGenerator(4));
+        car.moveForward();
+        Assertions.assertEquals(car.getPosition(), 1);
+    }
+
+    @Test
+    @DisplayName("무작위 값이 4이하면 전진하지 않는다.")
+    void CarTest4() {
+        String input = "abcde";
+
+        Car car = new Car(input, new FixedNumberGenerator(3));
+        car.moveForward();
+        Assertions.assertEquals(car.getPosition(), 0);
     }
 }
