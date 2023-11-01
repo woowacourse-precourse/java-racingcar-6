8e8dc3a47bba412c58fe5413668f2b58ab9426f4
seohokim <shk9946@gmail.com>
2023-11-01 23:03:42 +0900

    [FEAT] 초기 설정 init() 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -12,4 +12,17 @@ public class Racing {
         }
         System.out.println(result);
     }
+
+        private void init(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String inputcars = Console.readLine(); //자동차 이름 input
+        List<String> carnames = Arrays.asList(inputcars.split(",")); //Array로 변환
+        for (String name: carnames) {
+            this.cars.add(new Car(name)); // 객체 생성
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String inputNum = Console.readLine(); //횟수 input
+        numberOfRacing = Integer.parseInt(inputNum); //int로 변환
+    }
 }
