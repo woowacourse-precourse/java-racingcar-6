95e4c50cbcd5d0d503cf1eca49bff5a8a658e9dd
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-01 13:53:08 +0900

    Feat :Car 움직임 여부 판단 및 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,9 @@ import racingcar.utils.NumberGenerator;
 
 public class Car {
 
+    private static final int THRESHOLD = 4;
+
+
     private final String carName;
     private int position = 0;
     private final NumberGenerator numberGenerator;
@@ -13,4 +16,20 @@ public class Car {
         this.carName = carName;
         this.numberGenerator = numberGenerator;
     }
+
+    public void moveByRandomNumber() {
+        if (canMove()) {
+            position++;
+        }
+    }
+
+    private boolean canMove() {
+        int randomNumber = numberGenerator.generate();
+        return randomNumber >= THRESHOLD;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
 }
