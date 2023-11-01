83feb949488588c9b2870c2d106fdabe8a2d6def
권태현 <kygkth2011@gmail.com>
2023-11-01 23:17:01 +0900

    feat(OutputHandler) : StringJoiner를 통해 최종 우승자를 출력하는 printFinalResult() 구현

--- src/main/java/racingcar/OutputHandler.java
+++ src/main/java/racingcar/OutputHandler.java
@@ -1,8 +1,23 @@
 package racingcar;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 public class OutputHandler {
+    private static final String DELIMITER = ", ";
 
     public void printSubResult(Car car) {
         System.out.println(car);
+    }
+
+    public void printFinalResult(List<String> finalWinners) {
+        StringJoiner finalWinnersWithDelimiter = getFinalWinnerStringJoiner(finalWinners);
+        System.out.println(finalWinnersWithDelimiter);
+    }
+
+    private StringJoiner getFinalWinnerStringJoiner(List<String> finalWinners) {
+        StringJoiner joiner = new StringJoiner(DELIMITER);
+        finalWinners.forEach(item -> joiner.add(item));
+        return joiner;
     }
 }
