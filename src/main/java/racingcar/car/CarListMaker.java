04b4341944839b3006a7b2aa62c121854ac393c6
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 19:24:46 +0900

    feat : 자동차의 목록을 만드는 CarListMaker.java 생성

--- src/main/java/racingcar/car/CarListMaker.java
+++ src/main/java/racingcar/car/CarListMaker.java
@@ -1,9 +1,28 @@
 package racingcar.car;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.input.InputController;
+import racingcar.validation.CarNamesValidationChecker;
+
 public class CarListMaker {
-    InputController inputController = new InputController();
-    public List<String> makeCarList() {
-        String carNamesString = inputController.scanCarNames();
-        carNamesString
-    }
+
+	InputController inputController = new InputController();
+	CarNamesValidationChecker carNamesValidationChecker = new CarNamesValidationChecker();
+	private static final Integer InitialStartingStep = 0;
+	private static final String SplitingPoint = ",";
+
+	public List<Car> makeCarList() {
+		List<Car> carList = new ArrayList<Car>();
+		String carNamesString = inputController.scanCarNames();
+		try {
+			carNamesValidationChecker.checkCarNamesValidation(carNamesString);
+		} catch (IllegalArgumentException illegalArgumentException) {
+			throw illegalArgumentException;
+		}
+		for (String carName : List.of(carNamesString.split(SplitingPoint))) {
+			carList.add(new Car(carName, InitialStartingStep));
+		}
+		return carList;
+	}
 }
