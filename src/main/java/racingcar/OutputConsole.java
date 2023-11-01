da69c6a6d90f52a990b5f05fe62f1322dd36e824
yeop0740 <ssucgoo@gmail.com>
2023-11-01 18:18:01 +0900

    feat: add constant for status message

--- src/main/java/racingcar/OutputConsole.java
+++ src/main/java/racingcar/OutputConsole.java
@@ -12,6 +12,7 @@ public class OutputConsole {
     public static final String WINNER_MESSAGE = "최종 우승자";
     public static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String STATUS_MESSAGE = "실행 결과";
 
     static public void print(String message) {
         System.out.println(message);
