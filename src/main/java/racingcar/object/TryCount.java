6e14ea58d24d36787fa4287ac3550b12f539c092
gyeommm <dkndob02@naver.com>
2023-11-01 16:09:40 +0900

    TryCount 객체 분리 및 리팩토링

--- src/main/java/racingcar/object/TryCount.java
+++ src/main/java/racingcar/object/TryCount.java
@@ -1,2 +1,8 @@
-package racingcar.Object;public class TryCount {
+package racingcar.object;
+public class TryCount {
+    private int count;
+
+    public TryCount(int count) {
+        this.count = count;
+    }
 }
--- src/main/java/racingcar/service/TryCountService.java
+++ src/main/java/racingcar/service/TryCountService.java
@@ -1,26 +1,32 @@
-package racingcar;
+package racingcar.service;
+
+import racingcar.object.TryCount;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class TryCountService {
-    private static void printGuide_InputTryCount(){
+    public void printGuide_InputTryCount(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    private static Integer convert_StringToPositiveInteger(String string_try_count){
+    public int convert_StringToPositiveInt(String string_try_count){
+        if(string_try_count == null) {
+            throw new IllegalArgumentException("입력값이 존재해야합니다. 자연수를 입력해주세요.");
+        }
         try {
-            Integer int_try_count = Integer.valueOf(string_try_count);
+            int int_try_count = Integer.parseInt(string_try_count);
+
             if(int_try_count <= 0) {
                 throw new IllegalArgumentException("0 또는 음수는 입력할 수 없습니다. 자연수를 입력해주세요.");
             }
             return int_try_count;
         } catch(NumberFormatException e) {
-            throw new IllegalArgumentException("소수 또는 문자는 입력할 수 없습니다. 자연수를 입력해주세요.");
+            throw new IllegalArgumentException("소수와 문자는 입력할 수 없습니다. 자연수를 입력해주세요.");
         }
     }
 
-    private static Integer inputTryCount(){
+    public TryCount inputTryCount(){
         String input_try_count = readLine();
-        return convert_StringToPositiveInteger(input_try_count);
+        return new TryCount(convert_StringToPositiveInt(input_try_count));
     }
 }
