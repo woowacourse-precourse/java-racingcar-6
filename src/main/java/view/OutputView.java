b95544233af75394aed15c980bf1b7ed09d4e593
yujeonghyeon <dbdb1114@naver.com>
2023-11-01 20:59:52 +0900

    1. static으로 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,7 +3,7 @@ package view;
 import java.util.List;
 
 public class OutputView {
-    public void printResult(List<String> winnerList) {
+    public static void printResult(List<String> winnerList) {
         String winners = String.join(", ", winnerList);
         System.out.printf("최종 우승자 : %s", winners);
     }
