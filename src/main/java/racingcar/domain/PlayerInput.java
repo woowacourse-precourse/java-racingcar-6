6f03ca08858c2fe848aa33ce59235304c0c9160f
mini1115 <kmini9611@naver.com>
2023-11-01 21:36:48 +0900

    refactor : 입력값 순서보장을 위한 Hashmap -> LinkedHashMap 변경

--- src/main/java/racingcar/domain/PlayerInput.java
+++ src/main/java/racingcar/domain/PlayerInput.java
@@ -3,11 +3,12 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PlayerInput {
     public Map<String, Integer> nameInput() {
-        Map<String, Integer> racers = new HashMap<>();
+        Map<String, Integer> racers = new LinkedHashMap<>();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         Validate validate = new Validate();
