b4a43e9766ea27e21706022fefe0404010f809a6
gomdol <oioifor@naver.com>
2023-11-01 19:31:13 +0900

    feat(InputException) : 이름 길이, 공백, 빈칸 예외처리

--- src/main/java/racingcar/validation/InputException.java
+++ src/main/java/racingcar/validation/InputException.java
@@ -1,4 +1,24 @@
 package racingcar.validation;
 
 public class InputException {
+    public static void carNameLengthCheck(String input){
+        for (String name : input.split(",")) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름 길이를 초과하였습니다");
+            }
+        }
+
+    }
+
+    public static void containBlank(String input){
+        if(input.contains(" ")){
+            throw new IllegalArgumentException("공백이 포함되어 있습니다");
+        }
+    }
+
+    public static void black(String input){
+        if(input.isEmpty()){
+            throw new IllegalArgumentException("값을 입력해 주세요");
+        }]
+    }
 }
