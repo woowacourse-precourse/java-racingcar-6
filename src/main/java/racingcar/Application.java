0057bd4c90c4c57407d3d6707beed579e3fbd4a9
Hwang In Tae <sjobs66@gmail.com>
2023-11-01 16:50:29 +0900

    feat: run 구현
    추가적으로 GameController 생성자 타입 오류 수정함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,21 @@
 package racingcar;
 
 import racingcar.controller.GameController;
+import racingcar.controller.RacingCarGameController;
 import racingcar.model.Game;
+import racingcar.model.RacingCarGame;
 import racingcar.view.GameView;
-import racingcar.view.RepeatingGameView;
+import racingcar.view.RacingCarGameView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application.run(RacingCarGame.class, RacingCarGameView.class, RacingCarGameController.class,
+                "src/main/resources/messages/racing-car-game-messages.properties");
+    }
+
+    public static void run(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
+                           Class<? extends GameController> controllerClass, String messagePath) {
+        (new Application()).init(gameClass, viewClass, controllerClass, messagePath);
     }
 
     public void init(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
@@ -29,7 +37,7 @@ public class Application {
         }
 
         try {
-            gameController = controllerClass.getDeclaredConstructor(Game.class, RepeatingGameView.class)
+            gameController = controllerClass.getDeclaredConstructor(Game.class, GameView.class)
                     .newInstance(game, gameView);
         } catch (Exception e) {
             throw new IllegalArgumentException("게임 컨트롤러 인스턴스 생성 오류", e);
