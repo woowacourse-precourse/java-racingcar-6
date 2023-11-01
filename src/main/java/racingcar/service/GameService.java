4b2083a6460bcb76071abfd19f1bdbffbf5654ae
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-01 17:11:34 +0900

    리팩토링 - extract

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -8,14 +8,18 @@ public class GameService {
     static final ServiceOutputManager serviceOutputManager = new ServiceOutputManager();
 
 
-    public void execute(String cars, int number) {
+    public void execute(String cars, int times) {
         carManager = new CarManager(cars);
+        trial(times);
+        String winnerList = carManager.getWinnerList();
+        serviceOutputManager.winnerPrint(winnerList);
+
+    }
+
+    private void trial(int number) {
         for (int i = 0; i < number; i++) {
             carManager.goOrStop();
             carManager.getState();
         }
-        String winnerList = carManager.getWinnerList();
-        serviceOutputManager.winnerPrint(winnerList);
-
     }
 }
