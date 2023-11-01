f54fc06e41bf602a03f0dc3542f13c890e27ca24
nahyeon99 <nahyeonee99@gmail.com>
2023-11-01 20:06:19 +0900

    test(domain/CarTest): 자동차 이름 검증 테스트
    1. 길이가 1~5자인가
    2. 영어 소문자로만 구성되어있는가

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("자동차 이름은 영어 소문자로만 구성되고, 1~5자의 길이여야 합니다.")
+    void carNameFormatTest() {
+
+        String tooShortName = "";
+        Assertions.assertThatThrownBy(() -> new Name(tooShortName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+
+        String tooLongName = "tooLong";
+        Assertions.assertThatThrownBy(() -> new Name(tooLongName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        Name nameContainedNumber = new Name("poby1");
+        Assertions.assertThatThrownBy(() -> new Car(nameContainedNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
