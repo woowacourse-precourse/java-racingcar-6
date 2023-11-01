0e2244357f3cee16dd87790ac7318259b5e49762
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-01 20:17:02 +0900

    refactor: remove useless comment
    주석 지우기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
-    // Car 입력받기
-    // Round 입력받기
     private static final String CAR_NAME_QUESTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRIAL_QUESTION = "시도할 회수는 몇회인가요?";
 
