6cdb497dcfc16fdc22b1af522316e2c857a9d238
jeli01 <jeli01@naver.com>
2023-11-01 21:32:22 +0900

    refactor: change function position for readability

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -6,6 +6,10 @@ public class RacingCar {
     private String name;
     private int moveDistance = 0;
 
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
     public boolean isAvailableMove() {
         int num = Randoms.pickNumberInRange(0, 9);
         if (num >= 4) {
@@ -14,20 +18,16 @@ public class RacingCar {
         return false;
     }
 
+    public void moveForward() {
+        moveDistance += 1;
+    }
+
     public void printLocation() {
         System.out.print(name + " : ");
         for (int i = 0; i < moveDistance; i++) {
             System.out.print('-');
         }
         System.out.println();
-    }
-
-    public void moveForward() {
-        moveDistance += 1;
-    }
-
-    public RacingCar(String name) {
-        this.name = name;
     }
 
     public String getName() {
