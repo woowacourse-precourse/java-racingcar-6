db7c537ca714134d4852e01def7851d054920933
cheolhyeon <tghy4@naver.com>
2023-11-01 22:02:00 +0900

    refactor : 조건문의 숫자를 상수로 리팩토링하고, 상수 관련 유틸클래스에서 관리할 수 있도록 변경

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,5 +1,8 @@
 package racingcar.validation;
 
+import static racingcar.util.print.constants.RacingGameConstants.MIN_ADD_CAR;
+import static racingcar.util.print.constants.RacingGameConstants.MIN_CAR_NAME_LENGTH;
+
 import racingcar.user.User;
 
 public class Validator {
@@ -10,14 +13,14 @@ public class Validator {
     }
 
     public void checkCarNamesCount() {
-        if (user.getRacingCarNames().length <= 1) {
-            throw new IllegalArgumentException("?? 1?? ??? ??? ??????.");
+        if (user.getRacingCarNames().length <= MIN_ADD_CAR) {
+            throw new IllegalArgumentException("경주할 수 있는 자동차는 최소 1대 이상 등록해 주세요.");
         }
     }
     public void checkCarNamesLength() {
         for (String carName : user.getRacingCarNames()) {
-            if (carName.trim().length() > 5){
-                throw new IllegalArgumentException("??? ??? 5?? ????? ???.");
+        if (carName.trim().length() > MIN_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
             }
         }
     }
