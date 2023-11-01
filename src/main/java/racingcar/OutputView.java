03678497af9d2202002b49a20935edfd9657b9c5
jiwon1217 <koagjw@gmail.com>
2023-11-01 10:31:25 +0900

    :recycle: refactor: 우승자 출력 관련 메서드 OutputView로 분리

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
     public static void printExecutionResultText() {
         System.out.println("실행 결과");
@@ -8,5 +10,26 @@ public class OutputView {
     public static void printRoundResult(String result) {
         System.out.println(result);
         System.out.println();
+    }
+
+    public static void printWinners(List<Car> cars) {
+        List<String> winners = getWinnersName(cars);
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    private static List<String> getWinnersName(List<Car> cars) {
+        int maxPosition = getWinnersPosition(cars);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private static int getWinnersPosition(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getPosition)
+                .max(Integer::compare)
+                .orElse(0);
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -11,34 +11,12 @@ public class RacingGame {
 
         for (int count = 1; count <= round; count++) {
             proceedRound(cars);
-            System.out.println();
         }
-        printWinners(cars);
-    }
-
-    private void printWinners(List<Car> cars) {
-        List<String> winners = getWinnersName(cars);
-        System.out.println("최종 우승자: " + String.join(", ", winners));
+        OutputView.printWinners(cars);
     }
 
     private void proceedRound(List<Car> cars) {
         cars.forEach(Car::move);
         cars.forEach(car -> OutputView.printRoundResult(car.makeMoveResult()));
-    }
-
-    private List<String> getWinnersName(List<Car> cars) {
-        int maxPosition = getWinnersPosition(cars);
-
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .map(Car::getName)
-                .toList();
-    }
-
-    private int getWinnersPosition(List<Car> cars) {
-        return cars.stream()
-                .map(Car::getPosition)
-                .max(Integer::compare)
-                .orElse(0);
     }
 }
