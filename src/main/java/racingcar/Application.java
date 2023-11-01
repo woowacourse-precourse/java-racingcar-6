63ecd9f57f9ec6e950cf52f50be224fa9d2bef5a
ChanbyeongPark <coldbottle@korea.ac.kr>
2023-11-01 16:20:46 +0900

    feat(racingcar): 무작위 수에 따른 전진 여부 판단 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.Referee;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//        NumberGenerator generator = new NumberGenerator();
+//        int number = generator.createRandomNumber();
+//        System.out.println(number);
+
+        boolean moving1 = Referee.checkMoveForward();
+        boolean moving2 = Referee.checkMoveForward();
+        System.out.println(moving1);
+        System.out.println(moving2);
+
     }
 }
--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,5 +1,10 @@
 package racingcar.domain;
 
 public class Referee {
+    public static boolean checkMoveForward() {
+        NumberGenerator generator = new NumberGenerator();
+        int number = generator.createRandomNumber();
 
+        return number >= 4;
+    }
 }
