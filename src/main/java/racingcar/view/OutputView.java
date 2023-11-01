9e6e4ac275e2d66b1045e57d6d9eb2103aeca0cb
이지은 <jieun7056@naver.com>
2023-11-01 18:14:13 +0900

    feat: 횟수에 맞춰서 실행 결과 출력하기

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,15 @@ package racingcar.view;
 public class OutputView {
     public static void printResult() {
         System.out.println("\n실행 결과");
-
+    }
+    public static void printLineBreaking() {
+        System.out.print("\n");
+    }
+    public static void printResultDetail(String carName, int goNumber) {
+        System.out.print(carName+" : ");
+        for (int i = 0; i < goNumber; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
