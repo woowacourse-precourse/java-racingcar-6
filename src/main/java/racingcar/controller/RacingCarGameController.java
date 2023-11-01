45912278357c5794f9e229fab33600b89aae6065
KYBYK <dbqks56231106@gamil.com>
2023-11-01 19:08:07 +0900

    Feat : start에 기능 추가
    - 이름 입력값 나누어 car 생성하여 cars에 전달
    - game에 시도 횟수 전달

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,12 +1,26 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Game;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.List;
+
 public class RacingCarGameController {
+    Game game = new Game();
+    Cars cars = new Cars();
+
     public void start() {
         String nameData = InputView.askName();
+        List<String> name = List.of(nameData.replace(" ", "").split(","));
+        for(String carName : name) {
+            cars.add(new Car(carName));
+        }
+
         String TryNumberData = InputView.askTryNumber();
+        game.setTryNumber(TryNumberData);
     }
 
     public void inProgress() {
