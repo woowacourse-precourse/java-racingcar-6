862ed8a9328523d65b6b6fdd2724ec4a0f03c5b2
KimMinhee <99alsgmlalsl@naver.com>
2023-11-01 19:30:53 +0900

    Feat: GameRunner 내 랜덤값에 따른 자동차 경주 기능 구현

--- src/main/java/racingcar/GameRunner.java
+++ src/main/java/racingcar/GameRunner.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import racingcar.domain.car.Car;
 import racingcar.domain.car.GameCount;
 import racingcar.domain.car.Name;
 import racingcar.domain.car.NameParser;
+import racingcar.domain.car.Position;
+import racingcar.domain.car.RacingGame;
+import racingcar.domain.random.RandomNumberGenerator;
 import racingcar.io.ConsoleManager;
 
 import java.util.List;
@@ -12,15 +16,24 @@ import static racingcar.util.StringConverter.*;
 public class GameRunner {
     private final ConsoleManager consoleManager;
     private final NameParser nameParser;
+    private final RacingGame racingGame;
 
     public GameRunner(){
         consoleManager = new ConsoleManager();
         nameParser = new NameParser();
+        racingGame = new RacingGame(new RandomNumberGenerator());
     }
 
     public void run(){
-        readCarNames();
-        readGameCount();
+        List<Car> cars= createCars(readCarNames());
+        GameCount count = readGameCount();
+        playGame(cars, count);
+    }
+
+    private List<Car> createCars(List<Name> names){
+        return names.stream()
+                .map(n -> new Car(n,new Position(0)))
+                .toList();
     }
 
     private List<Name> readCarNames(){
@@ -31,6 +44,12 @@ public class GameRunner {
     private GameCount readGameCount(){
         String strCount = consoleManager.readGameCount();
         return new GameCount(toInteger(strCount));
+    }
+
+    private void playGame(List<Car> cars, GameCount count){
+        for(int i=0; i<count.getCount(); i++){
+            racingGame.play(cars);
+        }
     }
 
 }
