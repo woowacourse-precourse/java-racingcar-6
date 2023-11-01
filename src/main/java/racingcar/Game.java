fe6a85c46b28942d439e9a8b9dc663afef227db3
kmss6905 <kmss69052@naver.com>
2023-11-01 21:19:58 +0900

    Feature: 출전하는 레이싱카 입력 안내문 출력

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class Game {
+    private final OutView outView = new OutView();
+
+    public void run() {
+        // 출전할 레이싱카를 입력받아 출력한다.
+        outView.printInputRacingCar();
+
+        // 시도할 횟수를 입력받는다.
+
+        // 게임을 시작한다. (로직을 진행한다)
+
+        // 게임결과 출력한다.
+    }
+}
