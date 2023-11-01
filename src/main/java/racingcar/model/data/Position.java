f141d82048e659f58896ea3e40364c2d62223052
moong2 <www.castlehi@gmail.com>
2023-11-01 18:09:00 +0900

    refactor: make position to package-private

--- src/main/java/racingcar/model/data/Position.java
+++ src/main/java/racingcar/model/data/Position.java
@@ -3,14 +3,10 @@ package racingcar.model.data;
 import static racingcar.util.GameConstants.INITIAL_POSITION;
 
 public class Position {
-    private int position;
+    int position;
 
     public Position() {
         this.position = INITIAL_POSITION;
-    }
-
-    public int getPosition() {
-        return this.position;
     }
 
     public void forward() {
