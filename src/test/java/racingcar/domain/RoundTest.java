7c3ad4f514b845ae92c40db38a1b394d5d7841fd
Suyoung Oh <ohsusu515@gmail.com>
2023-11-01 15:14:05 +0900

    test: 한 라운드 진행 후 위치변화 확인 테스트

--- src/test/java/racingcar/domain/RoundTest.java
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -37,4 +37,33 @@ class RoundTest {
         assertEquals("bbb", cars.get(1).getName());
         assertEquals("ccc", cars.get(2).getName());
     }
+
+    @DisplayName("한 라운드를 진행하며 차마다 위치변화가 있는지 확인한다.")
+    @Test
+    void 한_라운드_진행_후_위치변화_확인() {
+        // given
+        List<String> carNames = new ArrayList<>();
+        carNames.add("aaa");
+        carNames.add("bbb");
+        carNames.add("ccc");
+        round.generateCars(carNames);
+
+        List<Car> cars = round.getCars();
+
+        // when
+        cars.forEach(car -> {
+            int originalCnt = car.getMoveCnt();
+            int randomNumber = car.generateNumber();
+            car.moveForward(randomNumber);
+
+            // then
+            int newCnt = car.getMoveCnt();
+            if (car.isMove(randomNumber)) {
+                assertEquals(originalCnt + 1, newCnt);
+            } else {
+                assertEquals(originalCnt, newCnt);
+
+            }
+        });
+    }
 }
