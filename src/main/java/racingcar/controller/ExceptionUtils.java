7b252583d208e0c1abd0d72bd37c890ea6447ac0
ud00ng <dagam1240@gmail.com>
2023-11-01 23:50:09 +0900

    feat : 예외 메소드 추가

--- src/main/java/racingcar/controller/ExceptionUtils.java
+++ src/main/java/racingcar/controller/ExceptionUtils.java
@@ -1,31 +1,41 @@
 package racingcar.controller;
 
 import static racingcar.model.Car.MAX_LENGTH;
-public class ExceptionUtils {
-    private ExceptionUtils() { }
 
-    private static void checkEmpty(String carName) { //빈 값의 경우
+import racingcar.model.Car;
+import racingcar.model.Racing;
+
+public class ExceptionUtils {
+    public ExceptionUtils() { }
+
+    public boolean checkEmpty(String carName) { //빈 값의 경우
         if (carName.equals("")) {
             throw new IllegalArgumentException("입력된 자동차 이름이 없습니다.");
         }
+        return true;
     }
 
-    private static void checkLength(String carName) { //글자 체크
+    public boolean checkCountString(String count) {
+        try {
+            Integer.parseInt(count);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean checkLength(String carName) { //글자 체크
         if (carName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 " + MAX_LENGTH + "글자 이하여야 합니다.");
         }
+        return true;
     }
 
-    private static void checkExistCar(String carArray) { //중복 이름 금지
+    public boolean checkExistComma(String carArray) {
         if(!carArray.contains(",")) {
-            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+            throw new IllegalArgumentException();
         }
-    }
-
-    public static void checkAllName(String carName) {
-        checkEmpty(carName);
-        checkLength(carName);
-        checkExistCar(carName);
+        return true;
     }
 
 }
