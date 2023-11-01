85e2c5727b76ff6e961f77a2aa9cb24911a93f38
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-11-01 21:17:53 +0900

    feat: 참가자 이름의 유효성 검사 기능 추가

--- src/main/java/util/NameValidator.java
+++ src/main/java/util/NameValidator.java
@@ -1,4 +1,36 @@
 package util;
 
+import racingcar.CarConstants;
+
 public class NameValidator {
+
+        public static boolean isValidInput(String input) {
+            String[] names = input.split(",");
+            for (String name : names) {
+                if (name.trim().isEmpty())
+                    return false;
+                if(!isValidLength(name))
+                    return false;
+                if(!isAlphabet(name))
+                    return false;
+            }
+            return true;
+        }
+    public static boolean isValidLength(String name) {
+        if(name.length() > CarConstants.MAX_NAME_LENGTH)
+            return false;
+        if(name.length() < CarConstants.MIN_NAME_LENGTH)
+            return false;
+
+        return true;
+    }
+
+    public static boolean isAlphabet(String name) {
+        for (char c : name.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
