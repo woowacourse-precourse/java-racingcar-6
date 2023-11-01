a1b10a8f0f42028a6ad4dd9bebf69dc4a3f8a1ac
Annjieun <dkslwdms2121@naver.com>
2023-11-01 16:31:39 +0900

    style(ExceptionMessage): BLANK_EXCEPTION 메시지 변경

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -5,7 +5,7 @@ import static racingcar.constant.GameNumber.NAME_LENGTH_LIMIT;
 
 public class ExceptionMessage {
     public static final String INVALID_NAME_EXCEPTION = "이름이 유효하지 않습니다.";
-    public static final String BLANK_EXCEPTION = "공백을 입력하였습니다.";
+    public static final String BLANK_EXCEPTION = "공백 문자를 입력하였습니다.";
     public static final String LENGTH_EXCEPTION = "이름은 " + NAME_LENGTH_LIMIT + "자 이내로 입력해주세요.";
     public static final String DUPLICATE_EXCEPTION = "중복된 이름이 있습니다.";
     public static final String NON_INTEGER_EXCEPTION = "올바른 양의 정수를 입력해주세요.";
