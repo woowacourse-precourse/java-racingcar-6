cabd1b54614c7559770b8b13a201c82230e628de
ddshpp <dshp753@gmail.com>
2023-11-01 21:28:40 +0900

    feat: 경주할 자동차의 이름을 쉼표로 구분하여 입력받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,14 @@
 package racingcar;
 
+import view.InputView;
+
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] inputCarNames = InputView.inputCarNames();
+
+        for (int i = 0; i < inputCarNames.length; i++) {
+            System.out.println(inputCarNames[i]);
+        }
     }
 }
