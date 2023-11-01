b29bae33acf12204b70c1091c320275756eeec9c
sungjinlee <qdlsungls@gmail.com>
2023-11-01 12:53:34 +0900

    RacingCarGame.java 수정 - delimeter 클래스 변수 추가

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -2,7 +2,10 @@ package racingcar.domain;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 class RacingCarGame {
+	static final String delimeter = ",";
 	int numberOfAttempts;
 	int numberOfCars;
 	List<Car> cars;
