3a9d6d03b807af83a24a0d2431c9a22f00861f0e
Minyongggg <naminyong97@gmail.com>
2023-11-01 20:05:23 +0900

    feat: 게임을 초기 세팅하는 기능 구현
    - 자동차 이름
    - 라운드 횟수

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,4 +1,32 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.util.StringParser;
+import racingcar.view.ConsoleScanner;
+
 public class RacingGame {
+    private Cars cars;
+    private Integer round;
+
+    public RacingGame() {
+    }
+
+    public void initiate() {
+        initiateCars();
+        initiateRound();
+    }
+
+    private void initiateCars() {
+        String carNameInput = ConsoleScanner.readCarName();
+        List<String> carNames = StringParser.parseCarName(carNameInput);
+        this.cars = new Cars(carNames);
+    }
+
+    private void initiateRound() {
+        String roundInput = ConsoleScanner.readRound();
+        this.round = StringParser.parseRound(roundInput);
+    }
 }
