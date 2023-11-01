e35e1ecb64565fc468a083db08501dcefb9e74b4
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 22:47:46 +0900

    [refactor] : change method logic
    GameService() : delete
    moveCar() : add constants parameters
    findWinners() : add return value logic with parameters
    findMaxPosition() : add parameters
    findWinnerCar() : add parameters

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -2,43 +2,40 @@ package racingcar.service;
 
 import racingcar.domain.Car;
 import racingcar.domain.CarName;
+import racingcar.domain.Constants;
 import racingcar.domain.Winner;
-import racingcar.utils.RandomNumber;
+import racingcar.utils.RandomUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameService {
 
-    private final List<Car> cars = new ArrayList<>();
-    private int maxPosition;
+    public static List<Car> createCar(List<String> carNames) {
 
-    public GameService(List<String> carNames) {
-        carNames.forEach(carName -> cars.add(new Car(new CarName(carName))));
+        return carNames.stream()
+                .map(CarName::new)
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public void moveCar(List<Car> cars) {
+        cars.forEach(car -> car.move(RandomUtils.createRandomNumber(Constants.MIN_RANDOM_NUMBER,Constants.MAX_RANDOM_NUMBER)));
     }
 
-    public void moveCar() {
-        cars.forEach(car -> car.move(RandomNumber.createRandomNumber()));
+    public Winner findWinners(List<Car> cars) {
+        int maxPosition = findMaxPosition(cars);
+        return findWinnerCar(cars, maxPosition);
     }
 
-    public Winner findWinners() {
-        findMaxPosition();
-        return findWinnerCar();
-    }
-
-    private void findMaxPosition() {
-        this.maxPosition = cars.stream()
+    private int findMaxPosition(List<Car> cars) {
+        return cars.stream()
                 .map(Car::getPosition)
                 .max(Integer::compareTo)
                 .orElse(Integer.MIN_VALUE); // 예외처리하기
     }
 
-    private Winner findWinnerCar() {
+    private Winner findWinnerCar(List<Car> cars, int maxPosition) {
         List<String> winnerCarNames = cars.stream()
                 .filter(car -> car.getPosition() == maxPosition)
                 .map(Car::getName)
