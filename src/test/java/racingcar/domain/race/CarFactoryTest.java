d30e7b65fcd13713067ffcb8c8e086c2627812dc
lkdcode <dlrlejr1@naver.com>
2023-11-01 15:12:47 +0900

    Test : CarFactoryTest

---
+++ src/test/java/racingcar/domain/race/CarFactoryTest.java
@@ -0,0 +1,23 @@
+package racingcar.domain.race;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+
+class CarFactoryTest {
+
+    @Test
+    void Car_Factory_다른_인스턴스_생성_테스트() {
+        // given
+        final String NAME = "pobi";
+
+        // when
+        Car car1 = CarFactory.newIntanceCar(NAME);
+        Car car2 = CarFactory.newIntanceCar(NAME);
+
+        // then
+        Assertions.assertThat(car1)
+                .isNotSameAs(car2);
+    }
+
+}
