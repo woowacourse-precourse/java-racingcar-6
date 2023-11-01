e03c233d76ea5b8340d85f55b72250c090ff4bb1
Sur0n <skyzephyr333@gmail.com>
2023-11-01 16:35:11 +0900

    feat : Write down GameControl and ValidateWinner

--- src/main/java/domain/GameControl.java
+++ src/main/java/domain/GameControl.java
@@ -1,5 +1,17 @@
 package domain;
+
+
+import static constant.GameUserInterface.*;
+import static domain.CarDrive.*;
+
+
 import domain.Car;
+import domain.GameInput;
+
+
+import java.util.List;
+//import static 해야지 enum 가져올 수 있음
+
 public class GameControl {
     private GameControl(){
 
@@ -7,10 +19,24 @@ public class GameControl {
 
     public static void start(){
 
+        System.out.println(ASK_CAR_NAME.getUserInterface());
+        List<String> carNames = GameInput.userCarNameInput();
+
+        System.out.println(ASK_GAME_TRYS.getUserInterface());
+        int tries = GameInput.userGameTries();
+
+        System.out.println(RACING_RESULT.getUserInterface());
+        //car에서 => carNameOutput() => carDistance() 입력순
+        getCarNames(carNames);
+        driving(tries);
+
+
     }
 
     public static void stop(){
 
+        GameOutput.gameResult(CarDrive.Winner());
+        GameInput.inputStop();
     }
 
 }
--- src/main/java/validate/ValidateWinner.java
+++ src/main/java/validate/ValidateWinner.java
@@ -1,13 +1,48 @@
 package validate;
 
+import domain.Car;
+import domain.GameOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ValidateWinner {
 
     private ValidateWinner(){
 
     }
 
-    private static String checkJointWinner(){
-        return "";
+    public static String checkJointWinner(Car[] carslist,int carnumbers) {
+        List<String> cars = new ArrayList<>();
+        List<Integer> integerdistance= new ArrayList<>();
+        int max = 0;
+        List<String> winner = new ArrayList<>();
+        String answers ="";
+        String commas= (", ");
+        // 카 이름과 distance length를 각 List에 박아넣자.
+
+        for (int i = 0; i < carnumbers; i++) {
+            cars.add(carslist[i].getCarname());
+            integerdistance.add(carslist[i].getDistance().length());
+            if (max<integerdistance.get(i)){
+                max=integerdistance.get(i);
+            }
+        }
+        for (int i = 0; i < carnumbers; i++) {
+
+            if (max==integerdistance.get(i)){
+                winner.add(cars.get(i));
+            }
+        }
+        // 4명이서 해서 공동우승 4면
+        // 0 1 2 3
+        // 0 , 2 3 4
+        // 0 , 2 , 4 , 6
+        // 홀수번째 리스트에 추가해주면 됨
+        // String.join(구분 스트링,list) 할 시 구분점 잡아서 리턴
+        answers = String.join(", ",winner);
+
+        return answers;
     }
 
 }
