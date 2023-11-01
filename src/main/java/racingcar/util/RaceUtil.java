2730e13ed6a101d4dfe0b722f5a2eeec424bc7f3
morenow <jjang9877@gmail.com>
2023-11-01 21:20:12 +0900

    refactor: 자동차 움직임 로직 분할

--- src/main/java/racingcar/util/RaceUtil.java
+++ src/main/java/racingcar/util/RaceUtil.java
@@ -5,9 +5,16 @@ import racingcar.domain.constant.ConstNumber;
 
 public final class RaceUtil {
 
-    public static boolean isMove () {
-        int randomNum = Randoms.pickNumberInRange(ConstNumber.MIN_RANDOM_NUMBER.getValue(),
-                ConstNumber.MAX_RANDOM_NUMBER.getValue());
+    public static boolean isMove() {
+        return isMove(createRandomNumber());
+    }
+
+    public static boolean isMove(int randomNum) {
         return randomNum >= ConstNumber.MOVE_CRITERIA.getValue();
+    }
+
+    private static int createRandomNumber() {
+        return Randoms.pickNumberInRange(ConstNumber.MIN_RANDOM_NUMBER.getValue(),
+                ConstNumber.MAX_RANDOM_NUMBER.getValue());
     }
 }
