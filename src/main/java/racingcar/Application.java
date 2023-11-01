42b826f9b7eb29e58a99acb7684cdcbc7caaf0cd
minu512 <minuet1215@jbnu.ac.kr>
2023-11-01 16:46:07 +0900

    feat(Application): 시도 회수를 입력받고 정수로 변환하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,12 +4,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import racingcar.domain.Car;
 import racingcar.domain.CarGroup;
+import racingcar.validator.InputValidator;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         CarGroup carGroup = new CarGroup();
         inputCars(carGroup);
+        int tryCount = inputCount();
+    }
+
+    private static int inputCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = readLine();
+        new InputValidator().validateTryCount(input);
+        return Integer.parseInt(input);
     }
 
     private static void inputCars(CarGroup carGroup) {
