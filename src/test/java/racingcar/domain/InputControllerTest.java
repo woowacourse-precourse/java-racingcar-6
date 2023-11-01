4844ae071b3afc16962412289f5597800249cddc
leejuno <zhqkfxm0127@naver.com>
2023-11-01 22:25:05 +0900

    refactor(InputControllerTest): 테스트 코드 재구성

--- src/test/java/racingcar/domain/InputControllerTest.java
+++ src/test/java/racingcar/domain/InputControllerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,11 +14,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputControllerTest {
+    private InputController inputController;
+
+    @BeforeEach
+    void setUp() {
+        inputController = new InputController();
+    }
 
     @Test
     @DisplayName("입력받은 이름으로 자동차를 생성하는 기능")
     void setCar_입력받은_이름으로_자동차를_생성하는_기능() {
-        InputController inputController = new InputController();
         String input = "pobi,woni,jun";
 
         List<Car> carList = inputController.setCar(input);
@@ -31,7 +37,6 @@ class InputControllerTest {
     @ValueSource(strings = {"pobiwoni,jun", "pobiwonijun", "pobibi,woni,jun"})
     @DisplayName("입력받은 이름양식이 잘못된 경우 예외처리")
     void setCar_입력받은_이름양식이_잘못된_경우_예외처리(String carNames) {
-        InputController inputController = new InputController();
 
         assertThatThrownBy(() -> inputController.setCar(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
