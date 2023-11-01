10ebf46847379dc1bc16c3aa264fb569541c7f0a
nature1216 <annekim1216@naver.com>
2023-11-01 22:02:35 +0900

    refactor: 사용하지 않는 메소드와 클래스 삭제 사용하지 않는 메소드와 클래스 삭제

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -13,16 +13,8 @@ public class Car {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCurrent() {
         return current;
-    }
-
-    public void setCurrent(int current) {
-        this.current = current;
     }
 
     public void forward(int rNum, int limit) {
