f50b93e2f79b6c539900fcec4531ace5cb05c1f2
최휘용 <chy2581@naver.com>
2023-11-01 15:40:03 +0900

    style: 코드 컨벤션 처리
    Circuit, ApplicationTest의 코드 컨벤션을 맞추기 위해 수정했습니다

--- src/main/java/racingcar/Circuit.java
+++ src/main/java/racingcar/Circuit.java
@@ -17,7 +17,7 @@ public class Circuit {
     public void makeCarList(String input) {
 
         String[] splitInput = input.split(",");
-        // 마지막이 쉼표 하나로 끝나는 경우 split에서 사라지므로 || 이후에 별도로 예외처리
+        // 마지막이 쉼표 하나로 끝나는 경우 splitinput에서 사라지므로 '||' 이후에 별도로 예외처리
         if (checkInputError(splitInput) || input.charAt(input.length() - 1) == ',') {
             throw new IllegalArgumentException();
         }
