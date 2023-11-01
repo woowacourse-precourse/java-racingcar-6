5b954d5ae5b1a82c27ecbf803d7a8bbdd5e29c0c
jihun <pjh2996@naver.com>
2023-11-01 11:20:13 +0900

    test : 최종 우승자 테스트 통합

--- src/test/java/racingcar/model/CarGameTest.java
+++ src/test/java/racingcar/model/CarGameTest.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarGameTest {
 
-    private CarGame carGame = new CarGame(new ArrayList<>());
+    private CarGame carGame;
+
+    @BeforeEach
+    void setUp() {
+        carGame = new CarGame(new ArrayList<>());
+    }
 
     @Test
     public void trim_메서드_이름_입력시_양쪽_공백은_문자_길이에_미포함() {
@@ -23,11 +31,11 @@ class CarGameTest {
         Assertions.assertThat(5).isEqualTo(length);
     }
 
-
-    @Test
-    public void 최종_우승자_1명_테스트() {
+    @ParameterizedTest
+    @CsvSource(value = {"JiHun:Mac:2:1:JiHun", "JiHun:Mac:1:1:JiHun,Mac"}, delimiter = ':')
+    public void 최종_우승자_테스트(String car1, String car2, int distance1, int distance2, String expected) {
         //given
-        List<Cars> input = Arrays.asList(new Cars("JiHun", 2), new Cars("Mac", 1));
+        List<Cars> input = Arrays.asList(new Cars(car1, distance1), new Cars(car2, distance2));
         carGame = new CarGame(input);
 
         //when
@@ -35,20 +43,7 @@ class CarGameTest {
         String winner = String.join(",", result);
 
         //then
-        Assertions.assertThat("JiHun").isEqualTo(winner);
+        Assertions.assertThat(expected).isEqualTo(winner);
     }
 
-    @Test
-    public void 최종_우승자_2명_테스트() {
-        //given
-        List<Cars> input = Arrays.asList(new Cars("JiHun", 2), new Cars("Mac", 2));
-        carGame = new CarGame(input);
-
-        //when
-        List<String> result = carGame.getWinner();
-        String winner = String.join(",", result);
-
-        //then
-        Assertions.assertThat("JiHun,Mac").isEqualTo(winner);
-    }
 }
