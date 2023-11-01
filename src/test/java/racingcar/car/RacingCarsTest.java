35ccc3d65720f2766b3da1892b04e8f027507c7c
namtndus <rnrmf0721@gmail.com>
2023-11-01 15:07:29 +0900

    test(RacingCarsTest): 자동차 경기의 실행 결과를 반환하는 함수 테스트

---
+++ src/test/java/racingcar/car/RacingCarsTest.java
@@ -0,0 +1,17 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarsTest {
+
+    RacingCars racingCars = new RacingCars(new String[]{"pobi","hash","neo"});
+
+    @Test
+    void output_자동화_경기의_실행_결과가_잘_출력_되는지_확인(){
+        assertThat(racingCars.toString()).isEqualTo("pobi : ");
+    }
+
+}
