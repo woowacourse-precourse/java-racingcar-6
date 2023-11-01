edf343229fb734ce007847695178091e65ebf2b1
EunhoRoh <deu04189@gmail.com>
2023-11-01 21:10:37 +0900

    feat: racingGameController add

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingGameController;
+
 public class Application {
+
+    private static final RacingGameController racingGameController = new RacingGameController();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        racingGameController.run();
+        Console.close();
     }
 }
