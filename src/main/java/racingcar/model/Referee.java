6bc4446a044a02632dc4dffb55e695b1b0d8fadb
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-01 22:50:22 +0900

    feat(Referee): 최종 우승자 계산 기능 추가

--- src/main/java/racingcar/model/Referee.java
+++ src/main/java/racingcar/model/Referee.java
@@ -1,9 +1,13 @@
 package racingcar.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Referee {
-    public List<String> getWinner() {
-        return null;
+    public List<Car> getWinner(List<Car> carList) {
+        int maxStatus = Collections.max(carList.stream().map(Car::getStatus).toList());
+        return carList.stream()
+                .filter(car -> car.getStatus() == maxStatus)
+                .toList();
     }
 }
