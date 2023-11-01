f2aeab30a57f5ffda634a55c588674d01a1e9290
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-01 11:37:12 +0900

    refactor: ReaderTest 리팩터링
    - 함수명 콘솔_입력 -> 입력으로 rename
    - String을 바로 입력()에 인자로 전달

--- src/test/java/racingcar/ReaderTest.java
+++ src/test/java/racingcar/ReaderTest.java
@@ -17,26 +17,23 @@ class ReaderTest {
 
     @Test
     void 문자_형태_시도_횟수_입력_시_예외_발생() {
-        String input = "no";
-        콘솔_입력(input);
+        입력("no");
         assertThatThrownBy(() -> Reader.attempts()).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 실수_형태_시도_횟수_입력_시_예외_발생() {
-        String input = "1.23";
-        콘솔_입력(input);
+        입력("1.23");
         assertThatThrownBy(() -> Reader.attempts()).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 정수_형태_시도_횟수_입력_시_해당하는_정수값_반환() {
-        String input = "10";
-        콘솔_입력(input);
+        입력("10");
         assertThat(Reader.attempts()).isEqualTo(10);
     }
 
-    private void 콘솔_입력(String input) {
+    private void 입력(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
     }
 
