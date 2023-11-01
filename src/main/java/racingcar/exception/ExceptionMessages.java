e88df250562074d5aa150c4a33fa6b68e0cc7c0c
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-01 23:31:25 +0900

    refactor(): 에러 메세지 안내

---
+++ src/main/java/racingcar/exception/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package racingcar.exception;
+
+public class ExceptionMessages {
+    public static final String INVALID_CAR_NAME_LENGTH = "[ERROR] 자동치 이름은 1~5자의 문자이어야 합니다.";
+    public static final String DUPLICATED_CAR_NAME = "[ERROR] 중복된 이름이 존재합니다";
+    public static final String NOT_NUMBER_TRY_COUNT = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+    public static final String NOT_NATURAL_TRY_COUNT = "[ERROR] 시도할 횟수는 0 이상이어야 합니다.";
+}
