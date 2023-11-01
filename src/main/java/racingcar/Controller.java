bf87a3acd463325963bf603620a805342013c042
jagarchi <relong1026@naver.com>
2023-11-01 20:31:55 +0900

    어플리케이션에서 실행 //controller에서 필요없는 것 지우기

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,5 +1,6 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
+
 public class Controller {
     public void gameStart(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
