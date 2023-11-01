c61d69ce46799f7f8e2690bb52f2c05d053b42cd
noah <jeonghogim318@gmail.com>
2023-11-01 17:44:07 +0900

    feat: 게임정보저장기능

--- src/main/java/racingcar/controller/Play.java
+++ src/main/java/racingcar/controller/Play.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,4 +22,31 @@ public class Play {
         //최종승자메세지생성기능
         championMessage();
     }
+    
+    public void inputGameInformation(){
+        //자동차 이름 저장(inputCarNmaeInformation)
+        String carName = Console.readLine();
+        //자동차 이름 공백 예외 던지기
+        varifyCarNameBlank(carName);
+        // 자동차 이름 맵에 저장
+        String[] carNameStringArray = carName.split(",");
+        // 자동차 이름 5자 이상 예외처리
+        varifyCarNameLength(carNameStringArray);
+        //맵에 저장
+        for (String carNameInformation : carNameStringArray) {
+            racerInformation.put(carNameInformation, 0);
+        }
+
+        System.out.println(COUNT_MESSAGE);
+
+        // 게임시도횟수 입력(inputTryNumberInformation)
+        String checkGameTryNumber = Console.readLine();
+        //공백 예외 던지기
+        varifyCarNameBlank(checkGameTryNumber);
+        //숫자가 아니면 예외 던지기
+        varifyTryNumberIsNumeric(checkGameTryNumber);
+        // 게임시도횟수 저장
+        gameTryNumber = Integer.parseInt(checkGameTryNumber);
+    }
+
 }
