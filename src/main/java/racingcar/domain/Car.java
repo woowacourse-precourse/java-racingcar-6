bf44f23d54bd8aab5d9fe0ba11b2c627f6d51c3d
jang-namu <ironhee8005@gmail.com>
2023-11-01 20:02:31 +0900

    feat: 자동차의 이동/정지 행동하는 기능. docs: 기능내용 수정 before 자동차가 이동할지 정지해있을지를 결정하는 기능. after 자동차의 이동/정지 행동하는 기능.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position;
@@ -9,4 +11,10 @@ public class Car {
         this.position = 0;
     }
 
+    public void moveOrNot() {
+        int randInt = Randoms.pickNumberInRange(0, 9);
+        if (randInt >= 4) {
+            position++;
+        }
+    }
 }
