cc91a3cbb88f4192d1c09b70c4f2357ba9504153
권용하 <yongha.kwon12@gmail.com>
2023-11-01 17:15:00 +0900

    feat : add main method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.application.CarGameService;
+import racingcar.domain.Referee;
+import racingcar.infra.MoveCommandMakerImpl;
+import racingcar.presentation.CarGameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarGameService carGameService = new CarGameService(new MoveCommandMakerImpl(), new Referee());
+        CarGameController carGameController = new CarGameController(carGameService);
+        try {
+            carGameController.startGame();
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
