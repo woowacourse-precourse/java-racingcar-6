8c7a944739e3409a49af8e936a7f2b69aec71b99
gmu1026 <curiouscomponent@gmail.com>
2023-11-01 11:31:52 +0900

    FIX: 차 이름 길이 제한 기능 구현

--- src/main/java/racingcar/car/CarFactory.java
+++ src/main/java/racingcar/car/CarFactory.java
@@ -5,18 +5,23 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class CarFactory {
-    private CarFactory() {}
+private CarFactory() {
+    }
+
     private static final Pattern specialCharacterPattern = Pattern.compile("[ !@#$%^&*(),.?\":{}|<>]");
     private static final String CAR_SEPARATOR = ",";
+    private static final int LIMIT_NAME_LENGTH = 5;
+    private static final String LIMIT_LENGTH_EXCEPTION_MESSAGE = "이름은 5자 이하로 입력해주세요.";
+    private static final String SPECIAL_CHARACTER_EXCEPTION_MESSAGE = "이름에 특수문자는 사용할 수 없습니다.";
 
     public static List<Car> createCar(String name) {
         List<String> names = splitCarName(name);
-        for (String carName:names) {
+        for (String carName : names) {
             validateName(carName);
         }
 
         List<Car> cars = new ArrayList<>();
-        for (String carName:names) {
+        for (String carName : names) {
             cars.add(Car.createCar(carName));
         }
 
@@ -28,8 +33,12 @@ public class CarFactory {
     }
 
     private static void validateName(String name) {
+        if (name.length() > LIMIT_NAME_LENGTH) {
+            throw new IllegalArgumentException(LIMIT_LENGTH_EXCEPTION_MESSAGE);
+        }
+
         if (specialCharacterPattern.matcher(name).find()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SPECIAL_CHARACTER_EXCEPTION_MESSAGE);
         }
     }
 }
