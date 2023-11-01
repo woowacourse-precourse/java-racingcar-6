6ebd72f397af97ed953f21ea70549e7a432a6a4b
jjw0071 <jjw0071@naver.com>
2023-11-01 21:20:27 +0900

    feat(Judgment) : 전진거리를 비교해 레이스 결과를 알려주는 기능 추가

--- src/main/java/racingcar/domain/Judgment.java
+++ src/main/java/racingcar/domain/Judgment.java
@@ -1,5 +1,24 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Judgment {
+    public List<String> compare(List<Car> carsAfteRace){
+        List<String> winners = new ArrayList<>();
+        int maxScore = -1;
+
+        for(Car car : carsAfteRace){
+            if(car.getScore()>maxScore){
+                maxScore = car.getScore();
+                winners.clear();
+                winners.add(car.getName());
+            }else if(car.getScore() == maxScore){
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
 
 }
