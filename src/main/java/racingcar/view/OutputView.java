0a445292e7115f3f5a98632fdb491cef5a839f8d
COBI-98 <tkdgus968@naver.com>
2023-11-01 10:10:54 +0900

    feat: 이동할 횟수 입력을 도와주는 출력뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,13 @@ package racingcar.view;
 public class OutputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
+    }
+
+    public void printRequestTryCount(){
+        System.out.println(INPUT_TRY_COUNT);
     }
 }
