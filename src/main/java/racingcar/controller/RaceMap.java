be5d4f8d1f90cdcda0fd43ad74fcebeb68975a92
동원 <ai06021@gmail.com>
2023-11-01 19:43:19 +0900

    style : input입력ê값 질문

--- src/main/java/racingcar/controller/RaceMap.java
+++ src/main/java/racingcar/controller/RaceMap.java
@@ -10,13 +10,15 @@ import racingcar.view.Try;
 
 public class RaceMap {
     public RaceMap() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> cars = Cars.name();
+        System.out.println("시도할 회수는 몇회인가요?");
         int maxTry = Try.input();
         int car_numbers = cars.size();
         int carLocation[] = new int[car_numbers];
 
         int tryCount = 0;
-
+        System.out.println("실행 결과\n");
         while (tryCount < maxTry) {
             carLocation = Race.racing(carLocation, car_numbers);
             ScorePrint.resultByOrder(carLocation, cars);
