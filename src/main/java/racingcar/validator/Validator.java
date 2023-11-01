c5b5646fc284b7501700a7dc90339fd1fac59188
devhyeon0 <devhyeon0@gmail.com>
2023-11-01 19:11:05 +0900

    feat: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨다

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,4 +1,21 @@
 package racingcar.validator;
 
 public class Validator {
+
+    public static final int INPUT_NAME_LENGTH = 5;
+
+    public static void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다,");
+        }
+        if (name.length() > INPUT_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+        }
+    }
+
+    public static void validatePosition(String position) {
+        if (!position.matches("[0-9]")) {
+            throw new IllegalArgumentException("반복 횟수는 숫자만 입력이 가능합니다.");
+        }
+    }
 }
