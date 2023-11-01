a5cff9de81e16c4bc9e2837db31993575df4c6c0
ro-el <chung3286@naver.com>
2023-11-01 12:24:42 +0900

    style: 시도 횟수 입력 문자열 변수명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,15 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_ATTEMPTION_NUMBER = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_ATTEMPT_NUMBER = "시도할 회수는 몇회인가요?";
 
     public static String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES);
         return Console.readLine();
     }
 
-    public static String inputAttemptionNumber() {
-        System.out.println(INPUT_ATTEMPTION_NUMBER);
+    public static String inputAttemptNumber() {
+        System.out.println(INPUT_ATTEMPT_NUMBER);
         return Console.readLine();
     }
 }
