590d27f932ea64e4e27a18f4c45f2f24982c4ae0
jooda00 <dragon2000a@gmail.com>
2023-11-01 22:41:43 +0900

    test(MovingCountTest): movingCount 리스트 특정 인덱스 값 +1 테스트

--- src/test/java/racingcar/model/MovingCountTest.java
+++ src/test/java/racingcar/model/MovingCountTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.atIndex;
 
 public class MovingCountTest {
     private MovingCount movingCount;
@@ -24,5 +23,12 @@ public class MovingCountTest {
         List<Integer> list = new ArrayList<>(Arrays.asList(0, 0, 0));
 
         assertThat(list).isEqualTo(movingCount.getMovingCount());
+    }
+
+    @Test
+    void 움직임_횟수_리스트의_특정_인덱스값_1만큼_증가() {
+        List<Integer> list = movingCount.getMovingCount();
+
+        assertThat(list.get(0) + 1).isEqualTo(movingCount.addCount(0));
     }
 }
