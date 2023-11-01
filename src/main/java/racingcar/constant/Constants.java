82170f5c8e42460a5dc659e9220e168d6b2bd110
wochae <officialwochae@gmail.com>
2023-11-01 10:21:58 +0900

    feat(constant): constant in logics
    상수 클래스에 이동 거리, 이동 조건, 시발점, 무작위 숫자 시작과 끝의 범위, 콘솔 안내 메시지들 포함

---
+++ src/main/java/racingcar/constant/Constants.java
@@ -0,0 +1,18 @@
+package racingcar.constant;
+
+public class Constants {
+
+    public static int IDENTITY_NUMBER = 0;
+    public static final int STARTING_POSITION = 0;
+    public static final int MOVEMENT_CONDITION = 4;
+    public static final int MOVEMENT = 1;
+
+    public static final int NUMBER_START = 0;
+    public static final int NUMBER_END = 9;
+
+    public static final String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static final String INPUT_TIMES = "시도할 회수는 몇회인가요?";
+    public static final String GAME_RESULT = "실행 결과";
+    public static final String RACE_WINNERS = "최종 우승자 : ";
+}
