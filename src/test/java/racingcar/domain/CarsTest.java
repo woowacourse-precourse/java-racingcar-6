7188d5a014e7e3aefe0af40c236bdcf45ae072fb
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 14:46:58 +0900

    feat : Cars 클래스의 depart() 테스트 코드 작성 - 어떠한 숫자가 들어와도 무조건 car의 goFoward 한번은 호출해야 함

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class CarsTest {
     private Cars cars;
@@ -21,6 +22,22 @@ class CarsTest {
         //then
         assertEquals(3, cars.cars.size());
     }
+    @Test
+    void 랜덤_숫자_받아서_car_클래스_접근(){
+        //given
+        Car mockCar1 = mock(Car.class);
+        Car mockCar2 = mock(Car.class);
 
+        cars.getCars().add(mockCar1);
+        cars.getCars().add(mockCar2);
+
+        //when
+        cars.depart();
+
+        //then
+        verify(mockCar1, times(1)).goForward(anyInt());
+        verify(mockCar2, times(1)).goForward(anyInt());
+
+    }
 
 }
