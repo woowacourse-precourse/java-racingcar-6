55054b645ac9fbcacbd3b3ea39afefc891e3bc35
dj <fjzl12@naver.com>
2023-11-01 19:27:47 +0900

    refactor(RandomNumberGenerator): 정적 메서드 - 인스턴스 메서드로 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,8 +1,15 @@
 package racingcar.domain;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
     public static int createRandomNumber() {
         int randomNumber = Randoms.pickNumberInRange(0,9);
+        return randomNumber;
+    }
+
+    public int createRandomNumber() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
         return randomNumber;
     }
 }
