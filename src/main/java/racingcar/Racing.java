154969f297e1e687641c803e91eac03666beeaab
seohokim <shk9946@gmail.com>
2023-11-01 23:05:44 +0900

    [FEAT] 한번 경주 race() 구현 및 import 추가

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,11 +1,15 @@
 package racingcar;
 
-
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Racing {
     private List<Car> cars = new ArrayList<>();
-        private int numberOfRacing;
-        public void gameStart() {
+    private int numberOfRacing;
+    public void gameStart() {
         init();
         for (int i = 0; i < this.numberOfRacing; i++) {
             race();
@@ -13,7 +17,7 @@ public class Racing {
         System.out.println(result);
     }
 
-        private void init(){
+    private void init(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String inputcars = Console.readLine(); //자동차 이름 input
         List<String> carnames = Arrays.asList(inputcars.split(",")); //Array로 변환
@@ -25,4 +29,17 @@ public class Racing {
         String inputNum = Console.readLine(); //횟수 input
         numberOfRacing = Integer.parseInt(inputNum); //int로 변환
     }
+
+    private void race() {
+        for (Car car : cars) {
+            int dice = Randoms.pickNumberInRange(0,9);
+            if(dice >= 4) {
+                car.moveForward(); //4 이상 나오면 move
+            }
+            int stepLength = car.movementHistory().length();
+            System.out.println(car.name() + " : " 
+            + car.movementHistory().substring(1,stepLength - 1)); //지금까지의 step 출력
+        }
+    }
+
 }
