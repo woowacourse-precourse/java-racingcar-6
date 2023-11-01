fe15eda00ddd4383834037b7d0c527fc955c62c1
Jeongwoo Lee <68156494+JeongwooLee1124@users.noreply.github.com>
2023-11-01 19:43:48 +0900

    feat: Car 객체 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
