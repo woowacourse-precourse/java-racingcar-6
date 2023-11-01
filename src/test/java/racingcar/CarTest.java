8b52ba9aeaf6866d429c98be6c3e412a06d42ed7
Seungjida <esg0107@pusan.ac.kr>
2023-11-01 16:21:47 +0900

    test(Car): toString 검증 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,18 @@
 package racingcar;
 
-public class CarTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void testToString() {
+        Car car = new Car("pobi");
+        car.moveForward();
+        car.moveForward();
+
+        String result = car.toString();
+
+        assertThat(result).isEqualTo("pobi : --");
+    }
 }
