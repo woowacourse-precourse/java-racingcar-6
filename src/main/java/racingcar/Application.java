15a1907a6b9396bd07dad0848884998237f24c81
youngchive <2000mily@naver.com>
2023-11-01 18:55:43 +0900

    feat(RandomNum): create randomnumber

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int n = new RandomNum().RandomNum();
+        System.out.println(n);
     }
 }
---
+++ src/main/java/racingcar/RandomNum.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNum {
+    public int RandomNum() {
+        int num = Randoms.pickNumberInRange(0, 9);
+        return num;
+    }
+
+
+}
