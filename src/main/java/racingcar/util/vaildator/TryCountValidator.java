1d6cd197f6e6b8f972205b4f7748f08795fd2e55
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 14:01:23 +0900

    feat: 시도 횟수가 0일 때를 검증한다.

--- src/main/java/racingcar/util/vaildator/TryCountValidator.java
+++ src/main/java/racingcar/util/vaildator/TryCountValidator.java
@@ -1,4 +1,12 @@
 package racingcar.util.vaildator;
 
 public class TryCountValidator {
+    private static final String INPUT_TRYCOUNT_ZERO_EXCEPTION = "0 이상의 숫자를 입력해주세요.";
+
+    public static void validateInputTryCount(String tryCount) {
+        int inputTryCount = Integer.parseInt(tryCount);
+        if (inputTryCount == 0) {
+            throw new IllegalArgumentException(INPUT_TRYCOUNT_ZERO_EXCEPTION);
+        }
+    }
 }
