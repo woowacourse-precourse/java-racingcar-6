62789d5cca8813cef0c1f5156839e7d20e5fcd9f
김민주 <69795199+ks00919@users.noreply.github.com>
2023-11-01 22:18:29 +0900

    feat: input 메소드

--- src/main/java/racingcar/RaceConsole.java
+++ src/main/java/racingcar/RaceConsole.java
@@ -2,20 +2,31 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class RaceConsole {
 
-    public static void start() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String names = Console.readLine();
-        String[] carNames = names.split(",");
+    public void start() {
+//        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+//        String names = Console.readLine();
+//        String[] carNames = names.split(",");
+//
+//        System.out.println("시도할 회수는 몇회인가요?");
+//        String round = Console.readLine();
+//        if (!round.matches("\\d+")) {
+//            throw new IllegalArgumentException("getCount");
+//        }
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        String round = Console.readLine();
-        if (!round.matches("\\d+")) {
-            throw new IllegalArgumentException("getCount");
-        }
-        CarRace race = new CarRace(carNames, Integer.parseInt(round));
+        String[] carNames = getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                s -> false,
+                s -> s.split(","));
+
+        int rounds = getInput("시도할 회수는 몇회인가요?",
+                s -> !s.matches("\\d+"),
+                Integer::parseInt);
+
+        CarRace race = new CarRace(carNames, rounds);
 
         List<String> winners = race.run();
         System.out.print("최종 우승자 : " + winners.get(0));
@@ -24,5 +35,13 @@ public class RaceConsole {
         }
     }
 
+    public <String, R> R getInput(String message, Predicate<String> predicate, Function<String, R> function) {
+        System.out.println(message);
+        String answer = (String) Console.readLine();
+        if (predicate.test(answer)) {
+            throw new IllegalArgumentException("invalid input");
+        }
+        return function.apply(answer);
+    }
 
 }
