5c5aed4bb66978a0abd6881ee7df151cf2b44fae
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-01 14:21:25 +0900

    Feat : 공백 혹은 띄어쓰기로만 이루어진 값을 입력할 경우 예외를 발생시킨다
    - 띄어쓰기로만 입력된 경우를 고려해 trim()을 사용

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -5,6 +5,7 @@ public class CarName {
 
     public CarName(String name) {
         validateLength(name);
+        validateEmpty(name);
         this.name = name;
     }
 
@@ -12,10 +13,20 @@ public class CarName {
         return this.name;
     }
 
+    private void validateEmpty(String name) {
+        if (isEmptyValue(name)) {
+            throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
+        }
+    }
+
     private void validateLength(String name) {
         if (isOverLength(name)) {
             throw new IllegalArgumentException("자동차 이름은 5자 까지만 입력할 수 있습니다.");
         }
+    }
+
+    private boolean isEmptyValue(String name) {
+        return name.trim().isBlank();
     }
 
     private boolean isOverLength(String name) {
