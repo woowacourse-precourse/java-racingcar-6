d0476be9cd6824f4c0b3285cc5cd12d3e46c88ce
tangba484 <tangba484@naver.com>
2023-11-01 22:14:42 +0900

    Test: readRounds메서드 테스트 코드작성

--- src/test/java/racingcar/io/InputTest.java
+++ src/test/java/racingcar/io/InputTest.java
@@ -1,37 +1,50 @@
 package racingcar.io;
 
-import camp.nextstep.edu.missionutils.Console;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import racingcar.model.Cars;
 
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class InputTest {
+    @AfterEach
+    void clearSystemIn() {
+        Input.consoleClose();
+    }
 
     @Test
     @DisplayName("Cars 객체가 잘 생성되는지 확인하는 테스트")
-    public void testReadCars() {
+    void ReadCars() {
         Input input = new Input();
-        String inputString = "A,B,C";
-        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        String userInput = "A,B,C";
+        setSystemInput(userInput);
         Cars cars = input.readCars();
         List<String> names = cars.getName();
         List<Integer> scores = cars.getScore();
-        assertEquals(3 , names.size());
-        assertEquals(3 , scores.size());
+        assertEquals(3, names.size());
+        assertEquals(3, scores.size());
         assertThat(names).contains("A", "C");
-        assertThat(names).containsExactly("A","B","C");
+        assertThat(names).containsExactly("A", "B", "C");
         assertEquals(Arrays.asList(0, 0, 0), scores);
+    }
+
+    @Test
+    @DisplayName("입력한 자연수가 Integer타입으로 변환이 잘 되는지 테스트")
+    void readRounds() {
+        Input input = new Input();
+        String userInput = "5";
+        setSystemInput(userInput);
+        Integer rounds = input.readRounds();
+        assertEquals(Integer.parseInt(userInput), rounds);
+    }
+
+    private void setSystemInput(String input) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
     }
 }
