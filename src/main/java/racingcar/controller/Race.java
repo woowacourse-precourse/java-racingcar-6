fb785f0018b40970f7620d6f651e76e2d7195747
HanDaWoon <bookpia.han@gmail.com>
2023-11-01 10:34:47 +0900

    feat: 레이스 컨트롤러 생성
    - 시작
    - 차량 이름
    - 시도 횟수
    - CarModel 생성
    - 시도 횟수 만큼 실행
    - 우승자 그룹 생성

---
+++ src/main/java/racingcar/controller/Race.java
@@ -0,0 +1,96 @@
+package racingcar.controller;
+
+import racingcar.model.CarModel;
+import racingcar.util.CarNamesToArray;
+import racingcar.util.RandomNumber;
+import racingcar.util.Verifier;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class Race {
+    CarModel[] cars;
+
+    /**
+     * 레이스 시작
+     */
+    public void start() {
+        String[] carNames = getCarNames();
+        int tryCount = getTryCount();
+        cars = createCars(carNames);
+
+        OutputView.printResult();
+
+        raceTry(tryCount);
+
+        OutputView.printWinners(getWinners());
+    }
+
+    private String[] getCarNames() {
+        String carNamesString = InputView.getCarNames();
+        Verifier.verifyCarNamesString(carNamesString);
+
+        String[] carNames = CarNamesToArray.convert(carNamesString);
+        Verifier.verifyCarNameDuplication(carNames);
+
+        Arrays.stream(carNames).forEach(Verifier::verifyCarName);
+
+        return carNames;
+    }
+
+    private int getTryCount() {
+        String tryCount = InputView.getTryCount();
+        Verifier.verifyTrialNumber(tryCount);
+
+        return Integer.parseInt(tryCount);
+    }
+
+    private CarModel[] createCars(String[] carNames) {
+        CarModel[] cars;
+
+        cars = new CarModel[carNames.length];
+
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new CarModel(carNames[i]);
+        }
+
+        return cars;
+    }
+
+    private void raceTry(Integer tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            randomMoveAllCars();
+            OutputView.printGameResult(cars);
+        }
+    }
+
+    private void randomMoveAllCars() {
+        for (CarModel car : cars) {
+            randomMove(car);
+        }
+    }
+
+    private void randomMove(CarModel car) {
+        if (RandomNumber.generate() >= 4) {
+            car.move();
+        }
+    }
+
+    private CarModel[] getWinners() {
+        int maxPosition = getMaxPosition();
+
+        return Arrays.stream(cars)
+                .filter(car -> car.getPosition() == maxPosition)
+                .toArray(CarModel[]::new);
+    }
+
+    private Integer getMaxPosition() {
+        OptionalInt optMaxPosition = Arrays.stream(cars)
+                .mapToInt(CarModel::getPosition)
+                .max();
+
+        return optMaxPosition.orElse(0);
+    }
+}
