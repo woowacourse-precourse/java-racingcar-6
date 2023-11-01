a1d1723b817f798e184a1aca8fb2a5a8c9279867
namtndus <rnrmf0721@gmail.com>
2023-11-01 22:39:49 +0900

    refactor(ExceptionMeaage): 예외시 발생하는 메세지들의 모임

--- src/main/java/racingcar/view/InputException.java
+++ src/main/java/racingcar/view/InputException.java
@@ -1,30 +1,32 @@
 package racingcar.view;
 
+import racingcar.constant.ExceptionMeaage;
+
 import java.util.regex.Pattern;
 
 public class InputException {
 
     public void inputSeparatorException(String carNames) throws IllegalArgumentException{
         if(!Pattern.matches("^[a-z|A-Z|ㄱ-ㅎ|가-힣|,]+$",carNames))
-           throw new IllegalArgumentException("쉽표(,)를 통해 구분해 주세요.");
+           throw new IllegalArgumentException(ExceptionMeaage.SEPARATOR_EXCEPTION.getMessage());
     }
 
     public void inputCarsSizeException(String[] carNames) throws IllegalArgumentException{
         if(carNames.length < 2){
-            throw new IllegalArgumentException("자동차는 2대 이상이 경기에 참여해야 합니다.");
+            throw new IllegalArgumentException(ExceptionMeaage.CARS_SIZE_EXCEPTION.getMessage());
         }
     }
 
     public void inputCarNameOfLengthException(String[] carNames) throws IllegalArgumentException{
         for(String carName : carNames){
             if(carName.length()  > 5){
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+                throw new IllegalArgumentException(ExceptionMeaage.CAR_NAME_OF_LENGTH_EXCEPTION.getMessage());
             }
         }
     }
 
     public void inputMovingNumberException(String str){
         if(!Pattern.matches("^[1-9]+$",str))
-            throw new IllegalArgumentException("숫자외 다른 문자는 쓸 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMeaage.MOVING_NUMBER_EXCEPTION.getMessage());
     }
 }
