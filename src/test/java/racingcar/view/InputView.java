cb675b3f712c47afcf8636044e676d4db6987c2d
compasstar <woohong24@naver.com>
2023-11-01 12:15:24 +0900

    feat: 추가 View

--- src/test/java/racingcar/view/InputView.java
+++ src/test/java/racingcar/view/InputView.java
@@ -1,8 +1,16 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
 
-    //1. 경주 할 자동차 이름 입력받기
+    public String getCarsName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return readLine();
+    }
 
-    //2. 시도할 횟수 입력받기
+    public String getAttempts() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return readLine();
+    }
 }
--- src/test/java/racingcar/view/OutputView.java
+++ src/test/java/racingcar/view/OutputView.java
@@ -1,8 +1,27 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
-    
-    // 1. 각 차수별 레이싱 실행 결과 출력
-    
-    // 2. 우승자 안내 문구 출력
+
+    public void printMoveResult(List<Car> cars, Integer attempts) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < attempts; i++) {
+            for (Car car : cars) {
+                System.out.println(car.getName() + " : " + "-".repeat(car.getMoves()));
+            }
+            System.out.println();
+        }
+    }
+
+    public void printWinners(List<Car> winners) {
+        String phrase = "최종 우승자 : ";
+        for (Car winner : winners) {
+            phrase += winner.getName() + ", ";
+        }
+        phrase = phrase.substring(phrase.length() - 2);
+        System.out.println(phrase);
+    }
 }
