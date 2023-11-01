548780fe6eb182de4617b7e4f418077b863b2ce2
imhungry2 <sjnnj@naver.com>
2023-11-01 17:27:06 +0900

    model - RacingGame 구현

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,21 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class RacingGame {
+    private List<Car> cars;
+    private int tryCount;
+
+    public RacingGame(String carNames, int tryCount) {
+        // 생성자 로직
+    }
+
+    public void playOneRound() {
+        // 한 라운드를 진행하는 로직
+    }
+
+    public void getWinners() {
+        // 우승자 결정 로직
+        return;
+    }
+}
