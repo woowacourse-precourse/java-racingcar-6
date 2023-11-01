edc153df7d0a01c660f77cfa662f229479b11ca1
namtndus <rnrmf0721@gmail.com>
2023-11-01 21:47:46 +0900

    rafactor(InputException): inputCarNameOfLengthException 함수 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -19,11 +19,7 @@ public class Input {
 
         String[] cars = input.split(",");
         inputException.inputCarsSizeException(cars);
-        for(String carName : cars){
-            if(carName.length()  > 5){
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
-        }
+        inputException.inputCarNameOfLengthException(cars);
         return cars;
     }
 
--- src/main/java/racingcar/view/InputException.java
+++ src/main/java/racingcar/view/InputException.java
@@ -9,9 +9,17 @@ public class InputException {
            throw new IllegalArgumentException("쉽표(,)를 통해 구분해 주세요.");
     }
 
-    public void inputCarsSizeException(String[] carNames){
+    public void inputCarsSizeException(String[] carNames) throws IllegalArgumentException{
         if(carNames.length < 2){
             throw new IllegalArgumentException("자동차는 2대 이상이 경기에 참여해야 합니다.");
+        }
+    }
+
+    public void inputCarNameOfLengthException(String[] carNames) throws IllegalArgumentException{
+        for(String carName : carNames){
+            if(carName.length()  > 5){
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
         }
     }
 }
