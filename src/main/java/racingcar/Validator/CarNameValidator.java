2c1d4de6fd433bda471d60bd1fe1fb9b74c0cd83
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 10:51:50 +0900

    [feat] Validator 작성 - 자동차 이름 작성 제한(공백 or 5글자 이상 작성)

--- src/main/java/racingcar/Validator/CarNameValidator.java
+++ src/main/java/racingcar/Validator/CarNameValidator.java
@@ -1,4 +1,29 @@
 package racingcar.Validator;
 
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class CarNameValidator {
+
+    public static List<String> validateCarNames(String carNamesInput) {
+        if (isNotSplitByComma(carNamesInput)) {
+            throw new IllegalArgumentException("자동차는 쉼표(,)로 구분해주세요.");
+        }
+
+        List<String> carNames = splitByComma(carNamesInput);
+        
+        return carNames;
+    }
+
+    private static List<String> splitByComma(String input) {
+        return Pattern.compile(",")
+                .splitAsStream(input)
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isNotSplitByComma(String carName) {
+        return !carName.contains(",");
+    }
 }
