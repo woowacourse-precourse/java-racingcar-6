c4b6e2b0cf60dd86a08bf4ade3857ebf6ea2b43a
sangxxjin <sj_0368@naver.com>
2023-11-01 12:32:42 +0900

    feat: 이름이 같을때 하나의 Car로 구현 @Override 사용

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,10 +1,28 @@
 package racingcar.model;
 
+import java.util.Objects;
+
 public class Car {
     private String name;
     private int distance;
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
