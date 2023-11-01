dab86f2a2cbe59517c493591d466261402a0f61e
ihun-il <hunil9978@gmail.com>
2023-11-01 22:02:55 +0900

    fixed: 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,6 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.controller.RacingGame;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.*;
-import static camp.nextstep.edu.missionutils.Randoms.*;
 
 public class Application {
     public static void main(String[] args) {
