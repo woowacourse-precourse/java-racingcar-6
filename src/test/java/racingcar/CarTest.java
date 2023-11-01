0027a6f50255f8160ac06204590b60ac644ab4cd
JoonKyo <joonkyolee@gmail.com>
2023-11-01 19:53:00 +0900

    test(CarTest): 자동차의 이동이 잘 이루어지는지 테스트하는 기능 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,10 +8,22 @@ import racingcar.model.Car;
 
 public class CarTest {
     private static final int FIRST_CAR_STATE = 0;
+    private static final int MOVED_ONE_STEP_STATE = 1;
+    private static final String MOVED_ONE_STEP_RESULT = "-";
 
     @Test
     void 초기_자동차의_이동거리는_0이어야_한다(){
         Car car = new Car("pobi");
         assertThat(car.getCarState()).isEqualTo(FIRST_CAR_STATE);
+    }
+
+    @Test
+    void 자동차_이동이_잘_이루어지는지_확인(){
+        Car car = new Car("jun");
+        car.setCarState();
+        car.setCarMovedResult();
+
+        assertThat(car.getCarState()).isEqualTo(MOVED_ONE_STEP_STATE);
+        assertThat(car.getCarMovedResult()).isEqualTo(MOVED_ONE_STEP_RESULT);
     }
 }
