a3c2ef18ca0c9473677c7b22ed53cc1292eda2db
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-01 09:18:22 +0900

    refactor: 에러 메시지를 ErrorMessage 클래스로 이동 에러 메시지를 상수로 선언하였음

--- src/main/java/racingcar/utils/ErrorMessage.java
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -1,4 +1,11 @@
 package racingcar.utils;
 
 public class ErrorMessage {
+    public static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 존재합니다.";
+    public static final String NAME_CONTAINS_WHITESPACE_ERROR_MESSAGE = "자동차 이름에 공백이 포함되어 있습니다.";
+    public static final String NAME_LENGTH_OUT_OF_RANGE_ERROR_MESSAGE = "자동차 이름은 1자에서 5자 사이어야 합니다.";
+    public static final String LESS_THAN_MINIMUM_CAR_COUNT_ERROR_MESSAGE = "최소 2대의 자동차 이름을 입력해야 합니다.";
+    public static final String COMMA_AT_END_ERROR_MESSAGE = "마지막 자동차 이름 뒤에 쉼표(,)를 입력하지 마세요.";
+    public static final String CASE_INSENSITIVE_DUPLICATE_ERROR_MESSAGE = "대소문자를 구분하지 않고 중복된 자동차 이름은 허용되지 않습니다.";
+    public static final String INVALID_CHARACTERS_ERROR_MESSAGE = "자동차 이름에 허용되지 않는 특수 문자가 포함되어 있습니다.";
 }
--- src/main/java/racingcar/validation/CarInputValidation.java
+++ src/main/java/racingcar/validation/CarInputValidation.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
+import racingcar.utils.ErrorMessage;
+import racingcar.utils.Util;
 
 public class CarInputValidation {
 
@@ -21,51 +23,51 @@ public class CarInputValidation {
         String[] inputCars = input.split(Util.CAR_NAME_DELIMITER);
         Set<String> names = new HashSet<>(Arrays.asList(inputCars));
         if (names.size() < inputCars.length) {
-            throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다.");
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAME_ERROR_MESSAGE);
         }
     }
 
     private void validateNoSpaces(String input) {
         if (input.contains(" ")) {
-            throw new IllegalArgumentException("자동차 이름에 공백이 포함되어 있습니다.");
+            throw new IllegalArgumentException(ErrorMessage.NAME_CONTAINS_WHITESPACE_ERROR_MESSAGE);
         }
     }
 
     private void validateNameLength(String input) {
-        String[] inputCars = input.split(",");
+        String[] inputCars = input.split(Util.CAR_NAME_DELIMITER);
         for (String name : inputCars) {
             if (name.length() < 1 || name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 1자에서 5자 사이어야 합니다.");
+                throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH_OUT_OF_RANGE_ERROR_MESSAGE);
             }
         }
     }
 
     private void validateMinCarCount(String input) {
-        String[] inputCars = input.split(",");
+        String[] inputCars = input.split(Util.CAR_NAME_DELIMITER);
         if (inputCars.length < 2) {
-            throw new IllegalArgumentException("최소 2대의 자동차 이름을 입력해야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.LESS_THAN_MINIMUM_CAR_COUNT_ERROR_MESSAGE);
         }
     }
 
     private void validateTrailingComma(String input) {
-        if (input.endsWith(",")) {
-            throw new IllegalArgumentException("마지막 자동차 이름 뒤에 쉼표(,)를 입력하지 마세요.");
+        if (input.endsWith(Util.CAR_NAME_DELIMITER)) {
+            throw new IllegalArgumentException(ErrorMessage.COMMA_AT_END_ERROR_MESSAGE);
         }
     }
 
     private void validateCaseRedundancy(String input) {
-        String[] inputCars = input.split(",");
+        String[] inputCars = input.split(Util.CAR_NAME_DELIMITER);
         Set<String> names = new HashSet<>();
         for (String name : inputCars) {
             String normalizedName = name.toLowerCase();
             if (names.contains(normalizedName)) {
-                throw new IllegalArgumentException("대소문자를 구분하지 않고 중복된 자동차 이름은 허용되지 않습니다.");
+                throw new IllegalArgumentException(ErrorMessage.CASE_INSENSITIVE_DUPLICATE_ERROR_MESSAGE);
             }
         }
     }
 
     private void validateSpecialCharacters(String input) {
-        String[] inputCars = input.split(",");
+        String[] inputCars = input.split(Util.CAR_NAME_DELIMITER);
         for (String name : inputCars) {
             if (!Pattern.matches("^[a-zA-Z0-9-_.]*$", name)) {
                 throw new IllegalArgumentException("자동차 이름에 허용되지 않는 특수 문자가 포함되어 있습니다.");
