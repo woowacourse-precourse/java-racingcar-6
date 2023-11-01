27f86c6773b66fa941a4b32f17b8c5b34aca312d
siyeonSon <siyeon0209@naver.com>
2023-11-01 11:19:17 +0900

    test(converter): 자동차 경주 단건 실행 결과를 변환한다

--- src/test/java/racingcar/util/ConverterTest.java
+++ src/test/java/racingcar/util/ConverterTest.java
@@ -1,8 +1,11 @@
 package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 import racingcar.model.Cars;
 
 class ConverterTest {
@@ -11,5 +14,17 @@ class ConverterTest {
         String carNames = "pobi,woni,jun";
         Cars cars = Converter.parseCarNames(carNames);
         assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 자동차_경주_단건_실행_결과를_변환한다() {
+        List<Car> carList = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+        Cars cars = new Cars(carList);
+        String result = Converter.formatCarsResult(cars);
+        assertAll(
+                () -> assertThat(result).contains("pobi : "),
+                () -> assertThat(result).contains("woni : "),
+                () -> assertThat(result).contains("jun : ")
+        );
     }
 }
