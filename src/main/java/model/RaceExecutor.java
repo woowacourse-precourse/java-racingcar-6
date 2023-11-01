6195f67ffbe9b80adf5f6a36616a17059d09b48a
nayonsoso <panda0329@naver.com>
2023-11-01 16:18:03 +0900

    feat: 경기 실행하는 클래스 생성

---
+++ src/main/java/model/RaceExecutor.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.HashMap;
+
+public class RaceExecutor {
+    private final MovementDecider movementDecider = new MovementDecider();
+
+    private HashMap<String, Integer> cars = new HashMap<>();
+
+    private void singleAttempt(){
+    }
+    // Dto를 입력받고, 회수에 해당하는 만큼 무브먼트 디사이더를 실행한다.
+}
