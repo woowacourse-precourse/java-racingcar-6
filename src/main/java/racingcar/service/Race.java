c7c9e5b66d72f2e02114a5d829ba6c0a3856a0b2
woookk <woook27@naver.com>
2023-11-01 23:42:33 +0900

    refactor: Race 로직을 Race와 RaceManager로 분리

--- src/main/java/racingcar/service/Race.java
+++ src/main/java/racingcar/service/Race.java
@@ -1,6 +1,5 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.console.Input;
 import racingcar.console.Output;
 import racingcar.constants.Constants;
@@ -11,63 +10,37 @@ import java.util.List;
 
 public class Race {
 
-    private List<Car> cars = new ArrayList<>();
+    private final RaceManager raceManager;
 
-    public void start() {
-        initializeCars();
-        int rounds = getRounds();
-        startRace(rounds);
+    public Race() {
+        List<Car> cars = initializeCars();
+        this.raceManager = new RaceManager(cars);
     }
 
-    private void initializeCars() {
+    public void start() {
+        int rounds = getRounds();
+        raceManager.startRace(rounds);
+        printRaceResult();
+    }
+
+    private List<Car> initializeCars() {
+        List<Car> cars = new ArrayList<>();
         String carNames = Input.getCarNames();
         for (String name : carNames.split(Constants.CAR_DELIMITER.getValue())) {
             cars.add(new Car(name.trim()));
         }
+        return cars;
     }
 
     private int getRounds() {
         return Input.getRounds();
     }
 
-    private void startRace(int rounds) {
-        for (int i = 0; i < rounds; i++) {
-            moveCars();
-            printCurrentState();
-        }
-        printWinner();
-    }
-
-    private void moveCars() {
-        for (Car car : cars) {
-            int randomNumber = Randoms.pickNumberInRange(Constants.RANDOM_MIN_RANGE.getValue(), Constants.RANDOM_MAX_RANGE.getValue());
-            car.move(randomNumber);
-        }
-    }
-
-    private void printCurrentState() {
-        for (Car car : cars) {
+    private void printRaceResult() {
+        for (Car car : raceManager.getCars()) {
             Output.printCarState(car.getName(), car.getPosition());
+            Output.printNewLine();
         }
-        Output.printNewLine();
-    }
-
-    private void printWinner() {
-        int maxPosition = getMaxPosition();
-        List<String> winners = new ArrayList<>();
-        for (Car car : cars) {
-            if (car.getPosition() == maxPosition) {
-                winners.add(car.getName());
-            }
-        }
-        Output.printWinner(String.join(Constants.NAME_SEPARATOR.getValue(), winners));
-    }
-
-    private int getMaxPosition() {
-        int max = -1;
-        for (Car car : cars) {
-            max = Math.max(max, car.getPosition());
-        }
-        return max;
+        Output.printWinner(String.join(Constants.NAME_SEPARATOR.getValue(), raceManager.getWinners()));
     }
 }
---
+++ src/main/java/racingcar/service/RaceManager.java
@@ -0,0 +1,53 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constants.Constants;
+import racingcar.entity.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceManager {
+
+    private final List<Car> cars;
+
+    public RaceManager(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void startRace(int rounds) {
+        for (int i = 0; i < rounds; i++) {
+            moveCars();
+        }
+    }
+
+    private void moveCars() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(Constants.RANDOM_MIN_RANGE.getValue(), Constants.RANDOM_MAX_RANGE.getValue());
+            car.move(randomNumber);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxPosition() {
+        int max = -1;
+        for (Car car : cars) {
+            max = Math.max(max, car.getPosition());
+        }
+        return max;
+    }
+}
