5cfe81adbd48cebb42224829165287035703b952
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-01 23:02:33 +0900

    Game 클래스에 주석 추가
    Game 클래스에 메소드에 설명을 위해서 상세하게 주석을 추가했습니다.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,10 +7,20 @@ public class Game {
     private List<Car> cars;
     private int round;
 
+    /**
+     * Game 객체를 생성합니다.
+     *
+     * @param cars  게임에 참여할 Car 객체의 리스트입니다.
+     * @param round 게임을 진행할 라운드 수입니다.
+     */
     public Game(List<Car> cars, int round) {
         this.cars = cars;
         this.round = round;
     }
+
+    /**
+     * 게임을 진행합니다. 각 라운드마다 모든 자동차의 move 메소드를 호출하고 결과를 출력합니다.
+     */
     public void play() {
         for (int i = 0; i < round; i++) {
             for (Car car : cars) {
@@ -19,7 +29,12 @@ public class Game {
             displayResult();
         }
     }
-    // getMaximumPosition()을 이용해서 우승자 찾기
+
+    /**
+     * 최종 우승자의 이름 리스트를 반환합니다.
+     *
+     * @return 최대 위치에 있는 자동차의 이름 리스트입니다.
+     */
     public List<String> getWinners() {
         List<String> winners = new ArrayList<>();
         int maxPosition = getMaximumPosition();
@@ -30,6 +45,12 @@ public class Game {
         }
         return winners;
     }
+
+    /**
+     * 참여한 모든 자동차 중에서 최대 위치 값을 찾아 반환합니다.
+     *
+     * @return 참여한 모든 자동차 중에서 최대 위치 값입니다.
+     */
     private int getMaximumPosition() {
         int max = 0;
         for (Car car : cars) {
@@ -37,6 +58,10 @@ public class Game {
         }
         return max;
     }
+
+    /**
+     * 현재까지의 게임 결과를 출력합니다.
+     */
     private void displayResult() {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
