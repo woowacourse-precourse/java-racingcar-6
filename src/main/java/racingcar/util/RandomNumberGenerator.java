7533b16da01bd1937298e78f7b3d92382a97db52
IDpluto <d0013159@gmail.com>
2023-11-01 10:39:06 +0900

    refactor: 메소드 길이 줄이기

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,14 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class RandomNumberGenerator {
-  private static final int START_NUMBER = 0;
-  private static final int END_NUMBER = 9;
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
 
-  public int generateUniqueNumbers() {
-
-    int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
-    return randomNumber;
-
-  }
+    public int generateUniqueNumbers() {
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+    }
 }
 
