838452d4d6bbe30a6cea38c959f4708fdd9e1cf0
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 20:28:27 +0900

    feat(CarManager): 우승자(가장 많이 전진한 자동차)를 구하는 기능 구현

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarManager {
     private List<Car> cars;
@@ -34,5 +35,18 @@ public class CarManager {
         Output.printLine();
     }
 
-    // TODO : 우승자(가장 많이 전진한 자동차)를 구합니다.
+    // 우승자(가장 많이 전진한 자동차)를 구합니다.
+    public void getWinner()
+    {
+        int maxMoveCount = getMaxMoveCount();
+
+        List<String> winners = cars.stream().filter(car -> car.getMoveCnt() == maxMoveCount).map(Car::getName)
+                .collect(Collectors.toList());
+
+        Output.printWinner(winners);
+    }
+
+    private int getMaxMoveCount() {
+        return cars.stream().mapToInt(Car::getMoveCnt).max().orElse(0);
+    }
 }
