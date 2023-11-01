1e3ba8a7eda606c13bc1e21e19c60c877868ea0c
jyoooooung <jyooooounh@naver.com>
2023-11-01 20:08:33 +0900

    RacingGame : 경주차 이름에 따라 생성

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,10 +1,21 @@
 package racingcar;
 
 import racingcar.Car.Car;
+import racingcar.Car.CarFactory;
+import racingcar.User.User;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class RacingGame {
+    public static void run() {
+        List<Car> cars = createCarsByInput();
 
+    }
+
+    private static List<Car> createCarsByInput() {
+        List<String> carNames = Arrays.asList(User.carNameInput().split(","));
+        return CarFactory.createByNames(carNames);
+    }
 
 }
