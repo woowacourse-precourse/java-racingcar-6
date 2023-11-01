893aee462bbbba596feb4f492e2c4312efe57475
jiixon <jr0107380@gmail.com>
2023-11-01 16:37:58 +0900

    feat: 게임 컨트롤러 작성

--- src/main/java/racingcar/Controller/Controller.java
+++ src/main/java/racingcar/Controller/Controller.java
@@ -1,7 +1,23 @@
 package racingcar.Controller;
 
+import racingcar.View.GameView;
+import racingcar.domain.RacingCars;
+
 public class Controller {
+    private final static GameView gameView = new GameView();
     public void play(){
+        String carNames= gameView.inputCarNames();
+        RacingCars racingCars = new RacingCars(carNames);
+
+        int trialNum = gameView.inputTrialGame();
+
+        gameView.printResultHead();
+
+        for(int i=0; i<trialNum; i++){
+            racingCars.movingRacingCars();
+            gameView.printCarStatus(racingCars.getCars());
+        }
+        gameView.printWinner(racingCars.getCars());
 
     }
 
