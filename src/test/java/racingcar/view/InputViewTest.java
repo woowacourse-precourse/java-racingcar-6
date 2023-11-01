e10bddbef507de6e5925ed00593c8d263345f128
Daseul <daseull.rep@gmail.com>
2023-11-01 15:49:59 +0900

    test(InputView): 횟수 입력 테스트 추가

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,10 +1,13 @@
 package racingcar.view;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -31,6 +34,32 @@ class InputViewTest {
         Assertions.assertThat(carNames.get(0)).isEqualTo(first);
 
         Console.close();
+    }
+
+    @DisplayName("횟수 입력 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10,10", "-1,-1"})
+    void readNumRound(String input, int result) {
+
+        setSystemIn(input);
+
+        int numRound = inputView.readNumRound();
+
+        Assertions.assertThat(numRound).isEqualTo(result);
+
+        Console.close();
+
+    }
+
+    @DisplayName("정수 범위를 벗어난 횟수 입력")
+    @Test
+    void readNumRoundOverInteger() {
+
+        String wrongInput = Integer.toString(Integer.MAX_VALUE) + "0";
+        setSystemIn(wrongInput);
+
+        assertThrows(IllegalArgumentException.class, () -> inputView.readNumRound());
+
     }
 
 }
