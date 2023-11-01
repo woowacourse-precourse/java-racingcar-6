30ac92a924b7eca535fee49e4e39a2e33f0fe91c
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-01 16:33:07 +0900

    Feat : domain - CarPlayer class 구현

--- src/main/java/racingcar/domain/CarPlayer.java
+++ src/main/java/racingcar/domain/CarPlayer.java
@@ -1,4 +1,26 @@
 package racingcar.domain;
 
 public class CarPlayer {
+    private final int id;
+    private final String name;
+    private int score;
+
+    public CarPlayer(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.score = 0;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+    public void setScore(){
+        this.score += 1;
+    }
+
 }
