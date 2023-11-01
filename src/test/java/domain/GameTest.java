5f20a5a665358107de686c6a9e93e78dce8fb2a4
PARK9898 <wjddjs21@naver.com>
2023-11-01 20:11:23 +0900

    "refactor(test): Game 생성자 매개변수 수정에 따른 test 코드 수정"

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -12,65 +12,47 @@ public class GameTest {
 
     @Test
     void 자동차들은_움직일_수_있다() {
-        List<Car> cars = List.of(
-                new Car("kim",0),
-                new Car("ray",0)
-        );
-        Car compareCar = new Car("park",1);
-        Game game = new Game(cars);
+        Car compareCar = new Car("park");
+        compareCar.moveOrStop(4);
+        Game game = new Game(List.of("kim", "park"));
         game.playOnce(() -> 4);
 
-        Assertions.assertThat(compareCar.isSamePosition(cars.get(0))).isTrue();
-        Assertions.assertThat(compareCar.isSamePosition(cars.get(1))).isTrue();
+        Assertions.assertThat(game.getCars().get(0).isSamePosition(compareCar)).isTrue();
+        Assertions.assertThat(game.getCars().get(1).isSamePosition(compareCar)).isTrue();
     }
 
     @Test
     void 자동차들은_여러번_움직일_수_있다() {
-        List<Car> cars = List.of(
-                new Car("kim",0),
-                new Car("ray",0)
-        );
-        Car compareCar = new Car("park",3);
-        Game game = new Game(cars);
+        Car compareCar = new Car("park");
+
+        for (int i = 0; i < 3; i++) {
+            compareCar.moveOrStop(4);
+        }
+
+        Game game = new Game(List.of("kim","jay"));
+
         for (int i = 0; i < 3; i++) {
             game.playOnce(() -> 4);
         }
-        Assertions.assertThat(compareCar.isSamePosition(cars.get(0))).isTrue();
-        Assertions.assertThat(compareCar.isSamePosition(cars.get(1))).isTrue();
+        Assertions.assertThat(game.getCars().get(0).isSamePosition(compareCar)).isTrue();
+        Assertions.assertThat(game.getCars().get(1).isSamePosition(compareCar)).isTrue();
     }
 
     @Test
     void 우승자를_가려낸다() {
-        List<Car> cars = List.of(
-                new Car("park",0),
-                new Car("kim",2)
-        );
-        Game game = new Game(cars);
+        Game game = new Game(List.of("kim","park","jay"));
+        game.getCars().get(0).moveOrStop(1);
+        game.getCars().get(1).moveOrStop(5);
+        game.getCars().get(2).moveOrStop(5);
+
         List<Car> winner = game.pickWinner();
 
-        Assertions.assertThat(winner.get(0).toString()).isEqualTo("kim");
-    }
-    @Test
-    void 우승자는_여러명일수_있다() {
-        List<Car> cars = List.of(
-                new Car("park",0),
-                new Car("kim",2),
-                new Car("jay",3),
-                new Car("ray",3)
-        );
-        Game game = new Game(cars);
-        List<Car> winner = game.pickWinner();
-
-        Assertions.assertThat(winner.get(0).toString()).isEqualTo("jay");
-        Assertions.assertThat(winner.get(1).toString()).isEqualTo("ray");
+        Assertions.assertThat(winner.get(0).getName()).isEqualTo("park");
+        Assertions.assertThat(winner.get(1).getName()).isEqualTo("jay");
     }
 
     @Test
     void 자동차_이름이_중복되면_예외를_던진다() {
-        List<Car> cars = List.of(
-                new Car("kim",0),
-                new Car("kim",0)
-        );
-        assertThrows(IllegalArgumentException.class, () -> new Game(cars));
+        assertThrows(IllegalArgumentException.class, () -> new Game(List.of("kim","kim")));
     }
 }
