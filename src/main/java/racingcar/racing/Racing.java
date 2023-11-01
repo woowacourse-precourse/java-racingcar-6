1e78855ecd55189024f6983a14436d50e4fccb69
HwangHarim <scopan01@naver.com>
2023-11-01 20:45:19 +0900

    feat : Racing 실행 도메인 구현

---
+++ src/main/java/racingcar/racing/Racing.java
@@ -0,0 +1,38 @@
+package racingcar.racing;
+
+import racingcar.domain.GameInfo;
+import racingcar.service.RacingCarService;
+import racingcar.view.View;
+
+public class Racing {
+
+    private final View view = new View();
+    private final RacingCarService racingCarService = new RacingCarService();
+    private GameInfo gameInfo;
+
+
+
+    public void racingConfig() {
+        String driverNames = view.driverInjectionView();
+        String roundNumber = view.roundInjectionView();
+        gameInfo = new GameInfo(driverNames, roundNumber);
+        racingCarService.createCars(gameInfo.getRacingCarDrivers());
+    }
+
+    public void startRacing() {
+        racingConfig();
+        runRacingDuringRounds();
+        finishRacing();
+    }
+
+    public void runRacingDuringRounds() {
+        for (int i = 0; i < gameInfo.getRound(); i++) {
+            racingCarService.updateRacingResult();
+            view.roundResultView(racingCarService.getRoundResults());
+        }
+    }
+
+    public void finishRacing() {
+        view.championView(racingCarService.getChampionResult());
+    }
+}
