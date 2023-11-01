b908fb7943e9803e88fc3359e64fe47bdfcdde11
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-01 22:17:43 +0900

    우승자 색출 메소드
    1등에 해당하는 위치를 찾는 메소드를 이용해서 우승자를 찾습니다.
    - 가장 처음에는 현재 1등의 위치를 찾습니다.
     - 1등과 같은 위치를 가진 자동차를 승리자 리스트에 추가합니다.
     - 승리자 리스트를 반환합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,13 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+    
 }
 
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -14,11 +15,20 @@ public class Game {
         for (int i = 0; i < round; i++) {
             for (Car car : cars) {
                 car.move();
+                displayResult();
             }
         }
     }
-    public String getWinner() {
-        return null;
+    // getMaximumPosition()을 이용해서 우승자 찾기
+    public List<String> getWinners() {
+        List<String> winners = new ArrayList<>();
+        int maxPosition = getMaximumPosition();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
     }
     private int getMaximumPosition() {
         int max = 0;
@@ -27,6 +37,10 @@ public class Game {
         }
         return max;
     }
-
-
+    private void displayResult() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
 }
