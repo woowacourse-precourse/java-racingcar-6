bccd62416328a06038231a220cc52bf1a6e8e3eb
조은별 <joeunbyeol98@gmail.com>
2023-11-01 22:48:01 +0900

    feat(ViewMessage): final 키워드 추가

--- src/main/java/racingcar/messages/ViewMessage.java
+++ src/main/java/racingcar/messages/ViewMessage.java
@@ -1,11 +1,11 @@
 package racingcar.messages;
 
 public class ViewMessage {
-    public static String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
-    public static String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?\n";
-    public static String OUTPUT_RESULT = "\n실행 결과\n";
-    public static String OUTPUT_CAR_RESULT = "%s : %s\n";
-    public static String OUTPUT_WINNERS = "최종 우승자 : %s\n";
-    public static String MOVEMENT = "-";
-    public static String SEPARATOR = ",";
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?\n";
+    public static final String OUTPUT_RESULT = "\n실행 결과\n";
+    public static final String OUTPUT_CAR_RESULT = "%s : %s\n";
+    public static final String OUTPUT_WINNERS = "최종 우승자 : %s\n";
+    public static final String MOVEMENT = "-";
+    public static final String SEPARATOR = ",";
 }
