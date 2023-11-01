f2e90d744766013380052ca97466626c690e7397
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-01 19:49:38 +0900

    style(PickWinnerServiceTest): 들여쓰기와 공백 변경

--- src/test/java/racingcar/service/PickWinnerServiceTest.java
+++ src/test/java/racingcar/service/PickWinnerServiceTest.java
@@ -25,24 +25,16 @@ public class PickWinnerServiceTest {
 		assertEquals(printWinner, winner);
 	}
 
-	static Stream<Arguments> createCheckWinnerTestValue(){
+	static Stream<Arguments> createCheckWinnerTestValue() {
 		return Stream.of(
-						Arguments.of(
-								new Cars(List.of(new Car("one", 1), new Car("two", 2), new Car("three", 3))),
-								"three"
-								),
-						Arguments.of(
-								new Cars(List.of(new Car("one", 1), new Car("two", 2), new Car("three", 2))),
-								"two, three"
-								),
-						Arguments.of(
-								new Cars(List.of(new Car("one", 1), new Car("two", 1), new Car("three", 1))),
-								"one, two, three"
-								),
-						Arguments.of(
-								new Cars(List.of(new Car("one", 0), new Car("two", 0), new Car("three", 0))),
-								"one, two, three"
-								)
-				);
+						Arguments.of(new Cars(List.of(new Car("one", 1), new Car("two", 2), new Car("three", 3))),
+								"three"),
+						Arguments.of(new Cars(List.of(new Car("one", 1), new Car("two", 2), new Car("three", 2))),
+								"two, three"),
+						Arguments.of(new Cars(List.of(new Car("one", 1), new Car("two", 1), new Car("three", 1))),
+								"one, two, three"),
+						Arguments.of(new Cars(List.of(new Car("one", 0), new Car("two", 0), new Car("three", 0))),
+								"one, two, three")
+						);
 	}
 }
