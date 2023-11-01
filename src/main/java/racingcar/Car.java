41858fef8ef1ea5067cb4edbe8f3127ffa0139dc
aammddkkzxc <ideal0427@naver.com>
2023-11-01 11:17:09 +0900

    refactor: distance 인트형 변수로 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,15 +1,14 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Car implements Comparator<Car> {
+public class Car {
     public static final String ONE_STEP = "-";
 
     private final String name;
-    private final List<String> distance = new ArrayList<>();
+    private int distance;
 
     public Car(String name) {
         this.name = name;
@@ -17,20 +16,15 @@ public class Car implements Comparator<Car> {
 
     public void move(boolean appropriateNumber) {
         if (appropriateNumber) {
-            distance.add(ONE_STEP);
+            distance++;
         }
-    }
-
-    @Override
-    public int compare(Car o1, Car o2) {
-        return o1.distance.size() - o2.distance.size();
     }
 
     public String getName() {
         return name;
     }
 
-    public List<String> getDistance() {
-        return Collections.unmodifiableList(distance);
+    public int getDistance() {
+        return distance;
     }
 }
