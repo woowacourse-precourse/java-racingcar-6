a1a7d0363d7c8e5fbddebded63eaef0c89cfd46a
ddohyeong <dohyeong7026@gmail.com>
2023-11-01 16:27:56 +0900

    refactor(RandomNumberGeneratorTest) : public에서 default 접근 제한자 리팩토링

--- src/test/java/domain/RandomNumberGeneratorTest.java
+++ src/test/java/domain/RandomNumberGeneratorTest.java
@@ -5,10 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class RandomNumberGeneratorTest {
-	// @ParameterizedTest
-	// @ValueSource
 	@Test
-	public void generateNumber_메서드로_랜덤_숫자는_0_9사이의_숫자_생성() {
+	void generateNumber_메서드로_랜덤_숫자는_0_9사이의_숫자_생성() {
 		//given
 		RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 		// when
