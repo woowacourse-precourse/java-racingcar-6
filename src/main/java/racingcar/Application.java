41aab2d2ac622b116a1c2e8b0beb250ef10ec4da
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-01 12:35:04 +0900

    기능 1: 사용자로부터 자동차 이름 입력받고 목록 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,36 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Car> cars = inputCarNames(scanner);
 
     }
-}
+    private static List<Car> inputCarNames(Scanner scanner) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = scanner.nextLine().split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+    static class Car {
+        private String name;
+        private int position;
 
-class Car {
-    private String name;
-    private int position;
+        public Car(String name) {
+            this.name = name;
+            this.position = 0;
+        }
 
-    public Car(String name) {
-        this.name = name;
-        this.position = 0;
+
     }
 }
