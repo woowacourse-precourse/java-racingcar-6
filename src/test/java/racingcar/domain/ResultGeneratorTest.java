4989d1ded5f325d038ff5d7642be70d173f1edcb
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-01 13:39:02 +0900

    test(domain.ResultGenerator) : 기능5. 한 라운드 경주를 실시한다
    playRound 메서드를 실행했을 때 실제로 한 라운드가 진행되는지를 확인하는 테스트 코드를 작성하였습니다.

--- src/test/java/racingcar/domain/ResultGeneratorTest.java
+++ src/test/java/racingcar/domain/ResultGeneratorTest.java
@@ -1,13 +1,43 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 
 class ResultGeneratorTest {
+    private ResultGenerator resultGenerator;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        resultGenerator = new ResultGenerator();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
     @Test
-    void playRound() {
+    void playRound_메서드로_게임라운드_진행() {
+        // given
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car("A", 0));
+        carList.add(new Car("B", 0));
+        carList.add(new Car("C", 0));
+
+        // when
+        resultGenerator.playRound(carList);
+
+        // then
+        assertThat(outputStreamCaptor.toString())
+                .contains("A :", "B : ", "C :");
     }
 
     @Test
@@ -20,5 +50,10 @@ class ResultGeneratorTest {
 
     @Test
     void printFinalResult() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 }
