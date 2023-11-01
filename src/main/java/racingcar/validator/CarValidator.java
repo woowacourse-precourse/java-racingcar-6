dd83140f45938c9a5e4ddbef76cd5a93102d505e
jooda00 <dragon2000a@gmail.com>
2023-11-01 16:07:53 +0900

    refactor(CarValidator): static 변수 이름 변경
    - 의도를 드러내는 변수 이름으로 변경

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -3,14 +3,14 @@ package racingcar.validator;
 import java.util.List;
 
 public class CarValidator {
-    private static final String CAR_NAME_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+    private static final String CAR_NAME_LENGTH_OVER_5 = "자동차 이름은 5자 이하여야 합니다.";
     private static final String CAR_NAME_DUPLICATE = "자동차 이름은 겹치지 않아야 합니다.";
     private static final String CAR_COUNT_UNDER_1 = "최소 두 대의 자동차가 있어야 게임이 가능합니다.";
 
     public void validateCarNameLength(List<String> carList) {
         for (String car : carList) {
             if (car.length() > 5) {
-                throw new IllegalArgumentException(CAR_NAME_LENGTH);
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_OVER_5);
             }
         }
     }
