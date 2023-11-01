f5163eea2c18e34fc2027a53991275e2dfbb5d80
kite707 <12201680@inha.edu>
2023-11-01 21:28:36 +0900

    refactor(racingcar): main함수 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.Game;
+import racingcar.domain.Judgement;
 import racingcar.domain.Referee;
 
 
@@ -13,9 +14,7 @@ public class Application {
         Referee referee = new Referee();
 
         List<String> carNames = askCarNames();
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        int attempts = Integer.parseInt(Console.readLine());
+        int attempts = askAttempts();
 
         List<Integer> scores = makeScores(carNames.size());
         for (int i = 0; i < attempts; i++) {
@@ -24,24 +23,28 @@ public class Application {
         }
 
         List<String> winner = referee.getWinner(carNames, scores);
-        String finalWinner = "";
-        for (int i = 0; i < winner.size(); i++) {
-            finalWinner += winner.get(i);
-            if (i != winner.size() - 1) {
-                finalWinner += ", ";
-            }
-        }
+        String finalWinner = winnerListToString(winner);
         System.out.print("최종 우승자 : " + finalWinner);
-
-
     }
 
     public static List<String> askCarNames() {
+        Judgement judgement = new Judgement();
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
         List<String> carNames = Arrays.asList(carNamesInput.split(","));
+        judgement.validateCarNames(carNames);
 
         return carNames;
+    }
+
+    public static Integer askAttempts() {
+        Judgement judgement = new Judgement();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String number = Console.readLine();
+
+        int attempts = judgement.tryConvertToInteger(number);
+        return attempts;
     }
 
     public static List<Integer> makeScores(int carCount) {
@@ -50,5 +53,16 @@ public class Application {
         List<Integer> scores = Arrays.asList(scoresArray);
 
         return scores;
+    }
+
+    public static String winnerListToString(List<String> winner) {
+        String finalWinner = "";
+        for (int i = 0; i < winner.size(); i++) {
+            finalWinner += winner.get(i);
+            if (i != winner.size() - 1) {
+                finalWinner += ", ";
+            }
+        }
+        return finalWinner;
     }
 }
