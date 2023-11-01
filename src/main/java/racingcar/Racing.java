160298e856893907c2b2edf2166bc953f22535e2
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-01 21:49:13 +0900

    feat: add race and find winner feature
    Add two features for update class Racing
    - create race() method that activates all vehicles during raceCount
    - create findWinner() method that find the winners in maximum position

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,15 +1,40 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Racing {
     Car[] cars;
     int raceCount;
     public Racing(String[] carList, int raceCount) {
         this.raceCount = raceCount;
         cars = new Car[carList.length];
-        for (int i = 0; i < carList.length; i++) {
+        for (int i = 0; i < carList.length; i++)
             cars[i] = new Car(carList[i], 0);
+    }
+
+    public void race() {
+        for (int i = 0; i < raceCount; i++) {
+            for (Car j : cars)
+                j.moveCar();
         }
     }
 
+    public void findWinner() {
+        int maxPosition = -1;
+        ArrayList<String> winnerList = new ArrayList<String>();
+
+        for (Car i : cars) {
+            if (i.position > maxPosition) {
+                maxPosition = i.position;
+                winnerList = new ArrayList<String>();
+                winnerList.add(i.name);
+            }
+            else if (i.position == maxPosition) {
+                winnerList.add(i.name);
+            }
+        }
+    }
+
+    
 
 }
