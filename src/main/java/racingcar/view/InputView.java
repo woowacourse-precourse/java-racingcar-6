4d055d34f165e03b3d3ee523965252a445acdb4b
jeongjihun <wlgns2234@naver.com>
2023-11-01 12:15:03 +0900

    refactor: 상수 이름 변경
    - 명확하고 간단하게 상수 이름을 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,13 +9,13 @@ public class InputView {
     private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRIAL_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private static final String DELIMITER_COMMA = ",";
+    private static final String COMMA = ",";
 
     public static List<String> inputNames() {
         System.out.println(INPUT_NAME_MESSAGE);
         try {
             String names = Console.readLine();
-            return Arrays.asList(names.split(DELIMITER_COMMA));
+            return Arrays.asList(names.split(COMMA));
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[Error] 잘못된 형식의 자동차 이름이 입력되었습니다.", e);
         }
