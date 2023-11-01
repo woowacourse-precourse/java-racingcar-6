11b84617c3497a48f2d105aef58098cc347b0f12
potatomoong <star901210@hanmail.net>
2023-11-01 22:50:56 +0900

    Test: 랜덤 숫자 생성이 되는가에 대한 테스트 구현

--- src/test/java/racingcar/domain/JudgeMovementTest.java
+++ src/test/java/racingcar/domain/JudgeMovementTest.java
@@ -7,7 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class JudgeMovementTest {
 
     @Test
-    void createRandomNumbers() {
+    void 랜덤_숫자_생성() {
+        int carCount = 3;
+        JudgeMovement judgeMovement = new JudgeMovement();
+
+        int[] result = judgeMovement.createRandomNumbers(carCount);
+
+        assertEquals(carCount, result.length);
     }
 
     @Test
