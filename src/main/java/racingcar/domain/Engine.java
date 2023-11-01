9e7d045636b9d8d441ad597fb9d022123638d28f
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-01 18:16:37 +0900

    refactor: 매직넘버 처리
    하드 코딩된 상수에 의미 추가

--- src/main/java/racingcar/domain/Engine.java
+++ src/main/java/racingcar/domain/Engine.java
@@ -4,12 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Engine {
     private final Integer MIN_FORWARD_NUM = 4;
+    private final Integer MIN_RANDOM_NUM = 0;
+    private final Integer MAX_RANDOM_NUM = 9;
 
     public Boolean canMoveForward() {
         return MIN_FORWARD_NUM <= createRandomNumber();
     }
 
     public Integer createRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
     }
 }
