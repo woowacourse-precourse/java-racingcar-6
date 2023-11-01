d32643bca2db35acc1f83caf85510929e8c8a231
leeminjun <sohwi0305@gmail.com>
2023-11-01 15:18:57 +0900

    feat: 우승자 View

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,9 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class OutputView {
+
+    private static final int SOLO = 1;
+
     public static void printHead() {
         System.out.println("실행 결과");
     }
@@ -16,6 +19,24 @@ public class OutputView {
     private static void printCarScore(Car car) {
         System.out.print(car.getName() + " : ");
         printPosition(car);
+        System.out.println();
+    }
+
+    private static boolean checkSoloWin(List<String> carNames) {
+        return carNames.size() == SOLO;
+    }
+
+    public static void printWinners(List<String> carNames) {
+        System.out.print("최종 우승자 : ");
+        System.out.print(carNames.get(0));
+        if (checkSoloWin(carNames)) {
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i < carNames.size(); i++) {
+            System.out.print(", ");
+            System.out.print(carNames.get(i));
+        }
         System.out.println();
     }
 
