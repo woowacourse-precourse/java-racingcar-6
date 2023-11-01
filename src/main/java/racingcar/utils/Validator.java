716cb1064f359bfaa06ccf67703f61714da78b3d
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:30:55 +0900

    feat: Validator의 실행횟수 검증 메소드 구현

---
+++ src/main/java/racingcar/utils/Validator.java
@@ -0,0 +1,23 @@
+package racingcar.utils;
+
+import racingcar.messages.ErrorMessage;
+
+public class Validator {
+    public static void validateRange(int num, int from, int to) throws IllegalArgumentException {
+        if (!(from <= num && num <= to)) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_FOR_COUNTS);
+        }
+    }
+
+    public static void validateDigit(String str) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_INTEGER);
+        }
+    }
+
+    public static void validateRangeOfExecutions(int numOfExecutions) throws IllegalArgumentException {
+        validateRange(numOfExecutions, 1, 1000);
+    }
+}
