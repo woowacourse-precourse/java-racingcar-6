f4c48f12e72894d2d63331c33c2f9c40dd124618
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 16:48:12 +0900

    fix: 출력 문자열 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,8 @@
 package racingcar.view;
 
 public class OutputView {
-    private static final String RACING_RESULT_MESSAGE = "실행 결과";
-    private static final String WINNER_RESULT_MESSAGE = "최종 우승자 : %s\n";
+    private static final String RACING_RESULT_MESSAGE = "\n실행 결과";
+    private static final String WINNER_RESULT_MESSAGE = "최종 우승자 : %s";
 
     public void printRacingResult(final String result) {
         System.out.println(RACING_RESULT_MESSAGE);
