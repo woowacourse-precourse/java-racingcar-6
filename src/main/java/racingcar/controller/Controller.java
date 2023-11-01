bc59ba05c777a3f3ead33e654d23048acb7cadf1
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-01 15:11:54 +0900

    Feat: Controller클래스 생성

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.domain.Game;
+import racingcar.utils.Parser;
+import racingcar.utils.Validator;
+
+
+public class Controller {
+
+    private final Validator validator = new Validator();
+    private final Parser parser = new Parser();
+    private Game game;
+
+}
+
