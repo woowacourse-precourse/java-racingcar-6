e827eff591dd4032b4ae0ffb77ec63b56159c084
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-01 18:08:54 +0900

    feat(racingcar): 입력받은 자동차 이름을 분리하여 저장한다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
+
+        String[] carNames = readLine().split(",");
+
     }
 }
+
