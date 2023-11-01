18d88eeb4dc3f6568a6a6ec7d417727923de5b02
psrom <psrom721@gmail.com>
2023-11-01 15:57:25 +0900

    refactor(input test): 가독성을 위해 TestFactory를 활용

--- src/test/java/racingcar/UserInputTest.java
+++ src/test/java/racingcar/UserInputTest.java
@@ -1,45 +1,31 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class UserInputTest extends NsTest {
-    @Test
-    void 이름_한개인_경우_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi"))
-                        .isInstanceOf(IllegalArgumentException.class)
+
+    @TestFactory
+    Stream<DynamicTest> testInvalidInputs() {
+        return Stream.of(
+                dynamicTest("이름_한개인_경우_예외_처리", () -> testInvalidInput("pobi")),
+                dynamicTest("쉼표_뒤에_공백인_경우_예외_처리", () -> testInvalidInput("pobi, ")),
+                dynamicTest("띄어쓰기가_포함된_이름인_경우_예외_처리", () -> testInvalidInput("pobi,wo ni")),
+                dynamicTest("이름이_중복된_경우_예외_처리", () -> testInvalidInput("pobi,pobi")),
+                dynamicTest("시도_횟수가_0인_경우_예외_처리", () -> testInvalidInput("pobi,woni", "0")),
+                dynamicTest("시도_횟수가_0_미만인_경우_예외_처리", () -> testInvalidInput("pobi,woni", "-5"))
         );
     }
 
-
-    @Test
-    void 쉼표_뒤에_공백인_경우_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi, "))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-
-    @Test
-    void 띄어쓰기가_포함된_이름인_경우_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,wo ni"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-
-    @Test
-    void 이름이_중복된_경우_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,pobi"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    private void testInvalidInput(String... inputs) {
+        assertThatThrownBy(() -> runException(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
