b4b02a271a9e752ae52ae276333d569fe2f02c27
JH CHO <junh9541@gmail.com>
2023-11-01 19:24:07 +0900

    feat(Cars): 생성한 자동체 객체 이동 기능, 중간결과 메세지 생성 기능 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -13,5 +13,20 @@ public class Cars {
     private void addCarToCarList(String carName){
         carList.add(new Car(carName));
     }
+    public String doRace(){
+        StringBuilder raceMsg = new StringBuilder();
+        carList.forEach((car)->{
+            car.tryMove();
+            raceMsg.append(createRaceMsg(car));
+        });
+        return raceMsg.toString();
+    }
+    public String createRaceMsg(Car car){
+        String name = car.getName();
+        int location = car.getLocation();
+        String locationVar = "-".repeat(location);
+        return String.format("%s : %s\n", name, locationVar);
+    }
+
 
 }
