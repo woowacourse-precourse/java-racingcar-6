b7f80d215519c0b09a8287f6a40631175f42dbb2
soyoungxxx <otherplet@gmail.com>
2023-11-01 19:28:40 +0900

    feat(racingcar): 이름 입력 기능 추가

--- src/main/java/racingcar/domain/PlayingService.java
+++ src/main/java/racingcar/domain/PlayingService.java
@@ -1,2 +1,24 @@
-package racingcar.domain;public class PlayingService {
+package racingcar.domain;
+
+import java.util.Map;
+import java.util.HashMap;
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+public class PlayingService {
+    private Car carsName;
+
+    public void generateGame() {
+
+    }
+
+    private void enterName() {
+        String[] name = Console.readLine().split(",");
+        Map<String, Integer> playerAndScore = new HashMap<>();
+        for (String s : name) {
+            s = s.strip();
+            playerAndScore.put(s, 0);
+        }
+        carsName = new Car(playerAndScore);
+    }
 }
