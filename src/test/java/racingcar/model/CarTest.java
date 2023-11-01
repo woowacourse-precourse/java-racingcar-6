292629f73071a16700bdfc87d27287174ba51fc5
Maljaaa <Maljaaa@github.com>
2023-11-01 23:12:32 +0900

    refactor(CarTest): 변수명 변경 및 사용 함수 변경
    - generator -> alwaysMoveGenerator
    - generator -> neverMoveGenerator
    - expected -> expectedPosition
    - actual -> actualPosition
    - assertTrue/False -> assertEquals

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -12,42 +12,44 @@ class CarTest {
     @Test
     public void 전진_가능_테스트() throws Exception {
         //given
-        NumberGenerator generator = new StubNumberGenerator(5);
-        Car car = new Car("minju", generator);
-        int expected = 1;
+        NumberGenerator alwaysMoveGenerator = new StubNumberGenerator(5);
+        Car car = new Car("pobi", alwaysMoveGenerator);
+        int expectedPosition = 1;
 
         //when
         car.move();
+        int actualPosition = car.getPosition();
 
         //then
-        assertTrue(car.getPosition() == expected);
+        assertEquals(expectedPosition, actualPosition);
     }
 
     @Test
     public void 전진_불가능_테스트() throws Exception {
         //given
-        NumberGenerator generator = new StubNumberGenerator(3);
-        Car car = new Car("minju", generator);
-        int expected = 0;
+        NumberGenerator neverMoveGenerator = new StubNumberGenerator(3);
+        Car car = new Car("pobi", neverMoveGenerator);
+        int expectedPosition = 0;
 
         //when
         car.move();
+        int actualPosition = car.getPosition();
 
         //then
-        assertTrue(car.getPosition() == expected);
+        assertEquals(expectedPosition, actualPosition);
     }
 
     @Test
     public void 자동차_이름_부여_테스트() throws Exception {
         //given
         NumberGenerator generator = new StubNumberGenerator(3);
-        Car car = new Car("minju", generator);
-        String expected = "minju";
+        Car car = new Car("pobi", generator);
+        String expectedName = "pobi";
 
         //when
-        String actual = car.getName();
+        String actualName = car.getName();
 
         //then
-        assertTrue(expected.equals(actual));
+        assertEquals(expectedName, actualName);
     }
 }
