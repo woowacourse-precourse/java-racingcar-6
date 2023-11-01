1c6837f68d678c75437102326e8b1fb369c34501
minwoo <mark42son@gmail.com>
2023-11-01 20:52:53 +0900

    style : 메서드 이름 오타 수정하기 및 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -25,10 +25,12 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 이름의_길이_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void 이름에_대한_예외_처리() {
+        assertSimpleTest(
+                () -> {
+                    runException("pobi,javaji");
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
         );
     }
 
