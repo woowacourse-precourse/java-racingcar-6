da09657dbd14b7096534b5a8b3f4da0c60f4647c
thisfetch1591 <thisfetch1591@gmail.com>
2023-11-01 18:09:58 +0900

    test: 여러 개의 테스트 코드를 추가했습니다
    - 이름이 6자 이상일 경우 오류를 테스트 하는 메서드 작성
    - 이름을 입력하지 않았을 경우 오류를 테스트 하는 메서드 작성
    - 이름이 중복될 경우 오류를 테스트 하는 메서드 작성
    - 공백 포함 문자열 입력 시 공백 제거를 테스트 하는 메서드 작성
    - 구분자만 명확하면 한글 기호 상관없이 입력 가능 테스트 하는 메서드 작성

--- src/test/java/racingcar/CustomApplicationTest.java
+++ src/test/java/racingcar/CustomApplicationTest.java
@@ -1,11 +1,64 @@
 package racingcar;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.validator.CarInputValidator;
+import racingcar.domain.Car;
+import racingcar.view.ConsoleInput;
 
 public class CustomApplicationTest {
 
+    protected void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
     @Test
     void 이름이_6자_이상일_경우_오류() {
+        assertThatThrownBy(() -> {
+            Car car = Car.nameOf("이름이6글자");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 이름을_입력하지_않았을_경우_오류() {
+        assertThatThrownBy(() -> {
+            Car car = Car.nameOf("");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_중복될_경우_오류() {
+        assertThatThrownBy(() -> {
+            systemIn("pobi,pobi,hello");
+            ConsoleInput.carNamesInput();
+        })
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 공백_포함_문자열_입력_시_공백제거() {
+        systemIn("pobi     ,pedro  ,hello");
+        List<Car> carList = ConsoleInput.carNamesInput();
+        List<String> carNameList = carList.stream()
+                .map(car -> car.getName())
+                .toList();
+        assertThat(carNameList).containsExactly("pobi", "pedro", "hello");
+    }
+
+    @Test
+    void 구분자만_명확하면_한글_기호_상관없이_입력가능() {
+        systemIn("한글1호,한글2호!,thisi");
+        List<Car> carList = ConsoleInput.carNamesInput();
+        List<String> carNameList = carList.stream()
+                .map(car -> car.getName())
+                .toList();
+        assertThat(carNameList).containsExactly("한글1호", "한글2호!", "thisi");
+    }
+
+
 }
