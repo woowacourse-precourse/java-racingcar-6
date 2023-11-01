c3c9bdb4e715650640389e164c21a001a07bd2de
lesw <lesw1216@gmail.com>
2023-11-01 21:50:41 +0900

    refactor: 필드에 final 추가

--- src/main/java/racingcar/dto/ControlTowerDto.java
+++ src/main/java/racingcar/dto/ControlTowerDto.java
@@ -4,8 +4,8 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class ControlTowerDto {
-    private List<Car> cars;
-    private int roundNumber;
+    private final List<Car> cars;
+    private final int roundNumber;
 
     public ControlTowerDto(List<Car> cars, int roundNumber) {
         this.cars = cars;
