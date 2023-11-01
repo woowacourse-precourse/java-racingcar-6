040367b189fcbeaddf2d91822fa9b27485a0e085
djc06048 <gsafe1213@gmail.com>
2023-11-01 10:56:21 +0900

    feat(controller): GameController 추가view, model 사이 중재

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,63 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.model.GameNumber;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GameController {
+    private static int tryCount;
+    private static List<Car> carsList;
+    private static List<Car> winnerList;
+
+    public static void startGame() { // 뷰는 컨트롤러에만 의존한다.
+        carsList = Cars.generateCar(InputView.generateCarNameInput());
+        tryCount= InputView.generateTryCountInput();
+        OutputView.processResult();
+        while (tryCount-- > 0) {
+            for (Car car: carsList) {
+                GameNumber gameNumber = new GameNumber();
+                int length = car.checkProcess(gameNumber.getRandomNumber());
+                String process = makeProcessBar(length);
+                System.out.println(car.getName() + " : " + process);
+            }
+            System.out.println();
+
+        }
+        winnerList=findWinners(carsList);
+        OutputView.GameResult();
+        for (int i = 0; i < winnerList.size(); i++) {
+            System.out.println(winnerList.get(i).getName());
+        }
+    }
+
+    private static String makeProcessBar(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    private static List<Car> findWinners(List<Car> carsList) {
+        List<Car> winners = new ArrayList<>();
+        int maxCarLength = -1;
+        for (Car car : carsList) {
+            if (car.getLength() > maxCarLength) {
+                maxCarLength = car.getLength();
+                winners.clear();
+                winners.add(car);
+            } else if (car.getLength() == maxCarLength) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+
+}
