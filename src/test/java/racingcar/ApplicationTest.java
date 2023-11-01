bc7efc236ffd7bc108f1286c3300963b169465e1
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-01 09:28:24 +0900

    test: 이름, 시도 횟수, 중복 우승자 경우에 대한 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,53 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 이름_사이에_엄청난_공백() {
+        assertSimpleTest(() ->
+                assertDoesNotThrow(() -> run("pobi,                                               woni", "1"))
+        );
+    }
+
+    @Test
+    void 이름이_길이가_0인_문자열일때_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_길이가_6을_넘는_문자열일때_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("someincredibleanduniqueusernamebutinvalidusername", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 중복된_이름에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 쉼표_사이에_이름_없을때_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도_횟수가_0인_것에_대한_예외_처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
     void 시도_횟수가_0보다_작은것에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,woni", "-1"))
@@ -55,6 +103,17 @@ class ApplicationTest extends NsTest {
                     assertThat(output()).contains("pobi : --", "woni : --", "sup : ", "최종 우승자 : pobi, woni");
                 },
                 MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 아무도_전진하지_못한_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,sup", "2");
+                    assertThat(output()).contains("pobi : ", "woni : ", "sup : ", "최종 우승자 : pobi, woni, sup");
+                },
+                STOP, STOP, STOP, STOP, STOP, STOP
         );
     }
 
