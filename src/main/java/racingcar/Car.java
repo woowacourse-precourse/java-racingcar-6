838679e2c34bf87c0517923adf84d18ebcbeeee8
leedoyeon849 <do83430208@gmail.com>
2023-11-01 19:10:16 +0900

    docs: Car 객체 주석 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,17 +1,28 @@
 package racingcar;
 
 public class Car {
+    /**
+     * 자동차의 이름입니다.
+     */
     private String name;
+    /**
+     * 자동차의 전진 횟수입니다.
+     */
     private Integer score = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
+    /** 자동차의 전진 횟수를 1증가시킵니다 */
     public void upScore() {
         score += 1;
     }
 
+    /**
+     * 자동차의 점수 현황을 출력합니다.
+     * '-'는 전진 횟수입니다.
+     * */
     @Override
     public String toString() {
 
@@ -22,5 +33,13 @@ public class Car {
         }
 
         return name + " : " + scoreBoard;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
     }
 }
