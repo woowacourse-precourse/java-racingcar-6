56a8ac084d7d6f566198571f50de1c6d6ae817db
djc06048 <gsafe1213@gmail.com>
2023-11-01 12:09:51 +0900

    style: 자바 코딩 규칙 반영해 클래스 메서드 호출 방식 변경

--- src/main/java/racingcar/util/CarNameValidator.java
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -1,10 +1,10 @@
 package racingcar.util;
 
 public class CarNameValidator {
-    private final static int MAXCARNAME = 5;
+    private final static int MAX_CARNAME = 5;
 
     public static String validateCarName(String carName) {
-        if (carName.length() > MAXCARNAME) {
+        if (carName.length() > MAX_CARNAME) {
             throw new IllegalArgumentException("자동차 이름이 5글자 이하이어야 합니다");
         } else return carName;
     }
