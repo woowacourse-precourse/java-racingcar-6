a93f4c2cef50bfb2d020d5bdb823784a095b63a3
bnfkim <kgr3819@gmail.com>
2023-11-01 11:57:25 +0900

    Refactor: private static final -> enum 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,12 +9,6 @@ public class InputView {
 
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_GAME_ROUND = "시도할 회수는 몇회인가요?";
-    private static final String BLANK_STRING = "입력 문자열이 비어 있습니다.";
-    private static final String REQUIRE_LENGTH_UNDER_FIVE = "자동차 이름을 5자 이하로 입력해주세요.";
-    private static final String REQUIRE_INTEGER_TYPE = "자동차 이름을 정수 값을 입력해주세요.";
-    private static final String REQUIRE_NOT_BLANK = "자동차 이름을 공백 없이 입력해주세요.";
-    private static final String REQUIRE_NOT_DUPLICATE = "자동차 이름을 중복 없이 입력해주세요.";
-    private static final String REQUIRE_OVER_ONE = "1회 이상 게임이 진행 되어야 합니다.";
 
     public static String inputCarName() {
         System.out.println(INPUT_CAR_NAME);
@@ -28,50 +22,58 @@ public class InputView {
         validInputGameRound(input);
         return input;
     }
+
     public static ArrayList<Car> inputCars(ArrayList<String> carNames) {
-        validCarNameBlank(carNames);
-        validCarNameLength(carNames);
-        validCarsDuplicate(carNames);
-
-        ArrayList<Car> cars = new ArrayList<>();
-        for(String car : carNames) cars.add(new Car(car));
-        return cars;
+        validInputCars(carNames);
+        return createCarListByCarNames(carNames);
     }
 
-    private static void validInputBlank(String input){
-        if(input.isBlank()) throw new IllegalArgumentException(BLANK_STRING);
-    }
-
-    private static void validCarNameLength(ArrayList<String> cars) {
-        for(String car : cars){
-            if(car.length() > 5) throw new IllegalArgumentException(REQUIRE_LENGTH_UNDER_FIVE);
-        }
-    }
-
-    private static void validCarNameBlank(ArrayList<String> cars) {
-        for(String car : cars){
-            if (car.contains(" ")) throw new IllegalArgumentException(REQUIRE_NOT_BLANK);
-        }
-    }
-
-    private static void validCarsDuplicate(ArrayList<String> cars){
-        HashSet<String> set = new HashSet<>(cars);
-        if(set.size() != cars.size()) throw new IllegalArgumentException(REQUIRE_NOT_DUPLICATE);
-    }
-
-    private static void validInputGameRound(String input) {
+    public static void validInputGameRound(String input) {
         validInputBlank(input);
         validInputInteger(input);
         validNotUnderZero(Integer.parseInt(input));
     }
 
+    public static void validInputCars(ArrayList<String> carNames) {
+        validCarNameBlank(carNames);
+        validCarNameLength(carNames);
+        validCarsDuplicate(carNames);
+    }
+
+    public static void validInputBlank(String input){
+        if(input.isBlank()) throw new IllegalArgumentException(ErrorMessage.BLANK_STRING.message());
+    }
+
+    private static ArrayList<Car> createCarListByCarNames(ArrayList<String> carNames) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for(String car : carNames) cars.add(new Car(car));
+        return cars;
+    }
+
+    private static void validCarNameLength(ArrayList<String> cars) {
+        for(String car : cars){
+            if(car.length() > 5) throw new IllegalArgumentException(ErrorMessage.REQUIRE_LENGTH_UNDER_FIVE.message());
+        }
+    }
+
+    private static void validCarNameBlank(ArrayList<String> cars) {
+        for(String car : cars){
+            if (car.contains(" ")) throw new IllegalArgumentException(ErrorMessage.REQUIRE_NOT_BLANK.message());
+        }
+    }
+
+    private static void validCarsDuplicate(ArrayList<String> cars){
+        HashSet<String> set = new HashSet<>(cars);
+        if(set.size() != cars.size()) throw new IllegalArgumentException(ErrorMessage.REQUIRE_NOT_DUPLICATE.message());
+    }
+
     private static void validInputInteger(String input){
         for(int i=0; i<input.length(); i++) {
-            if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException(REQUIRE_INTEGER_TYPE);
+            if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException(ErrorMessage.REQUIRE_INTEGER_TYPE.message());
         }
     }
 
     private static void validNotUnderZero(int num){
-        if(num<=0) throw new IllegalArgumentException(REQUIRE_OVER_ONE);
+        if(num<=0) throw new IllegalArgumentException(ErrorMessage.REQUIRE_OVER_ONE.message());
     }
 }
