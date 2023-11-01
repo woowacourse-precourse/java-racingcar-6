a92b3ea0f3258bde7b795ea68d19a45d637a74b0
yerimya <syl1602@naver.com>
2023-11-01 16:21:35 +0900

    [feat] 랜덤 정수 생성하는 generator 도메인

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -6,7 +6,7 @@ public class RandomNumberGenerator {
     private static final int MIN_NUMBER_SIZE = 0;
     private static final int MAX_NUMBER_SIZE = 9;
 
-    public int generate() {
+    public static int generate() {
         return Randoms.pickNumberInRange(MIN_NUMBER_SIZE, MAX_NUMBER_SIZE);
     }
 }
