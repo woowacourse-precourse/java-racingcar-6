7ecac962170108f513151c098883bbb18789f1ef
Soonrang <clearsbyul30@naver.com>
2023-11-01 23:25:55 +0900

    최종우승자 반환양식 수정

--- src/main/java/racingcar/PlayController.java
+++ src/main/java/racingcar/PlayController.java
@@ -5,12 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class PlayController {
-
-    List<Integer> positions = new ArrayList<>();
-
     // 자동차 이름 부여
-    public List<String> generateRacingCarName(){
-        //System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    public List<String> generateRacingCarName() {
         List<String> players = new ArrayList<>();
 
         String inputPlayers = Console.readLine();
@@ -20,28 +16,29 @@ public class PlayController {
         players.addAll(Arrays.asList(playersTemp));
         return players;
     }
-    public void movingCar(List<String> players, List<Integer> randomNumbers, List<Integer> positions){
-        List<String> movingPlayersList = new ArrayList<>();
-        for(int i=0; i<players.size(); i++){
-            if(randomNumbers.get(i) >3){
-                positions.set(i, positions.get(i)+1);
-                movingPlayersList.add(players.get(i));
+
+    public void movingCar(List<String> players, List<Integer> randomNumbers, List<Integer> positions) {
+        for (int i = 0; i < players.size(); i++) {
+            if (randomNumbers.get(i) > 3) {
+                int newPosition = positions.get(i) + 1;
+                positions.set(i, newPosition);
             }
         }
     }
 
-    public String winPlayers(List<String> players, List<Integer> positions){
+    public String winPlayers(List<String> players, List<Integer> positions) {
         List<String> winnerPlayers = new ArrayList<>();
         int winnerPosition = Collections.max(positions);
 
-        for(int i=0; i<positions.size(); i++){
-            if(winnerPosition == positions.get(i)){
+        for (int i = 0; i < positions.size(); i++) {
+            if (winnerPosition == positions.get(i)) {
                 winnerPlayers.add(players.get(i));
             }
         }
 
-        System.out.println(winnerPlayers.toString());
-        return winnerPlayers.toString();
-    }
+        String winnerMessage = "최종 우승자: " + String.join(", ", winnerPlayers);
 
+        System.out.println(winnerMessage);
+        return winnerMessage;
+    }
 }
