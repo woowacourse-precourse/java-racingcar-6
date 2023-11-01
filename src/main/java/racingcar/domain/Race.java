b20aa74272abae13c019586a24c38a87bd96e2af
Hyoseop Song <crad_on25@naver.com>
2023-11-01 22:23:45 +0900

    fix(Race): 기능 목록 주석 제거

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -4,10 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Race {
-    /**
-     * Race (Domain, 실제로 경주를 진행하는 객체) 경주에 참여하는 자동차들의 목록을 설정하는 기능 (InputView) 라운드 별 자동차의 전진/멈춤을 결정하는 기능 (Car) 라운드 별 경기
-     * 결과를 저장하고 반환하는 기능 (RaceResult)종료 조건을 확인하는 기능 (라운드 수)
-     */
     private final List<Car> cars;
     private final int rounds;
     private int currentRound;
