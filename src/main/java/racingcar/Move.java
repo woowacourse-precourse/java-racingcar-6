8a5a9445aa5a0b053305c173694264944ebccd5c
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-01 11:02:00 +0900

    feat: movement
    - 이동 구현: 다음을 사용자가 입력한 숫자만큼 반복
      - 자동차마다 이동 여부 결정
      - 해당 시점에서 자동차가 지금까지 이동한 정도 출력

--- src/main/java/racingcar/Move.java
+++ src/main/java/racingcar/Move.java
@@ -1,8 +1,13 @@
 package racingcar;
+import java.util.Collections;
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Move {
+    ArrayList<Integer> dist = new ArrayList<>();
+    ArrayList<String> names = new ArrayList<>();
+    int times = 0;
+
     private boolean chance() {
         if (Randoms.pickNumberInRange(0, 9) < 4) {
             return false;
@@ -10,7 +15,32 @@ public class Move {
         return true;
     }
 
+    private void run(Integer index) {
+        if (chance()) {
+            dist.set(index, dist.get(index) + 1);
+        }
+        System.out.printf("%s : %s\n", names.get(index), "-".repeat(dist.get(index)));
+    }
+
+    private int makeMovement() {
+        for (int i = 0; i < times; i++) {
+            for (int j = 0; j < names.size(); j++) {
+                run(j);
+            }
+            System.out.println();
+        }
+
+        return Collections.max(dist);
+    }
+
     public void move(ArrayList<String> names, int times) {
+        this.names = names;
+        this.times = times;
+
+        for (int i = 0; i < names.size(); i++) {
+            this.dist.add(0);
+        }
+
         return;
     }
 }
