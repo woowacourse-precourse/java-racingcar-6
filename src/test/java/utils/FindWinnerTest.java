34451909a4016e0545f54b0d0b85117e5bc90549
minnjung0887921 <minjeong0887921@gmail.com>
2023-11-01 20:42:40 +0900

    test : uilts/FindWinner 테스트 코드 작성

---
+++ src/test/java/utils/FindWinnerTest.java
@@ -0,0 +1,30 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class FindWinnerTest {
+    @Test
+    void getMostMovedCarTest() {
+        ArrayList<String> carDistanceList = new ArrayList<>();
+        carDistanceList.add("a : -");
+        carDistanceList.add("b : ");
+        ArrayList<String> result = findWinner.getMostMovedCar(carDistanceList);
+
+        assertThat(result).containsExactly("a");
+    }
+
+    @Test
+    void getRaceResultTest() {
+        ArrayList<String> carDistanceList = new ArrayList<>();
+        carDistanceList.add("a : -");
+        carDistanceList.add("b : ");
+        carDistanceList.add("c : -");
+        String result = findWinner.getRaceResult(carDistanceList);
+
+        assertThat(result).isEqualTo("최종 우승자 : a, c");
+    }
+}
