3dee62e95a92f3627354f94e8b98d94ee8a7006d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-01 14:16:30 +0900

    refactor(announcer): 줄바꿈 메서드를 따로 추출하지 않고 상수를 통해 기존 출력 메서드 사용으로 변경

--- src/main/java/racingcar/view/Announcer.java
+++ src/main/java/racingcar/view/Announcer.java
@@ -8,6 +8,7 @@ public class Announcer {
     private final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private final String RESULT_COMMENT = "실행 결과";
+    private final String NEW_LINE = "";
 
     public void announceToInputCarName() {
         println(INPUT_CAR_NAME);
@@ -18,7 +19,7 @@ public class Announcer {
     }
 
     public void announceRaceHistories(List<History> historyList) {
-        println();
+        println(NEW_LINE);
         println(RESULT_COMMENT);
 
         for (History history : historyList) {
@@ -32,7 +33,7 @@ public class Announcer {
             println(name + " : " + "-".repeat(nameAndPosition.get(name)));
         }
 
-        println();
+        println(NEW_LINE);
     }
 
     public void announceWinner(List<String> winnerList) {
@@ -42,9 +43,5 @@ public class Announcer {
 
     private void println(String message) {
         System.out.println(message);
-    }
-
-    private void println() {
-        System.out.println("");
     }
 }
