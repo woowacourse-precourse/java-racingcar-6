be869c7278f8c614128fecf082c01cba20363a10
june-777 <wlwhswnsrl96@gmail.com>
2023-11-01 13:03:21 +0900

    refactor(OutputView): 가독성을 고려하여 상수 변수명 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,9 +5,9 @@ import racingcar.dto.OneGameResultsDto;
 import racingcar.dto.ResultDto;
 
 public class OutputView {
-    private static final String HYPHEN = "-";
     private static final String PLAY_RESULT_TEXT = "\n실행 결과";
     private static final String RESULT_FORM = "%s : %s%n";
+    private static final String RESULT_FORM_HYPHEN = "-";
     private static final String FINAL_WINNER_FORM = "최종 우승자 : %s";
     private static final String FINAL_WINNER_FORM_DELIMITER = ", ";
 
@@ -17,7 +17,7 @@ public class OutputView {
 
     public void printResult(OneGameResultsDto resultDto) {
         for (ResultDto result : resultDto.getResults()) {
-            System.out.printf(RESULT_FORM, result.getName(), HYPHEN.repeat(result.getPosition()));
+            System.out.printf(RESULT_FORM, result.getName(), RESULT_FORM_HYPHEN.repeat(result.getPosition()));
         }
         System.out.println();
     }
