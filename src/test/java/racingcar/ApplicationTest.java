f9c9cef75bfc2c58e6ecc08f98919512fb3dcf87
kwon <kckc0608@naver.com>
2023-11-01 21:17:39 +0900

    feat: 공동우승, Car class 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,18 +24,22 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 공동_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-    }
-
-    @Test
-    void 자동차_이동_테스트() {
-        Car car = new Car("test");
-        car.move();
-        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
