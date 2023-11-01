b5d73f30c4fa9dd3fa33662dfe59fa77af47cbe8
dongwonkwon <ehddnjs0710@gmail.com>
2023-11-01 23:10:08 +0900

    feat: Racing클래스 자동차입력요구 출력문

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,9 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Car {
