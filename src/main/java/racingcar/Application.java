7d20dd76da8cd5baea14da03a38ce2f8f4f47078
Sungkyu <kxu4583@naver.com>
2023-11-01 15:27:05 +0900

    feat(Application): createRacerFromInput 함수 구현
    사용자로부터 입력을 받아 경주자 객체 배열을 생성함. 입력 예외 처리는 아직 되지 않았음.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
+    static Racer[] createRacerFromInput() {
+
+        String[] names = Console.readLine().split(",");
+
+        Racer[] racers = new Racer[names.length];
+
+        for(int i=0; i< names.length; i++){
+            racers[i] = new Racer(names[i]);
+        }
+
+        return racers;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        Racer[] racers = createRacerFromInput();
     }
 }
