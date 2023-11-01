7bd463bb1d9c74204d01aa547c272b3e267a131e
senzoo-oh <osj3418@gmail.com>
2023-11-01 19:08:06 +0900

    refactor: move RacingCar class to racingcar package
    RacingCar 클래스를 racingcar 패키지 내부로 이동

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCar {
+    String name;
+    int moveCount;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+    private int rollDice() {
+        int diceNumber = Randoms.pickNumberInRange(0,9);
+        return diceNumber;
+    }
+    private void move(int diceNumber) {
+        if (4 <= diceNumber) moveCount++;
+    }
+    public int showMoveCount() {
+        return moveCount;
+    }
+}
