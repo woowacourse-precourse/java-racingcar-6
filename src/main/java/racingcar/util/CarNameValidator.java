99f9f55f76779e0149802baf916b350282b12c87
djc06048 <gsafe1213@gmail.com>
2023-11-01 10:50:11 +0900

    feat(util): 자동차 이름 입력 부분 validator 구현

---
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import racingcar.model.Car;
+
+public class CarNameValidator {
+    private final static int MAXCARNAME = 5;
+
+    public static String validateCarName(String carName) {
+        if (carName.length() > MAXCARNAME) {
+            throw new IllegalArgumentException("자동차 이름이 5글자 이하이어야 합니다");
+        } else return carName;
+    }
+}
