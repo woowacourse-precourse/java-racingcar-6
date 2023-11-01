248d0b35b613e9ebb7ebbb4d4e0d20631d39bb19
권태현 <kygkth2011@gmail.com>
2023-11-01 22:14:10 +0900

    feat(GameSetting) : 게임 횟수, 게임 참여 차 List를 관리하는 GameSetting 구현

---
+++ src/main/java/racingcar/GameSetting.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameSetting {
+    private Integer repeatNumber;
+    private List<Car> cars = new ArrayList<>();
+
+    public GameSetting(Integer repeatNumber, List<Car> cars) {
+        this.repeatNumber = repeatNumber;
+        this.cars = cars;
+    }
+}
