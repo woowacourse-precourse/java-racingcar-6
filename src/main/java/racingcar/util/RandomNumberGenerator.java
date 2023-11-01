448ae9473b3d3e143b1a7a5a44e5ff832c0e214f
hyocheol An <anhyocheol@naver.com>
2023-11-01 22:55:30 +0900

    feat : 자동차 이동 메서드 구현

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,9 +2,7 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberGenerator {
-
     public int getRandomNumber () {
         return Randoms.pickNumberInRange(0,9);
     }
-
 }
