6077a4fd2f61d58909a5f161fb5c083b4f0a8db0
jooda00 <dragon2000a@gmail.com>
2023-11-01 20:21:10 +0900

    test(RacingGameTest): 자동차가 전진/멈추는 경우 테스트
    - movingCount의 addCount가 전진할 때만 작동하는지 확인

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,18 +13,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingGameTest {
     private RacingGame racingGame;
     private MovingCount movingCount;
+    private OutputView outputView;
 
     @BeforeEach
     void setUp() {
         movingCount = new MovingCount();
-        racingGame = new RacingGame(movingCount);
+        outputView = new OutputView();
+        racingGame = new RacingGame(movingCount, outputView);
+        movingCount.initMovingCount(3);
     }
 
     @Test
     void 랜덤값_추출해서_리스트에_정상_저장() {
         List<String> carList = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
-        int size = carList.size();
-        movingCount.initMovingCount(size);
         List<Integer> numberList = racingGame.extractRandomNumber(carList);
         List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 3, 5));
 
@@ -34,7 +36,6 @@ public class RacingGameTest {
     void 단독_우승한_자동차_리스트_정상_리턴() {
         List<String> carList = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
         int size = carList.size();
-        movingCount.initMovingCount(size);
         movingCount.addCount(0);
         movingCount.addCount(1);
         movingCount.addCount(1);
@@ -48,7 +49,6 @@ public class RacingGameTest {
     void 공동_우승한_자동차_리스트_정상_리턴() {
         List<String> carList = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
         int size = carList.size();
-        movingCount.initMovingCount(size);
         movingCount.addCount(0);
         movingCount.addCount(1);
         movingCount.addCount(1);
@@ -57,5 +57,23 @@ public class RacingGameTest {
         List<String> winnerList = new ArrayList<>(Arrays.asList("woni", "jun"));
 
         assertThat(winnerList).isEqualTo(racingGame.extractResult(carList, size));
+    }
+
+    @Test
+    void 자동차가_전진하는_경우() {
+        int target = 4;
+        int index = 0;
+        int count = movingCount.addCount(index);
+
+        assertThat(count + 1).isEqualTo(racingGame.evaluateMoving(target, count, index));
+    }
+
+    @Test
+    void 자동차가_전진하지_않는_경우() {
+        int target = 2;
+        int index = 0;
+        int count = movingCount.addCount(index);
+
+        assertThat(count).isEqualTo(racingGame.evaluateMoving(target, count, index));
     }
 }
