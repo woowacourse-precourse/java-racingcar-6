2a2fee73205e39c0459fe6a36dd913b9082c7e14
OhJongMin <jmoh7480@gmail.com>
2023-11-01 13:37:32 +0900

    test(racingcar):자동차 이름에 중복이 있을 때 테스트

--- src/test/java/racingcar/InputExceptionTest.java
+++ src/test/java/racingcar/InputExceptionTest.java
@@ -3,19 +3,32 @@ package racingcar;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.Service.InputException;
 
 public class InputExceptionTest {
+    private InputException ex;
+    @BeforeEach
+    void setUp(){
+        ex = new InputException();
+    }
     @Test
     void charAt_이름_5글자_이상_일_때_예외_발생(){
         List<String> input = Arrays.asList("aaaaaa","bbbbb");
-        InputException ex = new InputException();
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
                 ex.validCarNameRange(input);
         });
     }
 
+    @Test
+    void charAt_이름_중복이_있을_때_예외_발생(){
+        List<String> input = Arrays.asList("aaa","bbb","aaa");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ex.validCarNameDuplicated(input);
+        });
+    }
 
 }
