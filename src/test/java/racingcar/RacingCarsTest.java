9de83d27dfb16aa7cb2479224a8af46d0b2b5078
jihun-24k <gi020477@gmail.com>
2023-11-01 19:21:05 +0900

    test: 우승자 찾기 테스트

--- src/test/java/racingcar/RacingCarsTest.java
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,5 +32,18 @@ class RacingCarsTest {
         assertEquals(0, cars.get(0).getDistance());
         assertEquals(1, cars.get(1).getDistance());
         assertEquals(1, cars.get(2).getDistance());
+    }
+
+    @Test
+    void 우승자_찾기() {
+        List<Integer> firstTry = List.of(1, 4, 6);
+        List<Integer> secondTry = List.of(5, 4, 6);
+
+        racingCars.moveCars(firstTry);
+        racingCars.moveCars(secondTry);
+
+        List<String> winners = racingCars.findWinners();
+        assertTrue(winners.contains("woni"));
+        assertTrue(winners.contains("jun"));
     }
 }
