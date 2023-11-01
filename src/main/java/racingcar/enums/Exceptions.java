4e0e339e27af344cedc4d7819b9e34ee97261b4d
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 20:06:33 +0900

    feat : 자동차 이름 중복 검사 커스텀 예외 클래스 생성, 예외 메시지 추가

--- src/main/java/racingcar/enums/Exceptions.java
+++ src/main/java/racingcar/enums/Exceptions.java
@@ -3,7 +3,8 @@ package racingcar.enums;
 public enum Exceptions {
     CAR_NAME_IS_OVER_5("자동차 이름은 5글자 이하여야 합니다."),
     CAR_NAME_IS_BLANK("빈 자동차 이름은 입력할 수 없습니다."),
-    TRY_NUMBER_IS_UNDER_0("시도 횟수는 0 이상이어야 합니다.")
+    TRY_NUMBER_IS_UNDER_0("시도 횟수는 0 이상이어야 합니다."),
+    DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다.")
     ;
 
     private String message;
