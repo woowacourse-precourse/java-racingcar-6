a34ee47e584b2713cabfd5972be9113598bc70ea
leesh <qwqw8019@naver.com>
2023-11-01 16:19:26 +0900

    feat: 잘못된 시도 회수 입력 테스트 변수 주입

--- src/main/java/racingcar/util/TestConstantsProvider.java
+++ src/main/java/racingcar/util/TestConstantsProvider.java
@@ -141,4 +141,15 @@ public class TestConstantsProvider {
         );
     }
 
+    public static Stream<Arguments> provideInvalidSystemTestPlayNumberConstants() {
+        return Stream.of(
+                arguments(ONE_CAR_NAME, named("null", NULL_STRING)),
+                arguments(ONE_CAR_NAME, named("빈 문자열", EMPTY_STRING)),
+                arguments(ONE_CAR_NAME, named("공백 문자열", BLANK_STRING)),
+                arguments(ONE_CAR_NAME, named("0", PLAY_NO_TIME)),
+                arguments(ONE_CAR_NAME, named("0보다 작은 값", PLAY_NEGATIVE_TIME)),
+                arguments(ONE_CAR_NAME, named("숫자가 아닌 값", NOT_NUMBER_STRING))
+        );
+    }
+
 }
