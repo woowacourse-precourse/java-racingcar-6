d92155ec4f65cc320f968d886b0c8b60ff63ebc9
Kim sun wook <kimsw9603@gmail.com>
2023-11-01 23:20:15 +0900

    6. 경주 게임 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
     }
+
 }
