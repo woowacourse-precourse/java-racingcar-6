36f624f568cddc199b439c22947c604f4502e000
CJ <choijun0627@konkuk.ac.kr>
2023-11-01 19:44:19 +0900

    Feat : 무작위 값 생성 후 4이상 체크 기능
    무작위 값을 생성해 4 이상인지 체크하는 기능인
    boolean isOverFour() 메소드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String carName;
@@ -15,6 +17,14 @@ public class Car {
         if (carName.length() > 5) {
             throw new IllegalArgumentException("자동차 이름이 5자 초과인 잘못된 입력입니다.");
         }
+    }
+
+    public boolean isOverFour() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
     }
 
     public int getMovingCount() {
