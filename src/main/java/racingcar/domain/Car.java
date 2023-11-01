67aada588c47608ff726b918c8708da40120d5cc
wochae <officialwochae@gmail.com>
2023-11-01 11:04:38 +0900

    feat(domain): class Car implement Entity
    속성들 getter 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,11 +5,21 @@ public class Car {
     private final String name;
     private final int position;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
     public Car(int id, String name, int position) {
         this.id = id;
         this.name = name;
         this.position = position;
     }
-
-
 }
