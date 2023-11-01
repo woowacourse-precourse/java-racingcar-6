86627e40ca5bc4fff80f5ed3ec72ba53a8e72dd8
poow810 <poow810@naver.com>
2023-11-01 12:24:52 +0900

    fix($Test) : update input validate test Add new event to $Test: - use assertThrows method instead of assert method Resolves : #1

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -15,38 +15,27 @@ class InputValidatorTest {
     private final GameService gameService = new GameService();
 
     @Test
-    void is_car_name_null() {
+    @DisplayName("자동차 이름이 비어 있는지 테스트")
+    void is_car_name_blank() {
         List<Car> carList = Arrays.asList(new Car(null),
                 new Car("자동차2"), new Car("자동차3"));
-
-        try {
-            gameService.validateInput(carList);
-        } catch (Exception e) {
-            assert (e instanceof IllegalArgumentException);
-        }
+        assertThrows(IllegalArgumentException.class, () -> gameService.validateInput(carList),
+                "자동차 이름을 입력하여 주세요");
     }
 
     @Test
     @DisplayName("자동차 이름이 5자를 초과하는지 테스트")
     void is_name_too_long() {
         List<Car> carList = List.of(new Car("이름이 긴 아이"));
-
-        try {
-            gameService.validateInput(carList);
-        } catch (Exception e) {
-            assert (e instanceof IllegalArgumentException);
-        }
+        assertThrows(IllegalArgumentException.class, () -> gameService.validateInput(carList),
+                "이름은 5자 이하로 입력하여 주세요");
     }
 
     @Test
     @DisplayName("자동차 이름이 중복되는지 테스트")
     void is_name_duplicate() {
         List<Car> carList = Arrays.asList(new Car("이름1"), new Car("이름2"), new Car("이름1"));
-
-        try {
-            gameService.validateInput(carList);
-        } catch (Exception e) {
-            assert (e instanceof IllegalArgumentException);
-        }
+        assertThrows(IllegalArgumentException.class, () -> gameService.validateInput(carList),
+                "자동차 이름은 중복되지 않도록 해주세요");
     }
 }
