bcb91ccc767e4c084ff39b2e72b58a8e3830fb1d
jihun-24k <gi020477@gmail.com>
2023-11-01 13:42:37 +0900

    test: 랜덤값 4이상이면 자동차 전진

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,9 +2,10 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RacingCarTest {
 
@@ -19,5 +20,14 @@ class RacingCarTest {
     void 자동차_이름은_5자이하_예외_처리() {
         assertThatThrownBy(() -> new RacingCar("jihun_woowa"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @CsvSource({"4", "5", "6", "7", "8", "9"})
+    @ParameterizedTest
+    void 랜덤값이_4이상이면_자동차_전진(int inputNumber) {
+        RacingCar racingCar = new RacingCar("jihun");
+        racingCar.move(inputNumber);
+
+        assertEquals(1, racingCar.getDistance());
     }
 }
