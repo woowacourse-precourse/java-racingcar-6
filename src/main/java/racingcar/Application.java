f0bb4318afd85e81fa436fcfdf3c617d8c688c98
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-01 17:27:12 +0900

    refactor: TODO삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,6 @@ import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         RaceGameController controller = new RaceGameController();
         controller.startGame();
 
