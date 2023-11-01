0d8b0757830b371bdc2397074730451396580b71
KYBYK <dbqks56231106@gamil.com>
2023-11-01 18:42:38 +0900

    Test : 입력값 미입력 시 예외 처리 테스트 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,12 +2,19 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CarTest {
     @Test
-    void 이름에_대한_예외_처리() {
+    void 이름에_대한_예외_처리_5자초과() {
         assertThatThrownBy(() -> new Car("over5test"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름에_대한_예외_처리_입력값이_비어있을때() {
+        assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
