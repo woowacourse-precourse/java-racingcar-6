e25860a06f7c7c6b1e3c09c2d625cb9c06ec9699
tlarbals824 <simkm0824@naver.com>
2023-11-01 20:09:05 +0900

    refactor : 의존성 명시적으로 주입받을 수 있도록 수정

--- src/main/java/racingcar/input/RacingInfoUserInput.java
+++ src/main/java/racingcar/input/RacingInfoUserInput.java
@@ -1,10 +1,16 @@
 package racingcar.input;
 
+import racingcar.vo.CarCollection;
 import racingcar.vo.RacingInfo;
 
 public class RacingInfoUserInput implements UserInput<RacingInfo>{
-    private final CarCollectionUserInput carCollectionUserInput = new CarCollectionUserInput();
-    private final RepeatCountUserInput repeatCountUserInput = new RepeatCountUserInput();
+    private final UserInput<CarCollection> carCollectionUserInput;
+    private final UserInput<Integer> repeatCountUserInput;
+
+    public RacingInfoUserInput(UserInput<CarCollection> carCollectionUserInput, UserInput<Integer> repeatCountUserInput) {
+        this.carCollectionUserInput = carCollectionUserInput;
+        this.repeatCountUserInput = repeatCountUserInput;
+    }
 
     @Override
     public RacingInfo input() {
