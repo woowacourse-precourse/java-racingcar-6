134d55b84704337d4724d4645c619a619b7081bf
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-01 09:37:13 +0900

    사용자로부터 자동차 이름 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. 사용자로부터 자동차 이름 입력받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = scanner.nextLine().split(",");
+        int numCars = carNames.length;
     }
 }
