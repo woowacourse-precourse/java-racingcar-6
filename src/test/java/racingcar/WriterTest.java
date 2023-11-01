076ee031cb984608b23230bdf1531fd9241e0b3a
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-01 15:28:02 +0900

    test: 출력 기능 테스트 추가
    WriterTest에 출력 기능 테스트 작성
    - 게임 진행 결과 출력 테스트 추가
    - 우승자 출력 테스트 추가

---
+++ src/test/java/racingcar/WriterTest.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+class WriterTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void 게임_진행_결과_출력() {
+        Cars cars = 자동차들_생성();
+        OutputStream out = 출력_스트림_생성();
+        assertRandomNumberInRangeTest(
+                () -> {
+                    cars.attempt();
+                    Writer.cars(cars);
+                    assertThat(out.toString())
+                            .contains("love : -", "hate : -", "you : ");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 우승자_출력() {
+        Cars cars = 자동차들_생성();
+        OutputStream out = 출력_스트림_생성();
+        assertRandomNumberInRangeTest(
+                () -> {
+                    cars.attempt();
+                    Writer.winner(cars);
+                    assertThat(out.toString())
+                            .contains("최종 우승자", "you")
+                            .doesNotContain("love", "hate");
+                },
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    private Cars 자동차들_생성() {
+        return new Cars(new String[]{"love", "hate", "you"});
+    }
+
+    private OutputStream 출력_스트림_생성() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        return out;
+    }
+}
