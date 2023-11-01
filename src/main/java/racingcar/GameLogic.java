ccee12ba66a3e4dac81c52373421558723ff92e7
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 21:42:45 +0900

    feat : 자동차 경주 게임 완료 후 우승자를 알려준다

--- src/main/java/racingcar/GameLogic.java
+++ src/main/java/racingcar/GameLogic.java
@@ -1,12 +1,12 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameLogic {
-    private final Random random = new Random();
+    private List<String> progresses = new ArrayList<>();
     private int maxPosition = 0;
-    private String[] progress;
 
     public void runGame(List<String> carNames, int raceRounds) {
         initializeProgress(carNames.size());
@@ -14,22 +14,51 @@ public class GameLogic {
         for (int i = 0; i < raceRounds; i++) {
             for (int j = 0; j < carNames.size(); j++) {
                 updateProgress(j);
-                System.out.println(carNames.get(j) + " : " + progress[j]);
+                System.out.println(carNames.get(j) + " : " + progresses.get(j));
             }
             System.out.println();
         }
+        findWinner(carNames);
     }
 
     private void initializeProgress(int size) {
-        progress = new String[size];
         for (int i = 0; i < size; i++) {
-            progress[i] = "";
+            progresses.add("");
         }
     }
 
     private void updateProgress(int index) {
-        if (random.nextInt(10) >= 4) {
-            progress[index] += "-";
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            String currentProgress = progresses.get(index);
+            progresses.set(index, currentProgress + "-");
         }
+    }
+
+    private void findWinner(List<String> carNames) {
+        List<String> winners = getWinners(carNames);
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    private List<String> getWinners(List<String> carNames) {
+        List<String> winners = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            int currentProgress = countProgress(progresses.get(i));
+            updateWinners(winners, carNames.get(i), currentProgress);
+        }
+        return winners;
+    }
+
+    private void updateWinners(List<String> winners, String carName, int currentProgress) {
+        if (currentProgress >= maxPosition) {
+            if (currentProgress > maxPosition) {
+                winners.clear();
+                maxPosition = currentProgress;
+            }
+            winners.add(carName);
+        }
+    }
+
+    private int countProgress(String progress) {
+        return (int) progress.chars().filter(ch -> ch == '-').count();
     }
 }
