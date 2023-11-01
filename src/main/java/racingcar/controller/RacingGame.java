0fff38e1d25285c749016b1655df3f307e57cdb2
Aeraekun <lsch3374@naver.com>
2023-11-01 20:07:53 +0900

    feat: RacingGame.java ################
    ################
    
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

---
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -0,0 +1,21 @@
+package racingcar.controller;
+
+import racingcar.model.Race;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+public class RacingGame {
+
+    public void start() {
+        String carNames = InputView.getCarNames();
+        int rounds = InputView.getRounds();
+
+        Race race = new Race(carNames);
+        for (int i = 0; i < rounds; i++) {
+            race.run();
+            ResultView.showRaceResult(race.getCars());
+        }
+
+        ResultView.showWinner(race.getCars());
+    }
+}
