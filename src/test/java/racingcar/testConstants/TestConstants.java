fff5851ab0f56ecc460d1896c68899649ac5ab4c
Isaac Han <isaachan233@gmail.com>
2023-11-01 17:19:36 +0900

    docs: 최종 수정

---
+++ src/test/java/racingcar/testConstants/TestConstants.java
@@ -0,0 +1,8 @@
+package racingcar.testConstants;
+
+import racingcar.enums.Common;
+
+public class TestConstants {
+    private static final int MOVING_FORWARD = Common.STANDARD_NUMBER.getIntValue();
+    private static final int STOP = Common.STANDARD_NUMBER.getIntValue() - 1;
+}
