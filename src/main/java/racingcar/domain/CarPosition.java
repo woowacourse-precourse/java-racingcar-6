703cf2ab25970eb9f202cfdd18bb18368a7e4a5c
PeterChoi <94peterchoi@gmail.com>
2023-11-01 17:33:25 +0900

    feat: 자동차의 현재 위치값 만큼 대쉬 문자열을 생성하는 메소드 작성

--- src/main/java/racingcar/domain/CarPosition.java
+++ src/main/java/racingcar/domain/CarPosition.java
@@ -12,6 +12,15 @@ public class CarPosition {
         position++;
     }
 
+    public String createLine() {
+        StringBuilder dashes = new StringBuilder();
+        for (int i=0; i<position; i++) {
+            dashes.append("-");
+        }
+
+        return dashes.toString();
+    }
+
     public int getPosition() {
         return position;
     }
