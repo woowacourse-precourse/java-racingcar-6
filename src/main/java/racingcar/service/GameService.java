63f777fb18e2bfe6eeac4060a82dc38753b872ce
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-01 16:23:50 +0900

    refactor: 다른 곳에서 사용되지 않는 GameService에 있는 메서드 private으로 변경

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -2,10 +2,10 @@ package racingcar.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import racingcar.domain.gameInfo.InputManager;
 import racingcar.domain.Movable;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
+import racingcar.domain.gameInfo.InputManager;
 import racingcar.view.OutputView;
 
 public class GameService {
@@ -17,23 +17,23 @@ public class GameService {
         OutputView.gameStartMessage();
     }
 
-    public static int inputProcessing(InputManager inputManager){
+    private static int inputProcessing(InputManager inputManager) {
         List<Car> parsedCarList = inputManager.getParsedCarList();
         cars = new Cars(parsedCarList);
         return inputManager.getGameProcessCount();
     }
 
-    public static void progressRound(int round){
+    private static void progressRound(int round) {
         for (int i = 0; i < round; i++) {
             cars.oneRoundProgress(new Movable());
             OutputView.oneTurnResult(cars);
         }
     }
 
-    public static String chooseWinner(){
+    public static String chooseWinner() {
         List<Car> winnerList = cars.getWinners();
         return winnerList.stream()
-                .map(Car:: getName)
+                .map(Car::getName)
                 .collect(Collectors.joining(", "));
     }
 
