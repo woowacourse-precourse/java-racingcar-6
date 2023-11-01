76b1255531191b74eb9e50194ec790ef09cb0cdd
EvelynKimm <evekimmy09@gmail.com>
2023-11-01 15:10:34 +0900

    refactor: change final constant name into capital
    상수의 이름을 대문자로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,16 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     // Car 입력받기
     // Round 입력받기
-    private static final String CarNameQuestion = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String TrialQuestion = "시도할 회수는 몇회인가요?";
+    private static final String CAR_NAME_QUESTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL_QUESTION = "시도할 회수는 몇회인가요?";
 
     public static String requestCarName() {
-        System.out.println(CarNameQuestion);
+        System.out.println(CAR_NAME_QUESTION);
         return Console.readLine();
     }
 
     public static String requestRoundNumber() {
-        System.out.println(TrialQuestion);
+        System.out.println(TRIAL_QUESTION);
         return Console.readLine();
     }
 }
