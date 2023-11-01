5c6d211901291a840f977dd091e6e41b8daaff0b
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 20:51:49 +0900

    feat : initGame 함수 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,9 +4,13 @@ import racingcar.io.InputHandler;
 
 import java.util.Scanner;
 
-public class RacingGame {
+public class RacingGame extends Game{
     private String[] carNames;
     private int tryCount;
 
-
+    @Override
+    public void initGame() {
+        carNames = InputHandler.getCarNames("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)").split(",");
+        tryCount = InputHandler.getTryCount("시도할 회수는 몇회인가요?");
+    }
 }
