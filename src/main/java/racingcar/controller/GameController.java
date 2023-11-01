77ee33e367e6dbc85bcac60f2b0901d13fdc50b0
GyungA <GyungA@users.noreply.github.com>
2023-11-01 22:06:51 +0900

    feat: 자동차 전진 조건 통과 시 전진함

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,11 +1,31 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.CarCreator;
 import racingcar.model.User;
+
+import java.util.List;
+import java.util.Random;
 
 public class GameController {
     private final User user = new User();
+    private List<Car> cars;
 
     public void startNewGame() {
-        user.getUserInputForTryCount();
+        Integer tryCount = user.getUserInputForTryCount();
+        String[] carNames = user.getUserInputForCarNames();
+
+        cars = CarCreator.createCars(carNames);
+    }
+
+    // 0에서 9 사이의 무작위 값 생성. 4 이상이면 이동.
+    public void moveCars() {
+        Random random = new Random();
+        for (Car car : cars) {
+            int randomNumber = random.nextInt(10);
+            if (randomNumber >= 4) {
+                car.move();
+            }
+        }
     }
 }
