0b9f4ec16d02fab30573ac3385dc8cc959d66eea
senzoo-oh <osj3418@gmail.com>
2023-11-01 19:04:17 +0900

    feat: add 자동차 이름 입력 기능
    Application 클래스 내부에 자동차 이름 입력하는 코드 구현
    - 자동차 이름 글자수 확인하는 조건문

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ArrayList<RacingCar> racingCars = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String[] racingCarNames = readLine().split(",");
+        for (String racingCarName : racingCarNames) {
+            if (5 < racingCarName.length()) {
+                throw new IllegalArgumentException();
+            }
+            racingCars.add(new RacingCar(racingCarName));
+        }
+
     }
 }
