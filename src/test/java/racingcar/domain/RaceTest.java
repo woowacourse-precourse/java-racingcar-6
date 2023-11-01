650fd88d31a8bc9869878928c085ac5b2900d896
gundorit <packdev937@gmail.com>
2023-11-01 13:52:42 +0900

    test (Race) : 시도 횟수 전부 소모 시 레이스 종료를 테스트 한다

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Race 클래스")
 class RaceTest {
 
+    private static final int INITIAL_TRY_COUNT = 5;
     private Cars cars;
     private Try tryCount;
     private Race race;
@@ -19,9 +20,8 @@ class RaceTest {
     @BeforeEach
     void setup() {
         cars = new Cars(Arrays.asList("pobi", "crong", "jun"));
-        tryCount = new Try(5);
+        tryCount = new Try(INITIAL_TRY_COUNT);
         race = new Race(cars, tryCount);
-
     }
 
     @Test
@@ -40,7 +40,17 @@ class RaceTest {
     @Test
     void 레이스_후_시도_횟수가_1_차감() {
         race.doRace();
-        assertThat(tryCount.getCount()).isEqualTo(4);
+        assertThat(tryCount.getCount()).isEqualTo(INITIAL_TRY_COUNT - 1);
     }
 
+    @Test
+    void 시도_횟수_전부_소모_시_레이스_종료() {
+        // given
+        for (int i = 0; i < INITIAL_TRY_COUNT; i++) {
+            race.doRace();
+        }
+
+        // then
+        assertThat(race.isOver()).isTrue();
+    }
 }
