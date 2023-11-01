2db037452fff3ac007d1ac2b642f34a265312542
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-01 10:55:34 +0900

    feat : progressMatch 구현
    GameManager.progressMatch 구현

--- src/main/java/manager/GameManager.java
+++ src/main/java/manager/GameManager.java
@@ -2,12 +2,22 @@ package manager;
 
 import custom_object.Car;
 
+import utility.RandomNumberGenerator;
+
 import java.util.List;
 
 public class GameManager {
-    private List<Car> carList;
+    private final List<Car> carList;
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
     public GameManager(List<Car> carList) {
         this.carList = carList;
+    }
+
+    // 디미터의 법칙 위반.
+    public void progressMatch() {
+        for (Car car: carList) {
+            car.tryMoving(randomNumberGenerator.generatingForCarTryMoving());
+        }
     }
 }
