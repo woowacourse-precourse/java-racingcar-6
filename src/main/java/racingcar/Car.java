11478f2bf598bfcd269566563d87982059770a2b
Leespect <wewqwew153@gmail.com>
2023-11-01 16:05:15 +0900

    Feat : 자동차 객체 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+}
