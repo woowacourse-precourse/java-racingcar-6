cea3e96e9c63962ff21340d23a8e19a6793591cd
honeydanji <tjdwls3680@naver.com>
2023-11-01 14:10:10 +0900

    TEST : 자동차_이동거리에 관한 테스트 코드 추가
    - 자동차마다 이동거리 증가가 잘 되는 지 테스트
    - 자동차 이동거리를 반복 마다 출력 되는 지 테스트

--- src/test/java/racingcar/model/CarAndDistanceTest.java
+++ src/test/java/racingcar/model/CarAndDistanceTest.java
@@ -21,4 +21,34 @@ public class CarAndDistanceTest {
         assertThat(carAndDistance.getCarAndDistance(3)).isEqualTo("uncle : ____");
         assertThat(carAndDistance.getCarAndDistance(4)).isEqualTo("king : _____");
     }
+
+    @Test
+    void 자동차_이동거리_반복_테스트() {
+        CarAndDistance carAndDistance = new CarAndDistance();
+        String distance1 = "";
+        String distance2 = "";
+
+        for(int i = 0; i < 5; i++) {
+            distance1 += "_";
+            distance2 += "_";
+            carAndDistance.setCarAndDistance("dokgo", distance1);
+            carAndDistance.setCarAndDistance("may",distance2);
+        }
+
+        assertThat(carAndDistance.getCarAndDistance(0)).isEqualTo("dokgo : _____");
+        assertThat(carAndDistance.getCarAndDistance(1)).isEqualTo("may : _____");
+    }
+
+    @Test
+    void 자동차_이동거리_횟수마다_출력_테스트() {
+        CarAndDistance carAndDistance = new CarAndDistance();
+
+        String distance1 = "";
+
+        for(int i = 0; i < 5; i++) {
+            distance1 += "_";
+            carAndDistance.setCarAndDistance("dokgo", distance1);
+            assertThat(carAndDistance.getCarAndDistance(0)).isEqualTo("dokgo : " + distance1);
+        }
+    }
 }
