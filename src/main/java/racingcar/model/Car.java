98a587a0a21d6edb96c2226fa40414fa81f65262
Jeonwoojae <dnwo0719@icloud.com>
2023-11-01 22:38:39 +0900

    refactor: Car의 현재 위치 표현 OutputView가 하도록 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,23 +1,12 @@
 package racingcar.model;
 
-import static racingcar.view.OutputView.LOCATION_AND_NAME_DELIMITER;
-
 public class Car {
-    public static final String LOCATION = "-";
     private final CarName name;
-    private int location = 0;
+    private int location;
 
     public Car(String name) {
         this.name = new CarName(name);
-    }
-
-    @Override
-    public String toString() {
-        String result = name.name() + LOCATION_AND_NAME_DELIMITER;
-        for (int i = 0; i < location; i++) {
-            result += LOCATION;
-        }
-        return result;
+        this.location = 0;
     }
 
     public String getName() {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,15 +4,24 @@ import java.util.List;
 import racingcar.model.Car;
 
 public final class OutputView {
+    public static final String LOCATION = "-";
     public static final String LOCATION_AND_NAME_DELIMITER = " : ";
     public static final String NAME_DELIMITER = ", ";
     public static final String FINAL_WINNER = "최종 우승자";
 
     public static void displayPerRace(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(car.toString());
+            displayCurrentCar(car);
         }
         System.out.println();
+    }
+
+    private static void displayCurrentCar(Car car) {
+        String result = car.getName() + LOCATION_AND_NAME_DELIMITER;
+        for (int i = 0; i < car.getLocation(); i++) {
+            result += LOCATION;
+        }
+        System.out.println(result);
     }
 
     public static void displayResults(List<Car> cars) {
