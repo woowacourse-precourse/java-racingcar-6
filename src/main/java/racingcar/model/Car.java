1b2ed60fe3316c3a81f45294ffde1263abe88515
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-11-01 19:56:39 +0900

    refactor ($Validation,Car) : 매직넘버 제거하고 상수화 - 1 -> MIN_TRY_COUNT : 시도회수로 가능한 최소 수 (Validation) - 5 -> MAX_CAR_NAME_LENGTH : 차 이름 길이로 가능한 최대 수 (Validation) - 0 -> SET_PLACE : place 변수 0으로 초기화 (Car) - 0 -> MIN_RANDOM_NUM : 랜덤으로 나올 수 있는 최소 수 (Car) - 9 -> MAX_RANDOM_NUM : 랜덤으로 낭로 수 있는 최대 수 (Car)

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,7 +3,11 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private int place = 0;
+    private final static Integer SET_PLACE = 0;
+    private final static Integer MIN_RANDOM_NUM = 0;
+    private final static Integer MAX_RANDOM_NUM = 9;
+    private final static Integer AVAILABLE_FOWARD_NUM = 3;
+    private int place = SET_PLACE;
     private final String name;
 
     public Car(String name) {
@@ -19,12 +23,12 @@ public class Car {
     }
 
     public void forward() {
-        if (generateRandomNumber() > 3) {
+        if (generateRandomNumber() > AVAILABLE_FOWARD_NUM) {
             place++;
         }
     }
 
     private int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
     }
 }
--- src/main/java/racingcar/utils/Validation.java
+++ src/main/java/racingcar/utils/Validation.java
@@ -3,15 +3,17 @@ package racingcar.utils;
 import java.util.List;
 
 public class Validation {
+    private static final Integer MAX_CAR_NAME_LENGTH = 5;
+    public static final Integer MIN_TRY_COUNT = 1;
     public Integer validTryCount(Integer tryCount){
-        if (tryCount < 1){
+        if (tryCount < MIN_TRY_COUNT){
             throw new IllegalArgumentException("시도할 회수는 1이상이어야 합니다.");
         }
         return tryCount;
     }
     public List<String> validateCarNames(List<String> carNames) {
         boolean fiveUnder = carNames.stream()
-                .anyMatch(name ->name.length() > 6);
+                .anyMatch(name ->name.length() > MAX_CAR_NAME_LENGTH);
 
         if (fiveUnder) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
