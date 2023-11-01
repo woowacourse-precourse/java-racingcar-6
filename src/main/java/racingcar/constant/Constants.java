44b79ee820d811a63e09ddb33617fcc7fc1c0dd9
wochae <officialwochae@gmail.com>
2023-11-01 14:56:28 +0900

    feat(constant): constant attribution name length restrict
    자동차 인스턴스를 생성할 때, 이름의 길이가 5이하여야만 한다.

--- src/main/java/racingcar/constant/Constants.java
+++ src/main/java/racingcar/constant/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final int NUMBER_START = 0;
     public static final int NUMBER_END = 9;
 
+    public static final int NAME_MAX_LENGTH = 5;
     public static final String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public static final String INPUT_TIMES = "시도할 회수는 몇회인가요?";
