44dcb79e68e74a514247a2ae39869c7968113d4e
jeonseong-gwon <jeonsg9904@gmail.com>
2023-11-01 14:06:47 +0900

    Refactor : static 메서드 -> 일반 메서드로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
     public static final String CarNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String tryGame = "시도할 회수는 몇회인가요?";
 
-    public static String readCarNames(){
+    public String readCarNames(){
         System.out.println(CarNames);
         return Console.readLine();
     }
 
-    public static int readTryGame(){
+    public int readTryGame(){
         System.out.println(tryGame);
         return Integer.parseInt(Console.readLine());
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,11 +3,11 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-    public static void printInfo(String name, int position) {
+    public void printInfo(String name, int position) {
         System.out.println(name + " : " + "-".repeat(position));
     }
 
-    public static void racingWinners(List<String> winners) {
+    public void racingWinners(List<String> winners) {
         System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
