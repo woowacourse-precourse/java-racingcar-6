7f4224af97916821590ff973f8af4c4b97655afa
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-01 16:50:26 +0900

    Feat : View - OutputView class 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.CarPlayer;
+
 public class OutputView {
+    public void outputRaceProgress(List<CarPlayer> players){
+        for(CarPlayer player : players) {
+            StringBuilder score = new StringBuilder();
+
+            score.append(player.getName()).append(" : ");
+            score.append("-".repeat(Math.max(0, player.getScore())));
+
+            System.out.println(score);
+        }
+        System.out.println();
+    }
+
+    public void outputWinners(String winner){
+        System.out.println("최종 우승자 : "+winner);
+    }
 }
