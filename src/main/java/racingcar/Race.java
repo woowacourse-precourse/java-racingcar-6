aa9ccaed0b82959fe03217da5e28c3f2c782f7d5
동원 <ai06021@gmail.com>
2023-11-01 15:21:49 +0900

    fix : 잘못복사됨

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.view.Cars;
+import racingcar.view.Try;
+
+public class Race {
+    List<Cars> cars = Cars.make();
+    int timeToTry = Try.input();
+
+}
