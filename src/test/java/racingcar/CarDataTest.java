e6e872e1d21a0bfbb0ba213a1787855be14f67d2
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-01 23:32:17 +0900

    feat : 자동차 이름 생성에 대한 테스트 코드 추가

--- src/test/java/racingcar/CarDataTest.java
+++ src/test/java/racingcar/CarDataTest.java
@@ -2,7 +2,10 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.Car;
 import racingcar.model.CarData;
 
@@ -20,6 +23,14 @@ public class CarDataTest {
         Car car = new Car("test", 0);
         CarData carData = car.getData();
         assertEquals(0, carData.getMoveDistance());
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"lsqssw", "lswqwiiii", "lsasd!!erw", "xxxxxxxxxxxxxxxx"})
+    void 이름이_5글자를_넘는_경우_예외처리(String name) {
+        Assertions.assertThatThrownBy(() -> new Car(name, 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
