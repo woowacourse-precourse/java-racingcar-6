f13543c5e335bad9f36e58b3ed2d7bd4547ffb9c
SNGYNGJIN <jsy9508@gmail.com>
2023-11-01 22:25:03 +0900

    feat: CarList에 우승 자동차 반환 메소드 추가

--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -14,4 +14,19 @@ public class CarList {
         return carList;
     }
 
+    public List<Car> findWinner(List<Car> carList) {
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = carList.get(0).getPosition();
+
+        for (Car car : carList) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+                winners.clear();
+                winners.add(car);
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 }
