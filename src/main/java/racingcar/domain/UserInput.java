be37b58c9cebfb3fdf92082d3888a02e8c67f385
dj <fjzl12@naver.com>
2023-11-01 19:30:32 +0900

    feat(UserInput): 생성자 구현

--- src/main/java/racingcar/domain/UserInput.java
+++ src/main/java/racingcar/domain/UserInput.java
@@ -5,7 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class UserInput {
-    public static List<String> setCarNames() {
+    public UserInput() {
+    }
+
+    public List<String> setCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> nameList;
         try {
