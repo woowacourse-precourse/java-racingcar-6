4fc4dbf86e871a0f2c5a880d7890b82f7ef50783
nayonsoso <panda0329@naver.com>
2023-11-01 17:48:25 +0900

    refactor: RaceDto의 이동 정보 필드를 int에서 String으로 변경

--- src/main/java/model/RaceDto.java
+++ src/main/java/model/RaceDto.java
@@ -3,15 +3,15 @@ package model;
 import java.util.HashMap;
 
 public class RaceDto {
-    private HashMap<String, Integer> cars;
+    private HashMap<String, String> cars;
     private int attemptTimes;
 
-    RaceDto(HashMap<String, Integer> cars, int attemptTimes){
+    RaceDto(HashMap<String, String> cars, int attemptTimes){
         this.cars = cars;
         this.attemptTimes = attemptTimes;
     }
 
-    public HashMap<String, Integer> getCars(){
+    public HashMap<String, String> getCars(){
         return cars;
     }
 
