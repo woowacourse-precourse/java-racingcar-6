4cdf1498aeedbf431989eea6100917cf371beb2f
min050410 <min050410@gmail.com>
2023-11-01 23:19:01 +0900

    feature(Game): playSingleRound 메서드를 작성한다.

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,16 +1,42 @@
 package racingcar.service;
 
+import static racingcar.domain.Game.DISTANCE_TO_MOVE;
+
 import java.util.List;
 import racingcar.domain.Game;
+import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
+import racingcar.util.RandomNumberGeneratorUtil;
 
 public class GameService {
+
+    private static final int MIN_RANDOM_VALUE = 0;
+    private static final int MAX_RANDOM_VALUE = 9;
+    private static final int FORWARD_TRIGGER = 4;
 
     private final RacingCarService racingCarService = new RacingCarService();
 
     public Game createGame(List<String> racingCarNames, int totalAttempts) {
         RacingCars racingCars = racingCarService.createRacingCars(racingCarNames);
         return new Game(racingCars, totalAttempts);
+    }
+
+    public void playSingleRound(Game game) {
+        game.increaseAttempts();
+        moveCars(game.getRacingCarList());
+    }
+
+    private void moveCars(List<RacingCar> racingCarList) {
+        for (RacingCar racingCar : racingCarList) {
+            if (checkIsForwarding()) {
+                racingCar.updatePosition(DISTANCE_TO_MOVE);
+            }
+        }
+    }
+
+    private boolean checkIsForwarding() {
+        int randomNumber = RandomNumberGeneratorUtil.generate(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+        return randomNumber >= FORWARD_TRIGGER;
     }
 
 }
