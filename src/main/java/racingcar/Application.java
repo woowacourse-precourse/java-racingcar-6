28647e6a02bf77b8239dd4125f46314d063e468c
jjongwon7 <jjongwon7@naver.com>
2023-11-01 22:19:47 +0900

    feat: 경주할 자동차 입력 기능 추가 경주할 자동차 이름을 입력받는다. 이때, 자동차 이름은 쉼표(,)를 기준으로 구분한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String[] inputRacingCarNames = inputRacingCarNames();
     }
+
+    private static String[] inputRacingCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputRacingCarName = readLine();
+
+        return inputRacingCarName.split(",");
+    }
+
 }
