74373fae1fad65e7269cf228dfc4505ca5a91abc
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-11-01 20:58:01 +0900

    refactor: 랜덤숫자 생성 메소드 min, max 상수로 관리

--- src/main/java/racingcar/common/RandomNumberGenerator.java
+++ src/main/java/racingcar/common/RandomNumberGenerator.java
@@ -3,7 +3,10 @@ package racingcar.common;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+
     public static int generateRandomNumber(){
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(RANDOM_MIN,RANDOM_MAX);
     }
 }
