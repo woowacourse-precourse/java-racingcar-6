5e126b9b802ce1e92b0e8e29675ba20b19b4a62b
jooda00 <dragon2000a@gmail.com>
2023-11-01 22:59:21 +0900

    test(RacingGameValidatorTest): 모든 자동차가 전진하지 않은 경우 예외 테스트

---
+++ src/test/java/racingcar/validator/RacingGameValidatorTest.java
@@ -0,0 +1,28 @@
+package racingcar.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RacingGameValidatorTest {
+    private RacingGameValidator racingGameValidator;
+
+    @BeforeEach
+    void setUp() {
+        racingGameValidator = new RacingGameValidator();
+    }
+
+    @Test
+    void 모든_자동차가_아무도_전진하지_않음() {
+        List<Integer> resultList = new ArrayList<>(Arrays.asList(0, 0, 0));
+
+        assertThatThrownBy(() -> racingGameValidator.validateGameTerminated(resultList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이동한 자동차가 하나도 없어서 게임이 정상적으로 마무리되지 않았습니다.");
+    }
+}
