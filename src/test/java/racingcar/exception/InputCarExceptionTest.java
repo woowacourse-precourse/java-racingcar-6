1a21d7afa3a7ae8916c2e1214ceaf8dca60db90c
ghsyn <swanshawn715@gmail.com>
2023-11-01 20:28:27 +0900

    test: Implement car name remove whitespace

--- src/test/java/racingcar/exception/InputCarExceptionTest.java
+++ src/test/java/racingcar/exception/InputCarExceptionTest.java
@@ -4,10 +4,14 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputCarExceptionTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     void 자동차_이름_5자_이하() {
@@ -32,6 +36,21 @@ class InputCarExceptionTest extends NsTest {
 
     @Test
     void 자동차_이름_공백_제거() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi, woni, jun", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "jun : -", "최종 우승자 : pobi, jun");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,Kim SY", "1");
+                    assertThat(output()).contains("pobi : ", "KimSY : -", "최종 우승자 : KimSY");
+                },
+                STOP, MOVING_FORWARD
+        );
     }
 
     @Override
