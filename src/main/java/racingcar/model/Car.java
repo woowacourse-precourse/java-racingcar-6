c353f2bc610acab62919326eb5f200f0207c6f20
SNGYNGJIN <jsy9508@gmail.com>
2023-11-01 22:11:16 +0900

    feat: Car 클래스 drive 추가, 횟수 입력과 입력 예외 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,11 +2,17 @@ package racingcar.model;
 
 public class Car {
     private final String name;
-    private final int position;
+    private int position;
 
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void drive(boolean isDrive) {
+        if (isDrive) {
+            position++;
+        }
     }
 
     public String getName() {
