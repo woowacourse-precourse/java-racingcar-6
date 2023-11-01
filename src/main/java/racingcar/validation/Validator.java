95031854ec2a05735f30b70e2a5f494df14b26b8
seona-moon <fhflwhwl5@gmail.com>
2023-11-01 14:59:45 +0900

    feat: count가 양수가 아닌 경우 예외처리 및 가독성을 위해 메서드 이름 변경

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -7,27 +7,36 @@ public class Validator {
 
   public static final int MAX_LENGTH = 5;
 
-  public void validateNullValue(Object value) {
-    if (value.equals("") || value == null) {
+  public void isContainNull(String value) {
+    if (value.isEmpty()) {
       throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
     }
   }
 
-  public void validateNameLength(List<String> nameList) {
-    for (int i = 0; i < nameList.size(); i++) {
-      if (nameList.get(i).length() > MAX_LENGTH) {
-        throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH.getMessage());
-      }
-    }
-  }
-
-  public void validateNum(String count) {
+  public void isContainLetter(String count) {
     if (!count.matches("^[0-9]+$")) {
       throw new IllegalArgumentException(ErrorMessage.CONTAIN_LETTER.getMessage());
     }
   }
 
-  public void validateDuplicatedName(List<String> nameList) {
+  public void isNotPositiveNum(String count) {
+    if (Integer.parseInt(count) <= 0) {
+      throw new IllegalArgumentException(ErrorMessage..getMessage());
+    }
+  }
+
+  //TODO 유리수 검정
+
+
+  public void isMaxLengthExceeded(List<String> nameList) {
+    for (int i = 0; i < nameList.size(); i++) {
+      if ((nameList.get(i).length() > MAX_LENGTH)) {
+        throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH.getMessage());
+      }
+    }
+  }
+
+  public void isDuplicatedName(List<String> nameList) {
     for (int i = 0; i < nameList.size(); i++) {
       int count = Collections.frequency(nameList, nameList.get(i));
       if (count > 1) {
