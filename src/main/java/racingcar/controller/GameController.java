9bfe86c2a6d89bb4d9ba901ec2e0460932d92b3a
wook <dong930112@gmail.com>
2023-11-01 16:22:38 +0900

    refactor: Model로직과 View로직 변경에 따른 게임시작 기능 내부로직 리팩터링

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,40 +1,32 @@
 package racingcar.controller;
 
-import java.util.HashMap;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.model.CarRaceModel;
-import racingcar.model.CarTextModel;
 import racingcar.model.Validator;
 import racingcar.view.GamePlayView;
 
 public class GameController {
 
     CarRaceModel carRaceModel = new CarRaceModel();
-    CarTextModel carTextModel = new CarTextModel();
     GamePlayView gamePlayView = new GamePlayView();
     Validator validator = new Validator();
 
     public void gameStart() {
 
         String carNames = gamePlayView.inputCarName();
-        HashMap<String, String> carList = carTextModel.parseCarNames(carNames);
-        validator.validateCarName(carList);
+        List<String> carNameList = carRaceModel.generateCarName(carNames);
+        validator.validateCarName(carNameList);
+
+        List<Car> carList = carRaceModel.generateCar(carNameList);
 
         int attempts = gamePlayView.inputAttemptsCount();
         validator.validateAttemptInput(attempts);
 
-        System.out.println();
-        System.out.println("실행 결과");
-
-        for(int i=0; i<attempts; i++) {
-            carList = carRaceModel.saveCarMovement(carList);
-            gamePlayView.showTurnResult(carList);
-            System.out.println();
-        }
+        gamePlayView.showGameResult(carList, attempts);
 
         List<String> winnerList = carRaceModel.findWinner(carList);
 
         gamePlayView.showGameWinner(winnerList);
     }
-
 }
