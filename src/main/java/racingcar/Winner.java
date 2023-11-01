488b07750954587ec81762cbd8c218d87a0c2a5e
minwoo <mark42son@gmail.com>
2023-11-01 19:18:46 +0900

    refactor : Moving클래스 삭제 후 적절한 클래스로 편입

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -1,13 +1,16 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Winner {
 
     private static final String EXCEPTION_MESSAGE = "최종 우승자: ";
 
 
-    private void printSingleRacingresult(){
-        // 매 회차 실행 결과를 이름과 거리로 출력해주는 메서드
-    }
+
     private void calculateFinalWinner(){
         // 각 자동차의 최대 거리를 비교해서 최대 우승자를 선정하는 메서드
     }
