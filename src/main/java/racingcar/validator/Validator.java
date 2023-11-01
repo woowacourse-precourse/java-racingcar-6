31b64d257913cf4861bb8704e24f2061b0cb5bd9
KimSangHyeok <tkdgur0906@naver.com>
2023-11-01 18:47:44 +0900

    feat : 이름에 공백이 있는지 검증

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -9,4 +9,8 @@ public class Validator {
     public static boolean isFiveOrLessCharacter(List<String> names){
         return names.stream().allMatch(name -> name.length() <= 5);
     }
+
+    public static boolean isHasEmpty(List<String> names){
+        return names.stream().allMatch(name -> !name.contains(" "));
+    }
 }
