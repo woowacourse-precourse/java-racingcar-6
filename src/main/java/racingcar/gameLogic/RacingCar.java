4a13dc4edd7b1ffb24843767dd45b1b0077a5995
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-01 17:56:56 +0900

    sytle: 접근 제어자 수정
    테스트 코드상으로는 이상없으나
    RacingCar클래스의 roundCount변수를 private로 선언서
    우테코 플랫폼상에서 예제가 실패하는 문제가 보여 다시 실험하기위해 수정함

--- src/main/java/racingcar/gameLogic/RacingCar.java
+++ src/main/java/racingcar/gameLogic/RacingCar.java
@@ -9,7 +9,7 @@ public class RacingCar {
     private RaceOfficial raceOfficial;
     private User user;
     private List<Driver> drivers;
-    int roundCount;
+    private int roundCount;
 
     public void init() {
         raceOfficial = new RaceOfficial();
