b530a5717949c8e6e0d304473f7a7dad9cdf86b4
jun23314 <dlwnsfml@naver.com>
2023-11-01 15:55:27 +0900

    게임매니저 구조 수정 - startGame 함수 분리

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,18 +1,25 @@
 package racingcar;
 
 public class GameManager {
+    UserInput userInput;
+    Racing racing;
+    PrintResult printResult;
     GameManager(){
-        UserInput userInput = new UserInput();
+        userInput = new UserInput();
+        racing = new Racing();
+        printResult = new PrintResult();
+    }
+
+    public void startGame(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = userInput.inputCarName();
         System.out.println("시도할 회수는 몇회인가요?");
         int count = userInput.inputCount();
         int[] location = new int[carNames.length];
 
-        Racing racing = new Racing();
-        PrintResult printResult = new PrintResult();
-        for(int i = 0; i < carNames.length; i++){
-            location = racing.startOneRound(location);
+        System.out.println("실행 결과");
+        for(int i = 0; i < count; i++){
+            racing.playOneRound(location);
             printResult.printLocation(location, carNames);
         }
         printResult.printWinner(location, carNames);
