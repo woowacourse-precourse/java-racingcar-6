b6d19680f6587a8f50bb3c9f1403924776fa6bcb
seastamp <seastamp10@gmail.com>
2023-11-01 19:56:21 +0900

    refactor: 게임 결과 처리 메서드 클래스로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import racingcar.controller.GameManager;
+import racingcar.util.NumberGenerator;
+import racingcar.util.RandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        GameManager racingcarGame = new GameManager();
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        GameManager racingcarGame = new GameManager(numberGenerator);
         racingcarGame.runGame();
     }
 }
--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -4,29 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.util.NumberGenerator;
-import racingcar.util.RandomNumberGenerator;
 import racingcar.view.GameUI;
 import racingcar.view.RaceView;
 
 public class GameManager {
     private int remainingRounds;
     private List<Car> cars;
-    private NumberGenerator numberGenerator;
+    private final NumberGenerator numberGenerator;
 
-    public GameManager() {
+    public GameManager(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
     }
 
     public void runGame() {
         cars = new ArrayList<>();
-        numberGenerator = new RandomNumberGenerator();
         setupGame();
         playGame();
         displayGameResult();
     }
 
     private void setupGame() {
-        List<String> carsName = GameUI.promptForCarNames();
-        for (String name : carsName) {
+        List<String> carsNames = GameUI.promptForCarNames();
+        for (String name : carsNames) {
             cars.add(new Car(name));
         }
         remainingRounds = GameUI.promptForNumberOfRounds();
@@ -40,22 +39,7 @@ public class GameManager {
     }
 
     private void displayGameResult() {
-        List<Car> winners = determineWinners();
+        List<Car> winners = GameResultAnalyzer.determineWinners(cars);
         RaceView.displayWinner(winners);
-    }
-
-    private List<Car> determineWinners() {
-        int maxDistance = this.cars.stream()
-                .mapToInt(Car::getForwardDistance)
-                .max()
-                .orElse(0);
-
-        List<Car> winners = new ArrayList<>();
-        for (Car car : this.cars) {
-            if (car.getForwardDistance() == maxDistance) {
-                winners.add(car);
-            }
-        }
-        return winners;
     }
 }
