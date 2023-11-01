c4550ce95ed7dd9a2f00304ab134d07112e2dc8a
jihye134seo <jihye.seo134@gmail.com>
2023-11-01 14:15:08 +0900

    test(InputException.java): 경주 시도 회수 입력 예외처리 테스트 구현 - 숫자가 아닌 값이 입력될 경우 예외 처리 테스트 - 음수가 입력될 경우 예외 처리 테스트 - Integer 범위 이상의 값이 입력될 경우 예외 처리 테스트 - 입력이 없을 경우 예외 처리 테스트

--- src/test/java/racingcar/Exception/InputException.java
+++ src/test/java/racingcar/Exception/InputException.java
@@ -52,5 +52,47 @@ public class InputException {
 
     }
 
+
+    @Test
+    void 시도_회수_예외1_숫자가_아닌_값이_입력될_경우_예외발생() {
+
+        String input = "not";
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.checkTryCountValidation(input))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    void 시도_회수_예외2_음수가_입력될_경우_예외발생() {
+        String input = "-1";
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.checkTryCountValidation(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도_회수_예외3_Integer_범위_이상의_값이_입력될_경우_예외발생() {
+        String input = "2147483648";
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.checkTryCountValidation(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도_회수_예외4_입력이_비어있는_경우_예외발생() {
+
+        String input = "";
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.checkTryCountValidation(input))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+
 }
 
