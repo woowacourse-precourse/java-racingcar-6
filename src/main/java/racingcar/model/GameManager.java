bcfd90fe3b809744147efa19a4fe3cff9b71f9dc
nhn1030 <shgusska12@gmail.com>
2023-11-01 18:04:04 +0900

    feat: 게임의 흐름을 제어하기 위한 GameManager 클래스 정의 - 게임 참여 자동차를 저장하기 위한 리스트(cars)와 자동차 제어를 위한 controlManager - 객체 생성제한을 위해 팩토리 메서드로 GameManager 객체 생성

--- src/main/java/racingcar/model/GameManager.java
+++ src/main/java/racingcar/model/GameManager.java
@@ -16,33 +16,4 @@ public class GameManager {
         return new GameManager();
     }
 
-    public void addCar(String name) {
-        Car car = new Car(name);
-        car.setCarName(name);
-        cars.add(car);
-    }
-
-    public void moveAllCars() {
-        for (Car car : cars) {
-            controlManager.moveFowardCar(car);
-        }
-    }
-
-    public List<String> getCarNames() {
-        List<String> carNames = new ArrayList<>();
-        for (Car car : cars) {
-            carNames.add(car.getCarName());
-        }
-        return carNames;
-    }
-
-    public List<Integer> getCarPositions() {
-        List<Integer> carPositions = new ArrayList<>();
-        for (Car car : cars) {
-            carPositions.add(car.getCarCurrentPosition());
-        }
-        return carPositions;
-    }
-
-
 }
