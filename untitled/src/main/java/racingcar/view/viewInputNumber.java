d658ae2e79532f538ba1d3882019b82f4a1f4b49
seongnam <jseongnam1109@gmail.com>
2023-11-01 17:58:15 +0900

    viewInputNumber

--- untitled/src/main/java/racingcar/view/viewInputNumber.java
+++ untitled/src/main/java/racingcar/view/viewInputNumber.java
@@ -1,4 +1,17 @@
 package racingcar.view;
-
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+import racingcar.constant.ErrorMessage;
 public class viewInputNumber {
+    private final String NUMERIC_PATTERN = "^[0-9]*$";
+
+    public int viewInputNumber(){
+        String movementTimeStr = Console.readline();
+        if(!Pattern.matches(NUMERIC_PATTERN, movementTimeStr)){
+            throw new IllegalArgumentException(ErrorMessage.MOVEMENT_TIME_TYPE_EXCEPTION.getMessage());
+        }
+        int movementTimeInt = Integer.parseInt(movementTimeStr);
+
+        return movementTimeInt;
+    }
 }
