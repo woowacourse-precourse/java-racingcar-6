8a9c567a78bb1542930723b65d6dfbc02f2f54b4
Chaeros <chpark6851@koreatech.ac.kr>
2023-11-01 23:50:40 +0900

    refactor: service test 수정

--- src/test/java/racingcar/service/RacingCarGameServiceTest.java
+++ src/test/java/racingcar/service/RacingCarGameServiceTest.java
@@ -13,76 +13,76 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class RacingCarGameServiceTest {
-//
-//    RacingCarGameService racingCarGameService = new RacingCarGameService();
-//
-//    @DisplayName("4이상수_전진")
-//    @Test
-//    void isMoveFoward(){
-//        //given
-//        Car car = new Car("pobi",0);
-//        List<Car> inputCars = Arrays.asList(car);
-//        Cars cars = new Cars(inputCars);
-//
-//        assertRandomNumberInRangeTest(
-//                () -> {
-//                    //when
-//                    racingCarGameService.fowardCars(cars);
-//
-//                    //then
-//                    assertThat(cars.getCars().get(0).getStep()).isEqualTo(1);
-//                },
-//                4
-//        );
-//    }
-//
-//    @DisplayName("4이하수_미전진")
-//    @Test
-//    void isNotFoward(){
-//        //given
-//        Car car = new Car("pobi",0);
-//        List<Car> inputCars = Arrays.asList(car);
-//        Cars cars = new Cars(inputCars);
-//
-//        assertRandomNumberInRangeTest(
-//                () -> {
-//                    //when
-//                    racingCarGameService.fowardCars(cars);
-//
-//                    //then
-//                    assertThat(cars.getCars().get(0).getStep()).isEqualTo(0);
-//                },
-//                3
-//        );
-//    }
-//
-//    @DisplayName("최대_전진거리_정상반환")
-//    @Test
-//    void isCorrectMaxStep(){
-//        //given
-//        Car car1 = new Car("pobi",2);
-//        Car car2 = new Car("woni",3);
-//        Car car3 = new Car("jun",4);
-//        List<Car> inputCars = Arrays.asList(car1, car2, car3);
-//        Cars cars = new Cars(inputCars);
-//
-//        //when
-//        int maxStepValue = racingCarGameService.getMaxStep(cars);
-//
-//        //then
-//        Assertions.assertThat(maxStepValue).isEqualTo(4);
-//    }
-//
-//    @DisplayName("정상범위_랜덤수출력")
-//    @Test
-//    void isCorrectRangeRandomNumber(){
-//
-//        for(int i=0;i<1000;++i){
-//            //when
-//            int value = racingCarGameService.generateRandomNumber();
-//
-//            //then
-//            if(value<0 || value>9) fail();
-//        }
-//    }
+
+    RacingCarGameService racingCarGameService = new RacingCarGameService();
+
+    @DisplayName("4이상수_전진")
+    @Test
+    void isMoveFoward(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    //given
+                    Car car = new Car("pobi",0);
+                    List<Car> inputCars = Arrays.asList(car);
+                    Cars cars = new Cars(inputCars);
+
+                    //when
+                    racingCarGameService.fowardCars(cars);
+
+                    //then
+                    assertThat(cars.getCars().get(0).getStep()).isEqualTo(1);
+                },
+                4
+        );
+    }
+
+    @DisplayName("4이하수_미전진")
+    @Test
+    void isNotFoward(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    //given
+                    Car car = new Car("pobi",0);
+                    List<Car> inputCars = Arrays.asList(car);
+                    Cars cars = new Cars(inputCars);
+
+                    //when
+                    racingCarGameService.fowardCars(cars);
+
+                    //then
+                    assertThat(cars.getCars().get(0).getStep()).isEqualTo(0);
+                },
+                3
+        );
+    }
+
+    @DisplayName("최대_전진거리_정상반환")
+    @Test
+    void isCorrectMaxStep(){
+        //given
+        Car car1 = new Car("pobi",2);
+        Car car2 = new Car("woni",3);
+        Car car3 = new Car("jun",4);
+        List<Car> inputCars = Arrays.asList(car1, car2, car3);
+        Cars cars = new Cars(inputCars);
+
+        //when
+        int maxStepValue = racingCarGameService.getMaxStep(cars);
+
+        //then
+        Assertions.assertThat(maxStepValue).isEqualTo(4);
+    }
+
+    @DisplayName("정상범위_랜덤수출력")
+    @Test
+    void isCorrectRangeRandomNumber(){
+
+        for(int i=0;i<1000;++i){
+            //when
+            int value = racingCarGameService.generateRandomNumber();
+
+            //then
+            if(value<0 || value>9) fail();
+        }
+    }
 }
