c0f7620f3e6ccc78cce34ede0bc8075d4cf14e83
Hugh <lee2317884@gmail.com>
2023-11-01 23:36:13 +0900

    test: 게임 라운드 테스트 케이스 추가

--- src/test/java/racingcar/domain/round/GameRoundTest.java
+++ src/test/java/racingcar/domain/round/GameRoundTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain.round;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,9 @@ import racingcar.constant.errorMessage.ErrorMessages;
 import racingcar.constant.errorMessage.InvalidArgumentException;
 
 class GameRoundTest {
+
+    private static final int DECREASING_COUNT_NUMBER = 1;
+    private static final int GAME_FINISH_NUMBER = 0;
 
     @ParameterizedTest
     @ValueSource(ints = {0, -2, -3, -2000000})
@@ -26,5 +30,34 @@ class GameRoundTest {
     @DisplayName("게임 실행 횟수 객체 생성 테스트")
     void gameRoundObjectCreationTest(final int count) {
         assertDoesNotThrow(() -> new GameRound(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, 2, 3, 4, 100})
+    @DisplayName("게임 실행 횟수 1씩 감소하는지 테스트")
+    void decreaseCountTest(final int count) {
+        final GameRound gameRound = new GameRound(count);
+        gameRound.isCountDecrease();
+
+        final int actual = gameRound.getCount();
+        final int expected = count - DECREASING_COUNT_NUMBER;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, 2, 3, 4, 100})
+    @DisplayName("게임 실행 횟수 0까지 감소하는지 테스트")
+    void isNotFinishedTest(final int count) {
+        final GameRound gameRound = new GameRound(count);
+        checkCountIsDecrease(gameRound, count);
+
+        final int actual = gameRound.getCount();
+        assertThat(actual).isEqualTo(GAME_FINISH_NUMBER);
+    }
+
+    private void checkCountIsDecrease(final GameRound gameRound, final int count) {
+        for (int i = 0; i < count; i++) {
+            gameRound.isCountDecrease();
+        }
     }
 }
