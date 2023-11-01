f4815c61c52eeceee45e5bb13e4a44985b8a6c7f
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-01 23:01:07 +0900

    feat(Application): add 2 method to print
    add 'PrintGuideSentence' method and 'AskTrialCount' method
    - need to explain what should user write to user
    - first method print sentence about what user should do first
    - second method print sentence about How many times user is going to try

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
+    public static void printGuideSentence(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static void askTrialCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //  안내문 출력
+        printGuideSentence();
+        //  시도 횟수 질문 출력
+        askTrialCount();
     }
 }
