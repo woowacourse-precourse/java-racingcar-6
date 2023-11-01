42a0d5af33d7ec9e31f8e3bb9180692c0428aa0b
yujeonghyeon <dbdb1114@naver.com>
2023-11-01 21:35:58 +0900

    자동차 위치 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,26 @@
 package view;
 
 import java.util.List;
+import model.Car;
 
 public class OutputView {
+    public static void printProceeding(List<Car> carList) {
+        for (Car car : carList) {
+            printCarLocation(car);
+        }
+        System.out.println();
+    }
+
+    public static void printCarLocation(Car car) {
+        String name = (String) car.getCarLocation().get("name");
+        int location = (Integer) car.getCarLocation().get("location");
+
+        System.out.print(name + " : ");
+        for (int i = 0; i < location; i++) {
+            System.out.print("-");
+        }
+    }
+
     public static void printResult(List<String> winnerList) {
         String winners = String.join(", ", winnerList);
         System.out.printf("최종 우승자 : %s", winners);
