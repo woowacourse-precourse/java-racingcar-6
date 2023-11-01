0dfce0fe84005f6eba28888de341e4e5ba04ce30
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-01 21:02:40 +0900

    test($InputProcessor): 공백 유효성 테스트
    입력값에 공백을 받지 않는지에 대한 테스트 구현
    매번 Console.readLine() 호출시 Scanner를 닫아주도록 설정

--- src/test/java/study/InputProcessorTest.java
+++ src/test/java/study/InputProcessorTest.java
@@ -2,7 +2,10 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.InputProcessor;
@@ -10,10 +13,24 @@ import racingcar.domain.InputProcessor;
 @DisplayName("입력 프로세서 테스트")
 public class InputProcessorTest {
 
+    @AfterEach
+    void reset() {
+        Console.close();
+    }
+
     @DisplayName("성공: 입력값에 쉼표가 없는 경우")
     @Test
     void test1() {
         System.setIn(new ByteArrayInputStream("pobigokim".getBytes()));
+        InputProcessor inputProcessor = new InputProcessor();
+        assertThatCode(()->inputProcessor.getCarNames())
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("실패: 자동차 이름에 공백을 입력할 수 없다.")
+    @Test
+    void test2() {
+        System.setIn(new ByteArrayInputStream("".getBytes()));
         InputProcessor inputProcessor = new InputProcessor();
         assertThatCode(()->inputProcessor.getCarNames())
                 .doesNotThrowAnyException();
