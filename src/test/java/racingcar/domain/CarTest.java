b952a446a0982afce223fcd77114f3e50c15d61d
nahyeon99 <nahyeonee99@gmail.com>
2023-11-01 21:17:59 +0900

    refactor(domain/NameTest): Car 테스트 코드에서 Name 테스트 코드 제거
    1. Name 테스트 코드는 NameTest에서 진행하도록 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,25 +2,16 @@ package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
-    @Test
-    @DisplayName("자동차 이름은 영어 소문자로만 구성되고, 1~5자의 길이여야한다.")
-    void carNameFormatTest() {
-
-        String tooShortName = "";
-        Assertions.assertThatThrownBy(() -> new Name(tooShortName))
-                .isInstanceOf(IllegalArgumentException.class);
-
-
-        String tooLongName = "tooLong";
-        Assertions.assertThatThrownBy(() -> new Name(tooLongName))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Name nameContainedNumber = new Name("poby1");
-        Assertions.assertThatThrownBy(() -> new Car(nameContainedNumber))
+    @DisplayName("자동차 이름이 영어 소문자로만 구성되지 않으면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"poby1", "123", "포비"})
+    void carNameFormatTest(String failName) {
+        Assertions.assertThatThrownBy(() -> new Car(new Name(failName)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
