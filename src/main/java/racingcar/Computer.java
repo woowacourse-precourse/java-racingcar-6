873d996f217821010712ef21f21e906c194b7281
ziy00n <jiyun4188@gmail.com>
2023-11-01 20:59:30 +0900

    refactor : computer클래스, Race클래스 수정

--- src/main/java/racingcar/Computer.java
+++ src/main/java/racingcar/Computer.java
@@ -2,18 +2,36 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     public static final String PROMPT_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)";
     public static final String ATTEMPT_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
 
+    Validator validator = new Validator();
 
     public String getInputCarNames() {
         System.out.println(PROMPT_FOR_CAR_NAMES);
-        return Console.readLine();
+        String cars = Console.readLine();
+        validator.isValidCarNames(cars);
+        validator.isValidLength(cars);
+        return cars;
     }
 
     public int getInputRaceCount() {
         System.out.println(ATTEMPT_COUNT_PROMPT);
-        return Integer.parseInt(Console.readLine());
+        String raceCount = Console.readLine();
+        validator.isValidNumber(raceCount);
+        return Integer.parseInt(raceCount);
+    }
+
+    public List<Car> createCars(String carNames) {
+        String[] names = carNames.split(",");
+        List<Car> carsList = new ArrayList<>();
+        for (String name : names) {
+            carsList.add(new Car(name));
+        }
+        return carsList;
     }
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class Race {
@@ -9,8 +7,24 @@ public class Race {
     private final List<Car> cars;
     private final int count;
 
-    public Race(List<Car> cars, int count) {
-        this.cars = cars;
+    Computer computer = new Computer();
+
+    public Race() {
+        this.cars = null;
+        this.count = 0;
+    }
+
+    public Race(String carNames, int count) {
+        this.cars = computer.createCars(carNames);
         this.count = count;
     }
+
+    public Race start() {
+        String carNames = computer.getInputCarNames();
+        int cnt = computer.getInputRaceCount();
+
+        return new Race(carNames, cnt);
+    }
+
+
 }
