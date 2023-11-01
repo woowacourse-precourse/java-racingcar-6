12c54eda19adab862e4b9599e38215b6a66595e6
strongcookdas <goodjav141592@gmail.com>
2023-11-01 15:01:53 +0900

    feat : 최종 우승자들을 출력하는 printWinners 메서드 구현

--- src/main/java/racingcar/view/OutputManager.java
+++ src/main/java/racingcar/view/OutputManager.java
@@ -1,11 +1,24 @@
 package racingcar.view;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class OutputManager {
     public void printCarStatus(Map<String, Integer> carStatus) {
         carStatus.forEach((key, value) -> {
             System.out.println(String.format("%s : %s", key, "-".repeat(value)));
+        });
+    }
+
+    public void printWinners(List<String> winners){
+        System.out.print("최종 우승자 : ");
+        IntStream.range(0, winners.size()).forEach(idx -> {
+            if(idx == 0){
+                System.out.print(winners.get(idx));
+            }else {
+                System.out.print(", "+winners.get(idx));
+            }
         });
     }
 }
