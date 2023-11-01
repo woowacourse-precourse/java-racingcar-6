a5554b733937fa0e2c858f294363a5c1164b6d04
gemstoneyang <bennyws99@gmail.com>
2023-11-01 20:46:51 +0900

    feat: Car 클래스 구현 - 자동차 한대를 담당하는 Car 클래스 정의 - 확률적으로 이동에 성공하는 메소드 구현 - 객체 출력 시 이름과 현재 이동 거리가 출력되도록 오버라이딩

---
+++ src/main/java/racingcar/game/Car.java
@@ -0,0 +1,42 @@
+package racingcar.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+/**
+ * 자동차의 기능을 정의하는 클래스
+ *
+ * @author gemstoneyang
+ */
+public class Car {
+    private final String name;
+    private int distance;
+
+    /**
+     * @param name 자동차의 이름
+     */
+    Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    /**
+     * 자동차의 움직임을 시도
+     * <p>
+     * 성공할 경우 거리 1 증가, 실패할 경우 거리 0 증가
+     *
+     */
+    public void tryMove() {
+        int number = Randoms.pickNumberInRange(0, 9);
+
+        if (number >= 4) {
+            this.distance++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = this.name + " : " + "-".repeat(this.distance);
+
+        return result.trim();
+    }
+}
