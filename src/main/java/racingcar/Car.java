a8077ebd898f2228457c9dc825d64c63539e371a
JCH <ckdgusjang@naver.com>
2023-11-01 19:44:33 +0900

    feat : Car클래스에 0~9랜덤값 계산 기능추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     //현재 자동차의 이동거리 변수
     private int distance;
@@ -19,5 +21,13 @@ public class Car {
     Car(String name, int distance) {
         this.name = name;
         this.distance = distance;
+    }
+
+    //0~9랜덤값 가져오기
+    public void count_Random() {
+        int num = Randoms.pickNumberInRange(0, 9);
+        if (num >= 4) {
+            distance += 1;
+        }
     }
 }
