5c077e9f526ae49ca9be374a1eba670a86a004ba
JangHongJoon <wkdghdwns19969@gmail.com>
2023-11-01 14:31:02 +0900

    feat : 단위 테스트를 위한 코드들 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,11 +24,77 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 전진_정지_동시_우승자() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,joon", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "joon : ", "최종 우승자 : pobi,woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 이름이_없을때_예외_처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,woni","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_없을때_예외_처리_2(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름을_다른_문자로_나눴을때_예외처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi/woni","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름을_다른_문자로_나눴을때_예외처리2(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi woni","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_공백으로_이루어져_있을때_예외처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("    ,pobi,woni","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_겹쳤을때_예외처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi","1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_입력이_없을떄_예외처리(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("","1"))
+                        .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
