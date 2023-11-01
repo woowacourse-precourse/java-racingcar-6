1125096a7ef3decc9b4ce4ee0dbdbfe4695b2ff8
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-01 21:45:51 +0900

    feat(Car): 전진 기준값 및 getter 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,11 +2,21 @@ package racingcar.model;
 
 public class Car {
 
+    public static final int MOVE_FORWARD_THRESHOLD = 4;
     private final String name;
     private String status;
 
     public Car(String name) {
         this.name = name;
+        this.status = "";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getStatus() {
+        return this.status;
     }
 
     public void moveForward() {
