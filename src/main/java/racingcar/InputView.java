e94d3be993fe260bfe8f41991a559c7e9883af6f
DavinAn <blueapple1030@gmail.com>
2023-11-01 19:50:22 +0900

    feat(InputView) : 입력 요청문구, method header 작성

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class InputView {
+
+  private static final String CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+  private static final String TRIAL_NUM_REQUEST = "시도할 회수는 몇회인가요?";
+
+  public List<String> carName() {
+    return null;
+  }
+
+  public int trialNum() {
+    return 0;
+  }
+
+}
