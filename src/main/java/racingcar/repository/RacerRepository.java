c5c4a2c906c18f1450e061a238ed524c4c241db7
doyoungkim97 <eh4536@naver.com>
2023-11-01 22:33:41 +0900

    feat : 레이서 정보 리포지토리 인터페이스 구현

--- src/main/java/racingcar/repository/RacerRepository.java
+++ src/main/java/racingcar/repository/RacerRepository.java
@@ -2,11 +2,14 @@ package racingcar.repository;
 
 import racingcar.domain.Racer;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RacerRepository {
     public Racer save(Racer racer);
     public Optional<Racer> findById(Long id);
     public Optional<Racer> findByName(String name);
-    public int getMoving(String name);
+    public void setMoveByName(String name);
+    public int getMoveByName(String name);
+    public List<Racer> findAll();
 }
