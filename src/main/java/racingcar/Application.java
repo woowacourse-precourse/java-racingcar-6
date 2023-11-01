af79d9a22c1d09b3d7fa006b19b7cc2a16f5d3ee
jh9788 <gkwhdgns2@gmail.com>
2023-11-01 22:46:30 +0900

    Feat: initCars 함수 구현
    입력을 받고 저장하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -24,6 +24,37 @@ public class Application {
         if(car.length() > 5)
             throw new IllegalArgumentException();
     }
+    private static void initCars() throws IllegalArgumentException {
+        String input;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+
+
+        String[] carsArray;
+        List<String> carsList;
+        try {
+            carsArray = input.split(",");
+            carsList = Arrays.asList(carsArray);
+            for (String car : carsList) {
+                validateInput(car);
+            }
+        }catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        try{
+            count = sc.nextInt();
+        }catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+
+        for(String car : carsList) {
+            carsMap.put(car, 0);
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
