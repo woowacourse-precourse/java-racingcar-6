6495d76ac70504bd1b213fd7e4f8079ec314fc54
imhungry2 <sjnnj@naver.com>
2023-11-01 17:38:19 +0900

    model - Car 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,15 +1,21 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position;
 
     public Car(String name) {
-        // 생성자 로직
+        this.name = name;
+        this.position = 0;
     }
 
-    public void move(int randomValue) {
-        // 이동 로직
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            position++;
+        }
     }
 
     public String getName() {
