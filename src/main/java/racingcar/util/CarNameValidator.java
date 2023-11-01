0db6ba9a549157e0944cb50c243ec677ad58170d
djc06048 <gsafe1213@gmail.com>
2023-11-01 14:17:37 +0900

    refactor(util): CarNameValidator 리팩토링 입력값 검사 조건 추가 및 반환 값 변경해 코드간 의존도 낮춤 - 입력값 검사 컨트롤러에서 수행하도록 변경

--- src/main/java/racingcar/util/CarNameValidator.java
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -1,11 +1,28 @@
 package racingcar.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarNameValidator {
     private final static int MAX_CARNAME = 5;
 
-    public static String validateCarName(String carName) {
-        if (carName.length() > MAX_CARNAME) {
-            throw new IllegalArgumentException(String.format("자동차 이름이 %s글자 이하이어야 합니다", MAX_CARNAME));
-        } else return carName;
+    public static List<String> validateCarNameInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력 값이 없습니다");
+        }
+        if (input.split(",").length != input.chars().filter(c -> c == ',').count() + 1) {
+            throw new IllegalArgumentException("입력 값의 형식이 잘못되었습니다");
+        }
+        String[] carNames = input.split(",");
+        List<String> validateCarNames = new ArrayList<>();
+
+        for (String name : carNames) {
+            if (name.length() > MAX_CARNAME) {
+                throw new IllegalArgumentException(String.format("자동차 이름이 %s글자 이하이어야 합니다", MAX_CARNAME));
+            }
+            validateCarNames.add(name);
+        }
+        return validateCarNames;
+
     }
 }
