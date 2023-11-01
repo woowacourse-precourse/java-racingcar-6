973ebc318d1ef124f8b69c3b92d754750a605ca5
mini1115 <kmini9611@naver.com>
2023-11-01 21:39:10 +0900

    feat : readme에 따른 추가 테스트 기능 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,15 +1,11 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.domain.Game;
-import racingcar.domain.PlayerInput;
 import racingcar.domain.Validate;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -20,9 +16,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
-    private Game game;
-    private static final List<String> ANSWER = Arrays.asList("sam","smith");
-    private PlayerInput playerInput;
+    private static final List<String> ANSWER = Arrays.asList("sam", "smith");
+    private static final int MAX_SCORE = 8;
+
+    public static Map<String, Integer> racers() {
+        Map<String, Integer> racers = new LinkedHashMap<>();
+        racers.put("sam", 5);
+        racers.put("smith", 8);
+        return racers;
+    }
+
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
@@ -41,7 +44,17 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
+    @Test
+    void 최종_결과_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "5");
+                    assertThat(output()).contains("pobi : -----", "woni : -----", "jun : ---", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+                ,STOP,MOVING_FORWARD
+        );
+    }
     @Test
     void 이름에_대한_입력값_예외_확인() {
         String input = "bestpobi";
@@ -51,16 +64,23 @@ class ApplicationTest extends NsTest {
                 .hasMessageContaining(message);
 
     }
+
     @Test
-    void 자동차_이름값_성공_확인(){
-        Map<String, Integer> racers = new HashMap<>();
-        racers.put("sam",0);
-        racers.put("smith",0);
+    void 자동차_이름값_성공_확인() {
+
         List<String> ExpectedNames = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : racers.entrySet()) {
+        for (Map.Entry<String, Integer> entry : racers().entrySet()) {
             ExpectedNames.add(entry.getKey());
         }
         assertThat(ANSWER).isEqualTo(ExpectedNames);
+    }
+
+    @Test
+    void 가장_많이_이동한_횟수_출력() {
+
+        Game game = new Game();
+        int maxScore = game.topScore(racers());
+        assertThat(MAX_SCORE).isEqualTo(maxScore);
     }
 
     @Override
