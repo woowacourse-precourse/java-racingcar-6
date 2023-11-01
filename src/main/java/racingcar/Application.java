a20e1f06fd0851c77cd4442bfc5af015ef0650a4
minu512 <minuet1215@jbnu.ac.kr>
2023-11-01 16:22:59 +0900

    feat: 자동차 이름을 입력받고 생성하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import racingcar.domain.Car;
+import racingcar.domain.CarGroup;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarGroup carGroup = new CarGroup();
+        inputCars(carGroup);
+    }
+
+    private static void inputCars(CarGroup carGroup) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = readLine();
+        for (String carName : cars.split(",")) {
+            Car car = new Car(carName);
+            carGroup.addCar(car);
+        }
     }
 }
