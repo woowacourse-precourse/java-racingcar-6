75d52f18524a0d8bd08ab472a18dc10191c8564c
DavinAn <blueapple1030@gmail.com>
2023-11-01 19:52:32 +0900

    feat(RacingcarRandomNumber) : 0~9 사이의 무작위 숫자를 생성하는 메서드 작성

---
+++ src/main/java/racingcar/RacingcarRandomNumber.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingcarRandomNumber {
+
+  public int generate() {
+    return Randoms.pickNumberInRange(0,9);
+  }
+}
