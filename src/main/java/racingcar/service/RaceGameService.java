54566af557a6223bda0478c78f1fa7a611ad57c3
IDpluto <d0013159@gmail.com>
2023-11-01 10:32:51 +0900

    refactor: Car class의 static으로 선언된 createCar 제거-> RaceGameService의 initializeCars 수정

--- src/main/java/racingcar/service/RaceGameService.java
+++ src/main/java/racingcar/service/RaceGameService.java
@@ -5,28 +5,36 @@ import racingcar.domain.Referee;
 
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.view.OutputView;
 
 public class RaceGameService {
-  private static List<Car> cars = new ArrayList<>();
-  Referee referee = new Referee();
+    private final List<Car> cars = new ArrayList<>();
+    private final RandomNumberGenerator randomNumberGenerator;
 
-  public void initializeCars(String[] carNames) {
-    for (String name : carNames) {
-      cars.add(Car.createCar(name));
+    public RaceGameService(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
     }
-  }
-  public void race() {
-    for (Car car : cars) {
-      car.moveForwardCar();
+
+    public void initializeCars(String[] carNames) {
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
     }
-  }
 
-  public static List<Car> getCars() {
-    return cars;
-  }
-
-  public String getWinners() {
-    return referee.judgementGameWon(cars);
-  }
+    public void startRace(int maxRound, OutputView outputView) {
+        for (int round = 0; round < maxRound; round++) {
+            race();
+            outputView.printRaceResult(cars);
+        }
+    }
+    public void race() {
+        for (Car car : cars) {
+            car.moveForwardCar(randomNumberGenerator.generateUniqueNumbers());
+        }
+    }
+    public String getWinners(Referee referee) {
+        return referee.determineWinners(cars);
+    }
 
 }
