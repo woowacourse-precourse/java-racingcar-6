9133a00de523613e4ea354651375b1790c5d7dc1
jongmee <ra123221@gmail.com>
2023-11-01 15:19:22 +0900

    feat: 양옆 공백을 제거하고 경주차 이름을 파싱하기
    - 문자열을 파싱하는 과정은 생성자에 존재하기 보다 따로 메서드를 빼는 것이 기능 분리에 더 적합하다고 생각해 메서드를 분리하였다.

--- src/main/java/racingcar/dto/request/CarNamesDto.java
+++ src/main/java/racingcar/dto/request/CarNamesDto.java
@@ -4,6 +4,7 @@ import static racingcar.constants.Condition.NAME_SIZE;
 import static racingcar.constants.ErrorMessage.LENGTH_LIMIT;
 import static racingcar.constants.Format.CAR_NAME_DELIMITER;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,8 +13,17 @@ public class CarNamesDto {
     private final List<String> names;
 
     public CarNamesDto(String carNames) {
-        this.names = Arrays.asList(carNames.split(CAR_NAME_DELIMITER));
+        this.names = parseNames(carNames);
         validateNameLength(names);
+    }
+
+    List<String> parseNames(String carNames) {
+        String[] names = carNames.split(CAR_NAME_DELIMITER);
+        List<String> parsedNames = new ArrayList<>();
+        for(String name: names) {
+            parsedNames.add(name.trim());
+        }
+        return parsedNames;
     }
 
     private void validateNameLength(final List<String> names) {
