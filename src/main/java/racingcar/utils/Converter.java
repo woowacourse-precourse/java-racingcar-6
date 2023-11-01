cbbee836a4e508141a2094bdd5c93633711417ec
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:08:49 +0900

    feat: Converter 클래스 구현

---
+++ src/main/java/racingcar/utils/Converter.java
@@ -0,0 +1,18 @@
+package racingcar.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Converter {
+
+    public static List<String> toList(String before) {
+        return Arrays.stream(before.replaceAll(" ", "")
+                        .split(","))
+                .collect(Collectors.toList());
+    }
+
+    public static int toInteger(String before) {
+        return Integer.parseInt(before);
+    }
+}
