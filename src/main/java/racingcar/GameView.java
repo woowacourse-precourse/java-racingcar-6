b6c24fc33874a29c98022c00840ffd177eea85ed
TaeUkChu <hippo2012@naver.com>
2023-11-01 14:38:09 +0900

    사용자 입력 Validation 클래스 1차 작성 완료

--- src/main/java/racingcar/GameView.java
+++ src/main/java/racingcar/GameView.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import org.assertj.core.util.Arrays;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +11,23 @@ public class GameView {
         String inputCars = Console.readLine();
         return makeCarList(inputCars);
     }
+
 //    입력 : 게임 횟수
-    public int inputNums() throws IllegalArgumentException{
-        int num;
-        num = Integer.parseInt(Console.readLine());
-        return num;
+    public int inputNum() throws IllegalArgumentException{
+        int num = Integer.parseInt(Console.readLine());
+        return Validation.numValid(num);
     }
+
     public List<Car> makeCarList(String inputCars){
         List<Car> carList = new ArrayList<>();
         String[] splitCars = inputCars.split(",");
 
-        // List를 분리하는 다른 방법
-        // List<Object> carNames = Arrays.asList(inputCars.split(","));
+        // List를 분리하는 다른 방법 : List<Object> carNames = Arrays.asList(inputCars.split(","));
         for (String splitCar : splitCars) {
-            carList.add(new Car(splitCar));
+            if(Validation.carValid(splitCar)){     // Car 이름 검증
+                carList.add(new Car(splitCar));
+            }
         }
-
         return carList;
     }
 }
--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,2 +1,24 @@
-package racingcar;public class Validation {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Validation {
+//    자동차 이름 검증 메서드
+    public static boolean carValid(String name) throws IllegalArgumentException{
+        // 이름이 5자 초과이거나 입력하지 않는다면
+        if ((name.length() > 5) | (name.isEmpty()) | (name.equals(" "))){
+            System.out.println("이름 길이가 5자 미만이 되도록 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+//    게임 횟수 검증 메서드
+    public static int numValid(int inputNum) throws IllegalArgumentException{
+        if(inputNum < 0){
+            System.out.println("0 이상의 숫자만 입력해주세요");
+            throw new IllegalArgumentException();
+        }
+        return inputNum;
+    }
 }
