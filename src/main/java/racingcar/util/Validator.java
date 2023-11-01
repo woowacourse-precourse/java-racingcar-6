680fe3a050fce15496e0f60c3a2646686d92ae36
sunwoong <sunwoong1122@gmail.com>
2023-11-01 20:46:19 +0900

    feat: 자동차 이름 공백 여부 검증 기능 구현

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,12 @@
+package racingcar.util;
+
+public class Validator {
+    private final static String SYMBOL = ",";
+    private final static int MAXIMUM_NAME_LENGTH = 5;
+
+    private void validateIsBlank(String carsName) {
+        if (carsName.isBlank()) {
+            throw new IllegalArgumentException("자동차의 이름은 공백일 수 없습니다.");
+        }
+    }
+}
