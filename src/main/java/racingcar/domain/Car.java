5eb7f0dbe5352f0eb6cd5fcc09d84b47a41517fb
gunsookim <ccpc4896@naver.com>
2023-11-01 20:29:35 +0900

    feat: Validator 클래스와 중복되는 코드 제거
    이름 길이에 대한 예외 처리 코드 제거.
    중복된 이름에 대한 예외 처리 코드 제거.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import static racingcar.Vars.LENGTH_CAR_NAME;
+import racingcar.manager.Validator;
 import static racingcar.Vars.MOVE_CONDITION;
 
 public class Car {
@@ -8,15 +8,9 @@ public class Car {
     private int position;
 
     public Car(String name) {
-        validateName(name);
+        Validator.validateCarNameLength(name);
         this.name = name;
         this.position = 0;
-    }
-
-    private void validateName(String name) {
-        if (name.isEmpty() || name.length() > LENGTH_CAR_NAME) {
-            throw new IllegalArgumentException("자동차 이름은 1~5자만 가능합니다.");
-        }
     }
 
     public void move(int randomNumber) {
