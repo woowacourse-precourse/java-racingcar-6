ce82ed01bb557aa56c706a0551cff9e11c7f1e95
djc06048 <gsafe1213@gmail.com>
2023-11-01 10:51:56 +0900

    refactor(view): InputView static 으로 변경 및 반환 타입 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,23 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-public class InputView {
-    private final String carNameInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String tryCountInputString = "시도할 회수는 몇회인가요?";
 
-    public void generateCarNameInput() {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String carNameInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String tryCountInputString = "시도할 회수는 몇회인가요?";
+
+    public static String generateCarNameInput() {
         System.out.println(carNameInputString);
-        Console.readLine();
+        return Console.readLine();
     }
 
-    public void generateTryCountInput() {
+    public static int generateTryCountInput() {
         System.out.println(tryCountInputString);
-        Console.readLine();
+        int tryNumber = Integer.parseInt(Console.readLine());
+        return tryNumber;
     }
 }
