2428f4135dfb8d3d3cfb1c3ddc9708451408604b
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:26:31 +0900

    feat(Round): 속성 및 생성자 정의
    Round 클래스는 라운드 수를 표현하는 클래스로 int 값을 감싸는 래퍼 클래스입니다.
    Distance 클래스와 전반적으로 유사하긴 하나 추후 변경 가능성을 고려하여 분리하였습니다.
    
    속성
     - int round
    
    생성자
     - (int): 주어진 값으로 내부 값을 설정합니다. 만약, 주어진 값이 음수라면
    IllegalArgumentException 을 발생시킵니다.

--- src/main/java/racingcar/model/Round.java
+++ src/main/java/racingcar/model/Round.java
@@ -1,4 +1,18 @@
 package racingcar.model;
 
 public class Round {
+
+    private int round;
+
+    /**
+     * @param nonNegativeInteger
+     * @throws IllegalArgumentException nonNegativeInteger < 0
+     */
+    public Round(int nonNegativeInteger) throws IllegalArgumentException {
+        if (nonNegativeInteger < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.round = nonNegativeInteger;
+    }
 }
