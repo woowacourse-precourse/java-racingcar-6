26b427ae1eeb7bd23e48e0b5b392164fca25a3fe
masonkimseoul <masonkimseoul@gmail.com>
2023-11-01 15:17:19 +0900

    feat : Add exceptions in input functions

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -4,11 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String getCarNameString() {
-        return Console.readLine();
+    public static String getCarNamesString() {
+        String carNames = Console.readLine();
+
+        if (carNames.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 한 글자 이상 입력하셔야 합니다.");
+        }
+
+        return carNames;
     }
 
     public static int getTryCount() {
-        return Integer.parseInt(Console.readLine());
+        int tryCount;
+
+        try {
+            tryCount = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+
+        return tryCount;
     }
 }
