20225597e1c54a14b06e10a1f67d1fadd59c3c64
cozym <ddongyee99@daum.net>
2023-11-01 21:15:07 +0900

    test: 잘못된 입력에 대한 예외처리 테스트 코드 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,14 +1,34 @@
 package racingcar;
 
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
+    RacingGame game = new RacingGame();
 
     @Test
-    void inputCarsName() {
+    void 공백_입력_예외처리() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream("pobi,  woni,jun".getBytes()));
+            game.inputCarsName();
+        }, "공백 입력 예외가 발생하지 않았습니다");
     }
 
     @Test
-    void inputTryNum() {
+    void 자동차_이름길이_예외처리() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream("pobi,looooooong".getBytes()));
+            game.inputCarsName();
+        }, "자동차 이름길이 예외가 발생하지 않았습니다");
     }
+
+    @Test
+    void 시도횟수_입력_예외처리() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream("-1".getBytes()));
+            game.inputTryNum();
+        }, "시도횟수 입력 예외가 발생하지 않았습니다");
+    }
+
 }
