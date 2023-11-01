85c3ff772a6a646434f7ed8d1f531a1ad542cfd3
seohokim <shk9946@gmail.com>
2023-11-01 23:08:18 +0900

    [FEAT] 자동차 이름 유효성 검사 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -21,6 +21,7 @@ public class Racing {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String inputcars = Console.readLine(); //자동차 이름 input
         List<String> carnames = Arrays.asList(inputcars.split(",")); //Array로 변환
+        ValidateInput.validateCarNames(carnames); // 유효성 검사
         for (String name: carnames) {
             this.cars.add(new Car(name)); // 객체 생성
         }
---
+++ src/main/java/racingcar/ValidateInput.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class ValidateInput {
+
+    public static void validateCarNames(List<String> carNames) {
+        if (carNames == null || carNames.isEmpty()) {
+            throw new IllegalArgumentException("Car names cannot be empty");
+        }
+
+        for (String name : carNames) {
+            if (name == null || name.length() > 5 || name.length() == 0) {
+                throw new IllegalArgumentException("Each car name should have 1 to 5 characters");
+            } 
+        }
+    }
+
+}
