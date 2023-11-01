68d7f4625460d100021a6ee8e4e5dba771db086e
djc06048 <gsafe1213@gmail.com>
2023-11-01 10:56:59 +0900

    feat(view): OutputView 추가결과 view

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class OutputView {
+    public static void processResult() {
+        System.out.println("\n실행 결과");
+    }
+    public static void GameResult() {
+        System.out.print("\n최종 우승자 : ");
+    }
+
+
+}
