d7f189fed90b026eeae729796bab32801e8eb79c
Soonrang <clearsbyul30@naver.com>
2023-11-01 22:26:43 +0900

    랜덤숫자 수정

--- src/main/java/racingcar/GenerateNumber.java
+++ src/main/java/racingcar/GenerateNumber.java
@@ -2,13 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateNumber {
 
-    public void GenerateRandomNumber(String[] players){
-        int[] pickNumbers = new int[players.length];
-        for (int i=0; i<players.length; i++){
-            pickNumbers[i] = Randoms.pickNumberInRange(0,9);
+    public List<Integer> GenerateRandomNumber(String[] players) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < players.length; i++) {
+            randomNumbers.add(Randoms.pickNumberInRange(0, 9));
         }
+        return randomNumbers;
     }
 
 }
