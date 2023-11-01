95280cfb6069d14cfc488414c214a10369d33bbc
Leespect <wewqwew153@gmail.com>
2023-11-01 16:57:32 +0900

    Refactor : 자동차 입력값에 대한 유효성 검사 수정

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,30 +1,40 @@
 package racingcar;
 
 public class Validation {
-    public static void validateCarName(String carNames){
-        validateEmpty(carNames);
-        validateCars(carNames);
+    public static void checkCarName(String Input){
+        String userInput = Input.trim();
+        checkEmpty(userInput);
+        checkFirstComma(userInput, userInput.length() - 1);
+        checkLastComma(userInput);
+        checkConsecutiveCommas(userInput);
     }
 
-    public static void validateEmpty(String carNames){
-        if (carNames == null || carNames.isEmpty()) {
-            throw new IllegalArgumentException();
+    public static void checkEmpty(String userInput){
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
         }
     }
 
-    public static void validateCars(String carNames){
-        // ,로 split한 후, 비어있거나 스페이스바로 이루어진 이름이 있는지 확인
-        String[] cars = carNames.split(",");
-        for (String car : cars) {
-            if (car == null || car.trim().isEmpty()) {
-                throw new IllegalArgumentException();
-            }
+    public static void checkFirstComma(String userInput, int lastIndex) {
+        if (userInput.charAt(lastIndex) == ',') {
+            throw new IllegalArgumentException("쉼표(,)로 끝납니다.");
         }
+    }
 
+    public static void checkLastComma(String userInput) {
+        if (userInput.charAt(0) == ',') {
+            throw new IllegalArgumentException("쉼표(,)로 시작합니다.");
+        }
+    }
+
+    public static void checkConsecutiveCommas(String userInput) {
+        if (userInput.contains(",,") || userInput.contains(", ,")) {
+            throw new IllegalArgumentException("쉼표(,)가 연속으로 입력되었습니다.");
+        }
     }
 
     public static void validateRound(String round){
-        if (round == null || round.isEmpty()) {
+        if (round.isEmpty()) {
             throw new IllegalArgumentException();
         }
     }
