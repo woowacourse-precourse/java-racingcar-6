7d65ffd23b26ef61dc08898f54ea5f4635572aa5
Sim-GyuBin <skb0606@gmail.com>
2023-11-01 16:09:40 +0900

    feat: 시도 횟수 입력 받는 기능

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -4,13 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-    private static List<String> inputCarNames() {
-        System.out.print("숫자를 입력해주세요 : ");
+    public User() {
+
+    }
+
+    public static List<String> inputCarNames() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = Console.readLine();
 
         List<String> carList = splitCarNames(userInput);
 
         return carList;
+    }
+
+    public static Integer inputNumberOfTurns() {
+        System.out.print("시도할 회수는 몇회인가요?");
+        String userInputTurn = Console.readLine();
+
+        int numberOfTurns = Integer.parseInt(userInputTurn);
+
+        return numberOfTurns;
     }
 
     private static List<String> splitCarNames(String carNames) {
