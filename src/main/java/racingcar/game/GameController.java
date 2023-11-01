b41749166d2d8e49eeabee134bd999ae488c07d0
gmu1026 <curiouscomponent@gmail.com>
2023-11-01 14:58:54 +0900

    FIX: 이동 횟수 입력값 유효성 검사 추가

--- src/main/java/racingcar/game/GameController.java
+++ src/main/java/racingcar/game/GameController.java
@@ -2,44 +2,68 @@ package racingcar.game;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.regex.Pattern;
 import racingcar.car.Car;
 import racingcar.car.CarFactory;
 import racingcar.view.CarView;
 import racingcar.view.ResultView;
 
 public class GameController {
+    private static final Pattern NON_DIGIT = Pattern.compile("^(-|0|\\D)");
+
     private GameController() {
     }
 
     private static List<Car> cars;
 
     public static void start() {
+        CarView.printCarCreateMessage();
         inputCarName();
-        inputMoveCount();
+        CarView.printCarMoveMessage();
+        moveCar(inputMoveCount());
         ResultView.printWinnerMessage(cars);
     }
 
     private static void inputCarName() {
-        CarView.printCarCreateMessage();
         cars = CarFactory.createCar(Console.readLine());
     }
 
-    private static void inputMoveCount() {
-        CarView.printCarMoveMessage();
-        int inputMoveCount = Integer.parseInt(Console.readLine());
-        moveCar(inputMoveCount);
+    private static int inputMoveCount() {
+        String inputMoveCount = Console.readLine();
+        validateInputMoveCount(inputMoveCount);
+
+        return Integer.parseInt(inputMoveCount);
     }
 
+    private static void validateInputMoveCount(String inputMoveCount) {
+        validateEmptyInputMoveCount(inputMoveCount);
+        validateDigitInputMoveCount(inputMoveCount);
+    }
 
+    private static void validateEmptyInputMoveCount(String inputMoveCount) {
+        if (inputMoveCount.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateDigitInputMoveCount(String inputMoveCount) {
+        if (NON_DIGIT.matcher(inputMoveCount).find()) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     private static void moveCar(int inputMoveCount) {
-        for (int tryCount = 0; tryCount < inputMoveCount; tryCount++) {
-            for (Car car : cars) {
-                car.move();
-            }
+        ResultView.printResultCoverMessage();
 
-            ResultView.printResultCoverMessage();
+        for (int tryCount = 0; tryCount < inputMoveCount; tryCount++) {
+            moveCar();
             ResultView.printMoveResultMessage(cars);
+        }
+    }
+
+    private static void moveCar() {
+        for (Car car : cars) {
+            car.move();
         }
     }
 }
