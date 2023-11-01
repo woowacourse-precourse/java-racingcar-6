c6fb6118e01746234e6c19ae7f253f0887bd43f2
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-01 12:55:37 +0900

    refactor: Race 클래스에 대해 원시값 포장(1급 컬렉션) 적용

---
+++ src/main/java/racingcar/ParticipatingCars.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ParticipatingCars {
+    private List<Car> cars;
+
+    ParticipatingCars() {
+        this.cars = new ArrayList<>();
+    }
+
+    void registerEachCar(List<String> carNames) {
+        for (String carName : carNames) {
+            this.cars.add(new Car(carName));
+        }
+    }
+
+    void moveEachCar(Supplier<Integer> fuelSupplier) {
+        cars.forEach(car -> car.tryMove(fuelSupplier.get()));
+    }
+
+    List<String> getWinnersNames() {
+        int furthestLocation = findFurthestLocation();
+        return cars.stream()
+                .filter(car -> car.getLocation() == furthestLocation)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int findFurthestLocation() {
+        return cars.stream()
+                .mapToInt(Car::getLocation)
+                .max()
+                .orElse(0);
+    }
+
+    List<Car> getCars() {
+        return cars;
+    }
+}
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,35 +1,56 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 import racingcar.utils.Constants;
 import racingcar.utils.ErrorMessage;
 import racingcar.validation.ParticipatingCarsValidation;
 
 public class Race {
-    private List<Car> participatingCars;
-    private int moveCounts;
+    private ParticipatingCars participatingCars;
+    private MoveCounts moveCounts;
     public ParticipatingCarsValidation participatingCarsValidation;
 
     public Race() {
-        participatingCars = new ArrayList<>();
+        participatingCars = new ParticipatingCars();
         participatingCarsValidation = new ParticipatingCarsValidation();
     }
 
-    public void registerCar(List<String> participatingCars) {
-        participatingCarsValidation.validateParticipatingCars(participatingCars);
-        registerEachCar(participatingCars);
-    }
-
-    private void registerEachCar(List<String> carNames) {
-        for (String carName : carNames) {
-            this.participatingCars.add(new Car(carName));
-        }
+    public void registerCar(List<String> carNames) {
+        participatingCarsValidation.validateParticipatingCars(carNames);
+        participatingCars.registerEachCar(carNames);
     }
 
     public void registerMoveCounts(int moveCounts) {
-        this.moveCounts = moveCounts;
+        this.moveCounts = new MoveCounts(moveCounts);
+    }
+
+    public void startCarRacing() {
+        validateRaceNotOver();
+        conductRace();
+        moveCounts.decreaseMoveCounts();
+    }
+
+    private void validateRaceNotOver() {
+        if (moveCounts.isOver()) {
+            throw new IllegalArgumentException(ErrorMessage.RACE_ALREADY_FINISHED_ERROR_MESSAGE);
+        }
+    }
+
+    private void conductRace() {
+        participatingCars.moveEachCar(this::injectFuel);
+    }
+
+    public ParticipatingCars getParticipatingCars() {
+        return participatingCars;
+    }
+
+    public List<String> calculateWinners() {
+        return participatingCars.getWinnersNames();
+    }
+
+    public boolean isRaceOver() {
+        return moveCounts.isOver();
     }
 
     private int injectFuel() {
@@ -43,72 +64,5 @@ public class Race {
 
     private boolean isFuelInRange(int fuel) {
         return fuel >= Constants.MINIMUM_GENERATE_FUEL_LEVEL && fuel <= Constants.MAXIMUM_GENERATE_FUEL_LEVEL;
-    }
-
-    public void startCarRacing() {
-        validateRaceNotOver();
-        conductRace();
-        decreaseMoveCounts();
-    }
-
-    private void validateRaceNotOver() {
-        if (isRaceOver()) {
-            throw new IllegalArgumentException(ErrorMessage.RACE_ALREADY_FINISHED_ERROR_MESSAGE);
-        }
-    }
-
-    public boolean isRaceOver() {
-        return moveCounts <= Constants.MINIMUM_MOVE_COUNTS;
-    }
-
-    private void conductRace() {
-        participatingCars.forEach(car -> car.tryMove(injectFuel()));
-    }
-
-    private void decreaseMoveCounts() {
-        moveCounts--;
-    }
-
-    public List<Car> getParticipatingCars() {
-        return participatingCars;
-    }
-
-    public List<String> calculateWinners() {
-        int furthestLocation = findFurthestLocation();
-        return getWinnersNames(furthestLocation);
-    }
-
-    private int findFurthestLocation() {
-        int furthestLocation = 0;
-        for (Car car : participatingCars) {
-            furthestLocation = updateFurthestLocation(car, furthestLocation);
-        }
-        return furthestLocation;
-    }
-
-    private int updateFurthestLocation(Car car, int currentMaxLocation) {
-        return Math.max(car.getLocation(), currentMaxLocation);
-    }
-
-    private List<String> getWinnersNames(int furthestLocation) {
-        List<String> winnersNames = new ArrayList<>();
-        addWinners(furthestLocation, winnersNames);
-        return winnersNames;
-    }
-
-    private void addWinners(int furthestLocation, List<String> winnersNames) {
-        for (Car car : participatingCars) {
-            addWinner(car, furthestLocation, winnersNames);
-        }
-    }
-
-    private void addWinner(Car car, int furthestLocation, List<String> winnersNames) {
-        if (isWinner(car, furthestLocation)) {
-            winnersNames.add(car.getName());
-        }
-    }
-
-    private boolean isWinner(Car car, int furthestLocation) {
-        return car.getLocation() == furthestLocation;
     }
 }
