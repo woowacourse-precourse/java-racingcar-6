1ac329dd976b3e8a6de29bb6cb00f0e2d36e9677
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-01 20:41:16 +0900

    fix($InputProcessor): 테스트 파일명 변경
    테스트 클래스 파일명 양식 준수

--- src/test/java/study/InputProcessor.java
+++
@@ -1,8 +0,0 @@
-package study;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("입력 프로세서 테스트")
-public class InputProcessor {
-    
-}
---
+++ src/test/java/study/InputProcessorTest.java
@@ -0,0 +1,10 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("입력 프로세서 테스트")
+public class InputProcessorTest {
+}
