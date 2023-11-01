3b73f4bd81fec38ed7fa7efc0928b0205a7d8d99
ud00ng <dagam1240@gmail.com>
2023-11-01 23:49:07 +0900

    feat : add main service

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.controller.RacingStartController;
+import racingcar.model.Car;
+import racingcar.model.Racing;
+import racingcar.view.PrintView;
+
 public class Application {
+    private final static PrintView printView = new PrintView();
+    private final static RacingStartController startController = new RacingStartController();
+    private static void startRound(Racing racing) {
+        for (int i = 0; i < racing.getRaceCount(); i++) {
+            startController.startMove(racing);
+            printView.printFowardCount(racing.getCarList());
+        }
+    }
+
+    private static void run(String racingCount, List<Car> carList) {
+        if(startController.run(racingCount, carList) != null) {
+            Racing racing = startController.run(racingCount, carList);
+            startRound(racing);
+            startController.resultWinnerList(racing);
+            printView.printWinner(racing.getWinnerList());
+        }
+    }
+    private static void startRacing() {
+        String cars = printView.printStartMessage();
+        List<Car> carList = startController.getRacingCarList(cars);
+        String racingCount = printView.printCountMessage();
+        run(racingCount, carList);
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startRacing();
     }
 }
