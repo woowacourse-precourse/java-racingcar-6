d0c558d4411ea27163f2b02cc5e608d7cd7b0a45
jungyunmok <jungyunmok@gmail.com>
2023-11-01 12:40:32 +0900

    Docs : 구현 기능 목록

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
 public class Application {
+    // 자동차 이름 입력
+    public void carName() {
+
+    }
+
+    // 레이싱 게임
+
+    // 우승자 출력
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
