28f84f863062522148cff55928a2b8e8352ff4b6
devholic22 <hyunjoon.tech@gmail.com>
2023-11-01 13:02:04 +0900

    test: 횟수 음수 입력 예외처리 테스트
    음수 입력 시 예외 발생 테스트

--- src/test/java/racingcar/model/PlayCountTest.java
+++ src/test/java/racingcar/model/PlayCountTest.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static racingcar.view.exception.InputException.PLAY_COUNT_VALUE_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +14,13 @@ public class PlayCountTest {
     void validNumberInputTest() {
         // when & then
         assertDoesNotThrow(() -> PlayCount.createDefault("5"));
+    }
+
+    @Test
+    @DisplayName("음수를 입력했을 시 예외가 발생한다.")
+    void negativeNumberInputExceptionTest() {
+        // when & then
+        assertThatThrownBy(() -> PlayCount.createDefault("-5")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PLAY_COUNT_VALUE_EXCEPTION.getMessage());
     }
 }
