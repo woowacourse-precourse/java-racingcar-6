db290f602be6a1f2150b500cf5c9542d325c4219
Bellwin98 <jongseunghan@naver.com>
2023-11-01 10:25:38 +0900

    Refactor(Race): 접근제어자 수정 & 각각의 함수가 하나의 기능만을 수행하도록 분리 & moveOrStop 메서드 추가

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Race {
 
@@ -19,47 +20,53 @@ public class Race {
     }
 
     public void setRace() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String inputCarNames = Console.readLine();
+        enrollCars(inputCarNames); // 차량 정보 등록하기
 
-        enrollCars(); // 차량 정보 등록하기
         System.out.println("시도할 회수는 몇회인가요?");
         String inputTrial = Console.readLine();
         int trial = Integer.parseInt(inputTrial); // String -> int 변경
 
-        System.out.println("실행 결과");
-        for (int i = 0; i < trial; i++) {
-            for (Car car : cars) {
-                startRace(car);
-            }
-            System.out.println();
-        }
-        referee.announceWinner(scoreBoard);
+        startRace(trial); // 경주 시작
     }
 
-    public void enrollCars() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String inputCarNames = Console.readLine();
-        String[] carNames = inputCarNames.split(",");
-        for (String carName : carNames) {
+    private void enrollCars(String carNames) {
+
+        String[] carNamesArray = carNames.split(",");
+        for (String carName : carNamesArray) {
             if (inputValidation.checkName(carName)) {
                 Car car = new Car(carName);
                 cars.add(car);
             }
         }
-        inputValidation.checkDuplicate(Arrays.asList(carNames));
+        inputValidation.checkDuplicate(Arrays.asList(carNamesArray));
     }
 
+    private void startRace(int trial) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < trial; i++) {
+            for (Car car : cars) {
+                moveOrStop(car);
+            }
+            System.out.println();
+        }
+        Map<String, Integer> raceResult = scoreBoard.write(cars);
+        List<String> winners = referee.findWinners(raceResult);
 
-    public void startRace(Car car) {
+        referee.announceWinner(winners);
+    }
+
+    private void moveOrStop(Car car) {
         // 입력한 시도 횟수에 따른 경주 진행
         if (isAbleToMove()) {
             car.move();
         } else {
             car.stop();
         }
-        scoreBoard.write(cars);
     }
 
-    public boolean isAbleToMove() {
+    private boolean isAbleToMove() {
         int randomNumber = numberGenerator.createRandomNumber();
         return randomNumber >= CONDITION_RANDOM_VALUE;
     }
