6fdcbdfaec540e0c5cb0a239e07fadc535c721e5
Chris <hereokay88@gmail.com>
2023-11-01 17:37:55 +0900

    feat(racingcar): 무작위 값을 구하기 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,15 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     public void tryToMove(){
 
     }
 
-    public void generateRandomNumber(){
-
+    public int generateRandomNumber(){
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber;
     }
     public void compare(){
 
