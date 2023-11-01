0b17e1497eb27f09bf0626db05b0189159004c83
suin <parksuuuun@gmail.com>
2023-11-01 12:37:34 +0900

    feat: 화면에 출력할 메세지 목록 작성

---
+++ src/main/java/racingcar/UserInputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class UserInputView {
+
+    private static final String WELCOME_MESSAGE = "환영합니다! 지금부터 자동차 경주 게임을 시작합니다.";
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 직접 입력하십시오.";
+    private static final String ALERT_MESSAGE = " (주의사항: 본 게임은 최대 3명까지 이용 가능하며 이름은 쉼표(,)로 구분하고 "
+            + "5자 이하의 이름으로 입력할 것)";
+    private static final String TRY_TIMES_MESSAGE = "시도할 횟수는 몇 회입니까?";
+
+}
