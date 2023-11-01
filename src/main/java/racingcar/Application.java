0b11909ab606a3bdcfd99210f08057eb53def77d
gemstoneyang <bennyws99@gmail.com>
2023-11-01 20:28:40 +0900

    feat: 사용자의 입력을 받는 기능 추가 - DataIOHandler 클래스 제작 - 사용자의 이름 입력과 시도 입력 횟수를 받아 main문에 반환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.view.DataIOHandler;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = DataIOHandler.getNameInput();
+        Integer tryCount = DataIOHandler.getTryCountInput();
+
+
     }
 }
