5c7f908dab7ffe0f21df63e47bcfd22011764279
2021110806 <2021110806@dgu.ac.kr>
2023-11-01 19:55:15 +0900

    test(RacingGameTest.java): add canPlayRacingGame method 레이싱 게임의 전체 로직을 테스트하는 코드를 구현하였습니다.

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,18 +1,25 @@
 package racingcar;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.stubbing.OngoingStubbing;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class RacingGameTest {
 
     final int ENOUGH_BIG_NUMBER = 10000;
     RandomNumberMaker randomNumberMaker = new RandomNumberMaker();
+    String lineSeparator = System.lineSeparator();
     @Test
     void canCreateCars() throws NoSuchFieldException, IllegalAccessException {
         RacingGame racingGame = new RacingGame();
@@ -92,5 +99,30 @@ public class RacingGameTest {
             racingGame.moveCar((Car) carSet.get(2), 9);
         }
         assertEquals(answer,racingGame.getWinner());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"car1,car2\n3"})
+    void canPlayRacingGame(String userInput) {
+
+        int[] randomContents = {8, 7, 9, 0, 3, 1};
+        GameMachine gameMachine = new GameMachine();
+        final MockedStatic<Randoms> mock = mockStatic(Randoms.class);
+        OngoingStubbing<Integer> stubbing = mock.when(() -> Randoms.pickNumberInRange(0, 9));
+        for (int randomContent : randomContents) {
+           stubbing = stubbing.thenReturn(randomContent);
+        }
+        InputStream userInputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(userInputStream);
+        OutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        gameMachine.playRacingGame();
+        assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" + lineSeparator
+                + "시도할 회수는 몇회인가요?" + lineSeparator +  lineSeparator
+                + "실행 결과" + lineSeparator
+                + "car1 : -" + lineSeparator + "car2 : -" + lineSeparator + lineSeparator
+                + "car1 : --" + lineSeparator + "car2 : -" + lineSeparator + lineSeparator
+                + "car1 : --" + lineSeparator + "car2 : -" + lineSeparator + lineSeparator
+                + "최종 우승자 : car1" + lineSeparator, output.toString());
     }
 }
