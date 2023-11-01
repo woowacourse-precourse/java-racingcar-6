0e3f5de3ab074a62d32dd88e0d0abb4930e685d7
leemiyeon <lemingul7@gmail.com>
2023-11-01 23:18:13 +0900

    test: add RoundResultTest

---
+++ src/test/java/study/RoundResultTest.java
@@ -0,0 +1,25 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.result.CarResult;
+import racingcar.domain.result.RoundResult;
+
+public class RoundResultTest {
+    @Test
+    void getCarResultsTest() {
+
+        CarResult carResultA = new CarResult("pobi", 2);
+        CarResult carResultB = new CarResult("woni", 3);
+        List<CarResult> givenCarResults = Arrays.asList(carResultA, carResultB);
+
+        RoundResult roundResult = new RoundResult(givenCarResults);
+
+        List<CarResult> retrievedCarResults = roundResult.getCarResults();
+
+        assertThat(retrievedCarResults).containsExactly(carResultA, carResultB);
+    }
+}
