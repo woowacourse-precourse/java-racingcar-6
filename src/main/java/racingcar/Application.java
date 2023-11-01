2a9096701cc13aa6edbdb00951836253da1463e3
jooda00 <dragon2000a@gmail.com>
2023-11-01 13:49:21 +0900

    feat(Application): 객체 생성 및 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.model.Car;
+import racingcar.model.MovingCount;
+import racingcar.model.RacingGame;
+import racingcar.validator.CarValidator;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+        CarValidator carValidator = new CarValidator();
+
+        InputView inputView = new InputView(inputValidator);
+        OutputView outputView = new OutputView();
+        Car car = new Car(carValidator);
+
+        MovingCount movingCount = new MovingCount();
+        RacingGame racingGame = new RacingGame(movingCount);
+
+        GameController gameController = new GameController(inputView, car, racingGame, outputView);
+        gameController.play();
     }
 }
