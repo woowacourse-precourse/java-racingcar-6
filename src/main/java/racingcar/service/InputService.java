a902893d3bb7b80d56938549f7c6d38aaf035195
fly-like-you <qkrwnsgh71w@gmail.com>
2023-11-01 17:27:15 +0900

    feat: "추가 테스트 코드 작성에 따른 검증 메서드"

--- src/main/java/racingcar/service/InputService.java
+++ src/main/java/racingcar/service/InputService.java
@@ -1,5 +1,9 @@
 package racingcar.service;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import racingcar.domain.Car;
 import racingcar.view.InputView;
 
@@ -30,18 +34,37 @@ public class InputService {
 
     private static ArrayList<Car> transformCars(String carNames){
         ArrayList<String> carNameList = new ArrayList<>(Arrays.asList(carNames.split(",")));
+        validateCarDuplicatedName(carNameList);
         ArrayList<Car> cars = new ArrayList<>();
 
         for (String name : carNameList) {
+            validateCarName(name);
             cars.add(new Car(name));
         }
 
         return cars;
     }
 
-    private static void validateCarInput(String carInput) {
-        if (carInput.isEmpty() || carInput.contains(" ")) {
+    private static void validateCarDuplicatedName(ArrayList<String> carNameList) {
+        Set<String> carNameSet = new HashSet<>(carNameList);
+        if (carNameList.size() != carNameSet.size()){
+            throw new IllegalArgumentException("이름에 중복이 없는지 확인해주세요.");
+
+        }
+    }
+
+    private static void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("차량의 이름이 6자 이상입니다.");
+        }
+        if (name.isEmpty()) {
             throw new IllegalArgumentException("문자열에 공백이 포함되어 있습니다.");
+        }
+    }
+
+    private static void validateCarInput(String carInput) {
+        if (!carInput.matches("^[a-zA-Z,]*$")) {
+            throw new IllegalArgumentException("영어 알파벳과 쉼표(,) 이외의 문자가 포함되어 있습니다.");
         }
     }
 
