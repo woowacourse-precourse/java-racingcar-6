05a60b6fa690578a1f7724f728c87453f23edda5
qocksdlf123 <qocksdlf123@naver.com>
2023-11-01 22:12:06 +0900

    FEAT(CarNameVerifier): 자동차 입력 검증 클래스 구현

--- src/main/java/racingcar/verifier/CarNameVerifier.java
+++ src/main/java/racingcar/verifier/CarNameVerifier.java
@@ -2,6 +2,21 @@ package racingcar.verifier;
 
 import racingcar.exception.ExceptionMsg;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class CarNameVerifier {
+    private final List<String> carNames;
+
+    public CarNameVerifier(String input){
+        this.carNames = List.of(input.trim().split(","));   //배열을 만든 후 리스트 생성
+        validateNull();
+    }
+
+    private void validateNull(){
+        if(this.carNames.isEmpty()){
+            throw new IllegalArgumentException(ExceptionMsg.NULL_INPUT.getMessage());
+        }
+    }
 
 }
