5b9b6e261fbc957c87a4b7fa3afc711d678c898f
Isaac Han <isaachan233@gmail.com>
2023-11-01 12:02:17 +0900

    test: 생성자 예외 처리 케이스 테스트

--- src/test/java/racingcar/model/AttemptsTest.java
+++ src/test/java/racingcar/model/AttemptsTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AttemptsTest {
-
     private Attempts attempts;
     private final String inputNumber = "5";
     @BeforeEach
@@ -16,7 +15,26 @@ class AttemptsTest {
         attempts = new Attempts(inputNumber);
     }
     @Test
-    void getNumber() {
-        assertThat(attempts.getNumber()).isEqualTo(Integer.parseInt(inputNumber));
+    void getNumber() { assertThat(attempts.getNumber()).isEqualTo(Integer.parseInt(inputNumber));
+    }
+
+    @Test
+    void constructorNumberTest() {
+        Attempts attempts = new Attempts(5);
+        assertThat(attempts.getNumber() == 5).isTrue();
+    }
+    @Test
+    void constructorNotNumericTest() {
+        assertThatThrownBy(() -> new Attempts("isaac")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void constructorBlankTest() {
+        assertThatThrownBy(() -> new Attempts("   ")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void constructorNegativeTest() {
+        assertThatThrownBy(() -> new Attempts("-1")).isInstanceOf(IllegalArgumentException.class);
     }
 }
