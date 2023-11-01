9475d8c7ca0fa6bf773e54c1619651475c4095a7
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-01 16:12:10 +0900

    feat(OutputView): 모든 자동차의 상태 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,20 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String EXECUTION_RESULT = "\n실행 결과";
 
     public void printExecutionResult() {
         System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printRacingCarStatus(List<String> carStatus) {
+        for (String status : carStatus) {
+            System.out.println(status);
+        }
+        System.out.println();
     }
 
 }
