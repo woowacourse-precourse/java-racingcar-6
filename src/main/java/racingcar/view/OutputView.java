b6f6983baedb37b26d6aeb7ded41a050e4684f70
TaeDongUm <etd93@naver.com>
2023-11-01 23:59:11 +0900

    수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class OutputView {
 	public static void printError(String message) {
-		System.out.println(ERROR_OCCURED + message);
+		System.out.println(ERROR_OCCURED+ message);
 	}
 
 	public static void printGameResultMessage() {
