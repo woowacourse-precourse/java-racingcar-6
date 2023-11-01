7c87c2b9b3ba98f42fcf2eef3f1458b61523f110
OneCrazyman <cjw7155@naver.com>
2023-11-01 22:10:29 +0900

    feat(car): 자동차 이름을 입력받고 검사하는 메소드 생성

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingGame {
 
     public RacingGame() {
@@ -7,5 +9,17 @@ public class RacingGame {
     }
 
     private void Progress() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String[] names = InputName();
+    }
+
+    private String[] InputName() {
+        String[] names = Console.readLine().split(",");
+        //검사-자동차 수가 2개보다 작으면 예외처리
+        if (names.length <= 1) {
+            throw new IllegalArgumentException();
+        }
+        return names;
     }
 }
