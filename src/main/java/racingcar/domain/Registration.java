0edeec777b17befc1ee87060d69a61d64c0c0d4b
jjw0071 <jjw0071@naver.com>
2023-11-01 21:26:52 +0900

    feat(Registration) : 이름을 입력 받아 자동차 생성하는 기능 구현

--- src/main/java/racingcar/domain/Registration.java
+++ src/main/java/racingcar/domain/Registration.java
@@ -1,7 +1,26 @@
 package racingcar.domain;
 
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registration {
+    private InputValidator inputValidator;
+
+    public List<Car> registerCars(){
+        String input = Console.readLine();
+        List<Car> cars = new ArrayList<>();
+
+        for(String carName : input.split(",")){
+            inputValidator.validateLength(carName);
+            Car newCar = new Car(carName);
+            cars.add(newCar);
+        }
+
+        return cars;
+    }
 
 
 }
