f29ea01a3206629ffda69ffaa5a6269ab53a50ee
“mpfo0106” <mpfo0106@gmail.com>
2023-11-01 09:30:18 +0900

    test(racingcar): NumberGeneratorTest 생성 및 구현

--- src/test/java/racingcar/model/NumberGeneratorTest.java
+++ src/test/java/racingcar/model/NumberGeneratorTest.java
@@ -1,4 +1,24 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class NumberGeneratorTest {
+    @Test
+    void 생성한_랜덤값을_검증() {
+        //given (주어진 값)
+        int numOfCars = 5;
+        NumberGenerator numberGenerator = new NumberGenerator(numOfCars);
+
+        //when (기능 작동)
+        List<Integer> carRandomNumbers = numberGenerator.getRandomNumbers();
+
+        //then (기능 작동 후 검증)
+        assertThat(carRandomNumbers).hasSize(numOfCars);    //랜덤값 개수를 검증
+        assertThat(carRandomNumbers).allMatch(num -> num >=0 && num<=9);    //랜덤값이 0에서 9까지 검증
+    }
+
 }
