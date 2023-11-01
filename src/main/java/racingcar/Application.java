4834704a7b7ec7dd68d466ab1132e9708df66c93
seoyeon chae <83562035+che-so@users.noreply.github.com>
2023-11-01 22:45:05 +0900

    feat: Implement all features except exception handling

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,35 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    static String[] strArr;
+    static String[] carName;
 
     public static void main(String[] args) {
-        inputname();
+        int number = input();
+        race(number);
     }
 
-    public static void inputname() {
+    public static int input() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        splitName(readLine());
+
+        return splitName(readLine());
     }
 
     public static int splitName(String inputname) {
-        strArr = inputname.split(",");
+        carName = inputname.split(",");
         int Number_of_races = 0;
 
-        if (strArr.length == 0 || strArr.length == 1) {
-            inputname();
+        for(int i = 0; i < carName.length; i++) {
+            if(carName[i].length() > 5) {
+                System.out.println("자동차 이름은 5자를 초과할 수 없습니다. 다시 입력하세요.");
+                return input();
+            }
+        }
+
+        if (carName.length == 0 || carName.length == 1) {
+            input();
         } else {
             System.out.println("시도할 회수는 몇회인가요?");
 
@@ -33,5 +44,59 @@ public class Application {
         return Number_of_races;
     }
 
+    private static void race(int number) {
+        // 배열 크기를 지정합니다.
+        int random;
+        int[] score = new int[carName.length];
+
+        for (int i = 0; i < score.length; i++) {
+            score[i] = 0;
+        }
+
+        System.out.println("\n 실행 결과");
+        for(int i = 0; i < number; i++){
+            racescore(score);
+            System.out.println();
+        }
+
+        result(score);
+    }
+
+    private static int[] racescore(int[] score) {
+        int random;
+
+        for (int j = 0; j < carName.length; j++) {
+            random = Randoms.pickNumberInRange(0, 9);
+            if (random >= 4) {
+                score[j]++;
+            }
+            System.out.print(carName[j] + " : ");
+            for (int k = 0; k < score[j]; k++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+
+        return score;
+    }
+
+    private static void result(int[] score) {
+        int max = 0;
+        String winner = "";
+
+        for (int i = 0; i < score.length; i++) {
+            if (max < score[i]) {
+                max = score[i];
+            }
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            if (max == score[i]) {
+                winner += carName[i] + ", ";
+            }
+        }
+
+        System.out.println("최종 우승자 : " + winner.substring(0, winner.length() - 2));
+    }
 
 }
