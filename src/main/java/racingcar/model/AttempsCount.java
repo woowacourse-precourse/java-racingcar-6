0d906286a26e2ba55192791ccfe4617457efe223
ch1hyun <och2510@gmail.com>
2023-11-01 15:37:15 +0900

    refactor : Modify type of input attemps count variable, Integer -> String
    - AttempsCount.java

--- src/main/java/racingcar/model/AttempsCount.java
+++ src/main/java/racingcar/model/AttempsCount.java
@@ -8,16 +8,15 @@ public class AttempsCount {
     private final Integer MAX_ATTEMPTS_COUNT;
     private Integer currentAttempsCount;
 
-    public AttempsCount(Integer attempsCount) {
+    public AttempsCount(String attempsCount) {
         validatePositiveNumber(attempsCount);
 
-        this.MAX_ATTEMPTS_COUNT = attempsCount;
+        this.MAX_ATTEMPTS_COUNT = Integer.parseInt(attempsCount);
         this.currentAttempsCount = 0;
     }
 
-    private void validatePositiveNumber(Integer attempsCount) {
-        String attempsCountString = attempsCount.toString();
-        Matcher positiveNumberMatcher = GameConstant.POSITIVE_NUMBER_PATTERN.matcher(attempsCountString);
+    private void validatePositiveNumber(String attempsCount) {
+        Matcher positiveNumberMatcher = GameConstant.POSITIVE_NUMBER_PATTERN.matcher(attempsCount);
 
         if (positiveNumberMatcher.matches() == false) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_OF_ATTEMPTS_RANGE.getMessage());
