7ce34b0df5edee9a3500e7882545539959c7fe95
S-DPR <thankyouformuch@naver.com>
2023-11-01 17:13:37 +0900

    feat: InputHandler - initCars

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputHandler {
+
+    public static List<Car> initCars() {
+        String input = Console.readLine();
+        List<String> names = Arrays.stream(input.split(",")).toList();
+        List<Car> ret = new ArrayList<>();
+        names.forEach(name ->
+                ret.add(new Car(name))
+        );
+        return ret;
+    }
+}
