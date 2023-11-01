e1bb8c0242ba0aca8fd9a5ca2a0b71d7791920a2
Maljaaa <Maljaaa@github.com>
2023-11-01 23:19:09 +0900

    refactor(RacingRuleTest): static import
    - RacingRule 메서드를 static으로 import
    - new로 객체를 생성할 필요가 없어져서 삭제

--- src/test/java/racingcar/model/RacingRuleTest.java
+++ src/test/java/racingcar/model/RacingRuleTest.java
@@ -3,26 +3,24 @@ package racingcar.model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.model.RacingRule.isOkToUseAttemptCount;
+import static racingcar.model.RacingRule.isOkToUseCarName;
 
 class RacingRuleTest {
     
     @Test
     public void 자동차_이름_유효성_테스트() throws Exception {
         // given
-        RacingRule racingRule = new RacingRule();
-
         // when & then
-        assertDoesNotThrow(() -> racingRule.isOkToUseCarName("pobi"));
-        assertThrows(IllegalArgumentException.class, () -> racingRule.isOkToUseCarName("123456"));
+        assertDoesNotThrow(() -> isOkToUseCarName("pobi"));
+        assertThrows(IllegalArgumentException.class, () -> isOkToUseCarName("123456"));
     } 
     
     @Test
     public void 시도_횟수_유효성_테스트() throws Exception {
         // given
-        RacingRule racingRule = new RacingRule();
-
         // when & then
-        assertTrue(racingRule.isOkToUseAttemptCount(3));
+        assertTrue(isOkToUseAttemptCount(3));
     } 
 
 }
