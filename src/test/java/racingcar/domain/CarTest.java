8a088481e856701f125cb8eee104e4a25289e4c6
AidenRoh <colohrby@gmail.com>
2023-11-01 19:01:16 +0900

    test: add test code for car object and its function

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,81 @@
+package racingcar.domain;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import racingcar.dto.CarStatusDto;
+
+class CarTest {
+    private static MockedStatic<Randoms> randoms;
+    private static int SUCCESS_VALUE = 4;
+    private static int FAIL_VALUE = 3;
+
+    @BeforeAll
+    public static void beforeTest() {
+        randoms = mockStatic(Randoms.class);
+    }
+
+    @AfterAll
+    public static void afterTest() {
+        randoms.close();
+    }
+
+    @Test
+    void create() {
+        //given
+        String testName = "배달이";
+        //when
+        Car car = Car.create(testName);
+        //then
+        Assertions.assertThat(car.getName()).isEqualTo(testName);
+    }
+
+    @Nested
+    @DisplayName("Progress 클래스와 Car 클래스의 tryPregress 함수를 테스트합니다.")
+    class tryProgressTest {
+        @DisplayName("전진에 성공할 시, progressScore에 1값이 추가됩니다.")
+        @Test
+        void tryProgressSuccess() {
+            //given
+            given(Randoms.pickNumberInRange(Progress.MINIMUM_RANGE_NUMBER, Progress.MAXIMUM_RANGE_NUMBER))
+                    .willReturn(SUCCESS_VALUE);
+            Car car = Car.create("배달이");
+            //when
+            car.tryProgress();
+            //then
+            Assertions.assertThat(car.getProgressScore()).isEqualTo(1);
+        }
+
+        @DisplayName("전진에 실패할 시, progressScore에 0값이 추가됩니다.")
+        @Test
+        void tryProgressFail() {
+            //given
+            given(Randoms.pickNumberInRange(Progress.MINIMUM_RANGE_NUMBER, Progress.MAXIMUM_RANGE_NUMBER))
+                    .willReturn(FAIL_VALUE);
+            Car car = Car.create("배달이");
+            //when
+            car.tryProgress();
+            //then
+            Assertions.assertThat(car.getProgressScore()).isEqualTo(0);
+        }
+    }
+
+    @Test
+    void sendStatus() {
+        //given
+        Car car = Car.create("배달이");
+        //when
+        CarStatusDto carStatusDto = car.sendStatus();
+        //then
+        Assertions.assertThat(carStatusDto.name()).isEqualTo("배달이");
+        Assertions.assertThat(carStatusDto.score()).isEqualTo(0);
+    }
+}
