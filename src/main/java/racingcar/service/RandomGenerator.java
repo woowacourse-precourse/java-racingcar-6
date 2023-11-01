9832f1608f295e11a55b4b51fe1b3f6072e2685f
yang <hjyang@epience.com>
2023-11-01 13:59:49 +0900

    🚀refector(service/RandomGenerator): 리턴타입 변경

--- src/main/java/racingcar/service/RandomGenerator.java
+++ src/main/java/racingcar/service/RandomGenerator.java
@@ -5,18 +5,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 /* Random 생성기
 *  0 ~ 9*/
 public class RandomGenerator {
-    private boolean result;
+    private final int MIN_FORWARD_NUMBER = 4;
 
     // random 생성
-    public boolean generateRandom() {
-        return randomResult(Randoms.pickNumberInRange(0,9));
+    public int generateRandom() {
+        return Randoms.pickNumberInRange(0,9);
     }
 
     // random 결과가 4이상이면 true
-    public boolean randomResult(int random) {
-        if (random >= 4) {
-            return true;
-        }
-        return false;
+    public boolean isForward() {
+        return generateRandom() >= MIN_FORWARD_NUMBER;
     }
 }
