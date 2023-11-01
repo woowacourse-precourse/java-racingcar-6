44bd9c5d5714de57449da2345c3c16c8b840acb7
binary_ho <dfghcvb11@naver.com>
2023-11-01 12:58:51 +0900

    refactor : RacingCar에서 사용중인 숫자를 상수로 선언

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,12 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
 
+    private static final int MOVING_STANDARD = 4;
+    private static final int STARTING_POSITION = 0;
+    private static final int RANDOM_NUMBER_START_RANGE = 0;
+    private static final int RANDOM_NUMBER_END_RANGE = 9;
+
     private final String name;
     private int position;
 
     public RacingCar(String name) {
         this.name = name;
-        this.position = 0;
+        this.position = STARTING_POSITION;
     }
 
     public void race() {
@@ -27,7 +32,8 @@ public class RacingCar {
     }
 
     private boolean canMoveForward() {
-        int randomNumber = Randoms.pickNumberInRange(0, 9);
-        return randomNumber >= 4;
+        int randomNumber = Randoms.pickNumberInRange(
+            RANDOM_NUMBER_START_RANGE, RANDOM_NUMBER_END_RANGE);
+        return randomNumber >= MOVING_STANDARD;
     }
 }
