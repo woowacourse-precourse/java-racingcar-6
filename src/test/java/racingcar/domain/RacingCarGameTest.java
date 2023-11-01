5d6c0e931164dd27b2eb0c535b20c8e1ac7ac920
hjhan <hjhan.0621@gmail.com>
2023-11-01 22:19:16 +0900

    test(printOutWinner()): 우승자 출력하는 테스트코드

--- src/test/java/racingcar/domain/RacingCarGameTest.java
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -2,13 +2,18 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.customCollection.CustomArrayList;
 
-class RacingCarGameTest {
+class RacingCarGameTest extends NsTest {
 
     RacingCarGame rg = new RacingCarGame();
+    CustomArrayList<Car> carList = new CustomArrayList<>();
+    List<Car> winnerList = new ArrayList<>();
 
     @Test
     @DisplayName("자동차_이름_쪼개기_공백삭제")
@@ -22,7 +27,6 @@ class RacingCarGameTest {
     @DisplayName("각각의_이름으로_자동차_객체_생성")
     void test_generateCarList() {
         String[] arrayOfCarName = {"blue", "red", "green", "white"};
-        CustomArrayList<Car> carList = new CustomArrayList<>();
 
         rg.generateCarList(arrayOfCarName, carList);
 
@@ -30,4 +34,35 @@ class RacingCarGameTest {
         assertThat(carList.get(1).getName()).isEqualTo("red");
     }
 
+    @Test
+    @DisplayName("단독일등_승자이름_출력")
+    void test_printOutWinner1() {
+        String carName = "단독일등";
+        winnerList.add(new Car(carName));
+
+        rg.printOutWinner(winnerList);
+
+        assertThat(output()).isEqualTo(carName);
+    }
+
+    @Test
+    @DisplayName("복수일등_승자이름_출력")
+    void test_printOutWinner2() {
+        String carName1 = "black";
+        String carName2 = "red";
+        String carName3 = "white";
+        winnerList.add(new Car(carName1));
+        winnerList.add(new Car(carName2));
+        winnerList.add(new Car(carName3));
+
+        rg.printOutWinner(winnerList);
+
+        assertThat(output()).contains(carName1, carName2, carName3);
+        assertThat(output()).isEqualTo("black, red, white");
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
 }
