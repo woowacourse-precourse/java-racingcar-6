c66c642bc1fca226259085909277276b9b55d33a
minkyeong <stl9955@naver.com>
2023-11-01 21:24:19 +0900

    Feat(GameInterface): checkCarNameLength 메서드 구현

--- src/main/java/racingcar/GameInterface.java
+++ src/main/java/racingcar/GameInterface.java
@@ -17,4 +17,10 @@ public class GameInterface {
             checkCarNameLength(name);
         }
     }
+
+    private static void checkCarNameLength(String carName){
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
 }
