39617c62e4a2cf8bff3e0590694f6af86bec955a
Ukj0ng <xb253q@gmail.com>
2023-11-01 15:02:43 +0900

    feat(InputView): 입력받는 기능
    구현한 기능:
    - 경주 할 자동차 이름 입력받기
    - 시도할 회수 입력받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,19 @@
 package racingcar.view;
 
-public class RacingView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+
+    public static String askForCarName() {
+        System.out.println(UITexts.ASK_FOR_CAR_NAME.getText());
+        String carNames = Console.readLine();
+        return carNames;
+    }
+
+    public static String howManyTries() {
+        System.out.println(UITexts.HOW_MANY_TRIES.getText());
+        String tries = Console.readLine();
+        System.out.println();
+        return tries;
+    }
 }
