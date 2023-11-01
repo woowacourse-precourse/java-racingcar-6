95e939ca619e5c8ca2fc144466b2b9d366bea5b4
gmu1026 <curiouscomponent@gmail.com>
2023-11-01 11:28:50 +0900

    Revert "FIX: 차 이름 길이 제한 기능 구현"
    This reverts commit 7f72ea25fa6559e578c2b13a636c3cd392b1c0e7.

--- src/main/java/racingcar/view/CarView.java
+++ src/main/java/racingcar/view/CarView.java
@@ -1,9 +1,7 @@
 package racingcar.view;
 
 public class CarView {
-    private CarView() {
-    }
-
+    private CarView() {}
     private static final String CAR_CREATE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String CAR_MOVE_MESSAGE = "시도할 회수는 몇회인가요?";
 
