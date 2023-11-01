f21e448e5a8e2cadea4ef63e45ba5d67d65a7b2f
dj <fjzl12@naver.com>
2023-11-01 19:29:50 +0900

    refactor(RoundValidator): 정적 메서드 - 인스턴스 메서드로 수정

--- src/main/java/racingcar/domain/RoundValidator.java
+++ src/main/java/racingcar/domain/RoundValidator.java
@@ -4,13 +4,13 @@ public class RoundValidator {
     public RoundValidator() {
     }
 
-    public static int validate(String number) {
+    public int validate(String number) {
         int naturalNumber = checkNaturalNumber(number);
         isUnderTwenty(naturalNumber);
         return naturalNumber;
     }
 
-    public static int checkNaturalNumber(String number) {
+    private int checkNaturalNumber(String number) {
         try {
             int naturalNumber = Integer.parseInt(number);
             return naturalNumber;
@@ -19,7 +19,7 @@ public class RoundValidator {
         }
     }
 
-    public static void isUnderTwenty(int number) {
+    private void isUnderTwenty(int number) {
         if (number < 1 || number > 20) {
             throw new IllegalArgumentException("잘못된 값을 입력하여 게임을 종료합니다. (최대 20회까지 가능합니다.)");
         }
