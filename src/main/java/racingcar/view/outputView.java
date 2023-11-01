644aa9376cf4319b8c3fd29863a8a37973e787d3
nari <avakray@hansung.ac.kr>
2023-11-01 20:36:25 +0900

    feat(racingcar): 우승자 출력 문구 및 라운드별 결과 출력 문구 제작

--- src/main/java/racingcar/view/outputView.java
+++ src/main/java/racingcar/view/outputView.java
@@ -3,19 +3,33 @@ package racingcar.view;
 import java.util.List;
 
 public class outputView {
-
-    public static void printRacingResult(List<String> carNames, int round){
+    public static void printTitle(){
         System.out.println("\n실행 결과");
-
-        for(int i = 0; i < round; i++){
-            printRoundState(carNames);
+    }
+    public static void printGameResult(String carName, int pos){
+        printCarName(carName);
+        printRoundResult(pos);
+    }
+    public static void printGameWinners(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i));
+            checkSeveralWinner(winners, i);
         }
+        System.out.println();
     }
 
-    private static void printRoundState(List<String> carNames){
-        for(String carName : carNames){
-            System.out.print(carName + " : ");
-            System.out.println("--전진횟수");
+    private static void checkSeveralWinner(List<String> winners, int i){
+        if (i < winners.size() - 1) {
+            System.out.print(", ");
+        }
+    }
+    private static void printCarName(String carName){
+        System.out.print(carName + " : ");
+    }
+    private static void printRoundResult(int pos){
+        for(int i=0; i < pos; i++){
+            System.out.print("-");
         }
         System.out.println();
     }
