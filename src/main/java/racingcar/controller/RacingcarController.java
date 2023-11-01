a621c28fbc93993aa8839990a3415cdf20064174
Sung Hyobin <hellmir610@gmail.com>
2023-11-01 17:12:42 +0900

    feat: Add input funciton to choose cars to race

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,11 +1,13 @@
 package racingcar.controller;
 
+import racingcar.view.InputHandler;
 import racingcar.view.Printer;
 
 public class RacingcarController {
 
     public static void startRacingcarGame() {
         Printer.printCarChoiceMessage();
+        String inputMessage = InputHandler.getInputMessage();
     }
 
 }
