d0fe5e599440508c8d0698ae5a4d4d35bd14ef14
ddrongy <ddrongy@gmail.com>
2023-11-01 16:21:39 +0900

    test: carservice test

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,34 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarTest {
-  
+    @Test
+    void power가_4이상일때_전진한다() {
+        // given
+        Car car = new Car("car");
+
+        // when
+        car.move(4);
+
+        // then
+        assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    void power가_3이하일때_멈춘다() {
+        // given
+        Car car = new Car("car");
+
+        // when
+        car.move(3);
+
+        // then
+        assertEquals(0, car.getPosition());
+    }
+
+
+
 }
