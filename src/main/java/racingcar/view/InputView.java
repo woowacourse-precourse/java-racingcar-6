5b568eff3db100347ed51fe5c4d4b3160e3b6ff9
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 15:19:39 +0900

    feat: 이름 입력에 대하여 공백을 제거한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,6 +13,8 @@ public class InputView {
     private static final String INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private static final String COMMA = ",";
+    private static final String REGEX_SPACE = "\\s";
+    private static final String REGEX_NO_SPACE = "";
 
     private InputView() {
     }
@@ -24,7 +26,8 @@ public class InputView {
     public List<String> inputCarNames() {
         System.out.println(INPUT_CARS_NAME);
         String inputCarNames = readLine();
-        List<String> carNames = split(inputCarNames);
+        String carNamesWithoutSpace = stringWithoutSpace(inputCarNames);
+        List<String> carNames = split(carNamesWithoutSpace);
         for (String carName : carNames) {
             validateInputCarName(carName);
         }
@@ -36,6 +39,10 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
+    public String stringWithoutSpace(String input) {
+        return input.replaceAll(REGEX_SPACE, REGEX_NO_SPACE);
+    }
+
     public int inputTryCount() {
         System.out.println(INPUT_TRY_COUNT);
         String inputTryCount = readLine();
@@ -43,6 +50,7 @@ public class InputView {
         System.out.println();
         return Integer.parseInt(inputTryCount);
     }
+
 
     private String readLine() {
         return Console.readLine();
