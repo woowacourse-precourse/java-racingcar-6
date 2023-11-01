22c1bc6aada1ccff105c65fccd4b5d739d26e07e
sosojean <thwlsdl0989@gmail.com>
2023-11-01 09:38:24 +0900

    테스트 기능 초안 작성

--- src/test/java/racingcar/GameFuctionTest.java
+++ src/test/java/racingcar/GameFuctionTest.java
@@ -19,5 +19,52 @@ public class GameFuctionTest {
 
         assertThat(result).contains(expected);
     }
+    @Test
+    void 구분자를_포함하는가(){
+
+    }
+    @Test
+    void 구분자를_포함하지_않았을떄_(){
+
+    }
+
+    @Test
+    void 이름이_다섯자_이하인가(){
+
+    }
+    @Test
+    void 자동차_전진횟수_입력받기(){
+
+    }
+    @Test
+    void 이동횟수에_숫자를_입력했는가(){
+
+    }
+    @Test
+    void 영에서_구사이_난수_발생(){
+
+    }
+
+    @Test
+    void 사_이상인_경우에_전진(){
+
+    }
+    @Test
+    void 매회차마다_차의_전진여부를_합산(){
+
+    }
+
+    @Test
+    void 시도횟수만큼_반복하기(){
+
+    }
+    @Test
+    void 매_턴마다_게임_진행_출력하는_기능(){
+
+    }
+    @Test
+    void 우승자_출력하기(){
+
+    }
 
 }
