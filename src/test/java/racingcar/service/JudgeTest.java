c4f46e579fb3dbd788dc9febeae97b093101a5da
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-01 22:48:06 +0900

    test(JudgeTest) : Modify to use variables

--- src/test/java/racingcar/service/JudgeTest.java
+++ src/test/java/racingcar/service/JudgeTest.java
@@ -8,42 +8,49 @@ import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 
 public class JudgeTest {
+    String name1 = "pobi";
+    String name2 = "woni";
+    String name3 = "jun";
     Judge judge;
     List<Car> carList;
+    List<Car> winningCars;
 
-    private void preset() {
+    private void beforeEach() {
         judge = Judge.getInstance();
-
-        Car car1 = new Car("pobi");
-        Car car2 = new Car("woni");
-        Car car3 = new Car("jun");
+        Car car1 = new Car(name1);
+        Car car2 = new Car(name2);
+        Car car3 = new Car(name3);
 
         carList = new ArrayList<>();
         carList.add(car1);
         carList.add(car2);
         carList.add(car3);
+
+        winningCars = new ArrayList<>();
     }
 
     @Test
-    public void testGetWinningCars() {
-        preset();
-        List<Car> winningCars = judge.getWinningCars(carList);
+    public void testGetWinningCars_multiWinners() {
+        beforeEach();
+        winningCars = judge.getWinningCars(carList);
 
         assertEquals(3, winningCars.size());
         for (int i = 0; i < 3; i++) {
             assertEquals(carList.get(i).getName(), winningCars.get(i).getName());
         }
 
-        for (Car car : carList) {
-            car.setWinner(false);
-        }
+    }
 
+    @Test
+    public void testGetWinningCars_soloWinner() {
+        beforeEach();
         carList.get(0).move(1);
         carList.get(1).move(2);
         carList.get(2).move(3);
 
         winningCars = judge.getWinningCars(carList);
+
         assertEquals(1, winningCars.size());
-        assertEquals("jun", winningCars.get(0).getName());
+        assertEquals(name3, winningCars.get(0).getName());
     }
 }
