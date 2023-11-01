e30d8b7aa51bfb85c529678c4b2a9637001071cb
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-01 18:19:30 +0900

    feat) game: 자동차 경주 게임을 실행

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,2 +1,15 @@
-package racingcar;public class Game {
+package racingcar;
+
+import java.util.LinkedHashMap;
+
+public class Game {
+    public void process() {
+
+        Start start = new Start();
+        start.getInput();
+        Play play = new Play();
+        LinkedHashMap<String, Integer> map = play.show(start);
+        Referee finalResult = new Referee();
+        finalResult.show(map);
+    }
 }
