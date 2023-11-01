9b7107daae06b6f27a16c15e5211ecb0914d158d
wschoe <chldntjr0425@naver.com>
2023-11-01 21:13:11 +0900

    feat: add car

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,22 @@
+package racingcar.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private Integer location;
+
+    public Car(String name){
+        this.name = name;
+        this.location = 0;
+    }
+    public void move(){
+        if(Randoms.pickNumberInRange(0,9) >= 4){
+            location += 1;
+        }
+    }
+
+    public Integer getLocation(){
+        return location;
+    }
+}
