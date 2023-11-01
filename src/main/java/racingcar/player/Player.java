87d83b8c90af74d97b08db04637b8fa5187436bb
minkyuu <minkyu023@gmail.com>
2023-11-01 16:48:38 +0900

    feat: 플레이어 입력 기능 추가

--- src/main/java/racingcar/player/Player.java
+++ src/main/java/racingcar/player/Player.java
@@ -1,16 +1,24 @@
 package racingcar.player;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.message.Message;
+import racingcar.message.ErrorMessage;
 
 public class Player {
-    public int inputMoveCnt() {
-        final String MOVE_CNT_REGEX = "[0-9]+";
-        System.out.println(Message.INPUT_MOVE_COUNT);
-        String num = Console.readLine();
-        if (!num.matches(MOVE_CNT_REGEX)) {
-            throw new IllegalArgumentException();
+    public String[] inputCarNames() {
+        String inputValue = Console.readLine();
+        return parseCarNamesFromInput(inputValue);
+    }
+
+    private String[] parseCarNamesFromInput(String inputValue) {
+        final String separator = ",";
+        String[] result = inputValue.split(separator);
+        if (result.length == 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAMES.getMessage());
         }
-        return Integer.parseInt(num);
+        return result;
+    }
+
+    public String inputMoveCnt() {
+        return Console.readLine();
     }
 }
