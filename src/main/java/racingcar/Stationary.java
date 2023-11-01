cb81e50606e3f5de20291c35269d46d362751d93
Leespect <wewqwew153@gmail.com>
2023-11-01 15:17:37 +0900

    Feat : 안내 문구 생성

---
+++ src/main/java/racingcar/Stationary.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Stationary {
+    public static final String namingCarMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String getCountsMessage = "시도할 회수는 몇회인가요?";
+    public static final String printGamingMessage = "실행 결과";
+    public static final String printWinnerMessage = "최종 우승자 : ";
+}
