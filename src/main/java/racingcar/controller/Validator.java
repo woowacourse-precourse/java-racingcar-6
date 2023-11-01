9052382bdc0dff6a7b8de2c9a7a1e3055f412b23
jmj005 <jmj00005@naver.com>
2023-11-01 18:34:27 +0900

    refactor: 시도할 횟수 검증 패턴 상수화

--- src/main/java/racingcar/controller/Validator.java
+++ src/main/java/racingcar/controller/Validator.java
@@ -4,13 +4,15 @@ import static racingcar.view.messages.ErrorMessages.ERROR_TRY_COUNT_NUMERIC;
 import static racingcar.view.messages.ErrorMessages.ERROR_TRY_COUNT_RANGE;
 
 public class Validator {
+    private static final String TRY_COUNT_PATTERN = "^[0-9]+$";
+
     public static void validateTryCount(String tryCount) {
         validateTryCountNumeric(tryCount);
         validateTryCountRange(tryCount);
     }
 
     private static void validateTryCountNumeric(String tryCount) {
-        if (!tryCount.matches("^[0-9]+$")) {
+        if (!tryCount.matches(TRY_COUNT_PATTERN)) {
             throw new IllegalArgumentException(ERROR_TRY_COUNT_NUMERIC.getErrorMessage());
         }
     }
