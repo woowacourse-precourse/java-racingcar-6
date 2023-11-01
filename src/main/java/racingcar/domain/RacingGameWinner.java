79984eca6f4a1258ca4fd7a2a07f4f465a321020
minkyeong <stl9955@naver.com>
2023-11-01 21:09:34 +0900

    Feat: RacingGameWinner Class 생성

---
+++ src/main/java/racingcar/domain/RacingGameWinner.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+
+public class RacingGameWinner {
+    private ArrayList<Car> cars;
+
+    public RacingGameWinner(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+}
