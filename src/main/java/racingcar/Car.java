f00c53cf60324957ea34f0a33da37e5cb36993a7
gomdol <oioifor@naver.com>
2023-11-01 16:24:12 +0900

    feat(Car) : Car 객체 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = 0;
+    }
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+    public void move(){
+        if(createRandomNumber()>=4){
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String toString(){
+        return this.name + " : " + "-".repeat(this.position)+ "\n";
+
+    }
+
+}
