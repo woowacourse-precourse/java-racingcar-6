8a7e5883940a0cc00424ef79fd2c21a681ed4624
jhon3242 <rldotnr2@gmail.com>
2023-11-01 11:38:26 +0900

    docs : 추가 체크 목록 체크

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -4,6 +4,6 @@ public class ErrorMessage {
     private ErrorMessage() {} // 인스턴트화 방지
     public static String INPUT_BLANK = "빈 값을 입력하였습니다.";
     public static String INPUT_NOT_INTEGER = "숫자를 입력해야합니다.";
-    public static String INVALID_CAR_NAME = "자동차 이름은 1자 이상, 4자 이하 이여야합니다.";
+    public static String INVALID_CAR_NAME = "자동차 이름은 5자 이하 이여야합니다.";
     public static String INVALID_NEGATIVE = "0 보다 작은 값을 입력할 수 없습니다.";
 }
