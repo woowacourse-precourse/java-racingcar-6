2c29eb80b2045edd3413d52488c16454d42382f5
simjaeyoun <spqjfgogo@gmail.com>
2023-11-01 20:48:56 +0900

    feat: 'controller생성 후 게임 시작 기능 추가'

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,11 @@
 package racingcar;
 
+import controller.GeneralController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        GeneralController controller = new GeneralController();
+        controller.gameStart();
     }
 }
