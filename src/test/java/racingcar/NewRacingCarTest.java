da6de0be93bbad06a6fa83f92d9b0ada99757377
cheolhyeon <tghy4@naver.com>
2023-11-01 18:12:58 +0900

    refactor : 테스트 코드 구조 변경 및 runRaceTest, car객체의 move 메서드 통과

--- src/test/java/racingcar/NewRacingCarTest.java
+++ src/test/java/racingcar/NewRacingCarTest.java
@@ -1,41 +1,74 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.car.Car;
 
 public class NewRacingCarTest {
-    List<Car> racingCarList = new ArrayList<>();
-    class User {
-        public String[] carNames() {
-            return new String[] { "A", "B", "C"};
+    List<Car> racingCarList;
+
+    @BeforeEach
+    void setUp() {
+        racingCarList = new ArrayList<>();
+    }
+
+
+    static class User {
+        public static String[] carNames() {
+            return new String[]{"A", "B", "C"};
         }
-        public String racingRound() {
+
+        public static String racingRound() {
             return "3";
         }
     }
+
+    static class NumberGenerator {
+        public static int generateNumber() {
+            return 4;
+        }
+    }
+
+
     @Test
     void addRacingCarTest() {
-        // given
-        User user = new User();
         // when
-        String[] carNames = user.carNames();
+        String[] carNames = User.carNames();
         for (String carName : carNames) {
             racingCarList.add(new Car(carName));
         }
         // then
-        Assertions.assertThat(racingCarList.size()).isEqualTo(3);
-
+        assertThat(racingCarList.size()).isEqualTo(3);
     }
+
     @Test
     void racingRoundTest() {
-        // given
-        User user = new User();
         // when
-        String racingRound = user.racingRound();
+        String racingRound = User.racingRound();
         // then
-        Assertions.assertThat(racingRound).isEqualTo("3");
+        assertThat(racingRound).isEqualTo("3");
     }
+
+    @Test
+    void runRaceTest() {
+        // given
+        String[] carNames = User.carNames();
+        for (String carName : carNames) {
+            racingCarList.add(new Car(carName));
+        }
+        // when, then
+        for (Car car : racingCarList) {
+            car.move(NumberGenerator.generateNumber());
+        }
+
+        for (Car car : racingCarList) {
+            assertThat(car.getCurrentPosition()).isEqualTo(1);
+        }
+    }
+
 }
+
