c57e5d041d861bcc0c3369fa223231594ea94946
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 20:28:42 +0900

    [refactor] : change input car names method with input try count method logic

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,21 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.TryCount;
+import racingcar.utils.StringUtils;
+
+import java.util.List;
 
 public class InputView {
-    public static String inputCarNames() {
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String carNamesInput = Console.readLine();
+        return StringUtils.splitCarNames(carNamesInput);
     }
 
-    public static String inputTryCount() {
+    public static TryCount inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+
+        return new TryCount(Console.readLine());
     }
 }
