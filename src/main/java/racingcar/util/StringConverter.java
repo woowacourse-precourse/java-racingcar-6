2a4223e3c17a550a634b6ba75e97762802f5586c
KimMinhee <99alsgmlalsl@naver.com>
2023-11-01 23:54:14 +0900

    Refactor: 게임 횟수 입력 시 공백 제거 작업 추가 - trim() 사용

--- src/main/java/racingcar/util/StringConverter.java
+++ src/main/java/racingcar/util/StringConverter.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class StringConverter {
 
     public static int toInteger(String data){
+        data = data.trim();
         if(!Pattern.matches("\\d+",data)){
             throw new IllegalArgumentException("입력한 수는 정수가 아닙니다.");
         }
