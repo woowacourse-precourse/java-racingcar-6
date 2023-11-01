8a750cdacadf32741dd3029ff883156152d0e0d8
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 11:17:16 +0900

    fix: 메소드 이름에 클래스 이름이 담기지 않게 수정

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -6,28 +6,28 @@ public record Name(
     private static final int MAX_NAME_LENGTH = 5;
 
     public Name {
-        validateName(name);
+        validate(name);
     }
 
-    private void validateName(final String name) {
-        validateNameNullity(name);
-        validateNameBlank(name);
-        validateNameLength(name);
+    private void validate(final String name) {
+        validateNullity(name);
+        validateBlank(name);
+        validateLength(name);
     }
 
-    private void validateNameNullity(final String name) {
+    private void validateNullity(final String name) {
         if (name == null) {
             throw new IllegalArgumentException("자동차 이름은 null이 될 수 없습니다.");
         }
     }
 
-    private void validateNameBlank(final String name) {
+    private void validateBlank(final String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백만으로는 구성할 수 없습니다.");
         }
     }
 
-    private void validateNameLength(final String name) {
+    private void validateLength(final String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
