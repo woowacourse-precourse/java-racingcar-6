3330f68c0e144964defd9a00db959145d73ad27a
doha <dohaahi@naver.com>
2023-11-01 10:43:40 +0900

    test: car에 대한 position과 name 테스크 코드 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class CarTest {
     @DisplayName("move를 하면 car의 position이 증가한다")
     @Test
-    void test() {
+    void testMove() {
         // given
         Car carA = Car.of("carA");
 
@@ -17,5 +17,45 @@ class CarTest {
 
         // then
         assertThat(carA.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차가 움직였을 때 포지션을 얻을 수 있다")
+    @Test
+    void testGetPosition1() {
+        // given
+        Car carA = Car.of("carA");
+        carA.move();
+
+        // when
+        int position = carA.getPosition();
+
+        // then
+        assertThat(position).isEqualTo(1);
+    }
+
+    @DisplayName("자동차가 움직이지 않았을 때 포지션을 얻을 수 있다")
+    @Test
+    void testGetPosition2() {
+        // given
+        Car carA = Car.of("carA");
+
+        // when
+        int position = carA.getPosition();
+
+        // then
+        assertThat(position).isEqualTo(0);
+    }
+
+    @DisplayName("자동차의 이름을 얻을 수 있다")
+    @Test
+    void testGetName() {
+        // given
+        Car carA = Car.of("carA");
+
+        // when
+        String name = carA.getName();
+
+        // then
+        assertThat(name).isEqualTo("carA");
     }
 }
