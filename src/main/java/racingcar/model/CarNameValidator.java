9c6e78ee575d2c8a861fe1bf597e866ba2434eed
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 13:47:31 +0900

    2023.11.01_feat : CarNameValidator isRightNameSize 메서드 생성

--- src/main/java/racingcar/model/CarNameValidator.java
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -14,15 +14,18 @@ public class CarNameValidator {
         this.NAMES = Utils.toArrayList(namesString);
         validate();
     }
-
     public void validate() {
 
     }
-
     public void isRightNamesString() {
         // String 형식 확인 : 앞&뒤-문자, 중간-문자&구분자
-        if (Constants.nameStringPattern.matcher(NAMES_STRING).matches()) {
+        if (!Constants.nameStringPattern.matcher(NAMES_STRING).matches()) {
             throw new IllegalArgumentException(Constants.CAR_NAME_WRONG_ERROR);
+        }
+    }
+    public void isRightNameSize() {
+        if (NAMES.stream().anyMatch(name -> name.isEmpty() || name.length() > Constants.NAME_SIZE)) {
+            throw new IllegalArgumentException(Constants.CAR_NAME_SIZE_ERROR);
         }
     }
 }
