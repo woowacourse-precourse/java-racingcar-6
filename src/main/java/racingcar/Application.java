90251c37e974cf1b1073405780d4b937fc8721b4
SNGYNGJIN <jsy9508@gmail.com>
2023-11-01 22:26:01 +0900

    feat: 메인 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        RacingController racingController = new RacingController(inputView, outputView);
+
+        racingController.run();
     }
 }
