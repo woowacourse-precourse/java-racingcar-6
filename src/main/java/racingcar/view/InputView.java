a4f8614db88ba2a25aa2ffbdee1788547797b04e
plan11plan <tjtfy7028@gmail.com>
2023-11-01 21:01:56 +0900

    feat:[create][InputView] InputView 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,19 +2,40 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.Car;
 
 public class InputView {
-    public static List<Car> requestCarName() {
+    public static void requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분, 5자 이하)");
         String input = readLine();
         String[] carNames = input.split(",");
-        return Arrays.stream(carNames)
-                .map(String::trim)
-                .map(Car::new)
-                .collect(Collectors.toList());
+    }
+
+    public static List<Car> responseCarNames(String[] carNames) {
+        List<String> trimmedCarNames = doTrim(carNames);
+
+        List<Car> cars = putCars(trimmedCarNames);
+
+        return cars;
+    }
+
+    public static List<Car> putCars(List<String> trimmedCarNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : trimmedCarNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+    public static List<String> doTrim(String[] carNames) {
+        List<String> trimmedCarNames = new ArrayList<>();
+        for (String carName : carNames) {
+            trimmedCarNames.add(carName.trim());
+        }
+        return trimmedCarNames;
     }
 }
