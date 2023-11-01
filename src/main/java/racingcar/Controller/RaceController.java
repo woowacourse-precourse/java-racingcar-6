05932e2f010246ee7d9de3ffe0a281aad28f9a78
masonkimseoul <masonkimseoul@gmail.com>
2023-11-01 17:11:01 +0900

    feat : Add name validating function

--- src/main/java/racingcar/Controller/RaceController.java
+++ src/main/java/racingcar/Controller/RaceController.java
@@ -12,6 +12,8 @@ public class RaceController {
     private static final String SEPARATOR = ",";
     private static final int MINIMUM_TRY_COUNT = 1;
     private static final int MINIMUM_ENTRY_COUNT = 2;
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
 
     private final String carNames;
     private final int tryCount;
@@ -21,8 +23,21 @@ public class RaceController {
         this.tryCount = tryCount;
     }
 
+    private void isValidName(String name) {
+        if(name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(
+                    "이름은 최소 "
+                            + MINIMUM_NAME_LENGTH + "자에서 최대 "
+                            + MAXIMUM_NAME_LENGTH + "자 사이로 입력하셔야 합니다.");
+        }
+    }
+
     private Cars makeCars(String carNames) {
         String[] entries = carNames.split(SEPARATOR);
+
+        for (String name : entries) {
+            isValidName(name);
+        }
 
         return new Cars(Arrays.stream(entries)
                 .map(Car::new)
