a031ce706af731b8c9c928eaf0ccdf03f32b66e3
leejuno <zhqkfxm0127@naver.com>
2023-11-01 22:22:49 +0900

    refactor(RefereeTest): 테스트 코드 재구성

--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -6,28 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RefereeTest {
+    private Referee referee;
+    private List<Car> carList;
 
-    @Test
-    @DisplayName("가장 많이 이동한 자동차(우승자)를 식볋하는 기능")
-    void identifyWinner_가장_많이_이동한_자동차를_식별하는_기능() {
-        Referee referee = new Referee();
-        List<Car> carList = new ArrayList<>();
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
         Car car1 = new Car("car1");
         Car car2 = new Car("car2");
         Car car3 = new Car("car3");
-        car1.setForwardDistance(new StringBuffer("---"));
-        car2.setForwardDistance(new StringBuffer("---"));
-        car3.setForwardDistance(new StringBuffer("--"));
-        carList.add(car1);
-        carList.add(car2);
-        carList.add(car3);
+        carList = Arrays.asList(car1, car2, car3);
+    }
+
+    @Test
+    @DisplayName("가장 많이 이동한 자동차(우승자)를 식별하는 기능")
+    void identifyWinner_가장_많이_이동한_자동차를_식별하는_기능() {
+
+        carList.get(0).setForwardDistance(new StringBuffer("---"));
+        carList.get(1).setForwardDistance(new StringBuffer("---"));
+        carList.get(2).setForwardDistance(new StringBuffer("--"));
 
         List<Car> actual = referee.identifyWinner(carList);
 
-        assertThat(actual).isEqualTo(Arrays.asList(car1, car2));
+        assertThat(actual).isEqualTo(Arrays.asList(carList.get(0), carList.get(1)));
     }
 }
