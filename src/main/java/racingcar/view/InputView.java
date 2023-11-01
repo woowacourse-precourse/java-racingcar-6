923218c2bece5cbfa0a9f6105d87753bb52a985b
yhcho0405 <yhcho0405@kakao.com>
2023-11-01 20:27:55 +0900

    refactor: 출력 메세지 상수로 정의

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,9 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String CAR_NAMES_REQUEST_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUNDS_REQUEST_MSG = "시도할 회수는 몇회인가요?";
 
     public static String getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAMES_REQUEST_MSG);
 
         String carNames = Console.readLine();
         validateCarNamesInput(carNames);
@@ -14,7 +16,7 @@ public class InputView {
     }
 
     public static int getRounds() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(ROUNDS_REQUEST_MSG);
 
         String rounds = Console.readLine();
         validateRoundsInput(rounds);
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,9 +4,11 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class OutputView {
+    private static final String RACE_START_MSG = "\n실행 결과";
+    private static final String WINNER_MSG = "최종 우승자 : ";
 
     public static void printRaceStartMessage() {
-        System.out.println("\n실행 결과");
+        System.out.println(RACE_START_MSG);
     }
 
     public static void printRoundResult(List<Car> cars) {
@@ -17,8 +19,8 @@ public class OutputView {
     }
 
     public static void printWinners(List<String> winners) {
-        String winnersString = String.join(", ", winners);
-        System.out.print("최종 우승자 : " + winnersString);
+        String winnersListString = String.join(", ", winners);
+        System.out.print(WINNER_MSG + winnersListString);
     }
 
     private static void printCarPosition(Car car) {
