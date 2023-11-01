c1b407adb465f76d69f21ef5001fc8d3ad775e19
Bellwin98 <jongseunghan@naver.com>
2023-11-01 13:20:43 +0900

    Refactor(Race): 랜덤값에 따른 자동차 전진/정지 여부 결정하는 기능을 Car 클래스로 분리

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 public class Race {
 
-    private static final int CONDITION_RANDOM_VALUE = 4;
     private final NumberGenerator numberGenerator = new NumberGenerator();
     private final InputValidation inputValidation = new InputValidation();
     private final List<Car> cars = new ArrayList<>();
@@ -26,13 +25,14 @@ public class Race {
 
         System.out.println("시도할 회수는 몇회인가요?");
         String inputTrial = Console.readLine();
-        int trial = Integer.parseInt(inputTrial); // String -> int 변경
 
-        startRace(trial); // 경주 시작
+        if (inputValidation.checkTrial(inputTrial)) {
+            int trial = Integer.parseInt(inputTrial); // String -> int 변경
+            startRace(trial); // 경주 시작
+        }
     }
 
-    private void enrollCars(String carNames) {
-
+    public void enrollCars(String carNames) {
         String[] carNamesArray = carNames.split(",");
         for (String carName : carNamesArray) {
             if (inputValidation.checkName(carName)) {
@@ -47,27 +47,13 @@ public class Race {
         System.out.println("실행 결과");
         for (int i = 0; i < trial; i++) {
             for (Car car : cars) {
-                moveOrStop(car);
+                int fuel = numberGenerator.createRandomNumber();
+                car.moveOrStop(fuel);
             }
             System.out.println();
         }
         Map<String, Integer> raceResult = scoreBoard.write(cars);
         List<String> winners = referee.findWinners(raceResult);
-
         referee.announceWinner(winners);
-    }
-
-    private void moveOrStop(Car car) {
-        // 입력한 시도 횟수에 따른 경주 진행
-        if (isAbleToMove()) {
-            car.move();
-        } else {
-            car.stop();
-        }
-    }
-
-    private boolean isAbleToMove() {
-        int randomNumber = numberGenerator.createRandomNumber();
-        return randomNumber >= CONDITION_RANDOM_VALUE;
     }
 }
