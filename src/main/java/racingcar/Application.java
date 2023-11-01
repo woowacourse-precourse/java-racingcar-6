2ed7f6ec82f02baec2061a3268d1768ced2ce840
Chris <hereokay88@gmail.com>
2023-11-01 17:49:22 +0900

    feat(racingcar): 이름 및 몇번 이동할 것인지 입력받기 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,23 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
     }
 
-    public static void askName(){
+    public static String[] askName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
 
+        String[] names = readLine().split(",");
+        return names;
     }
-    public static void askCount(){
-
+    public static int askCount(){
+        System.out.println("시도할 회수는 몇회인가요?\n");
+        int n = Integer.parseInt(readLine());
+        return n;
     }
     public static void printResult(){
 
