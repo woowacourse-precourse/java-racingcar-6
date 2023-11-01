820e0f9c172956f57d42f1c2cbece9ff681afb12
minnjung0887921 <minjeong0887921@gmail.com>
2023-11-01 20:42:17 +0900

    test : uilts/CarRaceLog 테스트 코드 작성

---
+++ src/test/java/utils/CarRaceLogTest.java
@@ -0,0 +1,42 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CarRaceLogTest {
+    @Test
+    void initCarDistanceListValidInputTest() {
+        String carNameInput = "a, b";
+        ArrayList<String> result = carRaceLog.initCarDistanceList(carNameInput);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a : ");
+        expected.add("b : ");
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void initCarDistanceListEmptyInputTest() {
+        String carNameInput = "";
+        ArrayList<String> result = carRaceLog.initCarDistanceList(carNameInput);
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void updateCarDistanceListValidInputTest() {
+        int index = 0;
+        ArrayList<String> carDistanceList = new ArrayList<>();
+        carDistanceList.add("a : ");
+        ArrayList<String> result = carRaceLog.updateCarDistanceList(index, carDistanceList);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a : -");
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
