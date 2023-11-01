7cf02cc153b87ee0073bdf1b510718b9937bdee2
plan11plan <tjtfy7028@gmail.com>
2023-11-01 19:08:06 +0900

    test:[create][Car]-자동차 이름 Null이면 예외

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -14,6 +14,11 @@ class CarTest {
         assertThat(car.getName()).isEqualTo("car1");
         assertThat(car.getPosition()).isEqualTo(0);
     }
-
+    @Test
+    @DisplayName("자동차 이름 Null이면 예외")
+    void validateNull() {
+        Car car = new Car(null);
+        assertThat(car.getName()).isEqualTo(null);
+    }
 
 }
