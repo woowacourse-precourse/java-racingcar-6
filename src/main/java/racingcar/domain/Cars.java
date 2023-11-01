a1539a4766259358be5faadbf2b7ab40450970da
KYBYK <dbqks56231106@gamil.com>
2023-11-01 19:15:27 +0900

    Feat : 전진 조건 비교, 회차별 전진 결과 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +10,24 @@ public class Cars {
 
     public void add(Car car) {
         carList.add(car);
+    }
+
+    public void moveOrStop() {
+        for(Car car : carList){
+            if(car.canMove(Randoms.pickNumberInRange(1, 9))){
+                car.move();
+            }
+        }
+    }
+
+    public String getEachRoundResult() {
+        StringBuilder result = new StringBuilder();
+        for(Car car : carList){
+            result.append(car.getName());
+            result.append(" : ");
+            result.append(car.distanceToHyphen());
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
