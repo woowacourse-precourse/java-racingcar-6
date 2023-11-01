f7b662e0b7164aabfde447a78d8ac7c4451bdf8a
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-01 20:16:15 +0900

    feat(CarManager): 모든 자동차를 전진하거나 멈추는 기능 구현

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +10,6 @@ public class CarManager {
 
     // 자동차들을 등록합니다.
     public void setCars(List<String> carNames) {
-
         if (cars == null) {
             cars = new ArrayList<Car>();
         }
@@ -17,15 +18,21 @@ public class CarManager {
             Car newCar = new Car(carName);
             cars.add(newCar);
         }
-
-        System.out.println(cars.size());
-        for(Car car : cars)
-        {
-            System.out.println(car.getName());
-        }
     }
 
-    // TODO : 자동차들을 조회합니다.
+    // 모든 자동차를 전진하거나 멈춥니다.
+    public void moveEachCar()
+    {
+        for(Car car : cars) {
+            if (Randoms.pickNumberInRange(0,9) >= 4) {
+                car.move();
+            }
+
+            Output.printRoundResults(car.getName(), car.getMoveCnt());
+        }
+
+        Output.printLine();
+    }
 
     // TODO : 우승자(가장 많이 전진한 자동차)를 구합니다.
 }
