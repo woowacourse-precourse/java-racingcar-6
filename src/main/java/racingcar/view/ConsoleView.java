13da71c807e11154b022c413ba0a10ee6eb8cbf9
imhungry2 <sjnnj@naver.com>
2023-11-01 17:28:52 +0900

    view - ConsoleView 구현

---
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class ConsoleView {
+    public void printWelcomeMessage() {
+        // 환영 메시지 출력
+    }
+
+    public String getInputCarNames() {
+        // 자동차 이름 입력 받기
+        return null;
+    }
+
+    public int getInputTryCount() {
+        // 시도 횟수 입력 받기
+        return 0;
+    }
+
+    public void printRoundResults(List<Car> cars) {
+        // 한 라운드의 결과 출력
+    }
+
+    public void printWinners(List<String> winners) {
+        // 우승자 출력
+    }
+}
