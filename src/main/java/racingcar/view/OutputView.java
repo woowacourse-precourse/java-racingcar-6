ef46f43ba087de9e85a9ff0b44b8ce3c891d1f66
hoyeon <highoption@naver.com>
2023-11-01 10:59:18 +0900

    refactor(OutputView) : printOneGameResult, printWinner 코드 수정
    - 가독성을 위해 repeat함수를 사용
    - 기존 StringBuilder를 사용하던 것을 문자열 결합으로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,6 @@
 package racingcar.view;
 
 import java.util.List;
-import java.util.stream.IntStream;
 import racingcar.model.Cars;
 
 public class OutputView {
@@ -12,16 +11,13 @@ public class OutputView {
     public static void printOneGameResult(Cars cars) {
         cars.getCars().forEach(car -> {
             System.out.print(car.getName() + " : ");
-            IntStream.range(0, car.getDistance()).forEach(i -> System.out.print("-"));
-            System.out.println();
+            System.out.println("-".repeat(car.getDistance()));
         });
         System.out.println();
     }
 
     public static void printWinners(List<String> winners) {
-        StringBuilder result = new StringBuilder();
         String winner = String.join(",", winners);
-        result.append("최종 우승자 : ").append(winner);
-        System.out.println(result);
+        System.out.println("최종 우승자 : " + winner);
     }
 }
