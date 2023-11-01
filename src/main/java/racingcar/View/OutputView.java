c02dd25d8e08c3ff51ff1186389735326f8951dd
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-01 10:41:37 +0900

    [feat] View(OutputView) 작성

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,4 +1,27 @@
 package racingcar.View;
 
+import java.util.List;
+
 public class OutputView {
+
+    public static void gameResultMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printMovementIndicator(String name, int movement) {
+        StringBuilder indicator = new StringBuilder();
+        for (int i = 0; i < movement; i++) {
+            indicator.append("-");
+        }
+        System.out.println(name + " : " + indicator.toString());
+    }
+
+    public static void printSelectWinner(List<String> winners) {
+        if (winners.size() == 1) {
+            System.out.println("최종 우승자 : " + winners.get(0));
+        }
+        if (winners.size() > 1) {
+            System.out.println("최종 우승자 : " + String.join(", ", winners));
+        }
+    }
 }
