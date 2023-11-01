072343a6c94d5f1d00a1b87be4a53379cf7b9fd6
dabin <db021129@naver.com>
2023-11-01 11:05:24 +0900

    feat: Output의 printCarStatus메소드와 printWinner메소드 생성

---
+++ src/main/java/racingcar/service/Output.java
@@ -0,0 +1,36 @@
+package racingcar.service;
+
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Output {
+
+    private void printCarStatus(Car car, int index) {
+        String carName = car.carName.get(index);
+        int carDistance = car.carDistance[index];
+
+        System.out.println(String.format("%s : %d", carName, carDistance));
+    }
+
+    private void printWinner(Car car) {
+        int maxCarDistance = Arrays.stream(car.carDistance).max().getAsInt();
+        List<String> winner = new ArrayList<>();
+
+        System.out.print("최종 우승자 : ");
+
+        for (String carName : car.carName) {
+            int carNameIndex = car.carName.indexOf(carName);
+
+            if (maxCarDistance == carNameIndex) {
+                winner.add(carName);
+            }
+        }
+
+        String result = winner.stream().collect(Collectors.joining(", "));
+        System.out.print(result);
+    }
+}
