f238fbb55c00b9a8aa297a542e84b2331f46c3b7
jiwon1217 <koagjw@gmail.com>
2023-11-01 11:22:22 +0900

    :recycle: refactor: 정수형 상수 분리

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -3,7 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
+
     public static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
     }
 }
--- src/main/java/racingcar/TypeConverter.java
+++ src/main/java/racingcar/TypeConverter.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TypeConverter {
+    private static final int INIT_POSITION = 0;
+
     public static List<Car> convertStringArrayToCarList(String[] carNames) {
         return Arrays.stream(carNames)
-                .map(name -> new Car(name, 0))
+                .map(name -> new Car(name, INIT_POSITION))
                 .toList();
     }
 }
