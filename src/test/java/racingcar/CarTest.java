3a67e36286e6e7836333b3ca28c27f1acdf1c647
jiixon <jr0107380@gmail.com>
2023-11-01 22:51:26 +0900

    test: 랜덤값_4_이상일_경우_전진 코드 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -12,4 +12,17 @@ public class CarTest {
         Assertions.assertThat(result).isEqualTo("jiixon");
     }
 
+    @Test
+    void 랜덤값_4_이상일_경우_전진(){
+        Car car1 = new Car("aaa");
+        Car car2 = new Car("bbb");
+
+        car1.moveCar(4);
+        car2.moveCar(3);
+
+        Assertions.assertThat(car1.getScore()).isEqualTo(1);
+        Assertions.assertThat(car2.getScore()).isEqualTo(0);
+
+    }
+
 }
