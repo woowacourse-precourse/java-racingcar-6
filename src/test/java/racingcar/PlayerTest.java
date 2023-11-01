12d6588428e87d936d1da592c3985d1ad5cc8d2e
Jiwon Kwon <kjiwon411@gmail.com>
2023-11-01 10:04:44 +0900

    fix(racingcar): 입력한 값을 쉽표가 없는 배열로 반환하는 기능 테스트 수정

--- src/test/java/racingcar/PlayerTest.java
+++ src/test/java/racingcar/PlayerTest.java
@@ -10,14 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
     @Test
-    @DisplayName("입력 값을 , 구분자로 구분하고 다른 구분자가 주어진 경우 예외 처리하는지 확인")
+    @DisplayName("입력 값을 , 구분자로 구분하고 반환된 리스트 원소에 , 없는지 확인")
     void testSplitCarNames() {
         Player cars = new Player();
         String testInput = "Tom, tyla, Judy";
-        String[] testInputCarList = testInput.replace(" ", "").split(",");
 
         ArrayList<String> userInputCarList = cars.createPlayerCarList(testInput);
-        assertThat(userInputCarList).containsExactly(testInputCarList);
+        assertThat(userInputCarList).contains("Tom", "tyla", "Judy");
 
     }
 }
