854533e88d17e1b2281a2b3ee9c1c9473ab18b3a
sunwoong <sunwoong1122@gmail.com>
2023-11-01 19:26:33 +0900

    feat: 실행 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,9 @@ package racingcar.view;
 
 import racingcar.model.Car;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
     public void printCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,9 +14,14 @@ public class OutputView {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public void printExecutionResult() {
+        System.out.println("\n실행 결과");
+    }
+
     public void printExecutionResults(Car car) {
-        // TODO
-        System.out.println();
+        List<String> names = car.getNames();
+        Map<String, Integer> distance = car.getDistance();
+        names.forEach(name -> System.out.println(name + " : " + "-".repeat(distance.get(name))));
     }
 
     public void printResults(Car car) {
