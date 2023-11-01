db7d0f98d95b3c10ff987cf2da41a097553a1186
soyoungxxx <otherplet@gmail.com>
2023-11-01 20:50:52 +0900

    fix(PlayingService): Car 클래스 관련 내용 수정

--- src/main/java/racingcar/domain/PlayingService.java
+++ src/main/java/racingcar/domain/PlayingService.java
@@ -3,11 +3,10 @@ package racingcar.domain;
 import java.util.Map;
 import java.util.HashMap;
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.domain.Car;
 
 public class PlayingService {
-    private Car carsName;
     private Number number;
+    Map<String, Integer> playerAndScore = new HashMap<>();
 
     public PlayingService() {
         String names = Console.readLine();
@@ -19,11 +18,9 @@ public class PlayingService {
 
     void enterName(String names) {
         String[] name = names.split(",");
-        Map<String, Integer> playerAndScore = new HashMap<>();
         for (String s : name) {
             s = s.strip();
             playerAndScore.put(s, 0);
         }
-        carsName = new Car(playerAndScore);
     }
 }
