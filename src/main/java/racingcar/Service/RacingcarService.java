656cd05ba5e5a68951c22d04c6e19eeb74bd640f
S2Hyeon <tmdgus908@naver.com>
2023-11-01 16:19:41 +0900

    :sparkles: feat : 각 차수별 실행 결과 출력 구현

--- src/main/java/racingcar/Service/RacingcarService.java
+++ src/main/java/racingcar/Service/RacingcarService.java
@@ -8,6 +8,7 @@ import racingcar.Util.MoveConditionUtil;
 public class RacingcarService {
     private final GameStatus gameStatus = new GameStatus();
     private final MoveConditionUtil moveConditionUtil = new MoveConditionUtil();
+
     public void initailizeGame(List<Car> carList, int goalRound) {
         gameStatus.setCarList(carList);
         gameStatus.setGoalRound(goalRound);
@@ -20,13 +21,25 @@ public class RacingcarService {
 
     public String move(List<Car> carList) {
         StringBuilder sb = new StringBuilder();
-        for(Car car : carList) {
-            if(moveConditionUtil.checkMoveCondition()) {
+        for (Car car : carList) {
+            if (moveConditionUtil.checkMoveCondition()) {
                 updateCarStatus(car);
             }
             gameStatus.setMaxCount(car.getMovedCount());
 
             sb.append(car.getCarName()).append(" : ").append(car.getMovedStatus()).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public String getRoundResult() {
+        List<Car> carList = gameStatus.getCarList();
+        int goalRound = gameStatus.getGoalRound();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < goalRound; i++) {
+            sb.append(move(carList)).append("\n");
         }
 
         return sb.toString();
---
+++ src/main/java/racingcar/View/RoundResultView.java
@@ -0,0 +1,9 @@
+package racingcar.View;
+
+public class RoundResultView {
+    public void printRoundResult(String roundResult) {
+        System.out.println();
+        System.out.println("실행 결과");
+        System.out.println(roundResult);
+    }
+}
