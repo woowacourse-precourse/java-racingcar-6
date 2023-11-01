01bbf357b91f4bf854a2cf83506581f36004eb36
CJ <choijun0627@konkuk.ac.kr>
2023-11-01 17:33:43 +0900

    Feat : 출력 메시지 구현
    게임 관련 출력 메시지 상수 생성
    게임 관련 출력 메시지 출력하는 메소드 생성

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
     public OutputView() {
     }
 
@@ -25,5 +30,21 @@ public class OutputView {
             carMovingCountString += "-";
         }
         return carMovingCountString;
+    }
+
+    public void printNameMessage() {
+        System.out.println(NAME_MESSAGE);
+    }
+
+    public void printCountMessage() {
+        System.out.println(COUNT_MESSAGE);
+    }
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printWinnerMessage() {
+        System.out.println(WINNER_MESSAGE);
     }
 }
