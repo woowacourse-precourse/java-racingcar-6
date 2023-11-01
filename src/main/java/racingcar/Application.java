26e9c311318dceea8582b0af8bad30498abe38e7
hwangseonheon <dolphin33@jbnu.ac.kr>
2023-11-01 19:45:45 +0900

    feat: 시도 회수의 범위에 대한 예외 처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,7 @@ public class Application {
         final String INPUT_CAR_NAMES_COMMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
         final String INPUT_TRY_COUNT_COMMENT = "시도할 회수는 몇회인가요?";
         final String EXECUTION_RESULT_COMMENT = "실행 결과";
+        final String TRYCOUNT_RANGE_EXCEPTION_COMMENT = "시도 가능한 회수의 범위를 넘어섰습니다.";
 
         List<RacingCar> racingCars = new ArrayList<>();
         Referee referee = new Referee();
@@ -27,6 +28,9 @@ public class Application {
 
         System.out.println(INPUT_TRY_COUNT_COMMENT);
         int tryCount = Integer.parseInt(Console.readLine());
+        if(tryCount < 1 || tryCount > Integer.MAX_VALUE) {
+          throw new IllegalArgumentException(TRYCOUNT_RANGE_EXCEPTION_COMMENT);
+        }
 
         System.out.println(EXECUTION_RESULT_COMMENT);
         while(tryCount-- > 0) {
