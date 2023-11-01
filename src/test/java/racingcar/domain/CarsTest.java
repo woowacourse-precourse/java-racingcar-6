1f7bcd6b40d40554fb75150288d86bfbf270fb2a
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 19:02:01 +0900

    test : 우승자 반환 기능 테스트 작성 완료

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,6 +3,9 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -23,7 +26,7 @@ class CarsTest {
         assertEquals(3, cars.cars.size());
     }
     @Test
-    void 랜덤_숫자_받아서_car_클래스_접근(){
+    void 랜덤_숫자_받아서_car_클래스_접함(){
         //given
         Car mockCar1 = mock(Car.class);
         Car mockCar2 = mock(Car.class);
@@ -37,6 +40,29 @@ class CarsTest {
         //then
         verify(mockCar1, times(1)).goForward(anyInt());
         verify(mockCar2, times(1)).goForward(anyInt());
+
+    }
+    @Test
+    void 우승자_리스트_얻기(){
+        //given
+        cars.getCars().get(0).goForward(1);
+        cars.getCars().get(1).goForward(7);
+        cars.getCars().get(2).goForward(3);
+
+        //when
+        List<String> mvp = cars.getMVP();
+
+        //then
+        assertThat(mvp).containsExactly("jun");
+    }
+
+    @Test
+    void 전진_상태_얻기(){
+
+    }
+
+    @Test
+    void 리스트_얻기(){
 
     }
 
