cb6857049f352141571cc8ab6217091528e66542
mini1115 <kmini9611@naver.com>
2023-11-01 14:43:53 +0900

    feat : 메인 함수 게임 실행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.domain.Game;
+import racingcar.domain.PlayerInput;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayerInput player = new PlayerInput();
+        Map<String, Integer> racers = player.nameInput();
+        int time = player.playTimeInput();
+        Game game = new Game();
+        System.out.println("\n실행결과");
+        for (int i = 0; i < time; i++) {
+            game.printResult(racers);
+        }
+        System.out.print("최종 우승자 : " + String.join(", ", game.printWinner(racers)));
+//        System.out.println(racers);
     }
 }
+
+
