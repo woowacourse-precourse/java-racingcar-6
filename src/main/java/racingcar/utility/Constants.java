3babfe2a6e56c40fec0d6ed42b8117f242362b2e
“sangjun121” <juncho12011201@gmail.com>
2023-11-01 12:29:04 +0900

    feat: 레이싱 게임 결과 값 출력시 반복되는 문자 상수 선언완료
    콜론(:)과 대시(-)는 결과 값 출력할 때마다 반복적으로 사용하므로 상수 선언완료

--- src/main/java/racingcar/utility/Constants.java
+++ src/main/java/racingcar/utility/Constants.java
@@ -17,6 +17,10 @@ public final class Constants {
     public static final String CAR_NAME_INPUT_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String RACING_COUNT_INPUT_PROMPT_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String EXECUTION_RESULT = "\n실행 결과";
+    public static final String COLON = " : ";
+    public static final String ADVANCEMENT = "-";
+
+
 
 
 }
