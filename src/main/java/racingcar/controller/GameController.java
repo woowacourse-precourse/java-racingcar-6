5e084df1f64502f84060304b7fe0342db6353373
Annjieun <dkslwdms2121@naver.com>
2023-11-01 12:46:56 +0900

    refactor(GameController): 레이싱 게임을 진행하는 GameController 추가

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,36 @@
+package racingcar.controller;
+
+import racingcar.Utils;
+import racingcar.domain.Attempt;
+import racingcar.domain.Cars;
+import racingcar.domain.Game;
+import racingcar.view.OutputView;
+
+public class GameController {
+    private Cars cars;
+    private Attempt attempt;
+    private Game game;
+
+    public GameController() {
+        cars = new Cars();
+        attempt = new Attempt();
+    }
+
+    public void startRacing() {
+        initialize();
+        playRacing();
+    }
+
+    public void initialize() {
+        cars.createCars();
+        attempt.saveAttemptNumber();
+        Utils.printLineChanging();
+
+        game = new Game(cars.getCars(), attempt.getAttemptNumber());
+    }
+
+    public void playRacing() {
+        OutputView.printResultMessage();
+        game.startRacing();
+    }
+}
