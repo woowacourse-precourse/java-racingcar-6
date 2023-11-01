9e64ffbe75054d9bb329842aea2bbf5d01025a3b
ghdeo <goldencorn7@naver.com>
2023-11-01 22:38:38 +0900

    feat: Add input validation

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,8 +1,11 @@
 package racingcar.service;
 
 import racingcar.domain.Cars;
+import racingcar.validate.RaceValidator;
 
 import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class RacingService {
     private final Scanner sc = new Scanner(System.in);
@@ -19,14 +22,17 @@ public class RacingService {
     private void setCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        String playerInput = sc.nextLine();
+        String playerInput = readLine();
         String[] names = playerInput.split(",");
+
+        RaceValidator.validateCarNames(names);
+
         cars = new Cars(names);
     }
 
     private void setTrial() {
         System.out.println("시도할 회수는 몇회인가요?");
-        this.trial = sc.nextInt();
+        this.trial = RaceValidator.validateTrials(readLine());
     }
 
     private void race() {
