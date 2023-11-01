d9c1f099ca25828d20a21cb99aa45970ed0fbdc9
sangxxjin <sj_0368@naver.com>
2023-11-01 14:05:58 +0900

    feat: 차동의 이동거리를 실시간으로 나타내주기 위한 메소드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,4 +6,12 @@ public class OutputView {
     public static void showResultMessage() {
         System.out.println(RESULTMESSAGE);
     }
+
+    public static void showCarDistanceResults(String name, String distance) {
+        System.out.print(name + " : ");
+        for (int i = 0; i < distance.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
