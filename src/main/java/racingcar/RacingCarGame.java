8264a06e306abbadb20c44698492f1f59c779c86
JayM-Oh <277ipjk53@gmail.com>
2023-11-01 20:21:29 +0900

    chore: GamaManager

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,32 +1,76 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarGame {
-    public static void play(List<String> carNames, int tryCount){
-        for (int i = 0; i < tryCount; i++){
-            printRoundResult(i + 1, carNames);
+
+    private static List<String> carStatus;
+
+    public static void play(List<String> carNames, int tryCount) {
+        initializeCarStatus(carNames);
+
+        for (int i = 0; i < tryCount; i++) {
+            playOneRound(carNames);
+        }
+
+        printFinalResult(carStatus, carNames);
+    }
+
+    private static void initializeCarStatus(List<String> carNames) {
+        carStatus = new ArrayList<>(carNames.size());
+        for (int i = 0; i < carNames.size(); i++) {
+            carStatus.add("");
         }
     }
 
-    public static void printRoundResult(int round, List<String> carNames){
-        System.out.println("\n" + round + "회 경주 결과:");
-
-        for (String carName : carNames){
-            printCarMove(carName);
-        }
-        System.out.println();
+    private static void playOneRound(List<String> carNames) {
+        printRoundResult(carNames);
     }
 
-    private static void printCarMove(String carName){
-        if(Randoms.pickNumberInRange(0,9) >= 4){
-            System.out.print(carName + " : -");
+    private static void printRoundResult(List<String> carNames) {
+        System.out.print("\n" + "회 경주 결과:");
+
+        for (int i = 0; i < carStatus.size(); i++) {
+            updateAndPrintCarStatus(i, carNames);
+        }
+    }
+
+    private static void updateAndPrintCarStatus(int index, List<String> carNames) {
+        updateCarStatus(index, carNames);
+        System.out.println(carNames.get(index) + " : " + carStatus.get(index));
+    }
+
+    private static void updateCarStatus(int index, List<String> carNames) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            carStatus.set(index, carStatus.get(index) + "-");
         } else {
-            System.out.print(carName + " : ");
+            carStatus.set(index, carStatus.get(index) + " ");
         }
+    }
+
+    private static void printFinalResult(List<String> carStatus, List<String> carNames) {
+        System.out.print("\n최종 우승자 : ");
+        List<String> winners = findWinners(carStatus, carNames);
+        System.out.println(String.join(", ", winners));
+    }
+
+    private static List<String> findWinners(List<String> carStatus, List<String> carNames) {
+        int maxDistance = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (int i = 0; i < carStatus.size(); i++) {
+            int distance = carStatus.get(i).replace(" ", "").length();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                winners.clear();
+                winners.add(carNames.get(i));
+            } else if (distance == maxDistance) {
+                winners.add(carNames.get(i));
+            }
+        }
+
+        return winners;
     }
 }
-
-
