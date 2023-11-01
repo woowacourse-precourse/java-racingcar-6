6dc20e881e633d8d0e06698725cf8fe4ed6667db
icebear0111 <wltjq0901@naver.com>
2023-11-01 20:00:26 +0900

    refactor: start 메서드만 호출 하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameManager manager = new GameManager();
-        
+
         manager.start();
     }
 }
