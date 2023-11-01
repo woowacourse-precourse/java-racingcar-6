8f72057280c143d82250eb30e38b628f85cfba38
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:37:06 +0900

    feat: 게임 시작 시 시작 문구 출력

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,8 +1,14 @@
 package racingcar;
 
+import racingcar.messages.Message;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
     private final List<Car> cars = new ArrayList<>();
+
+    public void start() {
+        System.out.println(Message.GAME_START_NAME);
+    }
 }
