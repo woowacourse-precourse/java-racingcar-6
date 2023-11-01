d6c8fd2304c8f0564a23dfc91c085a29aed8d156
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-01 11:57:22 +0900

    feat: Application 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+
 public class Application {
+
+    private static final RacingCarController racingCarController = new RacingCarController();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        racingCarController.racingCar();
     }
 }
