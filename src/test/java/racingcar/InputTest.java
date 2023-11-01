7a30bd6379d26ddc9b626485d603e4ec8ebd1d3c
leorb159 <ldgsocial159@naver.com>
2023-11-01 20:37:48 +0900

    test(InputTest): 문자열 숫자 반환 및 예외 테스트

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -29,4 +29,23 @@ public class InputTest {
         assertThat(result).contains("fume", "mist");
         assertThat(result).containsExactly("mist", "fume");
     }
+
+    @Test
+    void 입력받은_문자열_숫자_반환_테스트() {
+        final String input = "5";
+        Input testInput = new Input();
+        int result = testInput.getTryCount(input);
+
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void 숫자가_아닌_문자열_예외_테스트() {
+        final String input = "six";
+        final Input testInput = new Input();
+
+        assertThatThrownBy(() -> testInput.getTryCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자를 입력해야 합니다.");
+    }
 }
