0222fd4e945de8c964741d3addcac2b4efc232e1
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-01 20:51:59 +0900

    refactor: 출력 메세지 enum으로 관리하기 위해 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.gameInfo.InputCars;
 import racingcar.domain.gameInfo.InputManager;
 import racingcar.domain.gameInfo.InputProgressCount;
+import racingcar.utils.GameMessage;
 
 public class InputView {
-    private final static String GAME_START_COMMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final static String INPUT_TRY_COUNT = "시도할 횟수는 몇회인가요?";
 
     public static InputManager startGameInput() {
         InputCars inputCars = new InputCars(inputCarList());
@@ -17,12 +16,12 @@ public class InputView {
     }
 
     private static String inputCarList() {
-        System.out.println(GAME_START_COMMENT);
+        System.out.println(GameMessage.GAME_START_COMMENT.getMessage());
         return Console.readLine();
     }
 
     private static String inputGameProcessCount() {
-        System.out.println(INPUT_TRY_COUNT);
+        System.out.println(GameMessage.INPUT_TRY_COUNT.getMessage());
         return Console.readLine();
     }
 
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,13 +2,11 @@ package racingcar.view;
 
 import racingcar.domain.car.Car;
 import racingcar.domain.dto.CarsDTO;
+import racingcar.utils.GameMessage;
 
 public class OutputView {
-    private static String ONE_TURN_MESSAGE = "실행 결과";
-    private static String ANNOUNCE_WINNER = "최종 우승자 : ";
-
     public static void gameStartMessage() {
-        System.out.println(ONE_TURN_MESSAGE);
+        System.out.println(GameMessage.ONE_TURN_MESSAGE.getMessage());
     }
 
     public static void oneTurnResult(CarsDTO carsDTO) {
@@ -19,6 +17,6 @@ public class OutputView {
     }
 
     public static void announceWinner(String winnerList) {
-        System.out.print(ANNOUNCE_WINNER + winnerList);
+        System.out.print(GameMessage.ANNOUNCE_WINNER.getMessage() + winnerList);
     }
 }
