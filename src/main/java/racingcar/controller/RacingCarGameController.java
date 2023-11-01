d7c822bdf274984cecf1507a1b2dbf7ce5583861
Hwang In Tae <sjobs66@gmail.com>
2023-11-01 16:33:28 +0900

    feat: race 구현

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,15 +1,22 @@
 package racingcar.controller;
 
 import racingcar.model.Game;
-import racingcar.view.GameView;
+import racingcar.view.RepeatingGameView;
 
 public class RacingCarGameController implements GameController {
     private final Game game;
-    private final GameView gameView;
+    private final RepeatingGameView gameView;
     private boolean playGame;
 
-    public RacingCarGameController(Game game, GameView gameView) {
+    public RacingCarGameController(Game game, RepeatingGameView gameView) {
         this.game = game;
         this.gameView = gameView;
+    }
+
+    public void race() {
+        while (game.continues()) {
+            game.play(null);
+            gameView.showProgressOf(game);
+        }
     }
 }
