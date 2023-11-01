5deb288aefd936e38178e3933ae2e78611c01ca5
jihun-24k <gi020477@gmail.com>
2023-11-01 13:33:17 +0900

    test: 자동차 이름 예외 테스트

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,11 @@ class RacingCarTest {
         RacingCar racingCar = new RacingCar("jihun");
 
         assertEquals("jihun", racingCar.getName());
+    }
+
+    @Test
+    void 자동차_이름은_5자이하_예외_처리() {
+        assertThatThrownBy(() -> new RacingCar("jihun_woowa"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
