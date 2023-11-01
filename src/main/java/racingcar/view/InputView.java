2e6577aad64ff7348caa76c4cc9b3583d6e482d0
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 23:23:05 +0900

    feat : - 게임 시도 횟수를 입력하는 메서드 추가 - 입력값이 숫자인지 검증하는 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,26 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import racingcar.enums.Exceptions;
+import racingcar.exception.TryNumberTypeException;
 import racingcar.util.Splitter;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public static String[] inputCarNames() {
-        return Splitter.splitName(Console.readLine());
+        return Splitter.splitName(readLine());
+    }
+
+    public static Integer inputTryNumber() {
+        String inputNumber = readLine();
+        validateInputIsNumber(inputNumber);
+        return Integer.parseInt(inputNumber);
+    }
+
+    private static void validateInputIsNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (IllegalArgumentException e) {
+            throw new TryNumberTypeException(Exceptions.TRY_NUMBER_IS_INTEGER.getMessage());
+        }
     }
 }
