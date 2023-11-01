76012eb348b62d172d379320e717434e84841787
Zeus6768 <akaps6768@gmail.com>
2023-11-01 10:01:26 +0900

    docs(CarMoveRuleConfig): 사용방법 주석 추가

--- src/main/java/racingcar/config/CarMoveRuleConfig.java
+++ src/main/java/racingcar/config/CarMoveRuleConfig.java
@@ -3,6 +3,38 @@ package racingcar.config;
 import racingcar.model.car.CarMoveRule;
 import racingcar.model.car.RandomCarMoveRule;
 
+/**
+ * 예제
+ *
+ * <p>CarMoveRule의 구현 클래스를 작성합니다.
+ * <pre>
+ * {@code AlwaysCarStopRule.java}
+ *
+ * public class AlwaysCarStopRule implements CarMoveRule {
+ *     {@Override}
+ *     public boolean isGo() { return false; }
+ * }
+ * </pre>
+ *
+ * <p>CarMoveRuleConfig에 클래스 변수를 추가하고 싱글톤 메소드를 작성합니다.
+ * <pre>
+ * {@code CarMoveRuleConfig.java}
+ *
+ * private static CarMoveRule alwaysCarStopRule;
+ *
+ * private static CarMoveRule getAlwaysCarStopRule() {
+ *     if (alwaysCarStopRule == null) {
+ *         alwaysCarStopRule = new AlwaysCarStopRule();
+ *     }
+ *     return alwaysCarStopRule;
+ * }
+ * </pre>
+ *
+ * <p>{@code setDefault()} 메소드를 사용해 기본 규칙을 설정합니다.
+ * <pre>
+ * private static void setDefault() { currentRule = getAlwaysCarStopRule(); }
+ * </pre>
+ */
 public class CarMoveRuleConfig {
 
     private static CarMoveRule currentRule;
