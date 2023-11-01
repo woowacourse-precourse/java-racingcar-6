6088134dc5cc215847f93f6e45a6a298d5d8fcdc
주은 <00-10-21@naver.com>
2023-11-01 22:24:09 +0900

    feat: Implement attempts input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,5 +15,8 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = scanner.nextLine();
         validateCarNames(carNamesInput);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int attempts = scanner.nextInt();
     }
 }
