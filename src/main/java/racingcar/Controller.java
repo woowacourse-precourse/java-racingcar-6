52351217214f97c0cf973cd29e1d79523d8452f9
jagarchi <relong1026@naver.com>
2023-11-01 20:20:06 +0900

    시도 횟수 입력받기

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,12 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+public class Controller {
+    public int tryNumber(){
+        String tryNumber = Console.readLine();
+        return Integer.parseInt(tryNumber);
+    }
+}
+
+
+
+
