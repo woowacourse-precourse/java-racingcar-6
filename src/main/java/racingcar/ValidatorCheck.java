7a55e28218c2a621b1d3067da9b068069fe6a2d0
jnjongjeon <jeonij23@gmail.com>
2023-11-01 23:53:21 +0900

    feat: 자동차 경주

---
+++ src/main/java/racingcar/ValidatorCheck.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import java.util.List;
+
+public class ValidatorCheck {
+
+    public void inputOnlyNumber(int playRound) {
+        if (!(playRound > 0)) {
+            throw new IllegalArgumentException("0이상의 정수를 입력해주세요.");
+        }
+    }
+
+    public void namingRacingCar_lengthCheck(List<String> players) {
+
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).length() > 5) {
+                throw new IllegalArgumentException("플레이어 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    public void namingRacingCar_check(String inputPlayers) {
+        int count = 0;
+        for (int i = 0; i < inputPlayers.length(); i++) {
+            if (inputPlayers.contains(",")) {
+                count++;
+            }
+        }
+
+        if (count != 2) {
+            throw new IllegalArgumentException("이름 구분은 쉼표로 합니다. ',' 를 넣어 입력해주세요");
+        }
+    }
+}
