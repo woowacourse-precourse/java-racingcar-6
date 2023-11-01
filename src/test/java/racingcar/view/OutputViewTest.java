d5274553300a3f028741911a8087f34b015b1686
dbstn1325 <dbstn1325@naver.com>
2023-11-01 15:40:34 +0900

    test(OutputViewTest): 자동차 경주 실행결과를 출력하는 기능 테스트
    - 자동차 로직의 반복적인 생성 방식을 @BeforeEach를 적용해 자동화 시켜보았습니다.
    - 단, 문구를 출력할 때에는 자동차 생성이 불필요하기 떄문에 이를 위해 testInfo를 적용해보았습니다.

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,13 +1,38 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import domain.Car;
+import domain.Cars;
+import domain.RaceResult;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import view.OutputView;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 public class OutputViewTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    private Cars cars;
+    private Car yoonCar;
+    private Car suCar;
+
+    @BeforeEach
+    void setUp(TestInfo testInfo) {
+        if(testInfo.getDisplayName().contains("문구")) {
+            return ;
+        }
+
+        String[] carNames = {"yoon", "su"};
+        cars = new Cars(carNames);
+        yoonCar = cars.getCars().get(0);
+        suCar = cars.getCars().get(1);
+    }
 
     @Test
     void 자동차_이름_입력_문구를_출력하는_기능_테스트() {
@@ -42,6 +67,25 @@ public class OutputViewTest extends NsTest {
         });
     }
 
+    @Test
+    void 자동차_경주_실행_결과를_출력하는_기능_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            //when
+            cars.moveAheadCars();
+            cars.moveAheadCars();
+            RaceResult yoonRaceResult = yoonCar.generateResult();
+            RaceResult suRaceResult = suCar.generateResult();
+
+            OutputView.printRaceResult(yoonRaceResult);
+            OutputView.printRaceResult(suRaceResult);
+
+            //then
+            Assertions.assertThat(output()).contains("yoon : --", "su : -");
+            },
+            MOVING_FORWARD, STOP,
+            MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
 
 
     @Override
