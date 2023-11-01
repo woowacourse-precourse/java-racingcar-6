1791f51cde2765e120fd509a0bc30eed963f6eaf
박은지 <qkrdmswl1018@naver.com>
2023-11-01 14:12:50 +0900

    refactor: 생성자를 통해 주입받도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,25 @@
 package racingcar;
 
+import racingcar.evaluator.WinnerEvaluator;
+import racingcar.game.Movement;
 import racingcar.game.RacingGame;
+import racingcar.printer.ResultPrinter;
+import racingcar.processor.CarDataProcessor;
+import racingcar.processor.Request;
+import racingcar.util.Generator;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
+        Request request = new Request();
+        Generator generator = new Generator();
+        WinnerEvaluator evaluator = new WinnerEvaluator();
+
+        CarDataProcessor processor = new CarDataProcessor(request);
+        Movement movement = new Movement(generator);
+        ResultPrinter printer = new ResultPrinter(evaluator);
+
+        RacingGame racingGame = new RacingGame(processor, movement, printer);
         racingGame.run();
     }
 }
--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -3,11 +3,8 @@ package racingcar.game;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.message.Console;
-import racingcar.evaluator.WinnerEvaluator;
 import racingcar.printer.ResultPrinter;
 import racingcar.processor.CarDataProcessor;
-import racingcar.util.Generator;
-import racingcar.processor.Request;
 
 public class RacingGame {
 
@@ -15,13 +12,10 @@ public class RacingGame {
     private final Movement movement;
     private final ResultPrinter printer;
 
-    public RacingGame() {
-        Request request = new Request();
-        Generator generator = new Generator();
-        WinnerEvaluator evaluator = new WinnerEvaluator();
-        this.processor = new CarDataProcessor(request);
-        this.movement = new Movement(generator);
-        this.printer = new ResultPrinter(evaluator);
+    public RacingGame(CarDataProcessor processor, Movement movement, ResultPrinter printer) {
+        this.processor = processor;
+        this.movement = movement;
+        this.printer = printer;
     }
 
     public void run() {
