f7af2188afd23f10a66f682b6a882066b825a863
Jyebin <yebin0322@naver.com>
2023-11-01 19:52:21 +0900

    Feat : 모든 기능 구현 완료
    - winner를 구하고 결과를 출력하는 기능 구현 완료
    - 레이스를 진행하는 기능 구현 완료
    - 레이스의 결과를 출력하는 메소드(진행 결과)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+
+    public static void main(String[] args) {
+        String[] carName = inputCars();
+        int tryCount = tryCnt();
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryCount; i++) {
+            isMove(carName);
+        }
+
+        printResult(carName);
+    }
+
     public static String[] inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
@@ -25,9 +37,9 @@ public class Application {
         return carNames;
     }
 
-    public static boolean isOnlyAlphabet(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            char carName = str.charAt(i);
+    public static boolean isOnlyAlphabet(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char carName = input.charAt(i);
             if (!(carName >= 'A' && carName <= 'Z') && !(carName >= 'a' && carName <= 'z')) {
                 return false;
             }
@@ -35,20 +47,53 @@ public class Application {
         return true;
     }
 
-    public static int tryTime(){
+    public static int tryCnt() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
-    //전진 조건 함수
-    public static boolean isGo(){
-        int randomNum = Randoms.pickNumberInRange(0,9);
-        if(randomNum>=4){
+    public static boolean isGo() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
+    public static void isMove(String[] carName) {
+        for (int i = 0; i < carName.length; i++) {
+            if (isGo()) {
+                System.out.print(carName[i] + " : -");
+            } else {
+                System.out.print(carName[i] + " : ");
+            }
+        }
+        System.out.println();
+    }
 
+    public static int getWinner(String[] carName) {
+        int biggestAdvance = 0;
+        for (int i = 0; i < carName.length; i++) {
+            if (biggestAdvance < carName[i].length()) {
+                biggestAdvance = carName[i].length();
+            }
+        }
+        return biggestAdvance;
+    }
+
+    public static void printResult(String[] carName) {
+        int biggestAdvance = getWinner(carName);
+        StringBuilder winners = new StringBuilder();
+
+        for (int i = 0; i < carName.length; i++) {
+            if (biggestAdvance == carName[i].length()) {
+                if (winners.length() > 0) {
+                    winners.append(", ");
+                }
+                winners.append(carName[i]);
+            }
+        }
+        System.out.println("최종 우승자 : " + winners);
+    }
 }
