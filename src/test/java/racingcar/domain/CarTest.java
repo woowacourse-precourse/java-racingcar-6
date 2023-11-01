b07e9b4154ba612ca5a881ed2380767d24b830b7
elephantLikesApple <elephant890@naver.com>
2023-11-01 23:11:57 +0900

    test : 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -6,9 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.Car;
 
 public class CarTest {
+    private static final int CRITICAL_OF_MOVING = 4;
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "woni", "jun", "abcde", "racer", "0123", "%45", "s23", "7df"})
@@ -22,5 +22,20 @@ public class CarTest {
     void ofExceptionTest() {
         assertThatThrownBy(() -> Car.of("invaildName"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "4"})
+    void moveOrStayTest(String input) {
+        int randomNumber = Integer.parseInt(input);
+        Car racer = Car.of("pobi");
+
+        racer.moveOrStay(randomNumber);
+
+        if (randomNumber < CRITICAL_OF_MOVING) {
+            assertThat(racer.getDistance()).isEqualTo(0);
+        } else {
+            assertThat(racer.getDistance()).isEqualTo(1);
+        }
     }
 }
