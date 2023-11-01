9ebfa87c23a9f1128cf3aea887d8d624e33ce282
wooni97 <jhw970714@gmail.com>
2023-11-01 11:35:07 +0900

    feat(OutputView): 매 회 레이스 결과 출력 구현
    - 이 커밋은 OutputView에 매 회 레이스 결과를 출력하는 메서드를 추가합니다. 자동차가 나아간 위치는 Int 타입인 position에 따라 포맷된 형식으로 출력하도록 했습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,24 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import java.util.List;
+
 public class OutputView {
     public final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public void displayGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void displayRaceStatus(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getName() + " : " + getFormattedPosition(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+
+    public String getFormattedPosition(int position) {
+        return "-".repeat(position);
     }
 }
