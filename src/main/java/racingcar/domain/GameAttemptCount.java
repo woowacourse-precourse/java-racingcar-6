2694b7db5006d8c903ceaae985a313b9ced9be2d
이정우 <acg6138@naver.com>
2023-11-01 11:31:58 +0900

    refactor(GameAttemptCount): 시도 회수를 100 이하의 수로 제한하는 메서드 추가

--- src/main/java/racingcar/domain/GameAttemptCount.java
+++ src/main/java/racingcar/domain/GameAttemptCount.java
@@ -2,16 +2,26 @@ package racingcar.domain;
 
 public class GameAttemptCount {
     private static final int END_COUNT = 0;
+    private static final int ZERO = 0;
+    private static final int HUNDRED = 100;
+
     private int attemptCount;
 
     public GameAttemptCount(int attemptCount) {
         validateNumberGreaterThanZero(attemptCount);
+        validateNumberLessThanHundred(attemptCount);
         this.attemptCount = attemptCount;
     }
 
     private void validateNumberGreaterThanZero(int attemptCount) {
-        if (attemptCount <= 0) {
+        if (attemptCount <= ZERO) {
             throw new IllegalArgumentException("[ERROR] 시도 회수는 0보다 커야합니다.");
+        }
+    }
+
+    private void validateNumberLessThanHundred(int attemptCount) {
+        if (attemptCount >= HUNDRED)   {
+            throw new IllegalArgumentException("[ERROR] 시도 회수는 100보다 작아야합니다.");
         }
     }
 
