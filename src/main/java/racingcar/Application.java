da2ca3e1252a221a79c84f0bfb251e0d445d65da
Kim-jihoo <jihoo1106@ewhain.net>
2023-11-01 22:36:24 +0900

    feat: 출력 기능 구현
    - 최종 우승자 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,20 +11,28 @@ public class Application {
     public static void main(String[] args) {
         List<String> players =  playerName();
         int playTimes = playTime();
-        playGame(players, playTimes);
+        List<String> results = playGame(players, playTimes);
+        List<String> winner = winningPlayer(players, results);
+        String winningPlayer = getJoin(winner);
+        System.out.println("최종 우승자 : " + winningPlayer);
+
     }
-    public static void playGame(List<String> players, int playTimes){
+
+    private static String getJoin(List<String> winner) {
+        return String.join(", ", winner);
+    }
+
+    public static List<String> playGame(List<String> players, int playTimes){
         System.out.println("실행 결과");
         List<String> results = playerResults(players);
-
         for (int round =0; round<playTimes; round++) {
            roundResults(players, results);
            System.out.println();
         }
-
+        return results;
     }
     public static List<String> playerResults(List<String> players){
-        List<String> results = new ArrayList<>();
+        List<String> results = getWinningPlayers();
        for (int i=0; i< players.size(); i++){
            results.add("");
        }
@@ -45,11 +53,10 @@ public class Application {
     }
     public static List<String> playerName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String > players = new ArrayList<>();
-        String input = Console.readLine();
-        String[] names = input.split(",");
-        for (int i=0; i<names.length; i++) {
-            String name = names[i];
+        List<String > players = getWinningPlayers();
+        String input = getInput();
+        String[] names = getNames(input);
+        for (String name : names) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("5자 이내로 입력해주세요");
             }
@@ -57,11 +64,20 @@ public class Application {
         }
         return players;
     }
+
+    private static String[] getNames(String input) {
+        return input.split(",");
+    }
+
+    private static String getInput() {
+        return Console.readLine();
+    }
+
     public static int playTime(){
         System.out.println("시도할 횟수는 몇회인가요?");
         int playTimes;
         try{
-            playTimes = Integer.parseInt(Console.readLine());
+            playTimes = Integer.parseInt(getInput());
             if(playTimes <=0){
                 throw new IllegalArgumentException("0보다 큰 숫자를 입력해주세요");
             }
@@ -69,6 +85,35 @@ public class Application {
             throw new IllegalArgumentException("시도횟수가 숫자가 아닙니다");
         }
         return playTimes;
+    }
+    public static List<String> winningPlayer(List<String> players, List<String> results){
+        int moveDashes = -1;
+        List<String> winningPlayers = getWinningPlayers();
+        for (int i =0; i < players.size(); i++){
+            int dashes = countDashes(results.get(i));
+            if(dashes>moveDashes){
+                moveDashes = dashes;
+                assert winningPlayers != null;
+                winningPlayers.add(players.get(i));
+            }else if(dashes==moveDashes){
+                winningPlayers.add(players.get(i));
+            }
+        }
+       return winningPlayers;
+    }
+
+    private static ArrayList<String> getWinningPlayers() {
+        return new ArrayList<>();
+    }
+
+    public static int countDashes(String str){
+        int dashes = 0;
+        for (int i =0; i<str.length(); i++){
+            if(str.charAt(i)=='-'){
+                dashes++;
+            }
+        }
+        return dashes;
     }
 
 }
