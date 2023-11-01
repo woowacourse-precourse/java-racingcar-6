ceefcc3fd79e8bbaf6160aed624eda7af80b2191
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-01 23:32:56 +0900

    feat : 점수판 초기화에 대한 테스트 코드 추가

--- src/test/java/racingcar/ScoreBoardTest.java
+++ src/test/java/racingcar/ScoreBoardTest.java
@@ -1,2 +1,26 @@
-package racingcar;public class ScoreBoardTest {
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.CarData;
+import racingcar.model.GameModel;
+
+public class ScoreBoardTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"lsw  ", "  lsw", " lsw ", "lsw"})
+    void 이름에_포함된_공백_제거_확인(String inputNames) {
+
+        GameModel gameModel = new GameModel(inputNames, "1");
+
+        for (CarData car : gameModel.aggregateRoundScore()) {
+            String carName = car.getName();
+            assertEquals("lsw", carName);
+        }
+
+    }
+
+    
 }
