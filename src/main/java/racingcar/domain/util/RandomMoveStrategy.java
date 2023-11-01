ea03e41053c34064a7ac08c70efdf84a43c0b461
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-11-01 18:09:24 +0900

    refactor(): 불필요한 static 수정 불필요하다고 판단된 static 변수는 static 키워드 제거

--- src/main/java/racingcar/domain/util/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/util/RandomMoveStrategy.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
     public static final int MIN_NUM = 4;
-    private static final int MIN_RANGE = 0;
-    private static final int MAX_RANGE = 9;
+    private final int MIN_RANGE = 0;
+    private final int MAX_RANGE = 9;
 
     @Override
     public boolean move() {
