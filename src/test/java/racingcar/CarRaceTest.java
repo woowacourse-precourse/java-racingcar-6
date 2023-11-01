cac90cc3f4a11fa9420601c4f849f761d5cfa203
김민주 <69795199+ks00919@users.noreply.github.com>
2023-11-01 23:37:52 +0900

    feat: CarRaceTest 코드 추가
    빈 배열이 들어갈 경우, 차 한대가 들어갔을 경우를 테스트하는 코드를 추가했습니다.

--- src/test/java/racingcar/CarRaceTest.java
+++ src/test/java/racingcar/CarRaceTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +13,18 @@ public class CarRaceTest {
     @Test
     @DisplayName("precondition 테스트")
     void constructorTest() {
-        String[] name = {"123456"};
-        assertThrows(IllegalArgumentException.class, () -> new CarRace(name, 5));
-        String[] names = {"1234,5678"};
-        assertThrows(IllegalArgumentException.class, () -> new CarRace(names, -1));
+        assertThrows(IllegalArgumentException.class, () -> new CarRace(new String[1], 5));
+        assertThrows(IllegalArgumentException.class, () -> new CarRace(new String[]{"123456"}, 5));
+        assertThrows(IllegalArgumentException.class, () -> new CarRace(new String[]{"1234"}, -1));
 
         assertThrows(IllegalArgumentException.class, () -> new CarRace(null, 6));
     }
 
+    @Test
+    @DisplayName("경기 결과 테스트")
+    void resultTest() {
+        List<String> result = new ArrayList<>();
+        result.add("123");
+        assertEquals(new CarRace(new String[]{"123"}, 1).run(), result);
+    }
 }
