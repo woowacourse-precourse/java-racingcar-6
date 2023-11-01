c6bf4311aec94064d1b91894a4736e5b2efe337f
ziy00n <jiyun4188@gmail.com>
2023-11-01 22:37:02 +0900

    feat : 게임 구현(라운드별 랜덤 수 생성/전진/이동결과 출력)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         Race race = new Race();
-        race.start();
+        Race game = race.InputForStart();
+        game.play();
     }
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,13 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Race {
 
+    private static final int MIN_RANDOM = 0;
+    private static final int MAX_RANDOM = 9;
+    private static final int END_OF_FORWARD = 4;
     private final List<Car> cars;
     private final int count;
+    private static String carNames;
+    private static int cnt;
 
     Computer computer = new Computer();
+    FindWinners findWinners = new FindWinners();
+
 
     public Race() {
         this.cars = null;
@@ -19,12 +28,43 @@ public class Race {
         this.count = count;
     }
 
-    public Race start() {
-        String carNames = computer.getInputCarNames();
-        int cnt = computer.getInputRaceCount();
+    public Race InputForStart() {
+        carNames = computer.getInputCarNames();
+        cnt = computer.getInputRaceCount();
 
         return new Race(carNames, cnt);
     }
 
+    public void play() {
+        for(int i = 0; i < count; i++) {
+            playOneRound();
+            printRoundResult();
+        }
+        printWinners();
+    }
 
+    public void playOneRound() {
+        for (Car car : cars) {
+            int randomNumber = createRandomNumber();
+            if(randomNumber >= END_OF_FORWARD) {
+                car.forward();
+            }
+        }
+    }
+
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM,MAX_RANDOM);
+    }
+
+    public void printRoundResult() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private void printWinners() {
+        List<Car> winners = findWinners.getWinners(cars);
+        Computer.printWinners(winners);
+    }
 }
