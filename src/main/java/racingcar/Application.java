4a583f1e6ff8f52ad8bf35da431170c36c20b3ea
Hyyena <sjtxm0320@gmail.com>
2023-11-01 22:21:35 +0900

    ✨ feat: 메인 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,17 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String[] carNames = Input.getCarNames();
+        int tryCount = Input.getTryCount();
+
+        Game game = new Game(carNames);
+
+        for (int i = 0; i < tryCount; i++) {
+            game.start();
+            Output.printRaceState(game.getCars());
+        }
+
+        Output.printWinners(game.getWinners());
     }
 }
