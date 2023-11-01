dd98e01f0e6feeed0cdb9e597afa07b7a87b77fd
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-11-01 18:54:56 +0900

    Test: MovingCondition 클래스 테스트 코드

--- src/test/java/racingcar/domain/MovingConditionTest.java
+++ src/test/java/racingcar/domain/MovingConditionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,5 +17,23 @@ class MovingConditionTest {
     @BeforeEach
     void setUp() {
         movingCondition = new MovingCondition();
+    }
+
+    @Test
+    @DisplayName("조건이 4 이상 경우 true를 반환해야 함")
+    void testReturnTrue() {
+        int condition = 4;
+
+        assertTrue(movingCondition.isMovingForward(condition));
+    }
+
+
+
+    @Test
+    @DisplayName("조건이 3 이하 경우 false 반환해야 함")
+    void testReturnFalse() {
+        int condition = 3;
+
+        assertFalse(movingCondition.isMovingForward(condition));
     }
 }
