7e16a1c82912b378e4b8b6334915fe61651f7b55
chanwoo <mdsoo55828@gmail.com>
2023-11-01 19:17:33 +0900

    Feat: OutputView 우승한 자동차 결과 출력 뷰 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String RESULT_ROUND_MESSAGE = "\n" + "실행 결과" + "\n";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
@@ -12,6 +14,16 @@ public class OutputView {
     public void printRoundGameResult(StringBuilder eachResultBuilder) {
         System.out.println(eachResultBuilder);
         stringBuilder.setLength(0);
+    }
+
+    public void printFinalWinner(List<String> winnerNames) {
+        String output = formatWinnerNames(winnerNames);
+        stringBuilder.append("\n").append(WINNER_MESSAGE).append(output);
+        System.out.println(stringBuilder);
+    }
+
+    private String formatWinnerNames(List<String> winnerNames) {
+        return String.join(", ", winnerNames);
     }
 
 }
