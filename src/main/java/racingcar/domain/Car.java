c9c9fc03008cdc9d15f343073d4fc00035372373
leejuno <zhqkfxm0127@naver.com>
2023-11-01 21:27:11 +0900

    refactor(Car): 불필요한 메소드 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -13,10 +13,6 @@ public class Car {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public StringBuffer getForwardDistance() {
         return forwardDistance;
     }
