22243185fa78fc249a9a85f096c9ddf6ace740f6
2021110806 <2021110806@dgu.ac.kr>
2023-11-01 23:17:01 +0900

    refactor(InputUtils.java, Input.java): add inputUtils class Input class에서 도구적 성질을 가지는 메소드들을 InputUtils class로 이동시켰습니다.

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -2,76 +2,24 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.HashSet;
-import java.util.NoSuchElementException;
-import java.util.Set;
-
 public class Input {
 
-    static boolean isOutOfRange(long tryNumber) {
-        return (tryNumber > 100 || tryNumber < 1);
-    }
-
-     static  boolean containsDuplicates(String[] carNames) {
-        Set<String> duplicateChecker = new HashSet<>();
-        for (String carName : carNames) {
-            if (!duplicateChecker.add(carName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static void checkEmptyOrBlankCarNames(String carNameInput) {
-        if (carNameInput.isBlank() || carNameInput.isEmpty()) {
-            Exception.generateInvalidCarNumberException();
-        }
-    }
-
-    static void checkDuplicateCarNames(String[] carNames) {
-        if (containsDuplicates(carNames)) {
-            Exception.generateInvalidDuplicateCarNameException();
-        }
-    }
-
-    static void checkTryNumberRange(long tryNumber) {
-        if (isOutOfRange(tryNumber)) {
-            Exception.generateInvalidTryNumberRangeException();
-        }
-    }
-
-    static void checkTryNumberInputLength(String tryNumberInput) {
-        if (tryNumberInput.length() > 10) {
-            Exception.generateInvalidTryNumberRangeException();
-        }
-    }
-
     static String[] getCarName() {
-        String carNameInput = "";
-        String[] carNames = {};
-        try {
-            carNameInput = Console.readLine();
-        } catch (NoSuchElementException noSuchElementException) {
-            Exception.generateInvalidCarNumberException();
-        }
+        String carNameInput = InputUtils.readCarNameInput();
         carNameInput = carNameInput.replaceAll("^,+|,+$", "").replaceAll(",+", ",");
-        checkEmptyOrBlankCarNames(carNameInput);
-        try {
-            carNames = carNameInput.split(",");
-        } catch (Error OutOfMemoryError){
-            Exception.generateOutOfMemoryError();
-        }
-        checkDuplicateCarNames(carNames);
+        InputUtils.checkEmptyOrBlankCarNames(carNameInput);
+        String[] carNames = InputUtils.splitCarNameInput(carNameInput);
+        InputUtils.checkDuplicateCarNames(carNames);
         return (carNames);
         }
 
     static int getTryNumber() {
         String tryNumberInput = Console.readLine();
-        checkTryNumberInputLength(tryNumberInput);
+        InputUtils.checkTryNumberInputLength(tryNumberInput);
         long tryNumber = 0;
         try {
             tryNumber = Long.parseLong(tryNumberInput);
-            checkTryNumberRange(tryNumber);
+            InputUtils.checkTryNumberRange(tryNumber);
         } catch (NumberFormatException numberFormatException) {
             Exception.generateInvalidNumberCharacterException();
         }
