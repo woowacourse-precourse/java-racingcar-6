6d590b1db758617975b7406e3975f1ded16319ef
Hyunjun KIM <hihj070914@icloud.com>
2023-11-01 22:22:22 +0900

    feat: InputValidation - InputValidaton 클래스의 세부 로직을 구현하였습니다. - 구현 후 유닛 테스트를 수행하였습니다.

---
+++ src/main/java/racingcar/InputValidation.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import java.util.HashSet;
+
+public class InputValidation {
+    private static final String INPUT_DELIMITER = ",";
+
+    public static String[] validateInputNames(String userInput) throws IllegalArgumentException {
+        String[] participantsNameList = userInput.split(INPUT_DELIMITER);
+        HashSet<String> checkDuplicate = new HashSet<>(participantsNameList.length);
+
+        for (String participantName : participantsNameList) {
+            if (participantName.length() > 6)
+                throw new IllegalArgumentException(participantName + " is too long!");
+            if (checkDuplicate.contains(participantName))
+                throw  new IllegalArgumentException(participantName + " already exists");
+            checkDuplicate.add(participantName);
+        }
+
+        return participantsNameList;
+    }
+
+    public static int validateInputMoveCount(String userInput) throws IllegalArgumentException {
+        int moveCount;
+        try {
+            moveCount = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(userInput + " is not an integer");
+        }
+
+        if (moveCount < 0)
+            throw  new IllegalArgumentException(userInput + " is negative");
+
+        return moveCount;
+    }
+}
