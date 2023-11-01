16019b41905ee7defb5f6d1223d4a6b19363a385
honeydanji <tjdwls3680@naver.com>
2023-11-01 16:59:50 +0900

    REFACTOR : 결과 코드 추가

--- src/main/java/racingcar/controller/CarRacingGameStart.java
+++ src/main/java/racingcar/controller/CarRacingGameStart.java
@@ -1,16 +1,24 @@
 package racingcar.controller;
 
+import racingcar.model.CarAndDistance;
 import racingcar.model.CarRacing;
+import racingcar.view.CarRaceResult;
 import racingcar.view.Enter;
 
 public class CarRacingGameStart {
 
     public void gameStart() {
+        // 입력
         Enter enter = new Enter();
-        String[] carsName =  enter.enterCarsName(); // String[] 반환
-        int numberTry =  enter.enterNumberTry();// 시도횟수 반환 (int)
+        String[] carsName =  enter.enterCarsName();
+        int numberTry =  enter.enterNumberTry();
 
+        // 게임 시작
         CarRacing carRacing = new CarRacing();
-        carRacing.racingController(carsName, numberTry);
+        CarAndDistance carAndDistance = carRacing.racingController(carsName, numberTry);
+
+        // 게임 결과
+        CarRaceResult carRaceResult = new CarRaceResult();
+        carRaceResult.carRacingWinner(carAndDistance);
     }
 }
