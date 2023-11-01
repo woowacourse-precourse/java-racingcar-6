772228897cbebe6eeb7f10d09f7b3b8949a3ac16
SWtheWhite <swthewhite@gmail.com>
2023-11-01 22:57:08 +0900

    feat: 뼈대 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,71 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars;
+        int numberOfTries;
+
+        cars = initCars();
+        numberOfTries = initNumberOfTries();
+
+        start(cars, numberOfTries);
+    }
+    private static List<Car> initCars() {
+
+    }
+    private static int initNumberOfTries()
+    {
+
+    }
+    private static void start(List<Car> cars, int numberOfTries) {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < numberOfTries; i++) {
+            moveCars(cars);
+            printCars(cars);
+        }
+        announceWinner(cars);
+    }
+    private static void moveCars(List<Car> cars) {
+
+    }
+
+    private static void printCars(List<Car> cars) {
+
+    }
+
+    private static void announceWinner(List<Car> cars) {
+        int maxPosition = getMaxPosition(cars);
+    }
+
+    private static int getMaxPosition(List<Car> cars) {
+
+    }
+}
+
+class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward() {
+        this.position++;
+    }
+
+    public String printPosition() {
+        return "-".repeat(this.position);
     }
 }
