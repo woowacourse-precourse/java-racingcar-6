923ce1ceb9226d331e6eb14e7ba2fe4c6091117e
naraspc <wodnrtkfka@naver.com>
2023-11-01 13:43:18 +0900

    feat : add InputValues Class test : add InputValues Class
    InputView 클래스, 테스트 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.view.InputValues;
+
 public class Application {
     public static void main(String[] args) {
+        InputValues inputValues = new InputValues();
+        inputValues.getName();
         // TODO: 프로그램 구현
     }
 }
