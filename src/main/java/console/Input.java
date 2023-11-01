1c60caea8a0bd79126389a339fd59fe62e9b45ed
julie0005 <julie0005@ajou.ac.kr>
2023-11-01 19:43:39 +0900

    refactor : 사용자 입력 검증 로직 메소드 추출

--- src/main/java/console/Input.java
+++ src/main/java/console/Input.java
@@ -8,20 +8,28 @@ public class Input {
     public static String[] carNamesInput() {
         String input = Console.readLine();
         String[] carNames = input.split(",");
-        for (String name : carNames) {
-            if (name.length() > NAME_LIMIT) throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
-        }
+        validateCarNames(carNames);
         return carNames;
     }
 
     public static int tryInput() {
         try {
             int tryCount = Integer.parseInt(Console.readLine());
-            if (tryCount <= 0 || tryCount >= 9) throw new IllegalArgumentException("입력값은 0에서 9 사이여야 합니다.");
+            validateTryInput(tryCount);
             return tryCount;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못된 자료형입니다.");
         }
+    }
+
+    private static void validateCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > NAME_LIMIT) throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    private static void validateTryInput(int tryCount) {
+        if (tryCount <= 0 || tryCount >= 9) throw new IllegalArgumentException("입력값은 0에서 9 사이여야 합니다.");
     }
 
 }
