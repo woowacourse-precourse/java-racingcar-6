e890f5e0acb2ccc5909392d738416a59ddb68f8c
wooni97 <jhw970714@gmail.com>
2023-11-01 21:57:48 +0900

    feat(constant): 상수 패키지로 분리
    - 상수를 패키지로 분류하여 추후 확장과 수정이 용이하게 합니다.

---
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -0,0 +1,12 @@
+package racingcar.constant;
+
+public class ErrorMessage {
+
+    public static final String INVALID_NAME_LENGTH_MESSAGE = "[Error]: 자동차 이름은 5자 이하로 입력해주세요";
+    public static final String INVALID_NAME_EMPTY_MESSAGE = "[Error]: 자동차 이름은 공백이면 안됩니다";
+    public static final String INVALID_NAME_CONTAINS_WHITE_SPACE_MESSAGE = "[Error]: 자동차 이름 내부의 공백은 불가합니다";
+    public static final String INVALID_NAMES_DUPLICATED_MESSAGE = "[Error]: 자동차 이름의 중복은 불가합니다";
+    public static final String INVALID_TRIALNUMBER_IS_NOT_NUMBER_MESSAGE = "[Error]: 시도 횟수는 0이상의 정수를 입력해주세요 ";
+
+
+}
---
+++ src/main/java/racingcar/constant/Number.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class Number {
+    public static final int INITIAL_POSITION = 0;
+    public static final int FORWARD_CONDITION_NUMBER = 4;
+    public static final int VALID_CAR_NAME_MAX_LENGTH = 5;
+    public static final int RANDOM_MIN_NUMBER = 0;
+    public static final int RANDOM_MAX_NUMBER = 9;
+    public static final int INITIAL_TRIAL_NUMBER = 0;
+}
