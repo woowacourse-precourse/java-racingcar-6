12dfc1f5d109028ae9da96ab7212ee94fadecbea
KongJunWoo <joonoo3@naver.com>
2023-11-01 22:58:01 +0900

    feat : 난수가 4 이상일 시 전진
    - isMove를 통해 난수가 4 이상인지 판별합니다.
    - 4 이상일 시 해당 car 객체의 move 메소드를 호출해 전진합니다.

--- src/main/java/racingcar/util/MoveCar.java
+++ src/main/java/racingcar/util/MoveCar.java
@@ -1,11 +1,21 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
 
 public class MoveCar {
+
+    public static void moveCar(Car car) {
+        if (isMove()) {
+            car.move();
+        }
+    }
 
     public static int makeRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
 
+    public static boolean isMove() {
+        return makeRandomNumber() >= 4;
+    }
 }
