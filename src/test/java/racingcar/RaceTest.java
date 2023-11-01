793eb08bb37d55a7436521696acec9d858cec77a
yeop0740 <ssucgoo@gmail.com>
2023-11-01 18:09:37 +0900

    test: add test code for Race#getWinners

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -325,4 +325,89 @@ class RaceTest {
         }
     }
 
+    @Nested
+    class 우승자를_뽑는_기능 {
+        static Car car1;
+        static Car car2;
+        static Car car3;
+
+        static Race race;
+
+        static int expectedRound = 3;
+
+        @Test
+        public void 모두_같은_경우() throws Exception {
+            car1 = new Car("alpha");
+            car2 = new Car("bravo");
+            car3 = new Car("char");
+
+            List<Car> expectedCars = Arrays.asList(car1, car2, car3);
+            race = new Race(3, List.of(car1, car2, car3));
+
+            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+                    () -> {
+                        for (int i = 0; i < expectedRound; i++) {
+                            race.proceedRound();
+                        }
+                        Assertions.assertThat(race.getWinners()).extracting("name", "distance")
+                                .isEqualTo(List.of(
+                                        Tuple.tuple("alpha", 0),
+                                        Tuple.tuple("bravo", 0),
+                                        Tuple.tuple("char", 0)));
+                    },
+                    STOP,
+                    STOP
+            );
+        }
+
+        @Test
+        public void 두개가_같은경우() throws Exception {
+            car1 = new Car("alpha");
+            car2 = new Car("bravo");
+            car3 = new Car("char");
+
+            List<Car> expectedCars = Arrays.asList(car2, car3);
+            race = new Race(3, List.of(car1, car2, car3));
+
+            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+                    () -> {
+                        for (int i = 0; i < expectedRound; i++) {
+                            race.proceedRound();
+                        }
+                        Assertions.assertThat(race.getWinners()).extracting("name", "distance")
+                                .isEqualTo(List.of(
+                                        Tuple.tuple("bravo", 3),
+                                        Tuple.tuple("char", 3)));
+                    },
+                    STOP,
+                    MOVING_FORWARD
+            );
+        }
+
+        @Test
+        public void 모두_다른_경우() throws Exception {
+            car1 = new Car("alpha");
+            car2 = new Car("bravo");
+            car3 = new Car("char");
+
+            List<Car> expectedCars = Arrays.asList(car1, car2, car3);
+            race = new Race(3, List.of(car1, car2, car3));
+
+            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+                    () -> {
+                        Randoms.pickNumberInRange(0, 9);
+                        for (int i = 0; i < expectedRound; i++) {
+                            race.proceedRound();
+                        }
+
+                        Assertions.assertThat(race.getWinners()).extracting("name", "distance")
+                                .isEqualTo(List.of(
+                                        Tuple.tuple("alpha", 3)));
+                    },
+                    STOP,
+                    MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD, STOP, STOP
+            );
+        }
+    }
+
 }
