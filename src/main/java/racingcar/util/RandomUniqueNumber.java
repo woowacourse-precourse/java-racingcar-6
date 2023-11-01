2888e8319747ad0fc0ce9f980d31d7f21c312ebb
sudo-dustle <asdf748596123@gmail.com>
2023-11-01 18:40:45 +0900

    feat: 게임을 위한 랜덤한 숫자를 뽑는 RandomUniqueNumber 클래스 생성

--- src/main/java/racingcar/util/RandomUniqueNumber.java
+++ src/main/java/racingcar/util/RandomUniqueNumber.java
@@ -2,9 +2,12 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumber {
+public class RandomUniqueNumber {
 
-    public static int pickRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
+    public static int pickRandomUniqueNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
