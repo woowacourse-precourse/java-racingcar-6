b2d05a19e69e41ec69c8d5a9792c02342332bb48
lkdcode <dlrlejr1@naver.com>
2023-11-01 15:19:12 +0900

    Add : RacingCarException. NUMERIC_INPUT_EXCEPTION

--- src/main/java/racingcar/exception/RacingCarException.java
+++ src/main/java/racingcar/exception/RacingCarException.java
@@ -3,6 +3,7 @@ package racingcar.exception;
 public enum RacingCarException {
     NAME_LENGTH_EXCEPTION("[Error] 이름은 5자 이하만 가능합니다."),
     ATTEMPT_MINIMUM_COUNT_EXCEPTION("[Error] 시도할 횟수는 최소 1회 이상입니다."),
+    NUMERIC_INPUT_EXCEPTION("[Error] 숫자만 입력해주세요."),
     ;
 
     private final String message;
