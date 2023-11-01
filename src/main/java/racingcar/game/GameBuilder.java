f3eef5d36b140f9f6ba4390da53bb377e8b6b8a1
zangsu <zangsu_@naver.com>
2023-11-01 13:38:21 +0900

    docs : remove Javadoc
    all method name show it's behavior very well, so remove Javadoc

--- src/main/java/racingcar/game/GameBuilder.java
+++ src/main/java/racingcar/game/GameBuilder.java
@@ -7,9 +7,6 @@ public class GameBuilder {
     private String[] names;
     private String roundString;
 
-    /**
-     * (use builder pattern) get cars name from user
-     */
     public GameBuilder getCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         this.names = Console.readLine().split(",");
@@ -17,9 +14,6 @@ public class GameBuilder {
         return this;
     }
 
-    /**
-     * (use builder pattern) get round number from user
-     */
     public GameBuilder getRoundNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         roundString = Console.readLine();
@@ -27,12 +21,10 @@ public class GameBuilder {
         return this;
     }
 
-
     public Game build() {
         if (names == null || roundString == null) {
             throw new IllegalArgumentException(ExceptionMessage.CANT_BUILD_GAME_YET);
         }
-
         return new Game(names, roundString);
     }
 }
