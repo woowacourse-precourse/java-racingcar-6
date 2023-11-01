5817905cd501157fbea76c3223b3627b3517dfab
kg9585 <kg9585@naver.com>
2023-11-01 21:15:33 +0900

    feat(RandomGenerator): 메소드 분리

--- src/main/java/racingcar/model/RandomGenerator.java
+++ src/main/java/racingcar/model/RandomGenerator.java
@@ -4,15 +4,22 @@ import java.util.Random;
 
 public class RandomGenerator {
     private static final Random random = new Random();
+
     public static int generateNumber(int min, int max) {
+        validateInput(min, max);
+        return generateRandomNumberInRange(min, max);
+    }
+
+    private static void validateInput(int min, int max) {
         if (min > max) {
-            int temp = min;
-            min = max;
-            max = temp;
+            throw new IllegalArgumentException("최소값은 최대값보다 크지 않아야 합니다.");
         }
+    }
+
+    private static int generateRandomNumberInRange(int min, int max) {
         if (min == max) {
             return min;
         }
-        return min + random.nextInt(max - min);
+        return generateRandomNumberInRange(min, max);
     }
 }
