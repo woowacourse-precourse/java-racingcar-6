f9e76404d2a21150e58976a4c5a9a00876671774
jyeun722 <skqhs7276@gamil.com>
2023-11-01 14:57:23 +0900

    feat: Add car names duplicate input exception

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,20 +2,26 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.*;
+
 public class Application {
     private static final int MAX_NAME_SIZE = 5;
 
     private static String[] inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine().replaceAll("[^a-zA-Z0-9,]", "");
-        System.out.println(carNamesInput);
 
+        HashSet<String> carNamesSet = new HashSet<>();
         String[] carNamesArray = carNamesInput.split(",");
         for (int i = 0; i < carNamesArray.length; i++) {
+            carNamesSet.add(carNamesArray[i]);
+
             if (carNamesArray[i].length() > MAX_NAME_SIZE) {
                 throw new IllegalArgumentException();
             }
         }
+        if (carNamesArray.length != carNamesSet.size()) throw new IllegalArgumentException();
+
         return carNamesArray;
     }
 
