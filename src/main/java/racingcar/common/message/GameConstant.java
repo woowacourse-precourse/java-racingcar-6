4e863fb9923eed7ec8fe3dfd281467150e9b30d6
jaepyo99 <jaepyo99@naver.com>
2023-11-01 15:35:33 +0900

    [Feat] 공통 문장 및 에러문장 객체 생성

---
+++ src/main/java/racingcar/common/message/GameConstant.java
@@ -0,0 +1,9 @@
+package racingcar.common.message;
+
+public class GameConstant {
+    public final static String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String INPUT_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    public final static String INIT_PRINT_GAME_RESULT_MESSAGE = "실행 결과";
+    public final static String ANNOUNCE_WINNER_MESSAGE = "최종 우승자 : ";
+
+}
