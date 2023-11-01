664b5b54e14bf65fdb9473057e0006c8e97b2e32
noah <jeongas9810@gmail.com>
2023-11-01 21:16:49 +0900

    feature: read car name list
    Enter the car names through the user's input.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
+
+    public static List<String> readCarNameList() {
+        List<String> carNames;
+
+        System.out.println("경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = Arrays.stream(Console.readLine().replaceAll(" ", "").split(",")).toList();
+
+        return carNames;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = readCarNameList();
     }
 }
