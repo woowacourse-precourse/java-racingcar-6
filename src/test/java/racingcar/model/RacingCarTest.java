597c9ec796ce04b4fbe8f4e02dbddf098b58f917
ASak1104 <khw56184@gmail.com>
2023-11-01 19:14:41 +0900

    test : 필요 없는 상속 제거

--- src/test/java/racingcar/model/RacingCarTest.java
+++ src/test/java/racingcar/model/RacingCarTest.java
@@ -4,20 +4,14 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class RacingCarTest extends NsTest {
+class RacingCarTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
-
-    @Override
-    protected void runMain() {
-        // do nothing
-    }
 
     @Nested
     @DisplayName("자동차 이름 테스트")
