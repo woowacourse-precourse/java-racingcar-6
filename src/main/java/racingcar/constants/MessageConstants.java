54d2a3720249b6be62f57608562dcea154b2832e
dhl1924 <dhl1924@naver.com>
2023-11-01 20:28:53 +0900

    refactor: Updated message constants for invalid number format and out-of-range exceptions

--- src/main/java/racingcar/constants/MessageConstants.java
+++ src/main/java/racingcar/constants/MessageConstants.java
@@ -7,8 +7,8 @@ public class MessageConstants {
     public static final String INVALID_CAR_NAME_FOR_BLANK = "공백은 포함할 수 없습니다.";
     public static final String INVALID_CAR_NAME_FOR_NULL = "널(null)은 포함할 수 없습니다";
     public static final String INVALID_CAR_NAME_FOR_DUPLICATE_NAME = "이름은 중복될 수 없습니다.";
-    public static final String INVALID_TRY_COUNT = "1회 이상 입력해주세요.";
-    public static final String INVALID_NUMBER_FORMAT = "숫자를 입력해 주세요";
+    public static final String INVALID_TRY_COUNT = "1회 이상 입력해 주세요.";
+    public static final String INVALID_NUMBER_FORMAT_OR_OUT_OF_INT_RANGE = "숫자가 아니거나 시행 가능한 범위(1~2,147,483,647)를 초과 하였습니다";
     public static final String EMPTY_CAR_LIST = "경주할 자동차가 없습니다.";
     public static final String FINAL_WINNERS = "최종 우승자 : ";
 }
