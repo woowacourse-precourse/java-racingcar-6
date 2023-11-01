76cf95eba5981973d417426ea007f0b0fcd65fe0
haebyun <bhk4392@gmail.com>
2023-11-01 19:00:46 +0900

    feat(RacingController): 자동차 이름들을 입력 및 검증하고, 검증 된 입력을 List 형태로 저장하는 기능 구현

--- src/main/java/racingcar/games/racing/controller/RacingController.java
+++ src/main/java/racingcar/games/racing/controller/RacingController.java
@@ -1,11 +1,31 @@
 package racingcar.games.racing.controller;
 
+import java.util.List;
 import racingcar.Game;
+import racingcar.games.racing.service.RacingService;
+import racingcar.games.racing.util.DefaultProcessor;
+import racingcar.games.racing.util.RacingFormatter;
+import racingcar.games.racing.view.RacingInputView;
+import racingcar.games.racing.view.RacingOutputView;
 
 public class RacingController implements Game {
 
+    private final RacingInputView racingInputView;
+    private final RacingFormatter racingFormatter;
+
+    public RacingController() {
+        this.racingInputView = new RacingInputView();
+        this.racingFormatter = new RacingFormatter();
+    }
+
+
     @Override
     public void execute() {
+        List<String> carNames = getValidatedCarNames();
+    }
 
+    private List<String> getValidatedCarNames() {
+        String input = racingInputView.readCarNames();
+        return racingFormatter.reformatCarNames(input);
     }
 }
