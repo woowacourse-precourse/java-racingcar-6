55e0800a06b3147a9255b090641ec486228d52b2
주은 <00-10-21@naver.com>
2023-11-01 22:51:21 +0900

    feat: Implement single round play functionally

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -4,8 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
 
+    private static final int MOVING_THRESHOLD = 4;
+
     public static int generateRandomValue() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public static void moveCarIfPossible(Car car, int randomValue) {
+        if (randomValue >= MOVING_THRESHOLD) {
+            car.move();
+        }
     }
 
 
