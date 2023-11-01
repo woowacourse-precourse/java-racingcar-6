e87ac42b350ab70e61e4f1f035d2a23d349eeb04
wooni97 <jhw970714@gmail.com>
2023-11-01 11:43:47 +0900

    refactor(Car): Validator 클래스 삭제 및 Car 도메인에 입력 유효성 검증 추가
    - 이 커밋은 기존의 입력 유효성을 검증하던 Validator 클래스를 삭제하고 해당 클래스에 존재하던 유효성 검증 메서드를 Car 도메인에 추가했습니다. 차 이름이 5글자 이하여야 하는 비즈니스 정책과 입력 유효성 검증은 도메인 객체의 책임이라고 생각하여 도메인 객체가 비즈니스 정책을 나타낼 수 있도로 구현했습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,16 +4,48 @@ public class Car {
     private final String name;
     private int position;
     private final int INITIAL_POSITION = 0;
-    private final int FOWARD_CONDITION_NUMBER = 4;
+    private final int FORWARD_CONDITION_NUMBER = 4;
+    private static final int VALID_CAR_NAME_MAX_LENGTH = 5;
 
     public Car(String name) {
+        checkCarNameLengthValid(name);
+        checkCarNameEmpty(name);
+        checkCarNameContainsWhiteSpace(name);
+
         this.name = name;
         this.position = INITIAL_POSITION;
     }
 
-    public void move(int condition) {
-        if (condition >= FOWARD_CONDITION_NUMBER) {
+    public void forwardOrStay(int condition) {
+        if (condition >= FORWARD_CONDITION_NUMBER) {
             position++;
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public static void checkCarNameLengthValid(String carName) {
+        if (carName.length() > VALID_CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkCarNameEmpty(String carName) {
+        if (carName == null || carName.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkCarNameContainsWhiteSpace(String carName) {
+        if (carName.length() != carName.trim().length()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
