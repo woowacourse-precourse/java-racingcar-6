ec6b4f44dc3b1b1e94355049ecb04fd8e287e3fc
jooda00 <dragon2000a@gmail.com>
2023-11-01 16:46:26 +0900

    refactor(OutputView): 변환 및 출력 메소드 각각 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,8 +7,12 @@ public class OutputView {
         System.out.println("실행 결과");
     }
 
+    public String convertListToString(List<String> winnerList) {
+        return String.join(", ", winnerList);
+    }
+
     public void showWinners(List<String> winnerList) {
-        String winners = String.join(", ", winnerList);
+        String winners = convertListToString(winnerList);
         System.out.print("최종 우승자 : " + winners);
     }
 }
