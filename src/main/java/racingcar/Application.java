6ffaca3e90c12bf9d60bac3d6aff1c386af0f2ea
주은 <00-10-21@naver.com>
2023-11-01 22:08:43 +0900

    docs: Delete Unuse feature from README.md

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
+
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = scanner.nextLine();
     }
 }
