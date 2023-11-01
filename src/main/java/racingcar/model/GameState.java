ef4a66e543bf6557c9fcae3543323d0e0bd7d0f8
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-01 20:10:19 +0900

    refactor: (GameState) 각 라운드당 display할 Car 상태 기능

--- src/main/java/racingcar/model/GameState.java
+++ src/main/java/racingcar/model/GameState.java
@@ -1,14 +1,24 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameState {
-    private int maxDistance;
-    private final int roundLength;
+    private Map<String,String> carStateMap;
 
-
-    public GameState(){
-        this.roundLength = 0;
-        this.maxDistance=0;
+    public GameState(ArrayList<Car> carList){
+        this.carStateMap= new HashMap<>();
+        settingCarStateMap(carList);
     }
 
+    private void settingCarStateMap(ArrayList<Car> carList){
+        for(Car car : carList){
+            carStateMap.put(car.getCarName(),car.getStepDistence());
+        }
+    }
 
+    public Map<String, String> getCarStateMap() {
+        return carStateMap;
+    }
 }
