b6ac2577b68f31fa23a66381f4ddb58929d00d66
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 23:05:06 +0900

    feat(Race): 속성 및 생성자 정의
    자동차 경주를 표현하는 클래스입니다.
    
    속성
     - 남은 라운드 수를 표현하는 Round 객체
     - 참가할 Car 객체를 포함하는 Entry 객체
    
    정적 getter 메소드
     - getMinEntrySize(): 최소 엔트리 크기를 반환하는 정적 getter 메소드가 존재합니다.
    
    생성자
     - (Round, Entry): 주어진 전체 라운드 수를 표현하는 Round 객체와 참가할 Car 객체를 포함하고 있는 Entry 객체를 이용하여 Race 객체를 생성한다.
    - 만약, 참가자 엔트리의 크기가 Race.getMinEntrySize() 보다 작은 경우
    IllegalArgumentException 예외를 발생시킨다.

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,4 +1,27 @@
 package racingcar.model;
 
 public class Race {
+
+    private Round roundLeft;
+    private Entry entry;
+
+    public static int getMinEntrySize() {
+        final int MIN_ENTRY_SIZE = 2;
+        return MIN_ENTRY_SIZE;
+    }
+
+    /**
+     * 명시된 라운드 수와 엔트리를 이용하여 경주(Race) 객체를 생성한다.
+     * @param totalRound 진행할 총 라운드 수
+     * @param entry 참가자 엔트리
+     * @throws IllegalArgumentException 참가자 엔트리의 크기가 Race.getMinEntrySize() 보다 작은 경우
+     */
+    public Race(Round totalRound, Entry entry) throws IllegalArgumentException {
+        if (entry.size() < Race.getMinEntrySize()) {
+            throw new IllegalArgumentException("경주를 진행하기 위해서는 최소 크기가 2인 엔트리가 필요합니다.");
+        }
+
+        this.roundLeft = totalRound;
+        this.entry = entry;
+    }
 }
