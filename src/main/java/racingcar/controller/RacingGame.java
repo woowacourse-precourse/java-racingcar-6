8e0816e17561766f3c5fe66ec049ce5ed0f0193f
jang-namu <ironhee8005@gmail.com>
2023-11-01 20:37:35 +0900

    feat: controller-run 작성.

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,5 +1,21 @@
 package racingcar.controller;
 
-public class RacingGame {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.component.Cars;
 
+public class RacingGame {
+    public static void run() {
+        int n;
+        Cars cars = new Cars();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        n = Integer.parseInt(Console.readLine());
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < n; i++) {
+            cars.runARound();
+            System.out.println();
+        }
+        cars.totalizeWinnersNames();
+    }
 }
