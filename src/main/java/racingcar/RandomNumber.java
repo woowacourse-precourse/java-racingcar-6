41bebb1fc4651060ae945dfb153032a9f3a9b1ec
이재현 <fanta4715@naver.com>
2023-11-01 17:04:10 +0900

    feat: RandomNumber 추가
    RandomNumber Generate시에 필요한 기준이 되는 값을 static을 통해 클래스로 묶어놓음

---
+++ src/main/java/racingcar/RandomNumber.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class RandomNumber {
+    public static final int RANDOM_MAX = 9;
+    public static final int RANDOM_MIN = 0;
+    public static final int MOVABLE_MIN = 4;
+}
