73b42262cf7fd0e6ede0ab658fe62aa1369b3dab
HongChaa <jun761_1@naver.com>
2023-11-01 11:37:22 +0900

    feat(GameController) : 경주 진행 메서드 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,32 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+
+    private Cars cars = new Cars();
+
+    public void proceedGame() {
+        List<Car> carList = cars.setPlayer(InputView.carsName());
+        int tryCount = InputView.tryCount();
+        race(carList, tryCount);
+    }
+
+    public void race(List<Car> carList, int tryCount) {
+        int countCheck = 1;
+
+        while (countCheck < tryCount) {
+            carList = cars.raceCars(carList);
+            OutputView.executeResult(carList);
+            countCheck ++;
+        }
+
+
+    }
+
+}
