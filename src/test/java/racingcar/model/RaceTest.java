222bd4aff76d3d2b34e14b5d2842fdc693ce0d7c
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 23:10:54 +0900

    test(RaceTest): Race 객체 생성에 대한 단위 테스트 작성
    1. Race_객체_생성_테스트
    2. Race_객체_생성_실패
    -> 엔트리 크기가 너무 작은 경우 IllegalArgumentException 발생하는 지 확인

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -2,6 +2,35 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class RaceTest {
 
+    @Test
+    void Race_객체_생성_테스트() {
+        Car c1 = new Car(new Name("a"));
+        Car c2 = new Car(new Name("b"));
+
+        Entry entry = new Entry();
+        entry.join(c1);
+        entry.join(c2);
+
+        try {
+            Race race = new Race(new Round(2), entry);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void Race_객체_생성_실패() {
+        // 엔트리 크기가 너무 작은 경우
+        assertThrows(IllegalArgumentException.class, () -> {
+            Entry entry = new Entry();
+            Car c1 = new Car(new Name("a"));
+            entry.join(c1);
+
+            Race race = new Race(new Round(2), entry);
+        });
+    }
 }
