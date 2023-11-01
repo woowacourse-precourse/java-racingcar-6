82065a6c69bf110bb149b798bdff9f63441aee00
fing9 <djajsl54@naver.com>
2023-11-01 13:22:05 +0900

    test : input 자동차 이름 글자수 유효성 검사 단위 테스트 추가

---
+++ src/test/java/racingcar/validation/ValidationTest.java
@@ -0,0 +1,18 @@
+package racingcar.validation;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ValidationTest {
+
+    @Test
+    void 다섯_글자를_초과한_입력_테스트() {
+        String input = "a,bb,ccc,dddd,eeeee,ffffff";
+        List<String> carNames = List.of(input.split(","));
+
+        assertThatThrownBy(() -> Validation.isLessThenSixLetters(carNames));
+    }
+
+}
