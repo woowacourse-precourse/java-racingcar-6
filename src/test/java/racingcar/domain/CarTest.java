d8507350ca03ae28922791b1d019a62a0ace95ae
hellomatia <shuaigejp80@gmail.com>
2023-11-01 20:49:41 +0900

    test(domain): CarTest클래스 추가.
    - 자동차 이름과, 크기를 저장하는 Car클래스를 테스트 함.
    - 이름, 초기 위치값, 위치 증가, 멈춤을 확인함.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,17 +1,39 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
 
     private Car car;
 
+    @BeforeEach
+    void 자동차_객체_생성() {
+        car = new Car("matia");
+    }
+
+    @Test
+    void 자동차_이름_확인() {
+        assertEquals(car.getName(), "matia");
+    }
+
+    @Test
+    void 자동차_위치_초기값_확인() {
+        assertEquals(car.getPosition(), 0);
+    }
+
+    @Test
+    void 자동차_위치_멈춤_확인() {
+        car.move(CarState.STOP);
+        assertEquals(car.getPosition(), 0);
+    }
+
+    @Test
+    void 자동차_위치_증가_확인() {
+        car.move(CarState.MOVE);
+        assertEquals(car.getPosition(), 1);
+    }
 }
 
