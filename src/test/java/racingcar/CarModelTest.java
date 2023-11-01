7e8a47f121dd480d14e7ff3da5bfbad42f3c282d
Minjoo <m0522inju@gmail.com>
2023-11-01 19:58:36 +0900

    style: 불필요한 import 구문 삭제

--- src/test/java/racingcar/CarModelTest.java
+++ src/test/java/racingcar/CarModelTest.java
@@ -1,18 +1,10 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.MockedStatic;
 import racingcar.model.Car;
 
 public class CarModelTest {
