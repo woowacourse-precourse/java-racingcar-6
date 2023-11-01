e7f0e9a5a1d6340cf99135337c794902ef73147e
ckkim817 <ckkim817@konkuk.ac.kr>
2023-11-01 20:47:35 +0900

    [Feat] 입력받은 이름으로 자동차 n대 생성 기능 구현

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -5,20 +5,31 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CarList {
 
-    private List<String> carNames;
+    private List<Car> cars;
 
     public void namesInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine().trim();
 
         List<String> carNames = Arrays.asList(input.split(","));
+        validateCarNames(carNames);
+
+        namingCars(carNames);
+    }
+
+    private void namingCars(List<String> carNames) {
+        this.cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validateCarNames(List<String> carNames) {
         validateCarNameFormat(carNames);
         validateNameDuplicate(carNames);
-
-        this.carNames = carNames;
     }
 
     private void validateCarNameFormat(List<String> carNames) {
