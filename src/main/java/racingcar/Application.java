8bc52f57d26018a6f3c77905e54891ce9a6d130a
songminhyuk <dnfldpden32@gmail.com>
2023-11-01 19:40:11 +0900

    feat: 실행결과 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,12 +10,22 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input=Console.readLine();
-        List<String> cars= List.of(input.split(","));
+        String[] splited=input.split(",");
+        ArrayList<Car> cars=new ArrayList<>();
+        for (int i=0;i<splited.length;i++){
+            cars.add(new Car(splited[i]));
+        }
         System.out.println("시도할 회수는 몇회인가요?");
         input=Console.readLine();
         final int count=Integer.parseInt(input);
 
+        //결과창 구현
         System.out.println("실행 결과");
-
+        for (int i=0;i<count;i++){
+            for(int j=0;j<cars.size();j++){
+                System.out.println(cars.get(j).name+" : "+cars.get(j).drive_random());
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,8 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private String name;
+    String name;
     private int distance=0;
+
+    public Car( String name) {
+        this.name=name;
+    }
 
     int drive_random(){
         if(Randoms.pickNumberInRange(0,9)>=4){
