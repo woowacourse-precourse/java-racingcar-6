5845446ea34fe4a435225c30313e24f2cebe4912
ehtjsv2 <ehtjsv2@naver.com>
2023-11-01 23:12:58 +0900

    Test: 자동차 객체 생성 테스트 추가

--- src/test/java/racingcar/developtest/CarFactoryTest.java
+++ src/test/java/racingcar/developtest/CarFactoryTest.java
@@ -3,16 +3,31 @@ package racingcar.developtest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.CarFactory;
 
 public class CarFactoryTest {
 
+
+    @Test
+    void createCars_두개_이상_자동차_객체_생성(){
+        InputStream readLine = setReadLine("pobi,crong");
+        System.setIn(readLine);
+        CarFactory carFactory = new CarFactory();
+        assertDoesNotThrow(carFactory::createCars);
+        Console.close();
+    }
+    @Test
+    void createCars_한개_일때_자동차_객체_생성(){
+        InputStream readLine = setReadLine("pobi");
+        System.setIn(readLine);
+        CarFactory carFactory = new CarFactory();
+        assertDoesNotThrow(carFactory::createCars);
+        Console.close();
+    }
     @Test
     void hasDuplicatedName_중복된_이름_가지고_있을때_예외() {
         InputStream readLine = setReadLine("pobi,pobi");
@@ -20,6 +35,7 @@ public class CarFactoryTest {
         CarFactory carFactory = new CarFactory();
         assertThatThrownBy(carFactory::createCars)
                 .isInstanceOf(IllegalArgumentException.class);
+        Console.close();
     }
 
     public static InputStream setReadLine(String readLine) {
