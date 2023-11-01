9a32254899d5b347f5c06ae6b9a9ca98a787d258
Yoo Hyunseok <brianyhs@naver.com>
2023-11-01 16:17:46 +0900

    refactor: ValidatorTest로 검증 테스트 분리

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,53 +1,10 @@
-package racingcar.car;
-
-import static org.assertj.core.api.Assertions.assertThat;
+package racingcar;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import racingcar.model.Car;
 import racingcar.validator.InputValidator;
 
-public class CarTest {
-
-    @Test
-    public void 자동차_전진_조건_부합() throws Exception{
-        // given
-        Car car = new Car("brian");
-
-        // when
-        car.moveForward(4);
-        car.moveForward(5);
-
-        // then
-        assertThat(car.getPosition()).isEqualTo(2);
-    }
-
-    @Test
-    public void 자동차_전진_조건_미달() throws Exception{
-        // given
-        Car car = new Car("brian");
-
-        // when
-        car.moveForward(2);
-        car.moveForward(3);
-
-        // then
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
-
-    @Test
-    public void 결과_출력() throws Exception{
-        // given
-        Car car = new Car("brian");
-
-        // when
-        car.moveForward(4);
-        car.moveForward(4);
-        car.moveForward(4);
-
-        // then
-        assertThat(car.getRoundResult()).isEqualTo("brian : ---");
-    }
+public class ValidatorTest {
 
     @Test
     public void 유효한_숫자_검증() throws Exception{
