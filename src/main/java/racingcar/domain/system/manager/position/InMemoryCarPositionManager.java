c499a7a072d5e411041496c5201edd361701f55b
belljun3395 <195850@jnu.ac.kr>
2023-11-01 14:45:59 +0900

    refactor(domain/system): CarPositionKey를 독립적인 키로 만듬으로 인한 수정
    - 저장인지 업데이트인지 확인하는 과정 명시화
    - key 획득 과정 명시화

--- src/main/java/racingcar/domain/system/manager/position/InMemoryCarPositionManager.java
+++ src/main/java/racingcar/domain/system/manager/position/InMemoryCarPositionManager.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import racingcar.domain.system.manager.car.value.SavedCar;
 import racingcar.domain.system.manager.position.key.CarPositionKey;
@@ -21,10 +22,44 @@ public class InMemoryCarPositionManager implements CarPositionManager {
      */
     private final Map<CarPositionKey, SavedCarPosition> positions = new TreeMap<>();
 
+    /**
+     * save로 save와 update를 동시에 사용하고 있음
+     */
     public SavedCarPosition save(SavedCar car, Long position) {
-        CarPositionKey key = CarPositionKey.of(car.getKey().getKey());
+        if (isUpdate(car)) {
+            CarPositionKey key = getKey(car);
+            return getSavedCarPosition(key, car, position);
+        }
+        CarPositionKey key = getKey();
+        return getSavedCarPosition(key, car, position);
+    }
+
+    private boolean isUpdate(SavedCar car) {
+        return positions.values().stream().anyMatch(position -> position.getSavedCar().equals(car));
+    }
+
+    private SavedCarPosition getSavedCarPosition(CarPositionKey key, SavedCar car, Long position) {
         positions.put(key, new SavedCarPosition(car, position, key));
         return positions.get(key);
+    }
+
+    /**
+     * isUpdate로 이미 존재 확인된 상태이기에 바로 Optional.get() 사용
+     */
+    private CarPositionKey getKey(SavedCar car) {
+        return positions.entrySet().stream().filter(getMatchCar(car)).findFirst().get().getKey();
+    }
+
+    private CarPositionKey getKey() {
+        int currentIdx = positions.size();
+        return CarPositionKey.of(currentIdx + 1L);
+    }
+
+    private Predicate<Entry<CarPositionKey, SavedCarPosition>> getMatchCar(SavedCar car) {
+        return e -> {
+            SavedCarPosition position = e.getValue();
+            return position.getSavedCar().equals(car);
+        };
     }
 
     public List<SavedCarPosition> saveAll(Map<SavedCar, Long> carPositions) {
