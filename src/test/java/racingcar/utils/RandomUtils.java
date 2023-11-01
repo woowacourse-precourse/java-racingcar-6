dd89f07d548491efda0d4144765384a4920bcd7d
Choijake <shameless8@naver.com>
2023-11-01 18:17:59 +0900

    Feat : Application에서 game 생성

--- src/test/java/racingcar/utils/RandomUtils.java
+++
@@ -1,9 +0,0 @@
-package racingcar.utils;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class RandomUtils {
-    public static int generateRandomNumber(){
-        return Randoms.pickNumberInRange(0, 9);
-    }
-}
