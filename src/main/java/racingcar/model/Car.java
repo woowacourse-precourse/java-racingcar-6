976839e07fc5501629fcb7a76c746f83060c0c71
siyeonSon <siyeon0209@naver.com>
2023-11-01 12:40:05 +0900

    refactor(car): 불필요한 상수 제거

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,7 +3,6 @@ package racingcar.model;
 import racingcar.util.NumberGenerator;
 
 public class Car {
-    private static final int LENGTH_LIMITER = 5;
     private static final int FORWARD_STANDARD = 4;
     private static final int INCREASE_AMOUNT = 1;
     private static final int MIN_NUMBER = 1;
