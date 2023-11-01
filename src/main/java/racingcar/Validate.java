d2b315b2e0abfaae5dec50aa776d62e871b2c0d9
iovsomnium <ksw09157.5212@gmail.com>
2023-11-01 23:27:33 +0900

    feat : Seperate method more

--- src/main/java/racingcar/Validate.java
+++ src/main/java/racingcar/Validate.java
@@ -1,21 +1,36 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Validate {
-    public static void validateInput(String input) throws IllegalArgumentException {
-        checkEmpty(input);
-        isInvalidLength(input);
-    }
 
-    public static void isInvalidLength(String input) throws IllegalArgumentException {
-        checkEmpty(input);
-        if (input.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-        }
-    }
-
-    public static void checkEmpty(String input) throws IllegalArgumentException {
+    public static void checkEmpty(String[] input) throws IllegalArgumentException {
         if (input == null) {
             throw new IllegalArgumentException("입력값이 null입니다");
         }
+    }
+
+    public static List<Car> validateCarName(String[] inputs) {
+        checkEmpty(inputs);
+        List<Car> cars = new ArrayList<>();
+        for (String input : inputs) {
+            if (input.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+            Car car = new Car(input);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    public static int checkNumber(String input) {
+        int result;
+        try {
+            result = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        return result;
     }
 }
