949c577931b2db7596116c035f3db94beedbc87b
Soonrang <clearsbyul30@naver.com>
2023-11-01 23:27:47 +0900

    리스트 타입 수정

--- src/main/java/racingcar/GenerateNumber.java
+++ src/main/java/racingcar/GenerateNumber.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public class GenerateNumber {
 
-    public List<Integer> GenerateRandomNumber(String[] players) {
+    public List<Integer> GenerateRandomNumber(List<String> players) {
         List<Integer> randomNumbers = new ArrayList<>();
-        for (int i = 0; i < players.length; i++) {
+        for (int i = 0; i < players.size(); i++) {
             randomNumbers.add(Randoms.pickNumberInRange(0, 9));
         }
         return randomNumbers;
