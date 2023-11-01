3561567957d4a711ea21f533c581d4e987083b3d
COBI-98 <tkdgus968@naver.com>
2023-11-01 18:20:50 +0900

    feat: 같은 위치의 자동차가 존재하는지 확인할 수 있다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,5 +28,21 @@ public class CarTest {
         assertThat(car.getDistance()).isEqualTo(curLocation);
     }
 
+    @DisplayName("같은 위치의 자동차가 존재하는지 확인할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"2,2,true","2,1,false"})
+    void isSameDistanceOfCar(int myCarLocation,
+                             int competeCarLocation,
+                             boolean sameDistanceCheck) throws Exception{
+        //given
+        Car myCar = new Car("pobi", myCarLocation);
+        Car competeCar = new Car("woni", competeCarLocation);
+
+        //when
+        boolean sameDistance = myCar.isSameDistance(competeCar);
+
+        //then
+        assertThat(sameDistance).isEqualTo(sameDistanceCheck);
+    }
 
 }
