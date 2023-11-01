9bd6f0b18504a4cbb713350f39a57a9dcc10a0a6
jihye134seo <jihye.seo134@gmail.com>
2023-11-01 14:43:39 +0900

    style(CarTest.java): 빈줄, 공백 수정

--- src/test/java/racingcar/Domain/CarTest.java
+++ src/test/java/racingcar/Domain/CarTest.java
@@ -1,53 +1,42 @@
 package racingcar.Domain;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
     @Test
-    void _0에서_9사이에_있는_무작위_값을_생성한다(){
-
+    void _0에서_9사이에_있는_무작위_값을_생성한다() {
         String input = "apple";
         Car car = new Car(input, 0);
         Integer result = car.createRandomValue();
 
         assertThat(result).isGreaterThanOrEqualTo(0);
         assertThat(result).isLessThan(10);
-
     }
 
-
     @Test
-    void 무작위_값이_4이상이라면_전진한다(){
-
+    void 무작위_값이_4이상이라면_전진한다() {
         String input = "apple";
         Car car = new Car(input, 0);
         Integer randomValue = car.createRandomValue();
 
-        if(car.checkRandomValueValidation(randomValue)) {
-
+        if (car.checkRandomValueValidation(randomValue)) {
             car.moveCar(randomValue);
             assertThat(car.getPoint()).isEqualTo(randomValue);
         }
-
     }
 
     @Test
-    void 무작위_값이_4미만이라면_정지한다(){
-
+    void 무작위_값이_4미만이라면_정지한다() {
         String input = "apple";
         Car car = new Car(input, 0);
         Integer randomValue = car.createRandomValue();
 
-        if(!car.checkRandomValueValidation(randomValue)) {
+        if (!car.checkRandomValueValidation(randomValue)) {
             assertThat(car.getPoint()).isEqualTo(0);
         }
-
     }
-
-
 
 }
 
