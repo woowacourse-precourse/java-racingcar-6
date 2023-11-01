63397aeb22d9f7daad9cefdb5c307e91defd8e17
nhn1030 <shgusska12@gmail.com>
2023-11-01 15:11:10 +0900

    feat: 유효한 이름인지 확인하는 isValidName 메서드 정의 - setName메서드에서 유효성 검사를 통과 시 - car의 setCarName 메서드를 호출하여 매개 값 변경

--- src/main/java/racingcar/model/CarNameManager.java
+++ src/main/java/racingcar/model/CarNameManager.java
@@ -1,7 +1,27 @@
 package racingcar.model;
 
 public class CarNameManager {
+
     public void setName(Car car, String name) {
-        car.setCarName(name);
+        if (isValidName(name)) {
+            car.setCarName(name);
+        } else {
+            throw new IllegalArgumentException("Invalid name format.");
+        }
+    }
+
+    private boolean isValidName(String name) {
+
+        String[] names = name.split(",");
+        if (names.length == 0) {
+            return false;
+        }
+        for (String singleName : names) {
+            if (singleName.length() > 5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
