e36aa976ead7dbf317c22deaf85854c3dee236f7
siho-song <thdtlgh234@naver.com>
2023-11-01 14:31:15 +0900

    [Test] 출력관련 테스트 베이스코드 구현
    상속을 위해 구현하였음

---
+++ src/test/java/racingcar/TestBase.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+
+    private PrintStream standardOut;
+    private OutputStream captor;
+
+    @BeforeEach
+    protected final void init() {
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+    }
+
+    @AfterEach
+    protected final void printOutput() {
+        System.setOut(standardOut);
+        System.out.println(output());
+    }
+
+    protected final String output() {
+        return captor.toString().trim();
+    }
+
+}
