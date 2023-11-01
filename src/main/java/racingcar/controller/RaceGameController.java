5a72a67827cf11551febdd47d533a4f413bd7cb7
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-01 16:01:49 +0900

    feat(RaceGameController): 스켈레톤 코드 작성

---
+++ src/main/java/racingcar/controller/RaceGameController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RaceGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+
+    }
+}
