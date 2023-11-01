ce23ca7bb3b7886192b7c1cc0fe295f936e93f61
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-01 18:36:48 +0900

    feat(racingcar): 임의의 수가 4 이상이면 전진

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,9 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
 
     public String name;
+    public int distance = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void takeTurn() {
+        int randomNumber = getRandomNumber();
+        if (canMoveForward(randomNumber)) {
+            this.distance += 1;
+        }
     }
 
     public boolean canMoveForward(int randomNumber) {
