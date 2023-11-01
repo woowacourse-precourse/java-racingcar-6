bc02fa38398546651d6c2b6dd441c13b3202c01a
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 20:39:47 +0900

    feat : printErrorMessage 함수 생성

--- src/main/java/racingcar/io/OutputHandler.java
+++ src/main/java/racingcar/io/OutputHandler.java
@@ -4,4 +4,8 @@ public class OutputHandler {
     public static void printMessage(String message){
         System.out.println(message);
     }
+
+    public static void printErrorMessage(String message){
+        System.err.println(message);
+    }
 }
