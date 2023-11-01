60045c682d64d844aef206f11fc864a335a6ca81
YEONGORI <dusrjf14@gmail.com>
2023-11-01 22:42:29 +0900

    [feat] 사용자 입력 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        String[] cars = names.split(",");
+
+        int len = cars.length;
+        int[] cars_move = new int[len];
+
+        int count;
+        try {
+            System.out.println("시도할 회수는 몇회인가요?");
+            count = Integer.parseInt(Console.readLine());
+            System.out.println();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < len; j++) {
+                if (Randoms.pickNumberInRange(1, 9) >= 4) {
+                    cars_move[j] += 1;
+                }
+            }
+
+        }
     }
 }
