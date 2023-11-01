fd85d4197ccd446eb457f3f337dd71576ea3a958
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 20:18:06 +0900

    test(DistanceTest): Distance 객체 생성 테스트
    테스트 메소드
      1. 매개변수_없는_생성자를_통한_객체생성
      2. 정수_하나를_요구하는_생성자를_통한_객체_생성
        - 전달하는 정수는 0이상 Integer.MAX_VALUE 이하의 정수이다.
      3. 음수를_전달하여_객체를_생성하면_실패
        - 음의 정수를 전달하여 객체 생성 시도
      4. 너무_큰_정수를_전달하여_객체를_생성하면_실패
        - 반드시 컴파일 에러가 발생해야 함. 이를 확인하고 주석 처리할 것

--- src/test/java/racingcar/model/DistanceTest.java
+++ src/test/java/racingcar/model/DistanceTest.java
@@ -2,6 +2,40 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DistanceTest {
+import org.junit.jupiter.api.Test;
 
+class DistanceTest {
+    @Test
+    void 매개변수_없는_생성자를_통한_객체생성() {
+        try {
+            Distance d = new Distance();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void 정수_하나를_요구하는_생성자를_통한_객체_생성() {
+        try {
+            Distance d = new Distance(10);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void 음수를_전달하여_객체를_생성하면_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Distance d = new Distance(-1);
+        });
+    }
+
+//    // 너무 큰 정수를 전달하면 컴파일 에러가 발생함을 확인
+//    @Test
+//    void 너무_큰_정수를_전달하여_객체를_생성하면_실패() {
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            Long tooBig = new Long(Integer.MAX_VALUE + 1);
+//            Distance d = new Distance(tooBig);
+//        });
+//    }
 }
