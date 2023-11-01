4664585bb4ec442092bf1aa107063694e0390be0
slowcloud <sju9417@gmail.com>
2023-11-01 20:29:48 +0900

    refactor(GameControllerTest): NsTest를 상속시켜 간결하게 변경

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -1,38 +1,23 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import racingcar.Application;
 import racingcar.domain.Attempt;
 import racingcar.domain.Car;
 import racingcar.domain.RandomNumber;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mockStatic;
 
-class GameControllerTest {
-
-    private final static PrintStream stdout = System.out;
-    private final static ByteArrayOutputStream captor = new ByteArrayOutputStream();
-
-    @BeforeAll
-    public static void beforeAll() {
-        System.setOut(new PrintStream(captor));
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        System.setOut(stdout);
-    }
+class GameControllerTest extends NsTest {
 
     @Test
     void play_게임_실행_테스트() {
@@ -47,13 +32,17 @@ class GameControllerTest {
         GameController gameController = new GameController(inputView, new OutputView());
         gameController.play();
 
-        String output = captor.toString().trim();
-        Assertions.assertThat(output).contains(
+        Assertions.assertThat(output()).contains(
                 "실행 결과", "foo : -", "bar : -", "최종 우승자 : foo, bar"
         );
 
         randomNumber.close();
 
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 
 }
