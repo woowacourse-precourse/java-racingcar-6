d771d725c1f4940111c35e3390a993a87fb82e08
peeaaccee <wlgp8958@naver.com>
2023-11-01 23:35:17 +0900

    feat(InputView): 차들의 이름을 입력받는 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    public List<String> inputNamesStringToList() {
+        String inputNames = Console.readLine();
+        Validator.validateEndsWith(inputNames);
+        return Arrays.stream(inputNames.split(","))
+                .collect(Collectors.toList());
+    }
+
+}
