8b5fd1df509c71465bea76c17cf6601c0d7e7376
plan11plan <tjtfy7028@gmail.com>
2023-11-01 22:28:48 +0900

    style:[update][InputView] inputCounts->inputRounds 이름변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -46,7 +46,7 @@ public class InputView {
         }
         return trimmedCarNames;
     }
-    public static int inputCounts(){
+    public static int inputRounds(){
         System.out.println("시도할 회수는 몇회인가요?");
         String input = readLine().trim();
         int intCounts = stringToInteger(input);
