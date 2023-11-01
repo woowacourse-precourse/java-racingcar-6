c38cf7251c3df4fda38853960a6705cfe28c238f
cheolhyeon <tghy4@naver.com>
2023-11-01 16:17:37 +0900

    refactor : 코드 구성 개선

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -3,10 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-    public String runRaceBasedOnInput() {
-        return Console.readLine();
-    }
     public String[] inputCarNames() {
         return Console.readLine().split(",");
+    }
+    public int inputNumber() {
+        return Integer.parseInt(Console.readLine());
     }
 }
