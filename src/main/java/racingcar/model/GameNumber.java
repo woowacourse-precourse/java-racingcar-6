acb011b768361c75b15e210c8824627316efb1a8
djc06048 <gsafe1213@gmail.com>
2023-11-01 12:13:36 +0900

    style: 자바 코딩 규칙 반영해 숫자를 클래스 상수로 변경

--- src/main/java/racingcar/model/GameNumber.java
+++ src/main/java/racingcar/model/GameNumber.java
@@ -3,10 +3,9 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameNumber {
-    private int randomNumber;
-
+    private static final int RANDOMNUMBER_START = 0;
+    private static final int RANDOMNUMBER_END= 9;
     public int generateRandomNumber() {
-        randomNumber = Randoms.pickNumberInRange(0, 9);
-        return randomNumber;
+        return Randoms.pickNumberInRange(RANDOMNUMBER_START, RANDOMNUMBER_END);
     }
 }
