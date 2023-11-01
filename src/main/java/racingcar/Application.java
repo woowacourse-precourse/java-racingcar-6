42fc952df4ffb74fada64793840e3ab557a05ac7
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-01 23:41:00 +0900

    feat(Application): add 3 methods to get cars by input and check lengths of car names
    - need to get cars to play in raning
    - GetCars() : when user inputs cars, get cars splitted by ","
    - CheckCars() : check whether length of car name is under 5 or not
    - if length of the car name is over 5, throw IllegalArgumentException

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,15 +6,30 @@ public class Application {
     public static void printGuideSentence(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+    public static String[] getCars(){
+        String[] carArr = Console.readLine().split(",");
+        return carArr;
+    }
+    public static void checkCars(String[] cars){
+        for (String car:cars) {
+            if (car.length()>5)
+                throw new IllegalArgumentException();
+        }
+    }
     public static void askTrialCount(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         //  안내문 출력
         printGuideSentence();
+        //  차 이름 배열 입력 받기
+        String[] inputCars = getCars();
+        //  이름 글자 수 확인
+        checkCars(inputCars);
+
         //  시도 횟수 질문 출력
         askTrialCount();
     }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,9 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+    public String name;
+    public int moveCount;
+    public void moveForward(){
+        moveCount += 1;
+    }
 }
