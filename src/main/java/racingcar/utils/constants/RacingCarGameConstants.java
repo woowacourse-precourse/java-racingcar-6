f41fc87888e284118406334850d0ef03a05d9bcb
JooDukHan <bfinecpa1@naver.com>
2023-11-01 18:47:24 +0900

    Style: 공유되는 상수 클래스 생성

---
+++ src/main/java/racingcar/utils/constants/RacingCarGameConstants.java
@@ -0,0 +1,8 @@
+package racingcar.utils.constants;
+
+public class RacingCarGameConstants {
+
+    public static final String INPUT_ERROR_MESSAGE = "잘못된 입력을 했습니다.";
+    public static final String COMMA_DELIMITER = ",";
+
+}
