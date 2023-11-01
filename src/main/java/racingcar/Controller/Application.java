38e58f32e21dab8acae50e0ee6af83f9e8160710
eunjin <12191804@inha.edu>
2023-11-01 19:53:55 +0900

    refactor: main 실행 함수와 입출력 분리

---
+++ src/main/java/racingcar/Controller/Application.java
@@ -0,0 +1,18 @@
+package racingcar.Controller;
+
+import java.util.List;
+
+import racingcar.Model.RacingGame;
+import racingcar.View.RacingGameView;
+
+public class Application {
+    public static void main(String[] args) {
+        RacingGameView view = new RacingGameView();
+
+        List<String> carNames = view.requestCarNames();
+        int rounds = view.requestRounds();
+
+        RacingGame racingGame = new RacingGame(carNames, rounds);
+        racingGame.start(view);
+    }
+}
