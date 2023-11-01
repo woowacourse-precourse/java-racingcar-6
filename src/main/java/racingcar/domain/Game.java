755ebb796bfd403c2d7eea10afbf858a4b02cc40
kite707 <12201680@inha.edu>
2023-11-01 19:53:12 +0900

    feat(racingcar): 횟수 1회 진행 기능 구현
    게임을 1회 진행하는 movingCars함수를 구현하였습니다.

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -3,7 +3,16 @@ package racingcar.domain;
 import java.util.List;
 
 public class Game {
-    public List<Integer> movingCars(List<String> carNames, List<Integer> scores) {
-        return null;
+    public List<Integer> movingCars(List<Integer> scores) {
+        final Engine engine = new Engine();
+        for (int carIdx = 0; carIdx < scores.size(); carIdx++) {
+            boolean result = engine.isMovingForward();
+            if (result) {
+                int beforeScore = scores.get(carIdx);
+                scores.set(carIdx, beforeScore + 1);
+            }
+        }
+        return scores;
     }
+    
 }
