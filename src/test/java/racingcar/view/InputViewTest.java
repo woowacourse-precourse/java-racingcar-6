5104d622653691ed1301d4af9185ea4a5c4e8df1
Dawin <dawin002@naver.com>
2023-11-01 18:54:12 +0900

    test(InputViewTest): 사용자 입력 관련 기능 테스트 추가
    사용자 입력에 관련된 기능 테스트 추가:
    - readCarNames_사용자가_입력한_자동차_이름을_문자열로_반환()
    - readRoundNumber_사용자가_입력한_이동_회수를_문자열로_반환()

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,60 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    private String getSystemOutput() {
+        return output.toString();
+    }
+
+    private InputStream createUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @BeforeEach
+    public void setUpStream() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restoreStream() {
+        System.setOut(System.out);
+        output.reset();
+    }
+
+    @Test
+    void readCarNames_사용자가_입력한_자동차_이름을_문자열로_반환() {
+        //given
+        System.setIn(createUserInput("dawin,woowa,han"));
+
+        //when
+        String carNamesInput = InputView.readCarNames();
+
+        //then
+        assertThat(getSystemOutput()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        assertThat(carNamesInput).isEqualTo("dawin,woowa,han");
+    }
+
+    @Test
+    void readRoundNumber_사용자가_입력한_이동_회수를_문자열로_반환() {
+        //given
+        System.setIn(createUserInput("15"));
+
+        //when
+        String carNamesInput = InputView.readRoundNumber();
+
+        //then
+        assertThat(getSystemOutput()).isEqualTo("시도할 회수는 몇회인가요?\n");
+        assertThat(carNamesInput).isEqualTo("15");
+    }
+}
