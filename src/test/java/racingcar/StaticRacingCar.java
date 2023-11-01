0b17f8b2a17aa771a22a29ce0605217bbbfa64d3
cheolhyeon <tghy4@naver.com>
2023-11-01 20:21:54 +0900

    refactor : 최종 우승자를 출력하기 위해 필요한 테스트 코드 작성

--- src/test/java/racingcar/StaticRacingCar.java
+++ src/test/java/racingcar/StaticRacingCar.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.car.Car;
 
 public class StaticRacingCar {
@@ -30,5 +32,23 @@ public class StaticRacingCar {
         }
         return result.toString();
     }
+    static int findMaxDistance(List<Car> racingCarsList) {
+        return racingCarsList.stream()
+                .max(Comparator.comparingInt(Car::getCurrentPosition))
+                .map(Car::getCurrentPosition)
+                .orElse(0);
+    }
+    static List<Car> decideWinner(List<Car> racingCarsList) {
+        int winnerDistance = findMaxDistance(racingCarsList);
 
+        return racingCarsList.stream()
+                .filter(car ->car.getCurrentPosition() == winnerDistance)
+                .collect(Collectors.toList());
+    }
+
+    static String[] toArrayWinner(List<Car> winner) {
+        return winner.stream()
+                .map(car -> car.getCarName())
+                .toArray(String[]::new);
+    }
 }
