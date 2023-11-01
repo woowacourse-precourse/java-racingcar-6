f91bb05a612a23496a498d09112ed4c2a911d167
cropkim <kjt0518@gmail.com>
2023-11-01 15:13:37 +0900

    RacingScoreBoard 클래스 리팩토링 - 자동차의 점수 올리는 메소드 - 우승자를 찾기 위해 현재의 점수를 반환하는 메소드

---
+++ src/main/java/racingcar/RacingScoreBoard.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RacingScoreBoard {
+    private final Map<String, Integer> scores;
+
+    //자동차 입력을 받고 이동 횟수는 0으로 초기화 된 HashMap을 만든다.
+    //HashMap(이름, 이동 횟수)
+    public RacingScoreBoard(String inputNames) {
+        scores = new HashMap<>();
+        String[] names = inputNames.split(",");
+        for (String name : names) {
+            scores.put(name.trim(), 0);
+        }
+    }
+
+    public void moveCar(String carName) {
+        scores.put(carName, scores.get(carName) + 1);
+    }
+
+    public Map<String, Integer> currentScores() {
+        return new HashMap<>(scores);
+    }
+
+}
