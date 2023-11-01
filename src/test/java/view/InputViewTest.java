65b0c8fae69818b18a2ddc511fbc01a49d8b3962
yeahdy <yejine98@naver.com>
2023-11-01 20:20:47 +0900

    test(isBlankTest()): 값 유무 검사 테스트

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -17,11 +17,11 @@ public class InputViewTest {
         inputView = new InputView();
     }
 
-    @DisplayName("자동차 이름들 값 유무검사")
+    @DisplayName("값 유무 검사")
     @ParameterizedTest
-    @ValueSource(strings = {"잠온다,자고싶다", ",","라라", " ","   ","라라, ","여섯글자인데, 붕붕이","다   섯"})
-    void validateCarNamesTest(String carNames){
-        Assertions.assertThatThrownBy(() -> assertThat(inputView.isCarNames(carNames)).isTrue())
+    @ValueSource(strings = {""," ",",","라라","''","라라, ","다   섯"})
+    void isBlankTest(String data){
+        Assertions.assertThatThrownBy(() -> assertThat(inputView.isBlank(data)).isTrue())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
