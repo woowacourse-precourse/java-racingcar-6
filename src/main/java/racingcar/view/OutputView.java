30564c81b6a636e11b0ff2d00a56c31a4a8dbe30
DonghaOh <donghadarwin@naver.com>
2023-11-01 17:21:09 +0900

    feat: 게임 실행 결과 문구 출력 - gameResultOutputPrint 메소드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,9 @@ import racingcar.model.Car;
 import racingcar.model.CarList;
 
 public class OutputView {
+    public static void gameResultOutputPrint(){
+        System.out.println("실행 결과");
+    }
     public static void gameRoundResultPrint(CarList carList){
         System.out.println(carList.carListMoveCountString());
     }
