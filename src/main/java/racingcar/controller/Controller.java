dd06f3d13562d54bdb938851e77736538276fcd9
nari <avakray@hansung.ac.kr>
2023-11-01 17:21:48 +0900

    feat(racingcar): User Input을 통한 자동차 객체 생성 메서드 작성

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -3,27 +3,34 @@ package racingcar.controller;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.view.InputView;
 import racingcar.view.outputView;
+import racingcar.repository.CarAdmin;
 import racingcar.util.Parser;
+import racingcar.domain.Car;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Controller {
-    List<String> carNames;
-    Parser parser;
+    private final Parser parser = new Parser();
+    private final CarAdmin carAdmin = new CarAdmin();
 
     public void testRun(){
-        parser = new Parser();
+        createCars(inputCarNames()); //자동차 생성
 
-        outputView.printRacingResult(getCarNames(), getRacingRound());
+        //outputView.printRacingResult(inputCarNames(), inputRacingRound());
     }
 
-    private int getRacingRound(){
-        InputView.requestRacingRoundNumber();
-        return parser.changeStringToInteger(Console.readLine());
+    private void createCars(List<String> carNames) {
+        for (String carName : carNames) {
+            carAdmin.createCar(new Car(carName)); //자동차별 객체 생성
+        }
     }
-
-    private List<String> getCarNames(){
-        InputView.requestCarNaming();
-        return parser.getSplitString(Console.readLine());
+    private List<String> inputCarNames(){
+        InputView.requestCarNaming(); //요청문구 출력
+        return parser.getSplitString(Console.readLine()); //리스트화 및 예외처리
+    }
+    private int inputRacingRound(){
+        InputView.requestRacingRoundNumber(); //요청문구 출력
+        return parser.changeStringToInteger(Console.readLine()); //타입변환 및 예외처리
     }
 }
