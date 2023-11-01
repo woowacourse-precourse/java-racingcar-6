c413f2eb7fd24294a5aee75c96281d0ef288e8ff
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-01 13:41:25 +0900

    2023.11.01_feat : CarNameValidator isRightNamesString 메서드 생성

---
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import racingcar.Constants;
+import racingcar.Utils;
+
+import java.util.ArrayList;
+
+public class CarNameValidator {
+    private final String NAMES_STRING;
+    public final ArrayList<String> NAMES;
+
+    public CarNameValidator(String namesString) {
+        this.NAMES_STRING = namesString;
+        this.NAMES = Utils.toArrayList(namesString);
+        validate();
+    }
+
+    public void validate() {
+
+    }
+
+    public void isRightNamesString() {
+        // String 형식 확인 : 앞&뒤-문자, 중간-문자&구분자
+        if (Constants.nameStringPattern.matcher(NAMES_STRING).matches()) {
+            throw new IllegalArgumentException(Constants.CAR_NAME_WRONG_ERROR);
+        }
+    }
+}
