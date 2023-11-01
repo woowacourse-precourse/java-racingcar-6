da0b95d039541bb4612496b321994bd52a4a0297
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-01 17:36:08 +0900

    test: 숫자가 아닌 문자열 입력할 경우 IllegalArgumentException 에러 테스트

--- src/test/java/racingcar/ConverterTest.java
+++ src/test/java/racingcar/ConverterTest.java
@@ -6,6 +6,7 @@ import racingcar.utils.Converter;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ConverterTest {
     @Test
@@ -27,5 +28,13 @@ public class ConverterTest {
         int actualCount = Converter.toInteger(count);
         //then
         assertThat(actualCount).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자가_아닌_문자열을_입력할_경우_에러() {
+        // given
+        String count = "5afa";
+        //then
+        assertThatThrownBy(() -> Converter.toInteger(count)).isInstanceOf(IllegalArgumentException.class);
     }
 }
