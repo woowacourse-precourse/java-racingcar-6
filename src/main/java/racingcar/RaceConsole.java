4e71fcf739e32fef9694c9ae3b8b8f2a9e9e6e6a
김민주 <69795199+ks00919@users.noreply.github.com>
2023-11-01 22:20:17 +0900

    refactor: 주석 제거

--- src/main/java/racingcar/RaceConsole.java
+++ src/main/java/racingcar/RaceConsole.java
@@ -8,16 +8,6 @@ import java.util.function.Predicate;
 public class RaceConsole {
 
     public void start() {
-//        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-//        String names = Console.readLine();
-//        String[] carNames = names.split(",");
-//
-//        System.out.println("시도할 회수는 몇회인가요?");
-//        String round = Console.readLine();
-//        if (!round.matches("\\d+")) {
-//            throw new IllegalArgumentException("getCount");
-//        }
-
         String[] carNames = getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
                 s -> false,
                 s -> s.split(","));
