1abbacd081a7c5dff252edb56b6b44bddf94b90a
KimJiyun <magi83105@gmail.com>
2023-11-01 14:37:49 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = getCarNames();
+    }
+
+    private static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
+        String userInput = Console.readLine();
+        String[] carNames = userInput.split(",");
+
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > 5) {
+                throw new IllegalArgumentException("5자 이내로 입력해야 합니다.");
+            }
+        }
+
+        return carNames;
     }
 }
