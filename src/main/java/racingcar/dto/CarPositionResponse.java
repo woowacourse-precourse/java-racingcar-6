bcfbaf329fd2d438acc04c3808c8824c716fd752
nahyeon99 <nahyeonee99@gmail.com>
2023-11-01 14:51:16 +0900

    feat(dto/CarPositionResponse): 자동차 이동 횟수(위치) 응답 dto 구현
    1. 1회 게임 실행 시, 실행 결과를 출력을 위해 '자동차 위치 응답 dto' 작성
    2. 멤버변수 : name(String), position(int)

---
+++ src/main/java/racingcar/dto/CarPositionResponse.java
@@ -0,0 +1,26 @@
+package racingcar.dto;
+
+import racingcar.domain.Car;
+
+public class CarPositionResponse {
+
+    private final String name;
+    private final int position;
+
+    private CarPositionResponse(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public static CarPositionResponse create(Car car) {
+        return new CarPositionResponse(car.getName(), car.getMoveCounts());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+}
