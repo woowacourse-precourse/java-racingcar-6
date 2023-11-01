be5911da4e567428e0ca23cbf947ea01f445ad9b
woong <hiidy115@gmail.com>
2023-11-01 20:29:22 +0900

    :sparkles: feat: 에러 메시지 추가 시도 횟수와 관련된 에러 메시지를 추가함.

--- src/main/java/racingcar/utils/Message.java
+++ src/main/java/racingcar/utils/Message.java
@@ -6,4 +6,6 @@ public class Message {
     public static final String NULL_ERROR_MESSAGE = "null 값을 입력할 수 없습니다.";
     public static final String DUPLICATE_NAME_ERROR_MESSAGE = "중복된 이름을 입력할 수 없습니다.";
     public static final String NAME_LENGTH_ERROR_MESSAGE = "이름은 최대 5자까지 입력 가능합니다.";
+    public static final String NOT_POSITIVE_NUMBER_ERROR_MESSAGE = "시도 횟수는 1 이상의 숫자를 입력해야 합니다.";
+    public static final String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해야 합니다.";
 }
