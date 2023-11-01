6be1a0c810495a278858d4492bd6e7aa9fc4821d
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-01 21:18:47 +0900

    test: 함수별 테스트 추가

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,31 +1,46 @@
 package domain;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CarsTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+public class CarsTest {
 
-    @DisplayName("우승자는 두 명이어야 한다.")
     @Test
-    void findWinners() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi, woni, jun", "1");
-                    assertThat(output()).contains("pobi : -", "woni : -", "jun : ", "최종 우승자 : pobi, woni");
-                },
-                MOVING_FORWARD, MOVING_FORWARD, STOP
-        );
+    @DisplayName("우승자는 한 명이어야 한다.")
+    void findWinner() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("KIA", 1));
+        carList.add(new Car("BMW", 0));
+        carList.add(new Car("AUDI", 0));
+
+        List<String> names = List.of("KIA", "BMW", "AUDI");
+        Cars cars = new Cars(names);
+        cars.setCars(carList);
+        List<String> result = cars.findWinners();
+
+        assertThat(result).containsExactly("KIA");
     }
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
+    @Test
+    @DisplayName("우승자는 두 명이어야 한다.")
+    void findWinners() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("KIA", 1));
+        carList.add(new Car("BMW", 1));
+        carList.add(new Car("AUDI", 0));
+
+        List<String> names = List.of("KIA", "BMW", "AUDI");
+        Cars cars = new Cars(names);
+        cars.setCars(carList);
+        List<String> result = cars.findWinners();
+
+        assertThat(result).containsExactly("KIA", "BMW");
     }
 }
