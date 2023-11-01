91e063765a4412dbeb22faa0ca2f7664f496276f
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-01 20:24:33 +0900

    test : inputNumbersOfMatchTest 구현
    GameInitializerTest.inputNumbersOfMatchTest 구현

--- src/test/java/manager/GameInitializerTest.java
+++ src/test/java/manager/GameInitializerTest.java
@@ -3,18 +3,25 @@ package manager;
 import custom_object.Car;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.ByteArrayInputStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameInitializerTest {
+
+    void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
     @ParameterizedTest
     @MethodSource("inputCarNameTestArguments")
     @DisplayName("inputCarNameTest")
@@ -34,6 +41,29 @@ public class GameInitializerTest {
         return Stream.of(
                 Arguments.of(new String[]{"test1"}, new String[]{"test1-0"}),
                 Arguments.of(new String[]{"test1", "test2"}, new String[]{"test1-0", "test2-0"})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputNumbersOfMatchTestArguments")
+    @DisplayName("inputNumbersOfMatchTest")
+    public void inputNumbersOfMatchTest(String numberOfMatch, int answer) {
+        GameInitializer gameInitializer = new GameInitializer();
+        int result;
+
+        provideInput(numberOfMatch);
+        result = gameInitializer.inputNumbersOfMatch();
+
+        Console.close();
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    public static Stream<Arguments> inputNumbersOfMatchTestArguments() {
+        return Stream.of(
+            Arguments.of("1", 1),
+            Arguments.of("2", 2),
+            Arguments.of("3", 3)
         );
     }
 }
