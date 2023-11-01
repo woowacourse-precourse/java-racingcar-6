97b023f30840319b97cfe2de158e6025a68d62e9
jjongwon7 <jjongwon7@naver.com>
2023-11-01 23:12:33 +0900

    fix: 자동차 이름 길이 Exception 문구 수정 RACING_CAR_MAX_LENGTH 이상 RACING_CAR_MAX_LENGTH 이하로 출력되던 메시지를 올바르게 수정

--- src/main/java/domain/RacingGameStartManager.java
+++ src/main/java/domain/RacingGameStartManager.java
@@ -38,7 +38,7 @@ public class RacingGameStartManager {
     private void validateRacingCarName(String racingCarName, Set<RacingCar> racingCarSet) {
         if (!isRacingCarNameLengthInRange(racingCarName)) {
             throw new IllegalArgumentException(
-                    "자동차 이름은 " + RACING_CAR_MAX_LENGTH + "자 이상 " + RACING_CAR_MAX_LENGTH + "자 이하만 입력 가능합니다."
+                    "자동차 이름은 " + RACING_CAR_MIN_LENGTH + "자 이상 " + RACING_CAR_MAX_LENGTH + "자 이하만 입력 가능합니다."
             );
         } else if (!isRacingCarNameUnique(racingCarName, racingCarSet)) {
             throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
