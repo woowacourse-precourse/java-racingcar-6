b1cbb79f6d0b0e297ba739d15afd6e5e848c0ac7
win9 <krrr883@gmail.com>
2023-11-01 16:07:03 +0900

    [Refactor]: 입출력 상수 변경
    입출력 문자 상수로 변경

--- src/main/java/racingcar/view/InputMessage.java
+++ src/main/java/racingcar/view/InputMessage.java
@@ -7,14 +7,18 @@ import racingcar.vo.TryTime;
 import java.util.List;
 
 public class InputMessage {
+
+    public static final String CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_REQUEST = "시도할 회수는 몇회인가요?";
+
     public static List<String> printCarNameInputMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAME_REQUEST);
         String input = Console.readLine();
         return Parser.parseCarNameInput(input);
     }
 
     public static TryTime printTryTimesInputMessage() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(TRY_COUNT_REQUEST);
         int time = Parser.parseTryTimesInput(Console.readLine());
         return new TryTime(time);
     }
--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class OutputMessage {
     static final String BAR = "-";
     public static final String COLON = ":";
+    public static final String WINNER_RESULT = "최종 우승자 " + COLON + " ";
     public static final String COMMA = ",";
+    public static final String PLAY_RESULT = "\n실행 결과";
 
     public static void printPlayOutputMessage() {
-        System.out.println("\n실행 결과");
+        System.out.println(PLAY_RESULT);
     }
 
     public static void printRacingProgressOutputMessage(List<Car> list) {
@@ -21,11 +23,11 @@ public class OutputMessage {
     }
 
     public static void printResultOutputMessage(List<Car> resultList) {
-        StringBuilder resultBuilder = new StringBuilder("최종 우승자 " + COLON + " ");
+        StringBuilder resultBuilder = new StringBuilder(WINNER_RESULT);
         for (Car car : resultList) {
             resultBuilder.append(car.getName() + COMMA + " ");
         }
         resultBuilder.delete(resultBuilder.length() - 2, resultBuilder.length());
-        System.out.println(resultBuilder.toString());
+        System.out.println(resultBuilder);
     }
 }
