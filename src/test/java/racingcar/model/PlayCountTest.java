5b1f9a1cb1003485e6934b752f9ee84aa377e263
devholic22 <hyunjoon.tech@gmail.com>
2023-11-01 13:05:52 +0900

    refactor: 횟수 예외처리 Nested 추가
    횟수 예외처리에 Nested 및 DisplayName을 붙임으로써 구조화

--- src/test/java/racingcar/model/PlayCountTest.java
+++ src/test/java/racingcar/model/PlayCountTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static racingcar.view.exception.InputException.PLAY_COUNT_VALUE_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class PlayCountTest {
@@ -16,19 +17,24 @@ public class PlayCountTest {
         assertDoesNotThrow(() -> PlayCount.createDefault("5"));
     }
 
-    @Test
-    @DisplayName("음수를 입력했을 시 예외가 발생한다.")
-    void negativeNumberInputExceptionTest() {
-        // when & then
-        assertThatThrownBy(() -> PlayCount.createDefault("-5")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(PLAY_COUNT_VALUE_EXCEPTION.getMessage());
-    }
+    @Nested
+    @DisplayName("횟수 예외 테스트")
+    class PlayCountExceptionTest {
 
-    @Test
-    @DisplayName("일반 문자를 입력했을 시 예외가 발생한다.")
-    void notNumberInputExceptionTest() {
-        // when & then
-        assertThatThrownBy(() -> PlayCount.createDefault("abc")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(PLAY_COUNT_VALUE_EXCEPTION.getMessage());
+        @Test
+        @DisplayName("음수를 입력했을 시 예외가 발생한다.")
+        void negativeNumberInputExceptionTest() {
+            // when & then
+            assertThatThrownBy(() -> PlayCount.createDefault("-5")).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(PLAY_COUNT_VALUE_EXCEPTION.getMessage());
+        }
+
+        @Test
+        @DisplayName("일반 문자를 입력했을 시 예외가 발생한다.")
+        void notNumberInputExceptionTest() {
+            // when & then
+            assertThatThrownBy(() -> PlayCount.createDefault("abc")).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(PLAY_COUNT_VALUE_EXCEPTION.getMessage());
+        }
     }
 }
