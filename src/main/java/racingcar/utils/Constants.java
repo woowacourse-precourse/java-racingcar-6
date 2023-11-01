0b74184f59c6fa5a37c9a6f70c4a19612b520ed4
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-01 14:56:18 +0900

    feat: 상수 MINIMUM_MOVE_COUNTS_FOR_INPUT 등록

--- src/main/java/racingcar/utils/Constants.java
+++ src/main/java/racingcar/utils/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final int MINIMUM_GENERATE_FUEL_LEVEL = 0;
     public static final int MAXIMUM_GENERATE_FUEL_LEVEL = 9;
     public static final int MINIMUM_MOVE_COUNTS = 0;
+    public static final int MINIMUM_MOVE_COUNTS_FOR_INPUT = 1;
     public static final int MINIMUM_MOVE_FUEL_LEVEL = 4;
 }
