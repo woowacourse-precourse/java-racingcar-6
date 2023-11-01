5a5e24411a4c1f1b71c274ce185bf43c7d850edd
gyeommm <dkndob02@naver.com>
2023-11-01 17:57:07 +0900

    애플리케이션 main함수 작성(기본 제공된 테스트 코드 all success)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,27 @@
 package racingcar;
 
+import racingcar.object.Car;
+import racingcar.object.TryCount;
+import racingcar.service.CarService;
+import racingcar.service.TryCountService;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarService carService = new CarService();
+        TryCountService tryCountService = new TryCountService();
 
+        carService.printGuide_InputCarsName();
+        List<Car> carList = carService.inputCarsName();
+        tryCountService.printGuide_InputTryCount();
+        TryCount tryCount = tryCountService.inputTryCount();
+
+        for(int i = 0; i < tryCount.getCount(); i++) {
+            carService.moveCars(carList);
+            carService.printCarStatus(carList);
+        }
+        carService.printWinners(carList);
     }
 }
