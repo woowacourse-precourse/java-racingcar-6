42dc5d3dccb44c6be1f712da66e9477c1957733a
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-01 14:54:05 +0900

    test: add finish function test to race test

---
+++ src/test/java/racingcar/model/RaceTest.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RaceTest {
+    @Test
+    public void isNotFinish() {
+        //given
+        List<String> carNameList = new ArrayList<>(List.of("a", "b", "c"));
+        Race race = new Race(5,carNameList);
+
+        //when
+        race.runRace();
+
+        //then
+        Assertions.assertThat(race.isFinish()).isFalse();
+
+    }
+
+    @Test
+    public void isFinish() {
+        //given
+        List<String> carNameList = new ArrayList<>(List.of("a", "b", "c"));
+        Race race = new Race(1,carNameList);
+
+        //when
+        race.runRace();
+
+        //then
+        Assertions.assertThat(race.isFinish()).isTrue();
+
+    }
+
+
+}
