68b53cd7b4af2c5eed3f77e030cbecbeddf6b2ba
jimin <grapes612@naver.com>
2023-11-01 14:54:56 +0900

    Created Car class and implemented constructor

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
+class Car {
+    String name;
+    int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
+
 class Utils {
     public static List<String> parseInput(String input) {
         String[] splitInput = input.split(",");
@@ -24,5 +34,10 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCar = Console.readLine();
         List<String> carNames = Utils.parseInput(inputCar);
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
     }
 }
