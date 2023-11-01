05fe2b9e2f3d448c5d4aa487c567265058e91325
heondong9265 <heondong0723@gmail.com>
2023-11-01 18:00:41 +0900

    feat: Game, GameTest 구현

--- src/main/java/Domain/Game.java
+++ src/main/java/Domain/Game.java
@@ -1,2 +1,54 @@
-package Domain;public class Game {
+package Domain;
+
+public class Game {
+    private Car[] cars;
+    private int tryCount;
+
+    public Game(Car[] cars, int tryCount) {
+        this.cars = cars;
+        this.tryCount = tryCount;
+    }
+
+    public Car[] getCars() {
+        return cars;
+    }
+
+    public void runGame() {
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+        }
+    }
+
+    public String getWinners() {
+        int maxPosition = getMaxPosition();
+        return buildWinnersString(maxPosition);
+    }
+
+    int getMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
+
+    private String buildWinnersString(int maxPosition) {
+        StringBuilder winners = new StringBuilder();
+        boolean isFirstWinner = true;
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                if (isFirstWinner) {
+                    winners.append(car.getCarName());
+                    isFirstWinner = false;
+                } else {
+                    winners.append(",").append(car.getCarName());
+                }
+            }
+        }
+
+        return winners.toString();
+    }
 }
--- src/test/java/Domain/GameTest.java
+++ src/test/java/Domain/GameTest.java
@@ -1,2 +1,48 @@
-package Domain;public class GameTest {
+package Domain;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest {
+    @Test
+    public void testGame() {
+        // 자동차 객체 생성
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+
+        // 게임 객체 생성
+        Car[] cars = {car1, car2, car3};
+        int tryCount = 5;
+        Game game = new Game(cars, tryCount);
+
+        // 초기 위치 확인
+        for (Car car : cars) {
+            assertThat(car.getPosition()).isEqualTo(0);
+        }
+
+        // 게임 실행
+        game.runGame();
+
+        for (Car car : cars) {
+            String carName = car.getCarName();
+            int position = car.getPosition();
+            StringBuilder positionString = new StringBuilder();
+            for (int i = 0; i < position; i++) {
+                positionString.append("-");
+            }
+            System.out.println(carName + " : " + positionString);
+        }
+
+        // 우승자 확인
+        String winners = game.getWinners();
+        System.out.println("Winners: " + winners);
+
+        // 우승자 검증
+        for (Car car : cars) {
+            if (car.getPosition() == game.getMaxPosition()) {
+                assertThat(winners).contains(car.getCarName());
+            }
+        }
+    }
 }
