214f98eb7f0dd8cbbeb736bf7babe1df3df4fcd9
Aeraekun <lsch3374@naver.com>
2023-11-01 20:04:30 +0900

    refactor: GameController 삭제 ################
    ################
    
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import racingcar.controller.GameController;
-
 public class Application {
     public static void main(String[] args) {
         GameController gameController = new GameController();
