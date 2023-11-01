6b5635372d4846073116073b14d93a14ddcc32a0
woookk <woook27@naver.com>
2023-11-01 23:37:15 +0900

    test: Car 클래스 기본 테스트 코드 추가

--- src/test/java/racingcar/entity/CarTest.java
+++ src/test/java/racingcar/entity/CarTest.java
@@ -1,22 +1,38 @@
 package racingcar.entity;
 
 import org.junit.jupiter.api.Test;
+import racingcar.constants.Constants;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
     @Test
-    public void 차량_이름_테스트() {
-        // given
-        String expectedName = "테스트카";
-        Car car = new Car(expectedName);
+    void 자동차_객체_생성_테스트() {
+        Car car = new Car("test");
+        assertEquals("test", car.getName());
+    }
 
-        // when
-        String actualName = car.getName();
+    @Test
+    void 이름_길이_유효성_테스트() {
+        String longName = "abcdef";
+        assertThrows(IllegalArgumentException.class, () -> new Car(longName));
+    }
 
-        // then
-        assertEquals(expectedName, actualName);
+    @Test
+    void 자동차_전진_테스트() {
+        Car car = new Car("test");
+        int initialPosition = car.getPosition();
+        car.move(Constants.MIN_MOVE_NUMBER.<Integer>getValue());
+        assertEquals(initialPosition + 1, car.getPosition());
+    }
+
+    @Test
+    void 자동차_정지_테스트() {
+        Car car = new Car("test");
+        int initialPosition = car.getPosition();
+        car.move(Constants.MIN_MOVE_NUMBER.<Integer>getValue() - 1);
+        assertEquals(initialPosition, car.getPosition());
     }
 
 }
