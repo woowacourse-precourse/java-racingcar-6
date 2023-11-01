9e1b7751b27c2e99cee56508830829a61186a76c
honeydanji <tjdwls3680@naver.com>
2023-11-01 13:41:13 +0900

    FEAT : 게임 안내 메시지 추가

--- src/main/java/racingcar/utils/GameMessage.java
+++ src/main/java/racingcar/utils/GameMessage.java
@@ -4,6 +4,7 @@ public class GameMessage {
 
     public static String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static String TRY = "시도할 횟수는 몇회인가요?";
+    public static String RUN = "실행결과";
     public static String RESULT = "실행 결과";
     public static String WINNER = "최종 우승자 : ";
 
