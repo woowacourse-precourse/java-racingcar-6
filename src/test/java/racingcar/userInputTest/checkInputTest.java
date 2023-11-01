185bb29c503a23de165b2c367317cd5adb528156
YBM <99byoungmin@gmail.com>
2023-11-01 12:56:50 +0900

    test: More Than Five Letters

--- src/test/java/racingcar/userInputTest/checkInputTest.java
+++ src/test/java/racingcar/userInputTest/checkInputTest.java
@@ -9,11 +9,20 @@ public class checkInputTest {
 
     @DisplayName("쉼표가 없는 경우")
     @Test
-    void withoutCommas(){
+    void withoutCommas() {
         final checkException checkException = new checkException();
         final String input = "ABC";
 
         checkException.checkCommaCarName(input);
+    }
+
+    @DisplayName("자동차 이름이 다섯 글자를 넘는 경우")
+    @Test
+    void overFiveWords() {
+        final checkException checkException = new checkException();
+        final String[] carNameArray = {"pobi", "woni", "junnnnnnnn"};
+
+        checkException.checkCarNameException(carNameArray);
     }
 
 }
