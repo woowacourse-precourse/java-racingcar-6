7d9fcd63d36e9ba940e176ef9e03062632f6480c
Hyun Woo <hwsa10041@gmail.com>
2023-11-01 12:17:44 +0900

    chore : Controller 주석 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -9,14 +9,17 @@ public class RacingController {
 
     private final RacingService racingService = new RacingService();
 
+    // 경주에 참여할 자동차 리스트 생성 메서드
     public List<Car> createCarList(String[] carNames) {
         return racingService.createCarList(carNames);
     }
 
+    // 자동차 경주 라운드 진행 메서드
     public List<Car> playSingleRound(List<Car> cars) {
         return racingService.playSingleRound(cars);
     }
 
+    // 우승자 확인 메서드
     public List<String> readVictoryPlayers(List<Car> cars) {
         return racingService.findByVictoryPlayers(cars);
     }
