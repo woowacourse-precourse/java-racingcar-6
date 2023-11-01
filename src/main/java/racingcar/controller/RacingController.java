604dde0bf414951b03291d2594459aa7e73b7404
woogie01 <kwonhu3973@gmail.com>
2023-11-01 17:51:06 +0900

    feat: random number 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -7,8 +7,8 @@ import racingcar.util.CheckInput;
 import java.util.*;
 
 public class RacingController {
-    CheckInput checkInput = new CheckInput();
-    Racing racing = new Racing();
+    private final CheckInput checkInput = new CheckInput();
+    private final Racing racing = new Racing();
 
     public void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.util.RandomNumber;
 import racingcar.view.PrintCurrent;
 import racingcar.view.PrintWinner;
 
@@ -9,6 +9,7 @@ import java.util.*;
 public class Racing {
     PrintCurrent printCurrent = new PrintCurrent();
     PrintWinner printWinner = new PrintWinner();
+    RandomNumber randomNumber = new RandomNumber();
 
     public void play(List<String> players, int trials) {
         ArrayList<String>[] playerCurrent = new ArrayList[players.size()];
@@ -20,7 +21,7 @@ public class Racing {
         System.out.println("실행 결과");
         for (int trial = 0; trial < trials; trial++) {
             for (int idx = 0; idx < players.size(); idx++) {
-                int random = Randoms.pickNumberInRange(0,9);
+                int random = randomNumber.get();
 
                 if (random >= 4) {
                     playerCurrent[idx].add("-");
