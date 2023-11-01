2fe3da05c398cf59d23ad873dec03ebbc6dc7d1a
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-01 23:07:40 +0900

    [feat] : add join winner car name method

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -9,4 +9,8 @@ public class StringUtils {
     public static List<String> splitCarNames(String carNames) {
         return Arrays.asList(carNames.split(Constants.COMMA)); // 한줄에 점하나만 찍는다 검증.
     }
+
+    public static String joinWinnerCarName(List<String> winnerCarNames) {
+        return String.join(Constants.WINNER_DELIMITER,winnerCarNames);
+    }
 }
