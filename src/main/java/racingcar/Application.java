5f945d9cc61c1461fb9e38ca3790c294d35f38d5
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-01 17:07:55 +0900

    refactor: static 메서드로 game 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,10 @@
 package racingcar;
 
-
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	Game game = new Game();
-    	Game.playGame();
-    }
+	
+	public static void main(String[] args) {
+		
+		Game.play();
+
+	}
 }
