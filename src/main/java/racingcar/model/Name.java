f90a2c29d2e74bfd9ad4ce29c84ec57b6c38729c
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:05:54 +0900

    feat(Name): 속성 및 생성자 정의
    Name 클래스는 자동차의 이름을 나타내기 위해 String 값을 감싸는 래퍼 클래스이다.
    
    속성
    - String name
    
    생성자
     - (String): 주어진 값으로 내부 값을 설정합니다.
    만약, 주어진 문자열이 앞 또는 뒤에 1개 이상의 공백 문자를 포함하거나, 빈 문자열 이거나,
    5글자를 초과하는 경우, `IllegalArgumentException` 을 발생시킵니다.

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,5 +1,21 @@
 package racingcar.model;
 
 public class Name {
+    private String name;
 
+    /**
+     * 주어진 이름으로 자동차를 생성한다. 초기 주행 거리는 0이다.
+     * @param unsure 양 끝에 공백을 포함하지 않는 1자 이상 5자 이하인 문자열
+     * @throws IllegalArgumentException 유효하지 않은 name 형식
+     */
+    public Name(String unsure) throws IllegalArgumentException {
+
+        boolean isNameStartOrEndWithSpace = (unsure.trim().length() != unsure.length());
+        boolean isNameLengthOutOfRange = (unsure.length() < 1 || 5 < unsure.length());
+        if (isNameStartOrEndWithSpace || isNameLengthOutOfRange) {
+            throw new IllegalArgumentException("자동차의 이름은 양 끝에 공백을 포함하거나, 비어있거나, 5자를 초과할 수 없습니다.");
+        }
+
+        this.name = unsure;
+    }
 }
