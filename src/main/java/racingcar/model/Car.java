a8ef70f6055663e18550eeeceb7162d186101138
hyungsoolim <hyungsoolim0@gmail.com>
2023-11-01 12:32:52 +0900

    feat: new function for each car move result with random number
    Each car will move based on random number generated in Car class.
    The `runCount` will decide how many run the cars will make and each car will move.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private Name name;
-    private int position;
+    private int position = 0;
 
     public Car(String carName) {
         this.name = new Name(carName);
@@ -12,6 +12,9 @@ public class Car {
 
     public void move() {
         int randomResult = Randoms.pickNumberInRange(0, 9);
+        if (4 <= randomResult) {
+            this.position++;
+        }
     }
 
 }
