90564b89b28dc9c8e326207327da42123347dae0
gumgu <bienlee@naver.com>
2023-11-01 15:44:59 +0900

    Test : 한 라운드(Match - 모든 자동차 전진여부 결정) 기능 테스트

--- src/test/java/racingcar/business/CarManagerTest.java
+++ src/test/java/racingcar/business/CarManagerTest.java
@@ -51,16 +51,78 @@ class CarManagerTest {
         assertThat(nameList.size()).isEqualTo(size);
     }
 
+    @Test
+    @DisplayName("입력된 숫자와 동일하게 자동차들 전진. - 숫자가 모두 기준점보다 작은 경우")
+    void runMatch_underStandard() {
+        // given
+        setNames(Arrays.asList("pobi", "wooni", "kong")); // 자동차 이름 설정
+
+        List<Integer> expectedNumbers = Arrays.asList(1, 2, 3); // 각 자동차에 들어갈 숫자들
+        List<Integer> expectedDistance = Arrays.asList(0, 0, 0); // 예상 결과
+
+        // when
+        List<Car> matchResult = runMatch(expectedNumbers);
+
+        // then
+        List<Integer> distanceList = convertToDistance(matchResult);
+        assertThat(distanceList).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    @DisplayName("입력된 숫자와 동일하게 자동차들 전진. - 숫자가 모두 기준점보다 클 경우")
+    void runMatch_upStandard() {
+        // given
+        setNames(Arrays.asList("pobi", "wooni", "kong")); // 자동차 이름 설정
+
+        List<Integer> expectedNumbers = Arrays.asList(5, 6, 7); // 각 자동차에 들어갈 숫자들
+        List<Integer> expectedDistance = Arrays.asList(1, 1, 1); // 예상 결과
+
+        // when
+        List<Car> matchResult = runMatch(expectedNumbers);
+
+        // then
+        List<Integer> distanceList = convertToDistance(matchResult);
+        assertThat(distanceList).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    @DisplayName("입력된 숫자와 동일하게 자동차들 전진. - match 2회")
+    void runMatch_2cycle() {
+        // given
+        setNames(Arrays.asList("pobi", "wooni", "kong")); // 자동차 이름 설정
+
+
+        List<Integer> expectedNumbers = Arrays.asList(5, 6, 7, 5, 6, 7); // 각 자동차에 순서대로 주어질 숫자들
+        List<Integer> expectedDistance = Arrays.asList(2, 2, 2); // 예상 결과
+
+        // when
+        runMatch(expectedNumbers); // 1회
+        List<Car> matchResult = runMatch(expectedNumbers); //2회
+
+        // then
+        List<Integer> distanceList = convertToDistance(matchResult);
+        assertThat(distanceList).isEqualTo(expectedDistance);
+    }
+
+    private void setNames(List<String> nameList) {
+        carManager.generateCarList(nameList);
+    }
+
+    private List<Car> runMatch(List<Integer> expectedNumbers) {
+        expectedNumberMaker.setExpectedNumbers(expectedNumbers);
+        return carManager.runMatch();
+    }
+
     private List<String> convertToName(List<Car> carList) {
         return carList.stream()
                 .map(Car::getName)
                 .collect(Collectors.toList());
     }
 
-    private List<Car> runMatch(List<Integer> expectedNumbers) {
-        expectedNumberMaker.setExpectedNumbers(expectedNumbers);
-
-        return carManager.runMatch();
+    private List<Integer> convertToDistance(List<Car> carList) {
+        return carList.stream()
+                .map(Car::getDistance)
+                .collect(Collectors.toList());
     }
 
 }
