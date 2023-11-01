b43fe588462656c386ae451edae930b96db116a2
hjhan <hjhan.0621@gmail.com>
2023-11-01 21:18:32 +0900

    test(checkCarNameException()): 분리된 자동차 이름의 예외처리 테스트코드

--- src/test/java/racingcar/exception/ExceptionManagerTest.java
+++ src/test/java/racingcar/exception/ExceptionManagerTest.java
@@ -1,8 +1,10 @@
 package racingcar.exception;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.util.ErrorMessage.ERROR_NOT_EMPTY;
 import static racingcar.util.ErrorMessage.ERROR_WRONG_CARS_NAME;
+import static racingcar.util.ErrorMessage.ERROR_WRONG_CAR_NAME;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,15 +14,7 @@ class ExceptionManagerTest {
     ExceptionManager em = new ExceptionManager();
 
     @Test
-    @DisplayName("입력받은_자동차_이름_콤마로_끝날때")
-    void test_checkCarsNameException1() {
-        assertThatThrownBy(() -> em.checkCarsNameException("blue,red,"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_WRONG_CARS_NAME);
-    }
-
-    @Test
-    @DisplayName("입력받은_자동차_이름_입력값이_없을때")
+    @DisplayName("입력받은_자동차_이름_입력값이_없을때_예외")
     void test_checkCarsNameException2() {
         assertThatThrownBy(() -> em.checkCarsNameException(""))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -28,7 +22,29 @@ class ExceptionManagerTest {
     }
 
     @Test
-    void checkCarNameException() {
+    @DisplayName("입력받은_자동차_이름_콤마로_끝날때_예외")
+    void test_checkCarsNameException1() {
+        assertThatThrownBy(() -> em.checkCarsNameException("blue,red,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_WRONG_CARS_NAME);
+    }
+
+    @Test
+    @DisplayName("각각의_자동차_이름_입력값이_없을때_예외")
+    void test_checkCarNameException1() {
+        String[] arrayOfCarName = {"apple", ""};
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> em.checkCarNameException(arrayOfCarName))
+                .withMessage(ERROR_NOT_EMPTY);
+    }
+
+    @Test
+    @DisplayName("각각의_자동차_이름_글자수_5이상일때_예외")
+    void test_checkCarNameException2() {
+        String[] arrayOfCarName = {"apple", "banana"};
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> em.checkCarNameException(arrayOfCarName))
+                .withMessage(ERROR_WRONG_CAR_NAME);
     }
 
     @Test
