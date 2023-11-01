df45fac588a7702aa749f1776061aa1dea04662b
ekeprl <ykeon26@naver.com>
2023-11-01 21:04:33 +0900

    게임의 진행횟수를 결정하는 ROundNumber를 결정한다. 숫자만 입력하고 , 예외사항 발생 시 IllegalArgumentException 을 발동시키도록 구성했다.

---
+++ src/main/java/racingcar/Util/RoundNumberValidator.java
@@ -0,0 +1,24 @@
+package racingcar.Util;
+
+import racingcar.Util.Rule;
+
+
+public class RoundNumberValidator {
+    public final String ROUND_NUM_STRING;
+
+    public RoundNumberValidator(String roundNumString) {
+        this.ROUND_NUM_STRING = roundNumString;
+        validate();
+    }
+
+    public void validate() {
+        isInt();
+    }
+
+    private void isInt() {
+
+        if (!Rule.roundNumPattern.matcher(ROUND_NUM_STRING).matches()) {
+            throw new IllegalArgumentException(Rule.ROUND_NUM_WRONG_ERROR);
+        }
+    }
+}
