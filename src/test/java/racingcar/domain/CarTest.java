e2c6e5a111fa21bd5482e67940f40a6b431a2533
nature1216 <annekim1216@naver.com>
2023-11-01 22:42:21 +0900

    test: Car 도메인 테스트 추가 forward 메소드 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
     private static Car car = new Car("pobi", 1);
+    private final int FORWARD_LIMIT = 4;
     @Test
     void isMoreThanDigit_num이_digit_이상() {
         assertThat(car.isMoreThanDigit(3, 4))
@@ -24,5 +25,17 @@ public class CarTest {
     void isMoreThanDigit_num이_digit_이하() {
         assertThat(car.isMoreThanDigit(4, 3))
                 .isFalse();
+    }
+
+    @Test
+    void forward_랜덤값이_4() {
+        car.forward(4,FORWARD_LIMIT);
+        assertThat(car.getCurrent()).isEqualTo(2);
+    }
+
+    @Test
+    void forward_랜덤값이_3() {
+        car.forward(3, FORWARD_LIMIT);
+        assertThat(car.getCurrent()).isEqualTo(1);
     }
 }
