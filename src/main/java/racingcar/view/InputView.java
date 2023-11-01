f65110a421278fe24cfcba53084c394d8639ae6f
hoduulmu <timewarp96@naver.com>
2023-11-01 19:51:50 +0900

    feat: 시도 횟수가 양수가 아닐 시 Validation 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,10 +7,14 @@ import java.util.function.Supplier;
 
 public class InputView {
 
+    private static final int ZERO = 0;
     private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_ROUND_NUMBER = "시도할 회수는 몇회인가요?";
     private static final String COMMA = ",";
     private static final String ERROR_INPUT_MUST_NUMBER = "입력값은 숫자여야 합니다.";
+    private static final String ERROR_INPUT_MUST_POSITIVE = "입력값은 양수여야 합니다.";
+
+
     private final Supplier<String> inputReader;
 
     public InputView() {
@@ -31,7 +35,9 @@ public class InputView {
     public int askRoundNumber() {
         println(ASK_ROUND_NUMBER);
         String roundNumberInput = inputReader.get();
-        return toInteger(roundNumberInput);
+        int roundNumber = toInteger(roundNumberInput);
+        validateIsPositive(roundNumber);
+        return roundNumber;
     }
 
     private int toInteger(String string) {
@@ -39,6 +45,12 @@ public class InputView {
             return Integer.parseInt(string);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_INPUT_MUST_NUMBER);
+        }
+    }
+
+    private void validateIsPositive(int roundNumber) {
+        if (roundNumber <= ZERO) {
+            throw new IllegalArgumentException(ERROR_INPUT_MUST_POSITIVE);
         }
     }
 
