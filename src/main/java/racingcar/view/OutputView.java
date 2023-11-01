3fc3c18caf37b3d3f94b194670883e28515ea799
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-01 16:11:27 +0900

    feat(OutputView): 실행 결과 메시지 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String EXECUTION_RESULT = "\n실행 결과";
+
+    public void printExecutionResult() {
+        System.out.println(EXECUTION_RESULT);
+    }
+
+}
