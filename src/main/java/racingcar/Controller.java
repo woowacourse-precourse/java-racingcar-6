0ca17847a697550865dbd4632ac0eaff13ceaf48
jagarchi <relong1026@naver.com>
2023-11-01 20:23:20 +0900

    숫자가 아닐때 오류

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -4,14 +4,23 @@ public class Controller {
     public int tryNumber(){
         String tryNumber = Console.readLine();
         numbersizeError(tryNumber);
+        numberFormatError(tryNumber);
         return Integer.parseInt(tryNumber);
     }
 
-    public void numbersizeError(String tryNumber) {
+    public void numbersizeError(String tryNumber) {  //0이하의 숫자일 떄 오류
         int inputNumber = Integer.parseInt(tryNumber);
 
         if (inputNumber <= 0) {
             throw new IllegalArgumentException(ExceptionError.numbersizeError);
+        }
+    }
+
+    public void numberFormatError(String tryNumber) {  //숫자가 아닐떄 오류
+        try {
+            int inputNumber = Integer.parseInt(tryNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionError.numberFormatError);
         }
     }
 }
