c9b9eb5507fbe6552da697b77e161836853104b2
yoonion <yoonion97@gmail.com>
2023-11-01 18:13:38 +0900

    test: 입력한 자동차 map에 저장하는 메소드 테스트코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,7 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.objenesis.instantiator.basic.NewInstanceInstantiator;
 import racingcar.domain.NumberGenerator;
+import racingcar.domain.Race;
+
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -50,6 +54,19 @@ class ApplicationTest extends NsTest {
 
         // 범위 내에 있는 숫자이면 통과
         Assertions.assertTrue(checkRandomNumFlag);
+    }
+
+    @Test
+    void 입력한_자동차_이름_저장() {
+        String inputCarNameString = "car1,car2,car3";
+
+        Race race = new Race();
+        Map<String, String> carMap = race.saveCarMap(inputCarNameString);
+
+        Assertions.assertEquals(3, carMap.size());
+        Assertions.assertEquals("", carMap.get("car1"));
+        Assertions.assertEquals("", carMap.get("car2"));
+        Assertions.assertEquals("", carMap.get("car3"));
     }
 
     @Override
