ccd410f6db4f464a43396e9a8df0c61f3e61c453
Gyunseo Lee <rbstj0311@naver.com>
2023-11-01 20:31:02 +0900

    feat(GameManager): GameManager가 유저 입력을 받는 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -4,18 +4,26 @@
 
 - [] 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
 - [] 각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
-- [] 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
+- [x] 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
 - [] 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
 - [] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
 - [] 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
 - [] 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
-- [] 사용자가 잘못된 값을 입력할 경우 `IllegalArgumentException`을 발생시킨 후 애플리케이션은 종료되어야 한다.
+- [x] 사용자가 잘못된 값을 입력할 경우 `IllegalArgumentException`을 발생시킨 후 애플리케이션은 종료되어야 한다.
 
 ### 입출력 요구 사항
 
 #### 입력
 
-- [] 경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)
+- [x] 경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분) - GameManager#readUserInput()
+    - [x] 이름은 5자 이하만 가능하다. - GameManager#validateRacingCarName()
+    - [x] 이름은 쉼표(,)를 기준으로 구분한다. - GameManager#validateUserInput()
+        - [x] 구분된 이름이 `null`인 경우 `IllegalArgumentException`을 발생시킨다. - GameManager#validateRacingcarName()
+        - [x] 구분된 이름이 빈 문자열인 경우 `IllegalArgumentException`을 발생시킨다. - GameManager#validateRacingcarName()
+    - [x] 이름이 중복되는 경우 `IllegalArgumentException`을 발생시킨다. - GameManager#validateUserInput()
+    - [x] 사용자가 잘못된 값을 입력할 경우 `IllegalArgumentException`을 발생시킨다.
+        - [x] 잘못된 값이 `null`인 경우 `IllegalArgumentException`을 발생시킨다. - GameManager#validateUserInput()
+        - [x] 잘못된 값이 빈 문자열인 경우 `IllegalArgumentException`을 발생시킨다. - GameManager#validateUserInput()
 
 ```
 pobi,woni,jun
---
+++ src/main/java/racingcar/domain/GameManager.java
@@ -0,0 +1,55 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class GameManager {
+    private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GAME_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public GameManager() {
+
+    }
+
+    private static void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+    private static String readUserInput() {
+        return Console.readLine();
+    }
+
+    public static boolean validateRacingcarName(String racingcarName) {
+        if (racingcarName == null || racingcarName.isEmpty()) {
+            throw new IllegalArgumentException("racingcar 이름이 null이거나 비어있습니다.");
+        }
+        if (racingcarName.length() > 5) {
+            throw new IllegalArgumentException("racingcar 이름이 5자를 초과합니다.");
+        }
+
+        return true;
+    }
+
+    public static boolean validateUserInput(String userInput) {
+        if (userInput == null || userInput.isEmpty()) {
+            throw new IllegalArgumentException("유저 입력값이 null이거나 비어있습니다.");
+        }
+        List<String> racingcarNames = List.of(userInput.split(","));
+        // if there is the same racingcar name, throw exception
+        if (racingcarNames.stream().distinct().count() != racingcarNames.size()) {
+            throw new IllegalArgumentException("중복된 racingcar 이름이 있습니다.");
+        }
+
+        for (String racingcarName : racingcarNames) {
+            validateRacingcarName(racingcarName);
+        }
+        return true;
+    }
+
+    public static void runGame() {
+
+        printGameStartMessage();
+        String userInput = readUserInput();
+        validateUserInput(userInput);
+    }
+}
