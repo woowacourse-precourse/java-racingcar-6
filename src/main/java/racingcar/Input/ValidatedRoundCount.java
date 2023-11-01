af6f50119a13c347c6baad8cb77b2967a8759586
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-11-01 10:39:25 +0900

    feat: 시행 횟수를 입력 받을 수 있다.

---
+++ src/main/java/racingcar/Input/ValidatedRoundCount.java
@@ -0,0 +1,25 @@
+package racingcar.Input;
+
+public class ValidatedRoundCount {
+
+  private final int racingCount;
+
+  private ValidatedRoundCount(int racingCount) {
+    this.racingCount = racingCount;
+  }
+
+  public static ValidatedRoundCount of(String input) {
+    if (!input.matches("\\d+")) {
+      throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+    }
+    int racingCount = Integer.parseInt(input);
+    if (racingCount < 1 || racingCount > 100) {
+      throw new IllegalArgumentException("경주 횟수는 1 이상 이어야 합니다.");
+    }
+    return new ValidatedRoundCount(racingCount);
+  }
+
+  public int getRacingCount() {
+    return racingCount;
+  }
+}
