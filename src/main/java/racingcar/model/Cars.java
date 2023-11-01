dbcf0163b77aa59caf8b9ff6f29d2443c82e4ef7
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-01 18:41:35 +0900

    feat: for-each 구문을 사용하기 위해 iterable interface 구현 가장 전진을 많이 한 자동차들을 구하는 메소드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,13 +1,16 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public class Cars {
+public class Cars implements Iterable<Car> {
     private final List<Car> carList;
 
-    public int size() {
-        return carList.size();
+    @Override
+    public Iterator<Car> iterator() {
+        return new CarsIterator(carList);
     }
 
     public Cars(String input) {
@@ -19,7 +22,50 @@ public class Cars {
         this.carList = list;
     }
 
+    public int size() {
+        return carList.size();
+    }
+
+    private Car getBiggestDistCar() {
+        return Collections.max(carList);
+    }
+
+    public List<Car> getBiggestDistCars() {
+        List<Car> biggestDistCars = new ArrayList<>();
+        Car biggestCar = getBiggestDistCar();
+        for (Car car : carList) {
+            if (biggestCar.compareTo(car) == 0) {
+                biggestDistCars.add(car);
+            }
+        }
+        return biggestDistCars;
+    }
+
+    private Car getCarAt(int index) {
+        return carList.get(index);
+    }
+
     public void moveForwardValueOf(int index) {
-        carList.get(index).moveForward();
+        getCarAt(index).moveForward();
+    }
+}
+
+class CarsIterator implements Iterator<Car> {
+    private final List<Car> carList;
+    private int index;
+
+    public CarsIterator(List<Car> carList) {
+        this.carList = carList;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < carList.size();
+    }
+
+    @Override
+    public Car next() {
+        return carList.get(index++);
     }
 }
