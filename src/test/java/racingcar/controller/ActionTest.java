ab7363a8ad70c04de29964d243f1ce548b465481
dragon0622 <iamdragon.dev@gmail.com>
2023-11-01 19:05:26 +0900

    docs(racingCar): 불필요한 import 제거

--- src/test/java/racingcar/controller/ActionTest.java
+++ src/test/java/racingcar/controller/ActionTest.java
@@ -4,14 +4,8 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
-import racingcar.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ActionTest extends NsTest {
 
