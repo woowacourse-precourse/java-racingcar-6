1741c856ebfe829849f3fe9dc070a4840bab8bc7
S-DPR <thankyouformuch@naver.com>
2023-11-01 16:46:21 +0900

    feat: validateNameLengthNotOver 테스트코드 추가

---
+++ src/test/java/racingcar/ValidationTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidationTest {
+
+    @Test
+    void validateNameLengthNotOver_길이가짧을때_오류없음() {
+        Assertions.assertDoesNotThrow(() -> Validation.validateNameLengthNotOver("TEST", 5));
+    }
+    @Test
+    void validateNameLengthNotOver_길이가같을때_오류없음() {
+        Assertions.assertDoesNotThrow(() -> Validation.validateNameLengthNotOver("TEST", 4));
+    }
+
+    @Test
+    void validateNameLengthNotOver_길이가길때_오류발생() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Validation.validateNameLengthNotOver("test", 3));
+    }
+}
