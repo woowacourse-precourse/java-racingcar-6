36bc162d436f81f2770c269d0a035c6408f4c86b
hellomatia <shuaigejp80@gmail.com>
2023-11-01 15:45:42 +0900

    test(domain): CarState검증 테스트 변경.

--- src/test/java/racingcar/domain/CatStateTest.java
+++ src/test/java/racingcar/domain/CatStateTest.java
@@ -7,16 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CatStateTest {
 
     @Test
-    void true_일_경우_MOVING_반환() {
+    void true_일_경우_MOVE_반환() {
         CarState carState = CarState.from(true);
 
-        assertEquals(carState, CarState.MOVING);
+        assertEquals(carState, CarState.MOVE);
     }
 
     @Test
-    void false_일경우_STOPPED_반환() {
+    void false_일경우_STOP_반환() {
         CarState carState = CarState.from(false);
 
-        assertEquals(carState, CarState.STOPPED);
+        assertEquals(carState, CarState.STOP);
     }
 }
