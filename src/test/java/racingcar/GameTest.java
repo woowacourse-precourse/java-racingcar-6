b5dcb103a1bbe676e4d9e8d45b14ecf07ceeac35
aammddkkzxc <ideal0427@naver.com>
2023-11-01 16:01:09 +0900

    test: generate 메서드 테스트 수정

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void playAllRound() {
+    }
+}
--- src/test/java/racingcar/NumberGeneratorTest.java
+++ src/test/java/racingcar/NumberGeneratorTest.java
@@ -2,17 +2,9 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class NumberGeneratorTest {
-    private NumberGenerator numberGenerator;
-
-    @BeforeEach
-    void setUp() {
-        numberGenerator = new NumberGenerator();
-    }
-
     @Test
     void generate_메서드로_범위내의_임의의_수_생성() {
         int result = NumberGenerator.generate();
