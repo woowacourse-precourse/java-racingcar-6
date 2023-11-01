80df2ec3227ed06a23675d330e5e95e903b564b1
jjw0071 <jjw0071@naver.com>
2023-11-01 21:15:43 +0900

    feat(InputValidator) : 입력값이 5자이하인지 체크하는 기능 구현

--- src/main/java/racingcar/domain/InputValidator.java
+++ src/main/java/racingcar/domain/InputValidator.java
@@ -1,6 +1,10 @@
 package racingcar.domain;
 
 public class InputValidator {
-
+    public void validateLength(String carName){
+        if(carName.length()>5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
 
 }
