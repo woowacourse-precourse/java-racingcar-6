54afb5cdf1e7a0de5383b6c804becf3afebc60fd
HongChaa <jun761_1@naver.com>
2023-11-01 18:24:27 +0900

    feat(Validator) : 이름에 공백 여부 검증 메서드 구현 이름에 공백 존재 시 IllegalArgumentException 발생

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -4,10 +4,17 @@ public class Validator {
 
     public void checkName(String name) {
         validateNameLength(name);
+        validateNameBlank(name);
     }
 
     private void validateNameLength(String name) {
         if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameBlank(String name) {
+        if (name.contains(" ")) {
             throw new IllegalArgumentException();
         }
     }
