8d84f0262089a4186b4cb31bf9cc769ce2826c3d
devhyeon0 <devhyeon0@gmail.com>
2023-11-01 21:07:23 +0900

    test: 자동차 이동 성공/실패 테스트 코드 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,30 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarTest {
-  
+
+    Car car;
+
+    @BeforeEach
+    void init() {
+        car = new Car("test");
+    }
+
+    @Test
+    void 자동차_이동_성공_테스트() {
+        car.move(5);
+
+        assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    void 자동차_이동_실패_테스트() {
+        car.move(3);
+
+        assertEquals(0, car.getPosition());
+    }
 }
