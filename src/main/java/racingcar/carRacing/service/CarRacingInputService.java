a10ecca72b29b304a825654eec26e6024d0266ed
fing9 <djajsl54@naver.com>
2023-11-01 14:56:19 +0900

    feat : 레이싱 진행 횟수 입력 기능 추가

--- src/main/java/racingcar/carRacing/service/CarRacingInputService.java
+++ src/main/java/racingcar/carRacing/service/CarRacingInputService.java
@@ -3,10 +3,12 @@ package racingcar.carRacing.service;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.validation.Validation.areLessThenSixLetters;
 import static racingcar.validation.Validation.containDuplicateCarName;
+import static racingcar.validation.Validation.isValidRange;
 
 import java.util.List;
 
 public class CarRacingInputService {
+
     public static List<String> inputCarNameList() throws IllegalArgumentException {
         String carNameString = readLine();
         List<String> carNames = List.of(carNameString.split(","));
@@ -14,6 +16,19 @@ public class CarRacingInputService {
         areLessThenSixLetters(carNames);
         containDuplicateCarName(carNames);
         return carNames;
+    }
+
+    public Integer inputAttemptCount() {
+        String attemptCountString = readLine();
+        Integer attemptCount;
+
+        try {
+            attemptCount = Integer.parseInt(attemptCountString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("attempt count must be numeric.");
+        }
+        isValidRange(attemptCount);
+        return attemptCount;
     }
 
 }
