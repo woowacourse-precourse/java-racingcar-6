e3e9b1755ffcfaa68c2aac9ec8454276a83e491e
ehtjsv2 <ehtjsv2@naver.com>
2023-11-01 22:39:27 +0900

    Test: 필요없는 테스트 삭제

--- src/test/java/racingcar/developtest/CarFactoryTest.java
+++ src/test/java/racingcar/developtest/CarFactoryTest.java
@@ -4,27 +4,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.CarFactory;
 
 public class CarFactoryTest {
-
-    @Test
-    void createCar_쉼표기준_자동차_객체_리스트_생성() {
-        InputStream readLine = setReadLine("pobi,woni");
-        System.setIn(readLine);
-        CarFactory carFactory = new CarFactory();
-        assertDoesNotThrow(carFactory::createCars);
-    }
-
-    @Test
-    void creatCar_쉼표없이_자동차_하나일_때_리스트_생성() {
-        InputStream readLine = setReadLine("pobi");
-        System.setIn(readLine);
-        CarFactory carFactory = new CarFactory();
-        assertDoesNotThrow(carFactory::createCars);
-    }
 
     @Test
     void hasDuplicatedName_중복된_이름_가지고_있을때_예외() {
