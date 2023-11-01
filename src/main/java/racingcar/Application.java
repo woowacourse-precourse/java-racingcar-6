8ff8e23fb539395f620266d2a2097b23aaba98d2
Annjieun <dkslwdms2121@naver.com>
2023-11-01 12:41:53 +0900

    refactor(Application): GameController을 통해 레이싱을 시작하는 것으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,6 @@
 package racingcar;
 
-import racingcar.domain.Attempt;
-import racingcar.domain.Cars;
-import racingcar.domain.Game;
+import racingcar.controller.GameController;
 
 /**
  * 객체 지향 프로그래밍 1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다. 2. 필요한 기능을 (역할에 맞는) 인스턴스가 수행하게 한다. (의인화) 3. 각 결과를 종합한다.
@@ -10,13 +8,7 @@ import racingcar.domain.Game;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Cars cars = new Cars();
-        cars.startCreateCars();
-
-        Attempt attempt = new Attempt();
-        attempt.saveAttemptNumber();
-
-        Game game = new Game(cars.getCars(), attempt.getCount());
-        game.startRacing();
+        GameController gameController = new GameController();
+        gameController.startRacing();
     }
 }
