f778e5057aa2efbf47b99c7a4b7e90ae74031fde
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-01 23:14:48 +0900

    기존에 있던 테스트 코드 수정

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -26,15 +26,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> cars.addCar("pobi,javaji", "1"))
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-
-
-
-    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
