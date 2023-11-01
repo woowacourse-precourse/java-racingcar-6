14cac51acbdbe86fa7cecd39365c830a08dd4e89
unknown <sdcard11291@gmail.com>
2023-11-01 13:22:17 +0900

    1차수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -116,7 +116,6 @@ class RacingGame{
 }
 public class Application {
     public static void main(String[] args) {
-        //System.out.println("되는거야?");
         RacingGame game = new RacingGame();
         game.start();
     }
