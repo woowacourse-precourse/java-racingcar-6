f5bedf1cab4b71363ab5b05604ff0cd77159d732
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-01 14:10:15 +0900

    test: 아무것도 입력하지 않았을 때를 검증하는 테스트

--- src/test/java/racingcar/TryCountValidatorTest.java
+++ src/test/java/racingcar/TryCountValidatorTest.java
@@ -10,8 +10,15 @@ public class TryCountValidatorTest {
 
     @Test
     @DisplayName("시도 횟수가 0일 때를 검증한다.")
-    void validateTryInputCount() {
+    void validateInputTryCount() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> TryCountValidator.validateInputTryCount("0"));
+    }
+
+    @Test
+    @DisplayName("아무것도 입력하지 않았을 때를 검증한다.")
+    void validateInputTryCountNothing() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> TryCountValidator.validateInputTryCountNothing(""));
     }
 }
