116bd95564c10370e6c542eac296ec37173509c3
Nicholas <nkchung@ucsd.edu>
2023-11-01 22:03:10 +0900

    style: removed comments

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,7 @@ package racingcar;
 
 public class Application {
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
     GameLogic racecarGame = new GameLogic();
     racecarGame.startGame();
-
   }
 }
