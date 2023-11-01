507051997f7f36c5ea5067dcf2265df1254ce7f5
yebin <dev.devin.kr@gmail.com>
2023-11-01 16:50:39 +0900

    feat(game-controller): implement user console input handling

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,8 +1,16 @@
 package racingcar.controller;
 
+import racingcar.model.InputUtil;
+import racingcar.view.RaceStartView;
+
 public class GameController {
 
     public void run() {
+        RaceStartView raceStartView = new RaceStartView();
 
+        String carName = raceStartView.getCarName();
+        String[] carNameStrings = InputUtil.splitCarName(carName);
+
+        String raceLap = raceStartView.getRaceLap();
     }
 }
