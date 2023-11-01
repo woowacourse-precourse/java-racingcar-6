20fd44b9ca380a44ed36445ac1bbc4f4031fb872
TaeDongUm <etd93@naver.com>
2023-11-01 23:39:51 +0900

    [feat] Application Controller 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,10 @@
 package racingcar;
 
+import racingcar.controller.CarRacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-    	// test1
->>>>>>> branch 'master' of https://github.com/TaeDongUm/java-racingcar-6.git
->>>>>>> branch 'master' of https://github.com/TaeDongUm/java-racingcar-6.git
+        CarRacingController carRacingController = new CarRacingController();
+        carRacingController.start();
     }
 }
