d0216478c53aa0fb629735e3aadda934dda54e91
dongwonkwon <ehddnjs0710@gmail.com>
2023-11-01 23:12:46 +0900

    Racing 클래스 시도 횟수 출력문

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,7 +1,13 @@
 package racingcar;
 
 public class Racing {
+
+    private int count;
     public void printSetCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printSetCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
