77ecad53d581b04858e3b853e36fbd14a0cd873b
ddingmin <ddingminyt@gmail.com>
2023-11-01 14:56:11 +0900

    구현: 게임을 진행하는 RacingGame 클래스를 구현한다.
    1. 이름들을 입력받아 게임을 생성한다.
    2. 주어진 turn 만큼 게임을 진행한다.
        - 진행중에 dice의 결과에 따라 자동차가 전진한다.
        - 전진 여부를 확인한다.
        - 턴이 끝나면 현재 상환을 출력한다.
    3. 최종 결과를 출력한다.
        - 최대 거리를 계산한다.
        - 우승자를 추린다.

---
+++ src/main/java/racingcar/game/RacingGame.java
@@ -0,0 +1,55 @@
+package racingcar.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import racingcar.domain.Car;
+import racingcar.view.OutputView;
+
+public class RacingGame {
+	public static final int SCORE_LIMIT = 4;
+	private final List<Car> cars;
+
+	public RacingGame(List<String> names) {
+		cars = initialize(names);
+	}
+
+	private List<Car> initialize(List<String> names) {
+		List<Car> cars = new ArrayList<>();
+		names.forEach(name -> cars.add(Car.from(name)));
+		return cars;
+	}
+
+	public void progressGame(int turn) {
+		IntStream.range(0, turn).forEach(i -> progressTurn());
+	}
+
+	private void progressTurn() {
+		cars.stream()
+			.filter(car -> availableMove(Dice.throwDice()))
+			.forEach(Car::move);
+
+		OutputView.printTurnUserResult(cars);
+	}
+
+	private boolean availableMove(int diceNumber) {
+		return diceNumber >= SCORE_LIMIT;
+	}
+
+	public List<Car> getWinners() {
+		int maxDistance = getMaxDistance();
+
+		return cars.stream()
+			.filter(car -> car.getDistance() == maxDistance)
+			.collect(Collectors.toList());
+	}
+
+	private int getMaxDistance() {
+		return cars.stream()
+			.mapToInt(Car::getDistance)
+			.max()
+			.orElse(0);
+	}
+}
