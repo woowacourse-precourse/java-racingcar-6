72488f7d5d3b7378fb68d17b658121695398339b
bnfkim <kgr3819@gmail.com>
2023-11-01 16:46:12 +0900

    Refactor: util - InputView validate Validator class 로 분리

--- src/main/java/racingcar/util/CarNameValidator.java
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -6,19 +6,26 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class CarNameValidator {
-    public static void validCarNameLength(ArrayList<String> cars) {
+
+    public static void validInputCarName(ArrayList<String> carNames) {
+        validCarNameBlank(carNames);
+        validCarNameLength(carNames);
+        validCarsDuplicate(carNames);
+    }
+
+    private static void validCarNameLength(ArrayList<String> cars) {
         for(String car : cars){
             if(car.length() > 5) throw new IllegalArgumentException(ErrorMessage.REQUIRE_LENGTH_UNDER_FIVE.message());
         }
     }
 
-    public static void validCarNameBlank(ArrayList<String> cars) {
+    private static void validCarNameBlank(ArrayList<String> cars) {
         for(String car : cars){
             if (car.contains(" ")) throw new IllegalArgumentException(ErrorMessage.REQUIRE_NOT_BLANK.message());
         }
     }
 
-    public static void validCarsDuplicate(ArrayList<String> cars){
+    private static void validCarsDuplicate(ArrayList<String> cars){
         HashSet<String> set = new HashSet<>(cars);
         if(set.size() != cars.size()) throw new IllegalArgumentException(ErrorMessage.REQUIRE_NOT_DUPLICATE.message());
     }
--- src/main/java/racingcar/util/GameRoundValidator.java
+++ src/main/java/racingcar/util/GameRoundValidator.java
@@ -3,6 +3,11 @@ package racingcar.util;
 import racingcar.view.ErrorMessage;
 
 public class GameRoundValidator {
+    public static void validInputGameRound(String input) {
+        GameRoundValidator.validInputInteger(input);
+        GameRoundValidator.validNotUnderZero(Integer.parseInt(input));
+    }
+
     public static void validInputInteger(String input){
         for(int i=0; i<input.length(); i++) {
             if(!Character.isDigit(input.charAt(i))) throw new IllegalArgumentException(ErrorMessage.REQUIRE_INTEGER_TYPE.message());
