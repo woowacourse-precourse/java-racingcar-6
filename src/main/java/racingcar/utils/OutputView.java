c30cfcda87ff1a8f992ea016143df5745986edd2
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-01 14:58:49 +0900

    Feat: OutputView 레이스 상황 출력 기능 구현

--- src/main/java/racingcar/utils/OutputView.java
+++ src/main/java/racingcar/utils/OutputView.java
@@ -1,7 +1,12 @@
 package racingcar.utils;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
 
+    private static final String FOWARD_MARK = "-";
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
@@ -12,6 +17,22 @@ public class OutputView {
 
     public static void printAttemptCountRequest() {
         System.out.println(ATTEMPT_COUNT_REQUEST_MESSAGE);
+    }
+
+    public static void printRaceStatus(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.println(car.getCarName() + " : " + renderingCurrentPositionMark(car));
+        }
+        System.out.println();
+    }
+
+    private static String renderingCurrentPositionMark(Car car) {
+        StringBuilder currentPositionMark = new StringBuilder();
+        for (int i = 0; i < car.getPosition(); i++) {
+            currentPositionMark.append(FOWARD_MARK);
+        }
+        return currentPositionMark.toString();
     }
 
 }
--- src/test/java/racingcar/utils/OutputViewTest.java
+++ src/test/java/racingcar/utils/OutputViewTest.java
@@ -3,39 +3,72 @@ package racingcar.utils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Game;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OutputViewTest {
-    private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String ATTEMPT_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    @Nested
+    class OuterNested {
 
-    @BeforeEach
-    void setUpStreams() {
-        System.setOut(new PrintStream(outputStream));
-    }
+        private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        private static final String ATTEMPT_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    @AfterEach
-    void restoreStreams() {
-        System.setOut(System.out);
-        outputStream.reset();
-    }
+        private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-    @Test
-    void 차_이름_입력요청_메시지_출력_테스트() {
-        OutputView.printCarNameRequest();
-        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(CAR_NAME_REQUEST_MESSAGE);
-    }
-    
-    @Test
-    void 시도횟수_입력요청_메시지_출력_테스트() {
-        OutputView.printAttemptCountRequest();
-        Assertions.assertThat(outputStream.toString().trim()).contains(ATTEMPT_COUNT_REQUEST_MESSAGE);
+        @BeforeEach
+        void setUpStreams() {
+            System.setOut(new PrintStream(outputStream));
+        }
+
+        @AfterEach
+        void restoreStreams() {
+            System.setOut(System.out);
+            outputStream.reset();
+        }
+
+        @Test
+        void 차_이름_입력요청_메시지_출력_테스트() {
+            OutputView.printCarNameRequest();
+            Assertions.assertThat(outputStream.toString().trim())
+                    .isEqualTo(CAR_NAME_REQUEST_MESSAGE);
+        }
+
+        @Test
+        void 시도횟수_입력요청_메시지_출력_테스트() {
+            OutputView.printAttemptCountRequest();
+            Assertions.assertThat(outputStream.toString().trim())
+                    .contains(ATTEMPT_COUNT_REQUEST_MESSAGE);
+        }
+
+        @Nested
+        class InnerNested {
+            Game game;
+
+            @BeforeEach
+            void setUp() {
+                game = new Game(Arrays.asList(
+                        new Car("Tom", () -> 4),
+                        new Car("Jin", () -> 4),
+                        new Car("Ive", () -> 3)),
+                        1);
+            }
+
+            @Test
+            void 레이스_상황_출력_테스트() {
+                game.carRaceOnce();
+                OutputView.printRaceStatus(game.getCars());
+                Assertions.assertThat(outputStream.toString())
+                        .contains("실행 결과", "Tom : -", "Jin : -", "Ive : ");
+            }
+        }
     }
 }
