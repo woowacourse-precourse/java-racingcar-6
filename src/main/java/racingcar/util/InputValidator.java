7af67ef077ed94de32fd9f97c185edff787e35df
SeungGun <saverrw2864@naver.com>
2023-11-01 18:23:56 +0900

    feat: 입력 검증 유틸 클래스 구현

---
+++ src/main/java/racingcar/util/InputValidator.java
@@ -0,0 +1,54 @@
+package racingcar.util;
+
+import java.util.Arrays;
+
+import static racingcar.util.ErrorMessage.*;
+import static racingcar.util.ExceptionHandler.triggerException;
+
+public class InputValidator {
+
+    private static final int CAR_NAME_LENGTH_LIMIT = 5;
+
+    public static void validateCarsInput(String input) {
+        if (input.isEmpty()) {
+            triggerException(CAR_INPUT_EMPTY_STRING);
+        }
+
+        String[] split = Converter.convertSplitArray(input);
+
+        if (!isValidCarsLengthOfName(split)) {
+            triggerException(CAR_INPUT_EXCEED_NAME_LIMI_LENGTH);
+        }
+
+        if (hasDuplicateCarName(split)) {
+            triggerException(CAR_INPUT_DUPLICATE_CAR_NAME);
+        }
+
+        if (hasOnlySpaceCarName(split)) {
+            triggerException(CAR_INPUT_ONLY_SPACE_CAR_NAME);
+        }
+    }
+
+    private static boolean isValidCarsLengthOfName(String[] split) {
+        return Arrays.stream(split)
+                .filter(s -> s.length() > CAR_NAME_LENGTH_LIMIT)
+                .findAny()
+                .isEmpty();
+    }
+
+    public static boolean isPositive(int value) {
+        return value >= 1;
+    }
+
+    private static boolean hasDuplicateCarName(String[] carNames) {
+        return Arrays.stream(carNames)
+                .distinct()
+                .count() != carNames.length;
+    }
+
+    private static boolean hasOnlySpaceCarName(String[] carNames) {
+        return Arrays.stream(carNames)
+                .anyMatch(name -> name.replace(" ", "")
+                        .isEmpty());
+    }
+}
---
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -0,0 +1,138 @@
+package racingcar.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.util.ErrorMessage.*;
+
+class InputValidatorTest {
+
+    @Test
+    void 자동차_이름_빈_문자열_예외테스트() {
+        // given
+        String testInput = "";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarsInput(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_INPUT_EMPTY_STRING.getMessage());
+    }
+
+    @Test
+    void 자동차_이름_공백_예외테스트() {
+        // given
+        String testInput = "   ,    ,     ";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarsInput(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_INPUT_ONLY_SPACE_CAR_NAME.getMessage());
+
+    }
+
+    @Test
+    void 자동차_이름_제한글자_예외테스트() {
+        // given
+        String testInput = "abcdefg,xyz,aaabbb";
+        String testInput2 = "aabbcc";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarsInput(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_INPUT_EXCEED_NAME_LIMI_LENGTH.getMessage());
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarsInput(testInput2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_INPUT_EXCEED_NAME_LIMI_LENGTH.getMessage());
+    }
+
+    @Test
+    void 자동차_이름_중복_이름_예외테스트() {
+        // given
+        String testInput = "aabb,aabb,abc";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarsInput(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_INPUT_DUPLICATE_CAR_NAME.getMessage());
+    }
+
+    @Test
+    void 시도횟수_숫자가_아닌_예외테스트() {
+        // given
+        String testInput = "";
+        String testInput2 = "abcd";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> Converter.convertToValidInteger(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(TRY_COUNT_NOT_A_NUMBER.getMessage());
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> Converter.convertToValidInteger(testInput2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(TRY_COUNT_NOT_A_NUMBER.getMessage());
+    }
+
+    @Test
+    void 시도횟수_양수가_아닌_예외테스트() {
+        // given
+        String testInput1 = "0";
+        String testInput2 = "-3";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> Converter.convertToValidInteger(testInput1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(TRY_COUNT_NOT_A_POSITIVE_INTEGER.getMessage());
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> Converter.convertToValidInteger(testInput2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(TRY_COUNT_NOT_A_POSITIVE_INTEGER.getMessage());
+    }
+
+    @Test
+    void 자동차_이름_검증_테스트() {
+        // given
+        String testInput = "aaaa,bbb,ccccc,dd,e";
+        String testInput2 = "x,y,z";
+        String testInput3 = "seung,gun,sin";
+
+        // when, then
+        assertDoesNotThrow(
+                () -> InputValidator.validateCarsInput(testInput)
+        );
+
+        assertDoesNotThrow(
+                () -> InputValidator.validateCarsInput(testInput2)
+        );
+
+        assertDoesNotThrow(
+                () -> InputValidator.validateCarsInput(testInput3)
+        );
+    }
+
+    @Test
+    void 시도횟수_검증_테스트() {
+        // given
+        String testInput1 = "1";
+        String testInput2 = "5";
+        String testInput3 = "12";
+
+        // when, then
+        assertDoesNotThrow(
+                () -> Converter.convertToValidInteger(testInput1)
+        );
+
+        assertDoesNotThrow(
+                () -> Converter.convertToValidInteger(testInput2)
+        );
+        assertDoesNotThrow(
+                () -> Converter.convertToValidInteger(testInput3)
+        );
+    }
+
+
+}
