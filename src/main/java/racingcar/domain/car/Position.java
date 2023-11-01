13a7c99d900420efe24bb56e091a407ebc7caa5f
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 14:50:58 +0900

    feat: Position의 toString() 메소드 구현

--- src/main/java/racingcar/domain/car/Position.java
+++ src/main/java/racingcar/domain/car/Position.java
@@ -2,10 +2,17 @@ package racingcar.domain.car;
 
 public class Position {
     private static final int START_POSITION = 0;
+    private static final String POSITION_SYMBOL = "-";
+
     private int position = START_POSITION;
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public String toString() {
+        return POSITION_SYMBOL.repeat(position);
     }
 
     public void increase() {
