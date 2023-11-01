cbdbcfc937bf6924db4d39d24b000a69c23994ac
win9 <krrr883@gmail.com>
2023-11-01 15:51:31 +0900

    [Test]: 게임 전체 진행 테스트 구현
    단일, 공동 우승테스트 구현

--- src/test/java/racingcar/controller/RacingControllerTest.java
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -1,14 +1,17 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.Application;
 import racingcar.service.RacingService;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -16,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("RacingController 테스트")
 class RacingControllerTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
     private RacingController racingController;
 
     @BeforeEach
@@ -23,9 +28,15 @@ class RacingControllerTest extends NsTest {
         racingController = new RacingController();
     }
 
+    @AfterEach
+    void end() {
+        racingController.cleanUp();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {" ", "\n", "\r", "\r\n"})
     void 초기_세팅_이름_공백_예외_테스트(String input) {
+
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -78,6 +89,39 @@ class RacingControllerTest extends NsTest {
                     runException("pobi", input);
                     assertThat(output()).contains("실행 결과");
                 }
+        );
+    }
+
+    @Test
+    void 전체_진행_단일_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("kim,jack", "1");
+                    assertThat(output()).contains("kim : -", "jack : ", "최종 우승자 : kim");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 전체_진행_횟수_0_공동_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("kim,jack", "0");
+                    assertThat(output()).contains("최종 우승자 : kim, jack");
+                },
+                STOP, STOP
+        );
+    }
+
+    @Test
+    void 전체_진행_공동_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("kim,jack", "2");
+                    assertThat(output()).contains("kim : --", "jack : ", "최종 우승자 : kim");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
         );
     }
 
