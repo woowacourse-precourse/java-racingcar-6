562f633327f47f69e7f3aa1957b85487bd7cf4d5
minwoo <mark42son@gmail.com>
2023-11-01 19:11:32 +0900

    feat : 랜덤 숫자를 생성하고 전진 조건에 부합하면 움직이는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
+    private static final int MIN_RANGE_NUMBER = 0;
+    private static final int MAX_RANGE_NUMBER = 9;
+    private static final int MOVABLE_NUMBER = 4;
+    private int position = 0;
 
+
+    public void moveForward() {
+        if (randomMoveNumber()) {
+            this.position++;
+        }
+    }
+
+    private boolean randomMoveNumber() {
+        return Randoms.pickNumberInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER) >= MOVABLE_NUMBER;
+    }
 
 }
