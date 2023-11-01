9812fa996589e28cae97b2833a52f1bf63c334d2
Pearl <rkdwlswn227@gmail.com>
2023-11-01 19:42:10 +0900

    main: application 실행을 위한 세팅

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,8 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
 
+        RacingGame newRacingGame = new RacingGame();
+        newRacingGame.startGame();
+
     }
 }
