835abcb6a12486cc77c358391103fc88e0d97952
SuhyungK <dook4966@gmail.com>
2023-11-01 17:29:18 +0900

    feat: 사용자가 입력한 자동차 이름 raceCars 배열에 추가하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+class Cars {
+    public Map<String, Integer> raceCars = new HashMap<>();
+
+    public Cars(String[] inputCars) {
+        for (String car: inputCars) {
+            this.raceCars.put(car, 0);
+        }
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Cars cars = new Cars(Console.readLine().split(","));
+        
     }
 }
