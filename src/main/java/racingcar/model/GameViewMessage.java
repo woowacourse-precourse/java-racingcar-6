97d4a32e8a4451671d88659ea0fa7dd45c50b6e1
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-01 20:33:13 +0900

    refactor: (GameViewMessage) enum 변수명 변경

--- src/main/java/racingcar/model/GameViewMessage.java
+++ src/main/java/racingcar/model/GameViewMessage.java
@@ -1,12 +1,13 @@
 package racingcar.model;
 
 public enum GameViewMessage {
-    writeCarNameMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    writeRaceLengthMessage("시도할 회수는 몇회인가요?"),
-    outputFirstLineMessage("실행결과"),
-    outputWinnerMessage("최종 우승자"),
-    colonString(":"),
-    spaceBar(" ");
+    WRITE_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    WRITE_RACE_LENGTH_MESSAGE("시도할 회수는 몇회인가요?"),
+    OUTPUT_FIRST_LINE_MESSAGE("실행결과"),
+    OUTPUT_WINNER_MESSAGE("최종 우승자"),
+    COLONSTRING(":"),
+    COMMA(","),
+    SPACEBAR(" ");
 
 
     private final String message;
