97881ce8580a63f5b4c05b4e3ad5ed8e39832851
BINBEAN777 <peanutchan@naver.com>
2023-11-01 22:24:38 +0900

    <Refactor> : "메소드 단순화" start 안에 있던 여러 동작들을 메소드로 세분화 시켜 start 메소드 단순화 시킴.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -10,20 +10,14 @@ public class Output {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    ;
-
-    public static void tellGroupName(String[] groupName) {
-        System.out.println(String.join(",", groupName));
-    }
 
     public static void tellHowManyRoundWant() {
         System.out.println("시도할 회수는 몇회인가요?");
-
     }
 
-    public static void tellCurrent(HashMap map, String[] groupCar) {
-        for (String name : groupCar) {
-            String line = showHowMuchGo((Integer) map.get(name));
+    public static void userCondition(HashMap mapOfUserInfo, String[] stringArrayOfUserName) {
+        for (String name : stringArrayOfUserName) {
+            String line = showHowMuchGo((Integer) mapOfUserInfo.get(name));
             System.out.println(name + " : " + line);
         }
     }
@@ -38,7 +32,6 @@ public class Output {
 
 
     public static int findNumOfWinner(HashMap groupMap, String[] groupName, int getRoundNum) {
-        // 최고 전진 횟수를 찾고 이후 이 최고 전진횟수를 가진 유저를 찾는다.
         int roundNum = getRoundNum;
         boolean judgeTrue = true;
         while (!groupMap.containsValue(roundNum)) {
@@ -49,8 +42,6 @@ public class Output {
 
 
     public static String[] findWinner(HashMap groupMap, String[] groupName, int roundNum) {
-        //list에 몇명이 들어갈지 모름. 사이즈만큼 String 배열 만들고
-        // 배열만큼 쉼표 넣어서 구분지어서 승리자 출력
         List<String> winnerList = new ArrayList<>();
         for (String name : groupName) {
             if (groupMap.get(name) == (Integer) roundNum) {
@@ -65,7 +56,7 @@ public class Output {
     }
 
     public static void showWinner(String[] winnerGroup) {
-        System.out.print("최종 우승자 : "); // static final 로 상수화 꼭 시켜주는 작업해주기
+        System.out.print("최종 우승자 : ");
         if (winnerGroup.length == 1) {
             System.out.println(winnerGroup[0]);
         } else {
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,38 +9,43 @@ import java.util.List;
 public class RacingGame {
     public void start() {
         Output.tellStartAndWantName();
-        String[] carName = Input.getCarName();
-        // Output.tellGroupName(carName);
-        HashMap<String, Integer> userInfo = new HashMap<>(carName.length);
-        for (String name : carName) {
+        String[] userName = Input.getCarName();
+        HashMap<String, Integer> userInfo = new HashMap<>(userName.length);
+        userGetZeroBase(userInfo, userName);
+        int gameRoundNum = playRound(userInfo, userName);
+        int numOfWinnerGoNum = Output.findNumOfWinner(userInfo, userName, gameRoundNum);
+        Output.showWinner(Output.findWinner(userInfo, userName, numOfWinnerGoNum));
+    }
+
+    void countGo(HashMap userInfo, String[] userName) {
+        for (String name : userName) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                userInfo.put(name, (Integer) userInfo.get(name) + 1);
+            }
+        }
+    }
+
+    void userGetZeroBase(HashMap userInfo, String[] userName) {
+        for (String name : userName) {
             userInfo.put(name, 0);
         }
+    }
 
+    int playRound(HashMap userInfo, String[] userName) {
         int currentRoundNum = 0;
         Output.tellHowManyRoundWant();
         int gameRoundNum = Input.getGameRoundNum();
 
         while (gameRoundNum != currentRoundNum) {
-            countGo(userInfo, carName);
-            Output.tellCurrent(userInfo, carName);
+            countGo(userInfo, userName);
+            Output.userCondition(userInfo, userName);
             currentRoundNum += 1;
             System.out.println();
         }
-        int numOfWinnerGoNum = Output.findNumOfWinner(userInfo, carName, gameRoundNum);
-        Output.showWinner(Output.findWinner(userInfo, carName, numOfWinnerGoNum));
-
+        return gameRoundNum;
     }
-
-    void countGo(HashMap name, String[] groupName) {
-        for (String carName : groupName) {
-            if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                name.put(carName, (Integer) name.get(carName) + 1);
-            }
-        }
-    }
-
-
 }
+
 
 
 
