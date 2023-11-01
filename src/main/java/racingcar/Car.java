2bf3afb8105170a6d8ab49067349f53d8542351d
songyi <song21677@naver.com>
2023-11-01 21:50:49 +0900

    feat: 자동차 움직임 결정 및 자동차 전진 CarController 자동차 움직임 결정 담당 Car 자동차 움직임 담당 기능 목록 수정 테스트코드 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,12 @@ public class Car {
 		this.carName = carName;
 		moveCount = 0;
 	}
+
+	public boolean move(int random) {
+		if (random >= 4) {
+			moveCount++;
+			return true;
+		}
+		return false;
+	}
 }
