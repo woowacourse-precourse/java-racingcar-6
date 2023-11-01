cd6e176fe4702b4af31f85f28b2397d182bc9fbb
jinchiim <nellroll@syuin.ac.kr>
2023-11-01 20:13:42 +0900

    test: CarsTest test 추가
    CarsTest test 추가
    - 차가 난수에 따라 움직이는지 Test 추가
    - 차가 난수에 따라 움직이지 않는지 Test 추가

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -1,14 +1,22 @@
 package racingcar.domain.car;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import racingcar.util.RandomGenerator;
+import racingcar.util.RandomNumberGeneratorTest;
 import racingcar.view.input.error.InputIllegalArgumentException;
 
 public class CarsTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     void Cars_Constructor이_정상_작동하는지_Test() {
@@ -23,6 +31,28 @@ public class CarsTest extends NsTest {
         List<String> carNames = List.of("나혼자뛰뛰..");
 
         assertThrows(InputIllegalArgumentException.class, () -> new Cars(carNames));
+    }
+
+    @Test
+    void 차가_움직이는지_Test() {
+        Cars cars = new Cars(Arrays.asList("pobi", "wooni"));
+        RandomGenerator randomGenerator = new RandomNumberGeneratorTest(MOVING_FORWARD);
+
+        cars.randomCarMove(randomGenerator);
+        Map<String, String> carStatus = cars.generateCarStatus();
+
+        assertEquals("{pobi=-, wooni=-}", carStatus.toString());
+    }
+
+    @Test
+    void 차가_움직이지않는지_Test() {
+        Cars cars = new Cars(Arrays.asList("pobi", "wooni"));
+        RandomGenerator randomGenerator = new RandomNumberGeneratorTest(STOP);
+
+        cars.randomCarMove(randomGenerator);
+        Map<String, String> carStatus = cars.generateCarStatus();
+
+        assertEquals("{pobi=, wooni=}", carStatus.toString());
     }
 
     @Override
