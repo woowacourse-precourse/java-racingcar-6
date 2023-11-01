2e71d8ed1643f7cc7459e44b428567eb23773a6c
Ukj0ng <xb253q@gmail.com>
2023-11-01 15:31:55 +0900

    feat(RaceController): 경주를 진행하는 기능
    추가한 기능:
    - 한 라운드마다 각각의 자동차의 랜덤 숫자를 추출해 전진 or 정지하는 기능
    - 한 라운드가 지나면 사용자에게 현재 진행현황을 알리는 기능

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,2 +1,66 @@
-package racingcar.controller;public class RaceController {
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.RacingCars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    private static final int MOVE_FORWARD_NUMBER = 4;
+    private final RacingCars racingCars;
+    private int numberOfRounds;
+
+    public RaceController(RacingCars racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void startRace() {
+        this.numberOfRounds = getNumberOfRounds(InputView.howManyTries());
+        race(this.racingCars);
+    }
+
+    private void race(RacingCars racingCars) {
+        System.out.println(OutputView.resultText());
+        while(numberOfRounds-- > 0) {
+            round(racingCars.getRacingCars());
+            System.out.print(showRacingStatus(racingCars));
+        }
+    }
+
+    private String showRacingStatus(RacingCars racingCars) {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : racingCars.getRacingCars()) {
+            sb.append(showCarStatus(car)).append("\n");
+        }
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    private String showCarStatus(Car car) {
+        return car.getName() + " : " + OutputView.showMoveStatus(car.getMove());
+      }
+
+    private int getNumberOfRounds(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private void round(List<Car> racingCars) {
+        for(Car car : racingCars) {
+            moveForward(car);
+        }
+    }
+
+    private void moveForward(Car car) {
+        int forwardCheck = generateRandomNumber();
+        if(forwardCheck >= MOVE_FORWARD_NUMBER) {
+            car.moveForward();
+        }
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
