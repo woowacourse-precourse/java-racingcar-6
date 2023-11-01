d9e21c6bdfc8cf03406f98e79936736eac9bf0d5
kimminsu <xjfcnfw3@naver.com>
2023-11-01 21:30:31 +0900

    refactor: 검증 클래스 상수 이름 수정

--- src/main/java/racingcar/domain/trial/validate/PositiveIntegerValidator.java
+++ src/main/java/racingcar/domain/trial/validate/PositiveIntegerValidator.java
@@ -2,11 +2,11 @@ package racingcar.domain.trial.validate;
 
 public class PositiveIntegerValidator {
 
-    private static final String NEGATIVE_INTEGER = "시도 횟수는 양수로 입력하세요.";
+    private static final String NOT_POSITIVE_INTEGER = "시도 횟수는 양수로 입력하세요.";
 
     public static void validate(Integer number) {
         if (number <= 0) {
-            throw new IllegalArgumentException(NEGATIVE_INTEGER);
+            throw new IllegalArgumentException(NOT_POSITIVE_INTEGER);
         }
     }
 }
