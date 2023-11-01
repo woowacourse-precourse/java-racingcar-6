f64dfa2f4a2b34c21ed822251554516e5dbd8005
peter <97747863+wcorn@users.noreply.github.com>
2023-11-01 18:32:43 +0900

    [FEAT] 게임 실행

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,28 +1,65 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
 import racingcar.view.Output;
 import racingcar.view.Input;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Game {
     private int attempt;
+    private List<Car> cars;
     public void run() {
         inputCar();
         inputTimes();
+        startGame();
+    }
+
+    private void startGame() {
+        Output.startGamePrintExcuseResult();
+        excuseGame();
+    }
+
+    private void excuseGame() {
+        for (int i = 0; i < attempt; i++) {
+            for (Car c : cars) {
+                if (!canMove())
+                    continue;
+                c.move();
+            }
+            printMoveCar();
+        }
+    }
+
+    private void printMoveCar() {
+        Output.moveCar(cars);
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 
     private void inputTimes() {
         Output.inputAttemptNumPrint();
         String input = Input.inputAttemptNum();
         attempt = Validator.validateInputAttemptNum(input);
+        
     }
-[]
+
     private void inputCar() {
         Output.inputCarsNamePrint();
         String input = Input.inputCarsName();
         List<String> string = Arrays.stream(input.split(",")).toList();
         Validator.validateInputCarList(string);
+        cars = initCars(string);
+    }
+
+    private List<Car> initCars(List<String> validatedString) {
+        return validatedString.stream().map(
+                Car::new
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,19 @@
 package racingcar.model;
 
 public class Car {
-
+    private final String name;
+    private int movedDistance;
+    public Car(String name) {
+        this.name= name;
+        this.movedDistance = 0;
+    }
+    public void move(){
+        movedDistance++;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getMoveDistance(){
+        return movedDistance;
+    }
 }
