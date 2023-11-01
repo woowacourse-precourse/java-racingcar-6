12ea6d746fa41377eca27871b7475b1d5e4f843a
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-01 14:52:36 +0900

    feat : 사용자에게 경주할 자동차 이름을 입력 받는다

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -1,2 +1,18 @@
-package racingcar;public class InputManager {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputManager {
+    public void startGame() {
+        getCarNames();
+    }
+
+    public void getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> inputNames = new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
+    }
 }
