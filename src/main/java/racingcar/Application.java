a9f1f4f8946622d040424c898c83e96eac987265
MinseokGo <rhalstjr1999@naver.com>
2023-11-01 15:02:23 +0900

    refactor($Application): 심판 객체 의존 주입
    메인 클래스에서 RacingGame에 Referee 객체 의존 주입을 합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import racingcar.domain.RacingGame;
+import racingcar.domain.Referee;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingGame racingGame = new RacingGame();
+        Referee referee = new Referee();
+        RacingGame racingGame = new RacingGame(referee);
         racingGame.play();
     }
 }
