4c5963d16f754f5fc34d8d87a7cca47e1a98ead4
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-01 18:31:27 +0900

    test(racingcar): 전진 판단 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 class CarTest {
-  
+
+    @Test
+    void 전진_판단() {
+        Car car = new Car("testCar");
+        boolean result = car.canMoveForward(3);
+        assertThat(result).isEqualTo(false);
+    }
 }
