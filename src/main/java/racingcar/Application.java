657a9bbe63dda57c72f1bed385495c1b41166ad8
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-01 20:04:00 +0900

    test: 각 기능에 대한 테스트 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,8 +11,6 @@ public class Application {
     private static void StartGame() {
         Car[] cars = UserEnterCarName();
 
-//        MakeCarObject(cars);
-
         int repeatNumber = UserEnterRepeatNumber();
 
         PlayRound(repeatNumber, cars);
@@ -29,7 +27,7 @@ public class Application {
         }
     }
 
-    private static void WhoIsWinner(Car[] cars) {
+    static String WhoIsWinner(Car[] cars) {
         int max = 0;
         StringBuilder sb = new StringBuilder();
         for(Car car : cars){
@@ -42,6 +40,7 @@ public class Application {
         }
         sb.setLength(sb.length() -2);
         System.out.print("최종 우승자 : " + sb);
+        return sb.toString();
     }
 
     private static void PrintResult(Car[] cars) {
@@ -54,19 +53,19 @@ public class Application {
         }System.out.println();
     }
 
-    private static void CalculateGoOrStop(Car[] cars) {
+    static void CalculateGoOrStop(Car[] cars) {
         for(Car car : cars){
             int n = pickNumberInRange(1,9);
             if(n>=4) car.meter += 1;
         }
     }
 
-    private static int UserEnterRepeatNumber() throws IllegalArgumentException{
+    static int UserEnterRepeatNumber() throws IllegalArgumentException{
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(readLine());
     }
 
-    private static Car[] UserEnterCarName() throws IllegalArgumentException {
+    static Car[] UserEnterCarName() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String EnteredCarName = readLine();
 
@@ -86,12 +85,6 @@ public class Application {
 
         return cars;
     }
-
-//    private static void MakeCarObject(Car[] cars) {
-//        for(int i=0; i<cars.length; i++){
-//            cars[i] = new Car(cars[i].name);
-//        }
-//    }
 
     static class Car {
         String name;
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -7,6 +7,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -31,8 +32,79 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    public void testUserEnterCarName() {
+        // Arrange
+        String input = "Car1,Car2,Car3";
+
+        // Act
+        Application.Car[] cars = Application.UserEnterCarName();
+
+        // Assert
+        assertEquals(3, cars.length);
+        for (Application.Car car : cars) {
+            assertNotNull(car);
+            assertNotNull(car.name);
+            assertEquals(0, car.meter);
+        }
+    }
+
+    @Test
+    public void testUserEnterCarName_WithInvalidInput() {
+        // Arrange
+        String input = "Car1,Car2, Car3, Car4, Car5";  // Contains a car name with a space
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, Application::UserEnterCarName);
+    }
+
+    @Test
+    public void testUserEnterRepeatNumber() {
+        // Arrange
+        String input = "5";
+
+        // Act
+        int repeatNumber = Application.UserEnterRepeatNumber();
+
+        // Assert
+        assertEquals(5, repeatNumber);
+    }
+
+    @Test
+    public void testCalculateGoOrStop() {
+        // Arrange
+        Application.Car[] cars = new Application.Car[]{new Application.Car("Car1"), new Application.Car("Car2"), new Application.Car("Car3")};
+
+        // Act
+        Application.CalculateGoOrStop(cars);
+
+        // Assert
+        for (Application.Car car : cars) {
+            assertTrue(car.meter >= 0 && car.meter <= 1);
+        }
+    }
+
+    @Test
+    public void testWhoIsWinner() {
+        // Arrange
+        Application.Car car1 = new Application.Car("Car1");
+        Application.Car car2 = new Application.Car("Car2");
+        Application.Car car3 = new Application.Car("Car3");
+        car1.meter = 5;
+        car2.meter = 3;
+        car3.meter = 5;
+        Application.Car[] cars = new Application.Car[]{car1, car2, car3};
+
+        // Act
+        String winner = Application.WhoIsWinner(cars);
+
+        // Assert
+        assertEquals("Car1, Car3", winner);
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
