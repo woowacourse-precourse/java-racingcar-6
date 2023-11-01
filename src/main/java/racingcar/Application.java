7ecc20de0723109a8e04dfd065181ac46458b1c2
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-01 18:16:41 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 자동차 이름 입력 받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String[] cars = Console.readLine().split(",");
 
         // TODO: 시도할 회수 입력 받기
 
