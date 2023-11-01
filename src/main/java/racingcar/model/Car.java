678a7eab0c99ea85eff7cfac63e0fb3a3c6c389f
beomsic <qjatjr29@khu.ac.kr>
2023-11-01 14:36:05 +0900

    :recycle: Refactor: 차가 움직이거나 정지하는 메소드 수정
    - 움직이거나 정지하는 기능에 맞는 메소드 명으로 수정
    - 임의의 값을 파라미터로 받아서 이를 처리하도록 변경
    - 자동차에서는 임의의 값의 범위를 모르고 자신의 기준에만 넘는 값이 들어오면 움직일 수 있도록 해야한다고 생각

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -6,15 +6,11 @@ import static racingcar.common.exception.ErrorMessage.NAME_TOO_LONG;
 import static racingcar.common.exception.ErrorMessage.NULL_NAME;
 
 import racingcar.common.exception.CarException;
-import racingcar.util.RandomUtil;
 
 public class Car implements Comparable<Car> {
 
     private static final int MAX_LENGTH_OF_NAME = 5;
     private static final int MIN_RANDOM_VALUE_TO_MOVE = 4;
-    private static final int MIN_RANDOM_VALUE = 0;
-    private static final int MAX_RANDOM_VALUE = 9;
-
     private final String name;
     private Integer movedCount;
 
@@ -29,8 +25,8 @@ public class Car implements Comparable<Car> {
         return new Car(name);
     }
 
-    public Result move() {
-        if(canMove()) movedCount++;
+    public Result moveOrStop(int randomValue) {
+        if(canMove(randomValue)) movedCount++;
         return new Result(name, movedCount);
     }
 
@@ -47,8 +43,8 @@ public class Car implements Comparable<Car> {
         return cmpCar.getMovedCount()- this.getMovedCount();
     }
 
-    private boolean canMove() {
-        return RandomUtil.generateRandomValue(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) >= MIN_RANDOM_VALUE_TO_MOVE;
+    private boolean canMove(int randomValue) {
+        return randomValue >= MIN_RANDOM_VALUE_TO_MOVE;
     }
 
     private void validateNameLength(final String name) {
