ed7e939853365e0cc23faa8da48bea7cde2e2c75
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-01 21:27:41 +0900

    test(RoundTest): Round 객체 생성에 대한 단위 테스트 작성
    1. Round_객체_생성_성공
    
    2. 음수로_Round_객체_생성시_실패
    - IllegalArgumentException이 발생해야한다.

--- src/test/java/racingcar/model/RoundTest.java
+++ src/test/java/racingcar/model/RoundTest.java
@@ -2,6 +2,24 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class RoundTest {
 
+    @Test
+    void Round_객체_생성_성공() {
+        try {
+            Round r1 = new Round(0);
+            Round r2 = new Round(1);
+            Round r3 = new Round(Integer.MAX_VALUE);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+    @Test
+    void 음수로_Round_객체_생성시_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Round r = new Round(-1);
+        });
+    }
 }
