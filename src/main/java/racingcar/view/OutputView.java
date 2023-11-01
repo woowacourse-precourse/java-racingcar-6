81d930d506b8aa7e4711bf095fc0092a7264f57a
sunwoong <sunwoong1122@gmail.com>
2023-11-01 18:10:45 +0900

    feat: 실행 결과 출력 메서드 정의

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
 public class OutputView {
     public void printCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +9,10 @@ public class OutputView {
 
     public void printNumberOfAttempts() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printExecutionResults(Car car) {
+        // TODO
+        System.out.println();
     }
 }
