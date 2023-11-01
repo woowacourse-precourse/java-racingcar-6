aeff57d243ba1cfb2256d32172020afe351c9f42
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 13:53:24 +0900

    feat: 에러 추가 정의

--- src/main/java/racingcar/constants/ErrorConsts.java
+++ src/main/java/racingcar/constants/ErrorConsts.java
@@ -4,7 +4,9 @@ public enum ErrorConsts {
     NULL_CAR_NAME("자동차 이름은 null이 될 수 없습니다."),
     BLANK_CAR_NAME("자동차 이름은 공백만으로는 구성할 수 없습니다."),
     LONG_CAR_NAME("자동차 이름은 %d자 이하만 가능합니다."),
-    INSUFFICIENT_RACE_COUNT("레이스 횟수는 %d 이상이어야 합니다.");
+    INSUFFICIENT_RACE_COUNT("레이스 횟수는 %d 이상이어야 합니다."),
+    NOT_NUMBER_FORMATTED("Integer 범위 내의 숫자여야 합니다."),
+    ;
 
     private final String message;
 
