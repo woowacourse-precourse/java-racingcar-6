f12cf7a32b6e11878b3f2574052194ec95378c47
suhyun__ <thebbbb1206@gmail.com>
2023-11-01 18:51:19 +0900

    refactor: Java 코드 컨벤션에 맞춰 수정

--- src/test/java/racingcar/validator/CarValidatorTest.java
+++ src/test/java/racingcar/validator/CarValidatorTest.java
@@ -5,30 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.model.Car;
 
 public class CarValidatorTest {
     @Test
     public void 대문자가_포함된_경우() {
-        List<String> carNameList1 = Arrays.asList("Pobi","woni");
+        List<String> carNameList1 = Arrays.asList("Pobi", "woni");
         assertThrows(IllegalArgumentException.class, () -> CarValidator.checkUpperCase(carNameList1));
-        List<String> carNameList2 = Arrays.asList("pobi","woni");
+        List<String> carNameList2 = Arrays.asList("pobi", "woni");
         CarValidator.checkUpperCase(carNameList2);
     }
 
     @Test
     public void 이름이_다섯자리_초과인_경우() {
-        List<String> carNameList1 = Arrays.asList("suhyun","woni");
+        List<String> carNameList1 = Arrays.asList("suhyun", "woni");
         assertThrows(IllegalArgumentException.class, () -> CarValidator.checkCarNameLength(carNameList1));
-        List<String> carNameList2 = Arrays.asList("pobi","woni");
+        List<String> carNameList2 = Arrays.asList("pobi", "woni");
         CarValidator.checkCarNameLength(carNameList2);
     }
 
     @Test
     public void 이름이_중복인_경우() {
-        List<String> carNameList1 = Arrays.asList("woni","woni");
+        List<String> carNameList1 = Arrays.asList("woni", "woni");
         assertThrows(IllegalArgumentException.class, () -> CarValidator.checkDuplicateCarName(carNameList1));
-        List<String> carNameList2 = Arrays.asList("pobi","woni");
+        List<String> carNameList2 = Arrays.asList("pobi", "woni");
         CarValidator.checkDuplicateCarName(carNameList2);
     }
 }
