4233815e4f8fee2e05e0b12e1d2d63631e73db13
donghyun <kdhdh001@naver.com>
2023-11-01 19:09:32 +0900

    refactor: RandomGenerator 클래스에 MoveStrategy 인터페이스 구현 추가

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -1,8 +1,16 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.domain.MoveStrategy;
 
-public class RandomGenerator {
+public class RandomGenerator implements MoveStrategy {
+    private static final int FORWARD_NUM = 4;
+
+    @Override
+    public boolean canMove() {
+        return generate() >= FORWARD_NUM;
+
+    }
     public static int generate() {
         return Randoms.pickNumberInRange(0, 9);
     }
