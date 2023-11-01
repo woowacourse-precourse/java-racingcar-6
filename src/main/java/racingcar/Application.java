591b3795f8ee15c218003d13ccbb9c501e48afa3
sayishuu <smile52673@gmail.com>
2023-11-01 23:13:25 +0900

    feat : Application 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager();
+        RacingController racingController = new RacingController(inputManager);
+        racingController.run();
     }
 }
