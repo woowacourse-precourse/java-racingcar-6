b9a8f1dd913529a22b33f48ab494a18309c2ee83
dltjdn <hana40004@naver.com>
2023-11-01 20:21:22 +0900

    test(InputViewTest): 여러 그룹으로 나누어 테스트 구조화
    * 테스트 클래스 내에서 내부 클래스를 만들어 그룹화

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -9,8 +9,10 @@ import java.io.InputStream;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import racingcar.exception.ErrorMessage;
+
+
 
 class InputViewTest {
     InputView inputView;
@@ -20,81 +22,88 @@ class InputViewTest {
         inputView = new InputView();
     }
 
-    @Test
-    void 자동차_이름_입력_테스트() {
-        // given
-        String input = "Car1,Car2,Car3";
-        setInput(input);
+    @Nested
+    class readCarNamesTest{
+        @Test
+        void 자동차_이름_입력_테스트() {
+            // given
+            String input = "Car1,Car2,Car3";
+            setInput(input);
 
-        // when
-        List<String> names = inputView.readCarNames();
+            // when
+            List<String> names = inputView.readCarNames();
 
-        // then
-        assertThat(names).isEqualTo(List.of("Car1", "Car2", "Car3"));
+            // then
+            assertThat(names).isEqualTo(List.of("Car1", "Car2", "Car3"));
+        }
+
+        @Test
+        void 자동차_공백_포함한_이름_입력_테스트() {
+            // given
+            String input = "Car1, Car2, Car3";
+            setInput(input);
+
+            // when
+            List<String> names = inputView.readCarNames();
+
+            // then
+            assertThat(names).isEqualTo(List.of("Car1", "Car2", "Car3"));
+        }
+
+        @Test
+        void 자동차_이름_입력_빈값_예외_테스트() {
+            // given
+            String input = " ";
+            setInput(input);
+
+            // when & then
+            assertThatThrownBy(inputView::readCarNames)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("적어도 한 개 이상의 자동차 이름을 입력해주세요.");
+        }
+
     }
 
-    @Test
-    void 자동차_공백_포함한_이름_입력_테스트() {
-        // given
-        String input = "Car1, Car2, Car3";
-        setInput(input);
+   @Nested
+   class readTryCountTest{
+        @Test
+        void 시도_횟수_입력_테스트() {
+            // given
+            String input = "5";
+            setInput(input);
 
-        // when
-        List<String> names = inputView.readCarNames();
+            // when
+            int tryCount = inputView.readTryCount();
 
-        // then
-        assertThat(names).isEqualTo(List.of("Car1", "Car2", "Car3"));
-    }
+            // then
+            assertThat(tryCount).isEqualTo(5);
+        }
 
-    @Test
-    void 자동차_이름_입력_빈값_예외_테스트() {
-        // given
-        String input = " ";
-        setInput(input);
+        @Test
+        void 시도_횟수_음수_예외_테스트() {
+            // given
+            String input = "-3";
+            setInput(input);
 
-        // when & then
-        assertThatThrownBy(inputView::readCarNames)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.CAR_NAME_INPUT_SIZE_ERROR.getMessage());
-    }
+            // when & then
+            assertThatThrownBy(inputView::readTryCount)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 0 이상이어야 합니다.");
+        }
 
+        @Test
+        void 시도_횟수_잘못된_입력_예외_테스트() {
+            // given
+            String input = "invalidInput";
+            setInput(input);
 
-    @Test
-    void 시도_횟수_입력_테스트() {
-        // given
-        String input = "5";
-        setInput(input);
+            // when & then
+            assertThatThrownBy(inputView::readTryCount)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("잘못된 입력입니다. 유효한 숫자를 입력해주세요.");
+        }
+   }
 
-        // when
-        int tryCount = inputView.readTryCount();
-
-        // then
-        assertThat(tryCount).isEqualTo(5);
-    }
-
-    @Test
-    void 시도_횟수_음수_예외_테스트() {
-        // given
-        String input = "-3";
-        setInput(input);
-
-        // when & then
-        assertThatThrownBy(inputView::readTryCount)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("시도 횟수는 0 이상이어야 합니다.");
-    }
-
-    @Test
-    void 시도_횟수_잘못된_입력_예외_테스트() {
-        // given
-        String input = "invalidInput";
-        setInput(input);
-
-        // given & then
-        assertThatThrownBy(inputView::readTryCount)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("잘못된 입력입니다. 유효한 숫자를 입력해주세요.");
-    }
 
     private void setInput(String input) {
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
