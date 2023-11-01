9b1ab81ebf9d5baf3a7cf1d667b36b419916c838
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 23:26:32 +0900

    feat : 경기 최종 결과, 라운드별 결과, 최종 우승자, 이동 거리, 줄바꿈 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,12 @@
 package racingcar.view;
 
+import racingcar.enums.Constants;
 import racingcar.enums.Messages;
+import racingcar.model.Distance;
+import racingcar.model.Name;
+import racingcar.model.RacingResult;
+
+import java.util.List;
 
 public class OutputView {
     static StringBuilder sb;
@@ -10,5 +16,41 @@ public class OutputView {
 
     public static void printInputTryNumber() {
         System.out.println(Messages.INPUT_TRY_NUMBER_MESSAGE.getMessage());
+    }
+
+    public static void printRacingResult() {
+        System.out.println(Messages.RACING_RESULT.getMessage());
+    }
+
+    public static void printOneRoundResult(RacingResult racingResult, List<Name> carNames) {
+        sb = new StringBuilder();
+        for (Name name : carNames) {
+            sb.append(name.getName()).append(Constants.SEPARATOR.getMessage());
+            printDistance(racingResult.getRacingResult().get(name));
+            changeLine();
+        }
+        System.out.println(sb);
+    }
+
+    public static void printFinalWinner(List<Name> winners) {
+        sb = new StringBuilder();
+        sb.append(Messages.FINAL_WINNER.getMessage()).
+                append(Constants.SEPARATOR.getMessage());
+
+        for (Name name : winners) {
+            sb.append(name.getName()).append(Constants.COMMA.getMessage());
+        }
+
+        System.out.println(sb.substring(0, sb.length() - 1));
+    }
+
+    private static void printDistance(Distance distance) {
+        for (int d = 0; d < distance.getDistance(); d++) {
+            sb.append(Constants.MOVE.getMessage());
+        }
+    }
+
+    private static void changeLine() {
+        sb.append(Constants.LINE_CHANGER.getMessage());
     }
 }
