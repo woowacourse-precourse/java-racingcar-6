d301a4263f1db5848963992472dee728a9ed1f20
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-01 22:24:26 +0900

    add(Judge) : make Judge class a singleton

--- src/main/java/racingcar/service/Judge.java
+++ src/main/java/racingcar/service/Judge.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 
-public class JudgeMachine {
+public class Judge {
+
+    private static class SingletonJudge{
+        private static final Judge JUDGE = new Judge();
+    }
+    public static Judge getInstance(){
+        return SingletonJudge.JUDGE;
+    }
+    private Judge() {
+    }
     private void judge(List<Car> carList) {
         Integer winningDistance = 0;
         for (Car car : carList) {
@@ -24,7 +33,6 @@ public class JudgeMachine {
         List<Car> winningCars = new ArrayList<>();
         for (Car car : carList) {
             if (car.isWinner()) {
-                System.out.println(car.getName());
                 winningCars.add(car);
             }
         }
