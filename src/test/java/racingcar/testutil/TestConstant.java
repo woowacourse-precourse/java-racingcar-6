b3256bc403cc37847fde80047e58fc04ab39f3e0
Sung Hyobin <hellmir610@gmail.com>
2023-11-01 19:41:52 +0900

    test: Add test of generating random numbers by input count

--- src/test/java/racingcar/testutil/TestConstant.java
+++ src/test/java/racingcar/testutil/TestConstant.java
@@ -5,6 +5,9 @@ public class TestConstant {
     public static final String CAR_NAME1 = "자동차1";
     public static final String CAR_NAME2 = "car2";
     public static final String CAR_NAME3 = "Car3";
+    public static final int RANDOM_NUMBER1 = 0;
+    public static final int RANDOM_NUMBER2 = 1;
+    public static final int RANDOM_NUMBER3 = 9;
 
 
 }
