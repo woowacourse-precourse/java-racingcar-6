bf998780c47588769742d67fc2384ab80acf3657
woookk <woook27@naver.com>
2023-11-01 23:21:42 +0900

    refactor: 자동차 이름 유효성 검사를 Car 생성자로 이동

--- src/main/java/racingcar/console/Input.java
+++ src/main/java/racingcar/console/Input.java
@@ -6,21 +6,12 @@ public class Input {
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
-        validateCarNames(carNames);
         return carNames;
     }
 
     public static int getRounds() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
-    }
-
-    private static void validateCarNames(String names) {
-        for (String name : names.split(",")) {
-            if (name.trim().length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
-        }
     }
 }
 
--- src/main/java/racingcar/entity/Car.java
+++ src/main/java/racingcar/entity/Car.java
@@ -2,11 +2,20 @@ package racingcar.entity;
 
 public class Car {
 
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private static void validate(String name) {
+        if (name.trim().length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public String getName() {
