4282e7d4e7035a3b3d496a99c0cf122f32ca41d3
Silver <leeeun5423@naver.com>
2023-11-01 15:55:53 +0900

    test : testing regarding inputs on cars

--- src/test/java/racingcar/UserInputTest.java
+++ src/test/java/racingcar/UserInputTest.java
@@ -20,6 +20,33 @@ public class UserInputTest extends  NsTest{
                 );
     }
 
+    @Test
+    @DisplayName("testing_same_names")
+    void dupNameCheck() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("ej,ej", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("testing_one_player")
+    void onePlayersCheck() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("david", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("exception_when_zero_player")
+    void zeroPlayersCheck() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
