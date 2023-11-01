e48a930fadc823f21637aaab558218b2918c1b83
Minyongggg <naminyong97@gmail.com>
2023-11-01 17:37:06 +0900

    feat: 콘솔 메시지 상수 값 클래스 추가

--- src/main/java/racingcar/constant/ConsoleMessages.java
+++ src/main/java/racingcar/constant/ConsoleMessages.java
@@ -1,4 +1,7 @@
 package racingcar.constant;
 
 public class ConsoleMessages {
+    public static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String RESUILT_MESSAGE = "실행 결과";
 }
