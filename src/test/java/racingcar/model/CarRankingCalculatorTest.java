98f1c367190d75968a41569d0e10b48b5d961ef7
honeydanji <tjdwls3680@naver.com>
2023-11-01 15:07:52 +0900

    REFACTOR : 객체 타입 변경
    - 1명이 우승한 경우 >> 문제없음
    - N명이 우승한 경우
    >> 기존 : HashMap 으로 선언
    >> 문제점 : key의 순서가 틀어짐
    
    >> 수정 : LikedHashMap 으로 변경
    >> 해결 : 우승자가 순서대로 나열

--- src/test/java/racingcar/model/CarRankingCalculatorTest.java
+++ src/test/java/racingcar/model/CarRankingCalculatorTest.java
@@ -2,7 +2,6 @@ package racingcar.model;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class CarRankingCalculatorTest {
@@ -11,7 +10,7 @@ public class CarRankingCalculatorTest {
     void 우승자_1명_테스트() {
         CarRankingCalculator carRankingCalculator = new CarRankingCalculator();
 
-        HashMap<String, String> carAndDistance = new HashMap<>();
+        LinkedHashMap<String, String> carAndDistance = new LinkedHashMap<>();
         carAndDistance.put("dokgo","__");
         carAndDistance.put("may","_");
         carAndDistance.put("cat","___");
