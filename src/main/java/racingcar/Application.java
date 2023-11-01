5f2ecd5076ab5cb6b24ac7c31344265aa84f1853
plan11plan <tjtfy7028@gmail.com>
2023-11-01 22:30:30 +0900

    feat:[creat][main] 메인에서 게임 시작에서 종료까지 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Car> cars = InputView.inputCars();
+        int rounds = InputView.inputRounds();
+        RacingGame racingGame = new RacingGame(cars, rounds);
+        racingGame.start();
+
     }
 }
