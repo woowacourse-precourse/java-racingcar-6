3a26a535be62f85bf97f341e451ee97da71cd284
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-01 15:12:12 +0900

    refactor: change the error messages into final constant
    에러 메세지를 final 상수로 정의해서 재사용 가능

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -4,33 +4,38 @@ import java.util.List;
 import racingcar.model.Cars;
 
 public class Validation {
-    // indent depth = 2인 메서드임
-    // 글자(CarNames) 입력 검증
+    
+    private static final String ERROR_NULL = "빈칸입니다.";
+    private static final String ERROR_NAME_LENGTH =  "5자 이하의 이름만 입력 가능합니다.";
+    private static final String ERROR_NAME_DUPLICATE = "이름이 중복되었습니다.";
+    private static final String ERROR_NUMBER_FORMAT = "숫자가 아닙니다.";
+    private static final String ERROR_NUMBER_RANGE = "음수는 입력 불가합니다.";
+
 
     public static void validateNameInput(String input) {
         validateStringNull(input);
         List<String> list = Cars.makeCarList(input);
         validateNameLength(list);
-        validateDuplicateName(list);
+        validateNameDuplicate(list);
     }
 
     private static void validateStringNull(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("빈칸입니다.");
+            throw new IllegalArgumentException(ERROR_NULL);
         }
     }
 
     private static void validateNameLength(List<String> list) {
         for (String input : list) {
             if (input.length() > 5) {
-                throw new IllegalArgumentException("5자 이하의 이름만 입력 가능합니다");
+                throw new IllegalArgumentException(ERROR_NAME_LENGTH);
             }
         }
     }
 
-    private static void validateDuplicateName(List<String> list) {
+    private static void validateNameDuplicate(List<String> list) {
         if (list.size() != list.stream().distinct().count()) {
-            throw new IllegalArgumentException("이름이 중복되었습니다.");
+            throw new IllegalArgumentException(ERROR_NAME_DUPLICATE);
         }
     }
 
@@ -44,7 +49,7 @@ public class Validation {
 
     private static void validateNumberNull(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("빈칸입니다.");
+            throw new IllegalArgumentException(ERROR_NULL);
         }
     }
 
@@ -52,17 +57,13 @@ public class Validation {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
+            throw new IllegalArgumentException(ERROR_NUMBER_FORMAT);
         }
     }
 
     private static void validateNumberRange(String input) {
         if (Integer.parseInt(input) < 0) {
-            throw new IllegalArgumentException("음수는 입력 불가합니다.");
+            throw new IllegalArgumentException(ERROR_NUMBER_RANGE);
         }
     }
-
-
-
-
 }
