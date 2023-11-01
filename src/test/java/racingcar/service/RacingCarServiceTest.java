97f0c8f63903bb47ba2791193e9593cae4df0b9e
InJun2 <os2864@naver.com>
2023-11-01 15:03:42 +0900

    :art: racingCarServiceTest 클래스명 변경

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -7,11 +7,9 @@ import racingcar.model.Car;
 import racingcar.model.Cars;
 
 import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
-public class RaceTest {
+class RacingCarServiceTest {
     private final static String WINNER = "winner";
     private final static String LOSER = "loser";
 
@@ -24,11 +22,16 @@ public class RaceTest {
         Car car1 = Mockito.mock(Car.class);
         Car car2 = Mockito.mock(Car.class);
 
-        Mockito.when(cars.getCars()).thenReturn(Arrays.asList(car1, car2));
-        Mockito.when(car1.getName()).thenReturn(WINNER);
-        Mockito.when(car2.getName()).thenReturn(LOSER);
-        Mockito.when(car1.getDistance()).thenReturn(2);
-        Mockito.when(car2.getDistance()).thenReturn(1);
+        Mockito.when(cars.getCars())
+                .thenReturn(Arrays.asList(car1, car2));
+        Mockito.when(car1.getName())
+                .thenReturn(WINNER);
+        Mockito.when(car2.getName())
+                .thenReturn(LOSER);
+        Mockito.when(car1.getDistance())
+                .thenReturn(2);
+        Mockito.when(car2.getDistance())
+                .thenReturn(1);
 
         String winnerName = racingCarService.getWinnerCarNames(cars)
                 .get(0);
