e6451aead7a1bd04e1ae18b036af552e02528728
saera <yook.saera@gmail.com>
2023-11-01 20:03:41 +0900

    feat(racingcar): 사용자가 입력한 시도 횟수가 0~9로 이루어진 문자열이 아닌 경우 예외를 던져라

--- src/main/java/racingcar/TotalRoundsConverter.java
+++ src/main/java/racingcar/TotalRoundsConverter.java
@@ -3,7 +3,17 @@ package racingcar;
 public class TotalRoundsConverter {
 
     public int convert(String input) {
+        checkIsDigit(input);
         return Integer.parseInt(input);
+    }
+
+    private void checkIsDigit(String input) {
+        char[] characterizedInput = input.toCharArray();
+        for (char number : characterizedInput) {
+            if ('0' > number || '9' < number) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
 }
--- src/test/java/racingcar/TotalRoundsConverterTest.java
+++ src/test/java/racingcar/TotalRoundsConverterTest.java
@@ -1,18 +1,27 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 public class TotalRoundsConverterTest {
 
     @Test
-    void convert_메서드가_사용자가_입력한_문자열을_정수로_변환해_반환() {
+    void convert_메서드가_사용자가_입력한_문자열을_정수로_변라환해_반환() {
         String input = "90";
         TotalRoundsConverter totalRoundsConverter = new TotalRoundsConverter();
 
         int actual = totalRoundsConverter.convert(input);
 
         assertThat(actual).isEqualTo(90);
+    }
+
+    @Test
+    void convert_메서드가_checkIsDigit_메서드를_호출해_사용자_입력값에_숫자_문자가_아닌_문자가_있으면_예외_발생() {
+        String input = "90회";
+        TotalRoundsConverter totalRoundsConverter = new TotalRoundsConverter();
+
+        assertThatThrownBy(() -> totalRoundsConverter.convert(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
