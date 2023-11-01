0424aa7915103ad05863a4e2db6c88ed4d1038c3
Jongwoo Han <jongwooo.han@gmail.com>
2023-11-01 17:01:09 +0900

    refactor: remove unnecessary imports

--- src/test/java/racingcar/domain/game/RaceGameTest.java
+++ src/test/java/racingcar/domain/game/RaceGameTest.java
@@ -3,7 +3,6 @@ package racingcar.domain.game;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
