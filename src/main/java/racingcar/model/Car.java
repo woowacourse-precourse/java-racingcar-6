efe10f8cd9da2997dc10484db48be2460fc681c3
Jeonwoojae <dnwo0719@icloud.com>
2023-11-01 18:31:18 +0900

    refactor: Car 생성 시 이름 유효성 검증 로직 분리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,24 +4,29 @@ import static racingcar.view.ExceptionMessage.NOT_NULL;
 import static racingcar.view.ExceptionMessage.NOT_OVER_FIVE;
 import static racingcar.view.ExceptionMessage.NOT_KOREAN_OR_ENGLISH;
 
+import racingcar.view.ExceptionMessage;
+
 public class Car {
     public static final int NAME_MAX_LENGTH = 5;
     public static final int NAME_MIN_LENGTH = 1;
     private String name;
     private int location = 0;
 
-    // TODO validate 분리
     public Car(String name) {
-        if (name == null) {
-            throw new IllegalArgumentException(NOT_NULL.getMessage());
-        }
-        if (name.length() > NAME_MAX_LENGTH || name.length() < NAME_MIN_LENGTH) {
-            throw new IllegalArgumentException(NOT_OVER_FIVE.getMessage());
-        }
-        if (!name.matches("^[a-zA-Z가-힣]*$")) {
-            throw new IllegalArgumentException(NOT_KOREAN_OR_ENGLISH.getMessage());
-        }
+        validateName(name);
         this.name = name;
+    }
+
+    private static void validateName(String name) {
+        checkConditionAndThrow(name == null, NOT_NULL);
+        checkConditionAndThrow(name.length() > NAME_MAX_LENGTH || name.length() < NAME_MIN_LENGTH, NOT_OVER_FIVE);
+        checkConditionAndThrow(!name.matches("^[a-zA-Z가-힣]*$"), NOT_KOREAN_OR_ENGLISH);
+    }
+
+    private static void checkConditionAndThrow(boolean condition, ExceptionMessage message) {
+        if (condition) {
+            throw new IllegalArgumentException(message.getMessage());
+        }
     }
 
     public String getName() {
