d25cf3f56cadd1bd04ccd7d927d923ff27449306
nayonsoso <panda0329@naver.com>
2023-11-01 17:50:33 +0900

    refactor: RaceDto의 이동 정보 필드를 int에서 String으로 변경

--- src/main/java/model/RaceExecutor.java
+++ src/main/java/model/RaceExecutor.java
@@ -4,22 +4,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import view.OutputView;
 
 public class RaceExecutor {
 
     private final MovementDecider movementDecider = new MovementDecider();
+    private final OutputView outputView = new OutputView();
     private final RaceDto raceDto;
+    private List<String> names;
 
     RaceExecutor(RaceDto raceDto) {
         this.raceDto = raceDto;
+        this.names = raceDto.getCars().keySet().stream().toList();
     }
 
-    public RaceDto singleAttempt() {
-        Set<String> nameSet = raceDto.getCars().keySet();
-        for (String name : nameSet) {
+    public void attemptAndPrintResult(){
+        for (int i = 0; i < raceDto.getAttemptTimes(); i++) {
+            singleAttempt();
+            printAttemptResult();
+        }
+    }
+
+    private void singleAttempt() {
+        for (String name : names) {
             moveCarAtIfPossible(name);
         }
-        return raceDto;
     }
 
     private void moveCarAtIfPossible(String name) {
@@ -29,7 +38,17 @@ public class RaceExecutor {
     }
 
     private void moveCarByName(String name) {
-        int originalValue = raceDto.getCars().get(name);
-        raceDto.getCars().put(name, originalValue + 1);
+        String originalValue = getMovementString(name);
+        raceDto.getCars().put(name, originalValue + "-");
+    }
+
+    private void printAttemptResult(){
+        for (String name : names) {
+            outputView.printAttemptResult(name, getMovementString(name));
+        }
+    }
+
+    private String getMovementString(String name){
+        return raceDto.getCars().get(name);
     }
 }
