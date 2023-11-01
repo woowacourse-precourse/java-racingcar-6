c058e46198d196f3a8e55969c160a347460eaeea
ldw <leehan02@hanyang.ac.kr>
2023-11-01 15:47:36 +0900

    test(racingcar): 입력값이 공백으로만 이루어진 경우에 대한 단위 테스트 구현

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -6,18 +6,18 @@ import org.junit.jupiter.api.Test;
 public class InputValidatorTest {
 
     @Test
-    void 입력값이_빈_문자열_혹은_공백이_아닌_경우() {
+    void 입력값이_빈_문자열_혹은_공백이_아닌_유효한_경우() {
         String input = "This string is not blank";
 
         Assertions.assertDoesNotThrow(() -> InputValidator.blank(input));
     }
 
-//    @Test
-//    void 입력값이_공백_으로만_이루어진_경우_예외_발생() {
-//        String input = "        ";
-//
-//        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.blank(input));
-//    }
+    @Test
+    void 입력값이_공백_으로만_이루어진_경우_예외_발생() {
+        String input = "        ";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> InputValidator.blank(input));
+    }
 //
 //    @Test
 //    void 입력값이_빈_문자열인_경우_예외_발생() {
