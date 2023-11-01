fe45510aa575e4d56e70c76eb47316932e952016
mung96 <mung5079@gmail.com>
2023-11-01 11:26:44 +0900

    Refactor: createName을 수정
    기존에는 자동차 이름이 담긴 배열을 return, 수정후 Map에 이름,상태를 둘 다 담음

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Game {
-    public String[] createName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
+    private static HashMap<String,String> players= new HashMap<>();
+    public Map<String, String> createName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String[] cars = camp.nextstep.edu.missionutils.Console.readLine().split(",");
 
@@ -10,13 +14,14 @@ public class Game {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
+            players.put(name,"");
         }
-
-        return cars;
+        return players;
     }
 
     public int tryNumber(){
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
     }
-}
+
+    }
