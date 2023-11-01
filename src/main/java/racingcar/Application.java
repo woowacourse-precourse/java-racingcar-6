84ac6d2f2f20ba73f26ea27615cc93b408c44c48
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2023-11-01 16:14:35 +0900

    fet: 자동차 게임 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import racingcar.Controller.RacingGameController;
+import racingcar.View.RacingGameView;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input=sc.nextLine();
+        String[] carNames=input.split(",");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int game=sc.nextInt();
+
+        RacingGameView view = new RacingGameView();
+        RacingGameController controller = new RacingGameController(Arrays.asList(carNames),view );
+
+        System.out.println("실행 결과");
+        controller.startGame(game);
+
+        sc.close();
     }
 }
