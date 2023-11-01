82748e4085371bc46e1a221d874b79e08dacf4e7
dbstn1325 <dbstn1325@naver.com>
2023-11-01 22:54:17 +0900

    refactor(RacingCarTest): 전체 자동차 경주를 실행하는 기능 내 코딩 컨벤션을 위한 들여쓰기 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import controller.RacingCar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 
 public class RacingCarTest extends NsTest {
@@ -16,21 +17,21 @@ public class RacingCarTest extends NsTest {
         RacingCar racingCar = new RacingCar();
 
         assertRandomNumberInRangeTest(
-            () -> {
-                //when
-                run("yoon,su", "3");
-                racingCar.startGame();
-                //then
-                Assertions.assertThat(output()).contains(
-                        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-                        "시도할 회수는 몇회인가요?",
-                        "실행 결과",
-                        "yoon : -", "su : ",
-                        "yoon : -", "su : -",
-                        "yoon : --", "su : -",
-                        "최종 우승자 : yoon"
-                );
-            },
+                () -> {
+                    //when
+                    run("yoon,su", "3");
+                    racingCar.startGame();
+                    //then
+                    Assertions.assertThat(output()).contains(
+                            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                            "시도할 회수는 몇회인가요?",
+                            "실행 결과",
+                            "yoon : -", "su : ",
+                            "yoon : -", "su : -",
+                            "yoon : --", "su : -",
+                            "최종 우승자 : yoon"
+                    );
+                },
                 MOVING_FORWARD, STOP,
                 STOP, MOVING_FORWARD,
                 MOVING_FORWARD, STOP
