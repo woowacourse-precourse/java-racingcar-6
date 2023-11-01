c46bc4a28c223a38125591ad3a3e8837ba755afa
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-01 21:13:24 +0900

    feat: 자동차가 이동할 횟수 입력 받는 기능 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -7,4 +7,13 @@ public class Input {
     public static String readLine() {
         return Console.readLine();
     }
+
+    public static Integer readInteger() {
+        try {
+            String readLine = Console.readLine();
+            return Integer.parseInt(readLine);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력해야 합니다.");
+        }
+    }
 }
