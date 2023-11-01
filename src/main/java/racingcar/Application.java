61a3fae974caec0e03164bc8855476205ec70e68
nayonsoso <panda0329@naver.com>
2023-11-01 19:24:57 +0900

    feat: main함수에서 실행되게 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import controller.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceController raceController = new RaceController();
+        raceController.run();
     }
 }
