1043bf2b1a3e9489b5d3b0cdde07cfafab5215e1
ghsyn <swanshawn715@gmail.com>
2023-11-01 20:12:33 +0900

    test: implementing car name size under '5' test

--- src/test/java/racingcar/exception/InputCarExceptionTest.java
+++ src/test/java/racingcar/exception/InputCarExceptionTest.java
@@ -1,25 +1,32 @@
 package racingcar.exception;
 
-import org.junit.jupiter.api.BeforeEach;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class InputCarExceptionTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+class InputCarExceptionTest extends NsTest {
 
     @Test
     void 자동차_이름_5자_이하() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,siyeon", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
-    void 입력한_자동차의_개수가_2개_이상() {
+    void 입력한_자동차의_개수가_1개_이하() {
     }
 
     @Test
     void 자동차_이름_공백_제거() {
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
