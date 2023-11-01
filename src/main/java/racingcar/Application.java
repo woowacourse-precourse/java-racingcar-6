2503902d51766bb0495431c2ca4b058e6590d130
rakyun1 <dbskim234@naver.com>
2023-11-01 06:24:18 -0600

    Comment: fix Application Annotaion 1차 종합 피드백에 나온 내용을 수용하여 쓸데 없는 주석 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,125 +8,113 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        int tries;
-        int highestScore;
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carInput = readLine();
-        String[] carNames = validateCarName(carInput);
+	public static void main(String[] args) {
+		int tries;
+		int highestScore;
 
-        List<Car> cars = createCarsFromNames(carNames);
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String carInput = readLine();
+		String[] carNames = validateCarName(carInput);
 
-        System.out.println("시도할 횟수는 몇회인가요?");
-        String tryInput = readLine();
-        tries = validateAndParseTry(tryInput);
+		List<Car> cars = createCarsFromNames(carNames);
 
-        runRace(cars, tries);
+		System.out.println("시도할 횟수는 몇회인가요?");
+		String tryInput = readLine();
+		tries = validateAndParseTry(tryInput);
 
-        highestScore = setHighestScore(cars);
+		runRace(cars, tries);
 
-        printWinner(cars,highestScore);
-    }
+		highestScore = setHighestScore(cars);
 
-    /**
-     * 유저의 차 이름 입력을 받아 유효성을 검사하고 쉼표를 기준으로 나눠 String[]로 리턴
-     * @return String[] carNames
-     */
-    public static String[] validateCarName(String carInput){
-        if (carInput == null || carInput.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
+		printWinner(cars, highestScore);
+	}
 
-        String[] carNames = carInput.split(",", -1);
+	public static String[] validateCarName(String carInput) {
+		if (carInput == null || carInput.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
 
-        if (carNames.length == 0) {
-            throw new IllegalArgumentException();
-        }
+		String[] carNames = carInput.split(",", -1);
 
-        for (String name : carNames){
-            if (name.trim().isEmpty()){
-                throw new IllegalArgumentException();
-            }
-            if (name.length() > 5){
-                throw new IllegalArgumentException();
-            }
-        }
-        return carNames;
-    }
+		if (carNames.length == 0) {
+			throw new IllegalArgumentException();
+		}
 
-    /**
-     * String[]에 들어가 있는 차 이름들을 Car라는 클라스로 생성하면서 List에 담음
-     * @param carNames
-     * @return List<Car> cars
-     */
-    public static List<Car> createCarsFromNames(String[] carNames){
-        List<Car> cars = new ArrayList<>();
-        for (String name : carNames) {
-            cars.add(new Car(name));
-        }
-        return cars;
-    }
+		for (String name : carNames) {
+			if (name.trim().isEmpty()) {
+				throw new IllegalArgumentException();
+			}
+			if (name.length() > 5) {
+				throw new IllegalArgumentException();
+			}
+		}
+		return carNames;
+	}
 
-    /**
-     * 유저의 게임을 시도할 횟수를 입력 받아 유효성을 검사하고 int로 바꿔 리턴
-     * @return int tires
-     */
-    public static int validateAndParseTry(String tryInput){
-        int tries;
-        if (tryInput == null || tryInput.isEmpty() || !tryInput.matches("\\d+")) {
-            throw new IllegalArgumentException();
-        }
-        tries = parseInt(tryInput) ;
-        return tries;
-    }
+	/**
+	 * String[]에 들어가 있는 차 이름들을 Car라는 클라스로 생성하면서 List에 담음
+	 *
+	 * @param carNames
+	 * @return List<Car> cars
+	 */
+	public static List<Car> createCarsFromNames(String[] carNames) {
+		List<Car> cars = new ArrayList<>();
+		for (String name : carNames) {
+			cars.add(new Car(name));
+		}
+		return cars;
+	}
 
-    /**
-     * Car 클라스를 가지고 있는 list를 for each로 나누고
-     * Car 클래스 안에 있는 메소드 사용
-     * @param cars
-     * @param tries
-     */
-    public static void runRace(List<Car> cars, int tries){
-        for (int i = 0; i < tries; i++) {
-            System.out.println("실행 결과 : " + (i+1));
-            for (Car car : cars){
-                int number = Randoms.pickNumberInRange(0,9);
-                boolean canMove = car.canMove(number);
-                car.move(canMove);
-                System.out.println(car.display());
-            }
-            System.out.println();
-        }
-    }
+	public static int validateAndParseTry(String tryInput) {
+		int tries;
+		if (tryInput == null || tryInput.isEmpty() || !tryInput.matches("\\d+")) {
+			throw new IllegalArgumentException();
+		}
+		tries = parseInt(tryInput);
+		return tries;
+	}
 
-    /**
-     * Car클라스 안에 있는 getScore()메소드를 활용하여 최고점 점수 리턴
-     * @param cars
-     * @return int highestScore
-     */
-    public static int setHighestScore(List<Car> cars){
-        int highestScore = 0;
-        for (Car car : cars) {
-            if (car.getScore() > highestScore) {
-                highestScore = car.getScore();
-            }
-        }
-        return highestScore;
-    }
+	/**
+	 * Car 클라스를 가지고 있는 list를 for each로 나누고 랜덤 넘어를 생성해
+	 * canMove메소드를 통해 이동 가능 여부를 체크 후에 move메소드 호출
+	 * @param cars
+	 * @param tries
+	 */
+	public static void runRace(List<Car> cars, int tries) {
+		for (int i = 0; i < tries; i++) {
+			System.out.println("실행 결과 : " + (i + 1));
+			for (Car car : cars) {
+				boolean canMove = car.canMove(Randoms.pickNumberInRange(0, 9));
+				car.move(canMove);
+				System.out.println(car.display());
+			}
+			System.out.println();
+		}
+	}
 
-    /**
-     * List<Car> cars 안에 highestScore 점수와 같은 점수가 있다면 출력
-     * @param cars
-     * @param highestScore
-     */
-    public static void printWinner(List<Car> cars, int highestScore){
-        List<String> winners = new ArrayList<>();
-        for (Car car : cars) {
-            if (car.getScore() == highestScore) {
-                winners.add(car.name);
-            }
-        }
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
-    }
+	/**
+	 * Car클라스 안에 있는 getScore()메소드를 활용하여 최고점 점수 리턴
+	 * @param cars
+	 * @return int highestScore
+	 */
+	public static int setHighestScore(List<Car> cars) {
+		int highestScore = 0;
+		for (Car car : cars) {
+			if (car.getScore() > highestScore) {
+				highestScore = car.getScore();
+			}
+		}
+		return highestScore;
+	}
+
+	public static void printWinner(List<Car> cars, int highestScore) {
+		List<String> winners = new ArrayList<>();
+		for (Car car : cars) {
+			if (car.getScore() == highestScore) {
+				winners.add(car.name);
+			}
+		}
+		System.out.println("최종 우승자 : " + String.join(", ", winners));
+	}
 }
