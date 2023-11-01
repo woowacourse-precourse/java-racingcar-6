e517bf09a0e95636fc6ad1b37e98602fe2e98fb1
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-01 17:06:19 +0900

    refactor: 에러에 에러이유 기재

--- src/main/java/racingcar/InputValid.java
+++ src/main/java/racingcar/InputValid.java
@@ -5,36 +5,37 @@ import java.util.Set;
 
 public class InputValid {
 
-    public static void names(String[] carNames) {
-        
-    	Set<String> uniquecarnames = new HashSet<>();
-        
-        for (int i = 0; i<carNames.length;i++ ) {
-        	String names = carNames[i];
-        
-        	if (names.isEmpty()) {
-        		throw new IllegalArgumentException();
-        	}
-        	if (names.length()>5) {
-        		throw new IllegalArgumentException();
-        	}
-        	if (!uniquecarnames.add(names)) {
-        		throw new IllegalArgumentException();
-        	}
-        	
-        	uniquecarnames.add(names);
-        }
-        
-        
-        
+	public static void names(String[] carNames) {
 
+		Set<String> uniqueCarNames = new HashSet<>();
 
-    }
-    public static void count(int count) {
-    	
-    	if(count<1) {
-    		throw new IllegalArgumentException();
-    	}
-    }
+		for (int i = 0; i < carNames.length; i++) {
+
+			String names = carNames[i];
+
+			if (names.isEmpty()) {
+				throw new IllegalArgumentException("이름을 입력해야해요.");
+			}
+			if (names.length() > 5) {
+				throw new IllegalArgumentException("이름의 길이는 5자를 넘을 수 없어요.");
+			}
+			if (!uniqueCarNames.add(names)) {
+				throw new IllegalArgumentException("중복된 이름은 입력할 수 없어요.");
+			}
+			if (names.startsWith(" ")) {
+				throw new IllegalArgumentException("이름은 공백부터 시작할 수 없어요.");
+			}
+
+			uniqueCarNames.add(names);
+
+		}
+	}
+
+	public static void count(int count) {
+
+		if (count < 1) {
+			throw new IllegalArgumentException("시행 횟수는 1이상이어야 해요.");
+		}
+	}
 
 }
