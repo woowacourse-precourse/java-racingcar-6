8e3009b124e869cddc71bbd43d12fc0ed7c007d2
BDlhj <akk808@naver.com>
2023-11-01 13:19:45 +0900

    refactor: Create Constants.java to separate constants from RacingCarGame.java

---
+++ src/main/java/racingcar/Constants.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Constants {
+    static final String ASKING_FOR_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String COMMA = ",";
+    static final String ASKING_FOR_TRIAL_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    static final String EXECUTION_RESULT_MESSAGE = "실행결과";
+    static final String PROGRESS_EXPRESSION = "-";
+}
