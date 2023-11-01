33c39a894b062b53332bbdb8e2ec735a6eb5adf7
akskflwn <akskflwn@gmail.com>
2023-11-01 15:04:35 +0900

    refactor: CarDto 추가
    dto 추가함으로써 도메인에 불필요해진 코드 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import java.util.Objects;
+import racingcar.domain.dto.CarDto;
 
 public class Car {
 
@@ -28,31 +28,7 @@ public class Car {
         return randomNumber > MOVABLE_THRESHOLD;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Car car = (Car) o;
-        return position == car.position && Objects.equals(name, car.name);
+    public CarDto toDto() {
+        return new CarDto(name.getValue(), position.getValue());
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(position, name);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s : %s", name, POSITION_REGEX.repeat(Math.max(0, position)));
-    }
-
-    @Override
-    public int compareTo(Car otherCar) {
-        return this.position - otherCar.position;
-    }
-
 }
--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,7 +1,5 @@
 package racingcar.domain;
 
-import java.util.Objects;
-
 public class Name {
 
     private static final String NAME_RANGE_EXCEPTION_MESSAGE = " 이름은 5자 이하만 가능합니다.";
@@ -20,25 +18,7 @@ public class Name {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Name name = (Name) o;
-        return Objects.equals(value, name.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
+    public String getValue() {
         return value;
     }
 }
