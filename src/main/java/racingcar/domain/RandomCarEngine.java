f93f3ccd137b36d86f1fc7e4c6770fa09222d0db
kunsanglee <gosmdochee2@gmail.com>
2023-11-01 16:56:51 +0900

    refactor(RandomCarEngine): 선언한 상수의 접근 제어자를 private로 변경

--- src/main/java/racingcar/domain/RandomCarEngine.java
+++ src/main/java/racingcar/domain/RandomCarEngine.java
@@ -3,9 +3,9 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomCarEngine implements CarEngine {
-    public static final int MIN_RANDOM_NUMBER = 0;
-    public static final int MAX_RANDOM_NUMBER = 9;
-    public static final int MIN_START_ENGINE_NUMBER = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MIN_START_ENGINE_NUMBER = 4;
 
     @Override
     public boolean isStart() {
