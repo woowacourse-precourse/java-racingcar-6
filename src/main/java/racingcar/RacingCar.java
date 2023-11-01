2b54fc09ae5af3af80306b167004459ce072a379
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 20:44:00 +0900

    feat : RacingCar 생성자 생성 함수 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,5 +1,11 @@
 package racingcar;
 
 public class RacingCar {
+    private String name;
+    private int position = 0;
 
+    public RacingCar(String name){
+        this.name = name;
+    }
+    
 }
