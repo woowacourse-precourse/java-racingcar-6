8e2712fdc5c3a4f233ab1ad0af337d5b3b5c9300
조은별 <joeunbyeol98@gmail.com>
2023-11-01 12:31:22 +0900

    feat(CarController): 레이싱카 컨트롤러 클래스 정의, 초기 셋팅, 시뮬레이션 호출부 구현

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.dto.Car;
+import racingcar.model.service.CarService;
+import racingcar.model.service.CarServiceImpl;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarController {
+    private CarService carService = CarServiceImpl.getInstance();
+    private InputView inputView = InputView.getInstance();
+    private OutputView outputView = OutputView.getInstance();
+
+    public void run() {
+        initRacingcar();
+        simulRacingcar();
+    }
+
+    private void simulRacingcar() {
+        while (!carService.isRacingcarStop()) {
+            carService.goOrStop();
+            outputView.printAllCarResult(carService.showCarLineupInfo());
+        }
+    }
+
+    private void initRacingcar() {
+        carService.registerCarLineUp(inputView.inputCarNames());
+        carService.registerTryCount(inputView.inputTryCount());
+    }
+}
