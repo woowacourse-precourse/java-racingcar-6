a62fc2f5f52559ca689a1f40eaa01379a2fd09e5
sangxxjin <sj_0368@naver.com>
2023-11-01 14:05:30 +0900

    feat: 실행 결과라는 메시지 출력 메소드 및 메시지 상수처리

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class OutputView {
+    private final static String RESULTMESSAGE = "실행 결과";
+
+    public static void showResultMessage() {
+        System.out.println(RESULTMESSAGE);
+    }
+}
