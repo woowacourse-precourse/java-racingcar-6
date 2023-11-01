074e58e9940cbbb24a4aac3e68baaa470625195f
mung96 <mung5079@gmail.com>
2023-11-01 20:53:16 +0900

    test: 우승자가 1명일 때 테스트
    car1이 car2보다 더 긴 문자열을 가질 때 car1이 최종 우승자로 출력되는지 테스트

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    Game game = new Game();
+    Car car1 = new Car("car1");
+    Car car2 = new Car("car2");
+
+    @Test
+    void 우승자가_1명일때_테스트() {
+        game.players.add(car1);
+        game.players.add(car2);
+
+        car1.state = "----";
+        car2.state = "--";
+        String expected = "최종 우승자 : car1";
+        String result = "최종 우승자 : "+game.printWinners();
+
+        assertEquals(expected,result);
+    }
+}
