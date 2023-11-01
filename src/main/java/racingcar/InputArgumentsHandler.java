b78b4852e3e15028ca41ea39d7b14aaac975e9e3
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-11-01 15:51:31 +0900

    게임 setup 나머지 기능 구현완료, 입력값과 관련된 예외처리
    경주할 자동차의 이름은 5자이하만 가능하게 제한,
    입력값 (자동차이름과 시도할 횟수)과 관련된 예외 처리,
    필요없는 주석 삭제,
    코드 정렬

--- src/main/java/racingcar/InputArgumentsHandler.java
+++ src/main/java/racingcar/InputArgumentsHandler.java
@@ -10,19 +10,15 @@ import static racingcar.CommandLine.inputln;
 
 public class InputArgumentsHandler {
 
-    /**
-     * car 목록 초기화
-     */
     public List<Car> inputCars() {
         String input = inputln("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        validateName(input);
         return mapToCarList(input);
     }
 
-    /**
-     * 시도 횟수 초기화
-     */
     public int inputRoundNumbers() {
         String attempt = inputln("시도할 회수는 몇 회 인가요?");
+        validateAttempt(attempt);
         return Integer.parseInt(attempt);
     }
 
@@ -31,5 +27,36 @@ public class InputArgumentsHandler {
                 .stream(input.split(","))
                 .map(carName -> new Car(carName.trim(), 0))
                 .collect(toList());
+    }
+
+    private void validateName(String input) {
+        if (input.isBlank() || carNameLengthIsOverFive(input)) {
+            throwIllegalArgumentException();
+        }
+    }
+
+    private boolean carNameLengthIsOverFive(String input) {
+        String[] carList = input.split(",");
+        for (String car : carList) {
+            if (car.length() > 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void throwIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+
+    private void validateAttempt(String attempt) {
+        if (attempt.isBlank()) {
+            throwIllegalArgumentException();
+        }
+        try {
+            Integer.parseInt(attempt);
+        } catch (NumberFormatException e) {
+            throwIllegalArgumentException();
+        }
     }
 }
