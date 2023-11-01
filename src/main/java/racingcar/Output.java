c943da2ce07b8a5ebe990acb69d0722ded5be242
Gyumin Sim <gyu2781@gmail.com>
2023-11-01 18:21:55 +0900

    Refactor : 최종 우승자 문자열 상수로 변경

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -9,6 +9,7 @@ public class Output {
     static final String guideCarNameMessage = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     static final String guideTryCountMessage = "시도할 회수는 몇회인가요?";
     static final String resultMessage = "\n실행 결과";
+    static final String winnerMessage = "최종 우승자 : ";
 
     public static void printGuideCarName() {
         System.out.println(guideCarNameMessage);
@@ -30,7 +31,7 @@ public class Output {
     }
 
     public static void printWinner(List<Car> cars) {
-        System.out.print("최종 우승자 : ");
+        System.out.print(winnerMessage);
         for(Car car : cars) {
             System.out.print(car.getName());
             if (cars.indexOf(car) != cars.size() - 1) {
