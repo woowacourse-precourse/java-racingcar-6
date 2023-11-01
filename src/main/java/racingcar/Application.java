81695c00c43244466fd220760092115dc8bb2165
sunwoong <sunwoong1122@gmail.com>
2023-11-01 19:47:19 +0900

    feat: 자동차 경주 게임 시작 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.config.DependencyContainer;
+import racingcar.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DependencyContainer dependencyContainer = new DependencyContainer();
+        MainController mainController = dependencyContainer.getMainController();
+        mainController.startGame();
     }
 }
