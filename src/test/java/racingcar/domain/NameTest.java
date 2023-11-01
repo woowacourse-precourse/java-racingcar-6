56e618c4427f5fe1ec9a03b7a18aae19ba535614
seokmyungham <hellotjraud@gmail.com>
2023-11-01 12:13:48 +0900

    test(NameTest): simpleValue를 이용한 테스트 코드 개선

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameTest {
 
@@ -14,14 +16,11 @@ public class NameTest {
                 .hasMessage("자동차 이름은 5글자 이하만 가능합니다");
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"", "    "})
     @DisplayName("If Car Name Is Blank, Throw Exception")
-    void 이름이_공백일_경우_에러_발생() {
-        Assertions.assertThatThrownBy(() -> new Name(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 공백일 수 없습니다");
-
-        Assertions.assertThatThrownBy(() -> new Name("   "))
+    void 이름이_공백일_경우_에러_발생(String carName) {
+        Assertions.assertThatThrownBy(() -> new Name(carName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 공백일 수 없습니다");
     }
