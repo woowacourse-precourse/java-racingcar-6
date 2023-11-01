5d34eddbad7bc87d8fdbd096cdb91f58d169372a
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-01 20:45:11 +0900

    test($InputProcessor): 쉼표 유효성 테스트
    입력값에 쉼표가 없어도 입력값을 받을 수 있다.

--- src/test/java/study/InputProcessorTest.java
+++ src/test/java/study/InputProcessorTest.java
@@ -1,10 +1,21 @@
 package study;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.InputProcessor;
 
 @DisplayName("입력 프로세서 테스트")
 public class InputProcessorTest {
+
+    @DisplayName("성공: 입력값에 쉼표가 없는 경우")
+    @Test
+    void test1() {
+        System.setIn(new ByteArrayInputStream("pobigokim".getBytes()));
+        InputProcessor inputProcessor = new InputProcessor();
+        assertThatCode(()->inputProcessor.getCarNames())
+                .doesNotThrowAnyException();
+    }
 }
