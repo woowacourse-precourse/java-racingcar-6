69bd559c863bcf192919ba7e8a174f0dea0c574d
leejuno <zhqkfxm0127@naver.com>
2023-11-01 21:21:55 +0900

    refactor(InputController): 불필요한 요소제거

--- src/main/java/racingcar/domain/InputController.java
+++ src/main/java/racingcar/domain/InputController.java
@@ -6,14 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputController {
-    private int attemptCount;
-
-    public int getAttemptCount() {
-        return attemptCount;
-    }
 
     public String insertCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = readLine();
         return carNames;
     }
@@ -34,8 +29,9 @@ public class InputController {
         }
     }
 
-    public void insertAttemptCount() {
+    public int insertAttemptCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        this.attemptCount = Integer.parseInt(readLine());
+        int attemptCount = Integer.parseInt(readLine());
+        return attemptCount;
     }
 }
