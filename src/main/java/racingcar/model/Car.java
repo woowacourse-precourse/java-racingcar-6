2ac14edbc270db43aad05ba368e400cc002e69e9
sangxxjin <sj_0368@naver.com>
2023-11-01 12:32:05 +0900

    feat: Car클래스 생성자 구현 및 의존성 주입

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int distance;
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
