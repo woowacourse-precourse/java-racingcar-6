2496c40551ce88f1ee22b795c0d0c4283f6639ec
witwint <witwint1234@gmail.com>
2023-11-01 16:44:15 +0900

    test: carName입력 테스트 구현

--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -1,77 +1,65 @@
 package racingcar.controller;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RacingCarControllerTest {
 
-	private static final String SUCCESS_CAR_NAME = "aaa,bbb,ccc";
-	private static final String FAIL_LENGTH_CAR_NAME = "aaaaaa,bbb,cc";
-	private static final String FAIL_SPACE_CAR_NAME = "aa aa,bb,cc";
+	private static final String SUCCESS_CAR_NAME = "aaa,bbb,ccc" + "\n" + "6" +"\n";
+	private static final String FAIL_LENGTH_CAR_NAME = "aaaaaa,bbb,cc" +"\n";
+	private static final String FAIL_SPACE_CAR_NAME = "aa aa,bb,cc" + "\n";
+	private static final String FAIL_DUPLICATE_CAR_NAME = "aa,aa,cc" + "\n";
 
-	private InputStream originalIn;
+	@BeforeAll
+	static void setUp() {
+		String testText = SUCCESS_CAR_NAME + FAIL_LENGTH_CAR_NAME + FAIL_SPACE_CAR_NAME +
+			FAIL_DUPLICATE_CAR_NAME;
+		System.setIn(new ByteArrayInputStream(testText.getBytes()));
+	}
+	@org.junit.jupiter.api.Order(1)
+	@Test
+	void testSaveCarName_Success() {
+		RacingCarController controller = new RacingCarController();
+		assertThatCode(() -> controller.startGame())
+			.doesNotThrowAnyException();
+	}
 
-//	@BeforeEach
-//	void setUp() {
-//		originalIn = System.in;
-//		System.out.println("시작");
-//	}
-//
-//	@AfterEach
-//	void tearDown() {
-//		System.setIn(originalIn);
-//		System.out.println("끝");
-//
-//	}
-//
-//	@Test
-//	void testInputCar(){
-//		testSaveCarName_Success();
-//		testSaveCarName_Fail_Length();
-//
-//		testSaveCarName_Fail_Whitespace();
-//
-//	}
-//
-//	void testSaveCarName_Success() {
-//		System.out.println("1");
-//		RacingCarController controller = new RacingCarController();
-//
-//		System.setIn(new ByteArrayInputStream(SUCCESS_CAR_NAME.getBytes()));
-//
-//
-//
-//		assertThatCode(() -> controller.saveCarName())
-//			.doesNotThrowAnyException();
-//	}
-//
-//	void testSaveCarName_Fail_Length() {
-//		System.out.println("2");
-//		RacingCarController controller = new RacingCarController();
-//
-//		System.setIn(new ByteArrayInputStream(FAIL_LENGTH_CAR_NAME.getBytes()));
-//
-//		assertThatThrownBy(() -> controller.saveCarName())
-//			.isInstanceOf(IllegalArgumentException.class)
-//			.hasMessage("유효하지 않은 이름이 포함되어 있습니다.");
-//	}
-//
-//	void testSaveCarName_Fail_Whitespace() {
-//		System.out.println("3");
-//		RacingCarController controller = new RacingCarController();
-//
-//		System.setIn(new ByteArrayInputStream(FAIL_SPACE_CAR_NAME.getBytes()));
-//
-//		assertThatThrownBy(() -> controller.saveCarName())
-//			.isInstanceOf(IllegalArgumentException.class)
-//			.hasMessage("유효하지 않은 이름이 포함되어 있습니다.");
-//	}
+	@org.junit.jupiter.api.Order(2)
+	@Test
+	void testSaveCarName_Fail_Length() {
+		RacingCarController controller = new RacingCarController();
+		assertThatThrownBy(() -> controller.startGame())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("유효하지 않은 이름이 포함되어 있습니다.");
+	}
+
+	@org.junit.jupiter.api.Order(3)
+	@Test
+	void testSaveCarName_Fail_Whitespace() {
+		RacingCarController controller = new RacingCarController();
+		assertThatThrownBy(() -> controller.startGame())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("유효하지 않은 이름이 포함되어 있습니다.");
+	}
+
+	@org.junit.jupiter.api.Order(4)
+	@Test
+	void testSaveCarName_Fail_Duplicate_Name() {
+		RacingCarController controller = new RacingCarController();
+		assertThatThrownBy(() -> controller.startGame())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("중복된 이름이 포함되어 있습니다.");
+	}
+
+
+
 
 }
