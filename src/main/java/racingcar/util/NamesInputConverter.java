99ff3d60752b031a576662b0fbaf60bf70c19c55
binary_ho <dfghcvb11@naver.com>
2023-11-01 20:18:32 +0900

    refactor : NamesInputConverter에 빈 이름 확인 과정 추가

--- src/main/java/racingcar/util/NamesInputConverter.java
+++ src/main/java/racingcar/util/NamesInputConverter.java
@@ -1,5 +1,6 @@
 package racingcar.util;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NamesInputConverter {
@@ -7,8 +8,18 @@ public class NamesInputConverter {
     private NamesInputConverter() {
     }
 
-    public static List<String> convert(String input) {
-        String[] names = input.split(",");
-        return List.of(names);
+    public static List<String> convertToNames(String input) {
+        return Arrays.stream(input.split(","))
+            .filter(NamesInputConverter::isNotEmpty)
+            .filter(NamesInputConverter::isNotBlank)
+            .toList();
+    }
+
+    private static boolean isNotEmpty(String name) {
+        return !name.isEmpty();
+    }
+
+    private static boolean isNotBlank(String name) {
+        return !name.isBlank();
     }
 }
