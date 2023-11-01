63ea58cc90838045418a53c48241b166865171a9
JaeY0ungJung <hanol5216@gmail.com>
2023-11-01 12:30:45 +0900

    feat: add validator to check carNames and trycount input

---
+++ src/main/java/racingcar/Validator.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Validator {
+
+    static void carNames(String[] carNames) throws IllegalArgumentException {
+        for (String carName : carNames) {
+            if (carName.contains(" ")) {
+                throw new IllegalArgumentException();
+            }
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static int tryCountInput(String tryCountString) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(tryCountString);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
