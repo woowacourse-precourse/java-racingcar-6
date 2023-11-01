3ddae4c6b40f3b61b716e4e13f05a311839b6d8f
masonkimseoul <masonkimseoul@gmail.com>
2023-11-01 17:45:46 +0900

    feat : Update input view to check separator in car names

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -5,12 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final int MINIMUM_TRY_COUNT = 1;
+    private static final String SEPARATOR  = ",";
 
     public static String getCarNamesString() {
         String carNames = Console.readLine();
 
         if (carNames.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 한 글자 이상 입력하셔야 합니다.");
+        }
+        if (!carNames.contains(SEPARATOR)) {
+            throw new IllegalArgumentException("혼자서는 경주를 시작할 수 없습니다. 쉼표로 이름을 구분해주세요.");
         }
 
         return carNames;
