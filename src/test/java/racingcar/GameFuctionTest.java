4ca7cc4da80f1d651d8b0ec7caacd67ff9e9f2fd
sosojean <thwlsdl0989@gmail.com>
2023-11-01 22:12:51 +0900

    코드 컨벤션 수정

--- src/test/java/racingcar/GameFuctionTest.java
+++ src/test/java/racingcar/GameFuctionTest.java
@@ -12,12 +12,13 @@ import org.junit.jupiter.api.Test;
 
 
 public class GameFuctionTest {
+
     GameFunction gameFunction = new GameFunction();
     UserInput userInput = new UserInput();
     ValueCheck valueCheck = new ValueCheck();
 
     @Test
-    void 사용자에게_자동차이름_입력받기(){
+    void 사용자에게_자동차이름_입력받기() {
         String input = "1,2";
         String[] expected = input.split(",");
 
@@ -25,8 +26,9 @@ public class GameFuctionTest {
 
         assertThat(result).contains(expected);
     }
+
     @Test
-    void 구분자를_포함하지_않으면_예외발생(){
+    void 구분자를_포함하지_않으면_예외발생() {
 
         //UserInput userInput = new UserInput();
 
@@ -35,35 +37,36 @@ public class GameFuctionTest {
         System.setIn(readLine);
 
         assertThatThrownBy(userInput::getCarNames)
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
 
     }
 
     @Test
-    void 이름이_다섯자_이하인가(){
+    void 이름이_다섯자_이하인가() {
 
         //ValueCheck valueCheck = new ValueCheck();
 
         List<String> input = new ArrayList<>(List.of("aaaaaa,bbbb"));
-        assertThatThrownBy(()-> valueCheck.sizeUnderFive(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> valueCheck.sizeUnderFive(input))
+            .isInstanceOf(IllegalArgumentException.class);
 
     }
+
     @Test
-    void 자동차_전진횟수_숫자가_아닌경우_예외발생(){
+    void 자동차_전진횟수_숫자가_아닌경우_예외발생() {
 
         String input = "a";
         InputStream readLine = new ByteArrayInputStream(input.getBytes());
         System.setIn(readLine);
 
         assertThatThrownBy(userInput::getTurn)
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
 
 
     }
 
     @Test
-    void 매회차마다_차의_전진여부를_합산(){
+    void 매회차마다_차의_전진여부를_합산() {
         List<Car> carList = new ArrayList<>();
         List<Boolean> carCanGo = new ArrayList<>();
 
@@ -76,7 +79,6 @@ public class GameFuctionTest {
         carCanGo.add(false);
 
         gameFunction.applyScore(carList, carCanGo);
-
 
         assertThat(a.score).isEqualTo(1);
         assertThat(b.score).isEqualTo(0);
