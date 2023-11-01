51bb8e175e954bb6a3dc50879f4e62170821c2f7
sora0319 <sojeoung536985@gmail.com>
2023-11-01 16:50:54 +0900

    feat(NameValidation: checkDuplicate, checkCount) : 자동차 이름의 중복값을 찾거나 개수가 2개 이하인지 찾는다

--- src/main/java/racingcar/validator/NameValidation.java
+++ src/main/java/racingcar/validator/NameValidation.java
@@ -1,8 +1,36 @@
 package racingcar.validator;
 
+import java.util.Collections;
+import java.util.List;
+import racingcar.util.InputManufacture;
+
 public class NameValidation {
+
+    public void validate(String input) {
+        checkWord(input);
+        checkDuplicate(input);
+        checkCount(input);
+    }
+
     public void checkWord(String input) {
         if (!input.matches("^([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*$")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkDuplicate(String input) {
+        InputManufacture inputManufacture = new InputManufacture();
+        List<String> carCheckList = inputManufacture.makeNameList(input);
+        for (String s : carCheckList) {
+            if (Collections.frequency(carCheckList, s) > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void checkCount(String input) {
+        String[] carCheckList = input.split((","));
+        if (carCheckList.length < 2) {
             throw new IllegalArgumentException();
         }
     }
