57ea9c42552104b34ee0df0abbab94bd74a110db
HwangJenn <hwangjihyunh@naver.com>
2023-11-01 10:24:51 +0900

    메인 애플리케이션 로직 구현 사용자 입력, 게임 초기화, 게임 실행, 결과 출력, 우승자 판단 등의 주요기능 포함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,20 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Console console = new Console();
+
+        // 사용자로부터 자동차 이름과 이동 횟수를 입력 받는 부분
+        String carNames = console.readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+        int tryCount = console.readInt("시도할 회수는 몇회인가요?: ");
+
+        RacingGame racingGame = new RacingGame(carNames);
+        racingGame.start(tryCount);
+
+        console.println("실행 결과");
+        racingGame.printRaceResults();
+
+        console.println("최종 우승자: " + racingGame.getWinners());
     }
 }
+
+
