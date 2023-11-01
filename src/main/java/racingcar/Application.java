956e136fb012cb64775c5d1feda2d9137134749d
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2023-11-01 15:39:45 +0900

    method/createCars

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         String[] carNames = Service.getCarNamesFromUser();
         int tryCount = Service.getTryCountFromUser();
+
+        List<Car> cars = Service.createCars(carNames);
     }
 
 
--- src/main/java/racingcar/Service.java
+++ src/main/java/racingcar/Service.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
     static String[] getCarNamesFromUser(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -15,5 +18,16 @@ public class Service {
         }catch (NumberFormatException e){
             throw new IllegalArgumentException("잘못된 입력입니다. 회수는 숫자로 입력해주세요.");
         }
+    }
+
+    static List<Car> createCars(String[] carNames){
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
