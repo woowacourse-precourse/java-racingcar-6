aa239995b72d0f27ed0e3a01576cb2d1904f68f1
chaeheon <cogjs3210@naver.com>
2023-11-01 20:32:14 +0900

    feat(main): 사용자 입력 기능 및 출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarNames = readLine();
 
+        System.out.println("시도할 회수는 몇회인가요?");
+        int number = Integer.parseInt(readLine());
+
+        System.out.println("실행 결과");
     }
 }
