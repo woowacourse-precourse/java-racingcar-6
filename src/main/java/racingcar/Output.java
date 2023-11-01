93eb02389286ba544c3be05a2a6295dec67ed175
2021110806 <2021110806@dgu.ac.kr>
2023-11-01 19:31:11 +0900

    feat(Output.java): add lineSeparator before RESULT_TITLE "실행 결과" 메시지를 출력하기 전에 개행을 추가하였습니다.

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class Output {
+    static String lineSeparator = System.lineSeparator();
     static final String CAR_NAME_INPUT_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     static final String TRY_NUMBER_INPUT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
-    static final String RESULT_TITLE = "실행 결과";
+    static final String RESULT_TITLE = lineSeparator + "실행 결과";
     static final String WINNERS = "최종 우승자 : ";
 
     static void printCarNameInputRequest() {
