0db7a15691275a16ee1d33a5403d69880b7ee483
yebin <dev.devin.kr@gmail.com>
2023-11-01 17:24:45 +0900

    feat(game-controller): implement the race for a specified number of laps

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.InputUtil;
+import racingcar.model.RandomUtil;
 import racingcar.view.RaceStartView;
 
 public class GameController {
 
     private List<Car> carObjects;
+    private int lap;
 
     public void run() {
         RaceStartView raceStartView = new RaceStartView();
@@ -21,6 +23,20 @@ public class GameController {
         carObjects = new ArrayList<>();
         for (String carNameStr : carNameStrings) {
             carObjects.add(new Car(carNameStr));
+        }
+
+        lap = Integer.parseInt(raceLap);
+
+        for (int i = 0; i < lap; i++) {
+            oneLap();
+        }
+    }
+
+    private void oneLap() {
+        for (Car car : carObjects) {
+            if (RandomUtil.moveForwardOrNot()) {
+                car.moveForward();
+            }
         }
     }
 }
