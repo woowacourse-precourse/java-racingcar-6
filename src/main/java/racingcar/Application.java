cacd4e79b0d43d056b19e42a54c32cc0e9988eec
hanriverkitty <danieltls@naver.com>
2023-11-01 16:50:42 +0900

    주석처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,18 +15,25 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        //inputUser 인스턴스생성
         InputUser inputUser = new InputUser();
         String userInput = Console.readLine();
+
+        //split을 통한 이름을 나눠 배열에 저장
         List<String> carNames = inputUser.getNames(userInput);
         System.out.println("시도할 회수는 몇회인가요?");
         int num = inputUser.getNumber();
         System.out.println();
+
+        //각각의 차마다 전진과 멈춤 기록이 가능한 인스턴스 생성
         Cars cars = new Cars();
         cars.setCars(carNames);
+
         for (int i=0;i<num;i++){
             cars.move(carNames);
         }
         cars.printWinner(carNames);
-        // TODO: 프로그램 구현
+
     }
 }
