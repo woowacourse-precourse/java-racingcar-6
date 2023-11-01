8d73078956fea9e65998e9db821f479ac9186e5a
sudo-dustle <asdf748596123@gmail.com>
2023-11-01 18:40:21 +0900

    feat: 사용자의 입력값을 받기 위한 Input 클래스 생성

--- src/main/java/racingcar/util/Input.java
+++ src/main/java/racingcar/util/Input.java
@@ -3,7 +3,9 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Input {
@@ -11,50 +13,26 @@ public class Input {
     public static List<String> readCarName() {
         String readLine = Console.readLine();
 
-        validateRegex(readLine);
-
+        InputValidator.validateRegex(readLine);
+        InputValidator.validateCharacter(readLine);
         List<String> carNames = splitNames(readLine);
 
-        validateCarNamesSize(carNames);
-        validateNameLength(carNames);
+        InputValidator.validateDuplicate(carNames);
+        InputValidator.validateCarNamesSize(carNames);
+        InputValidator.validateNameLength(carNames);
 
         return carNames;
     }
 
-    private static void validateCarNamesSize(List<String> carNames) {
-        if(carNames.size() < 2) {
-            throw new IllegalArgumentException("2개 이상 입력해 주세요");
-        }
-    }
-
     private static List<String> splitNames(String readLine) {
         return Arrays.stream(readLine.split(","))
+                .map(String::trim)
                 .collect(Collectors.toList());
-    }
-
-    private static void validateNameLength(List<String> carNames) {
-        for(String carName : carNames) {
-            if(carName.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
-        }
-    }
-
-    private static void validateRegex(String readLine) {
-        if(!readLine.contains(",")) {
-            throw new IllegalArgumentException("쉼표로 구분되어야 합니다.");
-        }
     }
 
     public static int readTryCount() {
         String readLine = Console.readLine();
-        validateDigit(readLine);
+        InputValidator.validateDigit(readLine);
         return Integer.parseInt(readLine);
-    }
-
-    private static void validateDigit(String readLine) {
-        if(!readLine.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-        }
     }
 }
--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -1,31 +1,56 @@
 package racingcar.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class InputTest {
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class InputValidatorTest {
+
+    @Test
     @DisplayName("같은 이름 중복 처리")
     void test1() {
-
+        List<String> cars = List.of("asdf", "asdf");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> InputValidator.validateDuplicate(cars))
+                .withMessage("중복이 존재합니다.");
     }
 
+    @Test
     @DisplayName("5글자 초과 문자 검사")
     void test2() {
-
+        List<String> cars = List.of("asdf", "asdfasdf");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> InputValidator.validateNameLength(cars))
+                .withMessage("이름은 5자 이하만 가능합니다.");
     }
 
+    @Test
     @DisplayName("2개 이상의 경주할 자동차 검사")
     void test3() {
-
+        List<String> cars = List.of("asdf");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> InputValidator.validateCarNamesSize(cars))
+                .withMessage("2개 이상 입력해 주세요");
     }
 
-    @DisplayName("5글자 초과 문자 검사")
-    void test4() {
-
-    }
-
+    @Test
     @DisplayName("쉼표 구분 확인")
-    void test5() {
+    void test4() {
+        String string = "asdf wer wer";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> InputValidator.validateRegex(string))
+                .withMessage("쉼표로 구분되어야 합니다.");
+    }
 
+    @Test
+    @DisplayName("시도 회수가 숫자로 입력되었는지 확인")
+    void test5() {
+        String string = "1, asdf, 3";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> InputValidator.validateDigit(string))
+                .withMessage("숫자만 입력 가능합니다.");
     }
 }
