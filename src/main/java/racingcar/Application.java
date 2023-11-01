d6d2067eb64cd98f6db8f4c789edb240210b3f99
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-11-01 11:49:19 +0900

    기능 구상 1.getCarNames() 이름 부여 ->입력받고 String배열에 , 구분으로 부여 2.getNumberOfMove() 몇 번의 이동을 할 것인지 3.generateRandomNumber() 무작위값 구하기 4.printWinner() 누가 우승했는지 5.startRacingCar() 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        RacingCar racingCar = new RacingCar();
     }
 }
 
 class RacingCar {
-    public RacingCar(){
+    private String[] carNames;
 
+    public RacingCar(){
+        getCarNames();
     }
 
     private void getCarNames(){
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        carNames = names.split(",");
     }
 
     private void getNumberOfMove(){
