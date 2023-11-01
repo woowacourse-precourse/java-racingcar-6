6265d6bff3524d4636d5ff3f952ed1ad9ebe39aa
PeterChoi <94peterchoi@gmail.com>
2023-11-01 22:23:23 +0900

    fix: 랜덤 숫자 생성 시 잘못 설정된 범위 수정

--- src/main/java/racingcar/util/NumberGenerator.java
+++ src/main/java/racingcar/util/NumberGenerator.java
@@ -4,10 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
 
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
     public static int[] generateRandomNumbers(int size) {
         int[] numbers = new int[size];
         for (int i = 0; i < size; i++) {
-            numbers[i] = Randoms.pickNumberInRange(1, 9);
+            numbers[i] = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
         }
 
         return numbers;
