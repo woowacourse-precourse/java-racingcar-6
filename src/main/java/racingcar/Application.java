84cb208944cb176fb6c7de81044356aa7d33cd86
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-01 23:03:39 +0900

    fix : try catch문 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        try {
             RacingGame game = new RacingGame();
             GameConsole.play(game);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
