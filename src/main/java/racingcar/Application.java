30ee1c6971dc5a23a1d93261c8ec6367e6329a7b
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-11-01 20:22:30 +0900

    테스트 통과

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Application {
     public static void main(String[] args) {
         RacingCar racingCar = new RacingCar();
