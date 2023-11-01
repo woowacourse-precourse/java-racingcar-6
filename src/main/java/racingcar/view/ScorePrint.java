ae8280eb80460643d76e00a34471282bffd7b806
동원 <ai06021@gmail.com>
2023-11-01 19:35:05 +0900

    fix : 움직인 만큼 -출력

--- src/main/java/racingcar/view/ScorePrint.java
+++ src/main/java/racingcar/view/ScorePrint.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import java.util.List;
 
 public class ScorePrint {
-    public void resultByOrder(int currentLocation[], List<String> carsName) {
+    public static void resultByOrder(int currentLocation[], List<String> carsName) {
         int carMaxIndex = carsName.size();
         for (int carNumber = 0; carNumber < carMaxIndex; carNumber++) {
             carNamePrint(carsName.get(carNumber));
@@ -11,11 +11,11 @@ public class ScorePrint {
         }
     }
 
-    private void carNamePrint(String name) {
+    private static void carNamePrint(String name) {
         System.out.print(name + " : ");
     }
 
-    private void carMoveDistance(int distance) {
+    private static void carMoveDistance(int distance) {
         while (distance > 0) {
             System.out.print("-");
         }
