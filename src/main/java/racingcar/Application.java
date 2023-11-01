2547e03a96398003efe559de7ffed35e3d8988be
nayeong lee <lny021102@gmail.com>
2023-11-01 09:51:12 +0900

    경주 결과(1등, 2등...)를 출력하는 메소드 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+//인덴테이션(3 이상이면 안됨)에 유의, 3항 연산자 사용금지, 1함수 1업무, 기능별 테스트에 유의
+//기능 단위 커밋하기!!!!
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //기능별 구현 후 main함수 안쪽 구성하기
+    }
+
+    //경주 결과를 출력하는 메소드
+    public static void printRaceResults(String[] carNames, int[][] raceResults) {
+
+        for (int i = 0; i < raceResults.length; i++) {
+            for (int j = 0; j < raceResults[i].length; j++) {
+                String carName = carNames[j];
+                int result = raceResults[i][j];
+                StringBuilder dashes = new StringBuilder(); //stringBuilder 초기화시키기
+
+                for (int k = 0; k < result; k++) {
+                    dashes.append("-"); //랜덤하게 생성한 -를 반복 출력
+                }
+                //자동차 이름 뒤에 ---...가 출력되게 하기
+                System.out.println(carName + " : " + dashes.toString());
+            }
+            System.out.println();
+        }
     }
 }
