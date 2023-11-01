701d024121edd342ebbdd3b9646d6161dfedc815
dragon0622 <iamdragon.dev@gmail.com>
2023-11-01 20:12:43 +0900

    test(racingCar): 테스트 코드 작성, readme update

--- src/test/java/racingcar/exception/RoundExceptionTest.java
+++ src/test/java/racingcar/exception/RoundExceptionTest.java
@@ -1,12 +1,25 @@
 package racingcar.exception;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RoundExceptionTest {
+class RoundExceptionTest extends NsTest {
 
     @Test
     void 정수로입력된라운드수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("april,may", "ㄱㄱ"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
