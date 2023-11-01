aa4253767e3e5322f63944a08685cf7c1c604360
chanwoo <mdsoo55828@gmail.com>
2023-11-01 18:46:20 +0900

    Feat: InputView 자동차 이름 입력 받기 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,11 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
     private static final String  CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -11,6 +17,15 @@ public class InputView {
 
     public void printTimesInputMessage() {
         System.out.println(RACE_COUNT);
+    }
+
+    public List<String> inputCarNames() {
+        String carNames = Console.readLine();
+        List<String> carNameList = Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .toList();
+        Validator.InputCarName(carNameList);
+        return carNameList;
     }
 
 }
