268ff9cc1179f6974675708e5da17a9a32b7b2f6
Park Eunhyeong <silbroeh@gmail.com>
2023-11-01 14:34:02 +0900

    feat : Constants클래스 생성(상수값)

---
+++ src/main/java/racingcar/Constants.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public class Constants {
+    // MESSAGE
+    public static final String CARNAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRIALNUMBER_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_MESSAGE = "실행 결과";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+
+
+    public static final int MOVE_CONDITION_STANDARD = 4;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int MAX_RANDOM_NUMBER = 9;
+    public static final String NUMBER_EXPRESSION = "[0-9]";
+    public final static String CAR_FIELD_LINK = " : ";
+    public final static String DISTANCE_UNIT_STRING = "-";
+    public static final String COMMA = ",";
+    public static final String BLANK = " ";
+
+
+}
