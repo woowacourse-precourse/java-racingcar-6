44ed287755f5759f2d397551ea2b024f2599950c
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-11-01 21:10:22 +0900

    feat: 이름제한 상수 추가

--- src/main/java/racingcar/CarConstants.java
+++ src/main/java/racingcar/CarConstants.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 public class CarConstants {
+    public static final int MIN_NAME_LENGTH=1;
+    public static final int MAX_NAME_LENGTH=5;
     public static final int GO = 5;
     public static final String MESSAGE_INPUT_CARS_NAME="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String MESSAGE_INPUT_REPEAT_NUMBER="시도할 회수는 몇회인가요?";
