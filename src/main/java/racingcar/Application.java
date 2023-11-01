ea7c3c9792574450f102449e324e661380245dcd
rayark1 <max980203@snu.ac.kr>
2023-11-01 21:22:12 +0900

    feat: Add Application class as the entry point of the racing game
    - Create a RaceGame instance in the main() method.
    - Invoke the startGame() method to start the racing game.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RaceGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceGame raceGame = new RaceGame();
+        raceGame.startGame();
     }
 }
