477bc57ccfa51be912a050d2d4ddeab08ed26e50
mingeun0507 <tqdlfkg@naver.com>
2023-11-01 14:56:48 +0900

    feat: Cars 일급 컬렉션의 toString 및 moveAll 정의

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -4,11 +4,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
+    private static final String CAR_DELIMITER = "\n";
+
     private final List<Car> carList;
 
     public Cars(final List<Name> carNameList) {
         validate(carNameList);
         this.carList = carNameList.stream().map(Car::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return carList.stream().map(Car::toString).collect(Collectors.joining(CAR_DELIMITER));
+    }
+
+    public void moveAll() {
+        carList.forEach(Car::move);
     }
 
     private void validate(final List<Name> carNameList) {
