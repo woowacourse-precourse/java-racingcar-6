5aef633745ded406caffce0e05f20a7d3adcf4fe
jhkim <wellsy001012@gmail.com>
2023-11-01 18:36:00 +0900

    feat: 게임 실행하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,25 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.message.GameMessage;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            System.out.println(GameMessage.INPUT_MESSAGE);
-            String input = Console.readLine();
-            CarInput carInput = CarInput.of(input);
-            
+            System.out.println(GameMessage.CARNAME_INPUT);
+            CarInput carInput = CarInput.of(Console.readLine());
+            System.out.println(GameMessage.TRY_COUNT_INPUT);
+            Integer tryCount = Integer.parseInt(Console.readLine());
+            System.out.println();
+
+            RacingGame racingGame = new RacingGame.Builder()
+                    .carInput(carInput)
+                    .tryCount(tryCount)
+                    .build();
+
+            System.out.println(GameMessage.GAME_RESULT);
+            racingGame.play();
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw e;
---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,83 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.message.ErrorMessage;
+import racingcar.message.GameMessage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+    private List<Car> cars;
+    private Integer tryCount;
+    private Integer turnCount;
+
+    private RacingGame(Builder builder) {
+        this.cars = builder.carInput.getCarNames().stream().map(Car::new).collect(Collectors.toList());
+        this.tryCount = builder.tryCount;
+        this.turnCount = 0;
+    }
+
+    public void play() {
+        while(!isGameOver()) {
+            playOneRound();
+            turnCount++;
+        }
+//        printWinner();
+    }
+
+    private void playOneRound() {
+        for(Car car : cars) {
+            moveIfAble(car);
+        }
+        printPosition();
+    }
+
+    private void printPosition() {
+        for(Car car : cars) {
+            car.printPosition();
+        }
+        System.out.println();
+    }
+
+    private void moveIfAble(Car car) {
+        if (isMoveAble(car)) {
+            car.moveForward();
+        }
+    }
+
+    private boolean isMoveAble(Car car) {
+        Integer randNum = Randoms.pickNumberInRange(GameConfig.RANDOM_MIN_NUMBER, GameConfig.RANDOM_MAX_NUMBER);
+        boolean isMoveAble = randNum >= GameConfig.MOVE_CONDITION;
+        boolean atMaxPosition = car.atPosition(tryCount);
+
+        return isMoveAble && !atMaxPosition;
+    }
+
+    private boolean isGameOver() {
+        return this.turnCount.equals(this.tryCount);
+    }
+
+    public static class Builder {
+        private CarInput carInput;
+        private Integer tryCount;
+
+        public Builder Builder() {
+            return this;
+        }
+
+        public Builder carInput(CarInput carInput) {
+            this.carInput = carInput;
+            return this;
+        }
+
+        public Builder tryCount(Integer tryCount) {
+            this.tryCount = tryCount;
+            return this;
+        }
+
+        public RacingGame build() {
+            return new RacingGame(this);
+        }
+    }
+}
