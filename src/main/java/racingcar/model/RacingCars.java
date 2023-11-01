6aa3b6a29f017e66752ae1df8f52be4a40a3ca54
Ukj0ng <xb253q@gmail.com>
2023-11-01 14:52:54 +0900

    feat(RacingCars): 경주에 참가할 자동차 관리 기능 추가
    추가한 기능:
    - 경주에 참여할 자동차 리스트 반환
    - 경주에 참여할 자동차 이름 반환
    - 우승자 판별

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,16 +1,39 @@
-package racingcar.model;
+    package racingcar.model;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.Comparator;
+    import java.util.List;
+    import java.util.stream.Collectors;
 
-public class RacingCars {
-    private final List<Car> racingCars;
+    public class RacingCars {
+        private final List<Car> racingCars;
 
-    public RacingCars() {
-        this.racingCars = new ArrayList<>();
+        public RacingCars() {
+            this.racingCars = new ArrayList<>();
+        }
+
+        public void addCar(Car car) {
+            racingCars.add(car);
+        }
+
+        public List<Car> getRacingCars() {
+            return racingCars;
+        }
+
+        public List<Car> findWinners() {
+            int maxDistance = racingCars.stream()
+                .max(Comparator.comparingInt(Car::getMove))
+                .map(Car::getMove)
+                .orElse(0);
+
+            return racingCars.stream()
+                .filter(car -> car.getMove() == maxDistance)
+                .collect(Collectors.toList());
+        }
+
+        public List<String> getCarNames() {
+            return racingCars.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        }
     }
-
-    public void addCar(Car car) {
-        racingCars.add(car);
-    }
-}
