d91e80e8e1420b87d337dc4522d6fc0de3d22f7f
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-01 17:51:53 +0900

    chore : 랜덤 넘버 시작, 끝 상수로 처리 해주기

--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -3,8 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
+    private static final int RANDOM_NUMBER_START = 0;
+    private static final int RANDOM_NUMBER_FINISH = 9;
+
     public int createRandomNumber(){
-        int random = Randoms.pickNumberInRange(0,9);
+        int random = Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_FINISH);
         return random;
     }
 }
