cba7754bb6fa90407274c9694232fe5493c80fed
Concepty <khs1903b@gmail.com>
2023-11-01 09:15:27 +0900

    feat($RacingGame): change states
    Modify how $RacingGame tells its state
    - RacingGameState enum class is deleted
    - RacingGameState member is deleted
    - $RacingGame only tells if it's finished
    
    Simplify state announcing interfaces of $RacingGame since state of
    class might require developers to consider class specific
    implementation. Instead, isFinished method tells if the object is
    finished (which I think more comprehensive).

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -7,26 +7,23 @@ import racingcar.entity.Car;
 
 public class RacingGame {
 
-    public enum RacingGameState {
-        READY_TO_START, RUNNING, RESULT_READY
-    }
-
     // Game Configuration fields
     private static final int RANDOM_MIN = 0;
     private static final int RANDOM_MAX = 9;
     private static final int THRESHOLD = 4;
-
-    private RacingGameState state;
 
     private List<Car> cars;
 
     private int remainingRounds;
 
     protected RacingGame(String[] names, int rounds) {
-        this.state = RacingGameState.READY_TO_START;
         this.cars = new ArrayList<>();
         List.of(names).forEach(n -> cars.add(new Car(n)));
         this.remainingRounds = rounds;
+    }
+
+    protected boolean isFinished() {
+        return remainingRounds == 0;
     }
 
     protected void tryToProceedCars() {
@@ -37,7 +34,6 @@ public class RacingGame {
     private int generateRandomIntInRange() {
         return Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
     }
-
     private boolean isOverThreshold(int generatedInt) {
         return generatedInt >= THRESHOLD;
     }
