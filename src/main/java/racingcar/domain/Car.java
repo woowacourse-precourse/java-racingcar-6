adf197a2e56b2f37ed741ee5b1d1453ae9a5f556
minu512 <minuet1215@jbnu.ac.kr>
2023-11-01 15:55:53 +0900

    feat: Car 도메인 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+public class Car {
+    private final String nickname;
+    private int position = 0;
+    private final int MAXIMUM_NICKNAME_LENGTH = 5;
+
+    Car(String nickname) {
+        validate(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validate(String nickname) {
+        validateNicknameLength(nickname);
+        validateIsBlank(nickname);
+    }
+
+    private void validateIsBlank(String nickname) {
+        if (nickname.isBlank()) {
+            throw new IllegalArgumentException("공백만 입력할 수는 없습니다.");
+        }
+    }
+
+    private void validateNicknameLength(String nickname) {
+        if (nickname.length() > MAXIMUM_NICKNAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하로 입력해야 합니다.");
+        }
+    }
+}
