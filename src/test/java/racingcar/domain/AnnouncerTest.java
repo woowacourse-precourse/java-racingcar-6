f56622e468f100ecbc67f98c00dc1610f16b89ab
leejuno <zhqkfxm0127@naver.com>
2023-11-01 22:36:50 +0900

    refactor(Announcer): 반복 코드 제거

--- src/test/java/racingcar/domain/AnnouncerTest.java
+++ src/test/java/racingcar/domain/AnnouncerTest.java
@@ -5,22 +5,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class AnnouncerTest {
+    private Announcer announcer;
+    private Car car1;
+    private Car car2;
+
+    @BeforeEach
+    void setUp() {
+        announcer = new Announcer();
+        car1 = new Car("pobi");
+        car2 = new Car("jun");
+    }
 
     @Test
     @DisplayName("우승자 발표 기능 테스트")
     void displayWinner_우승자_발표_기능_테스트() {
-        Announcer announcer = new Announcer();
-        Car car1 = new Car("pobi");
-        Car car2 = new Car("jun");
         List<Car> winner = Arrays.asList(car1, car2);
 
         StringBuffer actual = announcer.displayWinner(winner);
 
         assertThat(actual.toString())
-                .isEqualTo(new StringBuffer("최종 우승자 : pobi, jun").toString());
+                .isEqualTo("최종 우승자 : pobi, jun");
+    }
+
+    @Test
+    @DisplayName("우승자 발표 기능 테스트 (솔로우승)")
+    void displayWinner_우승자_발표_기능_테스트_솔로우승() {
+        List<Car> winner = Arrays.asList(car1);
+
+        StringBuffer actual = announcer.displayWinner(winner);
+
+        assertThat(actual.toString())
+                .isEqualTo("최종 우승자 : pobi");
     }
 }
