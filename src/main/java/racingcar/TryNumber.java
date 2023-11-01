346160081008343ce298b0617b6a6d645870ebc8
carpediem365 <k2309123@gmail.com>
2023-11-01 15:05:53 +0900

    feat(racingcar): 시도  횟수 문자입력 예외 처리 구현

--- src/main/java/racingcar/TryNumber.java
+++ src/main/java/racingcar/TryNumber.java
@@ -5,7 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class TryNumber {
     public int getTryNubmers() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int tryNubmer = Integer.parseInt(Console.readLine());
-        return tryNubmer;
+        String inputTryNubmer = Console.readLine();
+        checkInputTryNubmer(inputTryNubmer);
+        return Integer.parseInt(inputTryNubmer);
     }
+
+    public static void checkInputTryNubmer(String inputTryNumer) {
+        try {
+            Integer.parseInt(inputTryNumer);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자만 입력해주세요. ");
+        }
+
+    }
+
 }
