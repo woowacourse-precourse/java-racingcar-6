2fec396dade9478cfedc3f109acb9afdf1b0ea54
kariskan <zkfzpf56@naver.com>
2023-11-01 13:54:18 +0900

    feat: 자동차 이름 검증 부분 일급 컬렉션으로 이전

--- src/main/java/racingcar/domain/RacingCarList.java
+++ src/main/java/racingcar/domain/RacingCarList.java
@@ -8,8 +8,14 @@ import racingcar.global.utils.generator.RandomNumberGenerator;
 
 public record RacingCarList(List<RacingCar> racingCars) {
 
+	private static final int MAX_LENGTH_OF_CAR_NAME = 5;
+	private static final int MIN_LENGTH_OF_CAR_NAME = 0;
 	private static final int MIN_COUNT_OF_ADVANCE = 0;
 	private static final int MOVING_FORWARD = 4;
+
+	public RacingCarList {
+		validateCarName(racingCars);
+	}
 
 	public int getMaxAdvanceCount() {
 		return racingCars.stream()
@@ -39,5 +45,17 @@ public record RacingCarList(List<RacingCar> racingCars) {
 		if (randomNumber >= MOVING_FORWARD) {
 			racingCar.increaseCount();
 		}
+	}
+
+	private void validateCarName(List<RacingCar> racingCars) {
+		racingCars.stream()
+				.map(RacingCar::getName)
+				.filter(this::isInValidCarNameLength)
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException(OutputType.EXCEPTION.getComment()));
+	}
+
+	private boolean isInValidCarNameLength(String carName) {
+		return carName.length() > MAX_LENGTH_OF_CAR_NAME || carName.length() == MIN_LENGTH_OF_CAR_NAME;
 	}
 }
--- src/main/java/racingcar/domain/service/ClientService.java
+++ src/main/java/racingcar/domain/service/ClientService.java
@@ -7,15 +7,11 @@ import java.util.List;
 import racingcar.global.utils.console.ConsoleUtil;
 
 public class ClientService {
-
-	private static final int MAX_LENGTH_OF_CAR_NAME = 5;
-	private static final int MIN_LENGTH_OF_CAR_NAME = 0;
 	private static final String SPLITTER = ",";
 
 	public List<String> getCarsName() {
 		String input = ConsoleUtil.input();
 		return Arrays.stream(input.split(SPLITTER))
-				.peek(this::validateCarName)
 				.toList();
 	}
 
@@ -28,16 +24,6 @@ public class ClientService {
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(EXCEPTION.getComment());
 		}
-	}
-
-	private void validateCarName(String carName) {
-		if (isInValidCarNameLength(carName)) {
-			throw new IllegalArgumentException(EXCEPTION.getComment());
-		}
-	}
-
-	private boolean isInValidCarNameLength(String carName) {
-		return carName.length() > MAX_LENGTH_OF_CAR_NAME || carName.length() == MIN_LENGTH_OF_CAR_NAME;
 	}
 
 	private void validateNumberOfAttempt(int numberOfAttempt) {
