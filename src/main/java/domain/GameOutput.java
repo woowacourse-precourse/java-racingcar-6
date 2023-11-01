e9f92afd494c19e21eade9bfbb209b256b42f31b
Sur0n <skyzephyr333@gmail.com>
2023-11-01 16:32:12 +0900

    feat : Write down GameOutput

--- src/main/java/domain/GameOutput.java
+++ src/main/java/domain/GameOutput.java
@@ -1,14 +1,26 @@
 package domain;
+import java.util.List;
+import validate.ValidateGo;
+import static constant.GameUserInterface.*;
 
 public class GameOutput {
 
 
+//    public static void
 
-    public static void carNameOutput(){
+    public static void carNameOutput(String carname){
+        System.out.print(carname + CAR_NAME_COLON.getUserInterface());
 
     }
-    public static void carDistance(){
+    public static void carDistance(String distance){
+        System.out.println(" "+distance);
+    }
 
+    public static void gameResult(String Winner){
+        System.out.print( RACING_WINNER.getUserInterface()
+                + CAR_NAME_COLON.getUserInterface()
+                + " "
+                + Winner);
     }
 
 
