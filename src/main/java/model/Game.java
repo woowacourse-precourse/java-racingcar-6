14785d754244afd92e589ff9a18656ce76adf632
yeahdy <yejine98@naver.com>
2023-11-01 22:10:07 +0900

    Feat(getMaxStepCount()): 가장 많이 움직인 전진 수 메소드 구현

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,49 +1,30 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Game {
     public static int tryCount;
 
-    /** 사용자 시도 횟수 저장 */
-    public static void setTryCount(int tryCount) {
-        Game.tryCount = tryCount;
-    }
-
-    /** 사용자 시도 횟수 얻기 */
-    public static int getTryCount() {
-        return tryCount;
-    }
-
     /**
-     * 자동차 실행 결과 가공하기
+     * 가장 많이 움직인 전진 수
      *
-     * @param carList 경주한 자동차와 이동횟수를 담은 List
-     * @return "자동차명 : 전진횟수" 형태로 가공하여 리턴
+     * @param carList 경주한 자동차들의 결과를 담은 List
+     * @return 경주한 자동차 중 가장 많이 움직인 전진 수 리턴
      */
-    public List<String> getRacingGameResultList(List<Car> carList){
-        List<String> racingGameResultList = new ArrayList<>();
+    public int getMaxStepCount(List<Car> carList){
+        int[] stepCountArray = carList.stream()
+                .mapToInt(Car::getStepCount)
+                .toArray();
 
-        for(Car car : carList){
-            String step = "";
-            int stepCount = car.getStepCount();
-            while (stepCount > 0){
-                stepCount--;
-                step += "-";
-            }
-
-            StringBuilder gameText = new StringBuilder(car.getName())
-                    .append(" : ")
-                    .append(step);
-            racingGameResultList.add(gameText.toString());
-        }
-
-        return racingGameResultList;
+        int maxStepCount = Arrays.stream(stepCountArray)
+                .max()
+                .orElse(0);
+        return maxStepCount;
     }
 
-//    우승자 판단하기()
-//    우승자 결과 데이터 가공하기()
+
+
 
 }
 
