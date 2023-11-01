ae1ef059c25237dfd401133299126af224a709a0
hunm719 <hunm719@naver.com>
2023-11-01 19:26:50 +0900

    요구 사항에 맞게 전체적인 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,13 +7,11 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNamesInput = Console.readLine();
+        String carNamesInput = inputCarNames();
         validateCarNames(carNamesInput);
         List<String> carNames = List.of(carNamesInput.split(","));
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        int attempts = Integer.parseInt(Console.readLine());
+        int attempts = inputAttempts();
 
         List<Car> cars = createCars(carNames);
 
@@ -27,12 +25,14 @@ public class Application {
         Console.close();
     }
 
-    private static List<Car> createCars(List<String> carNames) {
-        List<Car> cars = new ArrayList<>();
-        for (String name : carNames) {
-            cars.add(new Car(name));
-        }
-        return cars;
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int inputAttempts() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 
     private static void validateCarNames(String carNamesInput) {
@@ -44,12 +44,24 @@ public class Application {
         }
     }
 
+    private static List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
     private static void startRacing(int attempts, List<Car> cars) {
         for (int i = 0; i < attempts; i++) {
-            for (Car car : cars) {
-                car.move();
-            }
+            moveCars(cars);
             printCurrentPositions(cars);
+        }
+    }
+
+    private static void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            car.move();
         }
     }
 
@@ -61,7 +73,15 @@ public class Application {
     }
 
     private static List<String> determineWinners(List<Car> cars) {
-        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        int maxPosition = calculateMaxPosition(cars);
+        return findWinners(cars, maxPosition);
+    }
+
+    private static int calculateMaxPosition(List<Car> cars) {
+        return cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+    }
+
+    private static List<String> findWinners(List<Car> cars, int maxPosition) {
         List<String> winners = new ArrayList<>();
         for (Car car : cars) {
             if (car.getPosition() == maxPosition) {
@@ -73,6 +93,10 @@ public class Application {
 
     private static void printWinners(List<String> winners) {
         System.out.print("최종 우승자 : ");
+        printWinnerNames(winners);
+    }
+
+    private static void printWinnerNames(List<String> winners) {
         for (int i = 0; i < winners.size(); i++) {
             if (i != 0) {
                 System.out.print(", ");
