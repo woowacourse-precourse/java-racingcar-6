3b411eba4469dd1fcdd9f6e3f81a109ce62d5ffd
jiyeon <[leeyeon0908@gmail.com]>
2023-11-01 22:00:40 +0900

    data 보여주기

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,4 @@
 package racingcar;
-
 public class OutputView {
     public static final String INTRO_INPUT_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INTRO_INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
