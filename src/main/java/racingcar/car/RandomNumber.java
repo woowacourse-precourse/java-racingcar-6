6b229f1559e4aeb23706ef74c288228e98319ad2
th0267 <yth0779@naver.com>
2023-11-01 23:11:10 +0900

    feat:랜덤 함수 구현

--- src/main/java/racingcar/car/RandomNumber.java
+++ src/main/java/racingcar/car/RandomNumber.java
@@ -1,2 +1,9 @@
-package racingcar.car;public class RandomNumber {
+package racingcar.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+    public static int pickRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
