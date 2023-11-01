b11656b23f26b4892c8628e488a3a5eafe3754a9
Hyyena <sjtxm0320@gmail.com>
2023-11-01 22:07:22 +0900

    ✨ feat: 입력 기능

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine().split(",");
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+}
