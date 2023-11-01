4ac41cf4be5488eb0a774fa213abb664385fc3f7
zangsu <zangsu_@naver.com>
2023-11-01 14:03:24 +0900

    test : refactor test to use ParameterizedTest

--- src/test/java/racingcar/model/CarNameTest.java
+++ src/test/java/racingcar/model/CarNameTest.java
@@ -2,6 +2,8 @@ package racingcar.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constant.ExceptionMessage;
 
 class CarNameTest {
@@ -20,17 +22,11 @@ class CarNameTest {
                 .withMessage(ExceptionMessage.CAR_NAME_CANT_NOT_BLANK);
     }
 
-    @Test
-    public void 이름은_빈_값일_수_없다() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    public void 이름은_공백일_수_없다(String name) {
         Assertions.assertThatIllegalArgumentException()
-                .isThrownBy(() -> new CarName(""))
-                .withMessage(ExceptionMessage.CAR_NAME_CANT_NOT_BLANK);
-    }
-
-    @Test
-    public void 이름은_공백일_수_없다() {
-        Assertions.assertThatIllegalArgumentException()
-                .isThrownBy(() -> new CarName(" "))
+                .isThrownBy(() -> new CarName(name))
                 .withMessage(ExceptionMessage.CAR_NAME_CANT_NOT_BLANK);
     }
 }
