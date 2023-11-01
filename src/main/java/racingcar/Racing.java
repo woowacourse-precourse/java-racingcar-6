b180194656836261af699338ed0964f59ed10f4f
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-01 13:43:33 +0900

    feat: print the game start message

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Racing {
+    public void run() {
+        IOHandler ioHandler = new IOHandler();
+        ioHandler.printComment("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
