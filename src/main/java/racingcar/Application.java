c871875118df4dd435f827248771f79ecb2aa279
dustjq1004 <dustjq1005@naver.com>
2023-11-01 16:47:48 +0900

    yeonsup/feat : RacingCarGame 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.race();
+        racingGame.result();
     }
 }
