5c0c9f08fb52177b78f7d335f8f81fa6b03f4286
mywnajsldkf <mywnajsldkf@gmail.com>
2023-11-01 22:57:55 +0900

    refactor : static 제어자 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,11 +3,12 @@ package racingcar.view;
 import racingcar.enums.Messages;
 
 public class OutputView {
-    public void printInputRacingCar() {
+    static StringBuilder sb;
+    public static void printInputRacingCar() {
         System.out.println(Messages.INPUT_RACING_CAR_MESSAGE.getMessage());
     }
 
-    public void printInputTryNumber() {
+    public static void printInputTryNumber() {
         System.out.println(Messages.INPUT_TRY_NUMBER_MESSAGE.getMessage());
     }
 }
