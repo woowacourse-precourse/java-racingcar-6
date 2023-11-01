e5cb16fae2b7093dd3d6086931462ede5806804a
heondong9265 <heondong0723@gmail.com>
2023-11-01 22:17:19 +0900

    test: 수정...

---
+++ src/test/java/racingcar/ApplicationTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTest {
+
+    @Test
+    void testApplicationMain() {
+        // Arrange (준비)
+        // 아래 코드는 프로그램을 실행하는 방식을 테스트하는 방법입니다.
+        // 별도의 JUnit 테스트 메소드를 사용하여 main 메소드를 직접 호출하지 않습니다.
+        // 대신 프로그램을 실행하기 위한 다른 방법을 사용합니다.
+        // 예를 들어, System.out 대신 다른 방식으로 표준 출력을 캡처하고 테스트합니다.
+
+        // Act (실행)
+        // 프로그램을 실행하기 위한 방식을 여기에 추가합니다.
+        // main 메서드 호출 대신 다른 방식으로 프로그램을 실행합니다.
+        // 예를 들어, 메서드 호출 대신 프로그램을 실행할 수 있습니다.
+    }
+}
