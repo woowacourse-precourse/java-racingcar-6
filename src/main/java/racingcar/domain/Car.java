0003dcd695dff910cdd77397a4b343850399a671
IDpluto <d0013159@gmail.com>
2023-11-01 10:31:28 +0900

    refactor: Car class의 static으로 선언된 createCar 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,42 +2,28 @@ package racingcar.domain;
 
 import racingcar.util.RandomNumberGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car {
 
-  RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-  private static final int MOVE_THRESHOLD = 4;
-  private String name;
-  private int progressCount;
+    private static final int MOVE_THRESHOLD = 4;
+    private String name;
+    private int progressCount;
 
-  public Car(String name) {
-    this.name = name;
-    this.progressCount = 0;
-  }
-
-  public boolean moveForwardCar() {
-    int randomNumber = randomNumberGenerator.generateUniqueNumbers();
-    if (randomNumber >= MOVE_THRESHOLD) {
-      this.progressCount++;
+    public Car(String name) {
+        this.name = name;
+        this.progressCount = 0;
     }
-    return stopCar();
-  }
 
-  public boolean stopCar() {
-    return false;
-  }
+    public void moveForwardCar(int randomNumber) {
+        if (randomNumber >= MOVE_THRESHOLD) {
+            this.progressCount++;
+        }
+    }
 
-  public static Car createCar(String name) {
-    return new Car(name);
-  }
-  public String getCarName() {
-    return this.name;
-  }
+    public String getCarName() {
+        return this.name;
+    }
 
-  public int getProgressCount() {
-    return this.progressCount;
-  }
-
+    public int getProgressCount() {
+        return this.progressCount;
+    }
 }
