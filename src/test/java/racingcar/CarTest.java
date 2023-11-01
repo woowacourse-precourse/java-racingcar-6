4d529f8feb26207d9ca9bbbaf8d381b9cd1c12ed
jjaegii <hn06038@gmail.com>
2023-11-01 19:27:04 +0900

    refactor: import 정렬, 들여쓰기 정렬

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,21 +1,23 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 public class CarTest {
+
     @Test
     void move_forward_when_random_number_is_greater_than_or_eqaul_to_4() {
         int canMove = 4;
         assertRandomNumberInRangeTest(
-            () -> {
-                Car car = new Car("testCar");
-                int prevPosition = car.getPosition();
-                car.move();
-                assertThat(car.getPosition() - prevPosition).isEqualTo(1);
-            },
-            canMove
+                () -> {
+                    Car car = new Car("testCar");
+                    int prevPosition = car.getPosition();
+                    car.move();
+                    assertThat(car.getPosition() - prevPosition).isEqualTo(1);
+                },
+                canMove
         );
     }
 
@@ -23,13 +25,13 @@ public class CarTest {
     void stay_in_position_when_random_number_is_less_than_4() {
         int cantMove = 3;
         assertRandomNumberInRangeTest(
-            () -> {
-                Car car = new Car("testCar");
-                int prevPosition = car.getPosition();
-                car.move();
-                assertThat(car.getPosition() - prevPosition).isEqualTo(0);
-            },
-            cantMove
+                () -> {
+                    Car car = new Car("testCar");
+                    int prevPosition = car.getPosition();
+                    car.move();
+                    assertThat(car.getPosition() - prevPosition).isEqualTo(0);
+                },
+                cantMove
         );
     }
 }
