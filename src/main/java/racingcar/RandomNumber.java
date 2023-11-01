de23b747149ac35097bfa4220d5005a3062c4d9b
yev <ye897@naver.com>
2023-11-01 20:42:41 +0900

    랜덤 숫자 위치만큼 print

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -6,16 +6,19 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
     //랜덤 숫자 생성하기
-    public int getRandomNumber(){
+    public static int getRandomNumber(){
         int randomNum = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
         return randomNum;
     }
     
     //전진
-    public void locate(int randomNum){
+    public static void locate(int randomNum){
         int location = 0;
         if (randomNum >= GO_RANDOM_NUMBER) {
             location += 1;
+        }
+        for (int i = 0; i < location; i++) {
+            System.out.print("-");
         }
     }
 }
