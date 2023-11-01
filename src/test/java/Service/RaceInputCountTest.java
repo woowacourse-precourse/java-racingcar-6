d88ce58463d27fa8a5b45d0a71309e66ada072f0
KwangHo <skh990427@naver.com>
2023-11-01 13:44:51 +0900

    test: 레이스 회수 입력 0 이하값 테스트

--- src/test/java/Service/RaceInputCountTest.java
+++ src/test/java/Service/RaceInputCountTest.java
@@ -18,6 +18,19 @@ class RaceInputCountTest {
                 .hasMessage("올바른 숫자 형식이 아닙니다.");
     }
 
+    @Test
+    @DisplayName("레이스 회수 입력값이 숫자가 0이하면 예외처리")
+    public void CheckInputCountNum() {
+        //given
+        String inputString = "0";
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> numCheck(inputString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도할 회수는 0 이하일 수 없습니다.");
+    }
+
     private void numCheck(String inputString) {
         try {
             int gameCount = Integer.parseInt(inputString);
