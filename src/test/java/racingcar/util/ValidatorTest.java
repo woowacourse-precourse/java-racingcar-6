ecf16efaa3e2f46f4a674fb9165a027232bffe21
1015s <1015_s@naver.com>
2023-11-01 00:28:48 -0700

    feat(ValidatorTest) : 자동차이름 길이, 중복 예외 처리 테스트와 이동횟수 0 입력 테스트

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,2 +1,43 @@
-package racingcar.util;public class ValidatorTest {
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    private final Validator validator = new Validator();
+
+    @Test
+    void 자동차이름_1글자_미만_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.carNamesInput("a,b, ,c"))
+                        .isInstanceOf(IllegalArgumentException.class));
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.carNamesInput("a,b,,c"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 자동차이름_5글자_초과_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.carNamesInput("abcdef,g"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+    
+    @Test
+    void 자동차이름_중복_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.carNamesInput("a,a"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 이동횟수_0_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.tryCountInput("0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
