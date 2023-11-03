4ed49b1093a9e8428f96d6efd4def0047d4a7691
LEE-sh1673 <lsh901673@gmail.com>
2023-11-03 17:16:29 +0900

    refactor: 사용되지 않는 메서드를 삭제한다

--- src/main/java/racingcar/model/CarPosition.java
+++ src/main/java/racingcar/model/CarPosition.java
@@ -32,21 +32,4 @@ public class CarPosition {
     public int getPosition() {
         return position;
     }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final CarPosition other = (CarPosition) o;
-        return position == other.position;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(position);
-    }
 }
