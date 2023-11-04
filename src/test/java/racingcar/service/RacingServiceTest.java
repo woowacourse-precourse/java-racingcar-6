896cb7942c208f655bcc16a67858c09f755835d4
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-05 04:04:26 +0900

    test(service.RacingService) : 기능4. 전체 라운드를 진행한다
    playAllRounds 메서드가 실제로 전체 라운드를 진행하는 지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,4 +63,28 @@ class RacingServiceTest {
     }
 
 
+    @Test
+    @DisplayName("기능4 테스트: playAllRounds 메서드 실제로 전체 라운드를 진행하는지 확인한다.")
+    void playAllRoundsShouldInvokePlayRoundAsManyAsTotalRound() {
+        // given
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car(carName1, 0));
+        carList.add(new Car(carName2, 0));
+        carList.add(new Car(carName3, 0));
+
+        int totalCount = 5;
+
+        // when
+        String result = racingService.playAllRounds(carList, totalCount).toString();
+
+        int countCarName1 = StringUtils.countOccurrences(result, carName1);
+        int countCarName2 = StringUtils.countOccurrences(result, carName2);
+        int countCarName3 = StringUtils.countOccurrences(result, carName3);
+
+        // then
+        assertThat(countCarName1).isEqualTo(totalCount);
+        assertThat(countCarName2).isEqualTo(totalCount);
+        assertThat(countCarName3).isEqualTo(totalCount);
+    }
 }
