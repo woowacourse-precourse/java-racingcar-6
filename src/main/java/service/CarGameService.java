68d6f848cf16625cd1aa163a17b8cff0329e560a
yeahdy <yejine98@naver.com>
2023-11-04 19:16:12 +0900

    docs(CarGameService): Service 클래스 역할 정의, 메소드 기능 설명 보충

--- src/main/java/service/CarGameService.java
+++ src/main/java/service/CarGameService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import model.Car;
 import model.Game;
 
+/**
+ * 서로 역할을 가진 Car 와 Game 도메인을 함께 사용하여 기능을 만드는 클래스
+ */
 public class CarGameService {
     Game game = new Game();
 
@@ -12,11 +15,12 @@ public class CarGameService {
      * 경주 할 자동차 목록 만들기
      *
      * @param carNames 사용자가 입력한 경주한 자동차 이름들
-     * @return 각 자동차 이름을 분리하여 List에 담아 리턴한다.
+     * @return 각 자동차 이름을 분리하여 리스트에 담아 리턴한다.
      */
     public List<Car> getCarList(String carNames){
         List<Car> carList = new ArrayList<>();
         String[] carNameArray = carNames.split(",");
+
         for(String carName : carNameArray){
             carName = carName.trim();
             carList.add(new Car(carName));
@@ -39,14 +43,15 @@ public class CarGameService {
     }
 
     /**
-     * 자동차 경주 한판의 전진 결과
-     *
+     * <p>자동차 경주 한판의 전진 결과</p>
      * 각 자동차들은 임의의 값을 기준으로 전진하거나 멈춘다.
-     * @return List<Car>
+     *
+     * @return List<Car> 각 자동차들의 경주 결과를 담아서 리턴
      */
     private List<Car> getRacingGameResult(List<Car> carList){
         for(Car car : carList){
             int randomNumber = car.getRandomNumber();
+
             if(car.isStepForward(randomNumber)){
                 car.MoveForward();
             }
@@ -58,8 +63,8 @@ public class CarGameService {
     /**
      * 자동차 실행 결과 가공하기
      *
-     * @param carList 경주한 자동차와 이동횟수를 담은 List
-     * @return "자동차명 : 전진횟수" 형태로 가공하여 리턴
+     * @param carList 경주한 자동차와 이동횟수를 담은 리스트
+     * @return "{자동차명} : {전진횟수}" 형태로 가공하여 리턴
      */
     public List<String> getRacingGameResultList(List<Car> carList){
         List<String> racingGameResultList = new ArrayList<>();
@@ -67,6 +72,7 @@ public class CarGameService {
         for(Car car : carList){
             String step = "";
             int stepCount = car.getStepCount();
+
             while (stepCount > 0){
                 stepCount--;
                 step += "-";
@@ -90,8 +96,8 @@ public class CarGameService {
     public String getRacingGameWinner(List<Car> carList){
         int maxStepCount = game.getMaxStepCount(carList);
         String getWinnerList = game.getWinnerList(carList, maxStepCount);
+
         return getWinnerList;
     }
-
 
 }
