332f4eccfbfc3ac50d4d0a072af74bdfb71c669c
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-05 03:57:04 +0900

    feat(service.RacingService) : 기능4. 전체 라운드를 진행한다
    전체 라운드를 진행해주는 playAllRounds 메서드를 구현하였습니다. OutputView를 억지로 쓰려다 보니 코드가 복잡해져서 마음이 아팠습니다.

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -11,6 +11,8 @@ public class RacingService {
     // 비즈니스 규칙, 데이터 처리, 외부 시스템과의 상호작용 등을 처리하는 데 주로 사용
     // 도메인 객체를 사용하여 비즈니스 로직을 구현
     // 필요에 따라 데이터 베이스 또는 외부 서비스와 상호 작용
+    private final String PLAY_RESULT = "실행 결과";
+    private final String NEW_LINE = "/n";
 
     public List<String> playRound(List<Car> carList) {
         List<String> result = new ArrayList<>();
@@ -23,4 +25,18 @@ public class RacingService {
 
         return result;
     }
+
+    public List<String> playAllRounds(List<Car> carList, int totalRound) {
+        List<String> result = new ArrayList<>();
+
+        for (int round = 0; round < totalRound; round++) {
+            List<String> roundResult = playRound(carList);
+            roundResult.add(NEW_LINE);
+            result.addAll(roundResult);
+        }
+
+        return result;
+    }
+
+
 }
