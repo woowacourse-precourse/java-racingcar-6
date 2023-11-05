ed695c4c64009c91a76cf1a82a0c0e50ae9170e7
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-06 00:41:37 +0900

    test(main) : 메인 메서드 작성
    메인 메서드를 작성했습니다.
    
    ※ 고민 : racingController 인스턴스를 생성하는 부분이 마음에 들지 않네요 ㅜ.ㅜ

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,39 +1,12 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
-import racingcar.domain.Car;
-import racingcar.domain.ResultGenerator;
-import racingcar.domain.UserInputParser;
-import racingcar.view.InputView;
-
-import java.util.List;
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
 
 public class Application {
-    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String HOW_MANY_ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-
     public static void main(String[] args) {
-
-        UserInputParser userInputParser = new UserInputParser();
-
-        String carNameInput = InputView.readInput(CAR_NAME_INPUT_MESSAGE);
-        List<Car> carList = userInputParser.generateCarList(carNameInput);
-
-        String roundInput = InputView.readInput(CAR_NAME_INPUT_MESSAGE);
-        int totalRound = userInputParser.parseInt(roundInput);
-
-        ResultGenerator resultGenerator = new ResultGenerator();
-
-        System.out.println();
-        System.out.println("실행 결과");
-
-        for (int round = 0; round < totalRound; round++) {
-            resultGenerator.playRound(carList);
-            System.out.println();
-        }
-
-        List<String> winnerList = resultGenerator.generateWinnerList(carList);
-        resultGenerator.printFinalResult(winnerList);
+        RacingService racingService = new RacingService();
+        RacingController racingController = new RacingController(racingService);
+        racingController.play();
     }
 }
