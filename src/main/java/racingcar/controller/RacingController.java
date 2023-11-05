cd6ffa4f71f86b50067f7e32183cef58a1b40dcf
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-06 02:18:23 +0900

    style(controller.RacingController) : 상수와 메서드 위치 이동
    상수 -> 멤버변수 -> 생성자 -> 메서드 순으로 작성하라는 피드백에 따라 위치 조정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -9,12 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingController {
-    private final RacingService racingService;
     public static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String HOW_MANY_ROUND_INPUT = "시도할 회수는 몇회인가요?";
     public static final String PLAY_RESULT = "실행 결과";
     public static final String WRONG_CAR_NAME_ERROR_MESSAGE = "차량 이름은 5글자 이하이어야 합니다.";
     public static final String NOT_A_INTEGER_NUMBER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
+
+    private final RacingService racingService;
 
     public RacingController(RacingService racingService) {
         this.racingService = new RacingService();
@@ -33,6 +34,15 @@ public class RacingController {
     public int registerTotalRound() {
         String userInput = readInput(HOW_MANY_ROUND_INPUT);
         return userInputToInt(userInput);
+    }
+
+    public void playRacing(List<Car> carList, int totalRound) {
+        List<String> result = new ArrayList<>();
+
+        result.add(PLAY_RESULT);
+        result.addAll(racingService.playAllRounds(carList, totalRound));
+
+        printResult(result);
     }
 
     public void announceWinner(List<Car> carList) {
@@ -71,15 +81,6 @@ public class RacingController {
                 throw new IllegalArgumentException(WRONG_CAR_NAME_ERROR_MESSAGE);
             }
         }
-    }
-
-    public void playRacing(List<Car> carList, int totalRound) {
-        List<String> result = new ArrayList<>();
-
-        result.add(PLAY_RESULT);
-        result.addAll(racingService.playAllRounds(carList, totalRound));
-
-        printResult(result);
     }
 
 
