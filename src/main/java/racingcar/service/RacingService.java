70ec8cbf6fd289740c1feed477607180fe03d1dc
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-05 23:56:57 +0900

    fix(service.RacingService) : 상수값 변경 및 수정
    playRacing 테스트 도중에 NEW_LINE 상수 때문에 두줄이나 띄어지는 문제점이 발생하였습니다. System.out.println("\n")을 하면 두줄을 띄운다는 것을 알게되어 NEW_LINE을 WHITE_SPACE로 변경하였습니다.

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,7 +2,6 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +10,9 @@ public class RacingService {
     // 비즈니스 규칙, 데이터 처리, 외부 시스템과의 상호작용 등을 처리하는 데 주로 사용
     // 도메인 객체를 사용하여 비즈니스 로직을 구현
     // 필요에 따라 데이터 베이스 또는 외부 서비스와 상호 작용
-    private final String PLAY_RESULT = "실행 결과";
+
     private final String FINAL_RESULT = "최종 우승자 : ";
-    private final String NEW_LINE = "/n";
+    private final String WHITE_SPACE = "";
     private final String COMMA = ",";
 
     public List<String> playRound(List<Car> carList) {
@@ -33,7 +32,7 @@ public class RacingService {
 
         for (int round = 0; round < totalRound; round++) {
             List<String> roundResult = playRound(carList);
-            roundResult.add(NEW_LINE);
+            roundResult.add(WHITE_SPACE);
             result.addAll(roundResult);
         }
 
