ae5a11f93abcc7973ea5b451fdb559e13fe8ed4f
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-05 22:15:09 +0900

    test(controller.RacingController) : 자동차의 이름이 6글자 이상이면 에러를 발생시킨다
    자동차 이름을 정상적으로 입력했을 때와 자동차의 이름이 6글자 이상인 자동차가 있을 때로 나누어서 validateCarName이 정상적으로 동작하는 지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/racingcar/controller/RacingControllerTest.java
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,7 +27,7 @@ class RacingControllerTest {
         String[] carArray = racingController.userInputToCarArray(userInput);
 
         // then
-        Assertions.assertThat(carArray).isEqualTo(new String[]{"A"});
+        assertThat(carArray).isEqualTo(new String[]{"A"});
     }
 
     @Test
@@ -39,6 +40,29 @@ class RacingControllerTest {
         String[] carArray = racingController.userInputToCarArray(userInput);
 
         // then
-        Assertions.assertThat(carArray).isEqualTo(new String[]{"A","B"});
+        assertThat(carArray).isEqualTo(new String[]{"A", "B"});
+    }
+
+    @Test
+    @DisplayName("기능10 테스트: 자동차 이름이 정상일 경우 validateCarName는 에러를 발생시키지 않는다.")
+    public void validateCarNameDoesNotThrowIllegalArgumentExceptionWhenCarNameIsNormal() {
+        // given
+        String[] carList = new String[]{"A", "B", "CDEF"};
+
+        // when, then
+        assertThatCode(() -> racingController.validateCarNames(carList))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("기능11 테스트: 자동차 이름이 6글자 이상인 경우 validateCarName 메서드는 IllegalArgument Exception을 발생킨다.")
+    public void validateCarNameThrowIllegalArgumentExceptionWhenCarNameIsLongerThanSixWords() {
+        // given
+        String[] carList = new String[]{"A", "B", "CDEFGH"};
+
+        // when, then
+        assertThatThrownBy(() -> racingController.validateCarNames(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(RacingController.WRONG_CAR_NAME_ERROR_MESSAGE);
     }
 }
