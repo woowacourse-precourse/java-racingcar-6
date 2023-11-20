6960cc10a2921b7e9b53e823f762bf390be40e2d
Hoo-sung <gntjd135@gmail.com>
2023-11-20 09:45:21 +0900

    test(CarsTest): getCars 메소드 테스트 코드 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,13 +1,19 @@
 package racingcar.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.subclass.MockCar;
 import racingcar.domain.subclass.MockCars;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +23,7 @@ class CarsTest {
     class SingleWinnerTests {
         @ParameterizedTest
         @CsvSource({"4,3,2,pobi", "1,2,3,jun", "2,5,4,woni"})
-        void 단독우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
+        void 단독_우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
 
             List<MockCar> cars = new ArrayList<>();
             cars.add(new MockCar("pobi", pobiDistance));
@@ -35,7 +41,7 @@ class CarsTest {
     class MultipleWinnersTests {
         @ParameterizedTest
         @CsvSource(value = {"4:4:4:pobi,woni,jun", "4:2:4:pobi,jun", "0:1:1:woni,jun"}, delimiter = ':')
-        void 공동우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
+        void 공동_우승자인경우(int pobiDistance, int woniDistance, int junDistance, String expectedWinners) {
             List<MockCar> cars = new ArrayList<>();
             cars.add(new MockCar("pobi", pobiDistance));
             cars.add(new MockCar("woni", woniDistance));
@@ -45,6 +51,26 @@ class CarsTest {
             List<String> answer = raceCars.determineWinner();
             List<String> expected = List.of(expectedWinners.split(","));
             assertThat(answer).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class getCarsTest{
+        @DisplayName("getCars() 메소드 테스트")
+        @ParameterizedTest
+        @MethodSource("parameterProvider")
+        void getCars_메소드_테스트(List<MockCar> cars){
+            MockCars raceCars = new MockCars(cars);
+            Assertions.assertThat(raceCars.getCars()).isEqualTo(cars);
+        }
+
+        private Stream<Arguments> parameterProvider() {
+            return Stream.of(
+                    Arguments.of(List.of(new MockCar("pobi",4))),
+                    Arguments.of(List.of(new MockCar("pobi",4),new MockCar("enji",3))),
+                    Arguments.of(List.of(new MockCar("pobi",4),new MockCar("woni",3),new MockCar("jun",2)))
+            );
         }
     }
 
