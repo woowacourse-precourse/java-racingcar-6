5b558a621a4e66be9003e0afb79dd0814c2b2e62
심석현 <shim9597@gmail.com>
2023-11-20 14:37:05 +0900

    feat: 자동차 이름 검증
    5자이하인지, 공백 혹은 빈문자열인지

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,9 +1,21 @@
 package racingcar.domain.car;
 
+import racingcar.utils.Validator;
+
 public class Name {
+    private static final String NAME_BY_OVERSIZE = "[ERROR] 이름은 5자 이하로 입력해주세요.";
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
+        Validator.validateIsBlank(name);
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(NAME_BY_OVERSIZE);
+        }
     }
 }
