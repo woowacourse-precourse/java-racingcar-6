bc52b0a9116ace6a4cf9672931316313559cdace
심석현 <shim9597@gmail.com>
2023-11-20 15:39:59 +0900

    feat: 게임 진행 문구

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,11 @@ import racingcar.utils.Validator;
 
 public class InputView {
 
+    private static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRIAL_NUMBER = "시도할 회수는 몇회인가요?";
+
     public List<String> getCarNames() {
+        System.out.println(REQUEST_CAR_NAMES);
         String input = Console.readLine();
         Validator.validateIsBlank(input);
 
@@ -18,6 +22,7 @@ public class InputView {
     }
 
     public int getTrialNumber() {
+        System.out.println(REQUEST_TRIAL_NUMBER);
         String input = Console.readLine();
         Validator.validateIsBlank(input);
         Validator.validateIsInteger(input.trim());
