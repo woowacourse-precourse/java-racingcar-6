3abe993fd6b4cd3b6c2ed62d7af7eb95fd8efa8d
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-21 14:40:05 +0900

    feat: Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public final class Application {
     public static void main(final String[] args) {
+
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+        final RaceController raceController = new RaceController(inputView, outputView);
+
+        raceController.run();
     }
 }
