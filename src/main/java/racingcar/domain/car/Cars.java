637d73a65ba1ce84014cc88cc8884d97f12be6dd
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-21 14:38:17 +0900

    feat: `Cars` dto 변환 및 최대 position

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,9 +1,12 @@
-package racingcar.domain.dto.car;
+package racingcar.domain.car;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import racingcar.domain.dto.CarDto;
-import racingcar.domain.dto.RoundDto;
+import racingcar.controller.dto.CarDto;
+import racingcar.controller.dto.RoundDto;
+import racingcar.controller.dto.WinnerDto;
+import racingcar.controller.dto.WinnersDto;
 import racingcar.domain.move.Move;
 import racingcar.domain.move.MovePicker;
 
@@ -22,7 +25,6 @@ public final class Cars {
         return new Cars(cars);
     }
 
-    // moveAllBy
     public void moveAllBy(final MovePicker movePicker) {
         for (final Car car : cars) {
             final Move move = movePicker.pick();
@@ -30,11 +32,30 @@ public final class Cars {
         }
     }
 
+    private int maxPosition() {
+        return cars.stream()
+                .max(Comparator.comparingInt(Car::getPosition))
+                .orElseThrow(IllegalStateException::new)
+                .getPosition();
+    }
+
+
     public RoundDto toRoundDto() {
         final List<CarDto> carsDto = cars.stream()
                 .map(Car::toCarDto)
                 .toList();
 
         return new RoundDto(carsDto);
+    }
+
+    public WinnersDto toWinnersDto() {
+        final int maxPosition = maxPosition();
+
+        final List<WinnerDto> winners = cars.stream()
+                .filter(c -> c.getPosition() == maxPosition)
+                .map(Car::toWinnerDto)
+                .toList();
+
+        return new WinnersDto(winners);
     }
 }
