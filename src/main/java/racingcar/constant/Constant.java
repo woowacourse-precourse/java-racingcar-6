d23f72f98099150b79dcaa2d81e7de7adbb95a86
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-21 09:54:00 +0900

    :sparkles: [Feat] 상수값 관리

---
+++ src/main/java/racingcar/constant/Constant.java
@@ -0,0 +1,16 @@
+package racingcar.constant;
+
+public enum Constant {
+    ZERO(0),
+    ONE(1),
+    Four(4),
+    Five(5),
+    NINE(9);
+
+
+    public int number;
+
+    Constant(int number) {
+        this.number = number;
+    }
+}
