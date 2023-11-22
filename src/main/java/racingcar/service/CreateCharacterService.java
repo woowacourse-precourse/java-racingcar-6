9d113d82612d275e7d8576ce426f7ed73506feda
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-22 20:27:46 +0900

    :recycle: [Refactor] 랜덤 값 생성 리팩토링

--- src/main/java/racingcar/service/CreateCharacterService.java
+++ src/main/java/racingcar/service/CreateCharacterService.java
@@ -1,8 +1,12 @@
 package racingcar.service;
 
+import static racingcar.constant.Constant.ZERO;
+
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
+import racingcar.model.DriveStrategy;
+import racingcar.model.RandomDriveStrategy;
 import racingcar.view.input.InputView;
 import racingcar.view.output.OutputView;
 
@@ -20,8 +24,9 @@ public class CreateCharacterService {
         outputView.printOutCarName();
         List<String> carNames = inputView.inputCarName();
         List<Car> cars = new ArrayList<>();
+        DriveStrategy driveStrategy = new RandomDriveStrategy();
         for (String carName : carNames) {
-            Car car = new Car(carName);
+            Car car = new Car(driveStrategy, carName, ZERO.number);
             cars.add(car);
         }
 
