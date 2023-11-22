4dbfabf8336481246c56571ce3b55c58b0ebcf44
Moonjunwoong <myc0611@naver.com>
2023-11-23 01:48:12 +0900

    feat : 차 이름 가져오기

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.Model;
+
+public class Car {
+    String CarsName;
+    public void setCarsName(String CarsName){
+        this.CarsName = CarsName;
+    }
+
+    public String getCarsName(String CarsName){
+        return CarsName;
+    }
+}
