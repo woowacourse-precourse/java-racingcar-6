dd4e84665ca2ff97b00ed3778ed9ce20d99ce4e8
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2023-11-28 11:31:09 +0900

    refactor : 코드 리팩토링

--- src/main/java/racingcar/Service/InputCarsNameService.java
+++ src/main/java/racingcar/Service/InputCarsNameService.java
@@ -5,21 +5,19 @@ import racingcar.Model.Message;
 import racingcar.Util.CheckNameException;
 
 public class InputCarsNameService {
+    
     public String[] inputStringCarsName() {
         System.out.println(Message.INPUT_CARS_NAME);
         return InputString();
     }
+    
     private String[] InputString() {
         String CarsName = Console.readLine();
         CheckNameException.carsNameExceptionCheck(CarsName);
         return splitCarsNameByDelimiter(CarsName);
     }
+    
     private String[] splitCarsNameByDelimiter(String CarsName){
         return CarsName.split(",");
     }
-
-
-
-
-
 }
