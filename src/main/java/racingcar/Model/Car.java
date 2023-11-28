11a80ae727c04dacdf1e6307da07ac8d64999e02
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2023-11-28 11:29:45 +0900

    refactor : 코드 리팩토링

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -3,6 +3,7 @@ package racingcar.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    
     private String name;
     private int position;
 
@@ -10,9 +11,11 @@ public class Car {
         this.name = name;
         this.position = 0;
     }
+    
     public int createRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
+    
     public void move(){
         if(createRandomNumber()>=4){
             position++;
