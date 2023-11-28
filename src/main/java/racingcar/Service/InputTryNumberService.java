fe72982529bc70d06f874457a9a7bffb66f066e4
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2023-11-28 11:31:25 +0900

    refactor : 코드 리팩토링

--- src/main/java/racingcar/Service/InputTryNumberService.java
+++ src/main/java/racingcar/Service/InputTryNumberService.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.Util.CheckNumberException;
 
 public class InputTryNumberService {
+    
     public int input(){
         String input = Console.readLine();
         CheckNumberException.numberValidation(input);
