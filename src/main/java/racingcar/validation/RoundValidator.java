d2b13de37df6e4e4192ecc0b7e225840ea9922f8
JongHyunJung <jjh19960115@gmail.com>
2023-11-28 22:46:25 +0900

    refactor: 정규식을 Pattern 정적 객체로 변경

--- src/main/java/racingcar/validation/RoundValidator.java
+++ src/main/java/racingcar/validation/RoundValidator.java
@@ -2,6 +2,8 @@ package racingcar.validation;
 
 import racingcar.model.Round;
 
+import java.util.regex.Pattern;
+
 import static racingcar.type.PlayType.MAX_NUM;
 import static racingcar.type.PlayType.MIN_NUM;
 import static racingcar.type.message.ErrorMessageType.INVALID_TYPE;
@@ -9,6 +11,7 @@ import static racingcar.type.message.ErrorMessageType.INVALID_RANGE;
 
 public class RoundValidator implements Validator {
     public static final String VALID_RANGE = "^["+ MIN_NUM + "-" + MAX_NUM +"]+";
+    private static final Pattern NUMBER = Pattern.compile(VALID_RANGE);
     @Override
     public boolean support(Class<?> clazz) {
         return Round.class.isAssignableFrom(clazz);
@@ -21,7 +24,8 @@ public class RoundValidator implements Validator {
     }
 
     private void validateType(String value){
-        if (value != null && !value.matches(VALID_RANGE)){
+//        if (value != null && !value.matches(VALID_RANGE)){
+        if (value != null & !NUMBER.matcher(value).matches()){
             throw new IllegalArgumentException(INVALID_TYPE.getErrorMessageValue());
         }
     }
