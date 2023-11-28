148a058a8db26ed51bf738457c5a90c62845b408
JongHyunJung <jjh19960115@gmail.com>
2023-11-28 19:59:27 +0900

    refactor: RacingCar Application 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,15 @@
 package racingcar;
 
+import racingcar.application.RacingCarApplication;
 import racingcar.controller.RacingCarController;
 import racingcar.view.InputViewImpl;
 import racingcar.view.OutputViewImpl;
 
 public class Application {
     public static void main(String[] args) {
-        RacingCarController racingCarController = new RacingCarController(new InputViewImpl(), new OutputViewImpl());
-        racingCarController.run();
+//        RacingCarController racingCarController = new RacingCarController(new InputViewImpl(), new OutputViewImpl());
+//        racingCarController.run();
+        RacingCarApplication racingCarApplication = new RacingCarApplication();=
+        racingCarApplication.run();
     }
 }
