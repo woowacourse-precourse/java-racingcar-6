d9421e71c9f5ebc4995b2a7e3b783436d4064773
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2023-11-28 11:32:15 +0900

    refactor : 코드 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,9 +6,11 @@ import racingcar.Service.GameManager;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         GameManager gameManager = new GameManager();
+        
         RacingGame racingGame = new RacingGame(gameManager.inputCarsName(),gameManager.inputTryNumber());
+        
         racingGame.playRacingGame();
     }
 }
