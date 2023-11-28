5222ff4bef909ae1ab469d8ef7bf05f54f7578ac
JongHyunJung <jjh19960115@gmail.com>
2023-11-28 22:41:37 +0900

    test: 올바른 범위 랜덤 숫자 생성 테스트

---
+++ src/test/java/racingcar/util/RandomNumberGeneratorTest.java
@@ -0,0 +1,19 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.service.RandomNumberGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumberGeneratorTest {
+    @DisplayName("올바른 범위의 랜덤한 숫자 생성")
+    @Test
+    void checkRandomNumberGenerate(){
+        RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
+        int t = 10;
+        while (t --> 0){
+            assertThat(numberGenerator.generate()).isBetween(0,9);
+        }
+    }
+}
