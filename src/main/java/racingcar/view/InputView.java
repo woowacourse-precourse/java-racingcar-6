069c5b3a168c2b164b958fa006da3a5adf99b20b
minjun7410 <minjun7410@gmail.com>
2023-11-30 15:41:43 +0900

    refactor(InputView, OutputView): 입력 문구 이동
    * OutputView에 있던 입력하기 전 출력 문구를 InputView로 이동

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,14 +8,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
+    private static final String TRIAL_NUMBER_INPUT_TEXT = "시도할 회수는 몇회인가요?\n";
+    private static final String CAR_NAME_INPUT_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+
     public TrialNumber getTrialNumber() {
-        OutputView.printTrialNumberInputText();
+        System.out.print(TRIAL_NUMBER_INPUT_TEXT);
         String trialNumberInput = Console.readLine().trim();
         return new TrialNumber(trialNumberInput);
     }
 
     public Cars getCarsFromUser() {
-        OutputView.printCarNameInputText();
+        System.out.print(CAR_NAME_INPUT_TEXT);
         List<String> carNames = splitInputByComma(Console.readLine());
         return new Cars(carNames);
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 public class OutputView {
     private static final String FIRST_SHOWN_RESULT_TEXT = "실행 결과\n";
-    private static final String TRIAL_NUMBER_INPUT_TEXT = "시도할 회수는 몇회인가요?\n";
-    private static final String CAR_NAME_INPUT_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
     private static final String FIRST_SHOWN_WINNER_TEXT = "최종 우승자 : ";
 
     public void printWinners(List<String> winners) {
@@ -27,13 +25,5 @@ public class OutputView {
 
     public void printFirstShownResultText() {
         System.out.print(FIRST_SHOWN_RESULT_TEXT);
-    }
-
-    public void printTrialNumberInputText() {
-        System.out.print(TRIAL_NUMBER_INPUT_TEXT);
-    }
-
-    public void printCarNameInputText() {
-        System.out.print(CAR_NAME_INPUT_TEXT);
     }
 }
