dca08b19572efec6e79b6b348bf916249f01c399
rakyun1 <dbskim234@naver.com>
2023-12-08 00:07:53 -0700

    Test: add testHowMayMoved() in CarTest.java howManyMoved 함수를 테스트 하는 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,14 +2,22 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+	@BeforeEach
+	void setUp() {
+		car = new Car("Test");
+		car.addMove(true);
+	}
 	Car car;
 	@Test
 	void testAddMove(){
-		car = new Car("Test");
-		car.addMove(true);
 		assertEquals(car.moved.get(0), "-");
+	}
+	@Test
+	void testHowManyMoved(){
+		assertEquals(car.howManyMoved(), 1);
 	}
 }
