99fd3a0997c4f1c0d910ed9b2b6429358992a4a8
gahyeon <1000ji1@naver.com>
2023-12-10 21:26:15 +0900

    feat : Create Cars class to registerCars

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,9 +1,48 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
-    
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void registerCars(String carNames) {
+        String[] names = checkValid(carNames);
+        for (String name : names) {
+            addCar(name);
+        }
+    }
+
+    private String[] checkValid(String carNames) {
+        String[] names = carNames.split(",");
+        Validator.checkMinimumParticipants(names);
+        trimSpaces(names);
+        Validator.isNameDuplicate(names);
+
+        for (String name : names) {
+            Validator.isSpace(name);
+            Validator.checkNameLength(name);
+        }
+
+        return names;
+    }
+
+    public void trimSpaces(String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            names[i] = names[i].trim();
+        }
+    }
+
+    private void addCar(String name) {
+        Car car = new Car(name);
+        cars.add(car);
+    }
+
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
+    }
 }
