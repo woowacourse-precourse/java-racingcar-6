bf40421e1210d9e75adaba9c82e7dccc0a373bfa
MingyeomKim <mingyum119@naver.com>
2023-12-14 16:27:46 +0900

    feat: 자동차의 정보를 저장하는 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private Name name;
+    private int moved;
+
+    public Car(Name name) {
+        this.name = name;
+        this.moved = 0;
+    }
+}
