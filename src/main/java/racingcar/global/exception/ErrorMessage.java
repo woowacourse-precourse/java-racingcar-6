f561bc765db7608a158208c7dc093c4312250e45
MingyeomKim <mingyum119@naver.com>
2023-12-14 16:27:22 +0900

    feat: 커스텀 예외처리 클래스 구현

---
+++ src/main/java/racingcar/global/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package racingcar.global.exception;
+
+public enum ErrorMessage {
+    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
+    INVALID_LENGTH_ERROR("잘못된 길이의 문자열을 입력하였습니다. "),
+    DUPLICATED_CAR_ERROR("자동차의 이름이 중복되었습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
