24a4aede9452b018eee6da8caeb4f131ed78115d
asd <p_caso@naver.com>
2024-01-01 18:39:42 +0900

    커밋이 없다는 에러가 떠서 다시 포크해서 프로젝트를 새로 진행하느라 커밋이 없습니다 기존 프로젝트도 삭제해서 https://github.com/woowacourse-precourse/java-racingcar-6/pull/1543 참고해주세요

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int number) {
+        if (isMovePossible(number)) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getPositionString() {
+        return "-".repeat(position);
+    }
+
+    private boolean isMovePossible(int number) {
+        return number >= 4;
+    }
+}
