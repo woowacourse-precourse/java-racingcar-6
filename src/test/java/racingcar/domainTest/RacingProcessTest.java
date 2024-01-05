92a5440ab1c86d32e8ec9768a8e054cc69cba85a
Lee-Minju <mju7513@gmail.com>
2024-01-05 17:22:51 +0900

    RacingProcessTest 에 사용된 배열을 리스트로 수정

--- src/test/java/racingcar/domainTest/RacingProcessTest.java
+++ src/test/java/racingcar/domainTest/RacingProcessTest.java
@@ -17,18 +17,20 @@ public class RacingProcessTest {
     void 자동차목록을_받아서_진행도를_출력하는_기능_검사() {
         RacingProcess race = new RacingProcess();
         List<Car> carList = new ArrayList<>();
-        String[] names = {"pobi", "minju", "woni"};
-        int[] progress = {1, 2, 0};
+        List<String> names = new ArrayList<>(List.of("pobi", "minju", "woni"));
+        List<Integer> progress = new ArrayList<>(List.of(1, 2, 0));
+        List<String> results;
+
         for (int i = 0; i < 3; i++) {
-            carList.add(new Car(names[i], progress[i]));
+            carList.add(new Car(names.get(i), progress.get(i)));
         }
-        String[] results = new String[carList.size()];
-        for (int i = 0; i < results.length; i++) {
-            results[i] = race.makingStatusBar(carList.get(i));
+        results = new ArrayList<>(carList.size());
+        for (int i = 0; i < results.size(); i++) {
+            results.add(i, race.makingStatusBar(carList.get(i)));
         }
 
-        assertThat(results[0]).contains("pobi : -");
-        assertThat(results[1]).contains("minju : --");
-        assertThat(results[2]).contains("woni : ");
+        assertThat(results.get(0)).contains("pobi : -");
+        assertThat(results.get(1)).contains("minju : --");
+        assertThat(results.get(2)).contains("woni : ");
     }
 }
